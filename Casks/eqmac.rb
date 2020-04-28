2d5a0c35f1760aa2b515281b294802cb025e6914
John Stader <47798010+jstader@users.noreply.github.com>
2020-04-27 21:29:56 -0400

    Update eqmac.rb

--- Casks/eqmac.rb
+++ Casks/eqmac.rb
@@ -7,20 +7,20 @@ cask 'eqmac' do
   name 'eqMac'
   homepage 'https://github.com/bitgapp/eqMac/releases/'
 
-  app "eqMac.app"
+  app 'eqMac.app'
   installer script: {
                       executable: "#{staged_path}/eqMac.app/Contents/Resources/install_driver.sh",
                       sudo:       true,
                     }
 
-  uninstall quit:   "com.bitgapp.eqMac",
+  uninstall quit:   'com.bitgapp.eqMac',
             script: {
                       executable: "#{appdir}/eqMac.app/Contents/Resources/uninstall_driver.sh",
                       sudo:       true,
                     }
 
   zap trash: [
-               "~/Library/Caches/com.bitgapp.eqMac",
-               "~/Library/Cookies/com.bitgapp.eqMac.binarycookies",
+               '~/Library/Caches/com.bitgapp.eqMac',
+               '~/Library/Cookies/com.bitgapp.eqMac.binarycookies',
              ]
 end
