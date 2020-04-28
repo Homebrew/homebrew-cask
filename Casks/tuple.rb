06640887f5907540bb4eff090c4ef3ed1617024f
Randall <ran.dall@icloud.com>
2020-04-28 11:01:50 -0700

    Update tuple from 0.70.1,2020-04-19-22d584e5 to 0.71.0,2020-04-27-992dbc4e

--- Casks/tuple.rb
+++ Casks/tuple.rb
@@ -1,6 +1,6 @@
 cask 'tuple' do
-  version '0.70.1,2020-04-19-22d584e5'
-  sha256 'ab81ab1b48a4b2bb9b485f8d1c228683a2436640ad2e285d951d5ff0c09f2347'
+  version '0.71.0,2020-04-27-992dbc4e'
+  sha256 'eaee08d73d43bdd99d31b90b62118109326e6eaff8bf9029dff1641d75c951f6'
 
   # s3.us-east-2.amazonaws.com/tuple-releases/ was verified as official when first introduced to the cask
   url "https://s3.us-east-2.amazonaws.com/tuple-releases/production/sparkle/tuple-#{version.before_comma}-#{version.after_comma}.zip"
