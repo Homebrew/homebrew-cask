4c9edadf49d3123559cf3052234af10d05b4cbd9
owiecc <szymon.beczkowski@me.com>
2020-04-29 20:15:32 +0200

    Update Casks/plecs-standalone.rb
    Co-Authored-By: Dario Vladović <d.vladimyr@gmail.com>

--- Casks/plecs-standalone.rb
+++ Casks/plecs-standalone.rb
@@ -3,6 +3,7 @@ cask 'plecs-standalone' do
   sha256 'a517cebe68283865b34107351958ae817da3e25baa0aebb704d587258ab32842'
 
   url "https://www.plexim.com/sites/default/files/packages/plecs-standalone-#{version.dots_to_hyphens}_maci64.dmg"
+  appcast "https://www.plexim.com/download/latest_version?myversion=#{version}"
   name 'PLECS'
   homepage 'https://www.plexim.com/'
 
