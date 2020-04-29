393027c2063dab2c6c301ed1137ff277b219f4f7
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-29 13:04:43 -0700

    Update funter from 4.6,93 to 4.6.1,94 (#81596)

--- Casks/funter.rb
+++ Casks/funter.rb
@@ -4,8 +4,8 @@ cask 'funter' do
     sha256 'b059000717f7f8af67ee027d81aea6c2d9f343e0eb72e439a9f286ab265b7b93'
     url 'https://nektony.com/download/funter/funter_10.12.dmg'
   else
-    version '4.6,93'
-    sha256 '77e2623933607ce21067434e90e3ca4cc794161ce6b3ee2ea141380cca52527b'
+    version '4.6.1,94'
+    sha256 'a9637ed819c73a9eb6aac7465597426a33268453f1bef5b8ba94463fda38f0eb'
     url "https://nektony.com/promo/funter/update/update_#{version.before_comma}(#{version.after_comma}).zip"
     appcast 'https://nektony.com/promo/funter/update/update.xml'
   end
