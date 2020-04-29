7494a37711efef78aa3cd1a0edefe901fa57fd66
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-29 13:05:13 -0700

    Update merlin-project from 6.3.0 to 6.3.1 (#81594)

--- Casks/merlin-project.rb
+++ Casks/merlin-project.rb
@@ -1,6 +1,6 @@
 cask 'merlin-project' do
-  version '6.3.0'
-  sha256 '494ce72e4510836d3bdcb4655dae38cc17887e752c6b77f5035e5fca19b0be48'
+  version '6.3.1'
+  sha256 'e2e07eac68e21c85efe47c85b7da43b63fe75e50a84c295c78cd3279486884f1'
 
   url 'https://www.projectwizards.net/downloads/MerlinProject.zip'
   appcast 'https://www.projectwizards.net/en/support/release-notes/merlin-project-pwstore/xml'
