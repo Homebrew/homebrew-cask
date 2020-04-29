6f952a735ace203e3a47d2f66e5b0a3456e224d0
Chris Russell <chris.russell@havas.com>
2020-04-29 16:04:25 +0100

    Included version Included sha256 Fix url comment Fix url address to include dynamic version Add appcast url Fixed pkg name to include dynamic version

--- Casks/elektron-overbridge.rb
+++ Casks/elektron-overbridge.rb
@@ -1,13 +1,14 @@
 cask 'elektron-overbridge' do
-  version :latest
-  sha256 :no_check
+  version '2.0.37.3'
+  sha '3f54c2300af5d86b021e4400d78cad9ac5aea1c4b6ba96814a123521b87c3feb'
 
-  # https://www.elektron.se/overbridge/ was verified as official when first introduced to the cask
-  url 'https://s3-eu-west-1.amazonaws.com/se-elektron-devops/release/8d1ef129-6161-52ac-b65a-ea18489fd654/Elektron_Overbridge_2.0.37.3.dmg'
+  # https://s3-eu-west-1.amazonaws.com/se-elektron-devops/ was verified as official when first introduced to the cask
+  url "https://s3-eu-west-1.amazonaws.com/se-elektron-devops/release/8d1ef129-6161-52ac-b65a-ea18489fd654/Elektron_Overbridge_#{version}.dmg"
+  appcast 'https://www.elektron.se/support/?connection=overbridge#resources' 
   name 'Overbridge'
   homepage 'https://www.elektron.se/overbridge/'
 
-  pkg 'Elektron Overbridge Installer 2.0.37.3.pkg'
+  pkg "Elektron Overbridge Installer #{version}.pkg"
 
   uninstall script: {
                       executable: '/Applications/Elektron/Overbridge_Uninstaller.app/Contents/MacOS/Overbridge_Uninstaller',
