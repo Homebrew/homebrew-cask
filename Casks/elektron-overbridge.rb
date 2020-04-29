a8622a9e84aed3be1563fa7931d77392e9c407b5
Chris Russell <chris.russell@havas.com>
2020-04-29 16:11:42 +0100

    Fixed name for sha256

--- Casks/elektron-overbridge.rb
+++ Casks/elektron-overbridge.rb
@@ -1,6 +1,6 @@
 cask 'elektron-overbridge' do
   version '2.0.37.3'
-  sha '3f54c2300af5d86b021e4400d78cad9ac5aea1c4b6ba96814a123521b87c3feb'
+  sha256 '3f54c2300af5d86b021e4400d78cad9ac5aea1c4b6ba96814a123521b87c3feb'
 
   # https://s3-eu-west-1.amazonaws.com/se-elektron-devops/ was verified as official when first introduced to the cask
   url "https://s3-eu-west-1.amazonaws.com/se-elektron-devops/release/8d1ef129-6161-52ac-b65a-ea18489fd654/Elektron_Overbridge_#{version}.dmg"
