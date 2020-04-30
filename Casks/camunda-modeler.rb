99f5ad3280329ce0784895a4357276564a1a1b2c
Yannik Ehlert <kontakt@yanniks.de>
2020-04-30 18:50:08 +0200

    Update camunda-modeler from 3.7.2 to 3.7.3

--- Casks/camunda-modeler.rb
+++ Casks/camunda-modeler.rb
@@ -1,6 +1,6 @@
 cask 'camunda-modeler' do
-  version '3.7.2'
-  sha256 '450a48d916f08def2409aaa708ce677a86085d3ab7b89350f16e1a68342ddb1d'
+  version '3.7.3'
+  sha256 'c0f6648e71d052a35aa95dca8acdc77efa75297c2f02739a2492c62f3e390c7c'
 
   url "https://camunda.org/release/camunda-modeler/#{version}/camunda-modeler-#{version}-mac.zip"
   appcast 'https://camunda.com/download/modeler/'
