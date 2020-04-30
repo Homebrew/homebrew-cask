93737aab7ef71b009ea6b2b94e041238ed8ae48c
Jared Ledvina <jared@techsmix.net>
2020-04-29 23:04:03 -0400

    Update Plexamp to 3.0.2 (#81642)
    Release notes https://forums.plex.tv/t/plexamp-release-notes/221280/12
    
    Signed-off-by: Jared Ledvina <jared@techsmix.net>

--- Casks/plexamp.rb
+++ Casks/plexamp.rb
@@ -1,6 +1,6 @@
 cask 'plexamp' do
-  version '3.0.1'
-  sha256 '56ac23d42d7688671399e18556db19f2cba2bed05912a18a7c8b04cea8af918f'
+  version '3.0.2'
+  sha256 'd08757e341a215481e5395120a913ddf127f3d6945f68e739509acb0361b5d5c'
 
   # plexamp.plex.tv/ was verified as official when first introduced to the cask
   url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}.dmg"
