2f7b8ed639befc2eb9946e180e44d5ba8179d03f
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-30 12:50:07 -0700

    Update fontgoggles from 1.1.13 to 1.1.14 (#81685)

--- Casks/fontgoggles.rb
+++ Casks/fontgoggles.rb
@@ -1,6 +1,6 @@
 cask 'fontgoggles' do
-  version '1.1.13'
-  sha256 'b888dd0ee4330ef2b58fa6919202fab66aa26608cdcc95595c029ee6430e224d'
+  version '1.1.14'
+  sha256 '609287874d2a288b69a146e670fea2521b6d23b18940f777fc835ad981c10296'
 
   # github.com/justvanrossum/fontgoggles/ was verified as official when first introduced to the cask
   url "https://github.com/justvanrossum/fontgoggles/releases/download/v#{version}/FontGoggles.dmg"
