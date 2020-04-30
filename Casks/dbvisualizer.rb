be1047d6777996c640aa064abf4b5bdea0baac2e
Randall <ran.dall@icloud.com>
2020-04-30 13:37:07 -0700

    Update dbvisualizer from 11.0 to 11.0.1

--- Casks/dbvisualizer.rb
+++ Casks/dbvisualizer.rb
@@ -1,6 +1,6 @@
 cask 'dbvisualizer' do
-  version '11.0'
-  sha256 '79394fd6358ff8f2f88c436dea781cb713cc9abe34e263e84cdb33c07eb2a545'
+  version '11.0.1'
+  sha256 '84d53806ba9b22c3163f2c09d8d60127893fddc0e9a71377b675994f7bacf4c6'
 
   url "https://www.dbvis.com/product_download/dbvis-#{version}/media/dbvis_macos_#{version.dots_to_underscores}_jre.dmg"
   appcast "https://www.dbvis.com/download/#{version.major}.0"
