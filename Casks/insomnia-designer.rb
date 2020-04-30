8de1164b34614ee7c1086df6763ad68d8c5227b3
Alex Ordonez <me@ordonezalex.com>
2020-04-29 21:35:09 -0700

    Add Insomnia Designer v2020.1.3

---
+++ Casks/insomnia-designer.rb
@@ -0,0 +1,12 @@
+cask 'insomnia-designer' do
+  version '2020.1.3'
+  sha256 '35de4caeac06a7e62b9898db1f75e20212c1a52df9af578ec78454e817ba566d'
+
+  # github.com/Kong/insomnia/ was verified as official when first introduced to the cask
+  url "https://github.com/Kong/insomnia/releases/download/designer%40#{version}/Insomnia.Designer-#{version}.dmg"
+  appcast 'https://insomnia.rest/changelog/index.xml'
+  name 'Insomnia Designer'
+  homepage 'https://insomnia.rest/'
+
+  app 'Insomnia Designer.app'
+end
