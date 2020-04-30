7a9483e24952129f335352a2e1c3501eb1497c8d
Randall <ran.dall@icloud.com>
2020-04-30 13:05:28 -0700

    Update ledger-live from 2.2.4 to 2.3.2

--- Casks/ledger-live.rb
+++ Casks/ledger-live.rb
@@ -1,6 +1,6 @@
 cask 'ledger-live' do
-  version '2.2.4'
-  sha256 '18178170144c0a25edf06e7901911a7009d51db71ff1b1ff5fc9929dda793ff2'
+  version '2.3.2'
+  sha256 '889b83c5dd9b16819fef31598bf99fd0f9809fc716ca492bfd487a4ab533f079'
 
   # github.com/LedgerHQ/ledger-live-desktop/ was verified as official when first introduced to the cask
   url "https://github.com/LedgerHQ/ledger-live-desktop/releases/download/v#{version}/ledger-live-desktop-#{version}-mac.dmg"
