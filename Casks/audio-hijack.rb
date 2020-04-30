6d5de81c4cbe4db2a40b56110bf14fa17ec9c737
chrisfinazzo <chris.finazzo@gmail.com>
2020-04-30 14:48:26 -0400

    Update audio-hijack from 3.7.1 to 3.7.2

--- Casks/audio-hijack.rb
+++ Casks/audio-hijack.rb
@@ -1,6 +1,6 @@
 cask 'audio-hijack' do
-  version '3.7.1'
-  sha256 'e6387b0c6ecc13a8f3bde63a75e712fa3a422993fc3629bbce26c15f673f48e4'
+  version '3.7.2'
+  sha256 '1aeef85f4c60f419be2255733977da1b550e1a134d21ffa358aca91687830e94'
 
   url 'https://rogueamoeba.com/audiohijack/download/AudioHijack.zip'
   appcast 'https://www.rogueamoeba.com/audiohijack/releasenotes.php'
