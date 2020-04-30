a9133ed2224eef2314ab189709455243a582fa32
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-30 13:51:01 -0700

    Update anydo from 4.2.82 to 4.2.83 (#81714)

--- Casks/anydo.rb
+++ Casks/anydo.rb
@@ -1,6 +1,6 @@
 cask 'anydo' do
-  version '4.2.82'
-  sha256 'e68890b189dc8d310aca7bc5f89c00abf65b53ef7a83be1e8571d71dc7ba2e0c'
+  version '4.2.83'
+  sha256 '36cd2d0bdcd4c3a714b77cf2e45e81283e7b9c4dc365e2ed38c8a61e8e2b58eb'
 
   url 'https://electron-app.any.do/Any.do.dmg'
   appcast 'https://electron-app.any.do/latest-mac.yml'
