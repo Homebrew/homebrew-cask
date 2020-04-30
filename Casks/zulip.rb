c98106eb06bd424a3031e6794bd5af4a7fff18f4
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-30 12:48:56 -0700

    Update zulip from 5.0.0 to 5.1.0 (#81688)

--- Casks/zulip.rb
+++ Casks/zulip.rb
@@ -1,6 +1,6 @@
 cask 'zulip' do
-  version '5.0.0'
-  sha256 'a8189b22e9ba51db256e427e968ddd5324f6b02fa62bf7201957154733c6acb7'
+  version '5.1.0'
+  sha256 'c0c6cd9bc0aa0c5f7854aad2bb31ac7e3beeca591dd06b47321c262fb4a60f22'
 
   # github.com/zulip/zulip-desktop/ was verified as official when first introduced to the cask
   url "https://github.com/zulip/zulip-desktop/releases/download/v#{version}/Zulip-#{version}.dmg"
