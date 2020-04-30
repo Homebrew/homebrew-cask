8744f9ecfa6b5f71eafbd1da5ef6461a30ee59ac
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-30 12:49:24 -0700

    Update lyricsx from 1.5.2,2271 to 1.5.3,2273 (#81687)

--- Casks/lyricsx.rb
+++ Casks/lyricsx.rb
@@ -1,6 +1,6 @@
 cask 'lyricsx' do
-  version '1.5.2,2271'
-  sha256 'b5c4c19c25492e41505219f4788360b148b7006115678fc7e710ee00e695ff43'
+  version '1.5.3,2273'
+  sha256 '83504be1c83545614c9230106d233d21b5c283966e60279cf770309fda09b84b'
 
   url "https://github.com/ddddxxx/LyricsX/releases/download/v#{version.before_comma}/LyricsX_#{version.before_comma}+#{version.after_comma}.zip"
   appcast 'https://github.com/ddddxxx/LyricsX/releases.atom'
