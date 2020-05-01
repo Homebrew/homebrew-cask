487c5fd3db8eec456cb61d0e575c24248fe5dd1e
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-01 11:33:08 -0700

    Update atext from 2.35.5 to 2.36 (#81760)

--- Casks/atext.rb
+++ Casks/atext.rb
@@ -1,6 +1,6 @@
 cask 'atext' do
-  version '2.35.5'
-  sha256 '10daa7c10a4b095291db57b4d5477fef0c5ea1976d1feaa74cdbfc3783a88ad9'
+  version '2.36'
+  sha256 '6e03566fb0aab1bbbc1838a761f208f0e6f16f226fe4c7b3ad8af4d54f6f8b93'
 
   url 'https://www.trankynam.com/atext/downloads/aText.dmg'
   appcast 'https://www.trankynam.com/atext/aText-Appcast.xml'
