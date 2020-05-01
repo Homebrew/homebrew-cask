a1b80793ccecd3fa9716ac03125210679930a8b9
lulor <61948614+lulor@users.noreply.github.com>
2020-05-01 17:48:30 +0200

    Update intel-power-gadget (#81454)

--- Casks/intel-power-gadget.rb
+++ Casks/intel-power-gadget.rb
@@ -14,7 +14,7 @@ cask 'intel-power-gadget' do
   pkg 'Install Intel Power Gadget.pkg'
 
   uninstall pkgutil: 'com.intel.pkg.PowerGadget.*',
-            kext:    'EnergyDriver'
+            kext:    'com.intel.driver.EnergyDriver'
 
   zap trash: [
                '~/Library/Caches/com.intel.PowerGadget',
