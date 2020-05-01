03b32dd0c7456f271b56c7b6d1a187ae3be3684f
miccal <miccal.matthews@gmail.com>
2020-05-01 17:04:00 +0800

    Update ipepresenter from 7.2.15 to 7.2.16 (#81726)

--- Casks/ipepresenter.rb
+++ Casks/ipepresenter.rb
@@ -1,6 +1,6 @@
 cask 'ipepresenter' do
-  version '7.2.15'
-  sha256 '897a4bdce476aa563b5560ffea307cd5409dd948d52ba25d08ac12e53533ac8f'
+  version '7.2.16'
+  sha256 'e4510a7ac6408518db2453e3ddfb5d6e6a55af88010e2597ed64cf4bbefadee7'
 
   # bintray.com/otfried/ was verified as official when first introduced to the cask
   url "https://dl.bintray.com/otfried/generic/ipe/#{version.major_minor}/ipepresenter-#{version}-mac.dmg"
