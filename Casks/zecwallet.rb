afc08d35c28f398e9cc9b75e2c045818ab622090
Sven Freiberg <sven.freiberg@biq.solutions>
2020-05-01 21:59:21 +0400

    Updated zecwallet: fixed syle errors, added url audit comment

--- Casks/zecwallet.rb
+++ Casks/zecwallet.rb
@@ -2,6 +2,7 @@ cask 'zecwallet' do
   version '1.1.8'
   sha256 '052b271e992cc9513f39875a5d373f41a96d0a212c2f7ca8f223d22e7890a94a'
 
+  # github.com/adityapk00/zecwallet-lite/ was verified as official when first introduced to the cask
   url "https://github.com/adityapk00/zecwallet-lite/releases/download/v#{version}/Zecwallet.Lite-#{version}.dmg"
   appcast 'https://github.com/adityapk00/zecwallet-lite/releases.atom'
   name 'Zecwallet Lite'
@@ -10,16 +11,16 @@ cask 'zecwallet' do
   app 'Zecwallet Lite.app'
 
   zap trash: [
-    "~/Library/Preferences/co.zecwallet.lite",
-    "~/Library/Preferences/co~/Libraryzecwallet~/Librarylite~/Libraryplist",
-    "~/Library/Preferences/co.zecwallet.lite.plist",
-    "~/Library/Application Support/zecwallet",
-    "~/Library/Application Support/Zecwallet Lite",
-    "~/Library/Application Support/Zcash/zecwallet-light-wallet.dat",
-    "~/Library/Application Support/Zcash/zecwallet-light-wallet.debug.dat",
-    "~/Library/Application Support/Zcash",
-    "~/Library/Logs/Zecwallet Lite",
-    "~/Library/Saved Application State/co~/Libraryzecwallet~/Librarylite~/LibrarysavedState",
-    "~/Library/Saved Application State/co.zecwallet.lite.savedState"
-  ]
+               '~/Library/Preferences/co.zecwallet.lite',
+               '~/Library/Preferences/co~/Libraryzecwallet~/Librarylite~/Libraryplist',
+               '~/Library/Preferences/co.zecwallet.lite.plist',
+               '~/Library/Application Support/zecwallet',
+               '~/Library/Application Support/Zecwallet Lite',
+               '~/Library/Application Support/Zcash/zecwallet-light-wallet.dat',
+               '~/Library/Application Support/Zcash/zecwallet-light-wallet.debug.dat',
+               '~/Library/Application Support/Zcash',
+               '~/Library/Logs/Zecwallet Lite',
+               '~/Library/Saved Application State/co~/Libraryzecwallet~/Librarylite~/LibrarysavedState',
+               '~/Library/Saved Application State/co.zecwallet.lite.savedState',
+             ]
 end
