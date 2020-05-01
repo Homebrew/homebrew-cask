3e55ddaea4f1652508038c5f3a2ce82c5f0029e3
Randall <ran.dall@icloud.com>
2020-05-01 11:32:11 -0700

    Update shift from 4.0.25 to 4.0.28

--- Casks/shift.rb
+++ Casks/shift.rb
@@ -1,5 +1,5 @@
 cask 'shift' do
-  version '4.0.25'
+  version '4.0.28'
   sha256 '95142435a998148050282cf5a1f36937ea4e6386f1c137ae79b894afa3e9c332'
 
   url "https://update.tryshift.com/download/version/#{version}/osx_64"
