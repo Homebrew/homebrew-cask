713c9c746022cbeaf6306e2bee0e8056e987ba7f
Viktor Szakats <commit@vsz.me>
2020-05-01 11:50:26 +0000

    stack-stack: fix/improve urls

--- Casks/stack-stack.rb
+++ Casks/stack-stack.rb
@@ -2,9 +2,9 @@ cask 'stack-stack' do
   version '2.13.0'
   sha256 'e324a48e7c73791977c832583a5fc6229b7b9907597cc27d5a490a1215660e63'
 
-  # s3.eu-central-1.amazonaws.com/stack-v1 was verified as official when first introduced to the cask
-  url "https://s3.eu-central-1.amazonaws.com/stack-v1/builds/prod/Stack-#{version}.dmg"
-  appcast 'https://s3.eu-central-1.amazonaws.com/stack-v1'
+  # stack-v1.s3.amazonaws.com/ was verified as official when first introduced to the cask
+  url "https://stack-v1.s3.amazonaws.com/builds/prod/Stack-#{version}.dmg"
+  appcast 'https://stack-v1.s3.amazonaws.com/'
   name 'Stack'
   homepage 'https://stackers.app/'
 
