060d4ff14ecf65e9d1a406f7e5875fc12c83e1b6
suschizu <brigitte@corecode.io>
2020-05-02 16:37:11 +0200

    Update back-in-time from 5 to 5.1.2 (#81728)

--- Casks/back-in-time.rb
+++ Casks/back-in-time.rb
@@ -1,10 +1,11 @@
 cask 'back-in-time' do
-  version '5'
-  sha256 :no_check # required as upstream package is updated in-place
+  version '5.1.2'
+  sha256 '7a7bc1c0cead6f4b06320e571363364ff279e874f3880701310296242c3bce9c'
 
-  url "https://www.tri-edre.com/pub/files/backintime#{version}.dmg"
+  url "https://www.tri-edre.com/pub/files/backintime#{version.major}.dmg"
+  appcast 'https://www.tri-edre.com/news/backintimeen.html'
   name 'Back-In-Time'
   homepage 'https://www.tri-edre.com/english/backintime.html'
 
-  app "Back-In-Time #{version}.app"
+  app "Back-In-Time #{version.major}.app"
 end
