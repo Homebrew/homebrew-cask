e212135d3fc48d017f9c9fef0dea1e44f534048d
Work <brigitte@corecode.io>
2020-05-02 17:29:06 +0200

    Update odrive from 6624 to 6642

--- Casks/odrive.rb
+++ Casks/odrive.rb
@@ -1,6 +1,6 @@
 cask 'odrive' do
-  version '6624'
-  sha256 '9baf2588d72e0979939874b8235aa8fa26cfd180f32970e453e17cc17122a1e8'
+  version '6642'
+  sha256 '5544c9b6f8a1b574d1d1ddd3c9d7ea102fac6a8c451e3d38b51e5067d3e659cf'
 
   # downloads can be found at https://www.odrive.com/downloaddesktop
   # d3huse1s6vwzq6.cloudfront.net/ was verified as official when first introduced to the cask
