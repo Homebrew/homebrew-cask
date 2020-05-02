95c0c95d1cae035c98e0724239cb58736d3c86aa
suschizu <brigitte@corecode.io>
2020-05-02 16:36:48 +0200

    Update iridium from 2020.01-0 to 2020.04.81.0 (#81727)
    * Update iridium from 2020.01-0 to 2020.04.81.0
    
    * Update iridium.rb

--- Casks/iridium.rb
+++ Casks/iridium.rb
@@ -1,9 +1,10 @@
 cask 'iridium' do
-  version '2020.01-0'
-  sha256 '304e5f434adc70ff6afd30ce133415907db066d5427c18801fe7168ac8c4b4e4'
+  version '2020.04.81.0'
+  sha256 'd7295ffde57bb18057fd61f8458c01a16a062658448a96c87db5ec75ed1977eb'
 
-  url "https://downloads.iridiumbrowser.de/macos/#{version}/iridium-browser_#{version}_macos.dmg"
-  appcast 'https://iridiumbrowser.de/news/'
+  url "https://downloads.iridiumbrowser.de/macos/#{version.major_minor}-0/iridiumbrowser-#{version}.dmg"
+  appcast 'https://iridiumbrowser.de/news/',
+          configuration: version.major_minor
   name 'Iridium Browser'
   homepage 'https://iridiumbrowser.de/'
 
