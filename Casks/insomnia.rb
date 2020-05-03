e100509458ee7c07be76cbbf1deeaa60720e9a4b
Vítor Galvão <info@vitorgalvao.com>
2020-05-03 16:36:51 +0100

    update insomnia repo (#81844)

--- Casks/insomnia.rb
+++ Casks/insomnia.rb
@@ -2,8 +2,8 @@ cask 'insomnia' do
   version '7.1.1'
   sha256 '7718dd2c1a6ec0edafe36116d9123cda80b769abadc2d4fc5c5389922c7dfa6f'
 
-  # github.com/getinsomnia/insomnia/ was verified as official when first introduced to the cask
-  url "https://github.com/getinsomnia/insomnia/releases/download/v#{version}/Insomnia-#{version}.dmg"
+  # github.com/Kong/insomnia/ was verified as official when first introduced to the cask
+  url "https://github.com/Kong/insomnia/releases/download/v#{version}/Insomnia-#{version}.dmg"
   appcast 'https://api.insomnia.rest/changelog.json?app=com.insomnia.app'
   name 'Insomnia'
   homepage 'https://insomnia.rest/'
