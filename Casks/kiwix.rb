55c17cb6b3bf1a7577a6e1fe475daea7912dc305
suschizu <brigitte@corecode.io>
2020-05-03 13:23:17 +0200

    Update kiwix.rb

--- Casks/kiwix.rb
+++ Casks/kiwix.rb
@@ -3,7 +3,7 @@ cask 'kiwix' do
   sha256 '61056a5f48dce8ec63f1082e0b41c4fcbd0e8cf3f36dc06a1f6a3ef29c215669'
 
   url "https://download.kiwix.org/release/kiwix-desktop-macos/kiwix-desktop-macos_#{version}.dmg"
-  appcast 'https://www.kiwix.org/en/downloads/kiwix-reader/'
+  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.kiwix.org/release/kiwix-desktop-macos/kiwix-desktop-macos.dmg'
   name 'Kiwix'
   homepage 'https://www.kiwix.org/'
 
