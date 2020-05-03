6d534e7cf822410180d1d8083bd8a513e307e038
Meet Godhani <meetgodhani23@gmail.com>
2020-05-03 15:21:19 -0400

    Add Raven Reader.app v0.7.5 (#81852)
    * Add Raven Reader.app v0.7.5
    
    * Added appcast url for automatic updates

---
+++ Casks/raven-reader.rb
@@ -0,0 +1,13 @@
+cask 'raven-reader' do
+  version '0.7.5'
+  sha256 '8dc0945313cf14ec99a3e4ca8b294caece485efa3c72d41a34cc87cf9f5751e0'
+
+  url "https://download.ravenreader.app/ravenreader/Raven%20Reader-#{version}-mac.zip"
+  appcast 'https://download.ravenreader.app/ravenreader/latest-mac.yml'
+  name 'Raven Reader'
+  homepage 'https://ravenreader.app/'
+
+  auto_updates true
+
+  app 'Raven Reader.app'
+end
