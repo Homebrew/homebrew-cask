a403bfa2072a8302b3307a60aa87f09d3497c5f0
David Yantis <404NetworkError@users.noreply.github.com>
2020-05-03 12:04:17 -0400

    Update losslesscut from 3.21.0 to 3.22.0 (#81849)

--- Casks/losslesscut.rb
+++ Casks/losslesscut.rb
@@ -1,6 +1,6 @@
 cask 'losslesscut' do
-  version '3.21.0'
-  sha256 '8b1791511611dbe7411a0f2fc56e88797cb267748b49ec9bd390cb1460286c13'
+  version '3.22.0'
+  sha256 'c88135a5e34c76487a933a61c9909733d3324bece1f56c94aa009618d8e67b11'
 
   url "https://github.com/mifi/lossless-cut/releases/download/v#{version}/LosslessCut-mac.dmg"
   appcast 'https://github.com/mifi/lossless-cut/releases.atom'
