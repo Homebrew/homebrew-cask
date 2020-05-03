903ab4a1946e65cc71dae8ce5e5bd15a606920fb
Brigitte Zmoelnig <brigitte@corecode.io>
2020-05-03 13:12:09 +0200

    Update kiwix from 2.0 to 2.1

--- Casks/kiwix.rb
+++ Casks/kiwix.rb
@@ -1,6 +1,6 @@
 cask 'kiwix' do
-  version '2.0'
-  sha256 '544c1f813ed1043844d75c75e10d41aec9812e5eea6bcb0c4f3a66d34e39b36a'
+  version '2.1'
+  sha256 '61056a5f48dce8ec63f1082e0b41c4fcbd0e8cf3f36dc06a1f6a3ef29c215669'
 
   url "https://download.kiwix.org/release/kiwix-desktop-macos/kiwix-desktop-macos_#{version}.dmg"
   appcast 'https://www.kiwix.org/en/downloads/kiwix-reader/'
