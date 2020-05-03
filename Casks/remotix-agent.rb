d8ac8619f8c334515a470bdbc4acd8f0660cba58
Brigitte Zmoelnig <brigitte@corecode.io>
2020-05-03 13:03:27 +0200

    Update remotix-agent

--- Casks/remotix-agent.rb
+++ Casks/remotix-agent.rb
@@ -3,7 +3,7 @@ cask 'remotix-agent' do
   sha256 '6bc975c7dc4e49edaba5c918009f0bc80518d9fdebd91e7da11cb2fe25239349'
 
   url 'https://downloads.remotixcloud.com/agent-mac/RemotixAgent.pkg'
-  appcast 'https://remotixcloud.com/downloads/'
+  appcast 'https://remotix.com/downloads-mac/'
   name 'Remotix Agent'
   homepage 'https://remotixcloud.com/'
 
