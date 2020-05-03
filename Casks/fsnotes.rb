1a90d4852d8ff4101020c00896402db2bd98ed50
Balu <tw@b-a-l-u.de>
2020-05-03 15:18:17 +0200

    Update fsnotes from 4.2.0 to 4.2.1 (#81845)

--- Casks/fsnotes.rb
+++ Casks/fsnotes.rb
@@ -1,6 +1,6 @@
 cask 'fsnotes' do
-  version '4.2.0'
-  sha256 '94cf4a5f92bc0c21ca9929ee86e04b0525c8eff59c0906592d73bc8887551439'
+  version '4.2.1'
+  sha256 'a1d50a8a7bc6101dfbbf500f045d5b71c2a5d19789df1d6712ebb5b023251736'
 
   # github.com/glushchenko/fsnotes/ was verified as official when first introduced to the cask
   url "https://github.com/glushchenko/fsnotes/releases/download/#{version}/FSNotes_#{version}.zip"
