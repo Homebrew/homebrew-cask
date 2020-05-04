6695c9a79f44ed30c95c3a6529531d452780af59
Hanlin He <hanlhe@tetrationanalytics.com>
2020-05-04 11:58:10 -0700

    Update pdf-expert from 2.5.5,678 to 2.5.5,679

--- Casks/pdf-expert.rb
+++ Casks/pdf-expert.rb
@@ -1,6 +1,6 @@
 cask 'pdf-expert' do
-  version '2.5.5,678'
-  sha256 'b7e7c3656a8ff1fe3d566e7954c6fcf9294a5781df22e914d41b5a4b6b214c26'
+  version '2.5.5,679'
+  sha256 'a1082a92f469cea4fe1924f3ead10b1d701de63cb2d1349ce4a7ae8fee5cfcc9'
 
   # d1ke680phyeohy.cloudfront.net/ was verified as official when first introduced to the cask
   url "https://d1ke680phyeohy.cloudfront.net/versions/#{version.after_comma}/PDFExpert.dmg"
