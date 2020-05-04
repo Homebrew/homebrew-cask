6cc857ce90792c3d7c53df2e2b584e814f6b5500
Quang Lam <quang.lam2807@gmail.com>
2020-05-04 18:48:52 +0700

    Update webcatalog from 20.12.0 to 20.12.1 (#81877)
    Co-authored-by: runner <runner@Mac-1628.local>

--- Casks/webcatalog.rb
+++ Casks/webcatalog.rb
@@ -1,6 +1,6 @@
 cask 'webcatalog' do
-  version '20.12.0'
-  sha256 '3e03e90a83b9510cb1252c770234ab39ee2405f846f06aa68c9787c13ff603bf'
+  version '20.12.1'
+  sha256 '131759b0d49c7f521168d6f701d030f689eff1ef1447480d0cb2bde2cd2ac40d'
 
   # github.com/quanglam2807/webcatalog/ was verified as official when first introduced to the cask
   url "https://github.com/quanglam2807/webcatalog/releases/download/v#{version}/WebCatalog-#{version}-mac.zip"
