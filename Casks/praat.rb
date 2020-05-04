f6388e124814180a7957999006d647c9b25e9d36
Work <brigitte@corecode.io>
2020-05-04 14:43:23 +0200

    Update praat from 6.1.13 to 6.1.14

--- Casks/praat.rb
+++ Casks/praat.rb
@@ -1,9 +1,9 @@
 cask 'praat' do
-  version '6.1.13'
-  sha256 'b1de1e489d2a923b3ef0aaa1103486a8a6dbc574645af3bfa8964618d3269412'
+  version '6.1.14'
+  sha256 '08fbd3f4878d2312f92542ce7d604d398b2506c851c0bb072bd884b6d6846f8a'
 
   # github.com/praat/praat/ was verified as official when first introduced to the cask
-  url "https://github.com/praat/praat/releases/download/v#{version}a/praat#{version.no_dots}_mac64.dmg"
+  url "https://github.com/praat/praat/releases/download/v#{version}/praat#{version.no_dots}_mac64.dmg"
   appcast 'https://github.com/praat/praat/releases.atom'
   name 'Praat'
   homepage 'http://www.fon.hum.uva.nl/praat/'
