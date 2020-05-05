70490ea62a88ab41fb0cf0922aa181e72d80527b
Vítor Galvão <info@vitorgalvao.com>
2020-05-05 18:27:46 +0100

    inkscape.rb: keep only 1.0.0 (#81922)

--- Casks/inkscape.rb
+++ Casks/inkscape.rb
@@ -1,44 +1,30 @@
 cask 'inkscape' do
-  if MacOS.version <= :mojave
-    version '0.92.2-1'
-    sha256 'faece7a9a5fa9db7724b0c761f7f2014676d00ef8b90a0ef506fa39d09209fea'
+  version '1.0.0'
+  sha256 '9b42468815b4bcbc8ccb76a239aea48a2965dbd2f3ae7c3b560c7f2a7e48a955'
 
-    url "https://media.inkscape.org/dl/resources/file/Inkscape-#{version}-x11-10.7-x86_64.dmg"
-
-    depends_on x11: true
-
-    binary "#{appdir}/Inkscape.app/Contents/Resources/bin/inkscape"
-
-    zap trash: '~/.inkscape-etc'
-  else
-    version '1.0.0'
-    sha256 '9b42468815b4bcbc8ccb76a239aea48a2965dbd2f3ae7c3b560c7f2a7e48a955'
-
-    url "https://media.inkscape.org/dl/resources/file/Inkscape-#{version}.dmg"
-
-    # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
-    shimscript = "#{staged_path}/inkscape.wrapper.sh"
-
-    binary shimscript, target: 'inkscape'
-
-    preflight do
-      IO.write shimscript, <<~EOS
-        #!/bin/sh
-        exec '#{staged_path}/Inkscape.app/Contents/MacOS/Inkscape' "$@"
-      EOS
-    end
-
-    zap trash: [
-                 '~/.config/inkscape',
-                 '~/Library/Application Support/Inkscape',
-                 '~/Library/Application Support/org.inkscape.Inkscape',
-                 '~/Library/Preferences/org.inkscape.Inkscape.plist',
-                 '~/Library/Saved Application State/org.inkscape.Inkscape.savedState',
-               ]
-  end
-
+  url "https://media.inkscape.org/dl/resources/file/Inkscape-#{version}.dmg"
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://inkscape.org/release',
+          configuration: version.major_minor
   name 'Inkscape'
   homepage 'https://inkscape.org/'
 
   app 'Inkscape.app'
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/inkscape.wrapper.sh"
+  binary shimscript, target: 'inkscape'
+
+  preflight do
+    IO.write shimscript, <<~EOS
+      #!/bin/sh
+      exec '#{staged_path}/Inkscape.app/Contents/MacOS/Inkscape' "$@"
+    EOS
+  end
+
+  zap trash: [
+               '~/.config/inkscape',
+               '~/Library/Application Support/Inkscape',
+               '~/Library/Application Support/org.inkscape.Inkscape',
+               '~/Library/Preferences/org.inkscape.Inkscape.plist',
+               '~/Library/Saved Application State/org.inkscape.Inkscape.savedState',
+             ]
 end
