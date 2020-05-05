432f82a43bbd872d22a3f832e666b5ef49754b2c
Julian Mayer <julian@corecode.io>
2020-05-05 15:14:56 +0200

    Update lynx from 6.6.3.0 to 6.6.4.0

--- Casks/lynx.rb
+++ Casks/lynx.rb
@@ -1,6 +1,6 @@
 cask 'lynx' do
-  version '6.6.3.0'
-  sha256 '5823836f34607bce593ff5050de5d69ecaf869c5910bd6712d8dd402194bc633'
+  version '6.6.4.0'
+  sha256 '1d98d95108dbca33fc0fd8fd178912df8de1fa207679c30f4fc8fa7b8c1640c3'
 
   url 'https://download.saharasupport.com/lynx/production/macx/Lynx-install.dmg'
   appcast 'https://downloads.saharasupport.com/lynx/production/macx/version.txt'
