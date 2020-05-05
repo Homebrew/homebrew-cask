af76353037551ec8ec6791551c4692792d580c5b
Marc A. Paradise <marc@chef.io>
2020-05-05 12:32:51 -0400

    Code review updates
    * add appcask
    * replace `uninstall delete` file list with `script` of the uninstall
      script that we ship with Chef Workstation. It removes the same files,
      and lets us maintain that behavior in one place.
    
    Signed-off-by: Marc A. Paradise <marc@chef.io>

--- Casks/chef-workstation.rb
+++ Casks/chef-workstation.rb
@@ -3,6 +3,7 @@ cask 'chef-workstation' do
   sha256 'e9b9165a80222900a1e421f7ad72d2f8252bc0f46175162e43b8cf271b2d2207'
 
   url "https://packages.chef.io/files/stable/chef-workstation/#{version}/mac_os_x/10.13/chef-workstation-#{version}-1.dmg"
+  appcast 'https://omnitruck.chef.io/stable/chef-workstation/metadata?p=mac_os_x&pv=10.14&m=x86_64&v=latest'
   name 'Chef Workstation'
   homepage 'https://docs.chef.io/workstation/'
 
@@ -13,33 +14,10 @@ cask 'chef-workstation' do
   uninstall quit:      'sh.chef.chef-workstation',
             pkgutil:   'com.getchef.pkg.chef-workstation',
             launchctl: 'io.chef.chef-workstation.app',
-            delete:    [
-                         '/Applications/Chef Workstation App.app',
-                         '/opt/chef-workstation/',
-                         '/usr/local/bin/berks',
-                         '/usr/local/bin/chef',
-                         '/usr/local/bin/chef-cli',
-                         '/usr/local/bin/chef-analyze',
-                         '/usr/local/bin/chef-apply',
-                         '/usr/local/bin/chef-client',
-                         '/usr/local/bin/chef-run',
-                         '/usr/local/bin/chef-shell',
-                         '/usr/local/bin/chef-solo',
-                         '/usr/local/bin/chef-vault',
-                         '/usr/local/bin/chefx',
-                         '/usr/local/bin/cookstyle',
-                         '/usr/local/bin/dco',
-                         '/usr/local/bin/delivery',
-                         '/usr/local/bin/foodcritic',
-                         '/usr/local/bin/inspec',
-                         '/usr/local/bin/kitchen',
-                         '/usr/local/bin/knife',
-                         '/usr/local/bin/ohai',
-                         '/usr/local/bin/push-apply',
-                         '/usr/local/bin/pushy-client',
-                         '/usr/local/bin/pushy-service-manager',
-                         '/usr/local/bin/uninstall_chef_workstation',
-                       ]
+            script:    {
+                         executable: '/opt/chef-workstation/bin/uninstall_chef_workstation',
+                         sudo:       true,
+                       }
 
   zap trash: '~/.chef-workstation/'
 end
