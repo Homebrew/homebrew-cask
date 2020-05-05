6db84ad8e259f629399e7313ce6e951c5d291195
suschizu <brigitte@corecode.io>
2020-05-05 16:51:11 +0200

    Update eudic.rb

--- Casks/eudic.rb
+++ Casks/eudic.rb
@@ -3,7 +3,8 @@ cask 'eudic' do
   sha256 '5cec29af3ff50a6658c81c33466166aa0de18097ec649ec82557517cf2f73d2e'
 
   # static.frdic.com/ was verified as official when first introduced to the cask
-  url "https://static.frdic.com/pkg/eudicmac.dmg?v=#{version.before_comma}"
+  url "https://static.frdic.com/pkg/eudicmac.dmg?v=#{version.before_comma}",
+      user_agent: :fake
   appcast 'https://www.eudic.net/v4/en/app/download'
   name 'Eudic'
   name '欧路词典'
