e9b1b4244bbcff706c7ad887a9fdc551ac9aa109
Julian Mayer <julian@corecode.io>
2020-05-05 14:21:37 +0200

    Update ishowu-instant

--- Casks/ishowu-instant.rb
+++ Casks/ishowu-instant.rb
@@ -1,6 +1,6 @@
 cask 'ishowu-instant' do
   version '1.3.4'
-  sha256 'e02a00862ea0bf6e4855942b8ed285b0c743d0bb6fd387cc8a29157415240d38'
+  sha256 '1db9d09656c0ff07659e6488e648bc04f97fea5939e6f60fbb8770448a54786a'
 
   url "https://www.shinywhitebox.com/downloads/instant/iShowU_Instant_#{version}.dmg"
   appcast 'https://www.shinywhitebox.com/store/appcast.php?p=14'
