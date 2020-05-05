4953b7505b4ccb967b71465ae2d8cf0f5b8551a6
CoreCode <core-code@users.noreply.github.com>
2020-05-05 15:04:57 +0200

    Update navicat-data-modeler-essentials from 3.0.8 to 3.0.9 (#81947)

--- Casks/navicat-data-modeler-essentials.rb
+++ Casks/navicat-data-modeler-essentials.rb
@@ -1,6 +1,6 @@
 cask 'navicat-data-modeler-essentials' do
-  version '3.0.8'
-  sha256 '76d9ba97576a78dbed5df40ea8eb6126d72878ce64e6163260d4330ac9e3af47'
+  version '3.0.9'
+  sha256 'd7d3550c645352af43b3a68f87a8725c4d4880cce93703d47434bcebd7679f3a'
 
   url "http://download3.navicat.com/updater/modeler0#{version.major_minor.no_dots}_ess_mac_en.zip"
   appcast 'https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20Data%20Modeler%20Essentials'
