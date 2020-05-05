06a1de606eb3ecd6a1086289af6aed92fe1654f3
Reno <25192197+singularitti@users.noreply.github.com>
2020-05-05 09:47:21 -0400

    Update a new item
    to cleanly reset every setting
    according to
    https://www.jetbrains.com/help/pycharm/configuring-project-and-ide-settings.html#restore-defaults

--- Casks/pycharm.rb
+++ Casks/pycharm.rb
@@ -17,6 +17,7 @@ cask 'pycharm' do
 
   zap trash: [
                "~/Library/Application Support/PyCharm#{version.major_minor}",
+               "~/Library/Application Support/JetBrains/PyCharm#{version.major_minor}",
                "~/Library/Caches/PyCharm#{version.major_minor}",
                "~/Library/Logs/PyCharm#{version.major_minor}",
                "~/Library/Preferences/PyCharm#{version.major_minor}",
