6990188456544b8823072cda98e85a112463760a
RedHotHub <43639897+RedHotHub@users.noreply.github.com>
2020-05-05 15:15:56 +0300

    Update mysqlworkbench.rb
    Updated appcast for 8.0.16 (to archives)

--- Casks/mysqlworkbench.rb
+++ Casks/mysqlworkbench.rb
@@ -1,4 +1,7 @@
 cask 'mysqlworkbench' do
+ 
+  appcast 'https://downloads.mysql.com/archives/workbench/'
+  
   if MacOS.version <= :sierra
     version '6.3.10'
     sha256 '29857bf84bebb7c4442ce147e44602d00f8c001e3c09b3a6e3af356767e08d2c'
@@ -7,13 +10,13 @@ cask 'mysqlworkbench' do
     version '8.0.16'
     sha256 '3478800290e2797d294e3721fdaea4c41ddc1917f2b59ec94a935e16c18dc5d2'
     url "https://downloads.mysql.com/archives/get/p/#{version.major}/file/mysql-workbench-community-#{version}-macos-x86_64.dmg"
+    appcast 'https://downloads.mysql.com/archives/workbench/'
   elsif MacOS.version >= :mojave
     version '8.0.20'
     sha256 'a5c11b83fbcb1817e982eed7a6d170ffca6c67d3a9496817c395c4a60b571cc0'
     url "https://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-#{version}-macos-x86_64.dmg"
   end
 
-  appcast 'https://dev.mysql.com/downloads/workbench/'
   name 'MySQL Workbench'
   homepage 'https://www.mysql.com/products/workbench/'
 
