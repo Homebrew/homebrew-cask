177d04ec5b9c3a8973b6d4d7804ab16cccdf3bc8
Vítor Galvão <info@vitorgalvao.com>
2020-05-05 03:15:22 +0100

    Update inkscape.rb

--- Casks/inkscape.rb
+++ Casks/inkscape.rb
@@ -3,6 +3,8 @@ cask 'inkscape' do
   sha256 '9b42468815b4bcbc8ccb76a239aea48a2965dbd2f3ae7c3b560c7f2a7e48a955'
 
   url "https://media.inkscape.org/dl/resources/file/Inkscape-#{version}.dmg"
+  appcast 'https://gitlab.com/inkscape/inkscape/-/tags?format=atom',
+          configuration: version.major_minor
   name 'Inkscape'
   homepage 'https://inkscape.org/'
 
