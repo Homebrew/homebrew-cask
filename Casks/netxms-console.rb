a044f26f8c1d48ffffbf9c868a808e9649f1511d
Alex Kirhenshtein <alk@netxms.org>
2020-05-05 12:18:43 +0300

    NetXMS Console.app updated to version 3.3.277 (#81933)

--- Casks/netxms-console.rb
+++ Casks/netxms-console.rb
@@ -1,6 +1,6 @@
 cask 'netxms-console' do
-  version '3.2.493'
-  sha256 '4b719422be4cfaf46d2a5ce243e9282aa8edf6aa030f72a6770e00d2896be7bb'
+  version '3.3.277'
+  sha256 '094ec9a711cdefcc6639db28c05a569fd3d8051f305b1e70b8914979668d1180'
 
   url "https://netxms.org/download/releases/#{version.major_minor}/nxmc-#{version}.dmg"
   appcast "https://netxms.org/download/releases/#{version.major_minor}/"
