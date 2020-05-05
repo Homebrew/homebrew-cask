b445e79117d157359fb75f3c54db922203e22bfa
Jhen-Jie Hong <developer@jhen.me>
2020-05-05 13:48:51 +0800

    Update react-native-debugger (0.11.2) (#81928)

--- Casks/react-native-debugger.rb
+++ Casks/react-native-debugger.rb
@@ -1,6 +1,6 @@
 cask 'react-native-debugger' do
-  version '0.11.1'
-  sha256 'cdfdc5d1821b95b471c3bd5ccdf05a861477e4f36bc859d413956a2658a5737a'
+  version '0.11.2'
+  sha256 '5993924ab73f67eed6f9a8f326f61c7b6c503d24b38fa26f8c521783ee8bcf60'
 
   url "https://github.com/jhen0409/react-native-debugger/releases/download/v#{version}/rn-debugger-macos-x64.zip"
   appcast 'https://github.com/jhen0409/react-native-debugger/releases.atom'
