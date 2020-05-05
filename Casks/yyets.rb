ee71db6e597ab27ad2f1d9a5223a1ff15ad3845a
CoreCode <core-code@users.noreply.github.com>
2020-05-05 11:05:56 +0200

    Update yyets.rb

--- Casks/yyets.rb
+++ Casks/yyets.rb
@@ -2,7 +2,7 @@ cask 'yyets' do
   version '3.2.2'
   sha256 'dd0d1c9ad5f8550c06328f85d7249877e2ffa76ee2c99960c6d760b903c8c708'
 
-  url "http://appdown.rrys.tv/RRShare#{version}.dmg?"
+  url "http://appdown.rrys.tv/RRShare#{version}.dmg"
   appcast 'http://tj.playcvn.com/app/init?platform=5'
   name 'YYeTs'
   name '人人影视'
