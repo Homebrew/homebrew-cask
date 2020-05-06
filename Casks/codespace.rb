3212f95617908b0947ab08ddd074ba5c60900acf
Crysicia <7459685+Crysicia@users.noreply.github.com>
2020-05-06 23:58:09 +0200

    Add appcast & mac os minimal version

--- Casks/codespace.rb
+++ Casks/codespace.rb
@@ -3,8 +3,11 @@ cask 'codespace' do
   sha256 :no_check
 
   url 'https://codespace.app/download/latest'
+  appcast 'https://codespace.app/releases'
   name 'Codespace'
   homepage 'https://codespace.app/'
+
+  depends_on macos: '>= :el_capitan'
 
   app 'Codespace.app'
 end
