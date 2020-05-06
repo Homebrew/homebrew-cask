8ba6da66716696aa3176598eab705ffebf7caf1d
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-06 13:05:16 -0700

    Update presentation from 2.2.0 to 2.2.1 (#82080)

--- Casks/presentation.rb
+++ Casks/presentation.rb
@@ -1,6 +1,6 @@
 cask 'presentation' do
-  version '2.2.0'
-  sha256 '8c441e174e3d3920681c58b40b586c1d10af604f68659dbb264ae7c0572d90b2'
+  version '2.2.1'
+  sha256 'a782153138ac65e71c976365494beabddcccbec509df592af57a3fe8d188a7a0'
 
   url "http://iihm.imag.fr/blanch/software/osx-presentation/releases/osx-presentation-#{version}.pkg"
   appcast 'http://iihm.imag.fr/blanch/software/osx-presentation/'
