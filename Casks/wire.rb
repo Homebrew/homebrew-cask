ec07aa7898e37edcb0495888e3892ae7418dcb0f
Randall <ran.dall@icloud.com>
2020-05-06 11:00:10 -0700

    Update wire from 3.16.3630 to 3.17.3666

--- Casks/wire.rb
+++ Casks/wire.rb
@@ -1,6 +1,6 @@
 cask 'wire' do
-  version '3.16.3630'
-  sha256 '2573bf02530a3cde44549f64a3459e7b47fe1e4bbd9bfe7ab26634b80ab1d2d2'
+  version '3.17.3666'
+  sha256 'f646b364a448686dd88c5aa6ef6ab5427945030e3112332c375081deb29b6c64'
 
   # github.com/wireapp/wire-desktop/ was verified as official when first introduced to the cask
   url "https://github.com/wireapp/wire-desktop/releases/download/macos%2F#{version}/Wire.pkg"
