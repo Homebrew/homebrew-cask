d70e107efe4d7541f10a5d6763fda21c638b3ca4
Crysicia <7459685+Crysicia@users.noreply.github.com>
2020-05-06 21:35:37 +0200

    Add Codespace Latest

---
+++ Casks/codespace.rb
@@ -0,0 +1,10 @@
+cask 'codespace' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://codespace.app/download/latest'
+  name 'Codespace'
+  homepage 'https://codespace.app/'
+
+  app 'Codespace.app'
+end
