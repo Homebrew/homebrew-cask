a02ce2487203c565c696e6af0d0dd2b4ea41e72f
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-05 17:19:35 -0700

    Update aircall from 2.4.3 to 2.4.4 (#82005)

--- Casks/aircall.rb
+++ Casks/aircall.rb
@@ -1,6 +1,6 @@
 cask 'aircall' do
-  version '2.4.3'
-  sha256 'd424d5937c1140efbb2d745ce57c8306d813a8f161df91bcd63d52b30d76530f'
+  version '2.4.4'
+  sha256 'd4396268dda9645c3804a7b876087a37e4025821f5caee938766e6469cc94324'
 
   # aircall-electron-releases.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://aircall-electron-releases.s3.amazonaws.com/production/Aircall-#{version}-mac.zip"
