c75296edf3385fe4b28199372f7128afb0d1cf8a
Qays H. Poonawala <qayshp@gmail.com>
2020-05-05 22:42:35 -0700

    Update Chrysalis from 0.74 to 0.7.5

--- Casks/chrysalis.rb
+++ Casks/chrysalis.rb
@@ -1,5 +1,5 @@
 cask 'chrysalis' do
-  version '0.7.4'
+  version '0.7.5'
   sha256 '8159414ad121e0130044f6bb2a21894b6e98e8da38f7e171cbf3fa95fc794014'
 
   url "https://github.com/keyboardio/Chrysalis/releases/download/chrysalis-#{version}/Chrysalis-#{version}.dmg"
