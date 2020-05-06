3d1b1efcea8669a3770088278aa9b06530a07e0d
Vítor Galvão <info@vitorgalvao.com>
2020-05-06 04:17:38 +0100

    Update and rename android-clt.rb to android-commandlinetools.rb

--- Casks/android-commandlinetools.rb
+++ Casks/android-commandlinetools.rb
@@ -1,11 +1,12 @@
-cask 'android-clt' do
+cask 'android-commandlinetools' do
   version '6200805'
   sha256 '23f0626336a98d70aff7311a73292026af31bc577c6f06b509cd4ad33752313e'
 
   # dl.google.com/android/repository was verified as official when first introduced to the cask
   url "https://dl.google.com/android/repository/commandlinetools-mac-#{version}_latest.zip"
+  appcast 'https://developer.android.com/studio#cmdline-tools'
   name 'android-clt'
-  homepage 'https://developer.android.com/'
+  homepage 'https://developer.android.com/studio'
 
   conflicts_with cask: 'android-sdk'
 
@@ -21,14 +22,5 @@ cask 'android-clt' do
 
   uninstall_postflight do
     FileUtils.rm_f("#{HOMEBREW_PREFIX}/share/android-clt")
-  end
-
-  caveats do
-    depends_on_java '8'
-    <<~EOS
-      You can control android sdk packages via the sdkmanager command.
-      You may want to add to your profile:
-        'export ANDROID_SDK_ROOT="#{HOMEBREW_PREFIX}/share/android-clt"'
-    EOS
   end
 end
