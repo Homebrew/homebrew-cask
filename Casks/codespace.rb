7fbf189a20166ae854cfd4f7ef3a70e6cca1210b
Crysicia <7459685+Crysicia@users.noreply.github.com>
2020-05-07 00:23:16 +0200

    Fix Codespace appcast

--- Casks/codespace.rb
+++ Casks/codespace.rb
@@ -3,7 +3,7 @@ cask 'codespace' do
   sha256 '34a25da2b6034571a93ae278e41ccb4a94e6cf1c523567a4ba61dbba1e9ff001'
 
   url 'https://codespace.app/download/latest'
-  appcast 'https://codespace.app/releases'
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://codespace.app/download/latest'
   name 'Codespace'
   homepage 'https://codespace.app/'
 
