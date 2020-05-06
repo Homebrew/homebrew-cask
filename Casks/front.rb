d7d41751fb2b5db27e000fc592686b769d4599bb
Vítor Galvão <info@vitorgalvao.com>
2020-05-06 23:55:39 +0100

    front.rb: make versioned (3.20.21)

--- Casks/front.rb
+++ Casks/front.rb
@@ -1,8 +1,9 @@
 cask 'front' do
-  version :latest
-  sha256 :no_check
+  version '3.20.21'
+  sha256 '81680f4b27171e9a3e6bf9f7f9db9972a34931e0b92ab702c4f168e978d98631'
 
-  url 'https://dl.frontapp.com/macos/Front.dmg'
+  url "https://dl.frontapp.com/desktop/builds/#{version}/Front-#{version}.zip"
+  appcast 'https://dl.frontapp.com/desktop/updates/latest/mac/latest-mac.yml'
   name 'Front'
   homepage 'https://frontapp.com/'
 
