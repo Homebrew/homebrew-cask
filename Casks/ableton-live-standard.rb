4a0e8b8a59411bb58c62df2e404f03e50a8f0095
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-06 13:02:24 -0700

    Update ableton-live-standard from 10.1.9 to 10.1.13 (#82075)

--- Casks/ableton-live-standard.rb
+++ Casks/ableton-live-standard.rb
@@ -1,6 +1,6 @@
 cask 'ableton-live-standard' do
-  version '10.1.9'
-  sha256 '511b0d6d577e7e1c61cfae2223fd3756381e0df52730df1c74dd003eee6993d5'
+  version '10.1.13'
+  sha256 '5b61ebeaee0906618ff88311393d8ddce0d05ec2ff7c4242671e1234c17c409c'
 
   url "https://cdn-downloads.ableton.com/channels/#{version}/ableton_live_standard_#{version}_64.dmg"
   appcast "https://www.ableton.com/en/release-notes/live-#{version.major}/"
