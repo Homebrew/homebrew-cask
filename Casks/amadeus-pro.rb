5d02479417b1ad0311b1905dcd1fad8c526a58fb
Randall <ran.dall@icloud.com>
2020-05-07 10:27:52 -0700

    Update amadeus-pro from 2.8.1 to 2.8.2

--- Casks/amadeus-pro.rb
+++ Casks/amadeus-pro.rb
@@ -1,6 +1,6 @@
 cask 'amadeus-pro' do
-  version '2.8.1'
-  sha256 'f93390018cd4ec8792a040aeaab419eea08f23dac7b7f5e3b6f94f08dfd50c75'
+  version '2.8.2'
+  sha256 '252dde81287c609ad3e6c7c1a84774164cdb3fd569487d2d8f2b769037fbddc7'
 
   # s3.amazonaws.com/AmadeusPro2/ was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/AmadeusPro2/AmadeusPro.zip'
