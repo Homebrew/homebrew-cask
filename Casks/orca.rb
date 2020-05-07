d347b16c0f9a9fd6a73dc12c18291743c86a9336
Martino Mensio <martino.mensio@open.ac.uk>
2020-05-07 19:26:19 +0100

    fix

--- Casks/orca.rb
+++ Casks/orca.rb
@@ -2,9 +2,12 @@ cask 'orca' do
   version '1.3.1'
   sha256 '79b1b346df9113cd56e545b223a73179801116ca9e009f3c5fe0fac97c0df1ab'
 
-  url 'https://github.com/plotly/orca/releases/download/v{version}/mac-release.zip'
+  url "https://github.com/plotly/orca/releases/download/v#{version}/mac-release.zip"
+  appcast 'https://github.com/plotly/orca/releases.atom'
   name 'Orca'
-  homepage 'https://plotly.com/python/orca-management/'
+  homepage 'https://github.com/plotly/orca/'
+
+  container nested: "orca-#{version}.dmg"
 
   app 'orca.app'
 end
