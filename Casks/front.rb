1178bdf8da52ae0a15efd6e67c177281bc530e3e
Randall <ran.dall@icloud.com>
2020-05-07 10:02:58 -0700

    Update front from 3.20.21 to 3.21.3

--- Casks/front.rb
+++ Casks/front.rb
@@ -1,6 +1,6 @@
 cask 'front' do
-  version '3.20.21'
-  sha256 '81680f4b27171e9a3e6bf9f7f9db9972a34931e0b92ab702c4f168e978d98631'
+  version '3.21.3'
+  sha256 'e3fd40d5f77c196650f24a85b19180bd9845145e41bc8b49aab1f7b8d1a5c208'
 
   url "https://dl.frontapp.com/desktop/builds/#{version}/Front-#{version}.zip"
   appcast 'https://dl.frontapp.com/desktop/updates/latest/mac/latest-mac.yml'
