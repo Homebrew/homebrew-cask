d5da00016b01bd15ae84810cdd34d5825ef8f497
Viktor Szakats <vszakats@users.noreply.github.com>
2020-05-07 02:47:12 +0200

    Switch to subdomain-style S3 URLs (#81972)
    * aviatrix-vpn-client: fix/improve urls
    
    * bubo: fix/improve urls
    
    * qgroundcontrol: fix/improve urls
    
    * vip-access: fix/improve urls

--- Casks/qgroundcontrol.rb
+++ Casks/qgroundcontrol.rb
@@ -2,8 +2,8 @@ cask 'qgroundcontrol' do
   version :latest
   sha256 :no_check
 
-  # s3-us-west-2.amazonaws.com/qgroundcontrol/ was verified as official when first introduced to the cask
-  url 'https://s3-us-west-2.amazonaws.com/qgroundcontrol/latest/QGroundControl.dmg'
+  # qgroundcontrol.s3.amazonaws.com/latest was verified as official when first introduced to the cask
+  url 'https://qgroundcontrol.s3.amazonaws.com/latest/QGroundControl.dmg'
   name 'QGroundControl'
   homepage 'http://qgroundcontrol.com/'
 
