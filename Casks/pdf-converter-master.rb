fcb249fc312ef1d69544a0908e9b84c9606586fb
miccal <miccal.matthews@gmail.com>
2020-05-07 13:08:36 +0800

    Update pdf-converter-master from 6.2.1 to 6.2.0

--- Casks/pdf-converter-master.rb
+++ Casks/pdf-converter-master.rb
@@ -1,6 +1,6 @@
 cask 'pdf-converter-master' do
-  version '6.2.1'
-  sha256 '11110406915ad5a3c657e876adb000260d939cd3cffa91ef2b4476a84cd128d6'
+  version '6.2.0'
+  sha256 '3134b19c8f234b1d0e948fec3115008ada48973f3a8c48ccdd8fd4e8bad7b923'
 
   url "https://www.lightenpdf.com/upload/download/pdf-converter-master-#{version.no_dots}.dmg"
   appcast 'https://www.lightenpdf.com/pdf-converter-mac.html',
