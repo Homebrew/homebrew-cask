1a063b7f087d9ad036550c0b7132cea71d29bbeb
ZhengJin Fang <fangzhengjin@gmail.com>
2020-05-07 22:13:51 +0800

    Update utools from 1.0.0-beta to 1.0.2-beta

--- Casks/utools.rb
+++ Casks/utools.rb
@@ -1,6 +1,6 @@
 cask 'utools' do
-  version '1.0.0-beta'
-  sha256 '7ba4b6574f6ebab5a83e3639ca8bef03a7f5135787e5f400b0ab566c1638ff50'
+  version '1.0.2-beta'
+  sha256 'e6288cf846a733ef67d3fe87ab0454f0b9f44de1830daafa7f0ddbd7f00fbb16'
 
   # resource.u-tools.cn/ was verified as official when first introduced to the cask
   url "https://resource.u-tools.cn/currentversion/uTools-#{version}.dmg"
