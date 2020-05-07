6f3869d9e5dd931b8e9d876dbebb46a326499820
Randall <ran.dall@icloud.com>
2020-05-07 09:18:57 -0700

    Update pritunl from 1.0.2418.61 to 1.0.2428.78

--- Casks/pritunl.rb
+++ Casks/pritunl.rb
@@ -1,6 +1,6 @@
 cask 'pritunl' do
-  version '1.0.2418.61'
-  sha256 '6d74aee65804189a5f03cbb968d7f78c383a6f2dc0eccf205df178f97e0d99bf'
+  version '1.0.2428.78'
+  sha256 '50431e23ea402265c80044222c1cfe6dc7d1e01795465cd9b0e0e4fd82373a08'
 
   # github.com/pritunl/pritunl-client-electron/ was verified as official when first introduced to the cask
   url "https://github.com/pritunl/pritunl-client-electron/releases/download/#{version}/Pritunl.pkg.zip"
