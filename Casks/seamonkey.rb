d11a8033cb5c40b33fd5521333cc0380115d070d
Alex Koster <alexkoster@users.noreply.github.com>
2020-05-06 22:33:38 -0400

    Updated SeaMonkey from 2.53.1 to 2.53.2 (#82093)

--- Casks/seamonkey.rb
+++ Casks/seamonkey.rb
@@ -1,33 +1,33 @@
 cask 'seamonkey' do
-  version '2.53.1'
+  version '2.53.2'
 
   language 'de' do
-    sha256 '7227a9b9a428ba18eaaede58841ca5ad082d60dd73151d84f465613594781e47'
+    sha256 '06dd692e8debe3d8c64f34659f675ae207bf4f1d60b2a9810ab2a389d37c691f'
     'de'
   end
 
   language 'en-GB' do
-    sha256 '8d3b5c90d39d6ab5aa2079a6289d6cede27eb468d9bb97a2b5a7d85972d90124'
+    sha256 '46fdc10bc29f757bccd9cb3cf933490d06a59c63c2fbc572ca626d7bd25e00dd'
     'en-GB'
   end
 
   language 'en-US', default: true do
-    sha256 'c4fab248ee30709b462a9c04ed8ca6e3443ae486c40210d2e521bedf050289bc'
+    sha256 '4ed0131465029c5e6403bf881fd7469f7f7d002b436703166275fa131cab073e'
     'en-US'
   end
 
   language 'fr' do
-    sha256 '74cf24fca6550cb6c27bb33cc115549212ff055befbbf8b49a86f8341088affc'
+    sha256 '075f2fb8f3b2195c8475132054ac9a3d429b575349d6592b16943fedfed9ee68'
     'fr'
   end
 
   language 'it' do
-    sha256 '7a32c5b2e9da21beed987e6cd0a33801cdeab504582a5d0391006bd33eb2de97'
+    sha256 'b3fd64c5dfd7417a54415ce92c20b53ac98be784f263069c1f9d3ca654cc70b7'
     'it'
   end
 
   language 'ru' do
-    sha256 '7ff97c53c2cec5e08e32557cff36e603ba73ec9ac412cc5a7636685630cc464f'
+    sha256 '796867c304b411d07c9f0bf92a9f7330ae97ff830da461386855efc756def3f1'
     'ru'
   end
 
