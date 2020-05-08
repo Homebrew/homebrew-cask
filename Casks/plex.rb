9e7e006ffcfb525006c3986804d62d0ab6b2c101
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-08 15:43:55 -0700

    Update plex from 1.9.0.1203-00f2b4d9 to 1.10.0.1208-daa6b641 (#82240)

--- Casks/plex.rb
+++ Casks/plex.rb
@@ -1,6 +1,6 @@
 cask 'plex' do
-  version '1.9.0.1203-00f2b4d9'
-  sha256 '79d445e5ad203b811bbcc487601252937de894946fc691eb76777eb7dd6a0263'
+  version '1.10.0.1208-daa6b641'
+  sha256 'ff31321d4d63d7859a0e7ed834284bd50c5baba60c44d542553f9063550ef5b6'
 
   url "https://downloads.plex.tv/plex-desktop/#{version}/macos/Plex-#{version}-x86_64.zip"
   appcast 'https://plex.tv/api/downloads/6.json'
