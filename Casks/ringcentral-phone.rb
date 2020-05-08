70034272c8102f51484ccb13cb4fc1dcf98955ee
JayTheMarketer <jay.schwartz@herodigital.com>
2020-05-08 16:25:08 -0500

    Renamed RingCentral to RingCentral Phone

--- Casks/ringcentral-phone.rb
+++ Casks/ringcentral-phone.rb
@@ -1,11 +1,11 @@
-cask 'ringcentral' do
+cask 'ringcentral-phone' do
   version '20.1.0'
   sha256 '2a16143b8b167783c19bd47e79fa7fd94d6d942efdc6a41e19b2cb0c4fbcd816'
 
   url "https://downloads.ringcentral.com/sp/RingCentralPhone-#{version}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://downloads.ringcentral.com/sp/RingCentralForMac'
-  name 'RingCentral for Mac'
+  name 'RingCentral Phone'
   homepage 'https://www.ringcentral.com/apps/rc-phone'
 
-  app 'RingCentral for Mac.app'
+  app 'RingCentral Phone.app'
 end
