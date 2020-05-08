3c762298859bd728471031392738595af20c91cd
Vítor Galvão <info@vitorgalvao.com>
2020-05-08 16:51:17 +0100

    mysqlworkbench.rb: fix conditional

--- Casks/mysqlworkbench.rb
+++ Casks/mysqlworkbench.rb
@@ -3,13 +3,11 @@ cask 'mysqlworkbench' do
     version '6.3.10'
     sha256 '29857bf84bebb7c4442ce147e44602d00f8c001e3c09b3a6e3af356767e08d2c'
     url "https://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-#{version}-macos-x86_64.dmg"
-    appcast 'https://dev.mysql.com/downloads/workbench/'
-  elsif MacOS.version == :high_sierra
+  elsif MacOS.version <= :high_sierra
     version '8.0.16'
     sha256 '3478800290e2797d294e3721fdaea4c41ddc1917f2b59ec94a935e16c18dc5d2'
     url "https://downloads.mysql.com/archives/get/p/#{version.major}/file/mysql-workbench-community-#{version}-macos-x86_64.dmg"
-    appcast 'https://downloads.mysql.com/archives/workbench/'
-  elsif MacOS.version >= :mojave
+  else
     version '8.0.20'
     sha256 'a5c11b83fbcb1817e982eed7a6d170ffca6c67d3a9496817c395c4a60b571cc0'
     url "https://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-#{version}-macos-x86_64.dmg"
@@ -18,8 +16,6 @@ cask 'mysqlworkbench' do
 
   name 'MySQL Workbench'
   homepage 'https://www.mysql.com/products/workbench/'
-
-  depends_on macos: '>= :high_sierra'
 
   app 'MySQLWorkbench.app'
 
