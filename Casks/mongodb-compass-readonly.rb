bc2f1f9002dd72cf3608f2ebd0fd47821e78705b
Randall <ran.dall@icloud.com>
2020-05-08 15:37:55 -0700

    Update mongodb-compass-readonly from 1.21.0 to 1.21.1

--- Casks/mongodb-compass-readonly.rb
+++ Casks/mongodb-compass-readonly.rb
@@ -1,6 +1,6 @@
 cask 'mongodb-compass-readonly' do
-  version '1.21.0'
-  sha256 '80ef5e017c6100e9013930643a587e2e149e082637f80ddf30ac617f11bd9611'
+  version '1.21.1'
+  sha256 '3b3d09f91d2c8f553a2e747cb288ba35a9fbbab52886529acf277035a10b62fe'
 
   url "https://downloads.mongodb.com/compass/mongodb-compass-readonly-#{version}-darwin-x64.dmg"
   appcast 'https://www.mongodb.com/download-center/compass'
