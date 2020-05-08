37867e2eb0683f1965822219ee2cd44f1cc520c6
Robert Fairhead <robertfairhead@users.noreply.github.com>
2020-05-08 10:49:10 -0500

    Update Aptible to 0.16.5 (#82200)

--- Casks/aptible.rb
+++ Casks/aptible.rb
@@ -1,6 +1,6 @@
 cask 'aptible' do
-  version '0.16.3,20191024182313,194'
-  sha256 '3bb96ba7cc99842988f041075eadf203a949ad211bb83d10d073fc3647715e95'
+  version '0.16.5,20200508143959,206'
+  sha256 '7d3fc6d0a56ae25dd913e72930ac6c8ada6a5629e03771be6761b2fa03f60103'
 
   # omnibus-aptible-toolbelt.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://omnibus-aptible-toolbelt.s3.amazonaws.com/aptible/omnibus-aptible-toolbelt/master/#{version.after_comma.after_comma}/pkg/aptible-toolbelt-#{version.before_comma}%2B#{version.after_comma.before_comma}-mac-os-x.10.11.6-1.pkg"
