e3fd71442c24ae1bd29c233819810aa8c5729129
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-08 15:05:03 -0700

    Update http-toolkit from 0.1.21 to 0.1.22 (#82219)

--- Casks/http-toolkit.rb
+++ Casks/http-toolkit.rb
@@ -1,6 +1,6 @@
 cask 'http-toolkit' do
-  version '0.1.21'
-  sha256 '54ccdb363ebfa55a55452c68b66106476701fb2e1bc63e4336565e65189c10ae'
+  version '0.1.22'
+  sha256 '85c4b4252a581d79ac01cf1e9ba8e08ea2edba663a20d7869925056a4e117cf8'
 
   # github.com/httptoolkit/httptoolkit-desktop/ was verified as official when first introduced to the cask
   url "https://github.com/httptoolkit/httptoolkit-desktop/releases/download/v#{version}/HTTP.Toolkit.dmg"
