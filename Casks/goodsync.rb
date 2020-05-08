0dd1bc69d867fb2ba7d7e8c7c1bebe2c40cadac0
Felipe Martins <fefas@users.noreply.github.com>
2020-05-08 12:48:25 +0200

    Update goodsync from 11.1.6.8 to 11.1.7.7 (#82180)

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,6 +1,6 @@
 cask 'goodsync' do
-  version '11.1.6.8'
-  sha256 '9b2cff74afa6672714e06637779070edb49f7a48c817988af04b366a7c3bec7e'
+  version '11.1.7.7'
+  sha256 'defb18d018ddeef00363700544a798cd6ffbe63d90c6e88d13fe8a11a8fac040'
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
   appcast 'https://www.goodsync.com/download?os=macos',
