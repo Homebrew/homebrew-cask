f3bc701b431f5c0630aaf8076b5c2758e30015b4
JayTheMarketer <jay.schwartz@herodigital.com>
2020-05-08 15:40:34 -0500

    Updates to RingCentral Casks
    Glip has beeen renamed RingCentral Classic. RingCentral for Mac has been
    renamed RingCentral Phone. Added newly released RingCentral app.

---
+++ Casks/ringcentral-classic.rb
@@ -0,0 +1,24 @@
+cask 'ringcentral-classic' do
+  version '20.2.20'
+  sha256 'f7da52b2c132240a93e3fbff0bd92a0276288703ea2e2a7b9d54474be3f489cc'
+
+  url "https://downloads.ringcentral.com/glip/rc/#{version}/mac/RingCentral%20Classic-#{version}.dmg"
+  name 'RingCentral Classic'
+  homepage 'https://www.ringcentral.com/apps/rc-classic'
+
+  app 'RingCentral Classic.app'
+
+  uninstall signal: [
+                      ['TERM', 'com.Glip.Glip.helper'],
+                      ['TERM', 'com.Glip.Glip'],
+                    ]
+
+  zap trash: [
+               '~/Library/Application Support/Glip',
+               '~/Library/Caches/com.Glip.Glip',
+               '~/Library/Caches/com.Glip.Glip.ShipIt',
+               '~/Library/Preferences/com.Glip.Glip.helper.plist',
+               '~/Library/Preferences/com.Glip.Glip.plist',
+               '~/Library/Saved Application State/com.Glip.Glip.savedState',
+             ]
+end
