2b2f0833eae1057f7b39f239f7b1e3e17e6b4842
Randall <ran.dall@icloud.com>
2020-05-08 15:07:39 -0700

    Update bdash from 1.8.2 to 1.8.3

--- Casks/bdash.rb
+++ Casks/bdash.rb
@@ -1,6 +1,6 @@
 cask 'bdash' do
-  version '1.8.2'
-  sha256 'b4893236b613ca9d56df1de050cc0c37c597e7ee914a379ad41d66adbfddd7b3'
+  version '1.8.3'
+  sha256 '7d93babc7bdf2a1cad6e6083d2b52a55141fc5e6c24c41c96585357e84da5f04'
 
   url "https://github.com/bdash-app/bdash/releases/download/v#{version}/Bdash-#{version}-mac.zip"
   appcast 'https://github.com/bdash-app/bdash/releases.atom'
