00d12040854d3818b9fcc991c0b8c2a6ddfbb29d
LanikSJ <laniksj@gmail.com>
2020-05-07 18:40:48 -0700

    Update jump from 8.5.10 to 8.5.15

--- Casks/jump.rb
+++ Casks/jump.rb
@@ -1,6 +1,6 @@
 cask 'jump' do
-  version '8.5.10'
-  sha256 '9991fa64892e8cfe7f302925cb28885bc3d431a4888a92fa6babb0866e46141d'
+  version '8.5.15'
+  sha256 'e90e0211481a52bc74dac39bb9847bfe605fdce628225d87ef9c8961985bfed9'
 
   url "https://mirror.jumpdesktop.com/downloads/JumpDesktopMac-#{version}.zip"
   appcast 'https://jumpdesktop.com/downloads/viewer/jdmac-web-appcast.xml'
