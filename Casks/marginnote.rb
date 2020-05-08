16bde75f32d35db9a9c5445e8953d8bf4a2c64c1
suschizu <brigitte@corecode.io>
2020-05-08 20:24:09 +0200

    Update marginnote from 3.6.7 to 3.6.8 (#82190)
    * Update marginnote from 3.6.7 to 3.6.8
    
    * Update marginnote.rb
    
    * Update marginnote.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/marginnote.rb
+++ Casks/marginnote.rb
@@ -1,10 +1,10 @@
 cask 'marginnote' do
-  version '3.6.7'
-  sha256 '6495892ed0d10aa02fa7dde830aa6a1759158ee4386dd77c3ec486238972e97c'
+  version '3.6.8'
+  sha256 '689bfed0228fe0782a831e9530303b890fcaa645c01ae245eb73e36990d5925b'
 
   # marginstudy.com/ was verified as official when first introduced to the cask
   url "https://marginstudy.com/mac/MarginNote#{version.major}.dmg"
-  appcast 'https://api.appcenter.ms/v0.1/public/sparkle/apps/1451f4f6-9214-4d46-b91c-d5898c7e42cb'
+  appcast "https://dist.marginnote.cn/marginnote#{version.major}.xml"
   name 'MarginNote'
   homepage 'https://www.marginnote.com/'
 
