f6c618e3d07e6dd3c6e4171284b0028861b7fd64
Eduard Strehlau <herdinger@Eduards-MacBook-Pro.local>
2020-05-10 00:05:26 +0200

    Update retroarch-metal from 1.8.4 to 1.8.6

--- Casks/retroarch-metal.rb
+++ Casks/retroarch-metal.rb
@@ -1,6 +1,6 @@
 cask 'retroarch-metal' do
-  version '1.8.4'
-  sha256 'bdc9fd2cbaab1e848ec187e02f5f17abf9c03c1e8edf2b595356e2418d448986'
+  version '1.8.6'
+  sha256 '521afa552e147b912626b9a4a793eefe34c379d504cb159aa682edb019c166bf'
 
   url "https://buildbot.libretro.com/stable/#{version}/apple/osx/x86_64/RetroArch_Metal.dmg"
   appcast 'https://buildbot.libretro.com/stable/'
