d36905b62ecc452bc53986ca2e3c661b00c54424
Randall <ran.dall@icloud.com>
2020-05-09 14:11:15 -0700

    Update arq from 6.2.11 to 6.2.14

--- Casks/arq.rb
+++ Casks/arq.rb
@@ -1,5 +1,5 @@
 cask 'arq' do
-  version '6.2.11'
+  version '6.2.14'
   sha256 '116842dab5c794bf44e8d9c0010ecf0e9f1bad1cd2fcd2f1d0682816856c939d'
 
   url "https://www.arqbackup.com/download/arqbackup/Arq#{version.major}.pkg"
