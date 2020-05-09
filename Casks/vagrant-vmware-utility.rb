333541fbd28be224c9925ddfa403da25cf0d56df
António Meireles <antonio.meireles@reformi.st>
2020-05-09 19:49:21 +0100

    Update vagrant-vmware-utility from 1.0.7 to 1.0.9 (#82275)

--- Casks/vagrant-vmware-utility.rb
+++ Casks/vagrant-vmware-utility.rb
@@ -1,6 +1,6 @@
 cask 'vagrant-vmware-utility' do
-  version '1.0.7'
-  sha256 '19ef37ef3c1ca3a93cbde2ef8da8a934345ebdd91be78fde770ee6cfa7c6f725'
+  version '1.0.9'
+  sha256 '49fe628c8904e81112bc6644dfa570b8a6a5da640260cc92126ee8a04f96b661'
 
   # releases.hashicorp.com/vagrant-vmware-utility/ was verified as official when first introduced to the cask
   url "https://releases.hashicorp.com/vagrant-vmware-utility/#{version}/vagrant-vmware-utility_#{version}_x86_64.dmg"
