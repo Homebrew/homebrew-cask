9d5585a9bb282f999b174fc12ca5715beb825a39
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-10 14:19:15 -0700

    Update grids from 6.0.9 to 6.0.10 (#82347)

--- Casks/grids.rb
+++ Casks/grids.rb
@@ -1,6 +1,6 @@
 cask 'grids' do
-  version '6.0.9'
-  sha256 '3a643ba2c2cdccb15db85874e138db1ce45f47966f652068843394b94de230fb'
+  version '6.0.10'
+  sha256 '17c2673c70e6acd00e351398bce34829a482dae886ff8d7a4ec6ec713d88c6eb'
 
   url "https://gridsapp.net/bin/Grids_#{version}.zip"
   appcast 'https://gridsapp.net/appcast.json'
