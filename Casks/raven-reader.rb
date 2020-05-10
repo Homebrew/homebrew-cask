e97d1cd17f63afd6dae8de57b789229d4b10dce9
Meet Godhani <meetgodhani23@gmail.com>
2020-05-10 00:04:34 -0400

    Update raven-reader from 0.7.6 to 0.7.7 (#82307)

--- Casks/raven-reader.rb
+++ Casks/raven-reader.rb
@@ -1,6 +1,6 @@
 cask 'raven-reader' do
-  version '0.7.6'
-  sha256 'e3c2350da9b5e7f2f17a4f55063be3303b5a2527ec2137231fb446eaa484a6a4'
+  version '0.7.7'
+  sha256 'f8b8ea1c8b2ad6218c88a05ab32f782f1af904a587bb8ebd56f9d1ccb3aa7bde'
 
   url "https://download.ravenreader.app/ravenreader/Raven%20Reader-#{version}-mac.zip"
   appcast 'https://download.ravenreader.app/ravenreader/latest-mac.yml'
