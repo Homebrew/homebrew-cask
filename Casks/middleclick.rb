d6426017021a7013bed8acf3e80fcfb7fb21e6ef
Vítor Galvão <info@vitorgalvao.com>
2020-05-10 01:01:20 +0100

    middleclick.rb: discontinued

--- Casks/middleclick.rb
+++ Casks/middleclick.rb
@@ -13,4 +13,8 @@ cask 'middleclick' do
             quit:       'com.rouge41.middleClick'
 
   zap trash: '~/Library/Preferences/com.rouge41.middleClick.plist'
+
+  caaveats do
+    discontinued
+  end
 end
