57372e48b0283d643615c6ebf169bb343e7b1210
suschizu <brigitte@corecode.io>
2020-05-11 17:33:56 +0200

    Update epichrome.rb from 2.2.4 to 2.3.0 + update to pkg (#82366)
    * Update epichrome.rb
    
    * Update epichrome.rb

--- Casks/epichrome.rb
+++ Casks/epichrome.rb
@@ -1,11 +1,13 @@
 cask 'epichrome' do
-  version '2.2.4'
-  sha256 'a9da4cb25a8c8797fb8ae7fddb1e78511a6812761b701cabb5aa6007be3cba7a'
+  version '2.3.0'
+  sha256 'a1967f9cb554893a2745cb14a4853106341891d4a40c4b97f58cc3d544dc90f1'
 
-  url "https://github.com/dmarmor/epichrome/releases/download/v#{version}/epichrome-#{version}.dmg"
+  url "https://github.com/dmarmor/epichrome/releases/download/v#{version}/epichrome-#{version}.pkg"
   appcast 'https://github.com/dmarmor/epichrome/releases.atom'
   name 'Epichrome'
   homepage 'https://github.com/dmarmor/epichrome'
 
-  app 'Epichrome.app'
+  pkg "epichrome-#{version}.pkg"
+
+  uninstall pkgutil: 'org.epichrome.Epichrome'
 end
