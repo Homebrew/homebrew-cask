bc2059c69087e17ba5cad3848447ac8a56027305
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-11 14:09:28 -0700

    Update mattermost from 4.4.1 to 4.4.2 (#82378)

--- Casks/mattermost.rb
+++ Casks/mattermost.rb
@@ -1,6 +1,6 @@
 cask 'mattermost' do
-  version '4.4.1'
-  sha256 'd1b9f80f8c21ae4cdc5e52697ce49b3a7eedb33896ee2d9fdd5567b69bbff8e6'
+  version '4.4.2'
+  sha256 '90b70d659b39097fc49bb67fc323ec95b18c62229a6978c4b4f9a2328b6c8e84'
 
   url "https://releases.mattermost.com/desktop/#{version}/mattermost-desktop-#{version}-mac.zip"
   appcast 'https://github.com/mattermost/desktop/releases.atom'
