270ba3e9e0583878f6b145d8f621987c5abc28f1
ZhengJin Fang <fangzhengjin@gmail.com>
2020-05-11 23:19:13 +0800

    Update ntfstool.rb (#82371)

--- Casks/ntfstool.rb
+++ Casks/ntfstool.rb
@@ -1,6 +1,6 @@
 cask 'ntfstool' do
-  version '2.2.0'
-  sha256 'd0c9ac70759774e3aea99709de9145c288dc3c179fcdd5f793d1efe818d0d1fb'
+  version '2.3.0'
+  sha256 'dcab362ff0e3b3f0bc0fae6429246d178812c0cdd554a0c931502c89dda8e8c8'
 
   url "https://github.com/ntfstool/ntfstool/releases/download/#{version}/NTFSTool-#{version}.dmg"
   appcast 'https://github.com/ntfstool/ntfstool/releases.atom'
