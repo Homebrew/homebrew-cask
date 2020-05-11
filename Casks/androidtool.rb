6ef1a67a7c209e4f11e9ac9d0798822a44156b6b
Andrew Mu <muandrew@gmail.com>
2020-05-10 18:36:16 -0700

    Update androidtool to 1.70.0
    The project repository was changed because the original
    author `mortenjust` is no longer maintaining this project.
    
    Thanks `mortenjust` for starting this excellent tool!

--- Casks/androidtool.rb
+++ Casks/androidtool.rb
@@ -1,11 +1,11 @@
 cask 'androidtool' do
-  version '1.66'
-  sha256 '4f432ad45660071a1f36c4878bd444dd8bedd520c7c70096e5d7a7907ff7c86a'
+  version '1.70.0'
+  sha256 'd643136a0694e922701305c7bc0c14302ba6652fc20a21defb41300f49a731d4'
 
-  url "https://github.com/mortenjust/androidtool-mac/releases/download/#{version}/AndroidTool.zip"
-  appcast 'https://github.com/mortenjust/androidtool-mac/releases.atom'
+  url "https://github.com/muandrew/androidtool-mac/releases/download/release%2F#{version}/AndroidTool-#{version}.zip"
+  appcast 'https://github.com/muandrew/androidtool-mac/releases.atom'
   name 'AndroidTool'
-  homepage 'https://github.com/mortenjust/androidtool-mac'
+  homepage 'https://github.com/muandrew/androidtool-mac'
 
   app 'AndroidTool.app'
 end
