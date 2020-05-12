68aae6e3d56f368a293538b0569b9353b27ac673
JayTheMarketer <jay.schwartz@herodigital.com>
2020-05-12 10:16:26 -0500

    Add RingCentral Phone 20.1.0

---
+++ Casks/ringcentral-phone.rb
@@ -0,0 +1,11 @@
+cask 'ringcentral-phone' do
+  version '20.1.0'
+  sha256 '2a16143b8b167783c19bd47e79fa7fd94d6d942efdc6a41e19b2cb0c4fbcd816'
+
+  url "https://downloads.ringcentral.com/sp/RingCentralPhone-#{version}.dmg"
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://downloads.ringcentral.com/sp/RingCentralForMac'
+  name 'RingCentral Phone'
+  homepage 'https://www.ringcentral.com/apps/rc-phone'
+
+  app 'RingCentral for Mac.app'
+end
