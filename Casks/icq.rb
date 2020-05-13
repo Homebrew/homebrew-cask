3159bd789266c1d2af33b6e7b4c7189b98adcce5
Viktor Oreshkin <imselfish@stek29.rocks>
2020-05-13 16:23:33 +0300

    Update icq url to include version and auto_updates

--- Casks/icq.rb
+++ Casks/icq.rb
@@ -2,11 +2,13 @@ cask 'icq' do
   version '3.0.27060'
   sha256 '54f277c93995f1c4ba70b524c040d8cc0539fe63c9e18787dddd2d768f57e1fc'
 
-  # hb.bizmrg.com/icq-www/ was verified as official when first introduced to the cask
-  url 'https://hb.bizmrg.com/icq-www/mac/x64/icq.dmg'
+  # icq-www.hb.bizmrg.com was verified as official when first introduced to the cask
+  url "https://icq-www.hb.bizmrg.com/mac/x64/#{version}/icq.dmg"
   appcast "https://icq-www.hb.bizmrg.com/mac/x64/#{version}/version.xml"
   name 'ICQ'
-  homepage 'https://icq.com/mac/en'
+  homepage 'https://icq.com/desktop'
+
+  auto_updates true
 
   app 'ICQ.app'
 
