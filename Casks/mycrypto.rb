558cf5268db7874bd3e4ddc1c44fc73f43be8526
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-12 17:07:00 -0700

    Update mycrypto from 1.7.9 to 1.7.10 (#82442)

--- Casks/mycrypto.rb
+++ Casks/mycrypto.rb
@@ -1,6 +1,6 @@
 cask 'mycrypto' do
-  version '1.7.9'
-  sha256 'b3fdba4dd5a9e17ec468970e30360bd993412b750a68d9580d95d515fff3e49a'
+  version '1.7.10'
+  sha256 '235918375040f575eb466f6761feab9ee8501ec2887fe866938d8d02ab1c2da8'
 
   # github.com/MyCryptoHQ/MyCrypto/ was verified as official when first introduced to the cask
   url "https://github.com/MyCryptoHQ/MyCrypto/releases/download/#{version}/mac_#{version}_MyCrypto.dmg"
