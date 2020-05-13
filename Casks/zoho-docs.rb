064a8ee125ca2584d60ac8f494486b11016b29a3
Julian Mayer <julian@corecode.io>
2020-05-13 12:11:19 +0200

    Update zoho-docs

--- Casks/zoho-docs.rb
+++ Casks/zoho-docs.rb
@@ -2,7 +2,7 @@ cask 'zoho-docs' do
   version :latest
   sha256 :no_check
 
-  url 'https://www.zoho.com/docs/36925/ZohoDocs.dmg'
+  url 'https://www.zoho.com/docs/download-page.html'
   name 'Zoho Docs'
   homepage 'https://www.zoho.com/docs/desktop-sync.html'
 
