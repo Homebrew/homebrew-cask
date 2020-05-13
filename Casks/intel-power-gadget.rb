009328cc0c3dda1a6c1a9cd374143c46342366a4
miccal <miccal.matthews@gmail.com>
2020-05-13 14:28:35 +0800

    Update intel-power-gadget.rb

--- Casks/intel-power-gadget.rb
+++ Casks/intel-power-gadget.rb
@@ -1,9 +1,10 @@
 cask 'intel-power-gadget' do
-  version '3.7.0'
+  version '3.6.0'
   sha256 'b99c5c55b541e7b6c5d1a390b11ee662352b8dae955df183d7e1113cebd94ab6'
 
   url 'https://software.intel.com/content/dam/develop/external/us/en/documents/Intel%20Power%20Gadget.dmg'
-  appcast 'https://software.intel.com/content/www/us/en/develop/articles/intel-power-gadget.html'
+  appcast 'https://software.intel.com/content/www/us/en/develop/articles/intel-power-gadget.html',
+          configuration: version.major
   name 'Intel Power Gadget'
   homepage 'https://software.intel.com/content/www/us/en/develop/articles/intel-power-gadget.html'
 
