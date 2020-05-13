5ca920060820f499d8b72f602df6eb415cee8ef6
gottabeleGIT <sandesh.manik@icloud.com>
2020-05-13 10:49:28 -0500

    Update brave-browser.rb

--- Casks/brave-browser.rb
+++ Casks/brave-browser.rb
@@ -1,6 +1,6 @@
 cask 'brave-browser' do
-  version '81.1.8.95,108.95'
-  sha256 '5854792c0e942434c25ba7e3e7b36527579980fbc4193de0d4dbea62c76c587f'
+  version '81.1.8.96,108.96'
+  sha256 '82e81c9c0065003ea941f5c05b6c27ee4162aac218a79ac8f828bc9ebecb3fd6'
 
   # updates-cdn.bravesoftware.com/sparkle/Brave-Browser/ was verified as official when first introduced to the cask
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/stable/#{version.after_comma}/Brave-Browser.dmg"
