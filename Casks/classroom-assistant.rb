fa669204f30a031ef00d3c2b013c8103122aff37
Konstantin Malanchev <hombit@gmail.com>
2020-05-13 10:12:42 +0300

    Update classroom-assistant from 2.0.0 to 2.0.1

--- Casks/classroom-assistant.rb
+++ Casks/classroom-assistant.rb
@@ -1,6 +1,6 @@
 cask 'classroom-assistant' do
-  version '2.0.0'
-  sha256 '9af87976ab74f131a3486329229ffdd40ecdb3ebff082cdc2c2fac08a22394c5'
+  version '2.0.1'
+  sha256 'b3e13813d5895cb115a44714d24a0a279efc7c6e04b59922de3dd3cd7bfeeac2'
 
   url "https://github.com/education/classroom-assistant/releases/download/v#{version}/Classroom.Assistant-darwin-x64-#{version}.zip"
   appcast 'https://github.com/education/classroom-assistant/releases.atom'
