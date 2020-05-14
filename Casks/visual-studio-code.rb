d6b44163837bb1fe9a2897c691649e0a36a6b2e5
linbo0518 <linbo0518@gmail.com>
2020-05-14 16:28:18 -0500

    Update visual-studio-code from 1.45.0 to 1.45.1

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -1,6 +1,6 @@
 cask 'visual-studio-code' do
-  version '1.45.0'
-  sha256 'c924f6937c6cdd6aedc82c98179deb783e354c057799caba69963dd5add16570'
+  version '1.45.1'
+  sha256 '367e8c3642fbb976cde0d1fa93a87d89ea7db422403a0e51ef1a5b243711acb0'
 
   url "https://update.code.visualstudio.com/#{version}/darwin/stable"
   appcast 'https://vscode-update.azurewebsites.net/api/update/darwin/stable/VERSION'
