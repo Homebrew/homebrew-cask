da4ca81af7955e37d0591cd415a0fea61d016dd3
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-13 17:50:34 -0700

    Update knime from 4.1.1 to 4.1.3 (#82561)

--- Casks/knime.rb
+++ Casks/knime.rb
@@ -1,6 +1,6 @@
 cask 'knime' do
-  version '4.1.1'
-  sha256 '4904d7fb32faf9bfab26401ec51875601d1110021471fb3fa323e6d51317da7b'
+  version '4.1.3'
+  sha256 'c87665c6746462c23f7ba567a281d6fd7146987b7c918a1ce0f9ac04bb82cdcc'
 
   # download.knime.org/analytics-platform/macosx/ was verified as official when first introduced to the cask
   url "https://download.knime.org/analytics-platform/macosx/knime_#{version}.app.macosx.cocoa.x86_64.dmg"
