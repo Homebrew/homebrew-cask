e69a1b3e3987912b7061e770e96aa6243b51aeb3
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-14 12:34:27 -0700

    Update wondershare-filmora from 9.4.7.1 to 9.4.8.1 (#82621)

--- Casks/wondershare-filmora.rb
+++ Casks/wondershare-filmora.rb
@@ -1,6 +1,6 @@
 cask 'wondershare-filmora' do
-  version '9.4.7.1'
-  sha256 '54ade79fa347750ab398024c84f8572f354b5fc6a828e9c685e392f7f6862e6f'
+  version '9.4.8.1'
+  sha256 'd28f314ea60d2fbc913271cb1e9b75203e0bb5416c4fedafbf46173df1db9304'
 
   url "http://download.wondershare.com/filmora#{version.major}-mac_full718.dmg"
   name 'Wondershare Filmora9'
