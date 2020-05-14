0b98986d0158c36153593c4e4b902cadf67a81cd
Mashiro Wang <hyperoot@gmail.com>
2020-05-14 19:40:27 +0800

    update lightproxy.app to 1.1.15

--- Casks/lightproxy.rb
+++ Casks/lightproxy.rb
@@ -1,9 +1,9 @@
 cask 'lightproxy' do
-  version '1.1.14'
-  sha256 '4530ecd308c2b7111bc6e5e58f412e8c31575970c4b697b935ee9226484da433'
+  version '1.1.15'
+  sha256 '0d6a265c05f6cfc97dcd39a17137934dcfc7cc249d04e9cdb9f7a91416842d0b'
 
   # gw.alipayobjects.com/os/LightProxy/ was verified as official when first introduced to the cask
-  url 'https://gw.alipayobjects.com/os/LightProxy/LightProxy.dmg'
+  url 'https://gw.alipayobjects.com/os/LightProxy/a6d8f427-0b65-4967-9354-21a0d66cf4dd/LightProxy.dmg'
   appcast 'https://github.com/alibaba/lightproxy/tree/master/CHANGELOG'
   name 'LightProxy'
   homepage 'https://alibaba.github.io/lightproxy/'
