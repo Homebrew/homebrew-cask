e0720162675192dd2b6f588cf2e09857e14b13fc
Peter Lewis <pdl25@bath.ac.uk>
2020-05-14 16:07:57 +0100

    Update vivi from 2.18.4 to 2.19.0

--- Casks/vivi.rb
+++ Casks/vivi.rb
@@ -1,6 +1,6 @@
 cask 'vivi' do
-  version '2.18.4'
-  sha256 '3a1c34e2bf120c32d4b7f5e4bec40e47ec06cebb7126bcdd11746f42cbf60a93'
+  version '2.19.0'
+  sha256 '1c5570600243fa9e91735f42a33e686e0f6bfb5fe7c441f9a4eaea90b04f31fa'
 
   url "https://downloads.vivi.io/app/#{version}/Vivi.pkg"
   name 'Vivi'
