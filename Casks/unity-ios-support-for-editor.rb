3367f80faff33e73b2eaac7922f3ee4fef090fb2
suschizu <brigitte@corecode.io>
2020-05-15 00:08:25 +0200

    Update unity-ios-support-for-editor from 2019.3.12f1,84b23722532d to 2019.3.13f1,d4ddf0d95db9 (#82582)

--- Casks/unity-ios-support-for-editor.rb
+++ Casks/unity-ios-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask 'unity-ios-support-for-editor' do
-  version '2019.3.12f1,84b23722532d'
-  sha256 '3ec8114e4589a1e6760b393ed3fda6694acacfa928e37bd31ba61f2e5d3fa536'
+  version '2019.3.13f1,d4ddf0d95db9'
+  sha256 'ef39ecce48455966060ad63b9c543638a5c3fb64ed873c0425fce15372da4624'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-#{version.before_comma}.pkg"
   appcast 'https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json'
