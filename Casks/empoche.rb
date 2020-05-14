11a8f9817250b97f901f18bb551f485d34c30f2a
Randall <ran.dall@icloud.com>
2020-05-14 12:31:58 -0700

    Update empoche from 0.3.2 to 0.3.3

--- Casks/empoche.rb
+++ Casks/empoche.rb
@@ -1,6 +1,6 @@
 cask 'empoche' do
-  version '0.3.2'
-  sha256 'ff49288a14b0676d168607356811beddd4a5cb259c3356d38baf7476e92efa34'
+  version '0.3.3'
+  sha256 'e856b04297e4b63898a5429f5afcf6bd84fb3bbe0866b81230e23c1395acb9f6'
 
   # empoche-desktop.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://empoche-desktop.s3.amazonaws.com/Empoche-#{version}-mac.zip"
