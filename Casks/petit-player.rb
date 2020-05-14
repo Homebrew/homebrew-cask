eb8622d32baf96f76cf8a6f4413b6f0e9b692b7d
saltylight <micaelace@kaist.ac.kr>
2020-05-14 23:21:35 +0900

    fix checks

--- Casks/petit-player.rb
+++ Casks/petit-player.rb
@@ -1,12 +1,12 @@
-cask 'petitplayer' do
+cask 'petit-player' do
   version '0.0.11'
   sha256 '796ade626c551955882e3f84d719b223f3059247a195c4b28a1cdaedfff560dc'
 
+  # github.com/getpetit was verified as official when first introduced to the cask
   url "https://github.com/getpetit/petit-youtube-music-release/releases/download/v#{version}/Petit-Player-#{version}.dmg"
   appcast 'https://github.com/getpetit/petit-youtube-music-release/releases.atom'
   name 'Petit Player'
-  homepage 'https://getpetit.github.io'
+  homepage 'https://getpetit.github.io/'
 
   app 'Petit Player.app'
 end
-
