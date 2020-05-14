f505f662f9a8b0ba505862007cdcf989375f61de
Yassine ElBadaoui <byassine52@users.noreply.github.com>
2020-05-15 08:18:13 +0900

    Add Fireworks.app

---
+++ Casks/fireworks.rb
@@ -0,0 +1,10 @@
+cask 'fireworks' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.fireworksapp.xyz/public/fireworks.zip'
+  name 'Fireworks'
+  homepage 'https://www.fireworksapp.xyz/'
+
+  app 'Fireworks.app'
+end
