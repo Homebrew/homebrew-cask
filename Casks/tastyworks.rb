9b76344fa79ba68d53395539c3e649be7316c34e
suschizu <brigitte@corecode.io>
2020-05-14 14:19:00 +0200

    Update tastyworks from 1.5.4 to 1.6.0 (#82590)

--- Casks/tastyworks.rb
+++ Casks/tastyworks.rb
@@ -1,6 +1,6 @@
 cask 'tastyworks' do
-  version '1.5.4'
-  sha256 '8918e10a73c0bc8280c36f6caf038bef11c39112b23b11f8110e4f40d092b121'
+  version '1.6.0'
+  sha256 '8fe9a9554a1b97f77b002cd549912a4a1cb3a9ad73f761aca104d86e65a56f35'
 
   url "https://download.tastyworks.com/desktop-#{version.major}.x.x/#{version}/tastyworks-#{version}.dmg"
   appcast 'https://tastyworks.freshdesk.com/support/solutions/articles/43000435186-recent-release-notes',
