5b36cab27439222097c5174062d41fddcd9763f9
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-15 11:08:04 -0700

    Update remember-the-milk from 1.1.20 to 1.1.21 (#82679)

--- Casks/remember-the-milk.rb
+++ Casks/remember-the-milk.rb
@@ -1,6 +1,6 @@
 cask 'remember-the-milk' do
-  version '1.1.20'
-  sha256 'ab319267db20a6eaf6384c72a8f53fe27134ad4ddc1e2a96ceedc16942aba5d8'
+  version '1.1.21'
+  sha256 'da8b71984866c4b0fafd8baa93a237c34d10bb24bf64a13c8fb4bc82f6a4a1b8'
 
   url "https://www.rememberthemilk.com/download/mac/RememberTheMilk-#{version}.zip"
   appcast 'https://www.rememberthemilk.com/services/mac/'
