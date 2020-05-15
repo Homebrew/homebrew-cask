69b5097c757d9ed2538083b7500878cbb0aeed84
suschizu <brigitte@corecode.io>
2020-05-16 00:25:43 +0200

    Update wey.rb

--- Casks/wey.rb
+++ Casks/wey.rb
@@ -2,7 +2,7 @@ cask 'wey' do
   version '0.3.7'
   sha256 'a98359f3a1ce34c0e45887628b37fad2949a273d0dd814a0ae37ad3f3b59f3aa'
 
-  url "https://github.com/yue/wey/releases/download/v#{version}/wey-v0.3.6-darwin-x64.zip"
+  url "https://github.com/yue/wey/releases/download/v#{version}/wey-v#{version}-darwin-x64.zip"
   appcast 'https://github.com/yue/wey/releases.atom'
   name 'Wey'
   homepage 'https://github.com/yue/wey'
