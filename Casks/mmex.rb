3f8e2a1045ff78b8e626462250473a8dfa8a97f2
Brigitte Zmoelnig <brigitte@corecode.io>
2020-05-15 19:07:32 +0200

    Update mmex

--- Casks/mmex.rb
+++ Casks/mmex.rb
@@ -1,9 +1,9 @@
 cask 'mmex' do
   version '1.3.4'
-  sha256 '3d094573241527ab8c578c4dd879373df5ff662d528cb329ab74f36f4836efe2'
+  sha256 '8338c3e8c859b900d86437b8ff72e80c16f440b568068419bc65aa920a4f66a7'
 
-  # github.com/moneymanagerex/moneymanagerex/ was verified as official when first introduced to the cask
-  url "https://github.com/moneymanagerex/moneymanagerex/releases/download/v#{version}-Release/mmex-#{version}-Darwin.dmg"
+  # sourceforge.net/projects/moneymanagerex/ was verified as official when first introduced to the cask
+  url 'https://sourceforge.net/projects/moneymanagerex/files/latest/download'
   appcast 'https://github.com/moneymanagerex/moneymanagerex/releases.atom'
   name 'Money Manager Ex'
   homepage 'https://www.moneymanagerex.org/'
