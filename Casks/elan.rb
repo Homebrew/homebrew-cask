dd1c9affba447cbeab4f5c5cdf8361bf3a5ee6e9
suschizu <brigitte@corecode.io>
2020-05-16 00:19:55 +0200

    Update elan (#82668)

--- Casks/elan.rb
+++ Casks/elan.rb
@@ -3,9 +3,9 @@ cask 'elan' do
   sha256 '91aa8a64403641a77a135719a8b5bd7f5a5e5d73649adb1f3eaad92dffde584d'
 
   url "https://www.mpi.nl/tools/elan/ELAN_#{version.dots_to_hyphens}_mac.zip"
-  appcast 'https://tla.mpi.nl/tools/tla-tools/elan/release-notes/'
+  appcast 'https://archive.mpi.nl/tla/elan/release-notes'
   name 'ELAN'
-  homepage 'https://tla.mpi.nl/tools/tla-tools/elan/'
+  homepage 'https://archive.mpi.nl/tla/elan'
 
   depends_on macos: '>= :high_sierra'
 
