f4c1d5d1a866b5a04868dc3f0b7a5db8b96fc3c7
Brigitte Zmoelnig <brigitte@corecode.io>
2020-05-16 23:00:07 +0200

    Update tweeten from 4.7.0 to 5.0.3

--- Casks/tweeten.rb
+++ Casks/tweeten.rb
@@ -1,6 +1,6 @@
 cask 'tweeten' do
-  version '4.7.0'
-  sha256 '0feb238d2d1ab57ba3bae2949ffb4b3dc7e51f78257dbdde3d00654acb8e1c0b'
+  version '5.0.3'
+  sha256 '942a5dcd010caae035e511939e98ef2135d458434f5d575fc3e652e179cbec50'
 
   # github.com/MehediH/Tweeten/ was verified as official when first introduced to the cask
   url "https://github.com/MehediH/Tweeten/releases/download/v#{version}/tweeten-darwin-x64.zip"
