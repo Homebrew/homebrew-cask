537fb82933214ffab47f7ebc720f0216929d7164
singularitti <singularitti@outlook.com>
2020-05-16 03:05:46 -0400

    Update mactracker from 7.9.1 to 7.9.2

--- Casks/mactracker.rb
+++ Casks/mactracker.rb
@@ -1,6 +1,6 @@
 cask 'mactracker' do
-  version '7.9.1'
-  sha256 'fb811b503fd16580299cf56195b20a50c46fce07e332b1a73a759df6f4c6d116'
+  version '7.9.2'
+  sha256 'f9fc3a163d33ae08c34a3efcda61cfeaee7f764123fb559a362a476f04a520d1'
 
   url "https://www.mactracker.ca/downloads/Mactracker_#{version}.zip"
   appcast 'https://update.mactracker.ca/appcast-b.xml'
