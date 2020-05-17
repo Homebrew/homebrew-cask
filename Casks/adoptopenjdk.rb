80cc951b838af96cc554cec9a4c712c6519da212
George Adams <george.adams@microsoft.com>
2020-05-17 18:25:14 +0100

    Switch adoptopenjdk cask to use pkg (#73145)

--- Casks/adoptopenjdk.rb
+++ Casks/adoptopenjdk.rb
@@ -1,14 +1,16 @@
 cask 'adoptopenjdk' do
   version '14.0.1,7'
-  sha256 'b11cb192312530bcd84607631203d0c1727e672af12813078e6b525e3cce862d'
+  sha256 'e85e446fac091f74e9e962bee5efb77c347fca3ad138a5db058cc62cb4d86e2a'
 
   # github.com/AdoptOpenJDK/ was verified as official when first introduced to the cask
-  url "https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/download/jdk-#{version.before_comma}%2B#{version.after_comma}/OpenJDK#{version.major}U-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma}.tar.gz"
+  url "https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/download/jdk-#{version.before_comma}%2B#{version.after_comma}/OpenJDK#{version.major}U-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma}.pkg"
   appcast "https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/latest"
   name 'AdoptOpenJDK Java Development Kit'
   homepage 'https://adoptopenjdk.net/'
 
-  artifact "jdk-#{version.before_comma}+#{version.after_comma}", target: "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.major}.jdk"
+  pkg "OpenJDK#{version.major}U-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma}.pkg"
+
+  uninstall pkgutil: "net.adoptopenjdk.#{version.major}.jdk"
 
   caveats <<~EOS
     More versions are available in the AdoptOpenJDK tap:
