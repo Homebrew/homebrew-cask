cb795d669b6f2cb3ce57185d2f39f4a5f11a5384
Randall <ran.dall@icloud.com>
2020-05-17 13:06:10 -0700

    Update clashx from 1.20.0 to 1.20.1

--- Casks/clashx.rb
+++ Casks/clashx.rb
@@ -1,6 +1,6 @@
 cask 'clashx' do
-  version '1.20.0'
-  sha256 '6bddd177bbc3ec5fb9ef056525953d43533dbffd61fca64084e83cc0f568b4d1'
+  version '1.20.1'
+  sha256 '24ac6f8100e16588ed32089878f83ba2b3bdb9210a6c44baf2f12f9b92d70885'
 
   url "https://github.com/yichengchen/clashX/releases/download/#{version}/ClashX.dmg"
   appcast 'https://github.com/yichengchen/clashX/releases.atom'
