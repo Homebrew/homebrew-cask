33fd7a91eb9f53a47e68e6f3e005b8fd387b6578
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-16 17:07:07 -0700

    Update ghost-browser from 2.1.1.11 to 2.1.1.12 (#82747)

--- Casks/ghost-browser.rb
+++ Casks/ghost-browser.rb
@@ -1,6 +1,6 @@
 cask 'ghost-browser' do
-  version '2.1.1.11'
-  sha256 '39c151fe69e451bd76d7765cc29eb470deb04c836376483c54d3641b91f9fdf5'
+  version '2.1.1.12'
+  sha256 '81c1e2da47c5f7b1d1892cebe91b5fd755699110d2ae9ab7248e8bc7a3853780'
 
   # ghostbrowser.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://ghostbrowser.s3.amazonaws.com/downloads/GhostBrowser-#{version}.dmg"
