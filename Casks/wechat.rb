8da93a5b863868989a907f65e88485567a564b68
b0ttle <48851366+b0ttle@users.noreply.github.com>
2020-05-18 01:52:58 +0800

    Update wechat from 2.4.1.17 to 2.4.1.18 (#82697)
    Co-authored-by: b0ttle <shixu@localhost>

--- Casks/wechat.rb
+++ Casks/wechat.rb
@@ -1,6 +1,6 @@
 cask 'wechat' do
-  version '2.4.1.17'
-  sha256 '4b23e9e78e959f279af0328686ca6c0a9b47f01be6f713b21ae63d111fe44f96'
+  version '2.4.1.18'
+  sha256 '336bc27938f64ea16c681259e79f9a4f96c442085475c759a929819facb30458'
 
   url 'https://dldir1.qq.com/weixin/mac/WeChatMac.dmg'
   appcast 'https://dldir1.qq.com/weixin/mac/mac-release.xml'
