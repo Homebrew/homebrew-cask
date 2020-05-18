9f6c4df85fc421b6cdb2bff5c6099049d49d3f36
Kirk Klobe <kklobe@gmail.com>
2020-05-18 10:24:23 -0500

    Update segger-embedded-studio-for-arm from 4.52b to 4.52c (#82808)
    Co-authored-by: Kirk Klobe <kirk@habitaware.com>

--- Casks/segger-embedded-studio-for-arm.rb
+++ Casks/segger-embedded-studio-for-arm.rb
@@ -1,6 +1,6 @@
 cask 'segger-embedded-studio-for-arm' do
-  version '4.52b'
-  sha256 '3f55cf1242d59fcf65797a2d785efa363ea8fc7299e475d167b8c9cc16a53ea2'
+  version '4.52c'
+  sha256 '3a380781029296cfbe11121971743afc96659ccae109c8ea746c9a138fe3998e'
 
   url "https://www.segger.com/downloads/embedded-studio/Setup_EmbeddedStudio_ARM_v#{version.no_dots}_macos_x64.dmg"
   appcast 'https://studio.segger.com/arm_segger_studio_release_notes.htm'
