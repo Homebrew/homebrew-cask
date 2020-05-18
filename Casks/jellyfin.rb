3e43f52f70b9c947421873f0eaabe8724e754a81
Shivan Sornarajah <sornars@sornars.com>
2020-05-18 12:20:28 +0100

    Re-add appcast to Jellyfin.app v10.5.5b

--- Casks/jellyfin.rb
+++ Casks/jellyfin.rb
@@ -3,6 +3,7 @@ cask 'jellyfin' do
   sha256 'e60ebbda1998a2f1daebdb33a46b4b1076696cec01f5b33c974342a8f75bed27'
 
   url "https://repo.jellyfin.org/releases/server/macos/stable/Jellyfin_#{version}.dmg"
+  appcast 'https://repo.jellyfin.org/releases/server/macos/stable/README_FIRST.txt'
   name 'Jellyfin'
   homepage 'https://jellyfin.org/'
 
