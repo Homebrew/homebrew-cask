01c2624032bc1cad7045fb155c453ff382dd7df8
John Benninghoff <jbenninghoff@mac.com>
2020-05-18 07:48:48 -0500

    Update graphicconverter from 11.2,4376 to 11.2.1,4400 (#82805)

--- Casks/graphicconverter.rb
+++ Casks/graphicconverter.rb
@@ -1,6 +1,6 @@
 cask 'graphicconverter' do
-  version '11.2,4376'
-  sha256 'd786ffde76832be3b39111bc62857d463e1f99cfd572b998a618b5342c94909e'
+  version '11.2.1,4400'
+  sha256 'd03bb3c8e7676b034e2e8bffdc9323357593634ecb83054a60d1f4791716b2d0'
 
   # lemkesoft.info/ was verified as official when first introduced to the cask
   url "https://www.lemkesoft.info/files/graphicconverter/gc#{version.major}_build#{version.after_comma}.zip"
