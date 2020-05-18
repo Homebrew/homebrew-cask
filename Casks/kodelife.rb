767db41bcc35b04547dee0c55e43dca6a747df0e
Randall <ran.dall@icloud.com>
2020-05-18 15:13:30 -0700

    Update kodelife from 0.9.1.132 to 0.9.2.134

--- Casks/kodelife.rb
+++ Casks/kodelife.rb
@@ -1,6 +1,6 @@
 cask 'kodelife' do
-  version '0.9.1.132'
-  sha256 'a4782eeefc7c4a91d81fc36925ee4465dea471eeaa53feae934480bacf40d92d'
+  version '0.9.2.134'
+  sha256 '99059e98f5cdc60ecc2d298fb791506811653be9481dee49d84ef7ef53db2cd2'
 
   url "https://hexler.net/pub/kodelife/kodelife-#{version}-macos.zip"
   appcast 'https://hexler.net/pub/kodelife/appcast.hex'
