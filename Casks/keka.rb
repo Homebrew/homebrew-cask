11fe4be175c7652a29dae5f326b584fc8de3cefe
Robert Blair <robertleeblairjr@gmail.com>
2020-05-19 12:40:54 -0500

    Update keka from 1.1.28 to 1.1.29

--- Casks/keka.rb
+++ Casks/keka.rb
@@ -1,6 +1,6 @@
 cask 'keka' do
-  version '1.1.28'
-  sha256 'c76ae2096348c8d4d54152d4a83c7075c3afbc6bf903161be90dd26ec2b0dcf2'
+  version '1.1.29'
+  sha256 '2f0edc0897913351b408427e96898b71548a648c538423b789e5a8d0e02fd69c'
 
   # github.com/aonez/Keka/ was verified as official when first introduced to the cask
   url "https://github.com/aonez/Keka/releases/download/v#{version}/Keka-#{version}.dmg"
