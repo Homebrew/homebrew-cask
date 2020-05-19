45c52a71555ca518a3d1c79b828e09aa4475ebe3
suschizu <brigitte@corecode.io>
2020-05-19 16:23:43 +0200

    Update zalo.rb from 20.3.3 to 20.4.2 (#82863)
    * Update zalo.rb
    
    * Update zalo.rb

--- Casks/zalo.rb
+++ Casks/zalo.rb
@@ -1,9 +1,9 @@
 cask 'zalo' do
-  version '20.3.3'
-  sha256 'ae2b694e8eebc2a582a1cbe11d99d8dd18e65aed449271f78bf5c777197d1a26'
+  version '20.4.2'
+  sha256 '2864653a83c1dbb4a3693b2726d2897ad1d654ce9e19262122023ffed55dcb62'
 
   # res-zaloapp-aka.zdn.vn/mac/ was verified as official when first introduced to the cask
-  url "https://res-zaloapp-aka.zdn.vn/mac/ZaloSetup_#{version}.dmg"
+  url "https://res-zaloapp-aka.zdn.vn/mac/ZaloSetup-#{version}.dmg#"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?user_agent=Macintosh&url=https://zalo.me/download/zalo-pc'
   name 'Zalo'
   homepage 'https://zalo.me/'
