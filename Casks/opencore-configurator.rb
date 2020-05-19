e814ef31af1fce98579a9c066c00c6641e00573a
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-19 19:20:02 +0000

    Update opencore-configurator from 2.3.0.0 to 2.4.0.0 (#82879)

--- Casks/opencore-configurator.rb
+++ Casks/opencore-configurator.rb
@@ -1,6 +1,6 @@
 cask 'opencore-configurator' do
-  version '2.3.0.0'
-  sha256 'ec6ac5ad5931b70c9553b8ccea481c44ca5d3ddcf16f50bdbae7e847d6ed3f61'
+  version '2.4.0.0'
+  sha256 'f546ed55d5d7308b3f1c41524b4bda62993c0b771fb16506cd2748ed2c3dc978'
 
   url 'https://mackie100projects.altervista.org/apps/opencoreconf/download-new-build.php?version=last'
   appcast 'https://mackie100projects.altervista.org/category/opencore-configurator-changelog/'
