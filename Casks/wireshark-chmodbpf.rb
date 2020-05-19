d4172a55cd98e62434e018bc226728eeeef12a5c
Florian Floimair <f.floimair@commend.com>
2020-05-19 22:48:09 +0200

    Update wireshark-chmodbpf from 3.2.3 to 3.2.4

--- Casks/wireshark-chmodbpf.rb
+++ Casks/wireshark-chmodbpf.rb
@@ -1,6 +1,6 @@
 cask 'wireshark-chmodbpf' do
-  version '3.2.3'
-  sha256 '066a05b20dce30f55a9ae8543cdf62771250352ab74c93186b8fb8a37a3aaf18'
+  version '3.2.4'
+  sha256 '735d43bdbde1caeb9bfff89acc62acfeb05ffda36e6d0d243bc08333e60fdac6'
 
   url "https://www.wireshark.org/download/osx/Wireshark%20#{version}%20Intel%2064.dmg"
   appcast 'https://www.wireshark.org/update/0/Wireshark/0.0.0/macOS/x86-64/en-US/stable.xml'
