c82117bbfbdecc7c8c5a2aef5a2e4724d96a688e
suschizu <brigitte@corecode.io>
2020-05-19 16:16:57 +0200

    Update zalo.rb

--- Casks/zalo.rb
+++ Casks/zalo.rb
@@ -3,7 +3,7 @@ cask 'zalo' do
   sha256 '2864653a83c1dbb4a3693b2726d2897ad1d654ce9e19262122023ffed55dcb62'
 
   # res-zaloapp-aka.zdn.vn/mac/ was verified as official when first introduced to the cask
-  url "https://res-zaloapp-aka.zdn.vn/mac/ZaloSetup_#{version}.dmg"
+  url "https://res-zaloapp-aka.zdn.vn/mac/ZaloSetup-#{version}.dmg#"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?user_agent=Macintosh&url=https://zalo.me/download/zalo-pc'
   name 'Zalo'
   homepage 'https://zalo.me/'
