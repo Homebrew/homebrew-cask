509c9bd693e29ba54e5483e9cfc34af6b2cd676e
Randall <ran.dall@icloud.com>
2020-05-19 12:09:51 -0700

    Update permute from 3.4.8,2259 to 3.4.9,2262

--- Casks/permute.rb
+++ Casks/permute.rb
@@ -1,6 +1,6 @@
 cask 'permute' do
-  version '3.4.8,2259'
-  sha256 '7c9c7cb0c9e52d8afb6cd9e8af8ff9856d4e0f6eb2bf517502856892e3537c14'
+  version '3.4.9,2262'
+  sha256 'e5d42fbd65fd5a1320d7b5a19619601f26a17bd4f9ed3f689bde8035679f0349'
 
   url "https://trial.charliemonroe.net/permute/v#{version.major}/Permute_#{version.major}_#{version.after_comma}.dmg"
   appcast "https://trial.charliemonroe.net/permute/updates_#{version.major}.xml"
