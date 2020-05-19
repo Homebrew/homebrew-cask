aea1a50a5d171c5da09fd0b65f74aef2a800250a
Randall <ran.dall@icloud.com>
2020-05-19 12:26:40 -0700

    Update abstract from 92.0.0 to 92.1.0

--- Casks/abstract.rb
+++ Casks/abstract.rb
@@ -1,6 +1,6 @@
 cask 'abstract' do
-  version '92.0.0'
-  sha256 '7c0e1987ec7dbb14ede5d5ad07bc486abfd6f1ab9363a2769ab17936690c4ade'
+  version '92.1.0'
+  sha256 '5e3f2ffd2e9e5dc9b76c688a2d0950b5268a0dbbb45edef5be372eaef1f98d31'
 
   url "https://downloads.goabstract.com/mac/Abstract-#{version}.dmg"
   appcast 'https://www.goabstract.com/release-notes/'
