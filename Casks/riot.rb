7a110b5e62557b843ccb35236f3c00acc7a91c10
Diego <jahndiego@mailbox.org>
2020-05-19 16:34:39 +0200

    Update riot from 1.6.0 to 1.6.1 (#82861)
    * Update riot from 1.6.0 to 1.6.1
    
    * Update riot.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/riot.rb
+++ Casks/riot.rb
@@ -1,9 +1,9 @@
 cask 'riot' do
-  version '1.6.0'
-  sha256 '81fe1585d187c21bcebcedfd0a98a5766e8b792123383ff1d91752dcec878bc5'
+  version '1.6.1'
+  sha256 '3501bbbd9c4968fd2fb9f36ba1ec955e8c03b35689f45ee0ba9cab98d0d74847'
 
   url "https://packages.riot.im/desktop/install/macos/Riot-#{version}.dmg"
-  appcast 'https://riot.im/download/desktop/'
+  appcast 'https://github.com/vector-im/riot-desktop/releases.atom'
   name 'Riot'
   homepage 'https://about.riot.im/'
 
