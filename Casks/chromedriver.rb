c90e6e86658fa9daad1b7052692b930573a74909
Andrew Grieser <agrieser@gmail.com>
2020-05-19 14:55:22 -0500

    Update chromedriver to 83.0.4103.39

--- Casks/chromedriver.rb
+++ Casks/chromedriver.rb
@@ -1,6 +1,6 @@
 cask 'chromedriver' do
-  version '81.0.4044.138'
-  sha256 '15e53d13dfeb7305a89aaa3701fd03d6aa14d60b98458f48333d1d5ec54c14d1'
+  version '83.0.4103.39'
+  sha256 'd902d4b3e77e466102ebfe25e13a25798b473655461fa3e928268faf6fc7fef6'
 
   # chromedriver.storage.googleapis.com/ was verified as official when first introduced to the cask
   url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_mac64.zip"
