57f6f1207d149b40db6879656eefb16469979d03
suschizu <brigitte@corecode.io>
2020-05-19 23:45:20 +0200

    Update houdahspot from 5.1 to 5.1.1 (#82858)

--- Casks/houdahspot.rb
+++ Casks/houdahspot.rb
@@ -1,8 +1,8 @@
 cask 'houdahspot' do
-  version '5.1'
+  version '5.1.1'
   sha256 'e353651fbcdb972432798102edce6b6395d1e401abc2183fb69be5a9302bc43f'
 
-  url "https://www.houdah.com/houdahSpot/updates/cast5_assets/HoudahSpot#{version}.zip"
+  url "https://www.houdah.com/houdahSpot/download_assets/HoudahSpot#{version}.zip"
   appcast "https://www.houdah.com/houdahSpot/updates/cast#{version.major}.xml"
   name 'HoudahSpot'
   homepage 'https://www.houdah.com/houdahSpot/'
