23baddd20e368654c5afaf91a207e17e63a35e71
suschizu <brigitte@corecode.io>
2020-05-19 17:47:48 +0200

    Update sizzy from 0.19.0 to 0.20.0 (#82859)

--- Casks/sizzy.rb
+++ Casks/sizzy.rb
@@ -1,6 +1,6 @@
 cask 'sizzy' do
-  version '0.19.0'
-  sha256 'c71a6d3a4c04a0fb7a0100bfd6652446010bca527c3952d95fe2f5427614ffc8'
+  version '0.20.0'
+  sha256 'f9780d350b9e69666069af63cdeadec2ecea960c988e02df0a4e01b51f1a00ec'
 
   url 'https://sizzy.co/get-app'
   appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://sizzy.co/get-app&user_agent=Intel%20Mac%20OS%20X'
