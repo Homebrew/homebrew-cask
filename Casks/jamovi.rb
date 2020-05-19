c774c9a6dfa48618f81216801018d4ffbfd3352b
CoreCode <core-code@users.noreply.github.com>
2020-05-19 18:22:52 +0200

    Update jamovi.rb (#82860)
    * Update jamovi.rb
    
    * Update jamovi.rb
    
    * Update jamovi.rb

--- Casks/jamovi.rb
+++ Casks/jamovi.rb
@@ -3,7 +3,8 @@ cask 'jamovi' do
   sha256 'edc430dd3c3be63b2e7a4ea42a09c2c7cb3d34d747f0e8b4742c37c08c00af44'
 
   url "https://www.jamovi.org/downloads/jamovi-#{version}-macos.dmg"
-  appcast 'https://www.macupdater.net/cgi-bin/extract_text/extract_text_split_easy.cgi?url=https://www.jamovi.org/download.html&splitter_1=macos.dmg&index_1=0&splitter_2=download-button&index_2=-1'
+  appcast 'https://www.jamovi.org/download.html',
+          configuration: "solid</td><td>.dmg</td><td>#{version.chomp('.0')}"
   name 'jamovi'
   homepage 'https://www.jamovi.org/'
 
