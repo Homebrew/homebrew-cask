57b006da36e5147064f93176661c1c6325732afd
Randall <ran.dall@icloud.com>
2020-05-20 10:34:05 -0700

    Update flipper from 0.42.0 to 0.43.0

--- Casks/flipper.rb
+++ Casks/flipper.rb
@@ -1,6 +1,6 @@
 cask 'flipper' do
-  version '0.42.0'
-  sha256 '3db7d206b4b7d02695cceebf3f1acccbf3eb2d099edd0374abfafffa6c15a021'
+  version '0.43.0'
+  sha256 '73c11edd7f0e1553350714090d722c94e3bfabcb0174fea7a81809d0a1d5c5bc'
 
   # github.com/facebook/flipper/ was verified as official when first introduced to the cask
   url "https://github.com/facebook/flipper/releases/download/v#{version}/Flipper-mac.zip"
