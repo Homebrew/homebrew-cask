4ac93d020b293bd6c1f692a1350d8864779607da
eloyaldea <35566945+eloyaldea@users.noreply.github.com>
2020-05-20 19:34:32 +0200

    Update altserver from 1.3 to 1.3.1 (#82944)
    Co-authored-by: EAG <eag@EAG15.local>

--- Casks/altserver.rb
+++ Casks/altserver.rb
@@ -1,6 +1,6 @@
 cask 'altserver' do
-  version '1.3'
-  sha256 'c83d5076435d88260ae99674776274bb3f23aa6dc54e717ad65a0c26c66f88f4'
+  version '1.3.1'
+  sha256 '40ac78739c0a24bbc8b8da0694c0197c73fc31872a92903391f558ec195048b5'
 
   # f000.backblazeb2.com/file/ was verified as official when first introduced to the cask
   url "https://f000.backblazeb2.com/file/altstore/altserver/#{version.dots_to_underscores}.zip"
