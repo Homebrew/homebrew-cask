a2b532d9138fb7fd6a2355a669a9e03748088756
suschizu <brigitte@corecode.io>
2020-05-20 20:13:39 +0200

    Update gdlauncher from 0.12.4 to 1.0.3 (#82940)

--- Casks/gdlauncher.rb
+++ Casks/gdlauncher.rb
@@ -1,9 +1,9 @@
 cask 'gdlauncher' do
-  version '0.12.4'
-  sha256 'd73f751659443117b0bf8948e3e74494f96a1c2060cdb1dcaa21854d270209f3'
+  version '1.0.3'
+  sha256 'ccf9cd4d7ffb03a0717c344e64c95db71f687becf27e15770c57a43552881b15'
 
   # github.com/gorilla-devs/GDLauncher/ was verified as official when first introduced to the cask
-  url "https://github.com/gorilla-devs/GDLauncher/releases/download/v#{version}/GDLauncher-mac-setup.zip"
+  url "https://github.com/gorilla-devs/GDLauncher/releases/download/v#{version}/GDLauncher-mac-setup.dmg"
   appcast 'https://github.com/gorilla-devs/GDLauncher/releases.atom'
   name 'GDLauncher'
   homepage 'https://gdevs.io/'
