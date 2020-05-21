c3df59d9fe117bd7623553fea201d04defd633fb
Phill Tran <web@philltran.com>
2020-05-20 22:00:51 -0400

    Update lando from 3.0.1 to 3.0.3

--- Casks/lando.rb
+++ Casks/lando.rb
@@ -1,6 +1,6 @@
 cask 'lando' do
-  version '3.0.1'
-  sha256 '280eff6b2a9b262c122d6cc705f80bdea8dcf3d229400248ffedd7868bbd9ac4'
+  version '3.0.3'
+  sha256 'a89a3919b6d95cdf15d92dd12234936477593e18f748ad1aeb4217fd6e6624a5'
 
   # github.com/lando/lando/ was verified as official when first introduced to the cask
   url "https://github.com/lando/lando/releases/download/v#{version}/lando-v#{version}.dmg"
