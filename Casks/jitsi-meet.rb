1a9f4f9227d518bbd265a28fdb35ae40812cecd3
Thomas Walter <tw@b-a-l-u.de>
2020-05-21 19:42:26 +0200

    Update jitsi-meet from 2.0.2 to 2.1.0

--- Casks/jitsi-meet.rb
+++ Casks/jitsi-meet.rb
@@ -1,6 +1,6 @@
 cask 'jitsi-meet' do
-  version '2.0.2'
-  sha256 'ae42f86862762d73e3f7a091c76b79626a2ca4d8604db203e4b92e59582dd5bb'
+  version '2.1.0'
+  sha256 'a2afa085b079db79bf21de29a81a85b9a9341eeea0a0f6cf19b7569e4457ea3e'
 
   url "https://github.com/jitsi/jitsi-meet-electron/releases/download/v#{version}/jitsi-meet.dmg"
   appcast 'https://github.com/jitsi/jitsi-meet-electron/releases.atom'
