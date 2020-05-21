917e319d7defd705423d5d2a7a08da5d6b91b37b
Ben <BenjaminHCCarr@users.noreply.github.com>
2020-05-21 13:46:09 -0400

    Bump MenuMeters to 2.0.7

--- Casks/menumeters.rb
+++ Casks/menumeters.rb
@@ -1,6 +1,6 @@
 cask 'menumeters' do
-  version '2.0.6.1'
-  sha256 '2959fbd4474738afec9172127701e3c1f47fa6f7db0ff1bdb90ec745c500ebce'
+  version '2.0.7'
+  sha256 'a63217c32670f19913f2f814c91b760b5df57413649440e8f1c80821b763713e'
 
   url "https://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/zips/MenuMeters_#{version}.zip"
   appcast 'https://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/MenuMeters-Update.xml'
