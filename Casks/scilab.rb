684b2c1979bc02131501768a06e8f1839cc1cf48
Zhenfu Shi <szf1234@me.com>
2020-05-20 22:30:29 -0400

    Update scilab (#82926)
    * Update scilab to 6.1.0
    
    * Update scilab.rb
    
    * Update scilab.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/scilab.rb
+++ Casks/scilab.rb
@@ -1,20 +1,26 @@
 cask 'scilab' do
-  version '6.0.2'
-  sha256 'aec8b67c29fb93c7fbae85e8b42d96f908e893ca680fb2d3e4fbb7012d8c8720'
+  version '6.1.0'
+  sha256 '8076c09047d5039291388b5ceadc13d9f98e943ef885fd16f7f46c46bc62df85'
 
   # utc.fr/~mottelet/scilab/ was verified as official when first introduced to the cask
-  url "https://www.utc.fr/~mottelet/scilab/download/#{version}/scilab-#{version}-x86_64.dmg"
+  url "https://www.utc.fr/~mottelet/scilab/download/branch-#{version.major_minor}/scilab-branch-#{version.major_minor}-x86_64.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.scilab.org/download/'
   name 'Scilab'
   homepage 'https://www.scilab.org/'
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: '>= :high_sierra'
 
-  app "scilab-#{version}.app"
-  binary "#{appdir}/Scilab-#{version}.app/Contents/MacOS/bin/scilab"
-  binary "#{appdir}/Scilab-#{version}.app/Contents/MacOS/bin/scilab-cli"
+  app "scilab-branch-#{version.major_minor}.app"
+  binary "#{appdir}/Scilab-branch-#{version.major_minor}.app/Contents/MacOS/bin/scilab"
+  binary "#{appdir}/Scilab-branch-#{version.major_minor}.app/Contents/MacOS/bin/scilab-cli"
 
   caveats do
     depends_on_java '8'
+
+    <<~EOS
+      If prompted to install the legacy Java 6, point #{token} to your installed JDK with the enableJDK tool provided by the vendor:
+
+        https://www.utc.fr/~mottelet/scilab_for_macOS.html
+    EOS
   end
 end
