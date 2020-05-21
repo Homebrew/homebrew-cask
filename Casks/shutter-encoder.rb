22329b34530ff9eac220b984fef37ba213c70a97
CoreCode <core-code@users.noreply.github.com>
2020-05-21 03:11:50 +0200

    Update shutter-encoder (#82929)

--- Casks/shutter-encoder.rb
+++ Casks/shutter-encoder.rb
@@ -1,8 +1,8 @@
 cask 'shutter-encoder' do
   version '13.7'
-  sha256 '964da3a0a6cf657fbb24490f026c6d04a23c0ef178a26de4e8fe5e657b57922c'
+  sha256 '154d3fcaa1977243f956672bc5f7d1b0a99e4202326713a5501b5ffcf81ff89f'
 
-  url "https://www.shutterencoder.com/Shutter%20Encoder%20(MAC%20Version%20#{version}).dmg"
+  url "https://www.shutterencoder.com/Shutter%20Encoder%20(MAC%20Version%20#{version}).zip"
   appcast 'https://www.shutterencoder.com/changelog.txt'
   name 'Shutter Encoder'
   homepage 'https://www.shutterencoder.com/'
