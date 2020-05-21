bd35e88c2c5bd831e74b8cb965b988332730faef
suschizu <brigitte@corecode.io>
2020-05-21 14:24:02 +0200

    Update macwinzipper (#82999)

--- Casks/macwinzipper.rb
+++ Casks/macwinzipper.rb
@@ -2,10 +2,10 @@ cask 'macwinzipper' do
   version '2.7'
   sha256 '37ff612125d80acf39a873e087a301c524708bb0c4af64be55a335555af23d62'
 
-  url "https://tida.me/files/MacWinZipper-#{version}.dmg?download"
-  appcast 'https://tida.me/macwinzipper'
+  url "https://tida.co.jp/files/MacWinZipper-#{version}.dmg"
+  appcast 'https://tida.co.jp/macwinzipper'
   name 'MacWinZipper'
-  homepage 'https://tida.me/macwinzipper'
+  homepage 'https://tida.co.jp/macwinzipper'
 
   app 'MacWinZipper.app'
 end
