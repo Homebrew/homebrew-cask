5e74bb3bff4bc1a83196335eebb2b3d8ae3106b9
John Benninghoff <jbenninghoff@mac.com>
2020-05-21 08:04:07 -0500

    Update delicious-library from 3.9.1 to 3.9.2 (#83005)

--- Casks/delicious-library.rb
+++ Casks/delicious-library.rb
@@ -1,6 +1,6 @@
 cask 'delicious-library' do
-  version '3.9.1'
-  sha256 'c306034b7f0915adc3b83689923913db88f0f0fbc9fe7a1c70d6814eab758193'
+  version '3.9.2'
+  sha256 'b7cd5d5e4a410f7191b7cfcda3433818797787cc73507c5672a3502326dc2590'
 
   url "https://www.delicious-monster.com/downloads/DeliciousLibrary#{version.major}/v#{version}/DeliciousLibrary#{version.major}.zip"
   appcast "https://www.delicious-monster.com/downloads/DeliciousLibrary#{version.major}.xml"
