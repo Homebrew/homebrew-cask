0325b315b7d1d4ee896d8e16611f46f4d739d92f
suschizu <brigitte@corecode.io>
2020-05-22 23:26:38 +0200

    Update visit.rb (#83080)

--- Casks/visit.rb
+++ Casks/visit.rb
@@ -1,13 +1,13 @@
 cask 'visit' do
-  version '3.1.1'
-
   if MacOS.version <= :high_sierra
+    version '3.1.1'
     sha256 '4213daed23de17ee8bcfba779a96cce3ef92d3075ae666f7aeaffa824d484924'
 
     # github.com/visit-dav/visit/ was verified as official when first introduced to the cask
     url "https://github.com/visit-dav/visit/releases/download/v#{version}/visit#{version}.darwin-x86_64-10.13.dmg"
   else
-    sha256 '4bb596c4411dc5f7b19598fc1a1ccaefe227527e2623cd1c628288c12be69d99'
+    version '3.1.2'
+    sha256 '10a8c52d947f4c1adea820238775273da3d60a1b2484d5cbd4fe5781451f567d'
 
     # github.com/visit-dav/visit/ was verified as official when first introduced to the cask
     url "https://github.com/visit-dav/visit/releases/download/v#{version}/visit#{version}.darwin-x86_64-10_14.dmg"
