690a28064429c3c93c291f25829c9154ad19324c
Randall <ran.dall@icloud.com>
2020-05-21 17:59:37 -0700

    Update pagico from 9.3.20200518 to 9.3.20200521

--- Casks/pagico.rb
+++ Casks/pagico.rb
@@ -1,6 +1,6 @@
 cask 'pagico' do
-  version '9.3.20200518'
-  sha256 'cac5ccfe5002ff1dfbd5f952cb4d880baa2c3a81a0a359456935ebf0b4e052d1'
+  version '9.3.20200521'
+  sha256 'c4a8036696c3c85af3bd3b1d9750cbc0433d1cac7838252da25ffdb02b0c3e0d'
 
   url "https://www.pagico.com/downloads/Pagico_macOS_r#{version.patch}.dmg"
   appcast "https://www.pagico.com/api/pagico#{version.major}.mac.xml",
