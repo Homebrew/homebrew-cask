c45721ab8bb368b59e24c44a6203cabb577d50e4
Randall <ran.dall@icloud.com>
2020-05-22 15:59:00 -0700

    Update lingo from 10.5,111 to 10.6,2998

--- Casks/lingo.rb
+++ Casks/lingo.rb
@@ -1,6 +1,6 @@
 cask 'lingo' do
-  version '10.5,111'
-  sha256 'a92fa560723151dbb6a9a2450e6c73dcca43c4b1103153c519d2a258c2608197'
+  version '10.6,2998'
+  sha256 '226195d7b760fe16bd4e2e803a99e86069aead8852cb07734f01fac4645471de'
 
   # rink.hockeyapp.net/api/2/apps/ was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/7d71478daf6447bda4094e216e97b0cf/app_versions/#{version.after_comma}?format=zip"
