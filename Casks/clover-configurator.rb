d6ace0254244896fef8b85f813c106709e941273
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-22 01:21:18 +0000

    Update clover-configurator from 5.13.0.1 to 5.13.1.0 (#83034)

--- Casks/clover-configurator.rb
+++ Casks/clover-configurator.rb
@@ -1,6 +1,6 @@
 cask 'clover-configurator' do
-  version '5.13.0.1'
-  sha256 'd21f43f5181af2c586207586819ad1ea16ad2bdacd33261072fdcfcc756dffb0'
+  version '5.13.1.0'
+  sha256 'dbfc95c454ebf033da79a46a8fc70b3a3a8d586b111f37591c6e51a486cf4758'
 
   url 'https://mackie100projects.altervista.org/apps/cloverconf/download-new-build.php?version=global'
   appcast 'https://mackie100projects.altervista.org/apps/cloverconf/CCG/update-data-builds.xml'
