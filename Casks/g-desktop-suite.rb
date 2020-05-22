02389f14ded6246cd31dc5f1e9fab70c40828032
Alex Gyujin Kim <alexgkim205@gmail.com>
2020-05-22 17:50:57 -0400

    Upgrade G Desktop Suite to v0.3.0

--- Casks/g-desktop-suite.rb
+++ Casks/g-desktop-suite.rb
@@ -1,11 +1,11 @@
 cask 'g-desktop-suite' do
-  version '0.2.1'
-  sha256 '6220c88551ab2c19180d662387330ab31e44a45cb5dabad51bb86a0bf47904eb'
+  version '0.3.0'
+  sha256 '61ec58243304678ea9c6e224af4fb0654fe671651bc3bc16ab12da497333da44'
 
-  url "https://github.com/alexkim205/G-Desktop-Suite/releases/latest/download/G.Desktop.Suite-#{version}.dmg"
-  appcast 'https://github.com/alexkim205/G-Desktop-Suite/releases.atom'
+  url "https://github.com/alexkim205/g-desktop-suite/releases/latest/download/g-desktop-suite-#{version}.dmg"
+  appcast 'https://github.com/alexkim205/g-desktop-suite/releases.atom'
   name 'G Desktop Suite'
-  homepage 'https://github.com/alexkim205/G-Desktop-Suite'
+  homepage 'https://github.com/alexkim205/g-desktop-suite'
 
   app 'G Desktop Suite.app'
 end
