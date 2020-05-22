da90d89ee40d5eaef782b408bf9eafb72df7ce23
Randall <ran.dall@icloud.com>
2020-05-21 17:53:39 -0700

    Update qownnotes from 20.5.10,b5641-172547 to 20.5.11,b5663-111445

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,6 +1,6 @@
 cask 'qownnotes' do
-  version '20.5.10,b5641-172547'
-  sha256 '000c7faac8614cfad7e00c0a46863a8f72048ee91434b2d499af07d3f84544f2'
+  version '20.5.11,b5663-111445'
+  sha256 '57ca6cb7221835299e8481a29f7ffaa13fb4f1ceddd8a9c9eccb007f3cba9b22'
 
   # github.com/pbek/QOwnNotes/ was verified as official when first introduced to the cask
   url "https://github.com/pbek/QOwnNotes/releases/download/macosx-#{version.after_comma}/QOwnNotes-#{version.before_comma}.dmg"
