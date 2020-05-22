eaf26d4e41497e5216ce3890c83c9823b0d0d1ea
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-22 02:04:40 +0000

    Update upwork from 5.3.3.883,1f817bc1fefd44e7 to 5.4.1.10,974ee44ed5aa421c (#83066)

--- Casks/upwork.rb
+++ Casks/upwork.rb
@@ -1,6 +1,6 @@
 cask 'upwork' do
-  version '5.3.3.883,1f817bc1fefd44e7'
-  sha256 'acb2e43b6bde16b293974c38b41c10763186c92cefd2a69cf486cea9aca30060'
+  version '5.4.1.10,974ee44ed5aa421c'
+  sha256 '63b702d9d2db1611674a72c599c65c505c55ac62cbd88f3a5d212a5c342faba7'
 
   url "https://updates-desktopapp.upwork.com/binaries/v#{version.before_comma.dots_to_underscores}_#{version.after_comma}/Upwork.dmg"
   appcast 'https://www.macupdater.net/cgi-bin/extract_text/extract_text_split_easy.cgi?url=https://updates-desktopapp.upwork.com/binaries/versions-mac.json&splitter_1=Beta&index_1=0'
