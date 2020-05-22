086b5993591519b630816fef2b248df470440a5f
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-22 01:06:26 +0000

    Update trojan-qt5 from 1.1.1 to 1.1.2 (#83025)

--- Casks/trojan-qt5.rb
+++ Casks/trojan-qt5.rb
@@ -1,7 +1,7 @@
 cask 'trojan-qt5' do
   # note: "5" is not a version number, but an intrinsic part of the product name
-  version '1.1.1'
-  sha256 'df2e7708e1d42f16f45c44c19eb1b368c8a79fc06687fc061b15517e67f7f1ed'
+  version '1.1.2'
+  sha256 '7c21c2e5c33d2004f576d24d66494307981111e415d0a430d10322a6cb7e39b2'
 
   url "https://github.com/TheWanderingCoel/Trojan-Qt5/releases/download/v#{version}/Trojan-Qt5-macOS.dmg"
   appcast 'https://github.com/TheWanderingCoel/Trojan-Qt5/releases.atom'
