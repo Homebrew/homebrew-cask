90f287c7c0ba68c5ddb602cd5e9c51b081c2bde1
Vítor Galvão <info@vitorgalvao.com>
2020-05-22 18:21:20 +0100

    navicat-premium.rb: do not return arrays in language (#83040)

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -1,21 +1,20 @@
 cask 'navicat-premium' do
-  version '15.0.15'
+  version '15.0.16'
 
   language 'zh', 'CN' do
-    sha256 'e4663b202cb37d9847328c04c43b6135a7ec5e2a2cabecba096b5c8a2866d5f7'
-    ['.com.cn', 'cs']
+    sha256 '03cacd764a53ab2df02aefa4af45e907a690cc161fdbaf41b034a3ef4a26a238'
+    url "http://download.navicat.com.cn/download/navicat#{version.major_minor.no_dots}_premium_cs.dmg"
+    homepage 'https://www.navicat.com.cn/products/navicat-premium'
   end
 
   language 'en', default: true do
-    sha256 '626609232cce4b67420ed66d68d0a6dd798ec8e40e1783a666f1bf281e875146'
-    ['.com', 'en']
+    sha256 '6da966165fdea8a1f26a33b9eca4de59c1974d64ad901e1090b03a1983a8bf58'
+    url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_premium_en.dmg"
+    appcast 'https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20Premium&appLang=en'
+    homepage 'https://www.navicat.com/products/navicat-premium'
   end
 
-  # download.navicat was verified as official when first introduced to the cask
-  url "http://download.navicat#{language[0]}/download/navicat#{version.major_minor.no_dots}_premium_#{language[1]}.dmg"
-  appcast 'https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20Premium&appLang=en'
   name 'Navicat Premium'
-  homepage "https://www.navicat#{language[0]}/products/navicat-premium"
 
   app 'Navicat Premium.app'
 end
