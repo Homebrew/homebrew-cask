595dd56f40f3be98c854f88c37d6fae2c1d59c2f
Randall <ran.dall@icloud.com>
2020-05-21 18:27:14 -0700

    Update local from 5.4.1,3311 to 5.5.1,3557

--- Casks/local.rb
+++ Casks/local.rb
@@ -1,6 +1,6 @@
 cask 'local' do
-  version '5.4.1,3311'
-  sha256 'ae2c5371d261a673f87a36c6f75ef496167e00032b4ee9a4e8b9b01d0721ccea'
+  version '5.5.1,3557'
+  sha256 '651e6a9cb60a5edadb6647dfbbf9c1ec132393e315970d33cd124b8546b07871'
 
   url "https://cdn.localwp.com/releases-stable/#{version.before_comma}+#{version.after_comma}/local-#{version.before_comma}-mac.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://cdn.localwp.com/stable/latest/mac'
