8557d1cb41a58e707ca158bbffdb614fca81fc3c
Randall <ran.dall@icloud.com>
2020-05-21 18:12:12 -0700

    Update insync from 3.1.7.40811 to 3.1.8.40816

--- Casks/insync.rb
+++ Casks/insync.rb
@@ -1,6 +1,6 @@
 cask 'insync' do
-  version '3.1.7.40811'
-  sha256 '67302966fafde4ffc2eab57d16d9b4d67d9e1c7fda04fb07a6af7882d46d019b'
+  version '3.1.8.40816'
+  sha256 '1a8b59e0909d1f662b80306962403f80ab5e3b058aa659e77b94cb81ef18ffa7'
 
   url "http://s.insynchq.com/builds/Insync-#{version}.dmg"
   appcast 'https://www.insynchq.com/downloads?start=true'
