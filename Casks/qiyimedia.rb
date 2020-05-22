e3888e508fcf486109e631a75d7edfe8682451e5
Brigitte Zmoelnig <brigitte@corecode.io>
2020-05-22 21:30:43 +0200

    Update qiyimedia

--- Casks/qiyimedia.rb
+++ Casks/qiyimedia.rb
@@ -3,7 +3,7 @@ cask 'qiyimedia' do
   sha256 '6f4db1836fe877c58b6ec9835b3c0a6a59e5792fb54e5587ef8ce2edca2160f1'
 
   url 'https://mbdapp.iqiyi.com/j/ot/iQIYIMedia_000.dmg'
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://mbdapp.iqiyi.com/j/ot/iQIYIMedia_000.dmg',
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=http://mbdapp.iqiyi.com/j/ot/iQIYIMedia_000.dmg',
           configuration: version.before_comma
   name '爱奇艺视频'
   homepage 'https://app.iqiyi.com/mac/player/index.html'
