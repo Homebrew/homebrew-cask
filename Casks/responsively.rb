36ecafe7b077ded9a0ef8c5a0603ee5e505c4428
Vítor Galvão <info@vitorgalvao.com>
2020-05-22 18:37:09 +0100

    Update responsively.rb

--- Casks/responsively.rb
+++ Casks/responsively.rb
@@ -2,7 +2,7 @@ cask 'responsively' do
   version '0.1.4'
   sha256 '7a671573de3dd66b8dacc7cb6a94c86c9f1290ae4b66970e265d90f8bcdaa005'
 
-  # github.com/manojVivek/responsively-app was verified as official when first introduced to the cask
+  # github.com/manojVivek/responsively-app/ was verified as official when first introduced to the cask
   url "https://github.com/manojVivek/responsively-app/releases/download/v#{version}/ResponsivelyApp-#{version}.dmg"
   appcast 'https://github.com/manojVivek/responsively-app/releases.atom'
   name 'Responsively'
