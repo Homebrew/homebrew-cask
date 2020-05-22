8322306b9c54618655d461b279d651c545dc28ef
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-22 01:49:11 +0000

    Update aqua-data-studio from 20.5.0-3 to 20.6.0-4 (#83058)

--- Casks/aqua-data-studio.rb
+++ Casks/aqua-data-studio.rb
@@ -1,6 +1,6 @@
 cask 'aqua-data-studio' do
-  version '20.5.0-3'
-  sha256 'b0053af263a1b648764c891ce14704418ba5ec03b896d5566ccf61c5f3c04b12'
+  version '20.6.0-4'
+  sha256 '53e70f183e924514ac27ab64f2c7508de1e510cdb74c1a13cbdc147f131bd535'
 
   url "http://downloads.aquafold.com/v#{version.major_minor}.0/osx/ads-osx-#{version}.tar.gz"
   appcast 'https://www.aquafold.com/aquadatastudio_downloads'
