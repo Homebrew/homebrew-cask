0e31458c84e98a01866ee3957371304a4eec462b
James Chevalier <jchevalier@gmail.com>
2020-05-22 10:24:06 -0400

    add zap trash section

--- Casks/responsively.rb
+++ Casks/responsively.rb
@@ -9,4 +9,9 @@ cask 'responsively' do
   homepage 'https://manojvivek.github.io/responsively-app/'
 
   app 'ResponsivelyApp.app'
+
+  zap trash: [
+    '~/Library/Application Support/ResponsivelyApp',
+    '~/Library/Preferences/app.responsively.plist'
+  ]
 end
