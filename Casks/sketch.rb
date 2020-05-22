5869a8c9a3a8245976ed7794fd1746f03a63af32
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-22 01:21:55 +0000

    Update sketch from 66-97063 to 66.1-97080 (#83033)

--- Casks/sketch.rb
+++ Casks/sketch.rb
@@ -1,6 +1,6 @@
 cask 'sketch' do
-  version '66-97063'
-  sha256 '516c6d049145bfe050fb99d87c74faac56c1103b84731d83eb241eb9720ce084'
+  version '66.1-97080'
+  sha256 '981ee12a6e9d2c932817457e1519988d3767b532f045a59074dc721ab398bed8'
 
   url "https://download.sketchapp.com/sketch-#{version}.zip"
   appcast 'https://download.sketchapp.com/sketch-versions.xml'
