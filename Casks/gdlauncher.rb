9ff195ebec2f8f9eb362816adaf3e50ba23000dd
Randall <ran.dall@icloud.com>
2020-05-22 15:50:23 -0700

    Update gdlauncher from 1.0.4 to 1.0.5

--- Casks/gdlauncher.rb
+++ Casks/gdlauncher.rb
@@ -1,6 +1,6 @@
 cask 'gdlauncher' do
-  version '1.0.4'
-  sha256 '9b72e85aee15c00746f18959702e549ec8cedc32bf5a2ae98145015809ad642e'
+  version '1.0.5'
+  sha256 '93bb5cdf7808be4582b4bf05eec59a0e20225a3fcb9ff7a385dc92fb8174d171'
 
   # github.com/gorilla-devs/GDLauncher/ was verified as official when first introduced to the cask
   url "https://github.com/gorilla-devs/GDLauncher/releases/download/v#{version}/GDLauncher-mac-setup.dmg"
