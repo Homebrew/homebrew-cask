e1d830c7cc5d24c844f3ef96cba67964c4b17821
Randall <ran.dall@icloud.com>
2020-05-21 18:11:14 -0700

    Update cleanshot from 3.2 to 3.2.1

--- Casks/cleanshot.rb
+++ Casks/cleanshot.rb
@@ -1,6 +1,6 @@
 cask 'cleanshot' do
-  version '3.2'
-  sha256 'dba035f183432104400e422ce5e2f7643c4d5e9bf5a3826d03cfbf2f26d37cf1'
+  version '3.2.1'
+  sha256 '39974394ab6ae9130e670484668f319d612e1d500f3ca814b851de79c270f610'
 
   url "https://updates.getcleanshot.com/v#{version.major}/CleanShot-X-#{version}.dmg"
   appcast "https://updates.getcleanshot.com/v#{version.major}/appcast.xml"
