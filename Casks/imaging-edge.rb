bfd16a0bd9ecdeb5bc894e408ced64028a73b396
CoreCode <core-code@users.noreply.github.com>
2020-05-22 22:09:39 +0200

    Update imaging-edge.rb

--- Casks/imaging-edge.rb
+++ Casks/imaging-edge.rb
@@ -1,9 +1,9 @@
 cask 'imaging-edge' do
-  version '1_0_02,2BqD6b8EKL'
+  version '1.0.02,2BqD6b8EKL'
   sha256 'b4eab497dc35974f9d56261c326ef2af538153b27455b93756d980d5786bb379'
 
   # di.update.sony.net/NEX/ was verified as official when first introduced to the cask
-  url "https://di.update.sony.net/NEX/#{version.after_comma}/ied_#{version.before_comma}.dmg"
+  url "https://di.update.sony.net/NEX/#{version.after_comma}/ied_#{version.before_comma.dots_to_underscores}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://support.d-imaging.sony.co.jp/disoft_DL/desktop_DL/mac?fm=en',
           configuration: version.after_comma
   name 'Sony Imaging Edge'
