924793308951777fe8139dcdd90929bd01fe79b4
Willie <willie@accurics.com>
2020-05-21 21:00:15 -0700

    This is the initial commit for the Accurics CLI project.
    The Accurics CLI is a tool to assist with static analysis for
    Infrastructure as Code (IaC) files defined by cloud provisioning
    software such as Terraform.

---
+++ Casks/accurics-cli.rb
@@ -0,0 +1,10 @@
+cask 'accurics-cli' do
+  version '0.0.2'
+  sha256 '35d9e8778b8a6c706436f01e78cc6be8e41be87d5a4349b8715c00b52a2325f3'
+
+  url 'http://downloads.accurics.com/cli/accurics-cli.dmg'
+  name 'Accurics CLI'
+  homepage 'https://www.accurics.com'
+
+  binary 'accurics-cli'
+end
