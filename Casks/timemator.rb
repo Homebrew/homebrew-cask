196e07fa724f2938633f1e1d9cfcee5e6cf2d7a0
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-22 23:09:18 +0000

    Update timemator from 2.5.1 to 2.5.2 (#83095)

--- Casks/timemator.rb
+++ Casks/timemator.rb
@@ -1,6 +1,6 @@
 cask 'timemator' do
-  version '2.5.1'
-  sha256 '8b8f2b3ed391f6976ef6f397b4faa3d1982319679befd56b5d2b9915416fedcb'
+  version '2.5.2'
+  sha256 'f4e6ee06818a24dc6bc1de943518c0aade50a2175729fe985f1b6774607749a9'
 
   # catforce-timemator.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url 'https://catforce-timemator.s3.amazonaws.com/releases/Timemator.dmg'
