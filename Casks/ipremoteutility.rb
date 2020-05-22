0095fa909a7cc47f711ee9aa4bc765e986da8bce
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-22 02:05:52 +0000

    Update ipremoteutility from 1.8.2 to 1.8.4 (#83063)

--- Casks/ipremoteutility.rb
+++ Casks/ipremoteutility.rb
@@ -1,6 +1,6 @@
 cask 'ipremoteutility' do
-  version '1.8.2'
-  sha256 '2c6bdbda882d52484891460f0e84cd5c3b77fa8973e6d15cb6b9c4066bc7b465'
+  version '1.8.4'
+  sha256 '5aebab95b3cc13a7f2c2cf38cf5b9018fa10b813ff7a6b1a6c6ebe9ec6655f05'
 
   url "https://www.flandersscientific.com/ip-remote/release/IPRemoteUtility-#{version}-macOSX.zip"
   appcast 'https://flandersscientific.com/ip-remote/'
