3f7b783a0c84674b74bb4c174fb0d266f4f4dcc3
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-22 01:36:01 +0000

    Update integrity from 9.6.5 to 9.6.9 (#83043)

--- Casks/integrity.rb
+++ Casks/integrity.rb
@@ -1,6 +1,6 @@
 cask 'integrity' do
-  version '9.6.5'
-  sha256 '207e7737c5fdec04eb07e7b392eb7e12c6a46fdd817152787bc5472a5aee5ed6'
+  version '9.6.9'
+  sha256 '92bfe5bb982803988754ecb9700b6f0decd2f93d103b38186bc16e746458d9e3'
 
   url 'https://peacockmedia.software/mac/integrity/integrity.dmg'
   appcast 'https://peacockmedia.software/mac/integrity/version_history.html'
