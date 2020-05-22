528b540214ae686e70f0121f5e95149f900ee495
CoreCode <core-code@users.noreply.github.com>
2020-05-22 22:13:32 +0200

    Update imaging-edge.rb

--- Casks/imaging-edge.rb
+++ Casks/imaging-edge.rb
@@ -7,7 +7,7 @@ cask 'imaging-edge' do
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://support.d-imaging.sony.co.jp/disoft_DL/desktop_DL/mac?fm=en',
           configuration: version.after_comma
   name 'Sony Imaging Edge'
-  homepage 'https://support.d-imaging.sony.co.jp/app/imagingedge/'
+  homepage 'https://imagingedge.sony.net/en-us/ie-desktop.html'
 
   pkg "ied_#{version.before_comma}.pkg"
 
