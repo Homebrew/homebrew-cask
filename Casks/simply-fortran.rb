7d32ffb7a5e584048c2275e45c1a85314ff861ab
suschizu <brigitte@corecode.io>
2020-05-23 17:09:35 +0200

    Update simply-fortran.rb from 3.12.3271 to 3.12.3272 (#83121)

--- Casks/simply-fortran.rb
+++ Casks/simply-fortran.rb
@@ -1,10 +1,12 @@
 cask 'simply-fortran' do
-  version '3.12.3271'
-
   if MacOS.version <= :mojave
+    version '3.12.3271'
+
     sha256 'ebe257a6715fae7869b16a435547679a74b65fb2af8767c32a687262535e7d4c'
     url "https://download.simplyfortran.com/#{version.major_minor}/macos/simplyfortran-#{version}.legacy.dmg"
   else
+    version '3.12.3272'
+
     sha256 '99e461f76eeb8ab39425efbd6a63332bcf136117c5cdad9abaf46e9a94a6a929'
     url "https://download.simplyfortran.com/#{version.major_minor}/macos/simplyfortran-#{version}.dmg"
   end
