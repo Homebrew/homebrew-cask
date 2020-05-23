2534986b9c711740ec5be5e04575d4ac6917d247
suschizu <brigitte@corecode.io>
2020-05-23 18:25:15 +0200

    Update imaging-edge from 2.0.2_1911a,DqbvQuTn2u to 1_0_02,2BqD6b8EKL (#82998)
    Co-authored-by: CoreCode <core-code@users.noreply.github.com>

--- Casks/imaging-edge.rb
+++ Casks/imaging-edge.rb
@@ -1,15 +1,14 @@
 cask 'imaging-edge' do
-  version '2.0.2_1911a,DqbvQuTn2u'
-  sha256 '8ccb39c6f716a388cd825b084fb83c85e73865a3397fd1badd034157d4b64b09'
+  version '1.0.02,2BqD6b8EKL'
+  sha256 'b4eab497dc35974f9d56261c326ef2af538153b27455b93756d980d5786bb379'
 
-  # ids.update.sony.net/IDC/ was verified as official when first introduced to the cask
-  url "http://ids.update.sony.net/IDC/#{version.after_comma}/IE#{version.before_comma.no_dots}.dmg"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://support.d-imaging.sony.co.jp/disoft_DL/imagingedge_DL/mac?fm=en',
+  url "https://di.update.sony.net/NEX/#{version.after_comma}/ied_#{version.before_comma.dots_to_underscores}.dmg"
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://support.d-imaging.sony.co.jp/disoft_DL/desktop_DL/mac?fm=en',
           configuration: version.after_comma
   name 'Sony Imaging Edge'
-  homepage 'https://support.d-imaging.sony.co.jp/app/imagingedge/'
+  homepage 'https://imagingedge.sony.net/en-us/ie-desktop.html'
 
-  pkg 'IE_INST.pkg'
+  pkg "ied_#{version.before_comma.dots_to_underscores}.pkg"
 
   uninstall pkgutil: 'com.sony.ImagingEdgeVer.1.pkg',
             delete:  '/Applications/Imaging Edge'
