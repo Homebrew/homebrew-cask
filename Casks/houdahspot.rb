2069466bec9d405da938b7bfb265dc2cc354c953
Vítor Galvão <info@vitorgalvao.com>
2020-05-23 19:38:09 +0100

    Update houdahspot.rb

--- Casks/houdahspot.rb
+++ Casks/houdahspot.rb
@@ -2,7 +2,7 @@ cask 'houdahspot' do
   version '5.1.1'
   sha256 '38587dee264c7bae0b5370a60c6175d2471e24de4804f592c12864965bfbf32e'
 
-  url "https://www.houdah.com/houdahSpot/download_assets/HoudahSpot#{version}.zip"
+  url "https://dl.houdah.com/houdahSpot/updates/cast#{version.major}_assets/HoudahSpot#{version}.zip"
   appcast "https://www.houdah.com/houdahSpot/updates/cast#{version.major}.xml"
   name 'HoudahSpot'
   homepage 'https://www.houdah.com/houdahSpot/'
