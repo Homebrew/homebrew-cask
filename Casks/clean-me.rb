57043b6029c3e5d25da47b300f8e62a32c4b7d60
Kevin De Koninck <kevin.de.koninck92@gmail.com>
2020-05-23 18:46:47 +0200

    Update clean-me from 1.3.0 to 1.4.1

--- Casks/clean-me.rb
+++ Casks/clean-me.rb
@@ -1,6 +1,6 @@
 cask 'clean-me' do
-  version '1.3.0'
-  sha256 '82e5e1230688b749f2b96d5d2146db5f0471eeb89a784c11bbe039aa7917798d'
+  version '1.4.1'
+  sha256 'a074546909de68b0b378b58f6804a118f40a03077083a5a61a19c588f0841648'
 
   url "https://github.com/Kevin-De-Koninck/Clean-Me/releases/download/v#{version}/Clean.Me.app.zip"
   appcast 'https://github.com/Kevin-De-Koninck/Clean-Me/releases.atom'
