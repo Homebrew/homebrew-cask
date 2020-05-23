f68f4e8fcf57923fd650cb54eb062b547624ca62
konoyono <okada.toshiki623@gmail.com>
2020-05-23 10:55:33 +0900

    add okadash 1.8.0

--- Casks/okadash.rb
+++ Casks/okadash.rb
@@ -1,8 +1,8 @@
 cask 'okadash' do
-  version '1.3.0'
-  sha256 'deb1a9a8bb35df7e5cdbe77eb38807cf11012457ceaadf861569faf0cd0e4e61'
+  version '1.8.0'
+  sha256 '063c70ba0419a3d0f6509ba408fa041589f40a9a10377ea18fa526046df5e6f6'
 
-  url 'https://github.com/konoyono/okadash/releases/download/1.3.0/okadash-1.3.0-mac.zip'
+  url 'https://github.com/konoyono/okadash/releases/download/1.8.0/okadash-1.8.0-mac.zip'
   appcast 'https://github.com/konoyono/okadash/releases/latest'
   name 'okadash'
   homepage 'https://github.com/konoyono/okadash'
