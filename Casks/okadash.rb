cea2c85a48f8d07d9f5b99ed4f6936cfcba27416
konoyono <okada.toshiki623@gmail.com>
2020-05-23 10:59:59 +0900

    fix download url

--- Casks/okadash.rb
+++ Casks/okadash.rb
@@ -2,7 +2,7 @@ cask 'okadash' do
   version '1.8.0'
   sha256 '063c70ba0419a3d0f6509ba408fa041589f40a9a10377ea18fa526046df5e6f6'
 
-  url 'https://github.com/konoyono/okadash/releases/download/#{version}/okadash-#{version}-mac.zip'
+  url 'https://github.com/konoyono/okadash/releases/download/1.8.0/okadash-1.8.0-mac.zip'
   appcast 'https://github.com/konoyono/okadash/releases/latest'
   name 'okadash'
   homepage 'https://github.com/konoyono/okadash'
