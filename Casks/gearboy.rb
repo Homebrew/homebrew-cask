54037987e287d641b6331861b117e1a39100bc68
Randall <ran.dall@icloud.com>
2020-05-24 12:31:44 -0700

    Update gearboy from 3.1.0 to 3.1.1

--- Casks/gearboy.rb
+++ Casks/gearboy.rb
@@ -1,6 +1,6 @@
 cask 'gearboy' do
-  version '3.1.0'
-  sha256 '3a2991acc45fc6234bff69493b66b5ff82b6969627b0e2fcca878de988530371'
+  version '3.1.1'
+  sha256 'ebe4813b32c99dff7c8e72340450b042cfb3c782416575ad7ad6dbe4b32c21c1'
 
   url "https://github.com/drhelius/Gearboy/releases/download/gearboy-#{version}/Gearboy-#{version}-macOS.zip"
   appcast 'https://github.com/drhelius/Gearboy/releases.atom'
