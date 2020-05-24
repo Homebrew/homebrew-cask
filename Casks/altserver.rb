457b9bddbc16b7f7eab3d1b0b3c46e947f280fb6
Khaldoon Mutahar <git@mutahar.me>
2020-05-24 11:23:39 +0300

    Update altserver from 1.3.1 to 1.3.2

--- Casks/altserver.rb
+++ Casks/altserver.rb
@@ -1,6 +1,6 @@
 cask 'altserver' do
-  version '1.3.1'
-  sha256 '40ac78739c0a24bbc8b8da0694c0197c73fc31872a92903391f558ec195048b5'
+  version '1.3.2'
+  sha256 '7c1db4387a700f389d173a8cf56d385673419b4c2227e1de4251101c5f14f153'
 
   # f000.backblazeb2.com/file/ was verified as official when first introduced to the cask
   url "https://f000.backblazeb2.com/file/altstore/altserver/#{version.dots_to_underscores}.zip"
