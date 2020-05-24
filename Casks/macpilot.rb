5ab150ed3354a22868fb18690e1513269bb489a1
suschizu <brigitte@corecode.io>
2020-05-24 20:16:01 +0200

    Update macpilot from 11.1.3 to 11.1.4 (#83165)

--- Casks/macpilot.rb
+++ Casks/macpilot.rb
@@ -1,13 +1,13 @@
 cask 'macpilot' do
-  version '11.1.3'
-  sha256 '1990b04414896ef24767e58cd9b56901460375ee8fb805572ca34da019bcda58'
+  version '11.1.4'
+  sha256 '75fc421d51ebd172ebdf149400fbcd010cf1224529e2e7d3fdf1915e62757f64'
 
   url 'https://www.koingosw.com/products/macpilot/download/macpilot.dmg'
   appcast 'https://www.koingosw.com/postback/versioncheck.php?appname=macpilot&type=sparkle'
   name 'MacPilot'
   homepage 'https://www.koingosw.com/products/macpilot/'
 
-  depends_on macos: '>= :high_sierra'
+  depends_on macos: '>= :mojave'
 
   app 'MacPilot.app'
 
