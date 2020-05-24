5817ddaf4400afc4433440188f0a2a0d9a8cd2c0
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-24 19:49:19 +0000

    Update julia from 1.4.1 to 1.4.2 (#83172)

--- Casks/julia.rb
+++ Casks/julia.rb
@@ -1,6 +1,6 @@
 cask 'julia' do
-  version '1.4.1'
-  sha256 '5e1084e996903842bb2ac5bcde4cd3f670c10e0b15eaa4b812d12bea944704de'
+  version '1.4.2'
+  sha256 '22c4cec8ab28452a283efef29a6c7d2701bfc6e7cc35f6a25366d35151987bee'
 
   url "https://julialang-s3.julialang.org/bin/mac/x64/#{version.major_minor}/julia-#{version}-mac64.dmg"
   appcast 'https://github.com/JuliaLang/julia/releases.atom'
