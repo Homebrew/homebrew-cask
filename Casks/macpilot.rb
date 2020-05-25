768263ac437bfe26e3dc139fd4a0744d35800ca5
CoreCode <core-code@users.noreply.github.com>
2020-05-25 13:23:22 +0200

    Update macpilot.rb

--- Casks/macpilot.rb
+++ Casks/macpilot.rb
@@ -8,9 +8,9 @@ cask 'macpilot' do
     sha256 '75fc421d51ebd172ebdf149400fbcd010cf1224529e2e7d3fdf1915e62757f64'
 
     url 'https://www.koingosw.com/products/macpilot/download/macpilot.dmg'
+    appcast 'https://www.koingosw.com/postback/versioncheck.php?appname=macpilot&type=sparkle'
   end
 
-  appcast 'https://www.koingosw.com/postback/versioncheck.php?appname=macpilot&type=sparkle'
   name 'MacPilot'
   homepage 'https://www.koingosw.com/products/macpilot/'
 
