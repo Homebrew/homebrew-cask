dffaa804912b12b71ecea8f38b57e0edc9b45a9a
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-25 21:08:19 +0000

    Update mailspring from 1.7.7 to 1.7.8 (#83204)

--- Casks/mailspring.rb
+++ Casks/mailspring.rb
@@ -1,6 +1,6 @@
 cask 'mailspring' do
-  version '1.7.7'
-  sha256 'c9a07fd1c27e4d07fd96087e313fdaa7b0e51cb08cbb258d684ff87d4bfa2445'
+  version '1.7.8'
+  sha256 '33ad05420735de09ee2cbc9b57aa3d9acceb0d571c45ddfa09c8a38dbbf81dc9'
 
   # github.com/Foundry376/Mailspring/ was verified as official when first introduced to the cask
   url "https://github.com/Foundry376/Mailspring/releases/download/#{version}/Mailspring.zip"
