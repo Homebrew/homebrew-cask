4effb3b22577918bca6986e589a704e985e50351
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-25 21:40:58 +0000

    Update alfaview from 8.4.0 to 8.5.0 (#83224)

--- Casks/alfaview.rb
+++ Casks/alfaview.rb
@@ -1,6 +1,6 @@
 cask 'alfaview' do
-  version '8.4.0'
-  sha256 'e17c79f6f641acc6fdbf53bbf4351719ec1b02c1d6d3155aa9c9c2af873258a5'
+  version '8.5.0'
+  sha256 'ead929fb5bd30dc1874606e770f66199510e97408a3ddd1c0e6a24cca89b094e'
 
   url "https://assets.alfaview.com/stable/mac/alfaview-mac-production-#{version}.dmg"
   appcast 'https://production-alfaview-assets.alfaview.com/stable/mac/version.info'
