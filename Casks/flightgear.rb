82a7a09e4c2962663c036768c8843d50fe01a25d
CoreCode <core-code@users.noreply.github.com>
2020-05-25 13:19:36 +0200

    Update flightgear.rb

--- Casks/flightgear.rb
+++ Casks/flightgear.rb
@@ -1,7 +1,7 @@
 cask 'flightgear' do
   version '2018.3.2'
   sha256 'dc7a40e19f64b80fdf7171be1516fd877dc7934981268ee72dfb974b53849b9a'
-  
+
   # sourceforge.net/flightgear/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/flightgear/FlightGear-#{version}.dmg"
   appcast 'https://sourceforge.net/projects/flightgear/rss'
