1d012578af6480451fd55ac5668bc33bc30b6546
Randall <ran.dall@icloud.com>
2020-05-26 14:10:52 -0700

    Update 4k-stogram from 3.0.2.3160 to 3.0.3.3190

--- Casks/4k-stogram.rb
+++ Casks/4k-stogram.rb
@@ -1,6 +1,6 @@
 cask '4k-stogram' do
-  version '3.0.2.3160'
-  sha256 '6f6d7381967733cf9d94ae5cbb06bea3bbf929c717e71556175f2c1e5077f31a'
+  version '3.0.3.3190'
+  sha256 '99e7fb94c330b5b92ca46c463852bbcb3fc1499caa78b628f1ac9dad9219a417'
 
   url "https://dl.4kdownload.com/app/4kstogram_#{version.major_minor_patch}.dmg"
   appcast 'https://www.4kdownload.com/download'
