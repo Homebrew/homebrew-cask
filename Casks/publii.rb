53332195bbeecd71181930602d4473bd83e6e435
Brigitte Zmoelnig <brigitte@corecode.io>
2020-05-26 17:06:05 +0200

    Update publii from 0.35.3 to 0.36.0

--- Casks/publii.rb
+++ Casks/publii.rb
@@ -1,8 +1,8 @@
 cask 'publii' do
-  version '0.35.3'
-  sha256 '4cd6883223e9fe6f4aada12dded4657f408670c70ff51c1e3fe3cafc4e5a1f3d'
+  version '0.36.0'
+  sha256 '4765d425ddf94c11ef73176a13a162453bbb74c23180277642209fe8c62c2e58'
 
-  url "https://cdn.getpublii.com/Publii__#{version}.dmg"
+  url "https://cdn.getpublii.com/Publii-#{version}.dmg"
   appcast 'https://getpublii.com/download/'
   name 'Publii'
   homepage 'https://getpublii.com/'
