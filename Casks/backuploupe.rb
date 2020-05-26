ff0c3165e6548b557271074874dc83c29f10afe3
John Benninghoff <jbenninghoff@mac.com>
2020-05-26 08:05:34 -0500

    Update backuploupe from 3.0.1 to 3.0.2 (#83231)

--- Casks/backuploupe.rb
+++ Casks/backuploupe.rb
@@ -1,6 +1,6 @@
 cask 'backuploupe' do
-  version '3.0.1'
-  sha256 '674247a93782b4ad22ccfeb32ebaf48a1d0cd50152a632248a21a131ca192f26'
+  version '3.0.2'
+  sha256 '2862d9f49334011f964f0929ef923bf09dc8ce3c356563b46c72b9d11ac87d03'
 
   url "https://www.soma-zone.com/download/files/BackupLoupe-#{version}.tar.bz2"
   appcast "https://www.soma-zone.com/BackupLoupe/a/appcast-update-#{version.major}.xml"
