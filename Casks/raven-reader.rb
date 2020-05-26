8db1c2d2cacd42de203536418c0079ecf61b6b0f
Meet Godhani <meetgodhani23@gmail.com>
2020-05-25 22:47:38 -0400

    Update raven-reader (#83226)

--- Casks/raven-reader.rb
+++ Casks/raven-reader.rb
@@ -1,6 +1,6 @@
 cask 'raven-reader' do
   version '0.7.9'
-  sha256 '1d6870f170a3e338beede297e88cf8ee09dffcccd952cd9615a1704f1b34f2cc'
+  sha256 'e4c283171f6efe512bd7d9221a4ce4c117a96e0412410b601508164c1599c247'
 
   url "https://download.ravenreader.app/ravenreader/Raven%20Reader-#{version}-mac.zip"
   appcast 'https://download.ravenreader.app/ravenreader/latest-mac.yml'
