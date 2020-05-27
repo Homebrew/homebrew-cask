528011430d415db7cd5c1e0a5b78f139c3fb9d8e
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-27 19:20:13 +0000

    Update cryptomator from 1.5.4 to 1.5.5 (#83313)

--- Casks/cryptomator.rb
+++ Casks/cryptomator.rb
@@ -1,6 +1,6 @@
 cask 'cryptomator' do
-  version '1.5.4'
-  sha256 'f9f2ba4136d9367cdedb509287734734aa9b132f5f48b1d960416ce4c3b6cc44'
+  version '1.5.5'
+  sha256 '4b5bc0194716c4e075efde4ce2d167108bd72b682ce539791d848b6c2f491e86'
 
   # dl.bintray.com/cryptomator/cryptomator/ was verified as official when first introduced to the cask
   url "https://dl.bintray.com/cryptomator/cryptomator/#{version}/Cryptomator-#{version}.dmg"
