34cff1c3cc0fde0521868dca09c62205efabd13a
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2020-05-27 22:21:15 +0900

    Style fix

--- Casks/box-notes.rb
+++ Casks/box-notes.rb
@@ -2,6 +2,7 @@ cask 'box-notes' do
   version :latest
   sha256 :no_check
 
+  # boxcdn.net/ was verified as official when first introduced to the cask
   url 'https://e3.boxcdn.net/box-installers/boxnotes/mac/latest/Box%20Notes.zip'
   name 'Box Notes'
   homepage 'https://www.box.com/resources/downloads/notes'
