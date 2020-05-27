59dc049fa8cff8b96fe0a2dbd4dff49360ceda23
suschizu <brigitte@corecode.io>
2020-05-27 17:32:55 +0200

    Update birdfont.rb

--- Casks/birdfont.rb
+++ Casks/birdfont.rb
@@ -3,8 +3,8 @@ cask 'birdfont' do
     version '2.19.4'
     sha256 '013d9c42c2252b57079453bd27e4c18dbbc09eda55563ff1516fd079c0499f76'
   elsif MacOS.version <= :mojave
-    version '3.32.2'
-    sha256 '858ed49d0a517861c2f5512c80640fe134c9ad9137fb88d1d3962fab5a768b19'
+    version '4.0.1'
+    sha256 '6fb76654291109a3afb6c9e9a3f8c4ea0494f3d371b918a08116accbb14468dc'
   else
     version '4.0.0'
     sha256 'd7918abcc43fad725af18ccec272c559688510eb4ec63854dcbdc29ab5e239ec'
