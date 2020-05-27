ddf93411964643cdae17260591fb40d22fae04c1
Alexander Zigelski <7140800+crunchtime-ali@users.noreply.github.com>
2020-05-27 19:06:42 +0200

    Update Engine Prime from 1.3.4 to 1.5.0

--- Casks/engine-prime.rb
+++ Casks/engine-prime.rb
@@ -1,9 +1,9 @@
 cask 'engine-prime' do
-  version '1.3.4'
-  sha256 '6d2a9f9387951b242a03b842ec00aa7069f263e04b0466885b9c6d4759d0a35a'
+  version '1.5.0'
+  sha256 'ee4de365448da76640758eb70cbb0af06a99ae9ddb14a9d6d7fa8550ac6713d2'
 
   # inmusicbrands.com/ was verified as official when first introduced to the cask
-  url "https://cdn.inmusicbrands.com/denondj/EnginePrime/Engine_Prime_#{version}_Setup.dmg"
+  url "https://cdn.inmusicbrands.com/denondj/EnginePrime/15/Engine_Prime_#{version}_Setup.dmg"
   name 'Engine Prime'
   homepage 'https://www.denondj.com/engineprime'
 
