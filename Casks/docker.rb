7605d51e3455c85d1581b7623bbd5121f3a811b8
Robert Blair <12276457+robertleeblairjr@users.noreply.github.com>
2020-05-27 12:05:55 -0500

    Update docker from 2.3.0.2,45183 to 2.3.0.3,45519 (#83302)

--- Casks/docker.rb
+++ Casks/docker.rb
@@ -3,8 +3,8 @@ cask 'docker' do
     version '18.06.1-ce-mac73,26764'
     sha256 '3429eac38cf0d198039ad6e1adce0016f642cdb914a34c67ce40f069cdb047a5'
   else
-    version '2.3.0.2,45183'
-    sha256 'ed187365b2b9c6de0ddaa1146c86796f0f3a2f6af1cb2693211e600aad233f35'
+    version '2.3.0.3,45519'
+    sha256 'f69bd8f9d0863497819b998d27da4825b65884519f3f6a0e2ce1d4c5cdd26f5e'
   end
 
   url "https://download.docker.com/mac/stable/#{version.after_comma}/Docker.dmg"
