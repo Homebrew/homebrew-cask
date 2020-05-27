6235a1f1b5c0d70618621a971343d703fc687c39
suschizu <brigitte@corecode.io>
2020-05-27 04:35:23 +0200

    Update catlight from 2.31.0 to 2.32.2 (#83236)

--- Casks/catlight.rb
+++ Casks/catlight.rb
@@ -1,9 +1,8 @@
 cask 'catlight' do
-  version '2.31.0'
-  sha256 'd06c54aed7e0c10f51245a039c64cb26e5006db8b8b0e8bb0a588384bdf04c5e'
+  version '2.32.2'
+  sha256 'b3f6a4027813ab8f4549243fd7fab85cdb4a3ff2a2059af4f05b772d12af78fb'
 
-  # de2nac35bcll0.cloudfront.net/ was verified as official when first introduced to the cask
-  url "https://de2nac35bcll0.cloudfront.net/dl/mac/beta/CatLightSetup-#{version}.zip"
+  url "https://download.catlight.io/rel/mac/beta/CatLightSetup-#{version}.zip"
   appcast 'https://catlight.io/downloads'
   name 'catlight'
   homepage 'https://catlight.io/'
