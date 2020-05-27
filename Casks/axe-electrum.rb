1c31ea8fe1174c6e2aef0b605628091914458fd8
-k <slowdive@me.com>
2020-05-26 20:07:57 -0700

    Update Axe Electrum to 3.3.8.8

--- Casks/axe-electrum.rb
+++ Casks/axe-electrum.rb
@@ -1,6 +1,6 @@
 cask 'axe-electrum' do
-  version '3.3.8.7'
-  sha256 'f3f8a7cba3df97447d422c09fecd891a6d50299132f16eaa6c1149d401652947'
+  version '3.3.8.8'
+  sha256 '4caf5256470824c9d5ba61e0c75fa64e1c092bf30017e76db3b4f11c0d136a3f'
 
   url "https://github.com/axerunners/electrum-axe/releases/download/#{version}/axe-electrum-#{version}-macosx.dmg"
   appcast 'https://github.com/axerunners/electrum-axe/releases.atom'
