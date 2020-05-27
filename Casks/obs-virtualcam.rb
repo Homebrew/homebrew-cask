0d8f7919c47320e7628cebed77e58ee125ad15c1
Zhenfu Shi <szf1234@me.com>
2020-05-26 22:50:06 -0400

    Update obs-virtualcam from 1.1.0,0e62cec to 1.1.2,a70ec47 (#83269)

--- Casks/obs-virtualcam.rb
+++ Casks/obs-virtualcam.rb
@@ -1,6 +1,6 @@
 cask 'obs-virtualcam' do
-  version '1.1.0,0e62cec'
-  sha256 'e0ad314f6e635c4e79d78bb75c694263f80ce524e5f084f4fcfba155042e0d51'
+  version '1.1.2,a70ec47'
+  sha256 '38a59fd4a75374c2fe101d796006e210647ae4dae6825b80720e495ed6219ac5'
 
   url "https://github.com/johnboiles/obs-mac-virtualcam/releases/download/v#{version.before_comma}/obs-mac-virtualcam-#{version.after_comma}-v#{version.before_comma}.pkg"
   appcast 'https://github.com/johnboiles/obs-mac-virtualcam/releases.atom'
