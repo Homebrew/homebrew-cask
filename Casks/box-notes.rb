c9d19005714e95b70e558a3f893cab6bda454dbf
Vítor Galvão <info@vitorgalvao.com>
2020-05-27 23:37:22 +0100

    Update box-notes.rb

--- Casks/box-notes.rb
+++ Casks/box-notes.rb
@@ -2,7 +2,7 @@ cask 'box-notes' do
   version '1.4.0'
   sha256 'aa383ad7665b9c0e5975f6440c109a5f18573fc138c3d934a7d903126071459d'
 
-  # boxcdn.net/ was verified as official when first introduced to the cask
+  # e3.boxcdn.net/box-installers/ was verified as official when first introduced to the cask
   url 'https://e3.boxcdn.net/box-installers/boxnotes/mac/latest/Box%20Notes.zip'
   appcast "https://notes.services.box.com/updates/latest?platform=darwin&v=#{version.major}.0.0"
   name 'Box Notes'
