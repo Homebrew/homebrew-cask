e83044da8d653c4179f189d9afebfcb8a4d05857
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-05-27 17:14:39 +0100

    Update mnemosyne from 2.6.1 to 2.7.1

--- Casks/mnemosyne.rb
+++ Casks/mnemosyne.rb
@@ -1,6 +1,6 @@
 cask 'mnemosyne' do
-  version '2.6.1'
-  sha256 '05a63b18d0f110db34898656855f71079f36bfa202f4c978ac7b52ce65463e89'
+  version '2.7.1'
+  sha256 'c7567a0f007cdaf16e8649e0bc24a7f265bfcb77be210f481752a6a51ee47997'
 
   # sourceforge.net/mnemosyne-proj/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/mnemosyne-proj/mnemosyne/mnemosyne-#{version}/Mnemosyne-#{version}.dmg"
