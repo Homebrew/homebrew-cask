da49760bed25f591eb046912e2a0484b55e73a14
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-05-27 20:09:35 +0100

    Update auristor-client from 0.194 to 0.195

--- Casks/auristor-client.rb
+++ Casks/auristor-client.rb
@@ -1,5 +1,5 @@
 cask 'auristor-client' do
-  version '0.194'
+  version '0.195'
 
   if MacOS.version <= :sierra
     sha256 '909f53f46b1dcfe627fd421f3b91f0e62010b35891d4407100780a5c52cc1704'
@@ -11,7 +11,7 @@ cask 'auristor-client' do
     sha256 '47830671ce3b5cb7253b9554701ec81bf61d4b4153c2a882e86774868c5dbad9'
     url "https://www.auristor.com/downloads/auristor/osx/macos-10.14/Auristor-client-#{version}-Mojave.dmg"
   else
-    sha256 '734da14030894e4f866f59f6aa4f4b7094aba3779956211b8699ec16ace3eab1'
+    sha256 'f24a450376bbfe9d6994c34a4bd8fe05af3c5522d93b01a8930d808d88dc5ca1'
     url "https://www.auristor.com/downloads/auristor/osx/macos-10.15/Auristor-client-#{version}-Catalina.dmg"
   end
 
