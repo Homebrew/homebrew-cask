c19ebc8bfe771c017affe8e503e8b42b777e2c60
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-27 20:33:45 +0000

    Update jprofiler from 11.1.2 to 11.1.3 (#83346)

--- Casks/jprofiler.rb
+++ Casks/jprofiler.rb
@@ -1,6 +1,6 @@
 cask 'jprofiler' do
-  version '11.1.2'
-  sha256 '6428b607ad163d9922f2c613dcecc8e48b23693eaed4db1da77ea58a15449e07'
+  version '11.1.3'
+  sha256 'b6a32779220e0bc14ec80d499b2e5a012e885ce6be5369ccd809ceee65e18c18'
 
   url "https://download-gcdn.ej-technologies.com/jprofiler/jprofiler_macos_#{version.dots_to_underscores}.dmg"
   appcast 'https://www.ej-technologies.com/feeds/jprofiler/'
