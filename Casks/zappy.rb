ca2feda4ae47664c343ca67de2a3464b0341047a
Brian Rotich <23168146+brotich@users.noreply.github.com>
2020-05-27 08:48:58 +0300

    Update zappy from 2.4.0 to 2.5.0 (#83276)

--- Casks/zappy.rb
+++ Casks/zappy.rb
@@ -1,6 +1,6 @@
 cask 'zappy' do
-  version '2.4.0'
-  sha256 '41ceeb3bb893585b4e75021ad774640af8618565079c333acb392c520d18865c'
+  version '2.5.0'
+  sha256 'fa7b19b1d0cfdaf22027a81f851b4b130cff068af6e6abb670a44895b10e1f84'
 
   url "https://zappy.zapier.com/releases/zappy-#{version}.zip"
   appcast 'https://zappy.zapier.com/releases/appcast.xml'
