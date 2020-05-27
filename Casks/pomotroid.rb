85ccd41a1f6da8d2815293810a6afb629fbca19d
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-27 02:35:27 +0000

    Update pomotroid from 0.8.0 to 0.9.0 (#83246)

--- Casks/pomotroid.rb
+++ Casks/pomotroid.rb
@@ -1,6 +1,6 @@
 cask 'pomotroid' do
-  version '0.8.0'
-  sha256 '502c177b3d2820001cad64de5dcbf9f7af4cd9206670ee6298abd0820e93c538'
+  version '0.9.0'
+  sha256 'a27a1c5f853f9e8bebbdb62ef539f3360ee68b057f9c6a43eb4e3e75edfee12e'
 
   url "https://github.com/Splode/pomotroid/releases/download/v#{version}/pomotroid-#{version}-macos.dmg"
   appcast 'https://github.com/Splode/pomotroid/releases.atom'
