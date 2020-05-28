72b1dd2d11a0dbb64b12d3ad90741bd609e1f569
Yves Hoppe <info@bytee.net>
2020-05-28 18:13:27 +0200

    Update Empoche from 0.3.3 to 0.4.0

--- Casks/empoche.rb
+++ Casks/empoche.rb
@@ -1,6 +1,6 @@
 cask 'empoche' do
-  version '0.3.3'
-  sha256 'e856b04297e4b63898a5429f5afcf6bd84fb3bbe0866b81230e23c1395acb9f6'
+  version '0.4.0'
+  sha256 '765e793fe3de5bbdc07c998c3e8596ba51bc106d4a1940f233ba3790d781bf7b'
 
   # empoche-desktop.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://empoche-desktop.s3.amazonaws.com/Empoche-#{version}-mac.zip"
