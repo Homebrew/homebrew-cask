1dd480427a95a794779619eb9674d027699b05f7
Randall <ran.dall@icloud.com>
2020-05-28 14:17:23 -0700

    Update firestormos from 6.0.2.56680 to 6.3.9.58205

--- Casks/firestormos.rb
+++ Casks/firestormos.rb
@@ -1,6 +1,6 @@
 cask 'firestormos' do
-  version '6.0.2.56680'
-  sha256 'f8e5fa3b1a72aff67ce91d44f82f536d898a9b4c3a818414799ccb8d288e7e97'
+  version '6.3.9.58205'
+  sha256 '7bb23c680a31259b7d7e8d45899421cbfe16fd1a0b245bfe733fc63e8cd6840f'
 
   url "https://downloads.firestormviewer.org/mac/Phoenix-FirestormOS-Releasex64_#{version.dots_to_underscores}_x86_64.dmg"
   appcast 'https://www.firestormviewer.org/mac-for-open-simulator/'
