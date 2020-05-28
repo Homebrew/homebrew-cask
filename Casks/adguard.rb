a122b42a776973aef7723b949aae3e41aed249ea
Randall <ran.dall@icloud.com>
2020-05-28 14:03:55 -0700

    Update adguard from 2.3.2.710 to 2.4.8.795

--- Casks/adguard.rb
+++ Casks/adguard.rb
@@ -1,6 +1,6 @@
 cask 'adguard' do
-  version '2.3.2.710'
-  sha256 'ce4d082e36b92917ccd4c1e22675cab9fbe3bf32d6ea64b6e5ec5eb82dbc30dd'
+  version '2.4.8.795'
+  sha256 '16768b764f6eb2fe89a97abf2c749077583c06c1ee4e5711500c8f33d3f3db2e'
 
   url "https://static.adguard.com/mac/release/AdGuard-#{version}.dmg"
   appcast 'https://static.adguard.com/mac/adguard-release-appcast.xml'
