ca0575ea2ca326dc78ce57137edc7e0804974b4b
Jake Ireland <jakewilliami@icloud.com>
2020-05-28 13:43:44 +1200

    New cask: League of Legends (PBE)
    Added cask for League of Legends' public beta environment.

---
+++ Casks/league-of-legends-pbe.rb
@@ -0,0 +1,24 @@
+cask 'league-of-legends-pbe' do
+  version :latest
+  sha256 :no_check 
+
+  url "https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/pbe.pbe.zip"
+  name 'League of Legends PBE'
+  homepage 'https://pbesignup.na.leagueoflegends.com/en_US/pbe'
+
+
+  app 'League of Legends (PBE).app'
+
+  zap trash: [
+               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.riotgames.maccontainer.sfl*',
+               '~/Library/Saved Application State/com.riotgames.LeagueofLegends.GameClient.savedState',
+               '~/Library/Saved Application State/com.riotgames.LeagueofLegends.LeagueClientUx.savedState',
+               '~/Library/Preferences/com.riotgames.LeagueofLegends.LeagueClientUxHelper.plist',
+               '~/Library/Caches/com.riotgames.LeagueofLegends.LeagueClient',
+               '/Users/Shared/Riot Games/RiotClientInstalls.json',
+               '/Users/Shared/Riot Games/com.riot.riotclient.checkinstalls-lock',
+             ],
+      rmdir: '~/Documents/League of Legends',
+      rmdir: '~/../Shared/Riot Games/Riot Client.app',
+      rmdir: '~/../Shared/Riot Games/Metadata'
+end
