51d0b56a3e5bab792d0a5108c8a4f9c604d77a89
Aren Patel <aren55555@users.noreply.github.com>
2020-05-28 13:05:06 -0700

    Update Casks/rectangle.rb
    Co-authored-by: Randall <17261190+ran-dall@users.noreply.github.com>

--- Casks/rectangle.rb
+++ Casks/rectangle.rb
@@ -2,7 +2,7 @@ cask 'rectangle' do
   version '0.30'
   sha256 'e0606ff22ca1bdbbb70034a29007c5ced60dfcd8d9294e3846edc4b07b52fe49'
 
-  # github.com was verified as official when first introduced to the cask
+  # github.com/rxhanson/Rectangle/ was verified as official when first introduced to the cask
   url "https://github.com/rxhanson/Rectangle/releases/download/v#{version}/Rectangle#{version}.dmg"
   appcast 'https://www.rectangleapp.com/downloads/updates.xml'
   name 'Rectangle'
