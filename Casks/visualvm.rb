55b2d7fd0cafdf08eebdc6300ab516d62e798b48
ryenus <ryenus@gmail.com>
2020-05-28 09:34:44 +0800

    Update visualvm from 2.0.1 to 2.0.2 (#83352)

--- Casks/visualvm.rb
+++ Casks/visualvm.rb
@@ -1,6 +1,6 @@
 cask 'visualvm' do
-  version '2.0.1'
-  sha256 '59a72c18c655fb0874930962ea4588857cc55bc458069371f6d76cb41f1d6b0e'
+  version '2.0.2'
+  sha256 '20e91893a4f19a1e69b8b0e545935db75674dfd6282861f3a1206dc2689e16c8'
 
   # github.com/oracle/visualvm/ was verified as official when first introduced to the cask
   url "https://github.com/oracle/visualvm/releases/download/#{version}/VisualVM_#{version.no_dots}.dmg"
