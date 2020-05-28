82b4eee0703eee6d7335e73d5e9d647dd56de6d7
Randall <ran.dall@icloud.com>
2020-05-28 14:01:44 -0700

    Update dingtalk from 5.0.15.5 to 5.1.1.2

--- Casks/dingtalk.rb
+++ Casks/dingtalk.rb
@@ -1,6 +1,6 @@
 cask 'dingtalk' do
-  version '5.0.15.5'
-  sha256 '0d5c9c8c5066256300f48df52a47cf5e35e7aef958094126128eccb0cae8da59'
+  version '5.1.1.2'
+  sha256 'ad389bf052aa2225f41bfbbf0c92befd36cd39b3e41ed4a315e35d356495832a'
 
   url "https://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/DingTalk_v#{version}.dmg"
   appcast 'https://im.dingtalk.com/manifest/appcast_en.xml'
