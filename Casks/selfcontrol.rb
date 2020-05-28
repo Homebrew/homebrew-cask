c97c165210459c1bb1778c76840fc6d58ab0850b
Randall <ran.dall@icloud.com>
2020-05-28 13:58:10 -0700

    Update selfcontrol from 3.0 to 3.0.1

--- Casks/selfcontrol.rb
+++ Casks/selfcontrol.rb
@@ -1,6 +1,6 @@
 cask 'selfcontrol' do
-  version '3.0'
-  sha256 'f96708e836d761ede7f965aedcb71dfbe96f6f0ab5670ba7e15f39c3f921fead'
+  version '3.0.1'
+  sha256 'fd4776384101c16855a8ea9339a12ba1bf626beeca0247d58615d1cf4932f518'
 
   url "https://downloads.selfcontrolapp.com/SelfControl-#{version}.zip"
   appcast 'https://selfcontrolapp.com/SelfControlAppcast.xml'
