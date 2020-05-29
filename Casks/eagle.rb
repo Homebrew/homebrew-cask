40dce72079b232fe9aa356d2f5c9546f0d69c5d0
Randall <ran.dall@icloud.com>
2020-05-29 13:44:04 -0700

    Update eagle from 9.6.1 to 9.6.2

--- Casks/eagle.rb
+++ Casks/eagle.rb
@@ -1,6 +1,6 @@
 cask 'eagle' do
-  version '9.6.1'
-  sha256 '8c548955927feefec3bfd9ee28a1e0288ada549e371c01a25a139be91486ace7'
+  version '9.6.2'
+  sha256 'bda7c9a5cc0bb8ae0489ca2d2772e5dd869f466fdd56454d94144e34887d9712'
 
   url "https://trial2.autodesk.com/NET17SWDLD/2017/EGLPRM/ESD/Autodesk_EAGLE_#{version}_English_Mac_64bit.pkg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.autodesk.com/eagle-download-mac'
