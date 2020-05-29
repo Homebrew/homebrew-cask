3c25d7ef763e331145119f127677ba592434bb8b
suschizu <brigitte@corecode.io>
2020-05-29 15:42:50 +0200

    Update silentknight.rb

--- Casks/silentknight.rb
+++ Casks/silentknight.rb
@@ -1,9 +1,9 @@
 cask 'silentknight' do
-  version '1.6'
-  sha256 'b0584a3adacc0911305e15953ec7a018df08db82606d765fc7acced3d03577a9'
+  version '1.7'
+  sha256 '23b0c313244c4e40954d648a313cd3a9333db567b6e6b2aca591b3c610923cf4'
 
   # eclecticlightdotcom.files.wordpress.com/ was verified as official when first introduced to the cask
-  url "https://eclecticlightdotcom.files.wordpress.com/2020/01/silentknight#{version.no_dots}.zip"
+  url "https://eclecticlightdotcom.files.wordpress.com/2020/05/silentknight#{version.no_dots}.zip"
   appcast 'https://eclecticlight.co/lockrattler-systhist/'
   name 'SilentKnight'
   homepage 'https://eclecticlight.co/lockrattler-systhist/'
