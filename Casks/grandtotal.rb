8caa9d017aef4be1981357f1cd5247fe17f1551d
Randall <ran.dall@icloud.com>
2020-05-29 13:31:54 -0700

    Update grandtotal from 6.1.2 to 6.1.3

--- Casks/grandtotal.rb
+++ Casks/grandtotal.rb
@@ -1,6 +1,6 @@
 cask 'grandtotal' do
-  version '6.1.2'
-  sha256 'fe4641953b9c70f222bd68a70b3bb65f3348697a02b005dc33fe1e562298ae60'
+  version '6.1.3'
+  sha256 '7b231677f4514a9c62b8c061291a0f39dbecd6f005cded8d3ceab0ec518a1aaf'
 
   url "https://mediaatelier.com/GrandTotal#{version.major}/GrandTotal_#{version}.zip"
   appcast "https://mediaatelier.com/GrandTotal#{version.major}/feed.php"
