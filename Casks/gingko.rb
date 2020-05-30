9b67319c9ee78b55c07032992b6215f4948426df
Randall <ran.dall@icloud.com>
2020-05-30 14:50:46 -0700

    Update gingko from 2.4.13 to 2.4.14

--- Casks/gingko.rb
+++ Casks/gingko.rb
@@ -1,6 +1,6 @@
 cask 'gingko' do
-  version '2.4.13'
-  sha256 '46fb75be0b6b4cc1b6d8de657ded195996e181cd4063df25634be5399ff6b9a3'
+  version '2.4.14'
+  sha256 'a37bb0be8116fabe0e6aa32329353a29f2ed3aa814ddc6ec8c82bdb7164fba45'
 
   # github.com/gingko/client/ was verified as official when first introduced to the cask
   url "https://github.com/gingko/client/releases/download/v#{version}/Gingko-#{version}-mac.zip"
