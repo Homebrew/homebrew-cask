c9d5940916799f21edad1534a5200ecb28a9d4bd
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-30 15:18:48 -0700

    Update retroarch from 1.8.7 to 1.8.8 (#83498)

--- Casks/retroarch.rb
+++ Casks/retroarch.rb
@@ -1,6 +1,6 @@
 cask 'retroarch' do
-  version '1.8.7'
-  sha256 '815d4d24ece7e0a56700180ea83d95374787169006643df9cdc2b8fc69ba6c03'
+  version '1.8.8'
+  sha256 'e649be3ef8d9478ac5f58f78a6959821f15c9691e011efb1e4ef341b6ac8b29f'
 
   url "https://buildbot.libretro.com/stable/#{version}/apple/osx/x86_64/RetroArch.dmg"
   appcast 'https://buildbot.libretro.com/stable/'
