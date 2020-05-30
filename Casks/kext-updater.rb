50b12c8f3ac8ce6a40f516b7eb9020f48a2ddcd3
i0ntempest <szf1234@me.com>
2020-05-30 15:42:30 -0400

    Update kext-updater from 3.5.0 to 3.5.1

--- Casks/kext-updater.rb
+++ Casks/kext-updater.rb
@@ -1,6 +1,6 @@
 cask 'kext-updater' do
-  version '3.5.0'
-  sha256 'fd5d2898229b5c14927e056ac6b6959deec2314ce27fb28d309827f37f48be20'
+  version '3.5.1'
+  sha256 '1898493e29041781b3c2f88bc19477ed2298df77b2c73b1143854de05ebab3aa'
 
   url 'https://update.kextupdater.de/kextupdater/kextupdaterng.zip'
   appcast 'https://update.kextupdater.de/kextupdater/appcastng.xml'
