775f8989221d7d07bc7dd65586fa09a2c194c902
suschizu <brigitte@corecode.io>
2020-05-30 22:03:55 +0200

    Update knotes.rb

--- Casks/knotes.rb
+++ Casks/knotes.rb
@@ -2,7 +2,7 @@ cask 'knotes' do
   version '2.5.0'
   sha256 '0f5d0da7eb1e1460da300931d4eabe06e3506b5e813ae408782717f9760a8ee6'
 
-  # knotes2.s3-ap-northeast-1.amazonaws.com/ was verified as official when first introduced to the cask
+  # knotes#{version.major}.s3-ap-northeast-1.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://knotes#{version.major}.s3-ap-northeast-1.amazonaws.com/download/mac/Knotes-#{version}.dmg"
   appcast 'https://knotesapp.com/'
   name 'Knotes'
