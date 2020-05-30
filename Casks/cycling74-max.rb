5806047bbbf51961790da653bb10ad9195345943
Ethan Bailey <ebailey256@gmail.com>
2020-05-29 22:06:37 -0400

    Update cycling74-max

--- Casks/cycling74-max.rb
+++ Casks/cycling74-max.rb
@@ -1,6 +1,6 @@
 cask 'cycling74-max' do
   version '8.1.4_200527'
-  sha256 '92b4b5e8754b7b30b907f3e659a57bfe5cc9700b405b65267353a02f4ec27494'
+  sha256 '86f4a4e1b7da1871aee5994c625b4249a64e21feb5e5d786ec0edbc1e2f1bc20'
 
   # akiaj5esl75o5wbdcv2a-maxmspjitter.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://akiaj5esl75o5wbdcv2a-maxmspjitter.s3.amazonaws.com/Max#{version.no_dots}.dmg"
