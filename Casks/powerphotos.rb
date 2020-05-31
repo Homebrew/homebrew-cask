1c693ea7e60a605c855338ffb0b0534ad74132ac
suschizu <brigitte@corecode.io>
2020-05-31 15:01:57 +0200

    Update powerphotos.rb

--- Casks/powerphotos.rb
+++ Casks/powerphotos.rb
@@ -16,8 +16,8 @@ cask 'powerphotos' do
     sha256 'e7c7d5970b734827a5f112029491d2d97f9a6bb318f457893905718bea6b595a'
     url "https://www.fatcatsoftware.com/powerphotos/PowerPhotos_#{version.no_dots}.zip"
   else
-    version '1.8.2'
-    sha256 'ee755f6477f759d43a75dcb7c864aeeebbec29a856d3d7b24b1cd2eb1afa4553'
+    version '1.8.3'
+    sha256 '8982971e979399beeaea8341ebc8b8591c873505ea8f46f4a95ec9d00ebab20a'
     url 'https://www.fatcatsoftware.com/powerphotos/PowerPhotos.zip'
   end
 
