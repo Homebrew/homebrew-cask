8ba78e20e736fa674f70a49d6c1a7b13e7618c93
Randall <ran.dall@icloud.com>
2020-05-31 10:14:32 -0700

    Update keymanager from 3.13.4 to 3.13.10

--- Casks/keymanager.rb
+++ Casks/keymanager.rb
@@ -1,6 +1,6 @@
 cask 'keymanager' do
-  version '3.13.4'
-  sha256 'f31ef3f3082453337a89e33207c83ee7efd517b1ab6f95eae9d50d9773462547'
+  version '3.13.10'
+  sha256 'bd09c00fc86d0421f5ae6dd7e52202fd173552df17f9d90726ba8749095a710a'
 
   # keymanager.trustasia.com/ was verified as official when first introduced to the cask
   url "https://keymanager.trustasia.com/release/KeyManager-#{version}.dmg"
