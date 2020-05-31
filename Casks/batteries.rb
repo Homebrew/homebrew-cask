6718180b450bdbaebf44c955b7109a673decddbe
ivanxfjiang(姜希凡) <ivanxfjiang@tencent.com>
2020-05-31 15:47:08 +0800

    Add Batteries v2.0.1

---
+++ Casks/batteries.rb
@@ -0,0 +1,12 @@
+cask 'batteries' do
+  version '2.0.1'
+  sha256 '9b4ec9875d6977392c182a9032ebead5857713bf9f78f7e78df29a7d8c592c1f'
+
+  # github.com/ronyfadel/BatteriesReleases was verified as official when first introduced to the cask
+  url "https://github.com/ronyfadel/BatteriesReleases/releases/download/v#{version}/Batteries.dmg"
+  appcast 'https://github.com/ronyfadel/BatteriesReleases/releases.atom'
+  name 'Batteries'
+  homepage 'https://www.fadel.io/batteries/'
+
+  app 'Batteries.app'
+end
