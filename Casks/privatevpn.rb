5aa0d09f0e1dfaf8c5e0622a8841c05cd99005f1
Francisco Pereira <chico.lopes@gmail.com>
2020-05-31 23:24:05 +0200

    Add zap stanza
    Co-authored-by: Randall <17261190+ran-dall@users.noreply.github.com>

--- Casks/privatevpn.rb
+++ Casks/privatevpn.rb
@@ -10,4 +10,13 @@ cask 'privatevpn' do
   depends_on macos: '>= :el_capitan'
 
   app 'PrivateVPN.app'
+
+  zap trash: [
+               '/Library/LaunchDaemons/com.privat.vpn.helper.plist',
+               '/Library/PrivilegedHelperTools/com.privat.vpn.helper',
+               '~/Library/Application Support/com.privat.vpn',
+               '~/Library/Caches/com.privat.vpn',
+               '~/Library/Cookies/com.privat.vpn.binarycookie',
+               '~/Library/Preferences/com.privat.vpn.plist',
+             ]
 end
