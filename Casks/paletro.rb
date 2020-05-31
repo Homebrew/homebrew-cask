3c2491f17db10be88eda01a59162e8c19436c1ab
AbnerLee <ilchenearly@gmail.com>
2020-05-31 14:32:14 +0800

    Fix styles of Paletro cask

--- Casks/paletro.rb
+++ Casks/paletro.rb
@@ -1,7 +1,7 @@
 cask 'paletro' do
   version '1.1.0'
   sha256 '8e36dfa44c9c68674cac45880a812192fc69400e018dded201c53e0593f9fffe'
-  
+
   url "https://appmakes.io/paletro/download/Paletro-#{version}.dmg"
   appcast 'https://appmakes.io/paletro/update.xml'
   name 'Paletro'
