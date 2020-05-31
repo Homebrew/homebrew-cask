f53d5efde6f660ac4f73b8eb05d926a02e3ec4a2
Randall <17261190+ran-dall@users.noreply.github.com>
2020-05-31 10:18:45 -0700

    Update moneywiz from 3.7.7 to 3.7.8 (#83530)

--- Casks/moneywiz.rb
+++ Casks/moneywiz.rb
@@ -1,6 +1,6 @@
 cask 'moneywiz' do
-  version '3.7.7'
-  sha256 'f3ee9881fa4d8dd51a63a63796e474ca7af266081bb43e678311135590fbd90d'
+  version '3.7.8'
+  sha256 '5efeebb2c88a16c3d6542b01e896c19c42a82ce701d9ad28c51fffee1c444e3d'
 
   url 'https://mac.wiz.money/MoneyWiz_Mac_Free.dmg'
   appcast 'https://macdistribution.wiz.money/version_info_free.xml'
