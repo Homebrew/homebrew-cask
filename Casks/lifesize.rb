ef1d0c4ed11dba7d18ee6aaf6ef80ba24f65218f
suschizu <brigitte@corecode.io>
2020-05-31 15:33:28 +0200

    Update lifesize.rb (#83519)

--- Casks/lifesize.rb
+++ Casks/lifesize.rb
@@ -1,6 +1,6 @@
 cask 'lifesize' do
-  version '2.210.2687'
-  sha256 '76827687e088f1f0b24bc1abb02e49ad8e17d39086fe245adf211d6c8a441440'
+  version '2.210.2697'
+  sha256 'ad39f87845e320bbc5a8cbd1edcd55118aa37719b4703883a55bb8b1676f30a6'
 
   # download.lifesizecloud.com/ was verified as official when first introduced to the cask
   url "https://download.lifesizecloud.com/Lifesize-#{version}.dmg"
