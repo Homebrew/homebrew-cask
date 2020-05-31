59e46bc1c4f86f6bf0f6bd16937a76275499595f
ivanxfjiang(姜希凡) <ivanxfjiang@tencent.com>
2020-05-31 16:05:57 +0800

    Add Tencent Meeting v1.5.0.461

---
+++ Casks/tencent-meeting.rb
@@ -0,0 +1,12 @@
+cask 'tencent-meeting' do
+  version '1.5.0.461'
+  sha256 'fc3191d0e3e9ace3891b9c87fc2c12d13e3cf89b217107669a045d74c49e1dbf'
+
+  # qq.com was verified as official when first introduced to the cask
+  url "https://down.qq.com/download/TencentMeeting_0300000000_#{version}.publish.dmg"
+  name 'Tencent Meeting'
+  name '腾讯会议'
+  homepage 'https://meeting.tencent.com/'
+
+  app 'TencentMeeting.app'
+end
