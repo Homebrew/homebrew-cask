1fcf5f8630adb5e47c8e8725ac515d0c1e6cb757
suschizu <brigitte@corecode.io>
2020-05-31 15:09:35 +0200

    Update 4k-youtube-to-mp3.rb

--- Casks/4k-youtube-to-mp3.rb
+++ Casks/4k-youtube-to-mp3.rb
@@ -1,7 +1,7 @@
 cask '4k-youtube-to-mp3' do
   # note: "3" is not a version number, but an intrinsic part of the product name
-  version '3.12.1.3660'
-  sha256 'a32684fb073171841550d7da0e2e5b2e872fe9c255c6e8712ba9983e3cbe1bfc'
+  version '3.12.2.3670'
+  sha256 '107ebaad71c5189b04c0aae705c08dd043fd8bd770d3aeef5c31202a5ac005de'
 
   url "https://dl.4kdownload.com/app/4kyoutubetomp3_#{version.major_minor_patch}.dmg"
   appcast 'https://www.4kdownload.com/download'
