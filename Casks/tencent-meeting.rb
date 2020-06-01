31793e96607115ace8a28190b4135877b69a168b
Ivan Jiang <ivanxfjiang@tencent.com>
2020-06-01 11:23:02 +0800

    Update tencent-meeting from 1.5.0.461 to 1.5.5.418 (#83540)

--- Casks/tencent-meeting.rb
+++ Casks/tencent-meeting.rb
@@ -1,12 +1,14 @@
 cask 'tencent-meeting' do
-  version '1.5.0.461'
-  sha256 'fc3191d0e3e9ace3891b9c87fc2c12d13e3cf89b217107669a045d74c49e1dbf'
+  version '1.5.5.418'
+  sha256 '33d222cbe010b3ced1bc3589ff756fa276df1ecca560d27b78d79b3d4f19f4d1'
 
   # qq.com was verified as official when first introduced to the cask
   url "https://down.qq.com/download/TencentMeeting_0300000000_#{version}.publish.dmg"
   name 'Tencent Meeting'
   name '腾讯会议'
   homepage 'https://meeting.tencent.com/'
+
+  auto_updates true
 
   app 'TencentMeeting.app'
 
