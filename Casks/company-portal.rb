7b0add23d57b09677c3dc0df11feaf67165c581e
David G Rosenberg <targendaz2@me.com>
2020-06-01 17:27:21 -0400

    Add Company Portal.app Latest

---
+++ Casks/company-portal.rb
@@ -0,0 +1,26 @@
+cask 'company-portal' do
+  version '2.5.200500'
+  sha256 'bbed1fdaaad2d47d39177ef38b14c99382f575e48f6d52e68647bb56bbb5e25c'
+
+  url "https://officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/CompanyPortal_#{version}-Installer.pkg"
+  name 'Company Portal'
+  homepage 'https://docs.microsoft.com/en-us/mem/intune/user-help/enroll-your-device-in-intune-macos-cp'
+
+  auto_updates true
+  depends_on macos: '>= :mojave'
+
+  pkg "CompanyPortal_#{version}-Installer.pkg"
+
+  uninstall pkgutil:   [
+                         'com.microsoft.package.Microsoft_AutoUpdate.app',
+                         'com.microsoft.CompanyPortal',
+                       ],
+            delete:    [
+                         '/Applications/Company Portal.app',
+                         '/Library/PrivilegedHelperTools/com.microsoft.autoupdate.helper',
+                       ],
+            launchctl: [
+                         'com.microsoft.autoupdate.helper',
+                         'com.microsoft.update.agent',
+                       ]
+end
