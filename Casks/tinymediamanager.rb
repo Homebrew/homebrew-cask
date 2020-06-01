fdfeea361255b61474b00551244a150ac2749153
Adrien <berthet.a@gmail.com>
2020-06-01 08:34:04 +0200

    Update tinymediamanager from 3.1.5 to 3.1.6 (#83544)

--- Casks/tinymediamanager.rb
+++ Casks/tinymediamanager.rb
@@ -1,6 +1,6 @@
 cask 'tinymediamanager' do
-  version '3.1.5'
-  sha256 '5b58056055ef98c99892224621cd0ecfb0dc278b90487586bdff8a9ce95acaff'
+  version '3.1.6'
+  sha256 '231b8a770d800ebd3aceaca5ae6329de7fece11563efab15fbd74a195393d877'
 
   url "https://release.tinymediamanager.org/v#{version.major}/dist/tmm_#{version}_mac.zip"
   appcast 'https://release.tinymediamanager.org/'
