48987d4e4b40042d60409d656e5079a50888f4ab
Vítor Galvão <info@vitorgalvao.com>
2020-06-01 14:59:20 +0100

    Update tcl.rb

--- Casks/tcl.rb
+++ Casks/tcl.rb
@@ -2,8 +2,7 @@ cask 'tcl' do
   version '8.6.9.0,8.6.9.8609.2'
   sha256 'bd462de2a09198b95c94ac727264f36aebee83f0ac9aab7260150c4f5e0f5c3b'
 
-  url "https://downloads.activestate.com/ActiveTcl/releases/#{version.before_comma}/ActiveTcl-#{version.after_comma}-macosx10.9-x86_64.pkg",
-      user_agent: :fake
+  url "https://downloads.activestate.com/ActiveTcl/releases/#{version.before_comma}/ActiveTcl-#{version.after_comma}-macosx10.9-x86_64.pkg"
   appcast 'https://www.activestate.com/products/tcl/downloads/',
           configuration: version.major_minor
   name 'ActiveTcl'
