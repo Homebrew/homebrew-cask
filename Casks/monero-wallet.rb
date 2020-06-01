fe8bccfa881987b88129b77f1a086e9263b76cde
Viktor Szakats <commit@vsz.me>
2020-06-01 12:25:11 +0000

    monero-wallet: fix/improve url in test

--- Casks/monero-wallet.rb
+++ Casks/monero-wallet.rb
@@ -3,7 +3,7 @@ cask 'monero-wallet' do
   sha256 '96effab969c1d4f715d323835d98b202e33f75b835ed4587bb49a76fb288bc6d'
 
   url "https://downloads.getmonero.org/gui/monero-gui-mac-x64-v#{version}.tar.bz2"
-  appcast 'http://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://downloads.getmonero.org/gui/mac64'
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://downloads.getmonero.org/gui/mac64'
   name 'Monero Wallet'
   homepage 'https://getmonero.org/'
 
