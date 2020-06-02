594895bc464f9ec1bac99ec0478eb96fbcd82dfc
Randall <ran.dall@icloud.com>
2020-06-02 13:23:43 -0700

    Update obsidian from 0.6.4 to 0.6.5

--- Casks/obsidian.rb
+++ Casks/obsidian.rb
@@ -1,6 +1,6 @@
 cask 'obsidian' do
-  version '0.6.4'
-  sha256 '1da5fffd869201ba044137ebfaf0f0659982a653f3f5b75d6634c410f8eb6b8c'
+  version '0.6.5'
+  sha256 'ff50dd63c01efc7d59cd48d5a387be813bbebead1b79f696943bfa7a54f1471b'
 
   # github.com/obsidianmd/ was verified as official when first introduced to the cask
   url "https://github.com/obsidianmd/obsidian-releases/releases/download/v#{version}/Obsidian-#{version}.dmg"
