642769ef2df253d07406747c0d4e28f8fa575be3
Randall <ran.dall@icloud.com>
2020-06-02 12:35:26 -0700

    Update jubler from 7.0.0 to 7.0.3

--- Casks/jubler.rb
+++ Casks/jubler.rb
@@ -1,6 +1,6 @@
 cask 'jubler' do
-  version '7.0.0'
-  sha256 '9d535ff444638804719d04010018c2b4ac620a7004866e5a9b7c3e5136fb798a'
+  version '7.0.3'
+  sha256 '1165706840cdff7ed729d3737e9f3540e5d0216c0ef4fe506e80bbddfdce572e'
 
   # github.com/teras/Jubler/ was verified as official when first introduced to the cask
   url "https://github.com/teras/Jubler/releases/download/v#{version}/Jubler-#{version}.dmg"
