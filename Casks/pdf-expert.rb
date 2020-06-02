d9b60f5457fa804b92e447f2749ed094ce134322
Luca Bomben <luca.bomben@gmail.com>
2020-06-02 12:58:17 +0200

    Update pdf-expert from 2.5.6,682 to 2.5.7,691

--- Casks/pdf-expert.rb
+++ Casks/pdf-expert.rb
@@ -1,6 +1,6 @@
 cask 'pdf-expert' do
-  version '2.5.6,682'
-  sha256 'a13723326ac6793812bc1bbc986b2cecbd63177833e41e84d06bc42e9f3a1fb9'
+  version '2.5.7,691'
+  sha256 'c69c9209da33dd0c47ecb7aa39f0aaf8f48374f2b38b98170f7a2f55b5c2d61b'
 
   # d1ke680phyeohy.cloudfront.net/ was verified as official when first introduced to the cask
   url "https://d1ke680phyeohy.cloudfront.net/versions/#{version.after_comma}/PDFExpert.dmg"
