89b8857a130a8f778521d14869bd62860179d43e
Reno <25192197+singularitti@users.noreply.github.com>
2020-06-02 08:19:02 -0400

    Update permute from 3.4.9,2262 to 3.4.10,2266 (#83588)

--- Casks/permute.rb
+++ Casks/permute.rb
@@ -1,6 +1,6 @@
 cask 'permute' do
-  version '3.4.9,2262'
-  sha256 'e5d42fbd65fd5a1320d7b5a19619601f26a17bd4f9ed3f689bde8035679f0349'
+  version '3.4.10,2266'
+  sha256 '1b87cab903f53d4afe56dbfe52a826a8c71398098b419950e9c46cc8427b7ddb'
 
   url "https://trial.charliemonroe.net/permute/v#{version.major}/Permute_#{version.major}_#{version.after_comma}.dmg"
   appcast "https://trial.charliemonroe.net/permute/updates_#{version.major}.xml"
