1a38abfc71e0982f9da9324439f9e90769298c9f
Randall <ran.dall@icloud.com>
2020-06-02 13:21:25 -0700

    Update tip from 1.3.0 to 2.0.0

--- Casks/tip.rb
+++ Casks/tip.rb
@@ -1,6 +1,6 @@
 cask 'tip' do
-  version '1.3.0'
-  sha256 'db26cdbb41c3834d291d3e08d7d13ebf0c4d89765afdf5b91c23f8c319dd76db'
+  version '2.0.0'
+  sha256 '4d986a461d1b24bb5776fb49063b9a1891939f336b306a6bc75f58d0a4e98bcb'
 
   url "https://github.com/tanin47/tip/releases/download/v#{version}/Tip.zip"
   appcast 'https://github.com/tanin47/tip/releases.atom'
