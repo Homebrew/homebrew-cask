b800d265f6662c73a2aac75df922dc94b3a29358
Cláudio de Oliveira Pereira <pclaudio.ans@gmail.com>
2020-06-02 15:52:38 -0400

    Update springtoolsuite from 4.6.1.RELEASE,4.15.0 to 4.6.2.RELEASE,4.15.0 (#83599)

--- Casks/springtoolsuite.rb
+++ Casks/springtoolsuite.rb
@@ -1,6 +1,6 @@
 cask 'springtoolsuite' do
-  version '4.6.1.RELEASE,4.15.0'
-  sha256 '40d60284b57c02c639737dedc47122db1603f770144b973e3b39003ecfaa9ca0'
+  version '4.6.2.RELEASE,4.15.0'
+  sha256 '9f093c3d99b877953eb683f1ff45f9510452fcdafe4a8b5b745b69f93fd93025'
 
   # download.springsource.com/release/ was verified as official when first introduced to the cask
   url "https://download.springsource.com/release/STS#{version.major}/#{version.before_comma}/dist/e#{version.after_comma.major_minor}/spring-tool-suite-#{version.major}-#{version.before_comma}-e#{version.after_comma}-macosx.cocoa.x86_64.dmg"
