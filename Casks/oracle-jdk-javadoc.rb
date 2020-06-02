cdb62e382badf896e2f2080e1977271a1d13ed95
Cláudio de Oliveira Pereira <pclaudio.ans@gmail.com>
2020-06-02 19:16:14 -0400

    Update oracle-jdk-javadoc to correct the artifact installation path (#83651)

--- Casks/oracle-jdk-javadoc.rb
+++ Casks/oracle-jdk-javadoc.rb
@@ -9,7 +9,7 @@ cask 'oracle-jdk-javadoc' do
   name 'Oracle Java Standard Edition Development Kit Documentation'
   homepage 'https://www.oracle.com/technetwork/java/javase/documentation/index.html'
 
-  artifact 'docs', target: "/Library/Java/JavaVirtualMachines/jdk-#{version.major}.jdk/Contents/Home/docs"
+  artifact 'docs', target: "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk/Contents/Home/docs"
 
   uninstall rmdir: "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk"
 
