3b4ef86b40a04dc1d807126d9aec45e14c28cabb
Zhenfu Shi <szf1234@me.com>
2020-06-02 14:33:33 -0400

    Update imazing from 2.11.5.13649 to 2.11.6.13688 (#83594)

--- Casks/imazing.rb
+++ Casks/imazing.rb
@@ -1,6 +1,6 @@
 cask 'imazing' do
-  version '2.11.5.13649'
-  sha256 '408e1da43b6dd3326109c396c03a3ca31eb4ce32c479e76a7ec07555de5d1e0a'
+  version '2.11.6.13688'
+  sha256 '3692a783318ac3c98b70c9859e37b75890ba847fbdf4a453e3c5c9540e381e7d'
 
   url "https://downloads.imazing.com/mac/iMazing/#{version}/iMazing_#{version}.dmg"
   appcast "https://downloads.imazing.com/com.DigiDNA.iMazing#{version.major}Mac.xml"
