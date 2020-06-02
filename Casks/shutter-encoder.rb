eb201404c8bb82de54d20b15e8018009d90321b6
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-02 00:36:51 +0000

    Update shutter-encoder from 13.7 to 13.8 (#83574)

--- Casks/shutter-encoder.rb
+++ Casks/shutter-encoder.rb
@@ -1,6 +1,6 @@
 cask 'shutter-encoder' do
-  version '13.7'
-  sha256 '154d3fcaa1977243f956672bc5f7d1b0a99e4202326713a5501b5ffcf81ff89f'
+  version '13.8'
+  sha256 '96e4970477763130a2a4859b95f5f42552a772aad2ec42627af780808081d566'
 
   url "https://www.shutterencoder.com/Shutter%20Encoder%20(MAC%20Version%20#{version}).zip"
   appcast 'https://www.shutterencoder.com/changelog.txt'
