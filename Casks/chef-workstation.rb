885d9c34365fa72d4afe6282e7ba45b95567df97
tyler-ball <tball@chef.io>
2020-06-03 13:00:12 -0700

    Updating Chef Workstation from 0.18.3 to 20.6.62
    Signed-off-by: tyler-ball <tball@chef.io>

--- Casks/chef-workstation.rb
+++ Casks/chef-workstation.rb
@@ -1,8 +1,8 @@
 cask 'chef-workstation' do
-  version '0.18.3'
-  sha256 '53312b80cfd0bbc29b3be73cfc0a6c438fd1873c60c0752ac3b636c62fdcbab3'
+  version '20.6.62'
+  sha256 'd13d6dcdb69c0c7843e2e0f07765138d6eb17aa47854b3b721ecc97e2e7911e7'
 
-  url "https://packages.chef.io/files/stable/chef-workstation/#{version}/mac_os_x/10.13/chef-workstation-#{version}-1.dmg"
+  url "https://packages.chef.io/files/stable/chef-workstation/#{version}/mac_os_x/10.14/chef-workstation-#{version}-1.dmg"
   appcast 'https://omnitruck.chef.io/stable/chef-workstation/metadata?p=mac_os_x&pv=10.14&m=x86_64&v=latest'
   name 'Chef Workstation'
   homepage 'https://docs.chef.io/workstation/'
