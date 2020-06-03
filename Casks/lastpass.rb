98a1445cd9233955ed016a9669d5c48db1792c82
Nikola Tisic <nikola.tisic@gmail.com>
2020-06-03 18:48:38 +0200

    Update lastpass from 4.48.0 to 4.49.0 (#83675)
    Co-authored-by: Nikola Tišić <nikolat@nordeus.com>

--- Casks/lastpass.rb
+++ Casks/lastpass.rb
@@ -1,6 +1,6 @@
 cask 'lastpass' do
-  version '4.48.0'
-  sha256 '494e599c207e134824b4df1d7f9d317ce00e445b6fe7c892f6d98c355c763ca3'
+  version '4.49.0'
+  sha256 '4f42f57d99d4f7eb737e8142308390f0a8d0e069382b1d38397b0009b141d185'
 
   url 'https://download.cloud.lastpass.com/mac/LastPass.dmg'
   appcast 'https://download.cloud.lastpass.com/mac/AppCast.xml'
