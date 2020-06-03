4d2e678253c8f149f6c9304bf119f64a9df32266
Randall <ran.dall@icloud.com>
2020-06-03 14:23:25 -0700

    Update react-studio from 1.7.12,375 to 1.7.14,379

--- Casks/react-studio.rb
+++ Casks/react-studio.rb
@@ -1,6 +1,6 @@
 cask 'react-studio' do
-  version '1.7.12,375'
-  sha256 'd248e36a4648842e7d652546134acf68ad558c3d8972d6900609a41f939988d3'
+  version '1.7.14,379'
+  sha256 '57698edeb8c417f43d9c9fa37d8e00d1f52f926138a8659692c69ca424829d46'
 
   # s3.amazonaws.com/sc.neonto.com/ was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/sc.neonto.com/ReactStudio_v#{version.before_comma.no_dots}_build#{version.after_comma}.zip"
