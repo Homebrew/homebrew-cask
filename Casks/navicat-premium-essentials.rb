74350de1396a852eac2e8e8137d737d3261be934
Randall <ran.dall@icloud.com>
2020-06-03 13:38:20 -0700

    Update navicat-premium-essentials from 15.0.16 to 15.0.17

--- Casks/navicat-premium-essentials.rb
+++ Casks/navicat-premium-essentials.rb
@@ -1,6 +1,6 @@
 cask 'navicat-premium-essentials' do
-  version '15.0.16'
-  sha256 'c9c771ab581d46c677b87cd992a77da64ae4a589b58f2a72daf9045434c33d6a'
+  version '15.0.17'
+  sha256 '24ce244fe8a46f556e15908a5b8485f5d71b85bf6b85b1ee8cec991d7bd5f8c2'
 
   url "http://download.navicat.com/download/navicatess#{version.major_minor.no_dots}_premium_en.dmg"
   appcast 'https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20Premium%20Essentials&appLang=en'
