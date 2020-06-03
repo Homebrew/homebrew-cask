05fcf1f3ef74e929ed1caa30e3ec972f1e8fe912
Yurii Kolesnykov <root@yurikoles.com>
2020-06-03 14:19:48 +0300

    Update qt-creator from 4.12.1 to 4.12.2 (#83665)

--- Casks/qt-creator.rb
+++ Casks/qt-creator.rb
@@ -1,6 +1,6 @@
 cask 'qt-creator' do
-  version '4.12.1'
-  sha256 '00b9112681f766f5416388c1f672fedaeb2e8f4bec25badf0409bfad0bb2a162'
+  version '4.12.2'
+  sha256 '9e170abe15b3d20c59a713858814e260a69dadb5cfff2997fbb1188bc268b470'
 
   url "https://download.qt.io/official_releases/qtcreator/#{version.major_minor}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
   appcast 'https://download.qt.io/official_releases/qtcreator/',
