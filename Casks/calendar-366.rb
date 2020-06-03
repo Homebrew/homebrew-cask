8de58202c5b6b3f7afe1947a33b8a691edc04a63
Randall <ran.dall@icloud.com>
2020-06-03 13:26:01 -0700

    Update calendar-366 from 2.8.5 to 2.8.6

--- Casks/calendar-366.rb
+++ Casks/calendar-366.rb
@@ -1,6 +1,6 @@
 cask 'calendar-366' do
-  version '2.8.5'
-  sha256 '6472f3896775422848956e9a22acdaf150232f76c6d76f5a32e9ab2071098264'
+  version '2.8.6'
+  sha256 'b0f83e8e152a65d2ea5db5426153e280bebbbddc23949b8d4b734fd5f8bdbf60'
 
   url "https://nspektor.com/downloads/Calendar366_v#{version}.dmg"
   appcast 'https://nspektor.com/downloads/Calendar366IIAsset.xml'
