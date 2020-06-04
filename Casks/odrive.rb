262a2d08e51c7edcb2f008a31c25a8a16fd62c11
Brigitte Zmoelnig <brigitte@corecode.io>
2020-06-04 13:50:18 +0200

    Update odrive from 6683 to 6696

--- Casks/odrive.rb
+++ Casks/odrive.rb
@@ -1,10 +1,10 @@
 cask 'odrive' do
-  version '6683'
-  sha256 '7825ae0d8b603b4a879d4d46551b29562d0b30d3765f98b8b0506a16958c786d'
+  version '6696'
+  sha256 '7deec3397c326b96d898d4bec8a386e336421c61fdf4e24335f4f5fb28752452'
 
   # downloads can be found at https://www.odrive.com/downloaddesktop
   # d3huse1s6vwzq6.cloudfront.net/ was verified as official when first introduced to the cask
-  url "https://d3huse1s6vwzq6.cloudfront.net/odrivesync.#{version}.dmg"
+  url "https://d3huse1s6vwzq6.cloudfront.net/odrivesync.#{version}.pkg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.odrive.com/downloaddesktop?platform=mac'
   name 'odrive'
   homepage 'https://www.odrive.com/'
