6c795361193b8e9d39a3de7bbf3b5481e221429e
Brigitte Zmoelnig <brigitte@corecode.io>
2020-06-04 13:07:54 +0200

    Update pycharm-with-anaconda-plugin from 2020.1.1 to 2020.1.2

--- Casks/pycharm-with-anaconda-plugin.rb
+++ Casks/pycharm-with-anaconda-plugin.rb
@@ -1,6 +1,6 @@
 cask 'pycharm-with-anaconda-plugin' do
-  version '2020.1.1'
-  sha256 'aaaab145b0a84e81dd2d2bd6caee2da2315e8667fcf24faa4adefa493093620f'
+  version '2020.1.2'
+  sha256 '3b4c0fe0d324f1be355c8b1318affa49856a38b6a1a21ccfcb5341a996f2547a'
 
   url "https://download.jetbrains.com/python/pycharm-professional-anaconda-#{version}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=PCC&latest=true&type=release'
