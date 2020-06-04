f519bc10ffcd51e8fd8fd340a361af448c0de333
Kristoffer Fossheim Håvik <khavik@protonmail.com>
2020-06-04 14:03:00 +0200

    Add Nord Sound Manager v7.54
    New cask

---
+++ Casks/nord-sound-manager.rb
@@ -0,0 +1,10 @@
+cask 'nord-sound-manager' do
+  version '7.54'
+  sha256 '6a58ef7ddbc5f5c40d278bf91b63331e5ee435e6faa2ec3dfb4eff22d9bc3061'
+
+  url "https://www.nordkeyboards.com/sites/default/files/files/downloads/software/nord-sound-manager/Nord%20Sound%20Manager%20v#{version}.dmg"
+  name 'Nord Sound Manager'
+  homepage 'https://www.nordkeyboards.com/downloads/software-tools/nord-sound-manager'
+
+  app "Nord Sound Manager v#{version}.app"
+end
