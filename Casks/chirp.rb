a88cf514b3231871f5d3e1dd2a307dbb1c579c92
Brigitte Zmoelnig <brigitte@corecode.io>
2020-06-04 13:24:18 +0200

    Update chirp from 20200521 to 20200603

--- Casks/chirp.rb
+++ Casks/chirp.rb
@@ -1,6 +1,6 @@
 cask 'chirp' do
-  version '20200521'
-  sha256 'f5115b5bffd3a2c931a4bd8ce45d62def44e2000899ba8738e8591d38557b8f9'
+  version '20200603'
+  sha256 '30fefb6b520f08fbbcfac7a0d43bf8b0c108f60542e843affafaac8a62fa8694'
 
   url "https://trac.chirp.danplanet.com/chirp_daily/LATEST/chirp-unified-daily-#{version}.app.zip"
   appcast 'https://trac.chirp.danplanet.com/chirp_daily/LATEST/SHA1SUM'
