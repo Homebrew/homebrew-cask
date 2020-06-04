1d0e623b7e80ffbde7015e1463ca775b4c07f04a
Kristoffer Fossheim Håvik <khavik@protonmail.com>
2020-06-04 20:01:35 +0200

    Style fix for nord-sound-manager
    Double to single quotes in simple string

--- Casks/nord-sound-manager.rb
+++ Casks/nord-sound-manager.rb
@@ -3,7 +3,7 @@ cask 'nord-sound-manager' do
   sha256 '6a58ef7ddbc5f5c40d278bf91b63331e5ee435e6faa2ec3dfb4eff22d9bc3061'
 
   url "https://www.nordkeyboards.com/sites/default/files/files/downloads/software/nord-sound-manager/Nord%20Sound%20Manager%20v#{version}.dmg"
-  appcast "https://www.nordkeyboards.com/software-tools/nord-sound-manager/update-history"
+  appcast 'https://www.nordkeyboards.com/software-tools/nord-sound-manager/update-history'
   name 'Nord Sound Manager'
   homepage 'https://www.nordkeyboards.com/downloads/software-tools/nord-sound-manager'
 
