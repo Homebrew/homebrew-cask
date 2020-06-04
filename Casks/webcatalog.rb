9a950c288c9ee249d41515bcd10c9e85f324657e
Quang Lam <quang.lam2807@gmail.com>
2020-06-04 20:18:56 +0700

    Update webcatalog from 21.1.0 to 22.0.0-alpha (#83769)
    Co-authored-by: runner <runner@Mac-1467.local>

--- Casks/webcatalog.rb
+++ Casks/webcatalog.rb
@@ -1,6 +1,6 @@
 cask 'webcatalog' do
-  version '21.1.0'
-  sha256 '6cbc9670f8da08e2b0ca51426ee52998e329883686c9871111e156c3c2b047d5'
+  version '22.0.0-alpha'
+  sha256 '79f6f28fb64fa4543f14e165401216f1594c231c73f02f2a5f751801c2a32dad'
 
   # github.com/quanglam2807/webcatalog/ was verified as official when first introduced to the cask
   url "https://github.com/quanglam2807/webcatalog/releases/download/v#{version}/WebCatalog-#{version}-mac.zip"
