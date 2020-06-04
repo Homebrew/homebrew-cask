be39d8756b1d20e5bf197ee95d1b2abd9e832498
Guy B. deBros <guydebros@users.noreply.github.com>
2020-06-04 11:44:23 -0400

    added zap stanza and updated url for Jump Desktop 8.5.15 (#83771)

--- Casks/jump.rb
+++ Casks/jump.rb
@@ -2,10 +2,12 @@ cask 'jump' do
   version '8.5.15'
   sha256 'e90e0211481a52bc74dac39bb9847bfe605fdce628225d87ef9c8961985bfed9'
 
-  url "https://mirror.jumpdesktop.com/downloads/JumpDesktopMac-#{version}.zip"
+  url 'https://dl.jumpdesktop.com/downloads/JumpDesktopMac.zip'
   appcast 'https://jumpdesktop.com/downloads/viewer/jdmac-web-appcast.xml'
   name 'Jump Desktop'
   homepage 'https://jumpdesktop.com/#jdmac'
 
   app 'Jump Desktop.app'
+
+  zap trash: '~/Documents/JumpDesktop'
 end
