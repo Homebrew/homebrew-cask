7079da2ed9752ef577f3cd7c5889ca8f5edb8564
Andrii Leitsius <leitsius@gmail.com>
2020-06-05 20:34:14 +0300

    Update Casks/meetingbar.rb
    Co-authored-by: Randall <17261190+ran-dall@users.noreply.github.com>

--- Casks/meetingbar.rb
+++ Casks/meetingbar.rb
@@ -2,11 +2,12 @@ cask 'meetingbar' do
   version '1.0b14'
   sha256 'fbc449bbb0996efb34eb4eae28e356e8ac2e6313b2c7190d6f7a9dcbfe0dad39'
 
-  appcast 'https://github.com/leits/MeetingBar/releases.atom'
-
   url "https://github.com/leits/MeetingBar/releases/download/v#{version}/MeetingBar.dmg"
+  appcast 'https://github.com/leits/MeetingBar/releases.atom'
   name 'MeetingBar'
   homepage 'https://github.com/leits/MeetingBar'
+
+  depends_on macos: '>= :catalina'
 
   app 'MeetingBar.app'
 end
