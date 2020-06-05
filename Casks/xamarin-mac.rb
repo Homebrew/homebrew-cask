0c5ede5d6f63635e1228e7fde2429cfae66a28a7
Randall <ran.dall@icloud.com>
2020-06-05 13:48:31 -0700

    Remove `appcast` `configuration

--- Casks/xamarin-mac.rb
+++ Casks/xamarin-mac.rb
@@ -3,7 +3,6 @@ cask 'xamarin-mac' do
   sha256 '1fd7e2c6c839aa958a568261c3af3df358d4ac64bfc73079a4c65e3455b4dccb'
 
   url "https://dl.xamarin.com/XamarinforMac/Mac/xamarin.mac-#{version}.pkg"
-          configuration: version.major_minor
   appcast 'https://github.com/xamarin/xamarin-macios/releases.atom'
   name 'Xamarin Mac'
   homepage 'https://www.xamarin.com/platform'
