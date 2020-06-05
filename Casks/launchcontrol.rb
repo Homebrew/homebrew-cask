14eb5d3941f6a4fc0dc2b6c025f8c01e59df73c9
i0ntempest <szf1234@me.com>
2020-06-05 14:39:59 -0400

    Update launchcontrol from 1.50 to 1.50.1

--- Casks/launchcontrol.rb
+++ Casks/launchcontrol.rb
@@ -1,6 +1,6 @@
 cask 'launchcontrol' do
-  version '1.50'
-  sha256 '6fd8a31784854c21a978f989b4c75fd5a64f4273f56428e297aeffaf467760f2'
+  version '1.50.1'
+  sha256 '3bb6ad46e8610b093321f0fc71d7d580f4c3aa6c54ffdb435f1dc8988183b0dc'
 
   url "https://www.soma-zone.com/download/files/LaunchControl-#{version}.tar.bz2"
   appcast 'https://www.soma-zone.com/LaunchControl/a/appcast.xml'
