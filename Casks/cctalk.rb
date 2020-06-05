76500ab119866756e19af83e62b6d14dcff2157c
Viktor Szakats <vszakats@users.noreply.github.com>
2020-06-05 19:10:53 +0200

    cctalk: restore HTTPS (#83823)

--- Casks/cctalk.rb
+++ Casks/cctalk.rb
@@ -3,7 +3,7 @@ cask 'cctalk' do
   sha256 '82983490f55ab5b4e37c184d47a5e59bf6376cc742f7a6c2aaba1f18668f6d78'
 
   # cc.hjfile.cn/ was verified as official when first introduced to the cask
-  url "http://cc.hjfile.cn/cc/#{version}/8/1/103/#{version}.dmg"
+  url "https://cc.hjfile.cn/cc/#{version}/8/1/103/#{version}.dmg"
   appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://www.cctalk.com/webapi/basic/v1.1/version/down%3Fapptype=1%26terminalType=8%26versionType=103'
   name 'CCtalk'
   homepage 'https://www.cctalk.com/download/'
