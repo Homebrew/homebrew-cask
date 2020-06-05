b4061af8f6cf61faa516b6f219606e156f23fb09
miccal <miccal.matthews@gmail.com>
2020-06-06 05:19:32 +0800

    Update pro-fit

--- Casks/pro-fit.rb
+++ Casks/pro-fit.rb
@@ -2,10 +2,11 @@ cask 'pro-fit' do
   version '7.0.15'
   sha256 'de345d34221ee9554c67b8b70ac5091b1c1e8ae79f617e58ddde87ffb0ea8fe6'
 
-  # quantum-soft.com was verified as official when first introduced to the cask
-  url 'https://quantum-soft.com/profit/pro_Fit_7_0_15.dmg'
+  # quantum-soft.com/ was verified as official when first introduced to the cask
+  url "https://quantum-soft.com/profit/pro_Fit_#{version.dots_to_underscores}.dmg"
+  appcast 'https://www.quansoft.com/hist.html'
   name 'pro Fit'
   homepage 'https://www.quansoft.com/'
 
-  app 'pro Fit 7.app'
+  app "pro Fit #{version.major}.app"
 end
