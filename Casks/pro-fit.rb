556f6e3aad23c2381be8c12d31cfa8ea910d1f8f
Andrew Sirolly <asirolly3@gmail.com>
2020-06-04 22:58:01 -0400

    Add pro Fit 7.app v7.0.15

---
+++ Casks/pro-fit.rb
@@ -0,0 +1,11 @@
+cask 'pro-fit' do
+  version '7.0.15'
+  sha256 'de345d34221ee9554c67b8b70ac5091b1c1e8ae79f617e58ddde87ffb0ea8fe6'
+
+  # quantum-soft.com was verified as official when first introduced to the cask
+  url "https://quantum-soft.com/profit/pro_Fit_7_0_15.dmg"
+  name 'pro Fit'
+  homepage 'https://www.quansoft.com'
+
+  app 'pro Fit 7.app'
+end
