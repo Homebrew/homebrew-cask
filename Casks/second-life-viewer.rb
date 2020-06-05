f7a80fcc7dc7dddf0aca8cabe7162ff1c0341f3b
Randall <ran.dall@icloud.com>
2020-06-04 21:45:20 -0700

    Update second-life-viewer from 6.4.3.542964 to 6.4.4.543142

--- Casks/second-life-viewer.rb
+++ Casks/second-life-viewer.rb
@@ -1,6 +1,6 @@
 cask 'second-life-viewer' do
-  version '6.4.3.542964'
-  sha256 '6e7750c35f6ee0b9273f0195dbd4e3b97acea4df1e7a394ee86a13b9f0b764a5'
+  version '6.4.4.543142'
+  sha256 '729912b502dd2787585211169fb231315d995c20ed2e557e3e2c3c77c0015499'
 
   url "http://download.cloud.secondlife.com/Viewer_#{version.major}/Second_Life_#{version.dots_to_underscores}_x86_64.dmg"
   appcast 'https://secondlife.com/support/downloads/'
