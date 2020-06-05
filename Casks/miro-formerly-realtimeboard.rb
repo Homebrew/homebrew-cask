ee2831283879f384afe459631c2562673ab9c801
Julian Mayer <julian@corecode.io>
2020-06-05 18:30:48 +0200

    Update miro-formerly-realtimeboard

--- Casks/miro-formerly-realtimeboard.rb
+++ Casks/miro-formerly-realtimeboard.rb
@@ -2,13 +2,12 @@ cask 'miro-formerly-realtimeboard' do
   version :latest
   sha256 :no_check
 
-  # desktop.realtimeboard.com/ was verified as official when first introduced to the cask
-  url 'https://desktop.realtimeboard.com/platforms/darwin/Miro%20-%20formerly%20RealtimeBoard.dmg'
+  url 'https://desktop.miro.com/platforms/darwin/Miro.dmg'
   name 'Miro'
   name 'RealtimeBoard'
   homepage 'https://miro.com/'
 
-  app 'Miro - formerly RealtimeBoard.app'
+  app 'Miro.app'
 
   zap trash: [
                '~/Library/Preferences/com.electron.realtimeboard.plist',
