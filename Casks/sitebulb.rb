6fe4dcc0b3149a599ac0b8baed571804d995e97b
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-06 02:49:34 +0000

    Update sitebulb from 3.6.3 to 3.6.4 (#83875)

--- Casks/sitebulb.rb
+++ Casks/sitebulb.rb
@@ -1,6 +1,6 @@
 cask 'sitebulb' do
-  version '3.6.3'
-  sha256 'fcf7ed70115cdd1ec2d4bb59b38a566d43177b449c298d5b5d8a03db81f7f12d'
+  version '3.6.4'
+  sha256 'f3ec327d9261a70a376c1ce49555599e17e5be53e1cb7136764569e71fc75a35'
 
   url "https://downloads.sitebulb.com/#{version}/macOS/Sitebulb.dmg"
   appcast 'https://sitebulb.com/download/'
