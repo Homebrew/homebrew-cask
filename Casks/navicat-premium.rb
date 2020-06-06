d47d2819cd23af22fedf247ebf77770f396cfc6b
Cláudio de Oliveira Pereira <pclaudio.ans@gmail.com>
2020-06-05 22:57:10 -0400

    Update navicat-premium from 15.0.16 to 15.0.17 (#83879)

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -1,14 +1,14 @@
 cask 'navicat-premium' do
-  version '15.0.16'
+  version '15.0.17'
 
   language 'zh', 'CN' do
-    sha256 '03cacd764a53ab2df02aefa4af45e907a690cc161fdbaf41b034a3ef4a26a238'
+    sha256 '2fe70a1cc4ab9f633746886a6a6048bf55af12635ed0b3c9d08c512d0524db4c'
     url "http://download.navicat.com.cn/download/navicat#{version.major_minor.no_dots}_premium_cs.dmg"
     homepage 'https://www.navicat.com.cn/products/navicat-premium'
   end
 
   language 'en', default: true do
-    sha256 '6da966165fdea8a1f26a33b9eca4de59c1974d64ad901e1090b03a1983a8bf58'
+    sha256 'ba0ddffa076c7d01384dedf7383bcc33695d27c584223944e62f8df388403f3d'
     url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_premium_en.dmg"
     appcast 'https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20Premium&appLang=en'
     homepage 'https://www.navicat.com/products/navicat-premium'
