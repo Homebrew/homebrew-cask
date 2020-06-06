315119eceaa77163f1dbfaf61d35095be5c36398
Randall <ran.dall@icloud.com>
2020-06-05 19:11:57 -0700

    Update dashcam-viewer from 3.4.1 to 3.4.2

--- Casks/dashcam-viewer.rb
+++ Casks/dashcam-viewer.rb
@@ -1,6 +1,6 @@
 cask 'dashcam-viewer' do
-  version '3.4.1'
-  sha256 '4ff16c32345d92127e0642f2d62b896140fd4253df6f52599aaf40021eecdec1'
+  version '3.4.2'
+  sha256 'e512a99a9fff4f39d5f6991dc69182d971e02d58bc1aa59605db6c75b7f3305b'
 
   # filedn.com/l2s8TAtm4VASBX72ds0zYD8/dcv/ was verified as official when first introduced to the cask
   url "https://filedn.com/l2s8TAtm4VASBX72ds0zYD8/dcv/Dashcam_Viewer_v#{version}.dmg"
