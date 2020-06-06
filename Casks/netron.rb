fdcf04ddb9eef33560642130e4dcdeaadd5224d9
Lutz Roeder <lutzroeder@users.noreply.github.com>
2020-06-06 08:06:18 -0700

    Add netron 4.2.8 (#83880)

--- Casks/netron.rb
+++ Casks/netron.rb
@@ -1,6 +1,6 @@
 cask 'netron' do
-  version '4.2.7'
-  sha256 '615e66ed6329a268cb2a0218e95f3ac8c6da5f16d68b24893f08d26e9f48c58e'
+  version '4.2.8'
+  sha256 'c6710d6d984249867a056f28c256a411a190151513d7f3c370f318bbd1d83015'
 
   url "https://github.com/lutzroeder/netron/releases/download/v#{version}/Netron-#{version}-mac.zip"
   appcast 'https://github.com/lutzroeder/netron/releases.atom'
