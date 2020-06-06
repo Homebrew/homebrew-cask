b4055f412bd8c22d40cf4cd876d0bbb2ee073120
Randall <ran.dall@icloud.com>
2020-06-05 19:24:00 -0700

    Update preform from 3.5.1,354 to 3.5.2,371

--- Casks/preform.rb
+++ Casks/preform.rb
@@ -1,6 +1,6 @@
 cask 'preform' do
-  version '3.5.1,354'
-  sha256 '1ae70748356b66f30c3bbf9b295ec54a857ec791c6e74cea6f1317d63429c17f'
+  version '3.5.2,371'
+  sha256 '7d823d863da9e31a111d0064c321723b68ef23c114fd0ea887c47b663260c490'
 
   # s3.amazonaws.com/FormlabsReleases/ was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/FormlabsReleases/Release/#{version.before_comma}/PreForm_#{version.before_comma}_release_origin_release_#{version.before_comma}_build_#{version.after_comma}.dmg"
