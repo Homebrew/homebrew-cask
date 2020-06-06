74cf116300b382c4203583f7689e2bb0d76e18ba
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-06 02:19:15 +0000

    Update airflow from 3.1.2 to 3.1.3u1 (#83868)

--- Casks/airflow.rb
+++ Casks/airflow.rb
@@ -1,6 +1,6 @@
 cask 'airflow' do
-  version '3.1.2'
-  sha256 'f0dfcafd0ef4c0775865d2c8e0534cdb2eecad1a9fd1fdcc79cce252c3adfcd1'
+  version '3.1.3u1'
+  sha256 'd7dd65cae80df5bb073ab7ed12d5552d2be727c5babac400ac9a84372cabe544'
 
   # cdn.downloads.iocave.net/Airflow/ was verified as official when first introduced to the cask
   url "https://cdn.downloads.iocave.net/Airflow/Airflow%20#{version}.zip"
