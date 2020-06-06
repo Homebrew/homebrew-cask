9fbd887f5024f1a636e7e292859f4926e302ab20
Randall <ran.dall@icloud.com>
2020-06-05 19:09:41 -0700

    Update plex from 1.11.0.1215-1d8c460c to 1.12.0.1232-aacabe78

--- Casks/plex.rb
+++ Casks/plex.rb
@@ -1,6 +1,6 @@
 cask 'plex' do
-  version '1.11.0.1215-1d8c460c'
-  sha256 '5863dbee971a09875514e3f3a6729d2bfd273a211d849a3e0b122781a00d59f3'
+  version '1.12.0.1232-aacabe78'
+  sha256 '6da7dd4cbacc3cae4f97a1ab103731e3a62e72cf9e18984e224ccfeb3797c8b0'
 
   url "https://downloads.plex.tv/plex-desktop/#{version}/macos/Plex-#{version}-x86_64.zip"
   appcast 'https://plex.tv/api/downloads/6.json'
