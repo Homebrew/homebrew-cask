191325432aa16b15e347dc187f679f11e03aa637
Randall <ran.dall@icloud.com>
2020-06-05 18:36:03 -0700

    Update session from 1.0.8 to 1.0.9

--- Casks/session.rb
+++ Casks/session.rb
@@ -1,6 +1,6 @@
 cask 'session' do
-  version '1.0.8'
-  sha256 'b3a47debad56e70cad80cf370c9a7b87bdd622b4ded0330c304a526856fd86c2'
+  version '1.0.9'
+  sha256 'dcdcaf12c18ef8b41fea50700b8c39580353757ac818a00a4c4e15ceeb482bf8'
 
   # github.com/loki-project/session-desktop/ was verified as official when first introduced to the cask
   url "https://github.com/loki-project/session-desktop/releases/download/v#{version}/session-messenger-desktop-mac-#{version}.dmg"
