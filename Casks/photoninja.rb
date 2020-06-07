2c5151ad7a99def2a3ff115555d7d3ec7bf10295
suschizu <brigitte@corecode.io>
2020-06-07 18:59:39 +0200

    Update photoninja from 1.3.8b to 1.3.9 (#83925)

--- Casks/photoninja.rb
+++ Casks/photoninja.rb
@@ -1,6 +1,6 @@
 cask 'photoninja' do
-  version '1.3.8b'
-  sha256 '40709d2711ab780054efccc5550941e4f31240311ed62fdf418378eb558a48cb'
+  version '1.3.9'
+  sha256 'f08dcb13709460e94898176bc0276716026974326eca24efc3927ecef24de0a9'
 
   # picturecode.cachefly.net/ was verified as official when first introduced to the cask
   url "https://picturecode.cachefly.net/photoninja/downloads/Install_PhotoNinja_#{version}.dmg"
