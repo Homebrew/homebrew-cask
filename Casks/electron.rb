2ca57d888d08be74f85c1e1dbff19a8d2bb6cf00
UP4B LLC <spaquet@up4b.com>
2020-06-07 09:44:02 -0700

    Update electron from 9.0.0 to 9.0.3 (#83921)

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,6 +1,6 @@
 cask 'electron' do
-  version '9.0.0'
-  sha256 '86096d2ecff0698957bfbbe8a8d56c7f9ed9abe7e5a5bfe84a8f4af0635625e1'
+  version '9.0.3'
+  sha256 '7d4061b0217f922c50ebdc924cbbe55faa4104d0f733aebe6e04079ee3c4eca6'
 
   # github.com/electron/electron/ was verified as official when first introduced to the cask
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
