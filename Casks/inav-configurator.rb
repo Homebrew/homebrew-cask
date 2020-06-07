1277a606256bb4bd4a3b55024925f7892c6ebe73
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-07 15:30:23 -0700

    Update inav-configurator from 2.4.1 to 2.5.0 (#83942)

--- Casks/inav-configurator.rb
+++ Casks/inav-configurator.rb
@@ -1,6 +1,6 @@
 cask 'inav-configurator' do
-  version '2.4.1'
-  sha256 'df3b5d4c75063e1c93393e2af986c76d7bad7560f0cd597a8d36456f1cfb7003'
+  version '2.5.0'
+  sha256 'cdecdb1bbf8fd22c2102b7e7ab3aaca8c9174040960fb66a882276fa9a0580b0'
 
   url "https://github.com/iNavFlight/inav-configurator/releases/download/#{version}/INAV-Configurator_macOS_#{version}.zip"
   appcast 'https://github.com/iNavFlight/inav-configurator/releases.atom'
