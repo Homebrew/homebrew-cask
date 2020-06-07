14f2783856875c9f8c4a7b3fb72561a4dc67627c
suschizu <brigitte@corecode.io>
2020-06-07 15:36:53 +0200

    Update klayout.rb

--- Casks/klayout.rb
+++ Casks/klayout.rb
@@ -1,18 +1,18 @@
 cask 'klayout' do
-  version '0.26.4'
+  version '0.26.5'
 
   if MacOS.version <= :high_sierra
-    sha256 '2dfadd201d54bb1c30a7713941759687cd22021ac0205f8e1dff20ef57728618'
+    sha256 'a5153ebc166991432076ae3e98d9ed5a2429d9dae959d44b05fe0aeaa51df0c9'
 
     # klayout.org/ was verified as official when first introduced to the cask
     url "https://www.klayout.org/downloads/MacOS/ST-klayout-#{version}-macOS-HighSierra-1-qt5MP-RsysPsys.dmg"
   elsif MacOS.version <= :mojave
-    sha256 'b6d12eff3b773b40330eaf839461d129dbb3d69fa7a3ca900bcedb688ec72801'
+    sha256 '72f2d93b4f935b3da632e0dffd1bbb90d20a6107d42a2f0d01bccba2effa21fc'
 
     # klayout.org/ was verified as official when first introduced to the cask
     url "https://www.klayout.org/downloads/MacOS/ST-klayout-#{version}-macOS-Mojave-1-qt5MP-RsysPsys.dmg"
   else
-    sha256 'f6a3c7a29f9c07c97c14d2673104118f7e8f372a7301a51997c99a8cbf8a6e37'
+    sha256 '6b97e5cf03e1404c9a62faa37c252aef2f3b67eee8da792a329bdc7a2a2b01ff'
 
     # klayout.org/ was verified as official when first introduced to the cask
     url "https://www.klayout.org/downloads/MacOS/ST-klayout-#{version}-macOS-Catalina-1-qt5MP-RsysPsys.dmg"
