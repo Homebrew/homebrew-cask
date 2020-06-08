f357341894d8c5e4ee95ce0834b55dc74d62ddc2
Kel Byers <kel_byers@homedepot.com>
2020-06-08 10:48:26 -0500

    Update sha256 for kekadefaultapp 1.1.22,1.0.1-r.7
    The sha256 value in the cask does not match the value of the file that
    is downloaded from github. This updates the cask to match the correct
    sha256 checksum.

--- Casks/kekadefaultapp.rb
+++ Casks/kekadefaultapp.rb
@@ -1,6 +1,6 @@
 cask 'kekadefaultapp' do
   version '1.1.22,1.0.1-r.7'
-  sha256 '64a9ba60d5fdf7e8e72b21e9bdec0c2804b07f83599de61368f2ecebdcb65eb1'
+  sha256 '14a6150bf81907b54430ab93b1963c16f1fb56a212f36a01da2a44de945299d4'
 
   url "https://github.com/aonez/Keka/releases/download/v#{version.before_comma}/KekaDefaultApp.v#{version.after_comma}.zip"
   appcast 'https://github.com/aonez/Keka/releases.atom'
