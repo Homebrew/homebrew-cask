ab90d38e7bbf6e669f5289fff25b6a027b1ba6e0
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2020-06-08 17:04:31 +0200

    Update burp-suite from 2020.4.1 to 2020.5 (#83956)

--- Casks/burp-suite.rb
+++ Casks/burp-suite.rb
@@ -1,6 +1,6 @@
 cask 'burp-suite' do
-  version '2020.4.1'
-  sha256 'dc5e2c9d8e663246c90f63a1ab9fa72b705c99f2f54885dfdc0b4084c85c2fa7'
+  version '2020.5'
+  sha256 '489afa0d0ce7b5d4998278ae15bd19943d0f43a1692fece47da6f2a7effe28d1'
 
   url "https://portswigger.net/burp/releases/download?product=community&version=#{version}&type=MacOsx"
   appcast 'https://portswigger.net/burp/releases?initialTab=community'
