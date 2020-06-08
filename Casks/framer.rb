0d4b61108f9c4fae78b1ffed9f5338fcfa2c9e13
Vítor Galvão <info@vitorgalvao.com>
2020-06-08 19:13:17 +0100

    Update framer.rb

--- Casks/framer.rb
+++ Casks/framer.rb
@@ -5,7 +5,7 @@ cask 'framer' do
   url "https://dl.framer.com/com.framer.desktop/#{version.before_comma}/#{version.after_comma}/FramerDesktop-#{version.before_comma}.zip"
   appcast 'https://updates.framer.com/sparkle/com.framer.desktop'
   name 'Framer'
-  homepage 'https://framer.com/'
+  homepage 'https://www.framer.com/desktop/'
 
   depends_on macos: '>= :sierra'
 
