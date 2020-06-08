c8aab03d600e8b42364c1e611cff4c4c61bd41d2
Yassine ElBadaoui <byassine52@users.noreply.github.com>
2020-06-08 17:14:36 +0900

    Update Yandex Disk

--- Casks/yandex-disk.rb
+++ Casks/yandex-disk.rb
@@ -1,6 +1,6 @@
 cask 'yandex-disk' do
-  version '3.1.17,30'
-  sha256 'f94f29b997ab5342158c5f67e32ddd678fb34d48a2fd6ddbbbafeecdfd5393db'
+  version '3.1.19,30'
+  sha256 '8259906fb6b1032c77987228d3dd120da08e564ff1028eeea8332b69b29b393a'
 
   url "https://disk.yandex.ru/download/YandexDisk#{version.after_comma}.dmg/?instant=1"
   name 'Yandex.Disk'
@@ -8,7 +8,7 @@ cask 'yandex-disk' do
 
   auto_updates true
 
-  app 'Yandex.Disk.2.app'
+  app 'Yandex.Disk.app'
 
   zap trash: [
                '~/Library/Application Support/Yandex.Disk.2',
