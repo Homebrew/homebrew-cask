9e055c48fb85924b635788398b79cf8063154a95
Ling Cong Xiang <ralcx@outlook.com>
2020-06-09 01:45:13 +0800

    crossover: Add zap stanza

--- Casks/crossover.rb
+++ Casks/crossover.rb
@@ -8,4 +8,14 @@ cask 'crossover' do
   homepage 'https://www.codeweavers.com/products/crossover-mac/'
 
   app 'CrossOver.app'
+
+  zap trash: [
+               '~/Library/Caches/com.apple.helpd/Generated/CrossOver Help*',
+               '~/Library/Caches/Cleanup At Startup/CrossOver CD Helper.app',
+               '~/Library/Caches/com.codeweavers.CrossOver',
+               '~/Library/Saved Application State/com.codeweavers.CrossOverHelper*',
+               '~/Library/Logs/CrossOver',
+               '~/Library/Preferences/com.codeweavers.*',
+               '~/Library/Application Support/CrossOver',
+             ]
 end
