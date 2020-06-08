5b21b824e64bc3d7b6e5d9eabb1cfe60530caeb2
John Pitchko <johnpitchko@Johns-iMac.local>
2020-06-08 16:49:37 -0600

    Add Tap Forms Mac 5.app

---
+++ Casks/tap-forms.rb
@@ -0,0 +1,11 @@
+cask 'tap-forms' do
+  version '5.3.11'
+  sha256 '4dd6383a90108a754914819ee0452ac53c05ead7b004a38e8bf243ca956bb0a7'
+
+  # paddle.com was verified as official when first introduced to the cask. Developer uses paddle.com to process licences and payments for the software
+  url 'https://vendors.paddle.com/download/product/503174'
+  name 'Tap Forms 5'
+  homepage 'https://www.tapforms.com/'
+
+  app 'Tap Forms Mac 5.app'
+end
