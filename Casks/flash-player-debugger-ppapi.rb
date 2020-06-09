8d68220b54e595d7c8cd65f6fb2d6a3a0aca291c
Andy Li <andy@onthewings.net>
2020-06-09 22:59:35 +0800

    Update flash-player-debugger-ppapi from 32.0.0.371 to 32.0.0.387 (#84025)
    Co-authored-by: runner <runner@Mac-1978.local>

--- Casks/flash-player-debugger-ppapi.rb
+++ Casks/flash-player-debugger-ppapi.rb
@@ -1,6 +1,6 @@
 cask 'flash-player-debugger-ppapi' do
-  version '32.0.0.371'
-  sha256 'ad95995edeb53c48658c3ac02dd5e73b644c200ae30b0c611fa991209db63761'
+  version '32.0.0.387'
+  sha256 '764657fd56bf0d02fb2ca754baac9cd81c4fc9b83aebdd54e46bf7a5cba5a0fc'
 
   url "https://fpdownload.adobe.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_ppapi_debug.dmg"
   appcast 'https://fpdownload.adobe.com/pub/flashplayer/update/current/xml/version_en_mac_pep.xml',
