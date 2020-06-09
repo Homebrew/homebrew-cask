b07aaed95404c19117b411d22932534284990fa2
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-09 02:45:06 +0000

    Update tandem from 1.5.9 to 1.5.11 (#84013)

--- Casks/tandem.rb
+++ Casks/tandem.rb
@@ -1,6 +1,6 @@
 cask 'tandem' do
-  version '1.5.9'
-  sha256 'fab32f45b9d5d43d101650ef965d108604e7d37cad462e158759ef9eaee35ac1'
+  version '1.5.11'
+  sha256 '7b89f35377d0166ce5fe522e9861b076e531096b5d02d5adab581b3e6305516a'
 
   # wisp-app.s3-accelerate.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://wisp-app.s3-accelerate.amazonaws.com/Tandem-#{version}-mac.zip"
