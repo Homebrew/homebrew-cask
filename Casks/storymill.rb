0b7dd18f05a8342b80786b588b48babff2a17050
Vítor Galvão <info@vitorgalvao.com>
2020-06-09 14:20:38 +0100

    Update storymill.rb

--- Casks/storymill.rb
+++ Casks/storymill.rb
@@ -1,20 +1,19 @@
 cask 'storymill' do
   version '4.0.5'
-  sha256 :no_check
+  sha256 'd16e6609df0530f08c7da830744602ca060dc40375bcc76b143056f58501fa8a'
 
-  # s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url 'https://s3.amazonaws.com/MarinerDownloads/StoryMill/StoryMill_Mac_4_0_5.dmg'
+  # s3.amazonaws.com/MarinerDownloads/StoryMill/ was verified as official when first introduced to the cask
+  url "https://s3.amazonaws.com/MarinerDownloads/StoryMill/StoryMill_Mac_#{version.dots_to_underscores}.dmg"
+  appcast 'http://www.marinersoftware.com/sparkle/StoryMill/StoryMill.xml'
   name 'StoryMill'
   homepage 'https://marinersoftware.com/'
 
-  app 'StoryMill'
+  app 'StoryMill.app'
 
   zap trash: [
                '~/Library/Caches/com.marinersoftware.StoryMill',
                '~/Library/Preferences/com.marinersoftware.StoryMill.plist',
                '~/Library/Application Support/.StoryMill.plist',
              ],
-      rmdir: [
-               '~/Library/Application Support/StoryMill/',
-             ]
+      rmdir: '~/Library/Application Support/StoryMill/'
 end
