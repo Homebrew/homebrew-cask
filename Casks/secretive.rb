00f1f19444d2987afb1337ae7025929d25b77cff
Christopher Nethercott <ccnethercott@gmail.com>
2020-06-09 14:26:36 +0100

    Create secretive.rb

---
+++ Casks/secretive.rb
@@ -0,0 +1,10 @@
+cask 'secretive' do
+  version '1.0.2'
+  sha256 '464ab05baaf14246476a04d05b439776ade06e2937477535a7ea68fa947f507f'
+
+  url "https://github.com/maxgoedjen/secretive/releases/latest/download/Secretive.zip"
+  name 'Secretive'
+  homepage 'https://github.com/maxgoedjen/secretive'
+
+  app 'Secretive.app'
+end
