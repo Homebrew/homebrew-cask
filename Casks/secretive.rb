f48c810edcf39faf33019e9da99da1e3b6b458f8
Vítor Galvão <info@vitorgalvao.com>
2020-06-09 14:44:10 +0100

    Update secretive.rb

--- Casks/secretive.rb
+++ Casks/secretive.rb
@@ -2,7 +2,8 @@ cask 'secretive' do
   version '1.0.2'
   sha256 '464ab05baaf14246476a04d05b439776ade06e2937477535a7ea68fa947f507f'
 
-  url 'https://github.com/maxgoedjen/secretive/releases/download/v1.0.2/Secretive.zip'
+  url "https://github.com/maxgoedjen/secretive/releases/download/v#{version}/Secretive.zip"
+  appcast 'https://github.com/maxgoedjen/secretive/releases.atom'
   name 'Secretive'
   homepage 'https://github.com/maxgoedjen/secretive'
 
