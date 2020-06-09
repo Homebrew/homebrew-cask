d2df0e7bca3ce5746038986c19ddd6a8dfd1cf74
Christopher Nethercott <ccnethercott@gmail.com>
2020-06-09 14:42:09 +0100

    Updated download path to v1.0.2 as that is the sha256 that I have supplied

--- Casks/secretive.rb
+++ Casks/secretive.rb
@@ -2,7 +2,7 @@ cask 'secretive' do
   version '1.0.2'
   sha256 '464ab05baaf14246476a04d05b439776ade06e2937477535a7ea68fa947f507f'
 
-  url 'https://github.com/maxgoedjen/secretive/releases/latest/download/Secretive.zip'
+  url 'https://github.com/maxgoedjen/secretive/releases/download/v1.0.2/Secretive.zip'
   name 'Secretive'
   homepage 'https://github.com/maxgoedjen/secretive'
 
