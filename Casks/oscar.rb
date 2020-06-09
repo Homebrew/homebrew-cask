16f553d7bb1f2e71ed099d03abd53fcd182a9fc6
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-08 18:46:14 -0700

    Update oscar from 1.1.0 to 1.1.1 (#84001)

--- Casks/oscar.rb
+++ Casks/oscar.rb
@@ -1,6 +1,6 @@
 cask 'oscar' do
-  version '1.1.0'
-  sha256 '1aa66aba091dd92760ef0e747514fdd35099d71e38e322da132122ebfeb9aa6e'
+  version '1.1.1'
+  sha256 '69f0f5e37f8c8aeaf2c2f03993cf1a15dc00952657fb6dd8fa4f7bb2b2b1b720'
 
   # apneaboard.com/OSCAR/ was verified as official when first introduced to the cask
   url "https://www.apneaboard.com/OSCAR/OSCAR-#{version}.dmg"
