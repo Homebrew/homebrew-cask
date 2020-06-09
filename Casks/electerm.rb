bf811a946f9e4bc934d666ae89c79e3ea7730390
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-08 18:26:37 -0700

    Update electerm from 1.3.35 to 1.3.36 (#83989)

--- Casks/electerm.rb
+++ Casks/electerm.rb
@@ -1,6 +1,6 @@
 cask 'electerm' do
-  version '1.3.35'
-  sha256 '7b585b2946cd8a948b4c73ec9c52475176cd7043f60a2a66f06a14c05017033a'
+  version '1.3.36'
+  sha256 'c551123867e175b4fccbc21092e29b8125852d224d160860db776b2287e283a5'
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac.dmg"
   appcast 'https://github.com/electerm/electerm/releases.atom'
