edc4ea2922fe322f90e59b1293609822d09d9ad7
Randall <ran.dall@icloud.com>
2020-06-09 22:15:09 -0700

    Update tagspaces from 3.5.2 to 3.5.4

--- Casks/tagspaces.rb
+++ Casks/tagspaces.rb
@@ -1,6 +1,6 @@
 cask 'tagspaces' do
-  version '3.5.2'
-  sha256 'e7ba6bf22185fb4f2e21082e5b5bbb81c93e1d98847ab633883d800765033254'
+  version '3.5.4'
+  sha256 '70a7310eedaa0dae35fd872e1849e8f59cf6e22b35b29e12b629af139ecc716a'
 
   # github.com/tagspaces/tagspaces/ was verified as official when first introduced to the cask
   url "https://github.com/tagspaces/tagspaces/releases/download/v#{version}/tagspaces-mac-#{version}.zip"
