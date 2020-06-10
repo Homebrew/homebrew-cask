a3126015c576f0d2200ac596c44d9674cb433401
Randall <ran.dall@icloud.com>
2020-06-09 22:12:58 -0700

    Update lbry from 0.45.2 to 0.46.1

--- Casks/lbry.rb
+++ Casks/lbry.rb
@@ -1,6 +1,6 @@
 cask 'lbry' do
-  version '0.45.2'
-  sha256 '75b184db304d898bdea1ccf55fab13cd98bb8cea7bad501809c2f1587cce7988'
+  version '0.46.1'
+  sha256 '88f5b6cbc7265d17b05dd39a322b032aaf34fbb91672a3f4832fbb8c1176a47c'
 
   # github.com/lbryio/lbry-desktop/ was verified as official when first introduced to the cask
   url "https://github.com/lbryio/lbry-desktop/releases/download/v#{version}/LBRY_#{version}.dmg"
