3b9da17fcef1b691b6b3a3d71e5f262d48a4e7c7
Claudia <claui@users.noreply.github.com>
2020-06-10 23:18:19 +0200

    amethyst: remove Mavericks support

--- Casks/amethyst.rb
+++ Casks/amethyst.rb
@@ -1,9 +1,5 @@
 cask 'amethyst' do
-  if MacOS.version <= :mavericks
-    version '0.9.10'
-    sha256 '82adf42ce6031ab59a3072e607788e73f594ad5f21c7118aabc6c5dafe3d0b47'
-    url "https://ianyh.com/amethyst/versions/Amethyst-#{version}.zip"
-  elsif MacOS.version <= :el_capitan
+  if MacOS.version <= :el_capitan
     version '0.10.1'
     sha256 '9fd1ac2cfb8159b2945a4482046ee6d365353df617f4edbabc4e8cadc448c1e7'
     url "https://ianyh.com/amethyst/versions/Amethyst-#{version}.zip"
