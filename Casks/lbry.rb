82e28ee27a5b656328348f16fe77ae8815b3cab4
CoreCode <core-code@users.noreply.github.com>
2020-06-10 21:18:37 +0200

    Update lbry from 0.45.2 to 0.46.2 (#84124)

--- Casks/lbry.rb
+++ Casks/lbry.rb
@@ -1,6 +1,6 @@
 cask 'lbry' do
-  version '0.45.2'
-  sha256 '75b184db304d898bdea1ccf55fab13cd98bb8cea7bad501809c2f1587cce7988'
+  version '0.46.2'
+  sha256 'fd64bd1aae3a19d141dd4b1e2257b09c0b2cb9beda0560bc39e989d5d6554f12'
 
   # github.com/lbryio/lbry-desktop/ was verified as official when first introduced to the cask
   url "https://github.com/lbryio/lbry-desktop/releases/download/v#{version}/LBRY_#{version}.dmg"
