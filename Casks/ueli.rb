d820ef244f3384174e9f8dbf00a80f009955af84
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-10 11:53:47 -0700

    Update ueli from 8.7.0 to 8.7.1 (#84060)

--- Casks/ueli.rb
+++ Casks/ueli.rb
@@ -1,6 +1,6 @@
 cask 'ueli' do
-  version '8.7.0'
-  sha256 'ceaeb1782283acaa086fd3ed1eb6f40dfc2c4fb9b151343bd9adc611f004c225'
+  version '8.7.1'
+  sha256 'caa7722348cdd4cb5e2eafa17777efec5fcfa35abbb8057319d13d6b328c299a'
 
   # github.com/oliverschwendener/ueli/ was verified as official when first introduced to the cask
   url "https://github.com/oliverschwendener/ueli/releases/download/v#{version}/ueli-#{version}.dmg"
