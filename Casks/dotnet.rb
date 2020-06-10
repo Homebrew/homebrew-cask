48583f4b1a35955f47bcc7394217615b3a403514
Robert Blair <robertleeblairjr@gmail.com>
2020-06-10 18:32:33 -0500

    Update dotnet from 3.1.4,b51c2705-f7e1-4a59-b6ba-2a70d9caded3:da2567cee8519d5dc4185cbee8f97498 to 3.1.5,e6494006-4940-48ed-9c28-a91fd4cfafba:fd10e3e409d55ff15f43611db6f9ead0

--- Casks/dotnet.rb
+++ Casks/dotnet.rb
@@ -1,6 +1,6 @@
 cask 'dotnet' do
-  version '3.1.4,b51c2705-f7e1-4a59-b6ba-2a70d9caded3:da2567cee8519d5dc4185cbee8f97498'
-  sha256 '164e4f0ae4f238a9e5abd2ca5f7bba9a4f0a1146050d0531edb6dffa942eb59b'
+  version '3.1.5,e6494006-4940-48ed-9c28-a91fd4cfafba:fd10e3e409d55ff15f43611db6f9ead0'
+  sha256 '1eaf839c614b189737e327644c170c2ca9a97809a4a8ed5667712c11f9a8a3d1'
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-runtime-#{version.before_comma}-osx-x64.pkg"
   appcast 'https://www.microsoft.com/net/download/macos'
