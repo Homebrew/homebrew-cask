1fc724fba51879f156fa58607b3e0cae66f67268
Vítor Galvão <info@vitorgalvao.com>
2020-06-11 00:41:41 +0100

    Update x-swiftformat.rb

--- Casks/x-swiftformat.rb
+++ Casks/x-swiftformat.rb
@@ -14,6 +14,6 @@ cask 'x-swiftformat' do
   zap trash: [
                '~/Library/Preferences/com.ruiaureliano.xswiftformat.plist',
                '~/Library/Caches/com.ruiaureliano.xswiftformat',
-               '~/Library/Containers/com.ruiaureliano.xswiftformat'
+               '~/Library/Containers/com.ruiaureliano.xswiftformat',
              ]
 end
