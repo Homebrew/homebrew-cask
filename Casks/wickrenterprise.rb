79ffedef44ece210e4e8877f3fbcf4fb4058e390
Jarmo van Lenthe <jarmovanlenthe@gmail.com>
2020-06-10 08:16:56 +0200

    Fix URL comment

--- Casks/wickrenterprise.rb
+++ Casks/wickrenterprise.rb
@@ -2,7 +2,7 @@ cask 'wickrenterprise' do
   version '5.55.7'
   sha256 '8b926b51744a53267bcb7bb1f5cc96e612cdc0be38921600bfa5023181ad3dce'
 
-  # This is the URL advertized on the homepage.
+  # amazonaws.com/static.wickr.com/downloads/mac/ent was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/static.wickr.com/downloads/mac/ent/WickrEnterprise-#{version}.dmg"
   name 'Wickr Enterprise'
   homepage 'https://enterprise-download.wickr.com/'
