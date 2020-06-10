471e37c2648695ed99684f153a90da2b61ffe117
Randall <ran.dall@icloud.com>
2020-06-09 22:44:45 -0700

    Update prepros from 7.2.25 to 7.2.27

--- Casks/prepros.rb
+++ Casks/prepros.rb
@@ -1,6 +1,6 @@
 cask 'prepros' do
-  version '7.2.25'
-  sha256 'd79ece1a2b45a82a8da50a6697a8c2dde2d99dce7080e124503cf27d5ffcb07a'
+  version '7.2.27'
+  sha256 '17507e37908490267bc493554f51b9b1bfa87f1b08830c43ca012c22342e6821'
 
   url "https://downloads.prepros.io/v#{version.major}/Prepros-#{version}.zip"
   appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://prepros.io/downloads/stable/mac'
