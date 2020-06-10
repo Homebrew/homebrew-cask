a4787767dbdb0e11421c9a04da9aa4267d5ba347
Claudia <claui@users.noreply.github.com>
2020-06-10 23:18:29 +0200

    poedit: remove Mavericks support

--- Casks/poedit.rb
+++ Casks/poedit.rb
@@ -1,17 +1,9 @@
 cask 'poedit' do
-  if MacOS.version <= :mavericks
-    version '1.8.12'
-    sha256 '0aa721a0733eb04635685d280093aeef56b28c0baddf0fc265e1c7d448dbc615'
+  version '2.3.1'
+  sha256 'a6e5314df29a1ac04a4798e28b74462b829c96f1d037058145083fea2a43e891'
 
-    url "https://poedit.net/dl/Poedit-#{version}.zip"
-  else
-    version '2.3.1'
-    sha256 'a6e5314df29a1ac04a4798e28b74462b829c96f1d037058145083fea2a43e891'
-
-    url "https://download.poedit.net/Poedit-#{version}.zip"
-    appcast "https://poedit.net/updates_v#{version.major}/osx/appcast"
-  end
-
+  url "https://download.poedit.net/Poedit-#{version}.zip"
+  appcast "https://poedit.net/updates_v#{version.major}/osx/appcast"
   name 'Poedit'
   homepage 'https://poedit.net/'
 
