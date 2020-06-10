1e846b261fd5a025dd2db228204572c5b058c744
Licardo <1014660822@qq.com>
2020-06-10 20:47:35 +0800

    Update x-swiftformat.rb

--- Casks/x-swiftformat.rb
+++ Casks/x-swiftformat.rb
@@ -2,7 +2,7 @@ cask 'x-swiftformat' do
   version '1.0.2'
   sha256 'bd1a235f98fd1c41832b1f820855109df18220b810ac346abcae0a6dc6b69202'
 
-  url "https://github.com/ruiaureliano/X-SwiftFormat/releases/download/#{version}/x-swiftformat_1.0.2.zip"
+  url "https://github.com/ruiaureliano/X-SwiftFormat/releases/download/#{version}/x-swiftformat_#{version}.zip"
   appcast 'https://github.com/ruiaureliano/X-SwiftFormat/releases.atom'
   name 'X-SwiftFormat'
   homepage 'https://github.com/ruiaureliano/X-SwiftFormat'
