9b7f0d4566ad9b4966aee0be468553f14e727168
Claudia <claui@users.noreply.github.com>
2020-06-10 23:18:31 +0200

    superduper: remove Mavericks support

--- Casks/superduper.rb
+++ Casks/superduper.rb
@@ -1,18 +1,9 @@
 cask 'superduper' do
-  if MacOS.version <= :mavericks
-    version '3.1.1'
-    sha256 '6639cf292fe391cfb1c2283fc3614c462a9859910a4c85e190119548d3c32c31'
+  version '3.3.1,121'
+  sha256 '589d2ae96d8d130e7a7de2124f6f8c12ab49a21b96e6260df734d39ebb896433'
 
-    # shirtpocket.s3.amazonaws.com/SuperDuper/ was verified as official when first introduced to the cask
-    url "https://shirtpocket.s3.amazonaws.com/SuperDuper/SuperDuper!#{version}.dmg"
-  else
-    version '3.3.1,121'
-    sha256 '589d2ae96d8d130e7a7de2124f6f8c12ab49a21b96e6260df734d39ebb896433'
-
-    # shirtpocket.s3.amazonaws.com/SuperDuper/ was verified as official when first introduced to the cask
-    url 'https://shirtpocket.s3.amazonaws.com/SuperDuper/SuperDuper!.dmg'
-  end
-
+  # shirtpocket.s3.amazonaws.com/SuperDuper/ was verified as official when first introduced to the cask
+  url 'https://shirtpocket.s3.amazonaws.com/SuperDuper/SuperDuper!.dmg'
   appcast 'https://versioncheck.blacey.com/superduper/version.xml?VSN=100',
           configuration: version.after_comma
   name 'SuperDuper!'
