97fb4771aae7536d1674d96c2a4262b1f69789d3
Hector Espert <hector@mrjeffapp.com>
2020-06-10 13:20:21 +0200

    Update netbeans from 11.3 to 12.0

--- Casks/netbeans.rb
+++ Casks/netbeans.rb
@@ -1,6 +1,6 @@
 cask 'netbeans' do
-  version '11.3'
-  sha256 '5a00202c8366111d5c3187c0fe8057411673fbc8a4f8b167d0b59ddf66cc9902'
+  version '12.0'
+  sha256 'd7f8f857b5f2a1fe54c73721671b9f86933c2d6e63535de5427569754acc85de'
 
   url "https://www-eu.apache.org/dist/netbeans/netbeans/#{version}/Apache-NetBeans-#{version}-bin-macosx.dmg"
   appcast 'https://www-eu.apache.org/dist/netbeans/netbeans/'
