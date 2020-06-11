9f2a79308be9e1eef40c6b4a2bbe9be9805ccfd8
Vítor Galvão <info@vitorgalvao.com>
2020-06-11 13:20:11 +0100

    switchhosts: change appcast configuratio to must_contain

--- Casks/switchhosts.rb
+++ Casks/switchhosts.rb
@@ -5,7 +5,7 @@ cask 'switchhosts' do
   # github.com/oldj/SwitchHosts/ was verified as official when first introduced to the cask
   url "https://github.com/oldj/SwitchHosts/releases/download/v#{version.major_minor_patch}/SwitchHosts._macOS_#{version}.dmg"
   appcast 'https://github.com/oldj/SwitchHosts/releases.atom',
-          configuration: version.major_minor_patch
+          must_contain: version.major_minor_patch
   name 'SwitchHosts!'
   homepage 'https://oldj.github.io/SwitchHosts/'
 
