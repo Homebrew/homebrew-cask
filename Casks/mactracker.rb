c831e0b04bfbcbc2b5d67332168c7c05ff8a3996
Andware <amuehlemann@andware.dk>
2020-06-11 10:33:07 +0200

    Update mactracker.rb
    Change SHA256

--- Casks/mactracker.rb
+++ Casks/mactracker.rb
@@ -1,6 +1,6 @@
 cask 'mactracker' do
   version '7.9.3'
-  sha256 '893c0417bd13811f534cf78d257193eab2152ef7e8ce39273d4b99eaf00de432'
+  sha256 'c7b387abe08907910eb1d059dcd29f031027383ee4d54f8b3a4e9dfb9d9f090b'
 
   url "https://www.mactracker.ca/downloads/Mactracker_#{version}.zip"
   appcast 'https://update.mactracker.ca/appcast-b.xml'
