966789c4b999a122293cf0bcce8f34d41941fc01
Randall <ran.dall@icloud.com>
2020-06-10 21:23:13 -0700

    Update ring from 2.5.0 to 2.6.0

--- Casks/ring.rb
+++ Casks/ring.rb
@@ -1,6 +1,6 @@
 cask 'ring' do
-  version '2.5.0'
-  sha256 'eab1d877c8812017e416683c2ceea4e5b8a200e662ea84c5109817d8799c5c47'
+  version '2.6.0'
+  sha256 'c598a87f10bf41926d67e7aa8a43bc1a0f1f50b9d2d07a5184e397ec727e118d'
 
   # ring-mac-app-assets.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://ring-mac-app-assets.s3.amazonaws.com/production/Ring_#{version}.zip"
