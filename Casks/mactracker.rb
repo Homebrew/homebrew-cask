fad5e65de971cb593d944463a0a2032186c291b9
Andware <amuehlemann@andware.dk>
2020-06-11 08:51:25 +0200

    Update to version 7.9.3
    Update Mactracker to version 7.9.3

--- Casks/mactracker.rb
+++ Casks/mactracker.rb
@@ -1,6 +1,6 @@
 cask 'mactracker' do
-  version '7.9.2'
-  sha256 'f9fc3a163d33ae08c34a3efcda61cfeaee7f764123fb559a362a476f04a520d1'
+  version '7.9.3'
+  sha256 '893c0417bd13811f534cf78d257193eab2152ef7e8ce39273d4b99eaf00de432'
 
   url "https://www.mactracker.ca/downloads/Mactracker_#{version}.zip"
   appcast 'https://update.mactracker.ca/appcast-b.xml'
