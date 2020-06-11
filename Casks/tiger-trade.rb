4c23f1ac4b9a8dec2f3e89fd77cd832247e00b0a
Randall <ran.dall@icloud.com>
2020-06-10 22:37:11 -0700

    Update tiger-trade from 5.8.2,20200528:509884 to 5.9.0,20200610:5D1CD2

--- Casks/tiger-trade.rb
+++ Casks/tiger-trade.rb
@@ -1,6 +1,6 @@
 cask 'tiger-trade' do
-  version '5.8.2,20200528:509884'
-  sha256 '65bc1f331e6175a3b57100ba8619cd5a5e32b4050ae994179f68d1facb275529'
+  version '5.9.0,20200610:5D1CD2'
+  sha256 '92b2a4756894d7f7fcf76a53dd2581a2457edd9fa6a8093669c18b3bf1cbcdf1'
 
   # s.tigerfintech.com/ was verified as official when first introduced to the cask
   url "https://s.tigerfintech.com/desktop/cdn/f/TigerTrade_#{version.before_comma}_#{version.after_comma.before_colon}_#{version.after_colon}.dmg"
