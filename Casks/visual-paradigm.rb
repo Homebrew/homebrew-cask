6547bbcb763be766c6a1a5546731ad2f0e003d61
Vítor Galvão <info@vitorgalvao.com>
2020-06-11 13:20:12 +0100

    visual-paradigm: change appcast configuratio to must_contain

--- Casks/visual-paradigm.rb
+++ Casks/visual-paradigm.rb
@@ -4,7 +4,7 @@ cask 'visual-paradigm' do
 
   url "https://eu8.dl.visual-paradigm.com/visual-paradigm/vp#{version.before_comma}/#{version.after_comma}/Visual_Paradigm_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
   appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.visual-paradigm.com/downloads/vp/checksum.html',
-          configuration: "#{version.before_comma}/#{version.after_comma}"
+          must_contain: "#{version.before_comma}/#{version.after_comma}"
   name 'Visual Paradigm'
   homepage 'https://www.visual-paradigm.com/'
 
