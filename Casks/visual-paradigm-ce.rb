cb3b7a074423618c8e478d622e69129b322a93fc
Vítor Galvão <info@vitorgalvao.com>
2020-06-11 13:20:12 +0100

    visual-paradigm-ce: change appcast configuratio to must_contain

--- Casks/visual-paradigm-ce.rb
+++ Casks/visual-paradigm-ce.rb
@@ -4,7 +4,7 @@ cask 'visual-paradigm-ce' do
 
   url "https://www.visual-paradigm.com/downloads/vpce/Visual_Paradigm_CE_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
   appcast 'https://www.visual-paradigm.com/downloads/vpce/checksum.html',
-          configuration: "#{version.before_comma.dots_to_underscores}_#{version.after_comma}"
+          must_contain: "#{version.before_comma.dots_to_underscores}_#{version.after_comma}"
   name 'Visual Paradigm Community Edition'
   homepage 'https://www.visual-paradigm.com/'
 
