e179919e6906d6f8c6441f6b2e1cc31d02f1a634
Vítor Galvão <info@vitorgalvao.com>
2020-06-11 13:20:06 +0100

    duet: change appcast configuratio to must_contain

--- Casks/duet.rb
+++ Casks/duet.rb
@@ -5,7 +5,7 @@ cask 'duet' do
   # duet.nyc3.cdn.digitaloceanspaces.com/Mac/ was verified as official when first introduced to the cask
   url "https://duet.nyc3.cdn.digitaloceanspaces.com/Mac/#{version.major_minor.dots_to_underscores}/duet-#{version.dots_to_hyphens}.zip"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://updates.duetdisplay.com/latestMac',
-          configuration: version.dots_to_hyphens
+          must_contain: version.dots_to_hyphens
   name 'Duet'
   homepage 'https://www.duetdisplay.com/'
 
