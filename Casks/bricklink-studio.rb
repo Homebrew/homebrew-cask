e7388bf7a60c358a873a1b6fb20d0e0c6f11a306
Daniel Bayley <daniel.bayley@me.com>
2020-06-11 05:04:43 +0100

    Update Studio to 2.1.5_10 (#84116)
    Co-authored-by: Randall <ran.dall@icloud.com>

--- Casks/bricklink-studio.rb
+++ Casks/bricklink-studio.rb
@@ -1,13 +1,25 @@
 cask 'bricklink-studio' do
-  version :latest
-  sha256 :no_check
+  version '2.1.5_10'
+  sha256 'f5d36786aab7590f1415e15682047025718787027797e56d54d206b510345b27'
 
-  # blstudio.s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url 'https://blstudio.s3.amazonaws.com/Stud.io.pkg'
-  name 'Stud.io'
-  homepage 'https://studio.bricklink.com/v2/build/studio.page'
+  # s3.amazonaws.com/blstudio was verified as official when first introduced to the cask
+  url "https://s3.amazonaws.com/blstudio/Studio#{version.major}.0/Archive/#{version}/Studio+#{version.major}.0.pkg"
+  appcast 'https://bricklink.com/v3/studio/download.page'
+  name 'Studio'
+  homepage 'https://bricklink.com/v3/studio/download.page'
 
-  pkg 'Stud.io.pkg'
+  auto_updates true
 
-  uninstall pkgutil: 'com.bricklink.pkg.Studio'
+  pkg "Studio #{version.major}.0.pkg"
+
+  uninstall pkgutil: "com.bricklink.pkg.Studio#{version.major}.0"
+
+  zap trash: [
+               '~/Library/Application Support/unity.BrickLink.Studio',
+               '~/Library/Preferences/unity.BrickLink.Studio.plist',
+               '~/Library/Preferences/unity.BrickLink.Patcher.plist',
+               '~/Library/Caches/com.plausiblelabs.crashreporter.data/unity.BrickLink.Studio',
+               '~/Library/Saved Application State/unity.BrickLink.Studio.savedState',
+               '~/Library/Saved Application State/unity.BrickLink.Patcher.savedState',
+             ]
 end
