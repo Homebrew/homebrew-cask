6a4ff45bc8ab6da7838a7cb7b5a661bae87dd3df
Vítor Galvão <info@vitorgalvao.com>
2020-06-11 13:20:05 +0100

    android-studio: change appcast configuratio to must_contain

--- Casks/android-studio.rb
+++ Casks/android-studio.rb
@@ -5,7 +5,7 @@ cask 'android-studio' do
   # google.com/dl/android/studio/ was verified as official when first introduced to the cask
   url "https://dl.google.com/dl/android/studio/install/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.dmg"
   appcast 'https://dl.google.com/android/studio/patches/updates.xml',
-          configuration: version.major_minor_patch
+          must_contain: version.major_minor_patch
   name 'Android Studio'
   homepage 'https://developer.android.com/studio/index.html'
 
