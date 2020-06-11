30c593b5efdcac54f2e38913b9e20403efdd6e4a
Vítor Galvão <info@vitorgalvao.com>
2020-06-11 13:20:11 +0100

    securityspy: change appcast configuratio to must_contain

--- Casks/securityspy.rb
+++ Casks/securityspy.rb
@@ -4,7 +4,7 @@ cask 'securityspy' do
 
   url 'https://www.bensoftware.com/securityspy/SecuritySpy.dmg'
   appcast 'https://www.bensoftware.com/securityspy/versionhistory.html',
-          configuration: version.chomp('.0')
+          must_contain: version.chomp('.0')
   name 'SecuritySpy'
   homepage 'https://www.bensoftware.com/securityspy/'
 
