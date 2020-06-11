99a7febd3154717d2092058b2289cbc52037fb28
Jean-Francois Simoneau <jfrancois.simoneau@accuweather.com>
2020-06-11 09:49:15 -0400

    Update grafx from 2.7.2978 to 2.7.2978,64

--- Casks/grafx.rb
+++ Casks/grafx.rb
@@ -1,6 +1,6 @@
 cask 'grafx' do
-  version '2.7.2978'
-  sha256 '26d6bcc4d721ef6c16fa43ab52da68eb92103d6d0e9d771636335886cde6cce2'
+  version '2.7.2978,64'
+  sha256 'e6406bea69124a4d3865988934ba2832392b7b07c45d44dce9bc16c720fb1d47'
 
   url "https://pulkomandy.tk/projects/GrafX#{version.major}/downloads/#{version.after_comma}"
   appcast 'https://gitlab.com/GrafX2/grafX2/-/tags?format=atom',
