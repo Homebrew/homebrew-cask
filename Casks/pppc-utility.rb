3830fbab884513b13ca91b13f19490076a2333fc
David Rosenberg <targendaz2@me.com>
2020-06-11 22:49:42 -0400

    Fixed appcast in PPPC Utility.app Cask

--- Casks/pppc-utility.rb
+++ Casks/pppc-utility.rb
@@ -3,7 +3,7 @@ cask 'pppc-utility' do
   sha256 '58ed98d1ca749bec88651c62ff0e4bb2550e84ba441cd0df7d3225059eb66c61'
 
   url "https://github.com/jamf/PPPC-Utility/releases/download/#{version}/PPPC-Utility.zip"
-  appcast 'https://github.com/jamf/PPPC-Utility/releases'
+  appcast 'https://github.com/jamf/PPPC-Utility/releases.atom'
   name 'PPPC Utility'
   name 'Privacy Preferences Policy Control Utility'
   homepage 'https://github.com/jamf/PPPC-Utility'
