9c74b9410b8e8c0c3e10783a1fa2dc367b592749
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-11 20:34:08 -0700

    Update lehreroffice from 2020.5.1 to 2020.5.2 (#84248)

--- Casks/lehreroffice.rb
+++ Casks/lehreroffice.rb
@@ -1,6 +1,6 @@
 cask 'lehreroffice' do
-  version '2020.5.1'
-  sha256 '89ffeb05b5929f1187df68d9e63ed6e70508ee59abd1517d9fc35c1667471f96'
+  version '2020.5.2'
+  sha256 'c266d5274ae31cc1ef998dd75fbdd67126bac3ab1f8871b75eb2adc665b58392'
 
   url 'https://www.lehreroffice.ch/lo/dateien/easy/lo_desktop_macos.dmg'
   appcast 'https://www.lehreroffice.ch/services/update/getcurrentversion.php?app=Desktop'
