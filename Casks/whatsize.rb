80e2cb73c0e3a3e24e1d47f1408aeef140dfd44d
Randall <ran.dall@icloud.com>
2020-06-11 19:36:55 -0700

    Update whatsize from 7.4.2 to 7.4.3

--- Casks/whatsize.rb
+++ Casks/whatsize.rb
@@ -1,6 +1,6 @@
 cask 'whatsize' do
-  version '7.4.2'
-  sha256 '04dad2063624f19b8aa5de72cca42816b7b14f327999a25364fa458c51d7c2a1'
+  version '7.4.3'
+  sha256 '2e1696853c64a3dad2313557304160d23ff2060d591d35713293a7ef57a19bee'
 
   url "https://www.whatsizemac.com/software/whatsize#{version.major}/whatsize_#{version}.tgz"
   appcast "https://www.whatsizemac.com/software/whatsize#{version.major}/release/notes.xml"
