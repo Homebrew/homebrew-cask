eab04e5d9f6dc2e787f4bcdae7c2bcbdee4bf6d3
Travis Plunk <travis.plunk@microsoft.com>
2020-06-12 11:03:53 -0700

    Add caveat recommending formula

--- Casks/powershell.rb
+++ Casks/powershell.rb
@@ -18,4 +18,8 @@ cask 'powershell' do
                '~/.config/PowerShell',
                '~/.local/share/powershell',
              ]
+
+  caveats <<~EOS
+    For faster updates use https://github.com/PowerShell/Homebrew-Tap#powershell-stable
+  EOS
 end
