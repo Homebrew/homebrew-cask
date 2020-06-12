ddd53d7a87428b51edf44c325f5ee3d98bd7d864
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-11 19:53:52 -0700

    Update kite from 0.20200608.0 to 0.20200609.2 (#84224)

--- Casks/kite.rb
+++ Casks/kite.rb
@@ -1,6 +1,6 @@
 cask 'kite' do
-  version '0.20200608.0'
-  sha256 '44a3a1ce1829de872570a170d1e82be2bf4567049833254e9edae33b8352e4ad'
+  version '0.20200609.2'
+  sha256 '3d4b4fe343f92307d022256523189f6316f7d3ab9e4c937f3644290f15483a6a'
 
   # draqv87tt43s0.cloudfront.net/ was verified as official when first introduced to the cask
   url "https://draqv87tt43s0.cloudfront.net/mac/#{version}/Kite.dmg"
