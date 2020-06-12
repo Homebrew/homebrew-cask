6153b7ec788c6fc55c20af06b9e08ba0d955e33f
Randall <ran.dall@icloud.com>
2020-06-11 19:31:15 -0700

    Update minitube from 3.4 to 3.4.2

--- Casks/minitube.rb
+++ Casks/minitube.rb
@@ -1,6 +1,6 @@
 cask 'minitube' do
-  version '3.4'
-  sha256 '52f423d4225cb9360df00d7f669d55890d674cb4a289e8855375236d660e49ab'
+  version '3.4.2'
+  sha256 'dc6f7d3d40000292c51fa14d2dae2536c7f31d9e54d42cda18ed4a1fd2531207'
 
   url 'https://flavio.tordini.org/files/minitube/minitube.dmg'
   appcast 'https://flavio.tordini.org/minitube-ws/appcast.xml'
