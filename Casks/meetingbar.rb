2a7ee20d7aaf5555cc3a6c2393aa8970c99b79a4
Andrii Leitsius <leitsius@gmail.com>
2020-06-12 18:18:41 +0300

    Up MeetingBar version to 1.1 (#84260)

--- Casks/meetingbar.rb
+++ Casks/meetingbar.rb
@@ -1,6 +1,6 @@
 cask 'meetingbar' do
-  version '1.0b14'
-  sha256 'fbc449bbb0996efb34eb4eae28e356e8ac2e6313b2c7190d6f7a9dcbfe0dad39'
+  version '1.1'
+  sha256 'bf9697b26392da91a05c73884dabcfd5f38d482869060a7d4db20faf7a1d2126'
 
   url "https://github.com/leits/MeetingBar/releases/download/v#{version}/MeetingBar.dmg"
   appcast 'https://github.com/leits/MeetingBar/releases.atom'
