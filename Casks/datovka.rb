0793503e9c97c1f9faf613932240afa180bdd9ec
suschizu <brigitte@corecode.io>
2020-06-12 03:58:57 +0200

    Update datovka.rb from 4.15.0 to 4.15.1 + update url (#84206)

--- Casks/datovka.rb
+++ Casks/datovka.rb
@@ -1,9 +1,9 @@
 cask 'datovka' do
-  version '4.15.0'
-  sha256 'b7b76c092252a45f66b11b43a049ce062ff58b2a9d0d5b027081ac6e3f5131e8'
+  version '4.15.1'
+  sha256 'f9e7e8699d49f87581a737da20124344f4275bf5999a362c4c778c4838537eb7'
 
   # secure.nic.cz/files/datove_schranky/ was verified as official when first introduced to the cask
-  url "https://secure.nic.cz/files/datove_schranky/#{version}/datovka-#{version}-64bit-osx10.7.dmg"
+  url "https://secure.nic.cz/files/datove_schranky/#{version}/datovka-#{version}-64bit-macos10.12.dmg"
   appcast 'https://www.datovka.cz/cs/pages/instalace.html'
   name 'Datovka'
   homepage 'https://www.datovka.cz/'
