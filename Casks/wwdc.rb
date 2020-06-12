5c72033feb2ba52d2741e41cb7698544992f66d8
Zhenfu Shi <szf1234@me.com>
2020-06-11 21:59:29 -0400

    Update wwdc from 7.0 to 7.0.1 (#84211)

--- Casks/wwdc.rb
+++ Casks/wwdc.rb
@@ -1,9 +1,9 @@
 cask 'wwdc' do
-  version '7.0'
-  sha256 '2c04ad6bc542ae6e46dc677434824541a379e681eeb2f0454558878d5ec3abff'
+  version '7.0.1'
+  sha256 '455b7ac02d8fb90ca4df525af27982d0d0f31a51cd9c2eb20e3bbcb1c920ddfb'
 
   # github.com/insidegui/WWDC/ was verified as official when first introduced to the cask
-  url "https://github.com/insidegui/WWDC/releases/download/#{version}/WWDC_v#{version.major}.zip"
+  url "https://github.com/insidegui/WWDC/releases/download/#{version}/WWDC_v#{version}.zip"
   appcast 'https://github.com/insidegui/WWDC/releases.atom'
   name 'WWDC'
   homepage 'https://wwdc.io/'
