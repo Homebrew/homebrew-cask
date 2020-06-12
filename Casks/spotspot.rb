34d2899fdcc9b6c90214cb6cd590dcfd3f6833b8
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-11 19:18:54 -0700

    Update spotspot from 3.3.0 to 4.0.2 (#84218)

--- Casks/spotspot.rb
+++ Casks/spotspot.rb
@@ -1,6 +1,6 @@
 cask 'spotspot' do
-  version '3.3.0'
-  sha256 '789f98df894c4896f24bac349f3ece7a24a50af2d31bf5fd9ab35de89a384090'
+  version '4.0.2'
+  sha256 '789ea9104c704bf7632ec4d0e06ec614470fe446bbd19076f818136aa05af76f'
 
   # github.com/will-stone/SpotSpot/ was verified as official when first introduced to the cask
   url "https://github.com/will-stone/SpotSpot/releases/download/v#{version}/SpotSpot-#{version}.dmg"
