26d1c6d8690fad5819dd9aea30504248df3bfd4e
Randall <ran.dall@icloud.com>
2020-06-11 20:06:29 -0700

    Update camtasia from 2020.0.3 to 2020.0.4

--- Casks/camtasia.rb
+++ Casks/camtasia.rb
@@ -1,6 +1,6 @@
 cask 'camtasia' do
-  version '2020.0.3'
-  sha256 '3078fb2b5a865296432c58b84981c6eeaf630c2c1b52b2195b43424a8f33a569'
+  version '2020.0.4'
+  sha256 '19b68b56650a2b5f1dc6359c3dba464216c9a2a7588fa9efe3f1a1b9daf834df'
 
   url 'https://download.techsmith.com/camtasiamac/releases/Camtasia.dmg'
   appcast 'https://support.techsmith.com/hc/en-us/articles/115006624748-Camtasia-Mac-Version-History'
