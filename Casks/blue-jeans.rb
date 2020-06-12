efa095cdc53c4397efb7bfdd3580c9f28b8df622
Damon Buckwalter <damon@cryptomeme.com>
2020-06-12 13:39:39 -0700

    Use automated installer
    The BlueJeans app auto-updates once the initial install is done.  This automates the initial install process

--- Casks/blue-jeans.rb
+++ Casks/blue-jeans.rb
@@ -1,17 +1,19 @@
 cask 'blue-jeans' do
   version '2.21.0.195'
-  sha256 '9bcc337540e652aeb4b32948d00ae0689fc63eebc78e90e1e115e18d953b3a9d'
+  sha256 'aa14b95151c0b525876c292acb4e077892de8a453306dacd89de31022a8f0dc6'
 
-  url "https://swdl.bluejeans.com/desktop-app/mac/#{version.major_minor_patch}/#{version}/BlueJeansInstaller.dmg"
+  url "https://swdl.bluejeans.com/desktop-app/mac/#{version.major_minor_patch}/#{version}/BlueJeansInstaller.pkg"
   appcast 'https://www.bluejeans.com/downloads'
   name 'Blue Jeans videoconferencing'
   homepage 'https://www.bluejeans.com/'
 
-  installer manual: 'BlueJeansInstaller.app'
+  auto_updates true
 
-  uninstall signal: [
-                      ['TERM', 'com.bluejeansnet.Blue'],
-                      ['TERM', 'com.bluejeansnet.BlueMenulet'],
-                    ],
-            delete: '/Applications/Blue Jeans.app'
+  pkg 'BluejeansInstaller.pkg'
+
+  uninstall signal:  [
+                       ['TERM', 'com.bluejeansnet.Blue'],
+                       ['TERM', 'com.bluejeansnet.BlueMenulet'],
+                     ],
+            pkgutil: 'com.bluejeansnet.*'
 end
