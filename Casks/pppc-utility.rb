a22c873b3a8d94db33965325f85a9ec42a8b51dc
David Rosenberg <targendaz2@me.com>
2020-06-11 21:15:55 -0400

    Add PPPC Utility.app Latest

---
+++ Casks/pppc-utility.rb
@@ -0,0 +1,14 @@
+cask 'pppc-utility' do
+  version '1.2.0'
+  sha256 '58ed98d1ca749bec88651c62ff0e4bb2550e84ba441cd0df7d3225059eb66c61'
+
+  url "https://github.com/jamf/PPPC-Utility/releases/download/#{version}/PPPC-Utility.zip"
+  appcast 'https://github.com/jamf/PPPC-Utility/releases'
+  name 'PPPC Utility'
+  name 'Privacy Preferences Policy Control Utility'
+  homepage 'https://github.com/jamf/PPPC-Utility'
+
+  app 'PPPC Utility.app'
+
+  uninstall quit: 'com.jamf.opensource.pppcutility'
+end
