e87a8efc6b9beec3004d988d7825d40292e6f9ce
Martin Senebald <martin@senebald.de>
2020-06-13 13:46:56 +0200

    updated clickup from 2.0.8 to 2.0.11

--- Casks/clickup.rb
+++ Casks/clickup.rb
@@ -1,6 +1,6 @@
 cask 'clickup' do
-  version '2.0.8'
-  sha256 '79bda23295db0e9c66fc7a72dc3a1226a8620384feaeacba6539bb4ed7eb092e'
+  version '2.0.11'
+  sha256 'c71edac3c19323d688e3fe75a6e2e0b8c6bd58bebd9bdb45631d06d03e340992'
 
   url "https://attachments.clickup.com/desktop/clickup-desktop-#{version}-mac.dmg"
   name 'ClickUp'
