078b015690c1e314ca29ce6f900019bb1492aa2e
Glenn 'devalias' Grant <glenn@devalias.net>
2020-06-13 14:10:58 +1000

    fix native-access download url error
    Before:
    ```
    ⇒  brew cask install native-access
    ==> Downloading https://native-instruments.com/fileadmin/downloads/Native_Access_Installer.dmg
    ##O#- #
    curl: (60) SSL certificate problem: certificate has expired
    More details here: https://curl.haxx.se/docs/sslcerts.html
    
    curl failed to verify the legitimacy of the server and therefore could not
    establish a secure connection to it. To learn more about this situation and
    how to fix it, please visit the web page mentioned above.
    Error: Download failed on Cask 'native-access' with message: Download failed: https://native-instruments.com/fileadmin/downloads/Native_Access_Installer.dmg
    ```
    
    After:
    ```
    ⇒  brew cask install native-access
    ==> Downloading https://www.native-instruments.com/fileadmin/downloads/Native_Access_Installer.dmg
    ######################################################################## 100.0%
    ==> Verifying SHA-256 checksum for Cask 'native-access'.
    ==> Installing Cask native-access
    ==> Moving App 'Native Access.app' to '/Applications/Native Access.app'.
    🍺  native-access was successfully installed!
    ```

--- Casks/native-access.rb
+++ Casks/native-access.rb
@@ -2,7 +2,7 @@ cask 'native-access' do
   version '1.12.1.129'
   sha256 '21d7f6cde1a173c12392e323bf8d23e51460f389e38552c367bb8ab41d61f20e'
 
-  url 'https://native-instruments.com/fileadmin/downloads/Native_Access_Installer.dmg'
+  url 'https://www.native-instruments.com/fileadmin/downloads/Native_Access_Installer.dmg'
   appcast "https://native-instruments.com/forum/threads/official-update-status-native-access-current-version-#{version.major_minor_patch.dots_to_hyphens}.313999",
           must_contain: version.major_minor_patch
   name 'Native Access'
