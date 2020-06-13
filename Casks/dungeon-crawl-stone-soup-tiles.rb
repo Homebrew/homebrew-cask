626cb0b24f05f4dd347f814d932f14af93279b55
suschizu <brigitte@corecode.io>
2020-06-13 19:14:17 +0200

    Update dungeon-crawl-stone-soup-tiles.rb

--- Casks/dungeon-crawl-stone-soup-tiles.rb
+++ Casks/dungeon-crawl-stone-soup-tiles.rb
@@ -1,8 +1,9 @@
 cask 'dungeon-crawl-stone-soup-tiles' do
-  version '0.24.0'
-  sha256 'f95146f701d5412b73fdd7d0369e16fc3158ce70d3892ed603e953266bbe2d68'
+  version '0.25.0'
+  sha256 '81034a2a22bab5d727d0c8e160e53b8b4e22d6cc76441bfc64455f61d870da5a'
 
-  url "https://crawl.develz.org/release/#{version.major_minor}/stone_soup-#{version}-tiles-macosx.zip"
+  # github.com/crawl/crawl/releases was verified as official when first introduced to the cask
+  url "https://github.com/crawl/crawl/releases/download/#{version}/dcss-#{version}-macos-tiles.zip"
   appcast 'https://github.com/crawl/crawl/releases.atom'
   name 'Dungeon Crawl Stone Soup'
   homepage 'https://crawl.develz.org/'
