aab21dea1869ebe9533ad9ac2821f241dbf42205
Randall <ran.dall@icloud.com>
2020-06-12 18:30:41 -0700

    Update adobe-dng-converter from 12.2 to 12.3

--- Casks/adobe-dng-converter.rb
+++ Casks/adobe-dng-converter.rb
@@ -1,6 +1,6 @@
 cask 'adobe-dng-converter' do
-  version '12.2'
-  sha256 'cc2962dd051c29bb2c4737a831477be5b0f35531f0fbc1ea2c10c2449201f0f7'
+  version '12.3'
+  sha256 '1cfac471868ad6032d53207dfddf92d6f2f2e8ec2aeb2a0d1b2a9b016956cea8'
 
   url "https://download.adobe.com/pub/adobe/dng/mac/DNGConverter_#{version.dots_to_underscores}.dmg"
   appcast 'https://supportdownloads.adobe.com/product.jsp?product=106&platform=Macintosh'
