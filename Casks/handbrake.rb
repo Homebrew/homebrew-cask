6949dee8bc9dfcc46c0b16f5dd3fa0c5488f0107
David Yantis <404NetworkError@users.noreply.github.com>
2020-06-13 19:05:05 -0400

    Update handbrake from 1.3.2 to 1.3.3 (#84321)

--- Casks/handbrake.rb
+++ Casks/handbrake.rb
@@ -1,6 +1,6 @@
 cask 'handbrake' do
-  version '1.3.2'
-  sha256 'f3680d583a1c1ebe0e0a5316ee22d224d48cb3c5be14af29b14ab9071744184f'
+  version '1.3.3'
+  sha256 '500cb924de7a3cc3a95cac4e69f303fc0623d3524fe59ac4ee4160ff43d56af6'
 
   # github.com/HandBrake/HandBrake/ was verified as official when first introduced to the cask
   url "https://github.com/HandBrake/HandBrake/releases/download/#{version}/HandBrake-#{version}.dmg"
