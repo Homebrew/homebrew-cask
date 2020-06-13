0cf9fddc0c1348773cadce741f42df826e06b835
Maurice <22131101+MJECloud@users.noreply.github.com>
2020-06-13 17:48:29 +0200

    Update 1password-cli from 1.0.0 to 1.1.0 (#84310)

--- Casks/1password-cli.rb
+++ Casks/1password-cli.rb
@@ -1,6 +1,6 @@
 cask '1password-cli' do
-  version '1.0.0'
-  sha256 '0f789498ca40163ffacca78d541ffc6204f7669def7722cf3f156ba575c365e9'
+  version '1.1.0'
+  sha256 'b4f4287c98dca3bde4f223df02e6c14a3d3abbd3db0a84b33bfdbbb4a4587ef3'
 
   # cache.agilebits.com/dist/1P/op/pkg/ was verified as official when first introduced to the cask
   url "https://cache.agilebits.com/dist/1P/op/pkg/v#{version}/op_darwin_amd64_v#{version}.pkg"
