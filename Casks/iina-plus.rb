0b02522569c576d0ae076ae836d90c67fa41a7b8
suschizu <brigitte@corecode.io>
2020-06-13 12:59:01 +0200

    Update iina-plus.rb

--- Casks/iina-plus.rb
+++ Casks/iina-plus.rb
@@ -1,11 +1,11 @@
 cask 'iina-plus' do
-  version '0.4.14,20040415'
-  sha256 '5429313015620e880c16dc30c6b5b4f9e187394914710f8b078f17e2aea42d50'
+  version '0.4.15,20061218'
+  sha256 'b8dfa5899f0f00e570484e951769a3f8400cba1c13f2ee6ae2bfc054a6a62d8d'
 
   url "https://github.com/xjbeta/iina-plus/releases/download/#{version.before_comma}(#{version.after_comma})/iina+.#{version.before_comma}.zip"
   appcast 'https://github.com/xjbeta/iina-plus/releases.atom'
   name 'IINA+'
   homepage 'https://github.com/xjbeta/iina-plus'
 
-  app 'iina+.app'
+  app "iina+ #{version.before_comma}.app"
 end
