e706139ea556e84136f4ee94b778da71b29cbcfb
Randall <ran.dall@icloud.com>
2020-06-12 18:08:02 -0700

    Update litecoin from 0.17.1 to 0.18.1

--- Casks/litecoin.rb
+++ Casks/litecoin.rb
@@ -1,6 +1,6 @@
 cask 'litecoin' do
-  version '0.17.1'
-  sha256 '7f8ec34706701482970baae4d4bc0fdb19016a6ab5260f983bf478428f0da518'
+  version '0.18.1'
+  sha256 'b81d9101c6ecb38b7699cf3d05ab57df7922f40f23c8a3377750c335d7102266'
 
   url "https://download.litecoin.org/litecoin-#{version}/osx/litecoin-#{version}-osx.dmg"
   appcast 'https://github.com/litecoin-project/litecoin/releases.atom'
