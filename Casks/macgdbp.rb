ffa88be1576da8edd98458ab14d182f36205fa47
Randall <ran.dall@icloud.com>
2020-06-13 17:01:35 -0700

    Update macgdbp from 2.0.2 to 2.0.3

--- Casks/macgdbp.rb
+++ Casks/macgdbp.rb
@@ -1,6 +1,6 @@
 cask 'macgdbp' do
-  version '2.0.2'
-  sha256 '438ea8cdbc4fc35b6cc80888e27a4e290f8c7023decc155227eed373ff2d0cfe'
+  version '2.0.3'
+  sha256 '1f80a3868ccb1023c602337a925b964aebde9da7c90f3c27679f5becd5c6d071'
 
   url "https://www.bluestatic.org/downloads/macgdbp/macgdbp-#{version}.zip"
   appcast 'https://www.bluestatic.org/versioncast.php/macgdbp'
