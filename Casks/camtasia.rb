ed6bbef94a6b5894ab1697edd687ab65139334e6
Bernd Krannich <berndkrannich@users.noreply.github.com>
2020-06-14 19:17:50 +0200

    Update camtasia (#84364)
    Co-authored-by: Bernd Krannich <bernd@krannich.net>

--- Casks/camtasia.rb
+++ Casks/camtasia.rb
@@ -1,6 +1,6 @@
 cask 'camtasia' do
   version '2020.0.4'
-  sha256 '19b68b56650a2b5f1dc6359c3dba464216c9a2a7588fa9efe3f1a1b9daf834df'
+  sha256 '1484cd3e0b92eef8f8013902e68d1fb0f9c22273a7e37a6aeae3934c085d369f'
 
   url 'https://download.techsmith.com/camtasiamac/releases/Camtasia.dmg'
   appcast 'https://support.techsmith.com/hc/en-us/articles/115006624748-Camtasia-Mac-Version-History'
