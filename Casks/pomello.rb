4a3899fabb0d56640447365b2a49630dce8e2433
Randall <ran.dall@icloud.com>
2020-06-13 17:09:15 -0700

    Update pomello from 0.10.13 to 0.10.14

--- Casks/pomello.rb
+++ Casks/pomello.rb
@@ -1,6 +1,6 @@
 cask 'pomello' do
-  version '0.10.13'
-  sha256 '7b9ca00db04be017c7fdce3e7fd3384f12dd3a85a53f164d332f233c6614744f'
+  version '0.10.14'
+  sha256 'a4082996e0fb71af673e11b55b73efaa6602bd0acbc7b895a39f9d4e28dce0b7'
 
   url 'https://pomelloapp.com/download/mac/latest'
   appcast 'https://pomelloapp.com/download/mac'
