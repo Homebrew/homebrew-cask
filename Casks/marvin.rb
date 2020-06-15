b35ea85c1ea46c29b4f0cdb1042b4609bdfcd73e
suschizu <brigitte@corecode.io>
2020-06-15 23:43:51 +0200

    Update marvin (#84406)

--- Casks/marvin.rb
+++ Casks/marvin.rb
@@ -1,6 +1,6 @@
 cask 'marvin' do
   version '1.52.1'
-  sha256 'b13e2649fcb309b549fc3bb86b150e7f0eb86fd56fd323133120e7f6c1f1aea2'
+  sha256 '4ac58324f4f527137e9dee1452212e0a690cd153cd0c7a642ede5349d5d4e7d2'
 
   # amazingmarvin.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://amazingmarvin.s3.amazonaws.com/Marvin-#{version}.dmg"
