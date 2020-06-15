1603c7419075bcfc77184a9b2995d1621de1e064
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-14 19:04:43 -0700

    Fix `verison` stanza on `isyncr` (#84372)

--- Casks/isyncr.rb
+++ Casks/isyncr.rb
@@ -1,10 +1,7 @@
 cask 'isyncr' do
-  if MacOS.version <= :lion
-    version '5.0.8'
-    sha256 '8cd6b1c96a902d8810e52aab6a980424370237617bfd3ff574367ff1ce8d4f4e'
-    elseif MacOS.version <= :mojave
+  if MacOS.version <= :mojave
     version '5.14.11'
-    sha256 '3952db5c64873cf63a5bfb6b308fd992f2eeb67cf838955fc200cd59640803aa'
+    sha256 'b234a1de565854fc9cdfefba9b2f05887e150ec01612bbfadaa0a0d95566034e'
   else
     version '6.0.3'
     sha256 'c7033eb946a6a6104a75cc5c182506f47a0399b54f3b4ce486e82a1c7d040154'
