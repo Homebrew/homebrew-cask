0e685c0f658f72087aec1796846716e1c32e3b57
John Williams <john@veloshots.com>
2020-06-15 13:34:07 -0500

    Fix comment about s3 url

--- Casks/hey.rb
+++ Casks/hey.rb
@@ -2,7 +2,7 @@ cask 'hey' do
   version '1.0.4'
   sha256 'f484d461b51c4f15f776baeeb3c3ab9deceb64a14a1b976471e2784b818e68c7'
 
-  # github.com/fitztrev/shuttle was verified as official when first introduced to the cask
+  # hey-desktop.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://hey-desktop.s3.amazonaws.com/HEY-#{version}.dmg"
   name 'Hey'
   homepage 'https://hey.com/'
