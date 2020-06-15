9feddedf2135c6d6563c3291e1c9872fc98b8448
suschizu <brigitte@corecode.io>
2020-06-15 03:23:21 +0200

    Update qsync-client from 3.4.0.0416 to 3.3.7.0514 (#84357)

--- Casks/qsync-client.rb
+++ Casks/qsync-client.rb
@@ -1,6 +1,6 @@
 cask 'qsync-client' do
-  version '3.4.0.0416'
-  sha256 '704638070a9e557682ca19c1aa62f6496fdc86ff3aa918b55d32ade859415aad'
+  version '3.3.7.0514'
+  sha256 '97d7813e1c2a171d07355969236719e7a70e5e475522ea840e87594ab8ea478b'
 
   url "https://download.qnap.com/Storage/Utility/QNAPQsyncClientMac-#{version}.dmg"
   appcast 'https://update.qnap.com/SoftwareRelease.xml'
