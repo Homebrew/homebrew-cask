0915d5ec6ed3c799aa6d2e856799c2d23eba8236
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-15 15:50:30 -0700

    Update meta from 1.9.5 to 1.9.6 (#84418)

--- Casks/meta.rb
+++ Casks/meta.rb
@@ -1,6 +1,6 @@
 cask 'meta' do
-  version '1.9.5'
-  sha256 '0d7a5e08a20a5f6911f1d195719a20d287ce129ba824b2216fc9d18d9f626e21'
+  version '1.9.6'
+  sha256 '128c08df7852bb662648fabfedb8f214b7948de6b865b806dbe94a48480f3811'
 
   url "https://www.nightbirdsevolve.com/meta/updates/bin/Meta%20#{version}.zip"
   appcast 'https://www.nightbirdsevolve.com/meta/updates/'
