9faa57a1f3b409588f53883bbf994757a9c7fbad
John Williams <john@veloshots.com>
2020-06-15 13:21:16 -0500

    Add Hey Mail App

---
+++ Casks/hey.rb
@@ -0,0 +1,11 @@
+cask 'hey' do
+  version '1.0.4'
+  sha256 'f484d461b51c4f15f776baeeb3c3ab9deceb64a14a1b976471e2784b818e68c7'
+
+  # github.com/fitztrev/shuttle was verified as official when first introduced to the cask
+  url "https://hey-desktop.s3.amazonaws.com/HEY-#{version}.dmg"
+  name 'Hey'
+  homepage 'https://hey.com/'
+
+  app 'Hey.app'
+end
