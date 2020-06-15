d90b7012d168ff4b718ed328be6fda4593d0b5c8
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-15 15:50:01 -0700

    Update rowmote-helper from 4.2.2 to 4.2.3 (#84419)

--- Casks/rowmote-helper.rb
+++ Casks/rowmote-helper.rb
@@ -1,6 +1,6 @@
 cask 'rowmote-helper' do
-  version '4.2.2'
-  sha256 '705223df2b4a655a1d414a369e1d68176617a4a2819a4d6faa615ff7cef040b1'
+  version '4.2.3'
+  sha256 '08a6803c66e55032252a2f946316cdff99f81f168a6c2e12f724ad37ffef1373'
 
   url "https://regularrateandrhythm.com/rowmote-pro/rh/rowmote-helper-#{version}.zip"
   appcast 'https://www.regularrateandrhythm.com/apps/rowmote-pro/rowmote-appcast.xml'
