25a6ba2d6e2787378904aa8291f0c89d62fbd7bf
Brigitte Zmoelnig <brigitte@corecode.io>
2020-06-15 23:44:25 +0200

    Update sia-ui from 1.4.7 to 1.4.11

--- Casks/sia-ui.rb
+++ Casks/sia-ui.rb
@@ -1,6 +1,6 @@
 cask 'sia-ui' do
-  version '1.4.7'
-  sha256 '041a6a9bba98db7ecde27a2ce46ddf1f129dd8dba72bc59b732b096e13334330'
+  version '1.4.11'
+  sha256 '93287c0c427522415346e8b79de5567f40017625bc2a7f920305e93e7b85cd61'
 
   url "https://sia.tech/releases/Sia-UI-v#{version}.dmg"
   name 'Sia-UI'
