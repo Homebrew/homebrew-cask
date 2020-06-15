3a4338340a8bf7a84297b3d5b4b0ce05d1b0eb69
Ganesh Rathinavel <ganeshrvel@outlook.com>
2020-06-15 17:38:13 +0530

    Updated the version

--- Casks/openmtp.rb
+++ Casks/openmtp.rb
@@ -1,6 +1,6 @@
 cask 'openmtp' do
-  version '2.4.1'
-  sha256 'e1e5465fc493278fd617887aef10b680c3b2ec8b2b3b989b46707477c949a3ce'
+  version '2.4.4'
+  sha256 '25423e374b0c50763670a5368ecea19c01789906d31f14d02a1501d541ed31de'
 
   # github.com/ganeshrvel/openmtp/ was verified as official when first introduced to the cask
   url "https://github.com/ganeshrvel/openmtp/releases/download/v#{version}/OpenMTP-#{version}-mac.zip"
