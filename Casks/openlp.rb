61ce76c7fab1e3898d0fceae196d9c8bf6527f81
suschizu <brigitte@corecode.io>
2020-06-15 23:32:33 +0200

    Update openlp (#84404)

--- Casks/openlp.rb
+++ Casks/openlp.rb
@@ -1,6 +1,6 @@
 cask 'openlp' do
   version '2.9.1'
-  sha256 '1ab87856634a8761d6598e1b4f2c468c3b32676f8e35f63f322a54b803bf823f'
+  sha256 'c01ff0a446716048c9f2e19b13ebf124a2d7fadcd6d73273e8d41cf60326c2c5'
 
   url "https://get.openlp.org/#{version}/OpenLP-#{version}.dmg"
   appcast 'https://get.openlp.org'
