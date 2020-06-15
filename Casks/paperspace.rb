3cef4b53bf61e962c160ecec755b0795dc2f9a77
Brigitte Zmoelnig <brigitte@corecode.io>
2020-06-15 23:40:41 +0200

    Update paperspace

--- Casks/paperspace.rb
+++ Casks/paperspace.rb
@@ -1,6 +1,6 @@
 cask 'paperspace' do
   version '8.4.1.0'
-  sha256 '99defd065b5d9afc8838934cc7b3e84792f967aacab5f81ace3b3e0f7da8e0b0'
+  sha256 '6b425ada9f9a9696a1925e7fc8ccfaca4e1db2d7fc2077618cc3e58626bede34'
 
   # ps-receiver.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://ps-receiver.s3.amazonaws.com/darwin/Paperspace-#{version}.dmg"
