2f1de0eb1da9ed434701995b53f089bd71d95aff
Brigitte Zmoelnig <brigitte@corecode.io>
2020-06-15 23:25:35 +0200

    Update metashape from 1.6.3 to 1.6.3,10732

--- Casks/metashape.rb
+++ Casks/metashape.rb
@@ -1,8 +1,8 @@
 cask 'metashape' do
-  version '1.6.3'
-  sha256 '08fad4f115d11f1c486c006ae4bfcb8342c58582e355cda5e5bc9fd59a9d1aa3'
+  version '1.6.3,10732'
+  sha256 '5a5c8336805edf21ebb69f54afa1a8b4d39c0ee5bb56de1d0b8ca216d873bae2'
 
-  url "http://download.agisoft.com/metashape_#{version.dots_to_underscores}.dmg"
+  url "http://download.agisoft.com/metashape_#{version.before_comma.dots_to_underscores}.dmg"
   appcast 'https://www.agisoft.com/downloads/installer/'
   name 'Agisoft Metashape Standard Edition'
   homepage 'https://www.agisoft.com/'
