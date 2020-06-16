5b162e3be433a864f5799d6a323eb6ccab37a570
suschizu <brigitte@corecode.io>
2020-06-16 13:18:55 +0200

    Update protopie.rb

--- Casks/protopie.rb
+++ Casks/protopie.rb
@@ -3,8 +3,7 @@ cask 'protopie' do
   sha256 'e0b1ace8506eba1d9c08c0532282c00e533dadd9114fcbc05829ac9928995a51'
 
   url "https://release.protopie.io/ProtoPie-#{version}.dmg"
-  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.protopie.io/darwin/latest
-'
+  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.protopie.io/darwin/latest'
   name 'ProtoPie'
   homepage 'https://www.protopie.io/'
 
