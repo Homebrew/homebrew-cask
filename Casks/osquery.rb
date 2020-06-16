b1b2404f021962214a17c880d732d8d00f392af7
Peter Lewis <peter@peterlew.is>
2020-06-16 11:50:57 +0100

    Add 'osquery'
    Add 'osquery' cask as osquery 4.x no longer ships through brew.

---
+++ Casks/osquery.rb
@@ -0,0 +1,19 @@
+cask 'osquery' do
+  version '4.3.0'
+  sha256 '197eae9624ec830bf3fec53b79b11e084792a04a461c3241d95cf6aebd3e6ac2'
+
+  url "https://pkg.osquery.io/darwin/osquery-#{version}.pkg"
+  name 'osquery'
+  homepage 'https://osquery.io/'
+
+  pkg "osquery-#{version}.pkg"
+
+  uninstall delete:    [
+                         '/private/var/log/osquery',
+                         '/private/var/osquery',
+                         '/usr/local/bin/osqueryctl',
+                         '/usr/local/bin/osqueryd',
+                       ],
+            launchctl: 'com.facebook.osqueryd',
+            pkgutil:   'com.facebook.osquery'
+end
