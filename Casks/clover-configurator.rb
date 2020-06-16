a9b8f6bbf2da1d6c60b7c40be2345ece3d91383a
Zhenfu Shi <szf1234@me.com>
2020-06-16 13:34:21 -0400

    Update clover-configurator from 5.14.0.0 to 5.14.1.0 (#84451)

--- Casks/clover-configurator.rb
+++ Casks/clover-configurator.rb
@@ -1,6 +1,6 @@
 cask 'clover-configurator' do
-  version '5.14.0.0'
-  sha256 '2c48e39d2e1d1cc698f5a6a274ca72c3e1b3d064f0caf9a639095a024dc52c85'
+  version '5.14.1.0'
+  sha256 '75c6f622a7e04ab7ae9ef839397fe8b93478cb9aeee8e0e923eee1bb00f67fb4'
 
   url 'https://mackie100projects.altervista.org/apps/cloverconf/download-new-build.php?version=global'
   appcast 'https://mackie100projects.altervista.org/apps/cloverconf/CCG/update-data-builds.xml'
