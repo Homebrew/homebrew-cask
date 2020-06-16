4b76ca73195f6e6cf81025a825ecac538330a9a3
suschizu <brigitte@corecode.io>
2020-06-16 14:27:54 +0200

    Update protopie from 4.3.3 to 5.0.0 (#84441)
    * Update protopie from 4.3.3 to 5.0.0
    
    * Update protopie.rb
    
    * Update protopie.rb

--- Casks/protopie.rb
+++ Casks/protopie.rb
@@ -1,9 +1,9 @@
 cask 'protopie' do
-  version '4.3.3'
-  sha256 '4079a25137ecf854154270cb6a9e6f9bedbdcfd37df869e20547fc5fcb9bc107'
+  version '5.0.0'
+  sha256 'e0b1ace8506eba1d9c08c0532282c00e533dadd9114fcbc05829ac9928995a51'
 
   url "https://release.protopie.io/ProtoPie-#{version}.dmg"
-  appcast 'https://www.protopie.io/support/updates/'
+  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.protopie.io/darwin/latest'
   name 'ProtoPie'
   homepage 'https://www.protopie.io/'
 
