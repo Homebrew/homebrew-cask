56935222f21885900b3fca775453cbb956e909b9
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2020-06-16 12:58:09 +0200

    Audit fixes

--- Casks/micronaut.rb
+++ Casks/micronaut.rb
@@ -5,6 +5,7 @@ cask 'micronaut' do
   # github.com/micronaut-projects/micronaut-starter was verified as official when first introduced to the cask
   url "https://github.com/micronaut-projects/micronaut-starter/releases/download/v#{version}/mn-darwin-amd64-v#{version}.zip"
   appcast 'https://github.com/micronaut-projects/micronaut-starter/releases.atom'
+  name 'Micronaut'
   homepage 'https://micronaut.io/'
 
   depends_on formula: 'openjdk'
