4d5dd0db69d8723ec88eaca2fc08d0cdf7e91bb2
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2020-06-16 12:42:25 +0200

    Add Micronaut CLI 2.0.0.M3

---
+++ Casks/micronaut.rb
@@ -0,0 +1,14 @@
+cask 'micronaut' do
+  version '2.0.0.M3'
+  sha256 '15a748072060263c0eee2b2bb808c3eed75516066542fcd2d1bccb39ae3e64e5'
+
+  # github.com/micronaut-projects/micronaut-starter was verified as official when first introduced to the cask
+  url "https://github.com/micronaut-projects/micronaut-starter/releases/download/v#{version}/mn-darwin-amd64-v#{version}.zip"
+  name 'Micronaut'
+  homepage 'https://micronaut.io'
+  appcast 'https://github.com/micronaut-projects/micronaut-starter/releases.atom'
+
+  depends_on formula: 'openjdk'
+
+  binary "mn-darwin-amd64-v#{version}/bin/mn"
+end
