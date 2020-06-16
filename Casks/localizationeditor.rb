7ef92ae42e1c0ff37b8fafab7d4c5d902f3a5c19
Igor Kulman <igor@kulman.sk>
2020-06-16 22:34:37 +0200

    Update localizationeditor from 2.5 to 2.6

--- Casks/localizationeditor.rb
+++ Casks/localizationeditor.rb
@@ -1,6 +1,6 @@
 cask 'localizationeditor' do
-  version '2.5'
-  sha256 '5f6b196e74e9040db506b90c6a72ce5bfea3a19d7e911377c7c4c5d0167305ba'
+  version '2.6'
+  sha256 '113eba2fff6939a539d01c8e6c3cde48af1e07546bf9bf60d566a4b456ece763'
 
   url "https://github.com/igorkulman/iOSLocalizationEditor/releases/download/v#{version}/LocalizationEditor.app.zip"
   appcast 'https://github.com/igorkulman/iOSLocalizationEditor/releases.atom'
