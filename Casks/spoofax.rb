5b102a58df07140d32076d865be20ba6abc2a894
Bruce Collie <brucecollie82@gmail.com>
2020-06-17 11:08:31 +0100

    Add Spoofax v2.5.9
    Spoofax is a set of tools implementing a programming language
    development workbench. This cask installs the stable Spoofax
    distribution.

---
+++ Casks/spoofax.rb
@@ -0,0 +1,10 @@
+cask 'spoofax' do
+  version '2.5.9'
+  sha256 '62dc7c3a9052acaa3bdb8524279859b9c9a75d7b68a9202cf242aba6b2ad5860'
+
+  url 'https://artifacts.metaborg.org/service/local/repositories/releases/content/org/metaborg/org.metaborg.spoofax.eclipse.dist/2.5.9/org.metaborg.spoofax.eclipse.dist-2.5.9-macosx-x64-jre.tar.gz'
+  name 'Spoofax'
+  homepage 'https://www.metaborg.org/'
+
+  app 'spoofax.app'
+end
