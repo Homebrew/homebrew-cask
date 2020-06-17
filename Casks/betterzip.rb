48141fc1c639885b1f8c2dedd1c88c5d2b996542
suschizu <brigitte@corecode.io>
2020-06-17 22:49:38 +0200

    Update betterzip from 4.2.5 to 5.0 (#84538)

--- Casks/betterzip.rb
+++ Casks/betterzip.rb
@@ -1,6 +1,6 @@
 cask 'betterzip' do
-  version '4.2.5'
-  sha256 '6fda66723dfacba7d7ffbf1817e06031aa7faeec586cd11b9619db28940fa179'
+  version '5.0'
+  sha256 '87c3e5be2fbf9fae630e0e5d307fdb47da81a6a351840fac4db23300fd27a984'
 
   # macitbetter.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://macitbetter.s3.amazonaws.com/BetterZip-#{version}.zip"
