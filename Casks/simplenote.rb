0a474494adaeb5c4734f2a1ef743101d46df4339
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-16 19:19:32 -0700

    Update simplenote from 1.17.0 to 1.18.0 (#84467)

--- Casks/simplenote.rb
+++ Casks/simplenote.rb
@@ -1,6 +1,6 @@
 cask 'simplenote' do
-  version '1.17.0'
-  sha256 'b427ff4471b4c662a46084f88bd0817c53aabe9e304da95fc08313c60cdddf47'
+  version '1.18.0'
+  sha256 'b1681a3b8370a6ac4c49b6fadf9a2d43db391e2048071beea581406be14306f4'
 
   url "https://github.com/Automattic/simplenote-electron/releases/download/v#{version}/Simplenote-macOS-#{version}.dmg"
   appcast 'https://github.com/Automattic/simplenote-electron/releases.atom'
