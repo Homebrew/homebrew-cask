3a57876d1971cdab8dd3936e37106ebcb3d9c7e1
Vítor Galvão <info@vitorgalvao.com>
2020-06-17 20:17:35 +0100

    Update keyboardcleantool.rb

--- Casks/keyboardcleantool.rb
+++ Casks/keyboardcleantool.rb
@@ -2,7 +2,6 @@ cask 'keyboardcleantool' do
   version :latest
   sha256 :no_check
 
-  # formerly boastr.net, then bettertouchtool.net - now folivora.ai
   url 'https://folivora.ai/releases/KeyboardCleanTool.zip'
   name 'KeyboardCleanTool'
   homepage 'https://folivora.ai/keyboardcleantool'
