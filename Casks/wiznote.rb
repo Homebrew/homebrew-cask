db8ca1818b2f5f226a5b1f3025280f85943db370
suschizu <brigitte@corecode.io>
2020-06-17 13:48:59 +0200

    Update wiznote from 2.8.5,2020-04-22 to 2.8.6,2020-06-15 (#84516)

--- Casks/wiznote.rb
+++ Casks/wiznote.rb
@@ -1,6 +1,6 @@
 cask 'wiznote' do
-  version '2.8.5,2020-04-22'
-  sha256 'fc851924922d6a655e1c47091471024247d25c79a677638d5f940e3a986de988'
+  version '2.8.6,2020-06-15'
+  sha256 '64d4e6bc8cfdd96d62384cf741a3c6b4c6323b0c38da8ebc857890f377521151'
 
   url "https://get.wiz.cn/wiznote-macos-#{version.after_comma}.dmg"
   appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://url.wiz.cn/u/mac',
