fd7695913174c9e857bc12075bf621149c27ce85
Julian Mayer <julian@corecode.io>
2020-06-18 16:14:34 +0200

    Update eclipse-php from 4.15.0,2020-03:R to 4.16.0,2020-06:R

--- Casks/eclipse-php.rb
+++ Casks/eclipse-php.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-php' do
-  version '4.15.0,2020-03:R'
-  sha256 '36f185c4ca1d4aa6b509ab25f0a145d686a197832857598c9d302b76ea11061a'
+  version '4.16.0,2020-06:R'
+  sha256 'f8cf367de4937397f24da6e5efcb6446b75d2a54ed6ec1bb564f2e31a9760cac'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-php-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse IDE for PHP Developers'
