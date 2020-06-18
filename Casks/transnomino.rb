32e20ae8b01e032453df17a1207b5eb9ae7e335c
Vítor Galvão <info@vitorgalvao.com>
2020-06-18 11:50:11 +0100

    Update transnomino.rb

--- Casks/transnomino.rb
+++ Casks/transnomino.rb
@@ -3,6 +3,7 @@ cask 'transnomino' do
   sha256 'f19444fec6076fa4165d79a4c931ee023651502426df4beb46ff5994c6958426'
 
   url 'https://transnomino.bastiaanverreijt.com/download/Transnomino-4.0.dmg'
+  appcast 'https://transnomino.bastiaanverreijt.com'
   name 'Transnomino'
   homepage 'https://transnomino.bastiaanverreijt.com/'
 
