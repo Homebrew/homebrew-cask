b9d94370c3f59c6b001a28f50e95eb18e83e941a
Randall <ran.dall@icloud.com>
2020-06-17 21:04:20 -0700

    Update scaleft from 1.44.6 to 1.45.3

--- Casks/scaleft.rb
+++ Casks/scaleft.rb
@@ -1,6 +1,6 @@
 cask 'scaleft' do
-  version '1.44.6'
-  sha256 '67f54555b58cdc8165f9c40bc903ee5d706a60f058e27ace20a03a6a93a242fb'
+  version '1.45.3'
+  sha256 'b9597adfcceff20402893f64c142c7d10395814cd04c12578312e050ef762540'
 
   url "https://dist.scaleft.com/client-tools/mac/v#{version}/ScaleFT-#{version}.pkg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://dist.scaleft.com/client-tools/mac/latest/ScaleFT.pkg'
