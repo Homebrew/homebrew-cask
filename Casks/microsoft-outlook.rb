58171100afaee52f041e956a0690ec724fd9a432
LanikSJ <LanikSJ@users.noreply.github.com>
2020-06-17 21:46:12 -0700

    Update microsoft-outlook from 16.37.20051002 to 16.38.20061401 (#84504)

--- Casks/microsoft-outlook.rb
+++ Casks/microsoft-outlook.rb
@@ -1,6 +1,6 @@
 cask 'microsoft-outlook' do
-  version '16.37.20051002'
-  sha256 '21d9ef144fd964a6972ff4dd81b2cd47c768f365dc249d1604755c2c03b182ca'
+  version '16.38.20061401'
+  sha256 '50bdd2a61bf9e941f6df4a0abb56b4e3c39e63becc81c2e86a323d129d2e5b90'
 
   # officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/ was verified as official when first introduced to the cask
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Outlook_#{version}_Installer.pkg"
@@ -18,7 +18,8 @@ cask 'microsoft-outlook' do
                          'com.microsoft.package.Microsoft_Outlook.app',
                          'com.microsoft.pkg.licensing',
                        ],
-            launchctl: 'com.microsoft.office.licensingV2.helper'
+            launchctl: 'com.microsoft.office.licensingV2.helper',
+            quit:      'com.microsoft.autoupdate2'
 
   zap trash:     [
                    '~/Library/Application Scripts/com.microsoft.Outlook',
