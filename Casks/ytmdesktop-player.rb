1c976235b98b22d22c304f25d4364e483c2ce1d4
Adler Luiz <adlerluiz10@gmail.com>
2020-06-18 17:10:34 -0300

    Fix and improve

--- Casks/ytmdesktop-player.rb
+++ Casks/ytmdesktop-player.rb
@@ -1,9 +1,9 @@
-cask 'ytmdesktop' do
+cask 'ytmdesktop-player' do
   version '1.11.0'
   sha256 '2a6a0280d4242d5a0ae48e021999269dac8b5d755115ef2d0d09b521947d61e4'
 
   url "https://dl.ytmdesktop.app/mac/YouTube%20Music%20Desktop%20App-#{version}.dmg"
-  appcast 'https://ytmdesktop.app/#download'
+  appcast 'https://github.com/ytmdesktop/ytmdesktop/releases.atom'
   name 'YouTube Music Desktop App'
   homepage 'https://ytmdesktop.app/'
 
