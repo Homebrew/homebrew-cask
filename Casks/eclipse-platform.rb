f4c0c0b625cf6249ab9f90088217b74ffc9a8dfc
Julian Mayer <julian@corecode.io>
2020-06-18 16:15:07 +0200

    Update eclipse-platform from 4.15,202003050155 to 4.16.0,2020-06:R

--- Casks/eclipse-platform.rb
+++ Casks/eclipse-platform.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-platform' do
-  version '4.15,202003050155'
-  sha256 '41a4ab3d88895129d07ff702eac85542795967b8128c98a5e9204ffbb4f3395e'
+  version '4.16.0,2020-06:R'
+  sha256 '00bf1b44639bd24b38708eb90f32b43c9f96133d9f7c48cd056c26045f9f9744'
 
   url "https://www.eclipse.org/downloads/download.php?file=/eclipse/downloads/drops#{version.major}/R-#{version.before_comma}-#{version.after_comma}/eclipse-SDK-#{version.before_comma}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse SDK'
