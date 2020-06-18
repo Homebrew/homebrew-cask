772249bf08c880ff72c2cda165497362929b81a3
Santiago Maturana Arce <santi2912@icloud.com>
2020-06-18 14:08:16 +0200

    Fixed url stanza.

--- Casks/transnomino.rb
+++ Casks/transnomino.rb
@@ -2,10 +2,10 @@ cask 'transnomino' do
   version '4.0'
   sha256 'f19444fec6076fa4165d79a4c931ee023651502426df4beb46ff5994c6958426'
 
-  url 'https://transnomino.bastiaanverreijt.com/download/Transnomino-#{version}.dmg'
+  url "https://transnomino.bastiaanverreijt.com/download/Transnomino-#{version}.dmg"
   appcast 'https://transnomino.bastiaanverreijt.com'
   name 'Transnomino'
-  homepage 'https://transnomino.bastiaanverreijt.com'
+  homepage 'https://transnomino.bastiaanverreijt.com/'
 
   auto_updates true
 
