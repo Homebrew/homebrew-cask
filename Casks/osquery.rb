eb6907695f4238c453f33d76f44aa4b8320709bc
Peter Lewis <820394+peterlewis@users.noreply.github.com>
2020-06-18 05:50:30 +0100

    Add 'osquery' (#84440)
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

---
+++ Casks/osquery.rb
@@ -0,0 +1,14 @@
+cask 'osquery' do
+  version '4.3.0'
+  sha256 '197eae9624ec830bf3fec53b79b11e084792a04a461c3241d95cf6aebd3e6ac2'
+
+  url "https://pkg.osquery.io/darwin/osquery-#{version}.pkg"
+  appcast 'https://github.com/osquery/osquery/releases.atom'
+  name 'osquery'
+  homepage 'https://osquery.io/'
+
+  pkg "osquery-#{version}.pkg"
+
+  uninstall launchctl: 'com.facebook.osqueryd',
+            pkgutil:   'com.facebook.osquery'
+end
