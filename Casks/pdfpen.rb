3145f476435c9a0d44359b3d1bf1cb6ef65e27e8
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-17 21:31:58 -0700

    Update pdfpen from 1202.4,1589250891 to 1210.10,1592365651 (#84549)

--- Casks/pdfpen.rb
+++ Casks/pdfpen.rb
@@ -1,6 +1,6 @@
 cask 'pdfpen' do
-  version '1202.4,1589250891'
-  sha256 '4b9eb4880c5625a76af874734cc8034b56f8cfe0415cb5e9d06ea307da0512e7'
+  version '1210.10,1592365651'
+  sha256 'f63aa42ed2460b6aa44df477a1573c60f2b3b1794c3ae8443296d99e9f9ee879'
 
   url "https://dl.smilesoftware.com/com.smileonmymac.PDFpen/#{version.before_comma}/#{version.after_comma}/PDFpen-#{version.before_comma}.zip"
   appcast 'https://updates.smilesoftware.com/com.smileonmymac.PDFpen.xml'
