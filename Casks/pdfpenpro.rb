e0ee7907ca76431ef553835921a25ce56b74f555
Randall <ran.dall@icloud.com>
2020-06-17 21:01:54 -0700

    Update pdfpenpro from 1202.4,1589250721 to 1210.10,1592365188

--- Casks/pdfpenpro.rb
+++ Casks/pdfpenpro.rb
@@ -1,6 +1,6 @@
 cask 'pdfpenpro' do
-  version '1202.4,1589250721'
-  sha256 '13f51f7c547b28019c024ea2491f097bbb4b0f4a4066cea38bd8c37427241a05'
+  version '1210.10,1592365188'
+  sha256 '4d4a972b2435728663d23b3c4bdbad16608fee3934d8bd5fd9e0f50ceabf194e'
 
   url "https://dl.smilesoftware.com/com.smileonmymac.PDFpenPro/#{version.before_comma}/#{version.after_comma}/PDFpenPro-#{version.before_comma}.zip"
   appcast 'https://updates.smilesoftware.com/com.smileonmymac.PDFpenPro.xml'
