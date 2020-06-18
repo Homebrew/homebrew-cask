4084f9ae0f0f2a8c542a4c9d5e4b151397b45433
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2020-06-18 11:54:46 +0200

    Remove incubation from eclipse-jee url

--- Casks/eclipse-jee.rb
+++ Casks/eclipse-jee.rb
@@ -2,7 +2,7 @@ cask 'eclipse-jee' do
   version '4.16.0,2020-06:R'
   sha256 'f795b689a2cce4c10739daa1ba00688ae0362cdc579052d9c34f866ff3542343'
 
-  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-jee-#{version.after_comma.before_colon}-#{version.after_colon}-incubation-macosx-cocoa-x86_64.dmg&r=1"
+  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-jee-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse IDE for Java EE Developers'
   homepage 'https://eclipse.org/'
 
