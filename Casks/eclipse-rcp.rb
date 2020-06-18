133edad29714bb78e5fb24fd2cce3b8381ef0c5c
Julian Mayer <julian@corecode.io>
2020-06-18 16:17:43 +0200

    Update eclipse-rcp from 4.15.0,2020-03:R to 4.16.0,2020-06:R

--- Casks/eclipse-rcp.rb
+++ Casks/eclipse-rcp.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-rcp' do
-  version '4.15.0,2020-03:R'
-  sha256 '570e9088a9bd3522723b573c913c6af9909e1360a12f6cab8c5199a68a233a22'
+  version '4.16.0,2020-06:R'
+  sha256 'a2a0e8c2505e5ab75dd39d275dd16cc99c7e656ee22d6038e2efe54d5e46117e'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-rcp-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse for RCP and RAP Developers'
