a5b94c20918a55584badc5f9a141a4c447dd6e62
Adler Luiz <adlerluiz10@gmail.com>
2020-06-18 14:58:27 -0300

    Add ytmdesktop v1.11.0

---
+++ Casks/ytmdesktop.rb
@@ -0,0 +1,11 @@
+cask 'ytmdesktop' do
+  version '1.11.0'
+  sha256 '2a6a0280d4242d5a0ae48e021999269dac8b5d755115ef2d0d09b521947d61e4'
+
+  url "https://github.com/ytmdesktop/ytmdesktop/releases/download/v#{version}/YouTube.Music.Desktop.App-#{version}.dmg"
+  appcast 'https://ytmdesktop.app/#download'
+  name 'YouTube Music Desktop App'
+  homepage 'https://ytmdesktop.app'
+
+  app 'ytmdesktop.app'
+end
