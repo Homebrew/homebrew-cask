16d320f8271e43703994cc68197b5663ff2a18ff
Julian Mayer <julian@corecode.io>
2020-06-18 16:06:42 +0200

    Update eclipse-java from 4.15.0,2020-03:R to 4.16.0,2020-06:R

--- Casks/eclipse-java.rb
+++ Casks/eclipse-java.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-java' do
-  version '4.15.0,2020-03:R'
-  sha256 '262c29ea38134360089232d8276fb479e3a9f70a5b1a6ffdf6463bc2aeac00f3'
+  version '4.16.0,2020-06:R'
+  sha256 '538de6e0aa2ac26ff7edfef5ffe1913c92d95fd313219d1cb6107301a423eea5'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-java-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse IDE for Java Developers'
