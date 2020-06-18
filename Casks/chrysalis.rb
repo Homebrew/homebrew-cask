a07607f7d8c5a7a33f90e5d75870df8efbd4af00
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-17 21:06:36 -0700

    Update chrysalis from 0.7.5 to 0.7.6 (#84546)

--- Casks/chrysalis.rb
+++ Casks/chrysalis.rb
@@ -1,6 +1,6 @@
 cask 'chrysalis' do
-  version '0.7.5'
-  sha256 '0d96c3e05bc4e53dd74658fec531640771098f050480386c5411e11856cdfd0b'
+  version '0.7.6'
+  sha256 '18bd572c59bef1f4d39b21eabd615aa8d8e464b460531ae88a9852871fc9230b'
 
   url "https://github.com/keyboardio/Chrysalis/releases/download/chrysalis-#{version}/Chrysalis-#{version}.dmg"
   appcast 'https://github.com/keyboardio/Chrysalis/releases.atom'
