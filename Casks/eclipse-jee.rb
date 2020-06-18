78a3d244749096c211d87a527d775e939aa0cfe7
Julian Mayer <julian@corecode.io>
2020-06-18 16:09:36 +0200

    Update eclipse-jee from 4.15.0,2020-03:R to 4.16.0,2020-06:R

--- Casks/eclipse-jee.rb
+++ Casks/eclipse-jee.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-jee' do
-  version '4.15.0,2020-03:R'
-  sha256 '9fbe1d6b52f84ded8ee9dec0faf740f9934225abbc40262c1ef8c576c50bf830'
+  version '4.16.0,2020-06:R'
+  sha256 'f795b689a2cce4c10739daa1ba00688ae0362cdc579052d9c34f866ff3542343'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-jee-#{version.after_comma.before_colon}-#{version.after_colon}-incubation-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse IDE for Java EE Developers'
