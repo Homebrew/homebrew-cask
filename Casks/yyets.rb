7c9bb284ad29141d4cd6d420240db18420c92e5e
Ethan <word.of.ethan@gmail.com>
2020-06-19 22:09:17 +0800

    Update yyets.rb
    Update SHA256.

--- Casks/yyets.rb
+++ Casks/yyets.rb
@@ -1,6 +1,6 @@
 cask 'yyets' do
   version '3.2.7'
-  sha256 '84cc5d0b4ac9eab243da4a6a62bc64c35e9a99e62b6770ffbbf153bff0cee29b'
+  sha256 'a23306f3e53ea4672e525cfc9f46862b0ba473a258c0e48e5ef6fecd29a7b855'
 
   url "http://appdown.rrys.tv/RRShare_#{version}.dmg"
   appcast 'http://app.rrys.tv/'
