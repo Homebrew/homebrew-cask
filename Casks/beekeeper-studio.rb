e3689608b59480f6d5766957b38e709316b02928
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-18 20:50:18 -0700

    Update beekeeper-studio from 1.5.1 to 1.5.2 (#84621)

--- Casks/beekeeper-studio.rb
+++ Casks/beekeeper-studio.rb
@@ -1,6 +1,6 @@
 cask 'beekeeper-studio' do
-  version '1.5.1'
-  sha256 '9c03213d368f0cfafc7b9b9377536ae86d85b254f177265332b01413ec23cb93'
+  version '1.5.2'
+  sha256 'ecabab1e8cb27577d576b5fd7d180ce0a664637a9edfe8c20d8980e851ce94e9'
 
   # github.com/beekeeper-studio/beekeeper-studio/ was verified as official when first introduced to the cask
   url "https://github.com/beekeeper-studio/beekeeper-studio/releases/download/v#{version}/Beekeeper-Studio-#{version}.dmg"
