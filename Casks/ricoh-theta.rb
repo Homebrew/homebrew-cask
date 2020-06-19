e06d5d12ad240d6b188f1b1545efa73b10c1475d
Rio Fujita <rio_github@rio.st>
2020-06-19 13:20:11 +0900

    fix url and add appcast

--- Casks/ricoh-theta.rb
+++ Casks/ricoh-theta.rb
@@ -2,9 +2,10 @@ cask 'ricoh-theta' do
   version '3.13.3'
   sha256 '6903ff2fa7c38a59c0d5562c51dade624d3e8de6acf908f9bf1df31913e9accf'
 
-  url 'https://support.theta360.com/intl/download/pcapp/macosx'
+  url 'https://theta360-statics.s3.amazonaws.com/app/viewer/RICOH%20THETA.dmg'
   name 'RICOH THETA'
-  homepage 'https://theta360.com/ja/'
+  homepage 'https://theta360.com/'
+  appcast 'https://support.theta360.com/en/download/pcmac/'
 
   app 'RICOH THETA.app'
 end
