1ef6d3da91843fc28619b8ad8354b1bde9c1278b
Brigitte Zmoelnig <brigitte@corecode.io>
2020-06-19 23:31:28 +0200

    Update funter from 4.6.1,94 to 5.0

--- Casks/funter.rb
+++ Casks/funter.rb
@@ -4,10 +4,10 @@ cask 'funter' do
     sha256 'b059000717f7f8af67ee027d81aea6c2d9f343e0eb72e439a9f286ab265b7b93'
     url 'https://nektony.com/download/funter/funter_10.12.dmg'
   else
-    version '4.6.1,94'
-    sha256 'a9637ed819c73a9eb6aac7465597426a33268453f1bef5b8ba94463fda38f0eb'
-    url "https://nektony.com/promo/funter/update/update_#{version.before_comma}(#{version.after_comma}).zip"
-    appcast 'https://nektony.com/promo/funter/update/update.xml'
+    version '5.0'
+    sha256 '16320269bb384cdbe3d2c94fb7033e6479a576b31d0548d73d6f7587c1af6e34'
+    url 'https://nektony.com/download/funter/Funter.dmg'
+    appcast 'https://nektony.com/pro-support/funter/update/update.xml'
   end
 
   name 'Funter'
