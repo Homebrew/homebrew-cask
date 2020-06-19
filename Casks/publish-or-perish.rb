7eb506fba9b4e3934c472b7e2aed3d8eef3fd3ce
suschizu <brigitte@corecode.io>
2020-06-19 22:09:03 +0200

    Update publish-or-perish.rb

--- Casks/publish-or-perish.rb
+++ Casks/publish-or-perish.rb
@@ -1,5 +1,5 @@
 cask 'publish-or-perish' do
-  version 'v7.22.2838'
+  version '7.22.2838'
   sha256 'd301e402c9882abe54ee4fce2772eac275880e245d9dafcc33860aee3f671377'
 
   url 'https://harzing.com/download/PoP7Mac.pkg'
