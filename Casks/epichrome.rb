3d5556c8727ce073f52cea9276b77dd8095f64f8
Randall <ran.dall@icloud.com>
2020-06-19 14:19:46 -0700

    Update epichrome from 2.3.2 to 2.3.3

--- Casks/epichrome.rb
+++ Casks/epichrome.rb
@@ -1,6 +1,6 @@
 cask 'epichrome' do
-  version '2.3.2'
-  sha256 '91b804ea80721f7da7f2a65cfb51c3b6ca898664ee0108f4582e8a83ea9cdf3d'
+  version '2.3.3'
+  sha256 'aec344b2ab69351ed0447bb73ef76e94e996515533386aa91025d01e9a1814bd'
 
   url "https://github.com/dmarmor/epichrome/releases/download/v#{version}/epichrome-#{version}.pkg"
   appcast 'https://github.com/dmarmor/epichrome/releases.atom'
