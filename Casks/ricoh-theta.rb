a5448126203ba13b989017591b9c32f6c6268f0c
Rio Fujita <rio_github@rio.st>
2020-06-19 14:37:31 +0900

    Add a comment for different URL and Hostname

--- Casks/ricoh-theta.rb
+++ Casks/ricoh-theta.rb
@@ -2,6 +2,7 @@ cask 'ricoh-theta' do
   version '3.13.3'
   sha256 '6903ff2fa7c38a59c0d5562c51dade624d3e8de6acf908f9bf1df31913e9accf'
 
+  # theta360-statics.s3.amazonaws.com was verified as official when first introduced to the cask
   url 'https://theta360-statics.s3.amazonaws.com/app/viewer/RICOH%20THETA.dmg'
   appcast 'https://support.theta360.com/en/download/pcmac/'
   name 'RICOH THETA'
