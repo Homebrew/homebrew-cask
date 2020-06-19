502d23eb434002e6fed3c83ee47a5b89302a9cc7
Rio Fujita <rio_github@rio.st>
2020-06-19 14:04:37 +0900

    fix the position of appcast

--- Casks/ricoh-theta.rb
+++ Casks/ricoh-theta.rb
@@ -3,9 +3,9 @@ cask 'ricoh-theta' do
   sha256 '6903ff2fa7c38a59c0d5562c51dade624d3e8de6acf908f9bf1df31913e9accf'
 
   url 'https://theta360-statics.s3.amazonaws.com/app/viewer/RICOH%20THETA.dmg'
+  appcast 'https://support.theta360.com/en/download/pcmac/'
   name 'RICOH THETA'
   homepage 'https://theta360.com/'
-  appcast 'https://support.theta360.com/en/download/pcmac/'
 
   app 'RICOH THETA.app'
 end
