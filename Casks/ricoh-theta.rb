c5e6a1f79fabc42dbbb10877f3fbc443972a5ead
Rio Fujita <rio@rio.st>
2020-06-19 10:39:18 +0900

    Add Ricoh Theta

---
+++ Casks/ricoh-theta.rb
@@ -0,0 +1,10 @@
+cask 'ricoh-theta' do
+  version '3.13.3'
+  sha256 '6903ff2fa7c38a59c0d5562c51dade624d3e8de6acf908f9bf1df31913e9accf'
+
+  url 'https://support.theta360.com/intl/download/pcapp/macosx'
+  name 'RICOH THETA'
+  homepage 'https://theta360.com/ja/'
+
+  app 'RICOH THETA.app'
+end
