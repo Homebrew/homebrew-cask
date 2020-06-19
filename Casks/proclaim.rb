ebf109d073ffefeb18d851f00b85bb750d84455f
Randall <ran.dall@icloud.com>
2020-06-19 14:25:05 -0700

    Update proclaim from 2.12.0.0050 to 2.12.0.0052

--- Casks/proclaim.rb
+++ Casks/proclaim.rb
@@ -1,6 +1,6 @@
 cask 'proclaim' do
-  version '2.12.0.0050'
-  sha256 '331c51f20b934b19d282bbee70027208e05bdaa1ba6212589dabdc69d94a7606'
+  version '2.12.0.0052'
+  sha256 'c81244d24b9b77e24b92725f7a5d50ad6b6b860ee032cf9a04bcd8d9ed0487ea'
 
   # logoscdn.com/Proclaim/ was verified as official when first introduced to the cask
   url "https://downloads.logoscdn.com/Proclaim/Installer/#{version}/Proclaim.dmg"
