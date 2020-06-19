36a5c6bd3d764c4cbd8475ad1d751f4db8f2d2e1
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-19 15:04:01 -0700

    Update pagico from 9.4.20200613 to 9.4.20200619 (#84687)

--- Casks/pagico.rb
+++ Casks/pagico.rb
@@ -1,6 +1,6 @@
 cask 'pagico' do
-  version '9.4.20200613'
-  sha256 '90481aa4e39c7cc6420fdd49659d3761476c5165fd27653dd42d626213956f0e'
+  version '9.4.20200619'
+  sha256 '1f361360fcd435829a77d0763d210fd49a0269efe7165f3fe3123af1a1454b40'
 
   url "https://www.pagico.com/downloads/Pagico_macOS_r#{version.patch}.dmg"
   appcast "https://www.pagico.com/api/pagico#{version.major}.mac.xml",
