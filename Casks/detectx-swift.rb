6f3c98b9dc91b45a7a477e216ece939f82574e93
Randall <ran.dall@icloud.com>
2020-06-20 09:58:34 -0700

    Update detectx-swift from 1.093 to 1.094

--- Casks/detectx-swift.rb
+++ Casks/detectx-swift.rb
@@ -1,6 +1,6 @@
 cask 'detectx-swift' do
-  version '1.093'
-  sha256 '017657d7eab9f423010378262fe00ec4d90a81e37f11166d98dc52b62531e3a0'
+  version '1.094'
+  sha256 '03a9467dd5422ff7ca723f3ecb5a72e741344f2592c2e95ff1281f0b9ee78281'
 
   # s3.amazonaws.com/sqwarq.com/ was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/sqwarq.com/PublicZips/DetectX_Swift.dmg'
