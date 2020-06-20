a56ca532100b4f83d0a23ec1fd0e66c0d4569f0d
Julian Mayer <julian@corecode.io>
2020-06-20 13:20:38 +0200

    Update publish-or-perish

--- Casks/publish-or-perish.rb
+++ Casks/publish-or-perish.rb
@@ -1,6 +1,6 @@
 cask 'publish-or-perish' do
   version '7.22.2838'
-  sha256 'd301e402c9882abe54ee4fce2772eac275880e245d9dafcc33860aee3f671377'
+  sha256 '7872b13ce4b8780b09e21ba4e8618ef084364956e876c504d9eccddb45bdc0a1'
 
   url 'https://harzing.com/download/PoP7Mac.pkg'
   appcast 'https://harzing.com/resources/publish-or-perish/os-x'
