2a43a7f12b1a05cafbee502bf29fc92389eb672b
miccal <miccal.matthews@gmail.com>
2020-06-21 07:37:10 +0800

    Update chromium from 778130 to 780599

--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -1,6 +1,6 @@
 cask 'chromium' do
-  version '778130'
-  sha256 'e0f2bd1dee11c80aa1e8771f7044362447f7197ca2441b6eab5bc5ecb3da5bd4'
+  version '780599'
+  sha256 '6b6bc0ddc85b912faab56107601bfa6275d72e269dd5aeedeaa45b982c735def'
 
   # commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/ was verified as official when first introduced to the cask
   url "https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/#{version}/chrome-mac.zip"
