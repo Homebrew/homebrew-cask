332fa545591fa2be19733e2ad9ef60a6a972d797
goalidea <goalidea@outlook.com>
2020-06-21 09:28:41 +0800

    Create apifox.rb

---
+++ Casks/apifox.rb
@@ -0,0 +1,11 @@
+cask 'apifox' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://cdn.apifox.cn/download/apifox-mac-latest.dmg'
+  name 'Apifox'
+  homepage 'https://www.apifox.cn'
+
+  app 'Apifox.app'
+
+end
