acf259304541810df9b12d314f54dfce67cabfec
zx8 <8544900+zx8@users.noreply.github.com>
2020-06-21 20:04:29 +0100

    subsync: Use `version.major_minor` in URL

--- Casks/subsync.rb
+++ Casks/subsync.rb
@@ -3,7 +3,7 @@ cask 'subsync' do
   sha256 '0e6f54806e30db861acc88592de474e026fa54e8b1d636e52be5fa2a5e941c0d'
 
   # github.com/sc0ty/subsync was verified as official when first introduced to the cask
-  url "https://github.com/sc0ty/subsync/releases/download/0.15/subsync-#{version}-mac-x86_64.dmg"
+  url "https://github.com/sc0ty/subsync/releases/download/#{version.major_minor}/subsync-#{version}-mac-x86_64.dmg"
   appcast 'https://github.com/sc0ty/subsync/releases.atom'
   name 'subsync'
   homepage 'https://subsync.online/'
