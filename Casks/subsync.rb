e1b22c2c8dbec7bf73e84746f501aabe7d17a73b
zx8 <8544900+zx8@users.noreply.github.com>
2020-06-21 22:02:54 +0100

    subsync: Fix shim quotes

--- Casks/subsync.rb
+++ Casks/subsync.rb
@@ -16,7 +16,7 @@ cask 'subsync' do
   preflight do
     IO.write shimscript, <<~EOS
       #!/bin/sh
-      exec #{appdir}/subsync.app/Contents/MacOS/subsync --cli "$@"
+      exec '#{appdir}/subsync.app/Contents/MacOS/subsync' --cli "$@"
     EOS
   end
 
