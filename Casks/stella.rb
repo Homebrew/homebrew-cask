907a60ac05e22c9d35a6d019ffdee8421ee58003
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-21 19:09:45 -0700

    Update stella from 6.2 to 6.2.1 (#84760)

--- Casks/stella.rb
+++ Casks/stella.rb
@@ -1,6 +1,6 @@
 cask 'stella' do
-  version '6.2'
-  sha256 'c8269959e9f24f59cca5862d83252f100a259247d7a60616d22c23b27a9600e5'
+  version '6.2.1'
+  sha256 'b3385d3f304ef4e77d7bc473e73749009b72190c42d827afe4b38f0d695f9070'
 
   # github.com/stella-emu/stella/ was verified as official when first introduced to the cask
   url "https://github.com/stella-emu/stella/releases/download/#{version}/Stella-#{version}-macos.dmg"
