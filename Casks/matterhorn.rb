b6962a8593282c8ad020a588153dbd1e491cd28e
Helge Aksdal <helge@mnemonic.no>
2020-06-22 08:25:25 +0200

    Update matterhorn

--- Casks/matterhorn.rb
+++ Casks/matterhorn.rb
@@ -1,6 +1,6 @@
 cask 'matterhorn' do
-  version '50200.6.0'
-  sha256 '773fed52878e901bfac2998048336c51cb2f406b62c7e6ea7f1f5d30616b0e33'
+  version '50200.9.0'
+  sha256 '5433f2ba56941b2ee7b6fe103d0545ff6355a2e601caba57640a106232266c28'
 
   url "https://github.com/matterhorn-chat/matterhorn/releases/download/#{version}/matterhorn-#{version}-Darwin-x86_64.tar.bz2"
   appcast 'https://github.com/matterhorn-chat/matterhorn/releases.atom'
