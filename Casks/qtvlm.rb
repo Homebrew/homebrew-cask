35c25a21fe667f86288b06a158b5e83f5af601ce
Armand Fardeau <fardeauarmand@gmail.com>
2020-06-22 10:46:52 +0200

    Add qtVlm

---
+++ Casks/qtvlm.rb
@@ -0,0 +1,10 @@
+cask 'qtvlm' do
+  version '5.9.10'
+  sha256 'b2fe20e80fae8d836a3fc4da0c613b62be29ff2f4de3fb0993fd7151f41971f6'
+
+  url 'https://www.meltemus.com/index.php/en/download/send/3-mac-os/217-qtvlm-5-9-10-2'
+  name 'qtvlm'
+  homepage 'https://www.meltemus.com/index.php/en/'
+
+  app 'qtVlm'
+end
