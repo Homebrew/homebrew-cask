e12d32869d0464c030b1d20ea2dd82f55423fcc4
miccal <miccal.matthews@gmail.com>
2020-06-23 01:36:58 +0800

    Update rectangle.rb

--- Casks/rectangle.rb
+++ Casks/rectangle.rb
@@ -13,7 +13,7 @@ cask 'rectangle' do
 
   app 'Rectangle.app'
 
-  uninstall quit:      'com.knollsoft.Rectangle'
+  uninstall quit: 'com.knollsoft.Rectangle'
 
   zap trash: [
                '~/Library/Application Scripts/com.knollsoft.RectangleLauncher',
