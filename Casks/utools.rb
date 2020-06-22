d3e1c0497235c95fd3a59685a2389c1bf8883448
suschizu <brigitte@corecode.io>
2020-06-22 23:28:14 +0200

    Update utools.rb

--- Casks/utools.rb
+++ Casks/utools.rb
@@ -1,5 +1,5 @@
 cask 'utools' do
-  version '1.0.7-beta'
+  version '1.1.1'
   sha256 '4495190c983231030b742ee1c413367e66be81bc6d1da5955840344d57a8f078'
 
   # resource.u-tools.cn/ was verified as official when first introduced to the cask
