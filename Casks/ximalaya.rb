9aa1f27657dd0a40166209fed5552739054afa84
Brigitte Zmoelnig <brigitte@corecode.io>
2020-06-22 23:29:20 +0200

    Update ximalaya from 1.2.18_1592472266 to 1.2.19_1592817383

--- Casks/ximalaya.rb
+++ Casks/ximalaya.rb
@@ -1,6 +1,6 @@
 cask 'ximalaya' do
-  version '1.2.18_1592472266'
-  sha256 'af66f1352970bc9c3782f25a83148d34241d7aab695e0a26450a1fa2ae2cb628'
+  version '1.2.19_1592817383'
+  sha256 '7dc20cb44e2cfecf29c6e5d7e0aff8fd5b36a9d83441c1315dde8f0d3d5c3fb3'
 
   # s1.xmcdn.com/ was verified as official when first introduced to the cask
   url "https://s1.xmcdn.com/yx/ximalaya-pc-client/last/download/Ximalaya-#{version}.dmg"
