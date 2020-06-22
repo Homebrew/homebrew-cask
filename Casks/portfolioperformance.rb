9b83a73c923c93ab963cd6214aac6f4a2bf577dd
Randall <ran.dall@icloud.com>
2020-06-21 18:54:57 -0700

    Update portfolioperformance from 0.46.3 to 0.46.4

--- Casks/portfolioperformance.rb
+++ Casks/portfolioperformance.rb
@@ -1,6 +1,6 @@
 cask 'portfolioperformance' do
-  version '0.46.3'
-  sha256 '801e09c87f27fd95e8854752438130db07f3d23e4745f0e1cee4b886df65213b'
+  version '0.46.4'
+  sha256 '00453b270a5604d4ff86fc63acfd4f0d7169bc128176c9f24cfcc6f5df5c0133'
 
   # github.com/buchen/portfolio/ was verified as official when first introduced to the cask
   url "https://github.com/buchen/portfolio/releases/download/#{version}/PortfolioPerformance-#{version}.dmg"
