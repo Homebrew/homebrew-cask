d64c2098fc6ebe5da64e84ce13e669733aca2160
Fatme <hfatme@gmail.com>
2020-06-22 08:33:48 +0300

    undefined

--- Casks/orka.rb
+++ Casks/orka.rb
@@ -1,9 +1,9 @@
 cask 'orka' do
-  version '1.2.0,6924a4c7'
-  sha256 '3be344b879b01b54524065b1e2026633b77260d79823f23860dcc8149f32d127'
+  version '1.3.0,c710094'
+  sha256 '4178c1f16d82e9b93a825419c6228b7d8237b609082537732acef387ee5914f5'
 
   # cli-builds-public.s3-eu-west-1.amazonaws.com/official/ was verified as official when first introduced to the cask
-  url "https://cli-builds-public.s3-eu-west-1.amazonaws.com/official/#{version.before_comma}/#{version.after_comma}/macos/orka.zip"
+  url "https://cli-builds-public.s3-eu-west-1.amazonaws.com/official/1.3.0/c710094/macos/orka.zip"
   appcast 'https://orkadocs.macstadium.com/docs/downloads'
   name 'Orka CLI'
   homepage 'https://orkadocs.macstadium.com/docs'
