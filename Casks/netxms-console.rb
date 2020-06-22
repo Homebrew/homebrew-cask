556ee605db4221584323b5143e348ba0bf1dc1e2
Alex Kirhenshtein <alk@netxms.org>
2020-06-22 14:56:48 +0300

    NetXMS Console.app updated to vesion 3.4.178, zap added.

--- Casks/netxms-console.rb
+++ Casks/netxms-console.rb
@@ -1,6 +1,6 @@
 cask 'netxms-console' do
-  version '3.3.320'
-  sha256 '16b83367034961be803d60b8fb0b031056f42c6625d7ca9dd875e7e7a7535e1d'
+  version '3.4.178'
+  sha256 '3b8259f768ddccaa18e502520ac9a3212b338953f2fa8b2ed2deaa59b7fe9f85'
 
   url "https://netxms.org/download/releases/#{version.major_minor}/nxmc-#{version}.dmg"
   appcast "https://netxms.org/download/releases/#{version.major_minor}/"
@@ -8,4 +8,8 @@ cask 'netxms-console' do
   homepage 'https://netxms.org/'
 
   app 'NetXMS Console.app'
+
+  zap trash: [
+               '~/.nxmc',
+             ]
 end
