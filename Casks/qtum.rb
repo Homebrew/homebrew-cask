256f1a95558f6b6f49b0d77afb28af34eeaeeba4
Randall <ran.dall@icloud.com>
2020-06-21 18:53:49 -0700

    Update qtum from 0.19.0.1 to 0.19.1

--- Casks/qtum.rb
+++ Casks/qtum.rb
@@ -1,6 +1,6 @@
 cask 'qtum' do
-  version '0.19.0.1'
-  sha256 'bb957b66f55173f342dd78d089e2d60c6271fda8653579d7bea6143d09869204'
+  version '0.19.1'
+  sha256 'dc4e543ffdf9ca5f1184d01d26157ad98566824f8875b56e65b5b27eabcc9d56'
 
   # github.com/qtumproject/qtum/ was verified as official when first introduced to the cask
   url "https://github.com/qtumproject/qtum/releases/download/mainnet-ignition-v#{version.major_minor_patch}/qtum-#{version}-osx-unsigned.dmg"
