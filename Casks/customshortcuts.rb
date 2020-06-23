a3ac4af6d2c8f89df3a4f62659d56ba703218a62
Khaldoon Mutahar <git@mutahar.me>
2020-06-24 00:59:48 +0300

    Add CustomShortcuts Version 1.0.1 (#84879)
    * Add CustomShortcuts Version 1.0.1
    
    * fix style error
    
    * added the zap trash stanza
    
    * Error in that the didn't get pushed
    
    * Update customshortcuts.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

---
+++ Casks/customshortcuts.rb
@@ -0,0 +1,17 @@
+cask 'customshortcuts' do
+  version '1.0.1'
+  sha256 'fb32fe34cd6fd60665555cdc36bd5624723da00a35efb913d18eb7d151ab46a4'
+
+  url "https://www.houdah.com/customShortcuts/download_assets/CustomShortcuts#{version}.zip"
+  appcast 'https://www.houdah.com/customShortcuts/updates/cast.xml'
+  name 'CustomShortcuts'
+  homepage 'https://www.houdah.com/customShortcuts/'
+
+  app 'CustomShortcuts.app'
+
+  zap trash: [
+               '~/Library/Application Support/com.houdah.CustomShortcuts',
+               '~/Library/Caches/com.houdah.CustomShortcuts',
+               '~/Library/Preferences/com.houdah.CustomShortcuts.plist',
+             ]
+end
