8a255cea0efe18944049aaec76dab7dc7f9216f1
Randall <ran.dall@icloud.com>
2020-06-23 09:41:16 -0700

    Add `appcast` to `babeledit`

--- Casks/babeledit.rb
+++ Casks/babeledit.rb
@@ -3,6 +3,8 @@ cask 'babeledit' do
   sha256 'eed54865625e41f3ebfcacaf1222f38f20d752251520444733d18addd5608656'
 
   url "https://www.codeandweb.com/download/babeledit/#{version}/BabelEdit-#{version}.dmg"
+  appcast 'https://www.codeandweb.com/babeledit/download',
+          must_contain: version
   name 'BabelEdit'
   homepage 'https://www.codeandweb.com/babeledit'
 
