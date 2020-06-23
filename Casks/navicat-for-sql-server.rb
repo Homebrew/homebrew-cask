fa20b06339a9b485860a3eb5afce65b9428e44ca
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-23 13:48:59 -0700

    Update navicat-for-sql-server from 15.0.17 to 15.0.18 (#84877)

--- Casks/navicat-for-sql-server.rb
+++ Casks/navicat-for-sql-server.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-sql-server' do
-  version '15.0.17'
-  sha256 'c4768f2464da9368a896374ba79377ab6aef1086df78aa90a88d99a9fdedc802'
+  version '15.0.18'
+  sha256 'f37c99a3ef442651660b0460c935e2f71ede7a9e3d043cec0e8ff9f7909b91a1'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_sqlserver_en.dmg"
   appcast 'https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20for%20SQL%20Server&appLang=en'
