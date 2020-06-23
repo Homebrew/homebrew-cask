ed5397c442d8721894dac4936f3b9c2d87ba3932
Randall <ran.dall@icloud.com>
2020-06-23 13:16:11 -0700

    Update maccy from 0.12.0 to 0.13.0

--- Casks/maccy.rb
+++ Casks/maccy.rb
@@ -1,6 +1,6 @@
 cask 'maccy' do
-  version '0.12.0'
-  sha256 '8a10dff4762620008e0166abfd1eb3b14eb94e261dc0b5ff54dbca46b87ca505'
+  version '0.13.0'
+  sha256 '82024126a7a17f3f5359d8e531e3db67e13707e59e1aa0320372b4eaf3025bf8'
 
   # github.com/p0deje/Maccy/ was verified as official when first introduced to the cask
   url "https://github.com/p0deje/Maccy/releases/download/#{version}/Maccy.app.zip"
