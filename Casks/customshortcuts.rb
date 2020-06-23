c161d3dfcc6ac99154099f2a1428cf96e7c840ae
Khaldoon Mutahar <git@mutahar.me>
2020-06-23 23:45:27 +0300

    Add CustomShortcuts Version 1.0.1

---
+++ Casks/customshortcuts.rb
@@ -0,0 +1,11 @@
+cask 'customshortcuts' do
+  version '1.0.1'
+  sha256 'fb32fe34cd6fd60665555cdc36bd5624723da00a35efb913d18eb7d151ab46a4'
+
+  url "https://www.houdah.com/customShortcuts/download_assets/CustomShortcuts#{version}.zip"
+  appcast'https://www.houdah.com/customShortcuts/release.html'
+  name 'CustomShortcuts'
+  homepage 'https://www.houdah.com/customShortcuts/'
+
+  app 'CustomShortcuts.app'
+end
