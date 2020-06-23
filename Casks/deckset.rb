3b41840bafa2994d492bcac474550ab9c598b772
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-23 13:35:16 -0700

    Update deckset from 2.0.16,2578 to 2.0.17,2580 (#84865)

--- Casks/deckset.rb
+++ Casks/deckset.rb
@@ -1,6 +1,6 @@
 cask 'deckset' do
-  version '2.0.16,2578'
-  sha256 '50d9234f3ccd352856457b035f037acd11e5cc996b431da6ed6e409bbc334151'
+  version '2.0.17,2580'
+  sha256 'aae4a05b12411494682a3faec538c764fd3e8a4e5e68d1b905e1a45420fbd057'
 
   url "https://dl.decksetapp.com/Deckset+#{version.before_comma}+(#{version.after_comma}).dmg"
   appcast 'https://dl.decksetapp.com/appcast.xml'
