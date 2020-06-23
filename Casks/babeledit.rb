5c850dafa66dea8c4921f204d2d8d09c47f06354
Nicholas Pleschko <n.pleschko@gmail.com>
2020-06-23 12:00:47 +0200

    Add BabelEdit.app v2.7.0

---
+++ Casks/babeledit.rb
@@ -0,0 +1,10 @@
+cask 'babeledit' do
+  version '2.7.0'
+  sha256 'eed54865625e41f3ebfcacaf1222f38f20d752251520444733d18addd5608656'
+
+  url "https://www.codeandweb.com/download/babeledit/#{version}/BabelEdit-#{version}.dmg"
+  name 'BabelEdit'
+  homepage 'https://www.codeandweb.com/babeledit'
+
+  app 'BabelEdit.app'
+end
