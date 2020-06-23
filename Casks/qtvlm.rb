272b0615dafc404432d899648d725ca6977914a8
Randall <ran.dall@icloud.com>
2020-06-23 07:46:24 -0700

    Update `appcast` and add `must_contain` stanza to `qtvlm`

--- Casks/qtvlm.rb
+++ Casks/qtvlm.rb
@@ -3,7 +3,8 @@ cask 'qtvlm' do
   sha256 'b2fe20e80fae8d836a3fc4da0c613b62be29ff2f4de3fb0993fd7151f41971f6'
 
   url "https://www.meltemus.com/index.php/en/download/send/3-mac-os/#{version.after_comma}-qtvlm-#{version.before_comma.dots_to_hyphens}-2"
-  appcast 'https://www.meltemus.com/index.php/en/download/category/3-mac-os'
+  appcast 'https://www.meltemus.com/index.php/en/download/category/3-mac-os',
+          must_contain: version.after_comma
   name 'qtvlm'
   homepage 'https://www.meltemus.com/index.php/en/'
 
