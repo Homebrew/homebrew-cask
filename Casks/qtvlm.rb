8b40b270fcd646a3ba3990b5db5b4c0b6481668b
Randall <ran.dall@icloud.com>
2020-06-23 07:12:28 -0700

    Correct `version` and `url` stanzas on `qtvim`

--- Casks/qtvlm.rb
+++ Casks/qtvlm.rb
@@ -1,8 +1,8 @@
 cask 'qtvlm' do
-  version '5.9.10'
+  version '5.9.10,217'
   sha256 'b2fe20e80fae8d836a3fc4da0c613b62be29ff2f4de3fb0993fd7151f41971f6'
 
-  url 'https://www.meltemus.com/index.php/en/download/send/3-mac-os/217-qtvlm-5-9-10-2'
+  url "https://www.meltemus.com/index.php/en/download/send/3-mac-os/#{version.after_comma}-qtvlm-#{version.before_comma.dots_to_hyphens}-2"
   name 'qtvlm'
   homepage 'https://www.meltemus.com/index.php/en/'
 
