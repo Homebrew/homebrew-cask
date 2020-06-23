2baa369593de45db6f8a791fd84525dc45d520e0
Randall <ran.dall@icloud.com>
2020-06-23 08:59:56 -0700

    Update `sha256` on `qtvlm`

--- Casks/qtvlm.rb
+++ Casks/qtvlm.rb
@@ -1,6 +1,6 @@
 cask 'qtvlm' do
   version '5.9.10,217'
-  sha256 'b2fe20e80fae8d836a3fc4da0c613b62be29ff2f4de3fb0993fd7151f41971f6'
+  sha256 'ed38ccd9565344ead60f46b59d4f1f4d4290e13a43f84eb730d31d8c1eca6523'
 
   url "https://www.meltemus.com/index.php/en/download/send/3-mac-os/#{version.after_comma}-qtvlm-#{version.before_comma.dots_to_hyphens}-2"
   appcast 'https://www.meltemus.com/index.php/en/download/category/3-mac-os',
