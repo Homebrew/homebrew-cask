5958fc32956037e775f1336dd765b5754fc2ff63
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-22 19:55:57 -0700

    Update codespace from 1.2.2 to 1.2.3 (#84833)

--- Casks/codespace.rb
+++ Casks/codespace.rb
@@ -1,6 +1,6 @@
 cask 'codespace' do
-  version '1.2.2'
-  sha256 'ff10a58e2468350d2f9d4a7cca002f06dafe18471d98f6a7729e5557026a55a3'
+  version '1.2.3'
+  sha256 'a6cce41cb3497bf455a867aba9d7a27cdf76f3999fa92409713701707dafab39'
 
   url 'https://codespace.app/download/latest'
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://codespace.app/download/latest&user_agent=Mac%20OS%20X'
