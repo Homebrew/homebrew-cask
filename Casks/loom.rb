667c6e8c300cefe05a01730c7c2b375c75680607
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-24 16:19:16 -0700

    Update loom from 0.40.3 to 0.41.0 (#84951)

--- Casks/loom.rb
+++ Casks/loom.rb
@@ -1,6 +1,6 @@
 cask 'loom' do
-  version '0.40.3'
-  sha256 '4f70b20f1e00a868d75bb229e4f2790a2f0d67cdd3f3b79cbc50e52e5e849a4e'
+  version '0.41.0'
+  sha256 'a12491d679a817e83d6f7a445065e0047ad32dd0b879258f60f7e3e635c87596'
 
   url "https://cdn.loom.com/desktop-packages/Loom-#{version}.dmg"
   appcast 'https://s3-us-west-2.amazonaws.com/loom.desktop.packages/loom-inc-production/desktop-packages/latest-mac.yml'
