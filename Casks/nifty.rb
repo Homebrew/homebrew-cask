f37aee9026fc343cedf4d56969ba6ffc24442f37
Yuichiro Smith <contact@yu-smith.com>
2020-06-24 17:36:17 +0900

    Add Nifty.app v3.15.2
    The official macOS app for https://niftypm.com

---
+++ Casks/nifty.rb
@@ -0,0 +1,10 @@
+cask 'nifty' do
+  version '3.15.2'
+  sha256 '23bf5cd732f08284ce5f8cd678347081b5c8d8b11c8cade4a79afd9b69c1cfea'
+
+  url 'https://niftypm.com/apps/Nifty.dmg'
+  name 'Nifty'
+  homepage 'https://niftypm.com/'
+
+  app 'Nifty.app'
+end
