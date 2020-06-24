9ff3132336c6e81af6fef6aa4eff748e7503b180
i0ntempest <szf1234@me.com>
2020-06-24 16:06:09 -0400

    Update browserosaurus from 10.9.0 to 10.9.1

--- Casks/browserosaurus.rb
+++ Casks/browserosaurus.rb
@@ -1,6 +1,6 @@
 cask 'browserosaurus' do
-  version '10.9.0'
-  sha256 '5f8ee37d10adde79f257a6056d903ddad77922d32268f7dba478a182063f980d'
+  version '10.9.1'
+  sha256 'd0e35f7a6626dba372d466f5e412c41450b826851d010042d0fa0e6f653d602e'
 
   # github.com/will-stone/browserosaurus/ was verified as official when first introduced to the cask
   url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-#{version}.dmg"
