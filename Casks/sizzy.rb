312d38e86045a5f4100f1dd1bdad8568bd3b763e
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-23 17:48:52 -0700

    Update sizzy from 0.23.1 to 0.24.0 (#84896)

--- Casks/sizzy.rb
+++ Casks/sizzy.rb
@@ -1,6 +1,6 @@
 cask 'sizzy' do
-  version '0.23.1'
-  sha256 '3815647171b62e0d0315b289c9f489db3914a01b5895d97b1402fdb93d63309e'
+  version '0.24.0'
+  sha256 '042b6e82fe72d39309a80355281a8c6bb84adfbe04f2dd4fbfabb2d3ef2c06a3'
 
   url 'https://sizzy.co/get-app'
   appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://sizzy.co/get-app&user_agent=Intel%20Mac%20OS%20X'
