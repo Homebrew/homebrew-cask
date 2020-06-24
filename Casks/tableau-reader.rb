6e66669e1d34b300d7730ed7766e8f330aedd20d
Randall <ran.dall@icloud.com>
2020-06-24 16:28:37 -0700

    Update tableau-reader from 2020.2.1 to 2020.2.2

--- Casks/tableau-reader.rb
+++ Casks/tableau-reader.rb
@@ -1,6 +1,6 @@
 cask 'tableau-reader' do
-  version '2020.2.1'
-  sha256 '8839ba497bc62868abfb39e95650d32087d4064494c0393595cff1638c89fd67'
+  version '2020.2.2'
+  sha256 '74b4f8bc12b462b11c94a5400f25010474f410e24ef8d943318049e05e76bf0c'
 
   url "https://downloads.tableau.com/tssoftware/TableauReader-#{version.dots_to_hyphens}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.tableau.com/downloads/reader/mac',
