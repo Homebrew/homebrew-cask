6732e03bcac39f0ffc7bb271f16db2250b38c8e7
apropostech <info@apropostech.com>
2020-06-24 09:21:02 +0200

    Update fastrawviewer from 1.5.7.1636 to 1.6.0.1639

--- Casks/fastrawviewer.rb
+++ Casks/fastrawviewer.rb
@@ -1,6 +1,6 @@
 cask 'fastrawviewer' do
-  version '1.5.7.1636'
-  sha256 'c47f4164e12ff3e2ccf95c92fdc434edf1d481ef8e9214682279f6fee3ddf7c4'
+  version '1.6.0.1639'
+  sha256 '355a1fd67aa56986628b4fe4ccdb8084d8403ff3cb7a2b94a9ca2a5c0f238cc8'
 
   url "https://updates.fastrawviewer.com/data/FastRawViewer-#{version}.dmg"
   appcast 'https://www.fastrawviewer.com/download'
