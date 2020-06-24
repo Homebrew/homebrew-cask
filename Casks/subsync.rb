501c2d2a09154a969fd81268bb2c6758353537b3
zx8 <zx8@users.noreply.github.com>
2020-06-24 13:40:42 +0100

    Add `subsync.app` (#84745)
    * Add `subsync.app`
    
    * subsync: Use `version.major_minor` in URL
    
    * subsync: Add `app` stanza
    
    * subsync: Fix shim quotes
    
    * subsync: Delete entire preferences directory
    
    * subsync: Make `zap` a string
    
    Co-authored-by: zx8 <8544900+zx8@users.noreply.github.com>

---
+++ Casks/subsync.rb
@@ -0,0 +1,24 @@
+cask 'subsync' do
+  version '0.15.0'
+  sha256 '0e6f54806e30db861acc88592de474e026fa54e8b1d636e52be5fa2a5e941c0d'
+
+  # github.com/sc0ty/subsync was verified as official when first introduced to the cask
+  url "https://github.com/sc0ty/subsync/releases/download/#{version.major_minor}/subsync-#{version}-mac-x86_64.dmg"
+  appcast 'https://github.com/sc0ty/subsync/releases.atom'
+  name 'subsync'
+  homepage 'https://subsync.online/'
+
+  app 'subsync.app'
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/subsync.wrapper.sh"
+  binary shimscript, target: 'subsync'
+
+  preflight do
+    IO.write shimscript, <<~EOS
+      #!/bin/sh
+      exec '#{appdir}/subsync.app/Contents/MacOS/subsync' --cli "$@"
+    EOS
+  end
+
+  zap trash: '~/Library/Preferences/subsync/'
+end
