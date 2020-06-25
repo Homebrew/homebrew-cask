702d161900ed1dfe00d8adf198f5091f1fc54715
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-25 15:21:41 -0700

    Update linear-linear from 1.2.7 to 1.2.8 (#85008)

--- Casks/linear-linear.rb
+++ Casks/linear-linear.rb
@@ -1,6 +1,6 @@
 cask 'linear-linear' do
-  version '1.2.7'
-  sha256 '9a8cdcd944f606a630d98fc55a7152f73c63707a45d223e7c26cb8fd8b4574a0'
+  version '1.2.8'
+  sha256 'bf4cfa1c578eb3264138e41f35f6f7d1ceeeb821c4e406d4fa2bef148428c1de'
 
   url "https://download.linear.app/darwin/Linear-darwin-x64-#{version}.zip"
   appcast 'https://api.linear.app/update/darwin/0.0.0'
