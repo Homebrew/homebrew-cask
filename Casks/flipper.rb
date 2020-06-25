f3836b20bd4887c321cbbda097322df16de7d228
Steven Conaway <steven.conaway@icloud.com>
2020-06-25 09:19:40 -0700

    flipper: v0.48.0 (#84988)

--- Casks/flipper.rb
+++ Casks/flipper.rb
@@ -1,6 +1,6 @@
 cask 'flipper' do
-  version '0.47.0'
-  sha256 'e7449125301bca9fab1a0389c9d5055f686645189c912cd7e5a8bea59d1e23dc'
+  version '0.48.0'
+  sha256 '45f28ed44c30252105bc598a7adae1ca0fd0a4b2c4ec1b8f3f0515cbed03372b'
 
   # github.com/facebook/flipper/ was verified as official when first introduced to the cask
   url "https://github.com/facebook/flipper/releases/download/v#{version}/Flipper-mac.zip"
