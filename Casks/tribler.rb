63ebd2acba8ef27e87546d92f0890b617c1e0098
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-25 14:49:27 -0700

    Update tribler from 7.5.0 to 7.5.1 (#84995)

--- Casks/tribler.rb
+++ Casks/tribler.rb
@@ -1,6 +1,6 @@
 cask 'tribler' do
-  version '7.5.0'
-  sha256 'b4181855a77e14541e158662814ae46800ada38e8fdc30b7a6299d424120c4cf'
+  version '7.5.1'
+  sha256 '3b4e5cb364309ffd99d0502bdb574e8d33ea70620f3abd918dd3e3f6ce705adb'
 
   # github.com/Tribler/tribler/ was verified as official when first introduced to the cask
   url "https://github.com/Tribler/tribler/releases/download/v#{version}/Tribler-#{version}.dmg"
