5df6141a655058797bbb758986e17d3951fd3d94
Dieter Blomme <dieterblomme@gmail.com>
2020-06-26 00:48:05 +0200

    Fix brew cask style --fix errors

--- Casks/sequel-ace.rb
+++ Casks/sequel-ace.rb
@@ -2,7 +2,6 @@ cask 'sequel-ace' do
   version '2.0.2'
   sha256 'c92518b7ac0d3800071cb831f55b7de4ac8c04e9ba0a3be589287e10c83247ac'
 
-  # github.com/Sequel-Ace/Sequel-Ace/ was verified as official when first introduced to the cask
   url "https://github.com/Sequel-Ace/Sequel-Ace/releases/download/#{version}/Sequel-Ace-#{version}-release.zip"
   appcast 'https://github.com/Sequel-Ace/Sequel-Ace/releases.atom'
   name 'Sequel Ace'
