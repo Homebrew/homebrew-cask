e4071c8d251836423bdb71cd4a444bf42ae6e65d
Randall <ran.dall@icloud.com>
2020-06-25 14:40:10 -0700

    Update menutube from 1.6.1 to 1.7.2

--- Casks/menutube.rb
+++ Casks/menutube.rb
@@ -1,6 +1,6 @@
 cask 'menutube' do
-  version '1.6.1'
-  sha256 'b50a106a88b96e9ce98feb99502446ad9ce8fcd610dfb3f479e40030e16a7d42'
+  version '1.7.2'
+  sha256 '3553cd053f622837fac6e365c19daa5946ce72c8ba10eadae4afb11c7fa57851'
 
   # github.com/edanchenkov/MenuTube/ was verified as official when first introduced to the cask
   url "https://github.com/edanchenkov/MenuTube/releases/download/#{version}/MenuTube-#{version}.dmg"
