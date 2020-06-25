a89c54d8c41d5a520df0ca0c0d913fffbc3189b3
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-25 15:49:23 -0700

    Update doly from 2.17.0 to 2.18.0 (#85024)

--- Casks/doly.rb
+++ Casks/doly.rb
@@ -1,6 +1,6 @@
 cask 'doly' do
-  version '2.17.0'
-  sha256 '80f111c59a04f3af2755a9826310430d53a85fa5386cfdfad6bf130ca51475d2'
+  version '2.18.0'
+  sha256 'b3f7a9074069cb8906d0b05195202d5f6f0ccba6e4d8ed9cfed87c06822b6c39'
 
   url "http://doly.jp/download/doly-mac_#{version}.dmg"
   appcast 'http://doly.jp/'
