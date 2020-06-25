82c09fdb9d434fceaf6b18b840c6038ade5e5e43
Randall <ran.dall@icloud.com>
2020-06-25 14:43:42 -0700

    Update deckset from 2.0.17,2580 to 2.0.18,2582

--- Casks/deckset.rb
+++ Casks/deckset.rb
@@ -1,6 +1,6 @@
 cask 'deckset' do
-  version '2.0.17,2580'
-  sha256 'aae4a05b12411494682a3faec538c764fd3e8a4e5e68d1b905e1a45420fbd057'
+  version '2.0.18,2582'
+  sha256 'aa8d1044aa719b746c56a0e02d4f454033da41460fd2e347f497a609981118e5'
 
   url "https://dl.decksetapp.com/Deckset+#{version.before_comma}+(#{version.after_comma}).dmg"
   appcast 'https://dl.decksetapp.com/appcast.xml'
