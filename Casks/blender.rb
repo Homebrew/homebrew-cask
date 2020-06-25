527f641089db3e86c6859f971d808f4e5c690058
CoreCode <core-code@users.noreply.github.com>
2020-06-25 22:24:31 +0200

    Update blender.rb

--- Casks/blender.rb
+++ Casks/blender.rb
@@ -1,10 +1,10 @@
 cask 'blender' do
-  version '2.83'
+  version '2.83.1'
   sha256 '9dfc532b78da557618a32877b6e6736df8385ffbba131f8e371f6fa2c01cec21'
 
-  url "https://download.blender.org/release/Blender#{version.major_minor.delete('a-z')}/blender-#{version}.1-macOS.dmg"
+  url "https://download.blender.org/release/Blender#{version.major_minor.delete('a-z')}/blender-#{version}-macOS.dmg"
   appcast 'https://download.blender.org/release/',
-          must_contain: version.delete('a-z')
+          must_contain: version.major_minor.delete('a-z')
   name 'Blender'
   homepage 'https://www.blender.org/'
 
