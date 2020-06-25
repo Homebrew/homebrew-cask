a0c38f2aa6b827da6eee4c72fcc0638ac03da80e
Randall <ran.dall@icloud.com>
2020-06-25 15:02:41 -0700

    Update mendeley-reference-manager from 2.27.0 to 2.28.0

--- Casks/mendeley-reference-manager.rb
+++ Casks/mendeley-reference-manager.rb
@@ -1,6 +1,6 @@
 cask 'mendeley-reference-manager' do
-  version '2.27.0'
-  sha256 '06964c7b32c4cb46428f9a351c75bb04ab9ad7ace544447cc47ec2ff84a5fe5b'
+  version '2.28.0'
+  sha256 '9d8c689102f10b1e0beae2ee21ad56a9f4417372f5c40db70759dcf82ad0c607'
 
   url "https://static.mendeley.com/bin/desktop/mendeley-reference-manager-#{version}.dmg"
   appcast 'https://static.mendeley.com/bin/desktop/latest-mac.yml'
