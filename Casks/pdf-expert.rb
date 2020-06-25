5c5131a881800204fdea5c0124fbbb7b897014eb
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-25 15:06:56 -0700

    Update pdf-expert from 2.5.7,691 to 2.5.9,702 (#85004)

--- Casks/pdf-expert.rb
+++ Casks/pdf-expert.rb
@@ -1,6 +1,6 @@
 cask 'pdf-expert' do
-  version '2.5.7,691'
-  sha256 'c69c9209da33dd0c47ecb7aa39f0aaf8f48374f2b38b98170f7a2f55b5c2d61b'
+  version '2.5.9,702'
+  sha256 '22972eeca0bdd86704b826bf799bd2b74168ebf97e5aaa504d1ff98ee8088c17'
 
   # d1ke680phyeohy.cloudfront.net/ was verified as official when first introduced to the cask
   url "https://d1ke680phyeohy.cloudfront.net/versions/#{version.after_comma}/PDFExpert.dmg"
