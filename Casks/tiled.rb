7f946809747a763cfd389c4d8162c42457751cf7
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-26 14:22:20 -0700

    Update tiled from 1.4.0 to 1.4.1 (#85045)

--- Casks/tiled.rb
+++ Casks/tiled.rb
@@ -1,6 +1,6 @@
 cask 'tiled' do
-  version '1.4.0'
-  sha256 'b016203133655f18ee0d06828d2eeefa10568b109e34ff875156eab1625d76ea'
+  version '1.4.1'
+  sha256 '23ab6204d0fa89a50e0591be9a9e8f54689d6941b55465f5711f123277c33f15'
 
   # github.com/bjorn/tiled/ was verified as official when first introduced to the cask
   url "https://github.com/bjorn/tiled/releases/download/v#{version}/Tiled-#{version}-macos.zip"
