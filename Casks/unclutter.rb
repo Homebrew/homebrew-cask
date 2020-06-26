4dea34fc281bf250ff5f0e69c94ebb03542e0e31
NanoSector <rick@nanosector.nl>
2020-06-26 14:26:03 +0200

    Add Unclutter Latest

---
+++ Casks/unclutter.rb
@@ -0,0 +1,10 @@
+cask 'unclutter' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://unclutterapp.com/files/Unclutter.zip'
+  name 'Unclutter'
+  homepage 'https://unclutterapp.com/'
+
+  app 'Unclutter.app'
+end
