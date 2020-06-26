fab3a6ffd53464d5e1bda5e790acd33d00128809
Dieter Blomme <dieterblomme@gmail.com>
2020-06-26 02:15:33 +0200

    Add Sequel-Ace (#85028)

---
+++ Casks/sequel-ace.rb
@@ -0,0 +1,18 @@
+cask 'sequel-ace' do
+  version '2.0.2'
+  sha256 'c92518b7ac0d3800071cb831f55b7de4ac8c04e9ba0a3be589287e10c83247ac'
+
+  url "https://github.com/Sequel-Ace/Sequel-Ace/releases/download/#{version}/Sequel-Ace-#{version}-release.zip"
+  appcast 'https://github.com/Sequel-Ace/Sequel-Ace/releases.atom'
+  name 'Sequel Ace'
+  homepage 'https://github.com/Sequel-Ace/Sequel-Ace'
+
+  app 'Sequel Ace.app'
+
+  zap trash: [
+               '~/Library/Application Support/Sequel Ace',
+               '~/Library/Caches/com.sequelace.SequelAce',
+               '~/Library/Preferences/com.sequelace.SequelAce.plist',
+               '~/Library/Saved Application State/com.sequelace.SequelAce.savedState',
+             ]
+end
