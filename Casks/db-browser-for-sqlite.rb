6da1006ab24f741ab7879a2f7dd1d6b11a52c88a
suschizu <brigitte@corecode.io>
2020-06-26 22:15:34 +0200

    Update db-browser-for-sqlite.rb

--- Casks/db-browser-for-sqlite.rb
+++ Casks/db-browser-for-sqlite.rb
@@ -2,8 +2,7 @@ cask 'db-browser-for-sqlite' do
   version '3.12.0'
   sha256 '4a7aaac7554c43ecec330d0631f356510dcad11e49bb01986ba683b6dfb59530'
 
-  # github.com/sqlitebrowser/sqlitebrowser/ was verified as official when first introduced to the cask
-  url "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v#{version}/DB.Browser.for.SQLite-#{version}.dmg"
+  url "https://download.sqlitebrowser.org/DB.Browser.for.SQLite-#{version}.dmg"
   appcast 'https://github.com/sqlitebrowser/sqlitebrowser/releases.atom'
   name 'SQLite Database Browser'
   homepage 'https://sqlitebrowser.org/'
