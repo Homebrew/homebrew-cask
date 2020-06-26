e69839930100e3077f07fa62bbf715a1b49618e8
Randall <ran.dall@icloud.com>
2020-06-26 14:26:47 -0700

    Update mixxx from 2.2.3 to 2.2.4

--- Casks/mixxx.rb
+++ Casks/mixxx.rb
@@ -1,6 +1,6 @@
 cask 'mixxx' do
-  version '2.2.3'
-  sha256 '8933127b9780fc8f7c5ef050d9b35e1fbeb98e5281595863d22741f5dd292420'
+  version '2.2.4'
+  sha256 '4baf38b866693ea1411c988ea7c1dcfabf9635179358191743109b7a27a0d4d3'
 
   url "https://downloads.mixxx.org/mixxx-#{version}/mixxx-#{version}-osxintel.dmg"
   appcast 'https://www.mixxx.org/download/'
