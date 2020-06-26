1ec2b0f13daca5db5ca603a245be3f232d2211af
Vítor Galvão <info@vitorgalvao.com>
2020-06-26 17:28:30 +0100

    Update unclutter.rb

--- Casks/unclutter.rb
+++ Casks/unclutter.rb
@@ -1,5 +1,5 @@
 cask 'unclutter' do
-  version '2.1.22d'
+  version '2.1.2200'
   sha256 'f2a60f896e8c8c1f21a1beb3b9c5a833343a2932ab423ebc8b791eb8db1cca1b'
 
   url 'https://unclutterapp.com/files/Unclutter.zip'
