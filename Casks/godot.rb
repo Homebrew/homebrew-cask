4b10fbbd908837d67ac2ba9da8ac1a292e955717
LRFLEW <LRFLEW@aol.com>
2020-06-27 13:12:56 -0500

    Update godot to 3.2.2

--- Casks/godot.rb
+++ Casks/godot.rb
@@ -1,6 +1,6 @@
 cask 'godot' do
-  version '3.2.1'
-  sha256 'bf03a9430d53ed1f748f25d8840cba109f99e1d5d11ae8b23535a0b75286ec95'
+  version '3.2.2'
+  sha256 '36d1002ad3a99314e03195f944b86b3d9b694ad4938a32e88c4ac51c7bf9b893'
 
   # downloads.tuxfamily.org/godotengine/ was verified as official when first introduced to the cask
   url "https://downloads.tuxfamily.org/godotengine/#{version}/Godot_v#{version}-stable_osx.64.zip"
