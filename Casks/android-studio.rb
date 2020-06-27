9e9f017dde26655fdec0fe54c2c41630c9a12fe3
yuzhy8701 <18453608+yuzhy8701@users.noreply.github.com>
2020-06-26 17:40:10 -0700

    Change download url to use Google's edge cache.
    For immutable files, `dl.google.com` has an edge cache with the corresponding path `redirector.gvt1.com/edgedl` which tends to be faster when users are not close to Google datacenters. `updates.xml` will still need to use `dl.google.com`.

--- Casks/android-studio.rb
+++ Casks/android-studio.rb
@@ -2,8 +2,8 @@ cask 'android-studio' do
   version '4.0.0.16,193.6514223'
   sha256 '61b71dd3274085c16c597ea4a51a5d6c1e39957f358a3434eda79ecc332235ee'
 
-  # google.com/dl/android/studio/ was verified as official when first introduced to the cask
-  url "https://dl.google.com/dl/android/studio/install/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.dmg"
+  # redirector.gvt1.com/edgedl/android/studio/ is Google's edge cache url for dl.google.com/android/studio/
+  url "https://redirector.gvt1.com/edgedl/android/studio/install/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.dmg"
   appcast 'https://dl.google.com/android/studio/patches/updates.xml',
           must_contain: version.major_minor_patch
   name 'Android Studio'
