bd4a2a6c2ef6702bf8afd610e33a7c093d9c513f
SgtLegend <sgtlegend@teamxlink.dev>
2020-06-27 13:29:22 +1000

    Update XLink Kai.app to v7.4.37

--- Casks/xlink-kai.rb
+++ Casks/xlink-kai.rb
@@ -1,9 +1,9 @@
 cask 'xlink-kai' do
-  version '7.4.36'
-  sha256 '931b8250de58bf24553e26cb6507186005520d8eec4c6f83d1db63c613693717'
+  version '7.4.37'
+  sha256 'f19d595d94b156a801f12529c95086f20d1c7ca25961f52a025153a2e4d30420'
 
   # github.com/Team-XLink/releases/ was verified as official when first introduced to the cask
-  url "https://github.com/Team-XLink/releases/releases/download/v#{version}/XLink.Kai.app.zip"
+  url "https://github.com/Team-XLink/releases/releases/download/v#{version}/XLinkKai-7.4.37-539579342-macOS.dmg"
   appcast 'https://github.com/Team-XLink/releases/releases.atom'
   name 'XLink Kai'
   homepage 'https://www.teamxlink.co.uk/'
