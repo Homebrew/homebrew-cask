65450fe877b3f0a69db2444c8554211289d148a8
Vítor Galvão <info@vitorgalvao.com>
2020-06-27 12:29:45 +0100

    Update nordvpn-teams.rb

--- Casks/nordvpn-teams.rb
+++ Casks/nordvpn-teams.rb
@@ -2,8 +2,9 @@ cask 'nordvpn-teams' do
   version '1.4.0'
   sha256 '5c549a6bc5f930f13471284a4ef20d7676bb43bf551d72e5e3873e4e588c2cd8'
 
-  # nordteams.com was verified as official when first introduced to the cask
+  # downloads.nordteams.com was verified as official when first introduced to the cask
   url 'https://downloads.nordteams.com/mac/latest/NordVPNTeams.pkg'
+  appcast 'https://downloads.nordteams.com/mac/update.xml'
   name 'NordVPN Teams'
   homepage 'https://nordvpnteams.com/'
 
