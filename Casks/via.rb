d8478f47fe57d14daf34f7f55dc1c0bd6a2cd79b
fauxpark <fauxpark@gmail.com>
2020-06-27 22:25:01 +1000

    Update via from 1.2.7 to 1.3.1

--- Casks/via.rb
+++ Casks/via.rb
@@ -1,6 +1,6 @@
 cask 'via' do
-  version '1.2.7'
-  sha256 'f005c4727977fee50e9646fb7183e9d6518515e4524405f6e172f50321b8f24b'
+  version '1.3.1'
+  sha256 'd7b9f6fd97cc920cebae8b92417c24f5b78856b8f2fc8512093b2fbfc9302e67'
 
   # github.com/the-via/releases was verified as official when first introduced to the cask
   url "https://github.com/the-via/releases/releases/download/v#{version}/via-#{version}-mac.dmg"
