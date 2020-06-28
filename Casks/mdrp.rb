5411d35b679a1c07f648ba6af95a2c6cc6c43989
suschizu <brigitte@corecode.io>
2020-06-28 21:07:10 +0200

    Update mdrp from 8.0.6 to 9.0.1 (#85116)

--- Casks/mdrp.rb
+++ Casks/mdrp.rb
@@ -1,6 +1,6 @@
 cask 'mdrp' do
-  version '8.0.6'
-  sha256 'e24a79bb47a0790c13c27948f9f0fc4730115347d52abf39aec5e4ccf6ec559a'
+  version '9.0.1'
+  sha256 '2bf6eae89b4299e9fa6062e83b7a3ee46434aaf73f3ed4f39f0f1f1b75acfa37'
 
   url "https://www.macdvdripperpro.com/MDRP_v#{version.no_dots}.zip"
   appcast "https://www.macdvdripperpro.com/mdrp_sparkle#{version.major}.xml"
