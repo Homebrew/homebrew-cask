f53ac168a7f84694c7a00058b67fbd43a91b56d7
sermelipharo <sermelipharo@Suzuha.local>
2020-06-29 01:47:31 +0300

    Update yandex-disk from 3.1.19,30 to 3.1.20,30

--- Casks/yandex-disk.rb
+++ Casks/yandex-disk.rb
@@ -1,6 +1,6 @@
 cask 'yandex-disk' do
-  version '3.1.19,30'
-  sha256 '8259906fb6b1032c77987228d3dd120da08e564ff1028eeea8332b69b29b393a'
+  version '3.1.20,30'
+  sha256 '2a91be3187b8d245e9a936a1098a4bb9e83085031fe03054da91447032ad9ff0'
 
   url "https://disk.yandex.ru/download/YandexDisk#{version.after_comma}.dmg/?instant=1"
   name 'Yandex.Disk'
