170c452f2377794c8ce8281fa29784fd7809ae9f
Randall <ran.dall@icloud.com>
2020-06-28 15:27:10 -0700

    Update therm from 0.4.1 to 0.4.2

--- Casks/therm.rb
+++ Casks/therm.rb
@@ -1,6 +1,6 @@
 cask 'therm' do
-  version '0.4.1'
-  sha256 '4b896ed75a24c7f3a3af163a265aa15d3593f487dde071e8a4bae24b52111efe'
+  version '0.4.2'
+  sha256 '7300338f81d3bb891852875aef58e93e99b1684f7f204158b99713bc94f8b22a'
 
   url "https://github.com/trufae/Therm/releases/download/#{version}/Therm-#{version}.zip"
   appcast 'https://github.com/trufae/Therm/releases.atom'
