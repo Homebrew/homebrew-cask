d199139c7dcf81b3e7542cb546b2824968cf9114
Randall <ran.dall@icloud.com>
2020-06-28 15:29:20 -0700

    Update boop from 1.0.0 to 1.1.0

--- Casks/boop.rb
+++ Casks/boop.rb
@@ -1,6 +1,6 @@
 cask 'boop' do
-  version '1.0.0'
-  sha256 'f04b3cbb9659a0879c90b915163c8278a6106bc75d82465a8cd8ce6fef62705c'
+  version '1.1.0'
+  sha256 '73842c977ee48b9905e94769fbfedc4a9c29881d280ce65ee06635079b3f6660'
 
   # github.com/IvanMathy/Boop was verified as official when first introduced to the cask
   url "https://github.com/IvanMathy/Boop/releases/download/#{version}/Boop.zip"
