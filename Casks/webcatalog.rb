cab756f995ff1fb181281ea50b91fbf5034f38f6
runner <runner@Mac-1538.local>
2020-06-29 08:57:18 +0000

    Update webcatalog from 22.0.2 to 22.1.0

--- Casks/webcatalog.rb
+++ Casks/webcatalog.rb
@@ -1,6 +1,6 @@
 cask 'webcatalog' do
-  version '22.0.2'
-  sha256 'e691f6a61a8bfbb0bea4a74653f0006ede92f7697b6e38d228e5f1db9c2b53fd'
+  version '22.1.0'
+  sha256 '45e9c957f2de037ecdeb19143e71610edcbe67268bdb2ac49d74ea2742fbf1b7'
 
   # github.com/quanglam2807/webcatalog/ was verified as official when first introduced to the cask
   url "https://github.com/quanglam2807/webcatalog/releases/download/v#{version}/WebCatalog-#{version}-mac.zip"
