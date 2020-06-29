bff65b710d0bd9dc34c7d8664088fc4e70e63908
Randall <17261190+ran-dall@users.noreply.github.com>
2020-06-29 09:34:02 -0700

    Update menutube from 1.7.2 to 1.7.3 (#85154)

--- Casks/menutube.rb
+++ Casks/menutube.rb
@@ -1,6 +1,6 @@
 cask 'menutube' do
-  version '1.7.2'
-  sha256 '3553cd053f622837fac6e365c19daa5946ce72c8ba10eadae4afb11c7fa57851'
+  version '1.7.3'
+  sha256 '2428a68c085c9a10407eca1d778c2d5b3d37e4d0dfadb12376b44eab2f9f43a1'
 
   # github.com/edanchenkov/MenuTube/ was verified as official when first introduced to the cask
   url "https://github.com/edanchenkov/MenuTube/releases/download/#{version}/MenuTube-#{version}.dmg"
