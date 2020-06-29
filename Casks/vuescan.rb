f4b7c24ed9d332e7efc99c0dd49e50a5f89d35df
Randall <ran.dall@icloud.com>
2020-06-29 09:26:31 -0700

    Update vuescan from 9.7.28 to 9.7.29

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,5 +1,5 @@
 cask 'vuescan' do
-  version '9.7.28'
+  version '9.7.29'
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
