a632707f730ad6c31b3299c8b143aa25099a8669
Randall <ran.dall@icloud.com>
2020-06-29 09:17:02 -0700

    Update gnucash from 3.11,1 to 4.0,2

--- Casks/gnucash.rb
+++ Casks/gnucash.rb
@@ -1,6 +1,6 @@
 cask 'gnucash' do
-  version '3.11,1'
-  sha256 '840eb08731a58e463efaf640bd011637d3f23a6125ee313c167de1f1640424dd'
+  version '4.0,2'
+  sha256 'c470b3415b7a6c363f237193f7c6df38bbc6d9b6deabf4bd8b6a25f34f29d7f1'
 
   # github.com/Gnucash/gnucash/ was verified as official when first introduced to the cask
   url "https://github.com/Gnucash/gnucash/releases/download/#{version.before_comma}/Gnucash-Intel-#{version.before_comma.chomp('b')}-#{version.after_comma}.dmg"
