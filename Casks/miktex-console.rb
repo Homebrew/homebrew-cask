e2e026d856145d2395e6052f8c5ecb0f18d15e60
suschizu <brigitte@corecode.io>
2020-06-30 16:07:35 +0200

    Update miktex-console from 2.9.7445-1 to 20.6 (#85166)

--- Casks/miktex-console.rb
+++ Casks/miktex-console.rb
@@ -1,6 +1,6 @@
 cask 'miktex-console' do
-  version '2.9.7445-1'
-  sha256 '8b8e19486ca519a1594de5248d6d3269bfd3b13505079924b6c601e189e272db'
+  version '20.6'
+  sha256 '8e28dfec0a309bd152f697acc0ca2ab751fa52ad1313a6947ea665e968c11010'
 
   url "https://miktex.org/download/ctan/systems/win32/miktex/setup/darwin-x86_64/miktex-#{version}-darwin-x86_64.dmg"
   appcast 'https://miktex.org/download'
