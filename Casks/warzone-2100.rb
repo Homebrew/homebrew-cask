ee46ce841334c937448c1e6baf6effba182959bf
Vítor Galvão <info@vitorgalvao.com>
2020-06-30 15:47:06 +0100

    Update warzone-2100.rb

--- Casks/warzone-2100.rb
+++ Casks/warzone-2100.rb
@@ -2,7 +2,7 @@ cask 'warzone-2100' do
   version '3.4.0'
   sha256 'c74ffe361f41965ea46a4e1e63685210584cba3281c7a85e7ce92d2bd7fef490'
 
-  # github.com/Warzone2100 was verified as official when first introduced to the cask
+  # github.com/Warzone2100/warzone2100/ was verified as official when first introduced to the cask
   url "https://github.com/Warzone2100/warzone2100/releases/download/#{version}/warzone2100_macOS.zip"
   appcast 'https://github.com/Warzone2100/warzone2100/releases.atom'
   name 'Warzone 2100'
