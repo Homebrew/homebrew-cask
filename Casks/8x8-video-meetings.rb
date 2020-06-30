e1a19a01fa1dc72db55809175865f5da31e31848
Vítor Galvão <info@vitorgalvao.com>
2020-06-30 15:37:06 +0100

    Update 8x8-video-meetings.rb

--- Casks/8x8-video-meetings.rb
+++ Casks/8x8-video-meetings.rb
@@ -2,7 +2,7 @@ cask '8x8-video-meetings' do
   version :latest
   sha256 :no_check
 
-  # vod-updates.8x8.com/ga was verified as official when first introduced to the cask
+  # vod-updates.8x8.com/ was verified as official when first introduced to the cask
   url 'https://vod-updates.8x8.com/ga/meet-dmg-latest.dmg'
   name '8x8 Video Meetings'
   homepage 'https://8x8.vc/'
