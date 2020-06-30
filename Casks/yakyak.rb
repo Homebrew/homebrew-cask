dc87e4a3099d37baa40d3edde06f648043151283
jason <jason@evilpaws.org>
2020-06-30 10:56:23 -0500

    Update yakyak from 1.5.7 to 1.5.8

--- Casks/yakyak.rb
+++ Casks/yakyak.rb
@@ -1,6 +1,6 @@
 cask 'yakyak' do
-  version '1.5.7'
-  sha256 '4fe6c97824cf90897035a7bcc612bf53caccc3844329c6e419aa4556b0f4794a'
+  version '1.5.8'
+  sha256 'c274d3ff966cd7399432344ce2a701025f9f9a325f28f0255ba74fb203bab91e'
 
   url "https://github.com/yakyak/yakyak/releases/download/v#{version}/yakyak-#{version}-osx.zip"
   appcast 'https://github.com/yakyak/yakyak/releases.atom'
