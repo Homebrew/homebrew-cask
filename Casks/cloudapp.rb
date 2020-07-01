9d3cf1711815d6330429c95dac8f494d6c2cd1d6
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-01 15:18:30 -0700

    Update cloudapp from 6.1.2.2002 to 6.2.1.2088 (#85282)

--- Casks/cloudapp.rb
+++ Casks/cloudapp.rb
@@ -1,6 +1,6 @@
 cask 'cloudapp' do
-  version '6.1.2.2002'
-  sha256 '357fa5e50ced82cf213b8b88d25d4296284e6a032a0224ed03a52547902a861f'
+  version '6.2.1.2088'
+  sha256 '1c69aa469dd156f234ce6715a06094cc5a486d4280ddd17abf07238a9f37df38'
 
   url "http://downloads.getcloudapp.com/mac/CloudApp-#{version}.zip"
   appcast 'https://d2plwz9jdz9z5d.cloudfront.net/mac/latest/appcast.xml'
