e20ac57f618c7797c1fe3229eb48e506496ebf5b
suschizu <brigitte@corecode.io>
2020-07-01 17:20:00 +0200

    Update polymail from 2.2.1 to 2.2.3 (#85240)

--- Casks/polymail.rb
+++ Casks/polymail.rb
@@ -1,6 +1,6 @@
 cask 'polymail' do
-  version '2.2.1'
-  sha256 '9acc1247cd2ff64d88ca9693c4f3a25e7e2e367a11fd11f85588e7e14353f43c'
+  version '2.2.3'
+  sha256 '3e15827725f0ac69943d846bfc6fcbfaf3a8123cf2e232d0f5272e18d4ec4f99'
 
   url "https://sparkle-updater.polymail.io/macos/builds/Polymail-v#{version}.zip"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://sparkle-updater.polymail.io/osx/Polymail-Latest.zip'
