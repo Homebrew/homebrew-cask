c22c0e5ae57f3b3fefca8fd5c912c3ed4cef4126
suschizu <brigitte@corecode.io>
2020-07-01 18:17:52 +0200

    Update shift from 5.0.65 to 5.0.72 (#85239)

--- Casks/shift.rb
+++ Casks/shift.rb
@@ -1,6 +1,6 @@
 cask 'shift' do
-  version '5.0.65'
-  sha256 '456837d6a0041d09a6c67bf4dc8d953de019a83091f87d62bc218dc3366c798e'
+  version '5.0.72'
+  sha256 'f02685f3264da799c6fad885a3d6c38cf85537f342779f44d9ee560092c4cc26'
 
   url "https://update.tryshift.com/download/version/#{version}/osx_64"
   appcast 'https://tryshift.com/download/?platform=mac'
