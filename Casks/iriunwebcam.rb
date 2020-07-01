e98b1c4751c3e0af9aa9416fe2b2bcabc9eadf92
Julian Mayer <julian@corecode.io>
2020-07-01 17:10:59 +0200

    Update iriunwebcam

--- Casks/iriunwebcam.rb
+++ Casks/iriunwebcam.rb
@@ -2,7 +2,7 @@ cask 'iriunwebcam' do
   version '2.3.3'
   sha256 '6bc44b7f054042ade322de0d4aa4628d2112509ce3c87307294def474a9c871c'
 
-  # 1758658189.rsc.cdn77.org was verified as official when first introduced to the cask
+  # 1758658189.rsc.cdn77.org/ was verified as official when first introduced to the cask
   url "https://1758658189.rsc.cdn77.org/IriunWebcam-#{version}.pkg"
   appcast 'https://iriun.com/'
   name 'Iriun'
