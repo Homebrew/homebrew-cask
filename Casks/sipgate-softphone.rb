1db05ff4634c09b73f3e6c99c2fcb7f7a0d80c3e
Brigitte Zmoelnig <brigitte@corecode.io>
2020-07-01 15:52:58 +0200

    Update sipgate-softphone

--- Casks/sipgate-softphone.rb
+++ Casks/sipgate-softphone.rb
@@ -2,8 +2,8 @@ cask 'sipgate-softphone' do
   version '1.9.12'
   sha256 'fa938c5475cfcec00dce8778ffa1e2048922a81f4e4f11d053581b9dd3b742f6'
 
-  # sipgate-desktop-app.s3.eu-central-1.amazonaws.com was verified as official when first introduced to the cask
-  url 'https://sipgate-desktop-app.s3.eu-central-1.amazonaws.com/sipgate-softphone.dmg'
+  # sipgate-desktop-app-dev.s3.amazonaws.com was verified as official when first introduced to the cask
+  url "https://sipgate-desktop-app-dev.s3.amazonaws.com/sipgate-softphone-#{version}.dmg"
   appcast 'https://sipgate-desktop-app-dev.s3.amazonaws.com/latest-mac.yml'
   name 'sipgate softphone'
   homepage 'https://www.sipgateteam.de/softphone'
