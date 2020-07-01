9e39fd6e017d02b57d0a925babb58ff1d625266b
Julian Mayer <julian@corecode.io>
2020-07-01 17:08:34 +0200

    Update icq

--- Casks/icq.rb
+++ Casks/icq.rb
@@ -2,7 +2,7 @@ cask 'icq' do
   version '3.0.27790'
   sha256 '8ea26bca0d9cc06cc89946574b256752ac1b642726ed0c750314b894739bb051'
 
-  # icq-www.hb.bizmrg.com was verified as official when first introduced to the cask
+  # icq-www.hb.bizmrg.com/ was verified as official when first introduced to the cask
   url "https://icq-www.hb.bizmrg.com/mac/x64/#{version}/icq.dmg"
   appcast "https://icq-www.hb.bizmrg.com/mac/x64/#{version}/version.xml"
   name 'ICQ'
