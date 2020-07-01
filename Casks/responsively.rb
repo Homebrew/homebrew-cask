e19b37e5159100d09b2dfdd1f02c73bf87cb03e6
Randall <ran.dall@icloud.com>
2020-07-01 14:34:21 -0700

    Update responsively from 0.4.0 to 0.5.0

--- Casks/responsively.rb
+++ Casks/responsively.rb
@@ -1,6 +1,6 @@
 cask 'responsively' do
-  version '0.4.0'
-  sha256 '73f5f3bc7c3556be97ce99d4de0a5952c123736442793ccfa659160bbe674b28'
+  version '0.5.0'
+  sha256 '8f979ccb3bbc712d9975ea0043b8aef59d427099be7a4a7a60946ef59bc50806'
 
   # github.com/manojVivek/responsively-app/ was verified as official when first introduced to the cask
   url "https://github.com/manojVivek/responsively-app/releases/download/v#{version}/ResponsivelyApp-#{version}.dmg"
