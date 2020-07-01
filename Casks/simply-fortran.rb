efc280173b16edda0f5f1fd733681f27388de0d6
Randall <ran.dall@icloud.com>
2020-06-30 18:25:42 -0700

    Update simply-fortran

--- Casks/simply-fortran.rb
+++ Casks/simply-fortran.rb
@@ -5,9 +5,9 @@ cask 'simply-fortran' do
     sha256 'ebe257a6715fae7869b16a435547679a74b65fb2af8767c32a687262535e7d4c'
     url "https://download.simplyfortran.com/#{version.major_minor}/macos/simplyfortran-#{version}.legacy.dmg"
   else
-    version '3.12.3272'
+    version '3.13.3306'
 
-    sha256 '99e461f76eeb8ab39425efbd6a63332bcf136117c5cdad9abaf46e9a94a6a929'
+    sha256 'ebe257a6715fae7869b16a435547679a74b65fb2af8767c32a687262535e7d4c'
     url "https://download.simplyfortran.com/#{version.major_minor}/macos/simplyfortran-#{version}.dmg"
   end
   appcast 'https://simplyfortran.com/download/?platform=macos',
