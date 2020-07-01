ca9d02e324da854eca82be140e07df4338383c15
Randall <ran.dall@icloud.com>
2020-06-30 17:53:25 -0700

    Update dynobase from 0.8.3 to 0.8.4

--- Casks/dynobase.rb
+++ Casks/dynobase.rb
@@ -1,6 +1,6 @@
 cask 'dynobase' do
-  version '0.8.3'
-  sha256 'b21991aa4a62e02d10aba8be031af943d959252e8053b72396a3ec8efcb438e3'
+  version '0.8.4'
+  sha256 'f57ba3510c3191a62ad8701e15640280cd024fbed294c4c984db6fef862a254c'
 
   # github.com/Dynobase/dynobase was verified as official when first introduced to the cask
   url "https://github.com/Dynobase/dynobase/releases/download/#{version}/Dynobase-#{version}.dmg"
