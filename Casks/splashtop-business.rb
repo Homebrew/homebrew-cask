4de8dda03b72a25b0bab5bf46906008de84a8970
Randall <ran.dall@icloud.com>
2020-06-30 18:03:29 -0700

    Update splashtop-business from 3.3.8.0 to 3.4.0.0

--- Casks/splashtop-business.rb
+++ Casks/splashtop-business.rb
@@ -1,6 +1,6 @@
 cask 'splashtop-business' do
-  version '3.3.8.0'
-  sha256 '7b3d1256f08fd722e233531744f0d340694d7d68177609a5a9b2bf2d474720fc'
+  version '3.4.0.0'
+  sha256 '323fc07eb4bf55d2a580cc4a1d0e4412b18d8f653bd7530be16a0d51dbf4188f'
 
   # d17kmd0va0f0mp.cloudfront.net/macclient/STB/ was verified as official when first introduced to the cask
   url "https://d17kmd0va0f0mp.cloudfront.net/macclient/STB/Splashtop_Business_Mac_INSTALLER_v#{version}.dmg"
