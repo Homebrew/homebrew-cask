8253d1a1f8c98c877005b45e070d34ea1555c587
CoreCode <core-code@users.noreply.github.com>
2020-07-02 00:44:04 +0200

    Update quickkeyextension (#85246)

--- Casks/quickkeyextension.rb
+++ Casks/quickkeyextension.rb
@@ -2,7 +2,7 @@ cask 'quickkeyextension' do
   version :latest
   sha256 :no_check
 
-  # littlefin.com was verified as official when first introduced to the cask
+  # littlefin.com/ was verified as official when first introduced to the cask
   url 'http://www.littlefin.com/downloads/qkextension.zip'
   name 'QuickKey Extension'
   homepage 'http://quickkey-app.com/extension/'
