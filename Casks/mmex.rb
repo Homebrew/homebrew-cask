e1b51bdf70b403433282dbade5c434befd9304e1
Julian Mayer <julian@corecode.io>
2020-07-01 17:38:27 +0200

    Update mmex

--- Casks/mmex.rb
+++ Casks/mmex.rb
@@ -2,8 +2,8 @@ cask 'mmex' do
   version '1.3.5'
   sha256 'ed27e39373c0e74ea856e2cb91b3b6f99fb745b8ec8b83424f7e406e8428fa7d'
 
-  # sourceforge.net/projects/moneymanagerex/ was verified as official when first introduced to the cask
-  url 'https://sourceforge.net/projects/moneymanagerex/files/latest/download'
+  # downloads.sourceforge.net/moneymanagerex/ was verified as official when first introduced to the cask
+  url "https://downloads.sourceforge.net/moneymanagerex/v#{version}/mmex-#{version}-macos10.9-wx3.1.3.dmg"
   appcast 'https://github.com/moneymanagerex/moneymanagerex/releases.atom'
   name 'Money Manager Ex'
   homepage 'https://www.moneymanagerex.org/'
