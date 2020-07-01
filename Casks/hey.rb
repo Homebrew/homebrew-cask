f32e9b1ae110cc53c59e2de907eb1afee6f05aab
CoreCode <core-code@users.noreply.github.com>
2020-07-02 00:43:05 +0200

    Update hey (#85241)

--- Casks/hey.rb
+++ Casks/hey.rb
@@ -2,7 +2,7 @@ cask 'hey' do
   version '1.0.6'
   sha256 'd8ac5db02bf311bf1ade3a5e69a9bc587bbb56df0d2792be029071a1571c9c13'
 
-  # hey-desktop.s3.amazonaws.com was verified as official when first introduced to the cask
+  # hey-desktop.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://hey-desktop.s3.amazonaws.com/HEY-#{version}.dmg"
   appcast 'https://hey-desktop.s3.amazonaws.com/latest-mac.yml'
   name 'Hey'
