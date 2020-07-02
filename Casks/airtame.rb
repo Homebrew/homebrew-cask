f01e5cfcde5327bda90e82f335a1de033865e059
Randall <ran.dall@icloud.com>
2020-07-02 11:31:55 -0700

    Update airtame from 4.0.1 to 4.1.0

--- Casks/airtame.rb
+++ Casks/airtame.rb
@@ -1,6 +1,6 @@
 cask 'airtame' do
-  version '4.0.1'
-  sha256 '159e7ccbebbe9028256422ac7e33b27b0daf45876be0a11c87393aae2d3c87a6'
+  version '4.1.0'
+  sha256 'aa90d0e147f9db6159ed21ee81e2141833fdcf059796a3ec93a2a63d247a3f23'
 
   url "https://downloads-cdn.airtame.com/app/latest/mac/Airtame-#{version}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://downloads-cdn.airtame.com/get.php?platform=osx_x64'
