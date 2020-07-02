1538cf24e44850d8f11591530b1ac6a2c71e671b
Marvin Schlegel <marvin.schlegel@picturepipe.com>
2020-07-02 09:30:34 +0200

    silentknight.rb: update from 1.7 to 1.8 (#85297)

--- Casks/silentknight.rb
+++ Casks/silentknight.rb
@@ -1,9 +1,9 @@
 cask 'silentknight' do
-  version '1.7'
-  sha256 '23b0c313244c4e40954d648a313cd3a9333db567b6e6b2aca591b3c610923cf4'
+  version '1.8'
+  sha256 '6076892e23d989f81f3050ef2a70ebe12979dd8480fb40a3c76890927d277550'
 
   # eclecticlightdotcom.files.wordpress.com/ was verified as official when first introduced to the cask
-  url "https://eclecticlightdotcom.files.wordpress.com/2020/05/silentknight#{version.no_dots}.zip"
+  url "https://eclecticlightdotcom.files.wordpress.com/2020/06/silentknight#{version.no_dots}.zip"
   appcast 'https://eclecticlight.co/lockrattler-systhist/'
   name 'SilentKnight'
   homepage 'https://eclecticlight.co/lockrattler-systhist/'
