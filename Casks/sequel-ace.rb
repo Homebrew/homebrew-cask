9cf2990821d906551feaa1cda16f0dd41493b5ba
Anuj Das <anujdas@gmail.com>
2020-07-02 00:06:18 -0700

    Update sequel-ace from 2.0.2 to 2.1.0 (#85295)

--- Casks/sequel-ace.rb
+++ Casks/sequel-ace.rb
@@ -1,6 +1,6 @@
 cask 'sequel-ace' do
-  version '2.0.2'
-  sha256 'c92518b7ac0d3800071cb831f55b7de4ac8c04e9ba0a3be589287e10c83247ac'
+  version '2.1.0'
+  sha256 '4e01b23cedcbbd5c087a46654f5707c591f80360022e24b16ed7ae0b7e3f8340'
 
   url "https://github.com/Sequel-Ace/Sequel-Ace/releases/download/#{version}/Sequel-Ace-#{version}-release.zip"
   appcast 'https://github.com/Sequel-Ace/Sequel-Ace/releases.atom'
