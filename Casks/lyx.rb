bc17661b940d484bcb930f67de0b7f53aeabca08
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-02 18:19:35 +0000

    Update lyx from 2.3.5.1 to 2.3.5.2 (#85334)

--- Casks/lyx.rb
+++ Casks/lyx.rb
@@ -1,6 +1,6 @@
 cask 'lyx' do
-  version '2.3.5.1'
-  sha256 'b4186192cec8f84f5140f88d42d49b1b03f31f58e534a0b575fa4d039e56eea2'
+  version '2.3.5.2'
+  sha256 '490433a7bfecc790e2764feb1811320b4cad85682939de5660d7c8ca3ec15dde'
 
   # ftp.lip6.fr/pub/lyx/ was verified as official when first introduced to the cask
   url "https://ftp.lip6.fr/pub/lyx/bin/#{version.major_minor_patch}/LyX-#{version}+qt5-x86_64-cocoa.dmg"
