f68fcb495cdebc14ba383199afe2d5bccc3053c5
Marvin Schlegel <marvin.schlegel@picturepipe.com>
2020-07-02 10:26:58 +0200

    silentknight.rb: Derive URL from version
    Inspired by lockrattler.rb Cask, this change will make it easier for users to submit updates,
    as they can simply use `brew cask-repair`.

--- Casks/silentknight.rb
+++ Casks/silentknight.rb
@@ -1,14 +1,14 @@
 cask 'silentknight' do
-  version '1.8'
+  version '1.8,2020.06'
   sha256 '6076892e23d989f81f3050ef2a70ebe12979dd8480fb40a3c76890927d277550'
 
   # eclecticlightdotcom.files.wordpress.com/ was verified as official when first introduced to the cask
-  url "https://eclecticlightdotcom.files.wordpress.com/2020/06/silentknight#{version.no_dots}.zip"
+  url "https://eclecticlightdotcom.files.wordpress.com/#{version.after_comma.major}/#{version.after_comma.minor}/silentknight#{version.before_comma.no_dots}.zip"
   appcast 'https://eclecticlight.co/lockrattler-systhist/'
   name 'SilentKnight'
   homepage 'https://eclecticlight.co/lockrattler-systhist/'
 
-  app "silentknight#{version.no_dots}/SilentKnight.app"
+  app "silentknight#{version.before_comma.no_dots}/SilentKnight.app"
 
   zap trash: [
                '~/Library/Caches/co.eclecticlight.SilentKnight',
