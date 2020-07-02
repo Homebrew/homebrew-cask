46f6f4d3029e4276bf012dbb7ad20461577f9e81
bgrnwd <bgwood18@gmail.com>
2020-07-02 11:17:13 -0400

    add sha256

--- Casks/freetube.rb
+++ Casks/freetube.rb
@@ -1,6 +1,6 @@
 cask 'freetube' do
   version '0.7.3'
-  sha256 :no_check
+  sha256 '2bd054ad61e2a9754e69a200311f3a0f8fca09d0f3429e6130dbe3ae8936567e'
 
   url "https://github.com/FreeTubeApp/FreeTube/releases/download/v#{version}-beta/FreeTube-#{version}-mac.dmg"
   appcast 'https://github.com/FreeTubeApp/FreeTube/releases.atom'
