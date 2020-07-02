e0ea07d67a970c9cd66a2b7d0795d8eb5fcbae88
Randall <ran.dall@icloud.com>
2020-07-02 11:25:53 -0700

    Update fm3-edit from 1.01.02 to 1.01.05

--- Casks/fm3-edit.rb
+++ Casks/fm3-edit.rb
@@ -1,6 +1,6 @@
 cask 'fm3-edit' do
-  version '1.01.02'
-  sha256 'f4b89d42b45b8ff674e1bafcb22e4010253f86f0410abafdaf2148c71e8c9d89'
+  version '1.01.05'
+  sha256 '17398d482cd963f9806141fd9b44e942a2129e7745afdf04e639185057b58702'
 
   url "https://www.fractalaudio.com/downloads/FM3-Edit/FM3-Edit-OSX-v#{version.tr('.', 'p')}.dmg"
   appcast 'https://www.fractalaudio.com/fm3-edit/'
