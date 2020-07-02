4827abd20bec39974c5ceaac68387f2e37363ff0
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-02 18:21:52 +0000

    Update virtualc64 from 3.3.2 to 3.4 (#85331)

--- Casks/virtualc64.rb
+++ Casks/virtualc64.rb
@@ -1,7 +1,7 @@
 cask 'virtualc64' do
   # note: "64" is not a version number, but an intrinsic part of the product name
-  version '3.3.2'
-  sha256 'ffe82a07306c56065e6e76b212ee4918308c93b72ba73e3ef7be8f6b5cd579ac'
+  version '3.4'
+  sha256 '542145ec4d6b71b5a05a5bfe55b30fc7792dd8eaf363e666a426eb6e045e8af4'
 
   url "http://www.dirkwhoffmann.de/virtualc64/VirtualC64_#{version}.zip"
   appcast 'http://dirkwhoffmann.de/virtualc64/VirtualC64Appcast.xml'
