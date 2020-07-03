5b957c46dc3118966c8d2cd40c5a48e8eb55859f
Michael Dahl <micdah@gmail.com>
2020-07-03 10:07:30 +0200

    Update studio-3t from 2020.5.0 to 2020.6.0

--- Casks/studio-3t.rb
+++ Casks/studio-3t.rb
@@ -1,6 +1,6 @@
 cask 'studio-3t' do
-  version '2020.5.0'
-  sha256 'd5419bf198cb94071420f499f6fbca36b6be1bd68f1fb26dcbf461f6333096ba'
+  version '2020.6.0'
+  sha256 '00bd75db9e0c34d4852735fa5f89f48b62609da2759ef0c5b2fe306a9e8dcda0'
 
   url "https://download.studio3t.com/studio-3t/mac/#{version}/Studio-3T.dmg"
   appcast 'https://files.studio3t.com/changelog/changelog.txt'
