ddf5c7744901530f4b0c214e3f7cf7d28a4d0500
suschizu <brigitte@corecode.io>
2020-07-03 18:55:34 +0200

    Update jasp.rb

--- Casks/jasp.rb
+++ Casks/jasp.rb
@@ -4,11 +4,11 @@ cask 'jasp' do
   if MacOS.version <= :mojave
     sha256 'adb48f890645868140e9252c087d6d16e7e41059f9336fbefe5a7ec7bf011e19'
 
-    url "http://static.jasp-stats.org/JASP-#{version}-preCatalina.dmg"
+    url "https://static.jasp-stats.org/JASP-#{version}-preCatalina.dmg"
   else
     sha256 '22a27b1842b34935d753d79cd3bd4398470220c40dd5b09bc8e601d264f8ccc8'
 
-    url "http://static.jasp-stats.org/JASP-#{version}-Catalina.dmg"
+    url "https://static.jasp-stats.org/JASP-#{version}-Catalina.dmg"
   end
   appcast 'https://jasp-stats.org/download/',
           must_contain: version.sub(%r{(.0)+$}, '')
