5dc4760a628a0261ec73b45c4ada119c4b0adb90
Borek Bernard <borekb@gmail.com>
2020-07-03 16:43:00 +0200

    Update checksum of DeepGit 4.0.1

--- Casks/deepgit.rb
+++ Casks/deepgit.rb
@@ -1,6 +1,6 @@
 cask 'deepgit' do
   version '4.0.1'
-  sha256 'cabc3a8c4e8b507fa3f999052a3cc1f6f8ddb06e4cfcdeb9c1d9c2ca6c34351f'
+  sha256 'e1905388c46e1e73e27fdc260350c95923186d962ef5bdc9c59ebba155f53041'
 
   url "https://www.syntevo.com/downloads/deepgit/deepgit-macosx-#{version.dots_to_underscores}.dmg"
   appcast 'https://syntevo.com/deepgit/download'
