fe4de1f60cfda6f162094b1f776457a418299065
tiiiecherle <tiiiecherle@gmail.com>
2020-07-03 10:14:34 +0200

    Update jitsi

--- Casks/jitsi.rb
+++ Casks/jitsi.rb
@@ -2,7 +2,8 @@ cask 'jitsi' do
   version '2.10.5550'
   sha256 'd902af9dde7b1fde6f76af5f97e4f27d6b853bd9d3e83b2fec5292dda787a0da'
 
-  url "https://download.jitsi.org/jitsi/macosx/jitsi-#{version}.dmg"
+  # github.com/jitsi was verified as official when first introduced to the cask
+  url "https://github.com/jitsi/jitsi/releases/download/Jitsi-#{version.major_minor}/jitsi-#{version}.dmg"
   appcast 'https://download.jitsi.org/jitsi/macosx/sparkle/updates.xml'
   name 'Jitsi'
   homepage 'https://jitsi.org/'
