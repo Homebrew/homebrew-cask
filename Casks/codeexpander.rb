ca57f437acc74d7168ed042de61dce6d896c00a1
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2020-07-05 00:48:55 +0200

    Update codeexpander from 3.3.6 to 3.5.0 (#85410)

--- Casks/codeexpander.rb
+++ Casks/codeexpander.rb
@@ -1,6 +1,6 @@
 cask 'codeexpander' do
-  version '3.3.6'
-  sha256 '54bf88b5da08f75ec5b93677860f0a284984d83db5f92446e4937b4c56af9f07'
+  version '3.5.0'
+  sha256 '835032d575715f57f1949156d2a7a441165ae17c398c45f30d6d58179faa3f02'
 
   url "https://github.com/oncework/codeexpander/releases/download/#{version.major_minor}.x/CodeExpander-#{version}.dmg"
   appcast 'https://github.com/oncework/codeexpander/releases.atom',
