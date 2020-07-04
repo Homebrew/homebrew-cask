12f5ac18f39f89ee6b6e380e199b5f3a91286fdc
Randall <ran.dall@icloud.com>
2020-07-03 17:15:55 -0700

    Update kui from 8.10.7 to 8.10.8

--- Casks/kui.rb
+++ Casks/kui.rb
@@ -1,6 +1,6 @@
 cask 'kui' do
-  version '8.10.7'
-  sha256 '56145d28843ceceff6441cd70bd21d57d4a47994d4a81765b37af787868dfc14'
+  version '8.10.8'
+  sha256 '2ab7ac0fb492a6ee4a620470e51bfb84e7c566ebf0d66ff320e3251d52779e94'
 
   # github.com/IBM/kui was verified as official when first introduced to the cask
   url "https://github.com/IBM/kui/releases/download/v#{version}/Kui-darwin-x64.tar.bz2"
