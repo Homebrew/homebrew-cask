927fe6af8763299265862fce36f9e41cc096afb9
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-04 23:37:52 +0000

    Update lmms from 1.2.1 to 1.2.2 (#85412)

--- Casks/lmms.rb
+++ Casks/lmms.rb
@@ -1,6 +1,6 @@
 cask 'lmms' do
-  version '1.2.1'
-  sha256 '157cca82f617e18edbfa3f9f9f91b4d44df048d4f865721bf46f4854f13495e9'
+  version '1.2.2'
+  sha256 'e5aa82086dc67817a763f3e54aa0786cdca590f26981584c07f9ff2ff1fb0503'
 
   # github.com/LMMS/lmms/ was verified as official when first introduced to the cask
   url "https://github.com/LMMS/lmms/releases/download/v#{version}/lmms-#{version}-mac10.13.dmg"
