651f06d0cf51e0f4508bb0be63fabcb777a428ef
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-04 00:35:40 +0000

    Update fedora-media-writer from 4.1.5 to 4.1.6 (#85379)

--- Casks/fedora-media-writer.rb
+++ Casks/fedora-media-writer.rb
@@ -1,6 +1,6 @@
 cask 'fedora-media-writer' do
-  version '4.1.5'
-  sha256 'c7e2284cb0a5fb5f4f54b8ed939b1549a09df3230e777ef7060b4d3b7a47e25c'
+  version '4.1.6'
+  sha256 '6a9d9f14423f98197730b097a9121f3c833baec489989e6d1168f353ae80c579'
 
   # github.com/FedoraQt/MediaWriter/ was verified as official when first introduced to the cask
   url "https://github.com/FedoraQt/MediaWriter/releases/download/#{version}/FedoraMediaWriter-osx-#{version}.unnotarized.dmg"
