8cdbf4539d08aa4f5974fe34d2f28bf963431d31
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2020-07-05 00:48:38 +0200

    Update chrysalis from 0.7.7 to 0.7.9 (#85411)

--- Casks/chrysalis.rb
+++ Casks/chrysalis.rb
@@ -1,6 +1,6 @@
 cask 'chrysalis' do
-  version '0.7.7'
-  sha256 '90decd69c206f0e55b66e47b414b17588379b607d5ccb14cdf448674d0474c42'
+  version '0.7.9'
+  sha256 '00f3ed6f9d6b4f45529996d9d9a344dc76f30ff424b9660f74ef6002e05bf6c7'
 
   url "https://github.com/keyboardio/Chrysalis/releases/download/chrysalis-#{version}/Chrysalis-#{version}.dmg"
   appcast 'https://github.com/keyboardio/Chrysalis/releases.atom'
