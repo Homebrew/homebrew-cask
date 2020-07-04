9201ac4d990f1c23f6892a6f096be1fcf4a56fcf
Thomas J. Faulkenberry <faulkenberry@tarleton.edu>
2020-07-04 05:27:36 -0500

    Update JASP to version 0.13.0.0

--- Casks/jasp.rb
+++ Casks/jasp.rb
@@ -1,14 +1,14 @@
 cask 'jasp' do
-  version '0.12.2.0'
-  sha256 '90fad0e14152066b9d93c67abb2894af3b6e89b3917294db4224747138eb0c8f'
+  version '0.13.0.0'
+  sha256 '22a27b1842b34935d753d79cd3bd4398470220c40dd5b09bc8e601d264f8ccc8'
 
-  url "https://static.jasp-stats.org/JASP-#{version}.dmg"
+  url "https://static.jasp-stats.org/JASP-#{version}-Catalina.dmg"
   appcast 'https://jasp-stats.org/download/',
           must_contain: version.sub(%r{(.0)+$}, '')
   name 'JASP'
   homepage 'https://jasp-stats.org/'
 
-  depends_on macos: '>= :high_sierra'
+  depends_on macos: '>= :catalina'
 
   app 'JASP.app'
 end
