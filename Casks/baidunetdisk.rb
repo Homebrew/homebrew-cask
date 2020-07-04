d08288df2a4bcb8199f6dd5957fb00d1b1d17c1c
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-04 01:17:00 +0000

    Update baidunetdisk from 3.3.0 to 3.3.1 (#85389)

--- Casks/baidunetdisk.rb
+++ Casks/baidunetdisk.rb
@@ -1,6 +1,6 @@
 cask 'baidunetdisk' do
-  version '3.3.0'
-  sha256 '97f59856e36a7ed8cb1c2a81d774ab959e9a563bc8ced4c3be593a0fca58507f'
+  version '3.3.1'
+  sha256 'a2e0e8f565e7b57cc933b592bc7fedaa9b10189993756f8ea8d2734e64ef0181'
 
   # baidupcs.com/issue/netdisk/MACguanjia/ was verified as official when first introduced to the cask
   url "https://wppkg.baidupcs.com/issue/netdisk/MACguanjia/BaiduNetdisk_mac_#{version}.dmg"
