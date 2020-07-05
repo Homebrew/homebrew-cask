01954adcfc14bf3986219c2000ee4e0f0b9119c1
Meet Godhani <meetgodhani23@gmail.com>
2020-07-05 10:33:48 -0400

    Update raven-reader from 0.8.2 to 0.8.3 (#85428)

--- Casks/raven-reader.rb
+++ Casks/raven-reader.rb
@@ -1,6 +1,6 @@
 cask 'raven-reader' do
-  version '0.8.2'
-  sha256 '9c4ea41fe0a2d4e12c253e123f45a9fc1bd3b1fbbfbc92f557e6e6698b4819b7'
+  version '0.8.3'
+  sha256 'e1d2796df71d2c0202ac4929aeecb548cc0e2fb6e54f392cd2eddade2ab5776a'
 
   url "https://download.ravenreader.app/ravenreader/Raven%20Reader-#{version}-mac.zip"
   appcast 'https://download.ravenreader.app/ravenreader/latest-mac.yml'
