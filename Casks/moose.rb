1c6c0246e7dd63577a648d405c24fcf05b5c35b8
David B. Waters <mrdavidbwaters@gmail.com>
2020-07-05 14:22:05 -0400

    initial

---
+++ Casks/moose.rb
@@ -0,0 +1,11 @@
+cask 'moose' do
+  version '0.6.2'
+  sha256 '79c5eaee0187fc0b131ffabe9d6abd04c39d936c3b096919390aaf12abaecac3'
+
+  # github.com/fitztrev/ritz078/moose was verified as official when first introduced to the cask
+  url "https://github.com/ritz078/moose/releases/download/v#{version}/moose-#{version}-mac.zip"
+  name 'moose'
+  homepage 'https://getmoose.in/'
+
+  app 'moose.app'
+end
