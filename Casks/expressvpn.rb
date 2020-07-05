8216c701d3ba42cf3beb06d62f2d94edbcd98a89
Brigitte Zmoelnig <brigitte@corecode.io>
2020-07-05 19:59:37 +0200

    Update expressvpn from 7.8.4.2 to 7.9.1.1

--- Casks/expressvpn.rb
+++ Casks/expressvpn.rb
@@ -1,6 +1,6 @@
 cask 'expressvpn' do
-  version '7.8.4.2'
-  sha256 '01616e697516b6cef8986b0ff71290cf041a68f45e20ab7762c198af670ac4a3'
+  version '7.9.1.1'
+  sha256 '333f15d1fe75eeeb08fb4a4c25991601868087718ecc9443970a905284a123df'
 
   url "https://download.expressvpn.xyz/clients/mac/expressvpn_mac_#{version}_release.pkg"
   appcast 'https://www.expressvpn.xyz/vpn-software/vpn-mac'
