e4c0a918ab74b9d98f3d27931807474fc2f93288
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-06 21:35:07 +0000

    Update macbreakz from 5.34 to 5.35 (#85473)

--- Casks/macbreakz.rb
+++ Casks/macbreakz.rb
@@ -1,6 +1,6 @@
 cask 'macbreakz' do
-  version '5.34'
-  sha256 '347c0d6f2379d5fa1b3737d39535dc48531c7c0c4f4104874caaeda5f0c7bb80'
+  version '5.35'
+  sha256 '1a3cc6c0506bdd5b5b15ef0eb34013d367c9f3edb8354c5d087a5fc7177d2083'
 
   url "https://www.publicspace.net/download/MacBreakZ#{version.major}.dmg"
   appcast "https://www.publicspace.net/app/signed_mb#{version.major}.xml"
