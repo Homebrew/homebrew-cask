4fbc39e2a96ce1437f684381657f37cb2b97bd23
Ali Serhal <anserhal@gmail.com>
2020-07-05 23:12:45 -0500

    Add iCollections.app

---
+++ Casks/icollections.rb
@@ -0,0 +1,10 @@
+cask 'icollections' do
+  version '6.5.1'
+  sha256 '8683c01f66f7480679f10b14e2606012b815665b8713d4d34b5047918e4dd0be'
+
+  url "https://naarakstudio.com/download/iCollections.dmg"
+  name 'iCollections'
+  homepage 'https://naarakstudio.com/icollections/'
+
+  app 'iCollections.app'
+end
