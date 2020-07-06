39a0338c03c48800345d346c71fe4efa78b54b26
Ted Carnahan <ted@tedcarnahan.com>
2020-07-05 19:21:03 -0500

    Add MCreator minecraft mod editor

---
+++ Casks/mcreator.rb
@@ -0,0 +1,10 @@
+cask 'mcreator' do
+  version '2020.3'
+  sha256 'd3c4b39886ae70f10592502c02834b5ddfcadc4c52c4eb062cff24fa404c691a'
+
+  url "https://mcreator.net/repository/2020-3/MCreator%202020.3%20Mac%2064bit.dmg"
+  name 'MCreator'
+  homepage 'https://mcreator.net/'
+
+  app 'MCreator.app'
+end
