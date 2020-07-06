3df36f0c5692c3fc1c126997e9d76a645ba386d2
Ted Carnahan <ted@tedcarnahan.com>
2020-07-05 19:29:39 -0500

    Fix cask audit failure

--- Casks/mcreator.rb
+++ Casks/mcreator.rb
@@ -2,7 +2,7 @@ cask 'mcreator' do
   version '2020.3'
   sha256 'd3c4b39886ae70f10592502c02834b5ddfcadc4c52c4eb062cff24fa404c691a'
 
-  url "https://mcreator.net/repository/2020-3/MCreator%202020.3%20Mac%2064bit.dmg"
+  url 'https://mcreator.net/repository/2020-3/MCreator%202020.3%20Mac%2064bit.dmg'
   name 'MCreator'
   homepage 'https://mcreator.net/'
 
