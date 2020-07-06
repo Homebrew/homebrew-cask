237e80f75322ad0d386616a0204fa9eec79ad058
gottabeleGIT <sandesh.manik@icloud.com>
2020-07-06 16:14:20 -0500

    Update playonmac.rb (#85459)

--- Casks/playonmac.rb
+++ Casks/playonmac.rb
@@ -1,14 +1,11 @@
 cask 'playonmac' do
-  version '4.3.4'
-  sha256 '6aba4a4d57be74dc36f0eb9782dcd84d6a2a7366a60e2b8091bd53fd28a5cf32'
+  version '4.4'
+  sha256 'b3077c7d5da7794802db47a755a13ce91e33a0594e3abad907ed812c85493413'
 
   url "https://repository.playonmac.com/PlayOnMac/PlayOnMac_#{version}.dmg"
   appcast "https://github.com/PlayOnLinux/POL-POM-#{version.major}/releases.atom"
   name 'PlayOnMac'
   homepage 'https://www.playonmac.com/en'
-
-  # PlayOnMac doesn't work on Catalina, see https://www.playonmac.com/en/topic-16558-macOS_Catalina_When_will_PlayOnMac_be_ready.html#m65029
-  depends_on macos: '<= :mojave'
 
   app 'PlayOnMac.app'
 end
