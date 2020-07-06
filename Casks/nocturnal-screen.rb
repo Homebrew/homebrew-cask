190d6c2660e907d8791de3fc223aad11108dce1f
JoshJon <joshua.j.jon@gmail.com>
2020-07-06 18:07:52 +1000

    Add Nocturnal.app v1.1

---
+++ Casks/nocturnal-screen.rb
@@ -0,0 +1,11 @@
+cask 'nocturnal-screen' do
+  version '1.1'
+  sha256 'b3ab92067e1fa999ef09cadf6a26e23c1a46e6595fd64d9a1b614225fdbd4681'
+
+  url 'https://github.com/joshjon/nocturnal/releases/download/v1.1/Nocturnal-1.1.zip'
+  appcast 'https://github.com/joshjon/nocturnal/releases.atom'
+  name 'Nocturnal Screen'
+  homepage 'https://github.com/joshjon/nocturnal'
+
+  app 'Nocturnal.app'
+end
