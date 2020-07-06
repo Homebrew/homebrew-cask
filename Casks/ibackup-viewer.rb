8c5c55f6ca859badf1ab4f92cb6ff64b245554f6
tiiiecherle <tiiiecherle@gmail.com>
2020-07-06 17:21:34 +0200

    Update ibackup-viewer from 4.1590 to 4.1591

--- Casks/ibackup-viewer.rb
+++ Casks/ibackup-viewer.rb
@@ -1,6 +1,6 @@
 cask 'ibackup-viewer' do
-  version '4.1590'
-  sha256 'e7a2d58f37d5039e2524141a5b21e000e816ae3b9cd582d1072fd32acc538b49'
+  version '4.1591'
+  sha256 '9c60c08382570b845532a75d082501b05717049718b9ae27125bb14e0753efa8'
 
   url 'https://www.imactools.com/download/iBackupViewer.dmg'
   appcast 'https://www.imactools.com/update/ibackupviewer.xml'
