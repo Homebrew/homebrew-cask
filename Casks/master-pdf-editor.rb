3439e42ed27a44ac1d7ae8fa4c5bedb624d9a91a
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-06 04:49:43 +0000

    Update master-pdf-editor from 5.4.38 to 5.6.09 (#85449)

--- Casks/master-pdf-editor.rb
+++ Casks/master-pdf-editor.rb
@@ -1,6 +1,6 @@
 cask 'master-pdf-editor' do
-  version '5.4.38'
-  sha256 'fb716d23900168b7378c2dd732d519ac5c42e14f2d2b57ca16069369f4e87f6c'
+  version '5.6.09'
+  sha256 '29ae1264884a7903c76332a7398bbe5d4619b5bc9393bbdecdfd109a3fc5151c'
 
   url 'https://code-industry.net/public/MasterPDFEditor.dmg'
   appcast 'https://code-industry.net/get-masterpdfeditor/'
