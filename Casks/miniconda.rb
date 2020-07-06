62a9588bc617ddbea7a94af88e7ce45ce9640e75
Michael <albertmichaelj@users.noreply.github.com>
2020-07-06 10:51:25 -0400

    Revert unintentional changes to package installation
    In a previous commit (24f7d49cceb9cfcd420720de877e75c293acc447), the git commit message just said that the version was being updated, but I think the author of that commit @b0ttle actually used an old version of the file. They ended up undoing all of the great changes that @xyu had made to the cask in order to ensure that the cask installs at the standard location. This commit reverts the changes and updates the version to 4.8.3. Again, this commit is not introducing novel changes. It is reverting what seems to be unintentional changes that reverted great work done by @xyu.

--- Casks/miniconda.rb
+++ Casks/miniconda.rb
@@ -1,27 +1,32 @@
 cask 'miniconda' do
-  version 'py37_4.8.2'
-  sha256 'f3ede3a58d82fb5dcbca52d291a9edb5cd962d84d823a20693dd4bb27506cdd0'
+  version 'py37_4.8.3'
+  sha256 'bb2e3cedd2e78a8bb6872ab3ab5b1266a90f8c7004a22d8dc2ea5effeb6a439a'
 
   # repo.anaconda.com/miniconda/ was verified as official when first introduced to the cask
-  url "https://repo.anaconda.com/miniconda/Miniconda3-#{version}-MacOSX-x86_64.pkg"
+  url "https://repo.anaconda.com/miniconda/Miniconda3-#{version}-MacOSX-x86_64.sh"
   name 'Continuum Analytics Miniconda'
   homepage 'https://conda.io/miniconda.html'
 
   auto_updates true
+  container type: :naked
 
-  pkg "Miniconda3-#{version}-MacOSX-x86_64.pkg"
+  installer script: {
+                      executable: "Miniconda3-#{version}-MacOSX-x86_64.sh",
+                      args:       ['-b', '-p', "#{caskroom_path}/base"],
+                    }
+  binary "#{caskroom_path}/base/condabin/conda"
 
-  uninstall pkgutil: [
-                       'io.continuum.pkg.apreinstall',
-                       'io.continuum.pkg.conda.exe',
-                       'io.continuum.pkg.pathupdate',
-                       'io.continuum.pkg.postextract',
-                       'io.continuum.pkg.preconda',
-                     ]
+  uninstall delete: "#{caskroom_path}/base"
 
   zap trash: [
                '~/.condarc',
                '~/.conda',
                '~/.continuum',
              ]
+
+  caveats <<~EOS
+    Please run the following to setup your shell:
+
+      conda init "$(basename "${SHELL}")"
+  EOS
 end
