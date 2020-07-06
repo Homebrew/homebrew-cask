a9e365b96db506bd4d7d2d235add0f1f524ca187
zubieta <zubieta@github.com>
2020-07-06 14:07:27 -0700

    Update dbeaver-community from 7.1.1 to 7.1.2

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,6 +1,6 @@
 cask 'dbeaver-community' do
-  version '7.1.1'
-  sha256 '7f7434d8919d08dcd65024ccd2b121f6a319a0310cfa878586e19bbe0fe0b72f'
+  version '7.1.2'
+  sha256 '52c3dd40939852a9af0963af0ae57673f8bd3386d5680551495237f68fb47f6d'
 
   # github.com/dbeaver/dbeaver/ was verified as official when first introduced to the cask
   url "https://github.com/dbeaver/dbeaver/releases/download/#{version}/dbeaver-ce-#{version}-macos.dmg"
