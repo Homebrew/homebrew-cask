725db3209a99bf38139e39112316f8af82dea0eb
Randall <ran.dall@icloud.com>
2020-07-06 14:19:00 -0700

    Update neofinder from 7.5.2 to 7.6

--- Casks/neofinder.rb
+++ Casks/neofinder.rb
@@ -1,6 +1,6 @@
 cask 'neofinder' do
-  version '7.5.2'
-  sha256 'f7fae30616ed60257c8e7723bf884d73165548be6c341be9c808d619e6697ca0'
+  version '7.6'
+  sha256 '3f9ac55d08a086050f6ed7d40010571504a9228aef6fe86599e70b1a4d41934e'
 
   # wfs-apps.de/ was verified as official when first introduced to the cask
   url "https://www.wfs-apps.de/updates/neofinder.#{version}.zip"
