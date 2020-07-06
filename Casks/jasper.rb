d6fa9e78600080d09720082986cb21b2bf9bcba5
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-06 04:34:36 +0000

    Update jasper from 0.8.0 to 0.9.1 (#85441)

--- Casks/jasper.rb
+++ Casks/jasper.rb
@@ -1,6 +1,6 @@
 cask 'jasper' do
-  version '0.8.0'
-  sha256 '4c3c49b4b1b36f59c76fa810400e9f87b783a7d6eefe84cc4f20c9f3e2601110'
+  version '0.9.1'
+  sha256 'fef8e9ce1fd036bca14579749d9aae57953b24f919fd16e219952a5597fe0851'
 
   # github.com/jasperapp/jasper/ was verified as official when first introduced to the cask
   url "https://github.com/jasperapp/jasper/releases/download/v#{version}/jasper_v#{version}_mac.zip"
