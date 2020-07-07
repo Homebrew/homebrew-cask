8a5897b1385b634e2516e2a0f3efad7af37ed72e
Thibaut Rousseau <thibaut.rousseau.44@gmail.com>
2020-07-07 21:23:38 +0200

    Update copyclip from 2.9.97 to 2.9.98

--- Casks/copyclip.rb
+++ Casks/copyclip.rb
@@ -1,6 +1,6 @@
 cask 'copyclip' do
-  version '2.9.97'
-  sha256 'ef40942a284f8af9484682b28ae9aa922f4b54ebf2b1c1c5d1b0bf3aa95b60fc'
+  version '2.9.98'
+  sha256 '907948736d8872ac831cd92832553e0d4a46fb4f74e9e1f14a3f4fcfbbd2bb21'
 
   # rink.hockeyapp.net/api/2/apps/ffb436060eb379c0cb23097402e92379 was verified as official when first introduced to the cask
   url 'https://rink.hockeyapp.net/api/2/apps/ffb436060eb379c0cb23097402e92379?format=zip'
