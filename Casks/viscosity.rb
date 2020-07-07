aac906510940f6aabf43168517b1e880e8d862cf
David G Rosenberg <targendaz2@me.com>
2020-07-07 13:18:51 -0400

    Update viscosity from 1.8.5 to 1.8.6 (#85497)

--- Casks/viscosity.rb
+++ Casks/viscosity.rb
@@ -1,6 +1,6 @@
 cask 'viscosity' do
-  version '1.8.5'
-  sha256 '566f449b85d11439da4bcc86521475fa07701e6f1fd4c707bafc887b41dcfca1'
+  version '1.8.6'
+  sha256 'cea41c17b1dba0b3a6bcdfa51a0de3da5063534e2728476fbccb6f6b3f252b1b'
 
   url "https://swupdate.sparklabs.com/download/mac/release/viscosity/Viscosity%20#{version}.dmg"
   appcast 'https://swupdate.sparklabs.com/appcast/mac/release/viscosity/'
