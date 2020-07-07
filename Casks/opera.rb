2e9eeb9616bab840a3b08770a6a76eccef82c918
ashendon <ashendon@users.noreply.github.com>
2020-07-07 14:04:08 -0400

    Update opera from 69.0.3686.36 to 69.0.3686.49

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask 'opera' do
-  version '69.0.3686.36'
-  sha256 '07e691ca60876f91421d18c46b46df88642ee027565ce2c994c6937e2509ca00'
+  version '69.0.3686.49'
+  sha256 'ece66e8fde34ff84a370dfc1b2d627cef17f7fcb39bdbabffe99c14c385e5863'
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   appcast 'https://ftp.opera.com/pub/opera/desktop/'
