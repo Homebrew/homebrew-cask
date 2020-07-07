ce7d3ce312a12467b560a4df06eb986550bcadef
Miguel Wahnon Monteiro <551167+mikeziri@users.noreply.github.com>
2020-07-07 23:35:13 +0100

    Update keepassxc from 2.5.4 to 2.6.0 (#85507)

--- Casks/keepassxc.rb
+++ Casks/keepassxc.rb
@@ -1,5 +1,5 @@
 cask 'keepassxc' do
-  version '2.5.4'
+  version '2.6.0'
 
   if MacOS.version <= :sierra
     # github.com/keepassxreboot/keepassxc/ was verified as official when first introduced to the cask
@@ -8,7 +8,7 @@ cask 'keepassxc' do
   else
     # github.com/keepassxreboot/keepassxc/ was verified as official when first introduced to the cask
     url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}.dmg"
-    sha256 '3d56ebbcb3471a2f6116abe884d1e7b662347b80e18cc0a12ca9fdf2c9a14d7a'
+    sha256 '2224047775b0184b78c252e97cc9c7487aada3a26d24701e45114ea32868f403'
   end
 
   appcast 'https://github.com/keepassxreboot/keepassxc/releases.atom'
