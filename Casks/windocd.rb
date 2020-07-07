e477b843e9af2f4361599aa0b82c0151ea53dc47
Ali Serhal <anserhal@gmail.com>
2020-07-06 19:06:55 -0500

    Add windOCD app

---
+++ Casks/windocd.rb
@@ -0,0 +1,10 @@
+cask 'windocd' do
+  version '1.7.4'
+  sha256 '72d5b9fd9b2e20245dbe7c1fb60d41982c725485901e002676ba00945ce91b2e'
+
+  url "https://download.moapp.software/windOCD.zip"
+  name 'windOCD'
+  homepage 'https://windocd.myownapp.com'
+
+  app 'windOCD.app'
+end
