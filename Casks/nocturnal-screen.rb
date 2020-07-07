a2d4c24919469b80d214f696b50b83960746aa80
Joshua Jon <joshua.j.jon@gmail.com>
2020-07-07 12:20:39 +1000

    Add version var to nocturnal url

--- Casks/nocturnal-screen.rb
+++ Casks/nocturnal-screen.rb
@@ -2,7 +2,7 @@ cask 'nocturnal-screen' do
   version '1.1'
   sha256 'b3ab92067e1fa999ef09cadf6a26e23c1a46e6595fd64d9a1b614225fdbd4681'
 
-  url 'https://github.com/joshjon/nocturnal/releases/download/v1.1/Nocturnal-1.1.zip'
+  url 'https://github.com/joshjon/nocturnal/releases/download/v#{version}/Nocturnal-#{version}.zip'
   appcast 'https://github.com/joshjon/nocturnal/releases.atom'
   name 'Nocturnal Screen'
   homepage 'https://github.com/joshjon/nocturnal'
