7bce60cdb5145095af70a534622b381403f84410
Manuel Pietschmann <manuel.pietschmann@gmail.com>
2020-07-08 22:48:51 +0200

    Update cryo from 0.5.6 to 0.5.10 (#85585)
    Co-authored-by: Manuel Pietschmann <mpi@cryonet.io>

--- Casks/cryo.rb
+++ Casks/cryo.rb
@@ -1,6 +1,6 @@
 cask 'cryo' do
-  version '0.5.6'
-  sha256 '12e4c7ae43b6b9456eabd18b75d51f4f39f2dce904b2cdbab3a78de1ac13a42f'
+  version '0.5.10'
+  sha256 '679bec106c9785ff8bf00fcb2388259b02dfde9a6c53d89e3c90fadac524b586'
 
   url "https://cryonet.io/downloads/macos/cryo_#{version}_macos.zip"
   name 'cryo'
