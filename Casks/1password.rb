b4e8d86aca3116940a14b092191097b3c96be134
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-07 17:48:38 -0700

    Update 1password from 7.5 to 7.6 (#85544)

--- Casks/1password.rb
+++ Casks/1password.rb
@@ -1,6 +1,6 @@
 cask '1password' do
-  version '7.5'
-  sha256 '64b98a5e1dfea7028ef483b679a97c6e0bf78b05b5cb3409e66ffc62e72be75c'
+  version '7.6'
+  sha256 'c75ac1e661950d48766e78f716b642d4e1d963e588af9fd09095dcad6cf52d55'
 
   url "https://c.1password.com/dist/1P/mac#{version.major}/1Password-#{version}.zip"
   appcast "https://app-updates.agilebits.com/product_history/OPM#{version.major}"
