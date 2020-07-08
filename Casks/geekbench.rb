4df3e786eccc0cafb90f62c9f413c6fef98ef9e2
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-07 17:29:20 -0700

    Update geekbench from 5.2.0 to 5.2.2 (#85535)

--- Casks/geekbench.rb
+++ Casks/geekbench.rb
@@ -3,8 +3,8 @@ cask 'geekbench' do
     version '4.4.2'
     sha256 '3c46e630a28a0752afd702fc1cd379edd2420001be22302c932e61751284c0cc'
   else
-    version '5.2.0'
-    sha256 '351596a697ff5fe495083ba9e909343b96bd0841f912e40d9c462a4d450679b5'
+    version '5.2.2'
+    sha256 '0b9091b94fde9c521e23a2cf9f2a6cf82bb99646a895c49ada26c6def10534c8'
   end
 
   url "https://cdn.geekbench.com/Geekbench-#{version}-Mac.zip"
