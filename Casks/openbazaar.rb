09e7661f79ae357154d0e74ba60748088ee313b5
Randall <ran.dall@icloud.com>
2020-07-07 17:09:39 -0700

    Update openbazaar from 2.4.5 to 2.4.6

--- Casks/openbazaar.rb
+++ Casks/openbazaar.rb
@@ -1,6 +1,6 @@
 cask 'openbazaar' do
-  version '2.4.5'
-  sha256 '9f9f026821f03a1ba2371e76f92f23e073a42463029a6bc4c90b0c4a9cb3b4b5'
+  version '2.4.6'
+  sha256 '4b359e8632a367d6eae13bb0eda23715ebddd5a01344689830f4773bbaa99172'
 
   # github.com/OpenBazaar/openbazaar-desktop/ was verified as official when first introduced to the cask
   url "https://github.com/OpenBazaar/openbazaar-desktop/releases/download/v#{version}/OpenBazaar#{version.major}-#{version}.dmg"
