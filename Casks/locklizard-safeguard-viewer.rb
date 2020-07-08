9a07f3d060b00c65d8aa022d67446bad6eef1fb4
Chad H. G. Allen <chad@chgallen.com>
2020-07-08 15:42:37 +0100

    Add appcast stanza

--- Casks/locklizard-safeguard-viewer.rb
+++ Casks/locklizard-safeguard-viewer.rb
@@ -3,6 +3,7 @@ cask 'locklizard-safeguard-viewer' do
   sha256 'd77a65e7f19aae939c5bbe2eb0db4ac7e678d6aa9dde6a9fdfd55964673bd08c'
 
   url 'https://downloads.locklizard.com/PDCViewerSetupOSX.zip'
+  appcast 'https://www.locklizard.com/download_pdf_viewers/#mac-viewer'
   name 'LockLizard Safeguard Viewer'
   name 'PDC Viewer'
   homepage 'https://www.locklizard.com/'
