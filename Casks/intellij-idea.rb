4d5ab476fe17a9deb6c6e66d7158a23ea8397820
jcbot <jcb@leipert.io>
2020-07-08 07:37:12 +0000

    Update intellij-idea to 2020.1.3

--- Casks/intellij-idea.rb
+++ Casks/intellij-idea.rb
@@ -1,6 +1,6 @@
 cask 'intellij-idea' do
-  version '2020.1.2'
-  sha256 '5d1fc8b708b67c8b806dcdf6dd46d149c7a20540d5a790a3b359d797135197e8'
+  version '2020.1.3'
+  sha256 'ae96f22aad30b2d2b73ffbfb907bc2f4b70d479cea48faff9c4afec2afb8da73'
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=IIU&latest=true&type=release'
