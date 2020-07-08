803778f412b0deec02dc2fb6c6e3a603f46ea46d
Svend <svend.jin@gmail.com>
2020-07-08 09:16:21 +0800

    Update uPic form v0.19.3 to v0.19.4

--- Casks/upic.rb
+++ Casks/upic.rb
@@ -1,6 +1,6 @@
 cask 'upic' do
-  version '0.19.3'
-  sha256 'be47e850363de2d872bdea867887ec6ee305dcc8c992bff021f0cbfb45c1b4d0'
+  version '0.19.4'
+  sha256 '090ff453b2d07e550f0d65b477facab11573be727c9e74d7b1c21fe048960eb3'
 
   url "https://github.com/gee1k/uPic/releases/download/v#{version}/uPic.zip"
   appcast 'https://github.com/gee1k/uPic/releases.atom'
