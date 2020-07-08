de4c43709f1f23ddf98d435cf6fcad1cef25b9ef
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-07 17:44:46 -0700

    Update houdahspot from 5.1.3 to 5.1.4 (#85530)

--- Casks/houdahspot.rb
+++ Casks/houdahspot.rb
@@ -1,6 +1,6 @@
 cask 'houdahspot' do
-  version '5.1.3'
-  sha256 '67eeb8925aecc19f9f957ce3d23d3b1e202dfe1b1f7d4fed687efd0d1c80c654'
+  version '5.1.4'
+  sha256 'fa5f591adf24650064c531621f6f0f075e2c27164af0fa070f00fc404ccc4398'
 
   url "https://dl.houdah.com/houdahSpot/updates/cast#{version.major}_assets/HoudahSpot#{version}.zip"
   appcast "https://www.houdah.com/houdahSpot/updates/cast#{version.major}.xml"
