03eac401d46d1604be2fdc9d9e050af7629f4ca8
Randall <ran.dall@icloud.com>
2020-07-07 17:19:53 -0700

    Update mailbutler from 2,2916-13651 to 3,3007-13895

--- Casks/mailbutler.rb
+++ Casks/mailbutler.rb
@@ -1,6 +1,6 @@
 cask 'mailbutler' do
-  version '2,2916-13651'
-  sha256 'edde104063edcd2394b205f9c000a0c5df50e7e1484f2f853c1a155d47065563'
+  version '3,3007-13895'
+  sha256 'a8876a4732167e285888fc6a29495f19ae645ec48e04ca10e3cfd8f5c840cbff'
 
   url "https://downloads.mailbutler.io/sparkle/public/Mailbutler_#{version.after_comma}.zip"
   appcast "https://www.mailbutler.io/appcast#{version.major}.php"
