59daec6b404e8c48fffaf5e75f11d1fc82243a36
Randall <ran.dall@icloud.com>
2020-07-07 17:29:52 -0700

    Update beatunes from 5.2.9 to 5.2.10

--- Casks/beatunes.rb
+++ Casks/beatunes.rb
@@ -1,6 +1,6 @@
 cask 'beatunes' do
-  version '5.2.9'
-  sha256 'e370014e564fb864396d72dffaff4f30ad54fc563585f4e0836987923f4db00d'
+  version '5.2.10'
+  sha256 '6dd137c8ea07d53667c41316f25acd819f256f7398e54c02a627c78631947931'
 
   url "http://coxy.beatunes.com/download/beaTunes-#{version.dots_to_hyphens}.dmg"
   appcast 'https://www.beatunes.com/en/beatunes-download.html',
