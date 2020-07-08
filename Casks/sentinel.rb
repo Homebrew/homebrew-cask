6b2c5677af848bb64cf99e912e9d6ef69cc4534d
Brian Menges <mengesb@gmail.com>
2020-07-07 21:34:15 -0700

    Update sentinel from 0.15.5 to 0.15.6 (#85559)

--- Casks/sentinel.rb
+++ Casks/sentinel.rb
@@ -1,6 +1,6 @@
 cask 'sentinel' do
-  version '0.15.5'
-  sha256 '700e979d8f047bff28445b4a6037dbffde7bf01774ef48b6b2e959f9ffbdfab2'
+  version '0.15.6'
+  sha256 '8b249304f5897dbf4314257a5f4cd28e1fb382524e68f6681cb1c254a72e9be2'
 
   url "https://releases.hashicorp.com/sentinel/#{version}/sentinel_#{version}_darwin_amd64.zip"
   appcast 'https://docs.hashicorp.com/sentinel/downloads/'
