99b57efa945c54093b5e157f5c2790400f676373
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-08 00:28:35 +0000

    Update mailbutler.rb

--- Casks/mailbutler.rb
+++ Casks/mailbutler.rb
@@ -1,5 +1,5 @@
 cask 'mailbutler' do
-  version '3,3007-13895'
+  version '2,3007-13895'
   sha256 'a8876a4732167e285888fc6a29495f19ae645ec48e04ca10e3cfd8f5c840cbff'
 
   url "https://downloads.mailbutler.io/sparkle/public/Mailbutler_#{version.after_comma}.zip"
