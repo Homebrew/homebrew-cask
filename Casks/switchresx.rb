cfbb7dc1673cb70871d5476ef1ed0bf993f1b984
Yurii Kolesnykov <root@yurikoles.com>
2020-07-09 20:16:19 +0300

    Update switchresx from 4.10.0 to 4.10.1 (#85567)

--- Casks/switchresx.rb
+++ Casks/switchresx.rb
@@ -1,6 +1,6 @@
 cask 'switchresx' do
-  version '4.10.0'
-  sha256 '99225a0980b49efaccdbd8ea1f2a8db7e811647c3a1a40440883b9d072cbdb92'
+  version '4.10.1'
+  sha256 '33b2bec0623f1edd83e32efa2ac1980f9d677211abdfd8a3d62289eb29abf20a'
 
   url "https://www.madrau.com/data/switchresx/SwitchResX#{version.major}.zip"
   appcast "https://www.madrau.com/SRXCurrentVersion#{version.major}"
