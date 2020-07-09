05310d9ba4b4728216f1486b71d562cb7c3d042e
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-08 19:50:12 -0700

    Update smartsynchronize from 4.0.3 to 4.1.0 (#85617)

--- Casks/smartsynchronize.rb
+++ Casks/smartsynchronize.rb
@@ -1,6 +1,6 @@
 cask 'smartsynchronize' do
-  version '4.0.3'
-  sha256 '6245fb232382c22ddf56d8b7b6d6f604b56d467b5fbd7d73f101bfef290e516b'
+  version '4.1.0'
+  sha256 '207f285df704db88f7a7338dfd714ffc283c498010b03205cd6ac21eab9004bf'
 
   url "https://www.syntevo.com/downloads/smartsynchronize/smartsynchronize-macosx-#{version.dots_to_underscores}.dmg"
   appcast 'https://www.syntevo.com/smartsynchronize/changelog.txt',
