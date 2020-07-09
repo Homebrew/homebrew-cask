c819536b08bee23ee7e30f03b2dec6284bf0a2a4
Randall <ran.dall@icloud.com>
2020-07-08 18:49:23 -0700

    Update mumble from 1.3.1 to 1.3.2

--- Casks/mumble.rb
+++ Casks/mumble.rb
@@ -1,6 +1,6 @@
 cask 'mumble' do
-  version '1.3.1'
-  sha256 '052ba6d5bc1fc64aed90881bcab036bf93ae857228be316243a6eb15dac99ce6'
+  version '1.3.2'
+  sha256 '416836d82edeb301389749e717ffba72f22f9546159ccb03fd20b11062e9baf0'
 
   # github.com/mumble-voip/mumble/ was verified as official when first introduced to the cask
   url "https://github.com/mumble-voip/mumble/releases/download/#{version}/Mumble-#{version}.dmg"
