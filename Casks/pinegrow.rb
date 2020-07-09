56bdddc886794bf109f72edf5567105eb32ca056
Brigitte Zmoelnig <brigitte@corecode.io>
2020-07-09 21:13:54 +0200

    Update pinegrow from 5.96.0 to 5.97

--- Casks/pinegrow.rb
+++ Casks/pinegrow.rb
@@ -1,6 +1,6 @@
 cask 'pinegrow' do
-  version '5.96.0'
-  sha256 '6a7de6dc5ea92ae56b333effc384d53c4912e0d05e94a0f4fecc76d5167a9e74'
+  version '5.97'
+  sha256 '26b038025ceb337259dd528d1d156f6a4d424370acc464e80350095beee03f71'
 
   url "http://download.pinegrow.com/PinegrowMac.#{version}.dmg"
   appcast 'https://pinegrow.com/'
