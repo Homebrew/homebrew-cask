d64f653b7ef0c37ee5b66c351f8bbe5767477233
Igor Kulman <igor@kulman.sk>
2020-07-09 12:19:17 +0200

    Update teamwire (#85624)

--- Casks/teamwire.rb
+++ Casks/teamwire.rb
@@ -1,6 +1,6 @@
 cask 'teamwire' do
   version '2.2.1'
-  sha256 'ccb12fe37e513b9be4345726f5a29b8881d2c64affbac7a54b1f084fa7621441'
+  sha256 '8471cee1b211c71e5764ec361a4eed4e9742a1de591091ab2022c317d11a8767'
 
   url "https://desktop.teamwire.eu/dist/#{version}/Teamwire-macos64_#{version}.dmg"
   name 'Teamwire'
