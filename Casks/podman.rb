b5557d1937d315af0c0e4bc3b6e4ce99f8622929
Ashley Cui <acui@redhat.com>
2020-07-09 14:19:42 -0400

    install conf file for podman 2.0.2

--- Casks/podman.rb
+++ Casks/podman.rb
@@ -1,6 +1,6 @@
 cask 'podman' do
   version '2.0.2'
-  sha256 '90e6fc832880323b9069617f9e5b384708be007dbafe50b3270b46923f454209'
+  sha256 '5ae54572d791b1a0f22090c5873ba288c777564d0ea4c23c0a32e3708819c0dc'
 
   url "https://github.com/containers/libpod/releases/download/v#{version}/podman-remote-release-darwin.zip"
   appcast 'https://github.com/containers/libpod/releases.atom'
@@ -12,10 +12,15 @@ cask 'podman' do
   postflight do
     man1 = Dir["#{staged_path}/docs/*.1"]
     FileUtils.mv(man1, "#{HOMEBREW_PREFIX}/share/man/man1/")
+
+    FileUtils.mkdir_p "#{ENV['HOME']}/.config/containers"
+    FileUtils.mv("#{staged_path}/containers.conf", "#{ENV['HOME']}/.config/containers/containers.conf") unless File.exist?("#{ENV['HOME']}/.config/containers/containers.conf")
   end
 
   uninstall_postflight do
     man1 = Dir["#{HOMEBREW_PREFIX}/share/man/man1/podman*.1"]
     FileUtils.rm(man1)
   end
+
+  zap trash: "#{ENV['HOME']}/.config/containers"
 end
