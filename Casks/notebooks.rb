384adf946569f2b3cb69d2147abae045a9c0badd
Randall <ran.dall@icloud.com>
2020-07-09 10:12:36 -0700

    Update notebooks from 2.1.1 to 2.2

--- Casks/notebooks.rb
+++ Casks/notebooks.rb
@@ -1,6 +1,6 @@
 cask 'notebooks' do
-  version '2.1.1'
-  sha256 '804a4d06b15acc4579f9f6a0846971ffd851a7571b1af2b5fa5dd501e8a10d10'
+  version '2.2'
+  sha256 '2704b5bd6e15f53da472842edda34a2b3bc20f4452c74eea197d84277bd0963e'
 
   url "https://www.notebooksapp.com/Download/macOS/v#{version.major}/Notebooks.dmg"
   appcast "https://notebooksapp.com/Download/macOS/v#{version.major}/Notebooks#{version.major}Appcast.xml"
