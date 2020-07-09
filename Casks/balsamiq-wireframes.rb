4bcd7f894d3ea982cea244732969134d02eb8236
Randall <ran.dall@icloud.com>
2020-07-08 19:30:27 -0700

    Update balsamiq-wireframes from 4.0.30 to 4.0.31

--- Casks/balsamiq-wireframes.rb
+++ Casks/balsamiq-wireframes.rb
@@ -1,6 +1,6 @@
 cask 'balsamiq-wireframes' do
-  version '4.0.30'
-  sha256 'f5818ffb00deeb64d9c4346332d494211d08434e4916bd59b2cec3e99cc56c84'
+  version '4.0.31'
+  sha256 'ff95ba6b84be6cfc240f924322d2f69a828fd33f8a640e7fe8c7b2b8815a1f55'
 
   url "https://builds.balsamiq.com/bwd/Balsamiq%20Wireframes%20#{version}.dmg"
   appcast 'https://builds.balsamiq.com/bwd/mac.jsonp'
