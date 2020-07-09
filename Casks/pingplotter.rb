1acf3e47cb477ac208a6fc73c6111ea1d47f4f57
Randall <ran.dall@icloud.com>
2020-07-08 19:29:33 -0700

    Update pingplotter from 5.18.0 to 5.18.1

--- Casks/pingplotter.rb
+++ Casks/pingplotter.rb
@@ -1,6 +1,6 @@
 cask 'pingplotter' do
-  version '5.18.0'
-  sha256 '62e5b59a3abcc838fdc88c9460e607ba70b0706c8303ce4b401a62508e50b2a9'
+  version '5.18.1'
+  sha256 'f70b5ad6c32cac19a2f31304c435009b56c2bb101af71dfc55e79ade36e9c784'
 
   url 'https://www.pingplotter.com/downloads/pingplotter_osx.zip'
   appcast 'https://www.pingplotter.com/download'
