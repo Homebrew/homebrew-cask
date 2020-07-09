0d2d90a7e1dba3b3c41c28cc06db3f0836fc6091
Randall <ran.dall@icloud.com>
2020-07-08 18:48:47 -0700

    Update obsidian from 0.7.4 to 0.7.6

--- Casks/obsidian.rb
+++ Casks/obsidian.rb
@@ -1,6 +1,6 @@
 cask 'obsidian' do
-  version '0.7.4'
-  sha256 '9d98791e107aa29764c80018882565668a7be31ec1690a42ddd97bb50a30c92d'
+  version '0.7.6'
+  sha256 '12a7c4b92203e4dd13a3527885a05354e8a33cb5c127ce7f25b7a375ad9645ba'
 
   # github.com/obsidianmd/ was verified as official when first introduced to the cask
   url "https://github.com/obsidianmd/obsidian-releases/releases/download/v#{version}/Obsidian-#{version}.dmg"
