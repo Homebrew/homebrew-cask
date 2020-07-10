81373f30665aefa2f6f528951c9a6d12aea05223
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2020-07-10 14:01:27 +0200

    Update resolume-arena from 7.2.0,70340 to 7.2.0,70452 (#85675)
    * Update resolume-arena from 7.2.0,70340 to 7.2.0,70452
    
    * Fix resolume-arena uninstall

--- Casks/resolume-arena.rb
+++ Casks/resolume-arena.rb
@@ -1,6 +1,6 @@
 cask 'resolume-arena' do
-  version '7.2.0,70340'
-  sha256 'bb9f281c02b8b629401aee2c99f46fee7d28a72a3a628a5483ddd50cc2279497'
+  version '7.2.0,70452'
+  sha256 'b5c6bca1e40302f706f7a8787af534fbe62d97dbeb36043f05fc84b0a7dfaa9e'
 
   url "https://resolume.com/download/Resolume_Arena_#{version.major_minor_patch.dots_to_underscores}_rev_#{version.after_comma}_Installer.dmg"
   appcast 'https://resolume.com/download/'
@@ -13,6 +13,8 @@ cask 'resolume-arena' do
                          'com.resolume.pkg.ResolumeArena.*',
                          'com.resolume.pkg.ResolumeDXV',
                          'com.resolume.pkg.ResolumeQuickLook',
+                         'com.resolume.pkg.ResolumeAlley',
+                         'com.resolume.pkg.ResolumeCommon',
                        ],
             delete:    "/Applications/Resolume Arena #{version.major}",
             signal:    ['TERM', 'com.resolume.arena'],
