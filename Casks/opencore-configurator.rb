b04b48b6232bf43270cfea323e04b1fed2139964
Goldgamer <11319871+iMonZ@users.noreply.github.com>
2020-07-10 08:52:19 +0200

    Update opencore-configurator.rb

--- Casks/opencore-configurator.rb
+++ Casks/opencore-configurator.rb
@@ -1,6 +1,6 @@
 cask 'opencore-configurator' do
-  version '2.7.0.0'
-  sha256 '908b74cf56fe5f078d6d7d28998ab615a42da8b90a45ee9b0f0a1c869c96aa6f'
+  version '2.7.0.1'
+  sha256 '6486794aaa707722c8176b72e6f5790508aa6e8f15efaa6ad0ef7b657f5c1184'
 
   url 'https://mackie100projects.altervista.org/apps/opencoreconf/download-new-build.php?version=last'
   appcast 'https://mackie100projects.altervista.org/category/opencore-configurator-changelog/'
