1893324e1842d9be60786f6918e540bf90717eec
EpLiar <e@epliar.com>
2020-07-10 10:21:01 +0800

    Update Qv2ray to 2.6.0

--- Casks/qv2ray.rb
+++ Casks/qv2ray.rb
@@ -1,6 +1,6 @@
 cask 'qv2ray' do
-  version '2.5.0'
-  sha256 '8c2f7604ac28396420ff042e69ebe2b2f13c6a670df09a4955ece7a855a243e3'
+  version '2.6.0'
+  sha256 '0f9536e87cee6b4451bb67be7023a7d1886420ab9ef5c219cf0fb752006a2eb2'
 
   # github.com/Qv2ray/Qv2ray/ was verified as official when first introduced to the cask
   url "https://github.com/Qv2ray/Qv2ray/releases/download/v#{version}/Qv2ray-v#{version}.macOS-x64.dmg"
