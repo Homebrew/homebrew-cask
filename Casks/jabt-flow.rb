a3320e84ca22a8b38932d92f3924cb31a9abfe11
Vítor Galvão <info@vitorgalvao.com>
2020-07-10 13:21:46 +0100

    Update jabt-flow.rb

--- Casks/jabt-flow.rb
+++ Casks/jabt-flow.rb
@@ -3,6 +3,7 @@ cask 'jabt-flow' do
   sha256 '1ff9593b6ecd5c9cebaf9041e7302b6891c4911a52006741c663eb61903352e9'
 
   # createwithflow-updates.s3-us-west-2.amazonaws.com/ was verified as official when first introduced to the cask
+  # Download page is at https://createwithflow.com/releases/
   url "https://createwithflow-updates.s3-us-west-2.amazonaws.com/Flow#{version.no_dots}_.zip"
   appcast 'https://createwithflow-updates.s3-us-west-2.amazonaws.com/appcast.xml'
   name 'Flow'
