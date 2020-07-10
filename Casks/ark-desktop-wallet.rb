a094f361af114c0b81ee0057ffc0256fd21790d1
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-10 13:33:22 -0700

    Update ark-desktop-wallet from 2.9.2 to 2.9.3 (#85693)

--- Casks/ark-desktop-wallet.rb
+++ Casks/ark-desktop-wallet.rb
@@ -1,6 +1,6 @@
 cask 'ark-desktop-wallet' do
-  version '2.9.2'
-  sha256 '039cc7126db917f66e333489e79a5425f9863cca237c57af6614d1695ce6d349'
+  version '2.9.3'
+  sha256 '71bbe46c7dd58d6cccc1797d27a39376dffcb73825192b14b94b6107f4e0d7b8'
 
   # github.com/ArkEcosystem/desktop-wallet/ was verified as official when first introduced to the cask
   url "https://github.com/ArkEcosystem/desktop-wallet/releases/download/#{version}/ark-desktop-wallet-mac-#{version}.dmg"
