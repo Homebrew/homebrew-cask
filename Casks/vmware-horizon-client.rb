187026fc0f5dea43a4264b6c4bfa33bcf9f9f1cf
Brigitte Zmoelnig <brigitte@corecode.io>
2020-07-11 18:55:06 +0200

    Update vmware-horizon-client from 5.4.2-15910566,CART21FQ1 to 5.4.3-16499473,CART21FQ1

--- Casks/vmware-horizon-client.rb
+++ Casks/vmware-horizon-client.rb
@@ -1,6 +1,6 @@
 cask 'vmware-horizon-client' do
-  version '5.4.2-15910566,CART21FQ1'
-  sha256 '2d5668402ef372656b487e8e868a8a5a139ec4b8ff88b10d15772231f0988c26'
+  version '5.4.3-16499473,CART21FQ1'
+  sha256 '3fe45ce4f1a84049ed9d7e445fdc0397da7c8b420ae870e686fd66876b2003d6'
 
   url "https://download3.vmware.com/software/view/viewclients/#{version.after_comma}/VMware-Horizon-Client-#{version.before_comma}.dmg"
   appcast 'https://docs.vmware.com/en/VMware-Horizon-Client-for-Mac/',
