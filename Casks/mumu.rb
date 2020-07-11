f60f218895aebcb8f8ea662d1677bcf5654d1040
Randall <ran.dall@icloud.com>
2020-07-11 12:54:14 -0700

    Fix `verison`, `sha256`, and `url` on `mumu`

--- Casks/mumu.rb
+++ Casks/mumu.rb
@@ -1,9 +1,9 @@
 cask 'mumu' do
-  version :latest
-  sha256 :no_check
+  version '1.0.1'
+  sha256 '6bc059e7d455056b68ba8b39635eec38758af7397fae65d0b9eeb24332a8addd'
 
-  # vendors.paddle.com was verified as official when first introduced to the cask
-  url 'https://vendors.paddle.com/download/product/597910'
+  # paddle.s3.amazonaws.com/fulfillment_downloads/116824/ was verified as official when first introduced to the cask
+  url "https://paddle.s3.amazonaws.com/fulfillment_downloads/116824/597910/3UjvmpBGSxuQuBAVlQm6_Mumu%20#{version}.dmg"
   name 'Mumu'
   homepage 'https://getmumu.com/'
 
