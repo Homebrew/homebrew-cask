6a008f9ca5d9ab47e8b0fe7311db96befffaa53c
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-10 17:13:54 -0700

    Update chirp from 20200622 to 20200710 (#85729)

--- Casks/chirp.rb
+++ Casks/chirp.rb
@@ -1,6 +1,6 @@
 cask 'chirp' do
-  version '20200622'
-  sha256 '55e5dc8319e5cb57dda88dda5488757878f338c80c8bedf4a2e78b0ef5701132'
+  version '20200710'
+  sha256 'a68514032a64e90116acd5f5b6dc0b6c57b2c4847b70016ca21a1154285421f7'
 
   url "https://trac.chirp.danplanet.com/chirp_daily/LATEST/chirp-unified-daily-#{version}.app.zip"
   appcast 'https://trac.chirp.danplanet.com/chirp_daily/LATEST/SHA1SUM'
