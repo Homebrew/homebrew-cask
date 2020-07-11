50e940e5019fee4cd6db07380217e036174a58e7
Randall <ran.dall@icloud.com>
2020-07-11 12:55:48 -0700

    Fix `appcast` on `mumu`

--- Casks/mumu.rb
+++ Casks/mumu.rb
@@ -2,7 +2,7 @@ cask 'mumu' do
   version '1.0.1'
   sha256 '6bc059e7d455056b68ba8b39635eec38758af7397fae65d0b9eeb24332a8addd'
 
-  # paddle.s3.amazonaws.com/fulfillment_downloads/116824/ was verified as official when first introduced to the cask
+  # paddle.s3.amazonaws.com/fulfillment_downloads/116824/597910/ was verified as official when first introduced to the cask
   url "https://paddle.s3.amazonaws.com/fulfillment_downloads/116824/597910/3UjvmpBGSxuQuBAVlQm6_Mumu%20#{version}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://vendors.paddle.com/download/product/597910'
   name 'Mumu'
