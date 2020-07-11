7b0879938c814d49b0ef3188730b2e35642ad104
Brigitte Zmoelnig <brigitte@corecode.io>
2020-07-11 19:56:59 +0200

    Update studio-3t from 2020.6.0 to 2020.6.1

--- Casks/studio-3t.rb
+++ Casks/studio-3t.rb
@@ -1,6 +1,6 @@
 cask 'studio-3t' do
-  version '2020.6.0'
-  sha256 '00bd75db9e0c34d4852735fa5f89f48b62609da2759ef0c5b2fe306a9e8dcda0'
+  version '2020.6.1'
+  sha256 'ed351b3726e2d78f45721d7d68fa72589badaf92732f1062de3c51b1505beacb'
 
   url "https://download.studio3t.com/studio-3t/mac/#{version}/Studio-3T.dmg"
   appcast 'https://files.studio3t.com/changelog/changelog.txt'
