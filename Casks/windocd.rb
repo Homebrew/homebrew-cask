def80314433b35720a6c1d42a978fb39e801cb02
Ali <ans87gh@users.noreply.github.com>
2020-07-11 15:11:51 -0500

    Update windocd.rb

--- Casks/windocd.rb
+++ Casks/windocd.rb
@@ -2,10 +2,11 @@ cask 'windocd' do
   version '1.7.4'
   sha256 '72d5b9fd9b2e20245dbe7c1fb60d41982c725485901e002676ba00945ce91b2e'
 
+  # download.moapp.software/ was verified as official when first introduced to the cask
   url 'https://download.moapp.software/windOCD.zip'
   appcast 'https://sparkle.moapp.software/windocd.xml'
   name 'windOCD'
-  homepage 'https://windocd.myownapp.com'
+  homepage 'https://windocd.myownapp.com/'
 
   app 'windOCD.app'
 end
