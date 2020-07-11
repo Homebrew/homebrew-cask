368ec4da57125e43dd6234a4c921909542250b57
Julian Mayer <julian@corecode.io>
2020-07-11 20:47:50 +0200

    Update itunes-producer

--- Casks/itunes-producer.rb
+++ Casks/itunes-producer.rb
@@ -1,6 +1,6 @@
 cask 'itunes-producer' do
   version '3.1.2'
-  sha256 '553a99b08ae34635ed777278fea7c3037e178ad3f7700d57914eec0fb8892435'
+  sha256 '21079edbf0d559db4403a76fb7f18f02c93e5fbb68006bb5aaf55af329301ded'
 
   url "https://itunespartner.apple.com/assets/downloads/iTunesProducer_#{version}.dmg"
   appcast 'https://itunespartner.apple.com/en/music/tools'
