2e077b42000fb12db779e4f503f8fd958da59720
Randall <ran.dall@icloud.com>
2020-07-11 15:12:52 -0700

    Update publish-or-perish from 7.22.2840 to 7.23.2852

--- Casks/publish-or-perish.rb
+++ Casks/publish-or-perish.rb
@@ -1,6 +1,6 @@
 cask 'publish-or-perish' do
-  version '7.22.2840'
-  sha256 '99c3472b70c64233a61457c9a56aa94bab6581149b985ad5074fe0ff360b51d6'
+  version '7.23.2852'
+  sha256 '94dbf1ce0b56eded8344ffc2ceb62abdfb64da8cf8dbc6714bcc6c433a5d97c5'
 
   url 'https://harzing.com/download/PoP7Mac.pkg'
   appcast 'https://harzing.com/resources/publish-or-perish/os-x'
