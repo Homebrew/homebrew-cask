9bdde0783f2052b7df0e1a266de5017abe2c97c5
Randall <ran.dall@icloud.com>
2020-07-11 14:14:20 -0700

    Fix `version` and `sha256` on `icollections`

--- Casks/icollections.rb
+++ Casks/icollections.rb
@@ -1,6 +1,6 @@
 cask 'icollections' do
-  version :latest
-  sha256 :no_check
+  version '6.5.1'
+  sha256 '8683c01f66f7480679f10b14e2606012b815665b8713d4d34b5047918e4dd0be'
 
   url 'https://naarakstudio.com/download/iCollections.dmg'
   name 'iCollections'
