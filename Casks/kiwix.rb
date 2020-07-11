f36ddcc9fed1a2a03b76b77f36229c179e909b2c
CoreCode <core-code@users.noreply.github.com>
2020-07-11 19:51:24 +0200

    Update kiwix from 2.1 to 2.1.1 (#85757)

--- Casks/kiwix.rb
+++ Casks/kiwix.rb
@@ -1,6 +1,6 @@
 cask 'kiwix' do
-  version '2.1'
-  sha256 '61056a5f48dce8ec63f1082e0b41c4fcbd0e8cf3f36dc06a1f6a3ef29c215669'
+  version '2.1.1'
+  sha256 '48062d3d3156eeecec78bce226a2612b97ef84434d2e7b3d3ad402063e17e3e1'
 
   url "https://download.kiwix.org/release/kiwix-desktop-macos/kiwix-desktop-macos_#{version}.dmg"
   appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.kiwix.org/release/kiwix-desktop-macos/kiwix-desktop-macos.dmg'
