f9d37cbc70e788b8731db1abde50afba010ddc1f
Randall <ran.dall@icloud.com>
2020-07-11 14:14:32 -0700

    Add `appcast` stanza to `icollections`

--- Casks/icollections.rb
+++ Casks/icollections.rb
@@ -3,6 +3,7 @@ cask 'icollections' do
   sha256 '8683c01f66f7480679f10b14e2606012b815665b8713d4d34b5047918e4dd0be'
 
   url 'https://naarakstudio.com/download/iCollections.dmg'
+  appcast 'https://naarakstudio.com/icollections/whats-new.pdf'
   name 'iCollections'
   homepage 'https://naarakstudio.com/icollections/'
 
