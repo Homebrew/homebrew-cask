a12f516fa4078be61f6f0c8fecc056aa9eed734f
Ali Serhal <anserhal@gmail.com>
2020-07-11 10:19:27 -0500

    Modified windOCD cask; added appcast

--- Casks/windocd.rb
+++ Casks/windocd.rb
@@ -2,7 +2,8 @@ cask 'windocd' do
   version '1.7.4'
   sha256 '72d5b9fd9b2e20245dbe7c1fb60d41982c725485901e002676ba00945ce91b2e'
 
-  url "https://download.moapp.software/windOCD.zip"
+  url 'https://download.moapp.software/windOCD.zip'
+  appcast 'https://sparkle.moapp.software/windocd.xml'
   name 'windOCD'
   homepage 'https://windocd.myownapp.com'
 
