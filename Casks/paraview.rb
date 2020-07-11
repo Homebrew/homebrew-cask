faf549798bff79e83fac35d4ce73a6e1290151d2
CoreCode <core-code@users.noreply.github.com>
2020-07-11 21:24:35 +0200

    Update paraview (#85780)

--- Casks/paraview.rb
+++ Casks/paraview.rb
@@ -1,6 +1,6 @@
 cask 'paraview' do
   version '5.8.0'
-  sha256 '36e84be8d389806f17fbc5a30b26bc242566bc919ba94dffb07d05cb6fbdbdb5'
+  sha256 '5cb92137ee86241004f5055dd6e5e9c67ebb6581771553ed1275e7050d35027c'
 
   url "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v#{version.major_minor}&type=binary&os=macOS&downloadFile=ParaView-#{version}-MPI-OSX10.12-Python2.7-64bit.dmg",
       user_agent: :fake
