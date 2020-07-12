8d018223a23298b33df8316ea4a282d90da137cc
Randall <ran.dall@icloud.com>
2020-07-12 14:05:10 -0700

    Update menubar-stats from 3.3 to 3.4

--- Casks/menubar-stats.rb
+++ Casks/menubar-stats.rb
@@ -1,6 +1,6 @@
 cask 'menubar-stats' do
-  version '3.3'
-  sha256 '1735a81fe773e2476b83fb8440f07739e46f4a57f844871482231e37886ef9e4'
+  version '3.4'
+  sha256 '0aef6819ee0cf4304e78f634f1c319b310206e068f12e8929e3498a57e9a33ff'
 
   url 'https://seense.com/menubarstats/updateapp/mbs.zip'
   appcast 'https://www.seense.com/menubarstats/updateapp/appcast.xml'
