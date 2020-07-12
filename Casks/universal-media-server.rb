f0ae32886567184839438c1603ba641c050a0f54
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-12 14:14:11 -0700

    Update universal-media-server from 9.7.1 to 9.7.2 (#85829)

--- Casks/universal-media-server.rb
+++ Casks/universal-media-server.rb
@@ -1,6 +1,6 @@
 cask 'universal-media-server' do
-  version '9.7.1'
-  sha256 '9a815ea6de0007fc1e6ef81e6b8f9f65dfa25ab15873d9ab135b29a4f6970684'
+  version '9.7.2'
+  sha256 '6c560bced84b30da599ac93773bcd8fc62712abe1169c413cccf55fa6179a08b'
 
   # github.com/UniversalMediaServer/UniversalMediaServer/ was verified as official when first introduced to the cask
   url "https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/#{version}/UMS-#{version}.dmg"
