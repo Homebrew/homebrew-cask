a54133f8cdb1a3dac69ea70aa2dca3a2d9eb93c3
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-12 14:36:34 -0700

    Update fldigi from 4.1.13,4.3.7 to 4.1.14,4.3.7 (#85840)

--- Casks/fldigi.rb
+++ Casks/fldigi.rb
@@ -1,6 +1,6 @@
 cask 'fldigi' do
-  version '4.1.13,4.3.7'
-  sha256 'a56d55812e18cb5cf4938a00bf13022759f317ed104a9eb7e1195553e3eca741'
+  version '4.1.14,4.3.7'
+  sha256 'e90c69c51df8b611a8d82851c6da2af1fce733b25809ac313460fc3a90b3e764'
 
   url "https://downloads.sourceforge.net/fldigi/fldigi/fldigi-#{version.before_comma}_x86_64.dmg"
   appcast 'https://sourceforge.net/projects/fldigi/rss?path=/fldigi'
