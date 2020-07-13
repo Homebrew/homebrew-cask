a7fac1ba8fdabe8ad0d274867813046fc4d0df1b
gottabeleGIT <sandesh.manik@icloud.com>
2020-07-13 18:50:57 -0500

    Create edenmath.rb

---
+++ Casks/edenmath.rb
@@ -0,0 +1,10 @@
+cask 'edenmath' do
+  version :latest
+  sha256 :no_check
+
+  url "http://www.edenwaith.com/downloads/edenmath.dmg"
+  name 'EdenMath'
+  homepage 'http://www.edenwaith.com/products/edenmath/'
+
+  app 'EdenMath.app'
+end
