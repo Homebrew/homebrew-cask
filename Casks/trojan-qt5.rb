7841e973b4e98f15d6921b91167a41281476e1da
SeekingMeaning <meaningseeking@protonmail.com>
2020-07-13 03:45:04 -0700

    Update trojan-qt5 from 1.3.1 to 1.4.0 (#85856)

--- Casks/trojan-qt5.rb
+++ Casks/trojan-qt5.rb
@@ -1,12 +1,12 @@
 cask 'trojan-qt5' do
   # note: "5" is not a version number, but an intrinsic part of the product name
-  version '1.3.1'
-  sha256 '8f5075837cfc0f58d4b41f272403e6efe333292a3069e0998a4eefea3177bfb1'
+  version '1.4.0'
+  sha256 '05886c42098dd3b9cc2e25daa0b18e43e4b65abc9e86b4e1973b12defdf73b82'
 
-  url "https://github.com/TheWanderingCoel/Trojan-Qt5/releases/download/v#{version}/Trojan-Qt5-macOS.dmg"
-  appcast 'https://github.com/TheWanderingCoel/Trojan-Qt5/releases.atom'
+  url "https://github.com/Trojan-Qt5/Trojan-Qt5/releases/download/v#{version}/Trojan-Qt5-macOS.dmg"
+  appcast 'https://github.com/Trojan-Qt5/Trojan-Qt5/releases.atom'
   name 'Trojan-Qt5'
-  homepage 'https://github.com/TheWanderingCoel/Trojan-Qt5'
+  homepage 'https://github.com/Trojan-Qt5/Trojan-Qt5'
 
   auto_updates true
   depends_on macos: '>= :sierra'
