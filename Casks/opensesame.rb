d1e6f991e52b5c186209876cb29f70b696df7675
SeekingMeaning <meaningseeking@protonmail.com>
2020-07-12 17:50:36 -0700

    Update opensesame from 3.3.2 to 3.3.3 (#85848)

--- Casks/opensesame.rb
+++ Casks/opensesame.rb
@@ -1,6 +1,6 @@
 cask 'opensesame' do
-  version '3.3.2'
-  sha256 '9857b59c2b0421290a3e3e15d7e487dd573d33aa496a77fb9a78d2caee2998ab'
+  version '3.3.3'
+  sha256 'f0f8ae1dfbbd5608e96a9ca891d834eae8a64abaed990d100354d53e0f28a210'
 
   # github.com/smathot/OpenSesame/ was verified as official when first introduced to the cask
   url "https://github.com/smathot/OpenSesame/releases/download/release%2F#{version}/opensesame_#{version}-py37-macos-1.dmg"
