089b8013521d34252b80dd5ad72309a3c93ee753
AkiraFukushima <h3.poteto@gmail.com>
2020-07-14 00:49:23 +0900

    Update whalebird to 4.2.0 (#85860)

--- Casks/whalebird.rb
+++ Casks/whalebird.rb
@@ -1,6 +1,6 @@
 cask 'whalebird' do
-  version '4.1.3'
-  sha256 '74e9c8b0adfe85426497fdbb03b38d86eab8921926a99d01698a9744d0f5462c'
+  version '4.2.0'
+  sha256 '152bc4d3cb9a3e0456ab833b9b634ca02d936d0e4a51ffc319e43d27a2d8e9e4'
 
   # github.com/h3poteto/whalebird-desktop/ was verified as official when first introduced to the cask
   url "https://github.com/h3poteto/whalebird-desktop/releases/download/#{version}/Whalebird-#{version}-darwin-x64.dmg"
