407c241c8a566c5951c5981a10a052ca80ee2a16
gottabeleGIT <sandesh.manik@icloud.com>
2020-07-13 18:55:34 -0500

    Update edenmath.rb

--- Casks/edenmath.rb
+++ Casks/edenmath.rb
@@ -2,7 +2,7 @@ cask 'edenmath' do
   version :latest
   sha256 :no_check
 
-  url "http://www.edenwaith.com/downloads/edenmath.dmg"
+  url 'http://www.edenwaith.com/downloads/edenmath.dmg'
   name 'EdenMath'
   homepage 'http://www.edenwaith.com/products/edenmath/'
 
