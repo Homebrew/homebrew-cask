c07372a412a15a750e29cbc71e7297b9b64586ce
Seeker <meaningseeking@protonmail.com>
2020-07-13 19:00:30 -0700

    Update kui from 8.10.12 to 8.10.13

--- Casks/kui.rb
+++ Casks/kui.rb
@@ -1,6 +1,6 @@
 cask 'kui' do
-  version '8.10.12'
-  sha256 'd0d683d400f7adc4b4f3691db54bf1c532f5b1f62b7279049ac52e29a0f0a7f9'
+  version '8.10.13'
+  sha256 '097a8f8f99798c4226fe90f58c5e6d9b8288bb9a3c7ada49bf5767fb6334ffd9'
 
   # github.com/IBM/kui was verified as official when first introduced to the cask
   url "https://github.com/IBM/kui/releases/download/v#{version}/Kui-darwin-x64.tar.bz2"
