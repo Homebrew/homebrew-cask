ad7650f755e5589ec8e212fbd6c4b88878310f59
Muneeb Ahmed <32603485+muneebmahmed@users.noreply.github.com>
2020-07-14 08:49:28 -0700

    Update nordvpn identifiers (#85898)

--- Casks/nordvpn.rb
+++ Casks/nordvpn.rb
@@ -12,21 +12,27 @@ cask 'nordvpn' do
 
   pkg 'NordVPN.pkg'
 
-  uninstall quit:       'com.nordvpn.NordVPN',
+  uninstall quit:       [
+                          'com.nordvpn.osx',
+                          'com.nordvpn.osx.NordVPNLauncher',
+                        ],
             launchctl:  [
                           'com.nordvpn.osx.helper',
                           'com.nordvpn.NordVPN.Helper',
                         ],
-            delete:     '/Library/PrivilegedHelperTools/com.nordvpn.NordVPN.Helper',
+            delete:     [
+                          '/Library/PrivilegedHelperTools/com.nordvpn.osx.helper',
+                          '/Library/PrivilegedHelperTools/com.nordvpn.osx.ovpnDnsManager',
+                        ],
             login_item: 'NordVPN',
             pkgutil:    'com.nordvpn.osx'
 
   zap trash: [
-               '~/Library/Application Support/com.nordvpn.NordVPN',
-               '~/Library/Caches/com.nordvpn.NordVPN',
+               '~/Library/Application Support/com.nordvpn.osx',
+               '~/Library/Caches/com.nordvpn.osx',
                '~/Library/Logs/NordVPN/',
-               '~/Library/Preferences/com.nordvpn.NordVPN.plist',
-               '~/Library/Saved Application State/com.nordvpn.NordVPN.savedState',
-               '~/Library/Cookies/com.nordvpn.NordVPN.binarycookies',
+               '~/Library/Preferences/com.nordvpn.osx.plist',
+               '~/Library/Saved Application State/com.nordvpn.osx.savedState',
+               '~/Library/Cookies/com.nordvpn.osx.binarycookies',
              ]
 end
