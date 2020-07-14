b50186be167d63dc8aa2970780b43884f15f4bd1
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-13 20:09:37 -0700

    Update prepros from 7.3.12 to 7.3.13 (#85884)

--- Casks/prepros.rb
+++ Casks/prepros.rb
@@ -1,6 +1,6 @@
 cask 'prepros' do
-  version '7.3.12'
-  sha256 '903a626e75492243c5b0c8deaa9f098b1b138010c5d8ca04470c6b8d47c3e7c7'
+  version '7.3.13'
+  sha256 '92eeccbefd5fc0cc59c7fe9cf722b84bcb797b2df324202c836a9ecbf9bb6e7b'
 
   url "https://downloads.prepros.io/v#{version.major}/Prepros-#{version}.zip"
   appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://prepros.io/downloads/stable/mac'
