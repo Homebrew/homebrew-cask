7ad0e6f5b0e34836f036d90bb91bb0bf6b008e0e
miccal <miccal.matthews@gmail.com>
2020-07-14 16:21:28 +0800

    Update devdocs

--- Casks/devdocs.rb
+++ Casks/devdocs.rb
@@ -2,11 +2,10 @@ cask 'devdocs' do
   version '0.7.1'
   sha256 'cec5ab99d9d3cff879fedf5379904d0993b09a43cf3fb5838e02450e3798753a'
 
-  # github.com/egoist/devdocs-desktop/ was verified as official when first introduced to the cask
   url "https://github.com/egoist/devdocs-desktop/releases/download/v#{version}/DevDocs-#{version}.dmg"
   appcast 'https://github.com/egoist/devdocs-desktop/releases.atom'
   name 'DevDocs App'
-  homepage 'https://devdocs.egoist.sh/'
+  homepage 'https://github.com/egoist/devdocs-desktop/'
 
   app 'DevDocs.app'
 
