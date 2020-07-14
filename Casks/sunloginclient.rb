1a5fdb48300f81ab371d5a4bf920dbd2fb6a384a
Vítor Galvão <info@vitorgalvao.com>
2020-07-14 16:55:51 +0100

    Update and rename sunlogin-x.rb to sunloginclient.rb

--- Casks/sunloginclient.rb
+++ Casks/sunloginclient.rb
@@ -1,10 +1,10 @@
-cask 'sunlogin-x' do
+cask 'sunloginclient' do
   version '10.2.0.28522'
   sha256 '3b854bff658cf6c514ff007dbb73c61b48187d32b353ff637ed9b25fff7dd78e'
 
   url "http://dl-cdn.oray.com/sunlogin/mac/SunloginClient_#{version}.dmg"
-  name 'Sunlogin X for macOS'
-  name '向日葵 X for macOS'
+  name 'Sunlogin X'
+  name '向日葵 X'
   homepage 'https://sunlogin.oray.com/'
 
   pkg 'SunloginClient.pkg'
