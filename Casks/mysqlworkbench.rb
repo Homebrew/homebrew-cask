4c60b6c14bbcef49ebfcb07c521651fa40dfb124
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2020-07-14 12:19:05 +0200

    Update mysqlworkbench from 8.0.20 to 8.0.21 (#85901)

--- Casks/mysqlworkbench.rb
+++ Casks/mysqlworkbench.rb
@@ -8,8 +8,8 @@ cask 'mysqlworkbench' do
     sha256 '3478800290e2797d294e3721fdaea4c41ddc1917f2b59ec94a935e16c18dc5d2'
     url "https://downloads.mysql.com/archives/get/p/#{version.major}/file/mysql-workbench-community-#{version}-macos-x86_64.dmg"
   else
-    version '8.0.20'
-    sha256 'a5c11b83fbcb1817e982eed7a6d170ffca6c67d3a9496817c395c4a60b571cc0'
+    version '8.0.21'
+    sha256 '7d812551cc1cc38e1d5f588e6c91b07f1778c78a04bfe94dafac3a23ea425e88'
     url "https://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-#{version}-macos-x86_64.dmg"
     appcast 'https://dev.mysql.com/downloads/workbench/'
   end
