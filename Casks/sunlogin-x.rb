5182decf22c3de3f18b2c534c7b7898ce9375ca0
chocau <64587008+chocau@users.noreply.github.com>
2020-07-14 17:11:01 +0800

    Add Sunlogin X v10.2.0.28522

---
+++ Casks/sunlogin-x.rb
@@ -0,0 +1,14 @@
+cask 'sunlogin-x' do
+  version '10.2.0.28522'
+  sha256 '3b854bff658cf6c514ff007dbb73c61b48187d32b353ff637ed9b25fff7dd78e'
+
+  url "http://dl-cdn.oray.com/sunlogin/mac/SunloginClient_#{version}.dmg"
+  name 'Sunlogin X for macOS'
+  name '向日葵 X for macOS'
+  homepage 'https://sunlogin.oray.com/'
+
+  pkg 'SunloginClient.pkg'
+
+  uninstall quit:    'com.oray.sunlogin.macclient',
+            pkgutil: 'com.mygreatcompany.pkg.SunloginClient'
+end
