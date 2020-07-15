a5feff3a356277e92a351cac8e2008073ffedbc4
suschizu <brigitte@corecode.io>
2020-07-15 13:14:52 +0200

    Update texpad.rb

--- Casks/texpad.rb
+++ Casks/texpad.rb
@@ -1,17 +1,17 @@
 cask 'texpad' do
   if MacOS.version <= :yosemite
-    version '1.7.45,237,1487350'
+    version '1.7.45,237:1487350'
     sha256 '5973da0e221a9f9168228d628e25b1f788bcdc9ca8cae86cb02089804f3240f5'
   elsif MacOS.version == :el_capitan
-    version '1.8.5,404,f8f30e5'
+    version '1.8.5,404:f8f30e5'
     sha256 '676a1b071142c022cdfda57668c811f7747b36ded442548073fe6dda1b9ca934'
   else
-    version '1.8.15,517,28a9c8f'
+    version '1.8.15,517:28a9c8f'
     sha256 'afddada57132ec25d32e393b7cc26190d4d2113e678edaf63f28ef166102dc85'
   end
 
   # download.texpadapp.com/ was verified as official when first introduced to the cask
-  url "https://download.texpadapp.com/apps/osx/updates/Texpad_#{version.before_comma.dots_to_underscores}__#{version.after_comma.before_comma}__#{version.after_comma.after_comma}.dmg"
+  url "https://download.texpadapp.com/apps/osx/updates/Texpad_#{version.before_comma.dots_to_underscores}__#{version.after_comma.before_colon}__#{version.after_colon}.dmg"
   appcast 'https://www.texpad.com/static-collected/upgrades/texpadappcast.xml'
   name 'Texpad'
   homepage 'https://www.texpad.com/osx'
