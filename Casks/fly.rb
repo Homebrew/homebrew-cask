2cb65a45a1a3191cf742769aba139ae0ce359d0b
therealmarv <1050582+therealmarv@users.noreply.github.com>
2020-07-15 23:44:34 +0300

    Update fly from 6.1.0 to 6.3.0 (#86024)

--- Casks/fly.rb
+++ Casks/fly.rb
@@ -1,6 +1,6 @@
 cask 'fly' do
-  version '6.1.0'
-  sha256 '1e33de1f8f845283b73f664a9a8bc6fb8dbf6b2d2d973e89ff44b7298f2bf7f2'
+  version '6.3.0'
+  sha256 'fb63f29121dcf6d9ab73f421e41d14551b00c6f2d1387c9c4ff17aa61b038444'
 
   url "https://github.com/concourse/concourse/releases/download/v#{version}/fly-#{version}-darwin-amd64.tgz"
   appcast 'https://github.com/concourse/concourse/releases.atom'
