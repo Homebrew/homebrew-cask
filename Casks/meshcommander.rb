273819389acf0f5e55c5bec35d98cf85b123887f
suschizu <brigitte@corecode.io>
2020-07-15 16:16:24 +0200

    Update meshcommander.rb

--- Casks/meshcommander.rb
+++ Casks/meshcommander.rb
@@ -5,7 +5,7 @@ cask 'meshcommander' do
   # bintray.com/gomesjj/APPS/ was verified as official when first introduced to the cask
   url "https://bintray.com/gomesjj/APPS/download_file?file_path=#{version.no_dots}%2FMeshCommander.dmg.zip"
   appcast 'https://dl.bintray.com/gomesjj/APPS/',
-          must_contain: "#{version.major_minor}#{version.patch}"
+          must_contain: version.no_dots
   name 'MeshCommander'
   homepage 'https://www.devtty.uk/apple/Intel_Mesh_Commander_on_Mac_OS_X/'
 
