1c6d495d1c624cd6bc29c41ac108e0e07785666e
suschizu <brigitte@corecode.io>
2020-07-15 15:57:22 +0200

    Update neo4j from 1.3.1 to 1.3.2 (#85993)

--- Casks/neo4j.rb
+++ Casks/neo4j.rb
@@ -1,7 +1,7 @@
 cask 'neo4j' do
   # note: "4" is not a version number, but an intrinsic part of the product name
-  version '1.3.1'
-  sha256 'f4a5fc6383cf5615ccda425a95d40b70a3e0d780ae126e8ea571a675e6413393'
+  version '1.3.2'
+  sha256 'd68d10cb7141c76e6ea6053a1e7c0b4c9b841915c02f9d6a500dd538f8e217e4'
 
   url "https://neo4j.com/artifact.php?name=neo4j-desktop-#{version}.dmg"
   appcast 'https://neo4j.com/download/'
