350d0e17ca62c8c4f9efbc0f70e3a8ee06ef04ee
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-14 17:52:30 -0700

    Update downie from 4.0.16,4125 to 4.0.17,4127 (#85951)

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,6 +1,6 @@
 cask 'downie' do
-  version '4.0.16,4125'
-  sha256 '64f3f1b5ba6ce8367d47e1f09e6d257c68d854b11ea4b2f381f70df82d7a2e02'
+  version '4.0.17,4127'
+  sha256 '33df034a7613a7393387fe314bfa1cf4abbbee57d6fc22a214386cd0adb3c4c0'
 
   # charliemonroesoftware.com/ was verified as official when first introduced to the cask
   url "https://charliemonroesoftware.com/trial/downie/v#{version.major}/Downie_#{version.major}_#{version.after_comma}.dmg"
