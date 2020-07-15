0285eece98e1540af2a585250e80dae61dabc148
Randall <ran.dall@icloud.com>
2020-07-14 18:08:02 -0700

    Update seadrive from 2.0.3 to 2.0.4

--- Casks/seadrive.rb
+++ Casks/seadrive.rb
@@ -1,6 +1,6 @@
 cask 'seadrive' do
-  version '2.0.3'
-  sha256 'd15832a8e76fdb2126d2c81a497fc9e30b8d14cc8d0f32f68a340e6c5fad12b5'
+  version '2.0.4'
+  sha256 '51e6b7588a2c3d1a3b8141890edf3ae4ae237b1162eeee7f7becff66a6aac6da'
 
   # download.seadrive.org/ was verified as official when first introduced to the cask
   url "https://download.seadrive.org/seadrive-#{version}.dmg"
