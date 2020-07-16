c94fbcfa31331ab44796b43b81750895d1e8aa13
tscp <tscp@lol>
2020-07-16 21:21:14 +0800

    Run style fix

--- Casks/voov-meeting.rb
+++ Casks/voov-meeting.rb
@@ -2,7 +2,7 @@ cask 'voov-meeting' do
   version '1.6.0'
   sha256 '581aeaa2004e8ab4df26a5383ff89fdb6886e1fcdcced9c60e96be2d49e4aa55'
 
-  url "https://4e0bb836a1be819870c428989fb03550.dlied1.cdntips.com/dlied1.qq.com/download/VooVMeeting_1410000198_1.6.0.520.publish.dmg?mkey=5f106b283b3b0043&f=1ea2&proto=https&access_type=$header_ApolloNet"
+  url 'https://4e0bb836a1be819870c428989fb03550.dlied1.cdntips.com/dlied1.qq.com/download/VooVMeeting_1410000198_1.6.0.520.publish.dmg?mkey=5f106b283b3b0043&f=1ea2&proto=https&access_type=$header_ApolloNet'
   name 'VooV Meeting'
   name 'Tencent Meeting International Version'
   homepage 'https://voovmeeting.com/'
@@ -10,10 +10,10 @@ cask 'voov-meeting' do
   app 'VooV Meeting.app'
 
   zap trash: [
-              'Containers/com.tencent.tencentmeeting',
-              'Preferences/com.tencent.tencentmeeting.plist',
-              'Saved Application State/com.tencent.tencentmeeting.savedState',
-              'WebKit/com.tencent.tencentmeeting',
-              'Application Support/com.tencent.rqd/com.tencent.tencentmeeting',
-  ]
+               'Containers/com.tencent.tencentmeeting',
+               'Preferences/com.tencent.tencentmeeting.plist',
+               'Saved Application State/com.tencent.tencentmeeting.savedState',
+               'WebKit/com.tencent.tencentmeeting',
+               'Application Support/com.tencent.rqd/com.tencent.tencentmeeting',
+             ]
 end
