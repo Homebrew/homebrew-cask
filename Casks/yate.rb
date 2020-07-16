57fc2bee84c1b0471510860f1bb05006d9da7c9c
Brigitte Zmoelnig <brigitte@corecode.io>
2020-07-16 17:01:25 +0200

    Update yate from 6.0 to 6.0.0.2

--- Casks/yate.rb
+++ Casks/yate.rb
@@ -1,6 +1,6 @@
 cask 'yate' do
-  version '6.0'
-  sha256 '9adc4656c736d0b65807be6664b537ff392e0244b65203ef25efb0dc8d12e680'
+  version '6.0.0.2'
+  sha256 'b91353c5620d028e138d1632e55de50506650931959dc5d719ac6384d1e914dc'
 
   url 'https://2manyrobots.com/Updates/Yate/Yate.zip'
   appcast 'https://2manyrobots.com/Updates/Yate/appcast.xml'
