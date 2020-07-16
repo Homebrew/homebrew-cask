df2a8f80459533d902ef640618e7e2eb2ceaf1ca
Randall <ran.dall@icloud.com>
2020-07-15 17:36:06 -0700

    Update drawio from 13.3.9 to 13.4.4

--- Casks/drawio.rb
+++ Casks/drawio.rb
@@ -1,6 +1,6 @@
 cask 'drawio' do
-  version '13.3.9'
-  sha256 'e538c2ec08394be7380416fa3881efb508fb589aadd3704ad0fd06f3d9ad7ea5'
+  version '13.4.4'
+  sha256 '2b583e11f968040e113632719970baf84cf1f70c0b11a6a61dc341e07cd01edb'
 
   # github.com/jgraph/drawio-desktop/ was verified as official when first introduced to the cask
   url "https://github.com/jgraph/drawio-desktop/releases/download/v#{version}/draw.io-#{version}.dmg"
