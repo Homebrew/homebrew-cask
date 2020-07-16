28105c23d761559962b9596d1e2037bcda33d76d
tscp <tscp@lol>
2020-07-16 21:25:47 +0800

    Add a comment to fix the style offense

--- Casks/voov-meeting.rb
+++ Casks/voov-meeting.rb
@@ -2,6 +2,7 @@ cask 'voov-meeting' do
   version '1.6.0'
   sha256 '581aeaa2004e8ab4df26a5383ff89fdb6886e1fcdcced9c60e96be2d49e4aa55'
 
+  # 4e0bb836a1be819870c428989fb03550.dlied1.cdntips.com/dlied1.qq.com/ was verified as official when first introduced to the cask
   url 'https://4e0bb836a1be819870c428989fb03550.dlied1.cdntips.com/dlied1.qq.com/download/VooVMeeting_1410000198_1.6.0.520.publish.dmg?mkey=5f106b283b3b0043&f=1ea2&proto=https&access_type=$header_ApolloNet'
   name 'VooV Meeting'
   name 'Tencent Meeting International Version'
