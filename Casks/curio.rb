106e5fb6b943dd6ac5a79df2bc200e6077292c1b
Randall <ran.dall@icloud.com>
2020-07-16 17:08:52 -0700

    Update curio from 14010.4 to 14020.5

--- Casks/curio.rb
+++ Casks/curio.rb
@@ -1,6 +1,6 @@
 cask 'curio' do
-  version '14010.4'
-  sha256 'd9efe96b6759866c4f64c7cea40a14148b896138902ba61ca0e48af7976031fd'
+  version '14020.5'
+  sha256 '3b5694ef98b34f493ecc4cba99bbe2da6f9e0e6167b9784be6807e3bb1fc1ba4'
 
   url "https://www.zengobi.com/downloads/Curio#{version.no_dots}.zip"
   appcast 'https://www.zengobi.com/appcasts/Curio14-2ZaxaUUlKorRS4Hf.xml'
