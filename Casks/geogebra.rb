e44dfa707aa5f94f5568d2a18ac883111ed5eda6
Brigitte Zmoelnig <brigitte@corecode.io>
2020-07-17 19:05:04 +0200

    Update geogebra from 6.0.592.0 to 6.0.593.0

--- Casks/geogebra.rb
+++ Casks/geogebra.rb
@@ -1,6 +1,6 @@
 cask 'geogebra' do
-  version '6.0.592.0'
-  sha256 '0a4e900059f199d8ad6fa8f39f2cbaddb250f43e1e8882e2b9c4a00241253688'
+  version '6.0.593.0'
+  sha256 'ba8e1915139ec023b5631cca2534ed2840b839a83643fcd7d6c23d066a3a6e2d'
 
   url "https://download.geogebra.org/installers/#{version.major_minor}/GeoGebra-Classic-#{version.major}-MacOS-Portable-#{version.dots_to_hyphens}.zip"
   appcast "https://download.geogebra.org/installers/#{version.major_minor}/version.txt",
