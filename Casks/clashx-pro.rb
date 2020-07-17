296abc72eb66189f7d774cbf08c326d78cdfc401
tddschn <45612704+tddschn@users.noreply.github.com>
2020-07-17 11:47:57 +0800

    Add ClashX Pro.app

---
+++ Casks/clashx-pro.rb
@@ -0,0 +1,10 @@
+cask 'clashx-pro' do
+  version '1.30.0.2'
+  sha256 'dd6bf4c0ba9ac4ea15840ea25abf5f69a58fa572d1f597bc6bf1499443bd8311'
+
+  url "https://appcenter-filemanagement-distrib2ede6f06e.azureedge.net/a01bdef6-e1a1-4e78-82b9-ac6c3820742c/ClashX.dmg?sv=2019-02-02&sr=c&sig=hXvX%2FTDE41zW3h%2B2U8LyUqGgHojv%2FPIkFTsh6nVOVYA%3D&se=2020-07-13T10%3A06%3A36Z&sp=r&download_origin=appcenter"
+  name 'ClashX Pro'
+  homepage 'https://install.appcenter.ms/users/clashx/apps/clashx-pro/distribution_groups/public'
+
+  app 'ClashX Pro.app'
+end
