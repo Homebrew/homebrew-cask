1761b6b3ab98189d64f90bfaa9aeadcdd1ce717b
chriswl <chris.landsiedel@gmail.com>
2020-07-17 16:27:19 +0200

    update microsoft-azure-storage-explorer to 1.14.0 (#85863)
    * update microsoft-azure-storage-explorer to 1.14.0
    
    * style fix
    
    * address comments
    
    Co-authored-by: Christian Landsiedel <christian.landsiedel@microsoft.com>

--- Casks/microsoft-azure-storage-explorer.rb
+++ Casks/microsoft-azure-storage-explorer.rb
@@ -1,8 +1,10 @@
 cask 'microsoft-azure-storage-explorer' do
-  version :latest
-  sha256 :no_check
+  version '1.14.0'
+  sha256 '6b23316dd85b5ba9621e747a00e54a073dcbb920b89ae72afa8b9db07915950d'
 
-  url 'https://download.microsoft.com/download/A/E/3/AE32C485-B62B-4437-92F7-8B6B2C48CB40/StorageExplorer_signed.zip'
+  # github.com/microsoft/AzureStorageExplorer/ was verified as official when first introduced to the cask
+  url "https://github.com/microsoft/AzureStorageExplorer/releases/download/v#{version}/Mac_StorageExplorer.zip"
+  appcast 'https://github.com/microsoft/AzureStorageExplorer/releases.atom'
   name 'Microsoft Azure Storage Explorer'
   homepage 'https://azure.microsoft.com/en-us/features/storage-explorer/'
 
