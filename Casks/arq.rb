20a978493c63c6702c5976441840931b76339edc
Randall <ran.dall@icloud.com>
2020-07-16 17:13:42 -0700

    Update arq from 6.2.46 to 6.2.41

--- Casks/arq.rb
+++ Casks/arq.rb
@@ -1,5 +1,5 @@
 cask 'arq' do
-  version '6.2.46'
+  version '6.2.41'
   sha256 'aa3bac1101a1a2eb493351491629a1b72fb95c824908b880d4e2df835a72da9a'
 
   url "https://www.arqbackup.com/download/arqbackup/Arq#{version.major}.pkg"
