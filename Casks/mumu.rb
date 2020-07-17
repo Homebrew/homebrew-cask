e975126ecb449fec1ad83f76c0a6c72ebc6f6bac
suschizu <brigitte@corecode.io>
2020-07-17 22:38:13 +0200

    Update mumu from 1.0.1 to 1.0.2,lhp4WSbSgCTUaP43pY93 (#86131)

--- Casks/mumu.rb
+++ Casks/mumu.rb
@@ -1,9 +1,9 @@
 cask 'mumu' do
-  version '1.0.1'
-  sha256 '6bc059e7d455056b68ba8b39635eec38758af7397fae65d0b9eeb24332a8addd'
+  version '1.0.2,lhp4WSbSgCTUaP43pY93'
+  sha256 '5c842f2669794c2a909a13434ec172d135eea661be71e91a9abbdd39c5d2b3d3'
 
   # paddle.s3.amazonaws.com/fulfillment_downloads/116824/597910/ was verified as official when first introduced to the cask
-  url "https://paddle.s3.amazonaws.com/fulfillment_downloads/116824/597910/3UjvmpBGSxuQuBAVlQm6_Mumu%20#{version}.dmg"
+  url "https://paddle.s3.amazonaws.com/fulfillment_downloads/116824/597910/#{version.after_comma}_Mumu%20#{version.before_comma}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://vendors.paddle.com/download/product/597910'
   name 'Mumu'
   homepage 'https://getmumu.com/'
