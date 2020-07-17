1503d70f061a7f2ceee271f0a53b6bae4c688276
Chris Nakagaki (zsoldier) <chris.nakagaki@virtustream.com>
2020-07-16 22:53:19 -0400

    Update Powershell to 7.0.3

--- Casks/powershell.rb
+++ Casks/powershell.rb
@@ -1,6 +1,6 @@
 cask 'powershell' do
-  version '7.0.2'
-  sha256 'f25aa06ce580ce5dd844d3dcfcdd335d56268e0fdd9f3944a303494bc5ee85dd'
+  version '7.0.3'
+  sha256 '2C7702956F11B037CF75BF78372A9B0CB34D102031C0A293342DE21C741800ED'
 
   url "https://github.com/PowerShell/PowerShell/releases/download/v#{version}/powershell-#{version}-osx-x64.pkg"
   appcast 'https://github.com/PowerShell/PowerShell/releases.atom'
