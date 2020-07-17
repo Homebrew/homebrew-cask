2e4e6c7c6cf3c021d7e31b737893dd035a4049ce
suschizu <brigitte@corecode.io>
2020-07-17 21:51:49 +0200

    Update microsoft-azure-storage-explorer.rb

--- Casks/microsoft-azure-storage-explorer.rb
+++ Casks/microsoft-azure-storage-explorer.rb
@@ -1,9 +1,9 @@
 cask 'microsoft-azure-storage-explorer' do
-  version '1.14.0'
-  sha256 '6b23316dd85b5ba9621e747a00e54a073dcbb920b89ae72afa8b9db07915950d'
+  version '1.14.2'
+  sha256 '8198ec4314c94945df06f9335e805f8322bac78532dcf58a5f86bdd0b605c3d6'
 
   # github.com/microsoft/AzureStorageExplorer/ was verified as official when first introduced to the cask
-  url "https://github.com/microsoft/AzureStorageExplorer/releases/download/v#{version}/Mac_StorageExplorer.zip"
+  url "https://github.com/microsoft/AzureStorageExplorer/releases/download/v#{version}/MaC_StorageExplorer.zip"
   appcast 'https://github.com/microsoft/AzureStorageExplorer/releases.atom'
   name 'Microsoft Azure Storage Explorer'
   homepage 'https://azure.microsoft.com/en-us/features/storage-explorer/'
