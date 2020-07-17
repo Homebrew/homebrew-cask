f4c9aca7afd5f4efbcc0ca326fb1b4f5c3d95d5e
Peter Lewis <peter@peterlew.is>
2020-07-17 18:09:05 +0100

    Update camo-studio.rb

--- Casks/camo-studio.rb
+++ Casks/camo-studio.rb
@@ -8,6 +8,6 @@ cask 'camo-studio' do
   homepage 'https://reincubate.com/camo/'
 
   app 'Camo Studio.app'
-  
+
   uninstall delete: '/Library/CoreMediaIO/Plug-Ins/DAL/ReincubateCamoDAL.plugin'
 end
