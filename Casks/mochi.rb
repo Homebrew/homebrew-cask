a6dc14f4b2196dc39a3390232f795f4ab4dd3e80
Matthew Steedman <steedman87@gmail.com>
2020-07-17 22:28:46 -0400

    Update mochi from 1.5.7 to 1.5.9 (#86158)

--- Casks/mochi.rb
+++ Casks/mochi.rb
@@ -1,6 +1,6 @@
 cask 'mochi' do
-  version '1.5.7'
-  sha256 'cc021ef6e6cd1cd1e06462c7377d3d0411a606023caf58167eb58914d1edb733'
+  version '1.5.9'
+  sha256 '4eac36200305344efbe905924dcba378a90ef27248732d87ea6737aa933146df'
 
   url "https://mochi.cards/releases/Mochi-#{version}.dmg"
   appcast 'https://mochi.cards/'
