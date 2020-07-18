c73ca87d8ef4854dbe054d93a68067810061d3b8
Robert Blair <robertleeblairjr@gmail.com>
2020-07-18 06:33:33 -0500

    Update zulu from 14.0.1,14.28.21-ca to 14.0.2,14.29.23-ca

--- Casks/zulu.rb
+++ Casks/zulu.rb
@@ -1,6 +1,6 @@
 cask 'zulu' do
-  version '14.0.1,14.28.21-ca'
-  sha256 '9b263699fbda8ed4dd5fbc3e566487b3145019d7944efcd607595ea52ae7b549'
+  version '14.0.2,14.29.23-ca'
+  sha256 '8ed14e3a3b8fabb799716e4cebf1cf0ac6033c5f5c7597aa659c19d4459ebc7b'
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
       referer: 'https://www.azul.com/downloads/zulu/zulu-mac/'
