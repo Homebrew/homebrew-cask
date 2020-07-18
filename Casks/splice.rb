8f9a370f3c90a4d23e3d0ca558dbe8905af40b10
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-17 19:39:52 -0700

    Update splice from 3.6.2 to 3.6.3 (#86167)

--- Casks/splice.rb
+++ Casks/splice.rb
@@ -1,6 +1,6 @@
 cask 'splice' do
-  version '3.6.2'
-  sha256 '8e7992b1b49057c3f8d31d84383583073dd865cf03fdcdf9dcb6dd4ed51b7f45'
+  version '3.6.3'
+  sha256 '9d7dbd701ee25c558d06424f60eeda73a15f2919a5683aa20a11544d97f1ac99'
 
   # splicedesktop.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url 'https://splicedesktop.s3.amazonaws.com/darwin/stable/Splice.app.zip'
