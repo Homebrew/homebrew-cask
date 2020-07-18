4b893552bbc48f24dd9057e73e58134de466017e
suschizu <brigitte@corecode.io>
2020-07-18 22:02:52 +0200

    Update parallels-client from 17.1.1-21772 to 17.1.2-21853 (#86191)

--- Casks/parallels-client.rb
+++ Casks/parallels-client.rb
@@ -1,6 +1,6 @@
 cask 'parallels-client' do
-  version '17.1.1-21772'
-  sha256 'f027d4dbfe884188f3a695a024c2743c74b5412c8fef1dd8285bf11b96a416d8'
+  version '17.1.2-21853'
+  sha256 '0a6faf0513cbf182423b60b7f6c621195efa5d19719be81079e7fc21d398f38b'
 
   url "https://download.parallels.com/ras/v#{version.major}/#{version.hyphens_to_dots}/RasClient-Mac-Appstore-#{version}.pkg"
   appcast "https://download.parallels.com/ras/v#{version.major}/RAS%20Client%20for%20Mac%20Changelog.txt"
