1231c7ebb78e8aa9e0beac6605e23b927c6a3bac
Randall <ran.dall@icloud.com>
2020-07-18 12:57:02 -0700

    Update bitwig-studio from 3.2.5 to 3.2.6

--- Casks/bitwig-studio.rb
+++ Casks/bitwig-studio.rb
@@ -1,6 +1,6 @@
 cask 'bitwig-studio' do
-  version '3.2.5'
-  sha256 'e0c83b24766644c72bb430f0c649971ca560ad0fda3ef294422d80392fdaf7ef'
+  version '3.2.6'
+  sha256 'f3f24088a21fb9815a18f0256ded2aa8231b2b37a55cd13eb7913277ef442a8e'
 
   url "https://downloads.bitwig.com/stable/#{version}/Bitwig%20Studio%20#{version}.dmg"
   appcast 'https://www.bitwig.com/en/download.html'
