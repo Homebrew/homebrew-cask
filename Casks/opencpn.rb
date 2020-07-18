04f670da19457e636d7e87af27720c5a6c5c865f
suschizu <brigitte@corecode.io>
2020-07-18 22:02:32 +0200

    Update opencpn from 5.0.1,0266678 to 5.2.0,1.4d956e1 (#86128)

--- Casks/opencpn.rb
+++ Casks/opencpn.rb
@@ -1,11 +1,16 @@
 cask 'opencpn' do
-  version '5.0.1,0266678'
-  sha256 'b3da4a758d898a9a2f79ba2618ff6cf9529b06dd1ccba14f4dd3e4996660b143'
+  version '5.2.0,1.4d956e1'
+  sha256 'b31c23bd02dfaf7a8844db39c894c0765b386b4a082cb0b2e8fab7e2ed9f1209'
 
-  url "http://download.opencpn.org/#{version.before_comma}/OpenCPN_#{version.before_comma}+#{version.after_comma}.dmg"
+  url "http://download.opencpn.org/#{version.before_comma}/OpenCPN_#{version.before_comma}+#{version.after_comma}.pkg"
   appcast 'https://github.com/OpenCPN/OpenCPN/releases.atom'
   name 'OpenCPN'
   homepage 'https://www.opencpn.org/'
 
-  app 'OpenCPN.app'
+  pkg "OpenCPN_#{version.before_comma} #{version.after_comma}.pkg"
+
+  uninstall pkgutil: [
+                       'org.opencpn.pkg.OpenCPN',
+                       'org.opencpn',
+                     ]
 end
