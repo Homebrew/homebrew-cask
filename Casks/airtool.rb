663d3d558d07b7c5076469bcf5e96679d446b11f
jabenninghoff <jbenninghoff@mac.com>
2020-07-18 12:50:32 -0500

    Update airtool from 1.9 to 1.9.1

--- Casks/airtool.rb
+++ Casks/airtool.rb
@@ -1,11 +1,11 @@
 cask 'airtool' do
-  version '1.9'
-  sha256 '605cf374a8c12ea2f976e78e33289095232a80a6ece1e748103c6f109b234d50'
+  version '1.9.1'
+  sha256 '85a469adad7be1a53a2c2e0a99edf97da8109de4a057e30431cebb0f7f1f9430'
 
-  url "https://www.adriangranados.com/downloads/Airtool_#{version}.pkg"
-  appcast 'https://www.adriangranados.com/apps/airtool'
+  url "https://www.intuitibits.com/downloads/Airtool_#{version}.pkg"
+  appcast 'https://www.intuitibits.com/appcasts/airtoolcast.xml'
   name 'Airtool'
-  homepage 'https://www.adriangranados.com/apps/airtool'
+  homepage 'https://www.intuitibits.com/products/airtool/'
 
   pkg "Airtool_#{version}.pkg"
 
@@ -14,12 +14,10 @@ cask 'airtool' do
   end
 
   uninstall pkgutil:    [
-                          'com.adriangranados.airtool.airtool-bpf.*',
-                          'com.adriangranados.airtool.Airtool.pkg',
+                          'com.intuitibits.airtool-helper.pkg',
+                          'com.intuitibits.airtool.pkg',
                         ],
-            launchctl:  'com.adriangranados.airtool.airtool-bpf',
+            launchctl:  'com.intuitibits.airtool.airtool-bpf',
             login_item: 'Airtool',
             delete:     '/Library/Application Support/Airtool'
-
-  zap trash: '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.adriangranados.airtool.sfl*'
 end
