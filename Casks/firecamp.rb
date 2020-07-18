8f84e5c4b9b3eed8c30f0212973b40d8068081ef
Nishchit <dnishchit@gmail.com>
2020-07-18 15:03:08 +0530

    Firecamp upgraded to v1.4.4

--- Casks/firecamp.rb
+++ Casks/firecamp.rb
@@ -1,5 +1,5 @@
 cask 'firecamp' do
-  version '1.4.4'
+  version '1.4.4' 
   sha256 'de6e433037328709b9c0d2e7b321550945cc323d92e8d7b83f5e7f3bd9c5a5aa'
 
   # firecamp.ams3.digitaloceanspaces.com/ was verified as official when first introduced to the cask
