603330098d471b7fc71fdb8e129d038119f7a422
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-19 14:19:36 -0700

    Update clean-me from 1.4.1 to 1.4.2 (#86237)

--- Casks/clean-me.rb
+++ Casks/clean-me.rb
@@ -1,6 +1,6 @@
 cask 'clean-me' do
-  version '1.4.1'
-  sha256 'a074546909de68b0b378b58f6804a118f40a03077083a5a61a19c588f0841648'
+  version '1.4.2'
+  sha256 'f58124740e8d9cbf8b4f45ee9a3e30a2aced109381fc34583d1c28d49d54dfe5'
 
   url "https://github.com/Kevin-De-Koninck/Clean-Me/releases/download/v#{version}/Clean.Me.app.zip"
   appcast 'https://github.com/Kevin-De-Koninck/Clean-Me/releases.atom'
