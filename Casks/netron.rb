425b56bbe2f077f04c46381f464567ee98675482
Lutz Roeder <lutzroeder@users.noreply.github.com>
2020-07-19 13:48:46 -0700

    Update Netron to 4.4.1 (#86231)

--- Casks/netron.rb
+++ Casks/netron.rb
@@ -1,6 +1,6 @@
 cask 'netron' do
-  version '4.4.0'
-  sha256 '3649a95a9bf85ae006972724af4a90d762f45ec313f2554d39aa8cc5a7aa2d75'
+  version '4.4.1'
+  sha256 '0dd0562677bf9fd22633ae512190b2ef0429ea878175d6a7ab0dbab25fb710ec'
 
   url "https://github.com/lutzroeder/netron/releases/download/v#{version}/Netron-#{version}-mac.zip"
   appcast 'https://github.com/lutzroeder/netron/releases.atom'
