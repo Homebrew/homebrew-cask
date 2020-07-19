79061e598fefe9615af5c22fb5e9e8690fe061e7
R0uter <R0uter@users.noreply.github.com>
2020-07-19 23:33:45 +1000

    Update loginputmac from 2.3.5 to 2.3.6 (#86224)

--- Casks/loginputmac.rb
+++ Casks/loginputmac.rb
@@ -1,6 +1,6 @@
 cask 'loginputmac' do
-  version '2.3.5'
-  sha256 '91c5e011792f3ab2ee7f3e69ff7ea3a45c156f02b574cba2c57dbcfec82dd4a6'
+  version '2.3.6'
+  sha256 '473040a7a8ebd5d1a811903af0791b11060044869e799832be73f1b232243bcd'
 
   # loginput-mac2.content-delivery.top/ was verified as official when first introduced to the cask
   url "https://loginput-mac2.content-delivery.top/loginputmac#{version.major}_latest.pkg"
