5a449f75c71358b3148a1089fb55852097c8d032
suschizu <brigitte@corecode.io>
2020-07-19 23:27:22 +0200

    Update camo-studio from 1.0.1,53 to 1.0.3,55 (#86233)

--- Casks/camo-studio.rb
+++ Casks/camo-studio.rb
@@ -1,6 +1,6 @@
 cask 'camo-studio' do
-  version '1.0.1,53'
-  sha256 '3da0ac5fbfea28848de0ee7fd5832298884e241a189d56150013dfebfba3589d'
+  version '1.0.3,55'
+  sha256 '2e38735162242808c8d4823a47fbf1551923a5a27bba2b836c551f90a36baf83'
 
   url "https://reincubate.com/res/labs/camo/Camo%20Studio%20#{version.before_comma}%20%28#{version.after_comma}%29%20%5BRelease%5D.app.zip"
   appcast 'https://uds.reincubate.com/release-notes/camo/?format=sparkle'
