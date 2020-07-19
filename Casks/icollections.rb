eb1fe40d2fab6abfdb8b44d3ee1eaf2c060ed9ac
Brigitte Zmoelnig <brigitte@corecode.io>
2020-07-19 22:56:53 +0200

    Update icollections

--- Casks/icollections.rb
+++ Casks/icollections.rb
@@ -1,6 +1,6 @@
 cask 'icollections' do
   version '6.5.1'
-  sha256 '469389ed85bd213c669d5488626d245d3d00843e8910bccdcac63223ba34675a'
+  sha256 'c9a1bc21761af8edc1b82e97b14b18e2b9c64a5439e8fed4dfab1c07c0645aea'
 
   url 'https://naarakstudio.com/download/iCollections.dmg'
   appcast 'https://naarakstudio.com/download/icollections/update.ver',
