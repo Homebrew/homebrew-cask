22f1865f3501d25c2d4dc4777dd4aea89bd566e5
Randall <ran.dall@icloud.com>
2020-07-20 16:40:36 -0700

    Update buttercup from 1.20.3 to 1.20.4

--- Casks/buttercup.rb
+++ Casks/buttercup.rb
@@ -1,6 +1,6 @@
 cask 'buttercup' do
-  version '1.20.3'
-  sha256 '7f64327a796d673353c6a71d746923b20d84bfc0f711d80556f8e96b60bae822'
+  version '1.20.4'
+  sha256 'dd5fc94a51deb271befbd5092b2e1523511bec762a6e72423672d34efbc67f4d'
 
   # github.com/buttercup/buttercup-desktop/ was verified as official when first introduced to the cask
   url "https://github.com/buttercup/buttercup-desktop/releases/download/v#{version}/Buttercup-#{version}.dmg"
