ca114416ab89a32f6f92fada46274896f1556023
Luke <lburbidg@ucsd.edu>
2020-07-20 12:48:40 -0700

    Rename token, name; remove target

--- Casks/project-slippi-dolphin.rb
+++ Casks/project-slippi-dolphin.rb
@@ -1,12 +1,12 @@
-cask 'slippi' do
+cask 'project-slippi-dolphin' do
   version '2.2.0'
   sha256 'dcb9ce958b53a283894ef96b9ffeb159b680ca50c1adbddf2342884c7cd5ffe8'
 
   # github.com/project-slippi/Ishiiruka/ was verified as official when first introduced to the cask
   url "https://github.com/project-slippi/Ishiiruka/releases/download/v#{version}/FM-Slippi-#{version}-Mac.zip"
   appcast 'https://github.com/project-slippi/Ishiiruka/releases.atom'
-  name 'Project Slippi'
+  name 'Slippi'
   homepage 'https://slippi.gg/'
 
-  app 'Dolphin.app', target: 'Slippi-Dolphin.app'
+  app 'Dolphin.app'
 end
