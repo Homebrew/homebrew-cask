1db746f8f68b524ed31b4f7560bae16bf21e7990
Brigitte Zmoelnig <brigitte@corecode.io>
2020-07-20 16:21:53 +0200

    Update tap-forms from 5.3.11 to 5.3.12

--- Casks/tap-forms.rb
+++ Casks/tap-forms.rb
@@ -1,10 +1,10 @@
 cask 'tap-forms' do
-  version '5.3.11'
-  sha256 '4dd6383a90108a754914819ee0452ac53c05ead7b004a38e8bf243ca956bb0a7'
+  version '5.3.12'
+  sha256 '4fb3edc3006d52f98e2ecdd148b8e9e6e52dd19dda1183f6da051dcefa53fcaa'
 
   # vendors.paddle.com/download/product/503174 was verified as official when first introduced to the cask. Developer uses paddle.com to process licences and payments for the software
   url 'https://vendors.paddle.com/download/product/503174'
-  appcast "https://www.tapforms.com/app/tf-mac-#{version.major_minor}-sparkle-appcast.xml"
+  appcast "https://www.tapforms.com/app/tf-mac-#{version.major}.1-sparkle-appcast.xml"
   name 'Tap Forms 5'
   homepage 'https://www.tapforms.com/'
 
