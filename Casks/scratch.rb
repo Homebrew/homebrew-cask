136b9404cc60154864586a8454bdfb2a5b13508d
Randall <ran.dall@icloud.com>
2020-07-20 16:49:58 -0700

    Update scratch from 3.10.2 to 3.12.0

--- Casks/scratch.rb
+++ Casks/scratch.rb
@@ -1,6 +1,6 @@
 cask 'scratch' do
-  version '3.10.2'
-  sha256 '3e2959ba7928f95f823826e29b41e57e4aa83f771350a871ab6782ae612a0544'
+  version '3.12.0'
+  sha256 '7d811ca578a38850e518a0ab22318193201916e533b50b5ca065b97c87c959e1'
 
   url "https://downloads.scratch.mit.edu/desktop/Scratch%20Desktop-#{version}.dmg"
   name 'Scratch'
