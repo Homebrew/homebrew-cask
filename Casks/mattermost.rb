e5826e0d0d8baa28eb6f9c0191784977d66dc369
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2020-07-21 08:15:10 -0700

    updated mattermost to 4.5.2

--- Casks/mattermost.rb
+++ Casks/mattermost.rb
@@ -1,6 +1,6 @@
 cask 'mattermost' do
-  version '4.5.1'
-  sha256 '94481909fd1c573bd8ea0fac23dcbc721adff3f71c370c898bf8fc1475b6e7f3'
+  version '4.5.2'
+  sha256 'd3d3cacf9185d9887fb3460eb66b6fbce398c7a7d85b8cc058a7385f89639e38'
 
   url "https://releases.mattermost.com/desktop/#{version}/mattermost-desktop-#{version}-mac.zip"
   appcast 'https://github.com/mattermost/desktop/releases.atom'
