c0c49eec7e449f6dd4d3c17466dd852dcff495d0
Randall <ran.dall@icloud.com>
2020-07-21 13:43:28 -0700

    Update remote-desktop-manager-free from 2020.2.0.0 to 2020.2.1.0

--- Casks/remote-desktop-manager-free.rb
+++ Casks/remote-desktop-manager-free.rb
@@ -1,6 +1,6 @@
 cask 'remote-desktop-manager-free' do
-  version '2020.2.0.0'
-  sha256 'dd9144baf23b9adea89cdede818f975e2b8a282f36b54e32f562a5eeb974525c'
+  version '2020.2.1.0'
+  sha256 '1cfe99a16ef2ee0b2ef26ab7fb9fcecbc3bcf6d2afc1b2d958a5408e4ff7df14'
 
   # devolutions.net/ was verified as official when first introduced to the cask
   url "https://cdn.devolutions.net/download/Mac/Devolutions.RemoteDesktopManager.Free.Mac.#{version}.dmg"
