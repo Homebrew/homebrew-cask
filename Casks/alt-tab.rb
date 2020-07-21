a3a1f54d757bee0e5d887a769fbf8ab70c0c4f6a
Travis CI <travis@Traviss-Mac.local>
2020-07-21 09:46:13 +0000

    Update alt-tab from 4.7.1 to 4.7.2

--- Casks/alt-tab.rb
+++ Casks/alt-tab.rb
@@ -1,6 +1,6 @@
 cask 'alt-tab' do
-  version '4.7.1'
-  sha256 'fb5b30ae6244be934d5cc05cdca1ceaee1cbb29b8c6f3cb6a84480f7e9e53201'
+  version '4.7.2'
+  sha256 'aad7f9fdb7a5f62b4f9f0f4c332f27009d2fe9ab8822b3b6973d3c61937a1448'
 
   url "https://github.com/lwouis/alt-tab-macos/releases/download/v#{version}/AltTab-#{version}.zip"
   appcast 'https://github.com/lwouis/alt-tab-macos/releases.atom'
