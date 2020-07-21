8595d2d0fa5231d4edf0a4e0729252f97751da36
Randall <ran.dall@icloud.com>
2020-07-21 14:29:57 -0700

    Update resilio-sync from 2.7.1 to 2.7.2

--- Casks/resilio-sync.rb
+++ Casks/resilio-sync.rb
@@ -1,6 +1,6 @@
 cask 'resilio-sync' do
-  version '2.7.1'
-  sha256 'ac17763ded34445e52ad958279631707ad91604d8fa7a8ff225286c4b43124f5'
+  version '2.7.2'
+  sha256 'be40c064b28284630b47a887923d815256af550acd85722960bafea7c2722c52'
 
   url "https://download-cdn.resilio.com/#{version}/osx/Resilio-Sync.dmg"
   appcast "https://help.resilio.com/hc/en-us/articles/206216855-Sync-#{version.major}-x-change-log"
