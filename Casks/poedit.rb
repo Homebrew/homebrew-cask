bb99526466d553fbcf025dcbea1628bb0df5f89a
Randall <ran.dall@icloud.com>
2020-07-21 13:52:05 -0700

    Update poedit from 2.3.1 to 2.4

--- Casks/poedit.rb
+++ Casks/poedit.rb
@@ -1,6 +1,6 @@
 cask 'poedit' do
-  version '2.3.1'
-  sha256 'a6e5314df29a1ac04a4798e28b74462b829c96f1d037058145083fea2a43e891'
+  version '2.4'
+  sha256 'e2a9adcab58ceef77b30ca2088fc98e34cb0287e47753e055e7f9c218e572627'
 
   url "https://download.poedit.net/Poedit-#{version}.zip"
   appcast "https://poedit.net/updates_v#{version.major}/osx/appcast"
