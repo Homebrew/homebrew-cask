de6329287058bd5e884fda59d6c9d857b8234901
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-21 14:19:51 -0700

    Update xmind-zen from 10.1.3-202005060027 to 10.2.0-202007201811 (#86348)

--- Casks/xmind-zen.rb
+++ Casks/xmind-zen.rb
@@ -1,6 +1,6 @@
 cask 'xmind-zen' do
-  version '10.1.3-202005060027'
-  sha256 '98cfc281f6be9adfcbe3fc714e8c0effb16e8445e0ca3f9ab73d0b7869b586e1'
+  version '10.2.0-202007201811'
+  sha256 '0bcb2bd00e38633bf72936f6685c25359c595b811afeec837b4f8a922a932658'
 
   url "https://www.xmind.net/xmind/downloads/XMind-2020-for-macOS-#{version}.dmg"
   appcast 'https://www.xmind.net/download/'
