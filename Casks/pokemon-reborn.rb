2fd718d771028a30c92a0be6c2adc3e61832fb13
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-20 17:47:20 -0700

    Update pokemon-reborn from 18.4.2 to 18.4.3 (#86293)

--- Casks/pokemon-reborn.rb
+++ Casks/pokemon-reborn.rb
@@ -1,6 +1,6 @@
 cask 'pokemon-reborn' do
-  version '18.4.2'
-  sha256 'fe8858e6890c00d9ee750fdbc63bae7a8ca765bcdfd56885b4b9fc6ed6fce393'
+  version '18.4.3'
+  sha256 '438933eb0696fddaa9969a16db8dd1fcfb61657752d509745618c85558534ef1'
 
   url "https://www.rebornevo.com/downloads/Reborn#{version}%204%20Fruits.zip"
   appcast 'https://www.rebornevo.com/pr/download/'
