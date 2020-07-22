2506f2d909aa42fd44789d5b6852d9bef5f8a951
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-22 07:32:50 -0700

    Update iina-plus from 0.4.15,20061218 to 0.5.0,20072018 (#86276)
    * Update iina-plus from 0.4.15,20061218 to 0.5.0,20072018
    
    * Update iina-plus.rb
    
    Co-authored-by: CoreCode <core-code@users.noreply.github.com>

--- Casks/iina-plus.rb
+++ Casks/iina-plus.rb
@@ -1,11 +1,11 @@
 cask 'iina-plus' do
-  version '0.4.15,20061218'
-  sha256 'b8dfa5899f0f00e570484e951769a3f8400cba1c13f2ee6ae2bfc054a6a62d8d'
+  version '0.5.0,20072018'
+  sha256 '2245826be480346e1f33fa1af942bcc6d57c727a34823a29f6caeb61030178e2'
 
   url "https://github.com/xjbeta/iina-plus/releases/download/#{version.before_comma}(#{version.after_comma})/iina+.#{version.before_comma}.zip"
   appcast 'https://github.com/xjbeta/iina-plus/releases.atom'
   name 'IINA+'
   homepage 'https://github.com/xjbeta/iina-plus'
 
-  app "iina+ #{version.before_comma}.app"
+  app 'iina+.app'
 end
