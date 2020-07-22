718e9cdc2daec3356653f68ce262c32c8a5aa7b6
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-22 10:18:34 -0700

    Update fantastical from 3.1.1 to 3.1.2 (#86402)

--- Casks/fantastical.rb
+++ Casks/fantastical.rb
@@ -1,6 +1,6 @@
 cask 'fantastical' do
-  version '3.1.1'
-  sha256 '831a5d188c638af576867f0f0e7c79fb62a581d70f9a6c4b57bc7930c02c9c32'
+  version '3.1.2'
+  sha256 'c7f774655620166a8becb690993fde4093075c2ccc1d69943596f4eed4073e25'
 
   url "https://cdn.flexibits.com/Fantastical_#{version}.zip"
   appcast 'https://flexibits.com/fantastical/appcast2.php'
