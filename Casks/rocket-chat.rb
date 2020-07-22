f0973bf014f007f653b204423083293f2ecd475f
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-22 10:34:14 -0700

    Update rocket-chat from 2.17.10 to 2.17.11 (#86386)

--- Casks/rocket-chat.rb
+++ Casks/rocket-chat.rb
@@ -1,6 +1,6 @@
 cask 'rocket-chat' do
-  version '2.17.10'
-  sha256 'e53be81124b86218e3c2443ce5b08d11df71eacc59b96643cde0bb3b1e088119'
+  version '2.17.11'
+  sha256 '13d7ac944ea2e411ecbd473f9a34d2cfa8e80dd52d115ac2a14b9c55e8fe3afd'
 
   # github.com/RocketChat/Rocket.Chat.Electron/ was verified as official when first introduced to the cask
   url "https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/#{version}/rocketchat-#{version}.dmg"
