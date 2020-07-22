010b96a6b613835f44a137840d5516a19e8b27a1
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2020-07-22 15:34:34 +0200

    Update splayer from 4.9.1 to 4.9.2 (#86369)

--- Casks/splayer.rb
+++ Casks/splayer.rb
@@ -1,8 +1,9 @@
 cask 'splayer' do
-  version '4.9.1'
-  sha256 'f7755ae224794f221cdd7fbd83cabc1b38fd4e554cd2a74907f75291bccf23bd'
+  version '4.9.2'
+  sha256 '92b6c3c564334593a415e05959a2cd325346b64b22f86438aa1f0acf9027a878'
 
-  url "https://cdn.splayer.org/download/SPlayer-#{version}.dmg"
+  # github.com/chiflix/splayerx/ was verified as official when first introduced to the cask
+  url "https://github.com/chiflix/splayerx/releases/download/#{version}/SPlayer-#{version}.dmg"
   appcast 'https://github.com/chiflix/splayerx/releases.atom'
   name 'SPlayer'
   homepage 'https://splayer.org/'
