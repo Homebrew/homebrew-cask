9b5d4d3a3f50e5f98b1084e70b9809618ed73370
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-22 10:19:46 -0700

    Update kitematic from 0.17.11 to 0.17.12 (#86387)

--- Casks/kitematic.rb
+++ Casks/kitematic.rb
@@ -1,6 +1,6 @@
 cask 'kitematic' do
-  version '0.17.11'
-  sha256 '52906fd3fc98d8ce94ff24e19714879024fb7d07a2daae4e59187ec969cd2ddc'
+  version '0.17.12'
+  sha256 '39e673d2760529fe05e57c213e7f0a5cbaaef1ea76911a3f8514276972f6579f'
 
   # github.com/docker/kitematic/ was verified as official when first introduced to the cask
   url "https://github.com/docker/kitematic/releases/download/v#{version}/Kitematic-#{version}-Mac.zip"
