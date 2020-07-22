29f442576dbcc34501028b5937e52239619469fd
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-22 10:57:52 -0700

    Update sweet-home3d from 6.3 to 6.4 (#86413)

--- Casks/sweet-home3d.rb
+++ Casks/sweet-home3d.rb
@@ -1,6 +1,6 @@
 cask 'sweet-home3d' do
-  version '6.3'
-  sha256 '15dc11dac6ee452b953196c0a74c2e1e5349b0446272d0321f73b3e4cdcac448'
+  version '6.4'
+  sha256 '39f1c2a97dcd5503e80e3bf1d3c0c5f3b70ada5504dc5377438bb41e0a910544'
 
   # sourceforge.net/sweethome3d/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/sweethome3d/SweetHome3D/SweetHome3D-#{version}/SweetHome3D-#{version}-macosx.dmg"
