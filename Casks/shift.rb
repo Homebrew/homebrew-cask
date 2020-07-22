8d53dc6dec944d2cde51b8151db5582cfff8f5b3
Randall <ran.dall@icloud.com>
2020-07-22 10:08:33 -0700

    Update shift from 5.0.72 to 5.0.76

--- Casks/shift.rb
+++ Casks/shift.rb
@@ -1,5 +1,5 @@
 cask 'shift' do
-  version '5.0.72'
+  version '5.0.76'
   sha256 'f02685f3264da799c6fad885a3d6c38cf85537f342779f44d9ee560092c4cc26'
 
   url "https://update.tryshift.com/download/version/#{version}/osx_64"
