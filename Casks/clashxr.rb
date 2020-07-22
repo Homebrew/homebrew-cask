ab2e830ef70d80bd4058422d53542378b74ecccb
Randall <ran.dall@icloud.com>
2020-07-22 09:49:09 -0700

    Update clashxr from 1.20.3 to 1.30.0

--- Casks/clashxr.rb
+++ Casks/clashxr.rb
@@ -1,6 +1,6 @@
 cask 'clashxr' do
-  version '1.20.3'
-  sha256 '6bc1823d89563604b277c8d9e8568147b4c41fd86fecc8b5d467bc858c05b00f'
+  version '1.30.0'
+  sha256 '1e12b79a90231764252557c30cd60a78158340acdab5f3082ff22fd42305a336'
 
   url "https://github.com/paradiseduo/ClashXR/releases/download/#{version}/ClashXR.dmg"
   appcast 'https://github.com/paradiseduo/ClashXR/releases.atom'
