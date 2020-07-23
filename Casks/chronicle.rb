fd55a2ce8ca85de8618b20d08713262c997a0b8b
suschizu <brigitte@corecode.io>
2020-07-23 20:22:24 +0200

    Update chronicle.rb from 9.7.0 to 9.7.1 (#86448)

--- Casks/chronicle.rb
+++ Casks/chronicle.rb
@@ -1,5 +1,5 @@
 cask 'chronicle' do
-  version '9.7.0'
+  version '9.7.1'
   sha256 '7ed6b98e0b237aa7ce954931ac15cd392d64062587f79c0b25d1cb8d86124aa9'
 
   url 'https://www.chronicleapp.com/static/downloads/chroniclepro.zip'
