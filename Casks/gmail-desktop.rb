67df09fa4d2c1166d818f6a4fe63b3a2eebfc10e
Wei Chen <wei@MacBook-Pro.zombat-dev>
2020-07-23 14:45:00 +1000

    Add Gmail Desktop v2.20.0

--- Casks/gmail-desktop.rb
+++ Casks/gmail-desktop.rb
@@ -2,7 +2,7 @@ cask 'gmail-desktop' do
   version '2.20.0'
   sha256 '0cbde7446c1545000397b0b818eb0a7069d915438f5acea5315c844f3a74e5ca'
 
-  url "https://github.com/timche/gmail-desktop/releases/download/v2.20.0/gmail-desktop-#{version}-mac.dmg"
+  url "https://github.com/timche/gmail-desktop/releases/download/v#{version}/gmail-desktop-#{version}-mac.dmg"
   appcast 'https://github.com/timche/gmail-desktop/releases.atom'
   name 'Gmail Desktop'
   homepage 'https://github.com/timche/gmail-desktop'
