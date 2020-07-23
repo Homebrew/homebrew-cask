3607ff0643153a48ab4f5bcf7c0033aea6dac92e
suschizu <brigitte@corecode.io>
2020-07-23 04:13:24 +0200

    Update yourkit-java-profiler.rb from 2019.8,b141 to 2019.8,b142 (#86381)

--- Casks/yourkit-java-profiler.rb
+++ Casks/yourkit-java-profiler.rb
@@ -1,6 +1,6 @@
 cask 'yourkit-java-profiler' do
-  version '2019.8,b141'
-  sha256 '0b717ad2c065e3eb7f3dfb5a06784b181a92345bbb8b77d296b310c231609065'
+  version '2019.8,b142'
+  sha256 'eba76051f0022f8d4fb0d800872bff84d958f3bde4f501926643f80aba7e8934'
 
   url "https://www.yourkit.com/download/YourKit-JavaProfiler-#{version.before_comma}-#{version.after_comma}.dmg"
   appcast 'https://www.yourkit.com/download/'
