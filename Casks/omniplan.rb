9d1b2008a0981f5846b94d92e855cad122b5da4e
Miccal Matthews <miccal.matthews@gmail.com>
2020-07-24 07:19:19 +0800

    Update omniplan from 4.0.1 to 4.0.2 (#86467)

--- Casks/omniplan.rb
+++ Casks/omniplan.rb
@@ -5,8 +5,8 @@ cask 'omniplan' do
 
     url "https://downloads.omnigroup.com/software/MacOSX/10.13/OmniPlan-#{version}.dmg"
   else
-    version '4.0.1'
-    sha256 'db3329b812699a3c2eeb269d372e390c081345fa5f64975848cc2cfa920653ce'
+    version '4.0.2'
+    sha256 'a7e2abcd825787d4e897280a91837a134c957578b7f4e5cc7cc5f57c31b95fc3'
 
     url "https://downloads.omnigroup.com/software/MacOSX/10.14/OmniPlan-#{version}.dmg"
   end
