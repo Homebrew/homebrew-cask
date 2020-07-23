cd23932f6a4912c50b58a33562b3eeb5e2483025
Guillaume <hi@255kb.dev>
2020-07-23 22:44:12 +0200

    Update Mockoon to v1.9.0

--- Casks/mockoon.rb
+++ Casks/mockoon.rb
@@ -1,6 +1,6 @@
 cask 'mockoon' do
-  version '1.8.0'
-  sha256 '5c9d951a26acb33584cf1a1ceaf374ada52428a440efb6070edfed1ac448678e'
+  version '1.9.0'
+  sha256 '6b316a3fbad7c62a8e5840a8cbaf18f2613930d944224a830000107717814794'
 
   # github.com/mockoon/mockoon/ was verified as official when first introduced to the cask
   url "https://github.com/mockoon/mockoon/releases/download/v#{version}/mockoon.setup.#{version}.dmg"
