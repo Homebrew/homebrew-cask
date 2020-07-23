541aa8773777f876a04fad0fed6173a4540ebc57
Sharjeel Junaid <sharjeelj@outlook.com>
2020-07-23 03:04:47 -0600

    Update plex from 1.14.0.1340-0acc754c to 1.15.1.1358-43550df5

--- Casks/plex.rb
+++ Casks/plex.rb
@@ -1,6 +1,6 @@
 cask 'plex' do
-  version '1.14.0.1340-0acc754c'
-  sha256 'a394dc35118c0b89042ae3718eecd1bd1162906aeb8130f79b2bbff233b2ab06'
+  version '1.15.1.1358-43550df5'
+  sha256 '221e390c66c93c0d0618249d03338af5f4e11b5949f0aec34cdf7c7ac0d758d5'
 
   url "https://downloads.plex.tv/plex-desktop/#{version}/macos/Plex-#{version}-x86_64.zip"
   appcast 'https://plex.tv/api/downloads/6.json'
