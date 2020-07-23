eea28bf5ca1e1ddd56fe4372a245d11a63c9679c
suschizu <brigitte@corecode.io>
2020-07-23 17:49:01 +0200

    Update grids.rb (#86447)

--- Casks/grids.rb
+++ Casks/grids.rb
@@ -1,6 +1,6 @@
 cask 'grids' do
-  version '6.1'
-  sha256 '49c8cbf35655af6a7e23b6e2f6d0a2e256ae7492390aff50afa13ef12e0c6965'
+  version '6.1.2'
+  sha256 '52222ea98e78352a2ec9f2c702b2e83cff063ba494f85c202b059eb3b4330129'
 
   url "https://gridsapp.net/bin/Grids_#{version}.zip"
   appcast 'https://gridsapp.net/appcast.json'
