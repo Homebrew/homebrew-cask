cffd4d4cd1d1ea77a647e60fdd39fa20cb3bc6fd
Brigitte Zmoelnig <brigitte@corecode.io>
2020-07-23 18:48:54 +0200

    Update remotix-agent from 1.4-20339 to 1.4.2-20472

--- Casks/remotix-agent.rb
+++ Casks/remotix-agent.rb
@@ -1,6 +1,6 @@
 cask 'remotix-agent' do
-  version '1.4-20339'
-  sha256 'c3771539be013612fcd87702ca7858fd1ff2a43df9eb738b6414e4578b6306de'
+  version '1.4.2-20472'
+  sha256 '97ccca1ae621a78b55fa98a6413727e58132b45ce812bbf218160271806ae4e6'
 
   # remotix.com/agent-mac was verified as official when first introduced to the cask
   url "https://downloads.remotix.com/agent-mac/RemotixAgent-#{version}.pkg"
