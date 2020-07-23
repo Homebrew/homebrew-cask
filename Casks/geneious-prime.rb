160b9983c275c6580683b63be22a07365d1b1acb
Jason Pickens <jasonpickensnz@gmail.com>
2020-07-24 10:01:30 +1200

    Update geneious-prime from 2020.2.1 to 2020.2.2

--- Casks/geneious-prime.rb
+++ Casks/geneious-prime.rb
@@ -1,6 +1,6 @@
 cask 'geneious-prime' do
-  version '2020.2.1'
-  sha256 'e7c4c5384bfe346d080381a3eff5ad2e3e853713014e4f8605adb8b9ffe0c255'
+  version '2020.2.2'
+  sha256 '056c6f05c49e494a2e7b7011e43aa993e545e26c3d20d948b26beffbd550da02'
 
   url "https://assets.geneious.com/installers/geneious/release/Geneious_Prime_mac64_#{version.dots_to_underscores}_with_jre.dmg"
   appcast 'https://www.geneious.com/download/'
