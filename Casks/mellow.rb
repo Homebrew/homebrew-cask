1d4ceeae960c44afd9b59de1dbdf2cd7c7553d8d
Brigitte Zmoelnig <brigitte@corecode.io>
2020-07-23 18:38:23 +0200

    Update mellow from 0.1.19 to 0.1.21

--- Casks/mellow.rb
+++ Casks/mellow.rb
@@ -1,6 +1,6 @@
 cask 'mellow' do
-  version '0.1.19'
-  sha256 '72d752b3abd8299f3e95df215f9659638c0bb3b0f57c1c8d4fa0e69a624a5106'
+  version '0.1.21'
+  sha256 '2f071d1f19323e6f1f2d727fc978a67b133004188a7f74dd87a4d6504ee10c68'
 
   url "https://github.com/mellow-io/mellow/releases/download/v#{version}/Mellow-#{version}.dmg"
   appcast 'https://github.com/mellow-io/mellow/releases.atom'
