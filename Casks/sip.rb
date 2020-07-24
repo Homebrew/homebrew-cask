ce5a2b4c15e18ef1704479c4de1783dec38103fd
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-23 19:20:42 -0700

    Update sip from 2.3 to 2.3.1 (#86486)

--- Casks/sip.rb
+++ Casks/sip.rb
@@ -3,8 +3,8 @@ cask 'sip' do
     version '1.2'
     sha256 '93569421eef761ccdd2784d73e5f201d29e5e22ad6814a7a169f459f460bf4ff'
   else
-    version '2.3'
-    sha256 '87581305377cb6c6687110272b1ba8a826aa04f0ecb2cc888973b9b52a6edced'
+    version '2.3.1'
+    sha256 'c1dc9b78a4577d20b2a1e03105f3c9840555f857b1a1fb1fa60ba98dcf4700f9'
   end
 
   url "https://sipapp.io/updates/v#{version.major}/sip-#{version}.zip"
