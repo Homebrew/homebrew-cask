3c50ca8fb3fb32a3e09a49c9307b46425c992b2d
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-24 15:18:59 -0700

    Update runjs from 1.10.0 to 1.10.1 (#86529)

--- Casks/runjs.rb
+++ Casks/runjs.rb
@@ -1,6 +1,6 @@
 cask 'runjs' do
-  version '1.10.0'
-  sha256 '89b1294b45fa071c606c5b8bfe4a837692c36e083ea01205d8c449048bbf3891'
+  version '1.10.1'
+  sha256 '05c6f9581c33adc73601b959e24d5a294cf62f245d2e3823a0c9972fec74c568'
 
   # github.com/lukehaas/runjs/ was verified as official when first introduced to the cask
   url "https://github.com/lukehaas/runjs/releases/download/v#{version}/RunJS-#{version}.dmg"
