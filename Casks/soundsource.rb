22011820b8e952a359eb00d73bb02892b5a9b8a2
Randall <ran.dall@icloud.com>
2020-07-23 18:34:32 -0700

    Update soundsource from 4.2.4 to 4.2.5

--- Casks/soundsource.rb
+++ Casks/soundsource.rb
@@ -1,6 +1,6 @@
 cask 'soundsource' do
-  version '4.2.4'
-  sha256 'd89891b1de33a71c05fbda95536284c97be175540eec93324365aba53164248e'
+  version '4.2.5'
+  sha256 'cc0999faa8abf62a02e077209e9d4f0dadffb7b10dd809a84b2d4ba59dd3955d'
 
   url 'https://rogueamoeba.com/soundsource/download/SoundSource.zip'
   appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.soundsource&system=10146&version=4000000'
