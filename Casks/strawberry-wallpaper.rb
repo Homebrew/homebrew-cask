027553f5a9dd59d5da7539b304813ec1d3b81a47
FormatToday <uKPeE93xC!F7Mv>
2020-07-24 22:48:38 +0800

    Add Strawberry Wallpaper.app v1.4.2

--- Casks/strawberry-wallpaper.rb
+++ Casks/strawberry-wallpaper.rb
@@ -1,12 +1,12 @@
 cask 'strawberry-wallpaper' do
-  version '1.4.1'
+  version '1.4.2'
   sha256 '733632a104fec8efc5024677332978d92d3375b00f877b50a95d0f450184f8e9'
 
+  # download url can be find in homepage
   url 'http://sw.taoacat.com/Strawberry%20Wallpaper-mac.dmg'
+  appcast 'http://sw.taoacat.com/version/latest-mac.yml'
   name 'Strawberry Wallpaper'
   homepage 'https://aitexiaoy.github.io/Strawberry-Wallpaper/'
-
-  # download url can be find in homepage
 
   app 'Strawberry Wallpaper.app'
 end
