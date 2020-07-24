ded220c722455e63bd2afb81276ba2bf973aeb0a
suschizu <brigitte@corecode.io>
2020-07-24 19:34:01 +0200

    Update tencent-lemon.rb

--- Casks/tencent-lemon.rb
+++ Casks/tencent-lemon.rb
@@ -1,5 +1,5 @@
 cask 'tencent-lemon' do
-  version '4.4.0'
+  version '4.5.0'
   sha256 '4e5f8ce0c737effd15aae7126d9dd378757f90a7e788b8e69284a7187e8db772'
 
   # pm.myapp.com/invc/xfspeed/qqpcmgr/ was verified as official when first introduced to the cask
