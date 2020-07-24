d2744ff7830256fe68a21c60fa52f82a268c1ec6
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-24 15:34:25 -0700

    Update outline from 3.24.0 to 3.24.3 (#86535)

--- Casks/outline.rb
+++ Casks/outline.rb
@@ -1,6 +1,6 @@
 cask 'outline' do
-  version '3.24.0'
-  sha256 'cb796869054f5fb6407737eeb9299c0110598e8f7e514a0a24fccd3c82647344'
+  version '3.24.3'
+  sha256 'cb9056b1440064e951a0c31eaa9540e73edacdd870e1b15d3678f054d329e69a'
 
   url "http://static.outline.ws/versions/Outline_#{version}.zip"
   appcast 'https://gorillized.s3.amazonaws.com/versions/update_channel.xml'
