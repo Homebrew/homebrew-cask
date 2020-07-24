8c9a82f7f815137c24a08eeaf8e2a6e8673049a6
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-23 18:49:36 -0700

    Update zotero from 5.0.88 to 5.0.89 (#86487)

--- Casks/zotero.rb
+++ Casks/zotero.rb
@@ -1,6 +1,6 @@
 cask 'zotero' do
-  version '5.0.88'
-  sha256 'a38ed6d887453e38cebea272fc2d0e21d93f97f11bfbca8fc75ab0b5c697f691'
+  version '5.0.89'
+  sha256 'bc56086c4305e138d2a7032b8ecacbc2c0bd8f3e4c3eb2c271eeb641ed381621'
 
   url "https://download.zotero.org/client/release/#{version}/Zotero-#{version}.dmg"
   appcast 'https://github.com/zotero/zotero/releases.atom'
