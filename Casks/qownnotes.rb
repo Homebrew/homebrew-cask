c74e172a982f9dd27f7936c484d0dfbb9007f9f6
Randall <ran.dall@icloud.com>
2020-07-24 15:08:17 -0700

    Update qownnotes from 20.7.4,b5811-165603 to 20.7.5

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,9 +1,9 @@
 cask 'qownnotes' do
-  version '20.7.4,b5811-165603'
-  sha256 '347dbc5e4290e96341490893b2811bbba278876221ff284585b772d59dc90f8f'
+  version '20.7.5'
+  sha256 'a426da8ee170663761831fc197d8a4a8ec0c523290426e87460bc608e30beada'
 
   # github.com/pbek/QOwnNotes/ was verified as official when first introduced to the cask
-  url "https://github.com/pbek/QOwnNotes/releases/download/macosx-#{version.after_comma}/QOwnNotes-#{version.before_comma}.dmg"
+  url "https://github.com/pbek/QOwnNotes/releases/download/v#{version}/QOwnNotes.dmg"
   appcast 'https://www.qownnotes.org/api/v1/last_release/QOwnNotes/macosx.json'
   name 'QOwnNotes'
   homepage 'https://www.qownnotes.org/'
