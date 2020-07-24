91dc12fe6f572d63a5b4ee155100e3fec595300d
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-24 15:48:55 -0700

    Update diskcatalogmaker from 8.2 to 8.2.0 (#86547)

--- Casks/diskcatalogmaker.rb
+++ Casks/diskcatalogmaker.rb
@@ -1,5 +1,5 @@
 cask 'diskcatalogmaker' do
-  version '8.2'
+  version '8.2.0'
   sha256 'a3c5434bd2e48ae7f538a0f2d5511afab2c5fb4c9382f8eb0c88166aa0b38b39'
 
   url 'https://download.diskcatalogmaker.com/zip/DiskCatalogMaker.zip'
