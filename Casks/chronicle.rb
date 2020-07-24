b860ce2a8dcf64df227d4333a2e63638fd0bc0ca
suschizu <brigitte@corecode.io>
2020-07-24 18:50:48 +0200

    Update chronicle.rb

--- Casks/chronicle.rb
+++ Casks/chronicle.rb
@@ -3,7 +3,7 @@ cask 'chronicle' do
   sha256 '51855fabab9a262231d96e4635a57a4a84c2361f001ce15bdd96a3ccadb00a91'
 
   url 'https://www.chronicleapp.com/static/downloads/chroniclepro.zip'
-  appcast 'http://www.littlefin.com/downloads/chronicle8.xml'
+  appcast 'https://chronicleapp.com/chronicle8.xml'
   name 'Chronicle'
   homepage 'https://chronicleapp.com/'
 
