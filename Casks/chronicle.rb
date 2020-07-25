da0c66b8e45784d45c8011b41483c707d395eae5
suschizu <brigitte@corecode.io>
2020-07-25 21:24:59 +0200

    Update appcast in chronicle (#86460)

--- Casks/chronicle.rb
+++ Casks/chronicle.rb
@@ -1,9 +1,9 @@
 cask 'chronicle' do
   version '9.7.1'
-  sha256 '7ed6b98e0b237aa7ce954931ac15cd392d64062587f79c0b25d1cb8d86124aa9'
+  sha256 '51855fabab9a262231d96e4635a57a4a84c2361f001ce15bdd96a3ccadb00a91'
 
   url 'https://www.chronicleapp.com/static/downloads/chroniclepro.zip'
-  appcast 'http://www.littlefin.com/downloads/chronicle8.xml'
+  appcast 'https://chronicleapp.com/chronicle8.xml'
   name 'Chronicle'
   homepage 'https://chronicleapp.com/'
 
