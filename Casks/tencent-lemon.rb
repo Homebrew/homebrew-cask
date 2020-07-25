01b103b8bed3192517628724d0324bd74d04de93
suschizu <brigitte@corecode.io>
2020-07-25 21:24:38 +0200

    Update tencent-lemon.rb from 4.4.0 to 4.5.0 (#86517)

--- Casks/tencent-lemon.rb
+++ Casks/tencent-lemon.rb
@@ -1,9 +1,9 @@
 cask 'tencent-lemon' do
-  version '4.4.0'
-  sha256 '4e5f8ce0c737effd15aae7126d9dd378757f90a7e788b8e69284a7187e8db772'
+  version '4.5.0'
+  sha256 '947fa22f1339b78153677494563ba92b0559f7f327bce01db6e06f1531ab82ef'
 
   # pm.myapp.com/invc/xfspeed/qqpcmgr/ was verified as official when first introduced to the cask
-  url "https://pm.myapp.com/invc/xfspeed/qqpcmgr/module_update/Lemon_#{version}.dmg"
+  url "https://pm.myapp.com/invc/xfspeed/qqpcmgr/module_update/Lemon-#{version}.dmg"
   appcast 'https://lemon.guanjia.qq.com/latest/package'
   name 'Tencent Lemon Cleaner'
   homepage 'https://mac.gj.qq.com/'
