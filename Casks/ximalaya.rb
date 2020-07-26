471421114a2704a6548fe74971e0ac736e837f2c
suschizu <brigitte@corecode.io>
2020-07-26 22:06:36 +0200

    Update ximalaya from 1.2.19_1592817383 to 1.2.19,1595577592 (#86592)

--- Casks/ximalaya.rb
+++ Casks/ximalaya.rb
@@ -1,9 +1,9 @@
 cask 'ximalaya' do
-  version '1.2.19_1592817383'
-  sha256 '7dc20cb44e2cfecf29c6e5d7e0aff8fd5b36a9d83441c1315dde8f0d3d5c3fb3'
+  version '1.2.19,1595577592'
+  sha256 '109ba925d97fc0206051f41e38f167b8e56a07b78b7e14268d633cf77494756a'
 
   # s1.xmcdn.com/ was verified as official when first introduced to the cask
-  url "https://s1.xmcdn.com/yx/ximalaya-pc-client/last/download/Ximalaya-#{version}.dmg"
+  url "http://s1.xmcdn.com/yx/ximalaya-pc/#{version.before_comma}/download/Ximalaya-#{version.before_comma}_#{version.after_comma}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.ximalaya.com/down/lite?client=mac'
   name 'ximalaya'
   name '喜马拉雅'
