64d527305eb55740e89848a2a52dfbfedb59fc40
suschizu <brigitte@corecode.io>
2020-07-26 22:14:44 +0200

    Update thorium from 1.3.0 to 1.4.0 (#86590)
    * Update thorium from 1.3.0 to 1.4.0
    
    * Update thorium.rb
    
    Co-authored-by: CoreCode <core-code@users.noreply.github.com>

--- Casks/thorium.rb
+++ Casks/thorium.rb
@@ -1,12 +1,12 @@
 cask 'thorium' do
-  version '1.3.0'
-  sha256 '058d0dddd3217cb72afc5131b621782e2f4116d3a7405f63f747c011b87b6601'
+  version '1.4.0'
+  sha256 'ec1e95c9e3ec0e6d75499b654e4f1e565ec11aa3148ab7753ba3977089dbfb86'
 
   # github.com/edrlab/thorium-reader was verified as official when first introduced to the cask
   url "https://github.com/edrlab/thorium-reader/releases/download/v#{version}/Thorium-#{version}.dmg"
   appcast 'https://github.com/edrlab/thorium-reader/releases'
   name 'Thorium Reader'
-  homepage 'https://edrlab.org/software/thorium-reader/'
+  homepage 'https://www.edrlab.org/software/thorium-reader/'
 
   app 'Thorium.app'
 
