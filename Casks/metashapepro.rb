dbe93f0975619c062428cc3fc13c4215b426a7b0
Randall <ran.dall@icloud.com>
2020-07-25 19:19:20 -0700

    Update metashapepro from 1.6.2 to 1.6.3

--- Casks/metashapepro.rb
+++ Casks/metashapepro.rb
@@ -1,6 +1,6 @@
 cask 'metashapepro' do
-  version '1.6.2'
-  sha256 'd0313cd8570da9cd331c1f073ec8fe22e57ce8da6257a603274e11b415a2dd22'
+  version '1.6.3'
+  sha256 '735e47721d21f1a94b9bb7dbf70f2dce745b000ddf276aee46596543bf33bb16'
 
   url "http://download.agisoft.com/metashape-pro_#{version.dots_to_underscores}.dmg"
   name 'Agisoft Metashape Professional Edition'
