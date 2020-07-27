0ead665ab77cdee7029129111ab7bfc708198683
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-26 18:49:04 -0700

    Update packetsender from 6.2.3 to 7.0.5 (#86603)

--- Casks/packetsender.rb
+++ Casks/packetsender.rb
@@ -1,6 +1,6 @@
 cask 'packetsender' do
-  version '6.2.3'
-  sha256 '4a886b351ef7d30906e20d05ea002a6ae2308c28dd1246957bcaa361e054ee2b'
+  version '7.0.5'
+  sha256 '30281225ee4e2baf3ca365123832c2931342aaf1011038268eab13ec73375fad'
 
   # github.com/dannagle/PacketSender/ was verified as official when first introduced to the cask
   url "https://github.com/dannagle/PacketSender/releases/download/v#{version}/PacketSender_v#{version}.dmg"
