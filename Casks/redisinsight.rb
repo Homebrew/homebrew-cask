a46ea09d4e5314e0fb084ba58f5b93904eb86be3
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:52 +0100

    redisinsight: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/redisinsight.rb
+++ Casks/redisinsight.rb
@@ -1,13 +1,13 @@
-cask 'redisinsight' do
-  version '1.6.0'
-  sha256 '3ff46465ebe7612af2dc5716bae3ab830e0c23839cb2ef37b84fd9f3b423daf3'
+cask "redisinsight" do
+  version "1.6.0"
+  sha256 "3ff46465ebe7612af2dc5716bae3ab830e0c23839cb2ef37b84fd9f3b423daf3"
 
   url "https://downloads.redisinsight.redislabs.com/#{version}/redisinsight-mac.dmg"
-  appcast 'https://docs.redislabs.com/latest/ri/release-notes/'
-  name 'RedisInsight'
-  homepage 'https://www.redislabs.com/redisinsight/'
+  appcast "https://docs.redislabs.com/latest/ri/release-notes/"
+  name "RedisInsight"
+  homepage "https://www.redislabs.com/redisinsight/"
 
-  app 'redisinsight.app'
+  app "redisinsight.app"
 
-  zap trash: '~/Library/Saved Application State/com.redislabs.redisinsight.savedState'
+  zap trash: "~/Library/Saved Application State/com.redislabs.redisinsight.savedState"
 end
