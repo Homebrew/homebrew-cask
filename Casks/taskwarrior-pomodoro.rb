c286945a4e2fe61b85f2dd0130c8d3aef8fa0e0c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:37:10 +0100

    taskwarrior-pomodoro: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/taskwarrior-pomodoro.rb
+++ Casks/taskwarrior-pomodoro.rb
@@ -1,11 +1,11 @@
-cask 'taskwarrior-pomodoro' do
-  version '1.8.0'
-  sha256 '07aad9949cf7ec5752d5da87333c52f07654b0c480b18779afaec0f5debb488a'
+cask "taskwarrior-pomodoro" do
+  version "1.8.0"
+  sha256 "07aad9949cf7ec5752d5da87333c52f07654b0c480b18779afaec0f5debb488a"
 
   url "https://github.com/coddingtonbear/taskwarrior-pomodoro/releases/download/v#{version}/taskwarrior-pomodoro-#{version}.dmg"
-  appcast 'https://github.com/coddingtonbear/taskwarrior-pomodoro/releases.atom'
-  name 'Taskwarrior-Pomodoro'
-  homepage 'https://github.com/coddingtonbear/taskwarrior-pomodoro'
+  appcast "https://github.com/coddingtonbear/taskwarrior-pomodoro/releases.atom"
+  name "Taskwarrior-Pomodoro"
+  homepage "https://github.com/coddingtonbear/taskwarrior-pomodoro"
 
-  app 'Taskwarrior Pomodoro.app'
+  app "Taskwarrior Pomodoro.app"
 end
