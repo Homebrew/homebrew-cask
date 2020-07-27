eca8a18d197f37db753b0e9c92b95fd8f71fce4f
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:35 +0100

    squadanimatorgaa: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/squadanimatorgaa.rb
+++ Casks/squadanimatorgaa.rb
@@ -1,10 +1,10 @@
-cask 'squadanimatorgaa' do
-  version '1.0.4'
-  sha256 '7e7c2f3eb637de07517a5b695ed20c9093fbab1a7f8ffb4af5167e19e60b2afa'
+cask "squadanimatorgaa" do
+  version "1.0.4"
+  sha256 "7e7c2f3eb637de07517a5b695ed20c9093fbab1a7f8ffb4af5167e19e60b2afa"
 
   url "https://www.squadanimator.com/downloads/SquadAnimatorGAA-OSX-#{version}.zip"
-  name 'SquadAnimator GAA'
-  homepage 'https://www.squadanimator.com/'
+  name "SquadAnimator GAA"
+  homepage "https://www.squadanimator.com/"
 
   app "SquadAnimatorGAA-#{version}.app"
 end
