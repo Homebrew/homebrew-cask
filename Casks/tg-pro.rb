ae8fb53ea6798896dbcd5da9a5e1cdfbca1dd1ca
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:37:22 +0100

    tg-pro: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/tg-pro.rb
+++ Casks/tg-pro.rb
@@ -1,20 +1,20 @@
-cask 'tg-pro' do
-  version '2.48'
-  sha256 '1dcf4c5a466b0accfe772af8575aa680ba6a9f7b68706c9f2f773f2104f6bf68'
+cask "tg-pro" do
+  version "2.48"
+  sha256 "1dcf4c5a466b0accfe772af8575aa680ba6a9f7b68706c9f2f773f2104f6bf68"
 
   url "https://www.tunabellysoftware.com/resources/TG%20Pro%20#{version}.dmg"
-  appcast 'https://www.tunabellysoftware.com/resources/sparkle/tgpro.xml'
-  name 'TG Pro'
-  homepage 'https://www.tunabellysoftware.com/tgpro/'
+  appcast "https://www.tunabellysoftware.com/resources/sparkle/tgpro.xml"
+  name "TG Pro"
+  homepage "https://www.tunabellysoftware.com/tgpro/"
 
-  app 'TG Pro.app'
+  app "TG Pro.app"
 
   zap trash: [
-               '/Library/LaunchDaemons/com.tunabellysoftware.TGFanHelper.plist',
-               '/Library/PrivilegedHelperTools/com.tunabellysoftware.TGFanHelper',
-               '~/Library/Application Support/TG Pro',
-               '~/Library/Caches/com.tunabellysoftware.tgpro',
-               '~/Library/Cookies/com.tunabellysoftware.tgpro.binarycookies',
-               '~/Library/Preferences/com.tunabellysoftware.tgpro.plist',
-             ]
+    "/Library/LaunchDaemons/com.tunabellysoftware.TGFanHelper.plist",
+    "/Library/PrivilegedHelperTools/com.tunabellysoftware.TGFanHelper",
+    "~/Library/Application Support/TG Pro",
+    "~/Library/Caches/com.tunabellysoftware.tgpro",
+    "~/Library/Cookies/com.tunabellysoftware.tgpro.binarycookies",
+    "~/Library/Preferences/com.tunabellysoftware.tgpro.plist",
+  ]
 end
