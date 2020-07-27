fa054f4a71daaa876fed7a73011ff1994a3dbcc4
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:27:28 +0100

    itrash: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/itrash.rb
+++ Casks/itrash.rb
@@ -1,10 +1,10 @@
-cask 'itrash' do
+cask "itrash" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.osxbytes.com/iTrash.dmg?forcedownload'
-  name 'iTrash'
-  homepage 'https://www.osxbytes.com/itrash.html'
+  url "https://www.osxbytes.com/iTrash.dmg?forcedownload"
+  name "iTrash"
+  homepage "https://www.osxbytes.com/itrash.html"
 
-  app 'iTrash.app'
+  app "iTrash.app"
 end
