8fceb399aadb2a4cc6455e62515d564f4e232c13
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:32 +0100

    second-life-viewer: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/second-life-viewer.rb
+++ Casks/second-life-viewer.rb
@@ -1,16 +1,16 @@
-cask 'second-life-viewer' do
-  version '6.4.5.544639'
-  sha256 'fb65ee7f6bdd9a40c52792dc6b9a08ccdc4cd2a380f4480e085633bd05e128c5'
+cask "second-life-viewer" do
+  version "6.4.5.544639"
+  sha256 "fb65ee7f6bdd9a40c52792dc6b9a08ccdc4cd2a380f4480e085633bd05e128c5"
 
   url "http://download.cloud.secondlife.com/Viewer_#{version.major}/Second_Life_#{version.dots_to_underscores}_x86_64.dmg"
-  appcast 'https://secondlife.com/support/downloads/'
-  name 'Linden Lab Second Life Viewer'
-  homepage 'https://secondlife.com/'
+  appcast "https://secondlife.com/support/downloads/"
+  name "Linden Lab Second Life Viewer"
+  homepage "https://secondlife.com/"
 
-  app 'Second Life Viewer.app'
+  app "Second Life Viewer.app"
 
   zap trash: [
-               '~/Library/Application Support/SecondLife',
-               '~/Library/Caches/SecondLife',
-             ]
+    "~/Library/Application Support/SecondLife",
+    "~/Library/Caches/SecondLife",
+  ]
 end
