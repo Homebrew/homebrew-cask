84f9a573ff0f1e7a8d40a08562a8e94ea8532c47
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:11 +0100

    logicsniffer: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/logicsniffer.rb
+++ Casks/logicsniffer.rb
@@ -1,11 +1,11 @@
-cask 'logicsniffer' do
-  version '0.9.7.2'
-  sha256 'fa38ea4d3a5a6dac2cddd66c860e75e60b6d4162e81e1c32d7adabc17056c99a'
+cask "logicsniffer" do
+  version "0.9.7.2"
+  sha256 "fa38ea4d3a5a6dac2cddd66c860e75e60b6d4162e81e1c32d7adabc17056c99a"
 
   url "https://www.lxtreme.nl/ols/ols-#{version}-full.dmg"
-  appcast 'https://www.lxtreme.nl/ols/'
-  name 'Logic Sniffer'
-  homepage 'https://www.lxtreme.nl/ols/'
+  appcast "https://www.lxtreme.nl/ols/"
+  name "Logic Sniffer"
+  homepage "https://www.lxtreme.nl/ols/"
 
-  app 'LogicSniffer.app'
+  app "LogicSniffer.app"
 end
