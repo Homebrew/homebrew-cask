f99b4666dc12c8db204ac93c8fd21aa077d25b05
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:11 +0100

    routeconverter: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/routeconverter.rb
+++ Casks/routeconverter.rb
@@ -1,14 +1,14 @@
-cask 'routeconverter' do
-  version '2.29.116'
-  sha256 '2f0df34831e8ee1a864070c5d11fe4b596ea32ed5a8acf1728220fc64547db51'
+cask "routeconverter" do
+  version "2.29.116"
+  sha256 "2f0df34831e8ee1a864070c5d11fe4b596ea32ed5a8acf1728220fc64547db51"
 
-  url 'https://static.routeconverter.com/download/RouteConverterMac.app.zip'
-  appcast 'https://static.routeconverter.com/download/previous-releases/',
+  url "https://static.routeconverter.com/download/RouteConverterMac.app.zip"
+  appcast "https://static.routeconverter.com/download/previous-releases/",
           must_contain: version.major_minor
-  name 'RouteConverter'
-  homepage 'https://www.routeconverter.com/'
+  name "RouteConverter"
+  homepage "https://www.routeconverter.com/"
 
   auto_updates true
 
-  app 'RouteConverterMacOpenSource.app'
+  app "RouteConverterMacOpenSource.app"
 end
