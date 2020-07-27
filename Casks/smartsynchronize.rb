9ac2235595c2c95dac5075630a136dd571e3c0de
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:09 +0100

    smartsynchronize: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/smartsynchronize.rb
+++ Casks/smartsynchronize.rb
@@ -1,13 +1,13 @@
-cask 'smartsynchronize' do
-  version '4.1.0'
-  sha256 '207f285df704db88f7a7338dfd714ffc283c498010b03205cd6ac21eab9004bf'
+cask "smartsynchronize" do
+  version "4.1.0"
+  sha256 "207f285df704db88f7a7338dfd714ffc283c498010b03205cd6ac21eab9004bf"
 
   url "https://www.syntevo.com/downloads/smartsynchronize/smartsynchronize-macosx-#{version.dots_to_underscores}.dmg"
-  appcast 'https://www.syntevo.com/smartsynchronize/changelog.txt',
-          must_contain: version.chomp('.0')
-  name 'SmartSynchronize'
-  homepage 'https://www.syntevo.com/smartsynchronize/'
+  appcast "https://www.syntevo.com/smartsynchronize/changelog.txt",
+          must_contain: version.chomp(".0")
+  name "SmartSynchronize"
+  homepage "https://www.syntevo.com/smartsynchronize/"
 
-  app 'SmartSynchronize.app'
+  app "SmartSynchronize.app"
   binary "#{appdir}/SmartSynchronize.app/Contents/MacOS/SmartSynchronize"
 end
