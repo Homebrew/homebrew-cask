2decf202da59bd04c5ce660404fd0b5bbf67a015
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:37:06 +0100

    tableau: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/tableau.rb
+++ Casks/tableau.rb
@@ -1,27 +1,27 @@
-cask 'tableau' do
-  version '2020.2.2'
-  sha256 '532a5dd6692557c780330f1f3cc18fe80e946f632caa5b9daeea3f8b87ae59a0'
+cask "tableau" do
+  version "2020.2.2"
+  sha256 "532a5dd6692557c780330f1f3cc18fe80e946f632caa5b9daeea3f8b87ae59a0"
 
   url "https://downloads.tableau.com/tssoftware/TableauDesktop-#{version.dots_to_hyphens}.dmg"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.tableau.com/downloads/desktop/mac',
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.tableau.com/downloads/desktop/mac",
           must_contain: version.dots_to_hyphens
-  name 'Tableau Desktop'
-  homepage 'https://www.tableau.com/products/desktop/download'
+  name "Tableau Desktop"
+  homepage "https://www.tableau.com/products/desktop/download"
 
-  depends_on macos: '>= :yosemite'
+  depends_on macos: ">= :yosemite"
 
-  pkg 'Tableau Desktop.pkg'
+  pkg "Tableau Desktop.pkg"
 
   uninstall pkgutil: [
-                       'com.amazon.redshiftodbc',
-                       'simba.sparkodbc',
-                       'com.simba.sparkodbc',
-                       'com.simba.sqlserverodbc',
-                       'com.tableausoftware.Desktop.app',
-                       'com.tableausoftware.DesktopShortcut',
-                       'com.tableausoftware.FLEXNet.11.*',
-                       'com.tableausoftware.mysql',
-                       'com.tableausoftware.oracle',
-                       'com.tableausoftware.postgresql',
-                     ]
+    "com.amazon.redshiftodbc",
+    "simba.sparkodbc",
+    "com.simba.sparkodbc",
+    "com.simba.sqlserverodbc",
+    "com.tableausoftware.Desktop.app",
+    "com.tableausoftware.DesktopShortcut",
+    "com.tableausoftware.FLEXNet.11.*",
+    "com.tableausoftware.mysql",
+    "com.tableausoftware.oracle",
+    "com.tableausoftware.postgresql",
+  ]
 end
