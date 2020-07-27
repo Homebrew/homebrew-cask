26fbfd12378233880774ae1296d0d086a135adf4
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:38:09 +0100

    upwork: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/upwork.rb
+++ Casks/upwork.rb
@@ -1,20 +1,20 @@
-cask 'upwork' do
-  version '5.4.2.8,17caa201e7ae4660'
-  sha256 '8c7638184de6fda7278e02f1931363c0063e475fb7703d8d547255ea60b519c7'
+cask "upwork" do
+  version "5.4.2.8,17caa201e7ae4660"
+  sha256 "8c7638184de6fda7278e02f1931363c0063e475fb7703d8d547255ea60b519c7"
 
   url "https://updates-desktopapp.upwork.com/binaries/v#{version.before_comma.dots_to_underscores}_#{version.after_comma}/Upwork.dmg"
-  appcast 'https://www.macupdater.net/cgi-bin/extract_text/extract_text_split_easy.cgi?url=https://updates-desktopapp.upwork.com/binaries/versions-mac.json&splitter_1=Beta&index_1=0'
-  name 'Upwork'
-  homepage 'https://www.upwork.com/'
+  appcast "https://www.macupdater.net/cgi-bin/extract_text/extract_text_split_easy.cgi?url=https://updates-desktopapp.upwork.com/binaries/versions-mac.json&splitter_1=Beta&index_1=0"
+  name "Upwork"
+  homepage "https://www.upwork.com/"
 
-  app 'Upwork.app'
+  app "Upwork.app"
 
   zap trash: [
-               '~/Library/Application Support/CrashReporter/Upwork_*',
-               '~/Library/Application Support/Upwork',
-               '~/Library/Caches/Upwork',
-               '~/Library/Logs/Upwork',
-               '~/Library/Preferences/com.upwork.Upwork.plist',
-               '~/Library/Saved Application State/com.upwork.upwork.savedState',
-             ]
+    "~/Library/Application Support/CrashReporter/Upwork_*",
+    "~/Library/Application Support/Upwork",
+    "~/Library/Caches/Upwork",
+    "~/Library/Logs/Upwork",
+    "~/Library/Preferences/com.upwork.Upwork.plist",
+    "~/Library/Saved Application State/com.upwork.upwork.savedState",
+  ]
 end
