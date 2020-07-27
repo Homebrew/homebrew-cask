58795a74f6f85f317ac20626004456cc4ff49b1e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:34 +0100

    sqlpro-studio: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/sqlpro-studio.rb
+++ Casks/sqlpro-studio.rb
@@ -1,17 +1,17 @@
-cask 'sqlpro-studio' do
-  version '2020.61'
-  sha256 'cf4bbe1d30e01df87f8ca45a1aade64aa7849e8d68075497c474ba17a2f2b508'
+cask "sqlpro-studio" do
+  version "2020.61"
+  sha256 "cf4bbe1d30e01df87f8ca45a1aade64aa7849e8d68075497c474ba17a2f2b508"
 
   # d3fwkemdw8spx3.cloudfront.net/studio/ was verified as official when first introduced to the cask
   url "https://d3fwkemdw8spx3.cloudfront.net/studio/SQLProStudio.#{version}.app.zip"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?user_agent=Macintosh&url=https://www.sqlprostudio.com/download.php'
-  name 'SQLPro Studio'
-  homepage 'https://www.sqlprostudio.com/'
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?user_agent=Macintosh&url=https://www.sqlprostudio.com/download.php"
+  name "SQLPro Studio"
+  homepage "https://www.sqlprostudio.com/"
 
-  app 'SQLPro Studio.app'
+  app "SQLPro Studio.app"
 
   zap trash: [
-               '~/Library/Containers/com.hankinsoft.osx.sqlprostudio',
-               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.hankinsoft.osx.sqlprostudio.sfl*',
-             ]
+    "~/Library/Containers/com.hankinsoft.osx.sqlprostudio",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.hankinsoft.osx.sqlprostudio.sfl*",
+  ]
 end
