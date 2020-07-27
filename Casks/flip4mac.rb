f58b2745b76afeb90942dea6cfa3ffd8316e4c7c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:24:13 +0100

    flip4mac: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/flip4mac.rb
+++ Casks/flip4mac.rb
@@ -1,12 +1,12 @@
-cask 'flip4mac' do
-  version '3.3.8'
-  sha256 'dde035ecd07f14224dcaa9ed70c873e18544bbff5ce79bb5b1bbbb05ff9f61c0'
+cask "flip4mac" do
+  version "3.3.8"
+  sha256 "dde035ecd07f14224dcaa9ed70c873e18544bbff5ce79bb5b1bbbb05ff9f61c0"
 
-  url "https://www.telestream.net/download-files/flip4mac/#{version.sub(%r{^(\d+)\.(\d+).*$}, '\1-\2')}/Flip4Mac-#{version}.dmg"
-  name 'Flip4Mac'
-  homepage 'https://www.telestream.net/flip4mac/'
+  url "https://www.telestream.net/download-files/flip4mac/#{version.sub(/^(\d+)\.(\d+).*$/, '\1-\2')}/Flip4Mac-#{version}.dmg"
+  name "Flip4Mac"
+  homepage "https://www.telestream.net/flip4mac/"
 
-  pkg 'Flip4Mac.pkg'
+  pkg "Flip4Mac.pkg"
 
-  uninstall pkgutil: 'net.telestream.Flip4Mac'
+  uninstall pkgutil: "net.telestream.Flip4Mac"
 end
