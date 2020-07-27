a168db5742a5f25a70779d731fda19eb8643737f
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:19 +0100

    qlmarkdown: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/qlmarkdown.rb
+++ Casks/qlmarkdown.rb
@@ -1,11 +1,11 @@
-cask 'qlmarkdown' do
-  version '1.3.6'
-  sha256 '810853c000dd5c3e18978070abb7f595ad52ddfa568fccb428d28b513d1810ab'
+cask "qlmarkdown" do
+  version "1.3.6"
+  sha256 "810853c000dd5c3e18978070abb7f595ad52ddfa568fccb428d28b513d1810ab"
 
   url "https://github.com/toland/qlmarkdown/releases/download/v#{version}/QLMarkdown.qlgenerator.zip"
-  appcast 'https://github.com/toland/qlmarkdown/releases.atom'
-  name 'QLMarkdown'
-  homepage 'https://github.com/toland/qlmarkdown'
+  appcast "https://github.com/toland/qlmarkdown/releases.atom"
+  name "QLMarkdown"
+  homepage "https://github.com/toland/qlmarkdown"
 
-  qlplugin 'QLMarkdown.qlgenerator'
+  qlplugin "QLMarkdown.qlgenerator"
 end
