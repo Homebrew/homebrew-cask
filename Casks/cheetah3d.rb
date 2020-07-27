7cba7e5fed43425b90d490150bb647d90043ed7a
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:19:49 +0100

    cheetah3d: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/cheetah3d.rb
+++ Casks/cheetah3d.rb
@@ -1,10 +1,10 @@
-cask 'cheetah3d' do
+cask "cheetah3d" do
   version :latest
   sha256 :no_check
 
-  url 'https://cheetah3d.com/download/Cheetah3D.dmg'
-  name 'Cheetah3D'
-  homepage 'https://www.cheetah3d.com/'
+  url "https://cheetah3d.com/download/Cheetah3D.dmg"
+  name "Cheetah3D"
+  homepage "https://www.cheetah3d.com/"
 
-  app 'Cheetah3D.app'
+  app "Cheetah3D.app"
 end
