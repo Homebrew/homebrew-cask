89483277c831f505d09482091979114b87c5fed2
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:48 +0100

    subgit: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/subgit.rb
+++ Casks/subgit.rb
@@ -1,14 +1,14 @@
-cask 'subgit' do
-  version '3.3.9'
-  sha256 '65039f787790e9eff7ee5802c2d6fb884dfabb32d9ab60d6882cdf5afd148d37'
+cask "subgit" do
+  version "3.3.9"
+  sha256 "65039f787790e9eff7ee5802c2d6fb884dfabb32d9ab60d6882cdf5afd148d37"
 
   url "https://subgit.com/download/subgit-#{version}.zip"
-  name 'SubGit'
-  homepage 'https://subgit.com/'
+  name "SubGit"
+  homepage "https://subgit.com/"
 
   binary "subgit-#{version}/bin/subgit"
 
   caveats do
-    depends_on_java '7+'
+    depends_on_java "7+"
   end
 end
