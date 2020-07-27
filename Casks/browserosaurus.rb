5be0cb40b2a6846eba215983875dc06abde9c326
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:19:01 +0100

    browserosaurus: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/browserosaurus.rb
+++ Casks/browserosaurus.rb
@@ -1,14 +1,14 @@
-cask 'browserosaurus' do
-  version '10.11.2'
-  sha256 '216e56b0cfc84c812973078345bea5accc1067a237c63a9065c5386ecdfb945c'
+cask "browserosaurus" do
+  version "10.11.2"
+  sha256 "216e56b0cfc84c812973078345bea5accc1067a237c63a9065c5386ecdfb945c"
 
   # github.com/will-stone/browserosaurus/ was verified as official when first introduced to the cask
   url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-#{version}.dmg"
-  appcast 'https://github.com/will-stone/browserosaurus/releases.atom'
-  name 'Browserosaurus'
-  homepage 'https://will-stone.github.io/browserosaurus/'
+  appcast "https://github.com/will-stone/browserosaurus/releases.atom"
+  name "Browserosaurus"
+  homepage "https://will-stone.github.io/browserosaurus/"
 
   auto_updates true
 
-  app 'Browserosaurus.app'
+  app "Browserosaurus.app"
 end
