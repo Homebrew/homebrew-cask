36568a14ab20b00caa883a6409aeaa99179837b9
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:30 +0100

    questrade-iq-edge: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/questrade-iq-edge.rb
+++ Casks/questrade-iq-edge.rb
@@ -1,10 +1,10 @@
-cask 'questrade-iq-edge' do
+cask "questrade-iq-edge" do
   version :latest
   sha256 :no_check
 
-  url 'https://media.questrade.com/iq_downloads/QuestradeIQEdge.dmg'
-  name 'Questrade IQ Edge'
-  homepage 'https://www.questrade.com/'
+  url "https://media.questrade.com/iq_downloads/QuestradeIQEdge.dmg"
+  name "Questrade IQ Edge"
+  homepage "https://www.questrade.com/"
 
-  app 'Questrade IQ Edge.app'
+  app "Questrade IQ Edge.app"
 end
