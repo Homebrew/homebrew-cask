ce5e8ee23854daeacc3908161b95d038317f6ea8
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:13:08 +0100

    1password-cli: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/1password-cli.rb
+++ Casks/1password-cli.rb
@@ -1,16 +1,16 @@
-cask '1password-cli' do
-  version '1.3.0'
-  sha256 '821d46c78d5a995423fce4df746f39bbd85371dcd4708b82760a23e8395508e0'
+cask "1password-cli" do
+  version "1.3.0"
+  sha256 "821d46c78d5a995423fce4df746f39bbd85371dcd4708b82760a23e8395508e0"
 
   # cache.agilebits.com/dist/1P/op/pkg/ was verified as official when first introduced to the cask
   url "https://cache.agilebits.com/dist/1P/op/pkg/v#{version}/op_darwin_amd64_v#{version}.pkg"
-  appcast 'https://app-updates.agilebits.com/product_history/CLI'
-  name '1Password CLI'
-  homepage 'https://support.1password.com/command-line/'
+  appcast "https://app-updates.agilebits.com/product_history/CLI"
+  name "1Password CLI"
+  homepage "https://support.1password.com/command-line/"
 
   pkg "op_darwin_amd64_v#{version}.pkg"
 
-  uninstall pkgutil: 'com.1password.op'
+  uninstall pkgutil: "com.1password.op"
 
-  zap trash: '~/.op'
+  zap trash: "~/.op"
 end
