407c2bce1bce73a70f0fafedfdabe74e53c1adf3
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:41 +0100

    popmaker: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/popmaker.rb
+++ Casks/popmaker.rb
@@ -1,10 +1,10 @@
-cask 'popmaker' do
-  version '0.3'
-  sha256 '0bce402882c2ecbe0ff9eaeb0f225a0964b5a5ec4ae4cddf2a0d1aa63b0561dc'
+cask "popmaker" do
+  version "0.3"
+  sha256 "0bce402882c2ecbe0ff9eaeb0f225a0964b5a5ec4ae4cddf2a0d1aa63b0561dc"
 
   url "https://brettterpstra.com/downloads/PopMaker#{version}.zip"
-  name 'PopMaker'
-  homepage 'https://brettterpstra.com/2014/05/12/popmaker-popclip-extension-generator/'
+  name "PopMaker"
+  homepage "https://brettterpstra.com/2014/05/12/popmaker-popclip-extension-generator/"
 
-  app 'PopMaker.app'
+  app "PopMaker.app"
 end
