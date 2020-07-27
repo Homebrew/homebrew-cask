d8df86db067ceac94064b4c718289055e5536235
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:24:28 +0100

    fontexplorer-x-pro: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/fontexplorer-x-pro.rb
+++ Casks/fontexplorer-x-pro.rb
@@ -1,31 +1,31 @@
-cask 'fontexplorer-x-pro' do
-  version '7.1.0'
-  sha256 '0c43cba22c42c35e8cfd9ad0f2c63718c9f121f58c1d09d0fcc995822fe6ca4c'
+cask "fontexplorer-x-pro" do
+  version "7.1.0"
+  sha256 "0c43cba22c42c35e8cfd9ad0f2c63718c9f121f58c1d09d0fcc995822fe6ca4c"
 
   url "https://fast.fontexplorerx.com/FontExplorerXPro#{version.no_dots}.dmg"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect_curl.cgi?url=https://fex.linotype.com/download/mac/FontExplorerXPro.dmg',
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect_curl.cgi?url=https://fex.linotype.com/download/mac/FontExplorerXPro.dmg",
           must_contain: version.no_dots
-  name 'FontExplorer X Pro'
-  homepage 'https://www.fontexplorerx.com/'
+  name "FontExplorer X Pro"
+  homepage "https://www.fontexplorerx.com/"
 
-  app 'FontExplorer X Pro.app'
+  app "FontExplorer X Pro.app"
 
-  uninstall delete:    '/Library/PrivilegedHelperTools/com.linotype.FontExplorerX.securityhelper',
+  uninstall delete:    "/Library/PrivilegedHelperTools/com.linotype.FontExplorerX.securityhelper",
             launchctl: [
-                         'com.linotype.FontExplorerX.securityhelper',
-                         'com.linotype.FontFolderProtector',
-                       ],
-            quit:      'com.linotype.FontExplorerX'
+              "com.linotype.FontExplorerX.securityhelper",
+              "com.linotype.FontFolderProtector",
+            ],
+            quit:      "com.linotype.FontExplorerX"
 
   zap trash: [
-               '~/Library/Application Support/Linotype/FontExplorer X',
-               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.linotype.fontexplorerx.sfl*',
-               '~/Library/Caches/com.linotype.FontExplorerX',
-               '~/Library/Caches/Metadata/FontExplorer X',
-               '~/Library/Cookies/com.linotype.FontExplorerX.binarycookies',
-               '~/Library/Preferences/com.linotype.FontExplorerX.plist',
-               '~/Library/Saved Application State/com.linotype.FontExplorerX.savedState',
-               '/Users/Shared/.FontExplorer X Server',
-               '/Users/Shared/FontExplorer X Server',
-             ]
+    "~/Library/Application Support/Linotype/FontExplorer X",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.linotype.fontexplorerx.sfl*",
+    "~/Library/Caches/com.linotype.FontExplorerX",
+    "~/Library/Caches/Metadata/FontExplorer X",
+    "~/Library/Cookies/com.linotype.FontExplorerX.binarycookies",
+    "~/Library/Preferences/com.linotype.FontExplorerX.plist",
+    "~/Library/Saved Application State/com.linotype.FontExplorerX.savedState",
+    "/Users/Shared/.FontExplorer X Server",
+    "/Users/Shared/FontExplorer X Server",
+  ]
 end
