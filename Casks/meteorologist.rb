ed4d5356805f9c828fc11a693778d98b6535c6da
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:30:24 +0100

    meteorologist: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/meteorologist.rb
+++ Casks/meteorologist.rb
@@ -1,12 +1,12 @@
-cask 'meteorologist' do
-  version '3.4.2'
-  sha256 '4c1276f40f8f4d05dfb870f35254e3e52ec8ce3f6db93ad7f8089ea5c00535a1'
+cask "meteorologist" do
+  version "3.4.2"
+  sha256 "4c1276f40f8f4d05dfb870f35254e3e52ec8ce3f6db93ad7f8089ea5c00535a1"
 
   # downloads.sourceforge.net/heat-meteo/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/heat-meteo/Meteorologist-#{version}.dmg"
-  appcast 'https://sourceforge.net/projects/heat-meteo/rss'
-  name 'Meteorologist'
-  homepage 'https://heat-meteo.sourceforge.io/'
+  appcast "https://sourceforge.net/projects/heat-meteo/rss"
+  name "Meteorologist"
+  homepage "https://heat-meteo.sourceforge.io/"
 
-  app 'Meteorologist.app'
+  app "Meteorologist.app"
 end
