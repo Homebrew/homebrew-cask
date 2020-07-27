69537180861b18b0d76d1be5013f8017063534d7
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:56 +0100

    simplefloatingclock: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/simplefloatingclock.rb
+++ Casks/simplefloatingclock.rb
@@ -1,10 +1,10 @@
-cask 'simplefloatingclock' do
+cask "simplefloatingclock" do
   version :latest
   sha256 :no_check
 
-  url 'http://www.splook.com/Software/distrib/sfc.zip'
-  name 'Simple Floating Clock'
-  homepage 'http://www.splook.com/Software/Simple_Floating_Clock.html'
+  url "http://www.splook.com/Software/distrib/sfc.zip"
+  name "Simple Floating Clock"
+  homepage "http://www.splook.com/Software/Simple_Floating_Clock.html"
 
-  app 'Simple Floating Clock/SimpleFloatingClock.app'
+  app "Simple Floating Clock/SimpleFloatingClock.app"
 end
