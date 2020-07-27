c9b6b0d629e116029c433a4117dca12ff6c06de6
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:31:11 +0100

    mqtt-explorer: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/mqtt-explorer.rb
+++ Casks/mqtt-explorer.rb
@@ -1,20 +1,20 @@
-cask 'mqtt-explorer' do
-  version '0.3.5'
-  sha256 '4322a9127c3ce9025d33afa3ff91e76e8873fff260493a112eadc4769c027778'
+cask "mqtt-explorer" do
+  version "0.3.5"
+  sha256 "4322a9127c3ce9025d33afa3ff91e76e8873fff260493a112eadc4769c027778"
 
   # github.com/thomasnordquist/MQTT-Explorer/ was verified as official when first introduced to the cask
   url "https://github.com/thomasnordquist/MQTT-Explorer/releases/download/v#{version}/MQTT-Explorer-#{version}-mac.zip"
-  appcast 'https://github.com/thomasnordquist/MQTT-Explorer/releases.atom'
-  name 'MQTT Explorer'
-  homepage 'https://mqtt-explorer.com/'
+  appcast "https://github.com/thomasnordquist/MQTT-Explorer/releases.atom"
+  name "MQTT Explorer"
+  homepage "https://mqtt-explorer.com/"
 
-  app 'MQTT Explorer.app'
+  app "MQTT Explorer.app"
 
-  uninstall quit: 'de.t7n.apps.mqtt-explorer'
+  uninstall quit: "de.t7n.apps.mqtt-explorer"
 
   zap trash: [
-               '~/Library/Containers/de.t7n.apps.mqtt-explorer',
-               '~/Library/Group Containers/*.de.t7n.apps.mqtt-explorer',
-               '~/Library/Application Scripts/de.t7n.apps.mqtt-explorer',
-             ]
+    "~/Library/Containers/de.t7n.apps.mqtt-explorer",
+    "~/Library/Group Containers/*.de.t7n.apps.mqtt-explorer",
+    "~/Library/Application Scripts/de.t7n.apps.mqtt-explorer",
+  ]
 end
