f3e87cfe5cd3145169bb1ef24c6a9eb246b32a85
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:30:53 +0100

    molotov: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/molotov.rb
+++ Casks/molotov.rb
@@ -1,11 +1,11 @@
-cask 'molotov' do
-  version '4.3.0'
-  sha256 '38722366cc0208622a5be110c741da723c17628a9cd9b637861b93f262b8e1a0'
+cask "molotov" do
+  version "4.3.0"
+  sha256 "38722366cc0208622a5be110c741da723c17628a9cd9b637861b93f262b8e1a0"
 
   url "https://desktop-auto-upgrade.molotov.tv/mac/Molotov-v#{version}.dmg"
-  appcast 'https://desktop-auto-upgrade.molotov.tv/mac/manifest.json'
-  name 'Molotov'
-  homepage 'https://www.molotov.tv/'
+  appcast "https://desktop-auto-upgrade.molotov.tv/mac/manifest.json"
+  name "Molotov"
+  homepage "https://www.molotov.tv/"
 
-  app 'Molotov.app'
+  app "Molotov.app"
 end
