d6a3c6710b2455d920d050761892c1543dc67677
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:08 +0100

    rocket: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/rocket.rb
+++ Casks/rocket.rb
@@ -1,22 +1,22 @@
-cask 'rocket' do
-  version '1.7.4,69'
-  sha256 'af589098cad82a5e84a9212410d4b98cd2904ad38472c8feb5291374aca33971'
+cask "rocket" do
+  version "1.7.4,69"
+  sha256 "af589098cad82a5e84a9212410d4b98cd2904ad38472c8feb5291374aca33971"
 
   url "https://macrelease.matthewpalmer.net/distribution/appcasts/Rocket-#{version.after_comma}.dmg"
-  appcast 'https://macrelease.matthewpalmer.net/distribution/appcasts/rocket.xml'
-  name 'Rocket'
-  homepage 'https://matthewpalmer.net/rocket/'
+  appcast "https://macrelease.matthewpalmer.net/distribution/appcasts/rocket.xml"
+  name "Rocket"
+  homepage "https://matthewpalmer.net/rocket/"
 
   auto_updates true
 
-  app 'Rocket.app'
+  app "Rocket.app"
 
-  uninstall quit: 'net.matthewpalmer.Rocket'
+  uninstall quit: "net.matthewpalmer.Rocket"
 
   zap trash: [
-               '/Users/Shared/Rocket',
-               '~/Library/Application Support/Rocket',
-               '~/Library/Caches/net.matthewpalmer.Rocket',
-               '~/Library/Preferences/net.matthewpalmer.Rocket.plist',
-             ]
+    "/Users/Shared/Rocket",
+    "~/Library/Application Support/Rocket",
+    "~/Library/Caches/net.matthewpalmer.Rocket",
+    "~/Library/Preferences/net.matthewpalmer.Rocket.plist",
+  ]
 end
