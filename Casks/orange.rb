e3db5facd45ab6227eda2666b39a16c46311d129
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:32:39 +0100

    orange: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/orange.rb
+++ Casks/orange.rb
@@ -1,11 +1,11 @@
-cask 'orange' do
-  version '3.24.1'
-  sha256 '4f31193260c4f2d9f0a495d664e8fbc7438aebfadc7e83f1d950c1c672873887'
+cask "orange" do
+  version "3.24.1"
+  sha256 "4f31193260c4f2d9f0a495d664e8fbc7438aebfadc7e83f1d950c1c672873887"
 
   url "https://download.biolab.si/download/files/Orange#{version.major}-#{version}.dmg"
-  appcast 'https://github.com/biolab/orange3/releases.atom'
-  name 'Orange'
-  homepage 'https://orange.biolab.si/'
+  appcast "https://github.com/biolab/orange3/releases.atom"
+  name "Orange"
+  homepage "https://orange.biolab.si/"
 
   app "Orange#{version.major}.app"
 end
