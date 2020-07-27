32b325f962bb5f4c0b9a50c287bca74e3335cb15
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:38:25 +0100

    voicemac: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/voicemac.rb
+++ Casks/voicemac.rb
@@ -1,15 +1,15 @@
-cask 'voicemac' do
-  version '0.4.1'
-  sha256 '527b3356c11d1ef94787445829769983e9ac5c99e9cea724ad829148c78bcd7a'
+cask "voicemac" do
+  version "0.4.1"
+  sha256 "527b3356c11d1ef94787445829769983e9ac5c99e9cea724ad829148c78bcd7a"
 
-  url 'https://download.mrgeckosmedia.com/VoiceMac.zip'
-  appcast 'https://mrgeckosmedia.com/applications/appcast/VoiceMac'
-  name 'VoiceMac'
-  homepage 'https://mrgeckosmedia.com/applications/info/VoiceMac'
+  url "https://download.mrgeckosmedia.com/VoiceMac.zip"
+  appcast "https://mrgeckosmedia.com/applications/appcast/VoiceMac"
+  name "VoiceMac"
+  homepage "https://mrgeckosmedia.com/applications/info/VoiceMac"
 
-  app 'VoiceMac/VoiceMac.app'
+  app "VoiceMac/VoiceMac.app"
 
   postflight do
-    set_permissions "#{appdir}/VoiceMac.app/Contents/Info.plist", 'a+r'
+    set_permissions "#{appdir}/VoiceMac.app/Contents/Info.plist", "a+r"
   end
 end
