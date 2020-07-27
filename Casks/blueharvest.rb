ab8a1990d0ed5908c0c7805bdd966ae2774e0c98
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:18:26 +0100

    blueharvest: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/blueharvest.rb
+++ Casks/blueharvest.rb
@@ -1,20 +1,20 @@
-cask 'blueharvest' do
-  version '7.2.2'
-  sha256 'dcff42cb1e56c512f4c73ffc4a1cfaea0ea49523c7df4ff41b5375fbc00d1ad4'
+cask "blueharvest" do
+  version "7.2.2"
+  sha256 "dcff42cb1e56c512f4c73ffc4a1cfaea0ea49523c7df4ff41b5375fbc00d1ad4"
 
   url "https://zeroonetwenty.com/blueharvest/downloads/BlueHarvest#{version.no_dots}.dmg"
-  appcast 'https://zeroonetwenty.com/blueharvest/release-notes.html'
-  name 'BlueHarvest'
-  homepage 'https://zeroonetwenty.com/blueharvest/'
+  appcast "https://zeroonetwenty.com/blueharvest/release-notes.html"
+  name "BlueHarvest"
+  homepage "https://zeroonetwenty.com/blueharvest/"
 
-  app 'BlueHarvest.app'
+  app "BlueHarvest.app"
 
   uninstall delete:    "/Library/PrivilegedHelperTools/com.zeroonetwenty.BlueHarvestHelper#{version.major}",
             launchctl: "com.zeroonetwenty.BlueHarvestHelper#{version.major}",
-            quit:      'com.zeroonetwenty.BlueHarvest5'
+            quit:      "com.zeroonetwenty.BlueHarvest5"
 
   zap trash: [
-               '~/Library/Caches/com.zeroonetwenty.BlueHarvest5',
-               '~/Library/Preferences/com.zeroonetwenty.BlueHarvest5.plist',
-             ]
+    "~/Library/Caches/com.zeroonetwenty.BlueHarvest5",
+    "~/Library/Preferences/com.zeroonetwenty.BlueHarvest5.plist",
+  ]
 end
