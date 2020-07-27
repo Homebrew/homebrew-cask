0f73f878604ebe40bf5be4a61d3ba36b6572e818
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:31:34 +0100

    navicat-for-sqlite: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/navicat-for-sqlite.rb
+++ Casks/navicat-for-sqlite.rb
@@ -1,11 +1,11 @@
-cask 'navicat-for-sqlite' do
-  version '15.0.18'
-  sha256 '93747ca90410e83fb401c88ad26fc7f99aa5a868fb8196cb09438b6944442156'
+cask "navicat-for-sqlite" do
+  version "15.0.18"
+  sha256 "93747ca90410e83fb401c88ad26fc7f99aa5a868fb8196cb09438b6944442156"
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_sqlite_en.dmg"
-  appcast 'https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20for%20SQLite&appLang=en'
-  name 'Navicat for SQLite'
-  homepage 'https://www.navicat.com/products/navicat-for-sqlite'
+  appcast "https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20for%20SQLite&appLang=en"
+  name "Navicat for SQLite"
+  homepage "https://www.navicat.com/products/navicat-for-sqlite"
 
-  app 'Navicat for SQLite.app'
+  app "Navicat for SQLite.app"
 end
