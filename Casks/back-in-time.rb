db1a63eb8bd90dfb21da31f9daca25c571f0a8f4
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:17:19 +0100

    back-in-time: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/back-in-time.rb
+++ Casks/back-in-time.rb
@@ -1,11 +1,11 @@
-cask 'back-in-time' do
-  version '5.1.3'
-  sha256 '866bcdb214dd3d4d51a9ca89f12a6ce42e3fd506984c067a9f3b243922afe976'
+cask "back-in-time" do
+  version "5.1.3"
+  sha256 "866bcdb214dd3d4d51a9ca89f12a6ce42e3fd506984c067a9f3b243922afe976"
 
   url "https://www.tri-edre.com/pub/files/backintime#{version.major}.dmg"
-  appcast 'https://www.tri-edre.com/news/backintimeen.html'
-  name 'Back-In-Time'
-  homepage 'https://www.tri-edre.com/english/backintime.html'
+  appcast "https://www.tri-edre.com/news/backintimeen.html"
+  name "Back-In-Time"
+  homepage "https://www.tri-edre.com/english/backintime.html"
 
   app "Back-In-Time #{version.major}.app"
 end
