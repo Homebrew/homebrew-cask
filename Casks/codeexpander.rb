72d8be8b5481a56b75243aaf799aee779481b43e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:20:39 +0100

    codeexpander: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/codeexpander.rb
+++ Casks/codeexpander.rb
@@ -1,12 +1,12 @@
-cask 'codeexpander' do
-  version '3.5.0'
-  sha256 '835032d575715f57f1949156d2a7a441165ae17c398c45f30d6d58179faa3f02'
+cask "codeexpander" do
+  version "3.5.0"
+  sha256 "835032d575715f57f1949156d2a7a441165ae17c398c45f30d6d58179faa3f02"
 
   url "https://github.com/oncework/codeexpander/releases/download/#{version.major_minor}.x/CodeExpander-#{version}.dmg"
-  appcast 'https://github.com/oncework/codeexpander/releases.atom',
+  appcast "https://github.com/oncework/codeexpander/releases.atom",
           must_contain: version.major_minor
-  name 'CodeExpander'
-  homepage 'https://github.com/oncework/codeexpander'
+  name "CodeExpander"
+  homepage "https://github.com/oncework/codeexpander"
 
-  app 'CodeExpander.app'
+  app "CodeExpander.app"
 end
