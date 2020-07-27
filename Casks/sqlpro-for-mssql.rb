14ae7265c9f0e59e03962772eb3d784c732c1af0
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:33 +0100

    sqlpro-for-mssql: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/sqlpro-for-mssql.rb
+++ Casks/sqlpro-for-mssql.rb
@@ -1,17 +1,17 @@
-cask 'sqlpro-for-mssql' do
-  version '2020.61'
-  sha256 '6d85475ccd1bb45555f8e508625e197bb4af397b41e11802f4398360b9cb05e0'
+cask "sqlpro-for-mssql" do
+  version "2020.61"
+  sha256 "6d85475ccd1bb45555f8e508625e197bb4af397b41e11802f4398360b9cb05e0"
 
   # d3fwkemdw8spx3.cloudfront.net/mssql/ was verified as official when first introduced to the cask
   url "https://d3fwkemdw8spx3.cloudfront.net/mssql/SQLProMSSQL.#{version}.app.zip"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?user_agent=Macintosh&url=https://www.macsqlclient.com/download.php'
-  name 'SQLPro for MSSQL'
-  homepage 'https://www.macsqlclient.com/'
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?user_agent=Macintosh&url=https://www.macsqlclient.com/download.php"
+  name "SQLPro for MSSQL"
+  homepage "https://www.macsqlclient.com/"
 
-  app 'SQLPro for MSSQL.app'
+  app "SQLPro for MSSQL.app"
 
   zap trash: [
-               '~/Library/Containers/com.hankinsoft.osx.tinysqlstudio',
-               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.hankinsoft.osx.tinysqlstudio.sfl*',
-             ]
+    "~/Library/Containers/com.hankinsoft.osx.tinysqlstudio",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.hankinsoft.osx.tinysqlstudio.sfl*",
+  ]
 end
