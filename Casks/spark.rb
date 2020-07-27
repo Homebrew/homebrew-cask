c3ef1236ae3f80b1949598bc54ecd8a238311439
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:24 +0100

    spark: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/spark.rb
+++ Casks/spark.rb
@@ -1,12 +1,12 @@
-cask 'spark' do
+cask "spark" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.shadowlab.org/softwares/Spark/Spark.zip'
-  name 'Spark'
-  homepage 'https://www.shadowlab.org/softwares/spark.php'
+  url "https://www.shadowlab.org/softwares/Spark/Spark.zip"
+  name "Spark"
+  homepage "https://www.shadowlab.org/softwares/spark.php"
 
-  app 'Spark.app'
+  app "Spark.app"
 
-  zap trash: '~/Library/Application Support/Spark'
+  zap trash: "~/Library/Application Support/Spark"
 end
