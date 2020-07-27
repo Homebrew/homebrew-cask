cf010db3fc4796a827ea5193197abb43e84c3f1c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:21:49 +0100

    deepgit: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/deepgit.rb
+++ Casks/deepgit.rb
@@ -1,11 +1,11 @@
-cask 'deepgit' do
-  version '4.0.1'
-  sha256 'e1905388c46e1e73e27fdc260350c95923186d962ef5bdc9c59ebba155f53041'
+cask "deepgit" do
+  version "4.0.1"
+  sha256 "e1905388c46e1e73e27fdc260350c95923186d962ef5bdc9c59ebba155f53041"
 
   url "https://www.syntevo.com/downloads/deepgit/deepgit-macosx-#{version.dots_to_underscores}.dmg"
-  appcast 'https://syntevo.com/deepgit/download'
-  name 'DeepGit'
-  homepage 'https://www.syntevo.com/deepgit/'
+  appcast "https://syntevo.com/deepgit/download"
+  name "DeepGit"
+  homepage "https://www.syntevo.com/deepgit/"
 
-  app 'DeepGit.app'
+  app "DeepGit.app"
 end
