b3567b12aab74222e6e9235b42b845a5b8ee02e9
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:15:37 +0100

    alt-tab: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/alt-tab.rb
+++ Casks/alt-tab.rb
@@ -1,22 +1,22 @@
-cask 'alt-tab' do
-  version '4.9.1'
-  sha256 'a15136442023c1981d347a6568bb30502a03cea652e3ffaa17953869de4da556'
+cask "alt-tab" do
+  version "4.9.1"
+  sha256 "a15136442023c1981d347a6568bb30502a03cea652e3ffaa17953869de4da556"
 
   url "https://github.com/lwouis/alt-tab-macos/releases/download/v#{version}/AltTab-#{version}.zip"
-  appcast 'https://github.com/lwouis/alt-tab-macos/releases.atom'
-  name 'alt-tab'
-  homepage 'https://github.com/lwouis/alt-tab-macos'
+  appcast "https://github.com/lwouis/alt-tab-macos/releases.atom"
+  name "alt-tab"
+  homepage "https://github.com/lwouis/alt-tab-macos"
 
   auto_updates true
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'AltTab.app'
+  app "AltTab.app"
 
-  uninstall quit: 'com.lwouis.alt-tab-macos'
+  uninstall quit: "com.lwouis.alt-tab-macos"
 
   zap trash: [
-               '~/Library/Caches/com.lwouis.alt-tab-macos',
-               '~/Library/Cookies/com.lwouis.alt-tab-macos.binarycookies',
-               '~/Library/Preferences/com.lwouis.alt-tab-macos.plist',
-             ]
+    "~/Library/Caches/com.lwouis.alt-tab-macos",
+    "~/Library/Cookies/com.lwouis.alt-tab-macos.binarycookies",
+    "~/Library/Preferences/com.lwouis.alt-tab-macos.plist",
+  ]
 end
