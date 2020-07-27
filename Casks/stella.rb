a6d5c9a7edb807628025e2b5bd8d1615bc717cb9
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:43 +0100

    stella: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/stella.rb
+++ Casks/stella.rb
@@ -1,12 +1,12 @@
-cask 'stella' do
-  version '6.2.1'
-  sha256 'b3385d3f304ef4e77d7bc473e73749009b72190c42d827afe4b38f0d695f9070'
+cask "stella" do
+  version "6.2.1"
+  sha256 "b3385d3f304ef4e77d7bc473e73749009b72190c42d827afe4b38f0d695f9070"
 
   # github.com/stella-emu/stella/ was verified as official when first introduced to the cask
   url "https://github.com/stella-emu/stella/releases/download/#{version}/Stella-#{version}-macos.dmg"
-  appcast 'https://github.com/stella-emu/stella/releases.atom'
-  name 'Stella'
-  homepage 'https://stella-emu.github.io/'
+  appcast "https://github.com/stella-emu/stella/releases.atom"
+  name "Stella"
+  homepage "https://stella-emu.github.io/"
 
-  app 'Stella.app'
+  app "Stella.app"
 end
