b8a1f6c8bdaeb59d33a4f9ee549b5b5a19a96e88
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:16:25 +0100

    aqua-data-studio: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/aqua-data-studio.rb
+++ Casks/aqua-data-studio.rb
@@ -1,11 +1,11 @@
-cask 'aqua-data-studio' do
-  version '20.6.0-4'
-  sha256 '53e70f183e924514ac27ab64f2c7508de1e510cdb74c1a13cbdc147f131bd535'
+cask "aqua-data-studio" do
+  version "20.6.0-4"
+  sha256 "53e70f183e924514ac27ab64f2c7508de1e510cdb74c1a13cbdc147f131bd535"
 
   url "http://downloads.aquafold.com/v#{version.major_minor}.0/osx/ads-osx-#{version}.tar.gz"
-  appcast 'https://www.aquafold.com/aquadatastudio_downloads'
-  name 'Aquafold Aqua Data Studio'
-  homepage 'https://www.aquafold.com/aquadatastudio'
+  appcast "https://www.aquafold.com/aquadatastudio_downloads"
+  name "Aquafold Aqua Data Studio"
+  homepage "https://www.aquafold.com/aquadatastudio"
 
-  app 'Aqua Data Studio.app'
+  app "Aqua Data Studio.app"
 end
