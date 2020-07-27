06e3cbab41b214571aba731420df40e27b05dd03
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:32:10 +0100

    octomouse: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/octomouse.rb
+++ Casks/octomouse.rb
@@ -1,11 +1,11 @@
-cask 'octomouse' do
-  version '1.7'
-  sha256 'b47639b8b4504834caee65a61c0cbe3a42e798cf4901472241f9ce3ab72793e9'
+cask "octomouse" do
+  version "1.7"
+  sha256 "b47639b8b4504834caee65a61c0cbe3a42e798cf4901472241f9ce3ab72793e9"
 
   url "https://github.com/KonsomeJona/OctoMouse/releases/download/v#{version}/OctoMouse.app.zip"
-  appcast 'https://github.com/KonsomeJona/OctoMouse/releases.atom'
-  name 'OctoMouse'
-  homepage 'https://github.com/KonsomeJona/OctoMouse'
+  appcast "https://github.com/KonsomeJona/OctoMouse/releases.atom"
+  name "OctoMouse"
+  homepage "https://github.com/KonsomeJona/OctoMouse"
 
-  app 'OctoMouse.app'
+  app "OctoMouse.app"
 end
