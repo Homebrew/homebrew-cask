0ea64281d00d1d7bd7a481a65c6de05f7e0dcbfb
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:38:49 +0100

    wraparound: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/wraparound.rb
+++ Casks/wraparound.rb
@@ -1,11 +1,11 @@
-cask 'wraparound' do
-  version '2.0-2021'
-  sha256 '4efd0a5051b2ab0487e7d3d4f33093f0d6f23ff0d203d0fc3d66facd6e893474'
+cask "wraparound" do
+  version "2.0-2021"
+  sha256 "4efd0a5051b2ab0487e7d3d4f33093f0d6f23ff0d203d0fc3d66facd6e893474"
 
   url "https://www.digicowsoftware.com/downloads/Wraparound#{version}.zip"
-  appcast 'https://www.digicowsoftware.com/detail?_app=Wraparound'
-  name 'Wraparound'
-  homepage 'https://www.digicowsoftware.com/detail?_app=Wraparound'
+  appcast "https://www.digicowsoftware.com/detail?_app=Wraparound"
+  name "Wraparound"
+  homepage "https://www.digicowsoftware.com/detail?_app=Wraparound"
 
-  app 'Wraparound.app'
+  app "Wraparound.app"
 end
