2fc6df13cf7e1cfe4182ae0a93b40653e488ab99
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:15:41 +0100

    amadeus-pro: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/amadeus-pro.rb
+++ Casks/amadeus-pro.rb
@@ -1,12 +1,12 @@
-cask 'amadeus-pro' do
-  version '2.8.4'
-  sha256 '34d89cb521e7bccef39c02f07ec5dbf25d5255a5e93d563e4b774c79a5a5829e'
+cask "amadeus-pro" do
+  version "2.8.4"
+  sha256 "34d89cb521e7bccef39c02f07ec5dbf25d5255a5e93d563e4b774c79a5a5829e"
 
   # s3.amazonaws.com/AmadeusPro2/ was verified as official when first introduced to the cask
-  url 'https://s3.amazonaws.com/AmadeusPro2/AmadeusPro.zip'
-  appcast 'https://www.hairersoft.com/pro.html'
-  name 'Amadeus Pro'
-  homepage 'https://www.hairersoft.com/pro.html'
+  url "https://s3.amazonaws.com/AmadeusPro2/AmadeusPro.zip"
+  appcast "https://www.hairersoft.com/pro.html"
+  name "Amadeus Pro"
+  homepage "https://www.hairersoft.com/pro.html"
 
-  app 'Amadeus Pro.app'
+  app "Amadeus Pro.app"
 end
