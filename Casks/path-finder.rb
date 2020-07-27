a21516d38e3bcd3657c6c0369d1ec90f54bdc5ca
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:02 +0100

    path-finder: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/path-finder.rb
+++ Casks/path-finder.rb
@@ -1,18 +1,18 @@
-cask 'path-finder' do
-  version '9.3.5'
-  sha256 'bfd62b2bc0f0bd247828125c881030773e1e9d869a07ab19f0344d2b34df4ab0'
+cask "path-finder" do
+  version "9.3.5"
+  sha256 "bfd62b2bc0f0bd247828125c881030773e1e9d869a07ab19f0344d2b34df4ab0"
 
   url "https://get.cocoatech.com/PF#{version.major}.dmg"
-  appcast 'https://get.cocoatech.com/releasecast.xml'
-  name 'Path Finder'
-  homepage 'https://cocoatech.com/'
+  appcast "https://get.cocoatech.com/releasecast.xml"
+  name "Path Finder"
+  homepage "https://cocoatech.com/"
 
   auto_updates true
 
-  app 'Path Finder.app'
+  app "Path Finder.app"
 
   zap trash: [
-               '~/Library/Preferences/com.cocoatech.PathFinder.plist',
-               '~/Library/Application Support/Path Finder',
-             ]
+    "~/Library/Preferences/com.cocoatech.PathFinder.plist",
+    "~/Library/Application Support/Path Finder",
+  ]
 end
