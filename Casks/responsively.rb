d0ce5a810fd520ca8f66154e6ae3c16a8a45ebc5
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:58 +0100

    responsively: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/responsively.rb
+++ Casks/responsively.rb
@@ -1,17 +1,17 @@
-cask 'responsively' do
-  version '0.8.0'
-  sha256 '767b1789a0e8ad9d740817d37eeaa3c8c29426b5d3031cb9b1ab87012f728108'
+cask "responsively" do
+  version "0.8.0"
+  sha256 "767b1789a0e8ad9d740817d37eeaa3c8c29426b5d3031cb9b1ab87012f728108"
 
   # github.com/manojVivek/responsively-app/ was verified as official when first introduced to the cask
   url "https://github.com/manojVivek/responsively-app/releases/download/v#{version}/ResponsivelyApp-#{version}.dmg"
-  appcast 'https://github.com/manojVivek/responsively-app/releases.atom'
-  name 'Responsively'
-  homepage 'https://manojvivek.github.io/responsively-app/'
+  appcast "https://github.com/manojVivek/responsively-app/releases.atom"
+  name "Responsively"
+  homepage "https://manojvivek.github.io/responsively-app/"
 
-  app 'ResponsivelyApp.app'
+  app "ResponsivelyApp.app"
 
   zap trash: [
-               '~/Library/Application Support/ResponsivelyApp',
-               '~/Library/Preferences/app.responsively.plist',
-             ]
+    "~/Library/Application Support/ResponsivelyApp",
+    "~/Library/Preferences/app.responsively.plist",
+  ]
 end
