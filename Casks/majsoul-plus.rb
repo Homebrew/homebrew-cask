df19bab6e82beb6e2ce7f01cf9b089b84d6589f5
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:45 +0100

    majsoul-plus: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/majsoul-plus.rb
+++ Casks/majsoul-plus.rb
@@ -1,11 +1,11 @@
-cask 'majsoul-plus' do
-  version '2.0.1'
-  sha256 'efac0e077f871a092ce1654466055fdc73c74f22da6a2364588d7b460443a52b'
+cask "majsoul-plus" do
+  version "2.0.1"
+  sha256 "efac0e077f871a092ce1654466055fdc73c74f22da6a2364588d7b460443a52b"
 
   url "https://github.com/MajsoulPlus/majsoul-plus/releases/download/v#{version}/Majsoul_Plus-#{version}-darwin.dmg"
-  appcast 'https://github.com/MajsoulPlus/majsoul-plus/releases.atom'
-  name 'Majsoul Plus'
-  homepage 'https://github.com/MajsoulPlus/majsoul-plus/'
+  appcast "https://github.com/MajsoulPlus/majsoul-plus/releases.atom"
+  name "Majsoul Plus"
+  homepage "https://github.com/MajsoulPlus/majsoul-plus/"
 
-  app 'Majsoul Plus.app'
+  app "Majsoul Plus.app"
 end
