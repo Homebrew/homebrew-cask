4797f11117f56ceffbfdb6195900ffffb88093b1
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:43 +0100

    mailbutler: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/mailbutler.rb
+++ Casks/mailbutler.rb
@@ -1,13 +1,13 @@
-cask 'mailbutler' do
-  version '2,3020-13965'
-  sha256 '2d67fb2d1c348d506e15626fb51a04ce9b5c4cad8653da0d1f72d7b2a23b5a34'
+cask "mailbutler" do
+  version "2,3020-13965"
+  sha256 "2d67fb2d1c348d506e15626fb51a04ce9b5c4cad8653da0d1f72d7b2a23b5a34"
 
   url "https://downloads.mailbutler.io/sparkle/public/Mailbutler_#{version.after_comma}.zip"
   appcast "https://www.mailbutler.io/appcast#{version.major}.php"
-  name 'Mailbutler'
-  homepage 'https://www.mailbutler.io/'
+  name "Mailbutler"
+  homepage "https://www.mailbutler.io/"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'Mailbutler.app'
+  app "Mailbutler.app"
 end
