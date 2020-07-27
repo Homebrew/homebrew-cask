4757f8da2cf4b4bcaaccb52f706073331da80619
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:02 +0100

    sketchup-pro: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/sketchup-pro.rb
+++ Casks/sketchup-pro.rb
@@ -1,73 +1,73 @@
-cask 'sketchup-pro' do
-  version '20.1.228,2020.1'
+cask "sketchup-pro" do
+  version "20.1.228,2020.1"
 
-  language 'de' do
-    sha256 '54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815'
-    'de'
+  language "de" do
+    sha256 "54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815"
+    "de"
   end
 
-  language 'en', default: true do
-    sha256 '54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815'
-    'en'
+  language "en", default: true do
+    sha256 "54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815"
+    "en"
   end
 
-  language 'es' do
-    sha256 '54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815'
-    'es'
+  language "es" do
+    sha256 "54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815"
+    "es"
   end
 
-  language 'fr' do
-    sha256 '54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815'
-    'fr'
+  language "fr" do
+    sha256 "54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815"
+    "fr"
   end
 
-  language 'it' do
-    sha256 '54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815'
-    'it'
+  language "it" do
+    sha256 "54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815"
+    "it"
   end
 
-  language 'ja' do
-    sha256 '54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815'
-    'ja'
+  language "ja" do
+    sha256 "54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815"
+    "ja"
   end
 
-  language 'ko' do
-    sha256 '54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815'
-    'ko'
+  language "ko" do
+    sha256 "54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815"
+    "ko"
   end
 
-  language 'pt-BR' do
-    sha256 '54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815'
-    'pt-BR'
+  language "pt-BR" do
+    sha256 "54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815"
+    "pt-BR"
   end
 
-  language 'ru' do
-    sha256 '54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815'
-    'ru'
+  language "ru" do
+    sha256 "54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815"
+    "ru"
   end
 
-  language 'sv' do
-    sha256 '54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815'
-    'sv'
+  language "sv" do
+    sha256 "54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815"
+    "sv"
   end
 
-  language 'zh-CN' do
-    sha256 '54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815'
-    'zh-CN'
+  language "zh-CN" do
+    sha256 "54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815"
+    "zh-CN"
   end
 
-  language 'zh-TW' do
-    sha256 '54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815'
-    'zh-TW'
+  language "zh-TW" do
+    sha256 "54c9ec6e7c13d920be52d63a83f17487b219473fcd7a2920ee36957af08f1815"
+    "zh-TW"
   end
 
   # downloads can be found at https://sketchup.com/download/all
   url "https://www.sketchup.com/sketchup/SketchUpPro-#{language}-dmg"
-  appcast 'https://help.sketchup.com/en/release-notes-0'
-  name 'SketchUp'
-  homepage 'https://www.sketchup.com/'
+  appcast "https://help.sketchup.com/en/release-notes-0"
+  name "SketchUp"
+  homepage "https://www.sketchup.com/"
 
-  installer manual: 'Double-Click to Install Sketchup.app'
+  installer manual: "Double-Click to Install Sketchup.app"
 
   uninstall delete: "/Applications/SketchUp #{version.after_comma}"
 end
