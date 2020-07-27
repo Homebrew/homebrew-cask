7ea01c0d38f4d9a4e5cd88c5879d6951f002cb57
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:24 +0100

    sparkle: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/sparkle.rb
+++ Casks/sparkle.rb
@@ -1,12 +1,12 @@
-cask 'sparkle' do
-  version '1.22.0'
-  sha256 '50bdc8ad64593d871e4deb5b7526444422c3ac1e1f461fcfdd9e6aef0d2e34cd'
+cask "sparkle" do
+  version "1.22.0"
+  sha256 "50bdc8ad64593d871e4deb5b7526444422c3ac1e1f461fcfdd9e6aef0d2e34cd"
 
   # github.com/sparkle-project/Sparkle/ was verified as official when first introduced to the cask
   url "https://github.com/sparkle-project/Sparkle/releases/download/#{version}/Sparkle-#{version}.tar.bz2"
-  appcast 'https://github.com/sparkle-project/Sparkle/releases.atom'
-  name 'Sparkle'
-  homepage 'https://sparkle-project.org/'
+  appcast "https://github.com/sparkle-project/Sparkle/releases.atom"
+  name "Sparkle"
+  homepage "https://sparkle-project.org/"
 
-  app 'Sparkle Test App.app'
+  app "Sparkle Test App.app"
 end
