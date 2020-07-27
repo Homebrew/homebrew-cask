7781ad300f1544e4ba642248f43a454831747525
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:24:26 +0100

    folding-at-home: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/folding-at-home.rb
+++ Casks/folding-at-home.rb
@@ -1,18 +1,18 @@
-cask 'folding-at-home' do
-  version '7.6.13'
-  sha256 'ee96f4f1b724db46f9a37f7d805095a16b24f1ef7e9b9a1615a56e94e4119a36'
+cask "folding-at-home" do
+  version "7.6.13"
+  sha256 "ee96f4f1b724db46f9a37f7d805095a16b24f1ef7e9b9a1615a56e94e4119a36"
 
   url "https://download.foldingathome.org/releases/public/release/fah-installer/osx-10.11-64bit/v#{version.major_minor}/fah-installer_#{version}_x86_64.mpkg.zip"
-  appcast 'https://download.foldingathome.org/js/fah-downloads.js'
-  name 'Folding@home'
-  homepage 'https://foldingathome.org/'
+  appcast "https://download.foldingathome.org/js/fah-downloads.js"
+  name "Folding@home"
+  homepage "https://foldingathome.org/"
 
   pkg "fah-installer_#{version}_x86_64.pkg"
 
-  uninstall pkgutil:   'org.foldingathome.*',
-            launchctl: 'org.foldingathome.fahclient',
+  uninstall pkgutil:   "org.foldingathome.*",
+            launchctl: "org.foldingathome.fahclient",
             quit:      [
-                         'org.foldingathome.fahviewer',
-                         'org.foldingathome.fahcontrol',
-                       ]
+              "org.foldingathome.fahviewer",
+              "org.foldingathome.fahcontrol",
+            ]
 end
