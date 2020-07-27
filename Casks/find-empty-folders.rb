027af46c5890710ecf60f4278ae68325efbe9466
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:23:58 +0100

    find-empty-folders: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/find-empty-folders.rb
+++ Casks/find-empty-folders.rb
@@ -1,11 +1,11 @@
-cask 'find-empty-folders' do
-  version '1.2.1'
-  sha256 '63033d326f44c7eda130e0a1ce54d4b5a7009a095afa4bd8b3d557d2c357cece'
+cask "find-empty-folders" do
+  version "1.2.1"
+  sha256 "63033d326f44c7eda130e0a1ce54d4b5a7009a095afa4bd8b3d557d2c357cece"
 
-  url 'https://files.tempel.org/FindEmptyFolders/FindEmptyFolders.zip'
-  appcast 'https://www.tempel.org/FindEmptyFolders'
-  name 'Find Empty Folders'
-  homepage 'https://www.tempel.org/FindEmptyFolders'
+  url "https://files.tempel.org/FindEmptyFolders/FindEmptyFolders.zip"
+  appcast "https://www.tempel.org/FindEmptyFolders"
+  name "Find Empty Folders"
+  homepage "https://www.tempel.org/FindEmptyFolders"
 
-  app 'Find Empty Folders.app'
+  app "Find Empty Folders.app"
 end
