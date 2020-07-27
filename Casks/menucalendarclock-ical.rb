020c1d7b578a69c8538c11cff476df6181dbeafe
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:30:18 +0100

    menucalendarclock-ical: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/menucalendarclock-ical.rb
+++ Casks/menucalendarclock-ical.rb
@@ -1,11 +1,11 @@
-cask 'menucalendarclock-ical' do
-  version '4.5.2'
-  sha256 '7c78428fd22d0a3fa02e5df4c92fbb45dad881a7ba09205dc47c4bfaaf7f7f9e'
+cask "menucalendarclock-ical" do
+  version "4.5.2"
+  sha256 "7c78428fd22d0a3fa02e5df4c92fbb45dad881a7ba09205dc47c4bfaaf7f7f9e"
 
   url "http://www.objectpark.net/download/MenuCalendarClock-#{version}.dmg.gz"
-  appcast 'http://www.objectpark.net/appcasts/MCC3.xml'
-  name 'MenuCalendarClock'
-  homepage 'http://www.objectpark.net/en/mcc.html'
+  appcast "http://www.objectpark.net/appcasts/MCC3.xml"
+  name "MenuCalendarClock"
+  homepage "http://www.objectpark.net/en/mcc.html"
 
-  app 'MenuCalendarClock iCal.app'
+  app "MenuCalendarClock iCal.app"
 end
