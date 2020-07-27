92867d3d5663539e475d956f3332eeeabfb75cc4
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:31:04 +0100

    mountain: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/mountain.rb
+++ Casks/mountain.rb
@@ -1,11 +1,11 @@
-cask 'mountain' do
-  version '1.6.6'
-  sha256 '22496566734b7b610a24e3c7700b123a17c5502fdc1562d35d65c04ecf5e7070'
+cask "mountain" do
+  version "1.6.6"
+  sha256 "22496566734b7b610a24e3c7700b123a17c5502fdc1562d35d65c04ecf5e7070"
 
-  url 'https://appgineers.de/mountain/files/Mountain.zip'
-  appcast 'https://appgineers.de/mountain/files/mountaincast.xml'
-  name 'Mountain'
-  homepage 'https://appgineers.de/mountain/'
+  url "https://appgineers.de/mountain/files/Mountain.zip"
+  appcast "https://appgineers.de/mountain/files/mountaincast.xml"
+  name "Mountain"
+  homepage "https://appgineers.de/mountain/"
 
-  app 'Mountain.app'
+  app "Mountain.app"
 end
