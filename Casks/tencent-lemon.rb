180a0ff3cd7cbb5e90b1cf994a852d0dec95b1d9
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:37:16 +0100

    tencent-lemon: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/tencent-lemon.rb
+++ Casks/tencent-lemon.rb
@@ -1,12 +1,12 @@
-cask 'tencent-lemon' do
-  version '4.5.0'
-  sha256 '947fa22f1339b78153677494563ba92b0559f7f327bce01db6e06f1531ab82ef'
+cask "tencent-lemon" do
+  version "4.5.0"
+  sha256 "947fa22f1339b78153677494563ba92b0559f7f327bce01db6e06f1531ab82ef"
 
   # pm.myapp.com/invc/xfspeed/qqpcmgr/ was verified as official when first introduced to the cask
   url "https://pm.myapp.com/invc/xfspeed/qqpcmgr/module_update/Lemon-#{version}.dmg"
-  appcast 'https://lemon.guanjia.qq.com/latest/package'
-  name 'Tencent Lemon Cleaner'
-  homepage 'https://mac.gj.qq.com/'
+  appcast "https://lemon.guanjia.qq.com/latest/package"
+  name "Tencent Lemon Cleaner"
+  homepage "https://mac.gj.qq.com/"
 
-  app 'Tencent Lemon.app'
+  app "Tencent Lemon.app"
 end
