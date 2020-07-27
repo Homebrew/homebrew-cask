8410adeacd6be6d1f057c6ebef5fee8f351747a1
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:20:18 +0100

    clickup: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/clickup.rb
+++ Casks/clickup.rb
@@ -1,10 +1,10 @@
-cask 'clickup' do
-  version '2.0.14'
-  sha256 '155a1f2c55a26873a96774e49ec89eb9801b850483db6b1ec5426a4a39e9800b'
+cask "clickup" do
+  version "2.0.14"
+  sha256 "155a1f2c55a26873a96774e49ec89eb9801b850483db6b1ec5426a4a39e9800b"
 
   url "https://attachments.clickup.com/desktop/clickup-desktop-#{version}-mac.dmg"
-  name 'ClickUp'
-  homepage 'https://clickup.com/'
+  name "ClickUp"
+  homepage "https://clickup.com/"
 
-  app 'ClickUp.app'
+  app "ClickUp.app"
 end
