9623b33236090d9ea63625eb4b35ae76e2bcc367
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:25:37 +0100

    google-featured-photos: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/google-featured-photos.rb
+++ Casks/google-featured-photos.rb
@@ -1,10 +1,10 @@
-cask 'google-featured-photos' do
-  version '1.0.0.208'
-  sha256 '4d4abc378b38278b0cd247e99a008c730a3b6b77824437b01db0bf8beaf24bfb'
+cask "google-featured-photos" do
+  version "1.0.0.208"
+  sha256 "4d4abc378b38278b0cd247e99a008c730a3b6b77824437b01db0bf8beaf24bfb"
 
   url "https://dl.google.com/featuredphotosscreensaver/GoogleFeaturedPhotos-#{version}.dmg"
-  name 'Google Featured Photos'
-  homepage 'https://plus.google.com/featuredphotos'
+  name "Google Featured Photos"
+  homepage "https://plus.google.com/featuredphotos"
 
-  screen_saver 'Google Featured Photos.saver'
+  screen_saver "Google Featured Photos.saver"
 end
