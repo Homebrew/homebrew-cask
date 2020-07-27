e79da114d08c2437504766144e14598450b2309f
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:30:56 +0100

    mongodb-compass-readonly: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/mongodb-compass-readonly.rb
+++ Casks/mongodb-compass-readonly.rb
@@ -1,11 +1,11 @@
-cask 'mongodb-compass-readonly' do
-  version '1.21.2'
-  sha256 '404162d4be0a17c49ca6c43f1875e3b43b31ea8001359962fbb09078a8cb61f8'
+cask "mongodb-compass-readonly" do
+  version "1.21.2"
+  sha256 "404162d4be0a17c49ca6c43f1875e3b43b31ea8001359962fbb09078a8cb61f8"
 
   url "https://downloads.mongodb.com/compass/mongodb-compass-readonly-#{version}-darwin-x64.dmg"
-  appcast 'https://www.mongodb.com/download-center/compass'
-  name 'MongoDB Compass Readonly'
-  homepage 'https://www.mongodb.com/products/compass'
+  appcast "https://www.mongodb.com/download-center/compass"
+  name "MongoDB Compass Readonly"
+  homepage "https://www.mongodb.com/products/compass"
 
-  app 'MongoDB Compass Readonly.app'
+  app "MongoDB Compass Readonly.app"
 end
