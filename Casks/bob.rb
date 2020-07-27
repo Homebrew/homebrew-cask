932389996757d8983419777806ad0eb41ca367fb
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:18:30 +0100

    bob: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/bob.rb
+++ Casks/bob.rb
@@ -1,18 +1,18 @@
-cask 'bob' do
-  version '0.4.0'
-  sha256 '2862b11cd2cb456a1b684e5a7d06fc4dc6433ad1c18af4d61c04a4dbc1150a30'
+cask "bob" do
+  version "0.4.0"
+  sha256 "2862b11cd2cb456a1b684e5a7d06fc4dc6433ad1c18af4d61c04a4dbc1150a30"
 
   url "https://github.com/ripperhe/Bob/releases/download/v#{version}/Bob.app.zip"
-  appcast 'https://github.com/ripperhe/Bob/releases.atom'
-  name 'Bob'
-  homepage 'https://github.com/ripperhe/Bob'
+  appcast "https://github.com/ripperhe/Bob/releases.atom"
+  name "Bob"
+  homepage "https://github.com/ripperhe/Bob"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'Bob.app'
+  app "Bob.app"
 
   zap trash: [
-               '~/Library/Preferences/com.ripperhe.Bob.plist',
-               '~/Library/Caches/com.ripperhe.Bob',
-             ]
+    "~/Library/Preferences/com.ripperhe.Bob.plist",
+    "~/Library/Caches/com.ripperhe.Bob",
+  ]
 end
