a3883fd3fa4838cf2f79327eea83bfd1e4b1dc9a
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:25:01 +0100

    geekbench: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/geekbench.rb
+++ Casks/geekbench.rb
@@ -1,23 +1,23 @@
-cask 'geekbench' do
+cask "geekbench" do
   if MacOS.version <= :sierra
-    version '4.4.2'
-    sha256 '3c46e630a28a0752afd702fc1cd379edd2420001be22302c932e61751284c0cc'
+    version "4.4.2"
+    sha256 "3c46e630a28a0752afd702fc1cd379edd2420001be22302c932e61751284c0cc"
   else
-    version '5.2.3'
-    sha256 '51907120ea71b2100716fa9a37e914b2eb82ceac648ff2dbc2ba5156831e89fb'
+    version "5.2.3"
+    sha256 "51907120ea71b2100716fa9a37e914b2eb82ceac648ff2dbc2ba5156831e89fb"
   end
 
   url "https://cdn.geekbench.com/Geekbench-#{version}-Mac.zip"
   appcast "https://www.primatelabs.com/appcast/geekbench#{version.major}.xml"
-  name 'Geekbench'
-  homepage 'https://www.geekbench.com/'
+  name "Geekbench"
+  homepage "https://www.geekbench.com/"
 
   auto_updates true
 
   app "Geekbench #{version.major}.app"
 
   zap trash: [
-               "~/Library/Caches/com.primatelabs.Geekbench#{version.major}",
-               "~/Library/Preferences/com.primatelabs.Geekbench#{version.major}.plist",
-             ]
+    "~/Library/Caches/com.primatelabs.Geekbench#{version.major}",
+    "~/Library/Preferences/com.primatelabs.Geekbench#{version.major}.plist",
+  ]
 end
