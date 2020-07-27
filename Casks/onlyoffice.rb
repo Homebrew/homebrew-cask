af572ad635865cfce98babcfceb5419b78058208
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:32:20 +0100

    onlyoffice: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/onlyoffice.rb
+++ Casks/onlyoffice.rb
@@ -1,13 +1,13 @@
-cask 'onlyoffice' do
-  version '5.5.1'
-  sha256 '7090144f0d06328a7a1de43ea779c3db32b1afd7358295ca71ff2fbddb03601b'
+cask "onlyoffice" do
+  version "5.5.1"
+  sha256 "7090144f0d06328a7a1de43ea779c3db32b1afd7358295ca71ff2fbddb03601b"
 
   url "https://download.onlyoffice.com/install/desktop/editors/mac/updates/onlyoffice/ONLYOFFICE-#{version}.zip"
-  appcast 'https://download.onlyoffice.com/install/desktop/editors/mac/onlyoffice.xml'
-  name 'ONLYOFFICE'
-  homepage 'https://www.onlyoffice.com/'
+  appcast "https://download.onlyoffice.com/install/desktop/editors/mac/onlyoffice.xml"
+  name "ONLYOFFICE"
+  homepage "https://www.onlyoffice.com/"
 
   auto_updates true
 
-  app 'ONLYOFFICE.app'
+  app "ONLYOFFICE.app"
 end
