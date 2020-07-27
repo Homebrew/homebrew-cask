088076874621cf9ce547dd39d1268b45d8640b3a
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:24:29 +0100

    fontlab: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/fontlab.rb
+++ Casks/fontlab.rb
@@ -1,12 +1,12 @@
-cask 'fontlab' do
-  version '7.1.3.7495'
-  sha256 'd4c839c34e03d00268b8b595056d12f8abfedeb92ab0cab6bff8299a8455950e'
+cask "fontlab" do
+  version "7.1.3.7495"
+  sha256 "d4c839c34e03d00268b8b595056d12f8abfedeb92ab0cab6bff8299a8455950e"
 
   # fontlab.s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://fontlab.s3.amazonaws.com/fontlab-#{version.major}/#{version.split('.').last}/FontLab-#{version.major}-Mac-Install-#{version.split('.').last}.dmg"
+  url "https://fontlab.s3.amazonaws.com/fontlab-#{version.major}/#{version.split(".").last}/FontLab-#{version.major}-Mac-Install-#{version.split(".").last}.dmg"
   appcast "https://download.fontlab.com/fontlab-#{version.major}/appcast-mac.xml"
-  name 'Fontlab'
-  homepage 'https://www.fontlab.com/font-editor/fontlab/'
+  name "Fontlab"
+  homepage "https://www.fontlab.com/font-editor/fontlab/"
 
   app "FontLab #{version.major}.app"
 end
