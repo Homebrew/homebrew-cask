6cf48bce562d73ac690efd66af1810969364acf5
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:15:30 +0100

    alchemy: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/alchemy.rb
+++ Casks/alchemy.rb
@@ -1,11 +1,11 @@
-cask 'alchemy' do
-  version '008'
-  sha256 '7527f4e2231db8167c57d2639fcba166d8fefea091cca7884bd355fb52a3449a'
+cask "alchemy" do
+  version "008"
+  sha256 "7527f4e2231db8167c57d2639fcba166d8fefea091cca7884bd355fb52a3449a"
 
   url "https://al.chemy.org/files/Alchemy-#{version}.dmg"
-  appcast 'https://al.chemy.org/download/version-history/'
-  name 'Alchemy'
-  homepage 'https://al.chemy.org/'
+  appcast "https://al.chemy.org/download/version-history/"
+  name "Alchemy"
+  homepage "https://al.chemy.org/"
 
-  app 'Alchemy/Alchemy.app'
+  app "Alchemy/Alchemy.app"
 end
