fe0dd4e74926a7b36e0f5f5c1dd01b75c95f84ec
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:15 +0100

    loom: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/loom.rb
+++ Casks/loom.rb
@@ -1,13 +1,13 @@
-cask 'loom' do
-  version '0.42.10'
-  sha256 'bce3eaa2b02cb612be640be20fb1e875d704dd880e95434ea2d5ce218137a37b'
+cask "loom" do
+  version "0.42.10"
+  sha256 "bce3eaa2b02cb612be640be20fb1e875d704dd880e95434ea2d5ce218137a37b"
 
   url "https://cdn.loom.com/desktop-packages/Loom-#{version}.dmg"
-  appcast 'https://s3-us-west-2.amazonaws.com/loom.desktop.packages/loom-inc-production/desktop-packages/latest-mac.yml'
-  name 'Loom'
-  homepage 'https://www.loom.com/'
+  appcast "https://s3-us-west-2.amazonaws.com/loom.desktop.packages/loom-inc-production/desktop-packages/latest-mac.yml"
+  name "Loom"
+  homepage "https://www.loom.com/"
 
   auto_updates true
 
-  app 'Loom.app'
+  app "Loom.app"
 end
