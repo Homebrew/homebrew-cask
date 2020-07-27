bcc30218a9706529134976e9dcadb0760b116ae3
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:41 +0100

    stats: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/stats.rb
+++ Casks/stats.rb
@@ -1,24 +1,24 @@
-cask 'stats' do
-  version '2.1.14'
-  sha256 'cba7c87eba9632f411365d650d5c1dd5552c120b73e6ef90c50c9c499fb6fa3c'
+cask "stats" do
+  version "2.1.14"
+  sha256 "cba7c87eba9632f411365d650d5c1dd5552c120b73e6ef90c50c9c499fb6fa3c"
 
   url "https://github.com/exelban/stats/releases/download/v#{version}/Stats.dmg"
-  appcast 'https://github.com/exelban/stats/releases.atom'
-  name 'Stats'
-  homepage 'https://github.com/exelban/stats'
+  appcast "https://github.com/exelban/stats/releases.atom"
+  name "Stats"
+  homepage "https://github.com/exelban/stats"
 
   auto_updates true
-  depends_on macos: '>= :mojave'
+  depends_on macos: ">= :mojave"
 
-  app 'Stats.app'
+  app "Stats.app"
 
-  uninstall quit: 'eu.exelban.Stats'
+  uninstall quit: "eu.exelban.Stats"
 
   zap trash: [
-               '~/Library/Preferences/eu.exelban.Stats.plist',
-               '~/Library/Application Scripts/eu.exelban.Stats.LaunchAtLogin',
-               '~/Library/Containers/eu.exelban.Stats.LaunchAtLogin',
-               '~/Library/Cookies/eu.exelban.Stats.binarycookies',
-               '~/Library/Caches/eu.exelban.Stats',
-             ]
+    "~/Library/Preferences/eu.exelban.Stats.plist",
+    "~/Library/Application Scripts/eu.exelban.Stats.LaunchAtLogin",
+    "~/Library/Containers/eu.exelban.Stats.LaunchAtLogin",
+    "~/Library/Cookies/eu.exelban.Stats.binarycookies",
+    "~/Library/Caches/eu.exelban.Stats",
+  ]
 end
