882107e8b769c026397bfd41fd0d407cc3537a20
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:24:28 +0100

    fontbase: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/fontbase.rb
+++ Casks/fontbase.rb
@@ -1,18 +1,18 @@
-cask 'fontbase' do
-  version '2.11.3'
-  sha256 'c29ea63ae04de1ca67b51c892649e25783442d8d00d70ce41510cd43187d2d2c'
+cask "fontbase" do
+  version "2.11.3"
+  sha256 "c29ea63ae04de1ca67b51c892649e25783442d8d00d70ce41510cd43187d2d2c"
 
   url "https://releases.fontba.se/mac/FontBase-#{version}.dmg"
-  appcast 'https://releases.fontba.se/mac/latest-mac.yml'
-  name 'FontBase'
-  homepage 'https://fontba.se/'
+  appcast "https://releases.fontba.se/mac/latest-mac.yml"
+  name "FontBase"
+  homepage "https://fontba.se/"
 
-  app 'FontBase.app'
+  app "FontBase.app"
 
   zap trash: [
-               '~/Library/Preferences/com.dominiklevitsky.fontbase.helper.plist',
-               '~/Library/Preferences/com.dominiklevitsky.fontbase.plist',
-               '~/Library/Application Support/FontBase',
-               '~/Library/Saved Application State/com.dominiklevitsky.fontbase.savedState',
-             ]
+    "~/Library/Preferences/com.dominiklevitsky.fontbase.helper.plist",
+    "~/Library/Preferences/com.dominiklevitsky.fontbase.plist",
+    "~/Library/Application Support/FontBase",
+    "~/Library/Saved Application State/com.dominiklevitsky.fontbase.savedState",
+  ]
 end
