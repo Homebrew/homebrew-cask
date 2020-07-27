1ed8741e05d062c698a22dff886c03c9bc19c4e8
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:53 +0100

    signal: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/signal.rb
+++ Casks/signal.rb
@@ -1,20 +1,20 @@
-cask 'signal' do
-  version '1.34.4'
-  sha256 '2fa60f3ecfc55a407686850ec5b927dabd866b705d009fa2a35f5a10a2ddf465'
+cask "signal" do
+  version "1.34.4"
+  sha256 "2fa60f3ecfc55a407686850ec5b927dabd866b705d009fa2a35f5a10a2ddf465"
 
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{version}.dmg"
-  appcast 'https://github.com/signalapp/Signal-Desktop/releases.atom'
-  name 'Signal'
-  homepage 'https://signal.org/'
+  appcast "https://github.com/signalapp/Signal-Desktop/releases.atom"
+  name "Signal"
+  homepage "https://signal.org/"
 
   auto_updates true
 
-  app 'Signal.app'
+  app "Signal.app"
 
   zap trash: [
-               '~/Library/Application Support/Signal',
-               '~/Library/Preferences/org.whispersystems.signal-desktop.helper.plist',
-               '~/Library/Preferences/org.whispersystems.signal-desktop.plist',
-               '~/Library/Saved Application State/org.whispersystems.signal-desktop.savedState',
-             ]
+    "~/Library/Application Support/Signal",
+    "~/Library/Preferences/org.whispersystems.signal-desktop.helper.plist",
+    "~/Library/Preferences/org.whispersystems.signal-desktop.plist",
+    "~/Library/Saved Application State/org.whispersystems.signal-desktop.savedState",
+  ]
 end
