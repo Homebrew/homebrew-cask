03588b4c37e6fd1620c0fea0b040cb1432ff11c4
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:20:04 +0100

    cinebench: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/cinebench.rb
+++ Casks/cinebench.rb
@@ -1,22 +1,22 @@
-cask 'cinebench' do
-  version 'R20,281795'
-  sha256 'c2ee9b2dab04cdd8fc1b1a03bb2303cdaf1495cbc2614cd2f3fa56cd8151e47e'
+cask "cinebench" do
+  version "R20,281795"
+  sha256 "c2ee9b2dab04cdd8fc1b1a03bb2303cdaf1495cbc2614cd2f3fa56cd8151e47e"
 
   url "https://http.maxon.net/pub/cinebench/Cinebench#{version.before_comma}.dmg"
-  name 'Cinebench'
-  homepage 'https://www.maxon.net/products/cinebench/'
+  name "Cinebench"
+  homepage "https://www.maxon.net/products/cinebench/"
 
-  depends_on macos: '>= :el_capitan'
+  depends_on macos: ">= :el_capitan"
 
-  app 'Cinebench.app'
+  app "Cinebench.app"
 
-  uninstall quit: 'net.maxon.cinebench'
+  uninstall quit: "net.maxon.cinebench"
 
   zap trash: [
-               '~/Documents/MAXON',
-               '~/Library/Caches/net.maxon.cinebench',
-               '~/Library/Caches/net.maxon.cinema4d',
-               '~/Library/Preferences/MAXON',
-               '~/Library/Saved Application State/net.maxon.cinebench.savedState',
-             ]
+    "~/Documents/MAXON",
+    "~/Library/Caches/net.maxon.cinebench",
+    "~/Library/Caches/net.maxon.cinema4d",
+    "~/Library/Preferences/MAXON",
+    "~/Library/Saved Application State/net.maxon.cinebench.savedState",
+  ]
 end
