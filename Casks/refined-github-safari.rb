ba258ed167477a2e8fc88f65eef8ff07c1386ac8
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:52 +0100

    refined-github-safari: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/refined-github-safari.rb
+++ Casks/refined-github-safari.rb
@@ -1,20 +1,20 @@
-cask 'refined-github-safari' do
-  version '2.1.2'
-  sha256 'f3e67532f0d80b88e5161d2af3ed2f1450589234e52eb57eb01e871a2cc031eb'
+cask "refined-github-safari" do
+  version "2.1.2"
+  sha256 "f3e67532f0d80b88e5161d2af3ed2f1450589234e52eb57eb01e871a2cc031eb"
 
   url "https://github.com/lautis/refined-github-safari/releases/download/v#{version}/Refined-GitHub-for-Safari.zip"
-  appcast 'https://github.com/lautis/refined-github-safari/releases.atom'
-  name 'Refined GitHub for Safari'
-  homepage 'https://github.com/lautis/refined-github-safari'
+  appcast "https://github.com/lautis/refined-github-safari/releases.atom"
+  name "Refined GitHub for Safari"
+  homepage "https://github.com/lautis/refined-github-safari"
 
-  depends_on macos: '>= :mojave'
+  depends_on macos: ">= :mojave"
 
-  app 'Refined GitHub for Safari.app'
+  app "Refined GitHub for Safari.app"
 
   zap delete: [
-                '~/Library/Application Scripts/fi.lautanala.refined-github',
-                '~/Library/Application Scripts/fi.lautanala.refined-github-extension',
-                '~/Library/Containers/fi.lautanala.refined-github',
-                '~/Library/Containers/fi.lautanala.refined-github-extenstion',
-              ]
+    "~/Library/Application Scripts/fi.lautanala.refined-github",
+    "~/Library/Application Scripts/fi.lautanala.refined-github-extension",
+    "~/Library/Containers/fi.lautanala.refined-github",
+    "~/Library/Containers/fi.lautanala.refined-github-extenstion",
+  ]
 end
