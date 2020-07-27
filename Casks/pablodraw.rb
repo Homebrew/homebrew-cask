926081d2c2ffc13dc3a660f5a5deeb640eddb6f5
CoreCode <core-code@users.noreply.github.com>
2020-07-28 01:30:57 +0200

    Update pablodraw.rb (#86617)

--- Casks/pablodraw.rb
+++ Casks/pablodraw.rb
@@ -2,7 +2,7 @@ cask "pablodraw" do
   version "3.2.1"
   sha256 "91a89ac33ba3e064a7a2b44d5c7c271cbd1303882874e7ce159b8fb058589301"
 
-  url "http://download.picoe.ca/pablodraw/#{version.sub(/\.\d+$/, "")}/PabloDrawMac-#{version}.zip"
+  url "http://download.picoe.ca/pablodraw/#{version.major_minor}/PabloDrawMac-#{version}.zip"
   name "PabloDraw"
   homepage "http://picoe.ca/products/pablodraw/"
 
