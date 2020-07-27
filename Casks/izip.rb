2538857e3d2e0d721d4f4c12af4a7cbcf8819e4e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:27:32 +0100

    izip: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/izip.rb
+++ Casks/izip.rb
@@ -1,11 +1,11 @@
-cask 'izip' do
-  version '3.7'
-  sha256 '7be4dbc10384072058e56164b3a58a5164127e556e4a343a3acc14fd6f8104ba'
+cask "izip" do
+  version "3.7"
+  sha256 "7be4dbc10384072058e56164b3a58a5164127e556e4a343a3acc14fd6f8104ba"
 
-  url 'https://www.izip.com/izip.dmg'
-  appcast 'https://www.izip.com/download'
-  name 'iZip'
-  homepage 'https://www.izip.com/'
+  url "https://www.izip.com/izip.dmg"
+  appcast "https://www.izip.com/download"
+  name "iZip"
+  homepage "https://www.izip.com/"
 
-  app 'iZip.app'
+  app "iZip.app"
 end
