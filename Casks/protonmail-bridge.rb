29d9d6eb7dddd6d0618efef1505fcd90f7bf9915
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:00 +0100

    protonmail-bridge: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/protonmail-bridge.rb
+++ Casks/protonmail-bridge.rb
@@ -1,20 +1,20 @@
-cask 'protonmail-bridge' do
-  version '1.2.7'
-  sha256 '85d45e608e0ad8f7d9aba18d58546a958f970f8c564699bdff6aceb554a2f2f7'
+cask "protonmail-bridge" do
+  version "1.2.7"
+  sha256 "85d45e608e0ad8f7d9aba18d58546a958f970f8c564699bdff6aceb554a2f2f7"
 
-  url 'https://protonmail.com/download/Bridge-Installer.dmg'
-  appcast 'https://protonmail.com/download/current_version_darwin.json'
-  name 'ProtonMail Bridge'
-  homepage 'https://protonmail.com/bridge/'
+  url "https://protonmail.com/download/Bridge-Installer.dmg"
+  appcast "https://protonmail.com/download/current_version_darwin.json"
+  name "ProtonMail Bridge"
+  homepage "https://protonmail.com/bridge/"
 
-  app 'ProtonMail Bridge.app'
+  app "ProtonMail Bridge.app"
 
-  uninstall launchctl: 'ProtonMail Bridge',
-            quit:      'com.protonmail.bridge'
+  uninstall launchctl: "ProtonMail Bridge",
+            quit:      "com.protonmail.bridge"
 
   zap trash: [
-               '~/Library/Application Support/protonmail',
-               '~/Library/Caches/protonmail',
-               '~/Library/Caches/ProtonMail Bridge',
-             ]
+    "~/Library/Application Support/protonmail",
+    "~/Library/Caches/protonmail",
+    "~/Library/Caches/ProtonMail Bridge",
+  ]
 end
