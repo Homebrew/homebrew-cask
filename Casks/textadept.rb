01a8b860abd1b26ae40137caf212e39c74483b53
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:37:20 +0100

    textadept: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/textadept.rb
+++ Casks/textadept.rb
@@ -1,11 +1,11 @@
-cask 'textadept' do
-  version '10.8'
-  sha256 '3c5fb990325f4af3ef443a97dad2409fb0986faf1e553b8f87f6d42dfb6fcc24'
+cask "textadept" do
+  version "10.8"
+  sha256 "3c5fb990325f4af3ef443a97dad2409fb0986faf1e553b8f87f6d42dfb6fcc24"
 
   url "https://foicica.com/textadept/download/textadept_#{version}.osx.zip"
-  appcast 'https://foicica.com/textadept/feed'
-  name 'Textadept'
-  homepage 'https://foicica.com/textadept/'
+  appcast "https://foicica.com/textadept/feed"
+  name "Textadept"
+  homepage "https://foicica.com/textadept/"
 
   app "textadept_#{version}.osx/Textadept.app"
   binary "textadept_#{version}.osx/ta"
