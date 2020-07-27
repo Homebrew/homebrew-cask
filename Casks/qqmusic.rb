ecb7a4decaf9cf7339e7d174cbc29f525369ac24
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:24 +0100

    qqmusic: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/qqmusic.rb
+++ Casks/qqmusic.rb
@@ -1,20 +1,20 @@
-cask 'qqmusic' do
+cask "qqmusic" do
   version :latest
   sha256 :no_check
 
-  url 'https://dldir1.qq.com/music/clntupate/mac/QQMusicMac_Mgr.dmg'
-  name 'QQ音乐'
-  homepage 'https://y.qq.com/'
+  url "https://dldir1.qq.com/music/clntupate/mac/QQMusicMac_Mgr.dmg"
+  name "QQ音乐"
+  homepage "https://y.qq.com/"
 
-  app 'QQMusic.app'
+  app "QQMusic.app"
 
-  uninstall quit: 'com.tencent.QQMusicMac'
+  uninstall quit: "com.tencent.QQMusicMac"
 
   zap trash: [
-               '~/Library/Application Support/com.tencent.QQMusicMac',
-               '~/Library/Caches/com.tencent.QQMusicMac',
-               '~/Library/Containers/com.tencent.QQMusicMac',
-               '~/Library/Preferences/com.tencent.QQMusicMac.plist',
-               '~/Library/Saved Application State/com.tencent.QQMusicMac.savedState',
-             ]
+    "~/Library/Application Support/com.tencent.QQMusicMac",
+    "~/Library/Caches/com.tencent.QQMusicMac",
+    "~/Library/Containers/com.tencent.QQMusicMac",
+    "~/Library/Preferences/com.tencent.QQMusicMac.plist",
+    "~/Library/Saved Application State/com.tencent.QQMusicMac.savedState",
+  ]
 end
