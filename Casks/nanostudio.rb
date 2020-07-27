ce7532655d9b4405f13d7601be263d4c12601baf
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:31:29 +0100

    nanostudio: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/nanostudio.rb
+++ Casks/nanostudio.rb
@@ -1,13 +1,13 @@
-cask 'nanostudio' do
-  version '1.45'
-  sha256 '70407ffc54bf5ed2e3c8ec8f1a980739077199a828b8b9d28a2bae04c298856a'
+cask "nanostudio" do
+  version "1.45"
+  sha256 "70407ffc54bf5ed2e3c8ec8f1a980739077199a828b8b9d28a2bae04c298856a"
 
   url "https://www.blipinteractive.co.uk/downloads/NanoStudio-#{version}.dmg.zip"
-  appcast 'https://www.blipinteractive.co.uk/nanostudio1/'
-  name 'NanoStudio'
-  homepage 'https://www.blipinteractive.co.uk/nanostudio1/'
+  appcast "https://www.blipinteractive.co.uk/nanostudio1/"
+  name "NanoStudio"
+  homepage "https://www.blipinteractive.co.uk/nanostudio1/"
 
-  app 'NanoStudio.app'
-  app 'NanoStudio-Phone.app'
-  app 'NanoStudio-Tablet.app'
+  app "NanoStudio.app"
+  app "NanoStudio-Phone.app"
+  app "NanoStudio-Tablet.app"
 end
