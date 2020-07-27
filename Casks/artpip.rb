f3fbd5bb0ffa75066dcd6c74c65c55eeb30b07ce
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:16:39 +0100

    artpip: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/artpip.rb
+++ Casks/artpip.rb
@@ -1,10 +1,10 @@
-cask 'artpip' do
+cask "artpip" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.artpip.com/download/osx'
-  name 'Artpip'
-  homepage 'https://www.artpip.com/'
+  url "https://www.artpip.com/download/osx"
+  name "Artpip"
+  homepage "https://www.artpip.com/"
 
-  app 'Artpip.app'
+  app "Artpip.app"
 end
