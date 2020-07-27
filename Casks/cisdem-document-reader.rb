5c6b126142ca599fc11388453dc74e8603f164e2
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:20:06 +0100

    cisdem-document-reader: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/cisdem-document-reader.rb
+++ Casks/cisdem-document-reader.rb
@@ -1,11 +1,11 @@
-cask 'cisdem-document-reader' do
-  version '5.2.0'
-  sha256 'cd08ba974b7d468751268ccbc42ea00e94c0067a3f7f64d3ea73c2655f44e2de'
+cask "cisdem-document-reader" do
+  version "5.2.0"
+  sha256 "cd08ba974b7d468751268ccbc42ea00e94c0067a3f7f64d3ea73c2655f44e2de"
 
-  url 'http://download.cisdem.com/cisdem-documentreader.dmg'
-  appcast 'https://www.cisdem.com/document-reader-mac/release-notes.html'
-  name 'Cisdem Document Reader'
-  homepage 'https://www.cisdem.com/document-reader-mac.html'
+  url "http://download.cisdem.com/cisdem-documentreader.dmg"
+  appcast "https://www.cisdem.com/document-reader-mac/release-notes.html"
+  name "Cisdem Document Reader"
+  homepage "https://www.cisdem.com/document-reader-mac.html"
 
-  app 'Cisdem Document Reader.app'
+  app "Cisdem Document Reader.app"
 end
