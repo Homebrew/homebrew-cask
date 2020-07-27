b44d32b870acd17042ac59935dc9616d281ba608
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:19 +0100

    qlmobi: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/qlmobi.rb
+++ Casks/qlmobi.rb
@@ -1,11 +1,11 @@
-cask 'qlmobi' do
-  version '0.5'
-  sha256 '1035181685c3ed974bd45ade8f22717c62142ff6a75cf679128f8fb50ef26833'
+cask "qlmobi" do
+  version "0.5"
+  sha256 "1035181685c3ed974bd45ade8f22717c62142ff6a75cf679128f8fb50ef26833"
 
   url "https://github.com/bfabiszewski/QLMobi/releases/download/v#{version}/QLMobi.qlgenerator.zip"
-  appcast 'https://github.com/bfabiszewski/QLMobi/releases.atom'
-  name 'QLMobi'
-  homepage 'https://github.com/bfabiszewski/QLMobi'
+  appcast "https://github.com/bfabiszewski/QLMobi/releases.atom"
+  name "QLMobi"
+  homepage "https://github.com/bfabiszewski/QLMobi"
 
-  qlplugin 'QLMobi.qlgenerator'
+  qlplugin "QLMobi.qlgenerator"
 end
