d1f1ff25ee4d12c10ce4f2ef0f0f2db02af7b643
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:32:15 +0100

    omnigraffle: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/omnigraffle.rb
+++ Casks/omnigraffle.rb
@@ -1,26 +1,26 @@
-cask 'omnigraffle' do
+cask "omnigraffle" do
   if MacOS.version <= :sierra
-    version '7.8.2'
-    sha256 'ab463ea6c12d49c4104d3814ac3280d0359072702d4751f5074f644fc79de0c6'
+    version "7.8.2"
+    sha256 "ab463ea6c12d49c4104d3814ac3280d0359072702d4751f5074f644fc79de0c6"
     url "https://downloads.omnigroup.com/software/Archive/MacOSX/10.12/OmniGraffle-#{version}.dmg"
   elsif MacOS.version <= :high_sierra
-    version '7.11.5'
-    sha256 '83ef24af2dbd7977b9922e992f17f23e102562f0589d28bc37d5579b4a4d4938'
+    version "7.11.5"
+    sha256 "83ef24af2dbd7977b9922e992f17f23e102562f0589d28bc37d5579b4a4d4938"
     url "https://downloads.omnigroup.com/software/MacOSX/10.13/OmniGraffle-#{version}.dmg"
   else
-    version '7.17.1'
-    sha256 'e6df346d43a5df5ca42ba9cf9af6c2584a78c17d28bbdc4ea7727a2ec2ea5927'
+    version "7.17.1"
+    sha256 "e6df346d43a5df5ca42ba9cf9af6c2584a78c17d28bbdc4ea7727a2ec2ea5927"
     url "https://downloads.omnigroup.com/software/MacOSX/10.14/OmniGraffle-#{version}.dmg"
   end
 
   appcast "https://update.omnigroup.com/appcast/com.omnigroup.OmniGraffle#{version.major}"
-  name 'OmniGraffle'
-  homepage 'https://www.omnigroup.com/omnigraffle/'
+  name "OmniGraffle"
+  homepage "https://www.omnigroup.com/omnigraffle/"
 
   auto_updates true
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'OmniGraffle.app'
+  app "OmniGraffle.app"
 
-  zap trash: '~/Library/Application Support/The Omni Group/OmniGraffle'
+  zap trash: "~/Library/Application Support/The Omni Group/OmniGraffle"
 end
