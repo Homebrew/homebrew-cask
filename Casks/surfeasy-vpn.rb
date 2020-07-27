3ee899f47979f601b988fef6b9979a1fb26c247b
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:54 +0100

    surfeasy-vpn: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/surfeasy-vpn.rb
+++ Casks/surfeasy-vpn.rb
@@ -1,10 +1,10 @@
-cask 'surfeasy-vpn' do
+cask "surfeasy-vpn" do
   version :latest
   sha256 :no_check
 
-  url 'https://updates.surfeasy.com/downloads/se0212/SurfEasyVPN-Installer.dmg'
-  name 'SurfEasy VPN'
-  homepage 'https://www.surfeasy.com/'
+  url "https://updates.surfeasy.com/downloads/se0212/SurfEasyVPN-Installer.dmg"
+  name "SurfEasy VPN"
+  homepage "https://www.surfeasy.com/"
 
-  app 'SurfEasy VPN.app'
+  app "SurfEasy VPN.app"
 end
