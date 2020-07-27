64aebbe54acbe06976a126f60f0f3e81423d4eed
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:32:22 +0100

    openaudible: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/openaudible.rb
+++ Casks/openaudible.rb
@@ -1,14 +1,14 @@
-cask 'openaudible' do
-  version '2.2'
-  sha256 '388569c2054184e7e84ffba3174e1ad7e6dca7c531f061153b200871483c4bed'
+cask "openaudible" do
+  version "2.2"
+  sha256 "388569c2054184e7e84ffba3174e1ad7e6dca7c531f061153b200871483c4bed"
 
   # github.com/openaudible/ was verified as official when first introduced to the cask
   url "https://github.com/openaudible/openaudible/releases/download/v#{version}/OpenAudible_#{version}_mac.dmg"
-  appcast 'https://github.com/openaudible/openaudible/releases.atom'
-  name 'OpenAudible'
-  homepage 'https://openaudible.org/'
+  appcast "https://github.com/openaudible/openaudible/releases.atom"
+  name "OpenAudible"
+  homepage "https://openaudible.org/"
 
-  app 'OpenAudible.app'
+  app "OpenAudible.app"
 
-  zap trash: '/Library/OpenAudible'
+  zap trash: "/Library/OpenAudible"
 end
