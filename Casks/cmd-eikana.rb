b477646a852e88c6c00f1f3558d0a95d1d1f325c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:20:31 +0100

    cmd-eikana: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/cmd-eikana.rb
+++ Casks/cmd-eikana.rb
@@ -1,14 +1,14 @@
-cask 'cmd-eikana' do
-  version '2.2.3'
-  sha256 '8e4157304ae21566339e956423632d34aacd12c96e87f35ffc83bf2304ff9be4'
+cask "cmd-eikana" do
+  version "2.2.3"
+  sha256 "8e4157304ae21566339e956423632d34aacd12c96e87f35ffc83bf2304ff9be4"
 
   url "https://github.com/iMasanari/cmd-eikana/releases/download/v#{version}/eikana-#{version}.app.zip"
-  appcast 'https://github.com/iMasanari/cmd-eikana/releases.atom'
-  name 'cmd-eikana'
-  name '⌘英かな'
-  homepage 'https://github.com/iMasanari/cmd-eikana'
+  appcast "https://github.com/iMasanari/cmd-eikana/releases.atom"
+  name "cmd-eikana"
+  name "⌘英かな"
+  homepage "https://github.com/iMasanari/cmd-eikana"
 
-  app '⌘英かな.app'
+  app "⌘英かな.app"
 
-  zap trash: '~/Library/Preferences/io.github.imasanari.cmd-eikana.plist'
+  zap trash: "~/Library/Preferences/io.github.imasanari.cmd-eikana.plist"
 end
