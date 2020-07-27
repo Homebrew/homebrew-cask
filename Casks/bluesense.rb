0e2beae84579c5155a99115c046b805d9e4ecbfd
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:18:28 +0100

    bluesense: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/bluesense.rb
+++ Casks/bluesense.rb
@@ -1,20 +1,20 @@
-cask 'bluesense' do
-  version '1.3.1'
-  sha256 '303f00fc55208ab9340c6142b018e6b6daeef54a7ef4db41fbcb79377866e9a3'
+cask "bluesense" do
+  version "1.3.1"
+  sha256 "303f00fc55208ab9340c6142b018e6b6daeef54a7ef4db41fbcb79377866e9a3"
 
-  url 'https://apps.inspira.io/updates/bluesense.zip'
-  appcast 'https://apps.inspira.io/updates/bluesense-appcast.xml'
-  name 'BlueSense'
-  homepage 'https://apps.inspira.io/bluesense/'
+  url "https://apps.inspira.io/updates/bluesense.zip"
+  appcast "https://apps.inspira.io/updates/bluesense-appcast.xml"
+  name "BlueSense"
+  homepage "https://apps.inspira.io/bluesense/"
 
-  depends_on macos: '>= :yosemite'
+  depends_on macos: ">= :yosemite"
 
-  app 'BlueSense.app'
+  app "BlueSense.app"
 
   zap trash: [
-               '~/Library/Preferences/com.inspira.bluesense.plist',
-               '~/Library/Application Support/BlueSense',
-               '~/Library/Caches/com.inspira.bluesense',
-             ],
-      rmdir: '~/Library/Application Scripts/com.inspira.bluesense'
+    "~/Library/Preferences/com.inspira.bluesense.plist",
+    "~/Library/Application Support/BlueSense",
+    "~/Library/Caches/com.inspira.bluesense",
+  ],
+      rmdir: "~/Library/Application Scripts/com.inspira.bluesense"
 end
