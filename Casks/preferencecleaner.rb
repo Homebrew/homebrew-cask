e3e4709305a760e7ed70158861ce074b9976fa4d
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:49 +0100

    preferencecleaner: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/preferencecleaner.rb
+++ Casks/preferencecleaner.rb
@@ -1,11 +1,11 @@
-cask 'preferencecleaner' do
-  version '2.0'
-  sha256 '800f1df5ca2519eaf66e0efc383a7a3f76018f8f8760058c3cee08b5ec75bd8e'
+cask "preferencecleaner" do
+  version "2.0"
+  sha256 "800f1df5ca2519eaf66e0efc383a7a3f76018f8f8760058c3cee08b5ec75bd8e"
 
   url "https://www.echomist.co.uk/software/downloads/PreferenceCleaner_#{version}.dmg"
-  appcast 'https://www.echomist.co.uk/software/PreferenceCleaner.php'
-  name 'PreferenceCleaner'
-  homepage 'https://www.echomist.co.uk/software/PreferenceCleaner.php'
+  appcast "https://www.echomist.co.uk/software/PreferenceCleaner.php"
+  name "PreferenceCleaner"
+  homepage "https://www.echomist.co.uk/software/PreferenceCleaner.php"
 
   app "PreferenceCleaner #{version.major}.app"
 end
