920cb02519198934f475cbac77c66dd2ca60d597
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:37:16 +0100

    telegram-desktop: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/telegram-desktop.rb
+++ Casks/telegram-desktop.rb
@@ -1,22 +1,22 @@
-cask 'telegram-desktop' do
-  version '2.2.0'
-  sha256 '918a4986ab6609492f4eba53709c6563224f1251f940b7f0c98f09ff9772c70b'
+cask "telegram-desktop" do
+  version "2.2.0"
+  sha256 "918a4986ab6609492f4eba53709c6563224f1251f940b7f0c98f09ff9772c70b"
 
   # github.com/telegramdesktop/tdesktop/ was verified as official when first introduced to the cask
   url "https://github.com/telegramdesktop/tdesktop/releases/download/v#{version}/tsetup.#{version}.dmg"
-  appcast 'https://github.com/telegramdesktop/tdesktop/releases.atom'
-  name 'Telegram Desktop'
-  homepage 'https://desktop.telegram.org/'
+  appcast "https://github.com/telegramdesktop/tdesktop/releases.atom"
+  name "Telegram Desktop"
+  homepage "https://desktop.telegram.org/"
 
   auto_updates true
-  conflicts_with cask: 'telegram-desktop-dev'
+  conflicts_with cask: "telegram-desktop-dev"
 
   # Renamed to avoid conflict with telegram
-  app 'Telegram.app', target: 'Telegram Desktop.app'
+  app "Telegram.app", target: "Telegram Desktop.app"
 
   zap trash: [
-               '~/Library/Application Support/Telegram Desktop',
-               '~/Library/Preferences/com.tdesktop.Telegram.plist',
-               '~/Library/Saved Application State/com.tdesktop.Telegram.savedState',
-             ]
+    "~/Library/Application Support/Telegram Desktop",
+    "~/Library/Preferences/com.tdesktop.Telegram.plist",
+    "~/Library/Saved Application State/com.tdesktop.Telegram.savedState",
+  ]
 end
