eb652ab6e1b68ba758bffda72ff027be1fdcc511
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:15 +0100

    rsyncosx: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/rsyncosx.rb
+++ Casks/rsyncosx.rb
@@ -1,17 +1,17 @@
-cask 'rsyncosx' do
-  version '6.4.0'
-  sha256 '4c02aa7c6e01083fd45d939ca97df19a18c46e506bb407fa4cc8ed22f1bc884b'
+cask "rsyncosx" do
+  version "6.4.0"
+  sha256 "4c02aa7c6e01083fd45d939ca97df19a18c46e506bb407fa4cc8ed22f1bc884b"
 
   url "https://github.com/rsyncOSX/RsyncOSX/releases/download/v#{version}/RsyncOSX.#{version}.dmg"
-  appcast 'https://github.com/rsyncOSX/RsyncOSX/releases.atom'
-  name 'RsyncOSX'
-  homepage 'https://github.com/rsyncOSX/RsyncOSX'
+  appcast "https://github.com/rsyncOSX/RsyncOSX/releases.atom"
+  name "RsyncOSX"
+  homepage "https://github.com/rsyncOSX/RsyncOSX"
 
-  app 'RsyncOSX.app'
+  app "RsyncOSX.app"
 
   zap trash: [
-               '~/Library/Caches/no.blogspot.RsyncOSX',
-               '~/Library/Preferences/no.blogspot.RsyncOSX.plist',
-               '~/Library/Saved Application State/no.blogspot.RsyncOSX.savedState',
-             ]
+    "~/Library/Caches/no.blogspot.RsyncOSX",
+    "~/Library/Preferences/no.blogspot.RsyncOSX.plist",
+    "~/Library/Saved Application State/no.blogspot.RsyncOSX.savedState",
+  ]
 end
