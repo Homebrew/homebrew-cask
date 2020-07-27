d30e390a8c964117fd3764f8ec7b9eeb1d45fb52
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:38:45 +0100

    wireframe-sketcher: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/wireframe-sketcher.rb
+++ Casks/wireframe-sketcher.rb
@@ -1,10 +1,10 @@
-cask 'wireframe-sketcher' do
+cask "wireframe-sketcher" do
   version :latest
   sha256 :no_check
 
-  url 'https://wireframesketcher.com/downloads/WireframeSketcher-latest-macosx.zip'
-  name 'WireframeSketcher'
-  homepage 'https://wireframesketcher.com/'
+  url "https://wireframesketcher.com/downloads/WireframeSketcher-latest-macosx.zip"
+  name "WireframeSketcher"
+  homepage "https://wireframesketcher.com/"
 
-  app 'WireframeSketcher.app'
+  app "WireframeSketcher.app"
 end
