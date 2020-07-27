2a969332113ed0af25d071bfe554863ea7df3df7
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:19:53 +0100

    chirp: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/chirp.rb
+++ Casks/chirp.rb
@@ -1,13 +1,13 @@
-cask 'chirp' do
-  version '20200718'
-  sha256 'ba8535fbab8b94ca3c8987cd69b11cc72a8ea102c2d14549a49431d99b4e5e61'
+cask "chirp" do
+  version "20200718"
+  sha256 "ba8535fbab8b94ca3c8987cd69b11cc72a8ea102c2d14549a49431d99b4e5e61"
 
   url "https://trac.chirp.danplanet.com/chirp_daily/LATEST/chirp-unified-daily-#{version}.app.zip"
-  appcast 'https://trac.chirp.danplanet.com/chirp_daily/LATEST/SHA1SUM'
-  name 'CHIRP'
-  homepage 'https://chirp.danplanet.com/projects/chirp/wiki/Home'
+  appcast "https://trac.chirp.danplanet.com/chirp_daily/LATEST/SHA1SUM"
+  name "CHIRP"
+  homepage "https://chirp.danplanet.com/projects/chirp/wiki/Home"
 
-  depends_on cask: 'kk7ds-python-runtime'
+  depends_on cask: "kk7ds-python-runtime"
 
-  app 'CHIRP.app'
+  app "CHIRP.app"
 end
