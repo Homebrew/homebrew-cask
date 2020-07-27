8490c04849e3422d203d01945960630b23d701df
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:41 +0100

    setapp: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/setapp.rb
+++ Casks/setapp.rb
@@ -1,29 +1,29 @@
-cask 'setapp' do
-  version '2.5.7,1586177549'
-  sha256 'c4ee02059b5a239631f5b935f2b6c72b3711309138ae4c8dba4421acbed51fdc'
+cask "setapp" do
+  version "2.5.7,1586177549"
+  sha256 "c4ee02059b5a239631f5b935f2b6c72b3711309138ae4c8dba4421acbed51fdc"
 
   # devmate.com/com.setapp.InstallSetapp/ was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.setapp.InstallSetapp/#{version.before_comma}/#{version.after_comma}/InstallSetapp-#{version.before_comma}.zip"
-  appcast 'https://updates.devmate.com/com.setapp.InstallSetapp.xml'
-  name 'Setapp'
-  homepage 'https://setapp.com/'
+  appcast "https://updates.devmate.com/com.setapp.InstallSetapp.xml"
+  name "Setapp"
+  homepage "https://setapp.com/"
 
   auto_updates true
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  installer manual: 'Install Setapp.app'
+  installer manual: "Install Setapp.app"
 
   uninstall script: {
-                      executable: "#{appdir}/Setapp.app/Contents/Resources/SetappUninstaller.app/Contents/Resources/removeSetapp.sh",
-                      sudo:       true,
-                    }
+    executable: "#{appdir}/Setapp.app/Contents/Resources/SetappUninstaller.app/Contents/Resources/removeSetapp.sh",
+    sudo:       true,
+  }
 
   zap trash: [
-               '~/Library/Application Scripts/com.setapp.DesktopClient.SetappAgent.FinderSyncExt',
-               '~/Library/Caches/com.setapp.DesktopClient',
-               '~/Library/Caches/com.setapp.DesktopClient.SetappAgent',
-               '~/Library/Logs/Setapp',
-               '~/Library/Preferences/com.setapp.DesktopClient.SetappAgent.plist',
-               '~/Library/Saved Application State/com.setapp.DesktopClient.savedState',
-             ]
+    "~/Library/Application Scripts/com.setapp.DesktopClient.SetappAgent.FinderSyncExt",
+    "~/Library/Caches/com.setapp.DesktopClient",
+    "~/Library/Caches/com.setapp.DesktopClient.SetappAgent",
+    "~/Library/Logs/Setapp",
+    "~/Library/Preferences/com.setapp.DesktopClient.SetappAgent.plist",
+    "~/Library/Saved Application State/com.setapp.DesktopClient.savedState",
+  ]
 end
