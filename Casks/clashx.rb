3bcc1920dfcdcf59a0a4d5283f2391bdeab84dfd
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:20:12 +0100

    clashx: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/clashx.rb
+++ Casks/clashx.rb
@@ -1,20 +1,20 @@
-cask 'clashx' do
-  version '1.20.4'
-  sha256 'deb0ab3b8df928c058123dd935d063f238701803d691aba1ea5ac22963fab161'
+cask "clashx" do
+  version "1.20.4"
+  sha256 "deb0ab3b8df928c058123dd935d063f238701803d691aba1ea5ac22963fab161"
 
   url "https://github.com/yichengchen/clashX/releases/download/#{version}/ClashX.dmg"
-  appcast 'https://github.com/yichengchen/clashX/releases.atom'
-  name 'ClashX'
-  homepage 'https://github.com/yichengchen/clashX'
+  appcast "https://github.com/yichengchen/clashX/releases.atom"
+  name "ClashX"
+  homepage "https://github.com/yichengchen/clashX"
 
   auto_updates true
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'ClashX.app'
+  app "ClashX.app"
 
   zap trash: [
-               '~/Library/Preferences/com.west2online.ClashX.plist',
-               '~/Library/Logs/ClashX',
-               '~/.config/clash/',
-             ]
+    "~/Library/Preferences/com.west2online.ClashX.plist",
+    "~/Library/Logs/ClashX",
+    "~/.config/clash/",
+  ]
 end
