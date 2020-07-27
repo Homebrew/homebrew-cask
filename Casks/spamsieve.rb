aa80fe3f938bec0d1ef0da26e8766fd6b788387d
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:23 +0100

    spamsieve: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/spamsieve.rb
+++ Casks/spamsieve.rb
@@ -1,11 +1,11 @@
-cask 'spamsieve' do
-  version '2.9.39'
-  sha256 'a21ebca717449924d29eb20c33ccab96e662d4d77be903b9f8e0ea0f0d4fd7bb'
+cask "spamsieve" do
+  version "2.9.39"
+  sha256 "a21ebca717449924d29eb20c33ccab96e662d4d77be903b9f8e0ea0f0d4fd7bb"
 
   url "https://c-command.com/downloads/SpamSieve-#{version}.dmg"
-  appcast 'https://c-command.com/spamsieve/'
-  name 'SpamSieve'
-  homepage 'https://c-command.com/spamsieve/'
+  appcast "https://c-command.com/spamsieve/"
+  name "SpamSieve"
+  homepage "https://c-command.com/spamsieve/"
 
-  app 'SpamSieve.app'
+  app "SpamSieve.app"
 end
