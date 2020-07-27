97df05f438a4a59a4ca37f0f182732c95400c488
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:23:22 +0100

    envkey: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/envkey.rb
+++ Casks/envkey.rb
@@ -1,12 +1,12 @@
-cask 'envkey' do
-  version '1.4.17'
-  sha256 'b50025182856e915da8742394045a7c8c373d237fa0a25567642150db0f14346'
+cask "envkey" do
+  version "1.4.17"
+  sha256 "b50025182856e915da8742394045a7c8c373d237fa0a25567642150db0f14346"
 
   # github.com/envkey/envkey-app/ was verified as official when first introduced to the cask
   url "https://github.com/envkey/envkey-app/releases/download/darwin-x64-prod-v#{version}/EnvKey-#{version}-mac.zip"
-  appcast 'https://github.com/envkey/envkey-app/releases.atom'
-  name 'EnvKey'
-  homepage 'https://www.envkey.com/'
+  appcast "https://github.com/envkey/envkey-app/releases.atom"
+  name "EnvKey"
+  homepage "https://www.envkey.com/"
 
-  app 'EnvKey.app'
+  app "EnvKey.app"
 end
