10fe5a315f70ee5f0d40733c71b37fbe7ffb14c9
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:27:01 +0100

    inssider: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/inssider.rb
+++ Casks/inssider.rb
@@ -1,12 +1,12 @@
-cask 'inssider' do
-  version '0.0.2.14,5'
-  sha256 '8d995306facb7d1341a54521a7cd8e76290ff44763fa00a7dfca85932f82a524'
+cask "inssider" do
+  version "0.0.2.14,5"
+  sha256 "8d995306facb7d1341a54521a7cd8e76290ff44763fa00a7dfca85932f82a524"
 
   # nyc3.digitaloceanspaces.com/Installers/Mac/ was verified as official when first introduced to the cask
   url "https://metageek-desktop.nyc3.digitaloceanspaces.com/Installers/Mac/inSSIDer-#{version.after_comma}/inSSIDer.dmg"
   appcast "https://metageek-desktop.nyc3.digitaloceanspaces.com/Installers/Mac/inSSIDer-#{version.after_comma}/inssider_#{version.after_comma}_mac_appcast.xml"
-  name 'inSSIDer'
-  homepage 'https://www.metageek.com/products/inssider/'
+  name "inSSIDer"
+  homepage "https://www.metageek.com/products/inssider/"
 
-  app 'inSSIDer.app'
+  app "inSSIDer.app"
 end
