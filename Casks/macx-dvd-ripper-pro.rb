0d02c41945454c40d96279b1486d49a0c88d0711
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:39 +0100

    macx-dvd-ripper-pro: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/macx-dvd-ripper-pro.rb
+++ Casks/macx-dvd-ripper-pro.rb
@@ -1,11 +1,11 @@
-cask 'macx-dvd-ripper-pro' do
-  version '6.5.3,20200630'
-  sha256 '147b66ed1946dea61bb15a08b0c4e92875f82ed9e838e4db1495acf1180019cb'
+cask "macx-dvd-ripper-pro" do
+  version "6.5.3,20200630"
+  sha256 "147b66ed1946dea61bb15a08b0c4e92875f82ed9e838e4db1495acf1180019cb"
 
-  url 'https://www.macxdvd.com/download/macx-dvd-ripper-pro.dmg'
-  appcast 'https://www.macxdvd.com/mac-dvd-ripper-pro/upgrade/macx-dvd-ripper-pro'
-  name 'MacX DVD Ripper Pro'
-  homepage 'https://www.macxdvd.com/mac-dvd-ripper-pro/'
+  url "https://www.macxdvd.com/download/macx-dvd-ripper-pro.dmg"
+  appcast "https://www.macxdvd.com/mac-dvd-ripper-pro/upgrade/macx-dvd-ripper-pro"
+  name "MacX DVD Ripper Pro"
+  homepage "https://www.macxdvd.com/mac-dvd-ripper-pro/"
 
-  app 'MacX DVD Ripper Pro.app'
+  app "MacX DVD Ripper Pro.app"
 end
