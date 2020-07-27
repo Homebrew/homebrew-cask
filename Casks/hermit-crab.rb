70ddec38511097cd79ea46fa35a1c2cd648a1c3b
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:26:16 +0100

    hermit-crab: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/hermit-crab.rb
+++ Casks/hermit-crab.rb
@@ -1,17 +1,17 @@
-cask 'hermit-crab' do
-  version '1.0.2'
-  sha256 '5e33cc918264fb39c97eb256e899f971a504f89b907429627e15f4b8ba1d8ada'
+cask "hermit-crab" do
+  version "1.0.2"
+  sha256 "5e33cc918264fb39c97eb256e899f971a504f89b907429627e15f4b8ba1d8ada"
 
   url "https://belkadan.com/hermitcrab/updates/HermitCrab-#{version}.zip"
-  appcast 'https://belkadan.com/hermitcrab/updates/'
-  name 'Hermit Crab'
-  homepage 'https://belkadan.com/hermitcrab/'
+  appcast "https://belkadan.com/hermitcrab/updates/"
+  name "Hermit Crab"
+  homepage "https://belkadan.com/hermitcrab/"
 
   auto_updates true
 
-  app 'Hermit Crab.app'
+  app "Hermit Crab.app"
 
-  uninstall quit: 'com.belkadan.Hermit-Crab'
+  uninstall quit: "com.belkadan.Hermit-Crab"
 
-  zap trash: '~/Library/Preferences/com.belkadan.Hermit-Crab.plist'
+  zap trash: "~/Library/Preferences/com.belkadan.Hermit-Crab.plist"
 end
