bf44dc65f954a982fea294d52ee85a6dd2293827
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:38:38 +0100

    wechat: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/wechat.rb
+++ Casks/wechat.rb
@@ -1,26 +1,26 @@
-cask 'wechat' do
-  version '2.4.2.17'
-  sha256 '5af7a2d825de0e6be2186b054c70e287583f1d68df95af2be23eb5e27025a6f0'
+cask "wechat" do
+  version "2.4.2.17"
+  sha256 "5af7a2d825de0e6be2186b054c70e287583f1d68df95af2be23eb5e27025a6f0"
 
-  url 'https://dldir1.qq.com/weixin/mac/WeChatMac.dmg'
-  appcast 'https://dldir1.qq.com/weixin/mac/mac-release.xml'
-  name 'WeChat for Mac'
-  name '微信 Mac 版'
-  homepage 'https://mac.weixin.qq.com/'
+  url "https://dldir1.qq.com/weixin/mac/WeChatMac.dmg"
+  appcast "https://dldir1.qq.com/weixin/mac/mac-release.xml"
+  name "WeChat for Mac"
+  name "微信 Mac 版"
+  homepage "https://mac.weixin.qq.com/"
 
   auto_updates true
-  depends_on macos: '>= :yosemite'
+  depends_on macos: ">= :yosemite"
 
-  app 'WeChat.app'
+  app "WeChat.app"
 
-  uninstall quit: 'com.tencent.xinWeChat'
+  uninstall quit: "com.tencent.xinWeChat"
 
   zap trash: [
-               '~/Library/Application Scripts/com.tencent.xinWeChat',
-               '~/Library/Caches/com.tencent.xinWeChat',
-               '~/Library/Containers/com.tencent.xinWeChat',
-               '~/Library/Cookies/com.tencent.xinWeChat.binarycookies',
-               '~/Library/Group Containers/$(TeamIdentifierPrefix)com.tencent.xinWeChat',
-               '~/Library/Preferences/com.tencent.xinWeChat.plist',
-             ]
+    "~/Library/Application Scripts/com.tencent.xinWeChat",
+    "~/Library/Caches/com.tencent.xinWeChat",
+    "~/Library/Containers/com.tencent.xinWeChat",
+    "~/Library/Cookies/com.tencent.xinWeChat.binarycookies",
+    "~/Library/Group Containers/$(TeamIdentifierPrefix)com.tencent.xinWeChat",
+    "~/Library/Preferences/com.tencent.xinWeChat.plist",
+  ]
 end
