6f74a56def5ca595f22ea68f4a4b8d8404a066a3
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:31:51 +0100

    noisy: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/noisy.rb
+++ Casks/noisy.rb
@@ -1,10 +1,10 @@
-cask 'noisy' do
+cask "noisy" do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/downloads/jonshea/Noisy/Noisy.zip'
-  name 'Noisy'
-  homepage 'https://github.com/jonshea/Noisy'
+  url "https://github.com/downloads/jonshea/Noisy/Noisy.zip"
+  name "Noisy"
+  homepage "https://github.com/jonshea/Noisy"
 
-  app 'Noisy.app'
+  app "Noisy.app"
 end
