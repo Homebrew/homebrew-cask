7118a384d51a56515b2de3f5e7117cff89459a4e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:22:05 +0100

    digikam: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/digikam.rb
+++ Casks/digikam.rb
@@ -1,15 +1,15 @@
-cask 'digikam' do
-  version '7.0.0'
-  sha256 '8b5a4abbf0c5c51b429b14c8f37cd269ed66569614faaa0bc09ead3f577122a5'
+cask "digikam" do
+  version "7.0.0"
+  sha256 "8b5a4abbf0c5c51b429b14c8f37cd269ed66569614faaa0bc09ead3f577122a5"
 
   # kde.org/stable/digikam/ was verified as official when first introduced to the cask
   url "https://download.kde.org/stable/digikam/#{version}/digiKam-#{version}-MacOS-x86-64.pkg"
-  appcast 'https://download.kde.org/stable/digikam/'
-  name 'digiKam'
-  homepage 'https://www.digikam.org/'
+  appcast "https://download.kde.org/stable/digikam/"
+  name "digiKam"
+  homepage "https://www.digikam.org/"
 
   pkg "digiKam-#{version}-MacOS-x86-64.pkg"
 
-  uninstall pkgutil: 'org.freedesktop.digikam',
-            delete:  '/Applications/digiKam'
+  uninstall pkgutil: "org.freedesktop.digikam",
+            delete:  "/Applications/digiKam"
 end
