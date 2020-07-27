a95edf4ec8fc94dc21a8c4142c4c924baa19a602
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:25:19 +0100

    github: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/github.rb
+++ Casks/github.rb
@@ -1,29 +1,29 @@
-cask 'github' do
-  version '2.5.3-f26bc4fa'
-  sha256 '88e682ab30ebc8001ca1afb9f866be37de5745b64fcbb62b57ea85304c5789c0'
+cask "github" do
+  version "2.5.3-f26bc4fa"
+  sha256 "88e682ab30ebc8001ca1afb9f866be37de5745b64fcbb62b57ea85304c5789c0"
 
   # githubusercontent.com/ was verified as official when first introduced to the cask
   url "https://desktop.githubusercontent.com/releases/#{version}/GitHubDesktop.zip"
-  appcast 'https://github.com/desktop/desktop/releases.atom'
-  name 'GitHub Desktop'
-  homepage 'https://desktop.github.com/'
+  appcast "https://github.com/desktop/desktop/releases.atom"
+  name "GitHub Desktop"
+  homepage "https://desktop.github.com/"
 
   auto_updates true
 
-  app 'GitHub Desktop.app'
-  binary "#{appdir}/GitHub Desktop.app/Contents/Resources/app/static/github.sh", target: 'github'
+  app "GitHub Desktop.app"
+  binary "#{appdir}/GitHub Desktop.app/Contents/Resources/app/static/github.sh", target: "github"
 
   zap trash: [
-               '~/Library/Application Support/GitHub Desktop',
-               '~/Library/Application Support/com.github.GitHubClient',
-               '~/Library/Application Support/com.github.GitHubClient.ShipIt',
-               '~/Library/Application Support/ShipIt_stderr.log',
-               '~/Library/Application Support/ShipIt_stdout.log',
-               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.github.GitHubClient.sfl*',
-               '~/Library/Caches/com.github.GitHubClient',
-               '~/Library/Caches/com.github.GitHubClient.ShipIt',
-               '~/Library/Preferences/com.github.GitHubClient.helper.plist',
-               '~/Library/Preferences/com.github.GitHubClient.plist',
-             ],
-      rmdir: '~/.config/git'
+    "~/Library/Application Support/GitHub Desktop",
+    "~/Library/Application Support/com.github.GitHubClient",
+    "~/Library/Application Support/com.github.GitHubClient.ShipIt",
+    "~/Library/Application Support/ShipIt_stderr.log",
+    "~/Library/Application Support/ShipIt_stdout.log",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.github.GitHubClient.sfl*",
+    "~/Library/Caches/com.github.GitHubClient",
+    "~/Library/Caches/com.github.GitHubClient.ShipIt",
+    "~/Library/Preferences/com.github.GitHubClient.helper.plist",
+    "~/Library/Preferences/com.github.GitHubClient.plist",
+  ],
+      rmdir: "~/.config/git"
 end
