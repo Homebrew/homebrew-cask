e9abf34c613662cd1dad0c9ba68553484057fb35
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:53 +0100

    printrun: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/printrun.rb
+++ Casks/printrun.rb
@@ -1,11 +1,11 @@
-cask 'printrun' do
-  version '1.6.0,18Nov2017'
-  sha256 '426229043a2a33a6768dcca12135f0751c007595af078665eb854cf87e4e2999'
+cask "printrun" do
+  version "1.6.0,18Nov2017"
+  sha256 "426229043a2a33a6768dcca12135f0751c007595af078665eb854cf87e4e2999"
 
   url "https://github.com/kliment/Printrun/releases/download/printrun-#{version.before_comma}/Printrun-Mac-#{version.after_comma}.zip"
-  appcast 'https://github.com/kliment/Printrun/releases.atom'
-  name 'Printrun'
-  homepage 'https://github.com/kliment/Printrun'
+  appcast "https://github.com/kliment/Printrun/releases.atom"
+  name "Printrun"
+  homepage "https://github.com/kliment/Printrun"
 
   app "Printrun-Mac-#{version.after_comma}.app"
 end
