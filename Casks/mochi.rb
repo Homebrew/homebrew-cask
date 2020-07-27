84da862bfe854819267eab5e203f47ee03d32ca6
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:30:49 +0100

    mochi: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/mochi.rb
+++ Casks/mochi.rb
@@ -1,11 +1,11 @@
-cask 'mochi' do
-  version '1.6.1'
-  sha256 '300f81f5dd367575408e9639326a3f37801bbba5017eb5f63ad4da8cee52d8fb'
+cask "mochi" do
+  version "1.6.1"
+  sha256 "300f81f5dd367575408e9639326a3f37801bbba5017eb5f63ad4da8cee52d8fb"
 
   url "https://mochi.cards/releases/Mochi-#{version}.dmg"
-  appcast 'https://mochi.cards/'
-  name 'Mochi'
-  homepage 'https://mochi.cards/'
+  appcast "https://mochi.cards/"
+  name "Mochi"
+  homepage "https://mochi.cards/"
 
-  app 'Mochi.app'
+  app "Mochi.app"
 end
