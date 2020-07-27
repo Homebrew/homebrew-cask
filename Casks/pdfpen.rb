9424bf2da8712b5e4e3446133dbd83d4fe298d59
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:07 +0100

    pdfpen: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/pdfpen.rb
+++ Casks/pdfpen.rb
@@ -1,13 +1,13 @@
-cask 'pdfpen' do
-  version '1212.1,1594827478'
-  sha256 'bbbdd5f1e0a9e391f05063443ffa66c5e9d3378701a89b43a1503c6bc8c49223'
+cask "pdfpen" do
+  version "1212.1,1594827478"
+  sha256 "bbbdd5f1e0a9e391f05063443ffa66c5e9d3378701a89b43a1503c6bc8c49223"
 
   url "https://dl.smilesoftware.com/com.smileonmymac.PDFpen/#{version.before_comma}/#{version.after_comma}/PDFpen-#{version.before_comma}.zip"
-  appcast 'https://updates.smilesoftware.com/com.smileonmymac.PDFpen.xml'
-  name 'PDFpen'
-  homepage 'https://smilesoftware.com/PDFpen'
+  appcast "https://updates.smilesoftware.com/com.smileonmymac.PDFpen.xml"
+  name "PDFpen"
+  homepage "https://smilesoftware.com/PDFpen"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'PDFpen.app'
+  app "PDFpen.app"
 end
