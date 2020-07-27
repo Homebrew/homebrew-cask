bc6213055b7a91302ef7a6c752ddfc913b1e8495
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:19:47 +0100

    cheatsheet: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/cheatsheet.rb
+++ Casks/cheatsheet.rb
@@ -1,18 +1,18 @@
-cask 'cheatsheet' do
-  version '1.5.1'
-  sha256 '01148199feabc5cb592c37f7594bd0d24fac786270ea00431293dcac098b1461'
+cask "cheatsheet" do
+  version "1.5.1"
+  sha256 "01148199feabc5cb592c37f7594bd0d24fac786270ea00431293dcac098b1461"
 
   # mediaatelier.com/CheatSheet/ was verified as official when first introduced to the cask
   url "https://mediaatelier.com/CheatSheet/CheatSheet_#{version}.zip"
-  appcast 'https://mediaatelier.com/CheatSheet/feed.php'
-  name 'CheatSheet'
-  homepage 'https://www.cheatsheetapp.com/CheatSheet/'
+  appcast "https://mediaatelier.com/CheatSheet/feed.php"
+  name "CheatSheet"
+  homepage "https://www.cheatsheetapp.com/CheatSheet/"
 
-  app 'CheatSheet.app'
+  app "CheatSheet.app"
 
   zap trash: [
-               '~/Library/Application Support/com.mediaatelier.CheatSheet',
-               '~/Library/Caches/com.mediaatelier.CheatSheet',
-               '~/Library/Preferences/com.mediaatelier.CheatSheet.plist',
-             ]
+    "~/Library/Application Support/com.mediaatelier.CheatSheet",
+    "~/Library/Caches/com.mediaatelier.CheatSheet",
+    "~/Library/Preferences/com.mediaatelier.CheatSheet.plist",
+  ]
 end
