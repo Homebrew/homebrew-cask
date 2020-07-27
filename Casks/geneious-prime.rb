ab92bc41350675412a98de2272fb671c1e2184f4
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:25:02 +0100

    geneious-prime: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/geneious-prime.rb
+++ Casks/geneious-prime.rb
@@ -1,11 +1,11 @@
-cask 'geneious-prime' do
-  version '2020.2.2'
-  sha256 '056c6f05c49e494a2e7b7011e43aa993e545e26c3d20d948b26beffbd550da02'
+cask "geneious-prime" do
+  version "2020.2.2"
+  sha256 "056c6f05c49e494a2e7b7011e43aa993e545e26c3d20d948b26beffbd550da02"
 
   url "https://assets.geneious.com/installers/geneious/release/Geneious_Prime_mac64_#{version.dots_to_underscores}_with_jre.dmg"
-  appcast 'https://www.geneious.com/download/'
-  name 'Geneious Prime'
-  homepage 'https://www.geneious.com/'
+  appcast "https://www.geneious.com/download/"
+  name "Geneious Prime"
+  homepage "https://www.geneious.com/"
 
-  app 'Geneious Prime.app'
+  app "Geneious Prime.app"
 end
