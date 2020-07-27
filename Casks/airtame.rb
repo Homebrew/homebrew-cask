74e6f83864d6a1db7ac0a648793456656d809816
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:15:24 +0100

    airtame: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/airtame.rb
+++ Casks/airtame.rb
@@ -1,13 +1,13 @@
-cask 'airtame' do
-  version '4.1.0'
-  sha256 'aa90d0e147f9db6159ed21ee81e2141833fdcf059796a3ec93a2a63d247a3f23'
+cask "airtame" do
+  version "4.1.0"
+  sha256 "aa90d0e147f9db6159ed21ee81e2141833fdcf059796a3ec93a2a63d247a3f23"
 
   url "https://downloads-cdn.airtame.com/app/latest/mac/Airtame-#{version}.dmg"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://downloads-cdn.airtame.com/get.php?platform=osx_x64'
-  name 'Airtame'
-  homepage 'https://airtame.com/'
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://downloads-cdn.airtame.com/get.php?platform=osx_x64"
+  name "Airtame"
+  homepage "https://airtame.com/"
 
   auto_updates true
 
-  app 'Airtame.app'
+  app "Airtame.app"
 end
