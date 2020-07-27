51a2d76a3aec661e44004ad36587d99ca4ec1110
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:26:07 +0100

    happygrep: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/happygrep.rb
+++ Casks/happygrep.rb
@@ -1,11 +1,11 @@
-cask 'happygrep' do
-  version '1.0'
-  sha256 '05c5f33142c9ea4559b20ca421c76fa0b081ae3edc0d6e3b8f7c3dd8ba21a518'
+cask "happygrep" do
+  version "1.0"
+  sha256 "05c5f33142c9ea4559b20ca421c76fa0b081ae3edc0d6e3b8f7c3dd8ba21a518"
 
   url "https://github.com/happypeter/happygrep/releases/download/v#{version}/happygrep.zip"
-  appcast 'https://github.com/happypeter/happygrep/releases.atom'
-  name 'happygrep'
-  homepage 'https://github.com/happypeter/happygrep'
+  appcast "https://github.com/happypeter/happygrep/releases.atom"
+  name "happygrep"
+  homepage "https://github.com/happypeter/happygrep"
 
-  binary 'happygrep'
+  binary "happygrep"
 end
