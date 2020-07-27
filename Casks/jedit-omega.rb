90d4ae01603298a8cf1d6fccdc8e808841ed07b3
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:27:44 +0100

    jedit-omega: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/jedit-omega.rb
+++ Casks/jedit-omega.rb
@@ -1,17 +1,17 @@
-cask 'jedit-omega' do
+cask "jedit-omega" do
   if MacOS.version <= :el_capitan
-    version '1.32'
-    sha256 '251697fe6b76419b2cef41a89a2cb7d2e3f8caab0bc1ae82aed6c14a366fdad9'
+    version "1.32"
+    sha256 "251697fe6b76419b2cef41a89a2cb7d2e3f8caab0bc1ae82aed6c14a366fdad9"
   else
-    version '2.25'
-    sha256 '3496048ec1caea18618385507d7b4039d34fbf28f65abc947cd78284758c8ade'
+    version "2.25"
+    sha256 "3496048ec1caea18618385507d7b4039d34fbf28f65abc947cd78284758c8ade"
   end
 
   # artman21.co.jp/ was verified as official when first introduced to the cask
   url "http://download.artman21.co.jp/product/JeditOmega/JeditOmega#{version.no_dots}.dmg"
-  appcast 'http://www.artman21.com/en/jeditOmega/history.html'
-  name 'Jedit Ω'
-  homepage 'http://www.artman21.com/en/jeditOmega/'
+  appcast "http://www.artman21.com/en/jeditOmega/history.html"
+  name "Jedit Ω"
+  homepage "http://www.artman21.com/en/jeditOmega/"
 
-  app 'Jedit Ω.app'
+  app "Jedit Ω.app"
 end
