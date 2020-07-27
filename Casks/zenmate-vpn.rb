0382f02b820779b568a29c5703a3e9e2645f5f53
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:39:07 +0100

    zenmate-vpn: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/zenmate-vpn.rb
+++ Casks/zenmate-vpn.rb
@@ -1,13 +1,13 @@
-cask 'zenmate-vpn' do
-  version '5.1.0.21'
-  sha256 'aba2b4714517e55c852d21e217690de14e1b9b623e50a1e9516398f49978c85d'
+cask "zenmate-vpn" do
+  version "5.1.0.21"
+  sha256 "aba2b4714517e55c852d21e217690de14e1b9b623e50a1e9516398f49978c85d"
 
   url "https://download.zenmate.com/mac/#{version.major}/zm_#{version}.dmg"
-  appcast 'https://download.zenmate.com/mac/zenmate.xml'
-  name 'ZenMate VPN'
-  homepage 'https://zenmate.com/products/vpn-for-osx/'
+  appcast "https://download.zenmate.com/mac/zenmate.xml"
+  name "ZenMate VPN"
+  homepage "https://zenmate.com/products/vpn-for-osx/"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'ZenMate VPN.app'
+  app "ZenMate VPN.app"
 end
