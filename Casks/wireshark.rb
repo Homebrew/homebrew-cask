aa30814c0f334c23772f1e1458d2b7663056dd1b
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:38:45 +0100

    wireshark: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/wireshark.rb
+++ Casks/wireshark.rb
@@ -1,42 +1,42 @@
-cask 'wireshark' do
-  version '3.2.5'
-  sha256 'c95277ecc61c22b900591d07dae94ef659fe1d80d584479ddc46855b4e3a3745'
+cask "wireshark" do
+  version "3.2.5"
+  sha256 "c95277ecc61c22b900591d07dae94ef659fe1d80d584479ddc46855b4e3a3745"
 
   url "https://www.wireshark.org/download/osx/Wireshark%20#{version}%20Intel%2064.dmg"
-  appcast 'https://www.wireshark.org/update/0/Wireshark/0.0.0/macOS/x86-64/en-US/stable.xml'
-  name 'Wireshark'
-  homepage 'https://www.wireshark.org/'
+  appcast "https://www.wireshark.org/update/0/Wireshark/0.0.0/macOS/x86-64/en-US/stable.xml"
+  name "Wireshark"
+  homepage "https://www.wireshark.org/"
 
   auto_updates true
-  conflicts_with cask: 'wireshark-chmodbpf'
-  depends_on macos: '>= :sierra'
+  conflicts_with cask: "wireshark-chmodbpf"
+  depends_on macos: ">= :sierra"
 
-  app 'Wireshark.app'
-  pkg 'Install ChmodBPF.pkg'
-  pkg 'Add Wireshark to the system path.pkg'
+  app "Wireshark.app"
+  pkg "Install ChmodBPF.pkg"
+  pkg "Add Wireshark to the system path.pkg"
 
   uninstall_preflight do
-    system_command '/usr/sbin/installer',
+    system_command "/usr/sbin/installer",
                    args: [
-                           '-pkg', "#{staged_path}/Uninstall ChmodBPF.pkg",
-                           '-target', '/'
-                         ],
+                     "-pkg", "#{staged_path}/Uninstall ChmodBPF.pkg",
+                     "-target", "/"
+                   ],
                    sudo: true
-    system_command '/usr/sbin/installer',
+    system_command "/usr/sbin/installer",
                    args: [
-                           '-pkg', "#{staged_path}/Remove Wireshark from the system path.pkg",
-                           '-target', '/'
-                         ],
+                     "-pkg", "#{staged_path}/Remove Wireshark from the system path.pkg",
+                     "-target", "/"
+                   ],
                    sudo: true
   end
 
-  uninstall pkgutil: 'org.wireshark.*'
+  uninstall pkgutil: "org.wireshark.*"
 
   zap trash: [
-               '~/Library/Caches/org.wireshark.Wireshark',
-               '~/Library/Cookies/org.wireshark.Wireshark.binarycookies',
-               '~/Library/Preferences/org.wireshark.Wireshark.plist',
-               '~/Library/Saved Application State/org.wireshark.Wireshark.savedState',
-               '~/.config/wireshark',
-             ]
+    "~/Library/Caches/org.wireshark.Wireshark",
+    "~/Library/Cookies/org.wireshark.Wireshark.binarycookies",
+    "~/Library/Preferences/org.wireshark.Wireshark.plist",
+    "~/Library/Saved Application State/org.wireshark.Wireshark.savedState",
+    "~/.config/wireshark",
+  ]
 end
