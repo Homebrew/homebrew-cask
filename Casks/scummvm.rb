9ec113d767c3ba15b4818e05f33f1b8d36e39ac6
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:30 +0100

    scummvm: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/scummvm.rb
+++ Casks/scummvm.rb
@@ -1,11 +1,11 @@
-cask 'scummvm' do
-  version '2.1.2'
-  sha256 'f7e63342c70157337e489d7dba6eacad61842311b6406ca56f17ff1d059443bf'
+cask "scummvm" do
+  version "2.1.2"
+  sha256 "f7e63342c70157337e489d7dba6eacad61842311b6406ca56f17ff1d059443bf"
 
   url "https://scummvm.org/frs/scummvm/#{version}/scummvm-#{version}-macosx.dmg"
-  appcast 'https://www.scummvm.org/downloads/'
-  name 'ScummVM'
-  homepage 'https://www.scummvm.org/'
+  appcast "https://www.scummvm.org/downloads/"
+  name "ScummVM"
+  homepage "https://www.scummvm.org/"
 
-  app 'ScummVM.app'
+  app "ScummVM.app"
 end
