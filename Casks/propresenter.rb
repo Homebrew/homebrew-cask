c306d392edd7c6650089714dc8481876bc865868
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:59 +0100

    propresenter: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/propresenter.rb
+++ Casks/propresenter.rb
@@ -1,29 +1,29 @@
-cask 'propresenter' do
-  version '7.1.2,117506572'
-  sha256 '2aca90925531de12998a4018fab4e8437b322ad54b480a3a9e4d5a111bdb9631'
+cask "propresenter" do
+  version "7.1.2,117506572"
+  sha256 "2aca90925531de12998a4018fab4e8437b322ad54b480a3a9e4d5a111bdb9631"
 
   url "https://renewedvision.com/downloads/propresenter/mac/ProPresenter_#{version.before_comma}_#{version.after_comma}.zip"
-  appcast 'https://api.renewedvision.com/v1/pro/upgrade?platform=macos&osVersion=0&appVersion=0&buildNumber=0&includeNotes=0'
-  name 'ProPresenter'
-  homepage 'https://www.renewedvision.com/propresenter.php'
+  appcast "https://api.renewedvision.com/v1/pro/upgrade?platform=macos&osVersion=0&appVersion=0&buildNumber=0&includeNotes=0"
+  name "ProPresenter"
+  homepage "https://www.renewedvision.com/propresenter.php"
 
-  depends_on macos: '>= :mojave'
+  depends_on macos: ">= :mojave"
 
-  app 'ProPresenter.app'
+  app "ProPresenter.app"
 
   zap trash: [
-               '~/Library/Application Support/RenewedVision/ProPresenter6',
-               '~/Library/Caches/KSCrashReports/ProPresenter 6',
-               '~/Library/Caches/Sessions/ProPresenter 6',
-               '~/Library/Caches/com.renewedvision.ProPresenter6',
-               '~/Library/Preferences/com.renewedvision.ProPresenter6.plist',
-               '/Library/Application Support/RenewedVision',
-               '/Library/Caches/com.renewedvision.ProPresenter6',
-               '/Users/Shared/Renewed Vision Media',
-             ],
+    "~/Library/Application Support/RenewedVision/ProPresenter6",
+    "~/Library/Caches/KSCrashReports/ProPresenter 6",
+    "~/Library/Caches/Sessions/ProPresenter 6",
+    "~/Library/Caches/com.renewedvision.ProPresenter6",
+    "~/Library/Preferences/com.renewedvision.ProPresenter6.plist",
+    "/Library/Application Support/RenewedVision",
+    "/Library/Caches/com.renewedvision.ProPresenter6",
+    "/Users/Shared/Renewed Vision Media",
+  ],
       rmdir: [
-               '~/Library/Application Support/RenewedVision',
-               '~/Library/Caches/KSCrashReports',
-               '~/Library/Caches/Sessions',
-             ]
+        "~/Library/Application Support/RenewedVision",
+        "~/Library/Caches/KSCrashReports",
+        "~/Library/Caches/Sessions",
+      ]
 end
