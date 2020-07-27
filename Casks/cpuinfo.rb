584c4721d68e89f30766d0f9f7a646d88c002a95
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:21:06 +0100

    cpuinfo: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/cpuinfo.rb
+++ Casks/cpuinfo.rb
@@ -1,10 +1,10 @@
-cask 'cpuinfo' do
+cask "cpuinfo" do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/yusukeshibata/cpuinfo/blob/master/dist/cpuinfo.zip?raw=true'
-  name 'cpuinfo'
-  homepage 'https://github.com/yusukeshibata/cpuinfo/'
+  url "https://github.com/yusukeshibata/cpuinfo/blob/master/dist/cpuinfo.zip?raw=true"
+  name "cpuinfo"
+  homepage "https://github.com/yusukeshibata/cpuinfo/"
 
-  app 'cpuinfo.app'
+  app "cpuinfo.app"
 end
