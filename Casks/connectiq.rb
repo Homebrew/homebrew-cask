5c23fe665d08c548a9ea7e76ee3c70f6ababc3f7
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:20:51 +0100

    connectiq: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/connectiq.rb
+++ Casks/connectiq.rb
@@ -1,11 +1,11 @@
-cask 'connectiq' do
-  version '3.1.8-2020-05-01-5a72d0ab2'
-  sha256 '32b2d5ec65329b4c2ed0c53fbc9afa7af097ac48776a4caaa3c92649f3c52649'
+cask "connectiq" do
+  version "3.1.8-2020-05-01-5a72d0ab2"
+  sha256 "32b2d5ec65329b4c2ed0c53fbc9afa7af097ac48776a4caaa3c92649f3c52649"
 
   url "https://developer.garmin.com/downloads/connect-iq/sdks/connectiq-sdk-mac-#{version}.dmg"
-  appcast 'https://developer.garmin.com/connect-iq/sdk/'
-  name 'Garmin Connect IQ SDK'
-  homepage 'https://developer.garmin.com/connect-iq/'
+  appcast "https://developer.garmin.com/connect-iq/sdk/"
+  name "Garmin Connect IQ SDK"
+  homepage "https://developer.garmin.com/connect-iq/"
 
   app "connectiq-sdk-mac-#{version}/bin/ConnectIQ.app"
   binary "connectiq-sdk-mac-#{version}/bin/connectiq"
