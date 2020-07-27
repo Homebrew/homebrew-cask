ca56ba886aba5abb4c349b522322139ef09ad464
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:22:47 +0100

    dynobase: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/dynobase.rb
+++ Casks/dynobase.rb
@@ -1,17 +1,17 @@
-cask 'dynobase' do
-  version '0.8.6'
-  sha256 '2a8a0b5f85946a5daf1903ed428824fb91900599924b90aab6e235c6d141472d'
+cask "dynobase" do
+  version "0.8.6"
+  sha256 "2a8a0b5f85946a5daf1903ed428824fb91900599924b90aab6e235c6d141472d"
 
   # github.com/Dynobase/dynobase was verified as official when first introduced to the cask
   url "https://github.com/Dynobase/dynobase/releases/download/#{version}/Dynobase-#{version}.dmg"
-  appcast 'https://github.com/Dynobase/dynobase/releases.atom'
-  name 'Dynobase'
-  homepage 'https://dynobase.dev/'
+  appcast "https://github.com/Dynobase/dynobase/releases.atom"
+  name "Dynobase"
+  homepage "https://dynobase.dev/"
 
-  app 'Dynobase.app'
+  app "Dynobase.app"
 
   zap trash: [
-               '~/Library/Application Support/dynobase',
-               '~/Library/Saved Application State/com.rwilinski.dynobase.savedState',
-             ]
+    "~/Library/Application Support/dynobase",
+    "~/Library/Saved Application State/com.rwilinski.dynobase.savedState",
+  ]
 end
