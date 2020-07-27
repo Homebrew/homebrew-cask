a437ed0a7aff6350fad957a7e9006e2d2196e9e9
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:32:50 +0100

    packetsender: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/packetsender.rb
+++ Casks/packetsender.rb
@@ -1,14 +1,14 @@
-cask 'packetsender' do
-  version '7.0.5'
-  sha256 '30281225ee4e2baf3ca365123832c2931342aaf1011038268eab13ec73375fad'
+cask "packetsender" do
+  version "7.0.5"
+  sha256 "30281225ee4e2baf3ca365123832c2931342aaf1011038268eab13ec73375fad"
 
   # github.com/dannagle/PacketSender/ was verified as official when first introduced to the cask
   url "https://github.com/dannagle/PacketSender/releases/download/v#{version}/PacketSender_v#{version}.dmg"
-  appcast 'https://github.com/dannagle/PacketSender/releases.atom'
-  name 'Packet Sender'
-  homepage 'https://packetsender.com/'
+  appcast "https://github.com/dannagle/PacketSender/releases.atom"
+  name "Packet Sender"
+  homepage "https://packetsender.com/"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'PacketSender.app'
+  app "PacketSender.app"
 end
