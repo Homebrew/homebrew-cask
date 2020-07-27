2fccf26dae28d7e11bf2c1fdd3a725ed9e008f7c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:28:23 +0100

    kiwix: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/kiwix.rb
+++ Casks/kiwix.rb
@@ -1,11 +1,11 @@
-cask 'kiwix' do
-  version '2.1.1'
-  sha256 '48062d3d3156eeecec78bce226a2612b97ef84434d2e7b3d3ad402063e17e3e1'
+cask "kiwix" do
+  version "2.1.1"
+  sha256 "48062d3d3156eeecec78bce226a2612b97ef84434d2e7b3d3ad402063e17e3e1"
 
   url "https://download.kiwix.org/release/kiwix-desktop-macos/kiwix-desktop-macos_#{version}.dmg"
-  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.kiwix.org/release/kiwix-desktop-macos/kiwix-desktop-macos.dmg'
-  name 'Kiwix'
-  homepage 'https://www.kiwix.org/'
+  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.kiwix.org/release/kiwix-desktop-macos/kiwix-desktop-macos.dmg"
+  name "Kiwix"
+  homepage "https://www.kiwix.org/"
 
-  app 'Kiwix.app'
+  app "Kiwix.app"
 end
