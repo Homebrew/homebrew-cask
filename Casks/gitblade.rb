699e4f031ec66323cf50cfff98c8431e71784d9f
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:25:15 +0100

    gitblade: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/gitblade.rb
+++ Casks/gitblade.rb
@@ -1,11 +1,11 @@
-cask 'gitblade' do
-  version '1.0.6'
-  sha256 'cc759c3242e3ea26aea46b778bbcd7f2ca7db1ead5895f16db7e66a39d05fc03'
+cask "gitblade" do
+  version "1.0.6"
+  sha256 "cc759c3242e3ea26aea46b778bbcd7f2ca7db1ead5895f16db7e66a39d05fc03"
 
   url "https://gitblade.com/dl/gitblade-#{version}-darwin-x64.dmg"
-  appcast 'https://gitblade.com/releases'
-  name 'GitBlade'
-  homepage 'https://gitblade.com/'
+  appcast "https://gitblade.com/releases"
+  name "GitBlade"
+  homepage "https://gitblade.com/"
 
-  app 'GitBlade.app'
+  app "GitBlade.app"
 end
