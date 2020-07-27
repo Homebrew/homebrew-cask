32f24a218ee606a5bb3af8215d71690b226ee618
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:26:12 +0100

    hbuilderx: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/hbuilderx.rb
+++ Casks/hbuilderx.rb
@@ -1,12 +1,12 @@
-cask 'hbuilderx' do
-  version '2.8.2.20200726'
-  sha256 '78ea0843cc6fb0dd0f3eb588a8b406ecae096a2e7fc781753631a239662f7007'
+cask "hbuilderx" do
+  version "2.8.2.20200726"
+  sha256 "78ea0843cc6fb0dd0f3eb588a8b406ecae096a2e7fc781753631a239662f7007"
 
   # download1.dcloud.net.cn/ was verified as official when first introduced to the cask
   url "https://download1.dcloud.net.cn/download/HBuilderX.#{version}.dmg"
-  appcast 'https://dcloud.io/docs/HBuilder.json'
-  name 'HBuilderX'
-  homepage 'https://www.dcloud.io/hbuilderx.html'
+  appcast "https://dcloud.io/docs/HBuilder.json"
+  name "HBuilderX"
+  homepage "https://www.dcloud.io/hbuilderx.html"
 
-  app 'HBuilderX.app'
+  app "HBuilderX.app"
 end
