29c011a77a5e72d34439e5c2ed2df838b2b4c492
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:25:22 +0100

    gitter: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/gitter.rb
+++ Casks/gitter.rb
@@ -1,23 +1,23 @@
-cask 'gitter' do
-  version '1.177'
-  sha256 '0ca1c0d52c342548afbea8d3501282a4ccf494058aa2e23af27e09198a7a30a4'
+cask "gitter" do
+  version "1.177"
+  sha256 "0ca1c0d52c342548afbea8d3501282a4ccf494058aa2e23af27e09198a7a30a4"
 
   url "https://update.gitter.im/osx/Gitter-#{version}.dmg"
-  appcast 'https://update.gitter.im/osx/appcast.xml'
-  name 'Gitter'
-  homepage 'https://gitter.im/'
+  appcast "https://update.gitter.im/osx/appcast.xml"
+  name "Gitter"
+  homepage "https://gitter.im/"
 
-  app 'Gitter.app'
+  app "Gitter.app"
 
   uninstall quit: [
-                    'com.troupe.gitter.mac.Gitter',
-                    'com.troupe.gitter.mac.GitterHelperApp',
-                  ]
+    "com.troupe.gitter.mac.Gitter",
+    "com.troupe.gitter.mac.GitterHelperApp",
+  ]
 
   zap trash: [
-               '~/Library/Application Support/Gitter',
-               '~/Library/Caches/com.troupe.gitter.mac.Gitter',
-               '~/Library/Preferences/com.troupe.gitter.mac.Gitter.plist',
-               '~/Library/Saved Application State/com.troupe.gitter.mac.Gitter.savedState',
-             ]
+    "~/Library/Application Support/Gitter",
+    "~/Library/Caches/com.troupe.gitter.mac.Gitter",
+    "~/Library/Preferences/com.troupe.gitter.mac.Gitter.plist",
+    "~/Library/Saved Application State/com.troupe.gitter.mac.Gitter.savedState",
+  ]
 end
