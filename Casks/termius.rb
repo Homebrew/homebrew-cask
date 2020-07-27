84eb18d6c91e4ae5be950f38c5ee8ee1b713dd6d
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:37:18 +0100

    termius: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/termius.rb
+++ Casks/termius.rb
@@ -1,20 +1,20 @@
-cask 'termius' do
-  version '6.2.3'
-  sha256 'a079f85cfb85d837699098316b8f326a58d647f03a457576ab46915c916b14d9'
+cask "termius" do
+  version "6.2.3"
+  sha256 "a079f85cfb85d837699098316b8f326a58d647f03a457576ab46915c916b14d9"
 
   # s3.amazonaws.com/termius.desktop.autoupdate/mac/ was verified as official when first introduced to the cask
-  url 'https://s3.amazonaws.com/termius.desktop.autoupdate/mac/Termius.dmg'
-  appcast 'https://www.termius.com/mac-os'
-  name 'Termius'
-  homepage 'https://www.termius.com/'
+  url "https://s3.amazonaws.com/termius.desktop.autoupdate/mac/Termius.dmg"
+  appcast "https://www.termius.com/mac-os"
+  name "Termius"
+  homepage "https://www.termius.com/"
 
-  app 'Termius.app'
+  app "Termius.app"
 
   zap trash: [
-               '~/.termius',
-               '~/Library/Application Support/Termius',
-               '~/Library/Saved Application State/com.termius-dmg.mac.savedState',
-               '/Library/Preferences/com.termius-dmg.mac.plist',
-               '~/Library/Logs/Termius',
-             ]
+    "~/.termius",
+    "~/Library/Application Support/Termius",
+    "~/Library/Saved Application State/com.termius-dmg.mac.savedState",
+    "/Library/Preferences/com.termius-dmg.mac.plist",
+    "~/Library/Logs/Termius",
+  ]
 end
