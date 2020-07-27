a8c56dcabb76a6fa063a8683e1a0c7c9ba1fc30e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:16:06 +0100

    anydo: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/anydo.rb
+++ Casks/anydo.rb
@@ -1,13 +1,13 @@
-cask 'anydo' do
-  version '4.2.97'
-  sha256 '81e8b3baf2eb52625b26727fb1381811e64ac7d740b47bebe057907948f15f0c'
+cask "anydo" do
+  version "4.2.97"
+  sha256 "81e8b3baf2eb52625b26727fb1381811e64ac7d740b47bebe057907948f15f0c"
 
-  url 'https://electron-app.any.do/Any.do.dmg'
-  appcast 'https://electron-app.any.do/latest-mac.yml'
-  name 'Any.do'
-  homepage 'https://www.any.do/'
+  url "https://electron-app.any.do/Any.do.dmg"
+  appcast "https://electron-app.any.do/latest-mac.yml"
+  name "Any.do"
+  homepage "https://www.any.do/"
 
-  depends_on macos: '>= :high_sierra'
+  depends_on macos: ">= :high_sierra"
 
-  app 'Any.do.app'
+  app "Any.do.app"
 end
