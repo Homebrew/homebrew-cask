74298245276f769df9fb0fd6f7978af924c67635
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:23:30 +0100

    eventstore: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/eventstore.rb
+++ Casks/eventstore.rb
@@ -1,10 +1,10 @@
-cask 'eventstore' do
-  version '5.0.8'
-  sha256 '707e7fda884b9a0c03238f5e56d4927dfb85deda4402f5690c505a839b5159c4'
+cask "eventstore" do
+  version "5.0.8"
+  sha256 "707e7fda884b9a0c03238f5e56d4927dfb85deda4402f5690c505a839b5159c4"
 
   url "https://eventstore.org/downloads/macos/EventStore-OSS-MacOS-macOS-v#{version}.tar.gz"
-  name 'Event Store'
-  homepage 'https://eventstore.org/'
+  name "Event Store"
+  homepage "https://eventstore.org/"
 
   binary "EventStore-OSS-MacOS-macOS-v#{version}/eventstore"
   binary "EventStore-OSS-MacOS-macOS-v#{version}/eventstore-testclient"
