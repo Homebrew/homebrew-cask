60fa9a06f9b2d7b32a47d2f9d11d97cd54dad3b9
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:02 +0100

    lingon-x: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/lingon-x.rb
+++ Casks/lingon-x.rb
@@ -1,23 +1,23 @@
-cask 'lingon-x' do
+cask "lingon-x" do
   if MacOS.version <= :high_sierra
-    version '6.6.5'
-    sha256 'b0231b1a98dcc8f5c4234b419c9f5331407b8cce29b33f0ea2e32b12595adfa8'
+    version "6.6.5"
+    sha256 "b0231b1a98dcc8f5c4234b419c9f5331407b8cce29b33f0ea2e32b12595adfa8"
   else
-    version '7.5.8'
-    sha256 'e4022ad04dafd460fa8ba7acf0b623f5eb03e2a081a85313ec1586fe303ed766'
+    version "7.5.8"
+    sha256 "e4022ad04dafd460fa8ba7acf0b623f5eb03e2a081a85313ec1586fe303ed766"
   end
 
   url "https://www.peterborgapps.com/downloads/LingonX#{version.major}.zip"
   appcast "https://www.peterborgapps.com/updates/lingonx#{version.major}.plist"
-  name 'Lingon X'
-  homepage 'https://www.peterborgapps.com/lingon/'
+  name "Lingon X"
+  homepage "https://www.peterborgapps.com/lingon/"
 
-  depends_on macos: '>= :high_sierra'
+  depends_on macos: ">= :high_sierra"
 
-  app 'Lingon X.app'
+  app "Lingon X.app"
 
   zap trash: [
-               "~/Library/Application Scripts/com.peterborgapps.LingonX#{version.major}",
-               "~/Library/Containers/com.peterborgapps.LingonX#{version.major}",
-             ]
+    "~/Library/Application Scripts/com.peterborgapps.LingonX#{version.major}",
+    "~/Library/Containers/com.peterborgapps.LingonX#{version.major}",
+  ]
 end
