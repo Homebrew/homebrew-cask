609284992009a15a63f95388ef15923f741615ab
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:27:36 +0100

    jameica: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/jameica.rb
+++ Casks/jameica.rb
@@ -1,10 +1,10 @@
-cask 'jameica' do
-  version '2.8.6'
-  sha256 'da61520246d924892c9664bfc40aad2892a5b27bebdbb5d339edad302faae5f3'
+cask "jameica" do
+  version "2.8.6"
+  sha256 "da61520246d924892c9664bfc40aad2892a5b27bebdbb5d339edad302faae5f3"
 
   url "https://www.willuhn.de/products/jameica/releases/current/jameica/jameica-macos64-#{version}.zip"
-  name 'Jameica'
-  homepage 'https://www.willuhn.de/products/jameica/'
+  name "Jameica"
+  homepage "https://www.willuhn.de/products/jameica/"
 
-  app 'jameica.app'
+  app "jameica.app"
 end
