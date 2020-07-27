3a89404cd1e1364bc0646443e135a9262b322974
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:31 +0100

    springtoolsuite: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/springtoolsuite.rb
+++ Casks/springtoolsuite.rb
@@ -1,12 +1,12 @@
-cask 'springtoolsuite' do
-  version '4.7.0.RELEASE,4.15.0'
-  sha256 '89ae978f7e1f486a5777ea910d15ab6c78090006ad8ec0f215c5bdfbb5094791'
+cask "springtoolsuite" do
+  version "4.7.0.RELEASE,4.15.0"
+  sha256 "89ae978f7e1f486a5777ea910d15ab6c78090006ad8ec0f215c5bdfbb5094791"
 
   # download.springsource.com/release/ was verified as official when first introduced to the cask
   url "https://download.springsource.com/release/STS#{version.major}/#{version.before_comma}/dist/e#{version.after_comma.major_minor}/spring-tool-suite-#{version.major}-#{version.before_comma}-e#{version.after_comma}-macosx.cocoa.x86_64.dmg"
-  appcast 'https://github.com/spring-projects/sts4/releases.atom'
-  name 'Spring Tool Suite'
-  homepage 'https://spring.io/tools'
+  appcast "https://github.com/spring-projects/sts4/releases.atom"
+  name "Spring Tool Suite"
+  homepage "https://spring.io/tools"
 
   auto_updates true
 
