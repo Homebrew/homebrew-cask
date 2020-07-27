07ae6e8b4708320f1e22ce9c59e72db7d3e0a61c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:31:37 +0100

    neo4j: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/neo4j.rb
+++ Casks/neo4j.rb
@@ -1,12 +1,12 @@
-cask 'neo4j' do
+cask "neo4j" do
   # note: "4" is not a version number, but an intrinsic part of the product name
-  version '1.3.3'
-  sha256 'd59af2d7ddaaf417a657fa70dbd8e87657e4cef503faca2d4c34bfd727c43556'
+  version "1.3.3"
+  sha256 "d59af2d7ddaaf417a657fa70dbd8e87657e4cef503faca2d4c34bfd727c43556"
 
   url "https://neo4j.com/artifact.php?name=neo4j-desktop-#{version}.dmg"
-  appcast 'https://neo4j.com/download/'
-  name 'Neo4j Desktop'
-  homepage 'https://neo4j.com/download/'
+  appcast "https://neo4j.com/download/"
+  name "Neo4j Desktop"
+  homepage "https://neo4j.com/download/"
 
-  app 'Neo4j Desktop.app'
+  app "Neo4j Desktop.app"
 end
