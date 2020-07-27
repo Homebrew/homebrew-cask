e985fe034386eb69bf37b93468bb69d8ea96a7d7
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:19:50 +0100

    chemdoodle: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/chemdoodle.rb
+++ Casks/chemdoodle.rb
@@ -1,13 +1,13 @@
-cask 'chemdoodle' do
-  version '11.0.0'
-  sha256 '80c4687fb8d75b9141c90f59bc7a451400ea1c8e7e11d4cded825af5a98f607e'
+cask "chemdoodle" do
+  version "11.0.0"
+  sha256 "80c4687fb8d75b9141c90f59bc7a451400ea1c8e7e11d4cded825af5a98f607e"
 
   url "https://www.ichemlabs.com/downloads/ChemDoodle-osx-#{version}.dmg"
-  appcast 'https://www.ichemlabs.com/download#chemdoodle/osx-installation-instructions/'
-  name 'ChemDoodle'
-  homepage 'https://www.ichemlabs.com/'
+  appcast "https://www.ichemlabs.com/download#chemdoodle/osx-installation-instructions/"
+  name "ChemDoodle"
+  homepage "https://www.ichemlabs.com/"
 
-  depends_on macos: '>= :yosemite'
+  depends_on macos: ">= :yosemite"
 
-  suite 'ChemDoodle'
+  suite "ChemDoodle"
 end
