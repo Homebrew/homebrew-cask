d11f4c56ae6e88fc7cea25661149c6eaeadb5629
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:15 +0100

    rubitrack-pro: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/rubitrack-pro.rb
+++ Casks/rubitrack-pro.rb
@@ -1,13 +1,13 @@
-cask 'rubitrack-pro' do
-  version '5.3.2'
-  sha256 '789ec84384b2e5ce8187d4e957b98892b4c830a3b9abb513ed9747ee3ba22136'
+cask "rubitrack-pro" do
+  version "5.3.2"
+  sha256 "789ec84384b2e5ce8187d4e957b98892b4c830a3b9abb513ed9747ee3ba22136"
 
   url "https://www.rubitrack.com/files/rubiTrack-#{version}.dmg"
   appcast "https://www.rubitrack.com/autoupdate/sparkle#{version.major}.xml"
-  name 'rubiTrack'
-  homepage 'https://www.rubitrack.com/'
+  name "rubiTrack"
+  homepage "https://www.rubitrack.com/"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
   app "rubiTrack #{version.major} Pro.app"
 end
