27e0a9607d4a3298c533af5573b0b7e3b2a5beaf
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:18:38 +0100

    bootchamp: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/bootchamp.rb
+++ Casks/bootchamp.rb
@@ -1,11 +1,11 @@
-cask 'bootchamp' do
-  version '1.7'
-  sha256 'a6494fbc9efd057d8f25ba3aa8122e2b037145cc78e1bba12c69401afc06d0bc'
+cask "bootchamp" do
+  version "1.7"
+  sha256 "a6494fbc9efd057d8f25ba3aa8122e2b037145cc78e1bba12c69401afc06d0bc"
 
-  url 'https://kainjow.com/downloads/BootChamp.zip'
-  appcast 'https://kainjow.com/updates/bootchamp.xml'
-  name 'BootChamp'
-  homepage 'https://www.kainjow.com/'
+  url "https://kainjow.com/downloads/BootChamp.zip"
+  appcast "https://kainjow.com/updates/bootchamp.xml"
+  name "BootChamp"
+  homepage "https://www.kainjow.com/"
 
-  app 'BootChamp.app'
+  app "BootChamp.app"
 end
