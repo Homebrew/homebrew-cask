649bf9f4c92f21413b1a06f46ff586b94b880d3c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:37:35 +0100

    timeular: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/timeular.rb
+++ Casks/timeular.rb
@@ -1,14 +1,14 @@
-cask 'timeular' do
-  version '3.3.0'
-  sha256 '197c111e79ae0dd5f6f4a4b9c2f39dce66750a8853ed1dee42d3a6943c1cae01'
+cask "timeular" do
+  version "3.3.0"
+  sha256 "197c111e79ae0dd5f6f4a4b9c2f39dce66750a8853ed1dee42d3a6943c1cae01"
 
   # timeular-desktop-packages.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://timeular-desktop-packages.s3.amazonaws.com/mac/production/Timeular-#{version}.dmg"
-  appcast 'https://timeular-desktop-packages.s3.amazonaws.com/mac/production/latest-mac.yml'
-  name 'Timeular'
-  homepage 'https://timeular.com/'
+  appcast "https://timeular-desktop-packages.s3.amazonaws.com/mac/production/latest-mac.yml"
+  name "Timeular"
+  homepage "https://timeular.com/"
 
   auto_updates true
 
-  app 'Timeular.app'
+  app "Timeular.app"
 end
