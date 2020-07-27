7f94640ab41a7c83a4f8ee334bba401d9fd73fea
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:21:44 +0100

    dcp-o-matic: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/dcp-o-matic.rb
+++ Casks/dcp-o-matic.rb
@@ -1,11 +1,11 @@
-cask 'dcp-o-matic' do
-  version '2.14.33'
-  sha256 '5fee8351980b3c4f7557e397824ed4db8ab903ac1262541d1f10a9716df66940'
+cask "dcp-o-matic" do
+  version "2.14.33"
+  sha256 "5fee8351980b3c4f7557e397824ed4db8ab903ac1262541d1f10a9716df66940"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.9-main&version=#{version}"
-  appcast 'https://dcpomatic.com/download'
-  name 'DCP-o-matic'
-  homepage 'https://dcpomatic.com/'
+  appcast "https://dcpomatic.com/download"
+  name "DCP-o-matic"
+  homepage "https://dcpomatic.com/"
 
   app "DCP-o-matic #{version.major}.app"
 end
