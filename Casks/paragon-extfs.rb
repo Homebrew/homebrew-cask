b5c709c525c1721d717cdf2efdefa311207edaf0
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:32:56 +0100

    paragon-extfs: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/paragon-extfs.rb
+++ Casks/paragon-extfs.rb
@@ -1,21 +1,21 @@
-cask 'paragon-extfs' do
+cask "paragon-extfs" do
   version :latest
   sha256 :no_check
 
-  url 'https://dl.paragon-software.com/demo/trial_extfs.dmg'
-  name 'Paragon ExtFS'
-  homepage 'https://www.paragon-software.com/ufsdhome/extfs-mac/'
+  url "https://dl.paragon-software.com/demo/trial_extfs.dmg"
+  name "Paragon ExtFS"
+  homepage "https://www.paragon-software.com/ufsdhome/extfs-mac/"
 
-  installer manual: 'FSInstaller.app'
+  installer manual: "FSInstaller.app"
 
-  uninstall kext:      'com.paragon-software.filesystems.extfs',
-            launchctl: 'com.paragon-software.extfs*',
-            pkgutil:   'com.paragon-software.pkg.extfs',
-            quit:      'com.paragon-software.extfs*',
+  uninstall kext:      "com.paragon-software.filesystems.extfs",
+            launchctl: "com.paragon-software.extfs*",
+            pkgutil:   "com.paragon-software.pkg.extfs",
+            quit:      "com.paragon-software.extfs*",
             signal:    [
-                         ['KILL', 'com.paragon-software.extfs.FSMenuApp'],
-                         ['KILL', 'com.paragon-software.extfs.notification-agent'],
-                       ]
+              ["KILL", "com.paragon-software.extfs.FSMenuApp"],
+              ["KILL", "com.paragon-software.extfs.notification-agent"],
+            ]
 
-  zap trash: '~/Library/Preferences/com.paragon-software.extfs.fsapp.plist'
+  zap trash: "~/Library/Preferences/com.paragon-software.extfs.fsapp.plist"
 end
