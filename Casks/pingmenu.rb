8b030f0f628a33db9b349cf94dd29b4ff6faf1e5
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:22 +0100

    pingmenu: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/pingmenu.rb
+++ Casks/pingmenu.rb
@@ -1,10 +1,10 @@
-cask 'pingmenu' do
+cask "pingmenu" do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/kalleboo/PingMenu/raw/master/PingMenu.app.zip'
-  name 'PingMenu'
-  homepage 'https://github.com/kalleboo/PingMenu'
+  url "https://github.com/kalleboo/PingMenu/raw/master/PingMenu.app.zip"
+  name "PingMenu"
+  homepage "https://github.com/kalleboo/PingMenu"
 
-  app 'PingMenu.app'
+  app "PingMenu.app"
 end
