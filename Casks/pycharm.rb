22992ba644e139f7df3da2fd5644cdb4f9763c9f
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:10 +0100

    pycharm: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/pycharm.rb
+++ Casks/pycharm.rb
@@ -1,27 +1,32 @@
-cask 'pycharm' do
-  version '2020.1.4,201.8743.11'
-  sha256 '42187e76d16bacb79beff546d46e608b9e5d752edd6de7ec543772e7ffade333'
+cask "pycharm" do
+  version "2020.1.4,201.8743.11"
+  sha256 "42187e76d16bacb79beff546d46e608b9e5d752edd6de7ec543772e7ffade333"
 
   url "https://download.jetbrains.com/python/pycharm-professional-#{version.before_comma}.dmg"
-  appcast 'https://data.services.jetbrains.com/products/releases?code=PCP&latest=true&type=release'
-  name 'PyCharm'
-  homepage 'https://www.jetbrains.com/pycharm/'
+  appcast "https://data.services.jetbrains.com/products/releases?code=PCP&latest=true&type=release"
+  name "PyCharm"
+  homepage "https://www.jetbrains.com/pycharm/"
 
   auto_updates true
 
-  app 'PyCharm.app'
+  app "PyCharm.app"
 
   uninstall_postflight do
-    ENV['PATH'].split(File::PATH_SEPARATOR).map { |path| File.join(path, 'charm') }.each { |path| File.delete(path) if File.exist?(path) && File.readlines(path).grep(%r{# see com.intellij.idea.SocketLock for the server side of this interface}).any? }
+    ENV["PATH"].split(File::PATH_SEPARATOR).map { |path| File.join(path, "charm") }.each do |path|
+      if File.exist?(path) &&
+         File.readlines(path).grep(/# see com.intellij.idea.SocketLock for the server side of this interface/).any?
+        File.delete(path)
+      end
+    end
   end
 
   zap trash: [
-               "~/Library/Application Support/PyCharm#{version.major_minor}",
-               "~/Library/Application Support/JetBrains/PyCharm#{version.major_minor}",
-               "~/Library/Caches/PyCharm#{version.major_minor}",
-               "~/Library/Logs/PyCharm#{version.major_minor}",
-               "~/Library/Preferences/PyCharm#{version.major_minor}",
-               '~/Library/Preferences/jetbrains.pycharm.*.plist',
-               '~/Library/Saved Application State/com.jetbrains.pycharm.savedState',
-             ]
+    "~/Library/Application Support/PyCharm#{version.major_minor}",
+    "~/Library/Application Support/JetBrains/PyCharm#{version.major_minor}",
+    "~/Library/Caches/PyCharm#{version.major_minor}",
+    "~/Library/Logs/PyCharm#{version.major_minor}",
+    "~/Library/Preferences/PyCharm#{version.major_minor}",
+    "~/Library/Preferences/jetbrains.pycharm.*.plist",
+    "~/Library/Saved Application State/com.jetbrains.pycharm.savedState",
+  ]
 end
