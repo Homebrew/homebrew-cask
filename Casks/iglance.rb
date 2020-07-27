af2dc3db5b6d058f6c5f2d6745b371932fd9da58
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:26:47 +0100

    iglance: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/iglance.rb
+++ Casks/iglance.rb
@@ -1,19 +1,19 @@
-cask 'iglance' do
-  version '2.0.9'
-  sha256 'fc1c023f8156d07ab7d6cc8838d43311f0b8e8acbb6848cf653656bf0363b467'
+cask "iglance" do
+  version "2.0.9"
+  sha256 "fc1c023f8156d07ab7d6cc8838d43311f0b8e8acbb6848cf653656bf0363b467"
 
   url "https://github.com/iglance/iglance/releases/download/v#{version}/iGlance_v#{version}.zip"
-  appcast 'https://github.com/iglance/iglance/releases.atom'
-  name 'iGlance'
-  homepage 'https://github.com/iglance/iGlance'
+  appcast "https://github.com/iglance/iglance/releases.atom"
+  name "iGlance"
+  homepage "https://github.com/iglance/iGlance"
 
   auto_updates true
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'iGlance.app'
+  app "iGlance.app"
 
   zap trash: [
-               '~/Library/Caches/io.github.iglance.iGlance',
-               '~/Library/Preferences/io.github.iglance.iGlance.plist',
-             ]
+    "~/Library/Caches/io.github.iglance.iGlance",
+    "~/Library/Preferences/io.github.iglance.iGlance.plist",
+  ]
 end
