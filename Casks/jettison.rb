5522a8432a29f6a061388db74e9771ac98cc4677
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:27:47 +0100

    jettison: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/jettison.rb
+++ Casks/jettison.rb
@@ -1,22 +1,22 @@
-cask 'jettison' do
-  version '1.7.5'
-  sha256 '2008e23aaa87ef38b85fac5b16a703a0cbb212a0ef522ed311bd575768e4834e'
+cask "jettison" do
+  version "1.7.5"
+  sha256 "2008e23aaa87ef38b85fac5b16a703a0cbb212a0ef522ed311bd575768e4834e"
 
   url "https://stclairsoft.com/download/Jettison-#{version}.dmg"
-  appcast 'https://stclairsoft.com/cgi-bin/sparkle.cgi?JT'
-  name 'Jettison'
-  name 'St. Clair Software Jettison'
-  homepage 'https://stclairsoft.com/Jettison/'
+  appcast "https://stclairsoft.com/cgi-bin/sparkle.cgi?JT"
+  name "Jettison"
+  name "St. Clair Software Jettison"
+  homepage "https://stclairsoft.com/Jettison/"
 
   auto_updates true
 
-  app 'Jettison.app'
+  app "Jettison.app"
 
   zap trash: [
-               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.stclairsoft.jettison.sfl*',
-               '~/Library/Application Support/Jettison',
-               '~/Library/Preferences/com.stclairsoft.Jettison.plist',
-               '~/Library/Preferences/com.stclairsoft.Jettison.AppStore.plist',
-               '~/Library/Caches/com.stclairsoft.Jettison',
-             ]
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.stclairsoft.jettison.sfl*",
+    "~/Library/Application Support/Jettison",
+    "~/Library/Preferences/com.stclairsoft.Jettison.plist",
+    "~/Library/Preferences/com.stclairsoft.Jettison.AppStore.plist",
+    "~/Library/Caches/com.stclairsoft.Jettison",
+  ]
 end
