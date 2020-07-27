b183774ddecf880a0c6573e006aa38269f02780b
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:30:05 +0100

    mcbopomofo: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/mcbopomofo.rb
+++ Casks/mcbopomofo.rb
@@ -1,14 +1,14 @@
-cask 'mcbopomofo' do
-  version '0.9.11'
-  sha256 'eddbdecca34e1139a5c499a8afbc1d8153685908058f0de12647a6de9b2ab99a'
+cask "mcbopomofo" do
+  version "0.9.11"
+  sha256 "eddbdecca34e1139a5c499a8afbc1d8153685908058f0de12647a6de9b2ab99a"
 
   # github.com/ was verified as official when first introduced to the cask
   url "https://github.com/openvanilla/McBopomofo/releases/download/#{version}/McBopomofo-Installer-#{version}.zip"
-  appcast 'https://github.com/openvanilla/McBopomofo/releases.atom'
-  name 'McBopomofo'
-  homepage 'https://mcbopomofo.openvanilla.org/'
+  appcast "https://github.com/openvanilla/McBopomofo/releases.atom"
+  name "McBopomofo"
+  homepage "https://mcbopomofo.openvanilla.org/"
 
-  installer manual: 'McBopomofoInstaller.app'
+  installer manual: "McBopomofoInstaller.app"
 
-  uninstall delete: '~/Library/Input Methods/McBopomofo.app'
+  uninstall delete: "~/Library/Input Methods/McBopomofo.app"
 end
