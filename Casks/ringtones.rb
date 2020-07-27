b78381492765badf4992359fe0e08d3cc88a28d4
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:05 +0100

    ringtones: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/ringtones.rb
+++ Casks/ringtones.rb
@@ -1,11 +1,11 @@
-cask 'ringtones' do
-  version '1.1.3'
-  sha256 '459119df8f7895fe427c95a541df7c94477962c19ff5884a450afb3e5b867bd0'
+cask "ringtones" do
+  version "1.1.3"
+  sha256 "459119df8f7895fe427c95a541df7c94477962c19ff5884a450afb3e5b867bd0"
 
-  url 'http://files.thelittleappfactory.com/ringtones/Ringtones.zip'
-  appcast 'http://files.thelittleappfactory.com/ringtones/appcast.xml'
-  name 'Ringtones'
-  homepage 'http://thelittleappfactory.com/ringtones/'
+  url "http://files.thelittleappfactory.com/ringtones/Ringtones.zip"
+  appcast "http://files.thelittleappfactory.com/ringtones/appcast.xml"
+  name "Ringtones"
+  homepage "http://thelittleappfactory.com/ringtones/"
 
-  app 'Ringtones.app'
+  app "Ringtones.app"
 end
