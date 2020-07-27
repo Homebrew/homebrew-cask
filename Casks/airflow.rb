340b7efa61c6742c07e6f5562fa1e3c62db02a70
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:13:47 +0100

    airflow: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/airflow.rb
+++ Casks/airflow.rb
@@ -1,22 +1,22 @@
-cask 'airflow' do
-  version '3.1.3u1'
-  sha256 'd7dd65cae80df5bb073ab7ed12d5552d2be727c5babac400ac9a84372cabe544'
+cask "airflow" do
+  version "3.1.3u1"
+  sha256 "d7dd65cae80df5bb073ab7ed12d5552d2be727c5babac400ac9a84372cabe544"
 
   # cdn.downloads.iocave.net/Airflow/ was verified as official when first introduced to the cask
   url "https://cdn.downloads.iocave.net/Airflow/Airflow%20#{version}.zip"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://airflowapp.com/download/latest?mac'
-  name 'Airflow'
-  homepage 'https://airflowapp.com/'
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://airflowapp.com/download/latest?mac"
+  name "Airflow"
+  homepage "https://airflowapp.com/"
 
   auto_updates true
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'Airflow.app'
+  app "Airflow.app"
 
   zap trash: [
-               '~/Library/Application Support/Airflow',
-               '~/Library/Caches/com.bitcavehq.Airflow',
-               '~/Library/Preferences/com.bitcavehq.Airflow.plist',
-               '~/Library/Saved Application State/com.bitcavehq.Airflow.savedState',
-             ]
+    "~/Library/Application Support/Airflow",
+    "~/Library/Caches/com.bitcavehq.Airflow",
+    "~/Library/Preferences/com.bitcavehq.Airflow.plist",
+    "~/Library/Saved Application State/com.bitcavehq.Airflow.savedState",
+  ]
 end
