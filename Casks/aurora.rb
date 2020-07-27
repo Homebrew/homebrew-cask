b425bc3894dcf9c289cd41b56355c6611af23c7a
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:16:56 +0100

    aurora: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/aurora.rb
+++ Casks/aurora.rb
@@ -1,11 +1,11 @@
-cask 'aurora' do
-  version '5.0.3'
-  sha256 '4293c5ffdf6eb8e5d7031f02b1c83238472f486fe8a964f2f62ac71c72c22eb2'
+cask "aurora" do
+  version "5.0.3"
+  sha256 "4293c5ffdf6eb8e5d7031f02b1c83238472f486fe8a964f2f62ac71c72c22eb2"
 
   url "https://www.oneperiodic.com/files/Aurora%20v#{version}.zip"
   appcast "https://www.oneperiodic.com/aurora#{version.major}.xml"
-  name 'Aurora'
-  homepage 'https://www.oneperiodic.com/products/aurora/'
+  name "Aurora"
+  homepage "https://www.oneperiodic.com/products/aurora/"
 
-  app 'Aurora.app'
+  app "Aurora.app"
 end
