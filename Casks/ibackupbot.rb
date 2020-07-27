145538ea67a853cd29c21abdd3d570a8dbcfa39e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:26:35 +0100

    ibackupbot: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/ibackupbot.rb
+++ Casks/ibackupbot.rb
@@ -1,10 +1,10 @@
-cask 'ibackupbot' do
+cask "ibackupbot" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.icopybot.com/iBackupBot-Setup.dmg'
-  name 'iBackupBot'
-  homepage 'https://www.icopybot.com/itunes-backup-manager.htm'
+  url "https://www.icopybot.com/iBackupBot-Setup.dmg"
+  name "iBackupBot"
+  homepage "https://www.icopybot.com/itunes-backup-manager.htm"
 
-  app 'iBackupBot.app'
+  app "iBackupBot.app"
 end
