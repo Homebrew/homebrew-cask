d719d0abfd40290b48865a7ddbd3e6f4b16ca23f
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:44 +0100

    postman: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,24 +1,24 @@
-cask 'postman' do
-  version '7.29.1'
-  sha256 '2366031fe48d33f1412c431e5c96116a0b077a739da6be7bc94e348bff96b529'
+cask "postman" do
+  version "7.29.1"
+  sha256 "2366031fe48d33f1412c431e5c96116a0b077a739da6be7bc94e348bff96b529"
 
   # dl.pstmn.io/download/version/ was verified as official when first introduced to the cask
   url "https://dl.pstmn.io/download/version/#{version}/osx64"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://dl.pstmn.io/download/latest/osx'
-  name 'Postman'
-  homepage 'https://www.postman.com/'
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://dl.pstmn.io/download/latest/osx"
+  name "Postman"
+  homepage "https://www.postman.com/"
 
   auto_updates true
 
-  app 'Postman.app'
+  app "Postman.app"
 
   zap trash: [
-               '~/Library/Application Support/Postman',
-               '~/Library/Application Support/com.postmanlabs.mac.ShipIt',
-               '~/Library/Caches/Postman',
-               '~/Library/Caches/com.postmanlabs.mac',
-               '~/Library/Preferences/ByHost/com.postmanlabs.mac.ShipIt.*.plist',
-               '~/Library/Preferences/com.postmanlabs.mac.plist',
-               '~/Library/Saved Application State/com.postmanlabs.mac.savedState',
-             ]
+    "~/Library/Application Support/Postman",
+    "~/Library/Application Support/com.postmanlabs.mac.ShipIt",
+    "~/Library/Caches/Postman",
+    "~/Library/Caches/com.postmanlabs.mac",
+    "~/Library/Preferences/ByHost/com.postmanlabs.mac.ShipIt.*.plist",
+    "~/Library/Preferences/com.postmanlabs.mac.plist",
+    "~/Library/Saved Application State/com.postmanlabs.mac.savedState",
+  ]
 end
