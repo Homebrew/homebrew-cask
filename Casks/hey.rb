f06acbf845f3b477fc06582c26f812cbcff48077
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:26:18 +0100

    hey: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/hey.rb
+++ Casks/hey.rb
@@ -1,17 +1,17 @@
-cask 'hey' do
-  version '1.0.9'
-  sha256 '3310786e594494427d9414b78805bc06cb4d0d97ca6fb9ce2a907543ab0e96b1'
+cask "hey" do
+  version "1.0.9"
+  sha256 "3310786e594494427d9414b78805bc06cb4d0d97ca6fb9ce2a907543ab0e96b1"
 
   # hey-desktop.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://hey-desktop.s3.amazonaws.com/HEY-#{version}.dmg"
-  appcast 'https://hey-desktop.s3.amazonaws.com/latest-mac.yml'
-  name 'Hey'
-  homepage 'https://hey.com/'
+  appcast "https://hey-desktop.s3.amazonaws.com/latest-mac.yml"
+  name "Hey"
+  homepage "https://hey.com/"
 
-  app 'Hey.app'
+  app "Hey.app"
 
   zap trash: [
-               '~/Library/Application Support/HEY',
-               '~/Library/Preferences/com.hey.app.desktop.plist',
-             ]
+    "~/Library/Application Support/HEY",
+    "~/Library/Preferences/com.hey.app.desktop.plist",
+  ]
 end
