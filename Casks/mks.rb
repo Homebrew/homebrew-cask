a906a6b951cbcafb5b235f6c605e152417259acc
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:30:46 +0100

    mks: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/mks.rb
+++ Casks/mks.rb
@@ -1,11 +1,11 @@
-cask 'mks' do
-  version '1.6'
-  sha256 'b656c7e17ccec0a066998e952293fa234ec363df5f07e7b7d7d06b7f295bf03a'
+cask "mks" do
+  version "1.6"
+  sha256 "b656c7e17ccec0a066998e952293fa234ec363df5f07e7b7d7d06b7f295bf03a"
 
   url "https://github.com/x0054/MKS/releases/download/#{version}/MKS.dmg"
-  appcast 'https://github.com/x0054/MKS/releases.atom'
-  name 'MKS'
-  homepage 'https://github.com/x0054/MKS'
+  appcast "https://github.com/x0054/MKS/releases.atom"
+  name "MKS"
+  homepage "https://github.com/x0054/MKS"
 
-  app 'MKS.app'
+  app "MKS.app"
 end
