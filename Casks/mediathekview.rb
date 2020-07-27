f70f99a05809965a415490f1d8618ce0968c08c8
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:30:10 +0100

    mediathekview: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/mediathekview.rb
+++ Casks/mediathekview.rb
@@ -1,11 +1,11 @@
-cask 'mediathekview' do
-  version '13.5.1'
-  sha256 '745a965c74bbd449535bdd87e8473d0c0912a0e20abe298f2f174687786f075a'
+cask "mediathekview" do
+  version "13.5.1"
+  sha256 "745a965c74bbd449535bdd87e8473d0c0912a0e20abe298f2f174687786f075a"
 
   url "https://download.mediathekview.de/stabil/MediathekView-#{version}-mac.dmg"
-  appcast 'https://mediathekview.de/changelog/index.xml'
-  name 'MediathekView'
-  homepage 'https://mediathekview.de/'
+  appcast "https://mediathekview.de/changelog/index.xml"
+  name "MediathekView"
+  homepage "https://mediathekview.de/"
 
-  suite 'MediathekView'
+  suite "MediathekView"
 end
