9b0790ff1e02c74e8954eef339edefc21f590f05
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:05 +0100

    pdf-expert: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/pdf-expert.rb
+++ Casks/pdf-expert.rb
@@ -1,15 +1,15 @@
-cask 'pdf-expert' do
-  version '2.5.9,702'
-  sha256 '22972eeca0bdd86704b826bf799bd2b74168ebf97e5aaa504d1ff98ee8088c17'
+cask "pdf-expert" do
+  version "2.5.9,702"
+  sha256 "22972eeca0bdd86704b826bf799bd2b74168ebf97e5aaa504d1ff98ee8088c17"
 
   # d1ke680phyeohy.cloudfront.net/ was verified as official when first introduced to the cask
   url "https://d1ke680phyeohy.cloudfront.net/versions/#{version.after_comma}/PDFExpert.dmg"
-  appcast 'https://d1ke680phyeohy.cloudfront.net/release/appcast.xml'
-  name 'PDF Expert'
-  homepage 'https://pdfexpert.com/'
+  appcast "https://d1ke680phyeohy.cloudfront.net/release/appcast.xml"
+  name "PDF Expert"
+  homepage "https://pdfexpert.com/"
 
   auto_updates true
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'PDF Expert.app'
+  app "PDF Expert.app"
 end
