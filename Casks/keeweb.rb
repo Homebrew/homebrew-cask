560e37ed9418a79168f2022d1af5bdb41fbac3b1
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:28:08 +0100

    keeweb: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/keeweb.rb
+++ Casks/keeweb.rb
@@ -1,16 +1,16 @@
-cask 'keeweb' do
-  version '1.15.5'
-  sha256 'ccf19f007ef56422f1ea06028135dfe1f9ccd597fba67a5adfcba3b697d16233'
+cask "keeweb" do
+  version "1.15.5"
+  sha256 "ccf19f007ef56422f1ea06028135dfe1f9ccd597fba67a5adfcba3b697d16233"
 
   # github.com/keeweb/keeweb/ was verified as official when first introduced to the cask
   url "https://github.com/keeweb/keeweb/releases/download/v#{version}/KeeWeb-#{version}.mac.dmg"
-  appcast 'https://github.com/keeweb/keeweb/releases.atom'
-  name 'KeeWeb'
-  homepage 'https://keeweb.info/'
+  appcast "https://github.com/keeweb/keeweb/releases.atom"
+  name "KeeWeb"
+  homepage "https://keeweb.info/"
 
   auto_updates true
 
-  app 'KeeWeb.app'
+  app "KeeWeb.app"
 
   uninstall_preflight do
     set_ownership "#{appdir}/KeeWeb.app"
