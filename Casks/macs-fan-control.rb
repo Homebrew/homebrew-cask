81e84b23022048668a53b9aaab02cc70bfdad580
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:34 +0100

    macs-fan-control: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/macs-fan-control.rb
+++ Casks/macs-fan-control.rb
@@ -1,20 +1,20 @@
-cask 'macs-fan-control' do
-  version '1.5.7'
-  sha256 '23c9115cdb9df13c2a61671ef2fdb0abc5b8802e2b909d01c05eac6cbddb241c'
+cask "macs-fan-control" do
+  version "1.5.7"
+  sha256 "23c9115cdb9df13c2a61671ef2fdb0abc5b8802e2b909d01c05eac6cbddb241c"
 
   # github.com/crystalidea/macs-fan-control/ was verified as official when first introduced to the cask
   url "https://github.com/crystalidea/macs-fan-control/releases/download/v#{version.major_minor_patch}/macsfancontrol.zip"
-  appcast 'https://github.com/crystalidea/macs-fan-control/releases.atom',
+  appcast "https://github.com/crystalidea/macs-fan-control/releases.atom",
           must_contain: version.major_minor_patch
-  name 'Macs Fan Control'
-  homepage 'https://www.crystalidea.com/macs-fan-control'
+  name "Macs Fan Control"
+  homepage "https://www.crystalidea.com/macs-fan-control"
 
   auto_updates true
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'Macs Fan Control.app'
+  app "Macs Fan Control.app"
 
-  uninstall signal: ['TERM', 'com.crystalidea.MacsFanControl']
+  uninstall signal: ["TERM", "com.crystalidea.MacsFanControl"]
 
-  zap trash: '~/Library/Preferences/com.crystalidea.macsfancontrol.plist'
+  zap trash: "~/Library/Preferences/com.crystalidea.macsfancontrol.plist"
 end
