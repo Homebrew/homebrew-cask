ee65884b7fc394e56eb3b60a219242a76c1977c0
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:39:08 +0100

    zoc: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/zoc.rb
+++ Casks/zoc.rb
@@ -1,11 +1,11 @@
-cask 'zoc' do
-  version '7.26.0'
-  sha256 '813ecfaa31b64810b2c362b19ef316e550d2673086d55d12ac4d777aeff6252a'
+cask "zoc" do
+  version "7.26.0"
+  sha256 "813ecfaa31b64810b2c362b19ef316e550d2673086d55d12ac4d777aeff6252a"
 
   url "https://www.emtec.com/downloads/zoc/zoc#{version.no_dots}.dmg"
-  appcast 'https://www.emtec.com/downloads/zoc/zoc_changes.txt'
-  name 'ZOC'
-  homepage 'https://www.emtec.com/zoc/'
+  appcast "https://www.emtec.com/downloads/zoc/zoc_changes.txt"
+  name "ZOC"
+  homepage "https://www.emtec.com/zoc/"
 
   app "zoc#{version.major}.app"
 end
