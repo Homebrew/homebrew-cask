2528b88ec85cddc9bce1dfc3b78e8b2a2b71c5d4
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:47 +0100

    studio-3t: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/studio-3t.rb
+++ Casks/studio-3t.rb
@@ -1,13 +1,13 @@
-cask 'studio-3t' do
-  version '2020.6.1'
-  sha256 'ed351b3726e2d78f45721d7d68fa72589badaf92732f1062de3c51b1505beacb'
+cask "studio-3t" do
+  version "2020.6.1"
+  sha256 "ed351b3726e2d78f45721d7d68fa72589badaf92732f1062de3c51b1505beacb"
 
   url "https://download.studio3t.com/studio-3t/mac/#{version}/Studio-3T.dmg"
-  appcast 'https://files.studio3t.com/changelog/changelog.txt'
-  name 'Studio 3T'
-  homepage 'https://studio3t.com/'
+  appcast "https://files.studio3t.com/changelog/changelog.txt"
+  name "Studio 3T"
+  homepage "https://studio3t.com/"
 
   auto_updates true
 
-  app 'Studio 3T.app'
+  app "Studio 3T.app"
 end
