13995dbfd2c41af55e032a8b64bd0635e05a6321
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:20:11 +0100

    clamxav: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/clamxav.rb
+++ Casks/clamxav.rb
@@ -1,18 +1,18 @@
-cask 'clamxav' do
-  version '3.0.15_8328'
-  sha256 '6e2a6e1a061a28b953737fea590d3c94a6689255fd00b0f44e07c6f8c32dfdb7'
+cask "clamxav" do
+  version "3.0.15_8328"
+  sha256 "6e2a6e1a061a28b953737fea590d3c94a6689255fd00b0f44e07c6f8c32dfdb7"
 
   url "https://cdn.clamxav.com/ClamXAVdownloads/ClamXAV_#{version}.zip"
   appcast "https://www.clamxav.com/sparkle/appcast#{version.major}.xml"
-  name 'ClamXAV'
-  homepage 'https://www.clamxav.com/'
+  name "ClamXAV"
+  homepage "https://www.clamxav.com/"
 
-  app 'ClamXAV.app'
+  app "ClamXAV.app"
 
   zap trash: [
-               '~/Library/Caches/uk.co.markallan.clamxav',
-               '~/Library/Logs/clamXav-scan.*',
-             ]
+    "~/Library/Caches/uk.co.markallan.clamxav",
+    "~/Library/Logs/clamXav-scan.*",
+  ]
 
   caveats do
     # this happens sometime after installation, but still worth warning about
