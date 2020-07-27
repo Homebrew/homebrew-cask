775529f0286e30591ec19a0a35f844601df98225
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:31:13 +0100

    mudlet: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/mudlet.rb
+++ Casks/mudlet.rb
@@ -1,13 +1,13 @@
-cask 'mudlet' do
-  version '4.9.1'
-  sha256 'e7105e93cd7dd12b920856ae3cc9a0b1fc4538970768a316921eb6f657cf883d'
+cask "mudlet" do
+  version "4.9.1"
+  sha256 "e7105e93cd7dd12b920856ae3cc9a0b1fc4538970768a316921eb6f657cf883d"
 
   url "https://www.mudlet.org/wp-content/files/Mudlet-#{version}.dmg"
-  appcast 'https://github.com/Mudlet/Mudlet/releases.atom'
-  name 'Mudlet'
-  homepage 'https://www.mudlet.org/'
+  appcast "https://github.com/Mudlet/Mudlet/releases.atom"
+  name "Mudlet"
+  homepage "https://www.mudlet.org/"
 
-  depends_on macos: '>= :high_sierra'
+  depends_on macos: ">= :high_sierra"
 
-  app 'Mudlet.app'
+  app "Mudlet.app"
 end
