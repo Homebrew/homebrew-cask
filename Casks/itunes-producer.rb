1412bcf77b43e35541dd4ea32273c83c555ba608
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:27:29 +0100

    itunes-producer: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/itunes-producer.rb
+++ Casks/itunes-producer.rb
@@ -1,13 +1,13 @@
-cask 'itunes-producer' do
-  version '3.1.2'
-  sha256 '21079edbf0d559db4403a76fb7f18f02c93e5fbb68006bb5aaf55af329301ded'
+cask "itunes-producer" do
+  version "3.1.2"
+  sha256 "21079edbf0d559db4403a76fb7f18f02c93e5fbb68006bb5aaf55af329301ded"
 
   url "https://itunespartner.apple.com/assets/downloads/iTunesProducer_#{version}.dmg"
-  appcast 'https://itunespartner.apple.com/en/music/tools'
-  name 'iTunes Producer'
-  homepage 'https://itunesconnect.apple.com/WebObjects/iTunesConnect.woa/ra/ng/resources_page'
+  appcast "https://itunespartner.apple.com/en/music/tools"
+  name "iTunes Producer"
+  homepage "https://itunesconnect.apple.com/WebObjects/iTunesConnect.woa/ra/ng/resources_page"
 
-  pkg 'iTunesProducer.pkg'
+  pkg "iTunesProducer.pkg"
 
-  uninstall pkgutil: 'com.apple.pkg.iTunesProducer'
+  uninstall pkgutil: "com.apple.pkg.iTunesProducer"
 end
