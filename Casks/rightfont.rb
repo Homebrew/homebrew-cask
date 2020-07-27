42cedbb2aca88cbef37a2549c7088d06af1412ee
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:03 +0100

    rightfont: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/rightfont.rb
+++ Casks/rightfont.rb
@@ -1,14 +1,14 @@
-cask 'rightfont' do
-  version '5.8.4'
-  sha256 'e581cac8691bb397c3e09ec83af8c30eb8da9fe8291b3a8f49a3e4e8cc828e57'
+cask "rightfont" do
+  version "5.8.4"
+  sha256 "e581cac8691bb397c3e09ec83af8c30eb8da9fe8291b3a8f49a3e4e8cc828e57"
 
-  url 'https://rightfontapp.com/update/rightfont.zip'
+  url "https://rightfontapp.com/update/rightfont.zip"
   appcast "https://rightfontapp.com/update/appcast#{version.major}.xml"
-  name 'RightFont'
-  homepage 'https://rightfontapp.com/'
+  name "RightFont"
+  homepage "https://rightfontapp.com/"
 
   auto_updates true
-  depends_on macos: '>= :yosemite'
+  depends_on macos: ">= :yosemite"
 
   app "RightFont #{version.major}.app"
 end
