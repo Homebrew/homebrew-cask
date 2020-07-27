b2d576658e270052cf0eebb1ec11e6ddcf50688b
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:27:34 +0100

    jaksta-media-recorder: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/jaksta-media-recorder.rb
+++ Casks/jaksta-media-recorder.rb
@@ -1,22 +1,22 @@
-cask 'jaksta-media-recorder' do
-  version '2.2.5'
-  sha256 'f5bb7554c1361c574dcada598ce53f6b38e198395513466e3fa23f21a4dcf324'
+cask "jaksta-media-recorder" do
+  version "2.2.5"
+  sha256 "f5bb7554c1361c574dcada598ce53f6b38e198395513466e3fa23f21a4dcf324"
 
   url "https://www.jaksta.com/services/download/mac/jaksta-media-recorder/#{version}"
-  appcast 'https://www.jaksta.com/Services/VersionDirector/jmr-mac'
-  name 'Jaksta'
-  homepage 'https://www.jaksta.com/products'
+  appcast "https://www.jaksta.com/Services/VersionDirector/jmr-mac"
+  name "Jaksta"
+  homepage "https://www.jaksta.com/products"
 
   container type: :naked
 
-  pkg 'JMR.pkg'
+  pkg "JMR.pkg"
 
   # This is a horrible hack to force the file extension.
   # The backend code should be fixed so that this is not needed.
   preflight do
-    system_command '/bin/mv', args: ['--', staged_path.join(version.to_s), staged_path.join('JMR.pkg')]
+    system_command "/bin/mv", args: ["--", staged_path.join(version.to_s), staged_path.join("JMR.pkg")]
   end
 
-  uninstall pkgutil:   'com.shedworx.Jaksta',
-            launchctl: 'com.ProxyConfigDaemon'
+  uninstall pkgutil:   "com.shedworx.Jaksta",
+            launchctl: "com.ProxyConfigDaemon"
 end
