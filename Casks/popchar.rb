32043ba89e22794b9f8f4c8c118b28635dcc2452
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:40 +0100

    popchar: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/popchar.rb
+++ Casks/popchar.rb
@@ -1,12 +1,12 @@
-cask 'popchar' do
-  version '8.10'
-  sha256 'cac9830de0ac9d1fa01b44e91315be2c60d68978b61968c3a8b9211e983e1f4b'
+cask "popchar" do
+  version "8.10"
+  sha256 "cac9830de0ac9d1fa01b44e91315be2c60d68978b61968c3a8b9211e983e1f4b"
 
   url "https://www.ergonis.com/downloads/products/popcharx/PopCharX#{version.no_dots}-Install.dmg",
       user_agent: :fake
-  appcast 'https://update.ergonis.com/vck/popcharx.xml'
-  name 'PopChar X'
-  homepage 'https://www.ergonis.com/products/popcharx/'
+  appcast "https://update.ergonis.com/vck/popcharx.xml"
+  name "PopChar X"
+  homepage "https://www.ergonis.com/products/popcharx/"
 
-  app 'PopChar.app'
+  app "PopChar.app"
 end
