faaee80ba73e7bc5abf00ef4c5ac04ee184a60a5
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:07 +0100

    roboform: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/roboform.rb
+++ Casks/roboform.rb
@@ -1,30 +1,30 @@
-cask 'roboform' do
-  version '8.9.2'
-  sha256 '3343cc86b670cc3e137b1d040b1537d28c4d3c87441bca42aedebe5658a473e0'
+cask "roboform" do
+  version "8.9.2"
+  sha256 "3343cc86b670cc3e137b1d040b1537d28c4d3c87441bca42aedebe5658a473e0"
 
   url "https://www.roboform.com/dist/roboform-mac-v#{version.major}.dmg"
-  appcast 'https://www.roboform.com/news-mac'
-  name 'RoboForm'
-  homepage 'https://www.roboform.com/'
+  appcast "https://www.roboform.com/news-mac"
+  name "RoboForm"
+  homepage "https://www.roboform.com/"
 
   auto_updates true
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'RoboForm.app'
+  app "RoboForm.app"
 
   zap trash: [
-               '~/Library/Application Scripts/com.SiberSystems.RoboForm.safari-companion*',
-               '~/Library/Application Support/RoboForm',
-               '~/Library/Application Support/com.SiberSystems.RoboForm',
-               '~/Library/Application Support/com.SiberSystems.RoboForm.RoboFormService',
-               '~/Library/Caches/com.SiberSystems.RoboForm',
-               '~/Library/Caches/com.SiberSystems.RoboForm.RoboFormService',
-               '~/Library/Caches/com.crashlytics.data/com.SiberSystems.RoboForm',
-               '~/Library/Caches/com.crashlytics.data/com.SiberSystems.RoboForm.RoboFormService',
-               '~/Library/Caches/io.fabric.sdk.mac.data/com.SiberSystems.RoboForm',
-               '~/Library/Caches/io.fabric.sdk.mac.data/com.SiberSystems.RoboForm.RoboFormService',
-               '~/Library/Containers/com.SiberSystems.RoboForm.safari-companion*',
-               '~/Library/Group Containers/*.com.SiberSystems.RoboForm',
-               '~/Library/Saved Application State/com.SiberSystems.RoboForm.savedState',
-             ]
+    "~/Library/Application Scripts/com.SiberSystems.RoboForm.safari-companion*",
+    "~/Library/Application Support/RoboForm",
+    "~/Library/Application Support/com.SiberSystems.RoboForm",
+    "~/Library/Application Support/com.SiberSystems.RoboForm.RoboFormService",
+    "~/Library/Caches/com.SiberSystems.RoboForm",
+    "~/Library/Caches/com.SiberSystems.RoboForm.RoboFormService",
+    "~/Library/Caches/com.crashlytics.data/com.SiberSystems.RoboForm",
+    "~/Library/Caches/com.crashlytics.data/com.SiberSystems.RoboForm.RoboFormService",
+    "~/Library/Caches/io.fabric.sdk.mac.data/com.SiberSystems.RoboForm",
+    "~/Library/Caches/io.fabric.sdk.mac.data/com.SiberSystems.RoboForm.RoboFormService",
+    "~/Library/Containers/com.SiberSystems.RoboForm.safari-companion*",
+    "~/Library/Group Containers/*.com.SiberSystems.RoboForm",
+    "~/Library/Saved Application State/com.SiberSystems.RoboForm.savedState",
+  ]
 end
