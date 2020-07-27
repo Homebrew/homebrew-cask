bb2182c5e67b713ecc6d1714bbc90a2bd4f5fc9c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:13 +0100

    qctools: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/qctools.rb
+++ Casks/qctools.rb
@@ -1,11 +1,11 @@
-cask 'qctools' do
-  version '1.1'
-  sha256 '53a250ccfad607dc04820c8159a3cc006c5c2ccb3b272d38ab1ddf011a81ef3e'
+cask "qctools" do
+  version "1.1"
+  sha256 "53a250ccfad607dc04820c8159a3cc006c5c2ccb3b272d38ab1ddf011a81ef3e"
 
   url "https://mediaarea.net/download/binary/qctools/#{version}/QCTools_#{version}_mac.dmg"
-  appcast 'https://github.com/bavc/qctools/releases.atom'
-  name 'QCTools'
-  homepage 'https://mediaarea.net/QCTools'
+  appcast "https://github.com/bavc/qctools/releases.atom"
+  name "QCTools"
+  homepage "https://mediaarea.net/QCTools"
 
-  app 'QCTools.app'
+  app "QCTools.app"
 end
