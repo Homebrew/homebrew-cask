b7ecad76952fb63f904b61c70769c8ed46e07fbc
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:30:29 +0100

    microsoft-outlook: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/microsoft-outlook.rb
+++ Casks/microsoft-outlook.rb
@@ -1,43 +1,43 @@
-cask 'microsoft-outlook' do
-  version '16.39.20071300'
-  sha256 '764d3c2532cffb76029ca96928f67db849fe32ade963df06c5a485807c3e408b'
+cask "microsoft-outlook" do
+  version "16.39.20071300"
+  sha256 "764d3c2532cffb76029ca96928f67db849fe32ade963df06c5a485807c3e408b"
 
   # officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/ was verified as official when first introduced to the cask
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Outlook_#{version}_Installer.pkg"
-  appcast 'https://docs.microsoft.com/en-us/officeupdates/update-history-office-for-mac'
-  name 'Microsoft Outlook'
-  homepage 'https://products.office.com/en-us/outlook/email-and-calendar-software-microsoft-outlook'
+  appcast "https://docs.microsoft.com/en-us/officeupdates/update-history-office-for-mac"
+  name "Microsoft Outlook"
+  homepage "https://products.office.com/en-us/outlook/email-and-calendar-software-microsoft-outlook"
 
   auto_updates true
-  conflicts_with cask: 'microsoft-office'
-  depends_on macos: '>= :sierra'
+  conflicts_with cask: "microsoft-office"
+  depends_on macos: ">= :sierra"
 
   pkg "Microsoft_Outlook_#{version}_Installer.pkg"
 
   uninstall pkgutil:   [
-                         'com.microsoft.package.Microsoft_Outlook.app',
-                         'com.microsoft.pkg.licensing',
-                       ],
-            launchctl: 'com.microsoft.office.licensingV2.helper',
-            quit:      'com.microsoft.autoupdate2'
+    "com.microsoft.package.Microsoft_Outlook.app",
+    "com.microsoft.pkg.licensing",
+  ],
+            launchctl: "com.microsoft.office.licensingV2.helper",
+            quit:      "com.microsoft.autoupdate2"
 
   zap trash:     [
-                   '~/Library/Application Scripts/com.microsoft.Outlook',
-                   '~/Library/Caches/Microsoft/uls/com.microsoft.autoupdate.fba',
-                   '~/Library/Caches/com.microsoft.autoupdate.fba',
-                   '~/Library/Caches/com.microsoft.autoupdate2',
-                   '~/Library/Containers/com.microsoft.Outlook',
-                   '~/Library/Preferences/com.microsoft.autoupdate.fba.plist',
-                   '~/Library/Preferences/com.microsoft.autoupdate2.plist',
-                 ],
+    "~/Library/Application Scripts/com.microsoft.Outlook",
+    "~/Library/Caches/Microsoft/uls/com.microsoft.autoupdate.fba",
+    "~/Library/Caches/com.microsoft.autoupdate.fba",
+    "~/Library/Caches/com.microsoft.autoupdate2",
+    "~/Library/Containers/com.microsoft.Outlook",
+    "~/Library/Preferences/com.microsoft.autoupdate.fba.plist",
+    "~/Library/Preferences/com.microsoft.autoupdate2.plist",
+  ],
       rmdir:     [
-                   '~/Library/Caches/Microsoft/uls',
-                   '~/Library/Caches/Microsoft',
-                 ],
+        "~/Library/Caches/Microsoft/uls",
+        "~/Library/Caches/Microsoft",
+      ],
       launchctl: [
-                   'com.microsoft.autoupdate.helpertool',
-                   'com.microsoft.autoupdate.helper',
-                   'com.microsoft.update.agent',
-                 ],
-      pkgutil:   'com.microsoft.package.Microsoft_AutoUpdate.app'
+        "com.microsoft.autoupdate.helpertool",
+        "com.microsoft.autoupdate.helper",
+        "com.microsoft.update.agent",
+      ],
+      pkgutil:   "com.microsoft.package.Microsoft_AutoUpdate.app"
 end
