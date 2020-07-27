35884549653d7ea70295afac52a85bae9b2a5175
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:13:37 +0100

    adoptopenjdk: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/adoptopenjdk.rb
+++ Casks/adoptopenjdk.rb
@@ -1,12 +1,12 @@
-cask 'adoptopenjdk' do
-  version '14.0.2,12'
-  sha256 '9a5d065b47f98632fc9398d3d84086658af3cf8c1297ca1aa576db95918bca4d'
+cask "adoptopenjdk" do
+  version "14.0.2,12"
+  sha256 "9a5d065b47f98632fc9398d3d84086658af3cf8c1297ca1aa576db95918bca4d"
 
   # github.com/AdoptOpenJDK/ was verified as official when first introduced to the cask
   url "https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/download/jdk-#{version.before_comma}%2B#{version.after_comma}/OpenJDK#{version.major}U-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma}.pkg"
   appcast "https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/latest"
-  name 'AdoptOpenJDK Java Development Kit'
-  homepage 'https://adoptopenjdk.net/'
+  name "AdoptOpenJDK Java Development Kit"
+  homepage "https://adoptopenjdk.net/"
 
   pkg "OpenJDK#{version.major}U-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma}.pkg"
 
@@ -14,7 +14,7 @@ cask 'adoptopenjdk' do
 
   caveats <<~EOS
     More versions are available in the AdoptOpenJDK tap:
-      #{Formatter.url('https://github.com/AdoptOpenJDK/homebrew-openjdk')}
+      #{Formatter.url("https://github.com/AdoptOpenJDK/homebrew-openjdk")}
 
       brew tap adoptopenjdk/openjdk
   EOS
