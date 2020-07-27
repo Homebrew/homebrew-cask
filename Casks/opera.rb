2e761ec1d782c8b3532e40a5071e6c3b1d98b563
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:32:36 +0100

    opera: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,18 +1,18 @@
-cask 'opera' do
-  version '69.0.3686.77'
-  sha256 '9d0a68a9a9d96933bef4c67d85d3d4ccdd56913278548d1568f07cba35faf1a3'
+cask "opera" do
+  version "69.0.3686.77"
+  sha256 "9d0a68a9a9d96933bef4c67d85d3d4ccdd56913278548d1568f07cba35faf1a3"
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
-  appcast 'https://ftp.opera.com/pub/opera/desktop/'
-  name 'Opera'
-  homepage 'https://www.opera.com/'
+  appcast "https://ftp.opera.com/pub/opera/desktop/"
+  name "Opera"
+  homepage "https://www.opera.com/"
 
   auto_updates true
 
-  app 'Opera.app'
+  app "Opera.app"
 
   zap trash: [
-               '~/Library/Preferences/com.operasoftware.Opera.plist',
-               '~/Library/Application Support/com.operasoftware.Opera/',
-             ]
+    "~/Library/Preferences/com.operasoftware.Opera.plist",
+    "~/Library/Application Support/com.operasoftware.Opera/",
+  ]
 end
