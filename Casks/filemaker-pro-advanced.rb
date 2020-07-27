f747b33d359bf5414f9219cc7e58a2b8ce013b71
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:23:55 +0100

    filemaker-pro-advanced: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/filemaker-pro-advanced.rb
+++ Casks/filemaker-pro-advanced.rb
@@ -1,11 +1,11 @@
-cask 'filemaker-pro-advanced' do
-  version '18.0.3.317'
-  sha256 '4eba410556271b6a60eaed58450e2539b10281698ba854c96ab10c564040eda0'
+cask "filemaker-pro-advanced" do
+  version "18.0.3.317"
+  sha256 "4eba410556271b6a60eaed58450e2539b10281698ba854c96ab10c564040eda0"
 
   url "https://fmdl.filemaker.com/esd/fmpa_#{version}.dmg"
-  appcast 'https://www.filemaker.com/redirects/ss.txt'
-  name 'FileMaker Pro Advanced'
-  homepage 'https://www.filemaker.com/'
+  appcast "https://www.filemaker.com/redirects/ss.txt"
+  name "FileMaker Pro Advanced"
+  homepage "https://www.filemaker.com/"
 
   auto_updates true
 
@@ -13,12 +13,12 @@ cask 'filemaker-pro-advanced' do
 
   preflight do
     file = "#{staged_path}/Assisted Install.txt"
-    IO.write(file, IO.read(file).sub('AI_LICENSE_ACCEPTED=0', 'AI_LICENSE_ACCEPTED=1'))
+    IO.write(file, IO.read(file).sub("AI_LICENSE_ACCEPTED=0", "AI_LICENSE_ACCEPTED=1"))
   end
 
   uninstall pkgutil: "com.filemaker.FMPA#{version.major}*"
 
   caveats do
-    license 'https://www.filemaker.com/company/legal/docs/eula/fmpa_eula_wwe.pdf'
+    license "https://www.filemaker.com/company/legal/docs/eula/fmpa_eula_wwe.pdf"
   end
 end
