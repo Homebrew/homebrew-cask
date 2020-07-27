d57f81db5c8cafd6988bb1b9a86c033a72f79410
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:23:01 +0100

    eggplant: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/eggplant.rb
+++ Casks/eggplant.rb
@@ -1,10 +1,10 @@
-cask 'eggplant' do
+cask "eggplant" do
   version :latest
   sha256 :no_check
 
-  url 'http://downloads.testplant.com/downloads/Eggplant/eggPlant.dmg'
-  name 'eggPlant Functional'
-  homepage 'https://www.testplant.com/dlds/eggplant-functional/'
+  url "http://downloads.testplant.com/downloads/Eggplant/eggPlant.dmg"
+  name "eggPlant Functional"
+  homepage "https://www.testplant.com/dlds/eggplant-functional/"
 
-  app 'eggPlant.app'
+  app "eggPlant.app"
 end
