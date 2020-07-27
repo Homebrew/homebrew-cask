9f9656448e8178e9f2a3256b64ffa072a4c0199e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:32:44 +0100

    outguess: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/outguess.rb
+++ Casks/outguess.rb
@@ -1,13 +1,13 @@
-cask 'outguess' do
+cask "outguess" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.rbcafe.com/download/outguess.zip',
+  url "https://www.rbcafe.com/download/outguess.zip",
       user_agent: :fake
-  name 'Outguess'
-  homepage 'https://www.rbcafe.com/software/outguess/'
+  name "Outguess"
+  homepage "https://www.rbcafe.com/software/outguess/"
 
-  depends_on macos: '>= :el_capitan'
+  depends_on macos: ">= :el_capitan"
 
-  app 'Outguess.app'
+  app "Outguess.app"
 end
