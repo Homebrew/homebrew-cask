2b611636efba33a4a138487d44285202007b26c7
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:17 +0100

    sonarr: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/sonarr.rb
+++ Casks/sonarr.rb
@@ -1,18 +1,18 @@
-cask 'sonarr' do
+cask "sonarr" do
   version :latest
   sha256 :no_check
 
-  url 'https://download.sonarr.tv/v2/master/latest/NzbDrone.master.osx.zip'
-  name 'Sonarr'
-  homepage 'https://sonarr.tv/'
+  url "https://download.sonarr.tv/v2/master/latest/NzbDrone.master.osx.zip"
+  name "Sonarr"
+  homepage "https://sonarr.tv/"
 
-  depends_on cask: 'mono-mdk'
+  depends_on cask: "mono-mdk"
 
-  app 'Sonarr.app'
+  app "Sonarr.app"
 
   zap trash: [
-               '~/Library/Application Support/Sonarr',
-               '~/.config/NzbDrone',
-               '~/Library/Preferences/tv.sonarr.Sonarr.plist',
-             ]
+    "~/Library/Application Support/Sonarr",
+    "~/.config/NzbDrone",
+    "~/Library/Preferences/tv.sonarr.Sonarr.plist",
+  ]
 end
