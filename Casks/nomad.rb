8e59f9d47ac79c3b24bce14ef9c150811b7d81e7
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:31:52 +0100

    nomad: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/nomad.rb
+++ Casks/nomad.rb
@@ -1,20 +1,20 @@
-cask 'nomad' do
+cask "nomad" do
   version :latest
   sha256 :no_check
 
-  url 'https://files.nomad.menu/NoMAD.pkg'
-  name 'NoMAD'
-  homepage 'https://nomad.menu/'
+  url "https://files.nomad.menu/NoMAD.pkg"
+  name "NoMAD"
+  homepage "https://nomad.menu/"
 
-  depends_on macos: '>= :yosemite'
+  depends_on macos: ">= :yosemite"
 
-  pkg 'NoMAD.pkg'
+  pkg "NoMAD.pkg"
 
-  uninstall pkgutil: 'com.trusourcelabs.NoMAD'
+  uninstall pkgutil: "com.trusourcelabs.NoMAD"
 
   zap trash: [
-               '~/Library/Application Support/CrashReporter/NoMAD*',
-               '~/Library/Logs/DiagnosticReports/NoMAD*',
-               '~/Library/Preferences/com.trusourcelabs.NoMAD.plist',
-             ]
+    "~/Library/Application Support/CrashReporter/NoMAD*",
+    "~/Library/Logs/DiagnosticReports/NoMAD*",
+    "~/Library/Preferences/com.trusourcelabs.NoMAD.plist",
+  ]
 end
