50af8daf73ee17651be29c901b58cd8c5ccd96b2
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:37:20 +0100

    textexpander: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/textexpander.rb
+++ Casks/textexpander.rb
@@ -1,17 +1,17 @@
-cask 'textexpander' do
-  version '6.5.5'
-  sha256 'ff6b054397d1065da36789b9d84c8a76a89960488233127ac8cfdf2bbdd5dd9d'
+cask "textexpander" do
+  version "6.5.5"
+  sha256 "ff6b054397d1065da36789b9d84c8a76a89960488233127ac8cfdf2bbdd5dd9d"
 
   # cdn.textexpander.com/mac/ was verified as official when first introduced to the cask
   url "https://cdn.textexpander.com/mac/TextExpander_#{version}.zip"
   appcast "https://smilesoftware.com/appcast/TextExpander#{version.major}.xml"
-  name 'TextExpander'
-  homepage 'https://smilesoftware.com/TextExpander'
+  name "TextExpander"
+  homepage "https://smilesoftware.com/TextExpander"
 
   auto_updates true
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'TextExpander.app'
+  app "TextExpander.app"
 
-  zap trash: '~/Library/Application Support/TextExpander/'
+  zap trash: "~/Library/Application Support/TextExpander/"
 end
