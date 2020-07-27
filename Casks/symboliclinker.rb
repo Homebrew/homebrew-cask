a2ca2834d1cbdf02d5297e79795c2f935b5377c4
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:37:00 +0100

    symboliclinker: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/symboliclinker.rb
+++ Casks/symboliclinker.rb
@@ -1,11 +1,11 @@
-cask 'symboliclinker' do
-  version '2.1.1'
-  sha256 '58ed5a7992686f75981e4fe79678a9b9350267c487dd9cb4dc875ab6c87cfe08'
+cask "symboliclinker" do
+  version "2.1.1"
+  sha256 "58ed5a7992686f75981e4fe79678a9b9350267c487dd9cb4dc875ab6c87cfe08"
 
   url "https://github.com/nickzman/symboliclinker/releases/download/v#{version}/SymbolicLinker#{version}.dmg"
-  appcast 'https://github.com/nickzman/symboliclinker/releases.atom'
-  name 'SymbolicLinker'
-  homepage 'https://github.com/nickzman/symboliclinker'
+  appcast "https://github.com/nickzman/symboliclinker/releases.atom"
+  name "SymbolicLinker"
+  homepage "https://github.com/nickzman/symboliclinker"
 
-  service 'SymbolicLinker.service'
+  service "SymbolicLinker.service"
 end
