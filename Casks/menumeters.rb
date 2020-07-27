bd3487afb1474cb5259309ebb7b06d31fbbf9932
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:30:18 +0100

    menumeters: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/menumeters.rb
+++ Casks/menumeters.rb
@@ -1,23 +1,23 @@
-cask 'menumeters' do
-  version '2.0.7'
-  sha256 'a63217c32670f19913f2f814c91b760b5df57413649440e8f1c80821b763713e'
+cask "menumeters" do
+  version "2.0.7"
+  sha256 "a63217c32670f19913f2f814c91b760b5df57413649440e8f1c80821b763713e"
 
   url "https://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/zips/MenuMeters_#{version}.zip"
-  appcast 'https://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/MenuMeters-Update.xml'
-  name 'MenuMeters for El Capitan (and later)'
-  homepage 'https://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/'
+  appcast "https://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/MenuMeters-Update.xml"
+  name "MenuMeters for El Capitan (and later)"
+  homepage "https://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/"
 
   auto_updates true
-  depends_on macos: '>= :el_capitan'
+  depends_on macos: ">= :el_capitan"
 
-  app 'MenuMeters.app'
+  app "MenuMeters.app"
 
-  uninstall quit: 'com.yujitach.MenuMeters'
+  uninstall quit: "com.yujitach.MenuMeters"
 
   zap trash: [
-               '~/Library/Caches/com.yujitach.MenuMeters',
-               '~/Library/PreferencesPanes/MenuMeters.prefPane',
-               '~/Library/Preferences/com.ragingmenace.MenuMeters.plist',
-               '~/Library/Preferences/com.yujitach.MenuMeters.plist',
-             ]
+    "~/Library/Caches/com.yujitach.MenuMeters",
+    "~/Library/PreferencesPanes/MenuMeters.prefPane",
+    "~/Library/Preferences/com.ragingmenace.MenuMeters.plist",
+    "~/Library/Preferences/com.yujitach.MenuMeters.plist",
+  ]
 end
