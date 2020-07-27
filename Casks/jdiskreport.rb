540c9012ba1d84fe9ada03403927bdf624b56ceb
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:27:43 +0100

    jdiskreport: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/jdiskreport.rb
+++ Casks/jdiskreport.rb
@@ -1,10 +1,10 @@
-cask 'jdiskreport' do
-  version '1.4.1'
-  sha256 'cb64f3d7ea8e43b142a69cabdded832b8de8e503766ccf1c7a16b314d2bc0d44'
+cask "jdiskreport" do
+  version "1.4.1"
+  sha256 "cb64f3d7ea8e43b142a69cabdded832b8de8e503766ccf1c7a16b314d2bc0d44"
 
   url "http://www.jgoodies.com/download/jdiskreport/jdiskreport-#{version.dots_to_underscores}-mac.zip"
-  name 'JDiskReport'
-  homepage 'http://www.jgoodies.com/freeware/jdiskreport/'
+  name "JDiskReport"
+  homepage "http://www.jgoodies.com/freeware/jdiskreport/"
 
   app "JDiskReport #{version}/JDiskReport.app"
 end
