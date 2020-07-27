3caa81f8bf9c6dcd7b3e9e83bec374eed10c9951
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:38:00 +0100

    ueli: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/ueli.rb
+++ Casks/ueli.rb
@@ -1,20 +1,20 @@
-cask 'ueli' do
-  version '8.8.0'
-  sha256 'ce24b8e1de20e19d168b7d6337db5648d0eec9d64726cd9b2ca031b011f516d8'
+cask "ueli" do
+  version "8.8.0"
+  sha256 "ce24b8e1de20e19d168b7d6337db5648d0eec9d64726cd9b2ca031b011f516d8"
 
   # github.com/oliverschwendener/ueli/ was verified as official when first introduced to the cask
   url "https://github.com/oliverschwendener/ueli/releases/download/v#{version}/ueli-#{version}.dmg"
-  appcast 'https://github.com/oliverschwendener/ueli/releases.atom'
-  name 'Ueli'
-  homepage 'https://ueli.app/'
+  appcast "https://github.com/oliverschwendener/ueli/releases.atom"
+  name "Ueli"
+  homepage "https://ueli.app/"
 
-  app 'ueli.app'
+  app "ueli.app"
 
-  uninstall quit: 'ueli'
+  uninstall quit: "ueli"
 
   zap trash: [
-               '~/Library/Logs/ueli',
-               '~/Library/Application Support/ueli',
-               '~/Library/Preferences/com.electron.ueli.plist',
-             ]
+    "~/Library/Logs/ueli",
+    "~/Library/Application Support/ueli",
+    "~/Library/Preferences/com.electron.ueli.plist",
+  ]
 end
