c39906e13f5aefce4753b947a1a65de391333858
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:20:50 +0100

    compositor: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/compositor.rb
+++ Casks/compositor.rb
@@ -1,19 +1,19 @@
-cask 'compositor' do
-  version '1.14.0'
-  sha256 '9ede6513b3ff29d229a697f4cd3da445017c99e0221d744d506904be08c5a7b9'
+cask "compositor" do
+  version "1.14.0"
+  sha256 "9ede6513b3ff29d229a697f4cd3da445017c99e0221d744d506904be08c5a7b9"
 
   url "https://compositorapp.com/updates/Compositor_#{version}.zip"
-  appcast 'https://compositorapp.com/updates/appcast.xml'
-  name 'Compositor'
-  homepage 'https://compositorapp.com/'
+  appcast "https://compositorapp.com/updates/appcast.xml"
+  name "Compositor"
+  homepage "https://compositorapp.com/"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'Compositor.app'
+  app "Compositor.app"
 
   zap trash: [
-               '~/Library/Application Scripts/com.microlarge.Compositor',
-               '~/Library/Containers/com.microlarge.Compositor',
-               '~/Library/Preferences/com.microlarge.Compositor.plist',
-             ]
+    "~/Library/Application Scripts/com.microlarge.Compositor",
+    "~/Library/Containers/com.microlarge.Compositor",
+    "~/Library/Preferences/com.microlarge.Compositor.plist",
+  ]
 end
