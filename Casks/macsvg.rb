0d3b250917110c31772c881fee1d8bb0d95b8762
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:35 +0100

    macsvg: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/macsvg.rb
+++ Casks/macsvg.rb
@@ -1,19 +1,19 @@
-cask 'macsvg' do
-  version '1.1.7'
-  sha256 'a0dbf7636296accf0cee1df7635e8f2a379d32bca48f055030143e0b11091bc8'
+cask "macsvg" do
+  version "1.1.7"
+  sha256 "a0dbf7636296accf0cee1df7635e8f2a379d32bca48f055030143e0b11091bc8"
 
   # github.com/dsward2/macSVG/ was verified as official when first introduced to the cask
   url "https://github.com/dsward2/macSVG/releases/download/v#{version}/macSVG-v#{version}.zip"
-  appcast 'https://github.com/dsward2/macSVG/releases.atom'
-  name 'macSVG'
-  homepage 'https://macsvg.org/'
+  appcast "https://github.com/dsward2/macSVG/releases.atom"
+  name "macSVG"
+  homepage "https://macsvg.org/"
 
-  depends_on macos: '>= :mojave'
+  depends_on macos: ">= :mojave"
 
-  app 'macSVG.app'
+  app "macSVG.app"
 
   zap trash: [
-               '~/Library/Application Scripts/com.arkphone.macsvg',
-               '~/Library/Containers/com.arkphone.macsvg',
-             ]
+    "~/Library/Application Scripts/com.arkphone.macsvg",
+    "~/Library/Containers/com.arkphone.macsvg",
+  ]
 end
