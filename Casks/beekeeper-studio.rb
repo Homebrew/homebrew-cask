e4a861516f06873d80ec30b49f72798df5695027
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:17:52 +0100

    beekeeper-studio: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/beekeeper-studio.rb
+++ Casks/beekeeper-studio.rb
@@ -1,24 +1,24 @@
-cask 'beekeeper-studio' do
-  version '1.6.10'
-  sha256 'd0e4cf88a42a3876d3c4ddcd384b8c5e104a874b670b2480d65bce8d0a37914d'
+cask "beekeeper-studio" do
+  version "1.6.10"
+  sha256 "d0e4cf88a42a3876d3c4ddcd384b8c5e104a874b670b2480d65bce8d0a37914d"
 
   # github.com/beekeeper-studio/beekeeper-studio/ was verified as official when first introduced to the cask
   url "https://github.com/beekeeper-studio/beekeeper-studio/releases/download/v#{version}/Beekeeper-Studio-#{version}.dmg"
-  appcast 'https://github.com/beekeeper-studio/beekeeper-studio/releases.atom'
-  name 'Beekeeper Studio'
-  homepage 'https://www.beekeeperstudio.io/'
+  appcast "https://github.com/beekeeper-studio/beekeeper-studio/releases.atom"
+  name "Beekeeper Studio"
+  homepage "https://www.beekeeperstudio.io/"
 
   auto_updates true
 
-  app 'Beekeeper Studio.app'
+  app "Beekeeper Studio.app"
 
   zap trash: [
-               '~/Library/Application Support/Caches/beekeeper-studio-updater',
-               '~/Library/Application Support/beekeeper-studio',
-               '~/Library/Caches/io.beekeeperstudio.desktop',
-               '~/Library/Caches/io.beekeeperstudio.desktop.ShipIt',
-               '~/Library/Preferences/ByHost/io.beekeeperstudio.desktop.ShipIt.*.plist',
-               '~/Library/Preferences/io.beekeeperstudio.desktop.plist',
-               '~/Library/Saved Application State/io.beekeeperstudio.desktop.savedState',
-             ]
+    "~/Library/Application Support/Caches/beekeeper-studio-updater",
+    "~/Library/Application Support/beekeeper-studio",
+    "~/Library/Caches/io.beekeeperstudio.desktop",
+    "~/Library/Caches/io.beekeeperstudio.desktop.ShipIt",
+    "~/Library/Preferences/ByHost/io.beekeeperstudio.desktop.ShipIt.*.plist",
+    "~/Library/Preferences/io.beekeeperstudio.desktop.plist",
+    "~/Library/Saved Application State/io.beekeeperstudio.desktop.savedState",
+  ]
 end
