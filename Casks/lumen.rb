02f50ab3d3f0977923ac284a735fc52f6893344c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:18 +0100

    lumen: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/lumen.rb
+++ Casks/lumen.rb
@@ -1,11 +1,11 @@
-cask 'lumen' do
-  version '1.2.0'
-  sha256 'abfdb99bc1a317c6e6b3d8d5b5696bda786fa1bc8e8983971e74b23d4ef3d1f3'
+cask "lumen" do
+  version "1.2.0"
+  sha256 "abfdb99bc1a317c6e6b3d8d5b5696bda786fa1bc8e8983971e74b23d4ef3d1f3"
 
   url "https://github.com/anishathalye/lumen/releases/download/v#{version}/Lumen.zip"
-  appcast 'https://github.com/anishathalye/lumen/releases.atom'
-  name 'Lumen'
-  homepage 'https://github.com/anishathalye/lumen'
+  appcast "https://github.com/anishathalye/lumen/releases.atom"
+  name "Lumen"
+  homepage "https://github.com/anishathalye/lumen"
 
-  app 'Lumen.app'
+  app "Lumen.app"
 end
