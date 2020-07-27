78c59a68564e402b81ef79d0810c14151bdcd1cd
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:25:24 +0100

    globalmeet: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/globalmeet.rb
+++ Casks/globalmeet.rb
@@ -1,16 +1,16 @@
-cask 'globalmeet' do
+cask "globalmeet" do
   version :latest
   sha256 :no_check
 
-  url 'https://convergencedesktop.globalmeet.com/installers/stable/GlobalMeet.dmg'
-  name 'GlobalMeet'
-  homepage 'https://www.globalmeet.com/'
+  url "https://convergencedesktop.globalmeet.com/installers/stable/GlobalMeet.dmg"
+  name "GlobalMeet"
+  homepage "https://www.globalmeet.com/"
 
-  app 'GlobalMeet.app'
+  app "GlobalMeet.app"
 
   zap trash: [
-               '~/Library/Application Support/globalmeet',
-               '~/Library/Logs/GlobalMeet',
-               '~/Library/Saved Application State/com.electron.globalmeet.savedState',
-             ]
+    "~/Library/Application Support/globalmeet",
+    "~/Library/Logs/GlobalMeet",
+    "~/Library/Saved Application State/com.electron.globalmeet.savedState",
+  ]
 end
