1670ab0b412babb527b5f3e09e4efe823d858f5f
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:32:31 +0100

    opensesame: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/opensesame.rb
+++ Casks/opensesame.rb
@@ -1,12 +1,12 @@
-cask 'opensesame' do
-  version '3.3.3'
-  sha256 'f0f8ae1dfbbd5608e96a9ca891d834eae8a64abaed990d100354d53e0f28a210'
+cask "opensesame" do
+  version "3.3.3"
+  sha256 "f0f8ae1dfbbd5608e96a9ca891d834eae8a64abaed990d100354d53e0f28a210"
 
   # github.com/smathot/OpenSesame/ was verified as official when first introduced to the cask
   url "https://github.com/smathot/OpenSesame/releases/download/release%2F#{version}/opensesame_#{version}-py37-macos-1.dmg"
-  appcast 'https://github.com/smathot/OpenSesame/releases.atom'
-  name 'OpenSesame'
-  homepage 'https://osdoc.cogsci.nl/'
+  appcast "https://github.com/smathot/OpenSesame/releases.atom"
+  name "OpenSesame"
+  homepage "https://osdoc.cogsci.nl/"
 
-  app 'opensesame.app'
+  app "opensesame.app"
 end
