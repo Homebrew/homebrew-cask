da23877967698eaaa0fb3141a5b9745af3a3ce36
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:43 +0100

    sharemouse: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/sharemouse.rb
+++ Casks/sharemouse.rb
@@ -1,10 +1,10 @@
-cask 'sharemouse' do
+cask "sharemouse" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.sharemouse.com/ShareMouseSetup.dmg'
-  name 'ShareMouse'
-  homepage 'https://www.sharemouse.com/'
+  url "https://www.sharemouse.com/ShareMouseSetup.dmg"
+  name "ShareMouse"
+  homepage "https://www.sharemouse.com/"
 
-  app 'ShareMouse.app'
+  app "ShareMouse.app"
 end
