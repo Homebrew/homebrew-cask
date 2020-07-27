e70e756c70c0514d4bbc30784543d57c22bde267
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:31:06 +0100

    movist: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/movist.rb
+++ Casks/movist.rb
@@ -1,10 +1,10 @@
-cask 'movist' do
+cask "movist" do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/downloads/samiamwork/Movist/Movist.app.zip'
-  name 'Movist'
-  homepage 'https://github.com/samiamwork/Movist'
+  url "https://github.com/downloads/samiamwork/Movist/Movist.app.zip"
+  name "Movist"
+  homepage "https://github.com/samiamwork/Movist"
 
-  app 'Movist.app'
+  app "Movist.app"
 end
