8a2b0307eb1960c9bba7e24d44c49803c1253040
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:24:14 +0100

    flirc: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/flirc.rb
+++ Casks/flirc.rb
@@ -1,11 +1,11 @@
-cask 'flirc' do
-  version '3.25.3'
-  sha256 'ca01961197f99e232e13d27dd5fe198ef84f476268504e2860507b6530a7f101'
+cask "flirc" do
+  version "3.25.3"
+  sha256 "ca01961197f99e232e13d27dd5fe198ef84f476268504e2860507b6530a7f101"
 
   url "https://flirc.tv/software/flirc-usb/GUI/release/mac/Flirc-#{version}.dmg"
-  appcast 'https://flirc.tv/software/release/gui/mac/appcast.xml'
-  name 'Flirc'
-  homepage 'https://flirc.tv/'
+  appcast "https://flirc.tv/software/release/gui/mac/appcast.xml"
+  name "Flirc"
+  homepage "https://flirc.tv/"
 
-  app 'Flirc.app'
+  app "Flirc.app"
 end
