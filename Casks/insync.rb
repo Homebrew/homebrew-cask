110a0cfae1beb47e6c6afbe252751f9d62b116ea
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:27:03 +0100

    insync: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/insync.rb
+++ Casks/insync.rb
@@ -1,14 +1,14 @@
-cask 'insync' do
-  version '3.2.2.40848'
-  sha256 '155a7ee457e54790b3cdfb4cf3e78cb4b8efe03bc0dc8a257da5d80b6290ab9e'
+cask "insync" do
+  version "3.2.2.40848"
+  sha256 "155a7ee457e54790b3cdfb4cf3e78cb4b8efe03bc0dc8a257da5d80b6290ab9e"
 
   url "http://s.insynchq.com/builds/Insync-#{version}.dmg"
-  appcast 'https://www.insynchq.com/downloads?start=true'
-  name 'Insync'
-  homepage 'https://www.insynchq.com/'
+  appcast "https://www.insynchq.com/downloads?start=true"
+  name "Insync"
+  homepage "https://www.insynchq.com/"
 
   auto_updates true
-  conflicts_with cask: 'insync-beta'
+  conflicts_with cask: "insync-beta"
 
-  app 'Insync.app'
+  app "Insync.app"
 end
