ca4637d1130968e1df3cc3a33df4fafb91eade73
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:35 +0100

    selfcontrol: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/selfcontrol.rb
+++ Casks/selfcontrol.rb
@@ -1,15 +1,15 @@
-cask 'selfcontrol' do
-  version '3.0.2'
-  sha256 '26304906a8256d1794c02ea38f1faec0d048806a3f4c756d1fc0f3412cdd438f'
+cask "selfcontrol" do
+  version "3.0.2"
+  sha256 "26304906a8256d1794c02ea38f1faec0d048806a3f4c756d1fc0f3412cdd438f"
 
   url "https://downloads.selfcontrolapp.com/SelfControl-#{version}.zip"
-  appcast 'https://selfcontrolapp.com/SelfControlAppcast.xml'
-  name 'SelfControl'
-  homepage 'https://selfcontrolapp.com/'
+  appcast "https://selfcontrolapp.com/SelfControlAppcast.xml"
+  name "SelfControl"
+  homepage "https://selfcontrolapp.com/"
 
   auto_updates true
 
-  app 'SelfControl.app'
+  app "SelfControl.app"
 
-  zap trash: '~/Library/Preferences/org.eyebeam.SelfControl.plist'
+  zap trash: "~/Library/Preferences/org.eyebeam.SelfControl.plist"
 end
