a796f5346d7d52bbca6854f54a1cc5ea5c0210aa
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:00 +0100

    retroarch: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/retroarch.rb
+++ Casks/retroarch.rb
@@ -1,11 +1,11 @@
-cask 'retroarch' do
-  version '1.8.9'
-  sha256 '4b7e5510fcb4d15e8203679c310cebdac66dde209c017e0b582d08271945cab1'
+cask "retroarch" do
+  version "1.8.9"
+  sha256 "4b7e5510fcb4d15e8203679c310cebdac66dde209c017e0b582d08271945cab1"
 
   url "https://buildbot.libretro.com/stable/#{version}/apple/osx/x86_64/RetroArch.dmg"
-  appcast 'https://buildbot.libretro.com/stable/'
-  name 'RetroArch'
-  homepage 'https://www.libretro.com/'
+  appcast "https://buildbot.libretro.com/stable/"
+  name "RetroArch"
+  homepage "https://www.libretro.com/"
 
-  app 'RetroArch.app'
+  app "RetroArch.app"
 end
