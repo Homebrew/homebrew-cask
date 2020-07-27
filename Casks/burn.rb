ce60496ccd9d704886812b4ac18261246c3bfd79
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:19:06 +0100

    burn: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/burn.rb
+++ Casks/burn.rb
@@ -1,12 +1,12 @@
-cask 'burn' do
-  version '2.7.9'
-  sha256 '5b0c2a5a8676475b43048149d92dc3496b83ff3ec4f9a585f5112afe267f2ad7'
+cask "burn" do
+  version "2.7.9"
+  sha256 "5b0c2a5a8676475b43048149d92dc3496b83ff3ec4f9a585f5112afe267f2ad7"
 
   # downloads.sourceforge.net/burn-osx/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/burn-osx/Burn/#{version}/burn-#{version}.zip"
-  appcast 'https://sourceforge.net/projects/burn-osx/rss?path=/Burn'
-  name 'Burn'
-  homepage 'https://burn-osx.sourceforge.io/'
+  appcast "https://sourceforge.net/projects/burn-osx/rss?path=/Burn"
+  name "Burn"
+  homepage "https://burn-osx.sourceforge.io/"
 
-  app 'Burn.localized/Burn.app'
+  app "Burn.localized/Burn.app"
 end
