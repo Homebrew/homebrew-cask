ca13f0dd5b034a2968f141711f4fc6df8eb7d860
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:50 +0100

    presentation: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/presentation.rb
+++ Casks/presentation.rb
@@ -1,13 +1,13 @@
-cask 'presentation' do
-  version '2.2.1'
-  sha256 'a782153138ac65e71c976365494beabddcccbec509df592af57a3fe8d188a7a0'
+cask "presentation" do
+  version "2.2.1"
+  sha256 "a782153138ac65e71c976365494beabddcccbec509df592af57a3fe8d188a7a0"
 
   url "http://iihm.imag.fr/blanch/software/osx-presentation/releases/osx-presentation-#{version}.pkg"
-  appcast 'http://iihm.imag.fr/blanch/software/osx-presentation/'
-  name 'Présentation'
-  homepage 'http://iihm.imag.fr/blanch/software/osx-presentation/'
+  appcast "http://iihm.imag.fr/blanch/software/osx-presentation/"
+  name "Présentation"
+  homepage "http://iihm.imag.fr/blanch/software/osx-presentation/"
 
   pkg "osx-presentation-#{version}.pkg"
 
-  uninstall pkgutil: 'fr.imag.iihm.blanch.osx-presentation'
+  uninstall pkgutil: "fr.imag.iihm.blanch.osx-presentation"
 end
