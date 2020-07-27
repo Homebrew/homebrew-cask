52490e1d2da3730031fd7fec94e27bf7db57ea1a
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:26 +0100

    screencast: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/screencast.rb
+++ Casks/screencast.rb
@@ -1,11 +1,11 @@
-cask 'screencast' do
-  version '0.0.6'
-  sha256 '0fb23d34ee3c94eb47233b42d2ac93de84d14dcdafe1e7205ce0a4341ee812f0'
+cask "screencast" do
+  version "0.0.6"
+  sha256 "0fb23d34ee3c94eb47233b42d2ac93de84d14dcdafe1e7205ce0a4341ee812f0"
 
   url "https://github.com/soh335/Screencast/releases/download/#{version}/Screencast.dmg"
-  appcast 'https://github.com/soh335/Screencast/releases.atom'
-  name 'Screencast'
-  homepage 'https://github.com/soh335/Screencast'
+  appcast "https://github.com/soh335/Screencast/releases.atom"
+  name "Screencast"
+  homepage "https://github.com/soh335/Screencast"
 
-  app 'Screencast.app'
+  app "Screencast.app"
 end
