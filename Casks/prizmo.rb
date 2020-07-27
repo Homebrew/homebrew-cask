4948baac894b7187b7a35ce46d330f9f72202455
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:55 +0100

    prizmo: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/prizmo.rb
+++ Casks/prizmo.rb
@@ -1,14 +1,14 @@
-cask 'prizmo' do
-  version '4.0.4'
-  sha256 '9fc1720e1bc0dc07971899dadd9c2c113c142fb89f075c3d2c2dd738dea7e00f'
+cask "prizmo" do
+  version "4.0.4"
+  sha256 "9fc1720e1bc0dc07971899dadd9c2c113c142fb89f075c3d2c2dd738dea7e00f"
 
   url "https://www.creaceed.com/downloads/prizmo#{version.major}_#{version}.zip"
   appcast "https://www.creaceed.com/appcasts/prizmo#{version.major}.xml"
-  name 'Prizmo'
-  homepage 'https://creaceed.com/prizmo'
+  name "Prizmo"
+  homepage "https://creaceed.com/prizmo"
 
   auto_updates true
-  depends_on macos: '>= :yosemite'
+  depends_on macos: ">= :yosemite"
 
-  app 'Prizmo.app'
+  app "Prizmo.app"
 end
