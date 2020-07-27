b07ee0bc52c9a848e0220a1d1ff7f53c5501233f
Randall <ran.dall@icloud.com>
2020-07-26 18:37:24 -0700

    Update freeorion from 0.4.9,2020-02-02.db53471 to 0.4.10,2020-07-10.f3d403e

--- Casks/freeorion.rb
+++ Casks/freeorion.rb
@@ -1,6 +1,6 @@
 cask 'freeorion' do
-  version '0.4.9,2020-02-02.db53471'
-  sha256 'b90858dd1869166836e77497237b215609577247462b0a54def1e71425f694f7'
+  version '0.4.10,2020-07-10.f3d403e'
+  sha256 'fa9db8fd170915283775ccc052d80f5b4c3a7626ac57f3e00a4b321692b3e040'
 
   # github.com/freeorion/ was verified as official when first introduced to the cask
   url "https://github.com/freeorion/freeorion/releases/download/v#{version.before_comma}/FreeOrion_v#{version.before_comma}_#{version.after_comma}_MacOSX_10.9.dmg"
