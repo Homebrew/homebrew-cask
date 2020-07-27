173244f2e4dde722499642f67d8ae8f4bdc1a5f5
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:26:38 +0100

    iceberg: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/iceberg.rb
+++ Casks/iceberg.rb
@@ -1,20 +1,20 @@
-cask 'iceberg' do
-  version '1.3.1'
-  sha256 '08ba0cb792dd2121e4a492df03967c6c97d456bab4afda4b40400cd44ff01bd8'
+cask "iceberg" do
+  version "1.3.1"
+  sha256 "08ba0cb792dd2121e4a492df03967c6c97d456bab4afda4b40400cd44ff01bd8"
 
-  url 'http://s.sudre.free.fr/Software/files/Iceberg.dmg'
-  appcast 'http://s.sudre.free.fr/Software/documentation/Iceberg/English.lproj/documentation/Release%20Notes.html'
-  name 'Iceberg'
-  homepage 'http://s.sudre.free.fr/Software/Iceberg.html'
+  url "http://s.sudre.free.fr/Software/files/Iceberg.dmg"
+  appcast "http://s.sudre.free.fr/Software/documentation/Iceberg/English.lproj/documentation/Release%20Notes.html"
+  name "Iceberg"
+  homepage "http://s.sudre.free.fr/Software/Iceberg.html"
 
-  pkg 'Iceberg.mpkg'
+  pkg "Iceberg.mpkg"
 
   uninstall pkgutil: [
-                       'fr.white.iceberg.pkg.*',
-                       'fr.whitebox.pkg.*',
-                     ],
+    "fr.white.iceberg.pkg.*",
+    "fr.whitebox.pkg.*",
+  ],
             script:  {
-                       executable: 'uninstall.sh',
-                       sudo:       true,
-                     }
+              executable: "uninstall.sh",
+              sudo:       true,
+            }
 end
