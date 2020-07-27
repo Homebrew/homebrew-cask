dd2b6672602c881948fe1c4810798b24166b8e7c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:28:36 +0100

    kui: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/kui.rb
+++ Casks/kui.rb
@@ -1,13 +1,13 @@
-cask 'kui' do
-  version '8.11.3'
-  sha256 '14e91b9b00d5f0f997944c8e8c30c1f807c41f3cb8562831859d6e1848b3d4f2'
+cask "kui" do
+  version "8.11.3"
+  sha256 "14e91b9b00d5f0f997944c8e8c30c1f807c41f3cb8562831859d6e1848b3d4f2"
 
   # github.com/IBM/kui was verified as official when first introduced to the cask
   url "https://github.com/IBM/kui/releases/download/v#{version}/Kui-darwin-x64.tar.bz2"
-  appcast 'https://github.com/IBM/kui/releases.atom'
-  name 'Kui'
-  homepage 'https://kui.tools/'
+  appcast "https://github.com/IBM/kui/releases.atom"
+  name "Kui"
+  homepage "https://kui.tools/"
 
-  app 'Kui-darwin-x64/Kui.app'
+  app "Kui-darwin-x64/Kui.app"
   binary "#{appdir}/Kui.app/Contents/Resources/kubectl-kui"
 end
