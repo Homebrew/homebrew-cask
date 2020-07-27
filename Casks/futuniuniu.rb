d2b156305de10cbbfff6dde04a52cfe33d116bc8
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:24:51 +0100

    futuniuniu: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/futuniuniu.rb
+++ Casks/futuniuniu.rb
@@ -1,15 +1,15 @@
-cask 'futuniuniu' do
-  version '10.7.672_202007071046'
-  sha256 '26b894109cdcfb462a67859c21a9fed2a8cc525135d7b5d033743f0242d8dd45'
+cask "futuniuniu" do
+  version "10.7.672_202007071046"
+  sha256 "26b894109cdcfb462a67859c21a9fed2a8cc525135d7b5d033743f0242d8dd45"
 
   # software-file-1251001049.file.myqcloud.com/ was verified as official when first introduced to the cask
   url "https://software-file-1251001049.file.myqcloud.com/FTNNForMac_#{version}_Website.dmg"
-  appcast 'https://www.futu5.com/tools'
-  name 'FutuNiuniu'
-  name '富途牛牛'
-  homepage 'https://hk.futu5.com/'
+  appcast "https://www.futu5.com/tools"
+  name "FutuNiuniu"
+  name "富途牛牛"
+  homepage "https://hk.futu5.com/"
 
   # Renamed for consistency: app name is different in the Finder and in a shell.
   # Original discussion: https://github.com/Homebrew/homebrew-cask/pull/7435
-  app 'FutuNiuniu.app', target: '富途牛牛.app'
+  app "FutuNiuniu.app", target: "富途牛牛.app"
 end
