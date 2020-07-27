55361eefc535f21e370a1c4dea0baefa5cdb0477
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:23:01 +0100

    eiskaltdcpp: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/eiskaltdcpp.rb
+++ Casks/eiskaltdcpp.rb
@@ -1,11 +1,11 @@
-cask 'eiskaltdcpp' do
-  version '2.2.10-689-macOS10.13'
-  sha256 'b44f0fe56c411a4e158a86077196f7a956cc32b13757368e82be6d0658d93749'
+cask "eiskaltdcpp" do
+  version "2.2.10-689-macOS10.13"
+  sha256 "b44f0fe56c411a4e158a86077196f7a956cc32b13757368e82be6d0658d93749"
 
   url "https://downloads.sourceforge.net/eiskaltdcpp/EiskaltDC++-#{version}-x86_64.dmg"
-  appcast 'https://sourceforge.net/projects/eiskaltdcpp/rss'
-  name 'EiskaltDC++'
-  homepage 'https://sourceforge.net/projects/eiskaltdcpp/'
+  appcast "https://sourceforge.net/projects/eiskaltdcpp/rss"
+  name "EiskaltDC++"
+  homepage "https://sourceforge.net/projects/eiskaltdcpp/"
 
-  app 'EiskaltDC++.app'
+  app "EiskaltDC++.app"
 end
