ec00fa43b4255684f3f905e59bfc7c98f945835a
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:37:10 +0100

    taskade: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/taskade.rb
+++ Casks/taskade.rb
@@ -1,11 +1,11 @@
-cask 'taskade' do
-  version '3.1.1'
-  sha256 '2ef44ffc26d16a3f35fbf742f6add8a97b17ba1e0700dd24fc316d5307c6b2a9'
+cask "taskade" do
+  version "3.1.1"
+  sha256 "2ef44ffc26d16a3f35fbf742f6add8a97b17ba1e0700dd24fc316d5307c6b2a9"
 
   url "https://apps.taskade.com/updates/Taskade-#{version}-mac.zip"
-  appcast 'https://www.taskade.com/downloads'
-  name 'Taskade'
-  homepage 'https://www.taskade.com/'
+  appcast "https://www.taskade.com/downloads"
+  name "Taskade"
+  homepage "https://www.taskade.com/"
 
-  app 'Taskade.app'
+  app "Taskade.app"
 end
