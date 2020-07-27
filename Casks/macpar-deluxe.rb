9b1dd10d6d3743f43b660e0de977f934f32b7a5d
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:33 +0100

    macpar-deluxe: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/macpar-deluxe.rb
+++ Casks/macpar-deluxe.rb
@@ -1,10 +1,10 @@
-cask 'macpar-deluxe' do
+cask "macpar-deluxe" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.xs4all.nl/~gp/MacPAR_deLuxe/MacPARdeLuxe.dmg'
-  name 'MacPAR deLuxe'
-  homepage 'https://gp.home.xs4all.nl/Site/MacPAR_deLuxe.html'
+  url "https://www.xs4all.nl/~gp/MacPAR_deLuxe/MacPARdeLuxe.dmg"
+  name "MacPAR deLuxe"
+  homepage "https://gp.home.xs4all.nl/Site/MacPAR_deLuxe.html"
 
-  app 'MacPAR deLuxe.app'
+  app "MacPAR deLuxe.app"
 end
