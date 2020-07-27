1e3dedf5c95d032d5ceb24387dd4d482a6cf2c62
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:24:40 +0100

    freedom: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/freedom.rb
+++ Casks/freedom.rb
@@ -1,13 +1,13 @@
-cask 'freedom' do
-  version '2.3.2'
-  sha256 'eebeb9c90f356b46dbe6e7fe2fbb90390e110e2d0fae265ecb0f31ba081d59bc'
+cask "freedom" do
+  version "2.3.2"
+  sha256 "eebeb9c90f356b46dbe6e7fe2fbb90390e110e2d0fae265ecb0f31ba081d59bc"
 
   url "https://cdn.freedom.to/installers/updates/mac/#{version}/Freedom.zip"
-  appcast 'https://cdn.freedom.to/installers/updates/mac/Appcast.xml'
-  name 'Freedom'
-  homepage 'https://freedom.to/'
+  appcast "https://cdn.freedom.to/installers/updates/mac/Appcast.xml"
+  name "Freedom"
+  homepage "https://freedom.to/"
 
   auto_updates true
 
-  app 'Freedom.app'
+  app "Freedom.app"
 end
