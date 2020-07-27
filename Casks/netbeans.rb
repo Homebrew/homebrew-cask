e8a3f5d958db80914e86e48e9e196357ff722fe2
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:31:39 +0100

    netbeans: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/netbeans.rb
+++ Casks/netbeans.rb
@@ -1,19 +1,19 @@
-cask 'netbeans' do
-  version '12.0'
-  sha256 'd7f8f857b5f2a1fe54c73721671b9f86933c2d6e63535de5427569754acc85de'
+cask "netbeans" do
+  version "12.0"
+  sha256 "d7f8f857b5f2a1fe54c73721671b9f86933c2d6e63535de5427569754acc85de"
 
   url "https://www-eu.apache.org/dist/netbeans/netbeans/#{version}/Apache-NetBeans-#{version}-bin-macosx.dmg"
-  appcast 'https://www-eu.apache.org/dist/netbeans/netbeans/'
-  name 'NetBeans IDE'
-  homepage 'https://netbeans.apache.org/download/index.html'
+  appcast "https://www-eu.apache.org/dist/netbeans/netbeans/"
+  name "NetBeans IDE"
+  homepage "https://netbeans.apache.org/download/index.html"
 
   pkg "Apache NetBeans #{version}.pkg"
 
-  uninstall pkgutil: 'org.netbeans.ide.*|glassfish.*',
-            delete:  '/Applications/NetBeans'
+  uninstall pkgutil: "org.netbeans.ide.*|glassfish.*",
+            delete:  "/Applications/NetBeans"
 
   zap trash: [
-               '~/Library/Application Support/NetBeans',
-               '~/Library/Caches/NetBeans',
-             ]
+    "~/Library/Application Support/NetBeans",
+    "~/Library/Caches/NetBeans",
+  ]
 end
