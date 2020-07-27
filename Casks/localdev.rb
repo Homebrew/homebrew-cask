03232b1b3ea70d530ad73e09d6efdb3bc97e3982
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:08 +0100

    localdev: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/localdev.rb
+++ Casks/localdev.rb
@@ -1,22 +1,22 @@
-cask 'localdev' do
-  version '0.10.1'
-  sha256 '2d467fb36bc5f9794aca48d06d4b38886e83c3bbb1819ab5239d4a62031849fb'
+cask "localdev" do
+  version "0.10.1"
+  sha256 "2d467fb36bc5f9794aca48d06d4b38886e83c3bbb1819ab5239d4a62031849fb"
 
   # pantheon-localdev.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://pantheon-localdev.s3.amazonaws.com/Localdev-v#{version}.dmg"
-  name 'Localdev'
-  homepage 'https://pantheon.io/localdev'
+  name "Localdev"
+  homepage "https://pantheon.io/localdev"
 
-  app 'Localdev.app'
+  app "Localdev.app"
 
   uninstall delete: [
-                      '~/Library/Application Support/localdev',
-                      '~/Library/Preferences/io.pantheon.local.helper.plist',
-                      '~/Library/Preferences/io.pantheon.local.plist',
-                    ]
+    "~/Library/Application Support/localdev",
+    "~/Library/Preferences/io.pantheon.local.helper.plist",
+    "~/Library/Preferences/io.pantheon.local.plist",
+  ]
 
   zap trash: [
-               '~/Localdev',
-               '~/.pantheonlocaldev',
-             ]
+    "~/Localdev",
+    "~/.pantheonlocaldev",
+  ]
 end
