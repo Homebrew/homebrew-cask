1d6ce4507e44ef49e8f1a2b94261bb5efc646e01
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:27:59 +0100

    jumpshare: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/jumpshare.rb
+++ Casks/jumpshare.rb
@@ -1,21 +1,21 @@
-cask 'jumpshare' do
-  version '2.6.2'
-  sha256 '3ce2bc3d45a1789b125c6a4f3fb8607ef390326077d8cfb88b8c8c98c371ccbf'
+cask "jumpshare" do
+  version "2.6.2"
+  sha256 "3ce2bc3d45a1789b125c6a4f3fb8607ef390326077d8cfb88b8c8c98c371ccbf"
 
   url "https://apps.jumpshare.com/desktop/mac/updates/Jumpshare-#{version}.tar.bz2"
-  appcast 'https://apps.jumpshare.com/desktop/mac/updates/appcast.xml'
-  name 'Jumpshare'
-  homepage 'https://jumpshare.com/'
+  appcast "https://apps.jumpshare.com/desktop/mac/updates/appcast.xml"
+  name "Jumpshare"
+  homepage "https://jumpshare.com/"
 
-  depends_on macos: '>= :high_sierra'
+  depends_on macos: ">= :high_sierra"
 
-  app 'Jumpshare.app'
+  app "Jumpshare.app"
 
   zap trash: [
-               '~/Library/Application Scripts/com.jumpshare.JumpshareLoginHelper',
-               '~/Library/Application Support/com.jumpshare.Jumpshare',
-               '~/Library/Containers/com.jumpshare.JumpshareLoginHelper',
-               '~/Library/Cookies/com.jumpshare.Jumpshare.binarycookies',
-               '~/Library/Preferences/com.jumpshare.Jumpshare.plist',
-             ]
+    "~/Library/Application Scripts/com.jumpshare.JumpshareLoginHelper",
+    "~/Library/Application Support/com.jumpshare.Jumpshare",
+    "~/Library/Containers/com.jumpshare.JumpshareLoginHelper",
+    "~/Library/Cookies/com.jumpshare.Jumpshare.binarycookies",
+    "~/Library/Preferences/com.jumpshare.Jumpshare.plist",
+  ]
 end
