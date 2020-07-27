8d143dcbe5ba5f89240f285771a243846dee1bf0
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:31 +0100

    seafile-client: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/seafile-client.rb
+++ Casks/seafile-client.rb
@@ -1,12 +1,12 @@
-cask 'seafile-client' do
-  version '7.0.8'
-  sha256 'dd0fe6b956719eb473b6fa4ac6fcf1474d5c78d4f2080eecd4060ca1af7ea0af'
+cask "seafile-client" do
+  version "7.0.8"
+  sha256 "dd0fe6b956719eb473b6fa4ac6fcf1474d5c78d4f2080eecd4060ca1af7ea0af"
 
   # seadrive.org/ was verified as official when first introduced to the cask
   url "https://download.seadrive.org/seafile-client-#{version}.dmg"
-  appcast 'https://www.seafile.com/en/download/'
-  name 'Seafile Client'
-  homepage 'https://www.seafile.com/'
+  appcast "https://www.seafile.com/en/download/"
+  name "Seafile Client"
+  homepage "https://www.seafile.com/"
 
-  app 'Seafile Client.app'
+  app "Seafile Client.app"
 end
