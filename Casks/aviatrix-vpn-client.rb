1b48685f2722cfc348c12ac09927ae09498d9de3
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:17:06 +0100

    aviatrix-vpn-client: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/aviatrix-vpn-client.rb
+++ Casks/aviatrix-vpn-client.rb
@@ -1,16 +1,16 @@
-cask 'aviatrix-vpn-client' do
-  version '2.11.6'
-  sha256 '0b7b0913fede47ce456436d86a2931ec486a8a4d495c68f46968e09d4911183b'
+cask "aviatrix-vpn-client" do
+  version "2.11.6"
+  sha256 "0b7b0913fede47ce456436d86a2931ec486a8a4d495c68f46968e09d4911183b"
 
   # aviatrix-download.s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url 'https://aviatrix-download.s3.amazonaws.com/AviatrixVPNClient/AVPNC_mac.pkg'
-  appcast 'https://docs.aviatrix.com/Downloads/samlclient.html'
-  name 'Aviatrix VPN Client'
-  homepage 'https://docs.aviatrix.com/Downloads/samlclient.html'
+  url "https://aviatrix-download.s3.amazonaws.com/AviatrixVPNClient/AVPNC_mac.pkg"
+  appcast "https://docs.aviatrix.com/Downloads/samlclient.html"
+  name "Aviatrix VPN Client"
+  homepage "https://docs.aviatrix.com/Downloads/samlclient.html"
 
-  pkg 'AVPNC_mac.pkg'
+  pkg "AVPNC_mac.pkg"
 
-  uninstall pkgutil:   'com.Aviatrix.VPNClient',
-            delete:    '/Applications/Aviatrix VPN Client.app',
-            launchctl: 'aviatrix.vpn.client.rp.plist'
+  uninstall pkgutil:   "com.Aviatrix.VPNClient",
+            delete:    "/Applications/Aviatrix VPN Client.app",
+            launchctl: "aviatrix.vpn.client.rp.plist"
 end
