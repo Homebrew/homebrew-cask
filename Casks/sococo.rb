6336bc4d88c7b93cd6e3d9251bb747419e73c6de
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:14 +0100

    sococo: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/sococo.rb
+++ Casks/sococo.rb
@@ -1,10 +1,10 @@
-cask 'sococo' do
+cask "sococo" do
   version :latest
   sha256 :no_check
 
-  url 'https://s.sococo.com/rs/client/mac/SococoInstaller.dmg'
-  name 'Sococo'
-  homepage 'https://www.sococo.com/'
+  url "https://s.sococo.com/rs/client/mac/SococoInstaller.dmg"
+  name "Sococo"
+  homepage "https://www.sococo.com/"
 
-  app 'Sococo.app'
+  app "Sococo.app"
 end
