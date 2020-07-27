d4deca3bbf5d0abdd85583aa16182ff7b9d14073
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:28:59 +0100

    lightworks: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/lightworks.rb
+++ Casks/lightworks.rb
@@ -1,13 +1,13 @@
-cask 'lightworks' do
-  version '2020.1_r122068'
-  sha256 'c55738a83be001b2d020af7d77629791838c044428a7e74ca2260735023757a9'
+cask "lightworks" do
+  version "2020.1_r122068"
+  sha256 "c55738a83be001b2d020af7d77629791838c044428a7e74ca2260735023757a9"
 
   url "https://cdn.lwks.com/releases/lightworks_#{version}.dmg"
-  appcast 'https://www.lwks.com/index.php?option=com_lwks&view=download&Itemid=206&tab=2'
-  name 'Lightworks'
-  homepage 'https://www.lwks.com/'
+  appcast "https://www.lwks.com/index.php?option=com_lwks&view=download&Itemid=206&tab=2"
+  name "Lightworks"
+  homepage "https://www.lwks.com/"
 
-  app 'Lightworks.app'
+  app "Lightworks.app"
 
-  zap trash: '~/Library/Saved Application State/com.editshare.lightworks.savedState'
+  zap trash: "~/Library/Saved Application State/com.editshare.lightworks.savedState"
 end
