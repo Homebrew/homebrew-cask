0b7eb8655f276abc9fb3f67afcc2a397877cc5ef
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:13 +0100

    rq: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/rq.rb
+++ Casks/rq.rb
@@ -1,11 +1,11 @@
-cask 'rq' do
-  version '0.10.4'
-  sha256 'cae9ee8589dfb4fd0ab13ac7991d372fc2531f7c69374257b943dc2e096b750a'
+cask "rq" do
+  version "0.10.4"
+  sha256 "cae9ee8589dfb4fd0ab13ac7991d372fc2531f7c69374257b943dc2e096b750a"
 
   url "https://github.com/dflemstr/rq/releases/download/v#{version}/record-query-v#{version}-x86_64-apple-darwin.tar.gz"
-  appcast 'https://github.com/dflemstr/rq/releases.atom'
-  name 'rq'
-  homepage 'https://github.com/dflemstr/rq'
+  appcast "https://github.com/dflemstr/rq/releases.atom"
+  name "rq"
+  homepage "https://github.com/dflemstr/rq"
 
-  binary 'x86_64-apple-darwin/rq'
+  binary "x86_64-apple-darwin/rq"
 end
