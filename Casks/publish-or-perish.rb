9c5ef4719be1a192a00be8d692199cf676c87756
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:05 +0100

    publish-or-perish: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/publish-or-perish.rb
+++ Casks/publish-or-perish.rb
@@ -1,15 +1,15 @@
-cask 'publish-or-perish' do
-  version '7.24.2867'
-  sha256 'a9756d5af600ce0951c71c3adad165132ab247c61ea838c1da16cbcc2d42769e'
+cask "publish-or-perish" do
+  version "7.24.2867"
+  sha256 "a9756d5af600ce0951c71c3adad165132ab247c61ea838c1da16cbcc2d42769e"
 
-  url 'https://harzing.com/download/PoP7Mac.pkg'
-  appcast 'https://harzing.com/resources/publish-or-perish/os-x'
-  name 'Harzing Publish or Perish'
-  homepage 'https://harzing.com/resources/publish-or-perish'
+  url "https://harzing.com/download/PoP7Mac.pkg"
+  appcast "https://harzing.com/resources/publish-or-perish/os-x"
+  name "Harzing Publish or Perish"
+  homepage "https://harzing.com/resources/publish-or-perish"
 
-  depends_on macos: '>= :high_sierra'
+  depends_on macos: ">= :high_sierra"
 
-  pkg 'PoP7Mac.pkg'
+  pkg "PoP7Mac.pkg"
 
-  uninstall pkgutil: 'com.tarma.popmac.pkg'
+  uninstall pkgutil: "com.tarma.popmac.pkg"
 end
