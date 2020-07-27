84b4f86d86f753cc38b18cb040c102e8344d9ef9
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:15:52 +0100

    android-file-transfer: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/android-file-transfer.rb
+++ Casks/android-file-transfer.rb
@@ -1,18 +1,18 @@
-cask 'android-file-transfer' do
+cask "android-file-transfer" do
   version :latest
   sha256 :no_check
 
   # google.com/dl/androidjumper/ was verified as official when first introduced to the cask
-  url 'https://dl.google.com/dl/androidjumper/mtp/current/androidfiletransfer.dmg'
-  name 'Android File Transfer'
-  homepage 'https://www.android.com/filetransfer/'
+  url "https://dl.google.com/dl/androidjumper/mtp/current/androidfiletransfer.dmg"
+  name "Android File Transfer"
+  homepage "https://www.android.com/filetransfer/"
 
-  app 'Android File Transfer.app'
+  app "Android File Transfer.app"
 
   zap trash: [
-               '~/Library/Application Support/Google/Android File Transfer',
-               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.google.android.mtpviewer.sfl*',
-               '~/Library/Preferences/com.google.android.mtpviewer.plist',
-             ],
-      rmdir: '~/Library/Application Support/Google/'
+    "~/Library/Application Support/Google/Android File Transfer",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.google.android.mtpviewer.sfl*",
+    "~/Library/Preferences/com.google.android.mtpviewer.plist",
+  ],
+      rmdir: "~/Library/Application Support/Google/"
 end
