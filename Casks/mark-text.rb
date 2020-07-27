1aa802a92ccad0e96070ddb07a7719440257c6b0
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:54 +0100

    mark-text: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/mark-text.rb
+++ Casks/mark-text.rb
@@ -1,12 +1,12 @@
-cask 'mark-text' do
-  version '0.16.2'
-  sha256 '9ad1aa0ea087aebf17858611f40f62e76b2825c6cb0b13608fa8e131a9cb4867'
+cask "mark-text" do
+  version "0.16.2"
+  sha256 "9ad1aa0ea087aebf17858611f40f62e76b2825c6cb0b13608fa8e131a9cb4867"
 
   # github.com/marktext/marktext/ was verified as official when first introduced to the cask
   url "https://github.com/marktext/marktext/releases/download/v#{version}/marktext.dmg"
-  appcast 'https://github.com/marktext/marktext/releases.atom'
-  name 'Mark Text'
-  homepage 'https://marktext.app/'
+  appcast "https://github.com/marktext/marktext/releases.atom"
+  name "Mark Text"
+  homepage "https://marktext.app/"
 
-  app 'Mark Text.app'
+  app "Mark Text.app"
 end
