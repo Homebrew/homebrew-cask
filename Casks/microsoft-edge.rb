503fd820f887794eb0243fe5c131d54b4aa4bf21
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:30:27 +0100

    microsoft-edge: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/microsoft-edge.rb
+++ Casks/microsoft-edge.rb
@@ -1,31 +1,31 @@
-cask 'microsoft-edge' do
-  version '84.0.522.44'
-  sha256 '86bf0c784f3a7ed5ab90b08d094f1c35c701613f40850d7856cfca4cde3cfe52'
+cask "microsoft-edge" do
+  version "84.0.522.44"
+  sha256 "86bf0c784f3a7ed5ab90b08d094f1c35c701613f40850d7856cfca4cde3cfe52"
 
   # officecdn-microsoft-com.akamaized.net/ was verified as official when first introduced to the cask
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdge-#{version}.pkg"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://go.microsoft.com/fwlink/?linkid=2069148'
-  name 'Microsoft Edge'
-  homepage 'https://www.microsoft.com/edge'
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://go.microsoft.com/fwlink/?linkid=2069148"
+  name "Microsoft Edge"
+  homepage "https://www.microsoft.com/edge"
 
   auto_updates true
 
   pkg "MicrosoftEdge-#{version}.pkg"
 
-  uninstall pkgutil: 'com.microsoft.edgemac',
-            rmdir:   '/Library/Application Support/Microsoft'
+  uninstall pkgutil: "com.microsoft.edgemac",
+            rmdir:   "/Library/Application Support/Microsoft"
 
   zap launchctl: [
-                   'com.microsoft.autoupdate.helper',
-                   'com.microsoft.update.agent',
-                 ],
-      pkgutil:   'com.microsoft.package.Microsoft_AutoUpdate.app',
-      delete:    '/Library/PrivilegedHelperTools/com.microsoft.autoupdate.helper',
+    "com.microsoft.autoupdate.helper",
+    "com.microsoft.update.agent",
+  ],
+      pkgutil:   "com.microsoft.package.Microsoft_AutoUpdate.app",
+      delete:    "/Library/PrivilegedHelperTools/com.microsoft.autoupdate.helper",
       trash:     [
-                   '/Library/Application Support/Microsoft',
-                   '~/Library/Application Support/Microsoft Edge',
-                   '~/Library/Caches/Microsoft Edge',
-                   '~/Library/Preferences/com.microsoft.edgemac.plist',
-                   '~/Library/Saved Application State/com.microsoft.edgemac.savedState',
-                 ]
+        "/Library/Application Support/Microsoft",
+        "~/Library/Application Support/Microsoft Edge",
+        "~/Library/Caches/Microsoft Edge",
+        "~/Library/Preferences/com.microsoft.edgemac.plist",
+        "~/Library/Saved Application State/com.microsoft.edgemac.savedState",
+      ]
 end
