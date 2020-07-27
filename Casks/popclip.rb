b97b4ec9f8f6b646f9942bd8dccb0cc552926608
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:40 +0100

    popclip: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/popclip.rb
+++ Casks/popclip.rb
@@ -1,13 +1,13 @@
-cask 'popclip' do
-  version '2019.10'
-  sha256 '052cb6d24d1060fcc133837603c8d48bc937f3c0d9ad34eea6f695aedffb50e4'
+cask "popclip" do
+  version "2019.10"
+  sha256 "052cb6d24d1060fcc133837603c8d48bc937f3c0d9ad34eea6f695aedffb50e4"
 
   url "https://pilotmoon.com/downloads/PopClip-#{version}.zip"
-  appcast 'https://softwareupdate.pilotmoon.com/update/popclip/appcast.xml'
-  name 'PopClip'
-  homepage 'https://pilotmoon.com/popclip/'
+  appcast "https://softwareupdate.pilotmoon.com/update/popclip/appcast.xml"
+  name "PopClip"
+  homepage "https://pilotmoon.com/popclip/"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'PopClip.app'
+  app "PopClip.app"
 end
