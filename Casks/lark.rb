7a457e60993696383c8934571e6ac49acb03a585
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:28:39 +0100

    lark: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/lark.rb
+++ Casks/lark.rb
@@ -1,12 +1,12 @@
-cask 'lark' do
-  version '3.28.1'
-  sha256 'c3a322dac541c1da379465c8d90e5517b3400644cf389e3d031d589f1948e9b3'
+cask "lark" do
+  version "3.28.1"
+  sha256 "c3a322dac541c1da379465c8d90e5517b3400644cf389e3d031d589f1948e9b3"
 
   # sf3-ttcdn-tos.pstatp.com/ was verified as official when first introduced to the cask
   url "https://sf3-ttcdn-tos.pstatp.com/obj/ee-appcenter/Lark-#{version}.dmg"
-  appcast 'https://www.larksuite.com/api/downloads'
-  name 'Lark'
-  homepage 'https://www.larksuite.com/'
+  appcast "https://www.larksuite.com/api/downloads"
+  name "Lark"
+  homepage "https://www.larksuite.com/"
 
-  app 'Lark.app'
+  app "Lark.app"
 end
