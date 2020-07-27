6f992f6d5165f2796f8bdec212a2e16cbeb2f69e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:20:21 +0100

    clipgrab: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/clipgrab.rb
+++ Casks/clipgrab.rb
@@ -1,15 +1,15 @@
-cask 'clipgrab' do
-  version '3.8.13'
-  sha256 '3b78e17cd8f91f2a1518d87585ed7b6c747f27bd2651e65edfcca912cb03bee5'
+cask "clipgrab" do
+  version "3.8.13"
+  sha256 "3b78e17cd8f91f2a1518d87585ed7b6c747f27bd2651e65edfcca912cb03bee5"
 
   url "https://download.clipgrab.org/ClipGrab-#{version}.dmg"
-  appcast 'https://clipgrab.org/'
-  name 'ClipGrab'
-  homepage 'https://clipgrab.org/'
+  appcast "https://clipgrab.org/"
+  name "ClipGrab"
+  homepage "https://clipgrab.org/"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'ClipGrab.app'
+  app "ClipGrab.app"
 
-  zap trash: '~/Library/Preferences/de.clipgrab.ClipGrab.plist'
+  zap trash: "~/Library/Preferences/de.clipgrab.ClipGrab.plist"
 end
