9e808832c67f93fdc8d7642cb1beb12673acad43
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:54 +0100

    private-eye: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/private-eye.rb
+++ Casks/private-eye.rb
@@ -1,22 +1,22 @@
-cask 'private-eye' do
+cask "private-eye" do
   version :latest
   sha256 :no_check
 
-  url 'https://radiosilenceapp.com/downloads/Private_Eye_for_OS_X_10.9_and_later.pkg'
-  name 'Private Eye'
-  homepage 'https://radiosilenceapp.com/private-eye'
+  url "https://radiosilenceapp.com/downloads/Private_Eye_for_OS_X_10.9_and_later.pkg"
+  name "Private Eye"
+  homepage "https://radiosilenceapp.com/private-eye"
 
-  pkg 'Private_Eye_for_OS_X_10.9_and_later.pkg'
+  pkg "Private_Eye_for_OS_X_10.9_and_later.pkg"
 
   # We intentionally unload the kext twice as a workaround
   # See https://github.com/Homebrew/homebrew-cask/pull/1802#issuecomment-34171151
   uninstall early_script: {
-                            executable:   '/sbin/kextunload',
-                            args:         ['-b', 'com.radiosilenceapp.nke.PrivateEye'],
-                            must_succeed: false,
-                          },
-            quit:         'com.radiosilenceapp.PrivateEye',
-            kext:         'com.radiosilenceapp.nke.PrivateEye',
-            pkgutil:      'com.radiosilenceapp.privateEye.*',
-            launchctl:    'com.radiosilenceapp.nke.PrivateEye'
+    executable:   "/sbin/kextunload",
+    args:         ["-b", "com.radiosilenceapp.nke.PrivateEye"],
+    must_succeed: false,
+  },
+            quit:         "com.radiosilenceapp.PrivateEye",
+            kext:         "com.radiosilenceapp.nke.PrivateEye",
+            pkgutil:      "com.radiosilenceapp.privateEye.*",
+            launchctl:    "com.radiosilenceapp.nke.PrivateEye"
 end
