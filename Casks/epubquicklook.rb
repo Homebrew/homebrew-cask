4e3aa65af225b330490ed305e11124dd2af43862
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:23:26 +0100

    epubquicklook: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/epubquicklook.rb
+++ Casks/epubquicklook.rb
@@ -1,11 +1,11 @@
-cask 'epubquicklook' do
-  version '1.8'
-  sha256 '923b2ebcbffc4ee1da42550c2239b41bad088d61956b22b1a92b293329ef6fe5'
+cask "epubquicklook" do
+  version "1.8"
+  sha256 "923b2ebcbffc4ee1da42550c2239b41bad088d61956b22b1a92b293329ef6fe5"
 
   url "https://github.com/jaketmp/ePub-quicklook/releases/download/v#{version}/epub.qlgenerator.zip"
-  appcast 'https://github.com/jaketmp/ePub-quicklook/releases.atom'
-  name 'EPUB QuickLook'
-  homepage 'https://github.com/jaketmp/ePub-quicklook'
+  appcast "https://github.com/jaketmp/ePub-quicklook/releases.atom"
+  name "EPUB QuickLook"
+  homepage "https://github.com/jaketmp/ePub-quicklook"
 
-  qlplugin 'epub.qlgenerator'
+  qlplugin "epub.qlgenerator"
 end
