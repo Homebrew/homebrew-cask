c27aa67ca1ada2d8fe309d958a56e78e5fceb946
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:38:27 +0100

    vpn-enabler: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/vpn-enabler.rb
+++ Casks/vpn-enabler.rb
@@ -1,22 +1,22 @@
-cask 'vpn-enabler' do
+cask "vpn-enabler" do
   if MacOS.version <= :el_capitan
-    version '3.0.6'
-    sha256 'fae22c4e3b05c77d3658b45c92398c3a6f7fa7792bc124e1b463efb662519536'
-    url 'https://cutedgesystems.com/downloads/VPNEnablerForElCapitan.zip'
+    version "3.0.6"
+    sha256 "fae22c4e3b05c77d3658b45c92398c3a6f7fa7792bc124e1b463efb662519536"
+    url "https://cutedgesystems.com/downloads/VPNEnablerForElCapitan.zip"
   elsif MacOS.version <= :sierra
-    version '4.0'
-    sha256 '50e22bcc2e341adff7fc625714c251ac0ac889cfb55983e026cff8478ebd3793'
-    url 'https://cutedgesystems.com/downloads/VPNEnablerForSierra.zip'
+    version "4.0"
+    sha256 "50e22bcc2e341adff7fc625714c251ac0ac889cfb55983e026cff8478ebd3793"
+    url "https://cutedgesystems.com/downloads/VPNEnablerForSierra.zip"
   else
-    version '5.2'
-    sha256 '6c01122328280859007eb1f21cd6878600037a966612fc2c3cb0f7192c035175'
-    url 'https://cutedgesystems.com/downloads/VPNEnablerForHighSierra.zip'
+    version "5.2"
+    sha256 "6c01122328280859007eb1f21cd6878600037a966612fc2c3cb0f7192c035175"
+    url "https://cutedgesystems.com/downloads/VPNEnablerForHighSierra.zip"
   end
 
-  name 'VPN Enabler'
-  homepage 'https://cutedgesystems.com/'
+  name "VPN Enabler"
+  homepage "https://cutedgesystems.com/"
 
-  depends_on macos: '>= :el_capitan'
+  depends_on macos: ">= :el_capitan"
 
-  app 'VPN Enabler.app'
+  app "VPN Enabler.app"
 end
