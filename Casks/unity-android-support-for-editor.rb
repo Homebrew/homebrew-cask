467992f20d54f79f427a7f9aa99970fab5e68e4a
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:38:05 +0100

    unity-android-support-for-editor: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/unity-android-support-for-editor.rb
+++ Casks/unity-android-support-for-editor.rb
@@ -1,15 +1,15 @@
-cask 'unity-android-support-for-editor' do
-  version '2019.4.2f1,20b4642a3455'
-  sha256 'f55544548ee1730ab34249ac3641739738e2d3a99fa741597291862458f12513'
+cask "unity-android-support-for-editor" do
+  version "2019.4.2f1,20b4642a3455"
+  sha256 "f55544548ee1730ab34249ac3641739738e2d3a99fa741597291862458f12513"
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-#{version.before_comma}.pkg"
-  appcast 'https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json'
-  name 'Unity Android Build Support'
-  homepage 'https://unity3d.com/unity/'
+  appcast "https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json"
+  name "Unity Android Build Support"
+  homepage "https://unity3d.com/unity/"
 
-  depends_on cask: 'unity'
+  depends_on cask: "unity"
 
   pkg "UnitySetup-Android-Support-for-Editor-#{version.before_comma}.pkg"
 
-  uninstall pkgutil: 'com.unity3d.AndroidPlayer'
+  uninstall pkgutil: "com.unity3d.AndroidPlayer"
 end
