98713bb9f9e95479fc3be1e24ff64aa6c8b7975e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:31 +0100

    seadrive: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/seadrive.rb
+++ Casks/seadrive.rb
@@ -1,12 +1,12 @@
-cask 'seadrive' do
-  version '2.0.4'
-  sha256 '51e6b7588a2c3d1a3b8141890edf3ae4ae237b1162eeee7f7becff66a6aac6da'
+cask "seadrive" do
+  version "2.0.4"
+  sha256 "51e6b7588a2c3d1a3b8141890edf3ae4ae237b1162eeee7f7becff66a6aac6da"
 
   # download.seadrive.org/ was verified as official when first introduced to the cask
   url "https://download.seadrive.org/seadrive-#{version}.dmg"
-  appcast 'https://www.seafile.com/en/download/'
-  name 'Seadrive'
-  homepage 'https://www.seafile.com/en/home/'
+  appcast "https://www.seafile.com/en/download/"
+  name "Seadrive"
+  homepage "https://www.seafile.com/en/home/"
 
-  app 'Seadrive.app'
+  app "Seadrive.app"
 end
