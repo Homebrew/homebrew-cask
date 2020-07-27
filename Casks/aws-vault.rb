45be9a568971463a57765f63d0c87d2a89d3f794
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:17:14 +0100

    aws-vault: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/aws-vault.rb
+++ Casks/aws-vault.rb
@@ -1,11 +1,11 @@
-cask 'aws-vault' do
-  version '5.4.4'
-  sha256 '4b9d41808c9e9353128151a43ddd3ca842e5e94014e69c8cd96ef18548ebea7d'
+cask "aws-vault" do
+  version "5.4.4"
+  sha256 "4b9d41808c9e9353128151a43ddd3ca842e5e94014e69c8cd96ef18548ebea7d"
 
   url "https://github.com/99designs/aws-vault/releases/download/v#{version}/aws-vault-darwin-amd64.dmg"
-  appcast 'https://github.com/99designs/aws-vault/releases.atom'
-  name 'aws-vault'
-  homepage 'https://github.com/99designs/aws-vault'
+  appcast "https://github.com/99designs/aws-vault/releases.atom"
+  name "aws-vault"
+  homepage "https://github.com/99designs/aws-vault"
 
-  binary 'aws-vault'
+  binary "aws-vault"
 end
