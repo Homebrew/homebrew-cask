d389454b66c5e8ceb0d510975a698c38a44a6fb9
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:41 +0100

    popo: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/popo.rb
+++ Casks/popo.rb
@@ -1,19 +1,19 @@
-cask 'popo' do
-  version '3.4.1'
-  sha256 '7ed7fd9df2f90d21b2ea475533d379395504f6be0ee0eac7e827779580911e53'
+cask "popo" do
+  version "3.4.1"
+  sha256 "7ed7fd9df2f90d21b2ea475533d379395504f6be0ee0eac7e827779580911e53"
 
   url "http://popo.netease.com/file/popomac/POPO_Mac_V#{version.dots_to_underscores}.dmg"
-  appcast 'http://popo.netease.com/',
+  appcast "http://popo.netease.com/",
           must_contain: version.dots_to_underscores
-  name 'NetEase POPO'
-  homepage 'http://popo.netease.com/'
+  name "NetEase POPO"
+  homepage "http://popo.netease.com/"
 
-  app 'popo_mac.app'
+  app "popo_mac.app"
 
   zap trash: [
-               '~/Library/Application Support/Netease/Popo',
-               '~/Library/Saved Application State/com.netease.game.popo.savedState',
-               '~/Library/Preferences/com.netease.game.popo.plist',
-               '~/Library/Caches/com.netease.game.popo',
-             ]
+    "~/Library/Application Support/Netease/Popo",
+    "~/Library/Saved Application State/com.netease.game.popo.savedState",
+    "~/Library/Preferences/com.netease.game.popo.plist",
+    "~/Library/Caches/com.netease.game.popo",
+  ]
 end
