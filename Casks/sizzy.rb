f83b23beb3ccdd6ae3083bcf7d821b17152f6dd4
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:00 +0100

    sizzy: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/sizzy.rb
+++ Casks/sizzy.rb
@@ -1,13 +1,13 @@
-cask 'sizzy' do
-  version '0.28.1'
-  sha256 '6a6a1f206cca74c4c26fcc9f3eaeec1df35b3bb9d5d3bacb5bcf6bbf9f963214'
+cask "sizzy" do
+  version "0.28.1"
+  sha256 "6a6a1f206cca74c4c26fcc9f3eaeec1df35b3bb9d5d3bacb5bcf6bbf9f963214"
 
-  url 'https://sizzy.co/get-app'
-  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://sizzy.co/get-app&user_agent=Intel%20Mac%20OS%20X'
-  name 'Sizzy'
-  homepage 'https://sizzy.co/'
+  url "https://sizzy.co/get-app"
+  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://sizzy.co/get-app&user_agent=Intel%20Mac%20OS%20X"
+  name "Sizzy"
+  homepage "https://sizzy.co/"
 
   auto_updates true
 
-  app 'Sizzy.app'
+  app "Sizzy.app"
 end
