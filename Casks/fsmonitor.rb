9386f2d8580dc0ce37f24266c14da7239800ba7d
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:24:48 +0100

    fsmonitor: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/fsmonitor.rb
+++ Casks/fsmonitor.rb
@@ -1,26 +1,26 @@
-cask 'fsmonitor' do
-  version '1.2,137'
-  sha256 'f7e78086bbcae56c00582aae04abfe7bbbc50235d0228766d0dbc5b18fa0a3db'
+cask "fsmonitor" do
+  version "1.2,137"
+  sha256 "f7e78086bbcae56c00582aae04abfe7bbbc50235d0228766d0dbc5b18fa0a3db"
 
   # tristan-software.ch/FSMonitor/ was verified as official when first introduced to the cask
   url "https://tristan-software.ch/FSMonitor/Archives/FSMonitor_#{version.before_comma}(#{version.after_comma}).zip"
-  appcast 'https://fsmonitor.com/FSMonitor/Archives/appcast2.xml'
-  name 'FSMonitor'
-  homepage 'https://fsmonitor.com/'
+  appcast "https://fsmonitor.com/FSMonitor/Archives/appcast2.xml"
+  name "FSMonitor"
+  homepage "https://fsmonitor.com/"
 
   auto_updates true
 
-  app 'FSMonitor.app'
+  app "FSMonitor.app"
 
-  uninstall delete:    '/Library/PrivilegedHelperTools/com.tristan.fseventstool',
-            launchctl: 'com.tristan.fseventstool'
+  uninstall delete:    "/Library/PrivilegedHelperTools/com.tristan.fseventstool",
+            launchctl: "com.tristan.fseventstool"
 
   zap trash: [
-               '/Users/Shared/FSMonitor',
-               '~/Library/Application Support/FSMonitor',
-               '~/Library/Application Support/com.tristan.FSMonitor',
-               '~/Library/Caches/com.tristan.FSMonitor',
-               '~/Library/Preferences/com.tristan.FSMonitor.plist',
-               '~/Library/Saved Application State/com.tristan.FSMonitor.savedState',
-             ]
+    "/Users/Shared/FSMonitor",
+    "~/Library/Application Support/FSMonitor",
+    "~/Library/Application Support/com.tristan.FSMonitor",
+    "~/Library/Caches/com.tristan.FSMonitor",
+    "~/Library/Preferences/com.tristan.FSMonitor.plist",
+    "~/Library/Saved Application State/com.tristan.FSMonitor.savedState",
+  ]
 end
