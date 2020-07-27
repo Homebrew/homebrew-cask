aa7b2b3a7b1b8d7684bf065a085504ec494a6dfc
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:26:25 +0100

    houdahspot: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/houdahspot.rb
+++ Casks/houdahspot.rb
@@ -1,13 +1,13 @@
-cask 'houdahspot' do
-  version '5.1.4'
-  sha256 'fa5f591adf24650064c531621f6f0f075e2c27164af0fa070f00fc404ccc4398'
+cask "houdahspot" do
+  version "5.1.4"
+  sha256 "fa5f591adf24650064c531621f6f0f075e2c27164af0fa070f00fc404ccc4398"
 
   url "https://dl.houdah.com/houdahSpot/updates/cast#{version.major}_assets/HoudahSpot#{version}.zip"
   appcast "https://www.houdah.com/houdahSpot/updates/cast#{version.major}.xml"
-  name 'HoudahSpot'
-  homepage 'https://www.houdah.com/houdahSpot/'
+  name "HoudahSpot"
+  homepage "https://www.houdah.com/houdahSpot/"
 
-  depends_on macos: '>= :high_sierra'
+  depends_on macos: ">= :high_sierra"
 
-  app 'HoudahSpot.app'
+  app "HoudahSpot.app"
 end
