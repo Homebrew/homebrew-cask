160d0367a59e41cd8ab0c4604661829e960a030d
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:14 +0100

    logos: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/logos.rb
+++ Casks/logos.rb
@@ -1,23 +1,23 @@
-cask 'logos' do
-  version '8.15.0.0004'
-  sha256 'ef26720eb5c4d050ba7d5d117b6cf9f0ce94c7e3b55e6da2f466ce2c08633745'
+cask "logos" do
+  version "8.15.0.0004"
+  sha256 "ef26720eb5c4d050ba7d5d117b6cf9f0ce94c7e3b55e6da2f466ce2c08633745"
 
   # downloads.logoscdn.com/ was verified as official when first introduced to the cask
   url "https://downloads.logoscdn.com/LBS8/Installer/#{version}/LogosMac.dmg"
   appcast "https://clientservices.logos.com/update/v1/feed/logos#{version.major}-mac/stable.xml"
-  name 'Logos Bible Software'
-  homepage 'https://www.logos.com/'
+  name "Logos Bible Software"
+  homepage "https://www.logos.com/"
 
-  depends_on macos: '>= :el_capitan'
+  depends_on macos: ">= :el_capitan"
 
-  app 'Logos.app'
+  app "Logos.app"
 
-  uninstall launchctl: 'com.logos.LogosIndexer',
-            quit:      'com.logos.Logos'
+  uninstall launchctl: "com.logos.LogosIndexer",
+            quit:      "com.logos.Logos"
 
   zap trash: [
-               '~/Library/Preferences/com.logos.LogosIndexer.plist',
-               '~/Library/Preferences/com.logos.LogosCEF.plist',
-               '~/Library/Preferences/com.logos.Logos.plist',
-             ]
+    "~/Library/Preferences/com.logos.LogosIndexer.plist",
+    "~/Library/Preferences/com.logos.LogosCEF.plist",
+    "~/Library/Preferences/com.logos.Logos.plist",
+  ]
 end
