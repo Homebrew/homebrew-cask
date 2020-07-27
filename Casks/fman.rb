afd3007a4a67ff87f6a394dcac5c092fa1bddcb3
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:24:22 +0100

    fman: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/fman.rb
+++ Casks/fman.rb
@@ -1,11 +1,11 @@
-cask 'fman' do
-  version '1.7.3'
-  sha256 '27e9aa2ffae66f448a45c025a1fb1b9e06903aab5d2f117b951612deeee09531'
+cask "fman" do
+  version "1.7.3"
+  sha256 "27e9aa2ffae66f448a45c025a1fb1b9e06903aab5d2f117b951612deeee09531"
 
   url "https://fman.io/updates/mac/#{version}.zip"
-  appcast 'https://fman.io/updates/Appcast.xml'
-  name 'fman'
-  homepage 'https://fman.io/'
+  appcast "https://fman.io/updates/Appcast.xml"
+  name "fman"
+  homepage "https://fman.io/"
 
-  app 'fman.app'
+  app "fman.app"
 end
