e50b996fd20d92299a6078a38d2381d7bbf507f0
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:19:56 +0100

    chrome-remote-desktop-host: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/chrome-remote-desktop-host.rb
+++ Casks/chrome-remote-desktop-host.rb
@@ -1,18 +1,18 @@
-cask 'chrome-remote-desktop-host' do
+cask "chrome-remote-desktop-host" do
   version :latest
   sha256 :no_check
 
-  url 'https://dl.google.com/chrome-remote-desktop/chromeremotedesktop.dmg'
-  name 'Chrome Remote Desktop'
-  homepage 'https://chrome.google.com/webstore/detail/chrome-remote-desktop/gbchcmhmhahfdphkhkmpfmihenigjmpp'
+  url "https://dl.google.com/chrome-remote-desktop/chromeremotedesktop.dmg"
+  name "Chrome Remote Desktop"
+  homepage "https://chrome.google.com/webstore/detail/chrome-remote-desktop/gbchcmhmhahfdphkhkmpfmihenigjmpp"
 
-  pkg 'Chrome Remote Desktop Host.pkg'
+  pkg "Chrome Remote Desktop Host.pkg"
 
   uninstall script:  {
-                       executable: '/Applications/Chrome Remote Desktop Host Uninstaller.app/Contents/MacOS/remoting_host_uninstaller',
-                       args:       ['--no-ui'],
-                     },
-            pkgutil: 'com.google.pkg.ChromeRemoteDesktopHost'
+    executable: "/Applications/Chrome Remote Desktop Host Uninstaller.app/Contents/MacOS/remoting_host_uninstaller",
+    args:       ["--no-ui"],
+  },
+            pkgutil: "com.google.pkg.ChromeRemoteDesktopHost"
 
   caveats do
     logout
