e01f8923bc83c3e32e9448b38de7bf60045dbc34
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:38:07 +0100

    universal-media-server: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/universal-media-server.rb
+++ Casks/universal-media-server.rb
@@ -1,14 +1,14 @@
-cask 'universal-media-server' do
-  version '9.8.0'
-  sha256 '9f885a05aadfcfe6216ad767435c22faeef1792ee3a44a42e800d23c7bc910c9'
+cask "universal-media-server" do
+  version "9.8.0"
+  sha256 "9f885a05aadfcfe6216ad767435c22faeef1792ee3a44a42e800d23c7bc910c9"
 
   # github.com/UniversalMediaServer/UniversalMediaServer/ was verified as official when first introduced to the cask
   url "https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/#{version}/UMS-#{version}.dmg"
-  appcast 'https://github.com/UniversalMediaServer/UniversalMediaServer/releases.atom'
-  name 'Universal Media Server'
-  homepage 'https://www.universalmediaserver.com/'
+  appcast "https://github.com/UniversalMediaServer/UniversalMediaServer/releases.atom"
+  name "Universal Media Server"
+  homepage "https://www.universalmediaserver.com/"
 
-  app 'Universal Media Server.app'
+  app "Universal Media Server.app"
 
-  zap trash: '~/Library/Application Support/UMS/'
+  zap trash: "~/Library/Application Support/UMS/"
 end
