cd70389e4415b0996628784817399c54a46b67bb
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:52 +0100

    marble: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/marble.rb
+++ Casks/marble.rb
@@ -1,13 +1,13 @@
-cask 'marble' do
-  version '2.2.0'
-  sha256 '51397a9723571b59d6773b7126ccdc1402b81d3023fb0d7e0828443e33d91e41'
+cask "marble" do
+  version "2.2.0"
+  sha256 "51397a9723571b59d6773b7126ccdc1402b81d3023fb0d7e0828443e33d91e41"
 
   url "https://files.kde.org/marble/downloads/MacOSX/Marble-#{version}.pkg"
-  appcast 'https://marble.kde.org/install.php'
-  name 'Marble'
-  homepage 'https://marble.kde.org/'
+  appcast "https://marble.kde.org/install.php"
+  name "Marble"
+  homepage "https://marble.kde.org/"
 
   pkg "Marble-#{version}.pkg"
 
-  uninstall pkgutil: 'org.kde.marbleV220.Marble.pkg'
+  uninstall pkgutil: "org.kde.marbleV220.Marble.pkg"
 end
