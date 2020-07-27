4a7670da65a2f97fae73a3ef6f7367ae8652a8b7
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:24:22 +0100

    fm3-edit: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/fm3-edit.rb
+++ Casks/fm3-edit.rb
@@ -1,13 +1,13 @@
-cask 'fm3-edit' do
-  version '1.01.07'
-  sha256 '5b27d481e13eb00d578db2e9ca1371382daadd92a0b67cb33a71bb7044f7e391'
+cask "fm3-edit" do
+  version "1.01.07"
+  sha256 "5b27d481e13eb00d578db2e9ca1371382daadd92a0b67cb33a71bb7044f7e391"
 
-  url "https://www.fractalaudio.com/downloads/FM3-Edit/FM3-Edit-OSX-v#{version.tr('.', 'p')}.dmg"
-  appcast 'https://www.fractalaudio.com/fm3-edit/'
-  name 'FM3 Edit'
-  homepage 'https://www.fractalaudio.com/fm3-edit/'
+  url "https://www.fractalaudio.com/downloads/FM3-Edit/FM3-Edit-OSX-v#{version.tr(".", "p")}.dmg"
+  appcast "https://www.fractalaudio.com/fm3-edit/"
+  name "FM3 Edit"
+  homepage "https://www.fractalaudio.com/fm3-edit/"
 
-  app 'FM3-Edit.app'
+  app "FM3-Edit.app"
 
-  zap trash: '~/Library/Application Support/Fractal Audio/FM3-Edit'
+  zap trash: "~/Library/Application Support/Fractal Audio/FM3-Edit"
 end
