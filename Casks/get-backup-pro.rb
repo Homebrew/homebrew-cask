be71aeae16dab796fb99c4f6068e308f0f3717f2
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:25:06 +0100

    get-backup-pro: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/get-backup-pro.rb
+++ Casks/get-backup-pro.rb
@@ -1,12 +1,12 @@
-cask 'get-backup-pro' do
-  version '3.5.6'
-  sha256 '630a49fb7194bedd859be81d969bf46b656dce473692f4d961622fb894b4fc4c'
+cask "get-backup-pro" do
+  version "3.5.6"
+  sha256 "630a49fb7194bedd859be81d969bf46b656dce473692f4d961622fb894b4fc4c"
 
   # belightsoft.s3.amazonaws.com/updates/ was verified as official when first introduced to the cask
   url "https://belightsoft.s3.amazonaws.com/updates/Get+Backup+Pro+#{version.major}.zip"
   appcast "https://www.belightsoft.com/download/updates/appcast_getbackup_pro#{version.major}.xml"
   name "Get Backup Pro #{version.major}"
-  homepage 'https://www.belightsoft.com/products/getbackup/'
+  homepage "https://www.belightsoft.com/products/getbackup/"
 
   auto_updates true
 
