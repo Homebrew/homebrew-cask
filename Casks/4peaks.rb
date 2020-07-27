1e568f3a0dfc460fea4b8ded125bf7a7652c2e90
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:13:14 +0100

    4peaks: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/4peaks.rb
+++ Casks/4peaks.rb
@@ -1,11 +1,11 @@
-cask '4peaks' do
-  version '1.8'
-  sha256 'cc87ace54d8d667681b7a9a51538f877d3800b8a521a700c3d054f45cc0586bb'
+cask "4peaks" do
+  version "1.8"
+  sha256 "cc87ace54d8d667681b7a9a51538f877d3800b8a521a700c3d054f45cc0586bb"
 
-  url 'http://downloads.nucleobytes.com/4peaks.dmg'
-  appcast 'https://nucleobytes.com/4peaks/index.html'
-  name '4Peaks'
-  homepage 'https://nucleobytes.com/4peaks/index.html'
+  url "http://downloads.nucleobytes.com/4peaks.dmg"
+  appcast "https://nucleobytes.com/4peaks/index.html"
+  name "4Peaks"
+  homepage "https://nucleobytes.com/4peaks/index.html"
 
-  app '4Peaks.app'
+  app "4Peaks.app"
 end
