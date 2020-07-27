5d8c49875889b8b9344b8e441263c289265d5367
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:31:00 +0100

    moom: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/moom.rb
+++ Casks/moom.rb
@@ -1,18 +1,18 @@
-cask 'moom' do
-  version '3.2.17'
-  sha256 'c6bb33f478721dade0531ebf5fd0c9cd63d8b6d48b34afdc849d6c94a201aebd'
+cask "moom" do
+  version "3.2.17"
+  sha256 "c6bb33f478721dade0531ebf5fd0c9cd63d8b6d48b34afdc849d6c94a201aebd"
 
   url "https://manytricks.com/download/_do_not_hotlink_/moom#{version.no_dots}.dmg"
-  appcast 'https://manytricks.com/moom/appcast'
-  name 'Moom'
-  homepage 'https://manytricks.com/moom/'
+  appcast "https://manytricks.com/moom/appcast"
+  name "Moom"
+  homepage "https://manytricks.com/moom/"
 
   auto_updates true
 
-  app 'Moom.app'
+  app "Moom.app"
 
   zap trash: [
-               '~/Library/Preferences/com.manytricks.Moom.plist',
-               '~/Library/Application Support/Many Tricks',
-             ]
+    "~/Library/Preferences/com.manytricks.Moom.plist",
+    "~/Library/Application Support/Many Tricks",
+  ]
 end
