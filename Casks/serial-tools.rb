9c021d136afb0f9b434c7f6b7b6f4801ed2f11e2
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:39 +0100

    serial-tools: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/serial-tools.rb
+++ Casks/serial-tools.rb
@@ -1,10 +1,10 @@
-cask 'serial-tools' do
+cask "serial-tools" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.w7ay.net/site/Downloads/Serial%20Tools/Serial%20Tools%20app.dmg'
-  name 'Serial Tools'
-  homepage 'https://www.w7ay.net/site/Applications/Serial%20Tools/'
+  url "https://www.w7ay.net/site/Downloads/Serial%20Tools/Serial%20Tools%20app.dmg"
+  name "Serial Tools"
+  homepage "https://www.w7ay.net/site/Applications/Serial%20Tools/"
 
-  app 'Serial Tools.app'
+  app "Serial Tools.app"
 end
