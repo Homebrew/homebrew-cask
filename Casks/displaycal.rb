d36fb2d8de3de471e684c93c2e2c18371796a2fb
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:22:12 +0100

    displaycal: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/displaycal.rb
+++ Casks/displaycal.rb
@@ -1,23 +1,23 @@
-cask 'displaycal' do
-  version '3.8.9.3'
-  sha256 '3e3f4a506c3ffc1e2004d57c6cd521d6cacb1bb6a71f9e3fa4cd81ab6ad4f31a'
+cask "displaycal" do
+  version "3.8.9.3"
+  sha256 "3e3f4a506c3ffc1e2004d57c6cd521d6cacb1bb6a71f9e3fa4cd81ab6ad4f31a"
 
   # sourceforge.net/dispcalgui/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/dispcalgui/release/#{version}/DisplayCAL-#{version}.pkg"
-  appcast 'https://sourceforge.net/projects/dispcalgui/rss?path=/release'
-  name 'DisplayCAL'
-  homepage 'https://displaycal.net/'
+  appcast "https://sourceforge.net/projects/dispcalgui/rss?path=/release"
+  name "DisplayCAL"
+  homepage "https://displaycal.net/"
 
   auto_updates true
-  depends_on formula: 'argyll-cms'
+  depends_on formula: "argyll-cms"
 
   pkg "DisplayCAL-#{version}.pkg"
 
-  uninstall pkgutil: 'net.displaycal.*.DisplayCAL.*'
+  uninstall pkgutil: "net.displaycal.*.DisplayCAL.*"
 
   zap trash: [
-               '~/Library/Application Support/dispcalGUI',
-               '~/Library/Logs/dispcalGUI',
-               '~/Library/Preferences/dispcalGUI',
-             ]
+    "~/Library/Application Support/dispcalGUI",
+    "~/Library/Logs/dispcalGUI",
+    "~/Library/Preferences/dispcalGUI",
+  ]
 end
