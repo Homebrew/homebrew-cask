380f0620c84a0a2bf8cacd92499fa537218e480b
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:25:21 +0100

    gitscout: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/gitscout.rb
+++ Casks/gitscout.rb
@@ -1,17 +1,17 @@
-cask 'gitscout' do
+cask "gitscout" do
   version :latest
   sha256 :no_check
 
-  url 'https://gitscout.com/marshmallows'
-  name 'Gitscout'
-  homepage 'https://gitscout.com/'
+  url "https://gitscout.com/marshmallows"
+  name "Gitscout"
+  homepage "https://gitscout.com/"
 
-  app 'Gitscout.app'
+  app "Gitscout.app"
 
   zap trash: [
-               '~/Library/Application Support/Gitscout',
-               '~/Library/Preferences/com.electron.gitscout.helper.plist',
-               '~/Library/Preferences/com.electron.gitscout.plist',
-               '~/Library/Saved Application State/com.electron.gitscout.savedState',
-             ]
+    "~/Library/Application Support/Gitscout",
+    "~/Library/Preferences/com.electron.gitscout.helper.plist",
+    "~/Library/Preferences/com.electron.gitscout.plist",
+    "~/Library/Saved Application State/com.electron.gitscout.savedState",
+  ]
 end
