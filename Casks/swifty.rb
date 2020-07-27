47ad5b3fdea898579d2d735b1ce1f9f8016977fa
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:58 +0100

    swifty: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/swifty.rb
+++ Casks/swifty.rb
@@ -1,21 +1,21 @@
-cask 'swifty' do
-  version '0.5.2'
-  sha256 '17f4d7e3b7f9b03ca4188c84f8ac989547e6f4a41bc873f2c68ba2bc10df7b49'
+cask "swifty" do
+  version "0.5.2"
+  sha256 "17f4d7e3b7f9b03ca4188c84f8ac989547e6f4a41bc873f2c68ba2bc10df7b49"
 
   # github.com/swiftyapp/swifty/ was verified as official when first introduced to the cask
   url "https://github.com/swiftyapp/swifty/releases/download/v#{version}/Swifty-#{version}.dmg"
-  appcast 'https://github.com/swiftyapp/swifty/releases.atom'
-  name 'Swifty'
-  homepage 'https://getswifty.pro/'
+  appcast "https://github.com/swiftyapp/swifty/releases.atom"
+  name "Swifty"
+  homepage "https://getswifty.pro/"
 
   auto_updates true
 
-  app 'Swifty.app'
+  app "Swifty.app"
 
   zap trash: [
-               '~/Library/Application Support/Swifty',
-               '~/Library/Logs/Swifty',
-               '~/Library/Preferences/com.electron.swifty.plist',
-               '~/Library/Saved Application State/com.electron.swifty.savedState',
-             ]
+    "~/Library/Application Support/Swifty",
+    "~/Library/Logs/Swifty",
+    "~/Library/Preferences/com.electron.swifty.plist",
+    "~/Library/Saved Application State/com.electron.swifty.savedState",
+  ]
 end
