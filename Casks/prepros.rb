9840b3f6b0fc4583bd7ccb041fc4972d9f061d27
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:50 +0100

    prepros: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/prepros.rb
+++ Casks/prepros.rb
@@ -1,11 +1,11 @@
-cask 'prepros' do
-  version '7.3.17'
-  sha256 '1bf3ef540654fd9ce534437c03f40e7325910dba4b984ed0431349fe28b4087e'
+cask "prepros" do
+  version "7.3.17"
+  sha256 "1bf3ef540654fd9ce534437c03f40e7325910dba4b984ed0431349fe28b4087e"
 
   url "https://downloads.prepros.io/v#{version.major}/Prepros-#{version}.zip"
-  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://prepros.io/downloads/stable/mac'
-  name 'Prepros'
-  homepage 'https://prepros.io/'
+  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://prepros.io/downloads/stable/mac"
+  name "Prepros"
+  homepage "https://prepros.io/"
 
-  app 'Prepros.app'
+  app "Prepros.app"
 end
