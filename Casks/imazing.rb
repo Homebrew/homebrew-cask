2aa523638989eb9438518342cae37b621ea9b9ad
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:26:54 +0100

    imazing: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/imazing.rb
+++ Casks/imazing.rb
@@ -1,36 +1,36 @@
-cask 'imazing' do
-  version '2.11.8.13733'
-  sha256 '1ded77a920a9b090e31e9eb2855d5f2901d522d30220126c43e3ca4b43762991'
+cask "imazing" do
+  version "2.11.8.13733"
+  sha256 "1ded77a920a9b090e31e9eb2855d5f2901d522d30220126c43e3ca4b43762991"
 
   url "https://downloads.imazing.com/mac/iMazing/#{version}/iMazing_#{version}.dmg"
   appcast "https://downloads.imazing.com/com.DigiDNA.iMazing#{version.major}Mac.xml"
-  name 'iMazing'
-  homepage 'https://imazing.com/'
+  name "iMazing"
+  homepage "https://imazing.com/"
 
   auto_updates true
-  conflicts_with cask: 'imazing-mini'
+  conflicts_with cask: "imazing-mini"
 
-  app 'iMazing.app'
+  app "iMazing.app"
 
-  uninstall login_item: 'iMazing Mini',
+  uninstall login_item: "iMazing Mini",
             quit:       [
-                          "com.DigiDNA.iMazing#{version}Mac",
-                          "com.DigiDNA.iMazing#{version}Mac.Mini",
-                        ]
+              "com.DigiDNA.iMazing#{version}Mac",
+              "com.DigiDNA.iMazing#{version}Mac.Mini",
+            ]
 
   zap trash: [
-               '~/Library/Application Support/iMazing',
-               '~/Library/Application Support/iMazing Mini',
-               '~/Library/Application Support/MobileSync/Backup/iMazing.Versions',
-               "~/Library/Caches/com.DigiDNA.iMazing#{version.major}Mac",
-               "~/Library/Caches/com.DigiDNA.iMazing#{version.major}Mac.Mini",
-               "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.DigiDNA.iMazing#{version.major}Mac.Mini",
-               '~/Library/Caches/iMazing',
-               "~/Library/Preferences/com.DigiDNA.iMazing#{version.major}Mac.plist",
-               "~/Library/Preferences/com.DigiDNA.iMazing#{version.major}Mac.Mini.plist",
-               '/Users/Shared/iMazing Mini',
-               '/Users/Shared/iMazing',
-             ]
+    "~/Library/Application Support/iMazing",
+    "~/Library/Application Support/iMazing Mini",
+    "~/Library/Application Support/MobileSync/Backup/iMazing.Versions",
+    "~/Library/Caches/com.DigiDNA.iMazing#{version.major}Mac",
+    "~/Library/Caches/com.DigiDNA.iMazing#{version.major}Mac.Mini",
+    "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.DigiDNA.iMazing#{version.major}Mac.Mini",
+    "~/Library/Caches/iMazing",
+    "~/Library/Preferences/com.DigiDNA.iMazing#{version.major}Mac.plist",
+    "~/Library/Preferences/com.DigiDNA.iMazing#{version.major}Mac.Mini.plist",
+    "/Users/Shared/iMazing Mini",
+    "/Users/Shared/iMazing",
+  ]
 
   caveats <<~EOS
     Performing a zap on this cask removes files pertaining to both #{token}
