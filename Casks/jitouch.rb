cada83943a1bea3e71ecf06f53913709603fb9ab
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:27:50 +0100

    jitouch: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/jitouch.rb
+++ Casks/jitouch.rb
@@ -1,19 +1,19 @@
-cask 'jitouch' do
+cask "jitouch" do
   version :latest
   sha256 :no_check
 
   if MacOS.version <= :el_capitan
-    url 'https://www.jitouch.com/jitouch_el_capitan.zip'
+    url "https://www.jitouch.com/jitouch_el_capitan.zip"
   elsif MacOS.version <= :sierra
-    url 'https://www.jitouch.com/jitouch_sierra.zip'
+    url "https://www.jitouch.com/jitouch_sierra.zip"
   else
-    url 'https://www.jitouch.com/jitouch_mojave.zip'
+    url "https://www.jitouch.com/jitouch_mojave.zip"
   end
 
-  name 'jitouch'
-  homepage 'https://www.jitouch.com/'
+  name "jitouch"
+  homepage "https://www.jitouch.com/"
 
-  prefpane 'jitouch/Jitouch.prefPane'
+  prefpane "jitouch/Jitouch.prefPane"
 
-  zap trash: '~/Library/Preferences/com.jitouch.Jitouch.plist'
+  zap trash: "~/Library/Preferences/com.jitouch.Jitouch.plist"
 end
