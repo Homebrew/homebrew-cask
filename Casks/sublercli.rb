1ab1cc0586b73be15ada685f5fcdf97723063b6f
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:48 +0100

    sublercli: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/sublercli.rb
+++ Casks/sublercli.rb
@@ -1,10 +1,10 @@
-cask 'sublercli' do
-  version '0.32'
-  sha256 'e5e99fb67944acffff9df5c6bf0a66805a7c696fb41df8a81fbfaf0068ce4c26'
+cask "sublercli" do
+  version "0.32"
+  sha256 "e5e99fb67944acffff9df5c6bf0a66805a7c696fb41df8a81fbfaf0068ce4c26"
 
   url "https://bitbucket.org/galad87/sublercli/downloads/SublerCLI-#{version}.zip"
-  name 'SublerCLI'
-  homepage 'https://bitbucket.org/galad87/sublercli/'
+  name "SublerCLI"
+  homepage "https://bitbucket.org/galad87/sublercli/"
 
-  binary 'SublerCLI'
+  binary "SublerCLI"
 end
