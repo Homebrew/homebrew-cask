2c1f6bc4d09bb48e35e9f3858199dfea2a59fd6c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:49 +0100

    recovery-disk-assistant: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/recovery-disk-assistant.rb
+++ Casks/recovery-disk-assistant.rb
@@ -1,10 +1,10 @@
-cask 'recovery-disk-assistant' do
+cask "recovery-disk-assistant" do
   version :latest
   sha256 :no_check
 
-  url 'https://support.apple.com/downloads/DL1433/en_US/RecoveryDiskAssistant.dmg'
-  name 'Recovery Disk Assistant'
-  homepage 'https://support.apple.com/HT202294'
+  url "https://support.apple.com/downloads/DL1433/en_US/RecoveryDiskAssistant.dmg"
+  name "Recovery Disk Assistant"
+  homepage "https://support.apple.com/HT202294"
 
-  app 'Recovery Disk Assistant.app'
+  app "Recovery Disk Assistant.app"
 end
