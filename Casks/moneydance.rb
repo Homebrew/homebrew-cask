0fb987cdbbc2f80e183c714ecd13f5717f206ca8
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:30:54 +0100

    moneydance: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/moneydance.rb
+++ Casks/moneydance.rb
@@ -1,10 +1,10 @@
-cask 'moneydance' do
+cask "moneydance" do
   version :latest
   sha256 :no_check
 
-  url 'https://infinitekind.com/stabledl/current/Moneydance.zip'
-  name 'Moneydance'
-  homepage 'https://infinitekind.com/moneydance'
+  url "https://infinitekind.com/stabledl/current/Moneydance.zip"
+  name "Moneydance"
+  homepage "https://infinitekind.com/moneydance"
 
-  app 'Moneydance.app'
+  app "Moneydance.app"
 end
