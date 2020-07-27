8483b0cf9bf3c1f94fffa32a5f147a96e1d86ced
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:13:45 +0100

    aircall: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/aircall.rb
+++ Casks/aircall.rb
@@ -1,14 +1,14 @@
-cask 'aircall' do
-  version '2.5.8'
-  sha256 '59501a4466fcd5993bceec3b98afcf6755487e61b8d7a5467b1d86bb41117064'
+cask "aircall" do
+  version "2.5.8"
+  sha256 "59501a4466fcd5993bceec3b98afcf6755487e61b8d7a5467b1d86bb41117064"
 
   # s3-us-west-1.amazonaws.com/aircall-electron-releases/ was verified as official when first introduced to the cask
   url "https://s3-us-west-1.amazonaws.com/aircall-electron-releases/production/Aircall-#{version}.zip"
-  appcast 'https://electron.aircall.io/update/osx/0.0.0'
-  name 'Aircall'
-  homepage 'https://aircall.io/'
+  appcast "https://electron.aircall.io/update/osx/0.0.0"
+  name "Aircall"
+  homepage "https://aircall.io/"
 
   auto_updates true
 
-  app 'Aircall.app'
+  app "Aircall.app"
 end
