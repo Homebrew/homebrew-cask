805edd0635169877d52572493b0d5c14e459621f
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:21:15 +0100

    crystalmaker: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/crystalmaker.rb
+++ Casks/crystalmaker.rb
@@ -1,11 +1,11 @@
-cask 'crystalmaker' do
-  version '10.5.3'
-  sha256 '9d1814f19a712deaea1adb15ad75d60437e3d8ca067710de49d9c4fd083f7b30'
+cask "crystalmaker" do
+  version "10.5.3"
+  sha256 "9d1814f19a712deaea1adb15ad75d60437e3d8ca067710de49d9c4fd083f7b30"
 
-  url 'http://crystalmaker.com/downloads/crystalmaker_mac.zip'
+  url "http://crystalmaker.com/downloads/crystalmaker_mac.zip"
   appcast "http://crystalmaker.com/crystalmaker/release-notes/mac/#{version.major}/index.html"
-  name 'CrystalMaker'
-  homepage 'http://crystalmaker.com/index.html'
+  name "CrystalMaker"
+  homepage "http://crystalmaker.com/index.html"
 
-  app 'CrystalMaker.app'
+  app "CrystalMaker.app"
 end
