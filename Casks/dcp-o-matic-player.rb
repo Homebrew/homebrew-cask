ca13cff3d7e97071c9d483f8dace78e32d423dc0
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:21:43 +0100

    dcp-o-matic-player: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/dcp-o-matic-player.rb
+++ Casks/dcp-o-matic-player.rb
@@ -1,11 +1,11 @@
-cask 'dcp-o-matic-player' do
-  version '2.14.33'
-  sha256 '4e824c415f379ff7752a81f92e00271bd785ab7b80359451c861654badd5827e'
+cask "dcp-o-matic-player" do
+  version "2.14.33"
+  sha256 "4e824c415f379ff7752a81f92e00271bd785ab7b80359451c861654badd5827e"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.9-player&version=#{version}"
-  appcast 'https://dcpomatic.com/download'
-  name 'DCP-o-matic Player'
-  homepage 'https://dcpomatic.com/'
+  appcast "https://dcpomatic.com/download"
+  name "DCP-o-matic Player"
+  homepage "https://dcpomatic.com/"
 
   app "DCP-o-matic #{version.major} Player.app"
 end
