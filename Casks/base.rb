051aeea99fc76c93d5dc5f544033d3eeea7d8877
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:17:36 +0100

    base: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/base.rb
+++ Casks/base.rb
@@ -1,22 +1,22 @@
-cask 'base' do
-  version '2.5.1'
-  sha256 '42ac2783562f872f17de667c1c450b6bd0f936effc010bdbcceb49d13efef3ce'
+cask "base" do
+  version "2.5.1"
+  sha256 "42ac2783562f872f17de667c1c450b6bd0f936effc010bdbcceb49d13efef3ce"
 
   url "https://files.menial.co.uk/base/base_#{version}.zip"
-  appcast 'https://update.menial.co.uk/software/base/'
-  name 'Menial Base'
-  homepage 'https://menial.co.uk/base/'
+  appcast "https://update.menial.co.uk/software/base/"
+  name "Menial Base"
+  homepage "https://menial.co.uk/base/"
 
-  depends_on macos: '>= :mojave'
+  depends_on macos: ">= :mojave"
 
-  app 'Base.app'
+  app "Base.app"
 
   zap trash: [
-               '~/Library/Application Support/Base',
-               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/uk.co.menial.base.sfl*',
-               '~/Library/Caches/com.apple.helpd/Generated/uk.co.menial.base.help*',
-               '~/Library/Caches/com.apple.helpd/SDMHelpData/Other/English/HelpSDMIndexFile/uk.co.menial.base.help*',
-               '~/Library/Caches/uk.co.menial.Base',
-               '~/Library/Preferences/uk.co.menial.Base.plist',
-             ]
+    "~/Library/Application Support/Base",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/uk.co.menial.base.sfl*",
+    "~/Library/Caches/com.apple.helpd/Generated/uk.co.menial.base.help*",
+    "~/Library/Caches/com.apple.helpd/SDMHelpData/Other/English/HelpSDMIndexFile/uk.co.menial.base.help*",
+    "~/Library/Caches/uk.co.menial.Base",
+    "~/Library/Preferences/uk.co.menial.Base.plist",
+  ]
 end
