5af4c40115f94e08520caeb9de773c32bdc61fe5
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:15:49 +0100

    amorphousdiskmark: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/amorphousdiskmark.rb
+++ Casks/amorphousdiskmark.rb
@@ -1,15 +1,15 @@
-cask 'amorphousdiskmark' do
+cask "amorphousdiskmark" do
   version :latest
   sha256 :no_check
 
-  url 'http://katsurashareware.com/dl/AmorphousDiskMark.zip'
-  name 'AmorphousDiskMark'
-  homepage 'http://www.katsurashareware.com/pgs/adm.html'
+  url "http://katsurashareware.com/dl/AmorphousDiskMark.zip"
+  name "AmorphousDiskMark"
+  homepage "http://www.katsurashareware.com/pgs/adm.html"
 
-  app 'AmorphousDiskMark.app'
+  app "AmorphousDiskMark.app"
 
   zap trash: [
-               '~/Library/Preferences/com.katsurashareware.AmorphousDiskMark.plist',
-               '~/Library/Saved Application State/com.katsurashareware.AmorphousDiskMark.savedState',
-             ]
+    "~/Library/Preferences/com.katsurashareware.AmorphousDiskMark.plist",
+    "~/Library/Saved Application State/com.katsurashareware.AmorphousDiskMark.savedState",
+  ]
 end
