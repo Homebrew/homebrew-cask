2e43473d650176f2a9970b853d72092963a2f150
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:19:07 +0100

    busycal: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/busycal.rb
+++ Casks/busycal.rb
@@ -1,25 +1,25 @@
-cask 'busycal' do
-  version '3.10.2,401028'
-  sha256 'd41041c3f95b680d7c86f8d4a5469d3258a182349742011cf9d355eab9658a19'
+cask "busycal" do
+  version "3.10.2,401028"
+  sha256 "d41041c3f95b680d7c86f8d4a5469d3258a182349742011cf9d355eab9658a19"
 
-  url 'https://www.busymac.com/download/BusyCal.zip'
-  appcast 'https://www.busymac.com/busycal/news.plist'
-  name 'BusyCal'
-  homepage 'https://busymac.com/busycal/index.html'
+  url "https://www.busymac.com/download/BusyCal.zip"
+  appcast "https://www.busymac.com/busycal/news.plist"
+  name "BusyCal"
+  homepage "https://busymac.com/busycal/index.html"
 
   auto_updates true
 
-  pkg 'BusyCal Installer.pkg'
+  pkg "BusyCal Installer.pkg"
 
   uninstall pkgutil: "com.busymac.busycal#{version.major}.pkg",
             quit:    "N4RA379GBW.com.busymac.busycal#{version.major}.alarm",
-            signal:  ['TERM', "com.busymac.busycal#{version.major}"]
+            signal:  ["TERM", "com.busymac.busycal#{version.major}"]
 
   zap trash: [
-               "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.busymac.busycal#{version.major}.sfl*",
-               "~/Library/Containers/com.busymac.busycal#{version.major}",
-               "~/Library/Containers/N4RA379GBW.com.busymac.busycal#{version.major}.alarm",
-               "~/Library/Group Containers/com.busymac.busycal#{version.major}",
-               "~/Library/Group Containers/N4RA379GBW.com.busymac.busycal#{version.major}",
-             ]
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.busymac.busycal#{version.major}.sfl*",
+    "~/Library/Containers/com.busymac.busycal#{version.major}",
+    "~/Library/Containers/N4RA379GBW.com.busymac.busycal#{version.major}.alarm",
+    "~/Library/Group Containers/com.busymac.busycal#{version.major}",
+    "~/Library/Group Containers/N4RA379GBW.com.busymac.busycal#{version.major}",
+  ]
 end
