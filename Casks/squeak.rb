93777b8a62bf7b2da7aac5f6dceeab58459061d3
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:36 +0100

    squeak: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/squeak.rb
+++ Casks/squeak.rb
@@ -1,11 +1,11 @@
-cask 'squeak' do
-  version '5.3,19435'
-  sha256 '06c2d22064b2795c6f4a2c03feddd2ebf741aa8222676195f5ce7b9f607411bd'
+cask "squeak" do
+  version "5.3,19435"
+  sha256 "06c2d22064b2795c6f4a2c03feddd2ebf741aa8222676195f5ce7b9f607411bd"
 
   url "https://files.squeak.org/#{version.before_comma}/Squeak#{version.before_comma}-#{version.after_comma}-64bit/Squeak#{version.before_comma}-#{version.after_comma}-64bit-All-in-One.zip"
-  appcast 'https://squeak.org/downloads/'
-  name 'Squeak'
-  homepage 'https://squeak.org/'
+  appcast "https://squeak.org/downloads/"
+  name "Squeak"
+  homepage "https://squeak.org/"
 
   app "Squeak#{version.before_comma}-#{version.after_comma}-64bit-All-in-One.app"
 
