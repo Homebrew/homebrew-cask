499896d84602acbfd88cadf0170b16a78550b17c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:13:50 +0100

    airparrot: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/airparrot.rb
+++ Casks/airparrot.rb
@@ -1,22 +1,22 @@
-cask 'airparrot' do
-  version '3.0.0'
-  sha256 '79e985c3cd6bbfe0a3d87424d658d1193a884b98bf27c44dda76270156c2ee87'
+cask "airparrot" do
+  version "3.0.0"
+  sha256 "79e985c3cd6bbfe0a3d87424d658d1193a884b98bf27c44dda76270156c2ee87"
 
   url "https://download.airsquirrels.com/AirParrot#{version.major}/Mac/AirParrot-#{version}.dmg"
-  appcast 'https://www.airsquirrels.com/airparrot/release-notes/mac'
-  name 'AirParrot'
-  homepage 'https://www.airsquirrels.com/airparrot/'
+  appcast "https://www.airsquirrels.com/airparrot/release-notes/mac"
+  name "AirParrot"
+  homepage "https://www.airsquirrels.com/airparrot/"
 
   app "AirParrot #{version.major}.app"
 
   uninstall kext: [
-                    'com.squirrels.driver.AirParrotSpeakers',
-                    '/Library/Extensions/AirParrotDriver.kext',
-                    '/Library/Extensions/APExtFramebuffer.kext',
-                    '/System/Library/Extensions/AirParrotDriver.kext',
-                    '/System/Library/Extensions/APExtFramebuffer.kext',
-                  ],
+    "com.squirrels.driver.AirParrotSpeakers",
+    "/Library/Extensions/AirParrotDriver.kext",
+    "/Library/Extensions/APExtFramebuffer.kext",
+    "/System/Library/Extensions/AirParrotDriver.kext",
+    "/System/Library/Extensions/APExtFramebuffer.kext",
+  ],
             quit: "com.squirrels.AirParrot-#{version.major}"
 
-  zap trash: '~/Library/Preferences/com.squirrels.AirParrot-*.plist'
+  zap trash: "~/Library/Preferences/com.squirrels.AirParrot-*.plist"
 end
