8e18e9aa7868d917fba423951706e416bbcc5a96
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:27:32 +0100

    jabref: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/jabref.rb
+++ Casks/jabref.rb
@@ -1,12 +1,12 @@
-cask 'jabref' do
-  version '5.0'
-  sha256 'a02b02c1b54ebcfa5011e571800483d23d1774fb6117abff8e35055079e7d2e3'
+cask "jabref" do
+  version "5.0"
+  sha256 "a02b02c1b54ebcfa5011e571800483d23d1774fb6117abff8e35055079e7d2e3"
 
   # github.com/JabRef/jabref/ was verified as official when first introduced to the cask
   url "https://github.com/JabRef/jabref/releases/download/v#{version}/JabRef-#{version}.dmg"
-  appcast 'https://github.com/JabRef/jabref/releases.atom'
-  name 'JabRef'
-  homepage 'https://www.jabref.org/'
+  appcast "https://github.com/JabRef/jabref/releases.atom"
+  name "JabRef"
+  homepage "https://www.jabref.org/"
 
-  app 'JabRef.app'
+  app "JabRef.app"
 end
