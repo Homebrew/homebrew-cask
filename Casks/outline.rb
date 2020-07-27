8a1e49784b035721a6eb2f9d741c37e5db7778f7
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:32:44 +0100

    outline: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/outline.rb
+++ Casks/outline.rb
@@ -1,11 +1,11 @@
-cask 'outline' do
-  version '3.24.3'
-  sha256 'cb9056b1440064e951a0c31eaa9540e73edacdd870e1b15d3678f054d329e69a'
+cask "outline" do
+  version "3.24.3"
+  sha256 "cb9056b1440064e951a0c31eaa9540e73edacdd870e1b15d3678f054d329e69a"
 
   url "http://static.outline.ws/versions/Outline_#{version}.zip"
-  appcast 'https://gorillized.s3.amazonaws.com/versions/update_channel.xml'
-  name 'Outline'
-  homepage 'http://outline.ws/'
+  appcast "https://gorillized.s3.amazonaws.com/versions/update_channel.xml"
+  name "Outline"
+  homepage "http://outline.ws/"
 
-  app 'Outline.app'
+  app "Outline.app"
 end
