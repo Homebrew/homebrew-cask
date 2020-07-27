a7a3ecc5446c6c837309789d26906402f7446908
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:22:44 +0100

    dwihn0r-keepassx: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/dwihn0r-keepassx.rb
+++ Casks/dwihn0r-keepassx.rb
@@ -1,11 +1,11 @@
-cask 'dwihn0r-keepassx' do
-  version '0.4.4'
-  sha256 '53ae446d458ef0ab6dae268ba17b0eb9779959f3f821552de89122bd434769e2'
+cask "dwihn0r-keepassx" do
+  version "0.4.4"
+  sha256 "53ae446d458ef0ab6dae268ba17b0eb9779959f3f821552de89122bd434769e2"
 
   url "https://github.com/dwihn0r/keepassx/releases/download/v#{version}/KeePassX-#{version}-OSX.zip"
-  appcast 'https://github.com/dwihn0r/keepassx/releases.atom'
-  name 'KeePassX'
-  homepage 'https://github.com/dwihn0r/keepassx/'
+  appcast "https://github.com/dwihn0r/keepassx/releases.atom"
+  name "KeePassX"
+  homepage "https://github.com/dwihn0r/keepassx/"
 
-  app 'KeePassX.app'
+  app "KeePassX.app"
 end
