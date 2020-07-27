7a819b880a2937969680ee889f86ae2f34b21840
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:15:49 +0100

    ammonite: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/ammonite.rb
+++ Casks/ammonite.rb
@@ -1,11 +1,11 @@
-cask 'ammonite' do
-  version '1.20'
-  sha256 '1f8b34ef2f57305f85acfe09feab85049ca8e77c812182eee9061d478b65e877'
+cask "ammonite" do
+  version "1.20"
+  sha256 "1f8b34ef2f57305f85acfe09feab85049ca8e77c812182eee9061d478b65e877"
 
   url "https://www.soma-zone.com/download/files/Ammonite-#{version}.tar.bz2"
-  appcast 'https://www.soma-zone.com/Ammonite/a/appcast_update.xml'
-  name 'Ammonite'
-  homepage 'https://www.soma-zone.com/Ammonite/'
+  appcast "https://www.soma-zone.com/Ammonite/a/appcast_update.xml"
+  name "Ammonite"
+  homepage "https://www.soma-zone.com/Ammonite/"
 
-  app 'Ammonite.app'
+  app "Ammonite.app"
 end
