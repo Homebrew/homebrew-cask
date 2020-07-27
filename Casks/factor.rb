67059036591b8b7813d51140a1172bd3e14da939
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:23:40 +0100

    factor: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/factor.rb
+++ Casks/factor.rb
@@ -1,13 +1,13 @@
-cask 'factor' do
-  version '0.98'
-  sha256 '045847b1cf01e1dda270b834c34effd796ce52da40ada8e2fe52b55e3358db17'
+cask "factor" do
+  version "0.98"
+  sha256 "045847b1cf01e1dda270b834c34effd796ce52da40ada8e2fe52b55e3358db17"
 
   url "https://downloads.factorcode.org/releases/#{version}/factor-macosx-x86-64-#{version}.dmg"
-  appcast 'https://downloads.factorcode.org/releases/'
-  name 'Factor'
-  homepage 'https://factorcode.org/'
+  appcast "https://downloads.factorcode.org/releases/"
+  name "Factor"
+  homepage "https://factorcode.org/"
 
-  suite 'factor'
+  suite "factor"
 
   caveats do
     path_environment_variable "#{appdir}/factor"
