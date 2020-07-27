31fbc6763a22c3a1a958ac12e9f2443ec21fddbe
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:35 +0100

    sekey: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/sekey.rb
+++ Casks/sekey.rb
@@ -1,19 +1,19 @@
-cask 'sekey' do
-  version '0.1'
-  sha256 '8d473f7214ba76b70ca30905e2ebd07d7c421f1cff320285bd8ec8d2a7a5b083'
+cask "sekey" do
+  version "0.1"
+  sha256 "8d473f7214ba76b70ca30905e2ebd07d7c421f1cff320285bd8ec8d2a7a5b083"
 
   url "https://github.com/sekey/sekey/releases/download/#{version}/SeKey-#{version}.pkg"
-  appcast 'https://github.com/sekey/sekey/releases.atom'
-  name 'SeKey'
-  homepage 'https://github.com/sekey/sekey/'
+  appcast "https://github.com/sekey/sekey/releases.atom"
+  name "SeKey"
+  homepage "https://github.com/sekey/sekey/"
 
   pkg "SeKey-#{version}.pkg"
   binary "#{appdir}/SeKey.app/Contents/MacOS/sekey"
 
-  uninstall launchctl: 'com.ntrippar.sekey',
-            pkgutil:   'com.ntrippar.sekey'
+  uninstall launchctl: "com.ntrippar.sekey",
+            pkgutil:   "com.ntrippar.sekey"
 
-  zap trash: '~/.sekey'
+  zap trash: "~/.sekey"
 
   caveats <<~EOS
     Append the following line to your ~/.bash_profile or ~/.zshrc:
