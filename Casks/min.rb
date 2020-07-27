e08df26bf4470eaa11708ca71156d8be10a5c854
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:30:36 +0100

    min: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/min.rb
+++ Casks/min.rb
@@ -1,18 +1,18 @@
-cask 'min' do
-  version '1.15.0'
-  sha256 '4a81b39ec31e904449f4e9ffc1383e8b9ae3ce72c5fa61c5e4fd291163b0600e'
+cask "min" do
+  version "1.15.0"
+  sha256 "4a81b39ec31e904449f4e9ffc1383e8b9ae3ce72c5fa61c5e4fd291163b0600e"
 
   # github.com/minbrowser/min/ was verified as official when first introduced to the cask
   url "https://github.com/minbrowser/min/releases/download/v#{version}/Min-v#{version}-darwin-x64.zip"
-  appcast 'https://github.com/minbrowser/min/releases.atom'
-  name 'Min'
-  homepage 'https://minbrowser.github.io/min/'
+  appcast "https://github.com/minbrowser/min/releases.atom"
+  name "Min"
+  homepage "https://minbrowser.github.io/min/"
 
-  app 'Min.app'
+  app "Min.app"
 
   zap trash: [
-               '~/Library/Application Support/Min',
-               '~/Library/Caches/Min',
-               '~/Library/Saved Application State/com.electron.min.savedState',
-             ]
+    "~/Library/Application Support/Min",
+    "~/Library/Caches/Min",
+    "~/Library/Saved Application State/com.electron.min.savedState",
+  ]
 end
