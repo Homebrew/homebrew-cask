38f1f6431544206296efda8525807aaade97bc51
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:48 +0100

    reaper: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,29 +1,29 @@
-cask 'reaper' do
-  version '6.13.0,6.13'
+cask "reaper" do
+  version "6.13.0,6.13"
 
   if MacOS.version <= :mojave
-    sha256 '58f4e622a39fe7f57cd07a96e33d485b9fff8dfac5633bb0ac2b4255d2d9a972'
+    sha256 "58f4e622a39fe7f57cd07a96e33d485b9fff8dfac5633bb0ac2b4255d2d9a972"
 
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.after_comma.no_dots}_x86_64.dmg"
   else
-    sha256 'cc5b583c57ac8723820e3c448632713294354a3f7661bae4cae37bb22bf6cdba'
+    sha256 "cc5b583c57ac8723820e3c448632713294354a3f7661bae4cae37bb22bf6cdba"
 
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.after_comma.no_dots}_x86_64_catalina.dmg"
   end
 
-  appcast 'https://www.cockos.com/reaper/latestversion/?p=osx_64',
+  appcast "https://www.cockos.com/reaper/latestversion/?p=osx_64",
           must_contain: version.after_comma
-  name 'REAPER'
-  homepage 'https://www.reaper.fm/'
+  name "REAPER"
+  homepage "https://www.reaper.fm/"
 
-  app 'REAPER64.app'
-  app 'ReaMote64.app'
+  app "REAPER64.app"
+  app "ReaMote64.app"
 
   zap trash: [
-               '~/Library/Application Support/REAPER',
-               '~/Library/Saved Application State/com.cockos.reaper.savedState',
-               '~/Library/Saved Application State/com.cockos.reaperhosti386.savedState',
-               '~/Library/Saved Application State/com.cockos.reaperhostx8664.savedState',
-               '~/Library/Saved Application State/com.cockos.ReaMote.savedState',
-             ]
+    "~/Library/Application Support/REAPER",
+    "~/Library/Saved Application State/com.cockos.reaper.savedState",
+    "~/Library/Saved Application State/com.cockos.reaperhosti386.savedState",
+    "~/Library/Saved Application State/com.cockos.reaperhostx8664.savedState",
+    "~/Library/Saved Application State/com.cockos.ReaMote.savedState",
+  ]
 end
