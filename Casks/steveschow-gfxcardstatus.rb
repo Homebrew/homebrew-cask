958b1ec02201c4519ec230dd3d8543d8179ec47f
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:44 +0100

    steveschow-gfxcardstatus: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/steveschow-gfxcardstatus.rb
+++ Casks/steveschow-gfxcardstatus.rb
@@ -1,11 +1,11 @@
-cask 'steveschow-gfxcardstatus' do
-  version '2.4.4i'
-  sha256 '69c0d6602808cca60158a6d833eab8d82d59f683d3c25de34d977c83fe208dd0'
+cask "steveschow-gfxcardstatus" do
+  version "2.4.4i"
+  sha256 "69c0d6602808cca60158a6d833eab8d82d59f683d3c25de34d977c83fe208dd0"
 
   url "https://github.com/steveschow/gfxCardStatus/releases/download/v#{version}/gfxCardStatus.app.zip"
-  appcast 'https://github.com/steveschow/gfxCardStatus/releases.atom'
-  name 'gfxCardStatus'
-  homepage 'https://github.com/steveschow/gfxCardStatus'
+  appcast "https://github.com/steveschow/gfxCardStatus/releases.atom"
+  name "gfxCardStatus"
+  homepage "https://github.com/steveschow/gfxCardStatus"
 
-  app 'gfxCardStatus.app'
+  app "gfxCardStatus.app"
 end
