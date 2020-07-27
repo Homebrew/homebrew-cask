ff4fd53510449fc90c950cfff8fb4c2cebcb3325
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:17:24 +0100

    baiducloud: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/baiducloud.rb
+++ Casks/baiducloud.rb
@@ -1,21 +1,21 @@
-cask 'baiducloud' do
-  version '2.4.6'
-  sha256 '9ae594d4d6a4581d971ed9fcde7cda62022b04247476a8982802324fa16bee2e'
+cask "baiducloud" do
+  version "2.4.6"
+  sha256 "9ae594d4d6a4581d971ed9fcde7cda62022b04247476a8982802324fa16bee2e"
 
   # issuecdn.baidupcs.com/ was verified as official when first introduced to the cask
   url "https://issuecdn.baidupcs.com/issue/netdisk/mac_tongbupan/tongbupan_#{version}.dmg"
-  name 'Baidu Cloud'
-  name 'Baidu Yun Tong Bu Pan'
-  name '百度云同步盘'
-  homepage 'https://pan.baidu.com/'
+  name "Baidu Cloud"
+  name "Baidu Yun Tong Bu Pan"
+  name "百度云同步盘"
+  homepage "https://pan.baidu.com/"
 
-  app '百度云同步盘.app'
+  app "百度云同步盘.app"
 
-  uninstall quit: 'com.baidu.netdiskmac'
+  uninstall quit: "com.baidu.netdiskmac"
 
   zap trash: [
-               '~/Library/Application Support/百度云同步盘',
-               '~/Library/Preferences/com.baidu.netdiskmac.plist',
-               '~/Library/Caches/com.baidu.netdiskmac',
-             ]
+    "~/Library/Application Support/百度云同步盘",
+    "~/Library/Preferences/com.baidu.netdiskmac.plist",
+    "~/Library/Caches/com.baidu.netdiskmac",
+  ]
 end
