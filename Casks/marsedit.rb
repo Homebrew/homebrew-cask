a48e070ddf752cc0c8363a82c18761fb4a5404cf
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:56 +0100

    marsedit: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/marsedit.rb
+++ Casks/marsedit.rb
@@ -1,13 +1,13 @@
-cask 'marsedit' do
-  version '4.3.7'
-  sha256 'dab353d0abd0e5f09efc91ccc4d9845c141bd8100e3b44286e31294974b52022'
+cask "marsedit" do
+  version "4.3.7"
+  sha256 "dab353d0abd0e5f09efc91ccc4d9845c141bd8100e3b44286e31294974b52022"
 
   url "https://red-sweater.com/marsedit/MarsEdit#{version}.zip"
-  appcast 'https://red-sweater.com/marsedit/appcast4.php'
-  name 'MarsEdit'
-  homepage 'https://red-sweater.com/marsedit/'
+  appcast "https://red-sweater.com/marsedit/appcast4.php"
+  name "MarsEdit"
+  homepage "https://red-sweater.com/marsedit/"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'MarsEdit.app'
+  app "MarsEdit.app"
 end
