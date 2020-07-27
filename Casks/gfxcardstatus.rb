54fdf8d0bbb744bc65aa1f66540bb7cd051f0934
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:25:09 +0100

    gfxcardstatus: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/gfxcardstatus.rb
+++ Casks/gfxcardstatus.rb
@@ -1,18 +1,18 @@
-cask 'gfxcardstatus' do
-  version '2.3'
-  sha256 '092b3e2fad44681ba396cf498707c8b6c228fd55310770a8323ebb9344b4d9a1'
+cask "gfxcardstatus" do
+  version "2.3"
+  sha256 "092b3e2fad44681ba396cf498707c8b6c228fd55310770a8323ebb9344b4d9a1"
 
   url "https://gfx.io/downloads/gfxCardStatus-#{version}.zip"
-  appcast 'https://gfx.io/appcast.xml'
-  name 'gfxCardStatus'
-  homepage 'https://gfx.io/'
+  appcast "https://gfx.io/appcast.xml"
+  name "gfxCardStatus"
+  homepage "https://gfx.io/"
 
-  app 'gfxCardStatus.app'
+  app "gfxCardStatus.app"
 
   zap trash: [
-               '~/Library/Caches/com.codykrieger.gfxCardStatus',
-               '~/Library/Cookies/com.codykrieger.gfxCardStatus.binarycookies',
-               '~/Library/Preferences/com.codykrieger.gfxCardStatus-Preferences.plist',
-               '~/Library/Preferences/com.codykrieger.gfxCardStatus.plist',
-             ]
+    "~/Library/Caches/com.codykrieger.gfxCardStatus",
+    "~/Library/Cookies/com.codykrieger.gfxCardStatus.binarycookies",
+    "~/Library/Preferences/com.codykrieger.gfxCardStatus-Preferences.plist",
+    "~/Library/Preferences/com.codykrieger.gfxCardStatus.plist",
+  ]
 end
