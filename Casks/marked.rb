774c01b00b80c90333b1c18130cc05eb4627fa96
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:55 +0100

    marked: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/marked.rb
+++ Casks/marked.rb
@@ -1,26 +1,26 @@
-cask 'marked' do
-  version '2.5.43,990'
-  sha256 'de6c0ca79ad9dce7d677f6a35bfd39c8e623cd10b874217c936b88ea06c95acc'
+cask "marked" do
+  version "2.5.43,990"
+  sha256 "de6c0ca79ad9dce7d677f6a35bfd39c8e623cd10b874217c936b88ea06c95acc"
 
   url "https://updates.marked2app.com/Marked#{version.before_comma}#{version.after_comma}.zip"
-  appcast 'https://updates.marked2app.com/marked.xml'
-  name 'Marked'
-  homepage 'https://marked2app.com/'
+  appcast "https://updates.marked2app.com/marked.xml"
+  name "Marked"
+  homepage "https://marked2app.com/"
 
   auto_updates true
-  depends_on macos: '>= :yosemite'
+  depends_on macos: ">= :yosemite"
 
   app "Marked #{version.major}.app"
 
   uninstall quit: "com.brettterpstra.marked#{version.major}"
 
   zap trash: [
-               "~/Library/Application Support/Marked #{version.major}",
-               "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.brettterpstra.marked#{version.major}.sfl*",
-               "~/Library/Caches/Marked #{version.major}",
-               "~/Library/Caches/com.brettterpstra.marked#{version.major}",
-               "~/Library/Logs/Marked #{version.major}",
-               "~/Library/Preferences/com.brettterpstra.marked#{version.major}.plist",
-               "~/Library/Saved Application State/com.brettterpstra.marked#{version.major}.savedState",
-             ]
+    "~/Library/Application Support/Marked #{version.major}",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.brettterpstra.marked#{version.major}.sfl*",
+    "~/Library/Caches/Marked #{version.major}",
+    "~/Library/Caches/com.brettterpstra.marked#{version.major}",
+    "~/Library/Logs/Marked #{version.major}",
+    "~/Library/Preferences/com.brettterpstra.marked#{version.major}.plist",
+    "~/Library/Saved Application State/com.brettterpstra.marked#{version.major}.savedState",
+  ]
 end
