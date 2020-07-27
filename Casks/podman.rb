ced570d69e78f45baa8cdf434acf608ed0a55239
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:34 +0100

    podman: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/podman.rb
+++ Casks/podman.rb
@@ -1,20 +1,22 @@
-cask 'podman' do
-  version '2.0.3'
-  sha256 '9231c305c4e8c7374863c7377ac38a493e06fef09cea3bba975694941ae8dce3'
+cask "podman" do
+  version "2.0.3"
+  sha256 "9231c305c4e8c7374863c7377ac38a493e06fef09cea3bba975694941ae8dce3"
 
   url "https://github.com/containers/podman/releases/download/v#{version}/podman-remote-release-darwin.zip"
-  appcast 'https://github.com/containers/podman/releases.atom'
-  name 'podman'
-  homepage 'https://github.com/containers/podman/'
+  appcast "https://github.com/containers/podman/releases.atom"
+  name "podman"
+  homepage "https://github.com/containers/podman/"
 
-  binary 'podman'
+  binary "podman"
 
   postflight do
     man1 = Dir["#{staged_path}/docs/*.1"]
     FileUtils.mv(man1, "#{HOMEBREW_PREFIX}/share/man/man1/")
 
-    FileUtils.mkdir_p "#{ENV['HOME']}/.config/containers"
-    FileUtils.mv("#{staged_path}/containers.conf", "#{ENV['HOME']}/.config/containers/containers.conf") unless File.exist?("#{ENV['HOME']}/.config/containers/containers.conf")
+    FileUtils.mkdir_p "#{ENV["HOME"]}/.config/containers"
+    unless File.exist?("#{ENV["HOME"]}/.config/containers/containers.conf")
+      FileUtils.mv("#{staged_path}/containers.conf", "#{ENV["HOME"]}/.config/containers/containers.conf")
+    end
   end
 
   uninstall_postflight do
@@ -22,5 +24,5 @@ cask 'podman' do
     FileUtils.rm(man1)
   end
 
-  zap trash: '~/.config/containers'
+  zap trash: "~/.config/containers"
 end
