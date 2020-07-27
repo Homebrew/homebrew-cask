aa010c9c219794dc354a6a47edd7bdcba4aff94e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:27:38 +0100

    jandi: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/jandi.rb
+++ Casks/jandi.rb
@@ -1,21 +1,21 @@
-cask 'jandi' do
+cask "jandi" do
   version :latest
   sha256 :no_check
 
-  url 'https://cdn.jandi.com/jandi-pc/download/JANDI.dmg'
-  name 'JANDI'
-  homepage 'https://www.jandi.com/landing/'
+  url "https://cdn.jandi.com/jandi-pc/download/JANDI.dmg"
+  name "JANDI"
+  homepage "https://www.jandi.com/landing/"
 
-  app 'JANDI.app'
+  app "JANDI.app"
 
-  uninstall quit: 'com.jandi.osx.JANDI'
+  uninstall quit: "com.jandi.osx.JANDI"
 
   zap trash: [
-               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.jandi.osx.jandi.sfl*',
-               '~/Library/Application Support/JANDI',
-               '~/Library/Application Support/com.jandi.osx.JANDI.ShipIt',
-               '~/Library/Caches/com.jandi.osx.JANDI',
-               '~/Library/Preferences/com.jandi.osx.JANDI.plist',
-               '~/Library/Saved Application State/com.jandi.osx.JANDI',
-             ]
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.jandi.osx.jandi.sfl*",
+    "~/Library/Application Support/JANDI",
+    "~/Library/Application Support/com.jandi.osx.JANDI.ShipIt",
+    "~/Library/Caches/com.jandi.osx.JANDI",
+    "~/Library/Preferences/com.jandi.osx.JANDI.plist",
+    "~/Library/Saved Application State/com.jandi.osx.JANDI",
+  ]
 end
