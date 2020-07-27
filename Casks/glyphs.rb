6c195f2ac4f3ee83ede7db1835c8f61721957991
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:25:26 +0100

    glyphs: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/glyphs.rb
+++ Casks/glyphs.rb
@@ -1,21 +1,21 @@
-cask 'glyphs' do
-  version '2.6.6,1343'
-  sha256 '3eab5eb32ad77096b0e0ed7f67a234c5387fb7a3dad520eaa66a83aa83eaa549'
+cask "glyphs" do
+  version "2.6.6,1343"
+  sha256 "3eab5eb32ad77096b0e0ed7f67a234c5387fb7a3dad520eaa66a83aa83eaa549"
 
   url "https://updates.glyphsapp.com/Glyphs#{version.major_minor_patch}-#{version.after_comma}.zip"
   appcast "https://updates.glyphsapp.com/appcast#{version.major}.xml"
-  name 'Glyphs'
-  homepage 'https://glyphsapp.com/'
+  name "Glyphs"
+  homepage "https://glyphsapp.com/"
 
   auto_updates true
 
-  app 'Glyphs.app'
+  app "Glyphs.app"
 
   zap trash: [
-               '~/Library/Application Support/Glyphs',
-               '~/Library/Application Support/com.GeorgSeifert.Glyphs2',
-               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.georgseifert.glyphs2.sfl*',
-               '~/Library/Caches/com.apple.helpd/SDMHelpData/Other/English/HelpSDMIndexFile/com.GeorgSeifert.Glyphs2.help*',
-               '~/Library/Preferences/com.GeorgSeifert.Glyphs2.plist',
-             ]
+    "~/Library/Application Support/Glyphs",
+    "~/Library/Application Support/com.GeorgSeifert.Glyphs2",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.georgseifert.glyphs2.sfl*",
+    "~/Library/Caches/com.apple.helpd/SDMHelpData/Other/English/HelpSDMIndexFile/com.GeorgSeifert.Glyphs2.help*",
+    "~/Library/Preferences/com.GeorgSeifert.Glyphs2.plist",
+  ]
 end
