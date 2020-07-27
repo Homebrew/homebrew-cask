e3d2d3952b7b1286e0b022421f83867fb4da5d88
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:19:20 +0100

    camo-studio: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/camo-studio.rb
+++ Casks/camo-studio.rb
@@ -1,13 +1,13 @@
-cask 'camo-studio' do
-  version '1.0.5,60'
-  sha256 '8e728f514be6599f731da59af89a36a20643b922af50f0dc4cc77e1acce8369f'
+cask "camo-studio" do
+  version "1.0.5,60"
+  sha256 "8e728f514be6599f731da59af89a36a20643b922af50f0dc4cc77e1acce8369f"
 
   url "https://reincubate.com/res/labs/camo/Camo%20Studio%20#{version.before_comma}%20%28#{version.after_comma}%29%20%5BRelease%5D.app.zip"
-  appcast 'https://uds.reincubate.com/release-notes/camo/?format=sparkle'
-  name 'Camo Studio'
-  homepage 'https://reincubate.com/camo/'
+  appcast "https://uds.reincubate.com/release-notes/camo/?format=sparkle"
+  name "Camo Studio"
+  homepage "https://reincubate.com/camo/"
 
-  app 'Camo Studio.app'
+  app "Camo Studio.app"
 
-  uninstall delete: '/Library/CoreMediaIO/Plug-Ins/DAL/ReincubateCamoDAL.plugin'
+  uninstall delete: "/Library/CoreMediaIO/Plug-Ins/DAL/ReincubateCamoDAL.plugin"
 end
