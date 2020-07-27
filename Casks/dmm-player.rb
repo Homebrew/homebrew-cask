c64dbf9d8933fba143724cb951d485128aabb43a
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:22:16 +0100

    dmm-player: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/dmm-player.rb
+++ Casks/dmm-player.rb
@@ -1,12 +1,12 @@
-cask 'dmm-player' do
-  version '2.0.10'
-  sha256 '885a8ef3cf9ba5a62cb0451d60f5777bdb206d0312a00209ad063b02239b7c88'
+cask "dmm-player" do
+  version "2.0.10"
+  sha256 "885a8ef3cf9ba5a62cb0451d60f5777bdb206d0312a00209ad063b02239b7c88"
 
   url "https://portalapp.dmm.com/dmmplayerv#{version.major}/dmm/#{version.dots_to_underscores}/DMMPlayerV#{version.major}Installer_#{version.dots_to_underscores}.pkg"
-  name 'DMM Player'
-  homepage 'https://www.dmm.com/digital/howto_dmmplayer_html/'
+  name "DMM Player"
+  homepage "https://www.dmm.com/digital/howto_dmmplayer_html/"
 
   pkg "DMMPlayerV#{version.major}Installer_#{version.dots_to_underscores}.pkg"
 
-  uninstall pkgutil: 'com.dmm.dmmplayerv*'
+  uninstall pkgutil: "com.dmm.dmmplayerv*"
 end
