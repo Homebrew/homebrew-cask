0a99e6a7d8eab56c59752c667fd42800fe346296
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:38:44 +0100

    wingpersonal: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/wingpersonal.rb
+++ Casks/wingpersonal.rb
@@ -1,13 +1,13 @@
-cask 'wingpersonal' do
-  version '7.2.3.0'
-  sha256 'f97d1cfafbade9325bc6adc4ed6fd6d55c5d1701635a10119777093f542ec7e3'
+cask "wingpersonal" do
+  version "7.2.3.0"
+  sha256 "f97d1cfafbade9325bc6adc4ed6fd6d55c5d1701635a10119777093f542ec7e3"
 
   url "https://wingware.com/pub/wing-personal/#{version}/wing-personal-#{version}.dmg"
-  appcast 'https://www.wingware.com/downloads/wing-personal'
-  name 'WingPersonal'
-  homepage 'https://www.wingware.com/'
+  appcast "https://www.wingware.com/downloads/wing-personal"
+  name "WingPersonal"
+  homepage "https://www.wingware.com/"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'WingPersonal.app'
+  app "WingPersonal.app"
 end
