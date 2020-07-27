5fd63682328e9e729bc3ff6336b5a6c7ed6c6f91
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:44 +0100

    raven-reader: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/raven-reader.rb
+++ Casks/raven-reader.rb
@@ -1,13 +1,13 @@
-cask 'raven-reader' do
-  version '0.8.3'
-  sha256 'e1d2796df71d2c0202ac4929aeecb548cc0e2fb6e54f392cd2eddade2ab5776a'
+cask "raven-reader" do
+  version "0.8.3"
+  sha256 "e1d2796df71d2c0202ac4929aeecb548cc0e2fb6e54f392cd2eddade2ab5776a"
 
   url "https://download.ravenreader.app/ravenreader/Raven%20Reader-#{version}-mac.zip"
-  appcast 'https://download.ravenreader.app/ravenreader/latest-mac.yml'
-  name 'Raven Reader'
-  homepage 'https://ravenreader.app/'
+  appcast "https://download.ravenreader.app/ravenreader/latest-mac.yml"
+  name "Raven Reader"
+  homepage "https://ravenreader.app/"
 
   auto_updates true
 
-  app 'Raven Reader.app'
+  app "Raven Reader.app"
 end
