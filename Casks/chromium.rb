b8cb15d6d3577f9df843fad300a770ab8bb77764
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:19:57 +0100

    chromium: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -1,22 +1,22 @@
-cask 'chromium' do
-  version '791416'
-  sha256 'f1edc2b618a3ad9016c6f1ebc24861be451edb4d5d24755bb88435ac6f0d7489'
+cask "chromium" do
+  version "791416"
+  sha256 "f1edc2b618a3ad9016c6f1ebc24861be451edb4d5d24755bb88435ac6f0d7489"
 
   # commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/ was verified as official when first introduced to the cask
   url "https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/#{version}/chrome-mac.zip"
-  appcast 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2FLAST_CHANGE?alt=media'
-  name 'Chromium'
-  homepage 'https://www.chromium.org/Home'
+  appcast "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2FLAST_CHANGE?alt=media"
+  name "Chromium"
+  homepage "https://www.chromium.org/Home"
 
   conflicts_with cask: [
-                         'eloston-chromium',
-                         'freesmug-chromium',
-                       ]
+    "eloston-chromium",
+    "freesmug-chromium",
+  ]
 
-  app 'chrome-mac/Chromium.app'
+  app "chrome-mac/Chromium.app"
   # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
   shimscript = "#{staged_path}/chromium.wrapper.sh"
-  binary shimscript, target: 'chromium'
+  binary shimscript, target: "chromium"
 
   preflight do
     IO.write shimscript, <<~EOS
@@ -26,9 +26,9 @@ cask 'chromium' do
   end
 
   zap trash: [
-               '~/Library/Application Support/Chromium',
-               '~/Library/Caches/Chromium',
-               '~/Library/Preferences/org.chromium.Chromium.plist',
-               '~/Library/Saved Application State/org.chromium.Chromium.savedState',
-             ]
+    "~/Library/Application Support/Chromium",
+    "~/Library/Caches/Chromium",
+    "~/Library/Preferences/org.chromium.Chromium.plist",
+    "~/Library/Saved Application State/org.chromium.Chromium.savedState",
+  ]
 end
