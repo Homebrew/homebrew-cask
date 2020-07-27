596a9a4cc7204547e469d274feabe63ca23f9985
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:18:07 +0100

    bisq: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/bisq.rb
+++ Casks/bisq.rb
@@ -1,12 +1,12 @@
-cask 'bisq' do
-  version '1.3.6'
-  sha256 '80e15a5c5eb9164b48ffc293aa7c24988b1b9f586537ff4c49967e0d06c23704'
+cask "bisq" do
+  version "1.3.6"
+  sha256 "80e15a5c5eb9164b48ffc293aa7c24988b1b9f586537ff4c49967e0d06c23704"
 
   # github.com/bisq-network/bisq/ was verified as official when first introduced to the cask
   url "https://github.com/bisq-network/bisq/releases/download/v#{version}/Bisq-#{version}.dmg"
-  appcast 'https://github.com/bisq-network/bisq/releases.atom'
-  name 'Bisq'
-  homepage 'https://bisq.network/'
+  appcast "https://github.com/bisq-network/bisq/releases.atom"
+  name "Bisq"
+  homepage "https://bisq.network/"
 
-  app 'Bisq.app'
+  app "Bisq.app"
 end
