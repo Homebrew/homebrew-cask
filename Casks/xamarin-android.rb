da6c51c9f2d51488883cb480dc0059d95468040c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:38:52 +0100

    xamarin-android: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/xamarin-android.rb
+++ Casks/xamarin-android.rb
@@ -1,21 +1,21 @@
-cask 'xamarin-android' do
-  version '10.3.1.4'
-  sha256 '1ee7fe2568dc8682f29c61ce3ea54ddb1d632612898d7464d0208f3e40ad76e4'
+cask "xamarin-android" do
+  version "10.3.1.4"
+  sha256 "1ee7fe2568dc8682f29c61ce3ea54ddb1d632612898d7464d0208f3e40ad76e4"
 
   url "https://dl.xamarin.com/MonoforAndroid/Mac/xamarin.android-#{version}.pkg"
-  appcast 'https://docs.microsoft.com/en-us/xamarin/android/release-notes/',
+  appcast "https://docs.microsoft.com/en-us/xamarin/android/release-notes/",
           must_contain: version.major_minor
-  name 'Xamarin.Android'
-  homepage 'https://www.xamarin.com/platform'
+  name "Xamarin.Android"
+  homepage "https://www.xamarin.com/platform"
 
   pkg "xamarin.android-#{version}.pkg"
 
-  uninstall pkgutil: 'com.xamarin.android.pkg'
+  uninstall pkgutil: "com.xamarin.android.pkg"
 
   zap trash: [
-               '/Developer/MonoAndroid',
-               '/Library/Frameworks/Xamarin.Android.framework',
-               '~/Library/Caches/Xamarin.Android',
-               '~/Library/Caches/XamarinBuildDownload',
-             ]
+    "/Developer/MonoAndroid",
+    "/Library/Frameworks/Xamarin.Android.framework",
+    "~/Library/Caches/Xamarin.Android",
+    "~/Library/Caches/XamarinBuildDownload",
+  ]
 end
