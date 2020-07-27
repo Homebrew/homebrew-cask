efd30990c93add4aa535d3c544e8bbf52735dd2f
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:23:48 +0100

    feed-the-beast: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/feed-the-beast.rb
+++ Casks/feed-the-beast.rb
@@ -1,16 +1,16 @@
-cask 'feed-the-beast' do
-  version '1.0.9'
-  sha256 '67e9877f8eead9429af293c2505f48d55904bbc05b9b0962099c942776e42f48'
+cask "feed-the-beast" do
+  version "1.0.9"
+  sha256 "67e9877f8eead9429af293c2505f48d55904bbc05b9b0962099c942776e42f48"
 
   # apps.modpacks.ch/FTBApp/ was verified as official when first introduced to the cask
   url "https://apps.modpacks.ch/FTBApp/release/#{version}/FTBA_macos_#{version.dots_to_underscores}.dmg"
-  appcast 'https://www.feed-the-beast.com/app_release.xml'
-  name 'Feed the Beast'
-  homepage 'https://www.feed-the-beast.com/'
+  appcast "https://www.feed-the-beast.com/app_release.xml"
+  name "Feed the Beast"
+  homepage "https://www.feed-the-beast.com/"
 
-  app 'FTBApp.app'
+  app "FTBApp.app"
 
-  zap trash: '~/Library/Application Support/ftblauncher'
+  zap trash: "~/Library/Application Support/ftblauncher"
 
   caveats do
     depends_on_java
