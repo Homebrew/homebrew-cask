598019c1e0d75a1bbc805ba54d668941be5c7af8
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:20:52 +0100

    console: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/console.rb
+++ Casks/console.rb
@@ -1,13 +1,13 @@
-cask 'console' do
-  version '0.3.0'
-  sha256 '8cf565a7ec7bdb0a8f4e4fa9bf94b2893253e1012040b5ec57212c87b0d09eb8'
+cask "console" do
+  version "0.3.0"
+  sha256 "8cf565a7ec7bdb0a8f4e4fa9bf94b2893253e1012040b5ec57212c87b0d09eb8"
 
   url "https://github.com/macmade/Console/releases/download/#{version}/Console.app.zip"
-  appcast 'https://github.com/macmade/Console/releases.atom'
-  name 'Console'
-  homepage 'https://github.com/macmade/Console'
+  appcast "https://github.com/macmade/Console/releases.atom"
+  name "Console"
+  homepage "https://github.com/macmade/Console"
 
-  depends_on macos: '>= :el_capitan'
+  depends_on macos: ">= :el_capitan"
 
-  app 'Console.app'
+  app "Console.app"
 end
