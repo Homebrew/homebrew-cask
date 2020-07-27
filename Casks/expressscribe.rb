eac54e03e5f3690b1119f86b7c4fc92960e1c80f
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:23:37 +0100

    expressscribe: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/expressscribe.rb
+++ Casks/expressscribe.rb
@@ -1,17 +1,17 @@
-cask 'expressscribe' do
+cask "expressscribe" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.nch.com.au/scribe/scribemaci.zip'
-  name 'Express Scribe Transcription Software'
-  homepage 'https://www.nch.com.au/scribe/index.html'
+  url "https://www.nch.com.au/scribe/scribemaci.zip"
+  name "Express Scribe Transcription Software"
+  homepage "https://www.nch.com.au/scribe/index.html"
 
-  app 'ExpressScribe.app'
+  app "ExpressScribe.app"
 
-  uninstall quit:       'com.nchsoftware.expressscribe-free'
+  uninstall quit:       "com.nchsoftware.expressscribe-free"
 
   zap trash: [
-               '~/Library/Preferences/com.nchsoftware.expressscribe-free.plist',
-               '~/Library/Saved Application State/com.nchsoftware.expressscribe-free.savedState',
-             ]
+    "~/Library/Preferences/com.nchsoftware.expressscribe-free.plist",
+    "~/Library/Saved Application State/com.nchsoftware.expressscribe-free.savedState",
+  ]
 end
