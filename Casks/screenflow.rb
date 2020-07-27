3ef70083a3e66729abb61556ccbd449d7dcf4c7f
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:26 +0100

    screenflow: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/screenflow.rb
+++ Casks/screenflow.rb
@@ -1,14 +1,14 @@
-cask 'screenflow' do
-  version '9.0.4'
-  sha256 '8bd025c385f58b1c1675a4095fa7dd6d30d2bb500803c2d206320a9eada82eaf'
+cask "screenflow" do
+  version "9.0.4"
+  sha256 "8bd025c385f58b1c1675a4095fa7dd6d30d2bb500803c2d206320a9eada82eaf"
 
   url "https://www.telestream.net/download-files/screenflow/#{version.major_minor.dots_to_hyphens}/ScreenFlow-#{version}.dmg"
-  appcast 'https://www.telestream.net/updater/screenflow/appcast.xml'
-  name 'ScreenFlow'
-  homepage 'https://www.telestream.net/screenflow/'
+  appcast "https://www.telestream.net/updater/screenflow/appcast.xml"
+  name "ScreenFlow"
+  homepage "https://www.telestream.net/screenflow/"
 
   auto_updates true
-  depends_on macos: '>= :high_sierra'
+  depends_on macos: ">= :high_sierra"
 
-  app 'ScreenFlow.app'
+  app "ScreenFlow.app"
 end
