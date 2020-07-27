170a56bd56892091629a56f7f884a79d148da0cb
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:28:24 +0100

    klatexformula: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/klatexformula.rb
+++ Casks/klatexformula.rb
@@ -1,12 +1,12 @@
-cask 'klatexformula' do
-  version '4.1.0'
-  sha256 'fe868fcec17638f98f5c730f1dee20fe91981523392e49d6b9b23795f2b4b897'
+cask "klatexformula" do
+  version "4.1.0"
+  sha256 "fe868fcec17638f98f5c730f1dee20fe91981523392e49d6b9b23795f2b4b897"
 
   # downloads.sourceforge.net/klatexformula/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/klatexformula/klatexformula/klatexformula-#{version}/klatexformula-#{version}-macosx.dmg"
-  appcast 'https://sourceforge.net/projects/klatexformula/rss?path=/klatexformula'
-  name 'KLatexFormula'
-  homepage 'https://klatexformula.sourceforge.io/'
+  appcast "https://sourceforge.net/projects/klatexformula/rss?path=/klatexformula"
+  name "KLatexFormula"
+  homepage "https://klatexformula.sourceforge.io/"
 
-  app 'klatexformula.app'
+  app "klatexformula.app"
 end
