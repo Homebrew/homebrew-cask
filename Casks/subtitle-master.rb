11718c883d936cfa4a7b47db4149cac820acea92
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:50 +0100

    subtitle-master: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/subtitle-master.rb
+++ Casks/subtitle-master.rb
@@ -1,11 +1,11 @@
-cask 'subtitle-master' do
-  version '2.0.1'
-  sha256 '8936495ef4aefe9bf59c28dd4d5a92574c194062067c318667e3f55428245304'
+cask "subtitle-master" do
+  version "2.0.1"
+  sha256 "8936495ef4aefe9bf59c28dd4d5a92574c194062067c318667e3f55428245304"
 
   url "https://github.com/subtitle-master/subtitlemaster/releases/download/v#{version}-SNAPSHOT/Subtitle.Master-osx-v#{version}-SNAPSHOT.zip"
-  appcast 'https://github.com/subtitle-master/subtitlemaster/releases.atom'
-  name 'Subtitle Master'
-  homepage 'https://github.com/subtitle-master/subtitlemaster/releases'
+  appcast "https://github.com/subtitle-master/subtitlemaster/releases.atom"
+  name "Subtitle Master"
+  homepage "https://github.com/subtitle-master/subtitlemaster/releases"
 
-  app 'Subtitle Master.app'
+  app "Subtitle Master.app"
 end
