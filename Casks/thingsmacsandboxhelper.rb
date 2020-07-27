6f7b8c9b54c5240f6424da1f342080fe6084a166
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:37:25 +0100

    thingsmacsandboxhelper: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/thingsmacsandboxhelper.rb
+++ Casks/thingsmacsandboxhelper.rb
@@ -1,12 +1,12 @@
-cask 'thingsmacsandboxhelper' do
-  version '3.22'
-  sha256 '513e8a1aac1950a769b69f434655fa60e5110590e745aad4f34da93ff9aa59c8'
+cask "thingsmacsandboxhelper" do
+  version "3.22"
+  sha256 "513e8a1aac1950a769b69f434655fa60e5110590e745aad4f34da93ff9aa59c8"
 
   # culturedcode.cachefly.net/ was verified as official when first introduced to the cask
   url "https://culturedcode.cachefly.net/things/thingssandboxhelper/#{version}/ThingsHelper.zip"
-  appcast 'https://culturedcode.com/things/mac/help/things-sandboxing-helper-things3/'
-  name 'ThingsMacSandboxHelper'
+  appcast "https://culturedcode.com/things/mac/help/things-sandboxing-helper-things3/"
+  name "ThingsMacSandboxHelper"
   homepage "https://culturedcode.com/things/mac/help/things-sandboxing-helper-things#{version.major}/"
 
-  app 'ThingsMacSandboxHelper.app'
+  app "ThingsMacSandboxHelper.app"
 end
