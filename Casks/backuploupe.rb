921c7517f0d1f05207186a072c8d0557afc78c1a
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:17:22 +0100

    backuploupe: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/backuploupe.rb
+++ Casks/backuploupe.rb
@@ -1,11 +1,11 @@
-cask 'backuploupe' do
-  version '3.0.4'
-  sha256 'eec416e294d362418acc1c320b7e14aadcd04802a86cffa393651b45a4a412d0'
+cask "backuploupe" do
+  version "3.0.4"
+  sha256 "eec416e294d362418acc1c320b7e14aadcd04802a86cffa393651b45a4a412d0"
 
   url "https://www.soma-zone.com/download/files/BackupLoupe-#{version}.tar.bz2"
   appcast "https://www.soma-zone.com/BackupLoupe/a/appcast-update-#{version.major}.xml"
-  name 'BackupLoupe'
-  homepage 'https://www.soma-zone.com/BackupLoupe/'
+  name "BackupLoupe"
+  homepage "https://www.soma-zone.com/BackupLoupe/"
 
-  app 'BackupLoupe.app'
+  app "BackupLoupe.app"
 end
