29de5d563de06f705207ab579a07cda2be7c0de5
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:17 +0100

    lulu: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/lulu.rb
+++ Casks/lulu.rb
@@ -1,36 +1,36 @@
-cask 'lulu' do
-  version '1.2.3'
-  sha256 '9c7bf4333af8dc7330614f2c9a389feac27334bc07f4b6039107b97eb476a3df'
+cask "lulu" do
+  version "1.2.3"
+  sha256 "9c7bf4333af8dc7330614f2c9a389feac27334bc07f4b6039107b97eb476a3df"
 
   # bitbucket.org/objective-see/ was verified as official when first introduced to the cask
   url "https://bitbucket.org/objective-see/deploy/downloads/LuLu_#{version}.zip"
-  appcast 'https://objective-see.com/products/changelogs/LuLu.txt'
-  name 'LuLu'
-  homepage 'https://objective-see.com/products/lulu.html'
+  appcast "https://objective-see.com/products/changelogs/LuLu.txt"
+  name "LuLu"
+  homepage "https://objective-see.com/products/lulu.html"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
   installer script: {
-                      executable: "#{staged_path}/Lulu Installer.app/Contents/MacOS/LuLu Installer",
-                      args:       ['-install'],
-                      sudo:       true,
-                    }
+    executable: "#{staged_path}/Lulu Installer.app/Contents/MacOS/LuLu Installer",
+    args:       ["-install"],
+    sudo:       true,
+  }
 
   uninstall script: {
-                      executable: "#{staged_path}/Lulu Installer.app/Contents/MacOS/LuLu Installer",
-                      args:       ['-uninstall'],
-                      sudo:       true,
-                    }
+    executable: "#{staged_path}/Lulu Installer.app/Contents/MacOS/LuLu Installer",
+    args:       ["-uninstall"],
+    sudo:       true,
+  }
 
   zap trash: [
-               '~/Library/Caches/com.objective-see.lulu',
-               '~/Library/Caches/com.objective-see.lulu.helper',
-               '~/Library/Preferences/com.objective-see.lulu.plist',
-               '~/Library/Preferences/com.objective-see.lulu.helper.plist',
-               '/Library/LaunchDaemons/com.objective-see.lulu.plist',
-               '/Library/LaunchDaemons/com.objective-see.lulu.configHelper.plist',
-               '/Library/Logs/LuLu.log',
-             ]
+    "~/Library/Caches/com.objective-see.lulu",
+    "~/Library/Caches/com.objective-see.lulu.helper",
+    "~/Library/Preferences/com.objective-see.lulu.plist",
+    "~/Library/Preferences/com.objective-see.lulu.helper.plist",
+    "/Library/LaunchDaemons/com.objective-see.lulu.plist",
+    "/Library/LaunchDaemons/com.objective-see.lulu.configHelper.plist",
+    "/Library/Logs/LuLu.log",
+  ]
 
   caveats do
     kext
