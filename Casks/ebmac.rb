3366cd2cea252c8773720ee8c4a709ad29f7c8b4
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:22:52 +0100

    ebmac: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/ebmac.rb
+++ Casks/ebmac.rb
@@ -1,12 +1,12 @@
-cask 'ebmac' do
-  version '1.45.1'
-  sha256 '91f454e709607ae8b397b13e4201adc4add8e80394969918218fc097831f3c73'
+cask "ebmac" do
+  version "1.45.1"
+  sha256 "91f454e709607ae8b397b13e4201adc4add8e80394969918218fc097831f3c73"
 
   # ftp.vector.co.jp/72/54/938/ was verified as official when first introduced to the cask
   url "https://ftp.vector.co.jp/72/54/938/EBMac#{version}.dmg"
-  appcast 'http://ebstudio.info/manual/EBMac/'
-  name 'EBMac'
-  homepage 'http://ebstudio.info/manual/EBMac/'
+  appcast "http://ebstudio.info/manual/EBMac/"
+  name "EBMac"
+  homepage "http://ebstudio.info/manual/EBMac/"
 
-  app 'EBMac.app'
+  app "EBMac.app"
 end
