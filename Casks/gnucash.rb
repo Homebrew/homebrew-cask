e070f3f9c27b027b9bcb50719d2149121223c6cf
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:25:27 +0100

    gnucash: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/gnucash.rb
+++ Casks/gnucash.rb
@@ -1,18 +1,18 @@
-cask 'gnucash' do
-  version '4.1,1'
-  sha256 '2750e0391b545544f54aaa11117a1b999a82f178452c01d018c00642e82cf2b1'
+cask "gnucash" do
+  version "4.1,1"
+  sha256 "2750e0391b545544f54aaa11117a1b999a82f178452c01d018c00642e82cf2b1"
 
   # github.com/Gnucash/gnucash/ was verified as official when first introduced to the cask
-  url "https://github.com/Gnucash/gnucash/releases/download/#{version.before_comma}/Gnucash-Intel-#{version.before_comma.chomp('b')}-#{version.after_comma}.dmg"
-  appcast 'https://github.com/Gnucash/gnucash/releases.atom'
-  name 'GnuCash'
-  homepage 'https://www.gnucash.org/'
+  url "https://github.com/Gnucash/gnucash/releases/download/#{version.before_comma}/Gnucash-Intel-#{version.before_comma.chomp("b")}-#{version.after_comma}.dmg"
+  appcast "https://github.com/Gnucash/gnucash/releases.atom"
+  name "GnuCash"
+  homepage "https://www.gnucash.org/"
 
-  app 'Gnucash.app'
+  app "Gnucash.app"
 
   zap trash: [
-               '~/Library/Application Support/Gnucash',
-               '~/Library/Preferences/org.gnucash.Gnucash.plist',
-               '~/Library/Saved Application State/org.gnucash.Gnucash.savedState',
-             ]
+    "~/Library/Application Support/Gnucash",
+    "~/Library/Preferences/org.gnucash.Gnucash.plist",
+    "~/Library/Saved Application State/org.gnucash.Gnucash.savedState",
+  ]
 end
