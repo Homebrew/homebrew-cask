56d963a70a0725052ad88b36f3a004d2ce0a52e6
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:05 +0100

    pdf-images: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/pdf-images.rb
+++ Casks/pdf-images.rb
@@ -1,12 +1,12 @@
-cask 'pdf-images' do
-  version '1.1'
-  sha256 '76092c2978d062e7e18d6ec538c259d2360307261c53d98512dfdca340c9e19a'
+cask "pdf-images" do
+  version "1.1"
+  sha256 "76092c2978d062e7e18d6ec538c259d2360307261c53d98512dfdca340c9e19a"
 
   url "https://downloads.sourceforge.net/pdf-images/PDF-Images-#{version}.dmg"
-  appcast 'https://sourceforge.net/projects/pdf-images/rss'
-  name 'PDF-Images'
-  homepage 'https://sourceforge.net/projects/pdf-images/'
+  appcast "https://sourceforge.net/projects/pdf-images/rss"
+  name "PDF-Images"
+  homepage "https://sourceforge.net/projects/pdf-images/"
 
-  app 'PDF-Images.app'
+  app "PDF-Images.app"
   binary "#{appdir}/PDF-Images.app/Contents/MacOS/pdfimages"
 end
