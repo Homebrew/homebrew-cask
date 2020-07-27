deebced6e39dc26a0b16d2392d25450170316adf
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:28:06 +0100

    keep-it: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/keep-it.rb
+++ Casks/keep-it.rb
@@ -1,20 +1,20 @@
-cask 'keep-it' do
-  version '1.8.11'
-  sha256 '397fd43bc4edece305f9530c5656efd580d650eb126b34d8c0b9672288729118'
+cask "keep-it" do
+  version "1.8.11"
+  sha256 "397fd43bc4edece305f9530c5656efd580d650eb126b34d8c0b9672288729118"
 
   url "https://reinventedsoftware.com/keepit/downloads/KeepIt_#{version}.dmg"
-  appcast 'https://reinventedsoftware.com/keepit/downloads/keepit.xml'
-  name 'Keep It'
-  homepage 'https://reinventedsoftware.com/keepit/'
+  appcast "https://reinventedsoftware.com/keepit/downloads/keepit.xml"
+  name "Keep It"
+  homepage "https://reinventedsoftware.com/keepit/"
 
   auto_updates true
-  depends_on macos: '>= :high_sierra'
+  depends_on macos: ">= :high_sierra"
 
-  app 'Keep It.app'
+  app "Keep It.app"
 
   zap trash: [
-               '~/Library/Containers/com.reinvented.Keep-It-Indexing',
-               '~/Library/Containers/com.reinvented.Keep-It-Metadata',
-               '~/Library/Containers/com.reinvented.Keep It',
-             ]
+    "~/Library/Containers/com.reinvented.Keep-It-Indexing",
+    "~/Library/Containers/com.reinvented.Keep-It-Metadata",
+    "~/Library/Containers/com.reinvented.Keep It",
+  ]
 end
