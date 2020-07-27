34e90f71f18286b428ed79374c0fcd823cd728a0
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:38:28 +0100

    vrew: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/vrew.rb
+++ Casks/vrew.rb
@@ -1,15 +1,15 @@
-cask 'vrew' do
-  version '0.4.31'
-  sha256 'ccc848a635619a8cb753af9b75c1f01016ebd9e314b2eeab81759c35b906bed6'
+cask "vrew" do
+  version "0.4.31"
+  sha256 "ccc848a635619a8cb753af9b75c1f01016ebd9e314b2eeab81759c35b906bed6"
 
   url "https://vrew-files.voyagerx.com/Vrew-#{version}.dmg"
-  name 'vrew'
-  homepage 'https://vrew.voyagerx.com/'
+  name "vrew"
+  homepage "https://vrew.voyagerx.com/"
 
-  app 'Vrew.app'
+  app "Vrew.app"
 
   zap trash: [
-               '~/Library/Application Support/vrew',
-               '~/Library/Preferences/com.voyagerx.vrew.plist',
-             ]
+    "~/Library/Application Support/vrew",
+    "~/Library/Preferences/com.voyagerx.vrew.plist",
+  ]
 end
