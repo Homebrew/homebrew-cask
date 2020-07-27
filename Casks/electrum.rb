d79a63c3e02801ece2f16ecb0511f23a949def9d
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:23:08 +0100

    electrum: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/electrum.rb
+++ Casks/electrum.rb
@@ -1,18 +1,18 @@
-cask 'electrum' do
-  version '4.0.2'
-  sha256 'dfbdda1b09ea7b7ad39657a4930fd9b90957fb108970900cd978feafcfa82d10'
+cask "electrum" do
+  version "4.0.2"
+  sha256 "dfbdda1b09ea7b7ad39657a4930fd9b90957fb108970900cd978feafcfa82d10"
 
   url "https://download.electrum.org/#{version}/electrum-#{version}.dmg"
-  appcast 'https://github.com/spesmilo/electrum/releases.atom'
-  name 'Electrum'
-  homepage 'https://electrum.org/'
+  appcast "https://github.com/spesmilo/electrum/releases.atom"
+  name "Electrum"
+  homepage "https://electrum.org/"
 
-  app 'Electrum.app'
+  app "Electrum.app"
 
   zap trash: [
-               '~/.electrum',
-               '~/Library/Preferences/Electrum.plist',
-               '~/Library/Preferences/org.org.pythonmac.unspecified.Electrum.plist',
-               '~/Library/Saved Application State/Electrum.savedState',
-             ]
+    "~/.electrum",
+    "~/Library/Preferences/Electrum.plist",
+    "~/Library/Preferences/org.org.pythonmac.unspecified.Electrum.plist",
+    "~/Library/Saved Application State/Electrum.savedState",
+  ]
 end
