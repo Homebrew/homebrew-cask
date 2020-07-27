fbd54be3a69ac5a71981c5213f2aaf2cc6e90b03
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:24:00 +0100

    finicky: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/finicky.rb
+++ Casks/finicky.rb
@@ -1,13 +1,13 @@
-cask 'finicky' do
-  version '3.0.0'
-  sha256 '4cc8d684a0d43787a759679a646a3769e848f80212b7c6dfb4305fa3fd541249'
+cask "finicky" do
+  version "3.0.0"
+  sha256 "4cc8d684a0d43787a759679a646a3769e848f80212b7c6dfb4305fa3fd541249"
 
   url "https://github.com/johnste/finicky/releases/download/v#{version}/Finicky.zip"
-  appcast 'https://github.com/johnste/finicky/releases.atom'
-  name 'Finicky'
-  homepage 'https://github.com/johnste/finicky'
+  appcast "https://github.com/johnste/finicky/releases.atom"
+  name "Finicky"
+  homepage "https://github.com/johnste/finicky"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'Finicky.app'
+  app "Finicky.app"
 end
