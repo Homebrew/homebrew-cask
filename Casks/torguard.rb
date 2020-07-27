8aabfcdb41f474e8ccdf5cd0cfac52f2b972878a
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:37:42 +0100

    torguard: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/torguard.rb
+++ Casks/torguard.rb
@@ -1,20 +1,20 @@
-cask 'torguard' do
-  version '3.99.3'
-  sha256 'c7f80420af4f8d30c49731da1228a36bcebb1fb5adec7a3dfdbe7b1ca7ab9b3b'
+cask "torguard" do
+  version "3.99.3"
+  sha256 "c7f80420af4f8d30c49731da1228a36bcebb1fb5adec7a3dfdbe7b1ca7ab9b3b"
 
   # torguard.biz/ was verified as official when first introduced to the cask
   url "https://updates.torguard.biz/Software/MacOSX/TorGuard-v#{version}.dmg"
-  appcast 'https://updates.torguard.biz/Software/MacOSX/checksums.sha256'
-  name 'TorGuard'
-  homepage 'https://torguard.net/'
+  appcast "https://updates.torguard.biz/Software/MacOSX/checksums.sha256"
+  name "TorGuard"
+  homepage "https://torguard.net/"
 
-  pkg 'Install TorGuard.pkg'
+  pkg "Install TorGuard.pkg"
 
-  uninstall pkgutil: 'net.torguard.TorGuardDesktopQt',
-            delete:  '/Applications/TorGuard.app'
+  uninstall pkgutil: "net.torguard.TorGuardDesktopQt",
+            delete:  "/Applications/TorGuard.app"
 
   zap trash: [
-               '~/Library/Preferences/net.torguard.TorGuard*.plist',
-               '~/Library/Saved Application State/net.torguard.TorGuardDesktopQt.savedState',
-             ]
+    "~/Library/Preferences/net.torguard.TorGuard*.plist",
+    "~/Library/Saved Application State/net.torguard.TorGuardDesktopQt.savedState",
+  ]
 end
