a0640d716252473ca73a228fd7a175bf74cb0af4
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:25:45 +0100

    gpodder: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/gpodder.rb
+++ Casks/gpodder.rb
@@ -1,12 +1,12 @@
-cask 'gpodder' do
-  version '3.10.16'
-  sha256 '122ba5535a6a870b06ea60e670a51de29fc436d103c370439e91cc610dd46531'
+cask "gpodder" do
+  version "3.10.16"
+  sha256 "122ba5535a6a870b06ea60e670a51de29fc436d103c370439e91cc610dd46531"
 
   # github.com/gpodder/gpodder/ was verified as official when first introduced to the cask
   url "https://github.com/gpodder/gpodder/releases/download/#{version}/macOS-gPodder-#{version}.zip"
-  appcast 'https://github.com/gpodder/gpodder/releases.atom'
-  name 'gPodder'
-  homepage 'https://gpodder.github.io/'
+  appcast "https://github.com/gpodder/gpodder/releases.atom"
+  name "gPodder"
+  homepage "https://gpodder.github.io/"
 
-  app 'gPodder.app'
+  app "gPodder.app"
 end
