362bdb75ae9294c58ae75cf33f985244bd37ed38
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:59 +0100

    switch: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/switch.rb
+++ Casks/switch.rb
@@ -1,10 +1,10 @@
-cask 'switch' do
+cask "switch" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.nch.com.au/components/switchmaci.zip'
-  name 'Switch Audio Converter'
-  homepage 'https://www.nch.com.au/switch/'
+  url "https://www.nch.com.au/components/switchmaci.zip"
+  name "Switch Audio Converter"
+  homepage "https://www.nch.com.au/switch/"
 
-  app 'Switch.app'
+  app "Switch.app"
 end
