b529678289cbba0d6e973af87b5d856cc068516c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:30 +0100

    splitshow: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/splitshow.rb
+++ Casks/splitshow.rb
@@ -1,11 +1,11 @@
-cask 'splitshow' do
-  version '0.9.12-alpha'
-  sha256 '742b5ddc5d171c4bcc8281f4469df8ca62a59c71a7b318ff29ad9a664b80f2b8'
+cask "splitshow" do
+  version "0.9.12-alpha"
+  sha256 "742b5ddc5d171c4bcc8281f4469df8ca62a59c71a7b318ff29ad9a664b80f2b8"
 
   url "https://github.com/mpflanzer/splitshow/releases/download/v#{version}/SplitShow.app.zip"
-  appcast 'https://github.com/mpflanzer/splitshow/releases.atom'
-  name 'SplitShow'
-  homepage 'https://github.com/mpflanzer/splitshow'
+  appcast "https://github.com/mpflanzer/splitshow/releases.atom"
+  name "SplitShow"
+  homepage "https://github.com/mpflanzer/splitshow"
 
-  app 'SplitShow.app'
+  app "SplitShow.app"
 end
