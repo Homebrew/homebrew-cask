ada7caac2c9f61f756553285084e6d6ca999d7e4
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:49 +0100

    recents: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/recents.rb
+++ Casks/recents.rb
@@ -1,12 +1,12 @@
-cask 'recents' do
+cask "recents" do
   version :latest
   sha256 :no_check
 
-  url 'https://recentsapp.com/releases/Recents_latest.zip'
-  name 'Recents'
-  homepage 'https://recentsapp.com/'
+  url "https://recentsapp.com/releases/Recents_latest.zip"
+  name "Recents"
+  homepage "https://recentsapp.com/"
 
-  depends_on macos: '>= :high_sierra'
+  depends_on macos: ">= :high_sierra"
 
-  app 'Recents.app'
+  app "Recents.app"
 end
