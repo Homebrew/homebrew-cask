e7a9ffd1075c25d8fac771b49895624f55177df2
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:18 +0100

    sonoair: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/sonoair.rb
+++ Casks/sonoair.rb
@@ -1,10 +1,10 @@
-cask 'sonoair' do
-  version '1.0-beta-7.0'
-  sha256 '6badaa05917b97f4acc8eb09996f5a7748c04b006376511cdecc72ddb7c28760'
+cask "sonoair" do
+  version "1.0-beta-7.0"
+  sha256 "6badaa05917b97f4acc8eb09996f5a7748c04b006376511cdecc72ddb7c28760"
 
   url "https://sonoair.mihosoft.eu/releases/#{version}/SonoAir.zip"
-  name 'SonoAir'
-  homepage 'https://sonoair.mihosoft.eu/'
+  name "SonoAir"
+  homepage "https://sonoair.mihosoft.eu/"
 
-  app 'SonoAir.app'
+  app "SonoAir.app"
 end
