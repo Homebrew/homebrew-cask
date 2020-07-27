b354b28b33795175c9d9451ee313bc2eb039d5a7
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:30:08 +0100

    media-converter: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/media-converter.rb
+++ Casks/media-converter.rb
@@ -1,12 +1,12 @@
-cask 'media-converter' do
-  version '2.0.10'
-  sha256 'd5a7db000e8106e5892daad84ee7dea82651aa9662df5b4c6da0b943afae03c1'
+cask "media-converter" do
+  version "2.0.10"
+  sha256 "d5a7db000e8106e5892daad84ee7dea82651aa9662df5b4c6da0b943afae03c1"
 
   # downloads.sourceforge.net/media-converter/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/media-converter/media-converter/#{version}/media-converter-#{version}.zip"
-  appcast 'https://sourceforge.net/projects/media-converter/rss?path=/media-converter'
-  name 'Media Converter'
-  homepage 'https://media-converter.sourceforge.io/'
+  appcast "https://sourceforge.net/projects/media-converter/rss?path=/media-converter"
+  name "Media Converter"
+  homepage "https://media-converter.sourceforge.io/"
 
-  app 'Media Converter.localized/Media Converter.app'
+  app "Media Converter.localized/Media Converter.app"
 end
