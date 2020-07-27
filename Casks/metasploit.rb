c9afc9f9e65c2816fa53e95e60331d8793a220ea
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:30:23 +0100

    metasploit: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/metasploit.rb
+++ Casks/metasploit.rb
@@ -1,33 +1,33 @@
-cask 'metasploit' do
-  version '5.0.100,20200717103419'
-  sha256 '1aeeb74a3b31b4bf330969b96f8c8b3d5f89391bfc569a04692416ab9865df31'
+cask "metasploit" do
+  version "5.0.100,20200717103419"
+  sha256 "1aeeb74a3b31b4bf330969b96f8c8b3d5f89391bfc569a04692416ab9865df31"
 
   url "https://osx.metasploit.com/metasploit-framework-#{version.before_comma}+#{version.after_comma}-1rapid7-1.pkg"
-  appcast 'https://osx.metasploit.com/LATEST'
-  name 'Metasploit Framework'
-  homepage 'https://www.metasploit.com/'
+  appcast "https://osx.metasploit.com/LATEST"
+  name "Metasploit Framework"
+  homepage "https://www.metasploit.com/"
 
-  depends_on formula: 'nmap'
+  depends_on formula: "nmap"
 
   pkg "metasploit-framework-#{version.before_comma} #{version.after_comma}-1rapid7-1.pkg"
-  binary '/opt/metasploit-framework/bin/msfbinscan'
-  binary '/opt/metasploit-framework/bin/msfconsole'
-  binary '/opt/metasploit-framework/bin/msfd'
-  binary '/opt/metasploit-framework/bin/msfdb'
-  binary '/opt/metasploit-framework/bin/msfelfscan'
-  binary '/opt/metasploit-framework/bin/msfmachscan'
-  binary '/opt/metasploit-framework/bin/msfpescan'
-  binary '/opt/metasploit-framework/bin/msfrop'
-  binary '/opt/metasploit-framework/bin/msfrpc'
-  binary '/opt/metasploit-framework/bin/msfrpcd'
-  binary '/opt/metasploit-framework/bin/msfvenom'
+  binary "/opt/metasploit-framework/bin/msfbinscan"
+  binary "/opt/metasploit-framework/bin/msfconsole"
+  binary "/opt/metasploit-framework/bin/msfd"
+  binary "/opt/metasploit-framework/bin/msfdb"
+  binary "/opt/metasploit-framework/bin/msfelfscan"
+  binary "/opt/metasploit-framework/bin/msfmachscan"
+  binary "/opt/metasploit-framework/bin/msfpescan"
+  binary "/opt/metasploit-framework/bin/msfrop"
+  binary "/opt/metasploit-framework/bin/msfrpc"
+  binary "/opt/metasploit-framework/bin/msfrpcd"
+  binary "/opt/metasploit-framework/bin/msfvenom"
 
   uninstall script: {
-                      executable: '/opt/metasploit-framework/bin/msfremove',
-                      input:      ['y'],
-                      sudo:       true,
-                    },
-            rmdir:  '/opt/metasploit-framework'
+    executable: "/opt/metasploit-framework/bin/msfremove",
+    input:      ["y"],
+    sudo:       true,
+  },
+            rmdir:  "/opt/metasploit-framework"
 
-  zap trash: '~/.msf4'
+  zap trash: "~/.msf4"
 end
