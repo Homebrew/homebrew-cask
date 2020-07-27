9d74def04cc850b3b1d2df4dccb5dd207deaf45f
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:23:11 +0100

    elpass: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/elpass.rb
+++ Casks/elpass.rb
@@ -1,11 +1,11 @@
-cask 'elpass' do
-  version '1.1.7,266'
-  sha256 '4ad2d532c6bf1c07f67d5f0e963eb16ee22532a2042eb127de36cb89d0e1469c'
+cask "elpass" do
+  version "1.1.7,266"
+  sha256 "4ad2d532c6bf1c07f67d5f0e963eb16ee22532a2042eb127de36cb89d0e1469c"
 
   url "https://elpass.app/macos/Elpass-#{version.before_comma}-#{version.after_comma}.zip"
-  appcast 'https://elpass.app/macos/appcast.xml'
-  name 'Elpass'
-  homepage 'https://elpass.app/'
+  appcast "https://elpass.app/macos/appcast.xml"
+  name "Elpass"
+  homepage "https://elpass.app/"
 
-  app 'Elpass.app'
+  app "Elpass.app"
 end
