c5916b35cf75fd9090ea1556f17018e524d27cb2
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:13:19 +0100

    ableton-live: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/ableton-live.rb
+++ Casks/ableton-live.rb
@@ -1,26 +1,26 @@
-cask 'ableton-live' do
-  version '10.1.15'
-  sha256 'ea3e9332ecce5760d739025254f83a04f97d04d58b0d47c6cdc96c28940604e3'
+cask "ableton-live" do
+  version "10.1.15"
+  sha256 "ea3e9332ecce5760d739025254f83a04f97d04d58b0d47c6cdc96c28940604e3"
 
   url "https://cdn-downloads.ableton.com/channels/#{version}/ableton_live_trial_#{version}_64.dmg"
   appcast "https://www.ableton.com/en/release-notes/live-#{version.major}/"
-  name 'Ableton Live'
-  homepage 'https://www.ableton.com/en/live/'
+  name "Ableton Live"
+  homepage "https://www.ableton.com/en/live/"
 
   auto_updates true
-  depends_on macos: '>= :el_capitan'
+  depends_on macos: ">= :el_capitan"
 
   app "Ableton Live #{version.major} Trial.app"
 
-  uninstall quit: 'com.ableton.live'
+  uninstall quit: "com.ableton.live"
 
   zap trash: [
-               '~/Library/Application Support/CrashReporter/Ableton *_*.plist',
-               '~/Library/Application Support/CrashReporter/Live_*.plist',
-               '~/Library/Application Support/Ableton',
-               '~/Library/Caches/Ableton',
-               '~/Library/Preferences/Ableton',
-               '~/Library/Preferences/com.ableton.live.plist*',
-               '~/Music/Ableton',
-             ]
+    "~/Library/Application Support/CrashReporter/Ableton *_*.plist",
+    "~/Library/Application Support/CrashReporter/Live_*.plist",
+    "~/Library/Application Support/Ableton",
+    "~/Library/Caches/Ableton",
+    "~/Library/Preferences/Ableton",
+    "~/Library/Preferences/com.ableton.live.plist*",
+    "~/Music/Ableton",
+  ]
 end
