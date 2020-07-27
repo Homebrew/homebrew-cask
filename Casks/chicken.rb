316b7dc9024f3bcae53df3b4e837a95d8c9f3684
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:19:52 +0100

    chicken: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/chicken.rb
+++ Casks/chicken.rb
@@ -1,13 +1,13 @@
-cask 'chicken' do
-  version '2.2b2'
-  sha256 '20e910b6cbf95c3e5dcf6fe8e120d5a0911f19099128981fb95119cee8d5fc6b'
+cask "chicken" do
+  version "2.2b2"
+  sha256 "20e910b6cbf95c3e5dcf6fe8e120d5a0911f19099128981fb95119cee8d5fc6b"
 
   url "https://downloads.sourceforge.net/chicken/Chicken-#{version}.dmg"
-  appcast 'https://chicken.sourceforge.io/chicken.xml'
-  name 'Chicken'
-  homepage 'https://sourceforge.net/projects/chicken/'
+  appcast "https://chicken.sourceforge.io/chicken.xml"
+  name "Chicken"
+  homepage "https://sourceforge.net/projects/chicken/"
 
-  app 'Chicken.app'
+  app "Chicken.app"
 
-  zap trash: '~/Library/Preferences/net.sourceforge.chicken.plist'
+  zap trash: "~/Library/Preferences/net.sourceforge.chicken.plist"
 end
