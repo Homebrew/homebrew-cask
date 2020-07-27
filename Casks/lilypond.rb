227a2e8e166c743f60653c649937f89a1b075210
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:28:59 +0100

    lilypond: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/lilypond.rb
+++ Casks/lilypond.rb
@@ -1,23 +1,23 @@
-cask 'lilypond' do
-  version '2.20.0-1'
-  sha256 '1680ca85ff7bdb942a39bff323956357fbb0e6ab17edf9c0e145d52b6c0dc231'
+cask "lilypond" do
+  version "2.20.0-1"
+  sha256 "1680ca85ff7bdb942a39bff323956357fbb0e6ab17edf9c0e145d52b6c0dc231"
 
   url "https://lilypond.org/downloads/binaries/darwin-x86/lilypond-#{version}.darwin-x86.tar.bz2"
-  appcast 'https://lilypond.org/macos-x.html'
-  name 'LilyPond'
-  homepage 'https://lilypond.org/'
+  appcast "https://lilypond.org/macos-x.html"
+  name "LilyPond"
+  homepage "https://lilypond.org/"
 
-  depends_on macos: '<= :mojave'
+  depends_on macos: "<= :mojave"
 
-  app 'LilyPond.app'
+  app "LilyPond.app"
 
-  binaries = [
-               'abc2ly',
-               'convert-ly',
-               'lilypond',
-               'lilypond-book',
-               'musicxml2ly',
-             ]
+  binaries = %w[
+    abc2ly
+    convert-ly
+    lilypond
+    lilypond-book
+    musicxml2ly
+  ]
 
   binaries.each do |shimscript|
     binary "#{staged_path}/#{shimscript}.wrapper.sh", target: shimscript
@@ -34,7 +34,7 @@ cask 'lilypond' do
   end
 
   zap trash: [
-               '~/Library/Preferences/org.lilypond.lilypond.plist',
-               '~/Library/Preferences/org.lilypond.lilypond.LSSharedFileList.plist',
-             ]
+    "~/Library/Preferences/org.lilypond.lilypond.plist",
+    "~/Library/Preferences/org.lilypond.lilypond.LSSharedFileList.plist",
+  ]
 end
