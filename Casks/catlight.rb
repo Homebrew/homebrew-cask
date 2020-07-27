b93d772e67714e549164fea1bb0d6ffb039c0b4d
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:19:31 +0100

    catlight: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/catlight.rb
+++ Casks/catlight.rb
@@ -1,13 +1,13 @@
-cask 'catlight' do
-  version '2.32.2'
-  sha256 'b3f6a4027813ab8f4549243fd7fab85cdb4a3ff2a2059af4f05b772d12af78fb'
+cask "catlight" do
+  version "2.32.2"
+  sha256 "b3f6a4027813ab8f4549243fd7fab85cdb4a3ff2a2059af4f05b772d12af78fb"
 
   url "https://download.catlight.io/rel/mac/beta/CatLightSetup-#{version}.zip"
-  appcast 'https://catlight.io/downloads'
-  name 'catlight'
-  homepage 'https://catlight.io/'
+  appcast "https://catlight.io/downloads"
+  name "catlight"
+  homepage "https://catlight.io/"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'Catlight.app'
+  app "Catlight.app"
 end
