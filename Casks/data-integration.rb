ca257deb71eef062481d99588f491cb644c6c359
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:21:31 +0100

    data-integration: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/data-integration.rb
+++ Casks/data-integration.rb
@@ -1,11 +1,11 @@
-cask 'data-integration' do
-  version '9.0.0.0-423'
-  sha256 '05adf26c8b51fb14d1ea75e73579a7718d881228f502a4be5d1ce3a27d5c7997'
+cask "data-integration" do
+  version "9.0.0.0-423"
+  sha256 "05adf26c8b51fb14d1ea75e73579a7718d881228f502a4be5d1ce3a27d5c7997"
 
   url "https://downloads.sourceforge.net/pentaho/pdi-ce-#{version}.zip"
-  appcast 'https://sourceforge.net/projects/pentaho/rss?path=/'
-  name 'Pentaho Data Integration'
-  homepage 'https://sourceforge.net/projects/pentaho/'
+  appcast "https://sourceforge.net/projects/pentaho/rss?path=/"
+  name "Pentaho Data Integration"
+  homepage "https://sourceforge.net/projects/pentaho/"
 
-  app 'data-integration/Data Integration.app'
+  app "data-integration/Data Integration.app"
 end
