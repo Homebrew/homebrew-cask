b160655e3eeb3ec13336fdc8943ed322fb3b295b
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:31:19 +0100

    musescore: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/musescore.rb
+++ Casks/musescore.rb
@@ -1,19 +1,19 @@
-cask 'musescore' do
-  version '3.4.2'
-  sha256 '59f7ee60d77b63a67a89f998f19ae3434959b6ed2eeb20c9e90ff56f5659f824'
+cask "musescore" do
+  version "3.4.2"
+  sha256 "59f7ee60d77b63a67a89f998f19ae3434959b6ed2eeb20c9e90ff56f5659f824"
 
   # github.com/musescore/MuseScore/ was verified as official when first introduced to the cask
   url "https://github.com/musescore/MuseScore/releases/download/v#{version}/MuseScore-#{version}.dmg"
-  appcast 'https://github.com/musescore/MuseScore/releases.atom'
-  name 'MuseScore'
-  homepage 'https://musescore.org/'
+  appcast "https://github.com/musescore/MuseScore/releases.atom"
+  name "MuseScore"
+  homepage "https://musescore.org/"
 
-  depends_on macos: '>= :yosemite'
+  depends_on macos: ">= :yosemite"
 
   app "MuseScore #{version.major}.app"
   # shim script (https://github.com/caskroom/homebrew-cask/issues/18809)
   shimscript = "#{staged_path}/mscore.wrapper.sh"
-  binary shimscript, target: 'mscore'
+  binary shimscript, target: "mscore"
 
   preflight do
     IO.write shimscript, <<~EOS
