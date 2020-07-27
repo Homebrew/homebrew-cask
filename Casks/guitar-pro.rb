374a97098167c5ba871d89d6040815d9abdb0356
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:25:57 +0100

    guitar-pro: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/guitar-pro.rb
+++ Casks/guitar-pro.rb
@@ -1,13 +1,13 @@
-cask 'guitar-pro' do
-  version '7.5'
+cask "guitar-pro" do
+  version "7.5"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://alt-downloads.guitar-pro.com/gp7/stable/guitar-pro-#{version.major}-setup.pkg"
-  appcast 'https://www.guitar-pro.com/en/index.php?pg=download'
-  name 'Guitar Pro'
-  homepage 'https://www.guitar-pro.com/'
+  appcast "https://www.guitar-pro.com/en/index.php?pg=download"
+  name "Guitar Pro"
+  homepage "https://www.guitar-pro.com/"
 
   pkg "guitar-pro-#{version.major}-setup.pkg"
 
-  uninstall pkgutil: 'com.arobas-music.guitarpro*'
+  uninstall pkgutil: "com.arobas-music.guitarpro*"
 end
