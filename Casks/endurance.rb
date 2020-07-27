41b3d92dcfdb91874cbde5854026b860bac48a4e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:23:17 +0100

    endurance: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/endurance.rb
+++ Casks/endurance.rb
@@ -1,14 +1,14 @@
-cask 'endurance' do
-  version '3.0.1'
-  sha256 'f4805baea7824ac6e3a695b841e0306cf789c72fabb5fd7dd579c76a11992069'
+cask "endurance" do
+  version "3.0.1"
+  sha256 "f4805baea7824ac6e3a695b841e0306cf789c72fabb5fd7dd579c76a11992069"
 
   url "https://enduranceapp.com/downloads/Endurance#{version}.zip"
-  appcast 'https://enduranceapp.com/appcast'
-  name 'Endurance'
-  homepage 'https://enduranceapp.com/'
+  appcast "https://enduranceapp.com/appcast"
+  name "Endurance"
+  homepage "https://enduranceapp.com/"
 
-  app 'Endurance.app'
+  app "Endurance.app"
 
-  uninstall delete:    '/Library/PrivilegedHelperTools/com.MagnetismStudios.endurance.helper',
-            launchctl: 'com.MagnetismStudios.endurance.helper'
+  uninstall delete:    "/Library/PrivilegedHelperTools/com.MagnetismStudios.endurance.helper",
+            launchctl: "com.MagnetismStudios.endurance.helper"
 end
