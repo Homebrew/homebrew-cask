4a2eed991949fb889bb520677cb61d7658703a9f
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:07 +0100

    purevpn: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/purevpn.rb
+++ Casks/purevpn.rb
@@ -1,11 +1,11 @@
-cask 'purevpn' do
+cask "purevpn" do
   version :latest
   sha256 :no_check
 
   # purevpn-dialer-assets.s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url 'https://purevpn-dialer-assets.s3.amazonaws.com/mac/app/purevpn_setup.dmg'
-  name 'PureVPN'
-  homepage 'https://www.purevpn.com/'
+  url "https://purevpn-dialer-assets.s3.amazonaws.com/mac/app/purevpn_setup.dmg"
+  name "PureVPN"
+  homepage "https://www.purevpn.com/"
 
-  app 'PureVPN.app'
+  app "PureVPN.app"
 end
