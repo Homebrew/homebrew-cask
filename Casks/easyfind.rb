440c54f7a73c0e78662953ad742234a2379cb6fa
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:22:50 +0100

    easyfind: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/easyfind.rb
+++ Casks/easyfind.rb
@@ -1,11 +1,11 @@
-cask 'easyfind' do
-  version '5.0'
-  sha256 'a389fc3c35a7d5111960c4ce5e631ad3b99ed1dd5534299b577a68cbd06bc327'
+cask "easyfind" do
+  version "5.0"
+  sha256 "a389fc3c35a7d5111960c4ce5e631ad3b99ed1dd5534299b577a68cbd06bc327"
 
   url "https://download.devontechnologies.com/download/freeware/easyfind/#{version}/EasyFind.app.zip"
-  appcast 'https://www.devontechnologies.com/apps/freeware'
-  name 'EasyFind'
-  homepage 'https://www.devontechnologies.com/apps/freeware'
+  appcast "https://www.devontechnologies.com/apps/freeware"
+  name "EasyFind"
+  homepage "https://www.devontechnologies.com/apps/freeware"
 
-  app 'EasyFind.app'
+  app "EasyFind.app"
 end
