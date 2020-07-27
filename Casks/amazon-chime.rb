617dc578941cbea090cc91357f6a67bf23804fde
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:15:43 +0100

    amazon-chime: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/amazon-chime.rb
+++ Casks/amazon-chime.rb
@@ -1,22 +1,22 @@
-cask 'amazon-chime' do
-  version '4.32.7871'
-  sha256 '0a929ce9f78fa8bebca404b513fd0c07020f1283d1674dfeecb0bbcbbefd5b61'
+cask "amazon-chime" do
+  version "4.32.7871"
+  sha256 "0a929ce9f78fa8bebca404b513fd0c07020f1283d1674dfeecb0bbcbbefd5b61"
 
   url "https://clients.chime.aws/mac/releases/AmazonChime-OSX.release-#{version}.dmg"
-  appcast 'https://clients.chime.aws/mac/appcast'
-  name 'Amazon Chime'
-  homepage 'https://chime.aws/'
+  appcast "https://clients.chime.aws/mac/appcast"
+  name "Amazon Chime"
+  homepage "https://chime.aws/"
 
-  depends_on macos: '>= :el_capitan'
+  depends_on macos: ">= :el_capitan"
 
-  app 'Amazon Chime.app'
+  app "Amazon Chime.app"
 
   zap trash: [
-               '~/Library/Application Support/Amazon Chime',
-               '~/Library/Application Support/com.amazon.Amazon-Chime',
-               '~/Library/Caches/com.amazon.Amazon-Chime',
-               '~/Library/Logs/Amazon Chime',
-               '~/Library/Preferences/com.amazon.Amazon-Chime.plist',
-               '~/Library/WebKit/com.amazon.Amazon-Chime',
-             ]
+    "~/Library/Application Support/Amazon Chime",
+    "~/Library/Application Support/com.amazon.Amazon-Chime",
+    "~/Library/Caches/com.amazon.Amazon-Chime",
+    "~/Library/Logs/Amazon Chime",
+    "~/Library/Preferences/com.amazon.Amazon-Chime.plist",
+    "~/Library/WebKit/com.amazon.Amazon-Chime",
+  ]
 end
