b2670473952f18ffdb7eaca1c5d224eabf224634
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:31:34 +0100

    navicat-for-sql-server: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/navicat-for-sql-server.rb
+++ Casks/navicat-for-sql-server.rb
@@ -1,11 +1,11 @@
-cask 'navicat-for-sql-server' do
-  version '15.0.18'
-  sha256 'f37c99a3ef442651660b0460c935e2f71ede7a9e3d043cec0e8ff9f7909b91a1'
+cask "navicat-for-sql-server" do
+  version "15.0.18"
+  sha256 "f37c99a3ef442651660b0460c935e2f71ede7a9e3d043cec0e8ff9f7909b91a1"
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_sqlserver_en.dmg"
-  appcast 'https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20for%20SQL%20Server&appLang=en'
-  name 'Navicat for SQL Server'
-  homepage 'https://www.navicat.com/products/navicat-for-sqlserver'
+  appcast "https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20for%20SQL%20Server&appLang=en"
+  name "Navicat for SQL Server"
+  homepage "https://www.navicat.com/products/navicat-for-sqlserver"
 
-  app 'Navicat For SQL Server.app'
+  app "Navicat For SQL Server.app"
 end
