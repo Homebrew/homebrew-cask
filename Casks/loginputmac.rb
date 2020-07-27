15015a517b6420d08f1e80e28fa51f441c77ea9c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:12 +0100

    loginputmac: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/loginputmac.rb
+++ Casks/loginputmac.rb
@@ -1,11 +1,11 @@
-cask 'loginputmac' do
-  version '2.3.6'
-  sha256 '473040a7a8ebd5d1a811903af0791b11060044869e799832be73f1b232243bcd'
+cask "loginputmac" do
+  version "2.3.6"
+  sha256 "473040a7a8ebd5d1a811903af0791b11060044869e799832be73f1b232243bcd"
 
   # loginput-mac2.content-delivery.top/ was verified as official when first introduced to the cask
   url "https://loginput-mac2.content-delivery.top/loginputmac#{version.major}_latest.pkg"
   appcast "https://im.logcg.com/appcast#{version.major}.xml"
-  name 'LoginputMac'
+  name "LoginputMac"
   homepage "https://im.logcg.com/loginputmac#{version.major}"
 
   auto_updates true
