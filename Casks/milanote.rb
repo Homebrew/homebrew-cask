5276620f9c77043133fde5d35848c895d12e9ddc
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:30:34 +0100

    milanote: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/milanote.rb
+++ Casks/milanote.rb
@@ -1,21 +1,21 @@
-cask 'milanote' do
-  version '2.2.4'
-  sha256 '1ae116ab8f41de25e8d8118e4609216a90f0b6c308cecac5fd84f129e724b6d8'
+cask "milanote" do
+  version "2.2.4"
+  sha256 "1ae116ab8f41de25e8d8118e4609216a90f0b6c308cecac5fd84f129e724b6d8"
 
   # milanote-app-releases.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://milanote-app-releases.s3.amazonaws.com/Milanote-#{version}.dmg"
-  appcast 'https://www.milanote.com/download-mac-app'
-  name 'Milanote'
-  homepage 'https://www.milanote.com/'
+  appcast "https://www.milanote.com/download-mac-app"
+  name "Milanote"
+  homepage "https://www.milanote.com/"
 
-  app 'Milanote.app'
+  app "Milanote.app"
 
   zap trash: [
-               '~/Library/Application Support/Milanote',
-               '~/Library/Caches/com.milanote.app',
-               '~/Library/Caches/com.milanote.app.ShipIt',
-               '~/Library/Library/Logs/Milanote',
-               '~/Library/Preferences/com.milanote.app.helper.plist',
-               '~/Library/Preferences/com.milanote.app.plist',
-             ]
+    "~/Library/Application Support/Milanote",
+    "~/Library/Caches/com.milanote.app",
+    "~/Library/Caches/com.milanote.app.ShipIt",
+    "~/Library/Library/Logs/Milanote",
+    "~/Library/Preferences/com.milanote.app.helper.plist",
+    "~/Library/Preferences/com.milanote.app.plist",
+  ]
 end
