176fc6836c80edcaa489d8b35c3946e66c9a6dd0
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:26:55 +0100

    imgur: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/imgur.rb
+++ Casks/imgur.rb
@@ -1,10 +1,10 @@
-cask 'imgur' do
+cask "imgur" do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/zbuc/imgurBar/raw/master/imgur.dmg'
-  name 'imgur'
-  homepage 'https://github.com/zbuc/imgurBar'
+  url "https://github.com/zbuc/imgurBar/raw/master/imgur.dmg"
+  name "imgur"
+  homepage "https://github.com/zbuc/imgurBar"
 
-  app 'imgur.app'
+  app "imgur.app"
 end
