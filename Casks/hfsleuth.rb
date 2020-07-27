72ae083e4b2e04aa7a1f80c1647aac8335a2ff8b
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:26:18 +0100

    hfsleuth: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/hfsleuth.rb
+++ Casks/hfsleuth.rb
@@ -1,11 +1,11 @@
-cask 'hfsleuth' do
+cask "hfsleuth" do
   version :latest
   sha256 :no_check
 
-  url 'http://newosxbook.com/files/hfsleuth.tar'
-  name 'HFSleuth'
-  homepage 'http://newosxbook.com/tools/hfsleuth.html'
+  url "http://newosxbook.com/files/hfsleuth.tar"
+  name "HFSleuth"
+  homepage "http://newosxbook.com/tools/hfsleuth.html"
 
-  binary 'hfsleuth.universal', target: 'hfsleuth'
-  manpage 'hfsleuth.1'
+  binary "hfsleuth.universal", target: "hfsleuth"
+  manpage "hfsleuth.1"
 end
