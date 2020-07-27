cdea717ddd8ad72aac43984207a11de8c41647bf
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:18 +0100

    qldds: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/qldds.rb
+++ Casks/qldds.rb
@@ -1,14 +1,14 @@
-cask 'qldds' do
-  version '1.33'
-  sha256 '7342f788f750bf4f40e84f6fcbfefb4603e78d00c99d55fae4ba65a78f9ff20f'
+cask "qldds" do
+  version "1.33"
+  sha256 "7342f788f750bf4f40e84f6fcbfefb4603e78d00c99d55fae4ba65a78f9ff20f"
 
   url "https://github.com/Marginal/QLdds/releases/download/rel-#{version.no_dots}/QLdds_#{version.no_dots}.pkg"
-  appcast 'https://github.com/Marginal/QLdds/releases.atom'
-  name 'QuickLook DDS'
-  homepage 'https://github.com/Marginal/QLdds'
+  appcast "https://github.com/Marginal/QLdds/releases.atom"
+  name "QuickLook DDS"
+  homepage "https://github.com/Marginal/QLdds"
 
   pkg "QLdds_#{version.no_dots}.pkg"
 
-  uninstall pkgutil:   'uk.org.marginal.qldds',
-            launchctl: 'uk.org.marginal.qldds.mdimporter'
+  uninstall pkgutil:   "uk.org.marginal.qldds",
+            launchctl: "uk.org.marginal.qldds.mdimporter"
 end
