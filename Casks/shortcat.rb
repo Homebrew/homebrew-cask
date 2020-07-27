4c2ae2e2c5d749dcca7d1cd2145a15340f1222e6
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:47 +0100

    shortcat: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/shortcat.rb
+++ Casks/shortcat.rb
@@ -1,17 +1,17 @@
-cask 'shortcat' do
-  version '0.7.11'
-  sha256 'bb62fcb6c644c1ccca356d0ac1ede1b29ebbc1cebe5809e186b3ba295ed51b07'
+cask "shortcat" do
+  version "0.7.11"
+  sha256 "bb62fcb6c644c1ccca356d0ac1ede1b29ebbc1cebe5809e186b3ba295ed51b07"
 
   url "https://files.shortcatapp.com/v#{version}/Shortcat.zip"
-  appcast 'https://shortcatapp.com/updates/appcast.xml'
-  name 'Sproutcube Shortcat'
-  homepage 'https://shortcatapp.com/'
+  appcast "https://shortcatapp.com/updates/appcast.xml"
+  name "Sproutcube Shortcat"
+  homepage "https://shortcatapp.com/"
 
-  app 'Shortcat.app'
+  app "Shortcat.app"
 
   zap trash: [
-               '~/Library/Application Support/Shortcat',
-               '~/Library/Cookies/com.sproutcube.Shortcat.binarycookies',
-               '~/Library/Preferences/com.sproutcube.Shortcat.plist',
-             ]
+    "~/Library/Application Support/Shortcat",
+    "~/Library/Cookies/com.sproutcube.Shortcat.binarycookies",
+    "~/Library/Preferences/com.sproutcube.Shortcat.plist",
+  ]
 end
