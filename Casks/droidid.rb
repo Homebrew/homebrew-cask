5835efe066f2aba62d2be03ee037f77448573750
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:22:31 +0100

    droidid: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/droidid.rb
+++ Casks/droidid.rb
@@ -1,10 +1,10 @@
-cask 'droidid' do
+cask "droidid" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.suyashsrijan.com/droidid/download/DroidID.zip'
-  name 'DroidID'
-  homepage 'https://www.suyashsrijan.com/droidid/'
+  url "https://www.suyashsrijan.com/droidid/download/DroidID.zip"
+  name "DroidID"
+  homepage "https://www.suyashsrijan.com/droidid/"
 
-  app 'DroidID.app'
+  app "DroidID.app"
 end
