e917c2c2dea9be345d89af39afc2696b2ec7fc09
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:38:45 +0100

    winzip: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/winzip.rb
+++ Casks/winzip.rb
@@ -1,10 +1,10 @@
-cask 'winzip' do
-  version '7.0.4565'
-  sha256 'e65aab9f4619a190da6fc29a01864049cf409e5ae6c962d840d96bbd25d98899'
+cask "winzip" do
+  version "7.0.4565"
+  sha256 "e65aab9f4619a190da6fc29a01864049cf409e5ae6c962d840d96bbd25d98899"
 
   url "https://download.winzip.com/winzipmacedition#{version.major}.dmg"
-  name 'WinZip'
-  homepage 'https://www.winzip.com/mac/en/winzip.html'
+  name "WinZip"
+  homepage "https://www.winzip.com/mac/en/winzip.html"
 
-  app 'WinZip.app'
+  app "WinZip.app"
 end
