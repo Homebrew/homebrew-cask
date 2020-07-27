a8447e39ec74cbc3a6b21cf72329a15e5f68da73
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:25:40 +0100

    googleappengine: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/googleappengine.rb
+++ Casks/googleappengine.rb
@@ -1,12 +1,12 @@
-cask 'googleappengine' do
-  version '1.9.89'
-  sha256 '9ed1780e28da381c92ea6667f64a8d6ba715658d3a8957bf5190fb7edbd00ba9'
+cask "googleappengine" do
+  version "1.9.89"
+  sha256 "9ed1780e28da381c92ea6667f64a8d6ba715658d3a8957bf5190fb7edbd00ba9"
 
   # storage.googleapis.com/appengine-sdks/ was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/appengine-sdks/featured/GoogleAppEngineLauncher-#{version}.dmg"
-  appcast 'https://storage.googleapis.com/appengine-sdks/featured/VERSION'
-  name 'Google App Engine'
-  homepage 'https://cloud.google.com/appengine/docs/standard/python/download#appengine_sdk/'
+  appcast "https://storage.googleapis.com/appengine-sdks/featured/VERSION"
+  name "Google App Engine"
+  homepage "https://cloud.google.com/appengine/docs/standard/python/download#appengine_sdk/"
 
-  app 'GoogleAppEngineLauncher.app'
+  app "GoogleAppEngineLauncher.app"
 end
