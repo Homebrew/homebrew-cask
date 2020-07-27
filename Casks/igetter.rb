bf3ad2f5c80ff9da1ef2ae875c7de2bb13c37b2d
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:26:47 +0100

    igetter: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/igetter.rb
+++ Casks/igetter.rb
@@ -1,11 +1,11 @@
-cask 'igetter' do
-  version '2.9.7'
-  sha256 'b085f89873005bd6c9055e6b7641738009229a8e83e0719be2206ec7ab037cc5'
+cask "igetter" do
+  version "2.9.7"
+  sha256 "b085f89873005bd6c9055e6b7641738009229a8e83e0719be2206ec7ab037cc5"
 
   url "https://www.igetter.net/search/downloads/iGetter#{version}.dmg"
-  appcast 'https://www.igetter.net/downloads.html'
-  name 'iGetter'
-  homepage 'https://www.igetter.net/index.html'
+  appcast "https://www.igetter.net/downloads.html"
+  name "iGetter"
+  homepage "https://www.igetter.net/index.html"
 
   app "iGetter #{version}/iGetter.app"
 end
