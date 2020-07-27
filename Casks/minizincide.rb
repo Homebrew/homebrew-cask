0f2239a2c30787d772ecc8c433eabcfc753f0890
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:30:41 +0100

    minizincide: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/minizincide.rb
+++ Casks/minizincide.rb
@@ -1,12 +1,12 @@
-cask 'minizincide' do
-  version '2.4.3'
-  sha256 'ac62403d386fd3b2471f347ce7d78fb2a5eb80fd08964c46881fc91a2603524e'
+cask "minizincide" do
+  version "2.4.3"
+  sha256 "ac62403d386fd3b2471f347ce7d78fb2a5eb80fd08964c46881fc91a2603524e"
 
   # github.com/MiniZinc/MiniZincIDE/ was verified as official when first introduced to the cask
   url "https://github.com/MiniZinc/MiniZincIDE/releases/download/#{version}/MiniZincIDE-#{version}-bundled.dmg"
-  appcast 'https://github.com/MiniZinc/MiniZincIDE/releases.atom'
-  name 'MiniZincIDE'
-  homepage 'https://www.minizinc.org/ide/index.html'
+  appcast "https://github.com/MiniZinc/MiniZincIDE/releases.atom"
+  name "MiniZincIDE"
+  homepage "https://www.minizinc.org/ide/index.html"
 
-  app 'MiniZincIDE.app'
+  app "MiniZincIDE.app"
 end
