b7684e6d2a134e051c7d97ab1517805a7c736e3c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:27:01 +0100

    insomnia: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/insomnia.rb
+++ Casks/insomnia.rb
@@ -1,25 +1,25 @@
-cask 'insomnia' do
-  version '2020.3.3'
-  sha256 'c22949f717ffaf8bdef10d0a833b1a9fe0eb2bebe317913db2e1ce7572fd5a44'
+cask "insomnia" do
+  version "2020.3.3"
+  sha256 "c22949f717ffaf8bdef10d0a833b1a9fe0eb2bebe317913db2e1ce7572fd5a44"
 
   # github.com/Kong/insomnia/ was verified as official when first introduced to the cask
   url "https://github.com/Kong/insomnia/releases/download/core%40#{version}/Insomnia.Core-#{version}.dmg"
-  appcast 'https://api.insomnia.rest/changelog.json?app=com.insomnia.app'
-  name 'Insomnia'
-  homepage 'https://insomnia.rest/'
+  appcast "https://api.insomnia.rest/changelog.json?app=com.insomnia.app"
+  name "Insomnia"
+  homepage "https://insomnia.rest/"
 
   auto_updates true
 
-  app 'Insomnia.app'
+  app "Insomnia.app"
 
   zap trash: [
-               '~/Library/Application Support/Insomnia',
-               '~/Library/Caches/com.insomnia.app',
-               '~/Library/Caches/com.insomnia.app.ShipIt',
-               '~/Library/Cookies/com.insomnia.app.binarycookies',
-               '~/Library/Preferences/ByHost/com.insomnia.app.ShipIt.*.plist',
-               '~/Library/Preferences/com.insomnia.app.helper.plist',
-               '~/Library/Preferences/com.insomnia.app.plist',
-               '~/Library/Saved Application State/com.insomnia.app.savedState',
-             ]
+    "~/Library/Application Support/Insomnia",
+    "~/Library/Caches/com.insomnia.app",
+    "~/Library/Caches/com.insomnia.app.ShipIt",
+    "~/Library/Cookies/com.insomnia.app.binarycookies",
+    "~/Library/Preferences/ByHost/com.insomnia.app.ShipIt.*.plist",
+    "~/Library/Preferences/com.insomnia.app.helper.plist",
+    "~/Library/Preferences/com.insomnia.app.plist",
+    "~/Library/Saved Application State/com.insomnia.app.savedState",
+  ]
 end
