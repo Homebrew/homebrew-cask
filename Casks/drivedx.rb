85437a609182313af7c327e4f81509c3564300d5
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:22:30 +0100

    drivedx: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/drivedx.rb
+++ Casks/drivedx.rb
@@ -1,11 +1,11 @@
-cask 'drivedx' do
-  version '1.9.1'
-  sha256 'cdea2b9609b2c31f0e7df0f558c5bd1198ded0dbd5eaf643b118c0fedc8621e2'
+cask "drivedx" do
+  version "1.9.1"
+  sha256 "cdea2b9609b2c31f0e7df0f558c5bd1198ded0dbd5eaf643b118c0fedc8621e2"
 
   url "https://binaryfruit.com/download/drivedx/mac/#{version.major}/bin/DriveDx.#{version}.zip"
   appcast "https://binaryfruit.com/download/drivedx/mac/#{version.major}/updates/?appcast&appName=DriveDxMac"
-  name 'DriveDX'
-  homepage 'https://binaryfruit.com/drivedx'
+  name "DriveDX"
+  homepage "https://binaryfruit.com/drivedx"
 
-  app 'DriveDX.app'
+  app "DriveDX.app"
 end
