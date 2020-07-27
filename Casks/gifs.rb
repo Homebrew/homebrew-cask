51298038c8690fb49c8ec7d393d3d15814336af2
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:25:12 +0100

    gifs: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/gifs.rb
+++ Casks/gifs.rb
@@ -1,11 +1,11 @@
-cask 'gifs' do
+cask "gifs" do
   version :latest
   sha256 :no_check
 
   # raw.githubusercontent.com/orta/GIFs/ was verified as official when first introduced to the cask
-  url 'https://raw.githubusercontent.com/orta/GIFs/master/web/GIFs.app.zip'
-  name 'GIFs'
-  homepage 'https://github.com/orta/GIFs'
+  url "https://raw.githubusercontent.com/orta/GIFs/master/web/GIFs.app.zip"
+  name "GIFs"
+  homepage "https://github.com/orta/GIFs"
 
-  app 'GIFs.app'
+  app "GIFs.app"
 end
