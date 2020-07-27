e828339df0d3dd306c2543b020eaf1303093e4b3
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:39 +0100

    r-name: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/r-name.rb
+++ Casks/r-name.rb
@@ -1,10 +1,10 @@
-cask 'r-name' do
+cask "r-name" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.jacek-dom.net/software/R-Name/R-Name.app.zip'
-  name 'R-Name'
-  homepage 'https://www.jacek-dom.net/software/R-Name/'
+  url "https://www.jacek-dom.net/software/R-Name/R-Name.app.zip"
+  name "R-Name"
+  homepage "https://www.jacek-dom.net/software/R-Name/"
 
-  app 'R-Name.app'
+  app "R-Name.app"
 end
