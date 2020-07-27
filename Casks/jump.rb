9f815ac1e173825f871a00054847990a6bcfd14b
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:27:58 +0100

    jump: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/jump.rb
+++ Casks/jump.rb
@@ -1,17 +1,17 @@
-cask 'jump' do
-  version '8.5.15'
-  sha256 'e90e0211481a52bc74dac39bb9847bfe605fdce628225d87ef9c8961985bfed9'
+cask "jump" do
+  version "8.5.15"
+  sha256 "e90e0211481a52bc74dac39bb9847bfe605fdce628225d87ef9c8961985bfed9"
 
-  url 'https://dl.jumpdesktop.com/downloads/JumpDesktopMac.zip'
-  appcast 'https://jumpdesktop.com/downloads/viewer/jdmac-web-appcast.xml'
-  name 'Jump Desktop'
-  homepage 'https://jumpdesktop.com/#jdmac'
+  url "https://dl.jumpdesktop.com/downloads/JumpDesktopMac.zip"
+  appcast "https://jumpdesktop.com/downloads/viewer/jdmac-web-appcast.xml"
+  name "Jump Desktop"
+  homepage "https://jumpdesktop.com/#jdmac"
 
-  app 'Jump Desktop.app'
+  app "Jump Desktop.app"
 
   zap trash: [
-               '~/Documents/JumpDesktop',
-               '~/Library/Caches/com.p5sys.jump.mac.viewer.web',
-               '~/Library/Cookies/com.p5sys.jump.mac.viewer.web.binarycookies',
-             ]
+    "~/Documents/JumpDesktop",
+    "~/Library/Caches/com.p5sys.jump.mac.viewer.web",
+    "~/Library/Cookies/com.p5sys.jump.mac.viewer.web.binarycookies",
+  ]
 end
