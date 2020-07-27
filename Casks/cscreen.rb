787ea56a8ede9fb7c5aebaee1b9b6d7adf4be349
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:21:16 +0100

    cscreen: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/cscreen.rb
+++ Casks/cscreen.rb
@@ -1,10 +1,10 @@
-cask 'cscreen' do
-  version '2012.09'
-  sha256 '522348667b4ac13a3bd63afee5a9b796b97cb06f12f4e9cbb1943dd4c8bb0895'
+cask "cscreen" do
+  version "2012.09"
+  sha256 "522348667b4ac13a3bd63afee5a9b796b97cb06f12f4e9cbb1943dd4c8bb0895"
 
   url "https://www.pyehouse.com/wp-content/uploads/#{version.major}/#{version.minor}/cscreenIntel.dmg"
-  name 'cscreen'
-  homepage 'https://www.pyehouse.com/cscreen/'
+  name "cscreen"
+  homepage "https://www.pyehouse.com/cscreen/"
 
-  binary 'cscreen'
+  binary "cscreen"
 end
