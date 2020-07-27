8cbc91c20f236e00c2f7d699415ba05cbd53784e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:31:42 +0100

    netron: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/netron.rb
+++ Casks/netron.rb
@@ -1,13 +1,13 @@
-cask 'netron' do
-  version '4.4.2'
-  sha256 '054d799778985e2cc911e4f058443b207af63d2a86c436250523a7f3f56669ca'
+cask "netron" do
+  version "4.4.2"
+  sha256 "054d799778985e2cc911e4f058443b207af63d2a86c436250523a7f3f56669ca"
 
   url "https://github.com/lutzroeder/netron/releases/download/v#{version}/Netron-#{version}-mac.zip"
-  appcast 'https://github.com/lutzroeder/netron/releases.atom'
-  name 'Netron'
-  homepage 'https://github.com/lutzroeder/netron'
+  appcast "https://github.com/lutzroeder/netron/releases.atom"
+  name "Netron"
+  homepage "https://github.com/lutzroeder/netron"
 
   auto_updates true
 
-  app 'Netron.app'
+  app "Netron.app"
 end
