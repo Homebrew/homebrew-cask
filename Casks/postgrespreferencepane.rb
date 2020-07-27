e9356477af766e7a51029a9c43d821a49750fd1e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:44 +0100

    postgrespreferencepane: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/postgrespreferencepane.rb
+++ Casks/postgrespreferencepane.rb
@@ -1,11 +1,11 @@
-cask 'postgrespreferencepane' do
-  version '2.4.2'
-  sha256 'fb3f618321efec12cbad21ec2d77e6a024a35f7a17bb304b421970f85e38337f'
+cask "postgrespreferencepane" do
+  version "2.4.2"
+  sha256 "fb3f618321efec12cbad21ec2d77e6a024a35f7a17bb304b421970f85e38337f"
 
   url "https://github.com/MaccaTech/PostgresPrefs/releases/download/v#{version}/PostgreSQL.prefPane-v#{version}.zip"
-  appcast 'https://github.com/MaccaTech/PostgresPrefs/releases.atom'
-  name 'PostgresPrefs'
-  homepage 'https://github.com/MaccaTech/PostgresPrefs'
+  appcast "https://github.com/MaccaTech/PostgresPrefs/releases.atom"
+  name "PostgresPrefs"
+  homepage "https://github.com/MaccaTech/PostgresPrefs"
 
-  prefpane 'PostgreSQL.prefPane'
+  prefpane "PostgreSQL.prefPane"
 end
