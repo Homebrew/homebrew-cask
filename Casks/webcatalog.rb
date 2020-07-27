a0eaf2c55a26ae2559d22bfdafaaa71eb8720fc0
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:38:34 +0100

    webcatalog: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/webcatalog.rb
+++ Casks/webcatalog.rb
@@ -1,22 +1,22 @@
-cask 'webcatalog' do
-  version '22.5.0'
-  sha256 '0830d3826f1e0ce93ce485c1b577c13e5f171ca0aca85e42dc482344ffa47771'
+cask "webcatalog" do
+  version "22.5.0"
+  sha256 "0830d3826f1e0ce93ce485c1b577c13e5f171ca0aca85e42dc482344ffa47771"
 
   # github.com/quanglam2807/webcatalog/ was verified as official when first introduced to the cask
   url "https://github.com/quanglam2807/webcatalog/releases/download/v#{version}/WebCatalog-#{version}-mac.zip"
-  appcast 'https://github.com/quanglam2807/webcatalog/releases.atom'
-  name 'WebCatalog'
-  homepage 'https://getwebcatalog.com/'
+  appcast "https://github.com/quanglam2807/webcatalog/releases.atom"
+  name "WebCatalog"
+  homepage "https://getwebcatalog.com/"
 
   auto_updates true
 
-  app 'WebCatalog.app'
+  app "WebCatalog.app"
 
   zap trash: [
-               '~/Library/Application Support/WebCatalog',
-               '~/Library/Caches/com.webcatalog.jordan',
-               '~/Library/Caches/com.webcatalog.jordan.ShipIt',
-               '~/Library/Preferences/com.webcatalog.jordan.plist',
-               '~/Library/Saved Application State/com.webcatalog.jordan.savedState',
-             ]
+    "~/Library/Application Support/WebCatalog",
+    "~/Library/Caches/com.webcatalog.jordan",
+    "~/Library/Caches/com.webcatalog.jordan.ShipIt",
+    "~/Library/Preferences/com.webcatalog.jordan.plist",
+    "~/Library/Saved Application State/com.webcatalog.jordan.savedState",
+  ]
 end
