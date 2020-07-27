f239ef3118e37e2b497e3289922ad9e4350bf501
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:32:38 +0100

    oracle-jdk: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/oracle-jdk.rb
+++ Casks/oracle-jdk.rb
@@ -1,21 +1,21 @@
-cask 'oracle-jdk' do
-  version '14.0.2,12:205943a0976c4ed48cb16f1043c5c647'
-  sha256 'ddf839f23f266fd6a70bcea5a7f2b2ec21cb616decf909fb11c0a6787c5fa75c'
+cask "oracle-jdk" do
+  version "14.0.2,12:205943a0976c4ed48cb16f1043c5c647"
+  sha256 "ddf839f23f266fd6a70bcea5a7f2b2ec21cb616decf909fb11c0a6787c5fa75c"
 
   url "https://download.oracle.com/otn-pub/java/jdk/#{version.before_comma}+#{version.after_comma.before_colon}/#{version.after_colon}/jdk-#{version.before_comma}_osx-x64_bin.dmg",
       cookies: {
-                 'oraclelicense' => 'accept-securebackup-cookie',
-               }
-  name 'Oracle Java Standard Edition Development Kit'
-  homepage 'https://www.oracle.com/technetwork/java/javase/overview/index.html'
+        "oraclelicense" => "accept-securebackup-cookie",
+      }
+  name "Oracle Java Standard Edition Development Kit"
+  homepage "https://www.oracle.com/technetwork/java/javase/overview/index.html"
 
-  depends_on macos: '>= :yosemite'
+  depends_on macos: ">= :yosemite"
 
   pkg "JDK #{version.before_comma}.pkg"
 
   uninstall pkgutil: "com.oracle.jdk-#{version.before_comma}"
 
   caveats do
-    license 'https://www.oracle.com/technetwork/java/javase/terms/license/javase-license.html'
+    license "https://www.oracle.com/technetwork/java/javase/terms/license/javase-license.html"
   end
 end
