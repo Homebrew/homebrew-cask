308711a0fd8e9c40eb035fa3c7d0f970c8ba5655
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:56 +0100

    proclaim: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/proclaim.rb
+++ Casks/proclaim.rb
@@ -1,12 +1,12 @@
-cask 'proclaim' do
-  version '2.12.0.0080'
-  sha256 '602a3e4ba29e08e883245cb65e882c49d7376695490418899daaaa331e9bec0c'
+cask "proclaim" do
+  version "2.12.0.0080"
+  sha256 "602a3e4ba29e08e883245cb65e882c49d7376695490418899daaaa331e9bec0c"
 
   # logoscdn.com/Proclaim/ was verified as official when first introduced to the cask
   url "https://downloads.logoscdn.com/Proclaim/Installer/#{version}/Proclaim.dmg"
-  appcast 'http://downloads.proclaimonline.com/mac/appcast.xml'
-  name 'Faithlife Proclaim'
-  homepage 'https://proclaim.faithlife.com/'
+  appcast "http://downloads.proclaimonline.com/mac/appcast.xml"
+  name "Faithlife Proclaim"
+  homepage "https://proclaim.faithlife.com/"
 
-  app 'Proclaim.app'
+  app "Proclaim.app"
 end
