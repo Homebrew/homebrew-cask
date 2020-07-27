70c472b338605b7904e9bd5a4e01c4a7ca897886
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:39 +0100

    stand: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/stand.rb
+++ Casks/stand.rb
@@ -1,14 +1,14 @@
-cask 'stand' do
-  version '2.0'
-  sha256 'eb0555f6376757105e1faea1fd1f9e4afae21eef73fdd5c7d0fb0c76a8cd9702'
+cask "stand" do
+  version "2.0"
+  sha256 "eb0555f6376757105e1faea1fd1f9e4afae21eef73fdd5c7d0fb0c76a8cd9702"
 
   # f001.backblazeb2.com/file/stand-app/ was verified as official when first introduced to the cask
   url "https://f001.backblazeb2.com/file/stand-app/#{version}/Stand.zip"
-  appcast 'https://standapp-sparkle-updater.herokuapp.com/'
-  name 'Stand'
-  homepage 'https://getstandapp.com/'
+  appcast "https://standapp-sparkle-updater.herokuapp.com/"
+  name "Stand"
+  homepage "https://getstandapp.com/"
 
-  depends_on macos: '>= :catalina'
+  depends_on macos: ">= :catalina"
 
-  app 'Stand.app'
+  app "Stand.app"
 end
