335aaa1e1fa22d3d95dbcd852470721b46ba13cf
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:28:56 +0100

    lifesize: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/lifesize.rb
+++ Casks/lifesize.rb
@@ -1,11 +1,11 @@
-cask 'lifesize' do
-  version '2.212.2712'
-  sha256 'ae941b7c9561e8757814ea1e20bb475dd032ec217db02c518d37fadec46eec53'
+cask "lifesize" do
+  version "2.212.2712"
+  sha256 "ae941b7c9561e8757814ea1e20bb475dd032ec217db02c518d37fadec46eec53"
 
   # download.lifesizecloud.com/ was verified as official when first introduced to the cask
   url "https://download.lifesizecloud.com/Lifesize-#{version}.dmg"
-  name 'lifesize'
-  homepage 'https://www.lifesize.com/'
+  name "lifesize"
+  homepage "https://www.lifesize.com/"
 
-  app 'Lifesize.app'
+  app "Lifesize.app"
 end
