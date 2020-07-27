803b9aae999d88533d0b66649bcdb1dfcc4f60aa
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:15 +0100

    qgis: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/qgis.rb
+++ Casks/qgis.rb
@@ -1,12 +1,12 @@
-cask 'qgis' do
-  version '3.14.1'
-  sha256 '9ff7dd12c37dd748190fd32d9223bc041850732c7377291b31cc02a9ba178625'
+cask "qgis" do
+  version "3.14.1"
+  sha256 "9ff7dd12c37dd748190fd32d9223bc041850732c7377291b31cc02a9ba178625"
 
-  url 'https://qgis.org/downloads/macos/qgis-macos-pr.dmg'
-  appcast 'https://qgis.org/downloads/macos/qgis-macos-pr.sha256sum',
+  url "https://qgis.org/downloads/macos/qgis-macos-pr.dmg"
+  appcast "https://qgis.org/downloads/macos/qgis-macos-pr.sha256sum",
           must_contain: version.dots_to_underscores
-  name 'QGIS'
-  homepage 'https://www.qgis.org/'
+  name "QGIS"
+  homepage "https://www.qgis.org/"
 
   app "QGIS#{version.major_minor}.app"
 end
