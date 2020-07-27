19241aaf9b260082d361e893b6ef6ad02c9ad4e2
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:18:53 +0100

    bria: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/bria.rb
+++ Casks/bria.rb
@@ -1,12 +1,12 @@
-cask 'bria' do
-  version '6.1.2_104265'
-  sha256 'fbd1768550a74875d049bbbc535df78fd92d03fbdeb5fb3ca8d80d9cda2f1f5c'
+cask "bria" do
+  version "6.1.2_104265"
+  sha256 "fbd1768550a74875d049bbbc535df78fd92d03fbdeb5fb3ca8d80d9cda2f1f5c"
 
   # counterpath.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://counterpath.s3.amazonaws.com/downloads/Bria_#{version}.dmg"
-  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.counterpath.com/Bria6forMac'
-  name 'Bria'
-  homepage 'https://www.counterpath.com/bria-solo/'
+  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.counterpath.com/Bria6forMac"
+  name "Bria"
+  homepage "https://www.counterpath.com/bria-solo/"
 
-  app 'Bria.app'
+  app "Bria.app"
 end
