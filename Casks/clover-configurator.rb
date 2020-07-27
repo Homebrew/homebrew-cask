f38f73895c960e5a7ef1e501a8f201720cb0ac17
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:20:30 +0100

    clover-configurator: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/clover-configurator.rb
+++ Casks/clover-configurator.rb
@@ -1,10 +1,10 @@
-cask 'clover-configurator' do
+cask "clover-configurator" do
   version :latest
   sha256 :no_check
 
-  url 'https://mackie100projects.altervista.org/apps/cloverconf/download-new-build.php?version=global'
-  name 'Clover Configurator'
-  homepage 'https://mackie100projects.altervista.org/'
+  url "https://mackie100projects.altervista.org/apps/cloverconf/download-new-build.php?version=global"
+  name "Clover Configurator"
+  homepage "https://mackie100projects.altervista.org/"
 
-  app 'Clover Configurator.app'
+  app "Clover Configurator.app"
 end
