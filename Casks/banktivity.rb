39d3eec2a4055e2ce3a5fc5039162cd666e4c716
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:17:31 +0100

    banktivity: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/banktivity.rb
+++ Casks/banktivity.rb
@@ -1,14 +1,14 @@
-cask 'banktivity' do
-  version '7.5.1'
-  sha256 '68728cfdb9b0b145cac19cbcabafc20166c564a2d91e3e0f4cb2200e1b22a73a'
+cask "banktivity" do
+  version "7.5.1"
+  sha256 "68728cfdb9b0b145cac19cbcabafc20166c564a2d91e3e0f4cb2200e1b22a73a"
 
   # iggsoft.com/ was verified as official when first introduced to the cask
   url "https://www.iggsoft.com/banktivity/Banktivity#{version.major}_Web.dmg"
-  appcast 'https://www.iggsoftware.com/product-versions.json'
-  name 'Banktivity'
-  homepage 'https://www.iggsoftware.com/banktivity/'
+  appcast "https://www.iggsoftware.com/product-versions.json"
+  name "Banktivity"
+  homepage "https://www.iggsoftware.com/banktivity/"
 
-  depends_on macos: '>= :high_sierra'
+  depends_on macos: ">= :high_sierra"
 
   app "Banktivity #{version.major}.app"
 end
