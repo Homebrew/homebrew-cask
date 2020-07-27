8c5e8fa1afc4300862f232167f4c3b37592c7664
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:18:22 +0100

    blu-ray-player-pro: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/blu-ray-player-pro.rb
+++ Casks/blu-ray-player-pro.rb
@@ -1,11 +1,11 @@
-cask 'blu-ray-player-pro' do
-  version '3.3.19_191021_2039'
-  sha256 'e398690c5d4d536fb267f5323314c39b56a39e4f7308ff8602fb4430c4528869'
+cask "blu-ray-player-pro" do
+  version "3.3.19_191021_2039"
+  sha256 "e398690c5d4d536fb267f5323314c39b56a39e4f7308ff8602fb4430c4528869"
 
   url "https://cdn.macblurayplayer.com/mac-bluray-player-pro3/Blu-ray_Player_Pro_#{version}.dmg"
-  appcast 'https://cdn.macblurayplayer.com/mac-bluray-player-pro3/appcast/Appcast.xml'
-  name 'Macgo Mac Blu-ray Player Pro'
-  homepage 'https://www.macblurayplayer.com/'
+  appcast "https://cdn.macblurayplayer.com/mac-bluray-player-pro3/appcast/Appcast.xml"
+  name "Macgo Mac Blu-ray Player Pro"
+  homepage "https://www.macblurayplayer.com/"
 
-  app 'Blu-ray Player Pro.app'
+  app "Blu-ray Player Pro.app"
 end
