77dfdffc9e8ed0202d33fe479f3ba87e0921a187
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:18:56 +0100

    brightness-sync: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/brightness-sync.rb
+++ Casks/brightness-sync.rb
@@ -1,13 +1,13 @@
-cask 'brightness-sync' do
-  version '2.2.0'
-  sha256 '1f0fc9d3d538f8cb5f323b9c8a152533d1299a3fcda607dd6aeb48a663e1a2f2'
+cask "brightness-sync" do
+  version "2.2.0"
+  sha256 "1f0fc9d3d538f8cb5f323b9c8a152533d1299a3fcda607dd6aeb48a663e1a2f2"
 
   url "https://github.com/OCJvanDijk/Brightness-Sync/releases/download/v#{version}/Brightness.Sync.app.zip"
-  appcast 'https://github.com/OCJvanDijk/Brightness-Sync/releases.atom'
-  name 'Brightness Sync'
-  homepage 'https://github.com/OCJvanDijk/Brightness-Sync'
+  appcast "https://github.com/OCJvanDijk/Brightness-Sync/releases.atom"
+  name "Brightness Sync"
+  homepage "https://github.com/OCJvanDijk/Brightness-Sync"
 
-  depends_on macos: '>= :catalina'
+  depends_on macos: ">= :catalina"
 
-  app 'Brightness Sync.app'
+  app "Brightness Sync.app"
 end
