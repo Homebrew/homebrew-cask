61e144c957b91e5f0b30a0638d9010a386fe41a9
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:21:23 +0100

    cyberduck: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/cyberduck.rb
+++ Casks/cyberduck.rb
@@ -1,21 +1,21 @@
-cask 'cyberduck' do
-  version '7.4.1.33065'
-  sha256 '84ef2844cd84c1e0cbb163ee66b410a3d5d3dbc0d2bd5ec4d44c71bec99bf765'
+cask "cyberduck" do
+  version "7.4.1.33065"
+  sha256 "84ef2844cd84c1e0cbb163ee66b410a3d5d3dbc0d2bd5ec4d44c71bec99bf765"
 
   url "https://update.cyberduck.io/Cyberduck-#{version}.zip"
-  appcast 'https://version.cyberduck.io/changelog.rss'
-  name 'Cyberduck'
-  homepage 'https://cyberduck.io/'
+  appcast "https://version.cyberduck.io/changelog.rss"
+  name "Cyberduck"
+  homepage "https://cyberduck.io/"
 
   auto_updates true
 
-  app 'Cyberduck.app'
+  app "Cyberduck.app"
 
   zap trash: [
-               '~/Library/Application Support/Cyberduck',
-               '~/Library/Caches/ch.sudo.cyberduck',
-               '~/Library/Group Containers/G69SCX94XU.duck',
-               '~/Library/Preferences/ch.sudo.cyberduck.plist',
-               '~/Library/Saved Application State/ch.sudo.cyberduck.savedState',
-             ]
+    "~/Library/Application Support/Cyberduck",
+    "~/Library/Caches/ch.sudo.cyberduck",
+    "~/Library/Group Containers/G69SCX94XU.duck",
+    "~/Library/Preferences/ch.sudo.cyberduck.plist",
+    "~/Library/Saved Application State/ch.sudo.cyberduck.savedState",
+  ]
 end
