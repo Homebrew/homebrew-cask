9b82fed905673ca544710dbce188e9ca45f3a28e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:20:25 +0100

    clocksaver: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/clocksaver.rb
+++ Casks/clocksaver.rb
@@ -1,11 +1,11 @@
-cask 'clocksaver' do
-  version '0.7.0'
-  sha256 '9497e774695cadfb25fb6ad1b5689a110f806041b34d3b536b862d653de8bca4'
+cask "clocksaver" do
+  version "0.7.0"
+  sha256 "9497e774695cadfb25fb6ad1b5689a110f806041b34d3b536b862d653de8bca4"
 
   url "https://github.com/soffes/Clock.saver/releases/download/v#{version}/Clock.saver-#{version}.zip"
-  appcast 'https://github.com/soffes/Clock.saver/releases.atom'
-  name 'Clock.saver screensaver'
-  homepage 'https://github.com/soffes/Clock.saver'
+  appcast "https://github.com/soffes/Clock.saver/releases.atom"
+  name "Clock.saver screensaver"
+  homepage "https://github.com/soffes/Clock.saver"
 
-  screen_saver 'Clock.saver'
+  screen_saver "Clock.saver"
 end
