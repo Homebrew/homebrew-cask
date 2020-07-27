23f0515b86741f24bfa81365d7192aa771d6237f
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:20:36 +0100

    coconutbattery: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/coconutbattery.rb
+++ Casks/coconutbattery.rb
@@ -1,31 +1,31 @@
-cask 'coconutbattery' do
+cask "coconutbattery" do
   if MacOS.version <= :yosemite
-    version '3.6.4'
-    sha256 '8e289fb4a75cb117fc1d7861020c9ab2384b09dfd18f066c7fadfc9d42c3ac56'
+    version "3.6.4"
+    sha256 "8e289fb4a75cb117fc1d7861020c9ab2384b09dfd18f066c7fadfc9d42c3ac56"
     url "https://www.coconut-flavour.com/downloads/coconutBattery_#{version}.zip"
   else
-    version '3.9.0'
-    sha256 '764110af9aff6cec1caa485aae0cb7b180dcf484213db82cbf1b15dfacf37ac5'
+    version "3.9.0"
+    sha256 "764110af9aff6cec1caa485aae0cb7b180dcf484213db82cbf1b15dfacf37ac5"
     url "https://www.coconut-flavour.com/downloads/coconutBattery_#{version.no_dots}.zip"
-    appcast 'https://coconut-flavour.com/updates/coconutBattery.xml'
+    appcast "https://coconut-flavour.com/updates/coconutBattery.xml"
   end
 
-  name 'coconutBattery'
-  homepage 'https://www.coconut-flavour.com/coconutbattery/'
+  name "coconutBattery"
+  homepage "https://www.coconut-flavour.com/coconutbattery/"
 
   auto_updates true
 
-  app 'coconutBattery.app'
+  app "coconutBattery.app"
 
-  uninstall launchctl: 'com.coconut-flavour.coconutBattery-Menu',
-            quit:      'com.coconut-flavour.coconutBattery-Menu'
+  uninstall launchctl: "com.coconut-flavour.coconutBattery-Menu",
+            quit:      "com.coconut-flavour.coconutBattery-Menu"
 
   zap trash: [
-               '~/Library/Application Support/coconutBattery',
-               '~/Library/Caches/com.coconut-flavour.coconutBattery*',
-               '~/Library/Group Containers/*.coconut-flavour.coconutBattery',
-               '~/Library/Preferences/com.coconut-flavour.coconutBattery.plist',
-               '~/Library/Preferences/com.coconut-flavour.coconutBattery-Menu.plist',
-               '~/Library/Saved Application State/com.coconut-flavour.coconutBattery.savedState',
-             ]
+    "~/Library/Application Support/coconutBattery",
+    "~/Library/Caches/com.coconut-flavour.coconutBattery*",
+    "~/Library/Group Containers/*.coconut-flavour.coconutBattery",
+    "~/Library/Preferences/com.coconut-flavour.coconutBattery.plist",
+    "~/Library/Preferences/com.coconut-flavour.coconutBattery-Menu.plist",
+    "~/Library/Saved Application State/com.coconut-flavour.coconutBattery.savedState",
+  ]
 end
