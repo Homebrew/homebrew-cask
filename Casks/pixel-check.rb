d476ff13193b8bbc8c8d76d5b36ec612e8ccf7b0
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:25 +0100

    pixel-check: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/pixel-check.rb
+++ Casks/pixel-check.rb
@@ -1,11 +1,11 @@
-cask 'pixel-check' do
-  version '1.3'
-  sha256 '99da44c4538463c6476be2f7d6584a4e5a9bfa175895d72e5baf1f3271316b02'
+cask "pixel-check" do
+  version "1.3"
+  sha256 "99da44c4538463c6476be2f7d6584a4e5a9bfa175895d72e5baf1f3271316b02"
 
   url "http://macguitar.me/apps/pixelcheck/PXC#{version}.zip"
-  appcast 'http://macguitar.me/apps/pixelcheck/'
-  name 'PiXel Check'
-  homepage 'http://macguitar.me/apps/pixelcheck/'
+  appcast "http://macguitar.me/apps/pixelcheck/"
+  name "PiXel Check"
+  homepage "http://macguitar.me/apps/pixelcheck/"
 
   app "PiXel Check #{version}/PiXel Check.app"
 end
