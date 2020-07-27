e4063fc0f4117c4c3e36312b3481d3d8fca898ff
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:49 +0100

    receipts: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/receipts.rb
+++ Casks/receipts.rb
@@ -1,13 +1,13 @@
-cask 'receipts' do
-  version '1.9.8-320'
-  sha256 'afb0342e99667f438691d6ee3e54600c837090246ebe8ca4c27be953613cb3ac'
+cask "receipts" do
+  version "1.9.8-320"
+  sha256 "afb0342e99667f438691d6ee3e54600c837090246ebe8ca4c27be953613cb3ac"
 
   url "https://www.receipts-app.com/update/download/Receipts-#{version}.zip"
-  appcast 'https://www.receipts-app.com/updater.php'
-  name 'Receipts'
-  homepage 'https://www.receipts-app.com/'
+  appcast "https://www.receipts-app.com/updater.php"
+  name "Receipts"
+  homepage "https://www.receipts-app.com/"
 
-  depends_on macos: '>= :yosemite'
+  depends_on macos: ">= :yosemite"
 
-  app 'Receipts.app'
+  app "Receipts.app"
 end
