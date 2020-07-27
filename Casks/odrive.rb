3114025348679cec8010efa8ff0e82b178953a75
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:32:12 +0100

    odrive: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/odrive.rb
+++ Casks/odrive.rb
@@ -1,16 +1,16 @@
-cask 'odrive' do
-  version '6733'
-  sha256 '6b0525ec9754e67fd89555574e71740c9d1fea33869298f2c7bfb5a508e0481f'
+cask "odrive" do
+  version "6733"
+  sha256 "6b0525ec9754e67fd89555574e71740c9d1fea33869298f2c7bfb5a508e0481f"
 
   # downloads can be found at https://www.odrive.com/downloaddesktop
   # d3huse1s6vwzq6.cloudfront.net/ was verified as official when first introduced to the cask
   url "https://d3huse1s6vwzq6.cloudfront.net/odrivesync.#{version}.pkg"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.odrive.com/downloaddesktop?platform=mac'
-  name 'odrive'
-  homepage 'https://www.odrive.com/'
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.odrive.com/downloaddesktop?platform=mac"
+  name "odrive"
+  homepage "https://www.odrive.com/"
 
   pkg "odrivesync.#{version}.pkg"
 
-  uninstall quit:    'com.oxygencloud.odrive',
-            pkgutil: 'com.oxygen.odrive.*'
+  uninstall quit:    "com.oxygencloud.odrive",
+            pkgutil: "com.oxygen.odrive.*"
 end
