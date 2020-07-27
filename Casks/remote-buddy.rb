d580facce9644689b6f8c873af07f54c1450119a
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:54 +0100

    remote-buddy: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/remote-buddy.rb
+++ Casks/remote-buddy.rb
@@ -1,10 +1,10 @@
-cask 'remote-buddy' do
+cask "remote-buddy" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.iospirit.com/static/objectfiles/file/101/RemoteBuddy.dmg'
-  name 'Remote Buddy'
-  homepage 'https://www.iospirit.com/products/remotebuddy/'
+  url "https://www.iospirit.com/static/objectfiles/file/101/RemoteBuddy.dmg"
+  name "Remote Buddy"
+  homepage "https://www.iospirit.com/products/remotebuddy/"
 
-  app 'Remote Buddy.app'
+  app "Remote Buddy.app"
 end
