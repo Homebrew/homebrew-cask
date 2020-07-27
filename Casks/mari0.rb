d882c89293bd5d6bb308e0f12fd11d93cd771ac6
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:53 +0100

    mari0: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/mari0.rb
+++ Casks/mari0.rb
@@ -1,10 +1,10 @@
-cask 'mari0' do
-  version '1.6'
-  sha256 'da280f99c744cb161ca4a731a5442a72116f92a4170bf94fd0c9d52557e38727'
+cask "mari0" do
+  version "1.6"
+  sha256 "da280f99c744cb161ca4a731a5442a72116f92a4170bf94fd0c9d52557e38727"
 
-  url 'https://stabyourself.net/dl.php?file=mari0-1006/mari0-osx.zip'
-  name 'Mari0'
-  homepage 'https://stabyourself.net/mari0/'
+  url "https://stabyourself.net/dl.php?file=mari0-1006/mari0-osx.zip"
+  name "Mari0"
+  homepage "https://stabyourself.net/mari0/"
 
-  app 'Mari0.app'
+  app "Mari0.app"
 end
