c6d45086bfc630e0e2df7d5e462968c5cffb40b5
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:23:38 +0100

    expressvpn: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/expressvpn.rb
+++ Casks/expressvpn.rb
@@ -1,19 +1,19 @@
-cask 'expressvpn' do
-  version '7.9.4.10'
-  sha256 '2ba96c120d851ba026848efa30a57f43a4f362c56ac6bc5d283b2a11fafe0c78'
+cask "expressvpn" do
+  version "7.9.4.10"
+  sha256 "2ba96c120d851ba026848efa30a57f43a4f362c56ac6bc5d283b2a11fafe0c78"
 
   url "https://download.expressvpn.xyz/clients/mac/expressvpn_mac_#{version}_release.pkg"
-  appcast 'https://www.expressvpn.xyz/vpn-software/vpn-mac'
-  name 'ExpressVPN'
-  homepage 'https://www.expressvpn.xyz/vpn-software/vpn-mac/'
+  appcast "https://www.expressvpn.xyz/vpn-software/vpn-mac"
+  name "ExpressVPN"
+  homepage "https://www.expressvpn.xyz/vpn-software/vpn-mac/"
 
   auto_updates true
 
   pkg "expressvpn_mac_#{version}_release.pkg"
 
   uninstall script:  {
-                       executable: "#{appdir}/ExpressVPN.app/Contents/Resources/uninstall.tool",
-                       input:      ['Yes'],
-                     },
-            pkgutil: 'com.expressvpn.ExpressVPN'
+    executable: "#{appdir}/ExpressVPN.app/Contents/Resources/uninstall.tool",
+    input:      ["Yes"],
+  },
+            pkgutil: "com.expressvpn.ExpressVPN"
 end
