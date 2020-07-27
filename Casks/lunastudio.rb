f19f77988b399309e7c6c2eba39d1d9dceaf7105
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:19 +0100

    lunastudio: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/lunastudio.rb
+++ Casks/lunastudio.rb
@@ -1,14 +1,14 @@
-cask 'lunastudio' do
+cask "lunastudio" do
   version :latest
   sha256 :no_check
 
-  url 'https://packages.luna-lang.org/darwin/lunaInstaller.zip'
-  name 'Luna Studio'
-  homepage 'https://www.luna-lang.org/'
+  url "https://packages.luna-lang.org/darwin/lunaInstaller.zip"
+  name "Luna Studio"
+  homepage "https://www.luna-lang.org/"
 
-  installer manual: 'lunaInstaller.app'
+  installer manual: "lunaInstaller.app"
 
-  uninstall delete: '/Applications/LunaStudio.app'
+  uninstall delete: "/Applications/LunaStudio.app"
 
-  zap trash: '~/.luna'
+  zap trash: "~/.luna"
 end
