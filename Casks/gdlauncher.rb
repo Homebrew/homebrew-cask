88f45a3b06317195a7f9cfa00e4ad8609956b226
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:24:59 +0100

    gdlauncher: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/gdlauncher.rb
+++ Casks/gdlauncher.rb
@@ -1,12 +1,12 @@
-cask 'gdlauncher' do
-  version '1.0.8'
-  sha256 'ffd7dd2445e72a0cfa34c9cd22d2490808f56942f173cb2153c1c9549c48e1c1'
+cask "gdlauncher" do
+  version "1.0.8"
+  sha256 "ffd7dd2445e72a0cfa34c9cd22d2490808f56942f173cb2153c1c9549c48e1c1"
 
   # github.com/gorilla-devs/GDLauncher/ was verified as official when first introduced to the cask
   url "https://github.com/gorilla-devs/GDLauncher/releases/download/v#{version}/GDLauncher-mac-setup.dmg"
-  appcast 'https://github.com/gorilla-devs/GDLauncher/releases.atom'
-  name 'GDLauncher'
-  homepage 'https://gdevs.io/'
+  appcast "https://github.com/gorilla-devs/GDLauncher/releases.atom"
+  name "GDLauncher"
+  homepage "https://gdevs.io/"
 
-  app 'GDLauncher.app'
+  app "GDLauncher.app"
 end
