6003f1674ee87aee3da66eafaaffeac04dbe9e54
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:14 +0100

    rss: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/rss.rb
+++ Casks/rss.rb
@@ -1,10 +1,10 @@
-cask 'rss' do
+cask "rss" do
   version :latest
   sha256 :no_check
 
-  url 'http://www.rssapplication.com/RSS.zip'
-  name 'RSS'
-  homepage 'http://www.rssapplication.com/'
+  url "http://www.rssapplication.com/RSS.zip"
+  name "RSS"
+  homepage "http://www.rssapplication.com/"
 
-  app 'RSS.app'
+  app "RSS.app"
 end
