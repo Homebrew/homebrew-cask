022b3bff316173974bc1331e7e1afa74e80fb66b
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:27:10 +0100

    ioquake3: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/ioquake3.rb
+++ Casks/ioquake3.rb
@@ -1,13 +1,13 @@
-cask 'ioquake3' do
+cask "ioquake3" do
   # note: "3" is not a version number, but an intrinsic part of the product name
-  version '1.36'
-  sha256 'ff310471aa641cc27980055691b3e3cf5496ac262f10967c9d5052fd3815a3fc'
+  version "1.36"
+  sha256 "ff310471aa641cc27980055691b3e3cf5496ac262f10967c9d5052fd3815a3fc"
 
   url "https://ioquake3.org/files/#{version}/ioquake3%20#{version}.dmg"
-  name 'ioquake3'
-  homepage 'https://ioquake3.org/'
+  name "ioquake3"
+  homepage "https://ioquake3.org/"
 
-  suite 'ioquake3'
+  suite "ioquake3"
 
   caveats <<~EOS
     To complete the installation of #{token}, you will have to copy the file 'pak0.pk3' from your Quake 3 Arena installation support directory into
