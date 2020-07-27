0b0a6231ff2964f32fd2e6f5cb369426864096fd
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:26 +0100

    macdive: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/macdive.rb
+++ Casks/macdive.rb
@@ -1,11 +1,11 @@
-cask 'macdive' do
-  version '2.11.4'
-  sha256 'acbd7c7f7e4fe81d47526a4acb6cd2cb9f0062e836c0f3340092dd7cccaf1d58'
+cask "macdive" do
+  version "2.11.4"
+  sha256 "acbd7c7f7e4fe81d47526a4acb6cd2cb9f0062e836c0f3340092dd7cccaf1d58"
 
   url "https://www.mac-dive.com/downloads/MacDive_#{version}.dmg"
-  appcast 'https://www.mac-dive.com/shimmer/?appcast&appName=MacDive'
-  name 'MacDive'
-  homepage 'https://www.mac-dive.com/'
+  appcast "https://www.mac-dive.com/shimmer/?appcast&appName=MacDive"
+  name "MacDive"
+  homepage "https://www.mac-dive.com/"
 
-  app 'MacDive.app'
+  app "MacDive.app"
 end
