7c36335b5646096ea23798e266ba9128bafc1962
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:13 +0100

    qcad: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/qcad.rb
+++ Casks/qcad.rb
@@ -1,17 +1,17 @@
-cask 'qcad' do
-  version '3.24.3'
+cask "qcad" do
+  version "3.24.3"
 
   if MacOS.version <= :high_sierra
-    sha256 '2313e906d1892ad7a433ff23074c9f28ab747e7731231c0c8a02ff6eab5ae44a'
+    sha256 "2313e906d1892ad7a433ff23074c9f28ab747e7731231c0c8a02ff6eab5ae44a"
     url "https://www.qcad.org/archives/qcad/qcad-#{version}-trial-macos-10.10-10.13.dmg"
   else
-    sha256 '9c03efd3d5c41952637cd6bde7b090466ef4f8d87229f509194268f3cadff4ef'
+    sha256 "9c03efd3d5c41952637cd6bde7b090466ef4f8d87229f509194268f3cadff4ef"
     url "https://www.qcad.org/archives/qcad/qcad-#{version}-trial-macos-10.14-10.15.dmg"
   end
 
-  appcast 'https://www.qcad.org/en/download'
-  name 'QCAD'
-  homepage 'https://www.qcad.org/'
+  appcast "https://www.qcad.org/en/download"
+  name "QCAD"
+  homepage "https://www.qcad.org/"
 
-  app 'QCAD.app'
+  app "QCAD.app"
 end
