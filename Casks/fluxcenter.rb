643a128341cfb1d92881b2dfbc1842d6c294fb2e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:24:20 +0100

    fluxcenter: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/fluxcenter.rb
+++ Casks/fluxcenter.rb
@@ -1,11 +1,11 @@
-cask 'fluxcenter' do
-  version '1.2.16.47921'
-  sha256 'b5114d716950881732878d8a8cc1c027e47b57ea90d579afea6637ac89b87d14'
+cask "fluxcenter" do
+  version "1.2.16.47921"
+  sha256 "b5114d716950881732878d8a8cc1c027e47b57ea90d579afea6637ac89b87d14"
 
   url "https://www.flux.audio/wp-content/uploads/files/Center/MacOS/Flux_FluxCenter_MacOS_Installer_(#{version}).dmg"
-  appcast 'https://www.flux.audio/wp-content/uploads/files/Center/changeLog.txt'
-  name 'FluxCenter'
-  homepage 'https://www.flux.audio/'
+  appcast "https://www.flux.audio/wp-content/uploads/files/Center/changeLog.txt"
+  name "FluxCenter"
+  homepage "https://www.flux.audio/"
 
-  app 'FluxCenter.app'
+  app "FluxCenter.app"
 end
