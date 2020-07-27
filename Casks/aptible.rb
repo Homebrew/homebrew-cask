1225b6158bf81aff50ce0b1b5bd38c51c3c51272
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:16:25 +0100

    aptible: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/aptible.rb
+++ Casks/aptible.rb
@@ -1,13 +1,13 @@
-cask 'aptible' do
-  version '0.16.5,20200508143959,206'
-  sha256 '7d3fc6d0a56ae25dd913e72930ac6c8ada6a5629e03771be6761b2fa03f60103'
+cask "aptible" do
+  version "0.16.5,20200508143959,206"
+  sha256 "7d3fc6d0a56ae25dd913e72930ac6c8ada6a5629e03771be6761b2fa03f60103"
 
   # omnibus-aptible-toolbelt.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://omnibus-aptible-toolbelt.s3.amazonaws.com/aptible/omnibus-aptible-toolbelt/master/#{version.after_comma.after_comma}/pkg/aptible-toolbelt-#{version.before_comma}%2B#{version.after_comma.before_comma}-mac-os-x.10.11.6-1.pkg"
-  name 'Aptible Toolbelt'
-  homepage 'https://www.aptible.com/documentation/deploy/cli.html'
+  name "Aptible Toolbelt"
+  homepage "https://www.aptible.com/documentation/deploy/cli.html"
 
   pkg "aptible-toolbelt-#{version.before_comma}+#{version.after_comma.before_comma}-mac-os-x.10.11.6-1.pkg"
 
-  uninstall pkgutil: 'com.aptible.toolbelt'
+  uninstall pkgutil: "com.aptible.toolbelt"
 end
