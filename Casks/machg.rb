7f7a4d349e00ad07bb11a0b84030f85366fccd7d
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:30 +0100

    machg: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/machg.rb
+++ Casks/machg.rb
@@ -1,11 +1,11 @@
-cask 'machg' do
-  version '1.0.2'
-  sha256 'af15173111c1d350ba5b62edf6fe5ab3a186cc8bdb6302455c4fb04f2a809305'
+cask "machg" do
+  version "1.0.2"
+  sha256 "af15173111c1d350ba5b62edf6fe5ab3a186cc8bdb6302455c4fb04f2a809305"
 
   url "http://jasonfharris.com/machg/downloads/assets/MacHg#{version}.zip"
-  appcast 'http://jasonfharris.com/machg/machg_appcast.xml'
-  name 'MacHg'
-  homepage 'http://jasonfharris.com/machg/'
+  appcast "http://jasonfharris.com/machg/machg_appcast.xml"
+  name "MacHg"
+  homepage "http://jasonfharris.com/machg/"
 
-  app 'MacHg.app'
+  app "MacHg.app"
 end
