0756fb766f752cfff7443f0924fbdaf90ae36564
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:38:59 +0100

    yakyak: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/yakyak.rb
+++ Casks/yakyak.rb
@@ -1,11 +1,11 @@
-cask 'yakyak' do
-  version '1.5.9'
-  sha256 'd671fda8da58ef414fee9728f438957f07a0720274c38f56f5d4985a8f441968'
+cask "yakyak" do
+  version "1.5.9"
+  sha256 "d671fda8da58ef414fee9728f438957f07a0720274c38f56f5d4985a8f441968"
 
   url "https://github.com/yakyak/yakyak/releases/download/v#{version}/yakyak-#{version}-osx.zip"
-  appcast 'https://github.com/yakyak/yakyak/releases.atom'
-  name 'Yakyak'
-  homepage 'https://github.com/yakyak/yakyak'
+  appcast "https://github.com/yakyak/yakyak/releases.atom"
+  name "Yakyak"
+  homepage "https://github.com/yakyak/yakyak"
 
-  app 'YakYak-darwin-x64/YakYak.app'
+  app "YakYak-darwin-x64/YakYak.app"
 end
