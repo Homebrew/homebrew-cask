2adb446ae97cf76f4be69aa81c3167d1cb804998
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:27:23 +0100

    isteg: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/isteg.rb
+++ Casks/isteg.rb
@@ -1,11 +1,11 @@
-cask 'isteg' do
-  version '1.6.2'
-  sha256 '5f385efce3416c9df75c6d97a35de82b76d9c829c2956461dd2fc95ca843702d'
+cask "isteg" do
+  version "1.6.2"
+  sha256 "5f385efce3416c9df75c6d97a35de82b76d9c829c2956461dd2fc95ca843702d"
 
   url "http://www.hanynet.com/isteg-#{version}.zip"
-  appcast 'http://www.hanynet.com/isteg/'
-  name 'iSteg'
-  homepage 'http://www.hanynet.com/isteg/'
+  appcast "http://www.hanynet.com/isteg/"
+  name "iSteg"
+  homepage "http://www.hanynet.com/isteg/"
 
-  app 'iSteg.app'
+  app "iSteg.app"
 end
