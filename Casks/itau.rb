705e63a5d71f51c20ea549d503e42ecb07472560
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:27:25 +0100

    itau: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/itau.rb
+++ Casks/itau.rb
@@ -1,12 +1,12 @@
-cask 'itau' do
+cask "itau" do
   version :latest
   sha256 :no_check
 
-  url 'https://guardiao.itau.com.br/UpdateServer/aplicativoitau.dmg'
-  name 'Itau'
-  homepage 'https://www.itau.com.br/computador/'
+  url "https://guardiao.itau.com.br/UpdateServer/aplicativoitau.dmg"
+  name "Itau"
+  homepage "https://www.itau.com.br/computador/"
 
-  app 'Itau.app'
+  app "Itau.app"
 
-  zap trash: '~/.aplicativoitau'
+  zap trash: "~/.aplicativoitau"
 end
