85253e5bb8349ba5ae2be2429dba58ba0a61f145
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:15:58 +0100

    angry-ip-scanner: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/angry-ip-scanner.rb
+++ Casks/angry-ip-scanner.rb
@@ -1,12 +1,12 @@
-cask 'angry-ip-scanner' do
-  version '3.7.2'
-  sha256 '229e1c2dcb1fcccacd2816c7a0e1ad43733f7a09cf76df4ecd53ccdafee8bdda'
+cask "angry-ip-scanner" do
+  version "3.7.2"
+  sha256 "229e1c2dcb1fcccacd2816c7a0e1ad43733f7a09cf76df4ecd53ccdafee8bdda"
 
   # github.com/angryip/ipscan/ was verified as official when first introduced to the cask
   url "https://github.com/angryip/ipscan/releases/download/#{version}/ipscan-mac-#{version}.zip"
-  appcast 'https://github.com/angryip/ipscan/releases.atom'
-  name 'Angry IP Scanner'
-  homepage 'https://angryip.org/'
+  appcast "https://github.com/angryip/ipscan/releases.atom"
+  name "Angry IP Scanner"
+  homepage "https://angryip.org/"
 
-  app 'Angry IP Scanner.app'
+  app "Angry IP Scanner.app"
 end
