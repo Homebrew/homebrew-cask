50b38e24efdfdf8b1a75d5d3e50e77fd43405dc4
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:20 +0100

    lyn: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/lyn.rb
+++ Casks/lyn.rb
@@ -1,17 +1,17 @@
-cask 'lyn' do
-  version '1.13'
-  sha256 '12178ed28ae46985c1c4cffd541a704ad4a3727ed450cf14b80fae755048a7bc'
+cask "lyn" do
+  version "1.13"
+  sha256 "12178ed28ae46985c1c4cffd541a704ad4a3727ed450cf14b80fae755048a7bc"
 
   url "https://www.lynapp.com/downloads/Lyn-#{version}.dmg"
-  appcast 'https://www.lynapp.com/lyn/update.xml'
-  name 'Lyn'
-  homepage 'https://www.lynapp.com/'
+  appcast "https://www.lynapp.com/lyn/update.xml"
+  name "Lyn"
+  homepage "https://www.lynapp.com/"
 
-  app 'Lyn.app'
+  app "Lyn.app"
 
   zap trash: [
-               '~/Library/Application Support/Lyn',
-               '~/Library/Caches/com.lynapp.lyn',
-               '~/Library/Preferences/com.lynapp.lyn.plist',
-             ]
+    "~/Library/Application Support/Lyn",
+    "~/Library/Caches/com.lynapp.lyn",
+    "~/Library/Preferences/com.lynapp.lyn.plist",
+  ]
 end
