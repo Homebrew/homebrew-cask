ad57eb978cd7c4ae4885628d9766e055e69785fc
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:19:01 +0100

    brooklyn: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/brooklyn.rb
+++ Casks/brooklyn.rb
@@ -1,11 +1,11 @@
-cask 'brooklyn' do
-  version '2.0.1'
-  sha256 '6ecde466f8f88c2649dbf3e38c98fd7abb95a7d1828ac4cc8d499f2c7a0cb9ec'
+cask "brooklyn" do
+  version "2.0.1"
+  sha256 "6ecde466f8f88c2649dbf3e38c98fd7abb95a7d1828ac4cc8d499f2c7a0cb9ec"
 
   url "https://github.com/pedrommcarrasco/Brooklyn/releases/download/#{version}/Brooklyn.saver.zip"
-  appcast 'https://github.com/pedrommcarrasco/Brooklyn/releases.atom'
-  name 'Brooklyn'
-  homepage 'https://github.com/pedrommcarrasco/Brooklyn'
+  appcast "https://github.com/pedrommcarrasco/Brooklyn/releases.atom"
+  name "Brooklyn"
+  homepage "https://github.com/pedrommcarrasco/Brooklyn"
 
-  screen_saver 'Brooklyn.saver'
+  screen_saver "Brooklyn.saver"
 end
