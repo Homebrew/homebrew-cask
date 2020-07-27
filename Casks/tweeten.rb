77c9fd620ca9520262dd2eb605b133a6278ca44c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:37:56 +0100

    tweeten: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/tweeten.rb
+++ Casks/tweeten.rb
@@ -1,12 +1,12 @@
-cask 'tweeten' do
-  version '5.0.5'
-  sha256 '1af9ccf4d566ab1c47f58203798839de94c669d72c951eb9fd8443d726387284'
+cask "tweeten" do
+  version "5.0.5"
+  sha256 "1af9ccf4d566ab1c47f58203798839de94c669d72c951eb9fd8443d726387284"
 
   # github.com/MehediH/Tweeten/ was verified as official when first introduced to the cask
   url "https://github.com/MehediH/Tweeten/releases/download/v#{version}/tweeten-darwin-x64.zip"
-  appcast 'https://github.com/MehediH/Tweeten/releases.atom'
-  name 'Tweeten'
-  homepage 'https://tweetenapp.com/'
+  appcast "https://github.com/MehediH/Tweeten/releases.atom"
+  name "Tweeten"
+  homepage "https://tweetenapp.com/"
 
-  app 'Tweeten.app'
+  app "Tweeten.app"
 end
