e31ed2d7c01b67d04c5197c042b1627df901a21c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:22:26 +0100

    doxie: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/doxie.rb
+++ Casks/doxie.rb
@@ -1,11 +1,11 @@
-cask 'doxie' do
-  version '2.12.2'
-  sha256 '6f48066eca465fc17838ed9552eb5421472dcdc40757ee484014d6d9e256446b'
+cask "doxie" do
+  version "2.12.2"
+  sha256 "6f48066eca465fc17838ed9552eb5421472dcdc40757ee484014d6d9e256446b"
 
   url "https://download.getdoxie.com/Doxie-#{version}.dmg"
-  appcast 'https://r.getdoxie.com/app/sparkle/doxie-macos.xml'
-  name 'Doxie'
-  homepage 'https://www.getdoxie.com/'
+  appcast "https://r.getdoxie.com/app/sparkle/doxie-macos.xml"
+  name "Doxie"
+  homepage "https://www.getdoxie.com/"
 
-  app 'Doxie.app'
+  app "Doxie.app"
 end
