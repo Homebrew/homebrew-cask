123d96a3775a0d1efec0e6c94b8695c2a6d287ec
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:26:27 +0100

    hstracker: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/hstracker.rb
+++ Casks/hstracker.rb
@@ -1,12 +1,12 @@
-cask 'hstracker' do
-  version '1.6.36'
-  sha256 '61da9895e8c319a780648e0b73db881ff539feb81ca673e6145560742bdf14d3'
+cask "hstracker" do
+  version "1.6.36"
+  sha256 "61da9895e8c319a780648e0b73db881ff539feb81ca673e6145560742bdf14d3"
 
   # github.com/HearthSim/HSTracker/ was verified as official when first introduced to the cask
   url "https://github.com/HearthSim/HSTracker/releases/download/#{version}/HSTracker.app.zip"
-  appcast 'https://github.com/HearthSim/HSTracker/releases.atom'
-  name 'Hearthstone Deck Tracker'
-  homepage 'https://hsdecktracker.net/'
+  appcast "https://github.com/HearthSim/HSTracker/releases.atom"
+  name "Hearthstone Deck Tracker"
+  homepage "https://hsdecktracker.net/"
 
-  app 'HSTracker.app'
+  app "HSTracker.app"
 end
