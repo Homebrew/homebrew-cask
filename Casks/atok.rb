980cd52d4d2f0580c80fe72d6361043441b03336
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:16:48 +0100

    atok: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/atok.rb
+++ Casks/atok.rb
@@ -1,29 +1,29 @@
-cask 'atok' do
-  version '2017,31'
-  sha256 '9d460310b5d44de9585f5429c17006684f5412ba62ac1f4ab47af6e523bbdb80'
+cask "atok" do
+  version "2017,31"
+  sha256 "9d460310b5d44de9585f5429c17006684f5412ba62ac1f4ab47af6e523bbdb80"
 
   url "https://gate.justsystems.com/download/atok/ut/mac/at#{version.after_comma}try.dmg"
-  name 'ATOK'
-  homepage 'https://www.justsystems.com/jp/products/atokmac/'
+  name "ATOK"
+  homepage "https://www.justsystems.com/jp/products/atokmac/"
 
-  pkg 'ATOK インストーラ.pkg'
+  pkg "ATOK インストーラ.pkg"
 
   uninstall pkgutil:   [
-                         "com.justsystems.atok#{version.after_comma}.doc.pkg",
-                         "com.justsystems.atok#{version.after_comma}.pkg",
-                         "com.justsystems.atok#{version.after_comma}.quicklook.pkg",
-                         "com.justsystems.atok#{version.after_comma}.sync.pkg",
-                         'com.justsystems.JustOnlineUpdate.pkg',
-                         'com.justsystems.pkg.lma',
-                       ],
+    "com.justsystems.atok#{version.after_comma}.doc.pkg",
+    "com.justsystems.atok#{version.after_comma}.pkg",
+    "com.justsystems.atok#{version.after_comma}.quicklook.pkg",
+    "com.justsystems.atok#{version.after_comma}.sync.pkg",
+    "com.justsystems.JustOnlineUpdate.pkg",
+    "com.justsystems.pkg.lma",
+  ],
             launchctl: [
-                         "com.justsystems.inputmethod.atok#{version.after_comma}",
-                         'com.justsystems.launchd.jslmaUI',
-                         'com.justsystems.launchd.UpdateChecker',
-                         "com.justsystems.launchd.Atok#{version.after_comma}.AlBg",
-                         "com.justsystems.atok#{version.after_comma}.enabler",
-                         'com.justsystems.OnlineUpdate',
-                         'com.justsystems.launchd.jslmad',
-                       ],
-            quit:      'com.justsystems.UpdateChecker'
+              "com.justsystems.inputmethod.atok#{version.after_comma}",
+              "com.justsystems.launchd.jslmaUI",
+              "com.justsystems.launchd.UpdateChecker",
+              "com.justsystems.launchd.Atok#{version.after_comma}.AlBg",
+              "com.justsystems.atok#{version.after_comma}.enabler",
+              "com.justsystems.OnlineUpdate",
+              "com.justsystems.launchd.jslmad",
+            ],
+            quit:      "com.justsystems.UpdateChecker"
 end
