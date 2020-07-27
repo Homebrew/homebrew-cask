d40a88e22e6035de73f9f79cd4a31b0b1694fdc6
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:27:01 +0100

    install-disk-creator: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/install-disk-creator.rb
+++ Casks/install-disk-creator.rb
@@ -1,12 +1,12 @@
-cask 'install-disk-creator' do
+cask "install-disk-creator" do
   version :latest
   sha256 :no_check
 
-  url 'https://macdaddy.io/InstallDiskCreator.zip'
-  name 'Install Disk Creator'
-  homepage 'https://macdaddy.io/install-disk-creator/'
+  url "https://macdaddy.io/InstallDiskCreator.zip"
+  name "Install Disk Creator"
+  homepage "https://macdaddy.io/install-disk-creator/"
 
-  app 'Install Disk Creator.app'
+  app "Install Disk Creator.app"
 
-  zap trash: '~/Library/Saved Application State/io.macdaddy.Install-Disk-Creator.savedState'
+  zap trash: "~/Library/Saved Application State/io.macdaddy.Install-Disk-Creator.savedState"
 end
