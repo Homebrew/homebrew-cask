c216d03f62ee30c50fe1b4157524fe18265455fc
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:32:59 +0100

    parsehub: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/parsehub.rb
+++ Casks/parsehub.rb
@@ -1,10 +1,10 @@
-cask 'parsehub' do
+cask "parsehub" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.parsehub.com/static/client/parsehub.dmg'
-  name 'ParseHub'
-  homepage 'https://www.parsehub.com/'
+  url "https://www.parsehub.com/static/client/parsehub.dmg"
+  name "ParseHub"
+  homepage "https://www.parsehub.com/"
 
-  app 'ParseHub.app'
+  app "ParseHub.app"
 end
