787236079af9090140488e0a89706cc6f233291c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:23:40 +0100

    fake: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/fake.rb
+++ Casks/fake.rb
@@ -1,11 +1,11 @@
-cask 'fake' do
-  version '1.9.1'
-  sha256 'eb4f02a3b5dea2cab679c1366f454a22d8bb4bbfa448c56c0ca00bb615d8867e'
+cask "fake" do
+  version "1.9.1"
+  sha256 "eb4f02a3b5dea2cab679c1366f454a22d8bb4bbfa448c56c0ca00bb615d8867e"
 
   url "http://fakeapp.com/dist/Fake_#{version}.zip"
-  appcast 'http://fakeapp.com/appcast/fake.rss'
-  name 'Fake'
-  homepage 'http://fakeapp.com/'
+  appcast "http://fakeapp.com/appcast/fake.rss"
+  name "Fake"
+  homepage "http://fakeapp.com/"
 
-  app 'Fake.app'
+  app "Fake.app"
 end
