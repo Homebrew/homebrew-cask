0576ac84ff0274944b150524f9dda13695f2c8b7
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:38:04 +0100

    unified-remote: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/unified-remote.rb
+++ Casks/unified-remote.rb
@@ -1,10 +1,10 @@
-cask 'unified-remote' do
+cask "unified-remote" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.unifiedremote.com/download/mac'
-  name 'Unified Remote'
-  homepage 'https://www.unifiedremote.com/'
+  url "https://www.unifiedremote.com/download/mac"
+  name "Unified Remote"
+  homepage "https://www.unifiedremote.com/"
 
-  app 'Unified Remote.app'
+  app "Unified Remote.app"
 end
