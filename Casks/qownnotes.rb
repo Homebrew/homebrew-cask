87b9ac4d2f04ee434ced26c8f64caa6884756238
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:22 +0100

    qownnotes: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,15 +1,15 @@
-cask 'qownnotes' do
-  version '20.7.8'
-  sha256 '242803ac1cb996e54c9ba3177506fee1e5b286f04417346c3bf15e091b6a13a9'
+cask "qownnotes" do
+  version "20.7.8"
+  sha256 "242803ac1cb996e54c9ba3177506fee1e5b286f04417346c3bf15e091b6a13a9"
 
   # github.com/pbek/QOwnNotes/ was verified as official when first introduced to the cask
   url "https://github.com/pbek/QOwnNotes/releases/download/v#{version}/QOwnNotes.dmg"
-  appcast 'https://www.qownnotes.org/api/v1/last_release/QOwnNotes/macosx.json'
-  name 'QOwnNotes'
-  homepage 'https://www.qownnotes.org/'
+  appcast "https://www.qownnotes.org/api/v1/last_release/QOwnNotes/macosx.json"
+  name "QOwnNotes"
+  homepage "https://www.qownnotes.org/"
 
   auto_updates true
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'QOwnNotes.app'
+  app "QOwnNotes.app"
 end
