404aec616484bcd1a8f47c0b9a1be5dc07264da1
CoreCode <core-code@users.noreply.github.com>
2020-07-27 19:02:23 +0200

    Update maintenance.rb

--- Casks/maintenance.rb
+++ Casks/maintenance.rb
@@ -1,18 +1,17 @@
 cask "maintenance" do
-  case MacOS.version
-  when :yosemite
+  if MacOS.version <= :yosemite
     version "2.0.7"
     sha256 "5c926159c0610fe705b0f23b56672f1c5e46c970f92013839c772147379520f9"
-  when :el_capitan
+  elsif MacOS.version <= :el_capitan
     version "2.1.8"
     sha256 "f27f5d0736e80cd80c85dcc5390dfeb893183424fe65b32b08e280c90b22b24c"
-  when :sierra
+  elsif MacOS.version <= :sierra
     version "2.3.0"
     sha256 "8fde91742126d10234451a3c973461f5d84c771e52c6ee14aff93f1d66a0dbca"
-  when :high_sierra
+  elsif MacOS.version <= :high_sierra
     version "2.4.2"
     sha256 "94c7a322d4d796afc5e52534f3564a562240d9c0ec0a60de210e68372fef2137"
-  when :mojave
+  elsif MacOS.version <= :mojave
     version "2.5.6"
     sha256 "d3b0152ce543b84ed597daba3360f74c3f20b4fb2b41d71005f3a7b311d4d681"
   else
