256b337845e27819974c633b19918a5ec83c0353
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:28:22 +0100

    kitty: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/kitty.rb
+++ Casks/kitty.rb
@@ -1,18 +1,18 @@
-cask 'kitty' do
-  version '0.18.1'
-  sha256 '3fb882c39b51768360e39e0965063ff89632969d1b1673fcd9c2833ddde03671'
+cask "kitty" do
+  version "0.18.1"
+  sha256 "3fb882c39b51768360e39e0965063ff89632969d1b1673fcd9c2833ddde03671"
 
   url "https://github.com/kovidgoyal/kitty/releases/download/v#{version}/kitty-#{version}.dmg"
-  appcast 'https://github.com/kovidgoyal/kitty/releases.atom'
-  name 'kitty'
-  homepage 'https://github.com/kovidgoyal/kitty'
+  appcast "https://github.com/kovidgoyal/kitty/releases.atom"
+  name "kitty"
+  homepage "https://github.com/kovidgoyal/kitty"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'kitty.app'
+  app "kitty.app"
   # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
   shimscript = "#{staged_path}/kitty.wrapper.sh"
-  binary shimscript, target: 'kitty'
+  binary shimscript, target: "kitty"
 
   preflight do
     IO.write shimscript, <<~EOS
@@ -22,8 +22,8 @@ cask 'kitty' do
   end
 
   zap trash: [
-               '~/.config/kitty',
-               '~/Library/Preferences/kitty',
-               '~/Library/Saved Application State/net.kovidgoyal.kitty.savedState',
-             ]
+    "~/.config/kitty",
+    "~/Library/Preferences/kitty",
+    "~/Library/Saved Application State/net.kovidgoyal.kitty.savedState",
+  ]
 end
