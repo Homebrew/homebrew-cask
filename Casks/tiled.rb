dd0328569dde6ba8b533091274ce76840c00d7d1
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:37:32 +0100

    tiled: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/tiled.rb
+++ Casks/tiled.rb
@@ -1,14 +1,14 @@
-cask 'tiled' do
-  version '1.4.1'
-  sha256 '23ab6204d0fa89a50e0591be9a9e8f54689d6941b55465f5711f123277c33f15'
+cask "tiled" do
+  version "1.4.1"
+  sha256 "23ab6204d0fa89a50e0591be9a9e8f54689d6941b55465f5711f123277c33f15"
 
   # github.com/bjorn/tiled/ was verified as official when first introduced to the cask
   url "https://github.com/bjorn/tiled/releases/download/v#{version}/Tiled-#{version}-macos.zip"
-  appcast 'https://github.com/bjorn/tiled/releases.atom'
-  name 'Tiled'
-  homepage 'https://www.mapeditor.org/'
+  appcast "https://github.com/bjorn/tiled/releases.atom"
+  name "Tiled"
+  homepage "https://www.mapeditor.org/"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'Tiled.app'
+  app "Tiled.app"
 end
