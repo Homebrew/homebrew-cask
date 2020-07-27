75efa9647ea95ece9a9ccf5acb004304662f5c9e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:32 +0100

    sqleditor: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/sqleditor.rb
+++ Casks/sqleditor.rb
@@ -1,11 +1,11 @@
-cask 'sqleditor' do
-  version '3.7.2'
-  sha256 '2e5fd0c9ccde345c50f965bea830c684471a6c79d9d9fbddd7498256499aae8c'
+cask "sqleditor" do
+  version "3.7.2"
+  sha256 "2e5fd0c9ccde345c50f965bea830c684471a6c79d9d9fbddd7498256499aae8c"
 
   url "https://www.malcolmhardie.com/sqleditor/releases/#{version}/SQLEditor-#{version.dots_to_hyphens}.zip"
-  appcast 'https://www.malcolmhardie.com/sqleditor/appcast/sq2release.xml'
-  name 'SQLEditor'
-  homepage 'https://www.malcolmhardie.com/sqleditor/'
+  appcast "https://www.malcolmhardie.com/sqleditor/appcast/sq2release.xml"
+  name "SQLEditor"
+  homepage "https://www.malcolmhardie.com/sqleditor/"
 
-  app 'SQLEditor.app'
+  app "SQLEditor.app"
 end
