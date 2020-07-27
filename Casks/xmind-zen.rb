408b883d33f63fd76100c19ba6acb38be0873855
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:38:56 +0100

    xmind-zen: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/xmind-zen.rb
+++ Casks/xmind-zen.rb
@@ -1,13 +1,13 @@
-cask 'xmind-zen' do
-  version '10.2.0-202007201811'
-  sha256 '0bcb2bd00e38633bf72936f6685c25359c595b811afeec837b4f8a922a932658'
+cask "xmind-zen" do
+  version "10.2.0-202007201811"
+  sha256 "0bcb2bd00e38633bf72936f6685c25359c595b811afeec837b4f8a922a932658"
 
   url "https://www.xmind.net/xmind/downloads/XMind-2020-for-macOS-#{version}.dmg"
-  appcast 'https://www.xmind.net/download/'
-  name 'XMind'
-  homepage 'https://www.xmind.net/zen/'
+  appcast "https://www.xmind.net/download/"
+  name "XMind"
+  homepage "https://www.xmind.net/zen/"
 
-  app 'XMind.app'
+  app "XMind.app"
 
-  zap trash: '~/Library/Application Support/XMind ZEN'
+  zap trash: "~/Library/Application Support/XMind ZEN"
 end
