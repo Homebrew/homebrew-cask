465f8886c9becc1c23dd2281aff52ea308ced0dc
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:51 +0100

    sidequest: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/sidequest.rb
+++ Casks/sidequest.rb
@@ -1,16 +1,16 @@
-cask 'sidequest' do
-  version '0.10.11'
-  sha256 '2399041835559111ad81a588f4aa8458e348d86bd1e103ebb8c6046ccf656a0e'
+cask "sidequest" do
+  version "0.10.11"
+  sha256 "2399041835559111ad81a588f4aa8458e348d86bd1e103ebb8c6046ccf656a0e"
 
   # github.com/the-expanse/SideQuest/ was verified as official when first introduced to the cask
   url "https://github.com/the-expanse/SideQuest/releases/download/v#{version}/SideQuest-#{version}.dmg"
-  appcast 'https://github.com/the-expanse/SideQuest/releases.atom'
-  name 'SideQuest'
-  homepage 'https://sidequestvr.com/'
+  appcast "https://github.com/the-expanse/SideQuest/releases.atom"
+  name "SideQuest"
+  homepage "https://sidequestvr.com/"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'SideQuest.app'
+  app "SideQuest.app"
 
-  zap trash: '~/Library/Application Support/SideQuest'
+  zap trash: "~/Library/Application Support/SideQuest"
 end
