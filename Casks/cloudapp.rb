3ab701cfe45285eb5e98b6a2dd3b1c3b6d07c0a4
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:20:27 +0100

    cloudapp: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/cloudapp.rb
+++ Casks/cloudapp.rb
@@ -1,13 +1,13 @@
-cask 'cloudapp' do
-  version '6.2.1.2088'
-  sha256 '1c69aa469dd156f234ce6715a06094cc5a486d4280ddd17abf07238a9f37df38'
+cask "cloudapp" do
+  version "6.2.1.2088"
+  sha256 "1c69aa469dd156f234ce6715a06094cc5a486d4280ddd17abf07238a9f37df38"
 
   url "http://downloads.getcloudapp.com/mac/CloudApp-#{version}.zip"
-  appcast 'https://d2plwz9jdz9z5d.cloudfront.net/mac/latest/appcast.xml'
-  name 'CloudApp'
-  homepage 'https://www.getcloudapp.com/'
+  appcast "https://d2plwz9jdz9z5d.cloudfront.net/mac/latest/appcast.xml"
+  name "CloudApp"
+  homepage "https://www.getcloudapp.com/"
 
-  app 'CloudApp.app'
+  app "CloudApp.app"
 
-  zap trash: '~/Library/Preferences/com.linebreak.CloudAppMacOSX.plist'
+  zap trash: "~/Library/Preferences/com.linebreak.CloudAppMacOSX.plist"
 end
