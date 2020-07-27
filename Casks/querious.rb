ecf3d1ddb9c8b9643cce54e6e832f816432bbfc3
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:29 +0100

    querious: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/querious.rb
+++ Casks/querious.rb
@@ -1,13 +1,13 @@
-cask 'querious' do
-  version '3.0.5'
-  sha256 'a2a97a15131b9fde640c9b92515cb24b38f939a75e793c28be747543caad44cd'
+cask "querious" do
+  version "3.0.5"
+  sha256 "a2a97a15131b9fde640c9b92515cb24b38f939a75e793c28be747543caad44cd"
 
   url "https://www.araelium.com/querious/downloads/versions/Querious#{version}.zip"
-  appcast 'https://arweb-assets.s3.amazonaws.com/downloads/querious/updates.json'
+  appcast "https://arweb-assets.s3.amazonaws.com/downloads/querious/updates.json"
   name "Querious #{version.major}"
-  homepage 'https://www.araelium.com/querious/'
+  homepage "https://www.araelium.com/querious/"
 
   auto_updates true
 
-  app 'Querious.app'
+  app "Querious.app"
 end
