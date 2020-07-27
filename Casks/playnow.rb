c7b79e80e739bfaad52542f64503101cc9bdf40e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:28 +0100

    playnow: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/playnow.rb
+++ Casks/playnow.rb
@@ -1,11 +1,11 @@
-cask 'playnow' do
-  version '0.4'
-  sha256 'c082bc144ca1991bf045ee5f416a0066c549e6adb35d49edf881a58f8dbec801'
+cask "playnow" do
+  version "0.4"
+  sha256 "c082bc144ca1991bf045ee5f416a0066c549e6adb35d49edf881a58f8dbec801"
 
   url "https://github.com/marcboquet/PlayNow/releases/download/v#{version}/PlayNow.tgz"
-  appcast 'https://github.com/marcboquet/PlayNow/releases.atom'
-  name 'PlayNow'
-  homepage 'https://github.com/marcboquet/PlayNow/'
+  appcast "https://github.com/marcboquet/PlayNow/releases.atom"
+  name "PlayNow"
+  homepage "https://github.com/marcboquet/PlayNow/"
 
-  app 'PlayNow.app'
+  app "PlayNow.app"
 end
