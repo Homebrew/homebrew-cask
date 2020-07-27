d80f35465c4b0c98e932db53a9a2cd33d548b5a5
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:15 +0100

    phocus: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/phocus.rb
+++ Casks/phocus.rb
@@ -1,12 +1,12 @@
-cask 'phocus' do
-  version '3.5.1'
-  sha256 '4455cc62b6da566e750b7a25fab14cb0efc4bc921870f047ae71b0eaf42b2854'
+cask "phocus" do
+  version "3.5.1"
+  sha256 "4455cc62b6da566e750b7a25fab14cb0efc4bc921870f047ae71b0eaf42b2854"
 
   url "https://cdn.hasselblad.com/software/Phocus-for-Mac/#{version}/Phocus-#{version}.dmg"
-  name 'Hasselblad Phocus'
-  homepage 'https://www.hasselblad.com/software/phocus'
+  name "Hasselblad Phocus"
+  homepage "https://www.hasselblad.com/software/phocus"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'Phocus.app'
+  app "Phocus.app"
 end
