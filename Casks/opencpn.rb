0f2ff1a0b9ba710dee996e27d9c23e61af9d594a
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:32:24 +0100

    opencpn: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/opencpn.rb
+++ Casks/opencpn.rb
@@ -1,16 +1,16 @@
-cask 'opencpn' do
-  version '5.2.0,1.4d956e1'
-  sha256 'b31c23bd02dfaf7a8844db39c894c0765b386b4a082cb0b2e8fab7e2ed9f1209'
+cask "opencpn" do
+  version "5.2.0,1.4d956e1"
+  sha256 "b31c23bd02dfaf7a8844db39c894c0765b386b4a082cb0b2e8fab7e2ed9f1209"
 
   url "http://download.opencpn.org/#{version.before_comma}/OpenCPN_#{version.before_comma}+#{version.after_comma}.pkg"
-  appcast 'https://github.com/OpenCPN/OpenCPN/releases.atom'
-  name 'OpenCPN'
-  homepage 'https://www.opencpn.org/'
+  appcast "https://github.com/OpenCPN/OpenCPN/releases.atom"
+  name "OpenCPN"
+  homepage "https://www.opencpn.org/"
 
   pkg "OpenCPN_#{version.before_comma} #{version.after_comma}.pkg"
 
   uninstall pkgutil: [
-                       'org.opencpn.pkg.OpenCPN',
-                       'org.opencpn',
-                     ]
+    "org.opencpn.pkg.OpenCPN",
+    "org.opencpn",
+  ]
 end
