29340d88c92b0898dbb3c63baf90dd3970bd6ab1
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:17:43 +0100

    bbc-iplayer-downloads: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/bbc-iplayer-downloads.rb
+++ Casks/bbc-iplayer-downloads.rb
@@ -1,12 +1,12 @@
-cask 'bbc-iplayer-downloads' do
-  version '2.12.5'
-  sha256 '1a2565d11cba6a863974c1ad9f60ab9a766475867e86c898cabd86cb335d99d3'
+cask "bbc-iplayer-downloads" do
+  version "2.12.5"
+  sha256 "1a2565d11cba6a863974c1ad9f60ab9a766475867e86c898cabd86cb335d99d3"
 
   # live-downloads-app-bucket-staticassetsbucket-ydn3z4ggyaof.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://live-downloads-app-bucket-staticassetsbucket-ydn3z4ggyaof.s3.amazonaws.com/releases/darwin-x64/BBCiPlayerDownloads-#{version}.dmg"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://downloads-app.iplayer.api.bbc.co.uk/stable/darwin-x64'
-  name 'BBC iPlayer Downloads'
-  homepage 'https://www.bbc.co.uk/iplayer/install'
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://downloads-app.iplayer.api.bbc.co.uk/stable/darwin-x64"
+  name "BBC iPlayer Downloads"
+  homepage "https://www.bbc.co.uk/iplayer/install"
 
-  app 'BBC iPlayer Downloads.app'
+  app "BBC iPlayer Downloads.app"
 end
