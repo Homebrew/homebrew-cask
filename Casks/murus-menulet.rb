5081d855394777acb550f3dd9f758d4e44b5dfec
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:31:17 +0100

    murus-menulet: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/murus-menulet.rb
+++ Casks/murus-menulet.rb
@@ -1,14 +1,14 @@
-cask 'murus-menulet' do
-  version '2.0'
-  sha256 '510c7422df80fc13e53e0d5809edc26edde07eb68f3c8fb71833b5112abbb3af'
+cask "murus-menulet" do
+  version "2.0"
+  sha256 "510c7422df80fc13e53e0d5809edc26edde07eb68f3c8fb71833b5112abbb3af"
 
   url "https://www.murusfirewall.com/downloads/murus_menulet-#{version}.zip"
-  name 'Murus Firewall'
-  homepage 'https://www.murusfirewall.com/'
+  name "Murus Firewall"
+  homepage "https://www.murusfirewall.com/"
 
-  app 'Murus Menulet.app'
+  app "Murus Menulet.app"
 
-  uninstall delete:    '/usr/local/bin/murusd',
-            launchctl: 'it.murus.murusd',
-            quit:      'it.murus.Murus-Menulet'
+  uninstall delete:    "/usr/local/bin/murusd",
+            launchctl: "it.murus.murusd",
+            quit:      "it.murus.Murus-Menulet"
 end
