b81cc37ebeb86343d74994e48a0fd1611ff977de
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:21:29 +0100

    dashcam-viewer: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/dashcam-viewer.rb
+++ Casks/dashcam-viewer.rb
@@ -1,13 +1,13 @@
-cask 'dashcam-viewer' do
-  version '3.5.1'
-  sha256 '993d42ad4992c3552ad0645ffa9cec42748a5f167c04db3904915d7eab41dfa3'
+cask "dashcam-viewer" do
+  version "3.5.1"
+  sha256 "993d42ad4992c3552ad0645ffa9cec42748a5f167c04db3904915d7eab41dfa3"
 
   # filedn.com/l2s8TAtm4VASBX72ds0zYD8/dcv/ was verified as official when first introduced to the cask
   url "https://filedn.com/l2s8TAtm4VASBX72ds0zYD8/dcv/Dashcam_Viewer_v#{version}.dmg"
-  appcast 'https://dashcamviewer.com/'
-  name 'Dashcam Viewer'
-  name 'Dashcam Viewer by Earthshine Software'
-  homepage 'https://dashcamviewer.com/'
+  appcast "https://dashcamviewer.com/"
+  name "Dashcam Viewer"
+  name "Dashcam Viewer by Earthshine Software"
+  homepage "https://dashcamviewer.com/"
 
-  app 'Dashcam Viewer.app'
+  app "Dashcam Viewer.app"
 end
