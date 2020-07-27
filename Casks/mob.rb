9ba5f847141b41224b05f36fc8e7b5b4f3bec808
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:30:48 +0100

    mob: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/mob.rb
+++ Casks/mob.rb
@@ -1,13 +1,13 @@
-cask 'mob' do
-  version '0.2.2'
-  sha256 'a70d66d92310737d9599215d558670a45265795be0be980934a91e9880eb4a73'
+cask "mob" do
+  version "0.2.2"
+  sha256 "a70d66d92310737d9599215d558670a45265795be0be980934a91e9880eb4a73"
 
   url "https://github.com/zenghongtu/Mob/releases/download/v#{version}/Mob-#{version}-mac.dmg"
-  appcast 'https://github.com/zenghongtu/Mob/releases.atom'
-  name 'Mob'
-  homepage 'https://github.com/zenghongtu/Mob'
+  appcast "https://github.com/zenghongtu/Mob/releases.atom"
+  name "Mob"
+  homepage "https://github.com/zenghongtu/Mob"
 
-  app 'Mob.app'
+  app "Mob.app"
 
-  zap trash: '~/Library/Application Support/mob'
+  zap trash: "~/Library/Application Support/mob"
 end
