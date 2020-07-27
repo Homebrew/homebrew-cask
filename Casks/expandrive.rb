ac4105fb082c15ad0f4025282f9145e072e8094a
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:23:35 +0100

    expandrive: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/expandrive.rb
+++ Casks/expandrive.rb
@@ -1,20 +1,20 @@
-cask 'expandrive' do
-  version '7.5.0'
-  sha256 '8c0ddc599010d8e01433a6593f3cc669ff1b8854ebb1f039adcc4cded2ee7e03'
+cask "expandrive" do
+  version "7.5.0"
+  sha256 "8c0ddc599010d8e01433a6593f3cc669ff1b8854ebb1f039adcc4cded2ee7e03"
 
   url "https://updates.expandrive.com/apps/expandrive#{version.major}/v/#{version.dots_to_hyphens}/update_download"
   appcast "https://updates.expandrive.com/appcast/expandrive#{version.major}.json?version=#{version.major}.0.0"
-  name 'ExpanDrive'
-  homepage 'https://www.expandrive.com/apps/expandrive/'
+  name "ExpanDrive"
+  homepage "https://www.expandrive.com/apps/expandrive/"
 
-  app 'ExpanDrive.app'
+  app "ExpanDrive.app"
 
   zap trash: [
-               '~/Library/Application Support/ExpanDrive',
-               '~/Library/Preferences/com.expandrive.exfs.plist',
-               '~/Library/Preferences/com.expandrive.ExpanDrive.plist',
-               '~/Library/Preferences/com.expandrive.ExpanDrive2.plist',
-               '~/Library/Preferences/com.expandrive.ExpanDrive3.plist',
-               '~/Library/Preferences/com.expandrive.ExpanDrive.helper.plist',
-             ]
+    "~/Library/Application Support/ExpanDrive",
+    "~/Library/Preferences/com.expandrive.exfs.plist",
+    "~/Library/Preferences/com.expandrive.ExpanDrive.plist",
+    "~/Library/Preferences/com.expandrive.ExpanDrive2.plist",
+    "~/Library/Preferences/com.expandrive.ExpanDrive3.plist",
+    "~/Library/Preferences/com.expandrive.ExpanDrive.helper.plist",
+  ]
 end
