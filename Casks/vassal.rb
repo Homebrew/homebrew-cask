a848409e1521c19abf587adfe57e639f748b5723
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:38:13 +0100

    vassal: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/vassal.rb
+++ Casks/vassal.rb
@@ -1,12 +1,12 @@
-cask 'vassal' do
-  version '3.3.2'
-  sha256 '669bc1bfb3da870a13b0fbe0e9438d9b4b0c5ac660588d10d88ac62db2357391'
+cask "vassal" do
+  version "3.3.2"
+  sha256 "669bc1bfb3da870a13b0fbe0e9438d9b4b0c5ac660588d10d88ac62db2357391"
 
   # github.com/vassalengine/vassal was verified as official when first introduced to the cask
   url "https://github.com/vassalengine/vassal/releases/download/#{version}/VASSAL-#{version}-macosx.dmg"
-  appcast 'https://github.com/vassalengine/vassal/releases.atom'
-  name 'VASSAL'
-  homepage 'http://www.vassalengine.org/'
+  appcast "https://github.com/vassalengine/vassal/releases.atom"
+  name "VASSAL"
+  homepage "http://www.vassalengine.org/"
 
-  app 'VASSAL.app'
+  app "VASSAL.app"
 end
