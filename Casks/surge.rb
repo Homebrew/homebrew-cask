25b87ba70e79345e74e15a690301848ba18eb543
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:55 +0100

    surge: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/surge.rb
+++ Casks/surge.rb
@@ -1,25 +1,25 @@
-cask 'surge' do
-  version '3.5.5-1123'
-  sha256 'e76790745f443b2694ffce2e0d552ba4c83bc8d8644da27f32066d22cc9ec4f8'
+cask "surge" do
+  version "3.5.5-1123"
+  sha256 "e76790745f443b2694ffce2e0d552ba4c83bc8d8644da27f32066d22cc9ec4f8"
 
   url "https://www.nssurge.com/mac/v#{version.major}/Surge-#{version}.zip"
   appcast "https://www.nssurge.com/mac/v#{version.major}/appcast-signed.xml"
-  name 'Surge'
-  homepage 'https://nssurge.com/'
+  name "Surge"
+  homepage "https://nssurge.com/"
 
   auto_updates true
-  depends_on macos: '>= :el_capitan'
+  depends_on macos: ">= :el_capitan"
 
-  app 'Surge.app'
+  app "Surge.app"
 
-  uninstall launchctl: 'com.nssurge.surge-mac.helper',
-            delete:    '/Library/PrivilegedHelperTools/com.nssurge.surge-mac.helper'
+  uninstall launchctl: "com.nssurge.surge-mac.helper",
+            delete:    "/Library/PrivilegedHelperTools/com.nssurge.surge-mac.helper"
 
   zap delete: [
-                '~/Library/Application Support/com.nssurge.surge-mac',
-                '~/Library/Caches/com.nssurge.surge-mac',
-                '~/Library/Caches/com.nssurge.surge-mac.plist',
-                '~/Library/Logs/Surge',
-                '~/Library/Preferences/com.nssurge.surge-mac.plist',
-              ]
+    "~/Library/Application Support/com.nssurge.surge-mac",
+    "~/Library/Caches/com.nssurge.surge-mac",
+    "~/Library/Caches/com.nssurge.surge-mac.plist",
+    "~/Library/Logs/Surge",
+    "~/Library/Preferences/com.nssurge.surge-mac.plist",
+  ]
 end
