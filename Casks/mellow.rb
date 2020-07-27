4dfab80ab06b420b4ea8b68eaecc35cdab1525d5
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:30:13 +0100

    mellow: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/mellow.rb
+++ Casks/mellow.rb
@@ -1,17 +1,17 @@
-cask 'mellow' do
-  version '0.1.21'
-  sha256 '2f071d1f19323e6f1f2d727fc978a67b133004188a7f74dd87a4d6504ee10c68'
+cask "mellow" do
+  version "0.1.21"
+  sha256 "2f071d1f19323e6f1f2d727fc978a67b133004188a7f74dd87a4d6504ee10c68"
 
   url "https://github.com/mellow-io/mellow/releases/download/v#{version}/Mellow-#{version}.dmg"
-  appcast 'https://github.com/mellow-io/mellow/releases.atom'
-  name 'Mellow'
-  homepage 'https://github.com/mellow-io/mellow'
+  appcast "https://github.com/mellow-io/mellow/releases.atom"
+  name "Mellow"
+  homepage "https://github.com/mellow-io/mellow"
 
-  app 'Mellow.app'
+  app "Mellow.app"
 
   zap trash: [
-               '/Library/Application Support/Mellow',
-               '~/Library/Logs/Mellow',
-               '~/Library/Preferences/org.mellow.mellow.plist',
-             ]
+    "/Library/Application Support/Mellow",
+    "~/Library/Logs/Mellow",
+    "~/Library/Preferences/org.mellow.mellow.plist",
+  ]
 end
