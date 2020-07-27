daa043ab1416b491914df20cddea50bb4379203e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:37:58 +0100

    typora: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/typora.rb
+++ Casks/typora.rb
@@ -1,25 +1,25 @@
-cask 'typora' do
-  version '0.9.9.34.2'
-  sha256 'af9cdc2c0ab14916a8c4c956c966921230a7de2ce433e657d79a88fb2ff6f36c'
+cask "typora" do
+  version "0.9.9.34.2"
+  sha256 "af9cdc2c0ab14916a8c4c956c966921230a7de2ce433e657d79a88fb2ff6f36c"
 
   url "https://www.typora.io/download/Typora-#{version}.dmg"
-  appcast 'https://www.typora.io/download/dev_update.xml'
-  name 'Typora'
-  homepage 'https://typora.io/'
+  appcast "https://www.typora.io/download/dev_update.xml"
+  name "Typora"
+  homepage "https://typora.io/"
 
   auto_updates true
-  depends_on macos: '>= :high_sierra'
+  depends_on macos: ">= :high_sierra"
 
-  app 'Typora.app'
+  app "Typora.app"
 
   zap trash: [
-               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/abnerworks.typora.sfl*',
-               '~/Library/Application Support/Typora',
-               '~/Library/Application Support/abnerworks.Typora',
-               '~/Library/Caches/abnerworks.Typora',
-               '~/Library/Cookies/abnerworks.Typora.binarycookies',
-               '~/Library/Preferences/abnerworks.Typora.plist',
-               '~/Library/Saved Application State/abnerworks.Typora.savedState',
-               '~/Library/WebKit/abnerworks.Typora',
-             ]
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/abnerworks.typora.sfl*",
+    "~/Library/Application Support/Typora",
+    "~/Library/Application Support/abnerworks.Typora",
+    "~/Library/Caches/abnerworks.Typora",
+    "~/Library/Cookies/abnerworks.Typora.binarycookies",
+    "~/Library/Preferences/abnerworks.Typora.plist",
+    "~/Library/Saved Application State/abnerworks.Typora.savedState",
+    "~/Library/WebKit/abnerworks.Typora",
+  ]
 end
