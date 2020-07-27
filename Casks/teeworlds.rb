38cc894341af6c3eff6144c9db47f5c0d1db9d68
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:37:15 +0100

    teeworlds: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/teeworlds.rb
+++ Casks/teeworlds.rb
@@ -1,12 +1,12 @@
-cask 'teeworlds' do
-  version '0.7.5'
-  sha256 'a79155c8bd7a0ce08457760f1ce37d8b7611f86717659bb3d90cd0e2dae5194b'
+cask "teeworlds" do
+  version "0.7.5"
+  sha256 "a79155c8bd7a0ce08457760f1ce37d8b7611f86717659bb3d90cd0e2dae5194b"
 
   url "https://downloads.teeworlds.com/teeworlds-#{version}-osx.dmg"
-  appcast 'https://teeworlds.com/?page=downloads'
-  name 'Teeworlds'
-  homepage 'https://www.teeworlds.com/'
+  appcast "https://teeworlds.com/?page=downloads"
+  name "Teeworlds"
+  homepage "https://www.teeworlds.com/"
 
-  app 'Teeworlds.app'
-  app 'Teeworlds Server.app'
+  app "Teeworlds.app"
+  app "Teeworlds Server.app"
 end
