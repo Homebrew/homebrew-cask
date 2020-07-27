49f4ac0a0da9c0c7ae712b766175eec685b82c15
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:20:59 +0100

    corelocationcli: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/corelocationcli.rb
+++ Casks/corelocationcli.rb
@@ -1,11 +1,11 @@
-cask 'corelocationcli' do
-  version '3.1.0'
-  sha256 '2849ad3d45ba836e49ce2d46ebe21a6d587c16bdd2c48d978bf8dc8494c9f26e'
+cask "corelocationcli" do
+  version "3.1.0"
+  sha256 "2849ad3d45ba836e49ce2d46ebe21a6d587c16bdd2c48d978bf8dc8494c9f26e"
 
   url "https://github.com/fulldecent/corelocationcli/releases/download/#{version}/CoreLocationCLI.zip"
-  appcast 'https://github.com/fulldecent/corelocationcli/releases.atom'
-  name 'Core Location CLI'
-  homepage 'https://github.com/fulldecent/corelocationcli'
+  appcast "https://github.com/fulldecent/corelocationcli/releases.atom"
+  name "Core Location CLI"
+  homepage "https://github.com/fulldecent/corelocationcli"
 
-  binary 'CoreLocationCLI'
+  binary "CoreLocationCLI"
 end
