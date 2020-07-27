a542019dab43bee93e62834068d822bd1146c4b5
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:46 +0100

    makemkv: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/makemkv.rb
+++ Casks/makemkv.rb
@@ -1,16 +1,16 @@
-cask 'makemkv' do
-  version '1.15.2'
-  sha256 '841bf2e6bbd13f793a84595aa1bb1bb02861eb4dae848d26cd96fe0ddd53f970'
+cask "makemkv" do
+  version "1.15.2"
+  sha256 "841bf2e6bbd13f793a84595aa1bb1bb02861eb4dae848d26cd96fe0ddd53f970"
 
   url "https://www.makemkv.com/download/makemkv_v#{version}_osx.dmg"
-  appcast 'https://www.makemkv.com/download/'
-  name 'MakeMKV'
-  homepage 'https://www.makemkv.com/'
+  appcast "https://www.makemkv.com/download/"
+  name "MakeMKV"
+  homepage "https://www.makemkv.com/"
 
-  app 'MakeMKV.app'
+  app "MakeMKV.app"
 
   zap trash: [
-               '~/Library/Preferences/com.makemkv.MakeMKV.plist',
-               '~/Library/Saved Application State/com.makemkv.MakeMKV.savedState',
-             ]
+    "~/Library/Preferences/com.makemkv.MakeMKV.plist",
+    "~/Library/Saved Application State/com.makemkv.MakeMKV.savedState",
+  ]
 end
