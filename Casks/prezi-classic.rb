e216dfc1390d00f1eff0f36da282c4f0b3aa0a3d
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:51 +0100

    prezi-classic: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/prezi-classic.rb
+++ Casks/prezi-classic.rb
@@ -1,11 +1,11 @@
-cask 'prezi-classic' do
-  version '6.14.0'
-  sha256 'ed9cc68796e4a055fa01cd28ab593370b6f4d87a937237e2f23f7fba9c70f332'
+cask "prezi-classic" do
+  version "6.14.0"
+  sha256 "ed9cc68796e4a055fa01cd28ab593370b6f4d87a937237e2f23f7fba9c70f332"
 
   url "https://desktopassets.prezi.com/mac/pd6/releases/Prezi_Classic_#{version}.dmg"
-  appcast 'https://prezidesktop.s3.amazonaws.com/assets/mac/pd6/updates/prezi-classic.xml'
-  name 'Prezi Classic'
-  homepage 'https://prezi.com/desktop'
+  appcast "https://prezidesktop.s3.amazonaws.com/assets/mac/pd6/updates/prezi-classic.xml"
+  name "Prezi Classic"
+  homepage "https://prezi.com/desktop"
 
-  app 'Prezi Classic.app'
+  app "Prezi Classic.app"
 end
