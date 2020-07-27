38c8a01665c23b68dcdd4a148bfd0eeca6e504d3
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:37:08 +0100

    tagspaces: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/tagspaces.rb
+++ Casks/tagspaces.rb
@@ -1,12 +1,12 @@
-cask 'tagspaces' do
-  version '3.5.4'
-  sha256 '47f3bca6918fd66628f3e4f70aaac4f9daa17b26092d78ce01b9c1a6ea4972d0'
+cask "tagspaces" do
+  version "3.5.4"
+  sha256 "47f3bca6918fd66628f3e4f70aaac4f9daa17b26092d78ce01b9c1a6ea4972d0"
 
   # github.com/tagspaces/tagspaces/ was verified as official when first introduced to the cask
   url "https://github.com/tagspaces/tagspaces/releases/download/v#{version}/tagspaces-mac-#{version}.zip"
-  appcast 'https://github.com/tagspaces/tagspaces/releases.atom'
-  name 'TagSpaces'
-  homepage 'https://www.tagspaces.org/'
+  appcast "https://github.com/tagspaces/tagspaces/releases.atom"
+  name "TagSpaces"
+  homepage "https://www.tagspaces.org/"
 
-  app 'TagSpaces.app'
+  app "TagSpaces.app"
 end
