da716e6b967e5d49464b6d92185f4d60fcbf7b60
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:23:06 +0100

    electron: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,19 +1,19 @@
-cask 'electron' do
-  version '9.1.1'
-  sha256 '354078a716b0e86bb7754f8e372318e69ce64a67e782ef25cea3ee3195fb190d'
+cask "electron" do
+  version "9.1.1"
+  sha256 "354078a716b0e86bb7754f8e372318e69ce64a67e782ef25cea3ee3195fb190d"
 
   # github.com/electron/electron/ was verified as official when first introduced to the cask
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
-  appcast 'https://github.com/electron/electron/releases.atom'
-  name 'Electron'
-  homepage 'https://electronjs.org/'
+  appcast "https://github.com/electron/electron/releases.atom"
+  name "Electron"
+  homepage "https://electronjs.org/"
 
-  app 'Electron.app'
+  app "Electron.app"
 
   zap trash: [
-               '~/Library/Application Support/Electron',
-               '~/Library/Caches/Electron',
-               '~/Library/Preferences/com.github.electron.helper.plist',
-               '~/Library/Preferences/com.github.electron.plist',
-             ]
+    "~/Library/Application Support/Electron",
+    "~/Library/Caches/Electron",
+    "~/Library/Preferences/com.github.electron.helper.plist",
+    "~/Library/Preferences/com.github.electron.plist",
+  ]
 end
