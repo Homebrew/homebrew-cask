3160fce3b7ed496e6ed5ae636420244e15a81eb0
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:29:10 +0100

    logdna-agent: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/logdna-agent.rb
+++ Casks/logdna-agent.rb
@@ -1,17 +1,17 @@
-cask 'logdna-agent' do
-  version '1.6.2'
-  sha256 '350956cbeddf0f0b1cf991a58d659e2a4c2344b621370e10c8adb950f9c6bc47'
+cask "logdna-agent" do
+  version "1.6.2"
+  sha256 "350956cbeddf0f0b1cf991a58d659e2a4c2344b621370e10c8adb950f9c6bc47"
 
   # github.com/logdna/logdna-agent/ was verified as official when first introduced to the cask
   url "https://github.com/logdna/logdna-agent/releases/download/#{version}/logdna-agent-#{version}.pkg"
-  appcast 'https://github.com/logdna/logdna-agent/releases.atom'
-  name 'LogDNA Agent'
-  homepage 'https://logdna.com/'
+  appcast "https://github.com/logdna/logdna-agent/releases.atom"
+  name "LogDNA Agent"
+  homepage "https://logdna.com/"
 
   pkg "logdna-agent-#{version}.pkg"
 
-  uninstall pkgutil:   'com.logdna.logdna-agent',
-            launchctl: 'com.logdna.logdna-agentd'
+  uninstall pkgutil:   "com.logdna.logdna-agent",
+            launchctl: "com.logdna.logdna-agentd"
 
   caveats <<~EOS
     When you first start logdna-agent, you must set your LogDNA API key with the command:
