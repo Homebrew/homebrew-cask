28702fd5349220ecaaaacb858a76ce75d569e3e1
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:34:53 +0100

    rekordbox: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/rekordbox.rb
+++ Casks/rekordbox.rb
@@ -1,14 +1,14 @@
-cask 'rekordbox' do
-  version '6.0.3,20200713191837'
-  sha256 'c7a071e51ba41cb2c512260aedab75d55819c6469723430c1ef4795c7d92f275'
+cask "rekordbox" do
+  version "6.0.3,20200713191837"
+  sha256 "c7a071e51ba41cb2c512260aedab75d55819c6469723430c1ef4795c7d92f275"
 
   url "https://cdn.rekordbox.com/files/#{version.after_comma}/Install_rekordbox_#{version.before_comma.dots_to_underscores}.pkg_.zip"
-  appcast 'https://rekordbox.com/en/support/releasenote/'
-  name 'rekordbox'
-  homepage 'https://rekordbox.com/en/'
+  appcast "https://rekordbox.com/en/support/releasenote/"
+  name "rekordbox"
+  homepage "https://rekordbox.com/en/"
 
   auto_updates true
-  depends_on macos: '>= :high_sierra'
+  depends_on macos: ">= :high_sierra"
 
   pkg "Install_rekordbox_#{version.before_comma.dots_to_underscores}.pkg"
 
@@ -16,7 +16,7 @@ cask 'rekordbox' do
             delete:  "/Applications/rekordbox #{version.major}"
 
   zap trash: [
-               '~/Library/Application Support/Pioneer/rekordbox',
-               '~/Library/Pioneer/rekordbox',
-             ]
+    "~/Library/Application Support/Pioneer/rekordbox",
+    "~/Library/Pioneer/rekordbox",
+  ]
 end
