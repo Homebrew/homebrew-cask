436b4b9e1af5cb97380f3dcb0534b063b9d38780
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:37:17 +0100

    terminology: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/terminology.rb
+++ Casks/terminology.rb
@@ -1,10 +1,10 @@
-cask 'terminology' do
+cask "terminology" do
   version :latest
   sha256 :no_check
 
-  url 'http://media.agiletortoise.com/Terminology-for-OS-X/Terminology-for-OS-X.zip'
-  name 'terminology'
-  homepage 'https://agiletortoise.com/terminology/mac/'
+  url "http://media.agiletortoise.com/Terminology-for-OS-X/Terminology-for-OS-X.zip"
+  name "terminology"
+  homepage "https://agiletortoise.com/terminology/mac/"
 
-  dictionary 'Terminology-for-OS-X/Terminology.dictionary'
+  dictionary "Terminology-for-OS-X/Terminology.dictionary"
 end
