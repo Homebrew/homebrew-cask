1ddb00450de584149d8095e5a922c2d0b8360576
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:22:26 +0100

    downie: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,15 +1,15 @@
-cask 'downie' do
-  version '4.1,4138'
-  sha256 '6f806b20905c340f5a087eebcdd27daaf7e483e841af075b6a2ccf400dd5005a'
+cask "downie" do
+  version "4.1,4138"
+  sha256 "6f806b20905c340f5a087eebcdd27daaf7e483e841af075b6a2ccf400dd5005a"
 
   # charliemonroesoftware.com/ was verified as official when first introduced to the cask
   url "https://charliemonroesoftware.com/trial/downie/v#{version.major}/Downie_#{version.major}_#{version.after_comma}.dmg"
   appcast "https://charliemonroesoftware.com/trial/downie/v#{version.major}/updates.xml"
-  name 'Downie'
-  homepage 'https://software.charliemonroe.net/downie.php'
+  name "Downie"
+  homepage "https://software.charliemonroe.net/downie.php"
 
   auto_updates true
-  depends_on macos: '>= :el_capitan'
+  depends_on macos: ">= :el_capitan"
 
   app "Downie #{version.major}.app"
 end
