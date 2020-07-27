3af5ff5f272cbd525782f287fe504f8049ddc6cd
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:30:48 +0100

    mnemosyne: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/mnemosyne.rb
+++ Casks/mnemosyne.rb
@@ -1,12 +1,12 @@
-cask 'mnemosyne' do
-  version '2.7.1'
-  sha256 'c7567a0f007cdaf16e8649e0bc24a7f265bfcb77be210f481752a6a51ee47997'
+cask "mnemosyne" do
+  version "2.7.1"
+  sha256 "c7567a0f007cdaf16e8649e0bc24a7f265bfcb77be210f481752a6a51ee47997"
 
   # sourceforge.net/mnemosyne-proj/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/mnemosyne-proj/mnemosyne/mnemosyne-#{version}/Mnemosyne-#{version}.dmg"
-  appcast 'https://sourceforge.net/projects/mnemosyne-proj/rss?path=/mnemosyne'
-  name 'Mnemosyne'
-  homepage 'https://mnemosyne-proj.org/'
+  appcast "https://sourceforge.net/projects/mnemosyne-proj/rss?path=/mnemosyne"
+  name "Mnemosyne"
+  homepage "https://mnemosyne-proj.org/"
 
-  app 'Mnemosyne.app'
+  app "Mnemosyne.app"
 end
