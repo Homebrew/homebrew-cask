ccd1f2ee371d67e09f8865f7a8ed2805ae4a3e63
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:19:08 +0100

    busycontacts: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/busycontacts.rb
+++ Casks/busycontacts.rb
@@ -1,16 +1,16 @@
-cask 'busycontacts' do
-  version '1.4.8,140803'
-  sha256 '012189b164a88de0d11fd31f0bdbc1cd334e49d93529546a9f9867fabd723ccd'
+cask "busycontacts" do
+  version "1.4.8,140803"
+  sha256 "012189b164a88de0d11fd31f0bdbc1cd334e49d93529546a9f9867fabd723ccd"
 
-  url 'https://www.busymac.com/download/BusyContacts.zip'
-  appcast 'https://www.busymac.com/busycontacts/news.plist'
-  name 'BusyContacts'
-  homepage 'https://www.busymac.com/busycontacts/index.html'
+  url "https://www.busymac.com/download/BusyContacts.zip"
+  appcast "https://www.busymac.com/busycontacts/news.plist"
+  name "BusyContacts"
+  homepage "https://www.busymac.com/busycontacts/index.html"
 
   auto_updates true
 
-  pkg 'BusyContacts Installer.pkg'
+  pkg "BusyContacts Installer.pkg"
 
-  uninstall pkgutil: 'com.busymac.busycontacts.pkg',
-            signal:  ['KILL', 'com.busymac.busycontacts']
+  uninstall pkgutil: "com.busymac.busycontacts.pkg",
+            signal:  ["KILL", "com.busymac.busycontacts"]
 end
