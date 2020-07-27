468f7f9af0797101de2d056ba44e8bb28b6ab93d
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:33 +0100

    securesafe: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/securesafe.rb
+++ Casks/securesafe.rb
@@ -1,18 +1,18 @@
-cask 'securesafe' do
-  version '2.7.1'
-  sha256 'dc501d465214a986c777019b16cef2ee77bd7062a3c23a7839ab6aa62d4b2366'
+cask "securesafe" do
+  version "2.7.1"
+  sha256 "dc501d465214a986c777019b16cef2ee77bd7062a3c23a7839ab6aa62d4b2366"
 
   url "https://www.securesafe.com/downloads/SecureSafe_#{version}.pkg"
-  appcast 'https://www.securesafe.com/en/downloads/'
-  name 'SecureSafe'
-  homepage 'https://www.securesafe.com/'
+  appcast "https://www.securesafe.com/en/downloads/"
+  name "SecureSafe"
+  homepage "https://www.securesafe.com/"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
   pkg "SecureSafe_#{version}.pkg"
 
   uninstall pkgutil: [
-                       'com.dswiss.securesafe.pkg.sync',
-                       'com.github.osxfuse.pkg.Core',
-                     ]
+    "com.dswiss.securesafe.pkg.sync",
+    "com.github.osxfuse.pkg.Core",
+  ]
 end
