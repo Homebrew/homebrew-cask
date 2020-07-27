94945539d6e3d2d894a3e8f8c8924354409f82e6
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:17:50 +0100

    beatport-pro: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/beatport-pro.rb
+++ Casks/beatport-pro.rb
@@ -1,11 +1,11 @@
-cask 'beatport-pro' do
-  version '2.4.5_188'
-  sha256 '8582347568c236695c24855d2fc32311961f58fe8b2b240819cf9eaa327bbd66'
+cask "beatport-pro" do
+  version "2.4.5_188"
+  sha256 "8582347568c236695c24855d2fc32311961f58fe8b2b240819cf9eaa327bbd66"
 
   url "https://pro.beatport.com/mac/#{version}/beatportpro_#{version}.zip"
-  appcast 'https://pro.beatport.com/mac/appcast.xml'
-  name 'Beatport Pro'
-  homepage 'https://www.beatport.com/'
+  appcast "https://pro.beatport.com/mac/appcast.xml"
+  name "Beatport Pro"
+  homepage "https://www.beatport.com/"
 
-  app 'Beatport Pro.app'
+  app "Beatport Pro.app"
 end
