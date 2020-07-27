81fa0d3d27efa14af0bd1ebd23748f4ac0e4f6a1
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:21:43 +0100

    dcp-o-matic-kdm-creator: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/dcp-o-matic-kdm-creator.rb
+++ Casks/dcp-o-matic-kdm-creator.rb
@@ -1,11 +1,11 @@
-cask 'dcp-o-matic-kdm-creator' do
-  version '2.14.33'
-  sha256 '77d0adf99ad318e463dbedf97ce773c44015e78f61c830550ff9623fc0ec08b7'
+cask "dcp-o-matic-kdm-creator" do
+  version "2.14.33"
+  sha256 "77d0adf99ad318e463dbedf97ce773c44015e78f61c830550ff9623fc0ec08b7"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.9-kdm&version=#{version}"
-  appcast 'https://dcpomatic.com/download'
-  name 'DCP-o-matic KDM Creator'
-  homepage 'https://dcpomatic.com/'
+  appcast "https://dcpomatic.com/download"
+  name "DCP-o-matic KDM Creator"
+  homepage "https://dcpomatic.com/"
 
   app "DCP-o-matic #{version.major} KDM Creator.app"
 end
