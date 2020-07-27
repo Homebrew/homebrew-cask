c451d636f9931ca3345f4184da07488c41e8ef9c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:24:49 +0100

    fugu: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/fugu.rb
+++ Casks/fugu.rb
@@ -1,11 +1,11 @@
-cask 'fugu' do
-  version '1.2.1pre1'
-  sha256 '3f96ac9c94516c100bab5fe03cc1571634955f15e1949d32d675b91f5058e328'
+cask "fugu" do
+  version "1.2.1pre1"
+  sha256 "3f96ac9c94516c100bab5fe03cc1571634955f15e1949d32d675b91f5058e328"
 
   url "https://downloads.sourceforge.net/fugussh/Unstable/fugu-#{version}/Fugu-#{version}.zip"
-  appcast 'https://sourceforge.net/projects/fugussh/rss'
-  name 'Fugu'
-  homepage 'https://sourceforge.net/projects/fugussh/'
+  appcast "https://sourceforge.net/projects/fugussh/rss"
+  name "Fugu"
+  homepage "https://sourceforge.net/projects/fugussh/"
 
-  app 'Fugu.app'
+  app "Fugu.app"
 end
