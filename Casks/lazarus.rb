9f57be3d8f58a31bbe98b1844c7809e788bce0fb
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:28:44 +0100

    lazarus: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/lazarus.rb
+++ Casks/lazarus.rb
@@ -1,18 +1,18 @@
-cask 'lazarus' do
-  version '2.0.10'
-  sha256 '13c000bb30cbaa73e76d3b5b2abbc55b219bd6c4443aecc158b8b51a5fceef5f'
+cask "lazarus" do
+  version "2.0.10"
+  sha256 "13c000bb30cbaa73e76d3b5b2abbc55b219bd6c4443aecc158b8b51a5fceef5f"
 
   # sourceforge.net/lazarus/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/lazarus/Lazarus%20macOS%20x86-64/Lazarus%20#{version}/Lazarus-#{version}-x86_64-macosx.pkg"
-  appcast 'https://sourceforge.net/projects/lazarus/rss'
-  name 'Lazarus'
-  homepage 'https://www.lazarus-ide.org/'
+  appcast "https://sourceforge.net/projects/lazarus/rss"
+  name "Lazarus"
+  homepage "https://www.lazarus-ide.org/"
 
-  depends_on formula: 'fpc'
-  depends_on cask: 'fpcsrc'
+  depends_on formula: "fpc"
+  depends_on cask: "fpcsrc"
 
   pkg "Lazarus-#{version}-x86_64-macosx.pkg"
 
-  uninstall pkgutil: 'org.lazarus-ide.pkg.LazarusIDE',
-            delete:  '/Applications/Lazarus.app'
+  uninstall pkgutil: "org.lazarus-ide.pkg.LazarusIDE",
+            delete:  "/Applications/Lazarus.app"
 end
