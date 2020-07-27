8a6c8d9558fb645303157eb0ae71645838d08191
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:45 +0100

    power-manager: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/power-manager.rb
+++ Casks/power-manager.rb
@@ -1,14 +1,14 @@
-cask 'power-manager' do
-  version '5.4.8'
-  sha256 '064f7172480c7b616331e7cdf374cc677be21336fd4d8c5dc7c16c5e4149fa3d'
+cask "power-manager" do
+  version "5.4.8"
+  sha256 "064f7172480c7b616331e7cdf374cc677be21336fd4d8c5dc7c16c5e4149fa3d"
 
   url "https://www.dssw.co.uk/powermanager/dsswpowermanager-#{version.no_dots}.dmg"
-  appcast 'https://version.dssw.co.uk/powermanager/'
-  name 'Power Manager'
-  homepage 'https://dssw.co.uk/powermanager/'
+  appcast "https://version.dssw.co.uk/powermanager/"
+  name "Power Manager"
+  homepage "https://dssw.co.uk/powermanager/"
 
   auto_updates true
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'Power Manager.app'
+  app "Power Manager.app"
 end
