54db5fc59aecf85a1f711b35d6124090af85d1be
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:28:46 +0100

    lbry: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/lbry.rb
+++ Casks/lbry.rb
@@ -1,12 +1,12 @@
-cask 'lbry' do
-  version '0.47.1'
-  sha256 'bcaa69e8f3c12dbde7d7d3761ea78af2576d788b71f793ebd45cc702d16568b5'
+cask "lbry" do
+  version "0.47.1"
+  sha256 "bcaa69e8f3c12dbde7d7d3761ea78af2576d788b71f793ebd45cc702d16568b5"
 
   # github.com/lbryio/lbry-desktop/ was verified as official when first introduced to the cask
   url "https://github.com/lbryio/lbry-desktop/releases/download/v#{version}/LBRY_#{version}.dmg"
-  appcast 'https://github.com/lbryio/lbry-desktop/releases.atom'
-  name 'LBRY'
-  homepage 'https://lbry.com/'
+  appcast "https://github.com/lbryio/lbry-desktop/releases.atom"
+  name "LBRY"
+  homepage "https://lbry.com/"
 
-  app 'LBRY.app'
+  app "LBRY.app"
 end
