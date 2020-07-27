c92c41fd21035a5648af822651f99fe384312cd0
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:30:00 +0100

    mate-translate: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/mate-translate.rb
+++ Casks/mate-translate.rb
@@ -1,14 +1,14 @@
-cask 'mate-translate' do
-  version '7.0.1'
-  sha256 'f865966ff61a124414c6622f30dff0e6ffd7556a8145836cca3735e2325b86d8'
+cask "mate-translate" do
+  version "7.0.1"
+  sha256 "f865966ff61a124414c6622f30dff0e6ffd7556a8145836cca3735e2325b86d8"
 
   # gikken.co/mate/ was verified as official when first introduced to the cask
-  url 'https://gikken.co/mate/MateTranslate.zip'
-  appcast 'https://gikken.co/mate/appcast.xml'
-  name 'Mate Translate'
-  homepage 'https://twopeoplesoftware.com/mate'
+  url "https://gikken.co/mate/MateTranslate.zip"
+  appcast "https://gikken.co/mate/appcast.xml"
+  name "Mate Translate"
+  homepage "https://twopeoplesoftware.com/mate"
 
-  depends_on macos: '>= :mojave'
+  depends_on macos: ">= :mojave"
 
-  app 'Mate Translate.app'
+  app "Mate Translate.app"
 end
