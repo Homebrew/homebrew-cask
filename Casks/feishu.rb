910b4176d853fb24b8acb515b46f42d461300a9e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:23:49 +0100

    feishu: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/feishu.rb
+++ Casks/feishu.rb
@@ -1,15 +1,15 @@
-cask 'feishu' do
-  version '3.28.1'
-  sha256 '22086cd6301c6c5da6b3b8145e4b9e6fac3cd6960f493c2bece1676ebd997f86'
+cask "feishu" do
+  version "3.28.1"
+  sha256 "22086cd6301c6c5da6b3b8145e4b9e6fac3cd6960f493c2bece1676ebd997f86"
 
   # sf3-ttcdn-tos.pstatp.com/ was verified as official when first introduced to the cask
   url "https://sf3-ttcdn-tos.pstatp.com/obj/ee-appcenter/Feishu-#{version}.dmg"
-  appcast 'https://www.feishu.cn/api/downloads'
-  name 'feishu'
-  homepage 'https://www.feishu.cn/'
+  appcast "https://www.feishu.cn/api/downloads"
+  name "feishu"
+  homepage "https://www.feishu.cn/"
 
   auto_updates true
 
   # Renamed for consistency: app name is different in the Finder and in a shell.
-  app 'Lark.app', target: 'Feishu.app'
+  app "Lark.app", target: "Feishu.app"
 end
