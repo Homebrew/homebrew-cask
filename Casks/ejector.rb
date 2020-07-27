3081496be646920ec259318ea0e64ae3f2459acf
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:23:02 +0100

    ejector: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/ejector.rb
+++ Casks/ejector.rb
@@ -1,11 +1,11 @@
-cask 'ejector' do
-  version '0.8.1'
-  sha256 'c6fefa1788a1a67793456e9620e18b9613c19634b649ebf6578e9c082ff4946e'
+cask "ejector" do
+  version "0.8.1"
+  sha256 "c6fefa1788a1a67793456e9620e18b9613c19634b649ebf6578e9c082ff4946e"
 
   url "https://www.jeb.com.fr/soft/Ejector-v#{version}.dmg"
-  appcast 'https://www.jeb.com.fr/en/ejector.shtml'
-  name 'Ejector'
-  homepage 'https://www.jeb.com.fr/en/ejector.shtml'
+  appcast "https://www.jeb.com.fr/en/ejector.shtml"
+  name "Ejector"
+  homepage "https://www.jeb.com.fr/en/ejector.shtml"
 
-  app 'Ejector.app'
+  app "Ejector.app"
 end
