369b5c0452f3b1e5a7f8a417ba12f19844798d82
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:27:08 +0100

    invisionsync: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/invisionsync.rb
+++ Casks/invisionsync.rb
@@ -1,11 +1,11 @@
-cask 'invisionsync' do
-  version '1.9.1,692'
-  sha256 'cb3c89b52a9c64c7996b4c427f73fe0805b6fa9b8dd5f213dc666391838f93ee'
+cask "invisionsync" do
+  version "1.9.1,692"
+  sha256 "cb3c89b52a9c64c7996b4c427f73fe0805b6fa9b8dd5f213dc666391838f93ee"
 
   url "https://projects.invisionapp.com/native_app/mac/sparkle/#{version.after_comma}.zip"
-  appcast 'https://projects.invisionapp.com/native_app/mac/sparkle/appcast_v2.xml'
-  name 'InVision Sync'
-  homepage 'https://www.invisionapp.com/'
+  appcast "https://projects.invisionapp.com/native_app/mac/sparkle/appcast_v2.xml"
+  name "InVision Sync"
+  homepage "https://www.invisionapp.com/"
 
-  app 'InVisionSync.app'
+  app "InVisionSync.app"
 end
