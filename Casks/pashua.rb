3d5fafbd06d62220428aebbaa9b8e235e11ba4ae
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:00 +0100

    pashua: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/pashua.rb
+++ Casks/pashua.rb
@@ -1,10 +1,10 @@
-cask 'pashua' do
+cask "pashua" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.bluem.net/files/Pashua.dmg'
-  name 'Pashua'
-  homepage 'https://www.bluem.net/en/mac/pashua/'
+  url "https://www.bluem.net/files/Pashua.dmg"
+  name "Pashua"
+  homepage "https://www.bluem.net/en/mac/pashua/"
 
-  app 'Pashua.app'
+  app "Pashua.app"
 end
