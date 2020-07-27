5fc8543718f6ebc7fcfc8c1117c7177b5263c38f
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:26:21 +0100

    holavpn: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/holavpn.rb
+++ Casks/holavpn.rb
@@ -1,11 +1,11 @@
-cask 'holavpn' do
-  version '1.0.25'
-  sha256 'a8f2468919174684210773ae01cc375feec8b83ccebfbd547c6bd230f2fd9b5f'
+cask "holavpn" do
+  version "1.0.25"
+  sha256 "a8f2468919174684210773ae01cc375feec8b83ccebfbd547c6bd230f2fd9b5f"
 
   url "https://cdn4.hola.org/static/HolaVPN-#{version}.dmg"
-  appcast 'https://hola.org/macos_vpn_update.xml'
-  name 'Hola VPN'
-  homepage 'https://hola.org/'
+  appcast "https://hola.org/macos_vpn_update.xml"
+  name "Hola VPN"
+  homepage "https://hola.org/"
 
-  app 'HolaVPN.app'
+  app "HolaVPN.app"
 end
