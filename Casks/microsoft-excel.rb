aa30e84144e187ae98361110fc6fdefdf89d371e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:30:28 +0100

    microsoft-excel: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/microsoft-excel.rb
+++ Casks/microsoft-excel.rb
@@ -1,52 +1,52 @@
-cask 'microsoft-excel' do
-  version '16.39.20071300'
-  sha256 '73c69912de92abdc29a32571c855b0798d61ac2590621be2c547ddaab5ab2ce0'
+cask "microsoft-excel" do
+  version "16.39.20071300"
+  sha256 "73c69912de92abdc29a32571c855b0798d61ac2590621be2c547ddaab5ab2ce0"
 
   # officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/ was verified as official when first introduced to the cask
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Excel_#{version}_Installer.pkg"
-  appcast 'https://docs.microsoft.com/en-us/officeupdates/update-history-office-for-mac'
-  name 'Microsoft Excel'
-  homepage 'https://products.office.com/en-US/excel'
+  appcast "https://docs.microsoft.com/en-us/officeupdates/update-history-office-for-mac"
+  name "Microsoft Excel"
+  homepage "https://products.office.com/en-US/excel"
 
   auto_updates true
-  conflicts_with cask: 'microsoft-office'
-  depends_on macos: '>= :sierra'
+  conflicts_with cask: "microsoft-office"
+  depends_on macos: ">= :sierra"
 
   pkg "Microsoft_Excel_#{version}_Installer.pkg"
 
   uninstall pkgutil:   [
-                         'com.microsoft.package.Microsoft_Excel.app',
-                         'com.microsoft.pkg.licensing',
-                       ],
+    "com.microsoft.package.Microsoft_Excel.app",
+    "com.microsoft.pkg.licensing",
+  ],
             launchctl: [
-                         'com.microsoft.office.licensingV2.helper',
-                         'com.microsoft.update.agent',
-                       ],
-            quit:      'com.microsoft.autoupdate2'
+              "com.microsoft.office.licensingV2.helper",
+              "com.microsoft.update.agent",
+            ],
+            quit:      "com.microsoft.autoupdate2"
 
   zap trash:     [
-                   '~/Library/Application Scripts/com.microsoft.Excel',
-                   '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.microsoft.excel.sfl*',
-                   '~/Library/Caches/Microsoft/uls/com.microsoft.autoupdate.fba',
-                   '~/Library/Caches/Microsoft/uls/com.microsoft.autoupdate2',
-                   '~/Library/Caches/com.microsoft.autoupdate.fba',
-                   '~/Library/Caches/com.microsoft.autoupdate2',
-                   '~/Library/Containers/com.microsoft.Excel',
-                   '~/Library/Cookies/com.microsoft.autoupdate.fba.binarycookies',
-                   '~/Library/Cookies/com.microsoft.autoupdate2.binarycookies',
-                   '~/Library/Preferences/com.microsoft.Excel.plist',
-                   '~/Library/Preferences/com.microsoft.autoupdate.fba.plist',
-                   '~/Library/Preferences/com.microsoft.autoupdate2.plist',
-                   '~/Library/Saved Application State/com.microsoft.autoupdate2.savedState',
-                 ],
+    "~/Library/Application Scripts/com.microsoft.Excel",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.microsoft.excel.sfl*",
+    "~/Library/Caches/Microsoft/uls/com.microsoft.autoupdate.fba",
+    "~/Library/Caches/Microsoft/uls/com.microsoft.autoupdate2",
+    "~/Library/Caches/com.microsoft.autoupdate.fba",
+    "~/Library/Caches/com.microsoft.autoupdate2",
+    "~/Library/Containers/com.microsoft.Excel",
+    "~/Library/Cookies/com.microsoft.autoupdate.fba.binarycookies",
+    "~/Library/Cookies/com.microsoft.autoupdate2.binarycookies",
+    "~/Library/Preferences/com.microsoft.Excel.plist",
+    "~/Library/Preferences/com.microsoft.autoupdate.fba.plist",
+    "~/Library/Preferences/com.microsoft.autoupdate2.plist",
+    "~/Library/Saved Application State/com.microsoft.autoupdate2.savedState",
+  ],
       rmdir:     [
-                   '~/Library/Caches/Microsoft/uls',
-                   '~/Library/Caches/Microsoft',
-                 ],
+        "~/Library/Caches/Microsoft/uls",
+        "~/Library/Caches/Microsoft",
+      ],
       launchctl: [
-                   'com.microsoft.autoupdate.helpertool',
-                   'com.microsoft.autoupdate.helper',
-                   'com.microsoft.update.agent',
-                 ],
-      pkgutil:   'com.microsoft.package.Microsoft_AutoUpdate.app'
+        "com.microsoft.autoupdate.helpertool",
+        "com.microsoft.autoupdate.helper",
+        "com.microsoft.update.agent",
+      ],
+      pkgutil:   "com.microsoft.package.Microsoft_AutoUpdate.app"
 end
