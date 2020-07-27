dce62b88b6c470f80a49400a8cc8c01f6fc5676d
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:37:33 +0100

    time-tracker: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/time-tracker.rb
+++ Casks/time-tracker.rb
@@ -1,11 +1,11 @@
-cask 'time-tracker' do
-  version '1.3.13'
-  sha256 'a6c982ee82f8babcf0f42d49950e5b3a1d613946736a7ee4a272b05916271aeb'
+cask "time-tracker" do
+  version "1.3.13"
+  sha256 "a6c982ee82f8babcf0f42d49950e5b3a1d613946736a7ee4a272b05916271aeb"
 
   url "https://github.com/rburgst/time-tracker-mac/releases/download/v#{version}-binary/Time.Tracker-#{version}-bin.zip"
-  appcast 'https://github.com/rburgst/time-tracker-mac/releases.atom'
-  name 'TimeTracker'
-  homepage 'https://github.com/rburgst/time-tracker-mac'
+  appcast "https://github.com/rburgst/time-tracker-mac/releases.atom"
+  name "TimeTracker"
+  homepage "https://github.com/rburgst/time-tracker-mac"
 
-  app 'Time Tracker.app'
+  app "Time Tracker.app"
 end
