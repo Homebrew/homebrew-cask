4d49380e969b6ae9d8f03e1c190b89d22d39abf6
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:36:43 +0100

    steermouse: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/steermouse.rb
+++ Casks/steermouse.rb
@@ -1,17 +1,17 @@
-cask 'steermouse' do
-  version '5.4.5'
-  sha256 'a2ad5821b923673ca5ee6ac27c0ee543127b3ec9c61dfd053f820246aa47417f'
+cask "steermouse" do
+  version "5.4.5"
+  sha256 "a2ad5821b923673ca5ee6ac27c0ee543127b3ec9c61dfd053f820246aa47417f"
 
   url "https://plentycom.jp/ctrl/files_sm/SteerMouse#{version}.dmg"
-  appcast 'https://plentycom.jp/en/steermouse/download.php'
-  name 'SteerMouse'
-  homepage 'https://plentycom.jp/en/steermouse/'
+  appcast "https://plentycom.jp/en/steermouse/download.php"
+  name "SteerMouse"
+  homepage "https://plentycom.jp/en/steermouse/"
 
-  prefpane 'SteerMouse.prefPane'
+  prefpane "SteerMouse.prefPane"
 
-  zap trash: '~/Library/Application Support/SteerMouse & CursorSense/Device.smsetting',
+  zap trash: "~/Library/Application Support/SteerMouse & CursorSense/Device.smsetting",
       rmdir: [
-               '~/Library/Application Support/SteerMouse & CursorSense/Device Definitions/',
-               '~/Library/Application Support/SteerMouse & CursorSense/',
-             ]
+        "~/Library/Application Support/SteerMouse & CursorSense/Device Definitions/",
+        "~/Library/Application Support/SteerMouse & CursorSense/",
+      ]
 end
