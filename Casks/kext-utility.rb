f112471555d53148e7150ef573d327b583fcdb55
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:28:10 +0100

    kext-utility: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/kext-utility.rb
+++ Casks/kext-utility.rb
@@ -1,11 +1,11 @@
-cask 'kext-utility' do
-  version '2.6.6'
-  sha256 '3527644157e2eefc78cf8719a71f34558d010c97db9b054bfe362b4542531f21'
+cask "kext-utility" do
+  version "2.6.6"
+  sha256 "3527644157e2eefc78cf8719a71f34558d010c97db9b054bfe362b4542531f21"
 
   url "https://cvad-mac.narod.ru/files/Kext_Utility.app.v#{version}.zip"
-  appcast 'https://cvad-mac.narod.ru/index/0-4'
-  name 'Kext Utility'
-  homepage 'https://cvad-mac.narod.ru/index/0-4'
+  appcast "https://cvad-mac.narod.ru/index/0-4"
+  name "Kext Utility"
+  homepage "https://cvad-mac.narod.ru/index/0-4"
 
-  app 'Kext Utility.app'
+  app "Kext Utility.app"
 end
