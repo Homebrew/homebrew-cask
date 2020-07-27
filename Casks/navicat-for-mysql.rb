0342fcf0e7441e323070c78b46167053bf27d634
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:31:33 +0100

    navicat-for-mysql: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/navicat-for-mysql.rb
+++ Casks/navicat-for-mysql.rb
@@ -1,11 +1,11 @@
-cask 'navicat-for-mysql' do
-  version '15.0.18'
-  sha256 'df13b80d8eb64faa94e29c88a1d46950b76de11a82810ec63e890afeee777968'
+cask "navicat-for-mysql" do
+  version "15.0.18"
+  sha256 "df13b80d8eb64faa94e29c88a1d46950b76de11a82810ec63e890afeee777968"
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_mysql_en.dmg"
-  appcast 'https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20for%20MySQL&appLang=en'
-  name 'Navicat for MySQL'
-  homepage 'https://www.navicat.com/products/navicat-for-mysql'
+  appcast "https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20for%20MySQL&appLang=en"
+  name "Navicat for MySQL"
+  homepage "https://www.navicat.com/products/navicat-for-mysql"
 
-  app 'Navicat for MySQL.app'
+  app "Navicat for MySQL.app"
 end
