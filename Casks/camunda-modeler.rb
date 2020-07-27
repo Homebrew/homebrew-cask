49018a34dc00c14f3edc449ab4c2613b2f1acc80
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:19:21 +0100

    camunda-modeler: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/camunda-modeler.rb
+++ Casks/camunda-modeler.rb
@@ -1,11 +1,11 @@
-cask 'camunda-modeler' do
-  version '4.1.1'
-  sha256 '890d625f1e76018d0fbc5f307ef03fb0ea98d9db7dbff66694dc1ab6649d097b'
+cask "camunda-modeler" do
+  version "4.1.1"
+  sha256 "890d625f1e76018d0fbc5f307ef03fb0ea98d9db7dbff66694dc1ab6649d097b"
 
   url "https://camunda.org/release/camunda-modeler/#{version}/camunda-modeler-#{version}-mac.zip"
-  appcast 'https://camunda.com/download/modeler/'
-  name 'Camunda Modeler'
-  homepage 'https://camunda.org/'
+  appcast "https://camunda.com/download/modeler/"
+  name "Camunda Modeler"
+  homepage "https://camunda.org/"
 
-  app 'Camunda Modeler.app'
+  app "Camunda Modeler.app"
 end
