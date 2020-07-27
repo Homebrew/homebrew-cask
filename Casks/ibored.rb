d362d5f0b7e8e708e08174554e085d70e3a5f0d8
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:26:36 +0100

    ibored: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/ibored.rb
+++ Casks/ibored.rb
@@ -1,11 +1,11 @@
-cask 'ibored' do
-  version '1.2'
-  sha256 'e85fbcc99f4920ed36e9f2f0b1c1bd55448eb2544c88888c64200d0786e88972'
+cask "ibored" do
+  version "1.2"
+  sha256 "e85fbcc99f4920ed36e9f2f0b1c1bd55448eb2544c88888c64200d0786e88972"
 
   url "https://files.tempel.org/iBored/iBored-Mac_#{version}.zip"
-  appcast 'https://apps.tempel.org/iBored/releasenotes.html'
-  name 'iBored'
-  homepage 'https://apps.tempel.org/iBored/'
+  appcast "https://apps.tempel.org/iBored/releasenotes.html"
+  name "iBored"
+  homepage "https://apps.tempel.org/iBored/"
 
-  app 'iBored.app'
+  app "iBored.app"
 end
