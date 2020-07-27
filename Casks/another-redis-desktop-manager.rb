620e310b2dd4985d4025694b5a9ecfd6df682b4d
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:16:02 +0100

    another-redis-desktop-manager: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/another-redis-desktop-manager.rb
+++ Casks/another-redis-desktop-manager.rb
@@ -1,11 +1,11 @@
-cask 'another-redis-desktop-manager' do
-  version '1.3.7'
-  sha256 'e5a8acdcf8f26c1240430a4702dc0d675d741b227711d33dbff63419673eeca1'
+cask "another-redis-desktop-manager" do
+  version "1.3.7"
+  sha256 "e5a8acdcf8f26c1240430a4702dc0d675d741b227711d33dbff63419673eeca1"
 
   url "https://github.com/qishibo/AnotherRedisDesktopManager/releases/download/v#{version}/Another-Redis-Desktop-Manager.#{version}.dmg"
-  appcast 'https://github.com/qishibo/AnotherRedisDesktopManager/releases.atom'
-  name 'Another Redis Desktop Manager'
-  homepage 'https://github.com/qishibo/AnotherRedisDesktopManager/'
+  appcast "https://github.com/qishibo/AnotherRedisDesktopManager/releases.atom"
+  name "Another Redis Desktop Manager"
+  homepage "https://github.com/qishibo/AnotherRedisDesktopManager/"
 
-  app 'Another Redis Desktop Manager.app'
+  app "Another Redis Desktop Manager.app"
 end
