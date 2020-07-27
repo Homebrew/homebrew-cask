e76846b3df84db2bef85510baa9dff001c13f0c1
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:19:58 +0100

    chronicle: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/chronicle.rb
+++ Casks/chronicle.rb
@@ -1,11 +1,11 @@
-cask 'chronicle' do
-  version '9.7.1'
-  sha256 '51855fabab9a262231d96e4635a57a4a84c2361f001ce15bdd96a3ccadb00a91'
+cask "chronicle" do
+  version "9.7.1"
+  sha256 "51855fabab9a262231d96e4635a57a4a84c2361f001ce15bdd96a3ccadb00a91"
 
-  url 'https://www.chronicleapp.com/static/downloads/chroniclepro.zip'
-  appcast 'https://chronicleapp.com/chronicle8.xml'
-  name 'Chronicle'
-  homepage 'https://chronicleapp.com/'
+  url "https://www.chronicleapp.com/static/downloads/chroniclepro.zip"
+  appcast "https://chronicleapp.com/chronicle8.xml"
+  name "Chronicle"
+  homepage "https://chronicleapp.com/"
 
-  app 'Chronicle Pro.app'
+  app "Chronicle Pro.app"
 end
