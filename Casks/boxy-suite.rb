78f287c73248ade96713411599961b43fee9d0d0
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:18:48 +0100

    boxy-suite: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/boxy-suite.rb
+++ Casks/boxy-suite.rb
@@ -1,14 +1,14 @@
-cask 'boxy-suite' do
+cask "boxy-suite" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.boxysuite.com/direct_download'
-  name 'Boxy Suite'
-  homepage 'https://www.boxysuite.com/'
+  url "https://www.boxysuite.com/direct_download"
+  name "Boxy Suite"
+  homepage "https://www.boxysuite.com/"
 
-  app 'Boxy Dashboard.app'
-  app 'Boxy for Gmail.app'
-  app 'Boxy for Calendar.app'
-  app 'Boxy for Keep.app'
-  app 'Boxy for Contacts.app'
+  app "Boxy Dashboard.app"
+  app "Boxy for Gmail.app"
+  app "Boxy for Calendar.app"
+  app "Boxy for Keep.app"
+  app "Boxy for Contacts.app"
 end
