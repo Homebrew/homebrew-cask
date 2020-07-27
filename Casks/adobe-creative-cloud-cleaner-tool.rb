ecb68fe4ad1f05adceafb939852c592698e7d90a
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:13:34 +0100

    adobe-creative-cloud-cleaner-tool: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/adobe-creative-cloud-cleaner-tool.rb
+++ Casks/adobe-creative-cloud-cleaner-tool.rb
@@ -1,18 +1,18 @@
-cask 'adobe-creative-cloud-cleaner-tool' do
+cask "adobe-creative-cloud-cleaner-tool" do
   version :latest
   sha256 :no_check
 
-  url 'https://swupmf.adobe.com/webfeed/CleanerTool/mac/AdobeCreativeCloudCleanerTool.dmg'
-  name 'Adobe Creative Cloud Cleaner Tool'
-  homepage 'https://helpx.adobe.com/creative-cloud/kb/cc-cleaner-tool-installation-problems.html'
+  url "https://swupmf.adobe.com/webfeed/CleanerTool/mac/AdobeCreativeCloudCleanerTool.dmg"
+  name "Adobe Creative Cloud Cleaner Tool"
+  homepage "https://helpx.adobe.com/creative-cloud/kb/cc-cleaner-tool-installation-problems.html"
 
-  app 'Adobe Creative Cloud Cleaner Tool.app'
+  app "Adobe Creative Cloud Cleaner Tool.app"
 
-  uninstall quit: 'com.Adobe.Installers.AdobeCreativeCloudCleanerTool'
+  uninstall quit: "com.Adobe.Installers.AdobeCreativeCloudCleanerTool"
 
   zap trash: [
-               '~/Library/Caches/com.Adobe.Installers.AdobeCreativeCloudCleanerTool',
-               '~/Library/Saved Application State/com.Adobe.Installers.AdobeCreativeCloudCleanerTool.savedState',
-               '~/Library/Logs/Adobe Creative Cloud Cleaner Tool.log',
-             ]
+    "~/Library/Caches/com.Adobe.Installers.AdobeCreativeCloudCleanerTool",
+    "~/Library/Saved Application State/com.Adobe.Installers.AdobeCreativeCloudCleanerTool.savedState",
+    "~/Library/Logs/Adobe Creative Cloud Cleaner Tool.log",
+  ]
 end
