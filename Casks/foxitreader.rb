487c8ff347e3173eb723174907dfdee8c1689a9c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:24:34 +0100

    foxitreader: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/foxitreader.rb
+++ Casks/foxitreader.rb
@@ -1,17 +1,17 @@
-cask 'foxitreader' do
-  version '4.0.0'
-  sha256 '56ada239bdd4ca321581dec57f55d95f916f1b9a93321c4cda8090b82979a43a'
+cask "foxitreader" do
+  version "4.0.0"
+  sha256 "56ada239bdd4ca321581dec57f55d95f916f1b9a93321c4cda8090b82979a43a"
 
   url "https://cdn09.foxitsoftware.com/pub/foxit/reader/desktop/mac/#{version.major}.x/#{version.major_minor}/ML/FoxitReader#{version.no_dots}.setup.pkg"
-  appcast 'https://www.foxitsoftware.com/downloads/downloadForm.php?retJson=1&product=Foxit-Reader&platform=Mac-OS-X'
-  name 'Foxit Reader'
-  homepage 'https://www.foxitsoftware.com/pdf-reader/'
+  appcast "https://www.foxitsoftware.com/downloads/downloadForm.php?retJson=1&product=Foxit-Reader&platform=Mac-OS-X"
+  name "Foxit Reader"
+  homepage "https://www.foxitsoftware.com/pdf-reader/"
 
   pkg "FoxitReader#{version.no_dots}.setup.pkg"
 
   uninstall pkgutil: [
-                       'com.foxitsoftware.reader.pkg',
-                       'com.foxit.pkg.reader',
-                     ],
-            delete:  '/Applications/Foxit Reader.app'
+    "com.foxitsoftware.reader.pkg",
+    "com.foxit.pkg.reader",
+  ],
+            delete:  "/Applications/Foxit Reader.app"
 end
