9bff5fa10e3825e769055f6da66697ebcf5b8f38
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:22 +0100

    scenebuilder: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/scenebuilder.rb
+++ Casks/scenebuilder.rb
@@ -1,15 +1,15 @@
-cask 'scenebuilder' do
-  version '11.0.0'
-  sha256 '0602d1cd7156fa0ff49cc4055e6aec020227df813449b2bc3782a3fddffb43bb'
+cask "scenebuilder" do
+  version "11.0.0"
+  sha256 "0602d1cd7156fa0ff49cc4055e6aec020227df813449b2bc3782a3fddffb43bb"
 
   url "https://download2.gluonhq.com/scenebuilder/#{version}/install/mac/SceneBuilder-#{version}.pkg"
-  appcast 'https://gluonhq.com/products/scene-builder/#download'
-  name 'Scene Builder'
-  homepage 'https://gluonhq.com/products/scene-builder/'
+  appcast "https://gluonhq.com/products/scene-builder/#download"
+  name "Scene Builder"
+  homepage "https://gluonhq.com/products/scene-builder/"
 
-  depends_on macos: '>= :high_sierra'
+  depends_on macos: ">= :high_sierra"
 
   pkg "SceneBuilder-#{version}.pkg"
 
-  uninstall pkgutil: 'com.gluonhq.scenebuilder'
+  uninstall pkgutil: "com.gluonhq.scenebuilder"
 end
