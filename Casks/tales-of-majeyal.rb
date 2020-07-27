1b2f200f7efa85a0669fbd8faad9362340ea8a1c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:37:09 +0100

    tales-of-majeyal: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/tales-of-majeyal.rb
+++ Casks/tales-of-majeyal.rb
@@ -1,12 +1,12 @@
-cask 'tales-of-majeyal' do
-  version '1.6.7'
-  sha256 'da0c3a2d6d48ec29ad2b8c1c44478258ac1d19b8931965a380984eaa3675b5c6'
+cask "tales-of-majeyal" do
+  version "1.6.7"
+  sha256 "da0c3a2d6d48ec29ad2b8c1c44478258ac1d19b8931965a380984eaa3675b5c6"
 
   url "https://te4.org/dl/t-engine/t-engine4-osx-#{version}.zip"
-  appcast 'https://te4.org/download'
+  appcast "https://te4.org/download"
   name "Tales of Maj'Eyal"
-  homepage 'https://te4.org/'
+  homepage "https://te4.org/"
 
   # Renamed for clarity: app name is inconsistent with its branding.
-  app 'T-Engine.app', target: "Tales-of-Maj'Eyal.app"
+  app "T-Engine.app", target: "Tales-of-Maj'Eyal.app"
 end
