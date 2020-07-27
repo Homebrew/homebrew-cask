62fbbb8f5079dfdf7bee700722a90308d043a535
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:15:27 +0100

    airy: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/airy.rb
+++ Casks/airy.rb
@@ -1,15 +1,15 @@
-cask 'airy' do
-  version '3.17,297'
-  sha256 '487a41c03afe1d3e4ac8ef7f6cb050cda6c682135c707e8b796c8f0c4ea07e48'
+cask "airy" do
+  version "3.17,297"
+  sha256 "487a41c03afe1d3e4ac8ef7f6cb050cda6c682135c707e8b796c8f0c4ea07e48"
 
-  url 'https://cdn.eltima.com/download/airy.dmg'
-  appcast 'https://cdn.eltima.com/download/airy-update/airy.xml'
-  name 'Airy'
-  homepage 'https://mac.eltima.com/youtube-downloader-mac.html'
+  url "https://cdn.eltima.com/download/airy.dmg"
+  appcast "https://cdn.eltima.com/download/airy-update/airy.xml"
+  name "Airy"
+  homepage "https://mac.eltima.com/youtube-downloader-mac.html"
 
-  app 'Airy.app'
+  app "Airy.app"
 
-  uninstall quit: 'com.eltima.Airy'
+  uninstall quit: "com.eltima.Airy"
 
-  zap trash: '~/Library/Preferences/com.eltima.Airy.plist'
+  zap trash: "~/Library/Preferences/com.eltima.Airy.plist"
 end
