589eaf1924e38ca520b5594b92976c865f417ae0
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:27:18 +0100

    iridient-developer: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/iridient-developer.rb
+++ Casks/iridient-developer.rb
@@ -1,11 +1,11 @@
-cask 'iridient-developer' do
-  version '3.3.13'
-  sha256 'cc7015a73a41c5209643d5a8ccf06748e5e46e640b0dc889564f4d9c2a5202c0'
+cask "iridient-developer" do
+  version "3.3.13"
+  sha256 "cc7015a73a41c5209643d5a8ccf06748e5e46e640b0dc889564f4d9c2a5202c0"
 
   url "https://www.iridientdigital.com/downloads/IridientDeveloper_#{version.no_dots}.dmg"
-  appcast 'https://www.iridientdigital.com/products/rawdeveloper_history.html'
-  name 'Iridient Developer'
-  homepage 'https://www.iridientdigital.com/'
+  appcast "https://www.iridientdigital.com/products/rawdeveloper_history.html"
+  name "Iridient Developer"
+  homepage "https://www.iridientdigital.com/"
 
-  app 'Iridient Developer.app'
+  app "Iridient Developer.app"
 end
