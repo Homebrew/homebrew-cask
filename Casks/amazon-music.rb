1477b6834bf1ac358bcd731677df2d49a644586b
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:15:43 +0100

    amazon-music: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/amazon-music.rb
+++ Casks/amazon-music.rb
@@ -1,34 +1,34 @@
-cask 'amazon-music' do
-  version '7.12.0.2203,2005262203_1ee547b2e9653d9520168396897e3c22'
-  sha256 '7d31d7e568e5e2acd497fd2cb56c41fa68d89d7af804aaba5b95b6fb8cb6fa57'
+cask "amazon-music" do
+  version "7.12.0.2203,2005262203_1ee547b2e9653d9520168396897e3c22"
+  sha256 "7d31d7e568e5e2acd497fd2cb56c41fa68d89d7af804aaba5b95b6fb8cb6fa57"
 
   # morpho-releases.s3-us-west-2.amazonaws.com/mac/ was verified as official when first introduced to the cask
   url "https://morpho-releases.s3-us-west-2.amazonaws.com/mac/#{version.after_comma}/AmazonMusicInstaller.dmg"
-  appcast 'https://www.amazon.com/gp/dmusic/desktop/downloadPlayer',
+  appcast "https://www.amazon.com/gp/dmusic/desktop/downloadPlayer",
           must_contain: version.after_comma
-  name 'Amazon Music'
-  homepage 'https://www.amazon.com/musicapps'
+  name "Amazon Music"
+  homepage "https://www.amazon.com/musicapps"
 
   auto_updates true
 
   installer script: {
-                      executable: 'Amazon Music Installer.app/Contents/MacOS/installbuilder.sh',
-                    }
+    executable: "Amazon Music Installer.app/Contents/MacOS/installbuilder.sh",
+  }
 
   uninstall quit:      [
-                         'com.amazon.music',
-                         'com.amazon.music-renderer',
-                       ],
-            delete:    '/Applications/Amazon Music.app',
+    "com.amazon.music",
+    "com.amazon.music-renderer",
+  ],
+            delete:    "/Applications/Amazon Music.app",
             launchctl: [
-                         'com.amazon.music',
-                         'com.amazon.music.startup',
-                       ]
+              "com.amazon.music",
+              "com.amazon.music.startup",
+            ]
 
   zap trash: [
-               '~/Library/Preferences/com.amazon.music.plist',
-               '~/Library/Application Support/Amazon Music',
-             ]
+    "~/Library/Preferences/com.amazon.music.plist",
+    "~/Library/Application Support/Amazon Music",
+  ]
 
   caveats <<~EOS
     If the app will not launch after installation, try
