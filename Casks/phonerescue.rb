d3ee2757426d4feb53ed3202629d3b4e1fa34f68
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:33:17 +0100

    phonerescue: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/phonerescue.rb
+++ Casks/phonerescue.rb
@@ -1,10 +1,10 @@
-cask 'phonerescue' do
+cask "phonerescue" do
   version :latest
   sha256 :no_check
 
-  url 'https://dl.imobie.com/phonerescue-for-ios.zip'
-  name 'PhoneRescue'
-  homepage 'https://www.imobie.com/phonerescue/'
+  url "https://dl.imobie.com/phonerescue-for-ios.zip"
+  name "PhoneRescue"
+  homepage "https://www.imobie.com/phonerescue/"
 
-  app 'PhoneRescue.app'
+  app "PhoneRescue.app"
 end
