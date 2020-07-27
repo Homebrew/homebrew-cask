708040f128886cf46b18c7471af83a6f7161a73f
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:35:49 +0100

    shutter-encoder: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/shutter-encoder.rb
+++ Casks/shutter-encoder.rb
@@ -1,11 +1,11 @@
-cask 'shutter-encoder' do
-  version '14.0'
-  sha256 '27ca443dd502efe5ef8527d629b8b4ef270c756590a084b57a8f59fbe836d600'
+cask "shutter-encoder" do
+  version "14.0"
+  sha256 "27ca443dd502efe5ef8527d629b8b4ef270c756590a084b57a8f59fbe836d600"
 
   url "https://www.shutterencoder.com/Shutter%20Encoder%20(MAC%20Version%20#{version}).zip"
-  appcast 'https://www.shutterencoder.com/changelog.txt'
-  name 'Shutter Encoder'
-  homepage 'https://www.shutterencoder.com/'
+  appcast "https://www.shutterencoder.com/changelog.txt"
+  name "Shutter Encoder"
+  homepage "https://www.shutterencoder.com/"
 
-  app 'Shutter Encoder.app'
+  app "Shutter Encoder.app"
 end
