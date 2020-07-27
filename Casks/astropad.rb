75f5ee4adfc23a1e7ea71931eb45a1fa1d507d8e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:16:45 +0100

    astropad: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/astropad.rb
+++ Casks/astropad.rb
@@ -1,20 +1,20 @@
-cask 'astropad' do
-  version '3.4.1'
-  sha256 '6edacd8c0985cd51304618a1ea9963a0a71fdf09be7af56d7d9c336b94b6ff3a'
+cask "astropad" do
+  version "3.4.1"
+  sha256 "6edacd8c0985cd51304618a1ea9963a0a71fdf09be7af56d7d9c336b94b6ff3a"
 
   url "https://downloads.astropad.com/standard/Astropad-#{version}.dmg"
-  appcast 'https://s3.amazonaws.com/astropad.com/downloads/sparkle.xml'
-  name 'Astropad'
-  homepage 'https://astropad.com/'
+  appcast "https://s3.amazonaws.com/astropad.com/downloads/sparkle.xml"
+  name "Astropad"
+  homepage "https://astropad.com/"
 
-  app 'Astropad.app'
+  app "Astropad.app"
 
-  uninstall quit: 'com.astro-hq.AstropadMac'
+  uninstall quit: "com.astro-hq.AstropadMac"
 
   zap trash: [
-               '~/Library/Caches/Astropad',
-               '~/Library/Caches/com.astro-hq.AstropadMac',
-               '~/Library/Preferences/com.astro-hq.AstropadMac.plist',
-               '~/Library/Saved Application State/com.astro-hq.AstropadMac.savedState',
-             ]
+    "~/Library/Caches/Astropad",
+    "~/Library/Caches/com.astro-hq.AstropadMac",
+    "~/Library/Preferences/com.astro-hq.AstropadMac.plist",
+    "~/Library/Saved Application State/com.astro-hq.AstropadMac.savedState",
+  ]
 end
