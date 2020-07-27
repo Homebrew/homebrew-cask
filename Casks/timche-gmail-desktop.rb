6582fec5a679bfc5984b8191d454c08e2b79bb30
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:37:32 +0100

    timche-gmail-desktop: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Casks/timche-gmail-desktop.rb
+++ Casks/timche-gmail-desktop.rb
@@ -1,18 +1,18 @@
-cask 'timche-gmail-desktop' do
-  version '2.20.1'
-  sha256 'b49e8b39b46702a0e6c32f57e23d16ec00f48d26a493a2a0f5017a7811329fb6'
+cask "timche-gmail-desktop" do
+  version "2.20.1"
+  sha256 "b49e8b39b46702a0e6c32f57e23d16ec00f48d26a493a2a0f5017a7811329fb6"
 
   url "https://github.com/timche/gmail-desktop/releases/download/v#{version}/gmail-desktop-#{version}-mac.dmg"
-  appcast 'https://github.com/timche/gmail-desktop/releases.atom'
-  name 'Gmail Desktop'
-  homepage 'https://github.com/timche/gmail-desktop'
+  appcast "https://github.com/timche/gmail-desktop/releases.atom"
+  name "Gmail Desktop"
+  homepage "https://github.com/timche/gmail-desktop"
 
-  app 'Gmail Desktop.app'
+  app "Gmail Desktop.app"
 
   zap trash: [
-               '~/Library/Logs/Gmail Desktop',
-               '~/Library/Preferences/io.cheung.gmail-desktop.plist',
-               '~/Library/Application Support/Gmail Desktop',
-               '~/Library/Saved Application State/io.cheung.gmail-desktop.savedState',
-             ]
+    "~/Library/Logs/Gmail Desktop",
+    "~/Library/Preferences/io.cheung.gmail-desktop.plist",
+    "~/Library/Application Support/Gmail Desktop",
+    "~/Library/Saved Application State/io.cheung.gmail-desktop.savedState",
+  ]
 end
