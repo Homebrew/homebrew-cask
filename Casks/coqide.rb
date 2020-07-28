365df3c964886c22dbd31578d1534bbb49a98112
Hanlin He <hanlhe@cisco.com>
2020-07-28 14:33:53 -0700

    Update coqide from 8.11.2 to 8.12.0 (#86737)

--- Casks/coqide.rb
+++ Casks/coqide.rb
@@ -1,6 +1,6 @@
 cask "coqide" do
-  version "8.11.2"
-  sha256 "18c09fbafaa2731bb7f6cff0b8e821b57cf6857d1bd1609055d0784d445c6d53"
+  version "8.12.0"
+  sha256 "be5bf7b8009e0693f5da35437a901c58a924c12c9464c5be3ff1ed3ebe495697"
 
   # github.com/coq/coq/ was verified as official when first introduced to the cask
   url "https://github.com/coq/coq/releases/download/V#{version.major_minor_patch}/coq-#{version}-installer-macos.dmg"
