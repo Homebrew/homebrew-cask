a7ac3380d99ea7184b1fe8aed767cb347fcb3824
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-27 22:53:57 -0700

    Update visual-paradigm-ce from 16.1,20200609 to 16.2,20200763 (#86660)

--- Casks/visual-paradigm-ce.rb
+++ Casks/visual-paradigm-ce.rb
@@ -1,6 +1,6 @@
 cask "visual-paradigm-ce" do
-  version "16.1,20200609"
-  sha256 "caed2b387d15f9365a1fa8b0c7033c5fc163bef11305cb9f21643aa1abbcc299"
+  version "16.2,20200763"
+  sha256 "b0276abaff42d3fe933b01ee9d8555ae1b4941bcdc4f519d4384905b81e9b239"
 
   url "https://www.visual-paradigm.com/downloads/vpce/Visual_Paradigm_CE_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
   appcast "https://www.visual-paradigm.com/downloads/vpce/checksum.html",
