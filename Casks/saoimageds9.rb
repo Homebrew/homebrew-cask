28720c08cabf99d2cc401dc126ced4d7d592ae50
CoreCode <core-code@users.noreply.github.com>
2020-07-28 09:51:30 +0200

    Update saoimageds9.rb (#86619)

--- Casks/saoimageds9.rb
+++ Casks/saoimageds9.rb
@@ -1,11 +1,11 @@
 cask "saoimageds9" do
+  # note: "9" is not a version number, but an intrinsic part of the product name
   version "8.1"
 
-  case MacOS.version
-  when :high_sierra
+  if MacOS.version <= :high_sierra
     sha256 "7cd4e49c47b30c3dbe13ef69a8d0577beaf241376c0cd77c4f8ec55afffebb9f"
     url "https://ds9.si.edu/download/macoshighsierra/SAOImageDS9%20#{version}.dmg"
-  when :mojave
+  elsif MacOS.version <= :mojave
     sha256 "bf95a1001189c029f4e134b2373375151f6c56415befce38f80e605801f0e7ef"
     url "https://ds9.si.edu/download/macosmojave/SAOImageDS9%20#{version}.dmg"
   else
