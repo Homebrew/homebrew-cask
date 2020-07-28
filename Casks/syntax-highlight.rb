7099b6314b97ca78887b34f2a5b64ecdfa59fb1e
i0ntempest <szf1234@me.com>
2020-07-28 08:18:22 -0400

    Update syntax-highlight from 1.0.b25 to 1.0.b26

--- Casks/syntax-highlight.rb
+++ Casks/syntax-highlight.rb
@@ -1,6 +1,6 @@
 cask "syntax-highlight" do
-  version "1.0.b25"
-  sha256 "a4a65e1c1ca7ac775296c7ae264ab2066cc315e68b3332f2fa5a1e3931439ab1"
+  version "1.0.b26"
+  sha256 "af4444b1604e488efbd207eb9a9e20c59be6848705d93aefb8d85803914d35f8"
 
   url "https://github.com/sbarex/SourceCodeSyntaxHighlight/releases/download/#{version}/Syntax.Highlight.zip"
   appcast "https://github.com/sbarex/SourceCodeSyntaxHighlight/releases.atom"
