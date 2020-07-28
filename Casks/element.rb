d2b16e87c7411adc9dec7bb35eb4ae07715189ec
Yurii Kolesnykov <root@yurikoles.com>
2020-07-28 10:11:37 +0300

    Update element from 1.7.1 to 1.7.2
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/element.rb
+++ Casks/element.rb
@@ -1,20 +1,21 @@
 cask "element" do
-  version "1.7.1"
-  sha256 "7ee9e2c1fd455ee887b0ca224dc275a4dca8ee48fb37471556f49d38da41476a"
+  version "1.7.2"
+  sha256 "f3d628019a67f9b4b6239b44b49e1aa76b3401b40d7e022c495d210ddc8bf71f"
 
   # packages.riot.im/desktop was verified as official when first introduced to the cask
-  url "https://packages.riot.im/desktop/install/macos/Element%20(Riot)-#{version}.dmg"
+  url "https://packages.riot.im/desktop/install/macos/Element-#{version}.dmg"
   appcast "https://github.com/vector-im/riot-desktop/releases.atom"
   name "Element (Riot)"
   homepage "https://element.io/get-started"
 
   auto_updates true
 
-  app "Element (Riot).app"
+  app "Element.app"
 
   zap trash: [
-    "~/Library/Application Support/Riot",
     "~/Library/Application Support/Element (Riot)",
+    "~/Library/Application Support/Element",
+    "~/Library/Application Support/Riot",
     "~/Library/Caches/im.riot.app",
     "~/Library/Caches/im.riot.app.ShipIt",
     "~/Library/Logs/Riot",
