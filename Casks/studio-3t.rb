3a28114eb622b4c2b3a84395c8fd3c166763216b
Randall <ran.dall@icloud.com>
2020-07-28 02:19:38 -0700

    Update studio-3t from 2020.6.1 to 2020.7.0

--- Casks/studio-3t.rb
+++ Casks/studio-3t.rb
@@ -1,6 +1,6 @@
 cask "studio-3t" do
-  version "2020.6.1"
-  sha256 "ed351b3726e2d78f45721d7d68fa72589badaf92732f1062de3c51b1505beacb"
+  version "2020.7.0"
+  sha256 "e60002d916f075aa6c540b82da6995325251d17996ce29dae84e818b01a9b2b6"
 
   url "https://download.studio3t.com/studio-3t/mac/#{version}/Studio-3T.dmg"
   appcast "https://files.studio3t.com/changelog/changelog.txt"
