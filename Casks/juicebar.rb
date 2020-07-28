cadacafeb6cba61416b8034631a15e53dd97309e
Randall <ran.dall@icloud.com>
2020-07-27 21:42:07 -0700

    Update juicebar from 1.0.62 to 1.0.63

--- Casks/juicebar.rb
+++ Casks/juicebar.rb
@@ -1,6 +1,6 @@
 cask "juicebar" do
-  version "1.0.62"
-  sha256 "3bac0d3df6cd68bf29813b4e2abf7cc730a106998ddd45071d386d773c1b56ed"
+  version "1.0.63"
+  sha256 "0947fe502aace9e0472c14bada8c4ad4a4698a56d2a2e3ee389534849100cd74"
 
   url "https://mango.get-juicebar.com/v#{version.major}/bundles/macOS/latest"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://mango.get-juicebar.com/v#{version.major}/bundles/macOS/latest"
