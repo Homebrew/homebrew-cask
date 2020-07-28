ada8cccb8904718f7bb04e609b1404958a563977
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-28 13:37:58 -0700

    Update airy from 3.17,297 to 3.18,301 (#86723)

--- Casks/airy.rb
+++ Casks/airy.rb
@@ -1,6 +1,6 @@
 cask "airy" do
-  version "3.17,297"
-  sha256 "487a41c03afe1d3e4ac8ef7f6cb050cda6c682135c707e8b796c8f0c4ea07e48"
+  version "3.18,301"
+  sha256 "deb9e45c82e1ae17ea8f05034d79fd253679ca07eadac1863d2e1a4d892e8c8b"
 
   url "https://cdn.eltima.com/download/airy.dmg"
   appcast "https://cdn.eltima.com/download/airy-update/airy.xml"
