16d22e8ca9fddeeec598c3b0c4cb9f33d035a07c
Randall <ran.dall@icloud.com>
2020-07-28 13:11:26 -0700

    Update marsedit from 4.3.7 to 4.4

--- Casks/marsedit.rb
+++ Casks/marsedit.rb
@@ -1,6 +1,6 @@
 cask "marsedit" do
-  version "4.3.7"
-  sha256 "dab353d0abd0e5f09efc91ccc4d9845c141bd8100e3b44286e31294974b52022"
+  version "4.4"
+  sha256 "92847eca5649183fcb190db8f5cad2f56546a7425d51fe1119c23579b254b74f"
 
   url "https://red-sweater.com/marsedit/MarsEdit#{version}.zip"
   appcast "https://red-sweater.com/marsedit/appcast4.php"
