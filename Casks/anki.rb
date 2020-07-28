29a6e2c2fa02490ba36020ab7538f0c621fcd724
desimaniac <5501908+desimaniac@users.noreply.github.com>
2020-07-28 00:33:15 -0500

    Update anki from 2.1.28 to 2.1.29

--- Casks/anki.rb
+++ Casks/anki.rb
@@ -1,19 +1,19 @@
-cask "anki" do
-  version "2.1.28"
-  sha256 "c4eec38d80804fcd9e82d24a4ee5d4fbc581b8f5de1e1f08056f80b4bef60a2d"
+cask 'anki' do
+  version '2.1.29'
+  sha256 'b3fc61553b62c014d979ad738c8eca1713a9fc9dcc8108203063b181ace4ba31'
 
   # github.com/ankitects/anki/ was verified as official when first introduced to the cask
   url "https://github.com/ankitects/anki/releases/download/#{version}/anki-#{version}-mac.dmg"
-  appcast "https://changes.ankiweb.net/README.md"
-  name "Anki"
-  homepage "https://apps.ankiweb.net/"
+  appcast 'https://changes.ankiweb.net/README.md'
+  name 'Anki'
+  homepage 'https://apps.ankiweb.net/'
 
-  depends_on macos: ">= :sierra"
+  depends_on macos: '>= :sierra'
 
-  app "Anki.app"
+  app 'Anki.app'
 
   zap trash: [
-    "~/Library/Application Support/Anki",
-    "~/Library/Application Support/Anki2",
-  ]
+               '~/Library/Application Support/Anki',
+               '~/Library/Application Support/Anki2',
+             ]
 end
