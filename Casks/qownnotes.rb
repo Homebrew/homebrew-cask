256979d6b360ecc44d7500ebb426563791ae58de
Randall <ran.dall@icloud.com>
2020-07-28 12:57:44 -0700

    Update qownnotes from 20.7.8 to 20.7.9

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,6 +1,6 @@
 cask "qownnotes" do
-  version "20.7.8"
-  sha256 "242803ac1cb996e54c9ba3177506fee1e5b286f04417346c3bf15e091b6a13a9"
+  version "20.7.9"
+  sha256 "3701aa7b3329f7d68c46b534d9309f66708350df0883855ad562d2801d80b222"
 
   # github.com/pbek/QOwnNotes/ was verified as official when first introduced to the cask
   url "https://github.com/pbek/QOwnNotes/releases/download/v#{version}/QOwnNotes.dmg"
