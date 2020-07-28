55159838a84dec753c79b9e39d6bab54d926fd2b
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-27 22:08:16 -0700

    Update nordvpn-teams from 1.4.1 to 1.5.0 (#86640)

--- Casks/nordvpn-teams.rb
+++ Casks/nordvpn-teams.rb
@@ -1,6 +1,6 @@
 cask "nordvpn-teams" do
-  version "1.4.1"
-  sha256 "ed25d9da5e34aed726599413b1c87360994040ba74f20ea7d29383ed6b801d45"
+  version "1.5.0"
+  sha256 "3a82fbe8bddbb6c0e144a38000b90d4020a83ff578205e356f86c53f63580811"
 
   # downloads.nordteams.com/ was verified as official when first introduced to the cask
   url "https://downloads.nordteams.com/mac/latest/NordVPNTeams.pkg"
