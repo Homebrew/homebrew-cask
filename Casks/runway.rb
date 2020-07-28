ef9db3c6779741088459b1e46b540ac1d0541f17
Randall <ran.dall@icloud.com>
2020-07-27 22:08:34 -0700

    Update runway from 0.13.7 to 0.13.8

--- Casks/runway.rb
+++ Casks/runway.rb
@@ -1,6 +1,6 @@
 cask "runway" do
-  version "0.13.7"
-  sha256 "6f442d9330ac3273abf568c33fce75a96378390f1d6af7dedcb90b620dba53f5"
+  version "0.13.8"
+  sha256 "1920728c5feaae7f075979f85f58e493a2d8f55b541a2cbb62beafbfa07532f6"
 
   # runway-releases.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://runway-releases.s3.amazonaws.com/Runway-#{version}.dmg"
