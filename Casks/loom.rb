947e40ef7a3814f3ae0822b4b0cb72f3832dc62c
Brigitte Zmoelnig <brigitte@corecode.io>
2020-07-29 22:50:01 +0200

    Update loom from 0.43.2 to 0.44.0

--- Casks/loom.rb
+++ Casks/loom.rb
@@ -1,6 +1,6 @@
 cask "loom" do
-  version "0.43.2"
-  sha256 "457ba90efc9d452f268636d1b35d602f593f97f51452ca67686ba56cd86850ee"
+  version "0.44.0"
+  sha256 "15fd88357412bc10cf969e3e0e27dd891a4c40bafb671e38219cc9084429abcc"
 
   url "https://cdn.loom.com/desktop-packages/Loom-#{version}.dmg"
   appcast "https://s3-us-west-2.amazonaws.com/loom.desktop.packages/loom-inc-production/desktop-packages/latest-mac.yml"
