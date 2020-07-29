16cb67f6d952142ab4baa0679cd15817daa2859f
Aadit Kamat <aadit.k12@gmail.com>
2020-07-29 21:32:55 +0800

    Add version and sha256 checksum

--- Casks/surfshark.rb
+++ Casks/surfshark.rb
@@ -1,6 +1,6 @@
 cask 'surfshark' do
-  version :latest
-  sha256 :no_check
+  version '2.5.1'
+  sha256 '8ed99b9179fd9bbc8b5e80ee8c5e818bbc16fb34ddb6cf6ae64fca16a76ebf3d'
 
   url 'https://downloads.surfshark.com/macOS/latest/Surfshark.dmg'
   name 'Surfshark'
