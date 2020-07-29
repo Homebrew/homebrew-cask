5a2cac5dfd9fd32cb30b8d6be15c2d7a4cf0cd26
Peter Lewis <peter@peterlew.is>
2020-07-29 17:56:41 +0100

    Add macintosh.js (#86756)
    * Create macintoshjs.rb
    
    * Update macintoshjs.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

---
+++ Casks/macintoshjs.rb
@@ -0,0 +1,11 @@
+cask "macintoshjs" do
+  version "1.0.2"
+  sha256 "7d6494b14cb895e6540b3a09608269bba2b97a3d8bf784d56c45f6da558bffbf"
+
+  url "https://github.com/felixrieseberg/macintosh.js/releases/download/v#{version}/macintosh.js-darwin-x64-#{version}.zip"
+  appcast "https://github.com/felixrieseberg/macintosh.js/releases.atom"
+  name "macintosh.js"
+  homepage "https://github.com/felixrieseberg/macintosh.js"
+
+  app "macintosh.js.app"
+end
