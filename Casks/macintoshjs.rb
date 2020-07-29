d86894e2c9bc6580b6ea4d9cfd3bd480811a5df2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 17:56:32 +0100

    Update macintoshjs.rb

--- Casks/macintoshjs.rb
+++ Casks/macintoshjs.rb
@@ -2,7 +2,7 @@ cask "macintoshjs" do
   version "1.0.2"
   sha256 "7d6494b14cb895e6540b3a09608269bba2b97a3d8bf784d56c45f6da558bffbf"
 
-  url "https://github.com/felixrieseberg/macintosh.js/releases/download/v1.0.2/macintosh.js-darwin-x64-#{version}.zip"
+  url "https://github.com/felixrieseberg/macintosh.js/releases/download/v#{version}/macintosh.js-darwin-x64-#{version}.zip"
   appcast "https://github.com/felixrieseberg/macintosh.js/releases.atom"
   name "macintosh.js"
   homepage "https://github.com/felixrieseberg/macintosh.js"
