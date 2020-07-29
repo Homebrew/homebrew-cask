7b7665db4f2a7d93089894ae53d23933d98c5885
Gustavo Leal <gustavo.soares.leal@gmail.com>
2020-07-29 18:46:57 -0300

    Adding Jamulus v3.5.9
    Adding Jamulus and JamulusServer v3.5.9

---
+++ Casks/jamulus.rb
@@ -0,0 +1,15 @@
+cask "jamulus" do
+  version "3.5.9"
+  sha256 "c11299ad62f603f70ada5f4d15c9c7cd4864db598069bf6ee19c484c24048b78"
+
+  # downloads.sourceforge.net/llcon/ was verified as official when first introduced to the cask
+  url "https://downloads.sourceforge.net/llcon/Jamulus-#{version}-installer-mac.dmg"
+  appcast "https://sourceforge.net/projects/llcon/rss"
+  name "Jamulus"
+  homepage "http://llcon.sourceforge.net/"
+
+  auto_updates true
+
+  app "Jamulus.app"
+  app "JamulusServer.app"
+end
