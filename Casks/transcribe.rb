674686a8fb5cfe034bd10d130f0b23ab5400ff9b
David G Rosenberg <targendaz2@me.com>
2020-07-29 00:40:48 -0400

    Add Transcribe!.app v8.75

---
+++ Casks/transcribe.rb
@@ -0,0 +1,13 @@
+cask 'transcribe' do
+  version '8.75'
+  sha256 '6d6ff7c8975629d9b511f1d82ba6dbc81f60899ab4404fc29f9e1208042c68fe'
+
+  url 'https://www.seventhstring.com/xscribe/transcribe.dmg'
+  appcast 'https://www.seventhstring.com/xscribe/history.html'
+  name 'Transcribe!'
+  homepage 'https://www.seventhstring.com/xscribe/overview.html'
+
+  app 'Transcribe!.app'
+
+  uninstall quit: 'com.seventhstring.transcribe'
+end
