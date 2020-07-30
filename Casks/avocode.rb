e4dad9b1b6ee2aacf80617efb4facbeca1f90912
Randall <ran.dall@icloud.com>
2020-07-29 19:44:07 -0700

    Update avocode from 4.8.0 to 4.8.1

--- Casks/avocode.rb
+++ Casks/avocode.rb
@@ -1,6 +1,6 @@
 cask "avocode" do
-  version "4.8.0"
-  sha256 "b001e5217a8911c447d198f574f32e134cfe1960733a9236553883ace70a84cd"
+  version "4.8.1"
+  sha256 "39b2f42e88909f62709357d5d4ecb1aad521fbeecf7ba4154124f1947112a828"
 
   url "https://media.avocode.com/download/avocode-app/#{version}/Avocode-#{version}-mac.zip"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://manager.avocode.com/download/avocode-app/mac-dmg/"
