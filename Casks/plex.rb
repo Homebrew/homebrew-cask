c0879d904866cb6b428de936084d68fc999a7c42
Phuc Nguyen Minh <minhphuc429@gmail.com>
2020-07-30 11:42:47 +0700

    Update plex from 1.15.1.1358-43550df5 to 1.16.0.1364-da192ff7 (#86793)

--- Casks/plex.rb
+++ Casks/plex.rb
@@ -1,6 +1,6 @@
 cask "plex" do
-  version "1.15.1.1358-43550df5"
-  sha256 "221e390c66c93c0d0618249d03338af5f4e11b5949f0aec34cdf7c7ac0d758d5"
+  version "1.16.0.1364-da192ff7"
+  sha256 "c835dd1bc104a65cea74561f1fd4b5f758f2f4a22d36c53e71d9a43680749e87"
 
   url "https://downloads.plex.tv/plex-desktop/#{version}/macos/Plex-#{version}-x86_64.zip"
   appcast "https://plex.tv/api/downloads/6.json"
@@ -8,7 +8,7 @@ cask "plex" do
   homepage "https://www.plex.tv/"
 
   auto_updates true
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :high_sierra"
 
   app "Plex.app"
 
