2664b51bd0aa64816af156504848b69c6cb3ed7b
Randall <ran.dall@icloud.com>
2020-07-29 19:26:22 -0700

    Update quicken from 5.17.2,517.34845.100 to 5.17.3,517.34913.100

--- Casks/quicken.rb
+++ Casks/quicken.rb
@@ -1,6 +1,6 @@
 cask "quicken" do
-  version "5.17.2,517.34845.100"
-  sha256 "5d738e15420152f36e16fdc01fe5e7cbcc0bbbc5bf24e6b1df618c6e9cc56a16"
+  version "5.17.3,517.34913.100"
+  sha256 "bc2e0c587d183749141eb19c3d7f7000d90bc35a571b3cb8b29731c0fa707a14"
 
   url "https://download.quicken.com/mac/Quicken/001/Release/031A96D9-EFE6-4520-8B6A-7F465DDAA3E4/Quicken-#{version.after_comma}/Quicken-#{version.after_comma}.zip"
   appcast "https://download.quicken.com/mac/Quicken/001/Release/031A96D9-EFE6-4520-8B6A-7F465DDAA3E4/appcast.xml"
