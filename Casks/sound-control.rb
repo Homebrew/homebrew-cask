677a6c87602035ce6cff6a8c8cdea4e15d62ba9b
plakak <a.krasnicki@gmail.com>
2020-07-30 18:57:17 +0200

    Update sound-control from 2.4.3,5156 to 2.5.0

--- Casks/sound-control.rb
+++ Casks/sound-control.rb
@@ -1,6 +1,6 @@
 cask "sound-control" do
-  version "2.4.3,5156"
-  sha256 "2f86d4f9c30b122dbc5b79c182b0d9b19492839919c032268d2632e7a3d5a8e2"
+  version "2.5.0"
+  sha256 "2c327d85678ab0a15c4603c90fcdeac65b43b6488df1e99b2e58213175dfd6ec"
 
   # staticz.net/ was verified as official when first introduced to the cask
   url "https://staticz.com/download/#{version.after_comma}/"
