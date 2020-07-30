d060158ea24fb32081ae788b9d329cb7349b0699
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-29 19:36:56 -0700

    Update sqlectron from 1.30.0 to 1.31.0 (#86779)

--- Casks/sqlectron.rb
+++ Casks/sqlectron.rb
@@ -1,6 +1,6 @@
 cask "sqlectron" do
-  version "1.30.0"
-  sha256 "a31c2c0cd892d814590ebe2cca5f18578e534a75e55d9ac07ac0f71b182cb3da"
+  version "1.31.0"
+  sha256 "3f572fba0ee5f07c919e8181b9f121d6380b6f367b3362a819ce8c3616f35dd5"
 
   # github.com/sqlectron/sqlectron-gui/ was verified as official when first introduced to the cask
   url "https://github.com/sqlectron/sqlectron-gui/releases/download/v#{version}/Sqlectron-#{version}-mac.zip"
