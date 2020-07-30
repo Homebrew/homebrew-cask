e4c6ff654de1359512fade8e20d74b4c5a9c2351
Stephen Jennings <stephen.g.jennings@gmail.com>
2020-07-30 15:44:18 -0700

    add kdiff3 v1.8.3

---
+++ Casks/kdiff3.rb
@@ -0,0 +1,32 @@
+cask "kdiff3" do
+  # note: "3" is not a version number, but an intrinsic part of the product name (3-way diff)
+  version "1.8.3"
+  sha256 "b6ad193fb01392f6dd05b99989c86c578054d97961753b97a20855453e33708a"
+
+  url "https://download.kde.org/stable/kdiff3/kdiff3-#{version}-macos-64.dmg"
+  appcast "https://invent.kde.org/sdk/kdiff3/-/tags?format=atom"
+  name "KDiff3"
+  homepage "https://invent.kde.org/sdk/kdiff3"
+
+  app "kdiff3.app"
+  shimscript = "#{staged_path}/kdiff3.wrapper.sh"
+  binary shimscript, target: "kdiff3"
+
+  preflight do
+    IO.write shimscript, <<~EOS
+      #!/bin/bash
+      '#{appdir}/kdiff3.app/Contents/MacOS/kdiff3' "$@"
+    EOS
+  end
+
+  zap trash: "~/.kdiff3rc"
+
+  caveats <<~END
+    kdiff3 is not signed, so you will need to reauthorize the app to run every
+    time the app is updated.
+
+    Run the app once. If macOS blocks it from running, go to:
+      System Preferences → Security & Privacy → General
+    To re-enable, click Open Anyway.
+  END
+end
