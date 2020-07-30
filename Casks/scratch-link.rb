20c8d7d2e44ec7358cc22cdc7cb65a03b741e89a
Pierre Beauhaire <bjr@bhre.me>
2020-07-30 16:41:39 +0200

    add scratch-link cask
    version '1.3.57'
    
    Scratch Link is a helper application which allows Scratch 3.0 to communicate with hardware peripherals.
    Scratch Link replaces the Scratch Device Manager and Scratch Device Plug-in.
    It is open-sourced (BSD) since 2019.
    
    The source code is available here
    https://github.com/LLK/scratch-link
    
    To learn more about Scratch & Scratch Link go here
    https://scratch.mit.edu/microbit
    
    This is also where the zip/pkg download link is provided
    https://downloads.scratch.mit.edu/link/mac.zip

---
+++ Casks/scratch-link.rb
@@ -0,0 +1,15 @@
+cask "scratch-link" do
+  version "1.3.57"
+  sha256 "58e9e3a5745bc65e6920bed934eb952e29b184022a9c6d8942dfcdf13b644c9d"
+
+  # downloads.scratch.mit.edu was verified as official when first introduced to the cask
+  url "https://downloads.scratch.mit.edu/link/mac.zip"
+  name "Scratch Link"
+  homepage "https://github.com/LLK/scratch-link"
+
+  license :bsd
+
+  pkg "scratch-link-#{version}.pkg"
+
+  uninstall pkgutil: "edu.mit.scratch.scratch-link"
+end
