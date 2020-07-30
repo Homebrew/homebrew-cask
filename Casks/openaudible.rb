e713131c90f31cc03ab9e8d5a31ea08ea850d698
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-29 19:34:20 -0700

    Update openaudible from 2.2 to 2.3 (#86782)

--- Casks/openaudible.rb
+++ Casks/openaudible.rb
@@ -1,6 +1,6 @@
 cask "openaudible" do
-  version "2.2"
-  sha256 "388569c2054184e7e84ffba3174e1ad7e6dca7c531f061153b200871483c4bed"
+  version "2.3"
+  sha256 "fde17fed3b36006653f39cbb3e7f513de5d06f6e61d67ea08e55ce4a0612c099"
 
   # github.com/openaudible/ was verified as official when first introduced to the cask
   url "https://github.com/openaudible/openaudible/releases/download/v#{version}/OpenAudible_#{version}_mac.dmg"
