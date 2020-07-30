04f7d31ef597a843ba7552995497f6fd2092cc0b
Sascha Peilicke <sascha@peilicke.de>
2020-07-30 14:17:11 +0200

    Update onlyoffice from 5.5.1 to 5.6

--- Casks/onlyoffice.rb
+++ Casks/onlyoffice.rb
@@ -1,6 +1,6 @@
 cask "onlyoffice" do
-  version "5.5.1"
-  sha256 "7090144f0d06328a7a1de43ea779c3db32b1afd7358295ca71ff2fbddb03601b"
+  version "5.6"
+  sha256 "e077a7b6b4ec642e24817dffdfafb48efb9d069fa340677a853bfa29dd5488f0"
 
   url "https://download.onlyoffice.com/install/desktop/editors/mac/updates/onlyoffice/ONLYOFFICE-#{version}.zip"
   appcast "https://download.onlyoffice.com/install/desktop/editors/mac/onlyoffice.xml"
