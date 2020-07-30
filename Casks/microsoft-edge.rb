ceeea00d4aaf302aa2db7f56fda473d66f50a0c0
秦新 <qinxin_0720@live.com>
2020-07-30 16:00:01 +0800

    Update microsoft-edge from 84.0.522.48 to 84.0.522.49 (#86771)

--- Casks/microsoft-edge.rb
+++ Casks/microsoft-edge.rb
@@ -1,6 +1,6 @@
 cask "microsoft-edge" do
-  version "84.0.522.48"
-  sha256 "8e9d6efc679db9073cd40efcb7ccd94955754028f2cf1b5be0d781c2164ec765"
+  version "84.0.522.49"
+  sha256 "fb495ae9f6db14ec66b858ec552809d26395d500977de3ff82823cd42502ef4c"
 
   # officecdn-microsoft-com.akamaized.net/ was verified as official when first introduced to the cask
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdge-#{version}.pkg"
@@ -12,7 +12,8 @@ cask "microsoft-edge" do
 
   pkg "MicrosoftEdge-#{version}.pkg"
 
-  uninstall pkgutil: "com.microsoft.edgemac",
+  uninstall quit:    "com.microsoft.autoupdate.fba",
+            pkgutil: "com.microsoft.edgemac",
             rmdir:   "/Library/Application Support/Microsoft"
 
   zap launchctl: [
