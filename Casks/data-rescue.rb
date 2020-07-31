c8e97723bb38bee8da92b371f16f4339eb23f668
suschizu <brigitte@corecode.io>
2020-07-31 21:06:47 +0200

    Update data-rescue from 5.0.11 to 6.0.0 (#86894)

--- Casks/data-rescue.rb
+++ Casks/data-rescue.rb
@@ -1,6 +1,6 @@
 cask "data-rescue" do
-  version "5.0.11"
-  sha256 "04025ee898a72c68b809e9722a197dd395b4e1100a9b5b2aafc35fe704749c87"
+  version "6.0.0"
+  sha256 "3b317918752603a116d34126fe5ef1894de7370b747046438a18b4295dce2fd9"
 
   url "https://downloads.prosofteng.com/dr/Data_Rescue_#{version}.dmg"
   appcast "https://www.prosofteng.com/resources/dr#{version.major}/dr#{version.major}_updates_mac.xml"
