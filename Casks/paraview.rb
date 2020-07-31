59b82645a4601d2006a2d4194beaac571013b68b
Randall <ran.dall@icloud.com>
2020-07-31 16:56:32 -0700

    Update paraview from 5.8.0 to 5.8.1

--- Casks/paraview.rb
+++ Casks/paraview.rb
@@ -1,6 +1,6 @@
 cask "paraview" do
-  version "5.8.0"
-  sha256 "5cb92137ee86241004f5055dd6e5e9c67ebb6581771553ed1275e7050d35027c"
+  version "5.8.1"
+  sha256 "50fed2e654438398786809d4d25eff9fd7a5487eba9f0320b81cdab10dbe9436"
 
   url "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v#{version.major_minor}&type=binary&os=macOS&downloadFile=ParaView-#{version}-MPI-OSX10.12-Python2.7-64bit.dmg",
       user_agent: :fake
