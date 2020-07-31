04c9173c7c61c9be0f73a642ca7ef8b251339919
CoreCode <core-code@users.noreply.github.com>
2020-08-01 01:08:57 +0200

    Update daily-english-listening.rb

--- Casks/daily-english-listening.rb
+++ Casks/daily-english-listening.rb
@@ -3,9 +3,9 @@ cask 'daily-english-listening' do
   sha256 '44dc63240b4532f5012773de4b2404c496ca46269f7cc3bd64bb5621796f0897'
 
   # static.frdic.com/pkg/ting_en/ was verified as official when first introduced to the cask
-  url 'https://static.frdic.com/pkg/ting_en/ting_en.dmg'
-  appcast 'https://static.frdic.com/pkg/ting_en/latest-mac.yml',
+  url 'https://static.frdic.com/pkg/ting_en/ting_en.dmg',
           user_agent: :fake
+  appcast 'https://static.frdic.com/pkg/ting_en/latest-mac.yml'
   name 'Daily English Listening'
   name '每日英语听力'
   homepage 'https://www.eudic.net/v4/en/app/ting'
