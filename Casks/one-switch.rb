cc072967789d052b45edd7e567d1338df703c108
singularitti <singularitti@outlook.com>
2020-07-31 17:25:55 -0400

    Update one-switch from 1.12,240 to 1.13,249

--- Casks/one-switch.rb
+++ Casks/one-switch.rb
@@ -1,6 +1,6 @@
 cask "one-switch" do
-  version "1.12,240"
-  sha256 "4d73686b7a0600ce85c74676dd99c448fea4b76a43da5c6961a4ea216b41a322"
+  version "1.13,249"
+  sha256 "7042d3fe93b1804035674fc5ab34922a6326b39c984bcf3e53a5aad77d31d467"
 
   url "https://fireball.studio/api/release_manager/downloads/studio.fireball.OneSwitch/#{version.after_comma}.zip"
   appcast "https://fireball.studio/api/release_manager/studio.fireball.OneSwitch.xml"
