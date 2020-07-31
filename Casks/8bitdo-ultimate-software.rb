5c15cd00346a707f19eeb3fb7c3f8e67134e6be2
Vítor Galvão <info@vitorgalvao.com>
2020-07-31 18:54:12 +0100

    Update 8bitdo-ultimate-software.rb

--- Casks/8bitdo-ultimate-software.rb
+++ Casks/8bitdo-ultimate-software.rb
@@ -2,7 +2,7 @@ cask "8bitdo-ultimate-software" do
   version "1.3.0"
   sha256 "634f9c911715efbf25f42a0c8b398c82a1ac613877e268c61616ea6fd8951848"
 
-  url "http://tools.8bitdo.com/8BitdoUltimateSoftware/8BitDoUltimateSoftwareV1.3.0.zip"
+  url "http://tools.8bitdo.com/8BitdoUltimateSoftware/8BitDoUltimateSoftwareV#{version}.zip"
   appcast "http://tools.8bitdo.com/8BitdoUltimateSoftware/appcast.xml"
   name "8BitDo Ultimate Software"
   homepage "https://support.8bitdo.com/ultimate-software.html"
