a7b158ec20ffcb913c1c8dd22a14ceb35252a87e
Randall <ran.dall@icloud.com>
2020-07-30 17:57:28 -0700

    Update rcloneosx from 2.2.0 to 2.2.1

--- Casks/rcloneosx.rb
+++ Casks/rcloneosx.rb
@@ -1,6 +1,6 @@
 cask "rcloneosx" do
-  version "2.2.0"
-  sha256 "405adaf55c0e22cc3942cab7ba88fe44a5937f4ec3d1cc517281de92687fb679"
+  version "2.2.1"
+  sha256 "38d1f45614e6b4541e31eaafdf7126178dbb3de6ef8389efe1bb13ee846078d7"
 
   url "https://github.com/rsyncOSX/rcloneosx/releases/download/v#{version}/rcloneosx.#{version}.dmg"
   appcast "https://github.com/rsyncOSX/rcloneosx/releases.atom"
