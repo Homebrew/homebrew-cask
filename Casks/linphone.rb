ac4c001892882baf8f97ba67cc2b0fdf58df76c4
Randall <ran.dall@icloud.com>
2020-07-30 18:47:39 -0700

    Update linphone from 4.2.1 to 4.2.2

--- Casks/linphone.rb
+++ Casks/linphone.rb
@@ -1,6 +1,6 @@
 cask "linphone" do
-  version "4.2.1"
-  sha256 "55c75b8afd24a253876b7db1e2de71800639cf4492a12519731ed19a1ac83582"
+  version "4.2.2"
+  sha256 "8e02953ba76bfbaf81f4cbdf3cce869ecb6a0dea3b18f4bf91962bee943fcc28"
 
   url "https://www.linphone.org/releases/macosx/app/Linphone-#{version}-mac.dmg"
   appcast "https://www.linphone.org/releases/macosx/RELEASE"
