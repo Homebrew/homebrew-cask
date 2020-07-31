2af91002c0252f85ef76851372a9d36a4e7fe08a
Randall <ran.dall@icloud.com>
2020-07-31 16:56:05 -0700

    Update terminus from 1.0.114 to 1.0.115

--- Casks/terminus.rb
+++ Casks/terminus.rb
@@ -1,6 +1,6 @@
 cask "terminus" do
-  version "1.0.114"
-  sha256 "cb85b91fc1e75942125fb84e4ad6dec89939b7b8de4174d1919f67eba99abb2c"
+  version "1.0.115"
+  sha256 "723a1ef6756adbba6dcd86687415c902ba263a75d7d817980800832186cfa849"
 
   # github.com/Eugeny/terminus/ was verified as official when first introduced to the cask
   url "https://github.com/Eugeny/terminus/releases/download/v#{version}/terminus-#{version}-macos.zip"
