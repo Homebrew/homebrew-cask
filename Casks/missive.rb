be476da79aafe7b840102ba15c5b5fd85a04dd3f
CoreCode <core-code@users.noreply.github.com>
2020-07-31 20:40:12 +0200

    Update missive from :latest to 9.32.2

--- Casks/missive.rb
+++ Casks/missive.rb
@@ -1,8 +1,10 @@
 cask "missive" do
-  version :latest
-  sha256 :no_check
+  version "9.32.2"
+  sha256 "b8a4eb258518ca170b1301bebda3a623cc3a43256308995fa1f0a26517c4d56b"
 
-  url "https://download.missiveapp.com/download/osx"
+  # d1weoh55n7oehu.cloudfront.net/ was verified as official when first introduced to the cask
+  url "https://d1weoh55n7oehu.cloudfront.net/#{version}/Missive-#{version}.dmg"
+  appcast "https://download.missiveapp.com/electron-updater/stable/darwin/latest-mac.yml"
   name "missive"
   homepage "https://missiveapp.com/"
 
