a85720cc96cf9461d94652d7b53d349445b1dc8b
Pierre Beauhaire <bjr@bhre.me>
2020-07-31 12:09:28 +0200

    Change homepage
    See PR comment
    https://github.com/Homebrew/homebrew-cask/pull/86826#pullrequestreview-458878904

--- Casks/scratch-link.rb
+++ Casks/scratch-link.rb
@@ -2,10 +2,9 @@ cask "scratch-link" do
   version "1.3.57"
   sha256 "58e9e3a5745bc65e6920bed934eb952e29b184022a9c6d8942dfcdf13b644c9d"
 
-  # downloads.scratch.mit.edu was verified as official when first introduced to the cask
   url "https://downloads.scratch.mit.edu/link/mac.zip"
   name "Scratch Link"
-  homepage "https://github.com/LLK/scratch-link"
+  homepage "https://scratch.mit.edu/microbit"
 
   pkg "scratch-link-#{version}.pkg"
 
