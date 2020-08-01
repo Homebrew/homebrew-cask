8f2627f3a379bdebbe7eeaefd6d4e9aef7ceeecf
singularitti <singularitti@outlook.com>
2020-07-31 20:56:07 -0400

    Update fantastical from 3.1.3 to 3.1.4

--- Casks/fantastical.rb
+++ Casks/fantastical.rb
@@ -1,6 +1,6 @@
 cask "fantastical" do
-  version "3.1.3"
-  sha256 "8995e3bb9c954c352ad3f081c2b1d03487f0dba1157bf72393dac8c57a8de18d"
+  version "3.1.4"
+  sha256 "24e84cde7e5d6187d65a6fd6c37fcbcd12140e993baba59f47a1bebe1c0905a7"
 
   url "https://cdn.flexibits.com/Fantastical_#{version}.zip"
   appcast "https://flexibits.com/fantastical/appcast2.php"
