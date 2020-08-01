638b1585b6d606e96633455468f8beb5136162b3
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-31 17:56:25 -0700

    Update postbox from 7.0.24 to 7.0.25 (#86910)

--- Casks/postbox.rb
+++ Casks/postbox.rb
@@ -1,6 +1,6 @@
 cask "postbox" do
-  version "7.0.24"
-  sha256 "41f1979951035fdab882841035a001d510bc1556bcbec900de057a12c533f703"
+  version "7.0.25"
+  sha256 "b67abe66ad89aca3849d1c610e224af52b75085fc875e91859d432bfb7fa6205"
 
   # d3nx85trn0lqsg.cloudfront.net/mac/ was verified as official when first introduced to the cask
   url "https://d3nx85trn0lqsg.cloudfront.net/mac/postbox-#{version}-mac64.dmg"
