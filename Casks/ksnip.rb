5f03fba5484358792061fc4b057aebed8b3db81f
Connor Adams <connorads@users.noreply.github.com>
2020-08-01 22:36:54 +0100

    Add appcast to ksnip cask

--- Casks/ksnip.rb
+++ Casks/ksnip.rb
@@ -3,6 +3,7 @@ cask "ksnip" do
   sha256 "df78187476c7d26e7551785d34b4050a51cdcfa2f781d1ce60f44b24702ba91f"
   
   url "https://github.com/ksnip/ksnip/releases/download/v#{version}/ksnip-#{version}.dmg"
+  appcast "https://github.com/ksnip/ksnip/releases.atom"
   name "Ksnip"
   homepage "http://ksnip.org/"
   
