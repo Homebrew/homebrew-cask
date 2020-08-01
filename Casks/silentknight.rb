89ce4976c3f7dc8007b8cde634a40670b6d90a89
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-31 17:54:19 -0700

    Update silentknight from 1.8,2020.06 to 1.9,2020.07 (#86914)

--- Casks/silentknight.rb
+++ Casks/silentknight.rb
@@ -1,6 +1,6 @@
 cask "silentknight" do
-  version "1.8,2020.06"
-  sha256 "6076892e23d989f81f3050ef2a70ebe12979dd8480fb40a3c76890927d277550"
+  version "1.9,2020.07"
+  sha256 "7bf565233f0ecdee4a8de0429b49f498290c4defc2d7c22dd97e101192d03534"
 
   # eclecticlightdotcom.files.wordpress.com/ was verified as official when first introduced to the cask
   url "https://eclecticlightdotcom.files.wordpress.com/#{version.after_comma.major}/#{version.after_comma.minor}/silentknight#{version.before_comma.no_dots}.zip"
