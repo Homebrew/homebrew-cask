f6bd04d00eff79c77c7c1583bc93f6e8a0a99e53
Brigitte Zmoelnig <brigitte@corecode.io>
2020-08-01 16:45:49 +0200

    Update sql-power-architect-jdbc

--- Casks/sql-power-architect-jdbc.rb
+++ Casks/sql-power-architect-jdbc.rb
@@ -2,10 +2,10 @@ cask "sql-power-architect-jdbc" do
   version "1.0.8"
   sha256 "a564e294aba5e7f4701717f8d36a898b06e6354000df332e5b6aa41377b06665"
 
-  url "https://download.sqlpower.ca/architect/#{version}/community/SQL-Power-Architect-OSX-#{version}.tar.gz"
-  appcast "https://download.sqlpower.ca/architect/"
+  url "http://www.bestofbi.com/downloads/architect/#{version}/SQL-Power-Architect-OSX-#{version}.tar.gz"
+  appcast "http://www.bestofbi.com/page/architect_download_os"
   name "SQL Power Architect Community edition"
-  homepage "https://www.sqlpower.ca/"
+  homepage "http://www.bestofbi.com/page/architect"
 
   app "SQL Power Architect JDBC.app"
 end
