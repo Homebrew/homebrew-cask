9858cf97d5ea6d205d3468997401cd95420aa433
Randall <ran.dall@icloud.com>
2020-08-02 15:47:03 -0700

    Update bitcoin-core from 0.20.0 to 0.20.1

--- Casks/bitcoin-core.rb
+++ Casks/bitcoin-core.rb
@@ -1,6 +1,6 @@
 cask "bitcoin-core" do
-  version "0.20.0"
-  sha256 "a6e44b928d9ac04f11d43e920f4971fbdf1e77a8c28f7c14fafdd741ca7bc99f"
+  version "0.20.1"
+  sha256 "c378d4e21109f09e8829f3591e015c66632dff2925a60b64d259be05a334c30b"
 
   url "https://bitcoincore.org/bin/bitcoin-core-#{version}/bitcoin-#{version}-osx.dmg"
   appcast "https://github.com/bitcoin/bitcoin/releases.atom"
