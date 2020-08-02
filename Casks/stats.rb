5de2fec72bfd6c3a60e7a10bde74587c6af64233
Randall <ran.dall@icloud.com>
2020-08-02 16:01:47 -0700

    Update stats from 2.1.14 to 2.2.0

--- Casks/stats.rb
+++ Casks/stats.rb
@@ -1,6 +1,6 @@
 cask "stats" do
-  version "2.1.14"
-  sha256 "cba7c87eba9632f411365d650d5c1dd5552c120b73e6ef90c50c9c499fb6fa3c"
+  version "2.2.0"
+  sha256 "bfe00b81152856601a3ea916e6e9ecd9b11f0b6ed85d785690f5dd80685ff9fb"
 
   url "https://github.com/exelban/stats/releases/download/v#{version}/Stats.dmg"
   appcast "https://github.com/exelban/stats/releases.atom"
