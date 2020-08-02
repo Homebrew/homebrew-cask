784c8cfe1008f6d6472553e9f738197c21ba9096
CoreCode <core-code@users.noreply.github.com>
2020-08-02 19:29:10 +0200

    Update appstudio.rb

--- Casks/appstudio.rb
+++ Casks/appstudio.rb
@@ -2,8 +2,8 @@ cask "appstudio" do
   version :latest
   sha256 :no_check
 
-  # dl.dropboxusercontent.com/s/27y2gzzd8f6sm1p/ was verified as official when first introduced to the cask
-  url "https://www.dropbox.com/s/27y2gzzd8f6sm1p/AppStudio.dmg"
+  # www.dropbox.com/s/27y2gzzd8f6sm1p/ was verified as official when first introduced to the cask
+  url "https://www.dropbox.com/s/27y2gzzd8f6sm1p/AppStudio.dmg?dl=1"
   name "NSB/AppStudio"
   homepage "https://www.nsbasic.com/"
 
