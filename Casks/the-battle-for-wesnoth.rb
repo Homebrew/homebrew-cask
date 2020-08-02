65d6c2339d81e4cdcb6ae270b0f65c8121f4f9a0
Toni Förster <toni.foerster@gmail.com>
2020-08-02 12:09:04 +0200

    downgrade the-the-battle-for-wesnoth.rb (#86943)

--- Casks/the-battle-for-wesnoth.rb
+++ Casks/the-battle-for-wesnoth.rb
@@ -1,6 +1,6 @@
 cask "the-battle-for-wesnoth" do
-  version "1.15.3"
-  sha256 "cc20b350b6f91db207a6a1f2be16e4574f81eb8fc597111dd1470acfdf79e77f"
+  version "1.14.13"
+  sha256 "e79a1dede6b8da6d3726ea87ff39a4c895e83fcd1dd0114201e3503dd37b5090"
 
   # sourceforge.net/wesnoth/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/wesnoth/Wesnoth_#{version}.dmg"
