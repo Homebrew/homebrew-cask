94d1cbe20729557e770492fe110b44182498dd1f
Randall <ran.dall@icloud.com>
2020-08-02 15:52:33 -0700

    Update iina-plus from 0.5.0,20072018 to 0.5.2,20080222

--- Casks/iina-plus.rb
+++ Casks/iina-plus.rb
@@ -1,6 +1,6 @@
 cask "iina-plus" do
-  version "0.5.0,20072018"
-  sha256 "2245826be480346e1f33fa1af942bcc6d57c727a34823a29f6caeb61030178e2"
+  version "0.5.2,20080222"
+  sha256 "9daac4df85524a024d092e13ca3bd1f08c1d1be6127b078ffe9a201efa363315"
 
   url "https://github.com/xjbeta/iina-plus/releases/download/#{version.before_comma}(#{version.after_comma})/iina+.#{version.before_comma}.zip"
   appcast "https://github.com/xjbeta/iina-plus/releases.atom"
