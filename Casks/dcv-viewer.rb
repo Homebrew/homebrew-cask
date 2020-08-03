741e8c543117a6ea9747d1edbe2f806263c4c651
Randall <ran.dall@icloud.com>
2020-08-03 15:07:40 -0700

    Update dcv-viewer from 2020.0.1716 to 2020.1.1910

--- Casks/dcv-viewer.rb
+++ Casks/dcv-viewer.rb
@@ -1,6 +1,6 @@
 cask "dcv-viewer" do
-  version "2020.0.1716"
-  sha256 "e4ddae68e8c827d3ec28ef7c557883d901058a8d17460495a456da7e15e1a4d0"
+  version "2020.1.1910"
+  sha256 "edc9d41225824e912829a74619b8aa3cfb7bb231089839f6e26b2d3d07bec155"
 
   # d1uj6qtbmh3dt5.cloudfront.net/ was verified as official when first introduced to the cask
   url "https://d1uj6qtbmh3dt5.cloudfront.net/#{version.major_minor}/Clients/nice-dcv-viewer-#{version}.dmg"
