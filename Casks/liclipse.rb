00be661de7dace6a0993a825ba58680a921a49fe
Randall <17261190+ran-dall@users.noreply.github.com>
2020-08-02 17:50:26 -0700

    Update liclipse from 6.2.0,ua5e9iiglndk16n to 6.3.0,7tlqxhknfqih3fs (#86993)

--- Casks/liclipse.rb
+++ Casks/liclipse.rb
@@ -1,6 +1,6 @@
 cask "liclipse" do
-  version "6.2.0,ua5e9iiglndk16n"
-  sha256 "1fd1f0c4692c16f62b97decfd7ce872b8826805e9d87ea7c922820a870edd967"
+  version "6.3.0,7tlqxhknfqih3fs"
+  sha256 "8ddf6eee0ffe9fe58c8c2144078689b53c1d154a033aace913c5c0f25f6502b2"
 
   # mediafire.com/file/ was verified as official when first introduced to the cask
   url "https://www.mediafire.com/file/#{version.after_comma}/liclipse_#{version.before_comma}_macosx.cocoa.x86_64.dmg"
