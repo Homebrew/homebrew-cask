022c463eb7b5ffb02b407222114172d575562e7d
Alireza Asadollahi <aliasadollahi@aol.com>
2020-08-03 23:18:07 +0430

    Update anydesk.rb

--- Casks/anydesk.rb
+++ Casks/anydesk.rb
@@ -1,6 +1,6 @@
 cask "anydesk" do
-  version :latest
-  sha256 :no_check
+  version :5.6.0
+  sha256 :bc5606943ad25155651c5fe6a17562bae9088d092e1b8cf7c1bc2eaeb9cbe219
 
   url "https://download.anydesk.com/anydesk.dmg"
   name "AnyDesk"
