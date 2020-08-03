4616283c025a000a1b31be8c08e0d907226ac131
Randall <ran.dall@icloud.com>
2020-08-03 14:21:53 -0700

    Update metasploit from 5.0.101,20200724102901 to 5.0.102,20200803102848

--- Casks/metasploit.rb
+++ Casks/metasploit.rb
@@ -1,6 +1,6 @@
 cask "metasploit" do
-  version "5.0.101,20200724102901"
-  sha256 "66aa2b356d3101c02f43b8883ee3e6958d6d6aea94f09ad9e92841d017342001"
+  version "5.0.102,20200803102848"
+  sha256 "578aa86600a490ab83d9bcf9cf179359341d7c6d9f601a731e0b6d9922ded6e1"
 
   url "https://osx.metasploit.com/metasploit-framework-#{version.before_comma}+#{version.after_comma}-1rapid7-1.pkg"
   appcast "https://osx.metasploit.com/LATEST"
