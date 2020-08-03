b74ae1dd2dd74c12d7b2a37e51efccf4b39be3d4
Osama Aamer <osama.aamer95@gmail.com>
2020-08-03 17:08:07 +0500

    Update dbeaver-community.rb from 7.1.3 to 7.1.4 (#86999)

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,6 +1,6 @@
 cask "dbeaver-community" do
-  version "7.1.3"
-  sha256 "e3fcd5b4eaecf13a6133b536bd27b42bf0d8dcf0e5f34bcb24a5ea387e76635e"
+  version "7.1.4"
+  sha256 "ce57517fa664188f50e186ff22b39ac63123c68230aa62b3c01b924abc3c6ff3"
 
   # github.com/dbeaver/dbeaver/ was verified as official when first introduced to the cask
   url "https://github.com/dbeaver/dbeaver/releases/download/#{version}/dbeaver-ce-#{version}-macos.dmg"
