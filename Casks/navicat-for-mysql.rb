5f8ce72151fe54e98c58c7b2709335e6a35704e5
Randall <17261190+ran-dall@users.noreply.github.com>
2020-08-03 15:11:20 -0700

    Update navicat-for-mysql from 15.0.18 to 15.0.20 (#87029)

--- Casks/navicat-for-mysql.rb
+++ Casks/navicat-for-mysql.rb
@@ -1,6 +1,6 @@
 cask "navicat-for-mysql" do
-  version "15.0.18"
-  sha256 "df13b80d8eb64faa94e29c88a1d46950b76de11a82810ec63e890afeee777968"
+  version "15.0.20"
+  sha256 "d6a1deb2a0481d86e89d0101dd37f7cf796f4134506224078630eda1afa517db"
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_mysql_en.dmg"
   appcast "https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20for%20MySQL&appLang=en"
