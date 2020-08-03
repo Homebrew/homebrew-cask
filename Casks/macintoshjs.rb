0e03b6bcecb5425a22600302b1205bf327e6b62b
suschizu <brigitte@corecode.io>
2020-08-03 20:48:38 +0200

    Update macintoshjs.rb (#87006)

--- Casks/macintoshjs.rb
+++ Casks/macintoshjs.rb
@@ -1,6 +1,6 @@
 cask "macintoshjs" do
-  version "1.0.2"
-  sha256 "7d6494b14cb895e6540b3a09608269bba2b97a3d8bf784d56c45f6da558bffbf"
+  version "1.0.5"
+  sha256 "8bd519f7a95d024861da8e829a916cc7a1b5ebf6103413271250988b8a9c0743"
 
   url "https://github.com/felixrieseberg/macintosh.js/releases/download/v#{version}/macintosh.js-darwin-x64-#{version}.zip"
   appcast "https://github.com/felixrieseberg/macintosh.js/releases.atom"
