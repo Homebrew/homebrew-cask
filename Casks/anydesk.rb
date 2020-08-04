f60d18257e075040e54ed6cbedd6b65492904b70
Alireza Asadollahi <aliasadollahi@aol.com>
2020-08-04 13:50:13 +0430

    Update anydesk.rb (#87008)
    * Update anydesk.rb
    
    * Update anydesk.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/anydesk.rb
+++ Casks/anydesk.rb
@@ -1,8 +1,9 @@
 cask "anydesk" do
-  version :latest
-  sha256 :no_check
+  version "5.6.0"
+  sha256 "bc5606943ad25155651c5fe6a17562bae9088d092e1b8cf7c1bc2eaeb9cbe219"
 
   url "https://download.anydesk.com/anydesk.dmg"
+  appcast "https://anydesk.com/en/downloads/mac-os"
   name "AnyDesk"
   homepage "https://anydesk.com/remote-desktop"
 
