579321957d24f000a3fdce10a9483d5ed55571b1
Hannes Braun <hannesbraun@mail.de>
2020-08-04 13:10:46 +0200

    Update jellyfin from 10.6.1 to 10.6.2

--- Casks/jellyfin.rb
+++ Casks/jellyfin.rb
@@ -1,8 +1,8 @@
 cask "jellyfin" do
-  version "10.6.1"
-  sha256 "0d7b562f0b799302db417a9ab5eb2bf07fdc8ca19b28649798ebcf266c949c1e"
+  version "10.6.2"
+  sha256 "5553490ef591114102de0dd8c5305685bb0d07cb2e28419d83760d116cc17aaa"
 
-  url "https://repo.jellyfin.org/releases/server/macos/stable/combined/Jellyfin_#{version}.dmg"
+  url "https://repo.jellyfin.org/releases/server/macos/stable/installer/Jellyfin_#{version}.dmg"
   appcast "https://repo.jellyfin.org/releases/server/macos/stable/"
   name "Jellyfin"
   homepage "https://jellyfin.org/"
