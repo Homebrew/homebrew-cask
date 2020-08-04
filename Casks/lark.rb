eb48b497857dfe9d3d1d2263b3210bc0abff8478
Hannes Braun <hannesbraun@mail.de>
2020-08-04 13:58:45 +0200

    Update lark from 3.28.1 to 3.29.2,36e42f

--- Casks/lark.rb
+++ Casks/lark.rb
@@ -1,9 +1,9 @@
 cask "lark" do
-  version "3.28.1"
-  sha256 "c3a322dac541c1da379465c8d90e5517b3400644cf389e3d031d589f1948e9b3"
+  version "3.29.2,36e42f"
+  sha256 "4e5c2023bb089206843bbd95aba29d1f09c279c15edb62ea3312a0f7a6d6cff8"
 
   # sf3-ttcdn-tos.pstatp.com/ was verified as official when first introduced to the cask
-  url "https://sf3-ttcdn-tos.pstatp.com/obj/ee-appcenter/Lark-#{version}.dmg"
+  url "https://sf3-ttcdn-tos.pstatp.com/obj/ee-appcenter/#{version.after_comma}/Lark-#{version.before_comma}.dmg"
   appcast "https://www.larksuite.com/api/downloads"
   name "Lark"
   homepage "https://www.larksuite.com/"
