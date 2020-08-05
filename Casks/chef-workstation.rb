55c0204a860e0df2c9c40923b212c30df4c03d9c
miccal <miccal.matthews@gmail.com>
2020-08-05 08:23:44 +0800

    Update chef-workstation

--- Casks/chef-workstation.rb
+++ Casks/chef-workstation.rb
@@ -2,8 +2,8 @@ cask "chef-workstation" do
   version "20.8.111"
   sha256 "0cfac31ad71cb5c84244137bb4976da426d4f2c61f9d534cc1bc08b8f390a4ec"
 
-  url "https://packages.chef.io/files/stable/chef-workstation/#{version}/mac_os_x/10.14/chef-workstation-#{version}-1.dmg"
-  appcast "https://omnitruck.chef.io/stable/chef-workstation/metadata?p=mac_os_x&pv=10.14&m=x86_64&v=latest"
+  url "https://packages.chef.io/files/stable/chef-workstation/#{version}/mac_os_x/10.15/chef-workstation-#{version}-1.dmg"
+  appcast "https://omnitruck.chef.io/stable/chef-workstation/metadata?p=mac_os_x&pv=10.15&m=x86_64&v=latest"
   name "Chef Workstation"
   homepage "https://docs.chef.io/workstation/"
 
