7c7691677571158f78ca68f38d9365c626485b43
Randall <17261190+ran-dall@users.noreply.github.com>
2020-08-04 18:24:06 -0700

    Update macintoshjs from 1.0.5 to 1.0.6 (#87081)

--- Casks/macintoshjs.rb
+++ Casks/macintoshjs.rb
@@ -1,6 +1,6 @@
 cask "macintoshjs" do
-  version "1.0.5"
-  sha256 "8bd519f7a95d024861da8e829a916cc7a1b5ebf6103413271250988b8a9c0743"
+  version "1.0.6"
+  sha256 "f1cf7e80c17f1ade18dcfde10d291d70f9e1c86762f42c1004dc7caa3717c2d0"
 
   url "https://github.com/felixrieseberg/macintosh.js/releases/download/v#{version}/macintosh.js-darwin-x64-#{version}.zip"
   appcast "https://github.com/felixrieseberg/macintosh.js/releases.atom"
