30717df8279b4fb5d0bd8a3b80b35105a3e6dbb4
Miccal Matthews <miccal.matthews@gmail.com>
2020-08-05 19:30:27 +0800

    Update kafka-tool from latest to 2.0.7 (#86632)

--- Casks/kafka-tool.rb
+++ Casks/kafka-tool.rb
@@ -1,21 +1,18 @@
 cask "kafka-tool" do
-  version :latest
-  sha256 :no_check
+  version "2.0.7"
+  sha256 "8f4a008a4d3ce83e274d15796474770165961c6b66b59503e03ccd8c6b93ebbd"
 
   url "https://www.kafkatool.com/download2/kafkatool.dmg"
+  appcast "https://www.kafkatool.com/download.html"
   name "Kafka Tool"
   homepage "https://www.kafkatool.com/index.html"
 
   installer script: {
-    executable: "Kafka Tool 2 Installer.app/Contents/MacOS/JavaApplicationStub",
+    executable: "Kafka Tool #{version.major} Installer.app/Contents/MacOS/JavaApplicationStub",
     args:       ["-q"],
   }
 
   uninstall delete: "/Applications/Kafka Tool.app"
 
-  zap trash: "~/.kafkatool2"
-
-  caveats do
-    depends_on_java "8"
-  end
+  zap trash: "~/.kafkatool#{version.major}"
 end
