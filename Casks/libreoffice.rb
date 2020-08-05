4a694a4112907d2ce8a5057915e832b2bb21b56d
Sascha Peilicke <sascha@peilicke.de>
2020-08-06 00:07:41 +0200

    Update libreoffice from 6.4.5 to 7.0.0 (#87121)
    * Update libreoffice from 6.4.5 to 7.0.0
    
    * Update libreoffice.rb
    
    * Update libreoffice.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/libreoffice.rb
+++ Casks/libreoffice.rb
@@ -1,10 +1,11 @@
 cask "libreoffice" do
-  version "6.4.5"
-  sha256 "8f8561085a93186ddb5d7d7b5f990e9effbb3305b1197768960bf6c688bef678"
+  version "7.0.0.3"
+  sha256 "74700ce918278b78464a4cd0e16d929e4af7b0447c42b400f61a2555818584e9"
 
   # documentfoundation.org/ was verified as official when first introduced to the cask
-  url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg"
-  appcast "https://download.documentfoundation.org/libreoffice/stable/"
+  url "https://download.documentfoundation.org/libreoffice/stable/#{version.major_minor_patch}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg"
+  appcast "https://download.documentfoundation.org/libreoffice/stable/",
+          must_contain: version.major_minor_patch
   name "LibreOffice"
   homepage "https://www.libreoffice.org/"
 
