ce7a0eb72b612c3dd1c8575f575758391ed900b1
James Vaughn <james.vaughn@canonical.com>
2020-08-05 21:08:00 +0100

    Update SoundSource to 5.0.0 (#87132)

--- Casks/soundsource.rb
+++ Casks/soundsource.rb
@@ -1,14 +1,14 @@
 cask "soundsource" do
-  version "4.2.5"
-  sha256 "cc0999faa8abf62a02e077209e9d4f0dadffb7b10dd809a84b2d4ba59dd3955d"
+  version "5.0.0"
+  sha256 "bd1252c860a0f7ef3a98ed71b78a26874f2e5a7a493caebe00e6021d7f5698e3"
 
   url "https://rogueamoeba.com/soundsource/download/SoundSource.zip"
-  appcast "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.soundsource&system=10146&version=4000000"
+  appcast "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.soundsource&system=10146&version=5000000"
   name "SoundSource"
   homepage "https://rogueamoeba.com/soundsource/"
 
   auto_updates true
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :high_sierra"
 
   app "SoundSource.app"
 end
