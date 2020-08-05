b40e8829741665d3191831b9d2b13686ddf05e14
Randall <ran.dall@icloud.com>
2020-08-04 18:03:56 -0700

    Update tidepool-uploader from 2.32.0 to 2.33.0

--- Casks/tidepool-uploader.rb
+++ Casks/tidepool-uploader.rb
@@ -1,6 +1,6 @@
 cask "tidepool-uploader" do
-  version "2.32.0"
-  sha256 "3a0e946d5ff2a231c31dafcfa96df89c9a078dee68cf64c21237e6fe955cb1df"
+  version "2.33.0"
+  sha256 "b4a4f906ff8d6aa9d84276598167111f3bf72677de4e43fc5fedcc80872e981a"
 
   # github.com/tidepool-org/chrome-uploader/ was verified as official when first introduced to the cask
   url "https://github.com/tidepool-org/chrome-uploader/releases/download/v#{version}/tidepool-uploader-#{version}.dmg/"
