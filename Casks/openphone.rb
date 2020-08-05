888e4e8b9815fc9378ba13685e788c292ed3e230
Jon Leibowitz <jonleibowitz@users.noreply.github.com>
2020-08-05 16:17:36 -0700

    Update openphone.rb (overlooked a new line)

--- Casks/openphone.rb
+++ Casks/openphone.rb
@@ -3,7 +3,8 @@ cask "openphone" do
   sha256 "a06985ee79cd2561a90e9d7c18b5d9a564321d4b18130a13d4969a6e276b20e5"
 
   url "https://download.openphone.co/OpenPhone-#{version}.dmg"
-  appcast "https://s3-us-west-2.amazonaws.com/download.openphone.co/latest-mac.yml"  name "OpenPhone"
+  appcast "https://s3-us-west-2.amazonaws.com/download.openphone.co/latest-mac.yml"  
+  name "OpenPhone"
   homepage "https://www.openphone.co/desktop"
 
   app "OpenPhone.app"
