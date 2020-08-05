3de3071b435e579d6ec6c6b75f63a775fcf0c098
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2020-08-05 13:26:52 +0200

    Update cctalk from 7.7.0.6 to 7.7.1-1082 (#87113)

--- Casks/cctalk.rb
+++ Casks/cctalk.rb
@@ -1,9 +1,9 @@
 cask "cctalk" do
-  version "7.7.0.6"
-  sha256 "fcb29bb3092e4ee6b7f7564cafaaf3ec19dec9c7b745a0ff5994cfad9b887bde"
+  version "7.7.1-1082"
+  sha256 "73311cac88f85c3dfaa8cda4dabef2253a4674eabf2120d0476e999bf26782d8"
 
   # cc.hjfile.cn/ was verified as official when first introduced to the cask
-  url "https://cc.hjfile.cn/cc/#{version}/8/1/103/#{version}.dmg"
+  url "https://cc.hjfile.cn/cc/CCtalk.#{version}/8/1/103/CCtalk.#{version}.dmg"
   appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://www.cctalk.com/webapi/basic/v1.1/version/down%3Fapptype=1%26terminalType=8%26versionType=103"
   name "CCtalk"
   homepage "https://www.cctalk.com/download/"
