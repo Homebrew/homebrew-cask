d97be9a1f28fc75072758066446bffca41ca806e
Miccal Matthews <miccal.matthews@gmail.com>
2020-08-06 07:16:22 +0800

    Update libreoffice.rb

--- Casks/libreoffice.rb
+++ Casks/libreoffice.rb
@@ -4,8 +4,7 @@ cask "libreoffice" do
 
   # documentfoundation.org/ was verified as official when first introduced to the cask
   url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg"
-  appcast "https://download.documentfoundation.org/libreoffice/stable/",
-          must_contain: version.major_minor_patch
+  appcast "https://download.documentfoundation.org/libreoffice/stable/"
   name "LibreOffice"
   homepage "https://www.libreoffice.org/"
 
