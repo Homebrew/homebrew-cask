0effb8ec2bdd3bece0dc4bf8f8ee7ca3e8a6a696
Amory Meltzer <Amorymeltzer@gmail.com>
2020-08-05 07:07:12 -0400

    Update ticktick from 3.6.70,144 to 3.7.00,148 (#87117)

--- Casks/ticktick.rb
+++ Casks/ticktick.rb
@@ -1,6 +1,6 @@
 cask "ticktick" do
-  version "3.6.70,144"
-  sha256 "20e77372090f41463c1a1bafc3a852e01f2dcf7aa1e901af4dda520b6a74e09c"
+  version "3.7.00,148"
+  sha256 "8eb63d84e3e0f57caf7f115979c81ef5afd34d897200eb3c5726570da323c416"
 
   # appest-public.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://appest-public.s3.amazonaws.com/download/mac/TickTick_#{version.before_comma}_#{version.after_comma}.dmg"
