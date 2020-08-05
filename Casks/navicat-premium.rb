049cfc8b1006fe6fb4f24e681c68e60dd53faa7f
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2020-08-05 13:07:48 +0200

    Update navicat-premium from 15.0.18 to 15.0.20 (#87115)

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -1,14 +1,14 @@
 cask "navicat-premium" do
-  version "15.0.18"
+  version "15.0.20"
 
   language "zh-CN" do
-    sha256 "f46f38799aaa69d830c04f789f917151bdab1eeee617433daaea6d33b5a7bd2d"
+    sha256 "e8139985b76f41b34fafc064c1983b5fc41903156b275e4ebc4809c37b7f7f0f"
     url "http://download.navicat.com.cn/download/navicat#{version.major_minor.no_dots}_premium_cs.dmg"
     homepage "https://www.navicat.com.cn/products/navicat-premium"
   end
 
   language "en", default: true do
-    sha256 "abc959616d08b2f0dabe08f40068013bfb716352f725197e2221f92e0e828707"
+    sha256 "68a77024aeab2db4664733c12721b446f0c661e370fa6250e24ae8b20fb27d48"
     url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_premium_en.dmg"
     appcast "https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20Premium&appLang=en"
     homepage "https://www.navicat.com/products/navicat-premium"
