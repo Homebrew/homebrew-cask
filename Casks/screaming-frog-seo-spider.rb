32c1159be7452c7d061fe10e26024e5f7dec0708
Andrei izotov <faradaytrs@gmail.com>
2020-08-05 09:56:09 +0800

    Update screaming-frog-seo-spider from 13.1 to 13.2

--- Casks/screaming-frog-seo-spider.rb
+++ Casks/screaming-frog-seo-spider.rb
@@ -1,6 +1,6 @@
 cask "screaming-frog-seo-spider" do
-  version "13.1"
-  sha256 "9284040f36fa465c973e721374e47028e3173a44aa86f4a8386261c910826a23"
+  version "13.2"
+  sha256 "0afd1df95a5aeabe346d839b53d80c2f5ea967192da098b0dff16cbfe8aa013d"
 
   url "https://download.screamingfrog.co.uk/products/seo-spider/ScreamingFrogSEOSpider-#{version}.dmg"
   appcast "https://www.screamingfrog.co.uk/wp-content/themes/screamingfrog/inc/download-modal.php"
