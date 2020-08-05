0e825b0febd686aaede7ea114acc830ce77a10ea
Jon Leibowitz <jon.with.no.h.leibowitz@gmail.com>
2020-08-05 09:49:00 -0700

    Add OpenPhone.app v1.9.1

---
+++ Casks/openphone.rb
@@ -0,0 +1,10 @@
+cask "openphone" do
+  version "1.9.1"
+  sha256 "a06985ee79cd2561a90e9d7c18b5d9a564321d4b18130a13d4969a6e276b20e5"
+
+  url "https://download.openphone.co/OpenPhone-1.9.1.dmg"
+  name "OpenPhone"
+  homepage "https://www.openphone.co/desktop"
+
+  app "OpenPhone.app"
+end
