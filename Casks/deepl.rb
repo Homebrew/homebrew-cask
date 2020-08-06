1ad8faf4317b482162db5fd89572eef65956c1ee
Chai Feng <chaifeng@chaifeng.com>
2020-08-06 09:24:22 +0800

    Update deepl from 1.8.1 to 1.9.0

--- Casks/deepl.rb
+++ Casks/deepl.rb
@@ -1,6 +1,6 @@
 cask "deepl" do
-  version "1.8.1"
-  sha256 "c40d917567c775a4e1dda096a624395d72ee9772293450a7db0a84bf2010f417"
+  version "1.9.0"
+  sha256 "fca6b8580609d5776a23cb4779445357bf3037980bd144aab9ce423ef3bc31f3"
 
   url "https://appdownload.deepl.com/macos/DeepL.dmg"
   name "DeepL"
