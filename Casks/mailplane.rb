19e4a14bd5d50b404e9dbe04a242e0692d21142d
Stephen Wade <stephen@stephenwade.me>
2020-08-06 23:34:09 -0400

    Update mailplane from 4.2.4,4812 to 4.2.5,4826 (#87222)

--- Casks/mailplane.rb
+++ Casks/mailplane.rb
@@ -1,6 +1,6 @@
 cask "mailplane" do
-  version "4.2.4,4812"
-  sha256 "129f545e105f744e3546d48f891aa016a66a4a511209b52fc6883641f6174728"
+  version "4.2.5,4826"
+  sha256 "bc7947625fefa9556987cf9391838f0241f66b6085ab1c55e686ca70216f313e"
 
   url "https://update.mailplaneapp.com/builds/Mailplane_#{version.major}_#{version.after_comma}.tbz"
   appcast "https://update.mailplaneapp.com/appcast.php?rqsr=1&osVersion=10.14.1&appVersion=#{version.after_comma}&shortVersionString=#{version.before_comma}"
