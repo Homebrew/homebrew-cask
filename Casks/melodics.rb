2575ba181a525ed9dc384591bfc6ebc7f343c6d1
Randall <ran.dall@icloud.com>
2020-08-06 18:49:24 -0700

    Update melodics from 2.1.4825 to 2.1.4831

--- Casks/melodics.rb
+++ Casks/melodics.rb
@@ -1,6 +1,6 @@
 cask "melodics" do
-  version "2.1.4825"
-  sha256 "cc27a1453794188c226d7e68ffa16e29a64834f8df4d78d7ea2e4e589f27ed0d"
+  version "2.1.4831"
+  sha256 "eae45ed1cafbe2607c6353743100c9e1fcbc723a51b08d2e95f0333bb6d8a064"
 
   url "https://web-cdn.melodics.com/download/MelodicsV#{version.major}.dmg"
   appcast "https://web-cdn.melodics.com/download/osxupdatescastv#{version.major}.xml"
