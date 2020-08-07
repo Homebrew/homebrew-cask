763e8874d3263be5e3c33e59293c1084c10860a2
Randall <ran.dall@icloud.com>
2020-08-07 12:29:33 -0700

    Update musictube from 1.12.1 to 1.13

--- Casks/musictube.rb
+++ Casks/musictube.rb
@@ -1,6 +1,6 @@
 cask "musictube" do
-  version "1.12.1"
-  sha256 "af748a4afdb577b3eb2bed4166c8d80cb7e82d9a9051878afee7abf5e5a3b084"
+  version "1.13"
+  sha256 "5f6c9a91d90931f47c5002cca11574339b6cfd6f586adb85c045bb2494d47e54"
 
   url "https://flavio.tordini.org/files/musictube/musictube.dmg"
   appcast "https://flavio.tordini.org/musictube-ws/appcast.xml"
