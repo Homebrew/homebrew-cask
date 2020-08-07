63fcaaea156115a30f5d3896cb22a50a430823ae
Randall <17261190+ran-dall@users.noreply.github.com>
2020-08-07 07:12:03 -0700

    Update responsively from 0.9.0 to 0.10.0 (#87189)

--- Casks/responsively.rb
+++ Casks/responsively.rb
@@ -1,12 +1,12 @@
 cask "responsively" do
-  version "0.9.0"
-  sha256 "f330dc9134b96a21bac77a43f240392391973b4acf82b6b1840ba6b962addde8"
+  version "0.10.0"
+  sha256 "8a7a2d9e488162e72fe49e793dcb1a49d5aad0638ec3eb16b8e5fc6cdb53c8fc"
 
-  # github.com/manojVivek/responsively-app/ was verified as official when first introduced to the cask
-  url "https://github.com/manojVivek/responsively-app/releases/download/v#{version}/ResponsivelyApp-#{version}.dmg"
-  appcast "https://github.com/manojVivek/responsively-app/releases.atom"
+  # github.com/responsively-org/responsively-app/ was verified as official when first introduced to the cask
+  url "https://github.com/responsively-org/responsively-app/releases/download/v#{version}/ResponsivelyApp-#{version}.dmg"
+  appcast "https://github.com/responsively-org/responsively-app/releases.atom"
   name "Responsively"
-  homepage "https://manojvivek.github.io/responsively-app/"
+  homepage "https://responsively.app/"
 
   app "ResponsivelyApp.app"
 
