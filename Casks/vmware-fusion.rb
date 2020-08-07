c42237bb14f10c6ef7b997b780b584314d6f9c74
Peter Lewis <peter@peterlew.is>
2020-08-07 09:23:23 +0100

    Update vmware-fusion from 11.5.5-16269456 to 11.5.6-16696540 (#87230)

--- Casks/vmware-fusion.rb
+++ Casks/vmware-fusion.rb
@@ -1,6 +1,6 @@
 cask "vmware-fusion" do
-  version "11.5.5-16269456"
-  sha256 "20cdb359334cd7fd5953ab73264610e03c75a8bcf7f1601ba6535ca8bd53fd83"
+  version "11.5.6-16696540"
+  sha256 "f26f8404204af4fcd8932789d849a56e96d0dbeab1a76e35e6a2b42f2f31ffc2"
 
   url "https://download3.vmware.com/software/fusion/file/VMware-Fusion-#{version}.dmg"
   appcast "https://softwareupdate.vmware.com/cds/vmw-desktop/fusion.xml"
