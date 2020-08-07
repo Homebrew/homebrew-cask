613f5c5cccbdee61a2b59a3589b812d027065cc1
tk4k <54473116+tk4k@users.noreply.github.com>
2020-08-07 09:44:34 +0900

    Update maccy from 0.13.0 to 0.14.0

--- Casks/maccy.rb
+++ Casks/maccy.rb
@@ -1,6 +1,6 @@
 cask "maccy" do
-  version "0.13.0"
-  sha256 "82024126a7a17f3f5359d8e531e3db67e13707e59e1aa0320372b4eaf3025bf8"
+  version "0.14.0"
+  sha256 "0a556bc017807561cf63ca9a82514e38a898a468ad7a05b3cf3e2a788ee2e7d3"
 
   # github.com/p0deje/Maccy/ was verified as official when first introduced to the cask
   url "https://github.com/p0deje/Maccy/releases/download/#{version}/Maccy.app.zip"
