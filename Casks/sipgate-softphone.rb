3521759c0d44b080e0f93c07fd0b7be7c9952aca
Randall <ran.dall@icloud.com>
2020-08-06 19:23:46 -0700

    Update sipgate-softphone from 1.10.9 to 1.10.10

--- Casks/sipgate-softphone.rb
+++ Casks/sipgate-softphone.rb
@@ -1,6 +1,6 @@
 cask "sipgate-softphone" do
-  version "1.10.9"
-  sha256 "41734fd9e06033a33647e61f1d81743f3a8bd0068a38b27557a9b6162f45211e"
+  version "1.10.10"
+  sha256 "427be751f62dd2b8bf2c76ffeed66aee1bda7f86fbe7f8418f4abf8a1e03b814"
 
   # sipgate-desktop-app-dev.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://sipgate-desktop-app-dev.s3.amazonaws.com/sipgate-softphone-#{version}.dmg"
