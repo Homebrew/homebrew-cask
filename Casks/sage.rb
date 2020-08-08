bca434ec6e76f5bcb7741418f82242d342473f87
Leonardo Gama <leonardo.reis.gama@gmail.com>
2020-08-07 21:32:27 -0300

    sage: Fix version choosing for macOS < Catalina (#87246)

--- Casks/sage.rb
+++ Casks/sage.rb
@@ -1,6 +1,11 @@
 cask "sage" do
-  version "9.1,10.15.4"
-  sha256 "139fd510245d94e9027fa6fff280b41a238b2d6044f1923c49e66a031da166e4"
+  if MacOS.version <= :mojave
+    version "9.1,10.11.6"
+    sha256 "23c13690b8a72deca1628dd0e66a0f7b83557f98c13c3db1dc7eb15d80cf3a8d"
+  else
+    version "9.1,10.15.4"
+    sha256 "139fd510245d94e9027fa6fff280b41a238b2d6044f1923c49e66a031da166e4"
+  end
 
   # mirrors.mit.edu/sage/osx/intel/ was verified as official when first introduced to the cask
   url "https://mirrors.mit.edu/sage/osx/intel/sage-#{version.before_comma}-OSX_#{version.after_comma}-x86_64.app.dmg"
@@ -8,7 +13,7 @@ cask "sage" do
   name "Sage"
   homepage "https://www.sagemath.org/"
 
-  depends_on macos: ">= :high_sierra"
+  depends_on macos: ">= :el_capitan"
 
   app "SageMath-#{version.before_comma}.app"
   binary "#{appdir}/SageMath-#{version.before_comma}.app/Contents/Resources/sage/sage"
