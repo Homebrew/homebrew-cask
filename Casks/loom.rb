526a8a14593ab93c78d4618f0ddbd96602e75a71
Randall <ran.dall@icloud.com>
2020-08-08 18:59:15 -0700

    Update loom from 0.46.4 to 0.46.5

--- Casks/loom.rb
+++ Casks/loom.rb
@@ -1,6 +1,6 @@
 cask "loom" do
-  version "0.46.4"
-  sha256 "d83eb57ee102aa06feb8b80d64caf8a15f15c1e5d8904592713031437203162c"
+  version "0.46.5"
+  sha256 "97f9a452d922a54ef88f65f20962db4bb1b2a39dbdd3dae92db693db6fdfe462"
 
   url "https://cdn.loom.com/desktop-packages/Loom-#{version}.dmg"
   appcast "https://s3-us-west-2.amazonaws.com/loom.desktop.packages/loom-inc-production/desktop-packages/latest-mac.yml"
