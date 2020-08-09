072a0cb43d72c724e753c28b6579c834a28a8cd3
Randall <ran.dall@icloud.com>
2020-08-08 18:52:35 -0700

    Update data-rescue from 6.0.0 to 6.0.1

--- Casks/data-rescue.rb
+++ Casks/data-rescue.rb
@@ -1,6 +1,6 @@
 cask "data-rescue" do
-  version "6.0.0"
-  sha256 "3b317918752603a116d34126fe5ef1894de7370b747046438a18b4295dce2fd9"
+  version "6.0.1"
+  sha256 "b6f14dd90acca70b9c35c46181e133c95c81e0792c3a0cf50ad2d0f6680aa19f"
 
   url "https://downloads.prosofteng.com/dr/Data_Rescue_#{version}.dmg"
   appcast "https://www.prosofteng.com/resources/dr#{version.major}/dr#{version.major}_updates_mac.xml"
