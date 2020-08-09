e1cfe95ccbfe1ba58853c939a0a3bc06131b58be
Randall <17261190+ran-dall@users.noreply.github.com>
2020-08-08 19:10:11 -0700

    Update altair-graphql-client from 2.4.11 to 2.4.12 (#87279)

--- Casks/altair-graphql-client.rb
+++ Casks/altair-graphql-client.rb
@@ -1,6 +1,6 @@
 cask "altair-graphql-client" do
-  version "2.4.11"
-  sha256 "01bca7d6532858904b034526da0d06f035f5a80b10f8876223b137a73996cc19"
+  version "2.4.12"
+  sha256 "743836a8159da30c699034679c860be6c6be9e9be6400c6c515314a1302c1cc8"
 
   # github.com/imolorhe/altair/ was verified as official when first introduced to the cask
   url "https://github.com/imolorhe/altair/releases/download/v#{version}/altair_#{version}_mac.zip"
