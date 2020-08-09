b87d028edbd0b91d4b18558773c9ec1761e3b7bb
suschizu <brigitte@corecode.io>
2020-08-09 23:32:25 +0200

    Update unity-download-assistant.rb

--- Casks/unity-download-assistant.rb
+++ Casks/unity-download-assistant.rb
@@ -1,5 +1,5 @@
 cask "unity-download-assistant" do
-  version "2019.4.2f1,20b4642a3455"
+  version "2020.1.1f1,2285c3239188"
   sha256 "732a59b3c664eee3b05063aa13922898be3e51544d0510e19533c9afbb107c4d"
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/UnityDownloadAssistant-#{version.before_comma}.dmg"
