19a62ad7dfe79c24ea3df8e5d98106d9a0ff277d
jota3 <jgrienenberger@gmail.com>
2020-08-10 11:55:21 +0200

    Fix style in flutter cask

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -1,17 +1,17 @@
-cask 'flutter' do
-  version '1.20.1'
-  sha256 'a6364d48455bd7eb749bcbd0b7c71b985bccb0149ea0dd95c6bef3bbb2e9f91b'
+cask "flutter" do
+  version "1.20.1"
+  sha256 "a6364d48455bd7eb749bcbd0b7c71b985bccb0149ea0dd95c6bef3bbb2e9f91b"
 
   # storage.googleapis.com/flutter_infra/releases/stable/macos/ was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_#{version}-stable.zip"
-  name 'Flutter SDK'
-  homepage 'https://flutter.dev/'
+  name "Flutter SDK"
+  homepage "https://flutter.dev/"
 
-  conflicts_with cask: 'dart'
-  depends_on macos: '>= :catalina'
+  conflicts_with cask: "dart"
+  depends_on macos: ">= :catalina"
 
-  app 'flutter'
-  binary 'flutter/bin/flutter'
+  app "flutter"
+  binary "flutter/bin/flutter"
 
   uninstall rmdir: [
     "#{appdir}/flutter",
