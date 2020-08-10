00274f3cbd03ebf4b36953d1672e766c6f5d825f
suschizu <brigitte@corecode.io>
2020-08-10 12:35:39 +0200

    Update unity-android-support-for-editor.rb from 2019.4.2f1,20b4642a3455 to 2020.1.1f1,2285c3239188 (#87316)
    * Update unity-android-support-for-editor.rb
    
    * Update unity-android-support-for-editor.rb

--- Casks/unity-android-support-for-editor.rb
+++ Casks/unity-android-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask "unity-android-support-for-editor" do
-  version "2019.4.2f1,20b4642a3455"
-  sha256 "f55544548ee1730ab34249ac3641739738e2d3a99fa741597291862458f12513"
+  version "2020.1.1f1,2285c3239188"
+  sha256 "b5ba15492f91bad92058384e3d0619aaaa7b8fea9ae1c85fe6f117f7161393f7"
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-#{version.before_comma}.pkg"
   appcast "https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json"
