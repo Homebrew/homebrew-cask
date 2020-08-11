d7f61885f6f76823e93fe24bf9ee357cdd161718
Jason Charrier <jcharr1@users.noreply.github.com>
2020-08-11 18:19:56 -0500

    Update banktivity from 7.5.1 to 7.5.2 (#87446)

--- Casks/banktivity.rb
+++ Casks/banktivity.rb
@@ -1,6 +1,6 @@
 cask "banktivity" do
-  version "7.5.1"
-  sha256 "68728cfdb9b0b145cac19cbcabafc20166c564a2d91e3e0f4cb2200e1b22a73a"
+  version "7.5.2"
+  sha256 "17b629f8af9d0d0e012595005380472f88a1fd95d83836c4cb46a7c6e5a20196"
 
   # iggsoft.com/ was verified as official when first introduced to the cask
   url "https://www.iggsoft.com/banktivity/Banktivity#{version.major}_Web.dmg"
