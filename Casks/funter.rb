73420f5ffc0c04153857fe03074d8eadaa049de4
Randall <ran.dall@icloud.com>
2020-08-10 17:36:03 -0700

    Update funter from 5.0 to 5.0.1

--- Casks/funter.rb
+++ Casks/funter.rb
@@ -4,8 +4,8 @@ cask "funter" do
     sha256 "b059000717f7f8af67ee027d81aea6c2d9f343e0eb72e439a9f286ab265b7b93"
     url "https://nektony.com/download/funter/funter_10.12.dmg"
   else
-    version "5.0"
-    sha256 "16320269bb384cdbe3d2c94fb7033e6479a576b31d0548d73d6f7587c1af6e34"
+    version "5.0.1"
+    sha256 "ecb8280144c72b9bd06863be33b534a92fa9ef73b3c6b21f05a3a870d8bc5704"
     url "https://nektony.com/download/funter/Funter.dmg"
     appcast "https://nektony.com/pro-support/funter/update/update.xml"
   end
