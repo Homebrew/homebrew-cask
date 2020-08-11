079df4fda6c76fded22be7a3ecb9b167705c57cd
Hanlin He <hanlhe@cisco.com>
2020-08-10 21:07:31 -0700

    Update uu-booster from 2.1.2,164 to 2.1.3,169 (#87399)

--- Casks/uu-booster.rb
+++ Casks/uu-booster.rb
@@ -1,6 +1,6 @@
 cask "uu-booster" do
-  version "2.1.2,164"
-  sha256 "3fb99880683ec6d82b781f975e6422f5c746ca250b04786f8162d4494a5541fe"
+  version "2.1.3,169"
+  sha256 "dc7c9d522e4718479edefef0b86488b0f27ed72aec70e6db404ff6ea178acd11"
 
   # uu.gdl.netease.com/ was verified as official when first introduced to the cask
   url "https://uu.gdl.netease.com/UU-macOS-#{version.before_comma}(#{version.after_comma}).dmg"
