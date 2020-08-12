e9df9303101f424d752ba2a93b4b6b80fe66a41c
Randall <17261190+ran-dall@users.noreply.github.com>
2020-08-12 19:36:40 +0000

    Update mqttx from 1.3.2 to 1.3.3 (#87506)

--- Casks/mqttx.rb
+++ Casks/mqttx.rb
@@ -1,6 +1,6 @@
 cask "mqttx" do
-  version "1.3.2"
-  sha256 "18311102107debddb9abcbef0b2c5950608e6bd5a8d9ca706a34cb3d35381e73"
+  version "1.3.3"
+  sha256 "5dd9f9ac4e3cf55daca657f6bc4bdeabb83b73ef10cc2c37a23f9975ef81ecc7"
 
   # github.com/emqx/MQTTX/ was verified as official when first introduced to the cask
   url "https://github.com/emqx/MQTTX/releases/download/v#{version}/MQTTX-#{version}-mac.zip"
