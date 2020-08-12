6d88110ed1f34a93f5ee62426a34c93ddb29f407
Seeker <meaningseeking@protonmail.com>
2020-08-12 09:59:11 -0700

    Update intel-psxe-ce-c-plus-plus from 2020.0.015 to 2020.2.899,16768

--- Casks/intel-psxe-ce-c-plus-plus.rb
+++ Casks/intel-psxe-ce-c-plus-plus.rb
@@ -1,15 +1,15 @@
 cask "intel-psxe-ce-c-plus-plus" do
-  version "2020.0.015"
-  sha256 "16b6cab00557e297f3e9b0ec647c1346ca707ccbd36bd16ca007ae1147d1ea46"
+  version "2020.2.899,16768"
+  sha256 "d224821978c56d3744ee04ae1f1e400e2b6294a4d6186bd06d8c4fd6565c4967"
 
-  url "https://registrationcenter-download.intel.com/akdlm/irc_nas/tec/16237/m_ccompxe_#{version}.dmg"
+  url "https://registrationcenter-download.intel.com/akdlm/irc_nas/tec/#{version.after_comma}/m_ccompxe_#{version.before_comma}.dmg"
   name "Intel Parallel Studio XE Composer Edition for C++"
   homepage "https://software.intel.com/en-us/parallel-studio-xe"
 
-  installer manual: "m_ccompxe_#{version}.app"
+  installer manual: "m_ccompxe_#{version.before_comma}.app"
 
   uninstall script: {
-    executable: "m_ccompxe_#{version}.app/Contents/MacOS/uninstall.sh",
+    executable: "m_ccompxe_#{version.before_comma}.app/Contents/MacOS/uninstall.sh",
     args:       ["--silent"],
     sudo:       true,
   }
