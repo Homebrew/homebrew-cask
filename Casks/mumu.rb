cc4cbdcd1954e1dc14e0244c198765bc7ac92a8f
suschizu <brigitte@corecode.io>
2020-08-12 12:07:54 +0200

    Update mumu from 1.1.1,qRhq2lkVQseTWZSHvJFo to 1.1.2,AjOnLLkkT8ycuoFpJJ1p (#87492)

--- Casks/mumu.rb
+++ Casks/mumu.rb
@@ -1,6 +1,6 @@
 cask "mumu" do
-  version "1.1.1,qRhq2lkVQseTWZSHvJFo"
-  sha256 "f122bb92fef91891dbd4f1f4a085614490c795af4bbe2e6ecfa4bf0b5cd7a905"
+  version "1.1.2,AjOnLLkkT8ycuoFpJJ1p"
+  sha256 "a0672673e7260cba26ea6d698a6421688159bbc0d7c1185f3b4b0f056336e6c2"
 
   # paddle.s3.amazonaws.com/fulfillment_downloads/116824/597910/ was verified as official when first introduced to the cask
   url "https://paddle.s3.amazonaws.com/fulfillment_downloads/116824/597910/#{version.after_comma}_Mumu%20#{version.before_comma}.dmg"
