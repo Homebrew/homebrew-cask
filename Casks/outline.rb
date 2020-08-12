f3505c9fbce400acc9922c82f2798bf8e1e73599
Randall <17261190+ran-dall@users.noreply.github.com>
2020-08-12 20:16:27 +0000

    Update outline.rb

--- Casks/outline.rb
+++ Casks/outline.rb
@@ -2,10 +2,10 @@ cask "outline" do
   version "3.2007.1"
   sha256 "8e5b231d0a33b0fca5be533b7a1fd546aa699515bc93f349991d0d186c17ceae"
 
-  url "http://static.outline.ws/versions/Outline_#{version}.zip"
+  url "https://static.outline.ws/versions/Outline_#{version}.zip"
   appcast "https://gorillized.s3.amazonaws.com/versions/update_channel.xml"
   name "Outline"
-  homepage "http://outline.ws/"
+  homepage "https://outline.ws/"
 
   app "Outline.app"
 end
