455c0e487eecbcbc4bbc7852a52fc1b9866bd7ef
Randall <ran.dall@icloud.com>
2020-08-11 17:59:45 -0700

    Update permute from 3.5.1,2287 to 3.5.2,2290

--- Casks/permute.rb
+++ Casks/permute.rb
@@ -1,6 +1,6 @@
 cask "permute" do
-  version "3.5.1,2287"
-  sha256 "775e8e87b4d7026da12245b43a6a9bdc063c69ea3e0ff23f37b47e8f11368b96"
+  version "3.5.2,2290"
+  sha256 "66caabfc154ca08f920a3f57fa934a05143a88915261875f70fe3739f136a23e"
 
   url "https://trial.charliemonroe.net/permute/v#{version.major}/Permute_#{version.major}_#{version.after_comma}.dmg"
   appcast "https://trial.charliemonroe.net/permute/updates_#{version.major}.xml"
