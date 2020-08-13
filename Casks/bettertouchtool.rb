79f89303f0577b30b04edc56efd793154e332b49
Miccal Matthews <miccal.matthews@gmail.com>
2020-08-13 18:02:22 +0800

    Update bettertouchtool.rb

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -1,9 +1,9 @@
 cask "bettertouchtool" do
-  version "3.402"
+  version "3.402-1633"
   sha256 "578f2e62805ae21e880c60e13ec455ec6057745edaff14214eb5c6eac5cf7705"
 
   url "https://folivora.ai/releases/BetterTouchTool.zip"
-  appcast "https://updates.folivora.ai/bettertouchtool_release_notes.html"
+  appcast "https://www.corecode.io/macupdater/appcasts/bettertouchtool.txt"
   name "BetterTouchTool"
   homepage "https://folivora.ai/"
 
