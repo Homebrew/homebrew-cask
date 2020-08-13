d980266b0a4c34733a64c83cc591d4cc0bc6c706
Miccal Matthews <miccal.matthews@gmail.com>
2020-08-13 18:19:33 +0800

    Update bettertouchtool url (#87497)

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -2,8 +2,7 @@ cask "bettertouchtool" do
   version "3.402-1633"
   sha256 "578f2e62805ae21e880c60e13ec455ec6057745edaff14214eb5c6eac5cf7705"
 
-  # bettertouchtool.net/releases/ was verified as official when first introduced to the cask
-  url "https://bettertouchtool.net/releases/btt#{version}.zip"
+  url "https://folivora.ai/releases/BetterTouchTool.zip"
   appcast "https://www.corecode.io/macupdater/appcasts/bettertouchtool.txt"
   name "BetterTouchTool"
   homepage "https://folivora.ai/"
