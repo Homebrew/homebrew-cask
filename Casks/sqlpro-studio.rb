001ba70dc3ab327da8bf68d1847202e79505ebed
Randall <ran.dall@icloud.com>
2020-08-14 13:18:28 -0700

    Update sqlpro-studio from 2020.68 to 2020.72

--- Casks/sqlpro-studio.rb
+++ Casks/sqlpro-studio.rb
@@ -1,6 +1,6 @@
 cask "sqlpro-studio" do
-  version "2020.68"
-  sha256 "3e7b85e5d8431ae863c1fa0105830484ce0531b0acb5d0c45946eba1486d0109"
+  version "2020.72"
+  sha256 "d84368a20408c48952cb6c92f8175dcc41a66cbd067a84b50f96b38e89f9afe4"
 
   # d3fwkemdw8spx3.cloudfront.net/studio/ was verified as official when first introduced to the cask
   url "https://d3fwkemdw8spx3.cloudfront.net/studio/SQLProStudio.#{version}.app.zip"
