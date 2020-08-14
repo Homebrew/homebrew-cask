d0cc42dd8240c988c9407657a11e66decdfe4599
Randall <17261190+ran-dall@users.noreply.github.com>
2020-08-14 05:43:10 +0000

    Update screens from 4.7.8,25692 to 4.7.9,25710 (#87582)

--- Casks/screens.rb
+++ Casks/screens.rb
@@ -1,6 +1,6 @@
 cask "screens" do
-  version "4.7.8,25692"
-  sha256 "2da1585c8061aff3d4a54c88c1da99e6dee5f5fa741b2a4988c035fabe9f19c5"
+  version "4.7.9,25710"
+  sha256 "d12674a10552c9012aba2decedf0bd066da5196700a488fd0172b2f2b9f51c3d"
 
   url "https://updates.edovia.com/com.edovia.screens#{version.major}.mac/Screens_#{version.before_comma}b#{version.after_comma}.zip"
   appcast "https://updates.edovia.com/com.edovia.screens#{version.major}.mac/appcast.xml"
