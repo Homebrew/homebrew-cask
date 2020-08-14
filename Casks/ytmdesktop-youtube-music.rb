d6a55a3561fe853ed8e54d320b523315130824ff
Adler Luiz <adlerluiz10@gmail.com>
2020-08-14 18:23:23 -0300

    Update ytmdesktop-youtube-music url 1.12.0 (#87601)

--- Casks/ytmdesktop-youtube-music.rb
+++ Casks/ytmdesktop-youtube-music.rb
@@ -2,7 +2,8 @@ cask "ytmdesktop-youtube-music" do
   version "1.12.0"
   sha256 "6d13d4107257d44bc0cea7083228b01a714c0abd1f4c797440ec03175dd8f13f"
 
-  url "https://dl.ytmdesktop.app/mac/YouTube%20Music%20Desktop%20App-#{version}.dmg"
+  # github.com/ytmdesktop was verified as official when first introduced to the cask
+  url "https://github.com/ytmdesktop/ytmdesktop/releases/download/v#{version}/YouTube.Music.Desktop.App-#{version}.dmg"
   appcast "https://github.com/ytmdesktop/ytmdesktop/releases.atom"
   name "YouTube Music Desktop App"
   homepage "https://ytmdesktop.app/"
