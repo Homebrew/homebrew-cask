44d889732b9f5318a0b69383163c90190244f4ab
OttNorml <ottnorml@gmail.com>
2020-08-14 10:21:24 +0200

    Update lens from 3.5.2 to 3.5.3

--- Casks/lens.rb
+++ Casks/lens.rb
@@ -1,6 +1,6 @@
 cask "lens" do
-  version "3.5.2"
-  sha256 "172ff2657453d001104e1e39ad384d66b0e16e3b1e6ecdc176e19449aa0bb0d9"
+  version "3.5.3"
+  sha256 "821478667fef53831f782365c731332909dbe33fa73e326722d21157f49e301d"
 
   # github.com/lensapp/lens/ was verified as official when first introduced to the cask
   url "https://github.com/lensapp/lens/releases/download/v#{version}/Lens-#{version}.dmg"
