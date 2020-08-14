19f62fa3900f800a0992ccc9764ace035d01791f
Randall <17261190+ran-dall@users.noreply.github.com>
2020-08-14 22:54:45 +0000

    Update axe-edit-iii from 1.06.08 to 1.06.09 (#87627)

--- Casks/axe-edit-iii.rb
+++ Casks/axe-edit-iii.rb
@@ -1,6 +1,6 @@
 cask "axe-edit-iii" do
-  version "1.06.08"
-  sha256 "45d40ce334a9c2e4c500569add720d87735b5ecf47e9d976ca2cd6b5eebe59f4"
+  version "1.06.09"
+  sha256 "6c7476558591b6bb95fb55de0027352b4fec76aff85ffecdc1275ce8cb180221"
 
   url "https://www.fractalaudio.com/downloads/Axe-Edit-III/Axe-Edit-III-OSX-v#{version.tr(".", "p")}.dmg"
   appcast "https://www.fractalaudio.com/axe-fx-iii-edit/"
