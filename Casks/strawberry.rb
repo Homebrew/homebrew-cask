e1995bd28a885525640ef17bfefd0b067017bad2
Randall <17261190+ran-dall@users.noreply.github.com>
2020-08-15 18:35:02 +0000

    Update strawberry from 0.6.13 to 0.7.1 (#87651)

--- Casks/strawberry.rb
+++ Casks/strawberry.rb
@@ -1,6 +1,6 @@
 cask "strawberry" do
-  version "0.6.13"
-  sha256 "cdf2d8cfc6189f9d20d83c51d880779bb71841787f31ec351f8a899029bd348c"
+  version "0.7.1"
+  sha256 "419f75ebe724a40559ffb002eb6ade1023ae83db9988f55ae6421e4f8919ab9c"
 
   # github.com/strawberrymusicplayer/strawberry/ was verified as official when first introduced to the cask
   url "https://github.com/strawberrymusicplayer/strawberry/releases/download/#{version}/strawberry-#{version}.dmg"
