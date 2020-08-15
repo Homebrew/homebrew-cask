8bc686d850f91b8f1a5205cb96b6265f27159e83
Brigitte Zmoelnig <brigitte@corecode.io>
2020-08-15 19:33:24 +0200

    Update cutter from 1.11.0 to 1.11.1

--- Casks/cutter.rb
+++ Casks/cutter.rb
@@ -1,6 +1,6 @@
 cask "cutter" do
-  version "1.11.0"
-  sha256 "59237d9fd90c4dd932f756eddfe5d27d9d53352d28ba0ec92bfe4ec19b200299"
+  version "1.11.1"
+  sha256 "6f35931d52915ce093825ecad6288c1798ca99af7cffde1592a7e2119deb98b6"
 
   # github.com/radareorg/cutter/ was verified as official when first introduced to the cask
   url "https://github.com/radareorg/cutter/releases/download/v#{version}/Cutter-v#{version}-x64.macOS.dmg"
