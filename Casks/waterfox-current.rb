d1d26e2553b4269341c95272578671d6fc3c49b9
suschizu <brigitte@corecode.io>
2020-08-15 19:34:32 +0200

    Update waterfox-current from 2020.07.1,6820.7.28 to 2020.07.2.1,6820.8.15 (#87646)

--- Casks/waterfox-current.rb
+++ Casks/waterfox-current.rb
@@ -1,6 +1,6 @@
 cask "waterfox-current" do
-  version "2020.07.1,6820.7.28"
-  sha256 "3cd9fcefd65310c172111a680597019085ac5a9badf39498877da1952e659783"
+  version "2020.07.2.1,6820.8.15"
+  sha256 "ea5ab1e637638a7687acb6f31a37587979745a3311fabb1fc39a0f455737b4fe"
 
   url "https://cdn.waterfox.net/releases/osx64/installer/Waterfox%20Current%20#{version.before_comma}%20Setup.dmg"
   appcast "https://www.waterfox.net/download/"
