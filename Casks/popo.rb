7cce996e6d7e532435c1f0ea22b8d1f3325c3e40
Markus Reiter <me@reitermark.us>
2020-08-15 03:47:47 +0200

    Update popo.rb

--- Casks/popo.rb
+++ Casks/popo.rb
@@ -3,7 +3,8 @@ cask "popo" do
   sha256 "3aa0ce487dd3494c9374fe3f1345ad1b04312df0fcb9e88bc85be6ba6b5ebb69"
 
   url "https://popo.netease.com/file/popomac/POPO_Mac_V#{version.dots_to_underscores}.dmg"
-  appcast "https://popo.netease.com/"
+  appcast "http://http.popo.netease.com:8080/api/open/jsonp/check_version?device=4",
+          must_contain: version.dots_to_underscores
   name "NetEase POPO"
   homepage "https://popo.netease.com/"
 
