8868a4419b0444d6ce018cc4e9e777a0a345432d
Alex Young <foray1010@users.noreply.github.com>
2020-08-15 14:42:13 +0800

    use HTTPS to fetch nosqlbooster-for-mongodb (#87639)

--- Casks/nosqlbooster-for-mongodb.rb
+++ Casks/nosqlbooster-for-mongodb.rb
@@ -2,7 +2,8 @@ cask "nosqlbooster-for-mongodb" do
   version "6.1.0"
   sha256 "ed01c3d31adf37ddd2b7938370541f830689563f2175d9dbfe88be8a147b6640"
 
-  url "https://nosqlbooster.com/s3/download/releasesv#{version.major}/nosqlbooster4mongo-#{version}.dmg"
+  # mongobooster.com was verified as official when first introduced to the cask
+  url "https://s3.mongobooster.com/download/releasesv#{version.major}/nosqlbooster4mongo-#{version}.dmg"
   appcast "https://nosqlbooster.com/downloads"
   name "NoSQLBooster for MongoDB"
   homepage "https://nosqlbooster.com/"
