5eb8e99e79062fc283f44bb2cb0e81bfab437e72
Andrei Borisov <andrei@borisov.dev>
2020-08-16 20:54:55 +0300

    Update version and installed location

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -1,15 +1,18 @@
 cask "flutter" do
-  version "1.20.1"
-  sha256 "a6364d48455bd7eb749bcbd0b7c71b985bccb0149ea0dd95c6bef3bbb2e9f91b"
+  version "1.20.2"
+  sha256 "c801653e989811b60f79d3edffa808bd1fba5b771ba8492bfc53ea49302afc0e"
 
   # storage.googleapis.com/flutter_infra/releases/stable/macos/ was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_#{version}-stable.zip"
   name "Flutter SDK"
+  desc "UI toolkit for building applications for mobile, web and desktop"
   homepage "https://flutter.dev/"
 
-  conflicts_with cask: "dart"
+  # Flutter manages its components by itself so it should not be updated by
+  # Homebrew in most cases, else those components would be lost on upgrade
+  auto_updates true
   depends_on macos: ">= :catalina"
 
-  suite "flutter"
+  binary "flutter/bin/dart"
   binary "flutter/bin/flutter"
 end
