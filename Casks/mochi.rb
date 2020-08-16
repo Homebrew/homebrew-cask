de1725d2ae49883b3bedfabf70ec029356d8d6a6
Randall <17261190+ran-dall@users.noreply.github.com>
2020-08-16 19:07:29 +0000

    Update mochi from 1.6.4 to 1.6.5 (#87707)

--- Casks/mochi.rb
+++ Casks/mochi.rb
@@ -1,6 +1,6 @@
 cask "mochi" do
-  version "1.6.4"
-  sha256 "c4f03fe6bd987c5fed952967c6065778b217d6fcbdec2fcbbad1f1f8c524bd15"
+  version "1.6.5"
+  sha256 "6e12ae11071f86304d229d19e1586f7b7ae98609bfa5293a8c60dd27ae268aba"
 
   url "https://mochi.cards/releases/Mochi-#{version}.dmg"
   appcast "https://mochi.cards/"
