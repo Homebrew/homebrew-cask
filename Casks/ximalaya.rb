265a1e58c2ab4e0afd45b14a7693b44ac15cde70
Randall <17261190+ran-dall@users.noreply.github.com>
2020-08-16 19:08:13 +0000

    Update ximalaya from 1.2.21,1596708911 to 1.2.21,1597310073 (#87706)

--- Casks/ximalaya.rb
+++ Casks/ximalaya.rb
@@ -1,6 +1,6 @@
 cask "ximalaya" do
-  version "1.2.21,1596708911"
-  sha256 "5f45015b94f0445ad69f09c1a7e4ed5ca37acdcbe68bdc6003eba19b3ef451a3"
+  version "1.2.21,1597310073"
+  sha256 "fcc76ea2971a623967f7d0bc149692c599ba9ddb8ad1c782447a06c8ac8db9d0"
 
   # s1.xmcdn.com/ was verified as official when first introduced to the cask
   url "http://s1.xmcdn.com/yx/ximalaya-pc/#{version.before_comma}/download/Ximalaya-#{version.before_comma}_#{version.after_comma}.dmg"
