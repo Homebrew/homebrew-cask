7cb52aca315d02c91b528f7c4d3e1bb1480204cd
Randall <ran.dall@icloud.com>
2020-08-16 11:28:57 -0700

    Update strawberry from 0.7.1 to 0.7.2

--- Casks/strawberry.rb
+++ Casks/strawberry.rb
@@ -1,6 +1,6 @@
 cask "strawberry" do
-  version "0.7.1"
-  sha256 "419f75ebe724a40559ffb002eb6ade1023ae83db9988f55ae6421e4f8919ab9c"
+  version "0.7.2"
+  sha256 "f85d1ff66d501f2e0c06349ce98e237fd91d73894a71e96d8363ffd804ccc04e"
 
   # github.com/strawberrymusicplayer/strawberry/ was verified as official when first introduced to the cask
   url "https://github.com/strawberrymusicplayer/strawberry/releases/download/#{version}/strawberry-#{version}.dmg"
