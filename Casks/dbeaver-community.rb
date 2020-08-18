c9b9f69ee9150daa66cd461cf27b4390ec066848
Michael Schmäschke <michael.schmaeschke@d-velop.de>
2020-08-18 11:32:32 +0200

    Update dbeaver-community (#87761)

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,6 +1,6 @@
 cask "dbeaver-community" do
   version "7.1.5"
-  sha256 "dd640534d8581e39a864c3750cc44f07f5069b78ad3466ef417406b288ce6069"
+  sha256 "c7550d7c13057a80d6bb2c760d172c98a6e3368d68fd397732003d6139829548"
 
   # github.com/dbeaver/dbeaver/ was verified as official when first introduced to the cask
   url "https://github.com/dbeaver/dbeaver/releases/download/#{version}/dbeaver-ce-#{version}-macos.dmg"
