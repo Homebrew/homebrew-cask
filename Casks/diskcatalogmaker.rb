dfc5419ed112623fb1c30e638631452cd1743f4c
suschizu <brigitte@corecode.io>
2020-08-18 21:34:09 +0200

    Update diskcatalogmaker from 8.2.2 to 8.2.4 (#87773)

--- Casks/diskcatalogmaker.rb
+++ Casks/diskcatalogmaker.rb
@@ -1,6 +1,6 @@
 cask "diskcatalogmaker" do
-  version "8.2.2"
-  sha256 "38c3d6ae60345841433a2c4d9d1a86c3a0dbabd4c03acee32af0d51a0d54960b"
+  version "8.2.4"
+  sha256 "5176424267508b37220b913e609760b13413c781ef8cf610d87f4b6fa86c95b1"
 
   url "https://download.diskcatalogmaker.com/zip/DiskCatalogMaker.zip"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.diskcatalogmaker.com/zip/DiskCatalogMaker.zip",
