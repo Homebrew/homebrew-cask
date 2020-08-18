371fff9fad345f6f6343524d45edc72032733a75
Mark Trolley <marktrolley@gmail.com>
2020-08-18 10:48:52 -0400

    Update musicbrainz-picard from 2.4.1 to 2.4.2 (#87764)

--- Casks/musicbrainz-picard.rb
+++ Casks/musicbrainz-picard.rb
@@ -1,6 +1,6 @@
 cask "musicbrainz-picard" do
-  version "2.4.1"
-  sha256 "3eb56ecc3895d3a497a45b124945237c2e6d0711778b162ea32e420ea05a3b18"
+  version "2.4.2"
+  sha256 "5d86a5c34461a7df3b55663dc7f3fb27371e3defb58225b85e95df5acd691ab5"
 
   # musicbrainz.osuosl.org/pub/ was verified as official when first introduced to the cask
   url "https://musicbrainz.osuosl.org/pub/musicbrainz/picard/MusicBrainz-Picard-#{version}.dmg"
