7712de45f98de464c581a3044d3c9aa481c88b17
Randall <ran.dall@icloud.com>
2020-08-18 18:52:15 -0700

    Update tibco-jaspersoft-studio from 6.13.0 to 6.14.0

--- Casks/tibco-jaspersoft-studio.rb
+++ Casks/tibco-jaspersoft-studio.rb
@@ -1,6 +1,6 @@
 cask "tibco-jaspersoft-studio" do
-  version "6.13.0"
-  sha256 "7772b404329b7c7a66f951905fab4f82f47105337a08dce82ea22d15e2d4be88"
+  version "6.14.0"
+  sha256 "8dd900162503297297ec01e07e1f34633e8b82aa8abe6343e64b57b1ae8f06a2"
 
   # sourceforge.net/jasperstudio/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/jasperstudio/JaspersoftStudio-#{version}/TIB_js-studiocomm_#{version}_macosx_x86_64.dmg"
