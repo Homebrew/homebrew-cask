fea6880cbe6857a3090a0b6aa8e6f69d1d69e4e0
Randall <ran.dall@icloud.com>
2020-08-18 18:38:50 -0700

    Update airflow from 3.1.8 to 3.1.9

--- Casks/airflow.rb
+++ Casks/airflow.rb
@@ -1,6 +1,6 @@
 cask "airflow" do
-  version "3.1.8"
-  sha256 "03c8c4741f074f632a1c3eae048e59f78a82cb87ca2eb617f4f98bd1885e6966"
+  version "3.1.9"
+  sha256 "dced2026abae2db3747d5ca439a2cef45c9b53cfffe8bbcdb4adb438c2c2f777"
 
   # cdn.downloads.iocave.net/Airflow/ was verified as official when first introduced to the cask
   url "https://cdn.downloads.iocave.net/Airflow/Airflow%20#{version}.zip"
