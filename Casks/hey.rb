5ca9ca63f54d483d7a7e1aab4b0a0fc9b2248443
Randall <ran.dall@icloud.com>
2020-08-18 18:28:39 -0700

    Update hey from 1.0.10 to 1.0.11

--- Casks/hey.rb
+++ Casks/hey.rb
@@ -1,6 +1,6 @@
 cask "hey" do
-  version "1.0.10"
-  sha256 "89d6b086e8988ce46355ca5f1ba9860969eb865e99614dbd13ce9bd712dc3847"
+  version "1.0.11"
+  sha256 "dd543fbc2b83fb1182cbaf133122bd17ea8e0ff97c12d76fd7b33a5aa1e06e31"
 
   # hey-desktop.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://hey-desktop.s3.amazonaws.com/HEY-#{version}.dmg"
