50b87a4bb4bfecc7f9857a007c5d214019291957
Randall <17261190+ran-dall@users.noreply.github.com>
2020-08-19 19:46:14 +0000

    Update wondershare-uniconverter from 12.0.2.5 to 2.0.3.17 (#87841)

--- Casks/wondershare-uniconverter.rb
+++ Casks/wondershare-uniconverter.rb
@@ -1,6 +1,6 @@
 cask "wondershare-uniconverter" do
-  version "12.0.2.5"
-  sha256 "b5489fe0f9832379e4417c962d2848de4a1f3c0d39015c597f6c4462470577b2"
+  version "2.0.3.17"
+  sha256 "f16c7777c6a9fa6b33835af15651130ced84dedd9b95c1a999d1cc0a922baf82"
 
   url "http://download.wondershare.com/cbs_down/video-converter-ultimate-mac_full735.dmg"
   name "UniConverter"
