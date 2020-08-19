70c89e5fd49b873d6758e65a2a11fcf856bf9256
Randall <ran.dall@icloud.com>
2020-08-18 18:22:49 -0700

    Update youku from 1.8.5.08111 to 1.8.5.08173

--- Casks/youku.rb
+++ Casks/youku.rb
@@ -1,6 +1,6 @@
 cask "youku" do
-  version "1.8.5.08111"
-  sha256 "de7fd34278dac6f74ef09960d5646f1154c1e720ba5c7053307f9e3a05b19c5c"
+  version "1.8.5.08173"
+  sha256 "0410fbcf1ed49eac3d21714ace2532087ca1cbe57fc3c9e673d54108215fccd3"
 
   url "https://pcclient.download.youku.com/ikumac/youkumac_#{version}.dmg"
   appcast "https://pd.youku.com/pc"
