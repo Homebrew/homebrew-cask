d0b1c15d1ff8c01f756f4976a691a128f95df805
Randall <17261190+ran-dall@users.noreply.github.com>
2020-08-20 22:09:10 +0000

    Update taskade from 3.2.0 to 3.2.1 (#87919)

--- Casks/taskade.rb
+++ Casks/taskade.rb
@@ -1,6 +1,6 @@
 cask "taskade" do
-  version "3.2.0"
-  sha256 "a831a07b977507c47a195c2cb491d4b5007279334a7b11b103cb7b73f9507826"
+  version "3.2.1"
+  sha256 "84e56c1851010baed8a2a17091383238e0f7ad6775da5e47c87c1aa1893f3095"
 
   url "https://apps.taskade.com/updates/Taskade-#{version}-mac.zip"
   appcast "https://www.taskade.com/downloads"
