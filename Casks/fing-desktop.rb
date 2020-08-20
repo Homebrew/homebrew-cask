ec9afa453d12a295a9e3ee474931ec3440804538
Alexander Bayandin <a.bayandin@gmail.com>
2020-08-20 19:53:52 +0100

    fing-desktop: 2.1.0 (#87886)

--- Casks/fing-desktop.rb
+++ Casks/fing-desktop.rb
@@ -1,19 +1,19 @@
-cask 'fing-desktop' do
-  version '2.0.0'
-  sha256 'fceb7cf920214f447333e80bf7b141f67661a423eda03041bc651e3323763810'
+cask "fing-desktop" do
+  version "2.1.0"
+  sha256 "4a9bb7c0fb8531cacc60fd4247c55ae322224cf300ecb67de303710bf9773631"
 
-  url 'https://get.fing.com/fing-desktop-releases/mac/Fing.dmg'
-  appcast 'https://get.fing.com/fing-desktop-releases/mac/latest-mac.yml'
-  name 'Fing Desktop'
-  homepage 'https://www.fing.com/products/fing-desktop'
+  url "https://get.fing.com/fing-desktop-releases/mac/Fing.dmg"
+  appcast "https://get.fing.com/fing-desktop-releases/mac/latest-mac.yml"
+  name "Fing Desktop"
+  homepage "https://www.fing.com/products/fing-desktop"
 
-  app 'Fing.app'
+  app "Fing.app"
 
-  uninstall launchctl: 'com.fing.service'
+  uninstall launchctl: "com.fing.service"
 
   zap trash: [
-               '~/Library/Application Support/Fing',
-               '~/Library/Preferences/com.fing.app.plist',
-               '~/Library/Saved Application State/com.fing.app.savedState',
-             ]
+    "~/Library/Application Support/Fing",
+    "~/Library/Preferences/com.fing.app.plist",
+    "~/Library/Saved Application State/com.fing.app.savedState",
+  ]
 end
