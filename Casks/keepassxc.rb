d981c4e3dcc3e584bd4ae6ee27e66751df42cb69
Vítor Galvão <info@vitorgalvao.com>
2020-08-20 13:04:02 +0100

    Update keepassxc.rb

--- Casks/keepassxc.rb
+++ Casks/keepassxc.rb
@@ -1,17 +1,9 @@
 cask "keepassxc" do
   version "2.6.1"
+  sha256 "ac0a74369f4009a6d5922840c3df8fe4641c11af8643cc60b9ba6103ff1eceda"
 
-  if MacOS.version <= :sierra
-    # github.com/keepassxreboot/keepassxc/ was verified as official when first introduced to the cask
-    version "2.5.4"
-    url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}-Sierra.dmg"
-    sha256 "7cd8dc34022091c240e538f7a9889afd7dc8f9f3957a66bca9d70c067045ade4"
-  else
-    # github.com/keepassxreboot/keepassxc/ was verified as official when first introduced to the cask
-    url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}.dmg"
-    sha256 "ac0a74369f4009a6d5922840c3df8fe4641c11af8643cc60b9ba6103ff1eceda"
-  end
-
+  # github.com/keepassxreboot/keepassxc/ was verified as official when first introduced to the cask
+  url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}.dmg"
   appcast "https://github.com/keepassxreboot/keepassxc/releases.atom"
   name "KeePassXC"
   homepage "https://keepassxc.org/"
