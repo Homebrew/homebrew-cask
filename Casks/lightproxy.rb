3ade27081580c7d97748b0f9864052b7ead4c446
Randall <ran.dall@icloud.com>
2020-08-20 13:44:48 -0700

    Update lightproxy from 1.1.33 to 1.1.36

--- Casks/lightproxy.rb
+++ Casks/lightproxy.rb
@@ -1,6 +1,6 @@
 cask "lightproxy" do
-  version "1.1.33"
-  sha256 "963abf41ba2b3b693ed3be33ac869183451e353cac73e57bf552977bbb04b818"
+  version "1.1.36"
+  sha256 "9d19c02a6db9c54a45436c0bd43cbaceeab0ba4da787f13cc43b8f9d85a12781"
 
   # github.com/alibaba/lightproxy/ was verified as official when first introduced to the cask
   url "https://github.com/alibaba/lightproxy/releases/download/v#{version}/LightProxy-#{version}.dmg"
