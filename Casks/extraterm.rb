fabfa7c7978d2253c6894496c41b148cfe3d3d84
Randall <ran.dall@icloud.com>
2020-08-20 13:47:47 -0700

    Update extraterm from 0.51.0 to 0.52.0

--- Casks/extraterm.rb
+++ Casks/extraterm.rb
@@ -1,6 +1,6 @@
 cask "extraterm" do
-  version "0.51.0"
-  sha256 "c9453666da9e32d83fc1cf570419f4c2977a898d2c080afe4c9a1ddfa324ca0f"
+  version "0.52.0"
+  sha256 "4b0491ec5f0d83e65ea1fefc555ccaffd78afda0e825c41b301cd58b51eb4b04"
 
   # github.com/sedwards2009/extraterm/ was verified as official when first introduced to the cask
   url "https://github.com/sedwards2009/extraterm/releases/download/v#{version}/extraterm-#{version}-darwin-x64.zip"
