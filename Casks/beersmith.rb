d34e608474e704c986f1159073ae2662b7a5e154
Randall <ran.dall@icloud.com>
2020-08-20 15:02:10 -0700

    Update beersmith from 3.1.7 to 3.1.8

--- Casks/beersmith.rb
+++ Casks/beersmith.rb
@@ -1,6 +1,6 @@
 cask "beersmith" do
-  version "3.1.7"
-  sha256 "d8b2b460ac1aea8278b248294dfafb1a753e03e68d1fd680e82cd77719bd243e"
+  version "3.1.8"
+  sha256 "0f2f23bcccd7e22fc0bc0b4ab63cf31a5dbe854c9135839919db1f6fce61edc0"
 
   # beersmith3-1.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://beersmith3-1.s3.amazonaws.com/BeerSmith#{version.dots_to_underscores}.dmg"
