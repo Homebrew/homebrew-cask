f33527ef0c13eea2bff9f1907d5eca903e7c42eb
Daniel Hagen <daniel.b.hagen@gmail.com>
2020-08-19 20:44:21 -0500

    Adding missing appcask url

--- Casks/teradici-pcoip-client.rb
+++ Casks/teradici-pcoip-client.rb
@@ -3,6 +3,7 @@ cask "teradici-pcoip-client" do
   sha256 "70a2cbb8e329cb459ff076ed754e44d1f168d291714bd4f4c5efebb87902959d"
 
   url "https://downloads.teradici.com/mac/stable/pcoip-client_#{version}.dmg"
+  appcast "https://downloads.teradici.com/mac/stable/"
   name "Teradici PCoIP Software Client for macOS"
   desc "PCoIP client for VM Agents and Remote Workstation Cards"
   homepage "https://docs.teradici.com/find/product/software-and-mobile-clients/"
