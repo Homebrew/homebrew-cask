70cae30543078d87c21f6b822f19389c1e26834f
suschizu <brigitte@corecode.io>
2020-08-21 16:36:27 +0200

    Update qcad.rb

--- Casks/qcad.rb
+++ Casks/qcad.rb
@@ -1,11 +1,11 @@
 cask "qcad" do
-  version "3.24.3"
+  version "3.25.0"
 
   if MacOS.version <= :high_sierra
-    sha256 "2313e906d1892ad7a433ff23074c9f28ab747e7731231c0c8a02ff6eab5ae44a"
+    sha256 "f904031d67ee95adadc773d314ca154c448324447b628527df51d213f90e2c70"
     url "https://www.qcad.org/archives/qcad/qcad-#{version}-trial-macos-10.10-10.13.dmg"
   else
-    sha256 "9c03efd3d5c41952637cd6bde7b090466ef4f8d87229f509194268f3cadff4ef"
+    sha256 "6ceb9145f8646d5a2c5c9de59e952b68bbdf1f544275b7ada95d0173eed84ea4"
     url "https://www.qcad.org/archives/qcad/qcad-#{version}-trial-macos-10.14-10.15.dmg"
   end
 
