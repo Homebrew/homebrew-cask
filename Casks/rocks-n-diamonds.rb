4b8ad59661e9352974eab9596eb8044d8030a0a1
Brigitte Zmoelnig <brigitte@corecode.io>
2020-08-21 20:17:07 +0200

    Update rocks-n-diamonds from 4.2.0.0 to 4.2.0.1

--- Casks/rocks-n-diamonds.rb
+++ Casks/rocks-n-diamonds.rb
@@ -1,6 +1,6 @@
 cask "rocks-n-diamonds" do
-  version "4.2.0.0"
-  sha256 "baa74572a248190c620881a6a80f8087e525b591a4de678db88a8ec24e45a27a"
+  version "4.2.0.1"
+  sha256 "316b6a579eee703b178abff9d2f6fc5e0796d79a9afcb2d4be9744f2dfa94d4b"
 
   url "https://www.artsoft.org/RELEASES/macosx/rocksndiamonds/rocksndiamonds-#{version}.dmg"
   appcast "https://www.artsoft.org/RELEASES/macosx/rocksndiamonds/"
