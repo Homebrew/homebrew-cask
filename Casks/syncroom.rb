83cf819d79a041a57f934a934af3e41802a28bd3
d-salt <d.salt.web+dev@gmail.com>
2020-08-23 00:18:32 +0900

    add an appcast

--- Casks/syncroom.rb
+++ Casks/syncroom.rb
@@ -3,6 +3,7 @@ cask "syncroom" do
   sha256 "a2686011622ef24ccaf3c10bf5a01eccbc59abc13845417138ac58bed261b38d"
 
   url "https://syncroom.yamaha.com/play/dl/app/SYNCROOM-JP-mac-#{version}.zip"
+  appcast "https://syncroom.yamaha.com/play/dl/"
   name "SYNCROOM"
   desc "Online remote concert service: enjoy playing even if far away from each other"
   homepage "https://syncroom.yamaha.com/"
