abf31cd87b4d178d9ac895111b05019d9d908eed
Randall <17261190+ran-dall@users.noreply.github.com>
2020-08-22 01:49:12 +0000

    Update proclaim from 3.0.0.0017 to 3.0.0.0022 (#87971)

--- Casks/proclaim.rb
+++ Casks/proclaim.rb
@@ -1,6 +1,6 @@
 cask "proclaim" do
-  version "3.0.0.0017"
-  sha256 "cbafcf1f466066662e6734ff82c7e968d6f83ea0e4dd95916487e0b7056e3558"
+  version "3.0.0.0022"
+  sha256 "fd6546c5c76d07d24be7ac793c7ca51f68cd810938fb3ef3df69564347a53f15"
 
   # logoscdn.com/Proclaim/ was verified as official when first introduced to the cask
   url "https://downloads.logoscdn.com/Proclaim/Installer/#{version}/Proclaim.dmg"
