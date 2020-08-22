b59ffe32a9d6d46f9fc48ac3d1a762285391418f
suschizu <brigitte@corecode.io>
2020-08-22 20:19:21 +0200

    Update expressvpn from 7.9.7.8 to 7.11.2.2 (#88002)

--- Casks/expressvpn.rb
+++ Casks/expressvpn.rb
@@ -1,9 +1,9 @@
 cask "expressvpn" do
-  version "7.9.7.8"
-  sha256 "8e79bbf950433885ba89d84cf33ce273f03c2b847fb86660efc3b556c4a15d31"
+  version "7.11.2.2"
+  sha256 "a349a32ef60e86ca42db35ef3cba493e1ee69887818559fcb028ce629a82843e"
 
   url "https://download.expressvpn.xyz/clients/mac/expressvpn_mac_#{version}_release.pkg"
-  appcast "https://www.expressvpn.xyz/vpn-software/vpn-mac"
+  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.expressvpn.xyz/clients/latest/mac"
   name "ExpressVPN"
   homepage "https://www.expressvpn.xyz/vpn-software/vpn-mac/"
 
@@ -11,10 +11,11 @@ cask "expressvpn" do
 
   pkg "expressvpn_mac_#{version}_release.pkg"
 
-  uninstall script:  {
-    executable: "#{appdir}/ExpressVPN.app/Contents/Resources/uninstall.tool",
-    input:      ["Yes"],
-    sudo:       true,
-  },
-            pkgutil: "com.expressvpn.ExpressVPN"
+  uninstall launchctl: "com.expressvpn.ExpressVPN.agent",
+            script:    {
+              executable: "#{appdir}/ExpressVPN.app/Contents/Resources/uninstall.tool",
+              input:      ["Yes"],
+              sudo:       true,
+            },
+            pkgutil:   "com.expressvpn.ExpressVPN"
 end
