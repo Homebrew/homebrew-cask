dd68534000c4cfcd7d924100f413f445436aa6d8
d-salt <d.salt.web+dev@gmail.com>
2020-08-22 23:03:10 +0900

    Add VipRiser.app Latest

---
+++ Casks/vipriser.rb
@@ -0,0 +1,11 @@
+cask "vipriser" do
+  version :latest
+  sha256 :no_check
+
+  url "https://onflapp.github.io/blog/releases/vipriser/VipRiser.zip"
+  name "VipRiser"
+  desc "Produce a PDF from any application that can print"
+  homepage "https://onflapp.github.io/blog/pages/vipriser-pdf-ops/vipriser.html"
+
+  app "VipRiser.app"
+end
