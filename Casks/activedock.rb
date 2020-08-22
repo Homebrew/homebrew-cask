6590fd2c4c9993e150bc808c0317e73ee0cc159a
Randall <ran.dall@icloud.com>
2020-08-21 18:44:33 -0700

    Update activedock from 2.36,2036 to 2.38,2038

--- Casks/activedock.rb
+++ Casks/activedock.rb
@@ -1,6 +1,6 @@
 cask "activedock" do
-  version "2.36,2036"
-  sha256 "0d0a8a0c4e33412c75930319c7781c27dbbece6c0e064efb9794a2fc22343c9a"
+  version "2.38,2038"
+  sha256 "2d7d5d3b151fc4c36b406fddeb16ed304adf44f6d8ceaabceaeec154f21d8a98"
 
   url "https://noteifyapp.com/download/ActiveDock.dmg"
   appcast "https://macplus-software.com/downloads/ActiveDock.xml"
