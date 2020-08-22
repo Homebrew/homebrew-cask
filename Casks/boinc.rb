fa36b73149f1e628c7cb9792dd42e2c36a86185a
CoreCode <core-code@users.noreply.github.com>
2020-08-22 23:30:47 +0200

    Update boinc.rb (#88008)

--- Casks/boinc.rb
+++ Casks/boinc.rb
@@ -3,8 +3,7 @@ cask "boinc" do
   sha256 "35e853ae2f637a109112a05256d038d543374da8a9b77997f37f2fb2f9cca277"
 
   url "https://boinc.berkeley.edu/dl/boinc_#{version}_macOSX_x86_64.zip"
-  appcast "https://boinc.berkeley.edu/wiki/Release_Notes",
-          must_contain: version.major_minor
+  appcast "https://www.macupdater.net/cgi-bin/extract_text/download_with_useragent.cgi?url=https://boinc.berkeley.edu/download.php&user_agent=Mac"
   name "Berkeley Open Infrastructure for Network Computing"
   name "BOINC"
   homepage "https://boinc.berkeley.edu/"
