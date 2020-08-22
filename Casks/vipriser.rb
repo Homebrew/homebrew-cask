d97d0af4bf401db25ab680f4e1f3cb77515eee24
d-salt <d.salt.web+dev@gmail.com>
2020-08-22 23:58:23 +0900

    add an appcast

--- Casks/vipriser.rb
+++ Casks/vipriser.rb
@@ -3,6 +3,7 @@ cask "vipriser" do
   sha256 :no_check
 
   url "https://onflapp.github.io/blog/releases/vipriser/VipRiser.zip"
+  appcast "https://onflapp.github.io/blog/releases/vipriser/appcast.xml"
   name "VipRiser"
   desc "Produce a PDF from any application that can print"
   homepage "https://onflapp.github.io/blog/pages/vipriser-pdf-ops/vipriser.html"
