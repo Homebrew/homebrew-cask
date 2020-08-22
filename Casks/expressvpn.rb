f0ae5bf0cc9dc819cb9e36a0bcc9777c2de98ef5
suschizu <brigitte@corecode.io>
2020-08-22 18:00:37 +0200

    Update expressvpn.rb

--- Casks/expressvpn.rb
+++ Casks/expressvpn.rb
@@ -3,7 +3,7 @@ cask "expressvpn" do
   sha256 "a349a32ef60e86ca42db35ef3cba493e1ee69887818559fcb028ce629a82843e"
 
   url "https://download.expressvpn.xyz/clients/mac/expressvpn_mac_#{version}_release.pkg"
-  appcast "https://www.expressvpn.xyz/vpn-software/vpn-mac"
+  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.expressvpn.xyz/clients/latest/mac"
   name "ExpressVPN"
   homepage "https://www.expressvpn.xyz/vpn-software/vpn-mac/"
 
