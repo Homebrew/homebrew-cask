7b0b0f82365ef54215bbbddb771625dbdb24ebcd
euphmat <euphmat@gmail.com>
2020-08-23 15:17:17 +0900

    Add Keysmith.app v0.7.2

---
+++ Casks/keysmith.rb
@@ -0,0 +1,11 @@
+cask "keysmith" do
+  version "0.7.2"
+  sha256 "97fa32b003eaa99feb423080afeef2ddb520dacac3c642ca402d5b76815edc5a"
+
+  url "https://www.keysmith.app/download"
+  name "Keysmith"
+  desc "Create custom keyboard shortcuts foranything"
+  homepage "https://www.keysmith.app/"
+
+  app "Keysmith.app"
+end
