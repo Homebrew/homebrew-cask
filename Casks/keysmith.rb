c7e0873c2f650ea15a9c86cc7be7ac8eadf58ed3
euphmat <euphmat@gmail.com>
2020-08-23 22:23:36 +0900

    Add Keysmith.app v0.7.2 (#88034)
    * Add Keysmith.app v0.7.2
    
    * Add Keysmith.app v0.5.22

---
+++ Casks/keysmith.rb
@@ -0,0 +1,12 @@
+cask "keysmith" do
+  version "0.5.22"
+  sha256 "97fa32b003eaa99feb423080afeef2ddb520dacac3c642ca402d5b76815edc5a"
+
+  url "https://keysmith.app/versions/Keysmith-#{version}.dmg"
+  appcast "https://www.keysmith.app/versions/appcast.xml"
+  name "Keysmith"
+  desc "Create custom keyboard shortcuts for anything"
+  homepage "https://www.keysmith.app/"
+
+  app "Keysmith.app"
+end
