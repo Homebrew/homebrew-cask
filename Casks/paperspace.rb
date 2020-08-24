473ace16d5ce51ace4dc62793a9e76634c1a8aa1
suschizu <brigitte@corecode.io>
2020-08-24 14:43:19 +0200

    Update paperspace.rb

--- Casks/paperspace.rb
+++ Casks/paperspace.rb
@@ -3,7 +3,7 @@ cask "paperspace" do
   sha256 "96eeaacd383285240092fee90c756e677d0f423e8fa917fba5b858abd0c687a5"
 
   url "https://assets.paperspace.com/native-app/prod/darwin/Paperspace-#{version}.dmg"
-  appcast "https://www.macupdater.net/cgi-bin/extract_text/extract_text_split_easy.cgi?url=https://www.paperspace.com/download&user_agent=Mozilla/5.0%20(Macintosh%3B%20Intel%20Mac%20OS%20X%2010_14_5)"
+  appcast "https://www.macupdater.net/cgi-bin/extract_text/send_post_request_data.cgi?url=https://api.paperspace.io/receivers/latestVersion&data={%22component%22:%22paperspaceInstaller%22,%22os%22:%22mac%22}&headers={%22user-agent%22:%22Mozilla%22,%22content-type%22:%22application/json%22}"
   name "Paperspace"
   homepage "https://www.paperspace.com/"
 
