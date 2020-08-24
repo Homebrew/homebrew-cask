16ddce5bad4705aab656e56aa4d2e8bb70a2e8d5
singularitti <singularitti@outlook.com>
2020-08-24 15:06:26 -0400

    Add font "JuliaMono"

---
+++ Casks/font-juliamono.rb
@@ -0,0 +1,17 @@
+cask "font-juliamono" do
+  version "0.009"
+  sha256 "fcb2392dec813d94c2c19621f2171493b8fd29d7a17ee1c2dd0978a64c875c55"
+
+  url "https://github.com/cormullion/juliamono/releases/download/v#{version}/JuliaMono.tar.gz"
+  appcast "https://github.com/cormullion/juliamono/releases.atom"
+  name "JuliaMono"
+  homepage "https://github.com/cormullion/juliamono"
+
+  font "JuliaMono-Black.ttf"
+  font "JuliaMono-Bold.ttf"
+  font "JuliaMono-ExtraBold.ttf"
+  font "JuliaMono-Light.ttf"
+  font "JuliaMono-Medium.ttf"
+  font "JuliaMono-Regular.ttf"
+  font "JuliaMono-RegularLatin.ttf"
+end
