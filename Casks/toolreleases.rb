194d6db0ef3274f74022fe590c56c0121713eed2
Vítor Galvão <info@vitorgalvao.com>
2020-08-24 14:39:38 +0100

    Update toolreleases.rb

--- Casks/toolreleases.rb
+++ Casks/toolreleases.rb
@@ -1,6 +1,5 @@
 cask "toolreleases" do
   version "1.3.4,30"
-  # sha256 is generated from the ToolReleases_v0.0.0.b00.zip file
   sha256 "933078535ca581d278b331761705b3791f03970ee8e14b62f24a2a6d74b8369a"
 
   url "https://github.com/DeveloperMaris/ToolReleases/releases/download/v#{version.before_comma}/ToolReleases_v#{version.before_comma}.b#{version.after_comma}.zip"
