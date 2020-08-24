88a43375aacfef524fe5bb56bb6e5b3bfe382277
suschizu <brigitte@corecode.io>
2020-08-24 12:29:04 +0200

    Update raven-reader.rb

--- Casks/raven-reader.rb
+++ Casks/raven-reader.rb
@@ -2,7 +2,8 @@ cask "raven-reader" do
   version "1.0.0"
   sha256 "9b964d8e5fbbb646b7a8515fd68cba5dbb1592a3c030715ab2c14eeee84d4878"
 
-  url "https://download.ravenreader.app/ravenreader/Raven%20Reader-#{version}-mac.zip"
+  # helloefficiency.com/ravenreader/ was verified as official when first introduced to the cask
+  url "https://download.helloefficiency.com/ravenreader/Raven%20Reader-#{version}.dmg",
   appcast "https://ridereceiptspro.sfo2.digitaloceanspaces.com/ravenreader/latest-mac.yml"
   name "Raven Reader"
   homepage "https://ravenreader.app/"
