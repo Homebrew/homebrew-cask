968b9f0d15917411a70c3fcc41af5be43ba2da28
suschizu <brigitte@corecode.io>
2020-08-24 15:46:00 +0200

    Update url and appcast in raven-reader.rb (#88056)
    * Update raven-reader.rb
    
    * Update raven-reader.rb
    
    * Update raven-reader.rb
    
    * Update raven-reader.rb

--- Casks/raven-reader.rb
+++ Casks/raven-reader.rb
@@ -1,9 +1,10 @@
 cask "raven-reader" do
   version "1.0.0"
-  sha256 "9b964d8e5fbbb646b7a8515fd68cba5dbb1592a3c030715ab2c14eeee84d4878"
+  sha256 "7e77c6546c3d1d08490ec22709d77c51c6621b5d0597af07778c6da488ad1b21"
 
-  url "https://download.ravenreader.app/ravenreader/Raven%20Reader-#{version}-mac.zip"
-  appcast "https://download.ravenreader.app/ravenreader/latest-mac.yml"
+  # download.helloefficiency.com/ravenreader/ was verified as official when first introduced to the cask
+  url "https://download.helloefficiency.com/ravenreader/Raven%20Reader-#{version}.dmg"
+  appcast "https://ridereceiptspro.sfo2.digitaloceanspaces.com/ravenreader/latest-mac.yml"
   name "Raven Reader"
   homepage "https://ravenreader.app/"
 
