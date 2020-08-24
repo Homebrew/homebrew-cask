76bde4cfaf10a7ffd3dc94067da7e7ac07260d16
suschizu <brigitte@corecode.io>
2020-08-24 14:54:23 +0200

    Update raven-reader.rb

--- Casks/raven-reader.rb
+++ Casks/raven-reader.rb
@@ -1,6 +1,6 @@
 cask "raven-reader" do
   version "1.0.0"
-  sha256 "9b964d8e5fbbb646b7a8515fd68cba5dbb1592a3c030715ab2c14eeee84d4878"
+  sha256 "7e77c6546c3d1d08490ec22709d77c51c6621b5d0597af07778c6da488ad1b21"
 
   # download.helloefficiency.com/ravenreader/ was verified as official when first introduced to the cask
   url "https://download.helloefficiency.com/ravenreader/Raven%20Reader-#{version}.dmg"
