0a7374ddeffd6fcea75a28a22f62d98a49959f87
Maris Lagzdins <maris.lagzdins@testdevlab.com>
2020-08-24 10:48:41 +0300

    Fix quote issue in ToolReleases.app Cask
    Many parameters in ToolReleases.app Cask was using single-quoted strings, modify them to use double-quoted strings

--- Casks/toolreleases.rb
+++ Casks/toolreleases.rb
@@ -1,15 +1,15 @@
-cask 'toolreleases' do
-  version '1.3.4,30'
+cask "toolreleases" do
+  version "1.3.4,30"
   # sha256 is generated from the ToolReleases_v0.0.0.b00.zip file
-  sha256 '933078535ca581d278b331761705b3791f03970ee8e14b62f24a2a6d74b8369a'
+  sha256 "933078535ca581d278b331761705b3791f03970ee8e14b62f24a2a6d74b8369a"
 
   url "https://github.com/DeveloperMaris/ToolReleases/releases/download/v#{version.before_comma}/ToolReleases_v#{version.before_comma}.b#{version.after_comma}.zip"
-  appcast 'https://developermaris.github.io/ToolReleases/appcast.xml'
-  name 'ToolReleases'
-  desc 'Informs about the latest Apple tool releases (including Beta releases)'
-  homepage 'https://github.com/DeveloperMaris/ToolReleases'
+  appcast "https://developermaris.github.io/ToolReleases/appcast.xml"
+  name "ToolReleases"
+  desc "Informs about the latest Apple tool releases (including Beta releases)"
+  homepage "https://github.com/DeveloperMaris/ToolReleases"
 
-  depends_on macos: '>= :catalina'
+  depends_on macos: ">= :catalina"
 
   app 'ToolReleases.app'
 end
