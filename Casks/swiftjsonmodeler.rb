b8143a26babf73180750623530cdd5d2e7f19405
Sven <liwanfu5@hikcreate.com>
2020-08-25 18:56:14 +0800

    Add SwiftJSONModeler v2.0.2

--- Casks/swiftjsonmodeler.rb
+++ Casks/swiftjsonmodeler.rb
@@ -5,9 +5,8 @@ cask "swiftjsonmodeler" do
   url "https://github.com/CodeOcenS/SwiftJSONModeler/releases/download/v#{version}/SwiftJSONModeler.For.Xcode.app.zip"
   appcast "https://github.com/CodeOcenS/SwiftJSONModeler/releases.atom"
   name "SwiftJSONModeler"
-  desc "json 转 swift model 插件"
+  desc "The Swift JSON Model generator for Xcode extension(Swift JSON 转模 Xcode 插件)"
   homepage "https://github.com/CodeOcenS/SwiftJSONModeler"
 
   app "SwiftJSONModeler For Xcode.app"
- 
 end
