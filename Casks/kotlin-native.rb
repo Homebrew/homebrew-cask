83b8b40f1dac4001956bc53c2fe9b17ff1038979
宽哥 <shenfakuan@163.com>
2020-08-25 16:47:54 +0800

    update kotlin-native from 1.3.72 to 1.4.0
    update kotlin-native from 1.3.72 to 1.4.0

--- Casks/kotlin-native.rb
+++ Casks/kotlin-native.rb
@@ -1,9 +1,10 @@
 cask "kotlin-native" do
-  version "1.3.72"
+  version_tar "1.4"
+  version "1.4.0"
   sha256 "c5742382cf42def4b59b583f9b04b06928bbebf1aa090ea3ea03bfea5570dcc2"
 
   # github.com/JetBrains/kotlin/ was verified as official when first introduced to the cask
-  url "https://github.com/JetBrains/kotlin/releases/download/v#{version}/kotlin-native-macos-#{version}.tar.gz"
+  url "https://github.com/JetBrains/kotlin/releases/download/v#{version}/kotlin-native-macos-#{version_tar}.tar.gz"
   appcast "https://github.com/JetBrains/kotlin-native/releases.atom"
   name "Kotlin Native"
   homepage "https://kotlinlang.org/docs/reference/native-overview.html"
