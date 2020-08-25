e3c5ba516f235c69210364c69c6d0a35b1dc21a5
Karol Olszacki <k.olszacki@timecamp.com>
2020-08-25 13:19:36 +0200

    Update timecamp from 1.6.1.0 to 1.6.1.6

--- Casks/timecamp.rb
+++ Casks/timecamp.rb
@@ -1,6 +1,6 @@
 cask "timecamp" do
-  version "1.6.1.0"
-  sha256 "b2bd7204d504f46beee90c0444d68777eb1db292de0bafd248dcb0a37cb09220"
+  version "1.6.1.6"
+  sha256 "3d0805892011cdbf959f6fb37ab15a3be0940d354817e73b98b6bd7ad6314077"
 
   # timecamp.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://timecamp.s3.amazonaws.com/downloadsoft/#{version}/TimeCampSetup_macOS.dmg"
