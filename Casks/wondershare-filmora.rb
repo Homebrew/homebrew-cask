9a098c875333455a1ff2f7212582b214589afc8c
Markus Reiter <me@reitermark.us>
2020-08-25 07:06:24 +0200

    Update wondershare-filmora.rb

--- Casks/wondershare-filmora.rb
+++ Casks/wondershare-filmora.rb
@@ -1,6 +1,6 @@
 cask "wondershare-filmora" do
   version "9.5.2.7"
-  sha256 "a5d72be65c12e158aeee05e4266d0af1ab956c7cb5fe65b18bde7b54b7457eac"
+  sha256 "9ab5a7ab7172c47d4bcccf94b0cb7ca07d7a35cd2f58c3214b4ceeb34767e9ae"
 
   url "https://download.wondershare.com/filmora#{version.major}-mac_full718.dmg"
   name "Wondershare Filmora9"
