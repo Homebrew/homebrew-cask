df0d3d80baff7b397effddd995b24c1915f602da
Randall <ran.dall@icloud.com>
2020-08-25 13:58:43 -0700

    Update gifox from 2.2.2,020202.00 to 2.2.3,020203.00

--- Casks/gifox.rb
+++ Casks/gifox.rb
@@ -1,6 +1,6 @@
 cask "gifox" do
-  version "2.2.2,020202.00"
-  sha256 "e81c067894161cecffd0afb1f5bc85fdad8b8d8ebe0775fdc7fc202d77ff1bce"
+  version "2.2.3,020203.00"
+  sha256 "de8eb279d7cf890705ff2ce5ef3543cb662709d49797f7c6365bcf00f129145d"
 
   # d3si16icyi9iar.cloudfront.net/gifox/ was verified as official when first introduced to the cask
   url "https://d3si16icyi9iar.cloudfront.net/gifox/#{version.after_comma}.dmg"
