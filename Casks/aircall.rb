30c80ce0aa0d0187344489bc9d28b58cc65febb4
Viktor Szakats <vszakats@users.noreply.github.com>
2020-08-26 07:55:11 +0200

    aircall: use region-agnostic, domain-style S3 url (#88116)

--- Casks/aircall.rb
+++ Casks/aircall.rb
@@ -2,8 +2,8 @@ cask "aircall" do
   version "2.5.9"
   sha256 "3dacd78ddb6076bd4b5248ff9c10382e63080ae16ad95e10b52fed581e4c4bd1"
 
-  # s3-us-west-1.amazonaws.com/aircall-electron-releases/ was verified as official when first introduced to the cask
-  url "https://s3-us-west-1.amazonaws.com/aircall-electron-releases/production/Aircall-#{version}.zip"
+  # aircall-electron-releases.s3.amazonaws.com/ was verified as official when first introduced to the cask
+  url "https://aircall-electron-releases.s3.amazonaws.com/production/Aircall-#{version}.zip"
   appcast "https://electron.aircall.io/update/osx/0.0.0"
   name "Aircall"
   homepage "https://aircall.io/"
