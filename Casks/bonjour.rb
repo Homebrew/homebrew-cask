9c2ee50b05696de0ac76010c41f8458ab99eba0a
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2020-08-26 13:36:45 +0200

    Add Bonjour.app v1.19.0

---
+++ Casks/bonjour.rb
@@ -0,0 +1,11 @@
+cask "bonjour" do
+  version "1.19.0"
+  sha256 "ea9535db061fe0bdc656985b1d453c18080ca7db8aafd357be9d893f06129759"
+
+  url "https://studio-releases.bonjour.io/download"
+  name "Bonjour"
+  desc "Videoconferencing designed for sales professionals"
+  homepage "https://bonjour.io/"
+
+  app "Bonjour.app"
+end
