0ba48691f0f6a1497c59dac92bd658478ac6e4e4
Martin Geiseler <m.geiseler@global-communication.de>
2020-08-26 14:30:25 +0200

    Update trader-workstation from 978.2e to 980.4h

--- Casks/trader-workstation.rb
+++ Casks/trader-workstation.rb
@@ -1,8 +1,8 @@
 cask "trader-workstation" do
-  version "978.2e"
-  sha256 "dc82975eccf95582aa9e66d26d6eecab448c3b478c140c5c24c3be7bf45c2cc7"
+  version "980.4h"
+  sha256 "d1a5db0e29abf0f806abe04c9ce0b83c0e701d32b9780974fcf93a7c247a7fcd"
 
-  url "https://download2.interactivebrokers.com/installers/tws/stable-standalone/tws-stable-standalone-macosx-x64.dmg"
+  url "https://download2.interactivebrokers.com/installers/tws/latest-standalone/tws-latest-standalone-macosx-x64.dmg"
   name "Trader Workstation"
   homepage "https://www.interactivebrokers.com/"
 
