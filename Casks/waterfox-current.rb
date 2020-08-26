9316bae0fab500ba56fa6c861400a09c825a948a
suschizu <brigitte@corecode.io>
2020-08-26 22:39:25 +0200

    Update waterfox-current.rb

--- Casks/waterfox-current.rb
+++ Casks/waterfox-current.rb
@@ -1,6 +1,6 @@
 cask "waterfox-current" do
-  version "2020.07.2.1,6820.8.15"
-  sha256 "ea5ab1e637638a7687acb6f31a37587979745a3311fabb1fc39a0f455737b4fe"
+  version "2020.08,6820.8.24"
+  sha256 "454dcdfc03adbf2a6f72ba514a039bd4f1ed383df31f9dc1aec0b3de569b3d94"
 
   url "https://cdn.waterfox.net/releases/osx64/installer/Waterfox%20Current%20#{version.before_comma}%20Setup.dmg"
   appcast "https://www.waterfox.net/download/"
