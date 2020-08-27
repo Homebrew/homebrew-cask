9ee37d0380c13e4470729e9383b074412cc509d2
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2020-08-27 12:07:53 +0200

    Update electron from 9.2.1 to 10.0.0 (#88245)

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,6 +1,6 @@
 cask "electron" do
-  version "9.2.1"
-  sha256 "9a790122136d44a6a6f491ef6508c64ab2fc6b17477fe60665fe40ead1fd18d9"
+  version "10.0.0"
+  sha256 "0c6ac8ea684931a66f33219d8de47ab3694d0b89e53f0deacaa6404e35a6555b"
 
   # github.com/electron/electron/ was verified as official when first introduced to the cask
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
