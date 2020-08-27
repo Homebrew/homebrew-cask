4c90cf90833ab3c096e2db64d7467bf32298abe9
Randall <17261190+ran-dall@users.noreply.github.com>
2020-08-27 04:08:25 +0000

    Update openvpn-connect from 3.2.1,1484 to 3.2.2,1899 (#88241)

--- Casks/openvpn-connect.rb
+++ Casks/openvpn-connect.rb
@@ -1,6 +1,6 @@
 cask "openvpn-connect" do
-  version "3.2.1,1484"
-  sha256 "00a3b10c29c13336e52eae6f6dd3960058e9c77e463eaf0742378a31ad0485d4"
+  version "3.2.2,1899"
+  sha256 "0fcab3724cc71d1b2fc1cf645982fb7b70a5d860f5a8eadf03bf9d01ca973ca2"
 
   url "https://swupdate.openvpn.net/downloads/connect/openvpn-connect-#{version.before_comma}.#{version.after_comma}_signed.dmg"
   appcast "https://openvpn.net/client-connect-vpn-for-mac-os/"
