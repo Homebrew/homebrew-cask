384d1bb90b4ad84d27ddf90c61207990041d3c22
Michael Bierman <github.mb@thebiermans.net>
2020-08-27 12:39:16 -0700

    Update aerial from 2.0.4 to 2.0.5

--- Casks/aerial.rb
+++ Casks/aerial.rb
@@ -1,6 +1,6 @@
 cask "aerial" do
-  version "2.0.4"
-  sha256 "9212164ce0cb63d1aae0d39552d18de1acc745e6b1c35878611e8d08ab2fa336"
+  version "2.0.5"
+  sha256 "15e54d80f3f3dea6b35d895c09271c771ebf5a248aa635f2db75c058d0e3b9b3"
 
   url "https://github.com/JohnCoates/Aerial/releases/download/v#{version}/Aerial.saver.zip"
   appcast "https://github.com/JohnCoates/Aerial/releases.atom"
