115a8a1b6fae2f48577571b5ec794d66fa784082
Randall <ran.dall@icloud.com>
2020-08-27 10:56:38 -0700

    Update zalo from 20.6.2 to 20.8.1

--- Casks/zalo.rb
+++ Casks/zalo.rb
@@ -1,6 +1,6 @@
 cask "zalo" do
-  version "20.6.2"
-  sha256 "21dd4524e89c4bbc6fca5bddbb5d7e33f1822721e3c5db2a1633565ecb2b97a7"
+  version "20.8.1"
+  sha256 "59efcf3b6a93add582b7c9df8e521a73eaba65730160ae5ec08e6dadb4dbf440"
 
   # res-zaloapp-aka.zdn.vn/mac/ was verified as official when first introduced to the cask
   url "https://res-zaloapp-aka.zdn.vn/mac/ZaloSetup-#{version}.dmg#"
