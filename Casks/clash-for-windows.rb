583148bb6d98bdc8144c1a76b179b91698d40f6c
Randall <17261190+ran-dall@users.noreply.github.com>
2020-08-28 15:07:57 +0000

    Update clash-for-windows from 0.11.5 to 0.11.6 (#88326)

--- Casks/clash-for-windows.rb
+++ Casks/clash-for-windows.rb
@@ -1,6 +1,6 @@
 cask "clash-for-windows" do
-  version "0.11.5"
-  sha256 "4d59021fadd98b2e63537f9c3b984f4a2c876fd73123acc0d357b3809b0990c8"
+  version "0.11.6"
+  sha256 "fbbab5759864d69a3d963103351f6e52d46cbcbb3644c5a0a93f01ef44b99139"
 
   url "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/#{version}/Clash.for.Windows-#{version}.dmg"
   appcast "https://github.com/Fndroid/clash_for_windows_pkg/releases.atom"
