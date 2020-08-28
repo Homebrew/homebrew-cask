1399c34927e137672bbe3a5ac4c0cf393f090f1f
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2020-08-28 19:03:36 +0200

    Update futuniuniu from 10.9.692_202008200948 to 10.10.702_202008271309 (#88352)

--- Casks/futuniuniu.rb
+++ Casks/futuniuniu.rb
@@ -1,6 +1,6 @@
 cask "futuniuniu" do
-  version "10.9.692_202008200948"
-  sha256 "883932eb32e392f92795c7b28a1aec766b3dc17a2184a5369bf06202edec5706"
+  version "10.10.702_202008271309"
+  sha256 "d6593178c90bfc9fa76b376e5f1d650c179b7b4c2c61c84085b268b542423a3b"
 
   # software-file-1251001049.file.myqcloud.com/ was verified as official when first introduced to the cask
   url "https://software-file-1251001049.file.myqcloud.com/FTNNForMac_#{version}_Website.dmg"
