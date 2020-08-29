d8dc39bb6339e48a86922d54c97af473769d0afb
Markus Reiter <me@reitermark.us>
2020-08-30 01:29:24 +0200

    Support renaming labels.

--- .github/actions/sync/labels.rb
+++ .github/actions/sync/labels.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 
+require 'json'
 require 'octokit'
+
+event = JSON.parse(File.read(ENV.fetch("GITHUB_EVENT_PATH")))
 
 github = Octokit::Client.new(access_token: ENV.fetch('GITHUB_TOKEN'))
 
@@ -15,7 +18,33 @@ to_repos.each do |repo|
 
   labels = github.labels(repo)
 
-  puts JSON.pretty_generate(labels)
+  if event["action"] == "edited"
+    new_label = event.fetch("label")
+
+    new_name = new_label.fetch("name")
+    new_color = new_label.fetch("color")
+    new_description = new_label.fetch("description")
+
+    old_name = event.dig("changes", "name", "from") || new_name
+
+    if old_label = labels.find { |l| l.name == old_name }
+      if dry_run
+        puts "Would update label “#{old_name}” to “#{new_name}”…"
+      else
+        puts "Updating label “#{old_name}” to “#{new_name}”…"
+        github.update_label(repo, old_name, name: new_name, color: new_color, description: new_description)
+      end
+    else
+      if dry_run
+        puts "Would add label “#{new_name}”…"
+      else
+        puts "Adding label “#{new_name}”…"
+        github.add_label(repo, new_name, new_color, description: new_description)
+      end
+    end
+
+    next
+  end
 
   deleted_labels = labels.select { |label| main_labels.none? { |main_label| main_label.name == label.name } }
   modified_labels = labels.select { |label| main_labels.any? { |main_label| main_label.name == label.name } }
