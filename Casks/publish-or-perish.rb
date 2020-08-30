bda470fb674784beb95ed0c8ade178217733fa9e
Dario Vladovic <d.vladimyr@gmail.com>
2020-08-31 01:29:34 +0200

    Update publish-or-perish
    Use actual appcast url, add `zap` stanza.

--- Casks/publish-or-perish.rb
+++ Casks/publish-or-perish.rb
@@ -3,7 +3,7 @@ cask "publish-or-perish" do
   sha256 "574e5f7bfe6a9f237876f81621945c7aedfaca98c743668b8cc68bf3803f5838"
 
   url "https://harzing.com/download/PoP7Mac.pkg"
-  appcast "https://harzing.com/resources/publish-or-perish/os-x"
+  appcast "https://harzing.com/download/pop7mac.txt"
   name "Harzing Publish or Perish"
   homepage "https://harzing.com/resources/publish-or-perish"
 
@@ -12,4 +12,10 @@ cask "publish-or-perish" do
   pkg "PoP7Mac.pkg"
 
   uninstall pkgutil: "com.tarma.popmac.pkg"
+
+  zap trash: [
+    "~/Library/Application Support/Publish or Perish",
+    "~/Library/Preferences/Publish or Perish.json",
+    "~/Library/Preferences/com.tarma.popmac.plist",
+  ]
 end
