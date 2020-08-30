f5dc3583a3eb19f7815a28f6d824c49bf61c4650
Brigitte Zmoelnig <brigitte@corecode.io>
2020-08-30 15:44:41 +0200

    Update redisinsight

--- Casks/redisinsight.rb
+++ Casks/redisinsight.rb
@@ -3,7 +3,8 @@ cask "redisinsight" do
   sha256 "337ebb483582742749a1a9312d35e06b4f82f3b8dd5b79523744073f4bc635e6"
 
   url "https://downloads.redisinsight.redislabs.com/#{version}/redisinsight-mac.dmg"
-  appcast "https://docs.redislabs.com/latest/ri/release-notes/"
+  appcast "https://docs.redislabs.com/latest/ri/release-notes/",
+          must_contain: version.major_minor
   name "RedisInsight"
   homepage "https://www.redislabs.com/redisinsight/"
 
