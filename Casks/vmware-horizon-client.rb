c8b62d0022c02d3fca9778cc46e1152ff2b6a7c0
Bryan Eargle <bryan.eargle@gmail.com>
2020-08-30 15:23:03 -0500

    Remove appcast and version check
    Remove appcast url check after VMware Horizon Client switched to calver.
    The major/minor version is no longer reliably found in release notes.

--- Casks/vmware-horizon-client.rb
+++ Casks/vmware-horizon-client.rb
@@ -3,8 +3,6 @@ cask "vmware-horizon-client" do
   sha256 "ef63a658b2233a8490421e33ecfbafb2a05ef57f0d3810bb6912b92f117cf468"
 
   url "https://download3.vmware.com/software/view/viewclients/#{version.after_comma}/VMware-Horizon-Client-#{version.before_comma}.dmg"
-  appcast "https://docs.vmware.com/en/VMware-Horizon-Client-for-Mac/",
-          must_contain: version.major_minor
   name "VMware Horizon Client"
   homepage "https://www.vmware.com/"
 
