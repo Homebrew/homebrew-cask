e30d52159ea8681e6bc4376d4415ed374ee6b4f6
Randall <17261190+ran-dall@users.noreply.github.com>
2020-08-31 17:04:32 +0000

    Update app-cleaner from 7.0.3 to 7.1 (#88388)

--- Casks/app-cleaner.rb
+++ Casks/app-cleaner.rb
@@ -1,6 +1,6 @@
 cask "app-cleaner" do
-  version "7.0.3"
-  sha256 "6b83428a2a0267df6eb8f9637ced57ac2e1832aea69568c8d82f49bd6186d4d4"
+  version "7.1"
+  sha256 "4809a00410ab09a88e70e66aed1e88a3f359c0c26a3004e124f467d7924d7fb2"
 
   url "https://nektony.com/download/app-cleaner-uninstaller/app-cleaner-uninstaller.dmg"
   appcast "https://nektony.com/pro-support/app-cleaner/update/update.xml"
