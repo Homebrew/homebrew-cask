bd6163c82e8669a3043be4ee46620e70fd019ba4
Casper Storm <casper+github@rogildstorm.com>
2020-08-31 15:21:55 +0200

    Add Ajour.app 0.2.3

---
+++ Casks/ajour.rb
@@ -0,0 +1,15 @@
+cask "ajour" do
+  version "0.2.3"
+  sha256 "084ad4e34ff28dd20dfcfe36c89233137b9380fc9d5f589c6a96a54326a32313"
+
+  # github.com/casperstorm/ajour/ was verified as official when first introduced to the cask
+  url "https://github.com/casperstorm/ajour/releases/download/#{version}/ajour-#{version}.dmg"
+  appcast "https://github.com/casperstorm/ajour/releases.atom"
+  name "Ajour"
+  desc "World of Warcraft addon manager written in Rust"
+  homepage "https://github.com/casperstorm/ajour"
+
+  app "Ajour.app"
+
+  zap trash: "~/.config/ajour"
+end
