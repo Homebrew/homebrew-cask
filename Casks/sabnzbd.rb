e8dc5ee78fa9b5eb96b9be225e3e165447367aa1
Randall <17261190+ran-dall@users.noreply.github.com>
2020-08-31 15:35:05 +0000

    Update sabnzbd from 3.0.1 to 3.0.2 (#88443)

--- Casks/sabnzbd.rb
+++ Casks/sabnzbd.rb
@@ -1,6 +1,6 @@
 cask "sabnzbd" do
-  version "3.0.1"
-  sha256 "523b69a2554ec26ca6e9f86aead87f879ebd5011dae076cc9b48a9abed7b42ec"
+  version "3.0.2"
+  sha256 "bc6dd19f888b6f3d28c9669c187be67d8efef2b0cc7685e18e1190830eb6bcc1"
 
   # github.com/sabnzbd/sabnzbd/ was verified as official when first introduced to the cask
   url "https://github.com/sabnzbd/sabnzbd/releases/download/#{version}/SABnzbd-#{version}-osx.dmg"
