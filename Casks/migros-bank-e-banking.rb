02b09b8396b81e8537f4d10d198471f8fffb48a6
Anon <anon@example.com>
2020-08-31 12:36:13 +0200

    thanks for feedback, did what miccal said

--- Casks/migros-bank-e-banking.rb
+++ Casks/migros-bank-e-banking.rb
@@ -2,14 +2,12 @@ cask "migros-bank-e-banking" do
   version "8.1.2500"
   sha256 "fea31fa340912cd5cab6332fd1b09c3ad366eade655590e7be3c0b42cfce04ad"
 
-  url "https://download.migrosbank.ch/mid/MigrosBank-EBanking-Mac-8.1.2500.pkg"
+  url "https://download.migrosbank.ch/mid/MigrosBank-EBanking-Mac-#{version}.pkg"
   name "Migros Bank E-Banking"
   desc "Client for Migros Bank E-Banking (Switzerland)"
   homepage "https://www.migrosbank.ch/de/privatpersonen/e-banking/zugriff.html"
 
   pkg "MigrosBank-EBanking-Mac-8.1.2500.pkg"
 
-  uninstall pkgutil: [
-    "com.kobil.midappqt.PROD",
-  ]
+  uninstall pkgutil: "com.kobil.midappqt.PROD"
 end
