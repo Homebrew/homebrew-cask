acce0c2aaa4d87aeac783528373df5f8edb563d2
greenrock <github@miss77.net>
2020-09-02 04:31:26 +0800

    Update wechatwebdevtools from 1.03.2006090 to 1.03.2008270 (#88495)

--- Casks/wechatwebdevtools.rb
+++ Casks/wechatwebdevtools.rb
@@ -1,8 +1,8 @@
 cask "wechatwebdevtools" do
-  version "1.03.2006090"
-  sha256 "32e0ec90fbceb4bc058ecbfd03c82192c9292c3be26111660953e6f3dfb31f7a"
+  version "1.03.2008270"
+  sha256 "2fd2d65e975c5e252bd661f743103bd97de104f5961091acaabdcaf5211c7b1f"
 
-  url "https://dldir1.qq.com/WechatWebDev/nightly/p-ae42ee2cde4d42ee80ac60b35f183a99/wechat_devtools_#{version}.dmg"
+  url "https://dldir1.qq.com/WechatWebDev/release/p-ae42ee2cde4d42ee80ac60b35f183a99/wechat_devtools_#{version}.dmg"
   appcast "https://developers.weixin.qq.com/miniprogram/dev/devtools/stable.html"
   name "wechat web devtools"
   name "微信web开发者工具"
