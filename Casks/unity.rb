ad9a8da5fd019059cf1f4615c8d014df1d535b79
Birger J. Nordølum <contact@mindtooth.no>
2020-09-01 12:57:40 +0200

    Update unity from 2020.1.1f1,2285c3239188 to 2020.1.4f1,fa717bb873ec

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -1,6 +1,6 @@
 cask "unity" do
-  version "2020.1.1f1,2285c3239188"
-  sha256 "6c7180c33d18a1ec3db76760954feafa96e336993aacf312557eab30ec45c362"
+  version "2020.1.4f1,fa717bb873ec"
+  sha256 "df7d34d6afc28f343f3ab6c9c7fae2c0112c90c00062215d79cfc815e67efd1f"
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorInstaller/Unity-#{version.before_comma}.pkg"
   appcast "https://unity3d.com/get-unity/download/archive"
