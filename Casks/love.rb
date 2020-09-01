305b80733556fd9eed8794b9c3959dc705d97d51
Paulo Pacitti <ppacitti@outlook.com>
2020-09-01 16:45:06 -0300

    Update: changed love repo

--- Casks/love.rb
+++ Casks/love.rb
@@ -2,8 +2,9 @@ cask "love" do
   version "11.3"
   sha256 "f506af5a6443a559d8edf9b165fd817acb5accdf9873a3be9baf41ab09e0d2d9"
 
-  # bitbucket.org/rude/love/ was verified as official when first introduced to the cask
-  url "https://bitbucket.org/rude/love/downloads/love-#{version}-macos.zip"
+  # bitbucket.org/rude/love/ is not the official repository anymore
+  # project migrated to GitHub
+  url "https://github.com/love2d/love/releases/download/#{version}/love-#{version}-macos.zip"
   appcast "https://love2d.org/"
   name "LÖVE"
   homepage "https://love2d.org/"
