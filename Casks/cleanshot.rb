b04ce26feb1077bda8158f15f101c8f59952c3de
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-01 18:37:37 +0000

    Update cleanshot from 3.3.5 to 3.3.6 (#88516)

--- Casks/cleanshot.rb
+++ Casks/cleanshot.rb
@@ -1,6 +1,6 @@
 cask "cleanshot" do
-  version "3.3.5"
-  sha256 "bb58d2be03794a4664639524feb3fa322ec9721b7794c53b96ac960667b49246"
+  version "3.3.6"
+  sha256 "9786bb4b8d89549d01c43a9554d9ca4d3d9f001d86866ac926218dfe2205a826"
 
   url "https://updates.getcleanshot.com/v#{version.major}/CleanShot-X-#{version}.dmg"
   appcast "https://updates.getcleanshot.com/v#{version.major}/appcast.xml"
