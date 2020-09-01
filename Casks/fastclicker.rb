f4547d7881a85bcd7282041fc28c72e670c70b56
haha1903 <haha1903@gmail.com>
2020-09-01 13:44:40 +0800

    FastClicker 1.1

---
+++ Casks/fastclicker.rb
@@ -0,0 +1,11 @@
+cask "fastclicker" do
+  version "1.1"
+  sha256 "72a2d3be8ccbd03cbf719a5fa63f9adc02c3087b252ce6b728a712cc8dbb0788"
+
+  url "http://www.advanced-mouse-auto-clicker.com/setup/FastClicker.zip"
+  name "FastClicker"
+  desc "Mac Auto Clicker is a Mouse Automation Utility"
+  homepage "http://www.advanced-mouse-auto-clicker.com/mac-auto-clicker.html"
+
+  app "FastClicker.app"
+end
