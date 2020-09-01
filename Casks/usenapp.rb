7b78a365d8596941b3ecd518371c8ba8b04d7afa
Markus Reiter <me@reitermark.us>
2020-09-01 13:50:24 +0200

    Remove platform from description.

--- Casks/usenapp.rb
+++ Casks/usenapp.rb
@@ -5,7 +5,7 @@ cask "usenapp" do
   url "https://www.usenapp.com/download/Usenapp-#{version}.dmg"
   appcast "https://www.usenapp.com/download/appcast_unp1.xml"
   name "Usenapp"
-  desc "Newsreader and usenet client for Mac OSX"
+  desc "Newsreader and usenet client"
   homepage "https://www.usenapp.com/"
 
   app "Usenapp.app"
