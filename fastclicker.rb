110d6595b49c5d62850faa72586139f517bd05d3
haha1903 <haha1903@gmail.com>
2020-09-01 13:25:03 +0800

    FastClicker

---
+++ fastclicker.rb
@@ -0,0 +1,10 @@
+cask "fastclicker" do
+  version "1.1"
+  sha256 "72a2d3be8ccbd03cbf719a5fa63f9adc02c3087b252ce6b728a712cc8dbb0788"
+
+  url "http://www.advanced-mouse-auto-clicker.com/setup/FastClicker.zip"
+  name "FastClicker"
+  homepage "http://www.advanced-mouse-auto-clicker.com/mac-auto-clicker.html"
+  desc "Mac Auto Clicker is a powerful yet easy-to-use tool with many more features that can free your hands and save your time.Mac Auto Clicker is a Mouse Automation Utility which can be used to Click at existing Mouse Cursor Location any number of times."
+  app "FastClicker.app"
+end
