f4e229cabfc995a31eddde42850e92ea08b905a0
Doosies <song961003@gmail.com>
2020-09-01 19:14:14 +0900

    Final modification

--- Casks/naver-whale.rb
+++ Casks/naver-whale.rb
@@ -1,19 +1,18 @@
 cask "naver-whale" do
-    version "2.8.105.22"
-    sha256 "7c68a5c58f8852db829f1ffdc5a2f592c36a818630b15e78fbcc42464d79615a"
-  
-    # github.com/cnr-isti-vclab/meshlab/ was verified as official when first introduced to the cask
-    url "http://update.whale.naver.net/downloads/installers/NaverWhale.dmg"
-    appcast "https://whale.naver.com/changelog/"
-    name "네이버 웨일"
-    homepage "https://whale.naver.com/ko/"
+  version "2.8.105.22"
+  sha256 "7c68a5c58f8852db829f1ffdc5a2f592c36a818630b15e78fbcc42464d79615a"
 
-    app "NAVER Whale.app"
+  # https://whale.naver.com/ko/download was verified as official when first introduced to the cask
+  url "http://update.whale.naver.net/downloads/installers/NaverWhale.dmg"
+  appcast "https://whale.naver.com/changelog/"
+  name "네이버 웨일"
+  homepage "https://whale.naver.com/ko/"
 
-    zap trash: [
-        "~/Library/Caches/com.naver.Whale",
-        "~/Library/Preferences/com.naver.Whale.plist",
-        "~/Library/Saved Application State/com.naver.Whale.savedState",
-        "var/folders/dr/xg4_by2j6xs9n8wl8j53rdb40000gn/C/com.naver.Whale"
-      ]
+  app "Whale.app"
+
+  zap trash: [
+    "~/Library/Caches/com.naver.Whale",
+    "~/Library/Preferences/com.naver.Whale.plist",
+    "~/Library/Saved Application State/com.naver.Whale.savedState",
+  ]
 end
