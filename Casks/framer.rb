9469f9f831bfbd96392972efd348f8919aa00275
Randall <ran.dall@icloud.com>
2020-09-01 10:31:42 -0700

    Update framer from 52581,1598370007 to 52896,1598956054

--- Casks/framer.rb
+++ Casks/framer.rb
@@ -1,6 +1,6 @@
 cask "framer" do
-  version "52581,1598370007"
-  sha256 "20aa6b1fde9ef1e11f38d515dc8a23b51ba5e1e00640b93381e7b4ee75a06d12"
+  version "52896,1598956054"
+  sha256 "9ddf61fa8a77d325a18844848ab9bba60465ae62d92515f1f161d44a591ba172"
 
   url "https://dl.framer.com/com.framer.desktop/#{version.before_comma}/#{version.after_comma}/FramerDesktop-#{version.before_comma}.zip"
   appcast "https://updates.framer.com/sparkle/com.framer.desktop"
