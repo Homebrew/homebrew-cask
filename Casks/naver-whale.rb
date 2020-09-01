37ecacfc19c06fc0c4c978ceff11bb77f0fa0ad3
Doosies <song961003@gmail.com>
2020-09-01 20:07:36 +0900

    fix remark, desc

--- Casks/naver-whale.rb
+++ Casks/naver-whale.rb
@@ -2,11 +2,11 @@ cask "naver-whale" do
   version "2.8.105.22"
   sha256 "7c68a5c58f8852db829f1ffdc5a2f592c36a818630b15e78fbcc42464d79615a"
 
-  # https://whale.naver.com/ko/download was verified as official when first introduced to the cask
+  # http://update.whale.naver.net/downloads/ was verified as official when first introduced to the cask
   url "http://update.whale.naver.net/downloads/installers/NaverWhale.dmg"
   appcast "https://whale.naver.com/changelog/"
   name "네이버 웨일"
-  desc "Web browser made by Naver Co., Ltd"
+  desc "Web browser made by Naver Co, Ltd"
   homepage "https://whale.naver.com/ko/"
 
   app "네이버 웨일.app"
