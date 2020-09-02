6f8ad82cae57d8c597c5d3d4390e092e65885ee0
CoreCode <core-code@users.noreply.github.com>
2020-09-02 17:04:47 +0200

    Update vmware-horizon-client.rb

--- Casks/vmware-horizon-client.rb
+++ Casks/vmware-horizon-client.rb
@@ -3,6 +3,7 @@ cask "vmware-horizon-client" do
   sha256 "ef63a658b2233a8490421e33ecfbafb2a05ef57f0d3810bb6912b92f117cf468"
 
   url "https://download3.vmware.com/software/view/viewclients/#{version.after_comma}/VMware-Horizon-Client-#{version.before_comma}.dmg"
+  appcast "https://my.vmware.com/channel/public/api/v1.0/dlg/details?locale=en_US&downloadGroup=CART21FQ2_MAC_800&productId=1027&rPId=48989"
   name "VMware Horizon Client"
   homepage "https://www.vmware.com/"
 
