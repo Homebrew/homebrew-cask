13b4834c56175d850fcf8552e7472ca5dca899b4
Doosies <song961003@gmail.com>
2020-09-02 09:06:33 +0900

    fix #, name, homepage, app

--- Casks/naver-whale.rb
+++ Casks/naver-whale.rb
@@ -2,14 +2,14 @@ cask "naver-whale" do
   version "2.8.105.22"
   sha256 "7c68a5c58f8852db829f1ffdc5a2f592c36a818630b15e78fbcc42464d79615a"
 
-  # http://update.whale.naver.net/downloads/ was verified as official when first introduced to the cask
+  # update.whale.naver.net/downloads/installers/ was verified as official when first introduced to the cask
   url "http://update.whale.naver.net/downloads/installers/NaverWhale.dmg"
   appcast "https://whale.naver.com/changelog/"
-  name "네이버 웨일"
+  name "NAVER Whale"
   desc "Web browser made by Naver Co, Ltd"
-  homepage "https://whale.naver.com/ko/"
+  homepage "https://whale.naver.com/"
 
-  app "네이버 웨일.app"
+  app "Whale.app"
 
   zap trash: [
     "~/Library/Caches/com.naver.Whale",
