18fc753cb04b96763d1d0f1d1671033aa9b8d772
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2020-09-02 13:04:08 +0200

    Update qcad from 3.25.0 to 3.25.1 (#88557)

--- Casks/qcad.rb
+++ Casks/qcad.rb
@@ -1,11 +1,11 @@
 cask "qcad" do
-  version "3.25.0"
+  version "3.25.1"
 
   if MacOS.version <= :high_sierra
-    sha256 "f904031d67ee95adadc773d314ca154c448324447b628527df51d213f90e2c70"
+    sha256 "600f04ed9524d1d1f8471500e73c456665618e0774f791a562ed080f9cc02e4d"
     url "https://www.qcad.org/archives/qcad/qcad-#{version}-trial-macos-10.10-10.13.dmg"
   else
-    sha256 "6ceb9145f8646d5a2c5c9de59e952b68bbdf1f544275b7ada95d0173eed84ea4"
+    sha256 "e6b811f9ca060a6e56fe6086a3ebff85cc1d2c9619659c808d2fa382daeceeb7"
     url "https://www.qcad.org/archives/qcad/qcad-#{version}-trial-macos-10.14-10.15.dmg"
   end
 
