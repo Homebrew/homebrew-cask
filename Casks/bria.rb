b8ebdad8b6c22cec2a2dea658cff793f7ce3918c
Randall <ran.dall@icloud.com>
2020-09-02 11:54:46 -0700

    Update bria from 6.1.2_104265 to 6.2.0_104643

--- Casks/bria.rb
+++ Casks/bria.rb
@@ -1,6 +1,6 @@
 cask "bria" do
-  version "6.1.2_104265"
-  sha256 "fbd1768550a74875d049bbbc535df78fd92d03fbdeb5fb3ca8d80d9cda2f1f5c"
+  version "6.2.0_104643"
+  sha256 "6938eaed642a3f8baafa374ed92c7d42800e085366823c9301e958bbd9bec4c4"
 
   # counterpath.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://counterpath.s3.amazonaws.com/downloads/Bria_#{version}.dmg"
