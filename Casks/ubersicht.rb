066288e1ffb73349f1cc582ffa399c250eec73d3
Viktor Szakats <commit@vsz.me>
2020-09-03 09:09:51 +0000

    ubersicht: secure urls

--- Casks/ubersicht.rb
+++ Casks/ubersicht.rb
@@ -2,10 +2,10 @@ cask "ubersicht" do
   version "1.4.61"
   sha256 "0e5090b99d48eb6c4b3dbcea67c32018079285015996ff2bb4cc68d383b7d019"
 
-  url "http://tracesof.net/uebersicht/releases/Uebersicht-#{version}.app.zip"
-  appcast "http://tracesof.net/uebersicht/updates.xml.rss"
+  url "https://tracesof.net/uebersicht/releases/Uebersicht-#{version}.app.zip"
+  appcast "https://tracesof.net/uebersicht/updates.xml.rss"
   name "Übersicht"
-  homepage "http://tracesof.net/uebersicht/"
+  homepage "https://tracesof.net/uebersicht/"
 
   auto_updates true
   depends_on macos: ">= :yosemite"
