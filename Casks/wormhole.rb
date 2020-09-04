55f7a76d1a97790945134b65fdea349c0ae09f83
Randall <ran.dall@icloud.com>
2020-09-04 13:05:26 -0700

    Update wormhole from 1.3.2 to 1.3.3

--- Casks/wormhole.rb
+++ Casks/wormhole.rb
@@ -1,6 +1,6 @@
 cask "wormhole" do
-  version "1.3.2"
-  sha256 "bec3d04e41d0c6eb33a27ec8e920ab0a6b42249bb7fc5b4c8124f822d294220e"
+  version "1.3.3"
+  sha256 "ab1950b888b95d20f39d1cbaa40790edf4b41cd846972fe10d9134d548ac1900"
 
   # files.octopusgame.com/ was verified as official when first introduced to the cask
   url "https://files.octopusgame.com/os/WormholeInstaller.dmg"
