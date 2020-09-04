32ebdf86cc5609ced4f6b6e2d2649f377fb9792d
Markus Reiter <me@reitermark.us>
2020-09-04 08:00:37 +0200

    Update aspera-connect.rb

--- Casks/aspera-connect.rb
+++ Casks/aspera-connect.rb
@@ -3,7 +3,8 @@ cask "aspera-connect" do
   sha256 "12fbb7468590136b408f108ae209522f78e2ea03dab61572bb5002337c3a9dcc"
 
   url "https://download.asperasoft.com/download/sw/connect/#{version.major_minor_patch}/IBMAsperaConnectInstaller-#{version}.dmg"
-  appcast "https://downloads.asperasoft.com/en/documentation/8"
+  appcast "https://downloads.asperasoft.com/connect2/",
+          must_contain: version.dots_to_underscores
   name "Aspera Connect"
   homepage "https://asperasoft.com/software/transfer-clients/connect-web-browser-plug-in/"
 
