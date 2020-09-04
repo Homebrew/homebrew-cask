d751b1d4c281a94a606fa9556f03c0c39f04c036
Work <brigitte@corecode.io>
2020-09-04 17:00:33 +0200

    Update masterway-note from 1.1.0.88242909 to 1.2.0

--- Casks/masterway-note.rb
+++ Casks/masterway-note.rb
@@ -1,9 +1,9 @@
 cask "masterway-note" do
-  version "1.1.0.88242909"
-  sha256 "f6a7e718f7f1c18dc5302ee1f9b320b9ef64d926dc2980f470529cd948c819c4"
+  version "1.2.0"
+  sha256 "dc65476d865f52bab13396c608e233c3f6399d63b65dddde4ffba5fdf6721d46"
 
   # prota.oss-cn-beijing.aliyuncs.com/ was verified as official when first introduced to the cask
-  url "https://prota.oss-cn-beijing.aliyuncs.com/downloads/#{version.major_minor}/%E5%A4%A7%E5%B8%88%E7%AC%94%E8%AE%B0-#{version}.dmg"
+  url "https://prota.oss-cn-beijing.aliyuncs.com/downloads/#{version.major_minor}/%E5%A4%A7%E5%B8%88%E7%AC%94%E8%AE%B0masterwaynote.pkg"
   appcast "https://masterwaynote.com/mac",
           must_contain: version.major_minor
   name "Masterway Note"
