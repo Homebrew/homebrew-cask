b3327a6fc9e219f23c671cbc7fee29cba9aea4a8
Randall <ran.dall@icloud.com>
2020-09-04 12:33:39 -0700

    Update marvel from 9.9.1 to 9.9.2

--- Casks/marvel.rb
+++ Casks/marvel.rb
@@ -1,6 +1,6 @@
 cask "marvel" do
-  version "9.9.1"
-  sha256 "23ae39d41c4a630508290a078daf7b484501278b159c6597c53fac3b2d05c782"
+  version "9.9.2"
+  sha256 "3af311b87452ea18307d1094745c8aa14197f1b2fe9b545c89549f33c1f3b7eb"
 
   # storage.googleapis.com/sketch-plugin/ was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/sketch-plugin/#{version}/Marvel.zip"
