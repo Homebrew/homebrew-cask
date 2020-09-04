99091ae9f4c624084aff46de4da24a72f2c09356
Damien Calesse <damien.calesse@gmail.com>
2020-09-04 16:29:03 +0200

    Add klogg.app v20.4
    Add new cask for klogg.app, currently in v20.4

---
+++ Casks/klogg.rb
@@ -0,0 +1,12 @@
+cask "klogg" do
+  version "20.4"
+  sha256 "3661f7525b2a20655b86074eebad83663513680811e9d2224741781b07a192fc"
+
+  url "https://github.com/variar/klogg/releases/download/v#{version}/klogg-#{version}.0-r589-OSX.dmg"
+  appcast "https://github.com/variar/klogg/releases.atom"
+  name "Klogg"
+  desc "GUI application for complex log files"
+  homepage "https://github.com/variar/klogg"
+
+  app "klogg.app"
+end
