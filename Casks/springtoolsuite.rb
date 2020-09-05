317b5cc4d9b3382211395fd7c6bc341a04a3c3c0
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-05 01:30:02 +0000

    Update springtoolsuite from 4.7.1.RELEASE,4.16.0 to 4.7.2.RELEASE,4.16.0 (#88708)

--- Casks/springtoolsuite.rb
+++ Casks/springtoolsuite.rb
@@ -1,6 +1,6 @@
 cask "springtoolsuite" do
-  version "4.7.1.RELEASE,4.16.0"
-  sha256 "b346ea5fb4755bf1bfb344f364205b94a6a14f3cf57a0e9e6411fb9dc22358a1"
+  version "4.7.2.RELEASE,4.16.0"
+  sha256 "f6e2d641c913d331318b7d062a356f3544374a0e12b40ba9027ab04b41ee69cb"
 
   # download.springsource.com/release/ was verified as official when first introduced to the cask
   url "https://download.springsource.com/release/STS#{version.major}/#{version.before_comma}/dist/e#{version.after_comma.major_minor}/spring-tool-suite-#{version.major}-#{version.before_comma}-e#{version.after_comma}-macosx.cocoa.x86_64.dmg"
