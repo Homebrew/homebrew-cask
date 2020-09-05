b7f9d1c2f5df4219c1b389a452f6a75f7df16cd1
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-05 21:03:24 +0000

    Update digikam from 7.0.0 to 7.1.0 (#88729)
    Co-authored-by: CoreCode <core-code@users.noreply.github.com>

--- Casks/digikam.rb
+++ Casks/digikam.rb
@@ -1,6 +1,6 @@
 cask "digikam" do
-  version "7.0.0"
-  sha256 "8b5a4abbf0c5c51b429b14c8f37cd269ed66569614faaa0bc09ead3f577122a5"
+  version "7.1.0"
+  sha256 "bea457513afb0e08b6d1fa0b9abd67c47d34fa2d141377f361c09176e7ae3b94"
 
   # kde.org/stable/digikam/ was verified as official when first introduced to the cask
   url "https://download.kde.org/stable/digikam/#{version}/digiKam-#{version}-MacOS-x86-64.pkg"
@@ -11,5 +11,8 @@ cask "digikam" do
   pkg "digiKam-#{version}-MacOS-x86-64.pkg"
 
   uninstall pkgutil: "org.freedesktop.digikam",
-            delete:  "/Applications/digiKam"
+            delete:  [
+              "/Applications/digikam.app",
+              "/Applications/showfoto.app",
+            ]
 end
