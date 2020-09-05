a1e1f32c4d281e4e5cb921876371c4e363671e66
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-05 17:44:00 +0000

    Update appium from 1.18.0-1 to 1.18.0-2 (#88750)

--- Casks/appium.rb
+++ Casks/appium.rb
@@ -1,6 +1,6 @@
 cask "appium" do
-  version "1.18.0-1"
-  sha256 "0d1f0746bebdd4fbcfcbabc190d272304e9a01f6c7f87f31d0f51cf39bc38328"
+  version "1.18.0-2"
+  sha256 "072b71d3a5680a4f7b67372c6439da8ff4ee9390344fcca2d8ab3f9e1332a331"
 
   # github.com/appium/appium-desktop/ was verified as official when first introduced to the cask
   url "https://github.com/appium/appium-desktop/releases/download/v#{version}/Appium-mac-#{version}.dmg"
