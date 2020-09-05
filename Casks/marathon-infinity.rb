126d6310fd2b2ac150df65da5392c3e1bcaa262e
Randall <ran.dall@icloud.com>
2020-09-05 10:13:05 -0700

    Update marathon-infinity from 20200830 to 20200904

--- Casks/marathon-infinity.rb
+++ Casks/marathon-infinity.rb
@@ -1,6 +1,6 @@
 cask "marathon-infinity" do
-  version "20200830"
-  sha256 "b191070a8116b5081870c8673995589b450cc1183bbf88b1b865b5d251459f7a"
+  version "20200904"
+  sha256 "ee96ebf857044ffd5df19b53bb0bdf26299179d1e2e2a6d8be506e31e0b67e8f"
 
   # github.com/Aleph-One-Marathon/alephone/ was verified as official when first introduced to the cask
   url "https://github.com/Aleph-One-Marathon/alephone/releases/download/release-#{version}/MarathonInfinity-#{version}-Mac.dmg"
