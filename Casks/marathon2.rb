cf43faabc50a1a516acbdfafc5fb64242ad03b1a
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-05 17:54:06 +0000

    Update marathon2 from 20200830 to 20200904 (#88752)

--- Casks/marathon2.rb
+++ Casks/marathon2.rb
@@ -1,7 +1,7 @@
 cask "marathon2" do
   # note: "2" is not a version number, but an intrinsic part of the product name
-  version "20200830"
-  sha256 "a4c56cc2e837cec92bcded8406f5cb4cc7ddb7ce4472ac19e088120a47f18e50"
+  version "20200904"
+  sha256 "55d1e7b595089adc4fdaf455742f3b373e730bc22c24204bffa5a884f78a14c3"
 
   # github.com/Aleph-One-Marathon/alephone/ was verified as official when first introduced to the cask
   url "https://github.com/Aleph-One-Marathon/alephone/releases/download/release-#{version}/Marathon2-#{version}-Mac.dmg"
