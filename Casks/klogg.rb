05c7fa5691d1c43c3c1210b8b141bb73ab504265
Markus Reiter <me@reitermark.us>
2020-09-05 19:11:29 +0200

    Update klogg.rb

--- Casks/klogg.rb
+++ Casks/klogg.rb
@@ -1,8 +1,8 @@
 cask "klogg" do
-  version "20.4"
+  version "20.4.0-r589"
   sha256 "3661f7525b2a20655b86074eebad83663513680811e9d2224741781b07a192fc"
 
-  url "https://github.com/variar/klogg/releases/download/v#{version}/klogg-#{version}.0-r589-OSX.dmg"
+  url "https://github.com/variar/klogg/releases/download/v#{version.major_minor}/klogg-#{version}-OSX.dmg"
   appcast "https://github.com/variar/klogg/releases.atom"
   name "Klogg"
   desc "Fast, advanced log explorer"
