ebd2aca8a28b898db4a91c4e3f1bd12d0debc955
Fedor Batonogov <batonogov@icloud.com>
2020-09-06 17:48:16 +0300

    Fix sintax

--- Casks/cisco-jabber.rb
+++ Casks/cisco-jabber.rb
@@ -10,14 +10,14 @@ cask "cisco-jabber" do
   pkg "Install_Cisco-Jabber-Mac.pkg"
 
   uninstall pkgutil: [
-              "com.cisco.Jabber",
-              "com.cisco.pkg.jabberplugin",
-              "com.jabra.CiscoJabberPlugin",
-              "com.logitech.LogiUCPlugin",
-              "com.PlantronicsPlugin.CiscoJabberPlugin",
-              "com.Sennheiser.pkg.SennheiserSDKv789904MacSDKv8602"
-            ],
-            delete: [
+    "com.cisco.Jabber",
+    "com.cisco.pkg.jabberplugin",
+    "com.jabra.CiscoJabberPlugin",
+    "com.logitech.LogiUCPlugin",
+    "com.PlantronicsPlugin.CiscoJabberPlugin",
+    "com.Sennheiser.pkg.SennheiserSDKv789904MacSDKv8602",
+  ],
+            delete:  [
               "/Applications/Cisco Jabber.app",
               "/Library/Logs/Jabber/",
               "/Library/Application Support/Cisco/Unified Communications/Jabber/",
