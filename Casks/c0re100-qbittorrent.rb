f14021373dcf838fa1433e56fe3c22d046342f95
Birger J. Nordølum <contact@mindtooth.no>
2020-09-07 19:13:04 +0200

    Update c0re100-qbittorrent from 4.2.5.15 to 4.2.5.16

--- Casks/c0re100-qbittorrent.rb
+++ Casks/c0re100-qbittorrent.rb
@@ -1,6 +1,6 @@
 cask "c0re100-qbittorrent" do
-  version "4.2.5.15"
-  sha256 "d728bc602ac0cc809d1ae7b38a24f425b910030874d0297d74e2fc83bbb91234"
+  version "4.2.5.16"
+  sha256 "295f87ab8ee874bd8b2543ee33362d0a8e1ea9ca3a846671a25c419b389d4de4"
 
   url "https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-#{version}/qBittorrent-#{version}.dmg"
   appcast "https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases.atom"
