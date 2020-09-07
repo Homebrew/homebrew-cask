1c956fa08630500b0d03270c5b8f5af79eb30eeb
Randall <ran.dall@icloud.com>
2020-09-07 13:23:33 -0700

    Update dbeaver-enterprise from 7.1.0 to 7.2.0

--- Casks/dbeaver-enterprise.rb
+++ Casks/dbeaver-enterprise.rb
@@ -1,6 +1,6 @@
 cask "dbeaver-enterprise" do
-  version "7.1.0"
-  sha256 "020bb975764e7b62e1d4fbe9f2dcf3e9e4250d635cd84759832f48521e43864e"
+  version "7.2.0"
+  sha256 "c65bdf23a79f4fd6bf1ee912d8dc0b2af2c9a355500b4585b907e194983e9f99"
 
   url "https://dbeaver.com/files/#{version}/dbeaver-ee-#{version}-macos.dmg"
   appcast "https://dbeaver.com/product/version.xml"
