1f65c58e01633c030040736dc435de410de7d054
suschizu <brigitte@corecode.io>
2020-09-07 23:05:38 +0200

    Update eudic.rb

--- Casks/eudic.rb
+++ Casks/eudic.rb
@@ -5,7 +5,8 @@ cask "eudic" do
   # static.frdic.com/ was verified as official when first introduced to the cask
   url "https://static.frdic.com/pkg/eudicmac.dmg?v=#{version.after_comma}",
       user_agent: :fake
-  appcast "https://www.eudic.net/v4/en/app/download"
+  appcast "https://www.eudic.net/v4/en/app/download",
+          must_contain: version.after_comma
   name "Eudic"
   name "欧路词典"
   homepage "https://www.eudic.net/v4/en/app/eudic"
