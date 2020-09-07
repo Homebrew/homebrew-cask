702d48423f87aa102c375a096a2d32490a13cd65
Randall <ran.dall@icloud.com>
2020-09-07 13:31:13 -0700

    Update file-juicer from 4.86 to 4.87

--- Casks/file-juicer.rb
+++ Casks/file-juicer.rb
@@ -1,5 +1,5 @@
 cask "file-juicer" do
-  version "4.86"
+  version "4.87"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://echoone.com/filejuicer/FileJuicer-#{version}.zip"
