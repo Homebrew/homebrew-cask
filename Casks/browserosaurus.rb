fd5ee5902ba8adcafdff88b4f4fe934df03cbb69
tk4k <54473116+tk4k@users.noreply.github.com>
2020-09-07 10:02:10 +0900

    Update browserosaurus from 11.0.1 to 11.1.0

--- Casks/browserosaurus.rb
+++ Casks/browserosaurus.rb
@@ -1,6 +1,6 @@
 cask "browserosaurus" do
-  version "11.0.1"
-  sha256 "d95057ccb8303c66dc42d40aaacbaf7751f3c63cb59fd85a4150b07219c48b6c"
+  version "11.1.0"
+  sha256 "72a3957bea5ca4704fff1fd15435cb3218c97806f741154bed25cc2a97c382aa"
 
   # github.com/will-stone/browserosaurus/ was verified as official when first introduced to the cask
   url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-#{version}.dmg"
