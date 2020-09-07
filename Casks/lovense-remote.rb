34a0a84ccb20806981fe62bd4a31ffb29e4df1f6
Kristian Matthews <kristian.matthews@me.com>
2020-09-07 17:39:57 +0100

    Add Lovense_Remote Latest

---
+++ Casks/lovense-remote.rb
@@ -0,0 +1,11 @@
+cask "lovense-remote" do
+  version ":latest"
+  sha256 "a21b7e5a3f6e68ee7dd940bb6b8bbf75246d4e4392045672cbdf04aeb006d3c0"
+
+  url "https://lovense.com/files/apps/remote/remote.dmg"
+  name "Lovense Remote"
+  desc "For remote use"
+  homepage "https://lovense.com/"
+
+  app "Lovense_Remote.app"
+end
