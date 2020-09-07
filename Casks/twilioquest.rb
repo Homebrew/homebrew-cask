9b3401e6e6fd2874a8324f2e47968432f5960129
Vítor Galvão <info@vitorgalvao.com>
2020-09-07 02:17:04 +0100

    Update twilioquest.rb

--- Casks/twilioquest.rb
+++ Casks/twilioquest.rb
@@ -1,9 +1,9 @@
 cask "twilioquest" do
-  version ":latest"
-  sha256 "6d58c8a88b1bf9b62bcbd6cf48f547721485eb979ca5e3069d81e152a4c553eb"
+  version :latest
+  sha256 :no_check
 
   # firebasestorage.googleapis.com/v0/b/twilioquest-prod.appspot.com was verified as official when first introduced to the cask
-  url "https://firebasestorage.googleapis.com/v0/b/twilioquest-prod.appspot.com/o/launchers%2F8%2Ftwilioquest-mac.zip?alt=media&token=94e06ace-3770-4c14-9aa7-5d77366ac7ec"
+  url "https://firebasestorage.googleapis.com/v0/b/twilioquest-prod.appspot.com/o/launchers%2F8%2Ftwilioquest-mac.zip?alt=media"
   name "TwilioQuest"
   desc "Learn to code and lead your crew on a mission to save The Cloud in a 16bit game"
   homepage "https://www.twilio.com/quest"
