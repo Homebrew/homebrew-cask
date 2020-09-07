6fb22f09e72e4898ef72932dc1104cd8db12f1cb
suschizu <brigitte@corecode.io>
2020-09-07 16:18:40 +0200

    Update eudic.rb

--- Casks/eudic.rb
+++ Casks/eudic.rb
@@ -1,9 +1,9 @@
 cask "eudic" do
-  version "2020-08-07,3.9.9"
+  version "3.9.9,2020-08-07"
   sha256 "ddd1c890d2affdf7067785f56c7572b80842ac2fde317f663d1f6d6739d46348"
 
   # static.frdic.com/ was verified as official when first introduced to the cask
-  url "https://static.frdic.com/pkg/eudicmac.dmg?v=#{version.before_comma}",
+  url "https://static.frdic.com/pkg/eudicmac.dmg?v=#{version.after_comma}",
       user_agent: :fake
   appcast "https://www.eudic.net/v4/en/app/download"
   name "Eudic"
