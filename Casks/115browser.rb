94bb4f54f9b806a970656638f08512d1c5e6052a
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-07 20:33:26 +0000

    Update 115browser from 23.9.0.4 to 23.9.1.7 (#88870)

--- Casks/115browser.rb
+++ Casks/115browser.rb
@@ -1,6 +1,6 @@
 cask "115browser" do
-  version "23.9.0.4"
-  sha256 "c019b756f928cb07c558f447cdd91b4c4d8a17d6f2cca2be323628efa431deed"
+  version "23.9.1.7"
+  sha256 "8350d29ee907de51ad9d5644a573f2c5c287974c31007e8e82b9db60527d56a8"
 
   url "https://down.115.com/client/mac/115pc_#{version}.dmg"
   appcast "https://appversion.115.com/1/web/1.0/api/chrome?callback=get_version"
