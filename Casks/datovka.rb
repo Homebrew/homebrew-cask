065e9679ea63e821efad483f03edad6e5b1d1359
Work <brigitte@corecode.io>
2020-09-07 13:16:08 +0200

    Update datovka

--- Casks/datovka.rb
+++ Casks/datovka.rb
@@ -1,6 +1,6 @@
 cask "datovka" do
   version "4.15.2"
-  sha256 "a144eff2c0dfc939a5b5243d0efa81297ceecbbb5e8969c9bca51b6ca0ae0b94"
+  sha256 "6ee6ee28e3b3f936b34f17b5c7848f5907fe516381984472ac53be1a69615316"
 
   # secure.nic.cz/files/datove_schranky/ was verified as official when first introduced to the cask
   url "https://secure.nic.cz/files/datove_schranky/#{version}/datovka-#{version}-64bit-macos10.12.dmg"
