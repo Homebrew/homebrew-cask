18d95e2e767791a2a19bb30cf0cbf5b7dcf95224
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-08 18:11:20 +0000

    Update ringcentral from 20.3.26 to 20.3.30 (#88925)

--- Casks/ringcentral.rb
+++ Casks/ringcentral.rb
@@ -1,6 +1,6 @@
 cask "ringcentral" do
-  version "20.3.26"
-  sha256 "065cae9ba9d1efa2df306deb6747c0bb405f662ba749613d7bb8156f64f3bf4e"
+  version "20.3.30"
+  sha256 "4a3676241c419e02c2d606cd8c75b4841ada24120513fe337c579d31b12860fe"
 
   url "https://app.ringcentral.com/downloads/RingCentral.pkg"
   appcast "https://app.ringcentral.com/download/latest-mac.yml"
