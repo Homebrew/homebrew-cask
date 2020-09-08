33d4ead33939296df1ae513e55f23fc87e08de35
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-08 11:02:20 +0000

    Update and rename toggl from 7.5.254 to toggl-track 7.5.260 (#88855)
    * Update toggl from 7.5.254 to 7.5.260
    
    * Update toggl.rb
    
    * Update and rename toggl.rb to toggl-track.rb
    
    * Update toggl-track.rb
    
    Co-authored-by: CoreCode <core-code@users.noreply.github.com>
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/toggl-track.rb
+++ Casks/toggl-track.rb
@@ -1,12 +1,13 @@
-cask "toggl" do
-  version "7.5.254"
-  sha256 "f4462fc3d5057069c0c4fa62bbef59b3d27e1390e64f21f9a7bd76e50f87c667"
+cask "toggl-track" do
+  version "7.5.260"
+  sha256 "4c001d7d9940ccd5563be1f03773ceeaca46efaf07be30819e04746e42bbeee3"
 
   # github.com/toggl-open-source/toggldesktop/ was verified as official when first introduced to the cask
   url "https://github.com/toggl-open-source/toggldesktop/releases/download/v#{version}/TogglDesktop-#{version.dots_to_underscores}.dmg"
   appcast "https://toggl-open-source.github.io/toggldesktop/assets/releases/darwin_stable_appcast.xml"
-  name "TogglDesktop"
-  homepage "https://www.toggl.com/toggl-desktop/"
+  name "Toggl Track"
+  desc "Time tracker"
+  homepage "https://www.toggl.com/track/toggl-desktop/"
 
-  app "TogglDesktop.app"
+  app "Toggl Track.app"
 end
