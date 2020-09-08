d14164fc676f0ba4a0ce50ef91a9c6c3fc89a75c
haha1903 <haha1903@gmail.com>
2020-09-08 08:59:19 +0800

    Fixed the brew cask style

--- Casks/fastclicker.rb
+++ Casks/fastclicker.rb
@@ -3,7 +3,7 @@ cask "fastclicker" do
   sha256 "72a2d3be8ccbd03cbf719a5fa63f9adc02c3087b252ce6b728a712cc8dbb0788"
 
   url "http://www.advanced-mouse-auto-clicker.com/setup/FastClicker.zip"
-  appcast 'http://www.advanced-mouse-auto-clicker.com/mac-auto-clicker.html'
+  appcast "http://www.advanced-mouse-auto-clicker.com/mac-auto-clicker.html"
   name "FastClicker"
   desc "Auto clicker and mouse automation utility"
   homepage "http://www.advanced-mouse-auto-clicker.com/mac-auto-clicker.html"
