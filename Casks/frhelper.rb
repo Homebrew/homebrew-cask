e5c24a400fa68d6bd6302e3d1767e1deee867549
Reno <25192197+singularitti@users.noreply.github.com>
2020-09-07 23:47:07 -0400

    Update frhelper from 3.9.6,2020-04-27 to 3.9.9,2020-08-08 (#88883)

--- Casks/frhelper.rb
+++ Casks/frhelper.rb
@@ -1,6 +1,6 @@
 cask "frhelper" do
-  version "3.9.6,2020-04-27"
-  sha256 "ec638576b929dfd9c2637b5dd152c59fc05812bdfa670c2419d056e329c6e44b"
+  version "3.9.9,2020-08-08"
+  sha256 "7c464b7a7d77cb82c07629b6cc51898b70517d3ad25cc39b57ad6d185ff0bd3a"
 
   # static.frdic.com/ was verified as official when first introduced to the cask
   url "https://static.frdic.com/pkg/fhmac.dmg?v=#{version.after_comma}",
