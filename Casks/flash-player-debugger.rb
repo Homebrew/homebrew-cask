3d96669768947bd49058ca6c81269ba672ad33f1
CoreCode <core-code@users.noreply.github.com>
2020-09-08 13:35:58 +0200

    Update flash-player-debugger.rb

--- Casks/flash-player-debugger.rb
+++ Casks/flash-player-debugger.rb
@@ -3,8 +3,7 @@ cask "flash-player-debugger" do
   sha256 "9d8efc65469359b782242c19e81875f12e1ed3ca00cb3616aecc521667c14da0"
 
   url "https://fpdownload.adobe.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_sa_debug.dmg"
-  appcast "https://fpdownload.adobe.com/pub/flashplayer/update/current/xml/version_en_mac_pep.xml",
-          must_contain: version.tr(".", ",")
+  appcast "https://www.adobe.com/support/flashplayer/debug_downloads.html"
   name "Adobe Flash Player projector content debugger"
   homepage "https://www.adobe.com/support/flashplayer/debug_downloads.html"
 
