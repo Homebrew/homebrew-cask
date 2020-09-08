40715de7e21fc3deae02c88881cb5e50c49ccf10
Vítor Galvão <info@vitorgalvao.com>
2020-09-08 11:46:39 +0100

    Update toggl-track.rb

--- Casks/toggl-track.rb
+++ Casks/toggl-track.rb
@@ -5,8 +5,8 @@ cask "toggl-track" do
   # github.com/toggl-open-source/toggldesktop/ was verified as official when first introduced to the cask
   url "https://github.com/toggl-open-source/toggldesktop/releases/download/v#{version}/TogglDesktop-#{version.dots_to_underscores}.dmg"
   appcast "https://toggl-open-source.github.io/toggldesktop/assets/releases/darwin_stable_appcast.xml"
-  name "Toggl Desktop"
   name "Toggl Track"
+  desc "Time tracker"
   homepage "https://www.toggl.com/track/toggl-desktop/"
 
   app "Toggl Track.app"
