45fe8a68c7612fa8ec47bc90551779ca02e13049
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-08 15:58:22 +0000

    Update keysmith from 1.0.5 to 1.0.6 (#88907)

--- Casks/keysmith.rb
+++ Casks/keysmith.rb
@@ -1,6 +1,6 @@
 cask "keysmith" do
-  version "1.0.5"
-  sha256 "f12e9266ffcb38efbea717b8557d6276ccf74598c31910225103d13a6796d398"
+  version "1.0.6"
+  sha256 "304e233de5728583abaf249ae62e7c1d7063517eb58a1e8eeb70d156a60d5664"
 
   url "https://keysmith.app/versions/Keysmith-#{version}.dmg"
   appcast "https://www.keysmith.app/versions/appcast.xml"
