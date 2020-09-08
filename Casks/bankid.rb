1e77fac0f4477ac07f2381a81111c0fe513a795b
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-08 17:12:13 +0000

    Update bankid from 7.9.1 to 7.9.2 (#88922)

--- Casks/bankid.rb
+++ Casks/bankid.rb
@@ -1,6 +1,6 @@
 cask "bankid" do
-  version "7.9.1"
-  sha256 "4dfc203b3c88625f9f294dcaf1bd935dc9862842a884639d6658bd8f01951ab3"
+  version "7.9.2"
+  sha256 "a97bc40668009ed5d2291a9493d141ad6f25b9c2c61762ee50414b5868e3c20a"
 
   url "https://install.bankid.com/Repository/BankID_installation_#{version.dots_to_underscores}.pkg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://install.bankid.com/FileDownloader?fileId=Mac",
