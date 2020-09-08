314200bfd059bf51909541997d8f3a0f35bafd5e
Miccal Matthews <miccal.matthews@gmail.com>
2020-09-08 16:02:24 +0800

    Update mkvtoolnix from 50.0.0 to 50.0.0.1 (#88881)

--- Casks/mkvtoolnix.rb
+++ Casks/mkvtoolnix.rb
@@ -1,18 +1,19 @@
 cask "mkvtoolnix" do
-  version "50.0.0"
-  sha256 "72d0c459db320dcf537009b7d1593e0712e5bc40cdd54bff3f85ae4cdb551d46"
+  version "50.0.0.1"
+  sha256 "7118e7f17f9ef49fe471d860b938362f6d575e75d4bc61a1fa41cb9880bb11e6"
 
   url "https://mkvtoolnix.download/macos/MKVToolNix-#{version}.dmg"
-  appcast "https://www.bunkus.org/blog/feed/"
+  appcast "https://mkvtoolnix.download/macos/"
   name "MKVToolNix"
+  desc "Set of tools to create, alter and inspect Matroska files"
   homepage "https://mkvtoolnix.download/"
 
   conflicts_with formula: "mkvtoolnix"
   depends_on macos: ">= :mojave"
 
-  app "MKVToolNix-#{version}.app"
-  binary "#{appdir}/MKVToolNix-#{version}.app/Contents/MacOS/mkvextract"
-  binary "#{appdir}/MKVToolNix-#{version}.app/Contents/MacOS/mkvinfo"
-  binary "#{appdir}/MKVToolNix-#{version}.app/Contents/MacOS/mkvmerge"
-  binary "#{appdir}/MKVToolNix-#{version}.app/Contents/MacOS/mkvpropedit"
+  app "MKVToolNix-#{version.major_minor_patch}.app"
+  binary "#{appdir}/MKVToolNix-#{version.major_minor_patch}.app/Contents/MacOS/mkvextract"
+  binary "#{appdir}/MKVToolNix-#{version.major_minor_patch}.app/Contents/MacOS/mkvinfo"
+  binary "#{appdir}/MKVToolNix-#{version.major_minor_patch}.app/Contents/MacOS/mkvmerge"
+  binary "#{appdir}/MKVToolNix-#{version.major_minor_patch}.app/Contents/MacOS/mkvpropedit"
 end
