8076141a21a8e5333a22be67a4a2ea32ee7a74be
suschizu <brigitte@corecode.io>
2020-09-09 18:05:15 +0200

    Update media-center from 26.00.18 to 26.0.107 (#88963)

--- Casks/media-center.rb
+++ Casks/media-center.rb
@@ -1,6 +1,6 @@
 cask "media-center" do
-  version "26.00.18"
-  sha256 "9dc309204b762df29ff07b8dd6db87956c5b6b4d359a902e0b0bbf875e86d152"
+  version "26.0.107"
+  sha256 "8865b68f48fca629729b50175498cfae7d782a078bb948fa1a54f349615f35a0"
 
   url "https://files.jriver.com/mediacenter/channels/v#{version.major}/latest/MediaCenter#{version.no_dots}.dmg"
   appcast "https://www.jriver.com/download.html",
