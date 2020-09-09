e18a547efdf62c7eabda25d9452a3b93fac45168
Work <brigitte@corecode.io>
2020-09-09 17:42:11 +0200

    Update prowritingaid from 2.0.45 to 2.0.46

--- Casks/prowritingaid.rb
+++ Casks/prowritingaid.rb
@@ -1,6 +1,6 @@
 cask "prowritingaid" do
-  version "2.0.45"
-  sha256 "54737c12dd7f5f60eff39943361e4ce13aa3498b9a4529ca11c9508c0cebcd8f"
+  version "2.0.46"
+  sha256 "c6430ef93c66f0f9d539802b722325448375687ffa0eba96fafc690ef3d14e9a"
 
   url "https://cdn.prowritingaid.com/prowritingaid-desktop/desktop_#{version.dots_to_underscores}/ProWritingAid.dmg"
   appcast "https://prowritingaid.com/en/App/DesktopHistory"
