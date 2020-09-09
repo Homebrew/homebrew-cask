828b8caf63b964e1eb3f8fee4198369ebd43c4dc
Randall <ran.dall@icloud.com>
2020-09-09 14:03:24 -0700

    Update thedesk from 21.1.1 to 21.2.2

--- Casks/thedesk.rb
+++ Casks/thedesk.rb
@@ -1,6 +1,6 @@
 cask "thedesk" do
-  version "21.1.1"
-  sha256 "0e48a79acd67187b2dc0c381086fca21d58b6fb2b5e2e012d2460ce17ce32ca4"
+  version "21.2.2"
+  sha256 "4cfc58fc93a1bc4f12f2ed79a232fb48d152910d66990dc148a7aebcd4a4388d"
 
   # github.com/cutls/TheDesk/ was verified as official when first introduced to the cask
   url "https://github.com/cutls/TheDesk/releases/download/v#{version}/TheDesk-#{version}.dmg"
