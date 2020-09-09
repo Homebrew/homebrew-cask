0e2d73992c31c59a58a8d4960e7f2ec1d345e572
Randall <ran.dall@icloud.com>
2020-09-09 14:36:20 -0700

    Update encryptme from 4.2.3 to 4.3.0

--- Casks/encryptme.rb
+++ Casks/encryptme.rb
@@ -1,6 +1,6 @@
 cask "encryptme" do
-  version "4.2.3"
-  sha256 "036c693afa300fa3374863860b11595e8849bb76021667dc65401b48ab935238"
+  version "4.3.0"
+  sha256 "5450d69384d6f18fb61166938442301aa6e98eb51d049d9f21958da429b80797"
 
   url "https://static.encrypt.me/downloads/osx/updates/Release/EncryptMe-#{version}.dmg"
   appcast "https://www.getcloak.com/updates/osx/public/"
