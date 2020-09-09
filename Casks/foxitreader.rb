9bac2854f8f2c98c5e03d9e6c277d1d4fdc5edcd
Work <brigitte@corecode.io>
2020-09-09 20:57:04 +0200

    Update foxitreader

--- Casks/foxitreader.rb
+++ Casks/foxitreader.rb
@@ -3,7 +3,6 @@ cask "foxitreader" do
   sha256 "56ada239bdd4ca321581dec57f55d95f916f1b9a93321c4cda8090b82979a43a"
 
   url "https://cdn09.foxitsoftware.com/pub/foxit/reader/desktop/mac/#{version.major}.x/#{version.major_minor}/ML/FoxitReader#{version.no_dots}.setup.pkg"
-  appcast "https://www.foxitsoftware.com/downloads/downloadForm.php?retJson=1&product=Foxit-Reader&platform=Mac-OS-X"
   name "Foxit Reader"
   homepage "https://www.foxitsoftware.com/pdf-reader/"
 
