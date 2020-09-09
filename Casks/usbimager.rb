9ac93919bbd2c4406a77fe2aec1ca7933a433b6e
V33m <48420633+V33m@users.noreply.github.com>
2020-09-09 11:39:54 +0200

    Add usbimager.app 1.0.4

---
+++ Casks/usbimager.rb
@@ -0,0 +1,12 @@
+cask "usbimager" do
+  version "1.0.4"
+  sha256 "42f85d64baee14b63b45ffcb0a7a24ee3f2a5339f76f2b5cdb767a9f069de432"
+
+  url "https://gitlab.com/bztsrc/usbimager/-/raw/binaries/usbimager_#{version}-intel-macosx-cocoa.zip"
+  appcast "https://gitlab.com/bztsrc/usbimager/-/tags?format=atom"
+  name "USBImager"
+  desc "A simple GUI application that writes compressed disk images to USB drives and creates backups."
+  homepage "https://gitlab.com/bztsrc/usbimager"
+
+  app "USBImager.app"
+end
