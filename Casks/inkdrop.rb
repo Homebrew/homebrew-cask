dffa152e157f920737fbba8288a27a979b276b61
Randall <ran.dall@icloud.com>
2020-09-10 10:51:34 -0700

    Update inkdrop from 5.0.0 to 5.0.1

--- Casks/inkdrop.rb
+++ Casks/inkdrop.rb
@@ -1,6 +1,6 @@
 cask "inkdrop" do
-  version "5.0.0"
-  sha256 "7f5925376f7412c89c5e501bc73a7cb719f393cfaed17e90bdf7195396c31e36"
+  version "5.0.1"
+  sha256 "536a9fa26f47b428d6840fdc453a1c37f19524559df4a8ae4b7dec5b7cdc2cca"
 
   # d3ip0rje8grhnl.cloudfront.net/ was verified as official when first introduced to the cask
   url "https://d3ip0rje8grhnl.cloudfront.net/v#{version}/Inkdrop-#{version}-Mac.zip"
