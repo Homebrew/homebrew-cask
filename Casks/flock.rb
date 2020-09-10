14dae35dc5e06969e8264a9e4cc742a028bf56fe
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-10 20:23:58 +0000

    Update flock from 2.2.389 to 2.2.430 (#89057)

--- Casks/flock.rb
+++ Casks/flock.rb
@@ -1,6 +1,6 @@
 cask "flock" do
-  version "2.2.389"
-  sha256 "84d0b0a94758d274feb8960339abb21195013cf75e00c46603dcd34c36b79898"
+  version "2.2.430"
+  sha256 "376b5e2a11a19ea9d1752e3884afea446488c976e612f69709413e0f15bd8011"
 
   # flock.co/ was verified as official when first introduced to the cask
   url "https://updates.flock.co/fl_mac_electron/Flock-macOS-#{version}.dmg"
