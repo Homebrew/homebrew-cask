2f47102ebff81a550776a2399201d7627f47e999
Zhenfu Shi <szf1234@me.com>
2020-09-10 18:27:11 -0400

    Update browserosaurus from 12.0.0 to 12.0.2 (#89037)

--- Casks/browserosaurus.rb
+++ Casks/browserosaurus.rb
@@ -1,6 +1,6 @@
 cask "browserosaurus" do
-  version "12.0.0"
-  sha256 "c3a74931e2582f9c82f91e189700f13974fc77a58a9f76bcf5c7ad97d1739348"
+  version "12.0.2"
+  sha256 "286825e307c0cff5a7dffac4b673cd8fd89a010a47510b05c026c5623d9b9303"
 
   # github.com/will-stone/browserosaurus/ was verified as official when first introduced to the cask
   url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-#{version}.dmg"
