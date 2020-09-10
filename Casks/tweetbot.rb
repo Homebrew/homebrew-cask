ae8e52280b18600c3412e886977c4fbfd798a952
suschizu <brigitte@corecode.io>
2020-09-10 13:34:34 +0200

    Update tweetbot from 3.4.100,4 to 3.4.200,4 (#89005)

--- Casks/tweetbot.rb
+++ Casks/tweetbot.rb
@@ -1,6 +1,6 @@
 cask "tweetbot" do
-  version "3.4.100,4"
-  sha256 "e57b54c79c87d81ce694fd34b511eeeb724f8a9578d04d8034d0805d5143b6a3"
+  version "3.4.200,4"
+  sha256 "b8f7af9cf9d1ace4995fc01ec650ab982afc73645a0f5ca2c84d6777506d2d04"
 
   # tapbots.net/ was verified as official when first introduced to the cask
   url "https://tapbots.net/tweetbot#{version.after_comma}/Tweetbot.#{version.before_comma.no_dots}.zip"
