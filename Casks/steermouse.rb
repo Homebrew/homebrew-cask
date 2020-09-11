4869a45699b8fd69936e0c9a6c7e8b349e7d51d2
Hugo Jobling <hello@thisishugo.com>
2020-09-11 11:22:01 +0100

    Update Steermouse from 5.4.5 to 5.5.1

--- Casks/steermouse.rb
+++ Casks/steermouse.rb
@@ -1,6 +1,6 @@
 cask "steermouse" do
-  version "5.4.5"
-  sha256 "a2ad5821b923673ca5ee6ac27c0ee543127b3ec9c61dfd053f820246aa47417f"
+  version "5.5.1"
+  sha256 "985ad80baa7d40248d2d66eda436f88580e3a017ac112a06d29eb6f098982c0f"
 
   url "https://plentycom.jp/ctrl/files_sm/SteerMouse#{version}.dmg"
   appcast "https://plentycom.jp/en/steermouse/download.php"
