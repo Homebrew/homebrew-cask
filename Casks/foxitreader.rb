6b03a37baf25d0fd38dbbacd49f873c5a49c23f9
Vítor Galvão <info@vitorgalvao.com>
2020-09-11 02:27:03 +0100

    foxitreader.rb: new appcast

--- Casks/foxitreader.rb
+++ Casks/foxitreader.rb
@@ -3,7 +3,8 @@ cask "foxitreader" do
   sha256 "56ada239bdd4ca321581dec57f55d95f916f1b9a93321c4cda8090b82979a43a"
 
   url "https://cdn09.foxitsoftware.com/pub/foxit/reader/desktop/mac/#{version.major}.x/#{version.major_minor}/ML/FoxitReader#{version.no_dots}.setup.pkg"
-  appcast "https://www.foxitsoftware.com/downloads/downloadForm.php?retJson=1&product=Foxit-Reader&platform=Mac-OS-X"
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.foxitsoftware.com/downloads/latest.html%3Fproduct%3DFoxit-Reader%26platform%3DMac-OS-X",
+          must_contain: version.no_dots
   name "Foxit Reader"
   desc "PDF reader"
   homepage "https://www.foxitsoftware.com/pdf-reader/"
