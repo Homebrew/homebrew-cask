1a2e6068f9ebc5e4a9d2d208603203311783b466
Jonathan Trujillo <trujillo.jonathan@gmail.com>
2020-09-11 17:57:16 +0200

    Update version, sha256, url and add appcast

--- Casks/dropbox-passwords.rb
+++ Casks/dropbox-passwords.rb
@@ -1,8 +1,10 @@
 cask "dropbox-passwords" do
-  version :latest
-  sha256 :no_check
+  version "5.1.4"
+  sha256 "3e586ee75ff0b846371f37e17a12569a2683df112d3cd7f70931e60d53482c15"
 
-  url "https://www.dropbox.com/dropbox-passwords-download/install?platform=mac"
+  # clientupdates.dropboxstatic.com was verified as official when first introduced to the cask
+  url "https://clientupdates.dropboxstatic.com/dbx-releng/dropbox_passwords/mac/DropboxPasswords_#{version}.dmg"
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.dropbox.com/dropbox-passwords-download/mac/beta"
   name "Dropbox Passwords"
   desc "Password manager that syncs across devices"
   homepage "https://www.dropbox.com/features/security/passwords"
