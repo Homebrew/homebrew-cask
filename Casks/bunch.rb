656362f84eb4b94e2601279273e43a9bfc081acc
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-11 18:36:59 +0000

    Update bunch from 1.2.5,45 to 1.2.6,46 (#89110)

--- Casks/bunch.rb
+++ Casks/bunch.rb
@@ -1,6 +1,6 @@
 cask "bunch" do
-  version "1.2.5,45"
-  sha256 "7b6477a47a6fa8f4b00c62d4204e5ffe7d54d1ae5240743f7d45c936b54836f7"
+  version "1.2.6,46"
+  sha256 "3e63ab86bced3bcb8cadcf5d899d036c29d11caf89e911cc3222a4804e217bff"
 
   url "https://cdn3.brettterpstra.com/updates/bunch/Bunch#{version.before_comma}#{version.after_comma}.dmg"
   appcast "https://brettterpstra.com/updates/bunch/appcast.xml"
