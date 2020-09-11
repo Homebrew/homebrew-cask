2df567d5687fb1b34c2e5ef6f1278c41170a4603
isfk <70327450+isfk@users.noreply.github.com>
2020-09-11 10:27:11 +0800

    update kotlin-native from 1.4.0 to 1.4.10

--- Casks/kotlin-native.rb
+++ Casks/kotlin-native.rb
@@ -1,23 +1,23 @@
 cask "kotlin-native" do
-  version "1.4.0"
-  sha256 "476f920631b0ccb4b8e25456ef49356fb33d6e3960e9f2ec8def0aaa23284168"
+  version "1.4.10"
+  sha256 "e84f1622a6160a44e1f821d7539771a52389cdf3c9acaa94534b079a571503f3"
 
   # github.com/JetBrains/kotlin/ was verified as official when first introduced to the cask
-  url "https://github.com/JetBrains/kotlin/releases/download/v#{version}/kotlin-native-macos-#{version.major_minor}.tar.gz"
+  url "https://github.com/JetBrains/kotlin/releases/download/v#{version}/kotlin-native-macos-#{version}.tar.gz"
   appcast "https://github.com/JetBrains/kotlin-native/releases.atom"
   name "Kotlin Native"
   homepage "https://kotlinlang.org/docs/reference/native-overview.html"
 
-  conflicts_with formula: "kotlin"
+  # conflicts_with formula: "kotlin"
 
-  binary "kotlin-native-macos-#{version.major_minor}/bin/cinterop"
-  binary "kotlin-native-macos-#{version.major_minor}/bin/jsinterop"
-  binary "kotlin-native-macos-#{version.major_minor}/bin/klib"
-  binary "kotlin-native-macos-#{version.major_minor}/bin/konan-lldb"
-  binary "kotlin-native-macos-#{version.major_minor}/bin/konanc"
-  binary "kotlin-native-macos-#{version.major_minor}/bin/kotlinc"
-  binary "kotlin-native-macos-#{version.major_minor}/bin/kotlinc-native"
-  binary "kotlin-native-macos-#{version.major_minor}/bin/run_konan"
+  binary "kotlin-native-macos-#{version}/bin/cinterop"
+  binary "kotlin-native-macos-#{version}/bin/jsinterop"
+  binary "kotlin-native-macos-#{version}/bin/klib"
+  binary "kotlin-native-macos-#{version}/bin/konan-lldb"
+  binary "kotlin-native-macos-#{version}/bin/konanc"
+  binary "kotlin-native-macos-#{version}/bin/kotlinc"
+  binary "kotlin-native-macos-#{version}/bin/kotlinc-native"
+  binary "kotlin-native-macos-#{version}/bin/run_konan"
 
   caveats do
     depends_on_java "6+"
