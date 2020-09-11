20715f7a6e93eb4a7b276cd58b9cfc9604e7ca22
Ralph Martin <ralph@cs.cf.ac.uk>
2020-09-11 19:17:42 +0100

    Update mactracker (#89101)

--- Casks/mactracker.rb
+++ Casks/mactracker.rb
@@ -1,6 +1,6 @@
 cask "mactracker" do
   version "7.9.4"
-  sha256 "318c3c6c4d8e6456cbb358d63ecd58f8526625792076ab1f2630af38a87d0e8b"
+  sha256 "6c423dbf1522a0643fe235461638ba8ed5fc277d1bedfdf76e017f02263b5bca"
 
   url "https://www.mactracker.ca/downloads/Mactracker_#{version}.zip"
   appcast "https://update.mactracker.ca/appcast-b.xml"
