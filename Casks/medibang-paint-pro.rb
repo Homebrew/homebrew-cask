70372adcd4ad24dfdf82c6480d4e3897c45169a0
weichenw <weichenw@gmail.com>
2020-09-11 18:10:53 +1000

    Add MediBang Paint Pro v25.4

---
+++ Casks/medibang-paint-pro.rb
@@ -0,0 +1,11 @@
+cask "medibang-paint-pro" do
+  version "25.4"
+  sha256 "217782a71e83a696be802b64c8ce878f90812d9dd8b6c6df1664a58a682eebb5"
+
+  url "https://medibangpaint.com/static/installer/MediBangPaintPro/MediBangPaintPro-#{version}.dmg"
+  name "MediBang Paint Pro"
+  desc "Create digital art and comics in MediBang Paint Pro"
+  homepage "https://medibangpaint.com/en/pc/"
+
+  app "MediBangPaintPro.app"
+end
