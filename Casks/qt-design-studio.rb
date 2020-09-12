e70630fd83fdcceb523ab52b1490751265737c02
CoreCode <core-code@users.noreply.github.com>
2020-09-12 19:31:43 +0200

    Update qt-design-studio from 1.5.0 to 1.6.0 (#89153)

--- Casks/qt-design-studio.rb
+++ Casks/qt-design-studio.rb
@@ -1,8 +1,9 @@
 cask "qt-design-studio" do
-  version "1.5.0"
-  sha256 "db05cafe77ea8db26505ec4100334362e196ec5835172d7db184b3988ce2df5e"
+  version "1.6.0"
+  sha256 "4e3bee4715b05014971fb23d2475e4a28aa4cb283380453cc8fff138cd19a822"
 
   url "https://download.qt.io/official_releases/qtdesignstudio/#{version}/qt-designstudio-mac-x86_64-#{version}-community.dmg"
+  appcast "https://download.qt.io/official_releases/qtdesignstudio/"
   name "Qt Design Studio"
   homepage "https://www.qt.io/ui-framework"
 
