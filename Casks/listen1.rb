204b9ffe6d73a494cdeeb9530b7dcc91caafa7b3
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-12 22:07:08 +0000

    Update listen1 from 2.12.3 to 2.13.0 (#89163)

--- Casks/listen1.rb
+++ Casks/listen1.rb
@@ -1,7 +1,7 @@
 cask "listen1" do
   # note: "1" is not a version number, but an intrinsic part of the product name
-  version "2.12.3"
-  sha256 "9bf5164c206c3232267a45de7bf5efac37089df26b839a4fbcb2f43f027f7131"
+  version "2.13.0"
+  sha256 "54a30c23890467d7fdae327815963c2e6c259f5d1140a080b56b8faa4d0ae3b4"
 
   # github.com/listen1/listen1_desktop/ was verified as official when first introduced to the cask
   url "https://github.com/listen1/listen1_desktop/releases/download/v#{version}/Listen1_#{version}_mac.dmg"
