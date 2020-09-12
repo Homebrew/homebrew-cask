d873649f90a033cba620b9871446ef707cc71652
Vítor Galvão <info@vitorgalvao.com>
2020-09-12 14:35:57 +0100

    Update netron from 4.5.0 to 4.5.1

--- Casks/netron.rb
+++ Casks/netron.rb
@@ -1,6 +1,6 @@
 cask "netron" do
-  version "4.5.0"
-  sha256 "f5deaa07d0e2494b253b997a1e649d9ac432dfbf178a52cb7e7675b0e2b1ad08"
+  version "4.5.1"
+  sha256 "5a6bbbc4b40707016c06ec48de5836a1efa34f5405089cac6b0ebae91265bfdf"
 
   url "https://github.com/lutzroeder/netron/releases/download/v#{version}/Netron-#{version}-mac.zip"
   appcast "https://github.com/lutzroeder/netron/releases.atom"
