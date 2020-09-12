5cb9641f169ba1213f5486e9763eb2ea902bd4db
CoreCode <core-code@users.noreply.github.com>
2020-09-12 18:51:02 +0200

    Update coconutbattery.rb

--- Casks/coconutbattery.rb
+++ Casks/coconutbattery.rb
@@ -4,9 +4,9 @@ cask "coconutbattery" do
     sha256 "8e289fb4a75cb117fc1d7861020c9ab2384b09dfd18f066c7fadfc9d42c3ac56"
     url "https://www.coconut-flavour.com/downloads/coconutBattery_#{version}.zip"
   else
-    version "3.9.1"
+    version "3.9.1,0af56ac0"
     sha256 "fd1f1141aa39ae73e7139686a4e5b0a9018b7ab704799e7d0fc52e0de19f9c76"
-    url "https://www.coconut-flavour.com/downloads/coconutBattery_#{version.no_dots}.zip"
+    url "https://www.coconut-flavour.com/downloads/coconutBattery_#{version.before_comma.no_dots}_.#{version.after_comma}zip"
     appcast "https://coconut-flavour.com/updates/coconutBattery.xml"
   end
 
