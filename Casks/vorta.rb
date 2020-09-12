f3a6df1b8f37165068beec32ab2df2ea44988266
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-12 17:37:22 +0000

    Update vorta from 0.7.0 to 0.7.1 (#89039)

--- Casks/vorta.rb
+++ Casks/vorta.rb
@@ -1,6 +1,6 @@
 cask "vorta" do
-  version "0.7.0"
-  sha256 "27ed5fb099578e376a7a46b68cd237de40368f688530cdf3b6d87e002456d6d5"
+  version "0.7.1"
+  sha256 "3a6a5ea8d85dc8815cacfdf6a5591d0d811c4cb2320c08f89cb7d9de1261b3fa"
 
   url "https://github.com/borgbase/vorta/releases/download/v#{version}/vorta-#{version}.dmg"
   appcast "https://github.com/borgbase/vorta/releases.atom"
