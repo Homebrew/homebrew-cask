bb3105b7b25c2ff8be2d55da8946e88881532e00
Vítor Galvão <info@vitorgalvao.com>
2020-09-12 14:39:22 +0100

    Update netron.rb

--- Casks/netron.rb
+++ Casks/netron.rb
@@ -5,7 +5,7 @@ cask "netron" do
   url "https://github.com/lutzroeder/netron/releases/download/v#{version}/Netron-#{version}-mac.zip"
   appcast "https://github.com/lutzroeder/netron/releases.atom"
   name "Netron"
-  desc "Visualizer for deep learning and machine learning models"
+  desc "Visualizer for neural network, deep learning, and machine learning models"
   homepage "https://github.com/lutzroeder/netron"
 
   auto_updates true
