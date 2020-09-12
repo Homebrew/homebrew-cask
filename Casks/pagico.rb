690c2dfc74f20773b2af4f80ce91ae0938e798b1
CoreCode <core-code@users.noreply.github.com>
2020-09-12 19:12:06 +0200

    Update pagico.rb

--- Casks/pagico.rb
+++ Casks/pagico.rb
@@ -1,6 +1,6 @@
 cask "pagico" do
-  version "9.5.20200910"
-  sha256 "d5ec384fd42ccd94bb52e7ce5188dcc09a22c56da5c6d59d99c4c6bc6b1caa6e"
+  version "9.5.20200911"
+  sha256 "6d9dbc55ce09f6fe3f2f5314a9e3b376f954c10c1e137d2456b2a27ac9dee9ea"
 
   url "https://www.pagico.com/downloads/Pagico_macOS_r#{version.patch}.dmg"
   appcast "https://www.pagico.com/api/pagico#{version.major}.mac.xml",
