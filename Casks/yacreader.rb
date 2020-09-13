36ca12566dded17835b047e61ac3628f13351e61
Seeker <meaningseeking@protonmail.com>
2020-09-12 23:08:34 -0700

    Update yacreader from 9.7.0.2009038 to 9.7.1.2009123 (#89185)

--- Casks/yacreader.rb
+++ Casks/yacreader.rb
@@ -1,6 +1,6 @@
 cask "yacreader" do
-  version "9.7.0.2009038"
-  sha256 "f69328c179b478d73fb6c74c4452a033fc4d72ce1300ca4757f6c0830a7caa00"
+  version "9.7.1.2009123"
+  sha256 "068dbed581b8098d2eb36d32a8a16966eb588519d8b64ae592a2383d91bb36ec"
 
   # github.com/YACReader/yacreader/ was verified as official when first introduced to the cask
   url "https://github.com/YACReader/yacreader/releases/download/#{version.major_minor_patch}/YACReader-#{version}.MacOSX-Intel.dmg"
