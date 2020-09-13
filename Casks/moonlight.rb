509ae3e38b1de5ff03f2a8d02dd6cf11b71d3c73
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-13 02:55:54 +0000

    Update moonlight from 2.1.0 to 2.2.0 (#89040)

--- Casks/moonlight.rb
+++ Casks/moonlight.rb
@@ -1,6 +1,6 @@
 cask "moonlight" do
-  version "2.1.0"
-  sha256 "31a216bb7428deaa67dbe164ff2ac0aff33b408b2d4c509f0a1fe55783789e19"
+  version "2.2.0"
+  sha256 "146578e5f0cff1165b9dabed8cdf8d2af4e342964b1bc1aa9b88d053bad70e8d"
 
   # github.com/moonlight-stream/moonlight-qt/ was verified as official when first introduced to the cask
   url "https://github.com/moonlight-stream/moonlight-qt/releases/download/v#{version}/Moonlight-#{version}.dmg"
