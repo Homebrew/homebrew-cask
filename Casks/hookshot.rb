a6cd2e43c8ba95622aad427201200e8b4b6a198f
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-13 16:28:17 +0000

    Update hookshot from 1.6.3 to 1.7 (#89194)

--- Casks/hookshot.rb
+++ Casks/hookshot.rb
@@ -1,6 +1,6 @@
 cask "hookshot" do
-  version "1.6.3"
-  sha256 "59156731c816a6a316be7898144ab1a52f1ad806c72ba857c0400194876dcb18"
+  version "1.7"
+  sha256 "91bdae08ba1cb4e285803730d478b5549c9cc042234f245f0fd79afa2e84eb1a"
 
   url "https://hookshot.app/downloads/Hookshot#{version}.dmg"
   appcast "https://hookshot.app/downloads/updates.xml"
