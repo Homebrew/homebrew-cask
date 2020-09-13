3222db1003659e63019c9d9bf9945f6eaf13148e
Tapan Shah <tapsboy@gmail.com>
2020-09-13 17:22:30 -0400

    Update rescuetime.rb

--- Casks/rescuetime.rb
+++ Casks/rescuetime.rb
@@ -1,5 +1,5 @@
 cask "rescuetime" do
-  version "2.16.3.1"
+  version "2.16.4.2"
   sha256 "e4a150a0e9278a32c787475c7549b5af47634b0061146e69552bc02a6396a2bf"
 
   url "https://www.rescuetime.com/installers/RescueTimeInstaller.pkg"
