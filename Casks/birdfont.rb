bcbb0ce6aa98ae5d99fcc52ae02a37a5e2abccec
Hannes Braun <hannesbraun@mail.de>
2020-09-13 18:41:48 +0200

    Update birdfont from 4.6.2 to 4.7.12

--- Casks/birdfont.rb
+++ Casks/birdfont.rb
@@ -3,11 +3,11 @@ cask "birdfont" do
     version "2.19.4"
     sha256 "013d9c42c2252b57079453bd27e4c18dbbc09eda55563ff1516fd079c0499f76"
   elsif MacOS.version <= :mojave
-    version "4.6.1"
-    sha256 "a76994562b6920a6685c65321a0349a637441699ba9425e5faae97b1c77799bd"
+    version "4.7.11"
+    sha256 "34f83d5e0a44e30b61d5570c3802f74a04dc174143dd1e30fe55db750665ed65"
   else
-    version "4.6.2"
-    sha256 "2b1a9edcbcd575f81b90d1517b6a49a4a6dbb03f6fd6f7ddd647b474031db7c4"
+    version "4.7.12"
+    sha256 "c461f033cf2852994666e301f3572cb49d308be4ca3662a818049000371f4d69"
   end
 
   url "https://birdfont.org/download/birdfont-#{version}-free.dmg"
