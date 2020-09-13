cc455edfc013b33fae7f213b94af2049b95599d8
Jianing Wang <wjnmailg@gmail.com>
2020-09-13 21:18:33 +0800

    Update OpenInEditor-Lite from 1.1.3 to 1.1.4 (#89190)

--- Casks/openineditor-lite.rb
+++ Casks/openineditor-lite.rb
@@ -1,6 +1,6 @@
 cask "openineditor-lite" do
-  version "1.1.3"
-  sha256 "b46436647a3c9b87c3180ca500bab051442979776cb7dc35543326d0cd3232a3"
+  version "1.1.4"
+  sha256 "97d35878eedd04cfc9b4206051895479031e12e5fd9fbf7bf6ac7135d32c4245"
 
   url "https://github.com/Ji4n1ng/OpenInTerminal/releases/download/#{version}/OpenInEditor-Lite.app.zip"
   appcast "https://github.com/Ji4n1ng/OpenInTerminal/releases.atom"
