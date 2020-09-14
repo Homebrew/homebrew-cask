e381523b1e5af7517fe396c1286227095cb75a51
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-14 17:55:13 +0000

    Update anydo from 4.2.105 to 4.2.106 (#89239)

--- Casks/anydo.rb
+++ Casks/anydo.rb
@@ -1,6 +1,6 @@
 cask "anydo" do
-  version "4.2.105"
-  sha256 "4fba8c340791a5e9ff3d541038e0596c61cf2d45a250bd3c464151942ae31c25"
+  version "4.2.106"
+  sha256 "145be6edd1ac5edfffaeeef8b02a2fb82a9499462d4fad57c878fb8b836cc6b7"
 
   url "https://electron-app.any.do/Any.do.dmg"
   appcast "https://electron-app.any.do/latest-mac.yml"
