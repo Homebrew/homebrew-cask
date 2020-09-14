2e352028c2cc8cf99870861ed37fcc2664c2837f
Randall <ran.dall@icloud.com>
2020-09-14 10:47:31 -0700

    Update airdroid from 3.6.7.0 to 3.6.8.0

--- Casks/airdroid.rb
+++ Casks/airdroid.rb
@@ -1,6 +1,6 @@
 cask "airdroid" do
-  version "3.6.7.0"
-  sha256 "2c35ec891a35e1d355a4f159f1dc6d5f2f4fc2d79664ffc332fc273b10d13409"
+  version "3.6.8.0"
+  sha256 "9f286c4de315a5fd988a54b05b63381c209042930d1400eeb2260a580cd4daae"
 
   # s3.amazonaws.com/dl.airdroid.com/ was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/dl.airdroid.com/AirDroid_Desktop_Client_#{version}.dmg"
