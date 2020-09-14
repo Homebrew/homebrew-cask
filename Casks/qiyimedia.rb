b7bd6872fff7a90ed84b8b190e3225ac25224c14
suschizu <brigitte@corecode.io>
2020-09-14 16:53:20 +0200

    Update qiyimedia from 20200828,11.8.5 to 20200914,11.9.0 (#89222)

--- Casks/qiyimedia.rb
+++ Casks/qiyimedia.rb
@@ -1,6 +1,6 @@
 cask "qiyimedia" do
-  version "20200828,11.8.5"
-  sha256 "38f653bf813d80a8fc54be4bc332e015b9dc239ecc0cafddee01db2ddd7b7dba"
+  version "20200914,11.9.0"
+  sha256 "f92a51ddcdd906935a5e4298f17c56e7c9e1af5da375867082ade0fcda8f44d2"
 
   url "https://mbdapp.iqiyi.com/j/ot/iQIYIMedia_271.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://mbdapp.iqiyi.com/j/ot/iQIYIMedia_271.dmg",
