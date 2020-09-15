6b1360e7d78dad94c181bcba36eb101eeb2e9a71
ivaquero <46377665+integzz@users.noreply.github.com>
2020-09-16 00:12:19 +0800

    add copytranslator 9.0.2

---
+++ Casks/copytranslator.rb
@@ -0,0 +1,14 @@
+cask "copytranslator" do
+    version "9.0.2"
+    sha256 "5da337e06e1ac6325627f99a0662042b1d97689613685cc046441d41d6d1f075"
+
+    url "https://github.com/CopyTranslator/CopyTranslator/releases/download/v#{version}/copytranslator-#{version}.dmg"
+    appcast "https://github.com/CopyTranslator/CopyTranslator/releases.atom"
+    
+    name "copytranslator"
+    homepage "https://copytranslator.github.io/"
+
+    depends_on macos: ">= :sierra"
+
+    app "copytranslator.app"
+end
