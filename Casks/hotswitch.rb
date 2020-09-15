87b04952acd61aa3754fed08ad9d58c9ef233146
CoreCode <core-code@users.noreply.github.com>
2020-09-15 11:24:42 +0200

    Update hotswitch.rb

--- Casks/hotswitch.rb
+++ Casks/hotswitch.rb
@@ -1,6 +1,6 @@
 cask "hotswitch" do
   version "1.21"
-  sha256 "ee1c0d171d1726ab2771408c1dc0e8427e0eeed0e110150e9f000e4dbf3a6801"
+  sha256 "8469f8804214983ff57cf5f7553e63abcafdfffbca7f4a471a799636e1560639"
 
   url "https://oniatsu.github.io/HotSwitch/release/zip/HotSwitch.zip"
   appcast "https://github.com/oniatsu/HotSwitch/releases.atom"
@@ -8,5 +8,5 @@ cask "hotswitch" do
   desc "Fast window switcher using a 2-stroke hotkey"
   homepage "https://oniatsu.github.io/HotSwitch/"
 
-  app "Users/ao/Downloads/HotSwitch 2020-09-14 03-48-40/HotSwitch.app"
+  app "HotSwitch.app"
 end
