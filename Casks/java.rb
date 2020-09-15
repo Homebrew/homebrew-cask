3e361e9912b95f12129c093be763bd33e3b3a8d3
Seeker <meaningseeking@protonmail.com>
2020-09-15 10:28:57 -0700

    Update java from 14.0.2,12:205943a0976c4ed48cb16f1043c5c647 to 15,36:779bf45e88a44cbd9ea6621d33e33db1

--- Casks/java.rb
+++ Casks/java.rb
@@ -1,6 +1,6 @@
 cask "java" do
-  version "14.0.2,12:205943a0976c4ed48cb16f1043c5c647"
-  sha256 "386a96eeef63bf94b450809d69ceaa1c9e32a97230e0a120c1b41786b743ae84"
+  version "15,36:779bf45e88a44cbd9ea6621d33e33db1"
+  sha256 "ab842c8c0953b816be308c098c1a021177a4776bef24da85b6bafbbd657c7e1a"
 
   url "https://download.java.net/java/GA/jdk#{version.before_comma}/#{version.after_colon}/#{version.after_comma.before_colon}/GPL/openjdk-#{version.before_comma}_osx-x64_bin.tar.gz"
   name "OpenJDK"
