b0b2479b79481a1c6dc270d8f3a1abcc6021dc76
Work <brigitte@corecode.io>
2020-09-15 18:56:06 +0200

    Update dropbox-passwords from 5.1.4 to 5.2.4

--- Casks/dropbox-passwords.rb
+++ Casks/dropbox-passwords.rb
@@ -1,10 +1,10 @@
 cask "dropbox-passwords" do
-  version "5.1.4"
-  sha256 "3e586ee75ff0b846371f37e17a12569a2683df112d3cd7f70931e60d53482c15"
+  version "5.2.4"
+  sha256 "74d41ac8296c4dfa71cda236320173c83856b1d99fcc6bcc9de820d669cd85ba"
 
   # clientupdates.dropboxstatic.com was verified as official when first introduced to the cask
   url "https://clientupdates.dropboxstatic.com/dbx-releng/dropbox_passwords/mac/DropboxPasswords_#{version}.dmg"
-  appcast "https://www.dropbox.com/dropbox-passwords-download/mac/beta"
+  appcast "https://www.dropbox.com/dropbox-passwords-download/mac/stable"
   name "Dropbox Passwords"
   desc "Password manager that syncs across devices"
   homepage "https://www.dropbox.com/features/security/passwords"
