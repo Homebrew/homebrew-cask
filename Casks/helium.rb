d58f546b26b9306a052deb1a82ebefe574c4902a
Viktor Szakats <vszakats@users.noreply.github.com>
2020-09-15 12:05:44 +0200

    Secure URLs (#89252)
    * helium: secure url
    
    * meshmixer: secure urls
    
    * td-agent: secure urls

--- Casks/helium.rb
+++ Casks/helium.rb
@@ -3,7 +3,7 @@ cask "helium" do
   sha256 :no_check
 
   # download.clockworkmod.com/carbon/ was verified as official when first introduced to the cask
-  url "http://download.clockworkmod.com/carbon/carbon-mac.zip"
+  url "https://download.clockworkmod.com/carbon/carbon-mac.zip"
   name "Helium"
   homepage "https://github.com/koush/support-wiki/wiki/Helium-Desktop-Installer-and-Android-App"
 
