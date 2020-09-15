5ed5d57c5936c478a35bc8067abfddff4546ebbb
Randall <ran.dall@icloud.com>
2020-09-15 11:14:05 -0700

    Update screen from 3.1.0 to 3.1.3

--- Casks/screen.rb
+++ Casks/screen.rb
@@ -1,6 +1,6 @@
 cask "screen" do
-  version "3.1.0"
-  sha256 "b7c246034c49356af31ea5311fd633a16c40220c5400ab3ab53cbc9956d7debe"
+  version "3.1.3"
+  sha256 "f0dddaaa94ac4085fc8f2b32bafae0d5dd5a56255e808280256c3a9cc6d95c2c"
 
   url "https://download.screen.so/desktop-app/darwin/#{version}/Screen.dmg"
   appcast "https://download.screen.so/"
