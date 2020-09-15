265eac74f579583aeef8c55535ed722d6f2af3dc
Randall <ran.dall@icloud.com>
2020-09-15 11:22:54 -0700

    Update aircall from 2.5.9 to 2.5.10

--- Casks/aircall.rb
+++ Casks/aircall.rb
@@ -1,6 +1,6 @@
 cask "aircall" do
-  version "2.5.9"
-  sha256 "3dacd78ddb6076bd4b5248ff9c10382e63080ae16ad95e10b52fed581e4c4bd1"
+  version "2.5.10"
+  sha256 "e62a5933447a2699d21c7fc26bf112a4f924146fffdd5a04320242c3e1460aee"
 
   # aircall-electron-releases.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://aircall-electron-releases.s3.amazonaws.com/production/Aircall-#{version}.zip"
