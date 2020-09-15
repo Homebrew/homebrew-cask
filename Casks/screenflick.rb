ac4790d82151747f3aeacc408c17274ffc2400ca
li-kbotelho <37940059+li-kbotelho@users.noreply.github.com>
2020-09-14 22:44:33 -0700

    update screenflick.rb checksum
    there was a checksum change, but the version remains the same ¯\_(ツ)_/¯

--- Casks/screenflick.rb
+++ Casks/screenflick.rb
@@ -1,6 +1,6 @@
 cask "screenflick" do
   version "2.7.52"
-  sha256 "fdccfa2d9a25e0f2cf77006ac6c0ff960d62785b6dae536030d88257628ef486"
+  sha256 "e7920e04cd1b3f583daa6a8e6d82af1aaf23a9e5f8ce5258c7d0e2c2177adfa5"
 
   url "https://store.araelium.com/screenflick/downloads/versions/Screenflick#{version}.zip"
   appcast "https://arweb-assets.s3.amazonaws.com/downloads/screenflick/updates.json"
