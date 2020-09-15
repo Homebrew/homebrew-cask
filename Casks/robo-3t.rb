bd19024fb1eb56e1a887dbafa3ed11b95fc54b27
Denis Pershin <positivemind@gmail.com>
2020-09-15 22:23:08 +0300

    Update robo-3t from 1.4.0 to 1.4.1

--- Casks/robo-3t.rb
+++ Casks/robo-3t.rb
@@ -1,6 +1,6 @@
 cask "robo-3t" do
-  version "1.4.0,12e54cc"
-  sha256 "a95ef2029a0d430b935dcd29dc18d48c86b3ccc92e26855db1afd256ef0ccf5b"
+  version "1.4.1,122dbd9"
+  sha256 "02cf60fd969e7c2f7037bb567f558e436618f9a707904f786d1f03f97193a263"
 
   # download.studio3t.com was verified as official when first introduced to the cask
   url "https://download.studio3t.com/robomongo/mac/robo3t-#{version.before_comma}-darwin-x86_64-#{version.after_comma}.dmg"
