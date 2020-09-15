d549e5323695487d1b4e7dc66de075468510274d
CoreCode <core-code@users.noreply.github.com>
2020-09-15 12:09:52 +0200

    Update katalon-studio from 7.6.6 to 7.7.0 (#89259)

--- Casks/katalon-studio.rb
+++ Casks/katalon-studio.rb
@@ -1,6 +1,6 @@
 cask "katalon-studio" do
-  version "7.6.6"
-  sha256 "c98118026fc202ba1a67a3d16d1d498663e52496772f73f12663c9f1688545d9"
+  version "7.7.0"
+  sha256 "0fbe8bc5470aea6a81194eb28b9943016130ecba943a377da58ac45d8a72cd2e"
 
   url "https://download.katalon.com/#{version}/Katalon%20Studio.dmg"
   appcast "https://github.com/katalon-studio/katalon-studio/releases.atom"
