a29cbf38e8609a9cd41dfc349228ec565f76226c
Randall <ran.dall@icloud.com>
2020-09-15 10:54:20 -0700

    Update commander-one from 2.4.2 to 2.5

--- Casks/commander-one.rb
+++ Casks/commander-one.rb
@@ -1,6 +1,6 @@
 cask "commander-one" do
-  version "2.4.2"
-  sha256 "969ff4d2c1679f6c18f7e38848400b27f165c4cd7d6ff0c5a2b40378b53a7e7e"
+  version "2.5"
+  sha256 "40ca3e2ce1de3e8507b80c6f33a3fb289aa133ef13c2d74774f886199754e846"
 
   # cdn.electronic.us/ was verified as official when first introduced to the cask
   url "https://cdn.electronic.us/products/commander/mac/download/commander.dmg"
