e207276ff06ad3bb25fd11bd2d4fe0c90f93378f
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-16 17:13:49 +0000

    Update zoc from 7.26.2 to 7.26.3 (#89395)

--- Casks/zoc.rb
+++ Casks/zoc.rb
@@ -1,6 +1,6 @@
 cask "zoc" do
-  version "7.26.2"
-  sha256 "e76d6213e7e2fd66ccb62ecaad19059a8746ee16db0f2c5cf273f81a69359004"
+  version "7.26.3"
+  sha256 "6748d05ec0ba5eca6f3976f0edc13be6b74056eff4d032a45b3a837c6f36ae48"
 
   url "https://www.emtec.com/downloads/zoc/zoc#{version.no_dots}.dmg"
   appcast "https://www.emtec.com/downloads/zoc/zoc_changes.txt"
