6af8cd92b20b2993dc434166ceebf414c9ac5ed5
Daniel de Haas <daniel.andrew.dehaas@gmail.com>
2020-09-16 11:57:50 -0700

    Update keysmith from 1.0.7 to 1.0.8

--- Casks/keysmith.rb
+++ Casks/keysmith.rb
@@ -1,6 +1,6 @@
 cask "keysmith" do
-  version "1.0.7"
-  sha256 "142ea572e5b8b4f0128ce83901d2631dd9d1a1b990c41c3302c2c8f9cf830624"
+  version "1.0.8"
+  sha256 "5358ce2424a1899770fa73fbeac89da7b5da35390f74a65d71783f5d3e9e45d9"
 
   url "https://keysmith.app/versions/Keysmith-#{version}.dmg"
   appcast "https://www.keysmith.app/versions/appcast.xml"
