8b700f47bf707adff4e021f75ae15c299cc57034
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-16 18:42:18 +0000

    Update torguard from 4.1.0 to 4.1.1 (#89416)

--- Casks/torguard.rb
+++ Casks/torguard.rb
@@ -1,6 +1,6 @@
 cask "torguard" do
-  version "4.1.0"
-  sha256 "85c58dd2527392f255bf4504253fc5d11ef9a3006947505c31b3254394789cb2"
+  version "4.1.1"
+  sha256 "7de7a29e537f7d16f7428a0f037239032eab5c693b2ddb5cce5f1264f963d6d2"
 
   # torguard.biz/ was verified as official when first introduced to the cask
   url "https://updates.torguard.biz/Software/MacOSX/TorGuard-v#{version}.dmg"
