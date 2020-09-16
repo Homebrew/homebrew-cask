4c8673678413286e9445840aa16f2b19e9d1b486
Randall <ran.dall@icloud.com>
2020-09-16 09:28:36 -0700

    Update icons8 from 5.7.3 to 5.7.4

--- Casks/icons8.rb
+++ Casks/icons8.rb
@@ -1,7 +1,7 @@
 cask "icons8" do
   # note: "8" is not a version number, but an intrinsic part of the product name
-  version "5.7.3"
-  sha256 "0ba663e09d78ad4b83166be3523a5f8b27d88f862e0040dfd1689e23b53e82a4"
+  version "5.7.4"
+  sha256 "35c0ed3421607a7a7591b15e3c305c6efd52db6f6d73bc0950b19a49007b4ae0"
 
   url "https://desktop.icons8.com/updates/mac/Icons8App_for_Mac_OS.dmg"
   appcast "https://desktop.icons8.com/updates/mac/icons8_cast.xml"
