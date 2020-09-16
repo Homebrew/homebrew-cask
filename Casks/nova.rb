bc84a754f2e8ff4df752524155f435b99d802e10
Hugo Jobling <hello@thisishugo.com>
2020-09-16 20:19:30 +0100

    Add Nova 1.0

---
+++ Casks/nova.rb
@@ -0,0 +1,19 @@
+cask "nova" do
+  version "1.0"
+  sha256 "c5414f5cf6888a8c53f91b53d5bdd1cd246e31aafad3c3bec381c10ef6a44a94"
+
+  # panic-c157.kxcdn.com was verified as official when first introduced to the cask
+  url "https://panic-c157.kxcdn.com/nova/Nova%20#{version}.zip"
+  name "Panic Nova"
+  desc "Native code editor"
+  homepage "https://nova.app/"
+
+  auto_updates true
+
+  app "Nova.app"
+  binary "#{appdir}/Nova.app/Contents/MacOS/Nova", target: "nova"
+
+  zap trash: [
+    "~/Library/Application Support/Nova",
+  ]
+end
