f48c61ac0959eca521b2bfbdbcb9686489405270
Randall <ran.dall@icloud.com>
2020-09-16 10:45:11 -0700

    Update kubernetic from 2.11.1 to 2.12.0

--- Casks/kubernetic.rb
+++ Casks/kubernetic.rb
@@ -1,6 +1,6 @@
 cask "kubernetic" do
-  version "2.11.1"
-  sha256 "2af1cdb480992b35f195e5c37aaec9af099fd89b29690156f22eaa02d42e8802"
+  version "2.12.0"
+  sha256 "fc46760581d57ed0401a2d6273edaaa55fd9c97451424c2373f61ed29c50ba25"
 
   # kubernetic.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://kubernetic.s3.amazonaws.com/Kubernetic-#{version}.dmg"
