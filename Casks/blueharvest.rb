165b7a49c0693adb2a5ed218050d1e88806d673c
CoreCode <core-code@users.noreply.github.com>
2020-09-17 23:46:07 +0200

    Update blueharvest.rb

--- Casks/blueharvest.rb
+++ Casks/blueharvest.rb
@@ -3,7 +3,8 @@ cask "blueharvest" do
   sha256 "a4e906fe8c0227a2b5fe02b8d82b01d5d680fed1e8bc1f3bddb86a66283061f3"
 
   url "https://zeroonetwenty.com/blueharvest/downloads/BlueHarvest#{version.no_dots}.dmg"
-  appcast "https://zeroonetwenty.com/blueharvest/release-notes.html"
+  appcast "https://zeroonetwenty.com/blueharvest/release-notes.html",
+          must_contain: "#{version.no_dots}.dmg"  
   name "BlueHarvest"
   homepage "https://zeroonetwenty.com/blueharvest/"
 
