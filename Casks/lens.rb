9fbfd46cdf4b2de0aa6a45759a2cfd3499089f3b
Randall <ran.dall@icloud.com>
2020-09-17 07:42:56 -0700

    Update lens from 3.6.0 to 3.6.2

--- Casks/lens.rb
+++ Casks/lens.rb
@@ -1,6 +1,6 @@
 cask "lens" do
-  version "3.6.0"
-  sha256 "c4d335b9bd96d62e92e95c851a408a484fe265004cf8aedaa7f9fce1aadb61d2"
+  version "3.6.2"
+  sha256 "b18e10c53f33c805ad57abec73476b4aea9de34dc48c0e8ab17a65689fa73a69"
 
   # github.com/lensapp/lens/ was verified as official when first introduced to the cask
   url "https://github.com/lensapp/lens/releases/download/v#{version}/Lens-#{version}.dmg"
