7d85b989d6de22ef42c2e057484060b949b5bff4
CoreCode <core-code@users.noreply.github.com>
2020-09-18 00:51:59 +0200

    Update loom.rb

--- Casks/loom.rb
+++ Casks/loom.rb
@@ -1,5 +1,5 @@
 cask "loom" do
-  version "0.50.7"
+  version "0.50.8"
   sha256 "47ffaa0d727e92ad4a2041bbb68ac87034e06da965e3602f72740c611efca836"
 
   url "https://cdn.loom.com/desktop-packages/Loom-#{version}.dmg"
