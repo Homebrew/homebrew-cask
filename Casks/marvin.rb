dc2085ccb9988d95f369c4f727f2c3d96751d39e
Randall <ran.dall@icloud.com>
2020-09-17 08:10:19 -0700

    Update marvin from 1.55.0 to 1.55.1

--- Casks/marvin.rb
+++ Casks/marvin.rb
@@ -1,6 +1,6 @@
 cask "marvin" do
-  version "1.55.0"
-  sha256 "cf5e6c409ae2622fda4ba943714760bddb5193bee30db79a314ead9c11828140"
+  version "1.55.1"
+  sha256 "e784c5145d20a7b1ec2ae9a95d151bb857b6f51fe0bfa53b123bf6d1b0948947"
 
   # amazingmarvin.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://amazingmarvin.s3.amazonaws.com/Marvin-#{version}.dmg"
