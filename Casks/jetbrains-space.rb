9ecd4b212652ed5be8b942fb14f0a1aed8649771
tiulpin <viktor@tiulp.in>
2020-09-17 19:22:20 +0300

    Add JetBrains Space 2020.1.8

---
+++ Casks/jetbrains-space.rb
@@ -0,0 +1,11 @@
+cask "jetbrains-space" do
+  version :latest
+  sha256 :no_check
+
+  url "https://download.jetbrains.com/space/jetbrains-space.dmg"
+  name "JetBrains Space"
+  desc "The integrated team environment"
+  homepage "https://www.jetbrains.com/space/"
+
+  app "JetBrains Space.app"
+end
