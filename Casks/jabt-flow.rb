671aeb2dd93efe649533a8b71daa9e043e1fd918
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-18 16:57:17 +0000

    Update jabt-flow from 1.8.8 to 1.9.0 (#89519)

--- Casks/jabt-flow.rb
+++ Casks/jabt-flow.rb
@@ -1,6 +1,6 @@
 cask "jabt-flow" do
-  version "1.8.8"
-  sha256 "dc0103e515989890d5147c16314a384fab689d8a622c1d88d200ad94fe54b2c4"
+  version "1.9.0"
+  sha256 "d65d524860b3aee28904f61bd9d831955dc0ade1f884716e62e4f02ff967bbf7"
 
   # Download page is at https://createwithflow.com/releases/
   # createwithflow-updates.s3-us-west-2.amazonaws.com/ was verified as official when first introduced to the cask
