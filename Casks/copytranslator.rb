9c7e097a03943a7a67873d11ab25494e753db4df
ivaquero <46377665+integzz@users.noreply.github.com>
2020-09-18 08:44:37 +0800

    style fix

--- Casks/copytranslator.rb
+++ Casks/copytranslator.rb
@@ -1,14 +1,14 @@
 cask "copytranslator" do
   version "9.0.2"
   sha256 "5da337e06e1ac6325627f99a0662042b1d97689613685cc046441d41d6d1f075"
-  
+
   # github.com/CopyTranslator/CopyTranslator/ was verified as official when first introduced to the cask
   url "https://github.com/CopyTranslator/CopyTranslator/releases/download/v#{version}/copytranslator-#{version}.dmg"
   appcast "https://github.com/CopyTranslator/CopyTranslator/releases.atom"
   name "CopyTranslator"
-  desc "复制即翻译的外文辅助阅读翻译解决方案"
+  desc "Copy-as-translation solutions for foreign language assisted reading and translation"
   homepage "https://copytranslator.github.io/"
-  
+
   depends_on macos: ">= :sierra"
 
   app "copytranslator.app"
