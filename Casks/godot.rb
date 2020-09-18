c009915056abfddfbc7f55e45473a8351fbcfc2e
George Linkovsky <Timofffee@users.noreply.github.com>
2020-09-18 14:34:45 +0400

    Update godot.rb

--- Casks/godot.rb
+++ Casks/godot.rb
@@ -1,6 +1,6 @@
 cask "godot" do
-  version "3.2.2"
-  sha256 "36d1002ad3a99314e03195f944b86b3d9b694ad4938a32e88c4ac51c7bf9b893"
+  version "3.2.3"
+  sha256 "196dc7319dc40f9b4ee5626371a4cb6d7227eda3c439023630f314b57a581672"
 
   # downloads.tuxfamily.org/godotengine/ was verified as official when first introduced to the cask
   url "https://downloads.tuxfamily.org/godotengine/#{version}/Godot_v#{version}-stable_osx.64.zip"
