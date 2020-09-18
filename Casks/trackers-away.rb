0064e14b15b7c5fb3a9eb9c9df75f49c25984616
tim-hub <tim_w@outlook.com>
2020-09-18 23:26:12 +1200

    fix by adding appcast for trackeraway v1.0.3

--- Casks/trackers-away.rb
+++ Casks/trackers-away.rb
@@ -3,6 +3,7 @@ cask "trackers-away" do
   sha256 "3877b21814f384e5fb34524a5aca966c5bf56de622f30c836826e5378d8ea60d"
 
   url "https://github.com/tim-hub/TrackersAway/releases/download/v#{version}/Trackers-Away-#{version}.dmg"
+  appcast "https://github.com/tim-hub/TrackersAway/releases.atom"
   name "Trackers Away"
   desc "The best cross-platform ads and trackers blocker and hosts manager."
   homepage "https://github.com/tim-hub/TrackersAway/"
