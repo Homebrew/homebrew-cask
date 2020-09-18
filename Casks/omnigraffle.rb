632f5b18bfd2f75492e5e46e90636788e6555b13
miccal <miccal.matthews@gmail.com>
2020-09-18 17:28:04 +0800

    omnigraffle 7.17.5

--- Casks/omnigraffle.rb
+++ Casks/omnigraffle.rb
@@ -8,13 +8,14 @@ cask "omnigraffle" do
     sha256 "83ef24af2dbd7977b9922e992f17f23e102562f0589d28bc37d5579b4a4d4938"
     url "https://downloads.omnigroup.com/software/MacOSX/10.13/OmniGraffle-#{version}.dmg"
   else
-    version "7.17.4"
-    sha256 "4c9d3eb90b52e4e7b9f9bb7c487468f2afaf356f0244d20dd8aa98d7677ceed3"
+    version "7.17.5"
+    sha256 "9a4c041288c2da2c6d2dc35908b328e2ab4b8033c74e9dac31fa299c43c36077"
     url "https://downloads.omnigroup.com/software/MacOSX/10.14/OmniGraffle-#{version}.dmg"
   end
 
   appcast "https://update.omnigroup.com/appcast/com.omnigroup.OmniGraffle#{version.major}"
   name "OmniGraffle"
+  desc "Visual communication software"
   homepage "https://www.omnigroup.com/omnigraffle/"
 
   auto_updates true
