15d4a86d64bc85c7e32462d2504dc357dd869d01
Niklas Graeber <dev@n1klas.net>
2020-09-19 02:17:01 +0200

    Home Assistant: fix url comment

--- Casks/home-assistant.rb
+++ Casks/home-assistant.rb
@@ -2,7 +2,7 @@ cask "home-assistant" do
   version "2020.7,1"
   sha256 "118fb5a137784c0f546157a08d04d02925e94da0d804bd6aa17458a246df247a"
 
-  # github.com was verified as official when first introduced to the cask
+  # github.com/home-assistant/iOS/ was verified as official when first introduced to the cask
   url "https://github.com/home-assistant/iOS/releases/download/mac/#{version.before_comma}/#{version.after_comma}/home-assistant-mac.zip"
   appcast "https://github.com/home-assistant/iOS/releases.atom"
   name "Home Assistant"
