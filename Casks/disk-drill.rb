16d78234965c24fa40a3ce22724a8063a6802e0e
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-19 19:40:12 +0000

    Update disk-drill from 3.8.975 to 3.8.977 (#89566)

--- Casks/disk-drill.rb
+++ Casks/disk-drill.rb
@@ -1,6 +1,6 @@
 cask "disk-drill" do
-  version "3.8.975"
-  sha256 "7681d8b279c772849b770dd01647f34d3ccc2b8df849c1e24d5aa4b97b5bc184"
+  version "3.8.977"
+  sha256 "c87f1d7d016d950396e328fafa237ea4aff5c6cbd2bd62e091c43750f6e0976a"
 
   url "https://www.cleverfiles.com/releases/DiskDrill_#{version}.zip"
   appcast "https://www.cleverfiles.com/releases/auto-update/dd2-newestr.xml"
