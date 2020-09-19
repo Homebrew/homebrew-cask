988d292bc1edcb4e9a1694f33af6cb2fcca60dea
Markus Reiter <me@reitermark.us>
2020-09-19 20:22:40 +0200

    Update copytranslator.rb

--- Casks/copytranslator.rb
+++ Casks/copytranslator.rb
@@ -2,7 +2,7 @@ cask "copytranslator" do
   version "9.0.2"
   sha256 "5da337e06e1ac6325627f99a0662042b1d97689613685cc046441d41d6d1f075"
 
-  # https://github.com/CopyTranslator/CopyTranslator/ was verified as official when first introduced to the cask
+  # github.com/CopyTranslator/CopyTranslator/ was verified as official when first introduced to the cask
   url "https://github.com/CopyTranslator/CopyTranslator/releases/download/v#{version}/copytranslator-#{version}.dmg"
   appcast "https://github.com/CopyTranslator/CopyTranslator/releases.atom"
   name "CopyTranslator"
