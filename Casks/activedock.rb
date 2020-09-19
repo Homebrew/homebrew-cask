440f74a44955f440bc1ecf4606b7eb5e51d61182
CoreCode <core-code@users.noreply.github.com>
2020-09-19 21:18:47 +0200

    Update activedock.rb

--- Casks/activedock.rb
+++ Casks/activedock.rb
@@ -1,5 +1,5 @@
 cask "activedock" do
-  version "2.48,2048"
+  version "2.49,2049"
   sha256 "9415f9574dc82e08fc875b1ec6a9698553cbe8b05e4587f6ec4a8ebc1322eb53"
 
   url "https://noteifyapp.com/download/ActiveDock.dmg"
