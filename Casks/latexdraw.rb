c85c6b92ea0c6983cd27900066b9b29300308b62
miccal <miccal.matthews@gmail.com>
2020-09-19 20:45:01 +0800

    latexdraw: add desc and zap

--- Casks/latexdraw.rb
+++ Casks/latexdraw.rb
@@ -6,7 +6,14 @@ cask "latexdraw" do
   url "https://downloads.sourceforge.net/latexdraw/LaTeXDraw-#{version}.dmg"
   appcast "https://github.com/latexdraw/latexdraw/releases.atom"
   name "LaTexDraw"
+  desc "Drawing editor for creating LaTeX PSTricks code"
   homepage "https://latexdraw.sourceforge.io/"
 
   app "LaTeXDraw.app"
+
+  zap trash: [
+    "~/.latexdraw",
+    "~/Library/Preferences/latexdraw.plist",
+    "~/Library/Saved Application State/latexdraw.savedState",
+  ]
 end
