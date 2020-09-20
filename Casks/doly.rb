cdb745c79556d9ea31c0da5d601edeb0aabffc7c
CoreCode <core-code@users.noreply.github.com>
2020-09-20 19:05:35 +0200

    Update doly appcast (#89580)

--- Casks/doly.rb
+++ Casks/doly.rb
@@ -3,7 +3,7 @@ cask "doly" do
   sha256 "379cb026668d7952a060ecf6f8abd6810bd19c3d8ad7f440bebe153c29d2d2cf"
 
   url "http://doly.jp/download/doly-mac_#{version}.dmg"
-  appcast "http://doly.jp/"
+  appcast "http://doly.jp/update_mac.html"
   name "Doly Ebook Reader"
   homepage "http://doly.jp/"
 
