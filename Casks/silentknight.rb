af88434e1646ae47a7bafeb751148c09f5b5d24e
CoreCode <core-code@users.noreply.github.com>
2020-09-20 20:46:06 +0200

    Update silentknight.rb

--- Casks/silentknight.rb
+++ Casks/silentknight.rb
@@ -4,7 +4,7 @@ cask "silentknight" do
 
   # eclecticlightdotcom.files.wordpress.com/ was verified as official when first introduced to the cask
   url "https://eclecticlightdotcom.files.wordpress.com/#{version.after_comma.major}/#{version.after_comma.minor}/silentknight#{version.before_comma.no_dots}.zip"
-  appcast "https://eclecticlight.co/lockrattler-systhist/"
+  appcast "https://www.macupdater.net/cgi-bin/extract_text/extract_text_split_easy.cgi?url=https://raw.githubusercontent.com/hoakleyelc/updates/master/eclecticapps.plist&splitter_1=SilentKnight&index_1=1&splitter_2=Version&index_2=1&splitter_3=dict&index_3=0"
   name "SilentKnight"
   desc "Automatically checks computer's security"
   homepage "https://eclecticlight.co/lockrattler-systhist/"
