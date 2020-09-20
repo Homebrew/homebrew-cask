fd5f831db97fccb6c5a216aa76077dc9657865c7
CoreCode <core-code@users.noreply.github.com>
2020-09-20 21:17:47 +0200

    Update jaxx-liberty.rb

--- Casks/jaxx-liberty.rb
+++ Casks/jaxx-liberty.rb
@@ -3,7 +3,7 @@ cask "jaxx-liberty" do
   sha256 "da93cbc60c72710d4b4cb34897751ca481a0fdbd72c06955a94e364b894c3085"
 
   url "https://download-liberty.jaxx.io/Jaxx.Liberty-#{version}.dmg"
-  appcast "https://jaxx.io/changeLog"
+  appcast "https://download-liberty.jaxx.io/latest-mac.yml"
   name "Jaxx Blockchain Wallet"
   homepage "https://jaxx.io/"
 
