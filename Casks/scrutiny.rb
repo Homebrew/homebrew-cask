d88386c5689f4aed995d951a25a387d58435952b
CoreCode <core-code@users.noreply.github.com>
2020-09-20 18:44:16 +0200

    Update scrutiny from 9.10.0 to 9.11.0 (#89578)

--- Casks/scrutiny.rb
+++ Casks/scrutiny.rb
@@ -1,6 +1,6 @@
 cask "scrutiny" do
-  version "9.10.0"
-  sha256 "a415c39269844af5a4461a199800fa76af87a00a3ab06894ab3404e08eccb5cb"
+  version "9.11.0"
+  sha256 "501ed61f5dbb30bcd6a1bad02b3673b2fedcc9cec9ab01e8ee0155c143693d45"
 
   url "https://peacockmedia.software/mac/scrutiny/scrutiny.dmg"
   appcast "https://peacockmedia.software/mac/scrutiny/version_history.html"
