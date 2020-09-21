d0bce13995a9f701cc3f3ea6f63e250ce5a3206d
Nathan Pierce <NorseGaud@users.noreply.github.com>
2020-09-21 12:51:11 -0400

    Add and update Anka casks (#89249)

--- Casks/anka-virtualization.rb
+++ Casks/anka-virtualization.rb
@@ -1,12 +1,12 @@
-cask "anka-flow" do
+cask "anka-virtualization" do
   version "2.2.3.118"
   sha256 "3f8937ea296ff16e87940b541e9b8ff67ffa23fe3f8d341c9bf5ff699598f776"
 
   # d1efqjhnhbvc57.cloudfront.net/ was verified as official when first introduced to the cask
-  url "https://d1efqjhnhbvc57.cloudfront.net/Anka-#{version}.pkg",
-      referer: "https://veertu.com/download-anka-run/"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://veertu.com/downloads/anka-virtualization-latest"
-  name "Veertu Anka Flow"
+  url "https://d1efqjhnhbvc57.cloudfront.net/Anka-#{version}.pkg"
+  appcast "https://ankadocs.veertu.com/docs/release-notes/"
+  name "Anka Virtualization"
+  desc "CLI tool for managing and creating macOS virtual machines"
   homepage "https://veertu.com/"
 
   depends_on macos: ">= :yosemite"
