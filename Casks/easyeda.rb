23d2e14570ea50806c505f982e454fdd98f38c3d
Randall <ran.dall@icloud.com>
2020-09-21 08:12:20 -0700

    Update easyeda from 6.4.5 to 6.4.7

--- Casks/easyeda.rb
+++ Casks/easyeda.rb
@@ -1,6 +1,6 @@
 cask "easyeda" do
-  version "6.4.5"
-  sha256 "6e3e7d9b443fa72b6b81c2d8f0a0dbe8568b38d2c552dfac73789db8dd576ade"
+  version "6.4.7"
+  sha256 "f2162b6d7ef6b2f7eb1c4ce00bce7ffbfee8392e9fd526fbafa1d84522bf2db9"
 
   url "https://image.easyeda.com/files/easyeda-mac-darwin-#{version}.zip"
   appcast "https://easyeda.com/page/download"
