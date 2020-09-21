c78f647d366dd5c18d7d9bdf75e67797d0d1d965
suschizu <brigitte@corecode.io>
2020-09-21 12:51:07 +0200

    Update macdive from 2.11.4 to 2.12.0 (#89608)

--- Casks/macdive.rb
+++ Casks/macdive.rb
@@ -1,6 +1,6 @@
 cask "macdive" do
-  version "2.11.4"
-  sha256 "acbd7c7f7e4fe81d47526a4acb6cd2cb9f0062e836c0f3340092dd7cccaf1d58"
+  version "2.12.0"
+  sha256 "6a41df499fba9cf5862287fb0f2e28800115742b658273cf74d9ac44677195b1"
 
   url "https://www.mac-dive.com/downloads/MacDive_#{version}.dmg"
   appcast "https://www.mac-dive.com/shimmer/?appcast&appName=MacDive"
