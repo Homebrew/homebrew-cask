946cab4b16ea0aebb880f30ccafe35335c091cf1
suschizu <brigitte@corecode.io>
2020-09-21 18:59:23 +0200

    Update frhelper.rb (#89635)

--- Casks/frhelper.rb
+++ Casks/frhelper.rb
@@ -1,6 +1,6 @@
 cask "frhelper" do
-  version "3.9.9,2020-08-08"
-  sha256 "7c464b7a7d77cb82c07629b6cc51898b70517d3ad25cc39b57ad6d185ff0bd3a"
+  version "3.9.10,2020-08-21"
+  sha256 "42a4f6a89c3491be96d5bc883f92f0dcac949cf4b4b0ad63117faf7c071a16e9"
 
   # static.frdic.com/ was verified as official when first introduced to the cask
   url "https://static.frdic.com/pkg/fhmac.dmg?v=#{version.after_comma}",
