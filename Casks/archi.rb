c8aed00fd72fe604686e9881cca071f44b997dbc
Pierre-Henri Schott <pierrehenri.schott@axa.com>
2020-09-21 15:55:55 +0200

    Fix 4 offenses detected by cask style.

--- Casks/archi.rb
+++ Casks/archi.rb
@@ -1,12 +1,11 @@
 cask "archi" do
-
   version "4.7.1"
   sha256 "19fa57ef812949963e1af5cc2516fc5f544f6e86dd69bd5608794e11935ee07d"
 
   url "https://www.archimatetool.com/downloads/71cb57dd/Archi-Mac-#{version}.zip"
   appcast "https://github.com/archimatetool/archi/releases.atom"
   name "Archimate modeling"
-  desc "An open source modelling toolkit to create ArchiMate 3.1 models and sketches."
+  desc "Open source modelling toolkit to create ArchiMate 3.1 models and sketches"
   homepage "https://www.archimatetool.com/"
 
   auto_updates true
@@ -20,5 +19,4 @@ cask "archi" do
       Or install cask using --no-quarantine option
     EOS
   end
-
 end
