9c8eacbbec386c979c98f08e67bb8dfc49617db8
suschizu <brigitte@corecode.io>
2020-09-22 23:08:09 +0200

    Update spatterlight.rb (#89658)

--- Casks/spatterlight.rb
+++ Casks/spatterlight.rb
@@ -1,9 +1,10 @@
 cask "spatterlight" do
-  version "0.5.0"
-  sha256 "1729c51676f791149f4829454318f373eff43bf8d8388ecbe3b345308c669ba1"
+  version "0.5.13b"
+  sha256 "3a145efc57aa4b206dc3a95bc81debe7385b83371a1e8d74c0372c77c4e43bf6"
 
-  url "http://ccxvii.net/spatterlight/download/spatterlight-#{version}.zip"
-  appcast "http://ccxvii.net/spatterlight/"
+  # github.com/angstsmurf/spatterlight/ was verified as official when first introduced to the cask
+  url "https://github.com/angstsmurf/spatterlight/releases/download/v#{version}/Spatterlight_Catalina.zip"
+  appcast "https://github.com/angstsmurf/spatterlight/releases.atom"
   name "Spatterlight"
   homepage "http://ccxvii.net/spatterlight/"
 
