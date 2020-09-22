63305bed6877287cb094bad98cc32fab28800619
Pierre-Henri Schott <pierrehenri.schott@axa.com>
2020-09-22 09:35:01 +0200

    Adding zap

--- Casks/archi.rb
+++ Casks/archi.rb
@@ -1,16 +1,20 @@
 cask "archi" do
-  version "4.7.1"
+  version "4.7.1,71cb57dd"
   sha256 "19fa57ef812949963e1af5cc2516fc5f544f6e86dd69bd5608794e11935ee07d"
 
-  url "https://www.archimatetool.com/downloads/71cb57dd/Archi-Mac-#{version}.zip"
+  url "https://www.archimatetool.com/downloads/#{version.after_comma}/Archi-Mac-#{version.before_comma}.zip"
   appcast "https://github.com/archimatetool/archi/releases.atom"
   name "Archimate modeling"
-  desc "Open source modelling toolkit to create ArchiMate 3.1 models and sketches"
+  desc "Toolkit for the ArchiMate modelling language"
   homepage "https://www.archimatetool.com/"
 
   auto_updates true
 
-  app "Archi"
+  suite "Archi"
+
+  zap trash: [
+    "~/Library/Application Support/Archi4",
+  ]
 
   def caveats
     <<~EOS
