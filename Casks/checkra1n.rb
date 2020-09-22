bd3fede3418044f154a20eda0c60ae947d3686b3
Lorenzo Pane <lpane@lmac.local>
2020-09-21 23:02:47 -0400

    Update checkra1n to beta 0.11.0 and add desc

--- Casks/checkra1n.rb
+++ Casks/checkra1n.rb
@@ -1,10 +1,11 @@
 cask "checkra1n" do
-  version "0.10.2"
-  sha256 "043fc022a54653e9b2b92d7d2da1d6316c8d462fd181780e1d1a23b9d40c4ce2"
+  version "0.11.0"
+  sha256 "d4def982494bc0b99c6df57dc94338c205902aaa8949e9ae046812ed57743ccb"
 
   url "https://checkra.in/assets/downloads/macos/#{sha256}/checkra1n%20beta%20#{version}.dmg"
   appcast "https://checkra.in/"
   name "checkra1n"
+  desc "Jailbreak for iPhone 5s through iPhone X, iOS 12.0 and up"
   homepage "https://checkra.in/"
 
   app "checkra1n.app"
