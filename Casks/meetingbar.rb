f95f8a5f6c48ce7ecfd01c1d0b6af6b3dabfa0d2
Andrii Leitsius <leits.dev@gmail.com>
2020-09-22 15:42:36 +0300

    Update meetingbar.rb

--- Casks/meetingbar.rb
+++ Casks/meetingbar.rb
@@ -1,6 +1,6 @@
 cask "meetingbar" do
   version "1.6.1"
-  sha256 "d7509eec023e62c3320ddd2a2a1c787d1b90bb96622d38952ea57254b7f66849"
+  sha256 "1f3a571341769c8777f0c493ae7e8e129d8cfcd340502ff7bde959a64596bcd4"
 
   url "https://github.com/leits/MeetingBar/releases/download/v#{version}/MeetingBar.dmg"
   appcast "https://github.com/leits/MeetingBar/releases.atom"
