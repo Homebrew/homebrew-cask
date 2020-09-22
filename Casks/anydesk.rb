4d18152725961396fa9d5436a07f92af09312fba
tzinmein <tzinmein@kidotech.com>
2020-09-23 06:30:13 +0800

    Update anydesk from 6.0.1 to 6.0.2 (#89683)

--- Casks/anydesk.rb
+++ Casks/anydesk.rb
@@ -1,6 +1,6 @@
 cask "anydesk" do
-  version "6.0.1"
-  sha256 "bcdd0f8c3a42b291f23aac43e3309a5a6b8d5016d7c220e8d306ab40b802eb02"
+  version "6.0.2"
+  sha256 "492fab19db6457fa30200436b8e882a05bd72f5a222ea79be554f60260aaca6f"
 
   url "https://download.anydesk.com/anydesk.dmg"
   appcast "https://anydesk.com/en/downloads/mac-os"
