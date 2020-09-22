48d84b994c8e5bfec9bc2d37138698120ca90dee
Randall <ran.dall@icloud.com>
2020-09-22 13:45:24 -0700

    Update responsively from 0.12.0 to 0.13.0

--- Casks/responsively.rb
+++ Casks/responsively.rb
@@ -1,6 +1,6 @@
 cask "responsively" do
-  version "0.12.0"
-  sha256 "b0565e324d68a993128ae6a335e7696f8ffe639a67bb8d623cc702dc19e07af1"
+  version "0.13.0"
+  sha256 "c0a387f7e0a53e5a8db5f7da717716cb6ec58e0fd3cf699d08c891254b17e241"
 
   # github.com/responsively-org/responsively-app/ was verified as official when first introduced to the cask
   url "https://github.com/responsively-org/responsively-app/releases/download/v#{version}/ResponsivelyApp-#{version}.dmg"
