e9e5f4085bbfaf9157cbcce6ee9b23c4a8a666fe
phschott <71520751+phschott@users.noreply.github.com>
2020-09-22 14:44:45 +0200

    Add Archi.app 4.7.1 (#89607)
    * Add Archi.app 4.7.1
    
    * Fix 4 offenses detected by cask style.
    
    * Update archi.rb
    
    * Update archi.rb
    
    * Adding zap
    
    * Update archi.rb
    
    * Update archi.rb
    
    Co-authored-by: Pierre-Henri Schott <pierrehenri.schott@axa.com>
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/archi.rb
@@ -0,0 +1,16 @@
+cask "archi" do
+  version "4.7.1,71cb57dd"
+  sha256 "19fa57ef812949963e1af5cc2516fc5f544f6e86dd69bd5608794e11935ee07d"
+
+  url "https://www.archimatetool.com/downloads/#{version.after_comma}/Archi-Mac-#{version.before_comma}.zip"
+  appcast "https://github.com/archimatetool/archi/releases.atom"
+  name "Archimate modeling"
+  desc "Toolkit for the ArchiMate modelling language"
+  homepage "https://www.archimatetool.com/"
+
+  auto_updates true
+
+  app "Archi/Archi.app"
+
+  zap trash: "~/Library/Application Support/Archi4"
+end
