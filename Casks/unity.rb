8b5d73ee0a3f3bf23560593fd11de429f4c40531
Brigitte Zmoelnig <brigitte@corecode.io>
2020-09-22 22:29:47 +0200

    Update unity from 2020.1.4f1,fa717bb873ec to 2020.1.6f1,fc477ca6df10

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -1,5 +1,5 @@
 cask "unity" do
-  version "2020.1.4f1,fa717bb873ec"
+  version "2020.1.6f1,fc477ca6df10"
   sha256 "df7d34d6afc28f343f3ab6c9c7fae2c0112c90c00062215d79cfc815e67efd1f"
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorInstaller/Unity-#{version.before_comma}.pkg"
