ff6062109a23a5dcbb27ad6ee4a0643065043819
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2020-09-23 15:25:44 -0500

    Add Craft Latest

---
+++ Casks/craft.rb
@@ -0,0 +1,11 @@
+cask "craft" do
+  version :latest
+  sha256 :no_check
+
+  url "https://craft.do/builds/Craft.dmg"
+  name "Craft"
+  desc "Fresh take on documents"
+  homepage "https://craft.do/"
+
+  app "Craft.app"
+end
