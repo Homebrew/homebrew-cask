63aebed6d22f869df8e93256ee41c2935e21d5ed
suschizu <brigitte@corecode.io>
2020-09-23 19:10:01 +0200

    Update unity-lumin-support-for-editor.rb

--- Casks/unity-lumin-support-for-editor.rb
+++ Casks/unity-lumin-support-for-editor.rb
@@ -2,10 +2,11 @@ cask "unity-lumin-support-for-editor" do
   version "2020.1.6f1,fc477ca6df10"
   sha256 "34754b7ab7bd3b66b2636d49bceb27d143988b1cadc36266236376d269541c33"
 
+  # download.unity3d.com/unity/ was verified as official when first introduced to the cask
   url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Lumin-Support-for-Editor-#{version.before_comma}.pkg"
   appcast "https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json"
   name "Unity Lumin Build Support"
-  homepage "https://unity3d.com/unity/whats-new/"
+  homepage "https://unity.com/products"
 
   depends_on cask: "unity"
 
