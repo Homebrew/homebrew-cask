3daae3d6663107ba616a5f62b3515f96c47dc443
Randall <ran.dall@icloud.com>
2020-09-23 12:09:04 -0700

    Update expressvpn from 7.11.4.4 to 7.11.5.4

--- Casks/expressvpn.rb
+++ Casks/expressvpn.rb
@@ -1,6 +1,6 @@
 cask "expressvpn" do
-  version "7.11.4.4"
-  sha256 "8aa38570798172fb4b1586ff89786d07648cc5a6175f74a15f50450c05b75a88"
+  version "7.11.5.4"
+  sha256 "5bc56de63dc00ebeb81201f9a1eb5879ac6ae6534c5cc1c236008711f6c05d8f"
 
   url "https://download.expressvpn.xyz/clients/mac/expressvpn_mac_#{version}_release.pkg"
   appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.expressvpn.xyz/clients/latest/mac"
