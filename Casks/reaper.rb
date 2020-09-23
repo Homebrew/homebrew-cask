b76424c879bc3b13eec89647918395d420afb9b7
Hannes Braun <hannesbraun@mail.de>
2020-09-23 13:12:09 +0200

    Update reaper

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -2,7 +2,7 @@ cask "reaper" do
   version "6.14.0,6.14"
 
   if MacOS.version <= :mojave
-    sha256 "58f4e622a39fe7f57cd07a96e33d485b9fff8dfac5633bb0ac2b4255d2d9a972"
+    sha256 "3fcd1d6168462d6440e8e0763311d47017f479e39695c11ef926900e2a46efc7"
 
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.after_comma.no_dots}_x86_64.dmg"
   else
