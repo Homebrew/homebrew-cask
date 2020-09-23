9fd0d881cbe3b478ff7b930a67c3fa87bd3b2c51
Miccal Matthews <miccal.matthews@gmail.com>
2020-09-23 09:38:13 +0800

    Update unity.rb

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -2,6 +2,7 @@ cask "unity" do
   version "2020.1.6f1,fc477ca6df10"
   sha256 "3e8e2e6a5cd75cce062f494dfe83b9c75884b17a9e23c3994143c124241644d2"
 
+  # netstorage.unity3d.com/unity/ was verified as official when first introduced to the cask
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorInstaller/Unity-#{version.before_comma}.pkg"
   appcast "https://unity3d.com/get-unity/download/archive"
   name "Unity Editor"
