7ae60a314af1eea0d8eab95b94789c4133fbeb3f
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-23 18:30:17 +0000

    Update sequel-ace from 2.1.7 to 2.1.8 (#89726)

--- Casks/sequel-ace.rb
+++ Casks/sequel-ace.rb
@@ -1,6 +1,6 @@
 cask "sequel-ace" do
-  version "2.1.7"
-  sha256 "bfbf7d55a0c2b518e9346599361aa5702c95b6a1776fa626101284d621b54e2a"
+  version "2.1.8"
+  sha256 "a3151bfa78381a08821310dc816bea59d529348b82a8411b22980fd72ce4678b"
 
   url "https://github.com/Sequel-Ace/Sequel-Ace/releases/download/#{version}/Sequel-Ace-#{version}-release.zip"
   appcast "https://github.com/Sequel-Ace/Sequel-Ace/releases.atom"
