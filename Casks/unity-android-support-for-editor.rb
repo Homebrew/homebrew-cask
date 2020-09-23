35c8c06afd1aac97074e8c5d78cc0147344244a6
suschizu <brigitte@corecode.io>
2020-09-23 19:08:16 +0200

    Update unity-android-support-for-editor.rb

--- Casks/unity-android-support-for-editor.rb
+++ Casks/unity-android-support-for-editor.rb
@@ -2,10 +2,11 @@ cask "unity-android-support-for-editor" do
   version "2020.1.6f1,fc477ca6df10"
   sha256 "21fe02a3c57e94fc4cc479fede6d509e975e7b72e7fcee8295916e14d998c9c0"
 
+  # netstorage.unity3d.com/unity/ was verified as official when first introduced to the cask
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-#{version.before_comma}.pkg"
   appcast "https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json"
   name "Unity Android Build Support"
-  homepage "https://unity3d.com/unity/"
+  homepage "https://unity.com/products"
 
   depends_on cask: "unity"
 
