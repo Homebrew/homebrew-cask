e4bec1dde0a7acbac15ba4d9351f143b7533ce67
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-23 21:02:42 +0000

    Update cmake from 3.18.2 to 3.18.3 (#89763)

--- Casks/cmake.rb
+++ Casks/cmake.rb
@@ -1,6 +1,6 @@
 cask "cmake" do
-  version "3.18.2"
-  sha256 "45960e10378c611a00d2b9e76ab88d40e308ea066b25cae694c55732912283a3"
+  version "3.18.3"
+  sha256 "7f22ec6c3aa66d25308d1c169898e1f43d458f03883586981a0eb0ee7cd73624"
 
   url "https://www.cmake.org/files/v#{version.major_minor}/cmake-#{version}-Darwin-x86_64.dmg"
   appcast "https://cmake.org/files/LatestRelease/"
