819d0fbf7e1291077bf46cb77f6cc102ba78d9d9
Miccal Matthews <miccal.matthews@gmail.com>
2020-09-23 09:13:07 +0800

    Update unity.rb

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -1,11 +1,12 @@
 cask "unity" do
   version "2020.1.6f1,fc477ca6df10"
-  sha256 "df7d34d6afc28f343f3ab6c9c7fae2c0112c90c00062215d79cfc815e67efd1f"
+  sha256 "3e8e2e6a5cd75cce062f494dfe83b9c75884b17a9e23c3994143c124241644d2"
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorInstaller/Unity-#{version.before_comma}.pkg"
   appcast "https://unity3d.com/get-unity/download/archive"
   name "Unity Editor"
-  homepage "https://unity3d.com/unity/"
+  desc "Platform for 3D content"
+  homepage "https://unity.com/products"
 
   pkg "Unity-#{version.before_comma}.pkg"
 
