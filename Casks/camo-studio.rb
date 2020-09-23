057d7c663c10db3167708babaf7dfe65539dabcb
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-23 19:49:16 +0000

    Update camo-studio from 1.0.7,71 to 1.0.8,83 (#89745)

--- Casks/camo-studio.rb
+++ Casks/camo-studio.rb
@@ -1,6 +1,6 @@
 cask "camo-studio" do
-  version "1.0.7,71"
-  sha256 "468297603f8b8f51b4d424a393671e73578323ccabebe4d5773f2533611636d8"
+  version "1.0.8,83"
+  sha256 "d932c9101ffa83075682f7c78195275f58d733da156bd8dde971f8700b3aa227"
 
   url "https://reincubate.com/res/labs/camo/Camo%20Studio%20#{version.before_comma}%20%28#{version.after_comma}%29%20%5BRelease%5D.app.zip"
   appcast "https://uds.reincubate.com/release-notes/camo/?format=sparkle"
