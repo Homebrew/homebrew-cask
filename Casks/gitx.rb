cbd3d872db455d8d491dcddd228f4276b628c4d4
Miccal Matthews <miccal.matthews@gmail.com>
2020-09-23 09:07:27 +0800

    Update gitx.rb

--- Casks/gitx.rb
+++ Casks/gitx.rb
@@ -2,8 +2,9 @@ cask "gitx" do
   version "0.16.2327"
   sha256 "c8dd6dcac8dd85808acdb937f3125bf67b2b1c2b36da5541f20de73628abf544"
 
-  url "https://github.com/gitx/gitx/releases/download/builds/#{version.major_minor}/#{version.patch}/GitX-dev-#{version.patch}.dmg"
-  appcast "https://github.com/gitx/gitx/releases.atom"
+  url "https://github.com/gitx/gitx/releases/download/build%2F#{version.major_minor}%2F#{version.patch}/GitX-dev-#{version.patch}.dmg"
+  appcast "https://github.com/gitx/gitx/releases.atom",
+          must_contain: "#{version.major_minor}%2F#{version.patch}"
   name "GitX"
   desc "GUI for git"
   homepage "https://github.com/gitx/gitx"
