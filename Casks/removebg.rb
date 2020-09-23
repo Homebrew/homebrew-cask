c4ec9d7ff43d3cea30fc9ef3d6669502f70341fe
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-23 21:31:10 +0000

    Update removebg from 1.3.0 to 1.4.0 (#89775)

--- Casks/removebg.rb
+++ Casks/removebg.rb
@@ -1,6 +1,6 @@
 cask "removebg" do
-  version "1.3.0"
-  sha256 "c10bf3b7a79dba32903628de1454fd1c884b69a3105276b79d042d56e57fbd1a"
+  version "1.4.0"
+  sha256 "5f63ed5881e147b05be59036aa8bdfcccd7a8d1953449db3d44df10277c8c760"
 
   url "https://static.remove.bg/download/removebg-for-mac-#{version}.dmg"
   appcast "https://www.remove.bg/windows-mac-linux/download"
