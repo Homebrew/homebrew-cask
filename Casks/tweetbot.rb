25f4fac87311be1d1346de3e28d7431e5b0afcda
Randall <ran.dall@icloud.com>
2020-09-23 12:04:23 -0700

    Update tweetbot from 3.4.200,4 to 3.4.300,4

--- Casks/tweetbot.rb
+++ Casks/tweetbot.rb
@@ -1,6 +1,6 @@
 cask "tweetbot" do
-  version "3.4.200,4"
-  sha256 "b8f7af9cf9d1ace4995fc01ec650ab982afc73645a0f5ca2c84d6777506d2d04"
+  version "3.4.300,4"
+  sha256 "3c4baf3e7166cfea474f6100fd663fa8d2a54074ea7fe8701a8c2bd7200cb5cd"
 
   # tapbots.net/ was verified as official when first introduced to the cask
   url "https://tapbots.net/tweetbot#{version.after_comma}/Tweetbot.#{version.before_comma.no_dots}.zip"
