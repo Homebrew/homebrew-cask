754a3b8c7df275bc7975cc96f40e5ffce8b3c549
ryenus <ryenus@gmail.com>
2020-09-24 01:24:58 -0500

    Update sapmachine-jdk from 14.0.2 to 15 (#89790)

--- Casks/sapmachine-jdk.rb
+++ Casks/sapmachine-jdk.rb
@@ -1,6 +1,6 @@
 cask "sapmachine-jdk" do
-  version "14.0.2"
-  sha256 "9c11682ba91c4285f8ca56682114a46c2e0bf723b65b81060b6f8403d681ff1f"
+  version "15"
+  sha256 "9ffd4a9b3d654d4ae4c4ba357cbef85cc97a40c334999c464d2c34cda07a0314"
 
   # github.com/SAP/SapMachine/ was verified as official when first introduced to the cask
   url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_osx-x64_bin.dmg"
