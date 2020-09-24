a5a0ea20c3b6ccecfa5ef56647308fd86f5c8397
Miccal Matthews <miccal.matthews@gmail.com>
2020-09-24 10:25:35 +0800

    Update unity-ios-support-for-editor.rb

--- Casks/unity-ios-support-for-editor.rb
+++ Casks/unity-ios-support-for-editor.rb
@@ -6,14 +6,12 @@ cask "unity-ios-support-for-editor" do
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-#{version.before_comma}.pkg"
   appcast "https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json"
   name "Unity iOS Build Support"
+  desc "iOS taget support for Unity"
   homepage "https://unity.com/products"
 
   depends_on cask: "unity"
 
   pkg "UnitySetup-iOS-Support-for-Editor-#{version.before_comma}.pkg"
 
-  uninstall pkgutil: [
-    "com.unity3d.iOSSupport",
-    "com.unity3d.UnityEditor5.x",
-  ]
+  uninstall pkgutil: "com.unity3d.iOSSupport"
 end
