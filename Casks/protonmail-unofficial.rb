2681fb89978244e9f33a075ffba49bfe702170be
suschizu <brigitte@corecode.io>
2020-09-24 12:57:17 +0200

    Update protonmail-unofficial.rb

--- Casks/protonmail-unofficial.rb
+++ Casks/protonmail-unofficial.rb
@@ -3,7 +3,7 @@ cask "protonmail-unofficial" do
   sha256 "012882c8ad6d564d96bdc9e172ee6c04a3a44ed81dc5db9c29dd033aa31cd46e"
 
   url "https://github.com/unofficial-protonmail-desktop/application/releases/download/v#{version}/Unofficial-desktop-client-for-ProtonMail-#{version}.dmg"
-  appcast "https://github.com/protonmail-desktop/application/releases.atom"
+  appcast "https://github.com/unofficial-protonmail-desktop/application/releases.atom"
   name "Protonmail Desktop"
   desc "Unofficial Electron wrapper for ProtonMail"
   homepage "https://github.com/unofficial-protonmail-desktop/application"
