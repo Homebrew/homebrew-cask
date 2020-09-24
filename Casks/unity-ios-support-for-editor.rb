08ab4d08033609a3adbe7bff962d98d88a8eb2d8
Miccal Matthews <miccal.matthews@gmail.com>
2020-09-24 13:34:24 +0800

    Update unity-ios-support-for-editor.rb

--- Casks/unity-ios-support-for-editor.rb
+++ Casks/unity-ios-support-for-editor.rb
@@ -2,8 +2,8 @@ cask "unity-ios-support-for-editor" do
   version "2020.1.6f1,fc477ca6df10"
   sha256 "43199461fb5288160f791f8abb5ffefa20fc0c33ae723acae057db7f926cd927"
 
-  # netstorage.unity3d.com/unity/ was verified as official when first introduced to the cask
-  url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-#{version.before_comma}.pkg"
+  # download.unity3d.com/download_unity/ was verified as official when first introduced to the cask
+  url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-#{version.before_comma}.pkg"
   appcast "https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json"
   name "Unity iOS Build Support"
   desc "iOS taget support for Unity"
