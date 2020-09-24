23045f36e16e7dfc3f6c2569660f344a407b9231
Chris Young <youngcm2@gmail.com>
2020-09-24 14:34:13 -0500

    Added TechSmith Capture

---
+++ Casks/techsmith-capture.rb
@@ -0,0 +1,13 @@
+cask "techsmith-capture" do
+  version "1.3.23"
+  sha256 :no_check # required as upstream package is updated in-place
+
+  url "https://assets.techsmith.com/techsmithcapture/mac/TechSmithCapture.dmg"
+  name "TechSmith Capture"
+  desc "Screen capture software"
+  homepage "https://www.techsmith.com/jing-tool.html"
+
+  depends_on macos: ">= :high_sierra"
+
+  app "TechSmith Capture.app"
+end
