5ef66e0faff402b4089810edf5dffbad3d0983e6
Miccal Matthews <miccal.matthews@gmail.com>
2020-09-24 13:36:16 +0800

    Update unity-windows-support-for-editor.rb

--- Casks/unity-windows-support-for-editor.rb
+++ Casks/unity-windows-support-for-editor.rb
@@ -2,8 +2,8 @@ cask "unity-windows-support-for-editor" do
   version "2020.1.6f1,fc477ca6df10"
   sha256 "db8e9dc12552ea3c65f73ac4711c5d122496bbfc32e628d880180f8c27b40c13"
 
-  # netstorage.unity3d.com/unity/ was verified as official when first introduced to the cask
-  url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-#{version.before_comma}.pkg"
+  # download.unity3d.com/download_unity/ was verified as official when first introduced to the cask
+  url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-#{version.before_comma}.pkg"
   appcast "https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json"
   name "Unity Windows (Mono) Build Support"
   desc "Windows (Mono) taget support for Unity"
