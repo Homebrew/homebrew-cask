da4a422cd2140651db087eab2f2773cc5090e71f
Miccal Matthews <miccal.matthews@gmail.com>
2020-09-25 03:05:26 +0800

    unity-hub 2.4.0 (#89792)

--- Casks/unity-hub.rb
+++ Casks/unity-hub.rb
@@ -1,17 +1,19 @@
 cask "unity-hub" do
-  version :latest
-  sha256 :no_check
+  version "2.4.0"
+  sha256 "0198fc9f2826bdec581c2e81e0fe028aad5f4b6f754b1617a8c99f250389e2a1"
 
   url "https://public-cdn.cloud.unity3d.com/hub/prod/UnityHubSetup.dmg"
+  appcast "https://public-cdn.cloud.unity3d.com/hub/prod/latest-mac.yml"
   name "Unity Hub"
-  homepage "https://unity3d.com/unity/beta-download"
+  desc "Management tool for Unity"
+  homepage "https://unity3d.com/get-unity/download"
 
   app "Unity Hub.app"
 
-  uninstall quit: "com.unity3d.unityhub"
+  uninstall quit:   "com.unity3d.unityhub",
+            delete: "/Applications/Unity/Hub"
 
   zap trash: [
-    "/Applications/Unity/Hub",
     "~/Library/Preferences/com.unity3d.unityhub.plist",
     "~/Library/Preferences/com.unity3d.unityhub.helper.plist",
   ]
