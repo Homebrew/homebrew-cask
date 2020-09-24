ccd2b74df8330e701357cb8265cca3d22a8851e2
Randall <ran.dall@icloud.com>
2020-09-24 14:49:55 -0700

    Update openrefine from 3.4 to 3.4.1

--- Casks/openrefine.rb
+++ Casks/openrefine.rb
@@ -1,6 +1,6 @@
 cask "openrefine" do
-  version "3.4"
-  sha256 "5310c439aae6402be713f3457ef6c369252fd45386aae633e7169c5f819cc961"
+  version "3.4.1"
+  sha256 "407078613f7cb6fad2f0731d15ed43a7dedd4311b58876198ebc52576bb89b98"
 
   # github.com/OpenRefine/OpenRefine/ was verified as official when first introduced to the cask
   url "https://github.com/OpenRefine/OpenRefine/releases/download/#{version}/openrefine-mac-#{version}.dmg"
