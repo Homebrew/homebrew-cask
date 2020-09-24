362a72f702121bf08d76dbb5237294a680b13c3f
Miccal Matthews <miccal.matthews@gmail.com>
2020-09-24 09:41:52 +0800

    Update craft.rb

--- Casks/craft.rb
+++ Casks/craft.rb
@@ -2,7 +2,7 @@ cask "craft" do
   version :latest
   sha256 :no_check
 
-  url "https://craft.do/builds/Craft.dmg"
+  url "https://www.craft.do/builds/Craft.dmg"
   name "Craft"
   desc "Document editor"
   homepage "https://craft.do/"
