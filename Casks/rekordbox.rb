2e2aa14db980b01c3c8a40dcbb601af1e9b02b5c
CoreCode <core-code@users.noreply.github.com>
2020-09-24 22:33:45 +0200

    Update rekordbox.rb

--- Casks/rekordbox.rb
+++ Casks/rekordbox.rb
@@ -3,7 +3,7 @@ cask "rekordbox" do
   sha256 "3ad7e16b5c72e7f5a0b90fd7cdce51eb744dc0fc0679c949ed07b15f33595fa2"
 
   url "https://cdn.rekordbox.com/files/#{version.after_comma}/Install_rekordbox_#{version.before_comma.dots_to_underscores}.pkg_.zip"
-  appcast "'https://rekordbox.com/en/download/"
+  appcast "https://rekordbox.com/en/download/"
   name "rekordbox"
   desc "Free Dj app to prepare and manage your music files"
   homepage "https://rekordbox.com/en/"
