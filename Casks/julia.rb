9fd2c178aa00d2bb16af83110e1888eb015ba146
Kyungdahm Yun <tomyun@gmail.com>
2020-09-24 11:49:42 -0700

    Update Julia 1.5.2 checksum (#89815)

--- Casks/julia.rb
+++ Casks/julia.rb
@@ -1,6 +1,6 @@
 cask "julia" do
   version "1.5.2"
-  sha256 "e48f020288512e61f188b3dd4260a5159202112f0773a6da3fbf0611082a0c52"
+  sha256 "e6a1eb4f4d26eef968d1e25f7bacf18a1bc1d47c2cc63c35b071825a7d96b966"
 
   url "https://julialang-s3.julialang.org/bin/mac/x64/#{version.major_minor}/julia-#{version}-mac64.dmg"
   appcast "https://github.com/JuliaLang/julia/releases.atom"
