2d225c859fb78749869c885b6c7d73d397f4624a
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-24 22:07:35 +0000

    Update vassal from 3.4.2 to 3.4.3 (#89838)

--- Casks/vassal.rb
+++ Casks/vassal.rb
@@ -1,6 +1,6 @@
 cask "vassal" do
-  version "3.4.2"
-  sha256 "899f44a2a1c892736be3f3381e44f9ea7bccb734de4072697a9b1e93dd8fc0b7"
+  version "3.4.3"
+  sha256 "2e19a56361d38f2266d5daf1bc80bb8963921664fdf27fbe306192bdcdec820e"
 
   # github.com/vassalengine/vassal was verified as official when first introduced to the cask
   url "https://github.com/vassalengine/vassal/releases/download/#{version}/VASSAL-#{version}-macosx.dmg"
