e6527acc1a9b0f915cc140f427fbc301b28e0264
Dario Vladović <d.vladimyr@gmail.com>
2020-09-24 22:19:09 +0200

    Update tuple from 0.78.2 to 0.79.0 (#89817)

--- Casks/tuple.rb
+++ Casks/tuple.rb
@@ -1,6 +1,6 @@
 cask "tuple" do
-  version "0.78.2,2020-08-07-14b75be6"
-  sha256 "f76c93c81a2314fa8487f7d2685d7828626c9cb7dea597a0e44c9296fa5255de"
+  version "0.79.0,2020-09-24-3231cd4e"
+  sha256 "2515a8519ffc582dd487521fe0bbcd59e72559ef1d6c8e7636a66ebdfeb312fc"
 
   # s3.us-east-2.amazonaws.com/tuple-releases/ was verified as official when first introduced to the cask
   url "https://s3.us-east-2.amazonaws.com/tuple-releases/production/sparkle/tuple-#{version.before_comma}-#{version.after_comma}.zip"
