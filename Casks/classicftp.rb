dd57927f4c4eeb27536ac5e9c2f1d6d1daccb04e
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2020-09-24 21:25:57 -0700

    Create classicftp.rb
    Intuitive FTP File Transfer Software to easily and securely maintain files on your website

---
+++ Casks/classicftp.rb
@@ -0,0 +1,19 @@
+cask "classicftp" do
+  version :latest
+  sha256 :no_check
+
+  url "https://www.nchsoftware.com/classic/classicmaci.zip"
+  name "ClassicFTP"
+  desc "Intuitive FTP File Transfer Software to easily and securely maintain files on your website"
+  homepage "https://www.nchsoftware.com/classic/index.html"
+
+  app "ClassicFTP.app"
+
+  zap trash: [
+    "~/Library/Application Support/ClassicFTP ",
+    "~/Library/Preferences/com.nchsoftware.classicftp.plist ",
+  ]
+end
+
+
+
