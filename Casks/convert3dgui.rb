9c5e31d63d89e2ff1e0bff0987b2760186a11e17
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2020-09-24 23:48:39 -0700

    Update convert3dgui.rb

--- Casks/convert3dgui.rb
+++ Casks/convert3dgui.rb
@@ -1,6 +1,6 @@
 cask "convert3dgui" do
   version "1.0.0"
-  sha256 "73fb4bce06f4194bba3a6bd6302ce34dfc13b939d01ea370173113a63ef04e4b"
+  sha256 "f90fc3732578e9c9378e6d9340611ab93955f8ee182af1a59edac55f8df4b728"
   
   url "https://downloads.sourceforge.net/c3d/c3d-#{version}-MacOS-x86_64.dmg"
   appcast "https://sourceforge.net/projects/c3d/rss?path=/c3d/#{version}"
