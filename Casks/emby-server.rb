40d487bf88fb05e0d04797e6ccdca4eef58a9d16
RT <58051+robbyt@users.noreply.github.com>
2020-09-24 23:48:33 -0400

    update emby-server to 4.5.0.50
    Released 4 days ago: https://github.com/MediaBrowser/Emby.Releases/releases/tag/4.5.0.50

--- Casks/emby-server.rb
+++ Casks/emby-server.rb
@@ -1,6 +1,6 @@
 cask "emby-server" do
-  version "4.4.3.0"
-  sha256 "b8d495a3f7703d020a2a9f6370fe802d6bfcc21a13c437c081a7af076ae028cd"
+  version "4.5.0.50"
+  sha256 "83dc5ec97e977b8ce194d99c62b3f33bd1f868b956c49b46ae90c06e5efe4b7f"
 
   # github.com/MediaBrowser/Emby.Releases/ was verified as official when first introduced to the cask
   url "https://github.com/MediaBrowser/Emby.Releases/releases/download/#{version}/embyserver-osx-x64-#{version}.zip"
