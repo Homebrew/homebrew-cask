5a9516e52ca09d6c4a2003a611bd763d12f8180a
Brigitte Zmoelnig <brigitte@corecode.io>
2020-09-25 16:08:02 +0200

    Update sidenotes from 1.2 to 1.2.1

--- Casks/sidenotes.rb
+++ Casks/sidenotes.rb
@@ -1,6 +1,6 @@
 cask "sidenotes" do
-  version "1.2"
-  sha256 "125272bb768c9e798a29824c4eb8d10e691a54020a5ec6b15605601b48e19288"
+  version "1.2.1"
+  sha256 "59ea38805bc91cc4b1a2b5cd5ed576f69a891ca2a1c8be7b6a4503a15e56fc31"
 
   url "https://www.apptorium.com/public/products/sidenotes/releases/SideNotes-#{version}.zip"
   appcast "https://www.apptorium.com/updates/sidenotes"
