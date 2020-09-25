a04d440eef89d519134221420b98f659d4d8458f
Chris Young <youngcm2@gmail.com>
2020-09-25 10:46:39 -0500

    Updated cask definition

--- Casks/techsmith-capture.rb
+++ Casks/techsmith-capture.rb
@@ -1,13 +1,14 @@
 cask "techsmith-capture" do
   version "1.3.23"
-  sha256 :no_check # required as upstream package is updated in-place
+  sha256 "6ac30e612b6f066debe4c1fa77533774a9857494d44c33a4b95b84e950968270"
 
   url "https://assets.techsmith.com/techsmithcapture/mac/TechSmithCapture.dmg"
+  appcast "https://assets.techsmith.com/techsmithcapture/mac/TechSmithCaptureAppcast.xml"
   name "TechSmith Capture"
   desc "Screen capture software"
   homepage "https://www.techsmith.com/jing-tool.html"
-  appcast "https://assets.techsmith.com/techsmithcapture/mac/TechSmithCaptureAppcast.xml"
-
+  
+  auto_updates true
   depends_on macos: ">= :high_sierra"
 
   app "TechSmith Capture.app"
