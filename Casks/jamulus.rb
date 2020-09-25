d7c15d67357239a8d1a0c6059a18d585b8541632
Viktor Szakats <commit@vsz.me>
2020-09-25 19:24:08 +0000

    jamulus: secure url

--- Casks/jamulus.rb
+++ Casks/jamulus.rb
@@ -2,10 +2,11 @@ cask "jamulus" do
   version "3.5.11"
   sha256 "6aed7335fc97f709e5e494d5a1ae909203026a33011dd6d88c76ce3d8dac025e"
 
+  # downloads.sourceforge.net/llcon/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/llcon/Jamulus-#{version}-installer-mac.dmg"
   appcast "https://sourceforge.net/projects/llcon/rss"
   name "Jamulus"
-  homepage "http://llcon.sourceforge.net/"
+  homepage "https://llcon.sourceforge.io/"
 
   auto_updates true
 
