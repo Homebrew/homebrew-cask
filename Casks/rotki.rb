e6cae322b8f3962dd52c870e1856bf657ee8f529
Liam Tiemon <tiemon.liam@gmail.com>
2020-09-25 12:41:55 -0400

    Add slash to end of url comment
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/rotki.rb
+++ Casks/rotki.rb
@@ -2,7 +2,7 @@ cask "rotki" do
   version "1.8.0"
   sha256 "2e4a94d8712c7d9e2ce5d2da3a7a880aa0d617992b9c06af736b86175a0613bb"
 
-  # github.com/rotki/rotki was verified as official when first introduced to the cask
+  # github.com/rotki/rotki/ was verified as official when first introduced to the cask
   url "https://github.com/rotki/rotki/releases/download/v#{version}/rotki-darwin-v#{version}.dmg"
   appcast "https://github.com/rotki/rotki/releases.atom"
   name "Rotki"
