89f946ea6c9aa46d5f07e4437e36bda4f0331b94
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2020-09-25 01:27:03 -0700

    Update tigerjython.rb

--- Casks/tigerjython.rb
+++ Casks/tigerjython.rb
@@ -2,10 +2,10 @@ cask "tigerjython" do
   version :latest
   sha256 :no_check
 
-  url "http://www.tjgroup.ch/download/TigerJython.dmg"
+  url "https://www.tjgroup.ch/download/TigerJython.dmg"
   name "TigerJython"
   desc "Jython-based educational programming environment"
-  homepage "http://www.tjgroup.ch/"
+  homepage "https://www.tjgroup.ch/"
 
   suite "TigerJython"
   zap trash: "~/Library/Caches/org.aplu.TigerJython-Mac"
