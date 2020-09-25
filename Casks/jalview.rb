3cca97232572039457069c2d1dc83fbc6d62c0b3
Randall <ran.dall@icloud.com>
2020-09-25 16:48:11 -0700

    Update jalview from 2.11.1.1 to 2.11.1.2

--- Casks/jalview.rb
+++ Casks/jalview.rb
@@ -1,6 +1,6 @@
 cask "jalview" do
-  version "2.11.1.1"
-  sha256 "7499e96b1956a3797ab4251132bc90968d3f77667f86c8e0578a0bc33a2357fc"
+  version "2.11.1.2"
+  sha256 "bfc0f6b05249c17508b189a3353190fa145f7665761cb5a08cf3e0c839400a14"
 
   url "https://www.jalview.org/getdown/release/install4j/1.8/Jalview-#{version.dots_to_underscores}-macos-java_8.dmg"
   appcast "https://www.jalview.org/getdown/release/install4j/1.8/updates.xml"
