8b46836a15416df103d35a321f61270b919524ef
Sebastian Riehm <hallo@herrriehm.de>
2020-09-25 12:05:20 +0200

    Update gobdokumente from 1.6.6 to 1.7.2

--- Casks/gobdokumente.rb
+++ Casks/gobdokumente.rb
@@ -1,6 +1,6 @@
 cask "gobdokumente" do
-  version "1.6.6"
-  sha256 "94627a8af764ff7a921e8593f7d2168d73ba4c51224a7f14d82ca076a3ba75f5"
+  version "1.7.2"
+  sha256 "2c788492fd66ffec93c70cbd0d1c10ceb03bcf2af1f8ccc5fbbbbb154e4999b1"
 
   # download.moapp.software/ was verified as official when first introduced to the cask
   url "https://download.moapp.software/GoBDokumente.zip"
