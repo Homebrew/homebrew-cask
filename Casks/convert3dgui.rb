c4c7cfeba545e4ce80fc39aa23b6b38411598cf9
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2020-09-24 23:37:49 -0700

    Update convert3dgui.rb

--- Casks/convert3dgui.rb
+++ Casks/convert3dgui.rb
@@ -2,13 +2,7 @@ cask "convert3dgui" do
   version "1.0.0"
   sha256 "73fb4bce06f4194bba3a6bd6302ce34dfc13b939d01ea370173113a63ef04e4b"
   
-  url do
-    require "open-uri"
-    project_name = "c3d"
-    filename = "c3d-#{version}-MacOS-x86_64"
-    ext = "dmg"
-    "https://master.dl.sourceforge.net/project/#{project_name}/#{project_name}/#{version}/#{filename}.#{ext}"
-  end
+  url "https://downloads.sourceforge.net/c3d/c3d-#{version}-MacOS-x86_64.dmg"
   appcast "https://sourceforge.net/projects/c3d/rss?path=/c3d/#{version}"
   name "Convert3DGUI"
   desc "Command-line tool for converting 3D images between common file formats"
