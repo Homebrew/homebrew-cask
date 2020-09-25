875331cec6725c232536537b84fe3bf79ebf7320
Randall <ran.dall@icloud.com>
2020-09-25 16:09:20 -0700

    Update craftmanager from 1.0.100 to 1.0.101

--- Casks/craftmanager.rb
+++ Casks/craftmanager.rb
@@ -1,6 +1,6 @@
 cask "craftmanager" do
-  version "1.0.100"
-  sha256 "f68be63744393960400f57914753016427dd327660b006c4d3ebaa8a9ef53078"
+  version "1.0.101"
+  sha256 "d9896be199be8af82d550359f9bb47b97724f4e67eb585db3436e0e06f7c400e"
 
   url "https://craft-assets.invisionapp.com/CraftManager/production/CraftManager.zip"
   appcast "https://craft-assets.invisionapp.com/CraftManager/production/appcast.xml"
