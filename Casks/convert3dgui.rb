ec6b67a374a55589d97987c2f9a94e06f2391c3d
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2020-09-25 01:01:17 -0700

    Update convert3dgui.rb

--- Casks/convert3dgui.rb
+++ Casks/convert3dgui.rb
@@ -9,6 +9,10 @@ cask "convert3dgui" do
   homepage "https://sourceforge.net/projects/c3d"
 
   app "Convert3DGUI.app"
+  binary "#{appdir}/Convert3DGUI.app/Contents/bin/c2d"
+  binary "#{appdir}/Convert3DGUI.app/Contents/bin/c3d"
+  binary "#{appdir}/Convert3DGUI.app/Contents/bin/c3d_affine_tool"
+  binary "#{appdir}/Convert3DGUI.app/Contents/bin/c4d"
 
   zap trash: [
     "~/Library/Saved Application State/org.itksnap.c3dgui.savedState",
