77693c3ff55edcb97c29e1097ed2d820e1803dc2
CoreCode <core-code@users.noreply.github.com>
2020-09-26 10:07:39 +0200

    Update missive from 9.38.1 to 9.39.2 (#89932)

--- Casks/missive.rb
+++ Casks/missive.rb
@@ -1,6 +1,6 @@
 cask "missive" do
-  version "9.38.1"
-  sha256 "aa10d40104a322ef119575e52ce8cf96a80f2afafca055a32ed168c3110e8e13"
+  version "9.39.2"
+  sha256 "6ad55f5e4a1f00a97e082cbcf8350cc1b1684df074deb082ef9b3ec02cbc6ac9"
 
   # d1weoh55n7oehu.cloudfront.net/ was verified as official when first introduced to the cask
   url "https://d1weoh55n7oehu.cloudfront.net/#{version}/Missive-#{version}.dmg"
