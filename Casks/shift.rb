99bd90b3833773013a14f86b719db4b439c125da
Randall <ran.dall@icloud.com>
2020-09-26 11:33:18 -0700

    Update shift from 5.0.87 to 5.0.88

--- Casks/shift.rb
+++ Casks/shift.rb
@@ -1,6 +1,6 @@
 cask "shift" do
-  version "5.0.87"
-  sha256 "3e00182e5e3b894b265e12cc6f4efa3b9150996522fce1cf27f35662fbbe2162"
+  version "5.0.88"
+  sha256 "2348d8a11e763c12e059e810e26e4a6df88c342b9164a508f57b5ca58c1520fe"
 
   url "https://update.tryshift.com/download/version/#{version}/osx_64"
   appcast "https://tryshift.com/download/?platform=mac"
