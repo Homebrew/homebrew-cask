7e19aacebb6172cda3cd43054099339bc6507d79
Randall <ran.dall@icloud.com>
2020-09-26 11:37:00 -0700

    Update musictube from 1.13.2 to 1.14

--- Casks/musictube.rb
+++ Casks/musictube.rb
@@ -1,6 +1,6 @@
 cask "musictube" do
-  version "1.13.2"
-  sha256 "ed93e0a63871bf224a661f7dade67340f40dfdacc2067913ccdfc1fe5e99357e"
+  version "1.14"
+  sha256 "572cb5e30475f16ada886cc7fe21a409c5775c9c788e1469c1a7d337f8218446"
 
   url "https://flavio.tordini.org/files/musictube/musictube.dmg"
   appcast "https://flavio.tordini.org/musictube-ws/appcast.xml"
