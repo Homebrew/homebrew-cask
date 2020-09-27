10f147b4e5164b3b935ef3db780a92618616dd81
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-27 18:37:26 +0000

    Update butt from 0.1.22 to 0.1.23 (#89982)

--- Casks/butt.rb
+++ Casks/butt.rb
@@ -1,6 +1,6 @@
 cask "butt" do
-  version "0.1.22"
-  sha256 "8ebb005fa788860500866cd23580c188f825febf5813ae96a9383c632f0cdcda"
+  version "0.1.23"
+  sha256 "d7c755b5335162ab159e30d56b2ad9b7b8aa707f09723c016bbace0f557b7498"
 
   # sourceforge.net/butt/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/butt/butt-#{version}.dmg"
