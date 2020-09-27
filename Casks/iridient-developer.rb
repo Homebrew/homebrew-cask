a344e51237076dd74fad7062b9accf5e78fd190c
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-27 04:06:24 +0000

    Update iridient-developer from 3.3.13 to 3.4.0 (#89943)

--- Casks/iridient-developer.rb
+++ Casks/iridient-developer.rb
@@ -1,10 +1,12 @@
 cask "iridient-developer" do
-  version "3.3.13"
-  sha256 "cc7015a73a41c5209643d5a8ccf06748e5e46e640b0dc889564f4d9c2a5202c0"
+  version "3.4.0"
+  sha256 "3eafbea911ea8e69f5910ff49f122843855cebb57f1ef5c8e301bedb01f8c3d2"
 
   url "https://www.iridientdigital.com/downloads/IridientDeveloper_#{version.no_dots}.dmg"
-  appcast "https://www.iridientdigital.com/products/rawdeveloper_history.html"
+  appcast "https://www.iridientdigital.com/products/rawdeveloper_history.html",
+          must_contain: version.major_minor
   name "Iridient Developer"
+  desc "Image processing application"
   homepage "https://www.iridientdigital.com/"
 
   app "Iridient Developer.app"
