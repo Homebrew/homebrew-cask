1056d42498a9faf0521d3b8d0a85896a41b9d538
Julian Mayer <julian@corecode.io>
2020-09-27 17:50:10 +0200

    Update sf-symbols from latest to 2.0

--- Casks/sf-symbols.rb
+++ Casks/sf-symbols.rb
@@ -1,8 +1,14 @@
 cask "sf-symbols" do
-  version :latest
-  sha256 :no_check
+  if MacOS.version <= :mojave
+    version "1.1"
+    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    url "https://devimages-cdn.apple.com/design/resources/download/SF-Symbols.dmg"
+  else
+    version "2.0"
+    sha256 "d6071715cc4e5ad46877fd34d7099f53cedeba190667b510927f5775f3f48d4a"
+    url "https://devimages-cdn.apple.com/design/resources/download/SF-Symbols-2.dmg"
+  end
 
-  url "https://developer.apple.com/design/downloads/SF-Symbols.dmg"
   name "SF Symbols"
   desc "Tool that provides consistent, highly configurable symbols for apps"
   homepage "https://developer.apple.com/design/human-interface-guidelines/sf-symbols/overview/"
