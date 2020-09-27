a157707671a36200f5a3295dea260c8839beccaf
Randall <ran.dall@icloud.com>
2020-09-27 11:23:24 -0700

    Update dropshare from 5.6.3,5148 to 5.7,5158

--- Casks/dropshare.rb
+++ Casks/dropshare.rb
@@ -1,6 +1,6 @@
 cask "dropshare" do
-  version "5.6.3,5148"
-  sha256 "0e44e216dbbd8b9f25c5706a558ea361733d057960ec994eca953194122bf55a"
+  version "5.7,5158"
+  sha256 "6cfddefdc30ce25ba9ee00ed12e4baa50950e45c9fe2f7e9ce649b4ca63bbfb8"
 
   # d2wvuuix8c9e48.cloudfront.net/ was verified as official when first introduced to the cask
   url "https://d2wvuuix8c9e48.cloudfront.net/Dropshare#{version.major}-#{version.after_comma}.app.zip"
