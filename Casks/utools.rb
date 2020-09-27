6d5ed338c703c947d3476ff280b9ba43dc7f3b40
Markus Reiter <me@reitermark.us>
2020-09-27 06:26:11 +0200

    Update utools.rb

--- Casks/utools.rb
+++ Casks/utools.rb
@@ -2,9 +2,9 @@ cask "utools" do
   version "1.3.2"
   sha256 "c4297e8b5a65734c4a3fcd8ed3f10e6adacb97edc9745242c4306e5712787fae"
 
-  # resource.u-tools.cn/ was verified as official when first introduced to the cask
-  url "https://resource.u-tools.cn/currentversion/uTools-#{version}.dmg"
-  appcast "https://resource.u-tools.cn/currentversion/latest-mac.yml"
+  # res.u-tools.cn/ was verified as official when first introduced to the cask
+  url "https://res.u-tools.cn/currentversion/uTools-#{version}.dmg"
+  appcast "https://res.u-tools.cn/currentversion/public-mac.yml"
   name "uTools"
   homepage "https://u.tools/index.html"
 
