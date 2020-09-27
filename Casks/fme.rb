0201964766f4a37a42a0dc7c8c66f99aba28e310
Miccal Matthews <miccal.matthews@gmail.com>
2020-09-27 11:45:07 +0800

    Update fme.rb

--- Casks/fme.rb
+++ Casks/fme.rb
@@ -1,10 +1,11 @@
 cask "fme" do
   version "2020.1.2.1-b20624"
-  sha256 "099e7834f6e8b5724d532fdfd3e9aef9d5495842fe83dbd248d6862af90b6f6d"
+  sha256 "4106b25ed0c893ba1e364b1ea8affff4f196d268b5a8cba1513eb006b2e3b33c"
 
   url "https://downloads.safe.com/fme/#{version.major}/fme-desktop-#{version}-macosx.dmg"
   appcast "https://www.macupdater.net/cgi-bin/extract_text/extract_text_split_easy.cgi?encoding=utf8&url=https://www.safe.com/api/downloads/&splitter_1=beta&index_1=0"
   name "FME Desktop"
+  desc "Platform for integrating spatial data"
   homepage "https://www.safe.com/"
 
   installer script: {
