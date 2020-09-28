c27f27580a67897f375691825357f6143d2d0925
Miccal Matthews <miccal.matthews@gmail.com>
2020-09-29 06:22:54 +0800

    Update icab.rb

--- Casks/icab.rb
+++ Casks/icab.rb
@@ -1,13 +1,16 @@
 cask "icab" do
-  version "6.0.0"
-  sha256 "c4494c5a944b9e03ccfc6e297bcd6bc971ddb3053a40bae5bca706f444f29710"
+  version "6.0.1"
+  sha256 "417ad1716c5d7d698d04e58c0886685f10c7571772cddf64e314b66e907311b6"
 
   # icab.clauss-net.de/ was verified as official when first introduced to the cask
-  url "https://icab.clauss-net.de/icab/iCab_#{version}_Intel.zip"
-  appcast "http://www.icab.de/dl.php"
+  url "http://icab.clauss-net.de/icab/iCab_#{version}.zip"
+  appcast "http://www.icab.de/download.html",
+          must_contain: version.major_minor
   name "iCab"
   desc "Alternative web browser"
   homepage "http://www.icab.de/"
 
-  app "iCab #{version} (64 Bit Intel for macOS 10.9-10.15)/iCab.app"
+  depends_on macos: ">= :high_sierra"
+
+  app "iCab #{version}/iCab.app"
 end
