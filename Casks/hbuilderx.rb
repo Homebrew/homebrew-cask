13b706dfb1bf9ad71dbc7653f25d4744b85d6254
Brigitte Zmoelnig <brigitte@corecode.io>
2020-09-28 17:27:20 +0200

    Update hbuilderx from 2.8.12.20200926 to 2.8.13.20200927

--- Casks/hbuilderx.rb
+++ Casks/hbuilderx.rb
@@ -1,6 +1,6 @@
 cask "hbuilderx" do
-  version "2.8.12.20200926"
-  sha256 "47ed3dbef1c9b08c145d6c99be425e2edf7fd3363d65b56441a601f35b5a01fc"
+  version "2.8.13.20200927"
+  sha256 "99c430598a4f909d900e4a7079d60c40686b724d7229feae904e2568395c9a24"
 
   # download1.dcloud.net.cn/ was verified as official when first introduced to the cask
   url "https://download1.dcloud.net.cn/download/HBuilderX.#{version}.dmg"
