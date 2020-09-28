de7d186fd3e6b83520e81ba368ee9efc77555713
Julian Mayer <julian@corecode.io>
2020-09-28 09:34:28 +0200

    Update butt

--- Casks/butt.rb
+++ Casks/butt.rb
@@ -1,6 +1,6 @@
 cask "butt" do
   version "0.1.23"
-  sha256 "d7c755b5335162ab159e30d56b2ad9b7b8aa707f09723c016bbace0f557b7498"
+  sha256 "ad17ecb463249767132c6721de96c0a196e32f3570966c0ec5b8a9ca83873980"
 
   # sourceforge.net/butt/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/butt/butt-#{version}.dmg"
