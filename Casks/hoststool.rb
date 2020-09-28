57af5123d6c01a4a346779c0cb425213a96039fd
Miccal Matthews <miccal.matthews@gmail.com>
2020-09-28 08:55:33 +0800

    Update hoststool.rb

--- Casks/hoststool.rb
+++ Casks/hoststool.rb
@@ -1,11 +1,11 @@
 cask "hoststool" do
-  version "2.6.0"
-  sha256 "8d7aeaf226b475900821a6fddf6e50ceec39417e4b4fc056e9fda3ad8f0058a6"
+  version "2.6.1"
+  sha256 "c9c3c6769ae922c28e49634fecd4d97aafe6ea2d40cf16f4812552905e1fa7a6"
 
   url "https://github.com/ZzzM/HostsToolforMac/releases/download/#{version}/HostsToolForMac.zip"
   appcast "https://github.com/ZzzM/HostsToolforMac/releases.atom"
   name "HostsToolForMac.zip"
-  desc "Update Your Hosts by One Click"
+  desc "local hosts update tool"
   homepage "https://github.com/ZzzM/HostsToolforMac/"
 
   depends_on macos: ">= :sierra"
