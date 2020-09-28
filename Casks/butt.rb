52b7305397926524060babeb4b123d772b635b26
CoreCode <core-code@users.noreply.github.com>
2020-09-28 14:07:46 +0200

    Update butt (#89988)

--- Casks/butt.rb
+++ Casks/butt.rb
@@ -1,12 +1,13 @@
 cask "butt" do
   version "0.1.23"
-  sha256 "d7c755b5335162ab159e30d56b2ad9b7b8aa707f09723c016bbace0f557b7498"
+  sha256 "ad17ecb463249767132c6721de96c0a196e32f3570966c0ec5b8a9ca83873980"
 
   # sourceforge.net/butt/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/butt/butt-#{version}.dmg"
   appcast "https://sourceforge.net/projects/butt/rss"
   name "Broadcast Using This Tool"
-  homepage "https://danielnoethen.de/"
+  desc "Shoutcast and Icecast streaming client"
+  homepage "https://danielnoethen.de/butt/"
 
   app "butt.app"
 end
