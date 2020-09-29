30da1780d1e5cd30eb35e597de34e40a44b59d1d
Miccal Matthews <miccal.matthews@gmail.com>
2020-09-30 06:17:12 +0800

    Update happymac.rb

--- Casks/happymac.rb
+++ Casks/happymac.rb
@@ -1,15 +1,14 @@
 cask "happymac" do
-  version "0.1.0"
-  sha256 "a3f5f4fda00e8bffcd81450f2978dd720b36bfccf7b6f1c204ace2bed41d6ed8"
+  version :latest
+  sha256 :no_check
 
-  # chrislaffra.com was verified as official when first introduced to the cask
+  # chrislaffra.com/ was verified as official when first introduced to the cask
   url "https://chrislaffra.com/happymac/happymac.dmg"
   name "HappyMac"
   desc "Watches, suspends and resumes background processes that slow down your system"
   homepage "https://www.happymac.app/"
 
-  auto_updates false
   depends_on macos: ">= :sierra"
 
-  app "HappyMac.app"
+  app "happymac.app"
 end
