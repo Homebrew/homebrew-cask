2d185d885ba49cf76f6d4437020f987d0fadcff9
Randall <ran.dall@icloud.com>
2020-09-29 11:07:31 -0700

    Update proxyman from 2.7.0 to 2.8.0

--- Casks/proxyman.rb
+++ Casks/proxyman.rb
@@ -1,6 +1,6 @@
 cask "proxyman" do
-  version "2.7.0"
-  sha256 "6b683ae0d632adb07e5d1e1a25dae3fd8116cd069a60b7840451838604b436be"
+  version "2.8.0"
+  sha256 "6ce5c448b32e3ed8a8d59e61c627b1db6994dd8bffdf2281c4ab04cacf20d241"
 
   # github.com/ProxymanApp/Proxyman/ was verified as official when first introduced to the cask
   url "https://github.com/ProxymanApp/Proxyman/releases/download/#{version}/Proxyman_#{version}.dmg"
