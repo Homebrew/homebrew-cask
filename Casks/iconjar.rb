a6dd805a87d714584ac0fde1f00f4d50e5780f83
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-29 22:41:45 +0000

    Update iconjar from 2.5.0,42895 to 2.6.0,45038 (#90053)

--- Casks/iconjar.rb
+++ Casks/iconjar.rb
@@ -1,6 +1,6 @@
 cask "iconjar" do
-  version "2.5.0,42895"
-  sha256 "d48371cd1f3b75805f7004c35b167a67c173f00c1bf0842d6637851203125757"
+  version "2.6.0,45038"
+  sha256 "6b69aa5a1bcda473ef92db9621b6c10252944e1815770418f41522eb14b74665"
 
   url "https://geticonjar.com/releases/IconJar.app.#{version.after_comma}.zip"
   appcast "https://geticonjar.com/releases/stable.xml"
