04213112593e3d239b0fda446e603b124e3dd7e6
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-29 22:43:06 +0000

    Update hammerspoon from 0.9.80 to 0.9.81 (#90044)

--- Casks/hammerspoon.rb
+++ Casks/hammerspoon.rb
@@ -1,6 +1,6 @@
 cask "hammerspoon" do
-  version "0.9.80"
-  sha256 "f0372ad2e17be816bad527ff194cdd55d4d15f72d418f65166c6fd7f94129c67"
+  version "0.9.81"
+  sha256 "b6b92735e64ae09709804cc3296212529d8669235a035d7a42b77e2e7a37fe29"
 
   # github.com/Hammerspoon/hammerspoon/ was verified as official when first introduced to the cask
   url "https://github.com/Hammerspoon/hammerspoon/releases/download/#{version}/Hammerspoon-#{version}.zip"
