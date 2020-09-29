3e9f4fe2cea2870ac1469c4ea60e7460936c0c4d
Randall <ran.dall@icloud.com>
2020-09-29 11:47:31 -0700

    Update tinkerwell from 2.9.0 to 2.10.1

--- Casks/tinkerwell.rb
+++ Casks/tinkerwell.rb
@@ -1,6 +1,6 @@
 cask "tinkerwell" do
-  version "2.9.0"
-  sha256 "924a68dccc9911e303a06c01d59a99b43f0241e49955371375f8d11023e32810"
+  version "2.10.1"
+  sha256 "9cec9e564e550615d73884e85038892e52e934249b2202e029e169eb75f5bc3c"
 
   # tinkerwell.fra1.digitaloceanspaces.com/ was verified as official when first introduced to the cask
   url "https://tinkerwell.fra1.digitaloceanspaces.com/tinkerwell/Tinkerwell-#{version}.dmg"
