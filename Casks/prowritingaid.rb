9915dff18a4fc4ca938928f1937cebde1104b55e
CoreCode <core-code@users.noreply.github.com>
2020-09-29 16:46:15 +0200

    Update prowritingaid.rb

--- Casks/prowritingaid.rb
+++ Casks/prowritingaid.rb
@@ -3,7 +3,8 @@ cask "prowritingaid" do
   sha256 "c6430ef93c66f0f9d539802b722325448375687ffa0eba96fafc690ef3d14e9a"
 
   url "https://cdn.prowritingaid.com/prowritingaid-desktop/desktop_#{version.dots_to_underscores}/ProWritingAid.dmg"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_sizeandmodified.cgi?url=https://cdn.prowritingaid.com/prowritingaid-desktop/ProWritingAid.dmg"
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_sizeandmodified.cgi?url=https://cdn.prowritingaid.com/prowritingaid-desktop/ProWritingAid.dmg",
+          must_contain: ''
   name "ProWritingAid"
   homepage "https://prowritingaid.com/"
 
