7d92f1ca0050e6295b6425cf4532f3c13c691be2
Randall <ran.dall@icloud.com>
2020-09-29 11:50:32 -0700

    Update electron from 10.1.2 to 10.1.3

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,6 +1,6 @@
 cask "electron" do
-  version "10.1.2"
-  sha256 "2e2696cb187a65330276ec8110537bba9f0a10fc9342dd958ea1b9c37d5aab4c"
+  version "10.1.3"
+  sha256 "7ad9c81ce05307d9f17b68ad38515d0d1e8a1ae81727b5a4edbc5c7a7f4503d9"
 
   # github.com/electron/electron/ was verified as official when first introduced to the cask
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
