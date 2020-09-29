62345a4e798a6f30aeb1d98f6ceff394b564989e
Brigitte Zmoelnig <brigitte@corecode.io>
2020-09-29 17:10:36 +0200

    Update publii from 0.37.0 to 0.37.1

--- Casks/publii.rb
+++ Casks/publii.rb
@@ -1,6 +1,6 @@
 cask "publii" do
-  version "0.37.0"
-  sha256 "dbb9a018274c71ec0e455e405a4fcae1cdda57c823d04d884fab60fb7d0a0949"
+  version "0.37.1"
+  sha256 "1b3398cdaf15aefa2fd0f8db47d3aaf61a1e2037b7d7994ed95a43a3d680689f"
 
   url "https://cdn.getpublii.com/Publii-#{version}.dmg"
   appcast "https://getpublii.com/download/"
