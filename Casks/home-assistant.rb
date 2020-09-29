f62a563c6d87ce49c52f26a5a1acc92642a53c97
Miccal Matthews <miccal.matthews@gmail.com>
2020-09-29 09:41:31 +0800

    Update home-assistant.rb

--- Casks/home-assistant.rb
+++ Casks/home-assistant.rb
@@ -3,7 +3,7 @@ cask "home-assistant" do
   sha256 "9117bd94e6e72963a12073c349d7975b71b4b07e4534eb4c9be2532447522762"
 
   # github.com/home-assistant/iOS/ was verified as official when first introduced to the cask
-  url "https://github.com/home-assistant/iOS/releases/download/mac/#{version.before_comma}/#{version.after_comma}/home-assistant-mac.zip"
+  url "https://github.com/home-assistant/iOS/releases/download/release%2F#{version.before_comma}%2F#{version.after_comma}/home-assistant-mac.zip"
   appcast "https://github.com/home-assistant/iOS/releases.atom"
   name "Home Assistant"
   desc "Companion app for Home Assistant home automation software"
