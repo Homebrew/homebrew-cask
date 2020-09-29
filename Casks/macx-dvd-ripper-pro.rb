b535f74ac9b1dd88ad79042748067f84cb905e13
CoreCode <core-code@users.noreply.github.com>
2020-09-29 21:59:23 +0200

    Update macx-dvd-ripper-pro.rb

--- Casks/macx-dvd-ripper-pro.rb
+++ Casks/macx-dvd-ripper-pro.rb
@@ -3,7 +3,7 @@ cask "macx-dvd-ripper-pro" do
   sha256 "049e143d60b42b6e77b590749a9d258d9a005a7e182ee155a35e419080f6dfa0"
 
   url "https://www.macxdvd.com/download/macx-dvd-ripper-pro.dmg"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_filesize.cgi?url=https://www.macxdvd.com/download/macx-dvd-ripper-pro.dmg"
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_filesize.cgi?url=https://www.macxdvd.com/download/macx-dvd-ripper-pro.dmg",
           must_contain: ""
   name "MacX DVD Ripper Pro"
   desc "DVD ripping application"
