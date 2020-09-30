2d52087c7c88092cfb8e4fa01e3c01879d1a2e3c
Reno <25192197+singularitti@users.noreply.github.com>
2020-09-29 20:53:10 -0400

    Update nteract from 0.25.0 to 0.25.1 (#90089)

--- Casks/nteract.rb
+++ Casks/nteract.rb
@@ -1,6 +1,6 @@
 cask "nteract" do
-  version "0.25.0"
-  sha256 "7e27a84a43254fa2f798a803c0316eaff4c7f2fdc670f768923fff6d4adaecbe"
+  version "0.25.1"
+  sha256 "553c7e9d1ef9f27c920000498fb0b388a6168a2e708490797099e71fe37e2764"
 
   url "https://github.com/nteract/nteract/releases/download/v#{version}/nteract-#{version}.dmg"
   appcast "https://github.com/nteract/nteract/releases.atom"
