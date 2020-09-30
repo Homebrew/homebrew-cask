2be0597126a702d23703f9ef43193079ce8fc37a
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-30 20:38:05 +0000

    Update visicut from 1.9-70-g2c0db535 to 1.9-80-g65b7f094 (#90148)

--- Casks/visicut.rb
+++ Casks/visicut.rb
@@ -1,6 +1,6 @@
 cask "visicut" do
-  version "1.9-70-g2c0db535"
-  sha256 "7549aec494c173d94b8210ed5ac49a779d62e99cbe556a3101490a717b987dda"
+  version "1.9-80-g65b7f094"
+  sha256 "26247e3e785a491b64ab186e48ba9f8734c2a5c2d847068dcd78d826e48420e8"
 
   url "https://download.visicut.org/files/master/MacOSX/VisiCutMac-#{version}.zip"
   appcast "https://download.visicut.org"
