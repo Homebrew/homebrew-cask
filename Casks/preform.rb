1ecbaf74cbdade22072d43ced2788fd6da50bd36
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-01 05:50:43 +0800

    Update preform.rb

--- Casks/preform.rb
+++ Casks/preform.rb
@@ -6,6 +6,7 @@ cask "preform" do
   url "https://s3.amazonaws.com/FormlabsReleases/Release/#{version.before_comma}/PreForm_#{version.before_comma}_release_origin_release_#{version.before_comma}_build_#{version.after_comma}.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://formlabs.com/download-preform-mac/"
   name "PreForm"
+  desc "3D printing setup, management, and monitoring"
   homepage "https://formlabs.com/tools/preform/"
 
   app "PreForm.app"
