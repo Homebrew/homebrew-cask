6f14022b1dc0fb9ad68096da528e10970feb82be
suschizu <brigitte@corecode.io>
2020-10-01 01:18:21 +0200

    Update macpilot (#90150)
    * Update macpilot
    
    * Update macpilot.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/macpilot.rb
+++ Casks/macpilot.rb
@@ -3,9 +3,14 @@ cask "macpilot" do
     version "11.1.3"
     sha256 "1990b04414896ef24767e58cd9b56901460375ee8fb805572ca34da019bcda58"
 
-    url "https://www.koingosw.com/products/macpilot/download/old/macpilot_11.1.3_intel_for_1013to1015.dmg"
-  else
+    url "https://www.koingosw.com/products/macpilot/download/old/macpilot_#{version}_intel_for_1013to1015.dmg"
+  elsif MacOS.version <= :catalina
     version "11.1.4"
+    sha256 "75fc421d51ebd172ebdf149400fbcd010cf1224529e2e7d3fdf1915e62757f64"
+
+    url "https://www.koingosw.com/products/macpilot/download/old/macpilot_#{version.no_dots}_intel_for_1015.dmg"
+  else
+    version "12.0"
     sha256 "75fc421d51ebd172ebdf149400fbcd010cf1224529e2e7d3fdf1915e62757f64"
 
     url "https://www.koingosw.com/products/macpilot/download/macpilot.dmg"
