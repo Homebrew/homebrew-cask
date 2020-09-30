f5ba5ba7759e47c99c66132ea182d7787ce88084
Steven Conaway <steven.conaway@icloud.com>
2020-09-30 16:02:30 -0700

    flipper: v0.60.0 (#90104)
    * flipper: v0.60.0
    
    * Remove erroneous name field
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/flipper.rb
+++ Casks/flipper.rb
@@ -1,12 +1,11 @@
 cask "flipper" do
-  version "0.59.0"
-  sha256 "5ff9bf31b3c320ee5aedbb97c1438da56901e89fcc3b7550a093feb5410053fc"
+  version "0.60.0"
+  sha256 "8e6b3902bdf7479d6a7c2e60574b2bc7b742caaa0ce27b2fcb866c6516428ab2"
 
   # github.com/facebook/flipper/ was verified as official when first introduced to the cask
   url "https://github.com/facebook/flipper/releases/download/v#{version}/Flipper-mac.zip"
   appcast "https://github.com/facebook/flipper/releases.atom"
   name "Facebook Flipper"
-  name "Sonar"
   desc "Desktop debugging platform for mobile developers"
   homepage "https://fbflipper.com/"
 
