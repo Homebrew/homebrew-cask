adb34b31a23fd117b3d8f284a23df9db0f863d6a
Julian Mayer <julian@corecode.io>
2020-09-30 10:47:38 +0200

    Update acquia-dev from 2.2020.08.10 to 2.2020.09.23

--- Casks/acquia-dev.rb
+++ Casks/acquia-dev.rb
@@ -1,6 +1,6 @@
 cask "acquia-dev" do
-  version "2.2020.08.10"
-  sha256 "6fe612c9f12d0ebe3b8383fd3b86cf27df3728ebb4790bb2f2f6c3c40b044cf0"
+  version "2.2020.09.23"
+  sha256 "3fda351ecda5d19ecaccdd3f3b8fa4930afe84943a14f9cdbd2aed62c25c5495"
 
   url "https://dev.acquia.com/sites/default/files/file/#{version.minor_patch.dots_to_hyphens}/AcquiaDevDesktop-#{version.dots_to_hyphens}.dmg"
   appcast "https://dev.acquia.com/downloads",
