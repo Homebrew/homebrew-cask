31c0263e2c299a9723342a45912bf144189e2a02
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-30 18:33:10 +0000

    Update techsmith-capture from 1.3.23 to 1.3.25 (#90118)

--- Casks/techsmith-capture.rb
+++ Casks/techsmith-capture.rb
@@ -1,6 +1,6 @@
 cask "techsmith-capture" do
-  version "1.3.23"
-  sha256 "6ac30e612b6f066debe4c1fa77533774a9857494d44c33a4b95b84e950968270"
+  version "1.3.25"
+  sha256 "8999a3e51bd17f33d6c87e6189468674b5a706860cabc9fbc7456e048d5cb6bb"
 
   url "https://assets.techsmith.com/techsmithcapture/mac/TechSmithCapture.dmg"
   appcast "https://assets.techsmith.com/techsmithcapture/mac/TechSmithCaptureAppcast.xml"
