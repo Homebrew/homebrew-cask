4c5e31c4ad20bfc0feca469b0b9df7c823c56274
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-30 18:16:26 +0000

    Update keysmith from 1.0.10 to 1.1.2 (#90116)

--- Casks/keysmith.rb
+++ Casks/keysmith.rb
@@ -1,6 +1,6 @@
 cask "keysmith" do
-  version "1.0.10"
-  sha256 "6c6315f91a6ff04dc2968d9df705eb2d17d07383cca6abbbaf775a6ae58709ca"
+  version "1.1.2"
+  sha256 "816e534e6d619a81612b6671e483f1536d92959d127dbb0c81a4cd6835fc4147"
 
   url "https://keysmith.app/versions/Keysmith-#{version}.dmg"
   appcast "https://www.keysmith.app/versions/appcast.xml"
