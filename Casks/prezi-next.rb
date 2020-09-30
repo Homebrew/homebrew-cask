2c6b81e7cc78c174f8ddd0dd76769e790c318925
Randall <ran.dall@icloud.com>
2020-09-30 11:03:55 -0700

    Update prezi-next from 1.46.0 to 1.47.0

--- Casks/prezi-next.rb
+++ Casks/prezi-next.rb
@@ -1,6 +1,6 @@
 cask "prezi-next" do
-  version "1.46.0"
-  sha256 "1b151c5e7e0eb7a97328261f9ae19ed72b02641ad5ce6c8e2cabd1dab3ccd3fe"
+  version "1.47.0"
+  sha256 "8c01cdf5860314d2252f4a7f7a5bea292da283cc29626bfd219a4bd2b91572ff"
 
   url "https://desktopassets.prezi.com/mac/pitch/releases/Prezi_Next_#{version}.dmg"
   appcast "https://prezidesktop.s3.amazonaws.com/assets/mac/pitch/updates/prezi-business.xml"
