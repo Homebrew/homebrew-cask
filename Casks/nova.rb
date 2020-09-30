4578f38b0c1fbbf468305c29e347bfa4eee78430
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-30 19:39:04 +0000

    Update nova from 1.1 to 1.2 (#90136)

--- Casks/nova.rb
+++ Casks/nova.rb
@@ -1,6 +1,6 @@
 cask "nova" do
-  version "1.1"
-  sha256 "9edacf86441d704bce464637bc0d3e2c5018baa4ef2b13b4f78f2a072752166c"
+  version "1.2"
+  sha256 "1a657420ebd9db499845a679e99ab1f531147bb2d14e726d78db0798553fe378"
 
   # panic-c157.kxcdn.com/ was verified as official when first introduced to the cask
   url "https://panic-c157.kxcdn.com/nova/Nova%20#{version}.zip"
