d42f656b0066cc5b206a1957084c8e75f1e6e230
Randall <ran.dall@icloud.com>
2020-10-01 10:11:43 -0700

    Update dosbox-x from 0.83.5,20200901011555 to 0.83.6,20200930235614

--- Casks/dosbox-x.rb
+++ Casks/dosbox-x.rb
@@ -1,6 +1,6 @@
 cask "dosbox-x" do
-  version "0.83.5,20200901011555"
-  sha256 "723454e056ea611cec9fd289af318b719b551d6c728a338d9af0db09f2260772"
+  version "0.83.6,20200930235614"
+  sha256 "acdbc504a38b8cdec89e53c41a26f3735b7467f09a993cdcf1d0243ad0778a30"
 
   # github.com/joncampbell123/dosbox-x/ was verified as official when first introduced to the cask
   url "https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v#{version.before_comma}/dosbox-x-macosx-x64-#{version.after_comma}.zip"
