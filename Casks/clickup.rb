29b594a1eab5fc2137e476200274abbb92067cb5
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-01 23:06:56 +0000

    Update clickup from 2.0.18 to 2.0.19 (#90204)

--- Casks/clickup.rb
+++ Casks/clickup.rb
@@ -3,7 +3,9 @@ cask "clickup" do
   sha256 "974cd39d03605f35f9300e86339961093adc631bf85774ccbe9924965dd5eacf"
 
   url "https://attachments.clickup.com/desktop/clickup-desktop-#{version}-mac.dmg"
+  appcast "https://github.com/clickup/clickup-release/releases.atom"
   name "ClickUp"
+  desc "Productivity platform for tasks, docs, goals, and chat"
   homepage "https://clickup.com/"
 
   app "ClickUp.app"
