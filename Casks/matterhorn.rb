1b90acbfb8da46f00de49c8929b882040a325ec5
Jonathan Daugherty <cygnus@foobox.com>
2020-10-01 14:54:10 -0700

    Casks/matterhorn: resolve issues

--- Casks/matterhorn.rb
+++ Casks/matterhorn.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 cask "matterhorn" do
   version "50200.10.2"
   sha256 "cd54f4758eafe8babf58534d3fb0bfde7e01c930cf1539bbe4500b783a73710c"
@@ -5,7 +7,7 @@ cask "matterhorn" do
   url "https://github.com/matterhorn-chat/matterhorn/releases/download/#{version}/matterhorn-#{version}-Darwin-x86_64.tar.bz2"
   appcast "https://github.com/matterhorn-chat/matterhorn/releases.atom"
   name "Matterhorn"
-  desc "A Unix terminal client for Mattermost"
+  desc "Unix terminal client for Mattermost"
   homepage "https://github.com/matterhorn-chat/matterhorn"
 
   binary "matterhorn-#{version}-Darwin-x86_64/matterhorn"
