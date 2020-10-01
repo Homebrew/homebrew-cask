56a1195c6f673995f258955114d968c279e5e9ca
Phuc Nguyen Minh <minhphuc429@gmail.com>
2020-10-01 11:02:03 +0700

    Update telegram from 7.0.1,205092 to 7.1,206103 (#90160)

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,6 +1,6 @@
 cask "telegram" do
-  version "7.0.1,205092"
-  sha256 "52a3d486a51dbcbaa052ed40db4134634ac31f1717832172f138aee38f31305d"
+  version "7.1,206103"
+  sha256 "c7596bdc89518b6f26eb4a0450ea45efb8bd6de346ebd159bf12003249daf60d"
 
   url "https://osx.telegram.org/updates/Telegram-#{version.before_comma}.#{version.after_comma}.app.zip"
   appcast "https://osx.telegram.org/updates/versions.xml"
