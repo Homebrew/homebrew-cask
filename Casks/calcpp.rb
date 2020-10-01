f2305e069598049f47d11279cf233d79bc7486e0
Antonio Kim <contact@antoniojkim.com>
2020-09-30 21:36:27 -0400

    Adding calcpp cask
    CalcPlusPlus (a.k.a. calcpp) is a highly performant, robust, easy to use
    scientific calculator.
    
    Docs: https://calcplusplus.readthedocs.io/en/latest/

---
+++ Casks/calcpp.rb
@@ -0,0 +1,15 @@
+cask "calcpp" do
+  version "0.4"
+  sha256 "f75d95f24e019a5cde0e3d8c0ff5cad93bd07524513dca19cf8b986e6278aa7f"
+
+  url "https://github.com/antoniojkim/CalcPlusPlus/releases/download/v0.4/calcplusplus.bottle.tgz"
+  appcast "https://github.com/antonim/CalcPlusPlus/releases.atom"
+  name "CalcPlusPlus"
+  desc "Advanced Scientific Calculator. Highly performant, robust, and easy to use"
+  homepage "https://github.com/antoniojkim/CalcPlusPlus"
+
+  depends_on formula: "gsl"
+  depends_on formula: "qt"
+
+  app "CalcPlusPlus/calcpp.app"
+end
