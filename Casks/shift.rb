01daa25265e3299af31d064022f79df0f7af490b
Randall <ran.dall@icloud.com>
2020-10-01 10:41:07 -0700

    Update shift from 5.0.88 to 5.0.89

--- Casks/shift.rb
+++ Casks/shift.rb
@@ -1,5 +1,5 @@
 cask "shift" do
-  version "5.0.88"
+  version "5.0.89"
   sha256 "2348d8a11e763c12e059e810e26e4a6df88c342b9164a508f57b5ca58c1520fe"
 
   url "https://update.tryshift.com/download/version/#{version}/osx_64"
