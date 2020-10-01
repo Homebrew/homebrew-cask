ae2d9891225f82cc086f964622b44048e81cd762
miccal <miccal.matthews@gmail.com>
2020-10-01 08:31:27 +0800

    prowritingaid: update must_contain, add desc

--- Casks/prowritingaid.rb
+++ Casks/prowritingaid.rb
@@ -3,9 +3,9 @@ cask "prowritingaid" do
   sha256 "43f30a5a7135026f36849c6b21b1321a82684d189cf7494ca4cff6c3e53805ee"
 
   url "https://cdn.prowritingaid.com/prowritingaid-desktop/desktop_#{version.dots_to_underscores}/ProWritingAid.dmg"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_sizeandmodified.cgi?url=https://cdn.prowritingaid.com/prowritingaid-desktop/ProWritingAid.dmg",
-          must_contain: ""
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_sizeandmodified.cgi?url=https://cdn.prowritingaid.com/prowritingaid-desktop/ProWritingAid.dmg"
   name "ProWritingAid"
+  desc "Grammar checker, style editor, and writing mentor"
   homepage "https://prowritingaid.com/"
 
   app "ProWritingAid.app"
