0728e1036c47fd5337e83873820006bc289637ca
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-02 05:43:44 +0800

    Update slicer.rb

--- Casks/slicer.rb
+++ Casks/slicer.rb
@@ -1,11 +1,12 @@
 cask "slicer" do
-  version "4.10.2.28257,1028599"
+  version "4.10.2,1028599"
   sha256 "dbaff1ef7f7c7354b1c8b9a460c77773c1f510a730d9b2f0c73e2a3106499c34"
 
   # slicer.kitware.com/midas3/ was verified as official when first introduced to the cask
   url "https://slicer.kitware.com/midas3/download?bitstream=#{version.after_comma}"
   appcast "https://github.com/Slicer/Slicer/releases.atom"
   name "3D Slicer"
+  desc "Medical image processing and visualization system"
   homepage "https://www.slicer.org/"
 
   app "Slicer.app"
