faf87a2089d31cb8584d92512d14ffcf76328a71
singularitti <singularitti@outlook.com>
2020-10-01 18:01:45 -0400

    Update microsoft-edge from 85.0.564.63 to 85.0.564.68

--- Casks/microsoft-edge.rb
+++ Casks/microsoft-edge.rb
@@ -1,6 +1,6 @@
 cask "microsoft-edge" do
-  version "85.0.564.63"
-  sha256 "1e9d51f4b4ae096ba9594cc0136a7745aa5a0116960ae43512c4530fb8d2f124"
+  version "85.0.564.68"
+  sha256 "ac9013ff4a0568eb0d238495e0d9bfcebfce42b88aa1c0a6a9e865fd64c7d0a7"
 
   # officecdn-microsoft-com.akamaized.net/ was verified as official when first introduced to the cask
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdge-#{version}.pkg"
