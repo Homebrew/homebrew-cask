e48bc0dce74db98ab574aa697554fed93f323d86
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2020-10-02 01:26:36 +0200

    Update gog-galaxy from 2.0.20.38 to 2.0.23.3 (#90221)

--- Casks/gog-galaxy.rb
+++ Casks/gog-galaxy.rb
@@ -1,6 +1,6 @@
 cask "gog-galaxy" do
-  version "2.0.20.38"
-  sha256 "67c47190352ba12461b187113b89cadd7cfdd68b8987bd006d1944b29ebd3a68"
+  version "2.0.23.3"
+  sha256 "854d46793e487f62b610bbb680311461ca6a4e858c035fe8b41447bd745b9206"
 
   url "https://cdn.gog.com/open/galaxy/client/galaxy_client_#{version}.pkg"
   appcast "https://www.macupdater.net/cgi-bin/extract_text/extract_text_split_easy.cgi?url=https://gog.com/galaxy&encoding=utf-8&user_agent=Macintosh"
