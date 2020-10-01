1da85659dbe88fdd2cf17ef9d9f50853e18683b1
Antonio Kim <contact@antoniojkim.com>
2020-09-30 21:47:19 -0400

    Fixed appcast

--- Casks/calcpp.rb
+++ Casks/calcpp.rb
@@ -3,7 +3,7 @@ cask "calcpp" do
   sha256 "f75d95f24e019a5cde0e3d8c0ff5cad93bd07524513dca19cf8b986e6278aa7f"
 
   url "https://github.com/antoniojkim/CalcPlusPlus/releases/download/v0.4/calcplusplus.bottle.tgz"
-  appcast "https://github.com/antonim/CalcPlusPlus/releases.atom"
+  appcast "https://github.com/antoniojkim/CalcPlusPlus/releases.atom"
   name "CalcPlusPlus"
   desc "Advanced Scientific Calculator. Highly performant, robust, and easy to use"
   homepage "https://github.com/antoniojkim/CalcPlusPlus"
