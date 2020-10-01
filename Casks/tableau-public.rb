feb8d901d35c9ecd35abac38fd9a4c98f868c543
Randall <ran.dall@icloud.com>
2020-10-01 15:59:49 -0700

    Update tableau-public from 2020.3.0 to 2020.3.1

--- Casks/tableau-public.rb
+++ Casks/tableau-public.rb
@@ -1,6 +1,6 @@
 cask "tableau-public" do
-  version "2020.3.0"
-  sha256 "4d53dd0300b48b081631234bfcda9f86fe601097e6bfcd380e8b7a445e823a0a"
+  version "2020.3.1"
+  sha256 "6bb45f0652ae94f21d7eebd46ad2762d210307206b8a2157151c3c9eca6f3f25"
 
   url "https://downloads.tableau.com/public/TableauPublic-#{version.dots_to_hyphens}.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.tableau.com/downloads/public/mac",
