b1f93d14467c896bdf5b36a5bc0e39a3d9f830c0
CoreCode <core-code@users.noreply.github.com>
2020-10-01 05:06:11 +0200

    Update acquia-dev from 2.2020.08.10 to 2.2020.09.23 (#90093)

--- Casks/acquia-dev.rb
+++ Casks/acquia-dev.rb
@@ -1,22 +1,25 @@
 cask "acquia-dev" do
-  version "2.2020.08.10"
-  sha256 "6fe612c9f12d0ebe3b8383fd3b86cf27df3728ebb4790bb2f2f6c3c40b044cf0"
+  version "2.2020.09.23"
+  sha256 "3fda351ecda5d19ecaccdd3f3b8fa4930afe84943a14f9cdbd2aed62c25c5495"
 
   url "https://dev.acquia.com/sites/default/files/file/#{version.minor_patch.dots_to_hyphens}/AcquiaDevDesktop-#{version.dots_to_hyphens}.dmg"
   appcast "https://dev.acquia.com/downloads",
           must_contain: version.dots_to_hyphens
   name "Acquia Dev Desktop"
+  desc "Install, test, and build Drupal sites locally"
   homepage "https://www.acquia.com/drupal/acquia-dev-desktop"
 
-  installer script: {
-    executable: "Acquia Dev Desktop Installer.app/Contents/MacOS/installbuilder.sh",
-    args:       ["--mode", "unattended", "--unattendedmodeui", "none"],
-    sudo:       true,
-  }
+  installer manual: "Acquia Dev Desktop Installer.app"
 
   uninstall script: {
     executable: "/Applications/DevDesktop/uninstall.app/Contents/MacOS/installbuilder.sh",
     args:       ["--mode", "unattended", "--unattendedmodeui", "none"],
     sudo:       true,
   }
+
+  zap trash: [
+    "~/.acquia",
+    "~/Library/Caches/com.acquia.acquia-dev-desktop#{version.major}",
+    "~/Library/Saved Application State/com.acquia.acquia-dev-desktop#{version.major}.savedState",
+  ]
 end
