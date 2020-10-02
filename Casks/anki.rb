ad2f5562c4587dda96dfe9a940f755ce829774d5
Daniel Parker <danpker@gmail.com>
2020-10-02 14:05:48 +0100

    Update anki from 2.1.34 to 2.1.35 (#90228)

--- Casks/anki.rb
+++ Casks/anki.rb
@@ -1,10 +1,10 @@
 cask "anki" do
-  version "2.1.34"
-  sha256 "dbba818c66b483b20433a16f5ae7500f4e9350f7d8818264756746a23d98b10d"
+  version "2.1.35"
+  sha256 "53f526243e44c2a4ecc44c2f79ab49a7c2a8bc008a15164038d8509da2974477"
 
   # github.com/ankitects/anki/ was verified as official when first introduced to the cask
   url "https://github.com/ankitects/anki/releases/download/#{version}/anki-#{version}-mac.dmg"
-  appcast "https://changes.ankiweb.net/README.md"
+  appcast "https://github.com/ankitects/anki/releases.atom"
   name "Anki"
   desc "Memory training application"
   homepage "https://apps.ankiweb.net/"
