d2f26a635843d06658fd9be6b63c0bc2adff46be
frihamn <hello@prudens.eu>
2020-10-02 12:06:23 +0200

    Update fastrawviewer from 1.6.0.1639 to 1.6.1.1664 (#90232)

--- Casks/fastrawviewer.rb
+++ Casks/fastrawviewer.rb
@@ -1,6 +1,6 @@
 cask "fastrawviewer" do
-  version "1.6.0.1639"
-  sha256 "355a1fd67aa56986628b4fe4ccdb8084d8403ff3cb7a2b94a9ca2a5c0f238cc8"
+  version "1.6.1.1664"
+  sha256 "899e1ae2f0583b1a8128e6c519288308cb59743a3f18234a3f586c4e9342879b"
 
   url "https://updates.fastrawviewer.com/data/FastRawViewer-#{version}.dmg"
   appcast "https://www.fastrawviewer.com/download"
