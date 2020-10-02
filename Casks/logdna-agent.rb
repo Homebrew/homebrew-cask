0ecd3256c5726ef14f3f95df4b2485d6afc6bb97
Samir Musali <samir.musali@logdna.com>
2020-10-02 15:45:37 -0400

    logdna-agent: release LogDNA Agent v2.0.0

--- Casks/logdna-agent.rb
+++ Casks/logdna-agent.rb
@@ -1,11 +1,12 @@
 cask "logdna-agent" do
-  version "1.6.5"
-  sha256 "7dde78a9930f75e3ab0111a1361a2764670d03b70a992158f833cfa42d415998"
+  version "2.0.0"
+  sha256 "ad87d1598b91130a9940c71412f15b4da356408d1a37bcfa7244cca18a452b37"
 
   # github.com/logdna/logdna-agent/ was verified as official when first introduced to the cask
   url "https://github.com/logdna/logdna-agent/releases/download/#{version}/logdna-agent-#{version}.pkg"
   appcast "https://github.com/logdna/logdna-agent/releases.atom"
   name "LogDNA Agent"
+  desc "Agent streams from log files to your LogDNA account"
   homepage "https://logdna.com/"
 
   pkg "logdna-agent-#{version}.pkg"
