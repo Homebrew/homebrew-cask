a68d9a2e206456291d58db3672372ec49944798a
Vítor Galvão <info@vitorgalvao.com>
2020-10-02 15:29:49 +0100

    Update tenable-nessus-agent.rb

--- Casks/tenable-nessus-agent.rb
+++ Casks/tenable-nessus-agent.rb
@@ -3,25 +3,19 @@ cask "tenable-nessus-agent" do
   sha256 "5f9cc7c8ac49226488e21bc2dac1a96ed38a8ed501ebda2e2915137dfc1a43d3"
 
   url "https://www.tenable.com/downloads/api/v1/public/pages/nessus-agents/downloads/11493/download?i_agree_to_tenable_license_agreement=true"
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://www.tenable.com/downloads/api/v1/public/pages/nessus-agents/downloads/11493/download?i_agree_to_tenable_license_agreement=true'
   name "Tenable Nessus Agent"
   desc "Agent for Nessus vulnerability scanner"
   homepage "https://www.tenable.com/"
 
   pkg "Install Nessus Agent.pkg"
 
-  uninstall pkgutil:   [
-    "com.tenablesecurity.NessusAgent.Preferences",
-  ],
+  uninstall pkgutil:   "com.tenablesecurity.NessusAgent.Preferences",
             delete:    [
               "/Library/NessusAgent",
               "/Library/LaunchDaemons/com.tenablesecurity.nessusagent.plist",
               "/Library/PreferencePanes/Nessus Agent Preferences.prefPane",
               "/private/etc/tenable_tag",
             ],
-            launchctl: [
-              "com.tenablesecurity.nessusagent",
-            ]
-
-  caveats "See https://docs.tenable.com/nessus/Content/InstallNessusAgentMacOSX.htm"\
-    "#Link-Agent-Using-Command-Line-Interface for instructions to link the agent to a server."
+            launchctl: "com.tenablesecurity.nessusagent"
 end
