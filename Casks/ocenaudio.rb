1a7653ad19b072b8cb8064398b0ec1ae49222992
drnhat <drnhat@hotmail.com>
2020-10-02 15:28:42 +0700

    Update ocenaudio from 3.9.1 to 3.9.2 (#90227)

--- Casks/ocenaudio.rb
+++ Casks/ocenaudio.rb
@@ -1,12 +1,12 @@
 cask "ocenaudio" do
-  version "3.9.1"
+  version "3.9.2"
 
   if MacOS.version <= :high_sierra
     sha256 "afe88197e61097964f08f015cf4d2676d284ecbecf49fd5f6a9f9defc4eb81a8"
 
     url "https://www.ocenaudio.com/downloads/index.php/ocenaudio_sierra.dmg"
   else
-    sha256 "27c611a200bb8eae2b39c6830a28a9597da3566094dd3b5198c7f4344d6b2c2a"
+    sha256 "5a0ff9b898752b7086a4cd59356770acd3e1314ba2dabbda2d1cfd907290b7ff"
 
     url "https://www.ocenaudio.com/downloads/index.php/ocenaudio_mojave.dmg"
   end
