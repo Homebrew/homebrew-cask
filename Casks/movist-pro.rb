be803841bef5886609ab03f7edbdb1fb430c02e7
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2020-10-02 05:47:08 -0700

    Update movist-pro.rb

--- Casks/movist-pro.rb
+++ Casks/movist-pro.rb
@@ -1,6 +1,6 @@
 cask "movist-pro" do
-  version "2.4.2"
-  sha256 "b473fb397b0ac809c1cf1a7e991e4dbee2ff28e67b7518ae4e57b2bfb9a06a90"
+  version "2.4.3"
+  sha256 "cfdeadc54986b7acfd05b3b84f3edecc6edfa6c7921d429c75db6b96495f10f5"
 
   # s3.ap-northeast-2.amazonaws.com/update.cocoable.com was verified as official when first introduced to the cask
   url "https://s3.ap-northeast-2.amazonaws.com/update.cocoable.com/MovistPro_#{version}.app.zip"
