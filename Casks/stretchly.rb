206cefdc742ad5668f6e13037138fa200a7640e8
eux <euxuuu@gmail.com>
2020-10-03 15:58:47 +0800

    Update stretchly from 1.1.0 to 1.2.0 (#90289)

--- Casks/stretchly.rb
+++ Casks/stretchly.rb
@@ -1,6 +1,6 @@
 cask "stretchly" do
-  version "1.1.0"
-  sha256 "23f2d5bb951b06f924b92e8a3d8b5b343d32c8c35b9e03c67f1620a8c5711eb4"
+  version "1.2.0"
+  sha256 "54b209b55286f61b5527b830ede1258553cc9eac339ad9148461de726a96c0f6"
 
   # github.com/hovancik/stretchly/ was verified as official when first introduced to the cask
   url "https://github.com/hovancik/stretchly/releases/download/v#{version}/stretchly-#{version}.dmg"
