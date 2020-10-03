04facc1fbd033dceecaff504db466f670778797e
Randall <ran.dall@icloud.com>
2020-10-03 09:22:56 -0700

    Update thebrain from 11.0.125.0 to 11.0.126.0

--- Casks/thebrain.rb
+++ Casks/thebrain.rb
@@ -1,6 +1,6 @@
 cask "thebrain" do
-  version "11.0.125.0"
-  sha256 "e500bb578087faf195e46a7bf36b6d8d1ede1398788d5c05d835faee08ded47c"
+  version "11.0.126.0"
+  sha256 "1dd46d1c0474a502ba194a095c082c554f2ab451df24be365f6e30c928077dda"
 
   url "https://updater.thebrain.com/files/TheBrain#{version}.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://salesapi.thebrain.com/?a=doDirectDownload%26id=11000"
