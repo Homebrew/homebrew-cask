d294069597381f21150db4078c4334e07b041d4a
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-03 16:40:22 +0000

    Update superproductivity from 5.9.0 to 5.9.2 (#90298)

--- Casks/superproductivity.rb
+++ Casks/superproductivity.rb
@@ -1,6 +1,6 @@
 cask "superproductivity" do
-  version "5.9.0"
-  sha256 "a71c06bcf0dd00b913fe2f78cce95510482d110d7d242bebc1694ee3c71c097e"
+  version "5.9.2"
+  sha256 "beec31ba34ab4f7842dae3ddd6ed177c9b01faa35560ba989902515dbf9275b9"
 
   # github.com/johannesjo/super-productivity/ was verified as official when first introduced to the cask
   url "https://github.com/johannesjo/super-productivity/releases/download/v#{version}/superProductivity-#{version}-mac.zip"
