aa1387905578626be009a451065ca61f74a92767
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-03 01:26:02 +0000

    Update icab from 6.0.2 to 6.0.3 (#90272)

--- Casks/icab.rb
+++ Casks/icab.rb
@@ -1,6 +1,6 @@
 cask "icab" do
-  version "6.0.2"
-  sha256 "46b22a3c772b8adfcb248a25f0a40ad691acdc979ffc14c602ed8b84544f2001"
+  version "6.0.3"
+  sha256 "641610d199b2532868f924a4710d8166bcf352bb9811400565d07f4bf25bbc07"
 
   # icab.clauss-net.de/ was verified as official when first introduced to the cask
   url "http://icab.clauss-net.de/icab/iCab_#{version}.zip"
