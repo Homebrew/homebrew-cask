6b83f98d03983ddcd75b54e97d6329a354eb26c2
Fran Bolívar <francisco.bolivar.lupianez@gmail.com>
2020-10-03 11:00:22 +0200

    Update alfaview from 8.8.3 to 8.9.1

--- Casks/alfaview.rb
+++ Casks/alfaview.rb
@@ -1,6 +1,6 @@
 cask "alfaview" do
-  version "8.8.3"
-  sha256 "c87757b9030ca4a96bde98568e1936af70a431803adb2c7d119bf2e7ac5a50b2"
+  version "8.9.1"
+  sha256 "5155cfdd96dc2a1c9dc85bc04259c9bdee121305f67265a4aac622548bf65bf6"
 
   url "https://assets.alfaview.com/stable/mac/alfaview-mac-production-#{version}.dmg"
   appcast "https://production-alfaview-assets.alfaview.com/stable/mac/version.info"
