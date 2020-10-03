eabff66a1291ee94be3b596c42bc3676d20d72a9
CoreCode <core-code@users.noreply.github.com>
2020-10-03 12:46:38 +0200

    Update noxappplayer.rb

--- Casks/noxappplayer.rb
+++ Casks/noxappplayer.rb
@@ -3,7 +3,7 @@ cask "noxappplayer" do
   sha256 "da26a38c66b0ad5e79c09bdcdb9e981a08c6ba775469d53fec7148ec40843252"
 
   url "https://res06.bignox.com/full/#{version.after_comma.before_colon}/#{version.after_colon}.dmg?filename=NoxInstaller_#{version.before_comma}_en.dmg"
-  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.bignox.com/en/download/fullPackage/mac_fullzip"
+  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect_curl.cgi?url=https://www.bignox.com/en/download/fullPackage/mac_fullzip"
   name "NoxAppPlayer"
   desc "Android emulator to play mobile games"
   homepage "https://www.bignox.com/"
