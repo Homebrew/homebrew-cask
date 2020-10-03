fa5accef1536bac0bbae3b4d0ab7de674f89c613
Randall <ran.dall@icloud.com>
2020-10-02 17:56:41 -0700

    Update makemkv from 1.15.2 to 1.15.3

--- Casks/makemkv.rb
+++ Casks/makemkv.rb
@@ -1,6 +1,6 @@
 cask "makemkv" do
-  version "1.15.2"
-  sha256 "841bf2e6bbd13f793a84595aa1bb1bb02861eb4dae848d26cd96fe0ddd53f970"
+  version "1.15.3"
+  sha256 "cce9311c109558acedc6d759666e1e18fc228fdcd6c98bb7323094a7ef271f6f"
 
   url "https://www.makemkv.com/download/makemkv_v#{version}_osx.dmg"
   appcast "https://www.makemkv.com/download/"
