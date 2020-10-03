952ec77ae93eb76a66f16776d721108e9251e47a
suschizu <brigitte@corecode.io>
2020-10-03 02:17:18 +0200

    Update rocket-chat from 3.0.0 to 3.0.1 (#90244)

--- Casks/rocket-chat.rb
+++ Casks/rocket-chat.rb
@@ -1,11 +1,12 @@
 cask "rocket-chat" do
-  version "3.0.0"
-  sha256 "67921d2e673a61b7c7c3dd3fa29376f3f2070b260e57e5f789877768d21ce5c5"
+  version "3.0.1"
+  sha256 "7fdda8dd654512f45fd23be24ccb5c51840e00ce011cfec1984b819b219e9eb8"
 
   # github.com/RocketChat/Rocket.Chat.Electron/ was verified as official when first introduced to the cask
   url "https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/#{version}/rocketchat-#{version}.dmg"
   appcast "https://github.com/RocketChat/Rocket.Chat.Electron/releases.atom"
   name "Rocket.Chat"
+  desc "Official desktop client for Rocket.Chat"
   homepage "https://rocket.chat/"
 
   auto_updates true
