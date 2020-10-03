3db3d221fb70a06244558f0024035a4875e1222d
zsoltion <zsoltion@users.noreply.github.com>
2020-10-03 02:03:51 +0200

    Update coconutbattery.rb
    Use latest build from server

--- Casks/coconutbattery.rb
+++ Casks/coconutbattery.rb
@@ -4,8 +4,8 @@ cask "coconutbattery" do
     sha256 "8e289fb4a75cb117fc1d7861020c9ab2384b09dfd18f066c7fadfc9d42c3ac56"
     url "https://www.coconut-flavour.com/downloads/coconutBattery_#{version}.zip"
   else
-    version "3.9.1,0af56ac0"
-    sha256 "6b0df9b3eb06ede70d15a6d43b01497d2b73a5ce193b8650227765efc9d1f0a7"
+    version "3.9.1,6c26a706"
+    sha256 "825ba09d8ca7dee2aaa4dceee696ca7c46b983810cb4488ece8d3f8445b6f93b"
     url "https://www.coconut-flavour.com/downloads/coconutBattery_#{version.before_comma.no_dots}_#{version.after_comma}.zip"
     appcast "https://coconut-flavour.com/updates/coconutBattery.xml"
   end
