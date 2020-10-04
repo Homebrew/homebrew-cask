ce1a862a07f856781a7ce435b6f8bb04f1635f66
CoreCode <core-code@users.noreply.github.com>
2020-10-04 21:35:21 +0200

    Update karabiner-elements.rb

--- Casks/karabiner-elements.rb
+++ Casks/karabiner-elements.rb
@@ -4,16 +4,22 @@ cask "karabiner-elements" do
     sha256 "c1b06252ecc42cdd8051eb3d606050ee47b04532629293245ffdfa01bbc2430d"
 
     url "https://pqrs.org/osx/karabiner/files/Karabiner-Elements-#{version}.dmg"
+  
+    pkg "Karabiner-Elements.sparkle_guided.pkg"
   elsif MacOS.version <= :mojave
     version "12.10.0"
     sha256 "53252f7d07e44f04972afea2a16ac595552c28715aa65ff4a481a1c18c8be2f4"
 
     url "https://github.com/pqrs-org/Karabiner-Elements/releases/download/v#{version}/Karabiner-Elements-#{version}.dmg"
+  
+    pkg "Karabiner-Elements.sparkle_guided.pkg"
   else
     version "13.0.0"
     sha256 "18e3f255fd479046f36e57f2c2e0319568aad48784d6deb84c6ae40e9c92db2b"
 
     url "https://github.com/pqrs-org/Karabiner-Elements/releases/download/v#{version}/Karabiner-Elements-#{version}.dmg"
+    
+    pkg "Karabiner-Elements.pkg"
   end
 
   appcast "https://pqrs.org/osx/karabiner/files/karabiner-elements-appcast.xml"
@@ -23,8 +29,6 @@ cask "karabiner-elements" do
 
   auto_updates true
   depends_on macos: ">= :el_capitan"
-
-  pkg "Karabiner-Elements.sparkle_guided.pkg"
 
   uninstall signal:  [
     ["TERM", "org.pqrs.Karabiner-Elements"],
