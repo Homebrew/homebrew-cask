44f8c01c178600b158379487eb7ed6ad7c97ee28
Randall <ran.dall@icloud.com>
2020-10-04 10:19:12 -0700

    Update macjournal from 7.0.3 to 7.0.4

--- Casks/macjournal.rb
+++ Casks/macjournal.rb
@@ -1,6 +1,6 @@
 cask "macjournal" do
-  version "7.0.3"
-  sha256 "981675196217b9776cfadcbdb244a74075648788b421b502e289fc82331cd1b5"
+  version "7.0.4"
+  sha256 "e5284f99961ebfbe3d19e39d6b2206c781d109a0b6806b1481004f06eb5ecb50"
 
   url "https://danschimpf.com/macjournal/MacJournal_#{version}.zip"
   appcast "https://danschimpf.com/appcasts/MacJournal#{version.major}.appcast"
