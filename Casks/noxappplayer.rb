3a31980ffa60b0b1d63c9513115c6ab13d7dde3e
zsoltion <zsoltion@users.noreply.github.com>
2020-10-04 11:06:23 +0200

    Update noxappplayer from 3.0.5.0 to 3.8.0.0 (#90251)

--- Casks/noxappplayer.rb
+++ Casks/noxappplayer.rb
@@ -1,9 +1,9 @@
 cask "noxappplayer" do
-  version "3.0.5.0,20200715:531d774287dc4ddfad58275245ceec8d"
-  sha256 "74f762b95a0fef3f0a27778eb847dc3346748abca021f3f55bc8c192a6571401"
+  version "3.8.0.0,20200910:c4fc37cebc6c4b95a365ecb56972a439"
+  sha256 "da26a38c66b0ad5e79c09bdcdb9e981a08c6ba775469d53fec7148ec40843252"
 
-  url "https://res06.bignox.com/full/#{version.after_comma.before_colon}/#{version.after_colon}.dmg?filename=Nox_installer_for_mac_intl_#{version.before_comma}.dmg"
-  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.bignox.com/en/download/fullPackage/mac_fullzip"
+  url "https://res06.bignox.com/full/#{version.after_comma.before_colon}/#{version.after_colon}.dmg?filename=NoxInstaller_#{version.before_comma}_en.dmg"
+  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect_curl.cgi?url=https://www.bignox.com/en/download/fullPackage/mac_fullzip"
   name "NoxAppPlayer"
   desc "Android emulator to play mobile games"
   homepage "https://www.bignox.com/"
