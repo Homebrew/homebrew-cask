911fea1189cafc3827088d6d573d90949a31672a
Omar Kilani <omar@rememberthemilk.com>
2020-10-03 19:51:13 -0700

    Update remember-the-milk from 1.2.8 to 1.2.9 (#90319)

--- Casks/remember-the-milk.rb
+++ Casks/remember-the-milk.rb
@@ -1,6 +1,6 @@
 cask "remember-the-milk" do
-  version "1.2.8"
-  sha256 "f8a8ed97a30e4bdc154cd0d10b1b4a689bb2f4d20365db660ab1f13c99fbd4d6"
+  version "1.2.9"
+  sha256 "c3e04a8b19a8b9c5e57ecc8f51fc8adf3aad28df644fee162839b72954724334"
 
   url "https://www.rememberthemilk.com/download/mac/RememberTheMilk-#{version}.zip"
   appcast "https://www.rememberthemilk.com/services/mac/"
