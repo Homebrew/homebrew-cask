6a7310340a4d3ccc0848993765b7d98752addf27
CoreCode <core-code@users.noreply.github.com>
2020-10-04 20:10:46 +0200

    Update karabiner-elements.rb

--- Casks/karabiner-elements.rb
+++ Casks/karabiner-elements.rb
@@ -1,12 +1,17 @@
 cask "karabiner-elements" do
-  if MacOS.version == :el_capitan
+  if MacOS.version <= :el_capitan
     version "11.6.0"
     sha256 "c1b06252ecc42cdd8051eb3d606050ee47b04532629293245ffdfa01bbc2430d"
 
     url "https://pqrs.org/osx/karabiner/files/Karabiner-Elements-#{version}.dmg"
-  else
+  elsif MacOS.version <= :mojave
     version "12.10.0"
     sha256 "53252f7d07e44f04972afea2a16ac595552c28715aa65ff4a481a1c18c8be2f4"
+
+    url "https://github.com/pqrs-org/Karabiner-Elements/releases/download/v#{version}/Karabiner-Elements-#{version}.dmg"
+  else
+    version "13.0.0"
+    sha256 "18e3f255fd479046f36e57f2c2e0319568aad48784d6deb84c6ae40e9c92db2b"
 
     url "https://github.com/pqrs-org/Karabiner-Elements/releases/download/v#{version}/Karabiner-Elements-#{version}.dmg"
   end
