5cf97fbaba722924042b82d9c086fc389b06accf
Randall <ran.dall@icloud.com>
2020-10-05 11:35:03 -0700

    Update hookshot from 1.7 to 1.8

--- Casks/hookshot.rb
+++ Casks/hookshot.rb
@@ -1,6 +1,6 @@
 cask "hookshot" do
-  version "1.7"
-  sha256 "91bdae08ba1cb4e285803730d478b5549c9cc042234f245f0fd79afa2e84eb1a"
+  version "1.8"
+  sha256 "88edcda3dce06cc10cf4c89d8325e71ed3fa92dab7d28594349f49bfbdb5a882"
 
   url "https://hookshot.app/downloads/Hookshot#{version}.dmg"
   appcast "https://hookshot.app/downloads/updates.xml"
