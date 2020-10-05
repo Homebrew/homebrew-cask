18aa6aee0e56acd3ef9ddc1d41a2e59f04de158f
Gian Marco Cinalli <gianmarco.cinalli@cyberdyne.it>
2020-10-05 09:52:24 +0200

    Update home-assistant from 2020.7,3 to 2020.7,5

--- Casks/home-assistant.rb
+++ Casks/home-assistant.rb
@@ -1,6 +1,6 @@
 cask "home-assistant" do
-  version "2020.7,3"
-  sha256 "d17ceec83c58f50ac33f7e9111966962c46eec4cd758d9306f8fdc5d86678338"
+  version "2020.7,5"
+  sha256 "42513d9f648ab332eb45cadbb73d0d2c04b54ddc9f426943417e72d2daf43a21"
 
   # github.com/home-assistant/iOS/ was verified as official when first introduced to the cask
   url "https://github.com/home-assistant/iOS/releases/download/release%2F#{version.before_comma}%2F#{version.after_comma}/home-assistant-mac.zip"
