d44cf25ed9cfacea93900fe8e0e50b1f328cff52
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-05 17:16:19 +0000

    Update praat from 6.1.24 to 6.1.25 (#90377)

--- Casks/praat.rb
+++ Casks/praat.rb
@@ -1,6 +1,6 @@
 cask "praat" do
-  version "6.1.24"
-  sha256 "c303e905fddcd19201bf13cfd49c6f81054c0bb695d68605c9e652f8c4f26948"
+  version "6.1.25"
+  sha256 "4e3b436a63f303fa2f72b3aae1bc453365949a8a3984ffc7ec420d11e6853e18"
 
   # github.com/praat/praat/ was verified as official when first introduced to the cask
   url "https://github.com/praat/praat/releases/download/v#{version}/praat#{version.no_dots}_mac64.dmg"
