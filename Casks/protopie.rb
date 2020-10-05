924b624a857d81a5ae99ce8c6f9fcbf0bc2e7ceb
Randall <ran.dall@icloud.com>
2020-10-05 12:05:23 -0700

    Update protopie from 5.1.0 to 5.1.1

--- Casks/protopie.rb
+++ Casks/protopie.rb
@@ -1,6 +1,6 @@
 cask "protopie" do
-  version "5.1.0"
-  sha256 "daaa08b8f9864d15f2f21d5d6fef3b9e492c5a7c8824c41c3476af2c891b469c"
+  version "5.1.1"
+  sha256 "237e5865e841bc66a807d171775c0a44cbe75fbe2460a034dbec037097ca855f"
 
   url "https://release.protopie.io/ProtoPie-#{version}.dmg"
   appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.protopie.io/darwin/latest"
