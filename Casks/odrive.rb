061a237b91ce04623518444e43e279fc95542227
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-05 19:15:54 +0000

    Update odrive from 6813 to 6829 (#90397)

--- Casks/odrive.rb
+++ Casks/odrive.rb
@@ -1,6 +1,6 @@
 cask "odrive" do
-  version "6813"
-  sha256 "781b94ef2cf492e422cdf30dfc31fceb6597cb3165042e4f24a73dc3220e0afb"
+  version "6829"
+  sha256 "5ba0f6e12603aa3abf910e050c08b78d3a88b35d86e8bfdf8520a9a8d51b8cd7"
 
   # downloads can be found at https://www.odrive.com/downloaddesktop
   # d3huse1s6vwzq6.cloudfront.net/ was verified as official when first introduced to the cask
