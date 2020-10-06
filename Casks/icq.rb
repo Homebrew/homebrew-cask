ca26c07a0deef701c70c4184db364bc61fab68df
Randall <ran.dall@icloud.com>
2020-10-06 12:47:22 -0700

    Update icq from 3.0.28888 to 3.0.29220

--- Casks/icq.rb
+++ Casks/icq.rb
@@ -1,6 +1,6 @@
 cask "icq" do
-  version "3.0.28888"
-  sha256 "d4df48dd83c5b632dbb479ea06fb5ed7e0bf20a0186b4359cc792886da00cfe1"
+  version "3.0.29220"
+  sha256 "46b93be042562e351a803bd3caec02f354c8fbe2ecb2c74eaeb493dec2e1a8df"
 
   # icq-www.hb.bizmrg.com/ was verified as official when first introduced to the cask
   url "https://icq-www.hb.bizmrg.com/mac/x64/#{version}/icq.dmg"
