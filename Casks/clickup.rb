7143c5ac6e87920cb360d675c420692e48d82ad9
Randall <ran.dall@icloud.com>
2020-10-06 12:30:48 -0700

    Update clickup from 2.0.19 to 2.0.20

--- Casks/clickup.rb
+++ Casks/clickup.rb
@@ -1,6 +1,6 @@
 cask "clickup" do
-  version "2.0.19"
-  sha256 "974cd39d03605f35f9300e86339961093adc631bf85774ccbe9924965dd5eacf"
+  version "2.0.20"
+  sha256 "f9255a34450cc504864d2560c798cec44644ce72ce1ed0860024d59ff2a8a63c"
 
   url "https://attachments.clickup.com/desktop/clickup-desktop-#{version}-mac.dmg"
   appcast "https://github.com/clickup/clickup-release/releases.atom"
