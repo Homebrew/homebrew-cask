c2afe291e15d1e274dfd42665c5bc626ddccd1e5
CoreCode <core-code@users.noreply.github.com>
2020-10-06 17:02:12 +0200

    Update timecamp from 1.6.1.8 to 1.6.2.2 (#90417)

--- Casks/timecamp.rb
+++ Casks/timecamp.rb
@@ -1,6 +1,6 @@
 cask "timecamp" do
-  version "1.6.1.8"
-  sha256 "9e98b80020e75de3d5b4bb2c66b33777cd75f42f5afcdab9922961bad1fa42e9"
+  version "1.6.2.2"
+  sha256 "8d02d1ab4c649bf6acb4e82aeb6b38b5bcb382dc436eca47e68b121ae56c4e4a"
 
   # timecamp.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://timecamp.s3.amazonaws.com/downloadsoft/#{version}/TimeCampSetup_macOS.dmg"
