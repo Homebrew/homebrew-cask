80aaa4371ec3d0479af81a3b32ea4e6a6f4583f8
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-06 19:35:22 +0000

    Update gisto from 1.13.2 to 1.13.3 (#90428)

--- Casks/gisto.rb
+++ Casks/gisto.rb
@@ -1,6 +1,6 @@
 cask "gisto" do
-  version "1.13.2"
-  sha256 "1d314ba1f87202d29b25b2d11a2f832baaf8f0b378e24d6d09de8ef89aeaa308"
+  version "1.13.3"
+  sha256 "10741de4aa155cff5857acbee80a277ee4907c788207651a593aa0a7b54a21d6"
 
   # github.com/Gisto/Gisto/ was verified as official when first introduced to the cask
   url "https://github.com/Gisto/Gisto/releases/download/v#{version}/Gisto-#{version}.dmg"
