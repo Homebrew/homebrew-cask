37de3da6085063320cdd8d24597ae98a2f2d5c38
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-07 23:51:41 +0000

    Update ajour from 0.3.5 to 0.4.0 (#90506)

--- Casks/ajour.rb
+++ Casks/ajour.rb
@@ -1,6 +1,6 @@
 cask "ajour" do
-  version "0.3.5"
-  sha256 "c5b4f0911dae3058efbacfdf13dd5aa337f9ecf66e1c43e16c9f96b41e07a008"
+  version "0.4.0"
+  sha256 "a3448469bc3a3eba63cad60a7c90d1d1c08b2f9f25ae6ade8a3878de6e3686de"
 
   url "https://github.com/casperstorm/ajour/releases/download/#{version}/ajour.dmg"
   appcast "https://github.com/casperstorm/ajour/releases.atom"
