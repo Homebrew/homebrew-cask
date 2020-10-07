39fb95d3def281dc878370e73df72458522a889d
Tony Smith <smittytone@icloud.com>
2020-10-07 16:46:14 +0100

    Inital upload (version 2.1.0)

---
+++ Casks/pdfmaker.rb
@@ -0,0 +1,11 @@
+cask "pdfmaker" do
+  version "2.1.0"
+  sha256 "9036d579f80e111e752c8b1c8370a513015a792a928e76e0b5b973f829eb9094"
+
+  url "https://www.smittytone.net/files/pdfmaker/pdfmaker-2-1-0.dmg"
+  name "pdfmaker"
+  desc "A command line tool to build PDFs from JPEG images"
+  homepage "https://www.smittytone.net/pdfmaker/index.html"
+
+  binary "pdfmaker"
+end
