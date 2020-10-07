e081c335110d691c600a1888a235af61167e18c2
Work <brigitte@corecode.io>
2020-10-07 19:22:11 +0200

    Update appstudio

--- Casks/appstudio.rb
+++ Casks/appstudio.rb
@@ -2,8 +2,8 @@ cask "appstudio" do
   version :latest
   sha256 :no_check
 
-  # dropbox.com/s/27y2gzzd8f6sm1p/ was verified as official when first introduced to the cask
-  url "https://www.dropbox.com/s/27y2gzzd8f6sm1p/AppStudio.dmg?dl=1"
+  # appstudio.s3.amazonaws.com/ was verified as official when first introduced to the cask
+  url "https://appstudio.s3.amazonaws.com/AppStudio.dmg"
   name "NSB/AppStudio"
   homepage "https://www.nsbasic.com/"
 
