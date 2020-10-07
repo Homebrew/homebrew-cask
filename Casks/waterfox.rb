236dd0193d2e9e293a38a09fbd482564c9bccd41
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-07 17:20:26 +0800

    Update waterfox.rb

--- Casks/waterfox.rb
+++ Casks/waterfox.rb
@@ -1,6 +1,6 @@
 cask "waterfox" do
-  version "2020.02"
-  sha256 "9c27aac645c269e765229adeeb48dcd298aab3b578709c70f3b9acdfdad07275"
+  version "2020.09"
+  sha256 "f98452eadb537f73354da1787ba535c1ce9a8e3447e97f3431825e8455d9f357"
 
   url "https://cdn.waterfox.net/releases/osx64/installer/Waterfox%20Classic%20#{version}%20Setup.dmg"
   appcast "https://www.waterfox.net/releases/"
