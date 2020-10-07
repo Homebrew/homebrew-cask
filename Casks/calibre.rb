d5569495080ac53eccb4635bcc4cbecbadf1579b
Loïc PÉRON <44115467+nicerloop@users.noreply.github.com>
2020-10-07 12:14:49 +0200

    Update calibre from 5.1.0 to 5.2.0 (#90473)

--- Casks/calibre.rb
+++ Casks/calibre.rb
@@ -4,8 +4,8 @@ cask "calibre" do
     sha256 "68829cd902b8e0b2b7d5cf7be132df37bcc274a1e5720b4605d2dd95f3a29168"
     url "https://download.calibre-ebook.com/#{version}/calibre-#{version}.dmg"
   else
-    version "5.1.0"
-    sha256 "e49d830ccecfe348d3a6468508cb5410d6e293019cbab65feb0f1b180143ceb9"
+    version "5.2.0"
+    sha256 "cf821cc9126fe1ff9c95248359954176810d0199f215cfac1fcc9835abe40064"
     # github.com/kovidgoyal/calibre/ was verified as official when first introduced to the cask
     url "https://github.com/kovidgoyal/calibre/releases/download/v#{version}/calibre-#{version}.dmg"
     appcast "https://github.com/kovidgoyal/calibre/releases.atom"
