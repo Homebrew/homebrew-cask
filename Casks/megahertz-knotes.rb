8df8d619dc718768a41028d349658b8b715584c1
Work <brigitte@corecode.io>
2020-10-07 12:42:55 +0200

    Update megahertz-knotes from 2.4.0 to 2.6.0

--- Casks/megahertz-knotes.rb
+++ Casks/megahertz-knotes.rb
@@ -1,8 +1,8 @@
 cask "megahertz-knotes" do
-  version "2.4.0"
-  sha256 "2c15b74006e17a04603cf7d8a802636d4e8fbbd1078ffd4a216f6eadc2d87f67"
+  version "2.6.0"
+  sha256 "5946b48b0e6ac588331336f760711c11c53ef9b0030616f39db6e9a41551d434"
 
-  url "http://cdn.knotesapp.cn/download/%e7%b3%af%e8%af%8d%e7%ac%94%e8%ae%b0-#{version}.dmg"
+  url "http://cdn.knotesapp.cn/download/Knotes-#{version}.dmg"
   appcast "https://knotes#{version.major}-release-cn.s3.amazonaws.com/mac/latest-mac.yml"
   name "Knotes"
   name "糯词笔记"
