b90ab4a29aca160c71849c709d485bbfc0d4c480
Viktor Szakats <commit@vsz.me>
2020-10-07 08:10:34 +0000

    ltspice: secure url

--- Casks/ltspice.rb
+++ Casks/ltspice.rb
@@ -2,7 +2,7 @@ cask "ltspice" do
   version :latest
   sha256 :no_check
 
-  url "http://ltspice.analog.com/software/LTspice.dmg"
+  url "https://ltspice.analog.com/software/LTspice.dmg"
   name "LTspice"
   homepage "https://www.analog.com/en/design-center/design-tools-and-calculators/ltspice-simulator.html"
 
