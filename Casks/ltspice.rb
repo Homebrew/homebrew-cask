a0fc60082436eeb7822462e6723294f3f2fed2d6
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-07 16:57:47 +0800

    Update ltspice.rb

--- Casks/ltspice.rb
+++ Casks/ltspice.rb
@@ -4,6 +4,7 @@ cask "ltspice" do
 
   url "https://ltspice.analog.com/software/LTspice.dmg"
   name "LTspice"
+  desc "SPICE simulation software, schematic capture and waveform viewer"
   homepage "https://www.analog.com/en/design-center/design-tools-and-calculators/ltspice-simulator.html"
 
   app "LTspice.app"
