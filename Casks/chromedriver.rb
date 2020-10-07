54690bd23531544d4e0ad28fa4753a91812d9816
Crystal Adkins <CrystalJewell@users.noreply.github.com>
2020-10-07 17:16:20 -0400

    Update chromedriver from 85.0.4183.87 to 86.0.4240.22 (#90503)

--- Casks/chromedriver.rb
+++ Casks/chromedriver.rb
@@ -1,6 +1,6 @@
 cask "chromedriver" do
-  version "85.0.4183.87"
-  sha256 "717ff47c926b6cfa5ebcb60d7cb57a42f338fa657083928d8e1d74ff9c4833dd"
+  version "86.0.4240.22"
+  sha256 "351ae30e9feab7ca6ccb94a549afcd62d6355561b78b8386cd4271d480a2fdc6"
 
   # chromedriver.storage.googleapis.com/ was verified as official when first introduced to the cask
   url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_mac64.zip"
