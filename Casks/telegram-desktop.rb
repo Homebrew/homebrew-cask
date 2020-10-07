5066752ecef2009184419772ccb936691a177f26
Stefano Campagna <stefano.campagna.1996@gmail.com>
2020-10-08 00:22:41 +0200

    Update telegram-desktop from 2.4.2 to 2.4.3

--- Casks/telegram-desktop.rb
+++ Casks/telegram-desktop.rb
@@ -1,6 +1,6 @@
 cask "telegram-desktop" do
-  version "2.4.2"
-  sha256 "0a9d9ca988a4e1bf5beb078e70c05f8ac863881d0ce25d07c560d27298e23cd6"
+  version "2.4.3"
+  sha256 "7a94343db376ca52d63970081174a889330b59b81408bce27c858b02f669817c"
 
   # github.com/telegramdesktop/tdesktop/ was verified as official when first introduced to the cask
   url "https://github.com/telegramdesktop/tdesktop/releases/download/v#{version}/tsetup.#{version}.dmg"
