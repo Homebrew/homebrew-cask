13148a20dfb5557499e525b582838dd84804f052
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-08 18:04:17 +0000

    Update tinymediamanager from 4.0.3 to 4.0.4 (#90558)

--- Casks/tinymediamanager.rb
+++ Casks/tinymediamanager.rb
@@ -1,6 +1,6 @@
 cask "tinymediamanager" do
-  version "4.0.3"
-  sha256 "273f605b708111ed23c9747e75a957b89e678eb54e59c4b6e75d6278744355f1"
+  version "4.0.4"
+  sha256 "0268db7244c548ed22c5d733bf5353e5a1b7d26da0794cac03057170bda8e41a"
 
   url "https://release.tinymediamanager.org/v#{version.major}/dist/tmm_#{version}_mac.zip"
   appcast "https://release.tinymediamanager.org/"
