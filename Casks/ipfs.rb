a3cc4df9cd2c48384b7b59823c74ca7d8c919b4f
Luflosi <luflosi@luflosi.de>
2020-10-08 20:35:42 +0200

    Update ipfs from 0.13.0 to 0.13.1

--- Casks/ipfs.rb
+++ Casks/ipfs.rb
@@ -1,6 +1,6 @@
 cask "ipfs" do
-  version "0.13.0"
-  sha256 "8927be0f9d7decd4c7def13472e69a7ced50f7d086b78dac29b3a3f4da04438d"
+  version "0.13.1"
+  sha256 "ec3fae4bc4f0f1cb4a3b892abc4e19de36918dc36fe9ede3aa193a7dfe8ef21e"
 
   url "https://github.com/ipfs-shipyard/ipfs-desktop/releases/download/v#{version}/ipfs-desktop-#{version}.dmg"
   appcast "https://github.com/ipfs-shipyard/ipfs-desktop/releases.atom"
