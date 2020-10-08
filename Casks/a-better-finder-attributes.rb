ebcb49110070a3fc06829ac8c252650f800c90dc
Tomy Hsieh <tomy0000000@gmail.com>
2020-10-08 22:29:53 +0800

    Update a-better-finder-attributes from 7.01 to 7.02 (#90548)

--- Casks/a-better-finder-attributes.rb
+++ Casks/a-better-finder-attributes.rb
@@ -1,6 +1,6 @@
 cask "a-better-finder-attributes" do
-  version "7.01"
-  sha256 "977a1ed8369c8bd53930de11edc2df003704e85895145412a3d2474fe1a91cd3"
+  version "7.02"
+  sha256 "778e23b79605989d86880ea2029db77391e58f3e71e6b95266c93659d8b3b314"
 
   url "https://www.publicspace.net/download/signedABFA#{version.major}.zip"
   appcast "https://www.publicspace.net/app/signed_abfa#{version.major}.xml"
