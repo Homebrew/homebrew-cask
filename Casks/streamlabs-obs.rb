34b6d86249d34f2c1c373ce40d95c1869a05ac9e
Randall <ran.dall@icloud.com>
2020-10-07 17:08:07 -0700

    Update streamlabs-obs from 0.23.4 to 0.24.0

--- Casks/streamlabs-obs.rb
+++ Casks/streamlabs-obs.rb
@@ -1,6 +1,6 @@
 cask "streamlabs-obs" do
-  version "0.23.4"
-  sha256 "dd926ff01ab1f57017a3d24361415eae6ed5c05a1efd4f9cb880d24471c32a5e"
+  version "0.24.0"
+  sha256 "2ae0c5f962eee67cbea1ade534c0f57e08e4d3026c0338db353156cb313ee72e"
 
   url "https://slobs-cdn.streamlabs.com/Streamlabs+OBS-#{version}.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://streamlabs.com/slobs/download&user_agent=Mac%20OS%20X"
