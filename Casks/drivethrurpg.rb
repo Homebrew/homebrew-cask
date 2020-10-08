0afc4af83541d24874de52f775b903111973fb5f
Randall <ran.dall@icloud.com>
2020-10-08 10:47:42 -0700

    Update drivethrurpg from 2.0.3.2 to 3.0.0.7

--- Casks/drivethrurpg.rb
+++ Casks/drivethrurpg.rb
@@ -1,5 +1,5 @@
 cask "drivethrurpg" do
-  version "2.0.3.2"
+  version "3.0.0.7"
   sha256 "a3cb7400e6f474057ea3045e73eafb63d46c81f130bc37b9fa03f601f21ad9c2"
 
   url "https://watermark.drivethrurpg.com/DriveThruRPG-setup-osx.dmg"
