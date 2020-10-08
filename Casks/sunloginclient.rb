b5af39c43c3fffa7ed94b93dfc7519e59be7aca0
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-08 13:16:50 +0800

    Update sunloginclient.rb

--- Casks/sunloginclient.rb
+++ Casks/sunloginclient.rb
@@ -5,15 +5,17 @@ cask "sunloginclient" do
   url "https://dl-cdn.oray.com/sunlogin/mac/SunloginClient_#{version}.dmg"
   name "Sunlogin X"
   name "向日葵 X"
+  desc "Remote desktop control and monitoring tool"
   homepage "https://sunlogin.oray.com/"
 
   pkg "SunloginClient.pkg"
 
   uninstall quit:      "com.oray.sunlogin.macclient",
-            pkgutil:   "com.mygreatcompany.pkg.SunloginClient",
+            pkgutil:   "com.oray.sunlogin.client",
             launchctl: [
-              "com.oray.sunlogin.desktopagent",
+              "com.oray.sunlogin",
+              "com.oray.sunlogin.agent",
               "com.oray.sunlogin.helper",
-              "com.oray.sunlogin.service",
+              "com.oray.sunlogin.startup",
             ]
 end
