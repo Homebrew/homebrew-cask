a27d2c53dc07845d4aff6d7e9937e13c43a283b1
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-10 07:16:44 +0800

    Update niftyman.rb

--- Casks/niftyman.rb
+++ Casks/niftyman.rb
@@ -4,8 +4,9 @@ cask "niftyman" do
 
   # notion-menubar-updates.s3-us-west-2.amazonaws.com was verified as official when first introduced to the cask
   url "https://notion-menubar-updates.s3-us-west-2.amazonaws.com/Niftyman-#{version}.dmg"
+  appcast "https://notion-menubar-updates.s3.amazonaws.com/latest-mac.yml"
   name "Niftyman"
-  desc "Beautifully crafted Notion experience in menu bar"
+  desc "Access the Notion tool from the menu bar"
   homepage "https://shadowfax.app/niftyman"
 
   app "Niftyman.app"
