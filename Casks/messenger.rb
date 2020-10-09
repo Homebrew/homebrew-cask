bb9bb849c01d4eae920860d1ced8ced403820865
Filip Havrlent <filip@havrlent.com>
2020-10-09 20:30:09 +0200

    Create messenger cask

---
+++ Casks/messenger.rb
@@ -0,0 +1,12 @@
+cask "messenger" do
+  version "72.6.119"
+  sha256 "3d5a2a0201b256604fb2623dc55c41b3f791a0ff5d5e3d76a8cff6a4c7986ebc"
+
+  url "https://scontent.fksc1-1.fna.fbcdn.net/v/t39.2270-6/10000000_31956740223927584_2690995267225778162_n.dmg/Messenger.72.6.119.dmg?_nc_cat=110&_nc_sid=435822&_nc_ohc=cIfKgqg6K8YAX9IGpTo&_nc_ht=scontent.fksc1-1.fna&oh=ec95767a0468b3cdcae0884d9e214699&oe=5FA759F1&dl=1"
+  name "Messenger"
+  desc "Desktop Facebook Messenger"
+  homepage "https://www.messenger.com/desktop"
+
+  app "Messenger.app"
+end
+
