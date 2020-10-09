e736dc9b54451212a4be9d4672fa9b1470535a6f
matanelgabsi <matan.elgabsi@gmail.com>
2020-10-09 11:06:53 +0300

    Update zeplin from 3.9,1010 to 3.10,1016 (#90573)

--- Casks/zeplin.rb
+++ Casks/zeplin.rb
@@ -1,6 +1,6 @@
 cask "zeplin" do
-  version "3.9,1010"
-  sha256 "f3bdc1ba9001f7f78c1471c96b673ff77a4d8be99910beab8c521c717418828f"
+  version "3.10,1016"
+  sha256 "9ffc3e4ad1012d254b8b500ccf2359b70f82d04ba71080f4ccb0de59cf0afb20"
 
   url "https://api.zeplin.io/urls/download-mac"
   appcast "https://rink.hockeyapp.net/api/2/apps/8926efffe734b6d303d09f41d90c34fc"
