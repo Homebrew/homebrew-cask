0ac4c2c83af164c28722d3c06452f89c4fc38bcc
Tom Matthews <trcm@pm.me>
2020-10-10 13:15:03 +0100

    Add Recaf v2.11.0

---
+++ Casks/recaf.rb
@@ -0,0 +1,44 @@
+cask "recaf" do
+  version "2.11.0"
+  sha256 :no_check
+
+  # github.com/Col-E/Recaf was verified as official when first introduced to the cask
+  url "https://github.com/Col-E/Recaf/releases/download/#{version}/recaf-#{version}-J8-jar-with-dependencies.jar"
+  appcast "https://github.com/Col-E/Recaf/releases.atom"
+  name "Recaf"
+  desc "Java bytecode editor that simplifies editing compiled Java applications"
+  homepage "https://www.coley.software/Recaf/"
+
+  container type: :naked
+
+  auto_updates true
+
+  # Renamed for clarity: jar file contains necessary JavaFX dependancies
+  app "recaf-#{version}-J8-jar-with-dependencies.jar", target: "Recaf.jar"
+
+
+  # Ideally, we disable auto-updates with this preference
+  postflight do
+    FileUtils.mkdir_p ENV["HOME"]+"/Library/Preferences/Recaf/config"
+
+    IO.write "#{ENV["HOME"]}/Library/Preferences/Recaf/config/update.json", <<~EOS
+      {
+        "update.active": false,
+        "update.lastcheck": 1600000000000,
+        "update.frequency": "WEEKLY"
+      }
+    EOS
+
+    IO.write "#{ENV["HOME"]}/Library/Preferences/Recaf/config/backend.json", <<~EOS
+      {
+        "backend.firsttime": false
+      }
+    EOS
+  end
+
+  caveats do
+    depends_on_java "8+"
+  end
+
+  zap trash: "~/Library/Preferences/Recaf"
+end
