cabc986d485a3bfa846de85efcac5598cd35b031
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-10 02:29:37 +0000

    Update multitouch from 1.18.7 to 1.19 (#90604)

--- Casks/multitouch.rb
+++ Casks/multitouch.rb
@@ -1,6 +1,6 @@
 cask "multitouch" do
-  version "1.18.7"
-  sha256 "ebd73b0d4be9c5ca9a449c94da9b1b33f4ac7ba3a62e8a7dfcebdfc8ba114e85"
+  version "1.19"
+  sha256 "b62bd542846434d4acae6eb2c27bb2643a6e0c0f9c57d6f15dbf7255eb95d1ae"
 
   url "https://multitouch.app/downloads/multitouch#{version}.dmg"
   appcast "https://www.multitouch.app/downloads/updates.xml"
