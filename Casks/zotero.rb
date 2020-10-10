d96b688b0b8f5227aa30e1a7f7de2ad86752cabd
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-10 16:58:45 +0000

    Update zotero from 5.0.90 to 5.0.91 (#90662)

--- Casks/zotero.rb
+++ Casks/zotero.rb
@@ -1,6 +1,6 @@
 cask "zotero" do
-  version "5.0.90"
-  sha256 "26c3d59834b81d46cff3fd53791e2d758a971943102d21093d0b8b23b18f77f9"
+  version "5.0.91"
+  sha256 "0c76e4ffb2ad79a96922d5ea3cb5533d8b76a0a902b99baf02817a8a79a1eff8"
 
   url "https://download.zotero.org/client/release/#{version}/Zotero-#{version}.dmg"
   appcast "https://www.zotero.org/download/"
