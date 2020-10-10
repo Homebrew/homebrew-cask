b07643e158d2f9029fed41e185e67eebc3e759ed
suschizu <brigitte@corecode.io>
2020-10-10 04:18:14 +0200

    Update airtame (#90594)

--- Casks/airtame.rb
+++ Casks/airtame.rb
@@ -3,8 +3,9 @@ cask "airtame" do
   sha256 "44ae3bbfe967e086ccc7d37247b3e549ee7d1c7866f01caeec9a4713f0389b4b"
 
   url "https://downloads-cdn.airtame.com/app/latest/mac/Airtame-#{version}.dmg"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://downloads-cdn.airtame.com/get.php?platform=osx_x64"
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://downloads-website.airtame.com/get.php?platform=mac"
   name "Airtame"
+  desc "Wireless screen sharing platform"
   homepage "https://airtame.com/"
 
   auto_updates true
