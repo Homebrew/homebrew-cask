7b21409df629a407267a6d4d375625c429e909a6
suschizu <brigitte@corecode.io>
2020-10-11 19:05:02 +0200

    Update activedock.rb

--- Casks/activedock.rb
+++ Casks/activedock.rb
@@ -2,7 +2,7 @@ cask "activedock" do
   version "2.56,2056"
   sha256 "1d87b5f3b3e0e8e04e559a22a9bda092c9244b78e182d1bd8d67a993a6d9b07b"
 
-  url "https://noteifyapp.com/download/ActiveDock.dmg"
+  url "https://macplus-software.com/downloads/ActiveDock.zip"
   appcast "https://macplus-software.com/downloads/ActiveDock.xml"
   name "ActiveDock"
   desc "Customizable dock, application launcher, dock replacement"
