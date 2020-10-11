bb84985f5b0759b1e3dbd2128e4b6931aa7edece
Work <brigitte@corecode.io>
2020-10-11 18:48:27 +0200

    Update jalbum from 22.0 to 22.1

--- Casks/jalbum.rb
+++ Casks/jalbum.rb
@@ -1,5 +1,5 @@
 cask "jalbum" do
-  version "22.0"
+  version "22.1"
   sha256 "badddb6b6575bce1748fbac62fdaf7ae251822eb54f64686df6dde5164c03bf8"
 
   url "https://download.jalbum.net/download/#{version.major}/MacOSX/jAlbum.dmg"
