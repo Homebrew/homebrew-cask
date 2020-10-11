867ec39eea982b85f49888c46afa58e4311db223
Work <brigitte@corecode.io>
2020-10-11 18:47:07 +0200

    Update foxitreader from 4.0.0 to 4.1.0

--- Casks/foxitreader.rb
+++ Casks/foxitreader.rb
@@ -1,8 +1,8 @@
 cask "foxitreader" do
-  version "4.0.0"
-  sha256 "56ada239bdd4ca321581dec57f55d95f916f1b9a93321c4cda8090b82979a43a"
+  version "4.1.0"
+  sha256 "78da8baf457be4efeee5445d196169dc142ba844d70f41b8b2439184ce14c4de"
 
-  url "https://cdn09.foxitsoftware.com/pub/foxit/reader/desktop/mac/#{version.major}.x/#{version.major_minor}/ML/FoxitReader#{version.no_dots}.setup.pkg"
+  url "https://cdn01.foxitsoftware.com/pub/foxit/reader/desktop/mac/#{version.major}.x/#{version.major_minor}/ML/FoxitReader#{version.no_dots}.L10N.Setup.pkg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.foxitsoftware.com/downloads/latest.html%3Fproduct%3DFoxit-Reader%26platform%3DMac-OS-X",
           must_contain: version.no_dots
   name "Foxit Reader"
