546d3f1c54fde0b3b8663fc6fee11a2e1b59536e
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-12 05:52:10 +0800

    Update activedock.rb

--- Casks/activedock.rb
+++ Casks/activedock.rb
@@ -2,6 +2,7 @@ cask "activedock" do
   version "2.56,2056"
   sha256 "c8a5c5bdceec8f0623e02250d3557ca5367a1efe3204a2711249c66f1311bbd4"
 
+  # macplus-software.com/ was verified as official when first introduced to the cask
   url "https://macplus-software.com/downloads/ActiveDock.zip"
   appcast "https://macplus-software.com/downloads/ActiveDock.xml"
   name "ActiveDock"
