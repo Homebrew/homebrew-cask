776b39bc30913ea31ed0de3b4e4dd2710ce54562
秦新 <qinxin_0720@live.com>
2020-10-12 14:28:25 +0800

    Update flutter from 1.22.0 to 1.22.1

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -1,6 +1,6 @@
 cask "flutter" do
-  version "1.22.0"
-  sha256 "d9663e5fe5fe6ee44ce04dd43d9626fc14db16c3b8d0a6e8ae666f5e6cf51ca4"
+  version "1.22.1"
+  sha256 "04f5b8a1af40814ff496d48c7a71fa8ff10ef396fca6a9feb2d231a3f7776b9a"
 
   # storage.googleapis.com/flutter_infra/ was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_#{version}-stable.zip"
