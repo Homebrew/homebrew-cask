0ae0fdb52f7ff10c0c3d497e97a66ed12529cf90
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-12 23:27:40 +0000

    Update navicat-for-oracle from 15.0.21 to 15.0.22 (#90727)

--- Casks/navicat-for-oracle.rb
+++ Casks/navicat-for-oracle.rb
@@ -1,6 +1,6 @@
 cask "navicat-for-oracle" do
-  version "15.0.21"
-  sha256 "e4daad3bd9d56258b93a4a0e8c86279b1e1f60e2f9ba1e833a0611150a8687f2"
+  version "15.0.22"
+  sha256 "60587c883cde2a36875bbb1e7b45690046cec463f226dcbe32d70cfd305f1916"
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_ora_en.dmg"
   appcast "https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20for%20Oracle&appLang=en"
