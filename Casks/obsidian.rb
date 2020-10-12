6772a5d02dcc1986e4acba18ff0fb245dff96167
Randall <ran.dall@icloud.com>
2020-10-12 15:57:44 -0700

    Update obsidian from 0.9.2 to 0.9.3

--- Casks/obsidian.rb
+++ Casks/obsidian.rb
@@ -1,6 +1,6 @@
 cask "obsidian" do
-  version "0.9.2"
-  sha256 "5aebad9378b1f0c125345f1215e1c1a4c31fbe93923c545d9c084ac055ad97d5"
+  version "0.9.3"
+  sha256 "1c2c0795b349f0acda2aab406f65442a389d67e897276d27fadf95c19430cce2"
 
   # github.com/obsidianmd/ was verified as official when first introduced to the cask
   url "https://github.com/obsidianmd/obsidian-releases/releases/download/v#{version}/Obsidian-#{version}.dmg"
