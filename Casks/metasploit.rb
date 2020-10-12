5f52515549cc2e1e19235214384620b7be654d2f
bubblecup <bubblecup@users.noreply.github.com>
2020-10-12 18:06:42 -0400

    Update metasploit from 6.0.9,20200928102826 to 6.0.11,20201012102846 (#90708)

--- Casks/metasploit.rb
+++ Casks/metasploit.rb
@@ -1,10 +1,11 @@
 cask "metasploit" do
-  version "6.0.9,20200928102826"
-  sha256 "4a28e3f3c77fb778014076afa2687e79827b12a88f6e0f2f3925eb5b9b4ed5bf"
+  version "6.0.11,20201012102846"
+  sha256 "90b21ed0e71ebdb4eccf9c1c524a5cfb022dc8a51ea83881d7da72223cfbab6a"
 
   url "https://osx.metasploit.com/metasploit-framework-#{version.before_comma}+#{version.after_comma}-1rapid7-1.pkg"
   appcast "https://osx.metasploit.com/LATEST"
   name "Metasploit Framework"
+  desc "Penetration testing framework"
   homepage "https://www.metasploit.com/"
 
   depends_on formula: "nmap"
