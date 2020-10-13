765a7a1d92196fdaf01e0962f6a93a8f1af6997f
Chris Varenhorst <varenc@gmail.com>
2020-10-13 15:48:21 -0700

    Update timer

--- Casks/timer.rb
+++ Casks/timer.rb
@@ -4,6 +4,7 @@ cask "timer" do
 
   url "https://www.apimac.com/download/Timer.zip"
   name "Timer"
+  desc "Stopwatch, alarm clock,  and clock utility"
   homepage "https://www.apimac.com/mac/timer/"
 
   app "Timer.app"
@@ -11,5 +12,7 @@ cask "timer" do
   zap trash: [
     "~/Library/Preferences/Apimac",
     "~/Library/Application Support/Apimac",
+    "~/Library/Preferences/com.apimac.TimerMac.pdata",
+    "~/Library/Preferences/com.apimac.Timer.plist",
   ]
 end
