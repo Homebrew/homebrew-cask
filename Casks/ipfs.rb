cb87612d52fbc7ffca98d5deb05e2c424f8a892d
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-13 08:56:50 +0800

    Update ipfs.rb

--- Casks/ipfs.rb
+++ Casks/ipfs.rb
@@ -1,8 +1,8 @@
 cask "ipfs" do
-  version "0.13.1"
-  sha256 "27e7cdf7521cf500b6d5d793910076715e7aa386139a1169581bdd3041c5793c"
+  version "0.13.2"
+  sha256 "e05caeaf457d994ab6b4ce25f6e2572b227f632f378c63a40cfbf5bfd9bf5efd"
 
-  url "https://github.com/ipfs-shipyard/ipfs-desktop/releases/download/v#{version}/ipfs-desktop-#{version}.dmg"
+  url "https://github.com/ipfs-shipyard/ipfs-desktop/releases/download/v#{version}/IPFS-Desktop-#{version}.dmg"
   appcast "https://github.com/ipfs-shipyard/ipfs-desktop/releases.atom"
   name "IPFS Desktop"
   desc "Menu bar application for the IPFS peer-to-peer network"
