60a13d59b8632e43814d4baf19c920238e4ef115
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-13 00:07:51 +0000

    Update alfaview from 8.9.1 to 8.10.0 (#90745)

--- Casks/alfaview.rb
+++ Casks/alfaview.rb
@@ -1,6 +1,6 @@
 cask "alfaview" do
-  version "8.9.1"
-  sha256 "5155cfdd96dc2a1c9dc85bc04259c9bdee121305f67265a4aac622548bf65bf6"
+  version "8.10.0"
+  sha256 "c8ff923d7bde8f7292287c4d1f5e3520d5d757c676c2eaf3406add1f880e94a4"
 
   url "https://assets.alfaview.com/stable/mac/alfaview-mac-production-#{version}.dmg"
   appcast "https://production-alfaview-assets.alfaview.com/stable/mac/version.info"
