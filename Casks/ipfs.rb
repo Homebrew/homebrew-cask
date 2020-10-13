e42ef3696fe328d8ae5e9dcecb40994e307b6091
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-13 01:24:48 +0000

    Update ipfs from 0.13.0 to 0.13.2 (#90736)

--- Casks/ipfs.rb
+++ Casks/ipfs.rb
@@ -1,8 +1,8 @@
 cask "ipfs" do
-  version "0.13.0"
-  sha256 "8927be0f9d7decd4c7def13472e69a7ced50f7d086b78dac29b3a3f4da04438d"
+  version "0.13.2"
+  sha256 "e05caeaf457d994ab6b4ce25f6e2572b227f632f378c63a40cfbf5bfd9bf5efd"
 
-  url "https://github.com/ipfs-shipyard/ipfs-desktop/releases/download/v#{version}/ipfs-desktop-#{version}.dmg"
+  url "https://github.com/ipfs-shipyard/ipfs-desktop/releases/download/v#{version}/IPFS-Desktop-#{version}.dmg"
   appcast "https://github.com/ipfs-shipyard/ipfs-desktop/releases.atom"
   name "IPFS Desktop"
   desc "Menu bar application for the IPFS peer-to-peer network"
