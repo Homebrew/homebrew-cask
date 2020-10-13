11cc757992322a3254b9f29186595e243ee19194
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-13 23:00:38 +0000

    Update timeular from 3.4.2 to 3.5.0 (#90816)

--- Casks/timeular.rb
+++ Casks/timeular.rb
@@ -1,6 +1,6 @@
 cask "timeular" do
-  version "3.4.2"
-  sha256 "cb638867c91ff3ca22644683a0a59d8dc6a7f08bf6b9af9c0b94d512da0096a1"
+  version "3.5.0"
+  sha256 "8632722c809f203e7139eb8c363477782185ed31dcc6df3942b52cc7af6b5cba"
 
   # timeular-desktop-packages.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://timeular-desktop-packages.s3.amazonaws.com/mac/production/Timeular-#{version}.dmg"
