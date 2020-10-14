59c465bea5c1a2affa739bef8e0c82c1d88c1be9
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-14 09:14:04 +0800

    Update gactions.rb

--- Casks/gactions.rb
+++ Casks/gactions.rb
@@ -1,11 +1,11 @@
 cask "gactions" do
-  version :latest
-  sha256 :no_check
+  version "3"
+  sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://dl.google.com/gactions/v3/release/gactions-sdk_darwin.tar.gz"
+  url "https://dl.google.com/gactions/v#{version}/release/gactions-sdk_darwin.tar.gz"
   name "gactions"
   desc "CLI tool for Google Actions SDK"
-  homepage "https://developers.google.com/assistant/conversational/overview"
+  homepage "https://developers.google.com/assistant/conversational"
 
   binary "aog_cli/gactions"
 end
