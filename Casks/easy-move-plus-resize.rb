bb81b2dcd478607703fa365e6fe3f2ec8d93b70d
suschizu <brigitte@corecode.io>
2020-10-15 00:31:51 +0200

    Update easy-move-plus-resize from 1.3.0 to 1.4.0 (#90843)
    * Update easy-move-plus-resize from 1.3.0 to 1.4.0
    
    * Update easy-move-plus-resize.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/easy-move-plus-resize.rb
+++ Casks/easy-move-plus-resize.rb
@@ -1,12 +1,12 @@
 cask "easy-move-plus-resize" do
-  version "1.3.0"
-  sha256 "da8c7c8e365c348f06c80cdc075271d85d16a954bf5f9494786dcb8251bc27b6"
+  version "1.4.0"
+  sha256 "b68ec37dd8243f20a79d4c80b66bd1fad047074e23f05b22720e2088c4ac6ed1"
 
-  url "https://github.com/dmarcotte/easy-move-resize/releases/download/#{version}/Easy.Move.Resize.app.zip"
+  url "https://github.com/dmarcotte/easy-move-resize/releases/download/#{version}/Easy.Move+Resize.app.zip"
   appcast "https://github.com/dmarcotte/easy-move-resize/releases.atom"
   name "Easy Move+Resize"
   desc "Utility to support moving and resizing using a modifier key and mouse drag"
   homepage "https://github.com/dmarcotte/easy-move-resize"
 
-  app "easy-move-resize/Easy Move+Resize.app"
+  app "Easy Move+Resize.app"
 end
