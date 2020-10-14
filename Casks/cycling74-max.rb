8c7208ae002fcece74168afa819a908a7b4b56c6
Randall <ran.dall@icloud.com>
2020-10-14 09:21:39 -0700

    Update cycling74-max from 8.1.6_200922 to 8.1.7_201013

--- Casks/cycling74-max.rb
+++ Casks/cycling74-max.rb
@@ -1,6 +1,6 @@
 cask "cycling74-max" do
-  version "8.1.6_200922"
-  sha256 "231c2b9e7a2a2e54723d7d1588e13c10976442784a1d65e9b423a88b12ed5c88"
+  version "8.1.7_201013"
+  sha256 "ef1d48a14baedffeef3a8be2b5b7cc0aa5c94f2bc83c7475389ca6e411b5cc52"
 
   # akiaj5esl75o5wbdcv2a-maxmspjitter.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://akiaj5esl75o5wbdcv2a-maxmspjitter.s3.amazonaws.com/Max#{version.no_dots}.dmg"
