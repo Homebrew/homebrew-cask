cb564ef0c9a5fdb818595a4fa20df9b0e333cd0f
Work <brigitte@corecode.io>
2020-10-14 15:56:30 +0200

    Update reunion from 12.0.0.200602 to 12.0.0.201013n

--- Casks/reunion.rb
+++ Casks/reunion.rb
@@ -1,6 +1,6 @@
 cask "reunion" do
-  version "12.0.0.200602"
-  sha256 "5ec2e7eb77df874ba889dadd1fa7cbff898708f02a862ed726a605ace69c9dab"
+  version "12.0.0.201013n"
+  sha256 "e0cdc703547393a21c931ec85d8a69699ed2f7cc10926c37060fb4659341d96c"
 
   url "https://store.leisterpro.com/updates/reunion#{version.major}/Reunion-#{version.dots_to_hyphens}.zip"
   appcast "https://store.leisterpro.com/updates/reunion#{version.major}/appcast.xml",
