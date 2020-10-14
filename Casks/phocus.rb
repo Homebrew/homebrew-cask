7b106d0859bc564667f30272d0606a7aa44225f2
Randall <ran.dall@icloud.com>
2020-10-14 09:10:01 -0700

    Update phocus from 3.5.2 to 3.5.3

--- Casks/phocus.rb
+++ Casks/phocus.rb
@@ -1,6 +1,6 @@
 cask "phocus" do
-  version "3.5.2"
-  sha256 "e0d35063ce9eb0bfaa5052a1925a2c60b25a759593d611fde0a6bf978bc8f67c"
+  version "3.5.3"
+  sha256 "90c2993af5d2ebef6246dcc2695d1348648f75f97632df64ea5651cb6e6ddb26"
 
   url "https://cdn.hasselblad.com/software/Phocus-for-Mac/#{version}/Phocus-#{version}.dmg"
   name "Hasselblad Phocus"
