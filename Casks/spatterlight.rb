24aa78610faf3fc166836a57de6ca7f41ee63ad0
Work <brigitte@corecode.io>
2020-10-14 16:02:30 +0200

    Update spatterlight from 0.5.14b to 0.5.5

--- Casks/spatterlight.rb
+++ Casks/spatterlight.rb
@@ -1,9 +1,9 @@
 cask "spatterlight" do
-  version "0.5.14b"
-  sha256 "7fb7b000b88f34e7ee6d6e53ab588dc6f3f58002a5e7c2c47e8ff164b9fb67bb"
+  version "0.5.5"
+  sha256 "0893a6cc4f2403334df53c8e01588db57aec08296b03202b31424ee0af3100a7"
 
   # github.com/angstsmurf/spatterlight/ was verified as official when first introduced to the cask
-  url "https://github.com/angstsmurf/spatterlight/releases/download/#{version}/Spatterlight_Catalina.zip"
+  url "https://github.com/angstsmurf/spatterlight/releases/download/v#{version}/Spatterlight_Catalina.zip"
   appcast "https://github.com/angstsmurf/spatterlight/releases.atom"
   name "Spatterlight"
   desc "Play most kinds of interactive fiction game files"
