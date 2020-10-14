1bd1c56a32b3453122afe35fb7e2e931b4e6081d
suschizu <brigitte@corecode.io>
2020-10-14 16:30:14 +0200

    Update camtasia from 2020.0.8 to 2020.0.9 (#90847)

--- Casks/camtasia.rb
+++ Casks/camtasia.rb
@@ -1,6 +1,6 @@
 cask "camtasia" do
-  version "2020.0.8"
-  sha256 "6377c4d5934514b713c9f58a7e6180408c143bf136f7aeb52a718b60788bafb7"
+  version "2020.0.9"
+  sha256 "ab9ddf8c8916edd426554339ff859f6252dff406d3a81b095c09127ed5a67424"
 
   url "https://download.techsmith.com/camtasiamac/releases/Camtasia.dmg"
   appcast "https://support.techsmith.com/hc/en-us/articles/115006624748-Camtasia-Mac-Version-History"
