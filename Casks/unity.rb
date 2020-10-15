231f3cffca2e2ea083493ed2e807d71525781b65
Robert Blair <12276457+robertleeblairjr@users.noreply.github.com>
2020-10-14 20:57:04 -0500

    Update unity from 2020.1.8f1,22e8c0b0c3ec to 2020.1.9f1,145f5172610f (#90890)

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -1,6 +1,6 @@
 cask "unity" do
-  version "2020.1.8f1,22e8c0b0c3ec"
-  sha256 "a2b6c818807bb17cfe0ec032bf074601d573d9e8e1b84c91acd52d6369698c8b"
+  version "2020.1.9f1,145f5172610f"
+  sha256 "5dd076f370a0dc42d28c4daab2e8a6da5c4f6d2c596fe07124fdf11997561bff"
 
   # download.unity3d.com/download_unity/ was verified as official when first introduced to the cask
   url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorInstaller/Unity-#{version.before_comma}.pkg"
