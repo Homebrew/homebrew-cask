92c7aad9d6e304848b785fe195a6d3e39e052396
Evan <evan@moderncsv.com>
2020-10-15 06:11:52 -0500

    Fix style issues with Modern CSV.app cask

--- Casks/modern-csv.rb
+++ Casks/modern-csv.rb
@@ -3,12 +3,12 @@ cask "modern-csv" do
   sha256 "3803503571e1e82f386ff5bef20972d9f99779f540e222c2b88328f86aa35f5f"
 
   url "https://www.moderncsv.com/release/ModernCSV-Mac-v#{version}.dmg"
-  appcast 'https://www.moderncsv.com/latest-version/'
+  appcast "https://www.moderncsv.com/latest-version/"
   name "Modern CSV"
   desc "CSV editor"
   homepage "https://www.moderncsv.com/"
 
   app "Modern CSV.app"
 
-  zap trash: ['~/Library/Application Support/Modern CSV',]
+  zap trash: ["~/Library/Application Support/Modern CSV"]
 end
