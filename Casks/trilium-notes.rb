9a7f6bc77fb9132639b10b2a031e912bad380d18
suschizu <brigitte@corecode.io>
2020-10-15 13:57:00 +0200

    Update trilium-notes from 0.44.6 to 0.44.8 (#90900)

--- Casks/trilium-notes.rb
+++ Casks/trilium-notes.rb
@@ -1,6 +1,6 @@
 cask "trilium-notes" do
-  version "0.44.6"
-  sha256 "d7be79ee6d4864859a9a0e21c8d9c2e90033c2c9655bfd52e8dde2808b552752"
+  version "0.44.8"
+  sha256 "556f6d7a131e1ea520f68e3c00c5fa047e317616709bf63661ed3cb219064be1"
 
   url "https://github.com/zadam/trilium/releases/download/v#{version}/trilium-mac-x64-#{version}.zip"
   appcast "https://github.com/zadam/trilium/releases.atom"
