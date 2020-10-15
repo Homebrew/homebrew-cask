4bd5e46eb192cb354721f207a9e45cf325069b61
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-15 17:51:40 +0000

    Update datovka from 4.15.4 to 4.15.5 (#90937)

--- Casks/datovka.rb
+++ Casks/datovka.rb
@@ -1,6 +1,6 @@
 cask "datovka" do
-  version "4.15.4"
-  sha256 "a56bc8b1b5d50c3560858793b7c9053ca97c10a52f9e0f2375322a680b965987"
+  version "4.15.5"
+  sha256 "f6d48c5911272195d91cffbed4519aed25325073c38453252d236026ce5e1eaf"
 
   # secure.nic.cz/files/datove_schranky/ was verified as official when first introduced to the cask
   url "https://secure.nic.cz/files/datove_schranky/#{version}/datovka-#{version}-64bit-macos10.12.dmg"
