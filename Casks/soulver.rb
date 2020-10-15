76bcc2e1ccd2f04addbc1cafda41eacc46c6e8a8
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-15 17:23:25 +0000

    Update soulver from 3.4.8-136 to 3.4.9-138 (#90923)

--- Casks/soulver.rb
+++ Casks/soulver.rb
@@ -1,6 +1,6 @@
 cask "soulver" do
-  version "3.4.8-136"
-  sha256 "d55741bda15a94960966a231426dbbd1867bb475949982998d656e462c55f86d"
+  version "3.4.9-138"
+  sha256 "f3bb76180892bbe87cceccb3b66c7e0619a8f630c6a331a8898875dbe9535464"
 
   url "https://soulver.app/mac/sparkle/soulver-#{version}.zip"
   appcast "https://soulver.app/mac/sparkle/appcast.xml"
