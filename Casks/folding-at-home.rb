94287e733120a3300610296daecf6cc5c156578f
Randall <ran.dall@icloud.com>
2020-10-16 11:52:59 -0700

    Update folding-at-home from 7.6.13 to 7.6.20

--- Casks/folding-at-home.rb
+++ Casks/folding-at-home.rb
@@ -1,6 +1,6 @@
 cask "folding-at-home" do
-  version "7.6.13"
-  sha256 "ee96f4f1b724db46f9a37f7d805095a16b24f1ef7e9b9a1615a56e94e4119a36"
+  version "7.6.20"
+  sha256 "e518f2c1886f5e132f67b8658263b295ac7091b4ff56eb301148f8c4d008d357"
 
   url "https://download.foldingathome.org/releases/public/release/fah-installer/osx-10.11-64bit/v#{version.major_minor}/fah-installer_#{version}_x86_64.mpkg.zip"
   appcast "https://download.foldingathome.org/js/fah-downloads.js"
