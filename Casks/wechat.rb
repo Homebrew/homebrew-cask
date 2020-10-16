c1bef8b636e10c0d198078d6bc63145184bc216f
Vítor Galvão <info@vitorgalvao.com>
2020-10-16 16:17:23 +0100

    Update wechat.rb

--- Casks/wechat.rb
+++ Casks/wechat.rb
@@ -1,13 +1,17 @@
 cask "wechat" do
-  version :latest
-  sha256 :no_check
+  version "2.5.0.17"
+  sha256 "423289c09531ba742c5f2cc5c0d87fe981592d1edfbef6fc94ddd83875be76cd"
 
   url "https://dldir1.qq.com/weixin/mac/WeChatMac.dmg"
+  # There is an appcast at https://dldir1.qq.com/weixin/mac/mac-release.xml,
+  # but it’s slower to update than the submissions we get. See:
+  #   https://github.com/Homebrew/homebrew-cask/pull/90907#issuecomment-710107547
   name "WeChat for Mac"
   name "微信 Mac 版"
   desc "Free messaging and calling application"
   homepage "https://mac.weixin.qq.com/"
 
+  auto_updates true
   depends_on macos: ">= :yosemite"
 
   app "WeChat.app"
