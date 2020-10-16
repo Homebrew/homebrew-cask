5de9433ff04cd7714d79726697550aaea0151d20
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-16 18:55:00 +0000

    Update blizz from 15.10.6 to 15.11.2 (#90995)

--- Casks/blizz.rb
+++ Casks/blizz.rb
@@ -1,6 +1,6 @@
 cask "blizz" do
-  version "15.10.6"
-  sha256 "6bceff792ef1e4af27b6be31a9c542f757dc0428fee92ae1b62f49740490f2b3"
+  version "15.11.2"
+  sha256 "1f7020033e670e7feb4f76090883628a5ad943be3179c4d94f7b312acfb649be"
 
   # dl.teamviewer.com/ was verified as official when first introduced to the cask
   url "https://dl.teamviewer.com/blizz/installer/mac/#{version}/Blizz.dmg"
