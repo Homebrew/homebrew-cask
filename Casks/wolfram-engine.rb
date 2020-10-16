023713c8fe4baa682fbabf339954349db6862507
ConnorGray <accounts@connorgray.com>
2020-10-16 10:42:22 -0500

    Add initial (incomplete) wolfram-engine.rb cask

---
+++ Casks/wolfram-engine.rb
@@ -0,0 +1,24 @@
+
+cask "wolfram-engine" do
+  version "12.1.1"
+  sha256 "fe43def3b2828286ffae0fee8ee82227322751d68467d4a60d2283be6df41fe3"
+
+  # url "http://buildmonitor.wolfram.com/artifact/download/6982007/Files/M-OSX-WolframEngine-12.1.1-6982007.dmg"
+  url "file:///Users/connorgray/Desktop/WolframEngine-12.1.1.dmg"
+  name "Wolfram Engine"
+  desc "Evaluator for the Wolfram Language"
+  homepage "https://www.wolfram.com/engine/"
+
+  depends_on macos: ">= :sierra"
+
+  app "Wolfram Engine.app"
+
+  zap trash: [
+    "~/Library/WolframEngine",
+    "~/Library/Caches/Wolfram"
+  ]
+
+  caveats do
+    free_license "https://www.wolfram.com/engine/free-license/"
+  end
+end
