0f381381cf0281280d307ccaa1061c1560eff594
Randall <ran.dall@icloud.com>
2020-10-16 11:16:24 -0700

    Update mamp from 6.0 to 6.0.1

--- Casks/mamp.rb
+++ Casks/mamp.rb
@@ -1,6 +1,6 @@
 cask "mamp" do
-  version "6.0"
-  sha256 "8b5a5d452c35bcb62e37eb50918126b75e98699b015355775940515af555b476"
+  version "6.0.1"
+  sha256 "dda16a6fbe7a431d67ed5e3cf403336a1ca646efaa9b0639cbb58648a1dbb5b1"
 
   url "https://downloads.mamp.info/MAMP-PRO/releases/#{version}/MAMP_MAMP_PRO_#{version}.pkg"
   appcast "https://www.mamp.info/en/downloads/"
