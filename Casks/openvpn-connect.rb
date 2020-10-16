ec3d8ae1fedc52b2feb8ace94e35645843d12579
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-16 09:20:54 +0800

    Update openvpn-connect.rb

--- Casks/openvpn-connect.rb
+++ Casks/openvpn-connect.rb
@@ -5,11 +5,15 @@ cask "openvpn-connect" do
   url "https://swupdate.openvpn.net/downloads/connect/openvpn-connect-#{version.before_comma}.#{version.after_comma}_signed.dmg"
   appcast "https://openvpn.net/client-connect-vpn-for-mac-os/"
   name "OpenVPN Connect client"
+  desc "Client program for the OpenVPN Access Server"
   homepage "https://openvpn.net/client-connect-vpn-for-mac-os/"
 
   pkg "OpenVPN_Connect_#{version.before_comma.dots_to_underscores}(#{version.after_comma})_Installer_signed.pkg"
 
-  uninstall launchctl: "org.openvpn.client",
+  uninstall launchctl: [
+    "org.openvpn.client",
+    "org.openvpn.helper",
+  ],
             script:    {
               executable: "/Applications/OpenVPN Connect/Uninstall OpenVPN Connect.app/Contents/Resources/remove.sh",
               sudo:       true,
@@ -19,5 +23,7 @@ cask "openvpn-connect" do
               "org.openvpn.client_framework.pkg",
               "org.openvpn.client_launch.pkg",
               "org.openvpn.client_uninstall.pkg",
+              "org.openvpn.helper_framework.pkg",
+              "org.openvpn.helper_launch.pkg",
             ]
 end
