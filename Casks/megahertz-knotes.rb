32ac9691d7e1acfa05d0456542200c567443143f
suschizu <brigitte@corecode.io>
2020-10-17 20:14:21 +0200

    Revert "Update megahertz-knotes from 2.4.0 to 2.6.0 (#90476)"
    This reverts commit 695ffca19aa6a1c183073162f54b55a35fa7a699.

--- Casks/megahertz-knotes.rb
+++ Casks/megahertz-knotes.rb
@@ -1,15 +1,14 @@
 cask "megahertz-knotes" do
-  version "2.6.0"
-  sha256 "5946b48b0e6ac588331336f760711c11c53ef9b0030616f39db6e9a41551d434"
+  version "2.4.0"
+  sha256 "2c15b74006e17a04603cf7d8a802636d4e8fbbd1078ffd4a216f6eadc2d87f67"
 
-  # knotes2.s3-ap-northeast-1.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://knotes2.s3-ap-northeast-1.amazonaws.com/download/mac/Knotes-#{version}.dmg"
+  url "http://cdn.knotesapp.cn/download/%e7%b3%af%e8%af%8d%e7%ac%94%e8%ae%b0-#{version}.dmg"
   appcast "https://knotes#{version.major}-release-cn.s3.amazonaws.com/mac/latest-mac.yml"
   name "Knotes"
-  desc "Reading notes management tool"
-  homepage "https://knotesapp.com/"
+  name "糯词笔记"
+  homepage "https://knotesapp.cn/"
 
-  app "Knotes.app"
+  app "糯词笔记.app"
 
   zap trash: [
     "~/Library/Application Support/Knotes*",
