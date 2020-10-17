57cde4666cb14c74bed00d2d4256312b089b80e6
Meet Godhani <meetgodhani23@gmail.com>
2020-10-17 12:24:07 -0400

    Update raven-reader from 1.0.4 to 1.0.5 (#91013)

--- Casks/raven-reader.rb
+++ Casks/raven-reader.rb
@@ -1,6 +1,6 @@
 cask "raven-reader" do
-  version "1.0.4"
-  sha256 "1eb862bd8e405fd3554e5ab2e0b3c80dc176f00b4a934edb9339307e154f3fbd"
+  version "1.0.5"
+  sha256 "0063367c9f859d1548fd6618389cfbb0358109d20d56302dc0087053c386e0cd"
 
   # download.helloefficiency.com/ravenreader/ was verified as official when first introduced to the cask
   url "https://download.helloefficiency.com/ravenreader/Raven%20Reader-#{version}.dmg"
