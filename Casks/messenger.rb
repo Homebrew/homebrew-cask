df1be89d5064465655f8a38757d66c4f79e1795a
Vítor Galvão <info@vitorgalvao.com>
2020-10-17 01:30:51 +0100

    Update messenger.rb

--- Casks/messenger.rb
+++ Casks/messenger.rb
@@ -2,7 +2,7 @@ cask "messenger" do
   version "73.2.121,249985498"
   sha256 "104ee1faf4b00d6c77b9500ddd601f6388f25dd781e5dbb5a2c0b73cce68eaa0"
 
-  # www.facebook.com/messenger/desktop/ was verified as official when first introduced to the cask
+  # facebook.com/messenger/desktop/ was verified as official when first introduced to the cask
   url "https://www.facebook.com/messenger/desktop/update/#{version.after_comma}.zip"
   appcast "https://www.facebook.com/messenger/desktop/update/latest-mac.yml"
   name "Facebook Messenger"
