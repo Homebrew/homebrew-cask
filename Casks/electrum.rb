86d8c559331e821fc48de6c27147b6ea28e641cf
Xander Harris <plebis@hotmail.com>
2020-10-17 10:45:58 -0700

    Correct style errors

--- Casks/electrum.rb
+++ Casks/electrum.rb
@@ -1,19 +1,21 @@
-cask "electrum" do
-  version "4.0.4"
-  sha256 "39f09e721aecec9b324ce1daf4ec5dd5b2403acce552c178bcbe61dc7503e006"
+# frozen_string_literal: true
+
+cask 'electrum' do
+  version '4.0.4'
+  sha256 '39f09e721aecec9b324ce1daf4ec5dd5b2403acce552c178bcbe61dc7503e006'
 
   url "https://download.electrum.org/#{version}/electrum-#{version}.dmg"
-  appcast "https://github.com/spesmilo/electrum/releases.atom"
-  name "Electrum"
-  desc "Bitcoin thin client"
-  homepage "https://electrum.org/"
+  appcast 'https://github.com/spesmilo/electrum/releases.atom'
+  name 'Electrum'
+  desc 'Bitcoin thin client'
+  homepage 'https://electrum.org/'
 
-  app "Electrum.app"
+  app 'Electrum.app'
 
   zap trash: [
-    "~/.electrum",
-    "~/Library/Preferences/Electrum.plist",
-    "~/Library/Preferences/org.org.pythonmac.unspecified.Electrum.plist",
-    "~/Library/Saved Application State/Electrum.savedState",
+    '~/.electrum',
+    '~/Library/Preferences/Electrum.plist',
+    '~/Library/Preferences/org.org.pythonmac.unspecified.Electrum.plist',
+    '~/Library/Saved Application State/Electrum.savedState'
   ]
 end
