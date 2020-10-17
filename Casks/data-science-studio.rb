1d677c5be3a295b7931e51f612030a78c43be853
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-17 18:58:51 +0000

    Update data-science-studio from 8.0.1 to 8.0.2 (#91029)

--- Casks/data-science-studio.rb
+++ Casks/data-science-studio.rb
@@ -1,6 +1,6 @@
 cask "data-science-studio" do
-  version "8.0.1"
-  sha256 "ffcbd822ea5f001ad5b844068170807da95545de679a7ab6195d9287b7f28feb"
+  version "8.0.2"
+  sha256 "eee29aeae730cd036c4cd61b73d3f2079c5cdab70113bff751110b54b7e4c197"
 
   url "https://downloads.dataiku.com/public/studio/#{version}/Data%20Science%20Studio%20#{version}.dmg"
   appcast "https://www.dataiku.com/dss/trynow/mac/"
