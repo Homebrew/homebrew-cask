c4ae8bc0d0b7dbf7bb92e82174f23ca8762ad42d
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-18 15:04:01 +0800

    Update raven-reader.rb

--- Casks/raven-reader.rb
+++ Casks/raven-reader.rb
@@ -6,6 +6,7 @@ cask "raven-reader" do
   url "https://download.helloefficiency.com/ravenreader/Raven%20Reader-#{version}.dmg"
   appcast "https://ridereceiptspro.sfo2.digitaloceanspaces.com/ravenreader/latest-mac.yml"
   name "Raven Reader"
+  desc "News reader with flexible settings"
   homepage "https://ravenreader.app/"
 
   auto_updates true
