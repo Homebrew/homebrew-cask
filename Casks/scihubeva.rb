f35880853e9af1037f0691f60f0c261f4f484bd0
Leo <leo-van@hotmail.com>
2020-10-18 14:12:43 +0800

    Update SciHubEVA from v4.0.1 to v4.1.0
    Update SciHubEVA from v4.0.1 to v4.1.0

--- Casks/scihubeva.rb
+++ Casks/scihubeva.rb
@@ -1,6 +1,6 @@
 cask "scihubeva" do
-  version "v4.0.1"
-  sha256 "5ba4ea891fa3781612d8c47c550920194e44bb0d4f49a724ab111d3aed35ebd0"
+  version "v4.1.0"
+  sha256 "d6feb6c6632209201e8b0f1e1a811fa073238b6414de40269f6ee2cccc29c0ba"
 
   url "https://github.com/leovan/SciHubEVA/releases/download/#{version}/SciHubEVA-#{version}.dmg"
   appcast "https://github.com/leovan/SciHubEVA/releases.atom"
