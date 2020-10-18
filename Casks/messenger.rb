55047907dc937cae2383a2e43db264cb37e37562
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-18 15:08:54 +0800

    Update messenger.rb

--- Casks/messenger.rb
+++ Casks/messenger.rb
@@ -4,6 +4,8 @@ cask "messenger" do
 
   # facebook.com/messenger/desktop/ was verified as official when first introduced to the cask
   url "https://www.facebook.com/messenger/desktop/update/#{version.after_comma}.zip"
+  # The appcast will fail CI due to its Cache-Control settings. See:
+  #   https://github.com/Homebrew/homebrew-cask/pull/90957#issuecomment-710740050
   appcast "https://www.facebook.com/messenger/desktop/update/latest-mac.yml"
   name "Facebook Messenger"
   desc "Native desktop app for Messenger (formerly Facebook Messenger)"
