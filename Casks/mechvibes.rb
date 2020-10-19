0b767f809d42af0fece20ef1ad4ba6d1c25e5d62
Daniel Fink <daniel.fink@student.unimelb.edu.au>
2020-10-19 16:55:21 +1100

    Add Mechvibes v2.1.1

---
+++ Casks/mechvibes.rb
@@ -0,0 +1,13 @@
+cask "mechvibes" do
+  version "2.1.1"
+  sha256 "aad610a9eba39869586eeb14b67c9ea6a29ea532763e413449b2f975e419c82f"
+
+  # github.com/hainguyents13/mechvibes was verified as official when first introduced to the cask
+  url "https://github.com/hainguyents13/mechvibes/releases/download/v#{version}/Mechvibes-#{version}.dmg"
+  appcast "https://github.com/hainguyents13/mechvibes/releases.atom"
+  name "Mechvibes"
+  desc "Lets you play mechanical keyboard sounds as you type on your keyboard"
+  homepage "https://mechvibes.com/"
+
+  app "Mechvibes.app"
+end
