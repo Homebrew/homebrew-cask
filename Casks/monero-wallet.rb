bade3974c1a45f70f623b6ef74b37e58c85ae433
selsta <selsta@sent.at>
2020-10-19 16:06:47 +0200

    Update monero-wallet from 0.17.1.0 to 0.17.1.1

--- Casks/monero-wallet.rb
+++ Casks/monero-wallet.rb
@@ -1,6 +1,6 @@
 cask "monero-wallet" do
-  version "0.17.1.0"
-  sha256 "b9c0cbdc8f9c74d6205858ccb4fb0f1eec792e301aa819bf8aa445a3d17869d3"
+  version "0.17.1.1"
+  sha256 "ebaecd3b4072c1ef22009dd69290208b5c83b9b597de1a1116bc05564a08a916"
 
   url "https://downloads.getmonero.org/gui/monero-gui-mac-x64-v#{version}.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://downloads.getmonero.org/gui/mac64"
