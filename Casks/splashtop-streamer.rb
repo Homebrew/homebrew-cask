e7dbe5510cbc959eb4dc1985dee598e8b9e10986
Randall <ran.dall@icloud.com>
2020-10-19 14:23:58 -0700

    Update splashtop-streamer from 3.4.0.0 to 3.4.2.0

--- Casks/splashtop-streamer.rb
+++ Casks/splashtop-streamer.rb
@@ -1,6 +1,6 @@
 cask "splashtop-streamer" do
-  version "3.4.0.0"
-  sha256 "b0ec92daf4ac3a6b0da2e02f7cb6fdbf1dba456021be7fca5750f8fb3bdc0a4c"
+  version "3.4.2.0"
+  sha256 "7dbd3c0246b6835bd929b17b5f1c2d3c5738feb30a861502930d2d10792ce781"
 
   # d17kmd0va0f0mp.cloudfront.net/ was verified as official when first introduced to the cask
   url "https://d17kmd0va0f0mp.cloudfront.net/mac/Splashtop_Streamer_Mac_INSTALLER_v#{version}.dmg"
