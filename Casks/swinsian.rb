916168619bef29b3a9a905dec32b1926d28ea32b
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-19 21:31:12 +0000

    Update swinsian from 2.2.4 to 2.3.0 (#91102)

--- Casks/swinsian.rb
+++ Casks/swinsian.rb
@@ -1,6 +1,6 @@
 cask "swinsian" do
-  version "2.2.4"
-  sha256 "2135ff20c9077e1205b9e12ad3ab1fb688b22a2be4ca4db7f5710b08498118e2"
+  version "2.3.0"
+  sha256 "9807dc6b0e8855e18eb77e23a5af460bd137e73d26bb2e964938283918b08cd4"
 
   url "https://www.swinsian.com/sparkle/Swinsian_#{version}.zip"
   appcast "https://www.swinsian.com/sparkle/sparklecast.xml"
