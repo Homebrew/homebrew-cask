a522a533c544e221da066cf3231e247996d52089
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-19 21:53:50 +0800

    Update visicut.rb

--- Casks/visicut.rb
+++ Casks/visicut.rb
@@ -1,10 +1,11 @@
 cask "visicut" do
-  version "1.9.83-2dc5fd7f"
-  sha256 "07b01fd9afb3a4f82c5c9fecae6fd9dc88aa75007415bc4d5a8d0c738285d9de"
+  version "1.9-83-g2dc5fd7f"
+  sha256 "a0d5cb13f9e4133b8721ce94603fb0ea6b9eeb7943fab79ab2f82cae6df4982a"
 
   url "https://download.visicut.org/files/master/MacOSX/VisiCutMac-#{version}.zip"
   appcast "https://download.visicut.org"
   name "VisiCut"
+  desc "Prepare, save and send jobs to Lasercutters"
   homepage "https://visicut.org/"
 
   app "VisiCut.app"
