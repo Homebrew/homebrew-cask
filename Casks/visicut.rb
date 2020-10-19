19fb6c9d1bf271574d77c0550169ac8fe257bc3d
Brigitte Zmoelnig <brigitte@corecode.io>
2020-10-19 11:45:30 +0200

    Update visicut from 1.9-80-g65b7f094 to 1.9.83-2dc5fd7f

--- Casks/visicut.rb
+++ Casks/visicut.rb
@@ -1,6 +1,6 @@
 cask "visicut" do
-  version "1.9-80-g65b7f094"
-  sha256 "26247e3e785a491b64ab186e48ba9f8734c2a5c2d847068dcd78d826e48420e8"
+  version "1.9.83-2dc5fd7f"
+  sha256 "07b01fd9afb3a4f82c5c9fecae6fd9dc88aa75007415bc4d5a8d0c738285d9de"
 
   url "https://download.visicut.org/files/master/MacOSX/VisiCutMac-#{version}.zip"
   appcast "https://download.visicut.org"
