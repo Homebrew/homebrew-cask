7b4cb0cc4db7f9037de3ba6b24c6f38ddd8f311c
Brigitte Zmoelnig <brigitte@corecode.io>
2020-10-20 12:39:41 +0200

    Update masterway-note from 1.2.0,60 to 1.2.1

--- Casks/masterway-note.rb
+++ Casks/masterway-note.rb
@@ -1,6 +1,6 @@
 cask "masterway-note" do
-  version "1.2.0,60"
-  sha256 "74684b9ebe07ee7c05e7b0b5e49063b802d29df20c35af82b796eb408b7c4e88"
+  version "1.2.1"
+  sha256 "7da3783f68564ec5b97164b31afc1b4dda5f957f752140fbe4c602f0b1228815"
 
   # prota.oss-cn-beijing.aliyuncs.com/ was verified as official when first introduced to the cask
   url "https://prota.oss-cn-beijing.aliyuncs.com/downloads/latest/%E5%A4%A7%E5%B8%88%E7%AC%94%E8%AE%B0masterwaynote.pkg"
