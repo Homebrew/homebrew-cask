3089a912a0ffa7c480b647a51efbc281c01e065a
crlorentzen <crlorentzen@gmail.com>
2020-10-20 18:07:26 -0400

    Cask repair update ocenaudio (#91131)

--- Casks/ocenaudio.rb
+++ Casks/ocenaudio.rb
@@ -1,17 +1,18 @@
 cask "ocenaudio" do
-  version "3.9.3"
+  version "3.9.5"
 
   if MacOS.version <= :high_sierra
-    sha256 "32c44f35967dca63baeaa3f646a5289cc78a0c963df9ce9ab6c67258b0e88b1b"
+    sha256 "a286a08532a18843bbb7c3e9dd1f087ddc4c7f8716eafb1f83d890bb32a556e7"
 
     url "https://www.ocenaudio.com/downloads/index.php/ocenaudio_sierra.dmg"
   else
-    sha256 "63aaec8cd3ad45149218f8412410aeeb6965c97a4a85c5d7294162ba57043d65"
+    sha256 "a5402b621ddbd0415a9303f1b80dc90ddf72dc18a26dc80ccc187b465aa9e828"
 
     url "https://www.ocenaudio.com/downloads/index.php/ocenaudio_mojave.dmg"
   end
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://www.ocenaudio.com/downloads/index.php/ocenaudio_mojave.dmg"
   name "ocenaudio"
+  desc "Audio editor"
   homepage "https://www.ocenaudio.com/en"
 
   depends_on macos: ">= :sierra"
