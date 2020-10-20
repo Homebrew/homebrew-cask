e5222f60b2d4960f8e3d46b0cfbf7ba877b48e9e
Ben Camarata <benmcamarata@gmail.com>
2020-10-20 11:42:36 -0700

    Create ohsstarboard.rb

---
+++ Casks/ohsstarboard.rb
@@ -0,0 +1,11 @@
+cask "ohsstarboard" do
+  version "1.4.6"
+  sha256 :no_check
+
+  url "https://github.com/starboardbrowser/starboard.desktop/blob/master/BrewSu$
+  name "Starboard"
+  desc "The smart browser"
+  homepage "https://github.com/starboardbrowser"
+
+  app "Starboard DE.app"
+end
