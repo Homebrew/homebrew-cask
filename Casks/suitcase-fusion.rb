1ff3f32921b12ef34b537c49706321f6138075cd
Randall <ran.dall@icloud.com>
2020-10-21 14:35:28 -0700

    Update suitcase-fusion from 21.1.1 to 21.2.0

--- Casks/suitcase-fusion.rb
+++ Casks/suitcase-fusion.rb
@@ -1,6 +1,6 @@
 cask "suitcase-fusion" do
-  version "21.1.1"
-  sha256 "8348d09cd84cdaf16cfa4fccf28b79008f0516deb88c76ce3eeccb961b709337"
+  version "21.2.0"
+  sha256 "71f73a1338e5776f0ecedf89bbb1b1925982769b5e30461e075a36ae7bd09465"
 
   url "https://bin.extensis.com/SuitcaseFusion-M-#{version.dots_to_hyphens}.dmg"
   appcast "https://www.extensis.com/support/suitcase-fusion-#{version.major}/release-notes/"
