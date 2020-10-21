3b5b0244367de388f9330c933bc6b02154da2914
Randall <ran.dall@icloud.com>
2020-10-21 14:32:41 -0700

    Update big-mean-folder-machine from 2.41 to 2.42

--- Casks/big-mean-folder-machine.rb
+++ Casks/big-mean-folder-machine.rb
@@ -1,6 +1,6 @@
 cask "big-mean-folder-machine" do
-  version "2.41"
-  sha256 "d5ac6ccdc6dc01d518b69576b0d969a1d784f7170746ddccd343e8f2977204fa"
+  version "2.42"
+  sha256 "5324a4904a5559edf587ffd3f5af70aa0a46744631721a8761463c792f755004"
 
   url "https://www.publicspace.net/download/BMFM.dmg"
   appcast "https://www.publicspace.net/app/bmfm#{version.major}.xml"
