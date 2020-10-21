fe2027e3c9cec338d42759b362c2b8437d322cea
iPumpkin <ipumpkin.lv17@gmail.com>
2020-10-21 14:37:57 +0800

    Add Yuque 0.3.13

--- Casks/yuque.rb
+++ Casks/yuque.rb
@@ -1,11 +1,12 @@
 cask "yuque" do
-  version "0.3.11"
-  sha256 "dc478f867ee094f4b2f530a716258941a6c79172247a32fa5ad3063b80eb9782"
+  version "0.3.13"
+  sha256 "b8051a8aa9b96d662657de9af14901187e8af0d04807a19be7327b8a90825f4c"
 
   url "https://app.nlark.com/yuque-desktop/Yuque-#{version}.dmg"
   appcast "https://www.yuque.com/install/desktop"
   name "Yuque"
   name "语雀"
+  desc "专业的云端知识库，打造更轻盈的书写体验"
   homepage "https://www.yuque.com/"
 
   app "语雀.app"
