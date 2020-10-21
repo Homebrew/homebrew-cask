ff94e7ccc2166a69c23e212271a0ab506047ce4c
Randall <ran.dall@icloud.com>
2020-10-20 22:32:24 -0700

    Update moneymoney from 2.3.33 to 2.3.34

--- Casks/moneymoney.rb
+++ Casks/moneymoney.rb
@@ -1,6 +1,6 @@
 cask "moneymoney" do
-  version "2.3.33"
-  sha256 "7c536147b63f1d57967e301b228f63b6a3d408e1edb330b0796ee358be0e2de6"
+  version "2.3.34"
+  sha256 "b57bcb9bc1c6151dda6659c3db0e7520927350c3d373187d46aedb13f581c948"
 
   url "https://service.moneymoney-app.com/1/MoneyMoney.zip"
   appcast "https://service.moneymoney-app.com/1/Appcast.xml"
