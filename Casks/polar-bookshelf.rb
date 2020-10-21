50cad94ed43334d75986cdbcaaf6ae6f6262d257
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-21 17:37:56 +0000

    Update polar-bookshelf from 2.0.42 to 2.0.53 (#91188)

--- Casks/polar-bookshelf.rb
+++ Casks/polar-bookshelf.rb
@@ -1,6 +1,6 @@
 cask "polar-bookshelf" do
-  version "2.0.42"
-  sha256 "a7aee91820b18d848abe92cbd7c7c36d6fb935a9b846941b316ed42c1b687687"
+  version "2.0.53"
+  sha256 "dedca2902093cc273168d1cd00198bafe97395edab457db91aeec3fff73dda7c"
 
   # github.com/burtonator/polar-bookshelf/ was verified as official when first introduced to the cask
   url "https://github.com/burtonator/polar-bookshelf/releases/download/v#{version}/Polar-#{version}.dmg"
