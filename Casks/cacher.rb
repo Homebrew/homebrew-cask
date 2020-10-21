f6d92a63bcae34329ae22d9cabb1ec8a9a63c264
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-21 22:01:56 +0000

    Update cacher from 2.33.0 to 2.33.1 (#91218)

--- Casks/cacher.rb
+++ Casks/cacher.rb
@@ -1,6 +1,6 @@
 cask "cacher" do
-  version "2.33.0"
-  sha256 "9bd73f969714cec41c5648855eddda27a9e6312c62a8493db4a974e13ee7f578"
+  version "2.33.1"
+  sha256 "b8c43017af52e73a06a3aac224b3c75a043a36f7f026aa0018ee987fab384b9e"
 
   # cacher-download.nyc3.digitaloceanspaces.com/ was verified as official when first introduced to the cask
   url "https://cacher-download.nyc3.digitaloceanspaces.com/Cacher-#{version}-mac.zip"
