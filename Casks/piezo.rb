3d048f0bbfa2038e2853dd0a4b394f03407713b0
Randall <ran.dall@icloud.com>
2020-10-21 14:43:01 -0700

    Update piezo

--- Casks/piezo.rb
+++ Casks/piezo.rb
@@ -1,6 +1,6 @@
 cask "piezo" do
   version "1.6.6"
-  sha256 "6c184031ea20e46af421f908597daf45147c07daa4c702c8909e95e9af9191e4"
+  sha256 "63f9a2e34d6223780e6f21572f2a5eaaca208dfd2ca3c9a81fac63b6bef30cfd"
 
   url "https://rogueamoeba.com/piezo/download/Piezo.zip"
   appcast "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Piezo&system=10146"
