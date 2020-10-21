07a5a969a40e335c9d8739f83ba2c651f8c80348
Randall <ran.dall@icloud.com>
2020-10-21 14:26:54 -0700

    Update workflowy from 1.3.5-11950 to 1.3.5-12042

--- Casks/workflowy.rb
+++ Casks/workflowy.rb
@@ -1,6 +1,6 @@
 cask "workflowy" do
-  version "1.3.5-11950"
-  sha256 "6f0124828bcbb56c00a9a2294e7cea07e08a66806ddd40614825ef5bf536c581"
+  version "1.3.5-12042"
+  sha256 "c9cd772f41a4fbdd2a79ebed5449c975c8077c3bfb46cf9976fc6ce529e6361f"
 
   # github.com/workflowy/desktop/ was verified as official when first introduced to the cask
   url "https://github.com/workflowy/desktop/releases/download/v#{version}/WorkFlowy.dmg"
