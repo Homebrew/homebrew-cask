3f8516c40779b4c547ff5c88d723e35712727c7f
Randall <ran.dall@icloud.com>
2020-10-21 15:02:13 -0700

    Update openphone from 1.9.6 to 1.9.7

--- Casks/openphone.rb
+++ Casks/openphone.rb
@@ -1,6 +1,6 @@
 cask "openphone" do
-  version "1.9.6"
-  sha256 "f2ef9b8a92f6f72c8bc8b96eebbe0ba695213a46a5fb3bd3c3c9c1a0177907e3"
+  version "1.9.7"
+  sha256 "c4c80840c8b0752f9a84ed7c2544161092a3232f7693d3a09ed29931fca73bbc"
 
   url "https://download.openphone.co/OpenPhone-#{version}.dmg"
   appcast "https://s3-us-west-2.amazonaws.com/download.openphone.co/latest-mac.yml"
