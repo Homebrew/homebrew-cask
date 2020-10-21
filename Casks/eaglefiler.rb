794634ed7cb0be365b0bb215fb857ce0a1d4bfac
Matt Bailey <a.matthew.bailey@gmail.com>
2020-10-21 15:47:39 -0400

    Updated eaglefiler.rb to latest v9 of EagleFiler.

--- Casks/eaglefiler.rb
+++ Casks/eaglefiler.rb
@@ -1,5 +1,5 @@
 cask "eaglefiler" do
-  version "1.8.14"
+  version "1.9"
   sha256 "1040bb6388bb689227eaad5ccaa9f4f2e1961b029a8f4f8eb1f211c3a993c150"
 
   url "https://c-command.com/downloads/EagleFiler-#{version}.dmg"
