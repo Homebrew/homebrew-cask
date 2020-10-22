9e88b43b817c0db1ee0d532bad5487294034d1c8
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-22 08:39:50 +0800

    Update origami-studio.rb

--- Casks/origami-studio.rb
+++ Casks/origami-studio.rb
@@ -2,10 +2,11 @@ cask "origami-studio" do
   version "252227598"
   sha256 "716b7d0d0596464bb5dd100e7d7bb687da0b38e4809d13548d32c229a765d82c"
 
-  # fb.me/ was verified as official when first introduced to the cask
-  url "https://fb.me/getorigamistudio"
-  appcast "https://www.facebook.com/mobile_builds/appcast.xml?app_id=892075810923571"
+  # facebook.com/designtools/ was verified as official when first introduced to the cask
+  url "https://facebook.com/designtools/origami/"
+  appcast "https://www.facebook.com/mobile_builds/appcast.xml?app_id=892075810923571&amp;flavor=production"
   name "Origami Studio"
+  desc "Design, animate, and prototype design tool from Facebook"
   homepage "https://origami.design/"
 
   depends_on macos: ">= :sierra"
