9c3876adfd628fd324cca6b6e2fa549724619d19
Javier Buzzi <buzzi.javier@gmail.com>
2020-10-21 22:13:40 -0400

    Update manycam.rb

--- Casks/manycam.rb
+++ Casks/manycam.rb
@@ -2,13 +2,22 @@ cask "manycam" do
   version "7.5.0.7"
   sha256 "67a5b485ab128e82717b62c8e0ec5898096979d1042567439ea3b4a6cfd10314"
 
+  # download3.manycams.com was verified as official when first introduced to the cask
   url "https://download3.manycams.com/ManyCam.dmg"
   appcast "https://manycam.com/mac_changes/"
   name "ManyCam"
   desc "Live Video Made Better"
-  homepage "https://manycamp.com/"
+  homepage "https://manycam.com/"
 
   depends_on macos: ">= :el_capitan"
 
   pkg "ManyCam-#{version}.pkg"
+
+  uninstall quit:   "com.visicom.ManyCam.application",
+            script: {
+              executable: "/Applications/ManyCam/Uninstall\ ManyCam.app/Contents/MacOS/Uninstall\ ManyCam",
+              args:       ["--mode", "unattended"],
+              sudo:       true,
+            },
+            delete: "/Applications/ManyCam"
 end
