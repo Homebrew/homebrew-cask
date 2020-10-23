18e5b3d56f12424206905fdd8586eeaef2dabd92
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-23 19:15:29 +0000

    Update sidenotes from 1.2.1 to 1.2.2 (#91325)

--- Casks/sidenotes.rb
+++ Casks/sidenotes.rb
@@ -1,6 +1,6 @@
 cask "sidenotes" do
-  version "1.2.1"
-  sha256 "59ea38805bc91cc4b1a2b5cd5ed576f69a891ca2a1c8be7b6a4503a15e56fc31"
+  version "1.2.2"
+  sha256 "5460bf265e36bba1eec12db42afd86d510ce472941396d8ffa425c8c270ba945"
 
   url "https://www.apptorium.com/public/products/sidenotes/releases/SideNotes-#{version}.zip"
   appcast "https://www.apptorium.com/updates/sidenotes"
