d943112fddf550373ae389e1acd2ff2f040500d3
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-23 19:14:06 +0000

    Update rightfont from 5.8.4 to 5.9.0 (#91324)

--- Casks/rightfont.rb
+++ Casks/rightfont.rb
@@ -1,6 +1,6 @@
 cask "rightfont" do
-  version "5.8.4"
-  sha256 "e581cac8691bb397c3e09ec83af8c30eb8da9fe8291b3a8f49a3e4e8cc828e57"
+  version "5.9.0"
+  sha256 "17539732acb77bfa7c156175b82f33f4a02e8ceb15784442752d93e10722d0f1"
 
   url "https://rightfontapp.com/update/rightfont.zip"
   appcast "https://rightfontapp.com/update/appcast#{version.major}.xml"
