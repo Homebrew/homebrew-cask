e1951c0d2c9374579a74ef995430af52f007102d
miccal <miccal.matthews@gmail.com>
2020-10-23 09:09:23 +0800

    onlyoffice 6.0.0

--- Casks/onlyoffice.rb
+++ Casks/onlyoffice.rb
@@ -1,10 +1,12 @@
 cask "onlyoffice" do
-  version "6.0"
-  sha256 "cbb4c94e62745f31d70da2dcfc84185f8926ae1bd49c3663755417bf45fd3381"
+  version "6.0.0"
+  sha256 "d82c5623e814a3df939fa039945b76b38af35bdf0ce06f6da85cb94891a8e3f2"
 
-  url "https://download.onlyoffice.com/install/desktop/editors/mac/updates/onlyoffice/ONLYOFFICE-#{version}.zip"
-  appcast "https://download.onlyoffice.com/install/desktop/editors/mac/onlyoffice.xml"
+  # github.com/ONLYOFFICE/DesktopEditors/ was verified as official when first introduced to the cask
+  url "https://github.com/ONLYOFFICE/DesktopEditors/releases/download/ONLYOFFICE-DesktopEditors-#{version}/ONLYOFFICE.dmg"
+  appcast "https://github.com/ONLYOFFICE/DesktopEditors/releases.atom"
   name "ONLYOFFICE"
+  desc "Document editor"
   homepage "https://www.onlyoffice.com/"
 
   auto_updates true
