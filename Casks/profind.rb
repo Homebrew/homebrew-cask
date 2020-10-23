6be653928b699ac9bdf27d099255ce383c8be41d
Randall <ran.dall@icloud.com>
2020-10-23 12:19:51 -0700

    Update profind from 1.8.2 to 1.8.3

--- Casks/profind.rb
+++ Casks/profind.rb
@@ -1,6 +1,6 @@
 cask "profind" do
-  version "1.8.2"
-  sha256 "00a9719a5d6a6d5f2fe9cea7b1a85ee9e2669500902e78124c48f3294f34860c"
+  version "1.8.3"
+  sha256 "475dac0db32936a636a742eb7955803340b72f5bdf387aae3b6555be0cff7383"
 
   url "https://www.zeroonetwenty.com/profind/downloads/ProFind#{version.no_dots}.dmg"
   appcast "https://www.zeroonetwenty.com/profind/downloads/ProFindVersionInfo.xml"
