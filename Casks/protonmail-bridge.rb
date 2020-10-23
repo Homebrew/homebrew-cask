de104aa4b35c3cda5346a1ef3a0678b5d390db35
Tom Matthews <trcm@pm.me>
2020-10-23 14:12:00 +0100

    Update protonmail-bridge from 1.4.4 to 1.4.5

--- Casks/protonmail-bridge.rb
+++ Casks/protonmail-bridge.rb
@@ -1,6 +1,6 @@
 cask "protonmail-bridge" do
-  version "1.4.4"
-  sha256 "11d40ad753e5bf63d40d0d2d0455865eb934c34adf68decd75154dee669d9fb8"
+  version "1.4.5"
+  sha256 "1f29124b09de61051ed013cecae4491b49c00a031e1b79f14179a5abc7e9a784"
 
   url "https://protonmail.com/download/Bridge-Installer.dmg"
   appcast "https://protonmail.com/download/current_version_darwin.json"
