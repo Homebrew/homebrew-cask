075db6091b8f13c935fd308fdb02ebaab161d4a1
Tim Adler <tim@Tims-MacBook-Pro.fritz.box>
2020-10-24 22:04:18 +0200

    Add SyncSettings Latest

---
+++ Casks/syncsettings.rb
@@ -0,0 +1,11 @@
+cask "syncsettings" do
+  version :latest
+  sha256 :no_check
+
+  url "https://neededapps.com/syncsettings/trial"
+  name "SyncSettings"
+  desc "Sync and save settings of mac apps"
+  homepage "https://neededapps.com/syncsettings/"
+
+  app "SyncSettings.app"
+end
