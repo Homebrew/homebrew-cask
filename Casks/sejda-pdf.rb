d536c76eb251d243c16e2e8c1253546dca20c876
Randall <ran.dall@icloud.com>
2020-10-24 09:17:17 -0700

    Update sejda-pdf from 7.0.11 to 7.0.12

--- Casks/sejda-pdf.rb
+++ Casks/sejda-pdf.rb
@@ -1,6 +1,6 @@
 cask "sejda-pdf" do
-  version "7.0.11"
-  sha256 "fe34218df42d76abb5faecc352bc076bb0f7be3e2d42edd04e91cb0e6b2c7137"
+  version "7.0.12"
+  sha256 "272a8ff20b903c7d059cce115f47549d6649a7d3d132ac9cb148f03fb3b2ed5b"
 
   # sejda-cdn.com/ was verified as official when first introduced to the cask
   url "https://sejda-cdn.com/downloads/sejda-desktop_#{version}.dmg"
