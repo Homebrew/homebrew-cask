7666465b3e7758ea3e88f25b282a20b682372317
Robert Blair <robertleeblairjr@outlook.com>
2020-10-23 20:41:59 -0500

    Update opera from 71.0.3770.284 to 72.0.3815.148

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask "opera" do
-  version "71.0.3770.284"
-  sha256 "e71ce0ff4f379dae584cb2880bbfb1ef47f34b2a1dd521ca14b659032876a40a"
+  version "72.0.3815.148"
+  sha256 "2af5930a1ba1120ef0a9374170439daf732cd1e305d180ffcab6c6437dc038f5"
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   appcast "https://ftp.opera.com/pub/opera/desktop/"
