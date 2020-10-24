f5ee0474d379786e6c2cf891ce7aac3a386c3378
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-25 07:03:19 +0800

    Update syncsettings.rb

--- Casks/syncsettings.rb
+++ Casks/syncsettings.rb
@@ -1,11 +1,14 @@
 cask "syncsettings" do
-  version :latest
-  sha256 :no_check
+  version "1.2.4"
+  sha256 "09e6d8b65fce1a21faa3933bb07f61a9a9b0386da2bb61f0688c1f04f28a9978"
 
-  url "https://neededapps.com/syncsettings/trial"
+  url "https://neededapps.com/appcasts/syncsettings/versions/SS-#{version}.zip"
+  appcast "https://neededapps.com/appcasts/syncsettings/changelog.xml"
   name "SyncSettings"
   desc "Sync and save settings of your apps"
   homepage "https://neededapps.com/syncsettings/"
+
+  depends_on macos: ">= :high_sierra"
 
   app "SyncSettings.app"
 end
