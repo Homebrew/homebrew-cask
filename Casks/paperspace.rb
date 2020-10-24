bfae26d8fddf20f9939c7d35ff4e536c278cab33
suschizu <brigitte@corecode.io>
2020-10-24 11:51:37 +0200

    Update paperspace from 9.4.1.1292 to 10.0.0.1561 (#91356)

--- Casks/paperspace.rb
+++ Casks/paperspace.rb
@@ -1,6 +1,6 @@
 cask "paperspace" do
-  version "9.4.1.1292"
-  sha256 "a77a32d7c5dcee2aae4575ceb645b23709213d8dc3a71e2990733dfb8106bc16"
+  version "10.0.0.1561"
+  sha256 "e5a4a921223d98a6b8a698299ec1646a4a79e01fec83427fd395ab9e89bca7ff"
 
   url "https://assets.paperspace.com/native-app/prod/darwin/Paperspace-#{version}.dmg"
   appcast "https://www.macupdater.net/cgi-bin/extract_text/send_post_request_data.cgi?url=https://api.paperspace.io/receivers/latestVersion&data={%22component%22:%22paperspaceInstaller%22,%22os%22:%22mac%22}&headers={%22user-agent%22:%22Mozilla%22,%22content-type%22:%22application/json%22}"
