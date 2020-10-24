f657035c4e92d02c28b51917d27cce6d37fe8236
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-25 06:27:58 +0800

    Update sitebulb.rb

--- Casks/sitebulb.rb
+++ Casks/sitebulb.rb
@@ -1,11 +1,12 @@
 cask "sitebulb" do
   version "4.4"
-  sha256 "86e52aa57caca7fd38864db420c7def8cac5a5abd812ffee9b020cf668795ec9"
+  sha256 "277c1f3d6d14e172d283e2f9646211e651cd9e9feaf408fdbab6a48e1a52bfac"
 
   url "https://downloads.sitebulb.com/#{version}/macOS/Sitebulb.dmg"
   appcast "https://sitebulb.com/download/",
           must_contain: version.major_minor
   name "Sitebulb"
+  desc "Website auditing tool"
   homepage "https://sitebulb.com/"
 
   app "Sitebulb.app"
