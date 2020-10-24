bb13a7a86eae5b8896793b516b0a47268f4bf84f
suschizu <brigitte@corecode.io>
2020-10-24 11:58:21 +0200

    Update sitebulb.rb

--- Casks/sitebulb.rb
+++ Casks/sitebulb.rb
@@ -1,8 +1,8 @@
 cask "sitebulb" do
-  version "4.3.0"
+  version "4.4"
   sha256 "86e52aa57caca7fd38864db420c7def8cac5a5abd812ffee9b020cf668795ec9"
 
-  url "https://downloads.sitebulb.com/#{version.major}/macOS/Sitebulb.dmg"
+  url "https://downloads.sitebulb.com/#{version}/macOS/Sitebulb.dmg"
   appcast "https://sitebulb.com/download/",
           must_contain: version.major_minor
   name "Sitebulb"
