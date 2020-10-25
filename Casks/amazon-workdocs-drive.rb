9adb4fecad1e31ee7bcfea6855395a22104327dc
Kristian Matthews <kristian.matthews@me.com>
2020-10-25 14:09:03 +0000

    Add Amazon WorkDocs Drive latest

---
+++ Casks/amazon-workdocs-drive.rb
@@ -0,0 +1,15 @@
+cask "amazon-workdocs-drive" do
+  version :latest
+  sha256 :no_check
+
+  # d3f2hupz96ggz3.cloudfront.net/ was verified as official when first introduced to the cask
+  url "https://d3f2hupz96ggz3.cloudfront.net/mac/AmazonWorkDocsDrive.pkg"
+  name "Amazon WorkDocs Drive"
+  desc "Fully managed, secure enterprise storage and sharing service"
+  homepage "https://aws.amazon.com/workdocs/"
+
+  pkg "AmazonWorkDocsDrive.pkg"
+
+  uninstall signal:  ["TERM", "com.Amazon.WorkDocs.Drive"],
+            pkgutil: "com.Amazon.WorkDocs.Drive"
+end
