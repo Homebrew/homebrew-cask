fc9fdcac838f44c9b8d2573915d143a2fd1118d6
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-26 12:51:58 +0800

    Update chrome-remote-desktop-host.rb

--- Casks/chrome-remote-desktop-host.rb
+++ Casks/chrome-remote-desktop-host.rb
@@ -4,6 +4,7 @@ cask "chrome-remote-desktop-host" do
 
   url "https://dl.google.com/chrome-remote-desktop/chromeremotedesktop.dmg"
   name "Chrome Remote Desktop"
+  desc "Remotely access another computer through the Chrome browser"
   homepage "https://chrome.google.com/webstore/detail/chrome-remote-desktop/inomeogfingihgjfjlpeplalcfajhgai"
 
   pkg "Chrome Remote Desktop Host.pkg"
@@ -11,8 +12,13 @@ cask "chrome-remote-desktop-host" do
   uninstall script:  {
     executable: "/Applications/Chrome Remote Desktop Host Uninstaller.app/Contents/MacOS/remoting_host_uninstaller",
     args:       ["--no-ui"],
+    sudo:       true,
   },
-            pkgutil: "com.google.pkg.ChromeRemoteDesktopHost"
+            pkgutil: [
+              "com.google.pkg.ChromeRemoteDesktopHost",
+              "com.google.pkg.ChromeRemoteDesktopHostService",
+              "com.google.pkg.ChromeRemoteDesktopHostUninstaller",
+            ]
 
   caveats do
     logout
