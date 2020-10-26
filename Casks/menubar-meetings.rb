c4e48f054d96a58db9e3a4179f34d5c1a06651a5
nicolai92 <nicolai.ernst@outlook.de>
2020-10-26 11:32:17 +0100

    Add Meetings.app v1.1
    Meetings is a menubar app, that shows todays Exchange meetings with the option to directly dial in.

--- Casks/menubar-meetings.rb
+++ Casks/menubar-meetings.rb
@@ -3,9 +3,12 @@ cask "menubar-meetings" do
   sha256 "92fb89e973435086966fb8fb1e8c880234a8c693e38b82355bd00b2073a7dcaa"
 
   url "https://github.com/nicolai92/akai/releases/download/v#{version}/Meetings.app.zip"
+  appcast "https://github.com/leits/MeetingBar/releases.atom"
   name "Meetings"
   desc "Join meetings from the menubar"
   homepage "https://github.com/nicolai92/akai/"
+
+  depends_on macos: ">= :high_sierra"
 
   app "Meetings.app"
 end
