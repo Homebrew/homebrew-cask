52d7b495b23f58c8b62ba37fbe7c8f4dbd872530
suschizu <brigitte@corecode.io>
2020-10-26 11:27:12 +0100

    Update wondershare-filmora.rb

--- Casks/wondershare-filmora.rb
+++ Casks/wondershare-filmora.rb
@@ -1,8 +1,9 @@
 cask "wondershare-filmora" do
-  version "10.0.0.61"
+  version "10.0.1"
   sha256 "e35f7e7f5e3289247106245fb4f3471318952e0c9b4dd8103e5493cca5bc69a8"
 
   url "https://download.wondershare.com/filmora-mac_full718.dmg"
+  appcast "http://cbs.wondershare.com/go.php?m=upgrade_info&pid=718&version=#{version}"
   name "Wondershare Filmora"
   desc "Video editor"
   homepage "https://filmora.wondershare.com/video-editor/"
