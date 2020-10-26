57135c813a04ce339f474c144c9037f4ab311a7c
Robert Fairhead <robertfairhead@users.noreply.github.com>
2020-10-26 10:42:01 -0500

    Update Aptible to v0.16.8 (#91344)
    * Update aptible.rb
    
    * add `desc`
    
    * Update description with additional context

--- Casks/aptible.rb
+++ Casks/aptible.rb
@@ -1,10 +1,11 @@
 cask "aptible" do
-  version "0.16.7,20200812001716,217"
-  sha256 "e186135254a7257e6f661b5520f5e2058ae8ed6b3574b81532a1a67d3f82ce0b"
+  version "0.16.8,20201023194353,220"
+  sha256 "2fb73ff153abdd761691826298452e0a4cd47f193235e5fb792a2226b116677d"
 
   # omnibus-aptible-toolbelt.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://omnibus-aptible-toolbelt.s3.amazonaws.com/aptible/omnibus-aptible-toolbelt/master/#{version.after_comma.after_comma}/pkg/aptible-toolbelt-#{version.before_comma}%2B#{version.after_comma.before_comma}-mac-os-x.10.11.6-1.pkg"
   name "Aptible Toolbelt"
+  desc "Command-line tool for Aptible Deploy, an audit-ready App Deployment Platform"
   homepage "https://www.aptible.com/documentation/deploy/cli.html"
 
   depends_on formula: "libu2f-host"
