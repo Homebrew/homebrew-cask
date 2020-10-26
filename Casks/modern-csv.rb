662e6a235ab36e7c1806c6c45645292b68014bce
Evan <evan@moderncsv.com>
2020-10-25 20:19:28 -0500

    Update modern-csv from 1.3.4 to 1.3.5

--- Casks/modern-csv.rb
+++ Casks/modern-csv.rb
@@ -1,6 +1,6 @@
 cask "modern-csv" do
-  version "1.3.4"
-  sha256 "b6bb1c6550cea551f7fb0e4d3d65521a55577460522dfb2bc464958ceb354c1e"
+  version "1.3.5"
+  sha256 "44538a20e4c85f403298552a1816449f7a65dca134583419bfe1e2e970f3c755"
 
   url "https://www.moderncsv.com/release/ModernCSV-Mac-v#{version}.dmg"
   appcast "https://www.moderncsv.com/latest-version/"
