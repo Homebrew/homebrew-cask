9217a56b4a7094c4a709f8512152841efba76189
Viktor Szakats <commit@vsz.me>
2020-10-27 22:55:52 +0000

    tongbu: secure url

--- Casks/tongbu.rb
+++ Casks/tongbu.rb
@@ -2,7 +2,7 @@ cask "tongbu" do
   version "3.0.9.0"
   sha256 "32a2309a115d9d017ea0657e5b9a5c69be9d49000ec1a6a3e74890ad9e89c555"
 
-  url "http://qd.tongbu.com/qd/tbzsqd/zsmac_#{version}/Tongbu_mac_#{version}.dmg"
+  url "https://qd.tongbu.com/qd/tbzsqd/zsmac_#{version}/Tongbu_mac_#{version}.dmg"
   appcast "https://zs.tongbu.com/mac/"
   name "Tongbu"
   homepage "https://zs.tongbu.com/mac/"
