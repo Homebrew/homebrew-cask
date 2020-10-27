ef56d92910267d48494787301b1695d5c199d04e
Randall <ran.dall@icloud.com>
2020-10-27 12:30:48 -0700

    Update altserver from 1.4 to 1.4.1

--- Casks/altserver.rb
+++ Casks/altserver.rb
@@ -1,6 +1,6 @@
 cask "altserver" do
-  version "1.4"
-  sha256 "1107c408f584dd2b3579562ecda59544b1f6547a1fb025c1a502a19b972c4272"
+  version "1.4.1"
+  sha256 "6c7c72f4fd2536e56ebd63f424e19fe00f51ae9360ca2089fd4e7eaf061b1cc8"
 
   # f000.backblazeb2.com/file/ was verified as official when first introduced to the cask
   url "https://f000.backblazeb2.com/file/altstore/altserver/#{version.dots_to_underscores}.zip"
