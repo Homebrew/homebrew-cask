14ea70e1b207c36c3fa0a6d1e2f1beecae79b774
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-28 05:43:32 +0800

    Update padloc.rb

--- Casks/padloc.rb
+++ Casks/padloc.rb
@@ -2,7 +2,7 @@ cask "padloc" do
   version "3.1.2"
   sha256 "c87aa25e1947bff68b92ef6b44efaa9f10cd06d6270d0d6cac58bd67226d719e"
 
-  # github.com/padloc/padloc was verified as official when first introduced to the cask
+  # github.com/padloc/padloc/ was verified as official when first introduced to the cask
   url "https://github.com/padloc/padloc/releases/download/v#{version}/Padloc-#{version}.dmg"
   appcast "https://github.com/padloc/padloc/releases.atom"
   name "Padloc"
@@ -12,7 +12,7 @@ cask "padloc" do
   app "Padloc.app"
 
   zap trash: [
-    "~/Library/Application Support/Padloc",
     "~/Library/Application Support/Caches/padloc-updater",
+    "~/Library/Application Support/Padloc",
   ]
 end
