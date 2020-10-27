e48e0e01783077e84ce8f3b9852025db68fa6542
MoonFruit <dkmoonfruit@gmail.com>
2020-10-27 13:27:50 +0800

    Update hstracker from 1.7.1 to 1.7.2

--- Casks/hstracker.rb
+++ Casks/hstracker.rb
@@ -1,6 +1,6 @@
 cask "hstracker" do
-  version "1.7.1"
-  sha256 "ca8d17023c53766bcd4f7f5de467f1758708a0877e6a4a8f14ea096f528106ba"
+  version "1.7.2"
+  sha256 "4d7e1a7fed68f98c3a8d3d279d8dbdd11c841a3ea065db92d5f153d3d3ea91da"
 
   # github.com/HearthSim/HSTracker/ was verified as official when first introduced to the cask
   url "https://github.com/HearthSim/HSTracker/releases/download/#{version}/HSTracker.app.zip"
