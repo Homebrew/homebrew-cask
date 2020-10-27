dc8a0d5a3e78aa8ddb9e93d740286773397f8da7
Randall <ran.dall@icloud.com>
2020-10-27 12:17:08 -0700

    Update electerm from 1.5.13 to 1.5.15

--- Casks/electerm.rb
+++ Casks/electerm.rb
@@ -1,6 +1,6 @@
 cask "electerm" do
-  version "1.5.13"
-  sha256 "cd133f7f8e43ba5b4c0cf44d3958a669844b529c22127207d79c24e2866ef3d7"
+  version "1.5.15"
+  sha256 "9cc2c22b5d608fe82864e47db57ff15c30b76b96dbf1c640a1b431bf7d051eb3"
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac.dmg"
   appcast "https://github.com/electerm/electerm/releases.atom"
