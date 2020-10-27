44ac5228bdf0c9de28941a242fd2413650528883
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-27 14:30:06 +0800

    Update listen1.rb

--- Casks/listen1.rb
+++ Casks/listen1.rb
@@ -7,6 +7,7 @@ cask "listen1" do
   url "https://github.com/listen1/listen1_desktop/releases/download/v#{version}/Listen1_#{version}_mac.dmg"
   appcast "https://github.com/listen1/listen1_desktop/releases.atom"
   name "Listen 1"
+  desc "Search and play songs from a variety of online sources"
   homepage "https://listen1.github.io/listen1/"
 
   app "Listen1.app"
