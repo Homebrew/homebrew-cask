e6d18e2cdf1534ac6cd31ef0e769a123d44d0920
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-29 07:24:53 +0800

    Update loom.rb

--- Casks/loom.rb
+++ Casks/loom.rb
@@ -1,10 +1,11 @@
 cask "loom" do
-  version "0.54.1"
-  sha256 "81d2bcc957ff80e1aff2225c2648985adcb82456497d09d03518b974a71df777"
+  version "0.54.3"
+  sha256 "030eed7ab1256ed9ce217af87666ffa400662b7acf17b7e15e533485022feebb"
 
   url "https://cdn.loom.com/desktop-packages/Loom-#{version}.dmg"
   appcast "https://s3-us-west-2.amazonaws.com/loom.desktop.packages/loom-inc-production/desktop-packages/latest-mac.yml"
   name "Loom"
+  desc "Screen and video recording software"
   homepage "https://www.loom.com/"
 
   auto_updates true
