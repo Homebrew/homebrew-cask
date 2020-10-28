9f26037a5befd18346ec00e3accbc5bd34d3ef60
Randall <ran.dall@icloud.com>
2020-10-28 15:26:01 -0700

    Update toggl-track from 7.5.298 to 7.5.347

--- Casks/toggl-track.rb
+++ Casks/toggl-track.rb
@@ -1,6 +1,6 @@
 cask "toggl-track" do
-  version "7.5.298"
-  sha256 "a3bc557a5e9dc099734511b8e70c128f192e540a48cd7e6c9f02ed96e37ff038"
+  version "7.5.347"
+  sha256 "74d74d4ef2b46f4b95d515e712045c2e2bf8eee8c3b6f498b8de602851fc91e4"
 
   # github.com/toggl-open-source/toggldesktop/ was verified as official when first introduced to the cask
   url "https://github.com/toggl-open-source/toggldesktop/releases/download/v#{version}/TogglDesktop-#{version.dots_to_underscores}.dmg"
