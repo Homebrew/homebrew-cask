015719afbb00aa576c705ecc7dda467cdce8ed5d
Ben <BenjaminHCCarr@users.noreply.github.com>
2020-10-28 17:00:34 -0400

    Update menumeters from 2.0.7 to 2.0.8 (#91576)

--- Casks/menumeters.rb
+++ Casks/menumeters.rb
@@ -1,6 +1,6 @@
 cask "menumeters" do
-  version "2.0.7"
-  sha256 "a63217c32670f19913f2f814c91b760b5df57413649440e8f1c80821b763713e"
+  version "2.0.8"
+  sha256 "acb318ef826f1eb66ea5e9d22c5495c2d334157b946a50eb3b60c8f40bc560c8"
 
   url "https://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/zips/MenuMeters_#{version}.zip"
   appcast "https://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/MenuMeters-Update.xml"
