209f665e28dea8aab5234cfe44554ef05bc02b70
Randall <ran.dall@icloud.com>
2020-10-28 16:41:25 -0700

    Update querious from 3.0.5 to 3.0.6

--- Casks/querious.rb
+++ Casks/querious.rb
@@ -1,6 +1,6 @@
 cask "querious" do
-  version "3.0.5"
-  sha256 "a2a97a15131b9fde640c9b92515cb24b38f939a75e793c28be747543caad44cd"
+  version "3.0.6"
+  sha256 "c552e0f0accdcd9736d68d35fa26ab0942ee374cee029761c28d6b7cede3818c"
 
   url "https://www.araelium.com/querious/downloads/versions/Querious#{version}.zip"
   appcast "https://arweb-assets.s3.amazonaws.com/downloads/querious/updates.json"
