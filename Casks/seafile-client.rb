b8bed7bb074f681b701dac34da7be70970d76191
Work <brigitte@corecode.io>
2020-10-28 13:47:07 +0100

    Update seafile-client from 7.0.9 to 7.0.10

--- Casks/seafile-client.rb
+++ Casks/seafile-client.rb
@@ -1,6 +1,6 @@
 cask "seafile-client" do
-  version "7.0.9"
-  sha256 "b00e859fc55d05fe0af1dfaddab64d166875e90b5969289fd35b4811aaeea71c"
+  version "7.0.10"
+  sha256 "f56c3f9d9aa3295e74ee9c08fe852f489188b14065af43f8d1f35aa9f0f53d36"
 
   # seadrive.org/ was verified as official when first introduced to the cask
   url "https://download.seadrive.org/seafile-client-#{version}.dmg"
