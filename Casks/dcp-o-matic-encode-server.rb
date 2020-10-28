8f337c936504343bd922f912f0dc515c5cb5512f
Randall <ran.dall@icloud.com>
2020-10-28 16:26:53 -0700

    Update dcp-o-matic-encode-server from 2.14.40 to 2.14.41

--- Casks/dcp-o-matic-encode-server.rb
+++ Casks/dcp-o-matic-encode-server.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-encode-server" do
-  version "2.14.40"
-  sha256 "2bc7778d0862f8b84017a61c7eca389019e2aa9b77fd0a2a84d947d25dac9f0c"
+  version "2.14.41"
+  sha256 "80f6f69e05ba055df9a81dee615d07c4b3db0d9bda09567e400859b524463aeb"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.9-server&version=#{version}"
   appcast "https://dcpomatic.com/download"
