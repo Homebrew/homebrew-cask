e8a2e675fc159955756d23f2722f18bd594f37db
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-28 16:12:07 +0800

    Update foxmail.rb

--- Casks/foxmail.rb
+++ Casks/foxmail.rb
@@ -1,10 +1,11 @@
 cask "foxmail" do
-  version :latest
-  sha256 :no_check
+  version "1.4.0.94071"
+  sha256 "dc5440507054d3140b830cbfb8f146435ae9570089e16e5dad4e5c82189619fe"
 
-  url "https://www.foxmail.com/mac/download"
+  url "https://dldir1.qq.com/foxmail/MacFoxmail/Foxmail_for_Mac_#{version}.dmg"
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.foxmail.com/mac/download"
   name "Foxmail"
-  desc "Simple, yet not common"
+  desc "Email client"
   homepage "https://www.foxmail.com/"
 
   app "Foxmail.app"
