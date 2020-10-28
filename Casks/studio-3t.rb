a3bab90c93f117aca529061b0b5313164b1fc713
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-28 23:55:20 +0000

    Update studio-3t from 2020.9.2 to 2020.9.3 (#91636)

--- Casks/studio-3t.rb
+++ Casks/studio-3t.rb
@@ -1,6 +1,6 @@
 cask "studio-3t" do
-  version "2020.9.2"
-  sha256 "4a62f593de642570538edf7b291777333b5933e8a1f0bdb0d394dabba45fc9cb"
+  version "2020.9.3"
+  sha256 "ba29666289a10badeb8640e15da0e4edab31aab28667f9bd4269c4fa93bb32e3"
 
   url "https://download.studio3t.com/studio-3t/mac/#{version}/Studio-3T.dmg"
   appcast "https://files.studio3t.com/changelog/changelog.txt"
