126049780e14327597b4cbe175fc3bfb92abf807
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-28 22:36:35 +0000

    Update tropy from 1.8.0 to 1.8.1 (#91591)

--- Casks/tropy.rb
+++ Casks/tropy.rb
@@ -1,6 +1,6 @@
 cask "tropy" do
-  version "1.8.0"
-  sha256 "300efb2421d8595afc1e85bd74d370866e68affdf02f498b2b5ad8611fc93e16"
+  version "1.8.1"
+  sha256 "95ce861ab468f490cb9ca70810551847ef22d737563f48225b5c2cce5f06860c"
 
   # github.com/tropy/tropy/ was verified as official when first introduced to the cask
   url "https://github.com/tropy/tropy/releases/download/#{version}/tropy-#{version}.dmg"
