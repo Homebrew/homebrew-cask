ac58dee3b4853abdd9f8037a94e49cf2fa460ea5
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-28 22:26:38 +0000

    Update angry-ip-scanner from 3.7.2 to 3.7.3 (#91586)

--- Casks/angry-ip-scanner.rb
+++ Casks/angry-ip-scanner.rb
@@ -1,6 +1,6 @@
 cask "angry-ip-scanner" do
-  version "3.7.2"
-  sha256 "229e1c2dcb1fcccacd2816c7a0e1ad43733f7a09cf76df4ecd53ccdafee8bdda"
+  version "3.7.3"
+  sha256 "b411cbafc30aeeb59c69803b5f962f87a653fdf4a4a6f4292ecb6280978c0cc2"
 
   # github.com/angryip/ipscan/ was verified as official when first introduced to the cask
   url "https://github.com/angryip/ipscan/releases/download/#{version}/ipscan-mac-#{version}.zip"
