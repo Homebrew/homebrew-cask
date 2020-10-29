b90d6b88fd4809173fb02f9c9a81451b9dd69341
Bob Olde Hampsink <bob@robuust.digital>
2020-10-29 10:19:16 +0100

    Added zap stanza

--- Casks/tunein.rb
+++ Casks/tunein.rb
@@ -9,4 +9,13 @@ cask "tunein" do
   homepage "https://tunein.com/"
 
   app "TuneIn.app"
+
+  zap trash: [
+               '~/Library/Application Support/TuneIn',
+               '~/Library/Caches/com.tunein.desktop',
+               '~/Library/Caches/com.tunein.desktop.ShipIt',
+               '~/Library/Containers/com.getdropbox.dropbox.garcon',
+               '~/Library/Logs/TuneIn',
+               '~/Library/Preferences/com.tunein.desktop.plist',
+             ]
 end
