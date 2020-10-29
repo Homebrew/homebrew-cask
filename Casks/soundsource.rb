6a62ac4c1fc2ca385d3c3c88caa5cb95aa1ab945
Randall <ran.dall@icloud.com>
2020-10-29 16:33:59 -0700

    Update soundsource from 5.0.5 to 5.1.0

--- Casks/soundsource.rb
+++ Casks/soundsource.rb
@@ -1,6 +1,6 @@
 cask "soundsource" do
-  version "5.0.5"
-  sha256 "f4d0dce55aa7ba6303736342f0ea013496e0dfe36640a020956163aced552fee"
+  version "5.1.0"
+  sha256 "9363d0a2ba8a1118c0a16d13f88cb243557e54bb56ae50360e9f8c5a91e3bdb3"
 
   url "https://rogueamoeba.com/soundsource/download/SoundSource.zip"
   appcast "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.soundsource&system=10146&version=5000000"
