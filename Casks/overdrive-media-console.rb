6926fab8009a99ae29fb2c9c84ecfc19c34f0c63
Work <brigitte@corecode.io>
2020-10-29 13:07:16 +0100

    Update overdrive-media-console

--- Casks/overdrive-media-console.rb
+++ Casks/overdrive-media-console.rb
@@ -2,7 +2,8 @@ cask "overdrive-media-console" do
   version "1.2"
   sha256 "6e04ac61337647aa86c67d04d62552a0c0a31a3104bf4856aa726e8317272be9"
 
-  url "https://app.overdrive.com/downloads/ODMediaConsoleSetup.dmg"
+  # static.od-cdn.com was verified as official when first introduced to the cask
+  url "http://static.od-cdn.com/ODMediaConsoleSetup.dmg"
   name "OverDrive Media Console"
   homepage "https://www.overdrive.com/"
 
