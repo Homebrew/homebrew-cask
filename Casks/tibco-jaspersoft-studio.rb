ccee55d35462309cf925f988d091034e93bcf004
Work <brigitte@corecode.io>
2020-10-29 12:57:01 +0100

    Update tibco-jaspersoft-studio

--- Casks/tibco-jaspersoft-studio.rb
+++ Casks/tibco-jaspersoft-studio.rb
@@ -1,6 +1,6 @@
 cask "tibco-jaspersoft-studio" do
   version "6.15.0"
-  sha256 "00e5cf1bc27db5baf53e85d3bcc80422c7629be4382308a6f96c6497cb842166"
+  sha256 "423efe1eeff55747bdae9ec02db7d570fef66edbab63f37e646e9bc4c9acaa52"
 
   # sourceforge.net/jasperstudio/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/jasperstudio/JaspersoftStudio-#{version}/TIB_js-studiocomm_#{version}_macosx_x86_64.dmg"
