a24cc568bfdfb43da4150e408b1764492a6bfd88
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-29 00:15:05 +0000

    Update loom from 0.54.0 to 0.54.3 (#91612)
    * Update loom from 0.54.0 to 0.54.1
    
    * Update loom.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/loom.rb
+++ Casks/loom.rb
@@ -1,10 +1,11 @@
 cask "loom" do
-  version "0.54.0"
-  sha256 "5b719f885771c34f2223c77487954e57d2dd4de0777a75e057e7be00831f02f8"
+  version "0.54.3"
+  sha256 "030eed7ab1256ed9ce217af87666ffa400662b7acf17b7e15e533485022feebb"
 
   url "https://cdn.loom.com/desktop-packages/Loom-#{version}.dmg"
   appcast "https://s3-us-west-2.amazonaws.com/loom.desktop.packages/loom-inc-production/desktop-packages/latest-mac.yml"
   name "Loom"
+  desc "Screen and video recording software"
   homepage "https://www.loom.com/"
 
   auto_updates true
