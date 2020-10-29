f5eb42abd578e1189c32b358eb62d3d78d952d97
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-29 23:30:18 +0000

    Update marvel from 10.3.1 to 10.4 (#91706)

--- Casks/marvel.rb
+++ Casks/marvel.rb
@@ -1,6 +1,6 @@
 cask "marvel" do
-  version "10.3.1"
-  sha256 "3c28f919dd68cb16b05023345fb4a8f12266b4768bf1934d7b0e53f63e9cfe68"
+  version "10.4"
+  sha256 "104b93f111469981fc4d07c062d85cff4b9580b65bac50d387e5a799250a99f2"
 
   # storage.googleapis.com/sketch-plugin/ was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/sketch-plugin/#{version}/Marvel.zip"
