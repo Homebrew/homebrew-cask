86a8b2e4924b67c1a351c79fdb0f1997cd772723
mariobrostech <>
2020-10-28 22:51:53 -0400

    Add Spitfire Audio v3.2.16

---
+++ Casks/spitfire-audio.rb
@@ -0,0 +1,11 @@
+cask "spitfire-audio" do
+  version "3.2.16"
+  sha256 "b9f9dfb82415188ccc34920709f0f50438689a3639d2e0324fa941c237733170"
+
+  url "https://www.spitfireaudio.com/library-manager/download/mac/"
+  name "Spitfire Audio"
+  desc "Downloade manager for Spitfire audio libraries"
+  homepage "https://www.spitfireaudio.com/info/library-manager/"
+
+  app "Spitfire Audio.app"
+end
