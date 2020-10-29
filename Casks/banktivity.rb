42a3e83ae8af250466f956b58eb50bf1051ead44
suschizu <brigitte@corecode.io>
2020-10-29 21:33:46 +0100

    Update banktivity.rb

--- Casks/banktivity.rb
+++ Casks/banktivity.rb
@@ -3,7 +3,7 @@ cask "banktivity" do
   sha256 "86587d772564d64436b131e2d8131af79a0d49909c47f230a1ea2c8efb9a3ab5"
 
   # iggsoft.com/ was verified as official when first introduced to the cask
-  url "https://www.iggsoft.com/banktivity/Banktivity#{version}.dmg"
+  url "https://www.iggsoft.com/banktivity/Banktivity#{version.major}.dmg"
   appcast "https://www.iggsoft.com/banktivity/banktivity-versions-feed.json"
   name "Banktivity"
   desc "App to manage bank accounts in one place"
