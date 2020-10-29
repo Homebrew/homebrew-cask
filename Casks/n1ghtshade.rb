10c1cddb00d3cad61a4bbb16a029b606bc3a76f2
mariobrostech <>
2020-10-28 23:42:41 -0400

    Add n1ghtshade RC3

---
+++ Casks/n1ghtshade.rb
@@ -0,0 +1,12 @@
+cask "n1ghtshade" do
+  version "RC3"
+  sha256 "b9a5138511d6164bb2dfe0002dbc9f31ae157e45d7097edbd4f804172645e00e"
+
+  url "https://github.com/synackuk/n1ghtshade/releases/download/#{version}/n1ghtshade-macos.zip"
+  appcast "https://github.com/synackuk/n1ghtshade/releases.atom"
+  name "n1ghtshade"
+  desc "Permits the downgrade/jailbreak of 32-bit iOS devices"
+  homepage "https://github.com/synackuk/n1ghtshade"
+
+  app "n1ghtshade.app"
+end
