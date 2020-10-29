c48a15ae3ca61a618157928103258adb6d7e4f37
Work <brigitte@corecode.io>
2020-10-29 13:14:56 +0100

    Update securesafe from 2.7.2 to 2.8.0

--- Casks/securesafe.rb
+++ Casks/securesafe.rb
@@ -1,8 +1,9 @@
 cask "securesafe" do
-  version "2.7.2"
-  sha256 "79cefe64d6c4d6c501496e9b333a9bc785fe8d7a64b473df2216c4e58396d59b"
+  version "2.8.0"
+  sha256 "1e193ae806896b304fc4ce4c62e1bb3b0c3db07650924f94b7baed7803f5e372"
 
-  url "https://www.securesafe.com/downloads/SecureSafe_#{version}.pkg"
+  # dswiss.com/userdata/downloads was verified as official when first introduced to the cask
+  url "https://www.dswiss.com/userdata/downloads/securesafe-#{version}.pkg"
   appcast "https://www.securesafe.com/en/downloads/"
   name "SecureSafe"
   homepage "https://www.securesafe.com/"
