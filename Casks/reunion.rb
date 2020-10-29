757cb5d3780a7666cfbb192a47df5f23d23950ac
Randall <ran.dall@icloud.com>
2020-10-29 16:22:48 -0700

    Update reunion from 13.0.0.201022n to 13.0.0.201029n

--- Casks/reunion.rb
+++ Casks/reunion.rb
@@ -1,6 +1,6 @@
 cask "reunion" do
-  version "13.0.0.201022n"
-  sha256 "f5461c2f2f9335ff17ced876cccd73073267b9da439430a04f899ec5441e70f8"
+  version "13.0.0.201029n"
+  sha256 "b278d600761e8b3a73c81438bb4a4e1f5c9249de02e3ec70b50c92263053c53a"
 
   url "https://store.leisterpro.com/updates/reunion#{version.major}/Reunion-#{version.dots_to_hyphens}.zip"
   appcast "https://store.leisterpro.com/updates/reunion#{version.major}/appcast.xml",
