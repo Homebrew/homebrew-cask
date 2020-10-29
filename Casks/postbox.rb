b17a6fff3246341d40d215994c0ffc89962692b0
suschizu <brigitte@corecode.io>
2020-10-29 17:37:24 +0100

    Update postbox.rb

--- Casks/postbox.rb
+++ Casks/postbox.rb
@@ -1,6 +1,6 @@
 cask "postbox" do
   version "7.0.36"
-  sha256 "e562f6ee6c92d72eb7186bc6cf93c82a3c3e8546e2e37a9caab2d2d52bd70fa3"
+  sha256 "d77219a8978ade273aa10f081d1140e16039014dc94a73e12b00e939c708b7d4"
 
   # d3nx85trn0lqsg.cloudfront.net/mac/ was verified as official when first introduced to the cask
   url "https://d3nx85trn0lqsg.cloudfront.net/mac/postbox-#{version}-mac64.dmg"
