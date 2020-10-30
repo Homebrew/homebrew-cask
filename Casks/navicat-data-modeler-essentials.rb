9918b5f531dca16ff475d5f8cbb11c4d1b0cca36
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-30 03:04:10 +0000

    Update navicat-data-modeler-essentials from 3.0.13 to 3.0.14 (#91703)

--- Casks/navicat-data-modeler-essentials.rb
+++ Casks/navicat-data-modeler-essentials.rb
@@ -1,8 +1,8 @@
 cask "navicat-data-modeler-essentials" do
-  version "3.0.13"
-  sha256 "5ba54b21c56da6d5904140c8c7e98c6f4c49eb81731740cc89e52468b2f0a21c"
+  version "3.0.14"
+  sha256 "d0e7bcce415607ce2a556e6292e9983b3dbf82a4891d1e7586c73c637739d66b"
 
-  url "http://download3.navicat.com/updater/modeler0#{version.major_minor.no_dots}_ess_mac_en.zip"
+  url "https://download3.navicat.com/updater/modeler0#{version.major_minor.no_dots}_ess_mac_en.zip"
   appcast "https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20Data%20Modeler%20Essentials"
   name "Navicat Data Modeler Essentials"
   homepage "https://www.navicat.com/en/products/navicat-data-modeler"
