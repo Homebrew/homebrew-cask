f3117f9c55ddc3c5c140dc063974314119feb4ea
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-30 20:40:46 +0800

    Update overdrive-media-console.rb

--- Casks/overdrive-media-console.rb
+++ Casks/overdrive-media-console.rb
@@ -5,6 +5,7 @@ cask "overdrive-media-console" do
   # static.od-cdn.com was verified as official when first introduced to the cask
   url "http://static.od-cdn.com/ODMediaConsoleSetup.dmg"
   name "OverDrive Media Console"
+  desc "Get eBooks, audiobooks, and videos from your local library"
   homepage "https://www.overdrive.com/"
 
   pkg "OverDrive-Mac-Installer-Version-#{version}.pkg"
