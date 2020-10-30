68c69c4f779d5b0bc575957503938cbdc9220fd6
Micah Lee <micah@micahflee.com>
2020-10-30 10:45:32 -0700

    Add desc and fix new styles for Dangerzone

--- Casks/dangerzone.rb
+++ Casks/dangerzone.rb
@@ -1,3 +1,6 @@
+# typed: false
+# frozen_string_literal: true
+
 cask "dangerzone" do
   version "0.1.4"
   sha256 "cdd035d5f0d48b78c36e7e4df4a7218875213600ce8ae79c0309547c0068b4b1"
@@ -6,6 +9,7 @@ cask "dangerzone" do
   url "https://github.com/firstlookmedia/dangerzone/releases/download/v#{version}/Dangerzone.#{version}.dmg"
   appcast "https://github.com/firstlookmedia/dangerzone/releases.atom"
   name "Dangerzone"
+  desc "Convert potentially dangerous PDFs or Office documents into to safe PDFs"
   homepage "https://dangerzone.rocks/"
 
   app "Dangerzone.app"
