11632e459dd7b4787132e6e8be39e29c62847101
Giorgio Gallo <giorgio.gallo@bitnic.it>
2020-10-30 11:39:57 +0100

    Update itsycal from 0.12.4 to 0.12.5 (#91751)

--- Casks/itsycal.rb
+++ Casks/itsycal.rb
@@ -7,8 +7,8 @@ cask "itsycal" do
     sha256 "fda1ba5611deaf4d5b834118b3af37ea9c5d08d1f8c813d04e7dd0552a270e11"
     appcast "https://itsycal.s3.amazonaws.com/itsycal.xml"
   else
-    version "0.12.4"
-    sha256 "ce787786ac7631417f2fa9911773233733942031a979a939f0fb2d6f4824a29f"
+    version "0.12.5"
+    sha256 "f1cc5d6afc8e0b38ba436eeeb404d6e32f2f109658624d99363de8c32facff25"
     appcast "https://itsycal.s3.amazonaws.com/itsycal.xml"
   end
 
