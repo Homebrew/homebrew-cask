8428606f3c43c170572e7049b045662f170ec324
Randall <ran.dall@icloud.com>
2020-10-29 17:04:29 -0700

    Update eiskaltdcpp from 2.2.10-710-macOS10.13 to 2.2.10-720-macOS10.13

--- Casks/eiskaltdcpp.rb
+++ Casks/eiskaltdcpp.rb
@@ -1,6 +1,6 @@
 cask "eiskaltdcpp" do
-  version "2.2.10-710-macOS10.13"
-  sha256 "4850c5d703b12a82435ff0d55d5a2de19529ee5cdba900eac09d336ec1c6fd19"
+  version "2.2.10-720-macOS10.13"
+  sha256 "332de5b05d1346526fd7a9e4cd9b5795a260a5af9d2ef9e264909aaff2c99e70"
 
   url "https://downloads.sourceforge.net/eiskaltdcpp/EiskaltDC++-#{version}-x86_64.dmg"
   appcast "https://sourceforge.net/projects/eiskaltdcpp/rss"
