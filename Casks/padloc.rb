722a2a9b85a7e273d3b6698feeef43b1ed54f8f0
Malte Kiefer <malte@radermacher-consulting.de>
2020-10-30 05:27:11 +0100

    Update padloc from 3.1.2 to 3.1.3

--- Casks/padloc.rb
+++ Casks/padloc.rb
@@ -1,6 +1,6 @@
 cask "padloc" do
-  version "3.1.2"
-  sha256 "c87aa25e1947bff68b92ef6b44efaa9f10cd06d6270d0d6cac58bd67226d719e"
+  version "3.1.3"
+  sha256 "168dc11c372db5d9c09a0283cd19840d6dac6f76ec241ed400620103dd53b36a"
 
   # github.com/padloc/padloc/ was verified as official when first introduced to the cask
   url "https://github.com/padloc/padloc/releases/download/v#{version}/Padloc-#{version}.dmg"
