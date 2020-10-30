8e9cbe0c6ceb6ecd5437b6eb6dcb2775258a33db
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-30 20:16:48 +0800

    Update tunein.rb

--- Casks/tunein.rb
+++ Casks/tunein.rb
@@ -2,7 +2,7 @@ cask "tunein" do
   version "1.19.0"
   sha256 "591b77049d1e95e61335268fbaded1c6ace515d46b38ea8fb673ca873e3d16a5"
 
-  # amazonaws.com was verified as official when first introduced to the cask
+  # s3-us-west-2.amazonaws.com/gemini-desktop-prod/ was verified as official when first introduced to the cask
   url "https://s3-us-west-2.amazonaws.com/gemini-desktop-prod/release/TuneIn-#{version}.dmg"
   name "TuneIn"
   desc "Free Internet Radio"
