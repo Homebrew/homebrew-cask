0abe18a557e602ccf55f4f8fc41eab67a5e787a7
Randall <ran.dall@icloud.com>
2020-10-30 10:57:18 -0700

    Update sidequest from 0.10.17 to 0.10.18

--- Casks/sidequest.rb
+++ Casks/sidequest.rb
@@ -1,6 +1,6 @@
 cask "sidequest" do
-  version "0.10.17"
-  sha256 "2ff5f23083af9084c2072c52aa11b2a3634eddd0b7da943363bc793be47d45ab"
+  version "0.10.18"
+  sha256 "a04aac781b7426234a5891f93e4fdf141a7d1fe9e6f2253572b0601f1d8e356a"
 
   # github.com/SideQuestVR/SideQuest/ was verified as official when first introduced to the cask
   url "https://github.com/SideQuestVR/SideQuest/releases/download/v#{version}/SideQuest-#{version}.dmg"
