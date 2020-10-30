cdcc568cfe8f929932af0a690a14a08c1e65ef38
Robert Blair <12276457+robertleeblairjr@users.noreply.github.com>
2020-10-30 08:14:08 -0500

    Update vellum from 2.7.3 to 2.8.0 (#91655)
    * Update vellum from 2.7.3 to 2.8.0
    
    * Update vellum.rb
    
    Added 'desc'.
    
    * Update vellum.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/vellum.rb
+++ Casks/vellum.rb
@@ -1,11 +1,12 @@
 cask "vellum" do
-  version "2.8.0"
+  version "2.8"
   sha256 "c6a0c70ce55fccd47c961ab8458690e5354bba61f446250aa3b2f3565a7f53b8"
 
   # 180g.s3.amazonaws.com/downloads/ was verified as official when first introduced to the cask
-  url "https://180g.s3.amazonaws.com/downloads/Vellum-#{version.no_dots}00.zip"
+  url "https://180g.s3.amazonaws.com/downloads/Vellum-#{version.no_dots}000.zip"
   appcast "https://get.180g.co/updates/vellum/"
   name "Vellum"
+  desc "Ebook creation software"
   homepage "https://vellum.pub/"
 
   auto_updates true
