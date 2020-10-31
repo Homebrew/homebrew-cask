fee8a162a40caf91802c1d526cf81441fc992820
Alessandro (Ale) Segala <43508+ItalyPaleAle@users.noreply.github.com>
2020-10-31 21:14:19 +0000

    Update zipcleaner.rb

--- Casks/zipcleaner.rb
+++ Casks/zipcleaner.rb
@@ -3,6 +3,7 @@ cask "zipcleaner" do
   sha256 "bfdb7d451bc2b0e8f65711e09690dc4527ed924f0d187e4c7eacd0d824e24c58"
 
   url "https://github.com/AmigaAbattoir/ZipCleaner/releases/download/V#{version}/ZipCleaner.app.zip"
+  appcast "https://github.com/AmigaAbattoir/ZipCleaner/releases.atom"
   name "ZipCleaner"
   homepage "https://github.com/AmigaAbattoir/ZipCleaner"
 
