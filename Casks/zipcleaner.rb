d98525b2ad4adfbbf2dac8bd47fa7d67d40322b6
Alessandro (Ale) Segala <43508+ItalyPaleAle@users.noreply.github.com>
2020-10-31 13:39:39 -0700

    Using a 64-bit build of ZipCleaner

--- Casks/zipcleaner.rb
+++ Casks/zipcleaner.rb
@@ -1,10 +1,10 @@
 cask "zipcleaner" do
   version :latest
-  sha256 :no_check
+  sha256 "bfdb7d451bc2b0e8f65711e09690dc4527ed924f0d187e4c7eacd0d824e24c58"
 
-  url "https://roger-jolly.nl/software/downloads/zipcleaner/ZipCleaner.zip"
+  url "https://github.com/AmigaAbattoir/ZipCleaner/releases/download/V1.1.0/ZipCleaner.app.zip"
   name "ZipCleaner"
-  homepage "https://roger-jolly.nl/software/#zipcleaner"
+  homepage "https://github.com/AmigaAbattoir/ZipCleaner"
 
   app "ZipCleaner.app"
 end
