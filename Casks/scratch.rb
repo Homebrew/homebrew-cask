18449c3f481906159484c8697332ab122644edc4
Brigitte Zmoelnig <brigitte@corecode.io>
2020-10-31 19:37:05 +0100

    Update scratch from 3.15.0 to 3.17.1

--- Casks/scratch.rb
+++ Casks/scratch.rb
@@ -1,8 +1,8 @@
 cask "scratch" do
-  version "3.15.0"
-  sha256 "fa3459ffdf01453e41aa8b9f0f43ad6da88e50006aa04d9b525370bc770b66f0"
+  version "3.17.1"
+  sha256 "e3b84a84afa75f0b42cd0cbf605ba60a3172a9f8f9c8598c6a5af13ae2cc347d"
 
-  url "https://downloads.scratch.mit.edu/desktop/Scratch%20Desktop-#{version}.dmg"
+  url "https://downloads.scratch.mit.edu/desktop/Scratch%20#{version}.dmg"
   name "Scratch"
   desc "Programs interactive stories, games, and animations"
   homepage "https://scratch.mit.edu/download"
