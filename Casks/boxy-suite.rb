fc3677532f9f6aa9ddb3a74370610677ccc5a819
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-01 18:35:34 +0800

    Update boxy-suite.rb

--- Casks/boxy-suite.rb
+++ Casks/boxy-suite.rb
@@ -2,6 +2,7 @@ cask "boxy-suite" do
   version :latest
   sha256 :no_check
 
+  # boxyteam-static.s3.amazonaws.com/release/ was verified as official when first introduced to the cask
   url "https://boxyteam-static.s3.amazonaws.com/release/Boxy%20Suite.dmg"
   name "Boxy Suite"
   desc "Gmail, Calendar, Keep and Contacts apps"
