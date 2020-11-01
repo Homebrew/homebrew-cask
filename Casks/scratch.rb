25e78a9453f779e1180806e25125d13991ddafa6
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-01 11:33:36 +0800

    Update scratch.rb

--- Casks/scratch.rb
+++ Casks/scratch.rb
@@ -3,11 +3,12 @@ cask "scratch" do
   sha256 "e3b84a84afa75f0b42cd0cbf605ba60a3172a9f8f9c8598c6a5af13ae2cc347d"
 
   url "https://downloads.scratch.mit.edu/desktop/Scratch%20#{version}.dmg"
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://downloads.scratch.mit.edu/desktop/Scratch.dmg"
   name "Scratch"
   desc "Programs interactive stories, games, and animations"
   homepage "https://scratch.mit.edu/download"
 
   depends_on macos: ">= :high_sierra"
 
-  app "Scratch Desktop.app"
+  app "Scratch #{version.major}.app"
 end
