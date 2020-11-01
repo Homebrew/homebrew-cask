a4f6174809cfd9a39859aadc1b6cbf40700a3bc1
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-01 10:33:20 +0800

    Update chromium from 820584 to 822990 (#91842)

--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -1,6 +1,6 @@
 cask "chromium" do
-  version "820584"
-  sha256 "4f7e5d33b499e6939fbcc5d3ebcca0336880616605f4118a337e5c06cae2bae3"
+  version "822990"
+  sha256 "bdc691b1ae8cae324cc91876e26b593e97bf5f7119338135ca11cbe331482acf"
 
   # commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/ was verified as official when first introduced to the cask
   url "https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/#{version}/chrome-mac.zip"
