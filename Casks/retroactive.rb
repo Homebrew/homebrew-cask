e3147b11d9e45f9bc290cb0ce8c3d12f915256cc
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-01 13:57:39 +0800

    Update retroactive.rb

--- Casks/retroactive.rb
+++ Casks/retroactive.rb
@@ -2,16 +2,13 @@ cask "retroactive" do
   version "1.9"
   sha256 "6f856bcdd62a25d31a16670a7091401f173059194136bfa5f83caae2255b28ae"
 
-  # github.com/cormiertyshawn895/Retroactive/ was verified as official when first introduced to the cask
   url "https://github.com/cormiertyshawn895/Retroactive/releases/download/#{version}/Retroactive.#{version}.zip"
   appcast "https://github.com/cormiertyshawn895/Retroactive/releases.atom"
   name "Retroactive"
-  desc "Patch many of Apple's apps to run on newer or older versions of macOS"
+  desc "Run Apple apps on incompatible OS versions"
   homepage "https://github.com/cormiertyshawn895/Retroactive"
 
-  app "Retroactive (right click to open).app"
+  app "Retroactive #{version}/Retroactive (right click to open).app"
 
-  zap delete: [
-    "~/Library/Caches/com.retroactive.Retroactive",
-  ]
+  zap delete: "~/Library/Caches/com.retroactive.Retroactive"
 end
