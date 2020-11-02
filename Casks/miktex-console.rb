fcdc958be65a94d81c4928d5e6867bae288ad15e
Randall <ran.dall@icloud.com>
2020-11-02 11:09:26 -0800

    Update miktex-console from 20.10 to 20.11

--- Casks/miktex-console.rb
+++ Casks/miktex-console.rb
@@ -1,6 +1,6 @@
 cask "miktex-console" do
-  version "20.10"
-  sha256 "64d72cb92aeda3c62b7bce4ef164f51a2367756bd10131e9860f1a6abab85cce"
+  version "20.11"
+  sha256 "859ec3032fec5c6d1b72c947eeb8f0ae93e8cbd584e02093a451964b814d4d11"
 
   url "https://miktex.org/download/ctan/systems/win32/miktex/setup/darwin-x86_64/miktex-#{version}-darwin-x86_64.dmg"
   appcast "https://miktex.org/download"
