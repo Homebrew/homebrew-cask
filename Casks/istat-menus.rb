f56e3f4afe84da28b52d9d385b3bb8db9ed0389e
tiiiecherle <tiiiecherle@gmail.com>
2020-11-02 09:42:21 +0100

    Update istat-menus from 6.40 to 6.50

--- Casks/istat-menus.rb
+++ Casks/istat-menus.rb
@@ -1,8 +1,9 @@
 cask "istat-menus" do
-  version "6.40"
+  version "6.50"
   sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://files.bjango.com/istatmenus#{version.major}/istatmenus#{version}.zip"
+  # s3.amazonaws.com/bjango was verified as official when first introduced to the cask
+  url "https://s3.amazonaws.com/bjango/files/istatmenus#{version.major}/istatmenus#{version}.zip"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.bjango.com/istatmenus/"
   name "iStats Menus"
   desc "System monitoring app"
