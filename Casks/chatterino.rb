69cbb0b1c626d66274c459476fdfeab69a1ecd1b
Pessi Päivärinne <pessip@hotmail.fi>
2020-11-02 22:21:27 +0200

    Add chatterino cask

---
+++ Casks/chatterino.rb
@@ -0,0 +1,12 @@
+cask "chatterino" do
+    version "2.2.2"
+    sha256 "00540c54962d0a90e58898a32472638810da544071417a19c6998c9e80e0af58"
+  
+    url "https://chatterino.com/download/#{version}/Chatterino.dmg"
+    appcast "https://chatterino.com/changelog"
+    name "Chatterino"
+    desc "Chatterino is a chat client for twitch chat. It aims to be an improved/extended version of the twitch web chat."
+    homepage "https://chatterino.com/"
+  
+    app "chatterino.app"
+  end
