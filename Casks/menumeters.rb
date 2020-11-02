03551dd0481ba5adf0fe37f33ee3b62ef442c22e
David Yantis <404NetworkError@users.noreply.github.com>
2020-11-01 21:28:47 -0500

    Update menumeters from 2.0.8.1 to 2.0.8.2 (#91873)

--- Casks/menumeters.rb
+++ Casks/menumeters.rb
@@ -1,6 +1,6 @@
 cask "menumeters" do
-  version "2.0.8.1"
-  sha256 "f1ef3195ae46bf3ba3a6b3d7a6f802742fa21bb2e726396b58dccfdda66b5cae"
+  version "2.0.8.2"
+  sha256 "85d284e080251558fc840d54ee343ae004ab89594b1941a90f2a2cc4bcec198b"
 
   # github.com/yujitach/MenuMeters was verified as official when first introduced to the cask
   url "https://github.com/yujitach/MenuMeters/releases/download/#{version}/MenuMeters_#{version}.zip"
