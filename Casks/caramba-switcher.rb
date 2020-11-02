9ad40073b82f3cae5505d80ed56f964ae16c8002
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-02 08:48:56 +0800

    Update caramba-switcher.rb

--- Casks/caramba-switcher.rb
+++ Casks/caramba-switcher.rb
@@ -2,24 +2,14 @@ cask "caramba-switcher" do
   version :latest
   sha256 :no_check
 
-  # download.caramba.tech was verified as official when first introduced to the cask
+  # download.caramba.tech/apps/switcher/ was verified as official when first introduced to the cask
   url "https://download.caramba.tech/apps/switcher/mac/CarambaSwitcherBeta.pkg"
   name "Caramba Switcher"
-  desc "Experimental, intelligent, self-turning keyboard switcher"
+  desc "Keyboard switcher"
   homepage "https://caramba-switcher.com/"
 
   pkg "CarambaSwitcherBeta.pkg"
 
   uninstall quit:    "tech.caramba.switcher",
             pkgutil: "tech.caramba.switcher"
-
-  zap trash: [
-    "/System/Volumes/Data/private/var/db/receipts/tech.caramba.switcher.bom",
-    "/System/Volumes/Data/private/var/db/receipts/tech.caramba.switcher.plist",
-    "/System/Volumes/Data/usr/local/Caskroom/caramba-switcher",
-    "/System/Volumes/Data/usr/local/Caskroom/caramba-switcher",
-    "/usr/local/Caskroom/caramba-switcher",
-    "~/Library/Caches/Homebrew/Cask/caramba-switcher--:latest.pkg",
-    "~/Library/Caches/Homebrew/Cask/caramba-switcher--latest.pkg",
-  ]
 end
