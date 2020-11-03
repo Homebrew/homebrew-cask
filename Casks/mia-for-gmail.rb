eb224eaf970aafba0af158473ef99d526187ff94
Julian Mayer <julian@corecode.io>
2020-11-03 22:09:40 +0100

    Update mia-for-gmail from 2.4.5 to 2.4.4

--- Casks/mia-for-gmail.rb
+++ Casks/mia-for-gmail.rb
@@ -1,5 +1,5 @@
 cask "mia-for-gmail" do
-  version "2.4.5"
+  version "2.4.4"
   sha256 "4130b480c013862b7d61d678409d915b1b09564a1c37380cdd29db717e37eed7"
 
   url "https://www.miaforgmail.com/application/notifier-pro-for-gmail/MiaForGmail.dmg"
