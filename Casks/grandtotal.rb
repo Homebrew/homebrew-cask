369ce7b46eb5f3531cf66651f4dda395605bc872
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-03 16:59:40 +0000

    Update grandtotal from 6.2 to 6.2.1 (#91953)

--- Casks/grandtotal.rb
+++ Casks/grandtotal.rb
@@ -1,6 +1,6 @@
 cask "grandtotal" do
-  version "6.2"
-  sha256 "042d743514ad94f3c933810cee8e2ce247771a4692fddf10d2bd28f0175b3cf3"
+  version "6.2.1"
+  sha256 "9c8a864a8e4062b3934186719535bb8c01af4f2ec348632f2ffb28516ee1f09e"
 
   url "https://mediaatelier.com/GrandTotal#{version.major}/GrandTotal_#{version}.zip"
   appcast "https://mediaatelier.com/GrandTotal#{version.major}/feed.php"
