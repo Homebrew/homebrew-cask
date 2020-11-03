915fdca24728bc365e81757339537a18c397cb4a
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-03 09:46:34 +0800

    Update epic.rb

--- Casks/epic.rb
+++ Casks/epic.rb
@@ -5,6 +5,7 @@ cask "epic" do
   url "https://cdn.epicbrowser.com/mac2/epic_#{version}.dmg"
   appcast "https://epicbrowser.com/thank_you.php"
   name "Epic Privacy Browser"
+  desc "Private, secure web browser"
   homepage "https://www.epicbrowser.com/"
 
   app "Epic.app"
