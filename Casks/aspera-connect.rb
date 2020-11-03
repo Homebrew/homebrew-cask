0135a67212e51a73de536dcfe0e668f6a3eebf2c
Work <brigitte@corecode.io>
2020-11-03 18:16:16 +0100

    Update aspera-connect from 3.10.0.180973 to 3.10.1.181943

--- Casks/aspera-connect.rb
+++ Casks/aspera-connect.rb
@@ -1,8 +1,9 @@
 cask "aspera-connect" do
-  version "3.10.0.180973"
-  sha256 "12fbb7468590136b408f108ae209522f78e2ea03dab61572bb5002337c3a9dcc"
+  version "3.10.1.181943"
+  sha256 "aff234db6ce414044a7932926137a8f451dd6bcc026c1b5f067283d43e1d18c7"
 
-  url "https://download.asperasoft.com/download/sw/connect/#{version.major_minor_patch}/IBMAsperaConnectInstaller-#{version}.dmg"
+  # d3gcli72yxqn2z.cloudfront.net was verified as official when first introduced to the cask
+  url "https://d3gcli72yxqn2z.cloudfront.net/connect_#{version.dots_to_underscores}_ga/v4/bin/IBMAsperaConnectInstallerOneClick-#{version}.dmg"
   appcast "https://downloads.asperasoft.com/connect2/",
           must_contain: version.dots_to_underscores
   name "Aspera Connect"
