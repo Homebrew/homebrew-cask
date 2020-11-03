86b71982b7c1ec0a66d14b9b88ab2b2a5505bc37
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-03 17:30:53 +0000

    Update ridibooks from 0.8.8 to 0.8.9 (#91968)

--- Casks/ridibooks.rb
+++ Casks/ridibooks.rb
@@ -1,6 +1,6 @@
 cask "ridibooks" do
-  version "0.8.8"
-  sha256 "90e78c8baf3b9b40860d165f582d710110cfb08c5c4b16f7e40ddb34ef2164de"
+  version "0.8.9"
+  sha256 "5221ca3b7ef94d84fd1e1e452645eeec745bfce8e89e25a2f41adb2195bd2ae1"
 
   # viewer-ota.ridicdn.net/pc_electron/ was verified as official when first introduced to the cask
   url "https://viewer-ota.ridicdn.net/pc_electron/Ridibooks-#{version}.dmg"
