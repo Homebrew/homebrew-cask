022bf52a98fd96327b54b4cd583f76c6a9332120
Work <brigitte@corecode.io>
2020-11-03 09:55:22 +0100

    Update switchhosts from 3.5.5.5541 to 3.5.6.5551

--- Casks/switchhosts.rb
+++ Casks/switchhosts.rb
@@ -1,6 +1,6 @@
 cask "switchhosts" do
-  version "3.5.5.5541"
-  sha256 "ad408c68d066674768591414724840d1076a3a88ac38f0c0e29dd399d6230925"
+  version "3.5.6.5551"
+  sha256 "600a19939862160daee29747d6c9d41275bd1d6aa8ad9ec52aec29c2b62e6c9f"
 
   # github.com/oldj/SwitchHosts/ was verified as official when first introduced to the cask
   url "https://github.com/oldj/SwitchHosts/releases/download/v#{version.major_minor_patch}/SwitchHosts._macOS_#{version}.dmg"
