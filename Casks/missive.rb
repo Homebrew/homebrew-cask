4d646fe94cd02116cf94b3243378e5e5d41668f6
Randall <ran.dall@icloud.com>
2020-11-03 09:13:29 -0800

    Update missive from 9.41.0 to 9.41.2

--- Casks/missive.rb
+++ Casks/missive.rb
@@ -1,6 +1,6 @@
 cask "missive" do
-  version "9.41.0"
-  sha256 "80f4dd7ded9dc6fbe0574d39e66bf9c66ed18eeb0d7ab546507dabdd229ce57e"
+  version "9.41.2"
+  sha256 "d77acfe015442579e1546faf3acf62d7a0d15cf265d09e6aa756c3535f050722"
 
   # d1weoh55n7oehu.cloudfront.net/ was verified as official when first introduced to the cask
   url "https://d1weoh55n7oehu.cloudfront.net/#{version}/Missive-#{version}.dmg"
