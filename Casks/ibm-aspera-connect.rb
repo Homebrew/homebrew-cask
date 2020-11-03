5cc21357efa0474149dfb5d74b4bc5bbf6428fcb
Vítor Galvão <info@vitorgalvao.com>
2020-11-03 23:47:26 +0000

    Update and rename aspera-connect.rb to ibm-aspera-connect.rb

--- Casks/ibm-aspera-connect.rb
+++ Casks/ibm-aspera-connect.rb
@@ -1,12 +1,12 @@
-cask "aspera-connect" do
+cask "ibm-aspera-connect" do
   version "3.10.1.181943"
   sha256 "aff234db6ce414044a7932926137a8f451dd6bcc026c1b5f067283d43e1d18c7"
 
   # d3gcli72yxqn2z.cloudfront.net was verified as official when first introduced to the cask
   url "https://d3gcli72yxqn2z.cloudfront.net/connect_#{version.dots_to_underscores}_ga/v4/bin/IBMAsperaConnectInstallerOneClick-#{version}.dmg"
-  appcast "https://downloads.asperasoft.com/connect2/",
+  appcast "https://www.ibm.com/aspera/connect/",
           must_contain: version.dots_to_underscores
-  name "Aspera Connect"
+  name "IBM Aspera Connect"
   homepage "https://www.ibm.com/aspera/connect/"
 
   installer manual: "IBM Aspera Connect Installer.app"
