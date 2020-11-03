cf0312851ef118393fd68dd47f838621161644ca
Adam Gibbins <adam@adamgibbins.com>
2020-11-03 23:35:50 +0000

    Update obsidian from 0.9.6 to 0.9.10 (#91982)

--- Casks/obsidian.rb
+++ Casks/obsidian.rb
@@ -1,6 +1,6 @@
 cask "obsidian" do
-  version "0.9.6"
-  sha256 "a86e4c2193011ef4a6467f4d8b8bfaa4013d06eb2fd944605fea7322fc4997e0"
+  version "0.9.10"
+  sha256 "60a3484efbef1e6a809d92c6d0118ff4ea047f290fe84dce18d37c23156edfff"
 
   # github.com/obsidianmd/ was verified as official when first introduced to the cask
   url "https://github.com/obsidianmd/obsidian-releases/releases/download/v#{version}/Obsidian-#{version}.dmg"
