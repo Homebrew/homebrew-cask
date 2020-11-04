e505544411d600f036ee035593eedfaa9b040aa0
Alexander <a@skwar.me>
2020-11-04 12:47:28 +0100

    Update icab from 6.0.5 to 6.0.5.1 (#91990)
    * Update icab from 6.0.5 to 6.0.5.1
    
    * Update icab.rb
    
    * Update icab.rb
    
    Co-authored-by: Alexander Skwar <alexander@tutti.ch>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/icab.rb
+++ Casks/icab.rb
@@ -1,9 +1,9 @@
 cask "icab" do
-  version "6.0.5"
-  sha256 "b2a5c24c3be117800ce36762b3f9f390720cea00aef654d07a09d540ca19f76a"
+  version "6.0.5.1"
+  sha256 "d3910c63ba1036df5048b1cacafd4b1d41e9d06517438322791256cbe50aa848"
 
   # icab.clauss-net.de/ was verified as official when first introduced to the cask
-  url "http://icab.clauss-net.de/icab/iCab_#{version}.zip"
+  url "http://icab.clauss-net.de/icab/iCab_#{version.major_minor_patch}.zip"
   appcast "http://www.icab.de/download.html",
           must_contain: version.major_minor
   name "iCab"
@@ -12,5 +12,5 @@ cask "icab" do
 
   depends_on macos: ">= :high_sierra"
 
-  app "iCab #{version}/iCab.app"
+  app "iCab #{version.major_minor_patch}/iCab.app"
 end
