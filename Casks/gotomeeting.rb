5fbaf7101ea89f947705d453542d746cc2f78bc6
Goldgamer <11319871+iMonZ@users.noreply.github.com>
2020-11-04 19:52:24 +0100

    Update gotomeeting.rb

--- Casks/gotomeeting.rb
+++ Casks/gotomeeting.rb
@@ -1,5 +1,5 @@
 cask "gotomeeting" do
-  version "10.13.0,18705"
+  version "10.13.1,18705"
   sha256 "e8f62a7715e6e28f3973b653d3ac8a771d6dc2183311234e8e47ace5e56f4b17"
 
   # builds.cdn.getgo.com/builds/g2m/ was verified as official when first introduced to the cask
