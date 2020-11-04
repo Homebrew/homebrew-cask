97ca07da12e29add79ccb949e181686c54f6d078
Phuc Nguyen Minh <minhphuc429@gmail.com>
2020-11-04 13:10:18 +0700

    Update plex.rb

--- Casks/plex.rb
+++ Casks/plex.rb
@@ -1,6 +1,6 @@
 cask "plex" do
-  version "1.21.0.1410-50a34597"
-  sha256 "7daf8eb0faf843d697fc0d63a65d4ee9db51de3bb2e36d404c4e79a8e12d91af"
+  version "1.22.0.1421-be6a7c42"
+  sha256 "4714a6799dbd7454c5047d9ca350fa0dbbdc7940955e439a0d0df405e2527e9c"
 
   url "https://downloads.plex.tv/plex-desktop/#{version}/macos/Plex-#{version}-x86_64.zip"
   appcast "https://plex.tv/api/downloads/6.json"
