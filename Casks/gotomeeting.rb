5f6deda27c9e66174b1b62abbea941e1c2eeb28f
Vítor Galvão <info@vitorgalvao.com>
2020-11-04 16:36:07 +0000

    Update gotomeeting.rb

--- Casks/gotomeeting.rb
+++ Casks/gotomeeting.rb
@@ -1,15 +1,13 @@
 cask "gotomeeting" do
-  version "10.12.0,18425"
-  sha256 "ac6ecdd0f45f50bbae3ff2d4b1e02ba404af28027f76079c8d0e67a3484439d9"
+  version :latest
+  sha256 :no_check
 
-  # builds.cdn.getgo.com/builds/g2m/ was verified as official when first introduced to the cask
-  url "https://builds.cdn.getgo.com/builds/g2m/#{version.after_comma}/GoToMeeting.dmg"
-  appcast "https://support.goto.com/meeting/help/whats-new-g2m010012"
-  name "GotoMeeting"
-  desc "Online meeting, desktop sharing and video conferencing software package"
+  url "https://link.gotomeeting.com/latest-dmg"
+  name "GoToMeeting"
+  desc "Online meetings, desktop sharing, and video conferencing"
   homepage "https://www.goto.com/meeting"
 
-  app "GotoMeeting.app"
+  app "GoToMeeting.app"
 
   zap trash: [
     "~/Library/Application Support/LogMeInInc/GoToMeeting",
