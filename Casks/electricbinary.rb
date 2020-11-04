7848e3bd953d747738d7213d9a5b56bf70385180
William Goeller <william@williamgoeller.com>
2020-11-04 11:48:06 -0500

    switch to https

--- Casks/electricbinary.rb
+++ Casks/electricbinary.rb
@@ -3,7 +3,7 @@ cask "electricbinary" do
   sha256 "3e383f673183265700e0c13a5f7b1d3348247465c3d8817c845d62e4c92d86ce"
 
   url "https://ftp.gnu.org/pub/gnu/electric/electricBinary-#{version}.jar"
-  appcast "http://ftp.gnu.org/pub/gnu/electric/"
+  appcast "https://ftp.gnu.org/pub/gnu/electric/"
   name "Electric VLSI Design System"
   desc "Electrical CAD system for the design of integrated circuits"
   homepage "https://www.gnu.org/software/electric/electric.html"
