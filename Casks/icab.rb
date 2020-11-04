9492945eda968a537b7b83b8e19dfeda6056db1c
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-04 19:39:40 +0800

    Update icab.rb

--- Casks/icab.rb
+++ Casks/icab.rb
@@ -1,9 +1,9 @@
 cask "icab" do
   version "6.0.5.1"
-  sha256 "c51322df747350c18c41cb09ce6deb3b545000cfe161dad769c6bc65a598b9af"
+  sha256 "d3910c63ba1036df5048b1cacafd4b1d41e9d06517438322791256cbe50aa848"
 
   # icab.clauss-net.de/ was verified as official when first introduced to the cask
-  url "http://icab.clauss-net.de/icab/iCab_#{version}.zip"
+  url "http://icab.clauss-net.de/icab/iCab_#{version.major_minor_patch}.zip"
   appcast "http://www.icab.de/download.html",
           must_contain: version.major_minor
   name "iCab"
