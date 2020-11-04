73168dd76f61e1be2e90a291a6d7e930b37a4b3e
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-04 19:27:27 +0800

    Update mia-for-gmail.rb

--- Casks/mia-for-gmail.rb
+++ Casks/mia-for-gmail.rb
@@ -2,6 +2,7 @@ cask "mia-for-gmail" do
   version "2.4.5"
   sha256 "31f94c2c5bafc2550adb50ed36919b45c2939bbd691ce1b27b8486d1094600c5"
 
+  # sovapps.com/application/notifier-pro-for-gmail/ was verified as official when first introduced to the cask
   url "https://www.sovapps.com/application/notifier-pro-for-gmail/mia.#{version}.zip"
   appcast "https://www.sovapps.com/application/notifier-pro-for-gmail/notifier.xml"
   name "Mia for Gmail"
@@ -12,13 +13,13 @@ cask "mia-for-gmail" do
 
   app "Mia for Gmail.app"
 
-  uninstall quit:       "com.sovapps.gmailnotifier"
+  uninstall quit: "com.sovapps.gmailnotifier"
 
   zap trash: [
-    "~/Library/Preferences/com.sovapps.gmailnotifier.plist",
-    "~/Library/Caches/com.sovapps.gmailnotifier",
-    "~/Library/Containers/com.sovapps.launchAtLoginHelper",
     "~/Library/Application Scripts/com.sovapps.gmailnotifier",
     "~/Library/Application Scripts/com.sovapps.launchAtLoginHelper",
+    "~/Library/Caches/com.sovapps.gmailnotifier",
+    "~/Library/Containers/com.sovapps.launchAtLoginHelper",
+    "~/Library/Preferences/com.sovapps.gmailnotifier.plist",
   ]
 end
