c24ef5e1466346961e6ed6bcc68cd6076e9291fc
Seeker <meaningseeking@protonmail.com>
2020-11-04 13:29:09 -0800

    Update solar2d from 2020.3623 to 2020.3624

--- Casks/solar2d.rb
+++ Casks/solar2d.rb
@@ -1,6 +1,6 @@
 cask "solar2d" do
-  version "2020.3623"
-  sha256 "74912541858a86cccfa054e6adf7ad5be4ec118161bdeaea6c37be953830010d"
+  version "2020.3624"
+  sha256 "da280322a9286ec1c1ae771907df961e99288f9070f69fc397caed4b073763d6"
 
   # github.com/coronalabs/corona was verified as official when first introduced to the cask
   url "https://github.com/coronalabs/corona/releases/download/#{version.minor}/Solar2D-macOS-#{version}.dmg"
