18ee7cf298a6ff4716a2928d5194f9b9aa63021f
Vítor Galvão <info@vitorgalvao.com>
2020-11-04 15:43:49 +0000

    gotomeeting.rb: update to 10.13.1,18705

--- Casks/gotomeeting.rb
+++ Casks/gotomeeting.rb
@@ -1,10 +1,11 @@
 cask "gotomeeting" do
-  version "10.12.0,18425"
+  version "10.13.0,18705"
   sha256 "ac6ecdd0f45f50bbae3ff2d4b1e02ba404af28027f76079c8d0e67a3484439d9"
 
   # builds.cdn.getgo.com/builds/g2m/ was verified as official when first introduced to the cask
   url "https://builds.cdn.getgo.com/builds/g2m/#{version.after_comma}/GoToMeeting.dmg"
-  appcast "https://support.goto.com/meeting/help/whats-new-g2m010012"
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://link.gotomeeting.com/latest-dmg",
+          must_contain: version.after_comma
   name "GotoMeeting"
   desc "Online meeting, desktop sharing and video conferencing software package"
   homepage "https://www.goto.com/meeting"
