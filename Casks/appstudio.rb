9420ebfa41666cb191faf43718bd33511a951dcb
suschizu <brigitte@corecode.io>
2020-11-04 16:46:48 +0100

    Update appstudio (#92003)

--- Casks/appstudio.rb
+++ Casks/appstudio.rb
@@ -2,8 +2,8 @@ cask "appstudio" do
   version :latest
   sha256 :no_check
 
-  # appstudio.s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://appstudio.s3.amazonaws.com/AppStudio.dmg"
+  # appstudio.us-east-1.linodeobjects.com/ was verified as official when first introduced to the cask
+  url "https://appstudio.us-east-1.linodeobjects.com/AppStudio.dmg"
   name "NSB/AppStudio"
   homepage "https://www.nsbasic.com/"
 
