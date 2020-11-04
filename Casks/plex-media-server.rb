fee4564df9046ce0fc12199142cd17d1dd608589
Robert Blair <robertleeblairjr@outlook.com>
2020-11-04 07:22:23 -0600

    Update plex-media-server from 1.20.3.3483-211702a9f to 1.20.4.3517-ab5e1197c

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -1,6 +1,6 @@
 cask "plex-media-server" do
-  version "1.20.3.3483-211702a9f"
-  sha256 "dd27216957245b2446249615457d4d5248f7ac1f5bfd7595c138a09026143a76"
+  version "1.20.4.3517-ab5e1197c"
+  sha256 "df86067612b85b6a7adfddb29b32c13e4867b47c88fb970b1bfd722da062e74e"
 
   url "https://downloads.plex.tv/plex-media-server-new/#{version}/macos/PlexMediaServer-#{version}-x86_64.zip"
   appcast "https://plex.tv/api/downloads/5.json"
