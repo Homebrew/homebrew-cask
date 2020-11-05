45f5f3630423410da6a626b60b9809170d929756
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-05 22:17:19 +0000

    Update garagesale from 8.2 to 8.2.1 (#92075)

--- Casks/garagesale.rb
+++ Casks/garagesale.rb
@@ -1,6 +1,6 @@
 cask "garagesale" do
-  version "8.2"
-  sha256 "ce13d8c96d6d38afcbc517e1d09c0fc926eff3813763f9d3dd90da95b78f9a30"
+  version "8.2.1"
+  sha256 "46b6b3fcc6db3f39b74a202bed0a1c8e2db6b701f61f4f6bbdb94e3a85c2f05f"
 
   url "https://downloads.iwascoding.com/downloads/GarageSale_#{version}.dmg"
   appcast "https://www.iwascoding.com/GarageSale/Downloads.html#VersionHistory"
