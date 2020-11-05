d8631482d61b722ba74ecf5917507fba904cb648
Randall <ran.dall@icloud.com>
2020-11-05 13:58:46 -0800

    Update melodics from 2.1.5134 to 2.1.5305

--- Casks/melodics.rb
+++ Casks/melodics.rb
@@ -1,6 +1,6 @@
 cask "melodics" do
-  version "2.1.5134"
-  sha256 "cf5800a8f633e47ec939a335f47f90aa32197a42c3ec3e0599b54ef8a6a487a6"
+  version "2.1.5305"
+  sha256 "b51a78eaebf395d7993abdb9705e98811bfdc169dfec482cb269212321c27f35"
 
   url "https://web-cdn.melodics.com/download/MelodicsV#{version.major}.dmg"
   appcast "https://web-cdn.melodics.com/download/osxupdatescastv#{version.major}.xml"
