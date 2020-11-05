3f6c91d0d79fb9e698a6c04ea7ea2950ff08ba5d
Work <brigitte@corecode.io>
2020-11-05 13:05:29 +0100

    Update tongbu from 3.1.1.0 to 3.1.2.0

--- Casks/tongbu.rb
+++ Casks/tongbu.rb
@@ -1,6 +1,6 @@
 cask "tongbu" do
-  version "3.1.1.0"
-  sha256 "f4bc8b3658c9d7ba5a6a00ab782a00dfcb6906f951515118bc3b0f40fd900eaf"
+  version "3.1.2.0"
+  sha256 "3183299c01064b92d77cceb395e8f72cb9688987363a2390f5bd2bd2ab568b17"
 
   url "https://qd.tongbu.com/qd/tbzsqd/zsmac_#{version}/%E5%90%8C%E6%AD%A5%E5%8A%A9%E6%89%8B_mac_v#{version}_empty.dmg"
   appcast "https://zs.tongbu.com/mac/"
