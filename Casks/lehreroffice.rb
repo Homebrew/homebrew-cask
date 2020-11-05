99acaa1f4a846c0a47be8ee71dae8b89582df48e
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-05 22:48:12 +0000

    Update lehreroffice from 2020.7.2 to 2020.8.0 (#92086)

--- Casks/lehreroffice.rb
+++ Casks/lehreroffice.rb
@@ -1,6 +1,6 @@
 cask "lehreroffice" do
-  version "2020.7.2"
-  sha256 "ae7de0f6c7b03a925a3a24472ae0ce8f6f71beb9c39b2a18cd90742d5f7d9ad8"
+  version "2020.8.0"
+  sha256 "135da42e80e0ad9f7ee0764ad77c1e83feeb98548ceda172009da545bf3a2c31"
 
   url "https://www.lehreroffice.ch/lo/dateien/easy/lo_desktop_macos.dmg"
   appcast "https://www.lehreroffice.ch/services/update/getcurrentversion.php?app=Desktop"
