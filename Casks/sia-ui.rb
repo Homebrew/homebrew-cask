fc5ff25dfddcc8cab8bc03f04aab8c46db3a0712
Randall <ran.dall@icloud.com>
2020-11-05 14:08:40 -0800

    Update sia-ui from 1.5.0 to 1.5.1

--- Casks/sia-ui.rb
+++ Casks/sia-ui.rb
@@ -1,6 +1,6 @@
 cask "sia-ui" do
-  version "1.5.0"
-  sha256 "89b32387e07aff9d0b31028f6a28b59f10628a9c621943fd0db67fe9add6ece3"
+  version "1.5.1"
+  sha256 "79d7faefa05e32d7b48fb4efac238b90a96020a537ac9e48211dcb47c76cd5e0"
 
   url "https://sia.tech/releases/Sia-UI-v#{version}.dmg"
   name "Sia-UI"
