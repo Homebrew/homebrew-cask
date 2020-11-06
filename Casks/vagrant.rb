f941d0ed467e6e471aff8e83f5919a2457d7fa7c
Matthias Fasching <fasching.matthias@gmail.com>
2020-11-06 12:10:27 +0100

    Revert "Update vagrant from 2.2.10 to 2.2.11" (#92101)
    * Revert "Update vagrant from 2.2.10 to 2.2.11 (#92098)"
    
    This reverts commit 35072639f8c83c504ba9954106c5d6189403180f.
    
    * Update vagrant.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/vagrant.rb
+++ Casks/vagrant.rb
@@ -1,11 +1,12 @@
 cask "vagrant" do
-  version "2.2.11"
-  sha256 "36a83de54330ee621b5fcfea4a6c98b54af23764adec24e4dc3c35c34bd2da9b"
+  version "2.2.10"
+  sha256 "913d959455d37a0bb410c89ba3aedc886ccc76ce06bae7b228ba7454294325da"
 
   # hashicorp.com/vagrant/ was verified as official when first introduced to the cask
   url "https://releases.hashicorp.com/vagrant/#{version}/vagrant_#{version}_x86_64.dmg"
   appcast "https://github.com/hashicorp/vagrant/releases.atom"
   name "Vagrant"
+  desc "Development environment"
   homepage "https://www.vagrantup.com/"
 
   pkg "vagrant.pkg"
