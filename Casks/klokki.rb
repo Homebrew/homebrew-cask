5dd6fa1284ae14d0d1a584849aa03e074badfea2
CoreCode <core-code@users.noreply.github.com>
2020-11-06 21:31:20 +0100

    Update klokki from 1.3.1 to 1.3.2 (#92150)

--- Casks/klokki.rb
+++ Casks/klokki.rb
@@ -1,6 +1,6 @@
 cask "klokki" do
-  version "1.3.1"
-  sha256 "82a9d565c696c12979e7565e64f23561d28da951660049930dd878d1cce2fa9e"
+  version "1.3.2"
+  sha256 "230030fee250b9c06b1f9748d7ffcfb94feeaa8d9b82e1e878b08735d1c6d4df"
 
   # storage.yandexcloud.net/klokki/ was verified as official when first introduced to the cask
   url "https://storage.yandexcloud.net/klokki/Klokki.dmg"
