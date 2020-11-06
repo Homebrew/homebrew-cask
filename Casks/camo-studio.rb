884ca1bb68e0ba9c2f5751354667f9cdd87a6b2d
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-06 19:26:29 +0000

    Update camo-studio from 1.1,110 to 1.1.1,112 (#92126)

--- Casks/camo-studio.rb
+++ Casks/camo-studio.rb
@@ -1,6 +1,6 @@
 cask "camo-studio" do
-  version "1.1,110"
-  sha256 "c4c595eaf15b27e9972794f0a4e89e2d00bfe2b200839bb727976bdf9febe419"
+  version "1.1.1,112"
+  sha256 "b570b70ee6e544b81c60334fedd74c094dfe9bfa62d699676df95cdc84bb54a4"
 
   url "https://reincubate.com/res/labs/camo/Camo%20Studio%20#{version.before_comma}%20%28#{version.after_comma}%29%20%5BRelease%5D.app.zip"
   appcast "https://uds.reincubate.com/release-notes/camo/?format=sparkle"
