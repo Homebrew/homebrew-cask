8d783dd73784e77ea3ec36d37755e9bf105eb932
CoreCode <core-code@users.noreply.github.com>
2020-11-06 15:53:58 +0100

    Update musaicfm from 1.2 to 1.2.1 (#92108)

--- Casks/musaicfm.rb
+++ Casks/musaicfm.rb
@@ -1,6 +1,6 @@
 cask "musaicfm" do
-  version "1.2"
-  sha256 "092137217d5a9f24abfede5d969d002bdd5e2405c331496cc0e7fb73ed473207"
+  version "1.2.1"
+  sha256 "52ad4091e48aa3a22504ed914191722c998d11bd4d56ea3a255434863ae045fa"
 
   url "https://github.com/docterd/MusaicFM/releases/download/#{version}/MusaicFM.saver.zip"
   appcast "https://github.com/docterd/MusaicFM/releases.atom"
