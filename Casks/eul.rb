eb82a0963500f12495fbb5d1f75de7dc3192e972
gao-sun <gaosun.dev@gmail.com>
2020-11-07 20:52:32 +0800

    Update eul.rb

--- Casks/eul.rb
+++ Casks/eul.rb
@@ -1,6 +1,6 @@
 cask "eul" do
-  version "1.3.4"
-  sha256 "0dbdfc92d654bf990cf2ba4f7da102f2f1bdb2277fad46afa4ce710e74571c76"
+  version "1.4"
+  sha256 "62d4c47ba366da8d957cfd93fe8791bd8b153decc552a8735cb814fc652ba23d"
 
   url "https://github.com/gao-sun/eul/releases/download/v#{version}/eul.app.zip"
   appcast "https://github.com/gao-sun/eul/releases.atom"
