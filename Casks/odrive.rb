279de6673e31e67133a1cd636863dcea5d734146
Randall <ran.dall@icloud.com>
2020-11-07 09:45:39 -0800

    Update odrive from 6849 to 6872

--- Casks/odrive.rb
+++ Casks/odrive.rb
@@ -1,6 +1,6 @@
 cask "odrive" do
-  version "6849"
-  sha256 "31a141b891340e3b97c4cc9217a25fa7cfe6bb1d2aa04e09d19afe79c5d0e469"
+  version "6872"
+  sha256 "e71fc75a1693c367f38e0fed879e1e47259a7369d12a34488b98f0fb25539de1"
 
   # downloads can be found at https://www.odrive.com/downloaddesktop
   # d3huse1s6vwzq6.cloudfront.net/ was verified as official when first introduced to the cask
