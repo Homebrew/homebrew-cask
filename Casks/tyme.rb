21c104f5d829fc7d8b240f510e9f9f077983a7c0
Diego Jahn <jahndiego@mailbox.org>
2020-11-07 21:02:30 +0100

    Add Tyme Latest

---
+++ Casks/tyme.rb
@@ -0,0 +1,11 @@
+cask "tyme" do
+  version :latest
+  sha256 :no_check
+
+  url "https://www.tyme-app.com/app/Tyme3.zip"
+  name "Tyme 3"
+  desc "Time tracking app"
+  homepage "https://www.tyme-app.com/"
+
+  app "Tyme.app"
+end
