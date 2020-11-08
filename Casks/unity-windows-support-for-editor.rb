ee0319c645f0f81a6ecd3371a90f39786a45675a
suschizu <brigitte@corecode.io>
2020-11-08 08:08:21 +0100

    Update unity-windows-support-for-editor from 2020.1.11f1,698c1113cef0 to 2020.1.12f1,55b56f0a86e3 (#92165)

--- Casks/unity-windows-support-for-editor.rb
+++ Casks/unity-windows-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask "unity-windows-support-for-editor" do
-  version "2020.1.11f1,698c1113cef0"
-  sha256 "639aa36db3c17b0232646a2093bd797805f8b540885a6593e4a3007337c054e7"
+  version "2020.1.12f1,55b56f0a86e3"
+  sha256 "56c1c3ef317a28d296567b6dc5bdd6fb9b0fff974d75f7b85e14836c3630235b"
 
   # download.unity3d.com/download_unity/ was verified as official when first introduced to the cask
   url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-#{version.before_comma}.pkg"
