079e3e82f2002a5223e7f5d43840308309808d1b
Brigitte Zmoelnig <brigitte@corecode.io>
2020-11-08 14:02:42 +0100

    Update iridium

--- Casks/iridium.rb
+++ Casks/iridium.rb
@@ -3,8 +3,7 @@ cask "iridium" do
   sha256 "d7295ffde57bb18057fd61f8458c01a16a062658448a96c87db5ec75ed1977eb"
 
   url "https://downloads.iridiumbrowser.de/macos/#{version.major_minor}-0/iridiumbrowser-#{version}.dmg"
-  appcast "https://iridiumbrowser.de/news/",
-          must_contain: version.major_minor
+  appcast "https://iridiumbrowser.de/downloads/macos"
   name "Iridium Browser"
   homepage "https://iridiumbrowser.de/"
 
