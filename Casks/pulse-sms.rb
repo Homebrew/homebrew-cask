a50711d3113bd66b42a2c1c9704c6d84c4a96879
suschizu <brigitte@corecode.io>
2020-11-08 08:17:17 +0100

    Update pulse-sms (#92155)
    * Update pulse-sms
    
    * Update pulse-sms.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/pulse-sms.rb
+++ Casks/pulse-sms.rb
@@ -2,11 +2,12 @@ cask "pulse-sms" do
   version "4.3.0"
   sha256 "c0932e82740398b35a90b52219062e07f9979cbf5ddfe2c77e6092008f834cd2"
 
-  # github.com/klinker-apps/messenger-desktop/ was verified as official when first introduced to the cask
-  url "https://github.com/klinker-apps/messenger-desktop/releases/download/v#{version}/pulse-sms-#{version}.dmg"
-  appcast "https://github.com/klinker-apps/messenger-desktop/releases.atom"
+  # github.com/maplemedia/pulse-sms-desktop/ was verified as official when first introduced to the cask
+  url "https://github.com/maplemedia/pulse-sms-desktop/releases/download/v#{version}/pulse-sms-#{version}.dmg"
+  appcast "https://github.com/maplemedia/pulse-sms-desktop/releases.atom"
   name "Pulse SMS"
-  homepage "https://messenger.klinkerapps.com/"
+  desc "Desktop client for Pulse SMS"
+  homepage "https://messenger.klinkerapps.com/overview/platform-mac.html"
 
   app "Pulse SMS.app"
 end
