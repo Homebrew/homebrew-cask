f7bc43aff07042bc821f0ad517d9eccdc3cd8ab1
suschizu <brigitte@corecode.io>
2020-11-08 14:43:17 +0100

    Update appgate-sdp-client from 5.2.2 to 5.3.0 (#92199)
    * Update appgate-sdp-client from 5.2.2 to 5.3.0
    
    * Update appgate-sdp-client.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/appgate-sdp-client.rb
+++ Casks/appgate-sdp-client.rb
@@ -1,10 +1,10 @@
 cask "appgate-sdp-client" do
-  version "5.2.2"
-  sha256 "49b4550fb39c2a8db193f11c1bcdb21795ceefa0642501cae1b9094568d8f6af"
+  version "5.3.0"
+  sha256 "681979bedefaef7ecf354b4118a9d99e1a8cc4812e44b6c1fa79d15911a3ae69"
 
   # bin.appgate-sdp.com/ was verified as official when first introduced to the cask
   url "https://bin.appgate-sdp.com/#{version.major_minor}/client/AppGate-SDP-#{version}-Installer.dmg"
-  appcast "https://www.appgate.com/software-defined-perimeter/support/appgate-sdp-v#{version.major}-#{version.minor}"
+  appcast "https://www.appgate.com/support/software-defined-perimeter-support/sdp-v#{version.major}-#{version.minor}"
   name "AppGate SDP Client for macOS"
   desc "Software-defined perimeter for secure network access"
   homepage "https://www.appgate.com/software-defined-perimeter/support"
