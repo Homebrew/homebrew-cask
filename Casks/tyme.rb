6bfcb1962202d14b6f96c5411dd34ed0b7541aec
Diego Jahn <jahndiego@mailbox.org>
2020-11-08 23:52:43 +0100

    Add zap stanza

--- Casks/tyme.rb
+++ Casks/tyme.rb
@@ -8,4 +8,16 @@ cask "tyme" do
   homepage "https://www.tyme-app.com/"
 
   app "Tyme.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.tyme-app.Tyme3-macOS",
+    "~/Library/Application Support/com.tyme-app.Tyme3-macOS",
+    "~/Library/Caches/CloudKit/com.tyme-app.Tyme3-macOS",
+    "~/Library/Caches/com.tyme-app.Tyme3-macOS",
+    "~/Library/Containers/com.tyme-app.Tyme3-macOS",
+    "~/Library/Group Containers/*.group.com.tyme-app.Tyme3",
+    "~/Library/Preferences/3https:/api.tyme-app.com",
+    "~/Library/Preferences/com.tyme-app.Tyme3-macOS.plist",
+    "~/Library/Saved Application State/com.tyme-app.Tyme3-macOS.savedState",
+  ]
 end
