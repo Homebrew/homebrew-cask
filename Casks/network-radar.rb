5cb11c86392ec9c31d5d241635a6c137f98e3606
CoreCode <core-code@users.noreply.github.com>
2020-11-09 01:14:47 +0100

    Update network-radar.rb

--- Casks/network-radar.rb
+++ Casks/network-radar.rb
@@ -3,7 +3,7 @@ cask "network-radar" do
   sha256 "566f9049d68e7287a5a2dc1da0ff04bca540db8d23fbbddd0da8546302892df5"
 
   url "https://witt-software.com/downloads/networkradar/Network%20Radar.dmg"
-  appcast "https://www.witt-software.com/releasenotes/?app=networkradar"
+  appcast "https://www.witt-software.com/downloads/networkradar/appcast.xml"
   name "Network Radar"
   desc "Tool to scan and monitor the network"
   homepage "https://www.witt-software.com/networkradar"
