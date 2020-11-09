2cb4548731733e7ed1cfc1baa8d325b322729f21
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-09 14:54:52 +0800

    Update usb-overdrive.rb

--- Casks/usb-overdrive.rb
+++ Casks/usb-overdrive.rb
@@ -4,9 +4,15 @@ cask "usb-overdrive" do
 
   url "https://www.usboverdrive.com/download/USB-Overdrive-#{version.no_dots}.dmg"
   name "USB Overdrive"
+  desc "USB and Bluetooth device driver"
   homepage "https://www.usboverdrive.com/"
 
   pkg "Install USB Overdrive.pkg"
 
-  uninstall pkgutil: "com.usboverdrive.installer"
+  uninstall pkgutil: "com.usboverdrive.installer",
+            kext:    "/Library/Extensions/USBOverdrive.kext",
+            delete:  [
+              "/Library/PreferencePanes/USB Overdrive.prefPane",
+              "/Applications/Utilities/Uninstall USB Overdrive.app",
+            ]
 end
