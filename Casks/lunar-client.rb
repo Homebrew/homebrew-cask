8f6f6ee39c15cdc31fa8560cbf73ae82086c7971
cphos <19873261+cphos@users.noreply.github.com>
2020-11-09 09:37:45 -0500

    Add Lunar Client 2.4.0 (#92241)
    * Add Lunar Client 2.4.0
    
    * Update lunar-client.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/lunar-client.rb
@@ -0,0 +1,22 @@
+cask "lunar-client" do
+  version "2.4.0"
+  sha256 "d5e1a4fc18a4e86b11fe702d7a96c942d02168eeaab4cf30be27290dba5a9915"
+
+  # launcherupdates.lunarclientcdn.com/ was verified as official when first introduced to the cask
+  url "https://launcherupdates.lunarclientcdn.com/Lunar%20Client%20v#{version}.dmg"
+  appcast "https://launcherupdates.lunarclientcdn.com/latest-mac.yml"
+  name "Lunar Client"
+  desc "Modpack for Minecraft 1.7.10 and 1.8.9"
+  homepage "https://www.lunarclient.com/"
+
+  app "Lunar Client.app"
+
+  zap trash: [
+    "~/Library/Application Support/lunarclient",
+    "~/Library/Caches/com.moonsworth.client",
+    "~/Library/Caches/com.moonsworth.client.ShipIt",
+    "~/Library/Logs/Lunar Client",
+    "~/Library/Preferences/com.moonsworth.client.plist",
+    "~/Library/Saved Application State/com.moonsworth.client.savedState",
+  ]
+end
