e2580a60d16bc669fce6c89dcb51cde20f9796c7
CoreCode <core-code@users.noreply.github.com>
2020-11-10 02:01:57 +0100

    Update sizzy from 0.41.0 to 0.42 (#92294)

--- Casks/sizzy.rb
+++ Casks/sizzy.rb
@@ -1,6 +1,6 @@
 cask "sizzy" do
-  version "0.41.0"
-  sha256 "b8257d6a59f8c54cb4728e6087397ad0722a84f12358b3c44de85013de82dd64"
+  version "0.42"
+  sha256 "371845f114b5717ebfc290700e8a717ebf2d76c7fece9f7404d0e7db2888effd"
 
   url "https://sizzy.co/get-app"
   appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://sizzy.co/get-app&user_agent=Intel%20Mac%20OS%20X"
