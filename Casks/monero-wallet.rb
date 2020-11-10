6f48b9f4119746313fa6a94a0bc745e8a8d0255f
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-10 20:16:55 +0000

    Update monero-wallet from 0.17.1.3 to 0.17.1.4 (#92360)

--- Casks/monero-wallet.rb
+++ Casks/monero-wallet.rb
@@ -1,6 +1,6 @@
 cask "monero-wallet" do
-  version "0.17.1.3"
-  sha256 "23405534c7973a8d6908b76121b81894dc853039c942d7527d254dfde0bd2e8f"
+  version "0.17.1.4"
+  sha256 "4e319a9dfb5e1caa42b52f6503480af3b44403f3bf4691da523880c6a6ec38a9"
 
   url "https://downloads.getmonero.org/gui/monero-gui-mac-x64-v#{version}.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://downloads.getmonero.org/gui/mac64"
