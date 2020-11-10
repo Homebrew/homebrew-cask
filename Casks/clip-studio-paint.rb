7d28cbed80d9a6ac862b10405d9ac00765956b17
Randall <ran.dall@icloud.com>
2020-11-10 11:55:06 -0800

    Update clip-studio-paint from 1.10.2 to 1.10.3

--- Casks/clip-studio-paint.rb
+++ Casks/clip-studio-paint.rb
@@ -1,6 +1,6 @@
 cask "clip-studio-paint" do
-  version "1.10.2"
-  sha256 "bd7079190df1477d0b3fd7c91a108a7c7150c4783a478f4b630a19ca5f545974"
+  version "1.10.3"
+  sha256 "7f9419aba695912cfffa7b01ad974d3f2fb229db065c0b4fc16cb878693547ad"
 
   url "https://vd.clipstudio.net/clipcontent/paint/app/#{version.no_dots}/CSP_#{version.no_dots}m_app.pkg"
   appcast "https://www.clipstudio.net/en/dl",
