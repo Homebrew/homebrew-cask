afc4857bc879bdf942b09a3cd1624cf538bc9494
CoreCode <core-code@users.noreply.github.com>
2020-11-10 23:19:25 +0100

    Update merlin-project from 6.4.5 to 7.0.0 (#92368)
    * Update merlin-project from 6.4.5 to 7.0.0
    
    * Update merlin-project.rb

--- Casks/merlin-project.rb
+++ Casks/merlin-project.rb
@@ -1,6 +1,6 @@
 cask "merlin-project" do
-  version "6.4.5"
-  sha256 "0d0dc8704ae00a261a28bd5a4af7ea863d78cc0b2c75448f2c93568fbcd8d53f"
+  version "7.0.0"
+  sha256 "f24b86ee4820fb2f5bcfb8f24e70cd005eb544678ebb00d80837a771527ed6e6"
 
   url "https://www.projectwizards.net/downloads/MerlinProject.zip"
   appcast "https://www.projectwizards.net/en/support/release-notes/merlin-project-pwstore/xml"
@@ -10,5 +10,5 @@ cask "merlin-project" do
 
   depends_on macos: ">= :sierra"
 
-  app "Merlin Project #{version.major}.app"
+  app "Merlin Project.app"
 end
