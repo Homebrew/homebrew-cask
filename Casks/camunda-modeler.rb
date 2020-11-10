5317e25a0218a4b020c80938889fafaaa7cde2a8
Randall <ran.dall@icloud.com>
2020-11-10 12:00:47 -0800

    Update camunda-modeler from 4.3.0 to 4.4.0

--- Casks/camunda-modeler.rb
+++ Casks/camunda-modeler.rb
@@ -1,6 +1,6 @@
 cask "camunda-modeler" do
-  version "4.3.0"
-  sha256 "9060ab2b2986865df12f10eaf9fa1e922cb1b4648070a96ff0a37e2d835c08fa"
+  version "4.4.0"
+  sha256 "8e2fa4c17c3c20d71655218d32a136534cb7dc66798be241a4c69c0fb241d47d"
 
   url "https://camunda.org/release/camunda-modeler/#{version}/camunda-modeler-#{version}-mac.zip"
   appcast "https://camunda.com/download/modeler/"
