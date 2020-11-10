b6dd8896e8aa69f0e6c6036ad3a9f76c08157d08
秦新 <qinxin_0720@live.com>
2020-11-11 06:24:22 +0800

    Update microsoft-outlook from 16.42.20101102 to 16.43.20110804 (#92332)

--- Casks/microsoft-outlook.rb
+++ Casks/microsoft-outlook.rb
@@ -1,6 +1,6 @@
 cask "microsoft-outlook" do
-  version "16.42.20101102"
-  sha256 "566a1bd7ad446fcdd31156ebca3fbd7f8f8bb2efbcc8f3217cb55aa16a586420"
+  version "16.43.20110804"
+  sha256 "0e53acefafc25d1eebbf257f343de0d0a5258099c154f7ba5d99aa709fb50d08"
 
   # officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/ was verified as official when first introduced to the cask
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Outlook_#{version}_Installer.pkg"
