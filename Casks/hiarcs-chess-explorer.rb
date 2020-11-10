89b78fbb14445c685348834eb0e3ac6f03c68deb
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-10 10:48:31 +0800

    Update hiarcs-chess-explorer.rb

--- Casks/hiarcs-chess-explorer.rb
+++ Casks/hiarcs-chess-explorer.rb
@@ -3,7 +3,7 @@ cask "hiarcs-chess-explorer" do
   sha256 "806d3407b0e2487695ce8744cfb72b9bda68d5e8aa25b946627f880f7ff4d693"
 
   url "https://www.hiarcs.com/hce/HIARCS-Chess-Explorer-Installer-v#{version}.pkg"
-  appcast "https://www.hiarcs.com/hce/mac-v120.htm"
+  appcast "https://www.hiarcs.com/mac-chess-explorer-download.htm"
   name "(Deep) HIARCS Chess Explorer"
   homepage "https://www.hiarcs.com/mac-chess-explorer.htm"
 
