d15cab85482167acd1f09f09df3e328cce019f48
Julian Mayer <julian@corecode.io>
2020-11-10 21:55:24 +0100

    Update rocket-typist from 2.2.2 to 2.3

--- Casks/rocket-typist.rb
+++ Casks/rocket-typist.rb
@@ -1,6 +1,6 @@
 cask "rocket-typist" do
-  version "2.2.2"
-  sha256 "2560dc511a56b90c7e84b1aa279960b1c467d75dfa65bc7318350bd7289855ae"
+  version "2.3"
+  sha256 "26b1dd1a8fcbe5f09f6f754073501db71d52cb89cd5eb21784edfbdc739a0939"
 
   url "https://witt-software.com/downloads/rockettypist/Rocket%20Typist.dmg"
   appcast "https://witt-software.com/downloads/rockettypist/appcast.xml"
