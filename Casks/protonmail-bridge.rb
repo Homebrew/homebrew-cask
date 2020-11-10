6c81695b8c61388a67c73c5a376c4e5648110df6
Randall <ran.dall@icloud.com>
2020-11-10 11:51:54 -0800

    Update protonmail-bridge from 1.4.5 to 1.5.0

--- Casks/protonmail-bridge.rb
+++ Casks/protonmail-bridge.rb
@@ -1,5 +1,5 @@
 cask "protonmail-bridge" do
-  version "1.4.5"
+  version "1.5.0"
   sha256 "1f29124b09de61051ed013cecae4491b49c00a031e1b79f14179a5abc7e9a784"
 
   url "https://protonmail.com/download/Bridge-Installer.dmg"
