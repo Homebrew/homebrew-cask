4ab1d90fa83d2f9fd44eff54084f9bacc43ae97e
Никита Тимофеев <nikita.t@ati.su>
2020-11-10 23:31:53 +0300

    Update dotnet-sdk from 3.1.403 to 5.0.100 (#92325)

--- Casks/dotnet-sdk.rb
+++ Casks/dotnet-sdk.rb
@@ -3,13 +3,14 @@ cask "dotnet-sdk" do
     version "2.2.402,7430e32b-092b-4448-add7-2dcf40a7016d:1076952734fbf775062b48344d1a1587"
     sha256 "e74d816bc034d0fcdfa847286a6cad097227d4864da1c97fe801012af0c26341"
   else
-    version "3.1.403,b4db3ee6-91da-4dd3-a279-37c45d9627b7:fad88a7335d374d6e4d53aef085f4b24"
-    sha256 "6b4f384b8a724bab114e0685285a98003ed21227acfb3c6ff0dc083d981812fc"
+    version "5.0.100,3bdcd7d2-1444-4f7d-a254-504a994ffe39:e4f42b83604673f971748c722aa20bec"
+    sha256 "8f601ad822d9abb2f96a81f6a949629c46c7416d67ab55decbf358ffa5fc8deb"
   end
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
   appcast "https://www.microsoft.com/net/download/macos"
-  name ".NET Core SDK"
+  name ".NET SDK"
+  desc "Free, cross-platform, open-source developer platform"
   homepage "https://www.microsoft.com/net/core#macos"
 
   conflicts_with cask: [
