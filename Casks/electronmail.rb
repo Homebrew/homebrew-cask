3e511728b601d02bc1df16b189edc42d5eb96ba9
Randall <ran.dall@icloud.com>
2020-11-11 14:47:22 -0800

    Update electronmail from 4.9.1 to 4.9.2

--- Casks/electronmail.rb
+++ Casks/electronmail.rb
@@ -1,6 +1,6 @@
 cask "electronmail" do
-  version "4.9.1"
-  sha256 "e8335b3409dd83c42249d9cdb40a830ce18b4d15947249330236e878b8e3fe4f"
+  version "4.9.2"
+  sha256 "f768bc14fb7c937650eaac603b68ba97163972b2febfefa135d2b2da85c30a18"
 
   url "https://github.com/vladimiry/ElectronMail/releases/download/v#{version}/electron-mail-#{version}-mac-mojave.dmg"
   appcast "https://github.com/vladimiry/ElectronMail/releases.atom"
