ae0cb5e481390213b2ea6d228075d6ab111a3dab
Randall <ran.dall@icloud.com>
2020-11-11 14:44:57 -0800

    Update microsoft-azure-storage-explorer from 1.15.1 to 1.16.0

--- Casks/microsoft-azure-storage-explorer.rb
+++ Casks/microsoft-azure-storage-explorer.rb
@@ -1,6 +1,6 @@
 cask "microsoft-azure-storage-explorer" do
-  version "1.15.1"
-  sha256 "69afb5a27f0349e83008cafa92b5a809a626b79fc7562814a3d2147e4ec0138b"
+  version "1.16.0"
+  sha256 "aab893610d0f8eb16abdc00af0c0265ac5f6c06dc9e3c58f235c1b71c70683e2"
 
   # github.com/microsoft/AzureStorageExplorer/ was verified as official when first introduced to the cask
   url "https://github.com/microsoft/AzureStorageExplorer/releases/download/v#{version}/MaC_StorageExplorer.zip"
