5dd907bbd0bf3f6855e309b555f7bba3c609a3cf
Kid <44045911+kidonng@users.noreply.github.com>
2020-11-11 21:00:00 +0800

    Update qlimagesize.rb

--- Casks/qlimagesize.rb
+++ Casks/qlimagesize.rb
@@ -1,4 +1,5 @@
 cask "qlimagesize" do
+  desc "QuickLook plugin for displaying image info and preview unsupported formats"
   version "2.6.1"
   sha256 "466c18539653056ccf7eb09eb6c68689fd9a8280a3c2ade9f2d457de53504821"
 
@@ -10,6 +11,10 @@ cask "qlimagesize" do
   depends_on macos: ">= :high_sierra"
 
   qlplugin "qlImageSize.qlgenerator"
-  
-  caveats "#{token} only shows info for natively unsupported images on Catalina, see https://github.com/Nyx0uf/qlImageSize/issues/45 for details."
+
+  caveats <<~EOS
+    qlimagesize only shows info for natively unsupported images on Catalina:
+
+      https://github.com/Nyx0uf/qlImageSize/issues/45
+  EOS
 end
