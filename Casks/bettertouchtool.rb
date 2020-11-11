c5717b065a1fdf1432748c307c729755aefe4b9a
Tom Matthews <trcm@pm.me>
2020-11-11 16:20:40 +0000

    Use version correctly.

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -2,7 +2,7 @@ cask "bettertouchtool" do
   version "3.504-1642"
   sha256 "1bef5d7e27214861bba24e1d24869ae627f76e32fdb71aaecac7bcb7398c580a"
 
-  url "https://folivora.ai/releases/btt3.504-1642.zip"
+  url "https://folivora.ai/releases/btt#{version}.zip"
   appcast "https://updates.folivora.ai/appcast_manual.xml"
   name "BetterTouchTool"
   desc "Tool to customize input devices and automate computer systems"
