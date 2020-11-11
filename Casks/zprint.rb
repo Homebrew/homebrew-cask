8d40eeb69d30d362609c5fa2ba5a0ae4a2cf5cc1
Seeker <meaningseeking@protonmail.com>
2020-11-11 01:07:20 -0800

    Update zprint from 1.0.1 to 1.0.2 (#92390)

--- Casks/zprint.rb
+++ Casks/zprint.rb
@@ -1,6 +1,6 @@
 cask "zprint" do
-  version "1.0.1"
-  sha256 "76b2f41b9a89668f05429dc30a436aff868bfc8e99700ada00788cb1157705fb"
+  version "1.0.2"
+  sha256 "c39b5b60cbd1db0ea40f51c4ce3b875540b4f721a28c617bfb86d54e9e500e4d"
 
   url "https://github.com/kkinnear/zprint/releases/download/#{version}/zprintm-#{version}"
   appcast "https://github.com/kkinnear/zprint/releases.atom"
