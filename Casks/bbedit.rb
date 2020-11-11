a9c4efabad47987a23d35ccd4fe543f9afd166b1
tiiiecherle <tiiiecherle@gmail.com>
2020-11-11 13:15:47 +0100

    Update bbedit from 13.5.1 to 13.5.2 (#92396)

--- Casks/bbedit.rb
+++ Casks/bbedit.rb
@@ -6,8 +6,8 @@ cask "bbedit" do
     version "12.6.7"
     sha256 "d0647c864268b187343bd95bfcf490d6a2388579b1f8fce64a289c65341b1144"
   else
-    version "13.5.1"
-    sha256 "2cc163be60b2e09651d06fe89ee614118f7dc24c59301089d5d5e6eab145d3dd"
+    version "13.5.2"
+    sha256 "83fd2877628c8635c9ba408128ef1572668b34992e4b8eb530805bad6c81ce29"
   end
   # s3.amazonaws.com/BBSW-download/ was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/BBSW-download/BBEdit_#{version}.dmg"
