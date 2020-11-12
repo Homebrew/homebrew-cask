12cea47bb13e0446c703bd4cf25d2b185ea7ce77
Marvin Schlegel <5002075+cybniv@users.noreply.github.com>
2020-11-12 01:25:24 +0100

    EurKEY: Switch to system-wide installation (#92410)
    This makes EurKEY also available in VmWare, the Lock screen and
    probably other places and is the recommended way [1].
    
    Also see [2].
    
    [1] https://github.com/jonasdiemer/EurKEY-Mac
    [2] https://superuser.com/questions/99172/how-to-stop-os-x-from-switching-input-method-keyboard-layout-automatically/561613#561613

--- Casks/eurkey.rb
+++ Casks/eurkey.rb
@@ -5,8 +5,9 @@ cask "eurkey" do
   # github.com/jonasdiemer/EurKEY-Mac/ was verified as official when first introduced to the cask
   url "https://github.com/jonasdiemer/EurKEY-Mac/archive/master.zip"
   name "EurKEY keyboard layout"
+  desc "Keyboard Layout for Europeans, Coders and Translators"
   homepage "https://eurkey.steffen.bruentjen.eu/"
 
-  artifact "EurKEY-Mac-master/EurKEY.icns", target: "#{ENV["HOME"]}/Library/Keyboard Layouts/EurKEY.icns"
-  artifact "EurKEY-Mac-master/EurKEY.keylayout", target: "#{ENV["HOME"]}/Library/Keyboard Layouts/EurKEY.keylayout"
+  artifact "EurKEY-Mac-master/EurKEY.icns", target: "/Library/Keyboard Layouts/EurKEY.icns"
+  artifact "EurKEY-Mac-master/EurKEY.keylayout", target: "/Library/Keyboard Layouts/EurKEY.keylayout"
 end
