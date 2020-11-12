f88b4a542bcd2b28370b93553d57b1aafb28001c
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-12 00:00:37 +0000

    Update adguard from 2.5.0.902 to 2.5.1.918 (#92439)

--- Casks/adguard.rb
+++ Casks/adguard.rb
@@ -1,6 +1,6 @@
 cask "adguard" do
-  version "2.5.0.902"
-  sha256 "cd72eb1a01e4e338a86ac33167f6cf73d96cfda3c9128d2c65f7ca8812155710"
+  version "2.5.1.918"
+  sha256 "2b6d81a44803e0cd517bbaa6b16ce0d33d5618868a2dcbbb6661304ef8c90f17"
 
   url "https://static.adguard.com/mac/release/AdGuard-#{version}.dmg"
   appcast "https://static.adguard.com/mac/adguard-release-appcast.xml"
