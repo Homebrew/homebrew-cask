2d129e31b516091b1d06f46375bddacd31a1179c
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-12 22:29:42 +0800

    Update malus.rb

--- Casks/malus.rb
+++ Casks/malus.rb
@@ -1,9 +1,9 @@
 cask "malus" do
-  version "1.5.1"
+  version "1.5.1,1.5.2"
   sha256 "dfca2126b338fc5836b9abba87860922b23455712a15bb854aa604aedf3d4efd"
 
-  # malus.s3cdn.net was verified as official when first introduced to the cask
-  url "https://malus.s3cdn.net/uploads/Malus-mac-152.dmg"
+  # malus.s3cdn.net/ was verified as official when first introduced to the cask
+  url "https://malus.s3cdn.net/uploads/Malus-mac-#{version.after_comma.no_dots}.dmg"
   appcast "https://api.getmalus.com/api/checkDesktopUpdate?type=mac"
   name "Malus"
   desc "Proxy to help accessing various online media resources/services"
@@ -14,8 +14,9 @@ cask "malus" do
 
   app "Malus.app"
 
+  uninstall rmdir: "/Library/Application Support/Malus"
+
   zap trash: [
-    "/Library/Application Support/Malus",
     "~/Library/Application Support/Malus",
     "~/Library/Application Support/com.getmalus.malus",
     "~/Library/Logs/com.getmalus.malus",
