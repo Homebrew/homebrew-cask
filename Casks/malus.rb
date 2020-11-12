20c88eb311563ab29ea4d967e90f17878b9361aa
i0ntempest <szf1234@me.com>
2020-11-12 16:13:53 -0500

    Update malus.rb

--- Casks/malus.rb
+++ Casks/malus.rb
@@ -3,7 +3,7 @@ cask "malus" do
   sha256 "dfca2126b338fc5836b9abba87860922b23455712a15bb854aa604aedf3d4efd"
 
   # malus.s3cdn.net/ was verified as official when first introduced to the cask
-  url "https://malus.s3cdn.net/uploads/Malus-mac-#{version.no_dots}.dmg"
+  url "https://malus.s3cdn.net/uploads/Malus-mac-#{version.after_comma}.dmg"
   appcast "https://api.getmalus.com/api/checkDesktopUpdate?type=mac"
   name "Malus"
   desc "Proxy to help accessing various online media resources/services"
