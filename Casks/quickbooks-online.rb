25a8ea4acda83bd74463ce87573a0a6b738cc5d6
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-12 21:44:20 +0000

    Update quickbooks-online from 4.2.1-79 to 4.3.0-82 (#92559)

--- Casks/quickbooks-online.rb
+++ Casks/quickbooks-online.rb
@@ -1,6 +1,6 @@
 cask "quickbooks-online" do
-  version "4.2.1-79"
-  sha256 "3d1f59f580e60c594eefc28d27acf82987171bcd212a0b8804371699da08216b"
+  version "4.3.0-82"
+  sha256 "900d2bbcfd7eedae916562f6fad65d851c9e40b3fdd20d046c8e151c2d60df54"
 
   url "https://http-download.intuit.com/http.intuit/CMO/qbo_client_web/static/release/mac/QuickBooks-#{version}-mac.zip"
   appcast "https://http-download.intuit.com/http.intuit/CMO/qbo_client_web/static/release/mac/latest-mac.yml"
