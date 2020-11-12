a50ac49a15fc35655c70b857724f4cf0519bd32c
sidneys.github.io <sidneys.github.io@outlook.com>
2020-11-12 16:11:06 +0100

    Add macXvideo.app 1.5 (#92487)
    * Add macXvideo 1.5
    
    * Update macx-video.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/macx-video.rb
@@ -0,0 +1,19 @@
+cask "macx-video" do
+  version "1.5"
+  sha256 "010b331fe12b6176c2a5591637ab8877a1a64d86236520ac886f97a93b971569"
+
+  url "https://www.videoproc.com/download/macxvideo.dmg"
+  appcast "https://www.videoproc.com/macxvideo/"
+  name "macXvideo"
+  desc "4K video processing software"
+  homepage "https://www.videoproc.com/macxvideo/"
+
+  app "macXvideo.app"
+
+  zap trash: [
+    "~/Library/Application Support/Digiarty",
+    "~/Library/Caches/com.digiarty.macXvideo",
+    "~/Library/Preferences/com.digiarty.macXvideo.plist",
+    "~/Library/Saved Application State/com.digiarty.macXvideo.savedState",
+  ]
+end
