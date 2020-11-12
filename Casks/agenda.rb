081321a917b46185ac424e8e70638d0740a4e036
singularitti <singularitti@outlook.com>
2020-11-12 18:10:09 -0500

    Update agenda from 11.2 to 11.2.1

--- Casks/agenda.rb
+++ Casks/agenda.rb
@@ -1,6 +1,6 @@
 cask "agenda" do
-  version "11.2"
-  sha256 "61d8f5f771ef8bdc45f27b73cc591fdceb4f9372adda785ebef489d02e47bf0a"
+  version "11.2.1"
+  sha256 "6a94494264769356f1d684bcb725e780f4697b4e9be483dd36a153226b555272"
 
   url "https://downloads.agenda.com/Agenda_#{version}.zip"
   appcast "https://downloads.agenda.com/AgendaSparkleAppcast.xml"
