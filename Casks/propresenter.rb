4b502ba9f98d43dd53e658bf762d0c59e36157d4
Randall <ran.dall@icloud.com>
2020-11-12 12:22:24 -0800

    Update propresenter from 7.2.2,117572106 to 7.3,117637163

--- Casks/propresenter.rb
+++ Casks/propresenter.rb
@@ -1,6 +1,6 @@
 cask "propresenter" do
-  version "7.2.2,117572106"
-  sha256 "bafdfa9c3310ff209dc910a9a47a226a562a46f431e92823d11c09583efcb0a8"
+  version "7.3,117637163"
+  sha256 "56ad11aa4aa87d5e6486ec7bc86311742ab12cc5b22e7b28bc0f35abe69db6a2"
 
   url "https://renewedvision.com/downloads/propresenter/mac/ProPresenter_#{version.before_comma}_#{version.after_comma}.zip"
   appcast "https://api.renewedvision.com/v1/pro/upgrade?platform=macos&osVersion=0&appVersion=0&buildNumber=0&includeNotes=0"
