827144b6760b40b5a646b6fb08f8ec7fba9afc86
Randall <ran.dall@icloud.com>
2020-11-12 11:58:01 -0800

    Update softorino-youtube-converter from 2.1.26 to 2.1.27

--- Casks/softorino-youtube-converter.rb
+++ Casks/softorino-youtube-converter.rb
@@ -1,6 +1,6 @@
 cask "softorino-youtube-converter" do
-  version "2.1.26"
-  sha256 "32828e4e2540349b2a01f205396533f2f43d66a1448b3ea68758d8fcdf033d6e"
+  version "2.1.27"
+  sha256 "daca7ee5d2082386015b1b7349c3e7608d75c925d0b7f43f157ec8b6112efc0c"
 
   url "https://shining.softorino.com/shine_uploads/softorinoyoutubeconverter#{version.major}mac_#{version}.dmg"
   appcast "https://shining.softorino.com/appcast.php?abbr=syc2m"
