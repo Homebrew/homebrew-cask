daee3221369172f9cf6efed6288da4d7945b500a
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-12 12:23:50 +0800

    Update transcribe.rb

--- Casks/transcribe.rb
+++ Casks/transcribe.rb
@@ -1,10 +1,11 @@
 cask "transcribe" do
-  version "8.80.0"
+  version "8.80"
   sha256 "2c32b81ebe15115550c74f6d758facac05acbd20d1da16237e1fe3fc60b04215"
 
   url "https://www.seventhstring.com/xscribe/transcribe.dmg"
   appcast "https://www.seventhstring.com/xscribe/history.html"
   name "Transcribe!"
+  desc "Transcribe recorded music"
   homepage "https://www.seventhstring.com/xscribe/overview.html"
 
   depends_on macos: ">= :yosemite"
