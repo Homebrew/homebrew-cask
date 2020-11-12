ef64b0197af34137fce8045d00de550a4d18299a
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-12 20:01:48 +0000

    Update ajour from 0.5.0 to 0.5.1 (#92515)

--- Casks/ajour.rb
+++ Casks/ajour.rb
@@ -1,6 +1,6 @@
 cask "ajour" do
-  version "0.5.0"
-  sha256 "af8dd0415d279034170ec7759a6bee1b08d6b756883dce1e64219cf6aa2c85e9"
+  version "0.5.1"
+  sha256 "410915e6675137314b9afc72b6b30b88ec941eb1e6ac5448ce587513307c0d26"
 
   url "https://github.com/casperstorm/ajour/releases/download/#{version}/ajour.dmg"
   appcast "https://github.com/casperstorm/ajour/releases.atom"
