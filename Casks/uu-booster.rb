1bd9e1dd8d67c7cf17d1980d87fa9c0212b76d44
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-12 21:44:06 +0000

    Update uu-booster from 2.3.0,183 to 2.3.1,185 (#92561)

--- Casks/uu-booster.rb
+++ Casks/uu-booster.rb
@@ -1,6 +1,6 @@
 cask "uu-booster" do
-  version "2.3.0,183"
-  sha256 "4384c4c963b05f2ad3465ad2beadea123fb4eab63862fcb17571dbeff1b9f175"
+  version "2.3.1,185"
+  sha256 "a219f4a4ec76db91a767109923e106ec66cbf876f0dd87d3d2cfecdc4127cd31"
 
   # uu.gdl.netease.com/ was verified as official when first introduced to the cask
   url "https://uu.gdl.netease.com/UU-macOS-#{version.before_comma}(#{version.after_comma}).dmg"
