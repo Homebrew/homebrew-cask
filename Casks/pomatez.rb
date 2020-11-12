fa5b2a7902e34a7d6ab2e0dd5c069baf394afefe
Nico Nußbaum <nico.nussbaum@implisense.com>
2020-11-12 13:10:41 +0100

    Add Pomatez.app 1.1.0

---
+++ Casks/pomatez.rb
@@ -0,0 +1,13 @@
+cask "pomatez" do
+  version "1.1.0"
+  sha256 "64b5e09d9ee44328e89cc0f75675380210b202dc92ecf2b618e7a8f5d58a7bda"
+
+  # github.com/roldanjr/pomatez/ was verified as official when first introduced to the cask
+  url "https://github.com/roldanjr/pomatez/releases/download/v#{version}/Pomatez-v#{version}-mac.dmg"
+  appcast "https://github.com/roldanjr/pomatez/releases.atom"
+  name "Pomatez"
+  desc "Pomodoro Timer"
+  homepage "https://roldanjr.github.io/pomatez"
+
+  app "Pomatez.app"
+end
