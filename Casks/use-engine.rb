ce5fd1edfed2909c1194a9a8e82c545db09ec5aa
fabien-bradmetz <56958784+fabien-bradmetz@users.noreply.github.com>
2020-11-12 16:51:32 +0100

    Update use-engine from 2.2.5.5 to 2.2.6 (#92494)

--- Casks/use-engine.rb
+++ Casks/use-engine.rb
@@ -1,6 +1,6 @@
 cask "use-engine" do
-  version "2.2.5.5"
-  sha256 "1f10b0e793099c8e55b88927bf31bf8b97997174249fda001c0bc834fc3d1d89"
+  version "2.2.6"
+  sha256 "1be3987efedf45bdce3dfb183c677c0f229a3585920685094f6522a8967cce94"
 
   url "https://repository.use-together.com/stable/use-engine/macos/#{version.major}.x/#{version}/use-engine.dmg"
   name "USE Engine"
