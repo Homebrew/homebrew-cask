c84e200a0e8863af1bbec3014d3dd21c431a69df
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-12 00:43:24 +0000

    Update pitch from 1.3.0 to 1.4.0 (#92451)

--- Casks/pitch.rb
+++ Casks/pitch.rb
@@ -1,6 +1,6 @@
 cask "pitch" do
-  version "1.3.0"
-  sha256 "9367306b0e21c0e08f87a2530034b702c724258b4924a3a446b6dc08e484490b"
+  version "1.4.0"
+  sha256 "399e15fc51ce3ee65057291a358ed831158cd1cfac32df9b73ca3b6bc6b66c5e"
 
   url "https://desktop-app-builds.pitch.com/Pitch-#{version}.dmg"
   appcast "https://desktop-app-builds.pitch.com/latest-mac.yml"
