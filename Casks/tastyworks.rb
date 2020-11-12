49b6f3754c8c6b2ff9e9d7928742fe333dc35d2f
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-12 21:09:29 +0000

    Update tastyworks from 1.10.0 to 1.11.0 (#92556)

--- Casks/tastyworks.rb
+++ Casks/tastyworks.rb
@@ -1,6 +1,6 @@
 cask "tastyworks" do
-  version "1.10.0"
-  sha256 "111ddded882be3a244cd560c9b3cf2303cc98347ae5dfb95aa38f00dd0ecb492"
+  version "1.11.0"
+  sha256 "8f6ecdfc906e73790f173fcb4519096a3ba4fa53797d2941f1cd93eb86794697"
 
   url "https://download.tastyworks.com/desktop-#{version.major}.x.x/#{version}/tastyworks-#{version}.dmg"
   appcast "https://tastyworks.freshdesk.com/support/solutions/articles/43000435186-recent-release-notes",
