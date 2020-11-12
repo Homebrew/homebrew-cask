606fd50d62d1fb1806e570ecf08bb769aa285aa0
i0ntempest <szf1234@me.com>
2020-11-12 11:58:46 -0500

    Update malus from 1.5.0 to 1.5.1,1.5.2

--- Casks/malus.rb
+++ Casks/malus.rb
@@ -1,9 +1,9 @@
 cask "malus" do
-  version "1.5.0"
-  sha256 "0aa09ca35933a4783f735e2c8dc9dbd10a8d692b0be3ccf547e7713376324bc3"
+  version "1.5.1,1.5.2"
+  sha256 "dfca2126b338fc5836b9abba87860922b23455712a15bb854aa604aedf3d4efd"
 
   # malus.s3cdn.net was verified as official when first introduced to the cask
-  url "https://malus.s3cdn.net/uploads/Malus-mac-#{version.no_dots}.dmg"
+  url "https://malus.s3cdn.net/uploads/Malus-mac-#{version.after_comma.no_dots}.dmg"
   appcast "https://api.getmalus.com/api/checkDesktopUpdate?type=mac"
   name "Malus"
   desc "Proxy to help accessing various online media resources/services"
