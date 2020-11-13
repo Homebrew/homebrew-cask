549f3d116bf6aa8a1f33187459e8d73b8bbf19fa
singularitti <singularitti@outlook.com>
2020-11-12 20:10:17 -0500

    Update understand from 5.1.1029 to 6.0.1041

--- Casks/understand.rb
+++ Casks/understand.rb
@@ -1,6 +1,6 @@
 cask "understand" do
-  version "5.1.1029"
-  sha256 "1ed79219c267d2da77efabeedfcbd790b2c4475bf3eedf4b0395426a47961d4e"
+  version "6.0.1041"
+  sha256 "27fad4d35a398c8e0ab9b4c4ac4651211cbaf7fc6311fcedb816cfbc65d21971"
 
   url "http://builds.scitools.com/all_builds/b#{version.patch}/Understand/Understand-#{version}-MacOSX-x86.dmg"
   appcast "https://scitools.com/download/all-builds/"
