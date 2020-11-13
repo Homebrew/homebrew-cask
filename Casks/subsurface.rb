96a840b65d8b351dcdfc21425e341ab2458052a8
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-13 21:58:58 +0000

    Update subsurface from 4.9.8 to 4.9.9 (#92681)

--- Casks/subsurface.rb
+++ Casks/subsurface.rb
@@ -1,6 +1,6 @@
 cask "subsurface" do
-  version "4.9.8"
-  sha256 "9d42ceaf57b0268fa5912c38c26075753dd74cfb82a87118c0857e49c2f36ec7"
+  version "4.9.9"
+  sha256 "4c84335dada4bef73e63303b04ed2de83a9c52d40c5513fb1f5b3ac522ab23a0"
 
   url "https://subsurface-divelog.org/downloads/Subsurface-#{version}.dmg"
   appcast "https://subsurface-divelog.org/download/"
