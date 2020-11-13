ad742f9a908e6ae14a8c353f2c4d71ff9bb6af31
CoreCode <core-code@users.noreply.github.com>
2020-11-13 02:07:22 +0100

    Update itubedownloader from 6.5.24 to 6.5.25 (#92589)

--- Casks/itubedownloader.rb
+++ Casks/itubedownloader.rb
@@ -1,6 +1,6 @@
 cask "itubedownloader" do
-  version "6.5.24"
-  sha256 "042c8e6279b18beed144b3cea991c43247edc2f622a73d2fa2be3c0e6d98a4ee"
+  version "6.5.25"
+  sha256 "654255357d079d06592fcf5e0e8aa72c07c06dbb9d93a20d15aaefe22d10b80b"
 
   # itubedownloader.s3.us-east-2.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://itubedownloader.s3.us-east-2.amazonaws.com/iTubeDownloader.dmg"
