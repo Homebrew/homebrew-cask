902deb8f859a5d4ad8f4c96d6c19d9a0e9e12e8b
Nehal Patel <nehalvpatel@users.noreply.github.com>
2020-11-13 17:57:09 -0600

    Update secretive from 1.0.2 to 2.0.0 (#92640)
    * Update secretive.rb
    
    * Update secretive.rb
    
    * Update secretive.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/secretive.rb
+++ Casks/secretive.rb
@@ -1,10 +1,16 @@
 cask "secretive" do
-  version "2.0.0"
-  sha256 "5d5b2f5073dcd6be48712f45d1c4214bc6ab170fe750a80da0562cdc92d84bc0"
+  if MacOS.version <= :catalina
+    version "1.0.3"
+    sha256 "d8522c153f20cd03513e6815bdb46be98eae0db2b2a45d30f60b25a6609d1657"
+  else
+    version "2.0.0"
+    sha256 "5d5b2f5073dcd6be48712f45d1c4214bc6ab170fe750a80da0562cdc92d84bc0"
+  end
 
   url "https://github.com/maxgoedjen/secretive/releases/download/v#{version}/Secretive.zip"
   appcast "https://github.com/maxgoedjen/secretive/releases.atom"
   name "Secretive"
+  desc "Store SSH keys in the Secure Enclave"
   homepage "https://github.com/maxgoedjen/secretive"
 
   depends_on macos: ">= :catalina"
