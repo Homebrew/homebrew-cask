21015b48b0ea0c03814f5deeae52b65dbe75b55c
Revisor01 <mail@simoneckhardt.de>
2020-11-13 11:46:39 +0100

    Update iterm2.rb

--- Casks/iterm2.rb
+++ Casks/iterm2.rb
@@ -1,16 +1,15 @@
 cask "iterm2" do
   # note: "2" is not a version number, but an intrinsic part of the product name
   if MacOS.version <= :high_sierra
-    version '3.3.12'
-    sha256 "6811b520699e8331b5d80b5da1e370e0ed467e68bc56906f08ecfa986e318167"
-    appcast "https://iterm2.com/appcasts/final_modern.xml" 
+  version "3.3.12"
+  sha256 "6811b520699e8331b5d80b5da1e370e0ed467e68bc56906f08ecfa986e318167" 
   else
-    version '3.4.0'
-    sha256 "5f78331d4786ead6fff49c86738be8fab3a1777346c3e35e6bb9c6cad52c1a47"
-    appcast "https://iterm2.com/appcasts/final_modern.xml"
+  version "3.4.0"
+  sha256 "5f78331d4786ead6fff49c86738be8fab3a1777346c3e35e6bb9c6cad52c1a47"
   end
 
   url "https://iterm2.com/downloads/stable/iTerm2-#{version.dots_to_underscores}.zip"
+  appcast "https://iterm2.com/appcasts/final_modern.xml"
   name "iTerm2"
   desc "Terminal emulator as alternative to Apple's Terminal app"
   homepage "https://www.iterm2.com/"
