d0e3fc11c29ff4ea3ac4b32e714592c4084cd5c4
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-13 21:51:56 +0000

    Update tencent-lemon from 4.8.0 to 4.8.1 (#92677)

--- Casks/tencent-lemon.rb
+++ Casks/tencent-lemon.rb
@@ -1,6 +1,6 @@
 cask "tencent-lemon" do
-  version "4.8.0"
-  sha256 "2ee0c2653221018d530237692dfc6893c2fbb1ef44cba1a610a7dccb8123f716"
+  version "4.8.1"
+  sha256 "06cf973276bdc7e9a5414b1831c12e655c739dcb911d303ae6724b13691bb3e8"
 
   # pm.myapp.com/invc/xfspeed/qqpcmgr/ was verified as official when first introduced to the cask
   url "https://pm.myapp.com/invc/xfspeed/qqpcmgr/module_update/Lemon_#{version}.dmg"
