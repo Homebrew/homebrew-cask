f6422961a406ef708c2f48d3141483995987e42a
suschizu <brigitte@corecode.io>
2020-11-13 18:37:42 +0100

    Update nvidia-geforce-now.rb from 2.0.24.105,8F16A9 to 2.0.25.119,D52EC7 (#92625)

--- Casks/nvidia-geforce-now.rb
+++ Casks/nvidia-geforce-now.rb
@@ -1,6 +1,6 @@
 cask "nvidia-geforce-now" do
-  version "2.0.24.105,8F16A9"
-  sha256 "863b22f8ac0943995e526084779c29db335881ca91423b3986cc9afdc2b13af3"
+  version "2.0.25.119,D52EC7"
+  sha256 "aba92e28e8497157836e44af15ee1839d95a155bdcc8cd96b3bb3def4e7d5e1c"
 
   url "https://ota-downloads.nvidia.com/ota/GeForceNOW-release.app_#{version.after_comma}.zip"
   appcast "https://ota.nvidia.com/release/available?product=GFN-mac&version=#{version.before_comma}&channel=OFFICIAL",
