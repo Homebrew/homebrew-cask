d0b2c030fff7635fab6afc48b5d67eb617d13f11
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-14 07:18:54 +0800

    Update metaimage.rb

--- Casks/metaimage.rb
+++ Casks/metaimage.rb
@@ -8,12 +8,14 @@ cask "metaimage" do
   desc "Editor to read, write and edit images metadata"
   homepage "https://neededapps.com/metaimage/"
 
+  depends_on macos: ">= :high_sierra"
+
   app "MetaImage.app"
 
   zap trash: [
     "~/Library/Application Support/MetaImage",
-    "~/Library/Saved Application State/com.jeremyvizzini.metaimage.macos.savedState",
     "~/Library/Caches/com.jeremyvizzini.metaimage.macos",
     "~/Library/Preferences/com.jeremyvizzini.metaimage.macos.plist",
+    "~/Library/Saved Application State/com.jeremyvizzini.metaimage.macos.savedState",
   ]
 end
