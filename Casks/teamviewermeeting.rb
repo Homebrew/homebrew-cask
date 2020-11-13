57c2440de6a430e9b1d6491f466e0a22aac59cf8
suschizu <brigitte@corecode.io>
2020-11-14 00:38:06 +0100

    Update teamviewermeeting.rb

--- Casks/teamviewermeeting.rb
+++ Casks/teamviewermeeting.rb
@@ -5,6 +5,7 @@ cask "teamviewermeeting" do
   url "https://dl.teamviewer.com/teamviewermeeting/installer/mac/#{version}/TeamViewerMeeting.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.teamviewer.com/blizz/mac"
   name "TeamViewerMeeting"
+  desc "Cross-platform videoconferencing and communication software"
   homepage "https://www.teamviewer.com/meeting/"
 
   app "TeamViewerMeeting.app"
