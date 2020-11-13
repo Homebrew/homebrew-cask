404a831f104f5d47cda5047725ecc00c8ba8d2da
Evgeny <mahnunchik@gmail.com>
2020-11-13 16:40:53 +0200

    Apply suggestions from code review "Coin Wallet.app" v3.0.1
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/coin.rb
+++ Casks/coin.rb
@@ -2,11 +2,11 @@ cask "coin" do
   version "3.0.1"
   sha256 "ee0cee1c90b0431a059e49a8aabf678dc1243c833c4891f635043a76ab1a2489"
 
-  # github.com/CoinSpace/CoinSpace was verified as official when first introduced to the cask
+  # github.com/CoinSpace/CoinSpace/ was verified as official when first introduced to the cask
   url "https://github.com/CoinSpace/CoinSpace/releases/download/v#{version}/Coin.Wallet.dmg"
   appcast "https://github.com/CoinSpace/CoinSpace/releases.atom"
   name "Coin Wallet"
-  desc "Digital Currency Wallet"
+  desc "Digital currency wallet"
   homepage "https://coin.space/"
 
   auto_updates true
