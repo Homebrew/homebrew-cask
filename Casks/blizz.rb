d7cb06f0eb10b90cb74c919442b071c313a5d1bc
suschizu <brigitte@corecode.io>
2020-11-13 18:28:54 +0100

    Update blizz.rb

--- Casks/blizz.rb
+++ Casks/blizz.rb
@@ -1,12 +1,12 @@
-cask "blizz" do
+cask "teamviewermeeting" do
   version "15.12.6"
   sha256 "fba3fe74dcd2e32fc161a97920f050db3786e550f01359c3926d83dfbe38389b"
 
   # dl.teamviewer.com/ was verified as official when first introduced to the cask
   url "https://dl.teamviewer.com/teamviewermeeting/installer/mac/#{version}/TeamViewerMeeting.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.teamviewer.com/blizz/mac"
-  name "Blizz"
+  name "TeamViewerMeeting"
   homepage "https://www.blizz.com/"
 
-  app "Blizz.app"
+  app "TeamViewerMeeting.app"
 end
