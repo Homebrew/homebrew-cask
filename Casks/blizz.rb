a9055e7287f876b20c7f08667643c1729455ad50
Brigitte Zmoelnig <brigitte@corecode.io>
2020-11-13 17:21:49 +0100

    Update blizz from 15.11.4 to 15.12.6

--- Casks/blizz.rb
+++ Casks/blizz.rb
@@ -1,9 +1,9 @@
 cask "blizz" do
-  version "15.11.4"
-  sha256 "fa3363000df086ab424e7da3d79156f709e04bed115e60ad70827be2e21c3938"
+  version "15.12.6"
+  sha256 "fba3fe74dcd2e32fc161a97920f050db3786e550f01359c3926d83dfbe38389b"
 
   # dl.teamviewer.com/ was verified as official when first introduced to the cask
-  url "https://dl.teamviewer.com/blizz/installer/mac/#{version}/Blizz.dmg"
+  url "https://dl.teamviewer.com/teamviewermeeting/installer/mac/#{version}/TeamViewerMeeting.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.teamviewer.com/blizz/mac"
   name "Blizz"
   homepage "https://www.blizz.com/"
