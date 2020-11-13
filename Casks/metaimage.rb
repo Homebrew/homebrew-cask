111f8775872799f7d06cdb84acfc0dbc2b60b8d8
singularitti <singularitti@outlook.com>
2020-11-13 14:49:07 -0500

    Add `zap` stanza

--- Casks/metaimage.rb
+++ Casks/metaimage.rb
@@ -9,4 +9,11 @@ cask "metaimage" do
   homepage "https://neededapps.com/metaimage/"
 
   app "MetaImage.app"
+
+  zap trash: [
+    "~/Library/Application Support/MetaImage",
+    "~/Library/Saved Application State/com.jeremyvizzini.metaimage.macos.savedState",
+    "~/Library/Caches/com.jeremyvizzini.metaimage.macos",
+    "~/Library/Preferences/com.jeremyvizzini.metaimage.macos.plist",
+  ]
 end
