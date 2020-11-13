60afdffd49969091ed5ee25d60c0e40fb397803c
Roger Chapman <rogchap@gmail.com>
2020-11-14 01:22:08 +1100

    Add Wombat.app v0.2.2 (#92479)
    * Add Wombat.app v0.2.2
    
    * add zap stanza to Wombat
    
    * Update wombat.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/wombat.rb
@@ -0,0 +1,14 @@
+cask "wombat" do
+  version "0.2.2"
+  sha256 "b48b1c101bfe1089c9d553ec82994c56c176f3cbf206ef397b2e2f7c4aaf5dda"
+
+  url "https://github.com/rogchap/wombat/releases/download/v#{version}/Wombat_v#{version}_Darwin_x86_64.dmg"
+  appcast "https://github.com/rogchap/wombat/releases.atom"
+  name "Wombat"
+  desc "Cross platform gRPC client"
+  homepage "https://github.com/rogchap/wombat"
+
+  app "Wombat.app"
+
+  zap trash: "~/Library/Application Support/Wombat"
+end
