3bbeba951c7897cfeb39d2a8730f0fab69cec2f2
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-13 21:33:23 +0000

    Update outline from 3.2011.0 to 3.2011.1 (#92668)

--- Casks/outline.rb
+++ Casks/outline.rb
@@ -1,6 +1,6 @@
 cask "outline" do
-  version "3.2011.0"
-  sha256 "16403eea82e76d3c024baa0484f5b9f23b1674e4eec776ec3d85eb0c22705b7a"
+  version "3.2011.1"
+  sha256 "051bf998a2c99cdc72ac9de3d525a8c779995977a74fc14ec9ce129830d59b5e"
 
   url "https://static.outline.ws/versions/Outline_#{version}.zip"
   appcast "https://gorillized.s3.amazonaws.com/versions/update_channel.xml"
