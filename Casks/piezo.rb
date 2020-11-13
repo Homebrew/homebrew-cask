82ba338ec6b91a8ebe6e3d87b1f58634a0c33e77
Julian Mayer <julian@corecode.io>
2020-11-13 01:59:07 +0100

    Update piezo from 1.6.9 to 1.6.10

--- Casks/piezo.rb
+++ Casks/piezo.rb
@@ -1,6 +1,6 @@
 cask "piezo" do
-  version "1.6.9"
-  sha256 "e85bd31273ee0c34f4e0cb2c6c7cfa4b3ee4a554696e9ed11229e3cec40e4163"
+  version "1.6.10"
+  sha256 "54d5c93c2f66356b66ffcfd2fd9bb87e893d0aacb3f0c6b8d16350d967498947"
 
   url "https://rogueamoeba.com/piezo/download/Piezo.zip"
   appcast "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Piezo&system=10146"
