8f2188b38acf4e2336d08cd631bb05ec02d2935b
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-13 22:51:14 +0800

    Update lulu.rb

--- Casks/lulu.rb
+++ Casks/lulu.rb
@@ -9,17 +9,20 @@ cask "lulu" do
   desc "Open-source firewall to block unknown outgoing connections"
   homepage "https://objective-see.com/products/lulu.html"
 
-  depends_on macos: ">= :big_sur"
+  depends_on macos: ">= :catalina"
 
   app "LuLu.app"
+
+  uninstall script: {
+    executable: "#{appdir}/LuLu.app/Contents/Resources/LuLu Uninstaller.app/Contents/MacOS/LuLu Uninstaller",
+    args:       ["-uninstall"],
+    sudo:       true,
+  }
 
   zap trash: [
     "~/Library/Caches/com.objective-see.lulu",
     "~/Library/Caches/com.objective-see.lulu.helper",
     "~/Library/Preferences/com.objective-see.lulu.plist",
     "~/Library/Preferences/com.objective-see.lulu.helper.plist",
-    "/Library/LaunchDaemons/com.objective-see.lulu.plist",
-    "/Library/LaunchDaemons/com.objective-see.lulu.configHelper.plist",
-    "/Library/Logs/LuLu.log",
   ]
 end
