1a5b8a27a296de6cf27c5ad1447daed5e0cbeb3e
Casper Rogild Storm <casper+github@rogildstorm.com>
2020-11-13 11:32:10 +0100

    chore: update ajour to 0.5.1

--- Casks/ajour.rb
+++ Casks/ajour.rb
@@ -1,6 +1,6 @@
 cask "ajour" do
-  version "0.3.2"
-  sha256 "c7a7b822a373fddf47682bd739894a7d19e5024436b90afcbc4c994fa0134b45"
+  version "0.5.1"
+  sha256 "410915e6675137314b9afc72b6b30b88ec941eb1e6ac5448ce587513307c0d27"
 
   url "https://github.com/casperstorm/ajour/releases/download/#{version}/ajour.dmg"
   appcast "https://github.com/casperstorm/ajour/releases.atom"
