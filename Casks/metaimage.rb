1d635ce17e2a9f4f1a03afaaea002b1ed01b246c
singularitti <singularitti@outlook.com>
2020-11-13 06:12:12 -0500

    Add app "MetaImage" 1.8.0

---
+++ Casks/metaimage.rb
@@ -0,0 +1,12 @@
+cask "metaimage" do
+  version "1.8.0"
+  sha256 "d8f62820f10d8fbd5a91fede00ba69efa21f572488372233662fe1a7a8495186"
+
+  url "https://neededapps.com/appcasts/metaimage/versions/MI-#{version}.zip"
+  appcast "https://neededapps.com/appcasts/metaimage/changelog.xml"
+  name "MetaImage"
+  desc "Editor to read, write and edit images metadata"
+  homepage "https://neededapps.com/metaimage/"
+
+  app "MetaImage.app"
+end
