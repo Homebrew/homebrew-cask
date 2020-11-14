8ce8eb5ab09f002aee051b19c93ad18cc880bb14
Licardo <1014660822@qq.com>
2020-11-15 00:31:55 +0800

    update surge from 3.8.7 to 4.0.0 (#92698)

--- Casks/surge.rb
+++ Casks/surge.rb
@@ -1,6 +1,6 @@
 cask "surge" do
-  version "3.5.7-1129"
-  sha256 "eaeb7faad3f0cc9d2a6d29f33e5cad51a784a3efae4157de2341b3c724ac663d"
+  version "4.0.0-1191-d8140b0084223fd3fc4335e4414c0884"
+  sha256 "daab57fcc757d8bcc8715ae7a21e06fc555d568775782c772b9ccb60e3ca5a09"
 
   url "https://www.nssurge.com/mac/v#{version.major}/Surge-#{version}.zip"
   appcast "https://www.nssurge.com/mac/v#{version.major}/appcast-signed.xml"
