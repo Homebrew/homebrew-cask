b6b35ecc1d4996aebdc63c0f23b360ba170825e1
Anver Housseini <anver@anykey.ch>
2020-11-14 07:36:59 +0100

    Update clipgrab from 3.8.15 to 3.9.0

--- Casks/clipgrab.rb
+++ Casks/clipgrab.rb
@@ -1,6 +1,6 @@
 cask "clipgrab" do
-  version "3.8.15"
-  sha256 "7820ef32c340bcfa0e26f74b5b8bc7a573b011cf86fbdccb79c892ebd3e0bda4"
+  version "3.9.0"
+  sha256 "f67a11742ee56ec9014449471b7f9fb803ae3260e3c2e9f25fdbb713913b70b1"
 
   url "https://download.clipgrab.org/ClipGrab-#{version}.dmg"
   appcast "https://clipgrab.org/"
