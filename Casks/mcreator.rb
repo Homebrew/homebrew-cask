50ed749bb4a79ddc4a5f98b29f19ad8229364a87
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-14 23:14:40 +0000

    Update mcreator from 2020.4 to 2020.5 (#92757)

--- Casks/mcreator.rb
+++ Casks/mcreator.rb
@@ -1,6 +1,6 @@
 cask "mcreator" do
-  version "2020.4"
-  sha256 "80178e3399c768ec2dd444e895ffd03f9ebdef7eeb0627c57f44b0ee23350239"
+  version "2020.5"
+  sha256 "007ceb832b15ac77243282534c5de4f1c87e068fcc493c0a4469ad7f433a5d9a"
 
   url "https://mcreator.net/repository/#{version.dots_to_hyphens}/MCreator%20#{version}%20Mac%2064bit.dmg"
   appcast "https://mcreator.net/download"
