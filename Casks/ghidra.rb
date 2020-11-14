8838da72c94743d42c5688ac17f73c01fd2d4f16
Seeker <meaningseeking@protonmail.com>
2020-11-13 19:04:39 -0800

    Update ghidra from 9.1.2_PUBLIC,20200212 to 9.2,20201113 (#92694)

--- Casks/ghidra.rb
+++ Casks/ghidra.rb
@@ -1,14 +1,14 @@
 cask "ghidra" do
-  version "9.1.2_PUBLIC,20200212"
-  sha256 "ebe3fa4e1afd7d97650990b27777bb78bd0427e8e70c1d0ee042aeb52decac61"
+  version "9.2,20201113"
+  sha256 "ffebd3d87bc7c6d9ae1766dd3293d1fdab3232a99b170f8ea8b57497a1704ff6"
 
-  url "https://www.ghidra-sre.org/ghidra_#{version.before_comma}_#{version.after_comma}.zip"
+  url "https://www.ghidra-sre.org/ghidra_#{version.before_comma}_PUBLIC_#{version.after_comma}.zip"
   name "Ghidra"
   homepage "https://www.ghidra-sre.org/"
 
   conflicts_with cask: "ghidra-beta"
 
-  binary "ghidra_#{version.before_comma}/ghidraRun"
+  binary "ghidra_#{version.before_comma}_PUBLIC/ghidraRun"
 
   zap trash: "~/.ghidra"
 
