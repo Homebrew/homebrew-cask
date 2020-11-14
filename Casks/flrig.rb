becc598f8618187b9efba2c4993a316b4cd44d3d
Nojan Moshiri <nojan@nojBookAir.local>
2020-11-14 15:57:43 -0800

    Add flrig.app v1.3.52
    flrig.app partners with fldigi.app which already has a cask

---
+++ Casks/flrig.rb
@@ -0,0 +1,12 @@
+cask "flrig" do
+  version "1.3.52"
+  sha256 "2cb4e59e92f27fb32c9514ad2cc51f5430d9fa1cfb61023ba8a330dbaf176b89"
+
+  url "https://downloads.sourceforge.net/fldigi/fldigi/flrig-#{version.before_comma}_x86_64.dmg"
+  appcast "https://sourceforge.net/projects/fldigi/rss?path=/flrig"
+  name "flrig"
+  desc "Ham radio rig control program cooperates with fldigi"
+  homepage "https://sourceforge.net/projects/fldigi/files/flrig/"
+
+  app "flrig-#{version.before_comma}.app"
+end
