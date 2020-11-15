1b51c67930465fc2a09400bf7904bdd88109304c
Vítor Galvão <info@vitorgalvao.com>
2020-11-15 14:32:14 +0000

    Update winx-hd-video-converter.rb

--- Casks/winx-hd-video-converter.rb
+++ Casks/winx-hd-video-converter.rb
@@ -3,7 +3,7 @@ cask "winx-hd-video-converter" do
   sha256 :no_check
 
   url "https://www.winxdvd.com/download/winx-hd-video-converter-for-mac.dmg"
-  name "Winx HD Video Converter for Mac"
+  name "Winx HD Video Converter"
   desc "HD video converter"
   homepage "https://www.winxdvd.com/hd-video-converter-for-mac/"
 
