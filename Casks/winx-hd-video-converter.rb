335edd51b66fd38120c22e3643efb1d73991c6a4
dvd-007 <dvd-007@users.noreply.github.com>
2020-11-15 22:39:36 +1100

    Add Winx HD Video Converter v6.5.1

---
+++ Casks/winx-hd-video-converter.rb
@@ -0,0 +1,11 @@
+cask "winx-hd-video-converter" do
+  version :latest
+  sha256 :no_check
+
+  url "https://www.winxdvd.com/download/winx-hd-video-converter-for-mac.dmg"
+  name "Winx HD Video Converter for Mac"
+  desc "Full featured HD video converting software, providing easy HD video conversion"
+  homepage "https://www.winxdvd.com/hd-video-converter-for-mac/"
+
+  app "Winx HD Video Converter for Mac.app"
+end
