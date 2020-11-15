3a268323a5115c7f350d7db980ef5abddcd4ff87
Vítor Galvão <info@vitorgalvao.com>
2020-11-15 00:03:57 +0000

    Update eudic.rb

--- Casks/eudic.rb
+++ Casks/eudic.rb
@@ -1,9 +1,9 @@
 cask "eudic" do
-  version "3.9.10,2020-10-14"
+  version "3.9.10"
   sha256 "2c5af4bdeb6b918c3007b97189669f1fdaabd8929730a4bb6df3e3d367475cfe"
 
   # static.frdic.com/ was verified as official when first introduced to the cask
-  url "https://static.frdic.com/pkg/eudicmac.dmg?v=#{version.after_comma}",
+  url "https://static.frdic.com/pkg/eudicmac.dmg",
       user_agent: :fake
   appcast "https://www.eudic.net/v4/en/app/download",
           must_contain: version.after_comma
