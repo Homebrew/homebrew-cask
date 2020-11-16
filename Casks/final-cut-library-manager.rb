b3b066bb4f753f70cbd5179d07271bb912a1d77f
Brigitte Zmoelnig <brigitte@corecode.io>
2020-11-16 11:22:19 +0100

    Update final-cut-library-manager from 3.80 to 3.82

--- Casks/final-cut-library-manager.rb
+++ Casks/final-cut-library-manager.rb
@@ -1,6 +1,6 @@
 cask "final-cut-library-manager" do
-  version "3.80"
-  sha256 "6559de714cc8e226ddaff82f360002b491afafb70f2074176e8c0e7033f2accf"
+  version "3.82"
+  sha256 "175538d3d9b654274a4a59b43bf279b533b2c5f82a9a99d0fed15499a5a11ba0"
 
   url "http://cdn.arcticwhiteness.com/finalcutlibrarymanager/download/zips/FinalCutLibraryManager_#{version}.zip"
   appcast "https://www.arcticwhiteness.com/finalcutlibrarymanager/download/appcast.xml"
