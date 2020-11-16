398a60148b29b1403207c5a6178859ac91a983fa
Randall <ran.dall@icloud.com>
2020-11-15 19:16:59 -0800

    Update apple-juice from 2020.11 to 2020.11.2

--- Casks/apple-juice.rb
+++ Casks/apple-juice.rb
@@ -1,6 +1,6 @@
 cask "apple-juice" do
-  version "2020.11"
-  sha256 "cdbfc948c76ad5777e5780c0411236c29c30ad606865a4e0c7a53c98cf5a60dc"
+  version "2020.11.2"
+  sha256 "7cb78a49500525d4189e69b4694f20f959356672faac8727675da7b5c08e8949"
 
   url "https://github.com/raphaelhanneken/apple-juice/releases/download/#{version}/Apple.Juice.dmg"
   appcast "https://github.com/raphaelhanneken/apple-juice/releases.atom"
