e4cdfa6d52ecd43578de59733de0d087650a7f64
singularitti <singularitti@outlook.com>
2020-11-16 15:58:22 -0500

    Update hazel from 4.4.5 to 5.0

--- Casks/hazel.rb
+++ Casks/hazel.rb
@@ -1,6 +1,6 @@
 cask "hazel" do
-  version "4.4.5"
-  sha256 "19f0a193831b8d61b8b3a5e87ab08e5295e4e7680d13615ed5b14b34f60c3cef"
+  version "5.0"
+  sha256 "4225675205e7780fa78d7824a825bda7e2cc21ee856a6855401953dd231e15b9"
 
   # s3.amazonaws.com/Noodlesoft/ was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/Noodlesoft/Hazel-#{version}.dmg"
@@ -9,16 +9,18 @@ cask "hazel" do
   desc "Automated organization"
   homepage "https://www.noodlesoft.com/"
 
-  auto_updates true
+  app "Hazel.app"
 
-  prefpane "Install Hazel.app/Contents/Resources/Hazel.prefPane"
-
-  uninstall quit: "com.noodlesoft.HazelHelper"
+  uninstall quit: "86Z3GCJ4MF.com.noodlesoft.HazelHelper"
 
   zap trash: [
     "~/Library/Logs/Hazel",
     "~/Library/Application Support/Hazel",
+    "~/Library/Caches/com.noodlesoft.HazelHelper",
     "~/Library/Preferences/com.noodlesoft.Hazel.plist",
     "~/Library/Preferences/com.noodlesoft.HazelHelper.plist",
+    "~/Library/Preferences/86Z3GCJ4MF.com.noodlesoft.HazelHelper.plist",
+    "~/Library/Saved Application State/com.noodlesoft.Hazel.savedState",
+    "/Applications/Hazel.app/Contents/Library/LoginItems/86Z3GCJ4MF.com.noodlesoft.HazelHelper.app",
   ]
 end
