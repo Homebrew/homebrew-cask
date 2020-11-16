514c0aa4b3978dd4fc0e477144820e4e8caca06b
CoreCode <core-code@users.noreply.github.com>
2020-11-16 01:45:47 +0100

    Update busycontacts.rb

--- Casks/busycontacts.rb
+++ Casks/busycontacts.rb
@@ -3,7 +3,7 @@ cask "busycontacts" do
   sha256 "e2d23240e43f1645a4cb309cc72ca944d9294d5e9407e907dadb637cf01dc05e"
 
   # 7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com was verified as official when first introduced to the cask
-  url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bct-#{version.before_comma}-#{version.after_comma}.zip"
+  url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bct-#{version}.zip"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.busymac.com/download/BusyContacts.zip"
   name "BusyContacts"
   desc "Contact manager focusing on efficiency"
