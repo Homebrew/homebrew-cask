3c8a95a32c30511fba585ba9e73d5d34f8d664fa
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-16 03:40:28 +0000

    Update transnomino from 5.2.1 to 5.3.0 (#92811)

--- Casks/transnomino.rb
+++ Casks/transnomino.rb
@@ -1,6 +1,6 @@
 cask "transnomino" do
-  version "5.2.1"
-  sha256 "9dfaed1b36ecc2013c985e4ddaad79c587a8c7eabc3f71c46bdce519d26d7310"
+  version "5.3.0"
+  sha256 "01f408e71385d22cab762fddee6e8f4fe7f116d05e589550c6f6129a82dc982f"
 
   url "https://transnomino.bastiaanverreijt.com/download/Transnomino-#{version}.dmg"
   appcast "https://transnomino.bastiaanverreijt.com/"
