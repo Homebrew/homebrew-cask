6270f24dd9815c0a1555d81737c05bcf7ea56c71
Jacob Hein <10779515+Jahhein@users.noreply.github.com>
2020-11-16 13:58:25 -0500

    Hazel update from 4.4.5 to 5.0

--- Casks/hazel.rb
+++ Casks/hazel.rb
@@ -1,6 +1,6 @@
 cask "hazel" do
-  version "4.4.5"
-  sha256 "19f0a193831b8d61b8b3a5e87ab08e5295e4e7680d13615ed5b14b34f60c3cef"
+  version "5.0"
+  sha256 "4225675205e7780fa78d7824a825bda7e2cc21ee856a6855401953dd231e15b9"
 
   # s3.amazonaws.com/Noodlesoft/ was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/Noodlesoft/Hazel-#{version}.dmg"
