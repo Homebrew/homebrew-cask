9d330de0f24d61dec59067f508b4029b0229fc5a
CoreCode <core-code@users.noreply.github.com>
2020-11-16 22:58:24 +0100

    Update hyperbackupexplorer.rb

--- Casks/hyperbackupexplorer.rb
+++ Casks/hyperbackupexplorer.rb
@@ -3,7 +3,7 @@ cask "hyperbackupexplorer" do
   sha256 "e5dc0c7bbe11782e69d602c11b373354fb64a82689e33eba73433d91bd8de15d"
 
   url "https://global.download.synology.com/download/Utility/HyperBackupExplorer/#{version}/Mac/x86_64/HyperBackupExplorer-#{version}-mac.dmg"
-  appcast "https://www.synology.com/en-us/dsm/packages/HyperBackup"
+  appcast "https://www.synology.com/en-global/releaseNote/HyperBackupExplorer"
   name "HyperBackupExplorer"
   homepage "https://www.synology.com/en-global/releaseNote/HyperBackupExplorer"
 
