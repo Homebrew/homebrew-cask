4c036688f93c61d2d738e0059919a5018da6e5cf
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-16 21:29:54 +0000

    Update wordpresscom from 6.3.0 to 6.4.0 (#92883)

--- Casks/wordpresscom.rb
+++ Casks/wordpresscom.rb
@@ -1,6 +1,6 @@
 cask "wordpresscom" do
-  version "6.3.0"
-  sha256 "eb51959511fc33640f60de73395f1a82788c65ac4581773fd3598a66828c74af"
+  version "6.4.0"
+  sha256 "970c7eb6b697f710c814f5abf7160f6d3523fb642eeeb6f2d5e49250156d6d17"
 
   url "https://public-api.wordpress.com/rest/v1.1/desktop/osx/download?type=app&ref=update&version=#{version}"
   appcast "https://public-api.wordpress.com/rest/v1.1/desktop/osx/version?compare=0.1.0&channel=stable"
