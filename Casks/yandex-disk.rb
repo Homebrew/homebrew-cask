573be3d6919f09b439385a6fcd499d31a03fc200
suschizu <brigitte@corecode.io>
2020-11-16 18:37:50 +0100

    Update yandex-disk from 3.2.0,30 to 3.2.1,30 (#92848)

--- Casks/yandex-disk.rb
+++ Casks/yandex-disk.rb
@@ -1,6 +1,6 @@
 cask "yandex-disk" do
-  version "3.2.0,30"
-  sha256 "cc849b7b2f4b6b2cbb346344bd740b9fd488d6eeb34c5393c35d4ffd5cc6f869"
+  version "3.2.1,30"
+  sha256 "cf69a3ff787ad970715c95aceaeb0e10de4664a8c97e60c314f3735eb44338ec"
 
   url "https://disk.yandex.ru/download/YandexDisk#{version.after_comma}.dmg/?instant=1"
   name "Yandex.Disk"
