0d82f38c13ed562c379bec47345c43089aacf515
Stephen Wade <stephen@stephenwade.me>
2020-11-15 21:02:33 -0500

    Update simplenote from 2.0.0 to 2.1.0

--- Casks/simplenote.rb
+++ Casks/simplenote.rb
@@ -1,6 +1,6 @@
 cask "simplenote" do
-  version "2.0.0"
-  sha256 "0e8696239fc2b144ef50e3af9a7d8e134a586e6422262b34b2c84c1ba71ce9da"
+  version "2.1.0"
+  sha256 "4f004f3acacf0d76a616d73d06face6c74ffbb10cc5e28efe17062c5a216324c"
 
   url "https://github.com/Automattic/simplenote-electron/releases/download/v#{version}/Simplenote-macOS-#{version}.dmg"
   appcast "https://github.com/Automattic/simplenote-electron/releases.atom"
