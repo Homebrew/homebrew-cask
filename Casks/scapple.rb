f3573bc1fafd48781131cd0b438f9f73431fd653
CoreCode <core-code@users.noreply.github.com>
2020-11-16 22:53:05 +0100

    Update scapple from 1.3.4 to 1.4 (#92890)

--- Casks/scapple.rb
+++ Casks/scapple.rb
@@ -1,6 +1,6 @@
 cask "scapple" do
-  version "1.3.4"
-  sha256 "0f81d00286f0c49fafc03966bfb2c4dd9fc036495d69def64988d489223e7892"
+  version "1.4"
+  sha256 "65254fc8178028dd9e006e5e3158b1284833dde56efcca4b3dd7758d406251b2"
 
   # scrivener.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://scrivener.s3.amazonaws.com/Scapple.dmg"
