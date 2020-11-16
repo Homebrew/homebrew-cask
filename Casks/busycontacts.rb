edc88cd11fa72632812619da30eb6f3ddb73aeab
Markus Reiter <me@reitermark.us>
2020-11-16 01:24:21 +0100

    Update busycontacts.rb

--- Casks/busycontacts.rb
+++ Casks/busycontacts.rb
@@ -1,8 +1,9 @@
 cask "busycontacts" do
-  version "1.4.11,141103"
+  version "1.4.11,2020-11-15-03-25"
   sha256 "e2d23240e43f1645a4cb309cc72ca944d9294d5e9407e907dadb637cf01dc05e"
 
-  url "https://www.busymac.com/download/BusyContacts.zip"
+  # 7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com was verified as official when first introduced to the cask
+  url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bct-#{version.before_comma}-#{version.after_comma}.zip"
   appcast "https://www.busymac.com/busycontacts/news.plist"
   name "BusyContacts"
   desc "Contact manager focusing on efficiency"
