5735366fd56781502e796ebce7ed33c78b673001
CoreCode <core-code@users.noreply.github.com>
2020-11-16 22:52:20 +0100

    Update sherlock from 1.8.1 to 1.9.0 (#92891)

--- Casks/sherlock.rb
+++ Casks/sherlock.rb
@@ -1,6 +1,6 @@
 cask "sherlock" do
-  version "1.8.1"
-  sha256 "093f02131ef3559da98f3801794d632eb63f585992d475c9645b487a33bae71f"
+  version "1.9.0"
+  sha256 "56bc2f5477633aa8e8672fc8da1bd4ac9cac3960762f3b3f13a062aca6faebbc"
 
   # dl.devmate.com/io.inspiredcode.Sherlock/ was verified as official when first introduced to the cask
   url "https://dl.devmate.com/io.inspiredcode.Sherlock/Sherlock.dmg"
