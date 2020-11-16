bf2dc5aa03109811184332acef726fc7cf737412
CoreCode <core-code@users.noreply.github.com>
2020-11-16 12:45:23 +0100

    Update openvanilla.rb

--- Casks/openvanilla.rb
+++ Casks/openvanilla.rb
@@ -3,7 +3,7 @@ cask "openvanilla" do
   sha256 "248c2a84f95f40ffea2fff71310814103ae70d87a829e136713ff801b39a29f2"
 
   # github.com/openvanilla/openvanilla/ was verified as official when first introduced to the cask
-  url "https://github.com/openvanilla/openvanilla/releases/download/#{version}/OpenVanilla-Installer-Mac-#{version}.zip"
+  url "https://github.com/openvanilla/openvanilla/releases/download/#{version.before_comma}/OpenVanilla-Installer-Mac-#{version.before_comma}.zip"
   appcast "https://github.com/openvanilla/openvanilla/releases.atom"
   name "OpenVanilla"
   homepage "https://openvanilla.org/"
