0589bbcfa152650542bafef934d4dede5240e588
suschizu <brigitte@corecode.io>
2020-11-16 09:01:25 +0100

    Update synology-note-station-client.rb

--- Casks/synology-note-station-client.rb
+++ Casks/synology-note-station-client.rb
@@ -3,7 +3,7 @@ cask "synology-note-station-client" do
   sha256 "abf3f0add609bb7eb4ed36e180a140f74cda62004cfc996f785a2c45e09fc360"
 
   url "https://global.download.synology.com/download/Tools/NoteStationClient/#{version}/Mac/x86_64/synology-note-station-client-#{version}-mac-x64.dmg"
-  appcast "https://archive.synology.com/download/Tools/NoteStationClient/"
+  appcast "https://www.synology.com/en-us/releaseNote/NoteStationClient"
   name "Synology Note Station Client"
   homepage "https://www.synology.com/en-us/dsm/packages/NoteStation"
 
