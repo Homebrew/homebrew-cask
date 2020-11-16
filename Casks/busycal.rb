ef1c4e55ad298d91c01f56a357d86f5ba7d98999
Markus Reiter <me@reitermark.us>
2020-11-16 02:45:31 +0100

    Update busycal.rb

--- Casks/busycal.rb
+++ Casks/busycal.rb
@@ -1,5 +1,5 @@
 cask "busycal" do
-  version "3.11.1-2020-11-15-05-54
+  version "3.11.1-2020-11-15-05-54"
   sha256 "ae039923ed462308264376ab69bedbcbe1dfe5d860799f494d9a4dfc0737e891"
 
   # 7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com was verified as official when first introduced to the cask
