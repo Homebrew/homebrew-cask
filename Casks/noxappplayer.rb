95726b7b8cb9ee4de722722ab7023b1a7ee5eb45
Brigitte Zmoelnig <brigitte@corecode.io>
2020-11-16 11:33:01 +0100

    Update noxappplayer from 3.8.1.0,20201014:0ea933d044244d13ab6472611973b163 to 3.8.5.0,20201113:05630baf34354e2f9c57162b8f3db659

--- Casks/noxappplayer.rb
+++ Casks/noxappplayer.rb
@@ -1,6 +1,6 @@
 cask "noxappplayer" do
-  version "3.8.1.0,20201014:0ea933d044244d13ab6472611973b163"
-  sha256 "b0ae0e2a42918d35b4664dc0b8e7e2d2ce502f31958888915c4fc6795db165bd"
+  version "3.8.5.0,20201113:05630baf34354e2f9c57162b8f3db659"
+  sha256 "89fa366f804fdf4d82f79a7a15cf2a12dfda473acb330f846e5f50a1eedd5f13"
 
   url "https://res06.bignox.com/full/#{version.after_comma.before_colon}/#{version.after_colon}.dmg?filename=NoxInstaller_#{version.before_comma}_en.dmg"
   appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect_curl.cgi?url=https://www.bignox.com/en/download/fullPackage/mac_fullzip"
