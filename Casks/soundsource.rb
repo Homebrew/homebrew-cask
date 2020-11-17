b85f520a7c524620d835a4d54202b45a7e1b88f1
Julian Mayer <julian@corecode.io>
2020-11-17 13:18:55 +0100

    Update soundsource from 5.1.2 to 5.1.3

--- Casks/soundsource.rb
+++ Casks/soundsource.rb
@@ -1,6 +1,6 @@
 cask "soundsource" do
-  version "5.1.2"
-  sha256 "4fe1a67a3865f173991821f215d5e22adc6e9edc418c40c7da6e1814945001a4"
+  version "5.1.3"
+  sha256 "9ba283c65f767242e947984daa7de765b2aee5db2cd25d69b7a6b0260de2e531"
 
   url "https://rogueamoeba.com/soundsource/download/SoundSource.zip"
   appcast "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.soundsource&system=10146&version=5000000"
