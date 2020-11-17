70f43e24807afdf5d34b8d88860c548600024d7e
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-17 22:46:25 +0000

    Update openphone from 1.9.7 to 2.0.0 (#92968)
    * Update openphone from 1.9.7 to 2.0.0
    
    * Update openphone.rb
    
    * Update openphone.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/openphone.rb
+++ Casks/openphone.rb
@@ -1,11 +1,12 @@
 cask "openphone" do
-  version "1.9.7"
-  sha256 "c4c80840c8b0752f9a84ed7c2544161092a3232f7693d3a09ed29931fca73bbc"
+  version "2.0.0"
+  sha256 "a7b7fb7f8ba608afd85791e1989255231781c4a1689b73bfcfae6833978c9726"
 
   url "https://download.openphone.co/OpenPhone-#{version}.dmg"
   appcast "https://s3-us-west-2.amazonaws.com/download.openphone.co/latest-mac.yml"
   name "OpenPhone"
-  homepage "https://www.openphone.co/desktop"
+  desc "Business phone for professionals, teams, and companies"
+  homepage "https://www.openphone.co/"
 
   app "OpenPhone.app"
 end
