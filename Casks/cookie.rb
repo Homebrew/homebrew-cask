0acb7789e4f7ffbb42047834cf5a95ebf0442172
Julian Mayer <julian@corecode.io>
2020-11-17 13:18:41 +0100

    Update cookie from 6.1.6 to 6.2

--- Casks/cookie.rb
+++ Casks/cookie.rb
@@ -1,6 +1,6 @@
 cask "cookie" do
-  version "6.1.6"
-  sha256 "72e2db63ae4a8bde9dd4985432d717c8327f0a269cbc589141df1c36578fd235"
+  version "6.2"
+  sha256 "db196a6f8d93dab9f0109de3bb754fd619c99320b172001b7c81a50552ab29a3"
 
   url "https://sweetpproductions.com/products/cookieapp/Cookie.dmg"
   appcast "https://sweetpproductions.com/products/cookieapp/appcast.xml"
