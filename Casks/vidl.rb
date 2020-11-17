2a4c087e10dd05596d380cf898571903f79aff66
Alexey Naiden <alexey.nayden@gmail.com>
2020-11-17 15:07:07 +0300

    Add ViDL Latest

---
+++ Casks/vidl.rb
@@ -0,0 +1,11 @@
+cask "vidl" do
+  version :latest
+  sha256 :no_check
+
+  url "https://omz-software.com/vidl/ViDL.zip"
+  name "ViDL for Mac"
+  desc "ViDL is a free Mac app that allows you to easily download videos from YouTube and hundreds of other websites for offline viewing."
+  homepage "https://omz-software.com/vidl/"
+
+  app "ViDL.app"
+end
