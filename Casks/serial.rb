3ff22b727558dcfc53e3a9125d003b86cb8509e3
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-17 19:15:16 +0000

    Update serial from 2.0.4 to 2.0.5 (#92952)

--- Casks/serial.rb
+++ Casks/serial.rb
@@ -1,6 +1,6 @@
 cask "serial" do
-  version "2.0.4"
-  sha256 "feed4fcc1be4c747fdc3868b1c235d2a1c25627b22c07df44639c93355b60e85"
+  version "2.0.5"
+  sha256 "f17db1aa5d7fbbbd15c8635203418b2729f7a679941a72b8e2ce9b43695170c4"
 
   url "https://download.decisivetactics.com/downloads/serial/Serial_#{version}.zip"
   appcast "https://api.decisivetactics.com/api/v1/public/appcast?app=serial"
