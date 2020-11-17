dcb3cfc3175bcde3dd2896c7dae5ce0f423a730b
CoreCode <core-code@users.noreply.github.com>
2020-11-17 12:48:03 +0100

    Update catalina-cache-cleaner.rb

--- Casks/catalina-cache-cleaner.rb
+++ Casks/catalina-cache-cleaner.rb
@@ -1,13 +1,13 @@
-cask "catalina-cache-cleaner" do
+cask "bigsur-cache-cleaner" do
   version :latest
   sha256 :no_check
 
-  url "https://www.northernsoftworks.com/downloads/ccc.dmg"
-  name "Catalina Cache Cleaner"
-  name "CCC"
-  homepage "https://www.northernsoftworks.com/catalinacachecleaner.html"
+  url "https://www.northernsoftworks.com/downloads/bscc.dmg"
+  name "Big Sur Cache Cleaner"
+  name "BSCC"
+  homepage "https://www.northernsoftworks.com/bigsurcachecleaner.html"
 
-  depends_on macos: ">= :catalina"
+  depends_on macos: ">= :yosemite"
 
-  app "Catalina Cache Cleaner.app"
+  app "Big Sur Cache Cleaner.app"
 end
