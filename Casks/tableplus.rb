86fbde6831a26e4c1463c2e5141ea2b206846454
Phuc Nguyen Minh <minhphuc429@gmail.com>
2020-11-17 21:48:29 +0700

    Update tableplus.rb from 3.10.0,348 to 3.11.0,352 (#92917)

--- Casks/tableplus.rb
+++ Casks/tableplus.rb
@@ -1,6 +1,6 @@
 cask "tableplus" do
-  version "3.10.0,348"
-  sha256 "87f8a72bf6fbd1f436a0fafc719f937103bab4f4fe093ac56b5d4e8c49da19c1"
+  version "3.11.0,352"
+  sha256 "a0ae5c2aa54a23ed336437e0df151f240de75ef7342e18ad08ae13ceb7be97f0"
 
   url "https://download.tableplus.com/macos/#{version.after_comma}/TablePlus.dmg"
   appcast "https://tableplus.com/osx/version.xml",
