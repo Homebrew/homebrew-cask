6b6c4c22172e6468e483b0546b845f7b7bb897c8
Guillaume Louel <guillaume@louel.fr>
2020-11-17 14:59:09 +0100

    Update aerial from 1.9.2 to 2.1.3

--- Casks/aerial.rb
+++ Casks/aerial.rb
@@ -1,6 +1,6 @@
 cask "aerial" do
-  version "1.9.2"
-  sha256 "1d21511a31895ece4a18c93c779cbb35a643c5a957a16748cbf4e35a611a27ba"
+  version "2.1.3"
+  sha256 "5fa5e8b31688ae37c3a500ed6849407904b81ef0f0577ae6177e404a386ad9b5"
 
   url "https://github.com/JohnCoates/Aerial/releases/download/v#{version}/Aerial.saver.zip"
   appcast "https://github.com/JohnCoates/Aerial/releases.atom"
