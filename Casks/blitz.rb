f8bd3b4b7a34903075f4ec93455ee024c2acef66
Randall <ran.dall@icloud.com>
2020-11-17 10:51:35 -0800

    Update blitz from 1.13.36 to 1.13.39

--- Casks/blitz.rb
+++ Casks/blitz.rb
@@ -1,6 +1,6 @@
 cask "blitz" do
-  version "1.13.36"
-  sha256 "ce66c7a2d9e9e6efc4d7843f535ec2784f4b638d983e604bc163c3c30ac10a19"
+  version "1.13.39"
+  sha256 "25869e9e523e70cacfa0d6c8b0eb4e1e014e628adde75c90559ee54089f8b659"
 
   url "https://dl.blitz.gg/download/Blitz-#{version}.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://dl.blitz.gg/download/mac"
