1ad30d7d0980398093320765c660022c8664461d
Connor Gray <connorg@wolfram.com>
2020-11-17 12:31:59 -0600

    Update URL to use file from files.wolframcdn.com

--- Casks/wolfram-engine.rb
+++ Casks/wolfram-engine.rb
@@ -2,8 +2,7 @@ cask "wolfram-engine" do
   version "12.1.1"
   sha256 "fe43def3b2828286ffae0fee8ee82227322751d68467d4a60d2283be6df41fe3"
 
-  # url "http://buildmonitor.wolfram.com/artifact/download/6982007/Files/M-OSX-WolframEngine-12.1.1-6982007.dmg"
-  url "file:///Users/connorgray/Desktop/WolframEngine-12.1.1.dmg"
+  url "https://files.wolframcdn.com/packages/Homebrew/12.1.1.0/WolframEngine_12.1.1_MAC.dmg"
   name "Wolfram Engine"
   desc "Evaluator for the Wolfram Language"
   homepage "https://www.wolfram.com/engine/"
