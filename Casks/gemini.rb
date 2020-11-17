d9ee160bdafac5d09e45a2a5ef9f8215852db2fc
Randall <ran.dall@icloud.com>
2020-11-17 10:45:17 -0800

    Update gemini from 2.7.0,356:1604685686 to 2.7.1,357:1605619595

--- Casks/gemini.rb
+++ Casks/gemini.rb
@@ -1,6 +1,6 @@
 cask "gemini" do
-  version "2.7.0,356:1604685686"
-  sha256 "e34ad01f530d11ce4a59b5dbe6cdff69b60e80683801ff04f37c95ff77ded88d"
+  version "2.7.1,357:1605619595"
+  sha256 "ce1fc36bed622a9419f7ecde0a3c671766bee87e8ec280c2e6c0c58700639aa3"
 
   # dl.devmate.com/com.macpaw.site.Gemini was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.macpaw.site.Gemini#{version.major}/#{version.after_comma.before_colon}/#{version.after_colon}/Gemini#{version.major}-#{version.after_comma.before_colon}.zip"
