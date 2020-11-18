62a09e3c6d67c5abf97bdbf44927391ed0da12f8
John Britton <public@johndbritton.com>
2020-11-18 15:31:02 -0500

    Add Teleport 1.2.2

---
+++ Casks/abyssoft-teleport.rb
@@ -0,0 +1,20 @@
+cask "abyssoft-teleport" do
+  version "1.2.2"
+  sha256 "b087e5309ae64a51fb8d1f0ded2a2795dee9d77134022df05bb48da413c3968a"
+
+  url "https://github.com/abyssoft/teleport/releases/download/v#{version}/teleport-v#{version}.zip"
+  appcast "https://github.com/abyssoft/teleport/releases.atom"
+  name "teleport"
+  desc "Virtual KVM"
+  homepage "https://github.com/abyssoft/teleport"
+
+  app "teleport.app"
+
+  zap trash: [
+    "~/Library/Caches/com.abyssoft.teleport",
+    "~/Library/Caches/com.abyssoft.teleport/Cache.db-shm",
+    "~/Library/Caches/com.abyssoft.teleport/Cache.db-wal",
+    "~/Library/Caches/com.abyssoft.teleport/Cache.db"
+    "~/Library/Preferences/com.abyssoft.teleport.plist",
+  ]
+end
