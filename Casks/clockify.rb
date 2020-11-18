87c00c701ce3b70a363604609b25041eb3a0c098
Julian Mayer <julian@corecode.io>
2020-11-18 11:33:11 +0100

    Update clockify from 2.5.8 to 2.5.10

--- Casks/clockify.rb
+++ Casks/clockify.rb
@@ -1,6 +1,6 @@
 cask "clockify" do
-  version "2.5.8"
-  sha256 "84297c0f98e2a3a80f0d57f1aa11cdf23e1c4226fb086dfc8b43f65948530597"
+  version "2.5.10"
+  sha256 "cf4b8b9312e4f3249ea4668e7a41bd990d91796f1624ab220ddb617fd91f6278"
 
   # clockify-resources.s3.eu-central-1.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://clockify-resources.s3.eu-central-1.amazonaws.com/downloads/ClockifyDesktop.zip"
