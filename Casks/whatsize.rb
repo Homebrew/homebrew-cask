46c765ef92a358c77752c875399042d4f6ef9062
Randall <ran.dall@icloud.com>
2020-11-18 12:31:17 -0800

    Update whatsize from 7.4.4 to 7.6.2

--- Casks/whatsize.rb
+++ Casks/whatsize.rb
@@ -1,6 +1,6 @@
 cask "whatsize" do
-  version "7.4.4"
-  sha256 "b42857c57cded85aeb7a70ac5d0c277ca1b56c02242491b0f7de9a83db9b5a17"
+  version "7.6.2"
+  sha256 "a753e639a52f33b8277d1785ea6c6c1ca80a2e238da19fbfc1dd325bae02cdde"
 
   url "https://www.whatsizemac.com/software/whatsize#{version.major}/whatsize_#{version}.tgz"
   appcast "https://www.whatsizemac.com/software/whatsize#{version.major}/release/notes.xml"
