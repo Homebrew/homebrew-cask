e8d37e553dbdb32b6b8a673864614781f118a19f
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-18 18:37:32 +0800

    Update avast-secure-browser.rb

--- Casks/avast-secure-browser.rb
+++ Casks/avast-secure-browser.rb
@@ -2,13 +2,14 @@ cask "avast-secure-browser" do
   version :latest
   sha256 :no_check
 
-  # cdn-download.avastbrowser.com was verified as official when first introduced to the cask
-  url "https://cdn-download.avastbrowser.com/AvastSecureBrowserSetup.pkg?cid=6302"
+  # cdn-download.avastbrowser.com/ was verified as official when first introduced to the cask
+  url "https://cdn-download.avastbrowser.com/AvastSecureBrowserSetup.pkg"
   name "Avast Secure Browser"
-  desc "Private browser that’s up to 4x faster"
-  homepage "https://platform.avast.com/ASB/Sync2/"
+  desc "Web browser focusing on privacy"
+  homepage "https://www.avast.com/secure-browser#mac"
 
   pkg "AvastSecureBrowserSetup.pkg"
 
-  uninstall pkgutil: "com.avast.browser"
+  uninstall launchctl: "com.avast.browser.*",
+            pkgutil:   "com.avast.browser"
 end
