0024a84a3576b3f468c6b910e73583fea7828953
Randall <ran.dall@icloud.com>
2020-11-18 12:14:41 -0800

    Update electron-cash from 4.2.0 to 4.2.1

--- Casks/electron-cash.rb
+++ Casks/electron-cash.rb
@@ -1,6 +1,6 @@
 cask "electron-cash" do
-  version "4.2.0"
-  sha256 "0d229e60c803536aab7510d14a66a8aaf8bcad936d637ee220329443d75ae76f"
+  version "4.2.1"
+  sha256 "65876d20f9bb8498f3b006c3990526a24da67db7c1b3717dc82915756ec94fea"
 
   url "https://electroncash.org/downloads/#{version}/mac/Electron-Cash-#{version}-macosx.dmg"
   appcast "https://github.com/Electron-Cash/Electron-Cash/releases.atom"
