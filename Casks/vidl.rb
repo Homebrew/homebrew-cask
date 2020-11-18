d0639ceefaac29027f8599960b922339a2cff219
Alexey Naiden <alexey.nayden@gmail.com>
2020-11-18 14:48:05 +0300

    Fix style

--- Casks/vidl.rb
+++ Casks/vidl.rb
@@ -11,9 +11,9 @@ cask "vidl" do
   app "ViDL.app"
 
   zap trash: [
-      "~/Library/Application Support/ViDL/", 
-      "~/Library/Caches/com.omz-software.ViDL", 
-      "~/Library/Containers/com.omz-software.ViDL.ViDL-SafariExtension", 
+      "~/Library/Application Support/ViDL/",
+      "~/Library/Caches/com.omz-software.ViDL",
+      "~/Library/Containers/com.omz-software.ViDL.ViDL-SafariExtension",
       "~/Library/Saved Application State/com.omz-software.ViDL.savedState",
   ]
 end
