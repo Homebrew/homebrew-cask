740b8c3ddad0b8fb141b33b30023d3d7bc5777e4
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-20 05:21:57 +0800

    Update bigsur-cache-cleaner.rb

--- Casks/bigsur-cache-cleaner.rb
+++ Casks/bigsur-cache-cleaner.rb
@@ -5,6 +5,7 @@ cask "bigsur-cache-cleaner" do
   url "https://www.northernsoftworks.com/downloads/bscc.dmg"
   name "Big Sur Cache Cleaner"
   name "BSCC"
+  desc "General purpose system maintenance tool"
   homepage "https://www.northernsoftworks.com/bigsurcachecleaner.html"
 
   depends_on macos: ">= :yosemite"
