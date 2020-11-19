d430448154604dc1382370aa461432f2ec761829
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-19 08:38:18 +0800

    Update avast-secure-browser.rb

--- Casks/avast-secure-browser.rb
+++ Casks/avast-secure-browser.rb
@@ -10,6 +10,6 @@ cask "avast-secure-browser" do
 
   pkg "AvastSecureBrowserSetup.pkg"
 
-  uninstall launchctl: "com.avast.browser.*",
-            pkgutil:   "com.avast.browser"
+  uninstall delete:  "~/Library/LaunchAgents/com.avast.browser.*",
+            pkgutil: "com.avast.browser"
 end
