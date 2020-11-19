e1cf406361cadc1e09853951bbedb55761487c95
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-20 05:09:41 +0800

    Update 7777.rb

--- Casks/7777.rb
+++ Casks/7777.rb
@@ -4,7 +4,7 @@ cask "7777" do
 
   url "https://releases.port7777.com/#{version}/macos/7777"
   name "7777"
-  desc "Your remote AWS database on your local port 7777"
+  desc "Remote AWS database on local port 7777"
   homepage "https://port7777.com/"
 
   binary "7777"
