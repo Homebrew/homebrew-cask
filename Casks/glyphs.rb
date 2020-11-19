c273a85add44004e147f204e1aedf6abf2a22f60
CoreCode <core-code@users.noreply.github.com>
2020-11-19 11:22:55 +0100

    Update glyphs.rb

--- Casks/glyphs.rb
+++ Casks/glyphs.rb
@@ -1,8 +1,8 @@
 cask "glyphs" do
-  version "2.6.6,1350"
-  sha256 "43bd69590c12eee4cd1a3047542379c2eef48b27138027275bd182c57705b930"
+  version "3.0,3032"
+  sha256 "8945fe168ba3212bfb04e4271e61ba669c5d150e907a71f0c44010c5085eb103"
 
-  url "https://updates.glyphsapp.com/Glyphs#{version.major_minor_patch}-#{version.after_comma}.zip"
+  url "https://updates.glyphsapp.com/Glyphs#{version.before_comma}-#{version.after_comma}.zip"
   appcast "https://updates.glyphsapp.com/appcast#{version.major}.xml"
   name "Glyphs"
   desc "Font Editor"
@@ -14,9 +14,9 @@ cask "glyphs" do
 
   zap trash: [
     "~/Library/Application Support/Glyphs",
-    "~/Library/Application Support/com.GeorgSeifert.Glyphs2",
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.georgseifert.glyphs2.sfl*",
-    "~/Library/Caches/com.apple.helpd/SDMHelpData/Other/English/HelpSDMIndexFile/com.GeorgSeifert.Glyphs2.help*",
-    "~/Library/Preferences/com.GeorgSeifert.Glyphs2.plist",
+    "~/Library/Application Support/com.GeorgSeifert.Glyphs3",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.georgseifert.glyphs3.sfl*",
+    "~/Library/Caches/com.apple.helpd/SDMHelpData/Other/English/HelpSDMIndexFile/com.GeorgSeifert.Glyphs3.help*",
+    "~/Library/Preferences/com.GeorgSeifert.Glyphs3.plist",
   ]
 end
