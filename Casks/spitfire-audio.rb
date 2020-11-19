7a3dfbdea1e7008d036cddf1b6dba32c3487d771
Hannes Braun <hannesbraun@mail.de>
2020-11-19 08:34:14 +0100

    Update spitfire-audio from 3.2.16 to 3.2.17,1605696000

--- Casks/spitfire-audio.rb
+++ Casks/spitfire-audio.rb
@@ -1,9 +1,9 @@
 cask "spitfire-audio" do
-  version "3.2.16"
-  sha256 "b9f9dfb82415188ccc34920709f0f50438689a3639d2e0324fa941c237733170"
+  version "3.2.17,1605696000"
+  sha256 "8a61a597f58dec65426a49c30da2bb9977e778233d18df9eceb3e8e6bc27be3b"
 
   # d1t3zg51rvnesz.cloudfront.net/ was verified as official when first introduced to the cask
-  url "https://d1t3zg51rvnesz.cloudfront.net/p/files/lm/1601024400/mac/SpitfireAudio-Mac-#{version}.dmg"
+  url "https://d1t3zg51rvnesz.cloudfront.net/p/files/lm/#{version.after_comma}/mac/SpitfireAudio-Mac-#{version.before_comma}.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.spitfireaudio.com/library-manager/download/mac/"
   name "Spitfire Audio"
   desc "Downloade manager for Spitfire audio libraries"
