38d884fb0532552525202319cd8e6a882e2e267d
Vítor Galvão <info@vitorgalvao.com>
2020-11-19 01:33:41 +0000

    Update avast-secure-browser.rb

--- Casks/avast-secure-browser.rb
+++ Casks/avast-secure-browser.rb
@@ -11,9 +11,10 @@ cask "avast-secure-browser" do
   pkg "AvastSecureBrowserSetup.pkg"
 
   uninstall_preflight do
-    avast_launch = Open3.capture2("launchctl", "list").first.split("\n").map { |a| a.sub(/^.*\t/, "") }.select { |a| a.start_with?("com.avast.browser.") }
+    avast_launch = Open3.capture2("launchctl", "list").first.split("\n")
+                        .map { |a| a.sub(/^.*\t/, "") }.select { |a| a.start_with?("com.avast.browser.") }
   end
 
-  uninstall launchctl: "#{avast_launch}",
+  uninstall launchctl: avast_launch,
             pkgutil:   "com.avast.browser"
 end
