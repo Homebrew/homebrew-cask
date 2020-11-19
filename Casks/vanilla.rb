8f8eacbc0012e05300333fc8a737798d7eb794cf
suschizu <brigitte@corecode.io>
2020-11-19 21:05:12 +0100

    Update vanilla from 2.0,46 to 2.0.1,47 (#93081)

--- Casks/vanilla.rb
+++ Casks/vanilla.rb
@@ -1,9 +1,9 @@
 cask "vanilla" do
-  version "2.0,46"
-  sha256 "1ef371f039dd099c13b9caf0b427fcb26fadc5cd52c01f2f5cc12860ee66ecc6"
+  version "2.0.1,47"
+  sha256 "69e9c73719e32f0b524b8c4fad99ac6d21a25df426fb8432e1a61c7cd1fc5ab2"
 
   url "https://macrelease.matthewpalmer.net/distribution/appcasts/Vanilla-#{version.after_comma}.dmg"
-  appcast "https://updates.devmate.com/net.matthewpalmer.Vanilla.xml"
+  appcast "https://macrelease.matthewpalmer.net/distribution/appcasts/vanilla.xml?beta=false"
   name "Vanilla"
   desc "Tool to hide menu bar icons"
   homepage "https://matthewpalmer.net/vanilla/"
