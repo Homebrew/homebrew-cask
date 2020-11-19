f9b4651ebce09081af3e907e4f1008ab72372060
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-20 04:57:56 +0800

    Update fsnotes.rb

--- Casks/fsnotes.rb
+++ Casks/fsnotes.rb
@@ -3,7 +3,7 @@ cask "fsnotes" do
   sha256 "fc5ec32c6412c319927edf0c85bf103ce484d8558bf84868207a2eece30932e3"
 
   # github.com/glushchenko/fsnotes/ was verified as official when first introduced to the cask
-  url "https://github.com/glushchenko/fsnotes/releases/download/#{version.before_comma},build#{version.after_comma}/FSNotes_#{version.before_comma}.zip"
+  url "https://github.com/glushchenko/fsnotes/releases/download/#{version.before_comma}_build#{version.after_comma}/FSNotes_#{version.before_comma}.zip"
   appcast "https://github.com/glushchenko/fsnotes/releases.atom"
   name "FSNotes"
   desc "Notes manager"
