00c781248586479a4151e146c71e5258f0fd22f3
Ori Peleg <orip@users.noreply.github.com>
2020-11-19 23:06:57 +0200

    Update fsnotes from 4.8.4 to 4.8.4_build442 (#93079)
    * Update fsnotes from 4.8.4 to 4.8.4_build442
    
    * Update fsnotes.rb
    
    * Update fsnotes.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/fsnotes.rb
+++ Casks/fsnotes.rb
@@ -1,9 +1,9 @@
 cask "fsnotes" do
-  version "4.8.4"
-  sha256 "364b702240afa9abf37bf05c42a13d808b27c951c3e097f0a103ab2df6b772cd"
+  version "4.8.4,442"
+  sha256 "fc5ec32c6412c319927edf0c85bf103ce484d8558bf84868207a2eece30932e3"
 
   # github.com/glushchenko/fsnotes/ was verified as official when first introduced to the cask
-  url "https://github.com/glushchenko/fsnotes/releases/download/#{version}/FSNotes_#{version}.zip"
+  url "https://github.com/glushchenko/fsnotes/releases/download/#{version.before_comma}_build#{version.after_comma}/FSNotes_#{version.before_comma}.zip"
   appcast "https://github.com/glushchenko/fsnotes/releases.atom"
   name "FSNotes"
   desc "Notes manager"
