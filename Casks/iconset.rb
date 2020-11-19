0a7d1aa9d1b2f45ae103a81bd8fd77df737c29d9
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-19 10:06:48 +0800

    Update iconset.rb

--- Casks/iconset.rb
+++ Casks/iconset.rb
@@ -2,7 +2,7 @@ cask "iconset" do
   version "1.3.11"
   sha256 "33b86a855464f16dcbbd9fcde1cbb2eeb4a20d7aecc1b68393a82d54bb1bbac4"
 
-  # github.com/IconsetApp/iconset was verified as official when first introduced to the cask
+  # github.com/IconsetApp/iconset/ was verified as official when first introduced to the cask
   url "https://github.com/IconsetApp/iconset/releases/download/v#{version}/Iconset-#{version}-mac.zip"
   appcast "https://github.com/IconsetApp/iconset/releases.atom"
   name "Iconset"
@@ -12,8 +12,8 @@ cask "iconset" do
   app "Iconset.app"
 
   zap trash: [
+    "~/Library/Application Support/Iconset",
     "~/Library/Preferences/io.iconset.app.plist",
     "~/Library/Saved Application State/io.iconset.app.savedState",
-    "~/Library/Application Support/Iconset",
   ]
 end
