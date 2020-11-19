caf21056425d6d848accafed01287d62c9b15505
Vítor Galvão <info@vitorgalvao.com>
2020-11-19 14:17:55 +0000

    Update fsnotes.rb

--- Casks/fsnotes.rb
+++ Casks/fsnotes.rb
@@ -1,9 +1,9 @@
 cask "fsnotes" do
-  version "4.8.4_build442"
+  version "4.8.4,442"
   sha256 "fc5ec32c6412c319927edf0c85bf103ce484d8558bf84868207a2eece30932e3"
 
   # github.com/glushchenko/fsnotes/ was verified as official when first introduced to the cask
-  url "https://github.com/glushchenko/fsnotes/releases/download/4.8.4_build442/FSNotes_4.8.4.zip"
+  url "https://github.com/glushchenko/fsnotes/releases/download/#{version.before_comma},build#{version.after_comma}/FSNotes_#{version.before_comma}.zip"
   appcast "https://github.com/glushchenko/fsnotes/releases.atom"
   name "FSNotes"
   desc "Notes manager"
