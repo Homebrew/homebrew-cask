20a08329c70cdfa68c60346807423a0daef417a7
gene <gene@genes-iMac-Pro.local>
2020-11-19 13:06:56 -0800

    Update rescuetime

--- Casks/rescuetime.rb
+++ Casks/rescuetime.rb
@@ -1,6 +1,6 @@
 cask "rescuetime" do
   version "2.16.6.1"
-  sha256 "e8edb268b37d24c64cacc5dc28ddd4310496c244c73c5722a4d4a12c6fe26c71"
+  sha256 "a0e360b9b57e738ac05f8aaba39439c94e1e3ab4ad537083a3130cf32cae1b8c"
 
   url "https://www.rescuetime.com/installers/RescueTimeInstaller.pkg"
   appcast "https://www.rescuetime.com/installers/appcast"
