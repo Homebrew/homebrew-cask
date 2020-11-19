882c5508cbfbc468d5373922b682a910c34a92f0
CoreCode <core-code@users.noreply.github.com>
2020-11-19 01:03:32 +0100

    Update pastebot from 2.3.3 to 2.4 (#93070)

--- Casks/pastebot.rb
+++ Casks/pastebot.rb
@@ -1,6 +1,6 @@
 cask "pastebot" do
-  version "2.3.3"
-  sha256 "10b68def60189611c84997e6cba5b74382e213fd16a0a23235d960adcb39ab97"
+  version "2.4"
+  sha256 "da7b93ca13110fcc4f2adc267c287a62692d25c7a4b09f14e849c5283bb898af"
 
   # tapbots.net/ was verified as official when first introduced to the cask
   url "https://tapbots.net/pastebot#{version.major}/Pastebot.dmg"
