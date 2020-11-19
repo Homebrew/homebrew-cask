e5fe4dfc0985ef20f73d4e700b30a56981e1d704
George Boot <george@famboot.nl>
2020-11-19 08:34:20 +0100

    Update Casks/7777.rb
    Co-authored-by: Matthieu Napoli <matthieu@mnapoli.fr>

--- Casks/7777.rb
+++ Casks/7777.rb
@@ -2,7 +2,7 @@ cask "7777" do
   version "1.0.3"
   sha256 "f74b2cb2ef792fc5ab81b5955a02d593dffe1972af61e20fca8a711ee017581b"
 
-  url "https://releases.port7777.com/1.0.3/macos/7777"
+  url "https://releases.port7777.com/#{version}/macos/7777"
   name "7777"
   desc "Your remote AWS database on your local port 7777"
   homepage "https://port7777.com/"
