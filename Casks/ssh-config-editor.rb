3e551e337c824af4e09c7a028f0c171943266fbf
suschizu <brigitte@corecode.io>
2020-11-19 21:05:29 +0100

    Update ssh-config-editor from 1.13.3,58 to 2.0,65 (#93102)

--- Casks/ssh-config-editor.rb
+++ Casks/ssh-config-editor.rb
@@ -1,9 +1,9 @@
 cask "ssh-config-editor" do
-  version "1.13.3,58"
-  sha256 "80b135f151d1f3aad5954a7de4986c7ed3c0ac6d7a3542c13cd0e2b9e4e4c349"
+  version "2.0,65"
+  sha256 "6a1b5acbf3577e7092dc61b5e474b2a9c4dd10d83eb2ccfc42c78b1608bb4b28"
 
   url "https://hejki.org/download/ssheditor/SSHConfigEditor-#{version.after_comma}.zip"
-  appcast "https://hejki.org/download/ssheditor/appcast.xml"
+  appcast "https://hejki.org/download/ssheditor/appcast#{version.major}.xml"
   name "SSH Config Editor"
   desc "Tool for managing the OpenSSH ssh client configuration file"
   homepage "https://www.hejki.org/ssheditor/"
