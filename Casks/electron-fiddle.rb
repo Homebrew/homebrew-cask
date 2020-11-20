826d5a9201ecf6bbfde9a1540d0dbf46dbaf8bfe
Randall <ran.dall@icloud.com>
2020-11-20 08:38:54 -0800

    Update electron-fiddle from 0.17.6 to 0.18.0

--- Casks/electron-fiddle.rb
+++ Casks/electron-fiddle.rb
@@ -1,6 +1,6 @@
 cask "electron-fiddle" do
-  version "0.17.6"
-  sha256 "f6e22f366b77c84918222e0572a7ea5c493f8a262cb9576336caa3759918f18f"
+  version "0.18.0"
+  sha256 "5e2a7090fe9237eb9b132ad383ff5b94cd2dc4a836608949b6d5fd330ee7b281"
 
   # github.com/electron/fiddle/ was verified as official when first introduced to the cask
   url "https://github.com/electron/fiddle/releases/download/v#{version}/Electron.Fiddle-darwin-x64-#{version}.zip"
