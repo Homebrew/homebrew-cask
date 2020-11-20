e0fe278e9e2f3b96814cd3c5d0f9a318bb6ba61d
suschizu <brigitte@corecode.io>
2020-11-20 16:46:49 +0100

    Update kode54-cog from 1148,3b628c398 to 1163,dca9655c6 (#93177)

--- Casks/kode54-cog.rb
+++ Casks/kode54-cog.rb
@@ -1,6 +1,6 @@
 cask "kode54-cog" do
-  version "1148,3b628c398"
-  sha256 "08a9db7b22c3507272f0919b5d1b9264881c413edd1c58c90ce0b3cb5f0e3fd8"
+  version "1163,dca9655c6"
+  sha256 "9e0a03d81c5b2e2c2e399432ea0cb9357660f1117ba42081bc09edbc45057295"
 
   # losno.co/cog/ was verified as official when first introduced to the cask
   url "https://f.losno.co/cog/Cog-#{version.after_comma}.zip"
