8f0197cac3fa6e215afc0073f0491be3dc50036e
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-20 09:44:10 -0800

    Update redisinsight from 1.7.0 to 1.8.0 (#93225)

--- Casks/redisinsight.rb
+++ Casks/redisinsight.rb
@@ -1,6 +1,6 @@
 cask "redisinsight" do
-  version "1.7.0"
-  sha256 "e5ffafb82727498a9b04d265cddc4d7c07fbe9e6501e58b7ce244b97f3805f97"
+  version "1.8.0"
+  sha256 "c1675342b848d41357afd5a436b1660a3cb1faa160f2fd5b9ef829c7db9aa83a"
 
   url "https://downloads.redisinsight.redislabs.com/#{version}/redisinsight-mac.dmg"
   appcast "https://docs.redislabs.com/latest/ri/release-notes/",
