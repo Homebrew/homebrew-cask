4a1c0f656e19a713dabaa971ad60d4bbfdbc6d44
Vítor Galvão <info@vitorgalvao.com>
2020-11-20 01:07:28 +0000

    trainerroad.rb: now an app (#93158)

--- Casks/trainerroad.rb
+++ Casks/trainerroad.rb
@@ -1,15 +1,14 @@
 cask "trainerroad" do
-  version :latest
-  sha256 :no_check
+  version "2020.47.109"
+  sha256 "8375aff2b7c69de9c5f0ec68e47abe2a4fbc7072ac5b5c67924f20dbc1eb92ca"
 
-  # trainerroadcdn1.blob.core.windows.net/ was verified as official when first introduced to the cask
-  url "https://trainerroadcdn1.blob.core.windows.net/mac/releases/production/TrainerRoad.pkg"
+  # trainrdtrcmn01un1softw01.blob.core.windows.net/ was verified as official when first introduced to the cask
+  url "https://trainrdtrcmn01un1softw01.blob.core.windows.net/installers/mac/v001/Production/TrainerRoad.dmg"
+  appcast "https://trainrdtrcmn01un1softw01.blob.core.windows.net/installers/mac/v001/Production/latest-mac.yml"
   name "TrainerRoad"
   homepage "https://www.trainerroad.com/"
 
-  pkg "TrainerRoad.pkg"
-
-  uninstall pkgutil: "com.TrainerRoad.Mac"
+  app "TrainerRoad.app"
 
   zap trash: "~/Library/Application Support/TrainerRoad"
 end
