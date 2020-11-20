c2060bf2a221b647dc1e6cb69123d11bfe328d86
suschizu <brigitte@corecode.io>
2020-11-20 18:28:35 +0100

    Update simply-fortran.rb

--- Casks/simply-fortran.rb
+++ Casks/simply-fortran.rb
@@ -1,11 +1,11 @@
 cask "simply-fortran" do
-  version "3.15.3375"
+  version "3.15.3382"
 
   if MacOS.version <= :mojave
-    sha256 "84eb3a7030f2174809c0727bb92a0681ca5f9f65be05c40ef24cbdcfbfc7ba2a"
+    sha256 "45441f0f222822af0a03dcb3da898f07252f739d71a88d534af5dd5f459f8da5"
     url "https://download.simplyfortran.com/#{version.major_minor}/macos/simplyfortran-#{version}.legacy.dmg"
   else
-    sha256 "a9116fba5a1350a3c742ca710c3311aee784a87926dea41982199b479b5d7c46"
+    sha256 "df2bbf66ca32781ff965b48e1f9f8b60aab53460dc7bcf81cbb1ef5f06277839"
     url "https://download.simplyfortran.com/#{version.major_minor}/macos/simplyfortran-#{version}.dmg"
   end
   appcast "https://simplyfortran.com/download/?platform=macos",
