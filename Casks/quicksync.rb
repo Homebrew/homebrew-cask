2dfbc04e823b80d673e6dc0e6d83d4d896682720
suschizu <brigitte@corecode.io>
2020-11-20 19:18:54 +0100

    Update quicksync.rb

--- Casks/quicksync.rb
+++ Casks/quicksync.rb
@@ -3,7 +3,7 @@ cask "quicksync" do
   sha256 "3d112da18e13b4bd0f4c98b812cc061e227d149f7dc9fc623d35ef0b07634d76"
 
   url "https://gse.gigaset.com/fileadmin/gigaset/images/CORE/QuickSync/Mac_#{version}/QuickSync.dmg"
-  appcast "https://www.gigaset.com/hq_en/cms/service/support-detail/gigaset-quicksync-mac-version.html"
+  appcast "https://cms.gigaset.com/macsw/gqs/leopard/appcast.xml"
   name "QuickSync"
   desc "Syncing software for Gigaset products"
   homepage "https://www.gigaset.com/hq_en/cms/home/support/support/downloads/quicksync/quicksync-for-mac.html"
