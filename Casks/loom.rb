ef8e9b3dcb398acac511e7c62323a42edd9f690f
Work <brigitte@corecode.io>
2020-11-20 18:14:51 +0100

    Update loom from 0.59.1 to 0.60.2

--- Casks/loom.rb
+++ Casks/loom.rb
@@ -1,6 +1,6 @@
 cask "loom" do
-  version "0.59.1"
-  sha256 "061336c5235531aff2ce9136318e4cf7cabe236d13a3e498d3c561facbffa968"
+  version "0.60.2"
+  sha256 "a7413be247db9518c513db0784df3a8a0b69a17fba9ac76738c3a7a4ad4da6c2"
 
   url "https://cdn.loom.com/desktop-packages/Loom-#{version}.dmg"
   appcast "https://s3-us-west-2.amazonaws.com/loom.desktop.packages/loom-inc-production/desktop-packages/latest-mac.yml"
