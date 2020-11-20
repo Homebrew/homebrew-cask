1c763a1f98632b2bba94fe78369b498f05f7dbe2
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-19 16:45:54 -0800

    Update pinegrow from 5.973 to 5.98 (#93149)

--- Casks/pinegrow.rb
+++ Casks/pinegrow.rb
@@ -1,6 +1,6 @@
 cask "pinegrow" do
-  version "5.973"
-  sha256 "18dc7c55b32327adea54c2eee9133da8636774fccb97bdce1bcab7e6cd3637f0"
+  version "5.98"
+  sha256 "bef63e9b9927cff74523e7d7cd743e2e2d51a07d4bb6639f3fe7baffaf065532"
 
   url "https://download.pinegrow.com/PinegrowMac.#{version}.dmg"
   appcast "https://pinegrow.com/"
