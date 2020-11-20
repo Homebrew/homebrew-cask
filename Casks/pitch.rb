89d6ff5cfced584389c0951122f64962babc145d
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-20 09:31:03 -0800

    Update pitch from 1.5.0 to 1.5.1 (#93214)

--- Casks/pitch.rb
+++ Casks/pitch.rb
@@ -1,6 +1,6 @@
 cask "pitch" do
-  version "1.5.0"
-  sha256 "fbaf87a8b1433fc857ab5f4b4ed64e99a0470e09cc8cd28594a4846f6369154b"
+  version "1.5.1"
+  sha256 "b2a8f4c7c23ce42c27010189e2b3acd27591d6a59b11f162eccb6e224e87f816"
 
   url "https://desktop-app-builds.pitch.com/Pitch-#{version}.dmg"
   appcast "https://desktop-app-builds.pitch.com/latest-mac.yml"
