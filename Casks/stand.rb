8027d9cae0a7c231c5d1855909833fb26e6f413e
Randall <ran.dall@icloud.com>
2020-11-19 16:06:45 -0800

    Update stand from 2.0 to 2.1

--- Casks/stand.rb
+++ Casks/stand.rb
@@ -1,6 +1,6 @@
 cask "stand" do
-  version "2.0"
-  sha256 "eb0555f6376757105e1faea1fd1f9e4afae21eef73fdd5c7d0fb0c76a8cd9702"
+  version "2.1"
+  sha256 "e7cc1237f50d9f6991ced65e598e02015174b98e69e90298bc42e4759bc40e4e"
 
   # f001.backblazeb2.com/file/stand-app/ was verified as official when first introduced to the cask
   url "https://f001.backblazeb2.com/file/stand-app/#{version}/Stand.zip"
