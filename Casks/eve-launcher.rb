d33b8edb63e0e67129bdb6ed78a04d2e553c0439
Randall <ran.dall@icloud.com>
2020-11-20 09:19:26 -0800

    Update eve-launcher from 1842222 to 1848961

--- Casks/eve-launcher.rb
+++ Casks/eve-launcher.rb
@@ -1,6 +1,6 @@
 cask "eve-launcher" do
-  version "1842222"
-  sha256 "11c5f7de2ba3f209f2474f823b03f6b766653eb125a847031810e44a9439dcb4"
+  version "1848961"
+  sha256 "05563e273b74dedc6f23046f73e59e2c32719f7d727e8a983e39809e21ad80a6"
 
   url "https://binaries.eveonline.com/EveLauncher-#{version}.dmg"
   appcast "https://launcher.eveonline.com/launcherVersions.json"
