c9cea2da3879f7e79a585e6cd660c47bb48296e7
Randall <ran.dall@icloud.com>
2020-11-20 09:18:42 -0800

    Update nosqlbooster-for-mongodb from 6.2.1 to 6.2.2

--- Casks/nosqlbooster-for-mongodb.rb
+++ Casks/nosqlbooster-for-mongodb.rb
@@ -1,6 +1,6 @@
 cask "nosqlbooster-for-mongodb" do
-  version "6.2.1"
-  sha256 "3f3702e7ee3503c86669040ad98c6714fee7a36d4b279f80f7e7eda670dfd450"
+  version "6.2.2"
+  sha256 "3f98a4d5ea50a78a75e8307f0208169076f385adabd30a4cc7e0d06f8cc0c5cd"
 
   # mongobooster.com was verified as official when first introduced to the cask
   url "https://s3.mongobooster.com/download/releasesv#{version.major}/nosqlbooster4mongo-#{version}.dmg"
