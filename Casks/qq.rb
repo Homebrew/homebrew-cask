a69be2e2de54e47387b3a6fd238bff6b40e69a1d
Elf Sundae <elf.sundae@gmail.com>
2020-11-20 10:16:54 +0800

    Update qq from 6.7.0.20110 to 6.7.0.20123

--- Casks/qq.rb
+++ Casks/qq.rb
@@ -1,6 +1,6 @@
 cask "qq" do
-  version "6.7.0.20110"
-  sha256 "cdc3320f5e75f2a97ed990a7bd1e90acfbbc8465bc9768027fffb7bf92d024df"
+  version "6.7.0.20123"
+  sha256 "c7580fd1d0603052ab9b8d6ab1936a17d35d9425557d492b85947457d1ffcb65"
 
   url "https://dldir1.qq.com/qqfile/QQforMac/QQ_#{version}_Universal.dmg"
   appcast "https://im.qq.com/proxy/domain/qzonestyle.gtimg.cn/qzone/qzactStatics/configSystem/data/1373/config1.js"
