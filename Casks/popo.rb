685a89a88f5b373a83e2368c8810ba8838cc7947
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-20 09:24:10 -0800

    Update popo from 3.13.0 to 3.14.0 (#93210)

--- Casks/popo.rb
+++ Casks/popo.rb
@@ -1,6 +1,6 @@
 cask "popo" do
-  version "3.13.0"
-  sha256 "6bd22898cea4e6375396f54f9d3dded1d98b3a6ae84a21add56c32333543f81e"
+  version "3.14.0"
+  sha256 "3a3de7bb120247cc0a5f19bf0e2aea022b08efffd0db15524cf45f59dc59b99a"
 
   url "https://popo.netease.com/file/popomac/POPO_Mac_V#{version.dots_to_underscores}.dmg"
   appcast "http://http.popo.netease.com:8080/api/open/jsonp/check_version?device=4",
