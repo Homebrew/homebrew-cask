0f1d227be81adaec2307c3c2200951ca596dd6bf
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-21 11:29:06 -0800

    Update superproductivity from 6.0.1 to 6.1.1 (#93270)

--- Casks/superproductivity.rb
+++ Casks/superproductivity.rb
@@ -1,6 +1,6 @@
 cask "superproductivity" do
-  version "6.0.1"
-  sha256 "8cc7940296dcb119ee7bdbf98de46ea065b012ec8606540b30f767a09bea3c07"
+  version "6.1.1"
+  sha256 "7a1e116404254e464d43ef40553b40e1ee609d7cf85e9f57fee7ea28b68ddfe7"
 
   # github.com/johannesjo/super-productivity/ was verified as official when first introduced to the cask
   url "https://github.com/johannesjo/super-productivity/releases/download/v#{version}/superProductivity-#{version}-mac.zip"
