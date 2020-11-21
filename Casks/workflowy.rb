635b4e9add156bf0278de4f9a7c59c4096bef35f
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-21 12:42:59 -0800

    Update workflowy from 1.3.5-12695 to 1.3.5-12780 (#93191)
    * Update workflowy from 1.3.5-12695 to 1.3.5-12780
    
    * Update workflowy.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/workflowy.rb
+++ Casks/workflowy.rb
@@ -1,9 +1,9 @@
 cask "workflowy" do
-  version "1.3.5-12695"
-  sha256 "cd35f26bddf64921648b965e19415ddb1b51e65c5cc130a9da8df60d67d6a897"
+  version "1.3.5-12780"
+  sha256 "b4d98de4f2324673efc48c03d4890cade36203fea1b6e6ad8c70b40931dba90c"
 
   # github.com/workflowy/desktop/ was verified as official when first introduced to the cask
-  url "https://github.com/workflowy/desktop/releases/download/v#{version}/WorkFlowy.dmg"
+  url "https://github.com/workflowy/desktop/releases/download/v#{version}/WorkFlowy.zip"
   appcast "https://github.com/workflowy/desktop/releases.atom"
   name "WorkFlowy"
   desc "Notetaking tool"
