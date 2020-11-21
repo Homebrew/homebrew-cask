f33f6dcbf8a0fadf21a5f10e84e01ad5cad1cfd7
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-22 03:06:07 +0800

    Update workflowy.rb

--- Casks/workflowy.rb
+++ Casks/workflowy.rb
@@ -1,9 +1,9 @@
 cask "workflowy" do
   version "1.3.5-12780"
-  sha256 "3c60061fc981cab5dac08d7b21a2428321ad15d5769cbb0f496627389fa0a849"
+  sha256 "b4d98de4f2324673efc48c03d4890cade36203fea1b6e6ad8c70b40931dba90c"
 
   # github.com/workflowy/desktop/ was verified as official when first introduced to the cask
-  url "https://github.com/workflowy/desktop/releases/download/v#{version}/WorkFlowy.dmg"
+  url "https://github.com/workflowy/desktop/releases/download/v#{version}/WorkFlowy.zip"
   appcast "https://github.com/workflowy/desktop/releases.atom"
   name "WorkFlowy"
   desc "Notetaking tool"
