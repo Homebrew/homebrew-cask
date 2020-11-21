a4851e4fc0424dae145815bd2e5f090fac2e4dd2
Vítor Galvão <info@vitorgalvao.com>
2020-11-21 13:17:48 +0000

    Update pixelorama.rb

--- Casks/pixelorama.rb
+++ Casks/pixelorama.rb
@@ -1,9 +1,9 @@
 cask "pixelorama" do
-  version "v0.8.1"
+  version "0.8.1"
   sha256 "0982080bea8f0d80235ce98c9cb3f576847acef90622e766b05c5e9ab448bb9f"
 
   # github.com/Orama-Interactive/Pixelorama/ was verified as official when first introduced to the cask
-  url "https://github.com/Orama-Interactive/Pixelorama/releases/download/#{version}/Pixelorama.Mac.dmg"
+  url "https://github.com/Orama-Interactive/Pixelorama/releases/download/v#{version}/Pixelorama.Mac.dmg"
   appcast "https://github.com/Orama-Interactive/Pixelorama/releases.atom"
   name "Pixelorama"
   desc "2D sprite editor made with the Godot Engine"
