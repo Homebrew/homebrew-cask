414e6b13244bb956958876de456aa42b97ecf704
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-21 12:50:49 -0800

    Update trainerroad from 2020.47.109 to 2020.47.110 (#93311)

--- Casks/trainerroad.rb
+++ Casks/trainerroad.rb
@@ -1,6 +1,6 @@
 cask "trainerroad" do
-  version "2020.47.109"
-  sha256 "8375aff2b7c69de9c5f0ec68e47abe2a4fbc7072ac5b5c67924f20dbc1eb92ca"
+  version "2020.47.110"
+  sha256 "090d2a13208dc583b1371c3c9865cd47f17c2bf2bba38518b29d63c6bff334ba"
 
   # trainrdtrcmn01un1softw01.blob.core.windows.net/ was verified as official when first introduced to the cask
   url "https://trainrdtrcmn01un1softw01.blob.core.windows.net/installers/mac/v001/Production/TrainerRoad.dmg"
