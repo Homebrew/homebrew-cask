895f9423f504a47fdb9dac04f840bedceb88119e
Randall <ran.dall@icloud.com>
2020-11-21 10:07:46 -0800

    Update weakauras-companion from 3.0.6 to 3.1.0

--- Casks/weakauras-companion.rb
+++ Casks/weakauras-companion.rb
@@ -1,6 +1,6 @@
 cask "weakauras-companion" do
-  version "3.0.6"
-  sha256 "02219c21542109135846e2a573c4495eaede52e23e832d6c8ca1a65af9e32111"
+  version "3.1.0"
+  sha256 "69b1e5ce1716dfaee05a8f9c0fbc1521ab6aa42bab7db412bd46cf8af1d8a32b"
 
   # github.com/WeakAuras/WeakAuras-Companion was verified as official when first introduced to the cask
   url "https://github.com/WeakAuras/WeakAuras-Companion/releases/download/v#{version}/WeakAuras-Companion-#{version}.dmg"
