0d2c243b4c8eb5931aae248fe114f9a00e0c05c7
suschizu <brigitte@corecode.io>
2020-11-21 17:19:57 +0100

    Update doly.rb

--- Casks/doly.rb
+++ Casks/doly.rb
@@ -1,6 +1,6 @@
 cask "doly" do
   version "2.23.0"
-  sha256 "b2141a1ae3443d20472a275c938ad82d6e9b61ef7916a1112e6e08bdf770f860"
+  sha256 "f07b10ac9e19e9a169b24ecad0d02e4b143fe758684ed58bc8ec1ae15e695411"
 
   url "http://doly.jp/download/doly-mac_#{version}.dmg"
   appcast "http://doly.jp/update_mac.html"
