de9da4e17930c6dbe5e0293bf2709609b6929a25
Randall <ran.dall@icloud.com>
2020-11-21 10:29:07 -0800

    Update mater from 1.0.7 to 1.0.8

--- Casks/mater.rb
+++ Casks/mater.rb
@@ -1,6 +1,6 @@
 cask "mater" do
-  version "1.0.7"
-  sha256 "4c62287a7eb4c95a709076f049be0c7e65f3f8c9324f5a8321fe67b7361f8c4b"
+  version "1.0.8"
+  sha256 "21b7612fd3be0c14719b217e7c3a28d7c9a4dc6eddba96d25af6d51c58de564a"
 
   url "https://github.com/jasonlong/mater/releases/download/v#{version}/Mater-darwin-x64.zip"
   appcast "https://github.com/jasonlong/mater/releases.atom"
