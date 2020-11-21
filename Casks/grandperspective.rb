26c91c46cca28d5a229ddd98a41e401119d4cde0
Randall <ran.dall@icloud.com>
2020-11-21 11:18:26 -0800

    Update grandperspective from 2.5.3 to 2.5.4

--- Casks/grandperspective.rb
+++ Casks/grandperspective.rb
@@ -1,6 +1,6 @@
 cask "grandperspective" do
-  version "2.5.3"
-  sha256 "d8ff960efcf028c19c5f53e756c39f83fa6a05ed0064a505a82f2d00f9558548"
+  version "2.5.4"
+  sha256 "69a27cfb2f9bdf9afec22649b01a98cd2779298229029e3508b84bcc64cc46e7"
 
   # downloads.sourceforge.net/grandperspectiv/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/grandperspectiv/grandperspective/#{version}/GrandPerspective-#{version.dots_to_underscores}.dmg"
