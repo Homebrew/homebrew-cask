817b568b9eb3f953236056b40942cc0bd44459e1
Sebastian Riehm <hallo@herrriehm.de>
2020-11-22 12:33:33 +0100

    Update gobdokumente from 1.7.2 to 1.8

--- Casks/gobdokumente.rb
+++ Casks/gobdokumente.rb
@@ -4,8 +4,8 @@ cask "gobdokumente" do
     sha256 "2c788492fd66ffec93c70cbd0d1c10ceb03bcf2af1f8ccc5fbbbbb154e4999b1"
     url "https://download.moapp.software/GoBDokumente.zip"
   else
-    version "1.7.2"
-    sha256 "f01564c3115619eddedeb9a4045c895fc8bbaaeb8d320a459cd71c62da92edc4"
+    version "1.8"
+    sha256 "5542a5b16b39d1bd84aed5c995a99990959da3f2188ba7787b73292226d7cf20"
     url "https://download.moapp.software/GoBDokumente_BS.zip"
   end
 
