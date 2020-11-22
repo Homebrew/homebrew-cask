c0a1d236433ba3243b938f79ee17d4bd867d9149
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-22 11:37:48 -0800

    Update nagbar from 1.3.6 to 1.3.7 (#93344)

--- Casks/nagbar.rb
+++ Casks/nagbar.rb
@@ -1,6 +1,6 @@
 cask "nagbar" do
-  version "1.3.6"
-  sha256 "31efe0ff0428dc1d45e04c8e4954dc25fd7f4f7839bb1adc763b626f44efc17d"
+  version "1.3.7"
+  sha256 "9a4b256250d4527423efd16e90cc7d087bc6ca9306bdc5267a6441194e73a44b"
 
   # github.com/volendavidov/NagBar/ was verified as official when first introduced to the cask
   url "https://github.com/volendavidov/NagBar/releases/download/#{version}/NagBar.zip"
