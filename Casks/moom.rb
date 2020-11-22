bb938203b6fc728bf84deaffec1887968d5f8fb9
Sebastian Riehm <herrriehm@users.noreply.github.com>
2020-11-22 11:05:57 +0100

    Update moom from 3.2.18 to 3.2.19 (#93328)

--- Casks/moom.rb
+++ Casks/moom.rb
@@ -1,6 +1,6 @@
 cask "moom" do
-  version "3.2.18"
-  sha256 "1f6e31470c1cf7de6f13b4b9a16a5e716eedc249fc69f01a43dff5aeac3d5df4"
+  version "3.2.19"
+  sha256 "6aab4a0a186d119835f24f8899853b1b89c4b914f6a0379946706a113babcd14"
 
   url "https://manytricks.com/download/_do_not_hotlink_/moom#{version.no_dots}.dmg"
   appcast "https://manytricks.com/moom/appcast"
