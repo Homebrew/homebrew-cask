71f4c6bc9e89733540317854b3a9c323494a7928
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-22 11:46:49 -0800

    Update cookie from 6.2 to 6.3 (#93352)

--- Casks/cookie.rb
+++ Casks/cookie.rb
@@ -1,6 +1,6 @@
 cask "cookie" do
-  version "6.2"
-  sha256 "db196a6f8d93dab9f0109de3bb754fd619c99320b172001b7c81a50552ab29a3"
+  version "6.3"
+  sha256 "df9449ef44ddf4c49fff3c6d18ef91cc41228cd159f010e037e566f15b45fbf7"
 
   url "https://sweetpproductions.com/products/cookieapp/Cookie.dmg"
   appcast "https://sweetpproductions.com/products/cookieapp/appcast.xml"
