2aaf915710e08359fe700a76815e2378aef00148
Work <brigitte@corecode.io>
2020-11-22 17:39:47 +0100

    Update tower from 6.0,256:90c50e48 to 6.1,258:9c97138e

--- Casks/tower.rb
+++ Casks/tower.rb
@@ -1,6 +1,6 @@
 cask "tower" do
-  version "6.0,256:90c50e48"
-  sha256 "492da21c4f46e95b68bc768bda1fcd01ba00ea9ccd604680cb333b2d000346ba"
+  version "6.1,258:9c97138e"
+  sha256 "55e9e9ecab7f9315dc0b367ddb61e444ed88eb20f0f133de0d2c0b13e5b848ad"
 
   # fournova-app-updates.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://fournova-app-updates.s3.amazonaws.com/apps/tower3-mac/#{version.after_comma.before_colon}-#{version.after_colon}/Tower-#{version.before_comma}-#{version.after_comma.before_colon}.zip"
