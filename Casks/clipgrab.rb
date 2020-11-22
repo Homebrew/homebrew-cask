f1b90701e4925b5a67674b38d8f1133b297918cc
Randall <ran.dall@icloud.com>
2020-11-22 11:52:34 -0800

    Update clipgrab from 3.9.2 to 3.9.3

--- Casks/clipgrab.rb
+++ Casks/clipgrab.rb
@@ -1,6 +1,6 @@
 cask "clipgrab" do
-  version "3.9.2"
-  sha256 "98a70c8148be1a9e6d966f8e0864b83a67f0185e4fc78ee056e0f197d74ad317"
+  version "3.9.3"
+  sha256 "b09ca32b4aa1debef962cff759e6c440e83ddd554d2e00df4057e86d41420a5e"
 
   url "https://download.clipgrab.org/ClipGrab-#{version}.dmg"
   appcast "https://clipgrab.org/"
