0cf35542e52e602caaf3c0af4cf1db7e9a2434b5
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-24 05:57:11 +0800

    Update family-tree-builder.rb

--- Casks/family-tree-builder.rb
+++ Casks/family-tree-builder.rb
@@ -4,7 +4,7 @@ cask "family-tree-builder" do
 
   # mhcache-myheritage.netdna-ssl.com/FP/FamilyTreeBuilder/ was verified as official when first introduced to the cask
   url "https://mhcache-myheritage.netdna-ssl.com/FP/FamilyTreeBuilder/family_tree_builder_#{version.split(".").last}.dmg"
-  appcast "https://www.myheritage.com/page/family-tree-software"
+  appcast "https://www.myheritage.com/FP/FamilyTreeBuilder/appcast.xml"
   name "MyHeritage Family Tree Builder"
   desc "Family tree creator"
   homepage "https://www.myheritage.com/"
