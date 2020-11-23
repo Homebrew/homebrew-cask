c4073b37c7851dc3a216b14a90bbfcf5333e9703
Julian Mayer <julian@corecode.io>
2020-11-23 12:13:33 +0100

    Update sherlock from 1.9.0 to 1.10.0

--- Casks/sherlock.rb
+++ Casks/sherlock.rb
@@ -1,6 +1,6 @@
 cask "sherlock" do
-  version "1.9.0"
-  sha256 "56bc2f5477633aa8e8672fc8da1bd4ac9cac3960762f3b3f13a062aca6faebbc"
+  version "1.10.0"
+  sha256 "66b5cbf36969015bd5cc335969ff95f24e47da777e0a6633cc55e5c356aef30a"
 
   # dl.devmate.com/io.inspiredcode.Sherlock/ was verified as official when first introduced to the cask
   url "https://dl.devmate.com/io.inspiredcode.Sherlock/Sherlock.dmg"
