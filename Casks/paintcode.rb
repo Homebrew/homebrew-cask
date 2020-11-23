c6ccc76afaa33a316135a11ccfcea45d071a6152
Elf Sundae <elf.sundae@gmail.com>
2020-11-23 15:54:44 +0800

    Update paintcode (#93372)

--- Casks/paintcode.rb
+++ Casks/paintcode.rb
@@ -3,7 +3,7 @@ cask "paintcode" do
   sha256 "07f10e3f7680ab58ade062e338a8ffe71d6a1e7bc950ba1df3bebcdbb23335dc"
 
   # pixelcut.com/ was verified as official when first introduced to the cask
-  url "https://www.pixelcut.com/paintcode#{version.major}/paintcode.zip"
+  url "https://www.pixelcut.com/paintcode#{version.major}/paintcode_#{version}.zip"
   appcast "https://www.pixelcut.com/paintcode#{version.major}/appcast.xml"
   name "PaintCode"
   homepage "https://www.paintcodeapp.com/"
