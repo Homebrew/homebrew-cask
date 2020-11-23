3796fc77b212a2d3b9ecee6612791c763f33a2e2
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-23 09:36:57 -0800

    Update binance from 1.7.1 to 1.7.2 (#93423)

--- Casks/binance.rb
+++ Casks/binance.rb
@@ -1,6 +1,6 @@
 cask "binance" do
-  version "1.7.1"
-  sha256 "e2fb156088bbe40eecda16297d44cb3d0816b7be05f0585230c27805e2352fd6"
+  version "1.7.2"
+  sha256 "666221d94cfdf93bd9a122554a5dd4f4aaec062b6613d1d29c99832547d7f3da"
 
   url "https://ftp.binance.com/electron-desktop/mac/production/binance-#{version}.dmg"
   appcast "https://ftp.binance.com/electron-desktop/mac/production/latest-mac.yml"
