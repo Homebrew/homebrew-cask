ee56296a50819157d77f705d9a660b155f54e958
Julian Mayer <julian@corecode.io>
2020-11-23 12:13:17 +0100

    Update musictube from 1.14.2 to 1.14.3

--- Casks/musictube.rb
+++ Casks/musictube.rb
@@ -1,6 +1,6 @@
 cask "musictube" do
-  version "1.14.2"
-  sha256 "90e48a0af7f42f6f6cc125a12c2774b0b4bb49046530c2ce66f768466dfba7d1"
+  version "1.14.3"
+  sha256 "bb274d6b6de5cd0420cf37ae413118b1c7e488a331f23819e3ad4316f61a4c54"
 
   url "https://flavio.tordini.org/files/musictube/musictube.dmg"
   appcast "https://flavio.tordini.org/musictube-ws/appcast.xml"
