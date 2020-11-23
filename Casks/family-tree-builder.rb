d9be878d91b229a628549eb1983a5ad2ef17b191
Vítor Galvão <info@vitorgalvao.com>
2020-11-23 14:35:42 +0000

    Update family-tree-builder.rb

--- Casks/family-tree-builder.rb
+++ Casks/family-tree-builder.rb
@@ -5,7 +5,7 @@ cask "family-tree-builder" do
   # mhcache-myheritage.netdna-ssl.com/FP/FamilyTreeBuilder/ was verified as official when first introduced to the cask
   url "https://mhcache-myheritage.netdna-ssl.com/FP/FamilyTreeBuilder/family_tree_builder_#{version.split(".").last}.dmg"
   name "MyHeritage Family Tree Builder"
-  desc "MyHeritage Family Tree Builder is a free family tree product that allows you to easily create your family tree. Family Tree Builder by MyHeritage supports 36 languages and is absolutely free. It features Smart Matching (a fun way to discover new relatives) and provides excellent support for photos, including face recognition technology."
+  desc "Create a family tree"
   homepage "https://www.myheritage.com/"
 
   depends_on macos: "<= :catalina"
