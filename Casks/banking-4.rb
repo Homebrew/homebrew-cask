85abb8f7bc9c5da3fa786b94734b80d498536e9c
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-23 09:17:31 -0800

    Update banking-4 from 7.5.0 to 7.5.1 (#93415)

--- Casks/banking-4.rb
+++ Casks/banking-4.rb
@@ -1,7 +1,7 @@
 cask "banking-4" do
   # NOTE: "4" is not a version number, but an intrinsic part of the product name
-  version "7.5.0"
-  sha256 "c5f4b7f885c66fb168ecc19b9bc1306a0cfc6a45cc45fd85de35a862a62b1c66"
+  version "7.5.1"
+  sha256 "ca4884641d0225fa06196722e02907a9ac032ae34c86c78b02e27e4b2e0eeb07"
 
   url "https://subsembly.com/download/MacBanking4.pkg"
   appcast "https://subsembly.com/banking4-macos-updates.php"
