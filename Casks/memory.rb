348f1e1637514d93fb7ba2626013f80f8fd755d8
CoreCode <core-code@users.noreply.github.com>
2020-11-24 18:22:13 +0100

    Update memory from 2020.22 to 2020.23 (#93472)

--- Casks/memory.rb
+++ Casks/memory.rb
@@ -1,6 +1,6 @@
 cask "memory" do
-  version "2020.22"
-  sha256 "6e24de111c90b6d11a4463383cb56bdfa2c93312aea4202fd4e3c59617ec1b06"
+  version "2020.23"
+  sha256 "de328e69d0fdcd81286e600882e38920b657fe72b69db028fc910fb8c000470c"
 
   # memorymacapp.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://memorymacapp.s3.amazonaws.com/Memory.zip"
