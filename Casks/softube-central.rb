ab2d88c1e08583af5bf960af9829e6b95cc076ce
Alexey Loginov <alexey@openbridge.com>
2020-11-24 21:18:56 +0200

    style fix

--- Casks/softube-central.rb
+++ Casks/softube-central.rb
@@ -1,3 +1,4 @@
+# typed: false
 cask "softube-central" do
   version "1.4.2"
   sha256 "163e9d50110d126d28a98743468c1e9e2d772adf8d6f87db6c1fa68efb0b3db5"
@@ -8,7 +9,6 @@ cask "softube-central" do
   homepage "https://www.softube.com/softube-central/"
 
   auto_updates true
-
   depends_on cask: "ilok-license-manager"
 
   pkg "Softube Central-#{version}.pkg"
