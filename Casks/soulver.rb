c2c8ce773e9d9f4cc5cb256f57da8ba4fa5f563f
Kid <44045911+kidonng@users.noreply.github.com>
2020-11-24 21:31:19 +0800

    soulver: add new zap entries (#93451)

--- Casks/soulver.rb
+++ Casks/soulver.rb
@@ -17,5 +17,12 @@ cask "soulver" do
     "~/Library/Application Support/Soulver 3",
     "~/Library/Application Support/app.soulver.mac",
     "~/Library/Preferences/app.soulver.mac.plist",
+    "~/Library/Caches/app.soulver.mac",
+    "~/Library/Containers/app.soulver.mac.QuicklookInSpotlight",
+    "~/Library/Group Containers/group.app.soulver",
+    "~/Library/HTTPStorages/app.soulver.mac.binarycookies",
+    "~/Library/Application Scripts/app.soulver.mac.QuicklookInSpotlight",
+    "~/Library/Saved Application State/app.soulver.mac.savedState",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/app.soulver.mac.sfl2",
   ]
 end
