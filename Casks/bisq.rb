4420ccdd6a81740a50d8ae663606f89bc7bd5b2b
Randall <ran.dall@icloud.com>
2020-11-24 11:07:01 -0800

    Update bisq from 1.4.2 to 1.5.0

--- Casks/bisq.rb
+++ Casks/bisq.rb
@@ -1,6 +1,6 @@
 cask "bisq" do
-  version "1.4.2"
-  sha256 "c2656ae74e52aae5e3637e18a11be8e2739ed3d79c4dd74df64cbefee202a28b"
+  version "1.5.0"
+  sha256 "1e9a3cc54679e4556f2238527cd50a56dc18aa854d99e8f79a6eba660d10a3e2"
 
   # github.com/bisq-network/bisq/ was verified as official when first introduced to the cask
   url "https://github.com/bisq-network/bisq/releases/download/v#{version}/Bisq-#{version}.dmg"
