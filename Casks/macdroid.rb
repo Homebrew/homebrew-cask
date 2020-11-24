8eb0e8e551ca6702a287373ab94f9e8f9bf59c2d
tddschn <45612704+tddschn@users.noreply.github.com>
2020-11-24 12:27:47 +0800

    Add new cask macdroid

---
+++ Casks/macdroid.rb
@@ -0,0 +1,25 @@
+cask "macdroid" do
+  version "1.2"
+  sha256 "b969bceebd17ba762fa5072ad43c883e9fa0f08a2e19e1bde9f361a9cf7d4ee2"
+
+  # cdn.electronic.us/products/macdroid/mac/download/ was verified as official when first introduced to the cask
+  url "https://cdn.electronic.us/products/macdroid/mac/download/macdroid.dmg"
+  # appcast ""
+  name "MacDroid"
+  desc "Connect to your Android devices with ADB / MTP"
+  homepage "https://www.macdroid.app/"
+
+  app "MacDroid.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/us.electronic.macdroid.AFTFinderSync",
+    "~/Library/Application Support/us.electronic.macdroid",
+    "~/Library/Caches/us.electronic.macdroid",
+    "~/Library/Containers/us.electronic.macdroid.AFTFinderSync",
+    "~/Library/Group Containers/XS85JU6YZ3.us.electronic.macdroid",
+    "~/Library/HTTPStorages/us.electronic.macdroid.binarycookies",
+    "~/Library/Preferences/us.electronic.macdroid.plist",
+    "~/Library/Application Support/MacDroid",
+    "~/Library/Logs/MacDroid.log",
+  ]
+end
