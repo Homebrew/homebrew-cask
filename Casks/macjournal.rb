c5bcd2d9e226b2c6b3800b3efda6822142b97df3
Randall <ran.dall@icloud.com>
2020-11-24 11:38:17 -0800

    Update macjournal from 7.1 to 7.1.1

--- Casks/macjournal.rb
+++ Casks/macjournal.rb
@@ -1,6 +1,6 @@
 cask "macjournal" do
-  version "7.1"
-  sha256 "b1052326234fa3199a15120f4339eafeab2391223410f48fbfea445e5b506964"
+  version "7.1.1"
+  sha256 "dd0ac29e88d2c4f58ae7ce95dc6db2d06b694dfc9c47c7d5f52a33624999d5cb"
 
   url "https://danschimpf.com/macjournal/MacJournal_#{version}.zip"
   appcast "https://danschimpf.com/appcasts/MacJournal#{version.major}.appcast"
