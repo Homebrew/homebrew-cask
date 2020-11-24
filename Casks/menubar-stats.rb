b1ebb48b47d999a102e308ef980346e8e9657ef6
Julian Mayer <julian@corecode.io>
2020-11-24 11:34:15 +0100

    Update menubar-stats from 3.5 to 3.5.2

--- Casks/menubar-stats.rb
+++ Casks/menubar-stats.rb
@@ -1,6 +1,6 @@
 cask "menubar-stats" do
-  version "3.5"
-  sha256 "f04ddc2dea949ebbf940ad836b2c7b2d65397610c0625ac5a34dcb0745654766"
+  version "3.5.2"
+  sha256 "4c010d4c12c71e7b2a4e040adb9a9a20bb40f87e12533d1244620dd46148f472"
 
   url "https://seense.com/menubarstats/updateapp/mbs.zip"
   appcast "https://www.seense.com/menubarstats/updateapp/appcast.xml"
