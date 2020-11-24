91e235952acd276cd2bea91f27359cac6220637a
Randall <ran.dall@icloud.com>
2020-11-24 12:09:02 -0800

    Update xctu from 40003027_AB to 40003027_AC

--- Casks/xctu.rb
+++ Casks/xctu.rb
@@ -1,6 +1,6 @@
 cask "xctu" do
-  version "40003027_AB"
-  sha256 "d0b1af42d5f8a61d29a433f07065da0d39312e388ef117ef7c46ace763576d64"
+  version "40003027_AC"
+  sha256 "a8c0bf2fd8b62caa86c1ee35e7c4379d70eab04e7c868f7f6a6875a5e41ada93"
 
   url "https://ftp1.digi.com/support/utilities/#{version}.zip"
   appcast "https://www.digi.com/support/includes/utilities.aspx?pid=3352"
