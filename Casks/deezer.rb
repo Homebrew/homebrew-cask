96397ecc26a3769a8a7e88b54c78423cdd046448
Randall <ran.dall@icloud.com>
2020-11-24 11:58:05 -0800

    Update deezer from 4.27.0 to 4.28.0

--- Casks/deezer.rb
+++ Casks/deezer.rb
@@ -1,6 +1,6 @@
 cask "deezer" do
-  version "4.27.0"
-  sha256 "78b2a8cc58c0849cc5e9f82141de78724d2d29314563c512d64efc5d65bbfbaf"
+  version "4.28.0"
+  sha256 "9a891b5e21c0ab8331f0ab527fd8d7d023f9da149292faa88572c17e0fc15abf"
 
   url "https://www.deezer.com/desktop/download/artifact/darwin/x64/#{version}"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.deezer.com/desktop/download%3Fplatform%3Ddarwin%26architecture=x64"
