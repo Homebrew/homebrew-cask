c2f2f80586ae400665559c3a125fe36390e72783
CoreCode <core-code@users.noreply.github.com>
2020-11-25 18:53:29 +0100

    Update anydo from 4.2.113 to 4.2.115 (#93569)

--- Casks/anydo.rb
+++ Casks/anydo.rb
@@ -1,6 +1,6 @@
 cask "anydo" do
-  version "4.2.113"
-  sha256 "74b028520e7ae25e5157e81d85aceb82905313e45e6018cecc597d6171312174"
+  version "4.2.115"
+  sha256 "a3b755e9664be7a1b16f4f93cf468583c6f8b03ffcf320293aa9ef3d66a798f5"
 
   url "https://electron-app.any.do/Any.do.dmg"
   appcast "https://electron-app.any.do/latest-mac.yml"
