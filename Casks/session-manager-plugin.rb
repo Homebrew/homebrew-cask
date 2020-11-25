116806f38537d8dcc406c06df75a814ee38851d5
Jakub Warczarek <jakub.warczarek@gmail.com>
2020-11-25 23:28:44 +0100

    Update session-manager-plugin from 1.2.7.0 to 1.2.30.0 (#93562)
    * session-manager-plugin 1.2.30.0
    
    * Add `desc` stanza for session-manager-plugin

--- Casks/session-manager-plugin.rb
+++ Casks/session-manager-plugin.rb
@@ -1,11 +1,12 @@
 cask "session-manager-plugin" do
-  version "1.2.7.0"
-  sha256 "1cffa8a4d8d2e77cd1f5e531a4d507183f6b64dd73ee50dcebe9ce351f1f6bfb"
+  version "1.2.30.0"
+  sha256 "900d2c3b1044edf70cb024288d581b2cccf5cd4d5ed3061e529b549b67b0bc05"
 
   # session-manager-downloads.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://session-manager-downloads.s3.amazonaws.com/plugin/#{version}/mac/sessionmanager-bundle.zip"
   appcast "https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html"
   name "Session Manager Plugin for the AWS CLI"
+  desc "Plugin for AWS CLI to start and end sessions that connect to managed instances"
   homepage "https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html"
 
   binary "sessionmanager-bundle/bin/session-manager-plugin"
