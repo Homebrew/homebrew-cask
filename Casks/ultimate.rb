42d66942098cbfb9d2a8604cf1cdffd8569ae351
suschizu <brigitte@corecode.io>
2020-11-25 20:39:06 +0100

    Update ultimate (#93578)

--- Casks/ultimate.rb
+++ Casks/ultimate.rb
@@ -1,6 +1,6 @@
 cask "ultimate" do
   version "3.0.12.1028"
-  sha256 "25c1feac46134b47a2645e98e8e49102f88d9201b29fcec79f35ef8f57567fd8"
+  sha256 "d9bf275660788248871114cbdf831a187ef641601c94d6bac4d6207b7339ae63"
 
   url "https://download.epubor.com/epubor_ultimate.zip"
   appcast "https://www.epubor.com/ultimate.html"
