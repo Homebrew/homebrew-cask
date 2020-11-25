0f3212fc892e4fba13421b277ad6571f57613746
Ryan <fauxpark@gmail.com>
2020-11-26 02:41:52 +1100

    Update sunvox from 1.9.6 to 1.9.6b (#93563)

--- Casks/sunvox.rb
+++ Casks/sunvox.rb
@@ -1,6 +1,6 @@
 cask "sunvox" do
-  version "1.9.6"
-  sha256 "9983bc45aaecf0107c6b149ba4de87bb3ba87aff8492723ba8203b54cd06c8b2"
+  version "1.9.6b"
+  sha256 "a61a58f6bd093212028afa9df50fb9fb73414b475e4369f98d2407ceb9eada83"
 
   url "https://www.warmplace.ru/soft/sunvox/sunvox-#{version}.zip"
   appcast "https://www.warmplace.ru/soft/sunvox/changelog.txt"
