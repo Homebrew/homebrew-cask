42379d8db89239227ee72a88460a50e933d48619
Julian Mayer <julian@corecode.io>
2020-11-25 17:58:04 +0100

    Update dcommander from 3870 to 3881

--- Casks/dcommander.rb
+++ Casks/dcommander.rb
@@ -1,6 +1,6 @@
 cask "dcommander" do
-  version "3870"
-  sha256 "5614ed71d9a6b2a5a0dc744ae130cc12e62647d8ab9db0bafbe4ac16bd93ff69"
+  version "3881"
+  sha256 "fc6b5b1424d892c821aa20a32a082f0a6f8a1e233b66459de43d05ad9d9b124b"
 
   url "https://devstorm-apps.com/dc/download.php"
   appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://devstorm-apps.com/dc/download.php"
