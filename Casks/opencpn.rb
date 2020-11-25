20cdfc24ce743491a1ce203cbfa500db782cc274
Dewayne McNair <dmcnair@vyopta.wpengine.com>
2020-11-25 17:21:41 -0600

    Fixed download url for OpenCPN cask

--- Casks/opencpn.rb
+++ Casks/opencpn.rb
@@ -2,7 +2,8 @@ cask "opencpn" do
   version "5.2.4,1.6b314e6"
   sha256 "05152e347480519bc010bb334b27601520671ae32953d8e915131ed54da738ca"
 
-  url "http://download.opencpn.org/#{version.before_comma}/OpenCPN_#{version.before_comma}+#{version.after_comma}.pkg"
+  # download-opencpn.netdna-ssl.com was verified as official when first introduced to the cask
+  url "https://download-opencpn.netdna-ssl.com/#{version.before_comma}/OpenCPN_#{version.before_comma}+#{version.after_comma}.pkg"
   appcast "https://github.com/OpenCPN/OpenCPN/releases.atom"
   name "OpenCPN"
   desc "Full-featured and concise ChartPlotter/Navigator"
