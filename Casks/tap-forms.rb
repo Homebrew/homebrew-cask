782d3ae517be593ce4a5beb6918dae53e120f29d
CoreCode <core-code@users.noreply.github.com>
2020-11-26 00:58:11 +0100

    Update tap-forms from 5.3.16 to 5.3.17 (#93555)
    * Update tap-forms from 5.3.16 to 5.3.17
    
    * Update tap-forms.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/tap-forms.rb
+++ Casks/tap-forms.rb
@@ -1,10 +1,10 @@
 cask "tap-forms" do
-  version "5.3.16"
-  sha256 "bef816d93001c344267b34afdc60dad133b420a82f65872fc230001ad95543fc"
+  version "5.3.17"
+  sha256 "35d29be1a8066aca40b043ef83913f2779c4221ffff5fddbb96ca2b5b3c154bd"
 
   # vendors.paddle.com/download/product/503174 was verified as official when first introduced to the cask. Developer uses paddle.com to process licences and payments for the software
   url "https://vendors.paddle.com/download/product/503174"
-  appcast "https://www.tapforms.com/app/tf-mac-#{version.major}.1-sparkle-appcast.xml"
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://vendors.paddle.com/download/product/503174"
   name "Tap Forms 5"
   desc "Helps to organize important files in one place"
   homepage "https://www.tapforms.com/"
