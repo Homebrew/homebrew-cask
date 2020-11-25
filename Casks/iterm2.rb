eda6466d8a2e33c5da8c051e2947f17b3d783abb
yhu266 <56977205+yhu266@users.noreply.github.com>
2020-11-25 00:11:49 -0800

    Update iterm2 from 3.4.1 to 3.4.2

--- Casks/iterm2.rb
+++ Casks/iterm2.rb
@@ -4,8 +4,8 @@ cask "iterm2" do
     version "3.3.12"
     sha256 "6811b520699e8331b5d80b5da1e370e0ed467e68bc56906f08ecfa986e318167"
   else
-    version "3.4.1"
-    sha256 "3b817cc0985e250359b37167f63d26bbc97ffafa2926d0eb64e664e095852ae4"
+    version "3.4.2"
+    sha256 "0ac49f8f5df0c28f8a3400952d3baf6e40233b62a1cec23ad9266f0019484e99"
   end
 
   url "https://iterm2.com/downloads/stable/iTerm2-#{version.dots_to_underscores}.zip"
