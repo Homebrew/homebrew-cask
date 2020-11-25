d32849128e23b680f0b99986e2eeea08de822c3b
Alexander Zigelski <ali@crunchtime.dev>
2020-11-25 10:07:12 +0100

    Update kui from 9.1.0 to 9.2.0

--- Casks/kui.rb
+++ Casks/kui.rb
@@ -1,6 +1,6 @@
 cask "kui" do
-  version "9.1.0"
-  sha256 "87ea209addc20734fd1f142152ecb383a228eb7656d56d2b069b4e86d993f933"
+  version "9.2.0"
+  sha256 "24aa7afa3bebe9447a5f3beb5e2a472109d98de0dc60290bc34ef3b750b7c00e"
 
   # github.com/IBM/kui was verified as official when first introduced to the cask
   url "https://github.com/IBM/kui/releases/download/v#{version}/Kui-darwin-x64.tar.bz2"
