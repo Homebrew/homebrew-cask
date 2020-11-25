9f5ec0802b54c7827647f603c41c398b08742f3e
Randall <ran.dall@icloud.com>
2020-11-25 14:29:52 -0800

    Update screenflick from 2.7.54 to 2.7.55

--- Casks/screenflick.rb
+++ Casks/screenflick.rb
@@ -1,6 +1,6 @@
 cask "screenflick" do
-  version "2.7.54"
-  sha256 "ac600a4294b65ae26ca5ca32465adbfa6c5036608f633830f55abcfa2bf39752"
+  version "2.7.55"
+  sha256 "d046738727109f99eba8c4f164ae795dca18edd36f3a6d165ba402683c9cdd4c"
 
   url "https://store.araelium.com/screenflick/downloads/versions/Screenflick#{version}.zip"
   appcast "https://arweb-assets.s3.amazonaws.com/downloads/screenflick/updates.json"
