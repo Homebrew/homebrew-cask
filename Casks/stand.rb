7658590e27cea9f4a603217caf04df223ae2a840
EinJochel <31616408+EinJochel@users.noreply.github.com>
2020-11-26 00:59:18 +0100

    Update stand from 2.0 to 2.1 (#93561)
    * Update stand from 2.0 to 2.1
    
    - Removed appcast "https://standapp-sparkle-updater.herokuapp.com/", because it does not seem to contain the news version.
    - Added a `desc` stanza.
    - Added a `uninstall` stanza.
    - Added a `zap` stanza.
    
    * Update stand.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/stand.rb
+++ Casks/stand.rb
@@ -1,14 +1,24 @@
 cask "stand" do
-  version "2.0"
-  sha256 "eb0555f6376757105e1faea1fd1f9e4afae21eef73fdd5c7d0fb0c76a8cd9702"
+  version "2.1"
+  sha256 "e7cc1237f50d9f6991ced65e598e02015174b98e69e90298bc42e4759bc40e4e"
 
   # f001.backblazeb2.com/file/stand-app/ was verified as official when first introduced to the cask
   url "https://f001.backblazeb2.com/file/stand-app/#{version}/Stand.zip"
-  appcast "https://standapp-sparkle-updater.herokuapp.com/"
+  appcast "https://getstandapp.com/"
   name "Stand"
+  desc "Reminds you to stand up once an hour"
   homepage "https://getstandapp.com/"
 
   depends_on macos: ">= :catalina"
 
   app "Stand.app"
+
+  uninstall quit: [
+    "com.reddavis.Stand",
+  ]
+
+  zap trash: [
+    "~/Library/Preferences/com.reddavis.Stand.plist",
+    "~/Library/Caches/com.reddavis.Stand",
+  ]
 end
