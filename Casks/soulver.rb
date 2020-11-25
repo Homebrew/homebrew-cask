0ce66ee1b1d0b85f302ea004f5db5bf9e84611de
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-25 15:11:38 -0800

    Update soulver from 3.4.12-162 to 3.4.13-165 (#93604)

--- Casks/soulver.rb
+++ Casks/soulver.rb
@@ -1,6 +1,6 @@
 cask "soulver" do
-  version "3.4.12-162"
-  sha256 "ffca884751e5ef44ff8bbbdfa1c926fd0b4e80723774713e63e9bc010d72aa8c"
+  version "3.4.13-165"
+  sha256 "e7272829e493a5e0d13c2dce2aeb63ae0c5f4e04248cd2d83debe53488e9ecce"
 
   url "https://soulver.app/mac/sparkle/soulver-#{version}.zip"
   appcast "https://soulver.app/mac/sparkle/appcast.xml"
