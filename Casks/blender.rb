95648b20c40e7ae7662aaa8662c4b8afe84e85d0
Julian Mayer <julian@corecode.io>
2020-11-25 22:15:15 +0100

    Update blender from 2.90.1 to 2.91.0

--- Casks/blender.rb
+++ Casks/blender.rb
@@ -1,6 +1,6 @@
 cask "blender" do
-  version "2.90.1"
-  sha256 "a0f2c33714c44c0897e2047a62ac84ad00f27e1daca6f74cdc8ae0f829b19400"
+  version "2.91.0"
+  sha256 "d080a17f6b335dd25871edf1100839cddf76d270e72c5bb6cb1ddbd60b848d88"
 
   url "https://download.blender.org/release/Blender#{version.major_minor.delete("a-z")}/blender-#{version}-macOS.dmg"
   appcast "https://download.blender.org/release/",
