4b6cab4a5db2bad3ffa062bc74aa7b6735cea924
Randall <ran.dall@icloud.com>
2020-11-25 14:11:15 -0800

    Update chirp from 20201121 to 20201125

--- Casks/chirp.rb
+++ Casks/chirp.rb
@@ -1,6 +1,6 @@
 cask "chirp" do
-  version "20201121"
-  sha256 "f36af4311b95363c4435906fd9253e9406caf11bd8b55d97cfbad23ad938c704"
+  version "20201125"
+  sha256 "7609f0c6ca67cc83201c3ebdb4008876619d12958aae58e4ea1b7504dab60b8a"
 
   url "https://trac.chirp.danplanet.com/chirp_daily/LATEST/chirp-unified-daily-#{version}.app.zip"
   appcast "https://trac.chirp.danplanet.com/chirp_daily/LATEST/SHA1SUM"
