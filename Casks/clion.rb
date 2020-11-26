729a83143f189e60e33de4c8ee55f50a4825eb74
jcbot <jcb@leipert.io>
2020-11-26 11:08:57 +0000

    Update clion to 2020.2.5,202.8194.17

--- Casks/clion.rb
+++ Casks/clion.rb
@@ -1,6 +1,6 @@
 cask "clion" do
-  version "2020.2.4,202.7660.37"
-  sha256 "dd83c148f196a1504ec86d93e105fdf6d8ef72b0c856af5a91153dfe38667281"
+  version "2020.2.5,202.8194.17"
+  sha256 "1686f15433cc47e6392c76083e1fd53a9a14bdf25d2ac088643b844da213c1a1"
 
   url "https://download.jetbrains.com/cpp/CLion-#{version.before_comma}.dmg"
   appcast "https://data.services.jetbrains.com/products/releases?code=CL&latest=true&type=release"
