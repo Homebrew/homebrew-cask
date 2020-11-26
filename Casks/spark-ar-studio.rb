35e5784c5c749905bb8a8760b9091ce412b5a814
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-27 06:26:00 +0800

    Update spark-ar-studio.rb

--- Casks/spark-ar-studio.rb
+++ Casks/spark-ar-studio.rb
@@ -2,7 +2,6 @@ cask "spark-ar-studio" do
   version "102.0"
   sha256 "8c651a82adc2fc1e87cb79dbc01137e0af5de3f389c2afb4f5d5d471d642a762"
 
-  # facebook.com/sparkarmacos/ was verified as official when first introduced to the cask
   url "https://www.facebook.com/sparkarmacos/download/"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.facebook.com/sparkarmacos/download/",
           must_contain: version.major
