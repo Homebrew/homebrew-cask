407ef69196e66bf11e449e46a561073572935dd8
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-27 05:57:45 +0800

    Update sequel-ace.rb

--- Casks/sequel-ace.rb
+++ Casks/sequel-ace.rb
@@ -1,10 +1,11 @@
 cask "sequel-ace" do
-  version "2.3.2-2121"
+  version "2.3.2,2121"
   sha256 "c9a5577f84807933247957974bf27247e36c73e8faff5beb88550b3c8b81a733"
 
-  url "https://github.com/Sequel-Ace/Sequel-Ace/releases/download/production/#{version}/Sequel-Ace-#{version.sub(/-\d+/, "")}-release.zip"
+  url "https://github.com/Sequel-Ace/Sequel-Ace/releases/download/production/#{version.before_comma}-#{version.after_comma}/Sequel-Ace-#{version.before_comma}-release.zip"
   appcast "https://github.com/Sequel-Ace/Sequel-Ace/releases.atom"
   name "Sequel Ace"
+  desc "MySQL/MariaDB database management"
   homepage "https://github.com/Sequel-Ace/Sequel-Ace"
 
   app "Sequel Ace.app"
