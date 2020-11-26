79761528b7bd4b8f4a1d4ee9cf07f26e65fe3ffe
Mario Fehr <mario.fehr@massiveart.com>
2020-11-26 21:21:49 +0100

    Update sequel-ace from 2.3.1 to 2.3.2-2121

--- Casks/sequel-ace.rb
+++ Casks/sequel-ace.rb
@@ -1,8 +1,8 @@
 cask "sequel-ace" do
-  version "2.3.1"
-  sha256 "79b19bf6d5d782c6534368fc45b36097ea767e50e27238ff3ea30a4775cf148e"
+  version "2.3.2-2121"
+  sha256 "c9a5577f84807933247957974bf27247e36c73e8faff5beb88550b3c8b81a733"
 
-  url "https://github.com/Sequel-Ace/Sequel-Ace/releases/download/#{version}/Sequel-Ace-#{version}-release.zip"
+  url "https://github.com/Sequel-Ace/Sequel-Ace/releases/download/production/#{version}/Sequel-Ace-#{version.sub(/-\d+/, "")}-release.zip"
   appcast "https://github.com/Sequel-Ace/Sequel-Ace/releases.atom"
   name "Sequel Ace"
   homepage "https://github.com/Sequel-Ace/Sequel-Ace"
