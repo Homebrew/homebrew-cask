a6da8316f1d4f9bda25212145022d6c6e9a9923d
suschizu <brigitte@corecode.io>
2020-11-26 15:07:41 +0100

    Update logos from 8.17.0.0014 to 9.0.0.0181 (#93649)

--- Casks/logos.rb
+++ Casks/logos.rb
@@ -1,14 +1,14 @@
 cask "logos" do
-  version "8.17.0.0014"
-  sha256 "4a720ac51ad6be85c60fd8e36e030524f75e7d41ce7a047ef665de970b90cd3c"
+  version "9.0.0.0181"
+  sha256 "70b4f968ab3b9148492457b0f8926afc2b2345153aca35c9ba9ceb5e6bdb574d"
 
   # downloads.logoscdn.com/ was verified as official when first introduced to the cask
-  url "https://downloads.logoscdn.com/LBS8/Installer/#{version}/LogosMac.dmg"
+  url "https://downloads.logoscdn.com/LBS#{version.major}/Installer/#{version}/LogosMac.dmg"
   appcast "https://clientservices.logos.com/update/v1/feed/logos#{version.major}-mac/stable.xml"
   name "Logos Bible Software"
   homepage "https://www.logos.com/"
 
-  depends_on macos: ">= :el_capitan"
+  depends_on macos: ">= :mojave"
 
   app "Logos.app"
 
