283fa53ae3771148672c0942e57737c252536bfb
CoreCode <core-code@users.noreply.github.com>
2020-11-26 23:36:10 +0100

    Update transcribe.rb (#93721)
    * Update transcribe.rb
    
    * Update transcribe.rb
    
    * Update transcribe.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/transcribe.rb
+++ Casks/transcribe.rb
@@ -1,15 +1,19 @@
 cask "transcribe" do
-  version "8.80.0"
-  sha256 "2c32b81ebe15115550c74f6d758facac05acbd20d1da16237e1fe3fc60b04215"
+  if MacOS.version <= :catalina
+    version "8.75.2"
+    sha256 "f01781100cd3b9987c8f8892145a2eaa358df07b92e10e26f30b6a877f5b352c"
+    url "https://www.seventhstring.com/xscribe/downmac_old/transcribe#{version.no_dots}.dmg"
+  else
+    version "8.80.0"
+    sha256 "2c32b81ebe15115550c74f6d758facac05acbd20d1da16237e1fe3fc60b04215"
+    url "https://www.seventhstring.com/xscribe/transcribe.dmg"
+  end
 
-  url "https://www.seventhstring.com/xscribe/transcribe.dmg"
   appcast "https://www.seventhstring.com/xscribe/history.html",
           must_contain: version.major_minor
   name "Transcribe!"
   desc "Transcribes recorded music"
   homepage "https://www.seventhstring.com/xscribe/overview.html"
-
-  depends_on macos: ">= :yosemite"
 
   app "Transcribe!.app"
 
