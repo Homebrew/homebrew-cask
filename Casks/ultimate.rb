c2dc89326bfad927fb333e23f9a7c2bcfd2f53e6
Brigitte <brigitte@corecode.io>
2020-11-26 16:35:56 +0100

    Update ultimate from 3.0.12.1028 to 3.0.12.1125

--- Casks/ultimate.rb
+++ Casks/ultimate.rb
@@ -1,6 +1,6 @@
 cask "ultimate" do
-  version "3.0.12.1028"
-  sha256 "d9bf275660788248871114cbdf831a187ef641601c94d6bac4d6207b7339ae63"
+  version "3.0.12.1125"
+  sha256 "029fd0839ce261877b7291c0d44ad6e1b97654ef75f968813dd5387f560a3500"
 
   url "https://download.epubor.com/epubor_ultimate.zip"
   appcast "https://www.epubor.com/ultimate.html"
