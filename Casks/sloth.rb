8a7ebe945c95d045925c1865a4ed60f12d2eefca
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-26 10:19:01 -0800

    Update sloth from 3.0 to 3.0.1 (#93688)

--- Casks/sloth.rb
+++ Casks/sloth.rb
@@ -1,6 +1,6 @@
 cask "sloth" do
-  version "3.0"
-  sha256 "8d4871176a0013f425411c152a76552a91d7b95ce637737fda5c54a103ee5314"
+  version "3.0.1"
+  sha256 "67ff13b1532381853eefaf1043fb58d2d4885320112e6fa6edd17f4d06caebdc"
 
   url "https://sveinbjorn.org/files/software/sloth/sloth-#{version}.zip"
   appcast "https://sveinbjorn.org/files/appcasts/SlothAppcast.xml"
