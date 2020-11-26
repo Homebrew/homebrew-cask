3104306811eabfb76e35d27c55c2821f3a091267
suschizu <brigitte@corecode.io>
2020-11-26 17:22:48 +0100

    Update tortoisehg from 5.5.2 to 5.6 (#93660)

--- Casks/tortoisehg.rb
+++ Casks/tortoisehg.rb
@@ -1,6 +1,6 @@
 cask "tortoisehg" do
-  version "5.5.2"
-  sha256 "f4824ad3365524e3a5accc9ba744bad9744b9fef357755585c2934ee1a118d34"
+  version "5.6"
+  sha256 "c6dbfe7d1f3ad9260113940d8529ee19924201f9dbb715fa1aa484e4b8946a7e"
 
   # mercurial-scm.org/release/tortoisehg/ was verified as official when first introduced to the cask
   url "https://www.mercurial-scm.org/release/tortoisehg/macos/TortoiseHg-#{version}-mac-x64-qt5.dmg"
