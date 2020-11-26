ebf392a723f898481d6bd63c7a2eda8c83945ef4
jcbot <jcb@leipert.io>
2020-11-26 21:01:42 +0100

    Update appcode to 2020.2.8,202.8194.20 (#93720)

--- Casks/appcode.rb
+++ Casks/appcode.rb
@@ -1,6 +1,6 @@
 cask "appcode" do
-  version "2020.2.7,202.7660.63"
-  sha256 "c7e6e3770831f07dd81d45fd6031dbdb354d2604480f4a13c40c44479774dd74"
+  version "2020.2.8,202.8194.20"
+  sha256 "e9d8a42ef64cd7d57e2859888e97f1f756738e4a2aa91c8ebfc28df4b8343e46"
 
   url "https://download.jetbrains.com/objc/AppCode-#{version.before_comma}.dmg"
   appcast "https://data.services.jetbrains.com/products/releases?code=AC&latest=true&type=release"
