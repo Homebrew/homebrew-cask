9c48113236e374986377b1e76e144c7519e64d6d
Daniel Neto <jdaniel.npereira@gmail.com>
2020-11-26 10:24:40 +0000

    Update joplin from 1.3.18 to 1.4.12

--- Casks/joplin.rb
+++ Casks/joplin.rb
@@ -1,6 +1,6 @@
 cask "joplin" do
-  version "1.3.18"
-  sha256 "4732a2524429c7739decae7d48a80294637d4951d013f5516b3468473bdca97e"
+  version "1.4.12"
+  sha256 "7048f14d3b30d3ed1bb0bbae4c11796403f37db6b6901f2993499ea516a63986"
 
   # github.com/laurent22/joplin/ was verified as official when first introduced to the cask
   url "https://github.com/laurent22/joplin/releases/download/v#{version}/Joplin-#{version}.dmg"
