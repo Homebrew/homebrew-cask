48129f72685a03cce034725d1f2d73a4b28736ee
Randall <ran.dall@icloud.com>
2020-11-26 09:32:17 -0800

    Update dcp-o-matic-player from 2.14.41 to 2.14.45

--- Casks/dcp-o-matic-player.rb
+++ Casks/dcp-o-matic-player.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-player" do
-  version "2.14.41"
-  sha256 "50d7687cf5a7450c87d1e10353774d199c7941a77d8f412077c331e50d751571"
+  version "2.14.45"
+  sha256 "e8ed37ee7ac4a2beb3a29a0b2037f99c35efcf60255eb8aff850ae4fa14e1456"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.9-player&version=#{version}"
   appcast "https://dcpomatic.com/download"
