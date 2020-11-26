ad0bd095dcac939a4e5378abd241da99b4e2d2cc
Randall <ran.dall@icloud.com>
2020-11-26 09:21:23 -0800

    Update family-tree-builder from 8.2.0.8521 to 8.2.0.8516

--- Casks/family-tree-builder.rb
+++ Casks/family-tree-builder.rb
@@ -1,6 +1,6 @@
 cask "family-tree-builder" do
-  version "8.2.0.8521"
-  sha256 "37d553af39747800a9f8b2bfd745ffae2ab521a5894364e377a8be496f36063e"
+  version "8.2.0.8516"
+  sha256 "babd0029c007932e0339a97e080608a06ffbc64f77c49879a2b8fd0a93f956c5"
 
   # mhcache-myheritage.netdna-ssl.com/FP/FamilyTreeBuilder/ was verified as official when first introduced to the cask
   url "https://mhcache-myheritage.netdna-ssl.com/FP/FamilyTreeBuilder/family_tree_builder_#{version.split(".").last}.dmg"
