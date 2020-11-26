63c92cbe64f7f5912c16a19aa677f5938806d38d
Oli Walker <oliwalker@btinternet.com>
2020-11-26 20:10:35 +0000

    Add Spark AR Studio.app v102.0

---
+++ Casks/spark-ar-studio.rb
@@ -0,0 +1,12 @@
+cask "spark-ar-studio" do
+  version "102.0"
+  sha256 "8c651a82adc2fc1e87cb79dbc01137e0af5de3f389c2afb4f5d5d471d642a762"
+
+  # scontent-lht6-1.xx.fbcdn.net was verified as official when first introduced to the cask
+  url "https://scontent-lht6-1.xx.fbcdn.net/v/t39.10537-6/10000000_285019562885279_3289071357230442714_n.dmg/SparkARStudio_v102.dmg?_nc_cat=100&ccb=2&_nc_sid=379304&_nc_ohc=7XB746pxn2gAX9dC3Vz&_nc_ad=z-m&_nc_cid=0&_nc_zor=4&_nc_ht=scontent-lht6-1.xx&oh=23208c2e1853e5a94e39785b133c5b7d&oe=5FE6014F"
+  name "Spark AR Studio"
+  desc "Create and share augmented reality experiences using the Facebook family of apps"
+  homepage "https://sparkar.facebook.com/ar-studio/"
+
+  app "Spark AR Studio.app"
+end
