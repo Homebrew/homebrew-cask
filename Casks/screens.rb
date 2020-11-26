ce853be566f47eb59378f663ee417d5e2b4924d3
Reno <25192197+singularitti@users.noreply.github.com>
2020-11-26 16:48:13 -0500

    Update screens from 4.8.1,1606319020 to 4.8.1,1606397907 (#93672)

--- Casks/screens.rb
+++ Casks/screens.rb
@@ -1,8 +1,8 @@
 cask "screens" do
-  version "4.8.1,1606319020"
-  sha256 "864af52ee650def80ec87149933f7e525913838df7d69819eb307a635587c668"
+  version "4.8.1,1606397907"
+  sha256 "75cb3e304fad852d31950773e0a03ecc3df81c19c7a2a43a6fb85a20034298e0"
 
-  url "https://updates.edovia.com/com.edovia.screens#{version.major}.mac/Screens_#{version.before_comma}b#{version.after_comma}.zip"
+  url "https://updates.edovia.com/com.edovia.screens#{version.major}.mac/Screens_#{version.before_comma}b%20#{version.after_comma}.zip"
   appcast "https://updates.edovia.com/com.edovia.screens#{version.major}.mac/appcast.xml"
   name "Screens"
   desc "Remote access software focusing on usability"
