53c2b712d25b11d18a98ad606b55ffc190d490de
suschizu <brigitte@corecode.io>
2020-11-26 13:05:49 +0100

    Update feishu from 3.36.2,51d33b to 3.37.1,358a23 (#93648)

--- Casks/feishu.rb
+++ Casks/feishu.rb
@@ -1,6 +1,6 @@
 cask "feishu" do
-  version "3.36.2,51d33b"
-  sha256 "18b61f10a07b73262ba8ab0b4ae926ec26ffddc5d9c79f85733c61048c99f9e6"
+  version "3.37.1,358a23"
+  sha256 "f4085562d928ee1f8c0fc976727640663d7603e6ccdc4a0761bac47bfb2cf2b5"
 
   # sf3-ttcdn-tos.pstatp.com/ was verified as official when first introduced to the cask
   url "https://sf3-ttcdn-tos.pstatp.com/obj/ee-appcenter/#{version.after_comma}/Feishu-#{version.before_comma}.dmg"
