f8c863bbff5f7c6cca6ac12641e14efb46315d40
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2020-11-25 19:34:28 -0800

    Create qit.rb

---
+++ Casks/qit.rb
@@ -0,0 +1,10 @@
+cask "qit" do
+  version :latest
+  sha256 :no_check
+
+  url "http://cabeen.io/download/qit-latest-mac.zip"
+  name "qit"
+  desc "visualization, exploration, and analysis of neuroimaging datasets"
+  homepage "http://cabeen.io/qitwiki"
+  
+end
