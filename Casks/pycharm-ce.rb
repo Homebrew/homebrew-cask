fed51c993c704064635924fba4747f5d4e9f29a9
jcbot <jcb@leipert.io>
2020-11-26 22:07:36 +0000

    Update pycharm-ce to 2020.2.4,202.8194.15

--- Casks/pycharm-ce.rb
+++ Casks/pycharm-ce.rb
@@ -1,6 +1,6 @@
 cask "pycharm-ce" do
-  version "2020.2.3,202.7660.27"
-  sha256 "021fcdd4564cbb65c8e53b26feb2652bb09b14d7ca36f97539be124dd1dffdc4"
+  version "2020.2.4,202.8194.15"
+  sha256 "661631b71ee0874ef56015b93cc11ce41cc02646f015e8316880736b450f1e79"
 
   url "https://download.jetbrains.com/python/pycharm-community-#{version.before_comma}.dmg"
   appcast "https://data.services.jetbrains.com/products/releases?code=PCC&latest=true&type=release"
