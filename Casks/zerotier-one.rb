87589e1620b12065f50f2e86a4b64b2aaed12be9
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-26 09:39:53 -0800

    Update zerotier-one from 1.6.0 to 1.6.1 (#93674)

--- Casks/zerotier-one.rb
+++ Casks/zerotier-one.rb
@@ -1,6 +1,6 @@
 cask "zerotier-one" do
-  version "1.6.0"
-  sha256 "8dd5377f3105f4a443b71ace3421c88c5b448227b4e69eff5d910d8da3ebc6f5"
+  version "1.6.1"
+  sha256 "9b87cf8f502bead3df6a9702ad94b29c13329405b6963328f8f05c3749cfe07a"
 
   url "https://download.zerotier.com/dist/ZeroTier%20One.pkg"
   appcast "https://github.com/zerotier/ZeroTierOne/releases.atom"
