8590a49be8b980303e79f68e3caff73aa6d32f94
CoreCode <core-code@users.noreply.github.com>
2020-11-26 01:02:42 +0100

    Update ocenaudio from 3.9.5 to 3.9.6 (#93632)

--- Casks/ocenaudio.rb
+++ Casks/ocenaudio.rb
@@ -1,12 +1,12 @@
 cask "ocenaudio" do
-  version "3.9.5"
+  version "3.9.6"
 
   if MacOS.version <= :high_sierra
     sha256 "a286a08532a18843bbb7c3e9dd1f087ddc4c7f8716eafb1f83d890bb32a556e7"
 
     url "https://www.ocenaudio.com/downloads/index.php/ocenaudio_sierra.dmg"
   else
-    sha256 "a5402b621ddbd0415a9303f1b80dc90ddf72dc18a26dc80ccc187b465aa9e828"
+    sha256 "f7d28332d637dc9263258073925a112ab25d6884bf74ea93bc977525552ace25"
 
     url "https://www.ocenaudio.com/downloads/index.php/ocenaudio_mojave.dmg"
   end
