cbc310a9f4d21f068b409777467cd99c0d472695
Kirk Klobe <kklobe@gmail.com>
2020-11-26 13:58:15 -0600

    Update segger-embedded-studio-for-arm from 5.20a to 5.30 (#93719)

--- Casks/segger-embedded-studio-for-arm.rb
+++ Casks/segger-embedded-studio-for-arm.rb
@@ -1,6 +1,6 @@
 cask "segger-embedded-studio-for-arm" do
-  version "5.20a"
-  sha256 "9660285f2cecd71ac24b0083e00709ec4206d98e55bb8d19dc91dce25ba78b64"
+  version "5.30"
+  sha256 "baeb17aa6ab5d60610986ff6c5e57d5e55256242d5e01998222784d0fc62c47d"
 
   url "https://www.segger.com/downloads/embedded-studio/Setup_EmbeddedStudio_ARM_v#{version.no_dots}_macos_x64.dmg"
   appcast "https://studio.segger.com/arm_segger_studio_release_notes.htm"
