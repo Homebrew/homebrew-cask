0cde80e1edb1a6a649b6ddd253abd7799fe75325
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-26 11:38:58 -0800

    Update zoc from 8.01.2 to 8.01.3 (#93715)

--- Casks/zoc.rb
+++ Casks/zoc.rb
@@ -1,6 +1,6 @@
 cask "zoc" do
-  version "8.01.2"
-  sha256 "a70a837f1815e1073e2278a946e2d564826fc9ae9798c33cc47d6ef8c0238044"
+  version "8.01.3"
+  sha256 "3a565f9535d09a38d8d012d58e5d7bd448017b412eb489737fc0db543f9d580e"
 
   url "https://www.emtec.com/downloads/zoc/zoc#{version.no_dots}.dmg"
   appcast "https://www.emtec.com/downloads/zoc/zoc_changes.txt"
