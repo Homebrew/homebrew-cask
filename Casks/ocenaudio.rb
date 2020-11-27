a8421b24a3c2555b26d89c4007c2e13a52b1e75a
Julian Mayer <julian@corecode.io>
2020-11-28 00:01:11 +0100

    Update ocenaudio from 3.9.6 to 3.9.7

--- Casks/ocenaudio.rb
+++ Casks/ocenaudio.rb
@@ -1,12 +1,12 @@
 cask "ocenaudio" do
-  version "3.9.6"
+  version "3.9.7"
 
   if MacOS.version <= :high_sierra
     sha256 "a286a08532a18843bbb7c3e9dd1f087ddc4c7f8716eafb1f83d890bb32a556e7"
 
     url "https://www.ocenaudio.com/downloads/index.php/ocenaudio_sierra.dmg"
   else
-    sha256 "f7d28332d637dc9263258073925a112ab25d6884bf74ea93bc977525552ace25"
+    sha256 "c68de91339b15e07415750f028bd79b5a4d6dd4db6d334a90595ed9e22efe86c"
 
     url "https://www.ocenaudio.com/downloads/index.php/ocenaudio_mojave.dmg"
   end
