c3a780b05f8f9319fc0f6ad925a43d8615e7287c
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-27 08:38:11 -0800

    Update brook from 20200909 to 20210101 (#93753)

--- Casks/brook.rb
+++ Casks/brook.rb
@@ -1,6 +1,6 @@
 cask "brook" do
-  version "20200909"
-  sha256 "7762ad30c86503974542e739e94e252f0721869a5ca00927df00b3338870577b"
+  version "20210101"
+  sha256 "4bcf42d6fc8777e96890e2ea0a181170da076cece59816cb3844d92320c6d781"
 
   url "https://github.com/txthinking/brook/releases/download/v#{version}/Brook.dmg"
   appcast "https://github.com/txthinking/brook/releases.atom"
