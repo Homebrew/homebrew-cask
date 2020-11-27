9dbdf9b9dd7c3fa8134a634ce47f425ed77f36dd
CoreCode <core-code@users.noreply.github.com>
2020-11-27 14:56:31 +0100

    Update naver-whale.rb

--- Casks/naver-whale.rb
+++ Casks/naver-whale.rb
@@ -1,5 +1,5 @@
 cask "naver-whale" do
-  version "2.8.109.22"
+  version "2.8.108.15"
   sha256 "2e3b26ba030b871f0078fe44521b338edbae31f28d9ad64d4398d2a89f653d90"
 
   # update.whale.naver.net/downloads/installers/ was verified as official when first introduced to the cask
