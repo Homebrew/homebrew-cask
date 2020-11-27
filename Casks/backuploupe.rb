c86938545c72772371060776c2c85ab9e6bc388b
Michael Ramirez <ramirez3494@gmail.com>
2020-11-27 18:40:01 -0500

    Update backuploupe SHA256

--- Casks/backuploupe.rb
+++ Casks/backuploupe.rb
@@ -1,6 +1,6 @@
 cask "backuploupe" do
   version "3.2.1"
-  sha256 "edce74918f82215e05cadc408d13aa1240e429bbd0d2af795c9d65ebc49e6bcc"
+  sha256 "c52a46ed8242f25bbb632921ec9fc956d30d8b9f9b2a2b85a405f5b332aeb9f6"
 
   url "https://www.soma-zone.com/download/files/BackupLoupe-#{version}.tar.bz2"
   appcast "https://www.soma-zone.com/BackupLoupe/a/appcast-update-#{version.major}.xml"
