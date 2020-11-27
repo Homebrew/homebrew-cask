9b52efae0b3814465c69c5dbd06d791849f45073
Randall <ran.dall@icloud.com>
2020-11-27 08:22:34 -0800

    Update electerm from 1.9.24 to 1.9.27

--- Casks/electerm.rb
+++ Casks/electerm.rb
@@ -1,6 +1,6 @@
 cask "electerm" do
-  version "1.9.24"
-  sha256 "9e538c4618dc98195fd0cb609686c8fe71af8cde564e4946f18ce001ad5150a9"
+  version "1.9.27"
+  sha256 "ddb84474c7aee98226e27df1c8e70b33e097f5d5c093c52720116974ebfabc5b"
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac.dmg"
   appcast "https://github.com/electerm/electerm/releases.atom"
