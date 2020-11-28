a6cd31f9d46a05eb5c404af2f0943f8208a12ec2
Brigitte <brigitte@corecode.io>
2020-11-28 18:00:16 +0100

    Update unity-android-support-for-editor from 2020.1.14f1,d81f64f5201d to 2020.1.15f1,97d0ae02d19d

--- Casks/unity-android-support-for-editor.rb
+++ Casks/unity-android-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask "unity-android-support-for-editor" do
-  version "2020.1.14f1,d81f64f5201d"
-  sha256 "bf74cf7341b594dec1a2ca492cb95c3d5cab3f1126b23f82a81d32866ce4b4d8"
+  version "2020.1.15f1,97d0ae02d19d"
+  sha256 "2e098766e598eeb60bae696201785afa5f42fe6b67aa7b89e97760a6a322d2b6"
 
   # download.unity3d.com/download_unity/ was verified as official when first introduced to the cask
   url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-#{version.before_comma}.pkg"
