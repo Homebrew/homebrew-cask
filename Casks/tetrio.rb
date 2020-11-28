a9b91b1eee3486a55acd622a67d5062c4d68af49
Justyn Boyer <justynboyer@gmail.com>
2020-11-28 17:13:58 +0000

    add zap

--- Casks/tetrio.rb
+++ Casks/tetrio.rb
@@ -6,6 +6,11 @@ cask "tetrio" do
   name "TETR.IO"
   desc "Free-to-play Tetris clone"
   homepage "https://tetr.io/about"
-
+  zap trash: [
+    "~/Library/Application Support/tetrio-desktop",
+    "~/Library/Preferences/sh.osk.tetrio-client.plist",
+    "~/Library/Saved Application State/sh.osk.tetrio-client.savedState",
+    "~/Library/WebKit/sh.osk.tetrio-client"
+  ]
   app "TETR.IO.app"
 end
