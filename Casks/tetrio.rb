9e336cb28c4d7aa0558d9ebb8e5786bf0c00cb6f
Jabster28 <29015942+Jabster28@users.noreply.github.com>
2020-11-28 20:19:53 +0000

    Add TETR.IO Latest (#93807)
    * Add TETR.IO Latest
    
    * add zap
    
    * run style fix

---
+++ Casks/tetrio.rb
@@ -0,0 +1,18 @@
+cask "tetrio" do
+  version :latest
+  sha256 :no_check
+
+  url "https://tetr.io/about/desktop/builds/TETR.IO%20Setup.dmg"
+  name "TETR.IO"
+  desc "Free-to-play Tetris clone"
+  homepage "https://tetr.io/about"
+
+  app "TETR.IO.app"
+
+  zap trash: [
+    "~/Library/Application Support/tetrio-desktop",
+    "~/Library/Preferences/sh.osk.tetrio-client.plist",
+    "~/Library/Saved Application State/sh.osk.tetrio-client.savedState",
+    "~/Library/WebKit/sh.osk.tetrio-client",
+  ]
+end
