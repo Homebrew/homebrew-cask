04b930323fa47d068027fae994abe37cc572b567
Inder Dhir <inderdhir@hotmail.com>
2020-11-28 17:24:42 -0500

    Update datweatherdoe from 1.3.0 to 1.3.1

--- Casks/datweatherdoe.rb
+++ Casks/datweatherdoe.rb
@@ -1,6 +1,6 @@
 cask "datweatherdoe" do
-  version "1.3.0"
-  sha256 "a076722eaa3bf55a0a08811e326e5e1160a57dc3d9ef5b6f0760e8f146b1b274"
+  version "1.3.1"
+  sha256 "8c7381955d8d6e92d190b2c57c82336f5fd2db07e33d8b7f8b5d77dd8508a2d9"
 
   url "https://github.com/inderdhir/DatWeatherDoe/releases/download/#{version}/DatWeatherDoe-#{version}.dmg"
   appcast "https://github.com/inderdhir/DatWeatherDoe/releases.atom"
