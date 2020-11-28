3097b3fba69f6b4ab35599ff3fb296621b926cd4
suschizu <brigitte@corecode.io>
2020-11-28 17:54:45 +0100

    Update and rename ezip.rb to maczip.rb

--- Casks/maczip.rb
+++ Casks/maczip.rb
@@ -1,14 +1,14 @@
-cask "ezip" do
+cask "maczip" do
   version "2.0.1"
   sha256 "acd1f3f61a93e227166c15e767fe5ae488c62786bd278ffd11fd251a79e14dad"
 
   url "https://cdn.awehunt.com/ezip/release/MacZip_V#{version}.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://ezip.awehunt.com/download?s=web"
-  name "ezip"
+  name "MacZip"
   desc "Utility to open, create and modify archive files"
   homepage "https://ezip.awehunt.com/"
 
-  app "eZip.app"
+  app "MacZip.app"
 
   zap trash: [
     "~/Library/Application Scripts/com.jinghaoshe.ezip",
