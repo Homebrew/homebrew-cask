5db7c3191e9c58fc7f5d6e5f0b8020472c0cbdba
Vítor Galvão <info@vitorgalvao.com>
2020-11-28 13:45:26 +0000

    Update freesurfer.rb

--- Casks/freesurfer.rb
+++ Casks/freesurfer.rb
@@ -3,7 +3,7 @@ cask "freesurfer" do
   sha256 :no_check
 
   url "https://surfer.nmr.mgh.harvard.edu/pub/dist/freesurfer/dev/freesurfer-darwin-macOS-dev.tar.gz"
-  name "freesurfer"
+  name "FreeSurfer"
   desc "Open source software suite for processing and analyzing brain MRI images"
   homepage "https://surfer.nmr.mgh.harvard.edu/"
 
