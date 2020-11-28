777030ef85dbb343d95932b742d2d4c241dfe468
suschizu <brigitte@corecode.io>
2020-11-28 18:45:37 +0100

    Update aether from 2.0.0-dev.14,1912161354.a1a015e to 2.0.0-dev.15,2011262249.19338c93 (#93823)

--- Casks/aether.rb
+++ Casks/aether.rb
@@ -1,6 +1,6 @@
 cask "aether" do
-  version "2.0.0-dev.14,1912161354.a1a015e"
-  sha256 "cd3a722137891601cd2f2103f2ea57f76b88bd1b930f28ba5877b26372abde06"
+  version "2.0.0-dev.15,2011262249.19338c93"
+  sha256 "87bb9f70525025dadb1d00328f37ee075fe3866044d092a9533e7df71653aca7"
 
   url "https://static.getaether.net/Releases/Aether-#{version.before_comma}/#{version.after_comma}/mac/Aether-#{version.before_comma}%2B#{version.after_comma}.dmg"
   appcast "https://static.getaether.net/WebsiteReleaseLinks/Latest/LatestReleaseLinks.json"
