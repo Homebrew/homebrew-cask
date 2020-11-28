f77773c435396ff8808db41e441ecac25a21c122
OCJvanDijk <ocj@vandijk.dev>
2020-11-28 16:20:36 +0100

    Update brightness-sync from 2.3.0 to 2.3.1 (#93805)

--- Casks/brightness-sync.rb
+++ Casks/brightness-sync.rb
@@ -1,6 +1,6 @@
 cask "brightness-sync" do
-  version "2.3.0"
-  sha256 "e94973ee405ad485d225e408452facfcb449ff3bcf9db80964e8da5449635c24"
+  version "2.3.1"
+  sha256 "3f19d9bdc59de7f882906c9e508ff27f377517f43659444fa712fdcb516bbcd6"
 
   url "https://github.com/OCJvanDijk/Brightness-Sync/releases/download/v#{version}/Brightness.Sync.app.zip"
   appcast "https://github.com/OCJvanDijk/Brightness-Sync/releases.atom"
