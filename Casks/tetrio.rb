cf755571986866ae5e937bb0eec9d527c0c573b4
Justyn Boyer <justynboyer@gmail.com>
2020-11-28 16:21:16 +0000

    Add TETR.IO Latest

---
+++ Casks/tetrio.rb
@@ -0,0 +1,11 @@
+cask "tetrio" do
+  version :latest
+  sha256 :no_check
+
+  url "https://tetr.io/about/desktop/builds/TETR.IO%20Setup.dmg"
+  name "TETR.IO"
+  desc "Free-to-play Tetris clone"
+  homepage "https://tetr.io/about"
+
+  app "TETR.IO.app"
+end
