ab18f4224a62983e54415af3d6d7cf5179bcccbd
Talley Lambert <talley.lambert@gmail.com>
2020-11-28 23:22:45 -0500

    Add napari.app v0.4.2

---
+++ Casks/napari.rb
@@ -0,0 +1,14 @@
+cask "napari" do
+  version "0.4.2"
+  sha256 "8a0481bf710fd2225a7777dbc94343caaa03928940d66e83b80d326ea61650ae"
+
+  url "https://github.com/napari/napari/releases/download/v#{version}/napari-#{version}-macOS.zip"
+  appcast "https://github.com/napari/napari/releases.atom"
+  name "napari"
+  desc "A fast, interactive, multi-dimensional image viewer for Python."
+  homepage "https://napari.org"
+
+  depends_on macos: "<= :catalina"
+  container nested: "napari-#{version}.dmg"
+  app "napari.app"
+end
