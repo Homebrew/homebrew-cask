4531f16ac0b6623c16132cc91bce8d9818aa8563
Talley Lambert <talley.lambert@gmail.com>
2020-11-29 13:13:03 -0500

    add zap, format path, modify desc

--- Casks/napari.rb
+++ Casks/napari.rb
@@ -2,15 +2,18 @@ cask "napari" do
   version "0.4.2"
   sha256 "8a0481bf710fd2225a7777dbc94343caaa03928940d66e83b80d326ea61650ae"
 
-  # github.com/napari/napari was verified as official when first introduced to the cask
+  # github.com/napari/napari/ was verified as official when first introduced to the cask
   url "https://github.com/napari/napari/releases/download/v#{version}/napari-#{version}-macOS.zip"
   appcast "https://github.com/napari/napari/releases.atom"
   name "napari"
-  desc "Fast, interactive, multi-dimensional image viewer for Python"
+  desc "Multi-dimensional image viewer for Python"
   homepage "https://napari.org/"
 
   depends_on macos: "<= :catalina"
   container nested: "napari-#{version}.dmg"
 
   app "napari.app"
+  
+  zap trash: '~/.config/napari/'
+
 end
