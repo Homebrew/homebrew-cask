6e58e4403a357f4e03d8fe1e16becdc181788422
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-29 10:33:41 -0800

    Update archipelago from 3.12.2 to 3.13.0 (#93858)

--- Casks/archipelago.rb
+++ Casks/archipelago.rb
@@ -1,6 +1,6 @@
 cask "archipelago" do
-  version "3.12.2"
-  sha256 "276bb59a41be257e56cfda5ec1912d297ae74c59bc9b2befc536d54afc970a07"
+  version "3.13.0"
+  sha256 "31ce353f8325b248bf55f063b19b303e01916b2a24df49ebae61dfef55ac5e22"
 
   url "https://github.com/npezza93/archipelago/releases/download/v#{version}/Archipelago-#{version}.dmg"
   appcast "https://github.com/npezza93/archipelago/releases.atom"
