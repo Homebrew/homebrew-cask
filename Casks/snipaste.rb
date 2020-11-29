275042a0b71b4ed15443ae9b503eaad09fd01302
Brigitte <brigitte@corecode.io>
2020-11-29 14:40:06 +0100

    Update snipaste from 2.5.2-Beta to 2.5.5-Beta

--- Casks/snipaste.rb
+++ Casks/snipaste.rb
@@ -1,6 +1,6 @@
 cask "snipaste" do
-  version "2.5.2-Beta"
-  sha256 "907f5c99563a6a49a6daeb4cfbb24dfcd78994db36d0f1d4597886870c71d7a5"
+  version "2.5.5-Beta"
+  sha256 "e92ec87c036bcafe2eef89bf4896e54ef3176db6ef4bd98c7639cb83b3a1071e"
 
   # bitbucket.org/liule/snipaste/ was verified as official when first introduced to the cask
   url "https://bitbucket.org/liule/snipaste/downloads/Snipaste-#{version}.dmg"
