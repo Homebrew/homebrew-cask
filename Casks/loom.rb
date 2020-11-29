1b66f65252418d3b16d2864f804eb48e36ec622d
suschizu <brigitte@corecode.io>
2020-11-29 14:46:04 +0100

    Update loom from 0.61.0 to 0.61.1 (#93847)

--- Casks/loom.rb
+++ Casks/loom.rb
@@ -1,6 +1,6 @@
 cask "loom" do
-  version "0.61.0"
-  sha256 "171930ac67eb2013a6432b37c3dafce038f406db395399e2e74bcb1fd3d29f66"
+  version "0.61.1"
+  sha256 "1d40cca7249e9f4b2bfce3e69393b7bd90def855cd8305122a5eee5a1566a633"
 
   url "https://cdn.loom.com/desktop-packages/Loom-#{version}.dmg"
   appcast "https://s3-us-west-2.amazonaws.com/loom.desktop.packages/loom-inc-production/desktop-packages/latest-mac.yml"
