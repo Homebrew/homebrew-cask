4ad422bb8640e7ffe260386e76106a6d69e7f17e
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-01 05:42:01 +0800

    Update wowup.rb

--- Casks/wowup.rb
+++ Casks/wowup.rb
@@ -3,10 +3,10 @@ cask "wowup" do
   sha256 "8a21083adb1db39fe2bf6ff8b5bfcda367d553f7e1f1a8a659341f5d1683bb47"
 
   # github.com/wowup/wowup/ was verified as official when first introduced to the cask
-  url "https://github.com/wowup/wowup/releases/download/v#{version}/wowup-#{version}-mac.zip"
+  url "https://github.com/WowUp/WowUp/releases/download/v#{version}/WowUp-#{version}.dmg"
   appcast "https://github.com/wowup/wowup/releases.atom"
   name "WowUp"
-  desc "World of Warcraft Addon manager"
+  desc "World of Warcraft addon manager"
   homepage "https://wowup.io/"
 
   auto_updates true
@@ -19,6 +19,6 @@ cask "wowup" do
     "~/Library/Application Support/WowUp",
     "~/Library/Logs/WowUp",
     "~/Library/Preferences/io.wowup.jliddev.plist",
-    "~/Library/Saved Application State/io.wowup.jliddev.savedState/",
+    "~/Library/Saved Application State/io.wowup.jliddev.savedState",
   ]
 end
