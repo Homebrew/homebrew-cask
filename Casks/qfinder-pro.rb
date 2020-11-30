aee668d223e0198550a7f79d61103ba5b84ed257
Randall <ran.dall@icloud.com>
2020-11-30 13:10:51 -0800

    Update qfinder-pro from 7.4.1.0910 to 7.4.2.1118

--- Casks/qfinder-pro.rb
+++ Casks/qfinder-pro.rb
@@ -1,6 +1,6 @@
 cask "qfinder-pro" do
-  version "7.4.1.0910"
-  sha256 "a7c18ac1cc78cd761ce376bcc7ae450cb3d75abf1eb6ecbf4ff019f19cfb4144"
+  version "7.4.2.1118"
+  sha256 "121ab79617ce7a861b6ae9f422efe98268a5a3fcddfcc3eada3c1a5d8ab22a71"
 
   url "https://download.qnap.com/Storage/Utility/QNAPQfinderProMac-#{version}.dmg"
   appcast "https://www.macupdater.net/cgi-bin/extract_text/extract_text_split_easy.cgi?url=https://update.qnap.com/SoftwareRelease.xml&splitter_1=Mac_for_QT&index_1=1&splitter_2=downloadURL&index_2=1"
