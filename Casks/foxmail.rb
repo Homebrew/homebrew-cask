a25b1065b990ed033fe8eba64c8ebfc1cc48347d
Randall <ran.dall@icloud.com>
2020-11-30 13:15:54 -0800

    Update foxmail from 1.4.3.94167 to 1.4.3.94169

--- Casks/foxmail.rb
+++ Casks/foxmail.rb
@@ -1,6 +1,6 @@
 cask "foxmail" do
-  version "1.4.3.94167"
-  sha256 "a1580a0d2845205dc41222a2623ac7b436f41501455fab16bfc7da4fea5bd1aa"
+  version "1.4.3.94169"
+  sha256 "e9821bd3d3b9ed3b46ca7a8195a51c4d53fdc5877adc3916dbb37734165a0008"
 
   # dldir1.qq.com/foxmail/MacFoxmail/ was verified as official when first introduced to the cask
   url "https://dldir1.qq.com/foxmail/MacFoxmail/Foxmail_for_Mac_#{version}.dmg"
