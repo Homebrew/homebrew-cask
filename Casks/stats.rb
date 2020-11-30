664a4c7747e88a3c828f025aac96df00f85588b2
optikfluffel <optik@fluffel.io>
2020-11-30 19:32:01 +0100

    Update stats from 2.4.5 to 2.4.6

--- Casks/stats.rb
+++ Casks/stats.rb
@@ -1,6 +1,6 @@
 cask "stats" do
-  version "2.4.5"
-  sha256 "8063bf26e31486a6798f10d268fa850f62a01d1121ba04da954f4b9c5507517c"
+  version "2.4.6"
+  sha256 "98223e7d68b8f663d6bc9f8c0924f47b6c2915d9061b69226e3b10b0dd964287"
 
   url "https://github.com/exelban/stats/releases/download/v#{version}/Stats.dmg"
   appcast "https://github.com/exelban/stats/releases.atom"
