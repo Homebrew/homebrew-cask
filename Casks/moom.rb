68a36955588dc30f3541ab78089704589a5e56ab
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-30 13:10:05 -0800

    Update moom from 3.2.19 to 3.2.20 (#93911)

--- Casks/moom.rb
+++ Casks/moom.rb
@@ -1,6 +1,6 @@
 cask "moom" do
-  version "3.2.19"
-  sha256 "6aab4a0a186d119835f24f8899853b1b89c4b914f6a0379946706a113babcd14"
+  version "3.2.20"
+  sha256 "924a08f89379a4c5ade4b079af9e41fb91ec0297d291d123f29800d18c187def"
 
   url "https://manytricks.com/download/_do_not_hotlink_/moom#{version.no_dots}.dmg"
   appcast "https://manytricks.com/moom/appcast"
