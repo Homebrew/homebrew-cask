768236b7dbda23d27a8e8cd77b91d818a4b794dd
Randall <ran.dall@icloud.com>
2020-12-01 12:50:30 -0800

    Update zerotier-one from 1.6.1 to 1.6.2

--- Casks/zerotier-one.rb
+++ Casks/zerotier-one.rb
@@ -1,5 +1,5 @@
 cask "zerotier-one" do
-  version "1.6.1"
+  version "1.6.2"
   sha256 "9b87cf8f502bead3df6a9702ad94b29c13329405b6963328f8f05c3749cfe07a"
 
   url "https://download.zerotier.com/dist/ZeroTier%20One.pkg"
