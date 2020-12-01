e4825805ffe98bb7295308d67741319ab267301f
Brigitte <brigitte@corecode.io>
2020-12-02 00:54:26 +0100

    Update orange

--- Casks/orange.rb
+++ Casks/orange.rb
@@ -1,6 +1,6 @@
 cask "orange" do
   version "3.27.1"
-  sha256 "156d434ce8fb392a3b645ff217dd9623a1813f65fb262056b6c8cf2d1e3b0865"
+  sha256 "f175f7562bd1ae34ef8ec4e4bcd9b7090e39b95383a1206cbbd8f18b4828eef2"
 
   url "https://download.biolab.si/download/files/Orange#{version.major}-#{version}-Python3.7.6.dmg"
   appcast "https://github.com/biolab/orange3/releases.atom"
