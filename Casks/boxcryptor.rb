473d68c41f0aad8d6c1ecf21c67ee98635bb1c68
Randall <ran.dall@icloud.com>
2020-12-01 13:19:03 -0800

    Update boxcryptor from 2.37.1043 to 2.38.1086

--- Casks/boxcryptor.rb
+++ Casks/boxcryptor.rb
@@ -1,6 +1,6 @@
 cask "boxcryptor" do
-  version "2.37.1043"
-  sha256 "aca956d433ee4bb76ba29de5eeb69825d71989979753d25ce2f16ff8965000a3"
+  version "2.38.1086"
+  sha256 "6a8e68f8152c94bc2a5ea8c8a4a475994ebb508ee29f9d99dcd9397d2ce567a8"
 
   url "https://downloads.boxcryptor.com/boxcryptor/mac/Boxcryptor_v#{version}_Installer.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.boxcryptor.com/l/download-macosx"
