0ba2ce78e5d923b87605b973f72d24045f97a0b8
Julian Mayer <julian@corecode.io>
2020-12-01 14:47:57 +0100

    Update zappy from 2.6.0 to 2.7.0

--- Casks/zappy.rb
+++ Casks/zappy.rb
@@ -1,6 +1,6 @@
 cask "zappy" do
-  version "2.6.0"
-  sha256 "05cc5b4f8b56bb21ce8055c196d7fbc1084d0115e817d9aa1d2bd512976e08b2"
+  version "2.7.0"
+  sha256 "3c45a299288de29f7f27561925cb2dba8f659d15b1a6f39c04a8308bfb22f44f"
 
   url "https://zappy.zapier.com/releases/zappy-latest.dmg"
   appcast "https://zappy.zapier.com/releases/appcast.xml"
