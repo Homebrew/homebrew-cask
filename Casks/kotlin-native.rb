b71d695ef67c0710fb63162ec0b84bee063acf8f
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-01 16:55:02 +0800

    Update kotlin-native.rb

--- Casks/kotlin-native.rb
+++ Casks/kotlin-native.rb
@@ -6,18 +6,19 @@ cask "kotlin-native" do
   url "https://github.com/JetBrains/kotlin/releases/download/v#{version}/kotlin-native-macos-#{version}.tar.gz"
   appcast "https://github.com/JetBrains/kotlin-native/releases.atom"
   name "Kotlin Native"
+  desc "LLVM backend for Kotlin"
   homepage "https://kotlinlang.org/docs/reference/native-overview.html"
 
   conflicts_with formula: "kotlin"
 
-  binary "kotlin-native-macos-#{version.major_minor}/bin/cinterop"
-  binary "kotlin-native-macos-#{version.major_minor}/bin/jsinterop"
-  binary "kotlin-native-macos-#{version.major_minor}/bin/klib"
-  binary "kotlin-native-macos-#{version.major_minor}/bin/konan-lldb"
-  binary "kotlin-native-macos-#{version.major_minor}/bin/konanc"
-  binary "kotlin-native-macos-#{version.major_minor}/bin/kotlinc"
-  binary "kotlin-native-macos-#{version.major_minor}/bin/kotlinc-native"
-  binary "kotlin-native-macos-#{version.major_minor}/bin/run_konan"
+  binary "kotlin-native-macos-#{version}/bin/cinterop"
+  binary "kotlin-native-macos-#{version}/bin/jsinterop"
+  binary "kotlin-native-macos-#{version}/bin/klib"
+  binary "kotlin-native-macos-#{version}/bin/konan-lldb"
+  binary "kotlin-native-macos-#{version}/bin/konanc"
+  binary "kotlin-native-macos-#{version}/bin/kotlinc"
+  binary "kotlin-native-macos-#{version}/bin/kotlinc-native"
+  binary "kotlin-native-macos-#{version}/bin/run_konan"
 
   caveats do
     depends_on_java "6+"
