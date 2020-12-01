4ba8ec253c1c0b9f9238453ae19e958e9ca04e5a
Tiansheng Xu <xuts45@outlook.com>
2020-12-01 18:31:16 +0800

    Update xmind-2020.rb

--- Casks/xmind-2020.rb
+++ Casks/xmind-2020.rb
@@ -1,8 +1,9 @@
 cask "xmind-2020" do
-  version "10.2.1"
+  version "10.2.1-202007271856"
   sha256 "a41f8647ef655e19e4a8066746475be95fd13b501c9fa09cc81abadb1da29580"
 
-  url "https://dl3.xmind.net/XMind-2020-for-macOS-10.2.1-202007271856.dmg"
+  url "https://www.xmind.net/xmind/downloads/XMind-2020-for-macOS-#{version}.dmg"
+  appcast "https://www.xmind.net/xmind2020/release-notes/"
   name "Xmind 2020"
   desc "Full-featured mindmap software"
   homepage "https://www.xmind.net/xmind2020/"
@@ -11,4 +12,6 @@ cask "xmind-2020" do
   conflicts_with cask: "xmind"
 
   app "XMind.app"
+
+  zap trash: "~/Library/Application Support/XMind ZEN"
 end
