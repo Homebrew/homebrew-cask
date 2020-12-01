f30128a40865e4d6ad49e68de6636e7eb2913c5f
Alexander Ronald Altman <alexanderaltman@me.com>
2020-12-01 01:03:13 -0800

    Update kotlin-native from 1.4.0 to 1.4.20 (#93941)
    * Update kotlin-native from 1.4.0 to 1.4.20
    
    * Update kotlin-native.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/kotlin-native.rb
+++ Casks/kotlin-native.rb
@@ -1,23 +1,24 @@
 cask "kotlin-native" do
-  version "1.4.0"
-  sha256 "476f920631b0ccb4b8e25456ef49356fb33d6e3960e9f2ec8def0aaa23284168"
+  version "1.4.20"
+  sha256 "5c4e838211ffe52d542fe559dbc55d2173a289dd91fd91294845fe7555478b9b"
 
   # github.com/JetBrains/kotlin/ was verified as official when first introduced to the cask
-  url "https://github.com/JetBrains/kotlin/releases/download/v#{version}/kotlin-native-macos-#{version.major_minor}.tar.gz"
+  url "https://github.com/JetBrains/kotlin/releases/download/v#{version}/kotlin-native-macos-#{version}.tar.gz"
   appcast "https://github.com/JetBrains/kotlin-native/releases.atom"
   name "Kotlin Native"
+  desc "LLVM backend for Kotlin"
   homepage "https://kotlinlang.org/docs/reference/native-overview.html"
 
   conflicts_with formula: "kotlin"
 
-  binary "kotlin-native-macos-#{version.major_minor}/bin/cinterop"
-  binary "kotlin-native-macos-#{version.major_minor}/bin/jsinterop"
-  binary "kotlin-native-macos-#{version.major_minor}/bin/klib"
-  binary "kotlin-native-macos-#{version.major_minor}/bin/konan-lldb"
-  binary "kotlin-native-macos-#{version.major_minor}/bin/konanc"
-  binary "kotlin-native-macos-#{version.major_minor}/bin/kotlinc"
-  binary "kotlin-native-macos-#{version.major_minor}/bin/kotlinc-native"
-  binary "kotlin-native-macos-#{version.major_minor}/bin/run_konan"
+  binary "kotlin-native-macos-#{version}/bin/cinterop"
+  binary "kotlin-native-macos-#{version}/bin/jsinterop"
+  binary "kotlin-native-macos-#{version}/bin/klib"
+  binary "kotlin-native-macos-#{version}/bin/konan-lldb"
+  binary "kotlin-native-macos-#{version}/bin/konanc"
+  binary "kotlin-native-macos-#{version}/bin/kotlinc"
+  binary "kotlin-native-macos-#{version}/bin/kotlinc-native"
+  binary "kotlin-native-macos-#{version}/bin/run_konan"
 
   caveats do
     depends_on_java "6+"
