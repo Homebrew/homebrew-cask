68188b12612115bd056ef29d6e560a92998e9042
Julian Mayer <julian@corecode.io>
2020-12-01 14:47:33 +0100

    Update anydo from 4.2.117 to 4.2.118

--- Casks/anydo.rb
+++ Casks/anydo.rb
@@ -1,6 +1,6 @@
 cask "anydo" do
-  version "4.2.117"
-  sha256 "9db706783bb4249d1120bb16ae66023515b0edf742fb0b493cdf7dabc3170138"
+  version "4.2.118"
+  sha256 "b6d84ef0d633943374324dc1cd145337164d8e5da6b166a526b364440044d9a8"
 
   url "https://electron-app.any.do/Any.do.dmg"
   appcast "https://electron-app.any.do/latest-mac.yml"
