5d38cee284f5a6708e28a68942ccd8a828cfbe14
Brigitte <brigitte@corecode.io>
2020-12-01 14:55:51 +0100

    Update iina-plus from 0.5.3,20101309 to 0.5.4,20112920

--- Casks/iina-plus.rb
+++ Casks/iina-plus.rb
@@ -1,8 +1,8 @@
 cask "iina-plus" do
-  version "0.5.3,20101309"
-  sha256 "f64fe67ad13ca29fc22b3aecd80536216437eb6b6b77a53f8db57850c2af612e"
+  version "0.5.4,20112920"
+  sha256 "81d3e6257abbf09b71319821732b30ad3917d5f888190706792f9eff19482ee4"
 
-  url "https://github.com/xjbeta/iina-plus/releases/download/#{version.before_comma}(#{version.after_comma})/iina+.#{version.before_comma}.zip"
+  url "https://github.com/xjbeta/iina-plus/releases/download/#{version.before_comma}(#{version.after_comma})/iina+.#{version.before_comma}.dmg"
   appcast "https://github.com/xjbeta/iina-plus/releases.atom"
   name "IINA+"
   desc "Extra danmaku support for iina. (iina 弹幕支持"
