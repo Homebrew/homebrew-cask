ba94200700623c514a47a3140a1adbe404cfbe33
Brigitte <brigitte@corecode.io>
2020-12-02 00:56:36 +0100

    Update mcreator

--- Casks/mcreator.rb
+++ Casks/mcreator.rb
@@ -1,6 +1,6 @@
 cask "mcreator" do
   version "2020.5"
-  sha256 "007ceb832b15ac77243282534c5de4f1c87e068fcc493c0a4469ad7f433a5d9a"
+  sha256 "370b1669fb8a9a596c47cb23d315a2b7241b22bb46482e4deba00a5c61ea3a44"
 
   url "https://mcreator.net/repository/#{version.dots_to_hyphens}/MCreator%20#{version}%20Mac%2064bit.dmg"
   appcast "https://mcreator.net/download"
