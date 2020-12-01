efa819dd9137e9b3923afd9e9b803dc8832f1fe7
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-01 15:34:20 +0000

    Update qtvlm from 5.9.10,217 to 5.9.14.p1.3,270

--- Casks/qtvlm.rb
+++ Casks/qtvlm.rb
@@ -1,8 +1,8 @@
 cask "qtvlm" do
-  version "5.9.10,217"
-  sha256 "ed38ccd9565344ead60f46b59d4f1f4d4290e13a43f84eb730d31d8c1eca6523"
+  version "5.9.14.p1.3,270"
+  sha256 "f0035418df85822276ac803a363519d0524013a0d4cb8fb369683df64301e419"
 
-  url "https://www.meltemus.com/index.php/en/download/send/3-mac-os/#{version.after_comma}-qtvlm-#{version.before_comma.dots_to_hyphens}-2"
+  url "https://www.meltemus.com/index.php/en/download/send/3-mac-os/#{version.after_comma}-qtvlm-#{version.before_comma.dots_to_hyphens}"
   appcast "https://www.meltemus.com/index.php/en/download/category/3-mac-os",
           must_contain: version.before_comma
   name "qtvlm"
