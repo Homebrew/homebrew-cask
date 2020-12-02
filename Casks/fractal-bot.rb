cec9e1de2a6c7a29fb8070b6ed9688655c34f7b1
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-02 09:00:14 +0800

    Update fractal-bot.rb

--- Casks/fractal-bot.rb
+++ Casks/fractal-bot.rb
@@ -1,11 +1,12 @@
 cask "fractal-bot" do
-  version "3.0.6"
-  sha256 "016b1039de30f3ac8bb4563bdb60f7f4c3368bcdcadf9dbd856cf332a567f02e"
+  version "3.00.06"
+  sha256 "32f8ad0cd6f733b176046b1256a5ccf9ce44f1fb7eea25fcb6e159882cee74ab"
 
   url "https://www.fractalaudio.com/downloads/fractal-bot/Fractal-Bot-OSX-v#{version.tr(".", "p")}.dmg"
   appcast "https://www.fractalaudio.com/fractal-bot/",
           must_contain: "#{version.tr(".", "p")}.dmg"
   name "Fractal Bot"
+  desc "Send and receive data to and from your Fractal Audio Systems products"
   homepage "https://www.fractalaudio.com/fractal-bot/"
 
   app "Fractal-Bot.app"
