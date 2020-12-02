868c0faa1e9a4c89861dcc8c708aeba80958a57d
suschizu <brigitte@corecode.io>
2020-12-03 00:05:52 +0100

    Update electerm from 1.9.31 to 1.10.0 (#94063)

--- Casks/electerm.rb
+++ Casks/electerm.rb
@@ -1,8 +1,8 @@
 cask "electerm" do
-  version "1.9.31"
-  sha256 "1d6fb6d40e69712a6f396c60ccc14f6c9f19cf3e0b300a0926a15a44dc03d79f"
+  version "1.10.0"
+  sha256 "ee2d292cf962a1985f588350e1ebf09e132f7a7b6423a104364d92266fbad5df"
 
-  url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac.dmg"
+  url "https://github.com/electerm/electerm/releases/download/#{version}/electerm-#{version}-mac.dmg"
   appcast "https://github.com/electerm/electerm/releases.atom"
   name "electerm"
   desc "Terminal/ssh/serial port/sftp client"
