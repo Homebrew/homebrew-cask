b2d2b05cb4592f0987e8d0d7ff223e017b775db0
Randall <ran.dall@icloud.com>
2020-12-02 08:42:40 -0800

    Update apptrap from 1.2.3 to 1.2.4

--- Casks/apptrap.rb
+++ Casks/apptrap.rb
@@ -1,6 +1,6 @@
 cask "apptrap" do
-  version "1.2.3"
-  sha256 "39a8923698c2e1a38ff3fd7fc2c12b7a847566cf1f31f965d0fb57e2280aaa5c"
+  version "1.2.4"
+  sha256 "54c01bca14a521513e5a90dee282a635a766e02b87e363df494a4bf7c220f0d1"
 
   url "http://onnati.net/apptrap/download/AppTrap#{version.dots_to_hyphens}.zip"
   appcast "http://onnati.net/apptrap/ReleaseNotes.xml"
