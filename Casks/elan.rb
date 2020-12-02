0d5483ca3f6b6bd1309815ab5d74e5cc0683a05c
Randall <ran.dall@icloud.com>
2020-12-02 09:08:14 -0800

    Update elan from 5.9 to 6.0

--- Casks/elan.rb
+++ Casks/elan.rb
@@ -1,6 +1,6 @@
 cask "elan" do
-  version "5.9"
-  sha256 "91aa8a64403641a77a135719a8b5bd7f5a5e5d73649adb1f3eaad92dffde584d"
+  version "6.0"
+  sha256 "8b3022783565e26c213cfe6f5caf42331eb97e264f3a6092620a835f66068714"
 
   url "https://www.mpi.nl/tools/elan/ELAN_#{version.dots_to_hyphens}_mac.zip"
   appcast "https://archive.mpi.nl/tla/elan/release-notes"
