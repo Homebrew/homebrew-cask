5a1533f5f2de8d7cc8aab2816d826e82a7789893
suschizu <brigitte@corecode.io>
2020-12-02 13:00:38 +0100

    Update webplotdigitizer (#94049)

--- Casks/webplotdigitizer.rb
+++ Casks/webplotdigitizer.rb
@@ -1,6 +1,6 @@
 cask "webplotdigitizer" do
   version "4.3"
-  sha256 "f0810ab5be0ad5900a7bf6700687d0652e0f57fae3083fd5f591456d8800d0c1"
+  sha256 "117866ef65bc7d1644159657cffc5c3e54040b8d454011eaf197375b084309a7"
 
   url "https://automeris.io/downloads/WebPlotDigitizer-#{version}-darwin-x64.zip"
   appcast "https://github.com/ankitrohatgi/WebPlotDigitizer/releases.atom"
