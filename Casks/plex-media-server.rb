7958af126786f1268e9f63874c399f064719d01d
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-02 10:31:02 -0800

    Update plex-media-server from 1.21.0.3616-d87012962 to 1.21.0.3711-b509cc236 (#94115)

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -1,6 +1,6 @@
 cask "plex-media-server" do
-  version "1.21.0.3616-d87012962"
-  sha256 "8219e64b414f7bbbaf70703cc795561f564deefd21523304418f0c74d12bd85c"
+  version "1.21.0.3711-b509cc236"
+  sha256 "e50bb16bb590a13c731d967e8738bcfdbf1381601de7b426a4c1bd16055a887b"
 
   url "https://downloads.plex.tv/plex-media-server-new/#{version}/macos/PlexMediaServer-#{version}-x86_64.zip"
   appcast "https://plex.tv/api/downloads/5.json"
