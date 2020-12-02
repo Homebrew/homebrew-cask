0b9ef30b9d7db5b7b0fc5e45d357acfc431ee3af
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-02 09:59:26 -0800

    Update milanote from 2.2.22 to 2.2.23 (#94110)

--- Casks/milanote.rb
+++ Casks/milanote.rb
@@ -1,6 +1,6 @@
 cask "milanote" do
-  version "2.2.22"
-  sha256 "10bb470c84ad19867b482faea23607f678fc273135e8f6f6a0c1534606c7cf7b"
+  version "2.2.23"
+  sha256 "c41c73079f73699116c7d5dabad0047c7c3ec49aee2af1dbe81e7fa582bab27f"
 
   # milanote-app-releases.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://milanote-app-releases.s3.amazonaws.com/Milanote-#{version}.dmg"
