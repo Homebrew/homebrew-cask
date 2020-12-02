5663a9908563332b7a26fa7600228d05c512cf8c
octoer <37321851+octoer@users.noreply.github.com>
2020-12-02 12:45:00 +0800

    Update surge.rb

--- Casks/surge.rb
+++ Casks/surge.rb
@@ -1,6 +1,6 @@
 cask "surge" do
   version "4.0.1-1207-ee7bea1b244950c82a6f90e060fa2d89"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "e143209bba17bd66a3dfd9eabd68059be5745ad31919f76d6c0ef0ce28e5e73d"
 
   url "https://www.nssurge.com/mac/v#{version.major}/Surge-#{version}.zip"
   appcast "https://www.nssurge.com/mac/v#{version.major}/appcast-signed.xml"
