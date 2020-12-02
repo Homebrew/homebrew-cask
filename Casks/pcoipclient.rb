4ba07ae6a710e1329f9587cdbc42772f2ae9ca56
suschizu <brigitte@corecode.io>
2020-12-02 14:53:20 +0100

    Update pcoipclient from 20.10.1 to 20.10.2 (#94071)

--- Casks/pcoipclient.rb
+++ Casks/pcoipclient.rb
@@ -1,6 +1,6 @@
 cask "pcoipclient" do
-  version "20.10.1"
-  sha256 "2d1053c6dc0aee0336338cf3365e3e2381302a681f2e3fd0f859d415d463b2f9"
+  version "20.10.2"
+  sha256 "b03b7a4358427011d5bdfa940c8ebe74a9115246e2dfd9667538ea6bf6c5c25d"
 
   url "https://downloads.teradici.com/mac/stable/pcoip-client_#{version}.dmg"
   appcast "https://downloads.teradici.com/mac/stable/"
