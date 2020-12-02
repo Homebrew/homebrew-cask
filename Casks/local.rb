8dcbc4bdfe294e37781e3a6c5e75ac14c409ddd7
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-01 16:28:54 -0800

    Update local from 5.9.3,5100 to 5.9.4,5115 (#94009)

--- Casks/local.rb
+++ Casks/local.rb
@@ -1,6 +1,6 @@
 cask "local" do
-  version "5.9.3,5100"
-  sha256 "c665014a3bf64d218e9e34e29e63c0ffc0c5dec9260222105d05286884de84ef"
+  version "5.9.4,5115"
+  sha256 "6da9743756370ef9f754fd939684ae37468693ba48901c2d6889bccf3dab67af"
 
   url "https://cdn.localwp.com/releases-stable/#{version.before_comma}+#{version.after_comma}/local-#{version.before_comma}-mac.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://cdn.localwp.com/stable/latest/mac"
