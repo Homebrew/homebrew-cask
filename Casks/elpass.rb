33a977431cb4d083a89c710a8cc4f967f7d9ba1f
Randall <ran.dall@icloud.com>
2020-12-02 08:41:27 -0800

    Update elpass from 1.2.0,309 to 1.2.2,324

--- Casks/elpass.rb
+++ Casks/elpass.rb
@@ -1,6 +1,6 @@
 cask "elpass" do
-  version "1.2.0,309"
-  sha256 "f35c1e2efc0c6406d471b8e728bfc90079a7a1daf91410bbdb9efe2c89c319bb"
+  version "1.2.2,324"
+  sha256 "b935496f83c1fd33afa87d7d53a7df9a9facefcbd482e4cd0650bb07b17d1911"
 
   url "https://elpass.app/macos/Elpass-#{version.before_comma}-#{version.after_comma}.zip"
   appcast "https://elpass.app/macos/appcast.xml"
