4041a32a973cf2e6d5537c5b34328d6bf72bc129
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-02 09:47:17 -0800

    Update visual-paradigm from 16.2,20201101 to 16.2,20201203 (#94108)

--- Casks/visual-paradigm.rb
+++ Casks/visual-paradigm.rb
@@ -1,6 +1,6 @@
 cask "visual-paradigm" do
-  version "16.2,20201101"
-  sha256 "0052e9cab930306d34d5ff7a74037d756b0ddbacbc20dbcb95444d29709b75ec"
+  version "16.2,20201203"
+  sha256 "585fed5cb34baa669150f56ad60aa654a0e6f5e805bc66f3af1495715c59dddd"
 
   url "https://eu8.dl.visual-paradigm.com/visual-paradigm/vp#{version.before_comma}/#{version.after_comma}/Visual_Paradigm_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
   appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.visual-paradigm.com/downloads/vp/checksum.html",
