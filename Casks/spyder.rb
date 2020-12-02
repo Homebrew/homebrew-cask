6dfea33b413159e5a02759c74f0b752ffa40c4f7
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-02 10:17:49 +0800

    Update spyder.rb

--- Casks/spyder.rb
+++ Casks/spyder.rb
@@ -2,8 +2,8 @@ cask "spyder" do
   version "4.2.0"
   sha256 "4c92ca2e93498a93d14c49e8c46b61220d3cd99c003ffaf176e6feaa3d70887a"
 
-  # github.com/spyder-ide/spyder/ was verified as official when first introduced to the cask
-  url "https://github.com/spyder-ide/spyder/releases/download/v#{version}/Spyder.dmg"
+  url "https://github.com/spyder-ide/spyder/releases/download/v#{version}/Spyder.dmg",
+      verified: "https://github.com/spyder-ide/spyder/"
   appcast "https://github.com/spyder-ide/spyder/releases.atom"
   name "Spyder"
   desc "Scientific Python IDE"
