aa0c7b7e5149cd292f70c8a023561e09a9d30d32
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-01 18:13:18 -0800

    Update qownnotes from 20.11.11 to 20.12.1 (#93984)
    * Update qownnotes from 20.11.11 to 20.12.0
    
    * Update qownnotes.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,11 +1,12 @@
 cask "qownnotes" do
-  version "20.11.11"
-  sha256 "2f5341f57094e702f2aa564099014a0a0426950c6e6313d1b1086a1468f650a1"
+  version "20.12.1"
+  sha256 "85a2d3aa6b6508040c8ae5658d723c2297999ee1025160aa15bce8497bb68b23"
 
   # github.com/pbek/QOwnNotes/ was verified as official when first introduced to the cask
   url "https://github.com/pbek/QOwnNotes/releases/download/v#{version}/QOwnNotes.dmg"
-  appcast "https://www.qownnotes.org/api/v1/last_release/QOwnNotes/macosx.json"
+  appcast "https://github.com/pbek/QOwnNotes/releases.atom"
   name "QOwnNotes"
+  desc "Plain-text file notepad and todo-list manager"
   homepage "https://www.qownnotes.org/"
 
   auto_updates true
