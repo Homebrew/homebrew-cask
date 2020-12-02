aa86499e00a98894609b9487f139a24a035bc7c4
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-02 10:35:32 +0800

    Update stashcat.rb

--- Casks/stashcat.rb
+++ Casks/stashcat.rb
@@ -2,7 +2,6 @@ cask "stashcat" do
   version :latest
   sha256 :no_check
 
-  # stashcat.s3-de-central.profitbricks.com was verified as official when first introduced to the cask
   url "https://stashcat.s3-de-central.profitbricks.com/installer/StashcatSetup.dmg",
       verified: "https://stashcat.s3-de-central.profitbricks.com/installer/"
   name "Stashcat"
