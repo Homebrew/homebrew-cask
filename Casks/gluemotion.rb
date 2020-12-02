467f83cee5191e328c952bf9484c602572437d90
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-02 09:01:55 +0800

    Update gluemotion.rb

--- Casks/gluemotion.rb
+++ Casks/gluemotion.rb
@@ -2,10 +2,10 @@ cask "gluemotion" do
   version "1.5.0"
   sha256 "349f01156bd41134bd8d43bfefc3dd445c9dd5c97dff59df37fcc357af9c36aa"
 
-  url "https://neededapps.com/appcasts/gluemotion/versions/GM-1.5.0.zip"
+  url "https://neededapps.com/appcasts/gluemotion/versions/GM-#{version}.zip"
   appcast "https://neededapps.com/appcasts/gluemotion/changelog.xml"
   name "GlueMotion"
-  desc "Create and correct beautiful time lapse movies"
+  desc "Create and correct time lapse movies"
   homepage "https://neededapps.com/gluemotion/"
 
   app "GlueMotion.app"
