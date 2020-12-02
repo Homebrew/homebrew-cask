e5d83f5b23559ee31045dbb43e6bcc37c92301aa
Julian Mayer <julian@corecode.io>
2020-12-02 16:08:24 +0100

    Update vitamin-r from 3.22 to 3.23

--- Casks/vitamin-r.rb
+++ Casks/vitamin-r.rb
@@ -3,8 +3,8 @@ cask "vitamin-r" do
     version "2.58"
     sha256 "c6c631430b44359aa022d9ca5ca6e98dbdf7258f2ceae0353f344a035682661e"
   else
-    version "3.22"
-    sha256 "cf1abffe802eef5727ee2ed0d5f539c21f47f310e70f9cf4b0227f6852765cd8"
+    version "3.23"
+    sha256 "7e3bf168ab4b2b56e86cb2592b281bee8405e081a97a485c18cf0e0f6aebc97f"
   end
 
   url "https://www.publicspace.net/download/signedVitamin#{version.major}.zip"
