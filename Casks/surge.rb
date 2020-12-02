91508cb63025167b76d2073a5f66b7dab7081df3
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-02 12:33:40 +0800

    Update surge.rb

--- Casks/surge.rb
+++ Casks/surge.rb
@@ -1,10 +1,11 @@
 cask "surge" do
-  version "4.0.1-1207"
+  version "4.0.1-1207-ee7bea1b244950c82a6f90e060fa2d89"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
   url "https://www.nssurge.com/mac/v#{version.major}/Surge-#{version}.zip"
   appcast "https://www.nssurge.com/mac/v#{version.major}/appcast-signed.xml"
   name "Surge"
+  desc "Network toolbox"
   homepage "https://nssurge.com/"
 
   auto_updates true
