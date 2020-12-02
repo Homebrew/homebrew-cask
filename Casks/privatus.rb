7d32da321ed7481ab04fabca4f14fd781cc1bd8e
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-02 10:31:04 +0800

    Update privatus.rb

--- Casks/privatus.rb
+++ Casks/privatus.rb
@@ -2,9 +2,9 @@ cask "privatus" do
   version "6.3"
   sha256 "a43405c336bdb86845ed21ff11145802bf7337046cc6c3c1d7cad8687096f1b3"
 
-  # sweetpproductions.com was verified as official when first introduced to the cask
-  url "https://sweetpproductions.com/products/privatus#{version.major}/Privatus#{version.major}.dmg"
-  appcast "https://sweetpproductions.com/products/privatus6/appcast.xml"
+  url "https://sweetpproductions.com/products/privatus#{version.major}/Privatus#{version.major}.dmg",
+      verified: "https://sweetpproductions.com/products/"
+  appcast "https://sweetpproductions.com/products/privatus#{version.major}/appcast.xml"
   name "Privatus"
   desc "Automated online privacy protection"
   homepage "https://privatusapp.com/"
@@ -14,8 +14,7 @@ cask "privatus" do
   app "Privatus.app"
 
   zap trash: [
-    "~/Library/Containers/com.sweetpproductions.Privatus6",
-    "~/Library/Containers/com.sweetpproductions.Privatus6/Data",
-    "~/Library/Preferences/com.sweetpproductions.Privatus6.plist",
+    "~/Library/Containers/com.sweetpproductions.Privatus#{version.major}",
+    "~/Library/Preferences/com.sweetpproductions.Privatus#{version.major}.plist",
   ]
 end
