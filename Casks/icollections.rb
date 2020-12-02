efac7a4c8f4f008d57744ca9334bbbdd24f36088
CoreCode <core-code@users.noreply.github.com>
2020-12-02 19:51:39 +0100

    Update icollections from 6.7.1,67102 to 6.7.2,67201 (#94116)

--- Casks/icollections.rb
+++ Casks/icollections.rb
@@ -1,6 +1,6 @@
 cask "icollections" do
-  version "6.7.1,67102"
-  sha256 "404833a990e93616159dba935b6f9e51389571de2f6c0d0125dc8a317dce3b09"
+  version "6.7.2,67201"
+  sha256 "0817e68a6599fbf65344eafdd5693233aa9f645f7e0b0cf4a58dcc454b7c1695"
 
   url "https://naarakstudio.com/download/iCollections.dmg"
   appcast "https://naarakstudio.com/download/icollections/update.ver",
