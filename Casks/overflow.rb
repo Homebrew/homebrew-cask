f3042b359506547c99206427be9bf2d4f3dab7a3
tiiiecherle <tiiiecherle@gmail.com>
2020-12-02 13:32:12 +0100

    Update overflow from 3.1 to 3.2.1 (#94044)
    * Update overflow from 3.1 to 3.2.1
    
    * Update overflow.rb
    
    * Update overflow.rb
    
    * Update overflow.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/overflow.rb
+++ Casks/overflow.rb
@@ -1,11 +1,12 @@
 cask "overflow" do
-  version "3.1"
-  sha256 "853928dbe2482194bfbe441184ef13fe2c8dd20219398daad43ba968abea41fe"
+  version "3.2.1"
+  sha256 "61c40e5d497a0f0e26dfb4062fbb958cbb9a0ea7895a7fcff64edca3e265f034"
 
   # stunt.s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://stunt.s3.amazonaws.com/overflow#{version}.zip"
+  url "https://stunt.s3.amazonaws.com/overflow_#{version}.zip"
   appcast "https://stuntsoftware.com/PHP/sparkle/overflow#{version.major}.php"
-  name "overflow"
+  name "Overflow"
+  desc "Visual application launcher"
   homepage "https://stuntsoftware.com/overflow/"
 
   depends_on macos: ">= :high_sierra"
@@ -13,7 +14,7 @@ cask "overflow" do
   app "Overflow #{version.major}.app"
 
   zap trash: [
-    "~/Library/Containers/com.stuntsoftware.Overflow3/Data/Library/Preferences/com.stuntsoftware.Overflow#{version.major}.plist",
-    "~/Library/Containers/com.stuntsoftware.Overflow3/Data/Library/Application Support/default.realm",
+    "~/Library/Containers/com.stuntsoftware.Overflow#{version.major}/Data/Library/Preferences/com.stuntsoftware.Overflow#{version.major}.plist",
+    "~/Library/Containers/com.stuntsoftware.Overflow#{version.major}/Data/Library/Application Support/default.realm",
   ]
 end
