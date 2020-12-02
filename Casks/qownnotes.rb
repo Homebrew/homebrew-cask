c7d34f3b9b602834fffe72d02f8f909bb366ee7c
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-02 08:54:42 +0800

    Update qownnotes.rb

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,11 +1,12 @@
 cask "qownnotes" do
-  version "20.12.0"
-  sha256 "ada9f4734da0455819d0529432e2c13b25e868418d2020292cbb992e2445ce71"
+  version "20.12.1"
+  sha256 "85a2d3aa6b6508040c8ae5658d723c2297999ee1025160aa15bce8497bb68b23"
 
   # github.com/pbek/QOwnNotes/ was verified as official when first introduced to the cask
   url "https://github.com/pbek/QOwnNotes/releases/download/v#{version}/QOwnNotes.dmg"
-  appcast "https://www.qownnotes.org/api/v1/last_release/QOwnNotes/macosx.json"
+  appcast "https://github.com/pbek/QOwnNotes/releases.atom"
   name "QOwnNotes"
+  desc "Plain-text file notepad and todo-list manager"
   homepage "https://www.qownnotes.org/"
 
   auto_updates true
