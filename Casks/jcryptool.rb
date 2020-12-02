69276766150110e011f8a7b63ca9c28cc9943641
Brigitte <brigitte@corecode.io>
2020-12-02 14:10:27 +0100

    Update jcryptool from 1.0.0,2020-11-01 to 1.0.1

--- Casks/jcryptool.rb
+++ Casks/jcryptool.rb
@@ -1,9 +1,9 @@
 cask "jcryptool" do
-  version "1.0.0,2020-11-01"
-  sha256 "4284466b7cee5e0d3f14a4b134d70dec197752b77c34fd4c38dd5d15033d51c2"
+  version "1.0.1"
+  sha256 "6e63d68b2d2fd4600368f6e325bfeb6b3fae56397c54d119f67accf41cc8268c"
 
   # github.com/jcryptool/core/ was verified as official when first introduced to the cask
-  url "https://github.com/jcryptool/core/releases/download/Weekly-Build--#{version.after_comma}/jcryptool-#{version.before_comma}-macosx.cocoa.x86_64.tar.gz"
+  url "https://github.com/jcryptool/core/releases/download/#{version}/JCrypTool-#{version}-macOS-64bit.tar.gz"
   appcast "https://github.com/jcryptool/core/releases.atom",
           must_contain: version.after_comma
   name "JCrypTool"
