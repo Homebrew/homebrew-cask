2645344c2178235921a825ea4ad7d26f37328dfd
suschizu <brigitte@corecode.io>
2020-12-02 15:37:02 +0100

    Update sauerbraten.rb

--- Casks/sauerbraten.rb
+++ Casks/sauerbraten.rb
@@ -5,7 +5,7 @@ cask "sauerbraten" do
   # sourceforge.net/sauerbraten/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/sauerbraten/sauerbraten/#{version.before_comma.dots_to_underscores}/sauerbraten_#{version.after_comma.dots_to_underscores}_macos.dmg"
   appcast "https://sourceforge.net/projects/sauerbraten/rss",
-          must_contain: version.dots_to_underscores
+          must_contain: version.after_comma.dots_to_underscores
   name "Cube 2: Sauerbraten"
   homepage "http://cubeengine.com/"
 
