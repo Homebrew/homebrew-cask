510c94683c4a66cf10be2375b7cf02134bfdfd55
Randall <ran.dall@icloud.com>
2020-12-02 08:32:18 -0800

    Update receipts from 1.9.9-340 to 1.9.10-360

--- Casks/receipts.rb
+++ Casks/receipts.rb
@@ -1,6 +1,6 @@
 cask "receipts" do
-  version "1.9.9-340"
-  sha256 "4d8bca2ffc4b45ae7e3deb1566ff838683bf6a9382d3533a4861d1d1e456d4c2"
+  version "1.9.10-360"
+  sha256 "e1895c47301107edb505d458a2d8a07503757b374c687576c583744e85dc0ce4"
 
   url "https://www.receipts-app.com/update/download/Receipts-#{version}.zip"
   appcast "https://www.receipts-app.com/updater.php"
