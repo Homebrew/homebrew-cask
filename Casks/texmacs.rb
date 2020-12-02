b27230c78ef39cbccc4fdd03705e82ada8421438
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-02 10:32:06 +0800

    texmacs: update sha256, add desc (#94033)

--- Casks/texmacs.rb
+++ Casks/texmacs.rb
@@ -1,10 +1,11 @@
 cask "texmacs" do
   version "1.99.16"
-  sha256 "50961fb519a2f663550bf5a6d963c425e150737a0603d77e59a3fef29a16bbce"
+  sha256 "ea194943e992d1b6335f6533527e65cad69ee096b2025398012656362cd4f04f"
 
   url "https://www.texmacs.org/Download/ftp/tmftp/macos/TeXmacs-#{version}.dmg"
   appcast "https://ftp.texmacs.org/TeXmacs/appcast/macos.xml"
   name "GNU TeXmacs"
+  desc "Scientific editing platform"
   homepage "https://www.texmacs.org/"
 
   app "TeXmacs.app"
