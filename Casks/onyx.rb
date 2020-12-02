e6fb77fdde4d28fa7403407511602960e3a77964
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-02 10:29:02 +0800

    Update onyx.rb

--- Casks/onyx.rb
+++ Casks/onyx.rb
@@ -14,9 +14,6 @@ cask "onyx" do
   elsif MacOS.version <= :mojave
     version "3.6.8"
     sha256 "d27529bc497b03c2486fcb8f0d3bfbb4e4a30d4abe25eddcd059ab47aaea6672"
-  elsif MacOS.version <= :catalina
-    version "3.8.7"
-    sha256 "e6fd409dbdb08197d7ba3bb34fd28712ea6320c9eb9cd04942f12c496c5e9c07"
   else
     version "3.8.7"
     sha256 "0dd8119ad3441c5130ca584ac90ad450c272aab8b577925561a2536da48d2d54"
