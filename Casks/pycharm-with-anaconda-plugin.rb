65ee1b59313485b92036e851b1a41ed1cc1cdc85
jcbot <jcb@leipert.io>
2020-12-02 09:23:32 +0100

    Update pycharm-with-anaconda-plugin to 2020.2.5,202.8194.22 (#94043)

--- Casks/pycharm-with-anaconda-plugin.rb
+++ Casks/pycharm-with-anaconda-plugin.rb
@@ -1,6 +1,6 @@
 cask "pycharm-with-anaconda-plugin" do
-  version "2020.2.4,202.8194.15"
-  sha256 "1af6b8d1289e06270afcd5ebccff5e433da7f5248b4e6f7f562e0bc97095b00b"
+  version "2020.2.5,202.8194.22"
+  sha256 "15a28bce1d4a4ba3ab60b95dfb4fda968533213b6882f702861fd97ec6ca0dfe"
 
   url "https://download.jetbrains.com/python/pycharm-professional-anaconda-#{version.before_comma}.dmg"
   appcast "https://data.services.jetbrains.com/products/releases?code=PCC&latest=true&type=release"
