f3e4972345dffb18fcf508031ec9a317bb2074bb
Brigitte <brigitte@corecode.io>
2020-12-02 14:19:50 +0100

    Update zoc from 8.01.3 to 8.01.4

--- Casks/zoc.rb
+++ Casks/zoc.rb
@@ -1,6 +1,6 @@
 cask "zoc" do
-  version "8.01.3"
-  sha256 "3a565f9535d09a38d8d012d58e5d7bd448017b412eb489737fc0db543f9d580e"
+  version "8.01.4"
+  sha256 "91bd30f56bab7626a023b879480aafec5f5d4237408cf21d0fc3b154c78ed26d"
 
   url "https://www.emtec.com/downloads/zoc/zoc#{version.no_dots}.dmg"
   appcast "https://www.emtec.com/downloads/zoc/zoc_changes.txt"
