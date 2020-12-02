6ed2ebcaad46cb12002bfe0e0fa616b79aa13fb2
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-02 08:39:34 -0800

    Update insomnia-designer from 2020.4.2 to 2020.5.0 (#94088)

--- Casks/insomnia-designer.rb
+++ Casks/insomnia-designer.rb
@@ -1,6 +1,6 @@
 cask "insomnia-designer" do
-  version "2020.4.2"
-  sha256 "91c5c0e93d2a0b7618fa6ea3184819a6b243878cce94d533d72e2d4033d723e9"
+  version "2020.5.0"
+  sha256 "e26b530a22f1a7fb3193230b8dfed0bda691cdbcde138a279934ccbe779ae56f"
 
   # github.com/Kong/insomnia/ was verified as official when first introduced to the cask
   url "https://github.com/Kong/insomnia/releases/download/designer%40#{version}/Insomnia.Designer-#{version}.dmg"
