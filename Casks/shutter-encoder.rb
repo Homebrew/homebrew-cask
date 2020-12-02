afc29cdd63f9d9634f626c194af199d18765d8bb
Brigitte <brigitte@corecode.io>
2020-12-02 12:59:58 +0100

    Update shutter-encoder from 14.3 to 14.4

--- Casks/shutter-encoder.rb
+++ Casks/shutter-encoder.rb
@@ -1,6 +1,6 @@
 cask "shutter-encoder" do
-  version "14.3"
-  sha256 "a24371fb5e9677422d59bb14b859052a400498e78608f022ada221c93b4ca149"
+  version "14.4"
+  sha256 "1e7107d399b9c40c30a8a16d8f283e4499a80c7f5ae35c4f5e4cae293e077be5"
 
   url "https://www.shutterencoder.com/Shutter%20Encoder%20(MAC%20Version%20#{version}).zip"
   appcast "https://www.shutterencoder.com/changelog.txt"
