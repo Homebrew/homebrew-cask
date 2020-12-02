00fa2850c78bb3656b4496e9ebb61f7a85934dd9
suschizu <brigitte@corecode.io>
2020-12-03 00:05:58 +0100

    Update flipper from 0.66.0 to 0.67.0 (#94064)

--- Casks/flipper.rb
+++ Casks/flipper.rb
@@ -1,9 +1,9 @@
 cask "flipper" do
-  version "0.66.0"
-  sha256 "fcbc12f5fa11b5fb41bcb1aec00af7e4db93692fb6d5554a46cb59cf5fbbeabc"
+  version "0.67.0"
+  sha256 "78395f1886fb0e56d13efdbfa9a4ab097bf02b77b61596216d0bfbbe5781e2da"
 
   # github.com/facebook/flipper/ was verified as official when first introduced to the cask
-  url "https://github.com/facebook/flipper/releases/download/v#{version}/Flipper-mac.zip"
+  url "https://github.com/facebook/flipper/releases/download/v#{version}/Flipper-mac.dmg"
   appcast "https://github.com/facebook/flipper/releases.atom"
   name "Facebook Flipper"
   desc "Desktop debugging platform for mobile developers"
