ed48841ade97a73b76c49f6e9397293346357042
singularitti <singularitti@outlook.com>
2020-12-02 11:37:39 -0500

    Update webplotdigitizer from 4.3 to 4.4

--- Casks/webplotdigitizer.rb
+++ Casks/webplotdigitizer.rb
@@ -1,6 +1,6 @@
 cask "webplotdigitizer" do
-  version "4.3"
-  sha256 "117866ef65bc7d1644159657cffc5c3e54040b8d454011eaf197375b084309a7"
+  version "4.4"
+  sha256 "e9ee0ba2b317b81205593a7d1688a165ba10806465308c505e0534eb28e9d0c3"
 
   url "https://automeris.io/downloads/WebPlotDigitizer-#{version}-darwin-x64.zip"
   appcast "https://github.com/ankitrohatgi/WebPlotDigitizer/releases.atom"
