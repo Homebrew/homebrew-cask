85f9836bd039fe04c6c42e1741006d923ff27ff5
suschizu <brigitte@corecode.io>
2020-12-03 00:05:42 +0100

    Update dosbox-x from 0.83.7,20201101004123 to 0.83.8,20201130192748 (#94067)

--- Casks/dosbox-x.rb
+++ Casks/dosbox-x.rb
@@ -1,9 +1,9 @@
 cask "dosbox-x" do
-  version "0.83.7,20201101004123"
-  sha256 "de8f365f6cb886f71899e1c869b70f968cc2e22529b953c467e80c0842fb5227"
+  version "0.83.8,20201130192748"
+  sha256 "3ad0277d0cd5121dfa8eda679881c2b6eb3f6419fb17f1b2b42dc5e854bdd588"
 
   # github.com/joncampbell123/dosbox-x/ was verified as official when first introduced to the cask
-  url "https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v#{version.before_comma}/dosbox-x-macosx-x64-#{version.after_comma}.zip"
+  url "https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v#{version.before_comma}/dosbox-x-macosx-x86_64-#{version.after_comma}.zip"
   appcast "https://github.com/joncampbell123/dosbox-x/releases.atom"
   name "DOSBox-X"
   desc "Fork of the DOSBox project"
