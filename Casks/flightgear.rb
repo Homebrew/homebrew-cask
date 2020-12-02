04f4489a2761c36b6de51a6e6664b51b881d805f
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-02 15:03:09 -0800

    Update flightgear from 2020.3.3 to 2020.3.4 (#94017)

--- Casks/flightgear.rb
+++ Casks/flightgear.rb
@@ -1,6 +1,6 @@
 cask "flightgear" do
-  version "2020.3.3"
-  sha256 "9a925ce6f0aac7d1e1ffe455603ebc1b42d52ba266c8fac1f9e5ff23a66280d1"
+  version "2020.3.4"
+  sha256 "cf70d77966956f218a477753e559f4111f05f2b0648822f38f30e6241810d0a2"
 
   # sourceforge.net/flightgear/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/flightgear/FlightGear-#{version}.dmg"
