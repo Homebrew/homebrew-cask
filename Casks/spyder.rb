b9791e1e7bfd0d5d5b3cda6c4e089590a78530c8
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-02 23:05:36 +0000

    Add spyder version 4.2.0 (#93967)

---
+++ Casks/spyder.rb
@@ -0,0 +1,13 @@
+cask "spyder" do
+  version "4.2.0"
+  sha256 "4c92ca2e93498a93d14c49e8c46b61220d3cd99c003ffaf176e6feaa3d70887a"
+
+  url "https://github.com/spyder-ide/spyder/releases/download/v#{version}/Spyder.dmg",
+      verified: "https://github.com/spyder-ide/spyder/"
+  appcast "https://github.com/spyder-ide/spyder/releases.atom"
+  name "Spyder"
+  desc "Scientific Python IDE"
+  homepage "https://www.spyder-ide.org/"
+
+  app "Spyder.app"
+end
