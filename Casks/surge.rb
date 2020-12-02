bc38d285aa2d2ea0cfc3e9df91768b1e970dd939
octoer <37321851+octoer@users.noreply.github.com>
2020-12-02 12:55:26 +0800

    update surge from 4.0.0 to 4.0.1 (#94038)
    * update surge
    
    * Update surge.rb
    
    * Update surge.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/surge.rb
+++ Casks/surge.rb
@@ -1,10 +1,11 @@
 cask "surge" do
-  version "4.0.0-1191-d8140b0084223fd3fc4335e4414c0884"
-  sha256 "daab57fcc757d8bcc8715ae7a21e06fc555d568775782c772b9ccb60e3ca5a09"
+  version "4.0.1-1207-ee7bea1b244950c82a6f90e060fa2d89"
+  sha256 "e143209bba17bd66a3dfd9eabd68059be5745ad31919f76d6c0ef0ce28e5e73d"
 
   url "https://www.nssurge.com/mac/v#{version.major}/Surge-#{version}.zip"
   appcast "https://www.nssurge.com/mac/v#{version.major}/appcast-signed.xml"
   name "Surge"
+  desc "Network toolbox"
   homepage "https://nssurge.com/"
 
   auto_updates true
