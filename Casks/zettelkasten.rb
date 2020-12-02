a4dbd31211b13ad8cad8c3e3ae43a1a88197745d
suschizu <brigitte@corecode.io>
2020-12-02 14:05:57 +0100

    Update zettelkasten (#94050)

--- Casks/zettelkasten.rb
+++ Casks/zettelkasten.rb
@@ -1,12 +1,17 @@
 cask "zettelkasten" do
   version "3.3.1"
-  sha256 "1f12515ac2943f0fc8083cf737f1216a35c2ee9249ecad2be02b99f7ebda8964"
+  sha256 "6e587e869a8c66d4feb7d6bb2e696369dc052f1df0d4e5644d170bbc8f58d741"
 
-  # github.com/sjPlot/Zettelkasten/ was verified as official when first introduced to the cask
-  url "https://github.com/sjPlot/Zettelkasten/releases/download/v#{version}/Zettelkasten_#{version}_Mac-Java9+.dmg"
-  appcast "https://github.com/sjPlot/Zettelkasten/releases.atom"
-  name "zettelkasten"
+  # github.com/Zettelkasten-Team/Zettelkasten/ was verified as official when first introduced to the cask
+  url "https://github.com/Zettelkasten-Team/Zettelkasten/releases/download/v#{version}/Zettelkasten_#{version}_Mac-Java8.zip"
+  appcast "https://github.com/Zettelkasten-Team/Zettelkasten/releases.atom"
+  name "Zettelkasten"
+  desc "Note box according to Luhmann"
   homepage "http://zettelkasten.danielluedecke.de/"
 
   app "Zettelkasten.app"
+
+  caveats do
+    depends_on_java "8"
+  end
 end
