a5ddd2b686d91027df84969bfce8e7ba681f8e4b
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-02 08:56:37 +0800

    Update boxcryptor.rb

--- Casks/boxcryptor.rb
+++ Casks/boxcryptor.rb
@@ -1,6 +1,6 @@
 cask "boxcryptor" do
-  version "2.38.1086"
-  sha256 "6a8e68f8152c94bc2a5ea8c8a4a475994ebb508ee29f9d99dcd9397d2ce567a8"
+  version "2.38.1090"
+  sha256 "974d097017bdc1377a62fa4e9e51033636198659fc9353372233cde0e5094285"
 
   url "https://downloads.boxcryptor.com/boxcryptor/mac/Boxcryptor_v#{version}_Installer.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.boxcryptor.com/l/download-macosx"
