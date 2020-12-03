a23de6fdad5bcfb69407ec736e3c08a7928883d4
CoreCode <core-code@users.noreply.github.com>
2020-12-03 20:53:27 +0100

    Update recordit.rb

--- Casks/recordit.rb
+++ Casks/recordit.rb
@@ -2,7 +2,7 @@ cask "recordit" do
   version "1.6.10"
   sha256 "ff5dab671541e8ed0493bcc0c20da357d4df21a65371bfc156837b36888b2fb7"
 
-  url "https://recordit.co/latest"
+  url "https://recordit.co/latest-osx"
   appcast "https://rink.hockeyapp.net/api/2/apps/5fcda0b48f1dcf0c938b289b9ab57790"
   name "RecordIt"
   homepage "https://recordit.co/"
