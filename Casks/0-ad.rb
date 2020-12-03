52c8dfcb6d4491bb1ebd00bd22311ab38c16231d
Vítor Galvão <info@vitorgalvao.com>
2020-12-03 03:13:56 +0000

    0-ad.rb: add verified parameter (#94161)

--- Casks/0-ad.rb
+++ Casks/0-ad.rb
@@ -2,8 +2,8 @@ cask "0-ad" do
   version "0.0.23b-alpha"
   sha256 "5fae1331bb1778bf16e49460fb850270b6cd76bee6483078ff27582e71ab263f"
 
-  # wildfiregames.com/ was verified as official when first introduced to the cask
-  url "https://releases.wildfiregames.com/0ad-#{version}-osx64.dmg"
+  url "https://releases.wildfiregames.com/0ad-#{version}-osx64.dmg",
+      verified: "releases.wildfiregames.com/"
   appcast "https://play0ad.com/download/mac/"
   name "0 A.D."
   homepage "https://play0ad.com/"
