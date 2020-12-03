451cd3c86a3ca60c48a612407dc06cf9d46d8a24
CoreCode <core-code@users.noreply.github.com>
2020-12-03 12:20:21 +0100

    Update lehreroffice from 2020.9.0 to 2020.10.0 (#94181)

--- Casks/lehreroffice.rb
+++ Casks/lehreroffice.rb
@@ -1,6 +1,6 @@
 cask "lehreroffice" do
-  version "2020.9.0"
-  sha256 "b59d8cf6bd9263bff612b31c4fcb5624342a3f463954d515b41a6f3746d0bf52"
+  version "2020.10.0"
+  sha256 "f732e3c1a0d1484bc6d7db12fcbce7753b11e2a8efbf3c7571f88b6c7164e87c"
 
   url "https://www.lehreroffice.ch/lo/dateien/easy/lo_desktop_macos.dmg"
   appcast "https://www.lehreroffice.ch/services/update/getcurrentversion.php?app=Desktop"
