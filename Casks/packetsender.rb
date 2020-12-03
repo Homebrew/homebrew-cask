f64f39c2d91821a32f48a0fb8e7ed09ef655517c
Vitaly <hellvesper@gmail.com>
2020-12-03 05:17:46 +0200

    packetsender BigSur specific version added (#94129)
    * packetsender big sur version added
    
    * Update packetsender.rb
    
    * Update packetsender.rb
    
    * Update packetsender.rb
    
    * Update packetsender.rb
    
    Co-authored-by: Vitalii <>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/packetsender.rb
+++ Casks/packetsender.rb
@@ -1,9 +1,18 @@
 cask "packetsender" do
-  version "7.0.5"
-  sha256 "30281225ee4e2baf3ca365123832c2931342aaf1011038268eab13ec73375fad"
+  if MacOS.version <= :catalina
+    version "7.0.5"
+    sha256 "30281225ee4e2baf3ca365123832c2931342aaf1011038268eab13ec73375fad"
 
-  # github.com/dannagle/PacketSender/ was verified as official when first introduced to the cask
-  url "https://github.com/dannagle/PacketSender/releases/download/v#{version}/PacketSender_v#{version}.dmg"
+    url "https://github.com/dannagle/PacketSender/releases/download/v#{version}/PacketSender_v#{version}.dmg",
+        verified: "github.com/dannagle/PacketSender/"
+  else
+    version "7.1.0,7.0.5"
+    sha256 "a2ab110a5d498389de5b0fd73ca7a63349f1b79235f4acda1da7c2e5322f0d7f"
+
+    url "https://github.com/dannagle/PacketSender/releases/download/v#{version.after_comma}/PacketSender_BigSur_v#{version.before_comma}.dmg",
+        verified: "github.com/dannagle/PacketSender/"
+  end
+
   appcast "https://github.com/dannagle/PacketSender/releases.atom"
   name "Packet Sender"
   desc "Network utility for sending / receiving TCP, UDP, SSL"
