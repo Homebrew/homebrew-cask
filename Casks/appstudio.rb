0a04116542126cbe8edfc0828baa17a2aaa3526a
suschizu <brigitte@corecode.io>
2020-12-03 01:35:26 +0100

    Update appstudio from latest to 8.4.2.0 (#94056)
    * Update appstudio from latest to 8.4.2.0
    
    * Update appstudio.rb
    
    * Update appstudio.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/appstudio.rb
+++ Casks/appstudio.rb
@@ -1,10 +1,11 @@
 cask "appstudio" do
-  version :latest
-  sha256 :no_check
+  version "8.4.2.0"
+  sha256 "f6500de4c4d34625b4892397070d4f752a407ab213b8282554140742024f2719"
 
   # appstudio.us-east-1.linodeobjects.com/ was verified as official when first introduced to the cask
-  url "https://appstudio.us-east-1.linodeobjects.com/AppStudio.dmg"
+  url "https://appstudio.us-east-1.linodeobjects.com/AppStudio#{version.major}.dmg"
   name "NSB/AppStudio"
+  desc "Development environment"
   homepage "https://www.nsbasic.com/"
 
   app "AppStudio.app"
