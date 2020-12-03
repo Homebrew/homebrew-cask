8116f4fd80394b7d40ffbeeb8c800887915546d6
Vítor Galvão <info@vitorgalvao.com>
2020-12-03 04:10:14 +0000

    spyder.rb: remove verified parameter

--- Casks/spyder.rb
+++ Casks/spyder.rb
@@ -3,7 +3,7 @@ cask "spyder" do
   sha256 "4c92ca2e93498a93d14c49e8c46b61220d3cd99c003ffaf176e6feaa3d70887a"
 
   url "https://github.com/spyder-ide/spyder/releases/download/v#{version}/Spyder.dmg",
-      verified: "https://github.com/spyder-ide/spyder/"
+      verified: "github.com/spyder-ide/spyder/"
   appcast "https://github.com/spyder-ide/spyder/releases.atom"
   name "Spyder"
   desc "Scientific Python IDE"
