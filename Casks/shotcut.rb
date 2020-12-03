6f6f01b5384551f8abc057aab8503963ee1b18cb
Vítor Galvão <info@vitorgalvao.com>
2020-12-03 03:02:38 +0000

    shotcut.rb: add verified comment

--- Casks/shotcut.rb
+++ Casks/shotcut.rb
@@ -2,8 +2,8 @@ cask "shotcut" do
   version "20.11.28"
   sha256 "a2d72ff4302ef5d08b379d605907dc061ed10c53a6478458c2098be0712456cc"
 
-  # github.com/mltframework/shotcut/ was verified as official when first introduced to the cask
-  url "https://github.com/mltframework/shotcut/releases/download/v#{version}/shotcut-macos-signed-#{version.no_dots}.dmg"
+  url "https://github.com/mltframework/shotcut/releases/download/v#{version}/shotcut-macos-signed-#{version.no_dots}.dmg",
+      verified: "github.com/mltframework/shotcut/"
   appcast "https://github.com/mltframework/shotcut/releases.atom"
   name "Shotcut"
   desc "Cross-platform and open-source video editor"
