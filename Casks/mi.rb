0d427e0638af3e0a11cbbe874d312f518d14eb58
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-03 11:17:29 -0800

    Update mi from 3.0.4 to 3.5 (#94215)

--- Casks/mi.rb
+++ Casks/mi.rb
@@ -1,6 +1,6 @@
 cask "mi" do
-  version "3.0.4"
-  sha256 "f6e5d1a6845277a9bb5b994df713f9523a0ce7e9071ed85a3a88917adee281e7"
+  version "3.5"
+  sha256 "9df5cae930b7b59b0022befdb69abcaff616bd4034f6612a24f3139fe01499a9"
 
   url "https://www.mimikaki.net/download/mi#{version}.dmg"
   appcast "https://www.mimikaki.net/download/appcast.xml"
