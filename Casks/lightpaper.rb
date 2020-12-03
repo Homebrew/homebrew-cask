ae700729aa3c6e8e8664a3223427427d3a91264f
Vítor Galvão <info@vitorgalvao.com>
2020-12-03 03:00:07 +0000

    lightpaper: add verified parameter

--- Casks/lightpaper.rb
+++ Casks/lightpaper.rb
@@ -2,8 +2,8 @@ cask "lightpaper" do
   version "1.4.2"
   sha256 "7e1f7f8304bd2b299e7973a34d55e9c5b6b57b9987f51c3e2200897150c06923"
 
-  # hockeyapp.net/api/2/apps/789cfa8846464727ae0fdb176ec8d3c8 was verified as official when first introduced to the cask
-  url "https://rink.hockeyapp.net/api/2/apps/789cfa8846464727ae0fdb176ec8d3c8?format=zip"
+  url "https://rink.hockeyapp.net/api/2/apps/789cfa8846464727ae0fdb176ec8d3c8?format=zip",
+      verified: "hockeyapp.net/api/2/apps/789cfa8846464727ae0fdb176ec8d3c8"
   appcast "https://rink.hockeyapp.net/api/2/apps/789cfa8846464727ae0fdb176ec8d3c8"
   name "LightPaper"
   homepage "https://getlightpaper.com/"
