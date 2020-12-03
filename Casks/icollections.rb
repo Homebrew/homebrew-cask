1d5bbaaf9a3970ebd42d1bd6e634fcb737a978ed
Julian Mayer <julian@corecode.io>
2020-12-03 16:11:12 +0100

    Update icollections from 6.7.2,67201 to 6.7.2,67210

--- Casks/icollections.rb
+++ Casks/icollections.rb
@@ -1,6 +1,6 @@
 cask "icollections" do
-  version "6.7.2,67201"
-  sha256 "0817e68a6599fbf65344eafdd5693233aa9f645f7e0b0cf4a58dcc454b7c1695"
+  version "6.7.2,67210"
+  sha256 "e173229182d69ad4bbdbb3f134af3d0d48f131528520a4e8217a9291fe85364a"
 
   url "https://naarakstudio.com/download/iCollections.dmg"
   appcast "https://naarakstudio.com/download/icollections/update.ver",
