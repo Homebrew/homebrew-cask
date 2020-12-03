f184bf20b704f88ec823716bc1e11177b8ac7430
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-03 12:36:43 -0800

    Update shift from 5.0.90 to 6.0.47 (#94231)

--- Casks/shift.rb
+++ Casks/shift.rb
@@ -1,6 +1,6 @@
 cask "shift" do
-  version "5.0.90"
-  sha256 "d97aba0ff5ed5acb925ec1a9473801f7fdd1c289ff981a975d05671e62a79a94"
+  version "6.0.47"
+  sha256 "3ac1d9218fab93d10ab8ffaf4bb517890125b8a10c8cdbba7f21d0c57a28f1f8"
 
   url "https://update.tryshift.com/download/version/#{version}/osx_64"
   appcast "https://tryshift.com/download/?platform=mac"
