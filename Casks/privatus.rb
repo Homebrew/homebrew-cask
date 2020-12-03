c31d123f980cd0f6bca1b20bae4b3a20ca301d86
Reno <25192197+singularitti@users.noreply.github.com>
2020-12-03 09:45:32 -0500

    Add app "privatus" 6.3 (#94032)

---
+++ Casks/privatus.rb
@@ -0,0 +1,20 @@
+cask "privatus" do
+  version "6.3"
+  sha256 "a43405c336bdb86845ed21ff11145802bf7337046cc6c3c1d7cad8687096f1b3"
+
+  url "https://sweetpproductions.com/products/privatus#{version.major}/Privatus#{version.major}.dmg",
+      verified: "sweetpproductions.com/products/"
+  appcast "https://sweetpproductions.com/products/privatus#{version.major}/appcast.xml"
+  name "Privatus"
+  desc "Automated online privacy protection"
+  homepage "https://privatusapp.com/"
+
+  depends_on macos: ">= :mojave"
+
+  app "Privatus.app"
+
+  zap trash: [
+    "~/Library/Containers/com.sweetpproductions.Privatus#{version.major}",
+    "~/Library/Preferences/com.sweetpproductions.Privatus#{version.major}.plist",
+  ]
+end
