2b00bd85ebf3e814ee2e1d5c458728fbe071a48a
Reno <25192197+singularitti@users.noreply.github.com>
2020-12-02 19:33:30 -0500

    Update webplotdigitizer from 4.3 to 4.4 (#94100)
    * Update webplotdigitizer from 4.3 to 4.4
    
    * Update webplotdigitizer.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/webplotdigitizer.rb
+++ Casks/webplotdigitizer.rb
@@ -1,10 +1,11 @@
 cask "webplotdigitizer" do
-  version "4.3"
-  sha256 "117866ef65bc7d1644159657cffc5c3e54040b8d454011eaf197375b084309a7"
+  version "4.4"
+  sha256 "e9ee0ba2b317b81205593a7d1688a165ba10806465308c505e0534eb28e9d0c3"
 
   url "https://automeris.io/downloads/WebPlotDigitizer-#{version}-darwin-x64.zip"
   appcast "https://github.com/ankitrohatgi/WebPlotDigitizer/releases.atom"
   name "WebPlotDigitizer"
+  desc "Extract numerical data from plot images"
   homepage "https://automeris.io/WebPlotDigitizer/"
 
   app "WebPlotDigitizer-#{version}-darwin-x64/WebPlotDigitizer-#{version}.app"
