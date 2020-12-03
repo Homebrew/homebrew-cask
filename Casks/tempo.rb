923a5e6a1b671ca64c93ed30d1ae815d0a998005
Randall <ran.dall@icloud.com>
2020-12-03 09:04:22 -0800

    Update tempo from 5.4.1 to 5.5.0

--- Casks/tempo.rb
+++ Casks/tempo.rb
@@ -1,6 +1,6 @@
 cask "tempo" do
-  version "5.4.1"
-  sha256 "c3619f940e0895af8d947483dfa232fcad48c8d7e3fdb129c67b3c53ca8617e4"
+  version "5.5.0"
+  sha256 "3522065815434fb1f8dd6f48e0203b9e9fa199ef37f55b88c59249e59b48f332"
 
   url "https://download.yourtempo.co/release/Tempo-#{version}.dmg"
   appcast "https://download.yourtempo.co/release/latest-mac.yml"
