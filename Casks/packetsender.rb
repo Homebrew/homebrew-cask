f9103b5fb031f6812bb37aaae3ec7cbd33374e9c
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-03 08:24:21 +0800

    Update packetsender.rb

--- Casks/packetsender.rb
+++ Casks/packetsender.rb
@@ -1,17 +1,16 @@
 cask "packetsender" do
   version "7.0.5"
 
-  if MacOS.version == :big_sur
-    sha256 "a2ab110a5d498389de5b0fd73ca7a63349f1b79235f4acda1da7c2e5322f0d7f"
-
-    # github.com/dannagle/PacketSender/ was verified as official when first introduced to the cask
-    url "https://github.com/dannagle/PacketSender/releases/download/v#{version}/PacketSender_BigSur_v7.1.0.dmg"
-
-  else
+  if MacOS.version <= :catalina
     sha256 "30281225ee4e2baf3ca365123832c2931342aaf1011038268eab13ec73375fad"
 
     # github.com/dannagle/PacketSender/ was verified as official when first introduced to the cask
     url "https://github.com/dannagle/PacketSender/releases/download/v#{version}/PacketSender_v#{version}.dmg"
+  else
+    sha256 "a2ab110a5d498389de5b0fd73ca7a63349f1b79235f4acda1da7c2e5322f0d7f"
+
+    # github.com/dannagle/PacketSender/ was verified as official when first introduced to the cask
+    url "https://github.com/dannagle/PacketSender/releases/download/v#{version}/PacketSender_BigSur_v7.1.0.dmg"
   end
 
   appcast "https://github.com/dannagle/PacketSender/releases.atom"
