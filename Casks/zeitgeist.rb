18904123331812d5488897ea1ff7ffd2419f022a
Khushraj Rathod <khushraj.rathod@gmail.com>
2020-12-03 23:33:06 +0530

    Add Zeitgeist.app v2.0.1

---
+++ Casks/zeitgeist.rb
@@ -0,0 +1,20 @@
+cask "zeitgeist" do
+  version "2.0.1"
+  sha256 "96aa56490747b12b51e8c28e6243f75b2150b1b8d3ac26572c35c4a6793e9c6f"
+
+  url "https://github.com/daneden/zeitgeist/releases/download/v#{version}/Zeitgeist.app.zip",
+      verified: "https://github.com/daneden/zeitgeist"
+  appcast "https://github.com/daneden/zeitgeist/releases.atom"
+  name "Zeitgeist"
+  desc "Keep an eye on your Vercel deployments"
+  homepage "https://zeitgeist.daneden.me/"
+
+  depends_on macos: ">= :big_sur"
+
+  app "Zeitgeist.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/me.daneden.Zeitgeist",
+    "~/Library/Containers/me.daneden.Zeitgeist",
+  ]
+end
