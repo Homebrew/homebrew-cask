966a19681d71ab028014145744e4b8f6215e1066
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-03 09:32:31 +0800

    Update videofusion.rb

--- Casks/videofusion.rb
+++ Casks/videofusion.rb
@@ -1,16 +1,15 @@
 cask "videofusion" do
-  version "1.0.1"
+  version "1.0.1.0"
   sha256 "106328304796a77ddf2180af014e3834c695cef09226a0f412e811f635e66a25"
 
-  # lf3-faceucdn-tos.pstatp.com was verified as official when first introduced to the cask
-  url "https://lf3-faceucdn-tos.pstatp.com/obj/faceu-packages/Jianying_Pro_#{version.dots_to_underscores}_0.pkg",
-      verified: "lf3-faceucdn-tos.pstatp.com"
+  url "https://lf3-faceucdn-tos.pstatp.com/obj/faceu-packages/Jianying_Pro_#{version.dots_to_underscores}.pkg",
+      verified: "https://lf3-faceucdn-tos.pstatp.com/obj/faceu-packages/"
   name "VideoFusion"
   name "剪映专业版"
   desc "Video editor"
   homepage "https://lv.ulikecam.com/"
 
-  pkg "Jianying_Pro_#{version.dots_to_underscores}_0.pkg"
+  pkg "Jianying_Pro_#{version.dots_to_underscores}.pkg"
 
   uninstall pkgutil: [
     "com.lemon.lvpro",
