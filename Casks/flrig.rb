d9fa351e6a47eaa921a55bb314c5422b149148f7
Randall <ran.dall@icloud.com>
2020-12-03 08:52:03 -0800

    Update flrig from 1.3.52 to 1.3.53

--- Casks/flrig.rb
+++ Casks/flrig.rb
@@ -1,6 +1,6 @@
 cask "flrig" do
-  version "1.3.52"
-  sha256 "2cb4e59e92f27fb32c9514ad2cc51f5430d9fa1cfb61023ba8a330dbaf176b89"
+  version "1.3.53"
+  sha256 "b39c156d4cdbfe0c26fbb2693957f8856c6b8ddfbc9ba88f2b58c1337d567815"
 
   url "https://downloads.sourceforge.net/fldigi/fldigi/flrig-#{version.before_comma}_x86_64.dmg"
   appcast "https://sourceforge.net/projects/fldigi/rss?path=/flrig"
