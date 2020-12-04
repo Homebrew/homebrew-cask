4e7962005b09266aea8558f06cbcb8236e3fa099
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-04 08:44:18 +0800

    Update recordit.rb

--- Casks/recordit.rb
+++ Casks/recordit.rb
@@ -2,9 +2,11 @@ cask "recordit" do
   version "1.6.10"
   sha256 "c6dd0d00665b6b8c143b0e1ae65f6404b4d9015a57dde141a1904b2a29776e45"
 
-  url "https://recordit.co/latest-osx"
+  url "https://recordit-co.s3.amazonaws.com/RecordIt.zip",
+      verified: "recordit-co.s3.amazonaws.com/"
   appcast "https://rink.hockeyapp.net/api/2/apps/5fcda0b48f1dcf0c938b289b9ab57790"
   name "RecordIt"
+  desc "Screencastign software"
   homepage "https://recordit.co/"
 
   app "RecordIt.app"
