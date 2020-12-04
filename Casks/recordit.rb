b66c6b6517555d32b629d893143104ef8d208538
CoreCode <core-code@users.noreply.github.com>
2020-12-04 01:53:04 +0100

    Update recordit.rb (#94241)
    * Update recordit.rb
    
    * Update recordit.rb
    
    * Update recordit.rb
    
    * Update recordit.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/recordit.rb
+++ Casks/recordit.rb
@@ -1,10 +1,12 @@
 cask "recordit" do
   version "1.6.10"
-  sha256 "ff5dab671541e8ed0493bcc0c20da357d4df21a65371bfc156837b36888b2fb7"
+  sha256 "c6dd0d00665b6b8c143b0e1ae65f6404b4d9015a57dde141a1904b2a29776e45"
 
-  url "https://recordit.co/latest"
+  url "https://recordit-co.s3.amazonaws.com/RecordIt.zip",
+      verified: "recordit-co.s3.amazonaws.com/"
   appcast "https://rink.hockeyapp.net/api/2/apps/5fcda0b48f1dcf0c938b289b9ab57790"
   name "RecordIt"
+  desc "Screencasting software"
   homepage "https://recordit.co/"
 
   app "RecordIt.app"
