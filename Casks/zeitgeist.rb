baecd8f1aa704ed43ef5a41376b2b5277d8ab8a2
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-04 09:12:52 +0800

    Update zeitgeist.rb

--- Casks/zeitgeist.rb
+++ Casks/zeitgeist.rb
@@ -3,7 +3,7 @@ cask "zeitgeist" do
   sha256 "96aa56490747b12b51e8c28e6243f75b2150b1b8d3ac26572c35c4a6793e9c6f"
 
   url "https://github.com/daneden/zeitgeist/releases/download/v#{version}/Zeitgeist.app.zip",
-      verified: "https://github.com/daneden/zeitgeist"
+      verified: "github.com/daneden/zeitgeist/"
   appcast "https://github.com/daneden/zeitgeist/releases.atom"
   name "Zeitgeist"
   desc "Keep an eye on your Vercel deployments"
