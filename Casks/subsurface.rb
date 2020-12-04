73bcf646f6c7f0bfed088934e10ba8c3b4437501
Brigitte <brigitte@corecode.io>
2020-12-04 17:43:52 +0100

    Update subsurface

--- Casks/subsurface.rb
+++ Casks/subsurface.rb
@@ -1,8 +1,14 @@
 cask "subsurface" do
   version "4.9.10"
-  sha256 "32d8ded0a937689183a8d915acab44330cbb26618ff12abc62be5ef42eefd9c4"
 
-  url "https://subsurface-divelog.org/downloads/Subsurface-#{version}.dmg"
+  if MacOS.version <= :mojave
+    sha256 "34274051fe008486eccd01b31fcfdc793724276b505438184b35393ffd1199fe"
+    url "https://subsurface-divelog.org/downloads/Subsurface-#{version}-10.13+14.dmg"
+  else
+    sha256 "32d8ded0a937689183a8d915acab44330cbb26618ff12abc62be5ef42eefd9c4"
+    url "https://subsurface-divelog.org/downloads/Subsurface-#{version}-10.15+11.0.dmg"
+  end
+
   appcast "https://subsurface-divelog.org/download/"
   name "Subsurface"
   homepage "https://subsurface-divelog.org/"
