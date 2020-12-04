9185605fd2e8782ff0f8a5c1b63385991f55273f
Vítor Galvão <info@vitorgalvao.com>
2020-12-04 23:12:38 +0000

    Update unity-appletv-support-for-editor.rb

--- Casks/unity-appletv-support-for-editor.rb
+++ Casks/unity-appletv-support-for-editor.rb
@@ -2,8 +2,8 @@ cask "unity-appletv-support-for-editor" do
   version "2020.1.15f1,97d0ae02d19d"
   sha256 "0a07791d5e6b4c0d0d41567a218c801550d414b47c088dfb8a88d77d8fc16b9d"
 
-  # download.unity3d.com/download_unity/ was verified as official when first introduced to the cask
-  url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-AppleTV-Support-for-Editor-#{version.before_comma}.pkg"
+  url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-AppleTV-Support-for-Editor-#{version.before_comma}.pkg",
+      verified: "download.unity3d.com/download_unity/"
   appcast "https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json"
   name "Unity AppleTV Build Support"
   desc "AppleTV target support for Unity"
