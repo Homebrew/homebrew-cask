e4cfe075eb4cb0ab5d1bd2d7beb87c69c9e3c725
Randall <ran.dall@icloud.com>
2020-12-04 08:44:36 -0800

    Update scroll-reverser from 1.7.6 to 1.8

--- Casks/scroll-reverser.rb
+++ Casks/scroll-reverser.rb
@@ -1,6 +1,6 @@
 cask "scroll-reverser" do
-  version "1.7.6"
-  sha256 "0df563089470152d2cbf4cfd4e21f3be0dd45b0c334882bbf6dbbe2002afd4b8"
+  version "1.8"
+  sha256 "57914a55c6ffc1687b2a5d2c559cd065dff90994814ce10741cb72c4b5ef1682"
 
   url "https://pilotmoon.com/downloads/ScrollReverser-#{version}.zip"
   appcast "https://softwareupdate.pilotmoon.com/update/scrollreverser/appcast.xml"
