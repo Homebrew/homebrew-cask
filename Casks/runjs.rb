f47089cb08d298e9660eccce4de26eb09ff15efd
Dario Vladović <d.vladimyr@gmail.com>
2020-12-04 01:31:41 +0100

    Update runjs (#94239)
    * Update runjs
    
    Add `auto_updates` & `zap` stanzas.
    
    * Use verified url param

--- Casks/runjs.rb
+++ Casks/runjs.rb
@@ -2,12 +2,21 @@ cask "runjs" do
   version "1.12.2"
   sha256 "3ad31592e79c97c5f78b057b43a5b57380abd23b82d353a055bb0d7b34f32eea"
 
-  # github.com/lukehaas/runjs/ was verified as official when first introduced to the cask
-  url "https://github.com/lukehaas/runjs/releases/download/v#{version}/RunJS-#{version}.dmg"
+  url "https://github.com/lukehaas/runjs/releases/download/v#{version}/RunJS-#{version}.dmg",
+      verified: "github.com/lukehaas/runjs/"
   appcast "https://github.com/lukehaas/runjs/releases.atom"
   name "RunJS"
   desc "JavaScript playground that auto-evaluates as code is typed"
   homepage "https://projects.lukehaas.me/runjs/"
 
+  auto_updates true
+
   app "RunJS.app"
+
+  zap trash: [
+    "~/Library/Application Support/runjs",
+    "~/Library/Logs/RunJS",
+    "~/Library/Preferences/me.lukehaas.runjs.plist",
+    "~/Library/Saved Application State/me.lukehaas.runjs.savedState",
+  ]
 end
