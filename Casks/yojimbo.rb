bc19a58a3df83b1038425438b60b628a2f09f927
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-04 09:55:17 -0800

    Update yojimbo from 4.6 to 4.6.1 (#94314)

--- Casks/yojimbo.rb
+++ Casks/yojimbo.rb
@@ -1,6 +1,6 @@
 cask "yojimbo" do
-  version "4.6"
-  sha256 "5c7584794d039bdfca650bf374567cc47b31482b1032d690fd76ff8aa5c43a21"
+  version "4.6.1"
+  sha256 "c2e2727e12f332b6c0f1885b4cccfe531f2c9043be0d0f17c47f5fe8b9794e12"
 
   # s3.amazonaws.com/BBSW-download/ was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/BBSW-download/Yojimbo_#{version}.dmg"
