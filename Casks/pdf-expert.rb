131d159d51885384d1ba8971102102b61789a56d
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-05 07:02:14 +0800

    Update pdf-expert.rb

--- Casks/pdf-expert.rb
+++ Casks/pdf-expert.rb
@@ -2,15 +2,15 @@ cask "pdf-expert" do
   version "2.5.15,734"
   sha256 "c793faede34b8e553cad5bfc04cd4e4d709b791ca19e6f8cd514a7998bdbd0b3"
 
-  # d1ke680phyeohy.cloudfront.net/ was verified as official when first introduced to the cask
-  url "https://d1ke680phyeohy.cloudfront.net/versions/#{version.after_comma}/PDFExpert.dmg"
+  url "https://d1ke680phyeohy.cloudfront.net/versions/#{version.after_comma}/PDFExpert.dmg",
+      verified: "d1ke680phyeohy.cloudfront.net/versions/"
   appcast "https://d1ke680phyeohy.cloudfront.net/release/appcast.xml"
   name "PDF Expert"
   desc "PDF reader, editor and annotator"
   homepage "https://pdfexpert.com/"
 
   auto_updates true
-  conflicts_with cask: "pdf-expert-beta"
+  conflicts_with cask: "homebrew/cask-versions/pdf-expert-beta"
   depends_on macos: ">= :sierra"
 
   app "PDF Expert.app"
