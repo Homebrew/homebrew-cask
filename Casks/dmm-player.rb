470a74b7c8d17965c92d7e4ab5eca7512924a609
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-04 09:57:27 -0800

    Update dmm-player from 2.1.2 to 2.1.3 (#94310)

--- Casks/dmm-player.rb
+++ Casks/dmm-player.rb
@@ -1,6 +1,6 @@
 cask "dmm-player" do
-  version "2.1.2"
-  sha256 "001f0f27895032ddd8e1449823f088cffdbea73b82c6931d5750052affbc35d8"
+  version "2.1.3"
+  sha256 "9152480dbb4cf6d629cf0d18f946672a88a54145116d8e098f7938752daf4380"
 
   url "https://portalapp.dmm.com/dmmplayerv#{version.major}/dmm/#{version.dots_to_underscores}/DMMPlayerV#{version.major}Installer_#{version.dots_to_underscores}.pkg"
   name "DMM Player"
