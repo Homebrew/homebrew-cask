eacf89efed5df64dd8e355877ea9f5d1fa3b3fb4
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-04 09:11:38 +0800

    Update aqua-data-studio.rb

--- Casks/aqua-data-studio.rb
+++ Casks/aqua-data-studio.rb
@@ -2,7 +2,7 @@ cask "aqua-data-studio" do
   version "21.0.3"
   sha256 "2a092e86eed401d632da60632db42049d65e4255486e97b6e7c31122b130b842"
 
-  url "http://downloads.aquafold.com/v#{version}/osx/ads-osx-#{version}.tar.gz"
+  url "https://downloads.aquafold.com/v#{version}/osx/ads-osx-#{version}.tar.gz"
   name "Aquafold Aqua Data Studio"
   desc "Database IDE with data management and visual analytics"
   homepage "https://www.aquafold.com/aquadatastudio"
