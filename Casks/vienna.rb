c80b7d7ab355e168471d62360d4d0330861f5dde
Barijaona Ramaholimihaso <github.com@barijaona.com>
2020-12-04 22:48:41 +0300

    Update vienna from 3.6.0 to 3.6.1

--- Casks/vienna.rb
+++ Casks/vienna.rb
@@ -1,6 +1,6 @@
 cask "vienna" do
-  version "3.6.0"
-  sha256 "1ad5fb5f41e4fe4e5f54e7fd121d8b89f114454d6e72056f17832d9e9b82da1b"
+  version "3.6.1"
+  sha256 "372a37317c9b3ea011f991111284f3ada8f4ecced0004fab9778cdcf79e5ebdc"
 
   # bintray.com/viennarss/ was verified as official when first introduced to the cask
   url "https://dl.bintray.com/viennarss/vienna-rss/Vienna#{version}.tar.gz"
