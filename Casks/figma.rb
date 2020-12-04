9e628fd7726f3656a8dcd894f61f44f942f7180c
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-04 09:48:08 -0800

    Update figma from 91.7.0 to 91.8.0 (#94307)

--- Casks/figma.rb
+++ Casks/figma.rb
@@ -1,6 +1,6 @@
 cask "figma" do
-  version "91.7.0"
-  sha256 "086abe63cabdae7a1be00812d592a76fe2a26227eb4a77a7fa80f38d8f12aafb"
+  version "91.8.0"
+  sha256 "65a10a8b7f080f914963c4ce59164823eddb596ad0ae8c8812e88eff31c5dd6b"
 
   url "https://desktop.figma.com/mac/Figma-#{version}.zip"
   appcast "https://desktop.figma.com/mac/RELEASE.json"
