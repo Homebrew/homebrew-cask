239671f22b10ab580b5aa99c7c55ca71bae7fdd5
Randall <ran.dall@icloud.com>
2020-12-05 11:09:20 -0800

    Update sauerbraten from 2020.11.29,2020.11.30 to 2020.11.29,2020.12.04

--- Casks/sauerbraten.rb
+++ Casks/sauerbraten.rb
@@ -1,6 +1,6 @@
 cask "sauerbraten" do
-  version "2020.11.29,2020.11.30"
-  sha256 "a4c633d40a4464a12ae67ebd7cd897be0d3f415c6617b446324f73f204b7ddd7"
+  version "2020.11.29,2020.12.04"
+  sha256 "6f35ac3bc0574118d80fe769e13f85b67a8e8fc789b467f0a53597b989fe5356"
 
   # sourceforge.net/sauerbraten/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/sauerbraten/sauerbraten/#{version.before_comma.dots_to_underscores}/sauerbraten_#{version.after_comma.dots_to_underscores}_macos.dmg"
