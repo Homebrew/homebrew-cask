7d5a54fa22c65b99d4ff3a7c2007629dd6f3bb8e
Reno <25192197+singularitti@users.noreply.github.com>
2020-12-05 02:28:09 -0500

    Update one-switch from 1.15,266 to 1.16,278 (#94364)

--- Casks/one-switch.rb
+++ Casks/one-switch.rb
@@ -1,6 +1,6 @@
 cask "one-switch" do
-  version "1.15,266"
-  sha256 "d851e46041bf4b31bccd36ae828714f664fd263552292e51ad07922313f16cbf"
+  version "1.16,278"
+  sha256 "a960e80cce590f571bcfc8ccf22d04b5680e6b137605f6df7f731fbad6c423d0"
 
   url "https://fireball.studio/api/release_manager/downloads/studio.fireball.OneSwitch/#{version.after_comma}.zip"
   appcast "https://fireball.studio/api/release_manager/studio.fireball.OneSwitch.xml"
