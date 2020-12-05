2baae59052741c66d193c27fd34c7727ee85bd73
Reno <25192197+singularitti@users.noreply.github.com>
2020-12-05 12:18:25 -0500

    Add cask "berrycast" 0.30.0 (#94369)

---
+++ Casks/berrycast.rb
@@ -0,0 +1,28 @@
+cask "berrycast" do
+  version "0.30.0,858b5929-15c3-5a2d-aac7-a8b168dddc50"
+  sha256 "6fe0f1e637bb5ffc29c3826480341a40b42ab3c4c316ec3092e6f5f91ae93aaf"
+
+  url "https://media.berrycast.app/desktop-installer/Berrycast-#{version.before_comma}-latest.dmg?display-file-name=Berrycast-#{version.before_comma}-latest_#{version.after_comma}.dmg",
+      verified: "media.berrycast.app"
+  appcast "https://media.berrycast.app/desktop-installer/v2/latest-mac.yml"
+  name "Berrycast"
+  desc "Screen recorder"
+  homepage "https://www.berrycast.com/"
+
+  app "Berrycast.app"
+
+  uninstall login_item: "Berrycast",
+            quit:       [
+              "com.openmindt.berrycast",
+              "com.openmindt.berrycast.helper",
+              "com.openmindt.berrycast.helper.GPU",
+              "com.openmindt.berrycast.helper.Plugin",
+              "com.openmindt.berrycast.helper.Renderer",
+            ]
+
+  zap trash: [
+    "~/Library/Logs/Berrycast",
+    "~/Library/Preferences/com.openmindt.berrycast.plist",
+    "~/Library/Saved Application State/com.openmindt.berrycast.savedState",
+  ]
+end
