3c450f480a8f65b021badbf8035e808114fdcdd3
Bevan Kay <bevanjkay@gmail.com>
2020-12-05 15:28:44 +1100

    Added new casks for pushplaylabs Sidekick Browser

---
+++ Casks/pushplaylabs-sidekick.rb
@@ -0,0 +1,11 @@
+cask "pushplaylabs-sidekick" do
+  version "87.7.36.5759"
+  sha256 "0db4e8414ba3d3e86768c23625fa691ab6105383d7844e8c50f33a66a222fbe9"
+
+  url "https://api.meetsidekick.com/downloads/df/mac"
+  name "Sidekick"
+  desc "Browser designed for modern work"
+  homepage "https://www.meetsidekick.com/"
+
+  app "Sidekick.app"
+end
