96c0af5e1f76fa958179e878e013c24946021083
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-05 10:39:40 -0800

    Update mic-drop from 1.3.1 to 1.3.3 (#94388)

--- Casks/mic-drop.rb
+++ Casks/mic-drop.rb
@@ -1,6 +1,6 @@
 cask "mic-drop" do
-  version "1.3.1"
-  sha256 "22ea299727325830b71b2316e8421f092323403301e4ab632174b66989c56409"
+  version "1.3.3"
+  sha256 "0190df305ccf7575c94a3113507c933cce0c11073636cf8c59de006b87fd9624"
 
   url "https://getmicdrop.com/downloads/Mic%20Drop%20#{version}.zip"
   appcast "https://getmicdrop.com/downloads/appcast.xml"
