abb5785ba0096b8926e23ce5ae98a2af73f8fb1b
Francisco Rodera <trek1s@gmail.com>
2020-12-05 11:57:58 +1100

    Update eloston-chromium from 87.0.4280.67-1.1 to 87.0.4280.88-1.1

--- Casks/eloston-chromium.rb
+++ Casks/eloston-chromium.rb
@@ -1,6 +1,6 @@
 cask "eloston-chromium" do
-  version "87.0.4280.67-1.1"
-  sha256 "7cf92e358a0559209c65a567a413dd545cfc5d32c40f69523caaaefbf6db3a9c"
+  version "87.0.4280.88-1.1"
+  sha256 "cc2c2da6bea68bd518fdeb6d7562ee4fb5cc9c1af6b533790cecd816f59cee22"
 
   # github.com/kramred/ungoogled-chromium-macos/ was verified as official when first introduced to the cask
   url "https://github.com/kramred/ungoogled-chromium-macos/releases/download/#{version}/ungoogled-chromium_#{version}_macos.dmg"
