a783d84b112c1aa77db9d2a568e8da768aebfe4d
suschizu <brigitte@corecode.io>
2020-12-06 19:22:15 +0100

    Update hbuilderx from 2.9.8.20201110 to 2.9.8.20201119 (#94464)

--- Casks/hbuilderx.rb
+++ Casks/hbuilderx.rb
@@ -1,10 +1,10 @@
 cask "hbuilderx" do
-  version "2.9.8.20201110"
-  sha256 "327091ef08e3759dd038f5b668cb0d04b29350bba75973568668ad907dd81723"
+  version "2.9.8.20201119"
+  sha256 "ba82f994fc33d36cd37fee0adcfadc6016938c27ec9aa41c2f894d0dfe240d28"
 
   # download1.dcloud.net.cn/ was verified as official when first introduced to the cask
   url "https://download1.dcloud.net.cn/download/HBuilderX.#{version}.dmg"
-  appcast "https://dcloud.io/docs/HBuilder.json"
+  appcast "https://update.dcloud.net.cn/hbuilderx/changelog/#{version}.html"
   name "HBuilderX"
   homepage "https://www.dcloud.io/hbuilderx.html"
 
