9893314089e67d547edf9e3dc450cf1089790c4d
John Benninghoff <jbenninghoff@mac.com>
2020-12-06 14:05:13 -0600

    Update mactracker from 7.9.7 to 7.10 (#94487)

--- Casks/mactracker.rb
+++ Casks/mactracker.rb
@@ -1,6 +1,6 @@
 cask "mactracker" do
-  version "7.9.7"
-  sha256 "a328e5302e91f2a355a3f70c34533d0296db2a4c2ba93600071607804b6d531d"
+  version "7.10"
+  sha256 "a6f568e068e4b7f29b797626b32963ff338bf9dcde38e4a2dd53f1f145f7b8a9"
 
   url "https://www.mactracker.ca/downloads/Mactracker_#{version}.zip"
   appcast "https://update.mactracker.ca/appcast-b.xml"
