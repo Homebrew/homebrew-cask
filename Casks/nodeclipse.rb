4c61a4db8945ef774e23c6090bc376d5e583b1ee
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-06 23:45:46 +0000

    Update nodeclipse from 0.17 to 4.5.0

--- Casks/nodeclipse.rb
+++ Casks/nodeclipse.rb
@@ -1,6 +1,6 @@
 cask "nodeclipse" do
-  version "0.17"
-  sha256 "674991d7c22ea05975a76800e6e9fe9231064a09a2a2412e6ec0448676bfa2e8"
+  version "4.5.0"
+  sha256 :no_check
 
   # sourceforge.net/nodeclipse/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/nodeclipse/Enide-2015/7/Enide-2015-7-macosx-x64-20150706.zip"
