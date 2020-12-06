1d2b1a83d8aa34e34ef99891689fa37689d1be59
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-06 08:38:39 -0800

    Update zeitgeist from 2.0.1 to 2.0.2 (#94437)

--- Casks/zeitgeist.rb
+++ Casks/zeitgeist.rb
@@ -1,6 +1,6 @@
 cask "zeitgeist" do
-  version "2.0.1"
-  sha256 "96aa56490747b12b51e8c28e6243f75b2150b1b8d3ac26572c35c4a6793e9c6f"
+  version "2.0.2"
+  sha256 "e5b28b70590d201a24313f0395b5452c5a25003b62b7198ff151b3e1858c0812"
 
   url "https://github.com/daneden/zeitgeist/releases/download/v#{version}/Zeitgeist.app.zip",
       verified: "github.com/daneden/zeitgeist/"
