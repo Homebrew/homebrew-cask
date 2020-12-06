e8aa4bb0deebb9d0d228f915ddb076db9dfd1f8f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-06 23:46:58 +0000

    Update smooze from 1.9.9 to 1.9.9,445

--- Casks/smooze.rb
+++ Casks/smooze.rb
@@ -1,6 +1,6 @@
 cask "smooze" do
-  version "1.9.9"
-  sha256 "59030cba19b2e4822f48b89e10aaef8d54ccbdd498b58e5a415a0bcc758651bf"
+  version "1.9.9,445"
+  sha256 :no_check
 
   url "https://smooze.co/updates/Smooze.dmg"
   appcast "https://smooze.co/updates/update.xml"
