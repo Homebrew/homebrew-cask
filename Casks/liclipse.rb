649b8680624c6be8315fa9cba73772cb4232ff1e
Randall <ran.dall@icloud.com>
2020-12-06 08:36:38 -0800

    Update liclipse from 7.0.1,d8v8ubpz8fkfy78 to 7.1.0,4avujhmdvxhyu49

--- Casks/liclipse.rb
+++ Casks/liclipse.rb
@@ -1,6 +1,6 @@
 cask "liclipse" do
-  version "7.0.1,d8v8ubpz8fkfy78"
-  sha256 "fd518bc450ddb450b6b1db1e8a55106694690361a1cabd6de1e97b51c711013c"
+  version "7.1.0,4avujhmdvxhyu49"
+  sha256 "0d85e1f1d1e9e50cd7188ca7c5c685323d05dd41a81ec58842a375531e0cf036"
 
   # mediafire.com/file/ was verified as official when first introduced to the cask
   url "https://www.mediafire.com/file/#{version.after_comma}/liclipse_#{version.before_comma}_macosx.cocoa.x86_64.dmg"
