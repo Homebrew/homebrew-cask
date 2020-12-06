18a1dd5645ffc216d831463b576d64839976c2ee
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-06 19:40:38 +0000

    Update swift-publisher from 5.5.6 to 5.5.6,4560

--- Casks/swift-publisher.rb
+++ Casks/swift-publisher.rb
@@ -1,6 +1,6 @@
 cask "swift-publisher" do
-  version "5.5.6"
-  sha256 "49645d8094deb89a5ee492243309e57d8cdd163fb5db88ebf93a0b6b71ae1d72"
+  version "5.5.6,4560"
+  sha256 :no_check
 
   # belightsoft.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://belightsoft.s3.amazonaws.com/SwiftPublisher.dmg"
