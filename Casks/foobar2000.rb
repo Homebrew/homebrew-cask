9b4a429fda8ae91d829d884fb1555986ac7f8b3f
Dmitrij Podabed <dmitrij@podabed.org>
2020-12-06 16:52:20 +0300

    Add url do section for working with foobar2000 download protection

--- Casks/foobar2000.rb
+++ Casks/foobar2000.rb
@@ -1,8 +1,14 @@
 cask "foobar2000" do
-  version "2.2.13"
-  sha256 "7455618cb826e72f0ff25474863b4ac4c5d9d14dde708539bf71d6cc5c57f7b1"
+  version :latest
+  sha256 :no_check
 
-  url "https://www.foobar2000.org/files/d2ac068cecd7975427f150ffad31d058/foobar2000-v#{version}.dmg"
+  url do
+    require "open-uri"
+
+    base_url = "https://www.foobar2000.org"
+    file = open("#{base_url}/mac").read.scan(/href="([^"]+.dmg)"/).flatten.first
+    "#{base_url}#{file}"
+  end
   name "foobar2000"
   desc "Is an advanced freeware audio player for the Windows platform"
   homepage "https://www.foobar2000.org/"
