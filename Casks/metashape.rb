f590e22efc2e1b05d1014b2dfa61ed0714178847
Randall <ran.dall@icloud.com>
2020-12-06 08:30:25 -0800

    Update metashape from 1.6.5 to 1.7.0

--- Casks/metashape.rb
+++ Casks/metashape.rb
@@ -1,6 +1,6 @@
 cask "metashape" do
-  version "1.6.5"
-  sha256 "ee52edc7c771498c6589247065bcfaee38735aae86517d1484e854b73d753672"
+  version "1.7.0"
+  sha256 "2dd889951cb4c3526d06ce4b6ed48ca94dbd33a9867ce239092d218cc6bc7eb6"
 
   url "http://download.agisoft.com/metashape_#{version.dots_to_underscores}.dmg"
   appcast "https://www.agisoft.com/downloads/installer/"
