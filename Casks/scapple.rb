41ce1b9bf65c4eaa041375d168cfd6bf941cfc64
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-06 23:40:15 +0000

    Update scapple from 1.4 to 1.4,1122

--- Casks/scapple.rb
+++ Casks/scapple.rb
@@ -1,6 +1,6 @@
 cask "scapple" do
-  version "1.4"
-  sha256 "65254fc8178028dd9e006e5e3158b1284833dde56efcca4b3dd7758d406251b2"
+  version "1.4,1122"
+  sha256 :no_check
 
   # scrivener.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://scrivener.s3.amazonaws.com/Scapple.dmg"
