271e59e049c3fbfd1c898a9eef4d92fc3d93f932
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-06 02:17:30 +0000

    Update sky-ticket from latest to 8.2.0

--- Casks/sky-ticket.rb
+++ Casks/sky-ticket.rb
@@ -1,5 +1,5 @@
 cask "sky-ticket" do
-  version :latest
+  version "8.2.0"
   sha256 :no_check
 
   # web.static.nowtv.com/watch/player/skyticket/ was verified as official when first introduced to the cask
