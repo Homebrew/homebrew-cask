228cb5c4ddcc97d8fb51931695ea95d72941539d
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-07 09:39:17 +0800

    Update clocker.rb

--- Casks/clocker.rb
+++ Casks/clocker.rb
@@ -1,10 +1,11 @@
 cask "clocker" do
-  version "20.10.03"
+  version "20.10.03,5613815"
   sha256 "27692bbfafb74d0c9f85a7e7bfae53708831900e59eb9ac35681dbebf4bce3c4"
 
-  url "https://github.com/n0shake/Clocker/files/5613815/Clocker.app.zip",
+  url "https://github.com/n0shake/Clocker/files/#{version.after_comma}/Clocker.app.zip",
       verified: "github.com/n0shake/Clocker/"
-  appcast "https://raw.githubusercontent.com/n0shake/Clocker/master/Clocker/Releases/appcast.xml"
+  appcast "https://github.com/n0shake/Clocker/releases.atom",
+          must_contain: version.before_comma
   name "Clocker"
   desc "Track timezones from your menubar"
   homepage "https://abhishekbanthia.com/clocker"
