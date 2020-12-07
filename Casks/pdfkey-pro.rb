d38c92d47959474aeabb6ae68b29bb5a51a1a3d3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-08 07:59:10 +1100

    Update pdfkey-pro from 4.3.9 to 4.3.9,40309 (#94659)

--- Casks/pdfkey-pro.rb
+++ Casks/pdfkey-pro.rb
@@ -1,6 +1,6 @@
 cask "pdfkey-pro" do
-  version "4.3.9"
-  sha256 "c00cd822f0453d87fb296858dc2e85f7617c96c3b67123711a2396ffc2fb46cd"
+  version "4.3.9,40309"
+  sha256 :no_check
 
   url "https://pdfkey.com/PDFKeyPro.dmg"
   appcast "https://pdfkey.com/PDFKeyProUpdateFX.xml"
