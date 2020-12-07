696ce08cc298ebba5a857270b6da9675ff575759
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-07 20:08:16 +0000

    Update clover-configurator from latest to 5.17.3.0

--- Casks/clover-configurator.rb
+++ Casks/clover-configurator.rb
@@ -1,5 +1,5 @@
 cask "clover-configurator" do
-  version :latest
+  version "5.17.3.0"
   sha256 :no_check
 
   url "https://mackie100projects.altervista.org/apps/cloverconf/download-new-build.php?version=global"
