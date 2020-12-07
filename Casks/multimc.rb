05a9e2fd01be8edc4513b1deeb54054513df5d0e
Markus Reiter <me@reitermark.us>
2020-12-08 00:32:49 +0100

    Update multimc.rb

--- Casks/multimc.rb
+++ Casks/multimc.rb
@@ -1,8 +1,8 @@
 cask "multimc" do
-  version "0.6.11.1370"
+  version "0.6.11"
   sha256 :no_check
 
-  url "https://files.multimc.org/downloads/mmc-stable-osx64.tar.gz"
+  url "https://github.com/MultiMC/MultiMC5/releases/download/#{version}/mmc-stable-osx64.tar.gz"
   appcast "https://github.com/MultiMC/MultiMC5/releases.atom"
   name "Multi MC"
   homepage "https://multimc.org/"
