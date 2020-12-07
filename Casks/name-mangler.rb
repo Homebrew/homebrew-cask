55b7a1b590c58c8a22f3f6cc60efc9165fa1b1c8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-07 00:19:03 +0000

    Update name-mangler from 3.6 to 3.6,3285

--- Casks/name-mangler.rb
+++ Casks/name-mangler.rb
@@ -1,6 +1,6 @@
 cask "name-mangler" do
-  version "3.6"
-  sha256 "920089b29f13e7a2036898b7dd720367698e92c1bbbb2b417bdad06a0c3e2cfa"
+  version "3.6,3285"
+  sha256 :no_check
 
   url "https://manytricks.com/download/namemangler"
   appcast "https://manytricks.com/namemangler/appcast/"
