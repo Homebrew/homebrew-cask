6786344ea06f51490e26fb68c8568c8e3caf7b1f
CoreCode <core-code@users.noreply.github.com>
2020-12-07 23:52:41 +0100

    Update eggplant.rb

--- Casks/eggplant.rb
+++ Casks/eggplant.rb
@@ -2,7 +2,7 @@ cask "eggplant" do
   version "21.0.0,2010241818"
   sha256 :no_check
 
-  url "http://downloads.testplant.com/downloads/Eggplant/eggPlant.dmg"
+  url "https://downloads.eggplantsoftware.com/downloads/Eggplant/eggPlant.dmg"
   name "eggPlant Functional"
   homepage "https://www.eggplantsoftware.com/eggplant-functional-downloads"
 
