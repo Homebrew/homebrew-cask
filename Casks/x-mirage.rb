f02e91bb23b2ea19dace3236a3525c2a6421b6e6
Markus Reiter <me@reitermark.us>
2020-12-07 21:24:05 +0100

    Update x-mirage.rb

--- Casks/x-mirage.rb
+++ Casks/x-mirage.rb
@@ -2,9 +2,9 @@ cask "x-mirage" do
   version "3.0.1"
   sha256 :no_check
 
-  url "http://dl.x-mirage.com/x-mirage.dmg"
+  url "https://dl.x-mirage.com/x-mirage.dmg"
   name "X-Mirage"
-  homepage "http://www.x-mirage.com/x-mirage/"
+  homepage "https://www.x-mirage.com/x-mirage/"
 
   app "X-Mirage.app"
 end
