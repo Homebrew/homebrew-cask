eb80aa07a8a16ff569fefd4e21e0a484749e3c13
CoreCode <core-code@users.noreply.github.com>
2020-12-07 16:22:11 +0100

    Update elmedia-player from 7.16,2296 to 7.17,2353 (#94595)

--- Casks/elmedia-player.rb
+++ Casks/elmedia-player.rb
@@ -1,6 +1,6 @@
 cask "elmedia-player" do
-  version "7.16,2296"
-  sha256 "d2909c4914e1e397e800165490e069b61856294ee5e005489a52940cc302faf7"
+  version "7.17,2353"
+  sha256 "077ca82d2e2d83c6c63142b38e6952e548eaab77b5a9daa6de2ca162c7fef174"
 
   url "https://cdn.eltima.com/download/elmediaplayer.dmg"
   appcast "https://cdn.eltima.com/download/elmediaplayer-update/elmediaplayer.xml"
