fa3e37990702ea128e52b2f5aa2fc88650043762
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-08 09:38:46 +1100

    Update wondershare-filmora (#94484)

--- Casks/wondershare-filmora.rb
+++ Casks/wondershare-filmora.rb
@@ -1,12 +1,12 @@
 cask "wondershare-filmora" do
   version "10.1.3"
-  sha256 "77b483f920a53493264680848e69d1cc014b60c9c42f1cd89afba9c9c25ab4c4"
+  sha256 :no_check
 
   url "https://download.wondershare.com/filmora-mac_full718.dmg"
   appcast "https://cbs.wondershare.com/go.php?m=upgrade_info&pid=718&version=#{version}"
   name "Wondershare Filmora"
   desc "Video editor"
-  homepage "https://filmora.wondershare.com/video-editor/"
+  homepage "https://filmora.wondershare.com/video-editor-mac/"
 
   depends_on macos: ">= :sierra"
 
