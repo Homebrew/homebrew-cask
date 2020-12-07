72d328a3adb4076fe0c0528dca1392dc06c1a8c7
Randall <ran.dall@icloud.com>
2020-12-07 07:41:27 -0800

    Update activedock from 2.60,2060 to 2.63,2063

--- Casks/activedock.rb
+++ Casks/activedock.rb
@@ -1,6 +1,6 @@
 cask "activedock" do
-  version "2.60,2060"
-  sha256 "9d895e16ee96c628516aee17317d749384d1185cc07dd6fd71f34f8800604d4e"
+  version "2.63,2063"
+  sha256 "05987f06247ec52d1abd626c28cd74f93c0793e7e8702d12f8c8072a8aac026e"
 
   # macplus-software.com/ was verified as official when first introduced to the cask
   url "https://macplus-software.com/downloads/ActiveDock.zip"
