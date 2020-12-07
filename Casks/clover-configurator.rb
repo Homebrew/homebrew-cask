fb284b952553be0bed1aa5b614a0a1be47d34cae
Markus Reiter <me@reitermark.us>
2020-12-07 21:19:10 +0100

    Update clover-configurator.rb

--- Casks/clover-configurator.rb
+++ Casks/clover-configurator.rb
@@ -3,8 +3,9 @@ cask "clover-configurator" do
   sha256 :no_check
 
   url "https://mackie100projects.altervista.org/apps/cloverconf/download-new-build.php?version=global"
+  appcast "https://mackie100projects.altervista.org/download-clover-configurator/"
   name "Clover Configurator"
-  homepage "https://mackie100projects.altervista.org/"
+  homepage "https://mackie100projects.altervista.org/clover-configurator/"
 
   app "Clover Configurator.app"
 end
