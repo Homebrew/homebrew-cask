8e8dbef0219dad20306d0f7670e96ab77f1dc2af
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-07 04:33:52 +0000

    Update uc-one from latest to 3.9.18.57

--- Casks/uc-one.rb
+++ Casks/uc-one.rb
@@ -1,5 +1,5 @@
 cask "uc-one" do
-  version :latest
+  version "3.9.18.57"
   sha256 :no_check
 
   # storage.googleapis.com/ucone_saas_and_meet_clients/ was verified as official when first introduced to the cask
