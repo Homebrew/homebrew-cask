a1b5199a9dcc499751d26df37e28bd57fc616c3e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-07 13:11:26 +1100

    Update nsregextester from latest to 1.0 (#94516)

--- Casks/nsregextester.rb
+++ Casks/nsregextester.rb
@@ -1,5 +1,5 @@
 cask "nsregextester" do
-  version :latest
+  version "1.0"
   sha256 :no_check
 
   # raw.githubusercontent.com/aaronvegh/nsregextester/master/ was verified as official when first introduced to the cask
