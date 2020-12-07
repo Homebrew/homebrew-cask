117a4fb3da49635e406f9009405363b545333ebc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-07 00:18:07 +0000

    Update blocs from 4.0.1 to 4.0.1,401

--- Casks/blocs.rb
+++ Casks/blocs.rb
@@ -1,6 +1,6 @@
 cask "blocs" do
-  version "4.0.1"
-  sha256 "d68e5ad79ae4c21dec0e92aa2623432099551095610dae25f6b76f1d338075ba"
+  version "4.0.1,401"
+  sha256 :no_check
 
   url "https://blocsapp.com/download/Blocs.zip"
   appcast "https://blocsapp.com/update/v#{version.major}/info.xml"
