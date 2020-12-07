9dcec65b65a07d7562e88db0a6c8fcd07b4b19d2
Julian Mayer <julian@corecode.io>
2020-12-07 12:08:02 +0100

    Update wormhole from 1.4.7 to 1.4.8

--- Casks/wormhole.rb
+++ Casks/wormhole.rb
@@ -1,6 +1,6 @@
 cask "wormhole" do
-  version "1.4.7"
-  sha256 "12ec1ff8f70cb828fdc7a91e16103b6b6b974554d670e2b79166dc443283298b"
+  version "1.4.8"
+  sha256 "d6794b369c2c8912182f59863f910232649a7c42354251d644e7dc763597ecb5"
 
   # files.octopusgame.com/ was verified as official when first introduced to the cask
   url "https://files.octopusgame.com/os/WormholeInstaller.dmg"
