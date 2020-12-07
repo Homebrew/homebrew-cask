94aa23258025fcf718a7c7183419a973e937d0a8
Randall <ran.dall@icloud.com>
2020-12-07 07:36:35 -0800

    Update raiderio from 1.6.3 to 2.0.0

--- Casks/raiderio.rb
+++ Casks/raiderio.rb
@@ -1,6 +1,6 @@
 cask "raiderio" do
-  version "1.6.3"
-  sha256 "e79d919bfc22442fd27ca09a5dd4e4182c733e02cf33d34210d266f7016ff832"
+  version "2.0.0"
+  sha256 "0877dc04541f92749f9c0c900bf067368dc2b2966368c7eaeff1e6bbb394a2b6"
 
   # github.com/RaiderIO/raiderio-client-builds was verified as official when first introduced to the cask
   url "https://github.com/RaiderIO/raiderio-client-builds/releases/download/v#{version}/RaiderIO_Client.dmg"
