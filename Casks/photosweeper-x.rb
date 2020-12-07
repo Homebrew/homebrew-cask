0c6b68167ee008ad77f7fd7918fd5fd43b245986
CoreCode <core-code@users.noreply.github.com>
2020-12-08 00:14:08 +0100

    Update photosweeper-x from 3.9.1 to 3.9.2 (#94686)

--- Casks/photosweeper-x.rb
+++ Casks/photosweeper-x.rb
@@ -1,6 +1,6 @@
 cask "photosweeper-x" do
-  version "3.9.1"
-  sha256 "b24845e310d9b00a5630947da61a5a46c62766a7e4e5a0acf8b88fc49f3844de"
+  version "3.9.2"
+  sha256 "8ee6a7f08a9c10ef015e41d8e09f454a71c9783da68afdc87c1d42c269f3b722"
 
   url "https://overmacs.com/downloads/PhotoSweeper_X.dmg"
   appcast "https://overmacs.com/feeds/photosweeper_update.xml"
