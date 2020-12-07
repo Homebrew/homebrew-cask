38b2ed61a73f80524678e86d586ad973617f729e
Randall <ran.dall@icloud.com>
2020-12-07 08:02:32 -0800

    Update screaming-frog-seo-spider from 14.0 to 14.1

--- Casks/screaming-frog-seo-spider.rb
+++ Casks/screaming-frog-seo-spider.rb
@@ -1,6 +1,6 @@
 cask "screaming-frog-seo-spider" do
-  version "14.0"
-  sha256 "0043c0b2021fdc0e936f4a77ef886da02dc8bc07b51d7b925397be00a0b0328e"
+  version "14.1"
+  sha256 "57e94997351e2ddc369af475f7651cfaa1d0420ec96752a0a91ec6a2f77cc04f"
 
   url "https://download.screamingfrog.co.uk/products/seo-spider/ScreamingFrogSEOSpider-#{version}.dmg"
   appcast "https://www.screamingfrog.co.uk/wp-content/themes/screamingfrog/inc/download-modal.php"
