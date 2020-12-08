ad2f8875b92109e429f01f51f4e690f947d987a6
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-09 05:06:15 +0800

    Update messenger.rb

--- Casks/messenger.rb
+++ Casks/messenger.rb
@@ -2,8 +2,8 @@ cask "messenger" do
   version "81.6.118,261169871"
   sha256 "cd3fe4d19f4aa920c30e84fccd801ee5c96ccd5e26f670ff7860bceb45884732"
 
-  # facebook.com/messenger/desktop/ was verified as official when first introduced to the cask
-  url "https://www.facebook.com/messenger/desktop/update/#{version.after_comma}.zip"
+  url "https://www.facebook.com/messenger/desktop/update/#{version.after_comma}.zip",
+      verified: "facebook.com/messenger/desktop/"
   # The appcast will fail CI due to its Cache-Control settings. See:
   #   https://github.com/Homebrew/homebrew-cask/pull/90957#issuecomment-710740050
   appcast "https://www.facebook.com/messenger/desktop/update/latest-mac.yml"
