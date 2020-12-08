471ea14747ff64726d879e60fbe1d01b589123f0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-08 01:24:32 +0000

    Update spacelauncher from 1.4.13 to 1.4.13,85

--- Casks/spacelauncher.rb
+++ Casks/spacelauncher.rb
@@ -1,6 +1,6 @@
 cask "spacelauncher" do
-  version "1.4.13"
-  sha256 "87f22cf99faad6b7ce06aeb8fc94e3a8410e6bc01a52187c4beb5a182e48e296"
+  version "1.4.13,85"
+  sha256 :no_check
 
   url "https://spacelauncherapp.com/download/SpaceLauncher.zip"
   appcast "https://spacelauncherapp.com/download/appcast.xml"
