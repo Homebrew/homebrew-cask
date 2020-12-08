2a97fbbf53fff024554bdd6ecc33e23361c1d8dc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-09 08:22:13 +1100

    Update icons8 from 5.7.4 to 5.7.4,57400 (#94868)

--- Casks/icons8.rb
+++ Casks/icons8.rb
@@ -1,7 +1,7 @@
 cask "icons8" do
   # NOTE: "8" is not a version number, but an intrinsic part of the product name
-  version "5.7.4"
-  sha256 "35c0ed3421607a7a7591b15e3c305c6efd52db6f6d73bc0950b19a49007b4ae0"
+  version "5.7.4,57400"
+  sha256 :no_check
 
   url "https://desktop.icons8.com/updates/mac/Icons8App_for_Mac_OS.dmg"
   appcast "https://desktop.icons8.com/updates/mac/icons8_cast.xml"
