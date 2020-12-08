88e7cc0cae0d926d4baa264f3c3521f0b9675571
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-09 04:58:16 +0800

    Update continuity-activation-tool.rb

--- Casks/continuity-activation-tool.rb
+++ Casks/continuity-activation-tool.rb
@@ -1,9 +1,10 @@
 cask "continuity-activation-tool" do
   version "2"
-  sha256 "be310c5581599ce92adcba5ed885832c69473882e960ac8222dd68ae71338e00"
+  sha256 :no_check
 
   url "https://github.com/dokterdok/Continuity-Activation-Tool/archive/master.zip"
   name "Continuity Activation Tool"
+  desc "Enable continuity features on compatible hardware"
   homepage "https://github.com/dokterdok/Continuity-Activation-Tool/"
 
   app "Continuity-Activation-Tool-master/Continuity Activation Tool.app"
