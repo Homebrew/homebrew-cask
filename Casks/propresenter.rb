a0d0adf1075cb2ef59a6b16f410b0f2b40d64ea4
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-08 09:30:35 +0800

    Update propresenter.rb

--- Casks/propresenter.rb
+++ Casks/propresenter.rb
@@ -5,6 +5,7 @@ cask "propresenter" do
   url "https://renewedvision.com/downloads/propresenter/mac/ProPresenter_#{version.before_comma}_#{version.after_comma}.zip"
   appcast "https://api.renewedvision.com/v1/pro/upgrade?platform=macos&osVersion=0&appVersion=0&buildNumber=0&includeNotes=0"
   name "ProPresenter"
+  desc "Presentation and production application for live events"
   homepage "https://www.renewedvision.com/propresenter.php"
 
   depends_on macos: ">= :mojave"
@@ -12,13 +13,13 @@ cask "propresenter" do
   app "ProPresenter.app"
 
   zap trash: [
-    "~/Library/Application Support/RenewedVision/ProPresenter6",
-    "~/Library/Caches/KSCrashReports/ProPresenter 6",
-    "~/Library/Caches/Sessions/ProPresenter 6",
-    "~/Library/Caches/com.renewedvision.ProPresenter6",
-    "~/Library/Preferences/com.renewedvision.ProPresenter6.plist",
+    "~/Library/Application Support/RenewedVision/ProPresenter#{version.major}",
+    "~/Library/Caches/KSCrashReports/ProPresenter #{version.major}",
+    "~/Library/Caches/Sessions/ProPresenter #{version.major}",
+    "~/Library/Caches/com.renewedvision.ProPresenter#{version.major}",
+    "~/Library/Preferences/com.renewedvision.ProPresenter#{version.major}.plist",
     "/Library/Application Support/RenewedVision",
-    "/Library/Caches/com.renewedvision.ProPresenter6",
+    "/Library/Caches/com.renewedvision.ProPresenter#{version.major}",
     "/Users/Shared/Renewed Vision Media",
   ],
       rmdir: [
