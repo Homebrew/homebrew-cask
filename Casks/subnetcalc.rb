12cdcac0b87eb7402d093ca00e812f61925a2dc1
zsoltion <zsoltion@users.noreply.github.com>
2020-12-08 02:20:56 +0100

    Update SubnetCalc from 1.5 to 1.6
    Update SubnetCalc from 1.5 to 1.6

--- Casks/subnetcalc.rb
+++ Casks/subnetcalc.rb
@@ -3,8 +3,8 @@ cask "subnetcalc" do
     version "1.1"
     sha256 "19157baacac0cede6849356cb305a0ed2e4290e1e9e8ac4b691fd1d7ce1920cf"
   else
-    version "1.5"
-    sha256 "7dd69dbc4f6f8d305c98091a5540ca5e708f8c7483e2a237c1b7d3d8e915e748"
+    version "1.6"
+    sha256 "46be97909783cdebca6c0cf22d777fcb31c46c417bd4378ea640882e6e609e2a"
   end
 
   url "http://subnetcalc.free.fr/download/subnetcalc-#{version}.dmg"
