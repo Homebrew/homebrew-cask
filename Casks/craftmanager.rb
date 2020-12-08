969dac0b35e2bbd7b4ce7fc775ed17a34453afaf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-08 05:41:26 +0000

    Update craftmanager from 1.0.103 to 1.0.103,216

--- Casks/craftmanager.rb
+++ Casks/craftmanager.rb
@@ -1,6 +1,6 @@
 cask "craftmanager" do
-  version "1.0.103"
-  sha256 "9062d87c880dbebda9900860bc867b1ac8d209e0688678108ac17a990984b5f6"
+  version "1.0.103,216"
+  sha256 :no_check
 
   url "https://craft-assets.invisionapp.com/CraftManager/production/CraftManager.zip"
   appcast "https://craft-assets.invisionapp.com/CraftManager/production/appcast.xml"
