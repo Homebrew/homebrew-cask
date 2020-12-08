7467a10c632a4ca4c23a394872febb65854feeae
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-08 02:59:16 +0000

    Update butler from 4.3.3 to 4.3.3,5096

--- Casks/butler.rb
+++ Casks/butler.rb
@@ -1,6 +1,6 @@
 cask "butler" do
-  version "4.3.3"
-  sha256 "8541af85e4b6c827cdeb91bec1ce31acbaf4727fb9c97dca976d260d7c211b37"
+  version "4.3.3,5096"
+  sha256 :no_check
 
   url "https://manytricks.com/download/butler"
   appcast "https://manytricks.com/butler/appcast"
