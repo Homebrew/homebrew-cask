d8fb364dbaecf1952bee3a7d8b9942f60e1d84fc
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-09 05:29:10 +0800

    Update pictogram.rb

--- Casks/pictogram.rb
+++ Casks/pictogram.rb
@@ -1,8 +1,9 @@
 cask "pictogram" do
-  version :latest
-  sha256 :no_check
+  version "0.1,13"
+  sha256 "fb35a21dcc548f0226453701632ebaa478ab8cb68fccfd0ae995cbb5c80d741b"
 
-  url "https://pictogramapp.com/Pictogram.zip"
+  url "https://pictogramapp.com/updates/v#{version.before_comma}%20%28Build%20#{version.after_comma}%29.zip"
+  appcast "https://pictogramapp.com/updates/appcast.xml"
   name "Pictogram"
   desc "Customize and maintain app icons"
   homepage "https://pictogramapp.com/"
