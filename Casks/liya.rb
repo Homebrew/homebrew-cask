006564bab6768452a7460e64d2fa0d4dc5cfab47
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-09 05:03:19 +0800

    Update liya.rb

--- Casks/liya.rb
+++ Casks/liya.rb
@@ -1,9 +1,10 @@
 cask "liya" do
   version "5.0.4"
-  sha256 "678136b9bf2443b7717b95f958a353c7f51af8508c1faa95dbf9d4c7bbe660e7"
+  sha256 :no_check
 
   url "https://cutedgesystems.com/downloads/Liya.zip"
   name "Liya"
+  desc "Access three database systems simultaneously from a single visual interface"
   homepage "https://cutedgesystems.com/software/liya/"
 
   depends_on macos: ">= :catalina"
