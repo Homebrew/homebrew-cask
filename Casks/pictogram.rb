ef794ddb02d63fa42c8a1b5de505aafdce98fc32
FIGBERT <figbert@figbert.com>
2020-12-08 13:16:44 -0800

    Add Pictogram v0.1

---
+++ Casks/pictogram.rb
@@ -0,0 +1,11 @@
+cask "pictogram" do
+  version :latest
+  sha256 :no_check
+
+  url "https://pictogramapp.com/Pictogram.zip"
+  name "Pictogram"
+  desc "Customize and maintain app icons"
+  homepage "https://pictogramapp.com/"
+
+  app "Pictogram.app"
+end
