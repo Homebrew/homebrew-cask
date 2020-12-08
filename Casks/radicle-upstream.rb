3d53bacf464398349594e32fcee9c5a93b7557d6
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-09 05:14:20 +0800

    Update radicle-upstream.rb

--- Casks/radicle-upstream.rb
+++ Casks/radicle-upstream.rb
@@ -3,6 +3,7 @@ cask "radicle-upstream" do
   sha256 "3653f9e228b51b1d193a1fbb8926bf0d26deb9c547c5944360316c309e5ef303"
 
   url "https://releases.radicle.xyz/radicle-upstream-#{version}.dmg"
+  appcast "https://github.com/radicle-dev/radicle-upstream/releases.atom"
   name "Radicle Upstream"
   desc "Desktop client for Radicle"
   homepage "https://radicle.xyz/"
