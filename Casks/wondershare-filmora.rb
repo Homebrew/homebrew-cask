44bad6df52ff56053e73dadd03565c37a8c0e001
Julian Mayer <julian@corecode.io>
2020-12-08 16:12:23 +0100

    Update wondershare-filmora from 10.1.3 to 10.1.5

--- Casks/wondershare-filmora.rb
+++ Casks/wondershare-filmora.rb
@@ -1,6 +1,6 @@
 cask "wondershare-filmora" do
-  version "10.1.3"
-  sha256 :no_check
+  version "10.1.5"
+  sha256 "08cc79cbfcb967fca8c5bf781e34565b2e5f5bbac1eae4dfb0814d6f6b5dffae"
 
   url "https://download.wondershare.com/filmora-mac_full718.dmg"
   appcast "https://cbs.wondershare.com/go.php?m=upgrade_info&pid=718&version=#{version}"
