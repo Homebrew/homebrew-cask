29d998adfd726e51265a3a38ff93349ad1927903
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-09 03:53:24 +1100

    Update vk-messenger from latest to 5.1.7,711 (#94862)

--- Casks/vk-messenger.rb
+++ Casks/vk-messenger.rb
@@ -1,5 +1,5 @@
 cask "vk-messenger" do
-  version :latest
+  version "5.1.7,711"
   sha256 :no_check
 
   # desktop.userapi.com/mac/master/ was verified as official when first introduced to the cask
