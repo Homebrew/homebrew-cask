8727944072ab81ca984a9442eb9eb09f4e42b5b8
Brigitte <brigitte@corecode.io>
2020-12-08 13:36:33 +0100

    Update continuity-activation-tool from 1.0 to 2

--- Casks/continuity-activation-tool.rb
+++ Casks/continuity-activation-tool.rb
@@ -1,6 +1,6 @@
 cask "continuity-activation-tool" do
-  version "1.0"
-  sha256 :no_check
+  version "2"
+  sha256 "be310c5581599ce92adcba5ed885832c69473882e960ac8222dd68ae71338e00"
 
   url "https://github.com/dokterdok/Continuity-Activation-Tool/archive/master.zip"
   name "Continuity Activation Tool"
