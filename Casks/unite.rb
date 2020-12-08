2d1af385cb0814f1cd003cf68c4d20b0ecb9abfd
Brigitte <brigitte@corecode.io>
2020-12-08 13:34:07 +0100

    Update unite from 3.1,WiusgeYwTKaK9HQ7XWkT to 3.2,638879:sWbXL0HYRsWzxwrH8Zw1

--- Casks/unite.rb
+++ Casks/unite.rb
@@ -1,9 +1,9 @@
 cask "unite" do
-  version "3.1,WiusgeYwTKaK9HQ7XWkT"
-  sha256 "9a16c4fdcfbd63c301102fb024a9a2d47301eb63d24d52cd307c38bbbe2bb685"
+  version "3.2,638879:sWbXL0HYRsWzxwrH8Zw1"
+  sha256 "80bef525cdbeee5d35da93d0aa71a42dd268b039ce4f61f242ee18f103538ff9"
 
   # paddle.s3.amazonaws.com/fulfillment_downloads/20398/576531/ was verified as official when first introduced to the cask
-  url "https://paddle.s3.amazonaws.com/fulfillment_downloads/20398/576531/#{version.after_comma}_Unite.zip"
+  url "https://paddle.s3.amazonaws.com/fulfillment_downloads/20398/#{version.after_comma.before_colon}/#{version.after_colon}_Unite.zip"
   appcast "https://drive.google.com/uc?export=download&id=1pPlm8G1yluV7ipcRh-2pmXP-nATWsjTK"
   name "Unite"
   desc "Turn websites into apps"
