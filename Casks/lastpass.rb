7ab32f9d48f4eebfa96b8abac2d4319974dd24be
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-09 06:38:46 +1100

    Update lastpass from 4.60.0 to 4.60.0,1121 (#94731)

--- Casks/lastpass.rb
+++ Casks/lastpass.rb
@@ -1,6 +1,6 @@
 cask "lastpass" do
-  version "4.60.0"
-  sha256 "7fc8feb33df09130562c195b397d6461e90c985ea1a0f327d0d4eb384df6bcc3"
+  version "4.60.0,1121"
+  sha256 :no_check
 
   url "https://download.cloud.lastpass.com/mac/LastPass.dmg"
   appcast "https://download.cloud.lastpass.com/mac/AppCast.xml"
