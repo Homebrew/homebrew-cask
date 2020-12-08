fdd55e296e689bb4aa2ee16aed028551c79093e2
Markus Reiter <me@reitermark.us>
2020-12-08 08:07:49 +0100

    Update opencore-configurator.rb

--- Casks/opencore-configurator.rb
+++ Casks/opencore-configurator.rb
@@ -3,8 +3,9 @@ cask "opencore-configurator" do
   sha256 :no_check
 
   url "https://mackie100projects.altervista.org/apps/opencoreconf/download-new-build.php?version=last"
+  appcast "https://mackie100projects.altervista.org/download-opencore-configurator/"
   name "OpenCore Configurator"
-  homepage "https://mackie100projects.altervista.org/download-opencore-configurator/"
+  homepage "https://mackie100projects.altervista.org/opencore-configurator/"
 
   app "OpenCore Configurator.app"
 end
