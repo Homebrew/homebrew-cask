eacdc5018926983b41d5e449112e09f8f4fea6d0
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-09 05:09:14 +0800

    Update crystalmaker.rb

--- Casks/crystalmaker.rb
+++ Casks/crystalmaker.rb
@@ -5,7 +5,10 @@ cask "crystalmaker" do
   url "http://crystalmaker.com/downloads/crystalmaker_mac.zip"
   appcast "http://crystalmaker.com/crystalmaker/release-notes/mac/#{version.major}/index.html"
   name "CrystalMaker"
+  desc "Visualize crystal and molecular structures"
   homepage "http://crystalmaker.com/index.html"
+
+  depends_on macos: ">= :sierra"
 
   app "CrystalMaker.app"
 end
