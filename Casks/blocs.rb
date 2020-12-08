91e269ca88d8327cee7325157e46f6ef249114b0
Julian Mayer <julian@corecode.io>
2020-12-08 16:11:22 +0100

    Update blocs from 4.0.1,401 to 4.0.2,402

--- Casks/blocs.rb
+++ Casks/blocs.rb
@@ -1,6 +1,6 @@
 cask "blocs" do
-  version "4.0.1,401"
-  sha256 :no_check
+  version "4.0.2,402"
+  sha256 "362e8f22528984824d975648e28ae086217577a98f186a442893de020a790070"
 
   url "https://blocsapp.com/download/Blocs.zip"
   appcast "https://blocsapp.com/update/v#{version.major}/info.xml"
