82bb66972696cc15a66366c4e4f7c29c088cf871
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-09 04:54:24 +0800

    Update unite.rb

--- Casks/unite.rb
+++ Casks/unite.rb
@@ -2,9 +2,8 @@ cask "unite" do
   version "4.0,sWbXL0HYRsWzxwrH8Zw1"
   sha256 "80bef525cdbeee5d35da93d0aa71a42dd268b039ce4f61f242ee18f103538ff9"
 
-  # paddle.s3.amazonaws.com/fulfillment_downloads/20398/576531/ was verified as official when first introduced to the cask
   url "https://paddle.s3.amazonaws.com/fulfillment_downloads/20398/638879/#{version.after_comma}_Unite.zip",
-      verified: "https://paddle.s3.amazonaws.com/fulfillment_downloads/20398/638879/"
+      verified: "paddle.s3.amazonaws.com/fulfillment_downloads/20398/638879/"
   appcast "https://drive.google.com/uc?export=download&id=1gb_luG8qUL6XZu8tdI-9zrUE9I_oFBmo"
   name "Unite"
   desc "Turn websites into apps"
