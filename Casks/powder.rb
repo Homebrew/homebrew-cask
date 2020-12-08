cbf20deb2a2433287ee1d7a2b8f12257c6512b36
Markus Reiter <me@reitermark.us>
2020-12-08 05:17:29 +0100

    Update powder.rb

--- Casks/powder.rb
+++ Casks/powder.rb
@@ -1,8 +1,9 @@
 cask "powder" do
-  version "MAJOR_VERSION"
+  version "95.0"
   sha256 :no_check
 
   url "https://powdertoy.co.uk/Download/powder-mac32.dmg"
+  appcast "https://powdertoy.co.uk"
   name "Powder Toy"
   homepage "https://powdertoy.co.uk/"
 
