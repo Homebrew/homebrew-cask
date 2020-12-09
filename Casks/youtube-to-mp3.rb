f78d9e2ea3a310c320bd0d873162543e37fbea0f
Randall <ran.dall@icloud.com>
2020-12-09 13:28:05 -0800

    Update youtube-to-mp3

--- Casks/youtube-to-mp3.rb
+++ Casks/youtube-to-mp3.rb
@@ -1,6 +1,6 @@
 cask "youtube-to-mp3" do
   version "3.9.9.49"
-  sha256 :no_check
+  sha256 "8dad480b1acfe9b11e13c39a6a6ef6d253d3f2a04301f25dfa6ea2ad500bec5a"
 
   url "https://www.mediahuman.com/files/YouTubeToMP3.dmg"
   name "MediaHuman YouTube to MP3 Converter"
