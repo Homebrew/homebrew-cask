565ff2f88a3a61a6d347f416e9f9cf457757d14b
Randall <ran.dall@icloud.com>
2020-12-09 13:07:15 -0800

    Update swift-publisher

--- Casks/swift-publisher.rb
+++ Casks/swift-publisher.rb
@@ -1,6 +1,6 @@
 cask "swift-publisher" do
   version "5.5.6,4560"
-  sha256 :no_check
+  sha256 "6fcf05dfb6d7ba5a0904dede306231de204464ba71e46ef66cd7592a6891ac25"
 
   # belightsoft.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://belightsoft.s3.amazonaws.com/SwiftPublisher.dmg"
