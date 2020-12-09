dcf050a42bdfe0569e7e1bc247d8d1134331ea3b
suschizu <brigitte@corecode.io>
2020-12-09 19:00:08 +0100

    Update perimeter81.rb

--- Casks/perimeter81.rb
+++ Casks/perimeter81.rb
@@ -1,6 +1,6 @@
 cask "perimeter81" do
   # NOTE: "81" is not a version number, but an intrinsic part of the product name
-  version "5.3.4"
+  version "6.1.0"
   sha256 :no_check
 
   url "https://static.perimeter81.com/apps/osx/sdp/Perimeter81.pkg"
