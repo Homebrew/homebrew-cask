e306037f56cb9216a1cd82ea294b96086f7b31c5
Randall <ran.dall@icloud.com>
2020-12-09 13:36:09 -0800

    Update recordit

--- Casks/recordit.rb
+++ Casks/recordit.rb
@@ -1,6 +1,6 @@
 cask "recordit" do
   version "1.6.10,114"
-  sha256 :no_check
+  sha256 "c6dd0d00665b6b8c143b0e1ae65f6404b4d9015a57dde141a1904b2a29776e45"
 
   url "https://recordit-co.s3.amazonaws.com/RecordIt.zip",
       verified: "recordit-co.s3.amazonaws.com/"
