05d86f9ed63ab68397bfb0ddd740a3f7423342c2
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-09 12:04:27 +0800

    Update trainerroad.rb

--- Casks/trainerroad.rb
+++ Casks/trainerroad.rb
@@ -1,9 +1,9 @@
 cask "trainerroad" do
-  version "2020.47.117"
+  version "2020.47.3.117"
   sha256 "0bd5555d8d5d51cf55cad7d197f0c6d2a31542ba5997cf428a573c5f74cad33f"
 
-  # trainrdtrcmn01un1softw01.blob.core.windows.net/ was verified as official when first introduced to the cask
-  url "https://trainrdtrcmn01un1softw01.blob.core.windows.net/installers/mac/v001/Production/TrainerRoad.dmg"
+  url "https://trainrdtrcmn01un1softw01.blob.core.windows.net/installers/mac/v001/Production/TrainerRoad-#{version}.dmg",
+      verified: "trainrdtrcmn01un1softw01.blob.core.windows.net/"
   appcast "https://trainrdtrcmn01un1softw01.blob.core.windows.net/installers/mac/v001/Production/latest-mac.yml"
   name "TrainerRoad"
   desc "Cycling training system"
