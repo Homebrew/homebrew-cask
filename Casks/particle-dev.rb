49416a59ce0aa2cfb5bc7c9a3401eb1c04044635
Randall <ran.dall@icloud.com>
2020-12-09 13:43:20 -0800

    Update particle-dev

--- Casks/particle-dev.rb
+++ Casks/particle-dev.rb
@@ -1,6 +1,6 @@
 cask "particle-dev" do
   version "1.18.0"
-  sha256 :no_check
+  sha256 "34f4643a39d3668a6d36dc38240bfea3fcf6e190f25c4aff0060c1cdcd85933a"
 
   # spark-website.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://spark-website.s3.amazonaws.com/particle-dev-mac.zip"
