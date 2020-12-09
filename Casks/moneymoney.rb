38503b2593ca58d3b44c2fa36af40eb731f07f01
Julian Mayer <julian@corecode.io>
2020-12-09 08:07:31 +0100

    Update moneymoney

--- Casks/moneymoney.rb
+++ Casks/moneymoney.rb
@@ -1,6 +1,6 @@
 cask "moneymoney" do
   version "2.3.35,375"
-  sha256 :no_check
+  sha256 "29757cbeb431914ae7f22dd1f99446d837a1183655b3d0c49f5e6929740facb7"
 
   url "https://service.moneymoney-app.com/1/MoneyMoney.zip"
   appcast "https://service.moneymoney-app.com/1/Appcast.xml"
