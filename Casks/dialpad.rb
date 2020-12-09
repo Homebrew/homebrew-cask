6eed9bd1c6ae3c0c0d74d99fdf7816e6c062e95e
Randall <ran.dall@icloud.com>
2020-12-09 13:28:53 -0800

    Update dialpad

--- Casks/dialpad.rb
+++ Casks/dialpad.rb
@@ -1,6 +1,6 @@
 cask "dialpad" do
   version "18.78.6"
-  sha256 :no_check
+  sha256 "1b205ee99052ef43cf047ec6aced7e183d9f10803872753a6e31e0a27b319576"
 
   # storage.googleapis.com/dialpad_native/osx/ was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/dialpad_native/osx/Dialpad.dmg"
