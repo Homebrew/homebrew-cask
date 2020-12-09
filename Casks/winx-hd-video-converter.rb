cd3ca5f0bb0d2c8a88cf08c61fed808b74f97535
Randall <ran.dall@icloud.com>
2020-12-09 13:37:11 -0800

    Update winx-hd-video-converter

--- Casks/winx-hd-video-converter.rb
+++ Casks/winx-hd-video-converter.rb
@@ -1,6 +1,6 @@
 cask "winx-hd-video-converter" do
   version "6.5.2,20201202"
-  sha256 :no_check
+  sha256 "e948e126bfcb0f3827714ee98c35039166c0af420e3c8e0f911cd61ab3104143"
 
   url "https://www.winxdvd.com/download/winx-hd-video-converter-for-mac.dmg"
   name "Winx HD Video Converter"
