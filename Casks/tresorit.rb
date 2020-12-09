dcf163417527cabf1da2ac1ea6e70c82afac28b4
Benedek Kozma <cyberbeni@gmail.com>
2020-12-09 21:53:46 +0100

    reposition auto_updates

--- Casks/tresorit.rb
+++ Casks/tresorit.rb
@@ -1,12 +1,13 @@
 cask "tresorit" do
   version :latest
-  auto_updates true
   sha256 :no_check
 
   # installerstorage.blob.core.windows.net/ was verified as official when first introduced to the cask
   url "https://installerstorage.blob.core.windows.net/public/install/Tresorit.dmg"
   name "Tresorit"
   homepage "https://tresorit.com/"
+
+  auto_updates true
 
   app "Tresorit.app"
 
