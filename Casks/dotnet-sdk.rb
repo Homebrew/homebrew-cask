67d6626e4b1486acdbc95cde2bedb1666e08ea86
Mickaël Mottet <MCKLMT@users.noreply.github.com>
2020-12-09 09:58:48 +0100

    Update dotnet-sdk from 5.0.100 to 5.0.101

--- Casks/dotnet-sdk.rb
+++ Casks/dotnet-sdk.rb
@@ -3,8 +3,8 @@ cask "dotnet-sdk" do
     version "2.2.402,7430e32b-092b-4448-add7-2dcf40a7016d:1076952734fbf775062b48344d1a1587"
     sha256 "e74d816bc034d0fcdfa847286a6cad097227d4864da1c97fe801012af0c26341"
   else
-    version "5.0.100,3bdcd7d2-1444-4f7d-a254-504a994ffe39:e4f42b83604673f971748c722aa20bec"
-    sha256 "8f601ad822d9abb2f96a81f6a949629c46c7416d67ab55decbf358ffa5fc8deb"
+    version "5.0.101,0a7fa783-02e1-4785-b7b1-3c430f8825dc:764e53ff2f5722bc1b8bbc178fe25930"
+    sha256 "280209019adf9b90480619d2150b818a572fe4e68c3e013d29cbb20fef5bc4bc"
   end
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
