1bf35ba46957aa081d01c50958898be16b7cfacb
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-11 07:09:39 +0800

    Update pritunl.rb

--- Casks/pritunl.rb
+++ Casks/pritunl.rb
@@ -1,6 +1,6 @@
 cask "pritunl" do
   if MacOS.version <= :catalina
-    version "v1.2.2615.73"
+    version "1.2.2615.73"
     sha256 "275f8498fb736a0a1ddebcbfcb6f03e82b8eb4fedf9b7eb61b0e306cb6de4a71"
   else
     version "1.2.2631.70"
