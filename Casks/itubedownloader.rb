b93434bb2c6469792ce81921af46816d91a4ad75
Markus Reiter <me@reitermark.us>
2020-12-10 07:32:17 +0100

    itubedownloader: use `sha256 :no_check` for unversioned URL

--- Casks/itubedownloader.rb
+++ Casks/itubedownloader.rb
@@ -1,6 +1,6 @@
 cask "itubedownloader" do
   version "6.5.27"
-  sha256 "bf4ff8452dc10ffcf99fa1de8e200cba8b99e0c4cadc9fc6f772ce5c80fca05c"
+  sha256 :no_check
 
   # itubedownloader.s3.us-east-2.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://itubedownloader.s3.us-east-2.amazonaws.com/iTubeDownloader.dmg"
