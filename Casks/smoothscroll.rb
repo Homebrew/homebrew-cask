fc1fe1b4634cae0555abe2e6fa262dd3dc2b9b41
Markus Reiter <me@reitermark.us>
2020-12-10 07:32:25 +0100

    smoothscroll: use `sha256 :no_check` for unversioned URL

--- Casks/smoothscroll.rb
+++ Casks/smoothscroll.rb
@@ -1,6 +1,6 @@
 cask "smoothscroll" do
   version "1.5.0"
-  sha256 "03b2e96516c71b3c39e80f67aaca11260ae58517a7ca53d613c3a0e068e6ce04"
+  sha256 :no_check
 
   url "https://www.smoothscroll.net/mac/download/SmoothScroll.app.zip"
   appcast "https://updater.smoothscroll.net/mac/updater.xml"
