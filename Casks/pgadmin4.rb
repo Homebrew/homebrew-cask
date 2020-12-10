27b39c1808705d7f62796ed53c7dc425951bb270
Randall <ran.dall@icloud.com>
2020-12-10 10:32:44 -0800

    Update pgadmin4 from 4.28 to 4.29

--- Casks/pgadmin4.rb
+++ Casks/pgadmin4.rb
@@ -1,6 +1,6 @@
 cask "pgadmin4" do
-  version "4.28"
-  sha256 "5493bbf06bb3a6f917065801e2d1480c97e4889030f20b32c9dbc6779621ed75"
+  version "4.29"
+  sha256 "9f461dc26393793e21c93862138f92fe120147c7d661cb9056c92dfda25a29dc"
 
   # ftp.postgresql.org/ was verified as official when first introduced to the cask
   url "https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v#{version}/macos/pgadmin4-#{version}.dmg"
