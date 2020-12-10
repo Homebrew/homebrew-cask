9f8baaa1bbb94a9607e0a5725be792ebb826f966
Florian Floimair <f.floimair@commend.com>
2020-12-10 11:31:50 +0100

    Update wireshark from 3.4.0 to 3.4.1

--- Casks/wireshark.rb
+++ Casks/wireshark.rb
@@ -1,6 +1,6 @@
 cask "wireshark" do
-  version "3.4.0"
-  sha256 "b0a09d499de618de8f17a0456047dd3839b78c3a57e64b6caf5a55ae5c398d8a"
+  version "3.4.1"
+  sha256 "a5bb21db6410c3151f48afc39e0d0e4a270bb87b8102e8b9348df3e3455bb241"
 
   url "https://2.na.dl.wireshark.org/osx/Wireshark%20#{version}%20Intel%2064.dmg"
   appcast "https://www.wireshark.org/update/0/Wireshark/0.0.0/macOS/x86-64/en-US/stable.xml"
@@ -23,7 +23,6 @@ cask "wireshark" do
   binary "#{appdir}/Wireshark.app/Contents/MacOS/editcap"
   binary "#{appdir}/Wireshark.app/Contents/MacOS/idl2wrs"
   binary "#{appdir}/Wireshark.app/Contents/MacOS/mergecap"
-  binary "#{appdir}/Wireshark.app/Contents/MacOS/mmdbresolve"
   binary "#{appdir}/Wireshark.app/Contents/MacOS/randpkt"
   binary "#{appdir}/Wireshark.app/Contents/MacOS/rawshark"
   binary "#{appdir}/Wireshark.app/Contents/MacOS/reordercap"
