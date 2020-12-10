7fbf08055d32556b7f48fb4439ab19e308305fb8
Brigitte <brigitte@corecode.io>
2020-12-10 18:06:53 +0100

    Update zeronet from 0.6.5 to 0.7.1

--- Casks/zeronet.rb
+++ Casks/zeronet.rb
@@ -1,9 +1,9 @@
 cask "zeronet" do
-  version "0.6.5"
+  version "0.7.1"
   sha256 :no_check
 
   # github.com/HelloZeroNet/ZeroNet-mac/ was verified as official when first introduced to the cask
-  url "https://github.com/HelloZeroNet/ZeroNet-mac/archive/dist/ZeroNet-mac.zip"
+  url "https://github.com/HelloZeroNet/ZeroNet-dist/archive/mac/ZeroNet-dist-mac.zip"
   name "ZeroNet"
   homepage "https://zeronet.io/"
 
