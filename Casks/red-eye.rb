545246713b7e36e5f56048ab95bd748f593060d4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-10 15:32:33 +0100

    Update red-eye from 1.1 to 1.1.0,15 (#94898)

--- Casks/red-eye.rb
+++ Casks/red-eye.rb
@@ -1,9 +1,10 @@
 cask "red-eye" do
-  version "1.1"
+  version "1.1.0,15"
   sha256 :no_check
 
   url "https://www.hexedbits.com/downloads/redeye.zip"
-  appcast "https://www.hexedbits.com/redeye/"
+  appcast "https://www.hexedbits.com/redeye/",
+          must_contain: version.before_comma.chomp(".0")
   name "Red Eye"
   homepage "https://www.hexedbits.com/redeye/"
 
