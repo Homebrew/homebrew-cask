b469954815bd99567f06fcbe89b53fc2f0724240
Markus Reiter <me@reitermark.us>
2020-12-10 13:27:03 +0100

    Update prizmo.rb

--- Casks/prizmo.rb
+++ Casks/prizmo.rb
@@ -2,8 +2,8 @@ cask "prizmo" do
   version "4.1"
   sha256 "ec6e8d70f604efa17b9380ef1e45b4ee93d232f7942098192a27a6e35d2501d6"
 
-  # creaceed.s3.amazonaws.com/downloads/ was verified as official when first introduced to the cask
-  url "https://creaceed.s3.amazonaws.com/downloads/prizmo#{version.major}_#{version}.zip"
+  url "https://creaceed.s3.amazonaws.com/downloads/prizmo#{version.major}_#{version}.zip",
+      verified: "creaceed.s3.amazonaws.com/downloads/"
   appcast "https://www.creaceed.com/appcasts/prizmo#{version.major}.xml"
   name "Prizmo"
   desc "Scanning application with Optical Character Recognition (OCR)"
