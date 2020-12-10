f1a7b4c3217fc8f0bf1a5040a3a8038eec41ea43
yhu266 <56977205+yhu266@users.noreply.github.com>
2020-12-10 14:50:39 -0800

    fix indentation

--- Casks/hstracker.rb
+++ Casks/hstracker.rb
@@ -4,7 +4,7 @@ cask "hstracker" do
 
   # github.com/HearthSim/HSTracker/ was verified as official when first introduced to the cask
   url "https://github.com/HearthSim/HSTracker/releases/download/#{version}/HSTracker.app.zip",
-    verified: "github.com/HearthSim/HSTracker/"
+      verified: "github.com/HearthSim/HSTracker/"
   appcast "https://github.com/HearthSim/HSTracker/releases.atom"
   name "Hearthstone Deck Tracker"
   desc "Deck tracker and deck manager for Hearthstone"
