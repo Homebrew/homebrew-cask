109aa4ba763bf7b0c10bd1329ba598fbc4802350
Randall <ran.dall@icloud.com>
2020-12-10 10:09:10 -0800

    Update pushplaylabs-sidekick from 87.7.36.5759-3cb1b0e-df to 87.7.36.5858-5b8f7ee-df

--- Casks/pushplaylabs-sidekick.rb
+++ Casks/pushplaylabs-sidekick.rb
@@ -1,6 +1,6 @@
 cask "pushplaylabs-sidekick" do
-  version "87.7.36.5759-3cb1b0e-df"
-  sha256 "0db4e8414ba3d3e86768c23625fa691ab6105383d7844e8c50f33a66a222fbe9"
+  version "87.7.36.5858-5b8f7ee-df"
+  sha256 "0680b01e5f1c6312d061475029f1603f825ccafa1f1bcc7d2293d25946eaca1a"
 
   url "https://cdn.meetsidekick.com/builds/sidekick-mac-release-x64-#{version}.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://api.meetsidekick.com/downloads/df/mac"
