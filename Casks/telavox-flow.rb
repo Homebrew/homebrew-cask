05c837d25a5e6cadec4a7e5a60849430a3ba47b8
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-10 10:56:17 -0800

    Update telavox-flow from 1.111.1 to 1.112.0 (#95163)

--- Casks/telavox-flow.rb
+++ Casks/telavox-flow.rb
@@ -1,6 +1,6 @@
 cask "telavox-flow" do
-  version "1.111.1"
-  sha256 "800aa82b7b2e0205cde696fa3a3d5e957c6b033d760740563916457b4bee0e5e"
+  version "1.112.0"
+  sha256 "d526fe00d39dc21a1899067c6bea7128659063c5918286a84d2a4ebe476a8e6d"
 
   # s3.eu-west-2.amazonaws.com/flow-desktop/ was verified as official when first introduced to the cask
   url "https://s3.eu-west-2.amazonaws.com/flow-desktop/Telavox-#{version}.dmg"
