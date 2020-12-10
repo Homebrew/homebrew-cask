2bc9a0c15b17cf38672e5bebc7743d352d2bcd71
suschizu <brigitte@corecode.io>
2020-12-10 21:08:09 +0100

    Update houdahspot (#95138)

--- Casks/houdahspot.rb
+++ Casks/houdahspot.rb
@@ -1,6 +1,6 @@
 cask "houdahspot" do
   version "6.0"
-  sha256 "649e6db7eaef750abcdb2f9d256cd0f69b48ef0d2f384eceb997e6859587a07e"
+  sha256 "4989462868d8a74bd20f17040e0f899d9ae8899de6b3cfd8b1364cd33722d8f8"
 
   url "https://dl.houdah.com/houdahSpot/updates/cast#{version.major}_assets/HoudahSpot#{version}.zip"
   appcast "https://www.houdah.com/houdahSpot/updates/cast#{version.major}.xml"
@@ -8,7 +8,7 @@ cask "houdahspot" do
   desc "File searching application"
   homepage "https://www.houdah.com/houdahSpot/"
 
-  depends_on macos: ">= :high_sierra"
+  depends_on macos: ">= :mojave"
 
   app "HoudahSpot.app"
 end
