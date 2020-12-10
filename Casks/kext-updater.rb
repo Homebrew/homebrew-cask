c0eb5d88e568ecfcf840efa9b23094d9c19965e6
Markus Reiter <me@reitermark.us>
2020-12-10 07:32:17 +0100

    kext-updater: use `sha256 :no_check` for unversioned URL

--- Casks/kext-updater.rb
+++ Casks/kext-updater.rb
@@ -1,6 +1,6 @@
 cask "kext-updater" do
   version "3.7.6"
-  sha256 "9d06223a453d6d514b104c66ac257bb2831b431505f01a76c5603c27f1954f3c"
+  sha256 :no_check
 
   url "https://update.kextupdater.de/kextupdater/kextupdaterng.zip"
   appcast "https://update.kextupdater.de/kextupdater/appcastng.xml"
