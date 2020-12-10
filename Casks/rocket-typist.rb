d372359f72276249c9e304cfcaad08746340d682
Markus Reiter <me@reitermark.us>
2020-12-10 07:32:23 +0100

    rocket-typist: use `sha256 :no_check` for unversioned URL

--- Casks/rocket-typist.rb
+++ Casks/rocket-typist.rb
@@ -1,6 +1,6 @@
 cask "rocket-typist" do
   version "2.3"
-  sha256 "26b1dd1a8fcbe5f09f6f754073501db71d52cb89cd5eb21784edfbdc739a0939"
+  sha256 :no_check
 
   url "https://witt-software.com/downloads/rockettypist/Rocket%20Typist.dmg"
   appcast "https://witt-software.com/downloads/rockettypist/appcast.xml"
