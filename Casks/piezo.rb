db7d2e3edf521a12893f8689cf1cd363f00a8f15
CoreCode <core-code@users.noreply.github.com>
2020-12-10 08:57:16 +0100

    Update piezo from 1.6.10 to 1.7.0 (#95084)

--- Casks/piezo.rb
+++ Casks/piezo.rb
@@ -1,6 +1,6 @@
 cask "piezo" do
-  version "1.6.10"
-  sha256 "54d5c93c2f66356b66ffcfd2fd9bb87e893d0aacb3f0c6b8d16350d967498947"
+  version "1.7.0"
+  sha256 "9addf37d00565ef323dd8fe6b37d314060baf1fff740bd9b359fba51c1d7ea67"
 
   url "https://rogueamoeba.com/piezo/download/Piezo.zip"
   appcast "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Piezo&system=10146"
