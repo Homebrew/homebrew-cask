0a7c47b07b267f130bf6ec5366605cd98a3e5c2e
Markus Reiter <me@reitermark.us>
2020-12-10 14:12:42 +0100

    Update ledger-live.rb

--- Casks/ledger-live.rb
+++ Casks/ledger-live.rb
@@ -1,5 +1,5 @@
 cask "ledger-live" do
-  version "2.18.0"
+  version "2.17.1"
   sha256 "55d9fea34355cf036ddcf4e4151feab81da52a1386d5231888d1ee06cf2863c0"
 
   # github.com/LedgerHQ/ledger-live-desktop/ was verified as official when first introduced to the cask
