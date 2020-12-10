7118b1a0ac5387de77c1656f749a2ed963a1eaa6
Randall <ran.dall@icloud.com>
2020-12-10 10:29:01 -0800

    Update securityspy

--- Casks/securityspy.rb
+++ Casks/securityspy.rb
@@ -1,6 +1,6 @@
 cask "securityspy" do
   version "5.3.1"
-  sha256 :no_check
+  sha256 "fe83ffc06b6eb48f8178cfbb3b5853ac959e5c8063832523e0f17a3b8200f9eb"
 
   url "https://www.bensoftware.com/securityspy/SecuritySpy.dmg"
   appcast "https://www.bensoftware.com/securityspy/versionhistory.html",
