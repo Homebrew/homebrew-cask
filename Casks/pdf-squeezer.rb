4ea74fc414fd47aa55b682d1102ba44723236ce1
Markus Reiter <me@reitermark.us>
2020-12-10 07:32:21 +0100

    pdf-squeezer: use `sha256 :no_check` for unversioned URL

--- Casks/pdf-squeezer.rb
+++ Casks/pdf-squeezer.rb
@@ -1,6 +1,6 @@
 cask "pdf-squeezer" do
   version "4.2.1"
-  sha256 "33fed893440985b5b422fa24bcdf68fff7490467525472c93d7960f353842d4d"
+  sha256 :no_check
 
   url "https://www.witt-software.com/downloads/pdfsqueezer/PDF%20Squeezer.dmg"
   appcast "https://www.witt-software.com/downloads/pdfsqueezer/pdfsq4-appcast.xml"
