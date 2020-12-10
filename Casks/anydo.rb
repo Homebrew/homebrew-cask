b93ae6586498063bebdc34e4b4f9b87694460d2e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-10 15:12:29 +0100

    Update anydo from 4.2.122 to 4.2.123 (#94872)

--- Casks/anydo.rb
+++ Casks/anydo.rb
@@ -1,8 +1,8 @@
 cask "anydo" do
   version "4.2.123"
-  sha256 :no_check
+  sha256 "96e1a31f80919316e76d60672be68eb51305de68f3d9ebbd320b3f3f2717d404"
 
-  url "https://electron-app.any.do/Any.do.dmg"
+  url "https://electron-app.any.do/Any.do-#{version}.dmg"
   appcast "https://electron-app.any.do/latest-mac.yml"
   name "Any.do"
   desc "Reminder, planner & calendar"
