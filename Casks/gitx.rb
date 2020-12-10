eda68758728e3c479812ded3bff101082f0aaec0
Markus Reiter <me@reitermark.us>
2020-12-10 07:32:15 +0100

    gitx: use `sha256 :no_check` for unversioned URL

--- Casks/gitx.rb
+++ Casks/gitx.rb
@@ -1,6 +1,6 @@
 cask "gitx" do
   version "0.7.1"
-  sha256 "d28a2cc1a1d0b83908e7b7fa47706c4e4fab3570277b2a7eae0324b1d86a4b87"
+  sha256 :no_check
 
   url "http://frim.frim.nl/GitXStable.app.zip"
   appcast "http://gitx.frim.nl/Downloads/appcast.xml"
