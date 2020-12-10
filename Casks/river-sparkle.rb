76bdcb1e7a64b66e9b9dc3a0e97314c1de3cfe58
Julian Mayer <julian@corecode.io>
2020-12-10 04:08:46 +0100

    Update river-sparkle

--- Casks/river-sparkle.rb
+++ Casks/river-sparkle.rb
@@ -1,6 +1,6 @@
 cask "river-sparkle" do
   version "3.0.3,8354"
-  sha256 :no_check
+  sha256 "cb23d7cb63e4457f1f2bb5f91fa3e5f80ec5318b80b4de9be527daf3545f8c88"
 
   url "https://sparkleapp.com/update/Sparkle.zip"
   appcast "https://sparkleapp.com/update/appcast.php?lang=en"
