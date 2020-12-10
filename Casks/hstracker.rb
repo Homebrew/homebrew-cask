afdce773111ec6ad6a04536bd90bba8020ecaa60
yhu266 <56977205+yhu266@users.noreply.github.com>
2020-12-10 14:46:56 -0800

    add `verified` parameter

--- Casks/hstracker.rb
+++ Casks/hstracker.rb
@@ -3,7 +3,8 @@ cask "hstracker" do
   sha256 "b02edee06d2fe1a13844565b82f47e467c0798ca11205b5b3bb1c6de45373e22"
 
   # github.com/HearthSim/HSTracker/ was verified as official when first introduced to the cask
-  url "https://github.com/HearthSim/HSTracker/releases/download/#{version}/HSTracker.app.zip"
+  url "https://github.com/HearthSim/HSTracker/releases/download/#{version}/HSTracker.app.zip",
+    verified: "github.com/HearthSim/HSTracker/"
   appcast "https://github.com/HearthSim/HSTracker/releases.atom"
   name "Hearthstone Deck Tracker"
   desc "Deck tracker and deck manager for Hearthstone"
