b4bb74d9c4d86a2b83d91951d2f06c36db7c0cf6
Markus Reiter <me@reitermark.us>
2020-12-10 07:32:18 +0100

    loopback: use `sha256 :no_check` for unversioned URL

--- Casks/loopback.rb
+++ Casks/loopback.rb
@@ -1,6 +1,6 @@
 cask "loopback" do
   version "2.1.9"
-  sha256 "3178b068a8b30ea5e361295746ac999f8501f9168cae43e627c63fcefc8403f9"
+  sha256 :no_check
 
   # d2oxtzozd38ts8.cloudfront.net/loopback/ was verified as official when first introduced to the cask
   url "https://d2oxtzozd38ts8.cloudfront.net/loopback/download/Loopback.zip"
