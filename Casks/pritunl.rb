4423394e9dfe03d0c47d4975d807a701364bf5d9
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-11 06:16:40 +0800

    Update pritunl.rb

--- Casks/pritunl.rb
+++ Casks/pritunl.rb
@@ -1,9 +1,13 @@
 cask "pritunl" do
-  version "1.2.2631.70"
-  sha256 "11b50134633c1fbaa138f5181fd14c42d61461b5b74a9010326c6e0b04f863ba"
+  if MacOS.version <= :catalina
+    version "1.2.2631.70"
+    sha256 "11b50134633c1fbaa138f5181fd14c42d61461b5b74a9010326c6e0b04f863ba"
+  else
+    version "v1.2.2615.73"
+    sha256 "275f8498fb736a0a1ddebcbfcb6f03e82b8eb4fedf9b7eb61b0e306cb6de4a71"
 
-  # github.com/pritunl/pritunl-client-electron/ was verified as official when first introduced to the cask
-  url "https://github.com/pritunl/pritunl-client-electron/releases/download/#{version}/Pritunl.pkg.zip"
+  url "https://github.com/pritunl/pritunl-client-electron/releases/download/#{version}/Pritunl.pkg.zip",
+      verified: "github.com/pritunl/pritunl-client-electron/"
   appcast "https://github.com/pritunl/pritunl-client-electron/releases.atom"
   name "Pritunl"
   desc "OpenVPN client"
