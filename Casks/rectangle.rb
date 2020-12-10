896db9250803ad82af9e6e25328df82dd2b1d8b2
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-11 05:16:56 +0800

    rectangle 0.40 (#95185)

--- Casks/rectangle.rb
+++ Casks/rectangle.rb
@@ -1,9 +1,9 @@
 cask "rectangle" do
-  version "0.39"
-  sha256 "2c6dc013c1a201a174872883ceee8d216c5e5d013b75501ac91ff343a2e62bc8"
+  version "0.40"
+  sha256 "70ad0351d512e0538d5b3ffc94dd48038d2e5e68077be8dc93f113baa21c3d3e"
 
-  # github.com/rxhanson/Rectangle/ was verified as official when first introduced to the cask
-  url "https://github.com/rxhanson/Rectangle/releases/download/v#{version}/Rectangle#{version}.dmg"
+  url "https://github.com/rxhanson/Rectangle/releases/download/v#{version}/Rectangle#{version}.dmg",
+      verified: "github.com/rxhanson/Rectangle/"
   appcast "https://www.rectangleapp.com/downloads/updates.xml"
   name "Rectangle"
   desc "Move and resize windows using keyboard shortcuts or snap areas"
