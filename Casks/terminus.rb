680ba9070ebaa361ecd4db9b28db840c02fdefc0
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-10 00:57:46 -0800

    Update terminus from 1.0.121 to 1.0.122 (#95046)

--- Casks/terminus.rb
+++ Casks/terminus.rb
@@ -1,6 +1,6 @@
 cask "terminus" do
-  version "1.0.121"
-  sha256 "c66ed2dda1a9256ca59c8974b9e6dda32c9765e8dd56c799928e5108b301f779"
+  version "1.0.122"
+  sha256 "3250680cf9a059ef7535278326cc5937f3d0eff56df700054411ee7e1bc7cdc5"
 
   # github.com/Eugeny/terminus/ was verified as official when first introduced to the cask
   url "https://github.com/Eugeny/terminus/releases/download/v#{version}/terminus-#{version}-macos.zip"
