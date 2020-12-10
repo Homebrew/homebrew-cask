109b28eb896b3fd229510193a140b3ee19b7dbcd
Nick Christensen <nick.g.christensen@gmail.com>
2020-12-10 14:14:28 -0600

    Update bitbar.rb (#95155)

--- Casks/bitbar.rb
+++ Casks/bitbar.rb
@@ -1,8 +1,8 @@
 cask "bitbar" do
-  version "1.9.2"
-  sha256 "9e317d58143f544ab3b2b35e4d0ef2f11b9578fe6872cec1415da961acb1aee4"
+  version "1.10.0"
+  sha256 "d0e023583d443f98f2adb3c307da01de608abc98d03887e5fd85d3004716d0fb"
 
-  url "https://github.com/matryer/bitbar/releases/download/v#{version}/BitBar-v#{version}.zip"
+  url "https://github.com/matryer/bitbar/releases/download/v#{version}/BitBar.app.zip"
   appcast "https://github.com/matryer/bitbar/releases.atom"
   name "BitBar"
   desc "Utility to display the output from any script or program in the menu bar"
