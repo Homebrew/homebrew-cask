b5e29b6797d48fa4bde612adcfcfe08748b4eb22
Randall <ran.dall@icloud.com>
2020-12-10 10:26:10 -0800

    Update openphone from 2.0.16 to 2.0.17

--- Casks/openphone.rb
+++ Casks/openphone.rb
@@ -1,6 +1,6 @@
 cask "openphone" do
-  version "2.0.16"
-  sha256 "fcf64cd3a731689a82c0a8a355fe788393a69858eab9d3b8ff6839d34ec051d6"
+  version "2.0.17"
+  sha256 "f3171bc0ddc60d728f0e43b1347c3000654c538b87eba72eb4e311be298cec32"
 
   url "https://download.openphone.co/OpenPhone-#{version}.dmg"
   appcast "https://s3-us-west-2.amazonaws.com/download.openphone.co/latest-mac.yml"
