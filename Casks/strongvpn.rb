5980a1c8471c6c37b212fcba71481b1914e978a0
Markus Reiter <me@reitermark.us>
2020-12-10 07:32:25 +0100

    strongvpn: use `sha256 :no_check` for unversioned URL

--- Casks/strongvpn.rb
+++ Casks/strongvpn.rb
@@ -1,6 +1,6 @@
 cask "strongvpn" do
   version "2.2.2,45556"
-  sha256 "a86e394249155fdca02f65ebad8bc269274b69aee6134b9b41e0abaff69030bc"
+  sha256 :no_check
 
   # static.colomovers.com/ was verified as official when first introduced to the cask
   url "https://static.colomovers.com/mac/StrongVPN.dmg"
