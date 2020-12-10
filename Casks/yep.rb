6a78c814c7050a2113a0d333ed7b3de61d6d1ec4
Markus Reiter <me@reitermark.us>
2020-12-10 07:32:28 +0100

    yep: use `sha256 :no_check` for unversioned URL

--- Casks/yep.rb
+++ Casks/yep.rb
@@ -1,6 +1,6 @@
 cask "yep" do
   version "4.0.2"
-  sha256 "89654e9e62c16d2a06ee8990cd65f0c8adf10c7cbde2e4c628f3b88c6a6fc189"
+  sha256 :no_check
 
   url "https://www.ironicsoftware.com/downloads/Yep.zip"
   appcast "https://ironicsoftware.com/downloads/yep.xml"
