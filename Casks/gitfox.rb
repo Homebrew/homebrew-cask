2c6daa5e15dadd457b53e0a9601f5fc6ada30ef3
maksiuP <jrh.mjr@gmail.com>
2020-12-11 00:15:23 +0100

    Mark Gitfox.app as auto-updating

--- Casks/gitfox.rb
+++ Casks/gitfox.rb
@@ -3,10 +3,13 @@ cask "gitfox" do
   sha256 :no_check
 
   # storage.googleapis.com/gitfox/ was verified as official when first introduced to the cask
-  url "https://storage.googleapis.com/gitfox/Gitfox.latest.stable.zip"
+  url "https://storage.googleapis.com/gitfox/Gitfox.latest.stable.zip",
+      verified: "storage.googleapis.com/gitfox/"
   name "Gitfox"
   desc "Git client"
   homepage "https://www.gitfox.app/"
+
+  auto_updates true
 
   app "Gitfox.app"
 
