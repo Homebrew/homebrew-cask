d83596b4f060a4fc21bd8029507e77996ac9f019
Markus Reiter <me@reitermark.us>
2020-12-10 07:32:20 +0100

    night-owl: use `sha256 :no_check` for unversioned URL

--- Casks/night-owl.rb
+++ Casks/night-owl.rb
@@ -1,6 +1,6 @@
 cask "night-owl" do
   version "2.92"
-  sha256 "f35185d46b11153b26f3dc452cd9432abb45b4c18192718fd20805960252b351"
+  sha256 :no_check
 
   # aki-null.net/ was verified as official when first introduced to the cask
   url "https://aki-null.net/yf/NightOwl.zip"
