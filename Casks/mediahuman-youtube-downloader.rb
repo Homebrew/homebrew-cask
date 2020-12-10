5d839c1ee4e8028ed47c62595371762127b506b6
Julian Mayer <julian@corecode.io>
2020-12-10 01:07:58 +0100

    Update mediahuman-youtube-downloader

--- Casks/mediahuman-youtube-downloader.rb
+++ Casks/mediahuman-youtube-downloader.rb
@@ -1,6 +1,6 @@
 cask "mediahuman-youtube-downloader" do
   version "3.9.9.49"
-  sha256 :no_check
+  sha256 "c67c2c2a538c12b69ad34c9ed47e4895e413a2582355d5844c2788506c095278"
 
   url "https://www.mediahuman.com/download/YouTubeDownloader.dmg"
   name "MediaHuman YouTube Downloader"
