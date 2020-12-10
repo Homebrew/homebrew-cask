f36d6c596cc47bcdad8bade096a763539ba9e2ad
Markus Reiter <me@reitermark.us>
2020-12-10 07:32:13 +0100

    dcommander: use `sha256 :no_check` for unversioned URL

--- Casks/dcommander.rb
+++ Casks/dcommander.rb
@@ -1,6 +1,6 @@
 cask "dcommander" do
   version "3881"
-  sha256 "fc6b5b1424d892c821aa20a32a082f0a6f8a1e233b66459de43d05ad9d9b124b"
+  sha256 :no_check
 
   url "https://devstorm-apps.com/dc/download.php"
   appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://devstorm-apps.com/dc/download.php"
