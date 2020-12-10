4adb4299f22b928e3a69a87cce29f34c876130e7
Randall <ran.dall@icloud.com>
2020-12-10 09:39:42 -0800

    Update ibackup-viewer

--- Casks/ibackup-viewer.rb
+++ Casks/ibackup-viewer.rb
@@ -1,6 +1,6 @@
 cask "ibackup-viewer" do
   version "4.1730"
-  sha256 :no_check
+  sha256 "3a3fd1084e78a0c208e615e8f2a69f43dc33dc0d3127beb7e58454b124146601"
 
   url "https://www.imactools.com/download/iBackupViewer.dmg"
   appcast "https://www.imactools.com/update/ibackupviewer.xml"
