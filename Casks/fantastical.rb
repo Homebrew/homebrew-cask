22c7f1467bfaebe2ec5239db7445b84e4130eab8
CoreCode <core-code@users.noreply.github.com>
2020-12-10 21:14:13 +0100

    Update fantastical.rb

--- Casks/fantastical.rb
+++ Casks/fantastical.rb
@@ -1,6 +1,6 @@
 cask "fantastical" do
-  version "3.3.2"
-  sha256 "28d8536c7f4b7f1eb1082318eee8d3f0a49a72a3fba0ec93d957c2833c364289"
+  version "3.3.3"
+  sha256 "50cac4fa7e1341fae11bb9d01ef1c091980001ed406be31570fd8f6c216e0070"
 
   url "https://cdn.flexibits.com/Fantastical_#{version}.zip"
   appcast "https://flexibits.com/fantastical/appcast2.php"
