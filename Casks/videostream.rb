ac379593e0f1507b63f38e60fb4832a0bb4b0953
Markus Reiter <me@reitermark.us>
2020-12-10 07:32:27 +0100

    videostream: use `sha256 :no_check` for unversioned URL

--- Casks/videostream.rb
+++ Casks/videostream.rb
@@ -1,6 +1,6 @@
 cask "videostream" do
   version "0.4.3"
-  sha256 "bcf6d31217985b454c7496e5c2df90f5a6fe9902171daae6d1b9baac0878a200"
+  sha256 :no_check
 
   url "https://cdn.getvideostream.com/videostream-native-updates/macOS/Videostream.pkg"
   appcast "https://videostream-cdn.s3.amazonaws.com/videostream-native-updates/macOS/manifest.json"
