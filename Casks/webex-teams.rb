349c213012bcb0bc5df0f0ee212f9e13856204e3
Julian Mayer <julian@corecode.io>
2020-12-10 12:28:34 +0100

    Update webex-teams from latest to 40.12.0.17293

--- Casks/webex-teams.rb
+++ Casks/webex-teams.rb
@@ -1,6 +1,6 @@
 cask "webex-teams" do
-  version :latest
-  sha256 :no_check
+  version "40.12.0.17293"
+  sha256 "3cc95a8f393ae89aecafc4842f4a306b4c4728eea898263f3081b49e237e2fad"
 
   url "https://binaries.webex.com/WebexTeamsDesktop-MACOS-Gold/WebexTeams.dmg"
   name "Webex Teams"
