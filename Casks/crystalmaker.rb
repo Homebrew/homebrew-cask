000feee938009c0fead16d2d914df3b81ce96c55
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-10 21:01:32 +0100

    Update crystalmaker from 10.5.5 to 10.5.5,46714 (#94801)

--- Casks/crystalmaker.rb
+++ Casks/crystalmaker.rb
@@ -1,9 +1,10 @@
 cask "crystalmaker" do
-  version "10.6.0"
+  version "10.6.0,46910"
   sha256 :no_check
 
   url "http://crystalmaker.com/downloads/crystalmaker_mac.zip"
-  appcast "http://crystalmaker.com/crystalmaker/release-notes/mac/#{version.major}/index.html"
+  appcast "http://crystalmaker.com/crystalmaker/release-notes/mac/#{version.major}/index.html",
+          must_contain: version.before_comma.chomp(".0")
   name "CrystalMaker"
   desc "Visualize crystal and molecular structures"
   homepage "http://crystalmaker.com/index.html"
