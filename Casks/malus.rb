8809412e490019fa8d006a105ba0cef8419af6cf
i0ntempest <szf1234@me.com>
2020-12-11 18:56:05 -0500

    Update malus from 1.5.1,152 to 1.6.0,160

--- Casks/malus.rb
+++ Casks/malus.rb
@@ -1,9 +1,9 @@
 cask "malus" do
-  version "1.5.1,152"
-  sha256 "dfca2126b338fc5836b9abba87860922b23455712a15bb854aa604aedf3d4efd"
+  version "1.6.0,160"
+  sha256 "85a86088ed17617d3a9917e31c25e4b601ef0e83cf5d662a01436ccef207841b"
 
   # malus.s3cdn.net/ was verified as official when first introduced to the cask
-  url "https://malus.s3cdn.net/uploads/Malus-mac-#{version.after_comma}.dmg"
+  url "https://malus.s3cdn.net/uploads/mac_malus_#{version.before_comma.dots_to_underscores}.dmg"
   appcast "https://api.getmalus.com/api/checkDesktopUpdate?type=mac"
   name "Malus"
   desc "Proxy to help accessing various online media resources/services"
