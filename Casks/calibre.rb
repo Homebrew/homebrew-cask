09264bce9c31c5ca91fe1f7667a06fc0ad35d795
Shiva Huang <shiva.huang@cocoazen.com>
2020-12-12 01:20:37 +0800

    Update calibre from 5.7.0 to 5.7.1

--- Casks/calibre.rb
+++ Casks/calibre.rb
@@ -4,8 +4,8 @@ cask "calibre" do
     sha256 "68829cd902b8e0b2b7d5cf7be132df37bcc274a1e5720b4605d2dd95f3a29168"
     url "https://download.calibre-ebook.com/#{version}/calibre-#{version}.dmg"
   else
-    version "5.7.0"
-    sha256 "da58c5aaf2fed244c58c1dc5909cfc4b96c3564d9c691a5d594fcb16c7fd67cf"
+    version "5.7.1"
+    sha256 "1b7c4dd4b0875fbfc3f7984f8f71d854bd83c9f9c6c750411d471130f3965c47"
     url "https://github.com/kovidgoyal/calibre/releases/download/v#{version}/calibre-#{version}.dmg",
         verified: "github.com/kovidgoyal/calibre/"
     appcast "https://github.com/kovidgoyal/calibre/releases.atom"
