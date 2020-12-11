8926891a9157f2d58757958ddf9cae41df99a9fc
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-11 09:07:50 -0800

    Update radarr from 3.0.0.4204 to 3.0.1.4259 (#95246)

--- Casks/radarr.rb
+++ Casks/radarr.rb
@@ -1,6 +1,6 @@
 cask "radarr" do
-  version "3.0.0.4204"
-  sha256 "eb9b9e95cc657d7fa904ea879613f1080524290009724b04ab24ce4afae16070"
+  version "3.0.1.4259"
+  sha256 "fe54a06eb7ad52facd0f449d76c1c7fdc8d8ca4433ed1916af80091508bdbefb"
 
   # github.com/Radarr/Radarr/ was verified as official when first introduced to the cask
   url "https://github.com/Radarr/Radarr/releases/download/v#{version}/Radarr.master.#{version}.osx-app-core-x64.zip"
