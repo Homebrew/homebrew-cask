abf834d83eb65f2b4353b096a614778f1b202260
Christian Kraemer <christian.kraemer@gmail.com>
2020-12-11 21:40:52 +0100

    Update qownnotes from 20.12.3 to 20.12.4 (#95292)

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,6 +1,6 @@
 cask "qownnotes" do
-  version "20.12.3"
-  sha256 "96f96e21a6af502b0c2add8be1e442282f0d22b637c6a1b0d24814a9ec02057d"
+  version "20.12.4"
+  sha256 "42c9bdeeda66d0386f84a8000bc64406604a0e7b9261782921916a3b0938ffa4"
 
   # github.com/pbek/QOwnNotes/ was verified as official when first introduced to the cask
   url "https://github.com/pbek/QOwnNotes/releases/download/v#{version}/QOwnNotes.dmg"
