2d070de7fa929a8dd3e7c7c3c46119f64a6d0102
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-11 14:19:12 +0800

    Update bome-network.rb

--- Casks/bome-network.rb
+++ Casks/bome-network.rb
@@ -3,13 +3,12 @@ cask "bome-network" do
   sha256 "be2549d8ad526f0e08ef136edf785005840ad3c554de68583788f89714744d5c"
 
   url "https://download.bome.com/BomeNet#{version}.dmg"
+  appcast "https://www.bome.com/products/bomenet#downloads"
   name "Bome Network"
   desc "Create MIDI connections between computers"
-  homepage "https://www.bome.com/products/bomenet#"
+  homepage "https://www.bome.com/products/bomenet"
 
   app "Bome Network.app"
 
-  zap trash: [
-    "~/Library/Preferences/com.bome.mt.player.plist",
-  ]
+  zap trash: "~/Library/Preferences/com.bome.mt.player.plist"
 end
