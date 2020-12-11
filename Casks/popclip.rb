afceb97085c03c6563e751da61ce28fd92a67794
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-11 09:27:33 -0800

    Update popclip from 2020.10 to 2020.12 (#95252)

--- Casks/popclip.rb
+++ Casks/popclip.rb
@@ -1,6 +1,6 @@
 cask "popclip" do
-  version "2020.10"
-  sha256 "c968666b645d96aa93b30391b27d68384b6bc26af58147809f78904641876443"
+  version "2020.12"
+  sha256 "1f186088aa42d8659d9608e042c551d59764440193dc77c04ae8f5df32a53ac9"
 
   url "https://pilotmoon.com/downloads/PopClip-#{version}.zip"
   appcast "https://softwareupdate.pilotmoon.com/update/popclip/appcast.xml"
