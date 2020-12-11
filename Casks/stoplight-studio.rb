bb53db705543f31321fa1f8278683c02e1ba9138
Brigitte <brigitte@corecode.io>
2020-12-11 20:30:08 +0100

    Update stoplight-studio from 2.1.0 to 2.2.0,5282.b08742e

--- Casks/stoplight-studio.rb
+++ Casks/stoplight-studio.rb
@@ -1,9 +1,9 @@
 cask "stoplight-studio" do
-  version "2.1.0"
-  sha256 "b7348eb320ebe5e65889f0b1fffac29bbd49949adbf8cb781486050d3762156d"
+  version "2.2.0,5282.b08742e"
+  sha256 "8712f55657d5d9283bb0e1d11718b8e55cbceb87bcbd90bf9d4952a6602eae6c"
 
   # github.com/stoplightio/studio/ was verified as official when first introduced to the cask
-  url "https://github.com/stoplightio/studio/releases/download/v#{version}/stoplight-studio-mac.dmg"
+  url "https://github.com/stoplightio/studio/releases/download/v#{version.before_comma}-stable.#{version.after_comma}/stoplight-studio-mac.dmg"
   appcast "https://github.com/stoplightio/studio/releases.atom"
   name "Stoplight Studio"
   desc "Editor for designing and documenting APIs"
