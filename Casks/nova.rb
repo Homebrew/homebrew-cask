81afd7f363c07f39540f0de0d9e61f78a7ccf031
Jason Short <jason.short@wpengine.com>
2020-12-11 10:10:00 -0600

    symlink to nova CLI tool, not app bundle

--- Casks/nova.rb
+++ Casks/nova.rb
@@ -3,7 +3,8 @@ cask "nova" do
   sha256 "f399a2af01360057b0f314ac3945b233a24a8d4b24e5972ce093190cc8cc4918"
 
   # download-cdn.panic.com/ was verified as official when first introduced to the cask
-  url "https://download-cdn.panic.com/nova/Nova%20#{version}.zip"
+  url "https://download-cdn.panic.com/nova/Nova%20#{version}.zip",
+      verified: "https://download-cdn.panic.com/nova/"
   name "Panic Nova"
   desc "Native code editor"
   homepage "https://nova.app/"
@@ -12,7 +13,7 @@ cask "nova" do
   depends_on macos: ">= :mojave"
 
   app "Nova.app"
-  binary "#{appdir}/Nova.app/Contents/MacOS/Nova", target: "nova"
+  binary "#{appdir}/Nova.app/Contents/SharedSupport/nova", target: "nova"
 
   zap trash: "~/Library/Application Support/Nova"
 end
