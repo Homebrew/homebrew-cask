1a28b2a82f45e273b8d90dd911a8b3a5f75dc9e6
Yifan Hu <56977205+yhu266@users.noreply.github.com>
2020-12-10 17:35:46 -0800

    Update hstracker from 1.7.6 to 1.7.7 (#95196)
    * Update hstracker from 1.7.6 to 1.7.7
    
    * add `verified` parameter
    
    * fix indentation

--- Casks/hstracker.rb
+++ Casks/hstracker.rb
@@ -1,9 +1,10 @@
 cask "hstracker" do
-  version "1.7.6"
-  sha256 "b7bae557537cbd1ae31c32e3e956a8326e97bb37dde03f5646e77eba48c62006"
+  version "1.7.7"
+  sha256 "b02edee06d2fe1a13844565b82f47e467c0798ca11205b5b3bb1c6de45373e22"
 
   # github.com/HearthSim/HSTracker/ was verified as official when first introduced to the cask
-  url "https://github.com/HearthSim/HSTracker/releases/download/#{version}/HSTracker.app.zip"
+  url "https://github.com/HearthSim/HSTracker/releases/download/#{version}/HSTracker.app.zip",
+      verified: "github.com/HearthSim/HSTracker/"
   appcast "https://github.com/HearthSim/HSTracker/releases.atom"
   name "Hearthstone Deck Tracker"
   desc "Deck tracker and deck manager for Hearthstone"
