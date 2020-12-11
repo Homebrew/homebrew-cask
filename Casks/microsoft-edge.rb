28bde5b95c4aa14d441efbe8efefcff34e663d45
Andrey Izotov <faradaytrs@gmail.com>
2020-12-11 09:26:55 +0800

    Update microsoft-edge from 87.0.664.57 to 87.0.664.60 (#95206)

--- Casks/microsoft-edge.rb
+++ Casks/microsoft-edge.rb
@@ -1,6 +1,6 @@
 cask "microsoft-edge" do
-  version "87.0.664.57"
-  sha256 "7a8e760ce4a0566ca752e5b0fb70e41f96b0da65ed3979ff2f395444c742b9eb"
+  version "87.0.664.60"
+  sha256 "e509141923dc3dbe5085c9a5f4f62c5e7cfba75f2ef3cdea1838e5152ed9f3ae"
 
   # officecdn-microsoft-com.akamaized.net/ was verified as official when first introduced to the cask
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdge-#{version}.pkg"
