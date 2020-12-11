87ab3302d24e4102ede856671a0eb8748cfc0fbd
Markus Reiter <me@reitermark.us>
2020-12-12 00:27:53 +0100

    spark-ar-studio: remove checksum for unversioned URL (#95297)
    * spark-ar-studio: remove checksum for unversioned URL
    
    * Update spark-ar-studio.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/spark-ar-studio.rb
+++ Casks/spark-ar-studio.rb
@@ -1,6 +1,6 @@
 cask "spark-ar-studio" do
-  version "102.0"
-  sha256 "8c651a82adc2fc1e87cb79dbc01137e0af5de3f389c2afb4f5d5d471d642a762"
+  version "103.0"
+  sha256 :no_check
 
   url "https://www.facebook.com/sparkarmacos/download/"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.facebook.com/sparkarmacos/download/",
