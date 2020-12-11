c01f5276a704d97c84e45765cf41f1eafc27b230
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-11 10:40:18 -0800

    Update runway from 0.16.2 to 0.16.3 (#95276)

--- Casks/runway.rb
+++ Casks/runway.rb
@@ -1,6 +1,6 @@
 cask "runway" do
-  version "0.16.2"
-  sha256 "2d96dd0e43c1d66e1b184f427331c7e53ef8110b16074b87998dfe8cea37163e"
+  version "0.16.3"
+  sha256 "c5fc36cc071078aa8eef6057a5a7a53b6f90ad15654a50b61dcd53f810c1ab57"
 
   # runway-releases.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://runway-releases.s3.amazonaws.com/Runway-#{version}.dmg"
