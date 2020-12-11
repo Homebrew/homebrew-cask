e5753113bc140f3302fd7d55e032fdf1d6000a58
suschizu <brigitte@corecode.io>
2020-12-11 20:41:05 +0100

    Update lingon-x from 8.0.1 to 8.0.2 (#95287)

--- Casks/lingon-x.rb
+++ Casks/lingon-x.rb
@@ -3,8 +3,8 @@ cask "lingon-x" do
     version "6.6.5"
     sha256 "b0231b1a98dcc8f5c4234b419c9f5331407b8cce29b33f0ea2e32b12595adfa8"
   else
-    version "8.0.1"
-    sha256 "d4958c69b7cf5210388fe129a58bc8c7bb915179914ec8749db468fc86972573"
+    version "8.0.2"
+    sha256 "227ed28788a6ae537e49b6808ec0ec7ad72dc718c87729cadd04c1c1151ded42"
   end
 
   url "https://www.peterborgapps.com/downloads/LingonX#{version.major}.zip"
