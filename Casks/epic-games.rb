068b32507dc1e09ff5cf9ea93e573326492c700e
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:32 +0100

    epic-games: switch to `verified` parameter

--- Casks/epic-games.rb
+++ Casks/epic-games.rb
@@ -2,8 +2,8 @@ cask "epic-games" do
   version "10.19.2"
   sha256 "ec45afdb5de87368bbbeb3953332176c9652c957565b69b90b8072385bff9ad3"
 
-  # epicgames-download1.akamaized.net/ was verified as official when first introduced to the cask
-  url "https://epicgames-download1.akamaized.net/Builds/UnrealEngineLauncher/Installers/Mac/EpicInstaller-#{version}.dmg"
+  url "https://epicgames-download1.akamaized.net/Builds/UnrealEngineLauncher/Installers/Mac/EpicInstaller-#{version}.dmg",
+      verified: "epicgames-download1.akamaized.net/"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncher.dmg"
   name "Epic Games Launcher"
   desc "Launcher for *Epic Games* games"
