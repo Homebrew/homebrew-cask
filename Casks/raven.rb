705a6f6d5331f322c730050bf52223178284e6bb
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:45 +0100

    raven: switch to `verified` parameter

--- Casks/raven.rb
+++ Casks/raven.rb
@@ -2,8 +2,8 @@ cask "raven" do
   version "0.2.0-alpha"
   sha256 "91792dfbe2abbc13f79bcafbe903e697f99b5abccfa900084b015e2c642eb633"
 
-  # github.com/robotlolita/raven/ was verified as official when first introduced to the cask
-  url "https://github.com/robotlolita/raven/releases/download/v#{version}/Raven-osx.zip"
+  url "https://github.com/robotlolita/raven/releases/download/v#{version}/Raven-osx.zip",
+      verified: "github.com/robotlolita/raven/"
   appcast "https://github.com/robotlolita/raven/releases.atom"
   name "Raven"
   desc "Minimal, distraction-free text editor"
