fbfdd13d84a369ca24412e805a28e450a5d26af5
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:07 +0100

    kindle: switch to `verified` parameter

--- Casks/kindle.rb
+++ Casks/kindle.rb
@@ -2,8 +2,8 @@ cask "kindle" do
   version "1.30.59055"
   sha256 "1b70345bb96394f5662c1dfedf15a318e83f259553c83d81e32fc1e25fa251ab"
 
-  # kindleformac.s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://kindleformac.s3.amazonaws.com/#{version.patch}/KindleForMac-#{version}.dmg"
+  url "https://kindleformac.s3.amazonaws.com/#{version.patch}/KindleForMac-#{version}.dmg",
+      verified: "kindleformac.s3.amazonaws.com/"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.amazon.com/kindlemacdownload/ref=klp_hz_mac"
   name "Kindle for Mac"
   desc "Interface for reading and syncing eBooks"
