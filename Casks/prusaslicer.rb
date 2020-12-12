d6164ba18e670f4beebe18756995b5a234140211
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:41 +0100

    prusaslicer: switch to `verified` parameter

--- Casks/prusaslicer.rb
+++ Casks/prusaslicer.rb
@@ -2,8 +2,8 @@ cask "prusaslicer" do
   version "2.2.0,202003211132"
   sha256 "4634361151e0ceb0b75cff6edafa523e3cc92d7979789c32ca50861eb0c3c7c6"
 
-  # github.com/prusa3d/PrusaSlicer/ was verified as official when first introduced to the cask
-  url "https://github.com/prusa3d/PrusaSlicer/releases/download/version_#{version.before_comma}/PrusaSlicer-#{version.before_comma}+-#{version.after_comma}.dmg"
+  url "https://github.com/prusa3d/PrusaSlicer/releases/download/version_#{version.before_comma}/PrusaSlicer-#{version.before_comma}+-#{version.after_comma}.dmg",
+      verified: "github.com/prusa3d/PrusaSlicer/"
   appcast "https://github.com/prusa3d/PrusaSlicer/releases.atom"
   name "PrusaSlicer"
   desc "G-code generator for 3D printers (RepRap, Makerbot, Ultimaker etc.)"
