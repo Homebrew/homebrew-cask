3391be43cd7921a65982c3ad7ebbaf16e3134022
Markus Reiter <me@reitermark.us>
2020-12-12 05:20:53 +0100

    airdroid: switch to `verified` parameter

--- Casks/airdroid.rb
+++ Casks/airdroid.rb
@@ -2,8 +2,8 @@ cask "airdroid" do
   version "3.6.9.1"
   sha256 "5537131e9c14032f2e962f00209a0f8c03a21fe0cae0b6cc3e46e1b2d5c79d67"
 
-  # s3.amazonaws.com/dl.airdroid.com/ was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/dl.airdroid.com/AirDroid_Desktop_Client_#{version}.dmg"
+  url "https://s3.amazonaws.com/dl.airdroid.com/AirDroid_Desktop_Client_#{version}.dmg",
+      verified: "s3.amazonaws.com/dl.airdroid.com/"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://srv3.airdroid.com/p20/web/getbinaryredirect?type=dmg&channel=&version="
   name "AirDroid"
   desc "Cross-platform mobile device management suite"
