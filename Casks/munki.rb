945e1b6aee0a26d155ece706954707c533e6a98a
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:25 +0100

    munki: switch to `verified` parameter

--- Casks/munki.rb
+++ Casks/munki.rb
@@ -2,8 +2,8 @@ cask "munki" do
   version "5.0.1.4059"
   sha256 "2f638ee7aebdee58ddc721c8203a12e16f6bdc49e94c31514d581c5961c78c5e"
 
-  # github.com/munki/munki/ was verified as official when first introduced to the cask
-  url "https://github.com/munki/munki/releases/download/v#{version.major_minor_patch}/munkitools-#{version}.pkg"
+  url "https://github.com/munki/munki/releases/download/v#{version.major_minor_patch}/munkitools-#{version}.pkg",
+      verified: "github.com/munki/munki/"
   appcast "https://github.com/munki/munki/releases.atom",
           must_contain: version.major_minor_patch
   name "Munki"
