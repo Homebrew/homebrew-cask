62a695561e7b55944bb1f5eba303e3f7bca89dd8
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:24 +0100

    mucommander: switch to `verified` parameter

--- Casks/mucommander.rb
+++ Casks/mucommander.rb
@@ -2,8 +2,8 @@ cask "mucommander" do
   version "0.9.5-1"
   sha256 "476ffd9e271163513d9fde2d015d1da3f2777e54b913b10d4ef49ac7e016bae1"
 
-  # github.com/mucommander/mucommander/ was verified as official when first introduced to the cask
-  url "https://github.com/mucommander/mucommander/releases/download/#{version}/mucommander-#{version}.dmg"
+  url "https://github.com/mucommander/mucommander/releases/download/#{version}/mucommander-#{version}.dmg",
+      verified: "github.com/mucommander/mucommander/"
   appcast "https://github.com/mucommander/mucommander/releases.atom"
   name "muCommander"
   desc "Lightweight, cross-platform file manager with a dual-pane interface"
