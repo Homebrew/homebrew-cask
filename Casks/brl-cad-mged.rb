749ecdb30313fefce3194d9c5f9ddea5ffdd98ee
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:10 +0100

    brl-cad-mged: switch to `verified` parameter

--- Casks/brl-cad-mged.rb
+++ Casks/brl-cad-mged.rb
@@ -2,8 +2,8 @@ cask "brl-cad-mged" do
   version "7.24.0"
   sha256 "5fe9eb9f5a2eb5544cf459b02e7e880040e02d1312d04c2207b90a731ebd8f0f"
 
-  # downloads.sourceforge.net/brlcad/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/brlcad/BRL-CAD%20for%20Mac%20OS%20X/#{version}/BRL-CAD%20#{version}.dmg"
+  url "https://downloads.sourceforge.net/brlcad/BRL-CAD%20for%20Mac%20OS%20X/#{version}/BRL-CAD%20#{version}.dmg",
+      verified: "downloads.sourceforge.net/brlcad/"
   appcast "https://sourceforge.net/projects/brlcad/rss?path=/BRL-CAD%20for%20Mac%20OS%20X"
   name "BRL-CAD"
   homepage "https://brlcad.org/"
