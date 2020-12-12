5e411ef7ac4ea3ac7643c3cb1b6a69d3b6094ef7
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:37 +0100

    forecast: switch to `verified` parameter

--- Casks/forecast.rb
+++ Casks/forecast.rb
@@ -2,8 +2,8 @@ cask "forecast" do
   version "0.9.1_129"
   sha256 "71425799cf7bb247cd83d0ff9275a1170245a0610b98d4613d186671da0fd49a"
 
-  # d2uzvmey2c90kn.cloudfront.net/appcast_download/ was verified as official when first introduced to the cask
-  url "https://d2uzvmey2c90kn.cloudfront.net/appcast_download/Forecast_#{version}.zip"
+  url "https://d2uzvmey2c90kn.cloudfront.net/appcast_download/Forecast_#{version}.zip",
+      verified: "d2uzvmey2c90kn.cloudfront.net/appcast_download/"
   appcast "https://overcast.fm/appcast/Forecast.xml"
   name "Forecast"
   homepage "https://overcast.fm/forecast"
