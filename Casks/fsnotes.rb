68163e9a1a9c3cd23a644d2a977a40415822582b
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:39 +0100

    fsnotes: switch to `verified` parameter

--- Casks/fsnotes.rb
+++ Casks/fsnotes.rb
@@ -2,8 +2,8 @@ cask "fsnotes" do
   version "4.9.2"
   sha256 "b4141358d4c4dc5e129463e2eda18bc2f207f7339b72554fffafa46c654c3a70"
 
-  # github.com/glushchenko/fsnotes/ was verified as official when first introduced to the cask
-  url "https://github.com/glushchenko/fsnotes/releases/download/#{version}/FSNotes_#{version}.zip"
+  url "https://github.com/glushchenko/fsnotes/releases/download/#{version}/FSNotes_#{version}.zip",
+      verified: "github.com/glushchenko/fsnotes/"
   appcast "https://github.com/glushchenko/fsnotes/releases.atom"
   name "FSNotes"
   desc "Notes manager"
