4de5156a59bd07060ddd762ae0af6bfda456f996
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:54 +0100

    sky-ticket: switch to `verified` parameter

--- Casks/sky-ticket.rb
+++ Casks/sky-ticket.rb
@@ -2,8 +2,8 @@ cask "sky-ticket" do
   version "8.2.0"
   sha256 :no_check
 
-  # web.static.nowtv.com/watch/player/skyticket/ was verified as official when first introduced to the cask
-  url "https://web.static.nowtv.com/watch/player/skyticket/de/latest/SkyTicket-Mac.dmg"
+  url "https://web.static.nowtv.com/watch/player/skyticket/de/latest/SkyTicket-Mac.dmg",
+      verified: "web.static.nowtv.com/watch/player/skyticket/"
   name "Sky Ticket Player"
   desc "Stream Sky Ticket content"
   homepage "https://skyticket.sky.de/"
