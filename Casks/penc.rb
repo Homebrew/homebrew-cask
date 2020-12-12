81ff6deb5c7a752e01a07ff88194d98f10cec683
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:37 +0100

    penc: switch to `verified` parameter

--- Casks/penc.rb
+++ Casks/penc.rb
@@ -2,8 +2,8 @@ cask "penc" do
   version "0.4.1"
   sha256 "89c4b239189e13993a0d5189e7eafdc9652dde41fe2edaea4746abb779e3c014"
 
-  # github.com/dgurkaynak/Penc/ was verified as official when first introduced to the cask
-  url "https://github.com/dgurkaynak/Penc/releases/download/#{version}/Penc-#{version}.dmg"
+  url "https://github.com/dgurkaynak/Penc/releases/download/#{version}/Penc-#{version}.dmg",
+      verified: "github.com/dgurkaynak/Penc/"
   appcast "https://github.com/dgurkaynak/Penc/releases.atom"
   name "Penc"
   desc "Trackpad-oriented window manager"
