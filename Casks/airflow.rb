72a6faeb8fab7aa769ab3e4c147e761379f3ae65
Markus Reiter <me@reitermark.us>
2020-12-12 05:20:53 +0100

    airflow: switch to `verified` parameter

--- Casks/airflow.rb
+++ Casks/airflow.rb
@@ -2,8 +2,8 @@ cask "airflow" do
   version "3.2.0"
   sha256 "71f078b3fdce42b40dabf6131dc28d563487e1759db538732a6e91cc1328ba05"
 
-  # cdn.downloads.iocave.net/Airflow/ was verified as official when first introduced to the cask
-  url "https://cdn.downloads.iocave.net/Airflow/Airflow%20#{version}.zip"
+  url "https://cdn.downloads.iocave.net/Airflow/Airflow%20#{version}.zip",
+      verified: "cdn.downloads.iocave.net/Airflow/"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://airflowapp.com/download/latest?mac"
   name "Airflow"
   desc "Watch local content on Apple TV and Chromecast"
