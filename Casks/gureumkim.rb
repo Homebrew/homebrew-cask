7afebd35f0624f8ae3a3a3c85e5192fcfde8bf95
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:45 +0100

    gureumkim: switch to `verified` parameter

--- Casks/gureumkim.rb
+++ Casks/gureumkim.rb
@@ -2,8 +2,8 @@ cask "gureumkim" do
   version "1.11.1"
   sha256 "f1e7db79b49a7675dbfc8ff57b4b55eaa55b3018803c6c46671c88b402b496c2"
 
-  # github.com/gureum/gureum/ was verified as official when first introduced to the cask
-  url "https://github.com/gureum/gureum/releases/download/#{version}/Gureum-#{version}.pkg"
+  url "https://github.com/gureum/gureum/releases/download/#{version}/Gureum-#{version}.pkg",
+      verified: "github.com/gureum/gureum/"
   appcast "https://github.com/gureum/gureum/releases.atom"
   name "구름 입력기"
   homepage "https://gureum.io/"
