91549bd5866d7c7e26a064b7ca3a4e545eb77ae4
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:37 +0100

    foxmail: switch to `verified` parameter

--- Casks/foxmail.rb
+++ Casks/foxmail.rb
@@ -2,8 +2,8 @@ cask "foxmail" do
   version "1.4.3.94169"
   sha256 "e9821bd3d3b9ed3b46ca7a8195a51c4d53fdc5877adc3916dbb37734165a0008"
 
-  # dldir1.qq.com/foxmail/MacFoxmail/ was verified as official when first introduced to the cask
-  url "https://dldir1.qq.com/foxmail/MacFoxmail/Foxmail_for_Mac_#{version}.dmg"
+  url "https://dldir1.qq.com/foxmail/MacFoxmail/Foxmail_for_Mac_#{version}.dmg",
+      verified: "dldir1.qq.com/foxmail/MacFoxmail/"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.foxmail.com/mac/download"
   name "Foxmail"
   desc "Email client"
