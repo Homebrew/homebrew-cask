c40f13d3a95db2074a930f1dabdd8256683dbd88
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:48 +0100

    http-toolkit: switch to `verified` parameter

--- Casks/http-toolkit.rb
+++ Casks/http-toolkit.rb
@@ -2,8 +2,8 @@ cask "http-toolkit" do
   version "1.0.2"
   sha256 "2a0283f9a3f41dae350753413aae85a5b35a250248da22b2564bd3ed4c68e305"
 
-  # github.com/httptoolkit/httptoolkit-desktop/ was verified as official when first introduced to the cask
-  url "https://github.com/httptoolkit/httptoolkit-desktop/releases/download/v#{version}/HTTP.Toolkit.dmg"
+  url "https://github.com/httptoolkit/httptoolkit-desktop/releases/download/v#{version}/HTTP.Toolkit.dmg",
+      verified: "github.com/httptoolkit/httptoolkit-desktop/"
   appcast "https://github.com/httptoolkit/httptoolkit-desktop/releases.atom"
   name "HTTP Toolkit"
   homepage "https://httptoolkit.tech/"
