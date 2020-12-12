9d03481ce3c7602a96070a6fa2376ceecbc11913
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:46 +0100

    react-studio: switch to `verified` parameter

--- Casks/react-studio.rb
+++ Casks/react-studio.rb
@@ -2,8 +2,8 @@ cask "react-studio" do
   version "1.7.35,404"
   sha256 "f0706819d1c4dfd1c64173a2c4a0d91d3de6c0d93b0428241ad1c07d6fd99906"
 
-  # s3.amazonaws.com/sc.neonto.com/ was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/sc.neonto.com/ReactStudio_v#{version.before_comma.no_dots}_build#{version.after_comma}.zip"
+  url "https://s3.amazonaws.com/sc.neonto.com/ReactStudio_v#{version.before_comma.no_dots}_build#{version.after_comma}.zip",
+      verified: "s3.amazonaws.com/sc.neonto.com/"
   appcast "https://www.macupdater.net/cgi-bin/extract_text/send_post_request.cgi?url=https://reactstudio.com/api/download/reactstudio",
           must_contain: version.before_comma.no_dots
   name "ReactStudio"
