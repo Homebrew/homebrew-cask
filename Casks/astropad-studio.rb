4b571ba2131bc538e17e430d8032697f163da240
Markus Reiter <me@reitermark.us>
2020-12-12 05:20:59 +0100

    astropad-studio: switch to `verified` parameter

--- Casks/astropad-studio.rb
+++ Casks/astropad-studio.rb
@@ -2,8 +2,8 @@ cask "astropad-studio" do
   version "3.5.0"
   sha256 "be952da8a7089186ae413a4b1dd173ae473ab572cd611d5a0a38b062b58082ff"
 
-  # s3.amazonaws.com/astropad.com/ was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/astropad.com/downloads/AstropadStudio-#{version}.dmg"
+  url "https://s3.amazonaws.com/astropad.com/downloads/AstropadStudio-#{version}.dmg",
+      verified: "s3.amazonaws.com/astropad.com/"
   appcast "https://s3.amazonaws.com/astropad.com/downloads/studio-sparkle.xml"
   name "Astropad Studio"
   homepage "https://astropad.com/"
