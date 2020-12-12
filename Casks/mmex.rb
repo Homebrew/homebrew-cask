5a44691ae64c4762b306e4c61c502192a6775bc9
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:22 +0100

    mmex: switch to `verified` parameter

--- Casks/mmex.rb
+++ Casks/mmex.rb
@@ -2,8 +2,8 @@ cask "mmex" do
   version "1.3.5"
   sha256 "ed27e39373c0e74ea856e2cb91b3b6f99fb745b8ec8b83424f7e406e8428fa7d"
 
-  # downloads.sourceforge.net/moneymanagerex/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/moneymanagerex/v#{version}/mmex-#{version}-macos10.9-wx3.1.3.dmg"
+  url "https://downloads.sourceforge.net/moneymanagerex/v#{version}/mmex-#{version}-macos10.9-wx3.1.3.dmg",
+      verified: "downloads.sourceforge.net/moneymanagerex/"
   appcast "https://github.com/moneymanagerex/moneymanagerex/releases.atom"
   name "Money Manager Ex"
   desc "Money management application"
