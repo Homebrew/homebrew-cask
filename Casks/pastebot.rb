bcd736c7318dbbc06f85829c259bacc385cd28ed
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:36 +0100

    pastebot: switch to `verified` parameter

--- Casks/pastebot.rb
+++ Casks/pastebot.rb
@@ -2,8 +2,8 @@ cask "pastebot" do
   version "2.4"
   sha256 "da7b93ca13110fcc4f2adc267c287a62692d25c7a4b09f14e849c5283bb898af"
 
-  # tapbots.net/ was verified as official when first introduced to the cask
-  url "https://tapbots.net/pastebot#{version.major}/Pastebot.dmg"
+  url "https://tapbots.net/pastebot#{version.major}/Pastebot.dmg",
+      verified: "tapbots.net/"
   name "Pastebot"
   desc "Workflow application to improve productivity"
   homepage "https://tapbots.com/pastebot/"
