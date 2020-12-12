a21b4b68eef0c930afb29232e6637cf89c0607bd
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:01 +0100

    syncthing: switch to `verified` parameter

--- Casks/syncthing.rb
+++ Casks/syncthing.rb
@@ -2,8 +2,8 @@ cask "syncthing" do
   version "1.12.0-1"
   sha256 "f5e50107998296d8c73d0d8c9830cb0430553486a9d90f4e3aea5a73303cb6c3"
 
-  # github.com/syncthing/syncthing-macos/ was verified as official when first introduced to the cask
-  url "https://github.com/syncthing/syncthing-macos/releases/download/v#{version}/Syncthing-#{version}.dmg"
+  url "https://github.com/syncthing/syncthing-macos/releases/download/v#{version}/Syncthing-#{version}.dmg",
+      verified: "github.com/syncthing/syncthing-macos/"
   appcast "https://github.com/syncthing/syncthing-macos/releases.atom"
   name "Syncthing"
   desc "Real time file synchronization software"
