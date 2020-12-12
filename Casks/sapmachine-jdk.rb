e2aedb6d48454f1fdb0e9f600fcf81f2a9bdb500
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:49 +0100

    sapmachine-jdk: switch to `verified` parameter

--- Casks/sapmachine-jdk.rb
+++ Casks/sapmachine-jdk.rb
@@ -2,8 +2,8 @@ cask "sapmachine-jdk" do
   version "15.0.1"
   sha256 "dd95938bdf230c052e13819e6ddfd228001abc4fec3afbd82b007b6fe0df7d23"
 
-  # github.com/SAP/SapMachine/ was verified as official when first introduced to the cask
-  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_osx-x64_bin.dmg"
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_osx-x64_bin.dmg",
+      verified: "github.com/SAP/SapMachine/"
   appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
   name "SapMachine OpenJDK Development Kit"
   desc "OpenJDK release maintained and supported by SAP"
