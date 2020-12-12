f8f04e40ea18dd6ef6f99ab2ab1e58dbe123a5db
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:26 +0100

    mymonero: switch to `verified` parameter

--- Casks/mymonero.rb
+++ Casks/mymonero.rb
@@ -2,8 +2,8 @@ cask "mymonero" do
   version "1.1.18"
   sha256 "be2f9f2452263f3084b82f94f39947994549fe98b03a29590d2604b3fabd990a"
 
-  # github.com/mymonero/mymonero-app-js/ was verified as official when first introduced to the cask
-  url "https://github.com/mymonero/mymonero-app-js/releases/download/v#{version}/MyMonero-#{version}.dmg"
+  url "https://github.com/mymonero/mymonero-app-js/releases/download/v#{version}/MyMonero-#{version}.dmg",
+      verified: "github.com/mymonero/mymonero-app-js/"
   appcast "https://github.com/mymonero/mymonero-app-js/releases.atom"
   name "MyMonero"
   desc "Wallet for the Monero cryptocurrency"
