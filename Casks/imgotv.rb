46be4d9a749aab57afdae4fd9ee39a6087dfd465
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:50 +0100

    imgotv: switch to `verified` parameter

--- Casks/imgotv.rb
+++ Casks/imgotv.rb
@@ -2,8 +2,8 @@ cask "imgotv" do
   version "6.3.6"
   sha256 "8fde36fcb283cb83cd4c1a5f8eb8f3c187c5859039dff42b7e702c4e4fb5ee99"
 
-  # download.imgo.tv/ was verified as official when first introduced to the cask
-  url "https://download.imgo.tv/app/mac/#{version}/mgtv-mango-#{version}.dmg"
+  url "https://download.imgo.tv/app/mac/#{version}/mgtv-mango-#{version}.dmg",
+      verified: "download.imgo.tv/"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?user_agent=Mac%20OS%20X&url=https://pcconf.api.mgtv.com/getPcDownloadUrl?source=mango2"
   name "芒果TV"
   homepage "https://www.mgtv.com/app/"
