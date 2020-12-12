2e44af5b3004c9d0bc540e62b568345acc27dc58
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:21 +0100

    dbglass: switch to `verified` parameter

--- Casks/dbglass.rb
+++ Casks/dbglass.rb
@@ -2,8 +2,8 @@ cask "dbglass" do
   version "0.1.0-beta.6"
   sha256 "f79f853299f7578f3b30a9df9b2d027ec73ecf933b91f78e287ad2993176f45d"
 
-  # github.com/web-pal/DBGlass/ was verified as official when first introduced to the cask
-  url "https://github.com/web-pal/DBGlass/releases/download/v#{version}/MAC_OS-X.zip"
+  url "https://github.com/web-pal/DBGlass/releases/download/v#{version}/MAC_OS-X.zip",
+      verified: "github.com/web-pal/DBGlass/"
   appcast "https://github.com/web-pal/dbglass/releases.atom"
   name "DBGlass"
   desc "PostgreSQL client built with Electron"
