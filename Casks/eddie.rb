b2d1c193770572642c1b0792c7f8ac9722915bdd
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:29 +0100

    eddie: switch to `verified` parameter

--- Casks/eddie.rb
+++ Casks/eddie.rb
@@ -2,8 +2,8 @@ cask "eddie" do
   version "2.18.9"
   sha256 "4b6e233496f8d63d1cbab01a2f24e46c480741746c1b1b6e7468442d41c648c1"
 
-  # eddie.website/ was verified as official when first introduced to the cask
-  url "https://eddie.website/download/?platform=macos&arch=x64&ui=ui&format=disk.dmg&version=#{version}"
+  url "https://eddie.website/download/?platform=macos&arch=x64&ui=ui&format=disk.dmg&version=#{version}",
+      verified: "eddie.website/"
   appcast "https://github.com/AirVPN/Eddie/releases.atom"
   name "Air VPN"
   name "Eddie"
