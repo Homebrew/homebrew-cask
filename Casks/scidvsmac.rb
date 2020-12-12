9f44b96e29a10e0c8a9837ca87d15220d7b83d64
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:50 +0100

    scidvsmac: switch to `verified` parameter

--- Casks/scidvsmac.rb
+++ Casks/scidvsmac.rb
@@ -2,8 +2,8 @@ cask "scidvsmac" do
   version "4.21"
   sha256 "985c6bd395a93b14e697ef11ab2c36f4dd1fa374ed8799591ca8eb57c3ad0551"
 
-  # downloads.sourceforge.net/scidvspc/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/scidvspc/ScidvsMac-#{version}.dmg"
+  url "https://downloads.sourceforge.net/scidvspc/ScidvsMac-#{version}.dmg",
+      verified: "downloads.sourceforge.net/scidvspc/"
   appcast "https://sourceforge.net/projects/scidvspc/rss"
   name "Scid vs. Mac"
   homepage "https://scidvspc.sourceforge.io/"
