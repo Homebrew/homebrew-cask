67a18518f2a67b4867b34431a2bdbae1bffc8987
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:39 +0100

    frhelper: switch to `verified` parameter

--- Casks/frhelper.rb
+++ Casks/frhelper.rb
@@ -2,8 +2,8 @@ cask "frhelper" do
   version "3.9.10,2020-10-14"
   sha256 "42a4f6a89c3491be96d5bc883f92f0dcac949cf4b4b0ad63117faf7c071a16e9"
 
-  # static.frdic.com/ was verified as official when first introduced to the cask
   url "https://static.frdic.com/pkg/fhmac.dmg?v=#{version.after_comma}",
+      verified:   "static.frdic.com/",
       user_agent: :fake
   appcast "https://www.eudic.net/v4/fr/app/download",
           must_contain: version.after_comma
