f3744010220104d895174ac1e911dff4d6d5248d
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:17 +0100

    maltego: switch to `verified` parameter

--- Casks/maltego.rb
+++ Casks/maltego.rb
@@ -2,8 +2,8 @@ cask "maltego" do
   version "4.2.13.13462"
   sha256 "d1db4084c4861c552575734689c10b53e2d669ad45fab78b9dc91a572766ae75"
 
-  # maltego-downloads.s3.us-east-2.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://maltego-downloads.s3.us-east-2.amazonaws.com/mac/Maltego.v#{version}.dmg"
+  url "https://maltego-downloads.s3.us-east-2.amazonaws.com/mac/Maltego.v#{version}.dmg",
+      verified: "maltego-downloads.s3.us-east-2.amazonaws.com/"
   appcast "https://maltego-downloads.s3.us-east-2.amazonaws.com/info.json"
   name "Maltego"
   homepage "https://www.maltego.com/pricing-plans/"
