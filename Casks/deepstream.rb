eca68bd9d0f73cb1cfd31feff0e07f70453baac1
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:22 +0100

    deepstream: switch to `verified` parameter

--- Casks/deepstream.rb
+++ Casks/deepstream.rb
@@ -2,8 +2,8 @@ cask "deepstream" do
   version "5.0.5"
   sha256 "5f7203a14a8d353d4be708e0f6d664f8b00434da33bed4c3762e4ee7609693da"
 
-  # github.com/deepstreamIO/deepstream.io/ was verified as official when first introduced to the cask
-  url "https://github.com/deepstreamIO/deepstream.io/releases/download/v#{version}/deepstream.io-mac-#{version}.pkg"
+  url "https://github.com/deepstreamIO/deepstream.io/releases/download/v#{version}/deepstream.io-mac-#{version}.pkg",
+      verified: "github.com/deepstreamIO/deepstream.io/"
   appcast "https://github.com/deepstreamIO/deepstream.io/releases.atom"
   name "deepstream"
   homepage "https://deepstream.io/"
