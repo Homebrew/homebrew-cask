c2745cb6224df697b3ae8cfa3b6b3e0e88de7af8
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:52 +0100

    sidestep: switch to `verified` parameter

--- Casks/sidestep.rb
+++ Casks/sidestep.rb
@@ -2,8 +2,8 @@ cask "sidestep" do
   version "1.4.1"
   sha256 "c25f7748d73b6f915aff268070ef85ca69f2902de98b044b77c49d1e1341d84e"
 
-  # github.com/chetan51/sidestep/ was verified as official when first introduced to the cask
-  url "https://github.com/chetan51/sidestep/releases/download/#{version}/Sidestep.zip"
+  url "https://github.com/chetan51/sidestep/releases/download/#{version}/Sidestep.zip",
+      verified: "github.com/chetan51/sidestep/"
   appcast "https://github.com/chetan51/sidestep/releases.atom"
   name "Sidestep"
   homepage "https://chetansurpur.com/projects/sidestep/"
