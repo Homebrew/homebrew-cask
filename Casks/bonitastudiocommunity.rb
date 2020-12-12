0f33cf023c66b7d1abe8b45fb1712a917b915119
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:07 +0100

    bonitastudiocommunity: switch to `verified` parameter

--- Casks/bonitastudiocommunity.rb
+++ Casks/bonitastudiocommunity.rb
@@ -2,8 +2,8 @@ cask "bonitastudiocommunity" do
   version "7.11.4"
   sha256 "d6c5058ac110c462932819f2330cdcb64a3812cae56ba10527d760ed735313d2"
 
-  # github.com/bonitasoft/bonita-platform-releases was verified as official when first introduced to the cask
-  url "https://github.com/bonitasoft/bonita-platform-releases/releases/download/#{version}/BonitaStudioCommunity-#{version}-x86_64.dmg"
+  url "https://github.com/bonitasoft/bonita-platform-releases/releases/download/#{version}/BonitaStudioCommunity-#{version}-x86_64.dmg",
+      verified: "github.com/bonitasoft/bonita-platform-releases/"
   appcast "https://www.bonitasoft.com/downloads",
           must_contain: version.major_minor
   name "Bonita Studio Community Edition"
