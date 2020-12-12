4db7a8f196fda82c309f88bb36bce4ff2b617346
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:11 +0100

    cacher: switch to `verified` parameter

--- Casks/cacher.rb
+++ Casks/cacher.rb
@@ -2,8 +2,8 @@ cask "cacher" do
   version "2.36.0"
   sha256 "e87e4e429fd710017f08e46c93e37eda5cc9cded57b95ddea43905c253fa2bb3"
 
-  # cacher-download.nyc3.digitaloceanspaces.com/ was verified as official when first introduced to the cask
-  url "https://cacher-download.nyc3.digitaloceanspaces.com/Cacher-#{version}-mac.zip"
+  url "https://cacher-download.nyc3.digitaloceanspaces.com/Cacher-#{version}-mac.zip",
+      verified: "cacher-download.nyc3.digitaloceanspaces.com/"
   appcast "https://cacher-download.nyc3.digitaloceanspaces.com/latest-mac.yml"
   name "Cacher"
   homepage "https://www.cacher.io/"
