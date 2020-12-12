be67906946cf069c76cfd4724261a60e2a330aab
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:12 +0100

    lens: switch to `verified` parameter

--- Casks/lens.rb
+++ Casks/lens.rb
@@ -2,8 +2,8 @@ cask "lens" do
   version "4.0.1"
   sha256 "1aeeba9f31f86cac0861a41c832b6619d1aadec78363b6f870c7d7457f7233d3"
 
-  # github.com/lensapp/lens/ was verified as official when first introduced to the cask
-  url "https://github.com/lensapp/lens/releases/download/v#{version}/Lens-#{version}.dmg"
+  url "https://github.com/lensapp/lens/releases/download/v#{version}/Lens-#{version}.dmg",
+      verified: "github.com/lensapp/lens/"
   appcast "https://github.com/lensapp/lens/releases.atom"
   name "Lens"
   desc "Kubernetes IDE"
