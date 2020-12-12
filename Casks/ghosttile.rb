34e930d7c066f68f105115064cbb7fc215379880
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:41 +0100

    ghosttile: switch to `verified` parameter

--- Casks/ghosttile.rb
+++ Casks/ghosttile.rb
@@ -2,8 +2,8 @@ cask "ghosttile" do
   version "15,1510040474"
   sha256 "6f723c7489a272a82648a3fb2a4ad0f91f6bd3d79eea748ea20a046a3aefe0ab"
 
-  # dl.devmate.com/im.kernelpanic.GhostTile/ was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/im.kernelpanic.GhostTile/#{version.before_comma}/#{version.after_comma}/GhostTile-#{version.before_comma}.zip"
+  url "https://dl.devmate.com/im.kernelpanic.GhostTile/#{version.before_comma}/#{version.after_comma}/GhostTile-#{version.before_comma}.zip",
+      verified: "dl.devmate.com/im.kernelpanic.GhostTile/"
   appcast "https://updates.devmate.com/im.kernelpanic.GhostTile.xml"
   name "Kernelpanic GhostTile"
   name "GhostTile"
