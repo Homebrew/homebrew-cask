6f4928d6d7ad3728f375f7ef015e9fe5432e8085
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:53 +0100

    jabref: switch to `verified` parameter

--- Casks/jabref.rb
+++ Casks/jabref.rb
@@ -2,8 +2,8 @@ cask "jabref" do
   version "5.1"
   sha256 "834b7b2efa4abc9a627a557de6eae4c627bb6860e545ff89e5307c36a3684f59"
 
-  # github.com/JabRef/jabref/ was verified as official when first introduced to the cask
-  url "https://github.com/JabRef/jabref/releases/download/v#{version}/JabRef-#{version}.dmg"
+  url "https://github.com/JabRef/jabref/releases/download/v#{version}/JabRef-#{version}.dmg",
+      verified: "github.com/JabRef/jabref/"
   appcast "https://github.com/JabRef/jabref/releases.atom"
   name "JabRef"
   homepage "https://www.jabref.org/"
