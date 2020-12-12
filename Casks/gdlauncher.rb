4f553a5cf0314bfd6c19ef447637f7243d7ad338
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:40 +0100

    gdlauncher: switch to `verified` parameter

--- Casks/gdlauncher.rb
+++ Casks/gdlauncher.rb
@@ -2,8 +2,8 @@ cask "gdlauncher" do
   version "1.0.11"
   sha256 "df52870383f0f1c4cdd9bb7e6c97212ef9768a78e8cefaa1db5c53f8e1f0fd28"
 
-  # github.com/gorilla-devs/GDLauncher/ was verified as official when first introduced to the cask
-  url "https://github.com/gorilla-devs/GDLauncher/releases/download/v#{version}/GDLauncher-mac-setup.dmg"
+  url "https://github.com/gorilla-devs/GDLauncher/releases/download/v#{version}/GDLauncher-mac-setup.dmg",
+      verified: "github.com/gorilla-devs/GDLauncher/"
   appcast "https://github.com/gorilla-devs/GDLauncher/releases.atom"
   name "GDLauncher"
   desc "Custom Minecraft Launcher"
