53ef052d0d0b51d1897ead928cc40cb106e4b0d9
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:13 +0100

    localdev: switch to `verified` parameter

--- Casks/localdev.rb
+++ Casks/localdev.rb
@@ -2,8 +2,8 @@ cask "localdev" do
   version "0.12.0"
   sha256 "e5e8d9620cd5c67e5a497855b20dde44ff8d482edeae984363d7ba1eed7d296b"
 
-  # pantheon-localdev.s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://pantheon-localdev.s3.amazonaws.com/Localdev-v#{version}.dmg"
+  url "https://pantheon-localdev.s3.amazonaws.com/Localdev-v#{version}.dmg",
+      verified: "pantheon-localdev.s3.amazonaws.com/"
   name "Localdev"
   homepage "https://pantheon.io/localdev"
 
