246a4a4cfc8415d71199610a8ad003edc84709f2
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:26 +0100

    nbtexplorer: switch to `verified` parameter

--- Casks/nbtexplorer.rb
+++ Casks/nbtexplorer.rb
@@ -2,8 +2,8 @@ cask "nbtexplorer" do
   version "2.0.3"
   sha256 "ae410b227d859cdf81dd8563c1686675c9c7835ea15369453e20f229e9a992ec"
 
-  # hocuspocus.taloncrossing.com/ was verified as official when first introduced to the cask
-  url "http://hocuspocus.taloncrossing.com/rii/NBTExplorer-Mac-#{version}.zip"
+  url "http://hocuspocus.taloncrossing.com/rii/NBTExplorer-Mac-#{version}.zip",
+      verified: "hocuspocus.taloncrossing.com/"
   name "NBTExplorer"
   desc "Named Binary Tag (NBT) data editor for Minecraft"
   homepage "https://www.minecraftforum.net/forums/mapping-and-modding-java-edition/minecraft-tools/1262665-nbtexplorer-nbt-editor-for-windows-and-mac"
