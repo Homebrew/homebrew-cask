f486f3f35c2ef53db8a7f88f9a133ef56b419cc4
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:25 +0100

    murus: switch to `verified` parameter

--- Casks/murus.rb
+++ Casks/murus.rb
@@ -2,8 +2,8 @@ cask "murus" do
   version "2.0.4"
   sha256 "6765f891e8b2fc2a72003f14675884465c9956ecf4d9dd6d87ef2dcb9b34ae6b"
 
-  # github.com/TheMurusTeam/Murus/ was verified as official when first introduced to the cask
-  url "https://github.com/TheMurusTeam/Murus/releases/download/v#{version}/murus-#{version}.zip"
+  url "https://github.com/TheMurusTeam/Murus/releases/download/v#{version}/murus-#{version}.zip",
+      verified: "github.com/TheMurusTeam/Murus/"
   appcast "https://github.com/TheMurusTeam/Murus/releases.atom"
   name "Murus Firewall"
   desc "Firewall app"
