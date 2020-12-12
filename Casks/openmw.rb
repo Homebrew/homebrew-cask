8872e25f8dc60f87812ee8e2f5eac3d58e74d394
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:32 +0100

    openmw: switch to `verified` parameter

--- Casks/openmw.rb
+++ Casks/openmw.rb
@@ -2,8 +2,8 @@ cask "openmw" do
   version "0.46.0"
   sha256 "9a123492f3017bff35e6a5bfe27ca55e3a4af406301d41ef255b53c4ded4cd63"
 
-  # github.com/ was verified as official when first introduced to the cask
-  url "https://github.com/OpenMW/openmw/releases/download/openmw-#{version}/OpenMW-#{version}.dmg"
+  url "https://github.com/OpenMW/openmw/releases/download/openmw-#{version}/OpenMW-#{version}.dmg",
+      verified: "github.com/"
   appcast "https://github.com/OpenMW/openmw/releases.atom"
   name "OpenMW"
   desc "Open-source open-world RPG game engine that supports playing Morrowind"
