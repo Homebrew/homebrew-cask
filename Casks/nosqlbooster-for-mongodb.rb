2561305a591dd1cf73cc107a07e6569c865b3eb1
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:28 +0100

    nosqlbooster-for-mongodb: switch to `verified` parameter

--- Casks/nosqlbooster-for-mongodb.rb
+++ Casks/nosqlbooster-for-mongodb.rb
@@ -2,8 +2,8 @@ cask "nosqlbooster-for-mongodb" do
   version "6.2.5"
   sha256 "1e4b79c2f066304cb643829d880e4b45e389c1414a6af12cb08e98e1f7d2a6ee"
 
-  # mongobooster.com was verified as official when first introduced to the cask
-  url "https://s3.mongobooster.com/download/releasesv#{version.major}/nosqlbooster4mongo-#{version}.dmg"
+  url "https://s3.mongobooster.com/download/releasesv#{version.major}/nosqlbooster4mongo-#{version}.dmg",
+      verified: "mongobooster.com/"
   appcast "https://nosqlbooster.com/downloads"
   name "NoSQLBooster for MongoDB"
   homepage "https://nosqlbooster.com/"
