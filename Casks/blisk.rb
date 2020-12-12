2cc7767b8e32a95e8a481ce77cd00b517a930676
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:06 +0100

    blisk: switch to `verified` parameter

--- Casks/blisk.rb
+++ Casks/blisk.rb
@@ -2,8 +2,8 @@ cask "blisk" do
   version "12.0.92.83"
   sha256 "c9cb87f735f27fe0b37921650d8b6b276f2cecc0cdfbed42c95e2b8d67067f0b"
 
-  # bliskcloudstorage.blob.core.windows.net/ was verified as official when first introduced to the cask
-  url "https://bliskcloudstorage.blob.core.windows.net/mac-installers/BliskInstaller_#{version}.dmg"
+  url "https://bliskcloudstorage.blob.core.windows.net/mac-installers/BliskInstaller_#{version}.dmg",
+      verified: "bliskcloudstorage.blob.core.windows.net/"
   appcast "https://blisk.io/"
   name "Blisk Browser"
   homepage "https://blisk.io/"
