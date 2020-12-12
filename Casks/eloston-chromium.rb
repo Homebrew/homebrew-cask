831229d2d256e0637ae3b5bdf6f110adcea9e2a0
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:31 +0100

    eloston-chromium: switch to `verified` parameter

--- Casks/eloston-chromium.rb
+++ Casks/eloston-chromium.rb
@@ -2,8 +2,8 @@ cask "eloston-chromium" do
   version "87.0.4280.88-1.1"
   sha256 "cc2c2da6bea68bd518fdeb6d7562ee4fb5cc9c1af6b533790cecd816f59cee22"
 
-  # github.com/kramred/ungoogled-chromium-macos/ was verified as official when first introduced to the cask
-  url "https://github.com/kramred/ungoogled-chromium-macos/releases/download/#{version}/ungoogled-chromium_#{version}_macos.dmg"
+  url "https://github.com/kramred/ungoogled-chromium-macos/releases/download/#{version}/ungoogled-chromium_#{version}_macos.dmg",
+      verified: "github.com/kramred/ungoogled-chromium-macos/"
   appcast "https://github.com/kramred/ungoogled-chromium-macos/releases.atom"
   name "Ungoogled Chromium"
   homepage "https://ungoogled-software.github.io/ungoogled-chromium-binaries/"
