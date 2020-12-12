a97eb4efed20b5d7b3a46a9263f66bf408529fcd
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:09 +0100

    brewtarget: switch to `verified` parameter

--- Casks/brewtarget.rb
+++ Casks/brewtarget.rb
@@ -2,8 +2,8 @@ cask "brewtarget" do
   version "2.3.0"
   sha256 "f1b3b33029f7d7cd873a1cdbf0741da22085f53dcefa2943dcfa8d35eda89fdd"
 
-  # launchpad.net/brewtarget/ was verified as official when first introduced to the cask
-  url "https://launchpad.net/brewtarget/trunk/#{version}/+download/Brewtarget-#{version}.dmg"
+  url "https://launchpad.net/brewtarget/trunk/#{version}/+download/Brewtarget-#{version}.dmg",
+      verified: "launchpad.net/brewtarget/"
   appcast "https://github.com/Brewtarget/brewtarget/releases.atom"
   name "brewtarget"
   homepage "http://www.brewtarget.org/"
