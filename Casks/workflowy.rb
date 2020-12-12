00d2dd171ac4da52511e2fd7cfef8b13640a97db
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:13 +0100

    workflowy: switch to `verified` parameter

--- Casks/workflowy.rb
+++ Casks/workflowy.rb
@@ -2,8 +2,8 @@ cask "workflowy" do
   version "1.3.5-13423"
   sha256 "f30a5bf21a92e5016e65cdacf9844b689a83aa37930b73946363751f755973e8"
 
-  # github.com/workflowy/desktop/ was verified as official when first introduced to the cask
-  url "https://github.com/workflowy/desktop/releases/download/v#{version}/WorkFlowy.zip"
+  url "https://github.com/workflowy/desktop/releases/download/v#{version}/WorkFlowy.zip",
+      verified: "github.com/workflowy/desktop/"
   appcast "https://github.com/workflowy/desktop/releases.atom"
   name "WorkFlowy"
   desc "Notetaking tool"
