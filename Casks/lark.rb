7d82f03720e0af8bff7d437e35e1902cb7d2d802
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:11 +0100

    lark: switch to `verified` parameter

--- Casks/lark.rb
+++ Casks/lark.rb
@@ -2,8 +2,8 @@ cask "lark" do
   version "3.38.1,de3732"
   sha256 "189b7c72f8107567b6e5d79d4869abac15fd5ca040d549fe2099298e1ac4b14e"
 
-  # sf16-va.larksuitecdn.com/obj/lark-artifact-storage/ was verified as official when first introduced to the cask
-  url "https://sf16-va.larksuitecdn.com/obj/lark-artifact-storage/#{version.after_comma}/Lark-#{version.before_comma}.dmg"
+  url "https://sf16-va.larksuitecdn.com/obj/lark-artifact-storage/#{version.after_comma}/Lark-#{version.before_comma}.dmg",
+      verified: "sf16-va.larksuitecdn.com/obj/lark-artifact-storage/"
   appcast "https://www.larksuite.com/api/downloads"
   name "Lark"
   desc "Project management software"
