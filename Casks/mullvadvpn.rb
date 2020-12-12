ca76710e6822a2fd0f6f8de2b491c82cc520852c
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:25 +0100

    mullvadvpn: switch to `verified` parameter

--- Casks/mullvadvpn.rb
+++ Casks/mullvadvpn.rb
@@ -2,8 +2,8 @@ cask "mullvadvpn" do
   version "2020.7"
   sha256 "7ec7729557afe6291731181173802761596d70e4fe93a4608809ed2f9320331c"
 
-  # github.com/mullvad/mullvadvpn-app/ was verified as official when first introduced to the cask
-  url "https://github.com/mullvad/mullvadvpn-app/releases/download/#{version}/MullvadVPN-#{version}.pkg"
+  url "https://github.com/mullvad/mullvadvpn-app/releases/download/#{version}/MullvadVPN-#{version}.pkg",
+      verified: "github.com/mullvad/mullvadvpn-app/"
   appcast "https://github.com/mullvad/mullvadvpn-app/releases.atom"
   name "Mullvad VPN"
   desc "VPN client"
