779a3f31ae224dec16749d3218445495a028e1a6
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:38 +0100

    freecad: switch to `verified` parameter

--- Casks/freecad.rb
+++ Casks/freecad.rb
@@ -2,8 +2,8 @@ cask "freecad" do
   version "0.18.4,16146"
   sha256 :no_check # required as upstream package is updated in-place
 
-  # github.com/FreeCAD/FreeCAD/ was verified as official when first introduced to the cask
-  url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version.before_comma}/FreeCAD_#{version.major_minor}-#{version.after_comma}-rev1-OSX-x86_64-conda-Qt5-Py3.dmg"
+  url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version.before_comma}/FreeCAD_#{version.major_minor}-#{version.after_comma}-rev1-OSX-x86_64-conda-Qt5-Py3.dmg",
+      verified: "github.com/FreeCAD/FreeCAD/"
   appcast "https://github.com/FreeCAD/FreeCAD/releases.atom"
   name "FreeCAD"
   desc "3D parametric modeler"
