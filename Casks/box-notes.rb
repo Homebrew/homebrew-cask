e9e7f84caf545d5481a05a676cb384fb7d3f3749
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:08 +0100

    box-notes: switch to `verified` parameter

--- Casks/box-notes.rb
+++ Casks/box-notes.rb
@@ -2,8 +2,8 @@ cask "box-notes" do
   version "1.4.0"
   sha256 :no_check
 
-  # e3.boxcdn.net/box-installers/ was verified as official when first introduced to the cask
-  url "https://e3.boxcdn.net/box-installers/boxnotes/mac/latest/Box%20Notes.zip"
+  url "https://e3.boxcdn.net/box-installers/boxnotes/mac/latest/Box%20Notes.zip",
+      verified: "e3.boxcdn.net/box-installers/"
   appcast "https://notes.services.box.com/updates/latest?platform=darwin&v=#{version.major}.0.0"
   name "Box Notes"
   homepage "https://www.box.com/resources/downloads/notes"
