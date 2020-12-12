c8ea3549aa9357bb1caadac1a4834045f56f6a71
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:36 +0100

    parse: switch to `verified` parameter

--- Casks/parse.rb
+++ Casks/parse.rb
@@ -2,8 +2,8 @@ cask "parse" do
   version "3.0.5"
   sha256 "30c305a76beb8913392b74977a6801e817c3477bd3de72ecd1887a8ca57ea888"
 
-  # github.com/parse-community/ was verified as official when first introduced to the cask
-  url "https://github.com/parse-community/parse-cli/releases/download/release_#{version}/parse"
+  url "https://github.com/parse-community/parse-cli/releases/download/release_#{version}/parse",
+      verified: "github.com/parse-community/"
   appcast "https://github.com/parse-community/parse-cli/releases.atom"
   name "Parse"
   homepage "https://parseplatform.org/"
