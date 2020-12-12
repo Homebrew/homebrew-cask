cd359ad830ba9d8f2e4dc826708e49ab7a9d122d
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:20 +0100

    microsoft-r-open: switch to `verified` parameter

--- Casks/microsoft-r-open.rb
+++ Casks/microsoft-r-open.rb
@@ -2,8 +2,8 @@ cask "microsoft-r-open" do
   version "3.5.1"
   sha256 "95739c07d4fb90f9a3aef19269dd4939f368457d928328adf5126b30c4229601"
 
-  # mran.blob.core.windows.net/ was verified as official when first introduced to the cask
-  url "https://mran.blob.core.windows.net/install/mro/#{version}/microsoft-r-open-#{version}.pkg"
+  url "https://mran.blob.core.windows.net/install/mro/#{version}/microsoft-r-open-#{version}.pkg",
+      verified: "mran.blob.core.windows.net/"
   appcast "https://github.com/Microsoft/microsoft-r-open/releases.atom"
   name "Microsoft R Open"
   name "MRO"
