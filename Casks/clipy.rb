bdce5f683dd2b2596c38d835e72867832b09d785
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:14 +0100

    clipy: switch to `verified` parameter

--- Casks/clipy.rb
+++ Casks/clipy.rb
@@ -2,8 +2,8 @@ cask "clipy" do
   version "1.2.1"
   sha256 "dfbb66ce3135fbaa2d64eaeea99a63e63485e322c9746045a1098b1696a1ecd5"
 
-  # github.com/Clipy/Clipy/ was verified as official when first introduced to the cask
-  url "https://github.com/Clipy/Clipy/releases/download/#{version}/Clipy_#{version}.dmg"
+  url "https://github.com/Clipy/Clipy/releases/download/#{version}/Clipy_#{version}.dmg",
+      verified: "github.com/Clipy/Clipy/"
   appcast "https://github.com/Clipy/Clipy/releases.atom"
   name "Clipy"
   desc "Clipboard extension app"
