034336c606ed6a2620a92f2d9b7533644a7c3569
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:10 +0100

    browserosaurus: switch to `verified` parameter

--- Casks/browserosaurus.rb
+++ Casks/browserosaurus.rb
@@ -2,8 +2,8 @@ cask "browserosaurus" do
   version "12.3.1"
   sha256 "1dadf4c75c76fc98be7522b062ba26cd77487b5fd8ac2e2acd66c3193ee114da"
 
-  # github.com/will-stone/browserosaurus/ was verified as official when first introduced to the cask
-  url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-#{version}.dmg"
+  url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-#{version}.dmg",
+      verified: "github.com/will-stone/browserosaurus/"
   appcast "https://github.com/will-stone/browserosaurus/releases.atom"
   name "Browserosaurus"
   desc "Open-source browser prompter"
