1cf693eec74fd7837ec3477264044519efc2a7cb
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:12 +0100

    lelivrescolairefr: switch to `verified` parameter

--- Casks/lelivrescolairefr.rb
+++ Casks/lelivrescolairefr.rb
@@ -2,8 +2,8 @@ cask "lelivrescolairefr" do
   version "2.1.2"
   sha256 :no_check
 
-  # ci.lls.fr/build/latest/mac/ was verified as official when first introduced to the cask
-  url "https://ci.lls.fr/build/latest/mac/Lelivrescolaire.fr.dmg"
+  url "https://ci.lls.fr/build/latest/mac/Lelivrescolaire.fr.dmg",
+      verified: "ci.lls.fr/build/latest/mac/"
   name "Lelivrescolaire.fr"
   homepage "https://www.lelivrescolaire.fr/"
 
