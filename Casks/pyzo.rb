ec7deef16652324a34ec93b077bafadc7591fcd9
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:42 +0100

    pyzo: switch to `verified` parameter

--- Casks/pyzo.rb
+++ Casks/pyzo.rb
@@ -2,8 +2,8 @@ cask "pyzo" do
   version "4.11.2"
   sha256 "3a0d2b25988bd27e13857265926ff28851240c17d5d07063b5e23b389cfa1a14"
 
-  # github.com/pyzo/pyzo/ was verified as official when first introduced to the cask
-  url "https://github.com/pyzo/pyzo/releases/download/v#{version}/pyzo-#{version}-macos.dmg"
+  url "https://github.com/pyzo/pyzo/releases/download/v#{version}/pyzo-#{version}-macos.dmg",
+      verified: "github.com/pyzo/pyzo/"
   appcast "https://github.com/pyzo/pyzo/releases.atom"
   name "Pyzo"
   homepage "https://pyzo.org/"
