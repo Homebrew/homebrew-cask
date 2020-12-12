e81157e56ae65f268c372f76b33da5ffd181eb99
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:46 +0100

    remote-desktop-manager-free: switch to `verified` parameter

--- Casks/remote-desktop-manager-free.rb
+++ Casks/remote-desktop-manager-free.rb
@@ -2,8 +2,8 @@ cask "remote-desktop-manager-free" do
   version "2020.3.3.0"
   sha256 "57018fb8f1535f19b02b19727509430460a0bf41b8172bf4296ebf2edb5b2a27"
 
-  # devolutions.net/ was verified as official when first introduced to the cask
-  url "https://cdn.devolutions.net/download/Mac/Devolutions.RemoteDesktopManager.Free.Mac.#{version}.dmg"
+  url "https://cdn.devolutions.net/download/Mac/Devolutions.RemoteDesktopManager.Free.Mac.#{version}.dmg",
+      verified: "devolutions.net/"
   appcast "https://cdn.devolutions.net/download/Mac/RemoteDesktopManagerFree.xml"
   name "Remote Desktop Manager Free"
   homepage "https://mac.remotedesktopmanager.com/"
