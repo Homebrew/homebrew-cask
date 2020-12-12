86798428dd1412aa035c10ed5c9c1e4d0030ffd8
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:57 +0100

    springtoolsuite: switch to `verified` parameter

--- Casks/springtoolsuite.rb
+++ Casks/springtoolsuite.rb
@@ -2,8 +2,8 @@ cask "springtoolsuite" do
   version "4.8.1.RELEASE,4.17.0"
   sha256 "47d6c58b625b84c529a7392199f28af411ce5c869ffc38ef26eb190be4cf1d3f"
 
-  # download.springsource.com/release/ was verified as official when first introduced to the cask
-  url "https://download.springsource.com/release/STS#{version.major}/#{version.before_comma}/dist/e#{version.after_comma.major_minor}/spring-tool-suite-#{version.major}-#{version.before_comma}-e#{version.after_comma}-macosx.cocoa.x86_64.dmg"
+  url "https://download.springsource.com/release/STS#{version.major}/#{version.before_comma}/dist/e#{version.after_comma.major_minor}/spring-tool-suite-#{version.major}-#{version.before_comma}-e#{version.after_comma}-macosx.cocoa.x86_64.dmg",
+      verified: "download.springsource.com/release/"
   appcast "https://github.com/spring-projects/sts4/releases.atom"
   name "Spring Tool Suite"
   desc "Next generation tooling for Spring Boot"
