39604e6eadc0ee31417200628db1e61fc85810c4
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:40 +0100

    gemini: switch to `verified` parameter

--- Casks/gemini.rb
+++ Casks/gemini.rb
@@ -2,8 +2,8 @@ cask "gemini" do
   version "2.7.2,358:1606230495"
   sha256 "3ffc8ba30913c2d987533905dec6dc1f5580f48637d38bc5aad0b71e23317150"
 
-  # dl.devmate.com/com.macpaw.site.Gemini was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/com.macpaw.site.Gemini#{version.major}/#{version.after_comma.before_colon}/#{version.after_colon}/Gemini#{version.major}-#{version.after_comma.before_colon}.zip"
+  url "https://dl.devmate.com/com.macpaw.site.Gemini#{version.major}/#{version.after_comma.before_colon}/#{version.after_colon}/Gemini#{version.major}-#{version.after_comma.before_colon}.zip",
+      verified: "dl.devmate.com/com.macpaw.site.Gemini"
   appcast "https://updates.devmate.com/com.macpaw.site.Gemini#{version.major}.xml"
   name "Gemini"
   desc "Disk space cleaner that finds and deletes duplicated and similar files"
