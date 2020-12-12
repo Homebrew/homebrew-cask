df786025a12881c8935346d1748426af25e51b37
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:13 +0100

    chatology: switch to `verified` parameter

--- Casks/chatology.rb
+++ Casks/chatology.rb
@@ -2,8 +2,8 @@ cask "chatology" do
   version "1.2.4"
   sha256 "e5ad4c9716afb2b5c1ac56f5b103ff8b8919de0cdda0627abd141c07368df7ad"
 
-  # d60ism0l33mmr.cloudfront.net/ was verified as official when first introduced to the cask
-  url "https://d60ism0l33mmr.cloudfront.net/Chatology_#{version}.zip"
+  url "https://d60ism0l33mmr.cloudfront.net/Chatology_#{version}.zip",
+      verified: "d60ism0l33mmr.cloudfront.net/"
   appcast "https://flexibits.com/chatology/appcast.php"
   name "Chatology"
   desc "Chat manager and message search software"
