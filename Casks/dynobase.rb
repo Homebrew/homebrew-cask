d627a4790d38b73a7d1cf076ad4f405f6a431ef3
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:29 +0100

    dynobase: switch to `verified` parameter

--- Casks/dynobase.rb
+++ Casks/dynobase.rb
@@ -2,8 +2,8 @@ cask "dynobase" do
   version "1.2.0"
   sha256 "82aca8d490a12c13562c0486e902cf65ff895e897526d506d26dd3b9416be700"
 
-  # github.com/Dynobase/dynobase was verified as official when first introduced to the cask
-  url "https://github.com/Dynobase/dynobase/releases/download/#{version}/Dynobase-#{version}.dmg"
+  url "https://github.com/Dynobase/dynobase/releases/download/#{version}/Dynobase-#{version}.dmg",
+      verified: "github.com/Dynobase/dynobase/"
   appcast "https://github.com/Dynobase/dynobase/releases.atom"
   name "Dynobase"
   desc "GUI Client for DynamoDB"
