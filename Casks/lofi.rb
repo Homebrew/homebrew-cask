067ddeadaa540844bf3e1c79a6aaf4de7d94b221
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:14 +0100

    lofi: switch to `verified` parameter

--- Casks/lofi.rb
+++ Casks/lofi.rb
@@ -2,8 +2,8 @@ cask "lofi" do
   version "1.5.0"
   sha256 "a26631070888a0c5937a140fc751bf3af1c8b21767c7893285fed83cb1b813e1"
 
-  # github.com/dvx/lofi/ was verified as official when first introduced to the cask
-  url "https://github.com/dvx/lofi/releases/download/#{version}/lofi-#{version}.dmg"
+  url "https://github.com/dvx/lofi/releases/download/#{version}/lofi-#{version}.dmg",
+      verified: "github.com/dvx/lofi/"
   appcast "https://github.com/dvx/lofi/releases.atom"
   name "Lofi"
   desc "Mini Spotify player with WebGL visualizations"
