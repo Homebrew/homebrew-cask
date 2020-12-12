b01a01e13773851ea46b3df75aee039926c3534d
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:08 +0100

    twilioquest: switch to `verified` parameter

--- Casks/twilioquest.rb
+++ Casks/twilioquest.rb
@@ -2,8 +2,8 @@ cask "twilioquest" do
   version "8.0.0"
   sha256 :no_check
 
-  # firebasestorage.googleapis.com/v0/b/twilioquest-prod.appspot.com was verified as official when first introduced to the cask
-  url "https://firebasestorage.googleapis.com/v0/b/twilioquest-prod.appspot.com/o/launchers%2F8%2Ftwilioquest-mac.zip?alt=media"
+  url "https://firebasestorage.googleapis.com/v0/b/twilioquest-prod.appspot.com/o/launchers%2F8%2Ftwilioquest-mac.zip?alt=media",
+      verified: "firebasestorage.googleapis.com/v0/b/twilioquest-prod.appspot.com/"
   name "TwilioQuest"
   desc "Learn to code and lead your crew on a mission to save The Cloud in a 16bit game"
   homepage "https://www.twilio.com/quest"
