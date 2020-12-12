6c0392e0b00d6760dc27fd954ac7827c0c3713de
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:08 +0100

    ueli: switch to `verified` parameter

--- Casks/ueli.rb
+++ Casks/ueli.rb
@@ -2,8 +2,8 @@ cask "ueli" do
   version "8.10.0"
   sha256 "38e9b6f4558ba32350cd1bdf516e5e70fb04d06f2365420531781c1b09793c58"
 
-  # github.com/oliverschwendener/ueli/ was verified as official when first introduced to the cask
-  url "https://github.com/oliverschwendener/ueli/releases/download/v#{version}/ueli-#{version}.dmg"
+  url "https://github.com/oliverschwendener/ueli/releases/download/v#{version}/ueli-#{version}.dmg",
+      verified: "github.com/oliverschwendener/ueli/"
   appcast "https://github.com/oliverschwendener/ueli/releases.atom"
   name "Ueli"
   desc "Keystroke launcher"
