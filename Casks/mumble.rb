15f0ca6b32048b3c873b394e29fa9e54c4d8dd48
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:25 +0100

    mumble: switch to `verified` parameter

--- Casks/mumble.rb
+++ Casks/mumble.rb
@@ -2,8 +2,8 @@ cask "mumble" do
   version "1.3.3"
   sha256 "dd779a1be92dfb162f7453230f8906f2a7d1799324c5b710fcc5ccd381ff12e5"
 
-  # github.com/mumble-voip/mumble/ was verified as official when first introduced to the cask
-  url "https://github.com/mumble-voip/mumble/releases/download/#{version}/Mumble-#{version}.dmg"
+  url "https://github.com/mumble-voip/mumble/releases/download/#{version}/Mumble-#{version}.dmg",
+      verified: "github.com/mumble-voip/mumble/"
   appcast "https://github.com/mumble-voip/mumble/releases.atom"
   name "Mumble"
   desc "Open-source, low-latency, high quality voice chat software for gaming"
