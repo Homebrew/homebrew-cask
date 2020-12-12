3290e9024ade755c66694d3a5d28a689ded5cac4
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:12 +0100

    wickrme: switch to `verified` parameter

--- Casks/wickrme.rb
+++ Casks/wickrme.rb
@@ -2,8 +2,8 @@ cask "wickrme" do
   version "5.68.7"
   sha256 "f84d7dff662a51f373a36970f39aa6bc2d02fd654ffa3d53bc246fd7d2d1a3d1"
 
-  # s3.amazonaws.com/static.wickr.com/ was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/static.wickr.com/downloads/mac/me/WickrMe-#{version}.dmg"
+  url "https://s3.amazonaws.com/static.wickr.com/downloads/mac/me/WickrMe-#{version}.dmg",
+      verified: "s3.amazonaws.com/static.wickr.com/"
   appcast "https://me-download.wickr.com/api/multiVerify/me/undefined/"
   name "Wickr Me"
   homepage "https://wickr.com/products/personal/"
