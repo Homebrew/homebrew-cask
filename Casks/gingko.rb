f6f5530f46bc8ffc0e3069bffdbfc2d6a925c03f
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:41 +0100

    gingko: switch to `verified` parameter

--- Casks/gingko.rb
+++ Casks/gingko.rb
@@ -2,8 +2,8 @@ cask "gingko" do
   version "2.4.15"
   sha256 "bb1367b0a07a80872be253eda0519ec801e0b96b04d59c5bff3f825cf9e380c4"
 
-  # github.com/gingko/client/ was verified as official when first introduced to the cask
-  url "https://github.com/gingko/client/releases/download/v#{version}/Gingko-#{version}-mac.zip"
+  url "https://github.com/gingko/client/releases/download/v#{version}/Gingko-#{version}-mac.zip",
+      verified: "github.com/gingko/client/"
   appcast "https://github.com/gingko/client/releases.atom"
   name "Gingko"
   desc "Word processor that shows structure and content"
