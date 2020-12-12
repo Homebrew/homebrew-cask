a9d44420ec6b8a388d991810cf8dae338d2972d3
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:29 +0100

    ebmac: switch to `verified` parameter

--- Casks/ebmac.rb
+++ Casks/ebmac.rb
@@ -2,8 +2,8 @@ cask "ebmac" do
   version "1.45.1"
   sha256 "91f454e709607ae8b397b13e4201adc4add8e80394969918218fc097831f3c73"
 
-  # ftp.vector.co.jp/72/54/938/ was verified as official when first introduced to the cask
-  url "https://ftp.vector.co.jp/72/54/938/EBMac#{version}.dmg"
+  url "https://ftp.vector.co.jp/72/54/938/EBMac#{version}.dmg",
+      verified: "ftp.vector.co.jp/72/54/938/"
   appcast "http://ebstudio.info/manual/EBMac/"
   name "EBMac"
   homepage "http://ebstudio.info/manual/EBMac/"
