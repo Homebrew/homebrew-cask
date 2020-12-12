f20ee30cc283768744c3964532391c270a007e81
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:12 +0100

    whalebird: switch to `verified` parameter

--- Casks/whalebird.rb
+++ Casks/whalebird.rb
@@ -2,8 +2,8 @@ cask "whalebird" do
   version "4.3.1"
   sha256 "d1fed937b756b43f1820b7e5b52e2b51307f4d7897c16a18f302002f680ad2f7"
 
-  # github.com/h3poteto/whalebird-desktop/ was verified as official when first introduced to the cask
-  url "https://github.com/h3poteto/whalebird-desktop/releases/download/#{version}/Whalebird-#{version}-darwin-x64.dmg"
+  url "https://github.com/h3poteto/whalebird-desktop/releases/download/#{version}/Whalebird-#{version}-darwin-x64.dmg",
+      verified: "github.com/h3poteto/whalebird-desktop/"
   appcast "https://github.com/h3poteto/whalebird-desktop/releases.atom"
   name "Whalebird"
   desc "Electron-based Mastodon and Pleroma client"
