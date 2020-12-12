c403dcc13d4311dda19af8db1c89e51d503569a4
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:02 +0100

    baiducloud: switch to `verified` parameter

--- Casks/baiducloud.rb
+++ Casks/baiducloud.rb
@@ -2,8 +2,8 @@ cask "baiducloud" do
   version "2.4.6"
   sha256 "9ae594d4d6a4581d971ed9fcde7cda62022b04247476a8982802324fa16bee2e"
 
-  # issuecdn.baidupcs.com/ was verified as official when first introduced to the cask
-  url "https://issuecdn.baidupcs.com/issue/netdisk/mac_tongbupan/tongbupan_#{version}.dmg"
+  url "https://issuecdn.baidupcs.com/issue/netdisk/mac_tongbupan/tongbupan_#{version}.dmg",
+      verified: "issuecdn.baidupcs.com/"
   name "Baidu Cloud"
   name "Baidu Yun Tong Bu Pan"
   name "百度云同步盘"
