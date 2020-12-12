3cdc1f9694ed02d299c59732ace4605a8f2a18fd
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:38 +0100

    freenettray: switch to `verified` parameter

--- Casks/freenettray.rb
+++ Casks/freenettray.rb
@@ -2,8 +2,8 @@ cask "freenettray" do
   version "2.2.0"
   sha256 "1351ee80def91f0fd7bb0903921d1987070e38787eacdb0ae8801b8f52bf0dac"
 
-  # github.com/freenet/mactray/ was verified as official when first introduced to the cask
-  url "https://github.com/freenet/mactray/releases/download/v#{version}/FreenetTray_#{version}.zip"
+  url "https://github.com/freenet/mactray/releases/download/v#{version}/FreenetTray_#{version}.zip",
+      verified: "github.com/freenet/mactray/"
   appcast "https://github.com/freenet/mactray/releases.atom"
   name "Freenet"
   desc "Menu bar application to control Freenet"
