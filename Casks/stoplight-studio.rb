d3da59c645e4566068b92b8259e7c96504d17ca8
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:59 +0100

    stoplight-studio: switch to `verified` parameter

--- Casks/stoplight-studio.rb
+++ Casks/stoplight-studio.rb
@@ -2,8 +2,8 @@ cask "stoplight-studio" do
   version "2.2.0,5282.b08742e"
   sha256 "8712f55657d5d9283bb0e1d11718b8e55cbceb87bcbd90bf9d4952a6602eae6c"
 
-  # github.com/stoplightio/studio/ was verified as official when first introduced to the cask
-  url "https://github.com/stoplightio/studio/releases/download/v#{version.before_comma}-stable.#{version.after_comma}/stoplight-studio-mac.dmg"
+  url "https://github.com/stoplightio/studio/releases/download/v#{version.before_comma}-stable.#{version.after_comma}/stoplight-studio-mac.dmg",
+      verified: "github.com/stoplightio/studio/"
   appcast "https://github.com/stoplightio/studio/releases.atom"
   name "Stoplight Studio"
   desc "Editor for designing and documenting APIs"
