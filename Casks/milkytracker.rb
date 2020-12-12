07f8b7c9264355103602effee9d48782c4359e8e
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:21 +0100

    milkytracker: switch to `verified` parameter

--- Casks/milkytracker.rb
+++ Casks/milkytracker.rb
@@ -2,8 +2,8 @@ cask "milkytracker" do
   version "1.02.00"
   sha256 "b526497dc957454a4150e77b1f2f835ce564122e56f49bbe7b494dc984be076d"
 
-  # github.com/milkytracker/MilkyTracker/ was verified as official when first introduced to the cask
-  url "https://github.com/milkytracker/MilkyTracker/releases/download/v#{version}/milkytracker-#{version}.dmg"
+  url "https://github.com/milkytracker/MilkyTracker/releases/download/v#{version}/milkytracker-#{version}.dmg",
+      verified: "github.com/milkytracker/MilkyTracker/"
   appcast "https://github.com/milkytracker/MilkyTracker/releases.atom"
   name "MilkyTracker"
   desc "Music tracker compatible with FT2"
