108fa05d5ae820dd4f3523d6ba06158ee9cbe88f
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:46 +0100

    ha-menu: switch to `verified` parameter

--- Casks/ha-menu.rb
+++ Casks/ha-menu.rb
@@ -2,8 +2,8 @@ cask "ha-menu" do
   version "2.4.1"
   sha256 "db63c175840d94f52eea74537a285853d567a54d337ec22fcbf75472dec370c0"
 
-  # github.com/codechimp-org/ha-menu/ was verified as official when first introduced to the cask
-  url "https://github.com/codechimp-org/ha-menu/releases/download/#{version}/HA.Menu.zip"
+  url "https://github.com/codechimp-org/ha-menu/releases/download/#{version}/HA.Menu.zip",
+      verified: "github.com/codechimp-org/ha-menu/"
   appcast "https://github.com/codechimp-org/ha-menu/releases.atom"
   name "HA Menu"
   homepage "https://hamenu.codechimp.org/"
