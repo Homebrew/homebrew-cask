932151fceefbfdfb37d6e98eedabba93c1b0a51c
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:28 +0100

    dukto: switch to `verified` parameter

--- Casks/dukto.rb
+++ Casks/dukto.rb
@@ -2,8 +2,8 @@ cask "dukto" do
   version "R6"
   sha256 "86f06ca92a28b5835cb3eaadfb09897f581a5da3fb8ee8246897b1bf4ee7348a"
 
-  # sourceforge.net/dukto/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/dukto/Dukto#{version}-OSX.dmg"
+  url "https://downloads.sourceforge.net/dukto/Dukto#{version}-OSX.dmg",
+      verified: "sourceforge.net/dukto/"
   appcast "https://sourceforge.net/projects/dukto/rss"
   name "Dukto"
   homepage "https://www.msec.it/blog/?page_id=11"
