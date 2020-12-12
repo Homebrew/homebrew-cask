4c17320e80cf0c22ae44f8d10416c2f4da450e68
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:36 +0100

    flutter: switch to `verified` parameter

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -2,8 +2,8 @@ cask "flutter" do
   version "1.22.4"
   sha256 "d73b71316e883f36e02d412ed237c712c7942902bdb84ad0ebfa1fa431b75b88"
 
-  # storage.googleapis.com/flutter_infra/ was verified as official when first introduced to the cask
-  url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_#{version}-stable.zip"
+  url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_#{version}-stable.zip",
+      verified: "storage.googleapis.com/flutter_infra/"
   name "Flutter SDK"
   desc "UI toolkit for building applications for mobile, web and desktop"
   homepage "https://flutter.dev/"
