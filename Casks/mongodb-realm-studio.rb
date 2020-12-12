7bb597372b79d896163e0afab1ea2c256342e57e
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:22 +0100

    mongodb-realm-studio: switch to `verified` parameter

--- Casks/mongodb-realm-studio.rb
+++ Casks/mongodb-realm-studio.rb
@@ -2,8 +2,8 @@ cask "mongodb-realm-studio" do
   version "10.0.1"
   sha256 "d8c7635eed6126a2b6772d3d5381b58763066780fc23b271e5f9aff371991663"
 
-  # github.com/realm/realm-studio/ was verified as official when first introduced to the cask
-  url "https://github.com/realm/realm-studio/releases/download/v#{version}/MongoDB.Realm.Studio-#{version}.dmg"
+  url "https://github.com/realm/realm-studio/releases/download/v#{version}/MongoDB.Realm.Studio-#{version}.dmg",
+      verified: "github.com/realm/realm-studio/"
   appcast "https://github.com/realm/realm-studio/releases.atom"
   name "MongoDB Realm Studio"
   desc "Tool for the Realm Database and Realm Platform"
