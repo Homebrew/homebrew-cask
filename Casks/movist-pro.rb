bd49461fb1d4477c5e62c75db110744ae417475e
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:23 +0100

    movist-pro: switch to `verified` parameter

--- Casks/movist-pro.rb
+++ Casks/movist-pro.rb
@@ -2,8 +2,8 @@ cask "movist-pro" do
   version "2.6.2"
   sha256 "85a4afac56f72a29cc9c4e9d7e5fa64680a63db32954be7e3097211a38e4ac66"
 
-  # s3.ap-northeast-2.amazonaws.com/update.cocoable.com was verified as official when first introduced to the cask
-  url "https://s3.ap-northeast-2.amazonaws.com/update.cocoable.com/MovistPro_#{version}.app.zip"
+  url "https://s3.ap-northeast-2.amazonaws.com/update.cocoable.com/MovistPro_#{version}.app.zip",
+      verified: "s3.ap-northeast-2.amazonaws.com/update.cocoable.com/"
   appcast "https://s3.ap-northeast-2.amazonaws.com/update.cocoable.com/appcast.xml"
   name "Movist Pro"
   desc "Media player"
