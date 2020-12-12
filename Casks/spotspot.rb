42f024462ce3af4d179ba42ee4affdf035638e75
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:57 +0100

    spotspot: switch to `verified` parameter

--- Casks/spotspot.rb
+++ Casks/spotspot.rb
@@ -2,8 +2,8 @@ cask "spotspot" do
   version "4.0.2"
   sha256 "789ea9104c704bf7632ec4d0e06ec614470fe446bbd19076f818136aa05af76f"
 
-  # github.com/will-stone/SpotSpot/ was verified as official when first introduced to the cask
-  url "https://github.com/will-stone/SpotSpot/releases/download/v#{version}/SpotSpot-#{version}.dmg"
+  url "https://github.com/will-stone/SpotSpot/releases/download/v#{version}/SpotSpot-#{version}.dmg",
+      verified: "github.com/will-stone/SpotSpot/"
   appcast "https://github.com/will-stone/SpotSpot/releases.atom"
   name "SpotSpot"
   desc "Spotify mini-player"
