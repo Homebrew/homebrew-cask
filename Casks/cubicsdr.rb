5c23eae1f84ad317a85bb86cf01da72ee0ceacfe
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:19 +0100

    cubicsdr: switch to `verified` parameter

--- Casks/cubicsdr.rb
+++ Casks/cubicsdr.rb
@@ -2,8 +2,8 @@ cask "cubicsdr" do
   version "0.2.5"
   sha256 "9180e56e84a1d78935fb13ce362e3b8636a96b38d0695748078b43f9f6c3cb2c"
 
-  # github.com/cjcliffe/CubicSDR/ was verified as official when first introduced to the cask
-  url "https://github.com/cjcliffe/CubicSDR/releases/download/#{version}/CubicSDR-#{version}-Darwin.dmg"
+  url "https://github.com/cjcliffe/CubicSDR/releases/download/#{version}/CubicSDR-#{version}-Darwin.dmg",
+      verified: "github.com/cjcliffe/CubicSDR/"
   appcast "https://github.com/cjcliffe/CubicSDR/releases.atom"
   name "CubicSDR"
   desc "Cross-Platform Software-Defined Radio Application"
