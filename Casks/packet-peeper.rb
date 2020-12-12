960b2361bb002917b2a6dda4e1516230acce08e6
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:36 +0100

    packet-peeper: switch to `verified` parameter

--- Casks/packet-peeper.rb
+++ Casks/packet-peeper.rb
@@ -2,8 +2,8 @@ cask "packet-peeper" do
   version "2019-12-17"
   sha256 "75f081ab7e9a75f6d41172518864891d426bf05f10f91890c61c330c18de1729"
 
-  # github.com/choll/packetpeeper/ was verified as official when first introduced to the cask
-  url "https://github.com/choll/packetpeeper/releases/download/#{version}/PacketPeeper_#{version}.dmg"
+  url "https://github.com/choll/packetpeeper/releases/download/#{version}/PacketPeeper_#{version}.dmg",
+      verified: "github.com/choll/packetpeeper/"
   appcast "https://packetpeeper.org/release-notes/"
   name "Packet Peeper"
   desc "Network protocol analyzer"
