15db4649368421624395e047616c74d4e3a128ed
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:23 +0100

    dexed: switch to `verified` parameter

--- Casks/dexed.rb
+++ Casks/dexed.rb
@@ -2,8 +2,8 @@ cask "dexed" do
   version "0.9.5"
   sha256 "4e2ba92331317aa2ce87194cdcc3eb6110b9b2fbbdb64355ada9bb9a7c56f0d5"
 
-  # github.com/asb2m10/dexed/ was verified as official when first introduced to the cask
-  url "https://github.com/asb2m10/dexed/releases/download/v#{version}/dexed-#{version}-osx.zip"
+  url "https://github.com/asb2m10/dexed/releases/download/v#{version}/dexed-#{version}-osx.zip",
+      verified: "github.com/asb2m10/dexed/"
   appcast "https://github.com/asb2m10/dexed/releases.atom"
   name "Dexed"
   desc "DX7 FM synthesizer"
