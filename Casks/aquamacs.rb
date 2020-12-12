7061c8cb6dfa239f6d3105ecff9a6cb2007fc731
Markus Reiter <me@reitermark.us>
2020-12-12 05:20:58 +0100

    aquamacs: switch to `verified` parameter

--- Casks/aquamacs.rb
+++ Casks/aquamacs.rb
@@ -2,8 +2,8 @@ cask "aquamacs" do
   version "3.5"
   sha256 "c27165c0b42b93ef3c9e5dfd0dd53527b10c683aae35fceedd4fecc52332c2ba"
 
-  # github.com/davidswelt/aquamacs-emacs/ was verified as official when first introduced to the cask
-  url "https://github.com/davidswelt/aquamacs-emacs/releases/download/Aquamacs-#{version}/Aquamacs-Emacs-#{version}.dmg"
+  url "https://github.com/davidswelt/aquamacs-emacs/releases/download/Aquamacs-#{version}/Aquamacs-Emacs-#{version}.dmg",
+      verified: "github.com/davidswelt/aquamacs-emacs/"
   appcast "https://github.com/davidswelt/aquamacs-emacs/releases.atom"
   name "Aquamacs"
   desc "Text editor based on GNU Emacs"
