f54b65b4844526bb22c09fb5f5b085f5eb5465df
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:06 +0100

    bitshares: switch to `verified` parameter

--- Casks/bitshares.rb
+++ Casks/bitshares.rb
@@ -2,8 +2,8 @@ cask "bitshares" do
   version "3.3.191120"
   sha256 "27553d1038f6c301b7bcdc4f8a644e871722024d24585c6b847c868962a967aa"
 
-  # github.com/bitshares/bitshares-ui/ was verified as official when first introduced to the cask
-  url "https://github.com/bitshares/bitshares-ui/releases/download/#{version}/BitShares-#{version}-mac.zip"
+  url "https://github.com/bitshares/bitshares-ui/releases/download/#{version}/BitShares-#{version}-mac.zip",
+      verified: "github.com/bitshares/bitshares-ui/"
   appcast "https://github.com/bitshares/bitshares-ui/releases.atom"
   name "BitShares"
   desc "GUI / Reference wallet for the BitShares blockchain"
