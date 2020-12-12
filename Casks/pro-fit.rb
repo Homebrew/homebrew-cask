d27cedb3da91132b0a384874cfbb24a73850ce63
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:41 +0100

    pro-fit: switch to `verified` parameter

--- Casks/pro-fit.rb
+++ Casks/pro-fit.rb
@@ -2,8 +2,8 @@ cask "pro-fit" do
   version "7.0.17"
   sha256 "761aaf7f1688d736b4544387aae9dfa20007f552ea3017925864bc3c096ce625"
 
-  # quantum-soft.com/ was verified as official when first introduced to the cask
-  url "https://quantum-soft.com/profit/pro_Fit_#{version.dots_to_underscores}.dmg"
+  url "https://quantum-soft.com/profit/pro_Fit_#{version.dots_to_underscores}.dmg",
+      verified: "quantum-soft.com/"
   appcast "https://www.quansoft.com/hist.html"
   name "pro Fit"
   homepage "https://www.quansoft.com/"
