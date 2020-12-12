e9f598ca2e432a86cccb14287299047cc081db37
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:27 +0100

    duckietv: switch to `verified` parameter

--- Casks/duckietv.rb
+++ Casks/duckietv.rb
@@ -2,8 +2,8 @@ cask "duckietv" do
   version "1.1.5"
   sha256 "9c2f72c011cd477071e51238d5bfa0c202babdd263c1e4ea6b3d4e4605da2907"
 
-  # github.com/SchizoDuckie/DuckieTV/ was verified as official when first introduced to the cask
-  url "https://github.com/SchizoDuckie/DuckieTV/releases/download/#{version}/DuckieTV-#{version}-OSX-x64.pkg"
+  url "https://github.com/SchizoDuckie/DuckieTV/releases/download/#{version}/DuckieTV-#{version}-OSX-x64.pkg",
+      verified: "github.com/SchizoDuckie/DuckieTV/"
   appcast "https://github.com/SchizoDuckie/DuckieTV/releases.atom"
   name "duckieTV"
   desc "Tool to track TV shows with semi-automagic torrent integration"
