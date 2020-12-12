18f064ea5b26f22f52ead684c6328b0b86374732
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:29 +0100

    nuclear: switch to `verified` parameter

--- Casks/nuclear.rb
+++ Casks/nuclear.rb
@@ -2,8 +2,8 @@ cask "nuclear" do
   version "0.6.6"
   sha256 "5620a90b90511272b3d785f3614bae735e054443059041002058b062df711e4e"
 
-  # github.com/nukeop/nuclear/ was verified as official when first introduced to the cask
-  url "https://github.com/nukeop/nuclear/releases/download/v#{version}/nuclear-v#{version}.dmg"
+  url "https://github.com/nukeop/nuclear/releases/download/v#{version}/nuclear-v#{version}.dmg",
+      verified: "github.com/nukeop/nuclear/"
   appcast "https://github.com/nukeop/nuclear/releases.atom"
   name "Nuclear"
   desc "Streaming music player"
