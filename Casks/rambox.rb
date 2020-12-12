dfcfd3fb9ba1520a2d4ca77b0275634a135ce6ed
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:45 +0100

    rambox: switch to `verified` parameter

--- Casks/rambox.rb
+++ Casks/rambox.rb
@@ -2,8 +2,8 @@ cask "rambox" do
   version "0.7.7"
   sha256 "b73accdc18e20b285dbf280d9db539cfdd8ccffa0ab4ccddc26bd596ab56a8df"
 
-  # github.com/ramboxapp/community-edition/ was verified as official when first introduced to the cask
-  url "https://github.com/ramboxapp/community-edition/releases/download/#{version}/Rambox-#{version}-mac.zip"
+  url "https://github.com/ramboxapp/community-edition/releases/download/#{version}/Rambox-#{version}-mac.zip",
+      verified: "github.com/ramboxapp/community-edition/"
   appcast "https://github.com/ramboxapp/community-edition/releases.atom"
   name "Rambox"
   desc "Free and Open Source messaging and emailing app"
