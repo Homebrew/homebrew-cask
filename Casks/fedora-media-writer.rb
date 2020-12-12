9ba92d69b402abe54fc8b089395a89113956b923
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:34 +0100

    fedora-media-writer: switch to `verified` parameter

--- Casks/fedora-media-writer.rb
+++ Casks/fedora-media-writer.rb
@@ -2,8 +2,8 @@ cask "fedora-media-writer" do
   version "4.1.6"
   sha256 "6a9d9f14423f98197730b097a9121f3c833baec489989e6d1168f353ae80c579"
 
-  # github.com/FedoraQt/MediaWriter/ was verified as official when first introduced to the cask
-  url "https://github.com/FedoraQt/MediaWriter/releases/download/#{version}/FedoraMediaWriter-osx-#{version}.unnotarized.dmg"
+  url "https://github.com/FedoraQt/MediaWriter/releases/download/#{version}/FedoraMediaWriter-osx-#{version}.unnotarized.dmg",
+      verified: "github.com/FedoraQt/MediaWriter/"
   appcast "https://github.com/FedoraQt/MediaWriter/releases.atom"
   name "Fedora Media Writer"
   desc "Tool to write Fedora images to portable media files"
