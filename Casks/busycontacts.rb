fdf62d36a6711f0bdebc63bc87d8c61d6d9f42c8
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:10 +0100

    busycontacts: switch to `verified` parameter

--- Casks/busycontacts.rb
+++ Casks/busycontacts.rb
@@ -2,8 +2,8 @@ cask "busycontacts" do
   version "1.4.11-2020-11-15-03-25"
   sha256 "e2d23240e43f1645a4cb309cc72ca944d9294d5e9407e907dadb637cf01dc05e"
 
-  # 7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com was verified as official when first introduced to the cask
-  url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bct-#{version}.zip"
+  url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bct-#{version}.zip",
+      verified: "7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.busymac.com/download/BusyContacts.zip"
   name "BusyContacts"
   desc "Contact manager focusing on efficiency"
