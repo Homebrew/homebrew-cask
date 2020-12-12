97cc844c4cd3ec66b0596981e270725989b0ae6e
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:19 +0100

    menutube: switch to `verified` parameter

--- Casks/menutube.rb
+++ Casks/menutube.rb
@@ -2,8 +2,8 @@ cask "menutube" do
   version "1.7.3"
   sha256 "2428a68c085c9a10407eca1d778c2d5b3d37e4d0dfadb12376b44eab2f9f43a1"
 
-  # github.com/edanchenkov/MenuTube/ was verified as official when first introduced to the cask
-  url "https://github.com/edanchenkov/MenuTube/releases/download/#{version}/MenuTube-#{version}.dmg"
+  url "https://github.com/edanchenkov/MenuTube/releases/download/#{version}/MenuTube-#{version}.dmg",
+      verified: "github.com/edanchenkov/MenuTube/"
   appcast "https://github.com/edanchenkov/MenuTube/releases.atom"
   name "MenuTube"
   desc "Tool to capture YouTube into the menu bar"
