2bf6bd747d97d00fda6385dfcbe4126255847845
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:17 +0100

    controlplane: switch to `verified` parameter

--- Casks/controlplane.rb
+++ Casks/controlplane.rb
@@ -2,8 +2,8 @@ cask "controlplane" do
   version "1.6.7"
   sha256 "beb444b8117ed91898921a6babc384501dfd92679f5b718fceb6d3aa7a0bf651"
 
-  # github.com/dustinrue/ControlPlane/ was verified as official when first introduced to the cask
-  url "https://github.com/dustinrue/ControlPlane/releases/download/#{version}/ControlPlane-#{version}.dmg"
+  url "https://github.com/dustinrue/ControlPlane/releases/download/#{version}/ControlPlane-#{version}.dmg",
+      verified: "github.com/dustinrue/ControlPlane/"
   appcast "https://github.com/dustinrue/ControlPlane/releases.atom"
   name "ControlPlane"
   homepage "https://www.controlplaneapp.com/"
