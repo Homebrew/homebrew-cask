f2c57d286010f20ae8038b7518b56781401640be
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:15 +0100

    luyten: switch to `verified` parameter

--- Casks/luyten.rb
+++ Casks/luyten.rb
@@ -2,8 +2,8 @@ cask "luyten" do
   version "0.5.4"
   sha256 "f0d900bd9bba5dc72eb3dfe374db9bbef4222e49477a6bf18c6c14e3ddcf8eb4"
 
-  # github.com/deathmarine/Luyten/ was verified as official when first introduced to the cask
-  url "https://github.com/deathmarine/Luyten/releases/download/v#{version}_Rebuilt_with_Latest_depenencies/luyten-OSX-#{version}.zip"
+  url "https://github.com/deathmarine/Luyten/releases/download/v#{version}_Rebuilt_with_Latest_depenencies/luyten-OSX-#{version}.zip",
+      verified: "github.com/deathmarine/Luyten/"
   appcast "https://github.com/deathmarine/Luyten/releases.atom"
   name "Luyten"
   desc "Open-source Java decompiler GUI for Procyon"
