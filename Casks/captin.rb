680fb7f322ffd126a87b71aa52abd6f76f4f7ecd
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:11 +0100

    captin: switch to `verified` parameter

--- Casks/captin.rb
+++ Casks/captin.rb
@@ -2,8 +2,8 @@ cask "captin" do
   version "1.1.1,129:1607066962"
   sha256 "47d82111549f7751263411bd6b3af6ad31b343de5d254d74562e57d3df6f1efe"
 
-  # dl.devmate.com/com.100hps.captin/ was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/com.100hps.captin/#{version.after_comma.before_colon}/#{version.after_colon}/Captin-#{version.after_comma.before_colon}.dmg"
+  url "https://dl.devmate.com/com.100hps.captin/#{version.after_comma.before_colon}/#{version.after_colon}/Captin-#{version.after_comma.before_colon}.dmg",
+      verified: "dl.devmate.com/com.100hps.captin/"
   appcast "https://updates.devmate.com/com.100hps.captin.xml"
   name "Captin"
   homepage "http://captin.strikingly.com/"
