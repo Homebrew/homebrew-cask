70cede0e4fea569ec7cc86d345fcc5d267fbba38
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:50 +0100

    scrivener: switch to `verified` parameter

--- Casks/scrivener.rb
+++ Casks/scrivener.rb
@@ -2,8 +2,8 @@ cask "scrivener" do
   version "3.2.1,14481"
   sha256 "4bb2a6c16f99d7d16a6edec9cfef865925d283f23e85cc6010edbcdc191abdce"
 
-  # scrivener.s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://scrivener.s3.amazonaws.com/mac_updates/Scrivener_1012_#{version.after_comma}.zip"
+  url "https://scrivener.s3.amazonaws.com/mac_updates/Scrivener_1012_#{version.after_comma}.zip",
+      verified: "scrivener.s3.amazonaws.com/"
   appcast "https://www.literatureandlatte.com/downloads/scrivener-#{version.major}.xml"
   name "Scrivener"
   desc "Word processing software with a typewriter style"
