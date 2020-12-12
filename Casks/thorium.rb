50d34dc1b63cb3dc6db6a3fbde72d011004c3d33
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:04 +0100

    thorium: switch to `verified` parameter

--- Casks/thorium.rb
+++ Casks/thorium.rb
@@ -2,8 +2,8 @@ cask "thorium" do
   version "1.5.0"
   sha256 "6c206707eee3a3749ab7283ef4eeefa97f29c1563890e3eed800fd0a7358c0dc"
 
-  # github.com/edrlab/thorium-reader was verified as official when first introduced to the cask
-  url "https://github.com/edrlab/thorium-reader/releases/download/v#{version}/Thorium-#{version}.dmg"
+  url "https://github.com/edrlab/thorium-reader/releases/download/v#{version}/Thorium-#{version}.dmg",
+      verified: "github.com/edrlab/thorium-reader/"
   appcast "https://github.com/edrlab/thorium-reader/releases.atom"
   name "Thorium Reader"
   desc "Epub reader"
