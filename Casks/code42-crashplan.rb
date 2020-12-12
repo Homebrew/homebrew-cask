138e079f7090f10bbfefe7f949f21a56f1e3f6be
Kenny Botelho <63611879+kennyb-222@users.noreply.github.com>
2020-12-11 17:06:39 -0800

    Update code42-crashplan.rb
    major version update to 8.x from 7.x

--- Casks/code42-crashplan.rb
+++ Casks/code42-crashplan.rb
@@ -1,19 +1,19 @@
 cask "code42-crashplan" do
-  version "7.0.3"
-  sha256 "d0d00edc7855a2c1a5282a138c2d398102d8f5f5d59f148787b0fda9d307b9e3"
+  version "8.2.1"
+  sha256 "89af29fd387aef977ca1da175105d0861c2a39742dc87dd648ea330548570ae4"
 
   # download.code42.com/ was verified as official when first introduced to the cask
-  url "https://download.code42.com/installs/mac/install/Code42CrashPlan/Code42CrashPlan_#{version}_Mac.dmg"
+  url "https://download.code42.com/installs/client-package-repository/#{version}/77/install/Code42_#{version}_1525200006821_77_Mac.dmg"
   appcast "https://support.code42.com/Release_Notes"
   name "CrashPlan"
   homepage "https://www.crashplan.com/"
 
   auto_updates true
 
-  pkg "Install Code42 CrashPlan.pkg"
+  pkg "Install Code42.pkg"
 
   uninstall launchctl: "com.backup42.desktop",
-            pkgutil:   "com.crashplan.app.pkg",
+            pkgutil:   "com.code42.app.pkg",
             script:    {
               executable: "Uninstall.app/Contents/Resources/uninstall.sh",
               sudo:       true,
