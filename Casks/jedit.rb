a3fbe201e3c822e0e8fcb17966d22ee4006714cb
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:54 +0100

    jedit: switch to `verified` parameter

--- Casks/jedit.rb
+++ Casks/jedit.rb
@@ -2,8 +2,8 @@ cask "jedit" do
   version "5.6.0"
   sha256 "7fc6bcf042401899915793e038bab826546d91b593b6f3855e9f29d4101aab61"
 
-  # sourceforge.net/jedit/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/jedit/jedit#{version}install.dmg"
+  url "https://downloads.sourceforge.net/jedit/jedit#{version}install.dmg",
+      verified: "sourceforge.net/jedit/"
   appcast "https://sourceforge.net/projects/jedit/rss"
   name "jEdit"
   homepage "http://www.jedit.org/"
