87fdcfab62c64bb8f55f0e2f9469c5736dfdc8db
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:41 +0100

    prismatik: switch to `verified` parameter

--- Casks/prismatik.rb
+++ Casks/prismatik.rb
@@ -2,8 +2,8 @@ cask "prismatik" do
   version "6.0.0.104"
   sha256 "72e94944f9732ab737836a399a6f92693d1dd1717e0bef0cae3d96654b1dd2af"
 
-  # woodenshark.com/4lightpack/ was verified as official when first introduced to the cask
-  url "https://woodenshark.com/4lightpack/Prismatik_#{version}_signed.dmg"
+  url "https://woodenshark.com/4lightpack/Prismatik_#{version}_signed.dmg",
+      verified: "woodenshark.com/4lightpack/"
   appcast "https://lightpack.tv/pages/downloads"
   name "Prismatik"
   homepage "https://lightpack.tv/pages/downloads"
