3693df9c7e2c4611da9c88c8e6514b1e9fc800ef
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:18 +0100

    coverload: switch to `verified` parameter

--- Casks/coverload.rb
+++ Casks/coverload.rb
@@ -2,8 +2,8 @@ cask "coverload" do
   version "2.1.1-752"
   sha256 "e6cee75fa95facfb3588d9435d7a92e492a2d7d8efc49a3f6aa302dda0f57aa2"
 
-  # s3-us-west-2.amazonaws.com/coverloadapp.com/ was verified as official when first introduced to the cask
-  url "https://s3-us-west-2.amazonaws.com/coverloadapp.com/Uploads/CoverLoad-#{version}.zip"
+  url "https://s3-us-west-2.amazonaws.com/coverloadapp.com/Uploads/CoverLoad-#{version}.zip",
+      verified: "s3-us-west-2.amazonaws.com/coverloadapp.com/"
   name "CoverLoad"
   homepage "https://coverloadapp.com/"
 
