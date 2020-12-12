0e3d6cd0d34f3ce81e99863e640939a9b7aefd9d
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-12 10:51:37 -0800

    Update ishowu-instant from 1.4.0 to 1.4.1 (#95336)

--- Casks/ishowu-instant.rb
+++ Casks/ishowu-instant.rb
@@ -1,6 +1,6 @@
 cask "ishowu-instant" do
-  version "1.4.0"
-  sha256 "eaa255cd315bb81323da017ec166850f0b08510c257808997eae1a077e6c6dd5"
+  version "1.4.1"
+  sha256 "2ab326182ec95f7a76387d2c206b7c6ee46eaeaae51962660709935f2177788c"
 
   url "https://www.shinywhitebox.com/downloads/instant/iShowU_Instant_#{version}.dmg"
   appcast "https://www.shinywhitebox.com/store/appcast.php?p=14"
