4ac2517c80f57016879825b1d484630a767958c4
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:18 +0100

    mcedit: switch to `verified` parameter

--- Casks/mcedit.rb
+++ Casks/mcedit.rb
@@ -2,8 +2,8 @@ cask "mcedit" do
   version "1.5.6.0"
   sha256 "e2026de3589e3e65086a385ee4e02d607337bc9da11357d1b3ac106e2ee843d7"
 
-  # github.com/Podshot/MCEdit-Unified/ was verified as official when first introduced to the cask
-  url "https://github.com/Podshot/MCEdit-Unified/releases/download/#{version}/MCEdit.v#{version}.OSX.64bit.zip"
+  url "https://github.com/Podshot/MCEdit-Unified/releases/download/#{version}/MCEdit.v#{version}.OSX.64bit.zip",
+      verified: "github.com/Podshot/MCEdit-Unified/"
   appcast "https://github.com/Podshot/MCEdit-Unified/releases.atom"
   name "MCEdit-Unified"
   homepage "https://www.mcedit-unified.net/"
