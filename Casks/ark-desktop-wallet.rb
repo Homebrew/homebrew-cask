dd79a09f0c6a96573c0e3c3c4fb03ad3e7ba6ebf
Markus Reiter <me@reitermark.us>
2020-12-12 05:20:58 +0100

    ark-desktop-wallet: switch to `verified` parameter

--- Casks/ark-desktop-wallet.rb
+++ Casks/ark-desktop-wallet.rb
@@ -2,8 +2,8 @@ cask "ark-desktop-wallet" do
   version "2.9.4"
   sha256 "4923e450f249a5e8166485d069e108ab9c6b39b1cae51c6625adf84c172c0af7"
 
-  # github.com/ArkEcosystem/desktop-wallet/ was verified as official when first introduced to the cask
-  url "https://github.com/ArkEcosystem/desktop-wallet/releases/download/#{version}/ark-desktop-wallet-mac-#{version}.dmg"
+  url "https://github.com/ArkEcosystem/desktop-wallet/releases/download/#{version}/ark-desktop-wallet-mac-#{version}.dmg",
+      verified: "github.com/ArkEcosystem/desktop-wallet/"
   appcast "https://github.com/ArkEcosystem/desktop-wallet/releases.atom"
   name "Ark Desktop Wallet"
   desc "Multi Platform ARK Desktop Wallet"
