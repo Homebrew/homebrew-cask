208ca92e3fee42260b8958aefedcdc4886615c19
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:18 +0100

    mcbopomofo: switch to `verified` parameter

--- Casks/mcbopomofo.rb
+++ Casks/mcbopomofo.rb
@@ -2,8 +2,8 @@ cask "mcbopomofo" do
   version "1.0"
   sha256 "5649e8b37056467fe1f2594796ddcf203d526d8290e0b90137f4fcca997a7178"
 
-  # github.com/ was verified as official when first introduced to the cask
-  url "https://github.com/openvanilla/McBopomofo/releases/download/#{version}/McBopomofo-Installer-#{version}.zip"
+  url "https://github.com/openvanilla/McBopomofo/releases/download/#{version}/McBopomofo-Installer-#{version}.zip",
+      verified: "github.com/"
   appcast "https://github.com/openvanilla/McBopomofo/releases.atom"
   name "McBopomofo"
   homepage "https://mcbopomofo.openvanilla.org/"
