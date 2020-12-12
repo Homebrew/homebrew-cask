f831036802e4c46fb262e2c10c1b980de7ea1083
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:41 +0100

    geotag-photos-pro: switch to `verified` parameter

--- Casks/geotag-photos-pro.rb
+++ Casks/geotag-photos-pro.rb
@@ -2,8 +2,8 @@ cask "geotag-photos-pro" do
   version "1.6.3"
   sha256 "78b0d269c11c4e65b42108197ce97dd875247ea0f4c7d86aeec6f6d3d5acad0b"
 
-  # github.com/tappytaps/geotag-desktop-app/ was verified as official when first introduced to the cask
-  url "https://github.com/tappytaps/geotag-desktop-app/releases/download/v#{version}/Geotag-Photos-Pro-2-#{version}.dmg"
+  url "https://github.com/tappytaps/geotag-desktop-app/releases/download/v#{version}/Geotag-Photos-Pro-2-#{version}.dmg",
+      verified: "github.com/tappytaps/geotag-desktop-app/"
   appcast "https://github.com/tappytaps/geotag-desktop-app/releases.atom"
   name "Geotag Photos Pro"
   homepage "https://www.geotagphotos.net/"
