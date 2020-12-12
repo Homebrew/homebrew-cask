8bea66eda7cd09d02f6d65ff35c9ae3b8c2e1ce5
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:50 +0100

    seadrive: switch to `verified` parameter

--- Casks/seadrive.rb
+++ Casks/seadrive.rb
@@ -2,8 +2,8 @@ cask "seadrive" do
   version "2.0.9"
   sha256 "8c0de67f4244e8a2da0159606e9d347bf2932d42e23b5be24f1a3cd9106a071a"
 
-  # download.seadrive.org/ was verified as official when first introduced to the cask
-  url "https://download.seadrive.org/seadrive-#{version}.dmg"
+  url "https://download.seadrive.org/seadrive-#{version}.dmg",
+      verified: "download.seadrive.org/"
   appcast "https://www.seafile.com/en/download/"
   name "Seadrive"
   desc "Manual for Seafile server"
