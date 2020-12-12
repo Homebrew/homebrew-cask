7f7dc63372e0db66b7460449189164fa7444f4a6
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:18 +0100

    media-converter: switch to `verified` parameter

--- Casks/media-converter.rb
+++ Casks/media-converter.rb
@@ -2,8 +2,8 @@ cask "media-converter" do
   version "2.0.10"
   sha256 "d5a7db000e8106e5892daad84ee7dea82651aa9662df5b4c6da0b943afae03c1"
 
-  # downloads.sourceforge.net/media-converter/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/media-converter/media-converter/#{version}/media-converter-#{version}.zip"
+  url "https://downloads.sourceforge.net/media-converter/media-converter/#{version}/media-converter-#{version}.zip",
+      verified: "downloads.sourceforge.net/media-converter/"
   appcast "https://sourceforge.net/projects/media-converter/rss?path=/media-converter"
   name "Media Converter"
   homepage "https://media-converter.sourceforge.io/"
