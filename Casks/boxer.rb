e1afc543a19def21ddbf48c48f137cf356db01ec
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:08 +0100

    boxer: switch to `verified` parameter

--- Casks/boxer.rb
+++ Casks/boxer.rb
@@ -2,8 +2,8 @@ cask "boxer" do
   version "1.4.0"
   sha256 "a75f9149612f90fa78f1016a6edef34ed600334f7170d624b083a310ae4c904e"
 
-  # github.com/alunbestor/Boxer/ was verified as official when first introduced to the cask
-  url "https://github.com/alunbestor/Boxer/releases/download/v#{version}/boxer-#{version}.zip"
+  url "https://github.com/alunbestor/Boxer/releases/download/v#{version}/boxer-#{version}.zip",
+      verified: "github.com/alunbestor/Boxer/"
   appcast "https://github.com/alunbestor/Boxer/releases.atom"
   name "Boxer"
   desc "DOS game emulator"
