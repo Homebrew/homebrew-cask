b25de7ceb09900c69a8eac8a8a5d23b3197a64a2
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:40 +0100

    gb-studio: switch to `verified` parameter

--- Casks/gb-studio.rb
+++ Casks/gb-studio.rb
@@ -2,8 +2,8 @@ cask "gb-studio" do
   version "1.2.1"
   sha256 "9d0c94179fbe56b1770cfc09b8bff392b98224a9b320235754a97a24dcbd9415"
 
-  # github.com/chrismaltby/gb-studio/ was verified as official when first introduced to the cask
-  url "https://github.com/chrismaltby/gb-studio/releases/download/v#{version}/GB-Studio-Mac-#{version}.zip"
+  url "https://github.com/chrismaltby/gb-studio/releases/download/v#{version}/GB-Studio-Mac-#{version}.zip",
+      verified: "github.com/chrismaltby/gb-studio/"
   appcast "https://github.com/chrismaltby/gb-studio/releases.atom"
   name "GB Studio"
   desc "Drag and drop retro game creator"
