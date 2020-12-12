e4a97c4c8c454863210edf1550ddeeb57c0db4df
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:58 +0100

    startupizer: switch to `verified` parameter

--- Casks/startupizer.rb
+++ Casks/startupizer.rb
@@ -2,8 +2,8 @@ cask "startupizer" do
   version "2.3.11,2060:1527510167"
   sha256 "56792a52fc91129f6f192cbbc0dbc6a7175d0699f2dab726baaf7d94f30ce150"
 
-  # dl.devmate.com/com.gentlebytes.Startupizer2/ was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/com.gentlebytes.Startupizer2/#{version.after_comma.before_colon}/#{version.after_colon}/Startupizer#{version.major}-#{version.after_comma.before_colon}.zip"
+  url "https://dl.devmate.com/com.gentlebytes.Startupizer2/#{version.after_comma.before_colon}/#{version.after_colon}/Startupizer#{version.major}-#{version.after_comma.before_colon}.zip",
+      verified: "dl.devmate.com/com.gentlebytes.Startupizer2/"
   appcast "https://updates.devmate.com/com.gentlebytes.Startupizer#{version.major}.xml"
   name "Startupizer#{version.major}"
   desc "Login items handler"
