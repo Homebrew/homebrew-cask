9745229a826d27c95f15b0807cb0245065b76db2
Markus Reiter <me@reitermark.us>
2020-12-12 05:20:53 +0100

    aircall: switch to `verified` parameter

--- Casks/aircall.rb
+++ Casks/aircall.rb
@@ -2,8 +2,8 @@ cask "aircall" do
   version "2.8.4"
   sha256 "1678830f90a1381ddbf1a7cf6cdb6f1c3563a21188a4b08466b7113aa5541eaf"
 
-  # aircall-electron-releases.s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://aircall-electron-releases.s3.amazonaws.com/production/Aircall-#{version}.zip"
+  url "https://aircall-electron-releases.s3.amazonaws.com/production/Aircall-#{version}.zip",
+      verified: "aircall-electron-releases.s3.amazonaws.com/"
   appcast "https://electron.aircall.io/update/osx/0.0.0"
   name "Aircall"
   desc "Cloud-based call center and phone system software"
