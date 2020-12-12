bd56179adf782377e658c84b187367a73565bbcb
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:33 +0100

    exfalso: switch to `verified` parameter

--- Casks/exfalso.rb
+++ Casks/exfalso.rb
@@ -2,8 +2,8 @@ cask "exfalso" do
   version "4.3.0"
   sha256 "988afe6d13691e2ea05c6672aa937d66f4a13f3372d44b6344b50a32da12cf0f"
 
-  # github.com/quodlibet/quodlibet/ was verified as official when first introduced to the cask
-  url "https://github.com/quodlibet/quodlibet/releases/download/release-#{version}/ExFalso-#{version}.dmg"
+  url "https://github.com/quodlibet/quodlibet/releases/download/release-#{version}/ExFalso-#{version}.dmg",
+      verified: "github.com/quodlibet/quodlibet/"
   appcast "https://github.com/quodlibet/quodlibet/releases.atom"
   name "Ex Falso"
   desc "Music tag editor"
