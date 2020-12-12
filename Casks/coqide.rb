64bee2744357614d5911b1f4776fb98e46b32b8f
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:18 +0100

    coqide: switch to `verified` parameter

--- Casks/coqide.rb
+++ Casks/coqide.rb
@@ -2,8 +2,8 @@ cask "coqide" do
   version "8.12.2"
   sha256 "6cc8417d5b90d823752e142561da677ceac94ab5cbfc5e14acad363c7aab46fc"
 
-  # github.com/coq/coq/ was verified as official when first introduced to the cask
-  url "https://github.com/coq/coq/releases/download/V#{version.major_minor_patch}/coq-#{version}-installer-macos.dmg"
+  url "https://github.com/coq/coq/releases/download/V#{version.major_minor_patch}/coq-#{version}-installer-macos.dmg",
+      verified: "github.com/coq/coq/"
   appcast "https://github.com/coq/coq/releases.atom"
   name "Coq"
   homepage "https://coq.inria.fr/"
