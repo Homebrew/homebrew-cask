a75123c931e0e539d529a002617e9be4650279dd
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:55 +0100

    kactus: switch to `verified` parameter

--- Casks/kactus.rb
+++ Casks/kactus.rb
@@ -2,8 +2,8 @@ cask "kactus" do
   version "0.3.32"
   sha256 "beb91b62c02ae1850a645b7d339b2a03850d0d7ae226ef3688a5e0185fe76c0b"
 
-  # github.com/kactus-io/kactus/ was verified as official when first introduced to the cask
-  url "https://github.com/kactus-io/kactus/releases/download/v#{version}/Kactus-macos.zip"
+  url "https://github.com/kactus-io/kactus/releases/download/v#{version}/Kactus-macos.zip",
+      verified: "github.com/kactus-io/kactus/"
   appcast "https://github.com/kactus-io/kactus/releases.atom"
   name "Kactus"
   desc "True version control tool for designers"
