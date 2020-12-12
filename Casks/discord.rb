382d5d0052b8c787856fb62ca24a175ea5c92cb7
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:24 +0100

    discord: switch to `verified` parameter

--- Casks/discord.rb
+++ Casks/discord.rb
@@ -2,7 +2,8 @@ cask "discord" do
   version "0.0.260"
   sha256 "9cddabb13d862e45a0287591b38c13fde9b372edee7f7a538ba7e375c8b32088"
 
-  url "https://cdn.discordapp.com/apps/osx/#{version}/Discord.dmg"
+  url "https://cdn.discordapp.com/apps/osx/#{version}/Discord.dmg",
+      verified: "cdn.discordapp.com/"
   appcast "https://discord.com/api/stable/updates?platform=osx"
   name "Discord"
   desc "Voice and text chat software"
