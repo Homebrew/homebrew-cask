53d053151dad9ef68053b192bb1b4d43500b0501
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:35 +0100

    firecamp: switch to `verified` parameter

--- Casks/firecamp.rb
+++ Casks/firecamp.rb
@@ -2,8 +2,8 @@ cask "firecamp" do
   version "1.4.4"
   sha256 "de6e433037328709b9c0d2e7b321550945cc323d92e8d7b83f5e7f3bd9c5a5aa"
 
-  # firecamp.ams3.digitaloceanspaces.com/ was verified as official when first introduced to the cask
-  url "https://firecamp.ams3.digitaloceanspaces.com/versions/mac/Firecamp-#{version}.dmg"
+  url "https://firecamp.ams3.digitaloceanspaces.com/versions/mac/Firecamp-#{version}.dmg",
+      verified: "firecamp.ams3.digitaloceanspaces.com/"
   appcast "https://firecamp.io/downloads/"
   name "Firecamp"
   homepage "https://firecamp.io/"
