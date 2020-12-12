cda2ee45e28fb3547f2f540a502ce628a16e7f48
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:12 +0100

    lektor: switch to `verified` parameter

--- Casks/lektor.rb
+++ Casks/lektor.rb
@@ -2,8 +2,8 @@ cask "lektor" do
   version "3.0.1"
   sha256 "112e42681bb86840261ee456f632f5e2807b5d24e41a2e84046f1586379e3572"
 
-  # github.com/lektor/lektor/ was verified as official when first introduced to the cask
-  url "https://github.com/lektor/lektor/releases/download/#{version}/Lektor-#{version}.dmg"
+  url "https://github.com/lektor/lektor/releases/download/#{version}/Lektor-#{version}.dmg",
+      verified: "github.com/lektor/lektor/"
   appcast "https://github.com/lektor/lektor/releases.atom"
   name "Lektor"
   desc "Static file content management system"
