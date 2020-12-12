9244457b8374059ac1294c05645950d6057f2d46
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:32 +0100

    openra: switch to `verified` parameter

--- Casks/openra.rb
+++ Casks/openra.rb
@@ -2,8 +2,8 @@ cask "openra" do
   version "20200503"
   sha256 "0240e82003fc46af6b321efd0a5ef7703f4f0293fe75212ef29cad67a705c4fb"
 
-  # github.com/OpenRA/OpenRA/ was verified as official when first introduced to the cask
-  url "https://github.com/OpenRA/OpenRA/releases/download/release-#{version}/OpenRA-release-#{version}.dmg"
+  url "https://github.com/OpenRA/OpenRA/releases/download/release-#{version}/OpenRA-release-#{version}.dmg",
+      verified: "github.com/OpenRA/OpenRA/"
   appcast "https://github.com/OpenRA/OpenRA/releases.atom"
   name "OpenRA"
   desc "Real-time strategy game engine for Westwood games"
