be7ed7ddee0a28d190aeaeecd228215da07ad7f3
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:19 +0100

    memory: switch to `verified` parameter

--- Casks/memory.rb
+++ Casks/memory.rb
@@ -2,8 +2,8 @@ cask "memory" do
   version "2020.23,237"
   sha256 :no_check
 
-  # memorymacapp.s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://memorymacapp.s3.amazonaws.com/Memory.zip"
+  url "https://memorymacapp.s3.amazonaws.com/Memory.zip",
+      verified: "memorymacapp.s3.amazonaws.com/"
   appcast "https://memorymacapp.s3-eu-west-1.amazonaws.com/sparkle.xml"
   name "Memory Tracker by Timely"
   homepage "https://memory.ai/timely/"
