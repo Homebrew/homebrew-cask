fc7c5208ae113d3b0e490e6a24976e85da75589e
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:10 +0100

    lando: switch to `verified` parameter

--- Casks/lando.rb
+++ Casks/lando.rb
@@ -2,8 +2,8 @@ cask "lando" do
   version "3.0.23"
   sha256 "d4a317ad725e7f3b72f91235ea7e0d43857489ee3db23132d14675aa1715974b"
 
-  # github.com/lando/lando/ was verified as official when first introduced to the cask
-  url "https://github.com/lando/lando/releases/download/v#{version}/lando-v#{version}.dmg"
+  url "https://github.com/lando/lando/releases/download/v#{version}/lando-v#{version}.dmg",
+      verified: "github.com/lando/lando/"
   appcast "https://github.com/lando/lando/releases.atom"
   name "Lando"
   homepage "https://docs.lando.dev/"
