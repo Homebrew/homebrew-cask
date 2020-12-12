1c3be29123141d2b0286a1dc078d0e44daac9b97
Randall <ran.dall@icloud.com>
2020-12-12 10:57:58 -0800

    Update razorsql from 9.2.6 to 9.2.7

--- Casks/razorsql.rb
+++ Casks/razorsql.rb
@@ -1,6 +1,6 @@
 cask "razorsql" do
-  version "9.2.6"
-  sha256 "56415127862b0ab85d25258e4230db1f99d01396259126ca52bec47e29c497b1"
+  version "9.2.7"
+  sha256 "683b5124c21166b4747cc6e165ba0a709d932634fa8d881d7a8aca1f3b39b959"
 
   url "https://s3.dualstack.us-east-1.amazonaws.com/downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}.dmg",
       verified: "s3.dualstack.us-east-1.amazonaws.com/"
