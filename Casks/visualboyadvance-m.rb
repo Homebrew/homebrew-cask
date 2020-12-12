f624fc01c2c64d48fa1c88b62ac211d3ccf86e43
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:10 +0100

    visualboyadvance-m: switch to `verified` parameter

--- Casks/visualboyadvance-m.rb
+++ Casks/visualboyadvance-m.rb
@@ -2,8 +2,8 @@ cask "visualboyadvance-m" do
   version "2.1.4"
   sha256 "9127149a452b6e67280c99eb133480fc22095a2ceafdb049d1df9324c35d9197"
 
-  # github.com/visualboyadvance-m/visualboyadvance-m/ was verified as official when first introduced to the cask
-  url "https://github.com/visualboyadvance-m/visualboyadvance-m/releases/download/v#{version}/visualboyadvance-m-Mac-64bit.zip"
+  url "https://github.com/visualboyadvance-m/visualboyadvance-m/releases/download/v#{version}/visualboyadvance-m-Mac-64bit.zip",
+      verified: "github.com/visualboyadvance-m/visualboyadvance-m/"
   appcast "https://github.com/visualboyadvance-m/visualboyadvance-m/releases.atom"
   name "Visual Boy Advance - M"
   homepage "https://vba-m.com/"
