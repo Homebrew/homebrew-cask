8484f97329d2bf9d2e5c31ae1a1795f948939780
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:48 +0100

    rssowl: switch to `verified` parameter

--- Casks/rssowl.rb
+++ Casks/rssowl.rb
@@ -2,8 +2,8 @@ cask "rssowl" do
   version "2.2.1"
   sha256 "9a94beac7951937b6684aeabea323993860c43f8adbe959bb7a109dc22f0598c"
 
-  # github.com/rssowl/RSSOwl/ was verified as official when first introduced to the cask
-  url "https://github.com/rssowl/RSSOwl/releases/download/#{version}/RSSOwl.#{version}.dmg"
+  url "https://github.com/rssowl/RSSOwl/releases/download/#{version}/RSSOwl.#{version}.dmg",
+      verified: "github.com/rssowl/RSSOwl/"
   appcast "https://github.com/rssowl/RSSOwl/releases.atom"
   name "RSSOwl"
   homepage "https://www.rssowl.org/"
