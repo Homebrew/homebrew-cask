ed519c740a1c20f7d1359b2a48969e7195564074
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:56 +0100

    spectacle-editor: switch to `verified` parameter

--- Casks/spectacle-editor.rb
+++ Casks/spectacle-editor.rb
@@ -2,8 +2,8 @@ cask "spectacle-editor" do
   version "0.1.6"
   sha256 "5dc93387bc6026dd44dbf2f88c9bf7730d931663d5b72f382c0a51bc6c57517b"
 
-  # github.com/FormidableLabs/spectacle-editor/ was verified as official when first introduced to the cask
-  url "https://github.com/FormidableLabs/spectacle-editor/releases/download/v#{version}/Spectacle.Editor-#{version}.dmg"
+  url "https://github.com/FormidableLabs/spectacle-editor/releases/download/v#{version}/Spectacle.Editor-#{version}.dmg",
+      verified: "github.com/FormidableLabs/spectacle-editor/"
   appcast "https://github.com/FormidableLabs/spectacle-editor/releases.atom"
   name "Spectacle Editor"
   desc "Drag and drop Spectacle editor"
