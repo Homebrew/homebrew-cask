81cff21bd3ac2262655d4470b0897ba21900eec4
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:56 +0100

    spitfire-audio: switch to `verified` parameter

--- Casks/spitfire-audio.rb
+++ Casks/spitfire-audio.rb
@@ -2,8 +2,8 @@ cask "spitfire-audio" do
   version "3.2.17,1605696000"
   sha256 "8a61a597f58dec65426a49c30da2bb9977e778233d18df9eceb3e8e6bc27be3b"
 
-  # d1t3zg51rvnesz.cloudfront.net/ was verified as official when first introduced to the cask
-  url "https://d1t3zg51rvnesz.cloudfront.net/p/files/lm/#{version.after_comma}/mac/SpitfireAudio-Mac-#{version.before_comma}.dmg"
+  url "https://d1t3zg51rvnesz.cloudfront.net/p/files/lm/#{version.after_comma}/mac/SpitfireAudio-Mac-#{version.before_comma}.dmg",
+      verified: "d1t3zg51rvnesz.cloudfront.net/"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.spitfireaudio.com/library-manager/download/mac/"
   name "Spitfire Audio"
   desc "Downloade manager for Spitfire audio libraries"
