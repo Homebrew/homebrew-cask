3981ff52dca519d786997dde28662f6eff6949de
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:46 +0100

    haptic-touch-bar: switch to `verified` parameter

--- Casks/haptic-touch-bar.rb
+++ Casks/haptic-touch-bar.rb
@@ -2,8 +2,8 @@ cask "haptic-touch-bar" do
   version "2.4.0,1540815050"
   sha256 "c7b044f1516bb0912e863e2e3a3fd080fa06833eaddc0f056caf55a5e9df94b5"
 
-  # dl.devmate.com/com.bopsoft.HapticTouchBar/ was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/com.bopsoft.HapticTouchBar/#{version.before_comma.no_dots}/#{version.after_comma}/HapticTouchBar-#{version.before_comma.no_dots}.zip"
+  url "https://dl.devmate.com/com.bopsoft.HapticTouchBar/#{version.before_comma.no_dots}/#{version.after_comma}/HapticTouchBar-#{version.before_comma.no_dots}.zip",
+      verified: "dl.devmate.com/com.bopsoft.HapticTouchBar/"
   appcast "https://updates.devmate.com/com.bopsoft.HapticTouchBar.xml"
   name "Haptic Touch Bar"
   homepage "https://www.haptictouchbar.com/"
