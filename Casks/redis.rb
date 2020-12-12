e6885db5eaedcdf3af611911a6eeced4aec9d6f7
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:46 +0100

    redis: switch to `verified` parameter

--- Casks/redis.rb
+++ Casks/redis.rb
@@ -2,8 +2,8 @@ cask "redis" do
   version "4.0.2-build.1"
   sha256 "1a4c0c82739a2bddbd5fa78f598cd28dd2c348467a12cb8de6687114f2bad4da"
 
-  # github.com/jpadilla/redisapp/ was verified as official when first introduced to the cask
-  url "https://github.com/jpadilla/redisapp/releases/download/#{version}/Redis.zip"
+  url "https://github.com/jpadilla/redisapp/releases/download/#{version}/Redis.zip",
+      verified: "github.com/jpadilla/redisapp/"
   appcast "https://github.com/jpadilla/redisapp/releases.atom"
   name "Redis"
   homepage "https://jpadilla.github.io/redisapp/"
