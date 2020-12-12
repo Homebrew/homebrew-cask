084a73354585861b1b0a673c0276be56f467058e
Randall <ran.dall@icloud.com>
2020-12-12 10:52:04 -0800

    Update mochi from 1.7.0 to 1.7.1

--- Casks/mochi.rb
+++ Casks/mochi.rb
@@ -1,6 +1,6 @@
 cask "mochi" do
-  version "1.7.0"
-  sha256 "87c1e5d65298809f868fbec3c0d35f696b5114ffb24f53d3c71bd867c3a9887b"
+  version "1.7.1"
+  sha256 "7b87fb1b1360c595c56a591c037a47e4ad21e4743ac7cb07151b1bed8283d5d2"
 
   url "https://mochi.cards/releases/Mochi-#{version}.dmg"
   appcast "https://mochi.cards/"
