da421410f2211d9228b94d1e133c05b4157b6d00
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:10 +0100

    kode54-cog: switch to `verified` parameter

--- Casks/kode54-cog.rb
+++ Casks/kode54-cog.rb
@@ -2,8 +2,8 @@ cask "kode54-cog" do
   version "1312,58b6ea588"
   sha256 "d99777b26b18fb04216bf144ef54ed13dfc2e311502d46df51c903211723d87b"
 
-  # losno.co/cog/ was verified as official when first introduced to the cask
-  url "https://f.losno.co/cog/Cog-#{version.after_comma}.zip"
+  url "https://f.losno.co/cog/Cog-#{version.after_comma}.zip",
+      verified: "losno.co/cog/"
   appcast "https://balde.losno.co/cog/mercury.xml"
   name "Cog"
   homepage "https://kode54.net/cog/"
