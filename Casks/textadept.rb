cdc1b0f18a51bfcef555fc5e9e36876b12158329
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:03 +0100

    textadept: switch to `verified` parameter

--- Casks/textadept.rb
+++ Casks/textadept.rb
@@ -2,7 +2,8 @@ cask "textadept" do
   version "11.0"
   sha256 "60b7e3c5debfc396092e8a2b14ebb806ba6803ea9bc665e5ca9a625e3248cf92"
 
-  url "https://github.com/orbitalquark/textadept/releases/download/textadept_#{version}/textadept_#{version}.macOS.zip"
+  url "https://github.com/orbitalquark/textadept/releases/download/textadept_#{version}/textadept_#{version}.macOS.zip",
+      verified: "github.com/orbitalquark/textadept/"
   appcast "https://github.com/orbitalquark/textadept/releases.atom"
   name "Textadept"
   desc "Cross-platform text editor"
