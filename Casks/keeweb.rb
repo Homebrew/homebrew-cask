59a051fff92aae7f6baff93a5ddea7411b03a2bc
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:56 +0100

    keeweb: switch to `verified` parameter

--- Casks/keeweb.rb
+++ Casks/keeweb.rb
@@ -2,8 +2,8 @@ cask "keeweb" do
   version "1.16.3"
   sha256 "91204ca969f3047d41e632c3a5efb0526aec460f7281747693f8bc9fc6b28281"
 
-  # github.com/keeweb/keeweb/ was verified as official when first introduced to the cask
-  url "https://github.com/keeweb/keeweb/releases/download/v#{version}/KeeWeb-#{version}.mac.x64.dmg"
+  url "https://github.com/keeweb/keeweb/releases/download/v#{version}/KeeWeb-#{version}.mac.x64.dmg",
+      verified: "github.com/keeweb/keeweb/"
   appcast "https://github.com/keeweb/keeweb/releases.atom"
   name "KeeWeb"
   desc "Free cross-platform password manager compatible with KeePass"
