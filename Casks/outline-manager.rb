0b8f16c196ccfeccd03211c48c66a82fad9c2424
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:35 +0100

    outline-manager: switch to `verified` parameter

--- Casks/outline-manager.rb
+++ Casks/outline-manager.rb
@@ -2,8 +2,8 @@ cask "outline-manager" do
   version "1.6.1"
   sha256 "329db2c8c64c5b8700f0ade335d69c68020dd5ab0ff788a68c90771b1dc6905a"
 
-  # github.com/Jigsaw-Code/outline-server/ was verified as official when first introduced to the cask
-  url "https://github.com/Jigsaw-Code/outline-server/releases/download/v#{version}/Outline-Manager.dmg"
+  url "https://github.com/Jigsaw-Code/outline-server/releases/download/v#{version}/Outline-Manager.dmg",
+      verified: "github.com/Jigsaw-Code/outline-server/"
   appcast "https://github.com/Jigsaw-Code/outline-server/releases.atom"
   name "Outline Manager"
   desc "Tool to create and manage Outline servers, powered by Shadowsocks"
