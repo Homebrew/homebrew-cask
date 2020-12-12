6cd72e9d7c0cf014e29fa66f2c56b47e952b88bd
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:12 +0100

    caret: switch to `verified` parameter

--- Casks/caret.rb
+++ Casks/caret.rb
@@ -2,8 +2,8 @@ cask "caret" do
   version "3.4.6"
   sha256 "a7d17bb7e9c938d8559f1569899a14413dae33bc4a7d4de038bf430447008aea"
 
-  # github.com/careteditor/issues/ was verified as official when first introduced to the cask
-  url "https://github.com/careteditor/issues/releases/download/#{version}/Caret.dmg"
+  url "https://github.com/careteditor/issues/releases/download/#{version}/Caret.dmg",
+      verified: "github.com/careteditor/issues/"
   appcast "https://github.com/careteditor/issues/releases.atom"
   name "Caret"
   homepage "https://caret.io/"
