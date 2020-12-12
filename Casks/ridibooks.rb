90016b22d6778a71318357a13ef54749ce43f6ce
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:47 +0100

    ridibooks: switch to `verified` parameter

--- Casks/ridibooks.rb
+++ Casks/ridibooks.rb
@@ -2,8 +2,8 @@ cask "ridibooks" do
   version "0.8.12"
   sha256 "1f3c84b3bca3952ba18af015303e00470e5ac786ee8e8a99b3ec1c8b196db55e"
 
-  # viewer-ota.ridicdn.net/pc_electron/ was verified as official when first introduced to the cask
-  url "https://viewer-ota.ridicdn.net/pc_electron/Ridibooks-#{version}.dmg"
+  url "https://viewer-ota.ridicdn.net/pc_electron/Ridibooks-#{version}.dmg",
+      verified: "viewer-ota.ridicdn.net/pc_electron/"
   appcast "https://s3-ap-northeast-2.amazonaws.com/viewer-ota.ridicdn.net/pc_electron/latest-mac.yml"
   name "Ridibooks"
   homepage "https://ridibooks.com/support/app/download"
