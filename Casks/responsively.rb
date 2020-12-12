07c0b6bc87d9ab5858910d6b9f1ced5373b38172
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:46 +0100

    responsively: switch to `verified` parameter

--- Casks/responsively.rb
+++ Casks/responsively.rb
@@ -2,8 +2,8 @@ cask "responsively" do
   version "0.15.0"
   sha256 "db01fb5e333056fe6cf4945d677b57190e82f6e26d4524277ef06c3df333f135"
 
-  # github.com/responsively-org/responsively-app/ was verified as official when first introduced to the cask
-  url "https://github.com/responsively-org/responsively-app/releases/download/v#{version}/ResponsivelyApp-#{version}.dmg"
+  url "https://github.com/responsively-org/responsively-app/releases/download/v#{version}/ResponsivelyApp-#{version}.dmg",
+      verified: "github.com/responsively-org/responsively-app/"
   appcast "https://github.com/responsively-org/responsively-app/releases.atom"
   name "Responsively"
   desc "Modified browser that helps in responsive web development"
