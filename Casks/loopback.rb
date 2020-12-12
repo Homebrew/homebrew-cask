24d796204cf93eb18ab12d43391ff3190ef98bc2
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:14 +0100

    loopback: switch to `verified` parameter

--- Casks/loopback.rb
+++ Casks/loopback.rb
@@ -2,8 +2,8 @@ cask "loopback" do
   version "2.1.9"
   sha256 :no_check
 
-  # d2oxtzozd38ts8.cloudfront.net/loopback/ was verified as official when first introduced to the cask
-  url "https://d2oxtzozd38ts8.cloudfront.net/loopback/download/Loopback.zip"
+  url "https://d2oxtzozd38ts8.cloudfront.net/loopback/download/Loopback.zip",
+      verified: "d2oxtzozd38ts8.cloudfront.net/loopback/"
   appcast "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.loopback&system=10141&platform=osx&arch=x86_64&version=2008000"
   name "Loopback"
   desc "Cable-free audio router"
