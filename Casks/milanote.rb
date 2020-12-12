235aa83036cd2f3d4071542815a4b19d11c5b38c
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:21 +0100

    milanote: switch to `verified` parameter

--- Casks/milanote.rb
+++ Casks/milanote.rb
@@ -2,8 +2,8 @@ cask "milanote" do
   version "2.2.24"
   sha256 "113228205b9af2f808d26a1d432449b6b07c022340f231d548c6004cdd01f3a2"
 
-  # milanote-app-releases.s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://milanote-app-releases.s3.amazonaws.com/Milanote-#{version}.dmg"
+  url "https://milanote-app-releases.s3.amazonaws.com/Milanote-#{version}.dmg",
+      verified: "milanote-app-releases.s3.amazonaws.com/"
   appcast "https://www.milanote.com/download-mac-app"
   name "Milanote"
   homepage "https://www.milanote.com/"
