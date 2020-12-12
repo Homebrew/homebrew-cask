0dcc3846dabfeeb2764e3b42d5280e33423d7ec9
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:28 +0100

    dupeguru: switch to `verified` parameter

--- Casks/dupeguru.rb
+++ Casks/dupeguru.rb
@@ -2,8 +2,8 @@ cask "dupeguru" do
   version "4.0.3"
   sha256 "805fa0e2ddf57ac7d7e79cebe893a3a505f24c28500a204d22eb2b6d3d889454"
 
-  # github.com/arsenetar/dupeguru/ was verified as official when first introduced to the cask
-  url "https://github.com/arsenetar/dupeguru/releases/download/#{version}/dupeguru_osx_#{version.dots_to_underscores}.dmg"
+  url "https://github.com/arsenetar/dupeguru/releases/download/#{version}/dupeguru_osx_#{version.dots_to_underscores}.dmg",
+      verified: "github.com/arsenetar/dupeguru/"
   appcast "https://github.com/arsenetar/dupeguru/releases.atom"
   name "dupeGuru"
   desc "Finds duplicate files in a computer system"
