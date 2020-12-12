7305d357778cead814aea904b355f6cd250f8897
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:30 +0100

    electron: switch to `verified` parameter

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -2,8 +2,8 @@ cask "electron" do
   version "11.0.4"
   sha256 "0b5bb200fa901eb08337c8691673583f8fee084704d45450bc2492f992f6c753"
 
-  # github.com/electron/electron/ was verified as official when first introduced to the cask
-  url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
+  url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip",
+      verified: "github.com/electron/electron/"
   appcast "https://www.electronjs.org/releases/stable"
   name "Electron"
   desc "Build cross-platform desktop apps with JavaScript, HTML, and CSS"
