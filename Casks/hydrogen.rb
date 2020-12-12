758e672be550dc668c35fdf3741ff1910a6e379b
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:48 +0100

    hydrogen: switch to `verified` parameter

--- Casks/hydrogen.rb
+++ Casks/hydrogen.rb
@@ -2,8 +2,8 @@ cask "hydrogen" do
   version "1.0.1"
   sha256 "20b33e3b8c308a2fb8a82dd0712c301c1187ec57b2da934ddfc1167a11252a47"
 
-  # downloads.sourceforge.net/hydrogen/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/hydrogen/Hydrogen-#{version}.dmg"
+  url "https://downloads.sourceforge.net/hydrogen/Hydrogen-#{version}.dmg",
+      verified: "downloads.sourceforge.net/hydrogen/"
   appcast "https://sourceforge.net/projects/hydrogen/rss?path=/Hydrogen"
   name "Hydrogen"
   desc "Software synthesizer"
