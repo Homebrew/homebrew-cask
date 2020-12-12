6a17fb7efa0a360ea83611ccbf129a032fd5e0ca
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:26 +0100

    downie: switch to `verified` parameter

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -2,8 +2,8 @@ cask "downie" do
   version "4.1.11,4185"
   sha256 "641335c07551b6f8352eff2485c87055c59c8c6b48d6acb2e64c47f1159a7320"
 
-  # charliemonroesoftware.com/ was verified as official when first introduced to the cask
-  url "https://charliemonroesoftware.com/trial/downie/v#{version.major}/Downie_#{version.major}_#{version.after_comma}.dmg"
+  url "https://charliemonroesoftware.com/trial/downie/v#{version.major}/Downie_#{version.major}_#{version.after_comma}.dmg",
+      verified: "charliemonroesoftware.com/"
   appcast "https://charliemonroesoftware.com/trial/downie/v#{version.major}/updates.xml"
   name "Downie"
   desc "Downloads videos from different websites"
