3b9b5e850cd2b1d59d2d100b450cd82e46c9cb39
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:24 +0100

    djview: switch to `verified` parameter

--- Casks/djview.rb
+++ Casks/djview.rb
@@ -2,8 +2,8 @@ cask "djview" do
   version "4.12,3"
   sha256 "f846b1488414f2063c1097768c5ca1d8a4dd8889749de03b108ce7c0941b9c3e"
 
-  # downloads.sourceforge.net/djvu/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/djvu/DjVuLibre-3.5.28%2BDjView-#{version.before_comma}-intel64-#{version.after_comma}.dmg"
+  url "https://downloads.sourceforge.net/djvu/DjVuLibre-3.5.28%2BDjView-#{version.before_comma}-intel64-#{version.after_comma}.dmg",
+      verified: "downloads.sourceforge.net/djvu/"
   appcast "https://sourceforge.net/projects/djvu/rss"
   name "DjView"
   homepage "https://djvu.sourceforge.io/"
