008f32b6ae0b69565651721863874de3ce419f22
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:38 +0100

    freecol: switch to `verified` parameter

--- Casks/freecol.rb
+++ Casks/freecol.rb
@@ -2,8 +2,8 @@ cask "freecol" do
   version "0.11.6"
   sha256 "f771f9e31876050281bddb3cc18cb3e5e7f10b32426730c59a9688e1f3496cb1"
 
-  # sourceforge.net/freecol/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/freecol/freecol-#{version}-mac.tar.bz2"
+  url "https://downloads.sourceforge.net/freecol/freecol-#{version}-mac.tar.bz2",
+      verified: "sourceforge.net/freecol/"
   appcast "https://sourceforge.net/projects/freecol/rss"
   name "FreeCol"
   homepage "http://www.freecol.org/"
