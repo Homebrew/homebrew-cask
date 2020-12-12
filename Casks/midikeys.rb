ce9f014c454af2e1a54cb5998fe4713b86713581
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:20 +0100

    midikeys: switch to `verified` parameter

--- Casks/midikeys.rb
+++ Casks/midikeys.rb
@@ -2,8 +2,8 @@ cask "midikeys" do
   version "1.9.0"
   sha256 "1a0d01695e039683429ff9fd679d4094756b95f2e46efb967e54eb14948ddb18"
 
-  # github.com/flit/MidiKeys/ was verified as official when first introduced to the cask
-  url "https://github.com/flit/MidiKeys/releases/download/v#{version}/MidiKeys_#{version.major_minor}.zip"
+  url "https://github.com/flit/MidiKeys/releases/download/v#{version}/MidiKeys_#{version.major_minor}.zip",
+      verified: "github.com/flit/MidiKeys/"
   appcast "https://immosw.com/versions/midikeys/appcast.xml"
   name "MidiKeys"
   homepage "https://www.manyetas.com/creed/midikeys.html"
