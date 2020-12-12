fdd2b4985d422a6d4e389d86abb074fb2b491961
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:10 +0100

    butt: switch to `verified` parameter

--- Casks/butt.rb
+++ Casks/butt.rb
@@ -2,8 +2,8 @@ cask "butt" do
   version "0.1.25"
   sha256 "d3ca79e4de20c71385da25738b721ef3953ef96eed94b09326112f5f83e060ba"
 
-  # sourceforge.net/butt/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/butt/butt-#{version}.dmg"
+  url "https://downloads.sourceforge.net/butt/butt-#{version}.dmg",
+      verified: "sourceforge.net/butt/"
   appcast "https://sourceforge.net/projects/butt/rss"
   name "Broadcast Using This Tool"
   desc "Shoutcast and Icecast streaming client"
