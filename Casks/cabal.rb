f1f3888adb33d117648ca540c7395bb73ae744ed
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:11 +0100

    cabal: switch to `verified` parameter

--- Casks/cabal.rb
+++ Casks/cabal.rb
@@ -2,8 +2,8 @@ cask "cabal" do
   version "6.0.5"
   sha256 "0361917bfdabc760037ae64cb3dc20e38f7c879666b963d3c33056e77da8af77"
 
-  # github.com/cabal-club/cabal-desktop/ was verified as official when first introduced to the cask
-  url "https://github.com/cabal-club/cabal-desktop/releases/download/v#{version}/cabal-desktop-#{version}-mac.dmg"
+  url "https://github.com/cabal-club/cabal-desktop/releases/download/v#{version}/cabal-desktop-#{version}-mac.dmg",
+      verified: "github.com/cabal-club/cabal-desktop/"
   appcast "https://github.com/cabal-club/cabal-desktop/releases.atom"
   name "Cabal"
   desc "Desktop client for the chat platform Cabal"
