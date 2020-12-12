86eb14cdebb780fb01167add9e96cbc45bccf6d4
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:00 +0100

    swift-publisher: switch to `verified` parameter

--- Casks/swift-publisher.rb
+++ Casks/swift-publisher.rb
@@ -2,8 +2,8 @@ cask "swift-publisher" do
   version "5.5.6,4560"
   sha256 :no_check
 
-  # belightsoft.s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://belightsoft.s3.amazonaws.com/SwiftPublisher.dmg"
+  url "https://belightsoft.s3.amazonaws.com/SwiftPublisher.dmg",
+      verified: "belightsoft.s3.amazonaws.com/"
   appcast "https://www.belightsoft.com/download/updates/appcast_SwiftPublisher#{version.major}.xml"
   name "Swift Publisher"
   desc "Page layout and desktop publishing application"
