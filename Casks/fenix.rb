f1d3df21551eb708e2c3debd8f87e3b0e4acc2c0
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:34 +0100

    fenix: switch to `verified` parameter

--- Casks/fenix.rb
+++ Casks/fenix.rb
@@ -2,8 +2,8 @@ cask "fenix" do
   version "2.0.0"
   sha256 "f2155a6611c7048f0f3a94c06de6cfa4b2e6e5df05e1d68ed7c9aebf8786c1cf"
 
-  # github.com/coreybutler/fenix/ was verified as official when first introduced to the cask
-  url "https://github.com/coreybutler/fenix/releases/download/v#{version}/fenix-osx-#{version}.zip"
+  url "https://github.com/coreybutler/fenix/releases/download/v#{version}/fenix-osx-#{version}.zip",
+      verified: "github.com/coreybutler/fenix/"
   appcast "https://github.com/coreybutler/fenix/releases.atom"
   name "Fenix"
   desc "Simple and visual static web server with collaboration features"
