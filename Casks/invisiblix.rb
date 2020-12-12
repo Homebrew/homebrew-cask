5211937380ee4db660914bbe92952fde01c12b0d
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:51 +0100

    invisiblix: switch to `verified` parameter

--- Casks/invisiblix.rb
+++ Casks/invisiblix.rb
@@ -2,8 +2,8 @@ cask "invisiblix" do
   version "3.2"
   sha256 "528328a0e7c3f0a72e763ea766324d491dfe20d6f18a2882eefda1a5a2c4d68e"
 
-  # sourceforge.net/invisiblix/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/invisiblix/invisibliX-#{version}.zip"
+  url "https://downloads.sourceforge.net/invisiblix/invisibliX-#{version}.zip",
+      verified: "sourceforge.net/invisiblix/"
   appcast "https://invisiblix.read-write.fr/appcast.xml"
   name "invisibliX"
   desc "Allows viewing and manipulation of hidden files in Finder"
