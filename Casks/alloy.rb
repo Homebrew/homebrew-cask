9ad48f1d39c931d032315166c10ea2b529009e5f
Markus Reiter <me@reitermark.us>
2020-12-12 05:20:54 +0100

    alloy: switch to `verified` parameter

--- Casks/alloy.rb
+++ Casks/alloy.rb
@@ -2,8 +2,8 @@ cask "alloy" do
   version "5.1.0"
   sha256 "177140c1e36a1429b91265b7e7e56d694cbf9bd991db40f8b3d92af1d9b8b837"
 
-  # github.com/AlloyTools/org.alloytools.alloy/ was verified as official when first introduced to the cask
-  url "https://github.com/AlloyTools/org.alloytools.alloy/releases/download/v#{version}/alloy.dmg"
+  url "https://github.com/AlloyTools/org.alloytools.alloy/releases/download/v#{version}/alloy.dmg",
+      verified: "github.com/AlloyTools/org.alloytools.alloy/"
   appcast "https://github.com/AlloyTools/org.alloytools.alloy/releases.atom"
   name "Alloy"
   homepage "https://alloytools.org/"
