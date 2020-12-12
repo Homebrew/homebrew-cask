ee95f7aa1779d3a60a6243a190c6252bb1a2b121
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:39 +0100

    freeyourmusic: switch to `verified` parameter

--- Casks/freeyourmusic.rb
+++ Casks/freeyourmusic.rb
@@ -2,8 +2,8 @@ cask "freeyourmusic" do
   version "5.6.7"
   sha256 "73d57a5da519a182d384b7516692c1b2b9d6fec88e92e239f00e45fa3330e11c"
 
-  # dzqeytqqx888.cloudfront.net/ was verified as official when first introduced to the cask
-  url "https://dzqeytqqx888.cloudfront.net/FreeYourMusic-#{version}.dmg"
+  url "https://dzqeytqqx888.cloudfront.net/FreeYourMusic-#{version}.dmg",
+      verified: "dzqeytqqx888.cloudfront.net/"
   appcast "https://stamp-live.s3.amazonaws.com/latest-mac.yml"
   name "FreeYourMusic⁩"
   homepage "https://freeyourmusic.com/"
