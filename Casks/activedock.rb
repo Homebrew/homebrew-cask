c5eb2b569b8654b80a266f8668ab04606d851363
Markus Reiter <me@reitermark.us>
2020-12-12 05:20:52 +0100

    activedock: switch to `verified` parameter

--- Casks/activedock.rb
+++ Casks/activedock.rb
@@ -2,8 +2,8 @@ cask "activedock" do
   version "2.63,2063"
   sha256 :no_check
 
-  # macplus-software.com/ was verified as official when first introduced to the cask
-  url "https://macplus-software.com/downloads/ActiveDock.zip"
+  url "https://macplus-software.com/downloads/ActiveDock.zip",
+      verified: "macplus-software.com/"
   appcast "https://macplus-software.com/downloads/ActiveDock.xml"
   name "ActiveDock"
   desc "Customizable dock, application launcher, dock replacement"
