d2003741f9ee34e5d1f3aa32a542a4f883b1f7a6
Markus Reiter <me@reitermark.us>
2020-12-12 05:20:53 +0100

    air-video-server-hd: switch to `verified` parameter

--- Casks/air-video-server-hd.rb
+++ Casks/air-video-server-hd.rb
@@ -2,8 +2,8 @@ cask "air-video-server-hd" do
   version "2.3.0-beta1u1"
   sha256 "1e45abf868349f1ad43b6f3a1234a254d31e943ef5c7d72b84eff9f955a15a33"
 
-  # s3.amazonaws.com/AirVideoHD/ was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/AirVideoHD/Download/Air+Video+Server+HD+#{version}.dmg"
+  url "https://s3.amazonaws.com/AirVideoHD/Download/Air+Video+Server+HD+#{version}.dmg",
+      verified: "s3.amazonaws.com/AirVideoHD/"
   appcast "https://s3.amazonaws.com/AirVideoHD/Download/appcast.xml"
   name "Air Video Server HD"
   desc "Tool to stream videos to Apple devices"
