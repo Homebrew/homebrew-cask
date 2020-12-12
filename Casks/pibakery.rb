fe75149eb2983669df1c5a25fc32e2376f55845a
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:38 +0100

    pibakery: switch to `verified` parameter

--- Casks/pibakery.rb
+++ Casks/pibakery.rb
@@ -2,8 +2,8 @@ cask "pibakery" do
   version "0.3.6"
   sha256 "f449bfe01806e4c4b30a8603c60c439ce94b579f237c471ffc3057abcf507642"
 
-  # github.com/davidferguson/ was verified as official when first introduced to the cask
-  url "https://github.com/davidferguson/pibakery/releases/download/v#{version}/PiBakery.pkg"
+  url "https://github.com/davidferguson/pibakery/releases/download/v#{version}/PiBakery.pkg",
+      verified: "github.com/davidferguson/"
   appcast "https://github.com/davidferguson/pibakery/releases.atom"
   name "PiBakery"
   desc "Blocks-based setup tool for Raspberry Pi"
