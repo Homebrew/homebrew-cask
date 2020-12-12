858895dbcbf22bacb41f595ae14205725c13a725
Markus Reiter <me@reitermark.us>
2020-12-12 05:20:59 +0100

    arrsync: switch to `verified` parameter

--- Casks/arrsync.rb
+++ Casks/arrsync.rb
@@ -2,8 +2,8 @@ cask "arrsync" do
   version "0.4.1"
   sha256 "8a254771065f90a9f9f882589eccaf7bc51ad5d0ac86faf15847381bcdbe64f0"
 
-  # downloads.sourceforge.net/arrsync/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/arrsync/arrsync-#{version}.dmg"
+  url "https://downloads.sourceforge.net/arrsync/arrsync-#{version}.dmg",
+      verified: "downloads.sourceforge.net/arrsync/"
   appcast "https://sourceforge.net/projects/arrsync/rss"
   name "arRsync"
   homepage "https://arrsync.sourceforge.io/"
