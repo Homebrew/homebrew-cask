6db8d9d7b96127770d67395c3aa0e685a209cdb2
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:45 +0100

    razorsql: switch to `verified` parameter

--- Casks/razorsql.rb
+++ Casks/razorsql.rb
@@ -2,8 +2,8 @@ cask "razorsql" do
   version "9.2.6"
   sha256 "56415127862b0ab85d25258e4230db1f99d01396259126ca52bec47e29c497b1"
 
-  # s3.dualstack.us-east-1.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://s3.dualstack.us-east-1.amazonaws.com/downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}.dmg"
+  url "https://s3.dualstack.us-east-1.amazonaws.com/downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}.dmg",
+      verified: "s3.dualstack.us-east-1.amazonaws.com/"
   appcast "https://razorsql.com/updates.html"
   name "RazorSQL"
   homepage "https://razorsql.com/"
