61fcce34b9e5564f586356999e722b12c1a46977
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:59 +0100

    subler: switch to `verified` parameter

--- Casks/subler.rb
+++ Casks/subler.rb
@@ -2,8 +2,8 @@ cask "subler" do
   version "1.6.9"
   sha256 "1e5ed973059bca998e089d51b6f350390325ea537f095542161d9dda31d9603d"
 
-  # bitbucket.org/galad87/subler/ was verified as official when first introduced to the cask
-  url "https://bitbucket.org/galad87/subler/downloads/Subler-#{version}.zip"
+  url "https://bitbucket.org/galad87/subler/downloads/Subler-#{version}.zip",
+      verified: "bitbucket.org/galad87/subler/"
   appcast "https://subler.org/appcast/appcast.xml"
   name "Subler"
   desc "Mux and tag mp4 files"
