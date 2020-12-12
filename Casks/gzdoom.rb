7ab01782b6d98a64c28ddbb20313a95534a6e8a6
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:46 +0100

    gzdoom: switch to `verified` parameter

--- Casks/gzdoom.rb
+++ Casks/gzdoom.rb
@@ -2,8 +2,8 @@ cask "gzdoom" do
   version "4.5.0"
   sha256 "b7a8056e63fa0a14497e8d1767bbbae2da1ebe9f47e4f87df613106f9ebe3522"
 
-  # github.com/coelckers/gzdoom/ was verified as official when first introduced to the cask
-  url "https://github.com/coelckers/gzdoom/releases/download/g#{version}/gzdoom-#{version.dots_to_hyphens}-macOS.dmg"
+  url "https://github.com/coelckers/gzdoom/releases/download/g#{version}/gzdoom-#{version.dots_to_hyphens}-macOS.dmg",
+      verified: "github.com/coelckers/gzdoom/"
   appcast "https://github.com/coelckers/gzdoom/releases.atom"
   name "gzdoom"
   desc "Adds an OpenGL renderer to the ZDoom source port"
