ebbd9fcec78fa1a40624c208897df02bea69fd78
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:35 +0100

    flightgear: switch to `verified` parameter

--- Casks/flightgear.rb
+++ Casks/flightgear.rb
@@ -2,8 +2,8 @@ cask "flightgear" do
   version "2020.3.4"
   sha256 "cf70d77966956f218a477753e559f4111f05f2b0648822f38f30e6241810d0a2"
 
-  # sourceforge.net/flightgear/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/flightgear/FlightGear-#{version}.dmg"
+  url "https://downloads.sourceforge.net/flightgear/FlightGear-#{version}.dmg",
+      verified: "sourceforge.net/flightgear/"
   appcast "https://sourceforge.net/projects/flightgear/rss"
   name "FlightGear"
   homepage "https://www.flightgear.org/"
