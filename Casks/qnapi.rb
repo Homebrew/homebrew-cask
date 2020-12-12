a92025cee17c32ee364ed1e66a051145474d3415
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:43 +0100

    qnapi: switch to `verified` parameter

--- Casks/qnapi.rb
+++ Casks/qnapi.rb
@@ -2,8 +2,8 @@ cask "qnapi" do
   version "0.2.3"
   sha256 "9bb7f9ba42f6795c7e094ba6555e14955e02b5e2f5277454dc4a96b1569871d1"
 
-  # github.com/QNapi/qnapi/ was verified as official when first introduced to the cask
-  url "https://github.com/QNapi/qnapi/releases/download/#{version}/QNapi-#{version}.dmg"
+  url "https://github.com/QNapi/qnapi/releases/download/#{version}/QNapi-#{version}.dmg",
+      verified: "github.com/QNapi/qnapi/"
   appcast "https://github.com/QNapi/qnapi/releases.atom"
   name "QNapi"
   homepage "https://qnapi.github.io/"
