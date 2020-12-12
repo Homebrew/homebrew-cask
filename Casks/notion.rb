ec6ebabae2cb9e5bc89db2c1ba8794284953f902
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:29 +0100

    notion: switch to `verified` parameter

--- Casks/notion.rb
+++ Casks/notion.rb
@@ -2,8 +2,8 @@ cask "notion" do
   version "2.0.11"
   sha256 "5b549f7ccc4738808ba997b22e63d626dbd79b9d7c544901fca7a9fe0a3073f8"
 
-  # desktop-release.notion-static.com/ was verified as official when first introduced to the cask
-  url "https://desktop-release.notion-static.com/Notion-#{version}.dmg"
+  url "https://desktop-release.notion-static.com/Notion-#{version}.dmg",
+      verified: "desktop-release.notion-static.com/"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.notion.so/desktop/mac/download&user_agent=MacOS"
   name "Notion"
   desc "App to write, plan, collaborate, and get organized"
