c3c7c40aebf0c7cf3d450ed6212f9ed638ce58d7
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:18 +0100

    mediaelch: switch to `verified` parameter

--- Casks/mediaelch.rb
+++ Casks/mediaelch.rb
@@ -2,8 +2,8 @@ cask "mediaelch" do
   version "2.6.6"
   sha256 "624f2d640838c535d618ad0aa2621d354f5248047ff1e2c65e1d4fa3f3294ad4"
 
-  # github.com/Komet/MediaElch/ was verified as official when first introduced to the cask
-  url "https://github.com/Komet/MediaElch/releases/download/v#{version}/MediaElch_#{version}_macOS.dmg"
+  url "https://github.com/Komet/MediaElch/releases/download/v#{version}/MediaElch_#{version}_macOS.dmg",
+      verified: "github.com/Komet/MediaElch/"
   appcast "https://github.com/Komet/MediaElch/releases.atom"
   name "MediaElch"
   desc "Media Manager for Kodi"
