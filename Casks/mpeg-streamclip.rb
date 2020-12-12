8cae1242011571093f26cfc6f5e4d1d2b63f39b9
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:23 +0100

    mpeg-streamclip: switch to `verified` parameter

--- Casks/mpeg-streamclip.rb
+++ Casks/mpeg-streamclip.rb
@@ -2,8 +2,8 @@ cask "mpeg-streamclip" do
   version "1.9.2"
   sha256 "f539e527a7232a9ac4398c0d3e7730010058b300c8d7fd33c4baf8c9ac232b85"
 
-  # alfanet.it/squared5/ was verified as official when first introduced to the cask
-  url "http://www.alfanet.it/squared5/MPEG_Streamclip_#{version}.dmg"
+  url "http://www.alfanet.it/squared5/MPEG_Streamclip_#{version}.dmg",
+      verified: "alfanet.it/squared5/"
   appcast "http://www.squared5.com/svideo/mpeg-streamclip-mac.html"
   name "MPEG Streamclip"
   desc "Video converter, player, and editor"
