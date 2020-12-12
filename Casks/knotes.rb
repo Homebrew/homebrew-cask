a78054d4f2e89289ad373bdf40da7a506635321e
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:09 +0100

    knotes: switch to `verified` parameter

--- Casks/knotes.rb
+++ Casks/knotes.rb
@@ -2,8 +2,8 @@ cask "knotes" do
   version "2.7.0"
   sha256 "0e83b5f86d3e41e54c8e968d596d5109b4870ec5b1af52fa4233641429ccef8b"
 
-  # knotes2.s3-ap-northeast-1.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://knotes2.s3-ap-northeast-1.amazonaws.com/download/mac/Knotes-#{version}.dmg"
+  url "https://knotes2.s3-ap-northeast-1.amazonaws.com/download/mac/Knotes-#{version}.dmg",
+      verified: "knotes2.s3-ap-northeast-1.amazonaws.com/"
   appcast "https://knotesapp.com/"
   name "Knotes"
   homepage "https://knotesapp.com/"
