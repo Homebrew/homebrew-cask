9a4424cb3dc62243fb3826d27a10beda267207ef
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:53 +0100

    jamulus: switch to `verified` parameter

--- Casks/jamulus.rb
+++ Casks/jamulus.rb
@@ -2,8 +2,8 @@ cask "jamulus" do
   version "3.6.1"
   sha256 "f22a883b739585b3bc2d71740489c898740b3f8bb9734f02d902096c3a2436db"
 
-  # downloads.sourceforge.net/llcon/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/llcon/Jamulus-#{version}-installer-mac.dmg"
+  url "https://downloads.sourceforge.net/llcon/Jamulus-#{version}-installer-mac.dmg",
+      verified: "downloads.sourceforge.net/llcon/"
   appcast "https://sourceforge.net/projects/llcon/rss"
   name "Jamulus"
   homepage "https://jamulus.io/"
