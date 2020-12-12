5a3e68f678766e3cab66dd7c4df1eb45a27d7134
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:30 +0100

    electron-fiddle: switch to `verified` parameter

--- Casks/electron-fiddle.rb
+++ Casks/electron-fiddle.rb
@@ -2,8 +2,8 @@ cask "electron-fiddle" do
   version "0.18.0"
   sha256 "5e2a7090fe9237eb9b132ad383ff5b94cd2dc4a836608949b6d5fd330ee7b281"
 
-  # github.com/electron/fiddle/ was verified as official when first introduced to the cask
-  url "https://github.com/electron/fiddle/releases/download/v#{version}/Electron.Fiddle-darwin-x64-#{version}.zip"
+  url "https://github.com/electron/fiddle/releases/download/v#{version}/Electron.Fiddle-darwin-x64-#{version}.zip",
+      verified: "github.com/electron/fiddle/"
   appcast "https://github.com/electron/fiddle/releases.atom"
   name "Electron Fiddle"
   desc "Easiest way to get started with Electron"
