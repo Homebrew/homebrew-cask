43bd0bd9954973e2aaeb086ca254a32dcf89b24f
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:42 +0100

    qcma: switch to `verified` parameter

--- Casks/qcma.rb
+++ Casks/qcma.rb
@@ -2,8 +2,8 @@ cask "qcma" do
   version "0.4.1,-1"
   sha256 "fc286229be41cbeb83fdb8800231f67d8f2f0d51c2fca07f09c7f6e9d4eecca7"
 
-  # github.com/codestation/ was verified as official when first introduced to the cask
-  url "https://github.com/codestation/qcma/releases/download/v#{version.before_comma}/Qcma_#{version.before_comma}#{version.after_comma}.dmg"
+  url "https://github.com/codestation/qcma/releases/download/v#{version.before_comma}/Qcma_#{version.before_comma}#{version.after_comma}.dmg",
+      verified: "github.com/codestation/"
   appcast "https://github.com/codestation/qcma/releases.atom"
   name "Qcma"
   homepage "https://codestation.github.io/qcma/"
