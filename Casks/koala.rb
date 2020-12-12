50e9b8ca7d557dd90b13485d58837d6cd97f330a
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:09 +0100

    koala: switch to `verified` parameter

--- Casks/koala.rb
+++ Casks/koala.rb
@@ -2,8 +2,8 @@ cask "koala" do
   version "2.3.0"
   sha256 "6494408132c8818956a0a0423ed284120506bad0d5dc2349e8ffa7e16c9696bc"
 
-  # github.com/oklai/koala/ was verified as official when first introduced to the cask
-  url "https://github.com/oklai/koala/releases/download/v#{version}/Koala.dmg"
+  url "https://github.com/oklai/koala/releases/download/v#{version}/Koala.dmg",
+      verified: "github.com/oklai/koala/"
   appcast "https://github.com/oklai/koala/releases.atom"
   name "Koala"
   homepage "http://koala-app.com/"
