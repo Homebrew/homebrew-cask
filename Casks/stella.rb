6a968fb345c185261d2b0a7e34f906695a742de2
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:59 +0100

    stella: switch to `verified` parameter

--- Casks/stella.rb
+++ Casks/stella.rb
@@ -2,8 +2,8 @@ cask "stella" do
   version "6.4"
   sha256 "dfd9288cfff0cc152cbc1aaebe36805b0b8ce7d1dd4c3cac9ca1b1f44cc1d540"
 
-  # github.com/stella-emu/stella/ was verified as official when first introduced to the cask
-  url "https://github.com/stella-emu/stella/releases/download/#{version}/Stella-#{version}-macos.dmg"
+  url "https://github.com/stella-emu/stella/releases/download/#{version}/Stella-#{version}-macos.dmg",
+      verified: "github.com/stella-emu/stella/"
   appcast "https://github.com/stella-emu/stella/releases.atom"
   name "Stella"
   desc "Multi-platform Atari 2600 Emulator"
