a4ea1aa0dd7476a6e2c38c2fb22043eff73c848f
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:05 +0100

    timecamp: switch to `verified` parameter

--- Casks/timecamp.rb
+++ Casks/timecamp.rb
@@ -2,8 +2,8 @@ cask "timecamp" do
   version "1.6.2.2"
   sha256 "8d02d1ab4c649bf6acb4e82aeb6b38b5bcb382dc436eca47e68b121ae56c4e4a"
 
-  # timecamp.s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://timecamp.s3.amazonaws.com/downloadsoft/#{version}/TimeCampSetup_macOS.dmg"
+  url "https://timecamp.s3.amazonaws.com/downloadsoft/#{version}/TimeCampSetup_macOS.dmg",
+      verified: "timecamp.s3.amazonaws.com/"
   name "TimeCamp"
   homepage "https://www.timecamp.com/"
 
