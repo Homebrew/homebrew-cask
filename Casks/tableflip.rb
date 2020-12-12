58d4586ccbfe9720ca376bcfd76d58ee7eb1e2fc
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:01 +0100

    tableflip: switch to `verified` parameter

--- Casks/tableflip.rb
+++ Casks/tableflip.rb
@@ -2,8 +2,8 @@ cask "tableflip" do
   version "1.3.0"
   sha256 "b947ad412074ee1b4cd5b5982233aed23f84d623bf3b5b79f315677df323615f"
 
-  # update.christiantietze.de/tableflip/ was verified as official when first introduced to the cask
-  url "https://update.christiantietze.de/tableflip/v#{version.major}/TableFlip-v#{version}.zip"
+  url "https://update.christiantietze.de/tableflip/v#{version.major}/TableFlip-v#{version}.zip",
+      verified: "update.christiantietze.de/tableflip/"
   appcast "https://update.christiantietze.de/tableflip/v#{version.major}/release.xml"
   name "TableFlip"
   desc "App to edit markdown files in place"
