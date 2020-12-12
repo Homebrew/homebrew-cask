643704f384fe85f3f8e65428a1003dc65cd78459
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:03 +0100

    textexpander: switch to `verified` parameter

--- Casks/textexpander.rb
+++ Casks/textexpander.rb
@@ -2,8 +2,8 @@ cask "textexpander" do
   version "6.5.6"
   sha256 "ec2bfd8336d27a2a31c9d42a4ffafac37c54aef852eb6edb54927e4043878432"
 
-  # cdn.textexpander.com/mac/ was verified as official when first introduced to the cask
-  url "https://cdn.textexpander.com/mac/TextExpander_#{version}.zip"
+  url "https://cdn.textexpander.com/mac/TextExpander_#{version}.zip",
+      verified: "cdn.textexpander.com/mac/"
   appcast "https://smilesoftware.com/appcast/TextExpander#{version.major}.xml"
   name "TextExpander"
   desc "Inserts pre-made snippets of text anywhere"
