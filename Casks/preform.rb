b92939f874f8f70412d65ef01698e1f9890652e9
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:40 +0100

    preform: switch to `verified` parameter

--- Casks/preform.rb
+++ Casks/preform.rb
@@ -2,8 +2,8 @@ cask "preform" do
   version "3.11.0,691"
   sha256 "d69198762918892fca78567cb2b7b085981a97d1550c6081fded7452a18a6cb1"
 
-  # s3.amazonaws.com/FormlabsReleases/ was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/FormlabsReleases/Release/#{version.before_comma}/PreForm_#{version.before_comma}_release__build_#{version.after_comma}.dmg"
+  url "https://s3.amazonaws.com/FormlabsReleases/Release/#{version.before_comma}/PreForm_#{version.before_comma}_release__build_#{version.after_comma}.dmg",
+      verified: "s3.amazonaws.com/FormlabsReleases/"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://formlabs.com/download-preform-mac/"
   name "PreForm"
   desc "3D printing setup, management, and monitoring"
