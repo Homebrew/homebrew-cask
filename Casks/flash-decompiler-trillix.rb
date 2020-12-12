7e07be3197703db6d06cc5752859ef9f5cdcf9f8
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:35 +0100

    flash-decompiler-trillix: switch to `verified` parameter

--- Casks/flash-decompiler-trillix.rb
+++ Casks/flash-decompiler-trillix.rb
@@ -2,8 +2,8 @@ cask "flash-decompiler-trillix" do
   version "5.3.1301"
   sha256 :no_check
 
-  # cdn.eltima.com/ was verified as official when first introduced to the cask
-  url "https://cdn.eltima.com/download/flash_decompiler.dmg"
+  url "https://cdn.eltima.com/download/flash_decompiler.dmg",
+      verified: "cdn.eltima.com/"
   appcast "https://cdn.eltima.com/download/fd-mac-update/fd-mac.xml"
   name "Flash Decompiler Trillix"
   homepage "http://www.flash-decompiler.com/mac.html"
