9328614f21fae7ad6649888b29e67fd4f668f123
Markus Reiter <me@reitermark.us>
2020-12-12 05:20:59 +0100

    asciidocfx: switch to `verified` parameter

--- Casks/asciidocfx.rb
+++ Casks/asciidocfx.rb
@@ -2,8 +2,8 @@ cask "asciidocfx" do
   version "1.7.3"
   sha256 "394be4606490939526fe584981c3d00c922258b07bc1a7409322b153b9b5509f"
 
-  # github.com/asciidocfx/AsciidocFX/ was verified as official when first introduced to the cask
-  url "https://github.com/asciidocfx/AsciidocFX/releases/download/v#{version}/AsciidocFX_Mac.dmg"
+  url "https://github.com/asciidocfx/AsciidocFX/releases/download/v#{version}/AsciidocFX_Mac.dmg",
+      verified: "github.com/asciidocfx/AsciidocFX/"
   appcast "https://github.com/asciidocfx/AsciidocFX/releases.atom"
   name "AsciidocFX"
   desc "Asciidoc Editor and Toolchain to build books, documents and slides"
