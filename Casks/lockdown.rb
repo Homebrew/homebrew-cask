f4e00619282fc5f10cecf08ccefbe8aeb43ec5aa
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:14 +0100

    lockdown: switch to `verified` parameter

--- Casks/lockdown.rb
+++ Casks/lockdown.rb
@@ -2,8 +2,8 @@ cask "lockdown" do
   version "1.0.0"
   sha256 "047f377e2a9495361084268c86cc80719e123bd8958d69fe51cd2be0d7ffd764"
 
-  # bitbucket.org/objective-see/ was verified as official when first introduced to the cask
-  url "https://bitbucket.org/objective-see/deploy/downloads/Lockdown_#{version}.zip"
+  url "https://bitbucket.org/objective-see/deploy/downloads/Lockdown_#{version}.zip",
+      verified: "bitbucket.org/objective-see/"
   appcast "https://objective-see.com/products/changelogs/Lockdown.txt"
   name "Lockdown"
   homepage "https://objective-see.com/products/lockdown.html"
