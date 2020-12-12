383a62aeaf331e542e0c1cdb739f68c0f1232bc2
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:22 +0100

    mjolnir: switch to `verified` parameter

--- Casks/mjolnir.rb
+++ Casks/mjolnir.rb
@@ -2,8 +2,8 @@ cask "mjolnir" do
   version "1.0.2"
   sha256 "eb39b9ff76258c3aa7970f70465a7c858290ce798e5b8e411cb5b7d300de16d1"
 
-  # github.com/mjolnirapp/mjolnir/ was verified as official when first introduced to the cask
-  url "https://github.com/mjolnirapp/mjolnir/releases/download/#{version}/Mjolnir.app.zip"
+  url "https://github.com/mjolnirapp/mjolnir/releases/download/#{version}/Mjolnir.app.zip",
+      verified: "github.com/mjolnirapp/mjolnir/"
   appcast "https://github.com/mjolnirapp/mjolnir/releases.atom"
   name "Mjolnir"
   desc "Lightweight automation and productivity app"
