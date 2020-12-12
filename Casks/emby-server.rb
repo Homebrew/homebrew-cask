8d5c41ba3a9dad05e48768610fc6bb310f11617b
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:31 +0100

    emby-server: switch to `verified` parameter

--- Casks/emby-server.rb
+++ Casks/emby-server.rb
@@ -2,8 +2,8 @@ cask "emby-server" do
   version "4.5.2.0"
   sha256 "d37a3c699aa12a8d21e46bafb7bef671db5f29d950d527d14678eedad7529b9c"
 
-  # github.com/MediaBrowser/Emby.Releases/ was verified as official when first introduced to the cask
-  url "https://github.com/MediaBrowser/Emby.Releases/releases/download/#{version}/embyserver-osx-x64-#{version}.zip"
+  url "https://github.com/MediaBrowser/Emby.Releases/releases/download/#{version}/embyserver-osx-x64-#{version}.zip",
+      verified: "github.com/MediaBrowser/Emby.Releases/"
   appcast "https://github.com/MediaBrowser/Emby.Releases/releases.atom"
   name "Emby Server"
   homepage "https://emby.media/"
