1880bbd45bf4f5ae49a61d8db1133fdb433353f3
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:54 +0100

    snipaste: switch to `verified` parameter

--- Casks/snipaste.rb
+++ Casks/snipaste.rb
@@ -2,8 +2,8 @@ cask "snipaste" do
   version "2.5.5-Beta2"
   sha256 "dcb158157eb8885dd996d52621167d585db8f56133e4b3db6467bd49eb5c01ae"
 
-  # bitbucket.org/liule/snipaste/ was verified as official when first introduced to the cask
-  url "https://bitbucket.org/liule/snipaste/downloads/Snipaste-#{version}.dmg"
+  url "https://bitbucket.org/liule/snipaste/downloads/Snipaste-#{version}.dmg",
+      verified: "bitbucket.org/liule/snipaste/"
   appcast "https://www.snipaste.com/download.html"
   name "Snipaste"
   homepage "https://www.snipaste.com/"
