057e190f96561399522cd492acbf2e1d08a6588b
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:16 +0100

    zalo: switch to `verified` parameter

--- Casks/zalo.rb
+++ Casks/zalo.rb
@@ -2,8 +2,8 @@ cask "zalo" do
   version "20.11.2"
   sha256 "9bcf98db09a01b30db03ac4730602ade91a9fe58e673ddd319c499d37f20e39c"
 
-  # res-zaloapp-aka.zdn.vn/mac/ was verified as official when first introduced to the cask
-  url "https://res-zaloapp-aka.zdn.vn/mac/ZaloSetup-#{version}.dmg#"
+  url "https://res-zaloapp-aka.zdn.vn/mac/ZaloSetup-#{version}.dmg#",
+      verified: "res-zaloapp-aka.zdn.vn/mac/"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?user_agent=Macintosh&url=https://zalo.me/download/zalo-pc"
   name "Zalo"
   homepage "https://zalo.me/"
