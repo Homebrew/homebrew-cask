57cdebe43d88471b5be19a9650fc37005e1dc3a1
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:17 +0100

    marathon-infinity: switch to `verified` parameter

--- Casks/marathon-infinity.rb
+++ Casks/marathon-infinity.rb
@@ -2,8 +2,8 @@ cask "marathon-infinity" do
   version "20200904"
   sha256 "ee96ebf857044ffd5df19b53bb0bdf26299179d1e2e2a6d8be506e31e0b67e8f"
 
-  # github.com/Aleph-One-Marathon/alephone/ was verified as official when first introduced to the cask
-  url "https://github.com/Aleph-One-Marathon/alephone/releases/download/release-#{version}/MarathonInfinity-#{version}-Mac.dmg"
+  url "https://github.com/Aleph-One-Marathon/alephone/releases/download/release-#{version}/MarathonInfinity-#{version}-Mac.dmg",
+      verified: "github.com/Aleph-One-Marathon/alephone/"
   appcast "https://github.com/Aleph-One-Marathon/alephone/releases.atom"
   name "Marathon Infinity"
   desc "First-person shooter, third in a trilogy"
