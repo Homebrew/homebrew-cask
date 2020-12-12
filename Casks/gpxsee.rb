515095bfe184a9fe693d7b27771fbe093e191672
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:44 +0100

    gpxsee: switch to `verified` parameter

--- Casks/gpxsee.rb
+++ Casks/gpxsee.rb
@@ -2,8 +2,8 @@ cask "gpxsee" do
   version "7.37"
   sha256 "477c37368d6c094ef804c3040172d8b8902d454f1df907ed07b1fcdadb200467"
 
-  # sourceforge.net/gpxsee/Mac%20OS%20X/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/gpxsee/Mac%20OS%20X/GPXSee-#{version}.dmg"
+  url "https://downloads.sourceforge.net/gpxsee/Mac%20OS%20X/GPXSee-#{version}.dmg",
+      verified: "sourceforge.net/gpxsee/Mac%20OS%20X/"
   appcast "https://sourceforge.net/projects/gpxsee/rss?path=/Mac%20OS%20X"
   name "GPXSee"
   homepage "https://www.gpxsee.org/"
