be6c03e35ae72b21a8d9c2acffbedf429919f97a
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:21 +0100

    db-browser-for-sqlite: switch to `verified` parameter

--- Casks/db-browser-for-sqlite.rb
+++ Casks/db-browser-for-sqlite.rb
@@ -2,8 +2,8 @@ cask "db-browser-for-sqlite" do
   version "3.12.1"
   sha256 "45c8789dc77461299a0aeb9eaae2d089086e76eebae31b45b7a0ee48c1c9e898"
 
-  # github.com/sqlitebrowser/sqlitebrowser/ was verified as official when first introduced to the cask
-  url "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v#{version}/DB.Browser.for.SQLite-#{version}-v2.dmg"
+  url "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v#{version}/DB.Browser.for.SQLite-#{version}-v2.dmg",
+      verified: "github.com/sqlitebrowser/sqlitebrowser/"
   appcast "https://github.com/sqlitebrowser/sqlitebrowser/releases.atom"
   name "DB Browser for SQLite"
   desc "Database browser for SQLite (DB4S) project"
