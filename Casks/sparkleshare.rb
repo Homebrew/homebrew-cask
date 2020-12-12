4a7a4424697ad21f7ba9a61d51e30f111ed159cd
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:56 +0100

    sparkleshare: switch to `verified` parameter

--- Casks/sparkleshare.rb
+++ Casks/sparkleshare.rb
@@ -2,8 +2,8 @@ cask "sparkleshare" do
   version "3.28"
   sha256 "d0e561706b65d379ae947f77a2fc443395b69462d1dd968ac334155c73a38381"
 
-  # github.com/hbons/SparkleShare/ was verified as official when first introduced to the cask
-  url "https://github.com/hbons/SparkleShare/releases/download/#{version}/sparkleshare-mac-#{version}.zip"
+  url "https://github.com/hbons/SparkleShare/releases/download/#{version}/sparkleshare-mac-#{version}.zip",
+      verified: "github.com/hbons/SparkleShare/"
   appcast "https://github.com/hbons/SparkleShare/releases.atom"
   name "SparkleShare"
   desc "Tool to sync with any Git repository instantly"
