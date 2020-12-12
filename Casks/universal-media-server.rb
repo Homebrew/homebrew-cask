d5fccf43dcf92196138c3814dd08a5e896317659
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:08 +0100

    universal-media-server: switch to `verified` parameter

--- Casks/universal-media-server.rb
+++ Casks/universal-media-server.rb
@@ -2,8 +2,8 @@ cask "universal-media-server" do
   version "9.8.2"
   sha256 "75ce25ca5559f1d379b27d0ce1aa0aed6cb720c4d7c53a89dcfc9b6d78b499bb"
 
-  # github.com/UniversalMediaServer/UniversalMediaServer/ was verified as official when first introduced to the cask
-  url "https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/#{version}/UMS-#{version}.dmg"
+  url "https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/#{version}/UMS-#{version}.dmg",
+      verified: "github.com/UniversalMediaServer/UniversalMediaServer/"
   appcast "https://github.com/UniversalMediaServer/UniversalMediaServer/releases.atom"
   name "Universal Media Server"
   desc "Media server supporting DLNA, UPnP and HTTP(S)"
