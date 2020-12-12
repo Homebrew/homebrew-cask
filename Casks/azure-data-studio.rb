1cc200cb115ca185a6b23e99d461cdfd7471d8ea
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:02 +0100

    azure-data-studio: switch to `verified` parameter

--- Casks/azure-data-studio.rb
+++ Casks/azure-data-studio.rb
@@ -2,8 +2,8 @@ cask "azure-data-studio" do
   version "1.25.0,8c6bd8c857592c14afbb589a7d307baec1f5b5ed"
   sha256 "87777adcd5adb1846751e266df18bc05ea1609faacb2b9cf3a5add427435698c"
 
-  # sqlopsbuilds.azureedge.net/stable was verified as official when first introduced to the cask
-  url "https://sqlopsbuilds.azureedge.net/stable/#{version.after_comma}/azuredatastudio-macos-#{version.before_comma}.zip"
+  url "https://sqlopsbuilds.azureedge.net/stable/#{version.after_comma}/azuredatastudio-macos-#{version.before_comma}.zip",
+      verified: "sqlopsbuilds.azureedge.net/stable/"
   appcast "https://github.com/Microsoft/azuredatastudio/releases.atom"
   name "Azure Data Studio"
   homepage "https://docs.microsoft.com/en-us/sql/azure-data-studio/"
