523d1a71db35109babdc0e7d815dc89f4dc96f25
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:14 +0100

    logos: switch to `verified` parameter

--- Casks/logos.rb
+++ Casks/logos.rb
@@ -2,8 +2,8 @@ cask "logos" do
   version "9.1.0.0018"
   sha256 "ad7b53b69d2118de3a2739ca76d91909905d81b2da7d7ef3fa45cc293d59e967"
 
-  # downloads.logoscdn.com/ was verified as official when first introduced to the cask
-  url "https://downloads.logoscdn.com/LBS#{version.major}/Installer/#{version}/LogosMac.dmg"
+  url "https://downloads.logoscdn.com/LBS#{version.major}/Installer/#{version}/LogosMac.dmg",
+      verified: "downloads.logoscdn.com/"
   appcast "https://clientservices.logos.com/update/v1/feed/logos#{version.major}-mac/stable.xml"
   name "Logos Bible Software"
   homepage "https://www.logos.com/"
