269e145be3de016e286da3fb25dc4acd8875567c
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:08 +0100

    brackets: switch to `verified` parameter

--- Casks/brackets.rb
+++ Casks/brackets.rb
@@ -2,8 +2,8 @@ cask "brackets" do
   version "1.14.2"
   sha256 "716be1a75099079aa1a22dedab403c9839155c4b99aab6677d8a24589f7f9a15"
 
-  # github.com/adobe/brackets/ was verified as official when first introduced to the cask
-  url "https://github.com/adobe/brackets/releases/download/release-#{version}/Brackets.Release.#{version}.dmg"
+  url "https://github.com/adobe/brackets/releases/download/release-#{version}/Brackets.Release.#{version}.dmg",
+      verified: "github.com/adobe/brackets/"
   appcast "https://github.com/adobe/brackets/releases.atom"
   name "Brackets"
   desc "Open-source code editor for web-developement"
