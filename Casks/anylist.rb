9ecdc98f184fd3a9e645d62d4ab7d50b20920321
Markus Reiter <me@reitermark.us>
2020-12-12 05:20:57 +0100

    anylist: switch to `verified` parameter

--- Casks/anylist.rb
+++ Casks/anylist.rb
@@ -2,8 +2,8 @@ cask "anylist" do
   version "1.1"
   sha256 "3b319c94865c0b643b1eeb88611ef0d632301a6ec807e070571b8bfbce9c5974"
 
-  # s3.amazonaws.com/files.anylistapp.com/ was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/files.anylistapp.com/mac/AnyList_v#{version.dots_to_underscores}.zip"
+  url "https://s3.amazonaws.com/files.anylistapp.com/mac/AnyList_v#{version.dots_to_underscores}.zip",
+      verified: "s3.amazonaws.com/files.anylistapp.com/"
   appcast "https://s3.amazonaws.com/files.anylistapp.com/mac/appcast.xml"
   name "AnyList"
   homepage "https://www.anylistapp.com/"
