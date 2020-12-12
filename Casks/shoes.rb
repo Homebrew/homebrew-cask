366088118c389c7373fbdb161239b9371be4ed9b
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:52 +0100

    shoes: switch to `verified` parameter

--- Casks/shoes.rb
+++ Casks/shoes.rb
@@ -2,8 +2,8 @@ cask "shoes" do
   version "3.3.7"
   sha256 "9d2d57d8e64a8befc08848939ac0995e2367aadee3aeb24608ebcc8d5c7bb93d"
 
-  # shoes.mvmanila.com/public/shoes/ was verified as official when first introduced to the cask
-  url "https://shoes.mvmanila.com/public/shoes/shoes-#{version}-osx-10.10.tgz"
+  url "https://shoes.mvmanila.com/public/shoes/shoes-#{version}-osx-10.10.tgz",
+      verified: "shoes.mvmanila.com/public/shoes/"
   appcast "http://shoesrb.com/downloads/"
   name "Shoes"
   homepage "http://shoesrb.com/"
