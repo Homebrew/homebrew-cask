36a5c7b9829f21fcf3496cd8c665264275ebd09a
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:10 +0100

    visit: switch to `verified` parameter

--- Casks/visit.rb
+++ Casks/visit.rb
@@ -3,20 +3,20 @@ cask "visit" do
     version "3.1.1"
     sha256 "4213daed23de17ee8bcfba779a96cce3ef92d3075ae666f7aeaffa824d484924"
 
-    # github.com/visit-dav/visit/ was verified as official when first introduced to the cask
-    url "https://github.com/visit-dav/visit/releases/download/v#{version}/visit#{version}.darwin-x86_64-10.13.dmg"
+    url "https://github.com/visit-dav/visit/releases/download/v#{version}/visit#{version}.darwin-x86_64-10.13.dmg",
+        verified: "github.com/visit-dav/visit/"
   elsif MacOS.version <= :mojave
     version "3.1.4"
     sha256 "e8636d4920dfc155c3c5236f8cab9675214e090bdb00eb6bdcb96cee634c2bc9"
 
-    # github.com/visit-dav/visit/ was verified as official when first introduced to the cask
-    url "https://github.com/visit-dav/visit/releases/download/v#{version}/visit#{version}.darwin-x86_64-10_14.dmg"
+    url "https://github.com/visit-dav/visit/releases/download/v#{version}/visit#{version}.darwin-x86_64-10_14.dmg",
+        verified: "github.com/visit-dav/visit/"
   else
     version "3.1.4"
     sha256 "a75da6c27f38dbd5d859562da5777e810c71b85d6b06df47cef585a282330b06"
 
-    # github.com/visit-dav/visit/ was verified as official when first introduced to the cask
-    url "https://github.com/visit-dav/visit/releases/download/v#{version}/visit#{version}.darwin-x86_64-10_15.dmg"
+    url "https://github.com/visit-dav/visit/releases/download/v#{version}/visit#{version}.darwin-x86_64-10_15.dmg",
+        verified: "github.com/visit-dav/visit/"
   end
 
   appcast "https://github.com/visit-dav/visit/releases.atom"
