5ecfc03687cd8eb66f639ac40ca78e97dbe614ac
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:28 +0100

    notable: switch to `verified` parameter

--- Casks/notable.rb
+++ Casks/notable.rb
@@ -2,8 +2,8 @@ cask "notable" do
   version "1.8.4"
   sha256 "5ca00f5135e769165c683dc0174ea95bd9052ca751a0704b1a1a20dd297b4e3d"
 
-  # github.com/notable/notable/ was verified as official when first introduced to the cask
-  url "https://github.com/notable/notable/releases/download/v#{version}/Notable-#{version}.dmg"
+  url "https://github.com/notable/notable/releases/download/v#{version}/Notable-#{version}.dmg",
+      verified: "github.com/notable/notable/"
   appcast "https://github.com/notable/notable/releases.atom"
   name "Notable"
   desc "Markdown-based note-taking app that doesn't suck"
