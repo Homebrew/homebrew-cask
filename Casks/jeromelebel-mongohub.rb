d6ed2aa2a7aa8694251839de1352f81e354af5b7
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:54 +0100

    jeromelebel-mongohub: switch to `verified` parameter

--- Casks/jeromelebel-mongohub.rb
+++ Casks/jeromelebel-mongohub.rb
@@ -2,8 +2,8 @@ cask "jeromelebel-mongohub" do
   version "3.1.4"
   sha256 :no_check
 
-  # mongohub.s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://mongohub.s3.amazonaws.com/MongoHub.zip"
+  url "https://mongohub.s3.amazonaws.com/MongoHub.zip",
+      verified: "mongohub.s3.amazonaws.com/"
   appcast "https://github.com/jeromelebel/MongoHub-Mac/releases.atom"
   name "MongoHub"
   desc "Native MongoDB client"
