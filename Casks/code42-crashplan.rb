49d8a4fb29943fe9c11eecce33566d20ad08967d
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:16 +0100

    code42-crashplan: switch to `verified` parameter

--- Casks/code42-crashplan.rb
+++ Casks/code42-crashplan.rb
@@ -2,8 +2,8 @@ cask "code42-crashplan" do
   version "7.0.3"
   sha256 "d0d00edc7855a2c1a5282a138c2d398102d8f5f5d59f148787b0fda9d307b9e3"
 
-  # download.code42.com/ was verified as official when first introduced to the cask
-  url "https://download.code42.com/installs/mac/install/Code42CrashPlan/Code42CrashPlan_#{version}_Mac.dmg"
+  url "https://download.code42.com/installs/mac/install/Code42CrashPlan/Code42CrashPlan_#{version}_Mac.dmg",
+      verified: "download.code42.com/"
   appcast "https://support.code42.com/Release_Notes"
   name "CrashPlan"
   homepage "https://www.crashplan.com/"
