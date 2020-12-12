c36ce254b2a1f35f926d0178c82dd29a63fa2e61
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:28 +0100

    dungeon-crawl-stone-soup-console: switch to `verified` parameter

--- Casks/dungeon-crawl-stone-soup-console.rb
+++ Casks/dungeon-crawl-stone-soup-console.rb
@@ -2,8 +2,8 @@ cask "dungeon-crawl-stone-soup-console" do
   version "0.25.1"
   sha256 "3fb8da37afbb276f86dc46b2b28f010c614133a77576a7349222b491fcd946ea"
 
-  # github.com/crawl/crawl/releases was verified as official when first introduced to the cask
-  url "https://github.com/crawl/crawl/releases/download/#{version}/dcss-#{version}-macos-console.zip"
+  url "https://github.com/crawl/crawl/releases/download/#{version}/dcss-#{version}-macos-console.zip",
+      verified: "github.com/crawl/crawl/releases/"
   appcast "https://github.com/crawl/crawl/releases.atom"
   name "Dungeon Crawl Stone Soup"
   desc "Game of dungeon exploration, combat and magic"
