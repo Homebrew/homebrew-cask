611004746a17e776c9ca759fb867ec0a6bbc420b
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:34 +0100

    fiji: switch to `verified` parameter

--- Casks/fiji.rb
+++ Casks/fiji.rb
@@ -2,8 +2,8 @@ cask "fiji" do
   version :latest
   sha256 :no_check
 
-  # downloads.imagej.net/fiji/ was verified as official when first introduced to the cask
-  url "https://downloads.imagej.net/fiji/latest/fiji-macosx.zip"
+  url "https://downloads.imagej.net/fiji/latest/fiji-macosx.zip",
+      verified: "downloads.imagej.net/fiji/"
   name "Fiji"
   desc "Open-source image processing package"
   homepage "https://fiji.sc/"
