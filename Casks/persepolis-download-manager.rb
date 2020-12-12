34dda0008b6ae88a1ffea109970833a8ec49768d
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:37 +0100

    persepolis-download-manager: switch to `verified` parameter

--- Casks/persepolis-download-manager.rb
+++ Casks/persepolis-download-manager.rb
@@ -2,8 +2,8 @@ cask "persepolis-download-manager" do
   version "3.2.0"
   sha256 "39514a158201483a713c918d744b823f63ae39142aa2024713c21174385400a3"
 
-  # github.com/persepolisdm/persepolis/ was verified as official when first introduced to the cask
-  url "https://github.com/persepolisdm/persepolis/releases/download/#{version}/persepolis_#{version}_mac.zip"
+  url "https://github.com/persepolisdm/persepolis/releases/download/#{version}/persepolis_#{version}_mac.zip",
+      verified: "github.com/persepolisdm/persepolis/"
   appcast "https://github.com/persepolisdm/persepolis/releases.atom"
   name "Persepolis"
   desc "GUI for aria2"
