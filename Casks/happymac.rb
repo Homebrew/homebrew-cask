130d8e166d46042c94dd2a852098d7e8c7fe05d9
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:46 +0100

    happymac: switch to `verified` parameter

--- Casks/happymac.rb
+++ Casks/happymac.rb
@@ -2,8 +2,8 @@ cask "happymac" do
   version "0.1.0"
   sha256 :no_check
 
-  # chrislaffra.com/ was verified as official when first introduced to the cask
-  url "https://chrislaffra.com/happymac/happymac.dmg"
+  url "https://chrislaffra.com/happymac/happymac.dmg",
+      verified: "chrislaffra.com/"
   name "HappyMac"
   desc "Watches, suspends and resumes background processes that slow down your system"
   homepage "https://www.happymac.app/"
