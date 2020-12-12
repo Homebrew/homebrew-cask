520cb297f3ef40ecfee479aca9e4918616ebd016
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:09 +0100

    klokki: switch to `verified` parameter

--- Casks/klokki.rb
+++ Casks/klokki.rb
@@ -2,8 +2,8 @@ cask "klokki" do
   version "1.3.3,62"
   sha256 :no_check
 
-  # storage.yandexcloud.net/klokki/ was verified as official when first introduced to the cask
-  url "https://storage.yandexcloud.net/klokki/Klokki.dmg"
+  url "https://storage.yandexcloud.net/klokki/Klokki.dmg",
+      verified: "storage.yandexcloud.net/klokki/"
   appcast "https://storage.yandexcloud.net/klokki/appcast.xml"
   name "Klokki"
   desc "Automatic time-tracking solution or freelancers and makers"
