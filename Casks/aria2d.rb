e9b4b74d3725a7c73ea685807f6a745083ce1f0e
Markus Reiter <me@reitermark.us>
2020-12-12 05:20:58 +0100

    aria2d: switch to `verified` parameter

--- Casks/aria2d.rb
+++ Casks/aria2d.rb
@@ -2,8 +2,8 @@ cask "aria2d" do
   version "1.3.6"
   sha256 "1840ce5b25571925cfec67635e3e48ce38306b856b509861f2ecfdfd0470cd7b"
 
-  # githubusercontent.com/xjbeta/ was verified as official when first introduced to the cask
-  url "https://raw.githubusercontent.com/xjbeta/AppUpdaterAppcasts/master/Aria2D/Aria2D%20#{version}.dmg"
+  url "https://raw.githubusercontent.com/xjbeta/AppUpdaterAppcasts/master/Aria2D/Aria2D%20#{version}.dmg",
+      verified: "githubusercontent.com/xjbeta/"
   appcast "https://raw.githubusercontent.com/xjbeta/AppUpdaterAppcasts/master/Aria2D/Appcast.xml"
   name "Aria2D"
   desc "Aria2 GUI"
