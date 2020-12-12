3a6b1bb6de1b557d81fcce7ca133f831bad5e68b
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:26 +0100

    nally: switch to `verified` parameter

--- Casks/nally.rb
+++ Casks/nally.rb
@@ -2,8 +2,8 @@ cask "nally" do
   version "1.4.9"
   sha256 "5b7835f8842aa33d0f40eebddda2686a39f2106dfacd0bde04c3f0911da625d1"
 
-  # yllan.github.io/nally/ was verified as official when first introduced to the cask
-  url "https://yllan.github.io/nally/download/Nally-#{version}.app.zip"
+  url "https://yllan.github.io/nally/download/Nally-#{version}.app.zip",
+      verified: "yllan.github.io/nally/"
   appcast "https://yllan.org/app/Nally/"
   name "Nally"
   desc "Beautiful telnet/ssh client optimized for Mandarin BBS"
