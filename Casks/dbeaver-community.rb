e41c4744f38f80c4908536acf32c547e59049aa0
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:21 +0100

    dbeaver-community: switch to `verified` parameter

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -2,8 +2,8 @@ cask "dbeaver-community" do
   version "7.3.0"
   sha256 "86c95939171b41f3eb93741ed2c9d78abeaa283a8aa83a140f52afabbdd67069"
 
-  # github.com/dbeaver/dbeaver/ was verified as official when first introduced to the cask
-  url "https://github.com/dbeaver/dbeaver/releases/download/#{version}/dbeaver-ce-#{version}-macos.dmg"
+  url "https://github.com/dbeaver/dbeaver/releases/download/#{version}/dbeaver-ce-#{version}-macos.dmg",
+      verified: "github.com/dbeaver/dbeaver/"
   appcast "https://github.com/dbeaver/dbeaver/releases.atom"
   name "DBeaver Community Edition"
   desc "Free universal database tool and SQL client"
