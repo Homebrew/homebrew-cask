4100f34e206b6a11bad1e70c44436acce10fce16
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:41 +0100

    proclaim: switch to `verified` parameter

--- Casks/proclaim.rb
+++ Casks/proclaim.rb
@@ -2,8 +2,8 @@ cask "proclaim" do
   version "3.0.0.0048"
   sha256 "22e790a0a777d6cdbf65d2820f890bb1d15e4f6859a33b5ec9a65996a95462da"
 
-  # logoscdn.com/Proclaim/ was verified as official when first introduced to the cask
-  url "https://downloads.logoscdn.com/Proclaim/Installer/#{version}/Proclaim.dmg"
+  url "https://downloads.logoscdn.com/Proclaim/Installer/#{version}/Proclaim.dmg",
+      verified: "logoscdn.com/Proclaim/"
   appcast "http://downloads.proclaimonline.com/mac/appcast.xml"
   name "Faithlife Proclaim"
   homepage "https://proclaim.faithlife.com/"
