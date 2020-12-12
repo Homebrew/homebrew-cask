afaf7f74e1b942e6f75d621ad777eeadd30f62ec
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:53 +0100

    jabt-flow: switch to `verified` parameter

--- Casks/jabt-flow.rb
+++ Casks/jabt-flow.rb
@@ -3,8 +3,8 @@ cask "jabt-flow" do
   sha256 "b1b990aefc8528e17d112a0f00e1c5625b04f6a068020973f850f907e36ba9e5"
 
   # Download page is at https://createwithflow.com/releases/
-  # createwithflow-updates.s3-us-west-2.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://createwithflow-updates.s3-us-west-2.amazonaws.com/Flow#{version.no_dots}_.zip"
+  url "https://createwithflow-updates.s3-us-west-2.amazonaws.com/Flow#{version.no_dots}_.zip",
+      verified: "createwithflow-updates.s3-us-west-2.amazonaws.com/"
   appcast "https://createwithflow-updates.s3-us-west-2.amazonaws.com/appcast.xml"
   name "Flow"
   homepage "https://createwithflow.com/"
