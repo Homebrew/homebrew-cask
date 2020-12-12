c8ce24b449bc97ebd3b9f120a69276c40bc11ee3
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:13 +0100

    windocd: switch to `verified` parameter

--- Casks/windocd.rb
+++ Casks/windocd.rb
@@ -2,8 +2,8 @@ cask "windocd" do
   version "1.8"
   sha256 :no_check
 
-  # download.moapp.software/ was verified as official when first introduced to the cask
-  url "https://download.moapp.software/windOCD.zip"
+  url "https://download.moapp.software/windOCD.zip",
+      verified: "download.moapp.software/"
   appcast "https://sparkle.moapp.software/windocd.xml"
   name "windOCD"
   homepage "https://windocd.myownapp.com/"
