e385c646a0e78caa96fac04cd45a5e61d3d8da1e
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:40 +0100

    portfolioperformance: switch to `verified` parameter

--- Casks/portfolioperformance.rb
+++ Casks/portfolioperformance.rb
@@ -2,8 +2,8 @@ cask "portfolioperformance" do
   version "0.49.3"
   sha256 "e7ff90326aacd29d20cc41b47a641abc7577708867e2ae90fcfd905f5aecac16"
 
-  # github.com/buchen/portfolio/ was verified as official when first introduced to the cask
-  url "https://github.com/buchen/portfolio/releases/download/#{version}/PortfolioPerformance-#{version}.dmg"
+  url "https://github.com/buchen/portfolio/releases/download/#{version}/PortfolioPerformance-#{version}.dmg",
+      verified: "github.com/buchen/portfolio/"
   appcast "https://github.com/buchen/portfolio/releases.atom"
   name "Portfolio Performance"
   desc "Calculate the overall performance of an investment portfolio"
