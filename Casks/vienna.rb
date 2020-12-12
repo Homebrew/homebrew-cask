81ea3a7e7a24fb563673afea3096514df42d3b1d
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:10 +0100

    vienna: switch to `verified` parameter

--- Casks/vienna.rb
+++ Casks/vienna.rb
@@ -2,8 +2,8 @@ cask "vienna" do
   version "3.6.2"
   sha256 "c43487411c8f39d29e82f0cc618a04d00a7147afa593dfe4cd45de2be5280bc6"
 
-  # bintray.com/viennarss/ was verified as official when first introduced to the cask
-  url "https://dl.bintray.com/viennarss/vienna-rss/Vienna#{version}.tar.gz"
+  url "https://dl.bintray.com/viennarss/vienna-rss/Vienna#{version}.tar.gz",
+      verified: "bintray.com/viennarss/"
   appcast "https://viennarss.github.io/sparkle-files/changelog.xml"
   name "Vienna"
   desc "RSS and Atom reader"
