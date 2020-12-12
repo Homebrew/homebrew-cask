582747716e99e7e504dd8f386a1232da691a0048
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:46 +0100

    handbrake: switch to `verified` parameter

--- Casks/handbrake.rb
+++ Casks/handbrake.rb
@@ -2,8 +2,8 @@ cask "handbrake" do
   version "1.3.3"
   sha256 "500cb924de7a3cc3a95cac4e69f303fc0623d3524fe59ac4ee4160ff43d56af6"
 
-  # github.com/HandBrake/HandBrake/ was verified as official when first introduced to the cask
-  url "https://github.com/HandBrake/HandBrake/releases/download/#{version}/HandBrake-#{version}.dmg"
+  url "https://github.com/HandBrake/HandBrake/releases/download/#{version}/HandBrake-#{version}.dmg",
+      verified: "github.com/HandBrake/HandBrake/"
   appcast "https://github.com/HandBrake/HandBrake/releases.atom"
   name "HandBrake"
   desc "Open-source video transcoder"
