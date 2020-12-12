d005bc4566aebf2a49b8a98ad182ed27ed4fe6e8
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:08 +0100

    ultrastardeluxe: switch to `verified` parameter

--- Casks/ultrastardeluxe.rb
+++ Casks/ultrastardeluxe.rb
@@ -2,8 +2,8 @@ cask "ultrastardeluxe" do
   version "1.1"
   sha256 "18e36476ec6994b1dd54e6272c08ceb86b10abf4984a18d834003e696153ca9b"
 
-  # downloads.sourceforge.net/ultrastardx/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/ultrastardx/UltraStarDeluxe-#{version}.dmg"
+  url "https://downloads.sourceforge.net/ultrastardx/UltraStarDeluxe-#{version}.dmg",
+      verified: "downloads.sourceforge.net/ultrastardx/"
   appcast "https://sourceforge.net/projects/ultrastardx/rss"
   name "UltraStar Deluxe"
   homepage "https://ultrastardx.sourceforge.io/"
