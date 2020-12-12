3eaafe1bea0b71ac5156efbf3d45dfaeab499af5
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:18 +0100

    cord: switch to `verified` parameter

--- Casks/cord.rb
+++ Casks/cord.rb
@@ -2,8 +2,8 @@ cask "cord" do
   version "0.5.7"
   sha256 "8f505b12b94167100b3b8b44ed3cee32ffcc94b73dc44fe0ecc896151f114100"
 
-  # downloads.sourceforge.net/cord/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/cord/cord/#{version}/CoRD_#{version}.zip"
+  url "https://downloads.sourceforge.net/cord/cord/#{version}/CoRD_#{version}.zip",
+      verified: "downloads.sourceforge.net/cord/"
   appcast "https://cord.sourceforge.io/sparkle.xml"
   name "CoRD"
   desc "Remote desktop client"
