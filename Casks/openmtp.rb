1a980f6542364b49286c3a88eca13f43a6a41c12
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:32 +0100

    openmtp: switch to `verified` parameter

--- Casks/openmtp.rb
+++ Casks/openmtp.rb
@@ -2,8 +2,8 @@ cask "openmtp" do
   version "2.4.4"
   sha256 "25423e374b0c50763670a5368ecea19c01789906d31f14d02a1501d541ed31de"
 
-  # github.com/ganeshrvel/openmtp/ was verified as official when first introduced to the cask
-  url "https://github.com/ganeshrvel/openmtp/releases/download/v#{version}/OpenMTP-#{version}-mac.zip"
+  url "https://github.com/ganeshrvel/openmtp/releases/download/v#{version}/OpenMTP-#{version}-mac.zip",
+      verified: "github.com/ganeshrvel/openmtp/"
   appcast "https://github.com/ganeshrvel/openmtp/releases.atom"
   name "OpenMTP"
   homepage "https://ganeshrvel.github.io/openmtp"
