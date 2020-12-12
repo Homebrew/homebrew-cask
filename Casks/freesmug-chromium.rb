d2d355c4cb3cebec87df15a615fa4075c5ee9001
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:39 +0100

    freesmug-chromium: switch to `verified` parameter

--- Casks/freesmug-chromium.rb
+++ Casks/freesmug-chromium.rb
@@ -2,8 +2,8 @@ cask "freesmug-chromium" do
   version "85.0.4183.83"
   sha256 "03676baff11bad7927459c5e183e251a1b4a4245aabd34aaa8f5adc02a5e1cab"
 
-  # sourceforge.net/osxportableapps/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/osxportableapps/Chromium_OSX_#{version}.dmg"
+  url "https://downloads.sourceforge.net/osxportableapps/Chromium_OSX_#{version}.dmg",
+      verified: "sourceforge.net/osxportableapps/"
   appcast "https://sourceforge.net/projects/osxportableapps/rss?path=/Chromium"
   name "Chromium"
   homepage "http://www.freesmug.org/chromium"
