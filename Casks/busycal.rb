28a13740ca0d32b0bd804e88089856ca779b235d
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:10 +0100

    busycal: switch to `verified` parameter

--- Casks/busycal.rb
+++ Casks/busycal.rb
@@ -2,8 +2,8 @@ cask "busycal" do
   version "3.11.1-2020-11-15-05-54"
   sha256 "ae039923ed462308264376ab69bedbcbe1dfe5d860799f494d9a4dfc0737e891"
 
-  # 7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com was verified as official when first introduced to the cask
-  url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bcl-#{version}.zip"
+  url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bcl-#{version}.zip",
+      verified: "7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.busymac.com/download/BusyCal.zip"
   name "BusyCal"
   desc "Calendar software focusing on flexibility and reliability"
