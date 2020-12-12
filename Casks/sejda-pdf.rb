2711bfb1650ba762b9ada7a36243ed0b35be2a85
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:51 +0100

    sejda-pdf: switch to `verified` parameter

--- Casks/sejda-pdf.rb
+++ Casks/sejda-pdf.rb
@@ -2,8 +2,8 @@ cask "sejda-pdf" do
   version "7.1.5"
   sha256 "a7cae7e79242cc2b2c04df03da8f2f2079a2316f82db27863d2f398c7e998450"
 
-  # sejda-cdn.com/ was verified as official when first introduced to the cask
-  url "https://sejda-cdn.com/downloads/sejda-desktop_#{version}.dmg"
+  url "https://sejda-cdn.com/downloads/sejda-desktop_#{version}.dmg",
+      verified: "sejda-cdn.com/"
   appcast "https://www.sejda.com/desktop"
   name "Sejda PDF Desktop"
   homepage "https://www.sejda.com/desktop"
