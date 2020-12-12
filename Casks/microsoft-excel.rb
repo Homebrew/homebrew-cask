5073e8fac20c94002ea34c4b520d82f805e485ca
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:20 +0100

    microsoft-excel: switch to `verified` parameter

--- Casks/microsoft-excel.rb
+++ Casks/microsoft-excel.rb
@@ -2,8 +2,8 @@ cask "microsoft-excel" do
   version "16.43.20110804"
   sha256 "2711a1b8864f7474458086b4b0a56673fee0097d2049f276788c50e004c47d72"
 
-  # officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/ was verified as official when first introduced to the cask
-  url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Excel_#{version}_Installer.pkg"
+  url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Excel_#{version}_Installer.pkg",
+      verified: "officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/"
   appcast "https://docs.microsoft.com/en-us/officeupdates/update-history-office-for-mac"
   name "Microsoft Excel"
   homepage "https://products.office.com/en-US/excel"
