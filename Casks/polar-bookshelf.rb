0e7a2c1ff75fe0ce1402a8ef1e9f001310c57029
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:40 +0100

    polar-bookshelf: switch to `verified` parameter

--- Casks/polar-bookshelf.rb
+++ Casks/polar-bookshelf.rb
@@ -2,8 +2,8 @@ cask "polar-bookshelf" do
   version "2.0.53"
   sha256 "dedca2902093cc273168d1cd00198bafe97395edab457db91aeec3fff73dda7c"
 
-  # github.com/burtonator/polar-bookshelf/ was verified as official when first introduced to the cask
-  url "https://github.com/burtonator/polar-bookshelf/releases/download/v#{version}/Polar-#{version}.dmg"
+  url "https://github.com/burtonator/polar-bookshelf/releases/download/v#{version}/Polar-#{version}.dmg",
+      verified: "github.com/burtonator/polar-bookshelf/"
   appcast "https://github.com/burtonator/polar-bookshelf/releases.atom"
   name "Polar Bookshelf"
   desc "Integrated reading environment"
