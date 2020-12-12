73bea292a95085cb07e4dff3f027e1b11759ad51
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:52 +0100

    ios-app-signer: switch to `verified` parameter

--- Casks/ios-app-signer.rb
+++ Casks/ios-app-signer.rb
@@ -2,8 +2,8 @@ cask "ios-app-signer" do
   version "1.13.1"
   sha256 "baf6826427706b0b3f685ffb3beced86cf348b74d60e1f81d75c4c3e8a779f47"
 
-  # github.com/DanTheMan827/ios-app-signer/ was verified as official when first introduced to the cask
-  url "https://github.com/DanTheMan827/ios-app-signer/releases/download/#{version}/iOS.App.Signer.app.zip"
+  url "https://github.com/DanTheMan827/ios-app-signer/releases/download/#{version}/iOS.App.Signer.app.zip",
+      verified: "github.com/DanTheMan827/ios-app-signer/"
   appcast "https://github.com/DanTheMan827/ios-app-signer/releases.atom"
   name "iOS App Signer"
   homepage "https://dantheman827.github.io/ios-app-signer/"
