4b6324f5fc2b2ac7dec94ac5d5113577cbb2b6c9
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:30 +0100

    electric-sheep: switch to `verified` parameter

--- Casks/electric-sheep.rb
+++ Casks/electric-sheep.rb
@@ -2,8 +2,8 @@ cask "electric-sheep" do
   version "3.0.2"
   sha256 "db689d0aeac776a3f189205efa32331c0c00b0bea608408b2e855557c56f5937"
 
-  # d100rc88eim93q.cloudfront.net/ was verified as official when first introduced to the cask
-  url "https://d100rc88eim93q.cloudfront.net/electricsheep-#{version}.dmg"
+  url "https://d100rc88eim93q.cloudfront.net/electricsheep-#{version}.dmg",
+      verified: "d100rc88eim93q.cloudfront.net/"
   appcast "https://electricsheep.org/#/download"
   name "Electric Sheep"
   desc "Collaborative abstract artwork software"
