cd349780d043e128d077b91f4d77360db72ff29a
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:40 +0100

    garagebuy: switch to `verified` parameter

--- Casks/garagebuy.rb
+++ Casks/garagebuy.rb
@@ -2,8 +2,8 @@ cask "garagebuy" do
   version "3.5.3"
   sha256 "da6d5de29c3593336ea62a0d7a10a134213bb659b2deee833ee1922e188ed3ea"
 
-  # iwascoding.de/ was verified as official when first introduced to the cask
-  url "https://www.iwascoding.de/downloads/GarageBuy_#{version}.dmg"
+  url "https://www.iwascoding.de/downloads/GarageBuy_#{version}.dmg",
+      verified: "iwascoding.de/"
   appcast "https://www.iwascoding.com/GarageBuy/Downloads.html"
   name "GarageBuy"
   desc "App to assist with finding, tracking, and purchasing items on eBay"
