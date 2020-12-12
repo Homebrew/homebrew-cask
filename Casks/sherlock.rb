e6fb8c327ce62b6d55a09996b3e7e2bfd5e9fd55
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:51 +0100

    sherlock: switch to `verified` parameter

--- Casks/sherlock.rb
+++ Casks/sherlock.rb
@@ -2,8 +2,8 @@ cask "sherlock" do
   version "1.12.0"
   sha256 :no_check
 
-  # dl.devmate.com/io.inspiredcode.Sherlock/ was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/io.inspiredcode.Sherlock/Sherlock.dmg"
+  url "https://dl.devmate.com/io.inspiredcode.Sherlock/Sherlock.dmg",
+      verified: "dl.devmate.com/io.inspiredcode.Sherlock/"
   appcast "http://sparkle.sherlock.inspiredcode.io"
   name "Sherlock"
   homepage "https://sherlock.inspiredcode.io/"
