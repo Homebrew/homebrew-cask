89b5fe12147a600ccadb471af88a388e51a4483d
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:28 +0100

    duplicacy-web-edition: switch to `verified` parameter

--- Casks/duplicacy-web-edition.rb
+++ Casks/duplicacy-web-edition.rb
@@ -2,8 +2,8 @@ cask "duplicacy-web-edition" do
   version "1.4.1"
   sha256 "0b1fcf4a02338711321396d2e054f79a15e201278848fa37877c7166263308cd"
 
-  # acrosync.com/duplicacy-web/ was verified as official when first introduced to the cask
-  url "https://acrosync.com/duplicacy-web/duplicacy_web_osx_x64_#{version}.dmg"
+  url "https://acrosync.com/duplicacy-web/duplicacy_web_osx_x64_#{version}.dmg",
+      verified: "acrosync.com/duplicacy-web/"
   appcast "https://duplicacy.com/download.html"
   name "Duplicacy Web Edition"
   homepage "https://duplicacy.com/"
