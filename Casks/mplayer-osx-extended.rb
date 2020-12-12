874f9caaf5adfff2971ac2299a04eaad9bd891f7
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:24 +0100

    mplayer-osx-extended: switch to `verified` parameter

--- Casks/mplayer-osx-extended.rb
+++ Casks/mplayer-osx-extended.rb
@@ -2,8 +2,8 @@ cask "mplayer-osx-extended" do
   version "rev16"
   sha256 "a52eae9a685a4d9854a5f989c4eb1e94b3f97b8c25a0e36ad4cdbc610fdf1023"
 
-  # github.com/sttz/MPlayer-OSX-Extended/ was verified as official when first introduced to the cask
-  url "https://github.com/sttz/MPlayer-OSX-Extended/releases/download/#{version}/MPlayer-OSX-Extended_#{version}.zip"
+  url "https://github.com/sttz/MPlayer-OSX-Extended/releases/download/#{version}/MPlayer-OSX-Extended_#{version}.zip",
+      verified: "github.com/sttz/MPlayer-OSX-Extended/"
   appcast "https://github.com/sttz/MPlayer-OSX-Extended/releases.atom"
   name "MPlayer OSX Extended"
   desc "Video player thats uses MPlayer as backend"
