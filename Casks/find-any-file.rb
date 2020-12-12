d90a50020206ab654d63b240ff1220edb9d2f496
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:35 +0100

    find-any-file: switch to `verified` parameter

--- Casks/find-any-file.rb
+++ Casks/find-any-file.rb
@@ -2,8 +2,8 @@ cask "find-any-file" do
   version "2.2.1"
   sha256 "7e3d2c53afc02aa39e3da928e03d8bea278cbe4f7bcc6db07094a627430b6d76"
 
-  # s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/files.tempel.org/FindAnyFile_#{version}.zip"
+  url "https://s3.amazonaws.com/files.tempel.org/FindAnyFile_#{version}.zip",
+      verified: "s3.amazonaws.com/"
   appcast "https://findanyfile.app/appcast2.php"
   name "Find Any File"
   desc "File finder"
