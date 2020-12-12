aea3cc471d79896cef05c383537dc9c00ab8452c
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:38 +0100

    free-ruler: switch to `verified` parameter

--- Casks/free-ruler.rb
+++ Casks/free-ruler.rb
@@ -2,8 +2,8 @@ cask "free-ruler" do
   version "2.0.3"
   sha256 "aa428907c113e249a6baedb329e788b10d6db67d9f9117c3986780d2c592da9d"
 
-  # github.com/pascalpp/FreeRuler/ was verified as official when first introduced to the cask
-  url "https://github.com/pascalpp/FreeRuler/releases/download/v#{version}/free-ruler-#{version}.zip"
+  url "https://github.com/pascalpp/FreeRuler/releases/download/v#{version}/free-ruler-#{version}.zip",
+      verified: "github.com/pascalpp/FreeRuler/"
   appcast "https://github.com/pascalpp/FreeRuler/releases.atom"
   name "Free Ruler"
   desc "Horizontal and vertical rulers"
