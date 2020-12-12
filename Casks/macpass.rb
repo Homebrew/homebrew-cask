e3bfe7dce6df88cd3691b83c0dfb2ed54557c766
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:16 +0100

    macpass: switch to `verified` parameter

--- Casks/macpass.rb
+++ Casks/macpass.rb
@@ -2,8 +2,8 @@ cask "macpass" do
   version "0.7.12"
   sha256 "acde7f51fd0b6529553e1e4ad4a7cbc137096bd355987bb5f4fb869f578f33be"
 
-  # github.com/MacPass/MacPass/ was verified as official when first introduced to the cask
-  url "https://github.com/MacPass/MacPass/releases/download/#{version}/MacPass-#{version}.zip"
+  url "https://github.com/MacPass/MacPass/releases/download/#{version}/MacPass-#{version}.zip",
+      verified: "github.com/MacPass/MacPass/"
   appcast "https://github.com/MacPass/MacPass/releases.atom"
   name "MacPass"
   desc "Open-source, KeePass-client and password manager"
