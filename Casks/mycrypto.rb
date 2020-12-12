335624f1127861cccc40330c1a54988d67a39995
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:26 +0100

    mycrypto: switch to `verified` parameter

--- Casks/mycrypto.rb
+++ Casks/mycrypto.rb
@@ -2,8 +2,8 @@ cask "mycrypto" do
   version "1.7.13"
   sha256 "de7f1133f9ea2c8aeb1c1f58c8ae0c864b79a956dc99904debe72fa5f707382c"
 
-  # github.com/MyCryptoHQ/MyCrypto/ was verified as official when first introduced to the cask
-  url "https://github.com/MyCryptoHQ/MyCrypto/releases/download/#{version}/mac_#{version}_MyCrypto.dmg"
+  url "https://github.com/MyCryptoHQ/MyCrypto/releases/download/#{version}/mac_#{version}_MyCrypto.dmg",
+      verified: "github.com/MyCryptoHQ/MyCrypto/"
   appcast "https://github.com/MyCryptoHQ/MyCrypto/releases.atom"
   name "MyCrypto"
   homepage "https://mycrypto.com/"
