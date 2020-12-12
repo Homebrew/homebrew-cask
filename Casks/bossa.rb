e04f949add04b6e8aa51ba5bf14160c0f414cd24
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:07 +0100

    bossa: switch to `verified` parameter

--- Casks/bossa.rb
+++ Casks/bossa.rb
@@ -2,8 +2,8 @@ cask "bossa" do
   version "1.9.1"
   sha256 "ef325525d2ce1fa6078732e5aa0c16cf78114bd3cf867016c4b948022d1cb31f"
 
-  # github.com/shumatech/BOSSA/ was verified as official when first introduced to the cask
-  url "https://github.com/shumatech/BOSSA/releases/download/#{version}/bossa-#{version}.dmg"
+  url "https://github.com/shumatech/BOSSA/releases/download/#{version}/bossa-#{version}.dmg",
+      verified: "github.com/shumatech/BOSSA/"
   appcast "https://github.com/shumatech/BOSSA/releases.atom"
   name "bossa"
   homepage "https://www.shumatech.com/web/products/bossa"
