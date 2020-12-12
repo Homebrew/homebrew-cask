eec87471200ce7b14ab3c5261c4717bf896297ac
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:11 +0100

    captain: switch to `verified` parameter

--- Casks/captain.rb
+++ Casks/captain.rb
@@ -2,8 +2,8 @@ cask "captain" do
   version "9.0.0"
   sha256 :no_check
 
-  # dropbox.com/s/pkayzy14unr4r0a/ was verified as official when first introduced to the cask
-  url "https://www.dropbox.com/s/pkayzy14unr4r0a/Captain.dmg?dl=1"
+  url "https://www.dropbox.com/s/pkayzy14unr4r0a/Captain.dmg?dl=1",
+      verified: "dropbox.com/s/pkayzy14unr4r0a/"
   name "Captain"
   homepage "https://getcaptain.co/"
 
