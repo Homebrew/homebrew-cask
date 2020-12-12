d9db79c3aa0bb524e5c963c9a5eec41af3e41411
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:43 +0100

    qownnotes: switch to `verified` parameter

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -2,8 +2,8 @@ cask "qownnotes" do
   version "20.12.4"
   sha256 "42c9bdeeda66d0386f84a8000bc64406604a0e7b9261782921916a3b0938ffa4"
 
-  # github.com/pbek/QOwnNotes/ was verified as official when first introduced to the cask
-  url "https://github.com/pbek/QOwnNotes/releases/download/v#{version}/QOwnNotes.dmg"
+  url "https://github.com/pbek/QOwnNotes/releases/download/v#{version}/QOwnNotes.dmg",
+      verified: "github.com/pbek/QOwnNotes/"
   appcast "https://github.com/pbek/QOwnNotes/releases.atom"
   name "QOwnNotes"
   desc "Plain-text file notepad and todo-list manager"
