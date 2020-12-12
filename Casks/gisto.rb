f5836db7ca3e50d604167b7066b0da623937cebc
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:42 +0100

    gisto: switch to `verified` parameter

--- Casks/gisto.rb
+++ Casks/gisto.rb
@@ -2,8 +2,8 @@ cask "gisto" do
   version "1.13.4"
   sha256 "40b8cb8654231af8550d0df76d39a8e69eb1e2fc909faba68882f2fe3576800e"
 
-  # github.com/Gisto/Gisto/ was verified as official when first introduced to the cask
-  url "https://github.com/Gisto/Gisto/releases/download/v#{version}/Gisto-#{version}.dmg"
+  url "https://github.com/Gisto/Gisto/releases/download/v#{version}/Gisto-#{version}.dmg",
+      verified: "github.com/Gisto/Gisto/"
   appcast "https://github.com/Gisto/Gisto/releases.atom"
   name "Gisto"
   desc "Snippets management desktop application with (team) sharing options"
