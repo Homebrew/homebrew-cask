f2016b955736a1ca4e39550ac285366f5b0da112
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:24 +0100

    mqttfx: switch to `verified` parameter

--- Casks/mqttfx.rb
+++ Casks/mqttfx.rb
@@ -2,8 +2,8 @@ cask "mqttfx" do
   version "1.7.1"
   sha256 "544b6ac0afefb80b5a56c2a6f2411f999862fb8f0f20502c912f28a9d1aa3b4a"
 
-  # jensd.de/apps/mqttfx/ was verified as official when first introduced to the cask
-  url "https://www.jensd.de/apps/mqttfx/#{version}/mqttfx-#{version}-macos.dmg"
+  url "https://www.jensd.de/apps/mqttfx/#{version}/mqttfx-#{version}-macos.dmg",
+      verified: "jensd.de/apps/mqttfx/"
   appcast "https://www.jensd.de/apps/mqttfx/appcast.xml"
   name "MQTT.fx"
   homepage "http://www.mqttfx.org/"
