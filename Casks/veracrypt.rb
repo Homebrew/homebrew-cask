f3ac821e76625d0a78c8a6cbb3f09d10b416f86e
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:09 +0100

    veracrypt: switch to `verified` parameter

--- Casks/veracrypt.rb
+++ Casks/veracrypt.rb
@@ -2,8 +2,8 @@ cask "veracrypt" do
   version "1.24-Update8"
   sha256 "9edeae9fbf7f1b2eb91ce55ff82481bfd4c099e85c3c88cb6d6dd91b460a3a6b"
 
-  # launchpad.net/veracrypt/trunk/ was verified as official when first introduced to the cask
-  url "https://launchpad.net/veracrypt/trunk/#{version.downcase}/+download/VeraCrypt_#{version}.dmg"
+  url "https://launchpad.net/veracrypt/trunk/#{version.downcase}/+download/VeraCrypt_#{version}.dmg",
+      verified: "launchpad.net/veracrypt/trunk/"
   appcast "https://github.com/veracrypt/VeraCrypt/releases.atom"
   name "VeraCrypt"
   desc "Disk encryption software focusing on security based on TrueCrypt"
