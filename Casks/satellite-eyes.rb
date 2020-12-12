346bd8aa5ee3575994e313521fd16c69f31a5ad2
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:49 +0100

    satellite-eyes: switch to `verified` parameter

--- Casks/satellite-eyes.rb
+++ Casks/satellite-eyes.rb
@@ -2,8 +2,8 @@ cask "satellite-eyes" do
   version "1.5.0"
   sha256 "c3646cd60618824f4aad9f4f744e5e6e3db7d96ceb11d72147a583bf02f74336"
 
-  # satellite-eyes.s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://satellite-eyes.s3.amazonaws.com/satellite-eyes-#{version}.zip"
+  url "https://satellite-eyes.s3.amazonaws.com/satellite-eyes-#{version}.zip",
+      verified: "satellite-eyes.s3.amazonaws.com/"
   appcast "https://satellite-eyes.s3.amazonaws.com/appcast.xml"
   name "Satellite Eyes"
   homepage "https://satelliteeyes.tomtaylor.co.uk/"
