435a8568550035519e9e8bb745cca6cc997d358c
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:02 +0100

    backlog: switch to `verified` parameter

--- Casks/backlog.rb
+++ Casks/backlog.rb
@@ -2,8 +2,8 @@ cask "backlog" do
   version "1.8.0"
   sha256 "de3747cee4d6b63ccc102c25ef9ca3ab8f21d78d2fbbc8cf34725035484e1fbd"
 
-  # github.com/czytelny/backlog/ was verified as official when first introduced to the cask
-  url "https://github.com/czytelny/backlog/releases/download/v#{version}/Backlog-darwin-x64.zip"
+  url "https://github.com/czytelny/backlog/releases/download/v#{version}/Backlog-darwin-x64.zip",
+      verified: "github.com/czytelny/backlog/"
   appcast "https://github.com/czytelny/backlog/releases.atom"
   name "Backlog"
   homepage "http://www.backlog.cloud/"
