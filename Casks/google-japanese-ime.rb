6bb14e5850c7c9d0f9fc1e79d1a98e19365faf95
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:44 +0100

    google-japanese-ime: switch to `verified` parameter

--- Casks/google-japanese-ime.rb
+++ Casks/google-japanese-ime.rb
@@ -2,8 +2,8 @@ cask "google-japanese-ime" do
   version :latest
   sha256 :no_check
 
-  # dl.google.com/japanese-ime/ was verified as official when first introduced to the cask
-  url "https://dl.google.com/japanese-ime/latest/GoogleJapaneseInput.dmg"
+  url "https://dl.google.com/japanese-ime/latest/GoogleJapaneseInput.dmg",
+      verified: "dl.google.com/japanese-ime/"
   name "Google Japanese Input Method Editor"
   homepage "https://www.google.co.jp/ime/"
 
