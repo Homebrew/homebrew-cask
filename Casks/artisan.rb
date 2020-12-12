efe9fd412cbebe41f51147ef7f866bbdba4ef9f6
Markus Reiter <me@reitermark.us>
2020-12-12 05:20:59 +0100

    artisan: switch to `verified` parameter

--- Casks/artisan.rb
+++ Casks/artisan.rb
@@ -2,8 +2,8 @@ cask "artisan" do
   version "2.4.2"
   sha256 "50c0e65b40fd68f5e0b1fce5948507abad8bde90f681ed01d63ba1bc7d60872c"
 
-  # github.com/artisan-roaster-scope/artisan/ was verified as official when first introduced to the cask
-  url "https://github.com/artisan-roaster-scope/artisan/releases/download/v#{version}/artisan-mac-#{version}.dmg"
+  url "https://github.com/artisan-roaster-scope/artisan/releases/download/v#{version}/artisan-mac-#{version}.dmg",
+      verified: "github.com/artisan-roaster-scope/artisan/"
   appcast "https://github.com/artisan-roaster-scope/artisan/releases.atom"
   name "Artisan"
   desc "Visual scope for coffee roasters"
