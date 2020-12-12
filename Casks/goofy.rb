e92de0633f071b76b677115d4e4e17bc96232b4d
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:43 +0100

    goofy: switch to `verified` parameter

--- Casks/goofy.rb
+++ Casks/goofy.rb
@@ -2,8 +2,8 @@ cask "goofy" do
   version "3.5.4"
   sha256 "a41bd37f7ba1f0cd88cd5241ffcd22ea9087320b9cfa525298565aea9fba121b"
 
-  # github.com/danielbuechele/goofy/ was verified as official when first introduced to the cask
-  url "https://github.com/danielbuechele/goofy/releases/download/v#{version}/Goofy-#{version}.dmg"
+  url "https://github.com/danielbuechele/goofy/releases/download/v#{version}/Goofy-#{version}.dmg",
+      verified: "github.com/danielbuechele/goofy/"
   appcast "https://github.com/danielbuechele/goofy/releases.atom"
   name "Goofy"
   desc "Desktop client for Facebook Messenger"
