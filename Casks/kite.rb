704a74b1af5f24dfa9bf5097d52f8e0babb7b3c9
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:08 +0100

    kite: switch to `verified` parameter

--- Casks/kite.rb
+++ Casks/kite.rb
@@ -2,8 +2,8 @@ cask "kite" do
   version "0.20201203.0"
   sha256 "74de74c8925f6e3d9f0db395e0cfdfe7534c9db3d5c09137cc4d19c42f90670a"
 
-  # draqv87tt43s0.cloudfront.net/ was verified as official when first introduced to the cask
-  url "https://draqv87tt43s0.cloudfront.net/mac/#{version}/Kite.dmg"
+  url "https://draqv87tt43s0.cloudfront.net/mac/#{version}/Kite.dmg",
+      verified: "draqv87tt43s0.cloudfront.net/"
   appcast "https://release.kite.com/appcast.xml"
   name "Kite"
   homepage "https://kite.com/"
