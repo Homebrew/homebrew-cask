eaf3ff68d59ba33342efbe2891839c33b824128e
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:13 +0100

    line-bot-designer: switch to `verified` parameter

--- Casks/line-bot-designer.rb
+++ Casks/line-bot-designer.rb
@@ -2,8 +2,8 @@ cask "line-bot-designer" do
   version "1.3.4"
   sha256 "d4b80baaa73d0947f6b6be2c2530877edc67d4bbe208a5cc1ad0c509b8f505b1"
 
-  # d.line-scdn.net/ was verified as official when first introduced to the cask
-  url "https://d.line-scdn.net/r/devcenter/bot-designer/LINE%20Bot%20Designer-#{version}.dmg"
+  url "https://d.line-scdn.net/r/devcenter/bot-designer/LINE%20Bot%20Designer-#{version}.dmg",
+      verified: "d.line-scdn.net/"
   appcast "https://developers.line.biz/en/bot-designer/download/"
   name "LINE Bot Designer"
   homepage "https://developers.line.biz/en/"
