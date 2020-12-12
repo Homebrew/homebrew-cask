ea20dc778d23bfb25a5bc7e5bd53d07f04018980
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:48 +0100

    home-assistant: switch to `verified` parameter

--- Casks/home-assistant.rb
+++ Casks/home-assistant.rb
@@ -2,8 +2,8 @@ cask "home-assistant" do
   version "2020.7,10"
   sha256 "ab45c66f672124a8e6e4af177ed447931b0d1e9bd3551774cfc7c98942ad1b8d"
 
-  # github.com/home-assistant/iOS/ was verified as official when first introduced to the cask
-  url "https://github.com/home-assistant/iOS/releases/download/release%2F#{version.before_comma}%2F#{version.after_comma}/home-assistant-mac.zip"
+  url "https://github.com/home-assistant/iOS/releases/download/release%2F#{version.before_comma}%2F#{version.after_comma}/home-assistant-mac.zip",
+      verified: "github.com/home-assistant/iOS/"
   appcast "https://github.com/home-assistant/iOS/releases.atom"
   name "Home Assistant"
   desc "Companion app for Home Assistant home automation software"
