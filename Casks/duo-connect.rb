8cd20ede7d17297617d2e51c24c9ac6cf4dbb5ba
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:28 +0100

    duo-connect: switch to `verified` parameter

--- Casks/duo-connect.rb
+++ Casks/duo-connect.rb
@@ -2,8 +2,8 @@ cask "duo-connect" do
   version "1.1.1"
   sha256 "ba54b65d5a4f8c0a79702f6d370e76fb14cb50148bc3f83b8ff29a4ac9997e78"
 
-  # dl.duosecurity.com/ was verified as official when first introduced to the cask
-  url "https://dl.duosecurity.com/DuoConnect-#{version}.pkg"
+  url "https://dl.duosecurity.com/DuoConnect-#{version}.pkg",
+      verified: "dl.duosecurity.com/"
   appcast "https://duo.com/docs/checksums#duoconnect-for-macos"
   name "DuoConnect"
   homepage "https://guide.duo.com/duoconnect"
