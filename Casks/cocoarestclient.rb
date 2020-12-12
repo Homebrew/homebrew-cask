c83bf542b9303e2ee3d7b1241ccd80ac40826af0
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:15 +0100

    cocoarestclient: switch to `verified` parameter

--- Casks/cocoarestclient.rb
+++ Casks/cocoarestclient.rb
@@ -2,8 +2,8 @@ cask "cocoarestclient" do
   version "1.4.5"
   sha256 "dccc638d7b8e9f793055cb1c4fc49da9f0f35e24e89f3559e53810150c77a6b1"
 
-  # github.com/mmattozzi/cocoa-rest-client/ was verified as official when first introduced to the cask
-  url "https://github.com/mmattozzi/cocoa-rest-client/releases/download/#{version}/CocoaRestClient-#{version}.dmg"
+  url "https://github.com/mmattozzi/cocoa-rest-client/releases/download/#{version}/CocoaRestClient-#{version}.dmg",
+      verified: "github.com/mmattozzi/cocoa-rest-client/"
   appcast "https://github.com/mmattozzi/cocoa-rest-client/releases.atom"
   name "CocoaRestClient"
   desc "App for testing HTTP/REST endpoints"
