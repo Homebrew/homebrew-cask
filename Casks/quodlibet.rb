9259b95dce8de10cf0ed02bc8ba7fe17d9aacffb
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:44 +0100

    quodlibet: switch to `verified` parameter

--- Casks/quodlibet.rb
+++ Casks/quodlibet.rb
@@ -2,8 +2,8 @@ cask "quodlibet" do
   version "4.3.0"
   sha256 "858afc8630d0df974466c7d4123ca7c46d0fa81c24ef3c25a5dc780f3334ef08"
 
-  # github.com/quodlibet/quodlibet/ was verified as official when first introduced to the cask
-  url "https://github.com/quodlibet/quodlibet/releases/download/release-#{version}/QuodLibet-#{version}.dmg"
+  url "https://github.com/quodlibet/quodlibet/releases/download/release-#{version}/QuodLibet-#{version}.dmg",
+      verified: "github.com/quodlibet/quodlibet/"
   appcast "https://github.com/quodlibet/quodlibet/releases.atom"
   name "Quod Libet"
   desc "Music player and music library manager"
