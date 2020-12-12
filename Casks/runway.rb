a62240592ce8cd22a04b83da55b5464a3afb8f07
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:49 +0100

    runway: switch to `verified` parameter

--- Casks/runway.rb
+++ Casks/runway.rb
@@ -2,8 +2,8 @@ cask "runway" do
   version "0.16.3"
   sha256 "c5fc36cc071078aa8eef6057a5a7a53b6f90ad15654a50b61dcd53f810c1ab57"
 
-  # runway-releases.s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://runway-releases.s3.amazonaws.com/Runway-#{version}.dmg"
+  url "https://runway-releases.s3.amazonaws.com/Runway-#{version}.dmg",
+      verified: "runway-releases.s3.amazonaws.com/"
   appcast "https://runway-releases.s3.amazonaws.com/latest-mac.yml"
   name "Runway"
   homepage "https://runwayml.com/"
