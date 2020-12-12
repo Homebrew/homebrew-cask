daaf9fcff6fa1a7530f0437fdbdee85c06e0bed0
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:48 +0100

    rotki: switch to `verified` parameter

--- Casks/rotki.rb
+++ Casks/rotki.rb
@@ -2,8 +2,8 @@ cask "rotki" do
   version "1.9.1"
   sha256 "3568a2f4a41c4ecf4785e4f4aff17e15704e91ae859497edd85e0ac81138339f"
 
-  # github.com/rotki/rotki/ was verified as official when first introduced to the cask
-  url "https://github.com/rotki/rotki/releases/download/v#{version}/rotki-darwin-v#{version}.dmg"
+  url "https://github.com/rotki/rotki/releases/download/v#{version}/rotki-darwin-v#{version}.dmg",
+      verified: "github.com/rotki/rotki/"
   appcast "https://github.com/rotki/rotki/releases.atom"
   name "Rotki"
   desc "Portfolio tracking and accounting tool"
