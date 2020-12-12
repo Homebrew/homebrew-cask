5fe8f2b7abe11d4b6b586aec24d65d1a5db4bfb2
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:42 +0100

    qbittorrent: switch to `verified` parameter

--- Casks/qbittorrent.rb
+++ Casks/qbittorrent.rb
@@ -2,8 +2,8 @@ cask "qbittorrent" do
   version "4.3.1"
   sha256 "e5060067cfbe32443c00e9f5ddb700968a3a12d2e0486dc3442243d33350204c"
 
-  # sourceforge.net/qbittorrent/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/qbittorrent/qbittorrent-mac/qbittorrent-#{version}/qbittorrent-#{version}.dmg"
+  url "https://downloads.sourceforge.net/qbittorrent/qbittorrent-mac/qbittorrent-#{version}/qbittorrent-#{version}.dmg",
+      verified: "sourceforge.net/qbittorrent/"
   appcast "https://sourceforge.net/projects/qbittorrent/rss?path=/qbittorrent-mac"
   name "qBittorrent"
   desc "Peer to peer Bitorrent client"
