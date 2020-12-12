dd8f663e9264163c694b5384d24b7ca441d6260c
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:49 +0100

    sabaki: switch to `verified` parameter

--- Casks/sabaki.rb
+++ Casks/sabaki.rb
@@ -2,8 +2,8 @@ cask "sabaki" do
   version "0.51.1"
   sha256 "61a4843666fdc6d21d262f47bf5b2b5b13a9f41a2d89667eaab4f13f6728729d"
 
-  # github.com/SabakiHQ/Sabaki/ was verified as official when first introduced to the cask
-  url "https://github.com/SabakiHQ/Sabaki/releases/download/v#{version}/sabaki-v#{version}-mac-x64.7z"
+  url "https://github.com/SabakiHQ/Sabaki/releases/download/v#{version}/sabaki-v#{version}-mac-x64.7z",
+      verified: "github.com/SabakiHQ/Sabaki/"
   appcast "https://github.com/SabakiHQ/Sabaki/releases.atom"
   name "Sabaki"
   homepage "https://sabaki.yichuanshen.de/"
