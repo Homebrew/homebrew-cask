ff9da4673e38e98602b2450c7b625a7f216a7aaf
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:52 +0100

    ipepresenter: switch to `verified` parameter

--- Casks/ipepresenter.rb
+++ Casks/ipepresenter.rb
@@ -2,8 +2,8 @@ cask "ipepresenter" do
   version "7.2.21"
   sha256 "ae849bbf19bfcac635226029ef198797ff042e02ecd6cfd7994d58bf56d674ff"
 
-  # bintray.com/otfried/ was verified as official when first introduced to the cask
-  url "https://dl.bintray.com/otfried/generic/ipe/#{version.major_minor}/ipepresenter-#{version}-mac.dmg"
+  url "https://dl.bintray.com/otfried/generic/ipe/#{version.major_minor}/ipepresenter-#{version}-mac.dmg",
+      verified: "bintray.com/otfried/"
   appcast "http://ipepresenter.otfried.org/"
   name "IpePresenter"
   desc "Make presentations from PDFs"
