db08321434658884440a2c71722091bd9becf48c
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:52 +0100

    iriunwebcam: switch to `verified` parameter

--- Casks/iriunwebcam.rb
+++ Casks/iriunwebcam.rb
@@ -2,8 +2,8 @@ cask "iriunwebcam" do
   version "2.4.2"
   sha256 "047a0568dc3f96ffac7dedd520bab6f71da153a7409561cc87c6872167a589eb"
 
-  # 1758658189.rsc.cdn77.org/ was verified as official when first introduced to the cask
-  url "https://1758658189.rsc.cdn77.org/IriunWebcam-#{version}.pkg"
+  url "https://1758658189.rsc.cdn77.org/IriunWebcam-#{version}.pkg",
+      verified: "1758658189.rsc.cdn77.org/"
   appcast "https://iriun.com/"
   name "Iriun"
   homepage "https://iriun.com/"
