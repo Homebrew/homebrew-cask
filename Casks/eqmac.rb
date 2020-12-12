901386cc4dbb319bda3974a4fdbc949ecfc06531
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:32 +0100

    eqmac: switch to `verified` parameter

--- Casks/eqmac.rb
+++ Casks/eqmac.rb
@@ -2,8 +2,8 @@ cask "eqmac" do
   version "0.3.7"
   sha256 "3285aa75f8735e89d8bd2ffa5be1d859b6cc7b9d9f0dc5864142aaf3a0ffa470"
 
-  # github.com/bitgapp/eqMac/ was verified as official when first introduced to the cask
-  url "https://github.com/bitgapp/eqMac/releases/download/v#{version}/eqMac.pkg"
+  url "https://github.com/bitgapp/eqMac/releases/download/v#{version}/eqMac.pkg",
+      verified: "github.com/bitgapp/eqMac/"
   appcast "https://github.com/bitgapp/eqMac/releases.atom"
   name "eqMac"
   desc "System-wide audio equalizer"
