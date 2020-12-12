de7979598e02197093adfdf7c774d0bd9fbd98e0
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:32 +0100

    espresso: switch to `verified` parameter

--- Casks/espresso.rb
+++ Casks/espresso.rb
@@ -2,8 +2,8 @@ cask "espresso" do
   version "5.4.1"
   sha256 "c28b93b6728869d47d2c277839079c1a1bbd88497d9ae0649800a50f51776072"
 
-  # downloads.kangacode.com/ was verified as official when first introduced to the cask
-  url "https://downloads.kangacode.com/Espresso/Espresso_#{version}.zip"
+  url "https://downloads.kangacode.com/Espresso/Espresso_#{version}.zip",
+      verified: "downloads.kangacode.com/"
   appcast "https://espressoapp.com/updates/"
   name "Espresso"
   desc "Website editor focusing on flair and efficiency"
