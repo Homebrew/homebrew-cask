8257c01a41bebe98169c477f93068ffd0c81e6ac
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:32 +0100

    opennx: switch to `verified` parameter

--- Casks/opennx.rb
+++ Casks/opennx.rb
@@ -2,8 +2,8 @@ cask "opennx" do
   version "0.16.0.729"
   sha256 "65dde1a3504a17ac58ed2a7178536347d829ee8c27cc90bebdae3e98c36fc6c6"
 
-  # sourceforge.net/opennx/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/opennx/OpenNX-#{version}.dmg"
+  url "https://downloads.sourceforge.net/opennx/OpenNX-#{version}.dmg",
+      verified: "sourceforge.net/opennx/"
   appcast "https://sourceforge.net/projects/opennx/rss"
   name "OpenNX"
   homepage "http://opennx.net/"
