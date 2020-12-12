041ef3c023b93f0644578ae83681ad6166dc29c8
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:27 +0100

    niftyman: switch to `verified` parameter

--- Casks/niftyman.rb
+++ Casks/niftyman.rb
@@ -2,8 +2,8 @@ cask "niftyman" do
   version "1.0.4"
   sha256 "f725330b9d875a30f1987d9396f26c82e548012b85eb6d3fe8d32448582f9cb1"
 
-  # notion-menubar-updates.s3-us-west-2.amazonaws.com was verified as official when first introduced to the cask
-  url "https://notion-menubar-updates.s3-us-west-2.amazonaws.com/Niftyman-#{version}.dmg"
+  url "https://notion-menubar-updates.s3-us-west-2.amazonaws.com/Niftyman-#{version}.dmg",
+      verified: "notion-menubar-updates.s3-us-west-2.amazonaws.com/"
   appcast "https://notion-menubar-updates.s3.amazonaws.com/latest-mac.yml"
   name "Niftyman"
   desc "Access the Notion tool from the menu bar"
