31101dd471a99d128509d361cb1b3a0f6c436948
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:38 +0100

    plexamp: switch to `verified` parameter

--- Casks/plexamp.rb
+++ Casks/plexamp.rb
@@ -2,8 +2,8 @@ cask "plexamp" do
   version "3.4.0"
   sha256 "63b8e77102b867ceaa8cb1617cd177a9ad4957e7a8d7fb93ccac4f7714c25b4f"
 
-  # plexamp.plex.tv/ was verified as official when first introduced to the cask
-  url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}.dmg"
+  url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}.dmg",
+      verified: "plexamp.plex.tv/"
   appcast "https://plexamp.plex.tv/plexamp.plex.tv/desktop/latest-mac.yml"
   name "Plexamp"
   desc "Music player focusing on visuals"
