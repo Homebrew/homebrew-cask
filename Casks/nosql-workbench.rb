6de986e4a786bc9598ec28e5263cc2754627e8b6
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:28 +0100

    nosql-workbench: switch to `verified` parameter

--- Casks/nosql-workbench.rb
+++ Casks/nosql-workbench.rb
@@ -2,8 +2,8 @@ cask "nosql-workbench" do
   version "2.1.0"
   sha256 "c3aa1b994f825a3929c0f474e1cfeb092263b9ea3fc735409835e9e8d238df65"
 
-  # s3.amazonaws.com/nosql-workbench/ was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/nosql-workbench/NoSQL%20Workbench-mac-#{version}.dmg"
+  url "https://s3.amazonaws.com/nosql-workbench/NoSQL%20Workbench-mac-#{version}.dmg",
+      verified: "s3.amazonaws.com/nosql-workbench/"
   appcast "https://nosql-workbench.s3.amazonaws.com/latest-mac.yml"
   name "NoSQL Workbench"
   desc "Client-side GUI application for modern database development and operations"
