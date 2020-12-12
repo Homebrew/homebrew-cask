ce9a816f86e7bd5ef619b1599e5ce697af9ee895
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:33 +0100

    extraterm: switch to `verified` parameter

--- Casks/extraterm.rb
+++ Casks/extraterm.rb
@@ -2,8 +2,8 @@ cask "extraterm" do
   version "0.55.2"
   sha256 "607fe541e451cb60ffc2ab3a8950fb6c898ec7202d10d8a09afaf90b169a338f"
 
-  # github.com/sedwards2009/extraterm/ was verified as official when first introduced to the cask
-  url "https://github.com/sedwards2009/extraterm/releases/download/v#{version}/extraterm-#{version}-darwin-x64.zip"
+  url "https://github.com/sedwards2009/extraterm/releases/download/v#{version}/extraterm-#{version}-darwin-x64.zip",
+      verified: "github.com/sedwards2009/extraterm/"
   appcast "https://github.com/sedwards2009/extraterm/releases.atom"
   name "extraterm"
   desc "Swiss army chainsaw of terminal emulators"
