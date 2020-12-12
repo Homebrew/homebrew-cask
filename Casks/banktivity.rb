04411bcb317d0e501b587ec88094454c723e42ee
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:02 +0100

    banktivity: switch to `verified` parameter

--- Casks/banktivity.rb
+++ Casks/banktivity.rb
@@ -2,8 +2,8 @@ cask "banktivity" do
   version "8.0.5"
   sha256 "c2ab4fd9124c921604e9f867828c5fab8fbc3e267ebec6e5ff79c1a8fbc07f41"
 
-  # iggsoft.com/ was verified as official when first introduced to the cask
-  url "https://www.iggsoft.com/banktivity/Banktivity#{version}.dmg"
+  url "https://www.iggsoft.com/banktivity/Banktivity#{version}.dmg",
+      verified: "iggsoft.com/"
   appcast "https://www.iggsoft.com/banktivity/banktivity-versions-feed.json"
   name "Banktivity"
   desc "App to manage bank accounts in one place"
