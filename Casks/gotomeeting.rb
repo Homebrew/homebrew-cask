690910942ea5ead9d13e5c2820b01946bb38ed4a
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:44 +0100

    gotomeeting: switch to `verified` parameter

--- Casks/gotomeeting.rb
+++ Casks/gotomeeting.rb
@@ -2,8 +2,8 @@ cask "gotomeeting" do
   version "10.14.0,18962"
   sha256 "ffcc6a3000eea1841bd79ba67f59036ed8392097f58a2bd438f37239c2c2e7df"
 
-  # builds.cdn.getgo.com/builds/g2m/ was verified as official when first introduced to the cask
-  url "https://builds.cdn.getgo.com/builds/g2m/#{version.after_comma}/GoToMeeting.dmg"
+  url "https://builds.cdn.getgo.com/builds/g2m/#{version.after_comma}/GoToMeeting.dmg",
+      verified: "builds.cdn.getgo.com/builds/g2m/"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://link.gotomeeting.com/latest-dmg",
           must_contain: version.after_comma
   name "GoToMeeting"
