b0ab9049ae64b225eee64b07b99e971651b069dc
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:32 +0100

    epub-to-pdf: switch to `verified` parameter

--- Casks/epub-to-pdf.rb
+++ Casks/epub-to-pdf.rb
@@ -2,8 +2,8 @@ cask "epub-to-pdf" do
   version "3.1"
   sha256 "dcfc59d57f756802e844614b7dae43bca67284ec85fe6b909f244e41f20987b3"
 
-  # googleapis.com/google-code-archive-downloads/v2/code.google.com/epub-2-pdf/ was verified as official when first introduced to the cask
-  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/epub-2-pdf/e2p-#{version.major}.dmg"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/epub-2-pdf/e2p-#{version.major}.dmg",
+      verified: "googleapis.com/google-code-archive-downloads/v2/code.google.com/epub-2-pdf/"
   name "epub-2-pdf"
   homepage "https://code.google.com/archive/p/epub-2-pdf"
 
