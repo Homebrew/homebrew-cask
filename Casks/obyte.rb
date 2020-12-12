8ace7dbfbb6e560b4b4982afb2711df9015d5052
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:29 +0100

    obyte: switch to `verified` parameter

--- Casks/obyte.rb
+++ Casks/obyte.rb
@@ -2,8 +2,8 @@ cask "obyte" do
   version "3.3.1"
   sha256 "339bfa9bdf96c299ae39f0cdbda5c2cbf0c7134f80deb720cd4dbad5b8668861"
 
-  # github.com/byteball/obyte-gui-wallet/ was verified as official when first introduced to the cask
-  url "https://github.com/byteball/obyte-gui-wallet/releases/download/v#{version}/Obyte-osx64.dmg"
+  url "https://github.com/byteball/obyte-gui-wallet/releases/download/v#{version}/Obyte-osx64.dmg",
+      verified: "github.com/byteball/obyte-gui-wallet/"
   appcast "https://github.com/byteball/obyte-gui-wallet/releases.atom"
   name "Obyte"
   desc "Smart payments made simple"
