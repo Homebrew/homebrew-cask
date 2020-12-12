58d5917a57890250e2cb8e6fc2a9e9f375e10c81
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:19 +0100

    metaz: switch to `verified` parameter

--- Casks/metaz.rb
+++ Casks/metaz.rb
@@ -2,8 +2,8 @@ cask "metaz" do
   version "1.0.beta-113"
   sha256 "5f0d43794ac76c025ad1c0f6419dfea58c8c696d7e2782060d2aa12554316588"
 
-  # github.com/griff/metaz/ was verified as official when first introduced to the cask
-  url "https://github.com/griff/metaz/releases/download/v#{version}/MetaZ-#{version}.zip"
+  url "https://github.com/griff/metaz/releases/download/v#{version}/MetaZ-#{version}.zip",
+      verified: "github.com/griff/metaz/"
   appcast "https://github.com/griff/metaz/releases.atom"
   name "MetaZ"
   desc "Mp4 meta-data editor"
