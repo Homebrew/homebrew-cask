5c4841d943763dc0bda9fcfd3b16446222ac3766
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:03 +0100

    bathyscaphe: switch to `verified` parameter

--- Casks/bathyscaphe.rb
+++ Casks/bathyscaphe.rb
@@ -2,8 +2,8 @@ cask "bathyscaphe" do
   version "310-v1089"
   sha256 "32f7758d2898fd8cccd1b506c8d78bbff6733640e392eba67c95a6626377a603"
 
-  # bitbucket.org/bathyscaphe/public/downloads/ was verified as official when first introduced to the cask
-  url "https://bitbucket.org/bathyscaphe/public/downloads/BathyScaphe-#{version}.dmg"
+  url "https://bitbucket.org/bathyscaphe/public/downloads/BathyScaphe-#{version}.dmg",
+      verified: "bitbucket.org/bathyscaphe/public/downloads/"
   appcast "https://bathyscaphe.bitbucket.io/appcast.xml"
   name "BathyScaphe"
   homepage "https://bathyscaphe.bitbucket.io/"
