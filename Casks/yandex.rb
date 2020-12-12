1bf198833d09f43447340dbd8434aca3da78af8b
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:15 +0100

    yandex: switch to `verified` parameter

--- Casks/yandex.rb
+++ Casks/yandex.rb
@@ -2,8 +2,8 @@ cask "yandex" do
   version "20.11.0.918"
   sha256 :no_check
 
-  # yandex.net/ was verified as official when first introduced to the cask
-  url "https://download.cdn.yandex.net/browser/yandex/ru/Yandex.dmg"
+  url "https://download.cdn.yandex.net/browser/yandex/ru/Yandex.dmg",
+      verified: "yandex.net/"
   name "Yandex.Browser"
   desc "Web browser"
   homepage "https://browser.yandex.ru/desktop/"
