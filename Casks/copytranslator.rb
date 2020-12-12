a1507723f4566af0f942bd949a031f6af9c87990
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:18 +0100

    copytranslator: switch to `verified` parameter

--- Casks/copytranslator.rb
+++ Casks/copytranslator.rb
@@ -2,8 +2,8 @@ cask "copytranslator" do
   version "9.0.2"
   sha256 "5da337e06e1ac6325627f99a0662042b1d97689613685cc046441d41d6d1f075"
 
-  # github.com/CopyTranslator/CopyTranslator/ was verified as official when first introduced to the cask
-  url "https://github.com/CopyTranslator/CopyTranslator/releases/download/v#{version}/copytranslator-#{version}.dmg"
+  url "https://github.com/CopyTranslator/CopyTranslator/releases/download/v#{version}/copytranslator-#{version}.dmg",
+      verified: "github.com/CopyTranslator/CopyTranslator/"
   appcast "https://github.com/CopyTranslator/CopyTranslator/releases.atom"
   name "CopyTranslator"
   desc "Tool that translates text in real-time while copying"
