128ffe46402b565fd89b20455aaa1c54efa619fe
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:05 +0100

    bisq: switch to `verified` parameter

--- Casks/bisq.rb
+++ Casks/bisq.rb
@@ -2,8 +2,8 @@ cask "bisq" do
   version "1.5.1"
   sha256 "31c44e76c3e1a8663bbfa32d21ab80be58b6fed01cac997a24ef86f3b969d575"
 
-  # github.com/bisq-network/bisq/ was verified as official when first introduced to the cask
-  url "https://github.com/bisq-network/bisq/releases/download/v#{version}/Bisq-#{version}.dmg"
+  url "https://github.com/bisq-network/bisq/releases/download/v#{version}/Bisq-#{version}.dmg",
+      verified: "github.com/bisq-network/bisq/"
   appcast "https://github.com/bisq-network/bisq/releases.atom"
   name "Bisq"
   desc "Decentralized bitcoin exchange network"
