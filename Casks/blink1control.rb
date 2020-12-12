74e55bcc9c7d800cf65c4ee9fc68cb8fa9c84eb9
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:06 +0100

    blink1control: switch to `verified` parameter

--- Casks/blink1control.rb
+++ Casks/blink1control.rb
@@ -2,8 +2,8 @@ cask "blink1control" do
   version "2.2.4"
   sha256 "ad050f81cf0970eadc569e8a32f9009a9442d1ef4a11941b09848fb2f22d6048"
 
-  # github.com/todbot/Blink1Control2/ was verified as official when first introduced to the cask
-  url "https://github.com/todbot/Blink1Control2/releases/download/v#{version}/Blink1Control#{version.major}-#{version}-mac.dmg"
+  url "https://github.com/todbot/Blink1Control2/releases/download/v#{version}/Blink1Control#{version.major}-#{version}-mac.dmg",
+      verified: "github.com/todbot/Blink1Control2/"
   appcast "https://github.com/todbot/Blink1Control2/releases.atom"
   name "Blink1Control"
   desc "Utility to control blink(1) USB RGB LED devices"
