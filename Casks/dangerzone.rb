4e55f601b457ae188305b1bb3b5745bc1177fa56
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:20 +0100

    dangerzone: switch to `verified` parameter

--- Casks/dangerzone.rb
+++ Casks/dangerzone.rb
@@ -2,8 +2,8 @@ cask "dangerzone" do
   version "0.1.4"
   sha256 "cdd035d5f0d48b78c36e7e4df4a7218875213600ce8ae79c0309547c0068b4b1"
 
-  # github.com/firstlookmedia/dangerzone/ was verified as official when first introduced to the cask
-  url "https://github.com/firstlookmedia/dangerzone/releases/download/v#{version}/Dangerzone.#{version}.dmg"
+  url "https://github.com/firstlookmedia/dangerzone/releases/download/v#{version}/Dangerzone.#{version}.dmg",
+      verified: "github.com/firstlookmedia/dangerzone/"
   appcast "https://github.com/firstlookmedia/dangerzone/releases.atom"
   name "Dangerzone"
   desc "Convert potentially dangerous PDFs or Office documents into safe PDFs"
