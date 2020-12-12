74761f7391df6795fb20bc3be8b1b5e833d20297
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:17 +0100

    marathon: switch to `verified` parameter

--- Casks/marathon.rb
+++ Casks/marathon.rb
@@ -2,8 +2,8 @@ cask "marathon" do
   version "20200904"
   sha256 "c3959e388909d0fc5d0742a2151fb2945ab7b8dfe9fcfdfabff3708a6a256949"
 
-  # github.com/Aleph-One-Marathon/alephone/ was verified as official when first introduced to the cask
-  url "https://github.com/Aleph-One-Marathon/alephone/releases/download/release-#{version}/Marathon-#{version}-Mac.dmg"
+  url "https://github.com/Aleph-One-Marathon/alephone/releases/download/release-#{version}/Marathon-#{version}-Mac.dmg",
+      verified: "github.com/Aleph-One-Marathon/alephone/"
   appcast "https://github.com/Aleph-One-Marathon/alephone/releases.atom"
   name "Marathon"
   desc "First-person shooter, first in a trilogy"
