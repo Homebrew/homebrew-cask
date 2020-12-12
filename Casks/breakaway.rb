4dee7e72dc007e8bd7f25a4ea5535f3e79fc1729
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:09 +0100

    breakaway: switch to `verified` parameter

--- Casks/breakaway.rb
+++ Casks/breakaway.rb
@@ -2,8 +2,8 @@ cask "breakaway" do
   version "2.0.1"
   sha256 "df5a3d42558f9cdc778c434f88f7e062032cf33dd751ef01e6109a9848a7f76a"
 
-  # sourceforge.net/breakaway/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/breakaway/breakaway-#{version}.zip"
+  url "https://downloads.sourceforge.net/breakaway/breakaway-#{version}.zip",
+      verified: "sourceforge.net/breakaway/"
   appcast "https://sourceforge.net/projects/breakaway/rss"
   name "Breakaway"
   homepage "http://mutablecode.com/apps/breakaway.html"
