a488755e02cb27b778b73169156c3a3897ab713b
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:43 +0100

    quassel: switch to `verified` parameter

--- Casks/quassel.rb
+++ Casks/quassel.rb
@@ -2,8 +2,8 @@ cask "quassel" do
   version "0.13.1"
   sha256 "5a2437dd0fc51a8fa12f2c83472e8294635edea41e55c523535df080c012379e"
 
-  # github.com/quassel/quassel/ was verified as official when first introduced to the cask
-  url "https://github.com/quassel/quassel/releases/download/#{version}/QuasselMono_MacOSX-x86_64_#{version}.dmg"
+  url "https://github.com/quassel/quassel/releases/download/#{version}/QuasselMono_MacOSX-x86_64_#{version}.dmg",
+      verified: "github.com/quassel/quassel/"
   appcast "https://github.com/quassel/quassel/releases.atom"
   name "Quassel"
   desc "IRC client"
