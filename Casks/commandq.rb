abe2e88d82d8414f90e26167536b72c6802dedaf
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:17 +0100

    commandq: switch to `verified` parameter

--- Casks/commandq.rb
+++ Casks/commandq.rb
@@ -2,8 +2,8 @@ cask "commandq" do
   version "2.0.6"
   sha256 "91c639e497994245ab32e021538ef0af0af3291625087e8dbd1d8d555c109d77"
 
-  # download.clickontyler.com/ was verified as official when first introduced to the cask
-  url "https://download.clickontyler.com/commandq/commandq20_#{version.no_dots}.zip"
+  url "https://download.clickontyler.com/commandq/commandq20_#{version.no_dots}.zip",
+      verified: "download.clickontyler.com/"
   appcast "https://shine.clickontyler.com/appcast.php?id=43"
   name "CommandQ"
   homepage "https://commandqapp.com/"
