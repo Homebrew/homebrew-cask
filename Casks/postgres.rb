66a7973b1bae772932ec762e25b462792b692b7b
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:40 +0100

    postgres: switch to `verified` parameter

--- Casks/postgres.rb
+++ Casks/postgres.rb
@@ -2,8 +2,8 @@ cask "postgres" do
   version "2.4.1"
   sha256 "c09c182ce8bbba0c374f216040f90e47ccd40ea5b9349c292691ba5f8de2f3e6"
 
-  # github.com/PostgresApp/PostgresApp/ was verified as official when first introduced to the cask
-  url "https://github.com/PostgresApp/PostgresApp/releases/download/v#{version}/Postgres-#{version}-9.5-9.6-10-11-12-13.dmg"
+  url "https://github.com/PostgresApp/PostgresApp/releases/download/v#{version}/Postgres-#{version}-9.5-9.6-10-11-12-13.dmg",
+      verified: "github.com/PostgresApp/PostgresApp/"
   appcast "https://github.com/PostgresApp/PostgresApp/releases.atom"
   name "Postgres"
   homepage "https://postgresapp.com/"
