bd9c25d6a7aa618ec716ed5a407fb19b8307b11c
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:22 +0100

    mjml: switch to `verified` parameter

--- Casks/mjml.rb
+++ Casks/mjml.rb
@@ -2,8 +2,8 @@ cask "mjml" do
   version "3.0.2"
   sha256 "ab7118386b9a39d34ebf9e4101dac640a7b59bc3eee499500fa51cd7531dc307"
 
-  # github.com/mjmlio/mjml-app/ was verified as official when first introduced to the cask
-  url "https://github.com/mjmlio/mjml-app/releases/download/v#{version}/mjml-app-#{version}-mac.dmg"
+  url "https://github.com/mjmlio/mjml-app/releases/download/v#{version}/mjml-app-#{version}-mac.dmg",
+      verified: "github.com/mjmlio/mjml-app/"
   appcast "https://github.com/mjmlio/mjml-app/releases.atom"
   name "MJML"
   desc "Desktop app for MJML"
