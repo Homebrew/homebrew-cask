e2b5a7c44fce9dbaec4911738eaa47f6f567dbfb
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:20 +0100

    dashcam-viewer: switch to `verified` parameter

--- Casks/dashcam-viewer.rb
+++ Casks/dashcam-viewer.rb
@@ -2,8 +2,8 @@ cask "dashcam-viewer" do
   version "3.6.1"
   sha256 "ac77ef84ea30ff0294b855cfe1f74baf0d82488166552cfd1b55b48c3b3ca7f3"
 
-  # filedn.com/l2s8TAtm4VASBX72ds0zYD8/dcv/ was verified as official when first introduced to the cask
-  url "https://filedn.com/l2s8TAtm4VASBX72ds0zYD8/dcv/Dashcam_Viewer_v#{version}.dmg"
+  url "https://filedn.com/l2s8TAtm4VASBX72ds0zYD8/dcv/Dashcam_Viewer_v#{version}.dmg",
+      verified: "filedn.com/l2s8TAtm4VASBX72ds0zYD8/dcv/"
   appcast "https://dashcamviewer.com/"
   name "Dashcam Viewer"
   name "Dashcam Viewer by Earthshine Software"
