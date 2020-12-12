bb0a451223b0929743b15e6c482c0ce6a8dde847
Markus Reiter <me@reitermark.us>
2020-12-12 05:20:57 +0100

    anki: switch to `verified` parameter

--- Casks/anki.rb
+++ Casks/anki.rb
@@ -2,8 +2,8 @@ cask "anki" do
   version "2.1.36"
   sha256 "35622e702bcbf658b633052091e9e00d0fb095881def1a007a2923586a8bd1c4"
 
-  # github.com/ankitects/anki/ was verified as official when first introduced to the cask
-  url "https://github.com/ankitects/anki/releases/download/#{version}/anki-#{version}-mac.dmg"
+  url "https://github.com/ankitects/anki/releases/download/#{version}/anki-#{version}-mac.dmg",
+      verified: "github.com/ankitects/anki/"
   appcast "https://github.com/ankitects/anki/releases.atom"
   name "Anki"
   desc "Memory training application"
