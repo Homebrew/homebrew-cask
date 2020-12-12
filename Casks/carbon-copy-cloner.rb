f8db7628431095b3d54653532b40e6db9f3362f5
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:12 +0100

    carbon-copy-cloner: switch to `verified` parameter

--- Casks/carbon-copy-cloner.rb
+++ Casks/carbon-copy-cloner.rb
@@ -2,8 +2,8 @@ cask "carbon-copy-cloner" do
   version "5.1.23.6121"
   sha256 "5ec1173273704575517aae54f86ff50f270f353aaf23f10f1767bde2e930b854"
 
-  # bombich.scdn1.secure.raxcdn.com/software/files/ was verified as official when first introduced to the cask
-  url "https://bombich.scdn1.secure.raxcdn.com/software/files/ccc-#{version}.zip"
+  url "https://bombich.scdn1.secure.raxcdn.com/software/files/ccc-#{version}.zip",
+      verified: "bombich.scdn1.secure.raxcdn.com/software/files/"
   appcast "https://bombich.com/software/updates/ccc.php?os_major=10&os_minor=14&os_bugfix=0&ccc=#{version.split(".").last}&beta=0"
   name "Carbon Copy Cloner"
   desc "Hard disk backup and cloning utility"
