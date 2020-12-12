b98b4f566a5bc5c60288f9e410e6c59faf9e89d6
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:02 +0100

    terminus: switch to `verified` parameter

--- Casks/terminus.rb
+++ Casks/terminus.rb
@@ -2,8 +2,8 @@ cask "terminus" do
   version "1.0.122"
   sha256 "3250680cf9a059ef7535278326cc5937f3d0eff56df700054411ee7e1bc7cdc5"
 
-  # github.com/Eugeny/terminus/ was verified as official when first introduced to the cask
-  url "https://github.com/Eugeny/terminus/releases/download/v#{version}/terminus-#{version}-macos.zip"
+  url "https://github.com/Eugeny/terminus/releases/download/v#{version}/terminus-#{version}-macos.zip",
+      verified: "github.com/Eugeny/terminus/"
   appcast "https://github.com/Eugeny/terminus/releases.atom"
   name "Terminus"
   desc "Terminal for a more modern age"
