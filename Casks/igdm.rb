be3bc34f50d10b84b7dbe503dda63d2f5f0cdb73
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:49 +0100

    igdm: switch to `verified` parameter

--- Casks/igdm.rb
+++ Casks/igdm.rb
@@ -2,8 +2,8 @@ cask "igdm" do
   version "3.0.1"
   sha256 "b114842282b64daccc3530790aedc4ed09914fa16e14724948c392b6d48236df"
 
-  # github.com/ifedapoolarewaju/igdm/ was verified as official when first introduced to the cask
-  url "https://github.com/ifedapoolarewaju/igdm/releases/download/v#{version}/IGdm-#{version}.dmg"
+  url "https://github.com/ifedapoolarewaju/igdm/releases/download/v#{version}/IGdm-#{version}.dmg",
+      verified: "github.com/ifedapoolarewaju/igdm/"
   appcast "https://github.com/ifedapoolarewaju/igdm/releases.atom"
   name "IG:dm"
   desc "Desktop application for Instagram DMs"
