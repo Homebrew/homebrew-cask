5fd31252c16699edf2c7364cfe3772e4a714c304
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:28 +0100

    nodebox: switch to `verified` parameter

--- Casks/nodebox.rb
+++ Casks/nodebox.rb
@@ -2,8 +2,8 @@ cask "nodebox" do
   version "3.0.51"
   sha256 "2187ebaa849de93f54c5550529678a8a160b06cf6186e8d21a66fc43f395e629"
 
-  # github.com/nodebox/nodebox/ was verified as official when first introduced to the cask
-  url "https://github.com/nodebox/nodebox/releases/download/v#{version}/NodeBox-#{version}.zip"
+  url "https://github.com/nodebox/nodebox/releases/download/v#{version}/NodeBox-#{version}.zip",
+      verified: "github.com/nodebox/nodebox/"
   appcast "https://www.nodebox.net/node/release-notes.html"
   name "NodeBox"
   desc "Node-based data application for visualization and generative design"
