1a373c65a49b137e29b0315744bfa0a19a085ff3
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:43 +0100

    go-server: switch to `verified` parameter

--- Casks/go-server.rb
+++ Casks/go-server.rb
@@ -2,8 +2,8 @@ cask "go-server" do
   version "19.1.0-8469"
   sha256 "6f7d6ce6875d51ab7194834ec836bc502a405b82c90b9359df520850850a27ee"
 
-  # download.gocd.io/binaries/ was verified as official when first introduced to the cask
-  url "https://download.gocd.io/binaries/#{version}/osx/go-server-#{version}-osx.zip"
+  url "https://download.gocd.io/binaries/#{version}/osx/go-server-#{version}-osx.zip",
+      verified: "download.gocd.io/binaries/"
   appcast "https://github.com/gocd/gocd/releases.atom"
   name "Go Server"
   name "GoCD Server"
