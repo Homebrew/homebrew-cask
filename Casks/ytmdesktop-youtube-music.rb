96c7bf24dfcda331326e2f4f5b6d79d3ce626f67
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:15 +0100

    ytmdesktop-youtube-music: switch to `verified` parameter

--- Casks/ytmdesktop-youtube-music.rb
+++ Casks/ytmdesktop-youtube-music.rb
@@ -2,8 +2,8 @@ cask "ytmdesktop-youtube-music" do
   version "1.13.0"
   sha256 "074e95032a51d7787eed8c4ee2b19046676d72e0cba5cca59af7cc6f79d3887e"
 
-  # github.com/ytmdesktop was verified as official when first introduced to the cask
-  url "https://github.com/ytmdesktop/ytmdesktop/releases/download/v#{version}/YouTube-Music-Desktop-App-#{version}.dmg"
+  url "https://github.com/ytmdesktop/ytmdesktop/releases/download/v#{version}/YouTube-Music-Desktop-App-#{version}.dmg",
+      verified: "github.com/ytmdesktop/"
   appcast "https://github.com/ytmdesktop/ytmdesktop/releases.atom"
   name "YouTube Music Desktop App"
   desc "YouTube music client"
