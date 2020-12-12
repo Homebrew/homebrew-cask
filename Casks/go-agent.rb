615fe8a78b6c4f06c68b689a5863898282c7ff2e
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:43 +0100

    go-agent: switch to `verified` parameter

--- Casks/go-agent.rb
+++ Casks/go-agent.rb
@@ -2,8 +2,8 @@ cask "go-agent" do
   version "19.1.0-8469"
   sha256 "113b21b10590c7a066bdd0597825b672bfed8b33b9cda756b9d6fea42a7c7c3e"
 
-  # download.gocd.io/binaries/ was verified as official when first introduced to the cask
-  url "https://download.gocd.io/binaries/#{version}/osx/go-agent-#{version}-osx.zip"
+  url "https://download.gocd.io/binaries/#{version}/osx/go-agent-#{version}-osx.zip",
+      verified: "download.gocd.io/binaries/"
   appcast "https://github.com/gocd/gocd/releases.atom"
   name "Go Agent"
   name "GoCD Agent"
