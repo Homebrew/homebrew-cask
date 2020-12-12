b6c4e73fc5695032baac11ee6ff2c232a1cd8972
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:56 +0100

    spatterlight: switch to `verified` parameter

--- Casks/spatterlight.rb
+++ Casks/spatterlight.rb
@@ -2,8 +2,8 @@ cask "spatterlight" do
   version "0.5.5"
   sha256 "0893a6cc4f2403334df53c8e01588db57aec08296b03202b31424ee0af3100a7"
 
-  # github.com/angstsmurf/spatterlight/ was verified as official when first introduced to the cask
-  url "https://github.com/angstsmurf/spatterlight/releases/download/v#{version}/Spatterlight_Catalina.zip"
+  url "https://github.com/angstsmurf/spatterlight/releases/download/v#{version}/Spatterlight_Catalina.zip",
+      verified: "github.com/angstsmurf/spatterlight/"
   appcast "https://github.com/angstsmurf/spatterlight/releases.atom"
   name "Spatterlight"
   desc "Play most kinds of interactive fiction game files"
