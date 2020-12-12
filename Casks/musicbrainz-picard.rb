76b59269fb8061570fcb50bd2648cdcda113faa8
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:25 +0100

    musicbrainz-picard: switch to `verified` parameter

--- Casks/musicbrainz-picard.rb
+++ Casks/musicbrainz-picard.rb
@@ -2,8 +2,8 @@ cask "musicbrainz-picard" do
   version "2.5.2"
   sha256 "cb9f2df4e0f476cd73b6d5099aec77c1789d6f3d8ed65323c6dc9b6713ccdd64"
 
-  # musicbrainz.osuosl.org/pub/ was verified as official when first introduced to the cask
-  url "https://musicbrainz.osuosl.org/pub/musicbrainz/picard/MusicBrainz-Picard-#{version}.dmg"
+  url "https://musicbrainz.osuosl.org/pub/musicbrainz/picard/MusicBrainz-Picard-#{version}.dmg",
+      verified: "musicbrainz.osuosl.org/pub/"
   appcast "https://picard.musicbrainz.org/downloads/"
   name "MusicBrainz Picard"
   desc "Cross-platform music tagger"
