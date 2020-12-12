10687c67172864effbf536a5cbe8400a4e4a57f3
Markus Reiter <me@reitermark.us>
2020-12-12 05:23:10 +0100

    vidcutter: switch to `verified` parameter

--- Casks/vidcutter.rb
+++ Casks/vidcutter.rb
@@ -2,8 +2,8 @@ cask "vidcutter" do
   version "6.0.2"
   sha256 "cebc95bd66d5df7fb46831bdd12993a9738ea1528850275e09a3ca69446851a7"
 
-  # github.com/ozmartian/vidcutter/ was verified as official when first introduced to the cask
-  url "https://github.com/ozmartian/vidcutter/releases/download/#{version.major_minor}.0/VidCutter-#{version}-macOS.dmg"
+  url "https://github.com/ozmartian/vidcutter/releases/download/#{version.major_minor}.0/VidCutter-#{version}-macOS.dmg",
+      verified: "github.com/ozmartian/vidcutter/"
   appcast "https://github.com/ozmartian/vidcutter/releases.atom"
   name "VidCutter"
   homepage "https://vidcutter.ozmartians.com/"
