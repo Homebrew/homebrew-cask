bd725b1fd200327c163c35cc97c61a37e97a3f17
Markus Reiter <me@reitermark.us>
2020-12-12 05:20:54 +0100

    airpass: switch to `verified` parameter

--- Casks/airpass.rb
+++ Casks/airpass.rb
@@ -2,8 +2,8 @@ cask "airpass" do
   version "1.0.1"
   sha256 "dd13d6de4d9b430a38e188a92f371525ceb9e539587003af5ed003597bbdc05c"
 
-  # github.com/alvesjtiago/airpass/ was verified as official when first introduced to the cask
-  url "https://github.com/alvesjtiago/airpass/releases/download/#{version}/Airpass.dmg"
+  url "https://github.com/alvesjtiago/airpass/releases/download/#{version}/Airpass.dmg",
+      verified: "github.com/alvesjtiago/airpass/"
   appcast "https://github.com/alvesjtiago/airpass/releases.atom"
   name "Airpass"
   desc "Status bar app to overcome time-constrained WiFi networks"
