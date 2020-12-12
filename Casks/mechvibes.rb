9de80e3e1aba7a614658ec0e7488f11efedeeec4
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:18 +0100

    mechvibes: switch to `verified` parameter

--- Casks/mechvibes.rb
+++ Casks/mechvibes.rb
@@ -2,8 +2,8 @@ cask "mechvibes" do
   version "2.2.0"
   sha256 "3da58cac995893c9f42f1ebc59b0b03d59681ded3482a15639658ddfdf2419d5"
 
-  # downloads.sourceforge.net/mechvibes/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/mechvibes/v#{version}/Mechvibes-#{version}.dmg"
+  url "https://downloads.sourceforge.net/mechvibes/v#{version}/Mechvibes-#{version}.dmg",
+      verified: "downloads.sourceforge.net/mechvibes/"
   appcast "https://sourceforge.net/projects/mechvibes/rss"
   name "Mechvibes"
   desc "Play mechanical keyboard sounds as you type"
