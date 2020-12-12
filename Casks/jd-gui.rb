e3a9c41d36e2c93386aaf4b5d57215a064835662
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:54 +0100

    jd-gui: switch to `verified` parameter

--- Casks/jd-gui.rb
+++ Casks/jd-gui.rb
@@ -2,8 +2,8 @@ cask "jd-gui" do
   version "1.6.6"
   sha256 "b16ce61bbcfd2f006046b66c8896c512a36c6b553afdca75896d7c5e27c7477d"
 
-  # github.com/java-decompiler/jd-gui/ was verified as official when first introduced to the cask
-  url "https://github.com/java-decompiler/jd-gui/releases/download/v#{version}/jd-gui-osx-#{version}.tar"
+  url "https://github.com/java-decompiler/jd-gui/releases/download/v#{version}/jd-gui-osx-#{version}.tar",
+      verified: "github.com/java-decompiler/jd-gui/"
   appcast "https://github.com/java-decompiler/jd-gui/releases.atom"
   name "JD-GUI"
   desc "Standalone Java Decompiler GUI"
