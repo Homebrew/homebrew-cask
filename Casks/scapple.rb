843448c1a986b3aff3a525953cb3f0151f34c948
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:50 +0100

    scapple: switch to `verified` parameter

--- Casks/scapple.rb
+++ Casks/scapple.rb
@@ -2,8 +2,8 @@ cask "scapple" do
   version "1.4,1122"
   sha256 :no_check
 
-  # scrivener.s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://scrivener.s3.amazonaws.com/Scapple.dmg"
+  url "https://scrivener.s3.amazonaws.com/Scapple.dmg",
+      verified: "scrivener.s3.amazonaws.com/"
   appcast "https://www.literatureandlatte.com/downloads/scapple/scapple.xml"
   name "Scapple"
   desc "Notepad software"
