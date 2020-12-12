add9bc8f999ffb8748af98b5ed31bae2de90b4b2
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:20 +0100

    microsoft-outlook: switch to `verified` parameter

--- Casks/microsoft-outlook.rb
+++ Casks/microsoft-outlook.rb
@@ -2,8 +2,8 @@ cask "microsoft-outlook" do
   version "16.43.20110804"
   sha256 "0e53acefafc25d1eebbf257f343de0d0a5258099c154f7ba5d99aa709fb50d08"
 
-  # officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/ was verified as official when first introduced to the cask
-  url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Outlook_#{version}_Installer.pkg"
+  url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Outlook_#{version}_Installer.pkg",
+      verified: "officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/"
   appcast "https://docs.microsoft.com/en-us/officeupdates/update-history-office-for-mac"
   name "Microsoft Outlook"
   desc "Cross-platform industry standard email client"
