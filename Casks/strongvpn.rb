068840cf7c99c63029e00b9f062bb1860f24cd19
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:59 +0100

    strongvpn: switch to `verified` parameter

--- Casks/strongvpn.rb
+++ Casks/strongvpn.rb
@@ -2,8 +2,8 @@ cask "strongvpn" do
   version "2.2.2,45556"
   sha256 :no_check
 
-  # static.colomovers.com/ was verified as official when first introduced to the cask
-  url "https://static.colomovers.com/mac/StrongVPN.dmg"
+  url "https://static.colomovers.com/mac/StrongVPN.dmg",
+      verified: "static.colomovers.com/"
   appcast "https://static.colomovers.com/mac/updates.xml"
   name "StrongVPN"
   homepage "https://strongvpn.com/vpn-apps/macos/"
