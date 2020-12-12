452a2848d569cd259ffe58d536af827b99609f38
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:28 +0100

    nordvpn: switch to `verified` parameter

--- Casks/nordvpn.rb
+++ Casks/nordvpn.rb
@@ -2,8 +2,8 @@ cask "nordvpn" do
   version "5.11.0"
   sha256 "8bacd3708ca26675ef754a1e4bc6e9e246dd2f76e1e641e8a41546082f2c4219"
 
-  # downloads.nordcdn.com/ was verified as official when first introduced to the cask
-  url "https://downloads.nordcdn.com/apps/macos/generic/NordVPN-OpenVPN/#{version}/NordVPN.pkg"
+  url "https://downloads.nordcdn.com/apps/macos/generic/NordVPN-OpenVPN/#{version}/NordVPN.pkg",
+      verified: "downloads.nordcdn.com/"
   appcast "https://downloads.nordcdn.com/apps/macos/generic/NordVPN-OpenVPN/latest/update_pkg.xml"
   name "NordVPN"
   desc "VPN client for secure internet access and private browsing"
