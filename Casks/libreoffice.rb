e677f0aa0c0c0c21c872e555eec4a1c836177779
Markus Reiter <me@reitermark.us>
2020-12-12 05:22:12 +0100

    libreoffice: switch to `verified` parameter

--- Casks/libreoffice.rb
+++ Casks/libreoffice.rb
@@ -2,8 +2,8 @@ cask "libreoffice" do
   version "7.0.3"
   sha256 "9e0a8fb874d4f98954ab7822399a28602362d535617b53ef04f1251bd91c0e4a"
 
-  # documentfoundation.org/ was verified as official when first introduced to the cask
-  url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg"
+  url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg",
+      verified: "documentfoundation.org/"
   appcast "https://download.documentfoundation.org/libreoffice/stable/"
   name "LibreOffice"
   desc "Free cross-platform office suite"
