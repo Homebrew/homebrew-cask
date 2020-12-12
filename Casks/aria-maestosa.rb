cf30b292c86e82e11c2a62df0fa993ff1c026fff
Markus Reiter <me@reitermark.us>
2020-12-12 05:20:58 +0100

    aria-maestosa: switch to `verified` parameter

--- Casks/aria-maestosa.rb
+++ Casks/aria-maestosa.rb
@@ -2,8 +2,8 @@ cask "aria-maestosa" do
   version "1.4.13"
   sha256 "51e059957841d63b37af31297150b993034fc40b20fb84f006eb8fd0a75a349f"
 
-  # downloads.sourceforge.net/ariamaestosa/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/ariamaestosa/AriaMaestosa-osx-64bits-#{version}.zip"
+  url "https://downloads.sourceforge.net/ariamaestosa/AriaMaestosa-osx-64bits-#{version}.zip",
+      verified: "downloads.sourceforge.net/ariamaestosa/"
   appcast "https://sourceforge.net/projects/ariamaestosa/rss"
   name "Aria Maestosa"
   homepage "https://ariamaestosa.sourceforge.io/"
