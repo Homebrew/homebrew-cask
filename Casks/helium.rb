6ee9a7b7690d0f6b14aaff1c61f51895a6e9d6f1
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:47 +0100

    helium: switch to `verified` parameter

--- Casks/helium.rb
+++ Casks/helium.rb
@@ -2,8 +2,8 @@ cask "helium" do
   version "1.0"
   sha256 :no_check
 
-  # download.clockworkmod.com/carbon/ was verified as official when first introduced to the cask
-  url "https://download.clockworkmod.com/carbon/carbon-mac.zip"
+  url "https://download.clockworkmod.com/carbon/carbon-mac.zip",
+      verified: "download.clockworkmod.com/carbon/"
   name "Helium"
   homepage "https://github.com/koush/support-wiki/wiki/Helium-Desktop-Installer-and-Android-App"
 
