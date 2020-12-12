04dcf29070210b9c45cd58132e8bbc9b39567432
Markus Reiter <me@reitermark.us>
2020-12-12 05:21:38 +0100

    freeplane: switch to `verified` parameter

--- Casks/freeplane.rb
+++ Casks/freeplane.rb
@@ -2,8 +2,8 @@ cask "freeplane" do
   version "1.8.10"
   sha256 "bf68aa48c856c6eb188105f596d9ab28ea31abf3a99ad29fb5c5e1a6c76938bd"
 
-  # downloads.sourceforge.net/freeplane/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/freeplane/freeplane%20stable/Freeplane-#{version}.dmg"
+  url "https://downloads.sourceforge.net/freeplane/freeplane%20stable/Freeplane-#{version}.dmg",
+      verified: "downloads.sourceforge.net/freeplane/"
   appcast "https://sourceforge.net/projects/freeplane/rss?path=/freeplane%20stable"
   name "Freeplane"
   desc "Mind mapping and knowledge management software"
