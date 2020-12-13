b55c8c12975bccec48f526444bb9c2c190bb313a
Randall <ran.dall@icloud.com>
2020-12-13 13:57:10 -0800

    Update anydo from 4.2.123 to 4.2.124

--- Casks/anydo.rb
+++ Casks/anydo.rb
@@ -1,6 +1,6 @@
 cask "anydo" do
-  version "4.2.123"
-  sha256 "96e1a31f80919316e76d60672be68eb51305de68f3d9ebbd320b3f3f2717d404"
+  version "4.2.124"
+  sha256 "88ce50a7d01cc75e991eba47b0c7a7a68828cfcdc47cfc0b05f072d48d3eb8af"
 
   url "https://electron-app.any.do/Any.do-#{version}.dmg"
   appcast "https://electron-app.any.do/latest-mac.yml"
