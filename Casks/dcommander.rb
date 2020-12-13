da91eda9714fcba38a1eb8cb7e7f7f012dea1ab2
suschizu <brigitte@corecode.io>
2020-12-13 20:48:12 +0100

    Update dcommander from 3.8.8,3880 to 3.8.8,3881 (#95388)
    * Update dcommander from 3.8.8,3880 to 3.8.8,3881
    
    * Update dcommander.rb

--- Casks/dcommander.rb
+++ Casks/dcommander.rb
@@ -1,11 +1,12 @@
 cask "dcommander" do
-  version "3.8.8,3880"
+  version "3.8.8,3881"
   sha256 :no_check
 
   url "https://devstorm-apps.com/dc/download.php"
   appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://devstorm-apps.com/dc/download.php",
           must_contain: version.after_comma
   name "DCommander"
+  desc "Two-pane file manager"
   homepage "https://devstorm-apps.com/dc/"
 
   app "DCommander.app"
