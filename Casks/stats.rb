ee2077a9ee6b2e03b12ac81ff8454067acda1f66
Udo Kramer <optik@fluffel.io>
2020-12-13 15:41:01 +0100

    Update stats from 2.4.7 to 2.4.8 (#95390)

--- Casks/stats.rb
+++ Casks/stats.rb
@@ -1,6 +1,6 @@
 cask "stats" do
-  version "2.4.7"
-  sha256 "b80d370498a2bed4c5457cbf7ec1ffd634401983f9bba033687ad387fd86833d"
+  version "2.4.8"
+  sha256 "19e75c31b8b0a6c2572503be6542c5403039f87abbca29436b4660b853820671"
 
   url "https://github.com/exelban/stats/releases/download/v#{version}/Stats.dmg"
   appcast "https://github.com/exelban/stats/releases.atom"
