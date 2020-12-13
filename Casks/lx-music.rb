68a1081522d3735ed3029c92a7dc031f8bd23cf5
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-13 13:56:22 -0800

    Update lx-music from 1.4.1 to 1.5.0 (#95403)

--- Casks/lx-music.rb
+++ Casks/lx-music.rb
@@ -1,6 +1,6 @@
 cask "lx-music" do
-  version "1.4.1"
-  sha256 "b653467163917fff5a0275605c09a064093e898fe9de2c38a5c3c965aa2f100a"
+  version "1.5.0"
+  sha256 "e9e96235d3db380db5ca2a295364edb6a7f740f4a512aa6a2b02932b2370232d"
 
   url "https://github.com/lyswhut/lx-music-desktop/releases/download/v#{version}/lx-music-desktop-#{version}.dmg"
   appcast "https://github.com/lyswhut/lx-music-desktop/releases.atom"
