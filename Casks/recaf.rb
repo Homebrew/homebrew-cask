a779a8fe543a7593dc0423b00a6dc82877c74f43
Markus Reiter <me@reitermark.us>
2020-12-14 12:47:39 +0100

    Update recaf.rb

--- Casks/recaf.rb
+++ Casks/recaf.rb
@@ -2,7 +2,6 @@ cask "recaf" do
   version "2.15.0"
   sha256 "d5fc24e429f4996fb3882299d8b31ffcf8c3f99c935c4766b222ebe52872f2a9"
 
-  # github.com/Col-E/Recaf/ was verified as official when first introduced to the cask
   url "https://github.com/Col-E/Recaf/releases/download/#{version}/recaf-#{version}-J8-jar-with-dependencies.jar",
       verified: "github.com/Col-E/Recaf/"
   appcast "https://github.com/Col-E/Recaf/releases.atom"
