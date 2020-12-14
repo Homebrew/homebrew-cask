b143a70a8d0423e1785809c1dad876abdfebc0a3
Vítor Galvão <info@vitorgalvao.com>
2020-12-14 18:51:11 +0000

    Update geoda.rb

--- Casks/geoda.rb
+++ Casks/geoda.rb
@@ -3,7 +3,7 @@ cask "geoda" do
   sha256 "035dc54b13098f2fb27462a4ddfe74eb7f73dda433e606b3d9b1d8e2a88db149"
 
   url "https://github.com/GeoDaCenter/geoda/releases/download/v#{version.major_minor}/GeoDa#{version}-Installer.dmg",
-      verified: "https://github.com/GeoDaCenter/geoda/"
+      verified: "github.com/GeoDaCenter/geoda/"
   appcast "https://github.com/GeoDaCenter/geoda/releases.atom"
   name "GeoDa"
   desc "Spatial analysis, statistics, autocorrelation and regression"
