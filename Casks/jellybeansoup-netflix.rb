54bd16486a5c25754768799332c47e6b6f73485c
Vítor Galvão <info@vitorgalvao.com>
2020-12-14 18:47:50 +0000

    Update jellybeansoup-netflix.rb

--- Casks/jellybeansoup-netflix.rb
+++ Casks/jellybeansoup-netflix.rb
@@ -2,7 +2,7 @@ cask "jellybeansoup-netflix" do
   version "1.0.4"
   sha256 "f9da1b57012e5e14777e6226d7a0009d5fe877713f3f889cfb72b438d6251e76"
 
-  url "https://github.com/jellybeansoup/macos-netflix/releases/download/v#{version}/Netflix.v#{version}.zip",
+  url "https://github.com/jellybeansoup/macos-netflix/releases/download/v#{version}/Netflix.v#{version}.zip"
   appcast "https://github.com/jellybeansoup/macos-netflix/releases.atom"
   name "Netflix"
   desc "Third-party app to use Netflix outside the browser"
