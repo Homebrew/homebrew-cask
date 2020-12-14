258a9d253d2338495181075fda877b8953f3cb04
Reno <25192197+singularitti@users.noreply.github.com>
2020-12-14 17:40:33 -0500

    Update eudic from 3.9.10,2020-11-01 to 4.0.1,2020-11-14 (#95484)

--- Casks/eudic.rb
+++ Casks/eudic.rb
@@ -1,6 +1,6 @@
 cask "eudic" do
-  version "3.9.10,2020-11-01"
-  sha256 "2c5af4bdeb6b918c3007b97189669f1fdaabd8929730a4bb6df3e3d367475cfe"
+  version "4.0.1,2020-11-14"
+  sha256 "7b381dca81eac835fb8972e15eb4895ba8aaccdb48aaa2b920f77c51b4532174"
 
   url "https://static.frdic.com/pkg/eudicmac.dmg?v=#{version.after_comma}",
       verified:   "static.frdic.com/",
@@ -19,12 +19,12 @@ cask "eudic" do
   ]
 
   zap trash: [
-    "~/Library/Caches/com.eusoft.eudic",
-    "~/Library/Caches/com.eusoft.eudic.LightPeek",
     "~/Library/Application Scripts/com.eusoft.eudic.QuickLook",
     "~/Library/Application Support/com.eusoft.eudic",
+    "~/Library/Caches/com.eusoft.eudic",
+    "~/Library/Caches/com.eusoft.eudic.LightPeek",
     "~/Library/Eudb_en",
-    "~/Library/Preferences/com.eusoft.eudic.LightPeek.plist",
     "~/Library/Preferences/com.eusoft.eudic.plist",
+    "~/Library/Preferences/com.eusoft.eudic.LightPeek.plist",
   ]
 end
