e44ae0c470b7ee2ecee8d70f226b8560b3f5a838
Randall <ran.dall@icloud.com>
2020-12-14 08:19:24 -0800

    Update mtgaprotracker from 2.1.12 to 2.1.13

--- Casks/mtgaprotracker.rb
+++ Casks/mtgaprotracker.rb
@@ -1,6 +1,6 @@
 cask "mtgaprotracker" do
-  version "2.1.12"
-  sha256 "7bc18e9ef984bd8631160391a10d9f777995f509a3ff3e203247cfd7a12be1f8"
+  version "2.1.13"
+  sha256 "40a43ea8243010b8f2bd728466a5df954714d1183a5d4f4714ecf9907c0fe65a"
 
   url "https://github.com/Razviar/mtgap/releases/download/v#{version}/mtgaprotracker.dmg",
       verified: "github.com/Razviar/mtgap/"
