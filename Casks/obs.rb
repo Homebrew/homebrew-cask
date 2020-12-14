a928789429c0206e4d007ab35c2493d9f9c05dd2
Vítor Galvão <info@vitorgalvao.com>
2020-12-14 23:44:52 +0000

    Update obs.rb

--- Casks/obs.rb
+++ Casks/obs.rb
@@ -3,7 +3,7 @@ cask "obs" do
   sha256 "f3713d629e1c9cda98b630fd11bfd25dbd4d0ba4fcf5fe85a2978da616981472"
 
   url "https://cdn-fastly.obsproject.com/downloads/obs-mac-#{version}.dmg"
-  appcast "https://obsproject.com/osx_update/stable/updates.xml"
+  appcast "https://github.com/obsproject/obs-studio/releases.atom"
   name "OBS"
   desc "Open-source software for live streaming and screen recording"
   homepage "https://obsproject.com/"
