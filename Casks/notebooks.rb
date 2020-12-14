9028d7bfeecd697deb7b882b9ebc68487818b2e7
Julian Mayer <julian@corecode.io>
2020-12-14 12:08:39 +0100

    Update notebooks from 2.3 to 2.3.1

--- Casks/notebooks.rb
+++ Casks/notebooks.rb
@@ -1,6 +1,6 @@
 cask "notebooks" do
-  version "2.3"
-  sha256 "615abf89d975f00421e7632ca6ec03171674984f77fd68c243b31b57199f0ab9"
+  version "2.3.1"
+  sha256 "3516ad3f39619822c486984f924ec2598ec344be81ba09420a81a5286076abad"
 
   url "https://www.notebooksapp.com/Download/macOS/v#{version.major}/Notebooks.dmg"
   appcast "https://notebooksapp.com/Download/macOS/v#{version.major}/Notebooks#{version.major}Appcast.xml"
