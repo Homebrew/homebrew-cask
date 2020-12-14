2b6c3f0ad0db2371a8037647c05bfffb0f262dda
Randall <ran.dall@icloud.com>
2020-12-14 08:14:54 -0800

    Update tribler from 7.6.0 to 7.6.1

--- Casks/tribler.rb
+++ Casks/tribler.rb
@@ -1,6 +1,6 @@
 cask "tribler" do
-  version "7.6.0"
-  sha256 "82d8f5d923250cfd1854cab78b6394bc003e566de03222ca2bff80df62ea3476"
+  version "7.6.1"
+  sha256 "3eefc1cdbd183275ad24834440b0da03991e714f4d3325068aad2dfcfa31b2a9"
 
   url "https://github.com/Tribler/tribler/releases/download/v#{version}/Tribler-#{version}.dmg",
       verified: "github.com/Tribler/tribler/"
