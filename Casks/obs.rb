029a2e3c95ef8e8dcbdd53b29a274b8f921150be
Yusuf Kursad Kaya <ykursadkaya@gmail.com>
2020-12-15 00:54:17 +0300

    Update obs zap and appcast
    Virtual camera feature has been added to OBS macOS version and when started for the first time it creates DAL driver in specified folder and this appcast is in the Info.plist from original app package.

--- Casks/obs.rb
+++ Casks/obs.rb
@@ -3,7 +3,7 @@ cask "obs" do
   sha256 "f3713d629e1c9cda98b630fd11bfd25dbd4d0ba4fcf5fe85a2978da616981472"
 
   url "https://cdn-fastly.obsproject.com/downloads/obs-mac-#{version}.dmg"
-  appcast "https://github.com/obsproject/obs-studio/releases.atom"
+  appcast "https://obsproject.com/osx_update/stable/updates.xml"
   name "OBS"
   desc "Open-source software for live streaming and screen recording"
   homepage "https://obsproject.com/"
@@ -13,6 +13,7 @@ cask "obs" do
   app "OBS.app"
 
   zap trash: [
+    "/Library/CoreMediaIO/Plug-Ins/DAL/obs-mac-virtualcam.plugin",
     "~/Library/Application Support/obs-studio",
     "~/Library/Preferences/com.obsproject.obs-studio.plist",
     "~/Library/Saved Application State/com.obsproject.obs-studio.savedState",
