a84545bbc5559f0f113b513f06402c13578b0517
Markus Reiter <me@reitermark.us>
2020-12-14 23:26:36 +0100

    Update xnviewmp.rb

--- Casks/xnviewmp.rb
+++ Casks/xnviewmp.rb
@@ -2,7 +2,7 @@ cask "xnviewmp" do
   version "0.98.0"
   sha256 "706b8e0dfea9efafc1b14413797ab594626be79a1cf12efe960e0862ed7116c0"
 
-  url "https://download.xnview.com/old_versions/XnViewMP-0980-mac.dmg"
+  url "https://download.xnview.com/old_versions/XnViewMP-#{version.no_dots}-mac.dmg"
   name "XnViewMP"
   desc "Photo viewer, image manager, image resizer and more"
   homepage "https://www.xnview.com/"
