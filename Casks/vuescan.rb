99e4d0f149e0058a6b1c25ec5ab36aa1c185b6f1
Vítor Galvão <info@vitorgalvao.com>
2020-12-15 00:23:45 +0000

    vuescan.rb: removed sha256 comment

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,6 +1,6 @@
 cask "vuescan" do
   version "9.7.37"
-  sha256 :no_check # required as upstream package is updated in-place
+  sha256 :no_check
 
   url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
   appcast "https://www.hamrick.com/vuescan/vuescan.htm"
