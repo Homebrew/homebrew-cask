3390ace81f8289a2c2edd53a905a83d988b45cad
Vítor Galvão <info@vitorgalvao.com>
2020-12-15 00:23:45 +0000

    gactions.rb: removed sha256 comment

--- Casks/gactions.rb
+++ Casks/gactions.rb
@@ -1,6 +1,6 @@
 cask "gactions" do
   version "3"
-  sha256 :no_check # required as upstream package is updated in-place
+  sha256 :no_check
 
   # Download page: https://developers.google.com/assistant/conversational/quickstart
   url "https://dl.google.com/gactions/v#{version}/release/gactions-sdk_darwin.tar.gz"
