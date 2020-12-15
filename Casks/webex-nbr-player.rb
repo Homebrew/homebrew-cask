d8f896c443a67e1d5383a90c3e587cc42b0c0765
Vítor Galvão <info@vitorgalvao.com>
2020-12-15 00:23:46 +0000

    webex-nbr-player.rb: removed sha256 comment

--- Casks/webex-nbr-player.rb
+++ Casks/webex-nbr-player.rb
@@ -1,6 +1,6 @@
 cask "webex-nbr-player" do
   version "T33L"
-  sha256 :no_check # required as upstream package is updated in-place
+  sha256 :no_check
 
   url "https://welcome.webex.com/client/#{version}/mac/intel/webexnbrplayer_intel.dmg"
   name "Webex Network Recording player"
