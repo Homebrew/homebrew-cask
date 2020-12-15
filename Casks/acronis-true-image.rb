634fcf2899bd061c8785bdc41148f16870867cc6
Vítor Galvão <info@vitorgalvao.com>
2020-12-15 00:23:45 +0000

    acronis-true-image.rb: removed sha256 comment

--- Casks/acronis-true-image.rb
+++ Casks/acronis-true-image.rb
@@ -1,6 +1,6 @@
 cask "acronis-true-image" do
   version "2021"
-  sha256 :no_check # required as upstream package is updated in-place
+  sha256 :no_check
 
   url "https://dl.acronis.com/u/AcronisTrueImage#{version}.dmg"
   appcast "https://www.acronis.com/en-us/support/updates/index.html"
