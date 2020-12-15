75f855f7b414ebc49100b0d5134f7c4356a76c3e
Randall <ran.dall@icloud.com>
2020-12-15 11:24:55 -0800

    Update glyphs from 3.0.2,3043 to 3.0.2,3044

--- Casks/glyphs.rb
+++ Casks/glyphs.rb
@@ -1,6 +1,6 @@
 cask "glyphs" do
-  version "3.0.2,3043"
-  sha256 "cdc06646c01f6bbe8d2590c05588b574f81ce4d5b6f23f32655faaacb326d8fe"
+  version "3.0.2,3044"
+  sha256 "8443af1de42dd2bd64805b101432312896db000e07cbbe8cc5ced0dd16a0a022"
 
   url "https://updates.glyphsapp.com/Glyphs#{version.before_comma}-#{version.after_comma}.zip"
   appcast "https://updates.glyphsapp.com/appcast#{version.major}.xml"
