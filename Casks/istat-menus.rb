c644b93244e5194d19c890cad9884acdb6271155
Vítor Galvão <info@vitorgalvao.com>
2020-12-15 00:23:45 +0000

    istat-menus.rb: removed sha256 comment

--- Casks/istat-menus.rb
+++ Casks/istat-menus.rb
@@ -1,6 +1,6 @@
 cask "istat-menus" do
   version "6.51"
-  sha256 :no_check # required as upstream package is updated in-place
+  sha256 :no_check
 
   url "https://cdn.bjango.com/files/istatmenus#{version.major}/istatmenus#{version}.zip"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.bjango.com/istatmenus/"
