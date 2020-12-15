d381f06573a63576c209338471e345494a9b9790
Julian Mayer <julian@corecode.io>
2020-12-15 18:08:38 +0100

    Update bitbar from 1.10.0 to 1.10.1

--- Casks/bitbar.rb
+++ Casks/bitbar.rb
@@ -1,14 +1,12 @@
 cask "bitbar" do
-  version "1.10.0"
-  sha256 "d0e023583d443f98f2adb3c307da01de608abc98d03887e5fd85d3004716d0fb"
+  version "1.10.1"
+  sha256 "8a7013dca92715ba80cccef98b84dd1bc8d0b4c4b603f732e006eb204bab43fa"
 
   url "https://github.com/matryer/bitbar/releases/download/v#{version}/BitBar.app.zip"
   appcast "https://github.com/matryer/bitbar/releases.atom"
   name "BitBar"
   desc "Utility to display the output from any script or program in the menu bar"
   homepage "https://github.com/matryer/bitbar/"
-
-  depends_on macos: ">= :big_sur"
 
   app "BitBar.app"
 
