de02a22f3d84fdf54d912e339026e02bdd0df0c7
Vítor Galvão <info@vitorgalvao.com>
2020-12-15 00:23:45 +0000

    paragon-ntfs.rb: removed sha256 comment

--- Casks/paragon-ntfs.rb
+++ Casks/paragon-ntfs.rb
@@ -1,6 +1,6 @@
 cask "paragon-ntfs" do
   version "15"
-  sha256 :no_check # required as upstream package is updated in-place
+  sha256 :no_check
 
   url "https://dl.paragon-software.com/demo/ntfsmac#{version}_trial.dmg"
   name "Paragon NTFS for Mac"
