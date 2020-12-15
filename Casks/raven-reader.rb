94c21be6db53b7979084a6d7efdf8bf7e0ca315e
suschizu <brigitte@corecode.io>
2020-12-15 20:11:41 +0100

    Update raven-reader from 1.0.14 to 1.0.15 (#95555)

--- Casks/raven-reader.rb
+++ Casks/raven-reader.rb
@@ -1,6 +1,6 @@
 cask "raven-reader" do
-  version "1.0.14"
-  sha256 "6bbeca4f1f41db9b12a56c1dd9bc0b03b7cf6ffc13c71e76aa6ee000b59dacb7"
+  version "1.0.15"
+  sha256 "e1175d7b11511f699e6da1556c5bcf0a87c795e6fa356bb2d5d7eb67ec68356f"
 
   url "https://download.helloefficiency.com/ravenreader/Raven%20Reader-#{version}.dmg",
       verified: "download.helloefficiency.com/ravenreader/"
