8f0b9db480f212a4a17ad6c49f0086bdde5e8b8d
kuerb <bjkr+github@posteo.net>
2020-12-16 18:27:03 +0100

    Update texpad (#95548)

--- Casks/texpad.rb
+++ Casks/texpad.rb
@@ -5,6 +5,9 @@ cask "texpad" do
   elsif MacOS.version <= :el_capitan
     version "1.8.5,404:f8f30e5"
     sha256 "676a1b071142c022cdfda57668c811f7747b36ded442548073fe6dda1b9ca934"
+  elsif MacOS.version <= :high_sierra
+    version "1.8.15,529:346c842"
+    sha256 "480bf4a3e8fd809c1eb07cb00099ed0d362996738a872efb42cb179488e8c1e1"
   else
     version "1.8.19,550:b20304d"
     sha256 "2e05ff385d35748a2ec69c1fa747187e2597d3461501cb63edfc5c503c3ceea5"
