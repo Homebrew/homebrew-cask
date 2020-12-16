1329a63395b4ddf3d4986b8bd1097c9f6b5a7fb4
suschizu <brigitte@corecode.io>
2020-12-16 16:06:52 +0100

    Update textexpander from 6.5.6 to 6.8,680.30 (#95647)

--- Casks/textexpander.rb
+++ Casks/textexpander.rb
@@ -1,8 +1,8 @@
 cask "textexpander" do
-  version "6.5.6"
-  sha256 "ec2bfd8336d27a2a31c9d42a4ffafac37c54aef852eb6edb54927e4043878432"
+  version "6.8,680.30"
+  sha256 "9cddd638d0e3179d62d4eb2e1127c516d58cee7021df3f78bbb44d1d6a1c9684"
 
-  url "https://cdn.textexpander.com/mac/TextExpander_#{version}.zip",
+  url "https://cdn.textexpander.com/mac/#{version.after_comma}/TextExpander_#{version.before_comma}.zip",
       verified: "cdn.textexpander.com/mac/"
   appcast "https://smilesoftware.com/appcast/TextExpander#{version.major}.xml"
   name "TextExpander"
