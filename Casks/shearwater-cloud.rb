72cd66e83e89a354fd85c5c0b6798ab2421c85bf
Paulo Bardes <bardes0022@gmail.com>
2020-12-16 12:48:05 -0300

    Fix style issues on shearwater-cloud cask

--- Casks/shearwater-cloud.rb
+++ Casks/shearwater-cloud.rb
@@ -4,8 +4,8 @@ cask "shearwater-cloud" do
 
   url "https://www.shearwater.com/wp-content/downloads/ShearwaterCloudInstaller_#{version}.dmg"
   name "Shearwater Cloud"
-  desc "Shearwater's divers log app. Used to review, edit and share dive log data. Additionally, the app is also capable of performing firmware updates and changing the language in your dive computer."
-  homepage "https://www.shearwater.com"
+  desc "Shearwater's divers log app. Used to review, edit and share dive log data"
+  homepage "https://www.shearwater.com/"
 
   app "Shearwater Cloud.app"
 end
