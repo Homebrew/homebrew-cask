12250bcb390aa709884dbc9f3dd70bb6123b823d
Randall <ran.dall@icloud.com>
2020-12-16 08:55:43 -0800

    Update kite from 0.20201203.0 to 0.20201214.0

--- Casks/kite.rb
+++ Casks/kite.rb
@@ -1,6 +1,6 @@
 cask "kite" do
-  version "0.20201203.0"
-  sha256 "74de74c8925f6e3d9f0db395e0cfdfe7534c9db3d5c09137cc4d19c42f90670a"
+  version "0.20201214.0"
+  sha256 "33f936937c580c4c1e76a9c460cc7a0500a0c5833dbb3a92e2857f5d4ae1a7fb"
 
   url "https://draqv87tt43s0.cloudfront.net/mac/#{version}/Kite.dmg",
       verified: "draqv87tt43s0.cloudfront.net/"
