37aa76e4bc0125588642c56e30c346da4e989a85
Brigitte <brigitte@corecode.io>
2020-12-16 12:41:25 +0100

    Update berrycast from 0.30.0,858b5929-15c3-5a2d-aac7-a8b168dddc50 to 0.31.0

--- Casks/berrycast.rb
+++ Casks/berrycast.rb
@@ -1,8 +1,8 @@
 cask "berrycast" do
-  version "0.30.0,858b5929-15c3-5a2d-aac7-a8b168dddc50"
-  sha256 "6fe0f1e637bb5ffc29c3826480341a40b42ab3c4c316ec3092e6f5f91ae93aaf"
+  version "0.31.0"
+  sha256 "7a1692a5bde3e3b93c2b169984d6c09c5b9bc4b54b212d966dfd957e0ed6f2aa"
 
-  url "https://media.berrycast.app/desktop-installer/Berrycast-#{version.before_comma}-latest.dmg?display-file-name=Berrycast-#{version.before_comma}-latest_#{version.after_comma}.dmg",
+  url "https://media.berrycast.app/desktop-installer/Berrycast-#{version}-latest.dmg",
       verified: "media.berrycast.app/"
   appcast "https://media.berrycast.app/desktop-installer/v2/latest-mac.yml"
   name "Berrycast"
