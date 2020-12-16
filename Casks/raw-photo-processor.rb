75a457f2ca1cd9bf53f5f00e23996a90ff41979c
frihamn <hello@prudens.eu>
2020-12-16 09:45:54 +0100

    Update raw-photo-processor from 1912Beta to 1915Beta

--- Casks/raw-photo-processor.rb
+++ Casks/raw-photo-processor.rb
@@ -1,7 +1,7 @@
 cask "raw-photo-processor" do
   # Betas of this software are release quality: https://groups.google.com/d/msg/raw-photo-processor/PJyyP2JwIwI/dn3CFknuCwAJ
-  version "1912Beta"
-  sha256 "bf5e63a511ab63fbdd893f0e38ec894267a430110885aec44c748364c69d24e8"
+  version "1915Beta"
+  sha256 "a621be34bc51a0072a132a852f4f562aae1a10fffbd2f701fd09931707262cdf"
 
   url "https://www.raw-photo-processor.com/RPP/RPP64_#{version}.zip"
   appcast "https://groups.google.com/forum/feed/raw-photo-processor/msgs/rss.xml?num=50"
