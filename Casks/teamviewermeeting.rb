6fb3d99e721f20b92b9b1ee4dbcc2677efe5a78d
Randall <ran.dall@icloud.com>
2020-12-16 09:13:00 -0800

    Update teamviewermeeting from 15.13.6 to 15.13.10

--- Casks/teamviewermeeting.rb
+++ Casks/teamviewermeeting.rb
@@ -1,6 +1,6 @@
 cask "teamviewermeeting" do
-  version "15.13.6"
-  sha256 "0cf9511373d0194601f67489ce0db2f20e966e603968db869d947d6b87c07cbc"
+  version "15.13.10"
+  sha256 "31b729de17f474f94e909527ce25557db0eec4f7b80f2d0a86d657cce6f18ca1"
 
   url "https://dl.teamviewer.com/teamviewermeeting/installer/mac/#{version}/TeamViewerMeeting.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.teamviewer.com/blizz/mac"
