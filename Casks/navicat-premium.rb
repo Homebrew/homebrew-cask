716d79445ee298fa5fbef9572131f16975bf4030
suschizu <brigitte@corecode.io>
2020-12-17 21:02:26 +0100

    Update navicat-premium.rb

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -7,7 +7,7 @@ cask "navicat-premium" do
     homepage "https://www.navicat.com.cn/products/navicat-premium"
   end
   language "en", default: true do
-    sha256 "b445b658654e2fa32e788f8175c7bdf4da87be97033b61eae7664a4deff9a9dc"
+    sha256 "0df19642f430db7d9032faac3fb00d9a2b040754f776ec4ae8da719a7c52437b"
     url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_premium_en.dmg"
     appcast "https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20Premium&appLang=en"
     homepage "https://www.navicat.com/products/navicat-premium"
