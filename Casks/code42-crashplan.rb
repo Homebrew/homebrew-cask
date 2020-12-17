d2149f753123c9655f030e6e108673150ca75d38
Vítor Galvão <info@vitorgalvao.com>
2020-12-17 17:27:03 +0000

    Update code42-crashplan.rb

--- Casks/code42-crashplan.rb
+++ Casks/code42-crashplan.rb
@@ -1,8 +1,8 @@
 cask "code42-crashplan" do
-  version "8.2.1,1525200006821,77"
+  version "8.2.1,1525200006821:77"
   sha256 "89af29fd387aef977ca1da175105d0861c2a39742dc87dd648ea330548570ae4"
 
-  url "https://download.code42.com/installs/client-package-repository/#{version.before_comma}/#{version.after_comma.after_comma}/install/Code42_#{version.before_comma}_#{version.after_comma.before_comma}_#{version.after_comma.after_comma}_Mac.dmg",
+  url "https://download.code42.com/installs/client-package-repository/#{version.before_comma}/#{version.after_colon}/install/Code42_#{version.before_comma}_#{version.after_comma.before_colon}_#{version.after_colon}_Mac.dmg",
       verified: "download.code42.com/"
   appcast "https://support.code42.com/Release_Notes"
   name "Code42 CrashPlan"
