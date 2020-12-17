2d9a02827aeec3b5a4545368c6f48c756fb6bfd2
Fabian Weisshaar <elnappo@users.noreply.github.com>
2020-12-17 15:37:57 +0100

    Update kstars from 3.4.3 to 3.5.0 (#95751)

--- Casks/kstars.rb
+++ Casks/kstars.rb
@@ -1,6 +1,6 @@
 cask "kstars" do
-  version "3.4.3"
-  sha256 "6bf27a3c97a81b520783d5ea864faa9ee3343ed5aa6e1a38f88f49e2e829375b"
+  version "3.5.0"
+  sha256 "9c848145ae76aa07f5feb102911a0be07e08642935a60ee3c7bd3fcc39698a71"
 
   url "https://www.indilib.org/jdownloads/kstars/kstars-#{version}.dmg",
       verified: "indilib.org/jdownloads/kstars/"
