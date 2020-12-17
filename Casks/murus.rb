fcfe677663cee5bc90ec986bfac1325fd1186923
Alexander Ronald Altman <alexanderaltman@me.com>
2020-12-16 17:34:50 -0800

    Update murus from 2.0.4 to 2.0.5

--- Casks/murus.rb
+++ Casks/murus.rb
@@ -1,6 +1,6 @@
 cask "murus" do
-  version "2.0.4"
-  sha256 "6765f891e8b2fc2a72003f14675884465c9956ecf4d9dd6d87ef2dcb9b34ae6b"
+  version "2.0.5"
+  sha256 "1076deda7f79293b8a36472a8d5b17c72d4842d2996cb7dfd4c4724168047991"
 
   url "https://github.com/TheMurusTeam/Murus/releases/download/v#{version}/murus-#{version}.zip",
       verified: "github.com/TheMurusTeam/Murus/"
