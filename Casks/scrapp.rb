de5f1c79e8a983f1f31c81b5a22e71f809c5243c
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-18 16:13:15 +0800

    Update scrapp.rb

--- Casks/scrapp.rb
+++ Casks/scrapp.rb
@@ -2,7 +2,8 @@ cask "scrapp" do
   version "1.6"
   sha256 "fdc29a6f404d88b17cc73c2bccb0bde782be1f86d8d165d3f9b4c3b83f6fdf61"
 
-  url "https://scrapp.me/download/mac"
+  url "https://scrap.blob.core.windows.net/download/#{version}/Scrapp.zip"
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://scrapp.me/download/mac"
   name "Scrapp"
   desc "Screenshot tool with cloud storage"
   homepage "https://scrapp.me/"
@@ -10,7 +11,7 @@ cask "scrapp" do
   app "Scrapp.app"
 
   zap trash: [
-    "/Users/daniel/Library/Caches/com.noArt.Scrapp",
-    "/Users/daniel/Library/Application Support/com.noArt.Scrapp",
+    "~/Library/Application Support/com.noArt.Scrapp",
+    "~/Library/Caches/com.noArt.Scrapp",
   ]
 end
