0eea150d647f82f2d80e0ec5d4d8045258ec5dcc
Jochen Schalanda <jochen@schalanda.name>
2020-12-18 10:22:07 +0100

    Update fly from 6.5.1 to 6.7.2 (#95826)
    * Update fly from 6.5.1 to 6.7.2
    
    * Fix description
    
    > Error: Description should start with a capital letter.
    > Error: Description shouldn't start with the cask name.

--- Casks/fly.rb
+++ Casks/fly.rb
@@ -1,10 +1,11 @@
 cask "fly" do
-  version "6.5.1"
-  sha256 "17b87c775dda5106118f080748b3526de88859c4c0fd7bf0d5a90ea4e4174569"
+  version "6.7.2"
+  sha256 "e095e0d631cd42a28b63e7101819840bad1b255d8b6d31ceb1fc0220c055605d"
 
   url "https://github.com/concourse/concourse/releases/download/v#{version}/fly-#{version}-darwin-amd64.tgz"
   appcast "https://github.com/concourse/concourse/releases.atom"
   name "fly"
+  desc "Official CLI tool for Concourse CI"
   homepage "https://github.com/concourse/concourse"
 
   binary "fly"
