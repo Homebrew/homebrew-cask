5531a4afe8571e549c109cf124cb36887fedd6a0
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-18 17:19:40 +0800

    Update tradingview.rb

--- Casks/tradingview.rb
+++ Casks/tradingview.rb
@@ -1,10 +1,10 @@
 cask "tradingview" do
   version "1.0.0-beta.1"
-  sha256 :no_check
+  sha256 "3aa3829f41e0e2891cf2c9bf247f6a3faf2be80bf779bbf6e8f1046e05418527"
 
   url "https://tvd-packages.tradingview.com/beta/#{version}/win32/x64/TradingView.dmg"
   name "TradingView Desktop Beta"
-  desc "Desktop application client based on Electron"
+  desc "Desktop client for TradingView"
   homepage "https://www.tradingview.com/blog/en/tradingview-desktop-beta-is-out-now-21962/"
 
   app "TradingView.app"
