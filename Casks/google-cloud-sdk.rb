56fa9bdd9730fe819cad46e77ff1efc98474039d
Evan <398607+xaocon@users.noreply.github.com>
2020-12-18 04:16:22 -0500

    [google-cloud-sdk] Get rid of python@3.8 requirement (#95807)
    * Get rid of python@3.8 requirment
    
    google-cloud-sdk now works with python 3.9. There is no need for a dep
    on 3.8 anymore.
    
    * quick fix for docs for fish shell
    
    Co-authored-by: Evan Pitstick <epitstick@paloaltonetworks.com>

--- Casks/google-cloud-sdk.rb
+++ Casks/google-cloud-sdk.rb
@@ -7,8 +7,6 @@ cask "google-cloud-sdk" do
   desc "Set of tools to manage resources and applications hosted on Google Cloud"
   homepage "https://cloud.google.com/sdk/"
 
-  depends_on formula: "python@3.8"
-
   stage_only true
 
   postflight do
@@ -16,8 +14,7 @@ cask "google-cloud-sdk" do
                    args: [
                      "--usage-reporting", "false", "--bash-completion", "false", "--path-update", "false",
                      "--rc-path", "false", "--quiet"
-                   ],
-                   env:  { "CLOUDSDK_PYTHON" => "#{HOMEBREW_PREFIX}/opt/python@3.8/libexec/bin/python" }
+                   ]
   end
 
   # Not actually necessary, since it would be deleted anyway.
@@ -28,17 +25,14 @@ cask "google-cloud-sdk" do
     #{token} is installed at #{staged_path}/#{token}. Add your profile:
 
       for bash users
-        export CLOUDSDK_PYTHON="#{HOMEBREW_PREFIX}/opt/python@3.8/libexec/bin/python"
         source "#{staged_path}/#{token}/path.bash.inc"
         source "#{staged_path}/#{token}/completion.bash.inc"
 
       for zsh users
-        export CLOUDSDK_PYTHON="#{HOMEBREW_PREFIX}/opt/python@3.8/libexec/bin/python"
         source "#{staged_path}/#{token}/path.zsh.inc"
         source "#{staged_path}/#{token}/completion.zsh.inc"
 
       for fish users
-        set -g -x "CLOUDSDK_PYTHON" "#{HOMEBREW_PREFIX}/opt/python@3.8/libexec/bin/python"
         source "#{staged_path}/#{token}/path.fish.inc"
   EOS
 end
