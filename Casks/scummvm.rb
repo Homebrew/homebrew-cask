87b95751b3adacced11d47417f2fba193fba536e
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-18 16:31:24 +0800

    Update scummvm.rb

--- Casks/scummvm.rb
+++ Casks/scummvm.rb
@@ -1,6 +1,6 @@
 cask "scummvm" do
   version "2.2.0"
-  sha256 "4d9721b97a3adc2e7fd956676284bf9900f48d2aaeda7f2a11a45c9c95e0fe63"
+  sha256 "fbe06e1d2ef271d44bb4f2a37340e49c0470becb9a3354b0e0f4eb4d27830a2c"
 
   url "https://scummvm.org/frs/scummvm/#{version}/scummvm-#{version}-macosx.dmg"
   appcast "https://www.scummvm.org/downloads/"
