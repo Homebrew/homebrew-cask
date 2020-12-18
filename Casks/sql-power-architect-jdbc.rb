2694019f139f3b2ae82d57c6845cb4f3a3c10613
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-18 17:28:59 +0800

    Update sql-power-architect-jdbc.rb

--- Casks/sql-power-architect-jdbc.rb
+++ Casks/sql-power-architect-jdbc.rb
@@ -2,10 +2,11 @@ cask "sql-power-architect-jdbc" do
   version "1.0.9"
   sha256 "dde6bbcb8327f95b36ad9aab785fda49d153fc6b1e9ff6739c206a6e90f49d09"
 
-  url "http://www.bestofbi.com/downloads/architect/#{version}/SQL-Power-Architect-OSX-#{version}.tar.gz"
+  url "http://www.bestofbi.com/downloads/architect/#{version}/SQL-Power-Architect-OSX-#{version}.dmg"
   appcast "http://www.bestofbi.com/page/architect_download_os"
   name "SQL Power Architect Community edition"
+  desc "Data warehouse modeling tool"
   homepage "http://www.bestofbi.com/page/architect"
 
-  app "SQL Power Architect JDBC.app"
+  app "SQL Power Architect.app"
 end
