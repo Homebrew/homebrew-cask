a0ad367a3168b7d36d9427b877040350e6188249
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-18 17:33:10 +0800

    Update sql-power-architect-jdbc.rb

--- Casks/sql-power-architect-jdbc.rb
+++ Casks/sql-power-architect-jdbc.rb
@@ -1,6 +1,6 @@
 cask "sql-power-architect-jdbc" do
   version "1.0.9"
-  sha256 "dde6bbcb8327f95b36ad9aab785fda49d153fc6b1e9ff6739c206a6e90f49d09"
+  sha256 "2936e0fdff0cecf4c3a4d111d2b5e7e26977c366a2b7905b9f28210e3f39f3e4"
 
   url "http://www.bestofbi.com/downloads/architect/#{version}/SQL-Power-Architect-OSX-#{version}.dmg"
   appcast "http://www.bestofbi.com/page/architect_download_os"
