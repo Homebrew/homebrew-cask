9096e8bee3e02ed4f5f68c1ff445d9f9e2cf8977
Willie <willie@accurics.com>
2020-12-18 14:29:57 -0800

    Update accurics from 1.0.4 to 1.0.11 (#95894)

--- Casks/accurics.rb
+++ Casks/accurics.rb
@@ -1,11 +1,12 @@
 cask "accurics" do
-  version "1.0.4"
-  sha256 "5d12405d22049206ca19f45aef9376ae3095f3b9bb5c13b4b7c7c5f7fa61396c"
+  version "1.0.11"
+  sha256 "953ac9f9d029782c7208bed4e19429e30bcd233b70d0f7dfa01cbd273bc5e848"
 
   url "https://downloads.accurics.com/cli/#{version}/accurics-cli.dmg"
   appcast "https://downloads.accurics.com/cli/#{version}/accurics-cli.yml",
           must_contain: version
   name "Accurics CLI"
+  desc "Security and compliance for Infrastructure as Code"
   homepage "https://www.accurics.com/"
 
   binary "accurics"
