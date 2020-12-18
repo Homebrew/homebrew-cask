8225265215e205d9c6c0423d6f8eb8d3044ff31b
suschizu <brigitte@corecode.io>
2020-12-18 02:23:53 +0100

    Update navicat-premium.rb from 15.0.22 to 15.0.24 (#95679)
    * Update navicat-premium.rb
    
    * Update navicat-premium.rb
    
    * Update navicat-premium.rb

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -1,13 +1,13 @@
 cask "navicat-premium" do
-  version "15.0.22"
+  version "15.0.24"
 
   language "zh-CN" do
-    sha256 "bd35c65e1178084b9c2c2385607272533edf0a86c332c631dbd818a76aafdba9"
-    url "http://download.navicat.com.cn/download/navicat#{version.major_minor.no_dots}_premium_cs.dmg"
+    sha256 "b445b658654e2fa32e788f8175c7bdf4da87be97033b61eae7664a4deff9a9dc"
+    url "https://download.navicat.com.cn/download/navicat#{version.major_minor.no_dots}_premium_cs.dmg"
     homepage "https://www.navicat.com.cn/products/navicat-premium"
   end
   language "en", default: true do
-    sha256 "dfff661abcab44af98bd80a403c7bb25f751d00d4dc73d2f21db86bd540478e4"
+    sha256 "0df19642f430db7d9032faac3fb00d9a2b040754f776ec4ae8da719a7c52437b"
     url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_premium_en.dmg"
     appcast "https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20Premium&appLang=en"
     homepage "https://www.navicat.com/products/navicat-premium"
