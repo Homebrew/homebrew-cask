873fdbb848975549c62f07535b913a5a7221b960
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-19 11:31:13 -0800

    Update busycal from 3.11.1-2020-11-15-05-54 to 3.12.0-2020-12-19-10-57 (#96077)

--- Casks/busycal.rb
+++ Casks/busycal.rb
@@ -1,6 +1,6 @@
 cask "busycal" do
-  version "3.11.1-2020-11-15-05-54"
-  sha256 "ae039923ed462308264376ab69bedbcbe1dfe5d860799f494d9a4dfc0737e891"
+  version "3.12.0-2020-12-19-10-57"
+  sha256 "02ac97e7c66486e700770238355d6e1eb20c72dda6e58d051e144f11eeca7f6b"
 
   url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bcl-#{version}.zip",
       verified: "7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/"
