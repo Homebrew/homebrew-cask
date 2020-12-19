ea4422abbb10928cea2c1079ee3a8c70794fe850
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-19 11:37:25 -0800

    Update streamlabs-obs from 0.26.1 to 0.26.2 (#96083)

--- Casks/streamlabs-obs.rb
+++ Casks/streamlabs-obs.rb
@@ -1,6 +1,6 @@
 cask "streamlabs-obs" do
-  version "0.26.1"
-  sha256 "d0b1e95a959ff406a8defd654d3fb2ede4431df6c86b6a0fcdb280bace25af59"
+  version "0.26.2"
+  sha256 "1390bd9e565d9d8d493fe6a008bf46b226a582ec0c669c464dad8c4ce8289038"
 
   url "https://slobs-cdn.streamlabs.com/Streamlabs+OBS-#{version}.dmg"
   appcast "https://slobs-cdn.streamlabs.com/latest-mac.yml"
