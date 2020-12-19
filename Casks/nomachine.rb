8d45be4d720e9f69b3d2562f467a0c26024fdca3
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-19 11:35:44 -0800

    Update nomachine from 7.0.208_4 to 7.0.209_4 (#96080)

--- Casks/nomachine.rb
+++ Casks/nomachine.rb
@@ -1,6 +1,6 @@
 cask "nomachine" do
-  version "7.0.208_4"
-  sha256 "0ada3811c004da1865a20b19975a08ea06b4feb4a9186d570eb441acdafef39d"
+  version "7.0.209_4"
+  sha256 "19d334f9665e2ba62249a8af3266598ed7612cb31b487abb7f1fb082f5b2f630"
 
   url "https://download.nomachine.com/download/#{version.major_minor}/MacOSX/nomachine_#{version}.dmg"
   appcast "https://www.nomachine.com/download/download&id=7"
