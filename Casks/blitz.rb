081062790577789052f3c57954bc4f1a22a6f4f5
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-19 11:22:50 -0800

    Update blitz from 1.13.54 to 1.13.55 (#96072)

--- Casks/blitz.rb
+++ Casks/blitz.rb
@@ -1,6 +1,6 @@
 cask "blitz" do
-  version "1.13.54"
-  sha256 "465cee363fd054a602d2916539f5b1b60bbdc32ccd106f8cda3777fde7681a71"
+  version "1.13.55"
+  sha256 "3ff9633fe44369a10751654a392d7934fd477cd3f2cf1aafa30f4c1632b8f088"
 
   url "https://dl.blitz.gg/download/Blitz-#{version}.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://dl.blitz.gg/download/mac"
