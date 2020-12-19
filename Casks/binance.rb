7df3cb232abfaf4a95cc927743bddf227325ca06
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-19 11:20:35 -0800

    Update binance from 1.9.0 to 1.9.1 (#96071)

--- Casks/binance.rb
+++ Casks/binance.rb
@@ -1,6 +1,6 @@
 cask "binance" do
-  version "1.9.0"
-  sha256 "a222c2c7ce4bcd57dd678afac1f962c3744c89b5c5e5efabad0f6af2558fca65"
+  version "1.9.1"
+  sha256 "1293121cfc5cccbbf287db470902af57494e3ff145c6e2cd5994ae12beb97c96"
 
   url "https://ftp.binance.com/electron-desktop/mac/production/binance-#{version}.dmg"
   appcast "https://ftp.binance.com/electron-desktop/mac/production/latest-mac.yml"
