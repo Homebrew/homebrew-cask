7788235794fef2bb3bd6606fce4b271554245c50
Tarık Taha UYGUN <ttuygun@yahoo.com>
2020-12-19 18:58:20 +0300

    Update to 1.7.0 (#96045)

--- Casks/swimat.rb
+++ Casks/swimat.rb
@@ -1,8 +1,8 @@
 cask "swimat" do
-  version "1.6.2"
-  sha256 "0bd8f283d5d396efe8a479910264ade87bf814cf51e65e46bf58c521206a7663"
+  version "1.7.0"
+  sha256 "adcf450d98c247fe4c45c68353f19afca3e2039c8431ec080f14000fe9cc36bf"
 
-  url "https://github.com/Jintin/Swimat/releases/download/v#{version}/Swimat.zip"
+  url "https://github.com/Jintin/Swimat/releases/download/#{version}/Swimat.zip"
   appcast "https://github.com/Jintin/Swimat/releases.atom"
   name "Swimat"
   desc "Xcode formatter plug-in for Swift code"
