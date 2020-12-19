b79c479896b7ace2a6cc756a75c92826547e490d
Leandro Facchinetti <git@leafac.com>
2020-12-19 20:59:04 +0000

    Update reaper from 6.18 to 6.19 (#96090)

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,12 +1,12 @@
 cask "reaper" do
-  version "6.18.0,6.18"
+  version "6.19.0,6.19"
 
   if MacOS.version <= :mojave
-    sha256 "4889ce35bf1056b2016ecc247935c20ca8508029b288d51b4fbf9d23010a528a"
+    sha256 "0d3eeaee7a014dff45c3343b2763b21d1afb897473b0c9bf160702522e77f0f8"
 
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.after_comma.no_dots}_x86_64.dmg"
   else
-    sha256 "a89222a082dbd3a5e49b67606d54c535956acb1af3b0380cd266c90d620b3998"
+    sha256 "1c04ed6d0dddfb3f0c9890293044050801aaf55ca8c55952db03065275ee8e3f"
 
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.after_comma.no_dots}_x86_64_catalina.dmg"
   end
