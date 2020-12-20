ec13eb2fa75d05040d30d5e075aa639a430cbf02
Randall <ran.dall@icloud.com>
2020-12-20 09:31:53 -0800

    Update eset-cyber-security-pro from 6.10.460 to 6.10.460.1

--- Casks/eset-cyber-security-pro.rb
+++ Casks/eset-cyber-security-pro.rb
@@ -1,5 +1,5 @@
 cask "eset-cyber-security-pro" do
-  version "6.10.460"
+  version "6.10.460.1"
   sha256 :no_check
 
   url "https://download.eset.com/com/eset/apps/home/ess/mac/latest/eset_cybersecurity_pro_en.dmg"
