b57446c14d9fcb07dd236be16d794ead2d169246
Michael Wojcik <michael-wojcik@users.noreply.github.com>
2020-12-20 14:49:55 -0600

    electronmail 4.10.0

--- Casks/electronmail.rb
+++ Casks/electronmail.rb
@@ -1,8 +1,8 @@
 cask "electronmail" do
-  version "4.9.2"
+  version "4.10.0"
   sha256 "f768bc14fb7c937650eaac603b68ba97163972b2febfefa135d2b2da85c30a18"
 
-  url "https://github.com/vladimiry/ElectronMail/releases/download/v#{version}/electron-mail-#{version}-mac-mojave.dmg"
+  url "https://github.com/vladimiry/ElectronMail/releases/download/v#{version}/electron-mail-#{version}-mac.dmg"
   appcast "https://github.com/vladimiry/ElectronMail/releases.atom"
   name "ElectronMail"
   desc "Unofficial ProtonMail Desktop App"
