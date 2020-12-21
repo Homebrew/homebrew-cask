95722cda7b9e302aa3a3ec15960db3a4841c2689
Kid <44045911+kidonng@users.noreply.github.com>
2020-12-21 22:48:46 +0800

    wireshark: add new zap entry

--- Casks/wireshark.rb
+++ Casks/wireshark.rb
@@ -58,6 +58,7 @@ cask "wireshark" do
     "~/Library/Cookies/org.wireshark.Wireshark.binarycookies",
     "~/Library/Preferences/org.wireshark.Wireshark.plist",
     "~/Library/Saved Application State/org.wireshark.Wireshark.savedState",
+    "~/Library/HTTPStorages/org.wireshark.Wireshark.binarycookies",
     "~/.config/wireshark",
   ]
 end
