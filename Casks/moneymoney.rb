04923ec18c075581a1b356288c5ab00a77081de9
Markus Reiter <me@reitermark.us>
2020-12-21 22:30:44 +0100

    moneymoney: add livecheck (#96471)

--- Casks/moneymoney.rb
+++ Casks/moneymoney.rb
@@ -3,10 +3,14 @@ cask "moneymoney" do
   sha256 :no_check
 
   url "https://service.moneymoney-app.com/1/MoneyMoney.zip"
-  appcast "https://service.moneymoney-app.com/1/Appcast.xml"
   name "MoneyMoney"
   desc "German banking and financial management software"
   homepage "https://moneymoney-app.com/"
+
+  livecheck do
+    url "https://service.moneymoney-app.com/1/Appcast.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
 
