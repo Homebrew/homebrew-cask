c2e9719f57a25a7f45ef1c62e8d790a0d38bd22d
Markus Reiter <me@reitermark.us>
2020-12-21 19:24:37 +0100

    lunar: add livecheck (#96406)

--- Casks/lunar.rb
+++ Casks/lunar.rb
@@ -3,10 +3,14 @@ cask "lunar" do
   sha256 "88bf404e03364eaf2b33aad449cddc1d80d9e21b5f99e35db2bc2db636b9c48d"
 
   url "https://lunar.fyi/download/#{version}"
-  appcast "https://lunar.fyi/appcast.xml"
   name "Lunar"
   desc "Adaptive brightness for external displays"
   homepage "https://lunar.fyi/"
+
+  livecheck do
+    url "https://lunar.fyi/appcast.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :yosemite"
