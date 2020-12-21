72feae6a554e15554cce8b4e10b14eef8e741936
Markus Reiter <me@reitermark.us>
2020-12-21 23:11:26 +0100

    quicken: add livecheck

--- Casks/quicken.rb
+++ Casks/quicken.rb
@@ -3,10 +3,14 @@ cask "quicken" do
   sha256 "8d067db5496fef10c187b94e7c0fc1dd663e17133608c686f8f90351096f4aed"
 
   url "https://download.quicken.com/mac/Quicken/001/Release/031A96D9-EFE6-4520-8B6A-7F465DDAA3E4/Quicken-#{version.after_comma}/Quicken-#{version.after_comma}.zip"
-  appcast "https://download.quicken.com/mac/Quicken/001/Release/031A96D9-EFE6-4520-8B6A-7F465DDAA3E4/appcast.xml"
   name "Quicken"
   desc "Personal finance mananger"
   homepage "https://www.quicken.com/mac"
+
+  livecheck do
+    url "https://download.quicken.com/mac/Quicken/001/Release/031A96D9-EFE6-4520-8B6A-7F465DDAA3E4/appcast.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :el_capitan"
 
