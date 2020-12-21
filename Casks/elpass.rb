444d31fb7448934fe802e61a355af7d38d786a94
Markus Reiter <me@reitermark.us>
2020-12-21 17:03:10 +0100

    elpass: add livecheck (#96282)

--- Casks/elpass.rb
+++ Casks/elpass.rb
@@ -3,10 +3,14 @@ cask "elpass" do
   sha256 "25251036d6961aad54852f54f3606467903834153b68252b4264d9e6f2da3778"
 
   url "https://elpass.app/macos/Elpass-#{version.before_comma}-#{version.after_comma}.zip"
-  appcast "https://elpass.app/macos/appcast.xml"
   name "Elpass"
   desc "Password manager"
   homepage "https://elpass.app/"
+
+  livecheck do
+    url "https://elpass.app/macos/appcast.xml"
+    strategy :sparkle
+  end
 
   app "Elpass.app"
 end
