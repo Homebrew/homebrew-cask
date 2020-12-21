00d18e48654016a00c07c2c5b413627e99b9182f
Markus Reiter <me@reitermark.us>
2020-12-21 14:04:03 +0100

    flexiglass: add livecheck

--- Casks/flexiglass.rb
+++ Casks/flexiglass.rb
@@ -1,11 +1,15 @@
 cask "flexiglass" do
-  version "1.7.1-12833"
+  version "1.7.1,12833"
   sha256 "9ceb47ae0093646033960c9fc9c665b1bc59d46cd3baa8047dcb18b96ebd18cc"
 
-  url "https://downloads.nulana.com/flexiglass/Flexiglass-#{version}.zip"
-  appcast "https://downloads.nulana.com/flexiglass/appcast.xml"
+  url "https://downloads.nulana.com/flexiglass/Flexiglass-#{version.before_comma}-#{version.after_comma}.zip"
   name "Flexiglass"
   homepage "https://nulana.com/flexiglass/"
+
+  livecheck do
+    url "https://downloads.nulana.com/flexiglass/appcast.xml"
+    strategy :sparkle
+  end
 
   app "Flexiglass.app"
 end
