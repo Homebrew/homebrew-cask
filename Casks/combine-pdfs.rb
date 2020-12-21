22c39c8cbfbd07426c9bf598a56dc8508ec63cb1
Markus Reiter <me@reitermark.us>
2020-12-21 17:01:35 +0100

    combine-pdfs: add livecheck (#96216)

--- Casks/combine-pdfs.rb
+++ Casks/combine-pdfs.rb
@@ -3,9 +3,13 @@ cask "combine-pdfs" do
   sha256 "2fee2e174fd02525dc599e52d13d14f6c8b5f0a56c76e04c5821ad33d3801651"
 
   url "https://www.monkeybreadsoftware.de/Software/CombinePDFs/CombinePDFs#{version.no_dots}.zip"
-  appcast "https://www.monkeybreadsoftware.de/Software/CombinePDFs/appcast.xml"
   name "Combine PDFs"
   homepage "https://www.monkeybreadsoftware.de/Software/CombinePDFs.shtml"
+
+  livecheck do
+    url "https://www.monkeybreadsoftware.de/Software/CombinePDFs/appcast.xml"
+    strategy :sparkle
+  end
 
   app "Combine PDFs.app"
 end
