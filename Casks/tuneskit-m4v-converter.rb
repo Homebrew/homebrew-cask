16962c40ae1f79dd3a0a8b0ad9521d5bc9f5652e
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-21 14:20:59 +0800

    Update tuneskit-m4v-converter.rb

--- Casks/tuneskit-m4v-converter.rb
+++ Casks/tuneskit-m4v-converter.rb
@@ -3,7 +3,8 @@ cask "tuneskit-m4v-converter" do
   sha256 :no_check
 
   url "https://www.tuneskit.com/TunesKitforMac.dmg"
-  appcast "https://www.tuneskit.com/api/product/version?pid=201"
+  appcast "https://www.tuneskit.com/api/product/version?pid=201",
+          must_contain: version.major_minor_patch
   name "TunesKit M4V Converter"
   homepage "https://www.tuneskit.com/"
 
