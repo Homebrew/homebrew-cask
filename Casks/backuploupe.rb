00edb0ed07514854abbc6bfafb9f9d3bb4c59197
Markus Reiter <me@reitermark.us>
2020-12-21 16:27:01 +0100

    backuploupe: add livecheck (#96173)

--- Casks/backuploupe.rb
+++ Casks/backuploupe.rb
@@ -1,12 +1,16 @@
 cask "backuploupe" do
-  version "3.2.3"
+  version "3.2.3,2053"
   sha256 "2e36ae70078e36ddd0db400ba87eb70650ae862f8c7f1c94aecdbdc9c75a3b92"
 
-  url "https://www.soma-zone.com/download/files/BackupLoupe-#{version}.tar.bz2"
-  appcast "https://www.soma-zone.com/BackupLoupe/a/appcast-update-#{version.major}.xml"
+  url "https://www.soma-zone.com/download/files/BackupLoupe-#{version.before_comma}.tar.bz2"
   name "BackupLoupe"
   desc "Alternative GUI for Time Machine"
   homepage "https://www.soma-zone.com/BackupLoupe/"
+
+  livecheck do
+    url "https://www.soma-zone.com/BackupLoupe/a/appcast-update-#{version.major}.xml"
+    strategy :sparkle
+  end
 
   app "BackupLoupe.app"
 end
