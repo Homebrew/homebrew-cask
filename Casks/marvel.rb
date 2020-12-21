2b908d63f36012ab1c46f840e076c7bf1116addf
Markus Reiter <me@reitermark.us>
2020-12-21 21:02:13 +0100

    marvel: add livecheck (#96443)

--- Casks/marvel.rb
+++ Casks/marvel.rb
@@ -1,13 +1,17 @@
 cask "marvel" do
-  version "10.5"
+  version "10.5,4596"
   sha256 "8bb11ab765dcfdc1a514d87b728f235e647d97364113ca56376e42499718ad98"
 
-  url "https://storage.googleapis.com/sketch-plugin/#{version}/Marvel.zip",
+  url "https://storage.googleapis.com/sketch-plugin/#{version.before_comma}/Marvel.zip",
       verified: "storage.googleapis.com/sketch-plugin/"
-  appcast "https://storage.googleapis.com/sketch-plugin/app.xml"
   name "Marvel"
   desc "Prototyping, testing and handoff tools"
   homepage "https://marvelapp.com/"
+
+  livecheck do
+    url "https://storage.googleapis.com/sketch-plugin/app.xml"
+    strategy :sparkle
+  end
 
   app "Marvel.app"
 end
