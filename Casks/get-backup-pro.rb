4d5e292563fd77e8b67fd311c5a102405345d628
Markus Reiter <me@reitermark.us>
2020-12-21 17:06:51 +0100

    get-backup-pro: add livecheck (#96308)

--- Casks/get-backup-pro.rb
+++ Casks/get-backup-pro.rb
@@ -1,13 +1,17 @@
 cask "get-backup-pro" do
-  version "3.6.1"
+  version "3.6.1,1536"
   sha256 "89a26c8b83ed621761a051eab879ebb3afdb0bfb1d16efc371b6b06d370986f1"
 
   url "https://belightsoft.s3.amazonaws.com/updates/Get+Backup+Pro+#{version.major}.zip",
       verified: "belightsoft.s3.amazonaws.com/updates/"
-  appcast "https://www.belightsoft.com/download/updates/appcast_getbackup_pro#{version.major}.xml"
   name "Get Backup Pro #{version.major}"
   desc "Backup software with folder synchronization"
   homepage "https://www.belightsoft.com/products/getbackup/"
+
+  livecheck do
+    url "https://www.belightsoft.com/download/updates/appcast_getbackup_pro#{version.major}.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
 
