0245e7226fc6f0bf7100f2c638da5f43922b0c04
Alexander Lauster <alexander@macwinlin.de>
2020-12-21 08:53:06 +0100

    Update bettertouchtool from 3.506-1644 to 3.508-1646 (#96089)
    * Update bettertouchtool from 3.506-1644 to 3.508-1646
    
    * Update bettertouchtool.rb
    
    Co-authored-by: Alexander Lauster <alex@Leda.localdomain>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -1,9 +1,9 @@
 cask "bettertouchtool" do
-  version "3.506-1644"
-  sha256 "97a9b11874ff60cb407b069f95b4feefd1aec7b574ffa5ad17ed555af7bd47e2"
+  version "3.508-1646"
+  sha256 "51b7c0ed3aacf0f9b3a6825dde1cbbdcdf9e1ceeb35e315db119014f1ffac81d"
 
   url "https://folivora.ai/releases/btt#{version}.zip"
-  appcast "https://updates.folivora.ai/appcast_manual.xml"
+  appcast "https://updates.folivora.ai/bettertouchtool_release_notes.html"
   name "BetterTouchTool"
   desc "Tool to customize input devices and automate computer systems"
   homepage "https://folivora.ai/"
