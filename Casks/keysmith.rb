a3257c554343b54a4f862d875b913044d1c35f4c
Markus Reiter <me@reitermark.us>
2020-12-21 17:44:27 +0100

    keysmith: add livecheck

--- Casks/keysmith.rb
+++ Casks/keysmith.rb
@@ -1,12 +1,16 @@
 cask "keysmith" do
-  version "1.8.2"
+  version "1.8.2,75"
   sha256 "1628e42d95c049bf7c0a309ce2812c6282025a6fd6b932b341f25416c77eeb55"
 
-  url "https://keysmith.app/versions/Keysmith-#{version}.dmg"
-  appcast "https://www.keysmith.app/versions/appcast.xml"
+  url "https://keysmith.app/versions/Keysmith-#{version.before_comma}.dmg"
   name "Keysmith"
   desc "Create custom keyboard shortcuts for anything"
   homepage "https://www.keysmith.app/"
+
+  livecheck do
+    url "https://www.keysmith.app/versions/appcast.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :catalina"
 
