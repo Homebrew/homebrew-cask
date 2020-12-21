f47e3e7b9a99e94f52c9fc3b31bd0b3105e65889
Markus Reiter <me@reitermark.us>
2020-12-21 20:47:29 +0100

    mi: add livecheck

--- Casks/mi.rb
+++ Casks/mi.rb
@@ -1,12 +1,16 @@
 cask "mi" do
-  version "3.5"
+  version "3.5,955"
   sha256 "9df5cae930b7b59b0022befdb69abcaff616bd4034f6612a24f3139fe01499a9"
 
-  url "https://www.mimikaki.net/download/mi#{version}.dmg"
-  appcast "https://www.mimikaki.net/download/appcast.xml"
+  url "https://www.mimikaki.net/download/mi#{version.before_comma}.dmg"
   name "mi"
   desc "Text editor"
   homepage "https://www.mimikaki.net/"
+
+  livecheck do
+    url "https://www.mimikaki.net/download/appcast.xml"
+    strategy :sparkle
+  end
 
   conflicts_with cask: "homebrew/cask-versions/mi-beta"
 
