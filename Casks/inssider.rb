6f4c7b182a1d0aa9a72b47fdbe2db6cd68a47a8c
Markus Reiter <me@reitermark.us>
2020-12-21 16:55:12 +0100

    inssider: add livecheck

--- Casks/inssider.rb
+++ Casks/inssider.rb
@@ -1,12 +1,16 @@
 cask "inssider" do
-  version "0.0.4.5,5"
+  version "0.0.4.5,8"
   sha256 "aae290a9e1c35db4b923ff6cf64332b1825ab82a848761e10debdddf3e3f9cfa"
 
-  url "https://metageek-desktop.nyc3.digitaloceanspaces.com/Installers/Mac/inSSIDer-#{version.after_comma}/inSSIDer.dmg",
-      verified: "nyc3.digitaloceanspaces.com/Installers/Mac/"
-  appcast "https://metageek-desktop.nyc3.digitaloceanspaces.com/Installers/Mac/inSSIDer-#{version.after_comma}/inssider_#{version.after_comma}_mac_appcast.xml"
+  url "https://metageek.link/inssider-5-mac-release-folder/inSSIDer-#{version.before_comma}.dmg",
+      verified: "metageek.link/"
   name "inSSIDer"
   homepage "https://www.metageek.com/products/inssider/"
+
+  livecheck do
+    url "https://metageek.link/inssider-5-mac-update-xml"
+    strategy :sparkle
+  end
 
   app "inSSIDer.app"
 end
