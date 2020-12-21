52c980a7ba904b5b604bb287097166e60c4c3e9f
Markus Reiter <me@reitermark.us>
2020-12-21 22:44:11 +0100

    permute: add livecheck

--- Casks/permute.rb
+++ Casks/permute.rb
@@ -3,10 +3,14 @@ cask "permute" do
   sha256 "62cf44e4b99e7f8a960d4d44928795888e80d7468b8a8dd23bcdbed81f7c0c26"
 
   url "https://trial.charliemonroe.net/permute/v#{version.major}/Permute_#{version.major}_#{version.after_comma}.dmg"
-  appcast "https://trial.charliemonroe.net/permute/updates_#{version.major}.xml"
   name "Permute"
   desc "Converts and edits video, audio or image files"
   homepage "https://software.charliemonroe.net/permute.php"
+
+  livecheck do
+    url "https://trial.charliemonroe.net/permute/updates_#{version.major}.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :el_capitan"
