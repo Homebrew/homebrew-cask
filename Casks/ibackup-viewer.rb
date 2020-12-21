46c03cfb6c569a15998c499e4c083b29e0f2fa30
Markus Reiter <me@reitermark.us>
2020-12-21 15:09:15 +0100

    ibackup-viewer: add livecheck

--- Casks/ibackup-viewer.rb
+++ Casks/ibackup-viewer.rb
@@ -3,9 +3,13 @@ cask "ibackup-viewer" do
   sha256 :no_check
 
   url "https://www.imactools.com/download/iBackupViewer.dmg"
-  appcast "https://www.imactools.com/update/ibackupviewer.xml"
   name "iBackup Viewer"
   homepage "https://www.imactools.com/iphonebackupviewer/"
+
+  livecheck do
+    url "https://www.imactools.com/update/ibackupviewer.xml"
+    strategy :sparkle
+  end
 
   app "iBackup Viewer.app"
 end
