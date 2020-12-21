cb27857fc04d302bd0b2cc71de149ac966a56eda
Markus Reiter <me@reitermark.us>
2020-12-21 17:00:40 +0100

    cloudapp: add livecheck (#96209)

--- Casks/cloudapp.rb
+++ Casks/cloudapp.rb
@@ -1,12 +1,16 @@
 cask "cloudapp" do
-  version "6.4.2.2192"
+  version "6.4.2,2192"
   sha256 "51f9c335f2592d12d81a06741798ca3cb6f7f968850814f02d1774290b1d7e8b"
 
-  url "http://downloads.getcloudapp.com/mac/CloudApp-#{version}.zip"
-  appcast "https://d2plwz9jdz9z5d.cloudfront.net/mac/latest/appcast.xml"
+  url "https://downloads.getcloudapp.com/mac/CloudApp-#{version.before_comma}.#{version.after_comma}.zip"
   name "CloudApp"
   desc "Visual communication platform"
   homepage "https://www.getcloudapp.com/"
+
+  livecheck do
+    url "https://d2plwz9jdz9z5d.cloudfront.net/mac/latest/appcast.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :mojave"
