3e8e98720b57077957d6f6f18a8dd6f5f4181fca
Markus Reiter <me@reitermark.us>
2020-12-21 17:02:26 +0100

    astropad-studio: add livecheck (#96168)

--- Casks/astropad-studio.rb
+++ Casks/astropad-studio.rb
@@ -1,12 +1,16 @@
 cask "astropad-studio" do
-  version "3.5.0"
+  version "3.5.0,3180"
   sha256 "be952da8a7089186ae413a4b1dd173ae473ab572cd611d5a0a38b062b58082ff"
 
-  url "https://s3.amazonaws.com/astropad.com/downloads/AstropadStudio-#{version}.dmg",
+  url "https://s3.amazonaws.com/astropad.com/downloads/AstropadStudio-#{version.before_comma}.dmg",
       verified: "s3.amazonaws.com/astropad.com/"
-  appcast "https://s3.amazonaws.com/astropad.com/downloads/studio-sparkle.xml"
   name "Astropad Studio"
   homepage "https://astropad.com/"
+
+  livecheck do
+    url "https://s3.amazonaws.com/astropad.com/downloads/studio-sparkle.xml"
+    strategy :sparkle
+  end
 
   app "Astropad Studio.app"
 
