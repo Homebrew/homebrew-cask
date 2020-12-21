0f8c2b9eafa1d8485848b618c731a481f33e16c9
Markus Reiter <me@reitermark.us>
2020-12-21 21:36:27 +0100

    movist-pro: add livecheck

--- Casks/movist-pro.rb
+++ Casks/movist-pro.rb
@@ -1,13 +1,17 @@
 cask "movist-pro" do
-  version "2.6.3"
+  version "2.6.3,167"
   sha256 "0715d43b095abf7358264334b53a14e3444874e3eaf4c0beae0576ab1c51a284"
 
-  url "https://s3.ap-northeast-2.amazonaws.com/update.cocoable.com/MovistPro_#{version}.app.zip",
+  url "https://s3.ap-northeast-2.amazonaws.com/update.cocoable.com/MovistPro_#{version.before_comma}.app.zip",
       verified: "s3.ap-northeast-2.amazonaws.com/update.cocoable.com/"
-  appcast "https://s3.ap-northeast-2.amazonaws.com/update.cocoable.com/appcast.xml"
   name "Movist Pro"
   desc "Media player"
   homepage "https://movistprime.com/"
+
+  livecheck do
+    url "https://s3.ap-northeast-2.amazonaws.com/update.cocoable.com/appcast.xml"
+    strategy :sparkle
+  end
 
   app "Movist Pro.app"
 end
