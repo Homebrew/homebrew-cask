c3b70bb542827babb442c55180da9328b82942c6
jcbot <jcb@leipert.io>
2020-12-21 12:20:15 +0100

    Update pycharm to 2020.3.1,203.6682.86 (#96270)

--- Casks/pycharm.rb
+++ Casks/pycharm.rb
@@ -1,6 +1,6 @@
 cask "pycharm" do
-  version "2020.3,203.5981.165"
-  sha256 "6b3f1f716657c13bfd87fc9850ef63873ca0bf3ce21a81eadab95c6592c27be5"
+  version "2020.3.1,203.6682.86"
+  sha256 "597d062024f6f41142a055733f0160061acf982c6cdb53becc2489787b6d9897"
 
   url "https://download.jetbrains.com/python/pycharm-professional-#{version.before_comma}.dmg"
   appcast "https://data.services.jetbrains.com/products/releases?code=PCP&latest=true&type=release"
