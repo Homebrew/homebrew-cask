355946f03cb925d57d6879cb705861dc004a2e81
Markus Reiter <me@reitermark.us>
2020-12-21 20:18:45 +0100

    mactracker: add livecheck (#96427)

--- Casks/mactracker.rb
+++ Casks/mactracker.rb
@@ -3,10 +3,14 @@ cask "mactracker" do
   sha256 "b0f9200c5fb43999fcc0e0138ccddf5ee24cbcfae6e25dcbc5a5ebb8a8ec1646"
 
   url "https://www.mactracker.ca/downloads/Mactracker_#{version}.zip"
-  appcast "https://update.mactracker.ca/appcast-b.xml"
   name "Mactracker"
   desc "Detailed information on every Apple product ever made"
   homepage "https://mactracker.ca/"
+
+  livecheck do
+    url "https://update.mactracker.ca/appcast-b.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
 
