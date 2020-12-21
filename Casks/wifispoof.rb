2dbca16a85a26d45e7a76db33f987fa251600cba
CoreCode <core-code@users.noreply.github.com>
2020-12-21 16:20:18 +0100

    Update wifispoof from 3.5 to 3.5.1 (#96357)

--- Casks/wifispoof.rb
+++ Casks/wifispoof.rb
@@ -1,6 +1,6 @@
 cask "wifispoof" do
-  version "3.5"
-  sha256 "c8dc13935de7024a2920e163d3a7977e5d9b74629ce30fc3a3b4ff35ade516eb"
+  version "3.5.1"
+  sha256 "c41db41250acfddef838bcda0e808e6f855827d23955fdac8e7a979bb7694512"
 
   url "https://sweetpproductions.com/products/wifispoof#{version.major}/WiFiSpoof#{version.major}.dmg",
       verified: "sweetpproductions.com/products/"
