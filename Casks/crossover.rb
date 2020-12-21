116c2a076bd45cae959893ee0dc4415a9c5a18fb
Markus Reiter <me@reitermark.us>
2020-12-21 18:54:49 +0100

    crossover: add livecheck (#96229)

--- Casks/crossover.rb
+++ Casks/crossover.rb
@@ -1,12 +1,16 @@
 cask "crossover" do
-  version "20.0.2"
+  version "20.0.2.33166"
   sha256 "3bdae0c3647266f6b40d2e5db7862d1054ff940d80c6306d3be3d0b7153ed689"
 
-  url "https://media.codeweavers.com/pub/crossover/cxmac/demo/crossover-#{version}.zip"
-  appcast "https://www.codeweavers.com/xml/versions/cxmac.xml"
+  url "https://media.codeweavers.com/pub/crossover/cxmac/demo/crossover-#{version.major_minor_patch}.zip"
   name "CrossOver"
   desc "Tool to run Windows software"
   homepage "https://www.codeweavers.com/products/crossover-mac/"
+
+  livecheck do
+    url "https://www.codeweavers.com/xml/versions/cxmac.xml"
+    strategy :sparkle
+  end
 
   app "CrossOver.app"
 
