a576d615e758e49bfb3905ab36eb4dc506bf5a72
Markus Reiter <me@reitermark.us>
2020-12-21 18:55:55 +0100

    displays: add livecheck (#96242)

--- Casks/displays.rb
+++ Casks/displays.rb
@@ -1,12 +1,16 @@
 cask "displays" do
-  version "1.9.7"
+  version "1.9.7,108"
   sha256 "15cbc743f29f241d87b6c79dc53a2e3c3b1294ede1e00dfc3723a7db7a9c4088"
 
-  url "https://www.jibapps.com/ressources/displays/files/Displays.#{version}.zip"
-  appcast "https://www.jibapps.com/ressources/displays/appcast.xml"
+  url "https://www.jibapps.com/ressources/displays/files/Displays.#{version.before_comma}.zip"
   name "Displays"
   desc "Monitor resolution and settings manager"
   homepage "https://www.jibapps.com/apps/displays/"
+
+  livecheck do
+    url "https://www.jibapps.com/ressources/displays/appcast.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
 
