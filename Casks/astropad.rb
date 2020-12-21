2486c5e5656467f4546f5f2f848cfe23ab379583
Markus Reiter <me@reitermark.us>
2020-12-21 17:01:20 +0100

    astropad: add livecheck (#96169)

--- Casks/astropad.rb
+++ Casks/astropad.rb
@@ -1,12 +1,16 @@
 cask "astropad" do
-  version "3.5.0"
+  version "3.5.0,3180"
   sha256 "cfdc0334b757cd34f7cec08523cd45e8874da6f5c5815d3f57a003a4ad8217bc"
 
-  url "https://downloads.astropad.com/standard/Astropad-#{version}.dmg"
-  appcast "https://s3.amazonaws.com/astropad.com/downloads/sparkle.xml"
+  url "https://downloads.astropad.com/standard/Astropad-#{version.before_comma}.dmg"
   name "Astropad"
   desc "Utility to turn an iPad into a drawing tablet"
   homepage "https://astropad.com/"
+
+  livecheck do
+    url "https://s3.amazonaws.com/astropad.com/downloads/sparkle.xml"
+    strategy :sparkle
+  end
 
   app "Astropad.app"
 
