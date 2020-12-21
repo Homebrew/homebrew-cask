c9862a0d72f24035d468c9e8bf7699bef2eb9920
suschizu <brigitte@corecode.io>
2020-12-21 15:39:01 +0100

    Update url and appcast in macmediakeyforwarder (#96339)

--- Casks/macmediakeyforwarder.rb
+++ Casks/macmediakeyforwarder.rb
@@ -2,10 +2,11 @@ cask "macmediakeyforwarder" do
   version "3.1"
   sha256 "176e672e9aa61564f06124eb0b6aedbfb04fae10dd3595479e85fba25c9d724a"
 
-  url "http://milgra.com/downloads/mmkf/MacMediaKeyForwarder#{version}.zip"
-  appcast "http://milgra.com/downloads/mmkf/"
+  url "https://github.com/milgra/macmediakeyforwarder/releases/download/#{version}/MacMediaKeyForwarder#{version}.zip",
+      verified: "github.com/milgra/macmediakeyforwarder/"
+  appcast "https://github.com/milgra/macmediakeyforwarder/releases"
   name "Mac Media Key Forwarder"
-  homepage "http://milgra.com/mac-media-key-forwarder.html"
+  homepage "http://milgra.com/"
 
   depends_on macos: ">= :sierra"
 
