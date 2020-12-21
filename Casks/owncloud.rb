15e9d1f5342117b2417df00d7570e16a9e0ef418
Christian Kraemer <christian.kraemer@gmail.com>
2020-12-21 20:04:49 +0100

    Update owncloud from 2.7.3.2877 to 2.7.4.2934 (#96417)

--- Casks/owncloud.rb
+++ Casks/owncloud.rb
@@ -1,6 +1,6 @@
 cask "owncloud" do
-  version "2.7.3.2877"
-  sha256 "de74292de46d833535842c071aa7f210aff96ce92e80ab30e2ae66a744fb32cc"
+  version "2.7.4.2934"
+  sha256 "07b9c8398d305375c8fe7f551c0b9d280933e92a63aed3b4b4b928ecbb71adbb"
 
   url "https://download.owncloud.com/desktop/ownCloud/stable/#{version}/mac/ownCloud-#{version}.pkg"
   appcast "https://github.com/owncloud/client/releases.atom"
