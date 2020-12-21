4bc6bd1d9596136f370f01812e5cac099028cd1e
Randall <17261190+ran-dall@users.noreply.github.com>
2020-12-21 11:04:14 -0800

    Update middle from 1.5.12 to 1.5.13 (#96420)

--- Casks/middle.rb
+++ Casks/middle.rb
@@ -1,6 +1,6 @@
 cask "middle" do
-  version "1.5.12"
-  sha256 "181d7a3cf3d946e8c97c00c66dc33dba391b3eda71e54c59aad812b8b1efafd5"
+  version "1.5.13"
+  sha256 "86be3b15c6566fcc45e99fdcc217c3fd234bd485f64a70ab2074007dc797f5de"
 
   url "https://middleclick.app/downloads/Middle#{version}.dmg"
   appcast "https://middleclick.app/downloads/updates.xml"
