e9a5b9a31cc8df36e4ceff1d9a30914e0e6ecbde
Markus Reiter <me@reitermark.us>
2020-12-21 16:26:02 +0100

    blocs: add livecheck (#96189)

--- Casks/blocs.rb
+++ Casks/blocs.rb
@@ -3,10 +3,14 @@ cask "blocs" do
   sha256 :no_check
 
   url "https://blocsapp.com/download/Blocs.zip"
-  appcast "https://blocsapp.com/update/v#{version.major}/info.xml"
   name "Blocs"
   desc "Visual web design software"
   homepage "https://blocsapp.com/"
+
+  livecheck do
+    url "https://blocsapp.com/update/v#{version.major}/info.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
   container nested: "Blocs/Blocs-#{version.major}.dmg"
