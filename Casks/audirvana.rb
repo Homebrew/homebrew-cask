ff3057d6c7783ccdf977b26baafbc6aac54cbc11
Markus Reiter <me@reitermark.us>
2020-12-21 18:59:45 +0100

    audirvana: add livecheck (#96172)

--- Casks/audirvana.rb
+++ Casks/audirvana.rb
@@ -1,12 +1,16 @@
 cask "audirvana" do
-  version "3.5.43"
+  version "3.5.43,3573"
   sha256 "d4ecff4fd5ea8fa501abd530475fdf300212a73a5a59ff874e0dd43c99002a15"
 
-  url "https://audirvana.com/delivery/Audirvana_#{version}.dmg"
-  appcast "https://audirvana.com/delivery/audirvana#{version.major}_#{version.minor}_appcast.xml"
+  url "https://audirvana.com/delivery/Audirvana_#{version.before_comma}.dmg"
   name "Audirvana"
   desc "Audio playback software"
   homepage "https://audirvana.com/"
+
+  livecheck do
+    url "https://audirvana.com/delivery/audirvana#{version.major}_#{version.minor}_appcast.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
 
