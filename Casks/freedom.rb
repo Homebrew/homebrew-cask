d6068055b4df8737581c0c4f87461c148759962f
Markus Reiter <me@reitermark.us>
2020-12-21 16:36:26 +0100

    freedom: add livecheck (#96301)

--- Casks/freedom.rb
+++ Casks/freedom.rb
@@ -1,11 +1,15 @@
 cask "freedom" do
-  version "2.4.3"
+  version "2.4.3,1716.1"
   sha256 "374450cc820d948a04a0e07c1ca199cfbc6c5f521d99f27eceb8697d636d3fdc"
 
-  url "https://cdn.freedom.to/installers/updates/mac/#{version}/Freedom.zip"
-  appcast "https://cdn.freedom.to/installers/updates/mac/Appcast.xml"
+  url "https://cdn.freedom.to/installers/updates/mac/#{version.before_comma}/Freedom.zip"
   name "Freedom"
   homepage "https://freedom.to/"
+
+  livecheck do
+    url "https://cdn.freedom.to/installers/updates/mac/Appcast.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
 
