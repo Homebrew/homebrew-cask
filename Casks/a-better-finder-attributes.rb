ac54efd43833e3cdb53f177575b7588bd58748ae
Markus Reiter <me@reitermark.us>
2020-12-21 17:00:35 +0100

    a-better-finder-attributes: add livecheck (#96141)

--- Casks/a-better-finder-attributes.rb
+++ Casks/a-better-finder-attributes.rb
@@ -3,10 +3,14 @@ cask "a-better-finder-attributes" do
   sha256 "3daefbd08953b9791de724eb68eb6ff7b100fac0fa2f911a3a984844630df6ce"
 
   url "https://www.publicspace.net/download/signedABFA#{version.major}.zip"
-  appcast "https://www.publicspace.net/app/signed_abfa#{version.major}.xml"
   name "A Better Finder Attributes"
   desc "File and photo tweaking tool"
   homepage "https://www.publicspace.net/ABetterFinderAttributes/"
+
+  livecheck do
+    url "https://www.publicspace.net/app/signed_abfa#{version.major}.xml"
+    strategy :sparkle
+  end
 
   app "A Better Finder Attributes #{version.major}.app"
 
