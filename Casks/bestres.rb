b2dd8afcd7fe1b22a3a904450a9799d6a60e0938
Markus Reiter <me@reitermark.us>
2020-12-21 03:31:22 +0100

    bestres: add livecheck

--- Casks/bestres.rb
+++ Casks/bestres.rb
@@ -1,12 +1,18 @@
 cask "bestres" do
-  version "1.0.0,1426778671"
+  version "1.0,100:1426778671"
   sha256 "1b706a3550edbc1411bdc4665490cce3b22cef68a6abae82af0fbd46370ce697"
 
-  url "https://dl.devmate.com/com.icyberon.BestRes/#{version.before_comma.no_dots}/#{version.after_comma}/BestRes-#{version.before_comma.no_dots}.zip",
+  url "https://dl.devmate.com/com.icyberon.BestRes/#{version.after_comma.before_colon}/#{version.after_colon}/BestRes-#{version.after_comma.before_colon}.zip",
       verified: "devmate.com/com.icyberon.BestRes/"
-  appcast "https://updates.devmate.com/com.icyberon.BestRes.xml"
   name "BestRes"
   homepage "https://bestres.wojtek.im/"
+
+  livecheck do
+    url "https://updates.devmate.com/com.icyberon.BestRes.xml"
+    strategy :sparkle do |item|
+      "#{item.short_version},#{item.version}:#{item.url[%r{/(\d+)/BestRes-\d+\.zip}, 1]}"
+    end
+  end
 
   depends_on macos: ">= :yosemite"
 
