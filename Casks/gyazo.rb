55a21f0b2de8b733ab74e2d5af39716f595422b9
Markus Reiter <me@reitermark.us>
2020-12-21 19:00:42 +0100

    gyazo: add livecheck (#96323)

--- Casks/gyazo.rb
+++ Casks/gyazo.rb
@@ -3,9 +3,13 @@ cask "gyazo" do
   sha256 "21f639c03526ac0e53e6c370530d1ddb5a96ec65a7ab00b80e11cf589036d85c"
 
   url "https://files.gyazo.com/setup/Gyazo-#{version}.dmg"
-  appcast "https://gyazo.com/api/gy_mac.xml"
   name "Nota Gyazo GIF"
   homepage "https://gyazo.com/"
+
+  livecheck do
+    url "https://gyazo.com/api/gy_mac.xml"
+    strategy :sparkle
+  end
 
   app "Gyazo.app"
   app "Gyazo GIF.app"
