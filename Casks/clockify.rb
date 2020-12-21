ea13b573d44a9759c3a6ac13a41b762ea75665d6
Markus Reiter <me@reitermark.us>
2020-12-21 04:56:27 +0100

    clockify: add livecheck

--- Casks/clockify.rb
+++ Casks/clockify.rb
@@ -4,10 +4,14 @@ cask "clockify" do
 
   url "https://clockify-resources.s3.eu-central-1.amazonaws.com/downloads/ClockifyDesktop.zip",
       verified: "clockify-resources.s3.eu-central-1.amazonaws.com/"
-  appcast "https://clockify-resources.s3.eu-central-1.amazonaws.com/downloads/appcast.xml"
   name "Clockify"
   desc "Time tracking tool for agencies and freelancers"
   homepage "https://clockify.me/mac-time-tracking"
+
+  livecheck do
+    url "https://clockify-resources.s3.eu-central-1.amazonaws.com/downloads/appcast.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :sierra"
