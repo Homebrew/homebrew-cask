7268390f67304c00480146d6531955258c78665b
Markus Reiter <me@reitermark.us>
2020-12-21 16:48:05 +0100

    alternote: add livecheck (#96154)

--- Casks/alternote.rb
+++ Casks/alternote.rb
@@ -3,9 +3,13 @@ cask "alternote" do
   sha256 "b1a3c0d311b0633bae1a327d393c61a82bf21e2a0363109ef984e8f1c99549ff"
 
   url "http://alternoteapp.com/assets/downloads/Alternote#{version.after_comma}.zip"
-  appcast "http://alternoteapp.com/assets/appcast.xml"
   name "Alternote"
   homepage "http://alternoteapp.com/"
+
+  livecheck do
+    url "http://alternoteapp.com/assets/appcast.xml"
+    strategy :sparkle
+  end
 
   app "Alternote.app"
 
