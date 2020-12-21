80e6a188547ee423a96179e64c6bb3bf0650cc22
Markus Reiter <me@reitermark.us>
2020-12-21 21:25:44 +0100

    marginnote: add livecheck (#96436)

--- Casks/marginnote.rb
+++ Casks/marginnote.rb
@@ -1,13 +1,17 @@
 cask "marginnote" do
-  version "3.6.15"
+  version "3.6.15,3.6.15003"
   sha256 "39d93696555f95864807a9f40838f4ef3d617851f89578b5ebc4db57cee10749"
 
   url "https://marginstudy.com/mac/MarginNote#{version.major}.dmg",
       verified: "marginstudy.com/"
-  appcast "https://dist.marginnote.cn/marginnote#{version.major}.xml"
   name "MarginNote"
   desc "E-reader"
   homepage "https://www.marginnote.com/"
+
+  livecheck do
+    url "https://dist.marginnote.cn/marginnote#{version.major}.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
 
