ec6b879127387381d3b2bab752211f1ebb99bc13
Alexander Ronald Altman <alexanderaltman@me.com>
2020-12-21 00:33:43 -0800

    Update abyssoft-teleport from 1.3.0 to 1.3.1 (#96257)

--- Casks/abyssoft-teleport.rb
+++ Casks/abyssoft-teleport.rb
@@ -1,6 +1,6 @@
 cask "abyssoft-teleport" do
-  version "1.3.0"
-  sha256 "9f521405191124b1ae27c9016a17d73a1f683082d57641691ca551101b156731"
+  version "1.3.1"
+  sha256 "b01173ca64edd4eeb7fbf1d249f2fe8aa7b20919a19cf20aaa1ef15fa30e8db1"
 
   url "https://github.com/abyssoft/teleport/releases/download/v#{version}/teleport-v#{version}.zip"
   appcast "https://github.com/abyssoft/teleport/releases.atom"
