bc98e58a38f8ba54f0ed1ded57983b3ed3edbc57
Markus Reiter <me@reitermark.us>
2020-12-21 17:07:01 +0100

    inboard: add livecheck (#96355)

--- Casks/inboard.rb
+++ Casks/inboard.rb
@@ -1,12 +1,16 @@
 cask "inboard" do
-  version "1.1.5-431"
+  version "1.1.5,431"
   sha256 "b7ec8f738aaf3f0a707fbfc432bfd58b5fe6bb6b880e80102e37c71492966a5b"
 
-  url "https://inboardapp.com/trial/Inboard-#{version}.zip"
-  appcast "https://inboardapp.com/appcast.xml"
+  url "https://inboardapp.com/trial/Inboard-#{version.before_comma}-#{version.after_comma}.zip"
   name "Inboard"
   desc "App to organize screenshots and photos"
   homepage "https://inboardapp.com/"
+
+  livecheck do
+    url "https://inboardapp.com/appcast.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :high_sierra"
 
