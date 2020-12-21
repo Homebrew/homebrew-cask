22747a339f2f492c1a46fb5c5ddd56decd6af6b4
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-21 15:34:19 +0800

    Update wondershare-filmora.rb

--- Casks/wondershare-filmora.rb
+++ Casks/wondershare-filmora.rb
@@ -3,7 +3,8 @@ cask "wondershare-filmora" do
   sha256 :no_check
 
   url "https://download.wondershare.com/filmora-mac_full718.dmg"
-  appcast "https://cbs.wondershare.com/go.php?m=upgrade_info&pid=718&version=#{version}"
+  appcast "https://cbs.wondershare.com/go.php?m=upgrade_info&pid=718&version=#{version}",
+          must_contain: version.major_minor_patch
   name "Wondershare Filmora"
   desc "Video editor"
   homepage "https://filmora.wondershare.com/video-editor-mac/"
