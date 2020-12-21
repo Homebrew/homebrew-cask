01e03078be09f0d2cfc4f7c1532dbf0c5355c80d
Markus Reiter <me@reitermark.us>
2020-12-21 16:30:52 +0100

    iconjar: add livecheck (#96340)

--- Casks/iconjar.rb
+++ Casks/iconjar.rb
@@ -3,9 +3,13 @@ cask "iconjar" do
   sha256 "84494993160fdc9843ce09a9262673a68b9bf1aa8ac198959a83dcfae2e20f9d"
 
   url "https://geticonjar.com/releases/IconJar.app.#{version.after_comma}.zip"
-  appcast "https://geticonjar.com/releases/stable.xml"
   name "IconJar"
   homepage "https://geticonjar.com/"
+
+  livecheck do
+    url "https://geticonjar.com/releases/stable.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :mojave"
