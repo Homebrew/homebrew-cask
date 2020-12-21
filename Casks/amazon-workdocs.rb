743f3a272d1ed7a91ca3fbafe3f793a6928993f9
Markus Reiter <me@reitermark.us>
2020-12-21 17:56:36 +0100

    amazon-workdocs: add livecheck (#96157)

--- Casks/amazon-workdocs.rb
+++ Casks/amazon-workdocs.rb
@@ -1,15 +1,19 @@
 cask "amazon-workdocs" do
-  version "1.2.200287.0"
-  sha256 :no_check
+  version "1.2.200287.0,98"
+  sha256 "a60fa8eaf7260b76a1fdcc3f2a09fe2541beb4903e2f057e9998c15f725aa829"
 
-  url "https://d28gdqadgmua23.cloudfront.net/mac/Amazon%20WorkDocs.pkg",
+  url "https://d28gdqadgmua23.cloudfront.net/mac/version/#{version.before_comma}/#{version.after_comma}/Amazon%20WorkDocs.app.zip",
       verified: "d28gdqadgmua23.cloudfront.net/"
-  appcast "https://d28gdqadgmua23.cloudfront.net/mac/appcast/appcast-workdocs-prod.xml"
   name "Amazon WorkDocs"
   homepage "https://aws.amazon.com/workdocs/"
 
-  pkg "Amazon WorkDocs.pkg"
+  livecheck do
+    url "https://d28gdqadgmua23.cloudfront.net/mac/appcast/appcast-workdocs-prod.xml"
+    strategy :sparkle do |item|
+      match = item.url.match(%r{/(\d+(?:\.\d+)*)/(\d+)/Amazon WorkDocs\.app\.zip})
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
-  uninstall signal:  ["TERM", "com.amazon.AmazonWorkDocs"],
-            pkgutil: "com.amazon.aws.AmazonWorkDocs"
+  app "Amazon WorkDocs.app"
 end
