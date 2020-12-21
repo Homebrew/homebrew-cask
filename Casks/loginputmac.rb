92fdef85bf5894408b68c99c1d2521226df81be2
Markus Reiter <me@reitermark.us>
2020-12-21 19:18:10 +0100

    loginputmac: add livecheck (#96404)

--- Casks/loginputmac.rb
+++ Casks/loginputmac.rb
@@ -1,12 +1,16 @@
 cask "loginputmac" do
-  version "2.4.1"
+  version "2.4.1,23130"
   sha256 "e1aab3f3d9edeab525ea7e60ecfeddfeab5ba666fac4a7caf1c4ad13351125d9"
 
   url "https://loginput-mac2.content-delivery.top/loginputmac#{version.major}_latest.pkg",
       verified: "loginput-mac2.content-delivery.top/"
-  appcast "https://im.logcg.com/appcast#{version.major}.xml"
   name "LoginputMac"
   homepage "https://im.logcg.com/loginputmac#{version.major}"
+
+  livecheck do
+    url "https://im.logcg.com/appcast#{version.major}.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
 
