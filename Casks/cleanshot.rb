bd7d87487c0cb2bc0460961c1ab7fe8a897ccae8
Markus Reiter <me@reitermark.us>
2020-12-21 18:11:01 +0100

    cleanshot: add livecheck (#96207)

--- Casks/cleanshot.rb
+++ Casks/cleanshot.rb
@@ -3,10 +3,14 @@ cask "cleanshot" do
   sha256 "1221c0d35b5b40f17969b95836c956418e583ee357d1c039637c05a88b68e75c"
 
   url "https://updates.getcleanshot.com/v#{version.major}/CleanShot-X-#{version}.dmg"
-  appcast "https://updates.getcleanshot.com/v#{version.major}/appcast.xml"
   name "CleanShot"
   desc "Screen capturing tool"
   homepage "https://getcleanshot.com/"
+
+  livecheck do
+    url "https://updates.getcleanshot.com/v#{version.major}/appcast.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
 
