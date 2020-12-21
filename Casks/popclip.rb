1a445405f8f4b16be5b847a29f44d9067496a728
Markus Reiter <me@reitermark.us>
2020-12-21 22:55:42 +0100

    popclip: add livecheck

--- Casks/popclip.rb
+++ Casks/popclip.rb
@@ -1,12 +1,16 @@
 cask "popclip" do
-  version "2020.12"
+  version "2020.12,3117"
   sha256 "1f186088aa42d8659d9608e042c551d59764440193dc77c04ae8f5df32a53ac9"
 
-  url "https://pilotmoon.com/downloads/PopClip-#{version}.zip"
-  appcast "https://softwareupdate.pilotmoon.com/update/popclip/appcast.xml"
+  url "https://pilotmoon.com/downloads/PopClip-#{version.before_comma}.zip"
   name "PopClip"
   desc "Used to access context-specific actions when text is selected"
   homepage "https://pilotmoon.com/popclip/"
+
+  livecheck do
+    url "https://softwareupdate.pilotmoon.com/update/popclip/appcast.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :sierra"
 
