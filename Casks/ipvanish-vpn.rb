1381585b2820b7fbdb6278cf1bda0606348b8356
Markus Reiter <me@reitermark.us>
2020-12-21 18:09:11 +0100

    ipvanish-vpn: add livecheck (#96369)

--- Casks/ipvanish-vpn.rb
+++ Casks/ipvanish-vpn.rb
@@ -1,11 +1,15 @@
 cask "ipvanish-vpn" do
-  version "3.2.0_b53150"
-  sha256 "093037b730c6822e9fd2cb027d4e0541b14d4225674a54f4afb342c1eeb22a31"
+  version "3.2.1,57174"
+  sha256 "ee3eb71fdf2624a13eb6dada48ba3ab03ebb4ab9b95e2b5c658f9ad37beda5fe"
 
-  url "https://www.ipvanish.com/software/osx/IPVanish_v#{version}.zip"
-  appcast "https://www.ipvanish.com/software/osx/updates.xml"
+  url "https://www.ipvanish.com/software/osx/IPVanish_v#{version.before_comma}_b#{version.after_comma}.zip"
   name "IPVanish"
   homepage "https://www.ipvanish.com/"
+
+  livecheck do
+    url "https://www.ipvanish.com/software/osx/updates.xml"
+    strategy :sparkle
+  end
 
   app "IPVanish VPN.app"
 end
