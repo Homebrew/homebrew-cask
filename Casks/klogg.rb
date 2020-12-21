708eb87860fd221cafd7190ae080738412e0e645
Damien Calesse <2787828+kranack@users.noreply.github.com>
2020-12-21 10:29:49 +0100

    Update klogg from 20.12.0.808 to 20.12.0.810 (#96261)

--- Casks/klogg.rb
+++ Casks/klogg.rb
@@ -1,6 +1,6 @@
 cask "klogg" do
-  version "20.12.0.808"
-  sha256 "e6c6ad913cde26ff7f570fa36c013efe816fe25a0159c88053345715b14ee345"
+  version "20.12.0.810"
+  sha256 "9500f238fd9e1576ce53d772ba2359ea6c78c39860991e31c9f604996b1df935"
 
   url "https://github.com/variar/klogg/releases/download/v#{version.major_minor}/klogg-#{version}-OSX.dmg"
   appcast "https://github.com/variar/klogg/releases.atom"
