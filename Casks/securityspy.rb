331539394cd989fb644834c623a8f84a93ac12a2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-21 07:21:34 +0100

    Update securityspy from 5.3.1 to 5.3.1,5.0 (#95905)
    * Update securityspy from 5.3.1 to 5.3.1,5.0
    
    * Update securityspy.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/securityspy.rb
+++ Casks/securityspy.rb
@@ -1,10 +1,10 @@
 cask "securityspy" do
-  version "5.3.1"
+  version "5.3.1,5.0"
   sha256 :no_check
 
   url "https://www.bensoftware.com/securityspy/SecuritySpy.dmg"
   appcast "https://www.bensoftware.com/securityspy/versionhistory.html",
-          must_contain: version.chomp(".0")
+          must_contain: version.before_comma
   name "SecuritySpy"
   homepage "https://www.bensoftware.com/securityspy/"
 
