c386108ce764fb29932f29602d00824044fd9121
Markus Reiter <me@reitermark.us>
2020-12-21 17:01:28 +0100

    airdisplay: add livecheck (#96150)

--- Casks/airdisplay.rb
+++ Casks/airdisplay.rb
@@ -1,12 +1,16 @@
 cask "airdisplay" do
-  version "3.4.2"
+  version "3.4.2,26581"
   sha256 "272d14f33b3a4a16e5e0e1ebb2d519db4e0e3da17f95f77c91455b354bee7ee7"
 
-  url "https://www.avatron.com/updates/software/airdisplay/ad#{version.no_dots}.zip"
-  appcast "https://www.avatron.com/updates/software/airdisplay/appcast.xml"
+  url "https://www.avatron.com/updates/software/airdisplay/ad#{version.before_comma.no_dots}.zip"
   name "Air Display"
   desc "Utility for using a tablet as a second monitor"
   homepage "https://avatron.com/applications/air-display/"
+
+  livecheck do
+    url "https://www.avatron.com/updates/software/airdisplay/appcast.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :mojave"
 
