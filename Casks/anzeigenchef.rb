4c6b64b5a03512551b44934c226d8d07a2af3b11
Markus Reiter <me@reitermark.us>
2020-12-21 18:59:54 +0100

    anzeigenchef: add livecheck (#96162)

--- Casks/anzeigenchef.rb
+++ Casks/anzeigenchef.rb
@@ -3,9 +3,13 @@ cask "anzeigenchef" do
   sha256 :no_check
 
   url "https://anzeigenchef.roundcubes.de/downloads/mac/AnzeigenChef.app.zip"
-  appcast "https://anzeigenchef.roundcubes.de/downloads/mac/appcast.xml"
   name "AnzeigenChef"
   homepage "https://anzeigenchef.roundcubes.de/"
+
+  livecheck do
+    url "https://anzeigenchef.roundcubes.de/downloads/mac/appcast.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :sierra"
 
