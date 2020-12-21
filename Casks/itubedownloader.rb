1326f342fa028187f8d44a736b1f33fd3839ce73
Markus Reiter <me@reitermark.us>
2020-12-21 18:10:11 +0100

    itubedownloader: add livecheck (#96377)

--- Casks/itubedownloader.rb
+++ Casks/itubedownloader.rb
@@ -4,10 +4,14 @@ cask "itubedownloader" do
 
   url "https://itubedownloader.s3.us-east-2.amazonaws.com/iTubeDownloader.dmg",
       verified: "itubedownloader.s3.us-east-2.amazonaws.com/"
-  appcast "https://itubedownloader.s3.us-east-2.amazonaws.com/appcast.xml"
   name "iTubeDownloader"
   desc "Download YouTube videos, channels, or playlists"
   homepage "https://alphasoftware.co/"
+
+  livecheck do
+    url "https://itubedownloader.s3.us-east-2.amazonaws.com/appcast.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :yosemite"
 
