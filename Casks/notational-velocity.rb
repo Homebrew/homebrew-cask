70d28718381afe17528dff15c9df89739cc63554
Markus Reiter <me@reitermark.us>
2020-12-21 23:25:57 +0100

    notational-velocity: add livecheck (#96494)

--- Casks/notational-velocity.rb
+++ Casks/notational-velocity.rb
@@ -1,11 +1,17 @@
 cask "notational-velocity" do
-  version "2.0b5"
+  version "2.0b5,8"
   sha256 :no_check
 
   url "http://notational.net/NotationalVelocity.zip"
-  appcast "http://notational.net/nvupdates.xml"
   name "Notational Velocity"
   homepage "http://notational.net/"
+
+  livecheck do
+    url "http://notational.net/nvupdates.xml"
+    strategy :sparkle do |item|
+      "#{item.short_version.sub("β", "b")},#{item.version}"
+    end
+  end
 
   app "Notational Velocity.app"
 end
