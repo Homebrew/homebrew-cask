470bf34f4acd859e123ee050111b5379fbe9b6a0
Philipp Dörfler <ph@doerfler.io>
2020-12-21 12:24:06 +0100

    Added CircuitJS1 app.

---
+++ Casks/circuitjs.rb
@@ -0,0 +1,11 @@
+cask "circuitjs" do
+  version "1"
+  sha256 "fe371f01bdfbe74fd3df2a6cffb2d734902de410bfb2c5813e3a43d699ecc9d5"
+
+  url "https://www.falstad.com/circuit/offline/CircuitJS#{version}-mac.dmg"
+  name "Falstad CircuitJS"
+  desc "Electronic circuit simulator"
+  homepage "https://www.falstad.com/circuit/"
+
+  app "CircuitJS1.app"
+end
