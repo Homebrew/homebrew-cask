0234fa5593644feae9d55b8316e89d93a02d4f97
Markus Reiter <me@reitermark.us>
2020-12-21 17:06:57 +0100

    figmadaemon: add livecheck (#96289)

--- Casks/figmadaemon.rb
+++ Casks/figmadaemon.rb
@@ -3,9 +3,13 @@ cask "figmadaemon" do
   sha256 "97501a70cbba84e329e284ee223d6236d409102f58deb0282e705f2a14b171ea"
 
   url "https://font-daemon.figma.com/mac/FigmaDaemon.#{version}.zip"
-  appcast "https://font-daemon.figma.com/mac/versions.xml"
   name "Figma Font Installers"
   homepage "https://www.figma.com/"
+
+  livecheck do
+    url "https://font-daemon.figma.com/mac/versions.xml"
+    strategy :sparkle
+  end
 
   app "FigmaDaemon.app"
 
