0f45f4628ea536b6de6d068f95a51342ff817905
Markus Reiter <me@reitermark.us>
2020-12-21 23:06:29 +0100

    moneywiz: add livecheck (#96472)

--- Casks/moneywiz.rb
+++ Casks/moneywiz.rb
@@ -3,10 +3,14 @@ cask "moneywiz" do
   sha256 :no_check
 
   url "https://mac.wiz.money/MoneyWiz_Mac_Free.dmg"
-  appcast "https://macdistribution.wiz.money/version_info_free.xml"
   name "MoneyWiz"
   desc "Finance management application"
   homepage "https://wiz.money/"
+
+  livecheck do
+    url "https://macdistribution.wiz.money/version_info_free.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :sierra"
 
