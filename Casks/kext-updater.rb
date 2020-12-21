bd5e0ceddc9c9330e2266cdbd54aadccf550f449
Markus Reiter <me@reitermark.us>
2020-12-21 18:54:03 +0100

    kext-updater: add livecheck (#96385)

--- Casks/kext-updater.rb
+++ Casks/kext-updater.rb
@@ -3,10 +3,14 @@ cask "kext-updater" do
   sha256 :no_check
 
   url "https://update.kextupdater.de/kextupdater/kextupdaterng.zip"
-  appcast "https://update.kextupdater.de/kextupdater/appcastng.xml"
   name "Kext Updater"
   desc "Automatic updater for kernel extensions required by 'Hackintoshes'"
   homepage "https://kextupdater.de/"
+
+  livecheck do
+    url "https://update.kextupdater.de/kextupdater/appcastng.xml"
+    strategy :sparkle
+  end
 
   app "Kext Updater.app"
 
