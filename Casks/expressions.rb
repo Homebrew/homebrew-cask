7ccdd97fabbe002dffa1e03ba47c34595f1a0574
Markus Reiter <me@reitermark.us>
2020-12-21 17:06:45 +0100

    expressions: add livecheck (#96285)

--- Casks/expressions.rb
+++ Casks/expressions.rb
@@ -1,12 +1,16 @@
 cask "expressions" do
-  version "1.3.3"
+  version "1.3.3,42"
   sha256 "cce8506474d9c071ce9e945c7709e21cadcab2e962805b3b3ba61246723195ad"
 
-  url "https://www.apptorium.com/public/products/expressions/releases/Expressions-#{version}.zip"
-  appcast "https://updates.devmate.com/com.apptorium.Expressions-dm.xml"
+  url "https://www.apptorium.com/public/products/expressions/releases/Expressions-#{version.before_comma}.zip"
   name "Expressions"
   desc "Regular expressions manager app"
   homepage "https://www.apptorium.com/expressions"
+
+  livecheck do
+    url "https://updates.devmate.com/com.apptorium.Expressions-dm.xml"
+    strategy :sparkle
+  end
 
   app "Expressions.app"
 
