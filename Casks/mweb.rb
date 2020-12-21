1be6b92c265e632ab056676cd62c397894a1d63f
Markus Reiter <me@reitermark.us>
2020-12-21 23:32:52 +0100

    mweb: add livecheck (#96483)

--- Casks/mweb.rb
+++ Casks/mweb.rb
@@ -1,13 +1,19 @@
 cask "mweb" do
-  version "3.4.4,592"
+  version "3.4.4,1607678309"
   sha256 :no_check
 
-  url "https://dl.devmate.com/com.coderforart.MWeb3/MWeb3-ProMarkdownwriting,notetakingandstaticbloggeneratorApp.dmg",
-      verified: "dl.devmate.com/com.coderforart.MWeb3/"
-  appcast "https://updates.devmate.com/com.coderforart.MWeb3.xml"
+  url "https://dl.devmate.com/com.coderforart.MWeb#{version.major}/#{version.before_comma}/#{version.after_comma}/MWeb#{version.major}-ProMarkdownwriting,notetakingandstaticbloggeneratorApp-#{version.before_comma}.zip",
+      verified: "dl.devmate.com/com.coderforart.MWeb#{version.major}/"
   name "MWeb"
   desc "Markdown writing, note taking, and static blog generator app"
   homepage "https://www.mweb.im/"
+
+  livecheck do
+    url "https://updates.devmate.com/com.coderforart.MWeb#{version.major}.xml"
+    strategy :sparkle do |item|
+      "#{item.short_version},#{item.url[%r{/(\d+)/MWeb}i, 1]}"
+    end
+  end
 
   depends_on macos: ">= :sierra"
 
