8c26299476c73c24c8be4c0109ac6e0aa96fb774
Markus Reiter <me@reitermark.us>
2020-12-21 18:10:30 +0100

    blu-ray-player-pro: add livecheck (#96190)

--- Casks/blu-ray-player-pro.rb
+++ Casks/blu-ray-player-pro.rb
@@ -1,12 +1,19 @@
 cask "blu-ray-player-pro" do
-  version "3.3.20_201204_0319"
+  version "3.3.20,201204_0319"
   sha256 "857684e9583ba4f06bbdff94c2e4a1070dbf756cbc44530eb2414003cf78c9c5"
 
-  url "https://cdn.macblurayplayer.com/mac-bluray-player-pro3/Blu-ray_Player_Pro_#{version}.dmg"
-  appcast "https://cdn.macblurayplayer.com/mac-bluray-player-pro3/appcast/Appcast.xml"
+  url "https://cdn.macblurayplayer.com/mac-bluray-player-pro3/Blu-ray_Player_Pro_#{version.before_comma}_#{version.after_comma}.dmg"
   name "Macgo Mac Blu-ray Player Pro"
   desc "Blu-ray player software"
   homepage "https://www.macblurayplayer.com/"
+
+  livecheck do
+    url "https://cdn.macblurayplayer.com/mac-bluray-player-pro3/appcast/Appcast.xml"
+    strategy :sparkle do |item|
+      match = item.url.match(/_(\d(?:\.\d+)*)_(.*?)\.dmg/)
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   app "Blu-ray Player Pro.app"
 end
