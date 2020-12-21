a8c7f7c31c437219b73bfd9d82272c062c0310f0
Markus Reiter <me@reitermark.us>
2020-12-21 21:40:14 +0100

    musictube: add livecheck

--- Casks/musictube.rb
+++ Casks/musictube.rb
@@ -3,10 +3,14 @@ cask "musictube" do
   sha256 :no_check
 
   url "https://flavio.tordini.org/files/musictube/musictube.dmg"
-  appcast "https://flavio.tordini.org/musictube-ws/appcast.xml"
   name "Musictube"
   desc "Streaming music player"
   homepage "https://flavio.tordini.org/musictube"
+
+  livecheck do
+    url "https://flavio.tordini.org/musictube-ws/appcast.xml"
+    strategy :sparkle
+  end
 
   app "Musictube.app"
 end
