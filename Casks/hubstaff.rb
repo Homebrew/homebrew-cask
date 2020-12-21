5eecf120f0bdc548eee6182e7ca9e7187f7d9d1b
Markus Reiter <me@reitermark.us>
2020-12-21 18:25:14 +0100

    hubstaff: add livecheck (#96334)

--- Casks/hubstaff.rb
+++ Casks/hubstaff.rb
@@ -3,9 +3,15 @@ cask "hubstaff" do
   sha256 "abea6948da97884d2f9890ffca45a75408d2a750681eae0212f79a202c77b429"
 
   url "https://app.hubstaff.com/download/#{version.after_comma}-mac-os-x-#{version.before_comma.dots_to_hyphens}-release"
-  appcast "https://app.hubstaff.com/appcast.xml"
   name "Hubstaff"
   homepage "https://hubstaff.com/"
+
+  livecheck do
+    url "https://app.hubstaff.com/appcast.xml"
+    strategy :sparkle do |item|
+      "#{item.short_version.split("-").first},#{item.url[%r{/(\d+)-mac-os-x-(?:\d(?:-\d+)*)-release}, 1]}"
+    end
+  end
 
   app "Hubstaff.app"
 end
