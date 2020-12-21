7029e588e4cfbe5218755eb7fef8489be830c5e7
Markus Reiter <me@reitermark.us>
2020-12-21 16:27:13 +0100

    houdahspot: add livecheck (#96333)

--- Casks/houdahspot.rb
+++ Casks/houdahspot.rb
@@ -1,12 +1,16 @@
 cask "houdahspot" do
-  version "6.0"
+  version "6.0,466"
   sha256 "4989462868d8a74bd20f17040e0f899d9ae8899de6b3cfd8b1364cd33722d8f8"
 
-  url "https://dl.houdah.com/houdahSpot/updates/cast#{version.major}_assets/HoudahSpot#{version}.zip"
-  appcast "https://www.houdah.com/houdahSpot/updates/cast#{version.major}.xml"
+  url "https://dl.houdah.com/houdahSpot/updates/cast#{version.major}_assets/HoudahSpot#{version.before_comma}.zip"
   name "HoudahSpot"
   desc "File searching application"
   homepage "https://www.houdah.com/houdahSpot/"
+
+  livecheck do
+    url "https://www.houdah.com/houdahSpot/updates/cast#{version.major}.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :mojave"
 
