ec9c5a2063153910df2362f8785ff8e9083c900d
Markus Reiter <me@reitermark.us>
2020-12-21 21:25:50 +0100

    melodics: add livecheck (#96452)

--- Casks/melodics.rb
+++ Casks/melodics.rb
@@ -3,9 +3,13 @@ cask "melodics" do
   sha256 "3bb5ae4faaf6e5865a2c9175b4f815a3a76c2e2f27ec712c41fe7339fa275dc0"
 
   url "https://web-cdn.melodics.com/download/MelodicsV#{version.major}.dmg"
-  appcast "https://web-cdn.melodics.com/download/osxupdatescastv#{version.major}.xml"
   name "Melodics"
   homepage "https://melodics.com/"
+
+  livecheck do
+    url "https://web-cdn.melodics.com/download/osxupdatescastv#{version.major}.xml"
+    strategy :sparkle
+  end
 
   app "Melodics.app"
 end
