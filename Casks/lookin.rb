0bc7cbb8bc9f7f759116419f0d5ce0ccc8179b83
Markus Reiter <me@reitermark.us>
2020-12-21 19:18:20 +0100

    lookin: add livecheck (#96405)

--- Casks/lookin.rb
+++ Casks/lookin.rb
@@ -1,11 +1,15 @@
 cask "lookin" do
-  version "1.0.0"
+  version "1.0.0,7"
   sha256 "298d9923ecd3d3771bcac0d375dcdfed66038bec8a13705de66efa3896a53be7"
 
-  url "https://cdn.lookin.work/release/Lookin-#{version.dots_to_hyphens}.zip"
-  appcast "https://lookin.work/release/appcast.xml"
+  url "https://cdn.lookin.work/release/Lookin-#{version.before_comma.dots_to_hyphens}.zip"
   name "Lookin"
   homepage "https://lookin.work/"
+
+  livecheck do
+    url "https://lookin.work/release/appcast.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :high_sierra"
