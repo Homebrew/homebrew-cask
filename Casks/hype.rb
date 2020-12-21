0b02e5b14be56678b1c15c63af5a219571acfed9
Markus Reiter <me@reitermark.us>
2020-12-21 15:07:49 +0100

    hype: add livecheck

--- Casks/hype.rb
+++ Casks/hype.rb
@@ -3,10 +3,14 @@ cask "hype" do
   sha256 :no_check
 
   url "https://tumult.com/hype/download/Hype.zip"
-  appcast "https://tumult.com/hype/appcast_hype#{version.major}.xml"
   name "Tumult Hype"
   desc "App to create animated and interactive web content"
   homepage "https://tumult.com/hype/"
+
+  livecheck do
+    url "https://tumult.com/hype/appcast_hype#{version.major}.xml"
+    strategy :sparkle
+  end
 
   # Renamed for consistency: app name is different in the Finder and in a shell
   app "Hype#{version.major}.app", target: "Hype #{version.major}.app"
