44ba931e65310f4d144d2435260e4e89d9a51396
Markus Reiter <me@reitermark.us>
2020-12-21 23:06:50 +0100

    proclaim: add livecheck

--- Casks/proclaim.rb
+++ Casks/proclaim.rb
@@ -1,12 +1,16 @@
 cask "proclaim" do
-  version "3.0.0.0048"
+  version "3.0.0.0048,310.0.48"
   sha256 "22e790a0a777d6cdbf65d2820f890bb1d15e4f6859a33b5ec9a65996a95462da"
 
-  url "https://downloads.logoscdn.com/Proclaim/Installer/#{version}/Proclaim.dmg",
+  url "https://downloads.logoscdn.com/Proclaim/Installer/#{version.before_comma}/Proclaim.dmg",
       verified: "logoscdn.com/Proclaim/"
-  appcast "http://downloads.proclaimonline.com/mac/appcast.xml"
   name "Faithlife Proclaim"
   homepage "https://proclaim.faithlife.com/"
+
+  livecheck do
+    url "http://downloads.proclaimonline.com/mac/appcast.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :mojave"
 
