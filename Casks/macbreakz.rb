57a72d366a3914e0c410b3305228a33302944a56
Markus Reiter <me@reitermark.us>
2020-12-21 20:16:18 +0100

    macbreakz: add livecheck (#96409)

--- Casks/macbreakz.rb
+++ Casks/macbreakz.rb
@@ -3,9 +3,13 @@ cask "macbreakz" do
   sha256 "941d07cc1a6e0c134f01147a69456b771719ad106be8ecd65d488a2a9fc8ff5d"
 
   url "https://www.publicspace.net/download/MacBreakZ#{version.major}.dmg"
-  appcast "https://www.publicspace.net/app/signed_mb#{version.major}.xml"
   name "MacBreakZ"
   homepage "https://www.publicspace.net/MacBreakZ/"
+
+  livecheck do
+    url "https://www.publicspace.net/app/signed_mb#{version.major}.xml"
+    strategy :sparkle
+  end
 
   app "MacBreakZ #{version.major}.app"
 end
