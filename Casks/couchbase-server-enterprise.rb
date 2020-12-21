7a599f664b47767c56383368e5502a80d893b228
Markus Reiter <me@reitermark.us>
2020-12-21 18:03:02 +0100

    Update couchbase-server-enterprise.rb

--- Casks/couchbase-server-enterprise.rb
+++ Casks/couchbase-server-enterprise.rb
@@ -12,15 +12,15 @@ cask "couchbase-server-enterprise" do
 
     url "https://packages.couchbase.com/releases/#{version}/couchbase-server-enterprise_#{version}-macos_x86_64.dmg"
 
-    name "Couchbase Server"
-    homepage "https://www.couchbase.com/"
-
-    livecheck do
-      url "http://appcast.couchbase.com/membasex.xml"
-      strategy :sparkle
-    end
-
     app "Couchbase Server.app"
+  end
+
+  name "Couchbase Server"
+  homepage "https://www.couchbase.com/"
+
+  livecheck do
+    url "http://appcast.couchbase.com/membasex.xml"
+    strategy :sparkle
   end
 
   conflicts_with cask: "couchbase-server-community"
