8d52d74963241eb2e147f46e2a0de0f5b1aeee89
Markus Reiter <me@reitermark.us>
2020-12-21 22:23:22 +0100

    minitube: add livecheck (#96468)

--- Casks/minitube.rb
+++ Casks/minitube.rb
@@ -3,10 +3,14 @@ cask "minitube" do
   sha256 :no_check
 
   url "https://flavio.tordini.org/files/minitube/minitube.dmg"
-  appcast "https://flavio.tordini.org/minitube-ws/appcast.xml"
   name "Minitube"
   desc "YouTube application"
   homepage "https://flavio.tordini.org/minitube"
+
+  livecheck do
+    url "https://flavio.tordini.org/minitube-ws/appcast.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :sierra"
 
