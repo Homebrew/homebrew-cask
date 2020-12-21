c460d1aff4b60f520c02117ff4fb56c22548c4ff
Markus Reiter <me@reitermark.us>
2020-12-21 22:30:39 +0100

    mmhmm: add livecheck (#96470)

--- Casks/mmhmm.rb
+++ Casks/mmhmm.rb
@@ -1,12 +1,16 @@
 cask "mmhmm" do
-  version "1.0.2"
-  sha256 "e1df9eb8dcfa9bc1f058fc5a2501c557f71b57bc9402aa3f9e707e855fde358b"
+  version "1.1.0,1608528000"
+  sha256 "e03fd50041e9c77de58bb0c9fbccca47d4ca7c76e3b4644e9037c336c62e7a68"
 
-  url "https://updates.mmhmm.app/mac/production/mmhmm_#{version}.zip"
-  appcast "https://updates.mmhmm.app/mac/production/sparkle.xml"
+  url "https://updates.mmhmm.app/mac/production/mmhmm_#{version.before_comma}.zip"
   name "mmhmm"
   desc "Virtual video presentation software"
   homepage "https://www.mmhmm.app/"
+
+  livecheck do
+    url "https://updates.mmhmm.app/mac/production/sparkle.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :mojave"
 
