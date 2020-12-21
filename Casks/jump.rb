127c961fcb60520de7ea27f40294cc657bc3b8a5
Markus Reiter <me@reitermark.us>
2020-12-21 18:53:55 +0100

    jump: add livecheck (#96381)

--- Casks/jump.rb
+++ Casks/jump.rb
@@ -1,12 +1,16 @@
 cask "jump" do
-  version "8.6.19"
+  version "8.6.19,80619"
   sha256 "77ceca3b2622125305d73975141799f94dd3425d1da9b55eedc08c2cd20c7a70"
 
-  url "https://dl.jumpdesktop.com/downloads/jdm/JumpDesktopMac-#{version}.zip"
-  appcast "https://jumpdesktop.com/downloads/viewer/jdmac-web-appcast.xml"
+  url "https://dl.jumpdesktop.com/downloads/jdm/JumpDesktopMac-#{version.before_comma}.zip"
   name "Jump Desktop"
   desc "Remote desktop application"
   homepage "https://jumpdesktop.com/#jdmac"
+
+  livecheck do
+    url "https://jumpdesktop.com/downloads/viewer/jdmac-web-appcast.xml"
+    strategy :sparkle
+  end
 
   app "Jump Desktop.app"
 
