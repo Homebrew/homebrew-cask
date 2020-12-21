661fde94fd7b56a4a59040a4950b8b2bee1fe0b9
Markus Reiter <me@reitermark.us>
2020-12-21 19:02:54 +0100

    dropshare: add livecheck (#96279)

--- Casks/dropshare.rb
+++ Casks/dropshare.rb
@@ -4,10 +4,16 @@ cask "dropshare" do
 
   url "https://d2wvuuix8c9e48.cloudfront.net/Dropshare#{version.major}-#{version.after_comma}.app.zip",
       verified: "d2wvuuix8c9e48.cloudfront.net/"
-  appcast "https://dropshare.app/sparkle/Dropshare#{version.major}.xml"
   name "Dropshare"
   desc "File sharing solution"
   homepage "https://dropshare.app/"
+
+  livecheck do
+    url "https://dropshare.app/sparkle/Dropshare#{version.major}.xml"
+    strategy :sparkle do |item|
+      "#{item.title[/(\d+(?:\.\d+)*)/, 1]},#{item.version}"
+    end
+  end
 
   app "Dropshare #{version.major}.app"
   binary "#{appdir}/Dropshare #{version.major}.app/Contents/Resources/ds.sh", target: "ds"
