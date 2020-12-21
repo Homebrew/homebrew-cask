e5515f65ebac13b6cb80f45a1ba7b92c350c0dee
Markus Reiter <me@reitermark.us>
2020-12-21 16:30:00 +0100

    final-cut-library-manager: add livecheck (#96290)

--- Casks/final-cut-library-manager.rb
+++ Casks/final-cut-library-manager.rb
@@ -1,12 +1,16 @@
 cask "final-cut-library-manager" do
-  version "3.82"
+  version "3.82.00"
   sha256 "175538d3d9b654274a4a59b43bf279b533b2c5f82a9a99d0fed15499a5a11ba0"
 
-  url "http://cdn.arcticwhiteness.com/finalcutlibrarymanager/download/zips/FinalCutLibraryManager_#{version}.zip"
-  appcast "https://www.arcticwhiteness.com/finalcutlibrarymanager/download/appcast.xml"
+  url "http://cdn.arcticwhiteness.com/finalcutlibrarymanager/download/zips/FinalCutLibraryManager_#{version.major_minor}.zip"
   name "Arctic Whiteness Final Cut Library Manager"
   name "FCPX Library Manager"
   homepage "https://www.arcticwhiteness.com/finalcutlibrarymanager/"
+
+  livecheck do
+    url "https://www.arcticwhiteness.com/finalcutlibrarymanager/download/appcast.xml"
+    strategy :sparkle
+  end
 
   app "Final Cut Library Manager.app"
 end
