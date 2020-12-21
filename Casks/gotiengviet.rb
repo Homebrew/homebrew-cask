b7c7dcfa9bf1246e72cae8fac1338fddf0cc4f5c
Markus Reiter <me@reitermark.us>
2020-12-21 18:11:30 +0100

    gotiengviet: add livecheck (#96320)

--- Casks/gotiengviet.rb
+++ Casks/gotiengviet.rb
@@ -3,9 +3,13 @@ cask "gotiengviet" do
   sha256 :no_check
 
   url "https://www.trankynam.com/gotv/downloads/GoTiengViet.dmg"
-  appcast "https://www.trankynam.com/gotv/macos/GoTiengVietMacOSX-Appcast.xml"
   name "GoTiengViet"
   homepage "https://www.trankynam.com/gotv/"
+
+  livecheck do
+    url "https://www.trankynam.com/gotv/macos/GoTiengVietMacOSX-Appcast.xml"
+    strategy :sparkle
+  end
 
   app "GoTiengViet.app"
 end
