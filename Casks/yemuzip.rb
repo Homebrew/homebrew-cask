97aaa377e31b08f77119fd9de1b1d9a531876067
Markus Reiter <me@reitermark.us>
2020-12-22 22:06:26 +0100

    yemuzip: add livecheck (#96718)

--- Casks/yemuzip.rb
+++ Casks/yemuzip.rb
@@ -3,9 +3,13 @@ cask "yemuzip" do
   sha256 "9c97ac9a715857dc8679b0e1d7fbddb253b0f56275641be2cd9a47b45bd29727"
 
   url "https://yellowmug.com/download/YemuZip_#{version}.dmg"
-  appcast "https://yellowmug.com/yemuzip/appcast-1014.xml"
   name "YemuZip"
   homepage "https://yellowmug.com/yemuzip/"
+
+  livecheck do
+    url "https://yellowmug.com/yemuzip/appcast-1014.xml"
+    strategy :sparkle
+  end
 
   app "YemuZip.app"
 end
