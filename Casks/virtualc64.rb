f039a5a9888294a62b4b3dad5f16da4e1a2a707d
Markus Reiter <me@reitermark.us>
2020-12-22 20:09:20 +0100

    virtualc64: add livecheck (#96688)

--- Casks/virtualc64.rb
+++ Casks/virtualc64.rb
@@ -4,9 +4,13 @@ cask "virtualc64" do
   sha256 "542145ec4d6b71b5a05a5bfe55b30fc7792dd8eaf363e666a426eb6e045e8af4"
 
   url "http://www.dirkwhoffmann.de/virtualc64/VirtualC64_#{version}.zip"
-  appcast "http://dirkwhoffmann.de/virtualc64/VirtualC64Appcast.xml"
   name "Virtual C64"
   homepage "http://www.dirkwhoffmann.de/virtualc64/"
+
+  livecheck do
+    url "http://dirkwhoffmann.de/virtualc64/VirtualC64Appcast.xml"
+    strategy :sparkle
+  end
 
   app "VirtualC64.app"
 end
