3a02ded827084abc675dcca50adef3059d64229f
Markus Reiter <me@reitermark.us>
2020-12-22 03:53:46 +0100

    photosrevive: add livecheck (#96518)

--- Casks/photosrevive.rb
+++ Casks/photosrevive.rb
@@ -1,12 +1,16 @@
 cask "photosrevive" do
-  version "1.3.0"
+  version "1.3.0,6029"
   sha256 "a7f265d6855be39418db73cc1613c3f9f4cc3526e2e7e57dcb7278331791cebd"
 
-  url "https://neededapps.com/appcasts/photosrevive/versions/PR-#{version}.zip"
-  appcast "https://neededapps.com/appcasts/photosrevive/changelog.xml"
+  url "https://neededapps.com/appcasts/photosrevive/versions/PR-#{version.before_comma}.zip"
   name "PhotosRevive"
   desc "Colorize old black and white photos automatically"
   homepage "https://neededapps.com/photosrevive/"
+
+  livecheck do
+    url "https://neededapps.com/appcasts/photosrevive/changelog.xml"
+    strategy :sparkle
+  end
 
   app "PhotosRevive.app"
 
