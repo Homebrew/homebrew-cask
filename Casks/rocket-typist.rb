8599725fbc5918954d177a73cd91c8e87f5c2e27
Markus Reiter <me@reitermark.us>
2020-12-22 17:57:37 +0100

    rocket-typist: add livecheck (#96559)

--- Casks/rocket-typist.rb
+++ Casks/rocket-typist.rb
@@ -3,9 +3,13 @@ cask "rocket-typist" do
   sha256 :no_check
 
   url "https://witt-software.com/downloads/rockettypist/Rocket%20Typist.dmg"
-  appcast "https://witt-software.com/downloads/rockettypist/appcast.xml"
   name "Rocket Typist"
   homepage "https://witt-software.com/rockettypist/"
+
+  livecheck do
+    url "https://witt-software.com/downloads/rockettypist/appcast.xml"
+    strategy :sparkle
+  end
 
   app "Rocket Typist.app"
 end
