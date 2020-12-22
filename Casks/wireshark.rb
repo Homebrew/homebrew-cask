0fc88287fc9c215c7499a09ad8231027c5efb3bb
Markus Reiter <me@reitermark.us>
2020-12-22 19:52:29 +0100

    wireshark: add livecheck

--- Casks/wireshark.rb
+++ Casks/wireshark.rb
@@ -3,10 +3,14 @@ cask "wireshark" do
   sha256 "2a1b79412c656e8c8efaf54d6adbc71bc8d8e7f6847fa614ada1f11395a6bad1"
 
   url "https://2.na.dl.wireshark.org/osx/Wireshark%20#{version}%20Intel%2064.dmg"
-  appcast "https://www.wireshark.org/update/0/Wireshark/0.0.0/macOS/x86-64/en-US/stable.xml"
   name "Wireshark"
   desc "Network protocol analyzer"
   homepage "https://www.wireshark.org/"
+
+  livecheck do
+    url "https://www.wireshark.org/update/0/Wireshark/0.0.0/macOS/x86-64/en-US/stable.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
   conflicts_with cask:    "wireshark-chmodbpf",
