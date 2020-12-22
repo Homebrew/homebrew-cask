f7555b5907fc6064e67ee68fda2945c166a8bb76
Markus Reiter <me@reitermark.us>
2020-12-22 18:33:54 +0100

    vivaldi: add livecheck

--- Casks/vivaldi.rb
+++ Casks/vivaldi.rb
@@ -3,10 +3,14 @@ cask "vivaldi" do
   sha256 "6426e4462cb7201a976f94cb11db6cd5c57e8d31794b410ad532253c0cc61737"
 
   url "https://downloads.vivaldi.com/stable/Vivaldi.#{version}.dmg"
-  appcast "https://update.vivaldi.com/update/1.0/public/mac/appcast.xml"
   name "Vivaldi"
   desc "Web browser focusing on customization and control"
   homepage "https://vivaldi.com/"
+
+  livecheck do
+    url "https://update.vivaldi.com/update/1.0/public/mac/appcast.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
 
