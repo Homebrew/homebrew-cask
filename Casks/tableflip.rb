775e3dd0cb768d538293a212f45c01580e858c86
Markus Reiter <me@reitermark.us>
2020-12-22 17:40:20 +0100

    tableflip: add livecheck (#96627)

--- Casks/tableflip.rb
+++ Casks/tableflip.rb
@@ -1,13 +1,17 @@
 cask "tableflip" do
-  version "1.3.0"
+  version "1.3.0,60"
   sha256 "b947ad412074ee1b4cd5b5982233aed23f84d623bf3b5b79f315677df323615f"
 
-  url "https://update.christiantietze.de/tableflip/v#{version.major}/TableFlip-v#{version}.zip",
+  url "https://update.christiantietze.de/tableflip/v#{version.major}/TableFlip-v#{version.before_comma}.zip",
       verified: "update.christiantietze.de/tableflip/"
-  appcast "https://update.christiantietze.de/tableflip/v#{version.major}/release.xml"
   name "TableFlip"
   desc "App to edit markdown files in place"
   homepage "https://tableflipapp.com/"
+
+  livecheck do
+    url "https://update.christiantietze.de/tableflip/v#{version.major}/release.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
 
