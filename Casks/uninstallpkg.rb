aeaa9cc35cc76e34d8f3c1181c59b5d90ce38821
Markus Reiter <me@reitermark.us>
2020-12-22 19:52:50 +0100

    uninstallpkg: add livecheck (#96682)

--- Casks/uninstallpkg.rb
+++ Casks/uninstallpkg.rb
@@ -1,11 +1,15 @@
 cask "uninstallpkg" do
-  version "1.1.9"
+  version "1.1.9,1405"
   sha256 "4815c2c4ce97f1e5768690651efaf2d7a4feec34a446a027cc866ad78b96ac02"
 
-  url "https://www.corecode.io/downloads/uninstallpkg_#{version}.zip"
-  appcast "https://www.corecode.io/uninstallpkg/uninstallpkg.xml"
+  url "https://www.corecode.io/downloads/uninstallpkg_#{version.before_comma}.zip"
   name "UninstallPKG"
   homepage "https://www.corecode.io/uninstallpkg/"
+
+  livecheck do
+    url "https://www.corecode.io/uninstallpkg/uninstallpkg.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :mojave"
 
