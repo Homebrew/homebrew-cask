0241720eac2d40bc0a35bd36bf26047fe16594f7
Markus Reiter <me@reitermark.us>
2020-12-22 17:21:38 +0100

    pdf-squeezer: add livecheck (#96511)

--- Casks/pdf-squeezer.rb
+++ Casks/pdf-squeezer.rb
@@ -3,10 +3,14 @@ cask "pdf-squeezer" do
   sha256 :no_check
 
   url "https://www.witt-software.com/downloads/pdfsqueezer/PDF%20Squeezer.dmg"
-  appcast "https://www.witt-software.com/downloads/pdfsqueezer/pdfsq4-appcast.xml"
   name "PDF Squeezer"
   desc "PDF compression tool"
   homepage "https://witt-software.com/pdfsqueezer/"
+
+  livecheck do
+    url "https://www.witt-software.com/downloads/pdfsqueezer/pdfsq4-appcast.xml"
+    strategy :sparkle
+  end
 
   app "PDF Squeezer.app"
 end
