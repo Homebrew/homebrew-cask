ef7ee30b2f4ff9cbccd86a6eb274ff846bfeecea
Markus Reiter <me@reitermark.us>
2020-12-22 18:10:56 +0100

    soqlxplorer: add livecheck (#96595)

--- Casks/soqlxplorer.rb
+++ Casks/soqlxplorer.rb
@@ -3,10 +3,14 @@ cask "soqlxplorer" do
   sha256 "df52a4749a712c7f527af3f87dcd88c8e08ff25714c666070c09f78f68ca36a3"
 
   url "https://www.pocketsoap.com/osx/soqlx/SoqlXplorer_v#{version}.zip"
-  appcast "https://www.pocketsoap.com/osx/soqlx/appcast.xml"
   name "SoqlXplorer"
   desc "Desktop client for Salesforce.com platform"
   homepage "https://www.pocketsoap.com/osx/soqlx/"
+
+  livecheck do
+    url "https://www.pocketsoap.com/osx/soqlx/appcast.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :high_sierra"
 
