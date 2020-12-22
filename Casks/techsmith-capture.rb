7720a74cb312e567ae741409abc2688a9a60e7ea
Markus Reiter <me@reitermark.us>
2020-12-22 17:40:34 +0100

    techsmith-capture: add livecheck (#96630)

--- Casks/techsmith-capture.rb
+++ Casks/techsmith-capture.rb
@@ -3,10 +3,14 @@ cask "techsmith-capture" do
   sha256 :no_check
 
   url "https://assets.techsmith.com/techsmithcapture/mac/TechSmithCapture.dmg"
-  appcast "https://assets.techsmith.com/techsmithcapture/mac/TechSmithCaptureAppcast.xml"
   name "TechSmith Capture"
   desc "Screen capture software"
   homepage "https://www.techsmith.com/jing-tool.html"
+
+  livecheck do
+    url "https://assets.techsmith.com/techsmithcapture/mac/TechSmithCaptureAppcast.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :high_sierra"
