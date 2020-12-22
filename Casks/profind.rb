038c615c5931466e686d3da0b049aa9632c037f7
Markus Reiter <me@reitermark.us>
2020-12-22 17:43:28 +0100

    profind: add livecheck (#96542)

--- Casks/profind.rb
+++ Casks/profind.rb
@@ -1,11 +1,15 @@
 cask "profind" do
-  version "1.8.9"
+  version "1.8.9,1817"
   sha256 "39d94a8dd273312e05af12afbf328ff6cbd95f8511c6291a7833df80e240b59d"
 
-  url "https://www.zeroonetwenty.com/profind/downloads/ProFind#{version.no_dots}.dmg"
-  appcast "https://www.zeroonetwenty.com/profind/downloads/ProFindVersionInfo.xml"
+  url "https://www.zeroonetwenty.com/profind/downloads/ProFind#{version.before_comma.no_dots}.dmg"
   name "ProFind"
   homepage "https://www.zeroonetwenty.com/profind/"
+
+  livecheck do
+    url "https://www.zeroonetwenty.com/profind/downloads/ProFindVersionInfo.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :high_sierra"
 
