9749c485abe0978a34b8468106b52828ea66d3d0
Markus Reiter <me@reitermark.us>
2020-12-22 19:47:18 +0100

    unicodechecker: add livecheck (#96681)

--- Casks/unicodechecker.rb
+++ Casks/unicodechecker.rb
@@ -3,9 +3,13 @@ cask "unicodechecker" do
   sha256 "3c173ef1984cc42baa0dd03cfbd4d07cf18c75e3699b2c2a3a24250ac77fbe4f"
 
   url "https://earthlingsoft.net/UnicodeChecker/UnicodeChecker%20#{version.before_comma}%20(#{version.after_comma}).zip"
-  appcast "https://earthlingsoft.net/UnicodeChecker/appcast.xml"
   name "UnicodeChecker"
   homepage "https://earthlingsoft.net/UnicodeChecker/"
+
+  livecheck do
+    url "https://earthlingsoft.net/UnicodeChecker/appcast.xml"
+    strategy :sparkle
+  end
 
   app "UnicodeChecker #{version.before_comma} (#{version.after_comma})/UnicodeChecker.app"
 
