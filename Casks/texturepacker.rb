5666b7c16869167f35691468540830999e33f104
Markus Reiter <me@reitermark.us>
2020-12-22 04:48:42 +0100

    texturepacker: add livecheck

--- Casks/texturepacker.rb
+++ Casks/texturepacker.rb
@@ -3,9 +3,13 @@ cask "texturepacker" do
   sha256 "7055bed6c6ee278143431755d1dd9026611e4d9aba396b74de10cb09c270e108"
 
   url "https://www.codeandweb.com/download/texturepacker/#{version}/TexturePacker-#{version}-uni.dmg"
-  appcast "https://www.codeandweb.com/releases/TexturePacker/appcast-mac-release.xml"
   name "TexturePacker"
   homepage "https://www.codeandweb.com/texturepacker"
+
+  livecheck do
+    url "https://www.codeandweb.com/releases/TexturePacker/appcast-mac-release.xml"
+    strategy :sparkle
+  end
 
   app "TexturePacker.app"
 end
