bff48c3ec0ad33eb96190718c0326ef5de7e6adc
Markus Reiter <me@reitermark.us>
2020-12-22 19:06:31 +0100

    todos: add livecheck (#96666)

--- Casks/todos.rb
+++ Casks/todos.rb
@@ -3,9 +3,13 @@ cask "todos" do
   sha256 :no_check
 
   url "http://dbachrach.com/opensoft/downloads/apps/Todos.dmg"
-  appcast "http://www.dbachrach.com/opensoft/appcasts/Todos.xml"
   name "Todos"
   homepage "http://dbachrach.com/opensoft/index.php?page=Todos"
+
+  livecheck do
+    url "http://www.dbachrach.com/opensoft/appcasts/Todos.xml"
+    strategy :sparkle
+  end
 
   app "Todos.app"
 end
