0d83a05e88b0bdf7556efbabd50d0bc2940468b1
Markus Reiter <me@reitermark.us>
2020-12-22 19:49:48 +0100

    usenapp: add livecheck (#96683)

--- Casks/usenapp.rb
+++ Casks/usenapp.rb
@@ -1,12 +1,16 @@
 cask "usenapp" do
-  version "0.94.4"
+  version "0.94.4,278"
   sha256 "ea3f0a1fc19eb45529d367204b9ff0f65d4d6749c599384fb43c83058e561c9a"
 
-  url "https://www.usenapp.com/download/Usenapp-#{version}.dmg"
-  appcast "https://www.usenapp.com/download/appcast_unp1.xml"
+  url "https://www.usenapp.com/download/Usenapp-#{version.before_comma}.dmg"
   name "Usenapp"
   desc "Newsreader and Usenet client"
   homepage "https://www.usenapp.com/"
+
+  livecheck do
+    url "https://www.usenapp.com/download/appcast_unp1.xml"
+    strategy :sparkle
+  end
 
   app "Usenapp.app"
 
