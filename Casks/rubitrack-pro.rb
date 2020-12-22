43eaf469279c8171e5fe4b1fdee5beae3589f7e9
Markus Reiter <me@reitermark.us>
2020-12-22 17:57:46 +0100

    rubitrack-pro: add livecheck (#96561)

--- Casks/rubitrack-pro.rb
+++ Casks/rubitrack-pro.rb
@@ -1,11 +1,15 @@
 cask "rubitrack-pro" do
-  version "5.3.3"
+  version "5.3.3,3200"
   sha256 "77f7fa4d251ea48eaa5d59792ac56b76f3fa822ee67172103fd2112a0edea714"
 
-  url "https://www.rubitrack.com/files/rubiTrack-#{version}.dmg"
-  appcast "https://www.rubitrack.com/autoupdate/sparkle#{version.major}.xml"
+  url "https://www.rubitrack.com/files/rubiTrack-#{version.before_comma}.dmg"
   name "rubiTrack"
   homepage "https://www.rubitrack.com/"
+
+  livecheck do
+    url "https://www.rubitrack.com/autoupdate/sparkle#{version.major}.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :sierra"
 
