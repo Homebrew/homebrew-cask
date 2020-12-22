ba4d1c563c90eb211feaf642e61b88612d853e69
Markus Reiter <me@reitermark.us>
2020-12-22 04:07:40 +0100

    skim: add livecheck (#96582)

--- Casks/skim.rb
+++ Casks/skim.rb
@@ -1,13 +1,17 @@
 cask "skim" do
-  version "1.5.13"
+  version "1.5.13,129"
   sha256 "6aff3b3a8e214d7fa3c95714e8f2815b698d4d9096b8e4ac01299c96d3037a81"
 
-  url "https://downloads.sourceforge.net/skim-app/Skim/Skim-#{version}/Skim-#{version}.dmg",
+  url "https://downloads.sourceforge.net/skim-app/Skim/Skim-#{version.before_comma}/Skim-#{version.before_comma}.dmg",
       verified: "downloads.sourceforge.net/skim-app/"
-  appcast "https://skim-app.sourceforge.io/skim.xml"
   name "Skim"
   desc "PDF reader and note-taking application"
   homepage "https://skim-app.sourceforge.io/"
+
+  livecheck do
+    url "https://skim-app.sourceforge.io/skim.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
 
