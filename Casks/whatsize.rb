6cc49bd37c5f311f75855f9da16be69fb379ba1e
Markus Reiter <me@reitermark.us>
2020-12-22 21:10:38 +0100

    whatsize: add livecheck (#96702)

--- Casks/whatsize.rb
+++ Casks/whatsize.rb
@@ -1,12 +1,16 @@
 cask "whatsize" do
-  version "7.6.4"
+  version "7.6.4,7640"
   sha256 "06c317d247ac0b01cbab146c6d6a7f70506223fbb39a32a7ebc55cfe63e8a92e"
 
-  url "https://www.whatsizemac.com/software/whatsize#{version.major}/whatsize_#{version}.tgz"
-  appcast "https://www.whatsizemac.com/software/whatsize#{version.major}/release/notes.xml"
+  url "https://www.whatsizemac.com/software/whatsize#{version.major}/whatsize_#{version.before_comma}.tgz"
   name "WhatSize"
   desc "File system utility used to view and reclaim disk space"
   homepage "https://www.whatsizemac.com/"
+
+  livecheck do
+    url "https://www.whatsizemac.com/software/whatsize#{version.major}/release/notes.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :sierra"
 
