abb1e7ac248c1e318f190046aacfe672b9bb4205
Markus Reiter <me@reitermark.us>
2020-12-22 05:03:39 +0100

    rectangle: add livecheck (#96548)

--- Casks/rectangle.rb
+++ Casks/rectangle.rb
@@ -1,13 +1,17 @@
 cask "rectangle" do
-  version "0.40"
+  version "0.40,45"
   sha256 "70ad0351d512e0538d5b3ffc94dd48038d2e5e68077be8dc93f113baa21c3d3e"
 
-  url "https://github.com/rxhanson/Rectangle/releases/download/v#{version}/Rectangle#{version}.dmg",
+  url "https://github.com/rxhanson/Rectangle/releases/download/v#{version.before_comma}/Rectangle#{version.before_comma}.dmg",
       verified: "github.com/rxhanson/Rectangle/"
-  appcast "https://www.rectangleapp.com/downloads/updates.xml"
   name "Rectangle"
   desc "Move and resize windows using keyboard shortcuts or snap areas"
   homepage "https://rectangleapp.com/"
+
+  livecheck do
+    url "https://www.rectangleapp.com/downloads/updates.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :el_capitan"
