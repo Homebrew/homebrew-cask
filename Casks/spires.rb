1c04e3a91e03f5a6c2a81d2fe84ebdd39084a667
Markus Reiter <me@reitermark.us>
2020-12-22 17:58:47 +0100

    spires: add livecheck (#96604)

--- Casks/spires.rb
+++ Casks/spires.rb
@@ -3,10 +3,14 @@ cask "spires" do
   sha256 "4c894aa95860eed5f71fa8ba124026060e903b970645bcb6e89d252257ebf27d"
 
   url "https://member.ipmu.jp/yuji.tachikawa/spires/zips/spires_#{version}.zip"
-  appcast "https://member.ipmu.jp/yuji.tachikawa/spires/spires-Update.xml"
   name "spires"
   desc "Frontend for inspire-hep and arxiv"
   homepage "https://member.ipmu.jp/yuji.tachikawa/spires/"
+
+  livecheck do
+    url "https://member.ipmu.jp/yuji.tachikawa/spires/spires-Update.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :el_capitan"
