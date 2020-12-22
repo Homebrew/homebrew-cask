b9f2602941d3e0c293cec37627c80bde43f0cb2d
Markus Reiter <me@reitermark.us>
2020-12-22 17:40:01 +0100

    synalyze-it-pro: add livecheck (#96623)

--- Casks/synalyze-it-pro.rb
+++ Casks/synalyze-it-pro.rb
@@ -4,10 +4,14 @@ cask "synalyze-it-pro" do
 
   url "https://www.synalyze-it.com/Downloads/SynalyzeItProTA_#{version}.zip",
       verified: "synalyze-it.com/Downloads/"
-  appcast "https://www.synalyze-it.com/SynalyzeItPro/appcast.xml"
   name "Synalyze It! Pro"
   desc "Hex editing and binary file analysis app"
   homepage "https://www.synalysis.net/"
+
+  livecheck do
+    url "https://www.synalyze-it.com/SynalyzeItPro/appcast.xml"
+    strategy :sparkle
+  end
 
   app "Synalyze It! Pro.app"
 end
