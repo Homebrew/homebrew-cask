3934400a15be03780f33888e746e4a6116a7121d
Markus Reiter <me@reitermark.us>
2020-12-22 18:47:30 +0100

    twist: add livecheck (#96674)

--- Casks/twist.rb
+++ Casks/twist.rb
@@ -3,10 +3,14 @@ cask "twist" do
   sha256 "8dfadf3cb9f952f04c5aa3eb37c0298151fb831b5ed70ff35682d8af692eedae"
 
   url "https://downloads.twist.com/mac/Twist-#{version.before_comma}.dmg"
-  appcast "https://downloads.twist.com/mac/AppCast.xml"
   name "Twist"
   desc "Team communication and collaboration software"
   homepage "https://twist.com/"
+
+  livecheck do
+    url "https://downloads.twist.com/mac/AppCast.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :el_capitan"
 
