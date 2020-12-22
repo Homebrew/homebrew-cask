7b3ccd5ab2771851a8539f0e5afdb5f7071be31f
Markus Reiter <me@reitermark.us>
2020-12-22 04:09:00 +0100

    scrivener: add livecheck (#96568)

--- Casks/scrivener.rb
+++ Casks/scrivener.rb
@@ -4,10 +4,14 @@ cask "scrivener" do
 
   url "https://scrivener.s3.amazonaws.com/mac_updates/Scrivener_1012_#{version.after_comma}.zip",
       verified: "scrivener.s3.amazonaws.com/"
-  appcast "https://www.literatureandlatte.com/downloads/scrivener-#{version.major}.xml"
   name "Scrivener"
   desc "Word processing software with a typewriter style"
   homepage "https://literatureandlatte.com/scrivener.php"
+
+  livecheck do
+    url "https://www.literatureandlatte.com/downloads/scrivener-#{version.major}.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :sierra"
