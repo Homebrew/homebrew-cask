216a0cbf7ec42fe80415d1a5161aa9b93590bf0a
Markus Reiter <me@reitermark.us>
2020-12-22 22:23:02 +0100

    bettertouchtool: add livecheck (#96184)

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -1,12 +1,16 @@
 cask "bettertouchtool" do
-  version "3.508-1646"
+  version "3.508,1646"
   sha256 "51b7c0ed3aacf0f9b3a6825dde1cbbdcdf9e1ceeb35e315db119014f1ffac81d"
 
-  url "https://folivora.ai/releases/btt#{version}.zip"
-  appcast "https://updates.folivora.ai/bettertouchtool_release_notes.html"
+  url "https://folivora.ai/releases/btt#{version.before_comma}-#{version.after_comma}.zip"
   name "BetterTouchTool"
   desc "Tool to customize input devices and automate computer systems"
   homepage "https://folivora.ai/"
+
+  livecheck do
+    url "https://updates.folivora.ai/appcast.xml?trial=1"
+    strategy :sparkle
+  end
 
   auto_updates true
 
