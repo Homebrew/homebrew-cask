42499098a8b3213897a4c439374d46fbc4baad04
Markus Reiter <me@reitermark.us>
2020-12-22 05:01:50 +0100

    outline: add livecheck (#96503)

--- Casks/outline.rb
+++ Casks/outline.rb
@@ -1,12 +1,16 @@
 cask "outline" do
-  version "3.2011.1"
+  version "3.2011.1,2011.1.1"
   sha256 "051bf998a2c99cdc72ac9de3d525a8c779995977a74fc14ec9ce129830d59b5e"
 
-  url "https://static.outline.ws/versions/Outline_#{version}.zip"
-  appcast "https://gorillized.s3.amazonaws.com/versions/update_channel.xml"
+  url "https://static.outline.ws/versions/Outline_#{version.before_comma}.zip"
   name "Outline"
   desc "Note taking app"
   homepage "https://outline.ws/"
+
+  livecheck do
+    url "https://gorillized.s3.amazonaws.com/versions/update_channel.xml"
+    strategy :sparkle
+  end
 
   app "Outline.app"
 end
