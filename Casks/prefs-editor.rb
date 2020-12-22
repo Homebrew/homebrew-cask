b6e05155da13bcec601d02fab691dcfdf4fb5775
Markus Reiter <me@reitermark.us>
2020-12-22 03:54:36 +0100

    prefs-editor: add livecheck (#96533)

--- Casks/prefs-editor.rb
+++ Casks/prefs-editor.rb
@@ -1,12 +1,16 @@
 cask "prefs-editor" do
-  version "1.2.5"
+  version "1.2.5.3.1"
   sha256 "1d59f2094fab451d8d669aa5490a8dadc4d5f534f78f10dbefa1cb4eed7f3a45"
 
-  url "https://files.tempel.org/Various/OSX_Prefs_Editor/PrefsEditor-#{version}_64bit.zip"
-  appcast "https://apps.tempel.org/PrefsEditor/appcast.xml"
+  url "https://files.tempel.org/Various/OSX_Prefs_Editor/PrefsEditor-#{version.major_minor_patch}_64bit.zip"
   name "Prefs Editor"
   desc "Graphical user interface for the 'defaults' command"
   homepage "https://apps.tempel.org/PrefsEditor/"
+
+  livecheck do
+    url "https://apps.tempel.org/PrefsEditor/appcast.xml"
+    strategy :sparkle
+  end
 
   app "Prefs Editor.app"
 
