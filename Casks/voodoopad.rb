088f8c429171038cca31d1a38ce6a5650a59f48f
Markus Reiter <me@reitermark.us>
2020-12-22 20:29:59 +0100

    voodoopad: add livecheck (#96692)

--- Casks/voodoopad.rb
+++ Casks/voodoopad.rb
@@ -1,12 +1,16 @@
 cask "voodoopad" do
-  version "5.3.0"
+  version "5.3.0,6450"
   sha256 "a2eb6a042df78170a88a9909fb40ee92bc407b4c95f414cac5b1fb8ddef390ce"
 
-  url "https://voodoopad.s3.amazonaws.com/VoodooPad-#{version}.zip",
+  url "https://voodoopad.s3.amazonaws.com/VoodooPad-#{version.before_comma}.zip",
       verified: "voodoopad.s3.amazonaws.com/"
-  appcast "https://www.primatelabs.com/appcast/voodoopad#{version.major}.xml"
   name "VoodooPad"
-  homepage "https://www.voodoopad.com/"
+  homepage "http://www.voodoopad.com/"
+
+  livecheck do
+    url "https://www.primatelabs.com/appcast/voodoopad#{version.major}.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :sierra"
 
