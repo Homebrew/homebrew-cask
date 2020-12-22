15a28651156e1a0caaf4ffd12c5905a6ee27939a
Markus Reiter <me@reitermark.us>
2020-12-22 22:01:23 +0100

    yam-display: add livecheck (#96716)

--- Casks/yam-display.rb
+++ Casks/yam-display.rb
@@ -3,9 +3,13 @@ cask "yam-display" do
   sha256 "9d7ea3042119dbbc83d1a243e4fb4c3f2795678d4a5fe51fd67f4987e6ccb859"
 
   url "https://www.yamdisplay.com/app/YamDisplay#{version}.zip"
-  appcast "https://www.yamdisplay.com/app/appcast.xml"
   name "Yam Display"
   homepage "https://www.yamdisplay.com/"
+
+  livecheck do
+    url "https://www.yamdisplay.com/app/appcast.xml"
+    strategy :sparkle
+  end
 
   app "Yam Display.app"
 
