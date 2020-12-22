90175b61e400ab08173c1fbeaf36658af7f9e3b5
Markus Reiter <me@reitermark.us>
2020-12-22 17:34:34 +0100

    textual: add livecheck (#96638)

--- Casks/textual.rb
+++ Casks/textual.rb
@@ -1,12 +1,18 @@
 cask "textual" do
-  version "7.1.6,8cdeb222c"
+  version "7.1.6,200409.17:8cdeb222c"
   sha256 "09b69a385f9c770b020ffe18b5d11515eed11f20001a21cc6758defe621b43d4"
 
-  url "https://cached.codeux.com/textual/downloads/resources/builds/standard-release/Textual-#{version.after_comma}.zip"
-  appcast "https://textual-updates-backend.codeux.com/sparkle/feeds/v7/feed-one.xml"
+  url "https://cached.codeux.com/textual/downloads/resources/builds/standard-release/Textual-#{version.after_colon}.zip"
   name "Textual"
   desc "Application for interacting with Internet Relay Chat (IRC) chatrooms"
   homepage "https://www.codeux.com/textual/"
+
+  livecheck do
+    url "https://textual-updates-backend.codeux.com/sparkle/feeds/v7/feed-one.xml"
+    strategy :sparkle do |item|
+      "#{item.short_version},#{item.version}:#{item.url[/Textual-(.*?)\.zip/i, 1]}"
+    end
+  end
 
   auto_updates true
 
