8f932b8f9aee7c75cf30f691f3cd9f6682801cb2
Markus Reiter <me@reitermark.us>
2020-12-22 21:37:08 +0100

    xld: add livecheck (#96712)

--- Casks/xld.rb
+++ Casks/xld.rb
@@ -1,15 +1,18 @@
 cask "xld" do
-  version "2020.11.23"
+  version "20201123,153.0"
   sha256 "1ad1b38dac80694da805b154ed031b0df3d9019e719a9b6d1c4a9d4351a72b70"
 
-  url "https://downloads.sourceforge.net/xld/xld-#{version.no_dots}.dmg",
+  url "https://downloads.sourceforge.net/xld/xld-#{version.before_comma}.dmg",
       verified: "sourceforge.net/xld/"
-  appcast "https://svn.code.sf.net/p/xld/code/appcast/xld-appcast_e.xml",
-          must_contain: version.no_dots
   name "X Lossless Decoder"
   name "XLD"
   desc "Lossless audio decoder"
   homepage "https://tmkk.undo.jp/xld/index_e.html"
+
+  livecheck do
+    url "https://svn.code.sf.net/p/xld/code/appcast/xld-appcast_e.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
 
