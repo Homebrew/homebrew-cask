3464799fc99f8e3c4863202f9a6034f5d959f2e1
Markus Reiter <me@reitermark.us>
2020-12-22 05:05:23 +0100

    paintbrush: add livecheck (#96505)

--- Casks/paintbrush.rb
+++ Casks/paintbrush.rb
@@ -1,13 +1,19 @@
 cask "paintbrush" do
-  version "2.5.0"
+  version "2.5.0,20190914"
   sha256 "fca1d23f6da1ff1fe6768ba67cd692362cbf86ad9adf72431514740a37c513b6"
 
-  url "https://downloads.sourceforge.net/paintbrush/Paintbrush%202.x/Paintbrush%20#{version}/Paintbrush-#{version}.zip",
+  url "https://downloads.sourceforge.net/paintbrush/Paintbrush%202.x/Paintbrush%20#{version.before_comma}/Paintbrush-#{version.before_comma}.zip",
       verified: "downloads.sourceforge.net/paintbrush/"
-  appcast "https://paintbrush.sourceforge.io/updates#{version.major}x.xml"
   name "Paintbrush"
   desc "Image editor"
   homepage "https://paintbrush.sourceforge.io/"
+
+  livecheck do
+    url "https://paintbrush.sourceforge.io/updates#{version.major}x.xml"
+    strategy :sparkle do |item|
+      "#{item.short_version}.0,#{item.version}"
+    end
+  end
 
   app "Paintbrush.app"
 
