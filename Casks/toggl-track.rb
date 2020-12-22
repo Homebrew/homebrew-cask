6ff4d45456c4ba746d5ebbbc993c51d4154ccc70
Markus Reiter <me@reitermark.us>
2020-12-22 18:11:23 +0100

    toggl-track: add livecheck (#96667)

--- Casks/toggl-track.rb
+++ Casks/toggl-track.rb
@@ -4,10 +4,14 @@ cask "toggl-track" do
 
   url "https://github.com/toggl-open-source/toggldesktop/releases/download/v#{version}/TogglDesktop-#{version.dots_to_underscores}.dmg",
       verified: "github.com/toggl-open-source/toggldesktop/"
-  appcast "https://toggl-open-source.github.io/toggldesktop/assets/releases/darwin_stable_appcast.xml"
   name "Toggl Track"
   desc "Time tracker"
   homepage "https://www.toggl.com/track/toggl-desktop/"
+
+  livecheck do
+    url "https://toggl-open-source.github.io/toggldesktop/assets/releases/darwin_stable_appcast.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
 
