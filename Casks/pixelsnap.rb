b5ec8c38b7c6b62413fb777a2601c8e88143a68c
Markus Reiter <me@reitermark.us>
2020-12-22 05:04:28 +0100

    pixelsnap: add livecheck (#96523)

--- Casks/pixelsnap.rb
+++ Casks/pixelsnap.rb
@@ -3,10 +3,14 @@ cask "pixelsnap" do
   sha256 "5a01820c1163cab3b5625cdeee09e84c52d07b6e3171bfb1234fea786750f16f"
 
   url "https://updates.getpixelsnap.com/v#{version.major}/PixelSnap-#{version.major}-#{version}.dmg"
-  appcast "https://updates.getpixelsnap.com/v#{version.major}/appcast.xml"
   name "PixelSnap"
   desc "Screen measuring tool"
   homepage "https://getpixelsnap.com/"
+
+  livecheck do
+    url "https://updates.getpixelsnap.com/v#{version.major}/appcast.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
 
