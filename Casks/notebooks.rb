2dae77bd609821313f73eeda42bee7865dce100b
Markus Reiter <me@reitermark.us>
2020-12-22 04:09:35 +0100

    notebooks: add livecheck (#96496)

--- Casks/notebooks.rb
+++ Casks/notebooks.rb
@@ -1,11 +1,15 @@
 cask "notebooks" do
-  version "2.3.1"
-  sha256 "3516ad3f39619822c486984f924ec2598ec344be81ba09420a81a5286076abad"
+  version "2.3.3,205"
+  sha256 "55a1d8caca12085092f5f1cdd96caa94c2528999733348e3f605b11a0a375028"
 
   url "https://www.notebooksapp.com/Download/macOS/v#{version.major}/Notebooks.dmg"
-  appcast "https://notebooksapp.com/Download/macOS/v#{version.major}/Notebooks#{version.major}Appcast.xml"
   name "Notebooks"
   homepage "https://www.notebooksapp.com/mac/"
+
+  livecheck do
+    url "https://notebooksapp.com/Download/macOS/v#{version.major}/Notebooks#{version.major}Appcast.xml"
+    strategy :sparkle
+  end
 
   app "Notebooks.app"
 end
