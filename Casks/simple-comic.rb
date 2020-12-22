9dbe7de7992750caac159990a2c5297c3cd0abe4
Bogdan Brinza <boggydigital@users.noreply.github.com>
2020-12-21 21:42:49 -0800

    Update simple-comic.rb to remove verified
    New community fork doesn't have a dedicated website, and verified only applies when download is different from homepage

--- Casks/simple-comic.rb
+++ Casks/simple-comic.rb
@@ -2,8 +2,7 @@ cask "simple-comic" do
   version "1.9.1"
   sha256 "d86e95504106718cd73f2eea8505428cabfdcfa42bc0cc7028807aa865e1d9b3"
 
-  url "https://github.com/MaddTheSane/Simple-Comic/releases/download/#{version}/Simple.Comic.app.zip",
-      verified: "https://github.com/MaddTheSane/Simple-Comic"
+  url "https://github.com/MaddTheSane/Simple-Comic/releases/download/#{version}/Simple.Comic.app.zip"
   appcast "https://github.com/MaddTheSane/Simple-Comic/releases.atom"
   name "Simple Comic"
   desc "Streamlined comic viewer"
