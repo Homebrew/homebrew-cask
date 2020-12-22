5e9b8040742ad08d9bacfe46f2daef1c6dd66845
Markus Reiter <me@reitermark.us>
2020-12-22 17:28:07 +0100

    the-tagger: add livecheck

--- Casks/the-tagger.rb
+++ Casks/the-tagger.rb
@@ -3,9 +3,13 @@ cask "the-tagger" do
   sha256 :no_check
 
   url "https://deadbeatsw.com/thetagger/TheTaggerLatest.zip"
-  appcast "https://deadbeatsw.com/thetagger/appcast.xml"
   name "The Tagger"
   homepage "https://deadbeatsw.com/thetagger/"
+
+  livecheck do
+    url "https://deadbeatsw.com/thetagger/appcast.xml"
+    strategy :sparkle
+  end
 
   app "The Tagger.app"
 end
