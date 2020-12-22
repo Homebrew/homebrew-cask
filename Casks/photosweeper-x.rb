b3f7b47b9264f4db01f8209102b57b5581894b31
Markus Reiter <me@reitermark.us>
2020-12-22 03:56:31 +0100

    photosweeper-x: add livecheck (#96519)

--- Casks/photosweeper-x.rb
+++ Casks/photosweeper-x.rb
@@ -3,10 +3,14 @@ cask "photosweeper-x" do
   sha256 :no_check
 
   url "https://overmacs.com/downloads/PhotoSweeper_X.dmg"
-  appcast "https://overmacs.com/feeds/photosweeper_update.xml"
   name "PhotoSweeper X"
   desc "Tool to eliminate similar or duplicate photos"
   homepage "https://overmacs.com/"
+
+  livecheck do
+    url "https://overmacs.com/feeds/photosweeper_update.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :sierra"
 
