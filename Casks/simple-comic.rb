9cf35769e0b564c15096248cc1eb180ec8eb8fc0
Bogdan Brinza <boggydigital@users.noreply.github.com>
2020-12-21 18:52:15 -0800

    Update Simple Comic to a community maintained fork
    Simple Comic homepage (http://dancingtortoise.com/simplecomic/) used to link to https://github.com/arauchfuss/Simple-Comic. However the last commit happened in Apr 2017 and after a discussion community has created a new modern maintained fork: https://github.com/MaddTheSane/Simple-Comic
    
    Here is the issue where the discussion happened with more details: https://github.com/arauchfuss/Simple-Comic/issues/126
    
    In this PR, I'm intentionally redirecting download and other links to this new community maintained fork. Let me know if there is other information that would help here.

--- Casks/simple-comic.rb
+++ Casks/simple-comic.rb
@@ -1,13 +1,13 @@
 cask "simple-comic" do
-  version "1.7_252"
-  sha256 "4ddd18a02a79fc8201824e6ab99291c6d4c8680f79f94bc372bf71f0535def35"
+  version "1.9.1"
+  sha256 "d86e95504106718cd73f2eea8505428cabfdcfa42bc0cc7028807aa865e1d9b3"
 
-  url "https://github.com/dancingtortoise/Simple-Comic/releases/download/#{version}/SimpleComic_#{version}.zip",
-      verified: "github.com/dancingtortoise/Simple-Comic/"
-  appcast "https://github.com/dancingtortoise/Simple-Comic/releases.atom"
+  url "https://github.com/MaddTheSane/Simple-Comic/releases/download/#{version}/Simple.Comic.app.zip",
+      verified: "https://github.com/MaddTheSane/Simple-Comic"
+  appcast "https://github.com/MaddTheSane/Simple-Comic/releases.atom"
   name "Simple Comic"
-  desc "Comic viewer/reader"
-  homepage "https://dancingtortoise.github.io/"
+  desc "Streamlined comic viewer for macOS"
+  homepage "https://github.com/MaddTheSane/Simple-Comic"
 
   app "Simple Comic.app"
 end
