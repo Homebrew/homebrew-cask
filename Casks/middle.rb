109781107a68cbfca1b31d1173f2e73697dbe912
Markus Reiter <me@reitermark.us>
2020-12-23 05:58:20 +0100

    middle: add livecheck (#96465)

--- Casks/middle.rb
+++ Casks/middle.rb
@@ -1,11 +1,15 @@
 cask "middle" do
-  version "1.5.13"
+  version "1.5.13,47"
   sha256 "86be3b15c6566fcc45e99fdcc217c3fd234bd485f64a70ab2074007dc797f5de"
 
-  url "https://middleclick.app/downloads/Middle#{version}.dmg"
-  appcast "https://middleclick.app/downloads/updates.xml"
+  url "https://middleclick.app/downloads/Middle#{version.before_comma}.dmg"
   name "Middle"
   homepage "https://middleclick.app/"
+
+  livecheck do
+    url "https://middleclick.app/downloads/updates.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
 
