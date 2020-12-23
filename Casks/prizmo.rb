5c9863b6c4b8860381c7ff95e9f5a091fd162881
Markus Reiter <me@reitermark.us>
2020-12-23 06:29:59 +0100

    prizmo: add livecheck (#96539)

--- Casks/prizmo.rb
+++ Casks/prizmo.rb
@@ -1,13 +1,17 @@
 cask "prizmo" do
-  version "4.1"
+  version "4.1,4.276.2226"
   sha256 "ec6e8d70f604efa17b9380ef1e45b4ee93d232f7942098192a27a6e35d2501d6"
 
-  url "https://creaceed.s3.amazonaws.com/downloads/prizmo#{version.major}_#{version}.zip",
+  url "https://creaceed.s3.amazonaws.com/downloads/prizmo#{version.major}_#{version.before_comma}.zip",
       verified: "creaceed.s3.amazonaws.com/downloads/"
-  appcast "https://www.creaceed.com/appcasts/prizmo#{version.major}.xml"
   name "Prizmo"
   desc "Scanning application with Optical Character Recognition (OCR)"
   homepage "https://creaceed.com/prizmo"
+
+  livecheck do
+    url "https://creaceed.com/appcasts/prizmo#{version.major}.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :yosemite"
