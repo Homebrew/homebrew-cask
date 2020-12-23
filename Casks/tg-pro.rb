adeb679fda379cc938b285ade48438afd3d39975
Markus Reiter <me@reitermark.us>
2020-12-23 06:46:21 +0100

    tg-pro: add livecheck (#96657)

--- Casks/tg-pro.rb
+++ Casks/tg-pro.rb
@@ -1,12 +1,16 @@
 cask "tg-pro" do
-  version "2.54"
+  version "2.54,11017"
   sha256 "ab4ddbaa75ce74881b192d271b4a07beb209f359b998242dde78dfc4011720ba"
 
-  url "https://www.tunabellysoftware.com/resources/TG%20Pro%20#{version}.dmg"
-  appcast "https://www.tunabellysoftware.com/resources/sparkle/tgpro.xml"
+  url "https://www.tunabellysoftware.com/resources/TG%20Pro%20#{version.before_comma}.dmg"
   name "TG Pro"
   desc "Temperature monitoring, fan control and diagnostics"
   homepage "https://www.tunabellysoftware.com/tgpro/"
+
+  livecheck do
+    url "https://www.tunabellysoftware.com/resources/sparkle/tgpro.xml"
+    strategy :sparkle
+  end
 
   app "TG Pro.app"
 
