fdd2eef04c4d372194aa24100502017c58e15df3
Markus Reiter <me@reitermark.us>
2020-12-23 06:18:14 +0100

    multitouch: add livecheck (#96480)

--- Casks/multitouch.rb
+++ Casks/multitouch.rb
@@ -1,11 +1,15 @@
 cask "multitouch" do
-  version "1.21"
+  version "1.21,121"
   sha256 "4d3d4467ffbb55ea4644a401df54ddffc60b5673bd61131e16a0c51e552ab7f3"
 
-  url "https://multitouch.app/downloads/multitouch#{version}.dmg"
-  appcast "https://www.multitouch.app/downloads/updates.xml"
+  url "https://multitouch.app/downloads/multitouch#{version.before_comma}.dmg"
   name "Multitouch"
   homepage "https://multitouch.app/"
+
+  livecheck do
+    url "https://www.multitouch.app/downloads/updates.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :sierra"
