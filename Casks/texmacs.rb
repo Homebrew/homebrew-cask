ad43d7d1df8e9f7dfe690fc7705b68ff1dcadd53
Markus Reiter <me@reitermark.us>
2020-12-23 14:47:53 +0100

    Update texmacs.rb

--- Casks/texmacs.rb
+++ Casks/texmacs.rb
@@ -2,13 +2,13 @@ cask "texmacs" do
   version "1.99.17"
   sha256 "8b525fea84a4afe93a9c651caf34e4cbca2f2f9d596b060f6f95fa85683a33d8"
 
-  url "http://ftp.texmacs.org/TeXmacs/tmftp/macos/TeXmacs-#{version}.dmg"
+  url "https://ftp.texmacs.org/TeXmacs/tmftp/macos/TeXmacs-#{version}.dmg"
   name "GNU TeXmacs"
   desc "Scientific editing platform"
   homepage "https://www.texmacs.org/"
 
   livecheck do
-    url "http://ftp.texmacs.org/TeXmacs/appcast/macos.xml"
+    url "https://ftp.texmacs.org/TeXmacs/appcast/macos.xml"
     strategy :sparkle
   end
 
