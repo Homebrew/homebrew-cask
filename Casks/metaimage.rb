7089a227001184c8ad83077dd9d37f357c781b11
Reno <25192197+singularitti@users.noreply.github.com>
2020-12-23 00:43:31 -0500

    Update metaimage from 1.9.4,6086 to 1.9.5,6087 (#96728)

--- Casks/metaimage.rb
+++ Casks/metaimage.rb
@@ -1,6 +1,6 @@
 cask "metaimage" do
-  version "1.9.4,6086"
-  sha256 "90e69d5b6ef346803eb012ce3a81fd1798a972e215904c60a7a9953172dccf66"
+  version "1.9.5,6087"
+  sha256 "f165da2b29fe44ff56f514b88aa7e10f210d3c3790926394e90d42240bdcaaf6"
 
   url "https://neededapps.com/appcasts/metaimage/versions/MI-#{version.before_comma}.zip"
   name "MetaImage"
