9b5f11db4930258abed2bd68ab5b4b98c83c8985
Markus Reiter <me@reitermark.us>
2020-12-24 09:07:35 +0100

    texmacs: add livecheck (#96633)
    * texmacs: add livecheck
    
    * Update texmacs.rb
    
    * Update texmacs.rb
    
    * Update texmacs.rb
    
    * Update texmacs.rb

--- Casks/texmacs.rb
+++ Casks/texmacs.rb
@@ -2,11 +2,15 @@ cask "texmacs" do
   version "1.99.17"
   sha256 "8b525fea84a4afe93a9c651caf34e4cbca2f2f9d596b060f6f95fa85683a33d8"
 
-  url "https://www.texmacs.org/Download/ftp/tmftp/macos/TeXmacs-#{version}.dmg"
-  appcast "https://ftp.texmacs.org/TeXmacs/appcast/macos.xml"
+  url "https://ftp.texmacs.org/TeXmacs/tmftp/macos/TeXmacs-#{version}.dmg"
   name "GNU TeXmacs"
   desc "Scientific editing platform"
   homepage "https://www.texmacs.org/"
+
+  livecheck do
+    url "https://ftp.texmacs.org/TeXmacs/appcast/macos.xml"
+    strategy :sparkle
+  end
 
   app "TeXmacs.app"
 
