4f55488661801c7b8881c6f4a4a454a12c94bf10
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-24 16:00:16 +0800

    Update spectx.rb

--- Casks/spectx.rb
+++ Casks/spectx.rb
@@ -3,7 +3,7 @@ cask "spectx" do
   sha256 "053f6e9d9c7539cd86bfbbc99787483822ee28eebe8f04dd24cd6d2ccd47575d"
 
   url "https://download.spectx.com/versions/#{version}/SpectXDesktop-v#{version}.dmg"
-  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.spectx.com/get-spectx"
+  appcast "https://www.spectx.com/get-spectx"
   name "SpectX Desktop"
   desc "Parse and investigate raw log files"
   homepage "https://www.spectx.com/"
