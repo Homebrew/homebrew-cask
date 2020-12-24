46bacbf982a9b27f24cb12e7e42591612201bf78
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-24 16:11:35 +0800

    Update skychart.rb

--- Casks/skychart.rb
+++ Casks/skychart.rb
@@ -1,8 +1,8 @@
 cask "skychart" do
-  version "4.2.1"
+  version "4.2.1,4073"
   sha256 "72670b4c3d4145a2f609197d62bb07351d2e7cdce36d889d0d0f9a002158aa47"
 
-  url "https://downloads.sourceforge.net/skychart/skychart-#{version}-4073-x86_64-macosx.dmg",
+  url "https://downloads.sourceforge.net/skychart/skychart-#{version.before_comma}-#{version.after_comma}-x86_64-macosx.dmg",
       verified: "downloads.sourceforge.net/skychart/"
   appcast "https://sourceforge.net/projects/skychart/rss?path=/"
   name "Skychart"
