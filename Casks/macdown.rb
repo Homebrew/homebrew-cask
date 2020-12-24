8297c04e6bbdb656af9cd71cfc2d2039da340861
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-24 17:44:08 +0800

    Update macdown.rb

--- Casks/macdown.rb
+++ Casks/macdown.rb
@@ -2,7 +2,7 @@ cask "macdown" do
   version "0.7.2"
   sha256 "271f11eb64c19fccee2615e092067cdecc29adf0c2ed0703dae9acda8fa0a672"
 
-  url "https://github.com/MacDownApp/macdown/releases/download/v#{version.before_comma}/MacDown.app.zip",
+  url "https://github.com/MacDownApp/macdown/releases/download/v#{version}/MacDown.app.zip",
       verified: "github.com/MacDownApp/macdown/"
   name "MacDown"
   desc "Open-source Markdown editor"
