db398ffea11340e6708f94b44860d60ff9f93dd4
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-24 16:44:55 +0800

    Update futuniuniu.rb

--- Casks/futuniuniu.rb
+++ Casks/futuniuniu.rb
@@ -1,16 +1,14 @@
 cask "futuniuniu" do
-  version "10.13.736,202012011017"
-  sha256 "074c8340ebf346a030508787be5e75da852666e9cf63a20d8028d0945475f369"
+  version "10.13.738,202012151016"
+  sha256 "a329ad46b20b6d571cac32f8e42b3f0841c6baf0200b422a341bb0fd59f7ece8"
 
-  url "https://software-file-1251001049.file.myqcloud.com/FTNNForMac_#{version}_Website.dmg",
-      verified: "software-file-1251001049.file.myqcloud.com/"
-  appcast "https://www.futu5.com/tools"
+  url "https://softwarefile.futunn.com/FTNNForMac_#{version.before_comma}_#{version.after_comma}_Website.dmg"
+  appcast "https://www.futunn.com/download/mac?lang=en-US"
   name "FutuNiuniu"
   name "富途牛牛"
   desc "Trading application"
-  homepage "https://hk.futu5.com/"
+  homepage "https://www.futunn.com"
 
   # Renamed for consistency: app name is different in the Finder and in a shell.
-  # Original discussion: https://github.com/Homebrew/homebrew-cask/pull/7435
-  app "FutuNiuniu.app", target: "富途牛牛.app"
+  app "FutuNiuniu.app", target: "Futubull.app"
 end
