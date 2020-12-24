f0bdd209be11a2bb7258feecb44a28812aa6a259
Jan Václavík <honza.vaci@gmail.com>
2020-12-24 09:26:20 +0100

    Bump spectx to v1.4.68 (#96360)
    * Bump spectx to v1.4.68
    
    * Update spectx.rb
    
    Co-authored-by: Václavík Jan <hv@antstudio.cz>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/spectx.rb
+++ Casks/spectx.rb
@@ -1,9 +1,9 @@
 cask "spectx" do
-  version "1.4.53"
-  sha256 "fa74d1bfbf4772fe3dc252c3432bf6edcaff6f5a671857f7c107415fa9925e7c"
+  version "1.4.68"
+  sha256 "053f6e9d9c7539cd86bfbbc99787483822ee28eebe8f04dd24cd6d2ccd47575d"
 
-  url "https://get.spectx.com/03f21b939e022/SpectXDesktop-v#{version}.dmg"
-  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://go.spectx.com/get/?desktop-osx64"
+  url "https://download.spectx.com/versions/#{version}/SpectXDesktop-v#{version}.dmg"
+  appcast "https://www.spectx.com/get-spectx"
   name "SpectX Desktop"
   desc "Parse and investigate raw log files"
   homepage "https://www.spectx.com/"
