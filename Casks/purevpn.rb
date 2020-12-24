5059ac074ff38231222befdb1e9e097f62fcb8ff
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-24 06:22:19 +0000

    Update purevpn from 3.0.11,626 to 3.0.17,702

--- Casks/purevpn.rb
+++ Casks/purevpn.rb
@@ -1,5 +1,5 @@
 cask "purevpn" do
-  version "3.0.11,626"
+  version "3.0.17,702"
   sha256 :no_check
 
   url "https://purevpn-dialer-assets.s3.amazonaws.com/mac-2.0/packages/Production/PureVPN.pkg",
