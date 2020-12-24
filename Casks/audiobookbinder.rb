3a12fbcc289c72fc420ab1765c81e3854a66f02e
Markus Reiter <me@reitermark.us>
2020-12-24 04:52:33 +0100

    audiobookbinder: add livecheck (#96750)

--- Casks/audiobookbinder.rb
+++ Casks/audiobookbinder.rb
@@ -1,11 +1,15 @@
 cask "audiobookbinder" do
-  version "2.1"
+  version "2.1,201"
   sha256 "ed0e722cbbbcad8ea305faa10e1f5f08c7719991118d015af132bb9d41f84170"
 
-  url "https://bluezbox.com/audiobookbinder/AudiobookBinder-#{version}.dmg"
-  appcast "https://bluezbox.com/audiobookbinder/appcast.xml"
+  url "https://bluezbox.com/audiobookbinder/AudiobookBinder-#{version.before_comma}.dmg"
   name "Audiobook Binder"
   homepage "https://bluezbox.com/audiobookbinder.html"
+
+  livecheck do
+    url "https://bluezbox.com/audiobookbinder/appcast.xml"
+    strategy :sparkle
+  end
 
   app "AudioBookBinder.app"
 end
