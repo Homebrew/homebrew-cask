6f1a657a085aa00d90d4a664ad0eadce79cb2489
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-24 16:30:39 +0800

    Update screenflow.rb

--- Casks/screenflow.rb
+++ Casks/screenflow.rb
@@ -2,7 +2,7 @@ cask "screenflow" do
   version "9.0.5,31579"
   sha256 "6e54638252c59ba707457d1be954e315c155d63d87291bc45054f77d76419d99"
 
-  url "https://www.telestream.net/download-files/screenflow/#{version.major_minor.dots_to_hyphens}/ScreenFlow-#{version}.dmg"
+  url "https://www.telestream.net/download-files/screenflow/#{version.before_comma.major_minor.dots_to_hyphens}/ScreenFlow-#{version.before_comma}.dmg"
   name "ScreenFlow"
   desc "Screen recording and video editing software"
   homepage "https://www.telestream.net/screenflow/"
