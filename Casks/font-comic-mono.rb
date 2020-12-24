68bccb7bcb1c1e79d20fd33b3158dae2312ff1f4
S Dellysse <me@sqlazer.com>
2020-12-24 15:21:41 -0500

    font-comic-mono 0.0.20201224 (new formula)

---
+++ Casks/font-comic-mono.rb
@@ -0,0 +1,13 @@
+cask "font-comic-mono" do
+  version "0.0.20201224"
+  sha256 "47147f4aba5c348c4712b0f386fef6c3f8b7da838128f09666ddd693cec1afc4"
+
+  url "https://github.com/dtinth/comic-mono-font/archive/66db8873f6e3219abb7924f0926947e00fec7448.zip"
+  name "font-comic-mono"
+  desc "like the famous font but monospace and with adjusted metric"
+  homepage "https://dtinth.github.io/comic-mono-font/"
+  license "MIT"
+
+  font "comic-mono-font-66db8873f6e3219abb7924f0926947e00fec7448/ComicMono.ttf"
+  font "comic-mono-font-66db8873f6e3219abb7924f0926947e00fec7448/ComicMono-Bold.ttf"
+end
