fb3a851aa5199235e5a0966efe30e541f41fc2cd
Markus Reiter <me@reitermark.us>
2020-12-25 03:55:49 +0100

    bluestacks: add livecheck (#96826)

--- Casks/bluestacks.rb
+++ Casks/bluestacks.rb
@@ -1,13 +1,19 @@
 cask "bluestacks" do
-  version "4.240.5.2912,e06a28fa1d15c65b5ab6ed80ad9e0bb6"
-  sha256 "620c55bf524e42fcfe5f25e840c9cb7efb2c29b365388901b5af419f4e1f374c"
+  version "4.240.5.2918,c72df965bb755c78ee4aa0dfd65a169b"
+  sha256 "30eb8e64a1c1aa85faa90f709ff68b9885c548f15041158972a59be0dad48a3a"
 
   url "https://cdn3.bluestacks.com/downloads/mac/bgp64_mac/#{version.before_comma}/#{version.after_comma}/x64/BlueStacksInstaller_#{version.before_comma}.dmg"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://cloud.bluestacks.com/api/getdownloadnow?platform=mac",
-          must_contain: version.before_comma
   name "BlueStacks"
   desc "Mobile gaming platform"
   homepage "https://www.bluestacks.com/"
+
+  livecheck do
+    url "https://cloud.bluestacks.com/api/getdownloadnow?platform=mac&mac_version=#{MacOS.full_version}"
+    strategy :header_match do |headers|
+      match = headers["location"].match(%r{/(\d(?:\.\d+)*)/([^/]+)/})
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   depends_on macos: ">= :sierra"
 
