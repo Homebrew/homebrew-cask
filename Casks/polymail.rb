a2bead82afee2fbb180fbd9711b2edc198087f3c
Markus Reiter <me@reitermark.us>
2020-12-25 20:29:53 +0100

    polymail: add livecheck (#96897)

--- Casks/polymail.rb
+++ Casks/polymail.rb
@@ -3,9 +3,13 @@ cask "polymail" do
   sha256 "590423c3ec9b9d11d84f217129465d48caff965562c421c138768837ea67dd6c"
 
   url "https://sparkle-updater.polymail.io/macos/builds/Polymail-v#{version}.zip"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://sparkle-updater.polymail.io/osx/Polymail-Latest.zip"
   name "Polymail"
   homepage "https://polymail.io/"
+
+  livecheck do
+    url "https://sparkle-updater.polymail.io/osx/Polymail-Latest.zip"
+    strategy :header_match
+  end
 
   auto_updates true
 
