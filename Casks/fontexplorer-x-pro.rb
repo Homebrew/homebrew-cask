255e5061d001865b193d9188fdecbe524aaff694
Markus Reiter <me@reitermark.us>
2020-12-25 08:33:58 +0100

    fontexplorer-x-pro: add livecheck (#96845)

--- Casks/fontexplorer-x-pro.rb
+++ Casks/fontexplorer-x-pro.rb
@@ -3,11 +3,16 @@ cask "fontexplorer-x-pro" do
   sha256 "9b275bb36d51943b93ff226c317d8caaf62d7e58b885614bbc5b1bc6d5ff02c2"
 
   url "https://fast.fontexplorerx.com/FontExplorerXPro#{version.no_dots}.dmg"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect_curl.cgi?url=https://fex.linotype.com/download/mac/FontExplorerXPro.dmg",
-          must_contain: version.no_dots
   name "FontExplorer X Pro"
   desc "Font finder and organizer"
   homepage "https://www.fontexplorerx.com/"
+
+  livecheck do
+    url "https://fex.linotype.com/download/mac/FontExplorerXPro.dmg"
+    strategy :header_match do |headers|
+      headers["location"][/FontExplorerXPro(\d+).dmg/i, 1].split("", 3).join(".")
+    end
+  end
 
   app "FontExplorer X Pro.app"
 
