4223f19c7530c5b62a3d3996b7c22f49aefbaba6
Markus Reiter <me@reitermark.us>
2020-12-25 08:32:14 +0100

    busycontacts: add livecheck (#96829)
    * busycontacts: add livecheck
    
    * Update busycontacts.rb

--- Casks/busycontacts.rb
+++ Casks/busycontacts.rb
@@ -1,18 +1,26 @@
 cask "busycontacts" do
-  version "1.5.0-2020-12-19-11-43"
-  sha256 "952ce28abaa5ce1ea18e3b0221b435e95470a23741e60e9a1b3cbf8833769775"
+  version "1.5.0,2020-12-23-13-13"
+  sha256 "4b83890cbe0e056e61a5c09a74189e1ab6e2cc9ea311ab312e855d703e5bac39"
 
-  url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bct-#{version}.zip",
+  url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bct-#{version.before_comma}-#{version.after_comma}.zip",
       verified: "7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.busymac.com/download/BusyContacts.zip"
   name "BusyContacts"
   desc "Contact manager focusing on efficiency"
   homepage "https://www.busymac.com/busycontacts/index.html"
+
+  livecheck do
+    url "https://www.busymac.com/download/BusyContacts.zip"
+    strategy :header_match do |headers|
+      match = headers["location"].match(/bct-(\d+(?:\.\d+)*)-(.*?)\.zip/)
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   auto_updates true
 
   pkg "BusyContacts Installer.pkg"
 
   uninstall pkgutil: "com.busymac.busycontacts.pkg",
+            quit:    "com.busymac.busycontacts",
             signal:  ["KILL", "com.busymac.busycontacts"]
 end
