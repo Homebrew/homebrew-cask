64791f1e5ff986dc98246ee03bfd2439c02306d0
Markus Reiter <me@reitermark.us>
2020-12-25 04:41:52 +0100

    epic-games: add livecheck (#96837)

--- Casks/epic-games.rb
+++ Casks/epic-games.rb
@@ -4,10 +4,14 @@ cask "epic-games" do
 
   url "https://epicgames-download1.akamaized.net/Builds/UnrealEngineLauncher/Installers/Mac/EpicInstaller-#{version}.dmg",
       verified: "epicgames-download1.akamaized.net/"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncher.dmg"
   name "Epic Games Launcher"
   desc "Launcher for *Epic Games* games"
   homepage "https://www.epicgames.com/"
+
+  livecheck do
+    url "https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncher.dmg"
+    strategy :header_match
+  end
 
   auto_updates true
   depends_on macos: ">= :sierra"
