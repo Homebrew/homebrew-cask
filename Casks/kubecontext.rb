447af55bddb55c73c430a2dc0519dfa3b5930024
Hasan Turken <turkenh@gmail.com>
2020-12-25 03:00:56 +0300

    Update kubecontext from v1.23 to v2.0.0 (#96812)

--- Casks/kubecontext.rb
+++ Casks/kubecontext.rb
@@ -1,6 +1,6 @@
 cask "kubecontext" do
-  version "1.23"
-  sha256 "262d0e2c2843c0e0220b557e02091d06c64e403b5ff09f09351f6e81841c8ac5"
+  version "2.0.0"
+  sha256 "b99db399fb038f87ac054f2d0741137de2a9b343f01f32a1db1b3f6dc313d882"
 
   url "https://github.com/turkenh/KubeContext/releases/download/v#{version}/KubeContext.zip"
   appcast "https://github.com/turkenh/KubeContext/releases.atom"
