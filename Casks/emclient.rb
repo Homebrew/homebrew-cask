f77b8493a25ad21dff2c33dbf3015af6d39f5cca
Markus Reiter <me@reitermark.us>
2020-12-25 08:55:58 +0100

    emclient: add livecheck (#96836)
    * emclient: add livecheck
    
    * Update emclient.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/emclient.rb
+++ Casks/emclient.rb
@@ -3,16 +3,23 @@ cask "emclient" do
   sha256 "ec85fdf7c1344fc92f9b04123cf27e466cdaf8297db32702903200b4152be21c"
 
   url "https://cdn-dist.emclient.com/dist/v#{version}_Mac/setup.pkg"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.emclient.com/dist/latest/setup.pkg"
   name "eM Client"
   desc "Email client"
   homepage "https://www.emclient.com/"
+
+  livecheck do
+    url "https://www.emclient.com/dist/latest/setup.pkg"
+    strategy :header_match do |headers|
+      headers["location"][/v(\d+(?:\.\d+)*)_Mac/i, 1]
+    end
+  end
 
   auto_updates true
 
   pkg "setup.pkg"
 
-  uninstall pkgutil: "com.emclient.mail.client.pkg"
+  uninstall quit:    "application.com.apple.Safari.12886706636",
+            pkgutil: "com.emclient.mail.client.pkg"
 
   zap trash: [
     "~/Library/Caches/com.emclient.mail.client",
