ffa1625195fa1190818d5166795aaa71f2651352
Markus Reiter <me@reitermark.us>
2020-12-25 15:01:11 +0100

    maczip: add livecheck (#96868)

--- Casks/maczip.rb
+++ Casks/maczip.rb
@@ -3,10 +3,14 @@ cask "maczip" do
   sha256 "acd1f3f61a93e227166c15e767fe5ae488c62786bd278ffd11fd251a79e14dad"
 
   url "https://cdn.awehunt.com/ezip/release/MacZip_V#{version}.dmg"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://ezip.awehunt.com/download?s=web"
   name "MacZip"
   desc "Utility to open, create and modify archive files"
   homepage "https://ezip.awehunt.com/"
+
+  livecheck do
+    url "https://ezip.awehunt.com/download?s=web"
+    strategy :header_match
+  end
 
   app "MacZip.app"
 
