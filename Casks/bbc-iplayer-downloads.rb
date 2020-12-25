5ae5df7f844869ef3f5fe0a6199e8fa1f8b63a96
Markus Reiter <me@reitermark.us>
2020-12-25 01:47:11 +0100

    bbc-iplayer-downloads: add livecheck

--- Casks/bbc-iplayer-downloads.rb
+++ Casks/bbc-iplayer-downloads.rb
@@ -3,9 +3,13 @@ cask "bbc-iplayer-downloads" do
   sha256 "c50dbb2b744a93407fe83ed69565eef60389836b1689ea9db0fa68bdb0c2a10e"
 
   url "https://static.files.bbci.co.uk/iplayer-pc-download-app/releases/darwin-x64/BBCiPlayerDownloads-#{version}.dmg"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://downloads-app.iplayer.api.bbc.co.uk/stable/darwin-x64"
   name "BBC iPlayer Downloads"
   homepage "https://www.bbc.co.uk/iplayer/install"
+
+  livecheck do
+    url "https://downloads-app.iplayer.api.bbc.co.uk/stable/darwin-x64"
+    strategy :header_match
+  end
 
   app "BBC iPlayer Downloads.app"
 end
