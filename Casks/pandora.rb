3eef70cad61dfdaa84b916c1d9797ef1fe9b51d9
Markus Reiter <me@reitermark.us>
2020-12-25 15:02:48 +0100

    pandora: add livecheck

--- Casks/pandora.rb
+++ Casks/pandora.rb
@@ -4,10 +4,14 @@ cask "pandora" do
 
   url "https://p-desktop-app.s3.amazonaws.com/releases/Pandora-#{version}.dmg",
       verified: "p-desktop-app.s3.amazonaws.com/"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://pdora.co/desktop_mac_download"
   name "Pandora"
   desc "Desktop client for the Pandora web radio service"
   homepage "https://www.pandora.com/desktop"
+
+  livecheck do
+    url "https://pdora.co/desktop_mac_download"
+    strategy :header_match
+  end
 
   app "Pandora.app"
 
