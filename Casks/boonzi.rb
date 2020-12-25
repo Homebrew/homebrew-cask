878a63dfae065a55d2acdf735b29fa7e4f2db4fb
Markus Reiter <me@reitermark.us>
2020-12-25 03:54:56 +0100

    boonzi: add livecheck (#96825)

--- Casks/boonzi.rb
+++ Casks/boonzi.rb
@@ -3,9 +3,13 @@ cask "boonzi" do
   sha256 "613bdc7342ffb225c892c40a2acfc4e81650f70b1b55b595cbdcc74ddad87a9e"
 
   url "https://www.boonzi.pt/releases/boonzi.#{version}.dmg"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.boonzi.pt/server/releases/latest/mac/email@address.com"
   name "Boonzi"
   homepage "https://www.boonzi.pt/"
+
+  livecheck do
+    url "https://www.boonzi.pt/server/releases/latest/mac/email@address.com"
+    strategy :header_match
+  end
 
   app "Boonzi.app"
 end
