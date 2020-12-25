0fcdcabfb612e73680ddd543cf0cb788ef37249d
Markus Reiter <me@reitermark.us>
2020-12-25 20:50:47 +0100

    odrive: add livecheck (#96884)

--- Casks/odrive.rb
+++ Casks/odrive.rb
@@ -2,16 +2,24 @@ cask "odrive" do
   version "6918"
   sha256 "9107f7634ac70cf017c889738b5bd8e3ac937bd991d5999119832d57407f0c22"
 
-  # downloads can be found at https://www.odrive.com/downloaddesktop
   url "https://d3huse1s6vwzq6.cloudfront.net/odrivesync.#{version}.pkg",
       verified: "d3huse1s6vwzq6.cloudfront.net/"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.odrive.com/downloaddesktop?platform=mac"
   name "odrive"
   desc "Tool to make any cloud storage unified, synchronized, shareable, and encrypted"
   homepage "https://www.odrive.com/"
 
+  livecheck do
+    # https://docs.odrive.com/docs/odrive-usage-guide#install-desktop-sync
+    url "https://www.odrive.com/downloaddesktop?platform=mac"
+    strategy :header_match
+  end
+
   pkg "odrivesync.#{version}.pkg"
 
-  uninstall quit:    "com.oxygencloud.odrive",
+  uninstall quit:    [
+    "com.oxygencloud.odrive",
+    "application.com.apple.installer.1152921500312163482",
+    "application.com.apple.systemevents.1152921500312199658",
+  ],
             pkgutil: "com.oxygen.odrive.*"
 end
