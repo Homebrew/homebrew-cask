d53092d746e6ea343b448a80b62476e8e8c28576
Markus Reiter <me@reitermark.us>
2020-12-25 02:31:13 +0100

    diskcatalogmaker: add livecheck

--- Casks/diskcatalogmaker.rb
+++ Casks/diskcatalogmaker.rb
@@ -1,13 +1,18 @@
 cask "diskcatalogmaker" do
-  version "8.3"
+  version "8.3.0"
   sha256 :no_check
 
   url "https://download.diskcatalogmaker.com/zip/DiskCatalogMaker.zip"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.diskcatalogmaker.com/zip/DiskCatalogMaker.zip",
-          must_contain: version.no_dots
   name "DiskCatalogMaker"
   desc "Toast-bundled version of DiskCatalogMaker"
   homepage "https://diskcatalogmaker.com/"
+
+  livecheck do
+    url "https://download.diskcatalogmaker.com/zip/DiskCatalogMaker.zip"
+    strategy :header_match do |headers|
+      headers["location"][/DiskCatalogMaker(\d+).zip/i, 1].split("", 3).join(".")
+    end
+  end
 
   app "DiskCatalogMaker.app"
 end
