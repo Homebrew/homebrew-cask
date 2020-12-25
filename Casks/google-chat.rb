a06ab01a4a7ad0ba7c120e97c1930df54d054f73
Markus Reiter <me@reitermark.us>
2020-12-25 04:38:53 +0100

    google-chat: add livecheck

--- Casks/google-chat.rb
+++ Casks/google-chat.rb
@@ -3,9 +3,13 @@ cask "google-chat" do
   sha256 "6cff1d7d42bf130e052cdbd8fd0d5c1e9b4b60ab872c26a4b8944f28c88c8e68"
 
   url "https://dl.google.com/chat/#{version}/InstallHangoutsChat.dmg"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://dl.google.com/chat/latest/InstallHangoutsChat.dmg"
   name "Chat"
   homepage "https://gsuite.google.com/products/chat/"
+
+  livecheck do
+    url "https://dl.google.com/chat/latest/InstallHangoutsChat.dmg"
+    strategy :header_match
+  end
 
   app "Chat.app"
 
