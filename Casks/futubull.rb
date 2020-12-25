5c8eb6252752b792f65671c7b16858f9b6bb2f3e
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-25 15:25:19 +0800

    Update and rename futuniuniu.rb to futubull.rb

--- Casks/futubull.rb
+++ Casks/futubull.rb
@@ -1,11 +1,11 @@
-cask "futuniuniu" do
+cask "futubull" do
   version "10.13.738,202012151016"
   sha256 "a329ad46b20b6d571cac32f8e42b3f0841c6baf0200b422a341bb0fd59f7ece8"
 
   url "https://softwarefile.futunn.com/FTNNForMac_#{version.before_comma}_#{version.after_comma}_Website.dmg"
   appcast "https://www.futunn.com/download/mac?lang=en-US"
+  name "Futubull"
   name "FutuNiuniu"
-  name "富途牛牛"
   desc "Trading application"
   homepage "https://www.futunn.com/"
 
