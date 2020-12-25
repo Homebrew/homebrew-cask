00ef15016283e84723b757ad0cbb7c0cb6a38bbd
Markus Reiter <me@reitermark.us>
2020-12-25 01:37:39 +0100

    airflow: add livecheck

--- Casks/airflow.rb
+++ Casks/airflow.rb
@@ -4,10 +4,14 @@ cask "airflow" do
 
   url "https://cdn.downloads.iocave.net/Airflow/Airflow%20#{version}.zip",
       verified: "cdn.downloads.iocave.net/Airflow/"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://airflowapp.com/download/latest?mac"
   name "Airflow"
   desc "Watch local content on Apple TV and Chromecast"
   homepage "https://airflowapp.com/"
+
+  livecheck do
+    url "https://airflowapp.com/download/latest?mac"
+    strategy :header_match
+  end
 
   auto_updates true
   depends_on macos: ">= :sierra"
