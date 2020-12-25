852e7b2e3b5a824715e2b02fcc9d0d2ba4ec0ad5
Markus Reiter <me@reitermark.us>
2020-12-25 02:34:48 +0100

    emclient: add livecheck

--- Casks/emclient.rb
+++ Casks/emclient.rb
@@ -3,10 +3,16 @@ cask "emclient" do
   sha256 "ec85fdf7c1344fc92f9b04123cf27e466cdaf8297db32702903200b4152be21c"
 
   url "https://cdn-dist.emclient.com/dist/v#{version}_Mac/setup.pkg"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.emclient.com/dist/latest/setup.pkg"
   name "eM Client"
   desc "Email client"
   homepage "https://www.emclient.com/"
+
+  livecheck do
+    url "https://www.emclient.com/dist/latest/setup.pkg"
+    strategy :header_match do |headers|
+      headers["location"][/v(\d+(?:\.\d+)*)_Mac/i, 1]
+    end
+  end
 
   auto_updates true
 
