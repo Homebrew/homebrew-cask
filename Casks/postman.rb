3c281732e0a31dd60a584bf6a23c6c09e24d608c
Markus Reiter <me@reitermark.us>
2020-12-25 20:31:58 +0100

    postman: add livecheck (#96899)

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -4,10 +4,14 @@ cask "postman" do
 
   url "https://dl.pstmn.io/download/version/#{version}/osx64",
       verified: "dl.pstmn.io/download/version/"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://dl.pstmn.io/download/latest/osx"
   name "Postman"
   desc "Collaboration platform for API development"
   homepage "https://www.postman.com/"
+
+  livecheck do
+    url "https://dl.pstmn.io/download/latest/osx"
+    strategy :header_match
+  end
 
   auto_updates true
 
