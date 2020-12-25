445595a0a442a04487c5f872cbef45246a19ed88
Markus Reiter <me@reitermark.us>
2020-12-25 03:44:10 +0100

    bankid: add livecheck (#96823)

--- Casks/bankid.rb
+++ Casks/bankid.rb
@@ -3,10 +3,13 @@ cask "bankid" do
   sha256 "4ea124c97c4d138bdf32de71ff32074bebd79614a4f1e35e16aa5a6b24fafd1e"
 
   url "https://install.bankid.com/Repository/BankID_installation_#{version.dots_to_underscores}.pkg"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://install.bankid.com/FileDownloader?fileId=Mac",
-          must_contain: version.dots_to_underscores
   name "BankID"
   homepage "https://install.bankid.com/"
+
+  livecheck do
+    url "https://install.bankid.com/FileDownloader?fileId=Mac"
+    strategy :header_match
+  end
 
   pkg "BankID_installation_#{version.dots_to_underscores}.pkg"
 
