f1b394c92fabb634914c0dcc9167a5f26d6f2d70
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-26 03:21:28 +0800

    Update gpg-suite.rb

--- Casks/gpg-suite.rb
+++ Casks/gpg-suite.rb
@@ -30,7 +30,7 @@ cask "gpg-suite" do
   },
             pkgutil:   "org.gpgtools.*",
             quit:      [
-              "application.org.gpgtools.gpgmail.upgrader.12890014530",
+              "application.org.gpgtools.gpgmail.upgrader.12890016062",
               "com.apple.mail",
               "org.gpgtools.gpgkeychainaccess",
               "org.gpgtools.gpgkeychain",
