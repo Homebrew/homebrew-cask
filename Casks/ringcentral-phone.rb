271e4e75af639038d31d79f0241712dc0c663c97
Markus Reiter <me@reitermark.us>
2020-12-25 16:39:24 +0100

    ringcentral-phone: add livecheck

--- Casks/ringcentral-phone.rb
+++ Casks/ringcentral-phone.rb
@@ -1,11 +1,15 @@
 cask "ringcentral-phone" do
-  version "20.3.2"
-  sha256 "b51bb7f76fed3a2cad5714e53171ff7d49316a64a7832ddf791ba2e4fb3a7137"
+  version "20.4.0"
+  sha256 "0720bdbec37fe992a66cb3b1ea960f8b23877b93ccd7a353c82da5a6b830dc72"
 
   url "https://downloads.ringcentral.com/sp/RingCentral-Phone-#{version}.dmg"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://downloads.ringcentral.com/sp/RingCentralForMac"
   name "RingCentral Phone"
   homepage "https://www.ringcentral.com/apps/rc-phone"
+
+  livecheck do
+    url "https://downloads.ringcentral.com/sp/RingCentralForMac"
+    strategy :header_match
+  end
 
   # Renamed for consistency: app name is different in the Finder and in a shell.
   app "RingCentral for Mac.app", target: "RingCentral Phone.app"
