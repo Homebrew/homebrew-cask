0ca6f96209b39a421f6e434770ca27babd73ceaa
Markus Reiter <me@reitermark.us>
2020-12-25 08:35:19 +0100

    fl-studio: add livecheck (#96841)

--- Casks/fl-studio.rb
+++ Casks/fl-studio.rb
@@ -3,9 +3,13 @@ cask "fl-studio" do
   sha256 "87dd3234daa6907ad3801f482b1f2ca399fdc2a7c88874cca9db43b77faba0ef"
 
   url "https://demodownload.image-line.com/flstudio/flstudio_mac_#{version}.dmg"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://support.image-line.com/redirect/flstudio20_mac_installer"
   name "FL Studio"
   homepage "https://www.image-line.com/flstudio/"
+
+  livecheck do
+    url "https://support.image-line.com/redirect/flstudio20_mac_installer"
+    strategy :header_match
+  end
 
   pkg "Install FL Studio.pkg"
 
