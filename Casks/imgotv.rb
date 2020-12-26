b6cd1abe407a17dc397c92a62383824aedfa6058
Markus Reiter <me@reitermark.us>
2020-12-27 00:40:58 +0100

    imgotv: add livecheck

--- Casks/imgotv.rb
+++ Casks/imgotv.rb
@@ -1,12 +1,16 @@
 cask "imgotv" do
-  version "6.3.6"
-  sha256 "8fde36fcb283cb83cd4c1a5f8eb8f3c187c5859039dff42b7e702c4e4fb5ee99"
+  version "6.3.7"
+  sha256 "423ed3eff7eca07145975c781c419909b5faee47ce97d2a7c20172b724399e2a"
 
   url "https://download.imgo.tv/app/mac/#{version}/mgtv-mango-#{version}.dmg",
       verified: "download.imgo.tv/"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?user_agent=Mac%20OS%20X&url=https://pcconf.api.mgtv.com/getPcDownloadUrl?source=mango2"
   name "芒果TV"
   homepage "https://www.mgtv.com/app/"
+
+  livecheck do
+    url "https://pcconf.api.mgtv.com/getPcDownloadUrl?source=mango2"
+    strategy :header_match
+  end
 
   auto_updates true
   depends_on macos: ">= :yosemite"
