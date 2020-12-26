d2b35fa6a80ab61304c4222404f921e7246dbb86
Markus Reiter <me@reitermark.us>
2020-12-26 16:58:57 +0100

    teamviewermeeting: add livecheck (#96944)

--- Casks/teamviewermeeting.rb
+++ Casks/teamviewermeeting.rb
@@ -3,10 +3,14 @@ cask "teamviewermeeting" do
   sha256 "9cc468ccc4fa8bf21ed98f4671db9e4be33922e1545e168028f7ae6700b874e7"
 
   url "https://dl.teamviewer.com/teamviewermeeting/installer/mac/#{version}/TeamViewerMeeting.dmg"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.teamviewer.com/blizz/mac"
   name "TeamViewerMeeting"
   desc "Cross-platform videoconferencing and communication software"
   homepage "https://www.teamviewer.com/meeting/"
+
+  livecheck do
+    url "https://download.teamviewer.com/blizz/mac"
+    strategy :header_match
+  end
 
   app "TeamViewerMeeting.app"
 end
