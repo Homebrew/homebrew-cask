0b35edc2207725b58c19cb36534667022fd5d7fc
Markus Reiter <me@reitermark.us>
2020-12-26 17:10:00 +0100

    ximalaya: add livecheck (#96952)

--- Casks/ximalaya.rb
+++ Casks/ximalaya.rb
@@ -4,11 +4,18 @@ cask "ximalaya" do
 
   url "https://s1.xmcdn.com/yx/ximalaya-pc-client/#{version.before_comma}/download/Ximalaya-#{version.before_comma}_#{version.after_comma}.dmg",
       verified: "s1.xmcdn.com/"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.ximalaya.com/down/lite?client=mac"
   name "ximalaya"
   name "喜马拉雅"
   desc "Platform for podcasting and audio-sharing"
   homepage "https://www.ximalaya.com/"
+
+  livecheck do
+    url "https://www.ximalaya.com/down/lite?client=mac"
+    strategy :header_match do |headers|
+      match = headers["location"].match(%r{/Ximalaya-(\d+(?:\.\d+)*)_(\d+)\.dmg}i)
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   app "喜马拉雅.app"
 end
