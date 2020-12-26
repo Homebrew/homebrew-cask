a687e1c3faad0837e7bdf5d4ed3ea2fe14fc8169
kkshmz <kyeshimizu@gmail.com>
2020-12-26 23:24:34 +0900

    bump touchdesigner  version to 2020.44350

--- Casks/touchdesigner.rb
+++ Casks/touchdesigner.rb
@@ -1,11 +1,12 @@
 cask "touchdesigner" do
-  version "2020.27390"
-  sha256 "92e0e8c0deecfc567f96a8e61ec59098c58729b5969af510e8871a146de269eb"
+  version "2020.44350"
+  sha256 "bf7b4f5c953a6b07fb5151c9bf8174a1386d2bdf46f3e880857d565c707c693a"
 
   url "https://download.derivative.ca/TouchDesigner.#{version}.dmg"
   appcast "https://www.derivative.ca/download/"
   name "Derivative TouchDesigner"
   homepage "https://www.derivative.ca/"
+  auto_updates true
 
   app "TouchDesigner.app"
 end
