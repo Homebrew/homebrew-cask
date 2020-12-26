9d28e9f47c5f9f4547be41a3e0c0541f613daae6
kkshmz <kyeshimizu@gmail.com>
2020-12-26 23:31:49 +0900

    add name and desc to cask

--- Casks/touchdesigner.rb
+++ Casks/touchdesigner.rb
@@ -1,6 +1,9 @@
 cask "touchdesigner" do
+  name "Touchdesigner"
+  desc "TouchDesigner is a node based visual programming language for real time interactive multimedia content"
   version "2020.44350"
   sha256 "bf7b4f5c953a6b07fb5151c9bf8174a1386d2bdf46f3e880857d565c707c693a"
+  
 
   url "https://download.derivative.ca/TouchDesigner.#{version}.dmg"
   appcast "https://www.derivative.ca/download/"
