d51e6f8315d46f697d2b091c7dbb750842fcc61e
Markus Reiter <me@reitermark.us>
2020-12-26 17:00:00 +0100

    spark-ar-studio: add livecheck (#96935)

--- Casks/spark-ar-studio.rb
+++ Casks/spark-ar-studio.rb
@@ -1,13 +1,18 @@
 cask "spark-ar-studio" do
-  version "104.0"
+  version "104"
   sha256 :no_check
 
   url "https://www.facebook.com/sparkarmacos/download/"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.facebook.com/sparkarmacos/download/",
-          must_contain: version.major
   name "Spark AR Studio"
   desc "Create and share augmented reality experiences using the Facebook family of apps"
   homepage "https://sparkar.facebook.com/ar-studio/"
+
+  livecheck do
+    url "https://www.facebook.com/sparkarmacos/download/"
+    strategy :header_match do |headers|
+      headers["location"][%r{/SparkARStudio_v(\d+)\.dmg}i, 1]
+    end
+  end
 
   app "Spark AR Studio.app"
 
