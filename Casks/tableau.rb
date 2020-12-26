b3e87d7ec60d7dacda7317a957548ee309cd0c99
Markus Reiter <me@reitermark.us>
2020-12-26 16:59:28 +0100

    tableau: add livecheck (#96942)

--- Casks/tableau.rb
+++ Casks/tableau.rb
@@ -3,11 +3,16 @@ cask "tableau" do
   sha256 "611161e6313fb51e7eec3e6b5c2614e85b1b5b341b8e67095b95f5d4fa739fc4"
 
   url "https://downloads.tableau.com/tssoftware/TableauDesktop-#{version.dots_to_hyphens}.dmg"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.tableau.com/downloads/desktop/mac",
-          must_contain: version.dots_to_hyphens
   name "Tableau Desktop"
   desc "Popular analytics platform"
   homepage "https://www.tableau.com/products/desktop/download"
+
+  livecheck do
+    url "https://www.tableau.com/downloads/desktop/mac"
+    strategy :header_match do |headers|
+      headers["location"][/-(\d+-\d+-\d+)\.dmg/i, 1].tr("-", ".")
+    end
+  end
 
   depends_on macos: ">= :yosemite"
 
