a7a484b2d20dfa8424ee49f5e4fc65dadb1769b6
Markus Reiter <me@reitermark.us>
2020-12-26 16:59:21 +0100

    thebrain: add livecheck (#96946)

--- Casks/thebrain.rb
+++ Casks/thebrain.rb
@@ -3,9 +3,13 @@ cask "thebrain" do
   sha256 "a92e40a9f8f766ec514df4d85d0fd3bc799169f2e56267113c60a24fdb246305"
 
   url "https://updater.thebrain.com/files/TheBrain#{version}.dmg"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://salesapi.thebrain.com/?a=doDirectDownload%26id=11000"
   name "TheBrain"
   homepage "https://www.thebrain.com/"
+
+  livecheck do
+    url "https://salesapi.thebrain.com/?a=doDirectDownload&id=11000"
+    strategy :header_match
+  end
 
   app "TheBrain #{version.major}.app"
 end
