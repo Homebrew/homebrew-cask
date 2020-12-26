79c80290d1dcbb3ae6d96dc495eb7f99828d7861
Markus Reiter <me@reitermark.us>
2020-12-26 17:00:25 +0100

    scratch: add livecheck (#96927)

--- Casks/scratch.rb
+++ Casks/scratch.rb
@@ -3,10 +3,14 @@ cask "scratch" do
   sha256 "5c447f3d88feeb193cb607a81eec585c74226c83b824f62d57524b33aabd4c44"
 
   url "https://downloads.scratch.mit.edu/desktop/Scratch%20#{version}.dmg"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://downloads.scratch.mit.edu/desktop/Scratch.dmg"
   name "Scratch"
   desc "Programs interactive stories, games, and animations"
   homepage "https://scratch.mit.edu/download"
+
+  livecheck do
+    url "https://downloads.scratch.mit.edu/desktop/Scratch.dmg"
+    strategy :header_match
+  end
 
   depends_on macos: ">= :high_sierra"
 
