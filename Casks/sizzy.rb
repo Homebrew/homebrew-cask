dc8f6194d2a24de5befa821f444167ae38475e0f
Markus Reiter <me@reitermark.us>
2020-12-27 12:48:50 +0100

    sizzy: add livecheck (#96985)

--- Casks/sizzy.rb
+++ Casks/sizzy.rb
@@ -3,10 +3,14 @@ cask "sizzy" do
   sha256 :no_check
 
   url "https://sizzy.co/get-app"
-  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://sizzy.co/get-app&user_agent=Intel%20Mac%20OS%20X"
   name "Sizzy"
   desc "Tool to simulate responsive designs on multiple devices"
   homepage "https://sizzy.co/"
+
+  livecheck do
+    url "https://sizzy.co/get-app"
+    strategy :header_match
+  end
 
   auto_updates true
 
