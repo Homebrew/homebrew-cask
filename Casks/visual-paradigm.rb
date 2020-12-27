c8289b5ccdba05af135814e909f1dec8e8278db0
Markus Reiter <me@reitermark.us>
2020-12-27 12:49:18 +0100

    visual-paradigm: add livecheck (#96992)

--- Casks/visual-paradigm.rb
+++ Casks/visual-paradigm.rb
@@ -1,12 +1,18 @@
 cask "visual-paradigm" do
-  version "16.2,20201203"
-  sha256 "585fed5cb34baa669150f56ad60aa654a0e6f5e805bc66f3af1495715c59dddd"
+  version "16.2,20201219"
+  sha256 "0267bd9990795c93793c47b91dc754f5834c33806007d81f27f464b01fe978e9"
 
   url "https://eu8.dl.visual-paradigm.com/visual-paradigm/vp#{version.before_comma}/#{version.after_comma}/Visual_Paradigm_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
-  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.visual-paradigm.com/downloads/vp/checksum.html",
-          must_contain: "#{version.before_comma}/#{version.after_comma}"
   name "Visual Paradigm"
   homepage "https://www.visual-paradigm.com/"
+
+  livecheck do
+    url "https://www.visual-paradigm.com/downloads/vp/checksum.html"
+    strategy :header_match do |headers|
+      match = headers["location"].match(%r{/vp(\d+(?:\.\d+)*)/(\d+)/checksum\.html}i)
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   app "Visual Paradigm.app"
 end
