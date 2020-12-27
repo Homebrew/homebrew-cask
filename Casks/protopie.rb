077c442dd53718fd503026dcfb57d3b10b17f898
Markus Reiter <me@reitermark.us>
2020-12-27 12:48:35 +0100

    protopie: add livecheck (#96982)

--- Casks/protopie.rb
+++ Casks/protopie.rb
@@ -3,9 +3,13 @@ cask "protopie" do
   sha256 "2cabe38670799533438419f40b1d7595aa48c566407e623cde59bdf2a43508b6"
 
   url "https://release.protopie.io/ProtoPie-#{version}.dmg"
-  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.protopie.io/darwin/latest"
   name "ProtoPie"
   homepage "https://www.protopie.io/"
+
+  livecheck do
+    url "https://download.protopie.io/darwin/latest"
+    strategy :header_match
+  end
 
   auto_updates true
 
