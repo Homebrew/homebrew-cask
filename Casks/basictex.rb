76afa2a67583b598eef9aa39e4230346e1b891ac
Markus Reiter <me@reitermark.us>
2020-12-27 18:39:36 +0100

    basictex: add livecheck

--- Casks/basictex.rb
+++ Casks/basictex.rb
@@ -1,12 +1,20 @@
 cask "basictex" do
-  version "2020.0407"
+  version "2020-04-07"
   sha256 "c6a27c95a8af3bc26121cc3be415f3fc7120ffaa4ff4c1a860a7fe9fe78ac31e"
 
-  url "http://mirror.ctan.org/systems/mac/mactex/mactex-basictex-#{version.no_dots}.pkg",
+  url "http://mirror.ctan.org/systems/mac/mactex/mactex-basictex-#{version.no_hyphens}.pkg",
       verified: "mirror.ctan.org/systems/mac/mactex/"
   name "BasicTeX"
   desc "Compact TeX distribution as alternative to the full TeX Live / MacTeX"
   homepage "https://www.tug.org/mactex/morepackages.html"
+
+  livecheck do
+    url "http://mirror.ctan.org/systems/mac/mactex/"
+    strategy :page_match do |page|
+      match = page.match(/href=.*?mactex-basictex-(\d{4})(\d{2})(\d{2})\.pkg/)
+      "#{match[1]}-#{match[2]}-#{match[3]}"
+    end
+  end
 
   conflicts_with cask: [
     "mactex-no-gui",
