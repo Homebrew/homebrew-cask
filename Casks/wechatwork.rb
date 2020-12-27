ab1153ceecff8faaca80533638662537eddedd71
Markus Reiter <me@reitermark.us>
2020-12-27 12:49:22 +0100

    wechatwork: add livecheck (#96993)

--- Casks/wechatwork.rb
+++ Casks/wechatwork.rb
@@ -3,10 +3,14 @@ cask "wechatwork" do
   sha256 "cb50dbdaf5bc55846be4c5b1537a4cc752aeebc370d84584bf7ac04371433419"
 
   url "https://dldir1.qq.com/wework/work_weixin/WeCom_#{version}.dmg"
-  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://work.weixin.qq.com/wework_admin/commdownload?platform=mac"
   name "WeChat Work"
   name "企业微信"
   homepage "https://work.weixin.qq.com/"
+
+  livecheck do
+    url "https://work.weixin.qq.com/wework_admin/commdownload?platform=mac"
+    strategy :header_match
+  end
 
   app "企业微信.app"
 
