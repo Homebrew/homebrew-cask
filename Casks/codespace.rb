ca4fbeb8d0099453528e627f5cc580d5c6624384
Markus Reiter <me@reitermark.us>
2020-12-27 01:28:33 +0100

    codespace: add livecheck (#96965)

--- Casks/codespace.rb
+++ Casks/codespace.rb
@@ -3,9 +3,13 @@ cask "codespace" do
   sha256 :no_check
 
   url "https://codespace.app/download/latest"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://codespace.app/download/latest&user_agent=Mac%20OS%20X"
   name "Codespace"
   homepage "https://codespace.app/"
+
+  livecheck do
+    url "https://codespace.app/download/latest"
+    strategy :header_match
+  end
 
   depends_on macos: ">= :el_capitan"
 
