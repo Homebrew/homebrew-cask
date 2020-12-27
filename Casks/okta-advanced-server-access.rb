beca8814ce1afcbf26b57ac85e508d8189a3dbd5
Markus Reiter <me@reitermark.us>
2020-12-27 02:49:55 +0100

    Rename `scaleft` to `okta-advanced-server-access`. (#96959)

---
+++ Casks/okta-advanced-server-access.rb
@@ -0,0 +1,22 @@
+cask "okta-advanced-server-access" do
+  version "1.50.4"
+  sha256 "76f2126da3f2e889db6148704f0d73c9529e3fa34bb7e08c1622c5a9f6ea60f7"
+
+  url "https://dist.scaleft.com/client-tools/mac/v#{version}/ScaleFT-#{version}.pkg",
+      verified: "scaleft.com/"
+  name "Okta Advanced Server Access"
+  name "ScaleFT"
+  desc "Identity and access management"
+  homepage "https://help.okta.com/en/prod/Content/Topics/Adv_Server_Access/docs/sft-osx.htm"
+
+  livecheck do
+    url "https://dist.scaleft.com/client-tools/mac/latest/ScaleFT.pkg"
+    strategy :header_match
+  end
+
+  pkg "ScaleFT-#{version}.pkg"
+
+  uninstall pkgutil: "com.scaleft.ScaleFT"
+
+  zap trash: "~/Library/Caches/com.scaleft.ScaleFT"
+end
