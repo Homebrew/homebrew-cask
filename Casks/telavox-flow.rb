d40021afd71b9634227034652aaf77d8257dbb56
Markus Reiter <me@reitermark.us>
2020-12-27 12:49:14 +0100

    telavox-flow: add livecheck (#96991)

--- Casks/telavox-flow.rb
+++ Casks/telavox-flow.rb
@@ -4,10 +4,14 @@ cask "telavox-flow" do
 
   url "https://s3.eu-west-2.amazonaws.com/flow-desktop/Telavox-#{version}.dmg",
       verified: "s3.eu-west-2.amazonaws.com/flow-desktop/"
-  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://deopappmanager.telavox.com/flow/download/mac/latest"
   name "Telavox Flow"
   desc "Communication and collaboration platform"
   homepage "https://telavox.com/en/apps/"
+
+  livecheck do
+    url "https://deopappmanager.telavox.com/flow/download/mac/latest"
+    strategy :header_match
+  end
 
   auto_updates true
 
