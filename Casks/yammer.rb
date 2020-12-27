ebc7cbc883bf04be9fdc898e3ac85ed69b8be4ac
Markus Reiter <me@reitermark.us>
2020-12-27 03:48:09 +0100

    yammer: add livecheck

--- Casks/yammer.rb
+++ Casks/yammer.rb
@@ -4,9 +4,13 @@ cask "yammer" do
 
   url "https://yammerdesktopapp.blob.core.windows.net/binaries/dist/darwin/x64/#{version}/Yammer-#{version}.dmg",
       verified: "yammerdesktopapp.blob.core.windows.net/binaries/dist/"
-  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://aka.ms/yammer_desktop_mac"
   name "Yammer"
   homepage "https://support.office.com/en-us/article/Yammer-for-Windows-and-Mac-50920c05-cbfc-4f11-8503-e20fb2e623a5"
+
+  livecheck do
+    url "https://aka.ms/yammer_desktop_mac"
+    strategy :header_match
+  end
 
   app "Yammer.app"
 end
