5412fe41bb599e060c54cc8fe64f4c56413d80d1
Markus Reiter <me@reitermark.us>
2020-12-27 19:15:22 +0100

    alfred: add livecheck (#97003)

--- Casks/alfred.rb
+++ Casks/alfred.rb
@@ -1,12 +1,19 @@
 cask "alfred" do
-  version "4.3_1205"
+  version "4.3,1205"
   sha256 "eca6e890f736b61c339d20e90ccb9b741141bdf05eb4f7695a371f23eceb6cb4"
 
-  url "https://cachefly.alfredapp.com/Alfred_#{version}.dmg"
-  appcast "https://www.alfredapp.com/app/update#{version.major}/general.xml"
+  url "https://cachefly.alfredapp.com/Alfred_#{version.before_comma}_#{version.after_comma}.dmg"
   name "Alfred"
   desc "Application launcher and productivity software"
   homepage "https://www.alfredapp.com/"
+
+  livecheck do
+    url "https://www.alfredapp.com/app/update#{version.major}/general.xml"
+    strategy :page_match do |page|
+      match = page.match(/Alfred_(\d(?:\.\d+)*)_(\d+)\.tar\.gz/i)
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   auto_updates true
 
