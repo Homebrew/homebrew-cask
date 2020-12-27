1bca56bdcd3677212864715241e083c6b18c7906
Markus Reiter <me@reitermark.us>
2020-12-27 13:06:08 +0100

    corretto: add livecheck (#96970)

--- Casks/corretto.rb
+++ Casks/corretto.rb
@@ -3,10 +3,16 @@ cask "corretto" do
   sha256 "e52699df867a28f5dcf22a99a1a0d8c9356659ff9f3ed1e5e24375e3619d7622"
 
   url "https://corretto.aws/downloads/resources/#{version.sub(/-\d+/, "")}/amazon-corretto-#{version}-macosx-x64.pkg"
-  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://corretto.aws/downloads/latest/amazon-corretto-#{version.major}-x64-macos-jdk.pkg"
   name "AWS Corretto JDK"
   desc "OpenJDK distribution from Amazon"
   homepage "https://corretto.aws/"
+
+  livecheck do
+    url "https://corretto.aws/downloads/latest/amazon-corretto-#{version.major}-x64-macos-jdk.pkg"
+    strategy :header_match do |headers|
+      headers["location"][%r{/amazon-corretto-(\d+(?:\.\d+)*)-macosx-x64\.pkg}i, 1]
+    end
+  end
 
   pkg "amazon-corretto-#{version}-macosx-x64.pkg"
 
