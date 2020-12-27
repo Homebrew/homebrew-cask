252b420ac01c537147f83b5a8acd8b5620d8bacc
Markus Reiter <me@reitermark.us>
2020-12-27 01:37:53 +0100

    expressvpn: add livecheck (#96967)

--- Casks/expressvpn.rb
+++ Casks/expressvpn.rb
@@ -3,10 +3,14 @@ cask "expressvpn" do
   sha256 "ba0a9838e9950ea1084b00189ae88a9d2d587feb7a8e9c5551476b96afc15298"
 
   url "https://download.expressvpn.xyz/clients/mac/expressvpn_mac_#{version}_release.pkg"
-  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.expressvpn.xyz/clients/latest/mac"
   name "ExpressVPN"
   desc "VPN client for secure internet access and private browsing"
   homepage "https://www.expressvpn.xyz/vpn-software/vpn-mac/"
+
+  livecheck do
+    url "https://www.expressvpn.xyz/clients/latest/mac"
+    strategy :header_match
+  end
 
   auto_updates true
 
