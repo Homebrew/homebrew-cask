14afd674a98130c32d11e900b305077f1c29c9fc
Zennon Gosalvez <1798166+zgosalvez@users.noreply.github.com>
2020-12-27 12:36:32 +0800

    Add Silicon.app v1.0.3

---
+++ Casks/silicon.rb
@@ -0,0 +1,17 @@
+cask "silicon" do
+  version "1.0.3"
+  sha256 "4f6cf3f6b6cf0b8f067995a292538f122278226fa067427bf184b179076cbf79"
+
+  url "https://github.com/DigiDNA/Silicon/releases/download/#{version}/Silicon.app.zip"
+  appcast "https://github.com/DigiDNA/Silicon/releases.atom"
+  name "Silicon"
+  desc "Identify Intel-only apps"
+  homepage "https://github.com/DigiDNA/Silicon"
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "Silicon.app"
+
+  zap trash: "~/Library/Caches/com.DigiDNA.Silicon"
+end
