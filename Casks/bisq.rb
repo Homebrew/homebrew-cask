48bd015de347de8bdf422411fcc3a7aab03ff07b
jerryskye <jarek@siedlarz.com>
2020-12-28 18:43:56 +0100

    Update bisq from 1.5.1 to 1.5.2

--- Casks/bisq.rb
+++ Casks/bisq.rb
@@ -1,6 +1,6 @@
 cask "bisq" do
-  version "1.5.1"
-  sha256 "31c44e76c3e1a8663bbfa32d21ab80be58b6fed01cac997a24ef86f3b969d575"
+  version "1.5.2"
+  sha256 "a1a9779d1f0c4099fcb8ed103b79eb1f3df99bb3cdcf4cf3405e937f86231165"
 
   url "https://github.com/bisq-network/bisq/releases/download/v#{version}/Bisq-#{version}.dmg",
       verified: "github.com/bisq-network/bisq/"
