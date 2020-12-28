934a75e5724371214eeba01c492897ce9cd21179
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-28 09:38:33 +0100

    Update purei-play from 0.35-44-gb365f4c1,24-12-2020 to 0.35-51-g4c8e705e,28-12-2020 (#97019)

--- Casks/purei-play.rb
+++ Casks/purei-play.rb
@@ -1,5 +1,5 @@
 cask "purei-play" do
-  version "0.35-44-gb365f4c1,24-12-2020"
+  version "0.35-51-g4c8e705e,28-12-2020"
   sha256 :no_check
 
   url "https://purei.org/download_latest.php?platform=macos"
