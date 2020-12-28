d4dabfee5f490b30efd083d3be8e5e268926a61f
Joost-Wim Boekesteijn <joost-wim@boekesteijn.nl>
2020-12-28 17:16:36 +0100

    Update fastrawviewer from 1.7.0.1684 to 1.7.1.1693 (#97024)

--- Casks/fastrawviewer.rb
+++ Casks/fastrawviewer.rb
@@ -1,6 +1,6 @@
 cask "fastrawviewer" do
-  version "1.7.0.1684"
-  sha256 "dd48cc8d2fd3adab42177db78c7904a4a62ec1174d5ac32ea30b14b4d9c2be31"
+  version "1.7.1.1693"
+  sha256 "370efe12ea2643cc20a30ca4833dc525bf4f526972c82dfa25eb6ef08107d14d"
 
   url "https://updates.fastrawviewer.com/data/FastRawViewer-#{version}.dmg"
   appcast "https://www.fastrawviewer.com/download"
