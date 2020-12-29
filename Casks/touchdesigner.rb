e9b15550d97d99d1a277851a93874638dd77326f
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-29 18:02:40 +0800

    Update touchdesigner.rb

--- Casks/touchdesigner.rb
+++ Casks/touchdesigner.rb
@@ -1,13 +1,13 @@
 cask "touchdesigner" do
   version "2020.44350"
   sha256 "bf7b4f5c953a6b07fb5151c9bf8174a1386d2bdf46f3e880857d565c707c693a"
-  
 
   url "https://download.derivative.ca/TouchDesigner.#{version}.dmg"
   appcast "https://www.derivative.ca/download/"
   name "Derivative TouchDesigner"
   desc "Visual programming language for multimedia content"
   homepage "https://www.derivative.ca/"
+
   auto_updates true
 
   app "TouchDesigner.app"
