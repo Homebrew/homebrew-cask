d79d98d9fa20a3878e2f5406fe45e8111d08c04c
miccal <miccal.matthews@gmail.com>
2020-12-29 17:55:44 +0800

    Update superslicer from 2.2.53.4,20201111 to 2.3.55.2,20201124

--- Casks/superslicer.rb
+++ Casks/superslicer.rb
@@ -1,6 +1,6 @@
 cask "superslicer" do
-  version "2.2.53.4,20201111"
-  sha256 "04c07dbd71395134cc0d491d4219722aa2b9387d4a28f57b46cc52de672c2cf6"
+  version "2.3.55.2,20201124"
+  sha256 "321fc71b53656d23a5481433c60a59d4412a723c09d08e7a61572bfa8f28d4b0"
 
   url "https://github.com/supermerill/SuperSlicer/releases/download/#{version.before_comma}/SuperSlicer_#{version.before_comma}_macos_#{version.after_comma}.dmg"
   appcast "https://github.com/supermerill/SuperSlicer/releases.atom"
