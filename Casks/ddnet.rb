7b36843fb5eac175a777dd449d8e28939f005717
Luca <b3z@zeppel.eu>
2020-12-30 09:38:59 +0100

    Update ddnet from 15.2.4 to 15.2.5 (#97072)

--- Casks/ddnet.rb
+++ Casks/ddnet.rb
@@ -1,6 +1,6 @@
 cask "ddnet" do
-  version "15.2.4"
-  sha256 "feeff23e551fbae69be15ae95f2bd852efa53d3d4515248e888d64ab589c51a4"
+  version "15.2.5"
+  sha256 "16cb6b106e65360223fc2322fa73e6c7dd25ea951abbbe99b471fc14b317910f"
 
   url "https://ddnet.tw/downloads/DDNet-#{version}-osx.dmg"
   name "DDNet"
