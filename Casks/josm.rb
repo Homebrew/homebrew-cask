8e2b9cf4fe9f39fdeda170552509054473af897b
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-31 05:44:23 +0800

    Update josm.rb

--- Casks/josm.rb
+++ Casks/josm.rb
@@ -1,6 +1,6 @@
 cask "josm" do
   version "17428"
-  sha256 "437c685093b561149b7b2d10b0d5c94894df95938bbdf0a6a79f080f0fa90c12Update josm.rb to 17428"
+  sha256 "437c685093b561149b7b2d10b0d5c94894df95938bbdf0a6a79f080f0fa90c12"
 
   url "https://github.com/grischard/josm/releases/download/#{version}-tested/JOSM-macOS-java15.zip",
       verified: "github.com/grischard/josm/"
