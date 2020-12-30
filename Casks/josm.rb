0922010cd2271a6e4d1fb84e87143bc6daf09e2f
Simon Legner <Simon.Legner@gmail.com>
2020-12-30 13:18:37 +0100

    Update josm.rb to 17428
    Ref: https://josm.openstreetmap.de/wiki/Changelog

--- Casks/josm.rb
+++ Casks/josm.rb
@@ -1,10 +1,10 @@
 cask "josm" do
-  version "17329"
-  sha256 "659b3ec7368eac4ceb0da967ab2913a6dc7aed053637983c2fa27d42e742cc8c"
+  version "17428"
+  sha256 "539a1fa80b0dbd7623aaa4c296b3cda58dc4cfdf851b7bd9b84dfdc0955e3518"
 
-  url "https://github.com/grischard/josm/releases/download/#{version}-tested/JOSM-macOS-java15.zip",
-      verified: "github.com/grischard/josm/"
-  appcast "https://github.com/grischard/josm/releases.atom"
+  url "https://github.com/openstreetmap/josm/releases/download/#{version}-tested/JOSM-macOS-java15.zip",
+      verified: "github.com/openstreetmap/josm/"
+  appcast "https://github.com/openstreetmap/josm/releases.atom"
   name "JOSM"
   desc "Extensible editor for OpenStreetMap"
   homepage "https://josm.openstreetmap.de/"
