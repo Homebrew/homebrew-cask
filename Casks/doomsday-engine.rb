417433141a404fd194b16ef60abcf374670e3c94
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-31 05:04:22 +0800

    Update doomsday-engine.rb

--- Casks/doomsday-engine.rb
+++ Casks/doomsday-engine.rb
@@ -3,7 +3,8 @@ cask "doomsday-engine" do
   sha256 "83b5d3f13e97119ca347a4f463adbb4a96a4b10f5f4f8367da2856ad18305be5"
 
   url "https://files.dengine.net/archive/doomsday_#{version}_x86_64.dmg"
-  appcast "http://api.dengine.net/1/builds/feed"
+  appcast "http://api.dengine.net/1/builds/feed",
+          must_contain: version.major_minor
   name "Doomsday Engine"
   desc "Enhanced source port of Doom, Heretic, and Hexen"
   homepage "https://dengine.net/"
