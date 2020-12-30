505ece4c934dbec789bcc97f94ae12eda5254355
Markus Reiter <me@reitermark.us>
2020-12-30 03:57:12 +0100

    fme: add livecheck (#97006)

--- Casks/fme.rb
+++ Casks/fme.rb
@@ -1,13 +1,19 @@
 cask "fme" do
-  version "2020.2.1,b20806"
+  version "2020.2.1,20806"
   sha256 "c3b8a47d174f0794abf9279156a7a886b965e147be63a247c76798a8c46368c6"
 
-  url "https://downloads.safe.com/fme/#{version.major}/fme-desktop-#{version.before_comma}-#{version.after_comma}-macosx.dmg"
-  appcast "https://www.macupdater.net/cgi-bin/extract_text/extract_text_split_easy.cgi?encoding=utf8&url=https://www.safe.com/api/downloads/&splitter_1=beta&index_1=0",
-          must_contain: "#{version.before_comma}-#{version.after_comma}"
+  url "https://downloads.safe.com/fme/#{version.major}/fme-desktop-#{version.before_comma}-b#{version.after_comma}-macosx.dmg"
   name "FME Desktop"
   desc "Platform for integrating spatial data"
   homepage "https://www.safe.com/"
+
+  livecheck do
+    url "https://www.safe.com/api/downloads/"
+    strategy :page_match do |page|
+      match = page.match(%r{/fme-desktop-(\d+(?:\.\d+)*)-b(\d+)-macosx\.dmg}i)
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   installer script: {
     executable: "FME Desktop Installer.app/Contents/MacOS/applet",
