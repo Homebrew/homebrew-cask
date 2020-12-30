3fb6b44bf36f7ca11c69bf371233b548adb30319
jcbot <jcb@leipert.io>
2020-12-30 05:42:24 +0100

    Update webstorm to 2020.3.1,203.6682.155 (#97061)

--- Casks/webstorm.rb
+++ Casks/webstorm.rb
@@ -1,6 +1,6 @@
 cask "webstorm" do
-  version "2020.3,203.5981.135"
-  sha256 "6365de397ecc2a8ecfd6421c3dec1d1da0d3a7bc68b3ede5bef4a1c608fe3af5"
+  version "2020.3.1,203.6682.155"
+  sha256 "4419ba11957b902dd9283f602d8dcd9ce3281db11195a9e9893005b4c50dd4e7"
 
   url "https://download.jetbrains.com/webstorm/WebStorm-#{version.before_comma}.dmg"
   appcast "https://data.services.jetbrains.com/products/releases?code=WS&latest=true&type=release"
