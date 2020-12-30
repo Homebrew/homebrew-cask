fd14f9f2ba1d0cc63fbc7886b50b834e152cc9d5
Schlaubischlump <Schlaubischlump@users.noreply.github.com>
2020-12-30 11:26:29 +0100

    Update locationsimulator from 0.1.5 to 0.1.7 (#97073)

--- Casks/locationsimulator.rb
+++ Casks/locationsimulator.rb
@@ -1,15 +1,15 @@
 cask "locationsimulator" do
-  version "0.1.5"
-  sha256 "699f212e81bbf0c7f501c10fa3952fcc3d9551218270398657e1b6d633f02da4"
+  version "0.1.7"
+  sha256 "f932fd797bcea1562816550b4b6965e82d5d85f119f389c36a449a71cff76f77"
 
   url "https://github.com/Schlaubischlump/LocationSimulator/releases/download/v#{version}/LocationSimulator_without_dependencies.app.zip"
   appcast "https://github.com/SchlaubiSchlump/LocationSimulator/releases.atom"
   name "LocationSimulator"
-  desc "Application to spoof your iOS / iPadOS / TvOS device location"
+  desc "Application to spoof your iOS / iPadOS device location"
   homepage "https://github.com/Schlaubischlump/LocationSimulator"
 
   depends_on formula: "libimobiledevice"
-  depends_on macos: ">= :high_sierra"
+  depends_on macos: ">= :catalina"
 
   app "LocationSimulator.app"
 
