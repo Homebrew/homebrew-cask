8d645820af2a60c667cfabf378c2018eabc71c38
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2020-12-30 04:50:11 +0100

    Update modulair from 1.2.0 to 1.2.1 (#97057)

--- Casks/modulair.rb
+++ Casks/modulair.rb
@@ -1,6 +1,6 @@
 cask "modulair" do
-  version "1.2.0"
-  sha256 "1ca588558d7252505307f2d9a71f935fe9c5590296b6e69e24c2ef2a657dffc9"
+  version "1.2.1"
+  sha256 "fe1818a43f1756e61054c9fcb054a285c0622a1342e1016f5996837b382d8460"
 
   url "https://www.fullbucket.de/music/dl/modulair_#{version.dots_to_underscores}_mac.pkg"
   appcast "https://www.fullbucket.de/music/modulair.html"
