f51c85e474b418b7078fe8bc2b1b9875b49665f4
Markus Reiter <me@reitermark.us>
2020-12-31 21:39:54 +0100

    chromedriver: add livecheck

--- Casks/chromedriver.rb
+++ Casks/chromedriver.rb
@@ -1,13 +1,17 @@
 cask "chromedriver" do
-  version "87.0.4280.20"
-  sha256 "99e98317a5f883d53973a044778f81abe71b42c570fb35f75786f86ea617bd1a"
+  version "87.0.4280.88"
+  sha256 "e8e4b2eaaf3e9433e368c125643973f5fabeedff349953014ab99c622ed61011"
 
   url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_mac64.zip",
       verified: "chromedriver.storage.googleapis.com/"
-  appcast "https://chromedriver.storage.googleapis.com/LATEST_RELEASE"
   name "ChromeDriver"
   desc "Automated testing of webapps for Google Chrome"
   homepage "https://sites.google.com/a/chromium.org/chromedriver/home"
+
+  livecheck do
+    url "https://chromedriver.storage.googleapis.com/LATEST_RELEASE"
+    regex(/v?(\d+(?:\.\d+)+)/i)
+  end
 
   conflicts_with cask: "homebrew/cask-versions/chromedriver-beta"
 
