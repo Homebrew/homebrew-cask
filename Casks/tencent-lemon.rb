f14126bd7403a82a3e49c9f544a53a8767e75671
JJacRookie <johnjacobs@sina.com>
2021-01-01 05:02:31 +0800

    Update tencent-lemon from 4.8.5 to 4.8.6 (#97127)

--- Casks/tencent-lemon.rb
+++ Casks/tencent-lemon.rb
@@ -1,6 +1,6 @@
 cask "tencent-lemon" do
-  version "4.8.5"
-  sha256 "c9710d05c8d78ad476a725578d0e50fa3eee8f96ddf850a415101acbe39dcc5d"
+  version "4.8.6"
+  sha256 "c26af042e9574faa9bb4d3a631864a43612e471e012ba184f5099b2394da2abf"
 
   url "https://pm.myapp.com/invc/xfspeed/qqpcmgr/module_update/Lemon_#{version}.dmg",
       verified: "pm.myapp.com/invc/xfspeed/qqpcmgr/"
