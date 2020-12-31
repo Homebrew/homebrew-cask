65398c331526a21d8d20c9cf0d8d8cc56292a03b
Markus Reiter <me@reitermark.us>
2020-12-31 22:05:41 +0100

    dbeaver-enterprise: add livecheck

--- Casks/dbeaver-enterprise.rb
+++ Casks/dbeaver-enterprise.rb
@@ -3,9 +3,13 @@ cask "dbeaver-enterprise" do
   sha256 "75e778c353d07fa02ed845fbb4f04afaa8d0462c46ac6b8e3a062349935fd873"
 
   url "https://dbeaver.com/files/#{version}/dbeaver-ee-#{version}-macos.dmg"
-  appcast "https://dbeaver.com/product/version.xml"
   name "DBeaver Enterprise Edition"
   homepage "https://dbeaver.com/"
+
+  livecheck do
+    url "https://dbeaver.com/product/version.xml"
+    regex(%r{<number[^>]*?>v?(\d+(?:\.\d+)+)</number>}i)
+  end
 
   app "DBeaverEE.app"
 
