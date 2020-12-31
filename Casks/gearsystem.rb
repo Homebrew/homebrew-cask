0414d137f6987751146dce25eceec7fbf2f02cbe
Ignacio Sanchez Gines <nacho@geardome.com>
2020-12-31 13:17:04 +0100

    Update gearsystem from 3.1.0 to 3.2.0

--- Casks/gearsystem.rb
+++ Casks/gearsystem.rb
@@ -1,6 +1,6 @@
 cask "gearsystem" do
-  version "3.1.0"
-  sha256 "2235cacee8eab55cf48796e3784f5b1ac2a474d1564e0d8f82799e6ad2b8c12f"
+  version "3.2.0"
+  sha256 "497013f2988490f2b9d0f58fd847c91c899cd05eb5a7a5f00370a4760b2f23a6"
 
   url "https://github.com/drhelius/Gearsystem/releases/download/gearsystem-#{version}/Gearsystem-#{version}-macOS.zip"
   appcast "https://github.com/drhelius/Gearsystem/releases.atom"
