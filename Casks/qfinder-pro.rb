3eeab0af53998891e4fc8084d1f64a7c22d7cf74
Markus Reiter <me@reitermark.us>
2020-12-31 23:21:06 +0100

    qfinder-pro: add livecheck

--- Casks/qfinder-pro.rb
+++ Casks/qfinder-pro.rb
@@ -3,10 +3,14 @@ cask "qfinder-pro" do
   sha256 "121ab79617ce7a861b6ae9f422efe98268a5a3fcddfcc3eada3c1a5d8ab22a71"
 
   url "https://download.qnap.com/Storage/Utility/QNAPQfinderProMac-#{version}.dmg"
-  appcast "https://www.macupdater.net/cgi-bin/extract_text/extract_text_split_easy.cgi?url=https://update.qnap.com/SoftwareRelease.xml&splitter_1=Mac_for_QT&index_1=1&splitter_2=downloadURL&index_2=1"
   name "Qnap Qfinder Pro"
   desc "NAS management application"
   homepage "https://www.qnap.com/en/utilities#utliity_5"
+
+  livecheck do
+    url "https://update.qnap.com/SoftwareRelease.xml"
+    regex(%r{/QNAPQfinderProMac-(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   pkg "Qfinder Pro.pkg"
 
