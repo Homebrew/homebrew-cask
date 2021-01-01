8eaef1e08b4110202e05b35927e4596248920069
Markus Reiter <me@reitermark.us>
2021-01-01 04:55:38 +0100

    qsync-client: add livecheck (#97166)

--- Casks/qsync-client.rb
+++ Casks/qsync-client.rb
@@ -3,9 +3,13 @@ cask "qsync-client" do
   sha256 "ebdace7b5c170e946a68e78a6dbf1d713996a2299eb09b2e7ddce06fff128b14"
 
   url "https://download.qnap.com/Storage/Utility/QNAPQsyncClientMac-#{version}.dmg"
-  appcast "https://update.qnap.com/SoftwareRelease.xml"
   name "Qnap Qsync"
   homepage "https://www.qnap.com/i/in/utility/#block_3"
+
+  livecheck do
+    url "https://update.qnap.com/SoftwareRelease.xml"
+    regex(/QNAPQsyncClientMac[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+  end
 
   installer manual: "Qsync Client.pkg"
 
