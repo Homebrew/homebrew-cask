9d215f234978cab9d43076f09e4a6b86e3776ba4
Markus Reiter <me@reitermark.us>
2021-01-01 02:24:55 +0100

    boinc: add livecheck (#97148)

--- Casks/boinc.rb
+++ Casks/boinc.rb
@@ -3,10 +3,14 @@ cask "boinc" do
   sha256 "9a0eabad4b6780f56543c5634bfcef46728bd77f95f3d5e46cc35a0b6bdbdce1"
 
   url "https://boinc.berkeley.edu/dl/boinc_#{version}_macOSX_universal.zip"
-  appcast "https://www.macupdater.net/cgi-bin/extract_text/download_with_useragent.cgi?url=https://boinc.berkeley.edu/download.php&user_agent=Mac"
   name "Berkeley Open Infrastructure for Network Computing"
   name "BOINC"
   homepage "https://boinc.berkeley.edu/"
+
+  livecheck do
+    url "https://boinc.berkeley.edu/download.php"
+    regex(%r{href=.*?/dl/boinc[._-]v?(\d+(?:\.\d+)+)_macOSX_universal\.zip}i)
+  end
 
   pkg "boinc_#{version}_macOSX_universal/BOINC Installer.app/Contents/Resources/BOINC.pkg"
 
