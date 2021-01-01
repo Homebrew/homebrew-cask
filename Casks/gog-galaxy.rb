0785d87d96988b4d944eea0dd375ebcce1985d06
Markus Reiter <me@reitermark.us>
2021-01-01 05:02:52 +0100

    gog-galaxy: add livecheck (#97161)

--- Casks/gog-galaxy.rb
+++ Casks/gog-galaxy.rb
@@ -1,12 +1,16 @@
 cask "gog-galaxy" do
-  version "2.0.33.23a"
-  sha256 "e11b6704f4f8af9c50f37d13056fb59f24743a6ec37998acd0bd7504764f9b2e"
+  version "2.0.35.19a"
+  sha256 "988eb7ec522a6faae39bc5fb1df373d6dbc96b0a2471095bc7c7f68986e8add5"
 
-  url "https://cdn.gog.com/open/galaxy/client/galaxy_client_#{version}.pkg"
-  appcast "https://www.macupdater.net/cgi-bin/extract_text/extract_text_split_easy.cgi?url=https://gog.com/galaxy&encoding=utf-8&user_agent=Macintosh"
-  name "GOG Galaxy Client"
-  desc "GOG gaming client"
+  url "https://cdn.gog.com/open/galaxy/client/#{version.chomp("a")}/galaxy_client_#{version}.pkg"
+  name "GOG Galaxy"
+  desc "Game client"
   homepage "https://www.gog.com/galaxy"
+
+  livecheck do
+    url "https://gog.com/galaxy"
+    regex(%r{href=.*?/galaxy_client_(\d+(?:\.\d+)*a?)\.pkg}i)
+  end
 
   auto_updates true
 
