0dcb137920aa8f4a9fa03163ac6bbfd13922de56
Markus Reiter <me@reitermark.us>
2021-01-01 05:12:55 +0100

    steermouse: add livecheck (#97168)

--- Casks/steermouse.rb
+++ Casks/steermouse.rb
@@ -3,9 +3,13 @@ cask "steermouse" do
   sha256 "c66465072162a7a2e9ee41cfe41b9538db83e4bcd20b7d066df5fa714083d80c"
 
   url "https://plentycom.jp/ctrl/files_sm/SteerMouse#{version}.dmg"
-  appcast "https://plentycom.jp/en/steermouse/download.php"
   name "SteerMouse"
   homepage "https://plentycom.jp/en/steermouse/"
+
+  livecheck do
+    url "https://plentycom.jp/en/steermouse/download.php"
+    regex(/href=.*?SteerMouse[._-]?v?(\d+(?:\.\d+)+)\.dmg/i)
+  end
 
   prefpane "SteerMouse.prefPane"
 
