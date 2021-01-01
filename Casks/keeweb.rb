7f7cc2cfb986e147f8329f051158fdf77a70d8e8
OttNorml <2350859+ottnorml@users.noreply.github.com>
2021-01-01 15:22:19 +0100

    Update keeweb from 1.16.5 to 1.16.6 (#97190)

--- Casks/keeweb.rb
+++ Casks/keeweb.rb
@@ -1,6 +1,6 @@
 cask "keeweb" do
-  version "1.16.5"
-  sha256 "d36cbc329f41c8e4192ff5297fd0f2fece04e5b5a22f12102bf8e887d9943733"
+  version "1.16.6"
+  sha256 "24b0854faa7feb8677f39545b2e96e7498082c04e8d458912708c1afed29146f"
 
   url "https://github.com/keeweb/keeweb/releases/download/v#{version}/KeeWeb-#{version}.mac.x64.dmg",
       verified: "github.com/keeweb/keeweb/"
