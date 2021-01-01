488c2018abd9afc7185c3e5006de87046b566dce
Markus Reiter <me@reitermark.us>
2021-01-01 05:28:47 +0100

    yed: add livecheck (#97171)

--- Casks/yed.rb
+++ Casks/yed.rb
@@ -3,10 +3,14 @@ cask "yed" do
   sha256 "8cf3666b7c83dcd365ef1308ab8f5eeb53bea2405682ae9cae01a3cfd055ca98"
 
   url "https://www.yworks.com/resources/yed/demo/yEd-#{version}_with-JRE14.dmg"
-  appcast "https://www.yworks.com/products/yed/download#download"
   name "yWorks yEd"
   desc "Create diagrams manually, or import external data for analysis"
   homepage "https://www.yworks.com/products/yed"
+
+  livecheck do
+    url "https://www.yworks.com/products/yed/download#download"
+    regex(/yEd[._-]v?(\d+(?:\.\d+)+)[._-]with[._-]JRE\d+?\.dmg/i)
+  end
 
   app "yEd.app"
 end
