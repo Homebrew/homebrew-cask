878e16742eb91af79c2fa505d83821deca7574d6
Markus Reiter <me@reitermark.us>
2021-01-01 03:10:13 +0100

    crystalmaker: add livecheck (#97153)

--- Casks/crystalmaker.rb
+++ Casks/crystalmaker.rb
@@ -1,12 +1,16 @@
 cask "crystalmaker" do
-  version "10.6.0,46910"
+  version "10.6.0"
   sha256 :no_check
 
   url "http://crystalmaker.com/downloads/crystalmaker_mac.zip"
-  appcast "http://crystalmaker.com/support/updates/VersionData-CMM.xml"
   name "CrystalMaker"
   desc "Visualize crystal and molecular structures"
   homepage "http://crystalmaker.com/index.html"
+
+  livecheck do
+    url "http://crystalmaker.com/support/updates/VersionData-CMM.xml"
+    regex(/<version[^>]+?number=["']?v?(\d+(?:\.\d+)+)["']?[^>]*?>/i)
+  end
 
   depends_on macos: ">= :sierra"
 
