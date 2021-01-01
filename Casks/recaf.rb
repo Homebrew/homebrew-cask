3ab0c52f0418202e87b9716f62e0a4a521f654e2
Tom Matthews <trcm@pm.me>
2021-01-01 11:15:49 +0000

    Update recaf from 2.16.6 to 2.16.8 (#97182)

--- Casks/recaf.rb
+++ Casks/recaf.rb
@@ -1,6 +1,6 @@
 cask "recaf" do
-  version "2.16.6"
-  sha256 "1bd837161f878346c8678a095491a2f8c1d72d122e96748a29b3802c62cf670e"
+  version "2.16.8"
+  sha256 "b4564eb8bf359d731a22e752198d010ab9a60ebe2c007dea314c277aa88afe6a"
 
   url "https://github.com/Col-E/Recaf/releases/download/#{version}/recaf-#{version}-J8-jar-with-dependencies.jar",
       verified: "github.com/Col-E/Recaf/"
