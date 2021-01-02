8d0f1cabeeaac23bda47ff1045b2fc309ed0e65d
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2021-01-02 15:15:34 -0800

    Update mricron.rb

--- Casks/mricron.rb
+++ Casks/mricron.rb
@@ -1,5 +1,5 @@
 cask "mricron" do
-  version :latest
+  version "v1.0.20201102"
   sha256 :no_check
 
   url "https://www.nitrc.org/frs/download.php/12005/MRIcron_macOS_Universal_X86_ARM.dmg?i_agree=1&download_now=1"
@@ -10,4 +10,6 @@ cask "mricron" do
   app "MRIcron.app"
   binary "#{appdir}/MRIcron.app/Contents/Resources/bet"
   binary "#{appdir}/MRIcron.app/Contents/Resources/dcm2niix"
+  
+  zap trash: "~/Library/Saved Application State/com.mricro.MRIcron.savedState"
 end
