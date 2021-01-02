01783e94124ed898940df54f5556af700d1b96d4
u254254 <72965213+u254254@users.noreply.github.com>
2021-01-02 17:27:35 +0900

    Update printrun.rb
    fix 404 error

--- Casks/printrun.rb
+++ Casks/printrun.rb
@@ -2,10 +2,10 @@ cask "printrun" do
   version "2.0.0rc7"
   sha256 "426229043a2a33a6768dcca12135f0751c007595af078665eb854cf87e4e2999"
 
-  url "https://github.com/kliment/Printrun/releases/download/printrun-#{version}/Printrun-Mac-#{version}.zip"
+  url "https://github.com/kliment/Printrun/releases/download/printrun-#{version}/Printrun-#{version}-macos.zip"
   appcast "https://github.com/kliment/Printrun/releases.atom"
   name "Printrun"
   homepage "https://github.com/kliment/Printrun"
 
-  app "pronterface.app"
+  app "Printrun-#{version}-macos/pronterface.app"
 end
