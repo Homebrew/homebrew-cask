3998a265748fd13599bea8a3acaa66b3d7c0b5e1
Zhustec <zhustec@foxmail.com>
2021-01-03 04:24:39 +0800

    Add zap to tencent-lemon (#97181)

--- Casks/tencent-lemon.rb
+++ Casks/tencent-lemon.rb
@@ -9,4 +9,22 @@ cask "tencent-lemon" do
   homepage "https://mac.gj.qq.com/"
 
   app "Tencent Lemon.app"
+
+  uninstall delete: [
+    "/Library/Logs/Lemon",
+    "/Library/Preferences/LemonDaemon_packet.dat",
+    "/private/var/folders/ht/cbrdb7ln52x64ck8kbxzgdw80000gn/C/com.tencent.Lemon",
+    "/private/var/folders/ht/cbrdb7ln52x64ck8kbxzgdw80000gn/C/com.tencent.LemonMonitor",
+    "/private/var/run/com.tencent.Lemon.socket",
+  ]
+
+  zap trash: [
+    "~/Library/Caches/com.tencent.Lemon",
+    "~/Library/Caches/com.tencent.LemonMonitor",
+    "~/Library/Logs/LemonMonitor.log",
+    "~/Library/Logs/Tencent Lemon.log",
+    "~/Library/Preferences/com.tencent.LemonUpdate.plist",
+    "~/Library/Preferences/LemonMonitor_packet.dat",
+    "~/Library/Preferences/Tencent Lemon_packet.dat",
+  ]
 end
