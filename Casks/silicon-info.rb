ba91a66222d0363899ec11248e90db4c55f28dd7
Zennon Gosalvez <1798166+zgosalvez@users.noreply.github.com>
2021-01-03 04:25:29 +0800

    Add Silicon Info.app v1.0.3 (#97174)

---
+++ Casks/silicon-info.rb
@@ -0,0 +1,19 @@
+cask "silicon-info" do
+  version "1.0.3"
+  sha256 "ada6ba4665e2aa2973e2faef59f312cc82ae689505236c4c2d412bc3ae18d0f9"
+
+  url "https://github.com/billycastelli/Silicon-Info/releases/download/#{version}/Silicon.Info.app.zip"
+  appcast "https://github.com/billycastelli/Silicon-Info/releases.atom"
+  name "Silicon Info"
+  desc "View the architecture of the running application"
+  homepage "https://github.com/billycastelli/Silicon-Info"
+
+  depends_on macos: ">= :big_sur"
+
+  app "Silicon Info.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.wcastelli.silicon-info",
+    "~/Library/Containers/com.wcastelli.silicon-info",
+  ]
+end
