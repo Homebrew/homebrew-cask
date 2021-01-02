cfd317b4494f8e6457075d6369f658035984fdf5
u254254 <72965213+u254254@users.noreply.github.com>
2021-01-02 17:29:05 +0900

    Update printrun.rb
    use new hash

--- Casks/printrun.rb
+++ Casks/printrun.rb
@@ -1,6 +1,6 @@
 cask "printrun" do
   version "2.0.0rc7"
-  sha256 "426229043a2a33a6768dcca12135f0751c007595af078665eb854cf87e4e2999"
+  sha256 "c2c6c2d519b5a224b67e6b9fa7be2a70aac83839f9acc6309c4f70f3c40e4858"
 
   url "https://github.com/kliment/Printrun/releases/download/printrun-#{version}/Printrun-#{version}-macos.zip"
   appcast "https://github.com/kliment/Printrun/releases.atom"
