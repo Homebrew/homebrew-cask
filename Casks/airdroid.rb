9454efd33e69ed81cec03e9ad98e5018a840d0cf
Caleb Xu <calebcenter@live.com>
2021-01-02 04:03:03 -0500

    Update airdroid from 3.6.9.1 to 3.6.9.2 (#97199)

--- Casks/airdroid.rb
+++ Casks/airdroid.rb
@@ -1,6 +1,6 @@
 cask "airdroid" do
-  version "3.6.9.1"
-  sha256 "5537131e9c14032f2e962f00209a0f8c03a21fe0cae0b6cc3e46e1b2d5c79d67"
+  version "3.6.9.2"
+  sha256 "5ed0cb9c480be4fc18ddfc184f92481d4b7c1871446f1ebab36c1dcb84614921"
 
   url "https://s3.amazonaws.com/dl.airdroid.com/AirDroid_Desktop_Client_#{version}.dmg",
       verified: "s3.amazonaws.com/dl.airdroid.com/"
