44b6ecc0b0ea47b01b4923d01e9c76651923f5d8
Caleb Xu <calebcenter@live.com>
2021-01-02 06:49:14 -0500

    Update trilium-notes from 0.45.6 to 0.45.7 (#97214)

--- Casks/trilium-notes.rb
+++ Casks/trilium-notes.rb
@@ -1,6 +1,6 @@
 cask "trilium-notes" do
-  version "0.45.6"
-  sha256 "b3f6ce1e74a1252f7bd6faf6dcf240c9f9c695cfb22a97d8e5b22e12576d526c"
+  version "0.45.7"
+  sha256 "ecb2f50c79243c26237e3d58c3bcf6e17c02764cbb1c23f6fde8673d2049a0de"
 
   url "https://github.com/zadam/trilium/releases/download/v#{version}/trilium-mac-x64-#{version}.zip"
   appcast "https://github.com/zadam/trilium/releases.atom"
