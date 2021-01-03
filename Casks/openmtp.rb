24a14bcf433993e0717d90147c785138ca2263ed
Ganesh Rathinavel <ganeshrvel@outlook.com>
2021-01-04 00:25:00 +0530

    updated to OpenMTP-3.0.0

--- Casks/openmtp.rb
+++ Casks/openmtp.rb
@@ -1,12 +1,12 @@
 cask "openmtp" do
-  version "2.4.4"
-  sha256 "25423e374b0c50763670a5368ecea19c01789906d31f14d02a1501d541ed31de"
+  version "3.0.0"
+  sha512 "U2iMdJuQMqSqmBzWHw+M5ENwrFXuoBql0gOOMlQfYHMVFDm1/8dP9qPI9RZ5VbzUwMRZlrmMhaTdW0+SyXGaGQ=="
 
   url "https://github.com/ganeshrvel/openmtp/releases/download/v#{version}/OpenMTP-#{version}-mac.zip",
       verified: "github.com/ganeshrvel/openmtp/"
   appcast "https://github.com/ganeshrvel/openmtp/releases.atom"
   name "OpenMTP"
-  homepage "https://ganeshrvel.github.io/openmtp"
+  homepage "https://openmtp.ganeshrvel.com/"
 
   app "OpenMTP.app"
 
@@ -14,5 +14,6 @@ cask "openmtp" do
     "~/.io.ganeshrvel",
     "~/Library/Application Support/OpenMTP",
     "~/Library/Preferences/io.ganeshrvel.openmtp.plist",
+    "~/Library/Application Support/io.ganeshrvel.openmtp",
   ]
 end
