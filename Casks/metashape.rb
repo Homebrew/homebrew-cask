374e4ef2ba32e4cc71fd86922793443f418e9576
Anver Housseini <anver@anykey.ch>
2021-01-03 19:57:29 +0100

    Update metashape from 1.6.5 to 1.7.0

--- Casks/metashape.rb
+++ Casks/metashape.rb
@@ -1,6 +1,6 @@
 cask "metashape" do
-  version "1.6.5"
-  sha256 "ee52edc7c771498c6589247065bcfaee38735aae86517d1484e854b73d753672"
+  version "1.7.0"
+  sha256 "dbe0731d2b6ed92ea93a2dd0d45b1cb005667967ca0aed9719bfd16c55a76ca8"
 
   url "http://download.agisoft.com/metashape_#{version.dots_to_underscores}.dmg"
   appcast "https://www.agisoft.com/downloads/installer/"
