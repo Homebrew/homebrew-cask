bf1574da6ed966740a1c8b59336d15c8cd39aaaa
u254254 <72965213+u254254@users.noreply.github.com>
2021-01-03 17:05:03 +0900

    Update printrun.rb

--- Casks/printrun.rb
+++ Casks/printrun.rb
@@ -5,9 +5,10 @@ cask "printrun" do
   url "https://github.com/kliment/Printrun/releases/download/printrun-#{version}/Printrun-#{version}-macos.zip"
   appcast "https://github.com/kliment/Printrun/releases.atom"
   name "Printrun"
+  desc "It have stable version (1.6.0,18Nov2017), but it cannot run in catalina."
   homepage "https://github.com/kliment/Printrun"
-  desc "It have stable version (1.6.0,18Nov2017), but it's 32-bit (cannot run in catalina). I replaced old 18Nov2017 to 2.0.0rc7."
-  caveats "You MUST run 'chmod a+x /Applications/pronterface.app/Contents/MacOS/pronterface' in shell after install."
 
   app "pronterface.app"
+
+  caveats "You MUST run 'chmod a+x /Applications/pronterface.app/Contents/MacOS/pronterface' in shell after install."
 end
