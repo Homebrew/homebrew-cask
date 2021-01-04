6ae8a2782b163b74d15a6cafbc76a1261d45448c
Dominik Moritz <domoritz@gmail.com>
2021-01-04 16:51:26 +0100

    Add miniforge

---
+++ Casks/miniforge.rb
@@ -0,0 +1,29 @@
+cask "miniforge" do
+  version "4.9.2-5"
+  sha256 "a33aca4dd6686b1dba790b224c9e686d7e08c86e6074379194f3bde478d883ed"
+
+  url "https://github.com/conda-forge/miniforge/releases/download/#{version}/Miniforge3-#{version}-MacOSX-x86_64.sh"
+  name "miniforge"
+  homepage "https://github.com/conda-forge/miniforge"
+
+  auto_updates true
+  container type: :naked
+
+  installer script: {
+    executable: "Miniforge3-#{version}-MacOSX-x86_64.sh",
+    args:       ["-b", "-p", "#{caskroom_path}/base"],
+  }
+  binary "#{caskroom_path}/base/condabin/conda"
+
+  uninstall delete: "#{caskroom_path}/base"
+
+  zap trash: [
+    "~/.condarc",
+    "~/.conda",
+  ]
+
+  caveats <<~EOS
+    Please run the following to setup your shell:
+      conda init "$(basename "${SHELL}")"
+  EOS
+end
