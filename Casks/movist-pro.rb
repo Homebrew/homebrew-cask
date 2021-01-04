43af2369132e58310e715ab71eaec6cb0dcf49b3
C Joo <moosoy@gmail.com>
2021-01-04 21:54:20 +0900

    Update movist-pro from 2.6.3,167 to 2.6.3_1

--- Casks/movist-pro.rb
+++ Casks/movist-pro.rb
@@ -1,6 +1,6 @@
 cask "movist-pro" do
-  version "2.6.3,167"
-  sha256 "0715d43b095abf7358264334b53a14e3444874e3eaf4c0beae0576ab1c51a284"
+  version "2.6.3_1"
+  sha256 "5e14e674b0ec896be54e2b95d68416a05e2b4acf7ef83fdd0faf771d470a9bb6"
 
   url "https://s3.ap-northeast-2.amazonaws.com/update.cocoable.com/MovistPro_#{version.before_comma}.app.zip",
       verified: "s3.ap-northeast-2.amazonaws.com/update.cocoable.com/"
