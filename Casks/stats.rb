5bc8ca97e65a2e0180829b9e8e371ab2a61912e2
bevanjkay <40621599+bevanjkay@users.noreply.github.com>
2021-01-06 04:42:42 +1100

    Update stats from 2.4.10 to 2.4.11 (#97267)

--- Casks/stats.rb
+++ Casks/stats.rb
@@ -1,6 +1,6 @@
 cask "stats" do
-  version "2.4.10"
-  sha256 "ee9eb9bb19b77560ce522e64ea071118f9ebed5f2afb11002a85b47bc6585de1"
+  version "2.4.11"
+  sha256 "8c37f2ce387fe78a313b2dc7d9e77b3ec9044436d56f4320a0dce53506bf3abb"
 
   url "https://github.com/exelban/stats/releases/download/v#{version}/Stats.dmg"
   appcast "https://github.com/exelban/stats/releases.atom"
