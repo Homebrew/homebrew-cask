76d4cb649f0494af929352135b53ff403d40626c
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-07 07:56:20 +0800

    Update resolutionator.rb

--- Casks/resolutionator.rb
+++ Casks/resolutionator.rb
@@ -3,7 +3,7 @@ cask "resolutionator" do
   sha256 "d3c9b2bd7fba21c6d9347f437b7cd1e3c8bf1079c6bf18ad2d6f095a2755e50b"
 
   url "https://manytricks.com/download/_do_not_hotlink_/resolutionator#{version.no_dots}.dmg"
-  appcast "https://manytricks.com/resolutionator/appcast"
+  appcast "https://manytricks.com/resolutionator/releasenotes/?rref=appcast"
   name "Resolutionator"
   homepage "https://manytricks.com/resolutionator/"
 
