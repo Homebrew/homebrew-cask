567633706d2bef85265e5a158a54ac03dd82d2d4
Luflosi <Luflosi@users.noreply.github.com>
2021-01-06 19:36:07 +0100

    Update gitify from 4.0.0 to 4.1.0 (#97310)

--- Casks/gitify.rb
+++ Casks/gitify.rb
@@ -1,6 +1,6 @@
 cask "gitify" do
-  version "4.0.0"
-  sha256 "2c051280f1ac2cb410736f013f0dc99d1a808e7574c0bc9b46b399dfa6ee466b"
+  version "4.1.0"
+  sha256 "f6753228a8226b353018c96ff861177bd631313a51bd898af5a7688acf23ced3"
 
   url "https://github.com/manosim/gitify/releases/download/v#{version}/Gitify-#{version}-mac.zip"
   appcast "https://github.com/manosim/gitify/releases.atom"
