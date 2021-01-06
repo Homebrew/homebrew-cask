6d5ddbaa2c9cdc7d56ac600811b11ede505d75d0
Joost-Wim Boekesteijn <joost-wim@boekesteijn.nl>
2021-01-06 20:45:31 +0100

    Update openmtp from 2.4.4 to 3.0.0 (#97294)

--- Casks/openmtp.rb
+++ Casks/openmtp.rb
@@ -1,6 +1,6 @@
 cask "openmtp" do
-  version "2.4.4"
-  sha256 "25423e374b0c50763670a5368ecea19c01789906d31f14d02a1501d541ed31de"
+  version "3.0.0"
+  sha256 "a77ca47ee572c570eba4cccc6c641b407895d350a654ca8affdda5a4325b8931"
 
   url "https://github.com/ganeshrvel/openmtp/releases/download/v#{version}/OpenMTP-#{version}-mac.zip",
       verified: "github.com/ganeshrvel/openmtp/"
