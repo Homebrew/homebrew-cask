9a0cb14328307f6c0863268c6ba7b0bcc9589014
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-07 07:04:23 +0800

    Update tuxera-ntfs.rb

--- Casks/tuxera-ntfs.rb
+++ Casks/tuxera-ntfs.rb
@@ -1,6 +1,6 @@
 cask "tuxera-ntfs" do
   version "2020"
-  sha256 :no_check
+  sha256 :no_check # required as upstream package is updated in-place
 
   url "https://tuxera.com/mac/tuxerantfs_#{version}.dmg"
   appcast "https://ntfsformac.tuxera.com/",
@@ -13,7 +13,10 @@ cask "tuxera-ntfs" do
 
   pkg ".packages/Flat/Install Tuxera NTFS.mpkg"
 
-  uninstall quit:    "com.tuxera.Tuxera-NTFS",
+  uninstall quit:    [
+    "com.tuxera.Tuxera-NTFS",
+    "com.tuxera.filesystems.ntfs.agent",
+  ],
             pkgutil: [
               "com.tuxera.pkg.Tuxera_NTFS",
               "com.tuxera.pkg.Tuxera_NTFS_compat",
