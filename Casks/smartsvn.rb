790f74387e8d0596d51a658fc58b55de976df805
Ralph Martin <ralph@cs.cf.ac.uk>
2021-01-06 17:37:43 +0000

    Update smartsvn from 14.0.2 to 14.0.3 (#97306)

--- Casks/smartsvn.rb
+++ Casks/smartsvn.rb
@@ -1,6 +1,6 @@
 cask "smartsvn" do
-  version "14.0.2"
-  sha256 "4c7d9df6bb07bd422f082f36b0c5d44f590b7175065a09fa0f65e95967f8e77e"
+  version "14.0.3"
+  sha256 "c38ddac8526a00ea297eb05bde20849e879867047e4833d7dc37c67ce0a3602a"
 
   url "https://www.smartsvn.com/downloads/smartsvn/smartsvn-macosx-#{version.dots_to_underscores}.dmg"
   appcast "https://www.smartsvn.com/documents/smartsvn/changelog.txt"
