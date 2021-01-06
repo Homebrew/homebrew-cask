a87c386affff372d76f17fb0c019bb6a8c48a8b2
Jeffrey C <spaz926@users.noreply.github.com>
2021-01-06 15:37:01 -0600

    Update macbreakz from 5.41 to 5.42 (#97354)

--- Casks/macbreakz.rb
+++ Casks/macbreakz.rb
@@ -1,6 +1,6 @@
 cask "macbreakz" do
-  version "5.41"
-  sha256 "941d07cc1a6e0c134f01147a69456b771719ad106be8ecd65d488a2a9fc8ff5d"
+  version "5.42"
+  sha256 "dd16318c25da75046e2ba84f8f509af453aa16575f7745b72d037275074285e4"
 
   url "https://www.publicspace.net/download/MacBreakZ#{version.major}.dmg"
   name "MacBreakZ"
