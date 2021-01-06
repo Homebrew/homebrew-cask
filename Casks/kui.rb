9d706fba1b5eb72a4469eca16b894f8f50d11fda
Alexander Zigelski <ali@crunchtime.dev>
2021-01-06 02:44:53 +0100

    Update kui from 9.3.2 to 9.3.7

--- Casks/kui.rb
+++ Casks/kui.rb
@@ -1,6 +1,6 @@
 cask "kui" do
-  version "9.3.2"
-  sha256 "4cd3f7da931203a6dec1fb5919ac4dfcd3e3d689d524a7be40ccb2202e0aaef1"
+  version "9.3.7"
+  sha256 "5adc0b3f584bd70ec4e2e06c105d3e0297f5373df9a9146466694ef1fb84610e"
 
   url "https://github.com/IBM/kui/releases/download/v#{version}/Kui-darwin-x64.tar.bz2",
       verified: "github.com/IBM/kui/"
