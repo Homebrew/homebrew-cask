3dab27926a670e3961c3525931ffd3f3382c5483
Bevan Kay <bevanjkay@gmail.com>
2021-01-06 22:01:34 +1100

    Update plex-media-server from 1.21.1.3830-6c22540d5 to 1.21.1.3876-3c3adfcb4

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -1,6 +1,6 @@
 cask "plex-media-server" do
-  version "1.21.1.3830-6c22540d5"
-  sha256 "129292c9ebc57d5d1a771f920e2dcc6ddad47783536cc42bca972a687e5f3bca"
+  version "1.21.1.3876-3c3adfcb4"
+  sha256 "82bc7de6fbf57638f94915e93ed1f80768c07111dc94c44a0741624adcd4119b"
 
   url "https://downloads.plex.tv/plex-media-server-new/#{version}/macos/PlexMediaServer-#{version}-x86_64.zip"
   appcast "https://plex.tv/api/downloads/5.json"
