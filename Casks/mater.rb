893e3ed4002f52ae612a4e5a372e27a3381a9fe9
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:06 +0100

    mater: add livecheck

--- Casks/mater.rb
+++ Casks/mater.rb
@@ -3,9 +3,13 @@ cask "mater" do
   sha256 "21b7612fd3be0c14719b217e7c3a28d7c9a4dc6eddba96d25af6d51c58de564a"
 
   url "https://github.com/jasonlong/mater/releases/download/v#{version}/Mater-darwin-x64.zip"
-  appcast "https://github.com/jasonlong/mater/releases.atom"
   name "Mater"
   homepage "https://github.com/jasonlong/mater"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Mater-darwin-x64/Mater.app"
 end
