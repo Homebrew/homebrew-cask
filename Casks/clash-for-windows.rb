5b35e2040e3646f4f7fa59419708a7534e544a45
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:06 +0100

    clash-for-windows: add livecheck

--- Casks/clash-for-windows.rb
+++ Casks/clash-for-windows.rb
@@ -3,9 +3,13 @@ cask "clash-for-windows" do
   sha256 "dbf45366dbdcee57025b6568866499567de2e620ee20fdc36b974ce96de69f6c"
 
   url "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/#{version}/Clash.for.Windows-#{version}.dmg"
-  appcast "https://github.com/Fndroid/clash_for_windows_pkg/releases.atom"
   name "Clash for Windows"
   homepage "https://github.com/Fndroid/clash_for_windows_pkg"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Clash for Windows.app"
 end
