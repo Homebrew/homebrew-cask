53c060a370a9d14a602d2d3ec4c83193841f8f91
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:19 +0100

    nocturnal: add livecheck

--- Casks/nocturnal.rb
+++ Casks/nocturnal.rb
@@ -3,10 +3,14 @@ cask "nocturnal" do
   sha256 "a66c59daa1d1c59e5403aee4eb868a3967f1bdb4d90033fa3ee692bffd7db0b9"
 
   url "https://github.com/HarshilShah/Nocturnal/releases/download/#{version}/Nocturnal.zip"
-  appcast "https://github.com/HarshilShah/Nocturnal/releases.atom"
   name "Nocturnal"
   desc "Simple app to toggle dark mode with one click"
   homepage "https://github.com/HarshilShah/Nocturnal"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   depends_on macos: ">= :mojave"
 
