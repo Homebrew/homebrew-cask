158dc59de48a95acca22be7c93f3eefd1e2278eb
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:23 +0100

    encryptr: add livecheck

--- Casks/encryptr.rb
+++ Casks/encryptr.rb
@@ -3,9 +3,13 @@ cask "encryptr" do
   sha256 "54e2ef8f8c4d22a20793b68fd35c65f8af34ffd783c48140b84c1a9b445589ea"
 
   url "https://spideroak.com/dist/encryptr/signed/osx/Encryptr-v#{version}-mac.dmg"
-  appcast "https://github.com/SpiderOak/Encryptr/releases.atom"
   name "SpiderOak Encryptr"
   homepage "https://spideroak.com/encryptr/"
+
+  livecheck do
+    url "https://github.com/SpiderOak/Encryptr/releases"
+    strategy :git
+  end
 
   app "Encryptr.app"
 
