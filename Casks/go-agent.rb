16c0f260107f2ea0cdd95bb02f67980a14689bd9
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:35 +0100

    go-agent: add livecheck

--- Casks/go-agent.rb
+++ Casks/go-agent.rb
@@ -4,11 +4,15 @@ cask "go-agent" do
 
   url "https://download.gocd.io/binaries/#{version}/osx/go-agent-#{version}-osx.zip",
       verified: "download.gocd.io/binaries/"
-  appcast "https://github.com/gocd/gocd/releases.atom"
   name "Go Agent"
   name "GoCD Agent"
   desc "Agent for the Go Continuous Delivery platform"
   homepage "https://www.gocd.org/"
+
+  livecheck do
+    url "https://github.com/gocd/gocd/releases"
+    strategy :git
+  end
 
   app "Go Agent.app"
 end
