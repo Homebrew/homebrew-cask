82b783750de4b675e275939e488ac3b19fb350e3
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:10 +0100

    minishift: add livecheck

--- Casks/minishift.rb
+++ Casks/minishift.rb
@@ -3,9 +3,13 @@ cask "minishift" do
   sha256 "3e80aa7987ae3440d15fbd529311a574ad1fa5cee77cff35d64d85e011ce1128"
 
   url "https://github.com/minishift/minishift/releases/download/v#{version}/minishift-#{version}-darwin-amd64.tgz"
-  appcast "https://github.com/minishift/minishift/releases.atom"
   name "Minishift"
   homepage "https://github.com/minishift/minishift"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   binary "minishift-#{version}-darwin-amd64/minishift"
 end
