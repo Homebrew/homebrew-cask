cbdf4e01c3f9829d93df0b49fac204bbcf31c163
Markus Reiter <me@reitermark.us>
2021-01-07 13:21:32 +0100

    Update mactex.rb

--- Casks/mactex.rb
+++ Casks/mactex.rb
@@ -1,8 +1,8 @@
 cask "mactex" do
-  version "2020-04-07"
+  version "2020.0407"
   sha256 "a33af89de36c7c84a76050c9704d50d23892e9c2070f04f6a53e1c6d5a332f67"
 
-  url "http://mirror.ctan.org/systems/mac/mactex/mactex-#{version.no_hyphens}.pkg",
+  url "http://mirror.ctan.org/systems/mac/mactex/mactex-#{version.no_dots}.pkg",
       verified: "mirror.ctan.org/systems/mac/mactex/"
   name "MacTeX"
   desc "Full TeX Live distribution with GUI applications"
@@ -12,7 +12,7 @@ cask "mactex" do
     url "http://mirror.ctan.org/systems/mac/mactex/"
     strategy :page_match do |page|
       match = page.match(/href=.*?mactex-(\d{4})(\d{2})(\d{2})\.pkg/)
-      "#{match[1]}-#{match[2]}-#{match[3]}"
+      "#{match[1]}.#{match[2]}#{match[3]}"
     end
   end
 
@@ -23,7 +23,7 @@ cask "mactex" do
   depends_on formula: "ghostscript"
   depends_on macos: ">= :high_sierra"
 
-  pkg "mactex-#{version.no_hyphens}.pkg",
+  pkg "mactex-#{version.no_dots}.pkg",
       choices: [
         {
           # Ghostscript
@@ -39,24 +39,24 @@ cask "mactex" do
         },
         {
           # GUI Applications
-          "choiceIdentifier" => "org.tug.mactex.gui#{version.split("-").first}",
+          "choiceIdentifier" => "org.tug.mactex.gui#{version.major}",
           "choiceAttribute"  => "selected",
           "attributeSetting" => 1,
         },
         {
           # TeXLive
-          "choiceIdentifier" => "org.tug.mactex.texlive#{version.split("-").first}",
+          "choiceIdentifier" => "org.tug.mactex.texlive#{version.major}",
           "choiceAttribute"  => "selected",
           "attributeSetting" => 1,
         },
       ]
 
   uninstall pkgutil: [
-    "org.tug.mactex.gui#{version.split("-").first}",
-    "org.tug.mactex.texlive#{version.split("-").first}",
+    "org.tug.mactex.gui#{version.major}",
+    "org.tug.mactex.texlive#{version.major}",
   ],
             delete:  [
-              "/usr/local/texlive/#{version.split("-").first}",
+              "/usr/local/texlive/#{version.major}",
               "/Applications/TeX",
               "/Library/TeX",
               "/etc/paths.d/TeX",
@@ -65,7 +65,7 @@ cask "mactex" do
 
   zap trash: [
     "/usr/local/texlive/texmf-local",
-    "~/Library/texlive/#{version.split("-").first}",
+    "~/Library/texlive/#{version.major}",
     # TexShop:
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/texshop.sfl*",
     "~/Library/Application Support/TeXShop",
