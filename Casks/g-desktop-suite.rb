d67250738b970777f5a4be38f3c6ff6647d3bb1b
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:31 +0100

    g-desktop-suite: add livecheck

--- Casks/g-desktop-suite.rb
+++ Casks/g-desktop-suite.rb
@@ -3,9 +3,13 @@ cask "g-desktop-suite" do
   sha256 "461c5d19149a088981f8f7a87148cda1762458b7c64d59e1ec5062106eb8ad90"
 
   url "https://github.com/alexkim205/g-desktop-suite/releases/latest/download/G-Desktop-Suite-#{version}.dmg"
-  appcast "https://github.com/alexkim205/g-desktop-suite/releases.atom"
   name "G Desktop Suite"
   homepage "https://github.com/alexkim205/g-desktop-suite"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "G Desktop Suite.app"
 end
