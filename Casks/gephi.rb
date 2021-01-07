df323915bf8db9a7f00927bd4c4f703ec642c7ca
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:32 +0100

    gephi: add livecheck

--- Casks/gephi.rb
+++ Casks/gephi.rb
@@ -4,10 +4,14 @@ cask "gephi" do
 
   url "https://github.com/gephi/gephi/releases/download/v#{version}/gephi-#{version}-macos.dmg",
       verified: "github.com/gephi/gephi/"
-  appcast "https://github.com/gephi/gephi/releases.atom"
   name "Gephi"
   desc "Utility to visualize and explore GraphViz graphs"
   homepage "https://gephi.org/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Gephi.app"
 end
