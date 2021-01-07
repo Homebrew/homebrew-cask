60a41b6ea1f534a15928582cd5acbcde0b1ab1e0
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:51 +0100

    sonic-pi: add livecheck

--- Casks/sonic-pi.rb
+++ Casks/sonic-pi.rb
@@ -3,10 +3,14 @@ cask "sonic-pi" do
   sha256 "52828a3132ed8657a30a64f995dfe72b32f0a3b718886c3ac7e6d41d846ab441"
 
   url "https://sonic-pi.net/files/releases/v#{version}/sonic-pi-for-mac-v#{version}.zip"
-  appcast "https://github.com/samaaron/sonic-pi/releases.atom"
   name "Sonic Pi"
   desc "Code-based music creation and performance tool"
   homepage "https://sonic-pi.net/"
+
+  livecheck do
+    url "https://github.com/samaaron/sonic-pi/releases"
+    strategy :git
+  end
 
   app "Sonic Pi.app"
 end
