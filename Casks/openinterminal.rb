cecdf399c816c06cd2f53372ba2e2e5377250543
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:24 +0100

    openinterminal: add livecheck

--- Casks/openinterminal.rb
+++ Casks/openinterminal.rb
@@ -3,9 +3,13 @@ cask "openinterminal" do
   sha256 "192a01d0d7978175f99ff438e2cc2db9b3959700d8ef4f8f71af21c42e323ac0"
 
   url "https://github.com/Ji4n1ng/OpenInTerminal/releases/download/#{version}/OpenInTerminal.app.zip"
-  appcast "https://github.com/Ji4n1ng/OpenInTerminal/releases.atom"
   name "OpenInTerminal"
   homepage "https://github.com/Ji4n1ng/OpenInTerminal"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   depends_on macos: ">= :sierra"
 
