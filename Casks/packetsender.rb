479eb013be10b19bdd8b1c329b63502bb907264f
Markus Reiter <me@reitermark.us>
2021-01-07 13:12:54 +0100

    packetsender: add livecheck

--- Casks/packetsender.rb
+++ Casks/packetsender.rb
@@ -6,17 +6,24 @@ cask "packetsender" do
     url "https://github.com/dannagle/PacketSender/releases/download/v#{version}/PacketSender_v#{version}.dmg",
         verified: "github.com/dannagle/PacketSender/"
   else
-    version "7.1.0,7.0.5"
+    version "7.0.5,7.1.0"
     sha256 "a2ab110a5d498389de5b0fd73ca7a63349f1b79235f4acda1da7c2e5322f0d7f"
 
-    url "https://github.com/dannagle/PacketSender/releases/download/v#{version.after_comma}/PacketSender_BigSur_v#{version.before_comma}.dmg",
+    url "https://github.com/dannagle/PacketSender/releases/download/v#{version.before_comma}/PacketSender_BigSur_v#{version.after_comma}.dmg",
         verified: "github.com/dannagle/PacketSender/"
   end
 
-  appcast "https://github.com/dannagle/PacketSender/releases.atom"
   name "Packet Sender"
   desc "Network utility for sending / receiving TCP, UDP, SSL"
   homepage "https://packetsender.com/"
+
+  livecheck do
+    url "https://github.com/dannagle/PacketSender/releases/latest"
+    strategy :page_match do |page|
+      match = page.match(%r{href=.*?/v?(\d+(?:\.\d+)*)/PacketSender_BigSur_v?(\d+(?:\.\d+)*)\.dmg}i)
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   depends_on macos: ">= :sierra"
 
