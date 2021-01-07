400182f62f01e3944d04a8c4ed7fa7d417be5428
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:03 +0100

    macintoshjs: add livecheck

--- Casks/macintoshjs.rb
+++ Casks/macintoshjs.rb
@@ -3,10 +3,14 @@ cask "macintoshjs" do
   sha256 "f1cf7e80c17f1ade18dcfde10d291d70f9e1c86762f42c1004dc7caa3717c2d0"
 
   url "https://github.com/felixrieseberg/macintosh.js/releases/download/v#{version}/macintosh.js-darwin-x64-#{version}.zip"
-  appcast "https://github.com/felixrieseberg/macintosh.js/releases.atom"
   name "macintosh.js"
   desc "Virtual Apple Macintosh with System 8, running in Electron"
   homepage "https://github.com/felixrieseberg/macintosh.js"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "macintosh.js.app"
 
