9a8f0a2064b82b865a31187f6da8e975078c1bd9
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:19 +0100

    noisebuddy: add livecheck

--- Casks/noisebuddy.rb
+++ Casks/noisebuddy.rb
@@ -3,9 +3,13 @@ cask "noisebuddy" do
   sha256 "e5497362319e111fb4a0c9ff9d479e21b75fda768a96ef3497c1fad7db2c7c38"
 
   url "https://github.com/insidegui/NoiseBuddy/releases/download/#{version}/NoiseBuddy_v#{version}.zip"
-  appcast "https://github.com/insidegui/NoiseBuddy/releases.atom"
   name "NoiseBuddy"
   homepage "https://github.com/insidegui/NoiseBuddy"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   depends_on macos: ">= :catalina"
 
