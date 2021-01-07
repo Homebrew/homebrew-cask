a7c22dfcfcd067d493b49181ea1ca6fb09924abb
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:24 +0100

    openineditor-lite: add livecheck

--- Casks/openineditor-lite.rb
+++ Casks/openineditor-lite.rb
@@ -3,9 +3,13 @@ cask "openineditor-lite" do
   sha256 "2b8eb742ca675cba26707192462598f52933cb33f9ba72850d075b81bdbe958c"
 
   url "https://github.com/Ji4n1ng/OpenInTerminal/releases/download/#{version}/OpenInEditor-Lite.app.zip"
-  appcast "https://github.com/Ji4n1ng/OpenInTerminal/releases.atom"
   name "OpenInEditor-Lite"
   homepage "https://github.com/Ji4n1ng/OpenInTerminal"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "OpenInEditor-Lite.app"
 end
