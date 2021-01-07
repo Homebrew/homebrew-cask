7f3eac8724f6a1b1508642e2895b682daaeca314
Markus Reiter <me@reitermark.us>
2021-01-07 14:44:57 +0100

    bob: add livecheck

--- Casks/bob.rb
+++ Casks/bob.rb
@@ -3,9 +3,13 @@ cask "bob" do
   sha256 "274472389a92603673b8780169857f7cb1db8659fa53e7e399c0d1a26f279e20"
 
   url "https://github.com/ripperhe/Bob/releases/download/v#{version}/Bob.zip"
-  appcast "https://github.com/ripperhe/Bob/releases.atom"
   name "Bob"
   homepage "https://github.com/ripperhe/Bob"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   depends_on macos: ">= :sierra"
 
