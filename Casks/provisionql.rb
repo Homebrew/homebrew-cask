12d0f604cd2bd7cf01ddc96d688aabd8c0857829
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:35 +0100

    provisionql: add livecheck

--- Casks/provisionql.rb
+++ Casks/provisionql.rb
@@ -3,9 +3,13 @@ cask "provisionql" do
   sha256 "762075acffde7c2aad376bbd946c4533ca81f214ef0e88eaab85d94551cc497a"
 
   url "https://github.com/ealeksandrov/ProvisionQL/releases/download/#{version}/ProvisionQL.zip"
-  appcast "https://github.com/ealeksandrov/ProvisionQL/releases.atom"
   name "ProvisionQL"
   homepage "https://github.com/ealeksandrov/ProvisionQL"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   qlplugin "ProvisionQL.qlgenerator"
 
