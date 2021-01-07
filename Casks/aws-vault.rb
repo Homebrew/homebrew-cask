dfd373b19f49e01a2ad5a63dd667e9a22aac2959
Markus Reiter <me@reitermark.us>
2021-01-07 14:44:50 +0100

    aws-vault: add livecheck

--- Casks/aws-vault.rb
+++ Casks/aws-vault.rb
@@ -3,9 +3,13 @@ cask "aws-vault" do
   sha256 "c6364250172b8fe0a8b5b90d9cbbb69283fe9b97a1e0ddf53468e85fd8af8bd3"
 
   url "https://github.com/99designs/aws-vault/releases/download/v#{version}/aws-vault-darwin-amd64.dmg"
-  appcast "https://github.com/99designs/aws-vault/releases.atom"
   name "aws-vault"
   homepage "https://github.com/99designs/aws-vault"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   binary "aws-vault"
 end
