5b22e4b388e04fe6793efe3a6e01a95d37530517
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:27 +0100

    outline-manager: add livecheck

--- Casks/outline-manager.rb
+++ Casks/outline-manager.rb
@@ -4,10 +4,14 @@ cask "outline-manager" do
 
   url "https://github.com/Jigsaw-Code/outline-server/releases/download/v#{version}/Outline-Manager.dmg",
       verified: "github.com/Jigsaw-Code/outline-server/"
-  appcast "https://github.com/Jigsaw-Code/outline-server/releases.atom"
   name "Outline Manager"
   desc "Tool to create and manage Outline servers, powered by Shadowsocks"
   homepage "https://www.getoutline.org/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Outline Manager.app"
 end
