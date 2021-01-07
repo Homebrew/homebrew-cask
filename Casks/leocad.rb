74d0e1a76801eb0b4d0298f8830a4c657aff3d20
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:58 +0100

    leocad: add livecheck

--- Casks/leocad.rb
+++ Casks/leocad.rb
@@ -3,10 +3,14 @@ cask "leocad" do
   sha256 "5f0a63b32b86d3ec50deb465e492189132fcd170594c0bef0937c1eead57899c"
 
   url "https://github.com/leozide/leocad/releases/download/v#{version}/LeoCAD-macOS-#{version}.dmg"
-  appcast "https://github.com/leozide/leocad/releases.atom"
   name "LeoCAD"
   desc "CAD program for creating virtual LEGO models"
   homepage "https://github.com/leozide/leocad"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   depends_on macos: ">= :sierra"
 
