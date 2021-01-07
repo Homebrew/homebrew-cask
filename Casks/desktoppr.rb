3bba466deea57951786be2b6c70661204641c1c8
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:16 +0100

    desktoppr: add livecheck

--- Casks/desktoppr.rb
+++ Casks/desktoppr.rb
@@ -3,10 +3,14 @@ cask "desktoppr" do
   sha256 "9a2ef033fd05b3b559af91f8401897ab6311f1a79d71581502c2fa0e9c52e664"
 
   url "https://github.com/scriptingosx/desktoppr/releases/download/v#{version}/desktoppr-#{version}.pkg"
-  appcast "https://github.com/scriptingosx/desktoppr/releases.atom"
   name "desktoppr"
   desc "Command-line tool to set the desktop picture"
   homepage "https://github.com/scriptingosx/desktoppr"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   pkg "desktoppr-#{version}.pkg"
 
