0e2bbdaf0a656c9ead2184754c71d7f35e99aa4c
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:02 +0100

    maccy: add livecheck

--- Casks/maccy.rb
+++ Casks/maccy.rb
@@ -4,10 +4,14 @@ cask "maccy" do
 
   url "https://github.com/p0deje/Maccy/releases/download/#{version}/Maccy.app.zip",
       verified: "github.com/p0deje/Maccy/"
-  appcast "https://github.com/p0deje/Maccy/releases.atom"
   name "Maccy"
   desc "Clipboard manager"
   homepage "https://maccy.app/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   auto_updates true
   depends_on macos: ">= :mojave"
