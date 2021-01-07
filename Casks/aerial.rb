5b824a31837ff716195b121d366dcca7d62d4d70
Markus Reiter <me@reitermark.us>
2021-01-07 14:44:39 +0100

    aerial: add livecheck

--- Casks/aerial.rb
+++ Casks/aerial.rb
@@ -3,9 +3,13 @@ cask "aerial" do
   sha256 "6858529edd298faa806e8c7acc224fdad232a0fc6c205d993c1e5caa2518a768"
 
   url "https://github.com/JohnCoates/Aerial/releases/download/v#{version}/Aerial.saver.zip"
-  appcast "https://github.com/JohnCoates/Aerial/releases.atom"
   name "Aerial Screensaver"
   homepage "https://github.com/JohnCoates/Aerial"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   screen_saver "Aerial.saver"
 
