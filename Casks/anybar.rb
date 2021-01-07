b0aa473d6a31e6c7343404029429023b801bd054
Markus Reiter <me@reitermark.us>
2021-01-07 14:44:44 +0100

    anybar: add livecheck

--- Casks/anybar.rb
+++ Casks/anybar.rb
@@ -3,10 +3,14 @@ cask "anybar" do
   sha256 "7362ac46347167335367b9039478aed98a1333b2898a03462f93f155b9d1b603"
 
   url "https://github.com/tonsky/AnyBar/releases/download/#{version}/AnyBar-#{version}.zip"
-  appcast "https://github.com/tonsky/AnyBar/releases.atom"
   name "AnyBar"
   desc "Menu bar status indicator"
   homepage "https://github.com/tonsky/AnyBar"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "AnyBar.app"
 end
