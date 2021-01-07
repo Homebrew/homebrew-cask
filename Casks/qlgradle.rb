49622f19c4561111650716f978f749547d31d026
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:37 +0100

    qlgradle: add livecheck

--- Casks/qlgradle.rb
+++ Casks/qlgradle.rb
@@ -3,9 +3,13 @@ cask "qlgradle" do
   sha256 "ea76846953ecfbd180d65167ec31cb7c316030f500a6669cd79857c03b951b63"
 
   url "https://github.com/Urucas/QLGradle/releases/download/#{version}/QLGradle.qlgenerator.zip"
-  appcast "https://github.com/Urucas/QLGradle/releases.atom"
   name "qlgradle"
   homepage "https://github.com/Urucas/QLGradle"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   qlplugin "QLGradle.qlgenerator"
 end
