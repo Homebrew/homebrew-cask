e1bebb0519a92e7f4ac1ef502c216860f6db045b
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:48 +0100

    servpane: add livecheck

--- Casks/servpane.rb
+++ Casks/servpane.rb
@@ -3,10 +3,14 @@ cask "servpane" do
   sha256 "07f37334bc723d4aefa81411e604d4a20306d89b1553500b8acc715ee503f314"
 
   url "https://github.com/aderyabin/ServPane/releases/download/v#{version}/ServPane-#{version}.dmg"
-  appcast "https://github.com/aderyabin/ServPane/releases.atom"
   name "ServPane"
   desc "Launchd menu bar app"
   homepage "https://github.com/aderyabin/ServPane"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "ServPane.app"
 end
