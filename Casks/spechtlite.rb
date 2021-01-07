2d4e097be003b5deedfc4e454d1bf9663c9201c7
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:52 +0100

    spechtlite: add livecheck

--- Casks/spechtlite.rb
+++ Casks/spechtlite.rb
@@ -3,10 +3,14 @@ cask "spechtlite" do
   sha256 "244e6daa406fa4e45451d0f08688d91770d25cb559485a76788568f47bac7101"
 
   url "https://github.com/zhuhaow/SpechtLite/releases/download/#{version}/SpechtLite.zip"
-  appcast "https://github.com/zhuhaow/SpechtLite/releases.atom"
   name "SpechtLite"
   desc "Rule-based proxy"
   homepage "https://github.com/zhuhaow/SpechtLite"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "SpechtLite.app"
 end
