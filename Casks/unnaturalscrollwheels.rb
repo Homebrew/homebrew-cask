b9de3fcd6639bf92908ff7fa568aea39353cab0e
Markus Reiter <me@reitermark.us>
2021-01-07 14:47:07 +0100

    unnaturalscrollwheels: add livecheck

--- Casks/unnaturalscrollwheels.rb
+++ Casks/unnaturalscrollwheels.rb
@@ -3,10 +3,14 @@ cask "unnaturalscrollwheels" do
   sha256 "87afed2eece015e1ebaa6857abcc2559e4dbbffda8b138ec2081366a0aa50441"
 
   url "https://github.com/ther0n/UnnaturalScrollWheels/releases/download/#{version}/UnnaturalScrollWheels-#{version}.dmg"
-  appcast "https://github.com/ther0n/UnnaturalScrollWheels/releases.atom"
   name "UnnaturalScrollWheels"
   desc "Tool to invert scroll direction for physical scroll wheels"
   homepage "https://github.com/ther0n/UnnaturalScrollWheels"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "UnnaturalScrollWheels.app"
 
