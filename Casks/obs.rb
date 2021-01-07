46b7fd148c0d665c8c8c4f0510db89383c411a52
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:22 +0100

    obs: add livecheck

--- Casks/obs.rb
+++ Casks/obs.rb
@@ -3,10 +3,14 @@ cask "obs" do
   sha256 "d3688312d8dd842ab0e31b7a829002a5c533a0e2d84bd4ac030feaeb1ecf7ae1"
 
   url "https://cdn-fastly.obsproject.com/downloads/obs-mac-#{version}.dmg"
-  appcast "https://github.com/obsproject/obs-studio/releases.atom"
   name "OBS"
   desc "Open-source software for live streaming and screen recording"
   homepage "https://obsproject.com/"
+
+  livecheck do
+    url "https://github.com/obsproject/obs-studio/releases"
+    strategy :git
+  end
 
   auto_updates true
 
