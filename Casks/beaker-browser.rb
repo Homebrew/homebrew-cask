a24d831bbef336ea78638ecfc4874e07563da765
Markus Reiter <me@reitermark.us>
2021-01-07 14:44:53 +0100

    beaker-browser: add livecheck

--- Casks/beaker-browser.rb
+++ Casks/beaker-browser.rb
@@ -4,10 +4,14 @@ cask "beaker-browser" do
 
   url "https://github.com/beakerbrowser/beaker/releases/download/#{version}/beaker-browser-#{version}.dmg",
       verified: "github.com/beakerbrowser/beaker/"
-  appcast "https://github.com/beakerbrowser/beaker/releases.atom"
   name "Beaker Browser"
   desc "Experimental peer-to-peer web browser"
   homepage "https://beakerbrowser.com/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   auto_updates true
 
