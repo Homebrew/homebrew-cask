1b37dd379e974438cc78f0629ce860fe2f162f4b
Markus Reiter <me@reitermark.us>
2021-01-07 14:47:10 +0100

    vscodium: add livecheck

--- Casks/vscodium.rb
+++ Casks/vscodium.rb
@@ -3,10 +3,14 @@ cask "vscodium" do
   sha256 "c0a44c9806766d70175880b3e0f2c2cd24875d5281864b1499171190e793f766"
 
   url "https://github.com/VSCodium/vscodium/releases/download/#{version}/VSCodium.x64.#{version}.dmg"
-  appcast "https://github.com/VSCodium/vscodium/releases.atom"
   name "VSCodium"
   desc "Binary releases of VS Code without MS branding/telemetry/licensing"
   homepage "https://github.com/VSCodium/vscodium"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   auto_updates true
   conflicts_with cask: "visual-studio-code"
