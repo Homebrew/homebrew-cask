876cdec0c24e39f0aded619bbedb1063e14745b3
Markus Reiter <me@reitermark.us>
2021-01-07 14:47:07 +0100

    universal-media-server: add livecheck

--- Casks/universal-media-server.rb
+++ Casks/universal-media-server.rb
@@ -4,10 +4,14 @@ cask "universal-media-server" do
 
   url "https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/#{version}/UMS-#{version}.dmg",
       verified: "github.com/UniversalMediaServer/UniversalMediaServer/"
-  appcast "https://github.com/UniversalMediaServer/UniversalMediaServer/releases.atom"
   name "Universal Media Server"
   desc "Media server supporting DLNA, UPnP and HTTP(S)"
   homepage "https://www.universalmediaserver.com/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "Universal Media Server.app"
 
