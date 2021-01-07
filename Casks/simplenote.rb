ebf3d427f844c202cebb67facc534b5986e0397f
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:50 +0100

    simplenote: add livecheck

--- Casks/simplenote.rb
+++ Casks/simplenote.rb
@@ -3,9 +3,13 @@ cask "simplenote" do
   sha256 "57d0d3b1ee89497dad2005ab01ce3094c3bb771074b9def3e21f4960ae88f6a5"
 
   url "https://github.com/Automattic/simplenote-electron/releases/download/v#{version}/Simplenote-macOS-#{version}.dmg"
-  appcast "https://github.com/Automattic/simplenote-electron/releases.atom"
   name "Simplenote"
   homepage "https://github.com/Automattic/simplenote-electron"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "Simplenote.app"
 
