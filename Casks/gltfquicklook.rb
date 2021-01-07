afebdde416e8125c1d560d4cf29a1ad175c27b62
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:35 +0100

    gltfquicklook: add livecheck

--- Casks/gltfquicklook.rb
+++ Casks/gltfquicklook.rb
@@ -3,9 +3,13 @@ cask "gltfquicklook" do
   sha256 "be9d7d65572ba50fbe71e1318b24f10bbba00bba9a9ca77a6d9219cac4b70b15"
 
   url "https://github.com/magicien/GLTFQuickLook/releases/download/v#{version}/GLTFQuickLook_v#{version}.zip"
-  appcast "https://github.com/magicien/GLTFQuickLook/releases.atom"
   name "GLTFQuickLook"
   homepage "https://github.com/magicien/GLTFQuickLook"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   qlplugin "GLTFQuickLook_v#{version}/GLTFQuickLook.qlgenerator"
 end
