6452eb696b7f97de17a885737e108f350443855e
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:16 +0100

    developerexcuses: add livecheck

--- Casks/developerexcuses.rb
+++ Casks/developerexcuses.rb
@@ -3,9 +3,13 @@ cask "developerexcuses" do
   sha256 "e1fe0105be02de196bbae04051abd506bfa2ac7a2c3ba5f9922c70700a2d61b3"
 
   url "https://github.com/kimar/DeveloperExcuses/releases/download/#{version}/DeveloperExcuses.saver.zip"
-  appcast "https://github.com/kimar/DeveloperExcuses/releases.atom"
   name "Developer Excuses Screensaver"
   homepage "https://github.com/kimar/DeveloperExcuses"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   screen_saver "DeveloperExcuses.saver"
 end
