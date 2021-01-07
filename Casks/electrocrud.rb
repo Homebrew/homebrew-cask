92f349dd33c1f95c5b15a40e2cdba3a5044f036d
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:21 +0100

    electrocrud: add livecheck

--- Casks/electrocrud.rb
+++ Casks/electrocrud.rb
@@ -3,10 +3,14 @@ cask "electrocrud" do
   sha256 "b5f758c354b1c4e0d0d5434e8c6856629a85e1bf05121467e913ce3d8485be2e"
 
   url "https://github.com/garrylachman/ElectroCRUD/releases/download/#{version}/ElectroCRUD-#{version}.dmg"
-  appcast "https://github.com/garrylachman/ElectroCRUD/releases.atom"
   name "ElectroCRUD"
   desc "Database CRUD application"
   homepage "https://github.com/garrylachman/ElectroCRUD"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "ElectroCRUD.app"
 end
