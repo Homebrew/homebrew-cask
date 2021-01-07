0d57f4c752590b6503b084c349e93478d0e1e2ce
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:54 +0100

    stats: add livecheck

--- Casks/stats.rb
+++ Casks/stats.rb
@@ -3,10 +3,14 @@ cask "stats" do
   sha256 "8c37f2ce387fe78a313b2dc7d9e77b3ec9044436d56f4320a0dce53506bf3abb"
 
   url "https://github.com/exelban/stats/releases/download/v#{version}/Stats.dmg"
-  appcast "https://github.com/exelban/stats/releases.atom"
   name "Stats"
   desc "System monitor for the menu bar"
   homepage "https://github.com/exelban/stats"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   auto_updates true
   depends_on macos: ">= :mojave"
