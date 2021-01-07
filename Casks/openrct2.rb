0dfa67fdc1dbecafe35afd29161b257f75cf7d57
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:25 +0100

    openrct2: add livecheck

--- Casks/openrct2.rb
+++ Casks/openrct2.rb
@@ -4,10 +4,14 @@ cask "openrct2" do
 
   url "https://github.com/OpenRCT2/OpenRCT2/releases/download/v#{version}/OpenRCT2-#{version}-macos-x86-64.zip",
       verified: "github.com/OpenRCT2/OpenRCT2/"
-  appcast "https://github.com/OpenRCT2/OpenRCT2/releases.atom"
   name "OpenRCT2"
   desc "Open-source re-implementation of RollerCoaster Tycoon 2"
   homepage "https://openrct2.io/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   depends_on macos: ">= :high_sierra"
 
