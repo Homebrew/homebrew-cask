8157f5731e78daaaaf5f057d89d067daab7e029e
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:02 +0100

    caprine: add livecheck

--- Casks/caprine.rb
+++ Casks/caprine.rb
@@ -3,10 +3,14 @@ cask "caprine" do
   sha256 "d1a768165d8bf74a5f1a9bda5d636d343c3eee329fdd731b69bd4dcf70788380"
 
   url "https://github.com/sindresorhus/caprine/releases/download/v#{version}/Caprine-#{version}.dmg"
-  appcast "https://github.com/sindresorhus/caprine/releases.atom"
   name "Caprine"
   desc "Elegant Facebook Messenger desktop app"
   homepage "https://github.com/sindresorhus/caprine"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   auto_updates true
 
