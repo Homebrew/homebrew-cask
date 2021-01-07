dea3795ad750000372d3861a125ed2c7dd8df46b
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:01 +0100

    cantata: add livecheck

--- Casks/cantata.rb
+++ Casks/cantata.rb
@@ -3,10 +3,14 @@ cask "cantata" do
   sha256 "c9eb8a1102d0a68cafc93f22df73445b8f69706f3322285f9a2f623a28df0176"
 
   url "https://github.com/CDrummond/cantata/releases/download/v#{version}/Cantata-#{version}.dmg"
-  appcast "https://github.com/CDrummond/cantata/releases.atom"
   name "Cantata"
   desc "Qt5 Graphical MPD Client"
   homepage "https://github.com/cdrummond/cantata"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   depends_on macos: ">= :sierra"
 
