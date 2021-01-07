ba0db64351cdb4c911aa22e25a750f73c3e4eaab
Jeffrey C <spaz926@me.com>
2021-01-06 21:28:49 -0600

    Changed case-when to else-elsif for MacOS.version

--- Casks/strawberry.rb
+++ Casks/strawberry.rb
@@ -1,12 +1,11 @@
 cask "strawberry" do
   version "0.8.5"
 
-  case MacOS.version
-  when :mojave
+  if MacOS.version <= :mojave
     sha256 "05351f24c29eb4f5a5cee9da1272e4c034e2d9277091aa1f9cb40d7f655c8fba"
     url "https://github.com/strawberrymusicplayer/strawberry/releases/download/#{version}/strawberry-#{version}-mojave.dmg",
         verified: "github.com/strawberrymusicplayer/strawberry/"
-  when :catalina
+  elsif MacOS.version <= :catalina
     sha256 "abadc6ebb4c6431e5c6b127ec4509b56a2918fe641adcf0b96c90c4fb94e184e"
     url "https://github.com/strawberrymusicplayer/strawberry/releases/download/#{version}/strawberry-#{version}-catalina.dmg",
         verified: "github.com/strawberrymusicplayer/strawberry/"
