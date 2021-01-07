f7e9110835f80c9842f511af1aa84c0050abc203
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:08 +0100

    cncjs: add livecheck

--- Casks/cncjs.rb
+++ Casks/cncjs.rb
@@ -4,9 +4,13 @@ cask "cncjs" do
 
   url "https://github.com/cncjs/cncjs/releases/download/v#{version}/cncjs-app-#{version}-mac-x64.dmg",
       verified: "github.com/cncjs/cncjs/"
-  appcast "https://github.com/cncjs/cncjs/releases.atom"
   name "CNSjs"
   homepage "https://cnc.js.org/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "CNCjs.app"
 end
