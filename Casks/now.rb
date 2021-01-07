b3b22cece211f3271a6974cfdf9c153d30856efe
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:20 +0100

    now: add livecheck

--- Casks/now.rb
+++ Casks/now.rb
@@ -4,10 +4,14 @@ cask "now" do
 
   url "https://github.com/zeit/now-desktop/releases/download/#{version}/Now-#{version}-mac.zip",
       verified: "github.com/zeit/now-desktop/"
-  appcast "https://github.com/zeit/now-desktop/releases.atom"
   name "Now"
   desc "Create deployments right from the tray menu"
   homepage "https://zeit.co/now"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Now.app"
 
