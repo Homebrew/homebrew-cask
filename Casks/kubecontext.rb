4114774c748296e55dfcc52aae325ef19e2a8dec
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:55 +0100

    kubecontext: add livecheck

--- Casks/kubecontext.rb
+++ Casks/kubecontext.rb
@@ -3,10 +3,14 @@ cask "kubecontext" do
   sha256 "b99db399fb038f87ac054f2d0741137de2a9b343f01f32a1db1b3f6dc313d882"
 
   url "https://github.com/turkenh/KubeContext/releases/download/v#{version}/KubeContext.zip"
-  appcast "https://github.com/turkenh/KubeContext/releases.atom"
   name "KubeContext"
   desc "Menu bar app for managing Kubernetes contexts"
   homepage "https://github.com/turkenh/KubeContext"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "KubeContext.app"
 end
