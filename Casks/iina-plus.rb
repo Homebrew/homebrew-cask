4f99d86ba7fa20f20c18b9b44111ae5cf7060b3c
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:45 +0100

    iina-plus: add livecheck

--- Casks/iina-plus.rb
+++ Casks/iina-plus.rb
@@ -3,10 +3,19 @@ cask "iina-plus" do
   sha256 "9d886b869dde56abfd91100536aed1d29ead029201d1d97ebca59562b7fdbacc"
 
   url "https://github.com/xjbeta/iina-plus/releases/download/#{version.before_comma}(#{version.after_comma})/iina+.#{version.before_comma}.dmg"
-  appcast "https://github.com/xjbeta/iina-plus/releases.atom"
   name "IINA+"
   desc "Extra danmaku support for iina. (iina 弹幕支持"
   homepage "https://github.com/xjbeta/iina-plus"
+
+  livecheck do
+    url :url
+    strategy :git do |tags|
+      tags.map do |tag|
+        match = tag.match(/(\d+(?:\.\d+)*)\((\d+)\)/i)
+        "#{match[1]},#{match[2]}" if match
+      end.compact
+    end
+  end
 
   app "iina+.app"
 end
