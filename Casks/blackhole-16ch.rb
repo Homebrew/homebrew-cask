1d831c3f1926a8adc08912fef0aee0ad88e358ec
Markus Reiter <me@reitermark.us>
2021-01-07 14:44:55 +0100

    blackhole-16ch: add livecheck

--- Casks/blackhole-16ch.rb
+++ Casks/blackhole-16ch.rb
@@ -3,10 +3,14 @@ cask "blackhole-16ch" do
   sha256 "5c94413bfc261b589e7d2b38527274b34cc1206ec826252a947ad3850e05e0ad"
 
   url "https://existential.audio/downloads/BlackHole16ch.v#{version}.pkg"
-  appcast "https://github.com/ExistentialAudio/BlackHole/releases.atom"
   name "BlackHole 16ch"
   desc "Virtual Audio Driver"
   homepage "https://existential.audio/blackhole/"
+
+  livecheck do
+    url "https://github.com/ExistentialAudio/BlackHole/releases"
+    strategy :git
+  end
 
   pkg "BlackHole16ch.v#{version}.pkg"
 
