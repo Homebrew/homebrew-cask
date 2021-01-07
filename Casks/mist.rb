aa04cad9280b90eda629b809b144237816317a5f
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:10 +0100

    mist: add livecheck

--- Casks/mist.rb
+++ Casks/mist.rb
@@ -3,10 +3,14 @@ cask "mist" do
   sha256 "2d18b86667c4daf690cfda327550d428f7efd09b6057b73456a71d2990c7efd1"
 
   url "https://github.com/ethereum/mist/releases/download/v#{version}/Mist-macosx-#{version.dots_to_hyphens}.dmg"
-  appcast "https://github.com/ethereum/mist/releases.atom"
   name "Mist"
   desc "Browser for Ðapps on the Ethereum network"
   homepage "https://github.com/ethereum/mist"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Mist.app"
 end
