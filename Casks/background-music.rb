b6fcd7425c5ade75a6809ba4381d7b669c04d5bd
Markus Reiter <me@reitermark.us>
2021-01-07 14:44:51 +0100

    background-music: add livecheck

--- Casks/background-music.rb
+++ Casks/background-music.rb
@@ -3,9 +3,13 @@ cask "background-music" do
   sha256 "0cd7b488b5ab97a1ecb496e484a6c209c29f35ab503e6f73b45e56719a7aba18"
 
   url "https://github.com/kyleneideck/BackgroundMusic/releases/download/v#{version}/BackgroundMusic-#{version}.pkg"
-  appcast "https://github.com/kyleneideck/BackgroundMusic/releases.atom"
   name "Background Music"
   homepage "https://github.com/kyleneideck/BackgroundMusic"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   depends_on macos: ">= :yosemite"
 
