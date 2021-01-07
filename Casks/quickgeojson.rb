11d43e491b5e260ab31cc5987eecb87650a3ad7d
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:40 +0100

    quickgeojson: add livecheck

--- Casks/quickgeojson.rb
+++ Casks/quickgeojson.rb
@@ -3,9 +3,13 @@ cask "quickgeojson" do
   sha256 "48a30865f3183c086c9ac70003276e2356c2fd4468e8818b0b2ec5adb3366332"
 
   url "https://github.com/irees/quickgeojson/releases/download/v#{version}/quickgeojson.qlgenerator.zip"
-  appcast "https://github.com/irees/quickgeojson/releases.atom"
   name "quickgeojson"
   homepage "https://github.com/irees/quickgeojson"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   qlplugin "quickgeojson.qlgenerator"
 end
