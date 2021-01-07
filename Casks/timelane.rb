65d46bcce0ee21f8a433e11f90946c6fa34a8c10
Markus Reiter <me@reitermark.us>
2021-01-07 14:47:02 +0100

    timelane: add livecheck

--- Casks/timelane.rb
+++ Casks/timelane.rb
@@ -4,9 +4,13 @@ cask "timelane" do
 
   url "https://github.com/icanzilb/Timelane/releases/download/#{version}/Timelane.app-#{version}.zip",
       verified: "github.com/icanzilb/Timelane/"
-  appcast "https://github.com/icanzilb/Timelane/releases.atom"
   name "Timelane"
   homepage "http://timelane.tools/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   depends_on macos: ">= :mojave"
 
