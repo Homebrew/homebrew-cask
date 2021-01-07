ab8844b651585b325cb53adfa73828322bd81f27
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:31 +0100

    fvim: add livecheck

--- Casks/fvim.rb
+++ Casks/fvim.rb
@@ -3,10 +3,19 @@ cask "fvim" do
   sha256 "6e5ef4c8f9b04dd65e872eef2b3e7b4444fc1f1e797aeea5bae0062b66d5dda7"
 
   url "https://github.com/yatli/fvim/releases/download/v#{version.before_comma}%2B#{version.after_comma}/fvim-osx.zip"
-  appcast "https://github.com/yatli/fvim/releases.atom"
   name "FVim"
   desc "GUI for the Neovim text editor"
   homepage "https://github.com/yatli/fvim"
+
+  livecheck do
+    url :url
+    strategy :git do |tags|
+      tags.map do |tag|
+        match = tag.match(/^v?(\d+(?:\.\d+)*)\+([0-9a-f]+)$/)
+        "#{match[1]},#{match[2]}" if match
+      end.compact
+    end
+  end
 
   app "FVim.app"
 
