4bf0f5195f37fbae61becf12d0f9c77e59e526e8
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:16 +0100

    decrediton: add livecheck

--- Casks/decrediton.rb
+++ Casks/decrediton.rb
@@ -3,9 +3,13 @@ cask "decrediton" do
   sha256 "3341b0b09bf100840643daf5b583cd61ee8981b7869a5190cb4c106cacba8b7e"
 
   url "https://github.com/decred/decred-binaries/releases/download/v#{version}/decrediton-v#{version}.dmg"
-  appcast "https://github.com/decred/decred-binaries/releases.atom"
   name "Decrediton"
   homepage "https://github.com/decred/decrediton"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "decrediton.app"
 end
