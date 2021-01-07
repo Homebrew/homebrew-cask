af2325f54dbceae714418be59adc59885dfdc394
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:19 +0100

    nightfall: add livecheck

--- Casks/nightfall.rb
+++ Casks/nightfall.rb
@@ -3,10 +3,14 @@ cask "nightfall" do
   sha256 "ad6242cbf9477a34b19d38f9c39dbc354dd3eca75c6585837757503bc5302b22"
 
   url "https://github.com/r-thomson/Nightfall/releases/download/v#{version}/Nightfall.dmg"
-  appcast "https://github.com/r-thomson/Nightfall/releases.atom"
   name "Nightfall"
   desc "Menu bar utility for toggling dark mode"
   homepage "https://github.com/r-thomson/Nightfall/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   depends_on macos: ">= :catalina"
 
