67ddb338d35d61139a2c437516c5009ba16e8e88
Markus Reiter <me@reitermark.us>
2021-01-07 14:44:40 +0100

    ajour: add livecheck

--- Casks/ajour.rb
+++ Casks/ajour.rb
@@ -3,10 +3,14 @@ cask "ajour" do
   sha256 "75e438111075b553fdcc5e0ec7d7349360a4ce7d4187e82866d5424dbb37e14a"
 
   url "https://github.com/casperstorm/ajour/releases/download/#{version}/ajour.dmg"
-  appcast "https://github.com/casperstorm/ajour/releases.atom"
   name "Ajour"
   desc "World of Warcraft addon manager"
   homepage "https://github.com/casperstorm/ajour"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Ajour.app"
 
