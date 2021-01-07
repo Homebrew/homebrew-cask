60edd0fff1679a74528d6740c2bef77e1cc1bf5f
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:21 +0100

    edex-ui: add livecheck

--- Casks/edex-ui.rb
+++ Casks/edex-ui.rb
@@ -3,9 +3,13 @@ cask "edex-ui" do
   sha256 "471def5c79a7620f9966e71a7026bcc8ba543ca5747bb4a7436e8e45c2a7cd74"
 
   url "https://github.com/GitSquared/edex-ui/releases/download/v#{version}/eDEX-UI-macOS.dmg"
-  appcast "https://github.com/GitSquared/edex-ui/releases.atom"
   name "eDEX-UI"
   homepage "https://github.com/GitSquared/edex-ui"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "eDEX-UI.app"
 end
