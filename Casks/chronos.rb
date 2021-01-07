8722437e800b28a2179bb76f7602fe5552f8ee48
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:04 +0100

    chronos: add livecheck

--- Casks/chronos.rb
+++ Casks/chronos.rb
@@ -3,9 +3,13 @@ cask "chronos" do
   sha256 "3130fcd1b0f62d88aa9f3f3b11f82ed359418377a0240b321c4975254285fbd4"
 
   url "https://github.com/web-pal/chronos-timetracker/releases/download/v#{version}/Chronos-#{version}-mac.zip"
-  appcast "https://github.com/web-pal/chronos-timetracker/releases.atom"
   name "Chronos Timetracker"
   homepage "https://github.com/web-pal/chronos-timetracker"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Chronos.app"
 end
