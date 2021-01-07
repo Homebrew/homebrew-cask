7c6f73aca39633cbae45c4d9e5abcc0648f6fe4c
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:22 +0100

    onionshare: add livecheck

--- Casks/onionshare.rb
+++ Casks/onionshare.rb
@@ -3,10 +3,14 @@ cask "onionshare" do
   sha256 "c9fc5a3634ac4a8d8d27e20f6d7c899962cd25c3d40522a544c9a1835ee66651"
 
   url "https://onionshare.org/dist/#{version}/OnionShare-#{version}.pkg"
-  appcast "https://github.com/micahflee/onionshare/releases.atom"
   name "OnionShare"
   desc "Securely and anonymously send and receive files"
   homepage "https://onionshare.org/"
+
+  livecheck do
+    url "https://github.com/micahflee/onionshare/releases"
+    strategy :github_latest
+  end
 
   pkg "OnionShare-#{version}.pkg"
 
