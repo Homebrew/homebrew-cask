a53313abe8edaa737f267ef5242e1efc3cf20946
Markus Reiter <me@reitermark.us>
2021-01-07 14:47:01 +0100

    tiddly: add livecheck

--- Casks/tiddly.rb
+++ Casks/tiddly.rb
@@ -3,9 +3,13 @@ cask "tiddly" do
   sha256 "5b311671ae872e34bc96b4ac96d69ce96b9910e38af9d4722cf01f192ab5e6b2"
 
   url "https://github.com/Jermolene/TiddlyDesktop/releases/download/v#{version}/tiddlydesktop-mac64-v#{version}.zip"
-  appcast "https://github.com/Jermolene/TiddlyDesktop/releases.atom"
   name "TiddlyWiki"
   homepage "https://github.com/Jermolene/TiddlyDesktop"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "TiddlyDesktop-mac64-v#{version}/TiddlyDesktop.app"
 end
