dff97d070a045d3b3889880688848e17aed0efea
Markus Reiter <me@reitermark.us>
2021-01-07 14:44:59 +0100

    brightness-sync: add livecheck

--- Casks/brightness-sync.rb
+++ Casks/brightness-sync.rb
@@ -3,10 +3,14 @@ cask "brightness-sync" do
   sha256 "b194bc3d3dbb6f05d855c293497019aa7f90bc8c6f794d0296d50e109470344e"
 
   url "https://github.com/OCJvanDijk/Brightness-Sync/releases/download/v#{version}/Brightness.Sync.app.zip"
-  appcast "https://github.com/OCJvanDijk/Brightness-Sync/releases.atom"
   name "Brightness Sync"
   desc "Utility to synchronize the brightness of LG UltraFine display(s)"
   homepage "https://github.com/OCJvanDijk/Brightness-Sync"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   depends_on macos: ">= :catalina"
 
