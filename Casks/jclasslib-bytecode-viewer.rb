63aa01a5703dce7d6f218fd25c534ea801dfe5f3
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:48 +0100

    jclasslib-bytecode-viewer: add livecheck

--- Casks/jclasslib-bytecode-viewer.rb
+++ Casks/jclasslib-bytecode-viewer.rb
@@ -3,9 +3,13 @@ cask "jclasslib-bytecode-viewer" do
   sha256 "f742ea0994d9f3292d0727d2360aa744f583d9d7daea80043c30a6e64ea3f002"
 
   url "https://github.com/ingokegel/jclasslib/releases/download/#{version}/jclasslib_macos_#{version.dots_to_underscores}.dmg"
-  appcast "https://github.com/ingokegel/jclasslib/releases.atom"
   name "jclasslib bytecode viewer"
   homepage "https://github.com/ingokegel/jclasslib"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   auto_updates true
 
