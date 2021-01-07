136e42df8c8c454b7f1b8bac6bd970987f36f61b
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:16 +0100

    musaicfm: add livecheck

--- Casks/musaicfm.rb
+++ Casks/musaicfm.rb
@@ -3,9 +3,13 @@ cask "musaicfm" do
   sha256 "52ad4091e48aa3a22504ed914191722c998d11bd4d56ea3a255434863ae045fa"
 
   url "https://github.com/docterd/MusaicFM/releases/download/#{version}/MusaicFM.saver.zip"
-  appcast "https://github.com/docterd/MusaicFM/releases.atom"
   name "MusaicFM Screensaver"
   homepage "https://github.com/docterd/MusaicFM"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   screen_saver "MusaicFM.saver"
 end
