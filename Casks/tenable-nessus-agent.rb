49bdc309065380be5d4d929bd95ee0fd071506b7
Markus Reiter <me@reitermark.us>
2021-01-07 21:28:58 +0100

    Update tenable-nessus-agent.rb

--- Casks/tenable-nessus-agent.rb
+++ Casks/tenable-nessus-agent.rb
@@ -2,20 +2,20 @@ cask "tenable-nessus-agent" do
   version "8.13.1,12203"
   sha256 "9c3e7e0360c0cfeeba4a49108369dd626cb76269bca4d00eb5179f63a0c7d93b"
 
-  url "https://www.tenable.com/downloads/api/v1/public/pages/nessus/downloads/#{version.after_comma}/download?i_agree_to_tenable_license_agreement=true"
+  url "https://www.tenable.com/downloads/api/v1/public/pages/nessus-agents/downloads/#{version.after_comma}/download?i_agree_to_tenable_license_agreement=true"
   name "Tenable Nessus Agent"
   desc "Agent for Nessus vulnerability scanner"
   homepage "https://www.tenable.com/"
 
   livecheck do
-    url "https://www.tenable.com/downloads/nessus?loginAttempted=true"
+    url "https://www.tenable.com/downloads/nessus-agents?loginAttempted=true"
     strategy :page_match do |page|
       match = page.match(/"id"\s*:\s*(\d+)\s*,\s*"file"\s*:\s*"Nessus-(\d+(?:\.\d+)*).dmg"/)
       "#{match[2]},#{match[1]}"
     end
   end
 
-  pkg "Install Nessus.pkg"
+  pkg "Install Nessus Agent.pkg"
 
   uninstall pkgutil:   "com.tenablesecurity.NessusAgent.Preferences",
             delete:    [
