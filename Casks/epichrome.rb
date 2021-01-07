8e3a43c3aea3f7bbfa5edd56cb43dd291dc2666b
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:24 +0100

    epichrome: add livecheck

--- Casks/epichrome.rb
+++ Casks/epichrome.rb
@@ -3,10 +3,14 @@ cask "epichrome" do
   sha256 "c987272abb89703c37ae8edbc830871668ef8c4ff8fe27e163d7ccf357eb0547"
 
   url "https://github.com/dmarmor/epichrome/releases/download/v#{version}/epichrome-#{version}.pkg"
-  appcast "https://github.com/dmarmor/epichrome/releases.atom"
   name "Epichrome"
   desc "Tool to create web-based applications that work like standalone apps"
   homepage "https://github.com/dmarmor/epichrome"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   pkg "epichrome-#{version}.pkg"
 
