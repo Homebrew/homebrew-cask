dcdb4395f2d8bbca39792fcf3841b06853a34563
Zhustec <zhustec@foxmail.com>
2021-01-07 09:46:45 +0800

    Add missing file in zap for kindle (#97240)
    * Add zap for kindle
    
    Signed-off-by: zhustec <zhustec@foxmail.com>
    
    * Update kindle.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/kindle.rb
+++ Casks/kindle.rb
@@ -15,10 +15,14 @@ cask "kindle" do
 
   app "Kindle.app"
 
+  uninstall delete: "/Library/Logs/DiagnosticReports/Kindle_*.diag"
+
   zap trash: [
+    "~/Library/Application Support/Kindle",
+    "~/Library/Caches/com.amazon.Kindle",
+    "~/Library/Caches/com.amazon.Kindle-Crash-Reporter",
+    "~/Library/HTTPStorages/com.amazon.Kindle.binarycookies",
     "~/Library/Preferences/com.amazon.Kindle.plist",
-    "~/Library/Application Support/Kindle/",
-    "~/Library/Saved Application State/com.amazon.Kindle.savedState/",
-    "~/Library/Caches/com.amazon.Kindle-Crash-Reporter/",
+    "~/Library/Saved Application State/com.amazon.Kindle.savedState",
   ]
 end
