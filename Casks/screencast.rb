9f3fbcfb337ec604e6eb53b5145295dad9347af6
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:47 +0100

    screencast: add livecheck

--- Casks/screencast.rb
+++ Casks/screencast.rb
@@ -3,9 +3,13 @@ cask "screencast" do
   sha256 "0fb23d34ee3c94eb47233b42d2ac93de84d14dcdafe1e7205ce0a4341ee812f0"
 
   url "https://github.com/soh335/Screencast/releases/download/#{version}/Screencast.dmg"
-  appcast "https://github.com/soh335/Screencast/releases.atom"
   name "Screencast"
   homepage "https://github.com/soh335/Screencast"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Screencast.app"
 end
