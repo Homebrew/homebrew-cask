a1545ed48e60d13e77c2e922c3d5219af8ede403
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:35 +0100

    proxyman: add livecheck

--- Casks/proxyman.rb
+++ Casks/proxyman.rb
@@ -4,10 +4,14 @@ cask "proxyman" do
 
   url "https://github.com/ProxymanApp/Proxyman/releases/download/#{version}/Proxyman_#{version}.dmg",
       verified: "github.com/ProxymanApp/Proxyman/"
-  appcast "https://github.com/ProxymanApp/Proxyman/releases.atom"
   name "Proxyman"
   desc "Modern and intuitive HTTP Debugging Proxy app"
   homepage "https://proxyman.io/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   auto_updates true
 
