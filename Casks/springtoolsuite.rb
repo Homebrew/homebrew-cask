46a080ad7157a47a1dcbbd528cd8d8dd14ff1918
Markus Reiter <me@reitermark.us>
2021-01-07 16:08:05 +0100

    springtoolsuite: add livecheck (#97415)

--- Casks/springtoolsuite.rb
+++ Casks/springtoolsuite.rb
@@ -1,13 +1,22 @@
 cask "springtoolsuite" do
-  version "4.9.0.RELEASE,4.18.0"
+  version "4.9.0,4.18.0"
   sha256 "56e14f18bfc6eca84403326db3465abd4ca91ae64d21754e3700639cbcf2320c"
 
-  url "https://download.springsource.com/release/STS#{version.major}/#{version.before_comma}/dist/e#{version.after_comma.major_minor}/spring-tool-suite-#{version.major}-#{version.before_comma}-e#{version.after_comma}-macosx.cocoa.x86_64.dmg",
+  url "https://download.springsource.com/release/STS#{version.major}/#{version.before_comma}.RELEASE/dist/e#{version.after_comma.major_minor}/spring-tool-suite-#{version.major}-#{version.before_comma}.RELEASE-e#{version.after_comma}-macosx.cocoa.x86_64.dmg",
       verified: "download.springsource.com/release/"
-  appcast "https://github.com/spring-projects/sts4/releases.atom"
   name "Spring Tool Suite"
   desc "Next generation tooling for Spring Boot"
   homepage "https://spring.io/tools"
+
+  livecheck do
+    url :homepage
+    strategy :page_match do |page|
+      match = page.match(
+        %r{href=.*?/spring-tool-suite-\d+-(\d+(?:\.\d+)*)\.RELEASE-e(\d+(?:\.\d+)*)-macosx\.cocoa\.x86_64\.dmg}i,
+      )
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   auto_updates true
 
