a6878ff2b290a39a3d5128881723d303ff00a586
Markus Reiter <me@reitermark.us>
2021-01-07 14:44:56 +0100

    bloodhound: add livecheck

--- Casks/bloodhound.rb
+++ Casks/bloodhound.rb
@@ -3,10 +3,14 @@ cask "bloodhound" do
   sha256 "ae2f9496b86befa082d8d2c4b714c1ace7399401356e1d9502b3a498d92f2bbc"
 
   url "https://github.com/BloodHoundAD/BloodHound/releases/download/#{version}/BloodHound-darwin-x64.zip"
-  appcast "https://github.com/BloodHoundAD/BloodHound/releases.atom"
   name "bloodhound"
   desc "Six Degrees of Domain Admin"
   homepage "https://github.com/BloodHoundAD/BloodHound"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "BloodHound-darwin-x64/BloodHound.app"
 end
