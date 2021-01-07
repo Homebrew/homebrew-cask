d6187f1fb1421435c623dbe43a8a2669ad3593cb
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:11 +0100

    mob: add livecheck

--- Casks/mob.rb
+++ Casks/mob.rb
@@ -3,9 +3,13 @@ cask "mob" do
   sha256 "a70d66d92310737d9599215d558670a45265795be0be980934a91e9880eb4a73"
 
   url "https://github.com/zenghongtu/Mob/releases/download/v#{version}/Mob-#{version}-mac.dmg"
-  appcast "https://github.com/zenghongtu/Mob/releases.atom"
   name "Mob"
   homepage "https://github.com/zenghongtu/Mob"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Mob.app"
 
