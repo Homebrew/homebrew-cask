e1101d7c7c6d7ede551528355532e94c0d1da513
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:45 +0100

    inav-configurator: add livecheck

--- Casks/inav-configurator.rb
+++ Casks/inav-configurator.rb
@@ -3,10 +3,14 @@ cask "inav-configurator" do
   sha256 "4dffd1159b9865d914ccfb5688104fc980a4ebb841ad647f18cd90f1dcf3d22a"
 
   url "https://github.com/iNavFlight/inav-configurator/releases/download/#{version}/INAV-Configurator_macOS_#{version}.zip"
-  appcast "https://github.com/iNavFlight/inav-configurator/releases.atom"
   name "INAV Configurator"
   desc "Configuration tool for the INAV flight control system"
   homepage "https://github.com/iNavFlight/inav-configurator/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "INAV Configurator.app"
 end
