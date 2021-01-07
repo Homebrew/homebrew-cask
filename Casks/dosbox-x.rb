20681fbb69bae16219f23d773b840b3530cb0f9e
Markus Reiter <me@reitermark.us>
2021-01-08 00:00:44 +0100

    Update dosbox-x.rb

--- Casks/dosbox-x.rb
+++ Casks/dosbox-x.rb
@@ -1,5 +1,5 @@
 cask "dosbox-x" do
-  version "0.83.9,20201231203002"
+  version "0.83.8,20201130192748"
   sha256 "2f8c06d1cfe42e1600bbb16116951939eee8e6d64a25f585d1b756964ff9911f"
 
   url "https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v#{version.before_comma}/dosbox-x-macosx-x86_64-#{version.after_comma}.zip",
