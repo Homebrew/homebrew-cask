635a2bce285f33bae89fdf03b60b80b4cfa84a79
Mehdi Bounya <mehdi.bounya@gmail.com>
2021-01-07 12:27:39 +0100

    Update munki from 5.0.1.4059 to 5.2.1.4260 (#97379)

--- Casks/munki.rb
+++ Casks/munki.rb
@@ -1,6 +1,6 @@
 cask "munki" do
-  version "5.0.1.4059"
-  sha256 "2f638ee7aebdee58ddc721c8203a12e16f6bdc49e94c31514d581c5961c78c5e"
+  version "5.2.1.4260"
+  sha256 "920fa04740f008809a41f9022688f600610ca98a4a2b8dc66d3c35094635ee8d"
 
   url "https://github.com/munki/munki/releases/download/v#{version.major_minor_patch}/munkitools-#{version}.pkg",
       verified: "github.com/munki/munki/"
