a061c70b544e6c840ab2f7adbe889f77779628e8
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:53 +0100

    spotify-now-playing: add livecheck

--- Casks/spotify-now-playing.rb
+++ Casks/spotify-now-playing.rb
@@ -3,9 +3,13 @@ cask "spotify-now-playing" do
   sha256 "3771b0a35b87264f387fbd591c50fd20554508692d3060d0ddce536aac23d17c"
 
   url "https://github.com/davicorreiajr/spotify-now-playing/releases/download/v#{version}/spotify-now-playing-#{version}.dmg"
-  appcast "https://github.com/davicorreiajr/spotify-now-playing/releases.atom"
   name "Spotify - now playing"
   homepage "https://github.com/davicorreiajr/spotify-now-playing"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Spotify - now playing.app"
 
