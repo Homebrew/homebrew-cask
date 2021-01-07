d84782d0a780cd0ed5e5b2c17c646f3ceaf46d8d
Markus Reiter <me@reitermark.us>
2021-01-07 14:44:43 +0100

    another-redis-desktop-manager: add livecheck

--- Casks/another-redis-desktop-manager.rb
+++ Casks/another-redis-desktop-manager.rb
@@ -3,10 +3,14 @@ cask "another-redis-desktop-manager" do
   sha256 "151d3c1250d7dda5adc46e4a9b89d0e5462c403c24da5f8fbad82d55ed7f63b9"
 
   url "https://github.com/qishibo/AnotherRedisDesktopManager/releases/download/v#{version}/Another-Redis-Desktop-Manager.#{version}.dmg"
-  appcast "https://github.com/qishibo/AnotherRedisDesktopManager/releases.atom"
   name "Another Redis Desktop Manager"
   desc "Redis desktop manager"
   homepage "https://github.com/qishibo/AnotherRedisDesktopManager/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Another Redis Desktop Manager.app"
 end
