f64b9f824037cbaa386d20d9abec14dab6285b57
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:33 +0100

    postgrespreferencepane: add livecheck

--- Casks/postgrespreferencepane.rb
+++ Casks/postgrespreferencepane.rb
@@ -3,9 +3,13 @@ cask "postgrespreferencepane" do
   sha256 "fb3f618321efec12cbad21ec2d77e6a024a35f7a17bb304b421970f85e38337f"
 
   url "https://github.com/MaccaTech/PostgresPrefs/releases/download/v#{version}/PostgreSQL.prefPane-v#{version}.zip"
-  appcast "https://github.com/MaccaTech/PostgresPrefs/releases.atom"
   name "PostgresPrefs"
   homepage "https://github.com/MaccaTech/PostgresPrefs"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   prefpane "PostgreSQL.prefPane"
 end
