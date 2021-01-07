be3d13f0c07e012596c43ff85de7962e48269b36
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:30 +0100

    picgo: add livecheck

--- Casks/picgo.rb
+++ Casks/picgo.rb
@@ -3,10 +3,15 @@ cask "picgo" do
   sha256 "49eb462ed3563bff862111c3b3ec49f2c3198536b2cf69b5fc468d2cc33b8aae"
 
   url "https://github.com/Molunerfinn/PicGo/releases/download/v#{version}/PicGo-#{version}-mac.zip"
-  appcast "https://github.com/Molunerfinn/PicGo/releases.atom"
   name "PicGo"
   desc "Tool for uploading images"
   homepage "https://github.com/Molunerfinn/PicGo"
+
+  livecheck do
+    url "https://github.com/Molunerfinn/PicGo/releases"
+    strategy :page_match
+    regex(%r{href=.*?/v?(\d+(?:\.\d+)*)/PicGo-(?:\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "PicGo.app"
 end
