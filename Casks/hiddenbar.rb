3d5f6e19a9d61106ad5fdb81cc44412fdc7712a5
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:41 +0100

    hiddenbar: add livecheck

--- Casks/hiddenbar.rb
+++ Casks/hiddenbar.rb
@@ -3,10 +3,14 @@ cask "hiddenbar" do
   sha256 "7043cf6a07d9ebdfc1e67656197183bc6dfa81504d447f22b7c89cb62e136893"
 
   url "https://github.com/dwarvesf/hidden/releases/download/v#{version}/Hidden.Bar.#{version}.dmg"
-  appcast "https://github.com/dwarvesf/hidden/releases.atom"
   name "Hidden Bar"
   desc "Utility to hide menu bar items"
   homepage "https://github.com/dwarvesf/hidden/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   depends_on macos: ">= :high_sierra"
 
