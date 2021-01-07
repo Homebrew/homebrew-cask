ea2c024f36c060e61440596c4ba8d38318ddc975
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:26 +0100

    operadriver: add livecheck

--- Casks/operadriver.rb
+++ Casks/operadriver.rb
@@ -3,9 +3,14 @@ cask "operadriver" do
   sha256 "04ce1c879266e8b1acfdded0de02b3b810da1d13514ccc8891628dbc349605a6"
 
   url "https://github.com/operasoftware/operachromiumdriver/releases/download/v.#{version}/operadriver_mac64.zip"
-  appcast "https://github.com/operasoftware/operachromiumdriver/releases.atom"
   name "operachromiumdriver"
   homepage "https://github.com/operasoftware/operachromiumdriver"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+    regex(%r{href=.*?/v.(\d+(?:\.\d+)*)/operadriver_mac64\.zip}i)
+  end
 
   binary "operadriver_mac64/operadriver"
 end
