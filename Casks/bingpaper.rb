e81569b32df8696da7b89e727ccabdaa068d81a6
Markus Reiter <me@reitermark.us>
2021-01-08 00:42:34 +0100

    bingpaper: add livecheck (#97432)

--- Casks/bingpaper.rb
+++ Casks/bingpaper.rb
@@ -3,10 +3,17 @@ cask "bingpaper" do
   sha256 "29694e8ae3bea1a50719865ffe4ca19e7794b1c7dc733a2cd532056cf35641ee"
 
   url "https://github.com/pengsrc/BingPaper/releases/download/v#{version.before_comma}/BingPaper.v#{version.before_comma}.build.#{version.after_comma}.zip"
-  appcast "https://github.com/pengsrc/BingPaper/releases.atom"
   name "BingPaper"
   desc "Use the Bing daily photo as your wallpaper"
   homepage "https://github.com/pengsrc/BingPaper"
+
+  livecheck do
+    url "https://github.com/pengsrc/BingPaper/releases/latest"
+    strategy :page_match do |page|
+      match = page.match(%r{href=.*?/BingPaper\.v?(\d+(?:\.\d+)*)\.build\.(\d+)\.zip}i)
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   depends_on macos: ">= :catalina"
 
