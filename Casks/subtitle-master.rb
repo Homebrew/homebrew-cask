a9de548e080e3fd6d87d05ff37a6a222c70ed0af
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:55 +0100

    subtitle-master: add livecheck

--- Casks/subtitle-master.rb
+++ Casks/subtitle-master.rb
@@ -3,9 +3,14 @@ cask "subtitle-master" do
   sha256 "8936495ef4aefe9bf59c28dd4d5a92574c194062067c318667e3f55428245304"
 
   url "https://github.com/subtitle-master/subtitlemaster/releases/download/v#{version}-SNAPSHOT/Subtitle.Master-osx-v#{version}-SNAPSHOT.zip"
-  appcast "https://github.com/subtitle-master/subtitlemaster/releases.atom"
   name "Subtitle Master"
   homepage "https://github.com/subtitle-master/subtitlemaster/releases"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+    regex(%r{href=.*?/Subtitle\.Master-osx-v?(\d+(?:\.\d+)*)-SNAPSHOT\.zip}i)
+  end
 
   app "Subtitle Master.app"
 end
