b31bf7d6fbe76197cbfa464ec25989038fa16a44
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:48 +0100

    jenkins-menu: add livecheck

--- Casks/jenkins-menu.rb
+++ Casks/jenkins-menu.rb
@@ -3,9 +3,13 @@ cask "jenkins-menu" do
   sha256 "dc2b69ab27b99ed0b0c165ade90b504b7c8201213b5334c6d927affd8cf106b4"
 
   url "https://github.com/qvacua/jenkins-menu/releases/download/v#{version}/Jenkins.Menu-#{version}.zip"
-  appcast "https://github.com/qvacua/jenkins-menu/releases.atom"
   name "Jenkins Menu"
   homepage "https://github.com/qvacua/jenkins-menu/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Jenkins Menu.app"
 
