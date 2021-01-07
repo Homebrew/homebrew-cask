e0bee6a1dc227aa2f95179badc548319fb70abd3
Markus Reiter <me@reitermark.us>
2021-01-07 14:47:16 +0100

    yt-music: add livecheck

--- Casks/yt-music.rb
+++ Casks/yt-music.rb
@@ -3,10 +3,14 @@ cask "yt-music" do
   sha256 "a49bbdd1a6da40a55de3a356f9f9dc8a8a537bcc4908379ee6b856f1e57bc43b"
 
   url "https://github.com/steve228uk/YouTube-Music/releases/download/#{version}/YT.Music.zip"
-  appcast "https://github.com/steve228uk/YouTube-Music/releases.atom"
   name "YouTube Music"
   desc "App wrapper for music.youtube.com"
   homepage "https://github.com/steve228uk/YouTube-Music"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "YT Music.app"
 end
