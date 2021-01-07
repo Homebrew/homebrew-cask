9ecf9c30fea0aae57abd4be88ebfbce80287d44d
Markus Reiter <me@reitermark.us>
2021-01-07 14:47:16 +0100

    youll-never-take-me-alive: add livecheck

--- Casks/youll-never-take-me-alive.rb
+++ Casks/youll-never-take-me-alive.rb
@@ -3,11 +3,15 @@ cask "youll-never-take-me-alive" do
   sha256 "6d89162762e4e952bec046943c094f37c06b89795d477fff46006362bc6c282a"
 
   url "https://github.com/iSECPartners/yontma-mac/releases/download/#{version}/yontma-#{version}.dmg"
-  appcast "https://github.com/iSECPartners/yontma-mac/releases.atom"
   name "You'll Never Take Me Alive!"
   name "YoNTMA"
   desc "Utility to enhance the protection of encrypted data"
   homepage "https://github.com/iSECPartners/yontma-mac"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "You'll Never Take Me Alive!.app"
 end
