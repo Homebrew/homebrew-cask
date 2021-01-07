1ddf441d11d3bc0c819cfd985304e2398eb751bb
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:32 +0100

    geany: add livecheck

--- Casks/geany.rb
+++ Casks/geany.rb
@@ -3,10 +3,14 @@ cask "geany" do
   sha256 "dc8ed9352f0b433503e4d8eef3e6da3e4e2e5d5d1b1b2d63a3e3c85518a74cc5"
 
   url "https://download.geany.org/geany-#{version}_osx-3.dmg"
-  appcast "https://github.com/geany/geany/releases.atom"
   name "Geany"
   desc "Fast and lightweight IDE"
   homepage "https://www.geany.org/"
+
+  livecheck do
+    url "https://github.com/geany/geany/releases"
+    strategy :git
+  end
 
   app "Geany.app"
 end
