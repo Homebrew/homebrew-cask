e1ea261820fa9b539ea11e8ee8dfce1f725e4410
Markus Reiter <me@reitermark.us>
2021-01-07 14:44:56 +0100

    blurred: add livecheck

--- Casks/blurred.rb
+++ Casks/blurred.rb
@@ -3,10 +3,14 @@ cask "blurred" do
   sha256 "15903ce2484f783c53cbad905ea93a5045c87767e7b89e37300d2200902dff37"
 
   url "https://github.com/dwarvesf/blurred/releases/download/v#{version}/Blurred.#{version}.dmg"
-  appcast "https://github.com/dwarvesf/blurred/releases.atom"
   name "Blurred"
   desc "Utility to dim background/inactive content in the screen"
   homepage "https://github.com/dwarvesf/blurred/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   depends_on macos: ">= :catalina"
 
