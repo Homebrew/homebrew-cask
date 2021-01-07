61c4e4c07d1ce4e673d85c3ea7bcc0b47ec65e9b
Markus Reiter <me@reitermark.us>
2021-01-08 00:18:58 +0100

    kekaexternalhelper: add livecheck

--- Casks/kekaexternalhelper.rb
+++ Casks/kekaexternalhelper.rb
@@ -3,10 +3,17 @@ cask "kekaexternalhelper" do
   sha256 "4e232df4d93ee4bff300feb91bf62f5c18880355f3c79dbe43b3e3f710461d21"
 
   url "https://github.com/aonez/Keka/releases/download/v#{version.before_comma}/KekaExternalHelper-v#{version.after_comma}.zip"
-  appcast "https://github.com/aonez/Keka/releases.atom"
   name "Keka External Helper"
   desc "Helper application for the Keka file archiver"
   homepage "https://github.com/aonez/Keka/wiki/Default-application"
+
+  livecheck do
+    url "https://github.com/aonez/Keka/releases"
+    strategy :page_match do |page|
+      match = page.match(%r{href=.*?/v?(\d+(?:\.\d+)*)/KekaExternalHelper-v?(\d+(?:\.\d+)*)\.zip}i)
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   app "KekaExternalHelper.app"
 end
