8dc8d672a0dbe2b55ffd69e9d12bb8b909920fe2
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:27 +0100

    osquery: add livecheck

--- Casks/osquery.rb
+++ Casks/osquery.rb
@@ -3,10 +3,14 @@ cask "osquery" do
   sha256 "1d687b00c8e27f68e95c624bd1ade9b79311321c0ce905ee35860b2c4e825d56"
 
   url "https://pkg.osquery.io/darwin/osquery-#{version}.pkg"
-  appcast "https://github.com/osquery/osquery/releases.atom"
   name "osquery"
   desc "SQL powered operating system instrumentation and analytics"
   homepage "https://osquery.io/"
+
+  livecheck do
+    url "https://github.com/osquery/osquery/releases"
+    strategy :git
+  end
 
   pkg "osquery-#{version}.pkg"
 
