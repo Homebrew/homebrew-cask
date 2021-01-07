54fb24f66b6efe6f07c79a1742370607c04588d0
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:32 +0100

    gearboy: add livecheck

--- Casks/gearboy.rb
+++ Casks/gearboy.rb
@@ -3,9 +3,13 @@ cask "gearboy" do
   sha256 "f07cdba0448cee3b63e9912dc7e5555681602ba01bb692c0c96ea504e69adce5"
 
   url "https://github.com/drhelius/Gearboy/releases/download/gearboy-#{version}/Gearboy-#{version}-macOS.zip"
-  appcast "https://github.com/drhelius/Gearboy/releases.atom"
   name "Gearboy"
   homepage "https://github.com/drhelius/Gearboy"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Gearboy-#{version}-macOS/Gearboy.app"
 end
