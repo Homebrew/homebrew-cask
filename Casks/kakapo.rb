fc21e962f31f3fb3f22512b264730415b2b0c294
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:51 +0100

    kakapo: add livecheck

--- Casks/kakapo.rb
+++ Casks/kakapo.rb
@@ -4,10 +4,14 @@ cask "kakapo" do
 
   url "https://github.com/bluedaniel/Kakapo-app/releases/download/v#{version}/Kakapo-#{version}-Mac.zip",
       verified: "github.com/bluedaniel/Kakapo-app/"
-  appcast "https://github.com/bluedaniel/Kakapo-app/releases.atom"
   name "Kakapo"
   desc "Open-source ambient sound mixer"
   homepage "http://www.kakapo.co/app.html"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Kakapo.app"
 end
