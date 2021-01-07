d6525179650deb4fb25728617ec031d95db12afc
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:34 +0100

    gitbook: add livecheck

--- Casks/gitbook.rb
+++ Casks/gitbook.rb
@@ -3,9 +3,13 @@ cask "gitbook" do
   sha256 "f8e2f7b28e7dfa18b3736f8d43d68068228774c0827fdc8c685846a129459c5f"
 
   url "https://github.com/GitbookIO/editor-legacy/releases/download/#{version}/gitbook-mac.dmg"
-  appcast "https://github.com/GitbookIO/editor-legacy/releases.atom"
   name "GitBook"
   homepage "https://github.com/GitbookIO/editor-legacy"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "GitBook.app"
 
