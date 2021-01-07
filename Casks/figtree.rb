6d579202fa4ec7f779daf21925494c55e743b1f0
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:27 +0100

    figtree: add livecheck

--- Casks/figtree.rb
+++ Casks/figtree.rb
@@ -3,9 +3,13 @@ cask "figtree" do
   sha256 "4a11741143982a9b7fea78e60c8315ce8e8436eeb96ab3ee5376c53c83e54b9b"
 
   url "https://github.com/rambaut/figtree/releases/download/v#{version}/FigTree.v#{version}.dmg"
-  appcast "https://github.com/rambaut/figtree/releases.atom"
   name "FigTree"
   homepage "https://github.com/rambaut/figtree/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "FigTree v#{version}.app"
   qlplugin "QuickLook Plugin/FigTreeQuickLookPlugin.qlgenerator"
