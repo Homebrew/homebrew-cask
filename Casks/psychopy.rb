e2f71104e29ac6c657553647f1c2cf5f293f3730
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:35 +0100

    psychopy: add livecheck

--- Casks/psychopy.rb
+++ Casks/psychopy.rb
@@ -3,10 +3,14 @@ cask "psychopy" do
   sha256 "38e9e2bf5a13d5fb46379ed510d007b6088a51d4dfb952279fe043bb0468ef43"
 
   url "https://github.com/psychopy/psychopy/releases/download/#{version}/StandalonePsychoPy-#{version}-macOS.dmg"
-  appcast "https://github.com/psychopy/psychopy/releases.atom"
   name "PsychoPy"
   desc "For running psychology and neuroscience experiments"
   homepage "https://github.com/psychopy/psychopy"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "PsychoPy.app"
 end
