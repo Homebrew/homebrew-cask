e87d1f32bca681bb60bea645f4b577d56f4e1695
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:11 +0100

    corelocationcli: add livecheck

--- Casks/corelocationcli.rb
+++ Casks/corelocationcli.rb
@@ -3,9 +3,13 @@ cask "corelocationcli" do
   sha256 "c82e5440172755a4c8e87abef72b87b9f4c422f9ac6beaed84777e9f1bd2b672"
 
   url "https://github.com/fulldecent/corelocationcli/releases/download/#{version}/CoreLocationCLI.zip"
-  appcast "https://github.com/fulldecent/corelocationcli/releases.atom"
   name "Core Location CLI"
   homepage "https://github.com/fulldecent/corelocationcli"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   binary "CoreLocationCLI"
 end
