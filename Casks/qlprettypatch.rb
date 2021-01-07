fe971c83d8784a65b41efc5e7875c1d2dcb28230
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:38 +0100

    qlprettypatch: add livecheck

--- Casks/qlprettypatch.rb
+++ Casks/qlprettypatch.rb
@@ -3,9 +3,13 @@ cask "qlprettypatch" do
   sha256 "ae2cb623cc741bf053fdfad0b5f1435c3bbad6d4b3f37d43b407296c46462182"
 
   url "https://github.com/atnan/QLPrettyPatch/releases/download/v#{version}/QLPrettyPatch.qlgenerator.zip"
-  appcast "https://github.com/atnan/QLPrettyPatch/releases.atom"
   name "QLPrettyPatch"
   homepage "https://github.com/atnan/QLPrettyPatch"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   qlplugin "QLPrettyPatch.qlgenerator"
 end
