04e7dbad266b1649f27cded48e123f366fbe5a31
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:28 +0100

    fontgoggles: add livecheck

--- Casks/fontgoggles.rb
+++ Casks/fontgoggles.rb
@@ -4,10 +4,14 @@ cask "fontgoggles" do
 
   url "https://github.com/justvanrossum/fontgoggles/releases/download/v#{version}/FontGoggles.dmg",
       verified: "github.com/justvanrossum/fontgoggles/"
-  appcast "https://github.com/justvanrossum/fontgoggles/releases.atom"
   name "FontGoggles"
   desc "Font viewer for various font formats"
   homepage "https://fontgoggles.org/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "FontGoggles.app"
 end
