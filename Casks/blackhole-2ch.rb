5e18a8c46e7a84f8b72b60cb2b3985b7a370843d
Markus Reiter <me@reitermark.us>
2021-01-07 14:44:55 +0100

    blackhole-2ch: add livecheck

--- Casks/blackhole-2ch.rb
+++ Casks/blackhole-2ch.rb
@@ -3,10 +3,14 @@ cask "blackhole-2ch" do
   sha256 "7f17c9e7272183f35472d896dd50492033c756ba98379245e62c5b918535c38c"
 
   url "https://existential.audio/downloads/BlackHole2ch.v#{version}.pkg"
-  appcast "https://github.com/ExistentialAudio/BlackHole/releases.atom"
   name "BlackHole 2ch"
   desc "Virtual Audio Driver"
   homepage "https://existential.audio/blackhole/"
+
+  livecheck do
+    url "https://github.com/ExistentialAudio/BlackHole/releases"
+    strategy :git
+  end
 
   pkg "BlackHole2ch.v#{version}.pkg"
 
