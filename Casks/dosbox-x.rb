e7bc33b51904f2d082db4159f6561ec819dc2e01
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-07 19:28:44 +0800

    Update dosbox-x.rb

--- Casks/dosbox-x.rb
+++ Casks/dosbox-x.rb
@@ -1,6 +1,6 @@
 cask "dosbox-x" do
   version "0.83.9,20201231203002"
-  sha256 "e2a15bc4cfda649a1eb131a3a69b1e9599016710426b84b6f00dd1979a73e2eb"
+  sha256 "af55d06fb9277c62708b1b8f22bba3d3555744eab759700d0e5e4cd0d439abe5"
 
   url "https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v#{version.before_comma}/dosbox-x-macosx-x86_64-#{version.after_comma}.zip",
       verified: "github.com/joncampbell123/dosbox-x/"
