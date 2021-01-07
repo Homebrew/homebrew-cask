024781efb132a081752a6dd9f47046efd82d4416
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:00 +0100

    bunqcommunity-bunq: add livecheck

--- Casks/bunqcommunity-bunq.rb
+++ Casks/bunqcommunity-bunq.rb
@@ -4,9 +4,13 @@ cask "bunqcommunity-bunq" do
 
   url "https://github.com/bunqCommunity/bunqDesktop/releases/download/#{version}/bunqDesktop-#{version}.dmg",
       verified: "github.com/bunqCommunity/bunqDesktop/"
-  appcast "https://github.com/bunqCommunity/bunqDesktop/releases.atom"
   name "bunqDesktop"
   homepage "https://bunqdesk.top/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "bunqDesktop.app"
 end
