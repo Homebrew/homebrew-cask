54293f78753fe4632ce1a1b6cf664bf6c6351aca
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:18 +0100

    next: add livecheck

--- Casks/next.rb
+++ Casks/next.rb
@@ -3,9 +3,13 @@ cask "next" do
   sha256 :no_check
 
   url "https://next.atlas.engineer/static/release/next-macos-webkit.dmg"
-  appcast "https://github.com/atlas-engineer/next/releases.atom"
   name "Next Browser"
   homepage "https://next.atlas.engineer/"
+
+  livecheck do
+    url "https://github.com/atlas-engineer/next/releases"
+    strategy :github_latest
+  end
 
   app "Next.app"
 
