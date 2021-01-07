8a4dadd4fdec3c411f278d096c1b8ba48fafbe01
Markus Reiter <me@reitermark.us>
2021-01-08 00:50:01 +0100

    gramps: add livecheck (#97439)

--- Casks/gramps.rb
+++ Casks/gramps.rb
@@ -4,10 +4,17 @@ cask "gramps" do
 
   url "https://github.com/gramps-project/gramps/releases/download/v#{version.before_comma}/Gramps-Intel-#{version.before_comma}-#{version.after_comma}.dmg",
       verified: "github.com/gramps-project/gramps/"
-  appcast "https://github.com/gramps-project/gramps/releases.atom"
   name "Gramps"
   desc "Genealogy software"
   homepage "https://gramps-project.org/blog/"
+
+  livecheck do
+    url "https://github.com/gramps-project/gramps/releases/latest"
+    strategy :page_match do |page|
+      match = page.match(%r{href=.*?/Gramps-Intel-(\d+(?:.\d+)*)-(\d+)\.dmg}i)
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   app "Gramps.app"
 end
