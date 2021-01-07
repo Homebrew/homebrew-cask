595f919cc8a5581b3722259dd42294277cd3ad7c
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:20 +0100

    dynamic-dark-mode: add livecheck

--- Casks/dynamic-dark-mode.rb
+++ Casks/dynamic-dark-mode.rb
@@ -3,10 +3,14 @@ cask "dynamic-dark-mode" do
   sha256 "696d5d605b3c2b54d2485936eff59cd150116f82f584b76938bf80f252d8f194"
 
   url "https://github.com/ApolloZhu/Dynamic-Dark-Mode/releases/download/#{version}/Dynamic_Dark_Mode-#{version}.zip"
-  appcast "https://github.com/ApolloZhu/Dynamic-Dark-Mode/releases.atom"
   name "Dynamic Dark Mode"
   desc "Automatic Dark Mode toggle"
   homepage "https://github.com/ApolloZhu/Dynamic-Dark-Mode"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   auto_updates true
   depends_on macos: ">= :mojave"
