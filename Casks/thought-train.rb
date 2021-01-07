0590e95f6e2f2629d6e1fe746df1ea6766bf132e
Markus Reiter <me@reitermark.us>
2021-01-07 14:47:00 +0100

    thought-train: add livecheck

--- Casks/thought-train.rb
+++ Casks/thought-train.rb
@@ -3,10 +3,14 @@ cask "thought-train" do
   sha256 "f135917e56bdc150923beb28d0005f1d00167f6b90ee67abb0e5d549ce39d0b4"
 
   url "https://thoughttrain.cc/downloads/thought-train-#{version}.pkg"
-  appcast "https://github.com/marcperel/thought-train-production/releases.atom"
   name "Thought Train"
   desc "Note taking and to-do-list application"
   homepage "https://www.thoughttrain.cc/"
+
+  livecheck do
+    url "https://github.com/marcperel/thought-train-production/releases"
+    strategy :git
+  end
 
   pkg "thought-train-#{version}.pkg"
 
