c8c7be4dc4862f943a6f5f484c48fe894b525732
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:21 +0100

    nwjs: add livecheck

--- Casks/nwjs.rb
+++ Casks/nwjs.rb
@@ -3,9 +3,14 @@ cask "nwjs" do
   sha256 "af29773d0227512a6a47d15aecf2d9c9a28e6ba01a1a4ad84bde3fb4d9afea79"
 
   url "https://dl.nwjs.io/v#{version}/nwjs-sdk-v#{version}-osx-x64.zip"
-  appcast "https://github.com/nwjs/nw.js/releases.atom"
   name "NW.js"
   homepage "https://nwjs.io/"
+
+  livecheck do
+    url "https://github.com/nwjs/nw.js/releases"
+    strategy :git
+    regex(/^nw-v?(\d+(?:\.\d+)*)$/)
+  end
 
   app "nwjs-sdk-v#{version}-osx-x64/nwjs.app"
   binary "nwjs-sdk-v#{version}-osx-x64/nwjc"
