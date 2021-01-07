a30182562ecb3a6d1a80a4a3b6c1c5749e5857bb
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:39 +0100

    gulp: add livecheck

--- Casks/gulp.rb
+++ Casks/gulp.rb
@@ -3,9 +3,13 @@ cask "gulp" do
   sha256 "59fed5d8c801c9302debf463f2d274404548e23433c965144e69a0b4a2e23851"
 
   url "https://github.com/sindresorhus/gulp-app/releases/download/#{version}/gulp.app.zip"
-  appcast "https://github.com/sindresorhus/gulp-app/releases.atom"
   name "gulp-app"
   homepage "https://github.com/sindresorhus/gulp-app"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "gulp.app"
 end
