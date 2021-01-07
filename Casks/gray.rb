3ffe4cd5d714861a2fb1081eaa9312f233448810
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:38 +0100

    gray: add livecheck

--- Casks/gray.rb
+++ Casks/gray.rb
@@ -3,10 +3,14 @@ cask "gray" do
   sha256 "e90a4eb35f258af6abf6fe2d37a3471071a805426aea5a04e9092c8a617015b9"
 
   url "https://github.com/zenangst/Gray/releases/download/#{version}/Gray.zip"
-  appcast "https://github.com/zenangst/Gray/releases.atom"
   name "Gray"
   desc "Tool to set light or dark appearance on a per-app basis"
   homepage "https://github.com/zenangst/Gray"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   depends_on macos: ">= :mojave"
 
