9706cc3b06c693b4a22b82f5833de8135a6dd47b
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:26 +0100

    orange: add livecheck

--- Casks/orange.rb
+++ Casks/orange.rb
@@ -3,10 +3,14 @@ cask "orange" do
   sha256 "f175f7562bd1ae34ef8ec4e4bcd9b7090e39b95383a1206cbbd8f18b4828eef2"
 
   url "https://download.biolab.si/download/files/Orange#{version.major}-#{version}-Python3.7.6.dmg"
-  appcast "https://github.com/biolab/orange3/releases.atom"
   name "Orange"
   desc "Component-based data mining software"
   homepage "https://orange.biolab.si/"
+
+  livecheck do
+    url "https://github.com/biolab/orange3/releases"
+    strategy :git
+  end
 
   app "Orange#{version.major}.app"
 end
