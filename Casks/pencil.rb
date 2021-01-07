f317ead8dd92e443d9c491f3405217623399deb1
Markus Reiter <me@reitermark.us>
2021-01-07 14:27:34 +0100

    pencil: add livecheck (#97402)

--- Casks/pencil.rb
+++ Casks/pencil.rb
@@ -1,13 +1,16 @@
 cask "pencil" do
-  version "3.1.0.ga"
+  version "3.1.0"
   sha256 "5369787a801a470179a86ba60407b33e647f826b1a8b001b62e4f81a6bb0629d"
 
-  url "https://pencil.evolus.vn/dl/V#{version}/Pencil-#{version}.dmg"
-  appcast "https://github.com/evolus/pencil/releases.atom",
-          must_contain: version.major_minor_patch
+  url "https://pencil.evolus.vn/dl/V#{version}.ga/Pencil-#{version}.ga.dmg"
   name "Pencil"
   name "The Pencil Project"
   homepage "https://pencil.evolus.vn/"
+
+  livecheck do
+    url "https://github.com/evolus/pencil/releases"
+    strategy :github_latest
+  end
 
   app "Pencil.app"
 end
