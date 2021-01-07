2f2079bee3663f2b4666cf0ee4cc7532ffb0a377
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:09 +0100

    michaelvillar-timer: add livecheck

--- Casks/michaelvillar-timer.rb
+++ Casks/michaelvillar-timer.rb
@@ -3,10 +3,14 @@ cask "michaelvillar-timer" do
   sha256 "9595154ca9f178edc7cb52aaf9360139d1fda85220bbe90a1d4fe5c0d23066e3"
 
   url "https://github.com/michaelvillar/timer-app/releases/download/#{version}/Timer.app.zip"
-  appcast "https://github.com/michaelvillar/timer-app/releases.atom"
   name "Timer"
   desc "Timer application"
   homepage "https://github.com/michaelvillar/timer-app"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Timer.app"
 end
