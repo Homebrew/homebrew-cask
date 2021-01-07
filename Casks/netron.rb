9fb78bd71754176eda1597a47d635d5049883e8f
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:18 +0100

    netron: add livecheck

--- Casks/netron.rb
+++ Casks/netron.rb
@@ -3,10 +3,14 @@ cask "netron" do
   sha256 "7c1af72da225e7e4c8594605e34ee1e9480d1ef883aeb7b43305d9d938f5ec28"
 
   url "https://github.com/lutzroeder/netron/releases/download/v#{version}/Netron-#{version}-mac.zip"
-  appcast "https://github.com/lutzroeder/netron/releases.atom"
   name "Netron"
   desc "Visualizer for neural network, deep learning, and machine learning models"
   homepage "https://github.com/lutzroeder/netron"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   auto_updates true
 
