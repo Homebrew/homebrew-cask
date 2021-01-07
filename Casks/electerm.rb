d901a8febbc03f8b8b192bac60576c06cbbf612b
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:21 +0100

    electerm: add livecheck

--- Casks/electerm.rb
+++ Casks/electerm.rb
@@ -3,10 +3,14 @@ cask "electerm" do
   sha256 "437ce69b3db05bf6077292076666cfd9d4b52f24db8085bb37059517a9a387ae"
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac.dmg"
-  appcast "https://github.com/electerm/electerm/releases.atom"
   name "electerm"
   desc "Terminal/ssh/serial port/sftp client"
   homepage "https://github.com/electerm/electerm/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "electerm.app"
 end
