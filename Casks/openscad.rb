5d5a0c304e830459ff4ac5be6f5c1fed0b29ea39
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:25 +0100

    openscad: add livecheck

--- Casks/openscad.rb
+++ Casks/openscad.rb
@@ -3,10 +3,15 @@ cask "openscad" do
   sha256 "df6f6f3d34ac0d07f533ec4ccf59082189fb37c0276c1b8df651291e2509420e"
 
   url "https://files.openscad.org/OpenSCAD-#{version}.dmg"
-  appcast "https://github.com/openscad/openscad/releases.atom"
   name "OpenSCAD"
   desc "Programmable solid 3D CAD modeller"
   homepage "https://www.openscad.org/"
+
+  livecheck do
+    url "https://files.openscad.org/"
+    strategy :page_match
+    regex(/href=.*?OpenSCAD-(\d+(?:\.\d+)*)\.dmg/i)
+  end
 
   app "OpenSCAD.app"
 
