6fcc78a5565ff6aa6edb09782a70e8945a33f3b7
J3n50m4t <J3n50m4t@users.noreply.github.com>
2021-01-07 22:57:54 +0100

    Update boost-note from 0.11.6 to 0.12.3 (#97428)

--- Casks/boost-note.rb
+++ Casks/boost-note.rb
@@ -1,6 +1,6 @@
 cask "boost-note" do
-  version "0.11.6"
-  sha256 "224df26e306b8c4594a40255fc5e01eeaa9a8fdd1df916eac0ef0cc9d4eb19bf"
+  version "0.12.3"
+  sha256 "cf3c4e41accf53726276ff9b803793b3e9043e08d12f11a8598db9327b9a0db7"
 
   url "https://github.com/BoostIO/BoostNote.next/releases/download/v#{version}/boost-note-mac.dmg",
       verified: "github.com/BoostIO/BoostNote.next/"
