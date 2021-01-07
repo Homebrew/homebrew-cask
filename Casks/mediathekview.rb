5e5db90c75fdfae8a7a2288f6aa8dcad8877b0d2
Markus Reiter <me@reitermark.us>
2021-01-08 00:27:49 +0100

    Update mediathekview.rb

--- Casks/mediathekview.rb
+++ Casks/mediathekview.rb
@@ -3,7 +3,6 @@ cask "mediathekview" do
   sha256 "fe20d561665841660af30d331a420325c4dbc809dec5c3c305b3f35f7dd1b36a"
 
   url "https://download.mediathekview.de/stabil/MediathekView-#{version}-mac.dmg"
-  appcast "https://mediathekview.de/changelog/index.xml"
   name "MediathekView"
   desc "Manages online multimedia libs of German, Austrian and Swiss public broadcasters"
   homepage "https://mediathekview.de/"
