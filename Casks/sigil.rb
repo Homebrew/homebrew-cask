23d5ca7d39ee03b7c760e54a98ea69b74df75c56
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:49 +0100

    sigil: add livecheck

--- Casks/sigil.rb
+++ Casks/sigil.rb
@@ -4,10 +4,14 @@ cask "sigil" do
 
   url "https://github.com/Sigil-Ebook/Sigil/releases/download/#{version}/Sigil.app-#{version}-Mac.txz",
       verified: "github.com/Sigil-Ebook/Sigil/"
-  appcast "https://github.com/Sigil-Ebook/Sigil/releases.atom"
   name "Sigil"
   desc "EPUB ebook editor"
   homepage "https://sigil-ebook.com/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   depends_on macos: ">= :sierra"
 
