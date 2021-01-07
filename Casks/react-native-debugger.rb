b707edfd50d7cd44e1a88d2da0064b3cd4582437
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:43 +0100

    react-native-debugger: add livecheck

--- Casks/react-native-debugger.rb
+++ Casks/react-native-debugger.rb
@@ -3,9 +3,13 @@ cask "react-native-debugger" do
   sha256 "3888b4f36401c814e9cf182363d8b1ca21d4b8a055d22c87ada3985dd25b4c0b"
 
   url "https://github.com/jhen0409/react-native-debugger/releases/download/v#{version}/rn-debugger-macos-x64.zip"
-  appcast "https://github.com/jhen0409/react-native-debugger/releases.atom"
   name "React Native Debugger"
   homepage "https://github.com/jhen0409/react-native-debugger"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   auto_updates true
 
