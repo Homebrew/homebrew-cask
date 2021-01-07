d88aeb19d641e73d2e2da8809f9bc05fd87a772e
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:15 +0100

    multi: add livecheck

--- Casks/multi.rb
+++ Casks/multi.rb
@@ -3,10 +3,14 @@ cask "multi" do
   sha256 "6b22e67312ba2f2c3c1c9095cc698cad45b1e51f8564aca3a61826a4779262d7"
 
   url "https://github.com/hkgumbs/multi/releases/download/v#{version}/Multi.#{version}.dmg"
-  appcast "https://github.com/hkgumbs/multi/releases.atom"
   name "Multi"
   desc "Create apps from groups of websites"
   homepage "https://github.com/hkgumbs/multi"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Multi.app"
 end
