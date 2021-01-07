3e4716fa8c601ef471cadc89444523708f87efe6
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:06 +0100

    clashxr: add livecheck

--- Casks/clashxr.rb
+++ Casks/clashxr.rb
@@ -3,10 +3,14 @@ cask "clashxr" do
   sha256 "13b845e31e13380e031a4df3650711398cbb16f8e2b01b91736a8b9d0bfd753a"
 
   url "https://github.com/paradiseduo/ClashXR/releases/download/#{version}/ClashXR.dmg"
-  appcast "https://github.com/paradiseduo/ClashXR/releases.atom"
   name "ClashXR"
   desc "Rule-based custom proxy with GUI based on clash"
   homepage "https://github.com/paradiseduo/ClashXR"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   auto_updates true
   depends_on macos: ">= :sierra"
