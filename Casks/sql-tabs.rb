b1adb5d0a39503b54ad3e496d8aa911eb7dbfe91
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:53 +0100

    sql-tabs: add livecheck

--- Casks/sql-tabs.rb
+++ Casks/sql-tabs.rb
@@ -4,9 +4,13 @@ cask "sql-tabs" do
 
   url "https://github.com/sasha-alias/sqltabs/releases/download/v#{version}/SQL-Tabs-#{version}.dmg",
       verified: "github.com/sasha-alias/sqltabs/"
-  appcast "https://github.com/sasha-alias/sqltabs/releases.atom"
   name "SQL Tabs"
   homepage "https://www.sqltabs.com/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "SQL Tabs.app"
 
