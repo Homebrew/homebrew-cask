8890420018e5fc2ff1715afcf702ea0962a72357
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:08 +0100

    clocksaver: add livecheck

--- Casks/clocksaver.rb
+++ Casks/clocksaver.rb
@@ -3,9 +3,13 @@ cask "clocksaver" do
   sha256 "9497e774695cadfb25fb6ad1b5689a110f806041b34d3b536b862d653de8bca4"
 
   url "https://github.com/soffes/Clock.saver/releases/download/v#{version}/Clock.saver-#{version}.zip"
-  appcast "https://github.com/soffes/Clock.saver/releases.atom"
   name "Clock.saver screensaver"
   homepage "https://github.com/soffes/Clock.saver"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   screen_saver "Clock.saver"
 end
