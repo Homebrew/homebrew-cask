8b07e13756b2d5ef57ebd654759b7d5ec85ee445
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:32 +0100

    postbird: add livecheck

--- Casks/postbird.rb
+++ Casks/postbird.rb
@@ -3,10 +3,14 @@ cask "postbird" do
   sha256 "d9ac960e0c48fab31b0662c726a3eb373794c14dd49651fa533ca3c1c67c67be"
 
   url "https://github.com/Paxa/postbird/releases/download/#{version}/Postbird-#{version}.dmg"
-  appcast "https://github.com/Paxa/postbird/releases.atom"
   name "Postbird"
   desc "Open-source PostgreSQL GUI client"
   homepage "https://github.com/Paxa/postbird"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Postbird.app"
 end
