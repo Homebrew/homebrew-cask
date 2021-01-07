bc786505040193e5be84efe4521e846ec2ffa245
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:50 +0100

    julia: add livecheck

--- Casks/julia.rb
+++ Casks/julia.rb
@@ -3,10 +3,14 @@ cask "julia" do
   sha256 "802cdb67748bbd8a943604981d4f2a2c4c98899f5bbad0aef8a7ea8bc9b31abd"
 
   url "https://julialang-s3.julialang.org/bin/mac/x64/#{version.major_minor}/julia-#{version}-mac64.dmg"
-  appcast "https://github.com/JuliaLang/julia/releases.atom"
   name "Julia"
   desc "Programming language for technical computing"
   homepage "https://julialang.org/"
+
+  livecheck do
+    url "https://github.com/JuliaLang/julia/releases"
+    strategy :git
+  end
 
   app "Julia-#{version.major_minor}.app"
   binary "#{appdir}/Julia-#{version.major_minor}.app/Contents/Resources/julia/bin/julia"
