ad4e293c12d038c216c3656d7c5fdef13b824fea
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:43 +0100

    rcloneosx: add livecheck

--- Casks/rcloneosx.rb
+++ Casks/rcloneosx.rb
@@ -3,10 +3,14 @@ cask "rcloneosx" do
   sha256 "dc44945e81c66ce9238212a4878cb35c507d364069e2361cd76b333bf0e0ab0c"
 
   url "https://github.com/rsyncOSX/rcloneosx/releases/download/v#{version}/rcloneosx.#{version}.dmg"
-  appcast "https://github.com/rsyncOSX/rcloneosx/releases.atom"
   name "RcloneOSX"
   desc "GUI for rclone"
   homepage "https://github.com/rsyncOSX/rcloneosx"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   depends_on formula: "rclone"
 
