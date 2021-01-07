4ff67a5f4d9e56180a3d33cd98b27daf30939af0
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:50 +0100

    silicon: add livecheck

--- Casks/silicon.rb
+++ Casks/silicon.rb
@@ -3,10 +3,14 @@ cask "silicon" do
   sha256 "4f6cf3f6b6cf0b8f067995a292538f122278226fa067427bf184b179076cbf79"
 
   url "https://github.com/DigiDNA/Silicon/releases/download/#{version}/Silicon.app.zip"
-  appcast "https://github.com/DigiDNA/Silicon/releases.atom"
   name "Silicon"
   desc "Identify Intel-only apps"
   homepage "https://github.com/DigiDNA/Silicon"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   depends_on macos: ">= :high_sierra"
 
