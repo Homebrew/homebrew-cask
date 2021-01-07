d06c78dae0481f159c292ee79006e56816de38cc
Markus Reiter <me@reitermark.us>
2021-01-07 14:47:08 +0100

    v2rayu: add livecheck

--- Casks/v2rayu.rb
+++ Casks/v2rayu.rb
@@ -3,10 +3,14 @@ cask "v2rayu" do
   sha256 "2e8f132c05a8af3b89bd0dacafd2fc9244636587848453f7cd877ab246a33d52"
 
   url "https://github.com/yanue/V2rayU/releases/download/#{version}/V2rayU.dmg"
-  appcast "https://github.com/yanue/V2rayU/releases.atom"
   name "V2rayU"
   desc "Collection of tools to build a dedicated basic communication network"
   homepage "https://github.com/yanue/V2rayU"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   auto_updates true
   depends_on macos: ">= :sierra"
