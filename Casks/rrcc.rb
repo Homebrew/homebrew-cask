6792df33729ab8365a25239ff869f2cd61c1e09f
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:45 +0100

    rrcc: add livecheck

--- Casks/rrcc.rb
+++ Casks/rrcc.rb
@@ -3,9 +3,13 @@ cask "rrcc" do
   sha256 "7c1a657544eb026aeef679d796d2032fb5cd83eadc26f8bf784c59702bcb434e"
 
   url "https://github.com/LazyT/rrcc/releases/download/#{version}/rrcc-#{version.no_dots}.dmg"
-  appcast "https://github.com/LazyT/rrcc/releases.atom"
   name "RoboRock Control Center"
   homepage "https://github.com/LazyT/rrcc"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "rrcc.app"
 
