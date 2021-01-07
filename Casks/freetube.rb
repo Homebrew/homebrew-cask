321de46ca3e4b7222771f84dcd33100be122abe2
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:29 +0100

    freetube: add livecheck

--- Casks/freetube.rb
+++ Casks/freetube.rb
@@ -3,10 +3,15 @@ cask "freetube" do
   sha256 "2a86d3cdba6613af13f83170a07934bf7507052e83ace94c7382f2621756e459"
 
   url "https://github.com/FreeTubeApp/FreeTube/releases/download/v#{version}-beta/freetube-#{version}-mac.dmg"
-  appcast "https://github.com/FreeTubeApp/FreeTube/releases.atom"
   name "FreeTube"
   desc "YouTube player focusing on privacy"
   homepage "https://github.com/FreeTubeApp/FreeTube"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+    regex(%r{href=.*?/freetube-(\d+(?:\.\d+)*)-mac\.dmg}i)
+  end
 
   app "FreeTube.app"
 
