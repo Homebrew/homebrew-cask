c5e79614ae8421234d0641ef2bd516dc361a1b55
Markus Reiter <me@reitermark.us>
2021-01-07 14:47:11 +0100

    wavtap: add livecheck

--- Casks/wavtap.rb
+++ Casks/wavtap.rb
@@ -3,10 +3,14 @@ cask "wavtap" do
   sha256 "b114703e407ce269070165421a81f74e64ac1730a85d829c148325b4ac1a18f6"
 
   url "https://github.com/pje/WavTap/releases/download/#{version}/WavTap.#{version}.pkg"
-  appcast "https://github.com/pje/WavTap/releases.atom"
   name "WavTap"
   desc "Tool to globally capture whatever sound is playing"
   homepage "https://github.com/pje/wavtap"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   pkg "WavTap.#{version}.pkg"
 
