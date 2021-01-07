b6c0d2854d6c4d58c5df58a425fafafabbe28a1b
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:03 +0100

    machoexplorer: add livecheck

--- Casks/machoexplorer.rb
+++ Casks/machoexplorer.rb
@@ -3,10 +3,14 @@ cask "machoexplorer" do
   sha256 "3f9b9e400e008174cb9c4eb8f186b18060c5a12a93622c9d5f60254500735232"
 
   url "https://github.com/everettjf/MachOExplorer/releases/download/v#{version}/MachOExplorer.dmg"
-  appcast "https://github.com/everettjf/MachOExplorer/releases.atom"
   name "MachOExplorer"
   desc "Mach-O Executable File Explorer"
   homepage "https://github.com/everettjf/MachOExplorer"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "MachOExplorer.app"
 end
