ee56053ecd168921b95afbc309df166c93cffade
Markus Reiter <me@reitermark.us>
2021-01-07 14:44:53 +0100

    betaflight-configurator: add livecheck

--- Casks/betaflight-configurator.rb
+++ Casks/betaflight-configurator.rb
@@ -3,10 +3,14 @@ cask "betaflight-configurator" do
   sha256 "29565420c31e849703d941bcb5ca2ee31af6a1c069e325c49e0d1133447b4b03"
 
   url "https://github.com/betaflight/betaflight-configurator/releases/download/#{version}/betaflight-configurator_#{version}_macOS.dmg"
-  appcast "https://github.com/betaflight/betaflight-configurator/releases.atom"
   name "Betaflight-Configurator"
   desc "Configuration tool for the Betaflight firmware"
   homepage "https://github.com/betaflight/betaflight-configurator"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Betaflight Configurator.app"
 end
