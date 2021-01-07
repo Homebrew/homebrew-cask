532d005f1d1be0d4ac5232060b491878d3bb02cc
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:28 +0100

    fly: add livecheck

--- Casks/fly.rb
+++ Casks/fly.rb
@@ -3,10 +3,14 @@ cask "fly" do
   sha256 "e095e0d631cd42a28b63e7101819840bad1b255d8b6d31ceb1fc0220c055605d"
 
   url "https://github.com/concourse/concourse/releases/download/v#{version}/fly-#{version}-darwin-amd64.tgz"
-  appcast "https://github.com/concourse/concourse/releases.atom"
   name "fly"
   desc "Official CLI tool for Concourse CI"
   homepage "https://github.com/concourse/concourse"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   binary "fly"
 end
