def480f9395b4e06f46ed975b097a88fb3d150b2
Markus Reiter <me@reitermark.us>
2021-01-07 14:47:07 +0100

    unshaky: add livecheck

--- Casks/unshaky.rb
+++ Casks/unshaky.rb
@@ -3,10 +3,14 @@ cask "unshaky" do
   sha256 "722fdd718fe47e58f1ea9c60fbcaf81cb4bbd6173b9cf7a8c4a6425a977a9fd9"
 
   url "https://github.com/aahung/Unshaky/releases/download/v#{version}/Unshaky_v#{version}.zip"
-  appcast "https://github.com/aahung/Unshaky/releases.atom"
   name "Unshaky"
   desc "Software fix for double key presses on Apple's butterfly keyboard"
   homepage "https://github.com/aahung/Unshaky"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   auto_updates true
   depends_on macos: ">= :sierra"
