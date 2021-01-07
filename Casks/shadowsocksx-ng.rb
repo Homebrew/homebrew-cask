0ebbefa737ef4aee9ca3f55fa88abd8999a04272
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:48 +0100

    shadowsocksx-ng: add livecheck

--- Casks/shadowsocksx-ng.rb
+++ Casks/shadowsocksx-ng.rb
@@ -3,9 +3,13 @@ cask "shadowsocksx-ng" do
   sha256 "dc06a995b63f8e32be9b86c265fd2979a6d73d4742d0ff16e1b2bb8f538d77a3"
 
   url "https://github.com/shadowsocks/ShadowsocksX-NG/releases/download/v#{version}/ShadowsocksX-NG.#{version}.zip"
-  appcast "https://github.com/shadowsocks/ShadowsocksX-NG/releases.atom"
   name "ShadowsocksX-NG"
   homepage "https://github.com/shadowsocks/ShadowsocksX-NG/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   conflicts_with cask: "shadowsocksx"
   depends_on macos: ">= :sierra"
