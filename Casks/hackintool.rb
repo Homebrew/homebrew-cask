6470259a40a676f1a9d77909a136d5d5a2d2ada8
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:39 +0100

    hackintool: add livecheck

--- Casks/hackintool.rb
+++ Casks/hackintool.rb
@@ -3,10 +3,14 @@ cask "hackintool" do
   sha256 "2c39478144b2e6f248a33940510ddabc0b36d2c3220e0af7785d0d41bf823064"
 
   url "https://github.com/headkaze/Hackintool/releases/download/#{version}/Hackintool.zip"
-  appcast "https://github.com/headkaze/Hackintool/releases.atom"
   name "Hackintool"
   desc "Hackintosh patching tool"
   homepage "https://github.com/headkaze/Hackintool"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   auto_updates true
 
