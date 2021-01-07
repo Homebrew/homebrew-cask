861fecab55fc84ffb1ab9504f9bcc58bfb66d786
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:32 +0100

    gearsystem: add livecheck

--- Casks/gearsystem.rb
+++ Casks/gearsystem.rb
@@ -3,9 +3,13 @@ cask "gearsystem" do
   sha256 "497013f2988490f2b9d0f58fd847c91c899cd05eb5a7a5f00370a4760b2f23a6"
 
   url "https://github.com/drhelius/Gearsystem/releases/download/gearsystem-#{version}/Gearsystem-#{version}-macOS.zip"
-  appcast "https://github.com/drhelius/Gearsystem/releases.atom"
   name "Gearsystem"
   homepage "https://github.com/drhelius/Gearsystem"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Gearsystem-#{version}-macOS/Gearsystem.app"
 end
