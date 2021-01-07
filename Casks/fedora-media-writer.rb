28b9fd841203e870e4f724bc7fac0304361cad86
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:26 +0100

    fedora-media-writer: add livecheck

--- Casks/fedora-media-writer.rb
+++ Casks/fedora-media-writer.rb
@@ -4,10 +4,14 @@ cask "fedora-media-writer" do
 
   url "https://github.com/FedoraQt/MediaWriter/releases/download/#{version}/FedoraMediaWriter-osx-#{version}.unnotarized.dmg",
       verified: "github.com/FedoraQt/MediaWriter/"
-  appcast "https://github.com/FedoraQt/MediaWriter/releases.atom"
   name "Fedora Media Writer"
   desc "Tool to write Fedora images to portable media files"
   homepage "https://docs.fedoraproject.org/en-US/quick-docs/creating-and-using-a-live-installation-image/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Fedora Media Writer.app"
 end
