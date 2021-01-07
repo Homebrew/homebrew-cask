81d9312171f3bf5742d5f8d2c763cbc44de54fdf
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:17 +0100

    disablemonitor: add livecheck

--- Casks/disablemonitor.rb
+++ Casks/disablemonitor.rb
@@ -3,10 +3,14 @@ cask "disablemonitor" do
   sha256 "5bdb362d779f7fbb6cf916d7d14545a79206b8158ce5309f2e5bbcb7203651fb"
 
   url "https://github.com/Eun/DisableMonitor/releases/download/G#{version}/DisableMonitor-G#{version}.zip"
-  appcast "https://github.com/Eun/DisableMonitor/releases.atom"
   name "DisableMonitor"
   desc "Tool to enable or disable a monitor"
   homepage "https://github.com/Eun/DisableMonitor"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "DisableMonitor.app"
 end
