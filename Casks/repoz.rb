9830980e312a0cd572acd8502e019642ef36454a
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:44 +0100

    repoz: add livecheck

--- Casks/repoz.rb
+++ Casks/repoz.rb
@@ -3,10 +3,14 @@ cask "repoz" do
   sha256 "2c482bab34fb0969080db5d0a7d008aa9a2271cbebba915ac3e8c7e8847bf160"
 
   url "https://github.com/awaescher/RepoZ/releases/download/v#{version}/RepoZ.#{version}.pkg"
-  appcast "https://github.com/awaescher/RepoZ/releases.atom"
   name "RepoZ"
   desc "Zero-conf git repository hub"
   homepage "https://github.com/awaescher/RepoZ"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   pkg "RepoZ.#{version}.pkg"
 
