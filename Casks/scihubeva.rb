9edb7efae00880a04b07c25e2a4994a40855f718
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:46 +0100

    scihubeva: add livecheck

--- Casks/scihubeva.rb
+++ Casks/scihubeva.rb
@@ -3,10 +3,14 @@ cask "scihubeva" do
   sha256 "2b652b98b360a3e950b247fcdc4ef1f20d86910bdf6d6cefd025ed37996e2725"
 
   url "https://github.com/leovan/SciHubEVA/releases/download/#{version}/SciHubEVA-#{version}.dmg"
-  appcast "https://github.com/leovan/SciHubEVA/releases.atom"
   name "SciHubEVA"
   desc "Cross-platform Sci-Hub GUI app"
   homepage "https://github.com/leovan/SciHubEVA"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "SciHubEVA.app"
 end
