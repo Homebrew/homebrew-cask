f7ada819b076c85f5ddd7a6edcd2847034387a67
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:56 +0100

    launchrocket: add livecheck

--- Casks/launchrocket.rb
+++ Casks/launchrocket.rb
@@ -3,9 +3,13 @@ cask "launchrocket" do
   sha256 "51dc78902fecfb7ec26ab5c6516b84d1c62692349864ef48aca2fde81bd2ef4a"
 
   url "https://github.com/jimbojsb/launchrocket/releases/download/v#{version}/LaunchRocket.prefPane.zip"
-  appcast "https://github.com/jimbojsb/launchrocket/releases.atom"
   name "LaunchRocket"
   homepage "https://github.com/jimbojsb/launchrocket"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   prefpane "LaunchRocket.prefPane"
 end
