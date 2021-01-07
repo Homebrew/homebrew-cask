2eeea8dfa35f3efe362b1873901072a4aa5aa255
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:29 +0100

    pennywise: add livecheck

--- Casks/pennywise.rb
+++ Casks/pennywise.rb
@@ -3,9 +3,13 @@ cask "pennywise" do
   sha256 "9e6195f1096d399aafd77da74e4461964364fdbeec3b667cd91ecf9704e73b69"
 
   url "https://github.com/kamranahmedse/pennywise/releases/download/v#{version}/Pennywise-#{version}.dmg"
-  appcast "https://github.com/kamranahmedse/pennywise/releases.atom"
   name "Pennywise"
   homepage "https://github.com/kamranahmedse/pennywise"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Pennywise.app"
 end
