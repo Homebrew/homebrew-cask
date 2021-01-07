eb40edf4c87500a1554da0c8b42bee16286a05db
Markus Reiter <me@reitermark.us>
2021-01-07 14:47:02 +0100

    timestamp: add livecheck

--- Casks/timestamp.rb
+++ Casks/timestamp.rb
@@ -4,10 +4,14 @@ cask "timestamp" do
 
   url "https://github.com/mzdr/timestamp/releases/download/#{version}/Timestamp-#{version}-mac.zip",
       verified: "github.com/mzdr/timestamp/"
-  appcast "https://github.com/mzdr/timestamp/releases.atom"
   name "Timestamp"
   desc "Improved clock for the menu bar"
   homepage "https://mzdr.github.io/timestamp/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Timestamp.app"
 end
