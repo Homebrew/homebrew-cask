1f8cca482e6b7aba50a1edc8aebbd83ca96dcedc
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:00 +0100

    localizationeditor: add livecheck

--- Casks/localizationeditor.rb
+++ Casks/localizationeditor.rb
@@ -3,9 +3,13 @@ cask "localizationeditor" do
   sha256 "0b2f24a4485d338edf7166b6ccbad34c9f0511bb7ecf82bb376440a800121a07"
 
   url "https://github.com/igorkulman/iOSLocalizationEditor/releases/download/v#{version}/LocalizationEditor.app.zip"
-  appcast "https://github.com/igorkulman/iOSLocalizationEditor/releases.atom"
   name "LocalizationEditor"
   homepage "https://github.com/igorkulman/iOSLocalizationEditor/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   depends_on macos: ">= :high_sierra"
 
