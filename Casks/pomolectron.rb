521147a4190f08e86bad9c585c30cb5ec6914ba6
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:32 +0100

    pomolectron: add livecheck

--- Casks/pomolectron.rb
+++ Casks/pomolectron.rb
@@ -3,10 +3,14 @@ cask "pomolectron" do
   sha256 "3c6f756985a06938f33bfe11c638fc227847075d203ab098a643a0ad26d59055"
 
   url "https://github.com/amitmerchant1990/pomolectron/releases/download/v#{version}/Pomolectron-darwin-x64.zip"
-  appcast "https://github.com/amitmerchant1990/pomolectron/releases.atom"
   name "Pomolectron"
   desc "Pomodoro app for the menu bar/tray"
   homepage "https://github.com/amitmerchant1990/pomolectron"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Pomolectron-darwin-x64/Pomolectron.app"
 end
