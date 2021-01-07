6018f9552b6a3c6bc242bb3c5cdc43f063fdbd94
Markus Reiter <me@reitermark.us>
2021-01-07 14:44:49 +0100

    aural: add livecheck

--- Casks/aural.rb
+++ Casks/aural.rb
@@ -3,10 +3,14 @@ cask "aural" do
   sha256 "7b814cd659def0e6783da4cbf4e83ec1f078b55bf04ae21f54ca2a98d033b529"
 
   url "https://github.com/maculateConception/aural-player/releases/download/#{version}/AuralPlayer-#{version}.dmg"
-  appcast "https://github.com/maculateConception/aural-player/releases.atom"
   name "Aural Player"
   desc "Audio player inspired by Winamp"
   homepage "https://github.com/maculateConception/aural-player"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Aural.app"
 
