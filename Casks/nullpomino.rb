bd034766f0893009f6da5d25a4b79e3cb82d5314
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:21 +0100

    nullpomino: add livecheck

--- Casks/nullpomino.rb
+++ Casks/nullpomino.rb
@@ -3,10 +3,14 @@ cask "nullpomino" do
   sha256 "6edfc0d21b7f006a255d63ce0a9d9f920400e5c01e7072fbcc4e139a18148eae"
 
   url "https://github.com/nullpomino/nullpomino/releases/download/v#{version}/NullpoMino#{version}.dmg"
-  appcast "https://github.com/nullpomino/nullpomino/releases.atom"
   name "NullpoMino"
   desc "Action puzzle game"
   homepage "https://github.com/nullpomino/nullpomino"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "NullpoMino_#{version}.app"
 end
