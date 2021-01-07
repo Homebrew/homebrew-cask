6bd26e9034a6f422848bf8e224885ddbefbf94f9
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:04 +0100

    chiaki: add livecheck

--- Casks/chiaki.rb
+++ Casks/chiaki.rb
@@ -3,10 +3,14 @@ cask "chiaki" do
   sha256 "1afb2054bffd7b174ba8ba2dbb56825855341962e6d71c83d75399045a446cbb"
 
   url "https://github.com/thestr4ng3r/chiaki/releases/download/v#{version}/Chiaki-v#{version}-macOS-x86_64.dmg"
-  appcast "https://github.com/thestr4ng3r/chiaki/releases.atom"
   name "Chiaki"
   desc "Free and open-source PS4 remote play client"
   homepage "https://github.com/thestr4ng3r/chiaki"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Chiaki.app"
 
