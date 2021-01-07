d62e1bf20ee4c338d9281e238d15a1a548efe7e6
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:02 +0100

    caption: add livecheck

--- Casks/caption.rb
+++ Casks/caption.rb
@@ -4,10 +4,14 @@ cask "caption" do
 
   url "https://github.com/gielcobben/caption/releases/download/v#{version}/caption-#{version}-mac.zip",
       verified: "github.com/gielcobben/caption/"
-  appcast "https://github.com/gielcobben/caption/releases.atom"
   name "Caption"
   desc "Finds and sets up subtitles automatically"
   homepage "https://getcaption.co/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Caption.app"
 end
