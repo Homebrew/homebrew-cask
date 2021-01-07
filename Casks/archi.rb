a0a21c128d9d6a7527cb03687caaa90cf22d8bef
Markus Reiter <me@reitermark.us>
2021-01-07 14:44:46 +0100

    archi: add livecheck

--- Casks/archi.rb
+++ Casks/archi.rb
@@ -3,10 +3,14 @@ cask "archi" do
   sha256 "dab1df62d8b99f1aabdf85e993d9ce995003d48df32afb3aaa8e229152780e53"
 
   url "https://www.archimatetool.com/downloads/a#{version.no_dots}/Archi-Mac-#{version}.zip"
-  appcast "https://github.com/archimatetool/archi/releases.atom"
   name "Archi"
   desc "Toolkit for the ArchiMate modelling language"
   homepage "https://www.archimatetool.com/"
+
+  livecheck do
+    url "https://www.archimatetool.com/download/"
+    regex(/id=["']?download["']?[^>]*data-version=["']?(\d+(?:\.\d+)*)["']?/i)
+  end
 
   app "Archi/Archi.app"
 
