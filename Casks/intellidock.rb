0467e25384f1ed7d363fc0372db0af61c313edc6
Reno <25192197+singularitti@users.noreply.github.com>
2021-01-07 00:47:38 -0500

    Add new cask "intellidock" v1.0 (#97295)

---
+++ Casks/intellidock.rb
@@ -0,0 +1,14 @@
+cask "intellidock" do
+  version "1.0"
+  sha256 "592ce456ddf29f3464c3dcdc530d86be92cadc72afd471d968f0f6fd07125e94"
+
+  url "https://mightymac.app/intellidock/download/IntelliDock-#{version}.dmg"
+  appcast "https://mightymac.app/intellidock/download/appcast.xml"
+  name "IntelliDock"
+  desc "Hides the Dock when it is overlapped by a window"
+  homepage "https://mightymac.app/intellidock/"
+
+  app "IntelliDock.app"
+
+  zap trash: "~/Library/Preferences/app.mightymac.IntelliDock.plist"
+end
