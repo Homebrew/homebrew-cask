535c2f873895816566b361751b42a327563ed46c
Markus Reiter <me@reitermark.us>
2021-01-07 14:47:10 +0100

    vorta: add livecheck

--- Casks/vorta.rb
+++ Casks/vorta.rb
@@ -3,10 +3,14 @@ cask "vorta" do
   sha256 "3a6a5ea8d85dc8815cacfdf6a5591d0d811c4cb2320c08f89cb7d9de1261b3fa"
 
   url "https://github.com/borgbase/vorta/releases/download/v#{version}/vorta-#{version}.dmg"
-  appcast "https://github.com/borgbase/vorta/releases.atom"
   name "Vorta"
   desc "Desktop Backup Client for Borg"
   homepage "https://github.com/borgbase/vorta"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   auto_updates true
   depends_on macos: ">= :mojave"
