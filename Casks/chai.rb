218d3ac2b0168badf4ee9f76763561743b7779df
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:04 +0100

    chai: add livecheck

--- Casks/chai.rb
+++ Casks/chai.rb
@@ -3,10 +3,14 @@ cask "chai" do
   sha256 "ee2565632e77243efd7d5ffa36f8cc4b479ff0295e2565a15b059c2b7c13485b"
 
   url "https://github.com/lvillani/chai/releases/download/v#{version}/Chai-v#{version}.zip"
-  appcast "https://github.com/lvillani/chai/releases.atom"
   name "Chai"
   desc "Utility to prevent the system from going to sleep"
   homepage "https://github.com/lvillani/chai"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   depends_on macos: ">= :sierra"
 
