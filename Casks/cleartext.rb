3014f786c2f8400a151813590eb621ab04139381
Markus Reiter <me@reitermark.us>
2021-01-07 14:45:07 +0100

    cleartext: add livecheck

--- Casks/cleartext.rb
+++ Casks/cleartext.rb
@@ -3,10 +3,14 @@ cask "cleartext" do
   sha256 "4b3d44a0666c2bf2dd838c0f190e9a5678b2dc4d76e83d70fe091295d4cabdbc"
 
   url "https://github.com/mortenjust/cleartext-mac/releases/download/#{version}/Cleartext.zip"
-  appcast "https://github.com/mortenjust/cleartext-mac/releases.atom"
   name "Cleartext"
   desc "Text editor"
   homepage "https://github.com/mortenjust/cleartext-mac"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "Cleartext.app"
 end
