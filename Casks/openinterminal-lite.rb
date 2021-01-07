ecd2cdfced504124062782c0ff8cbbd5028f5ae6
Markus Reiter <me@reitermark.us>
2021-01-07 14:46:24 +0100

    openinterminal-lite: add livecheck

--- Casks/openinterminal-lite.rb
+++ Casks/openinterminal-lite.rb
@@ -3,9 +3,13 @@ cask "openinterminal-lite" do
   sha256 "561f78a6e8fde5b48b2aee41390b227fa5fe01b6c7530cd7cb0a21d010849296"
 
   url "https://github.com/Ji4n1ng/OpenInTerminal/releases/download/#{version}/OpenInTerminal-Lite.app.zip"
-  appcast "https://github.com/Ji4n1ng/OpenInTerminal/releases.atom"
   name "OpenInTerminal-Lite"
   homepage "https://github.com/Ji4n1ng/OpenInTerminal"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   app "OpenInTerminal-Lite.app"
 end
