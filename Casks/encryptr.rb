43b873a01b5ca6ea0cb522bd6d8742c2d702ae5c
Jeffrey C <spaz926@users.noreply.github.com>
2021-01-08 17:27:55 -0600

    Update encryptr from 2.0.0 to 2.1.0 (#97510)
    * Update encryptr from 2.0.0 to 2.1.0
    
    * Update encryptr.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/encryptr.rb
+++ Casks/encryptr.rb
@@ -1,10 +1,11 @@
 cask "encryptr" do
-  version "2.0.0"
-  sha256 "54e2ef8f8c4d22a20793b68fd35c65f8af34ffd783c48140b84c1a9b445589ea"
+  version "2.1.0"
+  sha256 :no_check
 
-  url "https://spideroak.com/dist/encryptr/signed/osx/Encryptr-v#{version}-mac.dmg"
+  url "https://spideroak.com/release/Encryptr/osx"
   appcast "https://github.com/SpiderOak/Encryptr/releases.atom"
   name "SpiderOak Encryptr"
+  desc "Zero-knowledge cloud-based password manager"
   homepage "https://spideroak.com/encryptr/"
 
   app "Encryptr.app"
