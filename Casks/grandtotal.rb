8d8d725c7710d4821c2e054bf6121c0d29ee7265
Jeffrey C <spaz926@users.noreply.github.com>
2021-01-08 02:09:10 -0600

    Update grandtotal from 6.2.2 to 7.0.2 (#97508)

--- Casks/grandtotal.rb
+++ Casks/grandtotal.rb
@@ -1,6 +1,6 @@
 cask "grandtotal" do
-  version "6.2.2"
-  sha256 "3c0eb1f362c3ce93acdceea00570037e53acbcb52b41a27920d868fe9f9bd42a"
+  version "7.0.2"
+  sha256 "9f1672edb01cad0b6ca349c93e81743bd1e1cfe33e74509c8634802e6f25110d"
 
   url "https://mediaatelier.com/GrandTotal#{version.major}/GrandTotal_#{version}.zip"
   appcast "https://mediaatelier.com/GrandTotal#{version.major}/feed.php"
