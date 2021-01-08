0a882007306fc2dea8f955505feed25351c1fc20
Markus Reiter <me@reitermark.us>
2021-01-08 01:36:20 +0100

    owncloud: add livecheck (#97454)

--- Casks/owncloud.rb
+++ Casks/owncloud.rb
@@ -3,10 +3,15 @@ cask "owncloud" do
   sha256 "07b9c8398d305375c8fe7f551c0b9d280933e92a63aed3b4b4b928ecbb71adbb"
 
   url "https://download.owncloud.com/desktop/ownCloud/stable/#{version}/mac/ownCloud-#{version}.pkg"
-  appcast "https://github.com/owncloud/client/releases.atom"
   name "ownCloud"
   desc "Desktop syncing client for ownCloud"
   homepage "https://owncloud.com/"
+
+  livecheck do
+    url "https://owncloud.com/desktop-app/"
+    strategy :page_match
+    regex(%r{href=.*?/owncloud-(\d+(?:\.\d+)*)\.pkg}i)
+  end
 
   depends_on macos: ">= :sierra"
 
