9fc888873f3041933f0a5d9319b0f18ede1c4f18
moosoy <moosoy@gmail.com>
2021-01-08 18:14:44 +0900

    Update movist-pro from 2.6.3,167 to 2.6.4,169 (#97320)
    * Update movist-pro from 2.6.3,167 to 2.6.3_1,168
    
    * Update movist-pro.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/movist-pro.rb
+++ Casks/movist-pro.rb
@@ -1,8 +1,8 @@
 cask "movist-pro" do
-  version "2.6.3,167"
-  sha256 "0715d43b095abf7358264334b53a14e3444874e3eaf4c0beae0576ab1c51a284"
+  version "2.6.4,169"
+  sha256 "60faf636f55e01a5343a6b458a063140ff6c0f27ab0729266208be8a43758cfd"
 
-  url "https://s3.ap-northeast-2.amazonaws.com/update.cocoable.com/MovistPro_#{version.before_comma}.app.zip",
+  url "https://s3.ap-northeast-2.amazonaws.com/update.cocoable.com/MovistPro_#{version.before_comma}.dmg",
       verified: "s3.ap-northeast-2.amazonaws.com/update.cocoable.com/"
   name "Movist Pro"
   desc "Media player"
