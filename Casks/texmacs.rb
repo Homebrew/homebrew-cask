20187910ec27f002a16e17927a03ea48cec1d6e1
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-09 06:24:38 +0800

    Update texmacs.rb

--- Casks/texmacs.rb
+++ Casks/texmacs.rb
@@ -2,13 +2,13 @@ cask "texmacs" do
   version "1.99.18"
   sha256 "8e1bd0470fa6ea824cf077df1d08567f1fe05a207c94b00fac3bbbafeacf0403"
 
-  url "https://ftp.texmacs.org/TeXmacs/tmftp/macos/TeXmacs-#{version}.dmg"
+  url "http://ftp.texmacs.org/TeXmacs/tmftp/macos/TeXmacs-#{version}.dmg"
   name "GNU TeXmacs"
   desc "Scientific editing platform"
-  homepage "https://www.texmacs.org/"
+  homepage "http://www.texmacs.org/"
 
   livecheck do
-    url "https://ftp.texmacs.org/TeXmacs/appcast/macos.xml"
+    url "http://ftp.texmacs.org/TeXmacs/appcast/macos.xml"
     strategy :sparkle
   end
 
