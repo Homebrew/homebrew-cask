c6ebc1adcf66d16f44509be877985d4fbe052ef3
Markus Reiter <me@reitermark.us>
2021-01-08 01:22:30 +0100

    mediathekview: add livecheck (#97451)

--- Casks/mediathekview.rb
+++ Casks/mediathekview.rb
@@ -3,10 +3,15 @@ cask "mediathekview" do
   sha256 "fe20d561665841660af30d331a420325c4dbc809dec5c3c305b3f35f7dd1b36a"
 
   url "https://download.mediathekview.de/stabil/MediathekView-#{version}-mac.dmg"
-  appcast "https://mediathekview.de/changelog/index.xml"
   name "MediathekView"
   desc "Manages online multimedia libs of German, Austrian and Swiss public broadcasters"
   homepage "https://mediathekview.de/"
+
+  livecheck do
+    url "https://download.mediathekview.de/stabil/"
+    strategy :page_match
+    regex(%r{href=.*?/MediathekView-(\d+(?:\.\d+)*)-mac\.dmg}i)
+  end
 
   app "MediathekView.app"
 end
