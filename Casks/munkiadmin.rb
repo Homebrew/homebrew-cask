e91eed2a1d26ae265736a01a68faed2072e8d69c
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-08 13:32:11 +0800

    Update munkiadmin.rb

--- Casks/munkiadmin.rb
+++ Casks/munkiadmin.rb
@@ -2,10 +2,9 @@ cask "munkiadmin" do
   version "1.7.1"
   sha256 "0eb86e33162db7ee5c76c1f16b826481c2ec2002153479e738043f59c51c59b1"
 
-  url "https://github.com/hjuutilainen/munkiadmin/releases/download/v#{version.major_minor_patch}/MunkiAdmin-#{version.major_minor_patch}.dmg",
+  url "https://github.com/hjuutilainen/munkiadmin/releases/download/v#{version}/MunkiAdmin-#{version}.dmg",
       verified: "github.com/hjuutilainen/munkiadmin/"
-  appcast "https://github.com/hjuutilainen/munkiadmin/releases.atom",
-          must_contain: version.major_minor_patch
+  appcast "https://github.com/hjuutilainen/munkiadmin/releases.atom"
   name "MunkiAdmin"
   desc "Tool to manage Munki repositories"
   homepage "https://hjuutilainen.github.io/munkiadmin/"
