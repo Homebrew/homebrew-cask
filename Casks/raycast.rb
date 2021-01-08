770f670ee8791935304420f9d68fc67594f023f0
Matthew Gleich <email@mattglei.ch>
2021-01-08 09:52:31 -0500

    ✨ Add raycast cask

---
+++ Casks/raycast.rb
@@ -0,0 +1,11 @@
+cask "raycast" do
+  version "v1.5.1"
+  sha256 :no_check
+
+  url "https://api.raycast.app/v2/download"
+  name "Raycast"
+  desc "Control your tools with a few keystrokes. It’s designed to keep you focused"
+  homepage "https://raycast.app/"
+
+  app "Raycast.app"
+end
