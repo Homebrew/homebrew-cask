0ab16cf557a7780b0052891540307aaccdec1296
rohansatapathy <61920401+rohansatapathy@users.noreply.github.com>
2021-01-08 16:12:12 -0600

    Upgrade adobe-connect from 11,2020.10.3 to 11,2021.1.2 (#97543)

--- Casks/adobe-connect.rb
+++ Casks/adobe-connect.rb
@@ -1,8 +1,8 @@
 cask "adobe-connect" do
-  version "11,2020.10.3"
-  sha256 "958e7ebd3faf561a305b57cc0f8d2056a8c574023ae1bfa359889d2e8e772ff5"
+  version "11,2021.1.2"
+  sha256 "efa1cab4f61cec4eb0dda0360e088a0e6fe0f921136299d17e4d2313c1612471"
 
-  url "https://download.adobe.com/pub/connect/updaters/meeting/#{version.before_comma.dots_to_underscores}/AdobeConnect_#{version.after_comma}.dmg"
+  url "https://download.adobe.com/pub/connect/updaters/meeting/#{version.before_comma}/AdobeConnect_#{version.after_comma.dots_to_underscores}.dmg"
   name "Adobe Connect"
   homepage "https://www.adobe.com/products/adobeconnect.html"
 
