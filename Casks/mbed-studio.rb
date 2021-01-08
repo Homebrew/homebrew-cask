4f894dda997de8f24e9f46611279194ae3d2c1f4
Xo Wang <xo@geekshavefeelings.com>
2021-01-07 22:41:55 -0800

    Fix pkg and add sha256 for mbed-studio (#97261)

--- Casks/mbed-studio.rb
+++ Casks/mbed-studio.rb
@@ -1,12 +1,12 @@
 cask "mbed-studio" do
   version "1.3.1.3"
-  sha256 :no_check
+  sha256 "cc4561b6ce367ed9fa330dcc3a88469a662c90c9be43b6e6dbecd430cf0bfea2"
 
   url "https://studio.mbed.com/installers/latest/mac/MbedStudio.pkg"
   name "Mbed Studio"
   homepage "https://os.mbed.com/studio/"
 
-  pkg "MbedStudio.pkg"
+  pkg "MbedStudio-#{version.major_minor_patch}.pkg"
 
   uninstall pkgutil: "com.arm.mbed.studio"
 
