4f15df64d41d403d0dca31e1f3606d71c2e134c9
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-08 16:51:18 +0800

    Update missive.rb

--- Casks/missive.rb
+++ Casks/missive.rb
@@ -1,6 +1,6 @@
 cask "missive" do
   version "9.44.1"
-  sha256 "fdce6198d29dda74565d9edebb4b6183b92539862ead53c7901feb840cbe278b"
+  sha256 "1939edf270b963360e4c950862cc7b119b1d7ed8514c85b6a08a23ea22860bca"
 
   url "https://d1weoh55n7oehu.cloudfront.net/#{version}/Missive-#{version}.dmg",
       verified: "d1weoh55n7oehu.cloudfront.net/"
