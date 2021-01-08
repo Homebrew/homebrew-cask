5a3ca226343a3d21f445dda73269a73e204de724
Dominik Moritz <domoritz@gmail.com>
2021-01-08 20:32:25 +0100

    Miniforge support ARM (#97538)
    * Miniforge support ARM
    
    * move installer script inside conditional
    
    * simplify formula with ternary in template
    
    * Revert "simplify formula with ternary in template"
    
    This reverts commit 073cf7db3991cedb446f460f381b1c1e32129a83.

--- Casks/miniforge.rb
+++ Casks/miniforge.rb
@@ -1,8 +1,24 @@
 cask "miniforge" do
   version "4.9.2-5"
-  sha256 "a33aca4dd6686b1dba790b224c9e686d7e08c86e6074379194f3bde478d883ed"
 
-  url "https://github.com/conda-forge/miniforge/releases/download/#{version}/Miniforge3-#{version}-MacOSX-x86_64.sh"
+  if Hardware::CPU.intel?
+    sha256 "a33aca4dd6686b1dba790b224c9e686d7e08c86e6074379194f3bde478d883ed"
+    url "https://github.com/conda-forge/miniforge/releases/download/#{version}/Miniforge3-#{version}-MacOSX-x86_64.sh"
+
+    installer script: {
+      executable: "Miniforge3-#{version}-MacOSX-x86_64.sh",
+      args:       ["-b", "-p", "#{caskroom_path}/base"],
+    }
+  else
+    sha256 "c73d84a842a0b56ee546e6d180515ed660394eb22ce61864f88bc3ddd0b034c7"
+    url "https://github.com/conda-forge/miniforge/releases/download/#{version}/Miniforge3-#{version}-MacOSX-arm64.sh"
+
+    installer script: {
+      executable: "Miniforge3-#{version}-MacOSX-arm64.sh",
+      args:       ["-b", "-p", "#{caskroom_path}/base"],
+    }
+  end
+
   appcast "https://github.com/conda-forge/miniforge/releases.atom"
   name "miniforge"
   desc "Minimal installer for conda specific to conda-forge"
@@ -12,10 +28,6 @@ cask "miniforge" do
   conflicts_with cask: "miniconda"
   container type: :naked
 
-  installer script: {
-    executable: "Miniforge3-#{version}-MacOSX-x86_64.sh",
-    args:       ["-b", "-p", "#{caskroom_path}/base"],
-  }
   binary "#{caskroom_path}/base/condabin/conda"
 
   uninstall delete: "#{caskroom_path}/base"
