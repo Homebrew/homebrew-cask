87204ee518dfc550bcc4122dfb51dfb531fd43d9
Jeffrey C <spaz926@me.com>
2021-01-08 17:14:25 -0600

    add missing sparkle version

--- Casks/ammonite.rb
+++ Casks/ammonite.rb
@@ -1,5 +1,5 @@
 cask "ammonite" do
-  version "1.21"
+  version "1.21,508"
   sha256 "bce4d85f8c93d1d9eaf6a61b82f2d94e20263f278c2dd17a96f5f85fd4af06aa"
 
   url "https://www.soma-zone.com/download/files/Ammonite-#{version.before_comma}.tar.bz2"
