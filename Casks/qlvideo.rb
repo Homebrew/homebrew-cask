781f7521abf64dca1ba2d117f57bbd4eb6c1b94a
Markus Reiter <me@reitermark.us>
2021-01-08 01:59:04 +0100

    qlvideo: add livecheck (#97457)

--- Casks/qlvideo.rb
+++ Casks/qlvideo.rb
@@ -3,9 +3,16 @@ cask "qlvideo" do
   sha256 "a0bce00cc2073fe065722409d32605cc2c20eaab89981e2d965d571d84fd77e0"
 
   url "https://github.com/Marginal/QLVideo/releases/download/rel-#{version.no_dots}/QLVideo_#{version.no_dots}.pkg"
-  appcast "https://github.com/Marginal/QLVideo/releases.atom"
   name "QuickLook Video"
   homepage "https://github.com/Marginal/QLVideo"
+
+  livecheck do
+    url "https://github.com/Marginal/QLVideo/releases/latest"
+    strategy :page_match do |page|
+      match = page.match(%r{href=.*?/QLVideo_(\d+?)(\d+)\.pkg}i)
+      "#{match[1]}.#{match[2]}"
+    end
+  end
 
   pkg "QLVideo_#{version.no_dots}.pkg"
 
