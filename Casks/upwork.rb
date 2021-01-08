3095864570eb01e88be20a4fb411dfd2e91b4b1a
Markus Reiter <me@reitermark.us>
2021-01-08 02:27:57 +0100

    upwork: add livecheck (#97469)

--- Casks/upwork.rb
+++ Casks/upwork.rb
@@ -3,9 +3,16 @@ cask "upwork" do
   sha256 "6548f23af1006de6172c319d57eb458121b8b838e96079beeefde6e6a5eff788"
 
   url "https://updates-desktopapp.upwork.com/binaries/v#{version.before_comma.dots_to_underscores}_#{version.after_comma}/Upwork.dmg"
-  appcast "https://www.macupdater.net/cgi-bin/extract_text/extract_text_split_easy.cgi?url=https://updates-desktopapp.upwork.com/binaries/versions-mac.json&splitter_1=Beta&index_1=0"
   name "Upwork"
   homepage "https://www.upwork.com/"
+
+  livecheck do
+    url "https://updates-desktopapp.upwork.com/binaries/versions-mac.json"
+    strategy :page_match do |page|
+      match = page.match(%r{/v(\d+(?:_\d+)*)_([^/]+)/Upwork\.dmg}i)
+      "#{match[1].tr("_", ".")},#{match[2]}"
+    end
+  end
 
   app "Upwork.app"
 
