1ead99ee3d23b4da15d21fdf31717de818da9796
Jeffrey C <spaz926@users.noreply.github.com>
2021-01-08 02:28:53 -0600

    Update ajour from 0.6.0 to 0.6.1 (#97509)

--- Casks/ajour.rb
+++ Casks/ajour.rb
@@ -1,6 +1,6 @@
 cask "ajour" do
-  version "0.6.0"
-  sha256 "75e438111075b553fdcc5e0ec7d7349360a4ce7d4187e82866d5424dbb37e14a"
+  version "0.6.1"
+  sha256 "7b796acbd86e4c3bcdef3a12e0316dd251d379496981ee410a052c7ffe18579c"
 
   url "https://github.com/casperstorm/ajour/releases/download/#{version}/ajour.dmg"
   appcast "https://github.com/casperstorm/ajour/releases.atom"
