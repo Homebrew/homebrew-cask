aa732eb83f1a2277ee24c2b4bec2868db5376cbc
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-09 06:16:37 +0800

    Update encryptr.rb

--- Casks/encryptr.rb
+++ Casks/encryptr.rb
@@ -1,8 +1,8 @@
 cask "encryptr" do
   version "2.1.0"
-  sha256 "7b0613067aa2cd078327564273d7b907df09417dc20bb7d26d83974d487cf163"
+  sha256 :no_check
 
-  url "https://spideroak.com/dist/encryptr/signed/osx/Encryptr-v#{version}-osx.dmg"
+  url "https://spideroak.com/release/Encryptr/osx"
   appcast "https://github.com/SpiderOak/Encryptr/releases.atom"
   name "SpiderOak Encryptr"
   desc "Zero-knowledge cloud-based password manager"
