4e2580aec6a3eef95cd70004571db2fe0f0f3121
Markus Reiter <me@reitermark.us>
2021-01-08 01:25:06 +0100

    Update missive.rb

--- Casks/missive.rb
+++ Casks/missive.rb
@@ -1,5 +1,5 @@
 cask "missive" do
-  version "9.44.0"
+  version "9.44.1"
   sha256 "fdce6198d29dda74565d9edebb4b6183b92539862ead53c7901feb840cbe278b"
 
   url "https://d1weoh55n7oehu.cloudfront.net/#{version}/Missive-#{version}.dmg",
