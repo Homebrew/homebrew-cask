ffc907742f8fcec91151603c4ab7906a2ae552ab
Markus Reiter <me@reitermark.us>
2021-01-08 02:13:51 +0100

    stubbymanager: add livecheck (#97462)

--- Casks/stubbymanager.rb
+++ Casks/stubbymanager.rb
@@ -3,9 +3,17 @@ cask "stubbymanager" do
   sha256 "0b9de130ac55e02928a21f334ac5c481b092e6535445a402caa7bb165b0b3dba"
 
   url "https://dnsprivacy.org/wiki/download/attachments/#{version.after_comma}/StubbyManager.pkg"
-  appcast "https://github.com/Sinodun/stubby_manager_gui/releases.atom"
   name "StubbyManager"
   homepage "https://dnsprivacy.org/wiki/display/DP/Stubby+GUI+for+macOS"
+
+  livecheck do
+    url "https://dnsprivacy.org/wiki/display/DP/Stubby+GUI+for+macOS"
+    strategy :page_match do |page|
+      v = page[/stubby\s+version:\s*(\d+(?:\.\d+)*)/i, 1]
+      id = page[%r{href=.*?/(\d+)/StubbyManager\.pkg}i, 1]
+      "#{v},#{id}"
+    end
+  end
 
   pkg "StubbyManager.pkg"
 
