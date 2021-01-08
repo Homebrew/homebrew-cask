49abfe6f57ba90d2382bfdd6699c7090075f25c8
Jeffrey C <spaz926@users.noreply.github.com>
2021-01-08 17:49:22 -0600

    Update elpass from 1.2.3,330 to 1.3.1,338 (#97555)

--- Casks/elpass.rb
+++ Casks/elpass.rb
@@ -1,6 +1,6 @@
 cask "elpass" do
-  version "1.2.3,330"
-  sha256 "25251036d6961aad54852f54f3606467903834153b68252b4264d9e6f2da3778"
+  version "1.3.1,338"
+  sha256 "6e7160fb4e8129ddee4619c0c7fe754bb98cd781288f71fac4d6123da7e24ac9"
 
   url "https://elpass.app/macos/Elpass-#{version.before_comma}-#{version.after_comma}.zip"
   name "Elpass"
