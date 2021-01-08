32d8f86473fe91197e0f2139d56fc8ee5cb28c53
Markus Reiter <me@reitermark.us>
2021-01-08 15:08:25 +0100

    Fix pkg in mbed-studio (#97520)

--- Casks/mbed-studio.rb
+++ Casks/mbed-studio.rb
@@ -1,12 +1,17 @@
 cask "mbed-studio" do
-  version "1.3.1.3"
+  version "1.3.1"
   sha256 :no_check
 
   url "https://studio.mbed.com/installers/latest/mac/MbedStudio.pkg"
   name "Mbed Studio"
   homepage "https://os.mbed.com/studio/"
 
-  pkg "MbedStudio.pkg"
+  livecheck do
+    url :url
+    strategy :header_match
+  end
+
+  pkg "MbedStudio-#{version}.pkg"
 
   uninstall pkgutil: "com.arm.mbed.studio"
 
