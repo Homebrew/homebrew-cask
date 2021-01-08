869b5b2db94ce3edf256871d35f0482ff8631437
Markus Reiter <me@reitermark.us>
2021-01-08 02:09:47 +0100

    stoplight-studio: add livecheck (#97461)

--- Casks/stoplight-studio.rb
+++ Casks/stoplight-studio.rb
@@ -4,10 +4,17 @@ cask "stoplight-studio" do
 
   url "https://github.com/stoplightio/studio/releases/download/v#{version.before_comma}-stable.#{version.after_comma}/stoplight-studio-mac.dmg",
       verified: "github.com/stoplightio/studio/"
-  appcast "https://github.com/stoplightio/studio/releases.atom"
   name "Stoplight Studio"
   desc "Editor for designing and documenting APIs"
   homepage "https://stoplight.io/studio/"
+
+  livecheck do
+    url "https://github.com/stoplightio/studio/releases/latest"
+    strategy :page_match do |page|
+      match = page.match(%r{href=.*?/v?(\d+(?:\.\d+)*)-stable\.([^/]+)/stoplight-studio-mac\.dmg}i)
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   app "Stoplight Studio.app"
 end
