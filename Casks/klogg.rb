5266887c99f4ff15f24e068daaed8bc7870878e5
Markus Reiter <me@reitermark.us>
2021-01-08 03:54:00 +0100

    klogg: add livecheck (#97444)

--- Casks/klogg.rb
+++ Casks/klogg.rb
@@ -3,10 +3,15 @@ cask "klogg" do
   sha256 "a1814e017aed78d3a75071e5416563c10628035d949783537acd176b62783e1e"
 
   url "https://github.com/variar/klogg/releases/download/v#{version.major_minor}/klogg-#{version}-OSX.dmg"
-  appcast "https://github.com/variar/klogg/releases.atom"
   name "Klogg"
   desc "Fast, advanced log explorer"
   homepage "https://github.com/variar/klogg"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+    regex(%r{href=.*?/klogg-(\d+(?:\.\d+)*)-OSX\.dmg}i)
+  end
 
   app "klogg.app"
 end
