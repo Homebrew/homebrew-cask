28f4d732c8a2b8deeb848af48838b7d478e485f1
Jeffrey C <spaz926@users.noreply.github.com>
2021-01-08 03:26:06 -0600

    Update twine from 2.3.9 to 2.3.10 (#97517)

--- Casks/twine.rb
+++ Casks/twine.rb
@@ -1,6 +1,6 @@
 cask "twine" do
-  version "2.3.9"
-  sha256 "b6afb15ff3f496f88a42e2f268d11875df4d206376cc851f3911bd41ddc58ec5"
+  version "2.3.10"
+  sha256 "411d4e8621a39d3dd16858f497908d473755d207a495b28c277153d3842f8d5f"
 
   url "https://github.com/klembot/twinejs/releases/download/#{version}/twine_#{version}_macos.dmg",
       verified: "github.com/klembot/twinejs/"
