bb82649b11255e8a8b7d640ee873053547c99095
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-08 13:43:39 +0800

    Update valentina-studio.rb

--- Casks/valentina-studio.rb
+++ Casks/valentina-studio.rb
@@ -5,7 +5,7 @@ cask "valentina-studio" do
   url "https://valentina-db.com/en/all-downloads/vstudio/current/vstudio_mac_x64-dmg?format=raw"
   name "Valentina Studio"
   desc "Visual editors for data"
-  homepage "https://www.valentina-db.net/en/valentina-studio-overview"
+  homepage "https://valentina-db.com/en/valentina-studio-overview"
 
   app "Valentina Studio.app"
 end
