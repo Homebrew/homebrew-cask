ca6ed8956957db7cb6645b026458292214afc14c
Markus Reiter <me@reitermark.us>
2021-01-08 01:49:13 +0100

    xamarin-mac: add livecheck

--- Casks/xamarin-mac.rb
+++ Casks/xamarin-mac.rb
@@ -1,11 +1,16 @@
 cask "xamarin-mac" do
-  version "7.0.0.15"
-  sha256 "cd528ba6226763768b741584f5ff6495e361f092dfa56306b4169162eb32b5c1"
+  version "7.3.0.27"
+  sha256 "0958c05aaba2b30f162a1b1941905e5bd3afd6d4fdd6a054490402ea0416dfa5"
 
   url "https://dl.xamarin.com/XamarinforMac/Mac/xamarin.mac-#{version}.pkg"
-  appcast "https://github.com/xamarin/xamarin-macios/releases.atom"
   name "Xamarin Mac"
   homepage "https://www.xamarin.com/platform"
+
+  livecheck do
+    url "https://github.com/xamarin/xamarin-macios"
+    strategy :git
+    regex(/^xamarin-mac-(\d+(?:\.\d+)*)$/i)
+  end
 
   depends_on cask: "homebrew/cask-versions/mono-mdk-for-visual-studio"
 
