68416e5e70ea7daeafde0c6ce9e13cf9ab03faae
Dominik Moritz <domoritz@gmail.com>
2021-01-08 18:37:58 +0100

    Miniforge support ARM

--- Casks/miniforge.rb
+++ Casks/miniforge.rb
@@ -1,8 +1,14 @@
 cask "miniforge" do
   version "4.9.2-5"
-  sha256 "a33aca4dd6686b1dba790b224c9e686d7e08c86e6074379194f3bde478d883ed"
 
-  url "https://github.com/conda-forge/miniforge/releases/download/#{version}/Miniforge3-#{version}-MacOSX-x86_64.sh"
+  if Hardware::CPU.intel?
+    sha256 "a33aca4dd6686b1dba790b224c9e686d7e08c86e6074379194f3bde478d883ed"
+    url "https://github.com/conda-forge/miniforge/releases/download/#{version}/Miniforge3-#{version}-MacOSX-x86_64.sh"
+  else
+    sha256 "c73d84a842a0b56ee546e6d180515ed660394eb22ce61864f88bc3ddd0b034c7"
+    url "https://github.com/conda-forge/miniforge/releases/download/#{version}/Miniforge3-#{version}-MacOSX-arm64.sh"
+  end
+
   appcast "https://github.com/conda-forge/miniforge/releases.atom"
   name "miniforge"
   desc "Minimal installer for conda specific to conda-forge"
