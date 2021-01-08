b2f3dd95ecd0ae1213b9aebcddc83d5970fff8c0
Markus Reiter <me@reitermark.us>
2021-01-08 02:49:00 +0100

    webplotdigitizer: add livecheck (#97473)

--- Casks/webplotdigitizer.rb
+++ Casks/webplotdigitizer.rb
@@ -3,10 +3,15 @@ cask "webplotdigitizer" do
   sha256 "e9ee0ba2b317b81205593a7d1688a165ba10806465308c505e0534eb28e9d0c3"
 
   url "https://automeris.io/downloads/WebPlotDigitizer-#{version}-darwin-x64.zip"
-  appcast "https://github.com/ankitrohatgi/WebPlotDigitizer/releases.atom"
   name "WebPlotDigitizer"
   desc "Extract numerical data from plot images"
   homepage "https://automeris.io/WebPlotDigitizer/"
+
+  livecheck do
+    url "https://automeris.io/WebPlotDigitizer/download.html"
+    strategy :page_match
+    regex(%r{href=.*?/WebPlotDigitizer-(\d+(?:\.\d+)*)-darwin-x64\.zip}i)
+  end
 
   app "WebPlotDigitizer-#{version}-darwin-x64/WebPlotDigitizer-#{version}.app"
 end
