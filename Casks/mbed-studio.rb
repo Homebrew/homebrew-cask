4b63eff2e2de1f443e96fab02c3609cc05677991
Mike McQuaid <mike@mikemcquaid.com>
2021-01-08 09:38:43 +0000

    Revert "Fix pkg and add sha256 for mbed-studio (#97261)" (#97518)
    This reverts commit e78ff9891daeb5e89796b52326316769fc3ac172.

--- Casks/mbed-studio.rb
+++ Casks/mbed-studio.rb
@@ -1,12 +1,12 @@
 cask "mbed-studio" do
   version "1.3.1.3"
-  sha256 "cc4561b6ce367ed9fa330dcc3a88469a662c90c9be43b6e6dbecd430cf0bfea2"
+  sha256 :no_check
 
   url "https://studio.mbed.com/installers/latest/mac/MbedStudio.pkg"
   name "Mbed Studio"
   homepage "https://os.mbed.com/studio/"
 
-  pkg "MbedStudio-#{version.major_minor_patch}.pkg"
+  pkg "MbedStudio.pkg"
 
   uninstall pkgutil: "com.arm.mbed.studio"
 
