44b312011597a64e831d78cf809ff5f561202ee7
Markus Reiter <me@reitermark.us>
2021-01-08 02:18:05 +0100

    toolreleases: add livecheck (#97466)

--- Casks/toolreleases.rb
+++ Casks/toolreleases.rb
@@ -3,10 +3,17 @@ cask "toolreleases" do
   sha256 "53494264a42439c66183d9cec05d18e8e243986e3efbfd6dee937011e942a167"
 
   url "https://github.com/DeveloperMaris/ToolReleases/releases/download/v#{version.before_comma}/ToolReleases_v#{version.before_comma}.b#{version.after_comma}.zip"
-  appcast "https://github.com/DeveloperMaris/ToolReleases/releases.atom"
   name "ToolReleases"
   desc "Utility to notify about the latest Apple tool releases (including Beta releases)"
   homepage "https://github.com/DeveloperMaris/ToolReleases"
+
+  livecheck do
+    url "https://github.com/DeveloperMaris/ToolReleases/releases/latest"
+    strategy :page_match do |page|
+      match = page.match(%r{href=.*?/ToolReleases_v?(\d+(?:\.\d+)*)\.b(\d+)\.zip}i)
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   depends_on macos: ">= :catalina"
 
