551c750246bb94919c713841ae6a3cfd35a66437
Markus Reiter <me@reitermark.us>
2021-01-08 01:53:35 +0100

    mattermost: add livecheck (#97449)

--- Casks/mattermost.rb
+++ Casks/mattermost.rb
@@ -3,10 +3,15 @@ cask "mattermost" do
   sha256 "26bddc75d7a3d7507b6f9bdcbc6d06195c61acdab3924125c390deb579a99a8b"
 
   url "https://releases.mattermost.com/desktop/#{version}/mattermost-desktop-#{version}-mac.zip"
-  appcast "https://github.com/mattermost/desktop/releases.atom"
   name "Mattermost"
   desc "Open-source, self-hosted Slack-alternative"
   homepage "https://about.mattermost.com/"
+
+  livecheck do
+    url "https://docs.mattermost.com/install/desktop.html#macos-10-9"
+    strategy :page_match
+    regex(%r{href=.*?/mattermost-desktop-(\d+(?:\.\d+)*)-mac\.dmg}i)
+  end
 
   app "Mattermost.app"
 end
