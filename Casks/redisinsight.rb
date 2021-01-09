a70ed90b4a98422f52759265dc6533a37f8d5be7
Zachary Liu <zach@zliu.io>
2021-01-09 00:11:05 -0500

    Update redisinsight from 1.8.0 to 1.8.3 (#97570)

--- Casks/redisinsight.rb
+++ Casks/redisinsight.rb
@@ -1,6 +1,6 @@
 cask "redisinsight" do
-  version "1.8.0"
-  sha256 "c1675342b848d41357afd5a436b1660a3cb1faa160f2fd5b9ef829c7db9aa83a"
+  version "1.8.3"
+  sha256 "2afd454e19eaa1b1949d2154e7f28ec35c4a090e44bdd06bec8758fecd180a94"
 
   url "https://downloads.redisinsight.redislabs.com/#{version}/redisinsight-mac.dmg"
   appcast "https://docs.redislabs.com/latest/ri/release-notes/",
