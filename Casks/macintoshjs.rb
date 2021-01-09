a83236ec407a89f5b798759a6f3804febc3e02ef
Markus Reiter <me@reitermark.us>
2021-01-09 16:25:24 +0100

    Update macintoshjs.rb

--- Casks/macintoshjs.rb
+++ Casks/macintoshjs.rb
@@ -2,11 +2,13 @@ cask "macintoshjs" do
   version "1.1.0"
 
   if Hardware::CPU.intel?
-    url "https://github.com/felixrieseberg/macintosh.js/releases/download/v#{version}/macintosh.js-darwin-x64-#{version}.zip"
     sha256 "4ca41517f15c594e718da622074a2160754cb8f1293cb1fad908f6d0ae585384"
+
+    url "https://github.com/felixrieseberg/macintosh.js/releases/download/v#{version}/macintosh.js-darwin-x64-#{version}.zip"
   else
-    url "https://github.com/felixrieseberg/macintosh.js/releases/download/v#{version}/macintosh.js-darwin-arm64-#{version}.zip"
     sha256 "0af95bbcc075f939d6c3e1fdcdab0d8da2760e5546aecfbc82767e326640740c"
+
+    url "https://github.com/felixrieseberg/macintosh.js/releases/download/v#{version}/macintosh.js-darwin-arm64-#{version}.zip"
   end
 
   appcast "https://github.com/felixrieseberg/macintosh.js/releases.atom"
