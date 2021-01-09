1018b921d9a22ee7c7b26f3a49403295b13cfeab
Jeffrey C <spaz926@users.noreply.github.com>
2021-01-09 15:35:53 -0600

    clover-configurator: add desc, auto_updates, and zap procedure (#97592)

--- Casks/clover-configurator.rb
+++ Casks/clover-configurator.rb
@@ -5,7 +5,15 @@ cask "clover-configurator" do
   url "https://mackie100projects.altervista.org/apps/cloverconf/download-new-build.php?version=global"
   appcast "https://mackie100projects.altervista.org/download-clover-configurator/"
   name "Clover Configurator"
+  desc "Clover EFI bootloader configuration helper"
   homepage "https://mackie100projects.altervista.org/clover-configurator/"
 
+  auto_updates true
+
   app "Clover Configurator.app"
+
+  zap trash: [
+    "~/Library/Caches/org.altervista.mackie100projects.Clover-Configurator",
+    "~/Library/Preferences/org.altervista.mackie100projects.Clover-Configurator.plist",
+  ]
 end
