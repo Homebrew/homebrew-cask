fe894773455bcea2d2c6c16c00aa47f89263d19d
Markus Reiter <me@reitermark.us>
2021-01-09 18:40:06 +0100

    time-tracker: add livecheck (#97644)

--- Casks/time-tracker.rb
+++ Casks/time-tracker.rb
@@ -3,9 +3,14 @@ cask "time-tracker" do
   sha256 "a6c982ee82f8babcf0f42d49950e5b3a1d613946736a7ee4a272b05916271aeb"
 
   url "https://github.com/rburgst/time-tracker-mac/releases/download/v#{version}-binary/Time.Tracker-#{version}-bin.zip"
-  appcast "https://github.com/rburgst/time-tracker-mac/releases.atom"
   name "TimeTracker"
   homepage "https://github.com/rburgst/time-tracker-mac"
+
+  livecheck do
+    url :url
+    strategy :git
+    regex(/^v?(\d+(?:\.\d+)*)-binary$/i)
+  end
 
   app "Time Tracker.app"
 end
