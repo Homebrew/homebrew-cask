8b8561898ae33aa93672ac4a606394ecfd34727a
Markus Reiter <me@reitermark.us>
2021-01-09 16:11:36 +0100

    camera-live: add livecheck (#97601)

--- Casks/camera-live.rb
+++ Casks/camera-live.rb
@@ -3,10 +3,15 @@ cask "camera-live" do
   sha256 "4c7a6ecdbec677a6fbbb90af427e54b0d429278c87b49966c6448ce065c78e75"
 
   url "https://github.com/v002/v002-Camera-Live/releases/download/#{version}/Camera.Live.zip"
-  appcast "https://github.com/v002/v002-Camera-Live/releases.atom"
   name "Camera Live"
   desc "Live Syphon Camera"
   homepage "https://github.com/v002/v002-Camera-Live"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+    regex(%r{href=.*?/(\d+(?:\.\d+)*)/Camera\.Live\.zip}i)
+  end
 
   app "Camera Live.app"
 
