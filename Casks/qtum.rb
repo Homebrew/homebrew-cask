aa0a730fc6e0c1c80d194f6974ff19bd2bd98357
Markus Reiter <me@reitermark.us>
2021-01-09 17:48:30 +0100

    Update qtum.rb

--- Casks/qtum.rb
+++ Casks/qtum.rb
@@ -2,7 +2,7 @@ cask "qtum" do
   version "0.20.1"
   sha256 "cf909f0292020fd140011e79f22894639e1bf013ed83021a55b9ba8324ddbc33"
 
-  url "https://github.com/qtumproject/qtum/releases/download/mainnet-ignition-v#{version.major_minor_patch}/qtum-#{version}-osx-unsigned.dmg",
+  url "https://github.com/qtumproject/qtum/releases/download/mainnet-ignition-v#{version}/qtum-#{version}-osx-unsigned.dmg",
       verified: "github.com/qtumproject/qtum/"
   name "Qtum"
   desc "Cryptocurrency wallet"
