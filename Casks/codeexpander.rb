871a9740237481c2301345fccf57c858be6efea9
Markus Reiter <me@reitermark.us>
2021-01-09 16:12:00 +0100

    codeexpander: add livecheck (#97603)

--- Casks/codeexpander.rb
+++ Casks/codeexpander.rb
@@ -3,10 +3,14 @@ cask "codeexpander" do
   sha256 "8f62f78502da7196e3505f7db195d4fe0f1272629a098e8fa9cbb45fdb18dc80"
 
   url "https://github.com/oncework/codeexpander/releases/download/#{version.major_minor}.x/CodeExpander-#{version}.dmg"
-  appcast "https://github.com/oncework/codeexpander/releases.atom",
-          must_contain: version.major_minor
   name "CodeExpander"
   homepage "https://github.com/oncework/codeexpander"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+    regex(%r{href=.*?/CodeExpander-(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "CodeExpander.app"
 end
