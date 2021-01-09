360b719321ee6e74a651d572368e8ec645f2281b
Jeffrey C <spaz926@users.noreply.github.com>
2021-01-08 20:06:22 -0600

    Update ammonite from 1.20.1,487 to 1.21,508 (#97547)
    * Update ammonite from 1.20.1,487 to 1.21
    
    * add missing sparkle version

--- Casks/ammonite.rb
+++ Casks/ammonite.rb
@@ -1,6 +1,6 @@
 cask "ammonite" do
-  version "1.20.1,487"
-  sha256 "7727b45c4cd6dd624e5ca834fe03d17b27233721565f8ff1c6b703cac28770a7"
+  version "1.21,508"
+  sha256 "bce4d85f8c93d1d9eaf6a61b82f2d94e20263f278c2dd17a96f5f85fd4af06aa"
 
   url "https://www.soma-zone.com/download/files/Ammonite-#{version.before_comma}.tar.bz2"
   name "Ammonite"
