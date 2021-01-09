49ace81903268d61fe33afe95878f641858543fd
Jeffrey C <spaz926@users.noreply.github.com>
2021-01-09 01:56:01 -0600

    Update epichrome from 2.3.22 to 2.3.23 (#97573)

--- Casks/epichrome.rb
+++ Casks/epichrome.rb
@@ -1,6 +1,6 @@
 cask "epichrome" do
-  version "2.3.22"
-  sha256 "c987272abb89703c37ae8edbc830871668ef8c4ff8fe27e163d7ccf357eb0547"
+  version "2.3.23"
+  sha256 "53d99d00a7d79b961ddb0dc3be654b45e5befee96c4adabbe677beb944b6788f"
 
   url "https://github.com/dmarmor/epichrome/releases/download/v#{version}/epichrome-#{version}.pkg"
   appcast "https://github.com/dmarmor/epichrome/releases.atom"
