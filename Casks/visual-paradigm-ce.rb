35c99298b4d6696e9b24c99e1f4448e9e2b5b0d3
Jeffrey C <spaz926@me.com>
2021-01-08 20:00:50 -0600

    visual-paradigm-ce: add livecheck

--- Casks/visual-paradigm-ce.rb
+++ Casks/visual-paradigm-ce.rb
@@ -3,10 +3,16 @@ cask "visual-paradigm-ce" do
   sha256 "3a5abf036e364136ac18251d987bc0caf7239d36cf8da9f548b46f55afa8f72e"
 
   url "https://www.visual-paradigm.com/downloads/vpce/Visual_Paradigm_CE_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
-  appcast "https://www.visual-paradigm.com/downloads/vpce/checksum.html",
-          must_contain: "#{version.before_comma.dots_to_underscores}_#{version.after_comma}"
   name "Visual Paradigm Community Edition"
   homepage "https://www.visual-paradigm.com/"
+
+  livecheck do
+    url "https://www.visual-paradigm.com/downloads/vpce/checksum.html"
+    strategy :header_match do |headers|
+      match = headers["location"].match(%r{/vpce(\d+(?:\.\d+)*)/(\d+)/checksum\.html}i)
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   # Renamed to avoid conflict with visual-paradigm.
   app "Visual Paradigm.app", target: "Visual Paradigm CE.app"
