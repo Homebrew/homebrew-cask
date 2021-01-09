856bb8aae2a914de24ba4850ed09d87fcba09361
Markus Reiter <me@reitermark.us>
2021-01-09 18:36:17 +0100

    xit: add livecheck (#97655)

--- Casks/xit.rb
+++ Casks/xit.rb
@@ -3,10 +3,15 @@ cask "xit" do
   sha256 "92003bfb419c462f832ff143721cbc416017ebf53abe44276180e4f4b8a6c4fb"
 
   url "https://github.com/Uncommon/Xit/releases/download/#{version}/Xit.zip"
-  appcast "https://github.com/Uncommon/Xit/releases.atom"
   name "Xit"
   desc "GUI for the git version control system"
   homepage "https://github.com/Uncommon/Xit"
+
+  livecheck do
+    url :url
+    strategy :git
+    regex(/^v?(\d+(?:\.\d+)*(?:b\d+)?)$/i)
+  end
 
   depends_on macos: ">= :mojave"
 
