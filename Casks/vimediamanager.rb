2c8e12e51a5b1061a90776dadba39c782ed757bc
Markus Reiter <me@reitermark.us>
2021-01-09 18:20:57 +0100

    vimediamanager: add livecheck (#97649)

--- Casks/vimediamanager.rb
+++ Casks/vimediamanager.rb
@@ -3,9 +3,14 @@ cask "vimediamanager" do
   sha256 "dbb251822ba45a7a81847e863b092fcd06a811b184b39f17ad0754662d327b4b"
 
   url "https://github.com/vidalvanbergen/ViMediaManager/releases/download/v#{version}/ViMediaManager.dmg"
-  appcast "https://github.com/vidalvanbergen/ViMediaManager/releases.atom"
   name "ViMediaManager"
   homepage "https://github.com/vidalvanbergen/ViMediaManager"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+    regex(%r{href=.*?/v?(\d+(?:\.\d+)*[a-z]\d+)/ViMediaManager\.dmg}i)
+  end
 
   app "ViMediaManager.app"
 
