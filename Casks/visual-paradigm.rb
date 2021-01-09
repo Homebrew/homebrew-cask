01374af094e5eaaa116f4f46545f2b81938924d2
Jeffrey C <spaz926@me.com>
2021-01-08 19:30:39 -0600

    visual-paradigm: update url scheme

--- Casks/visual-paradigm.rb
+++ Casks/visual-paradigm.rb
@@ -2,7 +2,7 @@ cask "visual-paradigm" do
   version "16.2,20210101"
   sha256 "667ab269575783f16e37b167407dbac43c33087fa035b7ba24b6b97bdedc0ec8"
 
-  url "https://eu8.dl.visual-paradigm.com/visual-paradigm/vp#{version.before_comma}/#{version.after_comma}/Visual_Paradigm_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
+  url "https://www.visual-paradigm.com/downloads/vp#{version.before_comma}/#{version.after_comma}/Visual_Paradigm_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
   name "Visual Paradigm"
   desc "UML CASE Tool supporting UML 2, SysML and Business Process Modeling Notation"
   homepage "https://www.visual-paradigm.com/"
