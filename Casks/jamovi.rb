c512202364bc1574e31553e351866546537962ab
Markus Reiter <me@reitermark.us>
2021-01-10 18:41:38 +0100

    jamovi: add livecheck (#97770)

--- Casks/jamovi.rb
+++ Casks/jamovi.rb
@@ -1,13 +1,17 @@
 cask "jamovi" do
-  version "1.2.27.0"
-  sha256 "3c1621f407849a1bd0d64c5eda424010debdc5307669ae1ed5dfd46c905ba84d"
+  version "1.6.9.0"
+  sha256 "5db4cc272c14df652ab240c0d769c64714588e5aed10ac3331c882a93de9e64c"
 
   url "https://www.jamovi.org/downloads/jamovi-#{version}-macos.dmg"
-  appcast "https://www.jamovi.org/download.html",
-          must_contain: "solid</td><td>.dmg</td><td>#{version.chomp(".0")}"
   name "jamovi"
   desc "Free and open statistical software"
   homepage "https://www.jamovi.org/"
+
+  livecheck do
+    url "https://www.jamovi.org/download.html"
+    strategy :page_match
+    regex(%r{href=.*?/jamovi-(\d+(?:\.\d+)*)-macos\.dmg}i)
+  end
 
   auto_updates true
 
