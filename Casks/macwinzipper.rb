b13cf77066667f4c94468f70b3f684a6fb6936d7
Markus Reiter <me@reitermark.us>
2021-01-10 19:10:50 +0100

    macwinzipper: add livecheck (#97784)

--- Casks/macwinzipper.rb
+++ Casks/macwinzipper.rb
@@ -3,10 +3,15 @@ cask "macwinzipper" do
   sha256 "37ff612125d80acf39a873e087a301c524708bb0c4af64be55a335555af23d62"
 
   url "https://tida.co.jp/files/MacWinZipper-#{version}.dmg"
-  appcast "https://tida.co.jp/macwinzipper"
   name "MacWinZipper"
   desc "Zip archiver"
   homepage "https://tida.co.jp/macwinzipper"
+
+  livecheck do
+    url "https://tida.co.jp/macwinzipper"
+    strategy :page_match
+    regex(%r{href=.*?/MacWinZipper-(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "MacWinZipper.app"
 end
