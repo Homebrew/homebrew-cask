ed324042220bc76f1150ed6ea26a2e0d1cbcb52c
Jeffrey C <spaz926@users.noreply.github.com>
2021-01-09 22:30:32 -0600

    Update loom from 0.63.1 to 0.64.3 (#97672)

--- Casks/loom.rb
+++ Casks/loom.rb
@@ -1,6 +1,6 @@
 cask "loom" do
-  version "0.63.1"
-  sha256 "fa113de088b4d13ea74aa4aba80ded604cfdc7d05c8a5a2d7353b0dcd105238e"
+  version "0.64.3"
+  sha256 "32c4e33644425d28a2f2ab6e3192d287dbb1a73d7ea95690e7855131736fbdde"
 
   url "https://cdn.loom.com/desktop-packages/Loom-#{version}.dmg"
   appcast "https://s3-us-west-2.amazonaws.com/loom.desktop.packages/loom-inc-production/desktop-packages/latest-mac.yml"
