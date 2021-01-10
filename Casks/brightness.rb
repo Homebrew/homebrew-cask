9cc753970b088785bfc47462a46d3f72711966e6
Markus Reiter <me@reitermark.us>
2021-01-10 15:38:07 +0100

    brightness: add livecheck

--- Casks/brightness.rb
+++ Casks/brightness.rb
@@ -2,10 +2,15 @@ cask "brightness" do
   version "1.1.2"
   sha256 "1bb6e6a993ee5c64ba27c28f96160ed64c5136835f73f2f7fe9f553b90f4a90c"
 
-  url "http://www.bergdesign.com/resources/Brightness/Brightness_#{version}.dmg"
-  appcast "http://bergdesign.com/brightness/"
+  url "http://bergdesign.com/resources/Brightness/Brightness_#{version}.dmg"
   name "Brightness"
   homepage "http://bergdesign.com/brightness/"
+
+  livecheck do
+    url "http://bergdesign.com/brightness/"
+    strategy :page_match
+    regex(%r{href=.*?/Brightness_(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "Brightness.app"
 end
