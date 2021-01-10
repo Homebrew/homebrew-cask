96c9543bd9729c5252a8b469ca1384e18ab6a1cd
Markus Reiter <me@reitermark.us>
2021-01-10 18:40:38 +0100

    1password-cli: add livecheck (#97686)

--- Casks/1password-cli.rb
+++ Casks/1password-cli.rb
@@ -4,10 +4,15 @@ cask "1password-cli" do
 
   url "https://cache.agilebits.com/dist/1P/op/pkg/v#{version}/op_darwin_amd64_v#{version}.pkg",
       verified: "cache.agilebits.com/dist/1P/op/pkg/"
-  appcast "https://app-updates.agilebits.com/product_history/CLI"
   name "1Password CLI"
   desc "Command-line helper for the 1Password password manager"
   homepage "https://support.1password.com/command-line/"
+
+  livecheck do
+    url "https://app-updates.agilebits.com/product_history/CLI"
+    strategy :page_match
+    regex(%r{href=.*?/op_darwin_amd64_v?(\d+(?:\.\d+)*)\.pkg}i)
+  end
 
   pkg "op_darwin_amd64_v#{version}.pkg"
 
