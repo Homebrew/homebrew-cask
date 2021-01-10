ef921d1b2981bbbb185f89c96f927ad715e98918
Markus Reiter <me@reitermark.us>
2021-01-10 21:16:24 +0100

    qr-journal: add livecheck (#97841)
    * qr-journal: add livecheck
    
    * Update qr-journal.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/qr-journal.rb
+++ Casks/qr-journal.rb
@@ -3,10 +3,15 @@ cask "qr-journal" do
   sha256 "af8bcebf795de65955b3cbee1bd4f9568343e982461cd16ae827808b8f7e50ac"
 
   url "https://www.joshjacob.com/mac-development/QRJournal#{version}.dmg"
-  appcast "https://www.joshjacob.com/mac-development/qrjournal.php"
   name "QR Journal"
   desc "Allows users with an iSight (or compatible) camera to read QR codes"
   homepage "https://www.joshjacob.com/mac-development/qrjournal.php"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(%r{href=.*?/QRJournal(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "QR Journal.app"
 
