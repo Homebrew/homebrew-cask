7028aa0d3a8bdb60167a691104d834356bacb87a
Markus Reiter <me@reitermark.us>
2021-01-10 19:12:52 +0100

    kstars: add livecheck (#97778)

--- Casks/kstars.rb
+++ Casks/kstars.rb
@@ -1,11 +1,17 @@
 cask "kstars" do
-  version "3.5.0"
-  sha256 "9c848145ae76aa07f5feb102911a0be07e08642935a60ee3c7bd3fcc39698a71"
+  version "3.5.1"
+  sha256 "c324d6e81e15f1480a8ab06d60cc4aa854366d4f044a064450796bc5fa9275c6"
 
   url "https://www.indilib.org/jdownloads/kstars/kstars-#{version}.dmg",
       verified: "indilib.org/jdownloads/kstars/"
   name "KStars"
   homepage "https://edu.kde.org/kstars/"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(%r{href=.*?/kstars-(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "kstars.app"
 end
