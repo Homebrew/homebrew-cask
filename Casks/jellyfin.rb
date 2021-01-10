fe1c82209e1b2b1ae38310699f3490b3d2648448
Markus Reiter <me@reitermark.us>
2021-01-10 18:41:17 +0100

    jellyfin: add livecheck (#97771)

--- Casks/jellyfin.rb
+++ Casks/jellyfin.rb
@@ -3,9 +3,14 @@ cask "jellyfin" do
   sha256 "0ddfda5c16930202adc719cce89703ee5c27c2490fb36b59fe513188245ccffa"
 
   url "https://repo.jellyfin.org/releases/server/macos/stable/installer/Jellyfin_#{version}.dmg"
-  appcast "https://repo.jellyfin.org/releases/server/macos/stable/"
   name "Jellyfin"
   homepage "https://jellyfin.org/"
+
+  livecheck do
+    url "https://repo.jellyfin.org/releases/server/macos/stable/"
+    strategy :page_match
+    regex(%r{href=.*?/Jellyfin_(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "Jellyfin.app"
 end
