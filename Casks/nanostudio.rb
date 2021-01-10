a9846e1ab9d3fbb843db558a32985c1900f772c1
Markus Reiter <me@reitermark.us>
2021-01-10 19:48:53 +0100

    nanostudio: add livecheck (#97807)

--- Casks/nanostudio.rb
+++ Casks/nanostudio.rb
@@ -3,9 +3,14 @@ cask "nanostudio" do
   sha256 "70407ffc54bf5ed2e3c8ec8f1a980739077199a828b8b9d28a2bae04c298856a"
 
   url "https://www.blipinteractive.co.uk/downloads/NanoStudio-#{version}.dmg.zip"
-  appcast "https://www.blipinteractive.co.uk/nanostudio1/"
   name "NanoStudio"
   homepage "https://www.blipinteractive.co.uk/nanostudio1/"
+
+  livecheck do
+    url "https://www.blipinteractive.co.uk/nanostudio1/"
+    strategy :page_match
+    regex(%r{href=.*?/NanoStudio-(\d+(?:\.\d+)*)\.dmg\.zip}i)
+  end
 
   app "NanoStudio.app"
   app "NanoStudio-Phone.app"
