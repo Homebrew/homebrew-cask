d435aabf5836f6fe2176a525f3848eab18910e3f
Markus Reiter <me@reitermark.us>
2021-01-10 19:36:24 +0100

    cernbox: add livecheck (#97701)

--- Casks/cernbox.rb
+++ Casks/cernbox.rb
@@ -3,10 +3,15 @@ cask "cernbox" do
   sha256 "a85533f2e40fa6a408d7b97b1f41b42ff2fc3c4a7af404972823ebe54bca6354"
 
   url "https://cernbox.cern.ch/cernbox/doc/MacOSX/cernbox-#{version}.pkg"
-  appcast "https://cernbox.web.cern.ch/cernbox/downloads/"
   name "CERNBox Client"
   desc "Cloud storage for CERN users"
   homepage "https://cernbox.web.cern.ch/cernbox/"
+
+  livecheck do
+    url "https://cernbox.web.cern.ch/cernbox/downloads/"
+    strategy :page_match
+    regex(%r{href=.*?/cernbox-(\d+(?:\.\d+)*)\.pkg}i)
+  end
 
   pkg "cernbox-#{version}.pkg"
 
