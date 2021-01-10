3e8f1ec94f28ebbc9254e7c123812f4c78528fc4
Markus Reiter <me@reitermark.us>
2021-01-10 19:21:34 +0100

    max: add livecheck (#97790)

--- Casks/max.rb
+++ Casks/max.rb
@@ -2,7 +2,6 @@ cask "max" do
   if MacOS.version <= :mojave
     version "0.9.1"
     sha256 "722bf714696d3d39329ba98ffddc9f117f8cc6863f71670507cd12f62a5e5f14"
-    appcast "https://sbooth.org/Max/appcast.xml"
 
     url "https://files.sbooth.org/Max-#{version}.tar.bz2"
 
@@ -19,6 +18,12 @@ cask "max" do
   name "Max"
   desc "Creats high-quality audio files in various formats, from CDs or files"
   homepage "https://sbooth.org/Max/"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(%r{href=.*?/Max-(\d+(?:\.\d+)*b\d+)\.dmg}i)
+  end
 
   zap trash: "~/Library/Preferences/org.sbooth.Max.plist"
 end
