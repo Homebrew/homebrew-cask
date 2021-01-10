41f6d24bd5afc2d1cf29b903fe8096a16e44a4bd
Markus Reiter <me@reitermark.us>
2021-01-10 18:56:26 +0100

    kinza: add livecheck (#97774)

--- Casks/kinza.rb
+++ Casks/kinza.rb
@@ -1,12 +1,17 @@
 cask "kinza" do
-  version "5.0.1"
-  sha256 "2bbcd7c6312e024fe7e316e58757e5119d9897180f29c088e2958ee446926853"
+  version "6.7.2"
+  sha256 "2e10435fc51f5e14d3e016edd7c8fe77598197f378c2d57a397e96e2273ef859"
 
   url "https://d7s9ygw7nxr46.cloudfront.net/mac/kinza_#{version}.dmg",
       verified: "d7s9ygw7nxr46.cloudfront.net/"
-  appcast "https://www.kinza.jp/en/download/thankyou/mac/"
   name "Kinza"
   homepage "https://www.kinza.jp/en/"
+
+  livecheck do
+    url "https://www.kinza.jp/en/download/thankyou/mac/"
+    strategy :page_match
+    regex(%r{href=.*?/kinza_(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "Kinza.app"
 end
