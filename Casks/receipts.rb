9c8838ee0447f7dd48001af08872e08109b49ff3
Markus Reiter <me@reitermark.us>
2021-01-10 20:41:07 +0100

    receipts: add livecheck (#97845)

--- Casks/receipts.rb
+++ Casks/receipts.rb
@@ -3,9 +3,14 @@ cask "receipts" do
   sha256 "e1895c47301107edb505d458a2d8a07503757b374c687576c583744e85dc0ce4"
 
   url "https://www.receipts-app.com/update/download/Receipts-#{version}.zip"
-  appcast "https://www.receipts-app.com/updater.php"
   name "Receipts"
   homepage "https://www.receipts-app.com/"
+
+  livecheck do
+    url "https://www.receipts-app.com/updater.php"
+    strategy :page_match
+    regex(%r{href=.*?/Receipts-(\d+(?:\.\d+)*-\d+)\.zip}i)
+  end
 
   depends_on macos: ">= :yosemite"
 
