2c512ef0c2b8b103d2340fc5f5030f8aa2ed403b
Markus Reiter <me@reitermark.us>
2021-01-10 19:38:01 +0100

    birdfont: add livecheck (#97696)
    * birdfont: add livecheck
    
    * Update birdfont.rb

--- Casks/birdfont.rb
+++ Casks/birdfont.rb
@@ -7,11 +7,16 @@ cask "birdfont" do
     sha256 "067755cd2e02a70997aac3cb29a49ab2ec1c304c30d89f58b96add0df7802c88"
   else
     version "4.8.11"
-    sha256 "067755cd2e02a70997aac3cb29a49ab2ec1c304c30d89f58b96add0df7802c88"
+    sha256 "2289fefc1ba248ed680d8448d433aa1a65ec7bfa9f26b4056652bcebfff846d0"
+
+    livecheck do
+      url "https://birdfont.org/purchase.php"
+      strategy :page_match
+      regex(%r{href=.*?/birdfont-(\d+(?:\.\d+)*)-free\.dmg}i)
+    end
   end
 
   url "https://birdfont.org/download/birdfont-#{version}-free.dmg"
-  appcast "https://birdfont.org/purchase.php"
   name "BirdFont"
   desc "Free font editor"
   homepage "https://birdfont.org/"
