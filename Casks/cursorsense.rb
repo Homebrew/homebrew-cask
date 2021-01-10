434ef984a27fd2ec4237d95f68bab2e3e0c600a0
Markus Reiter <me@reitermark.us>
2021-01-10 19:31:02 +0100

    cursorsense: add livecheck (#97713)

--- Casks/cursorsense.rb
+++ Casks/cursorsense.rb
@@ -3,9 +3,14 @@ cask "cursorsense" do
   sha256 "9eafdd458e84550c50cb695f4517cf28d8111f6388516a0c1e8990437abbf63c"
 
   url "https://plentycom.jp/ctrl/files_cs/CursorSense#{version}.dmg"
-  appcast "https://plentycom.jp/en/cursorsense/download.php"
   name "CursorSense"
   homepage "https://plentycom.jp/en/cursorsense/"
+
+  livecheck do
+    url "https://plentycom.jp/en/cursorsense/download.php"
+    strategy :page_match
+    regex(%r{href=.*?/CursorSense(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   depends_on macos: ">= :sierra"
 
