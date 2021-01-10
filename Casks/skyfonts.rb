ade57eed40b1be5f9a19a8ed09a48e1dcf220135
Markus Reiter <me@reitermark.us>
2021-01-11 00:59:28 +0100

    skyfonts: add livecheck (#97864)

--- Casks/skyfonts.rb
+++ Casks/skyfonts.rb
@@ -3,10 +3,15 @@ cask "skyfonts" do
   sha256 "bfdd1e920e0c6871909fbc9ce441d7b1eba9be04fb30af32892649b131ff05ad"
 
   url "https://cdn1.skyfonts.com/client/Monotype_SkyFonts_Mac64_#{version}.dmg"
-  appcast "https://api.skyfonts.com/api/SkyFontsAppCast?osid=3"
   name "SkyFonts"
   desc "Font manager"
   homepage "https://skyfonts.com/"
+
+  livecheck do
+    url "https://api.skyfonts.com/api/SkyFontsAppCast?osid=3"
+    strategy :page_match
+    regex(%r{href=.*?/Monotype_SkyFonts_Mac64_(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   installer manual: "Install SkyFonts.app"
 
