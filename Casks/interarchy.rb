92885e9001125d0ec8f8bcfbd7a2b1ec5941fc4f
Markus Reiter <me@reitermark.us>
2021-01-10 18:58:24 +0100

    interarchy: add livecheck (#97760)

--- Casks/interarchy.rb
+++ Casks/interarchy.rb
@@ -3,9 +3,14 @@ cask "interarchy" do
   sha256 "c1f8fbb9add4d134123d72b80d1d04540d38157f7c51478cb9efb0e88138c090"
 
   url "https://downloads.kangacode.com/Interarchy/Interarchy_#{version}.zip"
-  appcast "https://www.kangacode.com/interarchy/"
   name "Interarchy"
   homepage "https://www.kangacode.com/interarchy/"
+
+  livecheck do
+    url "https://www.kangacode.com/interarchy/"
+    strategy :page_match
+    regex(%r{href=.*?/Interarchy_(\d+(?:\.\d+)*)\.zip}i)
+  end
 
   app "Interarchy.app"
 end
