29922c777c0054a2c365a5f8d07ba563869fb416
Markus Reiter <me@reitermark.us>
2021-01-10 19:30:43 +0100

    dashcam-viewer: add livecheck (#97714)

--- Casks/dashcam-viewer.rb
+++ Casks/dashcam-viewer.rb
@@ -1,13 +1,18 @@
 cask "dashcam-viewer" do
-  version "3.6.1"
-  sha256 "ac77ef84ea30ff0294b855cfe1f74baf0d82488166552cfd1b55b48c3b3ca7f3"
+  version "3.6.3"
+  sha256 "3a4c0df1d1e19000fb74d1afc61f692995420d6984e64f8f0c1a58263d3b920d"
 
   url "https://filedn.com/l2s8TAtm4VASBX72ds0zYD8/dcv/Dashcam_Viewer_v#{version}.dmg",
       verified: "filedn.com/l2s8TAtm4VASBX72ds0zYD8/dcv/"
-  appcast "https://dashcamviewer.com/"
   name "Dashcam Viewer"
   name "Dashcam Viewer by Earthshine Software"
   homepage "https://dashcamviewer.com/"
+
+  livecheck do
+    url "https://dashcamviewer.com/"
+    strategy :page_match
+    regex(%r{href=.*?/Dashcam_Viewer_v?(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "Dashcam Viewer.app"
 end
