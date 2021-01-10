8ef5394d118a45df767b4c8e6a1d68801b4b4344
Markus Reiter <me@reitermark.us>
2021-01-11 00:59:02 +0100

    skybox: add livecheck (#97863)

--- Casks/skybox.rb
+++ Casks/skybox.rb
@@ -3,9 +3,14 @@ cask "skybox" do
   sha256 "3fde0768b9d199c13763ed26f4b24de3e88698316f2b727f317ccd4a1a45ef32"
 
   url "https://download.skybox.xyz/skybox-mac-client/SKYBOX_for_Mac_#{version}.dmg"
-  appcast "https://skybox.xyz/en/download/"
   name "SKYBOX VR Video Player"
   homepage "https://skybox.xyz/"
+
+  livecheck do
+    url "https://skybox.xyz/en/download/"
+    strategy :page_match
+    regex(%r{href=.*?/SKYBOX_for_Mac_(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "SKYBOX.app"
 
