e1a703671503bb7db1aeec1930c2cf74addf6507
Markus Reiter <me@reitermark.us>
2021-01-10 19:42:28 +0100

    mkvtools: add livecheck (#97800)

--- Casks/mkvtools.rb
+++ Casks/mkvtools.rb
@@ -2,11 +2,16 @@ cask "mkvtools" do
   version "3.7.2"
   sha256 "c7b4ab1a0031146ad76660b526e3be778eefa2d1ffaf590023c9b1d839e433e1"
 
-  url "http://www.emmgunn.com/downloads/mkvtools#{version}.zip"
-  appcast "http://www.emmgunn.com/mkvtools-home/mkvtools-downloads/"
+  url "https://www.emmgunn.com/downloads/mkvtools#{version}.zip"
   name "MKVtools"
   desc "App to create and edit MKV videos"
-  homepage "http://www.emmgunn.com/mkvtools-home/"
+  homepage "https://www.emmgunn.com/mkvtools-home/"
+
+  livecheck do
+    url "https://www.emmgunn.com/mkvtools-home/mkvtools-downloads/"
+    strategy :page_match
+    regex(%r{href=.*?/mkvtools(\d+(?:\.\d+)*)\.zip}i)
+  end
 
   app "mkvtools#{version}/MKVtools.app"
 end
