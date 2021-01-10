c12e11c90825ea0570a72e7a14481836999e21d9
Markus Reiter <me@reitermark.us>
2021-01-10 20:22:55 +0100

    publii: add livecheck (#97833)

--- Casks/publii.rb
+++ Casks/publii.rb
@@ -3,9 +3,14 @@ cask "publii" do
   sha256 "60462b5c9977ca730e12c412150adde97c94c2aea8a71c0770aa399f92015137"
 
   url "https://cdn.getpublii.com/Publii-#{version}.dmg"
-  appcast "https://getpublii.com/download/"
   name "Publii"
   homepage "https://getpublii.com/"
+
+  livecheck do
+    url "https://getpublii.com/download/"
+    strategy :page_match
+    regex(%r{href=.*?/Publii-(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "Publii.app"
 
