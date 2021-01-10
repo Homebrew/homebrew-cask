77a4dea7ae4012876d9b5fe9ea3b612ddab9fb43
Markus Reiter <me@reitermark.us>
2021-01-10 20:44:00 +0100

    revisions: add livecheck (#97847)

--- Casks/revisions.rb
+++ Casks/revisions.rb
@@ -3,9 +3,14 @@ cask "revisions" do
   sha256 "3ba2adf68b799444b0a72f37d885e205e037de48fa40ee5dfcb694f1995b4823"
 
   url "https://revisionsapp.com/downloads/revisions-#{version}.dmg"
-  appcast "https://revisionsapp.com/releases"
   name "Revisions"
   homepage "https://revisionsapp.com/"
+
+  livecheck do
+    url "https://revisionsapp.com/releases"
+    strategy :page_match
+    regex(%r{href=.*?/revisions-(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "Revisions.app"
 end
