f2b037d5cc5c1ce707e5c5fd13d4be60bd4456c3
Markus Reiter <me@reitermark.us>
2021-01-10 19:27:16 +0100

    easyfind: add livecheck (#97724)

--- Casks/easyfind.rb
+++ Casks/easyfind.rb
@@ -3,10 +3,15 @@ cask "easyfind" do
   sha256 "05cf4f3661d73953ce8c30692ad49a5d30d0f6b710212ebbc41bb381a589b713"
 
   url "https://download.devontechnologies.com/download/freeware/easyfind/#{version}/EasyFind.app.zip"
-  appcast "https://www.devontechnologies.com/apps/freeware"
   name "EasyFind"
   desc "Find files, folders, or contents in any file"
   homepage "https://www.devontechnologies.com/apps/freeware"
+
+  livecheck do
+    url "https://www.devontechnologies.com/apps/freeware"
+    strategy :page_match
+    regex(%r{href=.*?/(\d+(?:\.\d+)*)/EasyFind\.app\.zip}i)
+  end
 
   app "EasyFind.app"
 end
