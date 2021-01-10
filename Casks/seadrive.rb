580eb838ba2ca2baddb057852e4e67b500661e2f
Markus Reiter <me@reitermark.us>
2021-01-10 20:47:16 +0100

    seadrive: add livecheck

--- Casks/seadrive.rb
+++ Casks/seadrive.rb
@@ -4,10 +4,15 @@ cask "seadrive" do
 
   url "https://download.seadrive.org/seadrive-#{version}.dmg",
       verified: "download.seadrive.org/"
-  appcast "https://www.seafile.com/en/download/"
   name "Seadrive"
   desc "Manual for Seafile server"
   homepage "https://www.seafile.com/en/home/"
+
+  livecheck do
+    url "https://www.seafile.com/en/download/"
+    strategy :page_match
+    regex(%r{href=.*?/seadrive-(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "Seadrive.app"
 end
