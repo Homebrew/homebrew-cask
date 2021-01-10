b8a2f3d8b805f498ed59e9da090348dbb0d5a3ec
Markus Reiter <me@reitermark.us>
2021-01-10 19:34:45 +0100

    mixxx: add livecheck (#97798)

--- Casks/mixxx.rb
+++ Casks/mixxx.rb
@@ -3,10 +3,15 @@ cask "mixxx" do
   sha256 "4baf38b866693ea1411c988ea7c1dcfabf9635179358191743109b7a27a0d4d3"
 
   url "https://downloads.mixxx.org/mixxx-#{version}/mixxx-#{version}-osxintel.dmg"
-  appcast "https://www.mixxx.org/download/"
   name "Mixxx"
   desc "Open-source DJ software"
   homepage "https://www.mixxx.org/"
+
+  livecheck do
+    url "https://www.mixxx.org/download/"
+    strategy :page_match
+    regex(%r{href=.*?/mixxx-(\d+(?:\.\d+)*)-osxintel\.dmg}i)
+  end
 
   app "Mixxx.app"
 end
