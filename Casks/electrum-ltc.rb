56eb40867d27ffd762cb1c6e341f36cbc706e45d
Markus Reiter <me@reitermark.us>
2021-01-10 19:23:54 +0100

    electrum-ltc: add livecheck (#97730)

--- Casks/electrum-ltc.rb
+++ Casks/electrum-ltc.rb
@@ -3,9 +3,14 @@ cask "electrum-ltc" do
   sha256 "167c6d7eb21b1f064254e215adf826c6c77f22bcad8e1851645d33d1a4af65d4"
 
   url "https://electrum-ltc.org/download/electrum-ltc-#{version}.dmg"
-  appcast "https://electrum-ltc.org/"
   name "Electrum-LTC"
   homepage "https://electrum-ltc.org/"
+
+  livecheck do
+    url "https://electrum-ltc.org/"
+    strategy :page_match
+    regex(%r{href=.*?/electrum-ltc-(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "Electrum-LTC.app"
 end
