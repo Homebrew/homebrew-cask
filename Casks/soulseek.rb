06775032e71e2ef9dd0f5109a13f10168f63a248
Markus Reiter <me@reitermark.us>
2021-01-10 23:22:39 +0100

    soulseek: add livecheck

--- Casks/soulseek.rb
+++ Casks/soulseek.rb
@@ -3,9 +3,14 @@ cask "soulseek" do
   sha256 "fa147c9181bf2d263289a6c1067f7552009071d3d0bea927eca0e17f4e2ef066"
 
   url "https://www.slsknet.org/SoulseekQt/Mac/SoulseekQt-#{version}.dmg"
-  appcast "https://www.slsknet.org/news/node/1"
   name "Soulseek"
   homepage "https://www.slsknet.org/"
+
+  livecheck do
+    url "https://www.slsknet.org/news/node/1"
+    strategy :page_match
+    regex(%r{href=.*?/SoulseekQt-(\d+(?:-\d+)*)\.dmg}i)
+  end
 
   app "SoulseekQt.app"
 end
