5464730c8cd410131a76b97b5a560ec087fc388a
Markus Reiter <me@reitermark.us>
2021-01-10 20:12:30 +0100

    plex-media-server: add livecheck (#97823)

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -3,10 +3,15 @@ cask "plex-media-server" do
   sha256 "82bc7de6fbf57638f94915e93ed1f80768c07111dc94c44a0741624adcd4119b"
 
   url "https://downloads.plex.tv/plex-media-server-new/#{version}/macos/PlexMediaServer-#{version}-x86_64.zip"
-  appcast "https://plex.tv/api/downloads/5.json"
   name "Plex Media Server"
   desc "Home media server"
   homepage "https://www.plex.tv/"
+
+  livecheck do
+    url "https://plex.tv/api/downloads/5.json"
+    strategy :page_match
+    regex(%r{href=.*?/PlexMediaServer-(\d+(?:\.\d+)*-\d+c\d+adfcb\d+)-x86_64\.zip}i)
+  end
 
   auto_updates true
 
