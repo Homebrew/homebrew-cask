5b70f17173e8d3278b39fb7559b2707d65248818
Markus Reiter <me@reitermark.us>
2021-01-10 19:18:01 +0100

    miktex-console: add livecheck

--- Casks/miktex-console.rb
+++ Casks/miktex-console.rb
@@ -3,9 +3,14 @@ cask "miktex-console" do
   sha256 "6199c6a991b59074045fb55990309c71aaf38717dcdd2192c50700efca8dfc9d"
 
   url "https://miktex.org/download/ctan/systems/win32/miktex/setup/darwin-x86_64/miktex-#{version}-darwin-x86_64.dmg"
-  appcast "https://miktex.org/download"
   name "MiKTeX"
   homepage "https://miktex.org/"
+
+  livecheck do
+    url "https://miktex.org/download"
+    strategy :page_match
+    regex(%r{href=.*?/miktex-(\d+(?:\.\d+)*)-darwin-x86_64\.dmg}i)
+  end
 
   app "MiKTeX Console.app"
 end
