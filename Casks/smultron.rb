b784c16b47f6148095cdf71150f3c6a42e3deb1b
Jeffrey C <spaz926@users.noreply.github.com>
2021-01-09 20:36:00 -0600

    Update smultron from 12.2.5 to 12.2.6 (#97505)

--- Casks/smultron.rb
+++ Casks/smultron.rb
@@ -1,15 +1,19 @@
 cask "smultron" do
-  version "12.2.5,12250"
-  sha256 "4fd4a60746bf49bab5aeff124ff36e5506e71dd3ab768d72cdcd817bb0147c6e"
+  version "12.2.6"
+  sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://www.peterborgapps.com/downloads/Smultron#{version.before_comma.major}.zip"
+  url "https://www.peterborgapps.com/downloads/Smultron#{version.major}.zip"
   name "Smultron"
   desc "General-purpose text editor"
   homepage "https://www.peterborgapps.com/smultron/"
 
+  auto_updates true
   depends_on macos: ">= :mojave"
 
   app "Smultron.app"
 
-  zap trash: "~/Library/Containers/com.peterborgapps.Smultron#{version.before_comma.major}"
+  zap trash: [
+    "~/Library/Application Scripts/com.peterborgapps.Smultron#{version.major}",
+    "~/Library/Containers/com.peterborgapps.Smultron#{version.major}",
+  ]
 end
