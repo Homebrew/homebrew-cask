6d1d6b1e3d9751458f58d0e50c748df4ba2c3e0c
Markus Reiter <me@reitermark.us>
2021-01-10 18:42:43 +0100

    ivideonclient: add livecheck (#97767)

--- Casks/ivideonclient.rb
+++ Casks/ivideonclient.rb
@@ -1,13 +1,18 @@
 cask "ivideonclient" do
-  version "6.9.1"
-  sha256 "c7fc9b6794c10c9daf7696578fd51cb15341754541e84a3c2adcf5bdf578d09c"
+  version "6.10.0"
+  sha256 "08fd2d9d4228a3d004063cb477ad2390295707d09edca920409d5ce994a34939"
 
   url "https://updates.iv-cdn.com/bundles/ivideon_client/#{version}/IvideonClient_#{version}_macosx-x86-64.dmg",
       verified: "updates.iv-cdn.com/"
-  appcast "https://www.ivideon.com/downloads/"
   name "Ivideon Client"
   desc "App to watch Ivideon’s cloud-based surveillance videos"
   homepage "https://www.ivideon.com/"
+
+  livecheck do
+    url "https://www.ivideon.com/downloads/"
+    strategy :page_match
+    regex(%r{href=.*?/IvideonClient_(\d+(?:\.\d+)*)_macosx-x86-64\.dmg}i)
+  end
 
   app "IvideonClient.app"
 end
