83cedebe89d49e230ee4a734acc51cc967715cec
Markus Reiter <me@reitermark.us>
2021-01-10 20:23:21 +0100

    prosys-opc-ua-browser: add livecheck (#97832)

--- Casks/prosys-opc-ua-browser.rb
+++ Casks/prosys-opc-ua-browser.rb
@@ -3,9 +3,14 @@ cask "prosys-opc-ua-browser" do
   sha256 "ebcbd868c2ff4d14607dcb7d9db4fd26cbcedc368ac8b8ee43fff6c13a00fe0e"
 
   url "https://www.prosysopc.com/opcua/apps/UaBrowser/dist/#{version}/prosys-opc-ua-browser-macos-#{version}.dmg"
-  appcast "https://downloads.prosysopc.com/opc-ua-browser-downloads.php"
   name "Prosys OPC UA Browser"
   homepage "https://www.prosysopc.com/products/opc-ua-client/"
+
+  livecheck do
+    url "https://downloads.prosysopc.com/opc-ua-browser-downloads.php"
+    strategy :page_match
+    regex(%r{href=.*?/prosys-opc-ua-browser-macos-(\d+(?:\.\d+)*-\d+)\.dmg}i)
+  end
 
   installer script: {
     executable: "Prosys OPC UA Browser Installer.app/Contents/MacOS/JavaApplicationStub",
