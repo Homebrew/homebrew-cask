3bd8d7b7a145cac9c267452b914059ff09d0bb3a
Markus Reiter <me@reitermark.us>
2021-01-10 20:35:49 +0100

    pinegrow: add livecheck (#97821)
    * pinegrow: add livecheck
    
    * Update pinegrow.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/pinegrow.rb
+++ Casks/pinegrow.rb
@@ -3,9 +3,14 @@ cask "pinegrow" do
   sha256 "397c11293b1e9dffad52cb8f236be27a935fb7c9b8526db9d8b9ad77439d7958"
 
   url "https://download.pinegrow.com/PinegrowMac.#{version}.dmg"
-  appcast "https://pinegrow.com/"
   name "Pinegrow"
   homepage "https://pinegrow.com/"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(%r{href=.*?/PinegrowMac\.(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "Pinegrow.app"
 end
