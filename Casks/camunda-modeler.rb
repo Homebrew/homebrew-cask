f41406c612fbefb190f98103ba27d088539b5a1f
Markus Reiter <me@reitermark.us>
2021-01-10 19:37:02 +0100

    camunda-modeler: add livecheck (#97699)

--- Casks/camunda-modeler.rb
+++ Casks/camunda-modeler.rb
@@ -3,9 +3,14 @@ cask "camunda-modeler" do
   sha256 "8e2fa4c17c3c20d71655218d32a136534cb7dc66798be241a4c69c0fb241d47d"
 
   url "https://camunda.org/release/camunda-modeler/#{version}/camunda-modeler-#{version}-mac.zip"
-  appcast "https://camunda.com/download/modeler/"
   name "Camunda Modeler"
   homepage "https://camunda.org/"
+
+  livecheck do
+    url "https://camunda.com/download/modeler/"
+    strategy :page_match
+    regex(%r{href=.*?/camunda-modeler-(\d+(?:\.\d+)*)-mac\.zip}i)
+  end
 
   app "Camunda Modeler.app"
 end
