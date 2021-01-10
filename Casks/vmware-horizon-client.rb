f509e63deaf59daa97d7738707c63c78c23d14ef
silverbios <aliasadollahi@aol.com>
2021-01-10 20:16:23 +0330

    Update vmware-horizon-client from 2006-8.0.0-16531451,CART21FQ2 to 2012-8.1.0-17353466,CART21FQ4

--- Casks/vmware-horizon-client.rb
+++ Casks/vmware-horizon-client.rb
@@ -1,6 +1,6 @@
 cask "vmware-horizon-client" do
-  version "2006-8.0.0-16531451,CART21FQ2"
-  sha256 "ef63a658b2233a8490421e33ecfbafb2a05ef57f0d3810bb6912b92f117cf468"
+  version "2012-8.1.0-17353466,CART21FQ4"
+  sha256 "d098250f2754fde0603d81085841e765ae072fec6385459955373b8e26587e0a"
 
   url "https://download3.vmware.com/software/view/viewclients/#{version.after_comma}/VMware-Horizon-Client-#{version.before_comma}.dmg"
   appcast "https://my.vmware.com/channel/public/api/v1.0/dlg/details?locale=en_US&downloadGroup=CART21FQ2_MAC_800&productId=1027&rPId=48989"
