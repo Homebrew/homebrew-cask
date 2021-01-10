0056fe62bc4c8fe7861927b51981f5bb8d353982
Markus Reiter <me@reitermark.us>
2021-01-10 20:18:22 +0100

    phd2: add livecheck (#97818)

--- Casks/phd2.rb
+++ Casks/phd2.rb
@@ -3,10 +3,15 @@ cask "phd2" do
   sha256 "6c891a7251df226c9090012dc537e36e7fe03119079935fa4daca7d197ee197a"
 
   url "https://openphdguiding.org/PHD2-#{version}-OSX-64.zip"
-  appcast "https://openphdguiding.org/changelog/"
   name "PHD2"
   desc "Telescope guiding software"
   homepage "https://openphdguiding.org/"
+
+  livecheck do
+    url "https://openphdguiding.org/changelog/"
+    strategy :page_match
+    regex(%r{href=.*?/PHD2-(\d+(?:\.\d+)*)-OSX-64\.zip}i)
+  end
 
   app "PHD2.app"
 end
