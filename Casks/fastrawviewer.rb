e392e6b840745f5399e79ddf145d07702f455af9
Markus Reiter <me@reitermark.us>
2021-01-10 19:21:54 +0100

    fastrawviewer: add livecheck (#97735)

--- Casks/fastrawviewer.rb
+++ Casks/fastrawviewer.rb
@@ -3,10 +3,15 @@ cask "fastrawviewer" do
   sha256 "16b121d501e10fba89f778006b9151709cb367b74d6aab23dc100bc3107d8b7b"
 
   url "https://updates.fastrawviewer.com/data/FastRawViewer-#{version}.dmg"
-  appcast "https://www.fastrawviewer.com/download"
   name "FastRawViewer"
   desc "Opens RAW files and renders them on-the-fly"
   homepage "https://www.fastrawviewer.com/"
+
+  livecheck do
+    url "https://www.fastrawviewer.com/download"
+    strategy :page_match
+    regex(%r{href=.*?/FastRawViewer-(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "FastRawViewer.app"
 end
