0e49e85ca9b46d2ffc4f3f8289c7528656fce9a6
Markus Reiter <me@reitermark.us>
2021-01-11 01:07:00 +0100

    taskade: add livecheck (#97883)

--- Casks/taskade.rb
+++ Casks/taskade.rb
@@ -3,10 +3,15 @@ cask "taskade" do
   sha256 "c99a2e45fb0a2f36156b86e5cf2670da11d9084ba394c2bc1aed337f35bf43b4"
 
   url "https://apps.taskade.com/updates/Taskade-#{version}.dmg"
-  appcast "https://www.taskade.com/downloads"
   name "Taskade"
   desc "Task manager for teams"
   homepage "https://www.taskade.com/"
+
+  livecheck do
+    url "https://www.taskade.com/downloads"
+    strategy :page_match
+    regex(%r{href=.*?/Taskade-(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "Taskade.app"
 end
