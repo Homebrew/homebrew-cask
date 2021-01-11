b56f04dd1b940642d982f4d89294f0a7c59de0ce
Daniel Schultz <slifty@gmail.com>
2021-01-10 21:25:51 -0500

    Update dungeon-crawl-stone-soup-tiles from 0.25.1 to 0.26.0 (#97925)

--- Casks/dungeon-crawl-stone-soup-tiles.rb
+++ Casks/dungeon-crawl-stone-soup-tiles.rb
@@ -1,6 +1,6 @@
 cask "dungeon-crawl-stone-soup-tiles" do
-  version "0.25.1"
-  sha256 "c24f556de6ad767c8fbfc545dda62e57a0f965d8748f427f72e9dd5c0d4d0f3d"
+  version "0.26.0"
+  sha256 "e51680f31e896413d385b5b57997285212461800f01d0b1edb90cfb52a1d9fe2"
 
   url "https://github.com/crawl/crawl/releases/download/#{version}/dcss-#{version}-macos-tiles.zip",
       verified: "github.com/crawl/crawl/releases/"
