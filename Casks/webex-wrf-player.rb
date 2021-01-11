b832c3f664e5eb9aa911e7199776cce5de5d565a
Markus Reiter <me@reitermark.us>
2021-01-11 02:55:53 +0100

    webex-wrf-player: add livecheck (#97913)

--- Casks/webex-wrf-player.rb
+++ Casks/webex-wrf-player.rb
@@ -6,6 +6,12 @@ cask "webex-wrf-player" do
   name "WebEx Player"
   homepage "https://www.webex.com/play-webex-recording.html"
 
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(%r{href=.*?/(T\d+L)/mac/intel/webexplayer_intel\.dmg}i)
+  end
+
   pkg "WebEx Player.pkg"
 
   uninstall pkgutil: "playback.mac.webex.com"
