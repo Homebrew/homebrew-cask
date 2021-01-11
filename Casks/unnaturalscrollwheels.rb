8f1f12e39800ca96839e801018308e4e4e740cfd
Mikkel Mansa Vilhelmsen <dosqion@gmail.com>
2021-01-11 02:56:48 +0100

    Update unnaturalscrollwheels from 1.1.0 to 1.1.1 (#97921)

--- Casks/unnaturalscrollwheels.rb
+++ Casks/unnaturalscrollwheels.rb
@@ -1,6 +1,6 @@
 cask "unnaturalscrollwheels" do
-  version "1.1.0"
-  sha256 "87afed2eece015e1ebaa6857abcc2559e4dbbffda8b138ec2081366a0aa50441"
+  version "1.1.1"
+  sha256 "92b2b8d6e43e64d0d74c2222c973d3173b6bacdca4f5f55d3b52e68aee2cb0f6"
 
   url "https://github.com/ther0n/UnnaturalScrollWheels/releases/download/#{version}/UnnaturalScrollWheels-#{version}.dmg"
   appcast "https://github.com/ther0n/UnnaturalScrollWheels/releases.atom"
