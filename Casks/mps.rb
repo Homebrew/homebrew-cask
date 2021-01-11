6703a2beb77a3b44a871cb627ec3b689dba6ba7a
jcbot <jcb@leipert.io>
2021-01-11 11:08:34 +0000

    Update mps to 2020.3,203.5981.1014

--- Casks/mps.rb
+++ Casks/mps.rb
@@ -1,6 +1,6 @@
 cask "mps" do
-  version "2020.2.3,202.8194.1069"
-  sha256 "43e72604518da881c8233c3ae89be4ce71a50169bdfd0b6a01ea4375bf8583be"
+  version "2020.3,203.5981.1014"
+  sha256 "6973e0d3aab3ab2099d9512bd2a77d6e96692385f27d5d4f49b95fcfa4de279a"
 
   url "https://download.jetbrains.com/mps/#{version.before_comma.major_minor}/MPS-#{version.before_comma}-macos.dmg"
   appcast "https://data.services.jetbrains.com/products/releases?code=MPS&latest=true&type=release"
