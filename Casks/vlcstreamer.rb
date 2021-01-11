f3b5192491c16a3efc11f443781b01e22556cc12
Markus Reiter <me@reitermark.us>
2021-01-11 02:02:13 +0100

    vlcstreamer: add livecheck (#97906)

--- Casks/vlcstreamer.rb
+++ Casks/vlcstreamer.rb
@@ -3,9 +3,14 @@ cask "vlcstreamer" do
   sha256 "7f5d68a69af857804150c7d24209b38fd50a136d0cda42b0abb8626a4abe2797"
 
   url "https://hobbyistsoftware.com/Downloads/VLCStreamer/Versions/VLCStreamer_#{version}.dmg"
-  appcast "https://hobbyistsoftware.com/vlcstreamer-versions"
   name "VLC Streamer"
   homepage "https://hobbyistsoftware.com/vlcstreamer"
+
+  livecheck do
+    url "https://hobbyistsoftware.com/vlcstreamer-versions"
+    strategy :page_match
+    regex(%r{href=.*?/VLCStreamer_(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "VLCStreamer.app"
 end
