a0b5695a4c2aa28a0f03c16ecebc11562a85c52b
Markus Reiter <me@reitermark.us>
2021-01-11 01:08:44 +0100

    teeworlds: add livecheck (#97884)

--- Casks/teeworlds.rb
+++ Casks/teeworlds.rb
@@ -3,9 +3,14 @@ cask "teeworlds" do
   sha256 "a79155c8bd7a0ce08457760f1ce37d8b7611f86717659bb3d90cd0e2dae5194b"
 
   url "https://downloads.teeworlds.com/teeworlds-#{version}-osx.dmg"
-  appcast "https://teeworlds.com/?page=downloads"
   name "Teeworlds"
   homepage "https://www.teeworlds.com/"
+
+  livecheck do
+    url "https://teeworlds.com/?page=downloads"
+    strategy :page_match
+    regex(%r{href=.*?/teeworlds-(\d+(?:\.\d+)*)-osx\.dmg}i)
+  end
 
   app "Teeworlds.app"
   app "Teeworlds Server.app"
