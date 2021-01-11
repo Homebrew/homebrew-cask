6510032bc83cc05f191ece54c903ab40538ab67f
Markus Dittrich <markusdittrich1@gmail.com>
2021-01-11 17:33:32 +0100

    fix desc of Space Saver.app

--- Casks/space-saver.rb
+++ Casks/space-saver.rb
@@ -4,7 +4,7 @@ cask "space-saver" do
 
   url "https://www.mariogt.com/appsChest/SpaceSaver#{version}.zip"
   name "Space Saver"
-  desc "Space Saver is a handy macOS App (Donationware), that lets you delete the local Time Machine Backups on your Mac. This operation could liberate several Megabytes or GigaBytes of space from your main drive. You can run this App when your Mac is not connected to the Time Machine drive and is wasting precious space on Time Machine local backups."
+  desc "Lets you  delete the local Time Machine Backups"
   homepage "https://www.mariogt.com/space-saver.html"
 
   app "Space Saver.app"
