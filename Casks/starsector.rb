4e69043224d24c22ecba7f3a4f883efa3a115bb4
Markus Reiter <me@reitermark.us>
2021-01-11 01:04:06 +0100

    starsector: add livecheck (#97875)

--- Casks/starsector.rb
+++ Casks/starsector.rb
@@ -4,10 +4,15 @@ cask "starsector" do
 
   url "https://s3.amazonaws.com/fractalsoftworks/starsector/starsector_mac-#{version}.zip",
       verified: "s3.amazonaws.com/fractalsoftworks/"
-  appcast "https://fractalsoftworks.com/preorder/"
   name "Starsector"
   desc "Open-world single-player space combat and trading RPG"
   homepage "https://fractalsoftworks.com/"
+
+  livecheck do
+    url "https://fractalsoftworks.com/preorder/"
+    strategy :page_match
+    regex(%r{href=.*?/starsector_mac-(\d+(?:\.\d+)*a-RC\d+)\.zip}i)
+  end
 
   app "Starsector.app"
 end
