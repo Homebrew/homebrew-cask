52b7fafb4dd94605b7bb0b769a35339547f68cdd
Markus Reiter <me@reitermark.us>
2021-01-11 01:02:05 +0100

    spectx: add livecheck (#97870)

--- Casks/spectx.rb
+++ Casks/spectx.rb
@@ -3,10 +3,15 @@ cask "spectx" do
   sha256 "053f6e9d9c7539cd86bfbbc99787483822ee28eebe8f04dd24cd6d2ccd47575d"
 
   url "https://download.spectx.com/versions/#{version}/SpectXDesktop-v#{version}.dmg"
-  appcast "https://www.spectx.com/get-spectx"
   name "SpectX Desktop"
   desc "Parse and investigate raw log files"
   homepage "https://www.spectx.com/"
+
+  livecheck do
+    url "https://www.spectx.com/get-spectx"
+    strategy :page_match
+    regex(%r{href=.*?/SpectXDesktop-v?(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "SpectXDesktop.app"
 end
