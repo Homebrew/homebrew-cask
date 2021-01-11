66d17773bb88e329731f4b3cee7cccac5cc8b229
R0uter <R0uter@users.noreply.github.com>
2021-01-11 19:44:42 +1000

    Update loginputmac.rb
    update version

--- Casks/loginputmac.rb
+++ Casks/loginputmac.rb
@@ -1,5 +1,5 @@
 cask "loginputmac" do
-  version "2.4.2"
+  version "2.4.2,23261"
   sha256 "9a770d5685e3445bc7b9d0181ecd0b43a1a478724ba904aec48f0b8991e1a486"
 
   url "https://loginput-mac2.content-delivery.top/loginputmac#{version.major}_latest.pkg",
