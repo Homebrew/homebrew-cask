daf1ee0ed9fdb97e74b7e710f46d411fb075e1d6
Markus Reiter <me@reitermark.us>
2021-01-11 18:32:28 +0100

    Update space-saver.rb

--- Casks/space-saver.rb
+++ Casks/space-saver.rb
@@ -1,8 +1,8 @@
 cask "space-saver" do
-  version "0-5"
+  version "0.5"
   sha256 "a6f26f90aef4dd06b222d02bf47c52004b9f71af6e56258467a955fea1d9bea8"
 
-  url "https://www.mariogt.com/appsChest/SpaceSaver#{version}.zip"
+  url "https://www.mariogt.com/appsChest/SpaceSaver#{version.dots_to_hyphens}.zip"
   name "Space Saver"
   desc "Lets you  delete the local Time Machine Backups"
   homepage "https://www.mariogt.com/space-saver.html"
