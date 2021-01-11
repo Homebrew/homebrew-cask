8ead5dc0f36dbda797164db7b41a4748968056f6
Markus Reiter <me@reitermark.us>
2021-01-11 02:09:20 +0100

    vnc-viewer: add livecheck (#97908)

--- Casks/vnc-viewer.rb
+++ Casks/vnc-viewer.rb
@@ -3,10 +3,15 @@ cask "vnc-viewer" do
   sha256 "fc3956181f1ffe2908d372c4be6a8a9baeb342d6ecf39179af0d86a57dca1d14"
 
   url "https://www.realvnc.com/download/file/viewer.files/VNC-Viewer-#{version}-MacOSX-x86_64.dmg"
-  appcast "https://www.realvnc.com/en/connect/download/viewer/macos/"
   name "Real VNC Viewer"
   desc "Remote desktop application focusing on security"
   homepage "https://www.realvnc.com/"
+
+  livecheck do
+    url "https://www.realvnc.com/en/connect/download/viewer/macos/"
+    strategy :page_match
+    regex(%r{href=.*?/VNC-Viewer-(\d+(?:\.\d+)*)-MacOSX-x86_64\.dmg}i)
+  end
 
   app "VNC Viewer.app"
 
