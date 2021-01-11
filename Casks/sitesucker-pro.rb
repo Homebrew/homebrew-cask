49bceb342838cb0dbe214b49f14ffe6da252ad98
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-11 13:27:19 +0800

    Update sitesucker-pro.rb

--- Casks/sitesucker-pro.rb
+++ Casks/sitesucker-pro.rb
@@ -2,7 +2,7 @@ cask "sitesucker-pro" do
   if MacOS.version <= :mojave
     version "3.2.7"
     sha256 "dd61a113ad86b580e0faf97b4aa86290e038bb3e098f2d19e67fc9e194ce1a3e"
-  elsif MacOS.version <= :big_sur
+  else
     version "4.0.3"
     sha256 "5c5b2af9eb2a35ec116786c52eb4774e8163071a53b65c101ab54496a2a77614"
   end
