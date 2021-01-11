433d97706c1479e1c91ee434cfb91f32f4c50603
Markus Reiter <me@reitermark.us>
2021-01-11 01:08:23 +0100

    spamsieve: add livecheck (#97869)
    * spamsieve: add livecheck
    
    * Update spamsieve.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/spamsieve.rb
+++ Casks/spamsieve.rb
@@ -3,10 +3,15 @@ cask "spamsieve" do
   sha256 "9241b80bfe2687eb4cc058b5875f0ad964c79003e9490834ff1ee59206f5ed66"
 
   url "https://c-command.com/downloads/SpamSieve-#{version}.dmg"
-  appcast "https://c-command.com/spamsieve/"
   name "SpamSieve"
   desc "Spam filtering extension for e-mail clients"
   homepage "https://c-command.com/spamsieve/"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(%r{href=.*?/SpamSieve-(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "SpamSieve.app"
 end
