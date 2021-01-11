9979612ae29229c95077ec4210a75e15c2a3dfa2
Jeffrey C <spaz926@users.noreply.github.com>
2021-01-11 04:04:11 -0600

    Update lilypond from 2.20.0-1 to 2.22.0-1 (#97951)

--- Casks/lilypond.rb
+++ Casks/lilypond.rb
@@ -1,6 +1,6 @@
 cask "lilypond" do
-  version "2.20.0-1"
-  sha256 "1680ca85ff7bdb942a39bff323956357fbb0e6ab17edf9c0e145d52b6c0dc231"
+  version "2.22.0-1"
+  sha256 "154c21991c534802dbdae8b101863ec696427fbf26e0670c496cf0888dbab374"
 
   url "https://lilypond.org/downloads/binaries/darwin-x86/lilypond-#{version}.darwin-x86.tar.bz2"
   name "LilyPond"
