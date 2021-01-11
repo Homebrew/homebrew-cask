e111e166349f22538b923c847583924345b5dab4
Jeffrey C <spaz926@users.noreply.github.com>
2021-01-10 21:30:38 -0600

    Update busycontacts from 1.5.1,2021-01-06-00-26 to 1.5.1,2021-01-09-18-07 (#97929)

--- Casks/busycontacts.rb
+++ Casks/busycontacts.rb
@@ -1,6 +1,6 @@
 cask "busycontacts" do
-  version "1.5.1,2021-01-06-00-26"
-  sha256 "3cea7cc3731029dfe20263d008aa8615208f6db7a2c066d90d214cb6fee2dfb8"
+  version "1.5.1,2021-01-09-18-07"
+  sha256 "d238877ca8a7590f797f9cd6e3f1968617996ef8f0bfb9fd00196dc6544af70d"
 
   url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bct-#{version.before_comma}-#{version.after_comma}.zip",
       verified: "7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/"
