a1ba8bec4d6ac8056fe55cad76eb086affe58cb0
adithyasunil26 <adithyasunil26@gmail.com>
2021-01-13 00:44:46 +0400

    Update tropy from 1.8.1 to 1.8.2

--- Casks/tropy.rb
+++ Casks/tropy.rb
@@ -1,6 +1,6 @@
 cask "tropy" do
-  version "1.8.1"
-  sha256 "95ce861ab468f490cb9ca70810551847ef22d737563f48225b5c2cce5f06860c"
+  version "1.8.2"
+  sha256 "68780c6f482524d84f88eecf6bed8271f3f26f1c6b8bf737484d3b20c6944fae"
 
   url "https://github.com/tropy/tropy/releases/download/#{version}/tropy-#{version}.dmg",
       verified: "github.com/tropy/tropy/"
