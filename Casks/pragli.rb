e93ce1510ff07871eca8ddcec5ad745f8005922b
Connor Adams <connorads@users.noreply.github.com>
2021-01-12 18:10:12 +0000

    Add Pragli

---
+++ Casks/pragli.rb
@@ -0,0 +1,12 @@
+cask "pragli" do
+  version :latest
+  sha256 :no_check
+
+  url "https://storage.googleapis.com/always-on-cdf01.appspot.com/dist/Pragli.dmg",
+    verified: "storage.googleapis.com/always-on-cdf01.appspot.com"
+  name "Pragli"
+  desc "Virtual office for remote teams"
+  homepage "https://pragli.com/"
+
+  app "Pragli.app"
+end
