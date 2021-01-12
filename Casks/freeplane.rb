907c9b11fd8ea6949b8f02d37596476bc5c9488a
shemeshg <gili.shemesh@gmail.com>
2021-01-12 15:54:30 +0200

    Upgrade Freeplane.app to v1.8.11 (#97993)

--- Casks/freeplane.rb
+++ Casks/freeplane.rb
@@ -1,6 +1,6 @@
 cask "freeplane" do
-  version "1.8.10"
-  sha256 "bf68aa48c856c6eb188105f596d9ab28ea31abf3a99ad29fb5c5e1a6c76938bd"
+  version "1.8.11"
+  sha256 "5f558640e8811c0d4b515dfc67c4d9ef5d877dfb3b7c88a3578d97001f6b337a"
 
   url "https://downloads.sourceforge.net/freeplane/freeplane%20stable/Freeplane-#{version}.dmg",
       verified: "downloads.sourceforge.net/freeplane/"
