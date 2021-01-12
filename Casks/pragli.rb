ad2f7c5716415d2933cbcc6bfb597b7d1ec012a4
Connor Adams <connorads@users.noreply.github.com>
2021-01-12 20:28:04 +0000

    Fix style

--- Casks/pragli.rb
+++ Casks/pragli.rb
@@ -3,7 +3,7 @@ cask "pragli" do
   sha256 :no_check
 
   url "https://storage.googleapis.com/always-on-cdf01.appspot.com/dist/Pragli.dmg",
-    verified: "storage.googleapis.com/always-on-cdf01.appspot.com"
+      verified: "storage.googleapis.com/always-on-cdf01.appspot.com"
   name "Pragli"
   desc "Virtual office for remote teams"
   homepage "https://pragli.com/"
