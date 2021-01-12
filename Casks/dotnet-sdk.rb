8411538091af8937c8576d46f07d5478f90716c2
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-13 04:27:47 +0800

    Update dotnet-sdk.rb

--- Casks/dotnet-sdk.rb
+++ Casks/dotnet-sdk.rb
@@ -15,8 +15,8 @@ cask "dotnet-sdk" do
 
   conflicts_with cask: [
     "dotnet",
-    "dotnet-preview",
-    "dotnet-sdk-preview",
+    "homebrew/cask-versions/dotnet-preview",
+    "homebrew/cask-versions/dotnet-sdk-preview",
   ]
   depends_on macos: ">= :sierra"
 
