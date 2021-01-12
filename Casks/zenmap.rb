91d09be59792bca38e231e170e4228ebef40f555
Markus Reiter <me@reitermark.us>
2021-01-12 16:22:35 +0100

    Remove appcast from zenmap. (#98013)

--- Casks/zenmap.rb
+++ Casks/zenmap.rb
@@ -3,13 +3,12 @@ cask "zenmap" do
   sha256 "6446cc4d52ac3c2818f6c9cbf34473be726d3fdbb7fb8c315f0149787b7303d0"
 
   url "https://nmap.org/dist/nmap-#{version}.dmg"
-  appcast "https://nmap.org/dist/?C=M&O=D"
   name "Zenmap"
   desc "Multi-platform graphical interface for official Nmap Security Scanner"
   homepage "https://nmap.org/zenmap/"
 
   livecheck do
-    url "https://nmap.org/dist/"
+    url "https://nmap.org/dist/?C=M;O=D"
     regex(/href=.*?nmap[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
