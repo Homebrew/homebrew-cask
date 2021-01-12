fe94b3ae558ef4f8cdc54b0ad9c9c19e4bbfaa17
adithyasunil26 <adithyasunil26@gmail.com>
2021-01-12 19:22:40 +0400

    Update electerm from 1.11.1 to 1.11.5

--- Casks/electerm.rb
+++ Casks/electerm.rb
@@ -1,6 +1,6 @@
 cask "electerm" do
-  version "1.11.1"
-  sha256 "7a0567fe9f1de38b2e3ea88ab2bfc2d0c65b4ed6b16d7e0aa0791ed739f4257f"
+  version "1.11.5"
+  sha256 "6fb8f2f5c59db5bf87778b71aff7026274c6d8471c85a465a688b92438dd47d7"
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac.dmg"
   appcast "https://github.com/electerm/electerm/releases.atom"
