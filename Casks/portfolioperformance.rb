4bd818018b09441ebf77aab7b38c2a21d9c64a35
PMK <355176+PMK@users.noreply.github.com>
2021-01-12 19:32:31 +0000

    Update portfolioperformance remove BigSur binary (#98010)
    * Update portfolioperformance removed BigSur binary
    
    The bump-cask-pr script created PR#97999. I was not aware of a temporary specific binary for BigSur, but is removed since 0.50.0.
    
    * Update portfolioperformance fix style reports

--- Casks/portfolioperformance.rb
+++ Casks/portfolioperformance.rb
@@ -1,18 +1,9 @@
 cask "portfolioperformance" do
   version "0.50.0"
+  sha256 "bbaf1feb4de2a92523495b0b7e7537016087f3051015aa646fe033eb95083b8b"
 
-  if MacOS.version <= :catalina
-    sha256 "bbaf1feb4de2a92523495b0b7e7537016087f3051015aa646fe033eb95083b8b"
-
-    url "https://github.com/buchen/portfolio/releases/download/#{version}/PortfolioPerformance-#{version}.dmg",
-        verified: "github.com/buchen/portfolio/"
-  else
-    sha256 "3c3b3da036bbb44d316f8c3d39383dd339da3c8a2797d1ed1d745646c5db6b42"
-
-    url "https://github.com/buchen/portfolio/releases/download/#{version}/PortfolioPerformance-#{version}.bigsur.dmg",
-        verified: "github.com/buchen/portfolio/"
-  end
-
+  url "https://github.com/buchen/portfolio/releases/download/#{version}/PortfolioPerformance-#{version}.dmg",
+      verified: "github.com/buchen/portfolio/"
   appcast "https://github.com/buchen/portfolio/releases.atom"
   name "Portfolio Performance"
   desc "Calculate the overall performance of an investment portfolio"
