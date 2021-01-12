bfbbc2542ba9eab3bdfadaececccf4b8e7420495
adithyasunil26 <adithyasunil26@gmail.com>
2021-01-13 00:41:49 +0400

    Update tla-plus-toolbox from 1.7.0 to 1.8.0

--- Casks/tla-plus-toolbox.rb
+++ Casks/tla-plus-toolbox.rb
@@ -1,6 +1,6 @@
 cask "tla-plus-toolbox" do
-  version "1.7.0"
-  sha256 "66ee35f952fd80239cbf3c7e869bd0315234165677efc2a0f00d2a00ac1c6512"
+  version "1.8.0"
+  sha256 "48b06c0e0259323d3db8f08eeeb5097b601c4ba8109fa1be808ca6ea922b30ff"
 
   url "https://github.com/tlaplus/tlaplus/releases/download/v#{version}/TLAToolbox-#{version}-macosx.cocoa.x86_64.zip",
       verified: "github.com/tlaplus/tlaplus/"
