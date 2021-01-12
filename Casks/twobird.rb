c62397459ea85e58a83ae0755e047ce93f19e213
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-12 19:48:55 +0800

    Update twobird.rb

--- Casks/twobird.rb
+++ Casks/twobird.rb
@@ -1,8 +1,9 @@
 cask "twobird" do
-  version :latest
+  version "1.0.25"
   sha256 :no_check
 
   url "https://www.twobird.com/download/mac"
+  appcast "https://dl.twobird.com/latest-mac.yml"
   name "Twobird"
   desc "Email client with collaborative notes"
   homepage "https://www.twobird.com/"
