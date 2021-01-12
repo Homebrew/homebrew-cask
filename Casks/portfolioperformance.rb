58462055d3d5b241a6c0253469baebdad86d922d
PMK <355176+PMK@users.noreply.github.com>
2021-01-12 14:55:30 +0000

    Update portfolioperformance fix style reports

--- Casks/portfolioperformance.rb
+++ Casks/portfolioperformance.rb
@@ -1,10 +1,9 @@
 cask "portfolioperformance" do
   version "0.50.0"
-
   sha256 "bbaf1feb4de2a92523495b0b7e7537016087f3051015aa646fe033eb95083b8b"
+
   url "https://github.com/buchen/portfolio/releases/download/#{version}/PortfolioPerformance-#{version}.dmg",
       verified: "github.com/buchen/portfolio/"
-
   appcast "https://github.com/buchen/portfolio/releases.atom"
   name "Portfolio Performance"
   desc "Calculate the overall performance of an investment portfolio"
