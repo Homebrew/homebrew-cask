610ba30e2bf3de36fe07a527141ae60bb6b00a37
Kevin Ji <kevin.ji@outlook.com>
2021-01-11 21:08:34 -0800

    Create twobird.rb

---
+++ Casks/twobird.rb
@@ -0,0 +1,11 @@
+cask "twobird" do
+  version :latest
+  sha256 :no_check
+
+  url "https://www.twobird.com/download/mac"
+  name "Twobird"
+  desc "Email client with collaborative notes"
+  homepage "https://www.twobird.com/"
+
+  app "Twobird.app"
+end
