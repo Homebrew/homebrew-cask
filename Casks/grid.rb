a43c5d65e8e38ec59631f8c20f638ab1812e83e3
Jeffrey C <spaz926@me.com>
2021-01-13 03:31:44 -0600

    Update grid from 1.2 to 1.3

--- Casks/grid.rb
+++ Casks/grid.rb
@@ -1,6 +1,6 @@
 cask "grid" do
-  version "1.2"
-  sha256 "6145076e4d08a6204f874bb6a4de5319429645c09b3b91b809da3ad238ec2805"
+  version "1.3"
+  sha256 "8978205dd741130832361cae4a732a42e5de7f76a468c0cb97d8ef4fc32386da"
 
   url "https://macgrid.app/download/Grid-#{version}.dmg"
   appcast "https://macgrid.app/appcast.xml"
