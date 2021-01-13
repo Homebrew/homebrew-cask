e184111d3b9b3a209568caf10b559206f0e88123
Vítor Galvão <info@vitorgalvao.com>
2021-01-13 16:26:44 +0000

    Update helo.rb

--- Casks/helo.rb
+++ Casks/helo.rb
@@ -1,19 +1,23 @@
-cask 'helo' do
-  version '1.0.2'
-  sha256 '952d103f3e2df875c0d3d4405ae9e3a908a037e73ee78514d57e3811996670fd'
+cask "helo" do
+  version "1.3.0"
+  sha256 "728d52ca3efee4cb94a0499a887c991028b29527ae8e6c007ffeb11e0fc381af"
 
-  # helo.fra1.digitaloceanspaces.com was verified as official when first introduced to the cask
-  url "https://helo.fra1.digitaloceanspaces.com/helo/HELO-#{version}.dmg"
-  appcast 'https://helo.fra1.digitaloceanspaces.com/helo/latest-mac.yml'
-  name 'HELO'
-  homepage 'https://usehelo.com/'
+  url "https://helo.fra1.digitaloceanspaces.com/helo/HELO-#{version}.dmg",
+      verified: "helo.fra1.digitaloceanspaces.com"
+  name "HELO"
+  homepage "https://usehelo.com/"
 
-  app 'HELO.app'
+  livecheck do
+    url "https://usehelo.com/download/latest/mac"
+    strategy :header_match
+  end
+
+  app "HELO.app"
 
   zap trash: [
-               '~/Library/Application Support/HELO',
-               '~/Library/Logs/HELO',
-               '~/Library/Preferences/de.beyondco.helo.plist',
-               '~/Library/Saved Application State/de.beyondco.helo.savedState',
+               "~/Library/Application Support/HELO",
+               "~/Library/Logs/HELO",
+               "~/Library/Preferences/de.beyondco.helo.plist",
+               "~/Library/Saved Application State/de.beyondco.helo.savedState",
              ]
 end
