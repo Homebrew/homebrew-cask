7370054f4ead6baa3f57d481685e18579ae2ab37
Kevin Ji <kevin@kevinji.com>
2021-01-12 21:43:23 -0500

    Create twobird.rb (#97970)
    * Create twobird.rb
    
    * Update twobird.rb
    
    * twobird: Add auto_updates and zap stanza
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/twobird.rb
@@ -0,0 +1,20 @@
+cask "twobird" do
+  version "1.0.25"
+  sha256 :no_check
+
+  url "https://www.twobird.com/download/mac"
+  appcast "https://dl.twobird.com/latest-mac.yml"
+  name "Twobird"
+  desc "Email client with collaborative notes"
+  homepage "https://www.twobird.com/"
+
+  auto_updates true
+
+  app "Twobird.app"
+
+  zap trash: [
+    "~/Library/Application Support/Twobird",
+    "~/Library/Preferences/com.gingerlabs.bagel.plist",
+    "~/Library/Saved Application State/com.gingerlabs.bagel.savedState",
+  ]
+end
