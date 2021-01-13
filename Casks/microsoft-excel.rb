765feb35f8c29b70303965fabb84b88665faadcf
singularitti <singularitti@outlook.com>
2021-01-13 18:36:36 -0500

    Update microsoft-excel from 16.44.20121301 to 16.45.21011103

--- Casks/microsoft-excel.rb
+++ Casks/microsoft-excel.rb
@@ -1,6 +1,6 @@
 cask "microsoft-excel" do
-  version "16.44.20121301"
-  sha256 "b9c86c0c3dcb69922b7c2446780c44e308165ddde4fc053c8029a371099cb1ba"
+  version "16.45.21011103"
+  sha256 "c4e9c332f0530b08f66ea65b8dc565763a59b191a69d5f886b11a5a98e0318d8"
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Excel_#{version}_Installer.pkg",
       verified: "officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/"
