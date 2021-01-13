15830d11c3b8eda726d77a56c2816f5096fc74a0
Daniel Llewellyn <daniel@bowlhat.net>
2021-01-13 01:03:45 +0000

    Dosbox-x support ARM
    Signed-off-by: Daniel Llewellyn <daniel@bowlhat.net>

--- Casks/dosbox-x.rb
+++ Casks/dosbox-x.rb
@@ -1,9 +1,20 @@
 cask "dosbox-x" do
-  version "0.83.9,20201231203002"
-  sha256 "af55d06fb9277c62708b1b8f22bba3d3555744eab759700d0e5e4cd0d439abe5"
+  if Hardware::CPU.intel?
+    version "0.83.9,20201231203002"
 
-  url "https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v#{version.before_comma}/dosbox-x-macosx-x86_64-#{version.after_comma}.zip",
-      verified: "github.com/joncampbell123/dosbox-x/"
+    sha256 "58054333eeaa36bfeb865a8aa179291419667dc734f02a57fbc4084996c0a4cf"
+
+    url "https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v#{version.before_comma}/dosbox-x-macosx-x86_64-#{version.after_comma}.zip",
+        verified: "github.com/joncampbell123/dosbox-x/"
+  else
+    version "0.83.9,20201231202930"
+
+    sha256 "7273733029cd14f843cd4f6e1888767d80a0d493a415c20f7d307a96d6c8b133"
+
+    url "https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v#{version.before_comma}/dosbox-x-macosx-arm64-#{version.after_comma}.zip",
+        verified: "github.com/joncampbell123/dosbox-x/"
+  end
+
   name "DOSBox-X"
   desc "Fork of the DOSBox project"
   homepage "https://dosbox-x.com/"
