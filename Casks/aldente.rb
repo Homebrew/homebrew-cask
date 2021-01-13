00ed8c9475c8db28d8e276ad0728ed02326194e5
simmac <simmac@users.noreply.github.com>
2021-01-13 22:07:13 +0100

    Add AlDente
    AlDente is an open source tool to limit maximum battery charge (in order
    to increase the lifetime of the battery)

---
+++ Casks/aldente.rb
@@ -0,0 +1,16 @@
+cask "aldente" do
+  version "1.2"
+  sha256 "a588dc29faca894b7321e23420ca17d6a944b9b3b46412435f519b96e4ebee7b"
+
+  url "https://github.com/davidwernhart/AlDente/releases/download/#{version}/AlDente.app.zip"
+  appcast "https://github.com/davidwernhart/AlDente/releases.atom"
+  name "AlDente"
+  desc "Menu bar tool to limit maximum charging percentage"
+  homepage "https://github.com/davidwernhart/AlDente"
+
+  app "AlDente.app"
+
+  uninstall quit: "com.davidwernhart.Helper", delete: "/Library/PrivilegedHelperTools/com.davidwernhart.Helper"
+
+  zap trash: "/Library/LaunchDaemons/com.davidwernhart.Helper.plist"
+end
