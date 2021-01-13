bc8217c883c987e244c5e66b9db678f12b904d36
Mickaël Mottet <MCKLMT@users.noreply.github.com>
2021-01-13 02:28:43 +0100

    Update dotnet-sdk from 5.0.101 to 5.0.102 (#98043)
    * Update dotnet-sdk from 5.0.101 to 5.0.102
    
    * Update dotnet-sdk.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/dotnet-sdk.rb
+++ Casks/dotnet-sdk.rb
@@ -3,8 +3,8 @@ cask "dotnet-sdk" do
     version "2.2.402,7430e32b-092b-4448-add7-2dcf40a7016d:1076952734fbf775062b48344d1a1587"
     sha256 "e74d816bc034d0fcdfa847286a6cad097227d4864da1c97fe801012af0c26341"
   else
-    version "5.0.101,0a7fa783-02e1-4785-b7b1-3c430f8825dc:764e53ff2f5722bc1b8bbc178fe25930"
-    sha256 "280209019adf9b90480619d2150b818a572fe4e68c3e013d29cbb20fef5bc4bc"
+    version "5.0.102,52bd43f7-88c9-420c-80c0-f19bae344293:a8d56f01fdb6f71adb5e22c3ddd1c3c6"
+    sha256 "0cfca74440da0ba98803547d3eda4a0c3c4152d5fbc69c6d83f2d9b663ac925f"
   end
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
@@ -15,8 +15,8 @@ cask "dotnet-sdk" do
 
   conflicts_with cask: [
     "dotnet",
-    "dotnet-preview",
-    "dotnet-sdk-preview",
+    "homebrew/cask-versions/dotnet-preview",
+    "homebrew/cask-versions/dotnet-sdk-preview",
   ]
   depends_on macos: ">= :sierra"
 
