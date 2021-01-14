73864fc7ab643a9b701527e2807b46cc20de9ebd
韦洪将 <weihongjiang@ecpark.cn>
2021-01-14 15:37:46 +0800

    Add MacStroke.app v2.0.0

---
+++ Casks/macstroke.rb
@@ -0,0 +1,13 @@
+cask "macstroke" do
+  version "2.0.0"
+  sha256 "14c4497aa0f1ec2c18535b9b0d6b069f9bf473b0a1bd7dd183e3bc8b15a92c12"
+
+  url "https://github.com/mtjo/MacStroke/releases/download/#{version}/MacStroke.zip"
+  name "MacStroke"
+  desc "MacStroke is a Configurable global mouse gesture for macOS.
+  MacStroke 是 macOS上一款高度可配置的全局鼠标手势软件。"
+  homepage "https://github.com/mtjo/MacStroke"
+  appcast "https://github.com/mtjo/MacStroke/blob/release/AppCast.xml"
+
+  app "MacStroke.app"
+end
