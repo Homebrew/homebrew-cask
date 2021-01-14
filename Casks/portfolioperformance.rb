c7eae9079e6b9646a40c9f87df81da9a9f1e214c
Markus Reiter <me@reitermark.us>
2021-01-14 22:19:24 +0100

    portfolioperformance: add livecheck

--- Casks/portfolioperformance.rb
+++ Casks/portfolioperformance.rb
@@ -1,6 +1,6 @@
 cask "portfolioperformance" do
-  version "0.50.0"
-  sha256 "bbaf1feb4de2a92523495b0b7e7537016087f3051015aa646fe033eb95083b8b"
+  version "0.50.1"
+  sha256 "0759495690665e76444b313ebfa326b7751b0dfd76c303ad0691f41fdeb0e30a"
 
   url "https://github.com/buchen/portfolio/releases/download/#{version}/PortfolioPerformance-#{version}.dmg",
       verified: "github.com/buchen/portfolio/"
@@ -8,6 +8,12 @@ cask "portfolioperformance" do
   name "Portfolio Performance"
   desc "Calculate the overall performance of an investment portfolio"
   homepage "https://www.portfolio-performance.info/en/"
+
+  livecheck do
+    url :url
+    strategy :git
+    regex(/^(\d+(?:\.\d+)*)$/)
+  end
 
   auto_updates true
 
