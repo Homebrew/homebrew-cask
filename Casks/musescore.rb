f403c5f10347bf3b79a98e13b5f22f5f84541091
Leopoldo Pla <lpla@dlsi.ua.es>
2021-01-14 17:00:22 +0100

    Restored chomp for download url version folder

--- Casks/musescore.rb
+++ Casks/musescore.rb
@@ -1,7 +1,7 @@
 cask "musescore" do
   version "3.6.0.485472625"
   sha256 "cbf9bd5b33b564719788d1c3618e492afad035027ac31dbfc47a83d054e1644f"
-  url "https://github.com/musescore/MuseScore/releases/download/v#{version.major_minor_patch}/MuseScore-#{version}.dmg",
+  url "https://github.com/musescore/MuseScore/releases/download/v#{version.major_minor_patch.chomp(".0")}/MuseScore-#{version}.dmg",
       verified: "github.com/musescore/MuseScore/"
   name "MuseScore"
   desc "Open-source music notation software"
