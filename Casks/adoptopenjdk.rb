4491553d8b9afb23cd2216ff51c936e39972f3bd
Seeker <meaningseeking@protonmail.com>
2021-01-14 04:26:09 -0800

    Update adoptopenjdk from 15,36 to 15.0.1,9.1 (#98128)

--- Casks/adoptopenjdk.rb
+++ Casks/adoptopenjdk.rb
@@ -1,14 +1,14 @@
 cask "adoptopenjdk" do
-  version "15,36"
-  sha256 "10ced23791428f002940c91c6d86aec3e701faf4478ddbb29a81ea3977a955b4"
+  version "15.0.1,9.1"
+  sha256 "d69015c5ea7a1a81256f51cd33adf67e6f80c829d62651dd02067df3c452789b"
 
-  url "https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/download/jdk-#{version.before_comma}%2B#{version.after_comma}/OpenJDK#{version.major}U-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma}.pkg",
+  url "https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/download/jdk-#{version.before_comma}%2B#{version.after_comma}/OpenJDK#{version.major}U-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma.major}.pkg",
       verified: "github.com/AdoptOpenJDK/"
   appcast "https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/latest"
   name "AdoptOpenJDK Java Development Kit"
   homepage "https://adoptopenjdk.net/"
 
-  pkg "OpenJDK#{version.major}U-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma}.pkg"
+  pkg "OpenJDK#{version.major}U-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma.major}.pkg"
 
   uninstall pkgutil: "net.adoptopenjdk.#{version.major}.jdk"
 
