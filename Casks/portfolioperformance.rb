7127414290cc670c674bde640df15912e308ec02
Michael <MichaelSp@users.noreply.github.com>
2021-01-14 12:19:44 +0100

    Update portfolioperformance from 0.50.0 to 0.50.1 (#98131)

--- Casks/portfolioperformance.rb
+++ Casks/portfolioperformance.rb
@@ -1,6 +1,6 @@
 cask "portfolioperformance" do
-  version "0.50.0"
-  sha256 "bbaf1feb4de2a92523495b0b7e7537016087f3051015aa646fe033eb95083b8b"
+  version "0.50.1"
+  sha256 "0759495690665e76444b313ebfa326b7751b0dfd76c303ad0691f41fdeb0e30a"
 
   url "https://github.com/buchen/portfolio/releases/download/#{version}/PortfolioPerformance-#{version}.dmg",
       verified: "github.com/buchen/portfolio/"
