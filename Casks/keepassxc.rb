eb43ff1b88f6a889088ceaa8c58fae5286d33266
xxyzz <gitpull@protonmail.com>
2021-01-14 16:52:15 +0000

    Keepassxc 2.6.3 (#98110)

--- Casks/keepassxc.rb
+++ Casks/keepassxc.rb
@@ -1,8 +1,8 @@
 cask "keepassxc" do
-  version "2.6.2,2"
-  sha256 "611b940952d5d51c2865d35e6916954a4ff3454bc8ae4dcc7abed0a4cda7d90d"
+  version "2.6.3"
+  sha256 "3b2e86aafa6943771f008ec530d0809b12f1a09773838f8e0e79ed71061a3c36"
 
-  url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version.before_comma}/KeePassXC-#{version.before_comma}-#{version.after_comma}.dmg",
+  url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}.dmg",
       verified: "github.com/keepassxreboot/keepassxc/"
   name "KeePassXC"
   desc "Password manager app"
@@ -10,10 +10,8 @@ cask "keepassxc" do
 
   livecheck do
     url "https://github.com/keepassxreboot/keepassxc/releases/latest"
-    strategy :page_match do |page|
-      match = page.match(%r{href=.*?/KeePassXC-(\d+(?:.\d+)*)-(\d+)\.dmg}i)
-      "#{match[1]},#{match[2]}"
-    end
+    strategy :page_match
+    regex(%r{href=.*?/KeePassXC-(\d+(?:\.\d+)*)\.dmg}i)
   end
 
   conflicts_with cask: "homebrew/cask-versions/keepassxc-beta"
