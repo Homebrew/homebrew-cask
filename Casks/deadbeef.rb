b49727ee3fcfb38239cd0b60fc8a51a8208fd2f0
Yurii Kolesnykov <root@yurikoles.com>
2021-01-14 23:03:37 +0300

    deadbeef: switch to specific version 1.8.5-beta2, instead of devel/nightly
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/deadbeef.rb
+++ Casks/deadbeef.rb
@@ -1,9 +1,10 @@
 cask "deadbeef" do
-  version "1.0,fd1a0f9"
-  sha256 :no_check
+  version "1.8.5-beta2"
+  sha256 "d6ad1718a8bb8ec2ccb75d657df287dcda134a3a91650430c9f8fd3615467e90"
 
-  url "https://downloads.sourceforge.net/deadbeef/travis/osx/master/deadbeef-devel-osx-x86_64.zip",
+  url "https://downloads.sourceforge.net/deadbeef/travis/osx/master/deadbeef-#{version}-osx-x86_64.zip",
       verified: "downloads.sourceforge.net/deadbeef/"
+  appcast "https://sourceforge.net/projects/deadbeef/rss?path=/travis/osx/master"
   name "DeaDBeeF"
   desc "Modular audio player"
   homepage "https://deadbeef.sourceforge.io/"
