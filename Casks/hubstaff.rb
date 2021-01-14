a3b9758a7ee2ce991d96258a25144205606f8b30
Jeffrey C <spaz926@users.noreply.github.com>
2021-01-14 12:34:57 -0600

    Update hubstaff from 1.5.15,2652 to 1.5.16,2850 (#97558)

--- Casks/hubstaff.rb
+++ Casks/hubstaff.rb
@@ -1,6 +1,6 @@
 cask "hubstaff" do
-  version "1.5.15,2652"
-  sha256 "abea6948da97884d2f9890ffca45a75408d2a750681eae0212f79a202c77b429"
+  version "1.5.16,2850"
+  sha256 "0b37c45740efd23873d350fd8fd1f700e0567dd33c966bccb9ce1a8675f5e5fe"
 
   url "https://app.hubstaff.com/download/#{version.after_comma}-mac-os-x-#{version.before_comma.dots_to_hyphens}-release"
   name "Hubstaff"
