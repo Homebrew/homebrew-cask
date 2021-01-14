f2ee9845490e2d57a0fdc1d1d8baa015b142db5e
Vladimír Kriška <vlado@keboola.com>
2021-01-14 09:22:09 +0100

    Add missing description

--- Casks/timeular.rb
+++ Casks/timeular.rb
@@ -6,6 +6,7 @@ cask "timeular" do
       verified: "timeular-desktop-packages.s3.amazonaws.com/"
   appcast "https://timeular-desktop-packages.s3.amazonaws.com/mac/production/latest-mac.yml"
   name "Timeular"
+  desc "Time tracking application"
   homepage "https://timeular.com/"
 
   auto_updates true
