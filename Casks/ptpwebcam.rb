ebb182cbcbc551ee48c0a04b8a9db09abe38a4b6
Sean Molenaar <smillerdev@me.com>
2021-01-15 13:48:12 +0100

    ptpwebcam 1.2.6 (new cask)

---
+++ Casks/ptpwebcam.rb
@@ -0,0 +1,27 @@
+cask "ptpwebcam" do
+  version "1.2.6"
+  sha256 "6d77dbb9d6052397f953b49e4a218cb809ec864a9cf3dfc83318f788c71e88c4"
+
+  url "https://github.com/dognotdog/ptpwebcam/releases/download/v#{version}/PTP_Webcam-v#{version}.pkg",
+  	  verified: "https://github.com/dognotdog/ptpwebcam"
+  appcast 'https://github.com/dognotdog/ptpwebcam/releases.atom'
+
+  name "ptpwebcam"
+  desc "DSLR Live View Video Plugin"
+  homepage "https://ptpwebcam.org/"
+
+  pkg "PTP_Webcam-v#{version}.pkg"
+  
+  uninstall pkgutil:   [
+    "org.ptpwebcam.pkg.PTPWebcam",
+    "org.ptpwebcam.pkg.EnableChrome",
+    "org.ptpwebcam.pkg.EnableSkype",
+    "org.ptpwebcam.pkg.EnableZoom",
+    "org.ptpwebcam.pkg.RemoveEOSWebcam",
+  ],
+  launchctl: "org.ptpwebcam.PtpWebcamAgent",
+  trash:   [
+    "/Library/CoreMediaIO/Plug-ins/DAL/PTPWebcamDALPlugin.plugin",
+    "/Library/LaunchDaemons/org.ptpwebcam.PtpWebcamAssistant.plist",
+  ]
+end
