5b79b29568d42f466342fab019000d2413f5b3c1
tiiiecherle <tiiiecherle@gmail.com>
2021-01-15 13:43:40 +0100

    Update cyberduck from 7.8.0,34081 to 7.8.1,34119 (#98166)

--- Casks/cyberduck.rb
+++ Casks/cyberduck.rb
@@ -1,6 +1,6 @@
 cask "cyberduck" do
-  version "7.8.0,34081"
-  sha256 "f80c425d8e580cfcd2e78b8fa748a1129b5b2368c6a96596641dafc206ccd85a"
+  version "7.8.1,34119"
+  sha256 "ee24b7cfdadbfbec37f2c0c1f7d0a8b17b72e2f2853fd39d5bc1601676e0b716"
 
   url "https://update.cyberduck.io/Cyberduck-#{version.before_comma}.#{version.after_comma}.zip"
   name "Cyberduck"
