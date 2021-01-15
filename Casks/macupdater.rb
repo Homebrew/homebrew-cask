4b2eba5bee74eeca6778f81a92c017f2f6e84e3d
Vítor Galvão <info@vitorgalvao.com>
2021-01-15 19:02:11 +0000

    Update macupdater from 2.0.0,10776 to 2.0.0,10776 (#98175)

--- Casks/macupdater.rb
+++ Casks/macupdater.rb
@@ -1,6 +1,6 @@
 cask "macupdater" do
   version "2.0.0,10776"
-  sha256 "8842c80d3945ac552f2ce2285dc2c98fd872655240b69aa8ae6f8e032d5f487c"
+  sha256 "4f49e4db72a57f65ac732660ce285e62c194efa0ac8d52aaf54659a37298395d"
 
   url "https://www.corecode.io/downloads/macupdater_#{version.before_comma}.dmg"
   name "MacUpdater"
