8c3f032c18796cfaf16a8a3a264149c165e1c304
Dominik Gedon <dgedon@suse.de>
2021-01-16 15:48:40 +0100

    Update freetube from 0.10.0 to 0.11.1 (#98196)

--- Casks/freetube.rb
+++ Casks/freetube.rb
@@ -1,6 +1,6 @@
 cask "freetube" do
-  version "0.10.0"
-  sha256 "2a86d3cdba6613af13f83170a07934bf7507052e83ace94c7382f2621756e459"
+  version "0.11.1"
+  sha256 "d86f1a5fd117ea23b335d1463c2bd22aee586de718208bf455efab7e210e4d6b"
 
   url "https://github.com/FreeTubeApp/FreeTube/releases/download/v#{version}-beta/freetube-#{version}-mac.dmg"
   name "FreeTube"
