6a454354225f13972e5901f146fd55a295034839
Zhenfu Shi <szf1234@me.com>
2021-01-16 12:34:31 +0800

    Update rstudio from 1.3.1093 to 1.4.1103 (#98180)

--- Casks/rstudio.rb
+++ Casks/rstudio.rb
@@ -1,6 +1,6 @@
 cask "rstudio" do
-  version "1.3.1093"
-  sha256 "bdc4d3a41f5e714c5399dbad466e31f7b85bf9b93db464c3010f5a560bd9914d"
+  version "1.4.1103"
+  sha256 "20148bd6ee7ed80d85ae8b309ebaa5d68df282d610056faeedf4acb8716aaaa2"
 
   url "https://rstudio-desktop.s3.amazonaws.com/desktop/macos/RStudio-#{version}.dmg",
       verified: "rstudio-desktop.s3.amazonaws.com/"
