aac485d2cf2eb278a99261f780829e1ab92c7665
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-16 20:43:17 +0800

    Update lightkey.rb

--- Casks/lightkey.rb
+++ Casks/lightkey.rb
@@ -1,9 +1,9 @@
 cask "lightkey" do
   version "3.4.3"
-  sha256 :no_check
+  sha256 "5878e233857b4d68bbc042c0cda2f52491a5e7a12479cddb70d0bc87c71979cc"
 
-  url "https://lightkeyapp.com/en/download"
-  appcast ""
+  url "https://lightkeyapp.com/content/06-download/Lightkey-#{version.dots_to_hyphens}/LightkeyInstaller.zip"
+  appcast "https://lightkeyapp.com/en/downloads"
   name "Lightkey"
   desc "DMX lighting control"
   homepage "https://lightkeyapp.com/"
@@ -15,8 +15,8 @@ cask "lightkey" do
   uninstall delete:  "/Applications/Lightkey.app",
             pkgutil: [
               "de.monospc.lightkey.pkg.App",
-              "de.monospc.lightkey.pkg.OLA",
               "de.monospc.lightkey.pkg.documentation",
+              "de.monospc.lightkey.pkg.OLA",
             ]
 
   zap trash: [
