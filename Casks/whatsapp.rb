e8f02aae6e186262df9bf75eb7ed9b3eeb94b22b
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-16 22:03:52 +0800

    whatsapp: add livecheck (#98192)
    * whatsapp: add livecheck
    
    * Update whatsapp.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/whatsapp.rb
+++ Casks/whatsapp.rb
@@ -3,10 +3,15 @@ cask "whatsapp" do
   sha256 "95dbe4f399d595e5293596f5b652ba5944d9dfbd5e8b6c52f51497cbf4011425"
 
   url "https://web.whatsapp.com/desktop/mac/files/release-#{version}.zip"
-  appcast "https://web.whatsapp.com/desktop/mac/releases?platform=darwin&arch=x64"
   name "WhatsApp"
   desc "Desktop client for WhatsApp"
   homepage "https://www.whatsapp.com/"
+
+  livecheck do
+    url "https://web.whatsapp.com/desktop/mac/releases"
+    strategy :page_match
+    regex(/release-(\d+(?:\.\d+)*)\.zip/i)
+  end
 
   auto_updates true
 
