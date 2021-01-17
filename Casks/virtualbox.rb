2c79cfef1c92a0cf8f949e4cd2333ffbb8bb0d86
James Morris <J-M0@users.noreply.github.com>
2021-01-17 15:06:05 -0500

    virtualbox: add livecheck block (#97141)

--- Casks/virtualbox.rb
+++ Casks/virtualbox.rb
@@ -3,10 +3,17 @@ cask "virtualbox" do
   sha256 "d7df0f05d9a9e7cba50ea01da264ac20948b1c9c0e0cccd2d628085c9f434d45"
 
   url "https://download.virtualbox.org/virtualbox/#{version.before_comma}/VirtualBox-#{version.before_comma}-#{version.after_comma}-OSX.dmg"
-  appcast "https://download.virtualbox.org/virtualbox/LATEST.TXT"
   name "Oracle VirtualBox"
   desc "Free and open-source hosted hypervisor for x86 virtualization"
   homepage "https://www.virtualbox.org/"
+
+  livecheck do
+    url "https://www.virtualbox.org/wiki/Downloads"
+    strategy :page_match do |page|
+      match = page.match(/href=.*?VirtualBox-(\d+(?:\.\d+)*)-(\d+)-OSX.dmg/)
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   conflicts_with cask: "virtualbox-beta"
 
