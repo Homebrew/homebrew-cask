fae46d948e7a0e8918a8e0fdc18eb65c53a1be4f
David Z <geek@lzw.name>
2021-01-17 22:21:00 +0800

    Update archi from 4.8.0 to 4.8.0

--- Casks/archi.rb
+++ Casks/archi.rb
@@ -2,7 +2,7 @@ cask "archi" do
   version "4.8.0"
   sha256 "b18580e675017325cbf4b772e62fe97f84b59df1f7be8a253f99e89e2b6638b3"
 
-  url "https://www.archimatetool.com/downloads/a#{version.no_dots}/Archi-Mac-#{version}.zip"
+  url "https://www.archimatetool.com/downloads/anusol#{version.no_dots}/Archi-Mac-#{version}.zip"
   name "Archi"
   desc "Toolkit for the ArchiMate modelling language"
   homepage "https://www.archimatetool.com/"
