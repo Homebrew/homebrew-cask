53d367ca46435e258f533de2f3c7fe605ee911e7
Adithya Sunil <adithyasunil26@gmail.com>
2021-01-17 21:46:18 +0400

    Update fme from 2020.2.1,20806 to 2020.2.2,20817 (#98232)

--- Casks/fme.rb
+++ Casks/fme.rb
@@ -1,6 +1,6 @@
 cask "fme" do
-  version "2020.2.1,20806"
-  sha256 "c3b8a47d174f0794abf9279156a7a886b965e147be63a247c76798a8c46368c6"
+  version "2020.2.2,20817"
+  sha256 "7db4bb4484c8bfb6f3303cb5b9a7f39c32957e743d6f21b7c2c34949a06adbf9"
 
   url "https://downloads.safe.com/fme/#{version.major}/fme-desktop-#{version.before_comma}-b#{version.after_comma}-macosx.dmg"
   name "FME Desktop"
