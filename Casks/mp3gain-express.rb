7bf09cb91369d7d40dd1ef8f74280edfee78126d
Adithya Sunil <adithyasunil26@gmail.com>
2021-01-18 00:58:24 +0400

    Update mp3gain-express from 2.4.0 to 2.4.1 (#98250)

--- Casks/mp3gain-express.rb
+++ Casks/mp3gain-express.rb
@@ -1,6 +1,6 @@
 cask "mp3gain-express" do
-  version "2.4.0"
-  sha256 "43725242307dc7bee59c133f0fcef1acf64cfeb7d209b833d72cbe930697d32e"
+  version "2.4.1"
+  sha256 "686158f1ed158763584966c732695cb625cc8ad6a4ac613cdf5ae914324bf65e"
 
   url "https://projects.sappharad.com/mp3gain/mp3gain_mac#{version.chomp(".0").no_dots}.zip"
   name "MP3Gain Express"
