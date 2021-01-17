cbf6061412292e63c5b171212ee86645d6ce00eb
Markus Reiter <me@reitermark.us>
2021-01-17 01:23:55 +0100

    Update redisinsight.rb

--- Casks/redisinsight.rb
+++ Casks/redisinsight.rb
@@ -3,10 +3,13 @@ cask "redisinsight" do
   sha256 "4c5e125784229f69dc2a39baf3fb48091ae35101ac12647fd0d7a03e34ddcc6e"
 
   url "https://downloads.redisinsight.redislabs.com/#{version}/redisinsight-mac.dmg"
-  appcast "https://docs.redislabs.com/latest/ri/release-notes/",
-          must_contain: version.major_minor
   name "RedisInsight"
   homepage "https://www.redislabs.com/redisinsight/"
+
+  livecheck do
+    url "https://d3fyopse48vfpi.cloudfront.net/latest/redisinsight-mac.dmg"
+    strategy :page_match
+  end
 
   app "RedisInsight.app"
 
