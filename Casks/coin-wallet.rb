e1ad567011a5bf21eb60e2e7f391a1ad6753bdb7
adithyasunil26 <adithyasunil26@gmail.com>
2021-01-17 20:39:36 +0400

    Update coin-wallet from 3.0.5 to 3.0.6

--- Casks/coin-wallet.rb
+++ Casks/coin-wallet.rb
@@ -1,6 +1,6 @@
 cask "coin-wallet" do
-  version "3.0.5"
-  sha256 "f7e2ac0ec598d0cdd2ddae6d940a15bf882e7e16323a918a20df307c0bacdcc9"
+  version "3.0.6"
+  sha256 "85463bfd6cc793f505558ed92a4bbb1640d5a3460b28b3da485d7ee26d0c069e"
 
   url "https://github.com/CoinSpace/CoinSpace/releases/download/v#{version}/Coin.Wallet.dmg",
       verified: "github.com/CoinSpace/CoinSpace/"
