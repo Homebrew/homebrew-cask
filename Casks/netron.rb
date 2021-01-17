538ca5bede7cf66124dea746839f9deea13e9521
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-17 09:13:35 +0800

    netron: update to 4.7.2, add livecheck (#98209)

--- Casks/netron.rb
+++ Casks/netron.rb
@@ -1,12 +1,17 @@
 cask "netron" do
-  version "4.7.1"
-  sha256 "8f21d16418c8395aad764931792286274ba247a8bfaf0e6486f8848704964b9b"
+  version "4.7.2"
+  sha256 "dc1e613419775505518e362f92d4fb2e7d0a822147cb92480d2f79ba79f724a7"
 
   url "https://github.com/lutzroeder/netron/releases/download/v#{version}/Netron-#{version}-mac.zip"
-  appcast "https://github.com/lutzroeder/netron/releases.atom"
   name "Netron"
   desc "Visualizer for neural network, deep learning, and machine learning models"
   homepage "https://github.com/lutzroeder/netron"
+
+  livecheck do
+    url :url
+    strategy :git
+    regex(/^v(\d+(?:\.\d+)*)$/)
+  end
 
   auto_updates true
 
