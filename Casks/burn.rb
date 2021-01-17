036f309db5d3320b5d64f6af9f185130debfbd8e
Hannes Braun <hannesbraun@mail.de>
2021-01-17 14:13:00 +0100

    Update burn from 3.0.3 to 3.0.5 (#98216)

--- Casks/burn.rb
+++ Casks/burn.rb
@@ -1,6 +1,6 @@
 cask "burn" do
-  version "3.0.3"
-  sha256 "90350b61f769b3022aa649d2a80572bada707cb951fc43d0d499e66cb26e1cc4"
+  version "3.0.5"
+  sha256 "82019cb2f99adb649b7ff38aa2036352c41016e934f542a1781975fbddbf992a"
 
   url "https://downloads.sourceforge.net/burn-osx/Burn/#{version}/Burn-#{version}.zip",
       verified: "downloads.sourceforge.net/burn-osx/"
