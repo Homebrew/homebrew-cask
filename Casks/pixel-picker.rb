088f33e39ada0ca9163ca5af59ff267b9039731c
adithyasunil26 <adithyasunil26@gmail.com>
2021-01-18 12:18:03 +0400

    Update pixel-picker from 1.4.0 to 1.5.0

--- Casks/pixel-picker.rb
+++ Casks/pixel-picker.rb
@@ -1,6 +1,6 @@
 cask "pixel-picker" do
-  version "1.4.0"
-  sha256 "d0ea25460ae8d14eca8b76bc9c69ec2d3f652007b960b8596d6bb4b97629e110"
+  version "1.5.0"
+  sha256 "61ed0cb9509b18a000491c9a9052296e1fd0789d95a5499768688bb941f87fc3"
 
   url "https://github.com/acheronfail/pixel-picker/releases/download/#{version}/Pixel.Picker.#{version}.dmg"
   appcast "https://github.com/acheronfail/pixel-picker/releases.atom"
