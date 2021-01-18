95714eb33a5b489a736e5f19f89e395eeb55111e
Adithya Sunil <adithyasunil26@gmail.com>
2021-01-18 12:27:14 +0400

    Update pomotroid from 0.12.0 to 0.13.0 (#98267)

--- Casks/pomotroid.rb
+++ Casks/pomotroid.rb
@@ -1,6 +1,6 @@
 cask "pomotroid" do
-  version "0.12.0"
-  sha256 "cb82bae8ff7bfc578b01b9cc5521605f122b851e1cf1cfc53a3055cf664f7447"
+  version "0.13.0"
+  sha256 "fab1a8598490db267639609b42312a8947ee2de075164935d0eba60c57790788"
 
   url "https://github.com/Splode/pomotroid/releases/download/v#{version}/pomotroid-#{version}-macos.dmg"
   appcast "https://github.com/Splode/pomotroid/releases.atom"
