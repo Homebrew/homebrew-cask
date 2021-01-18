1632135c8442d4f4abba23ed36a334a8235d4726
Zhenfu Shi <szf1234@me.com>
2021-01-18 10:01:58 +0800

    Update hackintool from 3.4.9 to 3.5.1 (#98257)

--- Casks/hackintool.rb
+++ Casks/hackintool.rb
@@ -1,6 +1,6 @@
 cask "hackintool" do
-  version "3.4.9"
-  sha256 "2c39478144b2e6f248a33940510ddabc0b36d2c3220e0af7785d0d41bf823064"
+  version "3.5.1"
+  sha256 "42a427778d89a6f6ba8c0715796a1e611ed261e069129a006aa0bbfa10306853"
 
   url "https://github.com/headkaze/Hackintool/releases/download/#{version}/Hackintool.zip"
   appcast "https://github.com/headkaze/Hackintool/releases.atom"
