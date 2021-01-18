252a1e63b48283af2981f62626347853f60c524b
yesRabbit <chandler.lu1997@gmail.com>
2021-01-18 21:00:22 +0800

    Update ogdesign-eagle to 2.0,2

--- Casks/ogdesign-eagle.rb
+++ Casks/ogdesign-eagle.rb
@@ -1,6 +1,6 @@
 cask "ogdesign-eagle" do
-  version "1.11,3"
-  sha256 "c504887564465c996e2a1ed5dfa5220dcae70bc601aa2c1606c8306cfa7751aa"
+  version "2.0,2"
+  sha256 "6f61e27f0cdae9c5ea1b08b6b97ec9d879306306f357299e7d0502978ac87257"
 
   url "https://eagleapp.s3-accelerate.amazonaws.com/releases/Eagle-#{version.before_comma}-build#{version.after_comma}.dmg",
       verified: "eagleapp.s3-accelerate.amazonaws.com/"
