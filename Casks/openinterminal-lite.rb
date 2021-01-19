6b3d243b76c6fd7b66113518b07f49d38f703769
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-19 09:39:14 +0800

    Update openinterminal-lite.rb

--- Casks/openinterminal-lite.rb
+++ Casks/openinterminal-lite.rb
@@ -1,8 +1,8 @@
 cask "openinterminal-lite" do
-  version "v1.2.0"
+  version "1.2.0"
   sha256 "89b7c88b5e477fbfbf3263d93272768865f22e4e99d3bb29ce853ab12886f5fc"
 
-  url "https://github.com/Ji4n1ng/OpenInTerminal/releases/download/#{version}/OpenInTerminal-Lite.app.zip"
+  url "https://github.com/Ji4n1ng/OpenInTerminal/releases/download/v#{version}/OpenInTerminal-Lite.app.zip"
   appcast "https://github.com/Ji4n1ng/OpenInTerminal/releases.atom"
   name "OpenInTerminal-Lite"
   desc "Finder Toolbar app to open the current directory in Terminal"
