b9ddac9d51822c5ff803a87c0cd3159554e416cf
Constantin Dullo <constantin.dullo@me.com>
2021-01-19 17:46:21 +0100

    Add PicoScope 6 v6.14.36.5676
    Test & Measurement oscilloscope software for PicoScope oscilloscops

---
+++ Casks/picoscope.rb
@@ -0,0 +1,13 @@
+cask "picoscope" do
+  version "6.14.36.5676"
+  sha256 "4c843d2b8b12bb614da12122341a0e02d42042d180c45160b6e2b6f29af504fb"
+
+  url "https://www.picotech.com/download/software/sr/PicoScope-#{version}.pkg"
+  name "PicoScope"
+  desc "Test & Measurement oscilloscope software for PicoScope oscilloscops"
+  homepage "https://www.picotech.com/"
+
+  pkg "PicoScope-#{version}.pkg"
+
+  uninstall pkgutil: "com.picotech.picoscope6"
+end
