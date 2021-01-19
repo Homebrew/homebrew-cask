6054de2301db9d7924541387452ae9d3643d1c5d
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-19 12:00:32 +0800

    Update nndd.rb

--- Casks/nndd.rb
+++ Casks/nndd.rb
@@ -2,7 +2,7 @@ cask "nndd" do
   version "2.4.3,62201"
   sha256 "6a73dcad2e73d877ad1503ed1162cae1a1c84f21d1abaa6aaf9b31bb2fbca531"
 
-  url "http://dl.osdn.jp/nndd/#{version.after_comma}/NNDD_v#{version.before_comma.dots_to_underscores}.dmg"
+  url "https://osdn.net/projects/nndd/downloads/#{version.after_comma}/NNDD_v#{version.before_comma.dots_to_underscores}.dmg"
   appcast "https://ja.osdn.net/projects/nndd/releases/rss"
   name "NNDD"
   homepage "https://osdn.jp/projects/nndd/"
