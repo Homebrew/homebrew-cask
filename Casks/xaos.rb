0007341a822ac8afc4c1d88aefcbfcf5f6524a69
adithyasunil26 <adithyasunil26@gmail.com>
2021-01-20 02:43:23 +0400

    Update xaos from 4.2 to 4.2.1

--- Casks/xaos.rb
+++ Casks/xaos.rb
@@ -1,6 +1,6 @@
 cask "xaos" do
-  version "4.2"
-  sha256 "80b01d8cd10b6cd6d567058acaf9430398e5608f5e179048a4167ecc57b3bd90"
+  version "4.2.1"
+  sha256 "be88d2b1c26b3ef1ec8a297195364acfc549a60facf91d49e01941415f662a72"
 
   url "https://github.com/xaos-project/XaoS/releases/download/release-#{version}/XaoS-#{version}.dmg",
       verified: "github.com/xaos-project/XaoS/"
