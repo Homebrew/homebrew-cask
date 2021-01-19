10d4d02c8a8c6088587b78846fbaa3105d002615
Vítor Galvão <info@vitorgalvao.com>
2021-01-19 22:01:57 +0000

    signal.rb: no cross-platform in desc (#98361)

--- Casks/signal.rb
+++ Casks/signal.rb
@@ -5,7 +5,7 @@ cask "signal" do
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{version}.dmg"
   appcast "https://github.com/signalapp/Signal-Desktop/releases.atom"
   name "Signal"
-  desc "Cross-platform instant messaging application focusing on security"
+  desc "Instant messaging application focusing on security"
   homepage "https://signal.org/"
 
   auto_updates true
