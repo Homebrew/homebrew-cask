f98f7b95c0097270c30c82d8e857f2498383cb29
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-19 09:39:40 +0800

    Update openineditor-lite.rb

--- Casks/openineditor-lite.rb
+++ Casks/openineditor-lite.rb
@@ -1,8 +1,8 @@
 cask "openineditor-lite" do
-  version "v1.2.0"
+  version "1.2.0"
   sha256 "f35c1a2a94d3e3f4c5472ad9be435baa4c82403cdb8991ef43f98da26c2ad9c0"
 
-  url "https://github.com/Ji4n1ng/OpenInTerminal/releases/download/#{version}/OpenInEditor-Lite.app.zip"
+  url "https://github.com/Ji4n1ng/OpenInTerminal/releases/download/v#{version}/OpenInEditor-Lite.app.zip"
   appcast "https://github.com/Ji4n1ng/OpenInTerminal/releases.atom"
   name "OpenInEditor-Lite"
   desc "Finder Toolbar app to open the current directory in Editor"
