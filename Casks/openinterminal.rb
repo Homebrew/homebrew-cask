bbac6fa1e6878dceb186440a6a42d9e90752f06e
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-19 09:38:40 +0800

    Update openinterminal.rb

--- Casks/openinterminal.rb
+++ Casks/openinterminal.rb
@@ -1,8 +1,8 @@
 cask "openinterminal" do
-  version "v2.3.0"
+  version "2.3.0"
   sha256 "b3637b042c3ce55b6cfc0ad9a0988ec9dfa16ff5d012ec5601cfac3e6a34081a"
 
-  url "https://github.com/Ji4n1ng/OpenInTerminal/releases/download/#{version}/OpenInTerminal.app.zip"
+  url "https://github.com/Ji4n1ng/OpenInTerminal/releases/download/v#{version}/OpenInTerminal.app.zip"
   appcast "https://github.com/Ji4n1ng/OpenInTerminal/releases.atom"
   name "OpenInTerminal"
   desc "Finder Toolbar app to open the current directory in Terminal or Editor"
