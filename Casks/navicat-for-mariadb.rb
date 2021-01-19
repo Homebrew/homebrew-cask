4885e9f62921da58a720d4745ffd000e9546ca43
Tomáš Holý <vantomas@vantomas.net>
2021-01-19 10:32:55 +0100

    Update navicat-for-mariadb from 15.0.24 to 15.0.25 (#98321)

--- Casks/navicat-for-mariadb.rb
+++ Casks/navicat-for-mariadb.rb
@@ -1,6 +1,6 @@
 cask "navicat-for-mariadb" do
-  version "15.0.24"
-  sha256 "3b1e43fe232bbcb9322fdd04a5910a1f90641802298ee0a037954b40fbd24e1d"
+  version "15.0.25"
+  sha256 "9b3398417b552887600fc1eb5d5c003238cdfeb7a9c49bd6371b07288ce952ed"
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_mariadb_en.dmg"
   appcast "https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20for%20MariaDB&appLang=en"
