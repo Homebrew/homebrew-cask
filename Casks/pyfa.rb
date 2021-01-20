4fb43f1485f0d05e2db126d031902f8fe8f87200
Vítor Galvão <info@vitorgalvao.com>
2021-01-20 06:31:09 +0000

    pyfa.rb: no cross-platform in desc (#98411)

--- Casks/pyfa.rb
+++ Casks/pyfa.rb
@@ -5,7 +5,7 @@ cask "pyfa" do
   url "https://github.com/pyfa-org/Pyfa/releases/download/v#{version}/pyfa-v#{version}-mac.zip"
   appcast "https://github.com/pyfa-org/Pyfa/releases.atom"
   name "pyfa"
-  desc "Python fitting assistant, cross-platform fitting tool for EVE Online"
+  desc "Fitting tool for EVE Online"
   homepage "https://github.com/pyfa-org/Pyfa"
 
   app "pyfa.app"
