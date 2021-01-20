92ebbbbd5d7daa7d9ef0c9876a3fae5754be8641
Aaron Carroll <xaaronc@gmail.com>
2021-01-19 22:43:38 -0800

    Update coqide to 8.13.0,1 (#98432)
    * Update coqide to 8.13.0,1
    
    The coqide 8.13.0 installer package has been updated after initial release without
    a version bump.
    
    * Update coqide.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/coqide.rb
+++ Casks/coqide.rb
@@ -1,11 +1,12 @@
 cask "coqide" do
   version "8.13.0"
-  sha256 "fa02786729169e8a86f478839088a2c045a3e18a4ac6b5339a23558c3aef72fb"
+  sha256 "718fc214d2b36bae440942e8a2615c4f9ab84545b505c7d421e17b742bbdd1b8"
 
-  url "https://github.com/coq/coq/releases/download/V#{version.major_minor_patch}/coq-#{version}-installer-macos.dmg",
+  url "https://github.com/coq/coq/releases/download/V#{version}/coq-#{version}-installer-macos.dmg",
       verified: "github.com/coq/coq/"
   appcast "https://github.com/coq/coq/releases.atom"
   name "Coq"
+  desc "Formal proof management system"
   homepage "https://coq.inria.fr/"
 
   depends_on macos: ">= :sierra"
