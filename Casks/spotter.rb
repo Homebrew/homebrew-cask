3d4b4cae7bcf73b4b22c68f5f5464220f287801d
Denis Zyulev <zzulev@gmail.com>
2021-01-20 20:06:34 +0100

    Spotter version bump

--- Casks/spotter.rb
+++ Casks/spotter.rb
@@ -1,13 +1,13 @@
 cask "spotter" do
-  version "0.0.1"
+  version "0.0.2"
   sha256 :no_check
 
-  url "https://github.com/spotter-application/spotter/releases/download/#{version}/spotter.#{version}.dmg",
-      verified: "github.com/spotter-application/spotter/"
+  url "https://github.com/spotter-application/spotter/releases/download/#{version}/spotter.#{version}.dmg"
   appcast "https://github.com/spotter-application/spotter/releases.atom"
   name "Spotter"
-  desc "🤖 Productivity tool that allows to launch everything."
+  desc "🤖 Productivity tool that allows to launch everything"
   homepage "https://github.com/spotter-application/spotter"
+
   depends_on macos: ">= :big_sur"
 
   app "spotter.app"
