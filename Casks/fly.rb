e7d4b00b64891e67da352b073c55fcc4d9a9ff5b
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-21 07:58:20 +0800

    Update fly.rb

--- Casks/fly.rb
+++ Casks/fly.rb
@@ -1,6 +1,6 @@
 cask "fly" do
-  version "6.7.2"
-  sha256 "e095e0d631cd42a28b63e7101819840bad1b255d8b6d31ceb1fc0220c055605d"
+  version "6.7.3"
+  sha256 "3e4a347868c444c6dc4b595082e508b6242d6fbd14a9475c576058ceda51294e"
 
   url "https://github.com/concourse/concourse/releases/download/v#{version}/fly-#{version}-darwin-amd64.tgz"
   appcast "https://github.com/concourse/concourse/releases.atom"
@@ -9,4 +9,8 @@ cask "fly" do
   homepage "https://github.com/concourse/concourse"
 
   binary "fly"
+
+  postflight do
+    Quarantine.release!(download_path: "$(brew --prefix)/bin/fly") if Quarantine.available?
+  end
 end
