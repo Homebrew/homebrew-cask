2c3814da5ca4e87811a4505411c1fd6c8d95fa4e
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-21 05:12:25 +0800

    Update spotter.rb

--- Casks/spotter.rb
+++ Casks/spotter.rb
@@ -1,11 +1,11 @@
 cask "spotter" do
   version "0.0.2"
-  sha256 :no_check
+  sha256 "71704dd6ca712feacc1615396e4e12b7872f60998f4073be50eaa7f58511dd01"
 
   url "https://github.com/spotter-application/spotter/releases/download/#{version}/spotter.dmg"
   appcast "https://github.com/spotter-application/spotter/releases.atom"
   name "Spotter"
-  desc "🤖 Productivity tool that allows to launch everything"
+  desc "Productivity tool to launch everything"
   homepage "https://github.com/spotter-application/spotter"
 
   depends_on macos: ">= :big_sur"
