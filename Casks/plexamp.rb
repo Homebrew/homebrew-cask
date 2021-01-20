1fea28c27746392f5d606c122bc06af2df489f80
Stefano Canducci <stefano@canducci.org>
2021-01-20 08:05:25 +0100

    Update plexamp from 3.4.0 to 3.4.1

--- Casks/plexamp.rb
+++ Casks/plexamp.rb
@@ -1,6 +1,6 @@
 cask "plexamp" do
-  version "3.4.0"
-  sha256 "63b8e77102b867ceaa8cb1617cd177a9ad4957e7a8d7fb93ccac4f7714c25b4f"
+  version "3.4.1"
+  sha256 "8e675bb1802b498ff2e3f68a647637b02c8c77bd02f7898e3dca850e82773ce0"
 
   url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}.dmg",
       verified: "plexamp.plex.tv/"
