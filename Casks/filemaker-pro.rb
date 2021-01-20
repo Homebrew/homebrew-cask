ae7e8e7c3171c63d558f2e36ce2e9ae533ee9f69
Vítor Galvão <info@vitorgalvao.com>
2021-01-20 08:20:07 +0000

    filemaker-pro.rb: no cross-platform in desc (#98403)
    * filemaker-pro.rb: no cross-platform in desc
    
    * Update filemaker-pro.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/filemaker-pro.rb
+++ Casks/filemaker-pro.rb
@@ -1,11 +1,11 @@
 cask "filemaker-pro" do
-  version "19.0.1.116"
-  sha256 "b561a0ebc62f7b6f50bc5e1b7c05660e5a1ead2027dd296907b3ff644f0b51dc"
+  version "19.2.1.14"
+  sha256 "7e57f30e4f1a72063824240eff78b795de148a5233bfbfc6aaf10c101a269496"
 
   url "https://downloads.claris.com/esd/fmp_#{version}.dmg"
   appcast "https://www.filemaker.com/redirects/ss.txt"
   name "FileMaker Pro"
-  desc "Cross-platform relational database and rapid application development platform"
+  desc "Relational database and rapid application development platform"
   homepage "https://www.claris.com/filemaker/"
 
   auto_updates true
