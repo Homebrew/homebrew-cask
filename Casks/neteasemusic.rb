48cf364d4517398fdd2c24b2875267abc08c93dd
Jiaming Shi <1519996858sjm@gmail.com>
2021-01-20 12:03:44 +0800

    update neteasemusic to 2.3.4,846

--- Casks/neteasemusic.rb
+++ Casks/neteasemusic.rb
@@ -1,6 +1,6 @@
 cask "neteasemusic" do
-  version "2.3.3,840"
-  sha256 "91eb3372e60bd299aee8a924eb0273570f610eeb1227ea26d9a4bb5b5621c609"
+  version "2.3.4,846"
+  sha256 "1dfee30efbfdc8cd37c841e00c01ea4f1db4b4bbafb379c4aeeec8e70e0812b7"
 
   url "https://d1.music.126.net/dmusic/NeteaseMusic_#{version.before_comma}_#{version.after_comma}_web.dmg",
       verified:   "d1.music.126.net/",
