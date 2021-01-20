3a675973d4675a6f7e8fd7e2bff8ef468f10f461
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-20 14:25:30 +0800

    Update coqide.rb

--- Casks/coqide.rb
+++ Casks/coqide.rb
@@ -1,8 +1,8 @@
 cask "coqide" do
-  version "8.13.0,1"
+  version "8.13.0"
   sha256 "718fc214d2b36bae440942e8a2615c4f9ab84545b505c7d421e17b742bbdd1b8"
 
-  url "https://github.com/coq/coq/releases/download/V#{version.major_minor_patch}/coq-#{version.major_minor_patch}-installer-macos.dmg",
+  url "https://github.com/coq/coq/releases/download/V#{version}/coq-#{version}-installer-macos.dmg",
       verified: "github.com/coq/coq/"
   appcast "https://github.com/coq/coq/releases.atom"
   name "Coq"
