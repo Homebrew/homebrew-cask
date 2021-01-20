d241a59a3b2d889a324b382fe9e70a7cec9c9a89
Hannes Braun <hannesbraun@mail.de>
2021-01-20 12:21:30 +0100

    Update kite from 0.20201221.0 to 0.20210114.0 (#98445)

--- Casks/kite.rb
+++ Casks/kite.rb
@@ -1,6 +1,6 @@
 cask "kite" do
-  version "0.20201221.0"
-  sha256 "5193460f47ef26c5d5d825890bf14d16c6fea9b4e1ddf8b9b272712e6b50d2e7"
+  version "0.20210114.0"
+  sha256 "43c5ea6f1df58b938f410a71606b40fb830a04a5f1c6cfa3d6f4a8d8aa075283"
 
   url "https://draqv87tt43s0.cloudfront.net/mac/#{version}/Kite.dmg",
       verified: "draqv87tt43s0.cloudfront.net/"
