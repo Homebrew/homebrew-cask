a39eae35a043bcab9f6e77f03ac34a8afc4f04aa
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-01-21 19:14:35 +0900

    sharemouse: update to 5.0.46

--- Casks/sharemouse.rb
+++ Casks/sharemouse.rb
@@ -1,10 +1,11 @@
 cask "sharemouse" do
-  version "5.0.45"
+  version "5.0.46"
   sha256 :no_check
 
   url "https://www.sharemouse.com/ShareMouseSetup.dmg"
   appcast "https://www.sharemouse.com/download/"
   name "ShareMouse"
+  desc "Mouse & Keyboard Sharing Features"
   homepage "https://www.sharemouse.com/"
 
   app "ShareMouse.app"
