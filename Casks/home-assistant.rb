95e893a2dbce5138271f04ce01af4abfb3ba70a4
R. van Veen <rvanveen@servoy.com>
2021-01-21 14:34:15 +0100

    Update home-assistant from 2021.1,6 to 2021.2,10

--- Casks/home-assistant.rb
+++ Casks/home-assistant.rb
@@ -1,6 +1,6 @@
 cask "home-assistant" do
-  version "2021.1,6"
-  sha256 "11de588ac58c33a76abe31e71a9f11b67c7f3c269a836979ee006292196ec9ad"
+  version "2021.2,10"
+  sha256 "6d91a98730542c01ffb1359c3d31b0cf8fa96394de6212b04d351a5460b54774"
 
   url "https://github.com/home-assistant/iOS/releases/download/release%2F#{version.before_comma}%2F#{version.after_comma}/home-assistant-mac.zip",
       verified: "github.com/home-assistant/iOS/"
