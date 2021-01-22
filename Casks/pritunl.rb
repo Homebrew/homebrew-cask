78b875e69e713d38cc2d9b47bdd50b4ef5480c34
Jeppe Fihl-Pearson <jeppe@tenzer.dk>
2021-01-22 16:45:37 +0000

    Bump pritunl and add Apple Silicon version (#98513)
    * Bump pritunl and add Apple Silicon version
    
    The different in names between the two architectures means that I had to move
    more fields into the if/else statement than I would have liked, but I didn't
    find an easy way to differentiate between them otherwise.
    
    * Update pritunl.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/pritunl.rb
+++ Casks/pritunl.rb
@@ -1,20 +1,31 @@
 cask "pritunl" do
-  if MacOS.version <= :catalina
-    version "1.2.2615.73"
-    sha256 "275f8498fb736a0a1ddebcbfcb6f03e82b8eb4fedf9b7eb61b0e306cb6de4a71"
+  if Hardware::CPU.intel?
+    if MacOS.version <= :catalina
+      version "1.2.2615.73"
+      sha256 "275f8498fb736a0a1ddebcbfcb6f03e82b8eb4fedf9b7eb61b0e306cb6de4a71"
+    else
+      version "1.2.2685.61"
+      sha256 "ae88691be2a4a2c07a87e6a5441fa7c99755418830e70ac52fb2d2ca557f402c"
+    end
+
+    url "https://github.com/pritunl/pritunl-client-electron/releases/download/#{version}/Pritunl.pkg.zip",
+        verified: "github.com/pritunl/pritunl-client-electron/"
+
+    pkg "Pritunl.pkg"
   else
-    version "1.2.2653.1"
-    sha256 "af0170b1641ea9cdb79f030c0e15a8cf00eddf43d906fd68b5566cea4bc3111c"
+    version "1.2.2685.61"
+    sha256 "daa6871edc484bbfc7e8035f6844b2dffa340ebd3e0f92e8a5b3a8d94092d257"
+
+    url "https://github.com/pritunl/pritunl-client-electron/releases/download/#{version}/Pritunl.arm64.pkg.zip",
+        verified: "github.com/pritunl/pritunl-client-electron/"
+
+    pkg "Pritunl.arm64.pkg"
   end
 
-  url "https://github.com/pritunl/pritunl-client-electron/releases/download/#{version}/Pritunl.pkg.zip",
-      verified: "github.com/pritunl/pritunl-client-electron/"
   appcast "https://github.com/pritunl/pritunl-client-electron/releases.atom"
   name "Pritunl"
   desc "OpenVPN client"
   homepage "https://client.pritunl.com/"
-
-  pkg "Pritunl.pkg"
 
   uninstall pkgutil:   "com.pritunl.pkg.Pritunl",
             launchctl: [
