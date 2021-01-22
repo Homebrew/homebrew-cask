87f31e2a1b6ffd8c4774b0d03864051d4a049f3a
Yusuf Kürşad Kaya <ykursadkaya@gmail.com>
2021-01-22 19:27:32 +0300

    Update reaper from 6.19.0,6.19 to 6.20.0,6.20 (#98516)

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,14 +1,14 @@
 cask "reaper" do
-  version "6.19.0,6.19"
+  version "6.20.0,6.20"
 
   if MacOS.version <= :mojave
-    sha256 "0d3eeaee7a014dff45c3343b2763b21d1afb897473b0c9bf160702522e77f0f8"
+    sha256 "ad8ac6bce453ea3aa5acbd7aa059a6686cfe85ac0e561bf9bc95c1ec3b886667"
 
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.after_comma.no_dots}_x86_64.dmg"
   else
-    sha256 "1c04ed6d0dddfb3f0c9890293044050801aaf55ca8c55952db03065275ee8e3f"
+    sha256 "69af746d23374b39bdfb5ebfc586dfd7f6ca94f4f6d752801ede1e37c7c390f0"
 
-    url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.after_comma.no_dots}_x86_64_catalina.dmg"
+    url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.after_comma.no_dots}_x86_64-catalina.dmg"
   end
 
   appcast "https://www.cockos.com/reaper/latestversion/?p=osx_64",
