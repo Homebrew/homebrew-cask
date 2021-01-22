33ee35c8f5f6f9bf815d323d67c986d4357f8be4
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-01-22 09:32:19 +0900

    sharemouse: update to 5.0.46 (#98469)
    * sharemouse: update to 5.0.46
    
    * Update sharemouse.rb
    
    * Update sharemouse.rb
    
    Co-authored-by: Markus Reiter <me@reitermark.us>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/sharemouse.rb
+++ Casks/sharemouse.rb
@@ -1,10 +1,11 @@
 cask "sharemouse" do
-  version "5.0.45"
+  version "5.0.46"
   sha256 :no_check
 
   url "https://www.sharemouse.com/ShareMouseSetup.dmg"
   appcast "https://www.sharemouse.com/download/"
   name "ShareMouse"
+  desc "Share peripherals between computers"
   homepage "https://www.sharemouse.com/"
 
   app "ShareMouse.app"
