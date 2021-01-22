7d6bc56e739225c0fad8a15666c35b9dca245dd1
Connor Gray <connorg@wolfram.com>
2021-01-22 06:44:03 -0600

    Add `verified` parameter to wolfram-engine.rb `url` field

--- Casks/wolfram-engine.rb
+++ Casks/wolfram-engine.rb
@@ -2,7 +2,8 @@ cask "wolfram-engine" do
   version "12.2.0"
   sha256 "5b3e10720b2e0a87cfb8732393c893a7b2efaad172ce50a35ef348603f0a010c"
 
-  url "https://files.wolframcdn.com/packages/Homebrew/#{version}.0/WolframEngine_#{version}_MAC.dmg"
+  url "https://files.wolframcdn.com/packages/Homebrew/#{version}.0/WolframEngine_#{version}_MAC.dmg",
+    verified: "files.wolframcdn.com/packages/"
   name "Wolfram Engine"
   desc "Evaluator for the Wolfram Language"
   homepage "https://www.wolfram.com/engine/"
