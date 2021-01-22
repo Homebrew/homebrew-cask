164a54c2d87eae259daebb4258e5748b7d00d97f
Logan Rosen <lrosen@linkedin.com>
2021-01-22 14:51:17 -0500

    mat: add livecheck

--- Casks/mat.rb
+++ Casks/mat.rb
@@ -3,9 +3,14 @@ cask "mat" do
   sha256 "e2edd8875aa5a100cf30f1423830cb1ecabbdc4414fbf7dd47c658547631916b"
 
   url "https://www.eclipse.org/downloads/download.php?r=1&file=/mat/#{version.major_minor_patch}/rcp/MemoryAnalyzer-#{version}-macosx.cocoa.x86_64.zip"
-  appcast "https://www.eclipse.org/mat/downloads.php"
   name "Eclipse Memory Analyzer"
+  desc "Java heap analyzer"
   homepage "https://www.eclipse.org/mat/"
+
+  livecheck do
+    url "https://www.eclipse.org/mat/downloads.php"
+    regex(/href=.*?MemoryAnalyzer-(\d+\.\d+\.\d+\.\d{8}).+\.zip/i)
+  end
 
   app "mat.app"
 end
