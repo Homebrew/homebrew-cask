b6463599935f12a1825a953af15549c763b7838c
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-23 13:20:35 +0800

    Update save-hollywood.rb

--- Casks/save-hollywood.rb
+++ Casks/save-hollywood.rb
@@ -7,5 +7,11 @@ cask "save-hollywood" do
   desc "Screen saver for custom video files"
   homepage "http://s.sudre.free.fr/Software/SaveHollywood/about.html"
 
+  livecheck do
+    url "http://s.sudre.free.fr/Software/SaveHollywood/release_notes.html"
+    strategy :page_match
+    regex(/Release_notes_Version.*?(\d+(?:\.\d+)*)/i)
+  end
+
   screen_saver "SaveHollywood.saver"
 end
