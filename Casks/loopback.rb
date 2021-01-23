38f6007b27315b268d7fd7352c87307a2de7a175
Yusuf Kürşad Kaya <ykursadkaya@gmail.com>
2021-01-24 02:46:00 +0300

    loopback: add livecheck (#98568)

--- Casks/loopback.rb
+++ Casks/loopback.rb
@@ -4,10 +4,14 @@ cask "loopback" do
 
   url "https://d2oxtzozd38ts8.cloudfront.net/loopback/download/Loopback.zip",
       verified: "d2oxtzozd38ts8.cloudfront.net/loopback/"
-  appcast "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.loopback&system=10141&platform=osx&arch=x86_64&version=2008000"
   name "Loopback"
   desc "Cable-free audio router"
   homepage "https://rogueamoeba.com/loopback/"
+
+  livecheck do
+    url "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.loopback&system=10141&platform=osx&arch=x86_64&version=2008000"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :sierra"
