78c03ca16e2ac874351b08fc0ea8fc1bfbd1ecc7
subic <subic@users.noreply.github.com>
2021-01-23 01:21:36 +0100

    Update SaveHollywood to 2.5
    Project version history is available in the release notes: http://s.sudre.free.fr/Software/SaveHollywood/release_notes.html

--- Casks/save-hollywood.rb
+++ Casks/save-hollywood.rb
@@ -1,9 +1,10 @@
 cask "save-hollywood" do
-  version :latest
+  version "2.5"
   sha256 :no_check
 
   url "http://s.sudre.free.fr/Software/files/SaveHollywood.dmg"
   name "SaveHollywood Screensaver"
+  desc "Screen saver for custom video files"
   homepage "http://s.sudre.free.fr/Software/SaveHollywood/about.html"
 
   screen_saver "SaveHollywood.saver"
