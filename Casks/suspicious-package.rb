c1e2806f68807032d0106f9d128ce7fdcb0d27db
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-23 09:05:06 +0800

    suspicious-package: add livecheck (#98451)

--- Casks/suspicious-package.rb
+++ Casks/suspicious-package.rb
@@ -3,10 +3,18 @@ cask "suspicious-package" do
   sha256 :no_check
 
   url "https://www.mothersruin.com/software/downloads/SuspiciousPackage.dmg"
-  appcast "https://www.mothersruin.com/software/SuspiciousPackage/data/SuspiciousPackageVersionInfo.plist"
   name "Suspicious Package"
   desc "Application for inspecting installer packages"
   homepage "https://www.mothersruin.com/software/SuspiciousPackage/"
+
+  livecheck do
+    url "https://www.mothersruin.com/software/SuspiciousPackage/data/SuspiciousPackageVersionInfo.plist"
+    strategy :page_match do |page|
+      svs = page.match(/CFBundleShortVersionString.*?\n.*?(\d+(?:\.\d+)*)/i)
+      bv = page.match(/CFBundleVersion.*?\n.*?(\d+(?:\.\d+)*)/i)
+      "#{svs[1]},#{bv[1]}"
+    end
+  end
 
   depends_on macos: ">= :mojave"
 
