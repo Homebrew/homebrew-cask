6f08900dfb95e837d8d1023ff7e5fecfe0cd0ea2
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-01-23 10:06:24 +0900

    anydo: add livecheck (#98517)
    * anydo: add livecheck
    
    * Update anydo.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/anydo.rb
+++ Casks/anydo.rb
@@ -3,10 +3,15 @@ cask "anydo" do
   sha256 "e3d47c92bcabc1fe053a7fef16790139ea7ff50386f909996367f5af81db667f"
 
   url "https://electron-app.any.do/Any.do-#{version}.dmg"
-  appcast "https://electron-app.any.do/latest-mac.yml"
   name "Any.do"
   desc "Reminder, planner & calendar"
   homepage "https://www.any.do/"
+
+  livecheck do
+    url "https://electron-app.any.do/latest-mac.yml"
+    strategy :page_match
+    regex(/version: (\d+(?:\.\d+)*)/i)
+  end
 
   depends_on macos: ">= :catalina"
 
