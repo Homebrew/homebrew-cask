485537dbe8c1afb1b557a58b5c2046536d07a77a
Michael Lutaaya <michael@lutaaya.com>
2021-01-23 15:08:27 -0500

    Update nova from 4.1 to 4.2 (#98573)

--- Casks/nova.rb
+++ Casks/nova.rb
@@ -1,6 +1,6 @@
 cask "nova" do
-  version "4.1"
-  sha256 "e416d0543d0ed00434fc720dcf97ab849ed08a88a98b472715e54220bd47bd5e"
+  version "4.2"
+  sha256 "e759ea62bc4aa13086ddef214f3a68e1bfcf3dd46c77e0419b467a97ee6d0606"
 
   url "https://download-cdn.panic.com/nova/Nova%20#{version}.zip",
       verified: "https://download-cdn.panic.com/nova/"
