2af66d542dfbb7393f1e6f17f07db8594478f621
Karl Sauter <karl.sauter@gmail.com>
2021-01-24 00:26:40 -0700

    Fixed sha256

--- Casks/logos.rb
+++ Casks/logos.rb
@@ -1,6 +1,6 @@
 cask "logos" do
   version "9.2.0.0014"
-  sha256 "ad7b53b69d2118de3a2739ca76d91909905d81b2da7d7ef3fa45cc293d59e967"
+  sha256 "cc82f4c116145b7e67a985d57a4c302627d1fedd67099894454dbee4a0ae9c9c"
 
   url "https://downloads.logoscdn.com/LBS#{version.major}/Installer/#{version}/LogosMac.dmg",
       verified: "downloads.logoscdn.com/"
