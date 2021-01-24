fa9a8ce060c6bb847edf3e51462128d163eedea4
Thierry Moisan <thierry.moisan@gmail.com>
2021-01-24 16:30:07 -0500

    Update angry-ip-scanner from 3.7.3 to 3.7.4 (#98620)

--- Casks/angry-ip-scanner.rb
+++ Casks/angry-ip-scanner.rb
@@ -1,6 +1,6 @@
 cask "angry-ip-scanner" do
-  version "3.7.3"
-  sha256 "b411cbafc30aeeb59c69803b5f962f87a653fdf4a4a6f4292ecb6280978c0cc2"
+  version "3.7.4"
+  sha256 "cd8c433651e8c1f9442c29ef575704a9a81168dd38e56ba882c02d1aa372c545"
 
   url "https://github.com/angryip/ipscan/releases/download/#{version}/ipscan-mac-#{version}.zip",
       verified: "github.com/angryip/ipscan/"
