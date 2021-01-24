667d459e2d94b815e3287a9c3aa1f26b875321d9
Reno <singularitti@outlook.com>
2021-01-24 04:05:34 -0500

    Update understand from 5.1.1029 to 6.0.1050 (#98590)
    * Update understand from 5.1.1029 to 6.0.1050
    
    * Update understand.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/understand.rb
+++ Casks/understand.rb
@@ -1,10 +1,11 @@
 cask "understand" do
-  version "5.1.1029"
-  sha256 "1ed79219c267d2da77efabeedfcbd790b2c4475bf3eedf4b0395426a47961d4e"
+  version "6.0.1050"
+  sha256 "7438196f90ad0d83f160172a5d6929cece6a856c33ee1a96a606d92f2888a9b2"
 
   url "http://builds.scitools.com/all_builds/b#{version.patch}/Understand/Understand-#{version}-MacOSX-x86.dmg"
-  appcast "https://scitools.com/download/all-builds/"
+  appcast "https://licensing.scitools.com/download/getChecksums/Understand/#{version.patch}"
   name "SciTools Understand"
+  desc "Code visualization and exploration tool"
   homepage "https://scitools.com/features/"
 
   app "Understand.app"
