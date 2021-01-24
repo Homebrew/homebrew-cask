b922c9f34559b3d3b0fc580af70fd47f55329998
Cláudio de Oliveira Pereira <pclaudio.ans@gmail.com>
2021-01-24 00:16:00 -0400

    Update oracle-jdk-javadoc from 15.0.1,9:51f4f36ad4ef43e39d0dfdbaf6549… (#98600)

--- Casks/oracle-jdk-javadoc.rb
+++ Casks/oracle-jdk-javadoc.rb
@@ -1,6 +1,6 @@
 cask "oracle-jdk-javadoc" do
-  version "15.0.1,9:51f4f36ad4ef43e39d0dfdbaf6549e32"
-  sha256 "3d761155f111bcc16e643432c39241d97e3d7bed202e5376b2416d67b2a696e8"
+  version "15.0.2,7:0d1cfde4252546c6931946de8db48ee2"
+  sha256 "d3616a9a3f91d11dc595861edc9f46fc88c6537471637741381385171633c491"
 
   url "https://download.oracle.com/otn-pub/java/jdk/#{version.before_comma}+#{version.after_comma.before_colon}/#{version.after_colon}/jdk-#{version.before_comma}_doc-all.zip",
       cookies: {
