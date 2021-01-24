1a18bdc28c04204db646421f7bc007d0ce3212ec
Quang Lam <quang.lam@webcatalog.app>
2021-01-25 00:08:44 +0700

    Update webcatalog from 28.0.2 to 28.0.3 (#98615)

--- Casks/webcatalog.rb
+++ Casks/webcatalog.rb
@@ -1,6 +1,6 @@
 cask "webcatalog" do
-  version "28.0.2"
-  sha256 "8a988fe4b08060859db9ba859e12ce743bd34284269b517395d35238237a5c7e"
+  version "28.0.3"
+  sha256 "c537990efb2dec5cb490b72cf990a4add52fb55e73d31c7b684af50f9cd470f2"
 
   url "https://github.com/webcatalog/webcatalog-app/releases/download/v#{version}/WebCatalog-#{version}.dmg",
       verified: "github.com/webcatalog/webcatalog-app/"
