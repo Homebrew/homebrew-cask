1375299a9176fd25adae1038fb93958b99da9bef
Bevan Kay <bevanjkay@gmail.com>
2021-01-25 10:12:14 +1100

    Update stats from 2.4.13 to 2.4.14 (#98642)

--- Casks/stats.rb
+++ Casks/stats.rb
@@ -1,6 +1,6 @@
 cask "stats" do
-  version "2.4.13"
-  sha256 "a11f8cae5f43a912d5243a54cd24734acc2ca7e117eaea740b4b6e0ab4f8de99"
+  version "2.4.14"
+  sha256 "bf144cbea9f84ae87a37cce0fc2e7915ba533957bcc0016e76a772ca6fe446e5"
 
   url "https://github.com/exelban/stats/releases/download/v#{version}/Stats.dmg"
   appcast "https://github.com/exelban/stats/releases.atom"
