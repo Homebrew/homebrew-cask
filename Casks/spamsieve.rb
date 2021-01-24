099824400b469092e126da4bd864e4d7121468c2
Sebastian Riehm <herrriehm@users.noreply.github.com>
2021-01-24 11:55:52 +0100

    Update spamsieve from 2.9.41 to 2.9.42 (#98605)

--- Casks/spamsieve.rb
+++ Casks/spamsieve.rb
@@ -1,6 +1,6 @@
 cask "spamsieve" do
-  version "2.9.41"
-  sha256 "9241b80bfe2687eb4cc058b5875f0ad964c79003e9490834ff1ee59206f5ed66"
+  version "2.9.42"
+  sha256 "57ea89729714da64388e98e4a1ff0f867a83454290adb876367a2fc15961f29b"
 
   url "https://c-command.com/downloads/SpamSieve-#{version}.dmg"
   name "SpamSieve"
