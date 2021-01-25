239afcf1548982ed4b04c297400ad377475bf137
Adithya Sunil <adithyasunil26@gmail.com>
2021-01-26 01:02:54 +0400

    Update prepros from 7.3.30 to 7.3.31 (#98679)

--- Casks/prepros.rb
+++ Casks/prepros.rb
@@ -1,6 +1,6 @@
 cask "prepros" do
-  version "7.3.30"
-  sha256 "bc5f2f9386566e56b50350750d30a07267afc37a2189fc3ff2a1207f626fc473"
+  version "7.3.31"
+  sha256 "ee627481d1d2b6949df39c24f40a409ca090322539b41792c5d168e9a8598016"
 
   url "https://downloads.prepros.io/v#{version.major}/Prepros-#{version}.zip"
   name "Prepros"
