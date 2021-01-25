b0304b97ad2e28faec270b055894229240e729b6
Adithya Sunil <adithyasunil26@gmail.com>
2021-01-26 01:09:21 +0400

    Update proclaim from 3.0.0.0048,310.0.48 to 3.1.0.0024,311.0.24 (#98682)

--- Casks/proclaim.rb
+++ Casks/proclaim.rb
@@ -1,6 +1,6 @@
 cask "proclaim" do
-  version "3.0.0.0048,310.0.48"
-  sha256 "22e790a0a777d6cdbf65d2820f890bb1d15e4f6859a33b5ec9a65996a95462da"
+  version "3.1.0.0024,311.0.24"
+  sha256 "89e6f3b21d33db97955317dadaca0a06c512fd380efb7d09afd9abf46786d7b1"
 
   url "https://downloads.logoscdn.com/Proclaim/Installer/#{version.before_comma}/Proclaim.dmg",
       verified: "logoscdn.com/Proclaim/"
