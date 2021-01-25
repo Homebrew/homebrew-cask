a44c039663926a3f61b1fe06604a2918f049710b
Adithya Sunil <adithyasunil26@gmail.com>
2021-01-26 00:55:43 +0400

    Update portfolioperformance from 0.50.2 to 0.50.3 (#98675)

--- Casks/portfolioperformance.rb
+++ Casks/portfolioperformance.rb
@@ -1,6 +1,6 @@
 cask "portfolioperformance" do
-  version "0.50.2"
-  sha256 "8bee8ef3a1c7ea2f686732a2392f50d94e8558f3cda3584e4b620a658bed29b9"
+  version "0.50.3"
+  sha256 "02cb1c5dc87adb3205df416294570673341c32084583f544b7317585ca6d9cfe"
 
   url "https://github.com/buchen/portfolio/releases/download/#{version}/PortfolioPerformance-#{version}.dmg",
       verified: "github.com/buchen/portfolio/"
