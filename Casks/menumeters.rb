6ac1aa8329462bb34782794b357acf504d3e6bff
Marc Schwede <9077622+schwedenmut@users.noreply.github.com>
2021-01-25 18:47:36 +0100

    Update menumeters from 2.1.2.2 to 2.1.3 (#98667)

--- Casks/menumeters.rb
+++ Casks/menumeters.rb
@@ -1,6 +1,6 @@
 cask "menumeters" do
-  version "2.1.2.2"
-  sha256 "42e8de5f751d162ddc076c8b4c831b8a073e179fe34976a3c90657e8ddad303b"
+  version "2.1.3"
+  sha256 "6796a69f2e93bd1b5df4b10ef2650391c33444e20ef348e2066b50ea7365240e"
 
   url "https://github.com/yujitach/MenuMeters/releases/download/#{version}/MenuMeters_#{version}.zip",
       verified: "github.com/yujitach/MenuMeters/"
