f326199ce7f417237b18fc0522d5d032ff5eb0ac
Seeker <meaningseeking@protonmail.com>
2021-01-25 13:13:22 -0800

    Update glyphs from 3.0.2,3052 to 3.0.2,3055 (#98683)

--- Casks/glyphs.rb
+++ Casks/glyphs.rb
@@ -1,6 +1,6 @@
 cask "glyphs" do
-  version "3.0.2,3052"
-  sha256 "ee814f77313caa1ac14d48d1b4ef3a820d13f0f03df6e2e73de194a00a72f973"
+  version "3.0.2,3055"
+  sha256 "91b08152ebdc3e7d5cb7cfa7d7b04cf6a9d2b3f2c5e76e0b1145d7c6438b3c81"
 
   url "https://updates.glyphsapp.com/Glyphs#{version.before_comma}-#{version.after_comma}.zip"
   name "Glyphs"
