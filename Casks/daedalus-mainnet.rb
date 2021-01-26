cd479f634f1c9de61e5dbbb53b76e769fa1106ea
yottalogical <yottalogical@gmail.com>
2021-01-26 14:27:21 -0500

    Update daedalus-mainnet from 3.1.0 to 3.2.0 (#98744)

--- Casks/daedalus-mainnet.rb
+++ Casks/daedalus-mainnet.rb
@@ -1,6 +1,6 @@
 cask "daedalus-mainnet" do
-  version "3.1.0,15733"
-  sha256 "b2929ec4c7dab03cd69db73d48e90223a51c05ea990727deaedc3a545d6d2e15"
+  version "3.2.0,16053"
+  sha256 "a7716a7aa79343ed0da92fa0d3065bdb2a16adc3703f9464f260ae2e5785bba3"
 
   url "https://update-cardano-mainnet.iohk.io/daedalus-#{version.before_comma}-mainnet-#{version.after_comma}.pkg",
       verified: "update-cardano-mainnet.iohk.io/"
