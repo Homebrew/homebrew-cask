e6834befae55635990fbb193f59a1646a77b52f6
Alexander Zigelski <7140800+crunchtime-ali@users.noreply.github.com>
2021-01-26 23:23:49 +0100

    Update kui from 9.3.11 to 9.3.12 (#98766)

--- Casks/kui.rb
+++ Casks/kui.rb
@@ -1,6 +1,6 @@
 cask "kui" do
-  version "9.3.11"
-  sha256 "4299a7edbb7dc42d7a7d76cd16af05a46d5439b6943a09fac534a786744ab96e"
+  version "9.3.12"
+  sha256 "f1df4b254518a284311b64062b817aa3c48db5c87d74e3909bb87c9b6bffbce1"
 
   url "https://github.com/IBM/kui/releases/download/v#{version}/Kui-darwin-x64.tar.bz2",
       verified: "github.com/IBM/kui/"
