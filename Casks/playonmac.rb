fd620226da0a285ae4681d8ada320130289a0e65
zandr <7629614+deathlyfrantic@users.noreply.github.com>
2021-01-26 07:47:31 -0500

    Fix hash in playonmac.rb (#98726)

--- Casks/playonmac.rb
+++ Casks/playonmac.rb
@@ -1,6 +1,6 @@
 cask "playonmac" do
   version "4.4.2"
-  sha256 "5c7300ca7115335a732cd4c02d07ed80aea1d5b11126677bfbce96a408ff48fb"
+  sha256 "8f458bcf14ed7431acfec1dc79d0e917bf450da89c78a13b5a6f3deeddbe4a8b"
 
   url "https://repository.playonmac.com/PlayOnMac/PlayOnMac_#{version}.dmg"
   appcast "https://repository.playonmac.com/PlayOnMac/"
