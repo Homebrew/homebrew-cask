e7ced1392f9d85dc4cae6acff37798e29fabf7fe
Seeker <meaningseeking@protonmail.com>
2021-01-25 23:02:34 -0800

    Update nteract from 0.27.0 to 0.28.0 (#98715)

--- Casks/nteract.rb
+++ Casks/nteract.rb
@@ -1,6 +1,6 @@
 cask "nteract" do
-  version "0.27.0"
-  sha256 "d28b4df9f8c62f33e603d276c5323afc19a91071f9a7aa030d1a6b42dd6f60c1"
+  version "0.28.0"
+  sha256 "de65abe5ed76489217a9c29bcc177aa5b2ee2f0657cd017301af33280ca8a737"
 
   url "https://github.com/nteract/nteract/releases/download/v#{version}/nteract-#{version}.dmg"
   appcast "https://github.com/nteract/nteract/releases.atom"
