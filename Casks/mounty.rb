5471bc63da691f4e865c671130b01f07258d08e8
tk4k <54473116+tk4k@users.noreply.github.com>
2021-01-26 13:14:38 +0900

    Update mounty.rb

--- Casks/mounty.rb
+++ Casks/mounty.rb
@@ -1,13 +1,17 @@
 cask "mounty" do
-  version "1.10,14"
-  sha256 :no_check
+  if MacOS.version <= :catalina
+    version "1.9"
+    sha256 :no_check
+    url "https://mounty.app/releases/Mounty-#{version}.dmg"
+  else
+    version "1.11,16"
+    sha256 :no_check
+    url "https://mounty.app/releases/Mounty.dmg"
+  end
 
-  url "https://mounty.app/releases/Mounty.dmg"
   name "Mounty for NTFS"
   desc "Re-mounts write-protected NTFS volumes"
   homepage "https://mounty.app/"
-
-  depends_on macos: ">= :big_sur"
 
   app "Mounty.app"
 end
