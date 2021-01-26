b42c645bc71e54c558f3aeeb106aa5d2b06e8c15
Curtis Whitby <cwhitby@users.noreply.github.com>
2021-01-26 11:20:11 +0000

    Update flutter from 1.22.5 to 1.22.6 (#98724)

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -1,6 +1,6 @@
 cask "flutter" do
-  version "1.22.5"
-  sha256 "a64f5df488b95ac41036559bf436189037a868b393b1c050649cbb61d7d3b932"
+  version "1.22.6"
+  sha256 "a5a7b66c1e4ac4058c72714d67bb863a80170749fe6f11f541e314474c899b05"
 
   url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_#{version}-stable.zip",
       verified: "storage.googleapis.com/flutter_infra/"
