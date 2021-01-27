e8e2a0976b9f886fa713aa7b24f7481d9142cce6
Thomas Gillis <25429825+thomasgillis@users.noreply.github.com>
2021-01-26 23:22:09 -0500

    Update paraview from 5.8.1 to 5.9.0 (#98738)
    * Update paraview from 5.8.1 to 5.9.0
    
    * Update paraview.rb
    
    * Update paraview.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/paraview.rb
+++ Casks/paraview.rb
@@ -1,8 +1,8 @@
 cask "paraview" do
-  version "5.8.1"
-  sha256 "df0de9347c8bfc674ffd2cf88a72ddc0005545ffa68412d63b8631466096656e"
+  version "5.9.0"
+  sha256 "cbb475816400ed1f24319826d38a4b87564d46765fd9000b2e59296b7dd0bf41"
 
-  url "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v#{version.major_minor}&type=binary&os=macOS&downloadFile=ParaView-#{version}-MPI-OSX10.12-Python2.7-64bit.dmg",
+  url "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v#{version.major_minor}&type=binary&os=macOS&downloadFile=ParaView-#{version}-MPI-OSX10.13-Python3.8-64bit.dmg",
       user_agent: :fake
   appcast "https://www.paraview.org/files/listing.txt"
   name "ParaView"
