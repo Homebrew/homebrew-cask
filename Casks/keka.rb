3653e38c3b133bf065e51db921bc210ee6a2f353
Yurii Kolesnykov <root@yurikoles.com>
2021-01-27 17:13:47 +0200

    Update keka from 1.2.7 to 1.2.8 (#98784)
    * Update keka from 1.2.7 to 1.2.8
    
    * Update keka.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/keka.rb
+++ Casks/keka.rb
@@ -1,6 +1,6 @@
 cask "keka" do
-  version "1.2.7"
-  sha256 "53586a0d994cc34f47fc58c48b1280acd442ef4e14c16b5608b402b6f87f3f11"
+  version "1.2.8"
+  sha256 "b055d03ec8501bffb8f83ade391c1d6c88bb803b83c18a6cde5e64196dbd66ee"
 
   url "https://github.com/aonez/Keka/releases/download/v#{version}/Keka-#{version}.dmg",
       verified: "github.com/aonez/Keka/"
@@ -10,7 +10,7 @@ cask "keka" do
   homepage "https://www.keka.io/"
 
   auto_updates true
-  conflicts_with cask: "keka-beta"
+  conflicts_with cask: "homebrew/cask-versions/keka-beta"
 
   app "Keka.app"
 
