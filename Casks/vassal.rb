cf525e33d67d6b182757c7fbc45bcd85ebfab662
Adithya Sunil <adithyasunil26@gmail.com>
2021-01-28 02:23:28 +0400

    Update vassal from 3.4.12 to 3.4.13 (#98815)

--- Casks/vassal.rb
+++ Casks/vassal.rb
@@ -1,6 +1,6 @@
 cask "vassal" do
-  version "3.4.12"
-  sha256 "98d39278823868310761ded273dac6532373ba68e7a112f13839e6c4d7009ac6"
+  version "3.4.13"
+  sha256 "300b6c5e0837172019813b399d924c5e880ee9b7821b0ab90a9253027860ff70"
 
   url "https://github.com/vassalengine/vassal/releases/download/#{version}/VASSAL-#{version}-macosx.dmg",
       verified: "github.com/vassalengine/vassal/"
