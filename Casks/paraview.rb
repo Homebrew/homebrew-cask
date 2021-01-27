46cb0ce40f955dda3e57384d007192fceb380a48
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-27 12:09:27 +0800

    Update paraview.rb

--- Casks/paraview.rb
+++ Casks/paraview.rb
@@ -2,7 +2,7 @@ cask "paraview" do
   version "5.9.0"
   sha256 "e1303d4817f07bb50a47f075fff05f9d591a5b7ba23b2bd35e7e3390887e42bd"
 
-  url "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v#{version.major_minor}&type=binary&os=macOS&downloadFile=ParaView-#{version}-MPI-OSX10.12-Python2.7-64bit.dmg",
+  url "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v#{version.major_minor}&type=binary&os=macOS&downloadFile=ParaView-#{version}-MPI-OSX10.13-Python3.8-64bit.dmg",
       user_agent: :fake
   appcast "https://www.paraview.org/files/listing.txt"
   name "ParaView"
