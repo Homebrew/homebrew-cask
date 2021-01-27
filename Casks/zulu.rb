ee50f30c26843b4f86ed41f902958983f77aa7f3
Kai Liu <15362012+erinacio@users.noreply.github.com>
2021-01-27 11:07:04 +0800

    Unify zulu x86_64 and arm64 version (#98723)

--- Casks/zulu.rb
+++ Casks/zulu.rb
@@ -1,13 +1,13 @@
 cask "zulu" do
+  version "15.0.2,15.29.15-ca"
+
   if Hardware::CPU.intel?
-    version "15.0.2,15.29.15-ca"
     sha256 "6284c7fb89cbbc8552788a3db522f6226a64d84454d21e075558c050328f6ed7"
 
     url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
         referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
   else
-    version "15.0.1,15.28.1013-ca"
-    sha256 "055a493236ed0023216f40d4e222ebc9cebd13bcaa8288d9f6c6c5c2d61f30ee"
+    sha256 "599c3399eb6edb824f4660ba0a4fbb52118458041700dc71aec891bd383910e5"
 
     url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_aarch64.dmg",
         referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
