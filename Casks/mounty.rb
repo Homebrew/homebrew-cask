6f8298c4778c7a4c1f46f7d39f44d1ff6a7effb9
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-27 11:14:38 +0800

    Update mounty.rb

--- Casks/mounty.rb
+++ Casks/mounty.rb
@@ -1,8 +1,8 @@
 cask "mounty" do
   if MacOS.version <= :catalina
-    version "1.9"
-    sha256 :no_check
-    url "https://mounty.app/releases/Mounty-#{version}.dmg"
+    version "1.9,13"
+    sha256 "5fcedfe712f59c14f39c3385dfed9aebc99d4e8d88f6e870f364cc48624590ef"
+    url "https://mounty.app/releases/Mounty-#{version.before_comma}.dmg"
   else
     version "1.11,16"
     sha256 :no_check
