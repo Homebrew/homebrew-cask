93656cff58b2f14d5689919a8af3f5c4aeea8046
OttNorml <2350859+ottnorml@users.noreply.github.com>
2021-01-27 04:09:11 +0100

    Update intellij-idea-ce from 2020.3.1,203.6682.168 to 2020.3.2 (#98741)
    * Update intellij-idea-ce from 2020.3.1,203.6682.168 to 2020.3.2
    
    * Update intellij-idea-ce.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/intellij-idea-ce.rb
+++ Casks/intellij-idea-ce.rb
@@ -1,14 +1,14 @@
 cask "intellij-idea-ce" do
-  version "2020.3.1,203.6682.168"
+  version "2020.3.2"
 
   if Hardware::CPU.intel?
-    sha256 "8ec4c857c1d7c65f7fc9a0db21ac4e282f98d29d9b4c86bb00ff410504855062"
+    sha256 "008259b4df15cea0d38edc55e166dc24b7973ad3a4985ce2ebaa02b6023a66ac"
 
-    url "https://download.jetbrains.com/idea/ideaIC-#{version.before_comma}.dmg"
+    url "https://download.jetbrains.com/idea/ideaIC-#{version}.dmg"
   else
     sha256 "440157793e71aabe15385dbd0fc5d94bbf6e27dae53564ce49fcf866f1468b6f"
 
-    url "https://download.jetbrains.com/idea/ideaIC-#{version.before_comma}-aarch64.dmg"
+    url "https://download.jetbrains.com/idea/ideaIC-#{version}-aarch64.dmg"
   end
 
   appcast "https://data.services.jetbrains.com/products/releases?code=IIC&latest=true&type=release"
