88f665755ef7ccb8260cdabe8f5b24914328d5df
Vítor Galvão <info@vitorgalvao.com>
2021-01-27 02:58:15 +0000

    geph.rb: remove appcast (#98746)

--- Casks/geph.rb
+++ Casks/geph.rb
@@ -3,7 +3,6 @@ cask "geph" do
   sha256 "0c2a2b0ba3b8ccf1acc66be891b3960716489fb4974e342a7dc9759552199d18"
 
   url "https://dl.geph.io/desktop-builds/geph-macos-#{version}.dmg"
-  appcast "https://geph.io/"
   name "Geph"
   homepage "https://geph.io/"
 
