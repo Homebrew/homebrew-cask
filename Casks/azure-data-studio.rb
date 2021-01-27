0d0068533c66adebbec253bee7e21c362ad84980
Seeker <meaningseeking@protonmail.com>
2021-01-26 19:22:50 -0800

    azure-data-studio: add livecheck (#98702)

--- Casks/azure-data-studio.rb
+++ Casks/azure-data-studio.rb
@@ -1,13 +1,17 @@
 cask "azure-data-studio" do
-  version "1.25.2,4d4917d328ab7a7526817a0f0ef03237a79aa54d"
+  version "1.25.2"
   sha256 "f97e3e78d2ba0caea1d74bd3c93defd174f5f48643b9b749f807f13ce24c941c"
 
-  url "https://sqlopsbuilds.azureedge.net/stable/#{version.after_comma}/azuredatastudio-macos-#{version.before_comma}.zip",
-      verified: "sqlopsbuilds.azureedge.net/stable/"
-  appcast "https://github.com/Microsoft/azuredatastudio/releases.atom"
+  url "https://azuredatastudio-update.azurewebsites.net/#{version}/darwin/stable",
+      verified: "azuredatastudio-update.azurewebsites.net/"
   name "Azure Data Studio"
   desc "Data management tool that enables working with SQL Server"
   homepage "https://docs.microsoft.com/en-us/sql/azure-data-studio/"
+
+  livecheck do
+    url "https://github.com/Microsoft/azuredatastudio/releases"
+    strategy :github_latest
+  end
 
   auto_updates true
 
