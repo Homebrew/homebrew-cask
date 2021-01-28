937466ca95903770b811e647f114c5262700cac0
Seeker <meaningseeking@protonmail.com>
2021-01-28 00:27:23 -0800

    Update project-slippi-dolphin from 2.2.0 to 2.2.5

--- Casks/project-slippi-dolphin.rb
+++ Casks/project-slippi-dolphin.rb
@@ -1,18 +1,18 @@
 cask "project-slippi-dolphin" do
-  version "2.2.0"
-  sha256 "dcb9ce958b53a283894ef96b9ffeb159b680ca50c1adbddf2342884c7cd5ffe8"
+  version "2.2.5"
+  sha256 "1106626ec4531b5cc3906a91f6f72605cad63eebed47fa1b348a53698c5492d5"
 
-  url "https://github.com/project-slippi/Ishiiruka/releases/download/v#{version}/FM-Slippi-#{version}-Mac.zip",
+  url "https://github.com/project-slippi/Ishiiruka/releases/download/v#{version}/FM-Slippi-#{version}-Mac.dmg",
       verified: "github.com/project-slippi/Ishiiruka/"
   appcast "https://github.com/project-slippi/Ishiiruka/releases.atom"
   name "Slippi"
   homepage "https://slippi.gg/"
 
-  app "Dolphin.app"
+  app "Slippi Dolphin.app"
 
   zap trash: [
-    "~/Library/Application Support/Dolphin",
-    "~/Library/Preferences/org.dolphin-emu.dolphin.plist",
+    "~/Library/Application Support/Slippi Dolphin",
+    "~/Library/Preferences/org.project-slippi.dolphin.plist",
     "~/Slippi",
   ]
 end
