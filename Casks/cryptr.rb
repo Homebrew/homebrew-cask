079b3d1e0041cd3cb8aa81828e0cdd54e90e845a
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-27 21:37:58 -0800

    cryptr: add livecheck (#98838)

--- Casks/cryptr.rb
+++ Casks/cryptr.rb
@@ -3,10 +3,14 @@ cask "cryptr" do
   sha256 "8b85da55b3b7e79ec935b794bda997e899211a9ed3ba96e0bf2887bd2799e81b"
 
   url "https://github.com/adobe/cryptr/releases/download/v#{version}/Cryptr-#{version}.dmg"
-  appcast "https://github.com/adobe/cryptr/releases.atom"
   name "Cryptr"
   desc "GUI for Hashicorp's Vault"
   homepage "https://github.com/adobe/cryptr"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "Cryptr.app"
 end
