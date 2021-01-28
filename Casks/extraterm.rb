593c5ff9eab4180f41f8c9d02f8bc711e754f383
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-27 21:04:55 -0800

    extraterm: add livecheck (#98858)

--- Casks/extraterm.rb
+++ Casks/extraterm.rb
@@ -4,10 +4,14 @@ cask "extraterm" do
 
   url "https://github.com/sedwards2009/extraterm/releases/download/v#{version}/extraterm-#{version}-darwin-x64.zip",
       verified: "github.com/sedwards2009/extraterm/"
-  appcast "https://github.com/sedwards2009/extraterm/releases.atom"
   name "extraterm"
   desc "Swiss army chainsaw of terminal emulators"
   homepage "https://extraterm.org/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "extraterm-#{version}-darwin-x64/extraterm.app"
 
