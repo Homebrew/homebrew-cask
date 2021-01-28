2f516810e118aec72d2b4ff619bf633a2f5cf996
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-27 21:35:01 -0800

    datweatherdoe: add livecheck (#98842)

--- Casks/datweatherdoe.rb
+++ Casks/datweatherdoe.rb
@@ -3,10 +3,14 @@ cask "datweatherdoe" do
   sha256 "69f3f66313922aed24daa100288332af9e1fa34f4bb6aae05811f6075a837f2a"
 
   url "https://github.com/inderdhir/DatWeatherDoe/releases/download/#{version}/DatWeatherDoe-#{version}.dmg"
-  appcast "https://github.com/inderdhir/DatWeatherDoe/releases.atom"
   name "DatWeatherDoe"
   desc "Menu bar weather app"
   homepage "https://github.com/inderdhir/DatWeatherDoe"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "DatWeatherDoe.app"
 end
