f1d7f6dbc74e07250c4ab2190f496edd631e6686
Moulick Aggarwal <Moulick@users.noreply.github.com>
2021-01-29 02:40:17 +0530

    Add livecheck to goland
    Add a live check to goland cask

--- Casks/goland.rb
+++ Casks/goland.rb
@@ -3,7 +3,6 @@ cask "goland" do
   sha256 "e9dfe06fb38eb29e320985cc179ce7c1995a5c10fe0bff3eca8d40456e277503"
 
   url "https://download.jetbrains.com/go/goland-#{version.before_comma}.dmg"
-  appcast "https://data.services.jetbrains.com/products/releases?code=GO&latest=true&type=release"
   name "Goland"
   desc "Go (golang) IDE"
   homepage "https://www.jetbrains.com/go/"
@@ -12,10 +11,16 @@ cask "goland" do
 
   app "GoLand.app"
 
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=GO&latest=true&type=release"
+    strategy :page_match
+    regex(/"version"\s*:\s*"(\d+(:?\.\d+)*)"/)
+  end
+
   uninstall_postflight do
     ENV["PATH"].split(File::PATH_SEPARATOR).map { |path| File.join(path, "goland") }.each do |path|
       if File.exist?(path) &&
-         File.readlines(path).grep(/# see com.intellij.idea.SocketLock for the server side of this interface/).any?
+          File.readlines(path).grep(/# see com.intellij.idea.SocketLock for the server side of this interface/).any?
         File.delete(path)
       end
     end
