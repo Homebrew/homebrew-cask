b4f7f31bbda934531c7fa5b93cdcbd9ae8615563
Seeker <meaningseeking@protonmail.com>
2021-01-28 08:19:07 -0800

    Update zesarux from 8.1.1 to 9.1

--- Casks/zesarux.rb
+++ Casks/zesarux.rb
@@ -1,8 +1,8 @@
 cask "zesarux" do
-  version "8.1.1"
-  sha256 "258f25575a769b05d0c61c52fb152b7662e29c2df2b59a56085b0efe97d6f58e"
+  version "9.1"
+  sha256 "4522b7fa4425258eaaa1d915c16a5141d81b01e0b7fc66c3f3f0ef09361f3e6c"
 
-  url "https://github.com/chernandezba/zesarux/releases/download/#{version.major_minor}/ZEsarUX_macos-#{version}.dmg.gz"
+  url "https://github.com/chernandezba/zesarux/releases/download/#{version}/ZEsarUX_macos-#{version}.dmg"
   appcast "https://github.com/chernandezba/zesarux/releases.atom"
   name "ZEsarUX"
   homepage "https://github.com/chernandezba/zesarux"
