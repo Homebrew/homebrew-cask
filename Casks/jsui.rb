e47f65660c81e0cf684bf4eb17d0e991c597670e
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-28 00:52:46 -0800

    jsui: add livecheck

--- Casks/jsui.rb
+++ Casks/jsui.rb
@@ -3,10 +3,14 @@ cask "jsui" do
   sha256 "c262c5875ff90521a7785b17de011acb3dce54e67e124c64ec005b87ef897736"
 
   url "https://github.com/kitze/JSUI/releases/download/v#{version}/JSUI-#{version}-mac.zip"
-  appcast "https://github.com/kitze/JSUI/releases.atom"
   name "JSUI"
   desc "UI toolkit for managing JavaScript apps"
   homepage "https://github.com/kitze/JSUI"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   auto_updates true
 
