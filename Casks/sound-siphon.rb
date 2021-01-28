29cf4fb50d1e1134e53ab6d2e35789339184c4e3
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-28 13:53:51 -0800

    sound-siphon: delete redundant `verified`

--- Casks/sound-siphon.rb
+++ Casks/sound-siphon.rb
@@ -2,8 +2,7 @@ cask "sound-siphon" do
   version "3.2.4,5876"
   sha256 "c05ce45e0eb4950926ffdfdca6a49540e562f756475df68aed2d39db53156e3f"
 
-  url "https://staticz.com/download/#{version.after_comma}/",
-      verified: "staticz.net/"
+  url "https://staticz.com/download/#{version.after_comma}/"
   appcast "http://staticz.net/updates/soundsiphon#{version.major}.rss"
   name "SoundSiphon"
   homepage "https://staticz.com/soundsiphon/"
