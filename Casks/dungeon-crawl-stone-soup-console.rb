49908716015d5c646da31e967b116aa35d8ed26a
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-27 21:08:25 -0800

    dungeon-crawl-stone-soup-console: add livecheck (#98850)

--- Casks/dungeon-crawl-stone-soup-console.rb
+++ Casks/dungeon-crawl-stone-soup-console.rb
@@ -4,10 +4,14 @@ cask "dungeon-crawl-stone-soup-console" do
 
   url "https://github.com/crawl/crawl/releases/download/#{version}/dcss-#{version}-macos-console.zip",
       verified: "github.com/crawl/crawl/releases/"
-  appcast "https://github.com/crawl/crawl/releases.atom"
   name "Dungeon Crawl Stone Soup"
   desc "Game of dungeon exploration, combat and magic"
   homepage "https://crawl.develz.org/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "Dungeon Crawl Stone Soup - Console.app"
 end
