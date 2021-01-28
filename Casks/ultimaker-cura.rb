0340dab8be24f37594a242eea75bb8e03fb1eeaa
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-28 06:15:00 -0800

    ultimaker-cura: add livecheck (#98950)

--- Casks/ultimaker-cura.rb
+++ Casks/ultimaker-cura.rb
@@ -3,11 +3,15 @@ cask "ultimaker-cura" do
   sha256 "b065b11bf6c9cc98e179de71120de6ed9f12e1c831f4a38e7ca333bffc203041"
 
   url "https://download.ultimaker.com/cura/Ultimaker_Cura-#{version}-Darwin.dmg"
-  appcast "https://github.com/Ultimaker/Cura/releases.atom"
   name "Ultimaker Cura"
   name "Cura"
   desc "3D printer and slicing GUI"
   homepage "https://ultimaker.com/en/products/cura-software"
+
+  livecheck do
+    url "https://github.com/Ultimaker/Cura"
+    strategy :github_latest
+  end
 
   app "Ultimaker Cura.app"
 
