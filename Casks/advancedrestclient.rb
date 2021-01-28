ceab773fd701ee3fd46b3b7216db68274097a42e
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-27 21:43:53 -0800

    advancedrestclient: add livecheck (#98825)

--- Casks/advancedrestclient.rb
+++ Casks/advancedrestclient.rb
@@ -3,9 +3,13 @@ cask "advancedrestclient" do
   sha256 "1dc4ef5e21dfb72abeba6de6a41673fadc0ce46ba4252c7d8d8b3648372fe86b"
 
   url "https://github.com/advanced-rest-client/arc-electron/releases/download/v#{version}/arc-#{version}.dmg"
-  appcast "https://github.com/advanced-rest-client/arc-electron/releases.atom"
   name "Advanced Rest Client"
   homepage "https://github.com/advanced-rest-client/arc-electron"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "AdvancedRestClient.app"
 
