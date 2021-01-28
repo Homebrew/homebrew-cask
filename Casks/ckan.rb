6395fd764aaf6a7375ac511e31874cb56d63de0c
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-27 21:39:34 -0800

    ckan: add livecheck (#98835)

--- Casks/ckan.rb
+++ Casks/ckan.rb
@@ -3,9 +3,13 @@ cask "ckan" do
   sha256 "768bbf3385ee3d762132bd4a82fc9222f0e2e60c69bdefda948b80ce8b7bcbad"
 
   url "https://github.com/KSP-CKAN/CKAN/releases/download/v#{version}/CKAN.dmg"
-  appcast "https://github.com/KSP-CKAN/CKAN/releases.atom"
   name "Comprehensive Kerbal Archive Network client"
   homepage "https://github.com/KSP-CKAN/CKAN"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   depends_on cask: "mono-mdk"
 
