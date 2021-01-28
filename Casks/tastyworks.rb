e7e7abf2ba7a10f8ee2708dfbe0a78a3ee343aa9
Chris Baker <ChrisBaker97@users.noreply.github.com>
2021-01-28 00:54:40 -0500

    Update tastyworks from 1.12.0 to 1.12.2 (#98804)
    * Update tastyworks from 1.12.0 to 1.12.2
    
    * Add `desc` stanza
    
    Co-authored-by: Chris Baker <chris@DESKTOP-71KF8HR.local>

--- Casks/tastyworks.rb
+++ Casks/tastyworks.rb
@@ -1,11 +1,12 @@
 cask "tastyworks" do
-  version "1.12.0"
-  sha256 "0d799bb36a82954a970087052eb092172edf6e5b79d0d6868db14d30bd3df12a"
+  version "1.12.2"
+  sha256 "3729eacbefefcd3b9a085f8bb6b8d5bad4333969d63deecb82b3c4c0e983eff5"
 
   url "https://download.tastyworks.com/desktop-#{version.major}.x.x/#{version}/tastyworks-#{version}.dmg"
   appcast "https://tastyworks.freshdesk.com/support/solutions/articles/43000435186-recent-release-notes",
           must_contain: version.major_minor
   name "tastyworks"
+  desc "Desktop trading platform for the tastyworks brokerage"
   homepage "https://tastyworks.com/"
 
   auto_updates true
