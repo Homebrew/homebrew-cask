4c874ac5ca4c4d82d9f1346b8828ec7bb4da6f28
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-28 07:10:32 -0800

    qownnotes: add livecheck (#98920)

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -4,10 +4,14 @@ cask "qownnotes" do
 
   url "https://github.com/pbek/QOwnNotes/releases/download/v#{version}/QOwnNotes.dmg",
       verified: "github.com/pbek/QOwnNotes/"
-  appcast "https://github.com/pbek/QOwnNotes/releases.atom"
   name "QOwnNotes"
   desc "Plain-text file notepad and todo-list manager"
   homepage "https://www.qownnotes.org/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   auto_updates true
   depends_on macos: ">= :sierra"
