8031ccad69caa819b7936f5db690893e77f20ee3
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-28 06:10:11 -0800

    graphql-playground: add livecheck (#98962)

--- Casks/graphql-playground.rb
+++ Casks/graphql-playground.rb
@@ -3,9 +3,13 @@ cask "graphql-playground" do
   sha256 "43d54be598ec83dddcaee789e8434c547d0ff1a49ffc2d95fb798996293b7f13"
 
   url "https://github.com/prisma/graphql-playground/releases/download/v#{version}/graphql-playground-electron-#{version}.dmg"
-  appcast "https://github.com/prisma/graphql-playground/releases.atom"
   name "GraphQL Playground"
   homepage "https://github.com/prisma/graphql-playground"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "GraphQL Playground.app"
 end
