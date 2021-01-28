bb7655fa70a2b039fdbc7687d0318972c866d4c4
Seeker <meaningseeking@protonmail.com>
2021-01-28 08:25:11 -0800

    Add livecheck

--- Casks/zesarux.rb
+++ Casks/zesarux.rb
@@ -3,9 +3,13 @@ cask "zesarux" do
   sha256 "4522b7fa4425258eaaa1d915c16a5141d81b01e0b7fc66c3f3f0ef09361f3e6c"
 
   url "https://github.com/chernandezba/zesarux/releases/download/#{version}/ZEsarUX_macos-#{version}.dmg"
-  appcast "https://github.com/chernandezba/zesarux/releases.atom"
   name "ZEsarUX"
   homepage "https://github.com/chernandezba/zesarux"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "ZEsarUX.app"
 end
