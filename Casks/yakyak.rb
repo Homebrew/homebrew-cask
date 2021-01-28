107905822bb3d08cebad267d4b8cef6532255433
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-28 06:12:40 -0800

    yakyak: add livecheck (#98954)

--- Casks/yakyak.rb
+++ Casks/yakyak.rb
@@ -3,10 +3,14 @@ cask "yakyak" do
   sha256 "c5b8c079d7ba076dc5c03cd00c8950cb5f7ce6400b33dc3ef58d271c0700502b"
 
   url "https://github.com/yakyak/yakyak/releases/download/v#{version}/yakyak-#{version}-osx-x64.zip"
-  appcast "https://github.com/yakyak/yakyak/releases.atom"
   name "Yakyak"
   desc "Desktop chat client for Google Hangouts"
   homepage "https://github.com/yakyak/yakyak"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "YakYak-darwin-x64/YakYak.app"
 end
