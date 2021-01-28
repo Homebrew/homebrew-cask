a983f2f260f7e934dd4c91ff9551be2e5438d6d3
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-28 06:29:58 -0800

    openframeworks: add livecheck (#98911)

--- Casks/openframeworks.rb
+++ Casks/openframeworks.rb
@@ -3,9 +3,13 @@ cask "openframeworks" do
   sha256 "4701be5c3cb480019ceb1ef6d936e29a958f1ef1fd980752f428babee89a5e25"
 
   url "https://openframeworks.cc/versions/v#{version}/of_v#{version}_osx_release.zip"
-  appcast "https://github.com/openframeworks/openFrameworks/releases.atom"
   name "Openframeworks"
   homepage "https://openframeworks.cc/"
+
+  livecheck do
+    url "https://github.com/openframeworks/openFrameworks"
+    strategy :github_latest
+  end
 
   suite "of_v#{version}_osx_release"
 end
