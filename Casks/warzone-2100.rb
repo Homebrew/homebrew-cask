c3e5a869ba3557f9f76b28e70038c8a452a26afa
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-28 07:30:49 -0800

    warzone-2100: add livecheck (#98951)

--- Casks/warzone-2100.rb
+++ Casks/warzone-2100.rb
@@ -4,10 +4,14 @@ cask "warzone-2100" do
 
   url "https://github.com/Warzone2100/warzone2100/releases/download/#{version}/warzone2100_macOS.zip",
       verified: "github.com/Warzone2100/warzone2100/"
-  appcast "https://github.com/Warzone2100/warzone2100/releases.atom"
   name "Warzone 2100"
   desc "Free and open-source real time strategy game"
   homepage "https://wz2100.net/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "Warzone 2100.app"
 
