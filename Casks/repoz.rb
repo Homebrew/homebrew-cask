e355ca581ee5bde519559fe0647bb9a182e11e34
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-28 00:58:00 -0800

    repoz: add livecheck

--- Casks/repoz.rb
+++ Casks/repoz.rb
@@ -3,10 +3,14 @@ cask "repoz" do
   sha256 "16bb83cad7f6a889cce9b6a4fa733e055469c7c918f7809cf87dbaebf862384f"
 
   url "https://github.com/awaescher/RepoZ/releases/download/v#{version}/RepoZ.#{version}.pkg"
-  appcast "https://github.com/awaescher/RepoZ/releases.atom"
   name "RepoZ"
   desc "Zero-conf git repository hub"
   homepage "https://github.com/awaescher/RepoZ"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   pkg "RepoZ.#{version}.pkg"
 
