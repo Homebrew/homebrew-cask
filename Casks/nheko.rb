5e0543d9f3d669e5a8a38a4e964438c28e35ced2
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-28 08:05:59 +0000

    Update nheko from 0.8.0 to 0.8.1

--- Casks/nheko.rb
+++ Casks/nheko.rb
@@ -1,6 +1,6 @@
 cask "nheko" do
-  version "0.8.0"
-  sha256 "40f120620ed3495ab42fb2fc490ac8737e5b41320017568710650c5ac03e100a"
+  version "0.8.1"
+  sha256 "9c6d6439e4999840bae18aaf066178abf52cdd60b4e69346647a38910f996b6f"
 
   url "https://github.com/Nheko-Reborn/nheko/releases/download/v#{version}/nheko-v#{version}.dmg",
       verified: "github.com/Nheko-Reborn/nheko/"
