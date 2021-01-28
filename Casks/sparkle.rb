05eee90c6ede61f1d177b13cafa693aa2d138639
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-28 08:44:56 -0800

    sparkle: add livecheck (#98979)

--- Casks/sparkle.rb
+++ Casks/sparkle.rb
@@ -4,9 +4,13 @@ cask "sparkle" do
 
   url "https://github.com/sparkle-project/Sparkle/releases/download/#{version}/Sparkle-#{version}.tar.xz",
       verified: "github.com/sparkle-project/Sparkle/"
-  appcast "https://github.com/sparkle-project/Sparkle/releases.atom"
   name "Sparkle"
   homepage "https://sparkle-project.org/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "Sparkle Test App.app"
 end
