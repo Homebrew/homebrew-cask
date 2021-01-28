fe7603f14cd6f341dcb82fb631a770d386eda202
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-28 06:37:57 -0800

    kui: add livecheck (#98890)

--- Casks/kui.rb
+++ Casks/kui.rb
@@ -4,10 +4,14 @@ cask "kui" do
 
   url "https://github.com/IBM/kui/releases/download/v#{version}/Kui-darwin-x64.tar.bz2",
       verified: "github.com/IBM/kui/"
-  appcast "https://github.com/IBM/kui/releases.atom"
   name "Kui"
   desc "Hybrid command-line/UI development experience for cloud-native development"
   homepage "https://kui.tools/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "Kui-darwin-x64/Kui.app"
   binary "#{appdir}/Kui.app/Contents/Resources/kubectl-kui"
