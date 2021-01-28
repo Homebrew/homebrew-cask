6832d6c169e6ac21d3216332a87b45ec65e47b2a
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-28 06:16:28 -0800

    todour: add livecheck (#98946)

--- Casks/todour.rb
+++ Casks/todour.rb
@@ -3,10 +3,14 @@ cask "todour" do
   sha256 "f116b76d90ab83f38d32383d553095161342740436074af26e8029297e1d5882"
 
   url "https://nerdur.com/Todour-v#{version}.dmg"
-  appcast "https://github.com/SverrirValgeirsson/Todour/releases.atom"
   name "Todour"
   desc "Todo.txt application Todour"
   homepage "https://nerdur.com/todour-pl/"
+
+  livecheck do
+    url "https://github.com/SverrirValgeirsson/Todour"
+    strategy :github_latest
+  end
 
   app "Todour.app"
 end
