93e344a602cf3cd1ced00759fe75eee3f61aec8c
Seeker <meaningseeking@protonmail.com>
2021-01-28 06:43:47 -0800

    Update obsidian from 0.10.8 to 0.10.9 (#98879)

--- Casks/obsidian.rb
+++ Casks/obsidian.rb
@@ -1,8 +1,8 @@
 cask "obsidian" do
-  version "0.10.8"
-  sha256 "a2a56594338439de4285fd68cb2cd24e70d8bf7a800a99287e31caaa60db4a97"
+  version "0.10.9"
+  sha256 "c2ee6ec0b3341bc71432a939a11243727aa0df6008bb284ffba9575f9b500af6"
 
-  url "https://github.com/obsidianmd/obsidian-releases/releases/download/v#{version}/Obsidian-#{version}.dmg",
+  url "https://github.com/obsidianmd/obsidian-releases/releases/download/v#{version}/Obsidian-#{version}-universal.dmg",
       verified: "github.com/obsidianmd/"
   appcast "https://github.com/obsidianmd/obsidian-releases/releases.atom"
   name "Obsidian"
