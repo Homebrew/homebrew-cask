4674edd0eaefd0cc48100dd952d75f447eaebfa6
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-28 06:29:06 -0800

    orca: add livecheck (#98913)

--- Casks/orca.rb
+++ Casks/orca.rb
@@ -3,9 +3,13 @@ cask "orca" do
   sha256 "79b1b346df9113cd56e545b223a73179801116ca9e009f3c5fe0fac97c0df1ab"
 
   url "https://github.com/plotly/orca/releases/download/v#{version}/mac-release.zip"
-  appcast "https://github.com/plotly/orca/releases.atom"
   name "Orca"
   homepage "https://github.com/plotly/orca/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   container nested: "orca-#{version}.dmg"
 
