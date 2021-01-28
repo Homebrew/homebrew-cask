d37893daf4de5fff3c43f9873453cf8442c9c837
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-28 13:54:02 -0800

    unpkg: delete redundant `verified`

--- Casks/unpkg.rb
+++ Casks/unpkg.rb
@@ -2,8 +2,7 @@ cask "unpkg" do
   version "4.6"
   sha256 "6949b648d5cd02138f9e7f505f1fb3541905d6016ed951daa71cb7250c3ace2e"
 
-  url "https://www.timdoug.com/unpkg/unpkg-#{version}.zip",
-      verified: "github.com/downloads/timdoug/unpkg/"
+  url "https://www.timdoug.com/unpkg/unpkg-#{version}.zip"
   appcast "https://www.timdoug.com/unpkg/"
   name "unpkg"
   desc "Unarchiver for .pkg and .mpkg that unpacks all the files in a package"
