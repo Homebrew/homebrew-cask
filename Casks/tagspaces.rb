b9e4a995db47d3e0a3847876e102eb23906ef30c
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-28 06:19:16 -0800

    tagspaces: add livecheck (#98938)

--- Casks/tagspaces.rb
+++ Casks/tagspaces.rb
@@ -4,10 +4,14 @@ cask "tagspaces" do
 
   url "https://github.com/tagspaces/tagspaces/releases/download/v#{version}/tagspaces-mac-#{version}.zip",
       verified: "github.com/tagspaces/tagspaces/"
-  appcast "https://github.com/tagspaces/tagspaces/releases.atom"
   name "TagSpaces"
   desc "Offline, open-source, document manager with tagging support"
   homepage "https://www.tagspaces.org/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "TagSpaces.app"
 end
