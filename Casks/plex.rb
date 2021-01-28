309ccd1a7fdf3aefecf8e726b7a6a458f9b4c339
Ondřej Führer <ondrejfuhrer@users.noreply.github.com>
2021-01-28 20:59:17 +0100

    Update plex from 1.25.0.1511-47afccd4 to 1.27.1.1571-2fface66 (#98989)

--- Casks/plex.rb
+++ Casks/plex.rb
@@ -1,6 +1,6 @@
 cask "plex" do
-  version "1.25.0.1511-47afccd4"
-  sha256 "b61134d3790c4e028046b088a30551bbff845f899c7b704880ec12b8227b8749"
+  version "1.27.1.1571-2fface66"
+  sha256 "ce65efe57273fa63982b8d690c44e56a783317e171409f56046b279fcf72525d"
 
   url "https://downloads.plex.tv/plex-desktop/#{version}/macos/Plex-#{version}-x86_64.zip"
   appcast "https://plex.tv/api/downloads/6.json"
