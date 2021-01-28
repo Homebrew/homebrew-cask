76877f030f1a1c0707791172d0599036d60365ef
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-27 21:43:34 -0800

    alacritty: add livecheck (#98826)

--- Casks/alacritty.rb
+++ Casks/alacritty.rb
@@ -3,10 +3,14 @@ cask "alacritty" do
   sha256 "40c9390b9067c25ff2882b41e8526df43466a3a48210b0f3188a9b87789f2541"
 
   url "https://github.com/alacritty/alacritty/releases/download/v#{version}/Alacritty-v#{version}.dmg"
-  appcast "https://github.com/alacritty/alacritty/releases.atom"
   name "Alacritty"
   desc "GPU-accelerated terminal emulator"
   homepage "https://github.com/alacritty/alacritty/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "Alacritty.app"
   binary "#{appdir}/Alacritty.app/Contents/MacOS/alacritty"
