26a5e7f9ae150c91c54aa21abbc54e368f68cd4e
Seeker <meaningseeking@protonmail.com>
2021-01-28 15:07:26 -0800

    Update obsidian from 0.10.9 to 0.10.11 (#98982)

--- Casks/obsidian.rb
+++ Casks/obsidian.rb
@@ -1,13 +1,17 @@
 cask "obsidian" do
-  version "0.10.9"
-  sha256 "c2ee6ec0b3341bc71432a939a11243727aa0df6008bb284ffba9575f9b500af6"
+  version "0.10.11"
+  sha256 "e91c2d375c0c202a646a6a0c429556a2b562b3bfea0468c36aacb8cc92299872"
 
   url "https://github.com/obsidianmd/obsidian-releases/releases/download/v#{version}/Obsidian-#{version}-universal.dmg",
       verified: "github.com/obsidianmd/"
-  appcast "https://github.com/obsidianmd/obsidian-releases/releases.atom"
   name "Obsidian"
   desc "Knowledge base that works on top of a local folder of plain text Markdown files"
   homepage "https://obsidian.md/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   auto_updates true
 
