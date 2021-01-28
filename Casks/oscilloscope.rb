1ba65e85cf965f95099716c087f6232f46847eb8
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-28 00:56:21 -0800

    oscilloscope: add livecheck

--- Casks/oscilloscope.rb
+++ Casks/oscilloscope.rb
@@ -3,9 +3,13 @@ cask "oscilloscope" do
   sha256 "952d017212a8cf35d01ac86ddad8bb2ac1bc57820396f5dfd5bc7a14fd4c9060"
 
   url "https://github.com/kritzikratzi/Oscilloscope/releases/download/#{version}/oscilloscope-#{version}-osx.zip"
-  appcast "https://github.com/kritzikratzi/Oscilloscope/releases.atom"
   name "Oscilloscope"
   homepage "https://github.com/kritzikratzi/Oscilloscope"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "Oscilloscope/Oscilloscope.app"
 end
