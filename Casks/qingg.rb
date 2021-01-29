c687762805ffc33438fa7fe436ac171df8e9d3ad
Igotit <aochin@gmail.com>
2021-01-29 19:06:46 +0800

    Update qingg 2.9

--- Casks/qingg.rb
+++ Casks/qingg.rb
@@ -1,23 +1,24 @@
-cask 'qingg' do
-  version '2.8.1'
-  sha256 'e38daedf7e2e30ed078c7f9094323b78f8685ebcaafa6b42b0c792ed41c388c7'
+cask "qingg" do
+  version "2.9"
+  sha256 "00dd9c50b77e91f9b5d0ba407caf1f4cf5e99b93f47c733811c5e9f8f55ed7b8"
 
   url "https://qingg.im/download/Qingg-#{version}.dmg"
-  appcast 'https://qingg.im/sparkle/appcast.php'
-  name 'QinggIM'
-  name '清歌输入法'
-  homepage 'https://qingg.im/mac/'
+  appcast "https://qingg.im/sparkle/appcast.php"
+  name "QinggIM"
+  name "清歌输入法"
+  desc "为Mac专门打造的五笔输入法"
+  homepage "https://qingg.im/mac/"
 
   auto_updates true
 
-  pkg 'Qingg.pkg'
+  pkg "Qingg.pkg"
 
-  uninstall pkgutil: 'com.aodaren.*'
+  uninstall pkgutil: "com.aodaren.*"
 
   zap trash: [
-               '~/Library/Application Support/Qingg',
-               '~/Library/Application Support/com.aodaren.inputmethod.Qingg',
-               '~/Library/Caches/com.aodaren.inputmethod.Qingg',
-               '~/Library/Preferences/com.aodaren.inputmethod.Qingg.plist',
-             ]
+    "~/Library/Application Support/Qingg",
+    "~/Library/Application Support/com.aodaren.inputmethod.Qingg",
+    "~/Library/Caches/com.aodaren.inputmethod.Qingg",
+    "~/Library/Preferences/com.aodaren.inputmethod.Qingg.plist",
+  ]
 end
