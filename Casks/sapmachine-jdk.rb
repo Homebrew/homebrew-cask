48aac0e00fe5e7d7a260a7d50d4a99f753bb5603
Seeker <meaningseeking@protonmail.com>
2021-01-28 21:24:26 -0800

    sapmachine-jdk: add livecheck (#99032)

--- Casks/sapmachine-jdk.rb
+++ Casks/sapmachine-jdk.rb
@@ -4,10 +4,15 @@ cask "sapmachine-jdk" do
 
   url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_osx-x64_bin.dmg",
       verified: "github.com/SAP/SapMachine/"
-  appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
   name "SapMachine OpenJDK Development Kit"
   desc "OpenJDK release maintained and supported by SAP"
   homepage "https://sapmachine.io/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+    regex(%r{href=.*/sapmachine-jdk-(\d+(?:\.\d+)*(?:\+\d+(?:\.\d+)*)?)_osx-x64_bin\.dmg}i)
+  end
 
   artifact "sapmachine-jdk-#{version}.jdk", target: "/Library/Java/JavaVirtualMachines/sapmachine-jdk-#{version}.jdk"
 end
