0d4856c032242b416f3212acee5a1aaf96600a96
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-28 19:37:39 -0800

    Update bonitastudiocommunity from 7.11.4 to 2021.1 (#99029)

--- Casks/bonitastudiocommunity.rb
+++ Casks/bonitastudiocommunity.rb
@@ -1,6 +1,6 @@
 cask "bonitastudiocommunity" do
-  version "7.11.4"
-  sha256 "d6c5058ac110c462932819f2330cdcb64a3812cae56ba10527d760ed735313d2"
+  version "2021.1"
+  sha256 "46e42b4c902922c103fdc465f8595115640b4a8a611ed506f9f2e978794a2308"
 
   url "https://github.com/bonitasoft/bonita-platform-releases/releases/download/#{version}/BonitaStudioCommunity-#{version}-x86_64.dmg",
       verified: "github.com/bonitasoft/bonita-platform-releases/"
