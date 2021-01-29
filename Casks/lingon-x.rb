76b6ded095d32a6ab25cd2a4f0be44798c17f992
Andrew Disley <andrew@simplified.co.uk>
2021-01-29 17:19:18 +0000

    Update lingon-x from 8.0.5 to 8.1

--- Casks/lingon-x.rb
+++ Casks/lingon-x.rb
@@ -3,8 +3,8 @@ cask "lingon-x" do
     version "6.6.5"
     sha256 "b0231b1a98dcc8f5c4234b419c9f5331407b8cce29b33f0ea2e32b12595adfa8"
   else
-    version "8.0.5"
-    sha256 "04855f755dd7d15ab13fb9dc224495e03cad7276adbd2737fa4339b148fdf5af"
+    version "8.1"
+    sha256 "113609b8e1e6b154617eb7ff929e6a1197844ce61a02f383771550bcf5f636d5"
   end
 
   url "https://www.peterborgapps.com/downloads/LingonX#{version.major}.zip"
