d49bfb10fc21aef690e38cf4dc103f6680c8b85d
OttNorml <2350859+ottnorml@users.noreply.github.com>
2021-01-30 16:08:42 +0100

    Update datagrip from 2020.3.1,203.6682.134 to 2020.3.2 (#99042)
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/datagrip.rb
+++ Casks/datagrip.rb
@@ -1,8 +1,8 @@
 cask "datagrip" do
-  version "2020.3.2,203.7148.68"
+  version "2020.3.2"
   sha256 "dbdc49e06cf2d120e55eecaa0cb6d5588a9ca73b0efd4607d6c2ad4fc8a36d0b"
 
-  url "https://download.jetbrains.com/datagrip/datagrip-#{version.before_comma}.dmg"
+  url "https://download.jetbrains.com/datagrip/datagrip-#{version}.dmg"
   appcast "https://data.services.jetbrains.com/products/releases?code=DG&latest=true&type=release"
   name "DataGrip"
   desc "Databases & SQL IDE"
