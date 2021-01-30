71bdd243b8b44f532ab02d3a3492eee4afdb4573
Lutz Roeder <lutzroeder@users.noreply.github.com>
2021-01-29 20:48:54 -0800

    Update netron to 4.7.4 (#99089)

--- Casks/netron.rb
+++ Casks/netron.rb
@@ -1,6 +1,6 @@
 cask "netron" do
-  version "4.7.3"
-  sha256 "32b7cf07f36f84f2fcc70648d2e4a5351512cd77069024006ae05ab752c59ee9"
+  version "4.7.4"
+  sha256 "323be03861b03de57db3e486df694c8d091db1e6e9c553bff3de468ab9993f8c"
 
   url "https://github.com/lutzroeder/netron/releases/download/v#{version}/Netron-#{version}-mac.zip"
   name "Netron"
