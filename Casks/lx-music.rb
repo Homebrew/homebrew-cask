e5168aef637c4fa34d18cbc484f2e3f5855dc045
Adithya Sunil <adithyasunil26@gmail.com>
2021-01-31 02:18:53 +0400

    Update lx-music from 1.6.1 to 1.7.1 (#99117)

--- Casks/lx-music.rb
+++ Casks/lx-music.rb
@@ -1,6 +1,6 @@
 cask "lx-music" do
-  version "1.6.1"
-  sha256 "f02a21abef01bd435b73b23ff5ea438691acb4e3ab6845ae16970a10c30893d7"
+  version "1.7.1"
+  sha256 "77e18ab33a152ba56891a019ea154f06dce8f1f8a4194cb3a1cb7fcf8feaeea5"
 
   url "https://github.com/lyswhut/lx-music-desktop/releases/download/v#{version}/lx-music-desktop-#{version}.dmg"
   appcast "https://github.com/lyswhut/lx-music-desktop/releases.atom"
