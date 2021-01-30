4f137573f7532c31b29f6b5cfbc0bc523b550525
Mykola Rybak <mykola.rybak@gmail.com>
2021-01-30 23:46:59 +0200

    Restore "verified" stanza

--- Casks/spatterlight.rb
+++ Casks/spatterlight.rb
@@ -3,6 +3,7 @@ cask "spatterlight" do
   sha256 "07f81dfb20b48142f8fa7bac7aabb005b4c99a63a03c7ffbe04e4ad41687b7be"
 
   url "https://github.com/angstsmurf/spatterlight/releases/download/v#{version}/Spatterlight.zip"
+      verified: "github.com/angstsmurf/spatterlight/"
   appcast "https://github.com/angstsmurf/spatterlight/releases.atom"
   name "Spatterlight"
   desc "Play most kinds of interactive fiction game files"
