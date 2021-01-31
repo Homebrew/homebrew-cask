1506950f83dbea962fabcb5b0f56410e9763eb49
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-01 02:55:20 +0800

    Update nodal.rb

--- Casks/nodal.rb
+++ Casks/nodal.rb
@@ -1,6 +1,6 @@
 cask "nodal" do
   version "2.0.3.1"
-  sha256 :no_check
+  sha256 "3534ced04059a6bb7e1397b5935b8a07f4d57d3ade60530eba256a160ca930c7"
 
   url "https://nodalmusic.com/downloads/Nodal-#{version}.dmg"
   name "Nodal"
