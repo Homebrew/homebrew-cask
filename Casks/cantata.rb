dc296159e5ef25bdb9b5498755f9bc93bc8e2afc
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-01-31 10:50:30 -0800

    cantata: add livecheck (#99104)

--- Casks/cantata.rb
+++ Casks/cantata.rb
@@ -3,10 +3,16 @@ cask "cantata" do
   sha256 "c9eb8a1102d0a68cafc93f22df73445b8f69706f3322285f9a2f623a28df0176"
 
   url "https://github.com/CDrummond/cantata/releases/download/v#{version}/Cantata-#{version}.dmg"
-  appcast "https://github.com/CDrummond/cantata/releases.atom"
   name "Cantata"
   desc "Qt5 Graphical MPD Client"
   homepage "https://github.com/cdrummond/cantata"
+
+  # We need to check all releases since not all releases are for macOS.
+  livecheck do
+    url "https://github.com/CDrummond/cantata/releases"
+    strategy :page_match
+    regex(%r{href=.*?/Cantata[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
+  end
 
   depends_on macos: ">= :sierra"
 
