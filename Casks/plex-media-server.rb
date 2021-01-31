0bc08808da1eac86cc84b03b50f55318a7efb36a
Bevan Kay <bevanjkay@gmail.com>
2021-02-01 06:09:10 +1100

    Fix plex-media-server Livecheck (#98761)
    * Fix plex-media-server Livecheck
    
    * Fix version
    
    * Fix sha
    
    * Update plex-media-server.rb
    
    * Bump version
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -1,6 +1,6 @@
 cask "plex-media-server" do
-  version "1.21.1.3876-3c3adfcb4"
-  sha256 "82bc7de6fbf57638f94915e93ed1f80768c07111dc94c44a0741624adcd4119b"
+  version "1.21.2.3943-a91458577"
+  sha256 "233e26b915e9d19ea2ddd40077f657575b77bbd50cf06aa2e18fda9fa2d7090f"
 
   url "https://downloads.plex.tv/plex-media-server-new/#{version}/macos/PlexMediaServer-#{version}-x86_64.zip"
   name "Plex Media Server"
@@ -10,7 +10,7 @@ cask "plex-media-server" do
   livecheck do
     url "https://plex.tv/api/downloads/5.json"
     strategy :page_match
-    regex(%r{href=.*?/PlexMediaServer-(\d+(?:\.\d+)*-\d+c\d+adfcb\d+)-x86_64\.zip}i)
+    regex(%r{href=.*?/PlexMediaServer-(\d+(?:\.\d+)*-[\da-f]+)-x86_64\.zip}i)
   end
 
   auto_updates true
