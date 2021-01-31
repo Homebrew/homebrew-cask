5c65c03eeb4ecdb2788f6361b030640c2357105b
adithyasunil26 <adithyasunil26@gmail.com>
2021-02-01 00:11:59 +0400

    Update seadrive from 2.0.10 to 2.0.12

--- Casks/seadrive.rb
+++ Casks/seadrive.rb
@@ -1,6 +1,6 @@
 cask "seadrive" do
-  version "2.0.10"
-  sha256 "d9f54d78111c2e8dfd7e82a10c9b07bcd0317bceddb2b79e6a76973321cfb029"
+  version "2.0.12"
+  sha256 "3b6591e2b6fc3b8a45fc384e57965c2db1c65b1f1be014d49d6801ee23de293f"
 
   url "https://download.seadrive.org/seadrive-#{version}.dmg",
       verified: "download.seadrive.org/"
