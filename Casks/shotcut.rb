e649d7b430a4da501a82511b4faa1eab3580bfc7
Adithya Sunil <adithyasunil26@gmail.com>
2021-02-01 00:19:23 +0400

    Update shotcut from 20.11.28 to 21.01.29 (#99147)

--- Casks/shotcut.rb
+++ Casks/shotcut.rb
@@ -1,6 +1,6 @@
 cask "shotcut" do
-  version "20.11.28"
-  sha256 "a2d72ff4302ef5d08b379d605907dc061ed10c53a6478458c2098be0712456cc"
+  version "21.01.29"
+  sha256 "b236343f9844bc9cd03177495fc4983b1c353bfb544427f676135bc1d06acf4d"
 
   url "https://github.com/mltframework/shotcut/releases/download/v#{version}/shotcut-macos-signed-#{version.no_dots}.dmg",
       verified: "github.com/mltframework/shotcut/"
