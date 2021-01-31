41149779fcb899161b0b1aafda8564513753a68a
chris <mail@chohner.com>
2021-01-31 15:52:56 +0100

    Update eddie from 2.18.9 to 2.19.7

--- Casks/eddie.rb
+++ Casks/eddie.rb
@@ -1,14 +1,16 @@
 cask "eddie" do
-  version "2.18.9"
-  sha256 "4b6e233496f8d63d1cbab01a2f24e46c480741746c1b1b6e7468442d41c648c1"
+  version "2.19.7"
+  sha256 "b766c9a5fec0421d9cd3772e6c1986d6070e0bb9bfa4312e588d332ec786e586"
 
-  url "https://eddie.website/download/?platform=macos&arch=x64&ui=ui&format=disk.dmg&version=#{version}",
+  url "https://eddie.website/download/?platform=macos-10.15&arch=x64&ui=ui&format=disk.dmg&version=#{version}",
       verified: "eddie.website/"
   appcast "https://github.com/AirVPN/Eddie/releases.atom"
   name "Air VPN"
   name "Eddie"
   desc "OpenVPN UI"
   homepage "https://airvpn.org/macos/"
+
+  depends_on macos: ">= :catalina"
 
   app "Eddie.app"
 
