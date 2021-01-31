145ff88981c1477d5fd69596bd49b7e3536b323b
Adithya Sunil <adithyasunil26@gmail.com>
2021-01-31 17:51:36 +0400

    Update quicken from 6.0.3,600.37136.100 to 6.1.0,601.37882.100 (#99138)

--- Casks/quicken.rb
+++ Casks/quicken.rb
@@ -1,6 +1,6 @@
 cask "quicken" do
-  version "6.0.3,600.37136.100"
-  sha256 "8d067db5496fef10c187b94e7c0fc1dd663e17133608c686f8f90351096f4aed"
+  version "6.1.0,601.37882.100"
+  sha256 "a540106b1131d18c9e6fe013c069fd9d3d90ea8ab9196eddbbeae1a7c1bba1c1"
 
   url "https://download.quicken.com/mac/Quicken/001/Release/031A96D9-EFE6-4520-8B6A-7F465DDAA3E4/Quicken-#{version.after_comma}/Quicken-#{version.after_comma}.zip"
   name "Quicken"
