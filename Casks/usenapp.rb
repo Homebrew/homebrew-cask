d2e8fb047df8fb6fb6037d9ac0f9e91d9f43ceba
Adithya Sunil <adithyasunil26@gmail.com>
2021-02-01 00:34:36 +0400

    Update usenapp from 0.98,292 to 0.99.1,299 (#99155)

--- Casks/usenapp.rb
+++ Casks/usenapp.rb
@@ -1,6 +1,6 @@
 cask "usenapp" do
-  version "0.98,292"
-  sha256 "413647ff6be9d7079247fd31e121f7365cda4f591e3163e8c92e403bff51e622"
+  version "0.99.1,299"
+  sha256 "f82fc60365ec30e3845c873b5a4fba86989fe4b1b065952164c53e7978f89b56"
 
   url "https://www.usenapp.com/download/Usenapp-#{version.before_comma}.dmg"
   name "Usenapp"
