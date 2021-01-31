f235f1787406d85397b60fd9cfcbfeeb95ad61c0
yibo <7483137+xyb994@users.noreply.github.com>
2021-01-31 10:18:50 -0500

    Update manico from 2.8.1,393 to 2.9,405 (#99140)

--- Casks/manico.rb
+++ Casks/manico.rb
@@ -1,6 +1,6 @@
 cask "manico" do
-  version "2.8.1,393"
-  sha256 "e83213c293baff826ca7624b3a130bf99ad5b05650364ba258266bc28645708c"
+  version "2.9,405"
+  sha256 "df892cf2cf2a883ff3e3ffd0058b552715f4cc9d58aa29f68e55ae2f92c4b241"
 
   url "https://manico.im/api/release_manager/downloads/im.manico.Manico/#{version.after_comma}.zip"
   name "Manico"
