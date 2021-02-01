bbeca363988d7b286d63bc1ce4312c0e4970b4a9
Adrian Fedoreanu <adrian.fedoreanu@gmail.com>
2021-02-01 11:20:51 +0100

    Update dbeaver-community from 7.3.3 to 7.3.4

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,9 +1,8 @@
 cask "dbeaver-community" do
-  version "7.3.3"
-  sha256 "1e9402c0d6ce5888ab96f71fc5683399d905146c575dda9de945f5ad321c8da7"
+  version "7.3.4"
+  sha256 "47ee7dd81a5fa710e2b497026bd2df114e69320aa9c125e6cbd8a0931cc385d4"
 
-  url "https://github.com/dbeaver/dbeaver/releases/download/#{version}/dbeaver-ce-#{version}-macos.dmg",
-      verified: "github.com/dbeaver/dbeaver/"
+  url "https://dbeaver.io/files/#{version}/dbeaver-ce-#{version}-macos.dmg"
   appcast "https://github.com/dbeaver/dbeaver/releases.atom"
   name "DBeaver Community Edition"
   desc "Free universal database tool and SQL client"
