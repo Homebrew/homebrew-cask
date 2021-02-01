35c710d4a3d4b284e410b85032f016c83fb611ff
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-01 10:29:28 +0800

    Update decloner.rb

--- Casks/decloner.rb
+++ Casks/decloner.rb
@@ -1,8 +1,8 @@
 cask "decloner" do
-  version "1.6.3"
+  version "1.6.3,23"
   sha256 "72123bcbbe734da007fea5becbb06236876cd1cf40ae4d6c8ea43f890c8eef8d"
 
-  url "https://www.pixelespressoapps.com/decloner/Decloner_#{version}.zip"
+  url "https://www.pixelespressoapps.com/decloner/Decloner_#{version.before_comma}.zip"
   name "Decloner"
   desc "Fast and reliable duplicate files finder"
   homepage "https://www.pixelespressoapps.com/decloner/"
