5ce24856c7e1f18a354df3095afeb2d16b20cc43
Alexander Goldstein <alexg@alexland.org>
2021-01-31 21:45:59 -0600

    ringcentral-phone: added zap stanza and desc (#99161)
    * ringcentral-phone: added zap stanza and desc
    
    * Update ringcentral-phone.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/ringcentral-phone.rb
+++ Casks/ringcentral-phone.rb
@@ -4,6 +4,7 @@ cask "ringcentral-phone" do
 
   url "https://downloads.ringcentral.com/sp/RingCentral-Phone-#{version}.dmg"
   name "RingCentral Phone"
+  desc "Phone system manager"
   homepage "https://www.ringcentral.com/apps/rc-phone"
 
   livecheck do
@@ -13,4 +14,9 @@ cask "ringcentral-phone" do
 
   # Renamed for consistency: app name is different in the Finder and in a shell.
   app "RingCentral for Mac.app", target: "RingCentral Phone.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.ringcentral.RingCentral Phone.plist",
+    "~/Library/Saved Application State/com.ringcentral.ringcentralformac.savedState",
+  ]
 end
