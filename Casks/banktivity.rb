4f428c74a844bb0001cc494c13345ea9d9a50f97
Jason Charrier <jason@innovativelanguage.com>
2021-02-01 15:50:32 -0600

    Update banktivity from 8.1.1 to 8.1.2

--- Casks/banktivity.rb
+++ Casks/banktivity.rb
@@ -1,6 +1,6 @@
 cask "banktivity" do
-  version "8.1.1"
-  sha256 "9edd9176022a3a65a044c0ba2c5fee7efe4693f3e3d7ef6403597401994e230e"
+  version "8.1.2"
+  sha256 "decf5db014e721ee6faa53f076971a671c659bc0848cfd0b560bc403605684a5"
 
   url "https://www.iggsoft.com/banktivity/Banktivity#{version}.dmg",
       verified: "iggsoft.com/"
