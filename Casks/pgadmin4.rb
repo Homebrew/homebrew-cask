e8507ceb0ef21c391532f76dbbd2b84b9132f303
maherma-adg <maherma-adg@users.noreply.github.com>
2021-02-01 10:00:55 +0100

    Update pgadmin4 from 4.29 to 4.30 (#99166)

--- Casks/pgadmin4.rb
+++ Casks/pgadmin4.rb
@@ -1,6 +1,6 @@
 cask "pgadmin4" do
-  version "4.29"
-  sha256 "9f461dc26393793e21c93862138f92fe120147c7d661cb9056c92dfda25a29dc"
+  version "4.30"
+  sha256 "2b58eaf87c2687f36fda21328c68a032ec7516c655675dacd6f47b2086638e0e"
 
   url "https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v#{version}/macos/pgadmin4-#{version}.dmg",
       verified: "ftp.postgresql.org/"
