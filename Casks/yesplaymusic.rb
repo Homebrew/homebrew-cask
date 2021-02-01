2d12628dfcb66dae4dc0b062add16af108fe8477
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-01 08:56:47 +0800

    Update yesplaymusic.rb

--- Casks/yesplaymusic.rb
+++ Casks/yesplaymusic.rb
@@ -1,12 +1,16 @@
 cask "yesplaymusic" do
   version "0.3.3"
-  sha256 "19f14abf0c2d8bea740a88473c77a3c25601361df4500b97a39bd6afcea2d05c"
+  sha256 "2a58aecf464c3bcb3b2060445b7b790e6d8e86a63013bb2aacaddf418e011a3a"
 
-  url "https://github.com/qier222/YesPlayMusic/releases/download/v#{version}/YesPlayMusic-#{version}-mac.zip"
-  appcast "https://github.com/qier222/YesPlayMusic/releases.atom"
+  url "https://github.com/qier222/YesPlayMusic/releases/download/v#{version}/YesPlayMusic-#{version}-mac.dmg"
   name "YesPlayMusic"
-  desc "Third-party NetEase Cloud Player"
+  desc "Third-party NetEase cloud player"
   homepage "https://github.com/qier222/YesPlayMusic"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "YesPlayMusic.app"
 
