5388f0ea52501f62024901d9fdef94750558e16a
Stephen Wade <stephen@stephenwade.me>
2021-02-01 18:31:31 -0500

    Update mailplane (#99181)

--- Casks/mailplane.rb
+++ Casks/mailplane.rb
@@ -1,8 +1,8 @@
 cask "mailplane" do
-  version "4.3,4846"
-  sha256 "467fbcc37bb7523e8db6a4d9693374efa1136f6f04a775bb63fc9865d1d6fd1b"
+  version "4.3.1,4859"
+  sha256 "c76e71b0b6db97dacd80a5421d1a5dc647504c16062db934b45ff5708fbbbd42"
 
-  url "https://update.mailplaneapp.com/builds/Mailplane_#{version.major}_#{version.after_comma}.tbz"
+  url "https://builds.mailplaneapp.com/Mailplane_#{version.major}_#{version.after_comma}.tbz"
   appcast "https://update.mailplaneapp.com/appcast.php?rqsr=1&osVersion=10.14.1&appVersion=#{version.after_comma}&shortVersionString=#{version.before_comma}"
   name "Mailplane"
   homepage "https://mailplaneapp.com/"
