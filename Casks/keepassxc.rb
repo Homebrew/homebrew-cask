987fa4584ca2ec1109baa5601172e6ea37f21fd1
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-01 10:44:28 +0800

    Update keepassxc.rb

--- Casks/keepassxc.rb
+++ Casks/keepassxc.rb
@@ -1,14 +1,14 @@
 cask "keepassxc" do
   version "2.6.4"
 
-  if Hardware::CPU.arm?
-    url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}-arm64.dmg",
-        verified: "github.com/keepassxreboot/keepassxc/"
-    sha256 "16dcdfae65ad8887b4d9cd86bf56bc7df2dca3e3fedf91b07b42a0f6e1465c48"
-  else
+  if Hardware::CPU.intel?
     url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}-x86_64.dmg",
         verified: "github.com/keepassxreboot/keepassxc/"
     sha256 "639fdfe0379dc3f00f1bd6a72c974cfddae0d4c8ddb66f7297d0b01b1a582ede"
+  else
+    url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}-arm64.dmg",
+        verified: "github.com/keepassxreboot/keepassxc/"
+    sha256 "16dcdfae65ad8887b4d9cd86bf56bc7df2dca3e3fedf91b07b42a0f6e1465c48"
   end
 
   name "KeePassXC"
