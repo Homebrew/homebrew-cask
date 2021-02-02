4655a1fa1327a5572b194d48d8415f25f0df1d7c
Logan Rosen <loganrosen@gmail.com>
2021-02-02 12:44:23 -0500

    mat: add livecheck (#98531)
    * mat: add livecheck
    
    * Update mat.rb
    
    * Update mat.rb
    
    * Update mat.rb
    
    * Update mat.rb
    
    Co-authored-by: Markus Reiter <me@reitermark.us>
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/mat.rb
+++ Casks/mat.rb
@@ -1,11 +1,16 @@
 cask "mat" do
   version "1.11.0.20201202"
-  sha256 "e2edd8875aa5a100cf30f1423830cb1ecabbdc4414fbf7dd47c658547631916b"
+  sha256 "01929115ddd07e4d03a01a7b76433a3fd675642ca41c622f9040daec8dbaec79"
 
-  url "https://www.eclipse.org/downloads/download.php?r=1&file=/mat/#{version.major_minor_patch}/rcp/MemoryAnalyzer-#{version}-macosx.cocoa.x86_64.zip"
-  appcast "https://www.eclipse.org/mat/downloads.php"
+  url "https://download.eclipse.org/mat/#{version.major_minor_patch}/rcp/MemoryAnalyzer-#{version}-macosx.cocoa.x86_64.dmg"
   name "Eclipse Memory Analyzer"
+  desc "Java heap analyzer"
   homepage "https://www.eclipse.org/mat/"
+
+  livecheck do
+    url "https://www.eclipse.org/mat/downloads.php"
+    regex(/href=.*?MemoryAnalyzer-(\d+(?:\.\d+)*).*?\.dmg/i)
+  end
 
   app "mat.app"
 end
