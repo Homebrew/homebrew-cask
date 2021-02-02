e1b0b4a109c62b65494a5751ef6e7fc095260f44
Fernando <fernando@dano.me>
2021-02-02 15:53:27 +0900

    Add jandi-statusbar 1.1

---
+++ Casks/jandi-statusbar.rb
@@ -0,0 +1,11 @@
+cask "jandi-statusbar" do
+   version "1.1"
+   sha256 "a0ec4ec1fbc7b166841527d7a7142b39cc52f9dc02ae05001f618f120a6dcc70"
+  
+   url "https://github.com/techinpark/Jandi/releases/download/v#{version}/jandi.dmg"
+   name "jandi"
+   desc "Github Contributions in your macOS status bar"
+   homepage "https://github.com/techinpark/Jandi"
+  
+   app "jandi.app"
+end
