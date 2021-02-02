6fe180c2c588b036473686c6d735ff6f777c31e5
adithyasunil26 <adithyasunil26@gmail.com>
2021-02-03 02:32:25 +0400

    Update matterhorn from 50200.11.0 to 50200.12.0

--- Casks/matterhorn.rb
+++ Casks/matterhorn.rb
@@ -1,6 +1,6 @@
 cask "matterhorn" do
-  version "50200.11.0"
-  sha256 "614f3036c65819ea280c6f2ce55ed434ea2a1e04105fc3be386f69064340df7e"
+  version "50200.12.0"
+  sha256 "b2b5b4082824dc49ddef0bf2957352b2fd309a64517dfda962a9a75c15e89842"
 
   url "https://github.com/matterhorn-chat/matterhorn/releases/download/#{version}/matterhorn-#{version}-Darwin-x86_64.tar.bz2"
   appcast "https://github.com/matterhorn-chat/matterhorn/releases.atom"
