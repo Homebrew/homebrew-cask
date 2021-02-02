8b2b91c3647f5babcb2009cb8c6e5cd465ed1f68
Helge Brügner <11437312+moredatapls@users.noreply.github.com>
2021-02-02 17:18:20 +0100

    Update dbvisualizer from 12.0 to 12.0.1 (#99224)

--- Casks/dbvisualizer.rb
+++ Casks/dbvisualizer.rb
@@ -1,6 +1,6 @@
 cask "dbvisualizer" do
-  version "12.0"
-  sha256 "68ff1d2dd20d8e536959a21be7daf9491824f26afe47a0df4e60858ab3734658"
+  version "12.0.1"
+  sha256 "dc50b7c9382b0e1da896a3673bf35275c330637dab9b78d97b6ff4f32c77e424"
 
   url "https://www.dbvis.com/product_download/dbvis-#{version}/media/dbvis_macos_#{version.dots_to_underscores}_jre.dmg"
   appcast "https://www.dbvis.com/download/#{version.major}.0"
