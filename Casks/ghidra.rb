f7a051691a34ec3b4b7ff1ea0dd12d3797e0a35a
Christian Werner <cwerner1@users.noreply.github.com>
2021-02-02 12:38:01 +0100

    Update ghidra from 9.2.2,20201229 to 9.2.2,20201229 (#99218)
    * Update ghidra from 9.2.2,20201229 to 9.2.2,20201229
    
    * Update ghidra.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/ghidra.rb
+++ Casks/ghidra.rb
@@ -1,12 +1,12 @@
 cask "ghidra" do
-  version "9.2.1,20201215"
-  sha256 "cfaeb2b5938dec90388e936f63600ad345d41b509ffed4727142ba9ed44cb5e8"
+  version "9.2.2,20201229"
+  sha256 "8cf8806dd5b8b7c7826f04fad8b86fc7e07ea380eae497f3035f8c974de72cf8"
 
   url "https://www.ghidra-sre.org/ghidra_#{version.before_comma}_PUBLIC_#{version.after_comma}.zip"
   name "Ghidra"
   homepage "https://www.ghidra-sre.org/"
 
-  conflicts_with cask: "ghidra-beta"
+  conflicts_with cask: "homebrew/cask-versions/ghidra-beta"
 
   binary "ghidra_#{version.before_comma}_PUBLIC/ghidraRun"
 
