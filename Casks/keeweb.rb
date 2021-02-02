ad849a91b340e1cf8074a56f18f9196c3237a2df
antelle <dmitryvit@gmail.com>
2021-02-02 18:35:33 +0100

    Fixed style

--- Casks/keeweb.rb
+++ Casks/keeweb.rb
@@ -1,5 +1,6 @@
 cask "keeweb" do
   version "1.16.7"
+
   if Hardware::CPU.intel?
     sha256 "793aae610275f762e7d96822f88f908ef290403fa4d3c26f30e56fdcda55a147"
     url "https://github.com/keeweb/keeweb/releases/download/v#{version}/KeeWeb-#{version}.mac.x64.dmg",
