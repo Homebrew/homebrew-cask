65d86abf7c9aa32c0430e52e33ecb4bee317bfbd
Dimitri Witkowski <dmitryvit@gmail.com>
2021-02-03 02:09:42 +0100

    Update KeeWeb for Apple Silicon (#99225)
    * Update keeweb for Apple Silicon
    
    * Fixed style
    
    * Update keeweb.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/keeweb.rb
+++ Casks/keeweb.rb
@@ -1,13 +1,24 @@
 cask "keeweb" do
   version "1.16.7"
-  sha256 "793aae610275f762e7d96822f88f908ef290403fa4d3c26f30e56fdcda55a147"
 
-  url "https://github.com/keeweb/keeweb/releases/download/v#{version}/KeeWeb-#{version}.mac.x64.dmg",
-      verified: "github.com/keeweb/keeweb/"
-  appcast "https://github.com/keeweb/keeweb/releases.atom"
+  if Hardware::CPU.intel?
+    sha256 "793aae610275f762e7d96822f88f908ef290403fa4d3c26f30e56fdcda55a147"
+    url "https://github.com/keeweb/keeweb/releases/download/v#{version}/KeeWeb-#{version}.mac.x64.dmg",
+        verified: "github.com/keeweb/keeweb/"
+  else
+    sha256 "e74f7a088ec689b0ec6420aecc78c60aad12e48292867748e263b1990ec5135c"
+    url "https://github.com/keeweb/keeweb/releases/download/v#{version}/KeeWeb-#{version}.mac.arm64.dmg",
+        verified: "github.com/keeweb/keeweb/"
+  end
+
   name "KeeWeb"
   desc "Password manager compatible with KeePass"
   homepage "https://keeweb.info/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   auto_updates true
 
