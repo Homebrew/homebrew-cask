b0787d3d7c8d54b3a51d350bdfa347a1c854da96
Adithya Sunil <adithyasunil26@gmail.com>
2021-02-04 00:26:12 +0400

    Update refined-github-safari from 2.1.26 to 2.1.27 (#99279)

--- Casks/refined-github-safari.rb
+++ Casks/refined-github-safari.rb
@@ -1,6 +1,6 @@
 cask "refined-github-safari" do
-  version "2.1.26"
-  sha256 "0436ae27ce8c223166a7d0c642051b6409da05c744a15cd29856ffc2e23e814d"
+  version "2.1.27"
+  sha256 "2b667d695676749d4acba3e678f0314456134b1658f285d33d0fe89bb3882bc2"
 
   url "https://github.com/lautis/refined-github-safari/releases/download/v#{version}/Refined-GitHub-for-Safari.zip"
   appcast "https://github.com/lautis/refined-github-safari/releases.atom"
