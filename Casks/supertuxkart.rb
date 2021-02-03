1637ce21f65ce9e74ca0ba50bca86086cad9f612
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-04 02:27:49 +0800

    Update supertuxkart.rb (#99264)

--- Casks/supertuxkart.rb
+++ Casks/supertuxkart.rb
@@ -2,15 +2,15 @@ cask "supertuxkart" do
   version "1.2"
   sha256 "0ce988c748f254dcf2cf441c575e4715ee578bb439a9535b045cc1a60618a2ed"
 
-  url "https://downloads.sourceforge.net/supertuxkart/SuperTuxKart-#{version}-mac.zip",
-      verified: "sourceforge.net/supertuxkart/"
+  url "https://github.com/supertuxkart/stk-code/releases/download/#{version}/SuperTuxKart-#{version}-mac.zip",
+      verified: "github.com/supertuxkart/stk-code"
   name "SuperTuxKart"
+  desc "Kart racing game"
   homepage "https://supertuxkart.net/Main_Page"
 
   livecheck do
-    url "https://supertuxkart.net/Download"
-    strategy :page_match
-    regex(%r{href=.*?/SuperTuxKart-(\d+(?:\.\d+)*)-mac\.zip}i)
+    url :url
+    strategy :github_latest
   end
 
   app "SuperTuxKart.app"
