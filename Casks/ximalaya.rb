9a2d8c398b49ef468542f43599d36eea5f7f337b
adithyasunil26 <adithyasunil26@gmail.com>
2021-02-04 00:25:37 +0400

    Update ximalaya from 1.7.0,1610336058 to 1.7.1,1611898412

--- Casks/ximalaya.rb
+++ Casks/ximalaya.rb
@@ -1,6 +1,6 @@
 cask "ximalaya" do
-  version "1.7.0,1610336058"
-  sha256 "ac3e7afc92f64f3fb8c1b818ac024b717c4333cc4ed5cad003304853f42f9fdf"
+  version "1.7.1,1611898412"
+  sha256 "c7a3f7ec20ca66d164ca91dd116d83c0c653107b0c05cf51085623fab0a0271f"
 
   url "https://s1.xmcdn.com/yx/ximalaya-pc-client/#{version.before_comma}/download/Ximalaya-#{version.before_comma}_#{version.after_comma}.dmg",
       verified: "s1.xmcdn.com/"
