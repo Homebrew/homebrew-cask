fb1b84ebf04a061075045b9850664ae6681ddc3f
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-03 08:45:55 +0800

    Update pathephone.rb

--- Casks/pathephone.rb
+++ Casks/pathephone.rb
@@ -1,10 +1,16 @@
 cask "pathephone" do
   version "2.2.1"
-  sha256 :no_check
+  sha256 "94f11a3dfd047aff65fa15430a127e07d733518dd39618a15344fc4738a74806"
   
-  url "https://github.com/pathephone/pathephone-desktop/releases/download/v2.2.1/Pathephone-2.2.1.dmg"
+  url "https://github.com/pathephone/pathephone-desktop/releases/download/#{version}/Pathephone-#{version}.dmg"
   name "Pathephone"
+  desc "Distributed audio player"
   homepage "https://pathephone.github.io/"
-  
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
   app "Pathephone.app"
 end
