d99d94128800f3cb98663ce42824e2a4ef33ba81
adithyasunil26 <adithyasunil26@gmail.com>
2021-02-04 00:23:57 +0400

    Update tribler from 7.7.1 to 7.8.0-RC1

--- Casks/tribler.rb
+++ Casks/tribler.rb
@@ -1,6 +1,6 @@
 cask "tribler" do
-  version "7.7.1"
-  sha256 "746ab057998616170277e365dbb7df35b4d329b9508a749b962937f35ae6cbac"
+  version "7.8.0-RC1"
+  sha256 "aff104e3993470e2e75490f34a0f67805df39afe53587999cedf9bed350e0c2e"
 
   url "https://github.com/Tribler/tribler/releases/download/v#{version}/Tribler-#{version}.dmg",
       verified: "github.com/Tribler/tribler/"
