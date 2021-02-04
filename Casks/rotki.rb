acc5f288bbaf856e6ffb390b5060043ef16ba323
adithyasunil26 <adithyasunil26@gmail.com>
2021-02-05 01:36:15 +0400

    Update rotki from 1.13.0 to 1.13.1

--- Casks/rotki.rb
+++ Casks/rotki.rb
@@ -1,6 +1,6 @@
 cask "rotki" do
-  version "1.13.0"
-  sha256 "d1e4838bcd36eca6103953eb4e97fc03d1f4bec8ff6f12de5b000b2f127199d0"
+  version "1.13.1"
+  sha256 "db49dd0c5d3bb7ce9bfbc179a85adb192d32b7dac45e69db64955e255736e7f7"
 
   url "https://github.com/rotki/rotki/releases/download/v#{version}/rotki-darwin-v#{version}.dmg",
       verified: "github.com/rotki/rotki/"
