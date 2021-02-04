8d0c4a1a1d626c0d158052b7b7134fe63564cc07
Theron Tjapkes <theron@zmagnet.com>
2021-02-03 23:14:31 -0500

    Update unnaturalscrollwheels from 1.2.0 to 1.2.1

--- Casks/unnaturalscrollwheels.rb
+++ Casks/unnaturalscrollwheels.rb
@@ -1,6 +1,6 @@
 cask "unnaturalscrollwheels" do
-  version "1.2.0"
-  sha256 "91663a673d1cbf606de67048ad7c44da85d2522badedda0fef01f9f2f8d252b5"
+  version "1.2.1"
+  sha256 "fcb2b86b6db362a8a66aea562e5ddeec609432da3e330a536431f7b74681a6be"
 
   url "https://github.com/ther0n/UnnaturalScrollWheels/releases/download/#{version}/UnnaturalScrollWheels-#{version}.dmg"
   appcast "https://github.com/ther0n/UnnaturalScrollWheels/releases.atom"
