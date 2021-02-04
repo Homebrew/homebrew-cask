89eb5471e137a36cf046c298e3ef53b5d780a85a
Adithya Sunil <adithyasunil26@gmail.com>
2021-02-05 02:08:07 +0400

    Update houdahspot from 6.0.3,555 to 6.0.4,576 (#99347)

--- Casks/houdahspot.rb
+++ Casks/houdahspot.rb
@@ -1,6 +1,6 @@
 cask "houdahspot" do
-  version "6.0.3,555"
-  sha256 "85ddeb49b658f32dc6ebdc8b9cf4985fd155007d48beeb50ccb83377b3ae8b67"
+  version "6.0.4,576"
+  sha256 "0ae5616f602cb8790089e8b01403240a0a77ff2371778b1fe79d4d1ea3caf944"
 
   url "https://dl.houdah.com/houdahSpot/updates/cast#{version.major}_assets/HoudahSpot#{version.before_comma}.zip"
   name "HoudahSpot"
