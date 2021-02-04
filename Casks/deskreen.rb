ece738c54465d9721363b73e5e888b07185b43e2
Vanilla <neko@hydev.org>
2021-02-04 12:42:26 +0800

    Add Deskreen.app v1.0.3 (#99299)
    * Add Deskreen.app v1.0.3
    
    * Update deskreen.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/deskreen.rb
@@ -0,0 +1,24 @@
+cask "deskreen" do
+  version "1.0.3"
+  sha256 "ac4479de2c9e0c76049acef202d67b4dea6f106f07900e6b0e9c0eb50b0f98d0"
+
+  url "https://github.com/pavlobu/deskreen/releases/download/v#{version}/Deskreen-#{version}.dmg",
+      verified: "https://github.com/pavlobu/deskreen/"
+  name "Deskreen"
+  desc "Turns any device with a web browser into a secondary screen"
+  homepage "https://deskreen.com/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "Deskreen.app"
+
+  zap trash: [
+    "~/Library/Application Support/Deskreen",
+    "~/Library/Logs/Deskreen",
+    "~/Library/Preferences/com.pavlobu.Deskreen.plist",
+    "~/Library/Saved Application State/com.pavlobu.Deskreen.savedState",
+  ]
+end
