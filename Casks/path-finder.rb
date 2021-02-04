c861c24777a76b15dd087f45bcdaba18985e0321
Adithya Sunil <adithyasunil26@gmail.com>
2021-02-05 03:07:03 +0400

    Update path-finder from 10.0.1,2084 to 10.0.2,2086 (#99363)

--- Casks/path-finder.rb
+++ Casks/path-finder.rb
@@ -1,6 +1,6 @@
 cask "path-finder" do
-  version "10.0.1,2084"
-  sha256 "2c7dfd4722f129ec9bc3315617638f28ae2bc31e57d8d9a1f84789e31305a1f1"
+  version "10.0.2,2086"
+  sha256 "15746dc6b3d4a10084fcb67c545b09f265dbd8eee92736a1c2123f6404c7906b"
 
   url "https://get.cocoatech.com/PF#{version.major}.dmg"
   name "Path Finder"
