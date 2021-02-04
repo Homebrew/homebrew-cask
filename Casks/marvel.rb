0eb51e4229f43ab1d0e97c57d5e6cae5d9c2111d
adithyasunil26 <adithyasunil26@gmail.com>
2021-02-05 01:23:53 +0400

    Update marvel from 10.6.1,4659 to 10.6.3,4663

--- Casks/marvel.rb
+++ Casks/marvel.rb
@@ -1,6 +1,6 @@
 cask "marvel" do
-  version "10.6.1,4659"
-  sha256 "da141012c3f8c39c4a3be1d6c4c8277d7b304d47dfc8fdc9ab528bbd7f0baca5"
+  version "10.6.3,4663"
+  sha256 "cab1bfd24efad18392deca57533c403b3a33c27b90b594beee2a9cf0833c0ed0"
 
   url "https://storage.googleapis.com/sketch-plugin/#{version.before_comma}/Marvel.zip",
       verified: "storage.googleapis.com/sketch-plugin/"
