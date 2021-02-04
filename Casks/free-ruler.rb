59c43ee12255fffa22b41059abc9f52437a89efd
Adithya Sunil <adithyasunil26@gmail.com>
2021-02-05 02:07:26 +0400

    Update free-ruler from 2.0.4 to 2.0.5 (#99345)

--- Casks/free-ruler.rb
+++ Casks/free-ruler.rb
@@ -1,6 +1,6 @@
 cask "free-ruler" do
-  version "2.0.4"
-  sha256 "b736d22bd730ef9e94c9579aa55a40bd264f491c245980a84b469eb156c7267c"
+  version "2.0.5"
+  sha256 "6332b9252a4fc58dbf4a5f74b5484d6ae20c2f4cb7db7a2c86020454fa66444d"
 
   url "https://github.com/pascalpp/FreeRuler/releases/download/v#{version}/free-ruler-#{version}.zip",
       verified: "github.com/pascalpp/FreeRuler/"
