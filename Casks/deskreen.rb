77699a9c14e15c8a23e226bd962fb94fe27bad7f
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-04 11:27:37 +0800

    Update deskreen.rb

--- Casks/deskreen.rb
+++ Casks/deskreen.rb
@@ -2,11 +2,16 @@ cask "deskreen" do
   version "1.0.3"
   sha256 "ac4479de2c9e0c76049acef202d67b4dea6f106f07900e6b0e9c0eb50b0f98d0"
 
-  url "https://github.com/pavlobu/deskreen/releases/download/v#{version}/Deskreen-#{version}.dmg", verified: "https://github.com/pavlobu/deskreen"
-  appcast "https://github.com/pavlobu/deskreen/releases.atom"
+  url "https://github.com/pavlobu/deskreen/releases/download/v#{version}/Deskreen-#{version}.dmg",
+      verified: "https://github.com/pavlobu/deskreen/"
   name "Deskreen"
   desc "Turns any device with a web browser into a secondary screen"
   homepage "https://deskreen.com/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "Deskreen.app"
 
