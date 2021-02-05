e9aa1bdd5779d83f18cdad7d84e93a80798833d6
adithyasunil26 <adithyasunil26@gmail.com>
2021-02-06 01:01:01 +0400

    Update arrsync from 0.4.1 to 0.4

--- Casks/arrsync.rb
+++ Casks/arrsync.rb
@@ -1,6 +1,6 @@
 cask "arrsync" do
-  version "0.4.1"
-  sha256 "8a254771065f90a9f9f882589eccaf7bc51ad5d0ac86faf15847381bcdbe64f0"
+  version "0.4"
+  sha256 "3c50f834712f6745d1cc723f88eb7d93339b7c9377edb2d551c130ea3f01bab3"
 
   url "https://downloads.sourceforge.net/arrsync/arrsync-#{version}.dmg",
       verified: "downloads.sourceforge.net/arrsync/"
