e043d0e0d1a1400d7bb7c8a76d089256a286624b
Hannes Braun <hannesbraun@mail.de>
2021-02-05 23:33:55 +0100

    Update blitz from 1.13.90 to 1.13.92 (#99416)

--- Casks/blitz.rb
+++ Casks/blitz.rb
@@ -1,6 +1,6 @@
 cask "blitz" do
-  version "1.13.90"
-  sha256 "7247195543760a456d3d5d59a2e80d7ac9efdce6df4aedfab721ee7b8c0d8638"
+  version "1.13.92"
+  sha256 "535de0f913006f8bb5d43956757cf6fc143fe8fd89c7eb646a8dfb99714cbdf2"
 
   url "https://dl.blitz.gg/download/Blitz-#{version}.dmg"
   name "Blitz"
