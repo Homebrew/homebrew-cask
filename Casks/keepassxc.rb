87a1ccb166d914c040a89865aa60cd4636faee33
Przemysław Szypowicz <pszypowicz@Mac-mini-Przemysaw.local>
2021-02-05 15:59:55 +0100

    Different build version for arm64

--- Casks/keepassxc.rb
+++ Casks/keepassxc.rb
@@ -1,14 +1,18 @@
 cask "keepassxc" do
-  version "2.6.4"
+  if Hardware::CPU.intel?
+    version "2.6.4"
+  else
+    version "2.6.4-2"
+  end
 
   if Hardware::CPU.intel?
     url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}-x86_64.dmg",
         verified: "github.com/keepassxreboot/keepassxc/"
     sha256 "639fdfe0379dc3f00f1bd6a72c974cfddae0d4c8ddb66f7297d0b01b1a582ede"
   else
-    url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}-arm64.dmg",
+    url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version.sub(/-\d+/, "")}/KeePassXC-#{version}-arm64.dmg",
         verified: "github.com/keepassxreboot/keepassxc/"
-    sha256 "16dcdfae65ad8887b4d9cd86bf56bc7df2dca3e3fedf91b07b42a0f6e1465c48"
+    sha256 "49c28e3f6341710b24d4415186ec0a52f6c0af5e72c0fc7548ddd7999d2eaa4d"
   end
 
   name "KeePassXC"
