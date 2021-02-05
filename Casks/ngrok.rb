c77359ee01e7e5478ad75ea6f99f0efb2c437f6c
Gerardo Di Giacomo <19227040+gedigi@users.noreply.github.com>
2021-02-04 23:45:04 -0800

    fix 'brew style' errors

--- Casks/ngrok.rb
+++ Casks/ngrok.rb
@@ -10,10 +10,9 @@ cask "ngrok" do
 
   binary "ngrok"
 
-  zap trash: "~/.ngrok2"
-
-
   postflight do
     FileUtils.chmod 0755, Dir.glob("#{staged_path}/ngrok")
   end
+
+  zap trash: "~/.ngrok2"
 end
