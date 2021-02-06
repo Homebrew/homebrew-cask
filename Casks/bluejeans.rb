4084d3ff9307c92a8c7254f2a33f7c99060d4e80
Logan Rosen <lrosen@linkedin.com>
2021-02-05 19:11:05 -0500

    bluejeans: add livecheck; bump to 2.26.0.289 (#99398)

--- Casks/bluejeans.rb
+++ Casks/bluejeans.rb
@@ -1,12 +1,16 @@
 cask "bluejeans" do
-  version "2.25.0.230"
-  sha256 "509c5a96341cc6e5fd1559dec715b9f64740d01fa8db6ea2ae3db8a9c3d6df00"
+  version "2.26.0.289"
+  sha256 "6d582a61201d11ac1042370264dc968765a2c75c983152ce7a6af6a335746df8"
 
   url "https://swdl.bluejeans.com/desktop-app/mac/#{version.major_minor_patch}/#{version}/BlueJeansInstaller.pkg"
-  appcast "https://www.bluejeans.com/downloads"
   name "BlueJeans"
   desc "Video conferencing for the digital workplace"
   homepage "https://www.bluejeans.com/"
+
+  livecheck do
+    url "https://swdl.bluejeans.com/desktop-app/mac/ga.appcast.xml"
+    strategy :sparkle
+  end
 
   pkg "BlueJeansInstaller.pkg"
 
