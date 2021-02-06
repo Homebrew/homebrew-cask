1516868a56e0a64bf7684418bf6817e8fa58d824
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-06 08:27:28 +0800

    Update keepassxc.rb

--- Casks/keepassxc.rb
+++ Casks/keepassxc.rb
@@ -1,15 +1,11 @@
 cask "keepassxc" do
   if Hardware::CPU.intel?
     version "2.6.4"
-  else
-    version "2.6.4-2"
-  end
-
-  if Hardware::CPU.intel?
     url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}-x86_64.dmg",
         verified: "github.com/keepassxreboot/keepassxc/"
     sha256 "639fdfe0379dc3f00f1bd6a72c974cfddae0d4c8ddb66f7297d0b01b1a582ede"
   else
+    version "2.6.4-2"
     url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version.sub(/-\d+/, "")}/KeePassXC-#{version}-arm64.dmg",
         verified: "github.com/keepassxreboot/keepassxc/"
     sha256 "49c28e3f6341710b24d4415186ec0a52f6c0af5e72c0fc7548ddd7999d2eaa4d"
