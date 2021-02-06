ca2ed96ea46c53ed8fe26a601c888a0461b37d5d
linbo0518 <linbo0518@gmail.com>
2021-02-06 13:37:38 +0800

    Update v2rayu from 2.3.1 to 3.0.0

--- Casks/v2rayu.rb
+++ Casks/v2rayu.rb
@@ -1,6 +1,6 @@
 cask "v2rayu" do
-  version "2.3.1"
-  sha256 "2e8f132c05a8af3b89bd0dacafd2fc9244636587848453f7cd877ab246a33d52"
+  version "3.0.0"
+  sha256 "b94e07e440f6b9ec501a7ddae05065792f8817a62ca5bf4cbc3460e0aa2ebc2d"
 
   url "https://github.com/yanue/V2rayU/releases/download/#{version}/V2rayU.dmg"
   appcast "https://github.com/yanue/V2rayU/releases.atom"
