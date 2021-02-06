bb8ca8f29f1a15b8200c15e122e20a35cc72a315
Thomas Amerongen <thomas.amerongen@gmail.com>
2021-02-06 19:46:11 +1100

    Update camo-studio from 1.2.2,129 to 1.2.3,139 (#99429)

--- Casks/camo-studio.rb
+++ Casks/camo-studio.rb
@@ -1,6 +1,6 @@
 cask "camo-studio" do
-  version "1.2.2,129"
-  sha256 "6029037c45831e078de403a8bb07d31106f8f2958e70ab27e4b318cb568f6931"
+  version "1.2.3,139"
+  sha256 "dc5983b84f2a0b5500fa44e59acf744228cf03d3e10497b05ba09944d6acfa08"
 
   url "https://reincubate.com/res/labs/camo/Camo%20Studio%20#{version.before_comma}%20%28#{version.after_comma}%29%20%5BRelease%5D.app.zip"
   appcast "https://uds.reincubate.com/release-notes/camo/?format=sparkle"
