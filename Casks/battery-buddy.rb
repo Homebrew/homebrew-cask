bd483ed6c3870b97b16ba4878059bcffc782d2ec
Salvatore Testa <sal.testa@gmail.com>
2021-02-06 11:57:57 -0800

    Add Battery Buddy.app v1.0.0
    Making @neilsardesai adorable app that much easier to download.

---
+++ Casks/battery-buddy.rb
@@ -0,0 +1,11 @@
+cask "battery-buddy" do
+  version "1.0.0"
+  sha256 :no_check
+
+  url "https://batterybuddy.app/releases/Battery%20Buddy.zip"
+  name "Battery Buddy"
+  desc "It’s like the default battery indicator in your menu bar, but cute"
+  homepage "https://batterybuddy.app/"
+
+  app "Battery Buddy.app"
+end
