7a03e5898dfbc4cee9fe4f36155a03e40bbec7f4
Wietse de Vries <wietsedvries@gmail.com>
2021-02-06 01:05:32 +0100

    Add Gather.app v0.0.6 (#99310)

---
+++ Casks/gather.rb
@@ -0,0 +1,14 @@
+cask "gather" do
+  version "0.0.6"
+  sha256 "d5de3b7b8f36d58c323052b88de549187a324b9486f8aed5269f71a269e83460"
+
+  url "https://github.com/gathertown/gather-town-desktop-releases/releases/download/v#{version}/Gather-#{version}-mac.zip",
+      verified: "github.com/gathertown/gather-town-desktop-releases"
+  name "Gather Town"
+  desc "Virtual video-calling space"
+  homepage "https://gather.town/"
+
+  app "Gather.app"
+
+  zap trash: "~/Library/Application Support/Gather"
+end
