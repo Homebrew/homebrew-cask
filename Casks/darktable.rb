d0d5553fd31daa181f5266eb85ec812da8830625
Ian Griffiths <6thimage@users.noreply.github.com>
2021-02-07 02:41:18 +0000

    Update darktable from 3.4.0 to 3.4.1 (#99441)
    * Update darktable from 3.4.0 to 3.4.1
    
    * Updated conflicts for darktable

--- Casks/darktable.rb
+++ Casks/darktable.rb
@@ -1,6 +1,6 @@
 cask "darktable" do
-  version "3.4.0"
-  sha256 "da72476993160d1cb2ff8e40c6bd402f1032884936a9eb818197c936d4b22a14"
+  version "3.4.1"
+  sha256 "e13112ed1d5f9c55e5287aa9d7276f04b90909b2e356640f36227a0a53321658"
 
   url "https://github.com/darktable-org/darktable/releases/download/release-#{version.major_minor_patch}/darktable-#{version}.dmg",
       verified: "github.com/darktable-org/darktable/"
@@ -9,7 +9,7 @@ cask "darktable" do
   desc "Photography workflow application and raw developer"
   homepage "https://www.darktable.org/"
 
-  conflicts_with cask: "darktable-dev"
+  conflicts_with cask: "homebrew/cask-versions/darktable-dev"
 
   app "darktable.app"
 end
