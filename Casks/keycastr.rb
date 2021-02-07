84d0f85135a23a64c2796160e913ce03988c7ee9
Andrew Kitchen <akitchen@gmail.com>
2021-02-07 12:47:34 -0800

    Update keycastr from 0.9.9 to 0.9.10 (#99483)

--- Casks/keycastr.rb
+++ Casks/keycastr.rb
@@ -1,6 +1,6 @@
 cask "keycastr" do
-  version "0.9.9"
-  sha256 "976e4a2dc1a364bfbf5164ab1e5a37bae7d7e4e8fb41f0c0a524e2802b79c0eb"
+  version "0.9.10"
+  sha256 "02f5b18b6911a74a82d2b95339d71e3941deed98c8051cc929d9af49c05dcdf3"
 
   url "https://github.com/keycastr/keycastr/releases/download/v#{version}/KeyCastr.app.zip"
   appcast "https://github.com/keycastr/keycastr/releases.atom"
