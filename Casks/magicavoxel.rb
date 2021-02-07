8adf3943588ffbd6a39c5874928cc61fb04ff31f
John Manoogian III <jm3@jm3.net>
2021-02-06 21:43:42 -0800

    update Magica Voxel to 0.99.6.2 (#99465)
    update release url schema
    add description
    
    Co-authored-by: jm3 <jm3@jm3.net stub>

--- Casks/magicavoxel.rb
+++ Casks/magicavoxel.rb
@@ -1,12 +1,13 @@
 cask "magicavoxel" do
-  version "0.99.4.2"
-  sha256 "2ee6e63c45b4e3d7a99b185d48bb9c2b8e8140e88d106c1d86303243379c2729"
+  version "0.99.6.2"
+  sha256 "4ee661e41da07f8c2b2b1c916bd1e18f7c5229900f30467eeea73ad74e861226"
 
-  url "https://github.com/ephtracy/ephtracy.github.io/releases/download/#{version.major_minor_patch}/MagicaVoxel-#{version}-alpha-macos.zip",
+  url "https://github.com/ephtracy/ephtracy.github.io/releases/download/#{version.major_minor_patch}/MagicaVoxel-#{version}-macos-10.15.zip",
       verified: "github.com/ephtracy/ephtracy.github.io/"
   appcast "https://github.com/ephtracy/ephtracy.github.io/releases.atom",
           must_contain: version.major_minor_patch
   name "MagicaVoxel"
+  desc "8-bit 3D voxel editor and interactive path tracing renderer"
   homepage "https://ephtracy.github.io/"
 
   suite staged_path, target: "MagicaVoxel"
