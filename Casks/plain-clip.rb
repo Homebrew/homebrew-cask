2fba71c454eea12f3ca6acfcc535eeae5ca104b8
Alexander <abuabara@users.noreply.github.com>
2021-02-08 00:24:12 -0600

    Update plain-clip.rb

--- Casks/plain-clip.rb
+++ Casks/plain-clip.rb
@@ -1,6 +1,6 @@
 cask "plain-clip" do
   version "2.5.2"
-  sha256 no_check
+  sha256 "d6672fd8596ccbd3af78c2c80a21e9aae7509e4be99c5e86a071bdfa83fd03bb"
 
   url "https://www.bluem.net/files/plain-clip.zip"
   name "Plain Clip"
