6dd8024dbce3e13de42d88ea541fd4864bfb8683
Adithya Sunil <adithyasunil26@gmail.com>
2021-02-08 13:07:21 +0400

    Update visual-paradigm from 16.2,20210101 to 16.2,20210201 (#99510)

--- Casks/visual-paradigm.rb
+++ Casks/visual-paradigm.rb
@@ -1,6 +1,6 @@
 cask "visual-paradigm" do
-  version "16.2,20210101"
-  sha256 "667ab269575783f16e37b167407dbac43c33087fa035b7ba24b6b97bdedc0ec8"
+  version "16.2,20210201"
+  sha256 "c2d659298266794f2f57553751c750badc86ef9a9aa15ce3d9bd14fd37fe6701"
 
   url "https://www.visual-paradigm.com/downloads/vp#{version.before_comma}/#{version.after_comma}/Visual_Paradigm_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
   name "Visual Paradigm"
