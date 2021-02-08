36db2f999b5fdfb401aecad080e70598603ef9ea
Vítor Galvão <info@vitorgalvao.com>
2021-02-08 03:00:55 +0000

    Update fspy.rb

--- Casks/fspy.rb
+++ Casks/fspy.rb
@@ -4,18 +4,9 @@ cask "fspy" do
 
   url "https://github.com/stuffmatic/fSpy/releases/download/v#{version}/fSpy-#{version}.dmg",
       verified: "github.com/stuffmatic/fSpy"
-  appcast "https://github.com/stuffmatic/fSpy/releases.atom"
   name "fSpy"
   desc "Still image camera matching"
   homepage "https://fspy.io/"
-
-  def caveats
-    <<~EOS
-      Download the Blender fSpy plugin here:
-      https://github.com/stuffmatic/fSpy-Blender
-      https://github.com/stuffmatic/fSpy-Blender/releases/download/v#{version}/fSpy-Blender-#{version}.zip
-    EOS
-  end
 
   app "fSpy.app"
 
