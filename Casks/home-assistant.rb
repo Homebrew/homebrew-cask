5918d9c5ccb65f0660ee11bbe24e28df229f61ab
Christopher Orr <chris@orr.me.uk>
2021-02-08 11:27:01 +0100

    Update home-assistant from 2021.2,2021.39 to 2021.2.1,2021.50 (#99516)

--- Casks/home-assistant.rb
+++ Casks/home-assistant.rb
@@ -1,6 +1,6 @@
 cask "home-assistant" do
-  version "2021.2,2021.39"
-  sha256 "3d81d83c6fbb045a067055b35d702c049331ebedea0a10b4bbbadfea83e2f44f"
+  version "2021.2.1,2021.50"
+  sha256 "14976b89bdc1fb4fdc44af23ffcc58d42c0cc19891b3e6cebdd8664166fc55ad"
 
   url "https://github.com/home-assistant/iOS/releases/download/release%2F#{version.before_comma}%2F#{version.after_comma}/home-assistant-mac.zip",
       verified: "github.com/home-assistant/iOS/"
