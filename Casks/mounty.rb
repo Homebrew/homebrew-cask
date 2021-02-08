2573ad4f5781403adf2f4ab20450a090efdf6e20
SupremeSir <haofanfang@gmail.com>
2021-02-08 11:55:53 +0800

    Update mounty from 1.12 to 1.12,19

--- Casks/mounty.rb
+++ Casks/mounty.rb
@@ -4,7 +4,7 @@ cask "mounty" do
     sha256 "5fcedfe712f59c14f39c3385dfed9aebc99d4e8d88f6e870f364cc48624590ef"
     url "https://mounty.app/releases/Mounty-#{version.before_comma}.dmg"
   else
-    version "1.11,16"
+    version "1.12,19"
     sha256 :no_check
     url "https://mounty.app/releases/Mounty.dmg"
   end
