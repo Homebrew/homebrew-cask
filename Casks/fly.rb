ef6062025729e310f7c3d049f33296d7dad188c0
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-02-09 20:13:09 +0900

    fly: update to 6.7.4 (#99549)

--- Casks/fly.rb
+++ Casks/fly.rb
@@ -1,6 +1,6 @@
 cask "fly" do
-  version "6.7.3"
-  sha256 "3e4a347868c444c6dc4b595082e508b6242d6fbd14a9475c576058ceda51294e"
+  version "6.7.4"
+  sha256 "eb3b118cd7d50552df5e66d490be3ccbb16b78ebf98e61865079ffbbc80b7223"
 
   url "https://github.com/concourse/concourse/releases/download/v#{version}/fly-#{version}-darwin-amd64.tgz"
   appcast "https://github.com/concourse/concourse/releases.atom"
