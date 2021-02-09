b9a9b8dd7590e1aba35756ff9fd1c003b4c3ad25
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-09 09:00:35 +0800

    Update jami.rb

--- Casks/jami.rb
+++ Casks/jami.rb
@@ -1,14 +1,18 @@
 cask "jami" do
-  version "20201208.1930"
-  sha256 "f91736b69a0b89dd4cb36e370e3fd807d3e9348e6c82b25f1f83086b472e82d3"
+  version "202102080940"
+  sha256 "4757a55f5b3bbd355d8f1ac29ea5be9ee6fef8e25b9d3a92fef8a832cd1510b2"
 
-  url "https://dl.jami.net/mac_osx/jami-#{version.no_dots}.dmg"
-  appcast "https://dl.jami.net/mac_osx/sparkle-ring.xml",
-          must_contain: version.no_dots
+  url "https://dl.jami.net/mac_osx/jami-#{version}.dmg"
   name "Jami"
   name "Savoir-faire Linux Ring"
   desc "Decentralised instant messenger and softphone"
   homepage "https://jami.net/"
+
+  livecheck do
+    url "https://dl.jami.net/mac_osx/sparkle-ring.xml"
+    strategy :page_match
+    regex(/jami-(\d+(?:\.\d+)*)\.dmg/i)
+  end
 
   auto_updates true
 
