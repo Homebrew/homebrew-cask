ec8be569c45c73817c860a4ecf32c28bd41609b4
Dmytro Kyshchenko <sys2712@gmail.com>
2021-02-10 00:10:27 +0200

    Update dotnet-sdk from 5.0.102,52bd43f7-88c9-420c-80c0-f19bae344293:a8d56f01fdb6f71adb5e22c3ddd1c3c6 to 5.0.103,3e9caf4-2087-4bc6-8031-5efba1268703:a6b0491578d385a9780603ea51df8de9

--- Casks/dotnet-sdk.rb
+++ Casks/dotnet-sdk.rb
@@ -3,8 +3,8 @@ cask "dotnet-sdk" do
     version "2.2.402,7430e32b-092b-4448-add7-2dcf40a7016d:1076952734fbf775062b48344d1a1587"
     sha256 "e74d816bc034d0fcdfa847286a6cad097227d4864da1c97fe801012af0c26341"
   else
-    version "5.0.102,52bd43f7-88c9-420c-80c0-f19bae344293:a8d56f01fdb6f71adb5e22c3ddd1c3c6"
-    sha256 "0cfca74440da0ba98803547d3eda4a0c3c4152d5fbc69c6d83f2d9b663ac925f"
+    version "5.0.103,43e9caf4-2087-4bc6-8031-5efba1268703:a6b0491578d385a9780603ea51df8de9"
+    sha256 "2abc63bdc41415cb7bd7743996045e4da38442ef2102c83f49a7a1020c9fa07c"
   end
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
