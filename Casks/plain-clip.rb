757ec9d3630d30c6fa7991b87d4162e468fba488
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-09 09:03:58 +0800

    Update plain-clip.rb

--- Casks/plain-clip.rb
+++ Casks/plain-clip.rb
@@ -1,10 +1,10 @@
 cask "plain-clip" do
   version "2.5.2"
-  sha256 "d6672fd8596ccbd3af78c2c80a21e9aae7509e4be99c5e86a071bdfa83fd03bb"
+  sha256 :no_check
 
   url "https://www.bluem.net/files/plain-clip.zip"
   name "Plain Clip"
   homepage "https://www.bluem.net/en/mac/plain-clip/"
 
-  app "Plain Clip"
+  app "Plain Clip.app"
 end
