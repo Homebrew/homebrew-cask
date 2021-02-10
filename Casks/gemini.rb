2955ef32855d397e0b9fc3925dc4931c4a58d6a4
adithyasunil26 <adithyasunil26@gmail.com>
2021-02-10 12:41:33 +0400

    Update gemini from 2.8.3,372:1612524856 to 2.8.4,373:1612881419

--- Casks/gemini.rb
+++ Casks/gemini.rb
@@ -1,6 +1,6 @@
 cask "gemini" do
-  version "2.8.3,372:1612524856"
-  sha256 "c10ced442fe907cd890fab9d6f1fb93196f9122bcd14d3f2bef2fdd4f78d93fb"
+  version "2.8.4,373:1612881419"
+  sha256 "a0cd7200bda18776cb3f0035de33306426deefc83fed9640fb5e145b7c52fb7c"
 
   url "https://dl.devmate.com/com.macpaw.site.Gemini#{version.major}/#{version.after_comma.before_colon}/#{version.after_colon}/Gemini#{version.major}-#{version.after_comma.before_colon}.zip",
       verified: "dl.devmate.com/com.macpaw.site.Gemini"
