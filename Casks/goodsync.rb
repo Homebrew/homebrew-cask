24714d3d6694d35956e5a131450637d05475342a
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-10 19:54:22 +0800

    Update goodsync.rb

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,13 +1,17 @@
 cask "goodsync" do
   version "11.5.6"
-  sha256 :no_check
+  sha256 "ca407a854c69cfe085247d0811606f6522df02568b1f15f9d3599ab3b0f6ac88"
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
-  appcast "https://www.goodsync.com/download?os=macos",
-          must_contain: version.major_minor_patch
   name "GoodSync"
   desc "File synchronization and backup software"
   homepage "https://www.goodsync.com/"
+
+  livecheck do
+    url "https://www.goodsync.com/download?os=macos"
+    strategy :page_match
+    regex(/GoodSync\ for\ Mac\ v\ (\d+(?:\.\d+)*)/i)
+  end
 
   depends_on macos: ">= :yosemite"
 
