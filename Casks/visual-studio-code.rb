6b2f38b5c6fd344013640f785cabec67283d53b8
EINDEX <eindex.me@outlook.com>
2021-02-10 11:50:47 +0800

    Add arm64 support for visual-studio-code

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -1,18 +1,32 @@
 cask "visual-studio-code" do
   version "1.53.1"
-  sha256 "cce23e760b7103d3e5e2d8ef488f8cf6b1f1e31aa42f68b0bcde66327db12cb2"
 
-  url "https://update.code.visualstudio.com/#{version}/darwin/stable"
+  if Hardware::CPU.intel?
+    sha256 "cce23e760b7103d3e5e2d8ef488f8cf6b1f1e31aa42f68b0bcde66327db12cb2"
+
+    url "https://update.code.visualstudio.com/#{version}/darwin/stable"
+
+    livecheck do
+      url "https://update.code.visualstudio.com/api/update/darwin/stable/VERSION"
+      strategy :page_match
+      regex(/"productVersion"\s*:\s*"(\d+(:?\.\d+)*)"/)
+    end
+  else
+    sha256 "decf286cae311a808c7f0383fa74c6799aa8a6a4533db0c869689496c4d108bb"
+
+    url "https://update.code.visualstudio.com/#{version}/darwin-arm64/stable"
+
+    livecheck do
+      url "https://update.code.visualstudio.com/api/update/darwin-arm64/stable/VERSION"
+      strategy :page_match
+      regex(/"productVersion"\s*:\s*"(\d+(:?\.\d+)*)"/)
+    end
+  end
+
   name "Microsoft Visual Studio Code"
   name "VS Code"
   desc "Open-source code editor"
   homepage "https://code.visualstudio.com/"
-
-  livecheck do
-    url "https://update.code.visualstudio.com/api/update/darwin/stable/VERSION"
-    strategy :page_match
-    regex(/"productVersion"\s*:\s*"(\d+(:?\.\d+)*)"/)
-  end
 
   auto_updates true
 
