570553bef7838a5d7fc471190c72d6f056495faf
i0ntempest <szf1234@me.com>
2021-02-10 09:41:42 -0500

    Update clover-configurator from 5.17.3.0 to 5.17.4.0

--- Casks/clover-configurator.rb
+++ Casks/clover-configurator.rb
@@ -1,5 +1,5 @@
 cask "clover-configurator" do
-  version "5.17.3.0"
+  version "5.17.4.0"
   sha256 :no_check
 
   url "https://mackie100projects.altervista.org/apps/cloverconf/download-new-build.php?version=global"
