cfb200b44d02b5e30e6264f1ca00c2ac5299fbe8
John Wyles <john@johnwyles.com>
2021-02-10 11:54:53 -0800

    Upgrading Webull 3.20.22 to 4.0.7

--- Casks/webull.rb
+++ Casks/webull.rb
@@ -1,13 +1,13 @@
 cask "webull" do
-  version "3.22.20"
-  sha256 "dd1658952e3b1b1fecb55628ba407322fb7b8976330e8a334ff8a8540965e735"
+  version "4.7.0"
+  sha256 "48e01c5b048230793cd9f5925e037b4c396b763a5be0e81d3d906a063f607d29"
 
-  url "https://pub.webull.com/global/DesktopClient/MacOS/Webull-#{version}.dmg"
+  url "https://pub.webull.com/us/desktop/Webull%20Desktop%20#{version}.dmg"
   name "Webull"
   desc "Desktop client for Webull Financial LLC"
   homepage "https://webull.com/"
 
-  app "Webull.app"
+  app "Webull Desktop.app"
 
   zap trash: [
     "~/Library/Application Support/Webull Desktop",
