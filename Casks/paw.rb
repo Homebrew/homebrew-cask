e87c6d7309b8e4c5542a5c81cdb680400ad6e7f7
Reno <singularitti@outlook.com>
2021-02-10 10:19:41 -0500

    Update paw from 3.2 to 3.2.1 (#99602)

--- Casks/paw.rb
+++ Casks/paw.rb
@@ -1,6 +1,6 @@
 cask "paw" do
-  version "3.2"
-  sha256 "d2ecc62da75260de539bc6ae3c14ebebfbc5e194ea2f385f6f48372d5eead4b1"
+  version "3.2.1"
+  sha256 "f532516ba27c0d1c5a0568eab1314c75c086846636a2c046b261717c8f9bf367"
 
   url "https://cdn-builds.paw.cloud/paw/Paw-#{version}.zip"
   appcast "https://paw.cloud/api/v2/updates/appcast"
