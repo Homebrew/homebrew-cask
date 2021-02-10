4e0e671f5d2858c2f966e7bea9aec5f8e3f07b72
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-10 15:43:18 +0800

    Update iterm2.rb

--- Casks/iterm2.rb
+++ Casks/iterm2.rb
@@ -14,8 +14,9 @@ cask "iterm2" do
   homepage "https://www.iterm2.com/"
 
   livecheck do
-    url "https://iterm2.com/appcasts/final_modern.xml"
-    strategy :sparkle
+    url "https://iterm2.com/appcasts/full_changes.txt"
+    strategy :page_match
+    regex(/(\d+(?:\.\d+)*)/i)
   end
 
   auto_updates true
