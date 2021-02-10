3f952b13fe0257eed0e85971df9ffdd4bdf8c78c
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-10 19:28:12 +0800

    Update cakebrewjs.rb

--- Casks/cakebrewjs.rb
+++ Casks/cakebrewjs.rb
@@ -3,10 +3,15 @@ cask "cakebrewjs" do
   sha256 "e38c9365b5312be9231a912f8a8d9ff93631dcd922b0128e540d4a13774fed5e"
 
   url "https://downloads.sourceforge.net/cakebrewjs/v#{version}/cakebrewjs-#{version}-mac.zip"
-  appcast "https://sourceforge.net/projects/cakebrewjs/rss?"
   name "cakebrewjs"
-  desc "Homebrew GUI App written in electron"
+  desc "Homebrew GUI app written in electron"
   homepage "https://sourceforge.net/projects/cakebrewjs"
+
+  livecheck do
+    url "https://sourceforge.net/projects/cakebrewjs/rss?"
+    strategy :page_match
+    regex(/cakebrewjs-(\d+(?:\.\d+)*)-mac\.zip/i)
+  end
 
   app "cakebrewjs.app"
 
