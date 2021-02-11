e1fea680f2cdf09926c976800652ee9b787220bb
robotkid <2223214+robotkid@users.noreply.github.com>
2021-02-11 15:10:00 +0800

    Upgrade SceneBuilder.app to 15.0.1 (#99619)
    * Upgrade SceneBuilder.app to 15.0.1
    
    SceneBuilder is now distributed as a .dmg instead of a .pkg. Add
    livecheck stanza.
    
    * scenebuilder style fix
    
    Co-authored-by: Dan Horwood <dpj@sent.com>

--- Casks/scenebuilder.rb
+++ Casks/scenebuilder.rb
@@ -1,15 +1,19 @@
 cask "scenebuilder" do
-  version "11.0.0"
-  sha256 "0602d1cd7156fa0ff49cc4055e6aec020227df813449b2bc3782a3fddffb43bb"
+  version "15.0.1"
+  sha256 "41cad631faf489d70c29368c5113312b9a4eb5d646a3b295aac266e7af863fb1"
 
-  url "https://download2.gluonhq.com/scenebuilder/#{version}/install/mac/SceneBuilder-#{version}.pkg"
-  appcast "https://gluonhq.com/products/scene-builder/#download"
+  url "https://download2.gluonhq.com/scenebuilder/#{version}/install/mac/SceneBuilder-#{version}.dmg"
   name "Scene Builder"
+  desc "Drag & drop GUI designer for JavaFX"
   homepage "https://gluonhq.com/products/scene-builder/"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(%r{href=.*?/SceneBuilder-(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   depends_on macos: ">= :high_sierra"
 
-  pkg "SceneBuilder-#{version}.pkg"
-
-  uninstall pkgutil: "com.gluonhq.scenebuilder"
+  app "SceneBuilder.app"
 end
