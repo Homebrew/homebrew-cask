cc0fb47c6c4a6ae4e936b79c4ee6d6d37222d26f
David Yantis <404NetworkError@users.noreply.github.com>
2021-02-11 02:08:33 -0500

    Update mullvadvpn from 2020.7 to 2021.1 (#99617)
    * Update mullvadvpn from 2020.7 to 2021.1
    
    * Update mullvadvpn.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/mullvadvpn.rb
+++ Casks/mullvadvpn.rb
@@ -1,15 +1,19 @@
 cask "mullvadvpn" do
-  version "2020.7"
-  sha256 "7ec7729557afe6291731181173802761596d70e4fe93a4608809ed2f9320331c"
+  version "2021.1"
+  sha256 "b5a7fa8ba33619f53a16e56398ec91c4db101d04a7bc0f766429fd721744bcd6"
 
   url "https://github.com/mullvad/mullvadvpn-app/releases/download/#{version}/MullvadVPN-#{version}.pkg",
       verified: "github.com/mullvad/mullvadvpn-app/"
-  appcast "https://github.com/mullvad/mullvadvpn-app/releases.atom"
   name "Mullvad VPN"
   desc "VPN client"
   homepage "https://mullvad.net/"
 
-  conflicts_with cask: "mullvadvpn-beta"
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  conflicts_with cask: "homebrew/cask-versions/mullvadvpn-beta"
 
   pkg "MullvadVPN-#{version}.pkg"
 
