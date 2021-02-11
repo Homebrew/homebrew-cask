988cd607098d41d05018ef71b38f0f60d42dcc0c
Charles Siu <chunheisiu@gmail.com>
2021-02-11 19:46:32 +0800

    Update futubull from 11.0.762,202101151723 to 11.1.778,202102011105

--- Casks/futubull.rb
+++ Casks/futubull.rb
@@ -1,6 +1,6 @@
 cask "futubull" do
-  version "11.0.762,202101151723"
-  sha256 "aefd294a93d8d7ef91c261a9be31e2ca5b570f5592c0aef81a5f39bb40e210f3"
+  version "11.1.778,202102011105"
+  sha256 "0baa3d100cfdc4e447e758e4756f784b7ae003c29b81f7efd5d3d1db95ea18fc"
 
   url "https://softwarefile.futunn.com/FTNNForMac_#{version.before_comma}_#{version.after_comma}_Website.dmg"
   appcast "https://www.futunn.com/download/mac?lang=en-US"
