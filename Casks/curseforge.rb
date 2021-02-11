94428bcd15f733d884d6a228955ac3ea5bf58977
Daniel Bergey <dbergey@barracuda.com>
2021-02-10 23:53:54 -0500

    Add CurseForge latest

---
+++ Casks/curseforge.rb
@@ -0,0 +1,19 @@
+cask "curseforge" do
+  version :latest
+  sha256 :no_check
+
+  url "https://curseforge.overwolf.com/downloads/curseforge-latest.dmg"
+  name "CurseForge"
+  desc "Download and manage your addons and mods with the new CurseForge app!"
+  homepage "https://curseforge.overwolf.com/"
+
+  app "CurseForge.app"
+
+  uninstall launchctl: "com.overwolf.curseforge.ShipIt",
+            quit:      "com.overwolf.curseforge"
+
+  zap trash: [
+    "~/Library/Application Support/Caches/curseforge-updater",
+    "~/Library/Application Support/CurseForge",
+  ]
+end
