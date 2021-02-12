ea587bfddade60d2e1c209e8a3be69d854439fe3
adithyasunil26 <adithyasunil26@gmail.com>
2021-02-13 00:40:44 +0400

    Update stoplight-studio from 2.2.0,5379.git-75c5189 to 2.3.0,5448.git-f63f57f

--- Casks/stoplight-studio.rb
+++ Casks/stoplight-studio.rb
@@ -1,6 +1,6 @@
 cask "stoplight-studio" do
-  version "2.2.0,5379.git-75c5189"
-  sha256 "829b41a2d24329837b3bc9335d7dd01c403ce6f871a2c71aadc0fa126f071317"
+  version "2.3.0,5448.git-f63f57f"
+  sha256 "e15eef17424b50379998e561b9d333fc0b99da49814e896596960f798eb105b2"
 
   url "https://github.com/stoplightio/studio/releases/download/v#{version.before_comma}-stable.#{version.after_comma}/stoplight-studio-mac.dmg",
       verified: "github.com/stoplightio/studio/"
