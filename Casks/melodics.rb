8572757c726415b813309a3a3f539647e8070e52
adithyasunil26 <adithyasunil26@gmail.com>
2021-02-13 00:27:47 +0400

    Update melodics from 2.1.5641 to 2.1.5687

--- Casks/melodics.rb
+++ Casks/melodics.rb
@@ -1,6 +1,6 @@
 cask "melodics" do
-  version "2.1.5641"
-  sha256 "b59942835ea96a68c71f531f75dbfd970f5f0fe384e1bac36561f356098049bd"
+  version "2.1.5687"
+  sha256 "aba296b59412d496c8c177114102c8f3a640c293bc909b44af8e7aceddaea9b8"
 
   url "https://web-cdn.melodics.com/download/MelodicsV#{version.major}.dmg"
   name "Melodics"
