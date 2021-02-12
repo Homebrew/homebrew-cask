d95afc7a444bb9f1b104dd1a39e5551496dc852c
Christopher Orr <chris@orr.me.uk>
2021-02-12 01:52:47 +0100

    home-assistant: Replace appcast with livecheck. (#99608)

--- Casks/home-assistant.rb
+++ Casks/home-assistant.rb
@@ -4,10 +4,20 @@ cask "home-assistant" do
 
   url "https://github.com/home-assistant/iOS/releases/download/release%2F#{version.before_comma}%2F#{version.after_comma}/home-assistant-mac.zip",
       verified: "github.com/home-assistant/iOS/"
-  appcast "https://github.com/home-assistant/iOS/releases.atom"
   name "Home Assistant"
   desc "Companion app for Home Assistant home automation software"
   homepage "https://companion.home-assistant.io/"
+
+  # We use the GitHubLatest strategy as Home Assistant also tags pre-releases, and
+  # we also specify a regex since tags are unconventional, e.g. `2021.2.2/2021.55`,
+  # and use a custom block to replace the slash with a comma in the resulting version
+  livecheck do
+    url :url
+    strategy :github_latest do |page|
+      version = page.match(%r{href=".+/tree/(?:mac|release)/([\d.]+)/([\d.]+)"}i)
+      "#{version[1]},#{version[2]}"
+    end
+  end
 
   depends_on macos: ">= :catalina"
 
