1915d1adf8bdfb6a10a0368afd1f13e73ab7048c
Adithya Sunil <adithyasunil26@gmail.com>
2021-02-13 01:49:39 +0400

    Update wowup from 2.1.1 to 2.1.2 (#99851)

--- Casks/wowup.rb
+++ Casks/wowup.rb
@@ -1,6 +1,6 @@
 cask "wowup" do
-  version "2.1.1"
-  sha256 "439c3e2945d1f3dd7d2cbcf3523a1e8da04b68a849fe279975d82681988a1c3c"
+  version "2.1.2"
+  sha256 "d4cd653143a7d2a1578a3c09409ee369e13ca5b63a10db2790b4db08ea7a1736"
 
   url "https://github.com/WowUp/WowUp/releases/download/v#{version}/WowUp-#{version}.dmg",
       verified: "github.com/WowUp/WowUp/"
