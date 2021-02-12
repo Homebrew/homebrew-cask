0658b44260fab7f62d22749091a586cd83fe9536
Adithya Sunil <adithyasunil26@gmail.com>
2021-02-13 01:01:16 +0400

    Update radicle-upstream from 0.1.9 to 0.1.10 (#99823)

--- Casks/radicle-upstream.rb
+++ Casks/radicle-upstream.rb
@@ -1,6 +1,6 @@
 cask "radicle-upstream" do
-  version "0.1.9"
-  sha256 "af00d268ef33c44360796e1c05a3fd09ff4b3a1cf96bd70e759913f558240777"
+  version "0.1.10"
+  sha256 "0253751a452214f97c37b692619949b16d3155193d316277fe5e80e470dc51fb"
 
   url "https://releases.radicle.xyz/radicle-upstream-#{version}.dmg"
   appcast "https://github.com/radicle-dev/radicle-upstream/releases.atom"
