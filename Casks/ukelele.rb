4e56e4a9c15a15b369f2009cea522925c3b73894
Josh Tilles <MerelyAPseudonym@gmail.com>
2021-02-12 13:08:12 -0600

    Update ukelele from 3.5.0,339 to 3.5.1,340 (#99805)

--- Casks/ukelele.rb
+++ Casks/ukelele.rb
@@ -1,6 +1,6 @@
 cask "ukelele" do
-  version "3.5.0,339"
-  sha256 "f7ed179571b0e5b0ca4f3b1ed3415d15ec7b97a543b05c5ca0e5dcee8950ac0f"
+  version "3.5.1,340"
+  sha256 "f512159bfbe7c13c4007e45b4149c7825916e4999ab6d632b2a8541580f92b85"
 
   url "https://software.sil.org/downloads/r/ukelele/Ukelele_#{version.before_comma}.dmg"
   name "Ukelele"
