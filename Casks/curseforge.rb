17d7164ae76829513e61f1d1e5885919aacf2eff
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-12 08:36:47 +0800

    Update curseforge.rb

--- Casks/curseforge.rb
+++ Casks/curseforge.rb
@@ -1,18 +1,11 @@
 cask "curseforge" do
-  version "0.167.4-5"
-  sha256 "491aa4424f6222b0c7dfc183e799b527ae59c189ff989d0e4257bd541f6fafb9"
+  version :latest
+  sha256 :no_check
 
-  url "https://github.com/overwolf/curseforge-mac/releases/download/v#{version}/CurseForge-#{version}.dmg",
-      verified: "github.com/overwolf/curseforge-mac/"
+  url "https://curseforge.overwolf.com/downloads/curseforge-latest.dmg"
   name "CurseForge"
   desc "Download and manage your addons and mods"
   homepage "https://curseforge.overwolf.com/"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-    regex(%r{href=.*?/CurseForge-(\d+(?:\.\d+).*)\.dmg}i)
-  end
 
   app "CurseForge.app"
 
