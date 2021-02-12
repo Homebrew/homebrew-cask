c5b904e40607e8cedfc5e043c589b08b8e990c4b
Sascha Peilicke <sascha@peilicke.de>
2021-02-12 12:57:39 +0100

    Update genymotion from 3.2.0 to 3.2.0

--- Casks/genymotion.rb
+++ Casks/genymotion.rb
@@ -1,6 +1,6 @@
 cask "genymotion" do
-  version "3.1.2"
-  sha256 "6dbed0cd76b11c8b5c01ef2caa08cf8fde4150ba31e3284e7b1e3f736ab244fd"
+  version "3.2.0"
+  sha256 "9025dd0baf74a7d7258570d6d2ca0f45af9f5c8e680080012cac7d42b411e9e3"
 
   url "https://dl.genymotion.com/releases/genymotion-#{version}/genymotion-#{version}.dmg"
   appcast "https://www.genymotion.com/product-release-note/desktop/"
