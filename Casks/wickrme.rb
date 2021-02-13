870a7542e79a2ce4188efa473f66304a09b80932
Peter Lewis <peter@peterlew.is>
2021-02-13 00:35:14 +0000

    Wickr Me - Bump to 5.72.9 and add desc (#99669)
    1. Bumped version to `5.72.9`
    2. Added `desc`

--- Casks/wickrme.rb
+++ Casks/wickrme.rb
@@ -1,11 +1,12 @@
 cask "wickrme" do
-  version "5.70.4"
-  sha256 "4a86b4e563e83fc9ba47f4dd5f00b575443fb682b75244ce97151ad44ca4fa6e"
+  version "5.72.9"
+  sha256 "44195be6ffd22d8b9da54fde5f3983baa90dff4527ad8e87bd3532c32d49f4d1"
 
   url "https://s3.amazonaws.com/static.wickr.com/downloads/mac/me/WickrMe-#{version}.dmg",
       verified: "s3.amazonaws.com/static.wickr.com/"
   appcast "https://me-download.wickr.com/api/multiVerify/me/undefined/"
   name "Wickr Me"
+  desc "Desktop client for Wickr Me"
   homepage "https://wickr.com/products/personal/"
 
   app "WickrMe.app"
