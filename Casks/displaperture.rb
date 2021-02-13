b6b1db24c97c109e3b586467fd93aba9631dcdf4
Fabian <31616408+EinJochel@users.noreply.github.com>
2021-02-13 07:56:34 +0100

    Added appcast, desc, uninstall and zap stanzas to displaperture (#99804)
    * Added appcast, desc, uninstall and zap stanzas to displaperture
    
    * Update displaperture.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/displaperture.rb
+++ Casks/displaperture.rb
@@ -3,8 +3,17 @@ cask "displaperture" do
   sha256 :no_check
 
   url "https://manytricks.com/download/displaperture"
+  appcast "https://manytricks.com/displaperture/appcast/?version=#{version.major}"
   name "Displaperture"
+  desc "Rounds your display corners"
   homepage "https://manytricks.com/displaperture/"
 
   app "Displaperture.app"
+
+  uninstall quit: "com.manytricks.Displaperture"
+
+  zap trash: [
+    "~/Library/Caches/com.manytricks.Displaperture",
+    "~/Library/Preferences/com.manytricks.Displaperture.plist",
+  ]
 end
