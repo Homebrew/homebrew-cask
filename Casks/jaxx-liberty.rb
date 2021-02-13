a8ea72c60a6d0ceecc2b693303e138b01296a3fb
Haodong DU <lovestudykid@gmail.com>
2021-02-13 00:44:22 -0500

    Update jaxx-liberty from 2.4.6 to 2.5.0 (#99856)

--- Casks/jaxx-liberty.rb
+++ Casks/jaxx-liberty.rb
@@ -1,6 +1,6 @@
 cask "jaxx-liberty" do
-  version "2.4.6"
-  sha256 "da93cbc60c72710d4b4cb34897751ca481a0fdbd72c06955a94e364b894c3085"
+  version "2.5.0"
+  sha256 "1100cf1d0b9da5333b2e1a46e46b3ee3fbce35287e1cfc1fc8d78f68d861110a"
 
   url "https://download-liberty.jaxx.io/Jaxx.Liberty-#{version}.dmg"
   appcast "https://download-liberty.jaxx.io/latest-mac.yml"
