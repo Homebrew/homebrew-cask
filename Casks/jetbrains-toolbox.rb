ededac85d1a768736d0956b9d0841d4fa64ba3db
John L. Cooper <plasticle@gmail.com>
2021-02-13 06:47:53 +0000

    add support for installation of Jetbrains Toolbox for Apple Silicon (#99854)

--- Casks/jetbrains-toolbox.rb
+++ Casks/jetbrains-toolbox.rb
@@ -1,8 +1,13 @@
 cask "jetbrains-toolbox" do
   version "1.20.7940"
-  sha256 "9d8b0644b182cf5266836eb0506ac7b03199dc1f45eaf5e0d4c0d48d891ea79e"
 
-  url "https://download.jetbrains.com/toolbox/jetbrains-toolbox-#{version}.dmg"
+  if Hardware::CPU.intel?
+    url "https://download.jetbrains.com/toolbox/jetbrains-toolbox-#{version}.dmg"
+    sha256 "9d8b0644b182cf5266836eb0506ac7b03199dc1f45eaf5e0d4c0d48d891ea79e"
+  else
+    url "https://download.jetbrains.com/toolbox/jetbrains-toolbox-#{version}-arm64.dmg"
+    sha256 "550cc384e497cc3fc54c3e8864c81726696577a8c5c5aef263f9cdb66ebde15a"
+  end
   appcast "https://data.services.jetbrains.com/products/releases?code=TBA&latest=true&type=release"
   name "JetBrains Toolbox"
   desc "JetBrains tools manager"
