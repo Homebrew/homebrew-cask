6268900d947d3a2fb594f6a59f825b49a7329d40
Denis Z <zzulev@gmail.com>
2021-02-13 18:58:26 +0100

    Update spotter from 0.0.4 to 0.0.5 (#99867)

--- Casks/spotter.rb
+++ Casks/spotter.rb
@@ -1,6 +1,6 @@
 cask "spotter" do
-  version "0.0.4"
-  sha256 "71d6894c728b7c8d5e3fd634727dba77908417598a6888f3e8510c70ce1de751"
+  version "0.0.5"
+  sha256 "4103a4acf57462688f8099e52e793ced507ae4dcaa096bc2052a999b4019712e"
 
   url "https://github.com/spotter-application/spotter/releases/download/#{version}/spotter.dmg"
   appcast "https://github.com/spotter-application/spotter/releases.atom"
