85aeb6b022f804b6a488ba0b31d65cf5384f9412
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-02-13 16:45:12 +0900

    sip: update to 2.5.2,252 (#99858)

--- Casks/sip.rb
+++ Casks/sip.rb
@@ -3,8 +3,8 @@ cask "sip" do
     version "1.2"
     sha256 "93569421eef761ccdd2784d73e5f201d29e5e22ad6814a7a169f459f460bf4ff"
   else
-    version "2.5.1,251"
-    sha256 "b9c3dab9ca147e7e39957fe1ec8a81ae2cb9de5d49c25ed7596d2980fdab3441"
+    version "2.5.2,252"
+    sha256 "bf9988c28ec0fb474f4b7a0330f9140f3a07898240ed01800dfc3dfcc77301b2"
   end
 
   url "https://sipapp.io/updates/v#{version.major}/sip-#{version.before_comma}.zip"
