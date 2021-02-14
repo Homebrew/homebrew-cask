a381c0f333b1bc50323511dbe2769fc5d619d89c
Quang Lam <quang.lam@webcatalog.app>
2021-02-14 10:48:41 +0700

    Update webcatalog (#99873)

--- Casks/webcatalog.rb
+++ Casks/webcatalog.rb
@@ -1,6 +1,6 @@
 cask "webcatalog" do
-  version "28.4.0"
-  sha256 "820d8766751853f8c5f7a44ae65f2f17e24671a2a56b9f8c7822c82a0b0b03a2"
+  version "28.5.0"
+  sha256 "ad1c03323041d66b9df70717003709c650555f16eed6cdef879adbf99eb8d23f"
 
   url "https://github.com/webcatalog/webcatalog-app/releases/download/v#{version}/WebCatalog-#{version}.dmg",
       verified: "github.com/webcatalog/webcatalog-app/"
