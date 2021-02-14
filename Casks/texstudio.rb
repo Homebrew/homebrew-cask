73a300a59f24d72e6c72672cce2438a0c625eef5
Ian Griffiths <6thimage@users.noreply.github.com>
2021-02-14 17:54:05 +0000

    Update texstudio from 3.0.4 to 3.0.5

--- Casks/texstudio.rb
+++ Casks/texstudio.rb
@@ -1,6 +1,6 @@
 cask "texstudio" do
-  version "3.0.4"
-  sha256 "ebb836eddc2c0aaa3521e0e9adcc732777f5c371614696df80e7645b041529cd"
+  version "3.0.5"
+  sha256 "ff057284eb39d2d814e5ab37da9160afd9522a53f4bdc97afd87d74a9ebc0bff"
 
   url "https://github.com/texstudio-org/texstudio/releases/download/#{version}/texstudio-#{version}-osx.dmg",
       verified: "github.com/texstudio-org/texstudio/"
