208200c3ca5e1ccd80118e376a662b4cd2a5f5e7
gkertai <gabor@production-minds.com>
2021-02-15 00:25:59 +0100

    Add Fawkes.app v0.3.1

---
+++ Casks/fawkes.rb
@@ -0,0 +1,14 @@
+cask "fawkes" do
+  version "0.3.1"
+  sha256 "7101ecb08e9ce319c66f1e6dae25443a3175917eaa48feeaf17b1864f91ab6f8"
+
+  url "https://mirror.cs.uchicago.edu/fawkes/files/#{version.major_minor}/Fawkes-#{version}.dmg",
+      verified: "sandlab.cs.uchicago.edu/fawkes/"
+  appcast "https://github.com/Shawn-Shan/fawkes/tags.atom",
+          must_contain: version.major_minor
+  name "Fawkes"
+  desc "Cloaks picture files to circumvent facial recognition"
+  homepage "https://sandlab.cs.uchicago.edu/fawkes/"
+
+  app "Fawkes.app"
+end
