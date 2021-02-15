48e89dd508d888b667e342e69e93e24b1fc8eb10
Kramer Campbell <kramer@kramerc.com>
2021-02-15 12:11:42 -0800

    Update checksum of dbeaver-community 7.3.5 (#99914)

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,6 +1,6 @@
 cask "dbeaver-community" do
   version "7.3.5"
-  sha256 "953e96c1794f5b58dc10045cba799817136590ab66b88657a6f188d314d5a70b"
+  sha256 "12d7cd6167a411f57ab9ece5f68dbbf0a0c689058836bb52fd0d140e5970217b"
 
   url "https://dbeaver.io/files/#{version}/dbeaver-ce-#{version}-macos.dmg"
   name "DBeaver Community Edition"
