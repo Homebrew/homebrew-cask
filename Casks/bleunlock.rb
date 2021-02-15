a4032413ae6bce1914664aa139c71aabae3f3fbb
Joey Hoer <me@joeyhoer.com>
2021-02-15 13:59:52 -0500

    Updating BLEUnlock to correct appcast URL

--- Casks/bleunlock.rb
+++ Casks/bleunlock.rb
@@ -3,9 +3,9 @@ cask "bleunlock" do
   sha256 "60ccc7dbe86521b29c0f1ac605db69a90713765abc1532975b61473258beffc1"
 
   url "https://github.com/ts1/BLEUnlock/releases/download/#{version}/BLEUnlock-#{version}.zip"
-  appcast "https://github.com/fitztrev/shuttle/releases.atom"
+  appcast "https://github.com/ts1/BLEUnlock/releases.atom"
   name "BLEUnlock"
-  desc "Lock and unlock a computer with a bluetooth low energy device"
+  desc "Lock/unlock Apple computers using the proximity of a bluetooth low energy device"
   homepage "https://github.com/ts1/BLEUnlock"
 
   app "BLEUnlock.app"
