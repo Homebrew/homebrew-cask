965b4ae8225c37a04e84f9e2e3fa4ef97c66e00f
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-16 10:58:43 +0800

    Update aural.rb

--- Casks/aural.rb
+++ Casks/aural.rb
@@ -1,6 +1,6 @@
 cask "aural" do
   version "2.3.0"
-  sha256 "2386432add225a54d8eacb55fd6a3be39f3efc13a3ff8de1fae5baf401eaef9e"
+  sha256 "0ac47febc70d9e2609322f8b93259e012fd169ca60eeb1f8600b8808c0424030"
 
   url "https://github.com/maculateConception/aural-player/releases/download/#{version}/AuralPlayer-#{version}.dmg"
   appcast "https://github.com/maculateConception/aural-player/releases.atom"
