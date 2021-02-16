c2f9b416333bb85515b7035b09e1d4647be7e4c0
Giovanni Merlino <gmerlino@unime.it>
2021-02-16 04:01:48 +0100

    Update mechvibes (#99886)

--- Casks/mechvibes.rb
+++ Casks/mechvibes.rb
@@ -1,10 +1,10 @@
 cask "mechvibes" do
-  version "2.2.0"
-  sha256 "3da58cac995893c9f42f1ebc59b0b03d59681ded3482a15639658ddfdf2419d5"
+  version "2.3.0"
+  sha256 "83c70ac29de277024567f94f1e0a2aa2679e20ce7cdc027b02f06577442dfcf8"
 
-  url "https://downloads.sourceforge.net/mechvibes/v#{version}/Mechvibes-#{version}.dmg",
-      verified: "downloads.sourceforge.net/mechvibes/"
-  appcast "https://sourceforge.net/projects/mechvibes/rss"
+  url "https://github.com/hainguyents13/mechvibes/releases/download/v#{version}/Mechvibes-#{version}.dmg",
+      verified: "github.com/hainguyents13/mechvibes/"
+  appcast "https://github.com/hainguyents13/mechvibes/releases.atom"
   name "Mechvibes"
   desc "Play mechanical keyboard sounds as you type"
   homepage "https://mechvibes.com/"
