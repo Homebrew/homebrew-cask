1aa09885c0bb37a5b4043db7dee0b09a59ecdddf
miccal <miccal.matthews@gmail.com>
2021-02-16 11:04:14 +0800

    Update tiny-player from 1.2.13,148 to 1.3.2,155

--- Casks/tiny-player.rb
+++ Casks/tiny-player.rb
@@ -1,6 +1,6 @@
 cask "tiny-player" do
-  version "1.2.13,148"
-  sha256 "b4fdc7374fd7fbb28e09c1834dc88379fc939a27a1f368b6230871f0ad795836"
+  version "1.3.2,155"
+  sha256 "29c3f4675a91a95427c9bddbd856cd90b3ed02ceda2ac5fa776f7e697bb2ebc0"
 
   url "https://download.catnapgames.com/TinyPlayer-#{version.before_comma}.zip"
   name "Tiny Player for Mac"
