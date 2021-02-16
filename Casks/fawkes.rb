42fefe2c65b82afc2f4257eeacc45f4d010c1b73
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-16 11:00:01 +0800

    Update fawkes.rb

--- Casks/fawkes.rb
+++ Casks/fawkes.rb
@@ -3,8 +3,8 @@ cask "fawkes" do
   sha256 "7101ecb08e9ce319c66f1e6dae25443a3175917eaa48feeaf17b1864f91ab6f8"
 
   url "https://mirror.cs.uchicago.edu/fawkes/files/#{version.major_minor}/Fawkes-#{version}.dmg",
-      verified: "sandlab.cs.uchicago.edu/fawkes/"
-  appcast "https://github.com/Shawn-Shan/fawkes/tags.atom",
+      verified: "mirror.cs.uchicago.edu/fawkes/"
+  appcast "https://github.com/Shawn-Shan/fawkes/releases.atom",
           must_contain: version.major_minor
   name "Fawkes"
   desc "Cloaks picture files to circumvent facial recognition"
