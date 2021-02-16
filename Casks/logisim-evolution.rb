3fa6c037e5426606ded6b37d9cf9e1b2d12e38d5
meow10811 <10765599+meow10811@users.noreply.github.com>
2021-02-16 17:05:31 -0500

    Changed cask to use .dmg

--- Casks/logisim-evolution.rb
+++ Casks/logisim-evolution.rb
@@ -1,20 +1,21 @@
 cask "logisim-evolution" do
   version "3.4.1"
-  sha256 "1b72dd3397290b7be95f886b933bd37a2d9182b8d33247fa5f059d0867e8111c"
+  sha256 "253290b94ae1dba6d5528ff2ea478c2750cee4a40c4710f7da6006dc3f0b4b52"
 
-  url "https://github.com/reds-heig/logisim-evolution/releases/download/v#{version}/logisim-evolution-#{version}-all.jar"
+  url "https://github.com/reds-heig/logisim-evolution/releases/download/v#{version}/logisim-evolution-#{version}.dmg"
   appcast "https://github.com/reds-heig/logisim-evolution/releases.atom"
   name "Logisim Evolution"
   desc "Digital logic designer and simulator"
   homepage "https://github.com/reds-heig/logisim-evolution"
 
-  container type: :naked
+  app "logisim-evolution.app"
 
-  app "logisim-evolution-#{version}-all.jar", target: "logisim-evolution.jar"
+  postflight do
+    # Fix xattr
+    system_command "/usr/bin/xattr",
+                   args: ["-cr", "#{appdir}/logisim-evolution.app"],
+                   sudo: true
+  end
 
   zap trash: "~/Library/Preferences/com.cburch.logisim.plist"
-
-  caveats do
-    depends_on_java "9+"
-  end
 end
