22f9ac01f762aa98ebbf4d0f1169df67ee736ea0
Florian Albrecht <flo@kaleidoscope.app>
2021-02-16 17:29:29 +0100

    Update Kaleidoscope to version 2.4

--- Casks/kaleidoscope.rb
+++ Casks/kaleidoscope.rb
@@ -1,16 +1,16 @@
 cask "kaleidoscope" do
-  version "2.3.6,1447-jan-18-2021"
+  version "2.4,1450-feb-4-2021"
   sha256 "81afa75197f5190d15c1bb41fa8c883e71538bdbe7734d156aa9f0952f530677"
 
-  url "https://updates.kaleidoscope.app/v2/prod/Kaleidoscope-#{version.before_comma}-build-#{version.after_comma}.app.zip",
+  url "https://updates.kaleidoscope.app/v2/prod/Kaleidoscope-#{version.before_comma}-#{version.after_comma}.app.zip",
       verified: "updates.kaleidoscope.app/"
   appcast "https://updates.kaleidoscope.app/v2/prod/appcast"
   name "Kaleidoscope"
-  desc "Spots differences in text and image files"
-  homepage "https://www.kaleidoscopeapp.com/"
+  desc "Spots and merge differences in text and image files or folders."
+  homepage "https://www.kaleidoscope.app"
 
   auto_updates true
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :high_sierra"
 
   app "Kaleidoscope.app"
   binary "#{appdir}/Kaleidoscope.app/Contents/Resources/bin/ksdiff"
