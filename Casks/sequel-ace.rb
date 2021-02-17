2392ffd7dacb29326ca926877fc9c068e9536241
Mario Fehr <mario.fehr@massiveart.com>
2021-02-17 12:34:39 +0100

    Update sequel-ace from 3.2.0,3019 to 3.2.1,3020 (#99970)

--- Casks/sequel-ace.rb
+++ Casks/sequel-ace.rb
@@ -1,6 +1,6 @@
 cask "sequel-ace" do
-  version "3.2.0,3019"
-  sha256 "f49d60a9bcf69a414141eb95e12c8caeabc4cce0328026fa2e8bad55832aebbd"
+  version "3.2.1,3020"
+  sha256 "f8e834b3e2fe91d548c6857474cf9b078909e7319989deebc3f96a004312d0fd"
 
   url "https://github.com/Sequel-Ace/Sequel-Ace/releases/download/production/#{version.before_comma}-#{version.after_comma}/Sequel-Ace-#{version.before_comma}.zip"
   appcast "https://github.com/Sequel-Ace/Sequel-Ace/releases.atom"
