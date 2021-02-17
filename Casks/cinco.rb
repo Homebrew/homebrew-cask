052f413c5d3715f337a58c6e1987c8207372e2bf
Fabian <31616408+EinJochel@users.noreply.github.com>
2021-02-17 13:14:49 +0100

    Added cask cinco

---
+++ Casks/cinco.rb
@@ -0,0 +1,26 @@
+cask "cinco" do
+  version "1.1"
+  sha256 "0e2ada95f3735cc123950f7ad999f4aa954f48dc82f6c379dda6f06b9a9e05d2"
+
+  url "https://ls5download.cs.tu-dortmund.de/cinco/releases/#{version}/cinco-#{version}-macosx.cocoa.x86_64.zip",
+      verified: "ls5download.cs.tu-dortmund.de/cinco/releases/"
+  name "Cinco"
+  desc "Generator-driven Eclipse IDE for domain-specific graphical modeling tools"
+  homepage "https://cinco.scce.info/"
+
+  # Renamed for consistency: App name in the Finder is different from its branding
+  app "cinco-#{version}.app", target: "Cinco.app"
+
+  uninstall quit: "de.jabc.cinco.meta.product.product"
+
+  zap trash: [
+    "~/Library/Preferences/de.jabc.cinco.meta.product.product.plist",
+    "~/Library/Saved Application State/de.jabc.cinco.meta.product.product.savedState",
+  ]
+
+  caveats "Gatekeeper:\n  To launch #{name} for the first time, right click it in Finder and choose\n  \"Open\"."
+  caveats do
+    license "https://www.eclipse.org/legal/epl-v10.html"
+    depends_on_java "8"
+  end
+end
