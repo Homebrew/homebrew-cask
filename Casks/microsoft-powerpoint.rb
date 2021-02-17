eec4770dbc45067dd6d8e1236f461310b8f586b0
Reno <singularitti@outlook.com>
2021-02-16 23:39:53 -0500

    Update microsoft-powerpoint from 16.45.21011103 to 16.46.21021202 (#99958)

--- Casks/microsoft-powerpoint.rb
+++ Casks/microsoft-powerpoint.rb
@@ -1,6 +1,6 @@
 cask "microsoft-powerpoint" do
-  version "16.45.21011103"
-  sha256 "400e60f5d7e0e2b8c388607ddfe85bf53aad0db83ed526dd18c5ed6a1f9f00e3"
+  version "16.46.21021202"
+  sha256 "03fd150e79f1c2dc485b0369658df4f60f733085c2b582abcba3ed88fda11ba6"
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_PowerPoint_#{version}_Installer.pkg",
       verified: "officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/"
