c4201b690647cc7fa26acbc1759ff1429c7868c1
Adithya Sunil <adithyasunil26@gmail.com>
2021-02-17 22:14:38 +0400

    Update astropad-studio from 3.5.0,3180 to 3.6.0,3204 (#99987)

--- Casks/astropad-studio.rb
+++ Casks/astropad-studio.rb
@@ -1,6 +1,6 @@
 cask "astropad-studio" do
-  version "3.5.0,3180"
-  sha256 "be952da8a7089186ae413a4b1dd173ae473ab572cd611d5a0a38b062b58082ff"
+  version "3.6.0,3204"
+  sha256 "4819188b06497bc9f9353baf891eae73f9434718ccae21e812fa8ffd41ef0dd5"
 
   url "https://s3.amazonaws.com/astropad.com/downloads/AstropadStudio-#{version.before_comma}.dmg",
       verified: "s3.amazonaws.com/astropad.com/"
