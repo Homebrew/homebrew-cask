6c48830cef3ee495210242c5af04f3b8bfaf8af6
Adithya Sunil <adithyasunil26@gmail.com>
2021-02-17 21:35:04 +0400

    Update anydo from 4.2.139 to 4.2.140 (#99984)

--- Casks/anydo.rb
+++ Casks/anydo.rb
@@ -1,6 +1,6 @@
 cask "anydo" do
-  version "4.2.139"
-  sha256 "a7bb23edbc0b0bc90fa00fd75d5404628ed36e4460f57fe0bc2a886cbba4f236"
+  version "4.2.140"
+  sha256 "1bb1834c3a00f977062e63795a00f9a3631dafcf15786f82cee6787e8c9ffe53"
 
   url "https://electron-app.any.do/Any.do-#{version}.dmg"
   name "Any.do"
