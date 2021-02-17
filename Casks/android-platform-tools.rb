537639c37115fc84ac9090979681430975c87dc3
Youfu Zhang <zhangyoufu@gmail.com>
2021-02-17 15:49:51 +0800

    add more binary for android-platform-tools

--- Casks/android-platform-tools.rb
+++ Casks/android-platform-tools.rb
@@ -10,8 +10,12 @@ cask "android-platform-tools" do
 
   binary "#{staged_path}/platform-tools/adb"
   binary "#{staged_path}/platform-tools/dmtracedump"
+  binary "#{staged_path}/platform-tools/e2fsdroid"
   binary "#{staged_path}/platform-tools/etc1tool"
   binary "#{staged_path}/platform-tools/fastboot"
   binary "#{staged_path}/platform-tools/hprof-conv"
+  binary "#{staged_path}/platform-tools/make_f2fs"
+  binary "#{staged_path}/platform-tools/make_f2fs_casefold"
   binary "#{staged_path}/platform-tools/mke2fs"
+  binary "#{staged_path}/platform-tools/sload_f2fs"
 end
