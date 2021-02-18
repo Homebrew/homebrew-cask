7881023ba1529813907112c0af7dacd5d273f956
LanikSJ <laniksj@gmail.com>
2021-02-18 13:37:56 -0800

    Add Kiwi for Gmail.app v3.2.0

---
+++ Casks/kiwi-for-gmail.rb
@@ -0,0 +1,11 @@
+cask "kiwi-for-gmail" do
+  version "3.2.0,3213"
+  sha256 :no_check
+
+  url "https://downloads.kiwiforgmail.com/kiwi/release/consumer/Kiwi+for+Gmail+Setup.dmg"
+  name "Kiwi for Gmail"
+  desc "Enhances Gmail seamlessly like a full-featured desktop office productivity app"
+  homepage "https://www.kiwiforgmail.com/"
+
+  app "Kiwi for Gmail.app"
+end
