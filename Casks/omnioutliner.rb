adc47e7c01e2cc739c286ed23f250d4472afb8e6
adithyasunil26 <adithyasunil26@gmail.com>
2021-02-19 00:08:09 +0400

    Update omnioutliner from 5.8.1 to 5.8.2

--- Casks/omnioutliner.rb
+++ Casks/omnioutliner.rb
@@ -4,8 +4,8 @@ cask "omnioutliner" do
     sha256 "a9364dcf2ee97a871a881530785fa54d269f5e95298e2e4d2e979c70b6365395"
     url "https://downloads.omnigroup.com/software/MacOSX/10.13/OmniOutliner-#{version}.dmg"
   else
-    version "5.8.1"
-    sha256 "302e27604ab9ed0c9695eb1d9135ed5145e0bef2a9bbdab973ad4e5e0ae593b7"
+    version "5.8.2"
+    sha256 "936bf053709596ccb19ad7fe4abf388956ae5acace7cbfbbc6305f4768c73f96"
     url "https://downloads.omnigroup.com/software/MacOSX/10.14/OmniOutliner-#{version}.dmg"
   end
 
