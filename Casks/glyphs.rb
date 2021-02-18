7f09602a6608c55a8cc589cda009050497be9181
Adithya Sunil <adithyasunil26@gmail.com>
2021-02-18 04:33:54 +0400

    Update glyphs from 3.0.3,3062 to 3.0.3,3065 (#99999)

--- Casks/glyphs.rb
+++ Casks/glyphs.rb
@@ -1,6 +1,6 @@
 cask "glyphs" do
-  version "3.0.3,3062"
-  sha256 "bd19cfaa4a657a55be7f79ee4c113a159ddcd58091979d8d62ed83e7740b7524"
+  version "3.0.3,3065"
+  sha256 "c1c665703d4eb2980a0b15b6044825faac544892994cc46fa6a60711de2c6437"
 
   url "https://updates.glyphsapp.com/Glyphs#{version.before_comma}-#{version.after_comma}.zip"
   name "Glyphs"
