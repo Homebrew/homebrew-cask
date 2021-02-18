c2821f754af3b1300510d6bd5e53f4fe8a4a02ce
tiiiecherle <tiiiecherle@gmail.com>
2021-02-18 09:05:14 +0100

    Update eaglefiler from 1.9.2 to 1.9.3 (#100022)

--- Casks/eaglefiler.rb
+++ Casks/eaglefiler.rb
@@ -1,6 +1,6 @@
 cask "eaglefiler" do
-  version "1.9.2"
-  sha256 "79b864922d251186944a6fa147193b1ab4cbafc740bd204d9e826735380318c6"
+  version "1.9.3"
+  sha256 "5c265098119bfb7c03ca5c62c3b9e16a8019c126eaf8b7948f64546bf3e3bf66"
 
   url "https://c-command.com/downloads/EagleFiler-#{version}.dmg"
   appcast "https://c-command.com/eaglefiler/help/version-history"
