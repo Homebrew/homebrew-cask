3fd066d445a6d2de6d7030b467bdc7d02e0eea61
tiiiecherle <tiiiecherle@gmail.com>
2021-02-18 12:30:20 +0100

    add livecheck for eaglefiler

--- Casks/eaglefiler.rb
+++ Casks/eaglefiler.rb
@@ -3,9 +3,15 @@ cask "eaglefiler" do
   sha256 "5c265098119bfb7c03ca5c62c3b9e16a8019c126eaf8b7948f64546bf3e3bf66"
 
   url "https://c-command.com/downloads/EagleFiler-#{version}.dmg"
-  appcast "https://c-command.com/eaglefiler/help/version-history"
   name "EagleFiler"
+  desc "Organize files, archive e-mails, save Web pages and notes, search everything"
   homepage "https://c-command.com/eaglefiler/"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(%r{href=.*?/EagleFiler-(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "EagleFiler.app"
 end
