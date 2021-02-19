aefefc5bec070daa86ae47cb04ba8c9d201c5a8b
Ben Ramsey <ben@benramsey.com>
2021-02-18 20:12:06 -0600

    Add TextBuddy.app v1 (#100070)
    * Add TextBuddy.app v1
    
    * Update textbuddy.rb
    
    * Update textbuddy.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/textbuddy.rb
@@ -0,0 +1,26 @@
+cask "textbuddy" do
+  version "1.0.2"
+  sha256 :no_check
+
+  url "https://textbuddy.app/download-v1/"
+  name "TextBuddy"
+  desc "Convert, filter, sort, and transform text"
+  homepage "https://textbuddy.app/"
+
+  livecheck do
+    url "https://shine.clickontyler.com/appcast.php?id=58"
+    strategy :page_match
+    regex(/TextBuddy\s(\d+(?:\.\d+)*)/i)
+  end
+
+  auto_updates true
+  depends_on macos: ">= :big_sur"
+
+  app "TextBuddy.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.clickontyler.TextBuddy",
+    "~/Library/Caches/com.clickontyler.TextBuddy",
+    "~/Library/Preferences/com.clickontyler.TextBuddy.plist",
+  ]
+end
