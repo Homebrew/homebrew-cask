bc6bcdc4b7204545d28fe726e3c53f155892e2e8
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-19 09:58:26 +0800

    Update textbuddy.rb

--- Casks/textbuddy.rb
+++ Casks/textbuddy.rb
@@ -1,11 +1,17 @@
 cask "textbuddy" do
-  version "1"
-  sha256 :no_check
+  version "1.0.2"
+  sha256 "db5d71aa4ae383687561f8a77667f60a855b954fb1a1983ff27dbac3d5c22140"
 
-  url "https://textbuddy.app/download-v1/"
+  url "https://download.clickontyler.com/textbuddy/textbuddy_#{version.major_minor.no_dots}.zip"
   name "TextBuddy"
   desc "Convert, filter, sort, and transform text"
   homepage "https://textbuddy.app/"
+
+  livecheck do
+    url "https://shine.clickontyler.com/appcast.php?id=58"
+    strategy :page_match
+    regex(/TextBuddy.(\d+(?:\.\d+)*)/i)
+  end
 
   auto_updates true
   depends_on macos: ">= :big_sur"
