e4ce4f2381a219066f31c3e11e209022af42f23b
Reno <singularitti@outlook.com>
2021-02-18 23:54:46 -0500

    Update mathinspector from 0.9.1 to 0.9.3 (#100078)

--- Casks/mathinspector.rb
+++ Casks/mathinspector.rb
@@ -1,6 +1,6 @@
 cask "mathinspector" do
-  version "0.9.1"
-  sha256 "b08c176a7fc7b905ec3188d2b76b17f647807ec61390e3494373301853fd045d"
+  version "0.9.3"
+  sha256 "833dbe2f362fa0df7c22939d05a71e91316dcf1d9e544414eb2985bced70a7e9"
 
   url "https://mathinspector.com/download/mathinspector_#{version}.dmg"
   name "Math Inspector"
