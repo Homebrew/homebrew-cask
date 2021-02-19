1a8929c718c343adfa887edc9dfd713b2f06ee15
Scott Humphries <sscotth@users.noreply.github.com>
2021-02-19 18:51:06 +0000

    Update kodi from 18.9-Leia to 19.0-Matrix (#100095)

--- Casks/kodi.rb
+++ Casks/kodi.rb
@@ -1,6 +1,6 @@
 cask "kodi" do
-  version "18.9-Leia"
-  sha256 "eb97807ff7d361512779368595df4af7394163817f3039c9e27ac829e83446c5"
+  version "19.0-Matrix"
+  sha256 "4654b0f5a4d391f1a3781c4d122ec78d8457284e21a38a4739e85f8296840579"
 
   url "https://mirrors.kodi.tv/releases/osx/x86_64/kodi-#{version}-x86_64.dmg"
   name "Kodi"
