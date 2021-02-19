3a369445a65ca1c59e4eca6e70fa1151f02f4209
Marc Schwede <9077622+schwedenmut@users.noreply.github.com>
2021-02-19 13:21:15 +0100

    Update lens from 4.1.1 to 4.1.2 (#100084)

--- Casks/lens.rb
+++ Casks/lens.rb
@@ -1,6 +1,6 @@
 cask "lens" do
-  version "4.1.1"
-  sha256 "adc4186b93691748901007c9090c611028a5a9732f4931c80e1ed0450fc927e7"
+  version "4.1.2"
+  sha256 "64042367f2da95b1c218b2f82a04d5e26c6f88ee04b60bb7daa868b8f57e31ff"
 
   url "https://github.com/lensapp/lens/releases/download/v#{version}/Lens-#{version}.dmg",
       verified: "github.com/lensapp/lens/"
