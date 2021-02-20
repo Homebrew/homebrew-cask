13cf5d374c037cb612da77751a905375d1e00729
Eric Knibbe <enk3@outlook.com>
2021-02-20 12:14:48 -0500

    Update 0-ad to 0.0.24-alpha

--- Casks/0-ad.rb
+++ Casks/0-ad.rb
@@ -1,6 +1,13 @@
 cask "0-ad" do
-  version "0.0.23b-alpha"
-  sha256 "5fae1331bb1778bf16e49460fb850270b6cd76bee6483078ff27582e71ab263f"
+  if MacOS.version <= :el_capitan
+    version "0.0.23b-alpha"
+    sha256 "5fae1331bb1778bf16e49460fb850270b6cd76bee6483078ff27582e71ab263f"
+    app "0 A.D.app"
+  else
+    version "0.0.24-alpha"
+    sha256 "a7164b407f5b20747dd4eba97a38b3c8d2f0af72bf92829bf0f1b0b27a0b36f7"
+    app "0 A.D..app"
+  end
 
   url "https://releases.wildfiregames.com/0ad-#{version}-osx64.dmg",
       verified: "releases.wildfiregames.com/"
@@ -12,6 +19,4 @@ cask "0-ad" do
     url "https://play0ad.com/download/mac/"
     regex(/href=.*?0ad[._-]v?(.*?)-osx64\.dmg/i)
   end
-
-  app "0 A.D.app"
 end
