d5bb435332c038ec16bdbfcbb439def432636f68
violet jane evans <47541235+virtual-violet@users.noreply.github.com>
2021-02-20 16:38:42 -0700

    Update twine from 2.3.12 to 2.3.13 (#100118)

--- Casks/twine.rb
+++ Casks/twine.rb
@@ -1,6 +1,6 @@
 cask "twine" do
-  version "2.3.12"
-  sha256 "0cd745968ba7db2c4119396022ede038e60a1f237dd9380168ef20f42a637ce4"
+  version "2.3.13"
+  sha256 "4f6f49b938a196181d87d6ab0dc96a5fc02a479b5e698ddaf216840e7d7af48a"
 
   url "https://github.com/klembot/twinejs/releases/download/#{version}/twine_#{version}_macos.dmg",
       verified: "github.com/klembot/twinejs/"
