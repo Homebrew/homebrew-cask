e497d5f3165d0058c7f66da8fd41d5d66ed95ab8
nuomi1 <nuomi1@qq.com>
2021-02-21 03:04:25 +0800

    Update 115browser to 23.10.1.5 (#100112)

--- Casks/115browser.rb
+++ Casks/115browser.rb
@@ -1,12 +1,18 @@
 cask "115browser" do
-  version "23.9.3.6"
-  sha256 "e9e2a9d18603453e18f70f4afabd37d0deb05975e3fcdfb5b364a98efcf20173"
+  version "23.10.1.5"
+  sha256 "5d544629d12264649351dd20f74930346470017f781d8be4febe21373b46eb5f"
 
   url "https://down.115.com/client/mac/115pc_#{version}.dmg"
-  appcast "https://appversion.115.com/1/web/1.0/api/chrome?callback=get_version"
   name "115Browser"
   name "115浏览器"
+  desc "Web browser"
   homepage "https://pc.115.com/"
+
+  livecheck do
+    url "https://appversion.115.com/1/web/1.0/api/chrome?callback=get_version"
+    strategy :page_match
+    regex(/115pc_(\d+(\.\d+)*).dmg/i)
+  end
 
   app "115Browser.app"
 
