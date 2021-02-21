24e77082485d03b81c0d7543f3cc6de088b8d727
Adithya Sunil <adithyasunil26@gmail.com>
2021-02-22 01:15:14 +0400

    Update microsoft-auto-update from 4.31.21011103 to 4.32.21021202 (#100152)

--- Casks/microsoft-auto-update.rb
+++ Casks/microsoft-auto-update.rb
@@ -1,6 +1,6 @@
 cask "microsoft-auto-update" do
-  version "4.31.21011103"
-  sha256 "209b444d2831b879bb73c92041a1c7318c1d5ba58b4689f6c4fc842b0930b532"
+  version "4.32.21021202"
+  sha256 "ccb758531b295848620c703957fd8cf3c7f1555c334fb320c4bb682aba0261d6"
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_AutoUpdate_#{version}_Updater.pkg",
       verified: "officecdn-microsoft-com.akamaized.net/"
