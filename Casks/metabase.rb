0647b89b857215ce0bba867ff8b6f308124ad083
Adithya Sunil <adithyasunil26@gmail.com>
2021-02-22 01:15:44 +0400

    Update metabase from 0.38.0 to 0.38.0.1 (#100151)

--- Casks/metabase.rb
+++ Casks/metabase.rb
@@ -1,6 +1,6 @@
 cask "metabase" do
-  version "0.38.0"
-  sha256 "2e0b6a8557be56e066a26e9caf99b27127bc689832adbdc6e2e9de41f6d7b238"
+  version "0.38.0.1"
+  sha256 "e07521911985a87a5cb09b841c649f06b8667c1f7920b008b67f7a20f96a4ca8"
 
   url "https://s3.amazonaws.com/downloads.metabase.com/v#{version}/Metabase.zip",
       verified: "s3.amazonaws.com/downloads.metabase.com/"
