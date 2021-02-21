2953b31f41165dfb9430f79287e91cf6fcbe8d4f
acheronfail <acheronfail@gmail.com>
2021-02-21 12:13:20 +1100

    Update pixel-picker from 1.5.0 to v1.5.0

--- Casks/pixel-picker.rb
+++ Casks/pixel-picker.rb
@@ -1,5 +1,5 @@
 cask "pixel-picker" do
-  version "1.5.0"
+  version "v1.5.0"
   sha256 "61ed0cb9509b18a000491c9a9052296e1fd0789d95a5499768688bb941f87fc3"
 
   url "https://github.com/acheronfail/pixel-picker/releases/download/#{version}/Pixel.Picker.#{version}.dmg"
