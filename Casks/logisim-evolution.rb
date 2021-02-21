d6d1fcf4ff7e417826a7270fbc6f6600b143cb99
adithyasunil26 <adithyasunil26@gmail.com>
2021-02-22 01:27:27 +0400

    Update logisim-evolution from 3.4.1 to 3.4.2

--- Casks/logisim-evolution.rb
+++ Casks/logisim-evolution.rb
@@ -1,6 +1,6 @@
 cask "logisim-evolution" do
-  version "3.4.1"
-  sha256 "1b72dd3397290b7be95f886b933bd37a2d9182b8d33247fa5f059d0867e8111c"
+  version "3.4.2"
+  sha256 "60a8419119fc527c28ee9b383fc982772e4c7036dd515f75471153e87d7e7f76"
 
   url "https://github.com/reds-heig/logisim-evolution/releases/download/v#{version}/logisim-evolution-#{version}-all.jar"
   appcast "https://github.com/reds-heig/logisim-evolution/releases.atom"
