111d65b37bc38422ffbdd7b4ef0129a9c971d200
Michele Gerarduzzi <hello@michelegera.dev>
2021-02-22 14:07:30 +0100

    Add Pika.app v0.0.9 (#100134)
    * Add Pika.app v0.0.9
    
    * Update pika.rb
    
    * Add zap to pika.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

---
+++ Casks/pika.rb
@@ -0,0 +1,18 @@
+cask "pika" do
+  version "0.0.9"
+  sha256 "17deb7dec50803e625705f29b40bb0f1bae6881bf57dc0ea7ad255858296fd16"
+
+  url "https://github.com/superhighfives/pika/releases/download/#{version}/Pika-#{version}.dmg",
+      verified: "github.com/superhighfives/pika"
+  name "Pika"
+  desc "Colour picker for colours onscreen"
+  homepage "https://superhighfives.com/pika"
+
+  app "Pika.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.superhighfives.Pika-LaunchAtLoginHelper",
+    "~/Library/Containers/com.superhighfives.Pika-LaunchAtLoginHelper",
+    "~/Library/Preferences/com.superhighfives.Pika.plist",
+  ]
+end
