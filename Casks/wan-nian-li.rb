b067c78e21bf90cf3cf965e71412914a225f3b12
asins <asinsimple@alibaba-inc.com>
2021-02-22 14:58:51 +0800

    Fix reference

--- Casks/wan-nian-li.rb
+++ Casks/wan-nian-li.rb
@@ -1,18 +1,16 @@
-cask "wan-nian-li" do
-  version :latest
-  sha256 :no_check
+cask "wannianli" do
+  version "2019-12-06"
+  sha256 "702298f34ca2576a02388a4103fc0e09a4aae753df21ca2e012f6f87497db6e9"
 
   url "https://calendar.zfdang.com/WanNianLi.app.zip",
-      verified: "calendar.zfdang.com"
+      verified: "https://calendar.zfdang.com"
   name "WanNianLi"
-  desc "Mac下的万年历状态栏小工具"
+  desc "Chinese Lunar Calendar Status Bar Widget"
   homepage "https://github.com/zfdang/chinese-lunar-calendar-for-mac/"
 
   depends_on macos: ">= :el_capitan"
 
   app "WanNianLi.app"
 
-  zap trash: [
-    "~/Library/Application Support/com.zfdang.calendar",
-  ]
+  zap trash: "~/Library/Application Support/com.zfdang.calendar"
 end
