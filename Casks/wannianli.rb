279a0fa212decb1cecd1882ac8dc52d224402aa3
asins <asinsimple@alibaba-inc.com>
2021-02-22 15:40:08 +0800

    Fix download url change to github

--- Casks/wannianli.rb
+++ Casks/wannianli.rb
@@ -2,7 +2,7 @@ cask "wannianli" do
   version "2019-12-06"
   sha256 "702298f34ca2576a02388a4103fc0e09a4aae753df21ca2e012f6f87497db6e9"
 
-  url "https://calendar.zfdang.com/WanNianLi.app.zip",
+  url "https://github.com/zfdang/chinese-lunar-calendar-for-mac/releases/download/2019-12-06/WanNianLi.app-v#{version.no_hyphens}.zip",
       verified: "https://calendar.zfdang.com"
   name "WanNianLi"
   desc "Chinese Lunar Calendar Status Bar Widget"
