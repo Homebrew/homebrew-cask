1a9df72f3f991a6f3e0823721f69ee9e0178f45c
Matthew Thompson <matthew.thompson@nasa.gov>
2021-02-22 15:48:14 -0500

    Update panoply from 4.12.2 to 4.12.3

--- Casks/panoply.rb
+++ Casks/panoply.rb
@@ -1,6 +1,6 @@
 cask "panoply" do
-  version "4.12.2"
-  sha256 "e4c192c9734b376ed1ca6a749347d8a4169ea88629f13bb9b1280d9b4ad4f8d3"
+  version "4.12.3"
+  sha256 "af291a4a8c163051433be7e666e9b82661d466a7768498cdbdd9fa8ce660d9e6"
 
   url "https://www.giss.nasa.gov/tools/panoply/download/PanoplyMacOS-#{version}.dmg"
   appcast "https://www.giss.nasa.gov/tools/panoply/download/"
