4efef306647d3a97d00c4b131eef113159dc54e1
LanikSJ <LanikSJ@users.noreply.github.com>
2021-02-22 14:27:03 -0800

    Update google-chrome from 88.0.4324.182,4324.182 to 88.0.4324.192,4324.192 (#100246)

--- Casks/google-chrome.rb
+++ Casks/google-chrome.rb
@@ -1,5 +1,5 @@
 cask "google-chrome" do
-  version "88.0.4324.182,4324.182"
+  version "88.0.4324.192,4324.192"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
