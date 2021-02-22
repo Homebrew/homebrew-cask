362502ff86fb16abe487e1edb2a562d5b49d8ca7
Steve Abatangle <sabat@area51.org>
2021-02-22 15:18:18 -0800

    update Chef Workstation cask to current 21.2.292

--- Casks/chef-workstation.rb
+++ Casks/chef-workstation.rb
@@ -1,8 +1,8 @@
 cask "chef-workstation" do
-  version "21.2.278"
-  sha256 "b4c72ddc844aefab234758a2ae48be6b6339239515c3d7f070539a6dc24a8f8b"
+  version "21.2.292"
+  sha256 "983ee9a08e25fc7e4f0ad3c58b622436aa3831ff1cda4c6e1aefd6720dd41a26"
 
-  url "https://packages.chef.io/files/stable/chef-workstation/#{version}/mac_os_x/10.15/chef-workstation-#{version}-1.dmg"
+  url "https://packages.chef.io/files/stable/chef-workstation/#{version}/mac_os_x/10.15/chef-workstation-#{version}-1.x86_64.dmg"
   appcast "https://omnitruck.chef.io/stable/chef-workstation/metadata?p=mac_os_x&pv=10.15&m=x86_64&v=latest"
   name "Chef Workstation"
   desc "All-in-one installer for the tools you need to manage your Chef infrastructure"
@@ -10,7 +10,7 @@ cask "chef-workstation" do
 
   depends_on macos: ">= :high_sierra"
 
-  pkg "chef-workstation-#{version}-1.pkg"
+  pkg "chef-workstation-#{version}-1.x86_64.pkg"
 
   uninstall quit:      "sh.chef.chef-workstation",
             pkgutil:   "com.getchef.pkg.chef-workstation",
