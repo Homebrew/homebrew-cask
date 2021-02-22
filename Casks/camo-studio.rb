981227b51d901ab425caf40c8fa4f71ad39eed76
Ayden Panhuyzen <git@ayden.dev>
2021-02-22 17:20:29 -0500

    Replace Camo Studio appcast with sparkle strategy livecheck
    Signed-off-by: Ayden Panhuyzen <git@ayden.dev>

--- Casks/camo-studio.rb
+++ Casks/camo-studio.rb
@@ -3,10 +3,14 @@ cask "camo-studio" do
   sha256 "37aea984109f4c8bf6f007a8bd8cb55de2e745773684c8cca3d736b421023ce0"
 
   url "https://reincubate.com/res/labs/camo/Camo%20Studio%20#{version.before_comma}%20%28#{version.after_comma}%29%20%5BRelease%5D.app.zip"
-  appcast "https://uds.reincubate.com/release-notes/camo/?format=sparkle"
   name "Camo Studio"
   desc "Use your phone as a pro webcam to look amazing on video calls"
   homepage "https://reincubate.com/camo/"
+
+  livecheck do
+    url "https://uds.reincubate.com/release-notes/camo/?format=sparkle"
+    strategy :sparkle
+  end
 
   auto_updates true
 
