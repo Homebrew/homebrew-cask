195d2d6f71031c0e5b08ef245e88d9c062b0a158
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-02-22 20:57:22 -0800

    Update 0-ad from 0.0.24-alpha to 0.0.24b-alpha (#100258)

--- Casks/0-ad.rb
+++ Casks/0-ad.rb
@@ -4,8 +4,8 @@ cask "0-ad" do
     sha256 "5fae1331bb1778bf16e49460fb850270b6cd76bee6483078ff27582e71ab263f"
     app "0 A.D.app"
   else
-    version "0.0.24-alpha"
-    sha256 "a7164b407f5b20747dd4eba97a38b3c8d2f0af72bf92829bf0f1b0b27a0b36f7"
+    version "0.0.24b-alpha"
+    sha256 "483bfdff249ff93435a4638631b47b4037b43e44719ba2efdc10f7f76771fc33"
     app "0 A.D..app"
   end
 
