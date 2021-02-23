3d08ec3b47d55dfe635e42c53c4b308dc77753ec
cho-m <Cho.Michael.H@gmail.com>
2021-02-22 18:44:02 -0800

    sf-symbols: only do livecheck for latest 2.x+ version

--- Casks/sf-symbols.rb
+++ Casks/sf-symbols.rb
@@ -4,28 +4,22 @@ cask "sf-symbols" do
     sha256 "eddca84dde246f358429e47a8a7906b026d892dba97e556b555b27e7070de04e"
 
     url "https://devimages-cdn.apple.com/design/resources/download/SF-Symbols.dmg"
-
-    livecheck do
-      url "https://developer.apple.com/sf-symbols/"
-      strategy :page_match
-      regex(/SF\s*Symbols\s*(\d+(?:\.\d+)*).*?\n?.*?Requires.*?macOS\s*10\.14/i)
-    end
   else
     version "2.1"
     sha256 "97af56dee070ddf7f22cf649aab9bde61edab4aa16021702e51d912a8303c02e"
 
     url "https://devimages-cdn.apple.com/design/resources/download/SF-Symbols-#{version}.dmg"
-
-    livecheck do
-      url "https://developer.apple.com/sf-symbols/"
-      strategy :page_match
-      regex(%r{href=.*?/SF-Symbols-(\d+(?:\.\d+)*)\.dmg}i)
-    end
   end
 
   name "SF Symbols"
   desc "Tool that provides consistent, highly configurable symbols for apps"
   homepage "https://developer.apple.com/design/human-interface-guidelines/sf-symbols/overview/"
+
+  livecheck do
+    url "https://developer.apple.com/sf-symbols/"
+    strategy :page_match
+    regex(%r{href=.*?/SF-Symbols-(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   depends_on macos: ">= :mojave"
 
