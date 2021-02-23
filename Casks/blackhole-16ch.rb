730fffcd915491514730651f56244b4ece89041c
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-23 14:44:29 +0800

    Update blackhole-16ch.rb

--- Casks/blackhole-16ch.rb
+++ Casks/blackhole-16ch.rb
@@ -26,5 +26,5 @@ cask "blackhole-16ch" do
   end
 
   uninstall quit:    "com.apple.audio.AudioMIDISetup",
-            pkgutil: "audio.existential.BlackHole16ch"
+            pkgutil: "audio.existential.BlackHole"
 end
