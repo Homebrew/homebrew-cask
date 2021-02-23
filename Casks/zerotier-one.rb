5b4d6d569b1e94cd91e6985e97032c3744f9f984
Ian Max Andolina <iandol@users.noreply.github.com>
2021-02-23 14:26:16 +0800

    Update zerotier-one.rb
    based on feedback from https://github.com/Homebrew/discussions/discussions/890#discussioncomment-396118 I've changed the URL and added the sha256

--- Casks/zerotier-one.rb
+++ Casks/zerotier-one.rb
@@ -1,8 +1,8 @@
 cask "zerotier-one" do
   version "1.6.4"
-  sha256 :no_check
+  sha256 "3271dfa409dba0cf05578f60c91e5a8981a5aa685692cf4444a2fe756aae7830"
 
-  url "https://download.zerotier.com/dist/ZeroTier%20One.pkg"
+  url "https://download.zerotier.com/RELEASES/1.6.4/dist/ZeroTier%20One.pkg"
   appcast "https://github.com/zerotier/ZeroTierOne/releases.atom"
   name "ZeroTier One"
   desc "Mesh VPN client"
