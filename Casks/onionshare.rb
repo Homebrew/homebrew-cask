0659bc8ac4cbc119c66a9685ad4aff2bfe8f6a67
Micah Lee <micah@micahflee.com>
2021-02-23 12:52:13 -0800

    Switch from appcast to livecheck

--- Casks/onionshare.rb
+++ Casks/onionshare.rb
@@ -3,10 +3,17 @@ cask "onionshare" do
   sha256 "5de2cb81c87a13bc27cd71061c8c1a1f5dde25efe3e1f6b4ddb02ca0a343323f"
 
   url "https://onionshare.org/dist/#{version}/OnionShare-#{version}.dmg"
-  appcast "https://github.com/micahflee/onionshare/releases.atom"
   name "OnionShare"
   desc "Securely and anonymously share files, host websites, and chat with friends"
   homepage "https://onionshare.org/"
+
+  livecheck do
+    url "https://onionshare.org/latest-version.txt"
+    strategy :page_match do |page|
+      match = page.match(%r{(.*)}i)
+      "#{match[1]}"
+    end
+  end
 
   app "OnionShare.app"
 end
