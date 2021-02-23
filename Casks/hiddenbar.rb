9d56a2af567e7602cae923c9b699142a8947993b
Phan Viết Trung <35858359+viettrungphan@users.noreply.github.com>
2021-02-23 13:18:14 +0700

    Update hiddenbar.rb
    Update v1.7

--- Casks/hiddenbar.rb
+++ Casks/hiddenbar.rb
@@ -1,5 +1,5 @@
 cask "hiddenbar" do
-  version "1.6"
+  version "1.7"
   sha256 "7043cf6a07d9ebdfc1e67656197183bc6dfa81504d447f22b7c89cb62e136893"
 
   url "https://github.com/dwarvesf/hidden/releases/download/v#{version}/Hidden.Bar.#{version}.dmg"
