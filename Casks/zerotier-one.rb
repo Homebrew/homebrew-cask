da20f96036ab0d4656a7248fde2e62a478151ada
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-23 14:47:26 +0800

    Update zerotier-one.rb

--- Casks/zerotier-one.rb
+++ Casks/zerotier-one.rb
@@ -2,7 +2,7 @@ cask "zerotier-one" do
   version "1.6.4"
   sha256 "3271dfa409dba0cf05578f60c91e5a8981a5aa685692cf4444a2fe756aae7830"
 
-  url "https://download.zerotier.com/RELEASES/1.6.4/dist/ZeroTier%20One.pkg"
+  url "https://download.zerotier.com/RELEASES/#{version}/dist/ZeroTier%20One.pkg"
   appcast "https://github.com/zerotier/ZeroTierOne/releases.atom"
   name "ZeroTier One"
   desc "Mesh VPN client"
