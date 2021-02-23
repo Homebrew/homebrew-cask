b99d387e621fc9a9238c1e2dfd1d27dd85abe837
cho-m <Cho.Michael.H@gmail.com>
2021-02-22 21:14:45 -0800

    sf-symbols: add livecheck and zap (#100221)
    * sf-symbols: add livecheck and zap
    
    * sf-symbols: only do livecheck for latest 2.x+ version

--- Casks/sf-symbols.rb
+++ Casks/sf-symbols.rb
@@ -2,10 +2,12 @@ cask "sf-symbols" do
   if MacOS.version <= :mojave
     version "1.1"
     sha256 "eddca84dde246f358429e47a8a7906b026d892dba97e556b555b27e7070de04e"
+
     url "https://devimages-cdn.apple.com/design/resources/download/SF-Symbols.dmg"
   else
     version "2.1"
     sha256 "97af56dee070ddf7f22cf649aab9bde61edab4aa16021702e51d912a8303c02e"
+
     url "https://devimages-cdn.apple.com/design/resources/download/SF-Symbols-#{version}.dmg"
   end
 
@@ -13,10 +15,18 @@ cask "sf-symbols" do
   desc "Tool that provides consistent, highly configurable symbols for apps"
   homepage "https://developer.apple.com/design/human-interface-guidelines/sf-symbols/overview/"
 
+  livecheck do
+    url "https://developer.apple.com/sf-symbols/"
+    strategy :page_match
+    regex(%r{href=.*?/SF-Symbols-(\d+(?:\.\d+)*)\.dmg}i)
+  end
+
   depends_on macos: ">= :mojave"
 
   pkg "SF Symbols.pkg"
 
   uninstall pkgutil: "com.apple.SFSymbols.plist",
             delete:  "/Applications/SF Symbols.app"
+
+  zap trash: "~/Library/Preferences/com.apple.SFSymbols.plist"
 end
