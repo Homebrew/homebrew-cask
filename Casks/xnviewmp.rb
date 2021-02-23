6ef9e3531dee9c1056ba1408166cb3c33224c32b
Joost-Wim Boekesteijn <joost-wim@boekesteijn.nl>
2021-02-23 08:08:13 +0100

    Update xnviewmp from 0.98.1 to 0.98.2 (#100265)

--- Casks/xnviewmp.rb
+++ Casks/xnviewmp.rb
@@ -1,6 +1,6 @@
 cask "xnviewmp" do
-  version "0.98.1"
-  sha256 "fdbdaeb527fe8791bcfcef4e7e1d750c3ea70708c1a9c20e2be07444daa7755c"
+  version "0.98.2"
+  sha256 "8afcc144d7c3da35f5e3c0fe42f61a33e69c0fad41308dde4b25dc900c7452ce"
 
   url "https://download.xnview.com/old_versions/XnViewMP-#{version.no_dots}-mac.dmg"
   name "XnViewMP"
