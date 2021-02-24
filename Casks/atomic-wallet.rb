7c7f5cfdbb23705d1735d8066e2b44dffbdab748
Timothy Clark <TheCoolBlackCat@users.noreply.github.com>
2021-02-24 10:15:04 +0000

    Add atomic-wallet v2.27.1

---
+++ Casks/atomic-wallet.rb
@@ -0,0 +1,11 @@
+cask "atomic-wallet" do
+  version "2.27.1"
+  sha256 :no_check
+
+  url "https://get.atomicwallet.io/download/atomicwallet.dmg"
+  name "Atomic Wallet"
+  desc "Manage your Bitcoin, Ethereum, XRP, Litecoin, XLM, and over 300 other coins and tokens."
+  homepage "https://atomicwallet.io"
+
+  app "Atomic Wallet.app"
+end
