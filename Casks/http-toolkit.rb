c85505188951eee080a0df9e775d761c595d8d10
Gaspard Philibert <contact@gaspardphilibert.com>
2021-02-24 04:47:11 +0100

    Upgrade HTTP Toolkit.app to v1.1.1 (#100292)

--- Casks/http-toolkit.rb
+++ Casks/http-toolkit.rb
@@ -1,8 +1,8 @@
 cask "http-toolkit" do
-  version "1.0.2"
-  sha256 "2a0283f9a3f41dae350753413aae85a5b35a250248da22b2564bd3ed4c68e305"
+  version "1.1.1"
+  sha256 "d5cd7ff72ea8d5347778f5bc944b5c6e9962eae1756ced2374baa97ac531a1ca"
 
-  url "https://github.com/httptoolkit/httptoolkit-desktop/releases/download/v#{version}/HTTP.Toolkit.dmg",
+  url "https://github.com/httptoolkit/httptoolkit-desktop/releases/download/v#{version}/HttpToolkit-#{version}.dmg",
       verified: "github.com/httptoolkit/httptoolkit-desktop/"
   appcast "https://github.com/httptoolkit/httptoolkit-desktop/releases.atom"
   name "HTTP Toolkit"
