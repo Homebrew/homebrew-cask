86bb5a58cc6bca716bcfba81a991b2ba6efd2786
Laurin Brandner <mail@laurinbrandner.ch>
2021-02-25 23:11:38 +0100

    Add Nuage.app v0.0.2

---
+++ Casks/nuage.rb
@@ -0,0 +1,13 @@
+cask "nuage" do
+  version "0.0.2"
+  sha256 "c44758127154e86181aa832fbdccf1d6526ce74a8223e3009b7b10d1981a003b"
+
+  url "https://github.com/lbrndnr/nuage-macos/releases/download/v#{version}/Nuage.app.zip"
+  name "Nuage"
+  desc "Free and open-source SoundCloud client"
+  homepage "https://github.com/lbrndnr/nuage-macos"
+
+  depends_on macos: ">= :big_sur"
+
+  app "Nuage.app"
+end
