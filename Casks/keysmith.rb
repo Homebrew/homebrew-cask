6f585f73eb4016acb499e2e386e8fdba7103ec4d
Daniel de Haas <daniel.andrew.dehaas@gmail.com>
2021-02-24 19:32:17 -0800

    Update keysmith from 1.6.0 to 1.13.0

--- Casks/keysmith.rb
+++ Casks/keysmith.rb
@@ -1,6 +1,6 @@
 cask "keysmith" do
-  version "1.6.0"
-  sha256 "afe0a83ff58bd61f00e28f0c0b1d337ad7050ca50133c934888c643beca416df"
+  version "1.13.0"
+  sha256 "0e7d6ee1e02c79299b937d54a6a45d1c8c408e7b8a21d731dabb8f7337fd4fdd"
 
   url "https://keysmith.app/versions/Keysmith-#{version}.dmg"
   appcast "https://www.keysmith.app/versions/appcast.xml"
