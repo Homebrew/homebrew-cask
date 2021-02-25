9e4b454fa288c659d45bfea758aef5450aa5e13b
Hong Minhee <hong.minhee@gmail.com>
2021-02-25 18:50:46 +0900

    Update rider to 2020.3.3,203.7148.23

--- Casks/rider.rb
+++ Casks/rider.rb
@@ -1,10 +1,11 @@
 cask "rider" do
-  version "2020.3.2,203.6682.21"
-  sha256 "0d1c49a640452a5c809c1b9f7359a9f0375b588c3e4132f96ed94ef4319ca360"
+  version "2020.3.3,203.7148.23"
+  sha256 "fb18ee4b60a18bd77b822753dd9be878330fd54aff32a796d40d3d1374809dfe"
 
   url "https://download.jetbrains.com/rider/JetBrains.Rider-#{version.before_comma}.dmg"
   appcast "https://data.services.jetbrains.com/products/releases?code=RD&latest=true&type=release"
   name "Jetbrains Rider"
+  desc "Cross-Platform .NET IDE from JetBrains"
   homepage "https://www.jetbrains.com/rider/"
 
   auto_updates true
