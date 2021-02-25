0a17ed065781ffef79fcdc81b6dc5df842adb5f8
Louis Orleans <louis@orleans.io>
2021-02-24 17:46:15 -0800

    🐛 specify verified url location

--- Casks/gitimoji.rb
+++ Casks/gitimoji.rb
@@ -3,7 +3,7 @@ cask "gitimoji" do
   sha256 "f5c8b1d924a84701566e223e68ec4d6f318fc76a92e506ab281ccccc6fd004c2"
 
   url "https://github.com/lovetodream/gitimoji/releases/download/v#{version}/gitimoji.app.zip",
-      verified: "https://github.com/"
+      verified: "https://github.com/lovetodream/gitimoji/"
   name "gitimoji"
   desc "Gitmoji on your computer"
   homepage "https://timozacherl.com/apps/#/gitimoji"
