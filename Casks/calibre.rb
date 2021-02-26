03af5e0d0b1d3d71166879128cab9ea83bdcc138
xxyzz <gitpull@protonmail.com>
2021-02-26 14:58:28 +0800

    calibre 5.12.0 (#100372)

--- Casks/calibre.rb
+++ Casks/calibre.rb
@@ -4,8 +4,8 @@ cask "calibre" do
     sha256 "68829cd902b8e0b2b7d5cf7be132df37bcc274a1e5720b4605d2dd95f3a29168"
     url "https://download.calibre-ebook.com/#{version}/calibre-#{version}.dmg"
   else
-    version "5.11.0"
-    sha256 "45abf47f9e95e9410d170665580244ec0938c15757a36b8935500163c58bf583"
+    version "5.12.0"
+    sha256 "eaf207c0c933fe3fe0336896747bd80157ef6dc553c2a958f706494fa7b24aa6"
     url "https://github.com/kovidgoyal/calibre/releases/download/v#{version}/calibre-#{version}.dmg",
         verified: "github.com/kovidgoyal/calibre/"
     appcast "https://github.com/kovidgoyal/calibre/releases.atom"
