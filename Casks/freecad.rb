f4c0a4771638daeb9a09e6562721f09fc3d946c6
Thomas Walter <tw@b-a-l-u.de>
2021-02-26 16:10:29 +0100

    Update freecad from 0.18.4,16146-rev1 to 0.18.6

--- Casks/freecad.rb
+++ Casks/freecad.rb
@@ -1,5 +1,5 @@
 cask "freecad" do
-  version "0.18.4,16146-rev1"
+  version "0.18.6"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version.before_comma}/FreeCAD_#{version.major_minor}-#{version.after_comma}-OSX-x86_64-conda-Qt5-Py3.dmg",
