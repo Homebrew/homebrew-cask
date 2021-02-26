c27d8e68ed55eca47f4d95148bbb6999371ed9bb
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-27 05:12:29 +0800

    Update subethaedit.rb

--- Casks/subethaedit.rb
+++ Casks/subethaedit.rb
@@ -1,5 +1,5 @@
 cask "subethaedit" do
-  version "5.2"
+  version "5.2,9748"
   sha256 "7e3cb09ee77b6a9aa15bd2a73aed08c081819605fabcb03e230eb02b847c5825"
 
   url "https://subethaedit.net/Releases/SubEthaEdit-#{version.before_comma}.zip"
