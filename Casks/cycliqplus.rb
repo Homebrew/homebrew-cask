24a2e76488e29b20138f7af089571444560fcd19
Vítor Galvão <info@vitorgalvao.com>
2021-02-27 00:24:35 +0000

    Update cycliqplus.rb

--- Casks/cycliqplus.rb
+++ Casks/cycliqplus.rb
@@ -3,7 +3,7 @@ cask "cycliqplus" do
   sha256 "76424d10b3e31640267c27a563c630431ad43e20e47b1baf5aebdd9dcb107f43"
 
   url "https://cycliq.com/files/software/CycliqPlus-#{version}.dmg"
-  name "CycliqPlus for Desktop"
+  name "CycliqPlus"
   desc "Footage and bike camera settings editor"
   homepage "https://cycliq.com/"
 
