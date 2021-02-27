7d9bb956ee0bd40b5ee64a19b7dbbe11dbbf37e1
ravenclaw900 <50060110+ravenclaw900@users.noreply.github.com>
2021-02-26 19:53:25 -0600

    Fix URL

--- Casks/nuclear.rb
+++ Casks/nuclear.rb
@@ -2,7 +2,7 @@ cask "nuclear" do
   version "0.6.9"
   sha256 "7393644f0c7ccef4964824883fe13cfb01e78cbab80517054ae973eaad6076d3"
 
-  url "https://github.com/nukeop/nuclear/releases/download/v#{version}/nuclear-v#{version}.dmg",
+  url "https://github.com/nukeop/nuclear/releases/download/v#{version}/nuclear-#{version}.dmg",
       verified: "github.com/nukeop/nuclear/"
   appcast "https://github.com/nukeop/nuclear/releases.atom"
   name "Nuclear"
