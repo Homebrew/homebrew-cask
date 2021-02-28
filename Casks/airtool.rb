97ef84b86d94ba352a61ee4a8740cc3f4b34bcaf
Raul Ferreira <raulmendesferreira@gmail.com>
2021-02-28 18:51:50 +0000

    Fix version number

--- Casks/airtool.rb
+++ Casks/airtool.rb
@@ -1,5 +1,5 @@
 cask "airtool" do
-  version "2.1"
+  version "2.1,5"
   sha256 "f42c2ac515a15c8892e66ff7d5467e3e8616dfe5d873d075cc024ccf623b5866"
 
   url "https://www.intuitibits.com/downloads/Airtool_#{version.before_comma}.pkg"
@@ -11,17 +11,19 @@ cask "airtool" do
     strategy :sparkle
   end
 
+  auto_updates true
+
   pkg "Airtool_#{version.before_comma}.pkg"
 
   uninstall_preflight do
-    set_ownership "/Library/Application Support/Airtool"
+    set_ownership "/Library/Application Support/Airtool 2"
   end
 
   uninstall pkgutil:    [
     "com.intuitibits.airtool-helper.pkg",
     "com.intuitibits.airtool.pkg",
   ],
-            launchctl:  "com.intuitibits.airtool.airtool-bpf",
+            launchctl:  "com.intuitibits.airtool2.airtool-bpf",
             login_item: "Airtool",
-            delete:     "/Library/Application Support/Airtool"
+            delete:     "/Library/Application Support/Airtool 2"
 end
