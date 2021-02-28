b64ea9aa8aabfa5276933f60721ed4a607a3de24
Leopoldo Pla <lpla@dlsi.ua.es>
2021-02-28 03:49:54 +0100

    Update sameboy from 0.13.6 to 0.14 (#100429)

--- Casks/sameboy.rb
+++ Casks/sameboy.rb
@@ -1,6 +1,6 @@
 cask "sameboy" do
-  version "0.13.6"
-  sha256 "d55eb020c789d59672f59ab4f75a5ac9beaf1b202d3d70482c926d27673e70e4"
+  version "0.14"
+  sha256 "9b48473b5fd81f65cae401dafc03bdc1ecdc65328fdbc86ddd9e1191516c3cb9"
 
   url "https://github.com/LIJI32/SameBoy/releases/download/v#{version}/sameboy_cocoa_v#{version}.zip",
       verified: "github.com/LIJI32/SameBoy/"
