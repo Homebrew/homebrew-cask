39fe1c4e1132c74d0f2bba2db9d0f3e04ec3fc5f
Roman Pearah <neverfox@neverfox.io>
2021-02-28 11:23:30 -0500

    Add skychart.app v4.2.1-4073

---
+++ Casks/skychart.rb
@@ -0,0 +1,22 @@
+cask "skychart" do
+  version "4.2.1-4073"
+  sha256 "72670b4c3d4145a2f609197d62bb07351d2e7cdce36d889d0d0f9a002158aa47"
+
+  url "https://downloads.sourceforge.net/skychart/skychart-#{version}-x86_64-macosx.dmg",
+      verified: "downloads.sourceforge.net/skychart/"
+  appcast "https://sourceforge.net/projects/skychart/rss?path=/"
+  name "SkyChart"
+  name "Cartes du Ciel"
+  desc "Free software to draw sky charts"
+  homepage "https://www.ap-i.net/skychart/"
+
+  pkg "Install Skychart.pkg"
+
+  uninstall pkgutil: "net.ap-i.pkg.skychart"
+
+  zap trash: [
+    "~/Library/Application Support/skychart",
+    "~/Library/Preferences/net.ap-i.skychart.plist",
+    "~/Library/Saved Application State/net.ap-i.skychart.savedState",
+  ]
+end
