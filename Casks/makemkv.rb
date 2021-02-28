9da43605106123fbc41e88fc0868f470425694ae
Luflosi <Luflosi@users.noreply.github.com>
2021-02-28 12:02:08 +0100

    Update makemkv (#100445)

--- Casks/makemkv.rb
+++ Casks/makemkv.rb
@@ -1,6 +1,6 @@
 cask "makemkv" do
-  version "1.15.4"
-  sha256 "a8fc6bc4f249d6b0853f5a2a76aac897cf9bbd35784a177ac0d29ac669853adf"
+  version "1.16.0"
+  sha256 "f60124dd6dbd89067a46b4d71c0d04497fe1795562a03b1e23bcd00142057292"
 
   url "https://www.makemkv.com/download/makemkv_v#{version}_osx.dmg"
   name "MakeMKV"
