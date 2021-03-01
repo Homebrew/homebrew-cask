3ac7d7483ed2fbfa7cb6e7e68f9dd6546f8c85f2
morusn <morusny12@outlook.com>
2021-03-01 07:53:22 +0100

    Update devolo-cockpit from 5.1.4 to 5.1.5.245

--- Casks/devolo-cockpit.rb
+++ Casks/devolo-cockpit.rb
@@ -1,6 +1,6 @@
 cask "devolo-cockpit" do
-  version "5.1.4"
-  sha256 "3fe25fe820e6abab489dd2f5e7de468430ff60e2131d3e6b4e8b8c3df8537cf1"
+  version "5.1.5.245"
+  sha256 "21fae361aac8abb9ac1410bd2a6f0ad4d72ddf72e036c1c3b2cd9976009fc20c"
 
   url "https://www.devolo.com/fileadmin/Web-Content/DE/products/hnw/devolo-cockpit/software/devolo-cockpit-v#{version.dots_to_hyphens}.dmg"
   appcast "https://www.devolo.com/support/downloads/download/devolo-cockpit"
