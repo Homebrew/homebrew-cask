d07ee20362a5cab93832fa60b1627520e3aec750
David Campbell <alchemydc@users.noreply.github.com>
2021-02-28 18:00:24 -0700

    Add Zecwallet Lite.app v1.4.2 (#100441)

---
+++ Casks/zecwallet-lite.rb
@@ -0,0 +1,22 @@
+cask "zecwallet-lite" do
+  version "1.4.2"
+  sha256 "09e99c3458f4f05f6cd3b68339073b13871fc93bfbefbc0c6bbd968611808331"
+
+  url "https://github.com/adityapk00/zecwallet-lite/releases/download/v#{version}/Zecwallet.Lite-#{version}.dmg",
+      verified: "github.com/adityapk00/zecwallet-lite/"
+  name "Zecwallet Lite"
+  desc "Zcash Light Wallet"
+  homepage "https://www.zecwallet.co/#download"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "Zecwallet Lite.app"
+
+  zap trash: [
+    "~/Library/Application Support/Zecwallet Lite",
+    "~/Library/Application Support/Zcash/zecwallet-light-wallet.debug.log",
+  ]
+end
