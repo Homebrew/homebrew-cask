748ac0e874bf239fa14300ca931d30f271f5c157
jiaoxuefeng <jiaoxuefeng@huobi.com>
2021-03-01 15:04:59 +0800

    update surge to 4.0.5

--- Casks/surge.rb
+++ Casks/surge.rb
@@ -1,6 +1,6 @@
 cask "surge" do
-  version "4.0.4-1227-9acb8b9e3f39e9048fc82e427184a4af"
-  sha256 "60e3d53b3e5dd6805abc69b382a022e4436e91de92a126786febef0f2ceebd22"
+  version "4.0.5-1262-db70f680cd0f15236c8415ec7b804c3a"
+  sha256 "3773f0468cabb0dc58823a968fb438196b256bc459803053957c8fe45a7ee310"
 
   url "https://www.nssurge.com/mac/v#{version.major}/Surge-#{version}.zip"
   appcast "https://www.nssurge.com/mac/v#{version.major}/appcast-signed.xml"
