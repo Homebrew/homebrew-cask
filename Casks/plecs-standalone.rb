7368065147731a0b44a41c15d14fdf8676f2a95e
morusn <morusny12@outlook.com>
2021-03-01 06:47:59 +0100

    Update plecs-standalone from 4.3.6 to 4.5.3

--- Casks/plecs-standalone.rb
+++ Casks/plecs-standalone.rb
@@ -1,6 +1,6 @@
 cask "plecs-standalone" do
-  version "4.3.6"
-  sha256 "a517cebe68283865b34107351958ae817da3e25baa0aebb704d587258ab32842"
+  version "4.5.3"
+  sha256 "13b222bfce3e29abdc32384338de52337380296b7d052e8c1959357f73eb86c3"
 
   url "https://www.plexim.com/sites/default/files/packages/plecs-standalone-#{version.dots_to_hyphens}_maci64.dmg"
   appcast "https://www.plexim.com/download/latest_version?myversion=#{version}"
