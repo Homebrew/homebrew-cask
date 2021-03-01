9c3931fc47a62911909124c056dd9812ef74df12
Brad Beyenhof <brad.beyenhof@servicenow.com>
2021-02-28 17:10:00 -0800

    Update textmate from 2.0.17 to 2.0.19 (#100439)

--- Casks/textmate.rb
+++ Casks/textmate.rb
@@ -1,6 +1,6 @@
 cask "textmate" do
-  version "2.0.17"
-  sha256 "78c0fd87506ac2ed984912296a8f6d5c69ff4c1616cac506f852d2b5f5c884eb"
+  version "2.0.19"
+  sha256 "e0117fc281931883fa4b40daf6f594cb9cdc076190ddf50dc40e6913ea6bdcfa"
 
   url "https://github.com/textmate/textmate/releases/download/v#{version}/TextMate_#{version}.tbz",
       verified: "github.com/textmate/textmate/"
