63a40c0eb1f7fb9b7d71dac73048f02ea604f3eb
Yurii Kolesnykov <root@yurikoles.com>
2021-03-01 22:32:51 +0200

    Update opencore-configurator from 2.28.0.0 to 2.29.0.0

--- Casks/opencore-configurator.rb
+++ Casks/opencore-configurator.rb
@@ -1,5 +1,5 @@
 cask "opencore-configurator" do
-  version "2.28.0.0"
+  version "2.29.0.0"
   sha256 :no_check
 
   url "https://mackie100projects.altervista.org/apps/opencoreconf/download-new-build.php?version=last"
