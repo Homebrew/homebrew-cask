cc9964a2e3b50f47ef83eae7de17a87f3f585e9a
Chris Chinchilla <chris@chrischinchilla.com>
2021-03-01 15:29:54 +0100

    Update raindropio.rb
    Updates version and changes to new URL structure. Looks like Raindrop did this to support Intel and arm architecture, but I haven't checked to see if that's possible with Brew yet, so just updating the intel formula right now

--- Casks/raindropio.rb
+++ Casks/raindropio.rb
@@ -1,8 +1,8 @@
 cask "raindropio" do
-  version "5.2.49"
+  version "5.2.71"
   sha256 :no_check
 
-  url "https://raindrop.io/releases/mac/Raindrop.dmg"
+  url "https://github.com/raindropio/desktop/releases/download/v#{version}/Raindrop-x64.dmg"
   name "Raindrop.io"
   desc "All-in-one bookmark manager"
   homepage "https://raindrop.io/"
