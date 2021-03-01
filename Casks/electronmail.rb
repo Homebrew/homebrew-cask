fcd232ae21a4213349b582f92efb56a70bd3cff4
Michael "MJ" Wojcik <michael-wojcik@users.noreply.github.com>
2021-02-28 18:47:11 -0600

    Update electronmail from 4.10.1 to 4.10.2 (#100467)

--- Casks/electronmail.rb
+++ Casks/electronmail.rb
@@ -1,6 +1,6 @@
 cask "electronmail" do
-  version "4.10.1"
-  sha256 "3e72c869855762431ecd03dc9a8820d75f512de2d042617680a01d7c47d4377c"
+  version "4.10.2"
+  sha256 "4bcff8555b7217e6339c281a4e36d776ee50fd43207326161e5edee6f4e83337"
 
   url "https://github.com/vladimiry/ElectronMail/releases/download/v#{version}/electron-mail-#{version}-mac.dmg"
   appcast "https://github.com/vladimiry/ElectronMail/releases.atom"
