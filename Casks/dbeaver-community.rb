db2626a8bb450a3674630d64fd5d24166f3072de
Tiago Mota <tiago.marques.mota@gmail.com>
2021-03-02 11:58:21 +0000

    Update dbeaver-community from 21.0.0 to 21.0.0 (#100507)

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,6 +1,6 @@
 cask "dbeaver-community" do
   version "21.0.0"
-  sha256 "db25d2369bbfaa452538e56adfa272f2e6dcca08055a2c5eb2a41884d08eb39b"
+  sha256 "9e0e6dd8248b7a100aa56668684769b1d7d74a667bb516ae60e1fed18eb3e70f"
 
   url "https://dbeaver.io/files/#{version}/dbeaver-ce-#{version}-macos.dmg"
   name "DBeaver Community Edition"
