4f509f6fc51c36480abdd7303e4d58ea4be370dd
Miccal Matthews <miccal.matthews@gmail.com>
2021-03-02 13:19:47 +0800

    Update banksiagui.rb

--- Casks/banksiagui.rb
+++ Casks/banksiagui.rb
@@ -3,7 +3,6 @@ cask "banksiagui" do
   sha256 "97d79126c90646f73f2fe4b11d65817cba10cd24731ec2c5980a997fb40a8c4c"
 
   url "https://banksiagui.com/dl/BanksiaGui-#{version}-mac.zip"
-  appcast "https://banksiagui.com/download/"
   name "BanksiaGui"
   desc "Chess GUI"
   homepage "https://banksiagui.com/"
