b53dc8d1730b245584aea9e3fd71c89c0184b690
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-03-02 14:21:58 -0800

    Update electron from 11.3.0 to 12.0.0

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,6 +1,6 @@
 cask "electron" do
-  version "11.3.0"
-  sha256 "7569db1d2e470b0db512735f27f99498f631da3cd86374345139f18df88789fe"
+  version "12.0.0"
+  sha256 "18546dec0ecc63d1a679762e00bc85fbb820e08f7ca205924681379bb0a7afc2"
 
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip",
       verified: "github.com/electron/electron/"
