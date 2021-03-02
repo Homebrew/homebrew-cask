fa79a9b0ca474122439a5775a1b57d8bb93cd8eb
Andrzej Undzillo <andrzej.undzillo@gmail.com>
2021-03-02 20:39:42 +0100

    haskell-language-server cask (1.0.0)

--- Casks/haskell-language-server.rb
+++ Casks/haskell-language-server.rb
@@ -1,11 +1,10 @@
 cask "haskell-language-server" do
-  version "0.8.0"
-  sha256 "1c1668bb455a5164df62774e6fb8f8f91a831ce527d9e5f89f453fb28ef54622"
+  version "1.0.0"
+  sha256 "74e7624c889c0235f0b02d7e7f164d5eb95b611d584fc8602f0b3a099b73f8be"
 
   url "https://github.com/haskell/haskell-language-server/releases/download/#{version}/haskell-language-server-macOS-#{version}.tar.gz"
-  appcast "https://github.com/haskell/haskell-language-server/releases.atom"
   name "Haskell Language Server"
-  desc "Integration point for ghcide and haskell-ide-engine. One IDE to rule them all"
+  desc "Integration point for ghcide and haskell-ide-engine. One IDE to rule them all."
   homepage "https://github.com/haskell/haskell-language-server"
 
   binary "haskell-language-server-wrapper"
@@ -14,12 +13,15 @@ cask "haskell-language-server" do
   binary "haskell-language-server-8.8.2"
   binary "haskell-language-server-8.8.3"
   binary "haskell-language-server-8.8.4"
-  binary "haskell-language-server-8.10.1"
   binary "haskell-language-server-8.10.2"
   binary "haskell-language-server-8.10.3"
+  binary "haskell-language-server-8.10.4"
 
   caveats <<-EOS
-   You need to provide GHC/stack/cabal yourself, depending on your needs
-   (see https://github.com/haskell/haskell-language-server#prerequisites).
+    Install with `brew install #{token} --no-quarantine`,
+    or manually approve security exceptions in System Preferences -> Security & Privacy.
+
+    You need to provide GHC/stack/cabal yourself, depending on your needs
+    (see https://github.com/haskell/haskell-language-server#prerequisites).
   EOS
 end
