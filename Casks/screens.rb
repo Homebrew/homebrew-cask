37e43e73e5dc8ef43d77150b1ce7cb89abe87d61
Reno <25192197+singularitti@users.noreply.github.com>
2021-03-02 10:25:13 -0500

    Update screens from 4.8.4,1609790953 to 4.8.5,1614695241 (#100513)

--- Casks/screens.rb
+++ Casks/screens.rb
@@ -1,6 +1,6 @@
 cask "screens" do
-  version "4.8.4,1609790953"
-  sha256 "dc7c88211e8ff8ca475f9ecc46fd5443308e27091c77bbfd2ce4b4b07a93f833"
+  version "4.8.5,1614695241"
+  sha256 "fedfe8ff7469ea4e8ac087f3a08400c93f5e8f415cdd8173ab1bba1b848f3d5b"
 
   url "https://updates.edovia.com/com.edovia.screens#{version.major}.mac/Screens_#{version.before_comma}b#{version.after_comma}.zip"
   name "Screens"
