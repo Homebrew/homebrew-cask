ba765fc1f511f19c8397977a7e5c83b93a2bcf40
Ondrej Führer <ondrejfuhrer@users.noreply.github.com>
2021-03-03 09:22:08 +0100

    chronos: fix rubocop offenses

--- Casks/chronos.rb
+++ Casks/chronos.rb
@@ -5,10 +5,10 @@ cask "chronos" do
   url "https://github.com/web-pal/chronos-timetracker/releases/download/v#{version}/Chronos-#{version}-mac.zip"
   appcast "https://github.com/web-pal/chronos-timetracker/releases.atom"
   name "Chronos Timetracker"
+  desc "Desktop client for JIRA and Trello"
   homepage "https://github.com/web-pal/chronos-timetracker"
 
   app "Chronos.app"
-  desc "Desktop client for JIRA. Track time, upload automatic and manual worklogs without a hassle. Cross-platform. Open-sourced."
 
   zap trash: [
     "~/Library/Application Support/Chronos",
