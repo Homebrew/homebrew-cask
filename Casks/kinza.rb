7539aa8c29cb9e78be8bc9975fb96922aa220046
Adithya Sunil <adithyasunil26@gmail.com>
2021-03-03 17:20:12 +0400

    Update kinza from 6.8.2 to 6.8.3 (#100613)

--- Casks/kinza.rb
+++ Casks/kinza.rb
@@ -1,6 +1,6 @@
 cask "kinza" do
-  version "6.8.2"
-  sha256 "d91cbb5df05a95735835da907b56985ba8c9b3705138b75be1a1e47e1d9f0756"
+  version "6.8.3"
+  sha256 "e4c3a7f105f5c95e7ce3c5e4ea9277b9b17e3ff48ba4ccb91e5388a3fcbb7cfe"
 
   url "https://d7s9ygw7nxr46.cloudfront.net/mac/kinza_#{version}.dmg",
       verified: "d7s9ygw7nxr46.cloudfront.net/"
