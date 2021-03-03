e552c039c5dbf9c98ab7cf6d9c3be54f5b8ead70
Adithya Sunil <adithyasunil26@gmail.com>
2021-03-03 17:16:10 +0400

    Update spectx from 1.4.70 to 1.4.72 (#100611)

--- Casks/spectx.rb
+++ Casks/spectx.rb
@@ -1,6 +1,6 @@
 cask "spectx" do
-  version "1.4.70"
-  sha256 "cf827926c331cf095b628b55a13cf0ab26c6a06f926ea7a131bd63d02c9667e9"
+  version "1.4.72"
+  sha256 "8149ab4f61fe920c286789627bd9789e88b7223d5293037322d4159706b37013"
 
   url "https://download.spectx.com/versions/#{version}/SpectXDesktop-v#{version}.dmg"
   name "SpectX Desktop"
