02d59f5257e32e10403db5430c8e6928eb6ece6b
Adithya Sunil <adithyasunil26@gmail.com>
2021-03-03 17:06:01 +0400

    Update jclasslib-bytecode-viewer from 5.7 to 5.8 (#100594)

--- Casks/jclasslib-bytecode-viewer.rb
+++ Casks/jclasslib-bytecode-viewer.rb
@@ -1,6 +1,6 @@
 cask "jclasslib-bytecode-viewer" do
-  version "5.7"
-  sha256 "f742ea0994d9f3292d0727d2360aa744f583d9d7daea80043c30a6e64ea3f002"
+  version "5.8"
+  sha256 "e62d16c01e25628d2b69f951e01f875f64d1a72e391f131643f85b6f993548d0"
 
   url "https://github.com/ingokegel/jclasslib/releases/download/#{version}/jclasslib_macos_#{version.dots_to_underscores}.dmg"
   appcast "https://github.com/ingokegel/jclasslib/releases.atom"
