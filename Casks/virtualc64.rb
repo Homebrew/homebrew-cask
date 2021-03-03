0dbd7c367c66ca364680c1a5760ea31e0cf5d4f3
Leopoldo Pla <lpla@dlsi.ua.es>
2021-03-03 02:00:01 +0100

    Update virtualc64 from 4.0 to 4.0.1 (#100522)

--- Casks/virtualc64.rb
+++ Casks/virtualc64.rb
@@ -1,16 +1,18 @@
 cask "virtualc64" do
   # NOTE: "64" is not a version number, but an intrinsic part of the product name
-  version "4.0"
-  sha256 "97bf78bd58b5855206a502ab9ae26be3e4c554c5865e5cde9d54ca52016979d8"
+  version "4.0.1"
+  sha256 "eb3467c08d66a5c80376bbe3dd1f63e99914cf9c9658d29548cfd130cc2058e4"
 
-  url "http://www.dirkwhoffmann.de/virtualc64/VirtualC64_#{version}.zip"
-  name "Virtual C64"
-  homepage "http://www.dirkwhoffmann.de/virtualc64/"
+  url "https://github.com/dirkwhoffmann/virtualc64/releases/download/v#{version}/VirtualC64_#{version}.app.zip",
+      verified: "github.com/dirkwhoffmann/virtualc64/"
+  name "VirtualC64"
+  desc "Cycle-accurate C64 emulator"
+  homepage "https://dirkwhoffmann.github.io/virtualc64"
 
   livecheck do
-    url "http://dirkwhoffmann.de/virtualc64/VirtualC64Appcast.xml"
-    strategy :sparkle
+    url :url
+    strategy :github_latest
   end
 
-  app "VirtualC64.app"
+  app "VirtualC64_#{version}.app"
 end
