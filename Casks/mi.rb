60803a59f954ea054902698a04889cf41c8724f8
Adithya Sunil <adithyasunil26@gmail.com>
2021-03-03 16:38:46 +0400

    Update mi from 3.5.1,975 to 3.5.2,1000 (#100571)

--- Casks/mi.rb
+++ Casks/mi.rb
@@ -1,6 +1,6 @@
 cask "mi" do
-  version "3.5.1,975"
-  sha256 "277cccdf47aafe9d1d4d3b65289861e47cc60269c4a280fcde9e428b7009ba5e"
+  version "3.5.2,1000"
+  sha256 "19646316623e83359b2f5c7096270e29ddc9b7db3b2f53d1350214bad3e23226"
 
   url "https://www.mimikaki.net/download/mi#{version.before_comma}.dmg"
   name "mi"
