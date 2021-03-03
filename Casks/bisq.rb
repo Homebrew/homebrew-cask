33b9d4c04902536430d8737d7f02f5d4784919d6
Hannes Braun <hannesbraun@mail.de>
2021-03-03 20:28:28 +0100

    Update bisq from 1.5.6 to 1.5.9 (#100643)

--- Casks/bisq.rb
+++ Casks/bisq.rb
@@ -1,6 +1,6 @@
 cask "bisq" do
-  version "1.5.6"
-  sha256 "d25a4e7188d9966436f9badf3f5859badd770a4646b29364f60a09f2cc33ac81"
+  version "1.5.9"
+  sha256 "420d02bcede1f2ae1c0fcd3053a11e9993030e3da3ab2666b7e451147f453a2f"
 
   url "https://github.com/bisq-network/bisq/releases/download/v#{version}/Bisq-#{version}.dmg",
       verified: "github.com/bisq-network/bisq/"
