dad71b4c7f835938bda7469cac127ae81bc52358
Adithya Sunil <adithyasunil26@gmail.com>
2021-03-03 17:06:40 +0400

    Update hubstaff from 1.5.16,2850 to 1.5.17,2982 (#100599)

--- Casks/hubstaff.rb
+++ Casks/hubstaff.rb
@@ -1,6 +1,6 @@
 cask "hubstaff" do
-  version "1.5.16,2850"
-  sha256 "0b37c45740efd23873d350fd8fd1f700e0567dd33c966bccb9ce1a8675f5e5fe"
+  version "1.5.17,2982"
+  sha256 "90d0791902d1aac8ea797f302e97aeae26110394cb702e207e2b70cd1448f55a"
 
   url "https://app.hubstaff.com/download/#{version.after_comma}-mac-os-x-#{version.before_comma.dots_to_hyphens}-release"
   name "Hubstaff"
