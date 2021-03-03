654b62c279e1af976c526141b4a2ac5cdb668f7e
Reno <25192197+singularitti@users.noreply.github.com>
2021-03-02 20:02:13 -0500

    Update wormhole from 1.4.9 to 1.5.2 (#100533)

--- Casks/wormhole.rb
+++ Casks/wormhole.rb
@@ -1,11 +1,12 @@
 cask "wormhole" do
-  version "1.4.9"
+  version "1.5.2"
   sha256 :no_check
 
   url "https://files.octopusgame.com/os/WormholeInstaller.dmg",
       verified: "files.octopusgame.com/"
   appcast "https://er.run/release"
   name "Wormhole"
+  desc "Browse & Control phone on PC, Screen Fusion for iOS & Android"
   homepage "https://er.run/"
 
   app "Wormhole.app"
