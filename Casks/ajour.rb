74e65ffe41e7b8887a86ae623c700153f724f9df
Adithya Sunil <adithyasunil26@gmail.com>
2021-03-03 16:39:17 +0400

    Update ajour from 0.7.1 to 0.7.2 (#100573)

--- Casks/ajour.rb
+++ Casks/ajour.rb
@@ -1,6 +1,6 @@
 cask "ajour" do
-  version "0.7.1"
-  sha256 "b2dbf7b16ed0ced74ff8f05b276c7bbc56b2b782cbe38e101bf2c650ba9c9da6"
+  version "0.7.2"
+  sha256 "a2cadd178e212ea5bdc98ce46cb4ca212fe40297415f0fcbd3889f1837526a5f"
 
   url "https://github.com/casperstorm/ajour/releases/download/#{version}/ajour.dmg"
   name "Ajour"
