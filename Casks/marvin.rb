25b28669a955416afa50c5a6f5bd6f0c9eca5feb
Adithya Sunil <adithyasunil26@gmail.com>
2021-03-03 16:27:04 +0400

    Update marvin from 1.58.2 to 1.59.0 (#100561)

--- Casks/marvin.rb
+++ Casks/marvin.rb
@@ -1,6 +1,6 @@
 cask "marvin" do
-  version "1.58.2"
-  sha256 "4174b7f1a593336b2c8045a61d106c8b6078d7ba979ebf10dd37280308d76b25"
+  version "1.59.0"
+  sha256 "a25e3e32e60d92992c7dbbdfd2028b45be7c49dfc22b357dc5cfa95b3d68fc88"
 
   url "https://amazingmarvin.s3.amazonaws.com/Marvin-#{version}.dmg",
       verified: "amazingmarvin.s3.amazonaws.com/"
