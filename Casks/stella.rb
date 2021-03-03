a416dcd239360526d125fb5db94a4de8deee7a87
adithyasunil26 <adithyasunil26@gmail.com>
2021-03-03 16:45:02 +0400

    Update stella from 6.5 to 6.5.2

--- Casks/stella.rb
+++ Casks/stella.rb
@@ -1,6 +1,6 @@
 cask "stella" do
-  version "6.5"
-  sha256 "d22e36a0a4c4f9ae835070bfd2dbf5c9fb222c32ea45cf0b146d88a15d875fa0"
+  version "6.5.2"
+  sha256 "793b183578f31bba7e718dcacfda248eb4666ea6cd8da74c220a2b9951aa2847"
 
   url "https://github.com/stella-emu/stella/releases/download/#{version}/Stella-#{version}-macos.dmg",
       verified: "github.com/stella-emu/stella/"
