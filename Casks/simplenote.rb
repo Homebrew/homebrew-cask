65f6792cd28e14474ce0b768a2759d96f7fa1424
Adithya Sunil <adithyasunil26@gmail.com>
2021-03-03 16:49:51 +0400

    Update simplenote from 2.6.0 to 2.7.0 (#100581)

--- Casks/simplenote.rb
+++ Casks/simplenote.rb
@@ -1,6 +1,6 @@
 cask "simplenote" do
-  version "2.6.0"
-  sha256 "326a5eba713525186df2dc2c795c03d4b7fa212a0bc618c0ec44715ea1494214"
+  version "2.7.0"
+  sha256 "d828b2a79046078e3dd94060e349f197ee43770db9b54f919aab9f62fe80cb78"
 
   url "https://github.com/Automattic/simplenote-electron/releases/download/v#{version}/Simplenote-macOS-#{version}.dmg"
   appcast "https://github.com/Automattic/simplenote-electron/releases.atom"
