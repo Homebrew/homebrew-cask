093315e3c3e0ad7df6a2e821465c58f453024b67
Tony Dinh <gh@tdinh.me>
2021-03-04 21:33:17 +0800

    Fix style for homepage address and change version

--- Casks/devutils.rb
+++ Casks/devutils.rb
@@ -2,10 +2,10 @@ cask "devutils" do
   version :latest
   sha256 :no_check
 
-  url "https://devutils.app/archives/DevUtils-#{version}.dmg"
+  url "https://devutils.app/archives/DevUtils-latest.dmg"
   name "DevUtils"
   desc "Offline Toolbox for Developers"
-  homepage "https://devutils.app"
+  homepage "https://devutils.app/"
 
   app "DevUtils.app"
 end
