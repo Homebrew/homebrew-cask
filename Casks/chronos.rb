78763ec3999c879e741bcbb1a49e68345c6e9e26
Ondřej Führer <ondrejfuhrer@users.noreply.github.com>
2021-03-04 01:41:31 +0100

    chronos: adding `zap` stanza (#100554)
    * chronos: adding `zap` stanza
    
    * chronos: fix rubocop offenses

--- Casks/chronos.rb
+++ Casks/chronos.rb
@@ -5,7 +5,14 @@ cask "chronos" do
   url "https://github.com/web-pal/chronos-timetracker/releases/download/v#{version}/Chronos-#{version}-mac.zip"
   appcast "https://github.com/web-pal/chronos-timetracker/releases.atom"
   name "Chronos Timetracker"
+  desc "Desktop client for JIRA and Trello"
   homepage "https://github.com/web-pal/chronos-timetracker"
 
   app "Chronos.app"
+
+  zap trash: [
+    "~/Library/Application Support/Chronos",
+    "~/Library/Preferences/com.web-pal.chronos.plist",
+    "~/Library/Saved Application State/com.web-pal.chronos.savedState",
+  ]
 end
