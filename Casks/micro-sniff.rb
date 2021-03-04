4fbcb0148689813376a508624f8f2803c84b9095
Armand Fardeau <armandfardeau@users.noreply.github.com>
2021-03-05 00:21:34 +0100

    Micro sniff (#100686)
    * Add micro-sniff
    
    * Add micro-sniff
    
    * Remove mac from description
    
    * Update micro-sniff.rb
    
    * Add zap
    
    * Update micro-sniff.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/micro-sniff.rb
@@ -0,0 +1,20 @@
+cask "micro-sniff" do
+  version "1.0.0"
+  sha256 "fbfd7eccc2de7fc9d5d5a4062057791850fdcedb4b14ec04538cfabce263aa5f"
+
+  url "https://github.com/dwarvesf/micro-sniff/releases/download/v#{version}/Micro.Sniff.#{version}.dmg"
+  name "Micro Sniff"
+  desc "Monitor microphone activity"
+  homepage "https://github.com/dwarvesf/micro-sniff"
+
+  depends_on macos: ">= :sierra"
+
+  app "Micro Sniff.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/foundation.dwarves.microsniff",
+    "~/Library/Application Scripts/foundation.dwarves.microsnifflauncher",
+    "~/Library/Containers/foundation.dwarves.microsniff",
+    "~/Library/Containers/foundation.dwarves.microsnifflauncher",
+  ]
+end
