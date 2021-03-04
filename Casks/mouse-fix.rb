ed094a7507127f9a3e1bca3ba9f257ddae942401
Randall <17261190+ran-dall@users.noreply.github.com>
2021-03-04 12:47:00 +0000

    Add Mouse Fix 1.0.1 (#100658)

---
+++ Casks/mouse-fix.rb
@@ -0,0 +1,19 @@
+cask "mouse-fix" do
+  version "1.0.1"
+  sha256 "923469ae24f796115eebab2baf71d05e0c6b0fef0a5a8deb2afe800c83995044"
+
+  url "https://github.com/noah-nuebling/mac-mouse-fix/releases/download/#{version}/MacMouseFixApp.zip",
+      verified: "github.com/noah-nuebling/mac-mouse-fix/"
+  name "Mouse Fix"
+  desc "Mouse utility to add gesture functions and smooth scrolling to 3rd party mice"
+  homepage "https://mousefix.org/"
+
+  auto_updates true
+
+  app "Mac Mouse Fix.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.nuebling.mac-mouse-fix",
+    "~/Library/LaunchAgents/com.nuebling.mac-mouse-fix.helper.plist",
+  ]
+end
