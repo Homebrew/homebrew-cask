284de3c61fcf3209f6750076761858e8f0b062d5
Vítor Galvão <info@vitorgalvao.com>
2021-03-04 16:52:44 +0000

    Update secure-pipes from 0.99.11,c67223c50be3604 to 0.99.11,c67223c50be3604

--- Casks/secure-pipes.rb
+++ Casks/secure-pipes.rb
@@ -1,6 +1,6 @@
 cask "secure-pipes" do
-  version "0.99.10,74ba1dcf08a3a62"
-  sha256 "4aad61805b83ca12077d09648cd787e093042240ee53f68a41a2254f94ae7c23"
+  version "0.99.11,c67223c50be3604"
+  sha256 "de8b6ad6c42509f2c394997694c13a161dcb4f74edfa87f5a63d7362897538f5"
 
   url "https://www.opoet.com/pyro/index.php/files/download/#{version.after_comma}"
   name "Secure Pipes"
