7857d7911ed38bfcf51eb1f7c975a8b741c431f4
Kid <44045911+kidonng@users.noreply.github.com>
2021-03-04 23:44:41 +0800

    outline-manager: add livecheck and zap entries (#100281)

--- Casks/outline-manager.rb
+++ Casks/outline-manager.rb
@@ -4,10 +4,21 @@ cask "outline-manager" do
 
   url "https://github.com/Jigsaw-Code/outline-server/releases/download/v#{version}/Outline-Manager.dmg",
       verified: "github.com/Jigsaw-Code/outline-server/"
-  appcast "https://github.com/Jigsaw-Code/outline-server/releases.atom"
   name "Outline Manager"
   desc "Tool to create and manage Outline servers, powered by Shadowsocks"
   homepage "https://www.getoutline.org/"
 
+  livecheck do
+    url "https://github.com/Jigsaw-Code/outline-releases/raw/master/manager/latest-mac.yml"
+    strategy :page_match
+    regex(/version: (\d+(?:\.\d+)*)/i)
+  end
+
   app "Outline Manager.app"
+
+  zap trash: [
+    "~/Library/Application Support/Outline Manager",
+    "~/Library/Preferences/com.electron.outline-manager.plist",
+    "~/Library/Saved Application State/com.electron.outline-manager.savedState",
+  ]
 end
