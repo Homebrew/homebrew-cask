6228e49a2f8561970998b492552ec7636af44b6c
Tony Dinh <gh@tdinh.me>
2021-03-04 21:30:58 +0800

    Add DevUtils.app

---
+++ Casks/devutils.rb
@@ -0,0 +1,11 @@
+cask "devutils" do
+  version :latest
+  sha256 :no_check
+
+  url "https://devutils.app/archives/DevUtils-#{version}.dmg"
+  name "DevUtils"
+  desc "Offline Toolbox for Developers"
+  homepage "https://devutils.app"
+
+  app "DevUtils.app"
+end
