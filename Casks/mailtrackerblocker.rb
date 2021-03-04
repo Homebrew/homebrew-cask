6f13b74328c59714e95dfc5fa90acdf6c3a61ca9
Aaron Lee <aaron@onefatgiraffe.com>
2021-03-05 00:43:37 +0900

    Add MailTrackerBlocker v0.3.16 (#100509)

---
+++ Casks/mailtrackerblocker.rb
@@ -0,0 +1,23 @@
+cask "mailtrackerblocker" do
+  version "0.3.16"
+  sha256 "feb3f217a6bf08423c7bcd2b4e97113384e1314123e37f57e2981a4b4d3cda46"
+
+  url "https://github.com/apparition47/MailTrackerBlocker/releases/download/#{version}/MailTrackerBlocker.pkg",
+      verified: "github.com/apparition47/MailTrackerBlocker/"
+  name "MailTrackerBlocker"
+  desc "Email tracker, read receipt and spy pixel blocker plugin for Apple Mail"
+  homepage "https://apparition47.github.io/MailTrackerBlocker/"
+
+  depends_on macos: ">= :el_capitan"
+
+  pkg "MailTrackerBlocker.pkg"
+
+  uninstall_postflight do
+    if system_command("ps", args: ["x"]).stdout.match?("Mail.app/Contents/MacOS/Mail")
+      opoo "Restart Mail.app to finish uninstalling #{token}"
+    end
+  end
+
+  uninstall pkgutil: "com.onefatgiraffe.mailtrackerblocker",
+            delete:  "/Library/Mail/Bundles/MailTrackerBlocker.mailbundle"
+end
