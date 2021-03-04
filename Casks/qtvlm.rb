1ce509f14b85308815d0dc25c6b5e7ffba9f00dc
Armand Fardeau <fardeauarmand@gmail.com>
2021-03-04 11:25:02 +0100

    Update qtvlm from 5.9.14-p2-3,277 to 5.9.14-p2-3,277

--- Casks/qtvlm.rb
+++ Casks/qtvlm.rb
@@ -1,6 +1,6 @@
 cask "qtvlm" do
-  version "5.9.14.p1.3,270"
-  sha256 "f0035418df85822276ac803a363519d0524013a0d4cb8fb369683df64301e419"
+  version "5.9.14-p2-3,277"
+  sha256 "2e0a21a21024559adbc179bca0c9a2437bded46ed99e6f085fb30e0f664718c4"
 
   url "https://www.meltemus.com/index.php/en/download/send/3-mac-os/#{version.after_comma}-qtvlm-#{version.before_comma.dots_to_hyphens}"
   appcast "https://www.meltemus.com/index.php/en/download/category/3-mac-os",
