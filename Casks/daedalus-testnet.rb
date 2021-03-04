9f2e9682ce4e650204eb11751fe351b04f0c1039
Vítor Galvão <info@vitorgalvao.com>
2021-03-04 13:40:49 +0000

    Update daedalus-testnet.rb

--- Casks/daedalus-testnet.rb
+++ Casks/daedalus-testnet.rb
@@ -1,13 +1,25 @@
+# typed: false
+# frozen_string_literal: true
+
 cask "daedalus-testnet" do
   version "4.0.0-FC1,16868"
   sha256 "44f4a34b8bc592231f84abb266a03a06ed5a529866ad19da940e1ae3012bb235"
 
   url "https://updates-cardano-testnet.s3.amazonaws.com/daedalus-#{version.before_comma}-testnet-#{version.after_comma}.pkg",
       verified: "updates-cardano-testnet.s3.amazonaws.com/"
-  appcast "https://updates-cardano-testnet.s3.amazonaws.com/daedalus-latest-version.json"
   name "Daedalus Testnet"
   desc "Cryptocurrency wallet for test ada on the Cardano Testnet blockchain"
   homepage "https://developers.cardano.org/en/testnets/cardano/get-started/wallet/"
+
+  livecheck do
+    url "https://updates-cardano-testnet.s3.amazonaws.com/daedalus-latest-version.json"
+
+    strategy :page_match do |page|
+      version = page.match(/"version":"(.+?)"/)
+      build = page.match(/-(\d+(?:\.\d+)*).pkg/)
+      "#{version[1]},#{build}"
+    end
+  end
 
   auto_updates true
   depends_on macos: ">= :high_sierra"
