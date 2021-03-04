36d6b8ef49e99d2e1b1a47f8c6cb5f10239e3bdf
Vítor Galvão <info@vitorgalvao.com>
2021-03-04 13:34:23 +0000

    yandex-disk.rb: remove version (#100684)

--- Casks/yandex-disk.rb
+++ Casks/yandex-disk.rb
@@ -1,13 +1,11 @@
 cask "yandex-disk" do
-  version "3.2.6"
+  version :latest
   sha256 :no_check
 
   url "https://disk.yandex.ru/download/Yandex.Disk.dmg/?instant=1"
   name "Yandex.Disk"
   desc "Cloud storage"
   homepage "https://disk.yandex.ru/"
-
-  auto_updates true
 
   app "Yandex.Disk.2.app"
 
