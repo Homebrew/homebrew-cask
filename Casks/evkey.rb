e189215942fe8717fd2a3845e9afb3d7c7b3c1b6
Miccal Matthews <miccal.matthews@gmail.com>
2021-03-04 09:22:07 +0800

    Update evkey.rb

--- Casks/evkey.rb
+++ Casks/evkey.rb
@@ -1,11 +1,11 @@
 cask "evkey" do
-  version "v3.3.6"
+  version "3.3.6"
   sha256 :no_check
 
   url "https://github.com/lamquangminh/EVKey/releases/download/Release/EVKeyMac.zip",
       verified: "github.com/lamquangminh/EVKey/"
   name "EVKey"
-  desc "Vietnamese Keyboard"
+  desc "Vietnamese keyboard"
   homepage "https://evkeyvn.com/"
 
   app "EVKey.app"
