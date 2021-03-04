38719d643d0109d1dcc77f10f183aaf8bda38e97
Alessandro (Ale) Segala <43508+ItalyPaleAle@users.noreply.github.com>
2021-03-04 23:40:36 +0000

    Reverted accidental changes during merge

--- Casks/github.rb
+++ Casks/github.rb
@@ -1,6 +1,6 @@
 cask "github" do
-  version "2.6.4-1864024f"
-  sha256 "9e9e99042752b954b67cca6c678010969cbba1553761794d25be85923ab554e1"
+  version "2.6.5-1995d7d8"
+  sha256 "f69916a4b8aa16ca6b6999a0d4c7375e0bf2d5b6c4451f495a7a32317e19b751"
 
   url "https://desktop.githubusercontent.com/releases/#{version}/GitHubDesktop.zip",
       verified: "githubusercontent.com/"
