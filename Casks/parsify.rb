71fa59f23a464fa3cb01d32d23e034196fcd0bac
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-03-04 20:40:52 +0900

    parsify: update to 1.6.0 (#100678)

--- Casks/parsify.rb
+++ Casks/parsify.rb
@@ -1,6 +1,6 @@
 cask "parsify" do
-  version "1.5.2"
-  sha256 "566f9e5332158f8339550d6eb29cad7fd6ad5b8a06b860908f9c24c304bffcac"
+  version "1.6.0"
+  sha256 "72841fce3810d80a6f0a5d7158e0335b0f7a9aa9c9ab2f10d8eaf9b8789df3aa"
 
   url "https://github.com/parsify-dev/desktop/releases/download/v#{version}/Parsify.Desktop-#{version}.dmg",
       verified: "github.com/parsify-dev/desktop/"
