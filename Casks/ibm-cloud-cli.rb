9a7b8b2c8d3221808a22db6aaca435492781e2ed
Markus Reiter <me@reitermark.us>
2021-03-04 15:21:48 +0100

    Fix `ibm-cloud-cli` uninstall. (#100672)
    * Fix `ibm-cloud-cli` uninstall.
    
    * Simplify `ibm-cloud-cli` uninstall.

--- Casks/ibm-cloud-cli.rb
+++ Casks/ibm-cloud-cli.rb
@@ -14,48 +14,10 @@ cask "ibm-cloud-cli" do
 
   pkg "IBM_Cloud_CLI_#{version}.pkg"
 
-  uninstall_postflight do
-    if File.exist?("/etc/profile")
-      system_command "/usr/bin/sed",
-                     args: [
-                       "-E",
-                       "-i", ".bluemix_uninstall_bak",
-                       "-e", "/^### Added by IBM Cloud CLI$/d",
-                       "-e", '/^source \/usr\/local\/Bluemix\/bx\/bash_autocomplete$/d',
-                       "/etc/profile"
-                     ],
-                     sudo: true
-    end
-
-    if File.exist?("#{ENV["HOME"]}/.bashrc")
-      system_command "/usr/bin/sed",
-                     args: [
-                       "-E",
-                       "-i", ".bluemix_uninstall_bak",
-                       "-e", "/^### Added by IBM Cloud CLI$/d",
-                       "-e", '/^source \/usr\/local\/Bluemix\/bx\/bash_autocomplete$/d',
-                       "#{ENV["HOME"]}/.bashrc"
-                     ]
-    end
-
-    if File.exist?("#{ENV["HOME"]}/.zshrc")
-      system_command "/usr/bin/sed",
-                     args: [
-                       "-E",
-                       "-i", ".bluemix_uninstall_bak",
-                       "-e", "/^### Added by IBM Cloud CLI$/d",
-                       "-e", '/^source \/usr\/local\/Bluemix\/bx\/zsh_autocomplete$/d',
-                       "#{ENV["HOME"]}/.zshrc"
-                     ]
-    end
-  end
-
   uninstall pkgutil: "com.ibm.cloud.cli",
             delete:  [
               "/usr/local/bin/bluemix",
               "/usr/local/bin/bx",
-              "/usr/local/bin/bluemix-analytics",
-              "/usr/local/Bluemix",
               "/usr/local/ibmcloud",
             ]
 
