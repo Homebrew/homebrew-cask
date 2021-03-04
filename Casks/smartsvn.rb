e991b9df357eade7d68f8c5f47dd7e5a8c47a7b9
Loïc PÉRON <44115467+nicerloop@users.noreply.github.com>
2021-03-04 13:16:19 +0100

    Add livecheck to smartsvn (#100677)

--- Casks/smartsvn.rb
+++ Casks/smartsvn.rb
@@ -3,10 +3,14 @@ cask "smartsvn" do
   sha256 "c38ddac8526a00ea297eb05bde20849e879867047e4833d7dc37c67ce0a3602a"
 
   url "https://www.smartsvn.com/downloads/smartsvn/smartsvn-macosx-#{version.dots_to_underscores}.dmg"
-  appcast "https://www.smartsvn.com/documents/smartsvn/changelog.txt"
   name "SmartSVN"
   desc "Subversion client"
   homepage "https://www.smartsvn.com/"
+
+  livecheck do
+    url "https://www.smartsvn.com/documents/smartsvn/changelog.txt"
+    regex(/SmartSVN\s+(\d+(?:\.\d+)*)/i)
+  end
 
   depends_on macos: ">= :el_capitan"
 
