014fb88c0ea7955d760720b0350e2ddbd6ef20f8
Jeff <31933684+upkit@users.noreply.github.com>
2021-03-05 23:34:54 +0800

    Update listen1 from 2.18.0 to 2.19.0 (#100741)

--- Casks/listen1.rb
+++ Casks/listen1.rb
@@ -1,7 +1,7 @@
 cask "listen1" do
   # NOTE: "1" is not a version number, but an intrinsic part of the product name
-  version "2.18.0"
-  sha256 "9107cd0f7fee525a7c32170fc73bb3f66ba3f96ffcad94b689717db5456168f1"
+  version "2.19.0"
+  sha256 "223dabacea168fd7bf860430f1c75d6e623a9eb1b7836585146a516c623a2783"
 
   url "https://github.com/listen1/listen1_desktop/releases/download/v#{version}/Listen1_#{version}_mac.dmg",
       verified: "github.com/listen1/listen1_desktop/"
