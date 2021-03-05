7ef5b1987502fa91bbc79249d3caaa4278ae278d
Alireza Asadollahi <aliasadollahi@aol.com>
2021-03-05 18:17:15 +0330

    Update gns3 from 2.2.18 to 2.2.19 (#100735)

--- Casks/gns3.rb
+++ Casks/gns3.rb
@@ -1,7 +1,7 @@
 cask "gns3" do
   # NOTE: "3" is not a version number, but an intrinsic part of the product name
-  version "2.2.18"
-  sha256 "05bb80947fd78978e61fa92fb4bf66cbc3208bc11e9d3ff4f0988299fc312cb8"
+  version "2.2.19"
+  sha256 "b601eeb28da0539a3324bdcfcdf1ae5452be211df4eeeec4fce6160460c05972"
 
   url "https://github.com/GNS3/gns3-gui/releases/download/v#{version}/GNS3-#{version}.dmg",
       verified: "github.com/GNS3/gns3-gui/"
