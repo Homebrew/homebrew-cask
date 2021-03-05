01f7b8276b3a4849869d98cbbddce8deb003ea57
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2021-03-04 21:03:24 -0800

    Update raze from 0.8.1 to 0.9.0

--- Casks/raze.rb
+++ Casks/raze.rb
@@ -1,6 +1,6 @@
 cask "raze" do
-  version "0.8.1"
-  sha256 "e17549f0626708f87f0f5dc2bc1f24ce587b31eb75026393c20ff0486bb1714a"
+  version "0.9.0"
+  sha256 "18e52a1ebc35ff3b14b35c3c39d0e05ebbed1814d1f3bf8690e62f30b3b427ac"
 
   url "https://github.com/coelckers/Raze/releases/download/#{version}_beta/raze-macos-#{version}.zip"
   name "Raze"
