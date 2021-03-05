8186d13f25ff02e5fc2c440bdad0f872d8da8af6
Jeppe Fihl-Pearson <jeppe@memrise.com>
2021-03-05 09:49:16 +0000

    Update insomnia from 2020.5.2 to 2021.1.0

--- Casks/insomnia.rb
+++ Casks/insomnia.rb
@@ -1,6 +1,6 @@
 cask "insomnia" do
-  version "2020.5.2"
-  sha256 "300845e30bc88fc2b9ed00cceb4b0f33a50b75bdd4247c41676aabe89e4588d2"
+  version "2021.1.0"
+  sha256 "d4380882c46015eabfc7bdcbf3eba614d2c14e887767b7705dad1deedce9b6b6"
 
   url "https://github.com/Kong/insomnia/releases/download/core%40#{version}/Insomnia.Core-#{version}.dmg",
       verified: "github.com/Kong/insomnia/"
