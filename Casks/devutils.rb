4c27c3a918f16103917c714f3f4057fbf096de23
Miccal Matthews <miccal.matthews@gmail.com>
2021-03-05 10:44:50 +0800

    Update devutils.rb

--- Casks/devutils.rb
+++ Casks/devutils.rb
@@ -1,8 +1,8 @@
 cask "devutils" do
-  version "1.8.1"
+  version "1.8.1,57"
   sha256 "4c1db289a972661d7c1eb5953e231de3b5b8b02d4b601d2f82845c8112bb774f"
 
-  url "https://devutils.app/archives/DevUtils-#{version}.dmg"
+  url "https://devutils.app/archives/DevUtils-#{version.before_comma}.dmg"
   name "DevUtils"
   desc "Offline toolbox for developers"
   homepage "https://devutils.app/"
