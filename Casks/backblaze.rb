f109dc4d340dc1a8ea6911a3ee9e60c842166d7c
Paul Mucur <mudge@mudge.name>
2021-03-06 01:15:07 +0000

    Add livecheck stanza to Backblaze (#100749)
    Replace the appcast stanza with an equivalent livecheck.

--- Casks/backblaze.rb
+++ Casks/backblaze.rb
@@ -3,11 +3,15 @@ cask "backblaze" do
   sha256 "45f4f94f7adf58666df79ba104c9fa2e6ca46c0c99859af15165406899f3490f"
 
   url "https://secure.backblaze.com/api/install_backblaze?file=bzinstall-mac-#{version}.zip"
-  appcast "https://secure.backblaze.com/api/clientversion.xml",
-          must_contain: "mac_version=\"#{version}\""
   name "Backblaze"
   desc "Data backup and storage service"
   homepage "https://backblaze.com/"
+
+  livecheck do
+    url "https://secure.backblaze.com/api/clientversion.xml"
+    strategy :page_match
+    regex(/mac_version=.*?(\d+(?:\.\d+)*)/i)
+  end
 
   auto_updates true
 
