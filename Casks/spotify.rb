10e1c93b4045c52dceabb446f48c3f660562dc88
Aleksandr Fedotov <mezonix@gmail.com>
2021-03-06 14:39:35 +0300

    Update spotify from 1.1.53.608.g7ed9c03a,1.1.53.608.g7ed9c03a-19 to 1.1.54.592.gc0b20638,1.1.54.592.gc0b20638-31 (#100781)

--- Casks/spotify.rb
+++ Casks/spotify.rb
@@ -1,5 +1,5 @@
 cask "spotify" do
-  version "1.1.53.608.g7ed9c03a,1.1.53.608.g7ed9c03a-19"
+  version "1.1.54.592.gc0b20638,1.1.54.592.gc0b20638-31"
   sha256 :no_check
 
   url "https://download.scdn.co/Spotify.dmg",
