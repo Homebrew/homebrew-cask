f3e6f29ec04726d90d1aa40f1961672f39f853b1
Marc Mogdanz <20729218+MarcMogdanz@users.noreply.github.com>
2021-03-06 14:28:28 +0100

    Update ledger-live from 2.21.3 to 2.23.0 (#100782)

--- Casks/ledger-live.rb
+++ Casks/ledger-live.rb
@@ -1,6 +1,6 @@
 cask "ledger-live" do
-  version "2.21.3"
-  sha256 "7b3ba82ed9a87599e49eef9c42903db5078fdaefc6bfb7ad8acdb0e71dcaf510"
+  version "2.23.0"
+  sha256 "33442db8661b8dda76244525409e03c24bdfff54b893cdde43eba7ef6668aa52"
 
   url "https://github.com/LedgerHQ/ledger-live-desktop/releases/download/v#{version}/ledger-live-desktop-#{version}-mac.dmg",
       verified: "github.com/LedgerHQ/ledger-live-desktop/"
