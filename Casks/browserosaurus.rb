a6d16a24038656cdfd7de6ee5b607ab8f4175322
Zhenfu Shi <szf1234@me.com>
2021-03-06 16:05:14 +0800

    Update browserosaurus from 13.2.0 to 13.3.2 (#100773)

--- Casks/browserosaurus.rb
+++ Casks/browserosaurus.rb
@@ -1,8 +1,8 @@
 cask "browserosaurus" do
-  version "13.2.0"
+  version "13.3.2"
 
   if Hardware::CPU.intel?
-    sha256 "17a3354e2ad6a9c7c9ba7714cc1a88d034d3b916d236fe27722ea2184a5390a7"
+    sha256 "49df861cf356a10d8fe22e29596257346f9e1dc1b6e64557e486bca9cd927cd3"
     url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-darwin-x64-#{version}.zip",
         verified: "github.com/will-stone/browserosaurus/"
   else
