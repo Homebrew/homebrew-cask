e10d673a7df52049853a5365437b9db55d1ced7a
Vítor Galvão <info@vitorgalvao.com>
2021-03-06 03:04:12 +0000

    Update knockknock.rb

--- Casks/knockknock.rb
+++ Casks/knockknock.rb
@@ -2,7 +2,8 @@ cask "knockknock" do
   version "2.3.0"
   sha256 "140f5f3c6d02541bed685810cb4c6916231fe2385ad749dd4856e7c9f2dcd05c"
 
-  url "https://github.com/objective-see/KnockKnock/releases/download/v#{version}/KnockKnock_#{version}.zip"
+  url "https://github.com/objective-see/KnockKnock/releases/download/v#{version}/KnockKnock_#{version}.zip",
+      verified: "github.com/objective-see/KnockKnock/"
   appcast "https://objective-see.com/products/changelogs/KnockKnock.txt"
   name "KnockKnock"
   desc "Tool to show what is persistently installed on the computer"
