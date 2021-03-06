53dedcfa8c9547e0d7823c1612ac5202bbdb656a
Reno <25192197+singularitti@users.noreply.github.com>
2021-03-06 18:53:01 -0500

    Update understand from 6.0.1053 to 6.0.1056 (#100801)

--- Casks/understand.rb
+++ Casks/understand.rb
@@ -1,6 +1,6 @@
 cask "understand" do
-  version "6.0.1053"
-  sha256 "cbd5bce42e600e15dfb10100a88cef21fe99d97fe19c81812d123a0fd604f597"
+  version "6.0.1056"
+  sha256 "bc6186e3530e9adc0cd17ae81ebab375fec16e5221f69c37c18c3bac6623d724"
 
   url "http://builds.scitools.com/all_builds/b#{version.patch}/Understand/Understand-#{version}-MacOSX-x86.dmg"
   appcast "https://licensing.scitools.com/download/getChecksums/Understand/#{version.patch}"
