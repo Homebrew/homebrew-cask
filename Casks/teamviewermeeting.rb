a7bfe1e11457de63ec822373bb99b6e7c88d86dc
Adithya Sunil <adithyasunil26@gmail.com>
2021-03-07 22:01:57 +0400

    Update teamviewermeeting from 15.14.2 to 15.15.3 (#100888)

--- Casks/teamviewermeeting.rb
+++ Casks/teamviewermeeting.rb
@@ -1,6 +1,6 @@
 cask "teamviewermeeting" do
-  version "15.14.2"
-  sha256 "5225a225c592e4051a84138d0ea0208c319adc2668abd828c45a91886e8a4ccc"
+  version "15.15.3"
+  sha256 "fc34f148d2154b227fc25031f1ed48177f8e50d5554de9feb78da4224e1a9952"
 
   url "https://dl.teamviewer.com/teamviewermeeting/installer/mac/#{version}/TeamViewerMeeting.dmg"
   name "TeamViewerMeeting"
