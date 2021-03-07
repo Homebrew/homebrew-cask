b2b0680ba4fcb0b68b1711a3b0eded4105fd161d
i0ntempest <szf1234@me.com>
2021-03-07 15:19:28 +0800

    Update netiquette from 1.1.1 to 1.2.0

--- Casks/netiquette.rb
+++ Casks/netiquette.rb
@@ -1,9 +1,8 @@
 cask "netiquette" do
-  version "1.1.1"
-  sha256 "974d1842a9cadd39ae6883f132625c95ab3132fb86ffb141c7146a738a3f83f3"
+  version "1.2.0"
+  sha256 "08795827772384abcc4426d63c44f21fbc9e75d3595ed975a559a68dac6019b9"
 
-  url "https://bitbucket.org/objective-see/deploy/downloads/Netiquette_#{version}.zip",
-      verified: "bitbucket.org/objective-see/"
+  url "https://github.com/objective-see/Netiquette/releases/download/v#{version}/Netiquette_#{version}.zip"
   appcast "https://objective-see.com/products/changelogs/Netiquette.txt"
   name "Netiquette"
   homepage "https://objective-see.com/products/netiquette.html"
