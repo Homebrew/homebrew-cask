78cac560bed45fa5592e2cf303b5d99739e7fda2
Leopoldo Pla <lpla@dlsi.ua.es>
2021-03-07 14:26:38 +0100

    Recompiled release differs previous shasum in sameboy 0.14.2 (#100820)

--- Casks/sameboy.rb
+++ Casks/sameboy.rb
@@ -1,6 +1,6 @@
 cask "sameboy" do
   version "0.14.2"
-  sha256 "c975e41b5558ac94523a6b73cb1d0e4d5390e5cbdaff5e3c23bd79ad91f42ffb"
+  sha256 "bd8ca989f4203562838612e5ef5239221c28e9821734dc885c980b5e5b38f72a"
 
   url "https://github.com/LIJI32/SameBoy/releases/download/v#{version}/sameboy_cocoa_v#{version}.zip",
       verified: "github.com/LIJI32/SameBoy/"
