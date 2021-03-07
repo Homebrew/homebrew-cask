656975b59ffb503cddd0151be1a73981dcfdb184
Adithya Sunil <adithyasunil26@gmail.com>
2021-03-07 22:05:20 +0400

    Update thebrain from 11.0.132.0 to 11.0.137.0 (#100891)

--- Casks/thebrain.rb
+++ Casks/thebrain.rb
@@ -1,6 +1,6 @@
 cask "thebrain" do
-  version "11.0.132.0"
-  sha256 "a92e40a9f8f766ec514df4d85d0fd3bc799169f2e56267113c60a24fdb246305"
+  version "11.0.137.0"
+  sha256 "67ec72cfc2166ffa9e19cebdd709c106aa52fef23eb8e58a106a424542d2955b"
 
   url "https://updater.thebrain.com/files/TheBrain#{version}.dmg"
   name "TheBrain"
