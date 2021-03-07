2647306e080c4fc6cdc43ec9e533517e95adc5d7
Adithya Sunil <adithyasunil26@gmail.com>
2021-03-07 21:22:02 +0400

    Update preform from 3.12.3,794 to 3.13.0,811 (#100855)

--- Casks/preform.rb
+++ Casks/preform.rb
@@ -1,6 +1,6 @@
 cask "preform" do
-  version "3.12.3,794"
-  sha256 "4cad228c0b163691ef829ba81a075a3fc9d094066a59e936ca7ef591d7a0caee"
+  version "3.13.0,811"
+  sha256 "ce9829ebaf02b27dfddefb88d688f957aed07e9698460cd6717665153a623b88"
 
   url "https://s3.amazonaws.com/FormlabsReleases/Release/#{version.before_comma}/PreForm_#{version.before_comma}_release__build_#{version.after_comma}.dmg",
       verified: "s3.amazonaws.com/FormlabsReleases/"
