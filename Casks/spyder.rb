fd4d1f8b2798dcc696b4f53a2adcc5362266fe46
Adithya Sunil <adithyasunil26@gmail.com>
2021-03-07 21:35:36 +0400

    Update spyder from 4.2.2 to 4.2.3 (#100866)

--- Casks/spyder.rb
+++ Casks/spyder.rb
@@ -1,6 +1,6 @@
 cask "spyder" do
-  version "4.2.2"
-  sha256 "2731a337199d8b30f03ee573602a4563d91637881e079c377d9ece127c9f4cd5"
+  version "4.2.3"
+  sha256 "e8b8477002927a8fd293d7d15cc10b0b96bb2d2b24641c56fa6b39d472b8e9a5"
 
   url "https://github.com/spyder-ide/spyder/releases/download/v#{version}/Spyder.dmg",
       verified: "github.com/spyder-ide/spyder/"
