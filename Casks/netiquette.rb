0283126eacb0941dd5724a295941ef6b1a43a1a0
Zhenfu Shi <szf1234@me.com>
2021-03-07 15:21:00 +0800

    Update netiquette.rb

--- Casks/netiquette.rb
+++ Casks/netiquette.rb
@@ -2,7 +2,8 @@ cask "netiquette" do
   version "1.2.0"
   sha256 "08795827772384abcc4426d63c44f21fbc9e75d3595ed975a559a68dac6019b9"
 
-  url "https://github.com/objective-see/Netiquette/releases/download/v#{version}/Netiquette_#{version}.zip"
+  url "https://github.com/objective-see/Netiquette/releases/download/v#{version}/Netiquette_#{version}.zip",
+      verified: "github.com/objective-see/"
   appcast "https://objective-see.com/products/changelogs/Netiquette.txt"
   name "Netiquette"
   homepage "https://objective-see.com/products/netiquette.html"
