480ebab220e895bf5f86b25f0db2bd84de2ce5c7
Paul Pavlo Buidenkov <pavlobu@gmail.com>
2021-03-08 15:55:58 +0200

    Update deskreen from 1.0.10 to 1.0.11 (#100924)

--- Casks/deskreen.rb
+++ Casks/deskreen.rb
@@ -1,6 +1,6 @@
 cask "deskreen" do
-  version "1.0.10"
-  sha256 "25373f4019a330701d08b595aa64d48732c724736343494ee8b3e7c079186a2b"
+  version "1.0.11"
+  sha256 "48904100e19c86a06e533facec525516a6a90ae6733946deb14753ef38a777f1"
 
   url "https://github.com/pavlobu/deskreen/releases/download/v#{version}/Deskreen-#{version}.dmg",
       verified: "https://github.com/pavlobu/deskreen/"
