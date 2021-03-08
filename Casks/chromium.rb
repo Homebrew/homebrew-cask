013613b5ff841daeb799d42f80c700b2aade3b5c
Miccal Matthews <miccal.matthews@gmail.com>
2021-03-08 18:06:13 +0800

    Update chromium from 859326 to 860657 (#100917)

--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -1,6 +1,6 @@
 cask "chromium" do
-  version "859326"
-  sha256 "cd0a95615c8ab03be727005cf9745fdab360fd310478455c857f0c6adbba813b"
+  version "860657"
+  sha256 "f78de95d673e4f2dadf3423cb7013eaee5db607bb9fe34c427f80fbf1672371d"
 
   url "https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/#{version}/chrome-mac.zip",
       verified: "commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/"
