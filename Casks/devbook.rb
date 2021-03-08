93aa7d2d61866410c4363da031749158813df182
Miccal Matthews <miccal.matthews@gmail.com>
2021-03-08 17:31:02 +0800

    Update devbook.rb

--- Casks/devbook.rb
+++ Casks/devbook.rb
@@ -1,5 +1,5 @@
 cask "devbook" do
-  version :latest
+  version "0.1.11"
   sha256 :no_check
 
   url "https://download.usedevbook.com/mac/dmg/x64"
@@ -7,9 +7,13 @@ cask "devbook" do
   desc "Search engine for developers"
   homepage "https://usedevbook.com/"
 
+  livecheck do
+    url "https://download.todesktop.com/2102273jsy18baz/latest-mac.yml"
+    strategy :page_match
+    regex(/version:.(\d+(?:\.\d+)*)/)
+  end
+
   app "Devbook.app"
 
-  zap trash: [
-    "~/Library/Application Support/com.foundrylabs.devbook/*",
-  ]
+  zap trash: "~/Library/Application Support/com.foundrylabs.devbook/*"
 end
