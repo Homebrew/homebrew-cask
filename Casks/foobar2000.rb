8f26f99706610d7ceb8403ca468756c7aad10706
Alex Kirhenshtein <alk@netxms.org>
2021-03-08 13:13:49 +0200

    Add foobar2000.app v2.2.16

---
+++ Casks/foobar2000.rb
@@ -0,0 +1,16 @@
+cask "foobar2000" do
+  version "2.2.16"
+  sha256 "7e8a18395c96ce0337ffc36592d06f63e6817b15cb95c4de0255b3b6054aa8a4"
+
+  url "https://www.foobar2000.org/files/foobar2000-v#{version}.dmg"
+  name "foobar2000"
+  desc "Advanced freeware audio player"
+  homepage "https://www.foobar2000.org/mac"
+
+  app "foobar2000.app"
+
+  zap trash: [
+    "~/Library/foobar2000",
+    "~/Library/Preferences/com.foobar2000.mac.plist",
+  ]
+end
