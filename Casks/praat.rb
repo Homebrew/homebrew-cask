454cff4811aa199343e4cbb6de7182cf7242cd4f
J-rg <j_rg@live.de>
2021-03-08 10:42:12 +0100

    Update praat from 6.1.38 to 6.1.40 (#100815)
    * Update praat from 6.1.38 to 6.1.40
    
    * Update praat.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/praat.rb
+++ Casks/praat.rb
@@ -1,13 +1,13 @@
 cask "praat" do
-  version "6.1.38"
-  sha256 "6c72f96b8b79ecf07370eead97051c97cc666fc53af883ece4df8416f090a374"
+  version "6.1.40"
+  sha256 "96b2e57b136e3695af34417ca00efd60ff1d6b7b64675954ebd586ae25fbae56"
 
   url "https://github.com/praat/praat/releases/download/v#{version}/praat#{version.no_dots}_mac.dmg",
       verified: "github.com/praat/praat/"
   appcast "https://github.com/praat/praat/releases.atom"
   name "Praat"
   desc "Doing phonetics by computer"
-  homepage "http://www.fon.hum.uva.nl/praat/"
+  homepage "https://www.fon.hum.uva.nl/praat/"
 
   app "Praat.app"
   binary "#{appdir}/Praat.app/Contents/MacOS/Praat", target: "praat"
