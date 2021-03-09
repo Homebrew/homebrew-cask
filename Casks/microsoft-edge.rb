28948a28409ce11be56bdb55b6a4c2cbf1c0805e
César Román <thecesrom@gmail.com>
2021-03-08 19:32:26 -0800

    Update microsoft-edge from 89.0.774.45 to 89.0.774.48 (#100934)

--- Casks/microsoft-edge.rb
+++ Casks/microsoft-edge.rb
@@ -1,6 +1,6 @@
 cask "microsoft-edge" do
-  version "89.0.774.45"
-  sha256 "77669cf2b9f762783bcdcb3027a0cde7474f9fb59039fedd496e1fb5fbf82d01"
+  version "89.0.774.48"
+  sha256 "86c0e69777be1a6409f5acc552bc824c08654d5b9b3a0e0c572893d344adbda3"
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/03ADF619-38C6-4249-95FF-4A01C0FFC962/MacAutoupdate/MicrosoftEdge-#{version}.pkg",
       verified: "officecdn-microsoft-com.akamaized.net/"
