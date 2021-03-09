47538ad148d7b97be8b25429f2c47286f0efc5a5
王仕杰 <wangshijie.init@bytedance.com>
2021-03-09 11:49:48 +0800

    Update Feishu.app to v3.43.5

--- Casks/feishu.rb
+++ Casks/feishu.rb
@@ -1,9 +1,9 @@
 cask "feishu" do
-  version "3.39.2,1536bc"
-  sha256 "4ade0db19ccabe6a0e797c29193ecf6d2efa51d475311176f9fce792798c451f"
+  version "3.43.5"
+  sha256 "c7d192ba13a687bf62ed0b042fee18fe9291fc37a13d548c52e318f12a2d3034"
 
-  url "https://sf3-ttcdn-tos.pstatp.com/obj/ee-appcenter/#{version.after_comma}/Feishu-#{version.before_comma}.dmg",
-      verified: "sf3-ttcdn-tos.pstatp.com/"
+  url "https://sf3-eecdn-tos.pstatp.com/obj/ee-appcenter/f94a8c/feishu-mac-#{version}.dmg",
+      verified: "sf3-eecdn-tos.pstatp.com/"
   appcast "https://www.feishu.cn/api/downloads"
   name "feishu"
   desc "Project management software"
