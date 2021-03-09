33ee592b67a4e84e35645818dc01a2571986d2d7
funjon <1018949+funjon@users.noreply.github.com>
2021-03-08 22:40:31 -0800

    Update cask OmniGraffle to 7.18.4 (#100942)

--- Casks/omnigraffle.rb
+++ Casks/omnigraffle.rb
@@ -8,8 +8,8 @@ cask "omnigraffle" do
     sha256 "83ef24af2dbd7977b9922e992f17f23e102562f0589d28bc37d5579b4a4d4938"
     url "https://downloads.omnigroup.com/software/MacOSX/10.13/OmniGraffle-#{version}.dmg"
   else
-    version "7.18.3"
-    sha256 "b4528cdcdac77dd4ac5e755b7b94ed2271769fbe2e7f8577d903e5c1f9c96049"
+    version "7.18.4"
+    sha256 "6ff08d59cdcffe9399858a4679fc01c2d8d802d17fbb97cb114d5f2f61c55891"
     url "https://downloads.omnigroup.com/software/MacOSX/10.14/OmniGraffle-#{version}.dmg"
   end
 
