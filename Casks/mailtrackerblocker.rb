546ca739af4db99bacffc1fb147cb3a94b1b24cf
Aaron Lee <aaron@onefatgiraffe.com>
2021-03-09 22:53:23 +0900

    Update to 0.3.19

--- Casks/mailtrackerblocker.rb
+++ Casks/mailtrackerblocker.rb
@@ -1,6 +1,6 @@
 cask "mailtrackerblocker" do
-  version "0.3.18"
-  sha256 "d867d06b506b3770819cfd7d922857a6054421d8b608e6616bbb685d0feb90b7"
+  version "0.3.19"
+  sha256 "4e09ee2b58305d320e032cc13b3489356f49fc8b0d2ddab19dcea7d02807d2dd"
 
   url "https://github.com/apparition47/MailTrackerBlocker/releases/download/#{version}/MailTrackerBlocker.pkg",
       verified: "github.com/apparition47/MailTrackerBlocker/"
