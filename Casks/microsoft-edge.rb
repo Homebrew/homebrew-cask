38d5b59bf2ea2dd4b0ca9fc39839a10795639c13
César Román <thecesrom@gmail.com>
2021-03-08 19:25:33 -0800

    use Universal pkg

--- Casks/microsoft-edge.rb
+++ Casks/microsoft-edge.rb
@@ -1,18 +1,9 @@
 cask "microsoft-edge" do
   version "89.0.774.48"
+  sha256 "86c0e69777be1a6409f5acc552bc824c08654d5b9b3a0e0c572893d344adbda3"
 
-  if Hardware::CPU.intel?
-    sha256 "d2682f097aabe82dac937f7134e48658bf599184ad577bb66dde236f46a35108"
-
-    url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdge-#{version}.pkg",
-        verified: "officecdn-microsoft-com.akamaized.net/"
-  else
-    sha256 "86c0e69777be1a6409f5acc552bc824c08654d5b9b3a0e0c572893d344adbda3"
-
-    url "https://officecdn-microsoft-com.akamaized.net/pr/03ADF619-38C6-4249-95FF-4A01C0FFC962/MacAutoupdate/MicrosoftEdge-#{version}.pkg",
-        verified: "officecdn-microsoft-com.akamaized.net/"
-  end
-
+  url "https://officecdn-microsoft-com.akamaized.net/pr/03ADF619-38C6-4249-95FF-4A01C0FFC962/MacAutoupdate/MicrosoftEdge-#{version}.pkg",
+      verified: "officecdn-microsoft-com.akamaized.net/"
   name "Microsoft Edge"
   desc "Multi-platform web browser"
   homepage "https://www.microsoft.com/edge"
