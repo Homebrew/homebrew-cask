218d26140b0520b7e36c335aed60a796fb0d6c76
Kostiantyn Boichenko <box@inernetbs.net>
2021-03-09 23:02:41 +0200

    Update VeePN.app v2.0.1

--- Casks/veepn.rb
+++ Casks/veepn.rb
@@ -3,12 +3,22 @@ cask "veepn" do
   sha256 "6aa503a97f9520d6047159299cf4d97e56fcce68ca85deaf1ad9e14c48869a7c"
 
   url "https://download.veepn.com/mac/VeePN_v#{version}.dmg"
-  appcast "https://download.veepn.com/mac/appcast.xml"
   name "VeePN"
-  desc "VPN client for secure internet access and private browsing"
+  desc "VPN client"
   homepage "https://veepn.com/vpn-apps/vpn-for-mac/"
+
+  livecheck do
+    url "https://download.veepn.com/mac/appcast.xml"
+    strategy :sparkle
+  end
 
   app "VeePN.app"
 
   uninstall launchctl: "com.veepn.macos.helper"
+
+  zap trash: [
+    "~/Library/Application Support/com.veepn.macos.direct",
+    "~/Library/Preferences/com.veepn.macos.direct.plist",
+    "/Library/PrivilegedHelperTools/com.veepn.macos.helper",
+  ]
 end
