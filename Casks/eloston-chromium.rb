8c6d0a83bf9672da732a5bf846c4f42079c1c7f7
frodera <219724+frodera@users.noreply.github.com>
2021-03-10 07:11:14 +1100

    Update eloston-chromium from 88.0.4324.192-1.1 to 89.0.4389.82-1.1 (arm64) (#100945)

--- Casks/eloston-chromium.rb
+++ Casks/eloston-chromium.rb
@@ -5,8 +5,8 @@ cask "eloston-chromium" do
     sha256 "ae38a71f70801e3caefff2a4e545c726aafd10ad7ca8d1ed1cc7b0ddacd89f90"
   else
     arch = "arm64"
-    version "88.0.4324.192-1.1"
-    sha256 "0fc60da4c8f67b87f0582cf4fe796f7ece55dad3a612ad0352a39c4deba5d365"
+    version "89.0.4389.82-1.1"
+    sha256 "867169979310eecfb547f8966889e4b2703eba7db2f17d85677d771416a3e115"
   end
 
   url "https://github.com/kramred/ungoogled-chromium-macos/releases/download/#{version}_#{arch}/ungoogled-chromium_#{version}_#{arch}-macos.dmg",
