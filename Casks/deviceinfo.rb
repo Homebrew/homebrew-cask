0acb223fffbc2c9f973d3d769ba17a1edc83e6fd
CoreNion <coreinion@gmail.com>
2021-03-10 22:04:58 +0900

    FIx Download url

--- Casks/deviceinfo.rb
+++ Casks/deviceinfo.rb
@@ -2,7 +2,7 @@ cask "deviceinfo" do
   version "1.0"
   sha256 "0387e3d5a12027d807bc8e84d11c8ae062fef56ca1bbcbba65e0ecc83989ed5e"
 
-  url "https://corenion.github.io/DeviceInfo/v#{version}/DeviceInfo_mac.zip"
+  url "https://github.com/CoreNion/DeviceInfo/releases/download/v#{version}/DeviceInfo.dmg"
   name "DeviceInfo"
   desc "Display device information"
   homepage "https://github.com/CoreNion/DeviceInfo/"
