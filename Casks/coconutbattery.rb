74add86cd8327a2c622e4f37916186abedc2167c
John Benninghoff <jbenninghoff@mac.com>
2021-03-10 08:56:32 -0600

    Update coconutbattery from 3.9.4,af177845 to 3.9.4,c6ea149f (#100982)

--- Casks/coconutbattery.rb
+++ Casks/coconutbattery.rb
@@ -5,8 +5,8 @@ cask "coconutbattery" do
 
     url "https://www.coconut-flavour.com/downloads/coconutBattery_#{version}.zip"
   else
-    version "3.9.4,af177845"
-    sha256 "2ae8df380e1af466653c4d60eb69c05a2ef41c17a8116d2d5da17ad0f827e8b4"
+    version "3.9.4,c6ea149f"
+    sha256 "bd9ffbcb0ecf76ac0c80f24a17d35aaea8fd4dc6b5c36d347650fc5dbb5916a4"
 
     url "https://www.coconut-flavour.com/downloads/coconutBattery_#{version.before_comma.no_dots}_#{version.after_comma}.zip"
 
