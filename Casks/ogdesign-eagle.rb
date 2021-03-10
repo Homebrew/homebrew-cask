a4d181cdf148f5bfcff87d94e6a4c11ee27acf91
prin <printempw@gmail.com>
2021-03-10 20:08:45 +0800

    Update ogdesign-eagle from 2.0,2 to 2.0,8 and add M1 build (#100962)

--- Casks/ogdesign-eagle.rb
+++ Casks/ogdesign-eagle.rb
@@ -1,11 +1,19 @@
 cask "ogdesign-eagle" do
-  version "2.0,2"
-  sha256 "6f61e27f0cdae9c5ea1b08b6b97ec9d879306306f357299e7d0502978ac87257"
+  version "2.0,8"
 
-  url "https://eagleapp.s3-accelerate.amazonaws.com/releases/Eagle-#{version.before_comma}-build#{version.after_comma}.dmg",
-      verified: "eagleapp.s3-accelerate.amazonaws.com/"
+  if Hardware::CPU.intel?
+    sha256 "fd487e03ab89bc85d6a9dff65286f8e2b20a91cfd66d2dfe20624536505af891"
+    url "https://eagleapp.s3-accelerate.amazonaws.com/releases/Eagle-#{version.before_comma}-build#{version.after_comma}.dmg",
+        verified: "eagleapp.s3-accelerate.amazonaws.com/"
+  else
+    sha256 "66d1bcd58ec4efd2426ee40aefe834fa2e367f40e90b798f4625123a8967cef6"
+    url "https://eagleapp.s3-accelerate.amazonaws.com/releases/Eagle-#{version.before_comma}-M1-build#{version.after_comma}.dmg",
+        verified: "eagleapp.s3-accelerate.amazonaws.com/"
+  end
+
   appcast "https://eagle.cool/changelog"
   name "Eagle"
+  desc "Organize all your reference images in one place"
   homepage "https://eagle.cool/macOS"
 
   app "Eagle.app"
