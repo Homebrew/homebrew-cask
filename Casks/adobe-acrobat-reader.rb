18bef1dbd27724b1d0f55bf2de034643fb8f8ee4
Loïc PÉRON <44115467+nicerloop@users.noreply.github.com>
2021-03-10 23:04:13 +0100

    Update adobe-acrobat-reader from 21.001.20140 to 21.001.20145 (#100992)

--- Casks/adobe-acrobat-reader.rb
+++ Casks/adobe-acrobat-reader.rb
@@ -1,6 +1,6 @@
 cask "adobe-acrobat-reader" do
-  version "21.001.20140"
-  sha256 "41180f211002f7eda70219aff266cb44db7adec38a6eb4ddc8e1689e6c9ec57f"
+  version "21.001.20145"
+  sha256 "c103f106a22027d7e1bfcb39a8bebce014ddd779818991f471ebece246ebd39f"
 
   url "https://ardownload2.adobe.com/pub/adobe/reader/mac/AcrobatDC/#{version.no_dots}/AcroRdrDC_#{version.no_dots}_MUI.dmg"
   appcast "https://www.adobe.com/devnet-docs/acrobatetk/tools/ReleaseNotesDC/index.html"
