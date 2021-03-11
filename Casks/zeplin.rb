1773230a446dd345d86357213c8bffed477656c6
Dareth NHANG <DSGND@users.noreply.github.com>
2021-03-12 00:06:38 +0100

    Update zeplin from 3.15,1063 to 3.16,1134 (#101006)

--- Casks/zeplin.rb
+++ Casks/zeplin.rb
@@ -1,12 +1,16 @@
 cask "zeplin" do
-  version "3.15,1063"
+  version "3.16,1134"
   sha256 :no_check
 
   url "https://api.zeplin.io/urls/download-mac"
-  appcast "https://api.appcenter.ms/v0.1/public/sparkle/apps/8926efff-e734-b6d3-03d0-9f41d90c34fc"
   name "Zeplin"
   desc "Share, organize and collaborate on designs"
   homepage "https://zeplin.io/"
+
+  livecheck do
+    url "https://api.appcenter.ms/v0.1/public/sparkle/apps/8926efff-e734-b6d3-03d0-9f41d90c34fc"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :sierra"
