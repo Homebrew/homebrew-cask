bf9757c12cf5b93268157382365f936dfb506a78
Markus Reiter <me@reitermark.us>
2021-03-11 17:17:53 +0100

    Update zeplin.rb

--- Casks/zeplin.rb
+++ Casks/zeplin.rb
@@ -3,10 +3,14 @@ cask "zeplin" do
   sha256 :no_check
 
   url "https://api.zeplin.io/urls/download-mac"
-  appcast "https://api.appcenter.ms/v0.1/public/sparkle/apps/8926efff-e734-b6d3-03d0-9f41d90c34fc"
   name "Zeplin"
   desc "Share, organize and collaborate on designs"
   homepage "https://zeplin.io/"
+
+  livecheck do
+    url "https://api.appcenter.ms/v0.1/public/sparkle/apps/8926efff-e734-b6d3-03d0-9f41d90c34fc"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :sierra"
