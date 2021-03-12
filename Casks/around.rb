69517eefe9b5d8d4a16c0689041e8729d2fa999f
Claudio Spiess <claudiosv@users.noreply.github.com>
2021-03-12 10:28:06 -0500

    Add LiveCheck and set version stanza for Around cask

--- Casks/around.rb
+++ Casks/around.rb
@@ -1,11 +1,16 @@
 cask "around" do
-  version :latest
+  version "0.47.10"
   sha256 :no_check
 
   url "https://downloads.around.co/Around-mac-installer.zip"
   name "Around"
   desc "Video calls designed for energy, ideas and action"
   homepage "https://www.around.co/"
+
+  livecheck do
+    url "https://meet.around.co/versions.json"
+    regex(/"desktopappMinVersion":"(\d\.\d{2}\.\d{2})"/i)
+  end
 
   installer manual: "Install Around.app"
 
