61814281d0dc9fadc80894acdad444410fba9196
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-03-12 21:49:58 +0900

    electerm: update to 1.12.2 (#101050)

--- Casks/electerm.rb
+++ Casks/electerm.rb
@@ -1,6 +1,6 @@
 cask "electerm" do
-  version "1.12.1"
-  sha256 "9539486fc4ffd533ce76f65038e63e4ad6a07f3a2cec2e91f3128927e52e9b19"
+  version "1.12.2"
+  sha256 "ecaeb903a29f2325bcda04572bc1f78d6301200d5d6329488a9e70ecb5113435"
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac.dmg"
   appcast "https://github.com/electerm/electerm/releases.atom"
