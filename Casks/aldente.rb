b1ecb99cc568c8504c466314d570d43f0063c939
davidwernhart <david.wernhart96@gmail.com>
2021-03-12 11:47:08 +0100

    Update aldente.rb

--- Casks/aldente.rb
+++ Casks/aldente.rb
@@ -1,9 +1,8 @@
 cask "aldente" do
-
   version "2.1.2"
   sha256 "39cebc2db5b75c4217abd42470ecfee8280a35178b0d2581df3857cd8b742880"
+  
   url "https://github.com/davidwernhart/AlDente/releases/download/#{version}/AlDente_#{version}_Notarized.app.zip"
-
   appcast "https://github.com/davidwernhart/AlDente/releases.atom"
   name "AlDente"
   desc "Menu bar tool to limit maximum charging percentage"
