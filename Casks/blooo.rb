e2d7a72e4e2224ce93b1703f42644f709ba4bb03
Hannes Braun <hannesbraun@mail.de>
2021-03-13 19:08:31 +0100

    blooo: add livecheck (#101079)

--- Casks/blooo.rb
+++ Casks/blooo.rb
@@ -3,10 +3,15 @@ cask "blooo" do
   sha256 "e795e71ac0d33f9c4dfa5d96a113ddc5376a88ddab598751cd625003578c5fbd"
 
   url "https://www.fullbucket.de/music/dl/blooo_#{version.dots_to_underscores}_mac.pkg"
-  appcast "https://www.fullbucket.de/music/blooo.html"
   name "the blooo"
   desc "Polyphonic synthesizer"
   homepage "https://www.fullbucket.de/music/blooo.html"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(/v(\d+(?:\.\d+)*)/)
+  end
 
   pkg "blooo_#{version.dots_to_underscores}_mac.pkg"
 
