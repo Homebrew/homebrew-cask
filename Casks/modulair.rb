997b42a9ca7b3aac1c088672b08b3411cacd4137
Hannes Braun <hannesbraun@mail.de>
2021-03-13 19:08:23 +0100

    modulair: add livecheck (#101074)

--- Casks/modulair.rb
+++ Casks/modulair.rb
@@ -3,10 +3,15 @@ cask "modulair" do
   sha256 "fe1818a43f1756e61054c9fcb054a285c0622a1342e1016f5996837b382d8460"
 
   url "https://www.fullbucket.de/music/dl/modulair_#{version.dots_to_underscores}_mac.pkg"
-  appcast "https://www.fullbucket.de/music/modulair.html"
   name "ModulAir"
   desc "Polyphonic modular synthesizer"
   homepage "https://www.fullbucket.de/music/modulair.html"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(/v(\d+(?:\.\d+)*)/)
+  end
 
   pkg "modulair_#{version.dots_to_underscores}_mac.pkg"
 
