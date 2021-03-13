e31abe8f0c5844528a680e818a9ebbca4f471c9c
Indexyz <jiduye@gmail.com>
2021-03-14 02:08:20 +0800

    Update vofa-plus from 1.3.8 to 1.3.9 (#101071)

--- Casks/vofa-plus.rb
+++ Casks/vofa-plus.rb
@@ -1,9 +1,9 @@
 cask "vofa-plus" do
-  version "1.3.8"
-  sha256 "73c590cbe3c69a41464b0739abe5a1064b7ffe3bc7194f774a114cd0fbc07bab"
+  version "1.3.9"
+  sha256 "e1e94e63abf346f6857fb5bd6dabc756666ea7ec57cf74bcb808046a3727dca2"
 
-  url "https://gutega.gitee.io/vofa_plus_res/downloads/vofa+_#{version}_amd64.dmg",
-      verified: "gutega.gitee.io/"
+  url "https://je00.github.io/downloads/vofa+_#{version}_amd64.dmg",
+      verified: "je00.github.io/"
   name "VOFA+"
   desc "Serial port console that is driven by the plugin"
   homepage "https://www.vofa.plus/"
