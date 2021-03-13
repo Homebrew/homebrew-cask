51fe6851ed92589b50f5b5078131a1c1ee2e33dd
Jeff <31933684+upkit@users.noreply.github.com>
2021-03-13 23:20:28 +0800

    Update lx-music from 1.8.1 to 1.8.2 (#101078)

--- Casks/lx-music.rb
+++ Casks/lx-music.rb
@@ -1,6 +1,6 @@
 cask "lx-music" do
-  version "1.8.1"
-  sha256 "27cbe78d658a8447e47a59a5d795d6a6ab42eb1d6a76b71da00c681faf8b2b6f"
+  version "1.8.2"
+  sha256 "08f73f7492b56879d6c609dda120354cd42169228f7cd636f9dde462ceeefc6e"
 
   url "https://github.com/lyswhut/lx-music-desktop/releases/download/v#{version}/lx-music-desktop-#{version}.dmg"
   appcast "https://github.com/lyswhut/lx-music-desktop/releases.atom"
