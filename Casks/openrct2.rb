6adfdc226195363e7238c721f967f6673f4b97cd
Eric Knibbe <enk3@outlook.com>
2021-03-13 22:14:30 -0500

    Update openrct2 to 0.3.3

--- Casks/openrct2.rb
+++ Casks/openrct2.rb
@@ -1,15 +1,20 @@
 cask "openrct2" do
-  version "0.3.2"
-  sha256 "ff03b02b585c1deb669e5800f10c83789bd3d23fdd7a126a49c897db1d8f859a"
+  if MacOS.version <= :sierra
+    version "0.2.6"
+    sha256 "0073933b486da10b181bc8a226a140badc64c7cd93f681d769c17b5715221a85"
+    url "https://github.com/OpenRCT2/OpenRCT2/releases/download/v#{version}/OpenRCT2-#{version}-macos-x86_64.zip",
+        verified: "github.com/OpenRCT2/OpenRCT2/"
+  else
+    version "0.3.3"
+    sha256 "60477520caded0bb4a139947a0d5f2df05fabdff8b59aa08b4dd09c91a160c21"
+    url "https://github.com/OpenRCT2/OpenRCT2/releases/download/v#{version}/OpenRCT2-#{version}-macos-x86-64.zip",
+        verified: "github.com/OpenRCT2/OpenRCT2/"
+  end
 
-  url "https://github.com/OpenRCT2/OpenRCT2/releases/download/v#{version}/OpenRCT2-#{version}-macos-x86-64.zip",
-      verified: "github.com/OpenRCT2/OpenRCT2/"
   appcast "https://github.com/OpenRCT2/OpenRCT2/releases.atom"
   name "OpenRCT2"
   desc "Open-source re-implementation of RollerCoaster Tycoon 2"
   homepage "https://openrct2.io/"
-
-  depends_on macos: ">= :high_sierra"
 
   app "OpenRCT2.app"
 end
