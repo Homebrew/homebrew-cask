93fa956209b31ca0bf4213f4e3ea266ecd4b1d05
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-03-14 15:32:01 -0700

    Update leocad from 21.01 to 21.03 (#101111)

--- Casks/leocad.rb
+++ Casks/leocad.rb
@@ -1,6 +1,6 @@
 cask "leocad" do
-  version "21.01"
-  sha256 "52ca051486b800104ab4f9faf38807a30d583dcdabfc44a36fb61bbee0e02187"
+  version "21.03"
+  sha256 "52459d5c4d143a7dea917e600cb80061c64d96894258be68c7b54d4a9794f971"
 
   url "https://github.com/leozide/leocad/releases/download/v#{version}/LeoCAD-macOS-#{version}.dmg"
   appcast "https://github.com/leozide/leocad/releases.atom"
