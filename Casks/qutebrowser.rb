1ece476f4dd797a898f595398bb17405211bc222
adithyasunil26 <adithyasunil26@gmail.com>
2021-03-15 19:43:32 +0400

    Update qutebrowser from 2.0.2 to 2.1.0

--- Casks/qutebrowser.rb
+++ Casks/qutebrowser.rb
@@ -1,6 +1,6 @@
 cask "qutebrowser" do
-  version "2.0.2"
-  sha256 "9f92bd8a1e525572fb447abb2e31995c16c4cb740e63317c173c9969a47eacc8"
+  version "2.1.0"
+  sha256 "a9c752eec5c14b1d21b25de189c15c6e940b3051ab9d7b36c51f3d712cd7e57f"
 
   url "https://github.com/qutebrowser/qutebrowser/releases/download/v#{version}/qutebrowser-#{version}.dmg",
       verified: "github.com/qutebrowser/qutebrowser/"
