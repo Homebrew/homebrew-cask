953434d7324d70a5c37cf435517b1f890f185b3a
Tim <tim-hub@users.noreply.github.com>
2021-03-15 10:44:53 +0000

    Update mmex.rb
    add version 1.3.6

--- Casks/mmex.rb
+++ Casks/mmex.rb
@@ -1,6 +1,6 @@
 cask "mmex" do
-  version "1.3.5"
-  sha256 "ed27e39373c0e74ea856e2cb91b3b6f99fb745b8ec8b83424f7e406e8428fa7d"
+  version "1.3.6"
+  sha256 "90aa96bf55dde10f19b8dd9b9cc1645ac088c526843848d6202c97109c4714d2"
 
   url "https://downloads.sourceforge.net/moneymanagerex/v#{version}/mmex-#{version}-macos10.9-wx3.1.3.dmg",
       verified: "downloads.sourceforge.net/moneymanagerex/"
