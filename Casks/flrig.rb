3ff62380a7f2ac0fcb58cc9a41e0e6f419512952
SteamedFish <SteamedFish@users.noreply.github.com>
2021-03-15 11:10:27 +0800

    Update flrig from 1.3.53 to 1.3.54 (#101121)

--- Casks/flrig.rb
+++ Casks/flrig.rb
@@ -1,6 +1,6 @@
 cask "flrig" do
-  version "1.3.53"
-  sha256 "b39c156d4cdbfe0c26fbb2693957f8856c6b8ddfbc9ba88f2b58c1337d567815"
+  version "1.3.54"
+  sha256 "76e4fc5dd3f83dd4e91462ec706be0397be45a92f9f51fe3a7444b7c72c301c5"
 
   url "https://downloads.sourceforge.net/fldigi/fldigi/flrig-#{version.before_comma}_x86_64.dmg"
   appcast "https://sourceforge.net/projects/fldigi/rss?path=/flrig"
