9cd0a8f17b25fe2380f3e66e01b1c09d9d1508b9
Zhenfu Shi <szf1234@me.com>
2021-03-15 09:58:48 +0800

    Update launchcontrol from 1.51.2,1470 to 1.52,1484 (#101117)

--- Casks/launchcontrol.rb
+++ Casks/launchcontrol.rb
@@ -1,6 +1,6 @@
 cask "launchcontrol" do
-  version "1.51.2,1470"
-  sha256 "4a8f1e5e3710f6238b0afc5a8830ad4af3033e8f174bef33414ffa5336b3f80b"
+  version "1.52,1484"
+  sha256 "9e37bf1425e13aac9f220a4cd4c08533ee85e2eeb70d1a9d2d3ce1079772bab0"
 
   url "https://www.soma-zone.com/download/files/LaunchControl-#{version.before_comma}.tar.bz2"
   name "LaunchControl"
