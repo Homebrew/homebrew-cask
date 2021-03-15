821d55558bcae9c14dad32ca31eec5b87bcba3da
Yurii Kolesnykov <root@yurikoles.com>
2021-03-15 19:04:01 +0200

    Update element from 1.7.22 to 1.7.23 (#101205)

--- Casks/element.rb
+++ Casks/element.rb
@@ -1,6 +1,6 @@
 cask "element" do
-  version "1.7.22"
-  sha256 "1f8d7e6f90eccbbe51431910883fdfb93f5e8c98bad7cdb898b7ff52a39965da"
+  version "1.7.23"
+  sha256 "8a0a20df393c11052aa31faf7816d710af50753b96acb5d10a1ff52c83740646"
 
   url "https://packages.riot.im/desktop/install/macos/Element-#{version}.dmg",
       verified: "packages.riot.im/desktop/"
