c8f0dc1a316b3db2b8c22f53b3d0aa27eb8b6aa0
Xavier Hurst <xavier@hurst.im>
2021-03-15 15:22:35 +0100

    Update yEd from 3.20.1 to 3.21

--- Casks/yed.rb
+++ Casks/yed.rb
@@ -1,8 +1,8 @@
 cask "yed" do
-  version "3.20.1"
-  sha256 "8cf3666b7c83dcd365ef1308ab8f5eeb53bea2405682ae9cae01a3cfd055ca98"
+  version "3.21"
+  sha256 "a7907fa7c4430bf5ba435140670e355bb361375b0212d6a63487735b05f3efd2"
 
-  url "https://www.yworks.com/resources/yed/demo/yEd-#{version}_with-JRE14.dmg"
+  url "https://www.yworks.com/resources/yed/demo/yEd-#{version}_with-JRE15.dmg"
   name "yWorks yEd"
   desc "Create diagrams manually, or import external data for analysis"
   homepage "https://www.yworks.com/products/yed"
