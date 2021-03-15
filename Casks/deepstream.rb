2b21b1332c132d2351a2a61a7e8f0559873f5503
Adithya Sunil <adithyasunil26@gmail.com>
2021-03-15 19:38:35 +0400

    Update deepstream from 5.2.2 to 5.2.3 (#101152)

--- Casks/deepstream.rb
+++ Casks/deepstream.rb
@@ -1,6 +1,6 @@
 cask "deepstream" do
-  version "5.2.2"
-  sha256 "8241774de0a32d883d56236276a8462b7de43e0abac12bfdd86e6d791660c281"
+  version "5.2.3"
+  sha256 "4db747af5fc500aca26c248a4fb8b143f0293d49186a50a759d5546a186034c5"
 
   url "https://github.com/deepstreamIO/deepstream.io/releases/download/v#{version}/deepstream.io-mac-#{version}.pkg",
       verified: "github.com/deepstreamIO/deepstream.io/"
