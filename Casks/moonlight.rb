70abeb7611e943e1987683c8e4d91df757014f65
adithyasunil26 <adithyasunil26@gmail.com>
2021-03-15 19:44:54 +0400

    Update moonlight from 3.0.0 to 3.1.0

--- Casks/moonlight.rb
+++ Casks/moonlight.rb
@@ -1,6 +1,6 @@
 cask "moonlight" do
-  version "3.0.0"
-  sha256 "bf3bd9739ce12191658ef688a0ed0f7777966b9e5d765012555d6a7a34011b78"
+  version "3.1.0"
+  sha256 "7ddc42ac65f21346958649cc3136dbd9b603b2ab30cd9d39b32f6d679296d3b1"
 
   url "https://github.com/moonlight-stream/moonlight-qt/releases/download/v#{version}/Moonlight-#{version}.dmg",
       verified: "github.com/moonlight-stream/moonlight-qt/"
