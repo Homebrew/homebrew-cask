db08214826159e7cb6f8922bc3caa529f1776b9f
Adithya Sunil <adithyasunil26@gmail.com>
2021-03-15 19:26:39 +0400

    Update clash-for-windows from 0.14.8 to 0.14.9 (#101144)

--- Casks/clash-for-windows.rb
+++ Casks/clash-for-windows.rb
@@ -1,6 +1,6 @@
 cask "clash-for-windows" do
-  version "0.14.8"
-  sha256 "8a7137a2951c797c175a96f116f9e9d1fb37b86399ee43f9e6bcfa3d5df216aa"
+  version "0.14.9"
+  sha256 "49c6b9c47146c993964211ce6add57c39be7e7d1b243cb212e334afc3d0fdb0f"
 
   url "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/#{version}/Clash.for.Windows-#{version}.dmg"
   name "Clash for Windows"
