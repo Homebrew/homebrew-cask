a8ef6b7c168cbd4611310ffbe858b4d9106b478d
Guillaume Rischard <github@stereo.lu>
2021-03-16 16:38:06 +0100

    Moving releases from my fork to openstreetmap org

--- Casks/josm.rb
+++ Casks/josm.rb
@@ -2,9 +2,9 @@ cask "josm" do
   version "17560"
   sha256 "1385ba7bd8b49ff1846d22785985ad40031a37fb33826062b298628e9afff45a"
 
-  url "https://github.com/grischard/josm/releases/download/#{version}-tested/JOSM-macOS-java15.zip",
-      verified: "github.com/grischard/josm/"
-  appcast "https://github.com/grischard/josm/releases.atom"
+  url "https://github.com/openstreetmap/josm/releases/download/#{version}-tested/JOSM-macOS-java15.zip",
+      verified: "github.com/openstreetmap/josm/"
+  appcast "https://github.com/openstreetmap/josm/releases.atom"
   name "JOSM"
   desc "Extensible editor for OpenStreetMap"
   homepage "https://josm.openstreetmap.de/"
