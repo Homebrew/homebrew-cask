5c5bae5373f9fb0db0d3c299cb553db3243117a5
kortenkamp <kortenkamp@cinderella.de>
2021-03-16 17:47:58 +0100

    Adding Cinderella cask
    This software can be downloaded for free and this cask formula adds a convenient way to install both on amd64 and aarch64.

---
+++ Casks/cinderella.rb
@@ -0,0 +1,15 @@
+cask "cinderella" do
+  version "3.0b.1987"
+  if Hardware::CPU.intel?
+	sha256 "3947b3d5d535b050c306c164624be1a3f53629932df12aa168a0c47d913cde35"
+  	url "https://beta.cinderella.de/Cinderella-#{version}.dmg"
+  else
+	sha256 "979a40c5e29e2bd27500ec2f08dd908adbd5fb5fed3b2e11b39096e334441636"
+	url "https://beta.cinderella.de/Cinderella-silicon-#{version}.dmg"
+  end
+  name "Cinderella"
+  desc "The Interactive Geometry Software Cinderella"
+  homepage "https://cinderella.de"
+
+  app "Cinderella.app"
+end
