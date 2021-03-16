7dea6459cef4d2852f628871ff140a844507af7b
Reno <25192197+singularitti@users.noreply.github.com>
2021-03-16 02:14:52 -0400

    Add `appcast` to medibangpaintpro.rb (#101091)

--- Casks/medibangpaintpro.rb
+++ Casks/medibangpaintpro.rb
@@ -3,6 +3,7 @@ cask "medibangpaintpro" do
   sha256 "7004726fa40274922fd981ad2e3cfcf67fe5d7d81073c1fca3e3640884672205"
 
   url "https://medibangpaint.com/static/installer/MediBangPaintPro/MediBangPaintPro-#{version}.dmg"
+  appcast "https://medibangpaint.com/en/pc/releasenote/"
   name "MediBang Paint Pro"
   desc "Create digital art and comics"
   homepage "https://medibangpaint.com/en/pc/"
