53158caa99d49534a000aa749532e823bbc48d1f
Adithya Sunil <adithyasunil26@gmail.com>
2021-03-16 09:50:24 +0400

    Update pritunl from 1.2.2709.72 to 1.2.2737.2 (#101181)

--- Casks/pritunl.rb
+++ Casks/pritunl.rb
@@ -4,8 +4,8 @@ cask "pritunl" do
       version "1.2.2615.73"
       sha256 "275f8498fb736a0a1ddebcbfcb6f03e82b8eb4fedf9b7eb61b0e306cb6de4a71"
     else
-      version "1.2.2709.72"
-      sha256 "014acca51584f24c74f581e7e44acd2f3f34cdeb0388e39f1f30097301a0ac78"
+      version "1.2.2737.2"
+      sha256 "f46a17a612a5b7c051cbacbc19d35578b35a36cf6dae736621cea2b22b9b1403"
     end
 
     url "https://github.com/pritunl/pritunl-client-electron/releases/download/#{version}/Pritunl.pkg.zip",
@@ -13,7 +13,7 @@ cask "pritunl" do
 
     pkg "Pritunl.pkg"
   else
-    version "1.2.2709.72"
+    version "1.2.2737.2"
     sha256 "2ca2f3b31d78177a591c9166990017521ac3f283b032933c1983ffdbde21475b"
 
     url "https://github.com/pritunl/pritunl-client-electron/releases/download/#{version}/Pritunl.arm64.pkg.zip",
