ff6baae761d8f171ac398f08d9b34d5ae3d7137f
Guillaume Rischard <github@stereo.lu>
2021-03-16 16:49:35 +0100

    Correct sha256

--- Casks/josm.rb
+++ Casks/josm.rb
@@ -1,6 +1,6 @@
 cask "josm" do
   version "17560"
-  sha256 "8386fd4126925efa057e40c1db3c09180aaf93e691acb876c216dc6fd83f40e8"
+  sha256 "7089f8bf8f63509467b2a6001472af5b5f9f5c30fc225e80ae93047ec90955da"
 
   url "https://github.com/openstreetmap/josm/releases/download/#{version}-tested/JOSM-macOS-java15.zip",
       verified: "github.com/openstreetmap/josm/"
