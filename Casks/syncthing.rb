3ebd712a8bf238bf8ddf288c5f7f86d1b30ed039
Alex Tomlins <alex@tomlins.org.uk>
2021-03-16 06:01:59 +0000

    Update syncthing from 1.13.0-1 to 1.14.0-1 (#101097)
    * Update syncthing from 1.13.0-1 to 1.14.0-1
    
    * Syncthing requires macos >= 10.12
    
    Starting with 1.14, 10.12 or higher is required -
    https://github.com/syncthing/syncthing-macos/releases/tag/v1.14.0-1

--- Casks/syncthing.rb
+++ Casks/syncthing.rb
@@ -1,6 +1,6 @@
 cask "syncthing" do
-  version "1.13.0-1"
-  sha256 "12241016ccbf2f6fe3c81aa804f002020fd14e0c53d75d9517b10e903fb4b2f6"
+  version "1.14.0-1"
+  sha256 "7050e4b989d91fa4a928e5eb980c0bee06aed812fb94fd70be2b9af840eeb2e4"
 
   url "https://github.com/syncthing/syncthing-macos/releases/download/v#{version}/Syncthing-#{version}.dmg",
       verified: "github.com/syncthing/syncthing-macos/"
@@ -15,7 +15,7 @@ cask "syncthing" do
   end
 
   auto_updates true
-  depends_on macos: ">= :yosemite"
+  depends_on macos: ">= :sierra"
 
   app "Syncthing.app"
 
