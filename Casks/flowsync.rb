4f5aeaea9c501df0a97d76815fbbbea3fbd10639
Dmitry Pogodin <lunaticare@users.noreply.github.com>
2021-03-16 16:20:14 +0300

    Update flowsync.rb

--- Casks/flowsync.rb
+++ Casks/flowsync.rb
@@ -2,7 +2,7 @@ cask "flowsync" do
   version "4.0.6"
   sha256 "e3a2999dd060deb8a30c4d507f0b9ee4298fa10415f1d87bfd6e2bed46afab06"
 
-  url "https://flowsync.flow.polar.com/app/FlowSync_installer-4.0.6.pkg"
+  url "https://flowsync.flow.polar.com/app/FlowSync_installer-#{version}.pkg"
   name "Polar FlowSync Software"
   desc "Syncing software for Polar Flow products"
   homepage "https://support.polar.com/uk-en/support/flowsync"
