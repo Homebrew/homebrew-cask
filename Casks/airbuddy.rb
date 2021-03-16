a7cbc398800e927d6c969795db4db6291248ee9d
Aleks Bunin <github@compuix.com>
2021-03-16 19:32:48 -0400

    AirBuddy2: add zap stanza

--- Casks/airbuddy.rb
+++ Casks/airbuddy.rb
@@ -11,4 +11,28 @@ cask "airbuddy" do
   auto_updates true
 
   app "AirBuddy.app"
+
+  zap trash: [
+    "~/Library/Group Containers/8C7439RJLG.group.codes.rambo.AirBuddy",
+    "~/Library/Preferences/codes.rambo.AirBuddy.plist",
+    "~/Library/Application Scripts/codes.rambo.AirBuddy.Batteries",
+    "~/Library/Application Scripts/codes.rambo.AirBuddy.ModernWidget",
+    "~/Library/Application Scripts/codes.rambo.AirBuddy.AssetService",
+    "~/Library/Application Scripts/codes.rambo.AirBuddy.ProxCardUIService",
+    "~/Library/Application Scripts/codes.rambo.AirBuddy.AirBuddyIntents",
+    "~/Library/Application Scripts/codes.rambo.AirBuddy.AirStatsService",
+    "~/Library/Application Scripts/codes.rambo.AirBuddyHelper",
+    "~/Library/HTTPStorages/codes.rambo.AirBuddy.binarycookies",
+    "~/Library/SyncedPreferences/codes.rambo.AirBuddy.plist",
+    "~/Library/LaunchAgents/codes.rambo.AirBuddyHelper.plist",
+    "~/Library/Containers/codes.rambo.AirBuddy.Batteries",
+    "~/Library/Containers/codes.rambo.AirBuddy.ModernWidget",
+    "~/Library/Containers/codes.rambo.AirBuddy.AssetService",
+    "~/Library/Containers/codes.rambo.AirBuddy.ProxCardUIService",
+    "~/Library/Containers/codes.rambo.AirBuddy.AirBuddyIntents",
+    "~/Library/Containers/codes.rambo.AirBuddy.AirStatsService",
+    "~/Library/Containers/codes.rambo.AirBuddyHelper",
+    "~/Library/Caches/codes.rambo.AirBuddy",
+    "~/Library/Caches/codes.rambo.AirCore",
+  ]
 end
