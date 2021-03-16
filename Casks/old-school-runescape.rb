763b3d700e47cfc429098a3f7625569a71b60731
Chris Kay <chris@cjkay.com>
2021-03-16 00:42:59 +0000

    Add Old School Runescape

---
+++ Casks/old-school-runescape.rb
@@ -0,0 +1,17 @@
+cask "old-school-runescape" do
+  version :latest
+  sha256 :no_check
+
+  url "https://www.runescape.com/downloads/OldSchool.dmg"
+  name "Old School RuneScape"
+  desc "Game client for Old School RuneScape"
+  homepage "https://oldschool.runescape.com/"
+
+  app "Old School RuneScape.app"
+
+  zap trash: [
+    "~/jagex_cl_oldschool_LIVE.dat",
+    "~/jagexcache/oldschool",
+    "~/random.dat",
+  ]
+end
