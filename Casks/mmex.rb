f12b4f5a7b3addcb156ba7425c527e0d15f48c7c
Miccal Matthews <miccal.matthews@gmail.com>
2021-03-16 13:55:16 +0800

    Update mmex.rb

--- Casks/mmex.rb
+++ Casks/mmex.rb
@@ -2,7 +2,7 @@ cask "mmex" do
   version "1.3.6"
   sha256 "90aa96bf55dde10f19b8dd9b9cc1645ac088c526843848d6202c97109c4714d2"
 
-  url "https://downloads.sourceforge.net/moneymanagerex/v#{version}/mmex-#{version}-macos10.9-wx3.1.3.dmg",
+  url "https://downloads.sourceforge.net/moneymanagerex/v#{version}/mmex%20v#{version}-macOS-fix.dmg",
       verified: "downloads.sourceforge.net/moneymanagerex/"
   appcast "https://github.com/moneymanagerex/moneymanagerex/releases.atom"
   name "Money Manager Ex"
