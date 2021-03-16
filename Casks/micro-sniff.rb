f0f18aca99dca6ef62189cdcd9c6de8af553574e
Reno <25192197+singularitti@users.noreply.github.com>
2021-03-16 01:56:45 -0400

    Update micro-sniff from 1.0.0 to 1.2.0 (#101213)

--- Casks/micro-sniff.rb
+++ Casks/micro-sniff.rb
@@ -1,6 +1,6 @@
 cask "micro-sniff" do
-  version "1.0.0"
-  sha256 "fbfd7eccc2de7fc9d5d5a4062057791850fdcedb4b14ec04538cfabce263aa5f"
+  version "1.2.0"
+  sha256 "92dbcad769340ab0df851483942003eaa6eaab388773408ef3191edce19b5bbe"
 
   url "https://github.com/dwarvesf/micro-sniff/releases/download/v#{version}/Micro.Sniff.#{version}.dmg"
   name "Micro Sniff"
