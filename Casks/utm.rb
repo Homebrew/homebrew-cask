7c06b3737464619b1c3953eace6a40b6e4b52adb
tiiiecherle <tiiiecherle@gmail.com>
2021-03-16 22:20:04 +0100

    Update utm from 2.0.23 to 2.0.24 (#101257)

--- Casks/utm.rb
+++ Casks/utm.rb
@@ -1,6 +1,6 @@
 cask "utm" do
-  version "2.0.23"
-  sha256 "7ef01c5c88645356d0236f38beb1f94ef9f305230d283442e08622f1d92acc7c"
+  version "2.0.24"
+  sha256 "0c262ac4916c59819f816934bcfb4dfe397eca8845cef0f19838b4dbaa721ed0"
 
   url "https://github.com/utmapp/UTM/releases/download/v#{version}/UTM.dmg",
       verified: "github.com/utmapp/UTM/"
