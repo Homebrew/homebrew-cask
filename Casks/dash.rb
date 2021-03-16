220998f34d38cc0a0d24d65d99e29d8f35e95212
Kapeli <support@kapeli.com>
2021-03-16 21:14:23 +0200

    Update dash from 6.0.2,970 to 6.0.3,972

--- Casks/dash.rb
+++ Casks/dash.rb
@@ -3,8 +3,8 @@ cask "dash" do
     version "4.6.7"
     sha256 "e2b5eb996645b25f12ccae15e24b1b0d8007bc5fed925e14ce7be45a2b693fb6"
   else
-    version "6.0.2,970"
-    sha256 "7bfd26292777acfb07faf5bc23ae9202b99d0d482205ca27b29ef95c36dd61bd"
+    version "6.0.3,972"
+    sha256 "d1ebf89839696883c6f283881afe4221075007303f5c7751a99b6ec97e4ecb63"
   end
 
   url "https://kapeli.com/downloads/v#{version.major}/Dash.zip"
