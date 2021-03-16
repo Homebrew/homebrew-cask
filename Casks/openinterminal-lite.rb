e1e8668a71421cbce57c1bd78dbf5d98c6a052b2
Jianing Wang <wjnmailg@gmail.com>
2021-03-16 10:11:29 +0800

    Update OpenInTerminal-Lite from 1.2.2 to 1.2.3 (#101217)

--- Casks/openinterminal-lite.rb
+++ Casks/openinterminal-lite.rb
@@ -1,6 +1,6 @@
 cask "openinterminal-lite" do
-  version "1.2.2"
-  sha256 "d36b2359eba2806b458aa130e5dd56bcccf48e396e5e3dc4211fbde6c820f973"
+  version "1.2.3"
+  sha256 "67239f7201fed7457d283add3a4f23320038ff757473a1f2238f986fd413b853"
 
   url "https://github.com/Ji4n1ng/OpenInTerminal/releases/download/v#{version}/OpenInTerminal-Lite.app.zip"
   appcast "https://github.com/Ji4n1ng/OpenInTerminal/releases.atom"
