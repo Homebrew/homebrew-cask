7ed0a0778aba87be3d7e2ed6cd45e5494edef859
Andreas Pohl <apohl79@users.noreply.github.com>
2021-03-16 07:20:25 +0100

    Update audiogridder from 1.1.0 to 1.1.1 (#101048)

--- Casks/audiogridder-server.rb
+++ Casks/audiogridder-server.rb
@@ -1,12 +1,12 @@
 cask "audiogridder-server" do
-  version "1.1.0"
+  version "1.1.1"
 
   if Hardware::CPU.intel?
-    sha256 "fb29a41c3917201d5925d9abca422fe5e55e36ae30dcd524c8a57d9e29f0728f"
+    sha256 "072e52bd56fedb1c481961beacca99096e3a65ba36339bdd78155fd1f1d133ea"
     url "https://audiogridder.com/releases/AudioGridderServer_#{version}_macOS-x86_64.pkg"
     pkg "AudioGridderServer_#{version}_macOS-x86_64.pkg"
   else
-    sha256 "09f7752677fc027f7f9193ed3eefc00f760c96a73622099bf706f2ab257f9694"
+    sha256 "26be020b2ba57d7a0102a152099fcc5e993ce4bbcb23f47d80d5d9f24421658b"
     url "https://audiogridder.com/releases/AudioGridderServer_#{version}_macOS-arm64.pkg"
     pkg "AudioGridderServer_#{version}_macOS-arm64.pkg"
   end
@@ -14,6 +14,12 @@ cask "audiogridder-server" do
   name "AudioGridder Server"
   desc "VST2/VST3/AU DSP Server"
   homepage "https://audiogridder.com/"
+
+  livecheck do
+    url "https://audiogridder.com/releases/latest.txt"
+    strategy :page_match
+    regex(/.*/)
+  end
 
   uninstall pkgutil: "com.e47.pkg.server"
 
