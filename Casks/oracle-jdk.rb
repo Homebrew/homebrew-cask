e3b13b0d3519b1042713e8c7d636e009219d2184
Mike Duigou <bondolo@users.noreply.github.com>
2021-03-16 09:54:04 -0700

    Update oracle-jdk to 16.0 (#101241)

--- Casks/oracle-jdk.rb
+++ Casks/oracle-jdk.rb
@@ -1,6 +1,6 @@
 cask "oracle-jdk" do
-  version "15.0.2,7:0d1cfde4252546c6931946de8db48ee2"
-  sha256 "106313be17b977df3da05a168b9449b41fd52858a3d18cc3fac5c8d9279d06a8"
+  version "16,36:7863447f0ab643c585b9bdebf67c69db"
+  sha256 "3401d484ebe4632d400cc8c82d66618b363d7829fd84d8a16b8a798f078a2d69"
 
   url "https://download.oracle.com/otn-pub/java/jdk/#{version.before_comma}+#{version.after_comma.before_colon}/#{version.after_colon}/jdk-#{version.before_comma}_osx-x64_bin.dmg",
       cookies: {
