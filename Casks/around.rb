e9dcc0f1116200828832c1ee7d069fc82d66fdf0
Claudio Spiess <claudiosv@users.noreply.github.com>
2021-03-16 14:28:35 -0500

    Add `zap` stanza to Around cask

--- Casks/around.rb
+++ Casks/around.rb
@@ -16,4 +16,12 @@ cask "around" do
 
   uninstall quit:   "co.teamport.around",
             delete: "/Applications/Around.app"
+
+  zap trash: [
+    "~/Library/Application Support/Around",
+    "~/Library/Caches/co.around.installer",
+    "~/Library/Preferences/co.teamport.around.plist",
+    "~/Library/Saved Application State/co.around.installer.savedState",
+    "~/Library/Saved Application State/co.teamport.around.savedState",
+  ]
 end
