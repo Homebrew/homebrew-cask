0970dcb68d21048503e97aecfc2493d6372694a3
Kenny Botelho <63611879+kennyb-222@users.noreply.github.com>
2021-03-16 19:39:46 -0700

    SHA256 mismatch -- unable to install via Brew (#101275)

--- Casks/ishowu-instant.rb
+++ Casks/ishowu-instant.rb
@@ -1,6 +1,6 @@
 cask "ishowu-instant" do
   version "1.4.2"
-  sha256 "c151aa4fd6d2ff230f3ef1f377780f2bc21b6207d8546ab40d9c98eda5dda1d1"
+  sha256 "fabe0f178a73cd86482a3f9613f088f9d253db18b2e02ecb8cd7bdbf1e2dcbee"
 
   url "https://www.shinywhitebox.com/downloads/instant/iShowU_Instant_#{version}.dmg"
   appcast "https://www.shinywhitebox.com/store/appcast.php?p=14"
