7e5c750e50cb5468fa301707363ce2bada3f2445
Yusuf Kursad Kaya <ykursadkaya@gmail.com>
2021-03-18 01:55:37 +0300

    burp-suite: add livecheck and desc

--- Casks/burp-suite.rb
+++ Casks/burp-suite.rb
@@ -3,9 +3,15 @@ cask "burp-suite" do
   sha256 "6361c376105197eaee6eb456645d40e94c881fd4cb318b8839308c8323b22960"
 
   url "https://portswigger.net/burp/releases/download?product=community&version=#{version}&type=MacOsx"
-  appcast "https://portswigger.net/burp/releases?initialTab=community"
   name "Burp Suite"
+  desc "Web security testing toolkit"
   homepage "https://portswigger.net/burp/"
+
+  livecheck do
+    url "https://portswigger.net/burp/releases?initialTab=community"
+    strategy :page_match
+    regex(%r{Professional\s*/\s*Community\s*(\d+(:?\.\d+)*)})
+  end
 
   installer script: {
     executable: "Burp Suite Community Edition Installer.app/Contents/MacOS/JavaApplicationStub",
