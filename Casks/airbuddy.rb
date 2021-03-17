7504867d834b09ab3a2301d50275e50263b78a4e
Miccal Matthews <miccal.matthews@gmail.com>
2021-03-17 14:21:04 +0800

    Update airbuddy.rb

--- Casks/airbuddy.rb
+++ Casks/airbuddy.rb
@@ -3,36 +3,29 @@ cask "airbuddy" do
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://su.airbuddy.app/kCRSAmcjBc/AirBuddy_v#{version}.dmg"
-  appcast "https://su.airbuddy.app/kCRSAmcjBc/appcast_hyeon.xml"
   name "Airbuddy"
   desc "AirPods companion app"
   homepage "https://v2.airbuddy.app/"
+
+  livecheck do
+    url "https://su.airbuddy.app/kCRSAmcjBc/appcast_hyeon.xml"
+    strategy :page_match
+    regex(/airbuddy_v(\d+(?:.\d+)*)\.dmg/i)
+  end
 
   auto_updates true
 
   app "AirBuddy.app"
 
   zap trash: [
-    "~/Library/Group Containers/8C7439RJLG.group.codes.rambo.AirBuddy",
     "~/Library/Preferences/codes.rambo.AirBuddy.plist",
-    "~/Library/Application Scripts/codes.rambo.AirBuddy.Batteries",
-    "~/Library/Application Scripts/codes.rambo.AirBuddy.ModernWidget",
-    "~/Library/Application Scripts/codes.rambo.AirBuddy.AssetService",
-    "~/Library/Application Scripts/codes.rambo.AirBuddy.ProxCardUIService",
-    "~/Library/Application Scripts/codes.rambo.AirBuddy.AirBuddyIntents",
-    "~/Library/Application Scripts/codes.rambo.AirBuddy.AirStatsService",
-    "~/Library/Application Scripts/codes.rambo.AirBuddyHelper",
-    "~/Library/HTTPStorages/codes.rambo.AirBuddy.binarycookies",
-    "~/Library/SyncedPreferences/codes.rambo.AirBuddy.plist",
-    "~/Library/LaunchAgents/codes.rambo.AirBuddyHelper.plist",
-    "~/Library/Containers/codes.rambo.AirBuddy.Batteries",
-    "~/Library/Containers/codes.rambo.AirBuddy.ModernWidget",
-    "~/Library/Containers/codes.rambo.AirBuddy.AssetService",
-    "~/Library/Containers/codes.rambo.AirBuddy.ProxCardUIService",
-    "~/Library/Containers/codes.rambo.AirBuddy.AirBuddyIntents",
-    "~/Library/Containers/codes.rambo.AirBuddy.AirStatsService",
-    "~/Library/Containers/codes.rambo.AirBuddyHelper",
+    "~/Library/Application Scripts/codes.rambo.AirBuddy*",
     "~/Library/Caches/codes.rambo.AirBuddy",
     "~/Library/Caches/codes.rambo.AirCore",
+    "~/Library/Containers/codes.rambo.AirBuddy*",
+    "~/Library/Group Containers/8C7439RJLG.group.codes.rambo.AirBuddy",
+    "~/Library/HTTPStorages/codes.rambo.AirBuddy.binarycookies",
+    "~/Library/LaunchAgents/codes.rambo.AirBuddyHelper.plist",
+    "~/Library/SyncedPreferences/codes.rambo.AirBuddy.plist",
   ]
 end
