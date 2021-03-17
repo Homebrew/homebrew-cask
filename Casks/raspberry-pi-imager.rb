00d9eec868f01d547b4129802a21d536e639e1e3
Paul Mucur <mudge@mudge.name>
2021-03-17 16:00:11 +0000

    Add livecheck to Raspberry Pi Imager (#101296)

--- Casks/raspberry-pi-imager.rb
+++ Casks/raspberry-pi-imager.rb
@@ -3,9 +3,15 @@ cask "raspberry-pi-imager" do
   sha256 "800c48d49eca9199f275a73821686381e23dc5a22d0b87b77b57322afafbe5f2"
 
   url "https://downloads.raspberrypi.org/imager/imager_#{version}.dmg"
-  appcast "https://github.com/raspberrypi/rpi-imager/releases.atom"
   name "Raspberry Pi Imager"
+  desc "Imaging utility to install operating systems to a microSD card"
   homepage "https://www.raspberrypi.org/downloads/"
+
+  livecheck do
+    url "https://www.raspberrypi.org/software/"
+    strategy :page_match
+    regex(%r{href=.*?/imager_(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "Raspberry Pi Imager.app"
 
