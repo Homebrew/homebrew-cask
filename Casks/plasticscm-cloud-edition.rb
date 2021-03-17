4689d4384e7f6d276d8b76c5016b7245dcc3c527
Randall <ran.dall@icloud.com>
2021-03-17 16:46:42 -0700

    Add PlasticSCM Cloud Edition

---
+++ Casks/plasticscm-cloud-edition.rb
@@ -0,0 +1,35 @@
+cask "plasticscm-cloud-edition" do
+  version "9.0.16.5201"
+  sha256 "13a1a47a896017eee29af6c6a5a5fa3c4a7c6130a81eef0628f7caaa8187e151"
+
+  url "https://s3.eu-west-2.amazonaws.com/plastic-releases/releases/#{version}/plasticscm/osx/plasticscm-cloud-#{version}.pkg.zip",
+      verified: "s3.eu-west-2.amazonaws.com/plastic-releases/"
+  name "PlasicSCM -a Cloud Edition"
+  desc "Install PlasticSCM locally and join a Cloud Edition subscription"
+  homepage "https://www.plasticscm.com/"
+
+  livecheck do
+    url "https://www.plasticscm.com/download/#{version}/macosx"
+    strategy :page_match
+    regex(%r{href=.*?(\d+\.\d+.\d+\.\d+)/macosx}i)
+  end
+
+  pkg "plasticscm-cloud-#{version}.pkg"
+
+  uninstall launchctl: [
+    "com.codicesoftware.plasticscm.macplastic",
+    "com.codicesoftware.plasticscm.server",
+  ],
+            quit:      [
+              "com.codicesoftware.plasticscm",
+            ],
+            pkgutil:   [
+              "com.codicesoftware.plasticscm.macplastic",
+              "com.codicesoftware.plasticscm.server",
+            ],
+            delete:    [
+              "/Applications/Gluon.app",
+              "/Applications/PlasticSCM.app",
+              "/Applications/PlasticSCMServer.app",
+            ]
+end
