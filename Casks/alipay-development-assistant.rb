5c1cbd6d67c6d43d602f93395cd73c72f1b948cb
ye4241 <ye4241@gmail.com>
2021-03-17 10:02:15 +0800

    add alipay-development-assistant.rb

---
+++ Casks/alipay-development-assistant.rb
@@ -0,0 +1,11 @@
+cask "alipay-development-assistant" do
+  version "1.0.7"
+  sha256 :no_check
+
+  url "https://ideservice.alipay.com/ide/getPluginUrl.htm?clientType=assistant&platform=mac&channelType=WEB"
+  name "Alipay Development Assistant"
+  desc "Alipay's open platform development assistant"
+  homepage "https://opendocs.alipay.com/open/291/introduce"
+
+  app "支付宝开放平台开发助手.app"
+end
