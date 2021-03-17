b707bb3be5ad47c8f7d8b889d6fc42130c7e8825
Paul Mucur <mudge@mudge.name>
2021-03-17 14:14:41 +0000

    Add livecheck for Raspberry Pi Imager

--- Casks/raspberry-pi-imager.rb
+++ Casks/raspberry-pi-imager.rb
@@ -3,9 +3,14 @@ cask "raspberry-pi-imager" do
   sha256 "800c48d49eca9199f275a73821686381e23dc5a22d0b87b77b57322afafbe5f2"
 
   url "https://downloads.raspberrypi.org/imager/imager_#{version}.dmg"
-  appcast "https://github.com/raspberrypi/rpi-imager/releases.atom"
   name "Raspberry Pi Imager"
   homepage "https://www.raspberrypi.org/downloads/"
+
+  livecheck do
+    url "https://www.raspberrypi.org/software/"
+    strategy :page_match
+    regex(%r{href=.*?/imager_(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "Raspberry Pi Imager.app"
 
