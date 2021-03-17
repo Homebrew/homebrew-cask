70a085a0c23dda19cace285bdd9813f6d2b564ee
Richi Rodriguez <richi.rodriguez.mia@gmail.com>
2021-03-16 21:23:26 -0600

    4k-stogram 3.3.3

--- Casks/4k-stogram.rb
+++ Casks/4k-stogram.rb
@@ -1,13 +1,14 @@
 cask "4k-stogram" do
-  version "3.2.2.3440"
-  sha256 "44a30064d1601d254a166c04d92a00de912d14827b4418e396179f9838f40cde"
+  version "3.3.3"
+  sha256 "e7b9067ea1169704fde887ae029cda45e764fa895601d6e935f1d3e0661d5b57"
 
   url "https://dl.4kdownload.com/app/4kstogram_#{version.major_minor_patch}.dmg"
   appcast "https://www.4kdownload.com/download"
   name "4K Stogram"
+  desc "Download Instagram photos, accounts, hashtags and locations"
   homepage "https://www.4kdownload.com/products/product-stogram"
 
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :high_sierra"
 
   app "4K Stogram.app"
 
