9e3b8a8b9d90d9d2a0e3680675cfd60ca0594e55
Markus Reiter <me@reitermark.us>
2021-03-17 21:18:58 +0100

    Update pwnagetool.rb

--- Casks/pwnagetool.rb
+++ Casks/pwnagetool.rb
@@ -1,5 +1,5 @@
 cask "pwnagetool" do
-  version "5.1.1-#{ENV['HOMEBREW_GITHUB_API_TOKEN']}"
+  version "5.1.1-#{ENV[:HOMEBREW_GITHUB_API_TOKEN.to_s]}"
   sha256 "84262734ad9f9186bce14a4f939d7ea290ed187782fdfa549a82c28bf837c808"
 
   url "https://iphoneroot.com/download/PwnageTool_#{version}.dmg"
