6104d82b14451be7de118446c0d8eaead540c9ab
Oleg <epushiron+github@gmail.com>
2021-03-17 12:27:52 +0500

    Update synthesia from 10.6 to 10.7 (#101018)
    * Update synthesia from 10.6 to 10.7
    
    * Update synthesia.rb
    
    * Update synthesia.rb
    
    Co-authored-by: Oleg <epushiron@gmall.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/synthesia.rb
+++ Casks/synthesia.rb
@@ -1,12 +1,18 @@
 cask "synthesia" do
-  version "10.6"
-  sha256 "53b3f9f0cbf4ff73738e859bd933aa6c15b245af6ff5e841843cde574e591bc0"
+  version "10.7"
+  sha256 "18dde0fa5ca7b10aab1964621509ae3cf4110a02b11c24470b3d6e0f1c3fee65"
 
   url "https://synthesia.s3.amazonaws.com/files/Synthesia-#{version}.dmg",
       verified: "synthesia.s3.amazonaws.com/"
-  appcast "https://feeds.feedburner.com/SynthesiaNews"
   name "Synthesia"
+  desc "Learn how to play the piano using falling notes"
   homepage "https://www.synthesiagame.com/"
+
+  livecheck do
+    url "https://www.synthesiagame.com/download"
+    strategy :page_match
+    regex(/synthesia\s(\d+(?:\.\d+)*)\sfor\smacOS/i)
+  end
 
   app "Synthesia.app"
 
