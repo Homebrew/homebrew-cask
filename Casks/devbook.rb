39f123a6a230d8912cd7790202287ee7fbbfbdae
Vítor Galvão <info@vitorgalvao.com>
2021-03-17 02:40:05 +0000

    devbook.rb: update livecheck strategy for electron-builder (#101248)

--- Casks/devbook.rb
+++ Casks/devbook.rb
@@ -8,8 +8,9 @@ cask "devbook" do
 
   livecheck do
     url "https://download.todesktop.com/2102273jsy18baz/latest-mac.yml"
-    strategy :page_match
-    regex(/version:.(\d+(?:\.\d+)*)/)
+    strategy :page_match do |page|
+      YAML.safe_load(page)["version"]
+    end
   end
 
   if Hardware::CPU.intel?
