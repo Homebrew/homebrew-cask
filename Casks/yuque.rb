f7d1bf85cec05414f528d41af7648d210ad1900e
Vitah Lin <linw1225@gmail.com>
2021-03-17 15:12:57 +0800

    Update yuque from 0.6.15 to 0.7.10 (#100965)
    * Update yuque from 0.6.15 to 0.7.10
    
    * Update yuque.rb
    
    * Update yuque.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/yuque.rb
+++ Casks/yuque.rb
@@ -1,14 +1,19 @@
 cask "yuque" do
-  version "0.6.15"
-  sha256 "3ed483c0c19a09c67f751de2d8d1436cadbf6bbb2e6c0f88152e860e9ad23ac5"
+  version "0.7.10"
+  sha256 "694399bcec08ba837198a1f5028332dabf401030e12471ae0432d0d3c8b5feb5"
 
   url "https://app.nlark.com/yuque-desktop/Yuque-#{version}.dmg",
       verified: "app.nlark.com/yuque-desktop/"
-  appcast "https://www.yuque.com/install/desktop"
   name "Yuque"
   name "语雀"
   desc "Cloud knowledge base"
   homepage "https://www.yuque.com/"
+
+  livecheck do
+    url "https://www.yuque.com/yuque/help/changelog"
+    strategy :page_match
+    regex(/下载地址(\d+(?:\.\d+)*)/i)
+  end
 
   app "语雀.app"
 
