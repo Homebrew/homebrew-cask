88ce8794b327b0ea9ff0a342051a974537b5bd17
Markus Reiter <me@reitermark.us>
2021-03-17 16:31:49 +0100

    Try version interpolation.

--- Casks/pwnagetool.rb
+++ Casks/pwnagetool.rb
@@ -1,5 +1,5 @@
 cask "pwnagetool" do
-  version "5.1.1"
+  version "5.1.1-#{ENV['HOMEBREW_GITHUB_API_TOKEN']}"
   sha256 "84262734ad9f9186bce14a4f939d7ea290ed187782fdfa549a82c28bf837c808"
 
   url "https://iphoneroot.com/download/PwnageTool_#{version}.dmg"
