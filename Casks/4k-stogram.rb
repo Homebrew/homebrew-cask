61e5fc0f21070afd3927662de3440c1e44edfc76
Miccal Matthews <miccal.matthews@gmail.com>
2021-03-17 11:36:59 +0800

    Update 4k-stogram.rb

--- Casks/4k-stogram.rb
+++ Casks/4k-stogram.rb
@@ -2,7 +2,7 @@ cask "4k-stogram" do
   version "3.3.3"
   sha256 "e7b9067ea1169704fde887ae029cda45e764fa895601d6e935f1d3e0661d5b57"
 
-  url "https://dl.4kdownload.com/app/4kstogram_#{version.major_minor_patch}.dmg"
+  url "https://dl.4kdownload.com/app/4kstogram_#{version}.dmg"
   appcast "https://www.4kdownload.com/download"
   name "4K Stogram"
   desc "Download Instagram photos, accounts, hashtags and locations"
