9e1f81dd43e0e8acc70fa702f863e4b1f10b63e8
cometeme <cometemeJackson@outlook.com>
2021-03-17 14:00:20 +0800

    Add flameshot.app v0.9.0

---
+++ Casks/flameshot.rb
@@ -0,0 +1,13 @@
+cask "flameshot" do
+  version "0.9.0"
+  sha256 "fb593aaf5f060c323207eaf33444b9a6e5e2dd62734f71367876104ff35a9d03"
+
+  url "https://github.com/flameshot-org/flameshot/releases/download/v#{version}/flameshot.dmg",
+      verified: "github.com/"
+  appcast "https://github.com/flameshot-org/flameshot/releases.atom"
+  name "Flameshot"
+  desc "Powerful yet simple to use screenshot software"
+  homepage "https://flameshot.org/"
+
+  app "flameshot.app"
+end
