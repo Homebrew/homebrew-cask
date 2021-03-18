8c43fb90120a426285273552d655467f5012eac7
Francis McKenzie <info@macca.tech>
2021-03-18 09:17:06 +0100

    Bump version to latest (v2.6)
    Version 2.4.2 does not work in Catalina as it is not notarised.
    
    https://github.com/MaccaTech/PostgresPrefs/issues/16

--- Casks/postgrespreferencepane.rb
+++ Casks/postgrespreferencepane.rb
@@ -1,8 +1,8 @@
 cask "postgrespreferencepane" do
-  version "2.4.2"
+  version "2.6"
   sha256 "fb3f618321efec12cbad21ec2d77e6a024a35f7a17bb304b421970f85e38337f"
 
-  url "https://github.com/MaccaTech/PostgresPrefs/releases/download/v#{version}/PostgreSQL.prefPane-v#{version}.zip"
+  url "https://github.com/MaccaTech/PostgresPrefs/releases/download/v#{version}/PostgresPrefs-v#{version}.dmg"
   appcast "https://github.com/MaccaTech/PostgresPrefs/releases.atom"
   name "PostgresPrefs"
   homepage "https://github.com/MaccaTech/PostgresPrefs"
