18d99c0bc21e1978e21d4da7fde7b0c5e6a3b03c
Felix Honer <privat@felix-honer.com>
2021-03-18 20:41:56 +0100

    Update jamulus from 3.6.2 to 3.7.0 (#101356)

--- Casks/jamulus.rb
+++ Casks/jamulus.rb
@@ -1,8 +1,8 @@
 cask "jamulus" do
-  version "3.6.2"
-  sha256 "128727f09265bac829d33e0a6316722b3dc635585c53970875c343966e5f93b3"
+  version "3.7.0"
+  sha256 "bdb6cf58b041fc5917937721f6de4a15108c647c9b4440881415b7392c0b5b2d"
 
-  url "https://downloads.sourceforge.net/llcon/Jamulus-#{version}-installer-mac.dmg",
+  url "https://downloads.sourceforge.net/llcon/jamulus_#{version}_mac.dmg",
       verified: "downloads.sourceforge.net/llcon/"
   appcast "https://sourceforge.net/projects/llcon/rss"
   name "Jamulus"
