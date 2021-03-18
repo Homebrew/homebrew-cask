1047d9b1338c3f356749da7f89916fb29f4fc4a0
Seeker <meaningseeking@protonmail.com>
2021-03-18 12:44:59 -0700

    Update terminus from 1.0.134 to 1.0.135 (#101345)

--- Casks/terminus.rb
+++ Casks/terminus.rb
@@ -1,13 +1,13 @@
 cask "terminus" do
-  version "1.0.134"
+  version "1.0.135"
 
   if Hardware::CPU.intel?
-    sha256 "d2c18d5dbc885d112cb36aa324dfe29c87146f69b5c9869041ef515dab4b2015"
+    sha256 "10d007984e6a96f77f76a2ae06903ac8790ea5db43e6419772f11cf8c3273356"
 
     url "https://github.com/Eugeny/terminus/releases/download/v#{version}/terminus-#{version}-macos-x86_64.zip",
         verified: "github.com/Eugeny/terminus/"
   else
-    sha256 "449b64f037bf3d4726d583de82aedf09bba3c4b9d2bb7c8c00fe328b4f2185c5"
+    sha256 "d4ebb75e58dfa604e4090484f0f8bc26ce1d6c0c9ea991d41615ecd73eca7307"
 
     url "https://github.com/Eugeny/terminus/releases/download/v#{version}/terminus-#{version}-macos-arm64.zip",
         verified: "github.com/Eugeny/terminus/"
