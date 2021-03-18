c1d9eeaac9e709f4a06cf4afc5f0405a8bd56ad9
Seeker <meaningseeking@protonmail.com>
2021-03-18 12:45:15 -0700

    Update dosbox-x from 0.83.9 to 0.83.11 (#101346)
    * Update dosbox-x from 0.83.9,20201231203002 to 0.83.11,20210301003839
    
    * Update dosbox-x.rb

--- Casks/dosbox-x.rb
+++ Casks/dosbox-x.rb
@@ -1,13 +1,13 @@
 cask "dosbox-x" do
   if Hardware::CPU.intel?
-    version "0.83.9,20201231203002"
-    sha256 "af55d06fb9277c62708b1b8f22bba3d3555744eab759700d0e5e4cd0d439abe5"
+    version "0.83.11,20210301003839"
+    sha256 "bba985e13ec27e7e617197c3a32ff3d864e72d419b0cade85626344720ed453a"
 
     url "https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v#{version.before_comma}/dosbox-x-macosx-x86_64-#{version.after_comma}.zip",
         verified: "github.com/joncampbell123/dosbox-x/"
   else
-    version "0.83.9,20201231202930"
-    sha256 "cd56080debf70543b030bbe8131fd009caf37c970624be35da76c0ad4d7fca99"
+    version "0.83.11,20210301003750"
+    sha256 "30cf1f6407ab70f0b958a7232d9555328d81ec2fe935dc2a70fdd5f2ef119599"
 
     url "https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v#{version.before_comma}/dosbox-x-macosx-arm64-#{version.after_comma}.zip",
         verified: "github.com/joncampbell123/dosbox-x/"
