bd0a4a2650c1d6793536aa7855dd37e6da5fd6dc
Mikaël Barbero <mikael.barbero@gmail.com>
2021-03-19 21:57:45 +0100

    It's GA, no u

--- Casks/adoptopenjdk.rb
+++ Casks/adoptopenjdk.rb
@@ -2,7 +2,7 @@ cask "adoptopenjdk" do
   version "16,36"
   sha256 "2d101b7bc4901c1a9554ba158b9eb4bf245b6dc314bd82078b1c9b85953ae5ae"
 
-  url "https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/download/jdk-#{version.before_comma}%2B#{version.after_comma}/OpenJDK#{version.major}U-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma.major}.pkg",
+  url "https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/download/jdk-#{version.before_comma}%2B#{version.after_comma}/OpenJDK#{version.major}-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma.major}.pkg",
       verified: "github.com/AdoptOpenJDK/"
   name "AdoptOpenJDK Java Development Kit"
   homepage "https://adoptopenjdk.net/"
