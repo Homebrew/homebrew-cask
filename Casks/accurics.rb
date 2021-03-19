dde1a680d8370fbf36b4ab5ab3de292e47b8432c
Willie Sana <willie@accurics.com>
2021-03-19 15:56:27 -0700

    Update accurics from 1.0.16 to 1.0.16

--- Casks/accurics.rb
+++ Casks/accurics.rb
@@ -1,6 +1,6 @@
 cask "accurics" do
-  version "1.0.15"
-  sha256 "0aebd800e289c74f63f02cedd1bec557cc28f1e2815e505553213d104704863e"
+  version "1.0.16"
+  sha256 "7cd184e443abc20b0a04800d3146d0d939d5ef56d240c168322b41fc2e732e74"
 
   url "https://downloads.accurics.com/cli/#{version}/accurics-cli.dmg"
   appcast "https://downloads.accurics.com/cli/#{version}/accurics-cli.yml",
