9f6717be0bf103f50958f89f654edb7198726daa
Miccal Matthews <miccal.matthews@gmail.com>
2021-03-19 13:26:24 +0800

    Update electron.rb

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,14 +1,14 @@
 cask "electron" do
   version "12.0.1"
-  
+
   if Hardware::CPU.intel?
     sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
     url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip",
-      verified: "github.com/electron/electron/"
+        verified: "github.com/electron/electron/"
   else
     sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
     url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-arm64.zip",
-      verified: "github.com/electron/electron/"
+        verified: "github.com/electron/electron/"
   end
 
   name "Electron"
