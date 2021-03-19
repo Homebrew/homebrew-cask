744ad4b3ca9d4cbfbfbd04b5ab9411bf81971c33
Ondřej Führer <ondrejfuhrer@users.noreply.github.com>
2021-03-19 19:51:45 +0100

    Update plex from 1.28.0.1681-cc6e807c to 1.29.1.1974-a65b3ef3 (#101384)

--- Casks/plex.rb
+++ Casks/plex.rb
@@ -1,6 +1,6 @@
 cask "plex" do
-  version "1.28.0.1681-cc6e807c"
-  sha256 "2c17f65bfab67fbc3cd577843a0eb73628f67da7498bc7347a263ad37abe3f64"
+  version "1.29.1.1974-a65b3ef3"
+  sha256 "7069f62d0ef70eeca941a0243ca99318ff63f2e5be6fcb70ce46db26faca3288"
 
   url "https://downloads.plex.tv/plex-desktop/#{version}/macos/Plex-#{version}-x86_64.zip"
   appcast "https://plex.tv/api/downloads/6.json"
