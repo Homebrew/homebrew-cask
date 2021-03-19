c3b82cdb7a332e62d72cee0140619e7c685611c6
Guillaume Rischard <github@stereo.lu>
2021-03-19 18:20:10 +0100

    Update josm.rb

--- Casks/josm.rb
+++ Casks/josm.rb
@@ -1,8 +1,8 @@
 cask "josm" do
-  version "17560"
-  sha256 "7089f8bf8f63509467b2a6001472af5b5f9f5c30fc225e80ae93047ec90955da"
+  version "17580"
+  sha256 "c18f7b4ba47d138bde1345ce2ebeb78481180ad47773f58d3a71fd5092c9132b"
 
-  url "https://github.com/openstreetmap/josm/releases/download/#{version}-tested/JOSM-macOS-java15.zip",
+  url "https://github.com/openstreetmap/josm/releases/download/#{version}-tested/JOSM-macOS-java16.zip",
       verified: "github.com/openstreetmap/josm/"
   appcast "https://github.com/openstreetmap/josm/releases.atom"
   name "JOSM"
