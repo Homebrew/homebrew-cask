2e9e4f23a5407edb6a2f77a0f6775e724352164a
Ondřej Führer <ondrejfuhrer@users.noreply.github.com>
2021-03-19 15:53:11 +0100

    aurora: adding zap and livecheck stanzas (#101389)

--- Casks/aurora.rb
+++ Casks/aurora.rb
@@ -3,9 +3,22 @@ cask "aurora" do
   sha256 "4293c5ffdf6eb8e5d7031f02b1c83238472f486fe8a964f2f62ac71c72c22eb2"
 
   url "https://www.oneperiodic.com/files/Aurora%20v#{version}.zip"
-  appcast "https://www.oneperiodic.com/aurora#{version.major}.xml"
   name "Aurora"
+  desc "Alarm clock"
   homepage "https://www.oneperiodic.com/products/aurora/"
 
+  livecheck do
+    url "https://www.oneperiodic.com/aurora#{version.major}.xml"
+    strategy :sparkle
+  end
+
   app "Aurora.app"
+
+  zap trash: [
+    "~/Library/Application Support/Aurora",
+    "~/Library/Caches/com.oneperiodic.Aurora",
+    "~/Library/Caches/com.apple.helpd/Generated/Aurora Help*5.0.3",
+    "~/Library/Preferences/com.oneperiodic.Aurora.plist",
+    "~/Library/Saved Application State/com.oneperiodic.Aurora.savedState",
+  ]
 end
