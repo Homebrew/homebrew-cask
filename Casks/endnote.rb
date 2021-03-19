426957cd467feadd8ada8fad83c88628c9e0f538
BojunJia <jbj0901@qq.com>
2021-03-19 08:51:56 +0800

    Update endnote from 9 to 20

--- Casks/endnote.rb
+++ Casks/endnote.rb
@@ -1,8 +1,8 @@
 cask "endnote" do
-  version "9"
+  version "20"
   sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://download.endnote.com/downloads/X#{version}/EndNoteX#{version}Installer.dmg"
+  url "https://download.endnote.com/downloads/#{version}/EndNote#{version}Installer.dmg"
   name "EndNote"
   homepage "https://endnote.com/"
 
