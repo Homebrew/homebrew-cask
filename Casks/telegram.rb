db3fe21ddc5b6100cc7a8c72de9b75b297d71e6f
Loyalsoldier <10487845+Loyalsoldier@users.noreply.github.com>
2021-03-20 02:38:34 +0800

    Update telegram from 7.5.1,213563 to 7.6,214328 (#101401)

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,6 +1,6 @@
 cask "telegram" do
-  version "7.5.1,213563"
-  sha256 "0e2d96d11258a132803e5b67f3d6fa6bdfa1f707e48428a05cd6e104087ef1b4"
+  version "7.6,214328"
+  sha256 "ce1762260365c955f0fe903dd1eb5bd84b23008a355104f98920d64d960d59de"
 
   url "https://osx.telegram.org/updates/Telegram-#{version.before_comma}.#{version.after_comma}.app.zip"
   name "Telegram for macOS"
