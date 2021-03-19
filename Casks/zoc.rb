af529a210928bf5689f1b0573c859c50598212d7
Nick Wind-McJetters <nick@elearningexperts.net>
2021-03-19 01:18:13 -0400

    Update zoc (#101376)

--- Casks/zoc.rb
+++ Casks/zoc.rb
@@ -1,6 +1,6 @@
 cask "zoc" do
-  version "8.02.0"
-  sha256 "92a7302c3481bda30472d59a67e1062e254846ae0b6dd742dfd490696c495fb1"
+  version "8.02.1"
+  sha256 "33a45c1f2ebdbe90ed66f7caed890b4de6acf202f62bb94c9e3aa7c5fcdcc96b"
 
   url "https://www.emtec.com/downloads/zoc/zoc#{version.no_dots}.dmg"
   appcast "https://www.emtec.com/downloads/zoc/zoc_changes.txt"
