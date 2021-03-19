e85e516f26cf4ca7e70c087487c3067aa23304b5
Ulrich Kortenkamp <>
2021-03-19 11:49:30 +0100

    Update cinderella from 3.0b.1989 to 3.0b.1999

--- Casks/cinderella.rb
+++ Casks/cinderella.rb
@@ -1,13 +1,8 @@
 cask "cinderella" do
-  version "3.0b.1989"
+  version "3.0b.1999"
+  sha256 "e33990ccedbee56806ccd189dea01167f443d18c7baf00be61f08921dcad5215"
 
-  if Hardware::CPU.intel?
-    sha256 "bf53c41a6f8639a48826958ecc9ce293afd651500865de0c1a17eb42f13f43fa"
-    url "https://beta.cinderella.de/Cinderella-#{version}.dmg"
-  else
-    sha256 "7072d4410077646f6013a352ae620aa2e7a2b4099b4926c2494dce8c8300b336"
-    url "https://beta.cinderella.de/Cinderella-silicon-#{version}.dmg"
-  end
+  url "https://beta.cinderella.de/Cinderella-#{version}.dmg"
   name "Cinderella"
   desc "Interactive Geometry Software"
   homepage "https://cinderella.de/"
