0cf84ccd6b452518db76d87321f8591f5fd7fd70
Lawrence Thorpe <thorpe_lawrence@yahoo.co.uk>
2021-03-19 10:02:40 +0000

    Create utterly.rb

---
+++ Casks/utterly.rb
@@ -0,0 +1,15 @@
+cask "utterly" do
+  version "0.9.0-SNAPSHOT-140e1bc"
+  sha256 :no_check
+
+  url "https://www.utterly.app/Utterly.pkg"
+  name "Utterly"
+  desc "With AI at the core, Utterly plugin will remove background noise during your calls in any audio or video conferencing app."
+  homepage "https://www.utterly.app"
+
+  pkg "Utterly.pkg"
+
+  uninstall script: {
+    executable: "/Applications/Utterly.app/Contents/Resources/_uninstall-non-interactive.sh"
+  }
+end
