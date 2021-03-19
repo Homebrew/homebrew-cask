18d2d1d0958e171b6994ec473aceeb9295d794cc
Ondrej Führer <ondrejfuhrer@users.noreply.github.com>
2021-03-19 09:19:26 +0100

    plex: adding livecheck

--- Casks/plex.rb
+++ Casks/plex.rb
@@ -3,10 +3,14 @@ cask "plex" do
   sha256 "7069f62d0ef70eeca941a0243ca99318ff63f2e5be6fcb70ce46db26faca3288"
 
   url "https://downloads.plex.tv/plex-desktop/#{version}/macos/Plex-#{version}-x86_64.zip"
-  appcast "https://plex.tv/api/downloads/6.json"
   name "Plex"
-  desc "Home media server"
+  desc "Home media player"
   homepage "https://www.plex.tv/"
+
+  livecheck do
+    url "https://plex.tv/api/downloads/6.json"
+    regex(/"version":"(\d\.\d{2}\.\d\.\d+-[a-f0-9]{8})"/i)
+  end
 
   auto_updates true
   depends_on macos: ">= :high_sierra"
