d8940a81f482ab2433f5bd3e4a0a6dd9c1913835
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-03-20 16:01:49 +0000

    Update amazon-workspaces from 3.1.3.1649 to 3.1.4.1751

--- Casks/amazon-workspaces.rb
+++ Casks/amazon-workspaces.rb
@@ -1,5 +1,5 @@
 cask "amazon-workspaces" do
-  version "3.1.3.1649"
+  version "3.1.4.1751"
   sha256 :no_check
 
   url "https://workspaces-client-updates.s3.amazonaws.com/prod/iad/osx/WorkSpaces.pkg",
