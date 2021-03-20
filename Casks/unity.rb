dc59fa83a15b691c178b273e31a119d38dc9ba18
Ryan Smith <hello@ryansmith.online>
2021-03-20 06:22:19 +0100

    Update unity.rb
    Update to the latest LTS 2020.3.0f1

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -1,6 +1,6 @@
 cask "unity" do
-  version "2020.1.17f1,9957aee8edc2"
-  sha256 "c6fa8c89eb9a93ee08830b84dc84a948afb32eb17ead7b220c2b5f44662bb6bf"
+  version "2020.3.0f1,c7b5465681fb"
+  sha256 "dd57438fe698a2fca1cd66ef98bd2467"
 
   url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorInstaller/Unity-#{version.before_comma}.pkg",
       verified: "download.unity3d.com/download_unity/"
