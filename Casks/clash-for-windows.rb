8b3b13785e2a9baf718de20cd242360e6a74fc6b
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-03-20 14:19:06 -0700

    Update clash-for-windows from 0.14.9 to 0.14.10 (#101450)

--- Casks/clash-for-windows.rb
+++ Casks/clash-for-windows.rb
@@ -1,6 +1,6 @@
 cask "clash-for-windows" do
-  version "0.14.9"
-  sha256 "49c6b9c47146c993964211ce6add57c39be7e7d1b243cb212e334afc3d0fdb0f"
+  version "0.14.10"
+  sha256 "fca51f49f9f43cb00e05d737f8ae39d309e1ee9eb360e9a3085e6b3b7ceaca76"
 
   url "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/#{version}/Clash.for.Windows-#{version}.dmg"
   name "Clash for Windows"
