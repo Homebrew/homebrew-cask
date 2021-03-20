cd98f6c281dc2bc028b63a7497585db81b760935
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-03-20 14:17:31 -0700

    Update jabt-flow from 1.10.1,106 to 1.10.2,107 (#101471)

--- Casks/jabt-flow.rb
+++ Casks/jabt-flow.rb
@@ -1,6 +1,6 @@
 cask "jabt-flow" do
-  version "1.10.1,106"
-  sha256 "0afe6999fb7b931d3dce6fd07fefb379727b41e713530ade42e9fea2398afbfe"
+  version "1.10.2,107"
+  sha256 "3380205a886018587e0c6fc559857eab2a0f345ec2a89d6226ce9d12ab4f476e"
 
   # Download page is at https://createwithflow.com/releases/
   url "https://createwithflow-updates.s3-us-west-2.amazonaws.com/Flow#{version.before_comma.no_dots}_.zip",
