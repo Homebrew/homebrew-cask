1acb631c80c8c8b6ddb31e386714e735b8c4585c
Seeker <meaningseeking@protonmail.com>
2021-03-20 14:18:12 -0700

    Update fly from 7.0.0 to 7.1.0 (#101461)

--- Casks/fly.rb
+++ Casks/fly.rb
@@ -1,6 +1,6 @@
 cask "fly" do
-  version "7.0.0"
-  sha256 "7cdbf924d8f4e18a6f7a6bef65edc25f5ac234161aca21c9f24ed18b337d19db"
+  version "7.1.0"
+  sha256 "461fb517430bd1d8f21fe0e2f59d4c293770d7c76e64d7c96133d5c63e3e04b5"
 
   url "https://github.com/concourse/concourse/releases/download/v#{version}/fly-#{version}-darwin-amd64.tgz"
   appcast "https://github.com/concourse/concourse/releases.atom"
