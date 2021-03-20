6befcad628d8d20d80be613c8704160551965df6
Boris Pek <tehnick-8@yandex.ru>
2021-03-21 01:14:27 +0300

    Update eiskaltdcpp from 2.4.1 to 2.4.2 (#101416)

--- Casks/eiskaltdcpp.rb
+++ Casks/eiskaltdcpp.rb
@@ -1,6 +1,6 @@
 cask "eiskaltdcpp" do
-  version "2.4.1"
-  sha256 "7a110fb067a6d657a8ac388253326fd8568742b59055a4822a3405a29c908358"
+  version "2.4.2"
+  sha256 "85b77157892985c0cc0e4c2b9244b4095bf5738f5e94e7a0941551e0c9a7a009"
 
   url "https://downloads.sourceforge.net/eiskaltdcpp/EiskaltDC++-#{version}-x86_64.dmg"
   appcast "https://sourceforge.net/projects/eiskaltdcpp/rss"
