893aa1e0d653d7d55fef71f8e764ebc94d42b29e
Seeker <meaningseeking@protonmail.com>
2021-03-20 09:57:39 -0700

    Update eclipse-platform from 4.18,202012021800 to 4.19,202103031800

--- Casks/eclipse-platform.rb
+++ Casks/eclipse-platform.rb
@@ -1,6 +1,6 @@
 cask "eclipse-platform" do
-  version "4.18,202012021800"
-  sha256 "57e6b02c31179470fef4edd5f62cdf323fefc166315f7736147ba05798577381"
+  version "4.19,202103031800"
+  sha256 "0b19648afda7b5773c0e5f93c1eab101662a5df6680225413e62d084c29d856e"
 
   url "https://www.eclipse.org/downloads/download.php?file=/eclipse/downloads/drops#{version.major}/R-#{version.before_comma}-#{version.after_comma}/eclipse-SDK-#{version.before_comma}-macosx-cocoa-x86_64.dmg&r=1"
   name "Eclipse SDK"
