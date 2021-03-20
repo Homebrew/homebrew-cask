efd1504b1ab9aa35f4306521c71cfbff3379f45a
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-03-20 14:11:31 -0700

    Update yuna from 1.4.21 to 1.4.22 (#101542)

--- Casks/yuna.rb
+++ Casks/yuna.rb
@@ -1,6 +1,6 @@
 cask "yuna" do
-  version "1.4.21"
-  sha256 "547d5759122535a14674906c8ff4494bc5e2e5f9bc001dc991ddaea574d8d5b4"
+  version "1.4.22"
+  sha256 "913cb1c36cfe3fa6d414dd6e51cdfe1d06ed5b92585c9fe7e248f9b92c482006"
 
   url "https://github.com/BeeeQueue/yuna/releases/download/v#{version}/Yuna-#{version}.dmg",
       verified: "github.com/BeeeQueue/yuna/"
