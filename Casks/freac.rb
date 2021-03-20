1110754c8018301e46162bce1f3272d2b2cd7424
Markus Reiter <me@reitermark.us>
2021-03-21 00:12:51 +0100

    Update freac.rb

--- Casks/freac.rb
+++ Casks/freac.rb
@@ -3,10 +3,12 @@ cask "freac" do
 
   if MacOS.version <= :catalina
     sha256 "c62588c0afe34cb1163968b5432bd9deb84bd75be8f8f3b7cc292884d735af9b"
+
     url "https://github.com/enzo1982/freac/releases/download/v#{version}/freac-#{version}-macos10.dmg",
         verified: "github.com/enzo1982/freac/"
   else
     sha256 "d5d961798b695569f7636e8b4c0dcf8ac8a5a7d6e57d0c85985f2cb51c936476"
+
     url "https://github.com/enzo1982/freac/releases/download/v#{version}/freac-#{version}-macos11.dmg",
         verified: "github.com/enzo1982/freac/"
   end
