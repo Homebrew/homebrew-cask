b5de6808dfcff9efea6435bd579cc35534b7b1a7
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-03-20 14:11:40 -0700

    Update yggdrasil from 0.3.15 to 0.3.16 (#101540)

--- Casks/yggdrasil.rb
+++ Casks/yggdrasil.rb
@@ -1,6 +1,6 @@
 cask "yggdrasil" do
-  version "0.3.15"
-  sha256 "66c0fb9ecff048ae50816f5920e02a4a2240e9e9cf27c1c8acc9a420f59a3a6f"
+  version "0.3.16"
+  sha256 "4d1bc756db8fb237671f5ba3f121133a597d3e7057fafbffabf5fdfd93e2b669"
 
   url "https://github.com/yggdrasil-network/yggdrasil-go/releases/download/v#{version}/yggdrasil-#{version}-macos-amd64.pkg"
   appcast "https://github.com/yggdrasil-network/yggdrasil-go/releases.atom"
