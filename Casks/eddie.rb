b80535eab1f4d4a0cbcfbe71805d48b130383cf7
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-03-20 16:46:12 +0000

    Update eddie from 2.19.7 to 2.20.0

--- Casks/eddie.rb
+++ Casks/eddie.rb
@@ -1,5 +1,5 @@
 cask "eddie" do
-  version "2.19.7"
+  version "2.20.0"
 
   if MacOS.version <= :mojave
     sha256 "a446563a6beb6f91542d6afbd6f90f6745b7a36fef363a13506cff9d8f078c78"
@@ -7,7 +7,7 @@ cask "eddie" do
     url "https://eddie.website/download/?platform=macos-10.9&arch=x64&ui=ui&format=disk.dmg&version=#{version}",
         verified: "eddie.website/"
   else
-    sha256 "b766c9a5fec0421d9cd3772e6c1986d6070e0bb9bfa4312e588d332ec786e586"
+    sha256 "3f960bfd47ef5847197012bf0455003f6b4999b9b6c52e58f61e8d5c2e81d263"
 
     url "https://eddie.website/download/?platform=macos-10.15&arch=x64&ui=ui&format=disk.dmg&version=#{version}",
         verified: "eddie.website/"
