3f92e093b0c66abf13a94854dcc77b7d38b3d536
Seeker <meaningseeking@protonmail.com>
2021-03-20 10:04:33 -0700

    Update freac from 1.1.3 to 1.1.4

--- Casks/freac.rb
+++ Casks/freac.rb
@@ -1,9 +1,16 @@
 cask "freac" do
-  version "1.1.3"
-  sha256 "5a7bb46b36e965ce5b0ecedbdee6a75982eb54ed6d594aed71716290a169b682"
+  version "1.1.4"
 
-  url "https://github.com/enzo1982/freac/releases/download/v#{version.delete("a")}/freac-#{version}-macosx.dmg",
-      verified: "github.com/enzo1982/freac/"
+  if MacOS.version <= :catalina
+    sha256 "c62588c0afe34cb1163968b5432bd9deb84bd75be8f8f3b7cc292884d735af9b"
+    url "https://github.com/enzo1982/freac/releases/download/v#{version}/freac-#{version}-macos10.dmg",
+        verified: "github.com/enzo1982/freac/"
+  else
+    sha256 "d5d961798b695569f7636e8b4c0dcf8ac8a5a7d6e57d0c85985f2cb51c936476"
+    url "https://github.com/enzo1982/freac/releases/download/v#{version}/freac-#{version}-macos11.dmg",
+        verified: "github.com/enzo1982/freac/"
+  end
+
   appcast "https://github.com/enzo1982/freac/releases.atom"
   name "fre:ac"
   desc "Audio converter and CD ripper"
