bc59af04e91c42063a1986cd3334cbce8ae0f3aa
CaptainYukinoshitaHachiman <CaptainYukinoshitaHachiman@protonmail.com>
2021-03-21 06:39:49 +0800

    Update geph from 4.2.5 to 4.2.5

--- Casks/geph.rb
+++ Casks/geph.rb
@@ -1,6 +1,6 @@
 cask "geph" do
-  version "4.2.3"
-  sha256 "ed0dfbb5b101c25903b70200461f1ab6dd0405c1bca0717a81f9382ceb14002b"
+  version "4.2.5"
+  sha256 "e6c4866eb31d55c944473d940f63aa971dcfddf86eda5e3fb72c6f44b0065261"
 
   url "https://f001.backblazeb2.com/file/geph4-dl/Geph4Releases/#{version}/geph-macos-#{version}.dmg",
       verified: "f001.backblazeb2.com/file/geph4-dl/"
