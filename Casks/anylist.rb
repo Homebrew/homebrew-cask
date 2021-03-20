9c2f3e4db4c0aed380c208af209a1a990541c876
Ondřej Führer <ondrejfuhrer@users.noreply.github.com>
2021-03-20 14:44:21 +0100

    anylist: adding zap stanza (#101388)

--- Casks/anylist.rb
+++ Casks/anylist.rb
@@ -5,6 +5,7 @@ cask "anylist" do
   url "https://s3.amazonaws.com/files.anylistapp.com/mac/AnyList_v#{version.before_comma.dots_to_underscores}.zip",
       verified: "s3.amazonaws.com/files.anylistapp.com/"
   name "AnyList"
+  desc "Grocery shopping list"
   homepage "https://www.anylistapp.com/"
 
   livecheck do
@@ -13,4 +14,13 @@ cask "anylist" do
   end
 
   app "AnyList.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.purplecover.anylist.mac.recipe-import",
+    "~/Library/Application Support/com.purplecover.anylist.mac",
+    "~/Library/Caches/com.purplecover.anylist.mac",
+    "~/Library/Containers/com.purplecover.anylist.mac.recipe-import",
+    "~/Library/Logs/AnyList",
+    "~/Library/Preferences/com.purplecover.anylist.mac.plist",
+  ]
 end
