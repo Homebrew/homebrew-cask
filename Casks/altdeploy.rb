2e2224b3c4bda793b05b0b1908544ba55820c89d
Markus Reiter <me@reitermark.us>
2021-03-21 00:07:57 +0100

    altdeploy: add livecheck

--- Casks/altdeploy.rb
+++ Casks/altdeploy.rb
@@ -3,9 +3,12 @@ cask "altdeploy" do
   sha256 "c9f1c27e3c83022fe0fae7abe3ea825792bc799390d3fb5492f8cfb97a9d7f56"
 
   url "https://github.com/pixelomer/AltDeploy/releases/download/v#{version}/AltDeploy.zip"
-  appcast "https://github.com/pixelomer/AltDeploy/releases.atom"
   name "AltDeploy"
   homepage "https://github.com/pixelomer/AltDeploy"
 
   app "AltDeploy.app"
+
+  caveats do
+    discontinued
+  end
 end
