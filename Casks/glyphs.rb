6c4f06e7f1eb8233721f1e6122ac7f75eee23f01
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-03-20 14:18:01 -0700

    Update glyphs from 3.0.3,3072 to 3.0.3,3073 (#101465)

--- Casks/glyphs.rb
+++ Casks/glyphs.rb
@@ -1,6 +1,6 @@
 cask "glyphs" do
-  version "3.0.3,3072"
-  sha256 "b447f43ffd9baa3246f82f7b6c5f40557c62c579702bcb9265b75143f1be0464"
+  version "3.0.3,3073"
+  sha256 "5dcde448225c98a664c38f1420094b03b0557b76759239b92f078764e70377ed"
 
   url "https://updates.glyphsapp.com/Glyphs#{version.before_comma}-#{version.after_comma}.zip"
   name "Glyphs"
