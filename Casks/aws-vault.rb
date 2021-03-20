b5b343beb17136eef972537463dd3bf87395a20d
Markus Reiter <me@reitermark.us>
2021-03-21 00:59:21 +0100

    Update aws-vault.rb

--- Casks/aws-vault.rb
+++ Casks/aws-vault.rb
@@ -1,18 +1,20 @@
 cask "aws-vault" do
   version "6.3.0"
 
+  if Hardware::CPU.intel?
+    url "https://github.com/99designs/aws-vault/releases/download/v#{version}/aws-vault-darwin-amd64.dmg"
+
+    sha256 "7af98c76d3afcffd60829716d06986224239d2583108fd7669cb660e215ff98e"
+  else
+    url "https://github.com/99designs/aws-vault/releases/download/v#{version}/aws-vault-darwin-arm64.dmg"
+
+    sha256 "1e99a2109ad21a30ec0af5004cd3f65c337ac9775a29a3fe1d27c5dd8d0ef703"
+  end
+
   appcast "https://github.com/99designs/aws-vault/releases.atom"
   name "aws-vault"
   desc "Securely stores and accesses AWS credentials in a development environment"
   homepage "https://github.com/99designs/aws-vault"
 
   binary "aws-vault"
-
-  if Hardware::CPU.intel?
-    url "https://github.com/99designs/aws-vault/releases/download/v#{version}/aws-vault-darwin-amd64.dmg"
-    sha256 "7af98c76d3afcffd60829716d06986224239d2583108fd7669cb660e215ff98e"
-  else
-    url "https://github.com/99designs/aws-vault/releases/download/v#{version}/aws-vault-darwin-arm64.dmg"
-    sha256 "1e99a2109ad21a30ec0af5004cd3f65c337ac9775a29a3fe1d27c5dd8d0ef703"
-  end
 end
