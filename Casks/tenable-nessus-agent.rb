8dcee6f21ff5b064394e37f0dee798a7bd088eaa
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-03-20 14:13:09 -0700

    Update tenable-nessus-agent from 8.2.2,12161 to 8.2.3,12556 (#101521)

--- Casks/tenable-nessus-agent.rb
+++ Casks/tenable-nessus-agent.rb
@@ -1,6 +1,6 @@
 cask "tenable-nessus-agent" do
-  version "8.2.2,12161"
-  sha256 "e85fe73995943af61b493bcf432e2c8cf2cd4cb4144cdf404e28630a6cb8eeae"
+  version "8.2.3,12556"
+  sha256 "5cf0141b68490de42e4d6f9611bb1ca51542b5a6bb474945450e70885d7d268f"
 
   url "https://www.tenable.com/downloads/api/v1/public/pages/nessus-agents/downloads/#{version.after_comma}/download?i_agree_to_tenable_license_agreement=true"
   name "Tenable Nessus Agent"
