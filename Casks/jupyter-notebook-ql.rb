3dac00b6b0a8b7fcfacbb4db37ee7c65b57c6128
Markus Reiter <me@reitermark.us>
2021-03-21 17:42:09 +0100

    jupyter-notebook-ql: add livecheck (#101871)

--- Casks/jupyter-notebook-ql.rb
+++ Casks/jupyter-notebook-ql.rb
@@ -3,7 +3,6 @@ cask "jupyter-notebook-ql" do
   sha256 "f3a39ba316a32cd3ba63e2274e2d3355a0ef639f6e79c2eec40d462020599653"
 
   url "https://github.com/jendas1/jupyter-notebook-quick-look/releases/download/v#{version}/jupyter-notebook-quick-look.qlgenerator.zip"
-  appcast "https://github.com/jendas1/jupyter-notebook-quick-look/releases.atom"
   name "Jupyter Notebook Quick Look"
   homepage "https://github.com/jendas1/jupyter-notebook-quick-look"
 
