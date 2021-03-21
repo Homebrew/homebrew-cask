873d873ea8d0313009db0ddcb61f87b0adf749d9
Markus Reiter <me@reitermark.us>
2021-03-21 17:42:30 +0100

    keycastr: add livecheck (#101881)

--- Casks/keycastr.rb
+++ Casks/keycastr.rb
@@ -3,7 +3,6 @@ cask "keycastr" do
   sha256 "02f5b18b6911a74a82d2b95339d71e3941deed98c8051cc929d9af49c05dcdf3"
 
   url "https://github.com/keycastr/keycastr/releases/download/v#{version}/KeyCastr.app.zip"
-  appcast "https://github.com/keycastr/keycastr/releases.atom"
   name "KeyCastr"
   desc "Open-source keystroke visualizer"
   homepage "https://github.com/keycastr/keycastr"
