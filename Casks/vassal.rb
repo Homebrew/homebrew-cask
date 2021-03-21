52d6e3121d013384e9f490cfca055c6b5a80fcf9
Markus Reiter <me@reitermark.us>
2021-03-22 00:18:59 +0100

    vassal: add livecheck

--- Casks/vassal.rb
+++ Casks/vassal.rb
@@ -4,7 +4,6 @@ cask "vassal" do
 
   url "https://github.com/vassalengine/vassal/releases/download/#{version}/VASSAL-#{version}-macosx.dmg",
       verified: "github.com/vassalengine/vassal/"
-  appcast "https://github.com/vassalengine/vassal/releases.atom"
   name "VASSAL"
   desc "Open-source boardgame engine"
   homepage "http://www.vassalengine.org/"
