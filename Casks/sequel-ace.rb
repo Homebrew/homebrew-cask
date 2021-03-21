22f5bf2fd817af9866481662586f1cef97608a09
Markus Reiter <me@reitermark.us>
2021-03-21 20:40:39 +0100

    sequel-ace: add livecheck

--- Casks/sequel-ace.rb
+++ Casks/sequel-ace.rb
@@ -3,7 +3,6 @@ cask "sequel-ace" do
   sha256 "c9119c42e5d266bee224e1b8cde3a458ab93e82979025947a3662dcc3e5b87cd"
 
   url "https://github.com/Sequel-Ace/Sequel-Ace/releases/download/production/#{version.before_comma}-#{version.after_comma}/Sequel-Ace-#{version.before_comma}.zip"
-  appcast "https://github.com/Sequel-Ace/Sequel-Ace/releases.atom"
   name "Sequel Ace"
   desc "MySQL/MariaDB database management"
   homepage "https://github.com/Sequel-Ace/Sequel-Ace"
