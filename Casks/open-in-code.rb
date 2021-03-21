223d4fde3ae66ece9b16678234dfded7661c5500
Markus Reiter <me@reitermark.us>
2021-03-21 20:16:27 +0100

    open-in-code: add livecheck (#102059)

--- Casks/open-in-code.rb
+++ Casks/open-in-code.rb
@@ -3,7 +3,6 @@ cask "open-in-code" do
   sha256 "a473c247179c91621ef447f75f3103e1c9bc1459f6622b108e0c7bdd5b6f8367"
 
   url "https://github.com/sozercan/OpenInCode/releases/download/v#{version}/OpenInCodeLight.zip"
-  appcast "https://github.com/sozercan/OpenInCode/releases.atom"
   name "OpenInCode"
   desc "Finder toolbar app to open current folder in Visual Studio Code"
   homepage "https://github.com/sozercan/OpenInCode"
