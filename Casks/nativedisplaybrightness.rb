5538cafc4b66b5a4d6c4639eafd43bf89563a62f
Markus Reiter <me@reitermark.us>
2021-03-21 21:50:44 +0100

    nativedisplaybrightness: add livecheck (#102023)

--- Casks/nativedisplaybrightness.rb
+++ Casks/nativedisplaybrightness.rb
@@ -3,7 +3,6 @@ cask "nativedisplaybrightness" do
   sha256 "2b79979892170036a5170746ca542518f20c980f5b5b08e067debd3e6fd14e11"
 
   url "https://github.com/Bensge/NativeDisplayBrightness/releases/download/v#{version}/NativeDisplayBrightness_#{version.dots_to_underscores}.app.zip"
-  appcast "https://github.com/Bensge/NativeDisplayBrightness/releases.atom"
   name "NativeDisplayBrightness"
   homepage "https://github.com/Bensge/NativeDisplayBrightness/"
 
