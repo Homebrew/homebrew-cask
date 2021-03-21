cc8e217afae6df04c5872356704a2f7097cecde4
Seeker <meaningseeking@protonmail.com>
2021-03-20 18:37:19 -0700

    Update hopper-debugger-server from 2.6 to 2.7 (#101693)

--- Casks/hopper-debugger-server.rb
+++ Casks/hopper-debugger-server.rb
@@ -1,6 +1,6 @@
 cask "hopper-debugger-server" do
-  version "2.6"
-  sha256 "dde3d5949a9550e4a62d135acfcd6cefa930c69d4ea07c6e4056d6e1cca136d3"
+  version "2.7"
+  sha256 "dd23a36d3ee52c972c251f692491878509beeadced79a85f170d8113de21dc28"
 
   url "https://www.hopperapp.com/HopperGDBServer/HopperDebuggerServer-#{version}.zip",
       user_agent: :fake
