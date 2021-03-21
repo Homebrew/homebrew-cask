2f928c642abf6aac18ba86679af1fcdad8e3f96b
Markus Reiter <me@reitermark.us>
2021-03-21 20:16:13 +0100

    openinterminal: add livecheck (#102064)

--- Casks/openinterminal.rb
+++ Casks/openinterminal.rb
@@ -3,7 +3,6 @@ cask "openinterminal" do
   sha256 "54dfbe8d21b4cb46885ed5ebc34fedf8382acfeb50040828d52b0a0b8a7c885b"
 
   url "https://github.com/Ji4n1ng/OpenInTerminal/releases/download/v#{version}/OpenInTerminal.app.zip"
-  appcast "https://github.com/Ji4n1ng/OpenInTerminal/releases.atom"
   name "OpenInTerminal"
   desc "Finder Toolbar app to open the current directory in Terminal or Editor"
   homepage "https://github.com/Ji4n1ng/OpenInTerminal"
