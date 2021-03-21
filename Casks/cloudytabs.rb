00029bc94e700b1fd4cad1f5e770c9a6775b5054
Markus Reiter <me@reitermark.us>
2021-03-21 02:15:00 +0100

    cloudytabs: add livecheck

--- Casks/cloudytabs.rb
+++ Casks/cloudytabs.rb
@@ -3,7 +3,6 @@ cask "cloudytabs" do
   sha256 "ce3f7c71b134798bbcf607cfacea215c20f1d527d3e3464edbd6510df26e5dac"
 
   url "https://github.com/josh-/CloudyTabs/releases/download/v#{version}/CloudyTabs.zip"
-  appcast "https://github.com/josh-/CloudyTabs/releases.atom"
   name "CloudyTabs"
   desc "Menu bar application that lists iCloud Tabs"
   homepage "https://github.com/josh-/CloudyTabs/"
