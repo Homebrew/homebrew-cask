9cb66a50b35ff3bb0574d3ec259a0ee461d6224c
Markus Reiter <me@reitermark.us>
2021-03-21 20:25:19 +0100

    reactotron: add livecheck

--- Casks/reactotron.rb
+++ Casks/reactotron.rb
@@ -3,7 +3,6 @@ cask "reactotron" do
   sha256 "0b498386dd5feecd8f5137a629cf2f3170c6c92f1d33eb8e40831e4daddfc959"
 
   url "https://github.com/infinitered/reactotron/releases/download/v#{version}/Reactotron-#{version}-mac.zip"
-  appcast "https://github.com/infinitered/reactotron/releases.atom"
   name "Reactotron"
   desc "Desktop app for inspecting React JS and React Native projects"
   homepage "https://github.com/infinitered/reactotron"
