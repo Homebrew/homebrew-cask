3cb2040db20f55c1452f510563c69d1f14a3040a
Markus Reiter <me@reitermark.us>
2021-03-21 05:39:52 +0100

    autovolume: add livecheck (#101595)

--- Casks/autovolume.rb
+++ Casks/autovolume.rb
@@ -3,7 +3,6 @@ cask "autovolume" do
   sha256 "41f242ce1608c7a48fab254273e0f018e9b916c1d1f5ab307e5b5534747193c8"
 
   url "https://github.com/jesse-c/AutoVolume/releases/download/v#{version}/AutoVolume.app.zip"
-  appcast "https://github.com/jesse-c/AutoVolume/releases.atom"
   name "AutoVolume"
   desc "Tool that automatically sets the volume to a specified volume"
   homepage "https://github.com/jesse-c/AutoVolume"
