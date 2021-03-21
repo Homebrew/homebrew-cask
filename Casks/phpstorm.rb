33875c5d3a9d750e6d3bb5465ff34278ebe745eb
Noah Peeters <noah@noahpeeters.de>
2021-03-21 18:47:17 +0100

    Update checksum of m1 mac download in phpstorm.rb (#101972)

--- Casks/phpstorm.rb
+++ Casks/phpstorm.rb
@@ -5,7 +5,7 @@ cask "phpstorm" do
     sha256 "3c72eea3d7da78e9183d7f3e64de69bb577aeaabb0e595044837517545d5e1a4"
     url "https://download.jetbrains.com/webide/PhpStorm-#{version}.dmg"
   else
-    sha256 "b1be4b4dfc7362f47ecc88ab3916b9515960e72fa59c411c25b1186abf2f8b96"
+    sha256 "b0538762e97c0d35e0452d78373d406aec6ac20151b21f4dd5f9ce8e510bf7c4"
     url "https://download.jetbrains.com/webide/PhpStorm-#{version}-aarch64.dmg"
   end
 
