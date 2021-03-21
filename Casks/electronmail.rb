883297da9b75ef9191261b2ab2a6fcb4c842d819
Markus Reiter <me@reitermark.us>
2021-03-21 05:37:21 +0100

    electronmail: add livecheck (#101755)

--- Casks/electronmail.rb
+++ Casks/electronmail.rb
@@ -3,7 +3,6 @@ cask "electronmail" do
   sha256 "4bcff8555b7217e6339c281a4e36d776ee50fd43207326161e5edee6f4e83337"
 
   url "https://github.com/vladimiry/ElectronMail/releases/download/v#{version}/electron-mail-#{version}-mac.dmg"
-  appcast "https://github.com/vladimiry/ElectronMail/releases.atom"
   name "ElectronMail"
   desc "Unofficial ProtonMail Desktop App"
   homepage "https://github.com/vladimiry/ElectronMail"
