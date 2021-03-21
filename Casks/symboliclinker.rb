1ac57593fbd794d962c1adcc0522fdbbf39dd2ce
Markus Reiter <me@reitermark.us>
2021-03-21 23:36:05 +0100

    symboliclinker: add livecheck (#102245)

--- Casks/symboliclinker.rb
+++ Casks/symboliclinker.rb
@@ -3,7 +3,6 @@ cask "symboliclinker" do
   sha256 "80257a720531d448c6034e74a494e68e82f293af14ef56edbb7379f658542e84"
 
   url "https://github.com/nickzman/symboliclinker/releases/download/v#{version}/SymbolicLinker#{version}.dmg"
-  appcast "https://github.com/nickzman/symboliclinker/releases.atom"
   name "SymbolicLinker"
   desc "Service that allows users to make symbolic links in the Finder"
   homepage "https://github.com/nickzman/symboliclinker"
