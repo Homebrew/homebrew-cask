72c1915707162ddacb097263a41c8df77694e843
Markus Reiter <me@reitermark.us>
2021-03-21 05:01:38 +0100

    easy-move-plus-resize: add livecheck

--- Casks/easy-move-plus-resize.rb
+++ Casks/easy-move-plus-resize.rb
@@ -3,7 +3,6 @@ cask "easy-move-plus-resize" do
   sha256 "b68ec37dd8243f20a79d4c80b66bd1fad047074e23f05b22720e2088c4ac6ed1"
 
   url "https://github.com/dmarcotte/easy-move-resize/releases/download/#{version}/Easy.Move+Resize.app.zip"
-  appcast "https://github.com/dmarcotte/easy-move-resize/releases.atom"
   name "Easy Move+Resize"
   desc "Utility to support moving and resizing using a modifier key and mouse drag"
   homepage "https://github.com/dmarcotte/easy-move-resize"
