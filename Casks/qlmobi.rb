ed217bb650e74d12eaff46e39262bbe85aef3da8
Markus Reiter <me@reitermark.us>
2021-03-21 23:47:55 +0100

    qlmobi: add livecheck (#102133)

--- Casks/qlmobi.rb
+++ Casks/qlmobi.rb
@@ -3,7 +3,6 @@ cask "qlmobi" do
   sha256 "1035181685c3ed974bd45ade8f22717c62142ff6a75cf679128f8fb50ef26833"
 
   url "https://github.com/bfabiszewski/QLMobi/releases/download/v#{version}/QLMobi.qlgenerator.zip"
-  appcast "https://github.com/bfabiszewski/QLMobi/releases.atom"
   name "QLMobi"
   homepage "https://github.com/bfabiszewski/QLMobi"
 
