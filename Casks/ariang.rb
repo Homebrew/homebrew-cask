56b84fa7ea48d3dbb8db61b5f9e0a9b63356c7bd
Markus Reiter <me@reitermark.us>
2021-03-21 01:09:41 +0100

    ariang: add livecheck

--- Casks/ariang.rb
+++ Casks/ariang.rb
@@ -3,7 +3,6 @@ cask "ariang" do
   sha256 "797e0a0017f20afae8417baa7aeb2bba4723a33af3ed2c37d2495a06711ce3b4"
 
   url "https://github.com/mayswind/AriaNg-Native/releases/download/#{version}/AriaNg_Native-#{version}-macOS.dmg"
-  appcast "https://github.com/mayswind/AriaNg-Native/releases.atom"
   name "AriaNg Native"
   desc "Better aria2 desktop frontend than AriaNg"
   homepage "https://github.com/mayswind/AriaNg-Native"
