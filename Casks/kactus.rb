0102adf984244fef66e9fc7e47bbcfa424c38b8f
Markus Reiter <me@reitermark.us>
2021-03-21 17:42:51 +0100

    kactus: add livecheck (#101873)

--- Casks/kactus.rb
+++ Casks/kactus.rb
@@ -4,7 +4,6 @@ cask "kactus" do
 
   url "https://github.com/kactus-io/kactus/releases/download/v#{version}/Kactus-macos.zip",
       verified: "github.com/kactus-io/kactus/"
-  appcast "https://github.com/kactus-io/kactus/releases.atom"
   name "Kactus"
   desc "True version control tool for designers"
   homepage "https://kactus.io/"
