bbdd504fd6da32e5e4ada73621e75c2cc0245086
Markus Reiter <me@reitermark.us>
2021-03-21 20:19:13 +0100

    packetproxy: add livecheck (#102080)

--- Casks/packetproxy.rb
+++ Casks/packetproxy.rb
@@ -3,7 +3,6 @@ cask "packetproxy" do
   sha256 "49343bb093be43b1adf1324078738312df5fee14c1c7465d9e5c59c9bc07593c"
 
   url "https://github.com/DeNA/PacketProxy/releases/download/#{version}/PacketProxy-#{version}-Installer-Mac-Signed.dmg"
-  appcast "https://github.com/DeNA/PacketProxy/releases.atom"
   name "PacketProxy"
   desc "Local proxy written in Java"
   homepage "https://github.com/DeNA/PacketProxy"
