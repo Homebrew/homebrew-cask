09a791df344db51732307d1e9cf63fba42b01c19
Markus Reiter <me@reitermark.us>
2021-03-21 21:36:14 +0100

    qview: add livecheck (#102154)

--- Casks/qview.rb
+++ Casks/qview.rb
@@ -3,7 +3,6 @@ cask "qview" do
   sha256 "bc8385149973c0564602d39bb5163de5b26bbc52f2c523a884f3310855a37c8d"
 
   url "https://github.com/jurplel/qView/releases/download/#{version}/qView-#{version}.dmg"
-  appcast "https://github.com/jurplel/qView/releases.atom"
   name "qView"
   homepage "https://github.com/jurplel/qView"
 
