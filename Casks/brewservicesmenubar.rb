f3987d0b22bfb4aedb82d51708a280b41fff0d84
Markus Reiter <me@reitermark.us>
2021-03-21 05:27:37 +0100

    brewservicesmenubar: add livecheck (#101645)

--- Casks/brewservicesmenubar.rb
+++ Casks/brewservicesmenubar.rb
@@ -3,7 +3,6 @@ cask "brewservicesmenubar" do
   sha256 "13c8401bc212b65d38a4aa96daf7f98f76e27758ac29fc80fb25e21c989b194d"
 
   url "https://github.com/andrewn/brew-services-menubar/releases/download/v#{version}/BrewServicesMenubar.zip"
-  appcast "https://github.com/andrewn/brew-services-menubar/releases.atom"
   name "Brew Services Menubar"
   homepage "https://github.com/andrewn/brew-services-menubar"
 
