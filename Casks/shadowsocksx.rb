d6bc2ce1317a9d960aa4b1a2e4bd05107b437407
Markus Reiter <me@reitermark.us>
2021-03-21 21:34:42 +0100

    shadowsocksx: add livecheck (#102190)

--- Casks/shadowsocksx.rb
+++ Casks/shadowsocksx.rb
@@ -3,7 +3,6 @@ cask "shadowsocksx" do
   sha256 "f0e263dd1d74b0b6977389f2b8b28c524bceccbc5ad24ca8a8164b92ede1c45a"
 
   url "https://github.com/shadowsocks/shadowsocks-iOS/releases/download/#{version}/ShadowsocksX-#{version}.dmg"
-  appcast "https://github.com/shadowsocks/shadowsocks-iOS/releases.atom"
   name "ShadowsocksX"
   desc "Removed according to regulations"
   homepage "https://github.com/shadowsocks/shadowsocks-iOS/wiki/Shadowsocks-for-OSX-Help"
