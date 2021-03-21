807410e521ab29e42f2cc6e55ed09ff8765957e3
Seeker <meaningseeking@protonmail.com>
2021-03-20 17:33:44 -0700

    aldente: skip livecheck on Catalina or older (#101463)

--- Casks/aldente.rb
+++ Casks/aldente.rb
@@ -3,13 +3,16 @@ cask "aldente" do
     version "1.2"
     sha256 "a588dc29faca894b7321e23420ca17d6a944b9b3b46412435f519b96e4ebee7b"
     url "https://github.com/davidwernhart/AlDente/releases/download/#{version}/AlDente.app.zip"
+
+    livecheck do
+      skip
+    end
   else
     version "2.1.2"
     sha256 "39cebc2db5b75c4217abd42470ecfee8280a35178b0d2581df3857cd8b742880"
     url "https://github.com/davidwernhart/AlDente/releases/download/#{version}/AlDente_#{version}_Notarized.app.zip"
   end
 
-  appcast "https://github.com/davidwernhart/AlDente/releases.atom"
   name "AlDente"
   desc "Menu bar tool to limit maximum charging percentage"
   homepage "https://github.com/davidwernhart/AlDente"
