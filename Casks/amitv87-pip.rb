b864f56e631e4d98dccb7a648a0cffaa8357030e
Markus Reiter <me@reitermark.us>
2021-03-21 05:28:48 +0100

    amitv87-pip: add livecheck (#101560)

--- Casks/amitv87-pip.rb
+++ Casks/amitv87-pip.rb
@@ -3,7 +3,6 @@ cask "amitv87-pip" do
   sha256 "8df2ae6c3a4bdfadeb477d598d0c09def97d78675ebd8f8af83ab476641b2d16"
 
   url "https://github.com/amitv87/PiP/releases/download/v#{version}/PiP-#{version}.dmg"
-  appcast "https://github.com/amitv87/PiP/releases.atom"
   name "PiP"
   desc "Always on top window preview"
   homepage "https://github.com/amitv87/PiP"
