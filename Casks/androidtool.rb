e267ccacaeba1172f9ef4344841ac52274ad5a77
Markus Reiter <me@reitermark.us>
2021-03-21 05:28:52 +0100

    androidtool: add livecheck (#101567)

--- Casks/androidtool.rb
+++ Casks/androidtool.rb
@@ -3,7 +3,6 @@ cask "androidtool" do
   sha256 "4f432ad45660071a1f36c4878bd444dd8bedd520c7c70096e5d7a7907ff7c86a"
 
   url "https://github.com/mortenjust/androidtool-mac/releases/download/#{version}/AndroidTool.zip"
-  appcast "https://github.com/mortenjust/androidtool-mac/releases.atom"
   name "AndroidTool"
   desc "App for recording the screen and installing apps in iOS and Android"
   homepage "https://github.com/mortenjust/androidtool-mac"
