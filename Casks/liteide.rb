26b65c71d045421d4f917d8a65d0f28bc9673d69
Markus Reiter <me@reitermark.us>
2021-03-21 18:13:07 +0100

    liteide: add livecheck (#101932)

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -3,7 +3,6 @@ cask "liteide" do
   sha256 "f739a15d05c227dc53c9501bfa8de917b1b2b7dcc8517c156a49bc55b75e8e67"
 
   url "https://github.com/visualfc/liteide/releases/download/x#{version}/liteidex#{version}.macos-qt5.12.5.zip"
-  appcast "https://github.com/visualfc/liteide/releases.atom"
   name "LiteIDE"
   desc "Go IDE"
   homepage "https://github.com/visualfc/liteide"
