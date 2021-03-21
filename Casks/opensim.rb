e17f8605b51ec8e9b8654662fef925cc3ccc1ca7
Markus Reiter <me@reitermark.us>
2021-03-21 22:13:13 +0100

    opensim: add livecheck (#102073)

--- Casks/opensim.rb
+++ Casks/opensim.rb
@@ -3,7 +3,6 @@ cask "opensim" do
   sha256 "4390edc443be22b5659ff9d6f69a66e0021c9f57e063147be79600235ceadfdb"
 
   url "https://github.com/luosheng/OpenSim/releases/download/#{version}/OpenSim.app.zip"
-  appcast "https://github.com/luosheng/OpenSim/releases.atom"
   name "OpenSim"
   desc "Open-source alternative to SimPholders, written in Swift"
   homepage "https://github.com/luosheng/OpenSim/"
