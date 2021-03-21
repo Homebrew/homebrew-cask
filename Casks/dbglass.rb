fafbbe33c5d20942ba7755f7829a07094d6dce42
Markus Reiter <me@reitermark.us>
2021-03-21 05:08:23 +0100

    dbglass: add livecheck (#101720)

--- Casks/dbglass.rb
+++ Casks/dbglass.rb
@@ -4,10 +4,13 @@ cask "dbglass" do
 
   url "https://github.com/web-pal/DBGlass/releases/download/v#{version}/MAC_OS-X.zip",
       verified: "github.com/web-pal/DBGlass/"
-  appcast "https://github.com/web-pal/dbglass/releases.atom"
   name "DBGlass"
   desc "PostgreSQL client built with Electron"
   homepage "http://dbglass.web-pal.com/"
 
   app "DBGlass-darwin-x64/DBGlass.app"
+
+  caveats do
+    discontinued
+  end
 end
