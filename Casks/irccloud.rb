03fffd85b73ef5891c998db6e1681cc8146faa68
Markus Reiter <me@reitermark.us>
2021-03-21 17:48:41 +0100

    irccloud: add livecheck (#101853)

--- Casks/irccloud.rb
+++ Casks/irccloud.rb
@@ -3,7 +3,6 @@ cask "irccloud" do
   sha256 "e942432cc457ce275960f6e5f120cc7be914ee06dc45fc88f9566a9aae251d79"
 
   url "https://github.com/irccloud/irccloud-desktop/releases/download/v#{version}/IRCCloud-#{version}-universal.dmg"
-  appcast "https://github.com/irccloud/irccloud-desktop/releases.atom"
   name "IRCCloud Desktop"
   desc "IRC client"
   homepage "https://github.com/irccloud/irccloud-desktop"
