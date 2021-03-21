096bee20e360f316ac5ac062a02dfc1fedb8e940
Markus Reiter <me@reitermark.us>
2021-03-21 17:49:15 +0100

    jd-gui: add livecheck (#101861)

--- Casks/jd-gui.rb
+++ Casks/jd-gui.rb
@@ -4,7 +4,6 @@ cask "jd-gui" do
 
   url "https://github.com/java-decompiler/jd-gui/releases/download/v#{version}/jd-gui-osx-#{version}.tar",
       verified: "github.com/java-decompiler/jd-gui/"
-  appcast "https://github.com/java-decompiler/jd-gui/releases.atom"
   name "JD-GUI"
   desc "Standalone Java Decompiler GUI"
   homepage "http://jd.benow.ca/"
