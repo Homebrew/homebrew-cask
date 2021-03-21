4554cea124d00dd187b12bccf95fee1805a21b6a
Markus Reiter <me@reitermark.us>
2021-03-21 18:26:27 +0100

    losslesscut: add livecheck (#101942)

--- Casks/losslesscut.rb
+++ Casks/losslesscut.rb
@@ -3,7 +3,6 @@ cask "losslesscut" do
   sha256 "b20ddb49483815dfd81a55c206bee276c84d146b54585ff63cfd64c5d7564175"
 
   url "https://github.com/mifi/lossless-cut/releases/download/v#{version}/LosslessCut-mac.dmg"
-  appcast "https://github.com/mifi/lossless-cut/releases.atom"
   name "LosslessCut"
   desc "Trimms video and audio files losslessly"
   homepage "https://github.com/mifi/lossless-cut"
