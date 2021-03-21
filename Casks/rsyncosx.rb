43d200ff6e387f742e6afa6813b18fd6decee9d2
Markus Reiter <me@reitermark.us>
2021-03-21 21:35:15 +0100

    rsyncosx: add livecheck (#102175)

--- Casks/rsyncosx.rb
+++ Casks/rsyncosx.rb
@@ -3,7 +3,6 @@ cask "rsyncosx" do
   sha256 "b123c4c4c70944cdf0f86d867e815710ea461b62d109d8974035ad6b05621e59"
 
   url "https://github.com/rsyncOSX/RsyncOSX/releases/download/v#{version}/RsyncOSX.#{version}.dmg"
-  appcast "https://github.com/rsyncOSX/RsyncOSX/releases.atom"
   name "RsyncOSX"
   desc "GUI for rsync"
   homepage "https://github.com/rsyncOSX/RsyncOSX"
