4bf658eb555a26cec5e236a6ad9b6c208cb9a920
Markus Reiter <me@reitermark.us>
2021-03-22 00:12:49 +0100

    trilium-notes: add livecheck (#102289)

--- Casks/trilium-notes.rb
+++ Casks/trilium-notes.rb
@@ -3,7 +3,6 @@ cask "trilium-notes" do
   sha256 "0bbb37ff4a0ffb25873195d18ac54a41edb9b835502fe2c5ac21c1ecdafb52fc"
 
   url "https://github.com/zadam/trilium/releases/download/v#{version}/trilium-mac-x64-#{version}.zip"
-  appcast "https://github.com/zadam/trilium/releases.atom"
   name "Trilium Notes"
   desc "Personal knowledge base"
   homepage "https://github.com/zadam/trilium"
