172b485098488f58ce825777a55b2d38df7f2f95
Markus Reiter <me@reitermark.us>
2021-03-21 22:15:31 +0100

    thonny-xxl: add livecheck (#102258)

--- Casks/thonny-xxl.rb
+++ Casks/thonny-xxl.rb
@@ -4,7 +4,6 @@ cask "thonny-xxl" do
 
   url "https://github.com/thonny/thonny/releases/download/v#{version}/thonny-xxl-#{version}.pkg",
       verified: "github.com/thonny/thonny/"
-  appcast "https://github.com/thonny/thonny/releases.atom"
   name "Thonny (XXL bundle)"
   desc "Python IDE for beginners"
   homepage "https://thonny.org/"
