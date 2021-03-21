5a2f22bba633b16c957a58094e22967c6e630ca0
Markus Reiter <me@reitermark.us>
2021-03-21 17:48:18 +0100

    inloop-qlplayground: add livecheck (#101849)

--- Casks/inloop-qlplayground.rb
+++ Casks/inloop-qlplayground.rb
@@ -3,7 +3,6 @@ cask "inloop-qlplayground" do
   sha256 "44c25a7da0dc3748b01deb0c01634044ccbc625b4266b4fea0630cbedb773929"
 
   url "https://github.com/inloop/qlplayground/releases/download/v#{version}/inloop-qlplayground.v#{version}.zip"
-  appcast "https://github.com/inloop/qlplayground/releases.atom"
   name "inloop-qlplayground"
   homepage "https://github.com/inloop/qlplayground"
 
