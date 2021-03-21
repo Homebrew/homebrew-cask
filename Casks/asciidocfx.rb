f47908b98bc0965f3349bbba6f8a8fe58fcf43a0
Markus Reiter <me@reitermark.us>
2021-03-21 05:29:29 +0100

    asciidocfx: add livecheck (#101585)

--- Casks/asciidocfx.rb
+++ Casks/asciidocfx.rb
@@ -4,7 +4,6 @@ cask "asciidocfx" do
 
   url "https://github.com/asciidocfx/AsciidocFX/releases/download/v#{version}/AsciidocFX_Mac.dmg",
       verified: "github.com/asciidocfx/AsciidocFX/"
-  appcast "https://github.com/asciidocfx/AsciidocFX/releases.atom"
   name "AsciidocFX"
   desc "Asciidoc Editor and Toolchain to build books, documents and slides"
   homepage "https://www.asciidocfx.com/"
