c55607158ddcc253bc1e56d0208c5b91567ecfc2
Markus Reiter <me@reitermark.us>
2021-03-21 05:06:39 +0100

    countdown: add livecheck (#101706)

--- Casks/countdown.rb
+++ Casks/countdown.rb
@@ -3,7 +3,6 @@ cask "countdown" do
   sha256 "3616d9c01d79e14f8db01d492c882700bfcca6c4bf886f1a73e36ad04893213c"
 
   url "https://github.com/soffes/Countdown/releases/download/v#{version}/Countdown.saver-#{version}.zip"
-  appcast "https://github.com/soffes/Countdown/releases.atom"
   name "Countdown Screensaver"
   homepage "https://github.com/soffes/Countdown"
 
