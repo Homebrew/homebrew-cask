2a1ad46d8a55cd4191117d7d81983ae21e166fbb
Markus Reiter <me@reitermark.us>
2021-03-21 23:33:09 +0100

    stats: add livecheck (#102227)

--- Casks/stats.rb
+++ Casks/stats.rb
@@ -3,7 +3,6 @@ cask "stats" do
   sha256 "52110de8d27a5683c9581d74d0f5568006c7624d69e5833b42a0c2bf7803b3f9"
 
   url "https://github.com/exelban/stats/releases/download/v#{version}/Stats.dmg"
-  appcast "https://github.com/exelban/stats/releases.atom"
   name "Stats"
   desc "System monitor for the menu bar"
   homepage "https://github.com/exelban/stats"
