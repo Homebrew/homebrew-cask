bd71739eb08f3f94391ccd3de72f1496f39bc156
Markus Reiter <me@reitermark.us>
2021-03-22 00:49:49 +0100

    workbench: add livecheck (#102340)

--- Casks/workbench.rb
+++ Casks/workbench.rb
@@ -3,7 +3,6 @@ cask "workbench" do
   sha256 "7fec7bf1fb93854ed87cbaf4d9e21ef964adfc064d731b82a84a499f5b911e1f"
 
   url "https://github.com/mxcl/Workbench/releases/download/#{version}/Workbench-#{version}.zip"
-  appcast "https://github.com/mxcl/Workbench/releases.atom"
   name "Workbench"
   desc "Seamless, automatic, “dotfile” sync to iCloud"
   homepage "https://github.com/mxcl/Workbench"
