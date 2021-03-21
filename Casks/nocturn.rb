919f4222dba599b2320407c3f2889b5c2a656fd0
Markus Reiter <me@reitermark.us>
2021-03-21 22:12:55 +0100

    nocturn: add livecheck (#102035)

--- Casks/nocturn.rb
+++ Casks/nocturn.rb
@@ -3,7 +3,6 @@ cask "nocturn" do
   sha256 "5ec4c8d1e209d2a2f10d46ea8b4e0f16ddcdc28bebb76affba55560d7dfb3a2d"
 
   url "https://github.com/k0kubun/Nocturn/releases/download/v#{version}/Nocturn-darwin-x64.zip"
-  appcast "https://github.com/k0kubun/Nocturn/releases.atom"
   name "Nocturn"
   desc "Multi-platform Twitter client"
   homepage "https://github.com/k0kubun/Nocturn"
