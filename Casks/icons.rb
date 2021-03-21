67c85286f40fb6942c7952ce3079a2076e5c5999
Markus Reiter <me@reitermark.us>
2021-03-21 17:48:07 +0100

    icons: add livecheck (#101842)

--- Casks/icons.rb
+++ Casks/icons.rb
@@ -3,7 +3,6 @@ cask "icons" do
   sha256 "aff6836c0425c845afbc4d71579ebd8adf4d161f03413939ee8579b23782159a"
 
   url "https://github.com/exherb/icons/releases/download/#{version}/icons-v#{version}-macos-x64.zip"
-  appcast "https://github.com/exherb/icons/releases.atom"
   name "Icons"
   desc "Tool to generate icons for apps"
   homepage "https://github.com/exherb/icons"
