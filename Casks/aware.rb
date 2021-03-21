af28b6d074c3ee8938b8d12b41b121a219b7bce4
Markus Reiter <me@reitermark.us>
2021-03-21 01:18:06 +0100

    aware: add livecheck

--- Casks/aware.rb
+++ Casks/aware.rb
@@ -4,7 +4,6 @@ cask "aware" do
 
   url "https://github.com/josh/Aware/releases/download/v#{version}/Aware.zip",
       verified: "github.com/josh/Aware/"
-  appcast "https://github.com/josh/Aware/releases.atom"
   name "Aware"
   homepage "https://awaremac.com/"
 
