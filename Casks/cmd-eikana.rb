ee83c356e63b05553115ee63cfa1f0bfb710a1cf
Markus Reiter <me@reitermark.us>
2021-03-21 05:41:32 +0100

    cmd-eikana: add livecheck (#101684)

--- Casks/cmd-eikana.rb
+++ Casks/cmd-eikana.rb
@@ -3,7 +3,6 @@ cask "cmd-eikana" do
   sha256 "8e4157304ae21566339e956423632d34aacd12c96e87f35ffc83bf2304ff9be4"
 
   url "https://github.com/iMasanari/cmd-eikana/releases/download/v#{version}/eikana-#{version}.app.zip"
-  appcast "https://github.com/iMasanari/cmd-eikana/releases.atom"
   name "cmd-eikana"
   name "⌘英かな"
   homepage "https://github.com/iMasanari/cmd-eikana"
