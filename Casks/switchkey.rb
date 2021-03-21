3b83483a14857c34637e4bee9ddb8ce57b3b4e25
Markus Reiter <me@reitermark.us>
2021-03-21 23:36:02 +0100

    switchkey: add livecheck (#102244)

--- Casks/switchkey.rb
+++ Casks/switchkey.rb
@@ -3,7 +3,6 @@ cask "switchkey" do
   sha256 "a8a62231adddffc954a894b7fd00051d903de31a0e37d68787425bdc287399c8"
 
   url "https://github.com/itsuhane/SwitchKey/releases/download/v#{version}/SwitchKey.zip"
-  appcast "https://github.com/itsuhane/SwitchKey/releases.atom"
   name "SwitchKey"
   homepage "https://github.com/itsuhane/SwitchKey"
 
