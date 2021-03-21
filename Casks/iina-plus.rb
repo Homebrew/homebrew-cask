63d6e3f3a4212880f70e877cb16ce466974cd838
Yongqi WANG <wangyq977@gmail.com>
2021-03-21 05:08:48 +0100

    Add zap Stanza for iina-plus (#101563)

--- Casks/iina-plus.rb
+++ Casks/iina-plus.rb
@@ -5,8 +5,15 @@ cask "iina-plus" do
   url "https://github.com/xjbeta/iina-plus/releases/download/#{version.before_comma}(#{version.after_comma})/iina+.#{version.before_comma}.dmg"
   appcast "https://github.com/xjbeta/iina-plus/releases.atom"
   name "IINA+"
-  desc "Extra danmaku support for iina. (iina 弹幕支持"
+  desc "Extra danmaku support for iina (iina 弹幕支持)"
   homepage "https://github.com/xjbeta/iina-plus"
 
   app "iina+.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.xjbeta.iina-plus",
+    "~/Library/Caches/com.xjbeta.iina-plus",
+    "~/Library/Preferences/com.xjbeta.iina-plus.plist",
+    "~/Library/WebKit/com.xjbeta.iina-plus",
+  ]
 end
