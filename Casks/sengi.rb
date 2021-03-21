c511e9867b3d6014723c8f974c2cb2a693093767
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-03-21 16:27:47 -0700

    Update sengi from 1.1.2.1 to 1.1.3 (#102309)

--- Casks/sengi.rb
+++ Casks/sengi.rb
@@ -1,6 +1,6 @@
 cask "sengi" do
-  version "1.1.2.1"
-  sha256 "f1444e134ef2f43dc488de9543ab96a5b173919f3c2b40a02c2a52d786b4a8fb"
+  version "1.1.3"
+  sha256 "f46f87fc91d093e579b05c613b443737e5df31c8356f4367af4cfdc9ec3077e1"
 
   url "https://github.com/NicolasConstant/sengi/releases/download/#{version}/Sengi-#{version.major_minor_patch}-mac.dmg"
   name "Sengi"
