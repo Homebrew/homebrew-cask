7773b2dbb4153d11b3ec3d7880051b32ae1b0a7f
Markus Reiter <me@reitermark.us>
2021-03-21 05:15:38 +0100

    electronic-wechat: add livecheck (#101754)

--- Casks/electronic-wechat.rb
+++ Casks/electronic-wechat.rb
@@ -3,7 +3,6 @@ cask "electronic-wechat" do
   sha256 "eba20a9164e917f1a9962fc3202d2c1255a3d26802ae2fd1fe229feaba5b6242"
 
   url "https://github.com/geeeeeeeeek/electronic-wechat/releases/download/V#{version}/mac-osx.tar.gz"
-  appcast "https://github.com/geeeeeeeeek/electronic-wechat/releases.atom"
   name "Electronic WeChat"
   homepage "https://github.com/geeeeeeeeek/electronic-wechat"
 
