c239576b5d8e4fafeb04ce61f58c18ece8acd2fc
Markus Reiter <me@reitermark.us>
2021-03-21 19:35:39 +0100

    persepolis-download-manager: add livecheck

--- Casks/persepolis-download-manager.rb
+++ Casks/persepolis-download-manager.rb
@@ -4,7 +4,6 @@ cask "persepolis-download-manager" do
 
   url "https://github.com/persepolisdm/persepolis/releases/download/#{version}/persepolis_#{version}_mac.zip",
       verified: "github.com/persepolisdm/persepolis/"
-  appcast "https://github.com/persepolisdm/persepolis/releases.atom"
   name "Persepolis"
   desc "GUI for aria2"
   homepage "https://persepolisdm.github.io/"
