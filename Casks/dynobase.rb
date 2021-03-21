c6cac853f159a1808ef1b25cd2a6fcc273d4ebf4
Markus Reiter <me@reitermark.us>
2021-03-21 05:15:51 +0100

    dynobase: add livecheck (#101744)

--- Casks/dynobase.rb
+++ Casks/dynobase.rb
@@ -4,7 +4,6 @@ cask "dynobase" do
 
   url "https://github.com/Dynobase/dynobase/releases/download/#{version}/Dynobase-#{version}.dmg",
       verified: "github.com/Dynobase/dynobase/"
-  appcast "https://github.com/Dynobase/dynobase/releases.atom"
   name "Dynobase"
   desc "GUI Client for DynamoDB"
   homepage "https://dynobase.dev/"
