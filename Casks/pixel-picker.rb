f04e2144447f5bf41ff629f3120732042adf8a73
Markus Reiter <me@reitermark.us>
2021-03-21 20:23:36 +0100

    pixel-picker: add livecheck (#102099)

--- Casks/pixel-picker.rb
+++ Casks/pixel-picker.rb
@@ -3,7 +3,6 @@ cask "pixel-picker" do
   sha256 "d31f9b9f3b5dda4827a215c20c4488c3151e4e14ada79dd744ad3097fe38beb9"
 
   url "https://github.com/acheronfail/pixel-picker/releases/download/#{version}/Pixel.Picker.#{version}.dmg"
-  appcast "https://github.com/acheronfail/pixel-picker/releases.atom"
   name "Pixel Picker"
   homepage "https://github.com/acheronfail/pixel-picker"
 
