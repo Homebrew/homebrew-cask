c547689d39c25458bf97eea4147d2496594d893b
Markus Reiter <me@reitermark.us>
2021-03-21 21:37:55 +0100

    rq: add livecheck (#102174)

--- Casks/rq.rb
+++ Casks/rq.rb
@@ -3,7 +3,6 @@ cask "rq" do
   sha256 "49f732b2aabf4eaff231e425edf710ca34e6bf730cff9a71adf79d11e630f883"
 
   url "https://github.com/dflemstr/rq/releases/download/v#{version}/rq-v#{version}-x86_64-apple-darwin.tar.gz"
-  appcast "https://github.com/dflemstr/rq/releases.atom"
   name "rq"
   homepage "https://github.com/dflemstr/rq"
 
