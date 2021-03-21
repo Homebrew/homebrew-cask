9359cfeef0e1810bcad9b4d38a75385fb1717d99
Markus Reiter <me@reitermark.us>
2021-03-21 05:49:47 +0100

    graphiql: add livecheck

--- Casks/graphiql.rb
+++ Casks/graphiql.rb
@@ -3,7 +3,6 @@ cask "graphiql" do
   sha256 "204dd3db5c11bf265700d8d57defc4798cd0040651f751f35e2a646cb4846ac7"
 
   url "https://github.com/skevy/graphiql-app/releases/download/v#{version}/graphiql-app-#{version}-mac.zip"
-  appcast "https://github.com/skevy/graphiql-app/releases.atom"
   name "GraphiQL App"
   desc "Light, Electron-based Wrapper around GraphiQL"
   homepage "https://github.com/skevy/graphiql-app"
