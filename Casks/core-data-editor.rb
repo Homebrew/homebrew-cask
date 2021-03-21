667931d63e24350d21a5d5ac138f0f9d5bc2a7ca
Markus Reiter <me@reitermark.us>
2021-03-21 05:06:52 +0100

    core-data-editor: add livecheck (#101702)

--- Casks/core-data-editor.rb
+++ Casks/core-data-editor.rb
@@ -3,7 +3,6 @@ cask "core-data-editor" do
   sha256 "949902f859efeba9b5d9112210f32cb1020f9b43d81f43489c559ddd8e20f0e2"
 
   url "https://github.com/ChristianKienle/Core-Data-Editor/releases/download/#{version}/Core.Data.Editor.zip"
-  appcast "https://github.com/ChristianKienle/Core-Data-Editor/releases.atom"
   name "Core Data Editor"
   homepage "https://github.com/ChristianKienle/Core-Data-Editor/"
 
