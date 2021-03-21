9c560b65e62d11dd0cb173c17d93e11e05a315fe
Markus Reiter <me@reitermark.us>
2021-03-21 20:21:53 +0100

    ramme: add livecheck

--- Casks/ramme.rb
+++ Casks/ramme.rb
@@ -3,7 +3,6 @@ cask "ramme" do
   sha256 "49a4f8d1623f855322a34e670e17261a8add1ac8b764ffcd027ccb71b82fb1b0"
 
   url "https://github.com/terkelg/ramme/releases/download/v#{version}/Ramme-#{version}.dmg"
-  appcast "https://github.com/terkelg/ramme/releases.atom"
   name "Ramme"
   desc "Unofficial Instagram Desktop App"
   homepage "https://github.com/terkelg/ramme/"
