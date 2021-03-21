4224844b8ecaa4bd993ec06683f3e96a3ad84e30
Markus Reiter <me@reitermark.us>
2021-03-21 05:28:24 +0100

    audioscrobbler: add livecheck (#101587)

--- Casks/audioscrobbler.rb
+++ Casks/audioscrobbler.rb
@@ -3,7 +3,6 @@ cask "audioscrobbler" do
   sha256 "d14bd947f32a5e6c17645d9378b69b0aded91b95dd87a9a8971e013a8fff5063"
 
   url "https://github.com/mxcl/Audioscrobbler.app/releases/download/#{version}/Audioscrobbler-#{version}.zip"
-  appcast "https://github.com/mxcl/Audioscrobbler.app/releases.atom"
   name "Audioscrobbler"
   desc "Minimal, robust iTunes scrobbling"
   homepage "https://github.com/mxcl/Audioscrobbler.app"
