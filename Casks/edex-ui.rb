322b60173cc03df9233c61484ce00bcbe7dc89e5
SeekerTestBot <68891797+SeekerTestBot@users.noreply.github.com>
2021-03-20 17:30:58 -0700

    Update edex-ui from 2.2.6 to 2.2.7 (#101601)

--- Casks/edex-ui.rb
+++ Casks/edex-ui.rb
@@ -1,6 +1,6 @@
 cask "edex-ui" do
-  version "2.2.6"
-  sha256 "a423de8c7ae20545d561ec1d9228a90fd2f40137787a1f7276356aaf5df6358c"
+  version "2.2.7"
+  sha256 "80b2b43e50e000da035efbdde21ca96106c021a5ef5b47e45fad8e9b4bb17a95"
 
   url "https://github.com/GitSquared/edex-ui/releases/download/v#{version}/eDEX-UI-macOS.dmg"
   appcast "https://github.com/GitSquared/edex-ui/releases.atom"
