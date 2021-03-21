b6b0e401680b5b5a306b615abe1baf16cd1c5aef
Markus Reiter <me@reitermark.us>
2021-03-21 23:35:33 +0100

    stringz: add livecheck (#102235)

--- Casks/stringz.rb
+++ Casks/stringz.rb
@@ -3,7 +3,6 @@ cask "stringz" do
   sha256 "4acf81f3f19d0fd8483347811575f9f0414d668c3d22b95c67ce2f47042ea4c2"
 
   url "https://github.com/mohakapt/Stringz/releases/download/v#{version}/Stringz-#{version}.dmg"
-  appcast "https://github.com/mohakapt/Stringz/releases.atom"
   name "Stringz"
   desc "Editor for localizable files"
   homepage "https://github.com/mohakapt/Stringz"
