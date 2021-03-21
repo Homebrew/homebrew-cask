a70a69927cc50085e7bdaf0293ace118bff55c24
Markus Reiter <me@reitermark.us>
2021-03-21 06:15:42 +0100

    flying-carpet: add livecheck (#101780)

--- Casks/flying-carpet.rb
+++ Casks/flying-carpet.rb
@@ -3,7 +3,6 @@ cask "flying-carpet" do
   sha256 "a38cb60ee732412d03c0372f1b1b3cc46cb83e94f33961c6f677afb4f71d4aaf"
 
   url "https://github.com/spieglt/FlyingCarpet/releases/download/#{version}/Flying.Carpet.Mac.zip"
-  appcast "https://github.com/spieglt/flyingcarpet/releases.atom"
   name "Flying Carpet"
   desc "File transfer over ad-hoc wifi"
   homepage "https://github.com/spieglt/flyingcarpet"
