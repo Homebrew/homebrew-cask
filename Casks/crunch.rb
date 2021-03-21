28907bd522125d0001db9783a33c2647d698c890
Markus Reiter <me@reitermark.us>
2021-03-21 05:07:32 +0100

    crunch: add livecheck (#101710)

--- Casks/crunch.rb
+++ Casks/crunch.rb
@@ -3,7 +3,6 @@ cask "crunch" do
   sha256 "6969fcb91e5a93b9d9e604cca2e6a98b6ebdcc7dde0c53803c25d379d3e4e729"
 
   url "https://github.com/chrissimpkins/Crunch/releases/download/v#{version}/Crunch-Installer.dmg"
-  appcast "https://github.com/chrissimpkins/Crunch/releases.atom"
   name "Crunch"
   desc "Insane(ly slow but wicked good) PNG image optimization"
   homepage "https://github.com/chrissimpkins/Crunch"
