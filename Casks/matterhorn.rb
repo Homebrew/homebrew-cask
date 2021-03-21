5988baa75b259f21ebcae51da653130ce7469d05
Markus Reiter <me@reitermark.us>
2021-03-21 18:24:48 +0100

    matterhorn: add livecheck

--- Casks/matterhorn.rb
+++ Casks/matterhorn.rb
@@ -3,7 +3,6 @@ cask "matterhorn" do
   sha256 "b2b5b4082824dc49ddef0bf2957352b2fd309a64517dfda962a9a75c15e89842"
 
   url "https://github.com/matterhorn-chat/matterhorn/releases/download/#{version}/matterhorn-#{version}-Darwin-x86_64.tar.bz2"
-  appcast "https://github.com/matterhorn-chat/matterhorn/releases.atom"
   name "Matterhorn"
   desc "Unix terminal client for Mattermost"
   homepage "https://github.com/matterhorn-chat/matterhorn"
