5f0e18c693e5a6604e755a74cabc334182a06694
Markus Reiter <me@reitermark.us>
2021-03-21 20:18:11 +0100

    noti: add livecheck (#102042)

--- Casks/noti.rb
+++ Casks/noti.rb
@@ -4,7 +4,6 @@ cask "noti" do
 
   url "https://github.com/jariz/Noti/releases/download/#{version}/Noti.dmg",
       verified: "github.com/jariz/Noti/"
-  appcast "https://github.com/jariz/Noti/releases.atom"
   name "Noti"
   desc "Utility to show notifications from an Android device"
   homepage "https://noti.center/"
