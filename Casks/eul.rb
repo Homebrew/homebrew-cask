ad27d3d2a7fc432afdc6b9ba791d2fce1deffa80
Markus Reiter <me@reitermark.us>
2021-03-21 05:23:07 +0100

    eul: add livecheck (#101766)

--- Casks/eul.rb
+++ Casks/eul.rb
@@ -3,7 +3,6 @@ cask "eul" do
   sha256 "ebdd5a7032373f8477831b3c25c318c0c87290f95a969d405ee603cdbf36da84"
 
   url "https://github.com/gao-sun/eul/releases/download/v#{version}/eul.app.zip"
-  appcast "https://github.com/gao-sun/eul/releases.atom"
   name "eul"
   desc "Status monitoring"
   homepage "https://github.com/gao-sun/eul"
