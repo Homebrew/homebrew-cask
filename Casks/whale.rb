02b385bddc4c575538339e00e9fd851a4ebff87e
Markus Reiter <me@reitermark.us>
2021-03-22 00:33:57 +0100

    whale: add livecheck

--- Casks/whale.rb
+++ Casks/whale.rb
@@ -3,7 +3,6 @@ cask "whale" do
   sha256 "d9498d0c478daa9f53d3db4a26124f821e9238de993de98299c0805dfccfa50b"
 
   url "https://github.com/1000ch/whale/releases/download/v#{version}/Whale-#{version}.dmg"
-  appcast "https://github.com/1000ch/whale/releases.atom"
   name "Whale"
   desc "Unofficial Trello app :whale:"
   homepage "https://github.com/1000ch/whale"
