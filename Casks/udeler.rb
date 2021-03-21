50cbea1154faad3283ff31003e421987006199f6
Markus Reiter <me@reitermark.us>
2021-03-22 00:18:37 +0100

    udeler: add livecheck (#102296)

--- Casks/udeler.rb
+++ Casks/udeler.rb
@@ -3,7 +3,6 @@ cask "udeler" do
   sha256 "5a2b65b7e5d4db864a0895a72b7fc76626ed4c2c0204abe50b84c0b6f81f6d7f"
 
   url "https://github.com/FaisalUmair/udemy-downloader-gui/releases/download/v#{version}/Udeler-#{version}-mac.dmg"
-  appcast "https://github.com/FaisalUmair/udemy-downloader-gui/releases.atom"
   name "Udeler"
   desc "Desktop application for downloading Udemy Courses"
   homepage "https://github.com/FaisalUmair/udemy-downloader-gui/"
