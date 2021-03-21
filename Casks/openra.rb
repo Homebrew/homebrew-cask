b148edab5c4efe8d4e6578e26417e0ef28c27bb4
Johannes Würbach <johannes.wuerbach@googlemail.com>
2021-03-21 22:24:25 +0100

    Update openra from 20200503 to 20210321

--- Casks/openra.rb
+++ Casks/openra.rb
@@ -1,6 +1,6 @@
 cask "openra" do
-  version "20200503"
-  sha256 "0240e82003fc46af6b321efd0a5ef7703f4f0293fe75212ef29cad67a705c4fb"
+  version "20210321"
+  sha256 "20346073731e3dc0ccfc79ce05abe2e1e3a9d815fb945c0683169d455c6c1004"
 
   url "https://github.com/OpenRA/OpenRA/releases/download/release-#{version}/OpenRA-release-#{version}.dmg",
       verified: "github.com/OpenRA/OpenRA/"
