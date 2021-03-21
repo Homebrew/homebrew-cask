11bd16b657ff5658c5e437232e22447ad707c98b
Markus Reiter <me@reitermark.us>
2021-03-21 20:56:54 +0100

    qmoji: add livecheck (#102139)

--- Casks/qmoji.rb
+++ Casks/qmoji.rb
@@ -3,7 +3,6 @@ cask "qmoji" do
   sha256 "dbe2d6de7bbbba3160434d8638fdccb2cfa192ecd7cbb4e5d573df74e9f740c5"
 
   url "https://github.com/jaredly/qmoji/releases/download/#{version}/qmoji.zip"
-  appcast "https://github.com/jaredly/qmoji/releases.atom"
   name "qmoji"
   desc "Like mojibar, but written in reasonml"
   homepage "https://github.com/jaredly/qmoji"
