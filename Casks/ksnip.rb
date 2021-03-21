1c5e9350e93e1e6b5278b1ab869eefa92d0f5459
Markus Reiter <me@reitermark.us>
2021-03-21 17:43:17 +0100

    ksnip: add livecheck (#101889)

--- Casks/ksnip.rb
+++ Casks/ksnip.rb
@@ -3,7 +3,6 @@ cask "ksnip" do
   sha256 "5900023835668cac169ddc9ec5cea8aca44ecc4611dbca068e04a6d879feccf5"
 
   url "https://github.com/ksnip/ksnip/releases/download/v#{version}/ksnip-#{version}.dmg"
-  appcast "https://github.com/ksnip/ksnip/releases.atom"
   name "ksnip"
   homepage "https://github.com/ksnip/ksnip"
 
