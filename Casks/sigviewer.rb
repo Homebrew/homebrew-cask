69d7992829f6142ba5e4fdf3fe0c2d9abc5a8426
Markus Reiter <me@reitermark.us>
2021-03-22 00:13:21 +0100

    sigviewer: add livecheck (#102200)

--- Casks/sigviewer.rb
+++ Casks/sigviewer.rb
@@ -3,7 +3,6 @@ cask "sigviewer" do
   sha256 "ae253cbf79376fb2de9c7d334b306ca08e615d90e446ebbfbcebefebce81bb64"
 
   url "https://github.com/cbrnr/sigviewer/releases/download/v#{version}/sigviewer-#{version}-macos.dmg"
-  appcast "https://github.com/cbrnr/sigviewer/releases.atom"
   name "SigViewer"
   homepage "https://github.com/cbrnr/sigviewer"
 
