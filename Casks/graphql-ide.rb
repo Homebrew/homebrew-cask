5976815f78466951c87d3c859e42a5984180e912
Markus Reiter <me@reitermark.us>
2021-03-21 06:06:18 +0100

    graphql-ide: add livecheck (#101816)

--- Casks/graphql-ide.rb
+++ Casks/graphql-ide.rb
@@ -3,7 +3,6 @@ cask "graphql-ide" do
   sha256 "e7aa74f9dfc4874138d9fa74af33eb701130f5abe8ff70735c38c506f732408c"
 
   url "https://github.com/andev-software/graphql-ide/releases/download/v#{version}/GraphQL.IDE.zip"
-  appcast "https://github.com/andev-software/graphql-ide/releases.atom"
   name "GraphQL IDE"
   desc "IDE for exploring GraphQL APIs"
   homepage "https://github.com/andev-software/graphql-ide"
