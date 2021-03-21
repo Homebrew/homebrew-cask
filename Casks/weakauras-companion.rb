855c7168f9578910ac6a629025a6d821ff5e2d57
Markus Reiter <me@reitermark.us>
2021-03-22 00:29:11 +0100

    weakauras-companion: add livecheck

--- Casks/weakauras-companion.rb
+++ Casks/weakauras-companion.rb
@@ -4,7 +4,6 @@ cask "weakauras-companion" do
 
   url "https://github.com/WeakAuras/WeakAuras-Companion/releases/download/v#{version}/WeakAuras-Companion-#{version}.dmg",
       verified: "github.com/WeakAuras/WeakAuras-Companion/"
-  appcast "https://github.com/WeakAuras/WeakAuras-Companion/releases.atom"
   name "WeakAuras Companion"
   homepage "https://weakauras.wtf/"
 
