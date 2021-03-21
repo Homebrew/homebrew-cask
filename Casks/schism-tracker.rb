1fd67c5486eca73870e2d7af7ace489b4d2beced
Markus Reiter <me@reitermark.us>
2021-03-21 21:52:24 +0100

    schism-tracker: add livecheck (#102180)

--- Casks/schism-tracker.rb
+++ Casks/schism-tracker.rb
@@ -3,7 +3,6 @@ cask "schism-tracker" do
   sha256 "0f43b52949d60787eecc4fd5679f01694f114c17f6ee05a20d49ac995bfa6445"
 
   url "https://github.com/schismtracker/schismtracker/releases/download/#{version}/schismtracker-#{version}-osx.tar.gz"
-  appcast "https://github.com/schismtracker/schismtracker/releases.atom"
   name "Schism Tracker"
   desc "Oldschool sample-based music composition tool"
   homepage "https://github.com/schismtracker/schismtracker"
