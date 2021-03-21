a6b508c7122b0d97a619de0717ce2f245a625cc9
Markus Reiter <me@reitermark.us>
2021-03-21 20:43:51 +0100

    putio-adder: add livecheck (#102124)

--- Casks/putio-adder.rb
+++ Casks/putio-adder.rb
@@ -3,7 +3,6 @@ cask "putio-adder" do
   sha256 "de25d4b698118ce459794121e8a58279820c3872dba7021255097aac021b375f"
 
   url "https://github.com/nicoSWD/put.io-adder/releases/download/v#{version}/put.io-adder-v#{version}.zip"
-  appcast "https://github.com/nicoSWD/put.io-adder/releases.atom"
   name "Put.IO Adder"
   homepage "https://github.com/nicoSWD/put.io-adder"
 
