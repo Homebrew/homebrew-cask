56d388c52bd18247e21dcb21218bef0ed9475d50
Markus Reiter <me@reitermark.us>
2021-03-21 05:30:02 +0100

    falcon-sql-client: add livecheck (#101770)

--- Casks/falcon-sql-client.rb
+++ Casks/falcon-sql-client.rb
@@ -4,7 +4,6 @@ cask "falcon-sql-client" do
 
   url "https://github.com/plotly/falcon/releases/download/v#{version}/mac-falcon-v#{version}.zip",
       verified: "github.com/plotly/falcon/"
-  appcast "https://github.com/plotly/falcon/releases.atom"
   name "Falcon SQL Client"
   desc "Free, open-source SQL client"
   homepage "https://plot.ly/free-sql-client-download/"
