c55b9d58b54fc7a847a67609066aada4d16296f9
Markus Reiter <me@reitermark.us>
2021-03-21 04:58:07 +0100

    dropletmanager: add livecheck

--- Casks/dropletmanager.rb
+++ Casks/dropletmanager.rb
@@ -3,7 +3,6 @@ cask "dropletmanager" do
   sha256 "ed8011cef55f3bcdde4e7e7e331775808e4701fc41acc3191d3e4d80f8ab8335"
 
   url "https://github.com/deivuh/DODropletManager-OSX/releases/download/v#{version}/DropletManager.v#{version}.zip"
-  appcast "https://github.com/deivuh/DODropletManager-OSX/releases.atom"
   name "DigitalOcean Droplets Manager"
   desc "Digital Ocean droplet manager"
   homepage "https://github.com/deivuh/DODropletManager-OSX"
