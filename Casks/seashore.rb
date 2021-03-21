15fe535df62ea2125fd37b928ce0f6a62c503ea7
Markus Reiter <me@reitermark.us>
2021-03-21 21:52:02 +0100

    seashore: add livecheck (#102184)

--- Casks/seashore.rb
+++ Casks/seashore.rb
@@ -3,7 +3,6 @@ cask "seashore" do
   sha256 "79ff54b5018781765cbfae7fa12b5f920b8eff6292e9aee0330d7c59b7e1a9bc"
 
   url "https://github.com/robaho/seashore/releases/download/v#{version}/seashore-bin-#{version}.dmg"
-  appcast "https://github.com/robaho/seashore/releases.atom"
   name "Seashore"
   desc "Image editing application similar to GIMP"
   homepage "https://github.com/robaho/seashore"
