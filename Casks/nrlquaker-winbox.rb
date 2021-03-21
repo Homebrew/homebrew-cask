f60acea692fd6552a116bde52b7b714f95e9d656
Markus Reiter <me@reitermark.us>
2021-03-21 20:18:19 +0100

    nrlquaker-winbox: add livecheck (#102044)

--- Casks/nrlquaker-winbox.rb
+++ Casks/nrlquaker-winbox.rb
@@ -3,7 +3,6 @@ cask "nrlquaker-winbox" do
   sha256 "62f16c77853e6d8d8bf7f0147b3cc691352ee9505a4d48aab3780d645b771d3f"
 
   url "https://github.com/nrlquaker/winbox-mac/releases/download/v#{version}/Winbox-mac-#{version}.zip"
-  appcast "https://github.com/nrlquaker/winbox-mac/releases.atom"
   name "Winbox-mac"
   desc "MikroTik Winbox"
   homepage "https://github.com/nrlquaker/winbox-mac/"
