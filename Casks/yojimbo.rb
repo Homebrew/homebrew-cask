6621a69b51cd0fb6b367151b9a43a7ee89091f6f
adithyasunil26 <adithyasunil26@gmail.com>
2021-03-21 16:49:26 +0400

    Update yojimbo from 4.6.1 to 4.6.2

--- Casks/yojimbo.rb
+++ Casks/yojimbo.rb
@@ -1,6 +1,6 @@
 cask "yojimbo" do
-  version "4.6.1"
-  sha256 "c2e2727e12f332b6c0f1885b4cccfe531f2c9043be0d0f17c47f5fe8b9794e12"
+  version "4.6.2"
+  sha256 "93b246aa1e637f1d3c3483772eccc2cfc0f2a76dd30a8a91ca11e21f58baf330"
 
   url "https://s3.amazonaws.com/BBSW-download/Yojimbo_#{version}.dmg",
       verified: "s3.amazonaws.com/BBSW-download/"
