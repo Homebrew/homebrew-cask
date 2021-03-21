1de0db3d01d303e5166eb04aca19b93d7f5f1acc
Markus Reiter <me@reitermark.us>
2021-03-21 17:42:41 +0100

    kawa: add livecheck (#101877)

--- Casks/kawa.rb
+++ Casks/kawa.rb
@@ -3,7 +3,6 @@ cask "kawa" do
   sha256 "19a30df528471f688d3d4f43d82b6a5f5e435ba8c361e8f5ed971c0471705f72"
 
   url "https://github.com/utatti/kawa/releases/download/v#{version}/Kawa.zip"
-  appcast "https://github.com/utatti/kawa/releases.atom"
   name "Kawa"
   desc "Alternative input source switcher"
   homepage "https://github.com/utatti/kawa"
