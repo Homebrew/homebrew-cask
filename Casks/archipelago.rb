e9f05dc03779a5617176c071b972f0b9b9a2b52f
Markus Reiter <me@reitermark.us>
2021-03-21 05:29:19 +0100

    archipelago: add livecheck (#101578)

--- Casks/archipelago.rb
+++ Casks/archipelago.rb
@@ -3,7 +3,6 @@ cask "archipelago" do
   sha256 "8732617eb50ad782088f9a2a6e48b29035a5dd3ba92e75081888e670bb16a2f1"
 
   url "https://github.com/npezza93/archipelago/releases/download/v#{version}/Archipelago-#{version}.dmg"
-  appcast "https://github.com/npezza93/archipelago/releases.atom"
   name "Archipelago"
   desc "Open-source terminal emulator built on web technology"
   homepage "https://github.com/npezza93/archipelago"
