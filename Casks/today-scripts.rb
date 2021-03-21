7d436da5305c8b5b7bb312f5e1f250e9b6008805
Markus Reiter <me@reitermark.us>
2021-03-21 22:27:08 +0100

    today-scripts: add livecheck (#102271)

--- Casks/today-scripts.rb
+++ Casks/today-scripts.rb
@@ -3,7 +3,6 @@ cask "today-scripts" do
   sha256 "e9e1136e787347f257e9554efdd546c4fd92a6b2a6ae28d23f0065b3d7b96bc4"
 
   url "https://github.com/SamRothCA/Today-Scripts/releases/download/v#{version}/Today-Scripts.tar.gz"
-  appcast "https://github.com/SamRothCA/Today-Scripts/releases.atom"
   name "Today Scripts"
   homepage "https://github.com/SamRothCA/Today-Scripts"
 
