90f5b5d3be34b7a4f40a7d8bd2be715ee4354669
Markus Reiter <me@reitermark.us>
2021-03-21 05:17:04 +0100

    cinder: add livecheck (#101672)

--- Casks/cinder.rb
+++ Casks/cinder.rb
@@ -3,9 +3,13 @@ cask "cinder" do
   sha256 "b7576cc0259380d2730258f54021dfc2482f00413c005593e566eadf145e2381"
 
   url "https://libcinder.org/static/releases/cinder_#{version}_mac.zip"
-  appcast "https://github.com/cinder/cinder/releases.atom"
   name "Cinder"
   homepage "https://libcinder.org/"
+
+  livecheck do
+    url "https://github.com/cinder/cinder"
+    strategy :git
+  end
 
   suite "cinder_#{version}_mac"
 end
