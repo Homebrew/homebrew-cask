a003bfe9f327c44423184b31da0e1835ff9fc906
Markus Reiter <me@reitermark.us>
2021-03-21 05:19:54 +0100

    endless-sky: add livecheck (#101760)

--- Casks/endless-sky.rb
+++ Casks/endless-sky.rb
@@ -4,7 +4,6 @@ cask "endless-sky" do
 
   url "https://github.com/endless-sky/endless-sky/releases/download/v#{version}/endless-sky-macos-#{version}.dmg",
       verified: "github.com/endless-sky/endless-sky/"
-  appcast "https://github.com/endless-sky/endless-sky/releases.atom"
   name "Endless Sky"
   desc "Space exploration, trading, and combat game"
   homepage "https://endless-sky.github.io/"
