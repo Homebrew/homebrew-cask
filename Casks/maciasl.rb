34006ae2fd258cf2675dfe0c90d257c88dccb7ae
Markus Reiter <me@reitermark.us>
2021-03-21 18:13:58 +0100

    maciasl: add livecheck

--- Casks/maciasl.rb
+++ Casks/maciasl.rb
@@ -3,7 +3,6 @@ cask "maciasl" do
   sha256 "706bdb1ca7dde9a17c4d0b695a36bbc6191f9fd7b58b2772b82cf16147407f94"
 
   url "https://github.com/acidanthera/MaciASL/releases/download/#{version}/MaciASL-#{version}-RELEASE.zip"
-  appcast "https://github.com/acidanthera/MaciASL/releases.atom"
   name "MaciASL"
   homepage "https://github.com/acidanthera/MaciASL"
 
