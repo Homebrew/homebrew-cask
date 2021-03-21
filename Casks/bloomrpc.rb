8558339e43d2907a80036b009056631c143c8bd1
Markus Reiter <me@reitermark.us>
2021-03-21 05:27:30 +0100

    bloomrpc: add livecheck (#101630)

--- Casks/bloomrpc.rb
+++ Casks/bloomrpc.rb
@@ -3,7 +3,6 @@ cask "bloomrpc" do
   sha256 "af138f611156919169a1452a02d595ef5ae7a0e431dc90a537e9f47001f4c643"
 
   url "https://github.com/uw-labs/bloomrpc/releases/download/#{version}/BloomRPC-#{version}.dmg"
-  appcast "https://github.com/uw-labs/bloomrpc/releases.atom"
   name "BloomRPC"
   desc "GUI Client for GRPC Services"
   homepage "https://github.com/uw-labs/bloomrpc"
