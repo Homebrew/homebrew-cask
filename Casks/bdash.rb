a2be5c101f8cca99ad886787437651ded99e2106
Markus Reiter <me@reitermark.us>
2021-03-21 05:36:57 +0100

    bdash: add livecheck (#101609)

--- Casks/bdash.rb
+++ Casks/bdash.rb
@@ -3,7 +3,6 @@ cask "bdash" do
   sha256 "7d93babc7bdf2a1cad6e6083d2b52a55141fc5e6c24c41c96585357e84da5f04"
 
   url "https://github.com/bdash-app/bdash/releases/download/v#{version}/Bdash-#{version}-mac.zip"
-  appcast "https://github.com/bdash-app/bdash/releases.atom"
   name "Bdash"
   desc "Simple SQL Client for lightweight data analysis"
   homepage "https://github.com/bdash-app/bdash"
