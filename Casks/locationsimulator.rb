b8ccd206ac9461ec79b797404e9615d44dd0164a
Markus Reiter <me@reitermark.us>
2021-03-21 18:20:43 +0100

    locationsimulator: add livecheck (#101938)

--- Casks/locationsimulator.rb
+++ Casks/locationsimulator.rb
@@ -3,7 +3,6 @@ cask "locationsimulator" do
   sha256 "f932fd797bcea1562816550b4b6965e82d5d85f119f389c36a449a71cff76f77"
 
   url "https://github.com/Schlaubischlump/LocationSimulator/releases/download/v#{version}/LocationSimulator_without_dependencies.app.zip"
-  appcast "https://github.com/SchlaubiSchlump/LocationSimulator/releases.atom"
   name "LocationSimulator"
   desc "Application to spoof your iOS / iPadOS device location"
   homepage "https://github.com/Schlaubischlump/LocationSimulator"
