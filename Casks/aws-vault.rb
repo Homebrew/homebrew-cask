a2d9a5fcde5967c2efd5d12e1d15721ef2cc646e
Michael Tibben <michael.tibben@99designs.com>
2021-03-21 11:06:27 +1100

    Add arm64 support to aws-vault (#101413)

--- Casks/aws-vault.rb
+++ Casks/aws-vault.rb
@@ -1,10 +1,19 @@
 cask "aws-vault" do
   version "6.3.0"
-  sha256 "7af98c76d3afcffd60829716d06986224239d2583108fd7669cb660e215ff98e"
 
-  url "https://github.com/99designs/aws-vault/releases/download/v#{version}/aws-vault-darwin-amd64.dmg"
+  if Hardware::CPU.intel?
+    sha256 "7af98c76d3afcffd60829716d06986224239d2583108fd7669cb660e215ff98e"
+
+    url "https://github.com/99designs/aws-vault/releases/download/v#{version}/aws-vault-darwin-amd64.dmg"
+  else
+    sha256 "1e99a2109ad21a30ec0af5004cd3f65c337ac9775a29a3fe1d27c5dd8d0ef703"
+
+    url "https://github.com/99designs/aws-vault/releases/download/v#{version}/aws-vault-darwin-arm64.dmg"
+  end
+
   appcast "https://github.com/99designs/aws-vault/releases.atom"
   name "aws-vault"
+  desc "Securely stores and accesses AWS credentials in a development environment"
   homepage "https://github.com/99designs/aws-vault"
 
   binary "aws-vault"
