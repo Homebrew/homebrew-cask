9928926d3a9548cc0df7688f87fc26d2ed6cc0c9
Markus Reiter <me@reitermark.us>
2021-03-21 20:18:40 +0100

    pdfshaver: add livecheck (#102086)

--- Casks/pdfshaver.rb
+++ Casks/pdfshaver.rb
@@ -3,7 +3,6 @@ cask "pdfshaver" do
   sha256 "ebfacb5e30b0939c4549f36fe88be10c0d4bae1ffaa4616e70daf849d19df9d0"
 
   url "https://github.com/tparry/PDFShaver.app/releases/download/v#{version}/PDFShaver.zip"
-  appcast "https://github.com/tparry/PDFShaver.app/releases.atom"
   name "PDFShaver"
   desc "Shrink PDF files to make them smaller"
   homepage "https://github.com/tparry/PDFShaver.app"
