525d97c82743d44c0a25ca0af334423248354938
Markus Reiter <me@reitermark.us>
2021-03-21 20:18:57 +0100

    patchwork: add livecheck (#102083)

--- Casks/patchwork.rb
+++ Casks/patchwork.rb
@@ -3,7 +3,6 @@ cask "patchwork" do
   sha256 "9f98b5a0574ae854f2ef970db70e9f542013340f38dab4ab9b2bda7abb6d9278"
 
   url "https://github.com/ssbc/patchwork/releases/download/v#{version}/Patchwork-#{version}.dmg"
-  appcast "https://github.com/ssbc/patchwork/releases.atom"
   name "Patchwork"
   homepage "https://github.com/ssbc/patchwork"
 
