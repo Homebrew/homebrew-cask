971247092da134f78fc61b91c6d6daa311ee1778
Markus Reiter <me@reitermark.us>
2021-03-22 00:51:33 +0100

    wombat: add livecheck (#102339)

--- Casks/wombat.rb
+++ Casks/wombat.rb
@@ -3,7 +3,6 @@ cask "wombat" do
   sha256 "00813e11c214af6924ba3839fb3578c0e7623c068fd326dfecedba1fb38feba5"
 
   url "https://github.com/rogchap/wombat/releases/download/v#{version}/Wombat_v#{version}_Darwin_x86_64.dmg"
-  appcast "https://github.com/rogchap/wombat/releases.atom"
   name "Wombat"
   desc "Cross platform gRPC client"
   homepage "https://github.com/rogchap/wombat"
