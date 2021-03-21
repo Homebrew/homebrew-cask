a8ef7edbbc21c497d90141a9916daa1e36f8d00c
Markus Reiter <me@reitermark.us>
2021-03-21 20:42:23 +0100

    pusher: add livecheck (#102123)

--- Casks/pusher.rb
+++ Casks/pusher.rb
@@ -3,7 +3,6 @@ cask "pusher" do
   sha256 "b329a5106b6670bf50da4b91ba34d82102edb70074828cf4d0cd879b1a5e2180"
 
   url "https://github.com/noodlewerk/NWPusher/releases/download/#{version}/pusher.app.zip"
-  appcast "https://github.com/noodlewerk/NWPusher/releases.atom"
   name "NWPusher"
   homepage "https://github.com/noodlewerk/NWPusher"
 
