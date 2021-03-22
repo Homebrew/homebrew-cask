4822b7b39bead88a8a1a8519ecb9993e4d8524d0
Markus Reiter <me@reitermark.us>
2021-03-22 01:12:48 +0100

    yggdrasil: add livecheck (#102347)

--- Casks/yggdrasil.rb
+++ Casks/yggdrasil.rb
@@ -3,7 +3,6 @@ cask "yggdrasil" do
   sha256 "4d1bc756db8fb237671f5ba3f121133a597d3e7057fafbffabf5fdfd93e2b669"
 
   url "https://github.com/yggdrasil-network/yggdrasil-go/releases/download/v#{version}/yggdrasil-#{version}-macos-amd64.pkg"
-  appcast "https://github.com/yggdrasil-network/yggdrasil-go/releases.atom"
   name "Yggdrasil"
   desc "End-to-end encrypted IPv6 networking to connect worlds"
   homepage "https://github.com/yggdrasil-network/yggdrasil-go"
