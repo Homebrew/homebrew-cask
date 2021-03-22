bd50216f2e12bca785eff031d900d391e26971d3
Reno <25192197+singularitti@users.noreply.github.com>
2021-03-21 20:52:58 -0400

    Update aural from 2.7.0 to 2.8.0 (#102366)

--- Casks/aural.rb
+++ Casks/aural.rb
@@ -1,6 +1,6 @@
 cask "aural" do
-  version "2.7.0"
-  sha256 "1ecd832d6cc75c96d207bc1a137f42d7c1e1b3da59a6e9849d26665384006077"
+  version "2.8.0"
+  sha256 "15e0e697d02cf49c13d843752a9bbdfac7f4433c5a60047818cbd14c0a6d08bb"
 
   url "https://github.com/maculateConception/aural-player/releases/download/#{version}/AuralPlayer-#{version}.dmg"
   name "Aural Player"
