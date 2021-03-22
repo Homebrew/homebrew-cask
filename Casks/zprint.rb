f08f19facd57941876197395dd819f270618028b
Markus Reiter <me@reitermark.us>
2021-03-22 01:13:18 +0100

    zprint: add livecheck (#102359)

--- Casks/zprint.rb
+++ Casks/zprint.rb
@@ -3,7 +3,6 @@ cask "zprint" do
   sha256 "9ae2b454f680f81993803637c9891b54bbb0b56f46f951bc662024313fb9356e"
 
   url "https://github.com/kkinnear/zprint/releases/download/#{version}/zprintm-#{version}"
-  appcast "https://github.com/kkinnear/zprint/releases.atom"
   name "zprint"
   desc "Library to reformat Clojure and Clojurescript source code and s-expressions"
   homepage "https://github.com/kkinnear/zprint"
