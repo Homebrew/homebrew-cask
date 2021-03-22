e1506a4656c2f03ce34af5797c69718b149085a9
singularitti <singularitti@outlook.com>
2021-03-21 20:59:23 -0400

    Update chalk from 1.6.5 to 1.6.6

--- Casks/chalk.rb
+++ Casks/chalk.rb
@@ -1,8 +1,8 @@
 cask "chalk" do
-  version "1.6.5"
-  sha256 "af04ec05c17793c10629e840c0720a6cb1c052a11d6ee202af712045d0755af9"
+  version "1.6.6"
+  sha256 "3e664ba4a0e581b69f4438bcce070a4104b835dd5d34669d05d70b1313988439"
 
-  url "https://www.chachatelier.fr/chalk/downloads/Chalk-#{version.dots_to_underscores}.dmg",
+  url "https://pierre.chachatelier.fr/chalk/downloads/Chalk-#{version.dots_to_underscores}.dmg",
       user_agent: :fake
   appcast "https://pierre.chachatelier.fr/chalk/downloads/chalk-sparkle-en.rss"
   name "Chalk"
