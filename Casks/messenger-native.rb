6264bcbdf2e3989dfd60c05c3232d772a25c57fe
Markus Reiter <me@reitermark.us>
2021-03-22 12:00:13 +0100

    messenger-native: add livecheck

--- Casks/messenger-native.rb
+++ Casks/messenger-native.rb
@@ -3,7 +3,6 @@ cask "messenger-native" do
   sha256 "72d730197b91963a5804adf09cbb2a4df3d9374f0ec922035e34587d0e2c073a"
 
   url "https://github.com/gastonmorixe/MessengerNative/releases/download/#{version}/Mac64_MessengerNative#{version}.zip"
-  appcast "https://github.com/gastonmorixe/MessengerNative/releases.atom"
   name "Messenger Native"
   desc "Facebook's Messenger Native"
   homepage "https://github.com/gastonmorixe/MessengerNative"
