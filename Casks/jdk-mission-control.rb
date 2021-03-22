b6b7411a35b1dece5520eb556eb60fb9cb273d15
Tobias Stadler <ts.stadler@gmx.de>
2021-03-22 08:11:22 +0100

    jdk-mission-control: Fix urls

--- Casks/jdk-mission-control.rb
+++ Casks/jdk-mission-control.rb
@@ -3,10 +3,10 @@ cask "jdk-mission-control" do
   sha256 "3eb380e7ae241851cebeaf636b0de063d7a4a9585265c25ce1b1bfba8d56b100"
 
   url "https://download.java.net/java/GA/jmc#{version.major}/#{version.after_comma}/binaries/jmc-#{version.before_comma}_osx-x64.tar.gz"
-  appcast "https://jdk.java.net/jmc/",
+  appcast "https://jdk.java.net/jmc/8",
           must_contain: version.after_comma
   name "JDK Mission Control"
-  homepage "https://jdk.java.net/jmc/"
+  homepage "https://jdk.java.net/jmc/8"
 
   app "jmc-#{version.before_comma}_osx-x64/JDK Mission Control.app"
 
