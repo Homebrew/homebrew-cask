84b19753134bdad06d71780a48136e3e6d578d73
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2021-03-22 07:21:45 -0700

    Update parallels-virtualization-sdk from 16.1.2-49151 to 16.1.3-49160. (#102423)

--- Casks/parallels-virtualization-sdk.rb
+++ Casks/parallels-virtualization-sdk.rb
@@ -1,6 +1,6 @@
 cask "parallels-virtualization-sdk" do
-  version "16.1.2-49151"
-  sha256 "d48011bfa1c8086c5aa4cfff186a15f569500af0b68bc1bd5405ea5eea110288"
+  version "16.1.3-49160"
+  sha256 "bbf885c30c59919733dd197a8382ed5f6ed92945badbda23f1f7ac20926c9846"
 
   url "https://download.parallels.com/desktop/v#{version.major}/#{version}/ParallelsVirtualizationSDK-#{version}-mac.dmg"
   name "Parallels Virtualization SDK"
