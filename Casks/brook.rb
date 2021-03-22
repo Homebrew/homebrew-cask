0bd4b0e785246806c3f6f1d098fcd888ac18f269
Mcdull <30206217+McDull-GitHub@users.noreply.github.com>
2021-03-22 22:57:22 +0800

    Update brook (#102425)

--- Casks/brook.rb
+++ Casks/brook.rb
@@ -1,6 +1,6 @@
 cask "brook" do
   version "20210214"
-  sha256 "420c9158c0a2977eb8ba5432cac98e4ccbb22f3d37e49d70a05cbbe3c4907290"
+  sha256 "09e86be9d4a1711514a270ec56e8f1919b6b238841dd010441b17f5f11281d6f"
 
   url "https://github.com/txthinking/brook/releases/download/v#{version}/Brook.dmg"
   name "Brook"
