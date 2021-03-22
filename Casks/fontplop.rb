1ac02aa782547e31f994887db3c632f6fc59bfe9
Markus Reiter <me@reitermark.us>
2021-03-22 03:34:25 +0100

    fontplop: add livecheck (#102381)

--- Casks/fontplop.rb
+++ Casks/fontplop.rb
@@ -2,12 +2,10 @@ cask "fontplop" do
   version "1.3.0"
   sha256 "0ada8c9040bb3da489965b917990f6d88da23c8fec5602f798c91db7e2aafb0e"
 
-  url "https://github.com/matthewgonzalez/fontplop/releases/download/v#{version}/fontplop-#{version}.dmg",
-      verified: "github.com/matthewgonzalez/fontplop/"
-  appcast "https://github.com/matthewgonzalez/fontplop/releases.atom"
+  url "https://github.com/matthewgonzalez/fontplop/releases/download/v#{version}/fontplop-#{version}.dmg"
   name "Fontplop"
   desc "Open Source Webfont Converter"
-  homepage "https://www.fontplop.com/"
+  homepage "https://github.com/matthewgonzalez/fontplop"
 
   app "fontplop.app"
 end
