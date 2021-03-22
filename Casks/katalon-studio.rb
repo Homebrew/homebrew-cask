a7ec2bcf1703738bcb8527d5503dacc3f2ad2bec
Markus Reiter <me@reitermark.us>
2021-03-22 05:20:36 +0100

    katalon-studio: add livecheck (#102397)

--- Casks/katalon-studio.rb
+++ Casks/katalon-studio.rb
@@ -3,9 +3,14 @@ cask "katalon-studio" do
   sha256 "2c484168cf97fc6b7a3e0698e0c09f55348fbc4aaa773b57b03ccd6f69858a09"
 
   url "https://download.katalon.com/#{version}/Katalon%20Studio.dmg"
-  appcast "https://github.com/katalon-studio/katalon-studio/releases.atom"
   name "Katalon Studio"
   homepage "https://www.katalon.com/download/"
+
+  livecheck do
+    url "https://github.com/katalon-studio/katalon-studio"
+    strategy :git
+    regex(/^v?(\d+(?:\.\d+)*)$/i)
+  end
 
   app "Katalon Studio.app"
 end
