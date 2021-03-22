c78140621065b94b2d0e44558a20c83046575192
Markus Reiter <me@reitermark.us>
2021-03-22 03:34:34 +0100

    hoststool: add livecheck (#102390)

--- Casks/hoststool.rb
+++ Casks/hoststool.rb
@@ -3,7 +3,6 @@ cask "hoststool" do
   sha256 "52a8c777210dff249e5d37c839ec711910e3c87f68dfe127cb0b9b4719736782"
 
   url "https://github.com/ZzzM/HostsToolforMac/releases/download/#{version}/HostsToolForMac.zip"
-  appcast "https://github.com/ZzzM/HostsToolforMac/releases.atom"
   name "Hosts tool for Mac"
   desc "Local hosts update tool"
   homepage "https://github.com/ZzzM/HostsToolforMac/"
