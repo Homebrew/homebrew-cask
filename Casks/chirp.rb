f677170d923c02beb5a1c32bf95a83db1a171cc2
Reno <25192197+singularitti@users.noreply.github.com>
2021-03-21 21:07:50 -0400

    Update chirp from 20210212 to 20210310 (#102372)

--- Casks/chirp.rb
+++ Casks/chirp.rb
@@ -1,6 +1,6 @@
 cask "chirp" do
-  version "20210212"
-  sha256 "e86f848bb24d91c9a466baafadabcd1156b207d9ec80a30775c4aabd2d989630"
+  version "20210310"
+  sha256 "73ab5a38c6acbc6cc6cf7b670f6ce362d286d26e07d57591df59da7d673dab6d"
 
   url "https://trac.chirp.danplanet.com/chirp_daily/LATEST/chirp-unified-daily-#{version}.app.zip"
   appcast "https://trac.chirp.danplanet.com/chirp_daily/LATEST/SHA1SUM"
