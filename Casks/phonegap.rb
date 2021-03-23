c1350482784d59afd423d9923d607c05ce161486
Markus Reiter <me@reitermark.us>
2021-03-23 02:33:50 +0100

    phonegap: add livecheck (#102451)

--- Casks/phonegap.rb
+++ Casks/phonegap.rb
@@ -2,11 +2,13 @@ cask "phonegap" do
   version "0.4.5"
   sha256 "1158660f45637593d7721f2d50f2792ac8b1744950a37ca8072f29c9922efdf6"
 
-  url "https://github.com/phonegap/phonegap-app-desktop/releases/download/#{version}/PhoneGapDesktop.dmg",
-      verified: "github.com/phonegap/phonegap-app-desktop/"
-  appcast "https://github.com/phonegap/phonegap-app-desktop/releases.atom"
+  url "https://github.com/phonegap/phonegap-app-desktop/releases/download/#{version}/PhoneGapDesktop.dmg"
   name "PhoneGap"
-  homepage "https://phonegap.com/"
+  homepage "https://github.com/phonegap/phonegap-app-desktop"
 
   app "PhoneGap.app"
+
+  caveats do
+    discontinued
+  end
 end
