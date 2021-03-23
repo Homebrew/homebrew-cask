5d77d58038e8b24f2971fb933fd8ad1566eef49e
Markus Reiter <me@reitermark.us>
2021-03-23 16:59:22 +0100

    Update manuskript.rb

--- Casks/manuskript.rb
+++ Casks/manuskript.rb
@@ -1,5 +1,5 @@
 cask "manuskript" do
-  version "0.11.0"
+  version "0.10.0"
   sha256 "f48f9c96af19b42c61a1ab6119e581e9398c33a8795ba5051cc0d44add1d2d7f"
 
   url "https://github.com/olivierkes/manuskript/releases/download/#{version.major_minor_patch}/manuskript-#{version}-osx.zip",
