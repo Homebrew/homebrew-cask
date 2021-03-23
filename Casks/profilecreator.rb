44e59101484fcc2eb5ff64d61922af22fed88e00
Markus Reiter <me@reitermark.us>
2021-03-23 02:49:55 +0100

    profilecreator: add livecheck (#102453)

--- Casks/profilecreator.rb
+++ Casks/profilecreator.rb
@@ -3,9 +3,16 @@ cask "profilecreator" do
   sha256 "a4a1b45bfaa6bc83aac7ef532981aaa0c807cd17fbfb1f157980144e5d309aea"
 
   url "https://github.com/erikberglund/ProfileCreator/releases/download/v#{version.before_comma}/ProfileCreator_v#{version.before_comma}-#{version.after_comma}.dmg"
-  appcast "https://github.com/erikberglund/ProfileCreator/releases.atom"
   name "ProfileCreator"
   homepage "https://github.com/erikberglund/ProfileCreator"
+
+  livecheck do
+    url :url
+    strategy :github_latest do |page|
+      match = page.match(%r{href=.*?/ProfileCreator_v(\d+(?:\.\d+)*)-(.*?)\.dmg}i)
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   depends_on macos: ">= :sierra"
 
