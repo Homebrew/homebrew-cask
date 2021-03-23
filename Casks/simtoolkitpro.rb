0d02e3cea18ca44a2fdf48303c4f119b86ee599e
Miccal Matthews <miccal.matthews@gmail.com>
2021-03-23 11:59:46 +0800

    Update simtoolkitpro.rb

--- Casks/simtoolkitpro.rb
+++ Casks/simtoolkitpro.rb
@@ -3,10 +3,16 @@ cask "simtoolkitpro" do
   sha256 "f2a6a57d27052498b25bb16fad46d24a25c5ea7d5435e26c27736c76acf7f780"
 
   url "https://simtoolkitpro-cdn.net/automated-builds/SimToolkitPro-#{version}.Setup.dmg",
-      verified: "simtoolkitpro-cdn.net"
+      verified: "simtoolkitpro-cdn.net/"
   name "SimToolkitPro"
   desc "EFB software for your simulator"
   homepage "https://simtoolkitpro.co.uk/"
+
+  livecheck do
+    url "https://simtoolkitpro.co.uk/download/mac"
+    strategy :header_match
+    regex(/SimToolkitPro-(\d+(?:\.\d+)*).Setup\.dmg/i)
+  end
 
   app "SimToolkitPro.app"
 
