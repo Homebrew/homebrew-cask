a84ee84397d3c371da2b9ad0745042a0723c280b
Julien Lehuraux <julien@drov.io>
2021-03-23 16:37:08 +0100

    Rename and upgrade USE Engine.app v2.2.6.3 to Drovio.app v3.0.1

---
+++ Casks/drovio.rb
@@ -0,0 +1,16 @@
+cask "drovio" do
+  version "3.0.1"
+  sha256 "65c6f84f3d7169d79a6bd38d8bdaca393c8e815e59a89acc3fb810668e369610"
+
+  url "https://repository.drovio.com/stable/drovio/macos/#{version.major}.x/#{version}/drovio.dmg"
+  name "Drovio"
+  desc "Remote pair programming and team collaboration tool"
+  homepage "https://www.drovio.com/"
+
+  auto_updates true
+  depends_on macos: ">= :sierra"
+
+  app "Drovio.app"
+
+  zap trash: "~/Library/Application Support/Drovio"
+end
