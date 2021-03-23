63e883ef5c54b630387240164af69252535fae5e
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2021-03-23 12:56:25 +0900

    Update igv from 2.9.0 to 2.9.4 (#102472)

--- Casks/igv.rb
+++ Casks/igv.rb
@@ -1,6 +1,6 @@
 cask "igv" do
-  version "2.9.0"
-  sha256 "b7b53d45f27063df4a6be0f1e2060187bba18c9b548c3b81f72ab08e2e2dc950"
+  version "2.9.4"
+  sha256 "5bf4bf52337c32c9350d45aab0fcf3426ac5b8ecd1036b8499a7373c7f7a682f"
 
   url "https://data.broadinstitute.org/igv/projects/downloads/#{version.major_minor}/IGV_MacApp_#{version}.zip"
   appcast "https://data.broadinstitute.org/igv/projects/downloads/",
