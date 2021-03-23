a5db7fa3c742b48dd21ae0a87c94aa4f71bc50e0
Markus Reiter <me@reitermark.us>
2021-03-23 01:49:18 +0100

    multipatch: add livecheck (#102439)

--- Casks/multipatch.rb
+++ Casks/multipatch.rb
@@ -3,9 +3,13 @@ cask "multipatch" do
   sha256 "406e26a2518d447a26b6a2b852d369e97c04cf9b25eea7a4d5a5f63a880cf94d"
 
   url "https://projects.sappharad.com/tools/multipatch#{version.no_dots}.zip"
-  appcast "https://github.com/Sappharad/MultiPatch/releases.atom"
   name "MultiPatch"
   homepage "https://projects.sappharad.com/tools/multipatch.html"
+
+  livecheck do
+    url "https://github.com/Sappharad/MultiPatch"
+    strategy :git
+  end
 
   app "MultiPatch/MultiPatch.app"
 
