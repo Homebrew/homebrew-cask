feee449690bf39a42095f55c5a060eceaa81c813
Matan Kushner <hello@matchai.dev>
2021-03-23 11:55:50 -0400

    Update to v2.0.33-beta

--- Casks/xbar.rb
+++ Casks/xbar.rb
@@ -1,6 +1,6 @@
 cask "xbar" do
-  version "2.0.32-beta"
-  sha256 "3fbce5a0acd46ad227f269245a5e4d8acf44c84a322848da1e4cfe252f3decf3"
+  version "2.0.33-beta"
+  sha256 "b39d133badf0fb27c2fa6d3936f70bb9543cc1bc1e56dea7a817e0bda5b6e589"
 
   url "https://github.com/matryer/xbar/releases/download/v#{version}/xbar.v#{version}.dmg",
       verified: "github.com/matryer/xbar/"
