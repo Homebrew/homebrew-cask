6bc5b0a3716715b6257880e76ad0293b516ebe8f
Markus Reiter <me@reitermark.us>
2021-03-23 01:32:13 +0100

    mmex: add livecheck (#102420)

--- Casks/mmex.rb
+++ Casks/mmex.rb
@@ -4,10 +4,15 @@ cask "mmex" do
 
   url "https://downloads.sourceforge.net/moneymanagerex/v#{version}/mmex%20v#{version}-macOS-fix.dmg",
       verified: "downloads.sourceforge.net/moneymanagerex/"
-  appcast "https://github.com/moneymanagerex/moneymanagerex/releases.atom"
   name "Money Manager Ex"
   desc "Money management application"
   homepage "https://www.moneymanagerex.org/"
+
+  livecheck do
+    url "https://github.com/moneymanagerex/moneymanagerex"
+    strategy :git
+    regex(/^v?(\d+(?:\.\d+)*)$/)
+  end
 
   app "MMEX.app"
 
