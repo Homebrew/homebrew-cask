b7b8b1159dc0400f6afc35feabf7a6f59304b3bd
Yishen Miao <mys721tx@gmail.com>
2021-03-22 17:32:02 -0700

    Bumping renpy to 7.4.4 (#102432)

--- Casks/renpy.rb
+++ Casks/renpy.rb
@@ -1,9 +1,10 @@
 cask "renpy" do
-  version "7.3.5"
-  sha256 "3d6a46450be9bde378b9df43dc734550a511356081969f464af7a190f60761f9"
+  version "7.4.4"
+  sha256 "b06491573cb59d2fd0c511c550ddb82b22a78a0ef34680370f280d47ff9683a6"
 
   url "https://www.renpy.org/dl/#{version}/renpy-#{version}-sdk.zip"
   name "Ren'Py"
+  desc "Visual novel engine in Python"
   homepage "https://www.renpy.org/"
 
   suite "renpy-#{version}-sdk"
