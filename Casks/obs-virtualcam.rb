87707467e28d66089868fc7070218758b39fd399
Markus Reiter <me@reitermark.us>
2021-03-23 01:54:50 +0100

    Update obs-virtualcam.rb

--- Casks/obs-virtualcam.rb
+++ Casks/obs-virtualcam.rb
@@ -6,6 +6,15 @@ cask "obs-virtualcam" do
   name "OBS Virtual Camera"
   homepage "https://github.com/johnboiles/obs-mac-virtualcam"
 
+  livecheck do
+    url "https://github.com/johnboiles/obs-mac-virtualcam/releases"
+    strategy :page_match do |page|
+      p page
+      match = page.match(%r{href=.*?/obs-mac-virtualcam-(\d+(?:\.\d+)*)-v?([a-f0-9]+)\.pkg}i)
+      "#{match[1]},#{match[2]}"
+    end
+  end
+
   depends_on macos: ">= :high_sierra"
 
   pkg "obs-mac-virtualcam-#{version.after_comma}-v#{version.before_comma}.pkg"
