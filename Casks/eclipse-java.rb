4a92bb9444384060d5f8e785b1ec458dfbaae1b2
Markus Reiter <me@reitermark.us>
2021-03-24 17:03:23 +0100

    Update eclipse-java.rb

--- Casks/eclipse-java.rb
+++ Casks/eclipse-java.rb
@@ -1,20 +1,14 @@
 cask "eclipse-java" do
-  version "4.19.0,2021-03:R"
+  version "2021-03"
   sha256 "834dc1173718adaddc8129369711ade0ffa1b03af8423786f8ded18b0ddcc856"
 
-  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-java-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
+  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version}/R/eclipse-java-#{version}-R-macosx-cocoa-x86_64.dmg"
   name "Eclipse IDE for Java Developers"
   homepage "https://eclipse.org/"
 
   livecheck do
-    url "https://projects.eclipse.org/releases/"
-    strategy :page_match do |page|
-      page.scan(%r{href=.*projects.eclipse.org/releases/(\d+-\d+)}i).map do |release|
-        version_page = Net::HTTP.get(URI.parse("https://projects.eclipse.org/releases/#{release[0]}"))
-        version = version_page.scan(%r{href="/projects/eclipse/releases/(\d+(?:\.\d+)*)"}i)
-        "#{version[0][0]},#{release[0]}:R"
-      end
-    end
+    url "https://www.eclipse.org/downloads/packages/"
+    regex(%r{href=.*?/eclipse-java-(\d{4}-\d{2})-R-macosx-cocoa-x86_64\.dmg}i)
   end
 
   # Renamed to avoid conflict with other Eclipse.
