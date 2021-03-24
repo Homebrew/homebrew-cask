7d35e77ac3797ca1482ac8fe7e75acbc3f64899a
Mayur Ahir <ahirmayur@users.noreply.github.com>
2021-03-24 12:01:36 +0000

    Update quip from 7.28.2 to 7.31.1 (#102517)

--- Casks/quip.rb
+++ Casks/quip.rb
@@ -1,6 +1,6 @@
 cask "quip" do
-  version "7.28.2"
-  sha256 "af532f62e54ad5bd7245eeca905b9a0902b19929f74c7c18c2a39fb7e68b2e2b"
+  version "7.31.1"
+  sha256 "b765accf4ef50204de8196084709fb0a4f52846f753537d3198e64dcb7de25bb"
 
   url "https://quip-clients.com/macosx_#{version}.dmg",
       verified: "quip-clients.com/"
