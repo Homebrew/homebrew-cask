5bd8b98ebc5ce9dd8056e7560ebbf46dd0185055
Markus Reiter <me@reitermark.us>
2021-03-24 20:21:55 +0100

    macfusion-ng: add livecheck (#102411)

--- Casks/macfusion-ng.rb
+++ Casks/macfusion-ng.rb
@@ -3,9 +3,13 @@ cask "macfusion-ng" do
   sha256 "3e6e356f36623dde805aa3de941e29f6256b02cfe0720bcbd70df4526e2a7198"
 
   url "https://github.com/macfusion-ng/macfusion#{version.major}/releases/download/#{version}/Macfusion.zip"
-  appcast "https://github.com/macfusion-ng/macfusion#{version.major}/releases.atom"
   name "Macfusion"
   homepage "https://github.com/macfusion-ng/macfusion#{version.major}/"
+
+  livecheck do
+    url :url
+    strategy :git
+  end
 
   conflicts_with cask: "macfusion"
   depends_on formula: "sshfs"
