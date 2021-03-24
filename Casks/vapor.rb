c8d1c9febe9da8d8a53eaead0ba2df26d1aa977f
Markus Reiter <me@reitermark.us>
2021-03-24 20:05:57 +0100

    vapor: add livecheck (#102547)

--- Casks/vapor.rb
+++ Casks/vapor.rb
@@ -3,9 +3,13 @@ cask "vapor" do
   sha256 "13c2937f1ce9d25cb0aef3ea7fc4430386cbff73771502f2725a81591f08c792"
 
   url "https://github.com/NCAR/VAPOR/releases/download/#{version}/VAPOR3-#{version}-Darwin.dmg"
-  appcast "https://github.com/NCAR/VAPOR/releases.atom"
   name "VAPOR"
   homepage "https://github.com/NCAR/VAPOR"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "vapor.app"
 end
