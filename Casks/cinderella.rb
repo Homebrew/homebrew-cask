50fa60bc0566f5a2c4461c66c9e4e33a95b579d3
kortenkamp <kortenkamp@cinderella.de>
2021-03-24 07:31:27 +0100

    Update cinderella from 3.0b.1999 to 3.0b.2004 (#102487)
    * Update cinderella from 3.0b.1999 to 3.0b.2004
    
    * added a livecheck for the version number
    
    * fixed the style
    
    * fixed the version again

--- Casks/cinderella.rb
+++ Casks/cinderella.rb
@@ -1,11 +1,16 @@
 cask "cinderella" do
-  version "3.0b.1999"
-  sha256 "e33990ccedbee56806ccd189dea01167f443d18c7baf00be61f08921dcad5215"
+  version "3.0b.2004"
+  sha256 "a06f47c23d907085c3102886748a8f6d178d92b774e75bb04e3bb2611d5806bd"
 
   url "https://beta.cinderella.de/Cinderella-#{version}.dmg"
   name "Cinderella"
   desc "Interactive Geometry Software"
   homepage "https://cinderella.de/"
+
+  livecheck do
+    url "https://beta.cinderella.de/"
+    regex(/href="Cinderella-(\d+.\d+b?.+)\.dmg"/i)
+  end
 
   app "Cinderella.app"
 
