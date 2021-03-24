0facb2228146c44ca9151fd38ea2e5c76f5879be
Sam <sam@samcleathero.co.uk>
2021-03-24 15:59:15 +0000

    Update microsoft-remote-desktop from 10.5.1 to 10.5.2 (#102488)

--- Casks/microsoft-remote-desktop.rb
+++ Casks/microsoft-remote-desktop.rb
@@ -1,12 +1,18 @@
 cask "microsoft-remote-desktop" do
-  version "10.5.1"
-  sha256 "2b99e8980b8c57a4ab60bc2cde9b671afcdaec42e3148dc8b0df04e68b8c806f"
+  version "10.5.2"
+  sha256 "b648e704f3701116b9abbae7d5960a87fd45d85e9e129561c3cbfd692e48bb96"
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Remote_Desktop_#{version}_installer.pkg",
       verified: "officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/"
   name "Microsoft Remote Desktop"
   desc "Remote desktop client"
   homepage "https://docs.microsoft.com/en-us/windows-server/remote/remote-desktop-services/clients/remote-desktop-mac"
+
+  livecheck do
+    url "https://docs.microsoft.com/en-us/windows-server/remote/remote-desktop-services/clients/mac-whatsnew"
+    strategy :page_match
+    regex(/Updates\s*for\s*version\s*(\d+(?:\.\d+)*)/i)
+  end
 
   auto_updates true
   depends_on macos: ">= :high_sierra"
