1f3977f19ec48f1d468e3e0b1e6f0f5fd5bb806e
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-03-24 19:59:26 +0900

    Update seadrive from 2.0.12 to 2.0.13 (#102516)

--- Casks/seadrive.rb
+++ Casks/seadrive.rb
@@ -1,6 +1,6 @@
 cask "seadrive" do
-  version "2.0.12"
-  sha256 "3b6591e2b6fc3b8a45fc384e57965c2db1c65b1f1be014d49d6801ee23de293f"
+  version "2.0.13"
+  sha256 "1228efb62f6ee501511b0186ce12bef15b35250e5c295e9180a9ec4c12d4d7e1"
 
   url "https://download.seadrive.org/seadrive-#{version}.dmg",
       verified: "download.seadrive.org/"
