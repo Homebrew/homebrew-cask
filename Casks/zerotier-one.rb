f5dcb86352120c976655121c2d46e2387ffaff10
Markus Reiter <me@reitermark.us>
2021-03-24 20:14:07 +0100

    zerotier-one: add livecheck (#102555)

--- Casks/zerotier-one.rb
+++ Casks/zerotier-one.rb
@@ -3,10 +3,14 @@ cask "zerotier-one" do
   sha256 "3271dfa409dba0cf05578f60c91e5a8981a5aa685692cf4444a2fe756aae7830"
 
   url "https://download.zerotier.com/RELEASES/#{version}/dist/ZeroTier%20One.pkg"
-  appcast "https://github.com/zerotier/ZeroTierOne/releases.atom"
   name "ZeroTier One"
   desc "Mesh VPN client"
   homepage "https://www.zerotier.com/download.shtml"
+
+  livecheck do
+    url "https://github.com/zerotier/ZeroTierOne"
+    strategy :github_latest
+  end
 
   pkg "ZeroTier One.pkg"
 
