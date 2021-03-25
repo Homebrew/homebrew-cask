55ee30b7be522492fddd31293522913136be4add
Reno <25192197+singularitti@users.noreply.github.com>
2021-03-25 04:46:46 -0400

    Update mactracker from 7.10.3 to 7.10.4 (#102588)

--- Casks/mactracker.rb
+++ Casks/mactracker.rb
@@ -1,6 +1,6 @@
 cask "mactracker" do
-  version "7.10.3"
-  sha256 "e3b2d654126dc3b2a08e1f104bb1fdfe9f92043e0c758b6010847b2d145f6d10"
+  version "7.10.4"
+  sha256 "56df4dcab72453e27f164136e02ac7d96bba78ae46e8e8b407a86507e7c5aa7f"
 
   url "https://www.mactracker.ca/downloads/Mactracker_#{version}.zip"
   name "Mactracker"
