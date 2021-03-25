7b8380cfd493921d26964cb28f86569067bb17a3
Adon <ashendon@users.noreply.github.com>
2021-03-25 08:57:55 -0400

    Update electron from 12.0.1 to 12.0.2 (#102595)

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,12 +1,12 @@
 cask "electron" do
-  version "12.0.1"
+  version "12.0.2"
 
   if Hardware::CPU.intel?
-    sha256 "af2adac0b5b32c95ad2b834d5521bd301983954e986048b46cdf5c739423de17"
+    sha256 "766ca8f8adc4535db3069665ea8983979ea79dd5ec376e1c298f858b420ec58f"
     url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip",
         verified: "github.com/electron/electron/"
   else
-    sha256 "90c363499d21317934babe07cc7a016c6de8d5c4e0cb1e3d0e893088f5814ed0"
+    sha256 "d4f0f73e0a5a723ef7f3f1e6ca3743b6267eef385cf79aa63a2fb3f698a7931d"
     url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-arm64.zip",
         verified: "github.com/electron/electron/"
   end
