f5185f9dfd792ea41f37c4340151dfa8226a5bf6
Markus Reiter <me@reitermark.us>
2021-03-26 00:13:01 +0100

    Don't skip livecheck unless absolutely necessary. (#102593)

--- Casks/wechat.rb
+++ Casks/wechat.rb
@@ -1,5 +1,5 @@
 cask "wechat" do
-  version "3.0.0,17801"
+  version "3.0.0.17,17801"
   sha256 :no_check
 
   url "https://dldir1.qq.com/weixin/mac/WeChatMac.dmg"
@@ -8,11 +8,11 @@ cask "wechat" do
   desc "Free messaging and calling application"
   homepage "https://mac.weixin.qq.com/"
 
-  # There is an appcast at https://dldir1.qq.com/weixin/mac/mac-release.xml,
-  # but it's slower to update than the submissions we get. See:
+  # This appcast is slower to update than the submissions we get. See:
   #   https://github.com/Homebrew/homebrew-cask/pull/90907#issuecomment-710107547
   livecheck do
-    skip
+    url "https://dldir1.qq.com/weixin/mac/mac-release.xml"
+    strategy :sparkle
   end
 
   auto_updates true
