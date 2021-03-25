59f28c56aa09ba813ae569538430f6730beec964
Shiki <53338814+shikiroot@users.noreply.github.com>
2021-03-25 09:25:08 +0800

    update popo from 3.19.0 to 3.20.1 (#102569)

--- Casks/popo.rb
+++ Casks/popo.rb
@@ -1,6 +1,6 @@
 cask "popo" do
-  version "3.19.0"
-  sha256 "ae6392cc43183c0c14f92de89ac40655c17678abf63b9621eb26617a9698db80"
+  version "3.20.1"
+  sha256 "ca11e5db6881fa5b789bc66f3fe876b725fdb9ba9737ed135f7c38cbf67757e3"
 
   url "https://popo.netease.com/file/popomac/POPO_Mac_V#{version.dots_to_underscores}.dmg"
   appcast "http://http.popo.netease.com:8080/api/open/jsonp/check_version?device=4",
