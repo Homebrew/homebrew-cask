d1ea7f2bef381cce7888ff114807dba50fb03c79
Bevan Kay <bevanjkay@gmail.com>
2021-03-25 12:04:45 +1100

    Update canva from 1.17.0 to 1.18.0 (#102568)

--- Casks/canva.rb
+++ Casks/canva.rb
@@ -1,6 +1,6 @@
 cask "canva" do
-  version "1.17.0"
-  sha256 "5e9027de37b9178e4ba63f73eb2a8f0b24723934d5c043a3634ab728b9415663"
+  version "1.18.0"
+  sha256 "e357fb7a9cab423e577a6be335f8a93556aee372c0936d87b2863b6662cfe588"
 
   url "https://desktop-release.canva-deploy.com/Canva-#{version}.dmg",
       verified: "desktop-release.canva-deploy.com/"
