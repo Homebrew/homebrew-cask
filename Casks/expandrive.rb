476fc9d906e4ba5e218f225bb37f84e61d6d6d04
John Benninghoff <jbenninghoff@mac.com>
2021-03-25 10:05:01 -0500

    Update expandrive from 7.7.6 to 7.7.7 (#102603)

--- Casks/expandrive.rb
+++ Casks/expandrive.rb
@@ -1,6 +1,6 @@
 cask "expandrive" do
-  version "7.7.6"
-  sha256 "b3954c363c9de617ab6c0650ffbc795f4d010c465b73705a570e261391d79285"
+  version "7.7.7"
+  sha256 "7e6cb7ff512f2f10818eb62cfef21da31a19a97d582e15a0bc12b1b9b70b7c1d"
 
   url "https://updates.expandrive.com/apps/expandrive#{version.major}/v/#{version.dots_to_hyphens}/update_download"
   appcast "https://updates.expandrive.com/appcast/expandrive#{version.major}.json?version=#{version.major}.0.0"
