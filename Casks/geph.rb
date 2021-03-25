c90071208bc921edb575ce836d874520e29aa57c
Mcdull <30206217+McDull-GitHub@users.noreply.github.com>
2021-03-25 12:59:18 +0800

    Update geph from 4.2.5 to 4.2.6 (#102573)
    * Update geph from 4.2.5 to 4.2.6
    
    * add livecheck
    
    * fix livecheck
    
    * Update geph.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/geph.rb
+++ Casks/geph.rb
@@ -1,12 +1,16 @@
 cask "geph" do
-  version "4.2.5"
-  sha256 "e6c4866eb31d55c944473d940f63aa971dcfddf86eda5e3fb72c6f44b0065261"
+  version "4.2.6"
+  sha256 "e258c05053b72d65f711fc165452c9b7096c3fab36b98e0fe9ed77b1aba36f13"
 
   url "https://f001.backblazeb2.com/file/geph4-dl/Geph4Releases/#{version}/geph-macos-#{version}.dmg",
       verified: "f001.backblazeb2.com/file/geph4-dl/"
   name "Geph"
   desc "Modular Internet censorship circumvention system"
   homepage "https://geph.io/"
+
+  livecheck do
+    skip
+  end
 
   app "Geph.app"
 end
