f9ecd71dbedb070610cb249878ccd3a70fc8b069
Thomas Amerongen <thomas.amerongen@gmail.com>
2021-03-26 10:30:41 +1100

    Update fork from 2.5 to 2.5.6 (#102597)

--- Casks/fork.rb
+++ Casks/fork.rb
@@ -1,6 +1,6 @@
 cask "fork" do
-  version "2.5"
-  sha256 "c3572f18eb9f23f70f4ada23b1d1cbd0185bf084cec15a9c28bf8ae4fb556b7c"
+  version "2.5.6"
+  sha256 "76e089a29d5f2309b92c4470ce17a10941ca4c4df93fa5a05de5f00950d1da5e"
 
   url "https://forkapp.ams3.cdn.digitaloceanspaces.com/mac/Fork-#{version}.dmg",
       verified: "forkapp.ams3.cdn.digitaloceanspaces.com/mac/"
@@ -10,7 +10,9 @@ cask "fork" do
 
   livecheck do
     url "https://git-fork.com/update/feed.xml"
-    strategy :sparkle
+    strategy :sparkle do |item|
+      item.url[%r{/Fork-(\d+(?:\.\d+)*)\.dmg}i, 1]
+    end
   end
 
   auto_updates true
