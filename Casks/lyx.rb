623b3ee0d55e1ef7cb61a6007e1a726e11d74368
Markus Reiter <me@reitermark.us>
2021-03-26 21:12:06 +0100

    lyx: add livecheck

--- Casks/lyx.rb
+++ Casks/lyx.rb
@@ -1,12 +1,16 @@
 cask "lyx" do
-  version "2.3.6"
-  sha256 "759d5e82af12ca7fa0749d7e4a928e4c2419dab1a54a7b77ea71a48afb127a22"
+  version "2.3.6.1"
+  sha256 "fd881be1cfd25349e09e5e8bb234d7ea66f9eab5da2d9dba80a2fdb23281937b"
 
-  url "https://ftp.lip6.fr/pub/lyx/bin/#{version.major_minor_patch}/LyX-#{version}+qt5-x86_64-cocoa.dmg",
+  url "https://ftp.lip6.fr/pub/lyx/bin/#{version.major_minor_patch}/LyX-#{version}+qt5-12-x86_64-cocoa.dmg",
       verified: "ftp.lip6.fr/pub/lyx/"
-  appcast "https://www.lyx.org/misc/rss/lyx_news_feed.xml"
   name "LyX"
   homepage "https://www.lyx.org/"
+
+  livecheck do
+    url "https://www.lyx.org/misc/rss/lyx_news_feed.xml"
+    regex(/LyX (\d+(?:\.\d+)*)/i)
+  end
 
   app "LyX.app"
 
