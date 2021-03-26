711227807fbc2c5b99f011b22614cfc7c6406127
Loïc PÉRON <loic.peron@gmail.com>
2021-03-26 10:55:01 +0100

    Update eclipse-dsl from 4.18.0,2020-12:R to 4.19.0,2021-03:R

--- Casks/eclipse-dsl.rb
+++ Casks/eclipse-dsl.rb
@@ -1,6 +1,6 @@
 cask "eclipse-dsl" do
-  version "4.18.0,2020-12:R"
-  sha256 "1a17c68f62d690576a4e32be7febc30dd4efb827b4a8a558a5ca46206263370b"
+  version "4.19.0,2021-03:R"
+  sha256 "88c544a0d54e3d0792721afc717a61e72e1484156676e2d0905144ebc03d2ffc"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-dsl-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name "Eclipse IDE for Java and DSL Developers"
