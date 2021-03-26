ef3ccd7cf9e48a7a5a065a674ab705e1944a0e29
Markus Reiter <me@reitermark.us>
2021-03-26 05:00:06 +0100

    droplr: add livecheck (#102616)
    * droplr: add livecheck
    
    * Update droplr.rb
    
    * Update droplr.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/droplr.rb
+++ Casks/droplr.rb
@@ -1,15 +1,24 @@
 cask "droplr" do
-  version "5.7.2,426"
-  sha256 "69ff334f9ec2925dc1b1e30dc562042435b736bbf03f3de9fcf511dcc302c176"
+  version "5.9.0,458"
+  sha256 "d7cad6194b2157e5674440f228e1e0c597a585aee85a3206cd8739763a935eb0"
 
-  url "https://files.droplr.com/apps/mac/Droplr-#{version.before_comma.no_dots}.dmg"
-  appcast "https://droplr.com/appcast/appcast-sandbox.xml"
+  url "https://files.droplr.com/apps/mac/Droplr#{version.before_comma.no_dots}-#{version.after_comma}.zip"
   name "Droplr"
   desc "Screenshot and screen recorder"
   homepage "https://droplr.com/"
 
+  livecheck do
+    url "https://files.droplr.com/apps/mac-current"
+    strategy :header_match do |headers|
+      match = headers["location"].match(%r{/Droplr(\d+)(\d+)(\d+)-(\d+)\.zip}i)
+      "#{match[1]}.#{match[2]}.#{match[3]},#{match[4]}"
+    end
+  end
+
   auto_updates true
   depends_on macos: ">= :sierra"
 
-  app "Droplr.app"
+  pkg "Droplr#{version.before_comma.no_dots}-#{version.after_comma}.pkg"
+
+  uninstall pkgutil: "com.droplr.droplr-mac"
 end
