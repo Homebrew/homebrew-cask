f3671740ba8c798db566e9d0c49313d3bd3b8c6c
Markus Reiter <me@reitermark.us>
2021-03-26 04:48:06 +0100

    feed-the-beast: add livecheck (#102619)

--- Casks/feed-the-beast.rb
+++ Casks/feed-the-beast.rb
@@ -1,12 +1,16 @@
 cask "feed-the-beast" do
-  version "202012101153-ce008c54b4-release"
-  sha256 "b5d26e4384bbe06f5b9783529d1682b4091bac199c9186598af5f2bf4e7d7115"
+  version "202101041428-54016a89dc"
+  sha256 "e2ad72443d003c8b46a4c973bbb711de71cdcb8ac51dc90289e6d4e438196c65"
 
-  url "https://apps.modpacks.ch/FTBApp/release/#{version}/FTBA_macos_#{version.dots_to_underscores}.dmg",
+  url "https://apps.modpacks.ch/FTBApp/release/#{version}-release/FTBA_macos_#{version}-release.dmg",
       verified: "apps.modpacks.ch/FTBApp/"
-  appcast "https://www.feed-the-beast.com/app_release.xml"
   name "Feed the Beast"
   homepage "https://www.feed-the-beast.com/"
+
+  livecheck do
+    url "https://www.feed-the-beast.com/app_release.xml"
+    regex(/FTBA_macos_(\d+-[a-f0-9]+)-release\.dmg/i)
+  end
 
   app "FTBApp.app"
 
