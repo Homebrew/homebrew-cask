e511474a5d17c9c2ccbba22d1ee45cdeeaa5bc6c
adithyasunil26 <adithyasunil26@gmail.com>
2021-03-26 17:15:29 +0400

    Update electerm from 1.12.6 to 1.12.7

--- Casks/electerm.rb
+++ Casks/electerm.rb
@@ -1,6 +1,6 @@
 cask "electerm" do
-  version "1.12.6"
-  sha256 "35ec0e0fc3fedea42932de316b08bb4a247075257e1ee4e403c2f26af06efff1"
+  version "1.12.7"
+  sha256 "267688bf3c1fa30064435f54e529133a80ca8245155d28a32c4918a9b3e794fc"
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac.dmg"
   name "electerm"
