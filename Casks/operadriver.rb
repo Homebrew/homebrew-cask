c8894502ab4d696c938dbfee7e30051b4b0e4e9e
Adithya Sunil <adithyasunil26@gmail.com>
2021-03-26 18:13:12 +0400

    Update operadriver from 88.0.4324.104 to 89.0.4389.82 (#102707)

--- Casks/operadriver.rb
+++ Casks/operadriver.rb
@@ -1,6 +1,6 @@
 cask "operadriver" do
-  version "88.0.4324.104"
-  sha256 "570b707484fb7ed674959da858b22d83ccea83cd03f487a9c023508201bd3db0"
+  version "89.0.4389.82"
+  sha256 "b034dd237e7708842ae7ffb1f315cc0ddc0d88fdb4fce22684695a3257dc3db5"
 
   url "https://github.com/operasoftware/operachromiumdriver/releases/download/v.#{version}/operadriver_mac64.zip"
   name "operachromiumdriver"
