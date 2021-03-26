7dc956e151e29b798082ac7416b60162f06f4147
Steve Abatangle <sabat@users.noreply.github.com>
2021-03-26 10:20:12 -0700

    chef-workstation version bump (#102716)

--- Casks/chef-workstation.rb
+++ Casks/chef-workstation.rb
@@ -1,6 +1,6 @@
 cask "chef-workstation" do
-  version "21.2.303"
-  sha256 "60ec2d6a7fcb294af0ee471a1a236a52d05c3e15f7720009f488975ef49b8f1d"
+  version "21.3.346"
+  sha256 "85a2b3d6af9dff20d6cee0fc64a4d7995c44c6e607794bf8a0468579bb09df11"
 
   url "https://packages.chef.io/files/stable/chef-workstation/#{version}/mac_os_x/10.15/chef-workstation-#{version}-1.x86_64.dmg"
   appcast "https://omnitruck.chef.io/stable/chef-workstation/metadata?p=mac_os_x&pv=10.15&m=x86_64&v=latest"
