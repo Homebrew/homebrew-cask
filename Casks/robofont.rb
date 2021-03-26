05da0e7a434fda428020045791b748010cb3a4cd
Markus Reiter <me@reitermark.us>
2021-03-26 23:50:03 +0100

    robofont: add livecheck

--- Casks/robofont.rb
+++ Casks/robofont.rb
@@ -1,12 +1,16 @@
 cask "robofont" do
-  version "3.4"
+  version "3.4,2004281315"
   sha256 :no_check
 
   url "https://static.typemytype.com/robofont/RoboFont.dmg",
       verified: "static.typemytype.com/robofont/"
-  appcast "https://doc.robofont.com/appcast.xml"
   name "RoboFont"
   homepage "https://robofont.com/"
+
+  livecheck do
+    url "https://doc.robofont.com/appcast.xml"
+    strategy :sparkle
+  end
 
   app "RoboFont.app"
 end
