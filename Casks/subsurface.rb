9fbaa858b6c4327ac51e2ffc737895338b2118a4
adithyasunil26 <adithyasunil26@gmail.com>
2021-03-26 17:07:45 +0400

    Update subsurface from 4.9.10 to 5.0.0

--- Casks/subsurface.rb
+++ Casks/subsurface.rb
@@ -1,11 +1,11 @@
 cask "subsurface" do
-  version "4.9.10"
+  version "5.0.0"
 
   if MacOS.version <= :mojave
     sha256 "34274051fe008486eccd01b31fcfdc793724276b505438184b35393ffd1199fe"
     url "https://subsurface-divelog.org/downloads/Subsurface-#{version}-10.13+14.dmg"
   else
-    sha256 "32d8ded0a937689183a8d915acab44330cbb26618ff12abc62be5ef42eefd9c4"
+    sha256 "0bcfffb8d3c113b34180220cce10fe6422a85ecede70d2781d06e3d61d52cd4b"
     url "https://subsurface-divelog.org/downloads/Subsurface-#{version}-10.15+11.0.dmg"
   end
 
