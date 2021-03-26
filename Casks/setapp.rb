347652aff3424878f311373e6125a34057331411
Markus Reiter <me@reitermark.us>
2021-03-27 00:41:18 +0100

    setapp: add livecheck

--- Casks/setapp.rb
+++ Casks/setapp.rb
@@ -1,13 +1,19 @@
 cask "setapp" do
-  version "2.7.15,1608219081"
-  sha256 "b5ab580ef30d48b0f981f1fdf57a91028598f8b9b0c7b2e31ae3e8692f848080"
+  version "2.9.2,1615393422"
+  sha256 "64a82cede80ec00e19141c7bb64d19998303ad02927931328d25ff3e411dbf5d"
 
   url "https://dl.devmate.com/com.setapp.InstallSetapp/#{version.before_comma}/#{version.after_comma}/InstallSetapp-#{version.before_comma}.zip",
       verified: "devmate.com/com.setapp.InstallSetapp/"
-  appcast "https://updates.devmate.com/com.setapp.InstallSetapp.xml"
   name "Setapp"
   desc "Collection of apps available by subscription"
   homepage "https://setapp.com/"
+
+  livecheck do
+    url "https://updates.devmate.com/com.setapp.InstallSetapp.xml"
+    strategy :sparkle do |item|
+      "#{item.short_version},#{item.url[%r{/(\d+)/InstallSetapp-(?:\d+(?:\.\d+)*)\.zip}i, 1]}"
+    end
+  end
 
   auto_updates true
   depends_on macos: ">= :sierra"
