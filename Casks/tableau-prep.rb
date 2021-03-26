0aae0149cf1400e6e9fa05639b21908203392c53
Anton Möller <anton@esportal.com>
2021-03-26 11:59:45 +0100

    Update tableau-prep from 2021.1.1 to 2021.1.2

--- Casks/tableau-prep.rb
+++ Casks/tableau-prep.rb
@@ -1,6 +1,6 @@
 cask "tableau-prep" do
-  version "2021.1.1"
-  sha256 "92fc32659cb06bc8e84a893d08ea325c0012cece090946a4df8f2996370c224d"
+  version "2021.1.2"
+  sha256 "6dfe2c62730085784351258b246592bc68a1f52268c350d3fd8ebf5b0b24b687"
 
   url "https://downloads.tableau.com/esdalt/tableau_prep/#{version}/TableauPrep-#{version.dots_to_hyphens}.dmg"
   name "Tableau Prep"
