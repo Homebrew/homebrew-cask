f9c07cd2f27445e15c38d1d2a062c2826e901b9c
Loïc PÉRON <loic.peron@gmail.com>
2021-03-26 10:48:32 +0100

    Update eclipse-php from 4.18.0,2020-12:R to 4.19.0,2021-03:R

--- Casks/eclipse-php.rb
+++ Casks/eclipse-php.rb
@@ -1,6 +1,6 @@
 cask "eclipse-php" do
-  version "4.18.0,2020-12:R"
-  sha256 "3f38772a0022bf9309e06ab4b9f950dc9cc62bbeda7917674ff72da490245ef4"
+  version "4.19.0,2021-03:R"
+  sha256 "ab155da2498634d7841b47e56a97e2025fbc21351fbbcbddfcfa9444a1208e38"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-php-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name "Eclipse IDE for PHP Developers"
