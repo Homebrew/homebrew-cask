d4c525b49a5e7ef716196f2e638c11f76a04b7f3
Adithya Sunil <adithyasunil26@gmail.com>
2021-03-26 17:24:45 +0400

    Update stremio from 4.4.132 to 4.4.135 (#102665)

--- Casks/stremio.rb
+++ Casks/stremio.rb
@@ -1,6 +1,6 @@
 cask "stremio" do
-  version "4.4.132"
-  sha256 "1373ab9a3d873e225a02826cc593468b2c7bc370c2286ac3babdd2367be6a913"
+  version "4.4.135"
+  sha256 "e1b2f3b66e0f3111121c98a99b181f3540306b4f410bedba7b25bd12e46b07a5"
 
   url "https://dl.strem.io/shell-osx/v#{version}/Stremio+#{version}.dmg"
   name "Stremio"
