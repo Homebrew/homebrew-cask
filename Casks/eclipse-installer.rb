60a8bec13bcbbd7c3704643d184f77e7527956e3
Loïc PÉRON <loic.peron@gmail.com>
2021-03-26 10:42:22 +0100

    Update eclipse-installer from 4.19.0,2021-03:R to 4.19.0,2021-03:R

--- Casks/eclipse-installer.rb
+++ Casks/eclipse-installer.rb
@@ -1,6 +1,6 @@
 cask "eclipse-installer" do
-  version "4.18.0,2020-12:R"
-  sha256 "43f09d0b45484676ebe4eca88c52ea001f63efd2576ae55f71e0fe91c2fd3cb4"
+  version "4.19.0,2021-03:R"
+  sha256 "7c7aec2b57c4b498fc1827391d9cf740b91d05089da2f3e28dc298b86b2d6369"
 
   url "https://eclipse.org/downloads/download.php?file=/oomph/epp/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-inst-mac64.tar.gz&r=1"
   name "Eclipse Installer"
