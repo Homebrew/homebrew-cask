be79ec9842966d8a6bfeda0cb31af4170a8f8cad
Sergey Parhomenko <sergey@parhomenko.net>
2021-03-26 17:10:56 +0100

    Add livecheck to google-cloud-sdk to fix CI

--- Casks/google-cloud-sdk.rb
+++ Casks/google-cloud-sdk.rb
@@ -2,6 +2,11 @@ cask "google-cloud-sdk" do
   version "333.0.0"
   sha256 "ed55af0312925a0685fd7d14f459dfb973f826b90ab81eb10ee947413a284c87"
 
+  livecheck do
+    url "https://dl.google.com/dl/cloudsdk/channels/rapid/components-2.json"
+    regex(/  \"version\": \"(\d+\.\d+\.\d+)\"\n\}$/i)
+  end
+
   url "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-#{version}-darwin-x86_64.tar.gz"
   name "Google Cloud SDK"
   desc "Set of tools to manage resources and applications hosted on Google Cloud"
