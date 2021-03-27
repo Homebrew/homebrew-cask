87dc3be090ecab26999dae487fb5e6b28d4feb5b
Markus Reiter <me@reitermark.us>
2021-03-27 05:25:24 +0100

    macx-video-converter-pro: add livecheck (#102731)

--- Casks/macx-video-converter-pro.rb
+++ Casks/macx-video-converter-pro.rb
@@ -1,12 +1,16 @@
 cask "macx-video-converter-pro" do
-  version "6.5.2,20201215"
+  version "6.5.2"
   sha256 :no_check
 
   url "https://www.macxdvd.com/download/macx-video-converter-pro.dmg"
-  appcast "http://www.macxdvd.com/mac-video-converter-pro/upgrade/video-converter-pro.xml"
   name "MacX Video Converter Pro"
   desc "Tool to convert, edit, download & resize videos"
   homepage "https://www.macxdvd.com/mac-video-converter-pro/"
+
+  livecheck do
+    url "http://www.macxdvd.com/mac-video-converter-pro/upgrade/video-converter-pro.xml"
+    regex(%r{LastestVersion</key>\s*<string>(\d+(?:\.\d+)*)<}i)
+  end
 
   app "MacX Video Converter Pro.app"
 end
