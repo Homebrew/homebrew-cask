e10addf1dbf04fbc969db690db20ad6b8e620a5e
Markus Reiter <me@reitermark.us>
2021-03-27 05:25:09 +0100

    mediainfo: add livecheck (#102732)

--- Casks/mediainfo.rb
+++ Casks/mediainfo.rb
@@ -3,9 +3,13 @@ cask "mediainfo" do
   sha256 "568313eca4e016ae61102e0ab15ce3c1aa7b1decdd2d683effc794bb2f21ec77"
 
   url "https://mediaarea.net/download/binary/mediainfo-gui/#{version}/MediaInfo_GUI_#{version}_Mac.dmg"
-  appcast "https://mediaarea.net/rss/mediainfo_updates.xml"
   name "MediaInfo"
   homepage "https://mediaarea.net/en/MediaInfo"
+
+  livecheck do
+    url "https://mediaarea.net/rss/mediainfo_updates.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :high_sierra"
 
