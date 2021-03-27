1b96619ea20c64f5e494380863026106009fa215
Josh Harriman <13487310+josh-harriman@users.noreply.github.com>
2021-03-27 06:34:43 -0400

    Upgrade remember-the-milk to 1.3.4 (#102759)

--- Casks/remember-the-milk.rb
+++ Casks/remember-the-milk.rb
@@ -1,12 +1,12 @@
 cask "remember-the-milk" do
-  version "1.3.3"
+  version "1.3.4"
 
   if Hardware::CPU.intel?
-    sha256 "6210e37543afc8a899afb1a5484f57675b21a223f33f4d3bb2e497e3a535742c"
+    sha256 "749d38b91562bb4180e8692e68ce6776eed627de2086d1029bd160e48c0aed98"
 
     url "https://www.rememberthemilk.com/download/mac/RememberTheMilk-#{version}-x64.zip"
   else
-    sha256 "3c63a4d3214bbfbe7ce28240252be2e20be6920f337256b5c194e3592a17df1d"
+    sha256 "0ed777bf49003b196291fca5f1bec0e1c45111f0b7ac36ce081b60cbc512f34e"
 
     url "https://www.rememberthemilk.com/download/mac/RememberTheMilk-#{version}-arm64.zip"
   end
