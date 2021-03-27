c3a42ffe9489aeb01d38028ef08475500c40cd8f
Markus Reiter <me@reitermark.us>
2021-03-27 05:24:53 +0100

    pagico: add livecheck (#102734)

--- Casks/pagico.rb
+++ Casks/pagico.rb
@@ -1,12 +1,15 @@
 cask "pagico" do
-  version "9.7.20201207"
-  sha256 "a2a86af4a81e433854302d12e40dc8df4475a9b0f5fe881742043bbf2d57b140"
+  version "9.9,20210326"
+  sha256 "4d6183cd8152e664019b12065bd8fef96362eb3554cc25a88faac1c5fec8e6e7"
 
-  url "https://www.pagico.com/downloads/Pagico_macOS_r#{version.patch}.dmg"
-  appcast "https://www.pagico.com/api/pagico#{version.major}.mac.xml",
-          must_contain: version.patch
+  url "https://www.pagico.com/downloads/Pagico_macOS_r#{version.after_comma}.dmg"
   name "Pagico"
   homepage "https://www.pagico.com/"
+
+  livecheck do
+    url "https://www.pagico.com/api/pagico#{version.major}.mac.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :sierra"
 
