9f57d2cd2912978157f965e17ab94da98411e028
Markus Reiter <me@reitermark.us>
2021-03-27 09:21:41 +0100

    jalview: add livecheck (#102726)
    * jalview: add livecheck
    
    * Update jalview.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/jalview.rb
+++ Casks/jalview.rb
@@ -3,10 +3,14 @@ cask "jalview" do
   sha256 "b49a13803461d35bad9df4ce587a910d7e0bc9123fcdd8b1060b8ed39dd92afa"
 
   url "https://www.jalview.org/getdown/release/install4j/1.8/Jalview-#{version.dots_to_underscores}-macos-java_8.dmg"
-  appcast "https://www.jalview.org/getdown/release/install4j/1.8/updates.xml"
   name "Jalview"
   desc "Multiple sequence alignment editor, visualisor, analysis and figure generator"
   homepage "https://www.jalview.org/"
+
+  livecheck do
+    url "https://www.jalview.org/getdown/release/install4j/1.8/updates.xml"
+    regex(/newVersion="(\d+(?:\.\d+)*)"/i)
+  end
 
   app "Jalview.app"
 end
