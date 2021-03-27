e050a24f22cc14326fecac56940862b2bbcc017f
Markus Reiter <me@reitermark.us>
2021-03-27 05:26:09 +0100

    keep-it: add livecheck (#102727)

--- Casks/keep-it.rb
+++ Casks/keep-it.rb
@@ -1,12 +1,16 @@
 cask "keep-it" do
-  version "1.9.7"
-  sha256 "a2bbcad500e4804c27d78bcfa95384d59db2afdc3695e1a6d0564800797713a9"
+  version "1.10.2,8200"
+  sha256 "7a7bd4fe5b521c5637810686039a395428803a86997c4b6dbf53944699c17ab6"
 
-  url "https://reinventedsoftware.com/keepit/downloads/KeepIt_#{version}.dmg"
-  appcast "https://reinventedsoftware.com/keepit/downloads/keepit.xml"
+  url "https://reinventedsoftware.com/keepit/downloads/KeepIt_#{version.before_comma}.dmg"
   name "Keep It"
   desc "Notebook, scrapbook and organizer tool"
   homepage "https://reinventedsoftware.com/keepit/"
+
+  livecheck do
+    url "https://reinventedsoftware.com/keepit/downloads/keepit.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :catalina"
