cf69bd99175a776e061da4c379edce6e63746204
Stephen Wade <stephen@stephenwade.me>
2021-03-27 17:51:54 -0400

    Update netnewswire from 5.1.3 to 6.0 (#102772)

--- Casks/netnewswire.rb
+++ Casks/netnewswire.rb
@@ -1,6 +1,6 @@
 cask "netnewswire" do
-  version "5.1.3"
-  sha256 "5f08f9ee986621d146c57f3ab3e98297944af368ce7ff1382a8e3a6c31fab5ea"
+  version "6.0"
+  sha256 "c57cecc0ca9c4e1e840c236bc74506620846c2c305c1342ace47cfe51d024dcb"
 
   url "https://github.com/brentsimmons/NetNewsWire/releases/download/mac-#{version}/NetNewsWire#{version}.zip",
       verified: "github.com/brentsimmons/NetNewsWire/"
