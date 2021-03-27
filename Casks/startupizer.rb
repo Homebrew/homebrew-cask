04ebbaf9435446c441b29dc4c28038bdca265116
Markus Reiter <me@reitermark.us>
2021-03-27 05:22:58 +0100

    startupizer: add livecheck (#102753)
    * startupizer: add livecheck
    
    * Update startupizer.rb

--- Casks/startupizer.rb
+++ Casks/startupizer.rb
@@ -1,13 +1,20 @@
 cask "startupizer" do
-  version "2.3.11,2060:1527510167"
+  version "2,2060:1527510167"
   sha256 "56792a52fc91129f6f192cbbc0dbc6a7175d0699f2dab726baaf7d94f30ce150"
 
-  url "https://dl.devmate.com/com.gentlebytes.Startupizer2/#{version.after_comma.before_colon}/#{version.after_colon}/Startupizer#{version.major}-#{version.after_comma.before_colon}.zip",
-      verified: "dl.devmate.com/com.gentlebytes.Startupizer2/"
-  appcast "https://updates.devmate.com/com.gentlebytes.Startupizer#{version.major}.xml"
+  url "https://dl.devmate.com/com.gentlebytes.Startupizer#{version.major}/#{version.after_comma.before_colon}/#{version.after_colon}/Startupizer#{version.major}-#{version.after_comma.before_colon}.zip",
+      verified: "dl.devmate.com/com.gentlebytes.Startupizer#{version.major}/"
   name "Startupizer#{version.major}"
   desc "Login items handler"
   homepage "http://gentlebytes.com/startupizer/"
+
+  livecheck do
+    url "https://updates.devmate.com/com.gentlebytes.Startupizer#{version.major}.xml"
+    strategy :sparkle do |item|
+      match = item.url.match(%r{/(\d+)/Startupizer(\d+)-\d+\.zip}i)
+      "#{match[2]},#{item.version}:#{match[1]}"
+    end
+  end
 
   app "Startupizer#{version.major}.app"
 
