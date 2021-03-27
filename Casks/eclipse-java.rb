f9c74e88dc6360d6caaf2532783433a84c7fd604
Miccal Matthews <miccal.matthews@gmail.com>
2021-03-27 16:16:06 +0800

    Update eclipse-java.rb

--- Casks/eclipse-java.rb
+++ Casks/eclipse-java.rb
@@ -1,6 +1,6 @@
 cask "eclipse-java" do
   version "2021-03"
-  sha256 "88628dc853584e80621ff5e3fd54c9b560385be4a65362006ba214506290242e"
+  sha256 "80c4c34d8d4a7fab30268e33c27a6256feb3852a9b0f9fbc6837ed626d26c6fc"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version}/R/eclipse-java-#{version}-R-macosx-cocoa-x86_64.dmg"
   name "Eclipse IDE for Java Developers"
