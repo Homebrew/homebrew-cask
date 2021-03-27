c274c975857e3ddd629da0a158a9d81e121a8cf6
Markus Reiter <me@reitermark.us>
2021-03-27 01:09:28 +0100

    Update startupizer.rb

--- Casks/startupizer.rb
+++ Casks/startupizer.rb
@@ -2,8 +2,8 @@ cask "startupizer" do
   version "2,2060:1527510167"
   sha256 "56792a52fc91129f6f192cbbc0dbc6a7175d0699f2dab726baaf7d94f30ce150"
 
-  url "https://dl.devmate.com/com.gentlebytes.Startupizer2/#{version.after_comma.before_colon}/#{version.after_colon}/Startupizer#{version.before_comma}-#{version.after_comma.before_colon}.zip",
-      verified: "dl.devmate.com/com.gentlebytes.Startupizer2/"
+  url "https://dl.devmate.com/com.gentlebytes.Startupizer#{version.major}/#{version.after_comma.before_colon}/#{version.after_colon}/Startupizer#{version.major}-#{version.after_comma.before_colon}.zip",
+      verified: "dl.devmate.com/com.gentlebytes.Startupizer#{version.major}/"
   name "Startupizer#{version.major}"
   desc "Login items handler"
   homepage "http://gentlebytes.com/startupizer/"
