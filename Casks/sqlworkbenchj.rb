273bd6f44ca16754226fb457ce71ea5110bab4e4
Markus Reiter <me@reitermark.us>
2021-03-27 09:00:22 +0100

    sqlworkbenchj: add livecheck (#102751)
    * sqlworkbenchj: add livecheck
    
    * Update sqlworkbenchj.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/sqlworkbenchj.rb
+++ Casks/sqlworkbenchj.rb
@@ -1,11 +1,15 @@
 cask "sqlworkbenchj" do
-  version "124"
+  version "127"
   sha256 "2173c7f00172bef3fed23e7f57e168a9d156c72c311af032ba469139f80d9fe9"
 
   url "https://www.sql-workbench.eu/Workbench-Build#{version}-Mac.tgz"
-  appcast "https://www.sql-workbench.eu/wb_news.xml"
   name "SQL Workbench/J"
   homepage "https://www.sql-workbench.eu/"
+
+  livecheck do
+    url "https://www.sql-workbench.eu/downloads.html"
+    regex(/Current\s+stable\s+release:\s+Build\s+(\d+)/i)
+  end
 
   app "SQLWorkbenchJ.app"
 
