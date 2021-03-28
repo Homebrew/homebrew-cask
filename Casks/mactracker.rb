429c9e2c16a82df212f558dfb7728add91f0ca73
jhaynesSITS <48290019+jhaynesSITS@users.noreply.github.com>
2021-03-28 10:36:33 -0400

    Update mactracker.rb
    Fix SHA-256 sum to correct sum provided by author at http://mactracker.ca/faq.html

--- Casks/mactracker.rb
+++ Casks/mactracker.rb
@@ -1,6 +1,6 @@
 cask "mactracker" do
   version "7.10.4"
-  sha256 "56df4dcab72453e27f164136e02ac7d96bba78ae46e8e8b407a86507e7c5aa7f"
+  sha256 "00e4d8dcbddede3df5bfc3f69ed38af3616bcc664c3bd4d8270611dddf708773"
 
   url "https://www.mactracker.ca/downloads/Mactracker_#{version}.zip"
   name "Mactracker"
