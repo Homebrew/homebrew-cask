a4f3710169bbad7a44bcb10587f6c58b52477949
Yusuf Kursad Kaya <ykursadkaya@gmail.com>
2021-03-29 17:07:59 +0300

    butt: add livecheck

--- Casks/butt.rb
+++ Casks/butt.rb
@@ -4,10 +4,14 @@ cask "butt" do
 
   url "https://downloads.sourceforge.net/butt/butt-#{version}.dmg",
       verified: "sourceforge.net/butt/"
-  appcast "https://sourceforge.net/projects/butt/rss"
   name "Broadcast Using This Tool"
   desc "Shoutcast and Icecast streaming client"
   homepage "https://danielnoethen.de/butt/"
+
+  livecheck do
+    url :url
+    strategy :sourceforge
+  end
 
   app "butt.app"
 end
