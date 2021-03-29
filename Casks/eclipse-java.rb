facb81eaea1d94a1cc8d7f984115f386423077af
Loïc PÉRON <loic.peron@gmail.com>
2021-03-29 15:12:16 +0200

    Update eclipse-java from 4.18.0,2020-12:R to 4.19.0,2021-03:R

--- Casks/eclipse-java.rb
+++ Casks/eclipse-java.rb
@@ -1,6 +1,6 @@
 cask "eclipse-java" do
-  version "4.18.0,2020-12:R"
-  sha256 "c525f64da8d9eb4e8f890a0aff2d523f44699ae4ddb853e6254d0064e78aec73"
+  version "4.19.0,2021-03:R"
+  sha256 "834dc1173718adaddc8129369711ade0ffa1b03af8423786f8ded18b0ddcc856"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-java-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name "Eclipse IDE for Java Developers"
