5495ec62321c32dc2ee743e9d615cc34aa1624c0
adithyasunil26 <adithyasunil26@gmail.com>
2021-03-31 02:59:43 +0400

    Update boxcryptor from 2.39.1119 to 2.40.1234

--- Casks/boxcryptor.rb
+++ Casks/boxcryptor.rb
@@ -3,8 +3,8 @@ cask "boxcryptor" do
     version "2.38.1090"
     sha256 "974d097017bdc1377a62fa4e9e51033636198659fc9353372233cde0e5094285"
   else
-    version "2.39.1119"
-    sha256 "b6415b07a0b9e73bf4314d19e319e3dbfdcfdfea1eda379a0c2a241bfb214a9c"
+    version "2.40.1234"
+    sha256 "b4acc528180493292a964ca1db457bad4f0d56764d1826488cbae251c0398214"
   end
 
   url "https://downloads.boxcryptor.com/boxcryptor/mac/Boxcryptor_v#{version}_Installer.dmg"
