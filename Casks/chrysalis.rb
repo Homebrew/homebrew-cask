020d1b032651a7ab74552f95213d382d1fc933ee
Adithya Sunil <adithyasunil26@gmail.com>
2021-03-31 03:13:55 +0400

    Update chrysalis from 0.8.2 to 0.8.3 (#102875)

--- Casks/chrysalis.rb
+++ Casks/chrysalis.rb
@@ -1,6 +1,6 @@
 cask "chrysalis" do
-  version "0.8.2"
-  sha256 "4008f87f49271bed6e27f3f7d359ce8537df179cad35dd8b0f6e56b4179b66e4"
+  version "0.8.3"
+  sha256 "7de803b7e9ffd24b126f2800fb3792a00a34c57993cb5e47533e08538863d3a9"
 
   url "https://github.com/keyboardio/Chrysalis/releases/download/v#{version}/Chrysalis-#{version}.dmg"
   name "Chrysalis"
