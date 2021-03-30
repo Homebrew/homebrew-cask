68cbe41aeeb0610722ed41fe304a89460db6a6e8
Bingqing Wang <10382462+bingtsingw@users.noreply.github.com>
2021-03-30 12:04:15 +0800

    Add livecheck and desc for wechatwebdevtools (#102810)

--- Casks/wechatwebdevtools.rb
+++ Casks/wechatwebdevtools.rb
@@ -3,10 +3,16 @@ cask "wechatwebdevtools" do
   sha256 "6b57f96c4031ff3568303e85011d24b6b14b544990411ab5781374e3922b4d25"
 
   url "https://dldir1.qq.com/WechatWebDev/release/p-ae42ee2cde4d42ee80ac60b35f183a99/wechat_devtools_#{version}.dmg"
-  appcast "https://developers.weixin.qq.com/miniprogram/dev/devtools/stable.html"
   name "wechat web devtools"
   name "微信web开发者工具"
+  desc "Weixin DevTools for Official Account and Mini Program development"
   homepage "https://mp.weixin.qq.com/debug/wxadoc/dev/devtools/download.html"
+
+  livecheck do
+    url "https://developers.weixin.qq.com/miniprogram/dev/devtools/download.html"
+    strategy :page_match
+    regex(%r{Stable Build</a>\s*\((\d+(?:\.\d+)*)}i)
+  end
 
   auto_updates true
 
