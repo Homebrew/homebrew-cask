7814825fcb107cdaf356d7636124a2ee8d5d1b82
cupcake99 <cupcake99@users.noreply.github.com>
2021-03-30 22:51:51 +0000

    Update omnioutliner.rb
    Remove invalid characters in version string

--- Casks/omnioutliner.rb
+++ Casks/omnioutliner.rb
@@ -1,6 +1,6 @@
 cask "omnioutliner" do
   if MacOS.version <= :high_sierra
-    version "5.4.2(n)"
+    version "5.4.2"
     sha256 "a9364dcf2ee97a871a881530785fa54d269f5e95298e2e4d2e979c70b6365395"
     url "https://downloads.omnigroup.com/software/MacOSX/10.13/OmniOutliner-#{version}.dmg"
   else
