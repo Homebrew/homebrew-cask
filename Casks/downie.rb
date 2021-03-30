ff43ed673977822e7f75b9930d7cb5b463b36d2a
Adithya Sunil <adithyasunil26@gmail.com>
2021-03-31 03:24:04 +0400

    Update downie from 4.2,4226 to 4.2.1,4229 (#102883)

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,6 +1,6 @@
 cask "downie" do
-  version "4.2,4226"
-  sha256 "1a34b2771ff0dee5092c340c8f6ad3e0569012da5528c3c28c151170f31371f9"
+  version "4.2.1,4229"
+  sha256 "8f8b92ec11426fb485089944efa809407938045ca6bc72c6ceb12dc1d522497b"
 
   url "https://charliemonroesoftware.com/trial/downie/v#{version.major}/Downie_#{version.major}_#{version.after_comma}.dmg",
       verified: "charliemonroesoftware.com/"
