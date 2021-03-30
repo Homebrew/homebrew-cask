aa391a46156ef38738e23df44ef3756a5fb88eda
Eric Nilsson <eric@isotop.se>
2021-03-30 08:02:05 +0200

    Update flutter from 2.0.2 to 2.0.3 (#102808)
    * Update flutter from 2.0.2 to 2.0.3
    
    * Update flutter.rb
    
    Co-authored-by: Eric Nilsson <eric.nilsson@isotop.se>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -1,12 +1,16 @@
 cask "flutter" do
-  version "2.0.2"
-  sha256 "0fd76e44c2080e3e9c02e2960628ca3ee0999d4ad13dd4e6b76d0f3fee76379e"
+  version "2.0.3"
+  sha256 "f9f7ef2118945252961a738b0d245e41aac289b793ee872735ae546637758551"
 
   url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_#{version}-stable.zip",
       verified: "storage.googleapis.com/flutter_infra/"
   name "Flutter SDK"
   desc "UI toolkit for building applications for mobile, web and desktop"
   homepage "https://flutter.dev/"
+
+  livecheck do
+    skip "No version information available"
+  end
 
   auto_updates true
 
