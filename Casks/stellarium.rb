0625e15ee05f2827f0f55f5e556b781102e8f62c
adithyasunil26 <adithyasunil26@gmail.com>
2021-03-31 03:16:50 +0400

    Update stellarium from 0.20.4 to 0.21.0

--- Casks/stellarium.rb
+++ Casks/stellarium.rb
@@ -1,6 +1,6 @@
 cask "stellarium" do
-  version "0.20.4"
-  sha256 "ed2c561c0fec34662672e3754aaef59713c041dfef35fe74006459bbb694a42d"
+  version "0.21.0"
+  sha256 "70fc56d06af3ea2528e9402df14494ccfc531e0ade49c5364edd4a9e9c05ffb7"
 
   url "https://github.com/Stellarium/stellarium/releases/download/v#{version.major_minor_patch}/Stellarium-#{version}.zip",
       verified: "github.com/Stellarium/stellarium/"
