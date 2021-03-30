6bad216f398f615b6606e6a63298339d3d1289c1
adithyasunil26 <adithyasunil26@gmail.com>
2021-03-31 03:27:41 +0400

    Update universal-media-server from 10.4.0 to 10.4.1

--- Casks/universal-media-server.rb
+++ Casks/universal-media-server.rb
@@ -1,6 +1,6 @@
 cask "universal-media-server" do
-  version "10.4.0"
-  sha256 "6ea596bf60cb2100eebcf359faceee81f77138e1d95e0f5ca4843d2384c6142f"
+  version "10.4.1"
+  sha256 "aa0752bda99d5dd1c3a43bd87682fafa8a25933bb25f4f162115a4cf1bf7b67f"
 
   url "https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/#{version}/UMS-#{version}.dmg",
       verified: "github.com/UniversalMediaServer/UniversalMediaServer/"
