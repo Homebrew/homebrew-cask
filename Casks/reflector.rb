64a8d4f53d0e15def9572885aefa6c5a59375f4c
Miccal Matthews <miccal.matthews@gmail.com>
2021-03-31 13:51:12 +0800

    Update reflector.rb

--- Casks/reflector.rb
+++ Casks/reflector.rb
@@ -1,14 +1,14 @@
 cask "reflector" do
-  version "4.0.0,4000"
+  version "4.0.0"
   sha256 "41fecbb26673dad787b32c4763f9d2049c61d571c49783cdc20e866d9a91f7d5"
 
-  url "https://download.airsquirrels.com/Reflector#{version.major}/Mac/Reflector-#{version.before_comma}.dmg"
+  url "https://download.airsquirrels.com/Reflector#{version.major}/Mac/Reflector-#{version}.dmg"
   name "Reflector"
   desc "Wireless screen-mirroring application"
   homepage "https://www.airsquirrels.com/reflector/"
 
   livecheck do
-    url "https://updates.airsquirrels.com/Reflector#{version.major}/Mac/Reflector#{version.major}.xml"
+    url "https://updates-prod.airsquirrels.com/Reflector#{version.major}/Mac/updateCheck/"
     strategy :sparkle
   end
 
