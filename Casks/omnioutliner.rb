9c8589b008671be048767f35c9dcfb3fe451f648
Miccal Matthews <miccal.matthews@gmail.com>
2021-03-31 14:52:15 +0800

    Update omnioutliner.rb

--- Casks/omnioutliner.rb
+++ Casks/omnioutliner.rb
@@ -1,8 +1,8 @@
 cask "omnioutliner" do
   if MacOS.version <= :high_sierra
-    version "5.4.2(n)"
+    version "5.4.2"
     sha256 "a9364dcf2ee97a871a881530785fa54d269f5e95298e2e4d2e979c70b6365395"
-    url "https://downloads.omnigroup.com/software/MacOSX/10.13/OmniOutliner-#{version}.dmg"
+    url "https://downloads.omnigroup.com/software/MacOSX/10.13/OmniOutliner-#{version}(n).dmg"
   else
     version "5.8.3"
     sha256 "e6b889949b2b1c1355b1c83b5c63e56ad3f27efdb10ff9228e37f40755df0865"
