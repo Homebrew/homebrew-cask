52408e2f92620bc77afbae9fd9818bdd22bde4d2
Mcdull <30206217+McDull-GitHub@users.noreply.github.com>
2021-04-01 00:50:03 +0800

    Update geph from 4.3.0 to 4.3.1 (#102947)

--- Casks/geph.rb
+++ Casks/geph.rb
@@ -1,6 +1,6 @@
 cask "geph" do
-  version "4.3.0"
-  sha256 "3bffcc36865c1f554b309eda6e2ab3a0d5a75dd7a76b071f801d9b91046646c3"
+  version "4.3.1"
+  sha256 "57afc05ecca54066be7999cb14ac4688d679c6b64b9ec40a77ad49564ad8d33b"
 
   url "https://f001.backblazeb2.com/file/geph4-dl/Geph4Releases/#{version}/geph-macos-#{version}.dmg",
       verified: "f001.backblazeb2.com/file/geph4-dl/"
