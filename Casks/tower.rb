58c469f9b367b7d1730ba803eff298ddae942081
Nick Wind-McJetters <nick@elearningexperts.net>
2021-03-31 03:10:26 -0400

    Update tower from 6.3,273:53291173 to 6.4,278:f34dfbdd (#102620)
    * Update tower from 6.3,273:53291173 to 6.4,278:f34dfbdd
    
    * Update tower.rb
    
    * Update tower.rb
    
    Co-authored-by: Markus Reiter <me@reitermark.us>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/tower.rb
+++ Casks/tower.rb
@@ -4,10 +4,17 @@ cask "tower" do
 
   url "https://fournova-app-updates.s3.amazonaws.com/apps/tower3-mac/#{version.after_comma.before_colon}-#{version.after_colon}/Tower-#{version.before_comma}-#{version.after_comma.before_colon}.zip",
       verified: "fournova-app-updates.s3.amazonaws.com/"
-  appcast "https://updates.fournova.com/updates/tower3-mac/stable"
   name "Tower"
   desc "Git client focusing on power and productivity"
   homepage "https://www.git-tower.com/"
+
+  livecheck do
+    url "https://updates.fournova.com/updates/tower3-mac/stable"
+    strategy :page_match do |page|
+      match = page.match(%r{(\d+(?:\.\d+)*)-([a-z0-9]+)/Tower-(\d+(?:\.\d+)*)-(\d+(?:\.\d+)*)\.zip}i)
+      "#{match[3]},#{match[1]}:#{match[2]}"
+    end
+  end
 
   auto_updates true
 
