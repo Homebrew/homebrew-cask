449b46c48272f81f55a79215da61c25360e53ef5
Markus Reiter <me@reitermark.us>
2021-03-31 07:51:42 +0200

    robofont: add livecheck (#102744)
    * robofont: add livecheck
    
    * Update robofont.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/robofont.rb
+++ Casks/robofont.rb
@@ -1,12 +1,19 @@
 cask "robofont" do
-  version "3.4"
+  version "3.4,2004281315"
   sha256 :no_check
 
   url "https://static.typemytype.com/robofont/RoboFont.dmg",
       verified: "static.typemytype.com/robofont/"
-  appcast "https://doc.robofont.com/appcast.xml"
   name "RoboFont"
   homepage "https://robofont.com/"
+
+  livecheck do
+    url "https://doc.robofont.com/appcast.xml"
+    strategy :page_match do |page|
+      match = page.match(/Version\s(\d+(?:\.\d+)*)\s\(build\s(\d+(?:\.\d+)*)\)/i)
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   app "RoboFont.app"
 end
