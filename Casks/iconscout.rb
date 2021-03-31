e56e891904ddd91d5309469eb63e60420e0b28be
ye4241 <ye4241@gmail.com>
2021-03-31 14:43:14 +0800

    add iconscout cask (#102628)
    * add iconscout cask;
    
    * Update iconscout.rb
    
    * Update iconscout.rb
    
    * remove livecheck logic;
    
    * Update iconscout.rb
    
    * Update iconscout.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/iconscout.rb
@@ -0,0 +1,25 @@
+cask "iconscout" do
+  version "1.0.1"
+  sha256 "610c8e2d3c9d5bc2c9f7907ecff4ffa4e6dd76bf6b1b6033aa53ebbd7a3a5859"
+
+  url "https://iconscout.com/download/v#{version}/Iconscout-#{version}.dmg"
+  name "Iconscout"
+  desc "Desktop toolbar for Iconscout"
+  homepage "https://iconscout.com/"
+
+  livecheck do
+    url "https://iconscout.com/download"
+    strategy :page_match
+    regex(/Iconscout-(\d+(?:\.\d+)*)/i)
+  end
+
+  app "Iconscout.app"
+
+  zap trash: [
+    "~/Library/Application Support/Iconscout",
+    "~/Library/Caches/com.electron.iconscout",
+    "~/Library/Caches/com.electron.iconscout.ShipIt",
+    "~/Library/Preferences/com.electron.iconscout.plist",
+    "~/Library/Saved Application State/com.electron.iconscout.savedState",
+  ]
+end
