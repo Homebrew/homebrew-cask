c1301452610473089a944e0be0d644e7f7dd4611
Loïc PÉRON <44115467+nicerloop@users.noreply.github.com>
2021-03-31 08:02:33 +0200

    Update eclipse-dsl from 4.18.0,2020-12:R to 4.19.0,2021-03:R (#102641)

--- Casks/eclipse-dsl.rb
+++ Casks/eclipse-dsl.rb
@@ -1,15 +1,15 @@
 cask "eclipse-dsl" do
-  version "4.18.0,2020-12:R"
-  sha256 "1a17c68f62d690576a4e32be7febc30dd4efb827b4a8a558a5ca46206263370b"
+  version "4.19.0,2021-03:R"
+  sha256 "88c544a0d54e3d0792721afc717a61e72e1484156676e2d0905144ebc03d2ffc"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-dsl-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name "Eclipse IDE for Java and DSL Developers"
   homepage "https://eclipse.org/"
 
   livecheck do
-    url "https://projects.eclipse.org/releases/"
+    url "https://www.eclipse.org/downloads/packages/"
     strategy :page_match do |page|
-      page.scan(%r{href=.*projects.eclipse.org/releases/(\d+-\d+)}i).map do |release|
+      page.scan(%r{href=.*?/downloads/packages/release/(\d+-\d+)}i).map do |release|
         version_page = Net::HTTP.get(URI.parse("https://projects.eclipse.org/releases/#{release[0]}"))
         version = version_page.scan(%r{href="/projects/eclipse/releases/(\d+(?:\.\d+)*)"}i)
         "#{version[0][0]},#{release[0]}:R"
