ba6fe863700df00ee2f0c4de257205f29a3cf453
Miccal Matthews <miccal.matthews@gmail.com>
2021-03-31 11:10:30 +0800

    Update napari from 0.4.2 to 0.4.7 (#102926)

--- Casks/napari.rb
+++ Casks/napari.rb
@@ -1,6 +1,6 @@
 cask "napari" do
-  version "0.4.2"
-  sha256 "8a0481bf710fd2225a7777dbc94343caaa03928940d66e83b80d326ea61650ae"
+  version "0.4.7"
+  sha256 "5f6151053cfc09d8ed569dffc0b95ef66861597ad81e19cb99f875b620dcf3d8"
 
   url "https://github.com/napari/napari/releases/download/v#{version}/napari-#{version}-macOS.zip",
       verified: "github.com/napari/napari/"
