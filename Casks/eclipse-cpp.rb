820ebf2f228a2b7b6a8958a29835eba3b1409de6
Loïc PÉRON <44115467+nicerloop@users.noreply.github.com>
2021-03-31 02:14:00 +0200

    Update eclipse-cpp from 4.18.0,2020-12:R to 4.19.0,2021-03:R (#102632)

--- Casks/eclipse-cpp.rb
+++ Casks/eclipse-cpp.rb
@@ -1,15 +1,15 @@
 cask "eclipse-cpp" do
-  version "4.18.0,2020-12:R"
-  sha256 "5a2d22ea7239ee7c4b20d6433fb77cbe8cfe3d55b2e5280615490de9098b8986"
+  version "4.19.0,2021-03:R"
+  sha256 "4eb035478fb52c1ae3ae26322dc9ec0967a18155283fb44afebee5f12189893a"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-cpp-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name "Eclipse IDE for C/C++ Developers"
   homepage "https://eclipse.org/"
 
   livecheck do
-    url "https://projects.eclipse.org/releases/"
+    url "https://www.eclipse.org/downloads/packages/"
     strategy :page_match do |page|
-      page.scan(%r{href=.*projects.eclipse.org/releases/(\d+-\d+)}i).map do |release|
+      page.scan(%r{href=.*?/downloads/packages/release/(\d+-\d+)}i).map do |release|
         version_page = Net::HTTP.get(URI.parse("https://projects.eclipse.org/releases/#{release[0]}"))
         version = version_page.scan(%r{href="/projects/eclipse/releases/(\d+(?:\.\d+)*)"}i)
         "#{version[0][0]},#{release[0]}:R"
