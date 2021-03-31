54090b13ca25caf0e369905424bdf215e065c615
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-31 06:47:37 +0100

    Update lyx from 2.3.6.1 to 2.3.6.2 (#102774)
    * Update lyx from 2.3.6.1 to 2.3.6.2
    
    * Update lyx.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/lyx.rb
+++ Casks/lyx.rb
@@ -1,6 +1,6 @@
 cask "lyx" do
-  version "2.3.6.1"
-  sha256 "fd881be1cfd25349e09e5e8bb234d7ea66f9eab5da2d9dba80a2fdb23281937b"
+  version "2.3.6.2"
+  sha256 "7eb15120428cbb45f5ad0e7e04c7cd314c87dd6657157bf70f5330e3d26811d0"
 
   url "https://ftp.lip6.fr/pub/lyx/bin/#{version.major_minor_patch}/LyX-#{version}+qt5-12-x86_64-cocoa.dmg",
       verified: "ftp.lip6.fr/pub/lyx/"
@@ -8,8 +8,8 @@ cask "lyx" do
   homepage "https://www.lyx.org/"
 
   livecheck do
-    url "https://www.lyx.org/misc/rss/lyx_news_feed.xml"
-    regex(/LyX (\d+(?:\.\d+)*)/i)
+    url "https://www.lyx.org/Download"
+    regex(/LyX-(\d+(?:\.\d+)*)\+qt5/i)
   end
 
   app "LyX.app"
