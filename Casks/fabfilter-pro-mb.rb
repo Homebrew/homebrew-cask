78c77d1df1b3c3f2bde2a1aca234942d707c1e3c
Markus Reiter <me@reitermark.us>
2021-03-31 02:05:14 +0200

    Update fabfilter-pro-mb.rb

--- Casks/fabfilter-pro-mb.rb
+++ Casks/fabfilter-pro-mb.rb
@@ -10,7 +10,7 @@ cask "fabfilter-pro-mb" do
   livecheck do
     url "https://www.fabfilter.com/download"
     strategy :page_match do |page|
-      match = page.match(%r{href=".+/ffpromb(\d)(\d\d).dmg"}i)
+      match = page.match(%r{href=".*?/ffpromb(\d)(\d\d)\.dmg}i)
       "#{match[1]}.#{match[2]}"
     end
   end
