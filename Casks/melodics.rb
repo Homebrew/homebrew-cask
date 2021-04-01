9eecf82955b3243589cb736539aa076da5157f93
Ralph Martin <ralph@cs.cf.ac.uk>
2021-04-01 07:15:29 +0100

    Update melodics from 2.1.5785 to 2.1.5818 (#102956)

--- Casks/melodics.rb
+++ Casks/melodics.rb
@@ -1,6 +1,6 @@
 cask "melodics" do
-  version "2.1.5785"
-  sha256 "e9b7e0884056727159ef0e26cce6fce62b8976f5a736d22e13febd8b8659b46a"
+  version "2.1.5818"
+  sha256 "171f5e166f31db27f6f50b63f1116d8d5ec9ac9e34b991163e90b47799b7292a"
 
   url "https://web-cdn.melodics.com/download/MelodicsV#{version.major}.dmg"
   name "Melodics"
