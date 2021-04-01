521f9f5a8c501953a536eb16486889b6ed551f51
Christian Kraemer <christian.kraemer@gmail.com>
2021-04-01 09:59:13 +0200

    Update qownnotes from 21.3.7 to 21.3.9 (#102959)

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,6 +1,6 @@
 cask "qownnotes" do
-  version "21.3.7"
-  sha256 "95ddaa48ac00ac10e81241e7b91ab1623f4a7fcb5ff391eac1628322c5610ebd"
+  version "21.3.9"
+  sha256 "06a858ebbf56bdb5073125afb2290c2e2943004e5c2e5f5827e180294c47dea9"
 
   url "https://github.com/pbek/QOwnNotes/releases/download/v#{version}/QOwnNotes.dmg",
       verified: "github.com/pbek/QOwnNotes/"
