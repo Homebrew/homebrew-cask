4a520521b8b8e0ab3a3094e67275253db379263e
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-01 17:34:27 +0800

    Update opencore-configurator.rb

--- Casks/opencore-configurator.rb
+++ Casks/opencore-configurator.rb
@@ -3,7 +3,6 @@ cask "opencore-configurator" do
   sha256 :no_check
 
   url "https://mackie100projects.altervista.org/apps/opencoreconf/download-new-build.php?version=last"
-  appcast "https://mackie100projects.altervista.org/download-opencore-configurator/"
   name "OpenCore Configurator"
   desc "OpenCore EFI bootloader configuration helper"
   homepage "https://mackie100projects.altervista.org/opencore-configurator/"
