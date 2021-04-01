799f7150c0016b1319ea73a56e5be24848a7215c
LanikSJ <LanikSJ@users.noreply.github.com>
2021-04-01 16:40:28 -0700

    Update brave-browser from 89.1.22.70,122.70 to 89.1.22.71,122.71 (#102994)

--- Casks/brave-browser.rb
+++ Casks/brave-browser.rb
@@ -1,8 +1,8 @@
 cask "brave-browser" do
-  version "89.1.22.70,122.70"
+  version "89.1.22.71,122.71"
 
   if Hardware::CPU.intel?
-    sha256 "af6cffe53e261bcb61eba4f13a454770b6116e8d9dcc5f6ee72dbced382f915a"
+    sha256 "81dd908101302727a78739fba3f4b732d809223d9ffe67cdd0e241805c14c6d2"
 
     url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/stable/#{version.after_comma}/Brave-Browser-x64.dmg",
         verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
@@ -12,7 +12,7 @@ cask "brave-browser" do
       strategy :sparkle
     end
   else
-    sha256 "873c57b3decce46f8a8bc59b12b00636dfd509529bc228fb622b7f7892317d9c"
+    sha256 "8e47833439e5ade7a4719373e41db785dc04fc9dd1d4c4cdef7987b2628847e6"
 
     url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/stable-arm64/#{version.after_comma}/Brave-Browser-universal.dmg",
         verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
