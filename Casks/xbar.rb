6303743b9ac201df48c70e51dca0fd8d95367116
Jeff Bonhag <jbonhag@hashicorp.com>
2021-04-01 13:26:40 -0400

    Update xbar from 2.0.33-beta to 2.0.38-beta (#102979)

--- Casks/xbar.rb
+++ Casks/xbar.rb
@@ -1,6 +1,6 @@
 cask "xbar" do
-  version "2.0.33-beta"
-  sha256 "b39d133badf0fb27c2fa6d3936f70bb9543cc1bc1e56dea7a817e0bda5b6e589"
+  version "2.0.38-beta"
+  sha256 "b3386dc7ac9ce092b1a1cd8e1cbbda6369db89584708e6b95c630f0c0450a44d"
 
   url "https://github.com/matryer/xbar/releases/download/v#{version}/xbar.v#{version}.dmg",
       verified: "github.com/matryer/xbar/"
