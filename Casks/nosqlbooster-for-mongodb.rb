147b47a81518bfd5f231bf7c636f05c681c0980c
Felix Honer <felix.honer@novatec-gmbh.de>
2021-04-01 21:50:15 +0200

    Update nosqlbooster-for-mongodb from 6.2.8 to 6.2.11 (#102987)

--- Casks/nosqlbooster-for-mongodb.rb
+++ Casks/nosqlbooster-for-mongodb.rb
@@ -1,6 +1,6 @@
 cask "nosqlbooster-for-mongodb" do
-  version "6.2.8"
-  sha256 "766274ca074b9cd5f9499435b9678c65c8efa24df0a71bedc92ae411c2fed786"
+  version "6.2.11"
+  sha256 "13677878e0b1015a0cc8f211e391297dbb5f291c28b88390ec80c538e27cdbf4"
 
   url "https://s3.mongobooster.com/download/releasesv#{version.major}/nosqlbooster4mongo-#{version}.dmg",
       verified: "mongobooster.com/"
