7a03d78da5b5e6cacc301e4c0b3c42d8e6c56fe0
Reno <25192197+singularitti@users.noreply.github.com>
2021-04-01 08:17:37 -0400

    Update downie from 4.2.1,4230 to 4.2.1,4231 (#102966)

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,6 +1,6 @@
 cask "downie" do
-  version "4.2.1,4230"
-  sha256 "e2f873315446cd915d00267a76b0dbd743e861b94723fef1e0012c5b6037cf8e"
+  version "4.2.1,4231"
+  sha256 "e08535d068bff1189ffe4531df6c4c56534eb60b73a593e264bd5bc9deb96eff"
 
   url "https://charliemonroesoftware.com/trial/downie/v#{version.major}/Downie_#{version.major}_#{version.after_comma}.dmg",
       verified: "charliemonroesoftware.com/"
