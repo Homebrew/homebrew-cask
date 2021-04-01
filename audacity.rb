00208c3a9eca039420c8895a4d5370e8f72e1ceb
Thomas <thomassth@users.noreply.github.com>
2021-04-02 02:57:08 +0800

    Add Audacity.app v3.0
    Continuing #12972
    
    By audacity 3.0.0, all compiled packages are listed on Github.
    
    [https://github.com/audacity/audacity/releases](https://github.com/audacity/audacity/releases)
    
    The rule "audacity is not allowed: Audacity was removed because it is too difficult to download programmatically." should no longer apply.

---
+++ audacity.rb
@@ -0,0 +1,12 @@
+cask "audacity" do
+  version "3.0.0"
+  sha256 "f769a9e8c0f2352171933339681616ddab43ca6d7b688918e5af8bd1382a0fad"
+
+  url "https://github.com/audacity/audacity/releases/download/Audacity-#{version}/audacity-macos-#{version}.dmg",
+      verified: "github.com/audacity/audacity/"
+  name "Audacity"
+  desc "Easy-to-use, multi-track audio editor and recorder"
+  homepage "https://www.audacityteam.org/"
+
+  app "Audacity.app"
+end
