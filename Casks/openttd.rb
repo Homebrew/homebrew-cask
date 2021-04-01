d9a4d570daaa870341fd2afd371c57f426e0e6ba
Eric Knibbe <enk3@outlook.com>
2021-04-01 18:28:59 -0400

    Update openttd from 1.10.3 to 1.11.0 (#102970)

--- Casks/openttd.rb
+++ Casks/openttd.rb
@@ -1,15 +1,16 @@
 cask "openttd" do
-  version "1.10.3"
-  sha256 "7ed03914700007849ab46fdb1a874267e7b53b637abfc5bad7b4c92001a7b858"
+  version "1.11.0"
+  sha256 "93316b061800271b933cd131a4b8507bc354d211e200e50066736b5d36a26786"
 
-  url "https://proxy.binaries.openttd.org/openttd-releases/#{version}/openttd-#{version}-macosx.zip"
+  url "https://proxy.binaries.openttd.org/openttd-releases/#{version}/openttd-#{version}-macos-universal.zip"
   name "OpenTTD"
+  desc "Open-source transport simulation game"
   homepage "https://www.openttd.org/"
 
   livecheck do
     url "https://www.openttd.org/downloads/openttd-releases/latest.html"
     strategy :page_match
-    regex(%r{href=.*?/openttd-(\d+(?:\.\d+)*)-macosx\.zip}i)
+    regex(%r{href=.*?/openttd-(\d+(?:\.\d+)*)-macos-universal\.zip}i)
   end
 
   app "OpenTTD.app"
