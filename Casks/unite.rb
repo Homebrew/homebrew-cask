8410ebe1cefe356e816285d2bfa64f7fa2d39076
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-02 07:08:27 +0800

    Update unite.rb

--- Casks/unite.rb
+++ Casks/unite.rb
@@ -4,10 +4,14 @@ cask "unite" do
 
   url "https://bzgdownloads.s3.amazonaws.com/Unite/Unite+#{version}.zip",
       verified: "bzgdownloads.s3.amazonaws.com/Unite/"
-  appcast "https://bzgdownloads.s3.amazonaws.com/Unite/App+Cast/Unite+4+appcast.xml"
   name "Unite"
   desc "Turn websites into apps"
   homepage "https://bzgapps.com/unite"
+
+  livecheck do
+    url "https://bzgdownloads.s3.amazonaws.com/Unite/App+Cast/Unite+#{version.major}+appcast.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :high_sierra"
