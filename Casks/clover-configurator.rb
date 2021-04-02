1e477fbd54fcbdaaef20e5698e8b55504a3ea9a0
Vítor Galvão <info@vitorgalvao.com>
2021-04-02 14:44:27 +0100

    Update clover-configurator.rb

--- Casks/clover-configurator.rb
+++ Casks/clover-configurator.rb
@@ -3,7 +3,7 @@ cask "clover-configurator" do
   sha256 :no_check
 
   url "https://mackie100projects.altervista.org/apps/cloverconf/download-new-build.php?version=global",
-    referer: "https://mackie100projects.altervista.org/download-clover-configurator/"
+    referer: "https://mackie100projects.altervista.org/"
   appcast "https://mackie100projects.altervista.org/download-clover-configurator/"
   name "Clover Configurator"
   desc "Clover EFI bootloader configuration helper"
