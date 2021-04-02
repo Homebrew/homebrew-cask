7402d363f51f56d9a5cdb6e4f81cf15001a5f99b
Vítor Galvão <info@vitorgalvao.com>
2021-04-02 23:23:57 +0100

    opencore-configurator.rb: add referer (#103008)

--- Casks/opencore-configurator.rb
+++ Casks/opencore-configurator.rb
@@ -2,7 +2,8 @@ cask "opencore-configurator" do
   version "2.31.0.0"
   sha256 :no_check
 
-  url "https://mackie100projects.altervista.org/apps/opencoreconf/download-new-build.php?version=last"
+  url "https://mackie100projects.altervista.org/apps/opencoreconf/download-new-build.php?version=last",
+      referer: "https://mackie100projects.altervista.org/"
   appcast "https://mackie100projects.altervista.org/download-opencore-configurator/"
   name "OpenCore Configurator"
   desc "OpenCore EFI bootloader configuration helper"
