8959fed315c46a84a3f5c0458fcd5b19c973144c
Thomas Amerongen <thomas.amerongen@gmail.com>
2021-04-03 10:40:29 +1100

    Update vmpk from 0.8.1 to 0.8.2

--- Casks/vmpk.rb
+++ Casks/vmpk.rb
@@ -1,5 +1,5 @@
 cask "vmpk" do
-  version "0.8.1"
+  version "0.8.2"
   sha256 "9465c053fc5118924063db24952d60302f8125619ae75bc4e17b7be0f6f7548d"
 
   url "https://downloads.sourceforge.net/vmpk/#{version.major_minor_patch}/vmpk-#{version}-mac-x64.dmg",
