ac6dd7f5d9abe2e968c63386ad3f7a330bc3d9fa
Changhyun Kwon <chkwon@users.noreply.github.com>
2021-04-02 21:16:46 -0400

    Update mactex from 2020.0407 to 2021.0328 (#103014)

--- Casks/mactex.rb
+++ Casks/mactex.rb
@@ -1,6 +1,6 @@
 cask "mactex" do
-  version "2020.0407"
-  sha256 "a33af89de36c7c84a76050c9704d50d23892e9c2070f04f6a53e1c6d5a332f67"
+  version "2021.0328"
+  sha256 "e541257d70f911550341853709fc45d9fa9fcd4c93058382000ebb19b284833b"
 
   url "http://mirror.ctan.org/systems/mac/mactex/mactex-#{version.no_dots}.pkg",
       verified: "mirror.ctan.org/systems/mac/mactex/"
@@ -9,7 +9,7 @@ cask "mactex" do
   homepage "https://www.tug.org/mactex/"
 
   livecheck do
-    url "http://mirror.ctan.org/systems/mac/mactex/"
+    url "https://ctan.org/texarchive/systems/mac/mactex/"
     strategy :page_match do |page|
       match = page.match(/href=.*?mactex-(\d{4})(\d{2})(\d{2})\.pkg/)
       "#{match[1]}.#{match[2]}#{match[3]}"
@@ -27,13 +27,13 @@ cask "mactex" do
       choices: [
         {
           # Ghostscript
-          "choiceIdentifier" => "org.tug.mactex.ghostscript9.50",
+          "choiceIdentifier" => "org.tug.mactex.ghostscript9.53.3",
           "choiceAttribute"  => "selected",
           "attributeSetting" => 0,
         },
         {
           # Ghostscript Dynamic Library
-          "choiceIdentifier" => "org.tug.mactex.ghostscript9.50libgs",
+          "choiceIdentifier" => "org.tug.mactex.ghostscript9.53.3libgs",
           "choiceAttribute"  => "selected",
           "attributeSetting" => 0,
         },
