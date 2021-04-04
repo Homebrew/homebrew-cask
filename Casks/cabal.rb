9169a5fc30e4440a5b722491e1b116ee0b8b61b3
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-04-04 16:27:50 +0900

    cabal: update to 6.0.7 (#103038)

--- Casks/cabal.rb
+++ Casks/cabal.rb
@@ -1,6 +1,6 @@
 cask "cabal" do
-  version "6.0.6"
-  sha256 "ff2a31c5383c6e16e1fc79b0d1ce6feaf5884c371bce4846b3bd837a6d0f833f"
+  version "6.0.7"
+  sha256 "4827aa8a686832fa79a0d08429e10e14d5e6e3a0ea36f453e611cc9ee14da027"
 
   url "https://github.com/cabal-club/cabal-desktop/releases/download/v#{version}/cabal-desktop-#{version}-mac.dmg",
       verified: "github.com/cabal-club/cabal-desktop/"
