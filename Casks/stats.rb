47f926583e423dbf1409897ea94cf4e4b43d9ddf
optikfluffel <optik@fluffel.io>
2021-04-04 22:18:47 +0200

    Update stats from 2.5.7 to 2.5.8

--- Casks/stats.rb
+++ Casks/stats.rb
@@ -1,6 +1,6 @@
 cask "stats" do
-  version "2.5.7"
-  sha256 "448495f161c2c36d9fb5db914f1ddad5616a552cb846fed757b147ff35ec535a"
+  version "2.5.8"
+  sha256 "d54af6b90efc23a464f7d8f0278c9dd1f50d84cdb2b9326bf7eae1e89f04634b"
 
   url "https://github.com/exelban/stats/releases/download/v#{version}/Stats.dmg"
   name "Stats"
