4d06a436c4dfb00591568f05e6b7bba7eb09fcfe
Misaka13514 <Misaka13514@gmail.com>
2021-04-05 15:10:25 +0900

    Update dosbox-x from 0.83.11 to 0.83.12

--- Casks/dosbox-x.rb
+++ Casks/dosbox-x.rb
@@ -1,12 +1,12 @@
 cask "dosbox-x" do
   if Hardware::CPU.intel?
-    version "0.83.11,20210301003839"
+    version "0.83.12,20210401031910"
     sha256 "bba985e13ec27e7e617197c3a32ff3d864e72d419b0cade85626344720ed453a"
 
     url "https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v#{version.before_comma}/dosbox-x-macosx-x86_64-#{version.after_comma}.zip",
         verified: "github.com/joncampbell123/dosbox-x/"
   else
-    version "0.83.11,20210301003750"
+    version "0.83.12,20210401114115"
     sha256 "30cf1f6407ab70f0b958a7232d9555328d81ec2fe935dc2a70fdd5f2ef119599"
 
     url "https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v#{version.before_comma}/dosbox-x-macosx-arm64-#{version.after_comma}.zip",
