549dcc6010b8678da436773e4801ec17040c3aec
Markus Reiter <me@reitermark.us>
2021-04-06 01:32:52 +0200

    Update principle from 6.3,6004 to 6.4,6006

--- Casks/principle.rb
+++ Casks/principle.rb
@@ -1,6 +1,6 @@
 cask "principle" do
-  version "6.3,6004"
-  sha256 "1968b6441587cf278a991d4a3f38030395bb99f0ac992db624c2bd5509b2bf5a"
+  version "6.4,6006"
+  sha256 "e5d8c4a14b51d4263a6e70fccd552f56e7f2fb98ced6c41313c793a85f2e5002"
 
   url "https://principleformac.com/download/Principle_#{version.before_comma.dots_to_underscores}.zip"
   name "Principle"
