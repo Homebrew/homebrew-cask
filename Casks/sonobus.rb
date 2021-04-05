4fe52a93737d6615bb3aabd55630d2a3f99764fe
Markus Reiter <me@reitermark.us>
2021-04-06 01:22:29 +0200

    Update sonobus from 1.4.2 to 1.4.3 (#103145)

--- Casks/sonobus.rb
+++ Casks/sonobus.rb
@@ -1,6 +1,6 @@
 cask "sonobus" do
-  version "1.4.2"
-  sha256 "44654fce01e82f44a69e9ec91775a2394282bd02fa9cd205776d861469096a06"
+  version "1.4.3"
+  sha256 "ca4693765e3cc43de47432925c2cb54e31291924d487f237d1132253dafa4235"
 
   url "https://sonobus.net/releases/sonobus-#{version}-mac.dmg"
   name "SonoBus"
