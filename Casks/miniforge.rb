2c5924ea3f94a3932899a4ca010de5cdf6d10c04
Mohammad Momayezi <mhdmyz@MHDMYZs-MacBook-Pro.local>
2021-04-06 01:38:25 +0430

    Update miniforge from 4.9.2-7 to 4.10.0-0

--- Casks/miniforge.rb
+++ Casks/miniforge.rb
@@ -1,16 +1,16 @@
 cask "miniforge" do
-  version "4.9.2-7"
+  version "4.10.0-0"
 
   if Hardware::CPU.intel?
     sha256 "5813f9eadda46f655fdcf2daee10a8586ba067bb453551466fb66168268ea31b"
-    url "https://github.com/conda-forge/miniforge/releases/download/#{version}/Miniforge3-#{version}-MacOSX-x86_64.sh"
+    url "https://github.com/conda-forge/miniforge/releases/download/4.10.0-0/Miniforge3-4.10.0-0-MacOSX-arm64.sh"
 
     installer script: {
       executable: "Miniforge3-#{version}-MacOSX-x86_64.sh",
       args:       ["-b", "-p", "#{caskroom_path}/base"],
     }
   else
-    sha256 "6778b36e1a4f79fc28609019eb00719e5b4253fd4dc8dfd24d17882d378aea5a"
+    sha256 "f939014048e40d127082c41b66bb98760fa2ce586b0b0dc043f6a38c78198e78"
     url "https://github.com/conda-forge/miniforge/releases/download/#{version}/Miniforge3-#{version}-MacOSX-arm64.sh"
 
     installer script: {
