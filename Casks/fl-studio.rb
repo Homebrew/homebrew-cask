4203d299dbde2e7b243b59631445a075a8054bfd
Misaka13514 <Misaka13514@gmail.com>
2021-04-05 16:17:06 +0900

    Update fl-studio from 20.8.3.1566 to 20.8.3.1574

--- Casks/fl-studio.rb
+++ Casks/fl-studio.rb
@@ -1,6 +1,6 @@
 cask "fl-studio" do
-  version "20.8.3.1566"
-  sha256 "51f71bfb9ba47f9aac242686c8ef1fda859724fb161476874977564c35ed904d"
+  version "20.8.3.1574"
+  sha256 "bff1151928daa91a97222a70b4944545dedb87f7483e14c2927e7c0666c86372"
 
   url "https://demodownload.image-line.com/flstudio/flstudio_mac_#{version}.dmg"
   name "FL Studio"
