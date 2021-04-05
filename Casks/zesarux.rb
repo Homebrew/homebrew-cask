0580bd9d07fab795aabf29f7ba4143ec30011e3f
Markus Reiter <me@reitermark.us>
2021-04-06 01:29:18 +0200

    Update zesarux from 9.1 to 9.2 (#103150)

--- Casks/zesarux.rb
+++ Casks/zesarux.rb
@@ -1,6 +1,6 @@
 cask "zesarux" do
-  version "9.1"
-  sha256 "4522b7fa4425258eaaa1d915c16a5141d81b01e0b7fc66c3f3f0ef09361f3e6c"
+  version "9.2"
+  sha256 "66920b8f4e8af7178e1267152f6eb5056349fb91ae5453c42940be0b29ad9a8f"
 
   url "https://github.com/chernandezba/zesarux/releases/download/#{version}/ZEsarUX_macos-#{version}.dmg"
   name "ZEsarUX"
