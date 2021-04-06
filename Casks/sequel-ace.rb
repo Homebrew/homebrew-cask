b7832628c19fc8e00216ecaab3689ec9bac5e66b
Mario Fehr <mario.fehr@massiveart.com>
2021-04-06 11:32:01 +0200

    Update sequel-ace from 3.2.3,3024 to 3.3.0,3026 (#103133)
    * Update sequel-ace from 3.2.3,3024 to 3.3.0,3026
    
    * Update sequel-ace.rb
    
    * Update sequel-ace.rb
    
    * Update sequel-ace.rb
    
    Co-authored-by: Markus Reiter <me@reitermark.us>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/sequel-ace.rb
+++ Casks/sequel-ace.rb
@@ -1,11 +1,21 @@
 cask "sequel-ace" do
-  version "3.2.3,3024"
-  sha256 "c9119c42e5d266bee224e1b8cde3a458ab93e82979025947a3662dcc3e5b87cd"
+  version "3.3.0,3026"
+  sha256 "8988b38ed3b21cbdb34920326d7d88b4a1b205adbab05234da2ce73a9fa26cff"
 
   url "https://github.com/Sequel-Ace/Sequel-Ace/releases/download/production/#{version.before_comma}-#{version.after_comma}/Sequel-Ace-#{version.before_comma}.zip"
   name "Sequel Ace"
   desc "MySQL/MariaDB database management"
   homepage "https://github.com/Sequel-Ace/Sequel-Ace"
+
+  livecheck do
+    url :url
+    strategy :git do |tags|
+      tags.map do |tag|
+        match = tag.match(%r{^production/(\d+(?:\.\d+)*)-(\d+)$}i)
+        "#{match[1]},#{match[2]}" if match
+      end.compact
+    end
+  end
 
   app "Sequel Ace.app"
 
