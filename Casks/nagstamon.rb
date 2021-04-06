70cefb051738da272ca2034a1cd51c49613ff2f3
Henri Wahl <h.wahl@ifw-dresden.de>
2021-04-06 11:15:48 +0200

    prepare nagstamon.de

--- Casks/nagstamon.rb
+++ Casks/nagstamon.rb
@@ -2,10 +2,10 @@ cask "nagstamon" do
   version "3.4.1"
   sha256 "3493578ca06b61a146d3f1af1f7858e2aee662c4fa3ef5dedfc45c0203896ac5"
 
-  url "https://nagstamon.ifw-dresden.de/files/stable/Nagstamon%20#{version}.dmg"
-  appcast "https://nagstamon.ifw-dresden.de/files/stable/sha256sums.txt"
+  url "https://github.com/HenriWahl/Nagstamon/releases/download/#{version}/Nagstamon.#{version}.dmg"
+  appcast "https://github.com/HenriWahl/Nagstamon/releases/download/#{version}/sha256sums.txt"
   name "Nagstamon"
-  homepage "https://nagstamon.ifw-dresden.de/"
+  homepage "https://nagstamon.de/"
 
   app "Nagstamon.app"
 
