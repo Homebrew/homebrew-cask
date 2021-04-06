b0b4bed7ce01ce118f085f457752a0ea74546965
Henri Wahl <h.wahl@ifw-dresden.de>
2021-04-06 13:50:27 +0200

    added 'v' to URL

--- Casks/nagstamon.rb
+++ Casks/nagstamon.rb
@@ -2,8 +2,8 @@ cask "nagstamon" do
   version "3.6.0"
   sha256 "5b2819664604b68c3659bd57f5127bab8f117324329c9ee6e7a75f87184f90a3"
 
-  url "https://github.com/HenriWahl/Nagstamon/releases/download/#{version}/Nagstamon.#{version}.dmg"
-  appcast "https://github.com/HenriWahl/Nagstamon/releases/download/#{version}/sha256sums.txt"
+  url "https://github.com/HenriWahl/Nagstamon/releases/download/v#{version}/Nagstamon.#{version}.dmg"
+  appcast "https://github.com/HenriWahl/Nagstamon/releases/download/v#{version}/sha256sums.txt"
   name "Nagstamon"
   homepage "https://nagstamon.de/"
 
