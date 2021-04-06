ef0462949365ed8b55b8fee7fad99aa7b72443ea
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-04-07 00:48:38 +0100

    Update sigilium-email-signatures from 1.3.1 to 1.3.2 (#103284)

--- Casks/sigilium-email-signatures.rb
+++ Casks/sigilium-email-signatures.rb
@@ -1,10 +1,14 @@
 cask "sigilium-email-signatures" do
-  version "1.3.1"
+  version "1.3.2"
   sha256 :no_check
 
   url "https://sigilium.com/apps/sigilium.zip"
   name "Sigilium Email Signatures"
   homepage "https://sigilium.com/"
+
+  livecheck do
+    skip "unversioned URL"
+  end
 
   app "Sigilium Email Signatures.app"
 end
