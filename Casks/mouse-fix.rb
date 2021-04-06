9c4457d36cadc34348704f7f1a5bc9f3516f86a5
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-06 13:46:29 +0800

    Update mouse-fix from 1.0.1 to 1.0.3 (#103202)
    * Update mouse-fix from 1.0.1 to 1.0.3
    
    * Update mouse-fix.rb

--- Casks/mouse-fix.rb
+++ Casks/mouse-fix.rb
@@ -1,12 +1,17 @@
 cask "mouse-fix" do
-  version "1.0.1"
-  sha256 "923469ae24f796115eebab2baf71d05e0c6b0fef0a5a8deb2afe800c83995044"
+  version "1.0.3"
+  sha256 "263cacc448411ddc05139e01b73e49f0cf39bc415604b5ff9678305d2814f565"
 
   url "https://github.com/noah-nuebling/mac-mouse-fix/releases/download/#{version}/MacMouseFixApp.zip",
       verified: "github.com/noah-nuebling/mac-mouse-fix/"
   name "Mouse Fix"
   desc "Mouse utility to add gesture functions and smooth scrolling to 3rd party mice"
   homepage "https://mousefix.org/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   auto_updates true
 
