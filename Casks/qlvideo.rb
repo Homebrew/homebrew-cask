854df6ed1bf0811bda69b857f1aa2ab710e0531b
Yusuf Kürşad Kaya <ykursadkaya@gmail.com>
2021-04-06 21:04:43 +0300

    Update qlvideo from 1.93 to 1.94 (#103232)

--- Casks/qlvideo.rb
+++ Casks/qlvideo.rb
@@ -1,6 +1,6 @@
 cask "qlvideo" do
-  version "1.93"
-  sha256 "a0bce00cc2073fe065722409d32605cc2c20eaab89981e2d965d571d84fd77e0"
+  version "1.94"
+  sha256 "a497bd1facd041c1e51fecc24060a7a02bfbeaa859c891aa33ebbe315018847f"
 
   url "https://github.com/Marginal/QLVideo/releases/download/rel-#{version.no_dots}/QLVideo_#{version.no_dots}.pkg"
   name "QuickLook Video"
