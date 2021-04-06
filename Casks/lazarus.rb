5f1884753f1c002d2d435353ce7931d34c2f64e4
欠陥電気 <Misaka13514@gmail.com>
2021-04-06 15:21:37 +0900

    Update lazarus from 2.0.10 to 2.0.12 (#103115)
    * Update lazarus from 2.0.10 to 2.0.12
    
    * Update lazarus.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/lazarus.rb
+++ Casks/lazarus.rb
@@ -1,6 +1,6 @@
 cask "lazarus" do
-  version "2.0.10"
-  sha256 "13c000bb30cbaa73e76d3b5b2abbc55b219bd6c4443aecc158b8b51a5fceef5f"
+  version "2.0.12"
+  sha256 "2411f014a9d0a259346f5d51ee55e593ef21bd4877dc6ecc9f53c9b1f82d8b56"
 
   url "https://downloads.sourceforge.net/lazarus/Lazarus%20macOS%20x86-64/Lazarus%20#{version}/Lazarus-#{version}-x86_64-macosx.pkg",
       verified: "sourceforge.net/lazarus/"
@@ -13,6 +13,9 @@ cask "lazarus" do
 
   pkg "Lazarus-#{version}-x86_64-macosx.pkg"
 
-  uninstall pkgutil: "org.lazarus-ide.pkg.LazarusIDE",
+  uninstall pkgutil: [
+    "org.freepascal.pkg.fpcsrc-320-laz",
+    "org.lazarus-ide.pkg.LazarusIDE",
+  ],
             delete:  "/Applications/Lazarus.app"
 end
