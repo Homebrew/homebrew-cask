5c004d6c85770072c25ef08c10681947ee534e61
Markus Reiter <me@reitermark.us>
2021-04-06 07:32:48 +0200

    Update hydrogen from 1.0.1 to 2020-12-20 (#103160)
    * Update hydrogen from 1.0.1 to 2020-12-20
    
    * Update hydrogen.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/hydrogen.rb
+++ Casks/hydrogen.rb
@@ -1,10 +1,9 @@
 cask "hydrogen" do
-  version "1.0.1"
-  sha256 "20b33e3b8c308a2fb8a82dd0712c301c1187ec57b2da934ddfc1167a11252a47"
+  version "2020-12-20"
+  sha256 "9934b24741fe3d7eed7eb1e2272626157aca72217ce76f03cb5bd015b6e14d2b"
 
   url "https://downloads.sourceforge.net/hydrogen/Hydrogen-#{version}.dmg",
       verified: "downloads.sourceforge.net/hydrogen/"
-  appcast "https://sourceforge.net/projects/hydrogen/rss?path=/Hydrogen"
   name "Hydrogen"
   desc "Software synthesizer"
   homepage "http://www.hydrogen-music.org/"
