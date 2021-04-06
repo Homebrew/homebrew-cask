094fe09f1e6331b14ab194ad08058b31f354a5c0
Markus Reiter <me@reitermark.us>
2021-04-07 01:41:28 +0200

    Update cpuinfo.rb

--- Casks/cpuinfo.rb
+++ Casks/cpuinfo.rb
@@ -1,8 +1,8 @@
 cask "cpuinfo" do
-  version "1.4.6,230"
-  sha256 :no_check
+  version "1.4.6"
+  sha256 "ffff"
 
-  url "https://github.com/yusukeshibata/cpuinfo/blob/master/dist/cpuinfo.zip?raw=true"
+  url "https://github.com/yusukeshibata/cpuinfo/raw/#{version}/dist/cpuinfo.zip"
   name "cpuinfo"
   desc "CPU meter menu bar app"
   homepage "https://github.com/yusukeshibata/cpuinfo/"
