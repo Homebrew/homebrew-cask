c4b68e8eafdeb1f99f2951692f90b50c74de570d
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-06 20:13:18 +0800

    Update google-chrome.rb

--- Casks/google-chrome.rb
+++ Casks/google-chrome.rb
@@ -3,10 +3,17 @@ cask "google-chrome" do
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
-  appcast "https://omahaproxy.appspot.com/history?os=mac;channel=stable"
   name "Google Chrome"
   desc "Web browser"
   homepage "https://www.google.com/chrome/"
+
+  livecheck do
+    url "https://omahaproxy.appspot.com/history?os=mac;channel=stable"
+    strategy :page_match do |page|
+      match = page.match(/mac,stable,(\d{2}).(\d{1}).(\d{4}).(\d{3})/i)
+      "#{match[1]}.#{match[2]}.#{match[3]}.#{match[4]},#{match[3]}.#{match[4]}"
+    end
+  end
 
   auto_updates true
   depends_on macos: ">= :yosemite"
