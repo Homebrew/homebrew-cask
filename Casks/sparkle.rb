4b44dccc82d0f03f933b1b8e70f0c9ac9545323e
Markus Reiter <me@reitermark.us>
2021-04-06 23:49:07 +0200

    Update sparkle from 1.24.0 to 1.26.0 (#103247)

--- Casks/sparkle.rb
+++ Casks/sparkle.rb
@@ -1,6 +1,6 @@
 cask "sparkle" do
-  version "1.24.0"
-  sha256 "1293bbb8bbe9ebfc1d9e214e7428fc4bd29df06ebc4cc81a1aa8f6214fc09508"
+  version "1.26.0"
+  sha256 "8312cbf7528297a49f1b97692c33cb8d33254c396dc51be394e9484e4b6833a0"
 
   url "https://github.com/sparkle-project/Sparkle/releases/download/#{version}/Sparkle-#{version}.tar.xz",
       verified: "github.com/sparkle-project/Sparkle/"
