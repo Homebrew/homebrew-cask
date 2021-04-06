b33f72cf0d1bc1fbda07e24d15957bda25c720bf
Henri Wahl <h.wahl@ifw-dresden.de>
2021-04-06 13:38:52 +0200

    nagstamon v3.6.0

--- Casks/nagstamon.rb
+++ Casks/nagstamon.rb
@@ -1,6 +1,6 @@
 cask "nagstamon" do
-  version "3.4.1"
-  sha256 "3493578ca06b61a146d3f1af1f7858e2aee662c4fa3ef5dedfc45c0203896ac5"
+  version "3.6.0"
+  sha256 "5b2819664604b68c3659bd57f5127bab8f117324329c9ee6e7a75f87184f90a3"
 
   url "https://github.com/HenriWahl/Nagstamon/releases/download/#{version}/Nagstamon.#{version}.dmg"
   appcast "https://github.com/HenriWahl/Nagstamon/releases/download/#{version}/sha256sums.txt"
