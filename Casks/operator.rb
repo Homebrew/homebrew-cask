8c5461d32ad1c8a3b9ecb488a3908bb21772e8ef
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-04-07 00:41:42 +0100

    Update operator from 0.9.9 to 0.9.19.66 (#103254)

--- Casks/operator.rb
+++ Casks/operator.rb
@@ -1,12 +1,16 @@
 cask "operator" do
-  version "0.9.9"
+  version "0.9.19"
   sha256 :no_check
 
   url "https://download.prelude.org/latest?platform=darwin&variant=zip"
-  appcast "https://s3.amazonaws.com/operator.versions/dist/latest-mac.yml"
   name "Operator"
   desc "Prelude Operator is a desktop adversary emulation platform"
   homepage "https://www.prelude.org/"
+
+  livecheck do
+    url "https://s3.amazonaws.com/operator.versions/dist/latest-mac.yml"
+    strategy :electron_builder
+  end
 
   app "Operator.app"
 
