5e49cc0c29ee613db0dc9124ad1b1e6394e58ea7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-04-07 00:17:24 +0100

    Update eggplant from 21.0.0,2010241818 to 21.1.0,2101270001 (#103277)

--- Casks/eggplant.rb
+++ Casks/eggplant.rb
@@ -1,10 +1,14 @@
 cask "eggplant" do
-  version "21.0.0,2010241818"
+  version "21.1.0,2101270001"
   sha256 :no_check
 
   url "https://downloads.eggplantsoftware.com/downloads/Eggplant/eggPlant.dmg"
   name "eggPlant Functional"
   homepage "https://www.eggplantsoftware.com/eggplant-functional-downloads"
+
+  livecheck do
+    skip "unversioned URL"
+  end
 
   app "eggPlant.app"
 end
