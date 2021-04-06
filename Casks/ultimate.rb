d802b2547d5bfebb5d64a5fa6aacc31ef26d36f6
Markus Reiter <me@reitermark.us>
2021-04-07 01:26:55 +0200

    Update ultimate.rb

--- Casks/ultimate.rb
+++ Casks/ultimate.rb
@@ -3,9 +3,14 @@ cask "ultimate" do
   sha256 :no_check
 
   url "https://download.epubor.com/epubor_ultimate.zip"
-  appcast "https://www.epubor.com/ultimate.html"
   name "Ultimate Converter"
   homepage "https://www.epubor.com/"
+
+  livecheck do
+    url "https://www.epubor.com/ultimate.html"
+    strategy :page_match
+    regex(/Version:\s*(\d+(?:\.\d+)*)/i)
+  end
 
   pkg "Ultimate.pkg"
 
