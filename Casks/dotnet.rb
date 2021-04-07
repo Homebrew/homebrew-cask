2bbceeb02360b92ea5d407d300c3d7a483613571
Alex <alex@alex.town>
2021-04-07 23:13:46 +0200

    Update dotnet from 5.0.4,c8320b32-f469-4c6b-9efe-39db01667197:c98a8dd2f5be1fca335e98c49e519fa6 to 5.0.5,0e2acfdc-5dad-48a8-935d-6095a0ae2217:3ffa41859dbb1ca40a7e1ff704651178

--- Casks/dotnet.rb
+++ Casks/dotnet.rb
@@ -1,6 +1,6 @@
 cask "dotnet" do
-  version "5.0.4,c8320b32-f469-4c6b-9efe-39db01667197:c98a8dd2f5be1fca335e98c49e519fa6"
-  sha256 "79f1f101d6f24df75e2200710f7da2e03686ed5ed87afdfaf738123af9a4b1dc"
+  version "5.0.5,0e2acfdc-5dad-48a8-935d-6095a0ae2217:3ffa41859dbb1ca40a7e1ff704651178"
+  sha256 "f4f3b02899e48b7cb0f1ed8b99f38db630f2e957d47b96de1ee76444b9201dae"
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-runtime-#{version.before_comma}-osx-x64.pkg"
   appcast "https://dotnet.microsoft.com/download/dotnet-core"
