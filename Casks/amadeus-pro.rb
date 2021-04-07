6b195aed9bd327bdbc2f7af353667dfaa99b446f
Markus Reiter <me@reitermark.us>
2021-04-07 02:10:18 +0200

    Update amadeus-pro.rb

--- Casks/amadeus-pro.rb
+++ Casks/amadeus-pro.rb
@@ -4,10 +4,15 @@ cask "amadeus-pro" do
 
   url "https://s3.amazonaws.com/AmadeusPro2/AmadeusPro.zip",
       verified: "s3.amazonaws.com/AmadeusPro2/"
-  appcast "https://www.hairersoft.com/pro.html"
   name "Amadeus Pro"
   desc "Multi-purpose audio recorder, editor and converter"
   homepage "https://www.hairersoft.com/pro.html"
+
+  livecheck do
+    url "https://www.hairersoft.com/pro.html"
+    strategy :page_match
+    regex(/Download\s*Amadeus\s*Pro\s*(\d+(?:\.\d+)*)/i)
+  end
 
   app "Amadeus Pro.app"
 end
