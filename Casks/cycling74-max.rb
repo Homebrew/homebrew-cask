89bc58d7f5b11fda12c4e39348f78a7c77bba5be
Alexey Loginov <alexey@openbridge.com>
2021-04-07 15:32:00 +0300

    Update cycling74-max from 8.1.8_201027 to 8.1.10_210222

--- Casks/cycling74-max.rb
+++ Casks/cycling74-max.rb
@@ -1,6 +1,6 @@
 cask "cycling74-max" do
-  version "8.1.8_201027"
-  sha256 "dff0739068c198fb9b34f8bca1c959422ed640882b282c6ef7b3d59e5391240e"
+  version "8.1.10_210222"
+  sha256 "b44f218fa00d279b3e55accfa583835e7d30e7228618e07bdec51110fc3f3b8a"
 
   url "https://akiaj5esl75o5wbdcv2a-maxmspjitter.s3.amazonaws.com/Max#{version.no_dots}.dmg",
       verified: "akiaj5esl75o5wbdcv2a-maxmspjitter.s3.amazonaws.com/"
