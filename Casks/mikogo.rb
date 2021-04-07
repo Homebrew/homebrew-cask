d80a4aa54cfce76a6369af3ac4d92a0545376287
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-04-07 16:22:30 +0100

    Update mikogo from 5.10.0 to 5.4.1 (#103303)

--- Casks/mikogo.rb
+++ Casks/mikogo.rb
@@ -1,11 +1,15 @@
 cask "mikogo" do
-  version "5.10.0"
+  version "5.4.1"
   sha256 :no_check
 
   url "https://download.mikogo4.com/Mikogo-installer.signed.pkg",
       verified: "mikogo4.com/"
   name "Mikogo"
   homepage "https://www.mikogo.com/"
+
+  livecheck do
+    skip "unversioned URL"
+  end
 
   pkg "Mikogo-installer.signed.pkg"
 
