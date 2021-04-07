5a5f803733a6fd0f15928516a56d28e9bdd97262
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-04-07 02:27:35 +0100

    Update ultimate from 3.0.12.1125 to 3.0.13.120 (#103262)

--- Casks/ultimate.rb
+++ Casks/ultimate.rb
@@ -1,11 +1,16 @@
 cask "ultimate" do
-  version "3.0.12.1125"
+  version "3.0.13.120"
   sha256 :no_check
 
   url "https://download.epubor.com/epubor_ultimate.zip"
-  appcast "https://www.epubor.com/ultimate.html"
   name "Ultimate Converter"
   homepage "https://www.epubor.com/"
+
+  livecheck do
+    url "https://www.epubor.com/ultimate.html"
+    strategy :page_match
+    regex(/Version:\s*(\d+(?:\.\d+)*)/i)
+  end
 
   pkg "Ultimate.pkg"
 
