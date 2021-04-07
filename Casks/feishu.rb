471ac85ea71caff09d6c444a6b7ab6e9587d3b67
Bingqing Wang <10382462+bingtsingw@users.noreply.github.com>
2021-04-07 22:03:49 +0800

    update feishu cask (#103339)

--- Casks/feishu.rb
+++ Casks/feishu.rb
@@ -1,16 +1,30 @@
 cask "feishu" do
-  version "3.43.5,f94a8c"
-  sha256 "c7d192ba13a687bf62ed0b042fee18fe9291fc37a13d548c52e318f12a2d3034"
+  version "3.45.4,077514"
+  sha256 "cbc5f1ceeed0ced9c3ebf33569bce85a11c5e404c70033b89d6d074f6eba6541"
 
-  url "https://sf3-eecdn-tos.pstatp.com/obj/ee-appcenter/#{version.after_comma}/feishu-mac-#{version.before_comma}.dmg",
-      verified: "sf3-eecdn-tos.pstatp.com/"
-  appcast "https://www.feishu.cn/api/downloads"
+  url "https://sf3-cn.feishucdn.com/obj/ee-appcenter/#{version.after_comma}/feishu-mac-#{version.before_comma}.dmg",
+      verified: "sf3-cn.feishucdn.com/"
   name "feishu"
   desc "Project management software"
   homepage "https://www.feishu.cn/"
+
+  livecheck do
+    url "https://www.feishu.cn/api/downloads"
+    strategy :page_match do |page|
+      match = page.match(%r{sf3-cn\.feishucdn\.com/obj/ee-appcenter/(\w+)/feishu-mac-(\d+(?:\.\d+)*)\.dmg}i)
+      "#{match[2]},#{match[1]}"
+    end
+  end
 
   auto_updates true
 
   # Renamed for consistency: app name is different in the Finder and in a shell.
   app "Lark.app", target: "Feishu.app"
+
+  zap trash: [
+    "~/Library/Caches/com.bytedance.lark.helper",
+    "~/Library/Caches/com.electron.lark",
+    "~/Library/Preferences/com.bytedance.lark.helper.plist",
+    "~/Library/Saved Application State/com.electron.lark.savedState",
+  ]
 end
