bf4a53a3e8e514483f0e242cefc9dad13ce30ccc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-04-07 15:43:42 +0100

    Update monal from 4.9,682 to 5.0,712 (#103319)
    * Update monal from 4.9,682 to 5.0,712
    
    * Update monal.rb
    
    Co-authored-by: Markus Reiter <me@reitermark.us>

--- Casks/monal.rb
+++ Casks/monal.rb
@@ -1,11 +1,15 @@
 cask "monal" do
-  version "4.9,682"
+  version "5.0,712"
   sha256 :no_check
 
   url "https://monal.im/Monal-OSX/Monal-OSX.zip"
   name "Monal"
   desc "Tool to securely connect to chat servers"
   homepage "https://monal.im/"
+
+  livecheck do
+    skip "unversioned URL"
+  end
 
   depends_on macos: ">= :catalina"
 
