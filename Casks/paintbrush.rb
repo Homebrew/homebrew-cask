fba39653cf4621c8cd6222af8c55b784cb44cf7e
Ondřej Führer <ondrejfuhrer@users.noreply.github.com>
2021-04-08 05:02:13 +0200

    Update paintbrush from 2.5.0,20190914 to 2.5.2,20201220 (#103347)
    * Update paintbrush from 2.5.0,20190914 to 2.5.2,20201220
    
    * Update paintbrush.rb
    
    * Update paintbrush.rb
    
    * Update paintbrush.rb
    
    * Update paintbrush.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/paintbrush.rb
+++ Casks/paintbrush.rb
@@ -1,6 +1,6 @@
 cask "paintbrush" do
-  version "2.5.0,20190914"
-  sha256 "fca1d23f6da1ff1fe6768ba67cd692362cbf86ad9adf72431514740a37c513b6"
+  version "2.5.2,20210331"
+  sha256 "0fdfb727ba8fdcd50fa3ed8e768b8b781aadb918630488c723278eb0bab2a164"
 
   url "https://downloads.sourceforge.net/paintbrush/Paintbrush%202.x/Paintbrush%20#{version.before_comma}/Paintbrush-#{version.before_comma}.zip",
       verified: "downloads.sourceforge.net/paintbrush/"
@@ -9,9 +9,9 @@ cask "paintbrush" do
   homepage "https://paintbrush.sourceforge.io/"
 
   livecheck do
-    url "https://paintbrush.sourceforge.io/updates#{version.major}x.xml"
+    url "https://paintbrush.sourceforge.io/updates/paintbrush-release.xml"
     strategy :sparkle do |item|
-      "#{item.short_version}.0,#{item.version}"
+      "#{item.short_version},#{item.version}"
     end
   end
 
