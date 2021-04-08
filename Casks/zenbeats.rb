b162e2ab43658c84603259926a1094878215f55c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-04-08 05:47:18 -0400

    Update zenbeats from 2.0.6,8159 to 2.1.2,8377 (#103316)
    * Update zenbeats from 2.0.6,8159 to 2.1.2,8377
    
    * Update zenbeats.rb
    
    * Update zenbeats.rb
    
    * Update zenbeats.rb
    
    * Update zenbeats.rb
    
    * Update zenbeats.rb
    
    * Update zenbeats.rb
    
    * Update zenbeats.rb
    
    * Update zenbeats.rb
    
    Co-authored-by: Markus Reiter <me@reitermark.us>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/zenbeats.rb
+++ Casks/zenbeats.rb
@@ -1,16 +1,19 @@
 cask "zenbeats" do
-  version "2.0.6,8159"
+  version "2.1.2,8377"
   sha256 :no_check
 
   url "https://static.roland.com/assets/media/pkg/Zenbeats_Installer.pkg"
   name "Zenbeats"
+  desc "Music creation app"
   homepage "https://www.roland.com/us/products/zenbeats/"
+
+  livecheck do
+    skip "unversioned URL"
+  end
 
   pkg "Zenbeats_Installer.pkg"
 
-  uninstall pkgutil: "jp.co.roland.zenbeats.Zenbeats.pkg",
-            quit:    "jp.co.roland.zenbeats",
-            signal:  ["KILL", "jp.co.roland.zenbeats"]
+  uninstall pkgutil: "“jp.co.roland.zenbeats”"
 
   zap trash: [
     "~/Library/Caches/com.juce.locks/juceAppLock_Roland Zenbeats",
