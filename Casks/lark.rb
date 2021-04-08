fae86b08d024529afe7e90030100c5484f16cddf
Bingqing Wang <10382462+bingtsingw@users.noreply.github.com>
2021-04-08 09:28:05 +0800

    update lark from 3.39.2 to 3.45.4

--- Casks/lark.rb
+++ Casks/lark.rb
@@ -1,8 +1,8 @@
 cask "lark" do
-  version "3.39.2,b72743"
-  sha256 "68604ac99d570287dcfd12fa713fe983692b2fd15ee2d8feb9725105816d7e65"
+  version "3.45.4,df4f0d"
+  sha256 "86201a70a3f6c0ac26f6c2cfe9e09917df36e3dd0be089ee82a32f6c24609176"
 
-  url "https://sf16-va.larksuitecdn.com/obj/lark-artifact-storage/#{version.after_comma}/Lark-#{version.before_comma}.dmg",
+  url "https://sf16-va.larksuitecdn.com/obj/lark-artifact-storage/#{version.after_comma}/lark-mac-#{version.before_comma}.dmg",
       verified: "sf16-va.larksuitecdn.com/obj/lark-artifact-storage/"
   appcast "https://www.larksuite.com/api/downloads"
   name "Lark"
