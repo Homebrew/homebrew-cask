1af9b987add0b3ac761b3f283d9775dfcb045094
Dave Murphy <186561+davemurphy@users.noreply.github.com>
2021-04-08 11:07:51 +0100

    Update ganttproject.rb
    Updated to version 3.0.3000 of ganttproject with updated sha256

--- Casks/ganttproject.rb
+++ Casks/ganttproject.rb
@@ -1,6 +1,6 @@
 cask "ganttproject" do
-  version "2.8.11,2396"
-  sha256 "b8f64286ca9c04f606336994785c5ed3995c1842f8b1d82199f7ad0bb01d2851"
+  version "3.0.3000"
+  sha256 "be30dc8ded552109f16c4f4388a440f7a09aa3151632832cb30c62efb5d875de"
 
   url "https://github.com/bardsoftware/ganttproject/releases/download/ganttproject-#{version.before_comma}/ganttproject-#{version.before_comma}-r#{version.after_comma}.dmg",
       verified: "github.com/bardsoftware/ganttproject/"
