3fd5bb747fd5444ba79607e38f32bfd53864ab37
Bingqing Wang <10382462+bingtsingw@users.noreply.github.com>
2021-04-08 09:28:39 +0800

    add zap for lark

--- Casks/lark.rb
+++ Casks/lark.rb
@@ -19,4 +19,18 @@ cask "lark" do
   auto_updates true
 
   app "Lark.app"
+
+  zap trash: [
+    # feishu
+    "~/Library/Caches/com.bytedance.lark.helper",
+    "~/Library/Preferences/com.bytedance.lark.helper.plist",
+
+    # lark
+    "~/Library/Caches/com.electron.lark.helper",
+    "~/Library/Preferences/com.electron.lark.helper.plist",
+
+    # both
+    "~/Library/Caches/com.electron.lark",
+    "~/Library/Saved Application State/com.electron.lark.savedState",
+  ]
 end
