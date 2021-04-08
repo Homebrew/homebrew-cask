ade7e2ab85405a620a6e1c2991df1edf10878304
Bret Martin <bam@miranda.org>
2021-04-08 17:46:44 -0400

    Update banktivity from 8.2.1 to 8.2.2 (#103407)

--- Casks/banktivity.rb
+++ Casks/banktivity.rb
@@ -1,6 +1,6 @@
 cask "banktivity" do
-  version "8.2.1"
-  sha256 "dcdf5200731369d8b02542bf8384b3d1aadbeedaaa1112c4eb5755c9cd9d8d99"
+  version "8.2.2"
+  sha256 "f598ec9310f3cbb7253a8ea2a4fe0c7011c85e30a505bd44a718c5d1c7da56c5"
 
   url "https://www.iggsoft.com/banktivity/Banktivity#{version}.dmg",
       verified: "iggsoft.com/"
