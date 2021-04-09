4f8149fa511d3a9540968ba0c94ca57ae6eaf9a9
adithyasunil26 <adithyasunil26@gmail.com>
2021-04-10 01:10:26 +0400

    Update colour-contrast-analyser from 3.1.2 to 3.1.3

--- Casks/colour-contrast-analyser.rb
+++ Casks/colour-contrast-analyser.rb
@@ -1,6 +1,6 @@
 cask "colour-contrast-analyser" do
-  version "3.1.2"
-  sha256 "fbc56bf2d07fc6500fa28c30b292b058a3f360c5bad55da3262bb40452100294"
+  version "3.1.3"
+  sha256 "03c83561eb53031fca04c20fcd5c8dd4a08561b2e16ce044a8637591d03068c3"
 
   url "https://github.com/ThePacielloGroup/CCAe/releases/download/v#{version}/CCA-#{version}.dmg",
       verified: "github.com/ThePacielloGroup/CCAe/"
