24eb48fd33530524ff84b68954f331a32b2d00ed
Christos Varelas <subzraw@colder.xyz>
2021-04-09 09:40:35 +0200

    Bump Exodus release (#103431)

--- Casks/exodus.rb
+++ Casks/exodus.rb
@@ -1,6 +1,6 @@
 cask "exodus" do
-  version "21.3.29"
-  sha256 "fb5be5d86de3dd358a548fb411e25ea7a58f14b89d830bfab9fa6edb12fcf344"
+  version "21.4.9"
+  sha256 "e9639535e380a81be08c67db55705f7f2ff8214ba6a26ff8517e0c732fae8a05"
 
   url "https://downloads.exodus.com/releases/exodus-macos-#{version}.dmg"
   appcast "https://www.exodus.com/releases/"
