cedfc5ae58169bf75205c585ed931129e33c4482
Amory Meltzer <Amorymeltzer@gmail.com>
2021-04-08 22:13:13 -0400

    Update marathon from 20200904 to 20210408 (#103426)

--- Casks/marathon.rb
+++ Casks/marathon.rb
@@ -1,6 +1,6 @@
 cask "marathon" do
-  version "20200904"
-  sha256 "c3959e388909d0fc5d0742a2151fb2945ab7b8dfe9fcfdfabff3708a6a256949"
+  version "20210408"
+  sha256 "2b0be87a12f697c83bb2560c6a4e2a218e643fcd5035f0cc6b75db1d770753cc"
 
   url "https://github.com/Aleph-One-Marathon/alephone/releases/download/release-#{version}/Marathon-#{version}-Mac.dmg",
       verified: "github.com/Aleph-One-Marathon/alephone/"
