8ac8bf67d4ed87206517242edbd318b69340145e
Mojo <blake+danger@classdojo.com>
2021-04-09 10:38:02 +0200

    Update sequel-ace from 3.3.0,3026 to 3.3.1,3028 (#103434)

--- Casks/sequel-ace.rb
+++ Casks/sequel-ace.rb
@@ -1,6 +1,6 @@
 cask "sequel-ace" do
-  version "3.3.0,3026"
-  sha256 "8988b38ed3b21cbdb34920326d7d88b4a1b205adbab05234da2ce73a9fa26cff"
+  version "3.3.1,3028"
+  sha256 "6e82c8714bfabca36d5d2a5cb51c1b2eaf59a9ae2f5469811de7742e68700780"
 
   url "https://github.com/Sequel-Ace/Sequel-Ace/releases/download/production/#{version.before_comma}-#{version.after_comma}/Sequel-Ace-#{version.before_comma}.zip"
   name "Sequel Ace"
