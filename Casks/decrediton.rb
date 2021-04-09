e2956302dff0bce55f9b0273427ca51345cb861c
Adithya Sunil <adithyasunil26@gmail.com>
2021-04-10 01:32:13 +0400

    Update decrediton from 1.6.1 to 1.6.2 (#103472)

--- Casks/decrediton.rb
+++ Casks/decrediton.rb
@@ -1,6 +1,6 @@
 cask "decrediton" do
-  version "1.6.1"
-  sha256 "fe1485949dcb066332a50ddb164d468a9cfbe10056cc483eb37841f52e77172b"
+  version "1.6.2"
+  sha256 "50ff42fd6b704c9e751f29b6c12007ddfdf291d3b078e9c8f75c695844a9d411"
 
   url "https://github.com/decred/decred-binaries/releases/download/v#{version}/decrediton-v#{version}.dmg"
   name "Decrediton"
