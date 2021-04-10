0ef098b8ea8956541c5e57b5aa9f01473a0678df
Markus Reiter <me@reitermark.us>
2021-04-10 19:48:35 +0200

    acronis-true-image: add livecheck

--- Casks/acronis-true-image.rb
+++ Casks/acronis-true-image.rb
@@ -3,10 +3,15 @@ cask "acronis-true-image" do
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://dl.acronis.com/u/AcronisTrueImage#{version}.dmg"
-  appcast "https://www.acronis.com/en-us/support/updates/index.html"
   name "Acronis True Image"
   desc "Full Image Backup & Cloning software"
   homepage "https://www.acronis.com/personal/computer-backup/"
+
+  livecheck do
+    url "https://www.acronis.com/en-us/support/updates/index.html"
+    strategy :page_match
+    regex(%r{Acronis\s*True\s*Image\s*(\d+(?:\.\d+)*)}i)
+  end
 
   app "Acronis True Image.app"
 end
