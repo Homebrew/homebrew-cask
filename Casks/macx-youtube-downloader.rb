f5c33ec3cc3c9e2887bc75305e0598108886915b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-04-10 03:25:41 +0100

    Update macx-youtube-downloader from 5.1.7,20201215 to 5.1.8 (#103523)

--- Casks/macx-youtube-downloader.rb
+++ Casks/macx-youtube-downloader.rb
@@ -1,11 +1,17 @@
 cask "macx-youtube-downloader" do
-  version "5.1.7,20201215"
+  version "5.1.8"
   sha256 :no_check
 
   url "https://www.macxdvd.com/download/macx-youtube-downloader-free.dmg"
   name "MacX YouTube Downloader"
   desc "Tool to download videos from YouTube"
   homepage "https://www.macxdvd.com/free-youtube-video-downloader-mac/"
+
+  livecheck do
+    url "https://download.cnet.com/MacX-YouTube-Downloader/3000-2071_4-76641301.html"
+    strategy :page_match
+    regex(/"softwareVersion"\s*:\s*"(\d+(?:\.\d+)*)"\s*,/i)
+  end
 
   app "MacX YouTube Downloader.app"
 end
