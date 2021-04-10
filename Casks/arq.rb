3d18839dc9cb67dfb6d6a46fcd5e51a1a3ed18a1
Markus Reiter <me@reitermark.us>
2021-04-10 23:10:51 +0200

    arq: add livecheck (#103636)

--- Casks/arq.rb
+++ Casks/arq.rb
@@ -3,10 +3,15 @@ cask "arq" do
   sha256 "91e47e9f546d43a185c8bdcf8e34b9b92ae8fc79730584fc9462714e42ecba4b"
 
   url "https://www.arqbackup.com/download/arqbackup/Arq#{version}.pkg"
-  appcast "https://www.arqbackup.com/download/arqbackup/arq#{version.major}_release_notes.html"
   name "Arq"
   desc "Multi-cloud backup application"
   homepage "https://www.arqbackup.com/"
+
+  livecheck do
+    url "https://www.arqbackup.com/download/arqbackup/arq#{version.major}_release_notes.html"
+    strategy :page_match
+    regex(/Version\s*(\d+(?:\.\d+)*)/i)
+  end
 
   auto_updates true
 
