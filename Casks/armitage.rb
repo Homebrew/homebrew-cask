60d76dde465c8d3ba1ee11d624cd634dfdc0bb70
Markus Reiter <me@reitermark.us>
2021-04-10 23:18:32 +0200

    armitage: add livecheck (#103635)
    * armitage: add livecheck
    
    * Update armitage.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/armitage.rb
+++ Casks/armitage.rb
@@ -3,9 +3,17 @@ cask "armitage" do
   sha256 "f44af478248fd01e71ea7e7bcfa558e4c9b291a78b0d1df1ad245c34cabd896b"
 
   url "http://www.fastandeasyhacking.com/download/armitage#{version.no_dots}.dmg"
-  appcast "http://www.fastandeasyhacking.com/changelog"
   name "Armitage"
+  desc "Scriptable collaboration tool for Metasploit"
   homepage "http://www.fastandeasyhacking.com/"
+
+  livecheck do
+    url "http://www.fastandeasyhacking.com/download/"
+    strategy :page_match do |page|
+      match = page.match(%r{href=.*?/armitage(\d\d+)(\d\d+)(\d\d+)\.dmg}i)
+      "#{match[1]}.#{match[2]}.#{match[3]}"
+    end
+  end
 
   app "Armitage.app"
   # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
