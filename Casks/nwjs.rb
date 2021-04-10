c1e0f0028ca357c423ef49885e261b3d8cbfd81a
Markus Reiter <me@reitermark.us>
2021-04-10 06:53:38 +0200

    Update nwjs from 0.52.1 to 0.52.2 (#103599)

--- Casks/nwjs.rb
+++ Casks/nwjs.rb
@@ -1,6 +1,6 @@
 cask "nwjs" do
-  version "0.52.1"
-  sha256 "7b85b405914f1ef6291122cec61cb90a76ea93d808559f83e05264369d54781d"
+  version "0.52.2"
+  sha256 "8a2bfe23c9ff6e83787ca7fc7a2ebd1d1c2942d76893ca7898f0aad67f3a436a"
 
   url "https://dl.nwjs.io/v#{version}/nwjs-sdk-v#{version}-osx-x64.zip"
   name "NW.js"
