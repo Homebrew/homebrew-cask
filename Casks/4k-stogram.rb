5d8948f283f19ec0781d9e414e25e958031436ff
Markus Reiter <me@reitermark.us>
2021-04-10 23:33:21 +0200

    4k-stogram: add livecheck (#103608)

--- Casks/4k-stogram.rb
+++ Casks/4k-stogram.rb
@@ -1,12 +1,17 @@
 cask "4k-stogram" do
-  version "3.3.3"
-  sha256 "e7b9067ea1169704fde887ae029cda45e764fa895601d6e935f1d3e0661d5b57"
+  version "3.3.4"
+  sha256 "d29099b5037778bdf002aeea2504336da5568044abfa466bccd91b2ce06e10df"
 
   url "https://dl.4kdownload.com/app/4kstogram_#{version}.dmg"
-  appcast "https://www.4kdownload.com/download"
   name "4K Stogram"
   desc "Download Instagram photos, accounts, hashtags and locations"
   homepage "https://www.4kdownload.com/products/product-stogram"
+
+  livecheck do
+    url "https://www.4kdownload.com/download"
+    strategy :page_match
+    regex(%r{href=.*?/4kstogram_(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   depends_on macos: ">= :high_sierra"
 
