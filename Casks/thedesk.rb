cb5dbddbb57c810b40336b206df824296fc2ef34
Markus Reiter <me@reitermark.us>
2021-04-10 05:34:23 +0200

    Update thedesk from 22.1.0 to 22.1.1 (#103554)

--- Casks/thedesk.rb
+++ Casks/thedesk.rb
@@ -1,6 +1,6 @@
 cask "thedesk" do
-  version "22.1.0"
-  sha256 "4a1884025f8b1c5b25ff635c53c505f8ff4b507910077552a5aafa721a15693f"
+  version "22.1.1"
+  sha256 "5ab424c86f84e47efa85248d0a2cea0c46837f02d07d4d660d663db216420145"
 
   url "https://github.com/cutls/TheDesk/releases/download/v#{version}/TheDesk-#{version}.dmg",
       verified: "github.com/cutls/TheDesk/"
