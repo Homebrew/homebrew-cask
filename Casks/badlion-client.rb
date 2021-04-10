3f9c18dd79ccc9af31b6437a4be1739ed6e2b4c2
Markus Reiter <me@reitermark.us>
2021-04-10 23:56:07 +0200

    badlion-client: add livecheck (#103649)
    * badlion-client: add livecheck
    
    * Update badlion-client.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/badlion-client.rb
+++ Casks/badlion-client.rb
@@ -1,11 +1,16 @@
 cask "badlion-client" do
-  version "3.0.1"
-  sha256 "51c9e1275b7831b891faf886d7936d49cb7aed9ae9ea2f83a2f220dc0847cd76"
+  version "3.1.0"
+  sha256 "00fee906c318e549ffce2129e76323453aec3e13d86b636716d3ff52a526643e"
 
   url "https://client-updates.badlion.net/Badlion%20Client-#{version}.dmg"
-  appcast "https://client-updates.badlion.net/latest-mac.yml"
   name "Badlion Client"
+  desc "Minecraft launcher"
   homepage "https://www.badlion.net/"
+
+  livecheck do
+    url "https://client-updates.badlion.net/latest-mac.yml"
+    strategy :electron_builder
+  end
 
   app "Badlion Client.app"
 
