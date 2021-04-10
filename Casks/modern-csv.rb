5c6b242311fc7160120e3523822e7fb4bb336da1
evanemm <72866915+evanemm@users.noreply.github.com>
2021-04-10 15:42:09 -0500

    Update modern-csv from 1.3.23 to 1.3.24 (#103643)

--- Casks/modern-csv.rb
+++ Casks/modern-csv.rb
@@ -1,6 +1,6 @@
 cask "modern-csv" do
-  version "1.3.23"
-  sha256 "bdd88d974d317daadc8fb129de8f059c5ae66da57628be87c0bfe636e507c058"
+  version "1.3.24"
+  sha256 "def8776d62c41bf231e89f7e9401633f66c10195209a6c13dd908997bc57b2f4"
 
   url "https://www.moderncsv.com/release/ModernCSV-Mac-v#{version}.dmg"
   appcast "https://www.moderncsv.com/latest-version/"
