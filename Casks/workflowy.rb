2a0179d531fc46ec5a5a4b37bc4715b0145b8258
Markus Reiter <me@reitermark.us>
2021-04-10 05:49:43 +0200

    Update workflowy from 1.3.5-16356 to 1.3.5-16569 (#103565)

--- Casks/workflowy.rb
+++ Casks/workflowy.rb
@@ -1,6 +1,6 @@
 cask "workflowy" do
-  version "1.3.5-16356"
-  sha256 "1d1c3048fbb1f6644cd05a9c472ac2d2719f6de70e7b268d34d3d11b3b60df85"
+  version "1.3.5-16569"
+  sha256 "1192f41c23710535751da2e9c1e1f80b4de82f44ea1ada7d019054bc9f3bd7f8"
 
   url "https://github.com/workflowy/desktop/releases/download/v#{version}/WorkFlowy.zip",
       verified: "github.com/workflowy/desktop/"
