e0096cbf8e5f1cea523770ec3fc51e9254f3b4ac
Markus Reiter <me@reitermark.us>
2021-04-10 05:32:23 +0200

    Update purei-play.rb

--- Casks/purei-play.rb
+++ Casks/purei-play.rb
@@ -2,7 +2,7 @@ cask "purei-play" do
   version "6ce95b5f"
   sha256 :no_check
 
-  url "https://s3.us-east-2.amazonaws.com/playbuilds/#{version)/Play.dmg",
+  url "https://s3.us-east-2.amazonaws.com/playbuilds/#{version}/Play.dmg",
       verified: "https://s3.us-east-2.amazonaws.com/playbuilds/"
   name "Play!"
   desc "PlayStation 2 emulator"
