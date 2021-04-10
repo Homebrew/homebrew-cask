d3772af0b6fea1c220f681af21a421d6dd57aed7
Markus Reiter <me@reitermark.us>
2021-04-10 06:35:57 +0200

    Update liya.rb

--- Casks/liya.rb
+++ Casks/liya.rb
@@ -7,6 +7,11 @@ cask "liya" do
   desc "Access three database systems simultaneously from a single visual interface"
   homepage "https://cutedgesystems.com/software/liya/"
 
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
+
   depends_on macos: ">= :catalina"
 
   app "Liya.app"
