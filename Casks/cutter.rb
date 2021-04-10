66539038d3ee148f34246025f2e2a349ae9eb252
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-11 05:06:42 +0800

    cutter 2.0.1 (#103645)

--- Casks/cutter.rb
+++ Casks/cutter.rb
@@ -1,9 +1,9 @@
 cask "cutter" do
-  version "2.0.0"
-  sha256 "d3b1359e6d0d19b423d760eb92e28242a60a07f9a46467d8ead58e905b89e466"
+  version "2.0.1"
+  sha256 "f797ab73079e5f261a7137c8ac78989d9f45326255bdde0eb2bfab7e544c8b58"
 
-  url "https://github.com/radareorg/cutter/releases/download/v#{version}/Cutter-v#{version}-x64.macOS.dmg",
-      verified: "github.com/radareorg/cutter/"
+  url "https://github.com/rizinorg/cutter/releases/download/v#{version}/Cutter-v#{version}-Darwin.dmg",
+      verified: "github.com/rizinorg/cutter/"
   name "Cutter"
   desc "Reverse engineering platform powered by radare2"
   homepage "https://cutter.re/"
