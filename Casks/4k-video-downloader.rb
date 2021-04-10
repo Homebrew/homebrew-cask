e1c648873311db7519094183d0819f64056078cf
Markus Reiter <me@reitermark.us>
2021-04-10 23:32:59 +0200

    4k-video-downloader: add livecheck (#103609)

--- Casks/4k-video-downloader.rb
+++ Casks/4k-video-downloader.rb
@@ -3,10 +3,15 @@ cask "4k-video-downloader" do
   sha256 "de4f4c56a54e3f5a3da3b7f680e2c3394112ceb4232cc75b7c1552acd2bd560c"
 
   url "https://dl.4kdownload.com/app/4kvideodownloader_#{version.major_minor_patch}.dmg"
-  appcast "https://www.4kdownload.com/download"
   name "4K Video Downloader"
   desc "Free video downloader"
   homepage "https://www.4kdownload.com/products/product-videodownloader"
+
+  livecheck do
+    url "https://www.4kdownload.com/download"
+    strategy :page_match
+    regex(%r{href=.*?/4kvideodownloader_(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   depends_on macos: ">= :sierra"
 
