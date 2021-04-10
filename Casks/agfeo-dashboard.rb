e3d1f83e13979392dd6562c6ac149b9911a2117f
Markus Reiter <me@reitermark.us>
2021-04-10 23:44:05 +0200

    agfeo-dashboard: add livecheck (#103623)
    * agfeo-dashboard: add livecheck
    
    * Update agfeo-dashboard.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/agfeo-dashboard.rb
+++ Casks/agfeo-dashboard.rb
@@ -1,11 +1,19 @@
 cask "agfeo-dashboard" do
-  version "1.2.0,A145F3A9D3FECE0BC12584F500300845"
-  sha256 "c00cef11a7f916b6877a3e5877dcf847d6f338beb1ab3d4d30ea0465f0464b40"
+  version "1.8.14,8A73B7762D0958D9C12586A30052AFE8"
+  sha256 "1bf4fe2b352573f344383e5b63113b62c296b7630863f1878ffb584c7171a1eb"
 
   url "https://www2.agfeo.de/agfeo_web/dokulib.nsf/Anlage_w/#{version.after_comma}/$FILE/AGFEO-Dashboard_x64_agfeo_#{version.before_comma}.dmg"
-  appcast "https://info.agfeo.de/agfeo_web/DokuLib.nsf/lu_04/5077_2"
   name "AGFEO Dashboard"
-  homepage "https://www.agfeo.de/"
+  desc "CTI/UC cockpit for your HyperVoice and ES communication system"
+  homepage "https://agfeo.de/produkt/agfeo-dashboard/"
+
+  livecheck do
+    url "https://info.agfeo.de/agfeo_web/hp3.nsf/softwareN_d?OpenForm&cat=AGFEODashboard"
+    strategy :page_match do |page|
+      match = page.match(%r{/([0-9a-f]+)/\$FILE/AGFEO-Dashboard_x64_agfeo_(\d+(?:\.\d+)*)\.dmg}i)
+      "#{match[2]},#{match[1]}"
+    end
+  end
 
   depends_on macos: ">= :sierra"
 
