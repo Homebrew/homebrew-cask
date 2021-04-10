088132ce575806d19c49256a14a1bb5257bdd1be
Markus Reiter <me@reitermark.us>
2021-04-10 23:44:24 +0200

    alchemy: add livecheck (#103625)
    * alchemy: add livecheck
    
    * Update alchemy.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/alchemy.rb
+++ Casks/alchemy.rb
@@ -3,9 +3,15 @@ cask "alchemy" do
   sha256 "7527f4e2231db8167c57d2639fcba166d8fefea091cca7884bd355fb52a3449a"
 
   url "https://al.chemy.org/files/Alchemy-#{version}.dmg"
-  appcast "https://al.chemy.org/download/version-history/"
   name "Alchemy"
+  desc "Open drawing project"
   homepage "https://al.chemy.org/"
+
+  livecheck do
+    url "http://al.chemy.org/download/"
+    strategy :page_match
+    regex(%r{href=.*?/Alchemy-(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "Alchemy/Alchemy.app"
 end
