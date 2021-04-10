76996e1eeddc7d8844bb538ca2ff5a21b1ed3adc
Markus Reiter <me@reitermark.us>
2021-04-10 23:13:42 +0200

    appzapper: add livecheck (#103633)

--- Casks/appzapper.rb
+++ Casks/appzapper.rb
@@ -3,11 +3,17 @@ cask "appzapper" do
   sha256 "bb541a89fd513c4fa95eeefe46ebac6b985ac6498a46da4e4622089a15ef6bcd"
 
   url "https://appzapper.com/downloads/appzapper#{version.no_dots}.zip"
-  appcast "https://www.appzapper.com/",
-          must_contain: version.no_dots
   name "AppZapper"
   desc "Tool to uninstall unwanted applications and their support files"
   homepage "https://www.appzapper.com/"
+
+  livecheck do
+    url :homepage
+    strategy :page_match do |page|
+      match = page.match(%r{href=.*?/appzapper(\d+)(\d+)(\d+)\.zip}i)
+      "#{match[1]}.#{match[2]}.#{match[3]}"
+    end
+  end
 
   auto_updates true
   depends_on macos: ">= :sierra"
