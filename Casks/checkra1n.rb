df9b13946b7f30c6ed21809ac043c14585800e92
Markus Reiter <me@reitermark.us>
2021-04-11 02:07:34 +0200

    checkra1n: add livecheck (#103656)

--- Casks/checkra1n.rb
+++ Casks/checkra1n.rb
@@ -3,10 +3,15 @@ cask "checkra1n" do
   sha256 "3c44df37558316ccf7e568ec90511f116496f7f6c8c9f223b971b2631b9f7fe5"
 
   url "https://checkra.in/assets/downloads/macos/#{sha256}/checkra1n%20beta%20#{version}.dmg"
-  appcast "https://checkra.in/"
   name "checkra1n"
   desc "Jailbreak for iPhone 5s through iPhone X, iOS 12.0 and up"
   homepage "https://checkra.in/"
+
+  livecheck do
+    url "https://checkra.in/"
+    strategy :page_match
+    regex(%r{href=.*?/checkra1n%20beta%20(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "checkra1n.app"
 end
