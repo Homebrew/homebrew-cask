e1978c6cf93afd0f53c0dfb37eb13f494b91187a
Reno <25192197+singularitti@users.noreply.github.com>
2021-04-11 12:35:48 -0400

    Update latexit from 2.16.1 to 2.16.2 (#103671)

--- Casks/latexit.rb
+++ Casks/latexit.rb
@@ -1,6 +1,6 @@
 cask "latexit" do
-  version "2.16.1"
-  sha256 "b2d44763faf5beac59ea141f0de3c906141055bd61620c73c3b0abef5a1914fb"
+  version "2.16.2"
+  sha256 "59b70471114e32b7d3ca5a206bd1d075eb8c0778a6e380e7401e616b947ca08d"
 
   url "https://pierre.chachatelier.fr/latexit/downloads/LaTeXiT-#{version.dots_to_underscores}.dmg",
       user_agent: :fake
