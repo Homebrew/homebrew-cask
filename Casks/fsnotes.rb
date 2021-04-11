d0c6961013b427fb892b5ee30455fd0c7e5b3917
Balu <tw@b-a-l-u.de>
2021-04-11 12:35:47 +0200

    Update fsnotes from 4.9.10 to 4.9.11 (#103666)

--- Casks/fsnotes.rb
+++ Casks/fsnotes.rb
@@ -1,6 +1,6 @@
 cask "fsnotes" do
-  version "4.9.10"
-  sha256 "b92824e627134b5305b2a41d5b8878691eac995f37d34c991d9fb1304e7896b3"
+  version "4.9.11"
+  sha256 "95ea1332759ba9bde5cf2ed0d92c14a43b3b7dc7fefb11d2d46acff71179114d"
 
   url "https://github.com/glushchenko/fsnotes/releases/download/#{version}/FSNotes_#{version}.zip",
       verified: "github.com/glushchenko/fsnotes/"
