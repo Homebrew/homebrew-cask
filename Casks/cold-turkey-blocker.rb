abc9d9ac240137ca165f2ccc52abd281052cce98
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-11 08:18:00 +0800

    Update cold-turkey-blocker.rb

--- Casks/cold-turkey-blocker.rb
+++ Casks/cold-turkey-blocker.rb
@@ -1,12 +1,19 @@
 cask "cold-turkey-blocker" do
   version "4.2.1"
-  sha256 :no_check
+  sha256 "745408cc07c761f8ecc58243adb4fad28be9a1a43e5922403432ab2e82e644ce"
 
   url "https://getcoldturkey.com/files/Cold_Turkey_Mac_Installer.pkg?v=#{version}"
   name "Cold Turkey"
+  desc "Block websites, games and applications"
   homepage "https://getcoldturkey.com/"
 
-  pkg "Cold_Turkey_Mac_Installer.pkg"
+  livecheck do
+    url "https://getcoldturkey.com/download/mac/"
+    strategy :page_match
+    regex(/Cold_Turkey_Mac_Installer\.pkg\?v=(\d+(?:\.\d+)*)/i)
+  end
+
+  pkg "Cold_Turkey_Blocker.pkg"
 
   uninstall launchctl: [
     "launchkeep.cold-turkey",
