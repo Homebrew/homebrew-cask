6cca1e01185880ec3180906733f1de1cd429569f
Yusuf Kürşad Kaya <ykursadkaya@gmail.com>
2021-04-12 00:03:36 +0300

    Update tunnelblick from 3.8.4b,5602 to 3.8.5,5670 (#103670)

--- Casks/tunnelblick.rb
+++ Casks/tunnelblick.rb
@@ -1,6 +1,6 @@
 cask "tunnelblick" do
-  version "3.8.4b,5602"
-  sha256 "37df1ed88e32dc7523ee1e7aadfc5054ebe65fee3f669cbeac3ab85888df7af5"
+  version "3.8.5,5670"
+  sha256 "1c7f4f441b472bfa841686dd981802caafa658b9a284e2b4c62d6918cd115bef"
 
   url "https://github.com/Tunnelblick/Tunnelblick/releases/download/v#{version.before_comma}/Tunnelblick_#{version.before_comma}_build_#{version.after_comma}.dmg",
       verified: "github.com/Tunnelblick/Tunnelblick/"
