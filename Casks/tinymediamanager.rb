d631cf7a6b410df65453ef575442051146ba8812
Markus Reiter <me@reitermark.us>
2021-04-11 02:19:05 +0200

    tinymediamanager: add livecheck (#103660)
    * tinymediamanager: add livecheck
    
    * Update tinymediamanager.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/tinymediamanager.rb
+++ Casks/tinymediamanager.rb
@@ -3,9 +3,15 @@ cask "tinymediamanager" do
   sha256 "f39526beac22ade508c5748dc050636215ae14c721d70e9052fa3c8279ebf2ae"
 
   url "https://release.tinymediamanager.org/v#{version.major}/dist/tmm_#{version}_mac.zip"
-  appcast "https://release.tinymediamanager.org/"
   name "tinyMediaManager"
+  desc "Media management tool"
   homepage "https://www.tinymediamanager.org/"
+
+  livecheck do
+    url "https://release.tinymediamanager.org/"
+    strategy :page_match
+    regex(%r{href=.*?/tmm_(\d+(?:\.\d+)*)_mac\.zip}i)
+  end
 
   auto_updates true
 
