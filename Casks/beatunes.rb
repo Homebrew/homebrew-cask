c7025e5626489036ecb680692a129cbcb971b886
Markus Reiter <me@reitermark.us>
2021-04-11 02:19:37 +0200

    beatunes: add livecheck (#103654)
    * beatunes: add livecheck
    
    * Update beatunes.rb
    
    * Update beatunes.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/beatunes.rb
+++ Casks/beatunes.rb
@@ -1,12 +1,19 @@
 cask "beatunes" do
-  version "5.2.17"
-  sha256 "2481f90e630f716b3e943984d636a0086a5fe5b9803b2feaa10361aa295aaa4d"
+  version "5.2.20"
+  sha256 "afddd19389c98230ce839a00cf8e0567a3890e701021013737b4c63ebf139571"
 
   url "http://coxy.beatunes.com/download/beaTunes-#{version.dots_to_hyphens}.dmg"
-  appcast "https://www.beatunes.com/en/beatunes-download.html",
-          must_contain: version.dots_to_hyphens
   name "beaTunes"
+  desc "Analyze, inspect, and play songs"
   homepage "https://www.beatunes.com/"
+
+  livecheck do
+    url "https://www.beatunes.com/en/beatunes-download.html"
+    strategy :page_match do |page|
+      v = page[/href=.*?beaTunes-(\d+(?:-\d+)*)\.dmg/i, 1]
+      v.tr("-", ".")
+    end
+  end
 
   depends_on macos: ">= :sierra"
 
