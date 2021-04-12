60c6f86890ac5ab18c2a519d371029a74b3035e9
Josh McKinney <joshka@users.noreply.github.com>
2021-04-11 22:56:01 -0700

    Bump bitwig studio 3.3.7 (#103665)
    * Update bitwig-studio from 3.3.3 to 3.3.7
    
    * Add bitwig-studio desc
    
    * Update bitwig-studio.rb
    
    * Update bitwig-studio.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/bitwig-studio.rb
+++ Casks/bitwig-studio.rb
@@ -3,9 +3,15 @@ cask "bitwig-studio" do
   sha256 "ceb104ce25652ac30458418d378320d68c0a174cccc861e66686c20fe04d393d"
 
   url "https://downloads.bitwig.com/stable/#{version}/Bitwig%20Studio%20#{version}.dmg"
-  appcast "https://www.bitwig.com/en/download.html"
   name "Bitwig Studio"
+  desc "Digital audio workstation"
   homepage "https://www.bitwig.com/"
+
+  livecheck do
+    url "https://www.bitwig.com/download/"
+    strategy :page_match
+    regex(/Bitwig\s*Studio\s*(\d+(?:\.\d+)*)/i)
+  end
 
   app "Bitwig Studio.app"
 
