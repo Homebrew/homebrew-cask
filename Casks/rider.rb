f08a68e8d6fb5037125dc5b36a75fc692c1d0550
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-12 13:48:29 +0800

    Update rider.rb

--- Casks/rider.rb
+++ Casks/rider.rb
@@ -3,10 +3,15 @@ cask "rider" do
   sha256 "5f040b1bc9f7660e54903eaaa725e14bd51db5444792d8f39c5655461befb141"
 
   url "https://download.jetbrains.com/rider/JetBrains.Rider-#{version.before_comma}.dmg"
-  appcast "https://data.services.jetbrains.com/products/releases?code=RD&latest=true&type=release"
   name "Jetbrains Rider"
-  desc "Cross-Platform .NET IDE"
+  desc "Cross-platform .NET IDE"
   homepage "https://www.jetbrains.com/rider/"
+
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=RD&latest=true&type=release"
+    strategy :page_match
+    regex(/JetBrains\.Rider-(\d+(?:\.\d+)*)\.dmg/i)
+  end
 
   auto_updates true
 
