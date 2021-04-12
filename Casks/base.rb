88d5869c8b7986dd16a0fbfac8b846da98b5a8ef
Markus Reiter <me@reitermark.us>
2021-04-12 07:41:05 +0200

    base: add livecheck (#103650)
    * base: add livecheck
    
    * Update base.rb
    
    * Update base.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/base.rb
+++ Casks/base.rb
@@ -1,12 +1,16 @@
 cask "base" do
-  version "2.5.2"
+  version "2.5.2,20502"
   sha256 "84848093681459b039572a15a82e1cc60e4366ccf5fb69a4de5c90d240871d8c"
 
-  url "https://files.menial.co.uk/base/base_#{version}.zip"
-  appcast "https://update.menial.co.uk/software/base/"
+  url "https://files.menial.co.uk/base/base_#{version.before_comma}.zip"
   name "Menial Base"
   desc "App to create, design, edit and browse SQLite 3 database files"
   homepage "https://menial.co.uk/base/"
+
+  livecheck do
+    url "https://update.menial.co.uk/software/base/"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :mojave"
 
