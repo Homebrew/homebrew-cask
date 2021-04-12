57b7428591391ab5ee9ee9db8058f1b4cdd31096
Markus Reiter <me@reitermark.us>
2021-04-12 16:28:24 +0200

    Update electerm from 1.12.9 to 1.12.15 (#103718)

--- Casks/electerm.rb
+++ Casks/electerm.rb
@@ -1,6 +1,6 @@
 cask "electerm" do
-  version "1.12.9"
-  sha256 "4fed71664f73644ec9c90f4f3532f68626ac83cf7eddb757f4dae55de2bee00a"
+  version "1.12.15"
+  sha256 "faef6bd1cc4f87e5d8107f4ecc87b9a7e825293e796d6e6833f5778b55f9dd4f"
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac.dmg"
   name "electerm"
