7ca09d18bdbba89400cbb4046d066d50dc48add9
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-12 13:46:13 +0800

    Update bitwig-studio.rb

--- Casks/bitwig-studio.rb
+++ Casks/bitwig-studio.rb
@@ -4,7 +4,7 @@ cask "bitwig-studio" do
 
   url "https://downloads.bitwig.com/stable/#{version}/Bitwig%20Studio%20#{version}.dmg"
   name "Bitwig Studio"
-  desc "Digital Audio Workstation"
+  desc "Digital audio workstation"
   homepage "https://www.bitwig.com/"
 
   livecheck do
