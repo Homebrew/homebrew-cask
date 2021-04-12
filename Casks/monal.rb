b5edc398de7611978c5358bcf03e44a988c717b0
janst97 <81253036+janst97@users.noreply.github.com>
2021-04-12 15:14:08 +0200

    Update Monal cask url (#103690)

--- Casks/monal.rb
+++ Casks/monal.rb
@@ -1,14 +1,15 @@
 cask "monal" do
-  version "5.0,712"
+  version "5.0,716"
   sha256 :no_check
 
-  url "https://monal.im/Monal-OSX/Monal-OSX.zip"
+  url "https://monal.im/macOS/Monal-macOS.tar.gz"
   name "Monal"
   desc "Tool to securely connect to chat servers"
   homepage "https://monal.im/"
 
   livecheck do
-    skip "unversioned URL"
+    url :url
+    strategy :extract_plist
   end
 
   depends_on macos: ">= :catalina"
