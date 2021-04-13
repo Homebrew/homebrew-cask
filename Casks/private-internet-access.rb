0a0507e15f18f63532e476b090a90afb7ee9d13b
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-13 16:33:09 +0800

    Update private-internet-access.rb

--- Casks/private-internet-access.rb
+++ Casks/private-internet-access.rb
@@ -9,7 +9,7 @@ cask "private-internet-access" do
 
   livecheck do
     url "https://www.privateinternetaccess.com/installer/x/download_installer_osx"
-    regex(/url=.*?pia-macos-(\d\.\d\.\d[._-]\d+)\.zip/i)
+    regex(/pia-macos-(\d+(?:.\d+)*)\.zip/i)
   end
 
   auto_updates true
