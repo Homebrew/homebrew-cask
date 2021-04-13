a50df56e6986fb2dd1cdd7e1dfc0bda4f50d3557
Markus Reiter <me@reitermark.us>
2021-04-13 03:39:12 +0200

    cinch: add livecheck

--- Casks/cinch.rb
+++ Casks/cinch.rb
@@ -1,12 +1,16 @@
 cask "cinch" do
-  version "1.2.4"
+  version "1.2.4,146"
   sha256 "68afff19aaff4885e803733df74cfc87fe6529ec57107d21bddcb00d61dd14a0"
 
-  url "https://www.irradiatedsoftware.com/downloads/Cinch_#{version}.zip"
-  appcast "https://www.irradiatedsoftware.com/updates/profiles/cinch.php"
+  url "https://www.irradiatedsoftware.com/downloads/Cinch_#{version.before_comma}.zip"
   name "Cinch"
   desc "Window management tool"
   homepage "https://www.irradiatedsoftware.com/cinch/"
+
+  livecheck do
+    url "https://www.irradiatedsoftware.com/updates/profiles/cinch.php"
+    strategy :sparkle
+  end
 
   app "Cinch.app"
 end
