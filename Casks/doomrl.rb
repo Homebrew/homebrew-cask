d12a95b174d2a5d853a6322a520d5d416110dcc7
Markus Reiter <me@reitermark.us>
2021-04-13 05:24:07 +0200

    doomrl: add livecheck (#103783)
    * doomrl: add livecheck
    
    * Update doomrl.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/doomrl.rb
+++ Casks/doomrl.rb
@@ -3,9 +3,15 @@ cask "doomrl" do
   sha256 "fe77aca0af5dd774d1a21dc4f2d0618636c1fe47aa4593bcfe29f0445209b4e2"
 
   url "https://doom.chaosforge.org/file_download/31/doomrl-#{version.no_dots}.dmg"
-  appcast "https://drl.chaosforge.org/downloads"
   name "Doom the Roguelike"
+  desc "Roguelike game inspired by Doom"
   homepage "https://doom.chaosforge.org/"
+
+  livecheck do
+    url "https://drl.chaosforge.org/downloads"
+    strategy :page_match
+    regex(/DRL\s*(\d+(?:\.\d+)*)/i)
+  end
 
   app "DoomRL.app"
 end
