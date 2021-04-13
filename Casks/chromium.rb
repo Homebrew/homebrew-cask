4a48654f2c9ffa8a78646346eaa675d3d8efa0cf
Markus Reiter <me@reitermark.us>
2021-04-13 05:29:39 +0200

    Update chromium.rb

--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -1,6 +1,6 @@
 cask "chromium" do
   version "871732"
-  sha256 "e67585cb4021458b6d08130d4755579ee9d8a610fa9201229b0737db60494d6d"
+  sha256 "f91d69ccdec3ee5fc428912d5a5ef553caa2e127c48a73c8e3e95485e06456da"
 
   url "https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/#{version}/chrome-mac.zip",
       verified: "commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/"
