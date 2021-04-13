77fc28bbc8c7f084568149dc8bf5bdc861a4eebc
Markus Reiter <me@reitermark.us>
2021-04-13 06:24:45 +0200

    figma: add livecheck (#103803)
    * figma: add livecheck
    
    * Update figma.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/figma.rb
+++ Casks/figma.rb
@@ -3,9 +3,15 @@ cask "figma" do
   sha256 "eb15e232b76de6e882fa4dfaf7db25c448319c41ef5692ef0fd5bd53e564c7d7"
 
   url "https://desktop.figma.com/mac/Figma-#{version}.zip"
-  appcast "https://desktop.figma.com/mac/RELEASE.json"
   name "Figma"
+  desc "Collaborative team software"
   homepage "https://www.figma.com/"
+
+  livecheck do
+    url "https://desktop.figma.com/mac/RELEASE.json"
+    strategy :page_match
+    regex(%r{/Figma-(\d+(?:\.\d+)*)\.zip}i)
+  end
 
   auto_updates true
 
