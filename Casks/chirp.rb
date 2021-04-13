f4629eff088f060cec880d08024b244a44ea05ee
Markus Reiter <me@reitermark.us>
2021-04-13 05:29:22 +0200

    chirp: add livecheck (#103761)

--- Casks/chirp.rb
+++ Casks/chirp.rb
@@ -3,10 +3,15 @@ cask "chirp" do
   sha256 "4fa18cbce9c7b5c469d73c255179140c132b911ab24e3d97657b17ca6db78934"
 
   url "https://trac.chirp.danplanet.com/chirp_daily/LATEST/chirp-unified-daily-#{version}.app.zip"
-  appcast "https://trac.chirp.danplanet.com/chirp_daily/LATEST/SHA1SUM"
   name "CHIRP"
   desc "Tool for programming amateur radio"
   homepage "https://chirp.danplanet.com/projects/chirp/wiki/Home"
+
+  livecheck do
+    url "https://trac.chirp.danplanet.com/chirp_daily/LATEST/SHA1SUM"
+    strategy :page_match
+    regex(/chirp-unified-daily-(\d+(?:\.\d+)*).app\.zip/i)
+  end
 
   app "CHIRP.app"
 end
