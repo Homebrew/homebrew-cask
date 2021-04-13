b946a68714ba6c839f9a1fd03d40388141ddaf36
Markus Reiter <me@reitermark.us>
2021-04-13 06:19:10 +0200

    fantastical: add livecheck (#103799)
    * fantastical: add livecheck
    
    * Update fantastical.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/fantastical.rb
+++ Casks/fantastical.rb
@@ -1,11 +1,16 @@
 cask "fantastical" do
-  version "3.3.7"
+  version "3.3.7,1272"
   sha256 "d139d12f9ee16174f4da44e7cee61bf1a798962d8d348ba8b16e9896a8c706b9"
 
-  url "https://cdn.flexibits.com/Fantastical_#{version}.zip"
-  appcast "https://flexibits.com/fantastical/appcast2.php"
+  url "https://cdn.flexibits.com/Fantastical_#{version.before_comma}.zip"
   name "Fantastical"
+  desc "Calendar software"
   homepage "https://flexibits.com/fantastical"
+
+  livecheck do
+    url "https://flexibits.com/fantastical/appcast2.php"
+    strategy :sparkle
+  end
 
   auto_updates true
 
