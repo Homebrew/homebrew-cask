4097046d457630c688734b32fa7005645476126e
Markus Reiter <me@reitermark.us>
2021-04-13 05:25:07 +0200

    deepgit: add livecheck (#103775)
    * deepgit: add livecheck
    
    * Update deepgit.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/deepgit.rb
+++ Casks/deepgit.rb
@@ -3,9 +3,17 @@ cask "deepgit" do
   sha256 "2190658fba01094093ca1e2b51fd0004821dc1a2f27a992b12b53890a37aac4e"
 
   url "https://www.syntevo.com/downloads/deepgit/deepgit-macosx-#{version.dots_to_underscores}.dmg"
-  appcast "https://syntevo.com/deepgit/download"
   name "DeepGit"
+  desc "Tool to investigate the history of source code"
   homepage "https://www.syntevo.com/deepgit/"
+
+  livecheck do
+    url "https://syntevo.com/deepgit/download"
+    strategy :page_match do |page|
+      v = page[%r{href=.*?/deepgit-macosx-(\d+(?:_\d+)*)\.dmg}i, 1]
+      v.tr("_", ".")
+    end
+  end
 
   app "DeepGit.app"
 end
