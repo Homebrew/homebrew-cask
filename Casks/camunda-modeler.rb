c8d74e901ff2da7284716c4c68e5409415810bb2
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-04-14 01:05:48 +0900

    camunda-modeler: update to 4.7.0 (#103812)

--- Casks/camunda-modeler.rb
+++ Casks/camunda-modeler.rb
@@ -1,9 +1,10 @@
 cask "camunda-modeler" do
-  version "4.6.0"
-  sha256 "2d3b709a86bbc7c030594307d977a851950d4495e1802924e393bcbaf24860de"
+  version "4.7.0"
+  sha256 "f5ada5c02117300260068b81a07e84f76c391cdc3e28c468c543657cccc739bc"
 
   url "https://camunda.org/release/camunda-modeler/#{version}/camunda-modeler-#{version}-mac.zip"
   name "Camunda Modeler"
+  desc "Workflow and Decision Automation Platform"
   homepage "https://camunda.org/"
 
   livecheck do
