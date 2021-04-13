2509ec0154f6212b4718a52fefe2355f7e159a20
Markus Reiter <me@reitermark.us>
2021-04-13 21:39:29 +0200

    filemaker-pro: add livecheck

--- Casks/filemaker-pro.rb
+++ Casks/filemaker-pro.rb
@@ -1,12 +1,17 @@
 cask "filemaker-pro" do
-  version "19.2.1.14"
-  sha256 "7e57f30e4f1a72063824240eff78b795de148a5233bfbfc6aaf10c101a269496"
+  version "19.2.2.234"
+  sha256 "723fccae725cfc54f50545eca68fde183492d2aaefd891fa38b8557dd26e7fa3"
 
   url "https://downloads.claris.com/esd/fmp_#{version}.dmg"
-  appcast "https://www.filemaker.com/redirects/ss.txt"
   name "FileMaker Pro"
   desc "Relational database and rapid application development platform"
   homepage "https://www.claris.com/filemaker/"
+
+  livecheck do
+    url "https://www.filemaker.com/redirects/ss.txt"
+    strategy :page_match
+    regex(%r{/fmp_(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   auto_updates true
   depends_on macos: ">= :mojave"
