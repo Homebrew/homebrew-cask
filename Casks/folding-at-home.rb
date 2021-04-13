624d62c719860b3dcd028a06cd72477a4cb7b2d0
Markus Reiter <me@reitermark.us>
2021-04-13 22:44:11 +0200

    folding-at-home: add livecheck

--- Casks/folding-at-home.rb
+++ Casks/folding-at-home.rb
@@ -3,10 +3,15 @@ cask "folding-at-home" do
   sha256 "c6a559f46e25aa7a9e67227affbccfef77527f663da80be6c55a4f7e7e29866f"
 
   url "https://download.foldingathome.org/releases/public/release/fah-installer/osx-10.11-64bit/v#{version.major_minor}/fah-installer_#{version}_x86_64.mpkg.zip"
-  appcast "https://download.foldingathome.org/releases/public/release/fah-installer/osx-10.11-64bit/v#{version.major_minor}/"
   name "Folding@home"
   desc "Graphical interface control for Folding"
   homepage "https://foldingathome.org/"
+
+  livecheck do
+    url "https://download.foldingathome.org/releases/public/release/fah-installer/osx-10.11-64bit/v#{version.major_minor}/"
+    strategy :page_match
+    regex(/href=.*?fah-installer_(\d+(?:\.\d+)*)_x86_64.mpkg\.zip/i)
+  end
 
   pkg "fah-installer_#{version}_x86_64-b.pkg"
 
