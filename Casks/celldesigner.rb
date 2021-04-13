67d6689e86a32b27da57886ba5a9acce8a9d1d0d
Markus Reiter <me@reitermark.us>
2021-04-13 05:28:32 +0200

    celldesigner: add livecheck (#103757)
    * celldesigner: add livecheck
    
    * Update celldesigner.rb
    
    * Update celldesigner.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/celldesigner.rb
+++ Casks/celldesigner.rb
@@ -3,9 +3,15 @@ cask "celldesigner" do
   sha256 "992a8350756ab8df7a02fab3b0fd76339b0ec6a286db5af4f284664c79a0f2c4"
 
   url "http://www.systems-biology.org/download/CellDesigner-#{version}-osx-installer.dmg"
-  appcast "http://systems-biology.org/software/celldesigner/"
   name "CellDesigner"
+  desc "Modeling tool of biochemical networks"
   homepage "http://systems-biology.org/software/celldesigner/"
+
+  livecheck do
+    url "http://systems-biology.org/software/celldesigner/"
+    strategy :page_match
+    regex(/CellDesigner\s*(\d+(?:\.\d+)*)/i)
+  end
 
   installer script: {
     executable: "#{staged_path}/CellDesigner-#{version}-osx-installer.app/Contents/MacOS/installbuilder.sh",
