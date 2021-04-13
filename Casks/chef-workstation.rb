6066416818fad677cb38edd20ecd5f9968b1dc41
Markus Reiter <me@reitermark.us>
2021-04-13 05:29:05 +0200

    chef-workstation: add livecheck (#103760)
    * chef-workstation: add livecheck
    
    * Update chef-workstation.rb

--- Casks/chef-workstation.rb
+++ Casks/chef-workstation.rb
@@ -3,10 +3,15 @@ cask "chef-workstation" do
   sha256 "ec495a0f75927a9600e708e9633d305158abf2a9af345f276898cad85126060b"
 
   url "https://packages.chef.io/files/stable/chef-workstation/#{version}/mac_os_x/10.15/chef-workstation-#{version}-1.x86_64.dmg"
-  appcast "https://omnitruck.chef.io/stable/chef-workstation/metadata?p=mac_os_x&pv=10.15&m=x86_64&v=latest"
   name "Chef Workstation"
   desc "All-in-one installer for the tools you need to manage your Chef infrastructure"
   homepage "https://docs.chef.io/workstation/"
+
+  livecheck do
+    url "https://omnitruck.chef.io/stable/chef-workstation/metadata?p=mac_os_x&pv=10.15&m=x86_64&v=latest"
+    strategy :page_match
+    regex(/version\s*(\d+(?:\.\d+)*)/i)
+  end
 
   depends_on macos: ">= :high_sierra"
 
