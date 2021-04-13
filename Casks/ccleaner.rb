be7d98b869a7ce7bd65b6514310bbaef2049eb5f
Markus Reiter <me@reitermark.us>
2021-04-13 03:12:12 +0200

    ccleaner: add livecheck

--- Casks/ccleaner.rb
+++ Casks/ccleaner.rb
@@ -1,11 +1,18 @@
 cask "ccleaner" do
-  version "1.18.30"
+  version "1.18"
   sha256 "52295172b846c477f1c5396c9f568d602b89fad52d2fcaba394a14296e096ce6"
 
   url "https://download.ccleaner.com/mac/CCMacSetup#{version.major_minor.no_dots}.dmg"
-  appcast "https://www.ccleaner.com/ccleaner/download?mac"
   name "Piriform CCleaner"
   homepage "https://www.ccleaner.com/ccleaner-mac"
+
+  livecheck do
+    url "https://www.ccleaner.com/ccleaner/download?mac"
+    strategy :page_match do |page|
+      match = page.match(%r{href=.*?/CCMacSetup(\d+?)(\d+)\.dmg}i)
+      "#{match[1]}.#{match[2]}"
+    end
+  end
 
   app "CCleaner.app"
 
