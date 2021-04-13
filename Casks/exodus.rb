9833fee96b4389c4347221ac9a4ca581a4b27f87
Markus Reiter <me@reitermark.us>
2021-04-13 06:07:39 +0200

    exodus: add livecheck (#103796)

--- Casks/exodus.rb
+++ Casks/exodus.rb
@@ -1,12 +1,17 @@
 cask "exodus" do
-  version "21.4.9"
-  sha256 "e9639535e380a81be08c67db55705f7f2ff8214ba6a26ff8517e0c732fae8a05"
+  version "21.4.12"
+  sha256 "3e7db321a726cea5e4db61ed1b3687295e96fc5906138f9a74f97bea6d86d08b"
 
   url "https://downloads.exodus.com/releases/exodus-macos-#{version}.dmg"
-  appcast "https://www.exodus.com/releases/"
   name "Exodus"
   desc "Desktop wallet for cryptocurrency assets"
   homepage "https://www.exodus.com/"
+
+  livecheck do
+    url "https://www.exodus.com/releases/"
+    strategy :page_match
+    regex(/new\s*in.*?(\d+(?:\.\d+)*)/i)
+  end
 
   auto_updates true
 
