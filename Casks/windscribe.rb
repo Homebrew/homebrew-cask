74652f203c148a39bb9cdbd1db407879478569af
Jivan Pal <jivan.pal@gmail.com>
2021-04-13 02:47:22 +0100

    windscribe: Update script to handle Windscribe v2

--- Casks/windscribe.rb
+++ Casks/windscribe.rb
@@ -1,5 +1,5 @@
 cask "windscribe" do
-  version "1.83.1.25"
+  version :latest
   sha256 :no_check
 
   url "https://assets.windscribe.com/desktop/mac/Windscribe.dmg"
@@ -7,11 +7,29 @@ cask "windscribe" do
   desc "VPN client for secure internet access and private browsing"
   homepage "https://windscribe.com/"
 
-  app "Windscribe.app"
+  installer manual: "WindscribeInstaller.app"
+
+  uninstall launchctl: [
+              "com.windscribe.helper.macos",
+              "com.windscribe.launcher.macos",
+            ],
+            quit:      [
+              "com.windscribe.gui.macos",
+              "com.windscribe.helper.macos",
+              "com.windscribe.launcher.macos",
+            ],
+            delete:    [
+              "/Applications/Windscribe.app",
+              "/usr/local/bin/windscribe-cli",
+              "/Library/PrivilegedHelperTools/com.windscribe.helper.macos",
+              "/Library/LaunchDaemons/com.windscribe.helper.macos.plist",
+              "/private/var/run/windscribe_helper_socket2",
+            ]
 
   zap trash: [
     "~/Library/Application Support/Windscribe",
-    "~/Library/Saved Application State/com.aaa.windscribe.windscribe.savedState",
-    "~/Library/Preferences/com.windscribe.Windscribe.plist",
+    "~/Library/Saved Application State/com.windscribe.gui.macos.savedState",
+    "~/Library/Preferences/com.windscribe.Windscribe2.plist",
+    "~/Library/Preferences/com.aaa.windscribe.windscribe.plist",
   ]
 end
