7734d9b32acbd5c31d72848b09c218a2db2bb747
Markus Reiter <me@reitermark.us>
2021-04-13 05:31:56 +0200

    comparemerge: add livecheck (#103767)

--- Casks/comparemerge.rb
+++ Casks/comparemerge.rb
@@ -3,10 +3,17 @@ cask "comparemerge" do
   sha256 "fc40bd6c8aefa75985df542169067048ca85b0fb2ae7feec5edf2e98399f51ea"
 
   url "https://downloads.sourceforge.net/comparemergenosandbox/CompareMerge#{version.before_comma}#{version.after_comma}.zip"
-  appcast "https://sourceforge.net/projects/comparemergenosandbox/rss"
   name "CompareMerge"
   desc "Tool for file comparison and merging text-like files"
   homepage "https://sourceforge.net/projects/comparemergenosandbox/"
+
+  livecheck do
+    url "https://sourceforge.net/projects/comparemergenosandbox/rss"
+    strategy :page_match do |page|
+      match = page.match(%r{/CompareMerge(\d+(?:\.\d+)*\w)(\d+)\.zip}i)
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   app "CompareMerge#{version.before_comma}#{version.after_comma}/CompareMerge#{version.before_comma}#{version.after_comma}.app"
 end
