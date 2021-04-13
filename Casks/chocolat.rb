f1bceaf5f6ea6fb2b2daa8cc49fc2afa2fdc848d
Markus Reiter <me@reitermark.us>
2021-04-13 03:36:41 +0200

    chocolat: add livecheck

--- Casks/chocolat.rb
+++ Casks/chocolat.rb
@@ -3,9 +3,13 @@ cask "chocolat" do
   sha256 "43f1f96350c10aa2918719a2238c1e4ec20dd2cfa7a4930f6e7d74f92207c4fe"
 
   url "https://chocolatapp.com/versions/Chocolat_#{version}.zip"
-  appcast "https://chocolatapp.com/userspace/appcast/appcast_alpha.php"
   name "Chocolat"
   homepage "https://chocolatapp.com/"
+
+  livecheck do
+    url "https://chocolatapp.com/userspace/appcast/appcast_alpha.php"
+    strategy :sparkle
+  end
 
   app "Chocolat.app"
 end
