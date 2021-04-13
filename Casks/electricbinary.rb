53e55161e3361ca61354cbe9fa38b77049a17cc5
Markus Reiter <me@reitermark.us>
2021-04-13 05:34:52 +0200

    electricbinary: add livecheck (#103791)

--- Casks/electricbinary.rb
+++ Casks/electricbinary.rb
@@ -3,10 +3,15 @@ cask "electricbinary" do
   sha256 "3e383f673183265700e0c13a5f7b1d3348247465c3d8817c845d62e4c92d86ce"
 
   url "https://ftp.gnu.org/pub/gnu/electric/electricBinary-#{version}.jar"
-  appcast "https://ftp.gnu.org/pub/gnu/electric/"
   name "Electric VLSI Design System"
   desc "Electrical CAD system for the design of integrated circuits"
   homepage "https://www.gnu.org/software/electric/electric.html"
+
+  livecheck do
+    url "https://ftp.gnu.org/pub/gnu/electric/"
+    strategy :page_match
+    regex(/href=.*?electricBinary-(\d+(?:\.\d+)*)\.jar/i)
+  end
 
   container type: :naked
 
