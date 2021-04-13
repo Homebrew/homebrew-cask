8fea2b519ac49966b2a1fedb72d833d707ea2f1c
Markus Reiter <me@reitermark.us>
2021-04-13 06:12:01 +0200

    fastscripts: add livecheck (#103800)

--- Casks/fastscripts.rb
+++ Casks/fastscripts.rb
@@ -1,12 +1,16 @@
 cask "fastscripts" do
-  version "2.8.3"
+  version "2.8.3,985"
   sha256 "0e942f689e2965613c8702abe214496084c5e94147bd87c9963fb392161e9d32"
 
-  url "https://red-sweater.com/fastscripts/FastScripts#{version}.zip"
-  appcast "https://red-sweater.com/fastscripts/appcast2.php"
+  url "https://redsweater.com/fastscripts/FastScripts#{version.before_comma}.zip"
   name "FastScripts"
   desc "Tool for running time-saving scripts"
-  homepage "https://red-sweater.com/fastscripts/"
+  homepage "https://redsweater.com/fastscripts/"
+
+  livecheck do
+    url "https://redsweater.com/fastscripts/appcast2.php"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :sierra"
 
