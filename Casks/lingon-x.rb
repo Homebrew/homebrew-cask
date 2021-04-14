fb1ee7799ce437758b9f1e11be07c58c25a9ea1f
Jacob Hein <10779515+Jahhein@users.noreply.github.com>
2021-04-14 00:47:13 -0400

    Update lingon-x from 8.1.2 to 8.2

--- Casks/lingon-x.rb
+++ Casks/lingon-x.rb
@@ -3,8 +3,8 @@ cask "lingon-x" do
     version "6.6.5"
     sha256 "b0231b1a98dcc8f5c4234b419c9f5331407b8cce29b33f0ea2e32b12595adfa8"
   else
-    version "8.1.2"
-    sha256 "0a0582b0e54008151c61727b0d5c30994efe2c90723934e7ced6812b7134b6a0"
+    version "8.2"
+    sha256 "bb661fc5cb453988297a36c3d412304dbc4de1e18324e86b0b6b3a059bf90c27"
   end
 
   url "https://www.peterborgapps.com/downloads/LingonX#{version.major}.zip"
