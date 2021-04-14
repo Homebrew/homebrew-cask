52f66991541156811b7de50dd35e0e1600ede36a
Markus Reiter <me@reitermark.us>
2021-04-14 05:02:25 +0200

    ishowu-instant: add livecheck (#103889)
    * ishowu-instant: add livecheck
    
    * Update ishowu-instant.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/ishowu-instant.rb
+++ Casks/ishowu-instant.rb
@@ -1,11 +1,16 @@
 cask "ishowu-instant" do
-  version "1.4.2"
-  sha256 "fabe0f178a73cd86482a3f9613f088f9d253db18b2e02ecb8cd7bdbf1e2dcbee"
+  version "1.4.4,1257"
+  sha256 "b166097006dc3e309a7e27981b57c979291587f254785e9e2ed33b8c313a6dbe"
 
-  url "https://www.shinywhitebox.com/downloads/instant/iShowU_Instant_#{version}.dmg"
-  appcast "https://www.shinywhitebox.com/store/appcast.php?p=14"
+  url "https://www.shinywhitebox.com/downloads/instant/iShowU_Instant_#{version.before_comma}.dmg"
   name "iShowU Instant"
+  desc "Realtime screen recording"
   homepage "https://www.shinywhitebox.com/ishowu-instant"
+
+  livecheck do
+    url "https://www.shinywhitebox.com/store/appcast.php?p=14"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :mojave"
 
