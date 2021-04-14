e08b815e7cb243161168eeecbf7ba7e745227197
Kid <44045911+kidonng@users.noreply.github.com>
2021-04-14 15:38:33 +0800

    Update microsoft-outlook from 16.47.21031401 to 16.48.21041102 (#103904)
    * Update microsoft-outlook from 16.47.21031401 to 16.48.21041102
    
    * Update microsoft-outlook.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/microsoft-outlook.rb
+++ Casks/microsoft-outlook.rb
@@ -1,13 +1,17 @@
 cask "microsoft-outlook" do
-  version "16.47.21031401"
-  sha256 "2058906f61be9c6a4822560d34ef2fd00bd49933a2b31f5c5ccb9e937f9bb8f4"
+  version "16.48.21041102"
+  sha256 "7012a8045a51871924a1dcd383697be2a77f0f2e1a1766ed5c5723f89557ac08"
 
-  url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Outlook_#{version}_Installer.pkg",
-      verified: "officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/"
-  appcast "https://docs.microsoft.com/en-us/officeupdates/update-history-office-for-mac"
+  url "https://officecdn-microsoft-com.akamaized.net/db/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Outlook_#{version}_Installer.pkg",
+      verified: "officecdn-microsoft-com.akamaized.net/db/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/"
   name "Microsoft Outlook"
   desc "Email client"
   homepage "https://products.office.com/en-us/outlook/email-and-calendar-software-microsoft-outlook"
+
+  livecheck do
+    url "https://go.microsoft.com/fwlink/p/?linkid=525137"
+    strategy :header_match
+  end
 
   auto_updates true
   conflicts_with cask: "microsoft-office"
