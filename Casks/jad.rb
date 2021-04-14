ad4f0bf12a9cf401082aeb08ced7fb9085d78e2b
Markus Reiter <me@reitermark.us>
2021-04-14 09:13:27 +0200

    jad: add livecheck (#103893)
    * jad: add livecheck
    
    * Update jad.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/jad.rb
+++ Casks/jad.rb
@@ -3,9 +3,15 @@ cask "jad" do
   sha256 "8e9e4ea6c4177acce6d27325a036f10a72c170ed60e48c37c3483335319d07b9"
 
   url "https://varaneckas.com/jad/jad#{version.no_dots}.mac.intel.zip"
-  appcast "https://varaneckas.com/jad/"
-  name "Jad - the fast Java Decompiler"
+  name "Jad"
+  desc "Java decompiler"
   homepage "https://varaneckas.com/jad/"
+
+  livecheck do
+    url "https://varaneckas.com/jad/"
+    strategy :page_match
+    regex(/Jad\s*(\d+(?:\.\d+)*\w?)\s*for/i)
+  end
 
   binary "jad"
   manpage "jad.1"
