136904c41471652a405fba42fc023f8a8901df18
Markus Reiter <me@reitermark.us>
2021-04-14 02:16:46 +0200

    genymotion: add livecheck (#103853)
    * genymotion: add livecheck
    
    * Update genymotion.rb

--- Casks/genymotion.rb
+++ Casks/genymotion.rb
@@ -3,10 +3,15 @@ cask "genymotion" do
   sha256 "9025dd0baf74a7d7258570d6d2ca0f45af9f5c8e680080012cac7d42b411e9e3"
 
   url "https://dl.genymotion.com/releases/genymotion-#{version}/genymotion-#{version}.dmg"
-  appcast "https://www.genymotion.com/product-release-note/desktop/"
   name "Genymotion"
   desc "Android emulator"
   homepage "https://www.genymotion.com/"
+
+  livecheck do
+    url "https://www.genymotion.com/download/"
+    strategy :page_match
+    regex(/Genymotion\s*Desktop\s*(\d+(?:\.\d+)*)/i)
+  end
 
   depends_on cask: "virtualbox"
 
