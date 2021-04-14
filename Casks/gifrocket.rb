2089ac9ec8f5ca0360571a366130e41411917338
Markus Reiter <me@reitermark.us>
2021-04-14 04:12:06 +0200

    gifrocket: add livecheck (#103860)
    * gifrocket: add livecheck
    
    * Update gifrocket.rb

--- Casks/gifrocket.rb
+++ Casks/gifrocket.rb
@@ -3,10 +3,15 @@ cask "gifrocket" do
   sha256 "c5407c9caad8c038f604d35da9a6554dede79611daaf5174116ea9517a704593"
 
   url "https://www.gifrocket.com/Gifrocket.#{version}.zip"
-  appcast "https://www.gifrocket.com/"
   name "Gifrocket"
   desc "Tool to create GIFs from videos"
   homepage "https://www.gifrocket.com/"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(/href=.*?Gifrocket.(\d+(?:\.\d+)*)\.zip/i)
+  end
 
   app "Gifrocket.app"
 end
