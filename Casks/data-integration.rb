ddebdb11465bacbc7a0cb3a76c8e5a7633a6a6ca
Markus Reiter <me@reitermark.us>
2021-04-14 02:12:55 +0200

    data-integration: add livecheck

--- Casks/data-integration.rb
+++ Casks/data-integration.rb
@@ -3,7 +3,6 @@ cask "data-integration" do
   sha256 "ffbcb7bba736af765bbb14ccb0a5f2ae239e75b5aebf0ecfee924a5738d2c530"
 
   url "https://downloads.sourceforge.net/pentaho/pdi-ce-#{version}.zip"
-  appcast "https://sourceforge.net/projects/pentaho/rss?path=/"
   name "Pentaho Data Integration"
   homepage "https://sourceforge.net/projects/pentaho/"
 
