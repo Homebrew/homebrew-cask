d33c99f63ce15c8783c43ff1a4419ede0bae175e
Markus Reiter <me@reitermark.us>
2021-04-14 02:51:43 +0200

    hey: add livecheck

--- Casks/hey.rb
+++ Casks/hey.rb
@@ -4,10 +4,14 @@ cask "hey" do
 
   url "https://hey-desktop.s3.amazonaws.com/HEY-#{version}.dmg",
       verified: "hey-desktop.s3.amazonaws.com/"
-  appcast "https://hey-desktop.s3.amazonaws.com/latest-mac.yml"
   name "HEY"
   desc "Access the HEY email service"
   homepage "https://hey.com/"
+
+  livecheck do
+    url "https://hey-desktop.s3.amazonaws.com/latest-mac.yml"
+    strategy :electron_builder
+  end
 
   auto_updates true
 
