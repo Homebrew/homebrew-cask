71cb6da2fe49d7fd7dd1fb1faa95399da41635c7
Markus Reiter <me@reitermark.us>
2021-04-14 04:59:52 +0200

    hermit-crab: add livecheck (#103871)
    * hermit-crab: add livecheck
    
    * Update hermit-crab.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/hermit-crab.rb
+++ Casks/hermit-crab.rb
@@ -3,9 +3,14 @@ cask "hermit-crab" do
   sha256 "f948f646c1a27f77921a21800f859653134db2631926c4b2f68658f84dd063a1"
 
   url "https://belkadan.com/hermitcrab/updates/HermitCrab-#{version}.zip"
-  appcast "https://belkadan.com/hermitcrab/updates/"
   name "Hermit Crab"
+  desc "Run shell commands without leaving your current app"
   homepage "https://belkadan.com/hermitcrab/"
+
+  livecheck do
+    url "https://belkadan.com/hermitcrab/updates/"
+    strategy :sparkle
+  end
 
   auto_updates true
 
