1be78f6f304d0a18e7b1ac5c921871c2a83b0b74
Markus Reiter <me@reitermark.us>
2021-04-14 05:01:04 +0200

    id3-editor: add livecheck (#103877)
    * id3-editor: add livecheck
    
    * Update id3-editor.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/id3-editor.rb
+++ Casks/id3-editor.rb
@@ -3,9 +3,15 @@ cask "id3-editor" do
   sha256 "c19eeb045d0a48862a9f66bbdc27b00f9077690a23491804b539d00d6b7ee352"
 
   url "http://www.pa-software.com/release/ID3Editor.ub.#{version}.dmg"
-  appcast "http://www.pa-software.com/id3editor/history/"
   name "ID3 Editor"
+  desc "MP3 and AIFF ID3 tag editor"
   homepage "http://www.pa-software.com/id3editor/"
+
+  livecheck do
+    url "http://www.pa-software.com/id3editor/history/"
+    strategy :page_match
+    regex(/Version\s*(\d+(?:\.\d+)*)/i)
+  end
 
   app "ID3 Editor.app"
 end
