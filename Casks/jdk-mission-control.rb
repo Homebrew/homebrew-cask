8bc4972a370ed57706bae27aea8a1e75d5c48a6e
Markus Reiter <me@reitermark.us>
2021-04-14 09:27:28 +0200

    jdk-mission-control: add livecheck (#103896)

--- Casks/jdk-mission-control.rb
+++ Casks/jdk-mission-control.rb
@@ -3,10 +3,17 @@ cask "jdk-mission-control" do
   sha256 "3eb380e7ae241851cebeaf636b0de063d7a4a9585265c25ce1b1bfba8d56b100"
 
   url "https://download.java.net/java/GA/jmc#{version.major}/#{version.after_comma}/binaries/jmc-#{version.before_comma}_osx-x64.tar.gz"
-  appcast "https://jdk.java.net/jmc/8",
-          must_contain: version.after_comma
   name "JDK Mission Control"
+  desc "Tools to manage, monitor, profile and troubleshoot Java applications"
   homepage "https://jdk.java.net/jmc/8"
+
+  livecheck do
+    url :homepage
+    strategy :page_match do |page|
+      match = page.match(%r{href=.*?/(\d+)/binaries/jmc-(\d+(?:\.\d+)*)_osx-x64.tar\.gz}i)
+      "#{match[2]},#{match[1]}"
+    end
+  end
 
   app "jmc-#{version.before_comma}_osx-x64/JDK Mission Control.app"
 
