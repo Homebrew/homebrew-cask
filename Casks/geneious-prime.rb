0f327dd3e6e8bcf16ba9a80432f0b05d1a9602c5
Markus Reiter <me@reitermark.us>
2021-04-14 04:15:54 +0200

    geneious-prime: add livecheck (#103850)
    * geneious-prime: add livecheck
    
    * Update geneious-prime.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/geneious-prime.rb
+++ Casks/geneious-prime.rb
@@ -1,11 +1,17 @@
 cask "geneious-prime" do
-  version "2021.0.1"
-  sha256 "cf270037994ac8d8b83ca16346cb793b0dbe51ba8647e1b6ac2b6e95d85d3f02"
+  version "2021.1.1"
+  sha256 "556bd07e1404681ec137296c61aed896aea6b0ab6fb7fb1a326923de62c1e88a"
 
   url "https://assets.geneious.com/installers/geneious/release/Geneious_Prime_mac64_#{version.dots_to_underscores}_with_jre.dmg"
-  appcast "https://www.geneious.com/download/"
   name "Geneious Prime"
+  desc "Bioinformatics software platform"
   homepage "https://www.geneious.com/"
+
+  livecheck do
+    url "https://www.geneious.com/download/"
+    strategy :page_match
+    regex(/Prime\s*(\d+(?:\.\d+)*)/i)
+  end
 
   app "Geneious Prime.app"
 end
