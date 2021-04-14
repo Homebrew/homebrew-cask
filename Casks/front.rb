8313e66b7ee31df60c47269025a2b6f164465bb8
Markus Reiter <me@reitermark.us>
2021-04-14 02:58:44 +0200

    front: add livecheck (#103844)
    * front: add livecheck
    
    * Update front.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/front.rb
+++ Casks/front.rb
@@ -1,11 +1,16 @@
 cask "front" do
-  version "3.25.3"
-  sha256 "7fb01f8ce1ada40928cf79ffdeb2cdafaeac450c2f1600a8643d4d9c573fcc0d"
+  version "3.26.0"
+  sha256 "ec465f1776033ca996c9a334fa82436f71e7570bced27297b434a51f4648aa2d"
 
   url "https://dl.frontapp.com/desktop/builds/#{version}/Front-#{version}.zip"
-  appcast "https://dl.frontapp.com/desktop/updates/latest/mac/latest-mac.yml"
   name "Front"
+  desc "Customer communication platform"
   homepage "https://frontapp.com/"
+
+  livecheck do
+    url "https://dl.frontapp.com/desktop/updates/latest/mac/latest-mac.yml"
+    strategy :electron_builder
+  end
 
   app "Front.app"
 end
