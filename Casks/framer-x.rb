b3f4d32973dea1859ff14e483575a1b2032a40ef
Markus Reiter <me@reitermark.us>
2021-04-14 02:53:10 +0200

    framer-x: add livecheck (#103841)
    * framer-x: add livecheck
    
    * Update framer-x.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/framer-x.rb
+++ Casks/framer-x.rb
@@ -3,9 +3,17 @@ cask "framer-x" do
   sha256 "ff8d2ea3ad84600942c25466e0fbc8efbf5cf0fa9995a8ac6796442a5e3e2359"
 
   url "https://dl.framer.com/com.framer.x/#{version.before_comma}/#{version.after_comma}/FramerX-#{version.before_comma}.zip"
-  appcast "https://updates.framer.com/sparkle/com.framer.x"
   name "Framer X"
+  desc "Tool that helps teams design every part of the product experience"
   homepage "https://framer.com/"
+
+  livecheck do
+    url "https://updates.framer.com/sparkle/com.framer.x"
+    strategy :page_match do |page|
+      match = page.match(%r{/(\d+)/FramerX-(\d+).zip}i)
+      "#{match[2]},#{match[1]}"
+    end
+  end
 
   auto_updates true
   depends_on macos: ">= :high_sierra"
