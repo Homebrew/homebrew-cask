21c42e8a43957d0b64383660270732ba9560d884
Loïc PÉRON <loic.peron@gmail.com>
2021-04-14 11:35:44 +0200

    Update drivethrurpg using application native version check source for livecheck and versioned download

--- Casks/drivethrurpg.rb
+++ Casks/drivethrurpg.rb
@@ -1,16 +1,16 @@
 cask "drivethrurpg" do
   version "3.0.1.3"
-  sha256 :no_check
+  sha256 "d12c39314aae8ef357e3204a1bf013319ebf3085fc47400c613345b5b2cdfc9e"
 
-  url "https://dtrpg-library-app.s3.us-east-2.amazonaws.com/DriveThruRPG.dmg",
+  url "https://dtrpg-library-app.s3.us-east-2.amazonaws.com/DriveThruRPG_v#{version}.dmg",
       verified: "dtrpg-library-app.s3.us-east-2.amazonaws.com/"
   name "DriveThruRPG Library App"
   desc "Sync DriveThruRPG libraries to compatible devices"
   homepage "https://www.drivethrurpg.com/library_client.php"
 
   livecheck do
-    url "https://www.drivethrurpg.com/library_client.php?os=Macintosh"
-    regex(%r{href=.*?/DriveThruRPG_v?(\d+(?:\.\d+)*)\.dmg}i)
+    url "https://www.drivethrurpg.com/library-client/current-version-osx.txt"
+    regex(%r{.*?/DriveThruRPG_v?(\d+(?:\.\d+)*)\.dmg}i)
   end
 
   depends_on macos: ">= :high_sierra"
