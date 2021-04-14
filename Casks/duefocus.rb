dce468808a1dc4dbd3a6aa394b2ed81a6ba841d5
Markus Reiter <me@reitermark.us>
2021-04-14 04:23:00 +0200

    duefocus: add livecheck (#103859)
    * duefocus: add livecheck
    
    * Update duefocus.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/duefocus.rb
+++ Casks/duefocus.rb
@@ -1,11 +1,17 @@
 cask "duefocus" do
   version "2.5.0"
-  sha256 "41cb010236fce73f91ad0e55a14d29c7fe1451984aba6be3b6a5de3f1f789271"
+  sha256 "28c9029ee71701ecc1daa8d6ed070f33232d3a5c0c9cf878d916dbec63a8dc7a"
 
-  url "https://web.duefocus.com/distribution/darwin/v3/DueFocus-#{version}-mac.zip"
-  appcast "https://web.duefocus.com/distribution/darwin/v3/appcast.html"
+  url "https://web.duefocus.com/distribution/darwin/v3/DueFocus-#{version}.dmg"
   name "DueFocus"
+  desc "Time tracking and productivity software"
   homepage "https://duefocus.com/"
+
+  livecheck do
+    url "https://web.duefocus.com/distribution/darwin/v3/appcast.html"
+    strategy :page_match
+    regex(%r{/DueFocus-(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "DueFocus.app"
 
