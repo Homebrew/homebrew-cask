e1d300235e03aa77c8cc58797288fbae9845d242
Markus Reiter <me@reitermark.us>
2021-04-15 01:54:05 +0200

    Update navicat-data-modeler.rb

--- Casks/navicat-data-modeler.rb
+++ Casks/navicat-data-modeler.rb
@@ -1,6 +1,6 @@
 cask "navicat-data-modeler" do
   version "3.0.15"
-  sha256 "05fefb0d7474b2ed235604f236dac3e78ae83c9122f6b38390f0762379c08b06"
+  sha256 :no_check
 
   url "http://download.navicat.com/download/modeler0#{version.major_minor.no_dots}_en.dmg"
   name "Navicat Data Modeler"
