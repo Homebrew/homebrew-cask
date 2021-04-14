d335fc524f3a0ddc0edc7753280e5ac2d41983f1
Markus Reiter <me@reitermark.us>
2021-04-14 14:29:20 +0200

    kaleidoscope: add livecheck

--- Casks/kaleidoscope.rb
+++ Casks/kaleidoscope.rb
@@ -1,12 +1,19 @@
 cask "kaleidoscope" do
-  version "2.4,1450-feb-4-2021"
+  version "2.4,1450:feb-4-2021"
   sha256 "f3cb0d9135fecb430c949bb6d2e53eacb6f7b81873b707fa021bd6ae1c16b1b9"
 
-  url "https://updates.kaleidoscope.app/v2/prod/Kaleidoscope-#{version.before_comma}-#{version.after_comma}.app.zip"
-  appcast "https://updates.kaleidoscope.app/v2/prod/appcast"
+  url "https://updates.kaleidoscope.app/v2/prod/Kaleidoscope-#{version.before_comma}-#{version.after_comma.before_colon}-#{version.after_colon}.app.zip"
   name "Kaleidoscope"
   desc "Spot and merge differences in text and image files or folders"
   homepage "https://www.kaleidoscope.app/"
+
+  livecheck do
+    url "https://updates.kaleidoscope.app/v2/prod/appcast"
+    strategy :sparkle do |item|
+      match = item.url.match(%r{/Kaleidoscope-(\d+(?:\.\d+)*)-(\d+)-(.+(?:\-.+)*)\.app\.zip}i)
+      "#{match[1]},#{match[2]}:#{match[3]}"
+    end
+  end
 
   auto_updates true
   depends_on macos: ">= :high_sierra"
