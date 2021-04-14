43596f21583724db7c210df14c7e7ee62fdc1f10
Markus Reiter <me@reitermark.us>
2021-04-14 04:25:45 +0200

    Update jgrasp.rb

--- Casks/jgrasp.rb
+++ Casks/jgrasp.rb
@@ -1,18 +1,15 @@
 cask "jgrasp" do
-  version "2.0.6,07"
+  version "2.0.6_07"
   sha256 "0ffec1427ec0587f29f1d7765382be42c1732c4da99b9b46504adc260884f124"
 
-  url "https://jgrasp.org/dl4g/jgrasp/jgrasp#{version.before_comma.no_dots}_#{version.after_comma}.pkg"
+  url "https://jgrasp.org/dl4g/jgrasp/jgrasp#{version.no_dots}.pkg"
   name "jgrasp"
   homepage "https://jgrasp.org/"
 
   livecheck do
     url "https://spider.eng.auburn.edu/user-cgi/grasp/grasp.pl?;dl=download_jgrasp.html"
     strategy :page_match
-    regex()
-      match = page.match(%r{/jgrasp(\d+)(\d+)(\d+)_(\d+)\.pkg}i)
-      "#{match[1]}.#{match[2]}.#{match[3]},#{match[4]}"
-    end
+    regex(/jGRASP\s*(\d+(?:\.\d+)*_\d+)/)
   end
 
   pkg "jgrasp#{version.no_dots}.pkg"
