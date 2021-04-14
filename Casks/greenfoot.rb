1d7626748fc6b91d49d9c96724ba1c12e7115b54
Markus Reiter <me@reitermark.us>
2021-04-14 04:29:30 +0200

    greenfoot: add livecheck (#103866)
    * greenfoot: add livecheck
    
    * Update greenfoot.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/greenfoot.rb
+++ Casks/greenfoot.rb
@@ -3,9 +3,15 @@ cask "greenfoot" do
   sha256 "9afbc077f6454d83749224cc62b5192eae96edf4d7d6beaee880712bb17e8646"
 
   url "https://www.greenfoot.org/download/files/Greenfoot-mac-#{version.no_dots}.zip"
-  appcast "https://www.greenfoot.org/download"
   name "Greenfoot"
+  desc "Teach object orientation with Java"
   homepage "https://www.greenfoot.org/home"
+
+  livecheck do
+    url "https://www.greenfoot.org/download"
+    strategy :page_match
+    regex(/Version:\s*(\d+(?:\.\d+)*)/i)
+  end
 
   app "Greenfoot #{version}/Greenfoot.app"
 end
