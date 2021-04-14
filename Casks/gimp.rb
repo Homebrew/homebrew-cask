d0067ee578fd2ee4092d066062b7a4be66eb3d67
Markus Reiter <me@reitermark.us>
2021-04-14 02:55:05 +0200

    gimp: add livecheck (#103861)

--- Casks/gimp.rb
+++ Casks/gimp.rb
@@ -3,11 +3,16 @@ cask "gimp" do
   sha256 "844dc06731cbd8ccaa6ffd4e0c74ad49ed1ecb6ae65db71988102acb6c219d56"
 
   url "https://download.gimp.org/pub/gimp/v#{version.major_minor}/osx/gimp-#{version}-x86_64-3.dmg"
-  appcast "https://download.gimp.org/pub/gimp/v#{version.major_minor}/osx/"
   name "GIMP"
   name "GNU Image Manipulation Program"
   desc "Free and open-source image editor"
   homepage "https://www.gimp.org/"
+
+  livecheck do
+    url "https://www.gimp.org/downloads/"
+    strategy :page_match
+    regex(%r{href=.*?/gimp-(\d+(?:\.\d+)*)-x86_64-3\.dmg}i)
+  end
 
   auto_updates true
 
