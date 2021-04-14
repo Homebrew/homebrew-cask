6f25c2af6c6d72165825bafc8526fb77ec158286
Markus Reiter <me@reitermark.us>
2021-04-14 09:14:22 +0200

    firestormos: add livecheck (#103827)
    * firestormos: add livecheck
    
    * Update firestormos.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/firestormos.rb
+++ Casks/firestormos.rb
@@ -1,11 +1,19 @@
 cask "firestormos" do
-  version "6.3.9.58205"
-  sha256 "7bb23c680a31259b7d7e8d45899421cbfe16fd1a0b245bfe733fc63e8cd6840f"
+  version "6.4.13.63251"
+  sha256 "989662f0d4ba19c0b9fc34a9fe0b246fe8af41636102d950138b74d8fa694733"
 
   url "https://downloads.firestormviewer.org/mac/Phoenix-FirestormOS-Releasex64_#{version.dots_to_underscores}_x86_64.dmg"
-  appcast "https://www.firestormviewer.org/mac-for-open-simulator/"
   name "Phoenix Firestorm viewer for OpenSim"
+  desc "Viewer for accessing Virtual Worlds"
   homepage "https://www.firestormviewer.org/"
+
+  livecheck do
+    url "https://www.firestormviewer.org/mac-for-open-simulator/"
+    strategy :page_match do |page|
+      v = page[%r{href=.*?/Phoenix-FirestormOS-Releasex64_(\d+(?:_\d+)*)_x86_64\.dmg}i, 1]
+      v.tr("_", ".")
+    end
+  end
 
   app "FirestormOS-Releasex64.app"
 
