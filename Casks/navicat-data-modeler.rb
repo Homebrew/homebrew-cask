9de267055217cf8b57352806e2738d2d2f63fc33
Markus Reiter <me@reitermark.us>
2021-04-15 01:52:13 +0200

    navicat-data-modeler: add livecheck

--- Casks/navicat-data-modeler.rb
+++ Casks/navicat-data-modeler.rb
@@ -3,9 +3,13 @@ cask "navicat-data-modeler" do
   sha256 "05fefb0d7474b2ed235604f236dac3e78ae83c9122f6b38390f0762379c08b06"
 
   url "http://download.navicat.com/download/modeler0#{version.major_minor.no_dots}_en.dmg"
-  appcast "https://navicat.com/en/products/navicat-data-modeler-release-note"
   name "Navicat Data Modeler"
   homepage "https://www.navicat.com/products/navicat-data-modeler"
+
+  livecheck do
+    url "https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20Data%20Modeler"
+    strategy :sparkle
+  end
 
   app "Navicat Data Modeler.app"
 end
