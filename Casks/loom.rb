6ced67345755b9041f7f819b9d472337bcc915e0
Markus Reiter <me@reitermark.us>
2021-04-14 23:01:54 +0200

    loom: add livecheck

--- Casks/loom.rb
+++ Casks/loom.rb
@@ -1,12 +1,16 @@
 cask "loom" do
-  version "0.68.2"
-  sha256 "68a245cfee5021307b7a493f290ee4fd779457fbdd6db12c557ef149fad9819b"
+  version "0.75.1"
+  sha256 "3427ca48c5b796e3ee22241da27a60a9f5a595d555f435d5871ef920d3b51243"
 
   url "https://cdn.loom.com/desktop-packages/Loom-#{version}.dmg"
-  appcast "https://s3-us-west-2.amazonaws.com/loom.desktop.packages/loom-inc-production/desktop-packages/latest-mac.yml"
   name "Loom"
   desc "Screen and video recording software"
   homepage "https://www.loom.com/"
+
+  livecheck do
+    url "https://s3-us-west-2.amazonaws.com/loom.desktop.packages/loom-inc-production/desktop-packages/latest-mac.yml"
+    strategy :electron_builder
+  end
 
   auto_updates true
 
