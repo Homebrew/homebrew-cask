942ed004893b597eee187096de6999a1628b3309
Markus Reiter <me@reitermark.us>
2021-04-14 04:24:27 +0200

    jgrasp: add livecheck

--- Casks/jgrasp.rb
+++ Casks/jgrasp.rb
@@ -1,11 +1,19 @@
 cask "jgrasp" do
-  version "2.0.6_07"
+  version "2.0.6,07"
   sha256 "0ffec1427ec0587f29f1d7765382be42c1732c4da99b9b46504adc260884f124"
 
-  url "https://jgrasp.org/dl4g/jgrasp/jgrasp#{version.no_dots}.pkg"
-  appcast "https://jgrasp.org/"
+  url "https://jgrasp.org/dl4g/jgrasp/jgrasp#{version.before_comma.no_dots}_#{version.after_comma}.pkg"
   name "jgrasp"
   homepage "https://jgrasp.org/"
+
+  livecheck do
+    url "https://spider.eng.auburn.edu/user-cgi/grasp/grasp.pl?;dl=download_jgrasp.html"
+    strategy :page_match
+    regex()
+      match = page.match(%r{/jgrasp(\d+)(\d+)(\d+)_(\d+)\.pkg}i)
+      "#{match[1]}.#{match[2]}.#{match[3]},#{match[4]}"
+    end
+  end
 
   pkg "jgrasp#{version.no_dots}.pkg"
 
