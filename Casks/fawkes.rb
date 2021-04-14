d6ff883a7db543b6ad32a2357212893c1d29820d
Markus Reiter <me@reitermark.us>
2021-04-14 02:28:22 +0200

    fawkes: add livecheck (#103802)
    * fawkes: add livecheck
    
    * Update fawkes.rb
    
    * Update fawkes.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/fawkes.rb
+++ Casks/fawkes.rb
@@ -3,11 +3,15 @@ cask "fawkes" do
   sha256 "7101ecb08e9ce319c66f1e6dae25443a3175917eaa48feeaf17b1864f91ab6f8"
 
   url "https://mirror.cs.uchicago.edu/fawkes/files/#{version.major_minor}/Fawkes-#{version}.dmg"
-  appcast "https://github.com/Shawn-Shan/fawkes/releases.atom",
-          must_contain: version.major_minor
   name "Fawkes"
   desc "Cloaks picture files to circumvent facial recognition"
   homepage "https://sandlab.cs.uchicago.edu/fawkes/"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(%r{href=.*?/Fawkes-(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "Fawkes.app"
 
