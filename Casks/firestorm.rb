704a803f829a9a79a9f3b55acac109588e4e6929
Markus Reiter <me@reitermark.us>
2021-04-14 02:52:07 +0200

    firestorm: add livecheck (#103826)
    * firestorm: add livecheck
    
    * Update firestorm.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/firestorm.rb
+++ Casks/firestorm.rb
@@ -1,11 +1,19 @@
 cask "firestorm" do
-  version "6.4.12.62831"
-  sha256 "0c0a7c8e21b9389bc141a18b489a76ac821049f4fa33f0b6ede5b77d23ae33ad"
+  version "6.4.13.63251"
+  sha256 "ea560607ce3a8262540951c33fdc250096fdccb66d056dfdb36e85f11bb8b087"
 
   url "https://downloads.firestormviewer.org/mac/Phoenix-Firestorm-Releasex64_#{version.dots_to_underscores}_x86_64.dmg"
-  appcast "https://www.firestormviewer.org/mac/"
   name "Phoenix Firestorm viewer for Second Life"
+  desc "Viewer for accessing Virtual Worlds"
   homepage "https://www.firestormviewer.org/"
+
+  livecheck do
+    url "https://www.firestormviewer.org/mac/"
+    strategy :page_match do |page|
+      v = page[%r{href=.*?/Phoenix-Firestorm-Releasex64_(\d+(?:_\d+)*)_x86_64\.dmg}i, 1]
+      v.tr("_", ".")
+    end
+  end
 
   app "Firestorm-Releasex64.app"
 
