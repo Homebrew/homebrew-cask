8f129a398627c1d18bf9a60d4ad5fd3657470d1f
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-14 10:24:12 +0800

    Update ibackup.rb

--- Casks/ibackup.rb
+++ Casks/ibackup.rb
@@ -4,6 +4,7 @@ cask "ibackup" do
 
   url "http://www.grapefruit.ch/iBackup/versions/iBackup%20#{version.major}.x/iBackup%20#{version}.dmg"
   name "iBackup"
+  desc "Backup utility"
   homepage "http://www.grapefruit.ch/iBackup/"
 
   livecheck do
