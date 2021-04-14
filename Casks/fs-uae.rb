1674c7ce787a6de22718c8a7fd247d05d41ba0dc
Markus Reiter <me@reitermark.us>
2021-04-14 02:58:59 +0200

    fs-uae: add livecheck (#103845)
    * fs-uae: add livecheck
    
    * Update fs-uae.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/fs-uae.rb
+++ Casks/fs-uae.rb
@@ -3,9 +3,15 @@ cask "fs-uae" do
   sha256 "dd91e365cb066f25267af620d138ad96ca171a192c704e9132b39ba0b4ce74c9"
 
   url "https://fs-uae.net/stable/#{version}/FS-UAE-Suite_#{version}_macOS_x86-64.dmg"
-  appcast "https://fs-uae.net/stable/latest-macosx"
   name "FS-UAE"
+  desc "Amiga emulator"
   homepage "https://fs-uae.net/"
+
+  livecheck do
+    url "https://fs-uae.net/stable/latest-macosx"
+    strategy :page_match
+    regex(/(\d+(?:\.\d+)*)/i)
+  end
 
   app "FS-UAE Arcade.app"
   app "FS-UAE Launcher.app"
