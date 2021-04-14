8a60c09173033ebbee2592034e8edeb0f3fcde45
Markus Reiter <me@reitermark.us>
2021-04-15 01:35:10 +0200

    clion: add livecheck

--- Casks/clion.rb
+++ Casks/clion.rb
@@ -1,18 +1,26 @@
 cask "clion" do
-  version "2021.1"
+  version "2021.1,211.6693.114"
 
   if Hardware::CPU.intel?
     sha256 "a60e0da5ad7ac01d8ddaf5cc4d7046e195fcfdfcc370d6fd98e6374fa7bf01d2"
-    url "https://download.jetbrains.com/cpp/CLion-#{version}.dmg"
+    url "https://download.jetbrains.com/cpp/CLion-#{version.before_comma}.dmg"
   else
     sha256 "f1421e0869db8490928280e7bd0f2798b9c933983a11043b511d6b90d9e91c48"
-    url "https://download.jetbrains.com/cpp/CLion-#{version}-aarch64.dmg"
+    url "https://download.jetbrains.com/cpp/CLion-#{version.before_comma}-aarch64.dmg"
   end
 
-  appcast "https://data.services.jetbrains.com/products/releases?code=CL&latest=true&type=release"
   name "CLion"
   desc "C and C++ IDE"
   homepage "https://www.jetbrains.com/clion/"
+
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=CL&latest=true&type=release"
+    strategy :page_match do |page|
+      version = page.match(/"version":"(\d+(?:\.\d+)*)/i)
+      build = page.match(/"build":"(\d+(?:\.\d+)*)/i)
+      "#{version[1]},#{build[1]}"
+    end
+  end
 
   auto_updates true
 
