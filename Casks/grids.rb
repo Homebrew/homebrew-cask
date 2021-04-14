2ea7192c5fe401541cf9b64814ee867a3db745cd
Markus Reiter <me@reitermark.us>
2021-04-14 02:54:13 +0200

    grids: add livecheck (#103867)

--- Casks/grids.rb
+++ Casks/grids.rb
@@ -1,12 +1,17 @@
 cask "grids" do
-  version "6.1.6"
-  sha256 "c3bd41b364b2151a06e542d60e341a19458b4641d67f6fefe6813cc78529762e"
+  version "7.0.4"
+  sha256 "eda2fbe56880bc50ada6be8432da379f782d42f669beb62006a16943bd0378ff"
 
   url "https://gridsapp.net/bin/Grids_#{version}.zip"
-  appcast "https://gridsapp.net/appcast.json"
   name "Grids"
   desc "Instagram desktop application"
   homepage "https://gridsapp.net/"
+
+  livecheck do
+    url "https://gridsapp.net/appcast.json"
+    strategy :page_match
+    regex(/"mac"\s*:\s*"(\d+(?:\.\d+)*)"/i)
+  end
 
   auto_updates true
   depends_on macos: ">= :sierra"
