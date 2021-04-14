23996037b6211c0f18f35090d99234ab424cfcc7
Markus Reiter <me@reitermark.us>
2021-04-14 03:35:15 +0200

    iridium: add livecheck

--- Casks/iridium.rb
+++ Casks/iridium.rb
@@ -3,10 +3,15 @@ cask "iridium" do
   sha256 "7107a639d78ca1d44313d1b54fe52d3df0e39ea782da52c3d9d528b2e8281ca5"
 
   url "https://downloads.iridiumbrowser.de/macos/#{version}-0/iridium-browser_#{version}-0_macos.dmg"
-  appcast "https://iridiumbrowser.de/downloads/macos"
   name "Iridium Browser"
   desc "Web browser focusing on security and privacy"
   homepage "https://iridiumbrowser.de/"
+
+  livecheck do
+    url "https://iridiumbrowser.de/downloads/macos"
+    strategy :page_match
+    regex(/current\s*version\s*(\d+(?:\.\d+)*)/i)
+  end
 
   app "Iridium.app"
 
