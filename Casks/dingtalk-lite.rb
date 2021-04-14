8ae56a81c83b5111ca32c79ef7a409bb482059fa
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-14 15:24:33 +0800

    Update dingtalk-lite.rb

--- Casks/dingtalk-lite.rb
+++ Casks/dingtalk-lite.rb
@@ -2,8 +2,7 @@ cask "dingtalk-lite" do
   version "5.1.21"
   sha256 "25f1fe4cb261f1bd597416290e285ea243cf417124c34dc24c51bb347767aaed"
 
-  url "https://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/DingTalkLite_v#{version}.dmg",
-      verified: "dtapp-pub.dingtalk.com/dingtalk-desktop/"
+  url "https://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/DingTalkLite_v#{version}.dmg"
   name "DingTalk Lite"
   name "钉钉 Lite"
   desc "Teamwork app by Alibaba Group"
