f01d3fef5f693f509dd99914687f7da984060435
Markus Reiter <me@reitermark.us>
2021-04-15 01:37:18 +0200

    datagrip: add livecheck

--- Casks/datagrip.rb
+++ Casks/datagrip.rb
@@ -1,18 +1,26 @@
 cask "datagrip" do
-  version "2021.1"
+  version "2021.1,211.6693.68"
 
   if Hardware::CPU.intel?
     sha256 "cf218fd07ec2bb9af0c7a1bb2de7ada4008c7cfcf7028bd6cb7923c828b54782"
-    url "https://download.jetbrains.com/datagrip/datagrip-#{version}.dmg"
+    url "https://download.jetbrains.com/datagrip/datagrip-#{version.before_comma}.dmg"
   else
     sha256 "3d25c4794beae3c48b422d78e10708ef56f155e889b7c0d6e8eaf79feb986dc4"
-    url "https://download.jetbrains.com/datagrip/datagrip-#{version}-aarch64.dmg"
+    url "https://download.jetbrains.com/datagrip/datagrip-#{version.before_comma}-aarch64.dmg"
   end
 
-  appcast "https://data.services.jetbrains.com/products/releases?code=DG&latest=true&type=release"
   name "DataGrip"
   desc "Databases & SQL IDE"
   homepage "https://www.jetbrains.com/datagrip/"
+
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=DG&latest=true&type=release"
+    strategy :page_match do |page|
+      version = page.match(/"version":"(\d+(?:\.\d+)*)/i)
+      build = page.match(/"build":"(\d+(?:\.\d+)*)/i)
+      "#{version[1]},#{build[1]}"
+    end
+  end
 
   auto_updates true
 
