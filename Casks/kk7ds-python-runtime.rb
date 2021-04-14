6ce6afe068016ebc7b4c23e00112dc4dea090f66
Markus Reiter <me@reitermark.us>
2021-04-14 22:38:42 +0200

    kk7ds-python-runtime: add livecheck

--- Casks/kk7ds-python-runtime.rb
+++ Casks/kk7ds-python-runtime.rb
@@ -1,11 +1,16 @@
 cask "kk7ds-python-runtime" do
-  version "10.1"
+  version "10"
   sha256 "5cee8acb941e39f93a4df6a99ed29a14c48da0bc5beb3b31068852b1fad8b009"
 
-  url "http://www.d-rats.com/download/OSX_Runtime/KK7DS_Python_Runtime_R#{version.major}.pkg"
-  appcast "http://www.d-rats.com/download/OSX_Runtime/"
+  url "http://www.d-rats.com/download/OSX_Runtime/KK7DS_Python_Runtime_R#{version}.pkg"
   name "KK7DS Python Runtime"
   homepage "http://www.d-rats.com/download/OSX_Runtime/"
+
+  livecheck do
+    url "http://www.d-rats.com/download/OSX_Runtime/"
+    strategy :page_match
+    regex(%r{href=.*?KK7DS_Python_Runtime_R(\d+(?:\.\d+)*)\.pkg}i)
+  end
 
   pkg "KK7DS_Python_Runtime_R#{version.major}.pkg"
 
