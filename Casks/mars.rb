73cd78d01e1f514c7f5c0a534fb28f45e00e3675
Markus Reiter <me@reitermark.us>
2021-04-15 04:40:41 +0200

    mars: add livecheck (#103964)

--- Casks/mars.rb
+++ Casks/mars.rb
@@ -3,10 +3,17 @@ cask "mars" do
   sha256 "ac340b676ba2b62246b9df77e62f81ad4447bcfd329ab539716bcd09950b7096"
 
   url "https://courses.missouristate.edu/KenVollmar/mars/MARS_#{version.before_comma.dots_to_underscores}_#{version.after_comma}/Mars#{version.before_comma.dots_to_underscores}.jar"
-  appcast "https://courses.missouristate.edu/KenVollmar/mars/Help/MarsHelpHistory.html"
   name "MARS"
   desc "Mips Assembly and Runtime Simulator"
   homepage "https://courses.missouristate.edu/KenVollmar/mars/index.htm"
+
+  livecheck do
+    url "https://courses.missouristate.edu/KenVollmar/mars/download.htm"
+    strategy :page_match do |page|
+      match = page.match(%r{href=.*?MARS_(\d+(?:_\d+)*)_(\w+\d+)/Mars(?:\d+(?:_\d+)*)\.jar}i)
+      "#{match[1].tr("_", ".")},#{match[2]}"
+    end
+  end
 
   container type: :naked
 
