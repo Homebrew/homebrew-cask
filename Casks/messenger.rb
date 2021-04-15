baeaf2b145ab8751ef43ba0d8c4ae006609307d4
Markus Reiter <me@reitermark.us>
2021-04-15 04:38:04 +0200

    messenger: add livecheck (#103972)

--- Casks/messenger.rb
+++ Casks/messenger.rb
@@ -1,15 +1,19 @@
 cask "messenger" do
-  version "83.5.128,262535382"
-  sha256 "07993d958f027ff898ed784d7435e87e34afb9cd710ba72a9a5965fad0e3ba4c"
+  version "97.11.116,283083801"
+  sha256 "940ce82dfa220ecdbf4d3b6757a4dc48362bb4384030316f50e1fbf980926544"
 
   url "https://www.facebook.com/messenger/desktop/update/#{version.after_comma}.zip",
       verified: "facebook.com/messenger/desktop/"
-  # The appcast will fail CI due to its Cache-Control settings. See:
-  #   https://github.com/Homebrew/homebrew-cask/pull/90957#issuecomment-710740050
-  appcast "https://www.facebook.com/messenger/desktop/update/latest-mac.yml"
   name "Facebook Messenger"
   desc "Native desktop app for Messenger (formerly Facebook Messenger)"
   homepage "https://www.messenger.com/desktop"
+
+  livecheck do
+    url "https://www.facebook.com/messenger/desktop/update/latest-mac.yml"
+    strategy :electron_builder do |yml|
+      "#{yml["version"]},#{yml["path"][%r{/(\d+)\.zip}i, 1]}"
+    end
+  end
 
   auto_updates true
 
