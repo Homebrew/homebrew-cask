41da11193e53027a2662d49a4cdd53ff381f8293
Markus Reiter <me@reitermark.us>
2021-04-15 08:01:32 +0200

    jprofiler: add livecheck (#103923)
    * jprofiler: add livecheck
    
    * Update jprofiler.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/jprofiler.rb
+++ Casks/jprofiler.rb
@@ -1,11 +1,17 @@
 cask "jprofiler" do
-  version "12.0"
-  sha256 "5f3221d6ef85d7ade5df0fa5f194e05643d7c0066cfb383f5e44810f1c03a9cd"
+  version "12.0.2"
+  sha256 "7f24428e32a62b9d247a23feaac1671278e7fb9d97dfd5bf21bd1984cc4d2911"
 
   url "https://download-gcdn.ej-technologies.com/jprofiler/jprofiler_macos_#{version.dots_to_underscores}.dmg"
-  appcast "https://www.ej-technologies.com/feeds/jprofiler/"
   name "JProfiler"
+  desc "Java profiler"
   homepage "https://www.ej-technologies.com/products/jprofiler/overview.html"
+
+  livecheck do
+    url "https://www.ej-technologies.com/feeds/jprofiler/"
+    strategy :page_match
+    regex(/JProfiler\s*Release\s*(\d+(?:\.\d+)*)/i)
+  end
 
   app "JProfiler.app"
 end
