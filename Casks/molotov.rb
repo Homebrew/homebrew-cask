2d884d927daa87f6d60c8e4b6d0ac012b85dcce1
Markus Reiter <me@reitermark.us>
2021-04-15 04:36:49 +0200

    molotov: add livecheck (#103980)

--- Casks/molotov.rb
+++ Casks/molotov.rb
@@ -1,12 +1,17 @@
 cask "molotov" do
-  version "4.4.0"
-  sha256 "d7ed7bec146eb0ffbabbdb419da4615ef2c20ea9b17f8bc7ac6de4a050fd94bc"
+  version "4.4.2"
+  sha256 "141c596bd144895fd11fe0e0af16d111d9c7ff77416132b4f0b835a165f909c3"
 
   url "https://desktop-auto-upgrade.molotov.tv/mac/Molotov-v#{version}.dmg"
-  appcast "https://desktop-auto-upgrade.molotov.tv/mac/manifest.json"
   name "Molotov"
   desc "French TV streaming service"
   homepage "https://www.molotov.tv/"
+
+  livecheck do
+    url "https://desktop-auto-upgrade.molotov.tv/mac/manifest.json"
+    strategy :page_match
+    regex(%r{/Molotov-v?(\d+(?:\.\d+)*)-mac\.zip}i)
+  end
 
   app "Molotov.app"
 end
