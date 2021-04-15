4714abf02bf43456dab434764f3c5e6dedd5085c
Markus Reiter <me@reitermark.us>
2021-04-15 04:27:17 +0200

    name-mangler: add livecheck (#103998)

--- Casks/name-mangler.rb
+++ Casks/name-mangler.rb
@@ -3,10 +3,14 @@ cask "name-mangler" do
   sha256 :no_check
 
   url "https://manytricks.com/download/namemangler"
-  appcast "https://manytricks.com/namemangler/appcast/"
   name "Name Mangler"
   desc "Multi-file renaming tool"
   homepage "https://manytricks.com/namemangler/"
+
+  livecheck do
+    url "https://manytricks.com/namemangler/appcast/"
+    strategy :sparkle
+  end
 
   auto_updates true
 
