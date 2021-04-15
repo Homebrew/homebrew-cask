b6f0196ddace5adb0c73ee5dcc2b7158bf38c8ce
Markus Reiter <me@reitermark.us>
2021-04-15 14:45:56 +0200

    Update adobe-air.rb

--- Casks/adobe-air.rb
+++ Casks/adobe-air.rb
@@ -1,11 +1,16 @@
 cask "adobe-air" do
-  version "32.0.0.125"
-  sha256 "d6abb9c4538f9dc9ca3d7f86629437fa52f5d7ee36ae2a3903adfb42d59fd4ad"
+  version "33.1.1.444"
+  sha256 :no_check
 
-  url "https://airdownload.adobe.com/air/mac/download/#{version.major_minor}/AdobeAIR.dmg"
-  appcast "https://helpx.adobe.com/au/air/kb/archived-air-sdk-version.html"
+  url "https://airsdk.harman.com/assets/downloads/AdobeAIR.dmg"
   name "Adobe AIR"
-  homepage "https://get.adobe.com/air/"
+  homepage "https://airsdk.harman.com/runtime"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(/version\s*(\d+(?:\.\d+)*)/i)
+  end
 
   installer script: {
     executable: "Adobe AIR Installer.app/Contents/MacOS/Adobe AIR Installer",
