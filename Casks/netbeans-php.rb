79b7eb1e80fe29f26dfd4a544f1bddc52681fea2
Markus Reiter <me@reitermark.us>
2021-04-15 13:53:13 +0200

    netbeans-php: add livecheck

--- Casks/netbeans-php.rb
+++ Casks/netbeans-php.rb
@@ -1,11 +1,16 @@
 cask "netbeans-php" do
-  version "8.2"
-  sha256 "084c34512db43a9027c0ee24bb1dc72364fc64ff6fa1b5ddeface07ba1c80c3b"
+  version "12.3"
+  sha256 "b8662b2b03b96a720c3e3189290cba95b0462df48435ba6f8ef7e8f8d9f26562"
 
   url "https://download.netbeans.org/netbeans/#{version}/final/bundles/netbeans-#{version}-php-macosx.dmg"
-  appcast "https://netbeans.org/downloads/"
   name "NetBeans IDE for PHP"
   homepage "https://netbeans.org/"
+
+  livecheck do
+    url "https://netbeans.org/downloads/"
+    strategy :page_match
+    regex(/NetBeans\s*(\d+(?:\.\d+)*)/i)
+  end
 
   pkg "NetBeans #{version}.pkg"
 
