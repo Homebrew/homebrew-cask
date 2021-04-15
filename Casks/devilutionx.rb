7f5cef1662b012092ed5d9b3cc6e49a557e58ef4
Eric Knibbe <enk3@outlook.com>
2021-04-15 04:49:59 -0400

    Add DevilutionX 1.2.0 (#103813)

---
+++ Casks/devilutionx.rb
@@ -0,0 +1,18 @@
+cask "devilutionx" do
+  version "1.2.1"
+  sha256 "763fc36ab64edbfed04bfe63afa6e4e6666bfd3d13681b9f4496cc877aaaece1"
+
+  url "https://github.com/diasurgical/devilutionX/releases/download/#{version}/devilutionx-mac-x86_64.dmg"
+  name "DevilutionX"
+  desc "Diablo build for modern operating systems"
+  homepage "https://github.com/diasurgical/devilutionX/"
+
+  depends_on macos: ">= :high_sierra"
+
+  app "devilutionx.app"
+
+  zap trash: [
+    "~/Library/Application Support/CrashReporter/devilutionX_*.plist",
+    "~/Library/Application Support/diasurgical/devilution",
+  ]
+end
