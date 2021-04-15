238e91a3b1956fed69232b6a79b5d28803638b01
Markus Reiter <me@reitermark.us>
2021-04-15 04:38:50 +0200

    medibangpaintpro: add livecheck (#103969)

--- Casks/medibangpaintpro.rb
+++ Casks/medibangpaintpro.rb
@@ -1,12 +1,17 @@
 cask "medibangpaintpro" do
-  version "26.1"
-  sha256 "7004726fa40274922fd981ad2e3cfcf67fe5d7d81073c1fca3e3640884672205"
+  version "26.2"
+  sha256 "d53fc36e02e3d9b77f3dd3c620f53452712aad4cdb6748b933e41a2b05d06a05"
 
   url "https://medibangpaint.com/static/installer/MediBangPaintPro/MediBangPaintPro-#{version}.dmg"
-  appcast "https://medibangpaint.com/en/pc/releasenote/"
   name "MediBang Paint Pro"
   desc "Create digital art and comics"
   homepage "https://medibangpaint.com/en/pc/"
+
+  livecheck do
+    url "https://medibangpaint.com/en/app-download/"
+    strategy :page_match
+    regex(%r{href=.*?/MediBangPaintPro-(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "MediBangPaintPro.app"
 
