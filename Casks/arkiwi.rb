871227fec4b96d64de75836e9302b24bc68a41d6
MarioGT <mario@mariogt.com>
2021-04-15 14:13:52 -0400

    fixed syntax error

--- Casks/arkiwi.rb
+++ Casks/arkiwi.rb
@@ -2,7 +2,7 @@ cask "arkiwi" do
   version "4.0"
   sha256 "6725544c12d8f012a7aafb832c3318ce9a04853296ef3047e9a79c9eee9113d5"
 
-  url "https://www.mariogt.com/appsChest/ArKiwi#{version.dots_to_hypens}.zip"
+  url "https://www.mariogt.com/appsChest/ArKiwi#{version.dots_to_hyphens}.zip"
   name "ArKiwi"
   desc "Blazing fast and optimized file archiver"
   homepage "https://www.mariogt.com/arkiwi.html"
