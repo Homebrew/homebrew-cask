8578134e19e6f2643a81703dd5602aac8afdbe7b
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-15 10:24:48 +0800

    Update navicat-for-sql-server.rb

--- Casks/navicat-for-sql-server.rb
+++ Casks/navicat-for-sql-server.rb
@@ -4,6 +4,7 @@ cask "navicat-for-sql-server" do
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_sqlserver_en.dmg"
   name "Navicat for SQL Server"
+  desc "Database administration and development tool for SQL-server"
   homepage "https://www.navicat.com/products/navicat-for-sqlserver"
 
   livecheck do
