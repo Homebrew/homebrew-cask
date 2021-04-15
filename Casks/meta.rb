fee94960b0684a8f21642b2fb7203b3b4355b356
Markus Reiter <me@reitermark.us>
2021-04-15 04:37:42 +0200

    meta: add livecheck (#103973)

--- Casks/meta.rb
+++ Casks/meta.rb
@@ -1,12 +1,16 @@
 cask "meta" do
-  version "1.9.7"
-  sha256 "2b77185c5fd62a79920844dc98b5db5492afa369955e343ab35ea6eb3d347f6f"
+  version "1.9.9,2761"
+  sha256 "ea08b6a91325aa0f81edddbd960978da7535b1eb116e14e81cb82e85d2b37a7c"
 
-  url "https://www.nightbirdsevolve.com/meta/updates/bin/Meta%20#{version}.zip"
-  appcast "https://www.nightbirdsevolve.com/meta/updates/"
+  url "https://www.nightbirdsevolve.com/meta/updates/bin/Meta%20#{version.before_comma}.zip"
   name "Meta"
   desc "Tag editor for digital music"
   homepage "https://www.nightbirdsevolve.com/meta/"
+
+  livecheck do
+    url "https://www.nightbirdsevolve.com/meta/updates/"
+    strategy :sparkle
+  end
 
   app "Meta.app"
 end
