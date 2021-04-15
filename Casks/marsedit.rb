9712668850544535f1d275077e3af7e2cb3b1259
Markus Reiter <me@reitermark.us>
2021-04-15 04:40:20 +0200

    marsedit: add livecheck (#103965)

--- Casks/marsedit.rb
+++ Casks/marsedit.rb
@@ -1,12 +1,16 @@
 cask "marsedit" do
-  version "4.4.12"
+  version "4.4.12,9528"
   sha256 "a8821e26ce32f8ae3836ac75cfa7c0fd491a45ca715ca58d1d68add2dfb86512"
 
-  url "https://red-sweater.com/marsedit/MarsEdit#{version}.zip"
-  appcast "https://red-sweater.com/marsedit/appcast4.php"
+  url "https://redsweater.com/marsedit/MarsEdit#{version.before_comma}.zip"
   name "MarsEdit"
   desc "Tool to write, preview and publish blogs"
-  homepage "https://red-sweater.com/marsedit/"
+  homepage "https://redsweater.com/marsedit/"
+
+  livecheck do
+    url "https://redsweater.com/marsedit/appcast#{version.major}.php"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :sierra"
 
