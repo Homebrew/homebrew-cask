2cbd3a04c8237ee4755a87b74228133e11c04c0a
Markus Reiter <me@reitermark.us>
2021-04-15 04:29:28 +0200

    jetbrains-toolbox: add livecheck (#103995)

--- Casks/jetbrains-toolbox.rb
+++ Casks/jetbrains-toolbox.rb
@@ -1,17 +1,26 @@
 cask "jetbrains-toolbox" do
-  version "1.20.7940"
+  version "1.20,1.20.7940"
 
   if Hardware::CPU.intel?
-    url "https://download.jetbrains.com/toolbox/jetbrains-toolbox-#{version}.dmg"
+    url "https://download.jetbrains.com/toolbox/jetbrains-toolbox-#{version.after_comma}.dmg"
     sha256 "9d8b0644b182cf5266836eb0506ac7b03199dc1f45eaf5e0d4c0d48d891ea79e"
   else
-    url "https://download.jetbrains.com/toolbox/jetbrains-toolbox-#{version}-arm64.dmg"
+    url "https://download.jetbrains.com/toolbox/jetbrains-toolbox-#{version.after_comma}-arm64.dmg"
     sha256 "550cc384e497cc3fc54c3e8864c81726696577a8c5c5aef263f9cdb66ebde15a"
   end
-  appcast "https://data.services.jetbrains.com/products/releases?code=TBA&latest=true&type=release"
+
   name "JetBrains Toolbox"
   desc "JetBrains tools manager"
   homepage "https://www.jetbrains.com/toolbox/app/"
+
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=TBA&latest=true&type=release"
+    strategy :page_match do |page|
+      version = page.match(/"version":"(\d+(?:\.\d+)*)/i)
+      build = page.match(/"build":"(\d+(?:\.\d+)*)/i)
+      "#{version[1]},#{build[1]}"
+    end
+  end
 
   auto_updates true
 
