5eafb2a8be3f5066dd83b92c704c395ab27c47bd
Markus Reiter <me@reitermark.us>
2021-04-15 07:23:50 +0200

    missive: add livecheck (#103978)
    * missive: add livecheck
    
    * Update missive.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/missive.rb
+++ Casks/missive.rb
@@ -1,12 +1,17 @@
 cask "missive" do
-  version "9.45.0"
-  sha256 "5cf291c7b592c8fe02c702412ec788ec722a16ef4b60fc50e55d8cf9d72b8531"
+  version "9.55.1"
+  sha256 "6ae084e4813ac19e249af9af4db478e0181a11be448e9640794573728e05c9d0"
 
   url "https://d1weoh55n7oehu.cloudfront.net/#{version}/Missive-#{version}.dmg",
       verified: "d1weoh55n7oehu.cloudfront.net/"
-  appcast "https://download.missiveapp.com/electron-updater/stable/darwin/latest-mac.yml"
-  name "missive"
+  name "Missive"
+  desc "Team inbox and chat tool"
   homepage "https://missiveapp.com/"
+
+  livecheck do
+    url "https://download.missiveapp.com/electron-updater/stable/darwin/latest-mac.yml"
+    strategy :electron_builder
+  end
 
   app "Missive.app"
 end
