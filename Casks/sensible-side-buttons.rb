5f003d5087d4055e569bd4dd73bb4960eed0c3f1
Scott Robertson <scottrobertson@users.noreply.github.com>
2021-04-15 14:40:23 +0100

    Update sensible-side-buttons.rb

--- Casks/sensible-side-buttons.rb
+++ Casks/sensible-side-buttons.rb
@@ -2,11 +2,10 @@ cask "sensible-side-buttons" do
   version "1.0.6"
   sha256 "1f2b3aefc47ac1ff8ce1e83af3ddab814dd7c6e6b974b73dce3694ec7435881b"
   url "https://github.com/archagon/sensible-side-buttons/releases/download/#{version}/SensibleSideButtons-#{version}.dmg"
-
   appcast "https://github.com/archagon/sensible-side-buttons/releases.atom"
   name "SensibleSideButtons"
-  desc "Actually functional side navigation buttons on your third-party mice in macOS."
-  homepage "https://sensible-side-buttons.archagon.net"
+  desc "Actually functional side navigation buttons on your third-party mice"
+  homepage "https://sensible-side-buttons.archagon.net/"
 
   depends_on macos: ">= :big_sur"
 
