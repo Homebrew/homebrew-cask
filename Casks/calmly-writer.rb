e4486e07033f71d26668585483cb713659d846ef
Joe Carpinelli <jdcarpinelli@gmail.com>
2021-04-15 16:24:02 -0400

    New desc, adding zap

--- Casks/calmly-writer.rb
+++ Casks/calmly-writer.rb
@@ -1,13 +1,24 @@
 cask "calmly-writer" do
   version "2.0.25"
-  sha256 :no_check
+  sha256 "92223bc82ca7af4718f41097edfa85da724f59b87b1a75d01a92db995325d48e"
 
   url "https://www.calmlywriter.com/releases/arm64/Calmly%20Writer-#{version}.pkg"
   name "Calmly Writer"
-  desc "Editor designed to focus on what you want to tell"
+  desc "Simple word processor with markdown formatting and select themes"
   homepage "https://calmlywriter.com/"
+
+  livecheck do
+    url "https://calmlywriter.com/releases/x64/download.php"
+    strategy :header_match
+  end
 
   pkg "Calmly Writer-#{version}.pkg"
 
   uninstall pkgutil: "Calmly Writer-#{version}.pkg"
+
+  zap trash: [
+    "~/Library/Preferences/calmlywriter.plist",
+    "~/Library/Saved Application State/calmlywriter.savedState",
+  ]
+  
 end
