2ae86957836f4aaf606dd17e36f4489ea2905513
Markus Reiter <me@reitermark.us>
2021-04-15 08:01:47 +0200

    jgrasp: add livecheck (#103899)
    * jgrasp: add livecheck
    
    * Update jgrasp.rb
    
    * Update jgrasp.rb
    
    * Update jgrasp.rb
    
    * Update jgrasp.rb
    
    * Update jgrasp.rb
    
    * Update jgrasp.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/jgrasp.rb
+++ Casks/jgrasp.rb
@@ -3,9 +3,15 @@ cask "jgrasp" do
   sha256 "0ffec1427ec0587f29f1d7765382be42c1732c4da99b9b46504adc260884f124"
 
   url "https://jgrasp.org/dl4g/jgrasp/jgrasp#{version.no_dots}.pkg"
-  appcast "https://jgrasp.org/"
   name "jgrasp"
+  desc "IDE with visualizations for improving software comprehensibility"
   homepage "https://jgrasp.org/"
+
+  livecheck do
+    url "https://spider.eng.auburn.edu/user-cgi/grasp/grasp.pl?;dl=download_jgrasp.html"
+    strategy :page_match
+    regex(/jGRASP\s*(\d+(?:\.\d+)*_\d+)\s(?!Beta)/i)
+  end
 
   pkg "jgrasp#{version.no_dots}.pkg"
 
