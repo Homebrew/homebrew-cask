c25a5ef44ab6288da8c6fa6e5b30a2dcf739f6fc
Markus Reiter <me@reitermark.us>
2021-04-15 07:37:20 +0200

    macdive: add livecheck (#103955)
    * macdive: add livecheck
    
    * Update macdive.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/macdive.rb
+++ Casks/macdive.rb
@@ -3,9 +3,14 @@ cask "macdive" do
   sha256 "8d2522e2cacfcd416095153f7bc930bba12d3de6803db2e63d6cfc2672367ef7"
 
   url "https://www.mac-dive.com/downloads/MacDive_#{version}.dmg"
-  appcast "https://www.mac-dive.com/shimmer/?appcast&appName=MacDive"
   name "MacDive"
+  desc "Digital dive log"
   homepage "https://www.mac-dive.com/"
+
+  livecheck do
+    url "https://www.mac-dive.com/shimmer/?appcast&appName=MacDive"
+    strategy :sparkle
+  end
 
   app "MacDive.app"
 end
