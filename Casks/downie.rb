3e86e9f167f11b000905e4071e64d2e319f30c0b
ZepDonald <63632671+ZepDonald@users.noreply.github.com>
2021-04-16 18:20:08 +0200

    Update downie from 4.2.2,4235 to 4.2.3,4238 (#104107)

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,6 +1,6 @@
 cask "downie" do
-  version "4.2.2,4235"
-  sha256 "a30d4f12b20c6995d7b5d73e1e5bd4e0b991a94fb8c6fc5c8f11ebd1fcd3434c"
+  version "4.2.3,4238"
+  sha256 "9fb1a54c78caeb939d9e1b3959476b315a7c208355aba5d493f88a0e00d001dc"
 
   url "https://charliemonroesoftware.com/trial/downie/v#{version.major}/Downie_#{version.major}_#{version.after_comma}.dmg",
       verified: "charliemonroesoftware.com/"
