a807cb820fb2c4ae19b45c72ad7e1f591a5ee20b
Joe Carpinelli <jdcarpinelli@gmail.com>
2021-04-16 14:34:36 -0400

    Removing whitespace

--- Casks/calmly-writer.rb
+++ Casks/calmly-writer.rb
@@ -3,7 +3,7 @@ cask "calmly-writer" do
 
   if Hardware::CPU.intel?
     sha256 "269dffa6f93deb8116c865001e8862b0d97ff9246269ca29609aba5a0102a520"
-    
+
     url "https://www.calmlywriter.com/releases/x64/Calmly%20Writer-#{version}.pkg"
   else
     sha256 "92223bc82ca7af4718f41097edfa85da724f59b87b1a75d01a92db995325d48e"
