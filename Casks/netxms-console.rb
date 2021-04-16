25f142d5bdc3621ea5f8a39b5275d7333c0678ab
Markus Reiter <me@reitermark.us>
2021-04-16 03:44:12 +0200

    netxms-console: add livecheck (#104032)
    * netxms-console: add livecheck
    
    * Update netxms-console.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/netxms-console.rb
+++ Casks/netxms-console.rb
@@ -1,11 +1,17 @@
 cask "netxms-console" do
-  version "3.8.226"
-  sha256 "d91e42a83b3f5943a35edf32b1e995e05b2d7dd63ddb17a095a28de64ef28924"
+  version "3.8.262"
+  sha256 "3f3d11266c76829702b5585f91c3f7127e59133b5c0415c4af155a06497b6d62"
 
   url "https://netxms.org/download/releases/#{version.major_minor}/nxmc-#{version}.dmg"
-  appcast "https://netxms.org/download/releases/#{version.major_minor}/"
   name "NetXMS Management Console"
+  desc "Network and infrastructure monitoring and management system"
   homepage "https://netxms.org/"
+
+  livecheck do
+    url "https://netxms.org/download"
+    strategy :page_match
+    regex(%r{href=.*?/nxmc-(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "NetXMS Console (#{version}).app"
 
