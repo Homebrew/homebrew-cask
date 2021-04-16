99bace2e8d6c8a62d644f3e6e119ea37001bc3dd
Markus Reiter <me@reitermark.us>
2021-04-16 04:03:02 +0200

    omnidazzle: add livecheck (#104047)
    * omnidazzle: add livecheck
    
    * Update omnidazzle.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/omnidazzle.rb
+++ Casks/omnidazzle.rb
@@ -3,9 +3,14 @@ cask "omnidazzle" do
   sha256 "47dfafb0740eeb164832804bd9ccf898f722057f919cd59197e04f9263f3a175"
 
   url "https://downloads.omnigroup.com/software/MacOSX/10.6/OmniDazzle-#{version}.dmg"
-  appcast "https://update.omnigroup.com/appcast/com.omnigroup.OmniDazzle"
   name "OmniDazzle"
+  desc "Set of plug-ins to highlight areas of your screen and your mouse pointer"
   homepage "https://support.omnigroup.com/omnidazzle-troubleshooting/"
+
+  livecheck do
+    url "https://update.omnigroup.com/appcast/com.omnigroup.OmniDazzle"
+    strategy :sparkle
+  end
 
   app "OmniDazzle.app"
 
