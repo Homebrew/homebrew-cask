9ac0cee73812249762be252baa3d9ecbd626fe53
Markus Reiter <me@reitermark.us>
2021-04-16 07:33:49 +0200

    netbeans-php: add livecheck (#104028)
    * netbeans-php: add livecheck
    
    * Update netbeans-php.rb
    
    * Update netbeans-php.rb
    
    * Update netbeans-php.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/netbeans-php.rb
+++ Casks/netbeans-php.rb
@@ -1,13 +1,27 @@
 cask "netbeans-php" do
-  version "8.2"
-  sha256 "084c34512db43a9027c0ee24bb1dc72364fc64ff6fa1b5ddeface07ba1c80c3b"
+  version "12.3"
+  sha256 "a34c046619b69e5b13a2618d12336b97bcb404424880fe5d0c4c10ed100da6ba"
 
-  url "https://download.netbeans.org/netbeans/#{version}/final/bundles/netbeans-#{version}-php-macosx.dmg"
-  appcast "https://netbeans.org/downloads/"
+  url "https://downloads.apache.org/netbeans/netbeans/#{version}/Apache-NetBeans-#{version}-bin-macosx.dmg",
+      verified: "downloads.apache.org/netbeans/netbeans/"
   name "NetBeans IDE for PHP"
+  desc "Development environment, tooling platform and application framework"
   homepage "https://netbeans.org/"
 
-  pkg "NetBeans #{version}.pkg"
+  livecheck do
+    url "https://netbeans.org/downloads/"
+    strategy :page_match
+    regex(/NetBeans\s*(\d+(?:\.\d+)*)/i)
+  end
 
-  uninstall delete: "/Applications/NetBeans"
+  pkg "Apache NetBeans #{version}.pkg"
+
+  uninstall pkgutil: [
+    "org.netbeans.ide.javaee.#{version}",
+    "org.netbeans.ide.javase.#{version}",
+    "org.netbeans.ide.nbide.#{version}",
+    "org.netbeans.ide.php.#{version}",
+    "org.netbeans.ide.webcommon.#{version}",
+  ],
+            delete:  "/Applications/NetBeans"
 end
