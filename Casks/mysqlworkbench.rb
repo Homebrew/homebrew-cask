Homebrew/homebrew-cask

commit 8a77e2b49f03de018d0324c0e6f45a40a83efb1d

mysqlworkbench: add livecheck (#103997)
* mysqlworkbench: add livecheck

* Update mysqlworkbench.rb

* Update mysqlworkbench.rb

Casks/mysqlworkbench.rb
@@ -8,23 +8,24 @@ cask "mysqlworkbench" do
     sha256 "3478800290e2797d294e3721fdaea4c41ddc1917f2b59ec94a935e16c18dc5d2"
     url "https://downloads.mysql.com/archives/get/p/#{version.major}/file/mysql-workbench-community-#{version}-macos-x86_64.dmg"
   elsif MacOS.version <= :mojave
-    version "8.0.21"
-    sha256 "7d812551cc1cc38e1d5f588e6c91b07f1778c78a04bfe94dafac3a23ea425e88"
-    url "https://downloads.mysql.com/archives/get/p/#{version.major}/file/mysql-workbench-community-#{version}-macos-x86_64.dmg"
-  elsif MacOS.version <= :catalina
     version "8.0.22"
-    sha256 "4e27de82d869043cf80e803f1a57cc041a91cabddf0aa6a6c054d68af1837d48"
+    sha256 "7d812551cc1cc38e1d5f588e6c91b07f1778c78a04bfe94dafac3a23ea425e88"
     url "https://downloads.mysql.com/archives/get/p/#{version.major}/file/mysql-workbench-community-#{version}-macos-x86_64.dmg"
   else
     version "8.0.23"
     sha256 "4c8664f5686a449a9760bda9b85d7e8c6beb1367d35f668048ffe534652da7b3"
     url "https://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-#{version}-macos-x86_64.dmg"
-    appcast "https://dev.mysql.com/downloads/workbench/"
   end
 
   name "MySQL Workbench"
   desc "Visual tool to design, develop and administer MySQL servers"
   homepage "https://www.mysql.com/products/workbench/"
+
+  livecheck do
+    url "https://dev.mysql.com/downloads/workbench/"
+    strategy :page_match
+    regex(/MySQL\s*Workbench\s*(\d+(?:\.\d+)*)/i)
+  end
 
   app "MySQLWorkbench.app"
 

commit d16cc8e1af1843bdb9b5ff6ec551bc0f60b7b006

Update mysqlworkbench 8.0.22 sha256sum
Update the mysqlworkbench 8.0.22 sha256 checksum.

I double checked the sha256 checksum of the actual DMG file being downloaded.

This issue was probably caused in commit 8a77e2b49f03de018d0324c0e6f45a40a83efb1d when things were readjusted, 
and the checksum wasn't updated for the version change.

Casks/mysqlworkbench.rb
@@ -9,7 +9,7 @@ cask "mysqlworkbench" do
     url "https://downloads.mysql.com/archives/get/p/#{version.major}/file/mysql-workbench-community-#{version}-macos-x86_64.dmg"
   elsif MacOS.version <= :mojave
     version "8.0.22"
-    sha256 "7d812551cc1cc38e1d5f588e6c91b07f1778c78a04bfe94dafac3a23ea425e88"
+    sha256 "4e27de82d869043cf80e803f1a57cc041a91cabddf0aa6a6c054d68af1837d48"
     url "https://downloads.mysql.com/archives/get/p/#{version.major}/file/mysql-workbench-community-#{version}-macos-x86_64.dmg"
   else
     version "8.0.23"