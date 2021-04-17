91b6608876cf700238ef6775a6b6cf997136fe4b
Markus Reiter <me@reitermark.us>
2021-04-17 05:03:14 +0200

    proxifier: add livecheck

--- Casks/proxifier.rb
+++ Casks/proxifier.rb
@@ -4,14 +4,21 @@ cask "proxifier" do
     sha256 :no_check
     url "https://www.proxifier.com/download/ProxifierMac.dmg"
 
-    appcast "https://www.proxifier.com/changelog/mac#{version.major}.html",
-            must_contain: version.major_minor
+    livecheck do
+      url "https://www.proxifier.com/changelog/mac#{version.major}.html"
+      strategy :page_match
+      regex(/Version\s*(\d+(?:\.\d+)*)/i)
+    end
   else
-    version "3.4.1"
+    version "3.4"
     sha256 "e06ad22b11416add6f94d66c28f101453a8a66a0a1f12ebd668e6744dca63279"
     url "https://www.proxifier.com/download/ProxifierMac#{version.major}.dmg"
-    appcast "https://www.proxifier.com/changelog/mac.html",
-            must_contain: version.major_minor
+
+    livecheck do
+      url "https://www.proxifier.com/changelog/mac.html"
+      strategy :page_match
+      regex(/Version\s*(\d+(?:\.\d+)*)/i)
+    end
   end
 
   name "Proxifier"
