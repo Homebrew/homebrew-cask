449309d3ed21e419dca5cf98fd6a2df643f4170c
Beni Ben Zikry <bbenzikry@gmail.com>
2021-04-17 15:31:25 +0300

    Add ILSpy.app v5.0-rc2

---
+++ Casks/ilspy.rb
@@ -0,0 +1,18 @@
+cask "ilspy" do
+  version "v5.0-rc2"
+  sha256 "219a657bb617603b35386500d2dc1994e38e3e2cf16c0e138bd58afa786df965"
+
+  url "https://github.com/icsharpcode/AvaloniaILSpy/releases/download/#{version}/ILSpy-osx-x64-Release.zip"
+  name "ILSpy"
+  desc "Cross-platform version of  built with Avalonia"
+  homepage "https://github.com/icsharpcode/AvaloniaILSpy"
+
+  livecheck do
+    url "https://github.com/icsharpcode/AvaloniaILSpy/releases.atom"
+    strategy :sparkle
+  end
+
+  depends_on arch: :intel
+
+  app "ILSpy.app"
+end
