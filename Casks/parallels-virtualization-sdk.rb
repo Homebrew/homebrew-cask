f7bef2bf6318d30c9f1acfcc6510de580b8c5e77
Manuel Grandeit <m.grandeit@gmail.com>
2021-04-17 21:40:31 +0200

    Update parallels-virtualization-sdk to 16.5.0

--- Casks/parallels-virtualization-sdk.rb
+++ Casks/parallels-virtualization-sdk.rb
@@ -1,11 +1,26 @@
 cask "parallels-virtualization-sdk" do
-  version "16.1.3-49160"
-  sha256 "bbf885c30c59919733dd197a8382ed5f6ed92945badbda23f1f7ac20926c9846"
+  if Hardware::CPU.arm?
+    version "16.5.0-50692"
+    sha256 "3a796228f1ea27f6261a56af89342e1e3292194628e0dd4042d296eb83f0f18e"
+  else
+    version "16.5.0-49183"
+    sha256 "5d4bcf6d6f0e341c5f33380f8601170c9f41b05a0f720050622bfc979e017e79"
+  end
 
   url "https://download.parallels.com/desktop/v#{version.major}/#{version}/ParallelsVirtualizationSDK-#{version}-mac.dmg"
   name "Parallels Virtualization SDK"
   desc "Desktop virtualization development kit"
   homepage "https://www.parallels.com/products/desktop/download/"
+
+  livecheck do
+    if Hardware::CPU.arm?
+      url "https://www.parallels.com/download/pvsdk/m1/"
+    else
+      url "https://www.parallels.com/download/pvsdk/intel/"
+    end
+    regex(/.*?parallelsvirtualizationsdk-?(\d+(?:\.\d+)+-\d+)(?:-mac)?\.d/i)
+    strategy :header_match
+  end
 
   pkg "Parallels Virtualization SDK.pkg"
 
