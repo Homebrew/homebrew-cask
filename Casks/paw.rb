4a2802f0efc62b35727884294b63b2d048ceaf0d
Markus Reiter <me@reitermark.us>
2021-04-17 13:47:54 +0200

    paw: add livecheck (#104127)

--- Casks/paw.rb
+++ Casks/paw.rb
@@ -1,12 +1,16 @@
 cask "paw" do
-  version "3.2.2"
+  version "3.2.2,3002002001"
   sha256 "2cae45407bf251840f10920cb76acb6127a0d4eebe642f7087aae47b0d56d9ec"
 
-  url "https://cdn-builds.paw.cloud/paw/Paw-#{version}.zip"
-  appcast "https://paw.cloud/api/v2/updates/appcast"
+  url "https://cdn-builds.paw.cloud/paw/Paw-#{version.before_comma}.zip"
   name "Paw"
   desc "HTTP client that helps testing and describing APIs"
   homepage "https://paw.cloud/"
+
+  livecheck do
+    url "https://paw.cloud/api/v2/updates/appcast"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :sierra"
