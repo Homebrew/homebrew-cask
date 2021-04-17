ff36053b82c37d116923bdde931373fa5e1658d3
Michael Cho <cho-m@tuta.io>
2021-04-16 17:06:08 -0700

    birdfont: update to 4.15.3, 4.15.2 (catalina), and 4.15.0 (older) (#104090)

--- Casks/birdfont.rb
+++ Casks/birdfont.rb
@@ -1,13 +1,19 @@
 cask "birdfont" do
   if MacOS.version <= :mojave
-    version "4.8.9"
-    sha256 "0f2e4f5398e0c9ad90f13c3e18910686d34f29e6e49b12c771ddb3e2f03d8589"
+    version "4.15.0"
+    sha256 "5433ee7bdbe68192bf69a253ce9c248cff844032ece43a2bbc910431c62b1a3f"
   elsif MacOS.version <= :catalina
-    version "4.8.10"
-    sha256 "067755cd2e02a70997aac3cb29a49ab2ec1c304c30d89f58b96add0df7802c88"
+    version "4.15.2"
+    sha256 "f5a34569ed38f293fe4902930b722649f3f1914c3fc9077de20482634a65cf01"
+
+    livecheck do
+      url "https://birdfont.org/purchase.php"
+      strategy :page_match
+      regex(%r{Mac\s*OS\s*10\.15.*?/birdfont-(\d+(?:\.\d+)*)-free\.dmg}i)
+    end
   else
-    version "4.14.3"
-    sha256 "7928073acf20b2b199e48154f53bf188a8b6553480a65f3cc1a49c2337ed2efe"
+    version "4.15.3"
+    sha256 "f36042a339a851aca029d7afa6dcbfb6e56efec94db925bca952d556055a07f7"
 
     livecheck do
       url "https://birdfont.org/purchase.php"
@@ -21,7 +27,7 @@ cask "birdfont" do
   desc "Free font editor"
   homepage "https://birdfont.org/"
 
-  depends_on macos: ">= :el_capitan"
+  depends_on macos: ">= :sierra"
 
   app "BirdFontNonCommercial.app"
 end
