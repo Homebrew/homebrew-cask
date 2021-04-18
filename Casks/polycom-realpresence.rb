5794695f63bee625db03580486f9754a375cb300
Markus Reiter <me@reitermark.us>
2021-04-19 01:17:50 +0200

    polycom-realpresence: add livecheck (#104143)

--- Casks/polycom-realpresence.rb
+++ Casks/polycom-realpresence.rb
@@ -1,12 +1,18 @@
 cask "polycom-realpresence" do
-  version "3.10.6.73302"
-  sha256 "3e61c69a4a080f16e5ab7bb303d2f7244c1e551f7916b723191a07ee5927f150"
+  version "3.11.2.73443"
+  sha256 "1724c702fe7424846da5e1052ed9465908888324018e52d048be007e77611b26"
 
   url "https://downloads.polycom.com/video/realpresence_desktop/RPDesktop_#{version.dots_to_underscores}.dmg"
-  appcast "https://support.polycom.com/content/support/north-america/usa/en/support/video/realpresence-desktop/realpresence-desktop.html",
-          must_contain: version.dots_to_underscores
   name "Polycom RealPresence Desktop"
   homepage "https://www.polycom.com/products-services/hd-telepresence-video-conferencing/realpresence-desktop/realpresence-desktop.html"
+
+  livecheck do
+    url "https://support.polycom.com/content/support/north-america/usa/en/support/video/realpresence-desktop/realpresence-desktop.html"
+    strategy :page_match do |page|
+      v = page[%r{href=.*?/RPDesktop_(\d+(?:_\d+)*)\.dmg}i, 1]
+      v.tr("_", ".")
+    end
+  end
 
   pkg "RealPresenceDesktop.pkg"
 
