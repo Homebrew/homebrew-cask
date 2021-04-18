7473fe2dd98bac8569ad9796ba593137246c7eaf
Yevhenii Havrylko <egavrilko@gmail.com>
2021-04-18 04:12:14 +0300

    Update ghidra from 9.2.2,20201229 to 9.2.3,20210325, add livecheck (#104182)

--- Casks/ghidra.rb
+++ Casks/ghidra.rb
@@ -1,10 +1,18 @@
 cask "ghidra" do
-  version "9.2.2,20201229"
-  sha256 "8cf8806dd5b8b7c7826f04fad8b86fc7e07ea380eae497f3035f8c974de72cf8"
+  version "9.2.3,20210325"
+  sha256 "9019c78f8b38d68fd40908b040466974a370e26ba51b2aaeafc744171040f714"
 
   url "https://www.ghidra-sre.org/ghidra_#{version.before_comma}_PUBLIC_#{version.after_comma}.zip"
   name "Ghidra"
   homepage "https://www.ghidra-sre.org/"
+
+  livecheck do
+    url "https://ghidra-sre.org/"
+    strategy :page_match do |page|
+      match = page.match(/href=.*?ghidra[._-](\d+(?:\.\d+)*)[._-]PUBLIC[._-](\d+)\.zip/i)
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   conflicts_with cask: "homebrew/cask-versions/ghidra-beta"
 
