c71800f108864c050f7595dddc3492f632e493a8
Markus Reiter <me@reitermark.us>
2021-04-19 01:26:01 +0200

    pcoipclient: add livecheck (#104128)

--- Casks/pcoipclient.rb
+++ Casks/pcoipclient.rb
@@ -1,12 +1,17 @@
 cask "pcoipclient" do
-  version "20.10.2"
-  sha256 "b03b7a4358427011d5bdfa940c8ebe74a9115246e2dfd9667538ea6bf6c5c25d"
+  version "21.03.0"
+  sha256 "4dd5b21afccf49778b551c6b6cbf155d92b9905a0b951d86c7b4205c89cdb182"
 
   url "https://downloads.teradici.com/mac/stable/pcoip-client_#{version}.dmg"
-  appcast "https://downloads.teradici.com/mac/stable/"
   name "Teradici PCoIP Software Client for macOS"
   desc "Client for VM agents and remote workstation cards"
   homepage "https://docs.teradici.com/find/product/software-and-mobile-clients/"
+
+  livecheck do
+    url "https://downloads.teradici.com/mac/stable/"
+    strategy :page_match
+    regex(/href=.*?pcoip-client_(\d+(?:\.\d+)*)\.dmg/i)
+  end
 
   app "PCoIPClient.app"
 
