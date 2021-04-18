e1c5787d19f463497ba9df750ffc26c55ff6375a
Markus Reiter <me@reitermark.us>
2021-04-19 00:58:34 +0200

    phpstorm: add livecheck (#104133)

--- Casks/phpstorm.rb
+++ Casks/phpstorm.rb
@@ -1,18 +1,26 @@
 cask "phpstorm" do
-  version "2021.1"
+  version "2021.1.1,211.7036.8"
 
   if Hardware::CPU.intel?
-    sha256 "3e79bb5617623a7e4e9516affb320aa82c10ec10c386011db18c72585ef83565"
-    url "https://download.jetbrains.com/webide/PhpStorm-#{version}.dmg"
+    sha256 "94df67e2fb8c2c63ae5d221c4c7d129e56fd959a1b6fd1e1a97c5d23a8272d83"
+    url "https://download.jetbrains.com/webide/PhpStorm-#{version.before_comma}.dmg"
   else
     sha256 "465aa85bc86f16de6e75a056e855aeab6e6b6104e478f963f19311eede0c98da"
-    url "https://download.jetbrains.com/webide/PhpStorm-#{version}-aarch64.dmg"
+    url "https://download.jetbrains.com/webide/PhpStorm-#{version.before_comma}-aarch64.dmg"
   end
 
-  appcast "https://data.services.jetbrains.com/products/releases?code=PS&latest=true&type=release"
   name "JetBrains PhpStorm"
   desc "PHP IDE by JetBrains"
   homepage "https://www.jetbrains.com/phpstorm/"
+
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=PS&latest=true&type=release"
+    strategy :page_match do |page|
+      version = page.match(/"version":"(\d+(?:\.\d+)*)/i)
+      build = page.match(/"build":"(\d+(?:\.\d+)*)/i)
+      "#{version[1]},#{build[1]}"
+    end
+  end
 
   auto_updates true
 
