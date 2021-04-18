6d0f91fb16303bb92cc086c4f9e96b48efc570c8
Markus Reiter <me@reitermark.us>
2021-04-19 01:33:56 +0200

    panoply: add livecheck (#104122)

--- Casks/panoply.rb
+++ Casks/panoply.rb
@@ -3,10 +3,15 @@ cask "panoply" do
   sha256 "4f882d7bde7ba39abadba79470d906f941605b2ccfacdd6915376f981d56ea57"
 
   url "https://www.giss.nasa.gov/tools/panoply/download/PanoplyMacOS-#{version}.dmg"
-  appcast "https://www.giss.nasa.gov/tools/panoply/download/"
   name "Panoply netCDF, HDF and GRIB Data Viewer"
   desc "Plot geo-referenced data from netCDF, HDF, and GRIB"
   homepage "https://www.giss.nasa.gov/tools/panoply/"
+
+  livecheck do
+    url "https://www.giss.nasa.gov/tools/panoply/download/"
+    strategy :page_match
+    regex(/href=.*?PanoplyMacOS-(\d+(?:\.\d+)*)\.dmg/i)
+  end
 
   app "Panoply.app"
 
