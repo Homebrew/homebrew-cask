fb9583df34fa26ca3670f7c2049ee9cd6456b883
Markus Reiter <me@reitermark.us>
2021-04-19 01:00:49 +0200

    pragli: add livecheck (#104146)

--- Casks/pragli.rb
+++ Casks/pragli.rb
@@ -4,10 +4,14 @@ cask "pragli" do
 
   url "https://storage.googleapis.com/always-on-cdf01.appspot.com/dist/Pragli.dmg",
       verified: "storage.googleapis.com/always-on-cdf01.appspot.com/"
-  appcast "https://storage.googleapis.com/always-on-cdf01.appspot.com/dist/latest-mac.yml"
   name "Pragli"
   desc "Virtual office for remote teams"
   homepage "https://pragli.com/"
+
+  livecheck do
+    url "https://storage.googleapis.com/always-on-cdf01.appspot.com/dist/latest-mac.yml"
+    strategy :electron_builder
+  end
 
   app "Pragli.app"
 
