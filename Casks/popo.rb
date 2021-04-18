bea2007a614f23d4f05b8f686eca5dbf294247b1
Markus Reiter <me@reitermark.us>
2021-04-19 00:59:37 +0200

    popo: add livecheck (#104144)

--- Casks/popo.rb
+++ Casks/popo.rb
@@ -3,10 +3,14 @@ cask "popo" do
   sha256 "03077ae7118de4a942b486a45883f41add3e5168dc68cb82d3676550038d5cc2"
 
   url "https://popo.netease.com/file/popomac/POPO_Mac_V#{version.dots_to_underscores}.dmg"
-  appcast "http://http.popo.netease.com:8080/api/open/jsonp/check_version?device=4",
-          must_contain: version.dots_to_underscores
   name "NetEase POPO"
   homepage "https://popo.netease.com/"
+
+  livecheck do
+    url "http://http.popo.netease.com:8080/api/open/jsonp/check_version?device=4"
+    strategy :page_match
+    regex(/"version"\s*:\s*"(\d+(?:\.\d+)*)"/i)
+  end
 
   app "popo_mac.app"
 
