745b2d6cc52b81d0bd83e848420e5dd996e9f53e
Markus Reiter <me@reitermark.us>
2021-04-19 01:19:41 +0200

    openphone: add livecheck (#104117)

--- Casks/openphone.rb
+++ Casks/openphone.rb
@@ -1,12 +1,16 @@
 cask "openphone" do
-  version "2.1.3"
-  sha256 "b53d869932b66b9b12e3260cc116ac263e57a02788f2e9356b8ed46995e87c7f"
+  version "3.1.4"
+  sha256 "9c505a63ddb909e3a675b9150a9238a388e7813bb1599407301742c203756a0e"
 
   url "https://download.openphone.co/OpenPhone-#{version}.dmg"
-  appcast "https://s3-us-west-2.amazonaws.com/download.openphone.co/latest-mac.yml"
   name "OpenPhone"
   desc "Business phone for professionals, teams, and companies"
   homepage "https://www.openphone.co/"
+
+  livecheck do
+    url "https://s3-us-west-2.amazonaws.com/download.openphone.co/latest-mac.yml"
+    strategy :electron_builder
+  end
 
   app "OpenPhone.app"
 end
