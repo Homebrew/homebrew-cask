3c6b093398cd70b6a31eaedc4f5178d9cb231cca
Manuel Grandeit <m.grandeit@gmail.com>
2021-04-18 21:11:15 +0200

    Fix trailing whitespaces

--- Casks/parallels-virtualization-sdk.rb
+++ Casks/parallels-virtualization-sdk.rb
@@ -2,7 +2,7 @@ cask "parallels-virtualization-sdk" do
   if Hardware::CPU.intel?
     version "16.5.0-49183"
     sha256 "5d4bcf6d6f0e341c5f33380f8601170c9f41b05a0f720050622bfc979e017e79"
-    
+
     livecheck do
       url "https://www.parallels.com/download/pvsdk/intel/"
       regex(%r{/parallelsvirtualizationsdk-?(\d+(?:\.\d+)+-\d+)(?:-mac)?\.dmg}i)
@@ -11,7 +11,7 @@ cask "parallels-virtualization-sdk" do
   else
     version "16.5.0-50692"
     sha256 "3a796228f1ea27f6261a56af89342e1e3292194628e0dd4042d296eb83f0f18e"
-    
+
     livecheck do
       url "https://www.parallels.com/download/pvsdk/m1/"
       regex(%r{/parallelsvirtualizationsdk-?(\d+(?:\.\d+)+-\d+)(?:-mac)?\.dmg}i)
