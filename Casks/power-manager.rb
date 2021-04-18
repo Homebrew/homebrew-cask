3dac47d1ccb2d1ab7119acb3775dd8dc7d8ad00e
Markus Reiter <me@reitermark.us>
2021-04-19 00:59:55 +0200

    power-manager: add livecheck (#104145)

--- Casks/power-manager.rb
+++ Casks/power-manager.rb
@@ -1,12 +1,17 @@
 cask "power-manager" do
-  version "5.5.2"
-  sha256 "9184adaf556c3674dfc2e445c2d02c62b3d63698fe78667cfb912a60dc6b842c"
+  version "5.6.0"
+  sha256 "971c85fc63c9653d4e9357b9444c2f4116b952cda43335e65469c4dc2d920b56"
 
   url "https://www.dssw.co.uk/powermanager/dsswpowermanager-#{version.no_dots}.dmg"
-  appcast "https://version.dssw.co.uk/powermanager/"
   name "Power Manager"
   desc "Utility to automate tasks and improve power management"
   homepage "https://dssw.co.uk/powermanager/"
+
+  livecheck do
+    url "https://version.dssw.co.uk/powermanager/"
+    strategy :page_match
+    regex(/Changes\s*in\s*v(\d+(?:\.\d+)*)/i)
+  end
 
   auto_updates true
   depends_on macos: ">= :sierra"
