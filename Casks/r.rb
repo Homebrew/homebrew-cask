8370d1b3db761fa3b33582cd58c835046cadad2c
Markus Reiter <me@reitermark.us>
2021-04-20 00:46:36 +0200

    r: add livecheck (#104371)

--- Casks/r.rb
+++ Casks/r.rb
@@ -3,10 +3,15 @@ cask "r" do
   sha256 "0143dd1b17dd5d8bccb9e13d361f69c2dd69e1c8289e3fb0271daef1615c57bd"
 
   url "https://cloud.r-project.org/bin/macosx/R-#{version}.pkg"
-  appcast "https://cloud.r-project.org/bin/macosx/"
   name "R"
   desc "Environment for statistical computing and graphics"
   homepage "https://www.r-project.org/"
+
+  livecheck do
+    url "https://cloud.r-project.org/bin/macosx/"
+    strategy :page_match
+    regex(/href=.*?R-(\d+(?:\.\d+)*)\.pkg/i)
+  end
 
   depends_on macos: ">= :el_capitan"
 
