5f6272d85e592607b4bb6bb4371ae960ec08b3a4
Markus Reiter <me@reitermark.us>
2021-04-19 18:32:16 +0200

    simpholders: add livecheck (#104246)

--- Casks/simpholders.rb
+++ Casks/simpholders.rb
@@ -1,13 +1,20 @@
 cask "simpholders" do
-  version "3.0.11,2350"
-  sha256 "9a58616f4584fc44c4dd6628a84b5f5ac7e45fe45691e503117ceafde4f7d6f6"
+  version "3.0.12,2366"
+  sha256 "0f74633bd6bd9723063af0be550364882b93fbb5cd67bac6e2066083174e5f0d"
 
-  url "https://simpholders.com/site/assets/files/#{version.after_comma}/simpholders_#{version.before_comma.dots_to_underscores}_dmg.dmg"
-  appcast "https://simpholders.com/releases/"
+  url "https://simpholders.com/site/assets/files/#{version.after_comma}/simpholders_#{version.before_comma.dots_to_underscores}.dmg"
   name "SimPholders"
   homepage "https://simpholders.com/"
 
+  livecheck do
+    url "https://simpholders.com/latest/"
+    strategy :header_match do |headers|
+      match = headers["location"].match(%r{/(\d+)/simpholders_(\d+(?:_\d+)*).dmg}i)
+      "#{match[2].tr("_", ".")},#{match[1]}"
+    end
+  end
+
   depends_on macos: ">= :catalina"
 
-  app "SimPholders.app"
+  app "simpholders_#{version.before_comma.dots_to_underscores}.app", target: "SimPholders.app"
 end
