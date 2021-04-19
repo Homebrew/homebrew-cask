ab010ff8801b2f21109c26f9321e5f0d74f6ae96
Josh Nichols <technicalpickles@github.com>
2021-04-19 10:23:53 -0400

    Apply some things from old formula. Fix URL.

--- Casks/hook.rb
+++ Casks/hook.rb
@@ -2,12 +2,10 @@ cask "hook" do
   version "2.2.2,2021-03"
   sha256 "75acc69d06d68082d5fad6679b57e0a06e46dddffef921e6a02cabb6221e233e"
 
-  url "https://hookproductivity.com/wp-content/uploads/2021/03/Hook-productivity-app-#{version}.dmg"
+  url "https://hookproductivity.com/wp-content/uploads/#{version.after_comma.tr("-", "/")}/Hook-productivity-app-#{version.before_comma}.dmg"
   name "Hook"
   desc "Link & retrieve key information"
   homepage "https://hookproductivity.com/"
-
-  app "Hook.app"
   
   livecheck do
     url :homepage
@@ -15,6 +13,18 @@ cask "hook" do
       match = page.match(%r{href=.*?/(\d+)/(\d+)/Hook-productivity-app-(\d+(?:\.\d+)*)\.dmg}i)
       "#{match[3]},#{match[1]}-#{match[2]}"
   end
+    
+  auto_updates true
 
-  zap trash: "~/Library/Preferences/com.cogsciapps.hook.plist"
+  app 'Hook.app'
+
+  uninstall launchctl: 'com.cogsciapps.hookautolaunchhelper',
+            quit:      'com.cogsciapps.hook'
+
+  zap trash:
+             [
+               '~/Library/Caches/com.cogsciapps.hook',
+               '~/Library/Logs/com.cogsciapps.hook',
+               '~/Library/Preferences/com.cogsciapps.hook.plist',
+             ]
 end
