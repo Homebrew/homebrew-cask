c560f58a9de7b1362bfccb47bfd4b45fa769e8e8
adithyasunil26 <adithyasunil26@gmail.com>
2021-04-19 23:49:26 +0400

    Update air-video-server-hd from 2.3.0-beta1u1,202.0902 to 1.0.15,100.15

--- Casks/air-video-server-hd.rb
+++ Casks/air-video-server-hd.rb
@@ -1,6 +1,6 @@
 cask "air-video-server-hd" do
-  version "2.3.0-beta1u1,202.0902"
-  sha256 "1e45abf868349f1ad43b6f3a1234a254d31e943ef5c7d72b84eff9f955a15a33"
+  version "1.0.15,100.15"
+  sha256 "06fa8089a3cc7ba7e6386aff9bab5f5827fc4add47ff4021e00d82b1d7a432b8"
 
   url "https://s3.amazonaws.com/AirVideoHD/Download/Air+Video+Server+HD+#{version.before_comma}.dmg",
       verified: "s3.amazonaws.com/AirVideoHD/"
