29ae0fb0c074f3f8a748a1107d95b4211cfde850
Markus Reiter <me@reitermark.us>
2021-04-19 06:12:13 +0200

    segger-embedded-studio-for-arm: add livecheck (#104231)

--- Casks/segger-embedded-studio-for-arm.rb
+++ Casks/segger-embedded-studio-for-arm.rb
@@ -3,9 +3,14 @@ cask "segger-embedded-studio-for-arm" do
   sha256 "7f960f0c0d30c1796f02f313e49989af1b68ba21bb9e99e1de6731b1308f249a"
 
   url "https://www.segger.com/downloads/embedded-studio/Setup_EmbeddedStudio_ARM_v#{version.no_dots}_macos_x64.dmg"
-  appcast "https://studio.segger.com/arm_segger_studio_release_notes.htm"
   name "SEGGER Embedded Studio for ARM"
   homepage "https://www.segger.com/products/development-tools/embedded-studio"
+
+  livecheck do
+    url "https://studio.segger.com/arm_segger_studio_release_notes.htm"
+    strategy :page_match
+    regex(/<h2[^>]*>\s*Version\s*(\d+(?:\.\d+)*[a-z]?)/i)
+  end
 
   pkg "Install SEGGER Embedded Studio for ARM #{version}.pkg"
 
