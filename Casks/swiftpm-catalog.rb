3ee4b2add1a0ca813e7818ff52c4b3f50b140c41
Markus Reiter <me@reitermark.us>
2021-04-19 18:11:56 +0200

    swiftpm-catalog: add livecheck (#104271)

--- Casks/swiftpm-catalog.rb
+++ Casks/swiftpm-catalog.rb
@@ -1,11 +1,18 @@
 cask "swiftpm-catalog" do
-  version "1.1.4-v22"
+  version "1.1.4,22"
   sha256 "b1434d32eca36c664a5b75e5f8601f0a53e02d86eec9b25f25b475618aa5c9e9"
 
-  url "https://zeezide.com/en/products/swiftpmcatalog/archives/SwiftPM%20Catalog.app-#{version}.zip"
-  appcast "https://zeezide.com/en/products/swiftpmcatalog/"
+  url "https://zeezide.com/en/products/swiftpmcatalog/archives/SwiftPM%20Catalog.app-#{version.before_comma}-v#{version.after_comma}.zip"
   name "SwiftPM Catalog"
   homepage "https://zeezide.com/en/products/swiftpmcatalog/"
+
+  livecheck do
+    url "https://zeezide.com/en/products/swiftpmcatalog/"
+    strategy :page_match do |page|
+      match = page.match(%r{href=.*?/SwiftPM\s*Catalog\.app-(\d+(?:\.\d+)*)-v(\d+)\.zip}i)
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   depends_on macos: ">= :mojave"
 
