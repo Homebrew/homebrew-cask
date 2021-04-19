7447698213ccdebea32bc9fe8e066ca49a7188a5
Markus Reiter <me@reitermark.us>
2021-04-19 05:51:37 +0200

    runway: add livecheck (#104226)

--- Casks/runway.rb
+++ Casks/runway.rb
@@ -1,12 +1,16 @@
 cask "runway" do
-  version "0.17.0"
-  sha256 "0f48d55b5fa184d9513fe7dbaddb4747c1dad2c810e295e6d6dd7aa7467279b1"
+  version "0.17.6"
+  sha256 "02596e42ca7db9481bb9d48fb0be22652329cc41b789b097778f430f8cf5f79a"
 
   url "https://runway-releases.s3.amazonaws.com/Runway-#{version}.dmg",
       verified: "runway-releases.s3.amazonaws.com/"
-  appcast "https://runway-releases.s3.amazonaws.com/latest-mac.yml"
   name "Runway"
   homepage "https://runwayml.com/"
+
+  livecheck do
+    url "https://runway-releases.s3.amazonaws.com/latest-mac.yml"
+    strategy :electron_builder
+  end
 
   app "Runway.app"
 end
