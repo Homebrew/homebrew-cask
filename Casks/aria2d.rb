943b0fd0220ca67a9f3fd5dab0028ac6740929ac
adithyasunil26 <adithyasunil26@gmail.com>
2021-04-19 23:51:57 +0400

    Update aria2d from 1.3.6,509 to 1.3.3,469

--- Casks/aria2d.rb
+++ Casks/aria2d.rb
@@ -1,6 +1,6 @@
 cask "aria2d" do
-  version "1.3.6,509"
-  sha256 "1840ce5b25571925cfec67635e3e48ce38306b856b509861f2ecfdfd0470cd7b"
+  version "1.3.3,469"
+  sha256 "14e649369bac231edb8b4374c0de142297027a5de0e04c80f4dbfcaf634af216"
 
   url "https://raw.githubusercontent.com/xjbeta/AppUpdaterAppcasts/master/Aria2D/Aria2D%20#{version.before_comma}.dmg",
       verified: "githubusercontent.com/xjbeta/"
