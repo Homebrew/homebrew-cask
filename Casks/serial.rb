70b47e164fcb980f872327cce0c12fa67b873a04
Markus Reiter <me@reitermark.us>
2021-04-19 06:13:32 +0200

    serial: add livecheck (#104232)

--- Casks/serial.rb
+++ Casks/serial.rb
@@ -1,12 +1,16 @@
 cask "serial" do
-  version "2.0.7"
-  sha256 "8288c8a5b7ca84de74d21f33a1304827075d1886baffb6fc2b9f45cac27ec601"
+  version "2.0.10,02001.07.50"
+  sha256 "a84791d9db5f7bad01427fb73326e2270c5c8ab6311dfaa0d850036e5aeb606a"
 
-  url "https://download.decisivetactics.com/downloads/serial/Serial_#{version}.zip"
-  appcast "https://api.decisivetactics.com/api/v1/public/appcast?app=serial"
+  url "https://download.decisivetactics.com/downloads/serial/Serial_#{version.before_comma}.zip"
   name "Serial"
   desc "Connect to almost anything with a serial port"
   homepage "https://www.decisivetactics.com/products/serial/"
+
+  livecheck do
+    url "https://api.decisivetactics.com/api/v1/public/appcast?app=serial"
+    strategy :sparkle
+  end
 
   auto_updates true
 
