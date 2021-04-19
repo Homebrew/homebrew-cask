d57be7c57f35ad39b5d1286e9396b7168a534613
Andre Ferraz <31141+deferraz@users.noreply.github.com>
2021-04-19 13:10:48 -0300

    bump version to 4.2.2 (#104278)

--- Casks/lens.rb
+++ Casks/lens.rb
@@ -1,6 +1,6 @@
 cask "lens" do
-  version "4.2.1"
-  sha256 "edd299988416a471012618e70df1066bb2425fa3ea9e7ab3853e8f0186449b49"
+  version "4.2.2"
+  sha256 "d34753f6da17df2bfd89525ec6f5a521c00f8a512c7b245b3153ec7a302d5198"
 
   url "https://github.com/lensapp/lens/releases/download/v#{version}/Lens-#{version}.dmg",
       verified: "github.com/lensapp/lens/"
