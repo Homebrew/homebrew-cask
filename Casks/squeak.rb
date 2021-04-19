4b244100e6fbaa838942592c1e33e187be3a7588
Markus Reiter <me@reitermark.us>
2021-04-19 09:15:22 +0200

    squeak: add livecheck (#104261)

--- Casks/squeak.rb
+++ Casks/squeak.rb
@@ -3,9 +3,16 @@ cask "squeak" do
   sha256 "06c2d22064b2795c6f4a2c03feddd2ebf741aa8222676195f5ce7b9f607411bd"
 
   url "https://files.squeak.org/#{version.before_comma}/Squeak#{version.before_comma}-#{version.after_comma}-64bit/Squeak#{version.before_comma}-#{version.after_comma}-64bit-All-in-One.zip"
-  appcast "https://squeak.org/downloads/"
   name "Squeak"
   homepage "https://squeak.org/"
+
+  livecheck do
+    url "https://squeak.org/downloads/"
+    strategy :page_match do |page|
+      match = page.match(%r{href=.*?/Squeak(\d+(?:\.\d+)*)-(\d+)-64bit-All-in-One\.zip}i)
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   app "Squeak#{version.before_comma}-#{version.after_comma}-64bit-All-in-One.app"
 
