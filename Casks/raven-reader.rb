80592640805142938ac44fef9f6510d188915c64
Markus Reiter <me@reitermark.us>
2021-04-20 00:46:59 +0200

    raven-reader: add livecheck (#104372)

--- Casks/raven-reader.rb
+++ Casks/raven-reader.rb
@@ -4,10 +4,14 @@ cask "raven-reader" do
 
   url "https://download.helloefficiency.com/ravenreader/Raven%20Reader-#{version}.dmg",
       verified: "download.helloefficiency.com/ravenreader/"
-  appcast "https://ridereceiptspro.sfo2.digitaloceanspaces.com/ravenreader/latest-mac.yml"
   name "Raven Reader"
   desc "News reader with flexible settings"
   homepage "https://ravenreader.app/"
+
+  livecheck do
+    url "https://ridereceiptspro.sfo2.digitaloceanspaces.com/ravenreader/latest-mac.yml"
+    strategy :electron_builder
+  end
 
   auto_updates true
 
