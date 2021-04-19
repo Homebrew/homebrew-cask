f82ef9f88b589a5c24ea4ccb2efdbd47804ab65d
Jakub Warczarek <jakub.warczarek@gmail.com>
2021-04-19 18:56:52 +0200

    Update session-manager-plugin from 1.2.30.0 to 1.2.54.0 (#104291)

--- Casks/session-manager-plugin.rb
+++ Casks/session-manager-plugin.rb
@@ -1,6 +1,6 @@
 cask "session-manager-plugin" do
-  version "1.2.30.0"
-  sha256 "900d2c3b1044edf70cb024288d581b2cccf5cd4d5ed3061e529b549b67b0bc05"
+  version "1.2.54.0"
+  sha256 "920c6842bb6edadb15e6dff7a00f99da3b98db884f3b39cf1a395032ca8e2198"
 
   url "https://session-manager-downloads.s3.amazonaws.com/plugin/#{version}/mac/sessionmanager-bundle.zip",
       verified: "session-manager-downloads.s3.amazonaws.com/"
