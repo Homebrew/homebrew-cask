4520712e4bb728e3530ae32e8cd4f137c399a66e
Thomas Walter <tw@b-a-l-u.de>
2021-04-19 18:40:54 +0200

    Update prusaslicer from 2.3.0 to 2.3.1

--- Casks/prusaslicer.rb
+++ Casks/prusaslicer.rb
@@ -1,8 +1,8 @@
 cask "prusaslicer" do
-  version "2.3.0,202101111304"
-  sha256 "e72e91183a5631b5b866fcb905e696148f7f691eaae37b7c5224af67d97a0663"
+  version "2.3.1,202104161356"
+  sha256 "4d3de8fe7f1db98cfa2968ff1b47c96f81b8833338c6a4e743619d87cfcdfbcf"
 
-  url "https://github.com/prusa3d/PrusaSlicer/releases/download/version_#{version.before_comma}/PrusaSlicer-#{version.before_comma}+-#{version.after_comma}.dmg",
+  url "https://github.com/prusa3d/PrusaSlicer/releases/download/version_#{version.before_comma}/PrusaSlicer-#{version.before_comma}+universal-#{version.after_comma}.dmg",
       verified: "github.com/prusa3d/PrusaSlicer/"
   name "PrusaSlicer"
   desc "G-code generator for 3D printers (RepRap, Makerbot, Ultimaker etc.)"
