e24193197f633b30f8b556c353b0f2eb0d675165
Adithya Sunil <adithyasunil26@gmail.com>
2021-04-20 05:00:04 +0400

    Update epichrome from 2.4.2 to 2.4.3 (#104325)

--- Casks/epichrome.rb
+++ Casks/epichrome.rb
@@ -1,6 +1,6 @@
 cask "epichrome" do
-  version "2.4.2"
-  sha256 "6c2145110355c30be607dba605f009903d0a7ad7a67822611b5d756b8d492f09"
+  version "2.4.3"
+  sha256 "352b34cca8d710026275577a8abd918361f4e6a48ec01ef2643a00f0e3280de4"
 
   url "https://github.com/dmarmor/epichrome/releases/download/v#{version}/epichrome-#{version}.pkg"
   name "Epichrome"
