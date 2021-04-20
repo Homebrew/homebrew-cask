3207726017602a4d8fc7ad582736ab9bd8fd822f
owine <owine@users.noreply.github.com>
2021-04-20 13:05:10 -0500

    LunaSea: Update version string

--- Casks/lunasea.rb
+++ Casks/lunasea.rb
@@ -1,8 +1,8 @@
 cask "lunasea" do
-  version "5.0.0"
+  version "5.0.0+50000018"
   sha256 "9a7894791d584b9ac142090c034ff7f0fe51bbdf47cab79c38fbf995e6117844"
 
-  url "https://github.com/CometTools/LunaSea/releases/download/#{version}%2B50000018/LunaSea-macOS-release.zip",
+  url "https://github.com/CometTools/LunaSea/releases/download/#{version}/LunaSea-macOS-release.zip",
       verified: "https://github.com/CometTools/LunaSea/"
   name "LunaSea"
   desc "Controller for all of your self-hosted media software"
