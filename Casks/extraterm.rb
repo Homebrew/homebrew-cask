d934a32fff6b9c3a2f15a4c6083eaf214654f8d9
Adithya Sunil <adithyasunil26@gmail.com>
2021-04-20 04:59:50 +0400

    Update extraterm from 0.58.0 to 0.59.0 (#104326)

--- Casks/extraterm.rb
+++ Casks/extraterm.rb
@@ -1,6 +1,6 @@
 cask "extraterm" do
-  version "0.58.0"
-  sha256 "f3e58de27b323b4e5d292e6a59ab96b5492023a68b9a3d2a4df255ebf3690022"
+  version "0.59.0"
+  sha256 "257341e3fd270d55c2c8ef0b015afb1521908c88c9a21b06798a173820c9e8c9"
 
   url "https://github.com/sedwards2009/extraterm/releases/download/v#{version}/extraterm-#{version}-darwin-x64.zip",
       verified: "github.com/sedwards2009/extraterm/"
