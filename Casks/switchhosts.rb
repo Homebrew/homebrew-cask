23856689f8f6314fe4ed0b8b3a82a43a7b63ec68
Tai Zhou <21986946+Tai-Zhou@users.noreply.github.com>
2021-04-21 19:53:17 +0800

    Update switchhosts.rb
    Fix style of indent.

--- Casks/switchhosts.rb
+++ Casks/switchhosts.rb
@@ -4,11 +4,11 @@ cask "switchhosts" do
   if Hardware::CPU.intel?
     sha256 "0745e449e15002e9afc3c326e1e78a165e761e00825fa02eef170c140a8e41db"
     url "https://github.com/oldj/SwitchHosts/releases/download/v#{version.major_minor_patch}/SwitchHosts_#{version}.dmg",
-      verified: "github.com/oldj/SwitchHosts/"
+        verified: "github.com/oldj/SwitchHosts/"
   else
     sha256 "1f0cfe32b87f9133098604a68289db9f309af8374af9b9341b8d24393de354bf"
     url "https://github.com/oldj/SwitchHosts/releases/download/v#{version.major_minor_patch}/SwitchHosts_arm64_#{version}.dmg",
-      verified: "github.com/oldj/SwitchHosts/"
+        verified: "github.com/oldj/SwitchHosts/"
   end
   
   name "SwitchHosts"
