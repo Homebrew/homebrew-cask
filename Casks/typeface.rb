d40857a8be8b43406b73be2150c0e03946bfa07e
Reno <25192197+singularitti@users.noreply.github.com>
2021-04-21 08:19:41 -0400

    Update typeface from 3.0.0,2765 to 3.0.0,2765

--- Casks/typeface.rb
+++ Casks/typeface.rb
@@ -1,8 +1,8 @@
 cask "typeface" do
-  version "2.7.0,2023"
+  version "3.0.0,2765"
   sha256 :no_check
 
-  url "https://dcdn.typefaceapp.com/latest"
+  url "https://dcdn.typefaceapp.com/Typeface-#{version.before_comma}-#{version.after_comma}/Typeface-#{version.before_comma}-#{version.after_comma}.dmg"
   name "Typeface"
   desc "Font manager application"
   homepage "https://typefaceapp.com/"
