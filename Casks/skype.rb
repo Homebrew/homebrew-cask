d45131fca8c9409fa8557c9170515a3e6117fd3d
Yusuf Kürşad Kaya <ykursadkaya@gmail.com>
2021-04-21 12:18:26 +0300

    Update skype from 8.69.0.88 to 8.71.0.45 (#104414)

--- Casks/skype.rb
+++ Casks/skype.rb
@@ -1,6 +1,6 @@
 cask "skype" do
-  version "8.69.0.88"
-  sha256 "e60662b2f7fd5f872506fcf5601d62ef7514f933cfb52924e3c122fe6ad21093"
+  version "8.71.0.45"
+  sha256 "4d69596e2cfac27e789b901030d01ab59355d08391cfb4b8e5f33252d642dd58"
 
   url "https://endpoint920510.azureedge.net/s4l/s4l/download/mac/Skype-#{version}.dmg",
       verified: "endpoint920510.azureedge.net/s4l/s4l/download/mac/"
