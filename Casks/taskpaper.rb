0685ba9f07c8bb60303ff8522267a5220773ec73
Markus Reiter <me@reitermark.us>
2021-04-21 16:10:13 +0200

    taskpaper: add livecheck

--- Casks/taskpaper.rb
+++ Casks/taskpaper.rb
@@ -1,12 +1,16 @@
 cask "taskpaper" do
-  version "3.8.13"
-  sha256 "c73fc8f7b5ace388af453b1511690bec97a7b1d44f45b99e3d06835999b217fc"
+  version "3.8.16,475"
+  sha256 "96c1efa86db091bbade3e9ef544254a347fdc6a0906cee086fbc27380ff4e2f0"
 
-  url "https://www.taskpaper.com/assets/app/TaskPaper-#{version}.dmg"
-  appcast "https://www.taskpaper.com/assets/app/TaskPaper.rss"
+  url "https://www.taskpaper.com/assets/app/TaskPaper-#{version.before_comma}.dmg"
   name "TaskPaper"
   desc "App to make lists and help with organization"
   homepage "https://www.taskpaper.com/"
+
+  livecheck do
+    url "https://www.taskpaper.com/assets/app/TaskPaper.rss"
+    strategy :sparkle
+  end
 
   auto_updates true
 
