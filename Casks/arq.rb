15f4f150d00acf6e45b8cf1752e886d1cf38b9a0
António Meireles <antonio.meireles@reformi.st>
2021-04-21 21:37:35 +0100

    Update arq from 7.4.2 to 7.5.1 (#104434)

--- Casks/arq.rb
+++ Casks/arq.rb
@@ -1,6 +1,6 @@
 cask "arq" do
-  version "7.4.2"
-  sha256 "91e47e9f546d43a185c8bdcf8e34b9b92ae8fc79730584fc9462714e42ecba4b"
+  version "7.5.1"
+  sha256 "72106e040076fa295aa553013de2e8aa7b21e978b24116b744856fa615068401"
 
   url "https://www.arqbackup.com/download/arqbackup/Arq#{version}.pkg"
   name "Arq"
