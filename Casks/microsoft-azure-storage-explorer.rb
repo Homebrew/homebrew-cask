cd6c872d302a483caa99533493b105e1d230dd0d
dcunited08 <dcunited08@gmail.com>
2021-04-21 10:14:05 -0400

    Updating microsoft-azure-storage-explorer to v1.19.0 - adding desc

--- Casks/microsoft-azure-storage-explorer.rb
+++ Casks/microsoft-azure-storage-explorer.rb
@@ -5,6 +5,7 @@ cask "microsoft-azure-storage-explorer" do
   url "https://github.com/microsoft/AzureStorageExplorer/releases/download/v#{version}/StorageExplorer_signed.zip",
       verified: "github.com/microsoft/AzureStorageExplorer/"
   name "Microsoft Azure Storage Explorer"
+  desc "Explorer for Azure Storage"
   homepage "https://azure.microsoft.com/en-us/features/storage-explorer/"
 
   app "Microsoft Azure Storage Explorer.app"
