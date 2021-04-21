d50432db946e8ded6a4b24e86ace50757339ed1f
Nico Graf <SalocinHB@users.noreply.github.com>
2021-04-21 22:38:49 +0200

    Update texworks from 0.6.5 to 0.6.6,202103111124:24442ac (#104431)

--- Casks/texworks.rb
+++ Casks/texworks.rb
@@ -1,8 +1,8 @@
 cask "texworks" do
-  version "0.6.5,202003251937:649699a"
-  sha256 "c171ad80782485f0bdd15a611fc234a94d38144fb2ee183ec50377338df5d690"
+  version "0.6.6,202103111124:24442ac"
+  sha256 "9517f6bd8f3a34264bc9e5c6ae59969a81bcfdafda2c830d58d2d0d652cffe57"
 
-  url "https://github.com/TeXworks/texworks/releases/download/release-#{version.before_comma}/TeXworks-osx-#{version.before_comma}-#{version.after_comma.before_colon}-git_#{version.after_colon}.dmg",
+  url "https://github.com/TeXworks/texworks/releases/download/release-#{version.before_comma}/TeXworks-macos-#{version.before_comma}-#{version.after_comma.before_colon}-git_#{version.after_colon}.dmg",
       verified: "github.com/TeXworks/texworks/"
   name "TeXworks"
   desc "Main codebase"
@@ -11,7 +11,7 @@ cask "texworks" do
   livecheck do
     url "https://github.com/TeXworks/texworks/releases/latest"
     strategy :page_match do |page|
-      match = page.match(%r{href=.*?/TeXworks-osx-(\d+(?:\.\d+)*)-(\d+)-git_(.*?)\.dmg}i)
+      match = page.match(%r{href=.*?/TeXworks-macos-(\d+(?:\.\d+)*)-(\d+)-git_(.*?)\.dmg}i)
       "#{match[1]},#{match[2]}:#{match[3]}"
     end
   end
