f24b6bef1306208f15fa52b722d0680b1eb9b518
Tai Zhou <21986946+Tai-Zhou@users.noreply.github.com>
2021-04-21 20:02:17 +0800

    Update switchhosts.rb
    Fix trailing whitespace.

--- Casks/switchhosts.rb
+++ Casks/switchhosts.rb
@@ -1,6 +1,6 @@
 cask "switchhosts" do
   version "4.0.0.6029"
-  
+
   if Hardware::CPU.intel?
     sha256 "0745e449e15002e9afc3c326e1e78a165e761e00825fa02eef170c140a8e41db"
     url "https://github.com/oldj/SwitchHosts/releases/download/v#{version.major_minor_patch}/SwitchHosts_#{version}.dmg",
@@ -10,7 +10,7 @@ cask "switchhosts" do
     url "https://github.com/oldj/SwitchHosts/releases/download/v#{version.major_minor_patch}/SwitchHosts_arm64_#{version}.dmg",
         verified: "github.com/oldj/SwitchHosts/"
   end
-  
+
   name "SwitchHosts"
   desc "App to switch hosts"
   homepage "https://oldj.github.io/SwitchHosts/"
