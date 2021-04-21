fe466d90ebf47413ef03eabb6cbe6cfdcb355674
Tri Khac Vo <33442340+vokhactri@users.noreply.github.com>
2021-04-21 09:16:57 +0700

    Update binance from 1.15.0 to 1.15.1

--- Casks/binance.rb
+++ Casks/binance.rb
@@ -1,6 +1,6 @@
 cask "binance" do
-  version "1.15.0"
-  sha256 "2f6f755dac4ddf37223bd34dae30cb763b804a0bb9510b0935c8e2604a899755"
+  version "1.15.1"
+  sha256 "d71f07153962c25b160fc30a56e8d8d67086f2913fe431b0a7b8f739860f4b54"
 
   url "https://ftp.binance.com/electron-desktop/mac/production/binance-#{version}.dmg"
   name "Binance"
