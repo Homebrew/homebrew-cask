e193767cd024ef0b605c95983b6fe3d73e95cd55
freayd <freayd@users.noreply.github.com>
2021-04-21 22:16:32 +0200

    Update tribler from 7.8.0 to 7.9.0

--- Casks/tribler.rb
+++ Casks/tribler.rb
@@ -1,6 +1,6 @@
 cask "tribler" do
-  version "7.8.0"
-  sha256 "621103e765fb87d9b0be9f301bfa190c3d7bff9a3a152281e89ea328ca8ea69b"
+  version "7.9.0"
+  sha256 "cfcd545e8c5d46130172b4a00dddb5082297a76c9f9fc202abcb5953b530dff1"
 
   url "https://github.com/Tribler/tribler/releases/download/v#{version}/Tribler-#{version}.dmg",
       verified: "github.com/Tribler/tribler/"
