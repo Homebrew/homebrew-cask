88b4573729776bc6541edf030ac686ee744a6afe
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-22 17:05:32 +0800

    Update ilspy.rb

--- Casks/ilspy.rb
+++ Casks/ilspy.rb
@@ -1,11 +1,16 @@
 cask "ilspy" do
-  version "v5.0-rc2"
+  version "5.0-rc2"
   sha256 "219a657bb617603b35386500d2dc1994e38e3e2cf16c0e138bd58afa786df965"
 
-  url "https://github.com/icsharpcode/AvaloniaILSpy/releases/download/#{version}/ILSpy-osx-x64-Release.zip"
+  url "https://github.com/icsharpcode/AvaloniaILSpy/releases/download/v#{version}/ILSpy-osx-x64-Release.zip"
   name "ILSpy"
   desc "Avalonia-based .NET decompiler"
   homepage "https://github.com/icsharpcode/AvaloniaILSpy"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "ILSpy.app"
 end
