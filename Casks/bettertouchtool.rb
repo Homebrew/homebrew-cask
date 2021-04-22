c850a179d757d77e9ac5ed328599951c83a92502
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-22 17:44:11 +0800

    Update bettertouchtool.rb

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -1,15 +1,16 @@
 cask "bettertouchtool" do
-  version "3.561,1701"
+  version "3.561-1701"
   sha256 "6f19467a71bec4ee992c61fe7fff762677c1d97856b6360f872aafc6f7c0a841"
 
-  url "https://folivora.ai/releases/btt#{version.before_comma}-#{version.after_comma}.zip"
+  url "https://folivora.ai/releases/btt#{version}.zip"
   name "BetterTouchTool"
   desc "Tool to customize input devices and automate computer systems"
   homepage "https://folivora.ai/"
 
   livecheck do
     url "https://updates.folivora.ai/appcast.xml?trial=1"
-    strategy :sparkle
+    strategy :page_match
+    regex(/btt(\d+(?:.\d+)*)\.zip/i)
   end
 
   auto_updates true
