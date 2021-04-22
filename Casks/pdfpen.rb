b99d6f771a9c1099fcbd15dd66587ff94a0105bf
miccal <miccal.matthews@gmail.com>
2021-04-22 17:28:50 +0800

    Update pdfpen from 12.2.2,1222.7 to 12.2.3

--- Casks/pdfpen.rb
+++ Casks/pdfpen.rb
@@ -1,6 +1,6 @@
 cask "pdfpen" do
-  version "12.2.2,1222.7"
-  sha256 "5beed8c14bf710ad973fdf01c2680b756ab7d6ae1add7652ab4a94595668c732"
+  version "12.2.3"
+  sha256 "7aafb25ce3234bac3a6c499c31ef97ef478e3a059e82ac548a49a04a933c9d17"
 
   url "https://cdn.smilesoftware.com/PDFpen_#{version.before_comma}.dmg"
   name "PDFpen"
