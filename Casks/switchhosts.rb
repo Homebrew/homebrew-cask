0bffbead02983e0a0cf33ab4d54ee8a689f2e200
Tai Zhou <21986946+Tai-Zhou@users.noreply.github.com>
2021-04-22 10:10:10 +0800

    Update switchhosts.rb (#104417)
    * Update switchhosts.rb
    
    Add url for the ARM version.
    
    * Update switchhosts.rb
    
    Fix style of indent.
    
    * Update switchhosts.rb
    
    Fix trailing whitespace.

--- Casks/switchhosts.rb
+++ Casks/switchhosts.rb
@@ -1,9 +1,16 @@
 cask "switchhosts" do
   version "4.0.0.6029"
-  sha256 "0745e449e15002e9afc3c326e1e78a165e761e00825fa02eef170c140a8e41db"
 
-  url "https://github.com/oldj/SwitchHosts/releases/download/v#{version.major_minor_patch}/SwitchHosts_#{version}.dmg",
-      verified: "github.com/oldj/SwitchHosts/"
+  if Hardware::CPU.intel?
+    sha256 "0745e449e15002e9afc3c326e1e78a165e761e00825fa02eef170c140a8e41db"
+    url "https://github.com/oldj/SwitchHosts/releases/download/v#{version.major_minor_patch}/SwitchHosts_#{version}.dmg",
+        verified: "github.com/oldj/SwitchHosts/"
+  else
+    sha256 "1f0cfe32b87f9133098604a68289db9f309af8374af9b9341b8d24393de354bf"
+    url "https://github.com/oldj/SwitchHosts/releases/download/v#{version.major_minor_patch}/SwitchHosts_arm64_#{version}.dmg",
+        verified: "github.com/oldj/SwitchHosts/"
+  end
+
   name "SwitchHosts"
   desc "App to switch hosts"
   homepage "https://oldj.github.io/SwitchHosts/"
