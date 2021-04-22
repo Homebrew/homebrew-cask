566a3c1a76cd69011f74f02427d8b2eb1a8fe65a
dcunited08 <dcunited08@gmail.com>
2021-04-21 22:03:33 -0400

    Updating microsoft-azure-storage-explorer to v1.19.0 (#104429)
    * Updating microsoft-azure-storage-explorer to v1.19.0
    
    * Updating microsoft-azure-storage-explorer to v1.19.0 - adding desc

--- Casks/microsoft-azure-storage-explorer.rb
+++ Casks/microsoft-azure-storage-explorer.rb
@@ -1,10 +1,11 @@
 cask "microsoft-azure-storage-explorer" do
-  version "1.18.1"
-  sha256 "53e9c9ef1266d03460321e0e49bb75e2e74878300308145cff5abc3e4f764ae2"
+  version "1.19.0"
+  sha256 "eee5249a445922acbe729d675daf198de249ae73ff3fe0b68cd2c187692d8fdd"
 
-  url "https://github.com/microsoft/AzureStorageExplorer/releases/download/v#{version}/MaC_StorageExplorer.zip",
+  url "https://github.com/microsoft/AzureStorageExplorer/releases/download/v#{version}/StorageExplorer_signed.zip",
       verified: "github.com/microsoft/AzureStorageExplorer/"
   name "Microsoft Azure Storage Explorer"
+  desc "Explorer for Azure Storage"
   homepage "https://azure.microsoft.com/en-us/features/storage-explorer/"
 
   app "Microsoft Azure Storage Explorer.app"
