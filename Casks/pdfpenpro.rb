a8f3f7af5b541a0995d00485de8119da5f6ae80e
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-22 17:06:04 +0800

    Update pdfpenpro.rb

--- Casks/pdfpenpro.rb
+++ Casks/pdfpenpro.rb
@@ -1,5 +1,5 @@
 cask "pdfpenpro" do
-  version "12.2.3"
+  version "12.2.3,1223.1"
   sha256 "6522b30a957ceeea13bc18506da501673ab3798fbdfe5fe73add6d764f5dc0a8"
 
   url "https://cdn.smilesoftware.com/PDFpenPro_#{version.before_comma}.dmg"
