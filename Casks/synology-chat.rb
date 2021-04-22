970a8ceedba141c3aec022422adce271c28b9598
Markus Reiter <me@reitermark.us>
2021-04-22 04:09:24 +0200

    synology-chat: add livecheck (#104424)

--- Casks/synology-chat.rb
+++ Casks/synology-chat.rb
@@ -3,10 +3,15 @@ cask "synology-chat" do
   sha256 "1f61431ab0e7ac5646bfedec610e2ca6be527f5ab4001789a14990ebd9aab78c"
 
   url "https://global.download.synology.com/download/Tools/ChatClient/#{version}/Mac/Installer/Chat-#{version}.dmg"
-  appcast "https://www.synology.com/en-us/releaseNote/ChatClient"
   name "Synology Chat"
   desc "Messaging service that runs on Synology NAS"
   homepage "https://www.synology.com/en-us/dsm/feature/chat"
+
+  livecheck do
+    url "https://www.synology.com/en-us/releaseNote/ChatClient"
+    strategy :page_match
+    regex(/Version:\s*(\d+(?:\.\d+)*-\d+)/i)
+  end
 
   app "Synology Chat.app"
 end
