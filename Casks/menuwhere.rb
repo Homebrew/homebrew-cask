5b66eb7af83b5b4b87f06414a155940bb887ed2c
Jed Fox <git@jedfox.com>
2021-04-22 12:17:11 -0400

    Add Menuwhere

---
+++ Casks/menuwhere.rb
@@ -0,0 +1,23 @@
+cask "menuwhere" do
+  version "1.0.0"
+  sha256 :no_check
+
+  url "https://manytricks.com/download/menuwhere"
+  name "Menuwhere"
+  desc "The menu, anywhere"
+  homepage "https://manytricks.com/menuwhere/"
+
+  livecheck do
+    url "https://manytricks.com/menuwhere/appcast/"
+    strategy :sparkle
+  end
+
+  app "Menuwhere.app"
+
+  uninstall quit: "com.manytricks.Menuwhere"
+
+  zap trash: [
+    "~/Library/Caches/com.manytricks.Menuwhere",
+    "~/Library/Preferences/com.manytricks.Menuwhere.plist",
+  ]
+end
