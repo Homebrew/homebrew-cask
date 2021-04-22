064ba7d70f3d3bbdda33866d353721de53a513ec
Schulze Thomas <Schulze_Thomas@c-programming.net>
2021-04-22 10:46:56 +0200

    update BetterTouchTool to version 3.561-1701

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -1,6 +1,6 @@
 cask "bettertouchtool" do
-  version "3.560,1700"
-  sha256 "cec80de7977fc19cd54f5d4bb4abda1db28bb8a2ff2267b77317eca195e9b287"
+  version "3.561,1701"
+  sha256 "6f19467a71bec4ee992c61fe7fff762677c1d97856b6360f872aafc6f7c0a841"
 
   url "https://folivora.ai/releases/btt#{version.before_comma}-#{version.after_comma}.zip"
   name "BetterTouchTool"
