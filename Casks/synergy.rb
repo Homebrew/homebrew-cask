67e3e767f0b0af8dc5e2d94d27ee9ab19a9d64db
Andrew Elgert <andrew.elgert@gmail.com>
2021-04-23 13:56:29 -0400

    Update synergy from 1.13.0,bdb8f767 to 1.13.1,063519a8

--- Casks/synergy.rb
+++ Casks/synergy.rb
@@ -1,8 +1,8 @@
 cask "synergy" do
-  version "1.13.0,bdb8f767"
-  sha256 "94796404c500d0c396d94a21e541fe3dca08bb7d491815f741f093bfe3b26ffa"
+  version "1.13.1,063519a8"
+  sha256 "8f58ab92b5609793ab10477aba6a7c0b22fce91c8cd51a7ab60bb5cb4b5bcc20"
 
-  url "https://binaries.symless.com/synergy/v#{version.before_comma.major}-core-standard/#{version.before_comma}-stable-#{version.after_comma}/synergy_#{version.before_comma}-stable.#{version.after_comma}_macos_x86-64.dmg"
+  url "https://binaries.symless.com/synergy/v#{version.before_comma.major}-core-standard/#{version.before_comma}-stable.#{version.after_comma}/synergy_#{version.before_comma}-stable.#{version.after_comma}_macos_x86-64.dmg"
   appcast "https://github.com/symless/synergy-core/releases.atom"
   name "Synergy"
   desc "Keyboard and mouse sharing tool - open-source core"
