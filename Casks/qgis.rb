61cef7e92d8c57a290dc5eb22d1c990b89037169
Markus Reiter <me@reitermark.us>
2021-04-23 20:52:14 +0200

    Update qgis.rb

--- Casks/qgis.rb
+++ Casks/qgis.rb
@@ -3,8 +3,6 @@ cask "qgis" do
   sha256 :no_check
 
   url "https://qgis.org/downloads/macos/qgis-macos-pr.dmg"
-  appcast "https://qgis.org/downloads/macos/qgis-macos-pr.sha256sum",
-          must_contain: version.dots_to_underscores
   name "QGIS"
   desc "Geographic Information System"
   homepage "https://www.qgis.org/"
