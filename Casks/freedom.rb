33e8b4da765d883585d4a7b67c33a8fb9b2652e0
Sean Molenaar <SMillerDev@users.noreply.github.com>
2021-04-23 09:18:10 +0200

    freedom: add description and version fix

--- Casks/freedom.rb
+++ Casks/freedom.rb
@@ -1,9 +1,10 @@
 cask "freedom" do
-  version "2.5.7"
+  version "2.5.7,1886"
   sha256 "fcf4d35252236f56af54f71db7e9b84de258024d32f5919a41e24cf0caa6053e"
 
   url "https://cdn.freedom.to/installers/updates/mac/#{version.before_comma}/Freedom.zip"
   name "Freedom"
+  desc "App and website blocker"
   homepage "https://freedom.to/"
 
   livecheck do
