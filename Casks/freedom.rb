1acabd814e5d4a540d825001e39c621c9bf3fdd2
Curudel <33662559+Curudel@users.noreply.github.com>
2021-04-23 20:26:59 +0200

    Update freedom from 2.4.3,1716.1 to 2.5.7 (#104465)

--- Casks/freedom.rb
+++ Casks/freedom.rb
@@ -1,9 +1,10 @@
 cask "freedom" do
-  version "2.4.3,1716.1"
-  sha256 "374450cc820d948a04a0e07c1ca199cfbc6c5f521d99f27eceb8697d636d3fdc"
+  version "2.5.7,1886"
+  sha256 "fcf4d35252236f56af54f71db7e9b84de258024d32f5919a41e24cf0caa6053e"
 
   url "https://cdn.freedom.to/installers/updates/mac/#{version.before_comma}/Freedom.zip"
   name "Freedom"
+  desc "App and website blocker"
   homepage "https://freedom.to/"
 
   livecheck do
