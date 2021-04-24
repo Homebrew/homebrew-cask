d64948bca8bc0f9e2b594f96c53c2c12551a0d73
Jeff McCollum <16550786+jeffmccollum@users.noreply.github.com>
2021-04-24 10:42:15 -0500

    Update sentinel from 0.17.1 to 0.18.0 (#104501)
    * Update sentinel from 0.17.1 to 0.18.0
    
    * Update sentinel.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/sentinel.rb
+++ Casks/sentinel.rb
@@ -1,13 +1,14 @@
 cask "sentinel" do
-  version "0.17.1"
-  sha256 "9ff6f740dab5d149600b4e7968ce83383bf444f0ff2b5761f1bac2fff2d3ae1d"
+  version "0.18.0"
+  sha256 "0a2390cf0bfae85ecf09f297c5bcb9a4673b315375bc02f8dba0bf83acf61dbe"
 
   url "https://releases.hashicorp.com/sentinel/#{version}/sentinel_#{version}_darwin_amd64.zip"
   name "Sentinel"
-  homepage "https://docs.hashicorp.com/sentinel/"
+  desc "Language and framework for policy as code"
+  homepage "https://docs.hashicorp.com/sentinel"
 
   livecheck do
-    url "https://docs.hashicorp.com/sentinel/downloads/"
+    url "https://docs.hashicorp.com/sentinel/downloads"
     strategy :page_match
     regex(%r{href=.*?/sentinel_(\d+(?:\.\d+)*)_darwin_amd64\.zip}i)
   end
