6a3f78b352c4e4a75cd529a65d5778e6ce49c38e
Andrew Elgert <andrew.elgert@gmail.com>
2021-04-24 19:18:51 -0400

    Set appcast URL to pass brew audit

--- Casks/synergy.rb
+++ Casks/synergy.rb
@@ -3,7 +3,7 @@ cask "synergy" do
   sha256 "8f58ab92b5609793ab10477aba6a7c0b22fce91c8cd51a7ab60bb5cb4b5bcc20"
 
   url "https://binaries.symless.com/synergy/v#{version.before_comma.major}-core-standard/#{version.before_comma}-stable.#{version.after_comma}/synergy_#{version.before_comma}-stable.#{version.after_comma}_macos_x86-64.dmg"
-  appcast "https://github.com/symless/synergy-core/releases.atom"
+  appcast "https://github.com/symless/synergy-core/releases.atom?after=1.13.2.2-snapshot"
   name "Synergy"
   desc "Keyboard and mouse sharing tool - open-source core"
   homepage "https://symless.com/synergy"
