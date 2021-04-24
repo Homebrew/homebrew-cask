69545f7f29b0e1fab54f17ae60425696b1c31d6d
Pedro P. Palazzo <palazzo@unb.br>
2021-04-24 14:03:28 +0100

    Create cask depthmapx

---
+++ Casks/depthmapx.rb
@@ -0,0 +1,12 @@
+cask "depthmapx" do
+  version "0.8.0"
+  sha256 "0c32fd8e15eff4bdd19fe4fae4f9dc29c6225a5a238bc25664cac0f5b6d5c1ab"
+
+  url "https://github.com/SpaceGroupUCL/depthmapX/releases/download/v#{version}/depthmapX-#{version}_macos.zip"
+  name "depthmapX"
+  desc "Multi-Platform Spatial Network Analysis Software"
+  homepage "https://github.com/SpaceGroupUCL/depthmapX"
+
+  app "depthmapX.app"
+end
+
