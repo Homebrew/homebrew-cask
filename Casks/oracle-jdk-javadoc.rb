0b3f6e62208f8cf76df11b9ce6e71a93b8048c36
Cláudio de Oliveira Pereira <pclaudio.ans@gmail.com>
2021-04-24 11:11:27 -0400

    Update oracle-jdk-javadoc from 16 to 16.0.1 (#104500)
    * Update oracle-jdk-javadoc from 16 to 16.0.1
    
    * Update oracle-jdk-javadoc.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/oracle-jdk-javadoc.rb
+++ Casks/oracle-jdk-javadoc.rb
@@ -1,13 +1,22 @@
 cask "oracle-jdk-javadoc" do
-  version "16,36:7863447f0ab643c585b9bdebf67c69db"
-  sha256 "cc427e860e3142d54d3c6f8f6b70d37d065cfec1f0a9676c93a702097f9e322b"
+  version "16.0.1,9:7147401fd7354114ac51ef3e1328291f"
+  sha256 "4e3d5270ebe36093c856d6eb321e86f7a0526afc7b144aa45218e6f8faf75b4e"
 
   url "https://download.oracle.com/otn-pub/java/jdk/#{version.before_comma}+#{version.after_comma.before_colon}/#{version.after_colon}/jdk-#{version.before_comma}_doc-all.zip",
       cookies: {
         "oraclelicense" => "accept-securebackup-cookie",
       }
   name "Oracle Java Standard Edition Development Kit Documentation"
+  desc "Documentation for the Oracle JDK"
   homepage "https://www.oracle.com/technetwork/java/javase/documentation/index.html"
+
+  livecheck do
+    url "https://www.oracle.com/java/technologies/javase-jdk16-doc-downloads.html"
+    strategy :page_match do |page|
+      match = page.match(%r{(\d+(?:\.\d+)*)\+(\d+(?:\.\d+)*)/(.+)/jdk-(\d+(?:\.\d+)*)_doc-all\.zip}i)
+      "#{match[1]},#{match[2]}:#{match[3]}"
+    end
+  end
 
   artifact "docs", target: "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk/Contents/Home/docs"
 
