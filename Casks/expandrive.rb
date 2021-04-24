95b472bf0197a25089e526a0cda7ad018c10721d
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-24 23:19:08 +0800

    Update expandrive.rb

--- Casks/expandrive.rb
+++ Casks/expandrive.rb
@@ -3,7 +3,6 @@ cask "expandrive" do
   sha256 "b2bfcc474626e4ea0be1ff1ab987ba0dcd9259530909ad3021f6088d6f3e5de6"
 
   url "https://updates.expandrive.com/apps/expandrive#{version.major}/v/#{version.dots_to_hyphens}/update_download"
-  appcast "https://updates.expandrive.com/appcast/expandrive#{version.major}.json?version=#{version.major}.0.0"
   name "ExpanDrive"
   desc "Network drive and browser for cloud storage"
   homepage "https://www.expandrive.com/apps/expandrive/"
