4152fb3598c6c8cd9715f1728487365f575b9222
adithyasunil26 <adithyasunil26@gmail.com>
2021-04-26 00:21:16 +0400

    Update get-iplayer-automator from 1.21.7,20210330001 to 1.21.9,20210423001

--- Casks/get-iplayer-automator.rb
+++ Casks/get-iplayer-automator.rb
@@ -1,6 +1,6 @@
 cask "get-iplayer-automator" do
-  version "1.21.7,20210330001"
-  sha256 "8fc31b64f3511d6b8dbc3ffd51326788c5975c5af7f5962a23738ecec35e491b"
+  version "1.21.9,20210423001"
+  sha256 "dfe76e596d295dbe3da302f3b7e3ccd7e0a1cab77adfcfaa0e465e4296dd3fd9"
 
   url "https://github.com/Ascoware/get-iplayer-automator/releases/download/v#{version.before_comma}/Get.iPlayer.Automator.v#{version.before_comma}.b#{version.after_comma}.zip"
   name "Get iPlayer Automator"
