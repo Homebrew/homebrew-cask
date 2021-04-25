e8634644b4ccd01d753f687240d6a0aea5d26d58
adithyasunil26 <adithyasunil26@gmail.com>
2021-04-26 01:14:33 +0400

    Update segger-embedded-studio-for-arm from 5.42a to 5.42b

--- Casks/segger-embedded-studio-for-arm.rb
+++ Casks/segger-embedded-studio-for-arm.rb
@@ -1,6 +1,6 @@
 cask "segger-embedded-studio-for-arm" do
-  version "5.42a"
-  sha256 "7f960f0c0d30c1796f02f313e49989af1b68ba21bb9e99e1de6731b1308f249a"
+  version "5.42b"
+  sha256 "56570f3251ed5bbec70fb5f9a02e590c5b9b6c25727f14c75118ee7e258df2d9"
 
   url "https://www.segger.com/downloads/embedded-studio/Setup_EmbeddedStudio_ARM_v#{version.no_dots}_macos_x64.dmg"
   name "SEGGER Embedded Studio for ARM"
