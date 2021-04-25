d68a300f4c81d92d8d8cd2b858d10ee3e640336b
adithyasunil26 <adithyasunil26@gmail.com>
2021-04-26 01:40:41 +0400

    Update tableau-reader from 2021.1.0 to 2021.1.1

--- Casks/tableau-reader.rb
+++ Casks/tableau-reader.rb
@@ -1,6 +1,6 @@
 cask "tableau-reader" do
-  version "2021.1.0"
-  sha256 "ab62a7ecbd45a04e083ad1c9d0f96517d789180257c5efac30e0052ae88214f7"
+  version "2021.1.1"
+  sha256 "5f6ab24d8d3ce30fd49ea616232b083fb7d34b7bbb755c42ac06a226f086d512"
 
   url "https://downloads.tableau.com/tssoftware/TableauReader-#{version.dots_to_hyphens}.dmg"
   name "Tableau Reader"
