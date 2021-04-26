8868b8b5fbdbd02b924bef248c0f05cbe710a828
Adithya Sunil <adithyasunil26@gmail.com>
2021-04-25 20:40:12 -0400

    Update raiderio from 2.1.2 to 2.1.3 (#104588)
    * Update raiderio from 2.1.2 to 2.1.3
    
    * Update raiderio.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/raiderio.rb
+++ Casks/raiderio.rb
@@ -1,10 +1,11 @@
 cask "raiderio" do
-  version "2.1.2"
-  sha256 "fd7b77d4b3bf588dba3f6e6e214c7ab58287a76c5105762048b76ac9cb32f54f"
+  version "2.1.3"
+  sha256 "62760510cb2062ea95a4bd1c92d5aeddb66990251bbd1ec66d4291570a07cd7b"
 
   url "https://github.com/RaiderIO/raiderio-client-builds/releases/download/v#{version}/RaiderIO_Client.dmg",
       verified: "github.com/RaiderIO/raiderio-client-builds/"
   name "RaiderIO Client"
+  desc "World of Warcraft rankings site client"
   homepage "https://raider.io/"
 
   app "RaiderIO.app"
