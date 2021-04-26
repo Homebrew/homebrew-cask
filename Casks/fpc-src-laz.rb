b02c85fe83c65eb11d9b113d718d379794e95d1d
Vítor Galvão <info@vitorgalvao.com>
2021-04-26 16:14:02 +0100

    Update fpc-src-laz.rb

--- Casks/fpc-src-laz.rb
+++ Casks/fpc-src-laz.rb
@@ -4,8 +4,8 @@ cask "fpc-src-laz" do
 
   url "https://downloads.sourceforge.net/lazarus/Lazarus%20macOS%20x86-64/Lazarus%20#{version.after_comma}/fpc-src-#{version.before_comma}-laz.pkg",
       verified: "sourceforge.net/lazarus/"
-  name "Pascal compiler source files for Lazerus"
-  desc "Pascal compiler source files for Lazerus"
+  name "Pascal compiler source files for Lazarus"
+  desc "Pascal compiler source files for Lazarus"
   homepage "https://www.lazarus-ide.org/"
 
   livecheck do
