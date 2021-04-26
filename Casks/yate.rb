0bac8ac726b18a8972964ace01ea736a87090420
Adithya Sunil <adithyasunil26@gmail.com>
2021-04-25 20:10:22 -0400

    Update yate from 6.4.1,10306 to 6.4.1.1,10307 (#104620)
    * Update yate from 6.4.1,10306 to 6.4.1.1,10307
    
    * Update yate.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/yate.rb
+++ Casks/yate.rb
@@ -1,9 +1,10 @@
 cask "yate" do
-  version "6.4.1,10306"
+  version "6.4.1.1,10307"
   sha256 :no_check
 
   url "https://2manyrobots.com/Updates/Yate/Yate.zip"
   name "Yate"
+  desc "Media file tag editor"
   homepage "https://2manyrobots.com/yate/"
 
   livecheck do
