ff322f176d2dd2f8001bae4f77ed2266b0167bf2
MoonFruit <dkmoonfruit@gmail.com>
2021-04-26 09:30:30 +0800

    Update tencent-meeting from 2.7.5 to 2.9.4 (#104189)

--- Casks/tencent-meeting.rb
+++ Casks/tencent-meeting.rb
@@ -1,12 +1,12 @@
 cask "tencent-meeting" do
   if Hardware::CPU.intel?
-    version "2.7.5.434,727e439f79327c7c63677a9097353d3d"
-    sha256 "f072dafd53fd816acc95542dae0f750727704532b81b7afc5861e0cb9df75d71"
+    version "2.9.4.401,b151cc342f235237a2702235d2220208"
+    sha256 "1e8f1123484e979944a3db9bef4ad1e78a82c82b0a602f4266b58b252b90e25c"
     url "https://updatecdn.meeting.qq.com/#{version.after_comma}/TencentMeeting_0300000000_#{version.before_comma}.publish.x86_64.dmg",
         verified: "qq.com/"
   else
-    version "2.7.5.434,2a4a7de84f9518b5ba69df232eb7a1d0"
-    sha256 "7e5c56185e9f29c3b6db10283101118e1d02b3d87178e45d90d9a0a27b6dc9fa"
+    version "2.9.4.401,4a9e49af106a6edf8ff31e5e095fa5e1"
+    sha256 "e4c033d34176cfa963de9cfe0cd6d883f265e151a1ad479c25a93a7857b97e8e"
     url "https://updatecdn.meeting.qq.com/#{version.after_comma}/TencentMeeting_0300000000_#{version.before_comma}.publish.arm64.dmg",
         verified: "qq.com/"
   end
@@ -15,6 +15,10 @@ cask "tencent-meeting" do
   name "腾讯会议"
   desc "Cloud video conferencing"
   homepage "https://meeting.tencent.com/"
+
+  livecheck do
+    skip "No version information available"
+  end
 
   auto_updates true
 
