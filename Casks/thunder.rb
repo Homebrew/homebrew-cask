ca0e60bfdd95d1727eaff37db4a66c7b00a27027
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-26 10:28:08 +0800

    Update thunder.rb (#104627)

--- Casks/thunder.rb
+++ Casks/thunder.rb
@@ -1,14 +1,19 @@
 cask "thunder" do
-  version "3.4.1.4368"
-  sha256 "a732b748d95005fbf7ecfe3373be037940b10127dc1adf3de30a4a05d46a97f2"
+  version "4.0.0.12079"
+  sha256 "e3afc8acb1bea5108fa9beb08a867b319355c534dc5529eec6df31bedf4983c3"
 
   url "https://down.sandai.net/mac/thunder_#{version}.dmg",
       verified: "down.sandai.net/"
-  appcast "https://static-xl9-ssl.xunlei.com/json/mac_download_url.json"
   name "Thunder"
   name "迅雷"
   desc "VPN and WiFi proxy"
   homepage "https://www.xunlei.com/"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(/thunder_(\d+(?:\.\d+)*)\.dmg/i)
+  end
 
   auto_updates true
   depends_on macos: ">= :yosemite"
