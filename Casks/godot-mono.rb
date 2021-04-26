da0b964d0d15477d9213caf2115c62e43a2f871b
Adithya Sunil <adithyasunil26@gmail.com>
2021-04-25 21:09:55 -0400

    Update godot-mono from 3.2.3 to 3.3 (#104561)
    * Update godot-mono from 3.2.3 to 3.3
    
    * Update godot-mono.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/godot-mono.rb
+++ Casks/godot-mono.rb
@@ -1,10 +1,11 @@
 cask "godot-mono" do
-  version "3.2.3"
-  sha256 "b9138b546ddf074105d39de070455404a77e6398d7fb5f0d10a8792cf21dd579"
+  version "3.3"
+  sha256 "e18cf3be1164209a800a7d3a16b69ce281927ed50472b8c178398389cfc5a1dd"
 
   url "https://downloads.tuxfamily.org/godotengine/#{version}/mono/Godot_v#{version}-stable_mono_osx.64.zip",
       verified: "downloads.tuxfamily.org/godotengine/"
   name "Godot Engine"
+  desc "2D and 3D game engine"
   homepage "https://godotengine.org/"
 
   livecheck do
