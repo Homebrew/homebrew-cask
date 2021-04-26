1fce15fd4885bdb06ee888a1a1d386aec829039f
Adithya Sunil <adithyasunil26@gmail.com>
2021-04-25 21:12:49 -0400

    Update flipper from 0.85.0 to 0.86.0 (#104555)

--- Casks/flipper.rb
+++ Casks/flipper.rb
@@ -1,6 +1,6 @@
 cask "flipper" do
-  version "0.85.0"
-  sha256 "b4bcf60b4cc70b6f198a8cf506d95815698552e8d0e9bc091dd714039380a591"
+  version "0.86.0"
+  sha256 "d5a5dadb270dfbf61ddb527d543d008a62d127130942e12e39c7d894c075044f"
 
   url "https://github.com/facebook/flipper/releases/download/v#{version}/Flipper-mac.dmg",
       verified: "github.com/facebook/flipper/"
