cbbcb38ac9ad74ec477cf2748c22474724868118
Adithya Sunil <adithyasunil26@gmail.com>
2021-04-25 20:51:47 -0400

    Update milkman from 5.1.0 to 5.2.0 (#104573)
    * Update milkman from 5.1.0 to 5.2.0
    
    * Update milkman.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/milkman.rb
+++ Casks/milkman.rb
@@ -1,9 +1,10 @@
 cask "milkman" do
-  version "5.1.0"
-  sha256 "ac460d8fb00992b7c7723859209b9c2f4c5e1172982a5fd43f5b274ffa1b22f1"
+  version "5.2.0"
+  sha256 "0ac66d97f7cfa506123862276c24f9f24e649ab29140e5474a59c2d28c67fd3a"
 
   url "https://github.com/warmuuh/milkman/releases/download/#{version}/milkman-dist-appbundle-macos64-bin.tgz"
   name "Milkman"
+  desc "Extensible request and response workbench"
   homepage "https://github.com/warmuuh/milkman"
 
   app "Milkman.app"
