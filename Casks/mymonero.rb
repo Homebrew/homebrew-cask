8e381660fd539720c4b32b77284df459dc1c6ad6
Adithya Sunil <adithyasunil26@gmail.com>
2021-04-25 20:41:03 -0400

    Update mymonero from 1.1.19 to 1.1.20 (#104577)

--- Casks/mymonero.rb
+++ Casks/mymonero.rb
@@ -1,6 +1,6 @@
 cask "mymonero" do
-  version "1.1.19"
-  sha256 "4d66cdd658d9e950143fcdb3cd339dbba9b7734077236f7ac41ed5f6fb60757f"
+  version "1.1.20"
+  sha256 "7f58b16e226db11432d510264dc6c100e61b327f15bed4e3c57ca1491a51994d"
 
   url "https://github.com/mymonero/mymonero-app-js/releases/download/v#{version}/MyMonero-#{version}.dmg",
       verified: "github.com/mymonero/mymonero-app-js/"
