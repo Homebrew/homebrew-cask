482133c5ffe5680c9c3c7de23a7575a3b1b99cf9
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-26 08:00:46 +0800

    Update zoho-workdrive.rb

--- Casks/zoho-workdrive.rb
+++ Casks/zoho-workdrive.rb
@@ -5,6 +5,7 @@ cask "zoho-workdrive" do
   url "https://files-accl.zohopublic.com/public/wdbin/download/46f971e4fc4a32b68ad5d7dade38a7d2",
       verified: "files-accl.zohopublic.com/"
   name "Zoho WorkDrive"
+  desc "Client for the Zoho cloud storage service"
   homepage "https://www.zoho.com/workdrive/desktop-sync.html"
 
   livecheck do
