7cf6cc85b952516dd68d80ac85debf3d8510cf26
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-26 21:27:00 +0800

    Update and rename fpcsrc.rb to fpc-src-laz.rb

--- Casks/fpc-src-laz.rb
+++ Casks/fpc-src-laz.rb
@@ -1,12 +1,12 @@
-cask "fpcsrc" do
+cask "fpc-src-laz" do
   version "3.2.0-2,2.0.12"
   sha256 "8f85c873c3a224961a98fd24e97320a2134bbcc7f91c6d8153e53b36beb7fe74"
 
   url "https://downloads.sourceforge.net/lazarus/Lazarus%20macOS%20x86-64/Lazarus%20#{version.after_comma}/fpc-src-#{version.before_comma}-laz.pkg",
       verified: "sourceforge.net/lazarus/"
   name "Pascal compiler source files for Lazerus"
-  desc "Pascal compiler source file for Lazerus"
-  homepage "https://www.freepascal.org/"
+  desc "Pascal compiler source files for Lazerus"
+  homepage "https://www.lazarus-ide.org/"
 
   pkg "fpc-src-#{version.before_comma}-laz.pkg"
 
