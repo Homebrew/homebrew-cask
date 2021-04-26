2994997a83cfadfb8c6f8bf9dbc3b5c573e0048c
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-26 08:11:25 +0800

    Update tableau-reader.rb

--- Casks/tableau-reader.rb
+++ Casks/tableau-reader.rb
@@ -4,6 +4,7 @@ cask "tableau-reader" do
 
   url "https://downloads.tableau.com/tssoftware/TableauReader-#{version.dots_to_hyphens}.dmg"
   name "Tableau Reader"
+  desc "Open and interact with data visualizations built in Tableau Desktop"
   homepage "https://www.tableau.com/products/reader"
 
   livecheck do
