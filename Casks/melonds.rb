c797db5bb9cbb978a7842337040cc67adbc41c28
dbrookman <53625739+dbrookman@users.noreply.github.com>
2021-04-27 00:05:53 -0400

    Update melonDS from 0.9.1 to 0.9.2
    Also changes the URL extension to account for change in distribution method.

--- Casks/melonds.rb
+++ Casks/melonds.rb
@@ -1,8 +1,8 @@
 cask "melonds" do
-  version "0.9.1"
-  sha256 "5928ad133661b6ea428264894160b87cc48cdfc79461fb220c72d984613d6a62"
+  version "0.9.2"
+  sha256 "80cc5c8b71eda3fdc1172809f6e957adc20899951facdd7cf1704317d5df6b86"
 
-  url "http://melonds.kuribo64.net/downloads/melonDS_#{version}_mac64.tar.gz"
+  url "http://melonds.kuribo64.net/downloads/melonDS_#{version}_mac64.dmg"
   name "melonDS"
   desc "Nintendo DS and DSi emulator"
   homepage "http://melonds.kuribo64.net/"
