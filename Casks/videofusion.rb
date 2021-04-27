0d24b261c4a6081dca7fbc3cceec82404dc90c35
Reno <25192197+singularitti@users.noreply.github.com>
2021-04-27 18:30:07 -0400

    Update videofusion from 1.2.3.0 to 1.2.5.0 (#104694)
    * Update videofusion from 1.2.3.0 to 1.2.5.0
    
    * Update videofusion.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/videofusion.rb
+++ Casks/videofusion.rb
@@ -1,6 +1,6 @@
 cask "videofusion" do
-  version "1.2.3.0"
-  sha256 "013f6f5ed2d9406de0df7823a66385a91767bd542f77e6159b3a35a0795c5837"
+  version "1.2.5.0"
+  sha256 "ba68157a96763f2547ae4e710c3bf64f6eea5bff96d603ad272302cd92e2672c"
 
   url "https://lf3-faceucdn-tos.pstatp.com/obj/faceu-packages/Jianying_Pro_#{version.dots_to_underscores}.pkg",
       verified: "lf3-faceucdn-tos.pstatp.com/obj/faceu-packages/"
@@ -8,6 +8,10 @@ cask "videofusion" do
   name "剪映专业版"
   desc "Video editor"
   homepage "https://lv.ulikecam.com/"
+
+  livecheck do
+    skip "No version information available"
+  end
 
   depends_on macos: ">= :mojave"
 
