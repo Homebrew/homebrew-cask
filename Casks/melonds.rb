5e1ba8af251d3c0082c68852ae4645778ee98ae0
dbrookman <53625739+dbrookman@users.noreply.github.com>
2021-04-27 17:58:02 -0400

    melonDS: Add ARM (#104695)
    * melonDS: Add ARM
    
    * melonDS: Fix SHA256

--- Casks/melonds.rb
+++ Casks/melonds.rb
@@ -1,8 +1,14 @@
 cask "melonds" do
   version "0.9.2"
-  sha256 "80cc5c8b71eda3fdc1172809f6e957adc20899951facdd7cf1704317d5df6b86"
 
-  url "http://melonds.kuribo64.net/downloads/melonDS_#{version}_mac64.dmg"
+  if Hardware::CPU.intel?
+    sha256 "253c3ce58c626d05c62444a347ccc6b3bd6e7698f685ffd404c9a51ac5c08a91"
+    url "http://melonds.kuribo64.net/downloads/melonDS_#{version}_mac64.dmg"
+  else
+    sha256 "5dc4f493aaabaac91bf8fdd79c5de1c5014daa2cce64718c43f461c2640c82ab"
+    url "http://melonds.kuribo64.net/downloads/melonDS_#{version}_macARM.dmg"
+  end
+
   name "melonDS"
   desc "Nintendo DS and DSi emulator"
   homepage "http://melonds.kuribo64.net/"
