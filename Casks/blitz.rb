7fd27e31b8482cb730246715627011d57814e8af
Hannes Braun <hannesbraun@mail.de>
2021-04-27 23:51:23 +0200

    Update blitz from 1.14.3 to 1.14.4 (#104690)

--- Casks/blitz.rb
+++ Casks/blitz.rb
@@ -1,6 +1,6 @@
 cask "blitz" do
-  version "1.14.3"
-  sha256 "982554ceb086c703c44af8ecba96bae69f6613f6ef37b64e9b029c6750aa7128"
+  version "1.14.4"
+  sha256 "97c5487f41069957ee4db73c197c317446da9be35e71a7913a32c71b2cfa19f4"
 
   url "https://dl.blitz.gg/download/Blitz-#{version}.dmg"
   name "Blitz"
