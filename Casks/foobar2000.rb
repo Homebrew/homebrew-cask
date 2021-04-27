a1f5c1b96f512f7fe2e716c1ae3c4b025967f903
Zhenfu Shi <szf1234@me.com>
2021-04-27 11:53:15 +0800

    Update foobar2000 from 2.2.16 to 2.2.17 (#104663)

--- Casks/foobar2000.rb
+++ Casks/foobar2000.rb
@@ -1,6 +1,6 @@
 cask "foobar2000" do
-  version "2.2.16"
-  sha256 "7e8a18395c96ce0337ffc36592d06f63e6817b15cb95c4de0255b3b6054aa8a4"
+  version "2.2.17"
+  sha256 "b10724d95724b3a2a0b32017a1348d5c1c3386b605dd324717957761eaeed6fa"
 
   url "https://www.foobar2000.org/files/foobar2000-v#{version}.dmg"
   name "foobar2000"
