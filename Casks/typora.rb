a9b4f9b983905dc2b615f1ab256ca96c36dfe37b
Vanilla <neko@hydev.org>
2021-04-27 10:44:17 +0800

    Update typora from 0.9.9.36.2,4901 to 0.10.3 (#104661)
    * Update typora from 0.9.9.36.2,4901 to 0.10.3
    
    * Update typora.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/typora.rb
+++ Casks/typora.rb
@@ -1,15 +1,16 @@
 cask "typora" do
-  version "0.9.9.36.2,4901"
-  sha256 "b7f0628cc0421a5cfbcd4fcfb07799091880f0f717494137ba19b5cfb24a424b"
+  version "0.10.3"
+  sha256 "905ad153b8d2652fc451cfcad85134d006671830d1cd6327c2b6fb855a9ee13a"
 
-  url "https://www.typora.io/download/Typora-#{version.before_comma}.dmg"
+  url "https://www.typora.io/download/Typora-#{version}.dmg"
   name "Typora"
   desc "Configurable document editor that supports Markdown"
   homepage "https://typora.io/"
 
   livecheck do
     url "https://www.typora.io/download/dev_update.xml"
-    strategy :sparkle
+    strategy :page_match
+    regex(/Typora-(\d+(?:\.\d+)*)\.dmg/i)
   end
 
   auto_updates true
