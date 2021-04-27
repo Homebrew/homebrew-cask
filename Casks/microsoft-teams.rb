519a217ae0fe00dd946782cc2616626a2a063634
Yusuf Kursad Kaya <ykursadkaya@gmail.com>
2021-04-27 03:03:32 +0300

    Update microsoft-teams from 1.4.00.7175 to 1.4.00.8872

--- Casks/microsoft-teams.rb
+++ Casks/microsoft-teams.rb
@@ -1,6 +1,6 @@
 cask "microsoft-teams" do
-  version "1.4.00.7175"
-  sha256 "b5c7ae5c96f84d7427b05bb41d697b5f5d2036a0903b0aeeb6c59bcd7eff1125"
+  version "1.4.00.8872"
+  sha256 "a93fba70f91acbfee641a140aadf8da9bfe411d7c8117495d3dd0c07397d5ed4"
 
   url "https://statics.teams.microsoft.com/production-osx/#{version}/Teams_osx.pkg"
   name "Microsoft Teams"
