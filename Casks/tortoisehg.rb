29e63c58798afeb37ee7c7ef61d210dfcda1347f
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-27 10:11:05 +0800

    Update tortoisehg.rb

--- Casks/tortoisehg.rb
+++ Casks/tortoisehg.rb
@@ -4,9 +4,15 @@ cask "tortoisehg" do
 
   url "https://www.mercurial-scm.org/release/tortoisehg/macos/TortoiseHg-#{version}-mac-x64-qt5.dmg",
       verified: "mercurial-scm.org/release/tortoisehg/"
-  appcast "https://www.mercurial-scm.org/release/tortoisehg/macos/"
   name "TortoiseHg"
+  desc "Tools for the Mercurial distributed revision control system"
   homepage "https://tortoisehg.bitbucket.io/"
+
+  livecheck do
+    url "https://www.mercurial-scm.org/release/tortoisehg/macos/"
+    strategy :page_match
+    regex(/TortoiseHg-(\d+(?:\.\d+)*)-mac-x64-qt5\.dmg/i)
+  end
 
   app "TortoiseHg.app"
 end
