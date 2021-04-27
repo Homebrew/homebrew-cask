7a979308b3b8d80c557f87908489e690bff533b5
Michael Cho <cho-m@tuta.io>
2021-04-27 15:29:50 -0700

    Update chalk from 1.6.8 to 1.6.9 and add livecheck (#104705)

--- Casks/chalk.rb
+++ Casks/chalk.rb
@@ -1,12 +1,17 @@
 cask "chalk" do
-  version "1.6.8"
-  sha256 "8ffa3b1aba42c446e99dc9f5b70462117364ad776ff7693ea7110eb217c59a82"
+  version "1.6.9"
+  sha256 "d8f9948681a234666c1a943f131c176048f2ddc495fc50febbe6c41cee9e07e9"
 
   url "https://pierre.chachatelier.fr/chalk/downloads/Chalk-#{version.dots_to_underscores}.dmg",
       user_agent: :fake
-  appcast "https://pierre.chachatelier.fr/chalk/downloads/chalk-sparkle-en.rss"
   name "Chalk"
+  desc "Calculator software"
   homepage "https://www.chachatelier.fr/chalk/"
+
+  livecheck do
+    url "https://pierre.chachatelier.fr/chalk/downloads/chalk-sparkle-en.rss"
+    strategy :sparkle
+  end
 
   auto_updates true
 
