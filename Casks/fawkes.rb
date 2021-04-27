5879daa43aa85b03b43fe343b715eb308adb43d2
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-28 06:44:39 +0800

    Update fawkes.rb

--- Casks/fawkes.rb
+++ Casks/fawkes.rb
@@ -1,6 +1,6 @@
 cask "fawkes" do
   version "1.0"
-  sha256 "83eb4f646cbe8ef31c7aa9b92877f371c9022fd64b5f3048b300e6f7f30db48b"
+  sha256 "2dc8c1f4087908b08ba1bf1b49ec7716004fa0004a3a7ce2cd7dd9fb2ff135bf"
 
   url "https://mirror.cs.uchicago.edu/fawkes/files/#{version.major_minor}/Fawkes-#{version}.dmg"
   name "Fawkes"
