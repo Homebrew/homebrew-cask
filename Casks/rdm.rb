1c0710f1cc5037fb1141d5a5936a0ad5907fc760
Miro Dojkic <mdojkic@gmail.com>
2021-04-28 21:29:08 +0200

    Rename Cask to rdm

--- Casks/rdm.rb
+++ Casks/rdm.rb
@@ -1,9 +1,9 @@
-cask "usr-sse2-rdm" do
+cask "rdm" do
   version "2.5.0"
   sha256 "9d583e43742018e45bad79c8af9e0accec6b6a10d94c60ac7d31721bbb3c237e"
 
   url "https://github.com/usr-sse2/RDM/releases/download/#{version}/RDM.zip"
-  name "usr-sse2-rdm"
+  name "rdm"
   desc "Utility to set a Retina display to custom resolutions"
   homepage "https://github.com/usr-sse2/RDM"
 
