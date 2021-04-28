3475dce215e618abed09fa03cb72bd260cdda91d
Michael Cho <cho-m@tuta.io>
2021-04-28 12:38:33 -0700

    Update neo4j from 1.3.11 to 1.4.3 and add livecheck

--- Casks/neo4j.rb
+++ Casks/neo4j.rb
@@ -1,12 +1,20 @@
 cask "neo4j" do
   # NOTE: "4" is not a version number, but an intrinsic part of the product name
-  version "1.3.11"
-  sha256 "5d60752d712f0e708cdf94d68c287508967ff79f8fd89bcca593d49a3982cc86"
+  version "1.4.3"
+  sha256 "2aa524605d529520495499e80334b6d8dc7f180217f37fa1dd90be2d86ee20e0"
 
   url "https://neo4j.com/artifact.php?name=neo4j-desktop-#{version}.dmg"
-  appcast "https://neo4j.com/download/"
   name "Neo4j Desktop"
+  desc "Developer IDE or Management Environment for Neo4j instances"
   homepage "https://neo4j.com/download/"
+
+  livecheck do
+    url "https://neo4j.com/download-center/#desktop"
+    strategy :page_match
+    regex(%r{href=.*?/neo4j-desktop/.*?flavour=osx.*?release=(\d+(?:\.\d+)*)}i)
+  end
+
+  depends_on macos: ">= :yosemite"
 
   app "Neo4j Desktop.app"
 end
