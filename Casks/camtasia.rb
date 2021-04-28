07185e0597923f336102b703505ce9c8e80156c4
Michael Cho <cho-m@tuta.io>
2021-04-28 08:58:19 -0700

    Update camtasia from 2020.0.18 to 2021.0.0 and add livecheck (#104732)

--- Casks/camtasia.rb
+++ Casks/camtasia.rb
@@ -1,14 +1,20 @@
 cask "camtasia" do
-  version "2020.0.18,114362"
-  sha256 :no_check
+  version "2021.0.0"
+  sha256 "1afed5fb6bef606eae88d116247bb8ba5d678b33716c342ea8a548d2a3f05240"
 
-  url "https://download.techsmith.com/camtasiamac/releases/Camtasia.dmg"
-  appcast "https://support.techsmith.com/hc/en-us/articles/115006624748-Camtasia-Mac-Version-History"
+  url "https://download.techsmith.com/camtasiamac/releases/#{version.major[-2..]}#{version.minor_patch.no_dots}/Camtasia.dmg"
   name "Camtasia"
+  desc "Screen recorder and video editor"
   homepage "https://www.techsmith.com/camtasia.html"
 
+  livecheck do
+    url "https://support.techsmith.com/hc/en-us/articles/115006624748-Camtasia-Mac-Version-History"
+    strategy :page_match
+    regex(/Camtasia\s*\(Mac\)\s*(\d+(?:\.\d+)*)/i)
+  end
+
   auto_updates true
-  depends_on macos: ">= :high_sierra"
+  depends_on macos: ">= :mojave"
 
   app "Camtasia #{version.major}.app"
 end
