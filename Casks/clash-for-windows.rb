496c6d353f9b18fef884d56df128d187cd2c5a75
FAN VINGA <fanalcest@gmail.com>
2021-04-28 18:50:04 +0800

    Add arm64 version for clash-for-windows

--- Casks/clash-for-windows.rb
+++ Casks/clash-for-windows.rb
@@ -1,8 +1,13 @@
 cask "clash-for-windows" do
   version "0.15.4"
-  sha256 "749729ed774af61aeac08f577f153d2ab0c351ba281fdf708f864a9617bce913"
 
-  url "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/#{version}/Clash.for.Windows-#{version}.dmg"
+  if Hardware::CPU.intel?
+    url "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/#{version}/Clash.for.Windows-#{version}.dmg"
+    sha256 "749729ed774af61aeac08f577f153d2ab0c351ba281fdf708f864a9617bce913"
+  else
+    url "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/#{version}/Clash.for.Windows-#{version}-arm64.dmg"
+    sha256 "c6e1766e6bf15cf80d37871401b840516c8896dcb418770f4e9faaaf316db0b3"
+  
   name "Clash for Windows"
   desc "GUI based on Clash"
   homepage "https://github.com/Fndroid/clash_for_windows_pkg"
