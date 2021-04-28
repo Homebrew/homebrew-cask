b6ef59d9c979041791461662e34b87e92cde0f09
Michael Cho <cho-m@tuta.io>
2021-04-28 00:24:14 -0700

    Update catch from 2.0 to 2.0.1 (#104734)

--- Casks/catch.rb
+++ Casks/catch.rb
@@ -1,11 +1,14 @@
 cask "catch" do
-  version "2.0"
-  sha256 "030a9ea67418f8f4b2469b7d23341dbe46d7cd5c4a661458f5a28043165285d7"
+  version "2.0.1"
+  sha256 "48b6bba277db609cf5848d35d8a4325e979b3e8ef21da13f935d8885bb19c135"
 
   url "https://github.com/mipstian/catch/releases/download/#{version}/Catch-#{version}.zip",
       verified: "github.com/mipstian/catch/"
   name "Catch"
-  homepage "https://www.giorgiocalderolla.com/index.html#catch"
+  desc "Broadcatching made easy"
+  homepage "https://www.giorgiocalderolla.com/catch.html"
+
+  depends_on macos: ">= :el_capitan"
 
   app "Catch.app"
 end
