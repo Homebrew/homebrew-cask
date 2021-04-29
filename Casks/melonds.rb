905b24fb2f71aa233ca6e3695943d37c172c7078
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-29 09:49:13 +0800

    Update melonds.rb

--- Casks/melonds.rb
+++ Casks/melonds.rb
@@ -4,21 +4,16 @@ cask "melonds" do
   if Hardware::CPU.intel?
     sha256 "253c3ce58c626d05c62444a347ccc6b3bd6e7698f685ffd404c9a51ac5c08a91"
     url "https://github.com/Arisotura/melonDS/releases/download/#{version}/melonDS_#{version}_mac64.dmg",
-        verified: "github.com/Arisotura/melonDS"
+        verified: "github.com/Arisotura/melonDS/"
   else
     sha256 "5dc4f493aaabaac91bf8fdd79c5de1c5014daa2cce64718c43f461c2640c82ab"
     url "https://github.com/Arisotura/melonDS/releases/download/#{version}/melonDS_#{version}_macARM.dmg",
-        verified: "github.com/Arisotura/melonDS"
+        verified: "github.com/Arisotura/melonDS/"
   end
 
   name "melonDS"
   desc "Nintendo DS and DSi emulator"
   homepage "http://melonds.kuribo64.net/"
-
-  livecheck do
-    url "https://github.com/Arisotura/melonDS"
-    strategy :github_latest
-  end
 
   app "melonDS.app"
 
