d70ad3f68a8fb967a2bf421ff4b3479f240e2bd4
Michael Cho <cho-m@tuta.io>
2021-04-30 13:18:35 -0700

    Update tempo from 5.5.4 to 5.14.0 and add livecheck

--- Casks/tempo.rb
+++ Casks/tempo.rb
@@ -1,11 +1,16 @@
 cask "tempo" do
-  version "5.5.4"
-  sha256 "e8c3c62ef616486551a463ce50dba9e5f03bbb45a135c6a1dc12c77d8ed49345"
+  version "5.14.0"
+  sha256 "4683ab958633cbe5054dc7a86166d8752f69e17eeb7bd65deb73bf84195f87aa"
 
   url "https://download.yourtempo.co/release/Tempo-#{version}.dmg"
-  appcast "https://download.yourtempo.co/release/latest-mac.yml"
   name "Tempo"
+  desc "Email client that delivers all email in batches"
   homepage "https://www.yourtempo.co/"
+
+  livecheck do
+    url "https://download.yourtempo.co/release/latest-mac.yml"
+    strategy :electron_builder
+  end
 
   auto_updates true
 
