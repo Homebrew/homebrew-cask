188016fa30584fcac974af109b080e2c4b3b062a
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-30 08:51:39 +0800

    Update spideroakone.rb

--- Casks/spideroakone.rb
+++ Casks/spideroakone.rb
@@ -10,9 +10,9 @@ cask "spideroakone" do
     url "https://spideroak.com/release/spideroak/osx"
   end
 
-  name 'SpiderOakOne'
-  desc 'Cloud backup and storage'
-  homepage 'https://spideroak.com/'
+  name "SpiderOakOne"
+  desc "Cloud backup and storage"
+  homepage "https://spideroak.com/"
 
   pkg "SpiderOakONE.pkg"
 
