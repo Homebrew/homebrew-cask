aab3fff86cc1a7444dc03e1fc03fa97cf159b59f
Sir Pesto ⚡️ <sirpesto@protonmail.com>
2021-04-30 12:53:09 +0200

    Fix name in Specter.app Cask

--- Casks/specter.rb
+++ Casks/specter.rb
@@ -3,7 +3,7 @@ cask "specter" do
   sha256 "4307f0fcd4c74c2ef83be05a882098d435e806af24d6e8dbb4de05a0b6c18b49"
 
   url "https://github.com/cryptoadvance/specter-desktop/releases/download/v#{version}/SpecterDesktop-v#{version}.dmg", verified: "github.com/cryptoadvance/specter-desktop/"
-  name "specter"
+  name "Specter"
   desc "Desktop GUI for Bitcoin Core optimised to work with hardware wallets"
   homepage "https://specter.solutions/"
 
