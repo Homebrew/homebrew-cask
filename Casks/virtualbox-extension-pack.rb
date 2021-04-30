2b39f82c64a6274ff484bdd6f4a39e921104315c
Erik Minekus <ErikMinekus@users.noreply.github.com>
2021-04-30 02:50:32 +0200

    Update virtualbox-extension-pack from 6.1.20 to 6.1.22 (#104803)

--- Casks/virtualbox-extension-pack.rb
+++ Casks/virtualbox-extension-pack.rb
@@ -1,6 +1,6 @@
 cask "virtualbox-extension-pack" do
-  version "6.1.20"
-  sha256 "15bdee9c0385ddd4cdbbe0e446c0180ad4ae21554ddd52f8b44783aebd4aeef4"
+  version "6.1.22"
+  sha256 "6d33d9cc1c5a8f8a2a70e5aaaa778a341322d2ba7eb34f7de420fb5f312b9e87"
 
   url "https://download.virtualbox.org/virtualbox/#{version}/Oracle_VM_VirtualBox_Extension_Pack-#{version}.vbox-extpack"
   name "Oracle VirtualBox Extension Pack"
