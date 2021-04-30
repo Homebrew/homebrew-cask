284a0f776ad6e910cb870aba39509b1987a7a730
Eric Norman <eric@norman.zone>
2021-04-30 13:11:11 -0700

    Simplify version logic since only 2 paths not 3

--- Casks/spideroakone.rb
+++ Casks/spideroakone.rb
@@ -2,9 +2,7 @@ cask "spideroakone" do
   version "7.5.0"
   sha256 :no_check
 
-  if MacOS.version <= :sierra
-    url "https://spideroak.com/release/spideroak/osx"
-  elsif MacOS.version <= :high_sierra
+  if MacOS.version == :high_sierra
     url "https://spideroak.com/release/spideroak/osx_hs"
   else
     url "https://spideroak.com/release/spideroak/osx"
