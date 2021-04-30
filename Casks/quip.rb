08e480686f09cf08533a0bfabaa8d4d9307802a5
Michael Cho <cho-m@tuta.io>
2021-04-30 13:49:49 -0700

    Update quip from 7.31.1 to 7.34.3 and add livecheck

--- Casks/quip.rb
+++ Casks/quip.rb
@@ -1,15 +1,20 @@
 cask "quip" do
-  version "7.31.1"
-  sha256 "b765accf4ef50204de8196084709fb0a4f52846f753537d3198e64dcb7de25bb"
+  version "7.34.3"
+  sha256 "785ae23fcbf684c66a9ae4e122428c088ccb304a5d83a18d49d2a0366bb12543"
 
   url "https://quip-clients.com/macosx_#{version}.dmg",
       verified: "quip-clients.com/"
-  appcast "https://api.quip.com/-/sparkle-feed?manual=0"
   name "Quip"
   desc "Tool for teams to create living documents"
   homepage "https://quip.com/"
 
+  livecheck do
+    url "https://api.quip.com/-/sparkle-feed?manual=0"
+    strategy :sparkle
+  end
+
   auto_updates true
+  depends_on macos: ">= :high_sierra"
 
   app "Quip.app"
 end
