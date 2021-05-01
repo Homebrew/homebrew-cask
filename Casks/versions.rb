ff6e70e6bb9e9c824a11e5089839bfc87a4fce9c
Michael Cho <cho-m@tuta.io>
2021-05-01 00:24:45 -0700

    Update versions from 1.4.1-1144 to 1.4.4,1147 and add livecheck (#104852)

--- Casks/versions.rb
+++ Casks/versions.rb
@@ -1,11 +1,16 @@
 cask "versions" do
-  version "1.4.1-1144"
-  sha256 "84d4320883b97140b920f3bf4fa95285cb4ecfc9a13c58ef346288f5d37db208"
+  version "1.4.4,1147"
+  sha256 "90c48061e30c6290914a5684e8f9770647430df5a85476a0365ac8c415e14709"
 
-  url "https://cdn.versionsapp.com/releases/Versions-#{version}.zip"
-  appcast "https://updates.blackpixel.com/updates?app=vs"
+  url "https://updates.versionsapp.com/v1/prod/Versions-#{version.before_comma}.zip"
   name "Versions"
+  desc "Subversion client"
   homepage "https://versionsapp.com/"
+
+  livecheck do
+    url "https://updates.versionsapp.com/v1/prod/appcast"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :sierra"
 
