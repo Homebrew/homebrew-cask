1cf1b98848489acaa184ec1159c3e34f31831407
XeL_Arjona <shell_ramix@hotmail.com>
2021-05-01 07:51:53 -0500

    Upgrade TradingView.app to 1.0.0-beta.3, new URL's and livecheck. (#104780)
    * Upgrade TradingView.app to 1.0.0-beta.3, new URL's and livecheck.
    
    App upgrade to 1.0.0-beta.3 as new definitions to correct URL's addressing dmg's to {darwin/x64} hrefs, livechek also defined to 'skip' as main homepage do not have version validation other than :latest.
    
    * Update Casks/tradingview.rb
    
    brew livecheck --debug tradingview --Match current version.
    
    Co-authored-by: Michael Cho <cho-m@tuta.io>
    
    Co-authored-by: Michael Cho <cho-m@tuta.io>

--- Casks/tradingview.rb
+++ Casks/tradingview.rb
@@ -1,16 +1,15 @@
 cask "tradingview" do
-  version "1.0.0-beta.1"
-  sha256 "03b03d765ff7f8cb1f3059490b64344e38aa35bf44e161327b7b8baeac66aa2c"
+  version "1.0.0-beta.3"
+  sha256 "4223b41a138e2904383f9970d2a4289db933365183ffd30f901b83e1b7a659ed"
 
-  url "https://tvd-packages.tradingview.com/beta/#{version}/win32/x64/TradingView.dmg"
-  name "TradingView Desktop Beta"
-  desc "Desktop client for TradingView"
+  url "https://tvd-packages.tradingview.com/stable/#{version}/darwin/x64/TradingView.dmg"
+  name "TradingView Desktop"
+  desc "Experience with the power and flexibility of a desktop application"
   homepage "https://www.tradingview.com/desktop/"
 
   livecheck do
-    url :homepage
-    strategy :page_match
-    regex(%r{href=.*?/(\d+(?:\.\d+)*-beta\.\d+)/win32/x64/TradingView\.dmg}i)
+    url "https://tvd-packages.tradingview.com/stable/latest/darwin/x64/stable-mac.yml"
+    strategy :electron_builder
   end
 
   app "TradingView.app"
