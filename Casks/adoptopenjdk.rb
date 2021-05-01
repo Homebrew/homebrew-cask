e3d57b570685506bc78a877a279acee8c4a11e32
Vítor Galvão <info@vitorgalvao.com>
2021-05-01 16:17:22 +0100

    adoptopenjdk.rb: remove mention of external tap

--- Casks/adoptopenjdk.rb
+++ Casks/adoptopenjdk.rb
@@ -20,11 +20,4 @@ cask "adoptopenjdk" do
   pkg "OpenJDK#{version.major}U-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma.major}.pkg"
 
   uninstall pkgutil: "net.adoptopenjdk.#{version.major}.jdk"
-
-  caveats <<~EOS
-    More versions are available in the AdoptOpenJDK tap:
-      #{Formatter.url("https://github.com/AdoptOpenJDK/homebrew-openjdk")}
-
-      brew tap adoptopenjdk/openjdk
-  EOS
 end
