b3face16c92a0d5e395036941eddc6217e5278cb
Michael Cho <cho-m@tuta.io>
2021-05-01 15:36:56 -0700

    izip: add livecheck

--- Casks/izip.rb
+++ Casks/izip.rb
@@ -3,10 +3,17 @@ cask "izip" do
   sha256 :no_check
 
   url "https://www.izip.com/izip.dmg"
-  appcast "https://www.izip.com/download"
   name "iZip"
   desc "App to manage ZIP, ZIPX, RAR, TAR, 7ZIP and other compressed files"
   homepage "https://www.izip.com/"
+
+  livecheck do
+    url "https://www.izip.com/download"
+    strategy :page_match
+    regex(%r{<li>Version:\s*(\d+(?:\.\d+)*)</li>}i)
+  end
+
+  depends_on macos: ">= :yosemite"
 
   app "iZip.app"
 end
