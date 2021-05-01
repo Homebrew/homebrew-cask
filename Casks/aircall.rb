d3686e0ef51c5b529d8237c3ebca3ae6e6b9c87d
Michael Cho <cho-m@tuta.io>
2021-05-01 02:07:26 -0700

    Update aircall from 2.9.0 to 2.15.3 and add livecheck

--- Casks/aircall.rb
+++ Casks/aircall.rb
@@ -1,15 +1,19 @@
 cask "aircall" do
-  version "2.9.0"
-  sha256 "f91cfc506e75a403306f4a36b95e8da3a88c1ed849145fbc2ce68b14f5690015"
+  version "2.15.3"
+  sha256 "6777fe2dce13266bb22f4616b18c1e9bb199c478da72cc2d5e37c4b26da2352b"
 
-  url "https://aircall-electron-releases.s3.amazonaws.com/production/Aircall-#{version}.zip",
-      verified: "aircall-electron-releases.s3.amazonaws.com/"
-  appcast "https://electron.aircall.io/update/osx/0.0.0"
+  url "https://download-electron.aircall.io/Aircall-#{version}.zip"
   name "Aircall"
   desc "Cloud-based call center and phone system software"
   homepage "https://aircall.io/"
 
+  livecheck do
+    url "https://electron.aircall.io/download/osx"
+    strategy :header_match
+  end
+
   auto_updates true
+  depends_on macos: ">= :sierra"
 
   app "Aircall.app"
 end
