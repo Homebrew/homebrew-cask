6620078f39e1dcd9f932943d5178ce0fb0a99965
Eitot <Eitot@users.noreply.github.com>
2021-05-02 13:33:08 +0200

    vienna: Move URL from Bintray to GitHub Releases

--- Casks/vienna.rb
+++ Casks/vienna.rb
@@ -2,8 +2,8 @@ cask "vienna" do
   version "3.7.1,7437"
   sha256 "3b6879e55ccc95ba965e6868084f5f3dd1d8d937f20f7c95730f1b8aedc2ba9a"
 
-  url "https://dl.bintray.com/viennarss/vienna-rss/Vienna#{version.before_comma}.tar.gz",
-      verified: "bintray.com/viennarss/"
+  url "https://github.com/ViennaRSS/vienna-rss/releases/download/v%2F#{version.before_comma}/Vienna#{version.before_comma}.tar.gz",
+      verified: "github.com/ViennaRSS/vienna-rss/"
   name "Vienna"
   desc "RSS and Atom reader"
   homepage "https://www.vienna-rss.com/"
