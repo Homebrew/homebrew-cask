d542ef6d0f06471e1b76441a614c5fb81c5a4563
Michael Cho <cho-m@tuta.io>
2021-05-02 12:03:55 -0700

    Update dcp-o-matic-player from 2.14.47 to 2.14.48 and add livecheck

--- Casks/dcp-o-matic-player.rb
+++ Casks/dcp-o-matic-player.rb
@@ -1,11 +1,17 @@
 cask "dcp-o-matic-player" do
-  version "2.14.47"
-  sha256 "41f67135e04e48d9a530093bcec274410d02e27f1b7ce86b0ea8b289b8ac214f"
+  version "2.14.48"
+  sha256 "7e4a8cf3ab93ab0e2b6fd18263fd749841306f431534328d363f4eafb683eeb4"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.9-player&version=#{version}"
-  appcast "https://dcpomatic.com/download"
   name "DCP-o-matic Player"
+  desc "Play Digital Cinema Packages"
   homepage "https://dcpomatic.com/"
+
+  livecheck do
+    url "https://dcpomatic.com/download"
+    strategy :page_match
+    regex(/href=.*?id=osx[0-9._-]*player[^"']*?version=(\d+(?:\.\d+)*)/i)
+  end
 
   app "DCP-o-matic #{version.major} Player.app"
 end
