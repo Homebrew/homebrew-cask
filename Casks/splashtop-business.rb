3b025532ed5b7cc59b1ade35a338e7e274c935ff
Michael Cho <cho-m@tuta.io>
2021-05-02 23:59:43 -0700

    Update splashtop-business from 3.4.2.0 to 3.4.4.0 and add livecheck (#104899)

--- Casks/splashtop-business.rb
+++ Casks/splashtop-business.rb
@@ -1,13 +1,18 @@
 cask "splashtop-business" do
-  version "3.4.2.0"
-  sha256 "3e4cbbb9d292eb2843bac09dc53ec7379b1f9b8d86f8f93a273c0c6cab0cc13a"
+  version "3.4.4.0"
+  sha256 "f5e3ac5fe792bd9c3073a70e40cc62b9f4aa766a74cdf0efc71e20d777ce8a0c"
 
   url "https://d17kmd0va0f0mp.cloudfront.net/macclient/STB/Splashtop_Business_Mac_INSTALLER_v#{version}.dmg",
       verified: "d17kmd0va0f0mp.cloudfront.net/macclient/STB/"
-  appcast "https://www.splashtop.com/wp-content/themes/responsive/downloadx.php?product=stb&platform=mac-client"
   name "Splashtop Business"
   desc "Remote access software"
   homepage "https://www.splashtop.com/business"
+
+  livecheck do
+    url "https://www.splashtop.com/wp-content/themes/responsive/downloadx.php?product=stb&platform=mac-client"
+    strategy :page_match
+    regex(/Splashtop_Business_Mac_INSTALLER_v(\d+(?:\.\d+)*)\.dmg/i)
+  end
 
   pkg "Splashtop Business.pkg"
 
