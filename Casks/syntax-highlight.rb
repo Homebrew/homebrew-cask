21b71f8af8dab40685c56fe1d18e3cacd0a8c6ab
AdamXweb <6800453+AdamXweb@users.noreply.github.com>
2021-05-03 18:11:44 +1000

    update syntax-highlight from v1.0.b31 to v2.0.3 (#104785)
    * update syntax-hightlight to v2.0.0 from v1.0.b31
    
    * cask styling
    
    * cask styling
    
    * update syntax-highlight from v2.0.0 to v2.0.1
    
    * update syntax-highlight from v2.0.1 to v2.0.2
    
    * remove syntax-highlight from prerelease list
    
    * remove unwanted header text
    
    * update syntax-highlight from v2.0.2 to v2.0.3

--- Casks/syntax-highlight.rb
+++ Casks/syntax-highlight.rb
@@ -1,13 +1,24 @@
 cask "syntax-highlight" do
-  version "1.0.b31"
-  sha256 "785243d13ed951b7903cf5f72f82ceebb5acd0106d52bb84d31cc6c3e3ba44a0"
+  version "2.0.3"
+  sha256 "6b46416b926941f992381bd215068fdcf503b1fddef718edad5f52f1228ae4cb"
 
   url "https://github.com/sbarex/SourceCodeSyntaxHighlight/releases/download/#{version}/Syntax.Highlight.zip"
   name "Syntax Highlight"
   desc "Quicklook extension for source files"
   homepage "https://github.com/sbarex/SourceCodeSyntaxHighlight"
 
+  auto_updates true
   depends_on macos: ">= :catalina"
 
   app "Syntax Highlight.app"
+
+  zap trash: [
+    "~Library/Application Scripts/org.sbarex.SourceCodeSyntaxHighlight",
+    "~Library/Application Scripts/org.sbarex.SourceCodeSyntaxHighlight.QuicklookExtension",
+    "~Library/Application Support/Syntax Highlight",
+    "~Library/Caches/com.apple.helpd/Generated/org.sbarex.SourceCodeSyntaxHighlight.help*1.0",
+    "~Library/Containers/org.sbarex.SourceCodeSyntaxHighlight",
+    "~Library/Containers/org.sbarex.SourceCodeSyntaxHighlight.QuicklookExtension",
+    "~Library/Preferences/org.sbarex.SourceCodeSyntaxHighlight.plist",
+  ]
 end
