d8b1159e8c555e3a6c8f32ad5a248acdd143cbf5
LordWire <ns.gavalas@gmail.com>
2021-05-03 17:11:54 +0200

    cryptonomic-galleon audited and style-fixed

--- Casks/cryptonomic-galleon.rb
+++ Casks/cryptonomic-galleon.rb
@@ -1,14 +1,12 @@
 cask "cryptonomic-galleon" do
-    version "1.1.20b"
-    sha256 "51896518f06ba333a47cc7e4f0e849a9b5e098e0da63fb7e467557f4eaebd387"
-  
-    url "https://cryptonomic-wallet.nyc3.digitaloceanspaces.com/release_#{version}/Galleon-#{version.delete_suffix('b')}.dmg",
-        verified: "cryptonomic-wallet.nyc3.digitaloceanspaces.com"
-    name "Galleon"
-    desc "A software wallet for the Tezos blockchain"
-    homepage "https://cryptonomic.tech/galleon.html"
-  
-    app "Galleon.app"
-  
-    zap trash: "~/.shuttle.json"
-  end
+  version "1.1.20b"
+  sha256 "51896518f06ba333a47cc7e4f0e849a9b5e098e0da63fb7e467557f4eaebd387"
+
+  url "https://cryptonomic-wallet.nyc3.digitaloceanspaces.com/release_#{version}/Galleon-#{version.delete_suffix("b")}.dmg",
+      verified: "cryptonomic-wallet.nyc3.digitaloceanspaces.com"
+  name "Galleon"
+  desc "Software wallet for the Tezos blockchain"
+  homepage "https://cryptonomic.tech/galleon.html"
+
+  app "Galleon.app"
+end
