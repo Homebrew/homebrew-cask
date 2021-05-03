f2b34af3946798433769e4cea91f5a631751ac83
Michael Cho <cho-m@tuta.io>
2021-05-02 22:59:26 -0700

    Update valentina-studio from 11.2.3 to 11.2.6

--- Casks/valentina-studio.rb
+++ Casks/valentina-studio.rb
@@ -1,5 +1,5 @@
 cask "valentina-studio" do
-  version "11.2.3"
+  version "11.2.6"
   sha256 :no_check
 
   url "https://valentina-db.com/en/all-downloads/vstudio/current/vstudio_mac_x64-dmg?format=raw"
