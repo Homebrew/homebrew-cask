cad760b0718d7e86a5c0f0028d07f2eb134c6978
Michael Cho <cho-m@tuta.io>
2021-05-03 00:02:48 -0700

    Update waterfox from 3.0.2 to 3.2.1 and add livecheck (#104933)

--- Casks/waterfox.rb
+++ Casks/waterfox.rb
@@ -1,12 +1,17 @@
 cask "waterfox" do
-  version "3.0.2,7820.12.15"
-  sha256 "0b215d7779af3c59eee7a43c84bd9695443c8d07e05db250343cdee5f7f56aa2"
+  version "3.2.1"
+  sha256 "19ead1d180d1e865e50fe20e46cb57215aed83b8954c409b96599bd83ea4c445"
 
-  url "https://cdn.waterfox.net/releases/osx64/installer/Waterfox%20G#{version.before_comma}%20Setup.dmg"
-  appcast "https://www.waterfox.net/download/"
+  url "https://cdn.waterfox.net/releases/osx64/installer/Waterfox%20G#{version}%20Setup.dmg"
   name "Waterfox"
   desc "Web browser"
   homepage "https://www.waterfox.net/"
+
+  livecheck do
+    url "https://www.waterfox.net/download/"
+    strategy :page_match
+    regex(%r{href=.*?/Waterfox%20G(\d+(?:\.\d+)*)%20Setup\.dmg}i)
+  end
 
   app "Waterfox.app"
 
