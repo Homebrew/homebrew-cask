0f5ab46e4541223dd7fef5c065c4e4887b1da36f
Michael Cho <cho-m@tuta.io>
2021-05-02 23:58:12 -0700

    Update retroarch-metal from 1.9.1 to 1.9.2 and add livecheck (#104904)

--- Casks/retroarch-metal.rb
+++ Casks/retroarch-metal.rb
@@ -1,12 +1,17 @@
 cask "retroarch-metal" do
-  version "1.9.1"
-  sha256 "f261dede40ea75e5b4f8bad6435fc5a41619a2fe9d882f75d3d6c2800d26665b"
+  version "1.9.2"
+  sha256 "73d2f50f61967b0720c10ea2c2080de696513ea0973ddd421d277b3e26390057"
 
   url "https://buildbot.libretro.com/stable/#{version}/apple/osx/universal/RetroArch_Metal.dmg"
-  appcast "https://buildbot.libretro.com/stable/"
   name "RetroArch"
   desc "Emulator frontend (Metal graphics API version)"
   homepage "https://www.libretro.com/"
+
+  livecheck do
+    url "https://buildbot.libretro.com/stable/"
+    strategy :page_match
+    regex(%r{href="/stable/(\d+(?:\.\d+)*)/"}i)
+  end
 
   conflicts_with cask: [
     "retroarch",
