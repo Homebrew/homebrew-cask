6a0a6bfe68f39e845de231b581ef387a0f8f0074
Michael Cho <cho-m@tuta.io>
2021-05-03 00:07:51 -0700

    shimo: add livecheck (#104909)

--- Casks/shimo.rb
+++ Casks/shimo.rb
@@ -1,13 +1,17 @@
 cask "shimo" do
-  version "5.0.3_8887"
+  version "5.0.3,8887"
   sha256 "6438ea5627591bbb12e8b7b2796ade24fe8d59712d8c523443a5632e66dfc9f3"
 
-  url "https://downloads.mailbutler.io/Shimo_#{version}.zip",
+  url "https://downloads.mailbutler.io/Shimo_#{version.before_comma}_#{version.after_comma}.zip",
       verified: "downloads.mailbutler.io/"
-  appcast "https://www.shimovpn.com/appcast#{version.major}.php"
   name "Shimo"
   desc "VPN client for secure internet access and private browsing"
   homepage "https://www.shimovpn.com/"
+
+  livecheck do
+    url "https://www.shimovpn.com/appcast#{version.major}.php"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :high_sierra"
