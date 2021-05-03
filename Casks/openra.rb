b593f03ba18ee0cc253dc47ed2545da1c6be38d1
xxyzz <gitpull@protonmail.com>
2021-05-03 16:27:30 +0800

    add openra zap

--- Casks/openra.rb
+++ Casks/openra.rb
@@ -11,4 +11,11 @@ cask "openra" do
   app "OpenRA - Dune 2000.app"
   app "OpenRA - Red Alert.app"
   app "OpenRA - Tiberian Dawn.app"
+
+  zap trash: [
+    "~/Library/Application Support/OpenRA",
+    "~/Library/Saved Application State/net.openra.mod.cnc.savedState",
+    "~/Library/Saved Application State/net.openra.mod.d2k.savedState",
+    "~/Library/Saved Application State/net.openra.mod.ra.savedState",
+  ]
 end
