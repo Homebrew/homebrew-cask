4f3b48a268567ae17a7417577588fa0e08bc711a
Michael Cho <cho-m@tuta.io>
2021-05-02 23:53:46 -0700

    find-any-file: add livecheck (#104888)

--- Casks/find-any-file.rb
+++ Casks/find-any-file.rb
@@ -4,10 +4,16 @@ cask "find-any-file" do
 
   url "https://s3.amazonaws.com/files.tempel.org/FindAnyFile_#{version}.zip",
       verified: "s3.amazonaws.com/"
-  appcast "https://findanyfile.app/appcast2.php"
   name "Find Any File"
   desc "File finder"
   homepage "https://apps.tempel.org/FindAnyFile/"
+
+  livecheck do
+    url "https://findanyfile.app/appcast#{version.major}.php"
+    strategy :sparkle, &:short_version
+  end
+
+  depends_on macos: ">= :el_capitan"
 
   app "Find Any File.app"
 
