182276a9d455d4912a9c7a15f571126423d64f1a
dcunited08 <dcunited08@gmail.com>
2021-05-04 14:33:35 -0400

    microsoft-azure-storage-explorer v1.19.1 fixing Download url

--- Casks/microsoft-azure-storage-explorer.rb
+++ Casks/microsoft-azure-storage-explorer.rb
@@ -2,7 +2,7 @@ cask "microsoft-azure-storage-explorer" do
   version "1.19.1"
   sha256 "f586dde6cf896f58ef60737abfcedd80529a8f51ee2ace175d773ec5234c2db6"
 
-  url "https://github.com/microsoft/AzureStorageExplorer/releases/download/v#{version}/Mac_StorageExplorer.zip",
+  url "https://github.com/microsoft/AzureStorageExplorer/releases/download/V#{version}/Mac_StorageExplorer.zip",
       verified: "github.com/microsoft/AzureStorageExplorer/"
   name "Microsoft Azure Storage Explorer"
   desc "Explorer for Azure Storage"
