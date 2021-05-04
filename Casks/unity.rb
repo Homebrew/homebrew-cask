cc2d3b477c1324fa1781971d2d7bafcf5f2296fb
Michael Cho <cho-m@tuta.io>
2021-05-04 15:35:59 -0700

    Update unity.rb

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -1,6 +1,6 @@
 cask "unity" do
-  version "2021.1.3f1,4bef613afd59"
-  sha256 "00d4017d2f5fe31911189b0a28b9013466b20de941705e9dbc442dcba4d3e862"
+  version "2021.1.5f1,3737af19df53"
+  sha256 "65da172336cd5fa667779963de6ee10ea3ac99b27be08c850eaa39a94c1971f3"
 
   url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorInstaller/Unity-#{version.before_comma}.pkg",
       verified: "download.unity3d.com/download_unity/"
@@ -11,8 +11,9 @@ cask "unity" do
   livecheck do
     url "https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json"
     strategy :page_match do |page|
-      match = page.match(%r{href=([0-9a-f]+)/MacEditorInstaller/Unity-(\d+(?:.\d+)*)\.pkg}i)
-      "#{match[2]},#{match[1]}"
+      page.scan(%r{/download_unity/(\h+)/MacEditorInstaller/Unity-(\d+(?:\.\d+)*[a-z]*\d*)\.pkg}i).map do |match|
+        "#{match[1]},#{match[0]}"
+      end
     end
   end
 
