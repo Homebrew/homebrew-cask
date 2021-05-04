1567e38ffc4751296113c1a208a87b3dd4e75762
MLL <baremetalcoder@gmail.com>
2021-05-04 10:46:28 -0700

    Update smartgit from 20.2.4 to 20.2.5 (#105096)
    * Update smartgit from 20.2.4 to 20.2.5
    
    * Update smartgit.rb
    
    Co-authored-by: Michael Cho <cho-m@tuta.io>

--- Casks/smartgit.rb
+++ Casks/smartgit.rb
@@ -1,9 +1,10 @@
 cask "smartgit" do
-  version "20.2.4"
-  sha256 "753934c729ac808b882f6798bc84b49db45bbeb4a8e6c4399cebcf3d559b62c1"
+  version "20.2.5"
+  sha256 "2078c75ca902b55175063b87e7a6cd28cb0a3b4d0c4f0427ce7352bb9fda237f"
 
   url "https://www.syntevo.com/downloads/smartgit/smartgit-macosx-#{version.dots_to_underscores}.dmg"
   name "SmartGit"
+  desc "Git client"
   homepage "https://www.syntevo.com/smartgit/"
 
   livecheck do
