7c21759300240420e9a2200da2b7df20d49ec97b
James Upjohn <jammehcow@jammehcow.co.nz>
2021-05-04 21:22:02 +1200

    Bump chatterino version to 2.3.1

--- Casks/chatterino.rb
+++ Casks/chatterino.rb
@@ -1,5 +1,5 @@
 cask "chatterino" do
-  version "2.3.0"
+  version "2.3.1"
   sha256 "76d6f706967aadf0ab6bec460f6d5b0aaf9d7b95bc92dbe9f5057b037ce9d2f8"
 
   url "https://chatterino.fra1.digitaloceanspaces.com/bin/#{version}/Chatterino.dmg",
