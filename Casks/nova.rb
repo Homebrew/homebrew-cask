905ef53be907477df718771f8b44c6212999cfd2
HappyStriker <32582138+HappyStriker@users.noreply.github.com>
2021-05-04 21:38:53 +0200

    Update nova from 6.1 to 6.2 (#105102)
    * Update nova from 6.1 to 6.2
    
    * Update nova.rb
    
    Co-authored-by: Daniel Messner <messner.daniel@icloud.com>
    Co-authored-by: Michael Cho <cho-m@tuta.io>

--- Casks/nova.rb
+++ Casks/nova.rb
@@ -1,6 +1,6 @@
 cask "nova" do
-  version "6.1"
-  sha256 "430fe7b76ca76c0ca6f437a74766243a5e2263d95152e5308e1abf819bc2b238"
+  version "6.2"
+  sha256 "77b8591065a8f596a260a88f76a8bc1757ce71272723c4bf8fbd466e2dcade2d"
 
   url "https://download-cdn.panic.com/nova/Nova%20#{version}.zip",
       verified: "https://download-cdn.panic.com/nova/"
@@ -9,9 +9,8 @@ cask "nova" do
   homepage "https://nova.app/"
 
   livecheck do
-    url "https://download-cdn.panic.com/nova/"
-    strategy :page_match
-    regex(/nova\s(\d+(?:\.\d+)*)\.zip/i)
+    url :homepage
+    regex(%r{href=.*?/Nova(?:\s*|%20)?(\d+(?:\.\d+)*)\.zip}i)
   end
 
   auto_updates true
