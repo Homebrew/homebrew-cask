875534dbe578cf934c067d0c9583914f36d53f99
Weiqi Liu <callmeliuweiqi@gmail.com>
2021-05-05 01:26:47 +0800

    Update listen1 from 2.21.3 to 2.21.4 (#105099)

--- Casks/listen1.rb
+++ Casks/listen1.rb
@@ -1,7 +1,7 @@
 cask "listen1" do
   # NOTE: "1" is not a version number, but an intrinsic part of the product name
-  version "2.21.3"
-  sha256 "993bbc54d1576d7ca313978827e5a0a937a6e79ff10bb1d8f596de5d7a6a2cdf"
+  version "2.21.4"
+  sha256 "a88e22ba9087800ae2e3751b871f37c58dac33948399ef59ef167fe5880001e1"
 
   url "https://github.com/listen1/listen1_desktop/releases/download/v#{version}/Listen1_#{version}_mac.dmg",
       verified: "github.com/listen1/listen1_desktop/"
