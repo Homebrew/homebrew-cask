ca6ea3e8a8fa6408dc1bca8d1d2cf62d3193082b
Felix Honer <felix.honer@novatec-gmbh.de>
2021-05-04 19:49:13 +0200

    Update nosqlbooster-for-mongodb from 6.2.12 to 6.2.13 (#105093)
    * Update nosqlbooster-for-mongodb from 6.2.12 to 6.2.13
    
    * Update nosqlbooster-for-mongodb.rb
    
    Co-authored-by: Michael Cho <cho-m@tuta.io>

--- Casks/nosqlbooster-for-mongodb.rb
+++ Casks/nosqlbooster-for-mongodb.rb
@@ -1,11 +1,12 @@
 cask "nosqlbooster-for-mongodb" do
-  version "6.2.12"
-  sha256 "0de168ce0ddc18b48939023060e927b92ccbe1fd99a755e6400ad4710e9eb3a3"
+  version "6.2.13"
+  sha256 "a6f928e189260b7b9c217b13776204940635802a3359dd446a30487df2a02135"
 
   url "https://s3.mongobooster.com/download/releasesv#{version.major}/nosqlbooster4mongo-#{version}.dmg",
       verified: "mongobooster.com/"
   appcast "https://nosqlbooster.com/downloads"
   name "NoSQLBooster for MongoDB"
+  desc "GUI tool and IDE for MongoDB"
   homepage "https://nosqlbooster.com/"
 
   app "NoSQLBooster for MongoDB.app"
