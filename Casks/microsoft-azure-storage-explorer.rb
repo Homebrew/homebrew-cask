3f0ebbf0561f5e8fb8c79b5965e7563d44c3f50c
dcunited08 <dcunited08@gmail.com>
2021-05-04 17:58:31 -0400

    Updating MSFT Azure Storage Explorer to 1.19.1 (#105101)
    * Updating MSFT Azure Storage Explorer to 1.19.1
    
    * microsoft-azure-storage-explorer v1.19.1 fixing Download url

--- Casks/microsoft-azure-storage-explorer.rb
+++ Casks/microsoft-azure-storage-explorer.rb
@@ -1,8 +1,8 @@
 cask "microsoft-azure-storage-explorer" do
-  version "1.19.0"
-  sha256 "eee5249a445922acbe729d675daf198de249ae73ff3fe0b68cd2c187692d8fdd"
+  version "1.19.1"
+  sha256 "f586dde6cf896f58ef60737abfcedd80529a8f51ee2ace175d773ec5234c2db6"
 
-  url "https://github.com/microsoft/AzureStorageExplorer/releases/download/v#{version}/StorageExplorer_signed.zip",
+  url "https://github.com/microsoft/AzureStorageExplorer/releases/download/V#{version}/Mac_StorageExplorer.zip",
       verified: "github.com/microsoft/AzureStorageExplorer/"
   name "Microsoft Azure Storage Explorer"
   desc "Explorer for Azure Storage"
