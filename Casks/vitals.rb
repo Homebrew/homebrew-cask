908f80f58007371dcc7e86983a5d7512560a8a6f
ymadd <Kb_yamato@protonmail.com>
2021-05-05 02:07:33 +0900

    Add Vital.app v0.3.0

---
+++ Casks/vitals.rb
@@ -0,0 +1,16 @@
+cask "vitals" do
+  version "0.3"
+  sha256 "5618987b45cc1f77a704ccfa38f2103ea3cdf22578898451aed405f0293ffbad"
+
+  url "https://github.com/hmarr/vitals/releases/download/v#{version}/vitals-v#{version}.zip"
+  name "Vitals"
+  desc "Tiny process monitor"
+  homepage "https://github.com/hmarr/vitals/"
+
+  app "Vitals.app"
+
+  zap trash: [
+    "~/Library/Application\ Scripts/com.hmarr.Vitals-LaunchAtLoginHelper",
+    "~/Library/Containers/com.hmarr.Vitals-LaunchAtLoginHelper",
+  ]
+end
