cbc3c4752c3ccce795dcc0b5590c09bbbd8cdcc5
Michael Cho <cho-m@tuta.io>
2021-05-03 18:36:29 -0700

    Update jcryptool from 1.0.4 to 1.0.5

--- Casks/jcryptool.rb
+++ Casks/jcryptool.rb
@@ -1,8 +1,8 @@
 cask "jcryptool" do
-  version "1.0.4"
-  sha256 "65b95a6d71d28d5a7be17feb6bb2181932ccb0e8c52982dd00c3bde24ba2af83"
+  version "1.0.5"
+  sha256 "d7e787e96a2a9d07423cc386fb8b179624f9212fa8ea4e74f09f2b2fd6664a5e"
 
-  url "https://github.com/jcryptool/core/releases/download/#{version}/JCrypTool-#{version}-macOS-64bit.tar.gz",
+  url "https://github.com/jcryptool/core/releases/download/v#{version}/JCrypTool-#{version}-macOS-64bit.tar.gz",
       verified: "github.com/jcryptool/core/"
   name "JCrypTool"
   desc "Apply and analyze cryptographic algorithms"
