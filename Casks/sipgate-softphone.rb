fe3c433552a509622c809e8c521c6ac44f86f30b
Michael Cho <cho-m@tuta.io>
2021-05-03 19:51:01 -0700

    Update sipgate-softphone from 1.15.5 to 1.15.8

--- Casks/sipgate-softphone.rb
+++ Casks/sipgate-softphone.rb
@@ -1,6 +1,6 @@
 cask "sipgate-softphone" do
-  version "1.15.5"
-  sha256 "2319b98add2802190a26f8925ded8671b4f8023a8334a8eb450b19003d9ef96e"
+  version "1.15.8"
+  sha256 "599da1e4390c3c2cedc41db4c349936677b85f5f2eff2f0f4b8aa15641f6c549"
 
   url "https://sipgate-desktop-app-dev.s3.amazonaws.com/sipgate-softphone-#{version}.dmg",
       verified: "sipgate-desktop-app-dev.s3.amazonaws.com/"
