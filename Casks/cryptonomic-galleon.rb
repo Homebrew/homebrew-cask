5a022f56cbfa1fe4f7b33fd1f81c13c2564eea22
Nick Gavalas <28487074+LordWire@users.noreply.github.com>
2021-05-05 00:02:48 +0200

    Adding Cryptonomic Galleon cask.  (#104964)
    * adding cask for galleon wallet
    
    * cryptonomic-galleon audited and style-fixed
    
    * Update cryptonomic-galleon.rb
    
    * Update cryptonomic-galleon.rb
    
    * Update cryptonomic-galleon.rb
    
    * Update cryptonomic-galleon.rb
    
    * adding zap stanza to cryptonomic-galleon cask
    
    * fixing style offenses in cryptonomic-galleon cask
    
    * Update cryptonomic-galleon.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/cryptonomic-galleon.rb
@@ -0,0 +1,24 @@
+cask "cryptonomic-galleon" do
+  version "1.1.20b"
+  sha256 "51896518f06ba333a47cc7e4f0e849a9b5e098e0da63fb7e467557f4eaebd387"
+
+  url "https://cryptonomic-wallet.nyc3.digitaloceanspaces.com/release_#{version}/Galleon-#{version.delete_suffix("b")}.dmg",
+      verified: "cryptonomic-wallet.nyc3.digitaloceanspaces.com"
+  name "Galleon"
+  desc "Software wallet for the Tezos blockchain"
+  homepage "https://cryptonomic.tech/galleon.html"
+
+  livecheck do
+    url "https://github.com/Cryptonomic/Deployments/wiki/Galleon%3A-Releases"
+    strategy :page_match
+    regex(/Galleon\s*(\d+(?:\.\d+)*\w)/i)
+  end
+
+  app "Galleon.app"
+
+  zap trash: [
+    "~/Library/Application Support/Galleon",
+    "~/Library/Preferences/tech.galleon-wallet.galleon.plist",
+    "~/Library/Saved Application State/tech.galleon-wallet.galleon.savedState",
+  ]
+end
