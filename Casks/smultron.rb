51755c023625ff9ed591a1c7663d33d8930df731
Reno <25192197+singularitti@users.noreply.github.com>
2021-05-04 15:57:32 -0400

    Update smultron from 12.4.1,12410 to 12.4.2,12420

--- Casks/smultron.rb
+++ Casks/smultron.rb
@@ -1,5 +1,5 @@
 cask "smultron" do
-  version "12.4.1,12410"
+  version "12.4.2,12420"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.peterborgapps.com/downloads/Smultron#{version.major}.zip"
