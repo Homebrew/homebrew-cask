5dc7321c50b1194eba24159f4df164487b55e8bd
Brice Dutheil <brice.dutheil@gmail.com>
2021-05-04 09:39:52 +0200

    Fix Autodesk Sketchbook 8.7.1 url

--- Casks/sketchbook.rb
+++ Casks/sketchbook.rb
@@ -1,18 +1,14 @@
 cask "sketchbook" do
-  version "8.7.1"
+  version "8.7.1,2019"
   sha256 "a96042dc95483cd6fac849a9f60a22980204ee4ee0a26b0804b0aa6ab23b842a"
 
-  url "https://update.sketchbook.com/mac/SketchBook_v#{version}_mac.dmg"
+  url "https://download.autodesk.com/us/support/files/sketchbook/sketchbook_#{version.after_comma}/sketchbook_v#{version.before_comma}_mac.dmg",
+      verified: "https://download.autodesk.com/us/support/files/sketchbook"
   name "Autodesk Sketchbook"
   desc "Draw, paint, & sketch application"
   homepage "https://www.sketchbook.com/"
 
-  livecheck do
-    url "https://update.sketchbook.com/mac/latest"
-    strategy :header_match
-  end
-
-  pkg "SketchBook_v#{version}_mac.pkg"
+  pkg "SketchBook_v#{version.before_comma}_mac.pkg"
 
   uninstall quit:    "com.autodesk.SketchBook",
             pkgutil: ".*SketchBook.*"
