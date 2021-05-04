ca655205b1e3ba71e07695c68eee65c76c3033ca
Michael Cho <cho-m@tuta.io>
2021-05-03 19:03:32 -0700

    Update tagspaces from 3.9.5 to 3.9.8 (#104987)

--- Casks/tagspaces.rb
+++ Casks/tagspaces.rb
@@ -1,6 +1,6 @@
 cask "tagspaces" do
-  version "3.9.5"
-  sha256 "5dd98dd4c674373dd8b80e7f46d2d65320831ae87309e7e2aff5c102fc1c78c1"
+  version "3.9.8"
+  sha256 "2a6901d85a3880cfad7525a01490c769fa7ee6d1d36d8a41de63188c30c8f215"
 
   url "https://github.com/tagspaces/tagspaces/releases/download/v#{version}/tagspaces-mac-#{version}.zip",
       verified: "github.com/tagspaces/tagspaces/"
