205db0a206777158249d55bdb85929220d3ced2b
Miccal Matthews <miccal.matthews@gmail.com>
2021-05-04 10:27:50 +0800

    Update blitz.rb

--- Casks/blitz.rb
+++ Casks/blitz.rb
@@ -2,13 +2,13 @@ cask "blitz" do
   version "1.14.6"
   sha256 "d55bb5f4c1a3b3b8d73317d03af0175f4b58de572e5bdab7f1fd3d2a0a860aa0"
 
-  url "https://dl.blitz.gg/download/mac"
+  url "https://dl.blitz.gg/download/Blitz-#{version}.dmg"
   name "Blitz"
   desc "Performance analysis software"
   homepage "https://blitz.gg/"
 
   livecheck do
-    url :url
+    url "https://dl.blitz.gg/download/mac"
     strategy :header_match
   end
 
