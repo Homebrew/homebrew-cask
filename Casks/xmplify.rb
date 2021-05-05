015582f4d9dc3a0b360c4a978d55132f2ef28482
Michael Cho <cho-m@tuta.io>
2021-05-05 00:03:51 -0700

    Update xmplify from 1.9.8 to 1.9.9 (#105131)

--- Casks/xmplify.rb
+++ Casks/xmplify.rb
@@ -1,15 +1,23 @@
 cask "xmplify" do
-  version "1.9.8"
-  sha256 "b742d5f039e119758ea2548a59a17aac5cb329c2102f835ca67e90e738f2cb7e"
+  if MacOS.version <= :mojave
+    version "1.9.8"
+    sha256 "b742d5f039e119758ea2548a59a17aac5cb329c2102f835ca67e90e738f2cb7e"
+  else
+    version "1.9.9"
+    sha256 "bab6ccb1fb1e727d4415a7b5b004e45091dc87add4377f2a524190ddf22cbfdf"
+  end
 
   url "http://xmplifyapp.com/releases/Xmplify-#{version}.dmg"
   name "Xmplify"
+  desc "XML editor"
   homepage "http://xmplifyapp.com/"
 
   livecheck do
     url "http://xmplifyapp.com/appcast.xml"
     strategy :sparkle
   end
+
+  depends_on macos: ">= :sierra"
 
   app "Xmplify.app"
 
