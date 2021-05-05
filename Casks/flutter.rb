dc1487fddb5d0fa8c28d2573ab60f2b8080bd6f8
Baw Appie <pp121324@gmail.com>
2021-05-05 23:19:04 +0900

    Update flutter from 2.0.5 to 2.0.6 (#105138)
    * Update flutter.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -1,6 +1,6 @@
 cask "flutter" do
-  version "2.0.5"
-  sha256 "98cdbab108586c359984c5a5e42f48d9244a3ecb56989111e16f999eae2ae95b"
+  version "2.0.6"
+  sha256 "d17ebaa5d1df01c8330a7558dbdf5ba1bfcdd12b4f63fc1d56e0f6082c57ed89"
 
   url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_#{version}-stable.zip",
       verified: "storage.googleapis.com/flutter_infra/"
