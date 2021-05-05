972e4a9075c456a04b1a071ae0a08172266c3b52
ymadd <Kb_yamato@protonmail.com>
2021-05-05 22:33:52 +0900

    Update waves-central from 12.0.11 to 12.0.19

--- Casks/waves-central.rb
+++ Casks/waves-central.rb
@@ -1,5 +1,5 @@
 cask "waves-central" do
-  version "12.0.11"
+  version "12.0.19"
   sha256 :no_check
 
   url "https://cf-installers.waves.com/WavesCentral/Install_Waves_Central.dmg"
@@ -8,4 +8,11 @@ cask "waves-central" do
   homepage "https://www.waves.com/"
 
   app "Waves Central.app"
+
+  zap trash: [
+    "~/Library/Application Support/Waves Central",
+    "~/Library/Application Support/Waves Audio",
+    "~/Library/Saved Application State/com.WavesAudio.central.savedState",
+    "~/Library/Preferences/com.WavesAudio.central.plist",
+  ]
 end
