d25b91e149ab30e99d60a500906937139248a01d
hkunzhe <hkunzhe24@gmail.com>
2021-05-05 16:06:24 +0800

    Add SiYuan.app v1.1.83

---
+++ Casks/siyuan.rb
@@ -0,0 +1,17 @@
+cask "siyuan" do
+  version "1.1.83"
+
+  if Hardware::CPU.intel?
+    sha256 "e3d602b82b2af2987c7c26589a94a98cc9c11b7f8789b5dc574df805791737e9"
+    url "https://github.com/siyuan-note/siyuan/releases/download/v#{version}/siyuan-#{version}-mac.dmg"
+  else
+    sha256 "28700a13550eda4637ce3119642bcb36860205fa4f0661b7a0fe2ea93678a106"
+    url "https://github.com/siyuan-note/siyuan/releases/download/v#{version}/siyuan-#{version}-mac-arm64.dmg"
+  end
+
+  name "siyuan"
+  desc "Local-first personal knowledge management system"
+  homepage "https://github.com/siyuan-note/siyuan"
+
+  app "SiYuan.app"
+end
