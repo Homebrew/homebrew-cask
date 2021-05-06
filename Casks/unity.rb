39ec1230fc0269687a7779363e1b066d25e24ec5
exgphe <exgphe@icloud.com>
2021-05-06 10:32:45 +0800

    Update unity from 2020.1.17f1,9957aee8edc2 to 2021.1.3f1,4bef613afd59 (#104181)
    * Update unity from 2020.1.17f1,9957aee8edc2 to 2021.1.3f1,4bef613afd59
    
    * Update unity.rb
    
    * Update unity.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Co-authored-by: Michael Cho <cho-m@tuta.io>

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -1,13 +1,21 @@
 cask "unity" do
-  version "2020.1.17f1,9957aee8edc2"
-  sha256 "c6fa8c89eb9a93ee08830b84dc84a948afb32eb17ead7b220c2b5f44662bb6bf"
+  version "2021.1.5f1,3737af19df53"
+  sha256 "65da172336cd5fa667779963de6ee10ea3ac99b27be08c850eaa39a94c1971f3"
 
   url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorInstaller/Unity-#{version.before_comma}.pkg",
       verified: "download.unity3d.com/download_unity/"
-  appcast "https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json"
   name "Unity Editor"
   desc "Platform for 3D content"
   homepage "https://unity.com/products"
+
+  livecheck do
+    url "https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json"
+    strategy :page_match do |page|
+      page.scan(%r{/download_unity/(\h+)/MacEditorInstaller/Unity-(\d+(?:\.\d+)*[a-z]*\d*)\.pkg}i).map do |match|
+        "#{match[1]},#{match[0]}"
+      end
+    end
+  end
 
   pkg "Unity-#{version.before_comma}.pkg"
 
