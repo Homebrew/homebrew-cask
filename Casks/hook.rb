708c132c0c7a6f5ec0954daf78d39d8489600648
Michael Cho <cho-m@tuta.io>
2021-05-05 23:48:58 -0700

    Update hook from 2.2.2,2021-03 to 3.0-4312-1,2021.05

--- Casks/hook.rb
+++ Casks/hook.rb
@@ -1,8 +1,8 @@
 cask "hook" do
-  version "2.2.2,2021-03"
-  sha256 "75acc69d06d68082d5fad6679b57e0a06e46dddffef921e6a02cabb6221e233e"
+  version "3.0-4312-1,2021.05"
+  sha256 "6629af835caf5de482f636ba5754564c8da2b2ab5367a68c5d794bbf17057eb3"
 
-  url "https://hookproductivity.com/wp-content/uploads/#{version.after_comma.tr("-", "/")}/Hook-productivity-app-#{version.before_comma}.dmg"
+  url "https://hookproductivity.com/wp-content/uploads/#{version.after_comma.major}/#{version.after_comma.minor}/Hook-productivity-app-#{version.before_comma}.dmg"
   name "Hook"
   desc "Link and retrieve key information"
   homepage "https://hookproductivity.com/"
@@ -10,8 +10,8 @@ cask "hook" do
   livecheck do
     url :homepage
     strategy :page_match do |page|
-      match = page.match(%r{href=.*?/(\d+)/(\d+)/Hook-productivity-app-(\d+(?:\.\d+)*)\.dmg}i)
-      "#{match[3]},#{match[1]}-#{match[2]}"
+      match = page.match(%r{href=.*?/(\d+)/(\d+)/Hook-productivity-app-(\d+(?:\.\d+)*(?:-\d+)*)\.dmg}i)
+      "#{match[3]},#{match[1]}.#{match[2]}"
     end
   end
 
