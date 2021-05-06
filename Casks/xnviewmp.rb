041732e2b8151a5aa998224fd0f6ec063e3b1f58
Miccal Matthews <miccal.matthews@gmail.com>
2021-05-07 07:51:32 +0800

    Update xnviewmp.rb

--- Casks/xnviewmp.rb
+++ Casks/xnviewmp.rb
@@ -1,11 +1,16 @@
 cask "xnviewmp" do
   version "0.98.3"
-  sha256 "2102a1a65219ec2de2ce63c0f8248b4c32cda0304e8abbaacd5052deaea62583"
+  sha256 :no_check
 
-  url "https://download.xnview.com/old_versions/XnViewMP-#{version.no_dots}-mac.dmg"
+  url "https://download.xnview.com/XnViewMP-mac.dmg"
   name "XnViewMP"
   desc "Photo viewer, image manager, image resizer and more"
   homepage "https://www.xnview.com/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   app "XnViewMP.app"
 end
