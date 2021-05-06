4c88ede807d59c2e01c2335643a99fda961c3246
dalisoft <dalisoft@mail.ru>
2021-05-06 19:35:02 +0500

    feat(framer): add new desktop app url
    See [here](https://www.framer.com/support/using-framer/windows-mac/) for more info.
    
    Also would be nice remove deprecated **Framer X** as they do not support it
    
    **NOTE**: I do not know where is URL of **livecheck** of new Framer Desktop app, if could knew, i would be add

--- Casks/framer.rb
+++ Casks/framer.rb
@@ -1,8 +1,17 @@
 cask "framer" do
-  version "63096,1616509244"
-  sha256 "010dac8f36fa842171832d863c69d898445b700623a65b7425b484971ea242d9"
+  version "2021.16.5"
+  
+  if Hardware::CPU.intel?
+    sha256 "008279eca248e6c7cad163223b1e7df2de330e62b918216f15aba469cd09d6da"
 
-  url "https://dl.framer.com/com.framer.desktop/#{version.before_comma}/#{version.after_comma}/FramerDesktop-#{version.before_comma}.zip"
+    url "https://updates.framer.com/electron/darwin/x64/Framer.zip"
+    end
+  else
+    sha256 "296efac5623ce68a5832b9be20a6483ee2c7d9692778f403c69d489c75042f53"
+
+    url "https://updates.framer.com/electron/darwin/arm64/Framer.zip"
+  end
+
   name "Framer"
   desc "Tool that helps teams design every part of the product experience"
   homepage "https://www.framer.com/desktop/"
