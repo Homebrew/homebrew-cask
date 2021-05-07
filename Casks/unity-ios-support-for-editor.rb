9d04c941930fcbe28e9b2a1b07928eebdf611e4c
Michael Cho <cho-m@tuta.io>
2021-05-06 22:56:05 -0700

    Update unity-ios-support-for-editor from 2020.1.17f1 to 2021.1.6f1

--- Casks/unity-ios-support-for-editor.rb
+++ Casks/unity-ios-support-for-editor.rb
@@ -1,13 +1,24 @@
 cask "unity-ios-support-for-editor" do
-  version "2020.1.17f1,9957aee8edc2"
-  sha256 "134b27ee7623d0cb3f35d06a27a002bae16409eeff817ca2a4028bc2fbdd62e9"
+  version "2021.1.6f1,c0fade0cc7e9"
+  sha256 "f8d87b9fe85ee1db17dfdffc59c7a47321706b48727c449372cb0264fb566dce"
 
   url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-#{version.before_comma}.pkg",
       verified: "download.unity3d.com/download_unity/"
-  appcast "https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json"
   name "Unity iOS Build Support"
   desc "iOS target support for Unity"
   homepage "https://unity.com/products"
+
+  livecheck do
+    url "https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json"
+    strategy :page_match do |page|
+      page.scan(%r{
+        /download_unity/(\h+)/MacEditorTargetInstaller
+        /UnitySetup-iOS-Support-for-Editor-(\d+(?:\.\d+)*[a-z]*\d*)\.pkg
+      }ix).map do |match|
+        "#{match[1]},#{match[0]}"
+      end
+    end
+  end
 
   depends_on cask: "unity"
 
