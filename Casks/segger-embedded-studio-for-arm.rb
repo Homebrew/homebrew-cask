98556aed31cde1128632e233af1c870662ae9443
Michael Cho <cho-m@tuta.io>
2021-05-06 21:21:56 -0700

    Update segger-embedded-studio-for-arm from 5.42b to 5.44 and add M1 DMG

--- Casks/segger-embedded-studio-for-arm.rb
+++ Casks/segger-embedded-studio-for-arm.rb
@@ -1,15 +1,22 @@
 cask "segger-embedded-studio-for-arm" do
-  version "5.42b"
-  sha256 "56570f3251ed5bbec70fb5f9a02e590c5b9b6c25727f14c75118ee7e258df2d9"
+  version "5.44"
 
-  url "https://www.segger.com/downloads/embedded-studio/Setup_EmbeddedStudio_ARM_v#{version.no_dots}_macos_x64.dmg"
+  if Hardware::CPU.intel?
+    sha256 "ad326ba338ef46863ca39c0a74d49a32336a3cb24fc464d51f6aeff9aed5fa8f"
+
+    url "https://www.segger.com/downloads/embedded-studio/Setup_EmbeddedStudio_ARM_v#{version.no_dots}_macos_x64.dmg"
+  else
+    sha256 "1d6f2a3ff284b151c56e03d02cfa97a2e2ba534f5fa5b2768116f9e12eda03d1"
+
+    url "https://www.segger.com/downloads/embedded-studio/Setup_EmbeddedStudio_ARM_v#{version.no_dots}_macos_arm64.dmg"
+  end
+
   name "SEGGER Embedded Studio for ARM"
   desc "IDE for embedded systems"
   homepage "https://www.segger.com/products/development-tools/embedded-studio"
 
   livecheck do
     url "https://studio.segger.com/arm_segger_studio_release_notes.htm"
-    strategy :page_match
     regex(/<h2[^>]*>\s*Version\s*(\d+(?:\.\d+)*[a-z]?)/i)
   end
 
