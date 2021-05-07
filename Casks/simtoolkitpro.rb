05cba0d265a54d4ed488ae4851ecf0a6bab4acd0
Michael Cho <cho-m@tuta.io>
2021-05-06 21:39:03 -0700

    Update simtoolkitpro from 0.6.76 to 0.6.80

--- Casks/simtoolkitpro.rb
+++ Casks/simtoolkitpro.rb
@@ -1,6 +1,6 @@
 cask "simtoolkitpro" do
-  version "0.6.76"
-  sha256 "b8e24ffaefb2eab1d75ef8838a4059ec5746eb82f9d3613c77dd064174246d29"
+  version "0.6.80"
+  sha256 "8c8ea93a2ab8e55af0b4ca7a7f7cb1356a6fec3de6b54b97f9adce77aa3b04e1"
 
   url "https://simtoolkitpro-cdn.net/automated-builds/SimToolkitPro-#{version}.Setup.dmg",
       verified: "simtoolkitpro-cdn.net/"
