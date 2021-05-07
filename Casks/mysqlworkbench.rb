13ceacbf6a59d456a099b98a6102e1d7a9ec86e9
Miccal Matthews <miccal.matthews@gmail.com>
2021-05-07 17:45:40 +0800

    Update mysqlworkbench.rb

--- Casks/mysqlworkbench.rb
+++ Casks/mysqlworkbench.rb
@@ -11,6 +11,10 @@ cask "mysqlworkbench" do
     version "8.0.22"
     sha256 "4e27de82d869043cf80e803f1a57cc041a91cabddf0aa6a6c054d68af1837d48"
     url "https://downloads.mysql.com/archives/get/p/#{version.major}/file/mysql-workbench-community-#{version}-macos-x86_64.dmg"
+  elsif MacOS.version <= :catalina
+    version "8.0.23"
+    sha256 "4c8664f5686a449a9760bda9b85d7e8c6beb1367d35f668048ffe534652da7b3"
+    url "https://downloads.mysql.com/archives/get/p/#{version.major}/file/mysql-workbench-community-#{version}-macos-x86_64.dmg"
   else
     version "8.0.24"
     sha256 "21d2145b29a05ff943ba7eb624e237a89436bfcea55f0a6372037857a1a99f63"
@@ -31,8 +35,8 @@ cask "mysqlworkbench" do
 
   zap trash: [
     "~/Library/Application Support/MySQL/Workbench",
-    "~/Library/Preferences/com.oracle.workbench.MySQLWorkbench.plist",
     "~/Library/Caches/com.oracle.workbench.MySQLWorkbench",
+    "~/Library/Preferences/com.oracle.workbench.MySQLWorkbench.plist",
     "~/Library/Saved Application State/com.oracle.workbench.MySQLWorkbench.savedState",
   ]
 end
