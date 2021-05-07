b7388e8f4a11f83de64912420214f9ad5b98a70b
Michael Cho <cho-m@tuta.io>
2021-05-07 14:50:44 -0700

    Update craftmanager.rb

--- Casks/craftmanager.rb
+++ Casks/craftmanager.rb
@@ -4,12 +4,15 @@ cask "craftmanager" do
 
   url "https://craft-assets.invisionapp.com/CraftManager/production/CraftManager.zip"
   name "CraftManager"
+  desc "Helps keep your Craft tools up to date"
   homepage "https://www.invisionapp.com/craft"
 
   livecheck do
     url "https://craft-assets.invisionapp.com/CraftManager/production/appcast.xml"
     strategy :sparkle
   end
+
+  depends_on macos: ">= :mojave"
 
   app "CraftManager.app"
 end
