5fa0bbcbaa349fffd5cb07b02f13d6670ebc4bbc
Michael Cho <cho-m@tuta.io>
2021-05-07 00:26:34 -0700

    Update marvin from 1.59.0 to 1.59.1

--- Casks/marvin.rb
+++ Casks/marvin.rb
@@ -1,6 +1,6 @@
 cask "marvin" do
-  version "1.59.0"
-  sha256 "a25e3e32e60d92992c7dbbdfd2028b45be7c49dfc22b357dc5cfa95b3d68fc88"
+  version "1.59.1"
+  sha256 "b7f0d236253dcc989663be2f50a5f084e29be7442c8bd3a9fd6fc86a2dd5e4c6"
 
   url "https://amazingmarvin.s3.amazonaws.com/Marvin-#{version}.dmg",
       verified: "amazingmarvin.s3.amazonaws.com/"
