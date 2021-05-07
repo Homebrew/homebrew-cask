154506022bfbb41576829672bc32725275de4a72
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-05-07 22:32:46 +0200

    Update printrun from 1.6.0 to 2.0.0rc8 (#104184)
    * Update printrun from 1.6.0 to 2.0.0rc8
    
    * add desc to printrun
    
    * printrun - description starts with a capital letter
    
    * Update printrun.rb
    
    * Update and rename printrun.rb to pronterface.rb
    
    Co-authored-by: Michael Cho <cho-m@tuta.io>
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

---
+++ Casks/pronterface.rb
@@ -0,0 +1,17 @@
+cask "pronterface" do
+  version "2.0.0rc8"
+  sha256 "45f2a288939b3cb2594c821a614fa21af37b17e2b46310d7b72f3be45e15f5af"
+
+  url "https://github.com/kliment/Printrun/releases/download/printrun-#{version}/pronterface-macos-app-#{version}.zip"
+  name "Printrun"
+  desc "Control your 3D printer from your PC"
+  homepage "https://github.com/kliment/Printrun"
+
+  livecheck do
+    url "https://github.com/kliment/Printrun/releases/latest"
+    strategy :page_match
+    regex(%r{href=.*?/pronterface-macos-app-(\d+(?:\.\d+)*(?:rc\d*)?)\.zip}i)
+  end
+
+  app "pronterface.app"
+end
