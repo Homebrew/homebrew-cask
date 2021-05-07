4268ffcee7ee881f4ad7baa05141ec1d69bae96a
Michael Cho <cho-m@tuta.io>
2021-05-07 02:59:39 -0700

    Update preform from 3.13.0,811 to 3.15.2,26_5009 (#105306)

--- Casks/preform.rb
+++ Casks/preform.rb
@@ -1,8 +1,8 @@
 cask "preform" do
-  version "3.13.0,811"
-  sha256 "ce9829ebaf02b27dfddefb88d688f957aed07e9698460cd6717665153a623b88"
+  version "3.15.2,26_5009"
+  sha256 "b5d064155e7e7fbabf538f6415bd24ce386c57adbf9d48c7259649665f4f5ea0"
 
-  url "https://s3.amazonaws.com/FormlabsReleases/Release/#{version.before_comma}/PreForm_#{version.before_comma}_release__build_#{version.after_comma}.dmg",
+  url "https://s3.amazonaws.com/FormlabsReleases/Release/#{version.before_comma}/PreForm_mac_#{version.before_comma}_release_releaser_#{version.after_comma}.dmg",
       verified: "s3.amazonaws.com/FormlabsReleases/"
   name "PreForm"
   desc "3D printing setup, management, and monitoring"
@@ -11,10 +11,12 @@ cask "preform" do
   livecheck do
     url "https://formlabs.com/download-preform-mac/"
     strategy :header_match do |headers|
-      match = headers["location"].match(%r{/PreForm_(\d+(?:\.\d+)*)_release__build_(\d+)\.dmg}i)
+      match = headers["location"].match(%r{/PreForm_mac_(\d+(?:\.\d+)*)_release_releaser_(\d+(?:[._-]\d+)*)\.dmg}i)
       "#{match[1]},#{match[2]}"
     end
   end
+
+  depends_on macos: ">= :high_sierra"
 
   app "PreForm.app"
 end
