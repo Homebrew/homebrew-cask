2429b9f1ce19f63a9d1c096f788abb0a710d99c4
rjpontefract <rjpontefract@users.noreply.github.com>
2021-05-07 15:18:40 +1200

    Update smartgit from 20.2.5 to 20.2.5 (#105227)

--- Casks/smartgit.rb
+++ Casks/smartgit.rb
@@ -1,6 +1,6 @@
 cask "smartgit" do
   version "20.2.5"
-  sha256 "2078c75ca902b55175063b87e7a6cd28cb0a3b4d0c4f0427ce7352bb9fda237f"
+  sha256 "7674b546af9544499599fe82f8806df8001525ff0d3e19cda8ef125452d969d0"
 
   url "https://www.syntevo.com/downloads/smartgit/smartgit-macosx-#{version.dots_to_underscores}.dmg"
   name "SmartGit"
