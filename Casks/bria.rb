944ec5238bb190239d8d6f4cd66569d836c0fe3c
Michael Cho <cho-m@tuta.io>
2021-05-06 18:03:14 -0700

    Update bria from 6.3.0,105536 to 6.4.0,106813

--- Casks/bria.rb
+++ Casks/bria.rb
@@ -1,6 +1,6 @@
 cask "bria" do
-  version "6.3.0,105536"
-  sha256 "ad71abb778fc6d3e591755f3632fcf84fb27bb9ab6ac6f5847a4167cbbf1d65d"
+  version "6.4.0,106813"
+  sha256 "c55cc742e0e227a75323e5f6e7f4b27c6b6b9a39e4a8a4a2d182fc593724c5d9"
 
   url "https://counterpath.s3.amazonaws.com/downloads/Bria_#{version.before_comma}_#{version.after_comma}.dmg",
       verified: "counterpath.s3.amazonaws.com/"
