f81f9c230e5cf0f38a2f274a6f18557521f74ba0
Michael Cho <cho-m@tuta.io>
2021-05-07 00:43:51 -0700

    Update wavebox from 10.0.495.2 to 10.0.503.2 and add ARM DMG

--- Casks/wavebox.rb
+++ Casks/wavebox.rb
@@ -1,20 +1,29 @@
 cask "wavebox" do
-  version "10.0.495.2"
-  sha256 "9b11ebcafce23a3cf1c3db03f922109c8eb2ef80ce45585b285ef4cbff39b711"
+  version "10.0.503.2"
 
-  url "https://download.wavebox.app/stable/mac/Install%20Wavebox%20#{version}.dmg",
-      verified: "download.wavebox.app/"
+  if Hardware::CPU.intel?
+    sha256 "a3fa2e909fac3ec29bcd71eb3ce8c596c978e4d9402e34f76ebb915e1e847f18"
+
+    url "https://download.wavebox.app/stable/mac/Install%20Wavebox%20#{version}.dmg",
+        verified: "download.wavebox.app/"
+  else
+    sha256 "821d75467210a717c4a716778275e9613ee9904a85d3df1057eba92330973ef5"
+
+    url "https://download.wavebox.app/stable/macarm64/Install%20Wavebox%20#{version}.dmg",
+        verified: "download.wavebox.app/"
+  end
+
   name "Wavebox"
   desc "Web browser"
   homepage "https://wavebox.io/"
 
   livecheck do
-    url "https://download.wavebox.app/stable/mac/appcast.xml"
-    strategy :page_match
-    regex(/Wavebox_(\d+(?:\.\d+)*)\.zip/i)
+    url "https://download.wavebox.app/latest/stable/mac"
+    strategy :header_match
   end
 
   auto_updates true
+  depends_on macos: ">= :sierra"
 
   app "Wavebox.app"
 
