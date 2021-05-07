85793804ac6c506229796a64aa1ca34a8f15063f
Michael Cho <cho-m@tuta.io>
2021-05-07 14:33:43 -0700

    Update atomic-wallet from 2.28.1 to 2.28.2 and try using versioned ZIP

--- Casks/atomic-wallet.rb
+++ Casks/atomic-wallet.rb
@@ -1,8 +1,8 @@
 cask "atomic-wallet" do
-  version "2.28.1"
-  sha256 :no_check
+  version "2.28.2"
+  sha256 "6c8f8469d6f03c30aba751cd2c379cbc1b3689990573309dc43fc9b2da29aeb5"
 
-  url "https://get.atomicwallet.io/download/atomicwallet.dmg"
+  url "https://releases.atomicwallet.io/AtomicWallet-#{version}.zip"
   name "Atomic Wallet"
   desc "Manage Bitcoin, Ethereum, XRP, Litecoin, XLM and over 300 other coins and tokens"
   homepage "https://atomicwallet.io/"
