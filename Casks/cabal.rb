189001922d2b223e8e071fa3dfc24fa4284153d1
Michael Cho <cho-m@tuta.io>
2021-05-08 09:36:22 -0700

    Update cabal from 6.0.7 to 6.0.8

--- Casks/cabal.rb
+++ Casks/cabal.rb
@@ -1,6 +1,6 @@
 cask "cabal" do
-  version "6.0.7"
-  sha256 "4827aa8a686832fa79a0d08429e10e14d5e6e3a0ea36f453e611cc9ee14da027"
+  version "6.0.8"
+  sha256 "220966a9d873ee5103a7ff6c5bb21a52ba789e9cfea981ef68e90e0afed91484"
 
   url "https://github.com/cabal-club/cabal-desktop/releases/download/v#{version}/cabal-desktop-#{version}-mac.dmg",
       verified: "github.com/cabal-club/cabal-desktop/"
