1f3d2ea910e1179e182f360e918d3686f2771efa
Tyler W <19383474+leftl@users.noreply.github.com>
2021-05-08 01:18:59 -0700

    Update jellyfin-media-player from 1.5.0 to 1.6.0 (#105352)

--- Casks/jellyfin-media-player.rb
+++ Casks/jellyfin-media-player.rb
@@ -1,6 +1,6 @@
 cask "jellyfin-media-player" do
-  version "1.5.0"
-  sha256 "cdbab31a50df1ec355ce5fcda42930ef6c49ae84186d02ac9c905154adab3858"
+  version "1.6.0"
+  sha256 "130eefdc149fd8a257c6f49b16251ec0eb79d0f39cbae407ce7a01f2efd4ead5"
 
   url "https://github.com/jellyfin/jellyfin-media-player/releases/download/v#{version}/JellyfinMediaPlayer-#{version}-macos.dmg",
       verified: "github.com/jellyfin/jellyfin-media-player/"
