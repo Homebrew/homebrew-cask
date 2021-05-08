e884dd20441d2c1905b93f4badd19ddf316e36fb
Michael Cho <cho-m@tuta.io>
2021-05-08 10:56:08 -0700

    Update sidequest from 0.10.19 to 0.10.20

--- Casks/sidequest.rb
+++ Casks/sidequest.rb
@@ -1,6 +1,6 @@
 cask "sidequest" do
-  version "0.10.19"
-  sha256 "8440b369f640f463197b85ee9f87335b295e7cff25784634ab666237a7ba2634"
+  version "0.10.20"
+  sha256 "9ebdcf72bf2c361769dfdf6f0141ae0fcad20545d3726babf7d8276aead5974d"
 
   url "https://github.com/SideQuestVR/SideQuest/releases/download/v#{version}/SideQuest-#{version}.dmg",
       verified: "github.com/SideQuestVR/SideQuest/"
