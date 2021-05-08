4c6de5847d55ef96d1505b6c84e891f02c5269ed
Michael Cho <cho-m@tuta.io>
2021-05-07 18:43:16 -0700

    Update xampp-vm from 7.3.5,0 to 8.0.5-0 and add livecheck

--- Casks/xampp-vm.rb
+++ Casks/xampp-vm.rb
@@ -1,10 +1,16 @@
 cask "xampp-vm" do
-  version "7.3.5,0"
-  sha256 "2b3d69aced3542d5c39fe9c8348f955edc0b75aac8f8494b67e70e415853f186"
+  version "8.0.5-0"
+  sha256 "5202681f4d9628a28f18674d1152379182562b1ef7df3810d97b3407f687f2dc"
 
-  url "https://www.apachefriends.org/xampp-files/#{version.before_comma}/xampp-osx-#{version.before_comma}-#{version.after_comma}-vm.dmg"
+  url "https://www.apachefriends.org/xampp-files/#{version.split("-").first}/xampp-osx-#{version}-vm.dmg"
   name "XAMPP-VM"
+  desc "Virtual machine with apache distribution containing MySQL, PHP, and Perl"
   homepage "https://www.apachefriends.org/index.html"
+
+  livecheck do
+    url "https://www.apachefriends.org/download.html"
+    regex(%r{href=.*?/xampp-osx-(\d+(?:\.\d+)*-\d+)-vm\.dmg}i)
+  end
 
   app "XAMPP.app"
 end
