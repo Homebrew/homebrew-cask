21c6905df6c2f991325dae8c0d552c8f8c156f81
adithyasunil26 <adithyasunil26@gmail.com>
2021-05-09 18:14:30 +0400

    Update munkiadmin from 1.7.1 to 1.8.0b1

--- Casks/munkiadmin.rb
+++ Casks/munkiadmin.rb
@@ -1,6 +1,6 @@
 cask "munkiadmin" do
-  version "1.7.1"
-  sha256 "0eb86e33162db7ee5c76c1f16b826481c2ec2002153479e738043f59c51c59b1"
+  version "1.8.0b1"
+  sha256 "798d3028463b8daa69ba6379e6f3ef420e27705aeee1dfac6f9216319d7a0902"
 
   url "https://github.com/hjuutilainen/munkiadmin/releases/download/v#{version}/MunkiAdmin-#{version}.dmg",
       verified: "github.com/hjuutilainen/munkiadmin/"
