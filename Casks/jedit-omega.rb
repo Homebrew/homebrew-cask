4bb9f3e6d2af620b8b23e7b0d5e093245cfcc5f5
Takeshi Owaki <owaki@kumanomi.org>
2021-05-10 01:29:23 +0900

    Update jedit-omega from 2.36 to 2.37 (#105445)
    Co-authored-by: Michael Cho <cho-m@tuta.io>

--- Casks/jedit-omega.rb
+++ Casks/jedit-omega.rb
@@ -3,14 +3,15 @@ cask "jedit-omega" do
     version "1.32"
     sha256 "251697fe6b76419b2cef41a89a2cb7d2e3f8caab0bc1ae82aed6c14a366fdad9"
   else
-    version "2.36"
-    sha256 "bec626de79c331d06d2047128d755fc8f61c9a25fdff5412fb579a03ef561b37"
+    version "2.37"
+    sha256 "8ab478d7a176f1981295c357fc6b5b31a3f1d1521d9aea3123db811f6e3509f7"
   end
 
   url "http://download.artman21.co.jp/product/JeditOmega/JeditOmega#{version.no_dots}.dmg",
       verified: "artman21.co.jp/"
   appcast "http://www.artman21.com/en/jeditOmega/history.html"
   name "Jedit Ω"
+  desc "Text editor"
   homepage "http://www.artman21.com/en/jeditOmega/"
 
   app "Jedit Ω.app"
