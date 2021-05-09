ba5bec599aa6a56eba129d0f512ab8cc4a536220
adithyasunil26 <adithyasunil26@gmail.com>
2021-05-09 18:11:38 +0400

    Update mucommander from 0.9.6-1 to 0.9.7-1

--- Casks/mucommander.rb
+++ Casks/mucommander.rb
@@ -1,6 +1,6 @@
 cask "mucommander" do
-  version "0.9.6-1"
-  sha256 "fe0cf62f5b3d1f729936d0887b82e248e89c407d48d8b4fb1089a44570ae2040"
+  version "0.9.7-1"
+  sha256 "ee3598069ba9081a8ad7ffd81714bc7aef78bdaef55cd64f03ebe910f9fc9a33"
 
   url "https://github.com/mucommander/mucommander/releases/download/#{version}/mucommander-#{version}.dmg",
       verified: "github.com/mucommander/mucommander/"
