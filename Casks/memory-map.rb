af92964b0f6b2350b82628c467f890cfd4270d90
Michael Cho <cho-m@tuta.io>
2021-05-09 08:27:50 -0700

    Update memory-map from 1.7-15 to 1.7.1-18 (#105419)

--- Casks/memory-map.rb
+++ Casks/memory-map.rb
@@ -1,16 +1,18 @@
 cask "memory-map" do
-  version "1.7-15"
-  sha256 "60a586d88d9d8746a35531b912d5e236104c49b1e5bb4896aed4db7a76340e56"
+  version "1.7.1-18"
+  sha256 "b12e2c3d42905d24c43aff6a19895d16ec145e2d8dc378f1b609371f3ca5b0b6"
 
   url "https://memory-map.com/download/mac/Memory-Map.#{version}.pkg"
   name "Memory-Map"
+  desc "GPS navigation software"
   homepage "https://memory-map.com/"
 
   livecheck do
-    url "https://memory-map.com/Mapping_Software_Downloads.html"
-    strategy :page_match
+    url "https://memory-map.com/downloads/"
     regex(%r{href=.*?/Memory-Map\.(\d+(?:\.\d+)*-\d+)\.pkg}i)
   end
+
+  depends_on macos: ">= :mojave"
 
   pkg "Memory-Map.#{version}.pkg"
 
