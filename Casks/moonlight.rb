454d47fdf015c04d14b839b2ae39bff3c228a99d
Michael Cho <cho-m@tuta.io>
2021-05-09 09:17:03 -0700

    Update moonlight from 3.1.2 to 3.1.3 (#105424)

--- Casks/moonlight.rb
+++ Casks/moonlight.rb
@@ -1,6 +1,6 @@
 cask "moonlight" do
-  version "3.1.2"
-  sha256 "8eef120885b69b6216722e73a7dd66ac9a338013e08c83fdfa99d87f258f9576"
+  version "3.1.3"
+  sha256 "c1bcc67d3b767d514a2d594aaf531a684709fd8516e39607f22a332cf0435302"
 
   url "https://github.com/moonlight-stream/moonlight-qt/releases/download/v#{version}/Moonlight-#{version}.dmg",
       verified: "github.com/moonlight-stream/moonlight-qt/"
