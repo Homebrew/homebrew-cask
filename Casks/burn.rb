3d3db083b07aabe3d444142d37ed4510e3849f41
adithyasunil26 <adithyasunil26@gmail.com>
2021-05-09 17:03:35 +0400

    Update burn from 3.1.2 to 3.1.3

--- Casks/burn.rb
+++ Casks/burn.rb
@@ -1,6 +1,6 @@
 cask "burn" do
-  version "3.1.2"
-  sha256 "8fa6ed2255b48953254dfe027f30ec9d4ec03d5a378d1da514243befec76c041"
+  version "3.1.3"
+  sha256 "414140ab2920137eed3a77a6d507e432e25562598ac515a82bc54df529a0ba32"
 
   url "https://downloads.sourceforge.net/burn-osx/Burn/#{version}/burn-#{version}.zip",
       verified: "downloads.sourceforge.net/burn-osx/"
