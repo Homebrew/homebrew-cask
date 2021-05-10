ffff4a9230623d37c6058b2a2357f5dba9f0b701
Michael Cho <cho-m@tuta.io>
2021-05-09 22:22:13 -0700

    focused: fix dead download URL

--- Casks/focused.rb
+++ Casks/focused.rb
@@ -1,18 +1,18 @@
 cask "focused" do
   version "3.2,1825:1544822315"
-  sha256 "e9da7b084243c73f78d2a441f1414b1e6d3a2b31c0391b8e9ae3631efa834f03"
+  sha256 :no_check
 
-  url "https://dl.devmate.com/com.71squared.focused/#{version.after_comma.before_colon}/#{version.after_colon}/Focused-#{version.after_comma.before_colon}.zip",
-      verified: "devmate.com/com.71squared.focused/"
+  url "https://www.codebots.co.uk/download/Focused"
   name "Focused"
+  desc "Markdown writing app"
   homepage "https://codebots.co.uk/"
 
   livecheck do
-    url "https://updates.devmate.com/com.71squared.focused.xml"
-    strategy :sparkle do |item|
-      "#{item.short_version},#{item.version}:#{item.url[%r{/(\d+)/Focused-\d+\.zip}i, 1]}"
-    end
+    url :url
+    strategy :extract_plist
   end
+
+  depends_on macos: ">= :el_capitan"
 
   app "Focused.app"
 end
