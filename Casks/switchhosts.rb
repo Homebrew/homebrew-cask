1cce92cc670ae573b8f5cb1744dff8666610e0b0
Michael Cho <cho-m@tuta.io>
2021-05-10 09:44:29 -0700

    Update switchhosts from 4.0.0.6029 to 4.0.1.6051

--- Casks/switchhosts.rb
+++ Casks/switchhosts.rb
@@ -1,12 +1,12 @@
 cask "switchhosts" do
-  version "4.0.0.6029"
+  version "4.0.1.6051"
 
   if Hardware::CPU.intel?
-    sha256 "0745e449e15002e9afc3c326e1e78a165e761e00825fa02eef170c140a8e41db"
+    sha256 "5777f55d3876abf0930c83b39c69d9a08d6ee779a197c7226900972f5ccf99fc"
     url "https://github.com/oldj/SwitchHosts/releases/download/v#{version.major_minor_patch}/SwitchHosts_#{version}.dmg",
         verified: "github.com/oldj/SwitchHosts/"
   else
-    sha256 "1f0cfe32b87f9133098604a68289db9f309af8374af9b9341b8d24393de354bf"
+    sha256 "6ebb0f04eea97f61c60e7b0c934f318ce8754f1183c73ea9fcdfe48a90ad252e"
     url "https://github.com/oldj/SwitchHosts/releases/download/v#{version.major_minor_patch}/SwitchHosts_arm64_#{version}.dmg",
         verified: "github.com/oldj/SwitchHosts/"
   end
