ec18ccf1b4d8f83b35d89e4f5f9e6e7aefcaa3a1
Michael Cho <cho-m@tuta.io>
2021-05-10 07:42:04 -0700

    Update minecraft.rb

--- Casks/minecraft.rb
+++ Casks/minecraft.rb
@@ -5,7 +5,13 @@ cask "minecraft" do
   url "https://launcher.mojang.com/download/Minecraft.dmg",
       verified: "mojang.com/"
   name "Minecraft"
+  desc "Sandbox construction video game"
   homepage "https://minecraft.net/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   app "Minecraft.app"
 
