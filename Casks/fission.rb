fa1e733f53a6d640fb097a831e4824542dee9dc7
Michael Cho <cho-m@tuta.io>
2021-05-09 18:49:33 -0700

    fission: add livecheck and update min OS to 10.13

--- Casks/fission.rb
+++ Casks/fission.rb
@@ -3,12 +3,16 @@ cask "fission" do
   sha256 :no_check
 
   url "https://rogueamoeba.com/fission/download/Fission.zip"
-  appcast "https://rogueamoeba.com/fission/releasenotes.php"
   name "Fission"
   desc "Audio editor"
   homepage "https://rogueamoeba.com/fission/"
 
-  depends_on macos: ">= :sierra"
+  livecheck do
+    url "https://rogueamoeba.com/fission/releasenotes.php"
+    regex(/ra-version=["']?(\d+(?:\.\d+)+)["' >]/i)
+  end
+
+  depends_on macos: ">= :high_sierra"
 
   app "Fission.app"
 end
