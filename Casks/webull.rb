dcc24dcd476772d4216ed9a111b7408b131167ca
Michael Cho <cho-m@tuta.io>
2021-05-10 11:53:17 -0700

    Update webull.rb

--- Casks/webull.rb
+++ Casks/webull.rb
@@ -7,6 +7,13 @@ cask "webull" do
   desc "Desktop client for Webull Financial LLC"
   homepage "https://webull.com/"
 
+  livecheck do
+    url "https://infoapi.webullfintech.com/api/operation/appver/last?platform=qt_mac_global"
+    regex(%r{"upgradeUrl"\s*:\s*"[^"]*/Webull%20Desktop%20(\d+(?:\.\d+)*(?:[._-][a-z]+)?)\.dmg"}i)
+  end
+
+  depends_on macos: ">= :sierra"
+
   app "Webull Desktop.app"
 
   zap trash: "~/Library/Application Support/Webull Desktop"
