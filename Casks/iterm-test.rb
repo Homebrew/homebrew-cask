f08f999cde06d907f5ab021c78d7fb4c2316ca21
Kyung Up Won <jack@netsarang.com>
2021-05-11 16:39:02 +0900

    Add item.app test

---
+++ Casks/iterm-test.rb
@@ -0,0 +1,11 @@
+cask "iterm-test" do
+  version "3.4.6"
+  sha256 :no_check
+
+  url "https://iterm2.com/downloads/stable/iTerm2-3_4_6.zip"
+  name "iterm"
+  desc "Lightning fast swatch tool for designers and developers"
+  homepage "https://iterm2.com/"
+
+  app "iterm.app"
+end
