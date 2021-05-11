29483f026c19acaf553cae164b02dac14d2d3790
Michael Cho <cho-m@tuta.io>
2021-05-10 17:41:53 -0700

    Update calmly-writer from 2.0.26 to 2.0.27 (#105511)

--- Casks/calmly-writer.rb
+++ Casks/calmly-writer.rb
@@ -1,12 +1,12 @@
 cask "calmly-writer" do
-  version "2.0.26"
+  version "2.0.27"
 
   if Hardware::CPU.intel?
-    sha256 "5cf3e92fa5fc2fc0399a5cf77eb44be7c115fbce160924e3ea431012d44b1e26"
+    sha256 "b3d74fa9fece33e306d534e14de46808990ab25a8f32135403a0c7449075a330"
 
     url "https://www.calmlywriter.com/releases/x64/Calmly%20Writer-#{version}.pkg"
   else
-    sha256 "0a39fdc3e990e2ea6ffcb61742b93da46b67f4e49fe6bf88e402fe1a362e5bae"
+    sha256 "5b3a9a3de0800c3c8de97e2261f69940128cff572e3aa600d024281e5fbf687c"
 
     url "https://www.calmlywriter.com/releases/arm64/Calmly%20Writer-#{version}.pkg"
   end
