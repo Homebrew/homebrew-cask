05e281b6d7d88d7516266935c4d6299f1a39ac25
Miccal Matthews <miccal.matthews@gmail.com>
2021-05-12 13:55:52 +0800

    Update dotnet.rb

--- Casks/dotnet.rb
+++ Casks/dotnet.rb
@@ -1,5 +1,5 @@
 cask "dotnet" do
-  version "5.0.6,4e2e2c76-626f-4927-8753-55d47ab79e06:4417509d68777cab1ed5f927e86db82"
+  version "5.0.6,4e2e2c76-626f-4927-8753-55d47ab79e06:24417509d68777cab1ed5f927e86db82"
   sha256 "208eae389ea28d76b6b081f944cecca9ec7f0d59bba6711ade976a8c2bf18994"
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-runtime-#{version.before_comma}-osx-x64.pkg"
