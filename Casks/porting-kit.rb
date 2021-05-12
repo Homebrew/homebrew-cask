f514b70141885c7fb7c80f2f5cf2a030edbfd1bc
Vítor Galvão <info@vitorgalvao.com>
2021-05-12 21:59:39 +0100

    Update porting-kit.rb

--- Casks/porting-kit.rb
+++ Casks/porting-kit.rb
@@ -14,14 +14,4 @@ cask "porting-kit" do
   auto_updates true
 
   app "Porting Kit.app"
-
-  zap trash: [
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/edu.ufrj.vitormm.porting-kit.sfl*",
-    "~/Library/Application Support/Porting-Kit",
-    "~/Library/Caches/edu.ufrj.vitormm.Porting-Kit",
-    "~/Library/Cookies/edu.ufrj.vitormm.Porting-Kit.binarycookies",
-    "~/Library/Preferences/edu.ufrj.vitormm.Porting-Kit.plist",
-    "~/Library/Saved Application State/edu.ufrj.vitormm.Porting-Kit.savedState",
-    "~/Library/WebKit/edu.ufrj.vitormm.Porting-Kit",
-  ]
 end
