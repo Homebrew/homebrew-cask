29ce013cfb4a4e2c0fc2804811eff842879bcf95
Miccal Matthews <miccal.matthews@gmail.com>
2021-05-12 11:13:00 +0800

    microsoft-office: add livecheck (#105565)
    * Update microsoft-office.rb
    
    * Update microsoft-office.rb
    
    * Update microsoft-office.rb
    
    * Update microsoft-office.rb

--- Casks/microsoft-office.rb
+++ Casks/microsoft-office.rb
@@ -1,12 +1,17 @@
 cask "microsoft-office" do
-  version "16.48.21041102"
-  sha256 "5e78cf692b8925a48e0b9f50402874f12293955444b4e6eace9505a4a5969379"
+  version "16.49.21050901"
+  sha256 "cf605558a84f9e51e11d766196be94c60ec236c17cbf7f2627bd5eefd4cd98e4"
 
-  url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Office_#{version}_Installer.pkg",
-      verified: "officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/"
-  appcast "https://docs.microsoft.com/en-us/officeupdates/update-history-office-for-mac"
+  url "https://officecdn-microsoft-com.akamaized.net/db/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Office_#{version}_Installer.pkg",
+      verified: "officecdn-microsoft-com.akamaized.net/db/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/"
   name "Microsoft Office"
+  desc "Office suite"
   homepage "https://products.office.com/mac/microsoft-office-for-mac/"
+
+  livecheck do
+    url "https://go.microsoft.com/fwlink/p/?linkid=525133"
+    strategy :header_match
+  end
 
   auto_updates true
   conflicts_with cask: %w[
