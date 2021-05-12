9080ff4358761dcd36382f6cbd5df9ac0a5d92cf
Miccal Matthews <miccal.matthews@gmail.com>
2021-05-12 11:31:20 +0800

    Update prudent.rb

--- Casks/prudent.rb
+++ Casks/prudent.rb
@@ -1,12 +1,11 @@
 cask "prudent" do
-  version "86.0.4240.75.49.0.24,24"
-  sha256 "7cc3e261608c7eb7e5c8ab996c8c171e12e7fd8ceacdb2d7a69b8f086b6483c6"
+  version "26"
+  sha256 "3dc4a1e65e2209cd3bc9a8c08c886ab33c78d40b85bed16068eac51b6c4cc9c7"
 
-  url "https://github.com/PrudentMe/main/releases/download/#{version.after_comma}/Prudent.zip",
+  url "https://github.com/PrudentMe/main/releases/download/#{version}/Prudent.zip",
       verified: "github.com/PrudentMe/main/"
-  appcast "https://github.com/PrudentMe/main/releases.atom",
-          must_contain: version.after_comma
   name "Prudent"
+  desc "Integrated environment for your personal and family ledger"
   homepage "https://prudent.me/"
 
   app "Prudent.app"
