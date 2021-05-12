9f55a47e65ec8aa24d524056045a73881f427a85
Tyler Schroeder <tschroeder@prodev.io>
2021-05-12 14:07:32 -0700

    fix audit / style

--- Casks/mem.rb
+++ Casks/mem.rb
@@ -2,9 +2,10 @@ cask "mem" do
   version "0.16.0"
   sha256 "6a7ff4ee59a3ed7006b5f3495cfcdf4a9645d503f9f888397f0f5cf4b82b3d77"
 
-  url "https://storage.googleapis.com/mem-desktop/Mem-0.16.0.dmg"
+  url "https://storage.googleapis.com/mem-desktop/Mem-#{version}.dmg",
+      verified: "storage.googleapis.com/mem-desktop/"
   name "Mem"
-  desc "Capture and access information from anywhere."
+  desc "Capture and access information from anywhere"
   homepage "https://get.mem.ai/"
 
   app "Mem.app"
