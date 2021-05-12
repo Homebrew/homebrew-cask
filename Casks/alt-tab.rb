fe2db8be27e03507c77209e6a00bc7912dc8556f
lwouis <lwouis@gmail.com>
2021-05-12 20:33:29 +0900

    Update alt-tab (#105623)
    Co-authored-by: Travis CI <travis@Traviss-Mac.local>

--- Casks/alt-tab.rb
+++ Casks/alt-tab.rb
@@ -1,6 +1,6 @@
 cask "alt-tab" do
-  version "6.22.0"
-  sha256 "093bf3113b710c65473528d8987c6861a9059647e83c62c2dd6b672f4500c59a"
+  version "6.22.1"
+  sha256 "7c53c944ad2b103c34453bc08697d01c49fda95d3b09b8731b6d9d05116ee0a1"
 
   url "https://github.com/lwouis/alt-tab-macos/releases/download/v#{version}/AltTab-#{version}.zip"
   name "alt-tab"
