96b5c8b3d4b9a0515963d7ea666d225fc8c6d801
Vitah Lin <linw1225@gmail.com>
2021-05-13 01:18:56 +0800

    Update yuque from 0.7.14 to 0.8.11 (#105627)
    Co-authored-by: Michael Cho <cho-m@tuta.io>

--- Casks/yuque.rb
+++ Casks/yuque.rb
@@ -1,6 +1,6 @@
 cask "yuque" do
-  version "0.7.14"
-  sha256 "badc59f2b537b2f611d104f74839f7d1dcec511cdf5e5fd9000675ed544dd093"
+  version "0.8.11"
+  sha256 "5f2a455e38d315871499dff6faeb66db4a33e663b4ce2e8879a13b4055af2562"
 
   url "https://app.nlark.com/yuque-desktop/Yuque-#{version}.dmg",
       verified: "app.nlark.com/yuque-desktop/"
@@ -10,8 +10,7 @@ cask "yuque" do
   homepage "https://www.yuque.com/"
 
   livecheck do
-    url "https://www.yuque.com/yuque/help/changelog"
-    strategy :page_match
+    url "https://www.yuque.com/yuque/yuque-desktop/changelog"
     regex(/下载地址(\d+(?:\.\d+)*)/i)
   end
 
