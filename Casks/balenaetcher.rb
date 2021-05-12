4c39585232d6fd91c0c42ca0f5a09db262426457
Hannes Braun <hannesbraun@mail.de>
2021-05-12 02:18:07 +0200

    Update balenaetcher from 1.5.117 to 1.5.120

--- Casks/balenaetcher.rb
+++ Casks/balenaetcher.rb
@@ -1,6 +1,6 @@
 cask "balenaetcher" do
-  version "1.5.117"
-  sha256 "b8872eb9e0790220627f95317fa2d3661a53727e26fbbfb9c5832955e6df16fd"
+  version "1.5.120"
+  sha256 "af5c3446ba7872997a3d33793cf4291d8a95cb7acc02c963c808da53cb64f3c7"
 
   url "https://github.com/balena-io/etcher/releases/download/v#{version}/balenaEtcher-#{version}.dmg",
       verified: "github.com/balena-io/etcher/"
