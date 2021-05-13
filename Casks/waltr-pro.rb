000cd377b8f3f96eef78c4dd3fe13bda39eb0662
Jacob Ford <j@cobford.com>
2021-05-13 01:04:10 -0400

    Convert appcast to livecheck

--- Casks/waltr-pro.rb
+++ Casks/waltr-pro.rb
@@ -3,9 +3,13 @@ cask "waltr-pro" do
   sha256 "d6b8d87156af2e96f0f0a18acf7547350111b389a40b54118c6e4eaf0c788674"
 
   url "https://shining.softorino.com/shine_uploads/waltrpromac_#{version}.dmg"
-  appcast "https://api.softorino.com/v1/app-manager/waltrpro/mac/updates"
   name "WALTR PRO"
   homepage "https://softorino.com/waltr/"
+
+  livecheck do
+    url "https://api.softorino.com/v1/app-manager/waltrpro/mac/updates"
+    strategy :sparkle
+  end
 
   app "WALTR PRO.app"
 end
