d9cf233360949459419399fa7951a54d91005498
Miccal Matthews <miccal.matthews@gmail.com>
2021-05-13 18:04:12 +0800

    Update cuda-z.rb

--- Casks/cuda-z.rb
+++ Casks/cuda-z.rb
@@ -2,9 +2,9 @@ cask "cuda-z" do
   version "0.10.251"
   sha256 "552081ce1f632a72231dedeb6c3ddb02b352d0b19eea45f9ae27d930ee9d7c35"
 
-  url "https://downloads.sourceforge.net/cuda-z/CUDA-Z-#{version}.dmg",
-      verified: "downloads.sourceforge.net/cuda-z/"
+  url "https://downloads.sourceforge.net/cuda-z/CUDA-Z-#{version}.dmg"
   name "CUDA-Z"
+  desc "Show basic information about CUDA-enabled GPUs and GPGPUs"
   homepage "http://cuda-z.sourceforge.net/"
 
   livecheck do
