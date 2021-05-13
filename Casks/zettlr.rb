c3c1362020fc9d771552c2844eb9d2699c02d79b
nasteffe <64329369+nasteffe@users.noreply.github.com>
2021-05-13 22:34:46 +0300

    Update zettlr from 1.8.8 to 1.8.9 (#105702)

--- Casks/zettlr.rb
+++ Casks/zettlr.rb
@@ -1,12 +1,12 @@
 cask "zettlr" do
-  version "1.8.8"
+  version "1.8.9"
 
   if Hardware::CPU.intel?
-    sha256 "0045e9ef42d327b0c2329e6e09db8fdc6b520ee9a211bf096abfa7e13605a562"
+    sha256 "4030d9b3a6f7fd7b7fc8a033f4512b09671965293821f892ae4a5554b5f22150"
     url "https://github.com/Zettlr/Zettlr/releases/download/v#{version}/Zettlr-#{version}-x64.dmg",
         verified: "github.com/Zettlr/Zettlr/"
   else
-    sha256 "e11fd5dbff090bca431bfb5f4cf86e7385f9dc24152895685794596e7d2b57ea"
+    sha256 "9308a45a7f0b84f55cef26d8004d3e9a48224fe41eaa8490853cbe3295f36ee6"
     url "https://github.com/Zettlr/Zettlr/releases/download/v#{version}/Zettlr-#{version}-arm64.dmg",
         verified: "github.com/Zettlr/Zettlr/"
   end
