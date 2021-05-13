2c5e1550220edc53082613d1840b40898f2c685e
Miccal Matthews <miccal.matthews@gmail.com>
2021-05-13 09:25:42 +0800

    Update tuxguitar.rb

--- Casks/tuxguitar.rb
+++ Casks/tuxguitar.rb
@@ -3,8 +3,8 @@ cask "tuxguitar" do
   sha256 "af2d3bee09057c9716eaf4a13d11898945f0431a3c84739a51a39cea49497f6e"
 
   url "https://downloads.sourceforge.net/tuxguitar/tuxguitar-#{version}-macosx-cocoa-64.app.tar.gz"
-  appcast "https://sourceforge.net/projects/tuxguitar/rss?path=/TuxGuitar"
   name "TuxGuitar"
+  desc "Multitrack guitar tablature editor and player"
   homepage "https://sourceforge.net/projects/tuxguitar/"
 
   app "tuxguitar-#{version}-macosx-cocoa-64.app"
