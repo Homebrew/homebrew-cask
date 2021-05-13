0164ccf37bb1f976eaca0dfbd23f9d0b768ab767
Jacob Ford <j@cobford.com>
2021-05-13 02:24:25 -0400

    Add WALTR PRO (#105681)
    * Fix SHA
    
    * Fix app name
    
    * Convert appcast to livecheck
    
    * Write desc
    
    * Write Zap stanza
    
    Starting with only folder I can find so far, though it's empty
    
    * Fix style
    
    * Update waltr-pro.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/waltr-pro.rb
@@ -0,0 +1,18 @@
+cask "waltr-pro" do
+  version "1.0.89"
+  sha256 "d6b8d87156af2e96f0f0a18acf7547350111b389a40b54118c6e4eaf0c788674"
+
+  url "https://shining.softorino.com/shine_uploads/waltrpromac_#{version}.dmg"
+  name "WALTR PRO"
+  desc "Media conversion and direct transfer tool for Apple devices"
+  homepage "https://softorino.com/waltr/"
+
+  livecheck do
+    url "https://api.softorino.com/v1/app-manager/waltrpro/mac/updates"
+    strategy :sparkle
+  end
+
+  app "WALTR PRO.app"
+
+  zap trash: "~/Library/Application Support/WALTR PRO"
+end
