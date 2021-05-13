46ceee117f61a4ef3b38d882f843a083e770a69e
Miccal Matthews <miccal.matthews@gmail.com>
2021-05-13 21:27:11 +0800

    Update arrsync.rb

--- Casks/arrsync.rb
+++ Casks/arrsync.rb
@@ -2,8 +2,7 @@ cask "arrsync" do
   version "0.4.1"
   sha256 "8a254771065f90a9f9f882589eccaf7bc51ad5d0ac86faf15847381bcdbe64f0"
 
-  url "https://downloads.sourceforge.net/arrsync/arrsync-#{version}.dmg",
-      verified: "downloads.sourceforge.net/arrsync/"
+  url "https://downloads.sourceforge.net/arrsync/arrsync-#{version}.dmg"
   name "arRsync"
   desc "Graphical front end for the utility rsync"
   homepage "http://arrsync.sourceforge.net/"
