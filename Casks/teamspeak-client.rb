20540d8f56528af348cc053c9b42d088b9f14fe5
Pascal Kutscha <paskutscha@gmail.com>
2021-05-14 00:56:59 +0200

    Update teamspeak-client.rb

--- Casks/teamspeak-client.rb
+++ Casks/teamspeak-client.rb
@@ -1,11 +1,11 @@
 cask "teamspeak-client" do
   version "3.5.6"
-
   sha256 "03c8e44256f2028917866924e496d4704de9e3298252f433fc53cfa37282770e"
-  url "https://dl.4players.de/ts/releases/#{version.before_comma}/TeamSpeak3-Client-macosx-#{version.before_comma}.dmg"
 
+  url "https://dl.4players.de/ts/releases/#{version.before_comma}/TeamSpeak3-Client-macosx-#{version.before_comma}.dmg",
+      verified: "https://dl.4players.de/ts"
   name "TeamSpeak 3 Client"
-  desc "powered by 4Netplayers"
+  desc "Powered by 4Netplayers"
   homepage "https://www.teamspeak.de/"
 
   livecheck do
@@ -15,5 +15,4 @@ cask "teamspeak-client" do
   auto_updates true
 
   app "TeamSpeak 3 Client.app"
-
 end
