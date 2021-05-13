215e4cd56837f227629d73d8e0c310caa1d795d5
jaklan <jakub.lanski@gmail.com>
2021-05-14 01:00:29 +0200

    Update name field in jpadilla-rabbitmq.rb

--- Casks/jpadilla-rabbitmq.rb
+++ Casks/jpadilla-rabbitmq.rb
@@ -4,7 +4,7 @@ cask "jpadilla-rabbitmq" do
 
   url "https://github.com/jpadilla/rabbitmqapp/releases/download/#{version}/RabbitMQ.zip",
       verified: "github.com/jpadilla/rabbitmqapp/"
-  name "RabbitMQ"
+  name "RabbitMQ.app"
   desc "App wrapper for RabbitMQ"
   homepage "https://jpadilla.github.io/rabbitmqapp/"
 
