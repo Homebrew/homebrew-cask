6859fedecb0b24b8cf9e52f5c55c979b31d4a7e7
William Davis <daviswill048@icloud.com>
2021-05-14 13:47:52 -0400

    Add JGR's OpenTTD patch pack v0.41.1

---
+++ Casks/openttd-jgrpp.rb
@@ -0,0 +1,11 @@
+cask "openttd-jgrpp" do
+  version "0.41.1"
+  sha256 "34aa82f3f9eb40d7f50782550101ba313bb82ea6d53544557a9b965e6327f728"
+
+  url "https://github.com/JGRennison/OpenTTD-patches/releases/download/jgrpp-#{version}/openttd-jgrpp-#{version}-macos-universal.dmg"
+  name "JGR's OpenTTD Patchpack"
+  desc "Collection of patches applied to OpenTTD"
+  homepage "https://github.com/JGRennison/OpenTTD-patches/"
+
+  app "OpenTTD.app"
+end
