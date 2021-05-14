b10f24b9aa91b1dd0ce7658d9d0eaecf177c81fb
R. Cody Erben <erben22@mtnaircomputer.net>
2021-05-14 00:05:03 -0600

    Update plex-media-server from 1.22.3.4392-d7c624def to 1.23.0.4497-a1b1f3c10

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -1,6 +1,6 @@
 cask "plex-media-server" do
-  version "1.22.3.4392-d7c624def"
-  sha256 "3ef51a68a59d7e7f7727c53733eb40b6194eb7a16d8effb0b1a4b5311a3f0fee"
+  version "1.23.0.4497-a1b1f3c10"
+  sha256 "743d8b9aa39443ec70aeec3266c1ca2f035dd39ac2daa10caf899d6f3893e5f0"
 
   url "https://downloads.plex.tv/plex-media-server-new/#{version}/macos/PlexMediaServer-#{version}-x86_64.zip"
   name "Plex Media Server"
