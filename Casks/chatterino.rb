29a44681b916335e08535355e0183c232494ce96
James Upjohn <jammehcow@jammehcow.co.nz>
2021-05-14 18:42:15 +1200

    Update sha256 sum for chatterino cask

--- Casks/chatterino.rb
+++ Casks/chatterino.rb
@@ -1,6 +1,6 @@
 cask "chatterino" do
   version "2.3.1"
-  sha256 "TODO"
+  sha256 "0622d1c22bc95ad5ae35d329015afa35fc8ca3b0bb45d35dad19c64829ea7cb3"
 
   url "https://chatterino.fra1.digitaloceanspaces.com/bin/#{version}/Chatterino.dmg",
       verified: "chatterino.fra1.digitaloceanspaces.com/"
