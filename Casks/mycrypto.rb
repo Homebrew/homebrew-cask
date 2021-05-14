404c21113e86e6def4e7db0e929b0c6108798b93
Ruslan Molchanov <ruslanys@gmail.com>
2021-05-15 00:11:04 +0300

    Update mycrypto.rb

--- Casks/mycrypto.rb
+++ Casks/mycrypto.rb
@@ -1,5 +1,5 @@
 cask "mycrypto" do
-  version "1.7.13"
+  version "1.7.16"
   sha256 "de7f1133f9ea2c8aeb1c1f58c8ae0c864b79a956dc99904debe72fa5f707382c"
 
   url "https://github.com/MyCryptoHQ/MyCrypto/releases/download/#{version}/mac_#{version}_MyCrypto.dmg",
