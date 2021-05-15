6a19e7449b932edfa21c5068dc26842ca103bfc1
Michael Cho <cho-m@tuta.io>
2021-05-15 13:38:31 -0700

    Update melodics from 2.1.6059 to 2.1.6079

--- Casks/melodics.rb
+++ Casks/melodics.rb
@@ -1,15 +1,17 @@
 cask "melodics" do
-  version "2.1.6059"
-  sha256 :no_check # required as upstream package is updated in-place
+  version "2.1.6079,A2A93732-8BA6-41EC-87BA-1C345A1C022B"
+  sha256 "6c9c3579742c271a7aeee3864fcd04cfd29f9ae18648fdf56bf2a426cb1ed562"
 
-  url "https://web-cdn.melodics.com/download/MelodicsV#{version.major}.dmg"
+  url "https://web-cdn.melodics.com/download/#{version.after_comma}.zip"
   name "Melodics"
   desc "Helps you learn to play your instrument"
   homepage "https://melodics.com/"
 
   livecheck do
     url "https://web-cdn.melodics.com/download/osxupdatescastv#{version.major}.xml"
-    strategy :sparkle
+    strategy :sparkle do |item|
+      "#{item.version},#{item.url[%r{/(\h+(?:-\h+)+)\.zip}i, 1]}"
+    end
   end
 
   depends_on macos: ">= :high_sierra"
