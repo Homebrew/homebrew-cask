4e6b2c4970a69251c54bac121509de743172bdb7
ymadd <Kb_yamato@protonmail.com>
2021-05-15 10:25:19 +0900

    Update keeweb from 1.18.4 to 1.18.5 (#105758)

--- Casks/keeweb.rb
+++ Casks/keeweb.rb
@@ -1,12 +1,12 @@
 cask "keeweb" do
-  version "1.18.4"
+  version "1.18.5"
 
   if Hardware::CPU.intel?
-    sha256 "1c9f4d8cafc78d9d4c61211647be98bc44057293de9413ec52027248d699f6b9"
+    sha256 "778b81b37a2d6861304b6c980001342a80ca6d5fbd54dae6bc60a9f96b992cd2"
     url "https://github.com/keeweb/keeweb/releases/download/v#{version}/KeeWeb-#{version}.mac.x64.dmg",
         verified: "github.com/keeweb/keeweb/"
   else
-    sha256 "5db8af72a10f74390ec5357a2ba50a05c2304521090dea30ebdb1b0bfd1a6105"
+    sha256 "a593c8dfcf17cf3af6584bbb8463d2bb437100f8146f34805a20e1b346564c9e"
     url "https://github.com/keeweb/keeweb/releases/download/v#{version}/KeeWeb-#{version}.mac.arm64.dmg",
         verified: "github.com/keeweb/keeweb/"
   end
