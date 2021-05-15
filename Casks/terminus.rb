8f56bdeb77018b0d0520888d7e7848d9f974d780
Michael Cho <cho-m@tuta.io>
2021-05-15 13:00:16 -0700

    Update terminus from 1.0.138 to 1.0.139

--- Casks/terminus.rb
+++ Casks/terminus.rb
@@ -1,13 +1,13 @@
 cask "terminus" do
-  version "1.0.138"
+  version "1.0.139"
 
   if Hardware::CPU.intel?
-    sha256 "26f02e6141bad4e93529b0b359b318d31a90a44c8b7fa6c3eda3128ab7c2705a"
+    sha256 "0616706e5e4324200b269d9434b30b2819b2e36683b71c639dd9280b7b9a5beb"
 
     url "https://github.com/Eugeny/terminus/releases/download/v#{version}/terminus-#{version}-macos-x86_64.zip",
         verified: "github.com/Eugeny/terminus/"
   else
-    sha256 "d88a830922a329e2f50b290bfdbd54765968e96d1efc77f67c675d27e7cf2534"
+    sha256 "dc84421cb3d7301b30d9d78d0254a3eafd6058947e4c7be6f5a3a8137f6d01e9"
 
     url "https://github.com/Eugeny/terminus/releases/download/v#{version}/terminus-#{version}-macos-arm64.zip",
         verified: "github.com/Eugeny/terminus/"
