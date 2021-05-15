1884cd8058c6f9868fd27cfc6de316c93112c844
Michael Cho <cho-m@tuta.io>
2021-05-15 02:08:32 -0700

    Update studio-3t from 2020.10.1 to 2021.2.0 and add livecheck (#105776)

--- Casks/studio-3t.rb
+++ Casks/studio-3t.rb
@@ -1,11 +1,16 @@
 cask "studio-3t" do
-  version "2020.10.1"
-  sha256 "b88b91834566418611c93920cfb5d9737664c374f825ff37331ada1d34f593da"
+  version "2021.2.0"
+  sha256 "97d6d33aa0d07878f211f71443ab3b1bdc9e79144655cf555d1a6d2cfd4e5016"
 
   url "https://download.studio3t.com/studio-3t/mac/#{version}/Studio-3T.dmg"
-  appcast "https://files.studio3t.com/changelog/changelog.txt"
   name "Studio 3T"
+  desc "IDE, client, and GUI for MongoDB"
   homepage "https://studio3t.com/"
+
+  livecheck do
+    url "https://studio3t.com/download-thank-you/?OS=osx"
+    regex(%r{/v?(\d+(?:\.\d+)+)/Studio[._-]?3T\.dmg}i)
+  end
 
   auto_updates true
 
