e5205647c9d6842dfd36c04c721a261720bdb92d
Michael Cho <cho-m@tuta.io>
2021-05-15 02:09:08 -0700

    Update valentina-studio from 11.2.6 to 11.2.7 and use versioned URL (#105780)

--- Casks/valentina-studio.rb
+++ Casks/valentina-studio.rb
@@ -1,16 +1,15 @@
 cask "valentina-studio" do
-  version "11.2.6"
-  sha256 :no_check
+  version "11.2.7"
+  sha256 "fe5dbe3b93acb616726ba8e3d74e4137fa7f7f69fa1a22ece89c571bcfe7b2cb"
 
-  url "https://valentina-db.com/en/all-downloads/vstudio/current/vstudio_mac_x64-dmg?format=raw"
+  url "https://valentina-db.com/download/prev_releases/#{version}/mac_64/vstudio_x64_#{version.major}_mac.dmg"
   name "Valentina Studio"
   desc "Visual editors for data"
   homepage "https://valentina-db.com/en/valentina-studio-overview"
 
   livecheck do
     url "https://valentina-db.com/en/all-downloads/vstudio"
-    strategy :page_match
-    regex(%r{href=['"]?/en/all-downloads/vstudio/current['"]?>\s*(\d+(?:\.\d+)*)}i)
+    regex(%r{href=['"]?/en/all-downloads/vstudio/current['"]?>\s*(\d+(?:\.\d+)+)}i)
   end
 
   app "Valentina Studio.app"
