47b8bd4e7483f9b21587a8e9591c9c4bb1a0cc59
Michael Cho <cho-m@tuta.io>
2021-05-15 02:14:55 -0700

    Update tempo from 5.14.0 to 5.15.0 and add ARM

--- Casks/tempo.rb
+++ Casks/tempo.rb
@@ -1,8 +1,16 @@
 cask "tempo" do
-  version "5.14.0"
-  sha256 "4683ab958633cbe5054dc7a86166d8752f69e17eeb7bd65deb73bf84195f87aa"
+  version "5.15.0"
 
-  url "https://download.yourtempo.co/release/Tempo-#{version}.dmg"
+  if Hardware::CPU.intel?
+    sha256 "97d01cc83d670f01afeb52816f84a98125a398fccf8492398cb7fb27e77e18b4"
+
+    url "https://download.yourtempo.co/release/Tempo-#{version}.dmg"
+  else
+    sha256 "0e2bf7aed66b78afc4069f08f025adbe041de1772cc3ae6293c83366cabb9945"
+
+    url "https://download.yourtempo.co/release-arm64/Tempo-#{version}.dmg"
+  end
+
   name "Tempo"
   desc "Email client that delivers all email in batches"
   homepage "https://www.yourtempo.co/"
