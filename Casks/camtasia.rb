d5a15b5f3c1e5308f3047338b84927d294c2ea9b
Michael Cho <cho-m@tuta.io>
2021-05-15 11:25:48 -0700

    Update camtasia from 2021.0.0 to 2021.0.1

--- Casks/camtasia.rb
+++ Casks/camtasia.rb
@@ -1,6 +1,6 @@
 cask "camtasia" do
-  version "2021.0.0"
-  sha256 "1afed5fb6bef606eae88d116247bb8ba5d678b33716c342ea8a548d2a3f05240"
+  version "2021.0.1"
+  sha256 "d53a0a29dc7af8f2d2b8ba16197ac84c6fc79fa8f0107f18b5fbd63db1782c61"
 
   url "https://download.techsmith.com/camtasiamac/releases/#{version.major[-2..]}#{version.minor_patch.no_dots}/Camtasia.dmg"
   name "Camtasia"
