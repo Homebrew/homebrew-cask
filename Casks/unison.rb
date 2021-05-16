645e4f62cc81965c612c50eddee2b68903a4e87d
Giovanni Merlino <gmerlino@unime.it>
2021-05-16 22:26:01 +0200

    Update unison from 2.51.2 to 2.51.3

--- Casks/unison.rb
+++ Casks/unison.rb
@@ -3,11 +3,10 @@ cask "unison" do
     version "2.48.3"
     sha256 "d578196d8b38f35c1e0410a1c86ff4e115a91f7eb211201db7a940a3a3e0f099"
 
-    url "https://github.com/bcpierce00/unison/releases/download/#{version}/Unison-OS-X-#{version}.zip",
-        verified: "github.com/bcpierce00/unison/"
+    url "https://github.com/bcpierce00/unison/releases/download/#{version}/unison-#{version}+ocaml-4.11.1+x86_64.macos-10.15.tar.gz,\n\ \ \ \ \ \ \ \ verified: github.com/bcpierce00/unison/"
   else
-    version "2.51.2"
-    sha256 "0738a6978fa29bb2af409322069cc20df293b770877942ac4b8774f06e774aa5"
+    version "2.51.3"
+    sha256 "f4dd94efcdbb5ca2627226ccefdf0263d0aad151db48c9f137fcb5c8f0be1746"
 
     url "https://github.com/bcpierce00/unison/releases/download/v#{version}/Unison-#{version}.OS.X.zip",
         verified: "github.com/bcpierce00/unison/"
