5422204760adc22e0b81ea3a39e1839d0682397e
Michael Cho <cho-m@tuta.io>
2021-05-16 01:19:25 -0700

    Update chronycontrol from 1.4.1,257 to 1.4.2,266 (#105818)
    * Update chronycontrol from 1.4.1,257 to 1.4.2,266
    
    * Update chronycontrol.rb

--- Casks/chronycontrol.rb
+++ Casks/chronycontrol.rb
@@ -1,9 +1,10 @@
 cask "chronycontrol" do
-  version "1.4.1,257"
-  sha256 "aad365450dea578130bce2b61a344be64d76b9af242afefdc92370cc6b82f664"
+  version "1.4.2,266"
+  sha256 "fb6e42f7d0ed9f2e0d706752f3bd995f7a022a8e0587debf6fcc8f438dc9eafe"
 
   url "https://www.whatroute.net/software/chronycontrol-#{version.before_comma}.zip"
   name "ChronyControl"
+  desc "Install and configure chronyd"
   homepage "https://whatroute.net/chronycontrol.html"
 
   livecheck do
