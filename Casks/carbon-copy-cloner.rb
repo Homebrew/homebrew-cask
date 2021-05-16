8f3ba210ee998d0322ec5b1d867153f7d87e6759
Michael Cho <cho-m@tuta.io>
2021-05-15 21:12:49 -0700

    Update carbon-copy-cloner from 5.1.27.6193 to 5.1.27.6196 (#105844)

--- Casks/carbon-copy-cloner.rb
+++ Casks/carbon-copy-cloner.rb
@@ -1,6 +1,6 @@
 cask "carbon-copy-cloner" do
-  version "5.1.27.6193"
-  sha256 "c6723d9e3075460115a88a3ede82eb46f3e37fe1a782888b6825b4186eeb7dcb"
+  version "5.1.27.6196"
+  sha256 "b2edc2798f3e7497c3b2f624517909a8876b89026d181ac0c321447a6d47849f"
 
   url "https://bombich.scdn1.secure.raxcdn.com/software/files/ccc-#{version}.zip",
       verified: "bombich.scdn1.secure.raxcdn.com/software/files/"
