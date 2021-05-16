54ae634735dfc9c23491b0a3ea2ea54c09242203
alexozwald <aoswald609@gmail.com>
2021-05-16 14:24:14 -0400

    Update yacreader from 9.7.1.2009123 to 9.8.0.2105165

--- Casks/yacreader.rb
+++ Casks/yacreader.rb
@@ -1,6 +1,6 @@
 cask "yacreader" do
-  version "9.7.1.2009123"
-  sha256 "068dbed581b8098d2eb36d32a8a16966eb588519d8b64ae592a2383d91bb36ec"
+  version "9.8.0.2105165"
+  sha256 "d22a4107e895cee961bcef4dcbfb7061942baae13a3c0a0d790bb82199153430"
 
   url "https://github.com/YACReader/yacreader/releases/download/#{version.major_minor_patch}/YACReader-#{version}.MacOSX-Intel.dmg",
       verified: "github.com/YACReader/yacreader/"
