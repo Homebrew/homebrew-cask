7633c7ff12cbe7bb9d24c52237368d53ba87ee0f
Michael Cho <cho-m@tuta.io>
2021-05-16 16:51:25 -0700

    Update grandtotal from 7.0.6 to 7.0.7

--- Casks/grandtotal.rb
+++ Casks/grandtotal.rb
@@ -1,6 +1,6 @@
 cask "grandtotal" do
-  version "7.0.6"
-  sha256 "27961bb23bbd839ee6f40cf9413a1af92e2dda5d4c6696f5c467835415ec2c77"
+  version "7.0.7"
+  sha256 "25303fdfdddaeef0137af338576750f03383e385ed73407c3a40ffe7c6c493c4"
 
   url "https://mediaatelier.com/GrandTotal#{version.major}/GrandTotal_#{version}.zip"
   name "GrandTotal"
