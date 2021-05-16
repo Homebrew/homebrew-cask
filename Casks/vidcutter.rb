2d6c0a4166a3d5c72656349fe0dd557dc9ac030e
Michael Cho <cho-m@tuta.io>
2021-05-16 15:33:47 -0700

    vidcutter: add livecheck and desc

--- Casks/vidcutter.rb
+++ Casks/vidcutter.rb
@@ -2,10 +2,16 @@ cask "vidcutter" do
   version "6.0.2"
   sha256 "cebc95bd66d5df7fb46831bdd12993a9738ea1528850275e09a3ca69446851a7"
 
-  url "https://github.com/ozmartian/vidcutter/releases/download/#{version.major_minor}.0/VidCutter-#{version}-macOS.dmg",
-      verified: "github.com/ozmartian/vidcutter/"
+  url "https://github.com/ozmartian/vidcutter/releases/download/#{version.major_minor}.0/VidCutter-#{version}-macOS.dmg"
   name "VidCutter"
-  homepage "https://vidcutter.ozmartians.com/"
+  desc "Media cutter and joiner"
+  homepage "https://github.com/ozmartian/vidcutter"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+    regex(%r{href=.*?/VidCutter[._-]v?(\d+(?:\.\d+)+)[._-]macOS\.dmg}i)
+  end
 
   app "VidCutter.app"
 end
