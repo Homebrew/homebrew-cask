0f9021e7f05af1ea160a4d335f493c1837aad449
Michael Cho <cho-m@tuta.io>
2021-05-16 01:20:12 -0700

    Update futubull from 11.3.928,202104142135 to 11.5.1008,202105100950 (#105851)

--- Casks/futubull.rb
+++ Casks/futubull.rb
@@ -1,6 +1,6 @@
 cask "futubull" do
-  version "11.3.928,202104142135"
-  sha256 "ff7f56f18119c53971a6c314b8599c79a552eee31821544a0e5bd438b6761893"
+  version "11.5.1008,202105100950"
+  sha256 "37babab32dcbd890cfb543b0825aefa657e1f51014435b7f84807ece436803f6"
 
   url "https://softwarefile.futunn.com/FTNNForMac_#{version.before_comma}_#{version.after_comma}_Website.dmg"
   name "Futubull"
