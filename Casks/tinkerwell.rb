d7da37e89d54240f7737dccab562f6a3f44691e4
Michael Cho <cho-m@tuta.io>
2021-05-16 01:19:30 -0700

    Update tinkerwell from 2.13.1 to 2.14.0 and add ARM DMG (#105812)

--- Casks/tinkerwell.rb
+++ Casks/tinkerwell.rb
@@ -1,8 +1,16 @@
 cask "tinkerwell" do
-  version "2.13.1"
-  sha256 "b5595a0d9b26fdcb7620d1813ad50e8314fb8adcfb8694520137a0d9a76cadc5"
+  version "2.14.0"
 
-  url "https://download.tinkerwell.app/tinkerwell/Tinkerwell-#{version}.dmg"
+  if Hardware::CPU.intel?
+    sha256 "f4f2274683b66332937f9086322a97a1a0aca5a6051f9e7cbef6c5b2f95ed42b"
+
+    url "https://download.tinkerwell.app/tinkerwell/Tinkerwell-#{version}.dmg"
+  else
+    sha256 "27b84d8fc8dc0e58d13b63d7b94b4f9f72814307ad6acd43d10fb885e273aea1"
+
+    url "https://download.tinkerwell.app/tinkerwell/Tinkerwell-#{version}-arm64.dmg"
+  end
+
   name "Tinkerwell"
   desc "Tinker tool for PHP and Laravel developers"
   homepage "https://tinkerwell.app/"
