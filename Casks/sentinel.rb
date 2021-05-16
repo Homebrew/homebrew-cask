59faf89ca84e9a56fbfd1547e1f639c6a8618d10
Michael Cho <cho-m@tuta.io>
2021-05-16 01:15:24 -0700

    Update sentinel from 0.18.0 to 0.18.1 (#105801)

--- Casks/sentinel.rb
+++ Casks/sentinel.rb
@@ -1,6 +1,6 @@
 cask "sentinel" do
-  version "0.18.0"
-  sha256 "0a2390cf0bfae85ecf09f297c5bcb9a4673b315375bc02f8dba0bf83acf61dbe"
+  version "0.18.1"
+  sha256 "3b4837e777c934a4e42fa7491e5aaca238c1670220bb6563be026733fddab200"
 
   url "https://releases.hashicorp.com/sentinel/#{version}/sentinel_#{version}_darwin_amd64.zip"
   name "Sentinel"
@@ -9,8 +9,7 @@ cask "sentinel" do
 
   livecheck do
     url "https://docs.hashicorp.com/sentinel/downloads"
-    strategy :page_match
-    regex(%r{href=.*?/sentinel_(\d+(?:\.\d+)*)_darwin_amd64\.zip}i)
+    regex(%r{href=.*?/sentinel[._-]?v?(\d+(?:\.\d+)+)[._-]darwin[._-]amd64\.zip}i)
   end
 
   binary "sentinel"
