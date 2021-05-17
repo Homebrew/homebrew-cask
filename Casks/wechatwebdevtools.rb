083353d8d3fdde545415ed7f5f2325fdcfbc0b66
Michael Cho <cho-m@tuta.io>
2021-05-16 17:28:41 -0700

    Update wechatwebdevtools from 1.05.2103200 to 1.05.2105100

--- Casks/wechatwebdevtools.rb
+++ Casks/wechatwebdevtools.rb
@@ -1,6 +1,6 @@
 cask "wechatwebdevtools" do
-  version "1.05.2103200"
-  sha256 "0b0b1ee28ad4edc0f422039ab9a47be885528552b04a11929c78bbfe5b4aa0a3"
+  version "1.05.2105100"
+  sha256 "cadaf941663783d78fd28c04ff80e2678ad237d288b2ccdd41673914c091c23c"
 
   url "https://dldir1.qq.com/WechatWebDev/release/p-ae42ee2cde4d42ee80ac60b35f183a99/wechat_devtools_#{version}.dmg"
   name "wechat web devtools"
