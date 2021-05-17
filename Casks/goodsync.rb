6f8f18b166439894137f353960ac0938416bc4cc
Michael Cho <cho-m@tuta.io>
2021-05-17 10:17:01 -0700

    Update goodsync from 11.6.5 to 11.6.6

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,6 +1,6 @@
 cask "goodsync" do
-  version "11.6.5"
-  sha256 "ce118f237b40da5b6c78bcc4a57c24aa12e775576f6a3b623d1dc47ab1703db2"
+  version "11.6.6"
+  sha256 "9d84a281f6d002c502cd27195f0b62afcd0ddfc2b9f6d4fd56e1b8979747cb8f"
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
   name "GoodSync"
