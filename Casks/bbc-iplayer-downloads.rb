fe53458d48db2d005906cee0b0e1332f199f6c6f
Michael Cho <cho-m@tuta.io>
2021-05-17 09:56:00 -0700

    Update bbc-iplayer-downloads from 2.12.10 to 2.13.1

--- Casks/bbc-iplayer-downloads.rb
+++ Casks/bbc-iplayer-downloads.rb
@@ -1,6 +1,6 @@
 cask "bbc-iplayer-downloads" do
-  version "2.12.10"
-  sha256 "c50dbb2b744a93407fe83ed69565eef60389836b1689ea9db0fa68bdb0c2a10e"
+  version "2.13.1"
+  sha256 "b64c54f63d283975f1eaa40356c0c467a885dfdd585514eccc1776621306b47c"
 
   url "https://static.files.bbci.co.uk/iplayer-pc-download-app/releases/darwin-x64/BBCiPlayerDownloads-#{version}.dmg"
   name "BBC iPlayer Downloads"
