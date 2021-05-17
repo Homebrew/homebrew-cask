e59c32a04a0bb9a58220d8b1e7fb07896f5b20fb
ymadd <Kb_yamato@protonmail.com>
2021-05-17 13:54:53 +0900

    Update electerm from 1.12.21 to 1.12.24

--- Casks/electerm.rb
+++ Casks/electerm.rb
@@ -1,6 +1,6 @@
 cask "electerm" do
-  version "1.12.21"
-  sha256 "b9a1aaa22d85d65ed6689370fe219c2c8c2c0fea9910d634196e052b5b763f25"
+  version "1.12.24"
+  sha256 "a40b236370f5e9e10db51bc4f5837a67609392e6f4ed2db824c273fe3bf252bc"
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac.dmg"
   name "electerm"
