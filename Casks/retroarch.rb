a79fad085efc97ca1d4bb3fb4e20498629d2f668
Michael Cho <cho-m@tuta.io>
2021-05-17 10:35:05 -0700

    Update retroarch.rb

--- Casks/retroarch.rb
+++ Casks/retroarch.rb
@@ -3,9 +3,14 @@ cask "retroarch" do
   sha256 "e579b9a52eb8326cf93d92c944bccbfee9f52a3a0588204a1e1aea4191be3418"
 
   url "https://buildbot.libretro.com/stable/#{version}/apple/osx/x86_64/RetroArch.dmg"
-  appcast "https://buildbot.libretro.com/stable/"
   name "RetroArch"
+  desc "Emulator frontend (OpenGL graphics API version)"
   homepage "https://www.libretro.com/"
+
+  livecheck do
+    url "https://buildbot.libretro.com/stable/"
+    regex(%r{href=["']?/stable/v?(\d+(?:\.\d+)+)/?["' >]}i)
+  end
 
   app "RetroArch.app"
 end
