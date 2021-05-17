7de2cd7f786b140b620c01ebe84b618fdb3c6feb
Michael Cho <cho-m@tuta.io>
2021-05-17 15:37:14 -0700

    Update vuescan from 9.7.54 to 9.7.55

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,5 +1,5 @@
 cask "vuescan" do
-  version "9.7.54"
+  version "9.7.55"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.hamrick.com/files/vuea64#{version.major_minor.no_dots}.dmg"
