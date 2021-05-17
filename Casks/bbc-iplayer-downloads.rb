a94dbb1678f11aa7fdb6046cd1e24a89fe5e3d96
Michael Cho <cho-m@tuta.io>
2021-05-17 09:58:48 -0700

    Update bbc-iplayer-downloads.rb

--- Casks/bbc-iplayer-downloads.rb
+++ Casks/bbc-iplayer-downloads.rb
@@ -4,12 +4,15 @@ cask "bbc-iplayer-downloads" do
 
   url "https://static.files.bbci.co.uk/iplayer-pc-download-app/releases/darwin-x64/BBCiPlayerDownloads-#{version}.dmg"
   name "BBC iPlayer Downloads"
+  desc "Download programmes from the BBC iPlayer website"
   homepage "https://www.bbc.co.uk/iplayer/install"
 
   livecheck do
     url "https://downloads-app.iplayer.api.bbc.co.uk/stable/darwin-x64"
     strategy :header_match
   end
+
+  depends_on macos: ">= :yosemite"
 
   app "BBC iPlayer Downloads.app"
 end
