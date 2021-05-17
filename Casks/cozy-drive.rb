58bf3739ec2795cadd0fca13bebb8ccb8385d6f1
Michael Cho <cho-m@tuta.io>
2021-05-17 11:19:22 -0700

    Update cozy-drive from 3.27.0 to 3.28.0 (#105932)
    * Update cozy-drive from 3.27.0 to 3.28.0
    
    * Update cozy-drive.rb

--- Casks/cozy-drive.rb
+++ Casks/cozy-drive.rb
@@ -1,16 +1,19 @@
 cask "cozy-drive" do
-  version "3.27.0"
-  sha256 "f197b068256c3faf79d369e87eb8145737d12438455d266aaeb0015a1b0dda3d"
+  version "3.28.0"
+  sha256 "6ef862d491a66ef87c714b0a7568a7cc88a1109ecf28c619d75507a08aa1504a"
 
   url "https://nuts.cozycloud.cc/download/channel/stable/CozyDrive-#{version}.dmg",
       verified: "nuts.cozycloud.cc/"
   name "Cozy Drive"
+  desc "File synchronization for Cozy Cloud"
   homepage "https://cozy.io/"
 
   livecheck do
     url "https://nuts.cozycloud.cc/download/channel/stable/osx"
     strategy :header_match
   end
+
+  depends_on macos: ">= :sierra"
 
   app "Cozy Drive.app"
 end
