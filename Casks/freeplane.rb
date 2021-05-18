e7499856c1b06e79dfd5a1c0ccfc40fb1b763fad
osx user <gili.shemesh@gmail.com>
2021-05-18 23:26:31 +0300

    freeplane.app bump to 1.9.18

--- Casks/freeplane.rb
+++ Casks/freeplane.rb
@@ -1,9 +1,18 @@
 cask "freeplane" do
-  version "1.8.11"
-  sha256 "5f558640e8811c0d4b515dfc67c4d9ef5d877dfb3b7c88a3578d97001f6b337a"
+  version "1.9.1"
 
-  url "https://downloads.sourceforge.net/freeplane/freeplane%20stable/Freeplane-#{version}.dmg",
-      verified: "downloads.sourceforge.net/freeplane/"
+  if Hardware::CPU.intel?
+    sha256 "8dfedf459be9d36fbaf473cee2ac78680a1cb95f58b36d48cfd03c27851a2291"
+    url "https://downloads.sourceforge.net/project/freeplane/freeplane%20stable/Freeplane-#{version}-intel.dmg",
+        verified: "downloads.sourceforge.net/project/freeplane/freeplane%20stable/Freeplane-#{version}-intel.dmg"
+
+  else
+    sha256 "255734becd2d6e2d4781ae0dc24b399c86172e92f3b6359b5c0f4c3f34b72ea7"
+
+    url "https://downloads.sourceforge.net/project/freeplane/freeplane%20stable/Freeplane-#{version}-apple.dmg",
+        verified: "downloads.sourceforge.net/project/freeplane/freeplane%20stable/Freeplane-#{version}-apple.dmg"
+  end
+
   name "Freeplane"
   desc "Mind mapping and knowledge management software"
   homepage "https://freeplane.sourceforge.io/"
