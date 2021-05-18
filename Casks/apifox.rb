212fdaa26d01a6a0955e0cffad92e4cd042aef7f
钟彪明 <biaomingzhong@gmail.com>
2021-05-18 13:20:10 +0800

    feat: fix apifox 1.3.6 cask rb script

--- Casks/apifox.rb
+++ Casks/apifox.rb
@@ -2,8 +2,10 @@ cask "apifox" do
   version "1.3.6"
   sha256 "6ac226c9fcdcb25fcb3c96070d49e1e823b7d43a274b11a0b05402c60e125cfa"
 
-  url "https://github.com/biaomingzhong/apifox-versions/releases/download/v#{version}/Apifox-#{version}.dmg"
+  url "https://github.com/biaomingzhong/apifox-versions/releases/download/v#{version}/Apifox-#{version}.dmg",
+      verified: "github.com/biaomingzhong/apifox-versions/"
   name "apifox"
+  desc "Apifox = Postman + Swagger + Mock + JMeter，接口文档、调试、Mock、自动化测试，提升 10 倍效率！"
   homepage "https://www.apifox.cn/"
 
   app "Apifox.app"
