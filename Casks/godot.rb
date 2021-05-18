34485b5102d8c45b82b49319d7e21304d8bb938d
Michael Cho <cho-m@tuta.io>
2021-05-18 16:38:58 -0700

    Update godot from 3.3 to 3.3.1

--- Casks/godot.rb
+++ Casks/godot.rb
@@ -1,6 +1,6 @@
 cask "godot" do
-  version "3.3"
-  sha256 "f490a91e840ada5f6cc1db030e2d6950ae58583b388e763484e9b5a16cbdcaf7"
+  version "3.3.1"
+  sha256 "205734a8d15f506a383d610f057491bdc51dc65cb5a3a57b94801e66855da944"
 
   url "https://downloads.tuxfamily.org/godotengine/#{version}/Godot_v#{version}-stable_osx.universal.zip",
       verified: "downloads.tuxfamily.org/godotengine/"
