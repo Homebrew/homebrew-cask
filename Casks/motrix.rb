2835c8efdf83d21ad1a33eda0b2f96ded6ea6f8b
Anagh Kumar Baranwal <6824881+darthShadow@users.noreply.github.com>
2021-05-18 21:24:17 +0530

    Update motrix from 1.6.10 to 1.6.11

--- Casks/motrix.rb
+++ Casks/motrix.rb
@@ -1,13 +1,13 @@
 cask "motrix" do
-  version "1.6.10"
+  version "1.6.11"
 
   if Hardware::CPU.intel?
-    sha256 "813e202a3d2555134fe841e5b790dcb2c539f6773389890412ec06a118ce629e"
+    sha256 "70fa245e44b8e0ec62f50ba5731bc0a876535a51dc0ea318010736d2a6be6dd9"
 
     url "https://github.com/agalwood/Motrix/releases/download/v#{version}/Motrix-#{version}.dmg",
         verified: "github.com/agalwood/Motrix/"
   else
-    sha256 "9cde89252b5a55eb714030038013f5a31dffabe8712bbfccbe4aa5ffc726c684"
+    sha256 "1705d6ef4781c17ebf19007dbb2ac52f6227592c783e91ad5b81a1432cf6b668"
 
     url "https://github.com/agalwood/Motrix/releases/download/v#{version}/Motrix-#{version}-arm64.dmg",
         verified: "github.com/agalwood/Motrix/"
