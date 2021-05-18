22c3eb87d0a1cab39eb01a5aaf00098ca0242b83
Michael Cho <cho-m@tuta.io>
2021-05-18 16:18:57 -0700

    Update zecwallet-lite from 1.6.1 to 1.6.3 (#105999)

--- Casks/zecwallet-lite.rb
+++ Casks/zecwallet-lite.rb
@@ -1,6 +1,6 @@
 cask "zecwallet-lite" do
-  version "1.6.1"
-  sha256 "903a069e672341a00bc9cf91865375ba09136834b0d0354e50de702d372a8066"
+  version "1.6.3"
+  sha256 "1b15f4ff5abb3356d4a06b81408d43327548e1b1dc2f664d522566e9efa1b55b"
 
   url "https://github.com/adityapk00/zecwallet-lite/releases/download/v#{version}/Zecwallet.Lite-#{version}.dmg",
       verified: "github.com/adityapk00/zecwallet-lite/"
