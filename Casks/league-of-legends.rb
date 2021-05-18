9038ed9fcaf47a6396cc26e2dba41b8661d57f4c
Miccal Matthews <miccal.matthews@gmail.com>
2021-05-18 12:15:07 +0800

    Update league-of-legends.rb

--- Casks/league-of-legends.rb
+++ Casks/league-of-legends.rb
@@ -5,28 +5,32 @@ cask "league-of-legends" do
   url "https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.na.zip",
       verified: "lol.secure.dyn.riotcdn.net/"
   name "League of Legends"
+  desc "Multiplayer online battle arena game"
   homepage "https://na.leagueoflegends.com/en-us/"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    skip "unversioned URL"
   end
 
   installer manual: "Install League of Legends na.app"
 
+  uninstall delete: "/Applications/League of Legends.app"
+
   zap trash: [
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.riotgames.maccontainer.sfl*",
+    "~/Library/Application Support/Riot Games/Install League of Legends",
+    "~/Library/Application Support/Riot Games/League of Legends",
+    "~/Library/Caches/com.riotgames.LeagueofLegends.LeagueClient",
+    "~/Library/Logs/Riot Games/Install League of Legends na",
+    "~/Library/Preferences/com.riotgames.LeagueofLegends.LeagueClientUxHelper.plist",
     "~/Library/Saved Application State/com.riotgames.LeagueofLegends.GameClient.savedState",
     "~/Library/Saved Application State/com.riotgames.LeagueofLegends.LeagueClientUx.savedState",
-    "~/Library/Preferences/com.riotgames.LeagueofLegends.LeagueClientUxHelper.plist",
-    "~/Library/Caches/com.riotgames.LeagueofLegends.LeagueClient",
-    "/Users/Shared/Riot Games",
+    "/Users/Shared/Riot Games/Metadata/league_of_legends.live.game_patch",
+    "/Users/Shared/Riot Games/Metadata/league_of_legends.live",
   ],
       rmdir: [
         "~/Documents/League of Legends",
-        "/Users/Shared/Riot Games/Riot Client.app",
-        "/Users/Shared/Riot Games/Metadata",
-        "~/Library/Application Support/Riot Games/League of Legends",
-        "~/Library/Application Support/Riot Games/Install League of Legends",
+        "~/Library/Application Support/Riot Games",
+        "/Users/Shared/Riot Games",
       ]
 end
