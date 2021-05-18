0903f41606523db599d0860cf9de69800d291dfb
Michael Cho <cho-m@tuta.io>
2021-05-17 18:35:43 -0700

    Update freetube from 0.12.0 to 0.13.0 (#105946)
    * Update freetube from 0.12.0 to 0.13.0
    
    * Update freetube.rb

--- Casks/freetube.rb
+++ Casks/freetube.rb
@@ -1,6 +1,6 @@
 cask "freetube" do
-  version "0.12.0"
-  sha256 "da7547fa3a79dd0b7bddde9c5f2434215b4b410bcacdb0ec0299a9e4bd057061"
+  version "0.13.0"
+  sha256 "8a8673124560251955651b57442d6e5bc9deb59425e6b9069b09a805f3d3226c"
 
   url "https://github.com/FreeTubeApp/FreeTube/releases/download/v#{version}-beta/freetube-#{version}-mac.dmg"
   name "FreeTube"
@@ -9,8 +9,7 @@ cask "freetube" do
 
   livecheck do
     url :url
-    strategy :git
-    regex(/^v?(\d+(?:\.\d+)*)/i)
+    regex(/^v?(\d+(?:\.\d+)+)/i)
   end
 
   app "FreeTube.app"
