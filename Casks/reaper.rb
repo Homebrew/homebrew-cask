49d2b566405b16022752ce438b60f63b654316d6
Vítor Galvão <info@vitorgalvao.com>
2021-05-20 00:33:47 +0100

    Update reaper.rb

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,18 +1,18 @@
 cask "reaper" do
-  version "6.29.0,6.29"
+  version "6.29.0"
 
   if MacOS.version <= :mojave
     sha256 "9cafc2dcda0ea11daea92f6558aea6eb3aa58917b91a50e81e2a623e3fda0f5f"
 
-    url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.after_comma.no_dots}_x86_64.dmg"
+    url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64.dmg"
   else
     sha256 "c29fca6eb8a9365a3b0a665eea546a428e4aec8bb4ac21e6d0f6f784c5530ca1"
 
-    url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.after_comma.no_dots}_x86_64_catalina.dmg"
+    url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64_catalina.dmg"
   end
 
   appcast "https://www.cockos.com/reaper/latestversion/?p=osx_64",
-          must_contain: version.after_comma
+          must_contain: version.major_minor
   name "REAPER"
   desc "Digital audio production application"
   homepage "https://www.reaper.fm/"
