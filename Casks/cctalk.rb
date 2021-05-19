a0e08701b25d6d7a86b6cd3833f8224f6ca52429
Michael Cho <cho-m@tuta.io>
2021-05-19 01:57:02 -0700

    Update cctalk from 7.8.3.1 to 7.8.5.3

--- Casks/cctalk.rb
+++ Casks/cctalk.rb
@@ -1,6 +1,6 @@
 cask "cctalk" do
-  version "7.8.3.1"
-  sha256 "02a68e31c257c0674c9d19ec89674026afade5184cc89f655cd942f3ca930996"
+  version "7.8.5.3"
+  sha256 "80ee2150bffeff9e8f4211acfb4c7589231533fabc23aad5cb31ffd456564263"
 
   url "https://cc.hjfile.cn/cc/CCtalk.#{version}/8/1/103/CCtalk.#{version}.dmg",
       verified: "cc.hjfile.cn/"
