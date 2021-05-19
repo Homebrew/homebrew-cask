8a4771a4ee26226b779762ed91f75e876d36efac
Alistair Evans <alistair.evans@enclave.io>
2021-05-19 02:15:08 +0100

    Add initial enclave cask (#105634)
    * Add enclave cask 2021.05.10
    
    * Upgrade enclave cask from 2021.05.10 to 2021.05.12 stable
    
    * Enclave cask 2021.05.12 - Apply requested changes from review

---
+++ Casks/enclave.rb
@@ -0,0 +1,27 @@
+cask "enclave" do
+  version "2021.05.12"
+  sha256 "a5af5020f28328672b89c8f782fda28909a1793e97d340816673c0d1326308ab"
+
+  url "https://release.enclave.io/enclave_osx-installer-x64-stable-#{version}.pkg"
+  name "Enclave"
+  desc "Safely build private networks without configs, firewalls or access control lists"
+  homepage "https://enclave.io/"
+
+  livecheck do
+    url "https://install.enclave.io/latest/osx-homebrew-version.txt"
+    regex(/(\d+(?:\.\d+)*)/)
+  end
+
+  depends_on macos: ">= :yosemite"
+
+  pkg "enclave_osx-installer-x64-stable-#{version}.pkg"
+  binary "/Applications/enclave/enclave"
+
+  uninstall launchctl: "com.enclave",
+            pkgutil:   "com.enclave.*"
+
+  zap delete: [
+    "/etc/enclave",
+    "/var/log/enclave",
+  ]
+end
