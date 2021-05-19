b8f661af6e5167d0af9d9529a40af92cc59fa6c5
Vítor Galvão <info@vitorgalvao.com>
2021-05-20 00:52:10 +0100

    Update Casks/pop.rb
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/pop.rb
+++ Casks/pop.rb
@@ -7,6 +7,9 @@ cask "pop" do
   desc "Remote pair programming"
   homepage "https://pop.com/"
 
+  # The download page (https://pop.com/download/mac) is rendered using
+  # JavaScript and the version information is sent over a WebSocket connection.
+  # As such, we can't check it and there don't appear to be any alternatives.
   livecheck do
     skip "File listing unavailable"
   end
