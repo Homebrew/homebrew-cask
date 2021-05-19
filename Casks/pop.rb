2b73330258099d3429ed1665f455ee4af3b14d1e
Vítor Galvão <info@vitorgalvao.com>
2021-05-20 00:53:05 +0100

    Update pop.rb

--- Casks/pop.rb
+++ Casks/pop.rb
@@ -7,10 +7,10 @@ cask "pop" do
   desc "Remote pair programming"
   homepage "https://pop.com/"
 
-  # The download page (https://pop.com/download/mac) is rendered using
-  # JavaScript and the version information is sent over a WebSocket connection.
-  # As such, we can't check it and there don't appear to be any alternatives.
   livecheck do
+    # The download page (https://pop.com/download/mac) is rendered using
+    # JavaScript and the version information is sent over a WebSocket connection.
+    # As such, we can't check it and there don't appear to be any alternatives.
     skip "Version information not available in a checkable format"
   end
 
