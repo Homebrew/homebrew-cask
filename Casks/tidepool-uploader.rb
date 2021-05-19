f225bc412f45e92e2c2c845639d261fd4a870dba
Michael Cho <cho-m@tuta.io>
2021-05-18 20:12:16 -0700

    Update tidepool-uploader from 2.35.0 to 2.36.1 (#106002)

--- Casks/tidepool-uploader.rb
+++ Casks/tidepool-uploader.rb
@@ -1,6 +1,6 @@
 cask "tidepool-uploader" do
-  version "2.35.0"
-  sha256 "377978d2308ac1296b980a9ca0e196c4f2a882d93b120eaa811415466fc7e5ca"
+  version "2.36.1"
+  sha256 "591ed4a559681c2e42f97426399b3a322a2c7ba9b0da0a446e0b087cc8493e39"
 
   url "https://github.com/tidepool-org/chrome-uploader/releases/download/v#{version}/tidepool-uploader-#{version}.dmg/",
       verified: "github.com/tidepool-org/chrome-uploader/"
