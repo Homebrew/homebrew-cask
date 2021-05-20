98250cbd5870ae76e92c2c421829516163a575ec
John C. Vernaleo <jcv@netpurgatory.com>
2021-05-20 11:06:34 -0400

    Address review comments.
    Rename to safe-multisig to better match standards

---
+++ Casks/safe-multisig.rb
@@ -0,0 +1,17 @@
+cask "safe-multisig" do
+  version "3.2.1"
+  sha256 "f100a2be2d9e3dea01f7a612418e5296dca2dd26488dd6cae1dd3b7407900345"
+
+  url "https://github.com/gnosis/safe-react/releases/download/v#{version}/Safe-Multisig-#{version}.dmg",
+      verified: "github.com/gnosis/safe-react/"
+  name "Gnosis Safe Multisig"
+  desc "Gnosis Safe Ethereum multisig wallet"
+  homepage "https://gnosis-safe.io/"
+
+  auto_updates false
+
+  app "Safe Multisig.app"
+
+  zap trash:
+             "~/Library/Application Support/Safe Multisig"
+end
