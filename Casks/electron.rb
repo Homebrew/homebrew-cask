e5e85fb79d60f2c603bc115cc332c594ea894064
ymadd <Kb_yamato@protonmail.com>
2021-05-20 16:30:11 +0900

    Update electron from 12.0.8 to 12.0.9 (#106050)

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,12 +1,12 @@
 cask "electron" do
-  version "12.0.8"
+  version "12.0.9"
 
   if Hardware::CPU.intel?
-    sha256 "fe849df4518f63e9315610e419f8db4ce2ea797756ab32800ffc27df0cf82875"
+    sha256 "b3f1e378f58e7c36b54451c5a3485adc370277827974e1eb0790b6965737c872"
     url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip",
         verified: "github.com/electron/electron/"
   else
-    sha256 "13aa43ff86b6702552ecb9df78e8c5f07837f7b127e1aed96e3d7f13d4a3b368"
+    sha256 "cb8aa8153005ea0d801182eb714d56af0217345b1152d867317291670731daeb"
     url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-arm64.zip",
         verified: "github.com/electron/electron/"
   end
