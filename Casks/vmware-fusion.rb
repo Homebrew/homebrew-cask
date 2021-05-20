dcaac369d3bfe7097fe8291a15d6e72abb22bce3
Vítor Galvão <info@vitorgalvao.com>
2021-05-20 21:35:18 +0100

    vmware-fusion.rb: update desc

--- Casks/vmware-fusion.rb
+++ Casks/vmware-fusion.rb
@@ -4,7 +4,7 @@ cask "vmware-fusion" do
 
   url "https://download3.vmware.com/software/fusion/file/VMware-Fusion-#{version}.dmg"
   name "VMware Fusion"
-  desc "App to run other operating systems without rebooting"
+  desc "Create, manage, and run virtual machinces"
   homepage "https://www.vmware.com/products/fusion.html"
 
   livecheck do
