3dc25490e5ca4a285aa44d128ca06ef68b6d7a37
ymadd <Kb_yamato@protonmail.com>
2021-05-20 22:11:08 +0900

    Add Instatus out.app

---
+++ Casks/instatus-out.rb
@@ -0,0 +1,23 @@
+cask "instatus-out" do
+  version "1.0.8"
+  sha256 "14ad0e0e13dd3b5747defccfd2483069297fedcd0d74727246d031bd23ab5649"
+
+  url "https://github.com/instatushq/out/releases/download/v#{version}/Instatus.Out-#{version}-mac.dmg",
+      verified: "github.com/instatushq/out"
+  name "instatus-out"
+  desc "Monitor services in your menu bar"
+  homepage "https://instatus.com/out"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "Instatus Out.app"
+
+  zap trash: [
+    "~/Library/Application Support/instatus-out",
+    "~/Library/Preferences/com.instatus.out.plist",
+    "~/Library/Saved Application State/com.instatus.out.savedState",
+  ]
+end
