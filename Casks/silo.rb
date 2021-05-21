3bf4ff7e5080fea09ac1414e947fd354e1f8380d
Michael Cho <cho-m@tuta.io>
2021-05-20 20:49:36 -0700

    Update silo from 2021.0.0 to 2021.1.0 (#106087)
    * Update silo from 2021.0.0 to 2021.1.0
    
    * Update silo.rb

--- Casks/silo.rb
+++ Casks/silo.rb
@@ -1,16 +1,18 @@
 cask "silo" do
-  version "2021.0.0"
-  sha256 "81490457a8b090421291ada3c32b64fd27dc593fd205059afffa853f1f265390"
+  version "2021.1.0"
+  sha256 "6312811094401449deb0bd2862fb69765872f67c0c6bbfabbab4d93e63560544"
 
   url "https://nevercenter.com/silo/download/filearchive/Install_Silo_#{version.major}_#{version.minor}#{version.patch}_mac.dmg"
   name "Silo"
+  desc "3D polygonal modeler and UV mapper"
   homepage "https://nevercenter.com/silo/"
 
   livecheck do
     url "https://nevercenter.com/silo/download/"
-    strategy :page_match
-    regex(/Silo\s*(\d+(?:\.\d+)*)\s*/)
+    regex(/Silo\s*(\d+(?:\.\d+)*)\s*/i)
   end
+
+  depends_on macos: ">= :mojave"
 
   app "Silo.app"
 end
