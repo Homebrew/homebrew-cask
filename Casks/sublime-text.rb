81a094b9a8e06afc54c499f4a6d121b7e6c92b60
Braam Beresford <braam@braam.email>
2021-05-21 13:21:42 -0700

    sublime-text: removed livecheck as update link no longer works

--- Casks/sublime-text.rb
+++ Casks/sublime-text.rb
@@ -7,13 +7,6 @@ cask "sublime-text" do
   desc "Text editor for code, markup and prose"
   homepage "https://www.sublimetext.com/"
 
-  livecheck do
-    url "https://www.sublimetext.com/updates/#{version.major}/stable/appcast_osx.xml"
-    strategy :sparkle do |item|
-      match = item.version.match(/(\d)(\d+)/)
-      "#{match[1]}.#{match[2]}"
-    end
-  end
 
   auto_updates true
   conflicts_with cask: "sublime-text-dev"
