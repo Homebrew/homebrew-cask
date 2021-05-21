b2c97d68e6ed5478d5ddfd4aa3dbb9c0f2459b28
Michael Cho <cho-m@tuta.io>
2021-05-20 20:36:36 -0700

    Update silo.rb

--- Casks/silo.rb
+++ Casks/silo.rb
@@ -4,13 +4,15 @@ cask "silo" do
 
   url "https://nevercenter.com/silo/download/filearchive/Install_Silo_#{version.major}_#{version.minor}#{version.patch}_mac.dmg"
   name "Silo"
+  desc "3D polygonal modeler and UV mapper"
   homepage "https://nevercenter.com/silo/"
 
   livecheck do
     url "https://nevercenter.com/silo/download/"
-    strategy :page_match
-    regex(/Silo\s*(\d+(?:\.\d+)*)\s*/)
+    regex(/Silo\s*(\d+(?:\.\d+)*)\s*/i)
   end
+
+  depends_on macos: ">= :mojave"
 
   app "Silo.app"
 end
