975d56da01b0db1c74e35e3589876e48761796ce
Miccal Matthews <miccal.matthews@gmail.com>
2021-05-21 11:09:10 +0800

    Update jidusm.rb

--- Casks/jidusm.rb
+++ Casks/jidusm.rb
@@ -4,22 +4,21 @@ cask "jidusm" do
 
   url "https://jidu.wrste.com/jiduocr/%E6%9E%81%E5%BA%A6%E6%89%AB%E6%8F%8F-#{version}.dmg"
   name "jidusm"
-  desc "Powerful tool supports scanning, text recognition and formula recognition"
+  desc "Tool that supports scanning, text recognition and formula recognition"
   homepage "https://jidusm.wrste.com/"
 
   livecheck do
     url :homepage
     strategy :page_match
-    regex(/href=.*?%E6%9E%81%E5%BA%A6%E6%89%AB%E6%8F%8F-(.*?).dmg/i)
+    regex(/href=.*?%E6%9E%81%E5%BA%A6%E6%89%AB%E6%8F%8F-(\d+(?:\.\d+)*).dmg/i)
   end
 
   app "极度扫描.app"
 
   zap trash: [
     "~/Library/Application Support/jidusm",
-    "~/Library/Saved Application State/com.wrste.jidusm.savedState",
-    "~/Library/Preferences/com.wrste.jidusm.plist",
     "~/Library/Logs/极度扫描",
-    "~/Library/Application Support/CrashReporter/极度扫描_E5B919C3-BFD6-5FEC-88F5-36F9F7B3B6DE.plist",
+    "~/Library/Preferences/com.wrste.jidusm.plist",
+    "~/Library/Saved Application State/com.wrste.jidusm.savedState",
   ]
 end
