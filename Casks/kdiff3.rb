d58abcd5f78f25b4fef7125ff8dcbdabdafb5bc0
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-05-22 18:48:15 +0900

    Update kdiff3 from 1.9.1 to 1.9.2 (#106116)

--- Casks/kdiff3.rb
+++ Casks/kdiff3.rb
@@ -1,7 +1,7 @@
 cask "kdiff3" do
   # NOTE: "3" is not a version number, but an intrinsic part of the product name
-  version "1.9.1"
-  sha256 "49172f0e5834d94d998c877f2ea05cb17dcd82876300cca9ed6623e38aa5e720"
+  version "1.9.2"
+  sha256 "0097bff0f98e659493fa66a4c630588767b0dc8c56f9152d73bde875ca1c3380"
 
   url "https://download.kde.org/stable/kdiff3/kdiff3-#{version}-macos-64.dmg"
   name "KDiff3"
