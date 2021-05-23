5170b0952764e19bfcfc15313d001727d35ad9a8
António Meireles <antonio.meireles@reformi.st>
2021-05-23 01:09:33 +0100

    Update signal from 5.2.0 to 5.2.1 (#106130)
    Co-authored-by: Michael Cho <cho-m@tuta.io>

--- Casks/signal.rb
+++ Casks/signal.rb
@@ -1,6 +1,6 @@
 cask "signal" do
-  version "5.2.0"
-  sha256 "9558047612f71d5148b1e95fd43e810bf7464c24dc06053fc354b62ca4657292"
+  version "5.2.1"
+  sha256 "ba3f743efbb71f623a8988789f937819c34cc579194a8fd9dce1fcf9cc0879c5"
 
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{version}.dmg"
   name "Signal"
@@ -8,8 +8,8 @@ cask "signal" do
   homepage "https://signal.org/"
 
   livecheck do
-    url "https://github.com/signalapp/Signal-Desktop"
-    strategy :github_latest
+    url "https://updates.signal.org/desktop/latest-mac.yml"
+    strategy :electron_builder
   end
 
   auto_updates true
