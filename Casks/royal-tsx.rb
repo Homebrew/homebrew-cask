9b324d406f22685781644dd19c31918aa7664dfc
qeeqez <qeeqez@users.noreply.github.com>
2021-05-23 12:10:44 +0300

    Update royal-tsx site links. Add description

--- Casks/royal-tsx.rb
+++ Casks/royal-tsx.rb
@@ -2,12 +2,13 @@ cask "royal-tsx" do
   version "4.3.6.1000"
   sha256 "9fd00742126a19acca5b93751c57236093639c7c77955af6375592dce4e44360"
 
-  url "https://royaltsx-v4.royalapplications.com/updates/royaltsx_#{version}.dmg"
+  url "https://royaltsx-v4.royalapps.com/updates/royaltsx_#{version}.dmg"
   name "Royal TSX"
-  homepage "https://www.royalapplications.com/ts/mac/features"
+  desc "Powerful Connection Management"
+  homepage "https://www.royalapps.com/ts/mac/features"
 
   livecheck do
-    url "https://royaltsx-v#{version.major}.royalapplications.com/updates_stable.php"
+    url "https://royaltsx-v#{version.major}.royalapps.com/updates_stable.php"
     strategy :sparkle
   end
 
