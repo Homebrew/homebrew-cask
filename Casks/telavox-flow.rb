5146e65488bda8ded93bd6e675e42e2ec226dbf5
Michael Cho <cho-m@tuta.io>
2021-05-23 02:22:15 -0700

    Update telavox-flow from 1.120.0 to 1.120.1

--- Casks/telavox-flow.rb
+++ Casks/telavox-flow.rb
@@ -1,6 +1,6 @@
 cask "telavox-flow" do
-  version "1.120.0"
-  sha256 "a12056a681ede03db346d711209b1b1ccf6051ce5ea0a9e8ee995620f6ff4bae"
+  version "1.120.1"
+  sha256 "bf33db04d745b24ff50ac377d9c1db2466a7f7eb12e8d10bac3833710e877730"
 
   url "https://s3.eu-west-2.amazonaws.com/flow-desktop/Telavox-#{version}.dmg",
       verified: "s3.eu-west-2.amazonaws.com/flow-desktop/"
