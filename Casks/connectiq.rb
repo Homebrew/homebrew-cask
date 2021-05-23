7a90e7b5ff454075e51674a52d896c6aff59b0c7
Michael Cho <cho-m@tuta.io>
2021-05-22 22:33:05 -0700

    Update connectiq from 4.0.1-2021-04-21-1bb58d97e to 4.0.2-2021-05-13-9b3b7f19f

--- Casks/connectiq.rb
+++ Casks/connectiq.rb
@@ -1,6 +1,6 @@
 cask "connectiq" do
-  version "4.0.1-2021-04-21-1bb58d97e"
-  sha256 "f718e543aff6ca39c080285f29fb24d47342e942c06cab149af248cae91a640f"
+  version "4.0.2-2021-05-13-9b3b7f19f"
+  sha256 "a7d9690555d3afaeed5a7a14c275fc858911b7ba8f931f3b1a67274381ce3e5c"
 
   url "https://developer.garmin.com/downloads/connect-iq/sdks/connectiq-sdk-mac-#{version}.dmg"
   name "Garmin Connect IQ SDK"
