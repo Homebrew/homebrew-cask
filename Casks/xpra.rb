6063b57f5ce324cf376bb1f49baf076b14bf0059
Michael Cho <cho-m@tuta.io>
2021-05-23 12:48:52 -0700

    Update xpra from 4.1.3 to 4.2,0

--- Casks/xpra.rb
+++ Casks/xpra.rb
@@ -1,15 +1,18 @@
 cask "xpra" do
-  version "4.1.3"
-  sha256 :no_check
+  version "4.2,0"
+  sha256 "af44d9943b1cfd08f40d2cc4ab4d9a14d231a0a46e0df95fb24ee79a1df1498d"
 
-  url "https://www.xpra.org/dists/osx/x86_64/Xpra-x86_64.pkg"
+  url "https://www.xpra.org/dists/osx/x86_64/Xpra-Python3-x86_64-#{version.before_comma}-r#{version.after_comma}.pkg"
   name "Xpra"
   desc "Screen and application forwarding system"
   homepage "https://www.xpra.org/"
 
   livecheck do
-    url "https://github.com/Xpra-org/xpra/releases"
-    strategy :git
+    url "https://www.xpra.org/dists/osx/x86_64/Xpra-x86_64.pkg.sha1"
+    strategy :page_match do |page|
+      match = page.match(/x86_64[._-]v?(\d+(?:\.\d+)+)[._-]r(\d+)\.pkg/i)
+      "#{match[1]},#{match[2]}"
+    end
   end
 
   pkg "Xpra-x86_64.pkg"
