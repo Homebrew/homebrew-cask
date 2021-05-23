afe40921d679e5917b19326b9c9350d2d4c7e69d
Michael Cho <cho-m@tuta.io>
2021-05-23 10:14:13 -0700

    Update jamovi from 1.8.1.0 to 1.8.2.0 (#106158)

--- Casks/jamovi.rb
+++ Casks/jamovi.rb
@@ -1,16 +1,15 @@
 cask "jamovi" do
-  version "1.8.1.0"
-  sha256 "b58fc42881aa6aa077a99e0a8bd1b2ee720acb3af6e17bf594079f56c92f4877"
+  version "1.8.2.0"
+  sha256 "ccf1d84cc06c7026f913e54606512daaed300b2b925759c5302728c4289bc360"
 
   url "https://www.jamovi.org/downloads/jamovi-#{version}-macos.dmg"
   name "jamovi"
-  desc "Free and open statistical software"
+  desc "Statistical software"
   homepage "https://www.jamovi.org/"
 
   livecheck do
     url "https://www.jamovi.org/download.html"
-    strategy :page_match
-    regex(%r{href=.*?/jamovi-(\d+(?:\.\d+)*)-macos\.dmg}i)
+    regex(%r{href=.*?/jamovi[._-]v?(\d+(?:\.\d+)+)[._-]macos\.dmg}i)
   end
 
   auto_updates true
