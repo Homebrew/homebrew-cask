f70bf1423e8600c716c8b5194a4bce1c760abc5b
Hannes Braun <hannesbraun@mail.de>
2021-05-24 01:54:58 +0200

    Update reaper from 6.28.0,6.28 to 6.29.0,6.29 (#106025)
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,24 +1,27 @@
 cask "reaper" do
-  version "6.28.0,6.28"
+  version "6.29"
 
   if MacOS.version <= :mojave
-    sha256 "c393ec7f1e7af2eefc3e074640c04194d744b54df78dcef29ed2ff054512906c"
+    sha256 "9cafc2dcda0ea11daea92f6558aea6eb3aa58917b91a50e81e2a623e3fda0f5f"
 
-    url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.after_comma.no_dots}_x86_64.dmg"
+    url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64.dmg"
   else
-    sha256 "fc04740094bdb4015525f616bb6cc949e29e6304c95f2f089f9f3c1965bcfe35"
+    sha256 "c29fca6eb8a9365a3b0a665eea546a428e4aec8bb4ac21e6d0f6f784c5530ca1"
 
-    url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.after_comma.no_dots}_x86_64_catalina.dmg"
+    url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64_catalina.dmg"
   end
 
-  appcast "https://www.cockos.com/reaper/latestversion/?p=osx_64",
-          must_contain: version.after_comma
   name "REAPER"
   desc "Digital audio production application"
   homepage "https://www.reaper.fm/"
 
-  app "REAPER64.app"
-  app "ReaMote64.app"
+  livecheck do
+    url "https://www.cockos.com/reaper/latestversion/?p=osx_64"
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  app "REAPER.app"
+  app "ReaMote.app"
 
   zap trash: [
     "~/Library/Application Support/REAPER",
