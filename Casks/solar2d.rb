a2980c0b8043ff43c96d82ba6820f110b8420954
Michael Cho <cho-m@tuta.io>
2021-05-23 01:19:14 -0700

    Update solar2d.rb

--- Casks/solar2d.rb
+++ Casks/solar2d.rb
@@ -5,13 +5,13 @@ cask "solar2d" do
   url "https://github.com/coronalabs/corona/releases/download/#{version.minor}/Solar2D-macOS-#{version}.dmg",
       verified: "github.com/coronalabs/corona/"
   name "Solar2D"
-  desc "Lua based game engine"
+  desc "Lua-based game engine"
   homepage "https://solar2d.com/"
 
   livecheck do
     url :url
     strategy :github_latest
-    regex(%r{href=.*?/Solar2D[._-]macOS[._-](\d+(?:\.\d+)+)\.dmg}i)
+    regex(%r{href=.*?/Solar2D[._-]macOS[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
   end
 
   suite "Corona-#{version.minor}"
