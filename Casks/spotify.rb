39ff8d24b46c18fc8eb6acc6f0a8487ec571db01
soberich <25544967+soberich@users.noreply.github.com>
2021-05-23 21:09:13 +0200

    Update spotify from 1.1.58.820.g2ae50076,1.1.58.820.g2ae50076-37 to 1.1.59.714.g7e040462,1.1.59.714.g7e040462-44 (#106189)
    Co-authored-by: Michael Cho <cho-m@tuta.io>

--- Casks/spotify.rb
+++ Casks/spotify.rb
@@ -1,5 +1,5 @@
 cask "spotify" do
-  version "1.1.58.820.g2ae50076,1.1.58.820.g2ae50076-37"
+  version "1.1.59.714.g7e040462,1.1.59.714.g7e040462-44"
   sha256 :no_check
 
   url "https://download.scdn.co/Spotify.dmg",
@@ -9,7 +9,8 @@ cask "spotify" do
   homepage "https://www.spotify.com/"
 
   livecheck do
-    skip "No version information available"
+    url :url
+    strategy :extract_plist
   end
 
   auto_updates true
