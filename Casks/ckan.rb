6cd4390fe8f1c0eac09aa1ce74a4672bb9ebcbd6
Michael Cho <cho-m@tuta.io>
2021-05-22 22:23:24 -0700

    Update ckan.rb

--- Casks/ckan.rb
+++ Casks/ckan.rb
@@ -3,12 +3,13 @@ cask "ckan" do
   sha256 "136ec251c2846af36578d7ff745912ae255f546597f340642d16533a44bb48c0"
 
   url "https://github.com/KSP-CKAN/CKAN/releases/download/v#{version}/CKAN.dmg"
-  name "Comprehensive Kerbal Archive Network client"
+  name "Comprehensive Kerbal Archive Network"
+  desc "Mod management solution for Kerbal Space Program"
   homepage "https://github.com/KSP-CKAN/CKAN"
 
   livecheck do
     url :url
-    strategy :github_latest
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
   depends_on cask: "mono-mdk"
