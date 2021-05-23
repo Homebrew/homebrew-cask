cfdcf06d1ee11fe4024ca3b5ec0ecdafdebb5425
Michael Cho <cho-m@tuta.io>
2021-05-23 09:42:42 -0700

    Update slippi-dolphin from 2.3.0 to 2.3.1 (#106171)

--- Casks/slippi-dolphin.rb
+++ Casks/slippi-dolphin.rb
@@ -1,6 +1,6 @@
 cask "slippi-dolphin" do
-  version "2.3.0"
-  sha256 "b76b8edbcc8ab12d42d73918c3f34608e4fd5c2bc7c1e73cd7cb606a9c0cfd8c"
+  version "2.3.1"
+  sha256 "507d1da543c969fe5b729265096d2d1a9fbb5b7b2611ac7d35d9b50908d4ed3d"
 
   url "https://github.com/project-slippi/Ishiiruka/releases/download/v#{version}/FM-Slippi-#{version}-Mac.dmg",
       verified: "github.com/project-slippi/Ishiiruka/"
