dadf6d6c8d23af565411e95994f74837155bdea5
adithyasunil26 <adithyasunil26@gmail.com>
2021-05-24 15:08:27 +0400

    Update epichrome from 2.4.7 to 2.4.8

--- Casks/epichrome.rb
+++ Casks/epichrome.rb
@@ -1,6 +1,6 @@
 cask "epichrome" do
-  version "2.4.7"
-  sha256 "1672a38ab7d33132f1e1b70e9427376f52b20e4359d434e95bf3859ce60cfea1"
+  version "2.4.8"
+  sha256 "6df7c1e458848acbf54fce1b5ff78a03d13f6b8afc1ad65ecc166ff260a4bc12"
 
   url "https://github.com/dmarmor/epichrome/releases/download/v#{version}/epichrome-#{version}.pkg"
   name "Epichrome"
