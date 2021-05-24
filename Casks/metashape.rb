b75e2505aa2fb66cec59d2a456a61a5768d4b5b6
Michael Cho <cho-m@tuta.io>
2021-05-23 20:33:52 -0700

    Update metashape from 1.7.2 to 1.7.3

--- Casks/metashape.rb
+++ Casks/metashape.rb
@@ -1,6 +1,6 @@
 cask "metashape" do
-  version "1.7.2"
-  sha256 "245a2268d1e4ca072685982acaaf22f06b878c0408d2799e4b8045812229d729"
+  version "1.7.3"
+  sha256 "4d8c36a003835c2c6c7e3f259ae5f4ec3b58214f96ea7b0f6d0816dc9bd848db"
 
   url "http://download.agisoft.com/metashape_#{version.dots_to_underscores}.dmg"
   name "Agisoft Metashape Standard Edition"
