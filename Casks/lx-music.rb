1b6da7771c55e36aeef4c7e165078a646b58cf31
Adithya Sunil <adithyasunil26@gmail.com>
2021-05-24 16:18:56 +0400

    Update lx-music from 1.9.0 to 1.10.0 (#106232)

--- Casks/lx-music.rb
+++ Casks/lx-music.rb
@@ -1,6 +1,6 @@
 cask "lx-music" do
-  version "1.9.0"
-  sha256 "d3a362b12afe2660cfcb1579fb97330a546f127e94fb01bd47e205936d5d76c0"
+  version "1.10.0"
+  sha256 "78836b46f224622fe66c22b04f41521999ad60463e94b0ab00d6efd8edcd1fc9"
 
   url "https://github.com/lyswhut/lx-music-desktop/releases/download/v#{version}/lx-music-desktop-#{version}.dmg"
   name "LX Music Assistant Desktop Edition"
