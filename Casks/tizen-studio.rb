3e1397c1025c6f7c24a3ce19b9b3c6744eccf6fd
dalisoft <dalisoft@mail.ru>
2021-05-24 10:39:38 +0500

    fix: error fixes and some improvements

--- Casks/tizen-studio.rb
+++ Casks/tizen-studio.rb
@@ -1,20 +1,27 @@
 cask "tizen-studio" do
   version "4.1"
-  sha256 :no_check
+  sha256 "c538328806b14cec190591213465d813a180bcbd"
 
-  url "https://download.tizen.org/sdk/Installer/tizen-studio_4.1/web-ide_Tizen_Studio_4.1_macos-64.dmg"
+  url "https://download.tizen.org/sdk/Installer/tizen-studio_#{version.major_minor}/web-ide_Tizen_Studio_#{version.major_minor}_macos-64.dmg"
 
   name "Tizen Studio"
-  desc "Tizen Studio is the official IDE for developing web and native applications for Tizen"
+  desc "The Official IDE for developing web and native applications for Tizen"
   homepage "https://developer.tizen.org/"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url "https://download.tizen.org/sdk/Installer"
+    regex(%r{href=.*?/tizen-studio[._-]?(\d+(?:\.\d+)+)/web-ide_Tizen_Studio[._-]?(\d+(?:\.\d+)+)_macos-64\.dmg}i)
   end
 
   auto_updates true
   depends_on macos: ">= :catalina"
 
   app "Tizen Studio.app"
+  
+  zap trash: [
+    "~/tizen-studio",
+    "~/Applications/tizen-studio",
+    "~/Library/Preferences/org.tizen.sdk.ide.plist",
+    "~/Library/Saved Application State/org.tizen.sdk.ide.savedState"
+  ]
 end
