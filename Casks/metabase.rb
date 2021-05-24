8a7b178908ff33389dd9159864aca9109c56ff17
Michael Cho <cho-m@tuta.io>
2021-05-23 20:31:53 -0700

    Update metabase from 0.39.1 to 0.39.2

--- Casks/metabase.rb
+++ Casks/metabase.rb
@@ -1,6 +1,6 @@
 cask "metabase" do
-  version "0.39.1"
-  sha256 "20f7203d36bbca24d652a5ee275038ef576bc6c99aa3dcebd9b2357c66d2549b"
+  version "0.39.2"
+  sha256 "e13fc25c3ae1ad9db8c1cefc8775b41bee2c1d331ea94e2761cc928e8d172933"
 
   url "https://s3.amazonaws.com/downloads.metabase.com/v#{version}/Metabase.zip",
       verified: "s3.amazonaws.com/downloads.metabase.com/"
