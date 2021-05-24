a3664bd353bebb87a208b6b31b043b3a97c9edf4
adithyasunil26 <adithyasunil26@gmail.com>
2021-05-24 16:35:59 +0400

    Update rowmote-helper from 4.2.3 to 4.2.4

--- Casks/rowmote-helper.rb
+++ Casks/rowmote-helper.rb
@@ -1,6 +1,6 @@
 cask "rowmote-helper" do
-  version "4.2.3"
-  sha256 "08a6803c66e55032252a2f946316cdff99f81f168a6c2e12f724ad37ffef1373"
+  version "4.2.4"
+  sha256 "552cdb8acd46758caa1944b46e5d3f2b53f4cbf1da70b195fc35a18804370cfd"
 
   url "https://regularrateandrhythm.com/rowmote-pro/rh/rowmote-helper-#{version}.zip"
   name "Rowmote Helper"
