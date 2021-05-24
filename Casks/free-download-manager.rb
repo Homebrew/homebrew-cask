a4afcefc6d92b3813b995a133e79c80c0fc670c7
Michael Cho <cho-m@tuta.io>
2021-05-24 09:27:49 -0700

    Update free-download-manager.rb

--- Casks/free-download-manager.rb
+++ Casks/free-download-manager.rb
@@ -3,12 +3,17 @@ cask "free-download-manager" do
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://dn3.freedownloadmanager.org/#{version.major}/latest/fdm.dmg"
-  appcast "https://www.freedownloadmanager.org/download-fdm-for-mac.htm"
   name "Free Download Manager"
   desc "Download accelerator and organizer"
   homepage "https://www.freedownloadmanager.org/"
 
+  livecheck do
+    url "https://www.freedownloadmanager.org/download-fdm-for-mac.htm"
+    regex(/>\s*FDM\s*v?(\d+(?:\.\d+)+)/i)
+  end
+
   auto_updates true
+  depends_on macos: ">= :sierra"
 
   app "Free Download Manager.app"
 
