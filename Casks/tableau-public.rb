6504707af721ade6030422ca5a5482fe09cb0a6f
adithyasunil26 <adithyasunil26@gmail.com>
2021-05-24 17:03:17 +0400

    Update tableau-public from 2021.1.1 to 2021.1.2

--- Casks/tableau-public.rb
+++ Casks/tableau-public.rb
@@ -1,6 +1,6 @@
 cask "tableau-public" do
-  version "2021.1.1"
-  sha256 "a72d0d828a1f4cd38a77649440550e14f1228fb15852539eed6578f912a640e8"
+  version "2021.1.2"
+  sha256 "c02477c154dbdf1d419531aeb47dbba911833e127837794e22d6d7fae8552a19"
 
   url "https://downloads.tableau.com/public/TableauPublic-#{version.dots_to_hyphens}.dmg"
   name "Tableau Public"
