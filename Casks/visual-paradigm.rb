e89b0064d0f0b8f61ea6b3b7b62993b7161bb43d
Michael Cho <cho-m@tuta.io>
2021-05-23 19:08:14 -0700

    Update visual-paradigm from 16.3,20210420 to 16.3,20210517

--- Casks/visual-paradigm.rb
+++ Casks/visual-paradigm.rb
@@ -1,6 +1,6 @@
 cask "visual-paradigm" do
-  version "16.3,20210420"
-  sha256 "dd94d4a64843844bbfedd4162f0860bc2e3df7c6594fa955ec019a08dc96dbc5"
+  version "16.3,20210517"
+  sha256 "1cb1028cb2f72fae14c93dd8783210a54c0e5ac161ab9aac6059b5c27be17101"
 
   url "https://www.visual-paradigm.com/downloads/vp#{version.before_comma}/#{version.after_comma}/Visual_Paradigm_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
   name "Visual Paradigm"
