64aea19cecbaae39586d04830c4a9cd4e2168103
Serge Bibauw <sbibauw@gmail.com>
2021-05-24 00:47:09 -0500

    Apply suggestions from code review
    Co-authored-by: Michael Cho <cho-m@tuta.io>

--- Casks/hightop.rb
+++ Casks/hightop.rb
@@ -3,14 +3,16 @@ cask "hightop" do
   sha256 "822582bedee41da9f7ab7e45a838b30d641b4459970a917c72e501edcfee20f3"
 
   url "https://hightop.app/downloads/HighTop#{version}.dmg"
-  name "hightop"
-  desc "Quick file access via the menu bar"
+  name "HighTop"
+  desc "File access via the menu bar"
   homepage "https://hightop.app/"
 
   livecheck do
     url "https://hightop.app/downloads/updates.xml"
     strategy :sparkle, &:short_version
   end
+
+  depends_on macos: ">= :sierra"
 
   app "HighTop.app"
 
