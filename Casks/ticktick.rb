78276b8ebf825ad21b5d49041fe960dc2dbcef3a
Kunzhe Huang <hkunzhe24@gmail.com>
2021-05-25 18:05:54 +0800

    Add zap stanza to TickTick (#106288)

--- Casks/ticktick.rb
+++ Casks/ticktick.rb
@@ -20,4 +20,21 @@ cask "ticktick" do
   depends_on macos: ">= :sierra"
 
   app "TickTick.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.TickTick.task.mac.MainWidget",
+    "~/Library/Application Scripts/com.TickTick.task.mac.TickTick-Safari-Extension",
+    "~/Library/Application Scripts/com.TickTick.task.mac.TickTick-Today-Widget",
+    "~/Library/Application Scripts/com.TickTick.task.mac.TickTick-WidgetConfiguration-Extension",
+    "~/Library/Application Support/com.TickTick.task.mac",
+    "~/Library/Caches/TickTick",
+    "~/Library/Caches/com.TickTick.task.mac",
+    "~/Library/Containers/com.TickTick.task.mac.MainWidget",
+    "~/Library/Containers/com.TickTick.task.mac.TickTick-Safari-Extension",
+    "~/Library/Containers/com.TickTick.task.mac.TickTick-Today-Widget",
+    "~/Library/Containers/com.TickTick.task.mac.TickTick-WidgetConfiguration-Extension",
+    "~/Library/Group Containers/75TY9UT8AY.com.TickTick.task.mac",
+    "~/Library/Preferences/com.TickTick.task.mac.plist",
+    "~/Library/Saved Application State/com.TickTick.task.mac.savedState",
+  ]
 end
