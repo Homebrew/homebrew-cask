cf4584ca02d884f4d88dbdde0b58a8ecc0bed755
Chaowen Chang <kojima.chang@asiayo.com>
2021-05-25 16:52:30 +0800

    Update flipper from 0.91.1 to 0.91.2 (#106287)

--- Casks/flipper.rb
+++ Casks/flipper.rb
@@ -1,6 +1,6 @@
 cask "flipper" do
-  version "0.91.1"
-  sha256 "2b0ce653e8c67f1d2df74699a568999b6fbf257ffe7c532eb710da27a4e3ce5d"
+  version "0.91.2"
+  sha256 "c4f7a4f8f6fca8e8895be337c4fc88f96d195eac985876f45cd76f3e548f16c4"
 
   url "https://github.com/facebook/flipper/releases/download/v#{version}/Flipper-mac.dmg",
       verified: "github.com/facebook/flipper/"
