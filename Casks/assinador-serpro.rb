5746bbdc5a726cb3bb36176e08bed873a01154f6
Miccal Matthews <miccal.matthews@gmail.com>
2021-05-25 11:55:08 +0800

    Update assinador-serpro.rb

--- Casks/assinador-serpro.rb
+++ Casks/assinador-serpro.rb
@@ -1,8 +1,8 @@
 cask "assinador-serpro" do
-  version "2-7-1"
+  version "2.7.1"
   sha256 "11dc80202d25008d83e82a76e1cc34e6fa1678af9ff530c61819e7c2a7b526b5"
 
-  url "https://www.serpro.gov.br/links-fixos-superiores/assinador-digital/assinador-serpro/arquivos/assinador-serpro-#{version}-instalador.dmg"
+  url "https://www.serpro.gov.br/links-fixos-superiores/assinador-digital/assinador-serpro/arquivos/assinador-serpro-#{version.dots_to_hyphens}-instalador.dmg"
   name "assinador-serpro"
   desc "Validate and sign documents using digital certificates"
   homepage "https://www.serpro.gov.br/links-fixos-superiores/assinador-digital/assinador-serpro"
@@ -11,8 +11,5 @@ cask "assinador-serpro" do
 
   app "Assinador Serpro.app"
 
-  zap trash: [
-    "~/Library/Application Support/CrashReporter/JavaAppLauncher_CA7F3306-C6D5-5B86-B49A-3E44E5E124DB.plist",
-    "~/Library/Preferences/org.demoiselle.signer.serpro.desktop.Main.plist",
-  ]
+  zap trash: "~/Library/Preferences/org.demoiselle.signer.serpro.desktop.Main.plist"
 end
