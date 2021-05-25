ee9386ec4280bfd5d67fa74d37afd57cf78bd0e5
Miccal Matthews <miccal.matthews@gmail.com>
2021-05-25 11:42:34 +0800

    Update tizen-studio.rb

--- Casks/tizen-studio.rb
+++ Casks/tizen-studio.rb
@@ -3,7 +3,6 @@ cask "tizen-studio" do
   sha256 "c538328806b14cec190591213465d813a180bcbd"
 
   url "https://download.tizen.org/sdk/Installer/tizen-studio_#{version.major_minor}/web-ide_Tizen_Studio_#{version.major_minor}_macos-64.dmg"
-
   name "Tizen Studio"
   desc "Official IDE for developing web and native applications for Tizen"
   homepage "https://developer.tizen.org/"
