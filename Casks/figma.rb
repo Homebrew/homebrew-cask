8bea76a701a3f9e4fdf02b1c73ac8524f5efd85c
王子 <meetzwang@gmail.com>
2021-05-25 11:46:41 +0800

    Add arm version for figma (#106277)

--- Casks/figma.rb
+++ Casks/figma.rb
@@ -1,8 +1,16 @@
 cask "figma" do
   version "98.11.0"
-  sha256 "e9899d8dafdb752414c8fe0e070d6f81b32bc713f220618b4fc90b380e345a72"
 
-  url "https://desktop.figma.com/mac/Figma-#{version}.zip"
+  if Hardware::CPU.intel?
+    sha256 "e9899d8dafdb752414c8fe0e070d6f81b32bc713f220618b4fc90b380e345a72"
+
+    url "https://desktop.figma.com/mac/Figma-#{version}.zip"
+  else
+    sha256 "85bf1fc63a799b46c0d8e2e041033b54537cca5ee5dcb27af3b6016f080ca7c0"
+
+    url "https://desktop.figma.com/mac-arm/Figma-#{version}.zip"
+  end
+
   name "Figma"
   desc "Collaborative team software"
   homepage "https://www.figma.com/"
