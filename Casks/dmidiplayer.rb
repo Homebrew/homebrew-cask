a7acf0830f50da111e00941bb7cadf10b9efe498
Pedro López-Cabanillas <pedrolcl@users.noreply.github.com>
2021-05-25 05:41:38 +0200

    Add dmidiplayer.app v1.3.0 (#106126)
    * Add dmidiplayer.app v1.3.0
    
    * zap stanza added
    
    * style fixed

---
+++ Casks/dmidiplayer.rb
@@ -0,0 +1,22 @@
+cask "dmidiplayer" do
+  version "1.3.0"
+  sha256 "61d1cc6842ba61b704cf45c98220c38fb5ccd506fb72fe10c434a476edd3f53d"
+
+  url "https://downloads.sourceforge.net/dmidiplayer/#{version}/dmidiplayer-#{version}-mac-x64.dmg",
+      verified: "downloads.sourceforge.net/dmidiplayer/"
+  name "dmidiplayer"
+  desc "Multiplatform MIDI File Player"
+  homepage "https://dmidiplayer.sourceforge.io/"
+
+  depends_on formula: "fluid-synth"
+  depends_on macos: ">= :sierra"
+  depends_on arch: :x86_64
+
+  app "dmidiplayer.app"
+
+  zap trash: [
+    "~/Library/Preferences/net.sourceforge.dmidiplayer.plist",
+    "~/Library/Preferences/net.sourceforge.drumstick.Drumstick MIDI File Multiplatform Player.plist",
+    "~/Library/Saved Application State/net.sourceforge.dmidiplayer.savedState",
+  ]
+end
