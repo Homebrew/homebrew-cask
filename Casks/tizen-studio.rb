1a8f5b582a906b27c115c58a560a3f9e0c0941a7
Miccal Matthews <miccal.matthews@gmail.com>
2021-05-25 12:27:16 +0800

    Update tizen-studio.rb

--- Casks/tizen-studio.rb
+++ Casks/tizen-studio.rb
@@ -2,14 +2,14 @@ cask "tizen-studio" do
   version "4.1"
   sha256 "e49893861fd9b0871e9decf9fd5ad963f6398957c32c5b604a18cce96c8a539f"
 
-  url "https://download.tizen.org/sdk/Installer/tizen-studio_#{version.major_minor}/web-ide_Tizen_Studio_#{version.major_minor}_macos-64.dmg"
+  url "https://download.tizen.org/sdk/Installer/tizen-studio_#{version}/web-ide_Tizen_Studio_#{version}_macos-64.dmg"
   name "Tizen Studio"
   desc "Official IDE for developing web and native applications for Tizen"
   homepage "https://developer.tizen.org/"
 
   livecheck do
-    url "https://download.tizen.org/sdk/Installer"
-    regex(%r{href=.*?/tizen-studio[._-]?(\d+(?:\.\d+)+)/web-ide_Tizen_Studio[._-]?(\d+(?:\.\d+)+)_macos-64\.dmg}i)
+    url "https://download.tizen.org/sdk/Installer/"
+    regex(/tizen-studio_(\d+(?:\.\d+)*)/}i)
   end
 
   auto_updates true
