1578dde37fc99dc631de27bf782de86cbe8cc698
Scott Foster <scott@imfoster.com>
2021-05-25 20:31:20 -0500

    Godot 3.3.1 -> 3.3.2

--- Casks/godot.rb
+++ Casks/godot.rb
@@ -1,5 +1,5 @@
 cask "godot" do
-  version "3.3.1"
+  version "3.3.2"
   sha256 "205734a8d15f506a383d610f057491bdc51dc65cb5a3a57b94801e66855da944"
 
   url "https://downloads.tuxfamily.org/godotengine/#{version}/Godot_v#{version}-stable_osx.universal.zip",
