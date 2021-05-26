18fe00c733d05e10adb9c29b585b2fd34c87d003
disrupted <hi@salomonpopp.me>
2021-05-26 09:45:08 +0200

    Update plexamp from 3.4.5 to 3.4.6

--- Casks/plexamp.rb
+++ Casks/plexamp.rb
@@ -1,6 +1,6 @@
 cask "plexamp" do
-  version "3.4.5"
-  sha256 "d4ced724e2847d5d3b256cd3b696b7f4403178268add148906b19ad35f6bb4ac"
+  version "3.4.6"
+  sha256 "cdccfc3d389b2310431e48ac9ed2f4449d95de85e9b3590e1319234adb213954"
 
   url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}.dmg",
       verified: "plexamp.plex.tv/"
