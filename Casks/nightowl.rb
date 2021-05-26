199ef233082c6f35f97e8d40101245826b7fac78
Loïc PÉRON <loic.peron@gmail.com>
2021-05-26 10:21:24 +0200

    Add livecheck to nightowl

--- Casks/nightowl.rb
+++ Casks/nightowl.rb
@@ -3,10 +3,17 @@ cask "nightowl" do
   sha256 :no_check
 
   url "https://nightowl.kramser.xyz/files/NightOwl.dmg"
-  appcast "https://nightowl.kramser.xyz/"
   name "NightOwl"
   desc "Utility to toggle dark mode"
   homepage "https://nightowl.kramser.xyz/"
+
+  livecheck do
+    url "https://nightowl.kramser.xyz/api/public_update"
+    regex(/"version": *"(.*)",\n *"build": *"(.*)"/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| match[0] + "," + match[1] }
+    end
+  end
 
   depends_on macos: ">= :mojave"
 
