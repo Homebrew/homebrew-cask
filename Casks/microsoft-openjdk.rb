3512b244a7813843e1423d58cf260e6158157952
George Adams <georgeadams1995@gmail.com>
2021-05-26 10:08:51 +0100

    Add Microsoft Build of OpenJDK 16.0.1.9.1

---
+++ Casks/microsoft-openjdk.rb
@@ -0,0 +1,14 @@
+cask "microsoft-openjdk" do
+  version "16.0.1.9.1"
+  sha256 "4c7acade7ee71673ad79b0e9dedfe791bd0ae60a03ad5807bda39057f568d725"
+
+  url "https://aka.ms/download-jdk/microsoft-jdk-#{version}-macOS-x64.pkg",
+      verified: "https://aka.ms/download-jdk/"
+  name "Microsoft Build of OpenJDK"
+  desc "OpenJDK distribution from Microsoft"
+  homepage "https://microsoft.com/openjdk"
+
+  pkg "microsoft-jdk-#{version}-macOS-x64.pkg"
+
+  uninstall pkgutil: "com.microsoft.#{version.major}.jdk"
+end
