c8af84aa94d515dbf61ff7f1f06dcf8602c5b390
Michael Cho <cho-m@tuta.io>
2021-05-26 18:53:16 -0700

    Update appium from 1.20.2 to 1.21.0 (#106338)

--- Casks/appium.rb
+++ Casks/appium.rb
@@ -1,10 +1,11 @@
 cask "appium" do
-  version "1.20.2"
-  sha256 "6f7c402e056530d7c43439d10fddd35393a76c90521dbf8471dc468fe9847f40"
+  version "1.21.0"
+  sha256 "bdcb55be5c8193466f1bf6665e8f821711423e87abd7c836618a8fdded57eeea"
 
   url "https://github.com/appium/appium-desktop/releases/download/v#{version}/Appium-mac-#{version}.dmg",
       verified: "github.com/appium/appium-desktop/"
   name "Appium Desktop"
+  desc "Graphical frontend to Appium automation server"
   homepage "https://appium.io/"
 
   livecheck do
