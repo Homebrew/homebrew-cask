cce77a0946cc885da933a48c694c8d1a1aab1cdf
Michael Cho <cho-m@tuta.io>
2021-05-27 01:34:02 -0700

    Update pingid.rb

--- Casks/pingid.rb
+++ Casks/pingid.rb
@@ -5,7 +5,13 @@ cask "pingid" do
   url "https://pingone-downloads.s3.amazonaws.com/pingid/mac-client/PingID.pkg",
       verified: "pingone-downloads.s3.amazonaws.com/pingid/mac-client/"
   name "PingID"
+  desc "Cloud-based, multi-factor authentication"
   homepage "https://www.pingidentity.com/"
+
+  livecheck do
+    url "https://prod-ping-sfc.zoominsoftware.io/api/bundle/pingid/toc?language=enus"
+    regex(/"title"\s*:\s*"Desktop\s+app\s+v?(\d+(?:\.\d+)+)/i)
+  end
 
   pkg "PingID.pkg"
 
