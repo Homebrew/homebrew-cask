7b5ba2608adeb462df4fca485de0e5ca3f197e88
dalisoft <dalisoft@mail.ru>
2021-05-27 14:11:15 +0500

    fix: Browserstack Local download url fix

--- Casks/browserstacklocal.rb
+++ Casks/browserstacklocal.rb
@@ -1,8 +1,8 @@
 cask "browserstacklocal" do
-  version "2.3,1"
+  version "3.2.1"
   sha256 :no_check
 
-  url "https://www.browserstack.com/BrowserStackLocal.dmg"
+  url "https://www.browserstack.com/local-testing/downloads/native-app/BrowserStackLocal.dmg"
   name "BrowserStack Local"
   homepage "https://www.browserstack.com/"
 
