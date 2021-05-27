34ee8290762dcafcfa06b10125a5167a9a70d69d
MoonFruit <dkmoonfruit@gmail.com>
2021-05-27 09:53:21 +0800

    Update tencent-lemon from 4.8.8 to 4.8.9

--- Casks/tencent-lemon.rb
+++ Casks/tencent-lemon.rb
@@ -1,8 +1,8 @@
 cask "tencent-lemon" do
-  version "4.8.8"
+  version "4.8.9"
   sha256 "02bc9c96dcb02820c8ff9a5a188c138e9917958075e500b617b4fa1f70925d55"
 
-  url "https://pm.myapp.com/invc/xfspeed/qqpcmgr/module_update/Lemon_#{version}.dmg",
+  url "https://pm.myapp.com/invc/xfspeed/qqpcmgr/module_update/Lemonv#{version}.dmg",
       verified: "pm.myapp.com/invc/xfspeed/qqpcmgr/"
   name "Tencent Lemon Cleaner"
   desc "Cleanup and system status tool"
@@ -11,7 +11,7 @@ cask "tencent-lemon" do
   livecheck do
     url "https://lemon.guanjia.qq.com/latest/package"
     strategy :page_match
-    regex(/Lemon_v?(\d+(?:\.\d+)*)\.dmg/i)
+    regex(/Lemonv?(\d+(?:\.\d+)*)\.dmg/i)
   end
 
   app "Tencent Lemon.app"
