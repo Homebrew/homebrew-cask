f6e450be529669b952d5683d5a3f569f24f5d67f
Michael Cho <cho-m@tuta.io>
2021-05-27 10:47:53 -0700

    Update sound-control.rb

--- Casks/sound-control.rb
+++ Casks/sound-control.rb
@@ -1,13 +1,21 @@
 cask "sound-control" do
-  version "2.6.2,4297"
+  version "2.6.2,5156"
   sha256 "041812f2361a0e0afb614f77137b9f7f083827feb7c971d0ed6e52ce0b4ca5a6"
 
   url "https://staticz.com/download/#{version.after_comma}/"
-  appcast "http://staticz.net/updates/soundcontrol.rss"
   name "Sound Control"
+  desc "Per-app audio controls"
   homepage "https://staticz.com/soundcontrol/"
 
+  livecheck do
+    url "http://staticz.net/updates/soundcontrol.rss"
+    strategy :sparkle do |item|
+      "#{item.version},#{item.url[%r{/download/(\d+)/}i, 1]}"
+    end
+  end
+
   auto_updates true
+  depends_on macos: ">= :mojave"
 
   app "Sound Control.app"
 
