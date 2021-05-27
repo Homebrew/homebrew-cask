a86f9e57ee82dfffce96079ee82d84ac2afc76b6
Adrian Petrescu <adrian@apetre.sc>
2021-05-27 14:08:38 -0400

    Unexpectedly 1.0b

---
+++ Casks/unexpectedly.rb
@@ -0,0 +1,11 @@
+cask "unexpectedly" do
+  version "1.0b"
+  sha256 :no_check
+
+  url "http://s.sudre.free.fr/Software/files/Unexpectedly.dmg"
+  name "Unexpectedly"
+  desc "Browse and visualize the reports from crashes"
+  homepage "http://s.sudre.free.fr/Software/Unexpectedly/about.html"
+
+  app "Unexpectedly.app"
+end
