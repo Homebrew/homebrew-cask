6af083ce0ad3c001a9386b706a08b11a4ce53c0d
Chris Simpkins <git.simpkins@gmail.com>
2021-05-27 11:45:33 -0400

    Update fontgoggles from 1.1.21 to 1.2.0 (#106384)

--- Casks/fontgoggles.rb
+++ Casks/fontgoggles.rb
@@ -1,6 +1,6 @@
 cask "fontgoggles" do
-  version "1.1.21"
-  sha256 "4d19603335dd8ae3d8739e38082ec40de6578162b19e6f6819d289f15ad46917"
+  version "1.2.0"
+  sha256 "076df6d16cfb25fd5b1481fb51359facf3d88b3e73578b18a7d5bdfd930ea3ee"
 
   url "https://github.com/justvanrossum/fontgoggles/releases/download/v#{version}/FontGoggles.dmg",
       verified: "github.com/justvanrossum/fontgoggles/"
