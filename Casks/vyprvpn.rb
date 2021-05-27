929b21c1e8f778cc533375b3c02a85465d371435
Michael Cho <cho-m@tuta.io>
2021-05-26 19:40:35 -0700

    Update vyprvpn from 4.2.0.9114 to 4.2.1.9152 (#106367)
    * Update vyprvpn from 4.2.0.9114 to 4.2.1.9152
    
    * Update vyprvpn.rb

--- Casks/vyprvpn.rb
+++ Casks/vyprvpn.rb
@@ -1,9 +1,10 @@
 cask "vyprvpn" do
-  version "4.2.0.9114"
-  sha256 "adbdc9cec1c9cc80b137a32f0043a5b22a092bcbde1f30c647de68abc69c55f1"
+  version "4.2.1.9152"
+  sha256 "8c0ee94cc3a66d8e024f76850b213d4532caecb97c51fcf37b913ec949850377"
 
   url "https://www.goldenfrog.com/downloads/vyprvpn/desktop/mac/production/#{version}/VyprVPN_v#{version}.dmg"
   name "VyprVPN"
+  desc "VPN client"
   homepage "https://www.goldenfrog.com/vyprvpn"
 
   livecheck do
