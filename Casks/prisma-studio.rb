415e05b8e651ad8417b4a36697b885359dfbead5
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-05-28 20:33:29 +0530

    Add prisma studio

---
+++ Casks/prisma-studio.rb
@@ -0,0 +1,24 @@
+cask "prisma-studio" do
+  version "0.394.0"
+  sha256 "9702305ea1df6424da5073dd0e8b253c5aea605db5395c056bec4fa0bfa6cf57"
+
+  url "https://github.com/prisma/studio/releases/download/v#{version}/Prisma-Studio.dmg",
+      verified: "github.com/prisma/studio/"
+  name "Prisma Studio"
+  desc "Visual database editor for Prisma projects"
+  homepage "https://www.prisma.io/studio"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "Prisma Studio.app"
+
+  zap trash: [
+    "~/Library/Application Support/Prisma Studio",
+    "~/Library/Logs/Prisma Studio",
+    "~/Library/Preferences/io.prisma.studio.plist",
+    "~/Library/Saved Application State/io.prisma.studio.savedState",
+  ]
+end
