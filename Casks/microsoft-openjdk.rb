10fddbd9713db05303eeeaaedbbf41bfd8290b6f
Michael Cho <cho-m@tuta.io>
2021-05-28 16:52:19 -0700

    Update microsoft-openjdk.rb

--- Casks/microsoft-openjdk.rb
+++ Casks/microsoft-openjdk.rb
@@ -3,15 +3,14 @@ cask "microsoft-openjdk" do
   sha256 "4c7acade7ee71673ad79b0e9dedfe791bd0ae60a03ad5807bda39057f568d725"
 
   url "https://aka.ms/download-jdk/microsoft-jdk-#{version}-macOS-x64.pkg",
-      verified: "https://aka.ms/download-jdk/"
+      verified: "aka.ms/download-jdk/"
   name "Microsoft Build of OpenJDK"
   desc "OpenJDK distribution from Microsoft"
   homepage "https://microsoft.com/openjdk"
 
   livecheck do
     url "https://docs.microsoft.com/java/openjdk/download"
-    strategy :page_match
-    regex(%r{href=.*?/microsoft-jdk-(\d+(?:\.\d+)*)-macOS-x64.pkg}i)
+    regex(%r{href=.*?/microsoft[._-]jdk[._-]v?(\d+(?:\.\d+)+)[._-]macOS[._-]x64\.pkg}i)
   end
 
   pkg "microsoft-jdk-#{version}-macOS-x64.pkg"
