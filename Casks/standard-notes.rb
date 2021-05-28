2a5725668be093d59e1d0ee1824b4d1cb21b0135
Stefano Canducci <stefano@canducci.org>
2021-05-28 10:22:11 +0200

    Update standard-notes from 3.6.14 to 3.7.1 (#106417)

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -1,6 +1,6 @@
 cask "standard-notes" do
-  version "3.6.14"
-  sha256 "4d4941c5aee20c44d0ce182665db8574284c4d4c5912e3b556808bd0ddaf147c"
+  version "3.7.1"
+  sha256 "c0dbf8e01d90ccc5d9dc17983a571fe9d0d7bbc850f70b5ff17d3d7bafd80a08"
 
   url "https://github.com/standardnotes/desktop/releases/download/v#{version}/Standard-Notes-#{version}-mac.zip",
       verified: "github.com/standardnotes/desktop/"
