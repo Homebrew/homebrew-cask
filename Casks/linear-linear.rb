70edfa327bb16a1c31d8738c7962402ab88a6560
Michael Cho <cho-m@tuta.io>
2021-05-28 11:29:39 -0700

    Update linear-linear.rb

--- Casks/linear-linear.rb
+++ Casks/linear-linear.rb
@@ -1,11 +1,12 @@
 cask "linear-linear" do
   version "1.3.4"
+  sha256 :no_check
+
   if Hardware::CPU.intel?
     url "https://desktop.linear.app/mac/dmg/x64"
   else
     url "https://desktop.linear.app/mac/dmg/arm64"
   end
-  sha256 :no_check
 
   name "Linear"
   desc "App to manage software development and track bugs"
