d34e593c1da5198e13403e6244042614b002eb34
Michael Cho <cho-m@tuta.io>
2021-05-29 12:56:12 -0700

    Update opera-gx from 75.0.3969.259 to 75.0.3969.279 (#106525)

--- Casks/opera-gx.rb
+++ Casks/opera-gx.rb
@@ -1,6 +1,6 @@
 cask "opera-gx" do
-  version "75.0.3969.259"
-  sha256 "bf16889d985bda03f328377871502e4f75d9f154bdf1e342fede63b784dbb794"
+  version "75.0.3969.279"
+  sha256 "337e8d5f8e6089e73914968961003495377478d6b0ce2470978419a8dce0da10"
 
   url "https://get.geo.opera.com/pub/opera_gx/#{version}/mac/Opera_GX_#{version}_Setup.dmg"
   name "Opera GX"
@@ -9,8 +9,7 @@ cask "opera-gx" do
 
   livecheck do
     url "https://ftp.opera.com/pub/opera_gx/"
-    strategy :page_match
-    regex(/href=['"]?(\d+(?:\.\d+)*)['"]?/i)
+    regex(%r{href=["']?v?(\d+(?:\.\d+)+)/?["' >]}i)
   end
 
   auto_updates true
