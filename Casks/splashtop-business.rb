5e32bdf0b401f8acd8592fec8b717389586b4040
Michael Cho <cho-m@tuta.io>
2021-05-28 20:59:36 -0700

    Update splashtop-business from 3.4.4.0 to 3.4.6.0

--- Casks/splashtop-business.rb
+++ Casks/splashtop-business.rb
@@ -1,6 +1,6 @@
 cask "splashtop-business" do
-  version "3.4.4.0"
-  sha256 "f5e3ac5fe792bd9c3073a70e40cc62b9f4aa766a74cdf0efc71e20d777ce8a0c"
+  version "3.4.6.0"
+  sha256 "b0bb3e8d8fb62c7f2f23e16442c48a16acd7676aa8a129cb1c3e03b3240468e7"
 
   url "https://d17kmd0va0f0mp.cloudfront.net/macclient/STB/Splashtop_Business_Mac_INSTALLER_v#{version}.dmg",
       verified: "d17kmd0va0f0mp.cloudfront.net/macclient/STB/"
