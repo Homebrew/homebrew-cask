ffb663d85820d32fb5e966cfbd14fb2c546eafd7
Michael Cho <cho-m@tuta.io>
2021-05-28 23:39:38 -0700

    Update glyphs from 3.0.3,3084 to 3.0.3,3086 (#106452)

--- Casks/glyphs.rb
+++ Casks/glyphs.rb
@@ -1,6 +1,6 @@
 cask "glyphs" do
-  version "3.0.3,3084"
-  sha256 "4c85fb113273928d54334ff4cb5e0a440d2d8e9685697ab902758cbbd92dad3b"
+  version "3.0.3,3086"
+  sha256 "6733994f7c6c1838807ad361145d62390962b88b3948ac0a63a419db3c03b0e7"
 
   url "https://updates.glyphsapp.com/Glyphs#{version.before_comma}-#{version.after_comma}.zip"
   name "Glyphs"
