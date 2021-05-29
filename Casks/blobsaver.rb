31f847a9dcaae1b5ba674239bb05e204edac0aaa
airsquared <36649395+airsquared@users.noreply.github.com>
2021-05-28 23:44:47 -0700

    Fix blobsaver v3.0.1

--- Casks/blobsaver.rb
+++ Casks/blobsaver.rb
@@ -1,6 +1,6 @@
 cask "blobsaver" do
   version "3.0.1"
-  sha256 "b39bd9d811e54907ba3fd03be12660d3dd8e9312b795c400eae03dc2f754672d"
+  sha256 "397dea9211abc25e35383ac67ea9ef87c7394047df1771490151ffbb3c31af8b"
 
   url "https://github.com/airsquared/blobsaver/releases/download/v#{version}/blobsaver-#{version}.dmg"
   name "blobsaver"
