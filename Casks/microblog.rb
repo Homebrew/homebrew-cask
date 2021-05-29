fd1d83c093210ce076bd80f5af4dcc9e1fc9e329
Michael Cho <cho-m@tuta.io>
2021-05-28 20:03:07 -0700

    Update microblog from 2.0.3,81 to 2.0.4,83

--- Casks/microblog.rb
+++ Casks/microblog.rb
@@ -1,6 +1,6 @@
 cask "microblog" do
-  version "2.0.3,81"
-  sha256 "387881f84207206de95d214b4d49e9d19378cc5332b87c045723ba78a8d8116a"
+  version "2.0.4,83"
+  sha256 "d21ac080eae06c828040552a433390a136db441e6587e6f45ca202b71bc0034c"
 
   url "https://s3.amazonaws.com/micro.blog/mac/Micro.blog_#{version.before_comma}.zip",
       verified: "s3.amazonaws.com/micro.blog/"
