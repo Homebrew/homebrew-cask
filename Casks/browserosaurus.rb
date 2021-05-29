8b8ff9682d43823baf21a562b52bb6132ee4b300
Zhenfu Shi <szf1234@me.com>
2021-05-29 16:08:12 +0800

    Update browserosaurus from 14.2.0 to 14.2.1 (#106515)

--- Casks/browserosaurus.rb
+++ Casks/browserosaurus.rb
@@ -1,12 +1,12 @@
 cask "browserosaurus" do
-  version "14.2.0"
+  version "14.2.1"
 
   if Hardware::CPU.intel?
-    sha256 "e869212101c9b05ae264fa0ab2e648214e5f5c9d19485269e5f5b46985d3986b"
+    sha256 "cbe34f8947a305e2ad6bf5e6c6e90abdc3f3e2be6b3dd19d0cd436d471847ab1"
     url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-darwin-x64-#{version}.zip",
         verified: "github.com/will-stone/browserosaurus/"
   else
-    sha256 "115e331fe9fcd647e190f84e9de903ab9be115873a19b9e506e619100e4eccdd"
+    sha256 "0c18b1a504351208be09a596dfc2ce963a3e947ef356081e3d2c11afbac3653f"
     url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-darwin-arm64-#{version}.zip",
         verified: "github.com/will-stone/browserosaurus/"
   end
