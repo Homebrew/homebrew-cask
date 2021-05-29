0897ddb974230df7bca25349cf62e97dd25bf52d
GalacticLion7 <47502020+GalacticLion7@users.noreply.github.com>
2021-05-29 18:31:35 +0400

    Update ExpressVPN mirror
    expressvpn.xyz is blocked in some countries, so use the new one, xpress-vpn.com.

--- Casks/expressvpn.rb
+++ Casks/expressvpn.rb
@@ -2,13 +2,13 @@ cask "expressvpn" do
   version "10.2.4.6"
   sha256 "8ab87794c098c752defc43b5aef21c2f420f418011ab90396d2b9678a77ced2d"
 
-  url "https://download.expressvpn.xyz/clients/mac/expressvpn_mac_#{version}_release.pkg"
+  url "https://download.xpress-vpn.com/clients/mac/expressvpn_mac_#{version}_release.pkg"
   name "ExpressVPN"
   desc "VPN client for secure internet access and private browsing"
-  homepage "https://www.expressvpn.xyz/vpn-software/vpn-mac/"
+  homepage "https://www.xpress-vpn.com/vpn-software/vpn-mac/"
 
   livecheck do
-    url "https://www.expressvpn.xyz/clients/latest/mac"
+    url "https://www.xpress-vpn.com/clients/latest/mac"
     strategy :header_match
   end
 
