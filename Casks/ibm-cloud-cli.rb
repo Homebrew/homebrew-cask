8ef6813e336af7b5954651ddc1184e502515a289
Michael Cho <cho-m@tuta.io>
2021-05-29 01:10:41 -0700

    Update ibm-cloud-cli from 1.5.1 to 1.6.0 (#106457)

--- Casks/ibm-cloud-cli.rb
+++ Casks/ibm-cloud-cli.rb
@@ -1,6 +1,6 @@
 cask "ibm-cloud-cli" do
-  version "1.5.1"
-  sha256 "dc0d847fd4a7f352b5c7900d80429eb749b47e40a5de3823a1127ebee8f99a69"
+  version "1.6.0"
+  sha256 "d202f4ff3fc49bb1acbc8c0098b7ae9129bfeeff716cf2f658e2cfa655cf9599"
 
   url "https://download.clis.cloud.ibm.com/ibm-cloud-cli/#{version}/IBM_Cloud_CLI_#{version}.pkg"
   name "IBM Cloud CLI"
@@ -9,7 +9,6 @@ cask "ibm-cloud-cli" do
 
   livecheck do
     url "https://github.com/IBM-Cloud/ibm-cloud-cli-release"
-    strategy :git
   end
 
   depends_on cask: "docker"
