df19c54191bd02ff7c3ef061da310ac1613ba334
Michael Cho <cho-m@tuta.io>
2021-05-28 19:07:28 -0700

    Update gzdoom from 4.5.0 to 4.6.0

--- Casks/gzdoom.rb
+++ Casks/gzdoom.rb
@@ -1,8 +1,8 @@
 cask "gzdoom" do
-  version "4.5.0"
-  sha256 "b7a8056e63fa0a14497e8d1767bbbae2da1ebe9f47e4f87df613106f9ebe3522"
+  version "4.6.0"
+  sha256 "2e6789b95129e7984d1629301121dc55cb3c94f2575219553c2675504dfc501b"
 
-  url "https://github.com/coelckers/gzdoom/releases/download/g#{version}/gzdoom-#{version.dots_to_hyphens}-macOS.dmg",
+  url "https://github.com/coelckers/gzdoom/releases/download/g#{version}/gzdoom-#{version.dots_to_hyphens}-macOS.zip",
       verified: "github.com/coelckers/gzdoom/"
   name "gzdoom"
   desc "Adds an OpenGL renderer to the ZDoom source port"
@@ -10,8 +10,7 @@ cask "gzdoom" do
 
   livecheck do
     url :url
-    strategy :git
-    regex(/^g(\d+(?:\.\d+)*)$/)
+    regex(/^g?(\d+(?:\.\d+)+)$/)
   end
 
   app "GZDoom.app"
