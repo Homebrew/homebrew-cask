ac0c66fbdb060825d3779f298c7213c462b4cf10
Michael Cho <cho-m@tuta.io>
2021-05-29 13:14:07 -0700

    Update chemdoodle.rb

--- Casks/chemdoodle.rb
+++ Casks/chemdoodle.rb
@@ -4,16 +4,16 @@ cask "chemdoodle" do
 
   url "https://www.ichemlabs.com/downloads/ChemDoodle-macos-#{version}.dmg"
   name "ChemDoodle"
+  name "ChemDoodle 2D"
   desc "2D chemical drawing, publishing and informatics"
   homepage "https://www.ichemlabs.com/"
 
   livecheck do
     url "https://www.ichemlabs.com/download#chemdoodle/osx-installation-instructions/"
-    strategy :page_match
-    regex(%r{href=.*?/ChemDoodle-macos-(\d+(?:\.\d+)*)\.dmg}i)
+    regex(%r{href=.*?/ChemDoodle[._-]macos[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
   end
 
-  depends_on macos: ">= :yosemite"
+  depends_on macos: ">= :el_capitan"
 
   suite "ChemDoodle"
 end
