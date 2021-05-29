69bf40fe878eb5fe4f2dee0253deaffb4713607a
Michael Cho <cho-m@tuta.io>
2021-05-29 11:57:14 -0700

    Update netxms-console from 3.8.366 to 3.8.382 (#106523)

--- Casks/netxms-console.rb
+++ Casks/netxms-console.rb
@@ -1,6 +1,6 @@
 cask "netxms-console" do
-  version "3.8.366"
-  sha256 "4e42b572f38b02732b44b7384b4271c27a3365aa4b8b3b2e3be35ef4c6ee3d2a"
+  version "3.8.382"
+  sha256 "b0aed45b3353eb252b5545b5fa35410526ffc9b91a6a676883f33ed4f8fa7dbe"
 
   url "https://netxms.org/download/releases/#{version.major_minor}/nxmc-#{version}.dmg"
   name "NetXMS Management Console"
@@ -9,8 +9,7 @@ cask "netxms-console" do
 
   livecheck do
     url "https://netxms.org/download"
-    strategy :page_match
-    regex(%r{href=.*?/nxmc-(\d+(?:\.\d+)*)\.dmg}i)
+    regex(%r{href=.*?/nxmc[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
   end
 
   app "NetXMS Console (#{version}).app"
