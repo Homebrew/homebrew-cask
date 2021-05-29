8d2ec5a066b1a48331ed8acc818576320864f581
Michael Cho <cho-m@tuta.io>
2021-05-29 14:08:07 -0700

    Update ksnip from 1.8.2 to 1.9.0

--- Casks/ksnip.rb
+++ Casks/ksnip.rb
@@ -1,6 +1,6 @@
 cask "ksnip" do
-  version "1.8.2"
-  sha256 "635d454aa13e697885dff46b65ec73a0b05c91bc6d3a5091c74d9bad9187ec09"
+  version "1.9.0"
+  sha256 "e1afd29e4b1a7e51ab3b84eca386e27ccf4decaf269751d1031aab25e4d506eb"
 
   url "https://github.com/ksnip/ksnip/releases/download/v#{version}/ksnip-#{version}.dmg"
   name "ksnip"
