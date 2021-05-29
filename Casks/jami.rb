1ca2ac790e0a2fc07c8435952b55781b11aa111b
Michael Cho <cho-m@tuta.io>
2021-05-29 16:40:40 -0700

    Update jami from 202105171555 to 202105272138 (#106551)

--- Casks/jami.rb
+++ Casks/jami.rb
@@ -1,6 +1,6 @@
 cask "jami" do
-  version "202105171555"
-  sha256 "5583a83aad953dbb04932bbff8369273332f586a52dede16ab39cd303ab85a80"
+  version "202105272138"
+  sha256 "cdb1d19abfed3213b7aa282bd489535752240041ff3b05dc032fab0f099eb8a7"
 
   url "https://dl.jami.net/mac_osx/jami-#{version}.dmg"
   name "Jami"
@@ -11,10 +11,11 @@ cask "jami" do
   livecheck do
     url "https://dl.jami.net/mac_osx/sparkle-ring.xml"
     strategy :page_match
-    regex(/jami-(\d+(?:\.\d+)*)\.dmg/i)
+    regex(/jami[._-]v?(\d+(?:\.\d+)*)\.dmg/i)
   end
 
   auto_updates true
+  depends_on macos: ">= :high_sierra"
 
   app "Jami.app"
 
