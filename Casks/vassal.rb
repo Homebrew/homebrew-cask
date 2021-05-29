765d8e71859501e00e285174eb1a899c55f25b06
Michael Cho <cho-m@tuta.io>
2021-05-29 14:34:07 -0700

    Update vassal from 3.5.5 to 3.5.6 (#106541)

--- Casks/vassal.rb
+++ Casks/vassal.rb
@@ -1,11 +1,11 @@
 cask "vassal" do
-  version "3.5.5"
-  sha256 "4ea3dac56ac4e8b3249d9bf009fe2c736a350967fa07b248276a2f4ce1002823"
+  version "3.5.6"
+  sha256 "1f89b217090116f56d0417efa368fc31770ec23633a2525cf1a1c2b7fded6907"
 
   url "https://github.com/vassalengine/vassal/releases/download/#{version}/VASSAL-#{version}-macos.dmg",
       verified: "github.com/vassalengine/vassal/"
   name "VASSAL"
-  desc "Open-source boardgame engine"
+  desc "Board game engine"
   homepage "http://www.vassalengine.org/"
 
   app "VASSAL.app"
