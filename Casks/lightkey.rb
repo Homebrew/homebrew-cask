eae61a9bc9ba5f50834cfc6be1da0f7e87f7b492
Michael Cho <cho-m@tuta.io>
2021-05-28 19:33:21 -0700

    Update lightkey from 3.6.1 to 3.6.2

--- Casks/lightkey.rb
+++ Casks/lightkey.rb
@@ -1,6 +1,6 @@
 cask "lightkey" do
-  version "3.6.1"
-  sha256 "16231581074667703ab7b7426169251f771916fdd8b77fd939273a2e1a073a8a"
+  version "3.6.2"
+  sha256 "db2a0baaa4b217d4a57192ba8aa33e022cc4c3257f11a4531c8ea865e94981da"
 
   url "https://lightkeyapp.com/content/06-download/Lightkey-#{version.dots_to_hyphens}/LightkeyInstaller.zip"
   name "Lightkey"
