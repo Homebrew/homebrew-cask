ee3517d8abc6d961057873a978eea2c05550e2be
Michael Cho <cho-m@tuta.io>
2021-05-28 19:37:36 -0700

    Update logseq from 0.1.0 to 0.1.1

--- Casks/logseq.rb
+++ Casks/logseq.rb
@@ -1,11 +1,11 @@
 cask "logseq" do
-  version "0.1.0"
+  version "0.1.1"
 
   if Hardware::CPU.intel?
-    sha256 "a066a8d65308ef075b33c2ebbf806e0ed7087a473c03ad1cbb9da30aae168b6e"
+    sha256 "dcbc2512d0596205ec0ec8121b69e488398c05a4a130a56c7ac5c11a8a9e7896"
     url "https://github.com/logseq/logseq/releases/download/#{version}/logseq-darwin-x64-#{version}.dmg"
   else
-    sha256 "bfe10616fae2256baf572881b9391d1554bf2feadacb3c157cb0f14a5fa79422"
+    sha256 "c4a63bebe8c69664dbcdb0bc620dc942b3a3240b3310aa0f1ea4358edc366ec1"
     url "https://github.com/logseq/logseq/releases/download/#{version}/logseq-darwin-arm64-#{version}.dmg"
   end
 
