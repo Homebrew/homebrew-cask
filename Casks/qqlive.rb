5ad7c8a33df9d3750387ffe552d5f6c14b51de2e
Michael Cho <cho-m@tuta.io>
2021-05-29 10:34:48 -0700

    Update qqlive from 2.20.0.52955 to 2.21.0.52979

--- Casks/qqlive.rb
+++ Casks/qqlive.rb
@@ -1,6 +1,6 @@
 cask "qqlive" do
-  version "2.20.0.52955"
-  sha256 "c3c08893dce4f8d4a89e159d2a8d79f68b695ffb288ef79ef3548ca832af14e2"
+  version "2.21.0.52979"
+  sha256 "e1c615bc21e62b087857161c745f7128705df7f6b1d8f59078aa19aa2abab61e"
 
   url "https://dldir1.qq.com/qqtv/mac/TencentVideo_V#{version}.dmg"
   name "QQLive"
