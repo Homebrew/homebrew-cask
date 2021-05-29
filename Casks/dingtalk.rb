ce73ed825684b164a789879a3f83df00ef5e68f1
Michael Cho <cho-m@tuta.io>
2021-05-29 14:27:04 -0700

    Update dingtalk from 6.0.15.6 to 6.0.15.8 (#106530)

--- Casks/dingtalk.rb
+++ Casks/dingtalk.rb
@@ -1,6 +1,6 @@
 cask "dingtalk" do
-  version "6.0.15.6"
-  sha256 "4bd3f790d103f5d616e88139c3ebefb95d3b31264eb9bc2ab03382acad825310"
+  version "6.0.15.8"
+  sha256 "fe26c48f262ab891e63ab97aacd2cb5abc3f711b854e07754e24e03291b6473b"
 
   url "https://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/DingTalk_v#{version}.dmg"
   name "DingTalk"
@@ -11,7 +11,6 @@ cask "dingtalk" do
   livecheck do
     url "https://www.dingtalk.com/mac/d/"
     strategy :header_match
-    regex(/DingTalk_v(\d+(?:\.\d+)*)\.dmg/i)
   end
 
   auto_updates true
