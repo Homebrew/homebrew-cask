a0690309d689d2bfce477d3a9f86ed98c67e7ffd
tiiiecherle <tiiiecherle@gmail.com>
2021-05-30 17:56:16 +0200

    add livecheck to gas-mask

--- Casks/gas-mask.rb
+++ Casks/gas-mask.rb
@@ -3,10 +3,14 @@ cask "gas-mask" do
   sha256 "9f75d0b11340d70832f87011c3d8ed97b9b18b3a56dec5f860d4040bb7404500"
 
   url "http://gmask.clockwise.ee/files/gas_mask_#{version}.zip"
-  appcast "http://gmask.clockwise.ee/check_update/"
   name "Gas Mask"
   desc "Hosts file editor/manager"
   homepage "http://clockwise.ee/"
+  
+  livecheck do
+    url "http://gmask.clockwise.ee/check_update/"
+    strategy :sparkle
+  end
 
   auto_updates true
 
