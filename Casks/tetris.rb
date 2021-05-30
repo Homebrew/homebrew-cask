d9ae18bfd5a7d7f0e3ace891311c27d26e1d1291
Vui-Chee <vuicheesiew@gmail.com>
2021-05-30 11:41:01 +0800

    Update name

--- Casks/tetris.rb
+++ Casks/tetris.rb
@@ -3,7 +3,7 @@ cask "tetris" do
   sha256 "9854cc35236f67ccbfe8cf12843e9b5ab302810e6783a1cba7849a52e576301f"
 
   url "https://github.com/Vui-Chee/Tetris-Solarized/releases/download/v#{version}/tetris-solarized-darwin-x64.zip"
-  name "tetris"
+  name "Tetris Solarized"
   desc "Desktop game of Tetris"
   homepage "https://github.com/Vui-Chee/Tetris-Solarized"
 
