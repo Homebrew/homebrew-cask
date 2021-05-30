01ce85cb4689a04538aeee0ed468eef27f2a4e8b
Michael Cho <cho-m@tuta.io>
2021-05-30 00:58:07 -0700

    Update chemdoodle from 11.4.0 to 11.5.0 (#106529)
    * Update chemdoodle from 11.4.0 to 11.5.0
    
    * Update chemdoodle.rb

--- Casks/chemdoodle.rb
+++ Casks/chemdoodle.rb
@@ -1,19 +1,19 @@
 cask "chemdoodle" do
-  version "11.4.0"
-  sha256 "153843a8149adfc171ba567052e03a4921e760e2300217501862a9b467d64567"
+  version "11.5.0"
+  sha256 "d65323044ef05028e333e847ea53c5c7e1e484d7e5fec8b67d9a2c47d2c99c98"
 
   url "https://www.ichemlabs.com/downloads/ChemDoodle-macos-#{version}.dmg"
   name "ChemDoodle"
+  name "ChemDoodle 2D"
   desc "2D chemical drawing, publishing and informatics"
   homepage "https://www.ichemlabs.com/"
 
   livecheck do
     url "https://www.ichemlabs.com/download#chemdoodle/osx-installation-instructions/"
-    strategy :page_match
-    regex(%r{href=.*?/ChemDoodle-macos-(\d+(?:\.\d+)*)\.dmg}i)
+    regex(%r{href=.*?/ChemDoodle[._-]macos[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
   end
 
-  depends_on macos: ">= :yosemite"
+  depends_on macos: ">= :el_capitan"
 
   suite "ChemDoodle"
 end
