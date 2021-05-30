fc861f09db33b861c5228b6c55ea41f5b9bfa238
miccal <miccal.matthews@gmail.com>
2021-05-30 16:07:53 +0800

    Update chromium from 886135 to 887589

--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -1,6 +1,6 @@
 cask "chromium" do
-  version "886135"
-  sha256 "2dcfd9f1bbb8bc3abd8a958ace4b85b83c1536083639c6ae4b6a951fbfec7a0d"
+  version "887589"
+  sha256 "f659582c04bdd4edfa188203256557bb1a44a235195cbb69b36d7f61b4abb7d4"
 
   url "https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/#{version}/chrome-mac.zip",
       verified: "commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/"
