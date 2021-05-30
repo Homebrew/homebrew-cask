1d73607dc734a2aa8cc5ec34bcbfb582b8843490
Michael Cho <cho-m@tuta.io>
2021-05-30 01:00:39 -0700

    Update amazon-workspaces from 3.1.7.1881 to 3.1.8.1929 (#106436)

--- Casks/amazon-workspaces.rb
+++ Casks/amazon-workspaces.rb
@@ -1,16 +1,19 @@
 cask "amazon-workspaces" do
-  version "3.1.7.1881"
-  sha256 :no_check
+  version "3.1.8.1929"
+  sha256 "53dde99e45e55f0cfa51048cb7d8b9f2b88583a34d94c349122d929b95c3b570"
 
-  url "https://workspaces-client-updates.s3.amazonaws.com/prod/iad/osx/WorkSpaces.pkg",
-      verified: "workspaces-client-updates.s3.amazonaws.com/"
+  url "https://d2td7dqidlhjx7.cloudfront.net/prod/iad/osx/WorkSpaces_AllProducts_#{version.split(".")[-1]}.zip",
+      verified: "d2td7dqidlhjx7.cloudfront.net/prod/iad/osx/"
   name "Amazon Workspaces"
+  desc "Cloud native persistent desktop virtualization"
   homepage "https://clients.amazonworkspaces.com/"
 
   livecheck do
     url "https://d2td7dqidlhjx7.cloudfront.net/prod/iad/osx/WorkSpacesAppCast_macOS_20171023.xml"
     strategy :sparkle
   end
+
+  depends_on macos: ">= :sierra"
 
   pkg "WorkSpaces.pkg"
 
