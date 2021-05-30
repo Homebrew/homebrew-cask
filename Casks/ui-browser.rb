edff5586127c31f4ba4b063f7de414407ca498be
tiiiecherle <tiiiecherle@gmail.com>
2021-05-30 18:31:22 +0200

    add livecheck ui-browser

--- Casks/ui-browser.rb
+++ Casks/ui-browser.rb
@@ -3,9 +3,14 @@ cask "ui-browser" do
   sha256 "a5695d3046a2230a0e56cbda68829802801d74afc39d4320161fef668b3a0376"
 
   url "https://pfiddlesoft.com/uibrowser/downloads/UIBrowser#{version.no_dots}.dmg"
-  appcast "https://pfiddlesoft.com/uibrowser/index-downloads.html"
   name "UI Browser"
   homepage "https://pfiddlesoft.com/uibrowser/"
+  
+  livecheck do
+    url "https://pfiddlesoft.com/uibrowser/index-downloads.html"
+    strategy :page_match
+    regex(%r{href=.*?UI\ Browser\ (\d+(?:\.\d+)*)}i)
+  end
 
   app "UI Browser.app"
 
