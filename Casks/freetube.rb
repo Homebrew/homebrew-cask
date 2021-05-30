50e190f345e6486d8593639795f7a99fd74c9512
Sebastian Riehm <herrriehm@users.noreply.github.com>
2021-05-30 09:55:57 +0200

    Update freetube from 0.13.0 to 0.13.1 (#106556)

--- Casks/freetube.rb
+++ Casks/freetube.rb
@@ -1,6 +1,6 @@
 cask "freetube" do
-  version "0.13.0"
-  sha256 "8a8673124560251955651b57442d6e5bc9deb59425e6b9069b09a805f3d3226c"
+  version "0.13.1"
+  sha256 "5f75d1a3669b9ee5436add583456a744cd479ba7df5e50f774be130df8d07287"
 
   url "https://github.com/FreeTubeApp/FreeTube/releases/download/v#{version}-beta/freetube-#{version}-mac.dmg"
   name "FreeTube"
