48451537852ddb3dff946601cb2aa3ae4f32857f
Michael Cho <cho-m@tuta.io>
2021-05-30 00:57:46 -0700

    Update netbeans from 12.3 to 12.4 (#106532)
    * Update netbeans from 12.3 to 12.4
    
    * Update netbeans.rb

--- Casks/netbeans.rb
+++ Casks/netbeans.rb
@@ -1,16 +1,15 @@
 cask "netbeans" do
-  version "12.3"
-  sha256 "a34c046619b69e5b13a2618d12336b97bcb404424880fe5d0c4c10ed100da6ba"
+  version "12.4"
+  sha256 "a4fa57eb1ade2a769667e4840ee190d4e9445542760b7cdc2cc89b4a4e4739c2"
 
   url "https://www-eu.apache.org/dist/netbeans/netbeans/#{version}/Apache-NetBeans-#{version}-bin-macosx.dmg"
   name "NetBeans IDE"
   desc "Development environment, tooling platform and application framework"
-  homepage "https://netbeans.apache.org/download/index.html"
+  homepage "https://netbeans.apache.org/"
 
   livecheck do
-    url "https://netbeans.org/downloads/"
-    strategy :page_match
-    regex(/NetBeans\s*(\d+(?:\.\d+)*)/i)
+    url "https://netbeans.apache.org/download/index.html"
+    regex(/NetBeans\s*v?(\d+(?:\.\d+)+)/i)
   end
 
   pkg "Apache NetBeans #{version}.pkg"
