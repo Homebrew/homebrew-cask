fdd792671fdd12cbea2be01f9d6042a83b89e322
Michael Cho <cho-m@tuta.io>
2021-05-30 00:59:01 -0700

    Update lunar from 3.2.3 to 4.0.4 (#106470)
    * Update lunar from 3.2.3 to 4.0.2
    
    * Update lunar from 4.0.2 to 4.0.4

--- Casks/lunar.rb
+++ Casks/lunar.rb
@@ -1,25 +1,27 @@
 cask "lunar" do
-  version "3.2.3"
-  sha256 "88bf404e03364eaf2b33aad449cddc1d80d9e21b5f99e35db2bc2db636b9c48d"
+  version "4.0.4"
+  sha256 "072ee4f7a1294e5239713471abe154c3ad04a17b97a1a4c17d96a54e6dbe58e7"
 
-  url "https://lunar.fyi/download/#{version}"
+  url "https://static.lunar.fyi/releases/Lunar-#{version}.pkg"
   name "Lunar"
   desc "Adaptive brightness for external displays"
   homepage "https://lunar.fyi/"
 
   livecheck do
-    url "https://lunar.fyi/appcast.xml"
+    url "https://static.lunar.fyi/appcast.xml"
     strategy :sparkle
   end
 
   auto_updates true
-  depends_on macos: ">= :yosemite"
+  depends_on macos: ">= :catalina"
 
-  app "Lunar.app"
+  pkg "Lunar-#{version}.pkg"
+
+  uninstall pkgutil: "fyi.lunar.Lunar"
 
   zap trash: [
     "~/Library/Application Support/Lunar",
-    "~/Library/Application Support/site.lunarapp.Lunar",
-    "~/Library/Preferences/site.lunarapp.Lunar.plist",
+    "~/Library/Application Support/fyi.lunar.Lunar",
+    "~/Library/Preferences/fyi.lunar.Lunar.plist",
   ]
 end
