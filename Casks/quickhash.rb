75a530d2195a78947f86fc22a3ec6a509a66bd00
Michael Cho <cho-m@tuta.io>
2021-05-30 19:18:12 -0700

    Update quickhash.rb

--- Casks/quickhash.rb
+++ Casks/quickhash.rb
@@ -1,15 +1,18 @@
 cask "quickhash" do
-  version "3.2.0,2020.08"
-  sha256 "8449c5042dd0061515ca5cc83ee24b487a228c7f9263ea44330d3f008cbc1dec"
+  version "3.3.0,2190"
+  sha256 "fa80950bc63c50cae18f820636f32e592eed9322928cdf5936c3f4bdec866dbb"
 
-  url "https://quickhash-gui.org/wp-content/uploads/#{version.after_comma.major}/#{version.after_comma.minor}/QuickHash-GUI-OSX-v#{version.before_comma}.zip"
+  url "https://www.quickhash-gui.org/download/quickhash-gui-v#{version.before_comma.dots_to_hyphens}-apple-osx/?wpdmdl=#{version.after_comma}"
   name "Quickhash"
+  desc "Data hashing tool"
   homepage "https://www.quickhash-gui.org/"
 
   livecheck do
     url "https://www.quickhash-gui.org/downloads/"
-    strategy :page_match
-    regex(/href=.*?Quickhash-GUI\ v(\d+(?:\.\d+)*)\ Apple OSX/i)
+    strategy :page_match do |page|
+      match = page.match(%r{/quickhash[._-](?:gui[._-])?v?(\d+(?:-\d+)+)[._-]apple[._-]osx/\?wpdmdl=(\d+)}i)
+      "#{match[1].tr("-", ".")},#{match[2]}"
+    end
   end
 
   app "Quickhash-GUI.app"
