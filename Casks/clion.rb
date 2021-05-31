63dfebb15e2800162cd0d15f64501ef1811dc9dd
Michael Cho <cho-m@tuta.io>
2021-05-30 20:59:24 -0700

    clion: add 10.13+ requirement and align livecheck

--- Casks/clion.rb
+++ Casks/clion.rb
@@ -3,9 +3,11 @@ cask "clion" do
 
   if Hardware::CPU.intel?
     sha256 "fe7262bc15279fbfacecf4e1e5e47d31e95da7cb1e5d33e0d4897fd704d850fb"
+
     url "https://download.jetbrains.com/cpp/CLion-#{version.before_comma}.dmg"
   else
     sha256 "2f72a522259646f903c621265fefc075b90039e36ec6063f6582d04a6da7d9d9"
+
     url "https://download.jetbrains.com/cpp/CLion-#{version.before_comma}-aarch64.dmg"
   end
 
@@ -16,13 +18,14 @@ cask "clion" do
   livecheck do
     url "https://data.services.jetbrains.com/products/releases?code=CL&latest=true&type=release"
     strategy :page_match do |page|
-      version = page.match(/"version":"(\d+(?:\.\d+)*)/i)
-      build = page.match(/"build":"(\d+(?:\.\d+)*)/i)
-      "#{version[1]},#{build[1]}"
+      JSON.parse(page)["CL"].map do |release|
+        "#{release["version"]},#{release["build"]}"
+      end
     end
   end
 
   auto_updates true
+  depends_on macos: ">= :high_sierra"
 
   app "CLion.app"
 
