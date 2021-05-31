cff8901e33cc20344fd5df7587584cd2543452fd
cebor <felix@stkn.org>
2021-05-31 09:09:58 +0200

    Update chromedriver from 90.0.4430.24 to 91.0.4472.19

--- Casks/chromedriver.rb
+++ Casks/chromedriver.rb
@@ -1,14 +1,14 @@
 cask "chromedriver" do
-  version "90.0.4430.24"
+  version "91.0.4472.19"
 
   if Hardware::CPU.intel?
     url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_mac64.zip",
         verified: "chromedriver.storage.googleapis.com/"
-    sha256 "1318b3717576154e36476c3253ee8e87a2e09eaf07631ee4948348a8f6a1545f"
+    sha256 "c23d8244cff767fc1c9bc259fb86fece0124d1eba9645397fe7903100e98651f"
   else
     url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_mac64_m1.zip",
         verified: "chromedriver.storage.googleapis.com/"
-    sha256 "c1af4f3eb0efd4050dc502db5928f72b267ac2c6ea1e76614491662a3db0300d"
+    sha256 "712603c85bc5588b98f35cd395cdfb9363e6d6724e7b8e237dc89751763d9d08"
   end
   name "ChromeDriver"
   desc "Automated testing of webapps for Google Chrome"
