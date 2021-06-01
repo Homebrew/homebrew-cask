94b95668bcda6e2e7bb4dee8db62c91133d6a71c
Eloy Aldea <eloy.aldea@gmail.com>
2021-06-01 09:31:17 +0200

    Update sitesucker-pro from 4.0.5 to 4.1

--- Casks/sitesucker-pro.rb
+++ Casks/sitesucker-pro.rb
@@ -3,8 +3,8 @@ cask "sitesucker-pro" do
     version "3.2.7"
     sha256 "dd61a113ad86b580e0faf97b4aa86290e038bb3e098f2d19e67fc9e194ce1a3e"
   else
-    version "4.0.5"
-    sha256 "a5d7d9ca2b4c4fafadfc092b0ff49d1ffc111ef1168a7e221d8a4da661e20a82"
+    version "4.1"
+    sha256 "e8065d802b57142f8552138cf3dc2971cecdc679489905c02ddb39d61fc1cd21"
   end
 
   url "https://ricks-apps.com/osx/sitesucker/archive/#{version.major}.x/#{version.major_minor}.x/#{version}/SiteSucker_Pro_#{version}.dmg"
