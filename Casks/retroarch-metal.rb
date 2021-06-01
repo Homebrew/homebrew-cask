e100c78b2a9f93182a806e006a0a8c7ec5e9a7ba
Ian Robert O'Brien <ian.r.obrien@protonmail.com>
2021-06-01 14:26:13 +0200

    Update retroarch-metal from 1.9.4 to 1.9.4

--- Casks/retroarch-metal.rb
+++ Casks/retroarch-metal.rb
@@ -1,6 +1,6 @@
 cask "retroarch-metal" do
   version "1.9.4"
-  sha256 "a4cb27bea4c0a027bfb0a44694c407a9b02da5bac66b135bcd353cd73d1ceaad"
+  sha256 "9bcded84ee19ab2eb1757e72acb13c51f5cd3bd1414eb4c6354a4e308dce0ec5"
 
   url "https://buildbot.libretro.com/stable/#{version}/apple/osx/universal/RetroArch_Metal.dmg"
   name "RetroArch"
