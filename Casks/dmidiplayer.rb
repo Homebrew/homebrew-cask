a061a6dad5f54ae198d54cf771975103206991b4
Pedro López-Cabanillas <plcl@users.sf.net>
2021-06-01 21:25:37 +0200

    Update dmidiplayer v1.3.1

--- Casks/dmidiplayer.rb
+++ Casks/dmidiplayer.rb
@@ -1,8 +1,8 @@
 cask "dmidiplayer" do
-  version "1.3.0"
-  sha256 "61d1cc6842ba61b704cf45c98220c38fb5ccd506fb72fe10c434a476edd3f53d"
+  version "1.3.1"
+  sha256 "07fd2d2b2b31a5ecfac53aa834a9c5e408bc7a548bcb7095825916b5d35d0779"
 
-  url "https://downloads.sourceforge.net/dmidiplayer/#{version}/dmidiplayer-#{version}-mac-x64.dmg",
+  url "https://downloads.sourceforge.net/dmidiplayer/v#{version}/dmidiplayer-#{version}-mac-x64.dmg",
       verified: "downloads.sourceforge.net/dmidiplayer/"
   name "dmidiplayer"
   desc "Multiplatform MIDI File Player"
@@ -10,7 +10,6 @@ cask "dmidiplayer" do
 
   depends_on formula: "fluid-synth"
   depends_on macos: ">= :sierra"
-  depends_on arch: :x86_64
 
   app "dmidiplayer.app"
 
