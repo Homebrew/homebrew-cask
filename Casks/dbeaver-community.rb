e18fe059cf634e5db15fee8b696bb5ae8c7f427f
Finlay McAfee <finlaymcafee@bulb.co.uk>
2021-06-02 14:46:35 +0100

    Update checksum on debeaver-community

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,6 +1,6 @@
 cask "dbeaver-community" do
   version "21.1.0"
-  sha256 "d8500c45917480c2532db584e0470b9c015abd25a09ce68beafd7a70dba3c3ff"
+  sha256 "dec806a96ea9609e45357ca31685e39495973d22ee1612099b73756cfe740573"
 
   url "https://dbeaver.io/files/#{version}/dbeaver-ce-#{version}-macos.dmg"
   name "DBeaver Community Edition"
