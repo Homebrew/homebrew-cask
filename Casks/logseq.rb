3e351619e9223aa8ec647a0c3b8ad7f901abc090
Baikang <eindex.me@outlook.com>
2021-06-02 20:43:02 +0800

    Update logseq from 0.1.2 to 0.1.3 (#106697)

--- Casks/logseq.rb
+++ Casks/logseq.rb
@@ -1,11 +1,11 @@
 cask "logseq" do
-  version "0.1.2"
+  version "0.1.3"
 
   if Hardware::CPU.intel?
-    sha256 "6a3edfba25d9ea41d57b22a2de4f19cba62a380113ecc125a5d82e085ba6a6c0"
+    sha256 "be683cc52d4f151ccb1ea9144cce20d5f9d7b8038ff73142e6af81af12517f0d"
     url "https://github.com/logseq/logseq/releases/download/#{version}/logseq-darwin-x64-#{version}.dmg"
   else
-    sha256 "532659267b615cf1f60907e0e522809b4c53b1c68fa1916c00f4880b8556c361"
+    sha256 "07e5eba95432f6a453c7071012fe68000602e7170548ebc526250e6e953c810d"
     url "https://github.com/logseq/logseq/releases/download/#{version}/logseq-darwin-arm64-#{version}.dmg"
   end
 
