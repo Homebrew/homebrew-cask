6bafca1ea6a0c96c3ee2f78fcd46fe083ef1ed99
Michael Cho <cho-m@tuta.io>
2021-06-02 14:02:44 -0700

    Update chromedriver.rb

--- Casks/chromedriver.rb
+++ Casks/chromedriver.rb
@@ -2,14 +2,17 @@ cask "chromedriver" do
   version "91.0.4472.19"
 
   if Hardware::CPU.intel?
+    sha256 "c23d8244cff767fc1c9bc259fb86fece0124d1eba9645397fe7903100e98651f"
+
     url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_mac64.zip",
         verified: "chromedriver.storage.googleapis.com/"
-    sha256 "c23d8244cff767fc1c9bc259fb86fece0124d1eba9645397fe7903100e98651f"
   else
+    sha256 "712603c85bc5588b98f35cd395cdfb9363e6d6724e7b8e237dc89751763d9d08"
+
     url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_mac64_m1.zip",
         verified: "chromedriver.storage.googleapis.com/"
-    sha256 "712603c85bc5588b98f35cd395cdfb9363e6d6724e7b8e237dc89751763d9d08"
   end
+
   name "ChromeDriver"
   desc "Automated testing of webapps for Google Chrome"
   homepage "https://sites.google.com/chromium.org/driver/"
