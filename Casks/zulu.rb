16f2c41d178220686d5bce1ac5848bd24fb0dc6a
Josh-Tilles <MerelyAPseudonym@gmail.com>
2021-06-02 17:01:38 -0400

    zulu: fix JDK 16 for macOS 11.4 on Apple Silicon (#106671)
    Co-authored-by: Michael Cho <cho-m@tuta.io>

--- Casks/zulu.rb
+++ Casks/zulu.rb
@@ -1,28 +1,37 @@
 cask "zulu" do
-  version "16.0.1,16.30.15-ca"
-
   if Hardware::CPU.intel?
+    version "16.0.1,16.30.15-ca"
     sha256 "a9988581a95f00da7768562f19632a71e13b7d695329b074504b8b6883a6dc30"
 
     url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
         referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
+
+    livecheck do
+      url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&ext=dmg&os=macos&arch=x86"
+      strategy :page_match do |page|
+        match = page.match(%r{url"\s*:\s*"https:.*?/zulu(\d+(?:\.\d+)*-.*?)-jdk(\d+(?:\.\d+)*)-macos}i)
+        "#{match[2]},#{match[1]}"
+      end
+    end
   else
-    sha256 "5899e7783d06df2ce72ea34435afa9ccedc52528ff94705a8eaa3344096cd9fc"
+    version "16.0.1,16.30.19-ca"
+    sha256 "4b00a4675cd2d467af3de06d60821e3cdb25c646e48d0c8a349992f1f8509eab"
 
     url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_aarch64.dmg",
         referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
+
+    livecheck do
+      url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&ext=dmg&os=macos&arch=arm"
+      strategy :page_match do |page|
+        match = page.match(%r{url"\s*:\s*"https:.*?/zulu(\d+(?:\.\d+)*-.*?)-jdk(\d+(?:\.\d+)*)-macos}i)
+        "#{match[2]},#{match[1]}"
+      end
+    end
   end
 
   name "Azul Zulu Java Standard Edition Development Kit"
-  homepage "https://www.azul.com/downloads/zulu/zulu-mac/"
-
-  livecheck do
-    url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&ext=dmg&os=macos"
-    strategy :page_match do |page|
-      match = page.match(%r{url"\s*:\s*"https:.*?/zulu(\d+(?:\.\d+)*-.*?)-jdk(\d+(?:\.\d+)*)-macos}i)
-      "#{match[2]},#{match[1]}"
-    end
-  end
+  desc "OpenJDK distribution from Azul"
+  homepage "https://www.azul.com/downloads/"
 
   depends_on macos: ">= :sierra"
 
