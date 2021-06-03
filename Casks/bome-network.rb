8f287d9db92963a39b4779164156e699e7483383
Michael Cho <cho-m@tuta.io>
2021-06-02 21:01:46 -0700

    Update bome-network from 1.2.1 to 1.4

--- Casks/bome-network.rb
+++ Casks/bome-network.rb
@@ -1,6 +1,6 @@
 cask "bome-network" do
-  version "1.2.1"
-  sha256 "be2549d8ad526f0e08ef136edf785005840ad3c554de68583788f89714744d5c"
+  version "1.4"
+  sha256 "aaafa7d6744950664baed6c040dd442768c438692bc01842cec494e9eccb8162"
 
   url "https://download.bome.com/BomeNet#{version}.dmg"
   name "Bome Network"
