32790353ef1911290f2ca269dcd30fa3e81f8a1f
tauqua <kittyandben@gmail.com>
2021-06-03 12:07:42 -0400

    Add verified parameter

--- Casks/lightwright.rb
+++ Casks/lightwright.rb
@@ -2,7 +2,8 @@ cask "lightwright" do
   version "6.0.43.420"
   sha256 "ab70a8d5eda5932feee2cb68356f1ff7d167e0841ae7a482acf01aceb0078ff2"
 
-  url "https://s3.us-east-1.amazonaws.com/lightwright-downloads/Lightwright_#{version}.dmg"
+  url "https://s3.us-east-1.amazonaws.com/lightwright-downloads/Lightwright_#{version}.dmg",
+      verified: "s3.us-east-1.amazonaws.com/lightwright-downloads/"
   name "Lightwright"
   desc "Utility for generating theatrical lighting data and paperwork"
   homepage "https://www.lightwright.com/"
