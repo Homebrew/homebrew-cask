22c2b00b879953a5505d4ea3006fbf5538c4f49e
Reno <25192197+singularitti@users.noreply.github.com>
2021-06-02 20:43:51 -0400

    Update sbarex-qlmarkdown from 1.0b25 to 1.0.1

--- Casks/sbarex-qlmarkdown.rb
+++ Casks/sbarex-qlmarkdown.rb
@@ -1,6 +1,6 @@
 cask "sbarex-qlmarkdown" do
-  version "1.0b25"
-  sha256 "4653980977900d38b8de46179ced9113f9ec06629f6358f272204b83d75dd8d3"
+  version "1.0.1"
+  sha256 "c0cef52213c6abcc0e90b3f3bdc1f288fb088bdaac88c5b639dba9a19b8f3690"
 
   url "https://github.com/sbarex/QLMarkdown/releases/download/#{version}/QLMarkdown.zip"
   name "sbarex-qlmarkdown"
