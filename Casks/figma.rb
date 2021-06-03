08484587d437a29ddaa8e17030a7b466eb6e0c58
Ondrej Führer <ondrejfuhrer@users.noreply.github.com>
2021-06-03 16:47:40 +0200

    `figma`: split livecheck for intel & arm version + intel version bump

--- Casks/figma.rb
+++ Casks/figma.rb
@@ -1,11 +1,11 @@
 cask "figma" do
-  version "98.11.0"
-
   if Hardware::CPU.intel?
+    version "98.13.0"
     sha256 "e9899d8dafdb752414c8fe0e070d6f81b32bc713f220618b4fc90b380e345a72"
 
     url "https://desktop.figma.com/mac/Figma-#{version}.zip"
   else
+    version "98.11.0"
     sha256 "85bf1fc63a799b46c0d8e2e041033b54537cca5ee5dcb27af3b6016f080ca7c0"
 
     url "https://desktop.figma.com/mac-arm/Figma-#{version}.zip"
@@ -16,7 +16,11 @@ cask "figma" do
   homepage "https://www.figma.com/"
 
   livecheck do
-    url "https://desktop.figma.com/mac/RELEASE.json"
+    if Hardware::CPU.intel?
+      url "https://desktop.figma.com/mac/RELEASE.json"
+    else
+      url "https://desktop.figma.com/mac-arm/RELEASE.json"
+    end
     regex(%r{/Figma[._-]v?(\d+(?:\.\d+)+)\.zip}i)
   end
 
