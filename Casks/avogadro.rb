ec83a8fbdcafc8cc47a28692327f57c364adee9f
Geoff Hutchison <geoff.hutchison@gmail.com>
2021-06-03 01:25:44 -0400

    Bump version, GitHub download (#106721)

--- Casks/avogadro.rb
+++ Casks/avogadro.rb
@@ -1,9 +1,9 @@
 cask "avogadro" do
-  version "1.90.0"
-  sha256 "71392555d2d38a486ea58d4a522304b59855fa8c3f71d88316bf3828f7a064da"
+  version "1.94.0"
+  sha256 "901447f1d160f7911c8f4609b762f0611f4a8effc9b3db3ba9fa201b686e8926"
 
-  url "https://downloads.sourceforge.net/avogadro/avogadro2/Avogadro2-#{version}-Darwin.dmg",
-      verified: "sourceforge.net/avogadro/avogadro2/"
+  url "https://github.com/OpenChemistry/avogadrolibs/releases/download/#{version}/Avogadro2-#{version}-Darwin.dmg",
+      verified: "github.com/OpenChemistry/avogadrolibs/"
   name "Avogadro"
   desc "Molecule editor and visualizer"
   homepage "https://avogadro.cc/"
