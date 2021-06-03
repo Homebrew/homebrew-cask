215553bcc7aa714bcb08ff502c8660eaff199ded
tauqua <kittyandben@gmail.com>
2021-06-03 11:51:04 -0400

    Add lightwright cask

---
+++ Casks/lightwright.rb
@@ -0,0 +1,17 @@
+cask "lightwright" do
+  version "6.0.43.420"
+  sha256 "ab70a8d5eda5932feee2cb68356f1ff7d167e0841ae7a482acf01aceb0078ff2"
+
+  url "https://s3.us-east-1.amazonaws.com/lightwright-downloads/Lightwright_#{version}.dmg"
+  name "Lightwright"
+  desc "Utility for generating theatrical lighting data and paperwork"
+  homepage "https://www.lightwright.com/"
+
+  app "Lightwright 6.app"
+
+  zap trash: [
+  "~/Library/Application Support/com.mckernon.lightwright6",
+  "/Users/Shared/Lightwright 6",
+]
+
+end
