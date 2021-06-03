21aaad7ead98f8cdd2faee07fd9ec7a3a6b0b353
Michael Cho <cho-m@tuta.io>
2021-06-02 21:30:34 -0700

    Update cog from 1534,297ba639 to 1537,668475c4

--- Casks/cog.rb
+++ Casks/cog.rb
@@ -1,6 +1,6 @@
 cask "cog" do
-  version "1534,297ba639"
-  sha256 "3df2298e53e88b4e9ed1f9b536151263a13cace7f921f85b925dfc2be7dd27dd"
+  version "1537,668475c4"
+  sha256 "a735af68b139d0d5201c3866f7b781f608ee7fb10c1a2c9b9ce30deff57637ae"
 
   url "https://f.losno.co/cog/Cog-#{version.after_comma}.zip"
   name "Cog"
