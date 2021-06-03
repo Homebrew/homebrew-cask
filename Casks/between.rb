53513d709790300e8e0bf123b3e038a48604437c
Michael Cho <cho-m@tuta.io>
2021-06-02 19:21:36 -0700

    Update between from 1.0.4 to 1.0.8 (#106734)
    * Update between from 1.0.4 to 1.0.8
    
    * Update between.rb
    
    * Update between.rb

--- Casks/between.rb
+++ Casks/between.rb
@@ -1,13 +1,14 @@
 cask "between" do
-  version "1.0.4"
-  sha256 "87b8c8c2762ae02fc65b1457dddc1613847bdef513677982f63ed9f13c6e296d"
+  version "1.0.8"
+  sha256 "2657704186c88668975f21a22eeadd3ee31ab3527c59214ca90a89b7342be896"
 
-  url "http://assets-pc.between.us/downloads/between-#{version}.dmg"
+  url "https://assets-pc.between.us/downloads/between-#{version}.dmg"
   name "Between"
+  desc "Relationship app for couples"
   homepage "https://between.us/"
 
   livecheck do
-    url "http://assets-pc.between.us/downloads/metadata-mac.xml"
+    url "https://assets-pc.between.us/downloads/metadata-mac.xml"
     strategy :sparkle
   end
 
