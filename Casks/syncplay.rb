75c86ab5228bc0cef164dbb3b7b664e21f834afc
Alberto Xamin <alberto@xamin.it>
2021-06-03 22:33:52 +0200

    Update syncplay.rb

--- Casks/syncplay.rb
+++ Casks/syncplay.rb
@@ -5,7 +5,7 @@ cask "syncplay" do
   version "1.6.8"
   sha256 "9fbe6d616865511bbf75daa24ae6eb1ec40797ef1123265c7c50a5d9f1421ea3"
 
-  url "https://github.com/Syncplay/syncplay/releases/download/v#{version}/Syncp>
+  url "https://github.com/Syncplay/syncplay/releases/download/v#{version}/Syncplay_#{version}.dmg",
       verified: "github.com/Syncplay/syncplay/"
   name "Syncplay"
   homepage "https://syncplay.pl/"
