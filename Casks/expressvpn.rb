e4e7ee81019f4d2ba5868f5cc9d81f0a4ade5cc9
Miccal Matthews <miccal.matthews@gmail.com>
2021-06-03 13:37:28 +0800

    Update expressvpn.rb

--- Casks/expressvpn.rb
+++ Casks/expressvpn.rb
@@ -2,13 +2,13 @@ cask "expressvpn" do
   version "10.2.4.6"
   sha256 "8ab87794c098c752defc43b5aef21c2f420f418011ab90396d2b9678a77ced2d"
 
-  url "https://download.xpress-vpn.com/clients/mac/expressvpn_mac_#{version}_release.pkg"
+  url "https://www.expressvpn.works/clients/mac/expressvpn_mac_#{version}_release.pkg"
   name "ExpressVPN"
   desc "VPN client for secure internet access and private browsing"
-  homepage "https://www.xpress-vpn.com/vpn-software/vpn-mac/"
+  homepage "https://www.expressvpn.works/"
 
   livecheck do
-    url "https://www.xpress-vpn.com/clients/latest/mac"
+    url "https://www.expressvpn.works/clients/latest/mac"
     strategy :header_match
   end
 
