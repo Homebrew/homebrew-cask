de7ae1c7fa5f0df00b3c5e280bf4859f610fd4fa
Michael Cho <cho-m@tuta.io>
2021-06-05 12:40:42 -0700

    miniconda: update livecheck
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/miniconda.rb
+++ Casks/miniconda.rb
@@ -8,9 +8,12 @@ cask "miniconda" do
   desc "Minimal installer for conda"
   homepage "https://conda.io/miniconda.html"
 
+  # This regex restricts matching to a specific Python version. This will need
+  # to be updated when the prefix changes in the latest version at the top of:
+  # https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
   livecheck do
-    url "https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh"
-    regex(/^#\s*VER:\s*((?:py\d*[._-])?v?(\d+(?:\.\d+)+))/i)
+    url "https://repo.anaconda.com/miniconda/"
+    regex(/>\s*Miniconda3-(py38[._-]\d+(?:\.\d+)+)-MacOSX-x86_64\.sh\s*</i)
   end
 
   auto_updates true
