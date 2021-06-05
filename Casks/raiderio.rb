cb385499f984dc04fbdfd3c458ec6549f167648a
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-06-05 20:45:32 +0900

    Update raiderio from 2.1.4 to 2.1.5

--- Casks/raiderio.rb
+++ Casks/raiderio.rb
@@ -1,6 +1,6 @@
 cask "raiderio" do
-  version "2.1.4"
-  sha256 "fa16789f0f4c49d7288445c31a155f1ce3391377524255f349bf52f598e2b275"
+  version "2.1.5"
+  sha256 "8bd8cd3f880bb5b5daee9f2dc9a2f66c8063c0118066bd63ab67b81afa868831"
 
   url "https://github.com/RaiderIO/raiderio-client-builds/releases/download/v#{version}/RaiderIO_Client.dmg",
       verified: "github.com/RaiderIO/raiderio-client-builds/"
