f3181836a0d590bc9f86715e3d43465d24eeddce
Pierre-Henri Schott <ph@schott.io>
2021-06-05 20:46:47 +0200

    Add Peugeot Update.app v1.1.1

---
+++ Casks/peugeot-update.rb
@@ -0,0 +1,12 @@
+cask "peugeot-update" do
+  version "1.1.1"
+  sha256 :no_check
+
+  url "https://fr.ndp.awsmpsa.com/majesticinstaller/peugeot_update.dmg",
+      verified: "fr.ndp.awsmpsa.com"
+  name "peugeot-update"
+  desc "Update Peugeot navigating system"
+  homepage "https://cartographie.peugeot.fr/mise-a-jour-cartographie-gps.html"
+
+  app "Peugeot Update.app"
+end
