d8a9f9d693485a3b3c1a9c2c35b897d96186d4e9
Michael Cho <cho-m@tuta.io>
2021-06-04 17:39:51 -0700

    Update ff-works from 2.3.1 to 2.3.2 (#106805)

--- Casks/ff-works.rb
+++ Casks/ff-works.rb
@@ -1,5 +1,5 @@
 cask "ff-works" do
-  version "2.3.1"
+  version "2.3.2"
   sha256 :no_check
 
   url "https://www.ffworks.net/ffWorks.dmg"
@@ -9,9 +9,10 @@ cask "ff-works" do
 
   livecheck do
     url "https://www.ffworks.net/download.html"
-    strategy :page_match
-    regex(/version\s*(\d+(?:\.\d+)*)/i)
+    regex(/version\s*(\d+(?:\.\d+)+)/i)
   end
+
+  depends_on macos: ">= :yosemite"
 
   app "ff·Works.app"
 end
