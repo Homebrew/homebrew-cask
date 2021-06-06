0d7a4d6f9aff08bfc9ccc93516c32606cec9e887
Thomas Deutsch <thomas@tuxpeople.org>
2021-06-06 14:48:01 +0200

    Add ARM version for kubenav.app Cask
    Signed-off-by: Thomas Deutsch <thomas@tuxpeople.org>

--- Casks/kubenav.rb
+++ Casks/kubenav.rb
@@ -1,9 +1,15 @@
 cask "kubenav" do
   version "3.5.0"
-  sha256 "500cf31408ed6c1704e3462a307b8f4f76ce25e64dccbd450a3bd8407c6cf06d"
 
-  url "https://github.com/kubenav/kubenav/releases/download/#{version}/kubenav-darwin-amd64.zip",
-      verified: "github.com/kubenav/kubenav/"
+  if Hardware::CPU.intel?
+    sha256 "500cf31408ed6c1704e3462a307b8f4f76ce25e64dccbd450a3bd8407c6cf06d"
+    url "https://github.com/kubenav/kubenav/releases/download/#{version}/kubenav-darwin-amd64.zip",
+        verified: "github.com/kubenav/kubenav/"
+  else
+    sha256 "4094c97f26034eae1d8a3caefa038ef72cd4b8f53363d8d71b23885653184c4b"
+    url "https://github.com/kubenav/kubenav/releases/download/#{version}/kubenav-darwin-arm64.zip",
+        verified: "github.com/kubenav/kubenav/"
+  end
   name "kubenav"
   desc "Navigator for your Kubernetes clusters right in your pocket"
   homepage "https://kubenav.io/"
