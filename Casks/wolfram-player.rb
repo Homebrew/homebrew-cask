fd06bee6a53d340a7ebbed983ebdf3fe4626735f
Michael Cho <cho-m@tuta.io>
2021-06-05 19:54:34 -0700

    Update wolfram-player.rb

--- Casks/wolfram-player.rb
+++ Casks/wolfram-player.rb
@@ -3,16 +3,21 @@ cask "wolfram-player" do
   sha256 "371371bcad5c42b8830e109e97d8926d7e1fa2cdd46116e2a5464df6bdd6efdc"
 
   url "https://account.wolfram.com/download/public/wolfram-player/#{version}/desktop/MAC"
-  name "wolfram-player"
+  name "Wolfram Player"
   desc "Tool to view and interact with Wolfram notebooks"
   homepage "https://www.wolfram.com/player"
+
+  livecheck do
+    url :homepage
+    regex(/Latest\s+version\s+(\d+(?:\.\d+)+)/i)
+  end
 
   depends_on macos: ">= :mojave"
 
   app "Wolfram Player.app"
 
   zap trash: [
-    "~/Library/WolframPlayer",
     "~/Library/Preferences/com.wolfram.Mathematica.Player.plist",
+    "~/Library/WolframPlayer",
   ]
 end
