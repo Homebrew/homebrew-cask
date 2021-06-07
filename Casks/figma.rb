7240d605453861b22fd622e2a707e8987f9707a8
Michael Cho <cho-m@tuta.io>
2021-06-06 21:23:12 -0700

    figma: match style of other casks

--- Casks/figma.rb
+++ Casks/figma.rb
@@ -4,25 +4,26 @@ cask "figma" do
     sha256 "213753d01235bc39925553bb78d8d079553885516cadf2e18702ccc6da46a8a8"
 
     url "https://desktop.figma.com/mac/Figma-#{version}.zip"
+
+    livecheck do
+      url "https://desktop.figma.com/mac/RELEASE.json"
+      regex(%r{/Figma[._-]v?(\d+(?:\.\d+)+)\.zip}i)
+    end
   else
     version "98.11.0"
     sha256 "85bf1fc63a799b46c0d8e2e041033b54537cca5ee5dcb27af3b6016f080ca7c0"
 
     url "https://desktop.figma.com/mac-arm/Figma-#{version}.zip"
+
+    livecheck do
+      url "https://desktop.figma.com/mac-arm/RELEASE.json"
+      regex(%r{/Figma[._-]v?(\d+(?:\.\d+)+)\.zip}i)
+    end
   end
 
   name "Figma"
   desc "Collaborative team software"
   homepage "https://www.figma.com/"
-
-  livecheck do
-    if Hardware::CPU.intel?
-      url "https://desktop.figma.com/mac/RELEASE.json"
-    else
-      url "https://desktop.figma.com/mac-arm/RELEASE.json"
-    end
-    regex(%r{/Figma[._-]v?(\d+(?:\.\d+)+)\.zip}i)
-  end
 
   auto_updates true
 
