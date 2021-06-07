604401af5515bbd8ca6bf3dd39bf1e4ff47871c3
ralph <ralph@cs.cf.ac.uk>
2021-06-07 06:08:31 +0100

    Update qcad from 3.26.2 to 3.26.4

--- Casks/qcad.rb
+++ Casks/qcad.rb
@@ -1,11 +1,11 @@
 cask "qcad" do
-  version "3.26.2"
+  version "3.26.4"
 
   if MacOS.version <= :high_sierra
     sha256 "d1c06bf0f2fc7df0bbe2f428e159c7aa12f13537141bc69d8e5dee1c793047b6"
     url "https://www.qcad.org/archives/qcad/qcad-#{version}-trial-macos-10.10-10.13.dmg"
   else
-    sha256 "46fb356960449ae4addf0ce1555872bad5d1b70aaf8f94c10793f5f61e429695"
+    sha256 "cf1ac7f91fad40b0ce3698c7f0b0f2b36baa61ba4c865d5d276f3890f3ba92cb"
     url "https://www.qcad.org/archives/qcad/qcad-#{version}-trial-macos-10.14-11.2.dmg"
   end
 
