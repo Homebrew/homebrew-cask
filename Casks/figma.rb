68ed0f4a47d359462b1d3990e9605bd1c017a4c4
Ondřej Führer <ondrejfuhrer@users.noreply.github.com>
2021-06-07 06:35:18 +0200

    `figma`: split livecheck for intel & arm version + intel version bump (#106770)

--- Casks/figma.rb
+++ Casks/figma.rb
@@ -1,24 +1,29 @@
 cask "figma" do
-  version "98.11.0"
-
   if Hardware::CPU.intel?
-    sha256 "e9899d8dafdb752414c8fe0e070d6f81b32bc713f220618b4fc90b380e345a72"
+    version "98.13.0"
+    sha256 "213753d01235bc39925553bb78d8d079553885516cadf2e18702ccc6da46a8a8"
 
     url "https://desktop.figma.com/mac/Figma-#{version}.zip"
+
+    livecheck do
+      url "https://desktop.figma.com/mac/RELEASE.json"
+      regex(%r{/Figma[._-]v?(\d+(?:\.\d+)+)\.zip}i)
+    end
   else
+    version "98.11.0"
     sha256 "85bf1fc63a799b46c0d8e2e041033b54537cca5ee5dcb27af3b6016f080ca7c0"
 
     url "https://desktop.figma.com/mac-arm/Figma-#{version}.zip"
+
+    livecheck do
+      url "https://desktop.figma.com/mac-arm/RELEASE.json"
+      regex(%r{/Figma[._-]v?(\d+(?:\.\d+)+)\.zip}i)
+    end
   end
 
   name "Figma"
   desc "Collaborative team software"
   homepage "https://www.figma.com/"
-
-  livecheck do
-    url "https://desktop.figma.com/mac/RELEASE.json"
-    regex(%r{/Figma[._-]v?(\d+(?:\.\d+)+)\.zip}i)
-  end
 
   auto_updates true
 
