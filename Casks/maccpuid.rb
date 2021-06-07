ac56417c88685d0a24f96c5ffa504edcca79339e
Tom Matthews <trcm@pm.me>
2021-06-07 12:44:45 +0100

    Update maccpuid

--- Casks/maccpuid.rb
+++ Casks/maccpuid.rb
@@ -2,10 +2,10 @@ cask "maccpuid" do
   version "3.1"
   sha256 :no_check
 
-  url "https://software.intel.com/sites/default/files/MacCPUID.zip"
-  appcast "https://software.intel.com/en-us/download/download-maccpuid"
+  url "https://software.intel.com/content/dam/develop/public/us/en/documents/maccpuid.zip"
+  appcast "https://software.intel.com/content/www/us/en/develop/download/download-maccpuid.html"
   name "MacCPUID"
-  homepage "https://software.intel.com/en-us/download/download-maccpuid"
+  homepage "https://software.intel.com/content/www/us/en/develop/download/download-maccpuid"
 
   app "MacCPUID.app"
 
