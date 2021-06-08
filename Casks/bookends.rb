2f8594e4034d2f8e69c4084a443fe7c597f14815
Frank Blechschmidt <contact@frank-blechschmidt.com>
2021-06-07 22:54:41 -0700

    Update bookends cask to 13.5.5 (#106883)
    Co-authored-by: Michael Cho <cho-m@tuta.io>

--- Casks/bookends.rb
+++ Casks/bookends.rb
@@ -1,5 +1,5 @@
 cask "bookends" do
-  version "13.5.4"
+  version "13.5.5"
   sha256 :no_check
 
   url "https://www.sonnysoftware.com/Bookends.dmg"
@@ -9,9 +9,10 @@ cask "bookends" do
 
   livecheck do
     url :homepage
-    strategy :page_match
-    regex(/version\s*(\d+(?:\.\d+)*)/i)
+    regex(/version\s*(\d+(?:\.\d+)+)/i)
   end
+
+  depends_on macos: ">= :high_sierra"
 
   app "Bookends.app"
 end
