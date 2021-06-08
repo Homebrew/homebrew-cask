c9bcbc3d6f6441c7af006f5efb7a2db16b1cc0f5
Eloy Aldea <eloy.aldea@gmail.com>
2021-06-08 23:03:09 +0200

    Update whatsapp from 2.2121.5 to 2.2121.6

--- Casks/whatsapp.rb
+++ Casks/whatsapp.rb
@@ -1,6 +1,6 @@
 cask "whatsapp" do
-  version "2.2121.5"
-  sha256 "8173416d9fee8b2c4ad6f57a47c767e61182bba40f25dbc31bdde6c9459d40b1"
+  version "2.2121.6"
+  sha256 "530bc2d4a2233c1636dc1d4eabac8172869ef013ddbfb07c9e95f272cb938507"
 
   url "https://web.whatsapp.com/desktop/mac/files/release-#{version}.zip"
   name "WhatsApp"
