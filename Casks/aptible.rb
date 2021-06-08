8234d743add461306e3836fb59d014021b56aa1f
Alex Kubacki <usernotfound@gmail.com>
2021-06-07 20:36:27 -0600

    Update aptible from 0.17.2,20210317010100,228 to 0.18.1,20210607165408,239 (#106873)
    Co-authored-by: Michael Cho <cho-m@tuta.io>

--- Casks/aptible.rb
+++ Casks/aptible.rb
@@ -1,12 +1,21 @@
 cask "aptible" do
-  version "0.17.2,20210317010100,228"
-  sha256 "b48ea991bfd79857b68e97e2c2433853dd6d51f0bbb7d7703cd332de04ad18b5"
+  version "0.18.1,20210607165408,239"
+  sha256 "07c089282fe7f257eb829fa32c92c364b6ff28f89e6fb28b0f7195643fe961a6"
 
   url "https://omnibus-aptible-toolbelt.s3.amazonaws.com/aptible/omnibus-aptible-toolbelt/master/#{version.after_comma.after_comma}/pkg/aptible-toolbelt-#{version.before_comma}%2B#{version.after_comma.before_comma}-mac-os-x.10.11.6-1.pkg",
       verified: "omnibus-aptible-toolbelt.s3.amazonaws.com/"
   name "Aptible Toolbelt"
   desc "Command-line tool for Aptible Deploy, an audit-ready App Deployment Platform"
   homepage "https://www.aptible.com/documentation/deploy/cli.html"
+
+  livecheck do
+    url :homepage
+    strategy :page_match do |page|
+      page.scan(%r{href=.*?/(\d+)/pkg/aptible-toolbelt[._-]v?(\d+(?:\.\d+)+)%2B(\d+)[._-]mac}i).map do |match|
+        "#{match[1]},#{match[2]},#{match[0]}"
+      end
+    end
+  end
 
   depends_on formula: "libu2f-host"
 
