fb41eda2c33befa6fc3445876380e9d2656ff134
Michael Cho <cho-m@tuta.io>
2021-06-07 22:53:33 -0700

    Update nosqlbooster-for-mongodb.rb

--- Casks/nosqlbooster-for-mongodb.rb
+++ Casks/nosqlbooster-for-mongodb.rb
@@ -4,10 +4,14 @@ cask "nosqlbooster-for-mongodb" do
 
   url "https://s3.mongobooster.com/download/releasesv#{version.major}/nosqlbooster4mongo-#{version}.dmg",
       verified: "mongobooster.com/"
-  appcast "https://nosqlbooster.com/downloads"
   name "NoSQLBooster for MongoDB"
   desc "GUI tool and IDE for MongoDB"
   homepage "https://nosqlbooster.com/"
+
+  livecheck do
+    url "https://nosqlbooster.com/downloads"
+    regex(/%22patch_ver%22(?:%20|\s)*%3A(?:%20|\s)*%22v?(\d+(?:\.\d+)+)%22/i)
+  end
 
   app "NoSQLBooster for MongoDB.app"
 end
