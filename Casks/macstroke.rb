a1ecd7337c5eb50140eb1621a0e9c8d3c8a4e83a
Miccal Matthews <miccal.matthews@gmail.com>
2021-06-08 16:05:06 +0800

    Update macstroke.rb

--- Casks/macstroke.rb
+++ Casks/macstroke.rb
@@ -3,16 +3,17 @@ cask "macstroke" do
   sha256 "c577aec2791f33a95ce0a33fe9e3691ae91c64d3bd02cc71dde6a24e1f71d7de"
 
   url "https://github.com/mtjo/MacStroke/releases/download/#{version}/MacStroke.zip"
-  name "macstroke"
-  desc "global mouse stroke"
-  homepage "github.com/mtjo/MacStroke"
-  appcast "https://github.com/mtjo/MacStroke/blob/release/AppCast.xml"
+  name "MacStroke"
+  desc "Configurable global mouse gestures"
+  homepage "https://github.com/mtjo/MacStroke/"
+
   app "MacStroke.app"
+
   zap trash: [
-  "~/Library/Application Scripts/net.mtjo.MacStroke.FinderSyncExtension",
-  "~/Library/Caches/net.mtjo.MacStroke",
-  "~/Library/Containers/net.mtjo.MacStroke.FinderSyncExtension",
-  "~/Library/Cookies/net.mtjo.MacStroke.binarycookies",
-  "~/Library/Preferences/net.mtjo.MacStroke.plist",
-]
+    "~/Library/Application Scripts/net.mtjo.MacStroke.FinderSyncExtension",
+    "~/Library/Caches/net.mtjo.MacStroke",
+    "~/Library/Containers/net.mtjo.MacStroke.FinderSyncExtension",
+    "~/Library/Cookies/net.mtjo.MacStroke.binarycookies",
+    "~/Library/Preferences/net.mtjo.MacStroke.plist",
+  ]
 end
