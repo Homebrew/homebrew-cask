7e8b848f9e6fda1fcf5c517fd01654ba0d43cc95
osx user <gili.shemesh@gmail.com>
2021-06-08 11:21:36 +0300

    Upgrade cakebrewjs.app to v0.1.29

--- Casks/cakebrewjs.rb
+++ Casks/cakebrewjs.rb
@@ -1,6 +1,6 @@
 cask "cakebrewjs" do
   version "0.1.28"
-  sha256 "99a2c0ec57a01db417a5225809b817b3d1b939d6b0356e01adc4373f51c4234f"
+  sha256 "8e9b4e767a1aa1243b17def6142cb1d7c244c3324afdc9cd8cf3fb6ad29ce605"
 
   url "https://downloads.sourceforge.net/cakebrewjs/v#{version}/cakebrewjs-#{version}-mac.zip"
   name "cakebrewjs"
