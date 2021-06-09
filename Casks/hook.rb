20abf7fd8935ffc2af2a64c0903a595be1277e07
nthumann <me@n-thumann.de>
2021-06-09 18:30:54 +0200

    hook: Fix download with fake user agent

--- Casks/hook.rb
+++ Casks/hook.rb
@@ -2,7 +2,8 @@ cask "hook" do
   version "3.1,2021.05"
   sha256 "91cb06373dfa28b9274ea1274d67656d657377c3c0ad74f5924e626462cbde97"
 
-  url "https://hookproductivity.com/wp-content/uploads/#{version.after_comma.major}/#{version.after_comma.minor}/Hook-productivity-app-#{version.before_comma}.dmg_.zip"
+  url "https://hookproductivity.com/wp-content/uploads/#{version.after_comma.major}/#{version.after_comma.minor}/Hook-productivity-app-#{version.before_comma}.dmg_.zip",
+      user_agent: :fake
   name "Hook"
   desc "Link and retrieve key information"
   homepage "https://hookproductivity.com/"
