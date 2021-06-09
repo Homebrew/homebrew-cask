1327213ac890a949817ccde024fa8982cd1b2acd
Janek Łukaszewicz <janluk@google.com>
2021-06-09 21:42:12 +0200

    Add Universal Battle 2

---
+++ Casks/universal-battle.rb
@@ -0,0 +1,11 @@
+cask "universal-battle" do
+  version :latest
+  sha256 :no_check
+
+  url "https://universalbattle2.com/builds/Mac.zip"
+  name "universal-battle"
+  desc "Realtime Multiplayer TableTop War Game"
+  homepage "https://universalbattle2.com/"
+
+  app "OSX/Universal Battle 2.app"
+end
