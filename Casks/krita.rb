e56fca360b290bada69b577158a836c6de5673f6
Loïc PÉRON <loic.peron@gmail.com>
2021-06-09 15:43:31 +0200

    Update krita from 4.4.3 to 4.4.5

--- Casks/krita.rb
+++ Casks/krita.rb
@@ -1,6 +1,6 @@
 cask "krita" do
-  version "4.4.3"
-  sha256 "f2415d5e1b6b15660996001dddb0b1860b7d32c1fb24f9a6334113929e0c84af"
+  version "4.4.5"
+  sha256 "1b0e6ecb539043f6522da6f5192c7fcdaae3641fbe5d4a766e4c7834288efc71"
 
   url "https://cdn.download.kde.org/stable/krita/#{version.major_minor_patch}/krita-#{version}.dmg",
       verified: "cdn.download.kde.org/stable/krita/"
