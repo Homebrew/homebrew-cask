ea31c9fa4e121e0e756fe7f7ec8b473fcf584590
Michael Cho <cho-m@tuta.io>
2021-06-08 18:45:46 -0700

    Update xamarin-mac from 7.10.0.5 to 7.14.0.1 (#106938)
    * Update xamarin-mac from 7.10.0.5 to 7.14.0.1
    
    * Update xamarin-mac.rb

--- Casks/xamarin-mac.rb
+++ Casks/xamarin-mac.rb
@@ -1,6 +1,6 @@
 cask "xamarin-mac" do
-  version "7.10.0.5"
-  sha256 "d29cef50d1bf817d332af6bbbeb6f2d4f0cb92e642eaaf419ce26b3f82355a60"
+  version "7.14.0.1"
+  sha256 "0cd715969246b7aba8ace5685260f70a8b486bd5ec5aacd0ffb0f3976d9b1818"
 
   url "https://dl.xamarin.com/XamarinforMac/Mac/xamarin.mac-#{version}.pkg"
   name "Xamarin Mac"
@@ -9,8 +9,7 @@ cask "xamarin-mac" do
 
   livecheck do
     url "https://software.xamarin.com/Service/Updates?v=2&pv0ab364ff-c0e9-43a8-8747-3afb02dc7731=0"
-    strategy :page_match
-    regex(%r{/xamarin\.mac-(\d+(?:\.\d+)*)\.pkg}i)
+    regex(%r{/xamarin\.mac[._-]v?(\d+(?:\.\d+)+)\.pkg}i)
   end
 
   depends_on cask: "homebrew/cask-versions/mono-mdk-for-visual-studio"
