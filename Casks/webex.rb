bc1aa0a5d4e78fad11b7036ad9402cbe125c5c51
Kesara Rathnayake <kesara@fq.nz>
2021-06-09 12:32:46 +1200

    Add Webex Apple Silicon version (#106917)

--- Casks/webex.rb
+++ Casks/webex.rb
@@ -1,8 +1,13 @@
 cask "webex" do
-  version "41.5.0.19072"
+  version "41.6.1.19162"
   sha256 :no_check
 
-  url "https://binaries.webex.com/WebexTeamsDesktop-MACOS-Gold/Webex.dmg"
+  if Hardware::CPU.intel?
+    url "https://binaries.webex.com/WebexTeamsDesktop-MACOS-Gold/Webex.dmg"
+  else
+    url "https://binaries.webex.com/WebexDesktop-MACOS-Apple-Silicon-Gold/Webex.dmg"
+  end
+
   name "Webex Teams"
   desc "Video communication and virtual meeting platform"
   homepage "https://www.webex.com/"
