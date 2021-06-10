1f7139b5fc7da5310feaa71b7ea3b87f6c0e9451
Miccal Matthews <miccal.matthews@gmail.com>
2021-06-10 08:41:34 +0800

    Update hot.rb

--- Casks/hot.rb
+++ Casks/hot.rb
@@ -3,7 +3,6 @@ cask "hot" do
   sha256 "af8fa54920f5dec0f95cb04cca55d203d7b7f2ec99259b211a51261b3e173d93"
 
   url "https://github.com/macmade/Hot/releases/download/#{version}/Hot.app.zip"
-  appcast "https://github.com/macmade/Hot/releases.atom"
   name "Hot"
   desc "Menu bar application that displays the CPU speed limit due to thermal issues"
   homepage "https://github.com/macmade/Hot"
