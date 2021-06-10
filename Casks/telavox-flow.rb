5b5a0513576765049939d24715147cf7a76ce911
Michael Cho <cho-m@tuta.io>
2021-06-10 15:03:15 -0700

    Update telavox-flow.rb

--- Casks/telavox-flow.rb
+++ Casks/telavox-flow.rb
@@ -2,8 +2,8 @@ cask "telavox-flow" do
   version "1.121.0"
   sha256 "471a41b8fd84b0a6132efdacce577f21eb68aa9c2607a1a2e6fef879a554ce1c"
 
-  url "https://flow-desktop.s3.amazonaws.com/Telavox-#{version}.dmg",
-      verified: "flow-desktop.s3.amazonaws.com/"
+  url "https://flow-desktop.s3.eu-west-2.amazonaws.com/Telavox-#{version}.dmg",
+      verified: "flow-desktop.s3.eu-west-2.amazonaws.com/"
   name "Telavox Flow"
   desc "Communication and collaboration platform"
   homepage "https://telavox.com/en/apps/"
