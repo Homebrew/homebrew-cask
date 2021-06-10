9debb0731a30eff2b8e915acc76908bf2d13acce
Miccal Matthews <miccal.matthews@gmail.com>
2021-06-10 21:12:27 +0800

    Update eclipse-rcp.rb

--- Casks/eclipse-rcp.rb
+++ Casks/eclipse-rcp.rb
@@ -4,6 +4,7 @@ cask "eclipse-rcp" do
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-rcp-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name "Eclipse for RCP and RAP Developers"
+  desc "Eclipse IDE for RCP and RAP developers"
   homepage "https://eclipse.org/"
 
   livecheck do
