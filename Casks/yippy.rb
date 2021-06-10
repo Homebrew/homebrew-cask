4a1598f8c1ba95c671f55dfe4099c20304a8eeb6
Miccal Matthews <miccal.matthews@gmail.com>
2021-06-10 08:29:45 +0800

    Update yippy.rb

--- Casks/yippy.rb
+++ Casks/yippy.rb
@@ -1,17 +1,14 @@
 cask "yippy" do
   version "2.5.0"
-  sha256 :no_check
+  sha256 "58e8a5ff11d9c9a2b3cfae91e36ba74250670210b58977ebc0e02d46dc8ddb36"
 
   url "https://github.com/mattDavo/Yippy/releases/download/#{version}/Yippy.zip",
       verified: "github.com/mattDavo/Yippy/"
-  appcast "https://github.com/mattDavo/Yippy/releases.atom"
   name "Yippy"
   desc "Open source clipboard manager"
   homepage "https://yippy.mattdavo.com/"
 
   app "Yippy.app"
 
-  zap trash: [
-    "~/Library/Application Support/MatthewDavidson.Yippy",
-  ]
+  zap trash: "~/Library/Application Support/MatthewDavidson.Yippy"
 end
