153c5802fcdf2dc0b5074a690fb3e9563cd1857e
Miccal Matthews <miccal.matthews@gmail.com>
2021-06-10 08:36:02 +0800

    Update yippy.rb

--- Casks/yippy.rb
+++ Casks/yippy.rb
@@ -1,6 +1,6 @@
 cask "yippy" do
   version "2.5.0"
-  sha256 "58e8a5ff11d9c9a2b3cfae91e36ba74250670210b58977ebc0e02d46dc8ddb36"
+  sha256 "39b59894324e9e4a986dc076d925a9449cf35edcf0e84fc7d57557d0c5e87318"
 
   url "https://github.com/mattDavo/Yippy/releases/download/#{version}/Yippy.zip",
       verified: "github.com/mattDavo/Yippy/"
