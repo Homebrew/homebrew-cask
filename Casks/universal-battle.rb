d64ffc004f67c509dc91e22c9546495f27fcdf2a
Miccal Matthews <miccal.matthews@gmail.com>
2021-06-10 21:18:32 +0800

    Update universal-battle.rb

--- Casks/universal-battle.rb
+++ Casks/universal-battle.rb
@@ -3,8 +3,8 @@ cask "universal-battle" do
   sha256 :no_check
 
   url "https://universalbattle2.com/builds/Mac.zip"
-  name "universal-battle"
-  desc "Realtime Multiplayer TableTop War Game"
+  name "Universal Battle"
+  desc "Realtime multiplayer tabletop war game"
   homepage "https://universalbattle2.com/"
 
   livecheck do
@@ -14,7 +14,5 @@ cask "universal-battle" do
 
   app "OSX/Universal Battle 2.app"
 
-  zap trash: [
-    "~/Library/Saved Application State/com.gamojo.sa.universalbattle2.savedState",
-  ]
+  zap trash: "~/Library/Saved Application State/com.gamojo.sa.universalbattle2.savedState"
 end
