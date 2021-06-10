b8802869e05d22e4137d18ce8fbb928f4056158a
Michael Cho <cho-m@tuta.io>
2021-06-10 16:41:42 -0700

    Update emclient from 8.2.1441 to 8.2.1457 (#107067)

--- Casks/emclient.rb
+++ Casks/emclient.rb
@@ -1,6 +1,6 @@
 cask "emclient" do
-  version "8.2.1441"
-  sha256 "904a82fc8fcf8c3e96c47ce44931a33380dc0d39be2198b6e05a56db7a21e1c1"
+  version "8.2.1457"
+  sha256 "f0a2cb598796656497890c58374844d49cd5eb203f5ef077b332cb55167f3cb4"
 
   url "https://cdn-dist.emclient.com/dist/v#{version}_Mac/setup.pkg"
   name "eM Client"
