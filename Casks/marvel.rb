27fe1557122b1247ac1fc4a729aea07380e11a12
Michael Cho <cho-m@tuta.io>
2021-06-10 16:49:21 -0700

    Update marvel from 10.7.4,4956 to 10.8.1,4979 (#107052)

--- Casks/marvel.rb
+++ Casks/marvel.rb
@@ -1,6 +1,6 @@
 cask "marvel" do
-  version "10.7.4,4956"
-  sha256 "e099441f68cc72630b1ba8df5db055bb29ff4fa5dc139232c70422a684fc5e7d"
+  version "10.8.1,4979"
+  sha256 "965b69dd3df86268c383473297adc4a6fab2e368f7b99e7305f983acc28b2012"
 
   url "https://storage.googleapis.com/sketch-plugin/#{version.before_comma}/Marvel.zip",
       verified: "storage.googleapis.com/sketch-plugin/"
