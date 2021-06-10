5e55c89f02fab5c8de0135fd54e3481e69737afc
Michael Cho <cho-m@tuta.io>
2021-06-10 16:49:50 -0700

    Update scratch from 3.22.0 to 3.23.1 (#107047)

--- Casks/scratch.rb
+++ Casks/scratch.rb
@@ -1,6 +1,6 @@
 cask "scratch" do
-  version "3.22.0"
-  sha256 "1d0ebb76eec0714385e1032ccc41cbef721472ea5af296f5d0239e1ccef74d84"
+  version "3.23.1"
+  sha256 "17c0e385d2a3542a39bf057028dcfa87ca0cd3a4e7cf1cc711aabee289fa3806"
 
   url "https://downloads.scratch.mit.edu/desktop/Scratch%20#{version}.dmg"
   name "Scratch"
