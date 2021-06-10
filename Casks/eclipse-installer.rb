139a9038ebc798bc5f688db512c9fd7a82d1662c
Loïc PÉRON <44115467+nicerloop@users.noreply.github.com>
2021-06-10 15:29:54 +0200

    Fix livecheck for eclipse-installer (#106995)
    * Fix livecheck for eclipse-installer
    
    * Update eclipse-installer.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/eclipse-installer.rb
+++ Casks/eclipse-installer.rb
@@ -4,12 +4,14 @@ cask "eclipse-installer" do
 
   url "https://eclipse.org/downloads/download.php?file=/oomph/epp/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-inst-mac64.tar.gz&r=1"
   name "Eclipse Installer"
+  name "Eclipse IDE installer"
   homepage "https://eclipse.org/"
 
   livecheck do
     url "https://www.eclipse.org/downloads/packages/"
     strategy :page_match do |page|
-      page.scan(%r{href=.*?/downloads/packages/release/(\d+-\d+)}i).map do |release|
+      page.scan(/Eclipse IDE (\d+-\d+) R Packages/i).map do |release|
+        require "net/http"
         version_page = Net::HTTP.get(URI.parse("https://projects.eclipse.org/releases/#{release[0]}"))
         version = version_page.scan(%r{href="/projects/eclipse/releases/(\d+(?:\.\d+)*)"}i)
         "#{version[0][0]},#{release[0]}:R"
