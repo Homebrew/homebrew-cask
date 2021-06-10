1ebe3ab328e893557b4dc6f62f548a4aae28c55d
MarioGT <mario@mariogt.com>
2021-06-10 15:09:32 -0400

    Fixed Cask syntax

--- Casks/pikopixelm1.rb
+++ Casks/pikopixelm1.rb
@@ -2,7 +2,7 @@ cask "pikopixelm1" do
   version "1.5b,150"
   sha256 "af5fbcceb79a2e8bc57892cd9fa7a34498bbe24885f403adaa2985991df63264"
 
-  url "https://github.com/mariogt/PikoPixelM1/releases/download/v#{version}/PikoPixelM1-#{version.before_comma.dots_to_hyphens}.zip"
+  url "https://github.com/mariogt/PikoPixelM1/releases/download/#{version.before_comma}/PikoPixelM1-#{version.before_comma.dots_to_hyphens}.zip"
   name "PikoPixelM1"
   desc "Native apple silicon port of the original pixel art drawing tool PikoPixel"
   homepage "https://github.com/mariogt/PikoPixelM1"
