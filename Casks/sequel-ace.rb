052d52f9a86e1c4d1f6b70af80f7fcaa811d3aa6
Matt Webb <hello@mattwebb.io>
2021-06-10 10:23:26 -0700

    Update sequel-ace from 3.3.2,3030 to 3.3.3,3034 (#107010)

--- Casks/sequel-ace.rb
+++ Casks/sequel-ace.rb
@@ -1,6 +1,6 @@
 cask "sequel-ace" do
-  version "3.3.2,3030"
-  sha256 "5bc0404b3410765e7e4318977caad824ae4e00aa6dd3b01ea9b15537a6ecfd84"
+  version "3.3.3,3034"
+  sha256 "e1297f2756b81ef8b22123e11e1df235849cf5a48897a8254a041f4c7ef5a86c"
 
   url "https://github.com/Sequel-Ace/Sequel-Ace/releases/download/production/#{version.before_comma}-#{version.after_comma}/Sequel-Ace-#{version.before_comma}.zip"
   name "Sequel Ace"
