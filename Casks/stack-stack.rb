f378cebb76ea78ab30d59284233ca5d2a4a7d5f7
Michael Cho <cho-m@tuta.io>
2021-06-10 16:57:42 -0700

    Update stack-stack from 3.30.4 to 3.32.0

--- Casks/stack-stack.rb
+++ Casks/stack-stack.rb
@@ -1,9 +1,17 @@
 cask "stack-stack" do
-  version "3.30.4"
-  sha256 "61e43c50bb9f162287c122d4e5b60880ec3fa07d7b10f11e5487c9e30f3bc786"
+  version "3.32.0"
 
-  url "https://stack-v1.s3.amazonaws.com/builds/prod/Stack-#{version}.dmg",
-      verified: "stack-v1.s3.amazonaws.com/"
+  if Hardware::CPU.intel?
+    sha256 "172199e4f2292cbeb93dc428dae21ffff27e3824d764198fb9117b47f002f5d2"
+
+    url "https://stack-v1.s3.amazonaws.com/builds/prod/Stack-#{version}.dmg",
+        verified: "stack-v1.s3.amazonaws.com/"
+  else
+    sha256 "0c8d4e35f5e320895a1bf2e6cd466cf672f7b992c5ab0bb706ff854f725e9759"
+
+    url "https://binaries.getstack.app/builds/prod/mac/arm64/Stack%20#{version}-arm64.dmg"
+  end
+
   name "Stack"
   desc "Workspace to manage all your productivity apps from one place"
   homepage "https://getstack.app/"
