25332dcae178225e29a5f9d9a0bdec8d8658b9a6
Loïc PÉRON <44115467+nicerloop@users.noreply.github.com>
2021-06-10 15:29:42 +0200

    Fix livecheck for eclipse-php (#106997)
    * Fix livecheck for eclipse-php
    
    * Update eclipse-php.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/eclipse-php.rb
+++ Casks/eclipse-php.rb
@@ -4,12 +4,14 @@ cask "eclipse-php" do
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-php-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name "Eclipse IDE for PHP Developers"
+  desc "Eclipse IDE for PHP developers"
   homepage "https://eclipse.org/"
 
   livecheck do
     url "https://www.eclipse.org/downloads/packages/"
     strategy :page_match do |page|
-      page.scan(%r{href=.*?/downloads/packages/release/(\d+-\d+)}i).map do |release|
+      page.scan(/Eclipse IDE (\d+-\d+) R Packages/i).map do |release|
+        require "net/http"
         version_page = Net::HTTP.get(URI.parse("https://projects.eclipse.org/releases/#{release[0]}"))
         version = version_page.scan(%r{href="/projects/eclipse/releases/(\d+(?:\.\d+)*)"}i)
         "#{version[0][0]},#{release[0]}:R"
