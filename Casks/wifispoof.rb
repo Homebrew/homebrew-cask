18dbbb58dd32eb82e3984a3705928d17e0078cf4
Michael Cho <cho-m@tuta.io>
2021-06-10 16:54:28 -0700

    Update wifispoof from 3.5.6 to 3.5.8 (#107030)
    * Update wifispoof from 3.5.6 to 3.5.8
    
    * Update wifispoof.rb

--- Casks/wifispoof.rb
+++ Casks/wifispoof.rb
@@ -1,10 +1,11 @@
 cask "wifispoof" do
-  version "3.5.6"
-  sha256 "a4a09e170b0f20eb4816c94e9512c08c18236f35bfc644f2af1224c9ce0bd0cb"
+  version "3.5.8"
+  sha256 "1a7971fc8c95bc5874ba800bf5bc63264dd5e5fe317f133985a925ecbbcc4d69"
 
   url "https://sweetpproductions.com/products/wifispoof#{version.major}/WiFiSpoof#{version.major}.dmg",
       verified: "sweetpproductions.com/products/"
   name "WiFiSpoof"
+  desc "Change your computer's MAC address"
   homepage "https://wifispoof.com/"
 
   livecheck do
@@ -13,6 +14,7 @@ cask "wifispoof" do
   end
 
   auto_updates true
+  depends_on macos: ">= :el_capitan"
 
   app "WiFiSpoof.app"
 end
