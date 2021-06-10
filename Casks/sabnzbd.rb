a0ae758dd6b7a9b8768c4964bf5cf5efb49544e0
Michael Cho <cho-m@tuta.io>
2021-06-10 14:50:04 -0700

    Update sabnzbd from 3.2.1 to 3.3.0

--- Casks/sabnzbd.rb
+++ Casks/sabnzbd.rb
@@ -1,6 +1,6 @@
 cask "sabnzbd" do
-  version "3.2.1"
-  sha256 "d007fd890ddfce91e693cd7b759f48ae9fb01750574108f8354eccd15effae99"
+  version "3.3.0"
+  sha256 "3efee16ed97854e35656d79f5aa72c27a690e39b97ee640f8f46029f897cebdd"
 
   url "https://github.com/sabnzbd/sabnzbd/releases/download/#{version}/SABnzbd-#{version}-osx.dmg",
       verified: "github.com/sabnzbd/sabnzbd/"
