66c82a4a02a77702be7d3d0269bb1cf36335865a
Michael Cho <cho-m@tuta.io>
2021-06-10 16:42:24 -0700

    Update openphone from 3.1.7 to 3.2.6 (#107064)

--- Casks/openphone.rb
+++ Casks/openphone.rb
@@ -1,6 +1,6 @@
 cask "openphone" do
-  version "3.1.7"
-  sha256 "8466b4730490f17c91db9b4fb2ac3d6640e5c8414b674ccdbd9d78ebf468b2b1"
+  version "3.2.6"
+  sha256 "7ac63d6c96aef51761a29f72969db75db5f93d03c1864b1e75b0fc670cb7bb96"
 
   url "https://download.openphone.co/OpenPhone-#{version}.dmg"
   name "OpenPhone"
