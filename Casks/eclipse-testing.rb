30d987a17850e4e6c5a3f8bcf10017380983b190
Miccal Matthews <miccal.matthews@gmail.com>
2021-06-10 08:31:59 +0800

    Update eclipse-testing.rb

--- Casks/eclipse-testing.rb
+++ Casks/eclipse-testing.rb
@@ -4,7 +4,12 @@ cask "eclipse-testing" do
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-testing-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name "Eclipse for Testers"
+  name "Eclipse IDE for testing purposes"
   homepage "https://eclipse.org/"
+
+  livecheck do
+    skip "Discontinued"
+  end
 
   # Renamed to avoid conflict with other Eclipse.
   app "Eclipse.app", target: "Eclipse Testing.app"
