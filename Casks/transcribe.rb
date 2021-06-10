16a7f6407da9c5b8c974c2afd92e9aa363c4788f
Michael Cho <cho-m@tuta.io>
2021-06-10 16:53:30 -0700

    Update transcribe from 9.00 to 9.10 (#107044)

--- Casks/transcribe.rb
+++ Casks/transcribe.rb
@@ -2,22 +2,26 @@ cask "transcribe" do
   if MacOS.version <= :catalina
     version "8.75.2"
     sha256 "f01781100cd3b9987c8f8892145a2eaa358df07b92e10e26f30b6a877f5b352c"
+
     url "https://www.seventhstring.com/xscribe/downmo/transcribe#{version.no_dots}.dmg"
 
     livecheck do
       url "https://www.seventhstring.com/xscribe/download_mac.html"
-      strategy :page_match
-      regex(/version\s*(\d+(?:\.\d+)*)\s*for\s*Mac\s*OS\s*10/i)
+      regex(/version\s*(\d+(?:\.\d+)+)\s*for\s*Mac\s*OS\s*10/i)
     end
   else
-    version "9.00"
+    version "9.10"
     sha256 :no_check
-    url "https://www.seventhstring.com/xscribe/transcribe.dmg"
+
+    if Hardware::CPU.intel?
+      url "https://www.seventhstring.com/xscribe/transcribe.dmg"
+    else
+      url "https://www.seventhstring.com/xscribe/transcribe_arm.dmg"
+    end
 
     livecheck do
       url "https://www.seventhstring.com/xscribe/download_mac.html"
-      strategy :page_match
-      regex(/version\s*(\d+(?:\.\d+)*)\s*for\s*Mac\s*OS\s*11/i)
+      regex(/version\s*(\d+(?:\.\d+)+)\s*for\s*Mac\s*OS\s*11/i)
     end
   end
 
