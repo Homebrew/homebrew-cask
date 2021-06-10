52fd086cda16addfdc1f3d72c86ce4b2a11eae75
Michael Cho <cho-m@tuta.io>
2021-06-10 16:05:50 -0700

    Update mutesync from 4.3.0 to 4.4.2

--- Casks/mutesync.rb
+++ Casks/mutesync.rb
@@ -1,6 +1,6 @@
 cask "mutesync" do
-  version "4.3.0"
-  sha256 "d4b0b0e5510bd1326993b3c514c911cad502b839504ab683ce6240dcfa764cc4"
+  version "4.4.2"
+  sha256 "01f065e50df7843e31fd1fa2d71db35c45ba01eda6dd5d26df4152bdcdb21937"
 
   url "https://mutesync.s3-us-west-2.amazonaws.com/mutesync-#{version}.dmg",
       verified: "mutesync.s3-us-west-2.amazonaws.com/"
