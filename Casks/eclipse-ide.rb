36f7b82b4798d092ec81141a21dd1b9a1498a459
Miccal Matthews <miccal.matthews@gmail.com>
2021-06-10 21:31:09 +0800

    Update eclipse-ide.rb

--- Casks/eclipse-ide.rb
+++ Casks/eclipse-ide.rb
@@ -4,7 +4,7 @@ cask "eclipse-ide" do
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-committers-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name "Eclipse IDE for Eclipse Committers"
-  desc "Eclipse IDE for committers"
+  desc "Eclipse integrated development environment"
   homepage "https://eclipse.org/"
 
   livecheck do
