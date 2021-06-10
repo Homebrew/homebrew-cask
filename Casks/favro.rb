a0e0d4f34226f7cd10417dbe279e802954495952
Michael Cho <cho-m@tuta.io>
2021-06-10 16:39:51 -0700

    Update favro from 1.0.49 to 1.0.59

--- Casks/favro.rb
+++ Casks/favro.rb
@@ -1,6 +1,6 @@
 cask "favro" do
-  version "1.0.49"
-  sha256 "3429dd7bdfb5aa652e73d8a8caa365a4644f2db9b3eb13eebb6af5259aa03a7d"
+  version "1.0.59"
+  sha256 "676d53cf53c1310addd268b4d80b143b533eec937043e03ab672e5ce0eb7d50e"
 
   url "https://download.favro.com/FavroDesktop/macOS/x64/Favro-#{version}.dmg"
   name "Favro"
