072fc33aa6417b9625434b8f6d053f1edd118f16
Miccal Matthews <miccal.matthews@gmail.com>
2021-06-10 08:40:26 +0800

    Update universal-battle.rb

--- Casks/universal-battle.rb
+++ Casks/universal-battle.rb
@@ -1,11 +1,16 @@
 cask "universal-battle" do
-  version :latest
+  version "1.4.1"
   sha256 :no_check
 
   url "https://universalbattle2.com/builds/Mac.zip"
   name "universal-battle"
   desc "Realtime Multiplayer TableTop War Game"
   homepage "https://universalbattle2.com/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   app "OSX/Universal Battle 2.app"
 end
