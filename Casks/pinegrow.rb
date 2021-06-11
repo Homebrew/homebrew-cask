500dc5f9bc5c90002f8dc2fb74773b9e08f744a8
Michael Cho <cho-m@tuta.io>
2021-06-11 15:12:00 -0700

    Update pinegrow from 5.992 to 6 (#107131)

--- Casks/pinegrow.rb
+++ Casks/pinegrow.rb
@@ -1,15 +1,15 @@
 cask "pinegrow" do
-  version "5.992"
-  sha256 "b4d932df613af141926cebb44fa5c54e0fe1ed0be95caa09026c555a0433a234"
+  version "6"
+  sha256 "e432483af91feab9be769229c0487a9acd7fd122590c5687e6adcadb7e9fc7bf"
 
   url "https://download.pinegrow.com/PinegrowMac.#{version}.dmg"
   name "Pinegrow"
+  desc "Web editor"
   homepage "https://pinegrow.com/"
 
   livecheck do
     url :homepage
-    strategy :page_match
-    regex(%r{href=.*?/PinegrowMac\.(\d+(?:\.\d+)*)\.dmg}i)
+    regex(%r{href=.*?/PinegrowMac[._-]v?(\d+(?:\.\d+)*)\.dmg}i)
   end
 
   app "Pinegrow.app"
