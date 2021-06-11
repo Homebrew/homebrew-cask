bfb7c0044bc78eaad4d74a797a6221b1237d6ca7
Michael Cho <cho-m@tuta.io>
2021-06-11 11:51:27 -0700

    Update operadriver from 90.0.4430.85 to 91.0.4472.77

--- Casks/operadriver.rb
+++ Casks/operadriver.rb
@@ -1,6 +1,6 @@
 cask "operadriver" do
-  version "90.0.4430.85"
-  sha256 "6d4c0c0e19a9b361e114dbd854024b61bd852708850c7b582c5dc19248f4a200"
+  version "91.0.4472.77"
+  sha256 "46d0c450bffec9b3912ee0233e3c4118277b492153e47c2f29705ddb23940436"
 
   url "https://github.com/operasoftware/operachromiumdriver/releases/download/v.#{version}/operadriver_mac64.zip"
   name "operachromiumdriver"
