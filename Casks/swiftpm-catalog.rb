87680eeb7a422144cf006e413a78030b1c4c633e
Michael Cho <cho-m@tuta.io>
2021-06-10 20:22:45 -0700

    Update swiftpm-catalog from 1.1.4,22 to 1.1.6,26 (#107076)
    * Update swiftpm-catalog from 1.1.4,22 to 1.1.6,26
    
    * Update swiftpm-catalog.rb

--- Casks/swiftpm-catalog.rb
+++ Casks/swiftpm-catalog.rb
@@ -1,16 +1,18 @@
 cask "swiftpm-catalog" do
-  version "1.1.4,22"
-  sha256 "b1434d32eca36c664a5b75e5f8601f0a53e02d86eec9b25f25b475618aa5c9e9"
+  version "1.1.6,26"
+  sha256 "b2f9af6967aa9460ced11be7d70206e0c3459648cca98623bbc8620fd525c6eb"
 
   url "https://zeezide.com/en/products/swiftpmcatalog/archives/SwiftPM%20Catalog.app-#{version.before_comma}-v#{version.after_comma}.zip"
   name "SwiftPM Catalog"
+  desc "Browse and search for Swift Package Manager packages"
   homepage "https://zeezide.com/en/products/swiftpmcatalog/"
 
   livecheck do
-    url "https://zeezide.com/en/products/swiftpmcatalog/"
+    url :homepage
     strategy :page_match do |page|
-      match = page.match(%r{href=.*?/SwiftPM\s*Catalog\.app-(\d+(?:\.\d+)*)-v(\d+)\.zip}i)
-      "#{match[1]},#{match[2]}"
+      page.scan(%r{href=.*?/SwiftPM\s*Catalog\.app[._-](\d+(?:\.\d+)+)[_-]v?(\d+)\.zip}i).map do |match|
+        "#{match[0]},#{match[1]}"
+      end
     end
   end
 
