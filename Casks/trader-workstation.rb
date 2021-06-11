694b63836470720ebe6ebc7af9ee298736e3917a
Michael Cho <cho-m@tuta.io>
2021-06-11 10:23:46 -0700

    Update trader-workstation.rb

--- Casks/trader-workstation.rb
+++ Casks/trader-workstation.rb
@@ -7,6 +7,11 @@ cask "trader-workstation" do
   desc "Trading software"
   homepage "https://www.interactivebrokers.com/"
 
+  livecheck do
+    url "https://download2.interactivebrokers.com/installers/tws/stable-standalone/version.json"
+    regex(/"buildVersion"\s*:\s*"(\d+(?:\.\d+)+[a-z]*)"/i)
+  end
+
   installer script: {
     executable: "#{staged_path}/Trader Workstation #{version.major} Installer.app/Contents/MacOS/JavaApplicationStub",
     args:       ["-q"],
@@ -14,7 +19,7 @@ cask "trader-workstation" do
 
   uninstall quit:   "com.install4j.5889-6375-8446-2021.22",
             script: {
-              executable: "#{ENV["HOME"]}/Applications/Trader Workstation #{version.major}/Trader Workstation #{version.major} Uninstaller.app/Contents/MacOS/JavaApplicationStub",
+              executable: "/Applications/Trader Workstation #{version.major}/Trader Workstation #{version.major} Uninstaller.app/Contents/MacOS/JavaApplicationStub",
               args:       ["-q"],
             }
 end
