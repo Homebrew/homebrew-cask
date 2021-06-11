39e9356d01c3daa36375165551d0801c7bb4b19e
Michael Cho <cho-m@tuta.io>
2021-06-10 20:21:28 -0700

    Update iina-plus from 0.5.16,21051222 to 0.5.17,21060717 (#107082)

--- Casks/iina-plus.rb
+++ Casks/iina-plus.rb
@@ -1,6 +1,6 @@
 cask "iina-plus" do
-  version "0.5.16,21051222"
-  sha256 "f7e5a3f8d0b0061f03b836f327d30cf9f3c84720ed6a3c3193e96cdfe24495a0"
+  version "0.5.17,21060717"
+  sha256 "214722fbe54f757443c1bea50b37b131e250c4d1696248244066c7df6b5616d5"
 
   url "https://github.com/xjbeta/iina-plus/releases/download/#{version.before_comma}(#{version.after_comma})/iina+.#{version.before_comma}.dmg"
   name "IINA+"
