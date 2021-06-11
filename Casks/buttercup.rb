f4864693856c7eeaadd099d5396f6bd48c4ec171
Michael Cho <cho-m@tuta.io>
2021-06-10 18:40:32 -0700

    Update buttercup from 2.8.0 to 2.8.3

--- Casks/buttercup.rb
+++ Casks/buttercup.rb
@@ -1,6 +1,6 @@
 cask "buttercup" do
-  version "2.8.0"
-  sha256 "2a407a094eb21bc7a8304a41883a033bec3e470eecff8965895957d8fac71c3a"
+  version "2.8.3"
+  sha256 "97ed5aba19e3d4c21ce089723213896febe618e344c20966ef81ed5f68de3cc9"
 
   url "https://github.com/buttercup/buttercup-desktop/releases/download/v#{version}/Buttercup-mac-x64-#{version}.dmg",
       verified: "github.com/buttercup/buttercup-desktop/"
