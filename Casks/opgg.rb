2bd037971c17a062ac71549a6ab19e372d6ea626
Michael Cho <cho-m@tuta.io>
2021-06-11 15:14:01 -0700

    Update opgg from 0.1.68 to 0.1.71 (#107122)

--- Casks/opgg.rb
+++ Casks/opgg.rb
@@ -1,6 +1,6 @@
 cask "opgg" do
-  version "0.1.68"
-  sha256 "051e29fcb6d76e135f26f73b008f5098e2b5428c2322479691823ff5f4838342"
+  version "0.1.71"
+  sha256 "4ef1fd3501a2ad805754fa0bb9b9d63e6551d10236200ee6b796ea317003e98f"
 
   url "https://opgg-desktop-patch.akamaized.net/OP.GG-#{version}.dmg",
       verified: "opgg-desktop-patch.akamaized.net/"
