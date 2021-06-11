307361b848de37824ba18069a0908edfece75eee
Michael Cho <cho-m@tuta.io>
2021-06-10 18:13:59 -0700

    Update swiftpm-catalog.rb

--- Casks/swiftpm-catalog.rb
+++ Casks/swiftpm-catalog.rb
@@ -4,13 +4,15 @@ cask "swiftpm-catalog" do
 
   url "https://zeezide.com/en/products/swiftpmcatalog/archives/SwiftPM%20Catalog.app-#{version.before_comma}-v#{version.after_comma}.zip"
   name "SwiftPM Catalog"
+  desc "Browse and search for Swift Package Manager packages"
   homepage "https://zeezide.com/en/products/swiftpmcatalog/"
 
   livecheck do
-    url "https://zeezide.com/en/products/swiftpmcatalog/"
+    url :homepage
     strategy :page_match do |page|
-      match = page.match(%r{href=.*?/SwiftPM\s*Catalog\.app-(\d+(?:\.\d+)*)-v(\d+)\.zip}i)
-      "#{match[1]},#{match[2]}"
+      page.scan(%r{href=.*?/SwiftPM\s*Catalog\.app[._-](\d+(?:\.\d+)+)[_-]v?(\d+)\.zip}i).map do |match|
+        "#{match[0]},#{match[1]}"
+      end
     end
   end
 
