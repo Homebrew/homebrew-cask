80bcbee474356dc1511bb0661169fc01df28797f
Michael Cho <cho-m@tuta.io>
2021-06-10 21:56:43 -0700

    Update zoc.rb

--- Casks/zoc.rb
+++ Casks/zoc.rb
@@ -3,10 +3,14 @@ cask "zoc" do
   sha256 "a456adb427a6ae04cf523bc23f7300fc2ccbbf5610e0fef52678a6b664e6b9e3"
 
   url "https://www.emtec.com/downloads/zoc/zoc#{version.no_dots}.dmg"
-  appcast "https://www.emtec.com/downloads/zoc/zoc_changes.txt"
   name "ZOC"
   desc "Professional SSH client and terminal emulator"
   homepage "https://www.emtec.com/zoc/"
+
+  livecheck do
+    url "https://www.emtec.com/downloads/zoc/zoc_changes.txt"
+    regex(/\*\s*VERSION\s*(\d+(?:\.\d+)+)/i)
+  end
 
   app "zoc#{version.major}.app"
 end
