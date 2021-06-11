9bfc188a02b7e9260dd3a11a239d24ce9c85ec0d
Michael Cho <cho-m@tuta.io>
2021-06-11 11:52:37 -0700

    Update operadriver.rb

--- Casks/operadriver.rb
+++ Casks/operadriver.rb
@@ -3,14 +3,13 @@ cask "operadriver" do
   sha256 "46d0c450bffec9b3912ee0233e3c4118277b492153e47c2f29705ddb23940436"
 
   url "https://github.com/operasoftware/operachromiumdriver/releases/download/v.#{version}/operadriver_mac64.zip"
-  name "operachromiumdriver"
+  name "OperaChromiumDriver"
   desc "Driver for Chromium-based Opera releases"
   homepage "https://github.com/operasoftware/operachromiumdriver"
 
   livecheck do
     url :url
-    strategy :git
-    regex(/^v?\.?(\d+(?:\.\d+)*)$/i)
+    regex(/^v?\.?(\d+(?:\.\d+)+)$/i)
   end
 
   binary "operadriver_mac64/operadriver"
