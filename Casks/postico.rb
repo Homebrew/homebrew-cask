df5b496f279bbc131f979313c3801fbc70aaecab
Michael Cho <cho-m@tuta.io>
2021-06-11 12:07:12 -0700

    Update postico from 1.5.18 to 1.5.19

--- Casks/postico.rb
+++ Casks/postico.rb
@@ -1,6 +1,6 @@
 cask "postico" do
-  version "1.5.18"
-  sha256 "5d60a4f69885c84f72c276b37917ab02524900e38fff341689c40797d82da95a"
+  version "1.5.19"
+  sha256 "68e4a446d789e2cadb2679f18938b134bbda05b0c5a69c44624cab379a78f067"
 
   url "https://eggerapps-downloads.s3.amazonaws.com/postico-#{version}.zip",
       verified: "eggerapps-downloads.s3.amazonaws.com/"
