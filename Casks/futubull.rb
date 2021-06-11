15fff023e3278f70583017d3d52a06e923144dcb
Michael Cho <cho-m@tuta.io>
2021-06-11 15:11:04 -0700

    Update futubull from 11.6.1108,202105270957 to 11.6.1118,202106071222 (#107135)
    * Update futubull from 11.6.1108,202105270957 to 11.6.1118,202106071222
    
    * Update futubull.rb

--- Casks/futubull.rb
+++ Casks/futubull.rb
@@ -1,6 +1,6 @@
 cask "futubull" do
-  version "11.6.1108,202105270957"
-  sha256 "e9ade6f9874ff2548d1fc2de3ec2cfdf6eb76700554a48bb78ff1c29f012a058"
+  version "11.6.1118,202106071222"
+  sha256 "8dd9fbeb950ac7862409be147e9549607bee1ae642ccf6f679b2c0554bc677c3"
 
   url "https://softwarefile.futunn.com/FTNNForMac_#{version.before_comma}_#{version.after_comma}_Website.dmg"
   name "Futubull"
@@ -11,8 +11,9 @@ cask "futubull" do
   livecheck do
     url "https://www.futunn.com/download/history?client=11"
     strategy :page_match do |page|
-      match = page.match(%r{/FTNNForMac_(\d+(?:\.\d+)*)_(\d+)_Website\.dmg}i)
-      "#{match[1]},#{match[2]}"
+      page.scan(%r{/FTNNForMac[._-]v?(\d+(?:\.\d+)*)[_-](\d+)[._-]Website\.dmg}i).map do |match|
+        "#{match[0]},#{match[1]}"
+      end
     end
   end
 
