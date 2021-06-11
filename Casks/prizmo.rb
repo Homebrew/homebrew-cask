5d46232f76ee42cbfb5fa71834ed62e9fab5d526
Michael Cho <cho-m@tuta.io>
2021-06-11 12:11:01 -0700

    Update prizmo from 4.2,4.277.1371 to 4.2.1,4.277.1372

--- Casks/prizmo.rb
+++ Casks/prizmo.rb
@@ -1,6 +1,6 @@
 cask "prizmo" do
-  version "4.2,4.277.1371"
-  sha256 "32ca7151fa58b77f1defa547af9c752bc9b94f9444756ec83bace700f86be7c8"
+  version "4.2.1,4.277.1372"
+  sha256 "2666e4b0818e9383dd0ab1a721444021d116389d530808170e032fb43eea324e"
 
   url "https://creaceed.s3.amazonaws.com/downloads/prizmo#{version.major}_#{version.before_comma}.zip",
       verified: "creaceed.s3.amazonaws.com/downloads/"
