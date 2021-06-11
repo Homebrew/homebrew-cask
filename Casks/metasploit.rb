2151f848721d380550b3c15c81387359d8419e20
Michael Cho <cho-m@tuta.io>
2021-06-11 15:09:48 -0700

    Update metasploit from 6.0.48,20210607102836 to 6.0.49,20210611102745 (#107141)

--- Casks/metasploit.rb
+++ Casks/metasploit.rb
@@ -1,6 +1,6 @@
 cask "metasploit" do
-  version "6.0.48,20210607102836"
-  sha256 "5710b57628b842b45fde3aec31f863394d72a4a1a497337a043e2a6b33fd7166"
+  version "6.0.49,20210611102745"
+  sha256 "29ad84270bbe3da6289201e7ff92f109371000fbc2b4ce100b7127e61bf4f200"
 
   url "https://osx.metasploit.com/metasploit-framework-#{version.before_comma}%2B#{version.after_comma}-1rapid7-1.pkg"
   name "Metasploit Framework"
