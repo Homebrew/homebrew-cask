2b226dcabfa006e880503b060c2381282b615539
Michael Cho <cho-m@tuta.io>
2021-06-11 15:14:41 -0700

    Update opera from 76.0.4017.177 to 77.0.4054.64 (#107119)
    * Update opera from 76.0.4017.177 to 77.0.4054.64
    
    * Update opera.rb

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask "opera" do
-  version "76.0.4017.177"
-  sha256 "924bf843a520a0df406289de8994773d503585bfc6ea93c80c9856920c5b2525"
+  version "77.0.4054.64"
+  sha256 "a46ebcf850b7c38922cb7c1aa8122f8b8324f1c83d300049c34354aff4bda50e"
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   name "Opera"
@@ -9,8 +9,7 @@ cask "opera" do
 
   livecheck do
     url "https://ftp.opera.com/pub/opera/desktop/"
-    strategy :page_match
-    regex(/href=['"]?(\d+(?:\.\d+)*)['"]?/i)
+    regex(%r{href=["']?v?(\d+(?:\.\d+)+)/?["' >]}i)
   end
 
   auto_updates true
@@ -18,7 +17,7 @@ cask "opera" do
   app "Opera.app"
 
   zap trash: [
-    "~/Library/Preferences/com.operasoftware.Opera.plist",
     "~/Library/Application Support/com.operasoftware.Opera/",
+    "~/Library/Preferences/com.operasoftware.Opera.plist",
   ]
 end
