f70591f8a77325f3c0e198faeecb903a69035053
Michael Cho <cho-m@tuta.io>
2021-06-11 23:44:07 -0700

    Update spyder from 5.0.3 to 5.0.4 (#107154)
    * Update spyder from 5.0.3 to 5.0.4
    
    * Update spyder.rb

--- Casks/spyder.rb
+++ Casks/spyder.rb
@@ -1,6 +1,6 @@
 cask "spyder" do
-  version "5.0.3"
-  sha256 "6805efe4b6c7bb6f30e7bdc5f8cebc5cae4a318a566cd38bb79e02ca55d91fa4"
+  version "5.0.4"
+  sha256 "ef68a75a342e21b91c071a923077a5f3841728ae71568ae9680536b55c5730ff"
 
   url "https://github.com/spyder-ide/spyder/releases/download/v#{version}/Spyder.dmg",
       verified: "github.com/spyder-ide/spyder/"
@@ -12,6 +12,8 @@ cask "spyder" do
     url :url
     strategy :github_latest
   end
+
+  depends_on macos: ">= :catalina"
 
   app "Spyder.app"
 end
