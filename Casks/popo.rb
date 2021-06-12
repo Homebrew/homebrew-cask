ebd60802dcabc19a5feb63add4a4d342337dcf88
Michael Cho <cho-m@tuta.io>
2021-06-11 23:42:52 -0700

    Update popo from 3.23.0 to 3.25.9 (#107158)
    * Update popo from 3.23.0 to 3.25.9
    
    * Update popo.rb

--- Casks/popo.rb
+++ Casks/popo.rb
@@ -1,6 +1,6 @@
 cask "popo" do
-  version "3.23.0"
-  sha256 "bc7517758ea1512ed348a78c2a05817361183fc3df231b1eebd342b7ac770675"
+  version "3.25.9"
+  sha256 "075ced3a47ff45b5d84b054d8b06918c26b597a086281077f681df184843db3c"
 
   url "https://popo.netease.com/file/popomac/POPO_Mac_V#{version.dots_to_underscores}.dmg"
   name "NetEase POPO"
@@ -9,16 +9,15 @@ cask "popo" do
 
   livecheck do
     url "http://http.popo.netease.com:8080/api/open/jsonp/check_version?device=4"
-    strategy :page_match
-    regex(/"version"\s*:\s*"(\d+(?:\.\d+)*)"/i)
+    regex(/"version"\s*:\s*"(\d+(?:\.\d+)+)"/i)
   end
 
   app "popo_mac.app"
 
   zap trash: [
     "~/Library/Application Support/Netease/Popo",
-    "~/Library/Saved Application State/com.netease.game.popo.savedState",
-    "~/Library/Preferences/com.netease.game.popo.plist",
     "~/Library/Caches/com.netease.game.popo",
+    "~/Library/Preferences/com.netease.game.popo.plist",
+    "~/Library/Saved Application State/com.netease.game.popo.savedState",
   ]
 end
