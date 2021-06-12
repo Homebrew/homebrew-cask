1222f01a1feeacbd196d719590e87a7cbd5f368d
Michael Cho <cho-m@tuta.io>
2021-06-12 10:40:10 -0700

    Update pushplaylabs-sidekick from 90.9.0.9578,fa50ce2 to 90.9.1.10107

--- Casks/pushplaylabs-sidekick.rb
+++ Casks/pushplaylabs-sidekick.rb
@@ -1,19 +1,35 @@
 cask "pushplaylabs-sidekick" do
-  version "90.9.1.9578,ba50ce2"
-  sha256 "02692b3456e75b601fc6a0dd9ffc4540cfe2db9eebc7228b5c93e844df507435"
+  if Hardware::CPU.intel?
+    version "90.9.1.10077,3969a7b"
+    sha256 "7bd3c49fdbce7f590ec20e2bbaa1bf17151d3487b27522a18e8611efb5c6a88b"
 
-  url "https://cdn.meetsidekick.com/builds/sidekick-mac-release-x64-#{version.before_comma}-#{version.after_comma}-df.dmg"
+    url "https://sidekick-cdn-production.meetsidekick.com/builds/sidekick-mac-release-x64-#{version.before_comma}-#{version.after_comma}-df.dmg"
+
+    livecheck do
+      url "https://api.meetsidekick.com/downloads/df/mac"
+      strategy :header_match do |headers|
+        match = headers["location"].match(/[_-](\d+(?:\.\d+)+)[_-](\h+)[._-]df\.dmg/i)
+        "#{match[1]},#{match[2]}"
+      end
+    end
+  else
+    version "90.9.1.10107,be53cbe"
+    sha256 "911891036bcd90371b452e45edd6316d400e7901df41a14571b478e9eea9b8b6"
+
+    url "https://sidekick-cdn-production.meetsidekick.com/builds/sidekick-mac-release-universal-#{version.before_comma}-#{version.after_comma}-df.dmg"
+
+    livecheck do
+      url "https://api.meetsidekick.com/downloads/macm1"
+      strategy :header_match do |headers|
+        match = headers["location"].match(/[_-](\d+(?:\.\d+)+)[_-](\h+)[._-]df\.dmg/i)
+        "#{match[1]},#{match[2]}"
+      end
+    end
+  end
+
   name "Sidekick"
   desc "Browser designed for modern work"
   homepage "https://www.meetsidekick.com/"
-
-  livecheck do
-    url "https://api.meetsidekick.com/downloads/df/mac"
-    strategy :header_match do |headers|
-      match = headers["location"].match(/[_-](\d+(?:\.\d+)+)[_-](\h+)[._-]df\.dmg/i)
-      "#{match[1]},#{match[2]}"
-    end
-  end
 
   app "Sidekick.app"
 
