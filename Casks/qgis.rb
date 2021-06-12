3dbcc96fdea00a08797766b8a8a027a59c3c268a
Michael Cho <cho-m@tuta.io>
2021-06-12 10:58:07 -0700

    Update qgis from 3.18.1 to 3.18.3,20210608_104042

--- Casks/qgis.rb
+++ Casks/qgis.rb
@@ -1,8 +1,8 @@
 cask "qgis" do
-  version "3.18.1"
-  sha256 :no_check
+  version "3.18.3,20210608_104042"
+  sha256 "b8b2fc7496716d87886f86762b9a0ebd5e3afef68e029d53338a406f83b1f895"
 
-  url "https://qgis.org/downloads/macos/qgis-macos-pr.dmg"
+  url "https://qgis.org/downloads/macos/pr/qgis_pr_final-#{version.before_comma.dots_to_underscores}_#{version.after_comma}.dmg"
   name "QGIS"
   desc "Geographic Information System"
   homepage "https://www.qgis.org/"
@@ -10,8 +10,8 @@ cask "qgis" do
   livecheck do
     url "https://qgis.org/downloads/macos/qgis-macos-pr.sha256sum"
     strategy :page_match do |page|
-      v = page[/qgis_pr_final-(\d+(?:_\d{,3})*)(?:_\d+)*\.dmg/i, 1]
-      v.tr("_", ".")
+      match = page.match(/qgis_pr_final[._-]v?(\d+(?:_\d+)+)[._-](\d+_\d+)\.dmg/i)
+      "#{match[1].tr("_", ".")},#{match[2]}"
     end
   end
 
