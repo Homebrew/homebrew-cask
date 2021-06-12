9167b4e5c66e12a55be6beb6f05b366d72ee645c
Mykola Rybak <mykola.rybak@gmail.com>
2021-06-12 17:38:08 +0300

    Update logseq from 0.1.6 to 0.1.7

--- Casks/logseq.rb
+++ Casks/logseq.rb
@@ -1,11 +1,11 @@
 cask "logseq" do
-  version "0.1.6"
+  version "0.1.7"
 
   if Hardware::CPU.intel?
-    sha256 "87d04713c306a6baa8230333b07105765349766d75eb15eeb9384aa42467e7ef"
+    sha256 "16de8fbee6414b326902f504d2bf6c4672d41e340ac97d762f5fc64777fbc755"
     url "https://github.com/logseq/logseq/releases/download/#{version}/logseq-darwin-x64-#{version}.dmg"
   else
-    sha256 "c6443e410a278301a24143e0ba51e859679f62dbede11bacf0caa361bbc79fbd"
+    sha256 "5b93edabcbef89aff3a160fde2bb941b15079c2db3c3883340c3fda0097ab379"
     url "https://github.com/logseq/logseq/releases/download/#{version}/logseq-darwin-arm64-#{version}.dmg"
   end
 
