d4ccec18dd63d5a3f1062688e9994b4f9b5cbf26
Michael Cho <cho-m@tuta.io>
2021-06-13 04:09:44 -0700

    Update electron-fiddle from 0.23.1 to 0.23.2 (#107222)

--- Casks/electron-fiddle.rb
+++ Casks/electron-fiddle.rb
@@ -1,13 +1,13 @@
 cask "electron-fiddle" do
-  version "0.23.1"
+  version "0.23.2"
 
   if Hardware::CPU.intel?
-    sha256 "2b3caec785c35000fd9a18f366a0eb29e2d012e8cb368be6515da9400748300f"
+    sha256 "091531c6d2f312b53d09eb9ade794dace7f9413de07eeb64bb8256aea1f11e59"
 
     url "https://github.com/electron/fiddle/releases/download/v#{version}/Electron.Fiddle-darwin-x64-#{version}.zip",
         verified: "github.com/electron/fiddle/"
   else
-    sha256 "f7a940a7fd1dd48c35c85584e44a65520fed18357747569ea8ea51f25c485366"
+    sha256 "0b46b112773913968b2217021be2ab9a764ecce7dfe972c6042d15d768d2011d"
 
     url "https://github.com/electron/fiddle/releases/download/v#{version}/Electron.Fiddle-darwin-arm64-#{version}.zip",
         verified: "github.com/electron/fiddle/"
@@ -15,7 +15,12 @@ cask "electron-fiddle" do
 
   name "Electron Fiddle"
   desc "Easiest way to get started with Electron"
-  homepage "https://electron.atom.io/"
+  homepage "https://www.electronjs.org/fiddle"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "Electron Fiddle.app"
 
