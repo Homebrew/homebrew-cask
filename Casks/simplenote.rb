fe9ede596a9ec18f3dc3b580a6f4f261453c86a2
Stephen Wade <stephen@stephenwade.me>
2021-06-12 22:03:46 -0400

    Update simplenote from 2.11.0 to 2.12.0 (#107200)
    * Update simplenote from 2.11.0 to 2.12.0
    
    * Update simplenote.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/simplenote.rb
+++ Casks/simplenote.rb
@@ -1,11 +1,16 @@
 cask "simplenote" do
-  version "2.11.0"
-  sha256 "4c5f578411974be12409ad00ed918cec2aa5a6d54299baada1db69a906dfc6de"
+  version "2.12.0"
+  sha256 "3b7b9bbccf320d7c3c7f47d038ec3a1af55fc4f9cfb217b6af86e123d3217e4d"
 
   url "https://github.com/Automattic/simplenote-electron/releases/download/v#{version}/Simplenote-macOS-#{version}.dmg"
   name "Simplenote"
   desc "React client for Simplenote"
   homepage "https://github.com/Automattic/simplenote-electron"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "Simplenote.app"
 
