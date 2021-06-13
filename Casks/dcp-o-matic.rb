c39df5212a99777dc5f9cde9441f4dd548690c2a
multiflexi <multiflexi@users.noreply.github.com>
2021-06-13 19:03:28 +0200

    Update of dcp-o-matic from 2.14.47 to 2.14.51

--- Casks/dcp-o-matic.rb
+++ Casks/dcp-o-matic.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic" do
-  version "2.14.47"
-  sha256 "774243e24527bd7bde151dc8d50d438f13659f30813e7b0a15a68c84d5e30a05"
+  version "2.14.51"
+  sha256 "2ef1f03c55af8649bb719704361c99b1d98f44e8ebe2125f9d17d2668f1f3d7d"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.9-main&version=#{version}"
   appcast "https://dcpomatic.com/download"
