56d0c7ddcfa282d6ce6fa0cf9d75fdb5f08f5f82
Michael Cho <cho-m@tuta.io>
2021-06-12 18:43:39 -0700

    Update purei-play from 15c750ad to 2021-06-11,475de030 (#107205)

--- Casks/purei-play.rb
+++ Casks/purei-play.rb
@@ -1,17 +1,19 @@
 cask "purei-play" do
-  version "15c750ad"
-  sha256 :no_check
+  version "2021-06-11,475de030"
+  sha256 "4d67383355763da058495bffe93c653251b05c76ce1de5b1bc37e6fbe5432064"
 
-  url "https://s3.us-east-2.amazonaws.com/playbuilds/#{version}/Play.dmg",
+  url "https://s3.us-east-2.amazonaws.com/playbuilds/#{version.after_comma}/Play.dmg",
       verified: "https://s3.us-east-2.amazonaws.com/playbuilds/"
   name "Play!"
   desc "PlayStation 2 emulator"
   homepage "https://purei.org/"
 
   livecheck do
-    url "https://purei.org/download_latest.php?platform=macos"
-    strategy :header_match
-    regex(%r{([0-9a-f]+)/Play\.dmg}i)
+    url "https://services.purei.org/api/builds"
+    strategy :page_match do |page|
+      build = JSON.parse(page)
+      "#{build["commitDate"][/^(\d+(?:-\d+)+)T/i, 1]},#{build["commitHash"]}"
+    end
   end
 
   app "Play.app"
