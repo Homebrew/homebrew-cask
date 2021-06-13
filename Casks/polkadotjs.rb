bce480a5b06720eb68271be7cda90abcc1964b08
Augusto Elesbão <aelesbao@gmail.com>
2021-06-13 21:28:02 +0200

    Add Polkadot-JS Apps v0.92.3

---
+++ Casks/polkadotjs.rb
@@ -0,0 +1,12 @@
+cask "polkadotjs" do
+  version "0.92.3"
+  sha256 "9053695edd6bb29dcd36871db06487cdd8e76e964cadc192f989f9763c726b83"
+
+  url "https://github.com/polkadot-js/apps/releases/download/v#{version}/Polkadot-JS-Apps-mac-#{version}.dmg",
+      verified: "github.com/polkadot-js/apps/"
+  name "polkadot{.js}"
+  desc "Portal into the Polkadot and Substrate networks"
+  homepage "https://polkadot.js.org/"
+
+  app "Polkadot-JS Apps.app"
+end
