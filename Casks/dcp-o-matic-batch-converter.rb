426bd4e7ab4245c09e61914235e3bc9111246c7b
multiflexi <multiflexi@users.noreply.github.com>
2021-06-13 18:57:29 +0200

    Update dcp-o-matic-batch-converter from 2.14.47 to 2.14.51

--- Casks/dcp-o-matic-batch-converter.rb
+++ Casks/dcp-o-matic-batch-converter.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-batch-converter" do
-  version "2.14.47"
-  sha256 "a7ff1fe7a27584c31a67439b16d0f119786357c26873bf52adfdc528075cc58f"
+  version "2.14.51"
+  sha256 "62c4f131986a0be0875b7bdd0bec187b34bd7139e03f44bb634189045d292435"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.9-batch&version=#{version}"
   appcast "https://dcpomatic.com/download"
