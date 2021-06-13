05815c054af9d356604708fce2fdf6cca0fa28a7
Miccal Matthews <miccal.matthews@gmail.com>
2021-06-13 10:08:53 +0800

    Update time-out.rb

--- Casks/time-out.rb
+++ Casks/time-out.rb
@@ -3,10 +3,14 @@ cask "time-out" do
   sha256 "e72d1f57e5e60f133e27b03543c2ebf5c7bef7317cbfe869c4783952840f4d1d"
 
   url "https://www.dejal.com/download/timeout-#{version}.zip"
-  appcast "https://dejal.net/appcast/?prod=timeout&aed=direct&from=2037&rel=gen"
   name "Time Out"
   desc "Customizable timing of breaks"
   homepage "https://www.dejal.com/timeout/"
+
+  livecheck do
+    url "https://dejal.net/appcast/?prod=timeout"
+    strategy :sparkle, &:short_version
+  end
 
   app "Time Out.app"
 end
