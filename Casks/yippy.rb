2b856fceb8740490266681f661815a8dcce4cfce
Matthew Davidson <mad1512@optusnet.com.au>
2021-06-13 17:45:23 +1000

    Update yippy from 2.5.0 to 2.6.0

--- Casks/yippy.rb
+++ Casks/yippy.rb
@@ -1,12 +1,17 @@
 cask "yippy" do
-  version "2.5.0"
-  sha256 "39b59894324e9e4a986dc076d925a9449cf35edcf0e84fc7d57557d0c5e87318"
+  version "2.6.0"
+  sha256 "6beb1020993074114941732b8c0db911776d1f66752ddbfb33118eb26a74672b"
 
   url "https://github.com/mattDavo/Yippy/releases/download/#{version}/Yippy.zip",
       verified: "github.com/mattDavo/Yippy/"
   name "Yippy"
   desc "Open source clipboard manager"
   homepage "https://yippy.mattdavo.com/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "Yippy.app"
 
