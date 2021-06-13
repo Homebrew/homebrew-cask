1f61e750be062d0c14ed2f6986da6af66b84996d
Vincent Guo <vincent.weiguo@gmail.com>
2021-06-14 02:56:56 +0800

    Update wechatwebdevtools from 1.05.2105100 to 1.05.2105170 (#107242)

--- Casks/wechatwebdevtools.rb
+++ Casks/wechatwebdevtools.rb
@@ -1,6 +1,6 @@
 cask "wechatwebdevtools" do
-  version "1.05.2105100"
-  sha256 "cadaf941663783d78fd28c04ff80e2678ad237d288b2ccdd41673914c091c23c"
+  version "1.05.2105170"
+  sha256 "9687e26e63b116ea9b908001406f2f60ddcf313dd0bfda70edbf889077a0549e"
 
   url "https://dldir1.qq.com/WechatWebDev/release/p-ae42ee2cde4d42ee80ac60b35f183a99/wechat_devtools_#{version}.dmg"
   name "wechat web devtools"
