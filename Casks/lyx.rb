5443d91065b248ab1de1d6bf654ffc9abe46adbb
Karan Chawla <55800358+1kc2@users.noreply.github.com>
2021-06-13 15:02:49 +0100

    Add binary artefacts and update zap stanza

--- Casks/lyx.rb
+++ Casks/lyx.rb
@@ -13,10 +13,18 @@ cask "lyx" do
   end
 
   app "LyX.app"
+  binary "#{appdir}/LyX.app/Contents/MacOS/inkscape"
+  binary "#{appdir}/LyX.app/Contents/MacOS/lyx"
+  binary "#{appdir}/LyX.app/Contents/MacOS/lyxclient"
+  binary "#{appdir}/LyX.app/Contents/MacOS/lyxconvert"
+  binary "#{appdir}/LyX.app/Contents/MacOS/lyxeditor"
+  binary "#{appdir}/LyX.app/Contents/MacOS/maxima"
+  binary "#{appdir}/LyX.app/Contents/MacOS/tex2lyx"
 
   zap trash: [
     "~/Library/Application Support/LyX-#{version.major_minor}",
     "~/Library/Preferences/org.lyx.LyX-#{version.major_minor}.plist",
+    "~/Library/Preferences/org.lyx.lyx.plist",
     "~/Library/Saved Application State/org.lyx.lyx.savedState",
   ]
 end
