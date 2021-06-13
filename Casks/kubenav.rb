c7c6b15d761500a53e0774bd5da9df2085a0f949
Thomas Deutsch <thomas@tuxpeople.org>
2021-06-13 03:57:08 +0200

    Update kubenav to 3.6.0 (#107184)
    * Update kubenav to 3.6.0
    
    * Update kubenav.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/kubenav.rb
+++ Casks/kubenav.rb
@@ -1,12 +1,12 @@
 cask "kubenav" do
-  version "3.5.0"
+  version "3.6.0"
 
   if Hardware::CPU.intel?
-    sha256 "500cf31408ed6c1704e3462a307b8f4f76ce25e64dccbd450a3bd8407c6cf06d"
+    sha256 "e2a23c6e10e404edf23c9a009484d1410adab05e262dee6662d546853cc1ad4d"
     url "https://github.com/kubenav/kubenav/releases/download/#{version}/kubenav-darwin-amd64.zip",
         verified: "github.com/kubenav/kubenav/"
   else
-    sha256 "4094c97f26034eae1d8a3caefa038ef72cd4b8f53363d8d71b23885653184c4b"
+    sha256 "6bb490f3eab3a178b8da01740409037a3db3fb44c4e158aada614958e321fd8d"
     url "https://github.com/kubenav/kubenav/releases/download/#{version}/kubenav-darwin-arm64.zip",
         verified: "github.com/kubenav/kubenav/"
   end
