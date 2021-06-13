937182be569f4216a33f63819ed6249837087606
Michael Cho <cho-m@tuta.io>
2021-06-13 00:00:14 -0700

    Update geotag-photos-pro from 1.8.1 to 1.8.2

--- Casks/geotag-photos-pro.rb
+++ Casks/geotag-photos-pro.rb
@@ -1,6 +1,6 @@
 cask "geotag-photos-pro" do
-  version "1.8.1"
-  sha256 "bee962dbbb53c50fe7d0442d4d571168a47808fc15d619c34a1ea6af01253b0d"
+  version "1.8.2"
+  sha256 "d9c89e4562e310749ebc7b9d58dcb4d474a652b9df5dc63781661a24339b54ec"
 
   url "https://github.com/tappytaps/geotag-desktop-app/releases/download/v#{version}/Geotag-Photos-Pro-2-#{version}.dmg",
       verified: "github.com/tappytaps/geotag-desktop-app/"
