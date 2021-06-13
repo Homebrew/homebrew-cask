f2e387d830cfccc7996d6cf698443b8ae235a6f7
Michael Cho <cho-m@tuta.io>
2021-06-13 01:11:26 -0700

    Update atlauncher from 3.4.4.3 to 3.4.4.4 (#107217)

--- Casks/atlauncher.rb
+++ Casks/atlauncher.rb
@@ -1,6 +1,6 @@
 cask "atlauncher" do
-  version "3.4.4.3"
-  sha256 "650bf251d212e392eeec44712265976bd855f6b87795b14ed76e0490c31c56fc"
+  version "3.4.4.4"
+  sha256 "89eaec2084333dbc5b81a7ffc9b7c24699ff872cd09a6faa19cac08511141a74"
 
   url "https://github.com/ATLauncher/ATLauncher/releases/download/v#{version}/ATLauncher-#{version}.zip",
       verified: "github.com/ATLauncher/ATLauncher/"
