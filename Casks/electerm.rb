28f054b7058e8201731a5190e1768a8d1886c78f
Michael Cho <cho-m@tuta.io>
2021-06-12 23:46:17 -0700

    Update electerm from 1.13.1 to 1.13.4

--- Casks/electerm.rb
+++ Casks/electerm.rb
@@ -1,8 +1,8 @@
 cask "electerm" do
-  version "1.13.1"
-  sha256 "621a041f888dde3cab4a5c59a7a1150ac36149bde9321e39a4596f1a852f0739"
+  version "1.13.4"
+  sha256 "1f192bc7570fcd0e8b77b746547cde0afcb23a8d875bbd1212b4fb4ba1321ece"
 
-  url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac.dmg"
+  url "https://github.com/electerm/electerm/releases/download/#{version}/electerm-#{version}-mac.dmg"
   name "electerm"
   desc "Terminal/ssh/serial port/sftp client"
   homepage "https://github.com/electerm/electerm/"
