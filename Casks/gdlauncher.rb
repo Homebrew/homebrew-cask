9d5139607e162861853c0e867083f44d6997c11b
Michael Cho <cho-m@tuta.io>
2021-06-13 01:16:13 -0700

    Update gdlauncher from 1.1.7 to 1.1.10 (#107224)

--- Casks/gdlauncher.rb
+++ Casks/gdlauncher.rb
@@ -1,6 +1,6 @@
 cask "gdlauncher" do
-  version "1.1.7"
-  sha256 "c6f516beef860a5dfc4c707ba85e4943c88b5f7ce6d7d7a9396f0fae978b77aa"
+  version "1.1.10"
+  sha256 "8bc79d7d799715f94e8a662f4a41f27b5d75777677b52a2d5daddfa4c476738c"
 
   url "https://github.com/gorilla-devs/GDLauncher/releases/download/v#{version}/GDLauncher-mac-setup.dmg",
       verified: "github.com/gorilla-devs/GDLauncher/"
