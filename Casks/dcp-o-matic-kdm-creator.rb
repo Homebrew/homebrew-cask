1484456a7386185497cf8f033f695daa1d7768ea
multiflexi <multiflexi@users.noreply.github.com>
2021-06-13 19:01:07 +0200

    Update dcp-o-matic-kdm-creator from 2.14.47 to 2.14.51

--- Casks/dcp-o-matic-kdm-creator.rb
+++ Casks/dcp-o-matic-kdm-creator.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-kdm-creator" do
-  version "2.14.47"
-  sha256 "be0527053c026aad80779b44f3fbb87669b70828628dc64b940ed8322f07264e"
+  version "2.14.51"
+  sha256 "5e93646ea8be33dfdf22a86dbc370436b209ed7f363724b74e15148ce1a79b48"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.9-kdm&version=#{version}"
   appcast "https://dcpomatic.com/download"
