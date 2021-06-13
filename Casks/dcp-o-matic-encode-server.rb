57b58aca45adcc4fe6963bb71c21ab890283a8e4
multiflexi <multiflexi@users.noreply.github.com>
2021-06-13 18:51:55 +0200

    Update dcp-o-matic-encode-server from 2.14.47 to 2.14.51

--- Casks/dcp-o-matic-encode-server.rb
+++ Casks/dcp-o-matic-encode-server.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-encode-server" do
-  version "2.14.47"
-  sha256 "7c6b37a0c327d8b81e43c9ba36f81987e22c34d9e6f912af0ad1a04bcf026227"
+  version "2.14.51"
+  sha256 "7b763121164bf89a15083b197cfdff7e0b86202fa8db6962cf5c7059c4943967"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.9-server&version=#{version}"
   appcast "https://dcpomatic.com/download"
