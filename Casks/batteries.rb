f3ee81eadf78f958388ae909d978e0969c68ac28
Michael Cho <cho-m@tuta.io>
2021-06-13 01:12:09 -0700

    Update batteries from 2.1.2 to 2.1.4 (#107218)

--- Casks/batteries.rb
+++ Casks/batteries.rb
@@ -1,6 +1,6 @@
 cask "batteries" do
-  version "2.1.2"
-  sha256 "402c2c5f088dabe374c2c7a21888e1606e6d1417e08ce6c9af0f91ec31db3452"
+  version "2.1.4"
+  sha256 "9e86a4b037556ad26c5065875d7a879ee52d05ce2fe41f98f460b8dc6d766a81"
 
   url "https://github.com/ronyfadel/BatteriesReleases/releases/download/v#{version}/Batteries.dmg",
       verified: "github.com/ronyfadel/BatteriesReleases/"
@@ -13,12 +13,12 @@ cask "batteries" do
   app "Batteries.app"
 
   zap trash: [
+    "~/Library/Application Scripts/io.fadel.Batteries.BatteriesTodayExtension",
     "~/Library/Caches/io.fadel.Batteries",
     "~/Library/Caches/io.fadel.Batteries.Helper",
     "~/Library/Containers/io.fadel.Batteries.BatteriesTodayExtension",
+    "~/Library/Group Containers/KUC6B4JW25.io.fadel.Batteries",
     "~/Library/Preferences/io.fadel.Batteries.Helper.plist",
     "~/Library/Preferences/io.fadel.Batteries.plist",
-    "~/Library/Group Containers/KUC6B4JW25.io.fadel.Batteries",
-    "~/Library/Application Scripts/io.fadel.Batteries.BatteriesTodayExtension",
   ]
 end
