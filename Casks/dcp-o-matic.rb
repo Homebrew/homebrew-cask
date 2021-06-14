9cf76795ce5f8990073d321fe8c3eb9112217eb9
multiflexi <multiflexi@users.noreply.github.com>
2021-06-14 03:44:47 +0200

    Update of dcp-o-matic from 2.14.47 to 2.14.51 (#107247)
    * Update of dcp-o-matic from 2.14.47 to 2.14.51
    
    * Update dcp-o-matic.rb
    
    * Update dcp-o-matic.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/dcp-o-matic.rb
+++ Casks/dcp-o-matic.rb
@@ -1,11 +1,17 @@
 cask "dcp-o-matic" do
-  version "2.14.47"
-  sha256 "774243e24527bd7bde151dc8d50d438f13659f30813e7b0a15a68c84d5e30a05"
+  version "2.14.51"
+  sha256 "2ef1f03c55af8649bb719704361c99b1d98f44e8ebe2125f9d17d2668f1f3d7d"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.9-main&version=#{version}"
-  appcast "https://dcpomatic.com/download"
   name "DCP-o-matic"
+  desc "Convert video, audio and subtitles into DCP (Digital Cinema Democratized)"
   homepage "https://dcpomatic.com/"
+
+  livecheck do
+    url "https://dcpomatic.com/download"
+    strategy :page_match
+    regex(/stable\s*release:\s*(\d+(?:\.\d+)*)/i)
+  end
 
   app "DCP-o-matic #{version.major}.app"
 end
