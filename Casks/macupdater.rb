ee4ee944f16a5fb2d441a0ef7f671f1b9f608164
Eloy Aldea <eloy.aldea@gmail.com>
2021-06-14 23:23:57 +0200

    Update macupdater from 2.0.4,11630 to 2.0.4,11674

--- Casks/macupdater.rb
+++ Casks/macupdater.rb
@@ -1,6 +1,6 @@
 cask "macupdater" do
-  version "2.0.4,11630"
-  sha256 "0ef2e01d61e9834e038546603ac2eba86ad8e9d7ce01e14c5fac907b78326833"
+  version "2.0.4,11674"
+  sha256 "301f2c7d7ca46aa5b0f5d2255b13510b40a1faeab43764902db3bf137c17bffb"
 
   url "https://www.corecode.io/downloads/macupdater_#{version.before_comma}.dmg"
   name "MacUpdater"
