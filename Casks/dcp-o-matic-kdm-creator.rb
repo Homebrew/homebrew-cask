458771cfec86bd8b76f42147453479a6c4966186
Miccal Matthews <miccal.matthews@gmail.com>
2021-06-14 09:36:04 +0800

    Update dcp-o-matic-kdm-creator.rb

--- Casks/dcp-o-matic-kdm-creator.rb
+++ Casks/dcp-o-matic-kdm-creator.rb
@@ -3,7 +3,6 @@ cask "dcp-o-matic-kdm-creator" do
   sha256 "5e93646ea8be33dfdf22a86dbc370436b209ed7f363724b74e15148ce1a79b48"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.9-kdm&version=#{version}"
-  appcast "https://dcpomatic.com/download"
   name "DCP-o-matic KDM Creator"
   desc "Convert video, audio and subtitles into DCP (Digital Cinema Democratized)"
   homepage "https://dcpomatic.com/"
