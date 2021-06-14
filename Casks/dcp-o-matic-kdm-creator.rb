3e8f8cc80fb5ce5d7d385b1ba37a440d68dd3be5
multiflexi <multiflexi@users.noreply.github.com>
2021-06-14 03:45:41 +0200

    Update dcp-o-matic-kdm-creator from 2.14.47 to 2.14.51 (#107246)
    * Update dcp-o-matic-kdm-creator from 2.14.47 to 2.14.51
    
    * Update dcp-o-matic-kdm-creator.rb
    
    * Update dcp-o-matic-kdm-creator.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/dcp-o-matic-kdm-creator.rb
+++ Casks/dcp-o-matic-kdm-creator.rb
@@ -1,11 +1,17 @@
 cask "dcp-o-matic-kdm-creator" do
-  version "2.14.47"
-  sha256 "be0527053c026aad80779b44f3fbb87669b70828628dc64b940ed8322f07264e"
+  version "2.14.51"
+  sha256 "5e93646ea8be33dfdf22a86dbc370436b209ed7f363724b74e15148ce1a79b48"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.9-kdm&version=#{version}"
-  appcast "https://dcpomatic.com/download"
   name "DCP-o-matic KDM Creator"
+  desc "Convert video, audio and subtitles into DCP (Digital Cinema Democratized)"
   homepage "https://dcpomatic.com/"
+
+  livecheck do
+    url "https://dcpomatic.com/download"
+    strategy :page_match
+    regex(/stable\s*release:\s*(\d+(?:\.\d+)*)/i)
+  end
 
   app "DCP-o-matic #{version.major} KDM Creator.app"
 end
