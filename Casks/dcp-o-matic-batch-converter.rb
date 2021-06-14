6f9e207b3de840feeeddbdae6f3193a090922a2b
Miccal Matthews <miccal.matthews@gmail.com>
2021-06-14 09:25:08 +0800

    Update dcp-o-matic-batch-converter.rb

--- Casks/dcp-o-matic-batch-converter.rb
+++ Casks/dcp-o-matic-batch-converter.rb
@@ -3,9 +3,15 @@ cask "dcp-o-matic-batch-converter" do
   sha256 "62c4f131986a0be0875b7bdd0bec187b34bd7139e03f44bb634189045d292435"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.9-batch&version=#{version}"
-  appcast "https://dcpomatic.com/download"
   name "DCP-o-matic Batch converter"
+  desc "Convert video, audio and subtitles into DCP (Digital Cinema Democratized)"
   homepage "https://dcpomatic.com/"
+
+  livecheck do
+    url "https://dcpomatic.com/download"
+    strategy :page_match
+    regex(/stable\s*release:\s*(\d+(?:\.\d+)*)/i)
+  end
 
   app "DCP-o-matic #{version.major} Batch converter.app"
 end
