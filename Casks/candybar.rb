59563dad804fb4cafa40d86ff517a5589b2df3d5
Sam Ford <1584702+samford@users.noreply.github.com>
2021-06-14 10:53:30 -0400

    candybar: remove livecheck

--- Casks/candybar.rb
+++ Casks/candybar.rb
@@ -7,12 +7,6 @@ cask "candybar" do
   desc "Tool to modify file icons"
   homepage "https://panic.com/blog/candybar-mountain-lion-and-beyond/"
 
-  livecheck do
-    url :homepage
-    strategy :page_match
-    regex(%r{href=.*?/CandyBar%20(\d+(?:\.\d+)*)\.zip}i)
-  end
-
   app "CandyBar.app"
 
   caveats do
