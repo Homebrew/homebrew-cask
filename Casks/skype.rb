2df0243e5b8664291e2a42a614f30f1d7a5832b4
Bevan Kay <bevanjkay@gmail.com>
2021-06-14 21:48:26 +1000

    Update skype from 8.72.0.94 to 8.73.0.92 (#107270)

--- Casks/skype.rb
+++ Casks/skype.rb
@@ -1,6 +1,6 @@
 cask "skype" do
-  version "8.72.0.94"
-  sha256 "43d193ac08ca2c40149b5016b1ae3b7c025a57c4b13a3d2b871b1b65921a0dae"
+  version "8.73.0.92"
+  sha256 "215e6c8c11c42d666f46cc3cf9a2c9c574e0aad2f33c89ba9e51daf074c830da"
 
   url "https://endpoint920510.azureedge.net/s4l/s4l/download/mac/Skype-#{version}.dmg",
       verified: "endpoint920510.azureedge.net/s4l/s4l/download/mac/"
