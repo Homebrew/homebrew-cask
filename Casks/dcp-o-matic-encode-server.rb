cc65811542069984513212c25ee977f8f649a093
Miccal Matthews <miccal.matthews@gmail.com>
2021-06-14 09:24:24 +0800

    Update dcp-o-matic-encode-server.rb

--- Casks/dcp-o-matic-encode-server.rb
+++ Casks/dcp-o-matic-encode-server.rb
@@ -3,10 +3,15 @@ cask "dcp-o-matic-encode-server" do
   sha256 "7b763121164bf89a15083b197cfdff7e0b86202fa8db6962cf5c7059c4943967"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.9-server&version=#{version}"
-  appcast "https://dcpomatic.com/download"
   name "DCP-o-matic Encode Server"
   desc "Convert video, audio and subtitles into DCP (Digital Cinema Democratized)"
   homepage "https://dcpomatic.com/"
+
+  livecheck do
+    url "https://dcpomatic.com/download"
+    strategy :page_match
+    regex(/stable\s*release:\s*(\d+(?:\.\d+)*)/i)
+  end
 
   app "DCP-o-matic #{version.major} Encode Server.app"
 end
