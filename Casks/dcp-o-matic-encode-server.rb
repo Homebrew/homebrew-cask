7a7777dbb097ec2e24d8ac70d5c6f1cd96fbf07d
multiflexi <multiflexi@users.noreply.github.com>
2021-06-14 03:37:10 +0200

    Update dcp-o-matic-encode-server from 2.14.47 to 2.14.51 (#107243)
    * Update dcp-o-matic-encode-server from 2.14.47 to 2.14.51
    
    * Update dcp-o-matic-encode-server.rb
    
    * Update dcp-o-matic-encode-server.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/dcp-o-matic-encode-server.rb
+++ Casks/dcp-o-matic-encode-server.rb
@@ -1,11 +1,17 @@
 cask "dcp-o-matic-encode-server" do
-  version "2.14.47"
-  sha256 "7c6b37a0c327d8b81e43c9ba36f81987e22c34d9e6f912af0ad1a04bcf026227"
+  version "2.14.51"
+  sha256 "7b763121164bf89a15083b197cfdff7e0b86202fa8db6962cf5c7059c4943967"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.9-server&version=#{version}"
-  appcast "https://dcpomatic.com/download"
   name "DCP-o-matic Encode Server"
+  desc "Convert video, audio and subtitles into DCP (Digital Cinema Democratized)"
   homepage "https://dcpomatic.com/"
+
+  livecheck do
+    url "https://dcpomatic.com/download"
+    strategy :page_match
+    regex(/stable\s*release:\s*(\d+(?:\.\d+)*)/i)
+  end
 
   app "DCP-o-matic #{version.major} Encode Server.app"
 end
