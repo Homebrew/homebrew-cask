e4cce0fcddabc5f3cdb4f119b3886e70202adf20
Miccal Matthews <miccal.matthews@gmail.com>
2021-06-14 09:36:23 +0800

    Update dcp-o-matic.rb

--- Casks/dcp-o-matic.rb
+++ Casks/dcp-o-matic.rb
@@ -3,7 +3,6 @@ cask "dcp-o-matic" do
   sha256 "2ef1f03c55af8649bb719704361c99b1d98f44e8ebe2125f9d17d2668f1f3d7d"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.9-main&version=#{version}"
-  appcast "https://dcpomatic.com/download"
   name "DCP-o-matic"
   desc "Convert video, audio and subtitles into DCP (Digital Cinema Democratized)"
   homepage "https://dcpomatic.com/"
