a0e7f3a4531889ffcce126c730036b185d7ab16d
Michael Cho <cho-m@tuta.io>
2021-06-15 00:18:44 -0700

    3cxphone: add livecheck (#107301)

--- Casks/3cxphone.rb
+++ Casks/3cxphone.rb
@@ -4,7 +4,13 @@ cask "3cxphone" do
 
   url "https://downloads.3cx.com/downloads/3CXPhoneForMac#{version}.dmg"
   name "3CXPhone"
+  desc "Office phone system"
   homepage "https://www.3cx.com/"
+
+  livecheck do
+    url "https://www.3cx.com/phone-system/download-links/"
+    regex(/>\s*Download\s+3CX\s+V?(\d+(?:\.\d+)*)\s*</i)
+  end
 
   app "3CXPhone.app"
 end
