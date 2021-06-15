f5110f2efe3a23c76b5a706b84541c83d7d52a5f
chengpanwang <chengpan168@126.com>
2021-06-15 16:00:24 +0800

    zap trash

--- Casks/redis-pro.rb
+++ Casks/redis-pro.rb
@@ -8,5 +8,13 @@ cask "redis-pro" do
   homepage "https://github.com/cmushroom/redis-pro"
 
   app "redis-pro.app"
-end
 
+  zap trash: [
+    "~/Library/Application Scripts/com.cmushroom.redis-pro",
+    "~/Library/Application Support/com.cmushroom.redis-pro",
+    "~/Library/Caches/com.cmushroom.redis-pro",
+    "~/Library/Containers/com.cmushroom.redis-pro",
+    "~/Library/Preferences/com.cmushroom.redis-pro.plist",
+    "~/Library/Saved Application State/com.cmushroom.redis-pro.savedState",
+  ]
+end
