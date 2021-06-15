a27c915203af2caf65923d66860657be36dc39b9
Michael Cho <cho-m@tuta.io>
2021-06-15 00:51:40 -0700

    digikam: add livecheck

--- Casks/digikam.rb
+++ Casks/digikam.rb
@@ -4,9 +4,14 @@ cask "digikam" do
 
   url "https://download.kde.org/stable/digikam/#{version}/digiKam-#{version}-MacOS-x86-64.pkg",
       verified: "kde.org/stable/digikam/"
-  appcast "https://download.kde.org/stable/digikam/"
   name "digiKam"
+  desc "Digital photo manager"
   homepage "https://www.digikam.org/"
+
+  livecheck do
+    url "https://download.kde.org/stable/digikam/"
+    regex(%r{href=["']?v?(\d+(?:\.\d+)+)/?["' >]}i)
+  end
 
   pkg "digiKam-#{version}-MacOS-x86-64.pkg"
 
