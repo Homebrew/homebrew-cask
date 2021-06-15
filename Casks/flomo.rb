06160321d862b87b616544a8d6415f38979a053c
Michael Cho <cho-m@tuta.io>
2021-06-15 12:28:29 -0700

    flomo: fix url and livecheck

--- Casks/flomo.rb
+++ Casks/flomo.rb
@@ -2,17 +2,18 @@ cask "flomo" do
   version "0.1.2"
   sha256 "066377a737908ce164467c67355b0be61c6cb9102fceecd4c116a0c373791394"
 
-  url "https://flomo.oss-cn-shanghai.aliyuncs.com/resource/flomo_v#{version}.zip",
-      verified: "flomo.oss-cn-shanghai.aliyuncs.com/"
+  url "https://flomo-resource.oss-cn-shanghai.aliyuncs.com/flomo_mac_v#{version}.zip",
+      verified: "flomo-resource.oss-cn-shanghai.aliyuncs.com/"
   name "flomo"
   desc "Memo note taking and management app"
   homepage "https://flomoapp.com/"
 
   livecheck do
     url "https://help.flomoapp.com/basic/app"
-    strategy :page_match
-    regex(/flomo_v(\d+(?:\.\d+)*)\.zip/i)
+    regex(%r{/flomo[._-]mac[._-]v?(\d+(?:\.\d+)+)\.zip}i)
   end
+
+  depends_on macos: ">= :catalina"
 
   app "flomo.app"
 
