59d8bf7ecb17753e6058f857dd5caefc7cc74b7a
Michael Cho <cho-m@tuta.io>
2021-06-15 10:23:33 -0700

    Update openboard from 1.6.1 to 1.6.1

--- Casks/openboard.rb
+++ Casks/openboard.rb
@@ -1,6 +1,6 @@
 cask "openboard" do
   version "1.6.1"
-  sha256 "203741c86d50a7ea63f36b9d9cc607d843fb7ee17729c46764f155f47e356b8b"
+  sha256 "feb8cc1a9182df28a422602a683923d40f650c815d089fa6e15eff7880edf38e"
 
   url "https://github.com/OpenBoard-org/OpenBoard/releases/download/v#{version}/OpenBoard-#{version}.dmg",
       verified: "github.com/OpenBoard-org/OpenBoard/"
@@ -16,9 +16,9 @@ cask "openboard" do
   app "OpenBoard.app"
 
   zap trash: [
-    "~/Music/OpenBoard",
-    "~/Movies/OpenBoard",
-    "~/Pictures/OpenBoard",
     "~/Library/Application Support/OpenBoard",
+    "~/Movies/OpenBoard",
+    "~/Music/OpenBoard",
+    "~/Pictures/OpenBoard",
   ]
 end
