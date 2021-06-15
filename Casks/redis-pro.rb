8b6bf09c4103b02a29ff74538318eac49ef17c11
chengpanwang <chengpan168@126.com>
2021-06-15 10:01:06 +0800

    Add redis-pro.app v1.0.0-beta
    Redis management application

---
+++ Casks/redis-pro.rb
@@ -0,0 +1,11 @@
+cask "redis-pro" do
+  version "1.0.0-beta"
+  sha256 "ec5e198b97ce8548f582bd074baed2ad4bd505bc345c5d8c5a1f2af1260154d0"
+
+  url "https://github.com/cmushroom/redis-pro/releases/download/#{version}/redis-pro_#{version}.dmg"
+  name "redis-pro"
+  desc "Redis management application"
+  homepage "https://github.com/cmushroom/redis-pro"
+
+  app "redis-pro.app"
+end
