3094790ccd117877a971115e47b98c9a9094e45f
Michael Cho <cho-m@tuta.io>
2021-06-15 23:21:28 -0700

    Update electron-cash from 4.2.4 to 4.2.4.1 (#107333)

--- Casks/electron-cash.rb
+++ Casks/electron-cash.rb
@@ -1,15 +1,15 @@
 cask "electron-cash" do
-  version "4.2.4"
-  sha256 "ddf5b127eefab76eb733659eefad730728d89c867eead8b4c12b35aeee2bff16"
+  version "4.2.4.1"
+  sha256 "a335f46b6899ba1692ce75cbf4172b835b8ab6b46d38a62863ebaabf34d6570d"
 
-  url "https://electroncash.org/downloads/#{version}/mac/Electron-Cash-#{version}-macosx.dmg"
+  url "https://electroncash.org/downloads/#{version.major_minor_patch}/mac/Electron-Cash-#{version}-macosx.dmg"
   name "Electron Cash"
   desc "Thin client for Bitcoin Cash"
-  homepage "https://www.electroncash.org/"
+  homepage "https://electroncash.org/"
 
   livecheck do
-    url "https://github.com/Electron-Cash/Electron-Cash"
-    strategy :github_latest
+    url :homepage
+    regex(%r{href=.*?/Electron[._-]Cash[._-]v?(\d+(?:\.\d+)+)[._-]macosx\.dmg}i)
   end
 
   app "Electron-Cash.app"
