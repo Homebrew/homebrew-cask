0a91ee9a4e5605c78509055cd588231f0d89cb21
Michael Cho <cho-m@tuta.io>
2021-06-15 23:20:54 -0700

    Update chirp from 20210520 to 20210612 (#107337)

--- Casks/chirp.rb
+++ Casks/chirp.rb
@@ -1,8 +1,8 @@
 cask "chirp" do
-  version "20210520"
-  sha256 "152a25140c8d3dddd725f83f61592d5697ea587ea4061dcda54e9f091c7ec084"
+  version "20210612"
+  sha256 "9e7d554a5c5857df067fb64e145174d351eb4bfc28f00e7dc493f681b3abed19"
 
-  url "https://trac.chirp.danplanet.com/chirp_daily/LATEST/chirp-unified-daily-#{version}.app.zip"
+  url "https://trac.chirp.danplanet.com/chirp_daily/daily-#{version}/chirp-unified-daily-#{version}.app.zip"
   name "CHIRP"
   desc "Tool for programming amateur radio"
   homepage "https://chirp.danplanet.com/projects/chirp/wiki/Home"
