aa5dac7a5eafab3979feeced1b1480648e18f292
Yihe Li <winmikedows@hotmail.com>
2021-06-16 15:48:42 +0800

    Change logisim-evolution to use v3.5.0 .dmg file

--- Casks/logisim-evolution.rb
+++ Casks/logisim-evolution.rb
@@ -1,8 +1,8 @@
 cask "logisim-evolution" do
   version "3.5.0"
-  sha256 "3cabd296b78457a7a9727ef66b1ba706ec4e4beccbbc28519fcc2e591fa9d1e4"
+  sha256 "feea8b0c11c03e08a2bd4cb5def08bb0fb5e3bc494ac178b3e210559b63848d5"
 
-  url "https://github.com/reds-heig/logisim-evolution/releases/download/v#{version}/logisim-evolution-#{version}-all.jar"
+  url "https://github.com/reds-heig/logisim-evolution/releases/download/v#{version}/Logisim-evolution-#{version}.dmg"
   name "Logisim Evolution"
   desc "Digital logic designer and simulator"
   homepage "https://github.com/reds-heig/logisim-evolution"
@@ -12,13 +12,7 @@ cask "logisim-evolution" do
     strategy :github_latest
   end
 
-  container type: :naked
-
-  app "logisim-evolution-#{version}-all.jar", target: "logisim-evolution.jar"
+  app "Logisim-evolution.app"
 
   zap trash: "~/Library/Preferences/com.cburch.logisim.plist"
-
-  caveats do
-    depends_on_java "9+"
-  end
 end
