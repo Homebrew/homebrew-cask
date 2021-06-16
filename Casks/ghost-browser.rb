72d0717f8afa63510c5df5bc9ddf1a460af08d2d
Michael Cho <cho-m@tuta.io>
2021-06-15 22:19:14 -0700

    Update ghost-browser from 2.1.1.19 to 2.1.3.3

--- Casks/ghost-browser.rb
+++ Casks/ghost-browser.rb
@@ -1,12 +1,17 @@
 cask "ghost-browser" do
-  version "2.1.1.19"
-  sha256 "086ed71f1d0315c791651b0f1ea911277b92b3d4237949dee6782a163fbd4562"
+  version "2.1.3.3"
+  sha256 "966367c1f4a8504e28c30d4f6d742c8c4c2e029bebad2303eec668434bb5b991"
 
   url "https://ghostbrowser.s3.amazonaws.com/downloads/GhostBrowser-#{version}.dmg",
       verified: "ghostbrowser.s3.amazonaws.com/"
-  appcast "https://ghostbrowser.s3.amazonaws.com/updates/changelog.html"
   name "Ghost Browser"
+  desc "Web browser"
   homepage "https://ghostbrowser.com/download/"
+
+  livecheck do
+    url "https://ghostbrowser.s3.amazonaws.com/updates/changelog.html"
+    regex(/>\s*Version\s*(\d+(?:\.\d+)+)/i)
+  end
 
   app "Ghost Browser.app"
 end
