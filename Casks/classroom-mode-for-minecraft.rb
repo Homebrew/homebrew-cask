f577a44e9bf77f0efdee07b44a0b17d9855f405a
Michael Cho <cho-m@tuta.io>
2021-06-16 13:43:25 -0700

    Update classroom-mode-for-minecraft.rb

--- Casks/classroom-mode-for-minecraft.rb
+++ Casks/classroom-mode-for-minecraft.rb
@@ -1,8 +1,8 @@
 cask "classroom-mode-for-minecraft" do
-  version "1.81,0"
+  version "1.81"
   sha256 "982eac2c28235d483351760ee17a3a45ca9d8d3f4ada05959e2bda859b0e3538"
 
-  url "https://meedownloads.blob.core.windows.net/retailbuilds/ClassroomMode/mac/Classroom_Mode_#{version.before_comma.dots_to_underscores}.dmg",
+  url "https://meedownloads.blob.core.windows.net/retailbuilds/ClassroomMode/mac/Classroom_Mode_#{version.dots_to_underscores}.dmg",
       verified: "meedownloads.blob.core.windows.net/"
   name "Classroom Mode for Minecraft"
   desc "Classroom management app for Minecraft Education Edition"
@@ -10,14 +10,15 @@ cask "classroom-mode-for-minecraft" do
 
   livecheck do
     url "https://aka.ms/meecmmacos"
-    strategy :extract_plist
+    strategy :header_match do |headers|
+      headers["location"][/Classroom[._-]Mode[._-]v?(\d+(?:_\d+)+)\.dmg/i, 1].tr("_", ".")
+    end
   end
 
   app "Classroom Mode for Minecraft.app"
 
   zap trash: [
     "~/Library/Caches/com.microsoft.mc-classroommode",
-    "~/Library/Preferences/com.apple.classroom.plist",
     "~/Library/WebKit/com.microsoft.mc-classroommode",
   ]
 end
