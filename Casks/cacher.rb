3853c31c03bfa387073e38803e7bc23a88c0922e
Hannes Braun <hannesbraun@mail.de>
2021-06-17 08:03:14 +0200

    Update cacher from 2.41.2 to 2.42.1

--- Casks/cacher.rb
+++ Casks/cacher.rb
@@ -1,6 +1,6 @@
 cask "cacher" do
-  version "2.41.2"
-  sha256 "e498ffeabe75dc45b40afcb6b94298ca42771d0b24f6a0829d77c2d843bf2fa4"
+  version "2.42.1"
+  sha256 "fb09d822305687fb06fc87ac5f1f20b7b120b4460fe18c1428736a3fdd6dac74"
 
   url "https://cacher-download.nyc3.digitaloceanspaces.com/Cacher-#{version}-mac.zip",
       verified: "cacher-download.nyc3.digitaloceanspaces.com/"
