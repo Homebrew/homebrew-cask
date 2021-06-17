b2dd484b608a7db3e3972022788c9894a21d8bc3
Michael Cho <cho-m@tuta.io>
2021-06-16 19:16:53 -0700

    Update understand.rb

--- Casks/understand.rb
+++ Casks/understand.rb
@@ -3,10 +3,14 @@ cask "understand" do
   sha256 "f278bd79a16accbd24ffdfb67e57f4636206ac9e2c3b339f319b285f0cfaa604"
 
   url "http://builds.scitools.com/all_builds/b#{version.patch}/Understand/Understand-#{version}-MacOSX-x86.dmg"
-  appcast "https://licensing.scitools.com/download/getChecksums/Understand/#{version.patch}"
   name "SciTools Understand"
   desc "Code visualization and exploration tool"
   homepage "https://scitools.com/features/"
+
+  livecheck do
+    url "https://licensing.scitools.com/download"
+    regex(/stablefilepaths=["'][^"']*?Understand[._-]v?(\d+(?:\.\d+)+)[._-]MacOSX[._-]x86\.dmg/i)
+  end
 
   app "Understand.app"
 end
