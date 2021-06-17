02d11e7d322edb675bcc95410f031cf11ac40a07
Hannes Braun <hannesbraun@mail.de>
2021-06-17 08:43:21 +0200

    Update cacher from 2.41.2 to 2.42.1 (#107414)
    * Update cacher from 2.41.2 to 2.42.1
    
    * Update cacher.rb
    
    Co-authored-by: Michael Cho <cho-m@tuta.io>

--- Casks/cacher.rb
+++ Casks/cacher.rb
@@ -1,6 +1,6 @@
 cask "cacher" do
-  version "2.41.2"
-  sha256 "e498ffeabe75dc45b40afcb6b94298ca42771d0b24f6a0829d77c2d843bf2fa4"
+  version "2.42.1"
+  sha256 "fb09d822305687fb06fc87ac5f1f20b7b120b4460fe18c1428736a3fdd6dac74"
 
   url "https://cacher-download.nyc3.digitaloceanspaces.com/Cacher-#{version}-mac.zip",
       verified: "cacher-download.nyc3.digitaloceanspaces.com/"
@@ -10,9 +10,7 @@ cask "cacher" do
 
   livecheck do
     url "https://cacher-download.nyc3.digitaloceanspaces.com/latest-mac.yml"
-    strategy :page_match do |page|
-      YAML.safe_load(page)["version"]
-    end
+    strategy :electron_builder
   end
 
   app "Cacher.app"
