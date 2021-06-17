983053b625d774151a75b6d13ca63a7931a46dbd
Luflosi <Luflosi@users.noreply.github.com>
2021-06-17 18:23:47 +0200

    Update ipfs from 0.15.0 to 0.15.1 (#107426)

--- Casks/ipfs.rb
+++ Casks/ipfs.rb
@@ -1,6 +1,6 @@
 cask "ipfs" do
-  version "0.15.0"
-  sha256 "927acdcd2043692edb5902ee5e82ea66a09df3c61f68846323930f7e85acf4ad"
+  version "0.15.1"
+  sha256 "63857d415a4f82a68df9bc6144e832ec9e19017be5b0e8090df2244ce2b204a8"
 
   url "https://github.com/ipfs-shipyard/ipfs-desktop/releases/download/v#{version}/IPFS-Desktop-#{version}.dmg"
   name "IPFS Desktop"
