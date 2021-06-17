89816862d873481169c7ac885d96cbbe273fa23a
Bevan Kay <bevanjkay@gmail.com>
2021-06-17 11:24:40 +1000

    Update plex from 1.31.1.2262-74fdc6b7 to 1.33.0.2444-a220eae4 (#107403)

--- Casks/plex.rb
+++ Casks/plex.rb
@@ -1,6 +1,6 @@
 cask "plex" do
-  version "1.31.1.2262-74fdc6b7"
-  sha256 "3e304ab3f39aa798f445470f78bb1c4250c071605ce8eba5a17bea2eedb8630d"
+  version "1.33.0.2444-a220eae4"
+  sha256 "3fb9008512e43237d66bf13063547f865c6b08e9bac45f7b8aeae320ccd7080e"
 
   url "https://downloads.plex.tv/plex-desktop/#{version}/macos/Plex-#{version}-x86_64.zip"
   name "Plex"
