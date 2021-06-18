8307e3ceec62d7566a58aae93c34d2cb82876dcc
ZepDonald <63632671+ZepDonald@users.noreply.github.com>
2021-06-18 04:56:15 +0200

    Update porting-kit from 4.1.45 to 4.1.54 (#107421)
    Co-authored-by: Michael Cho <cho-m@tuta.io>

--- Casks/porting-kit.rb
+++ Casks/porting-kit.rb
@@ -1,15 +1,18 @@
 cask "porting-kit" do
-  version "4.1.45"
-  sha256 "772d9e0841487b20a47f2c99ac1834da8fe6a62eb15074ae7da04094926efdb9"
+  version "4.1.54"
+  sha256 "b352bedcee4ca61642cced704213035746b3719955b70f2bebd9244ecfbb631e"
 
   url "https://portingkit.com/pub/portingkit/Porting%20Kit-#{version}-mac.zip"
   name "Porting Kit"
   desc "Install games and apps compiled for Microsoft Windows"
   homepage "https://portingkit.com/"
 
+  # We don't use the `ElectronBuilder` strategy because the YAML has
+  # an unquoted `releaseDate` which is parsed as a scalar timestamp.
+  # This currently errors as Time is not specified as a permitted class.
   livecheck do
     url "https://portingkit.com/pub/portingkit/latest-mac.yml"
-    strategy :electron_builder
+    regex(/Porting(?:%20|\s*)Kit[._-]v?(\d+(?:\.\d+)+)[._-]mac\.zip/i)
   end
 
   auto_updates true
