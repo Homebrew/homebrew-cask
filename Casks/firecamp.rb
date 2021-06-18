e3a8c56e22abde9e6b5f2baa0052f0d05ca5f190
Nishchit Dhanani <dnishchit@gmail.com>
2021-06-19 02:33:40 +0530

    Firecamp upgraded to v2.0.1

--- Casks/firecamp.rb
+++ Casks/firecamp.rb
@@ -1,6 +1,7 @@
 cask "firecamp" do
   version "2.0.1"
   sha256 "f2567ee4184187473519fb8502a4d01b6cc265646247ffd4dfc80fa9347e917d"
+  sha256 :no_check
 
   url "https://firecamp.netlify.app/.netlify/functions/download?pt=mac",
       verified: "firecamp.netlify.app/"
