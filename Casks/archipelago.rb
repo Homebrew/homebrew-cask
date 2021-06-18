a92820b1a9ab26701904066c13195ef8d9447f03
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-06-18 19:07:44 +0900

    Update archipelago from 3.16.0 to 3.16.1

--- Casks/archipelago.rb
+++ Casks/archipelago.rb
@@ -1,6 +1,6 @@
 cask "archipelago" do
-  version "3.16.0"
-  sha256 "d06a98597a980642b084a9d299cdd0040480967e08b885a4d4aad5af9814aa21"
+  version "3.16.1"
+  sha256 "b0b761d1293bc6876da2c3e5630e04957d299a4167f7296c4e1e4fde953d362d"
 
   url "https://github.com/npezza93/archipelago/releases/download/v#{version}/Archipelago-#{version}.dmg"
   name "Archipelago"
