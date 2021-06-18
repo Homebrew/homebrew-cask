a73db8fa66cb01af2cae950fad460da5d1d43255
Caden Haustein <code@brightlysalty.33mail.com>
2021-06-18 01:11:38 +0000

    Update librewolf from 89.0-1 to 89.0.1-1 (#107425)
    Co-authored-by: Michael Cho <cho-m@tuta.io>

--- Casks/librewolf.rb
+++ Casks/librewolf.rb
@@ -1,14 +1,36 @@
 cask "librewolf" do
-  version "89.0-1"
-
   if Hardware::CPU.intel?
-    sha256 "770fb7c8e341a967c77c69baae44ea35c489b4ebaf5059a2f63a377130f9958e"
-    url "https://gitlab.com/librewolf-community/browser/macos/uploads/b9599a19f0da293bb37d28d8e56b48bc/librewolf-#{version}.dmg",
-        verified: "gitlab.com/librewolf-community/browser/macos"
+    version "89.0.1-1,49443567873da35b2a6523fef6e214a5"
+    sha256 "a9695cc5be846e7e9fb023b680a9273d84f3b079475a1277219c1aed374f1f05"
+
+    url "https://gitlab.com/librewolf-community/browser/macos/uploads/#{version.after_comma}/librewolf-#{version.before_comma}.dmg",
+        verified: "gitlab.com/librewolf-community/browser/macos/"
+
+    livecheck do
+      url "https://gitlab.com/api/v4/projects/13853965/releases"
+      regex(%r{"url"\s*:\s*"[^"]*?/(\h+)/librewolf[._-]v?(\d+(?:\.\d+)+(?:-\d+)?)(?:[._-]x86)?\.dmg"}i)
+      strategy :page_match do |page, regex|
+        page.scan(regex).map do |match|
+          "#{match[1]},#{match[0]}"
+        end
+      end
+    end
   else
-    sha256 "99f32a2a6de6caaf5615911fdc3003961ad496c22b9447c8cbf299467561c0c5"
-    url "https://gitlab.com//librewolf-community/browser/macos/uploads/fb11511efb853b544f8a8ebd1963b675/librewolf-#{version}_aarch64_exp.dmg",
-        verified: "gitlab.com/librewolf-community/browser/macos"
+    version "89.0.1-1,d85200e90e304685067e840e6a8cb9d6"
+    sha256 "afa80fe07f1244e79a870de5d13e3783e1763e23f7e7e535f0e172e1cf84893f"
+
+    url "https://gitlab.com/librewolf-community/browser/macos/uploads/#{version.after_comma}/librewolf-#{version.before_comma}_aarch64_exp.dmg",
+        verified: "gitlab.com/librewolf-community/browser/macos/"
+
+    livecheck do
+      url "https://gitlab.com/api/v4/projects/13853965/releases"
+      regex(%r{"url"\s*:\s*"[^"]*?/(\h+)/librewolf[._-]v?(\d+(?:\.\d+)+(?:-\d+)?)[._-]aarch64(?:[._-]exp)?\.dmg"}i)
+      strategy :page_match do |page, regex|
+        page.scan(regex).map do |match|
+          "#{match[1]},#{match[0]}"
+        end
+      end
+    end
   end
 
   name "LibreWolf"
