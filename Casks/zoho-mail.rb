746bd74e4a76b097a69c706239adf5f382f46642
Nick Wind-McJetters <nick@elearningexperts.net>
2021-06-18 12:32:18 -0400

    Update zoho-mail from 1.2.0 to 1.3.0 (#107458)
    Co-authored-by: Michael Cho <cho-m@tuta.io>

--- Casks/zoho-mail.rb
+++ Casks/zoho-mail.rb
@@ -1,12 +1,17 @@
 cask "zoho-mail" do
-  version "1.2.0"
-  sha256 "682fc7c2f7e4750717066e0681d9b5168fe11a61c279a4534aaa49b7fe073d60"
+  version "1.3.0"
+  sha256 "8008bc94e2bf5c26c3f7d7c62308509275a7c64f3fc9b264786c8dcc0dbabab0"
 
   url "https://downloads.zohocdn.com/zmail-desktop/mac/zoho-mail-desktop-installer-v#{version}.dmg",
       verified: "downloads.zohocdn.com/zmail-desktop/mac/"
-  appcast "https://downloads.zohocdn.com/zmail-desktop/artifacts.json"
   name "Zoho Mail"
+  desc "Email client"
   homepage "https://www.zoho.com/mail/desktop/"
+
+  livecheck do
+    url "https://downloads.zohocdn.com/zmail-desktop/artifacts.json"
+    regex(/zoho[._-]mail[._-]desktop[._-]installer[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+  end
 
   app "Zoho Mail - Desktop.app"
 end
