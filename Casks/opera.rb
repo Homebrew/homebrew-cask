93e8e32212c5938cc304380b85c57162267b1eec
Adon <ashendon@users.noreply.github.com>
2021-06-19 11:58:30 -0400

    Update opera from 77.0.4054.64 to 77.0.4054.90 (#107487)

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask "opera" do
-  version "77.0.4054.64"
-  sha256 "a46ebcf850b7c38922cb7c1aa8122f8b8324f1c83d300049c34354aff4bda50e"
+  version "77.0.4054.90"
+  sha256 "e7d87d9507a2bd27c31358ee970591d925614887750b19f0cbd254769c8ffafa"
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   name "Opera"
