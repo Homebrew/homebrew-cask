69d82225ba4be4b3a0053285e3ba1ea0f8db1444
Zhenfu Shi <szf1234@me.com>
2021-06-19 15:00:48 +0800

    Update datagraph from 4.6.1,57 to 4.7.1,58.1 (#107474)
    Co-authored-by: Michael Cho <cho-m@tuta.io>

--- Casks/datagraph.rb
+++ Casks/datagraph.rb
@@ -1,10 +1,16 @@
 cask "datagraph" do
-  version "4.6.1,57"
+  version "4.7.1,58.1"
   sha256 :no_check
 
   url "https://www.visualdatatools.com/DataGraph/DataGraph.dmg"
   name "DataGraph"
+  desc "Scientific/statistical graphing software"
   homepage "https://www.visualdatatools.com/DataGraph/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   app "DataGraph.app"
 end
