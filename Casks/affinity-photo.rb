f9edaf8a5e10b42c5ecc02e3b268c2564e8b29dd
Aayush Kapoor <xeoneux@icloud.com>
2021-06-19 23:13:20 +0530

    Add affinity photo cask

---
+++ Casks/affinity-photo.rb
@@ -0,0 +1,12 @@
+cask "affinity-photo" do
+  version "1.9.3"
+  sha256 "764c4ed303cd6bfcfd7acaca3f1c0ea61cf071a690ca832865cf48189a2d3a6d"
+
+  url "https://store.serif.com/download/075b51"
+  name "Affinity Photo"
+  desc "Professional Image Editing Software"
+  homepage "https://affinity.serif.com/en-us/photo/"
+
+  auto_updates true
+  app "Affinity Photo.app"
+end
