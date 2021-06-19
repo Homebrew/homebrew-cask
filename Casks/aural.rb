388e3765b650356d37f6d9238918bc2006374d50
Reno <25192197+singularitti@users.noreply.github.com>
2021-06-19 18:49:01 -0400

    Update aural from 3.0.0 to 3.1.0

--- Casks/aural.rb
+++ Casks/aural.rb
@@ -1,6 +1,6 @@
 cask "aural" do
-  version "3.0.0"
-  sha256 "7e8cdeb00b31ee7e1e03e7b74be36f3630bb56d30b60cac90ff37e86e9d9e85d"
+  version "3.1.0"
+  sha256 "b09de2d9cc8c053e9124409fadd90fde346934bbfa891af132bf6aa382816405"
 
   url "https://github.com/maculateConception/aural-player/releases/download/#{version}/AuralPlayer-#{version}.dmg"
   name "Aural Player"
