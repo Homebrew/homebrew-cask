8b6fd41e5d9dce06776b3e1a4d49d0bd8c74e5e7
Dario Vladovic <d.vladimyr@gmail.com>
2021-06-20 00:19:58 +0200

    Update dashlane from 6.2048 to 6.2119
    Replace appcast stanza with livecheck block.

--- Casks/dashlane.rb
+++ Casks/dashlane.rb
@@ -1,13 +1,17 @@
 cask "dashlane" do
-  version "6.2048.0.41510"
-  sha256 "f81051c270fd8093e0489ba7a7104bc0a0247878ef283bde0f48c4ec1787f97e"
+  version "6.2119.0.45901"
+  sha256 "8619ecedb90299f8019e3bf324c04ea65a14c7faada502efbcc90df13ead16af"
 
   url "https://d3mfqat9ni8wb5.cloudfront.net/releases/#{version.major_minor_patch}/#{version}/release/Dashlane.dmg",
       verified: "d3mfqat9ni8wb5.cloudfront.net/releases/"
-  appcast "https://ws1.dashlane.com/5/binaries/query?format=json&os=OS_X_10_14_5&target=archive&platform=launcher_macosx"
   name "Dashlane"
   desc "Password manager and digital wallet application"
   homepage "https://www.dashlane.com/"
+
+  livecheck do
+    url "https://ws1.dashlane.com/5/binaries/query?format=xml&os=OS_X_10_14_5&target=archive&platform=server_osx"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :sierra"
