d72d0916154cfe7f1b9a1d29ab617536ebbfacaa
Nishchit <dnishchit@gmail.com>
2021-06-19 12:12:16 +0530

    versioned url set.

--- Casks/firecamp.rb
+++ Casks/firecamp.rb
@@ -2,8 +2,8 @@ cask "firecamp" do
   version "2.0.1"
   sha256 "f2567ee4184187473519fb8502a4d01b6cc265646247ffd4dfc80fa9347e917d"
 
-  url "https://firecamp.netlify.app/.netlify/functions/download?pt=mac",
-      verified: "firecamp.netlify.app/"
+  url "https://firecamp.ams3.digitaloceanspaces.com/versions/mac/Firecamp-#{version}.dmg",
+      verified: "firecamp.ams3.digitaloceanspaces.com/"
   name "Firecamp"
   desc "Multi-protocol API development platform"
   homepage "https://firecamp.io/"
