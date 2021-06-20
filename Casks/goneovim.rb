0875c62148fe77f5304139395b1c35b469c395e6
Ellis Kenyő <elken@users.noreply.github.com>
2021-06-20 20:24:09 +0100

    fix: Prefer catalina version check
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Casks/goneovim.rb
+++ Casks/goneovim.rb
@@ -1,14 +1,14 @@
 cask "goneovim" do
   version "0.4.11"
 
-  if MacOS.version <= :big_sur
-    sha256 "8d9d08db2f96e32944f41c9e7de29de83581deab607b199b3a1516cffcc234e4"
-    url "https://github.com/akiyosi/goneovim/releases/download/v#{version}/Goneovim-#{version}-macos11.tar.bz2"
-    app "Goneovim-#{version}-macos11/goneovim.app"
-  else
+  if MacOS.version <= :catalina
     sha256 "b33f1ab2038424263fd0d49058b16d43ce3eabd56a603c20b24aaed24295f051"
     url "https://github.com/akiyosi/goneovim/releases/download/v#{version}/Goneovim-#{version}-macos.tar.bz2"
     app "Goneovim-#{version}-macos/goneovim.app"
+  else
+    sha256 "8d9d08db2f96e32944f41c9e7de29de83581deab607b199b3a1516cffcc234e4"
+    url "https://github.com/akiyosi/goneovim/releases/download/v#{version}/Goneovim-#{version}-macos11.tar.bz2"
+    app "Goneovim-#{version}-macos11/goneovim.app"
   end
 
   name "Goneovim"
