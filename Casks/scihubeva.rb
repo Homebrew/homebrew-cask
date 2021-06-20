5c61886e7426ac75b5febf5a2b0fca74544e7535
Leo <leo-van@hotmail.com>
2021-06-21 00:17:31 +0800

    Update SciHubEVA from 4.1.1 to 5.0.0

--- Casks/scihubeva.rb
+++ Casks/scihubeva.rb
@@ -1,8 +1,8 @@
 cask "scihubeva" do
-  version "v5.0.0"
+  version "5.0.0"
   sha256 "6133fd7efb9b2c2b544277dd586e407c70dc6320130f0661f9e089308bc28911"
 
-  url "https://github.com/leovan/SciHubEVA/releases/download/#{version}/SciHubEVA-#{version}.dmg"
+  url "https://github.com/leovan/SciHubEVA/releases/download/v#{version}/SciHubEVA-v#{version}.dmg"
   name "SciHubEVA"
   desc "Sci-Hub GUI app"
   homepage "https://github.com/leovan/SciHubEVA"
