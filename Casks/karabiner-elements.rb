5f065a09bc364b09d5c244b6c7240a73950a0b40
eux <euxuuu@gmail.com>
2021-06-21 01:28:35 +0800

    Update karabiner-elements from 13.4.0 to 13.5.0 (#107531)

--- Casks/karabiner-elements.rb
+++ Casks/karabiner-elements.rb
@@ -15,8 +15,8 @@ cask "karabiner-elements" do
 
     pkg "Karabiner-Elements.sparkle_guided.pkg"
   else
-    version "13.4.0"
-    sha256 "50d7ef1f9f1596208311349cbc5eac9f2b03d62b0bc47485b9778c7f6f5ab6af"
+    version "13.5.0"
+    sha256 "d775433d00cef41e3dab4981bbad5439689ee3dca319b1d57ab352ded9df5240"
 
     url "https://github.com/pqrs-org/Karabiner-Elements/releases/download/v#{version}/Karabiner-Elements-#{version}.dmg",
         verified: "github.com/pqrs-org/Karabiner-Elements/"
