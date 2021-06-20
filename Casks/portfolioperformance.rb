bf58b228ceb45e61f9be0ad8580d1ed5c78708de
Daniel Neto <DanielNetoP@users.noreply.github.com>
2021-06-20 17:14:09 +0100

    Update portfolioperformance from 0.53.1 to 0.53.2 (#107527)

--- Casks/portfolioperformance.rb
+++ Casks/portfolioperformance.rb
@@ -1,6 +1,6 @@
 cask "portfolioperformance" do
-  version "0.53.1"
-  sha256 "e33ccf5bf4a61b5fd5cbbbeda21c5b390aed4d24585e2effd3f245a8baf50893"
+  version "0.53.2"
+  sha256 "6bea970e6bf0151931422920e41a2411235c357a45a08b814d5aa6679ff693b0"
 
   url "https://github.com/buchen/portfolio/releases/download/#{version}/PortfolioPerformance-#{version}.dmg",
       verified: "github.com/buchen/portfolio/"
@@ -10,8 +10,7 @@ cask "portfolioperformance" do
 
   livecheck do
     url :url
-    strategy :git
-    regex(/^(\d+(?:\.\d+)*)$/)
+    regex(/^v?(\d+(?:\.\d+)+)$/)
   end
 
   auto_updates true
