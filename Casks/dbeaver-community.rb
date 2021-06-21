8f2034f31ef4532dda19b670997a4bf60e41f752
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-06-21 20:33:09 +0900

    Update dbeaver-community from 21.1.0 to 21.1.1 (#107546)

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,6 +1,6 @@
 cask "dbeaver-community" do
-  version "21.1.0"
-  sha256 "dec806a96ea9609e45357ca31685e39495973d22ee1612099b73756cfe740573"
+  version "21.1.1"
+  sha256 "1b78981687562609cbc1e162644a1a1313b6d2b76ee1b2fc161d5bde0257a25b"
 
   url "https://dbeaver.io/files/#{version}/dbeaver-ce-#{version}-macos.dmg"
   name "DBeaver Community Edition"
