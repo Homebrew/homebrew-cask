56750d690a5d1d0f8e016f631e5bf1ba3a4419e6
Konstantin Vorobyov <vorobyov@gmail.com>
2021-06-21 10:43:38 +0600

    Revert "Removed unneeded redirects"
    This reverts commit 0d411e45bae0ef801e05b0ccdb097bd68b181458.

--- Casks/mediahuman-youtube-downloader.rb
+++ Casks/mediahuman-youtube-downloader.rb
@@ -2,10 +2,10 @@ cask "mediahuman-youtube-downloader" do
   version "3.9.9.57"
   sha256 :no_check
 
-  url "https://www.mediahuman.com/files/YouTubeDownloader.dmg"
+  url "https://www.mediahuman.com/download/YouTubeDownloader.dmg"
   name "MediaHuman YouTube Downloader"
   desc "YouTube videos downloader"
-  homepage "https://www.mediahuman.com/youtube-video-downloader/"
+  homepage "https://www.mediahuman.com/youtube-downloader/"
 
   livecheck do
     url :homepage
