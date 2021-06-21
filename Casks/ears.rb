797e4e7816e2073a6cf978bcd84ae726e6873e77
Lucas Saldanha <lucascrsaldanha@gmail.com>
2021-06-22 10:58:10 +1200

    Add Ears.app v1
    Creating cask for Ears app

---
+++ Casks/ears.rb
@@ -0,0 +1,11 @@
+cask "ears" do
+  version "1"
+  sha256 "f65abc67e55c309031f84f91248183c888b0137cf1e68cb89a82caebaab68941"
+
+  url "https://clickontyler.com/ears/download/v#{version}/"
+  name "ears"
+  desc "Instant audio switcher for macOS"
+  homepage "https://clickontyler.com/ears/"
+
+  app "Ears.app"
+end
