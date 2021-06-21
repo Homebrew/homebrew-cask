dc9ee928540fc44dda257192ecff92d2629d2dbb
LanikSJ <laniksj@gmail.com>
2021-06-21 09:45:41 -0700

    Update kiwi-for-gmail from 3.2.2,2330 to 3.3.0,2348

--- Casks/kiwi-for-gmail.rb
+++ Casks/kiwi-for-gmail.rb
@@ -1,5 +1,5 @@
 cask "kiwi-for-gmail" do
-  version "3.2.2,2330"
+  version "3.3.0,2348"
   sha256 :no_check
 
   url "https://downloads.kiwiforgmail.com/kiwi/release/consumer/Kiwi+for+Gmail+Setup.dmg"
