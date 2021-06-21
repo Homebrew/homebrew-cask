f74446705002c2dcce8d8938628289c96589bd13
Reno <25192197+singularitti@users.noreply.github.com>
2021-06-21 17:21:59 -0400

    Add link for arm64 as suggested
    https://github.com/Homebrew/homebrew-cask/pull/107488#issuecomment-865330519

--- Casks/koodo-reader.rb
+++ Casks/koodo-reader.rb
@@ -1,9 +1,18 @@
 cask "koodo-reader" do
   version "1.2.7"
-  sha256 "ad5c839644db62cb8edbbd3b2f5aa9b74d28154bf5afe35497057bd48a35578b"
 
-  url "https://github.com/troyeguo/koodo-reader/releases/download/v#{version}/Koodo-Reader-#{version}.dmg",
-      verified: "github.com/troyeguo/koodo-reader/"
+  if Hardware::CPU.intel?
+    sha256 "ad5c839644db62cb8edbbd3b2f5aa9b74d28154bf5afe35497057bd48a35578b"
+
+    url "https://github.com/troyeguo/koodo-reader/releases/download/v#{version}/Koodo-Reader-#{version}.dmg",
+        verified: "github.com/troyeguo/koodo-reader/"
+  else
+    sha256 "2ec9bce429a0fa81cdd81d1c1bc1fe69dd0bc0e332ad2c42d2a3be0b704eb21e"
+
+    url "https://github.com/troyeguo/koodo-reader/releases/download/v#{version}/Koodo-Reader-arm64#{version}.dmg",
+        verified: "github.com/troyeguo/koodo-reader/"
+  end
+
   name "Koodo Reader"
   desc "Open-source Epub reader with backup and restore support"
   homepage "https://koodo.960960.xyz/"
