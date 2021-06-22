ee7b2cfa2be3223edff9a2c9b8261ba28a07da33
kvorobyov <vorobyov@gmail.com>
2021-06-22 06:35:43 +0600

    Updates youtube-to-mp3 from 3.9.9.54 to 3.9.9.57 (#107519)

--- Casks/youtube-to-mp3.rb
+++ Casks/youtube-to-mp3.rb
@@ -1,14 +1,15 @@
 cask "youtube-to-mp3" do
-  version "3.9.9.54"
+  version "3.9.9.57"
   sha256 :no_check
 
   url "https://www.mediahuman.com/files/YouTubeToMP3.dmg"
   name "MediaHuman YouTube to MP3 Converter"
   desc "Downloads music from playlists or channels"
-  homepage "https://www.mediahuman.com/youtube-to-mp3-converter/"
+  homepage "https://www.mediahuman.com/youtube-to-mp3/"
 
   livecheck do
-    skip "unversioned URL"
+    url :homepage
+    regex(/>\s*Version:.*?(\d+(?:\.\d+)+)/i)
   end
 
   app "YouTube to MP3.app"
