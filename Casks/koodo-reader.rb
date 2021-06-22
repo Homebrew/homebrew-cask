93a9dd80bc271c012644796a2bebb86003543a11
Reno <25192197+singularitti@users.noreply.github.com>
2021-06-21 20:18:52 -0400

    Update koodo-reader from 1.2.6 to 1.2.7 and add ARM dmg (#107488)

--- Casks/koodo-reader.rb
+++ Casks/koodo-reader.rb
@@ -1,9 +1,18 @@
 cask "koodo-reader" do
-  version "1.2.6"
-  sha256 "e43318f6c29c82aeb81c26597e188733e8a55f0166c3aceb9bd31a4b8a457a10"
+  version "1.2.7"
 
-  url "https://github.com/troyeguo/koodo-reader/releases/download/v#{version}/Koodo-Reader-#{version}.dmg",
-      verified: "github.com/troyeguo/koodo-reader/"
+  if Hardware::CPU.intel?
+    sha256 "ad5c839644db62cb8edbbd3b2f5aa9b74d28154bf5afe35497057bd48a35578b"
+
+    url "https://github.com/troyeguo/koodo-reader/releases/download/v#{version}/Koodo-Reader-#{version}.dmg",
+        verified: "github.com/troyeguo/koodo-reader/"
+  else
+    sha256 "2ec9bce429a0fa81cdd81d1c1bc1fe69dd0bc0e332ad2c42d2a3be0b704eb21e"
+
+    url "https://github.com/troyeguo/koodo-reader/releases/download/v#{version}/Koodo-Reader-#{version}-arm64.dmg",
+        verified: "github.com/troyeguo/koodo-reader/"
+  end
+
   name "Koodo Reader"
   desc "Open-source Epub reader with backup and restore support"
   homepage "https://koodo.960960.xyz/"
