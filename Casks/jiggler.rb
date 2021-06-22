32edad49cdbd8ee870f1b223ecd13489812fc985
Bevan Kay <email@bevankay.me>
2021-06-22 10:53:07 +1000

    add desc, livecheck and zap stanzas

--- Casks/jiggler.rb
+++ Casks/jiggler.rb
@@ -4,7 +4,15 @@ cask "jiggler" do
 
   url "http://downloads.sticksoftware.com/Jiggler.dmg"
   name "Jiggler"
+  desc "Keep your computer awake"
   homepage "http://www.sticksoftware.com/software/Jiggler.html"
 
+  livecheck do
+    url :homepage
+    regex(/<h1>.+(\d+(?:\.\d+))\s/i)
+  end
+
   app "Jiggler.app"
+
+  zap trash: "~/Library/Preferences/com.stick.app.jiggler.plist"
 end
