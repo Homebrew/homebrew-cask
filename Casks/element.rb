6f793734a7dbddf8185751f09b73244cc952e252
Yurii Kolesnykov <root@yurikoles.com>
2021-06-22 23:23:29 +0300

    Update element from 1.7.30 to 1.7.31 (#107587)

--- Casks/element.rb
+++ Casks/element.rb
@@ -1,6 +1,6 @@
 cask "element" do
-  version "1.7.30"
-  sha256 "3a64ddfba0f8eee87308850e80d8e80fa73bb6dc008f60d82a3506a9841eb3b8"
+  version "1.7.31"
+  sha256 "6ed3d66834e8ef685bead44da0b5699d2b7703eacbcf2020addb086c292bf1bc"
 
   url "https://packages.riot.im/desktop/install/macos/Element-#{version}.dmg",
       verified: "packages.riot.im/desktop/"
