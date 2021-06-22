f2dd529e4fb7d8e27a0ebefcfb4e96b61dac914c
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-06-22 21:10:11 +0900

    Update electronmail from 4.12.2 to 4.12.3

--- Casks/electronmail.rb
+++ Casks/electronmail.rb
@@ -1,6 +1,6 @@
 cask "electronmail" do
-  version "4.12.2"
-  sha256 "b44ec31601f5fd1a60bcf072535f9569253c143bfe3a9079a9f5eba0ae1d2411"
+  version "4.12.3"
+  sha256 "aa13a30c5ad34dbcc423d10cf36ecdebfaf8688d9de760fb6a8f2bdc026c1d33"
 
   url "https://github.com/vladimiry/ElectronMail/releases/download/v#{version}/electron-mail-#{version}-mac-x64.dmg"
   name "ElectronMail"
