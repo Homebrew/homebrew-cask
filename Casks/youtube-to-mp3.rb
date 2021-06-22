a3c300eb3ecad4d2cbe0c00cb4378ae24df47361
Michael Cho <cho-m@tuta.io>
2021-06-21 17:14:51 -0700

    Update youtube-to-mp3.rb

--- Casks/youtube-to-mp3.rb
+++ Casks/youtube-to-mp3.rb
@@ -5,12 +5,11 @@ cask "youtube-to-mp3" do
   url "https://www.mediahuman.com/files/YouTubeToMP3.dmg"
   name "MediaHuman YouTube to MP3 Converter"
   desc "Downloads music from playlists or channels"
-  homepage "https://www.mediahuman.com/youtube-to-mp3-converter/"
+  homepage "https://www.mediahuman.com/youtube-to-mp3/"
 
   livecheck do
     url :homepage
-    strategy :page_match
-    regex(/Version:.*?(\d+(?:\.\d+)*)/i)
+    regex(/>\s*Version:.*?(\d+(?:\.\d+)+)/i)
   end
 
   app "YouTube to MP3.app"
