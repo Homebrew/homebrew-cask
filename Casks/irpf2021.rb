5f9e1aba2ff6c560767b909ea1122ae7ef49f424
Alisio Meneses <alisio.meneses@gmail.com>
2021-06-23 12:35:02 -0300

    Update irpf2021 from 1.7 to 1.8 (#107612)

--- Casks/irpf2021.rb
+++ Casks/irpf2021.rb
@@ -1,6 +1,6 @@
 cask "irpf2021" do
-  version "1.7"
-  sha256 "5b9ee4f762392315bda348b468ff82b9ae1428762782fb35ae846f8c59baa537"
+  version "1.8"
+  sha256 "2dab11d1547eea4a00e6330e5eeef58a560d71550adccc15c34cddde9e7e6991"
 
   url "https://downloadirpf.receita.fazenda.gov.br/irpf/2021/irpf/arquivos/IRPF2021-v#{version}.pkg"
   name "IRPF 2021"
