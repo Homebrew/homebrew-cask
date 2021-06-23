3c6eb097b36ebd5b1d089448ddf480072ec7b4f5
Daniel Neto <DanielNetoP@users.noreply.github.com>
2021-06-23 17:59:33 +0100

    Update standard-notes from 3.8.8 to 3.8.9 (#107623)

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -1,6 +1,6 @@
 cask "standard-notes" do
-  version "3.8.8"
-  sha256 "04157721b6bcf5373ca39e01b760d4b04320fe8a20bb93968c381ce5443f5e72"
+  version "3.8.9"
+  sha256 "42541f6967b51f96f7a2a109f3e301d8042da0418da68ea1679da3f5782ae3be"
 
   url "https://github.com/standardnotes/desktop/releases/download/v#{version}/Standard-Notes-#{version}-mac.zip",
       verified: "github.com/standardnotes/desktop/"
