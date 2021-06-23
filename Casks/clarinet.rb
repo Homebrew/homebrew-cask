9be9e33fd59ceec2519b2caa8dc414faffa22113
CharlieC3 <2747302+CharlieC3@users.noreply.github.com>
2021-06-23 16:08:26 -0400

    don't use target, add caveat

--- Casks/clarinet.rb
+++ Casks/clarinet.rb
@@ -1,11 +1,24 @@
 cask "clarinet" do
   version "0.12.0"
-  sha256 "81d4963686cc1334f29ee3cd38ca0db67452e130d11f256d60cca20c4d0fa236"
+  sha256 "ba280091e2db92de3a07ecdb4abd75e7abcd746e739684f8f9f2cac685210b28"
 
-  url "https://github.com/hirosystems/clarinet/releases/download/v#{version}/macos-x64"
+  url "https://github.com/hirosystems/clarinet/releases/download/v#{version}/macos-x64.zip"
   name "clarinet"
   desc "Clarity CLI designed to facilitate smart contract understanding, development, testing and deployment."
   homepage "https://github.com/hirosystems/clarinet/blob/master/README.md"
 
-  binary "macos-x64", target: "clarinet"
+  binary "clarinet"
+
+  caveats <<~EOS
+    Clarinet is not yet signed or notarized by Apple, and thus it must be installed without the 'quaratine' system attribute like so:
+
+      brew install --no-quarantine #{token}
+
+    If reading this message post-installation, you can copy/paste and execute this command to reinstall without the 'quarantine' attribute:
+
+      brew reinstall --no-quarantine #{token}
+
+    WARNING: quarantining is a macOS security feature and you disable it at your own risk!
+  EOS
+
 end
