f40286e7678c8b23877f30c816bee13068c669d5
Meet Godhani <meetgodhani23@gmail.com>
2021-06-23 00:03:41 -0400

    Update raven-reader from 1.0.59 to 1.0.60 (#107596)

--- Casks/raven-reader.rb
+++ Casks/raven-reader.rb
@@ -1,6 +1,6 @@
 cask "raven-reader" do
-  version "1.0.59"
-  sha256 "db0708a7c3cb0bc34901099444fdd37c79530c40b2535b2e7cb2b10da2794f1b"
+  version "1.0.60"
+  sha256 "fa49031019d4ece9715a9df9df40e88855ac2605ff96ffa74f249fb7630d26cf"
 
   url "https://download.helloefficiency.com/ravenreader/Raven%20Reader-#{version}.dmg",
       verified: "download.helloefficiency.com/ravenreader/"
