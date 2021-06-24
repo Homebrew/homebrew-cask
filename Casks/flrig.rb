2f2a41da54e0afde45dcf43cb192eeb481d9e602
Michael Cho <cho-m@tuta.io>
2021-06-23 19:29:58 -0700

    Update flrig from 1.3.54 to 1.4.0 (#107411)

--- Casks/flrig.rb
+++ Casks/flrig.rb
@@ -1,8 +1,16 @@
 cask "flrig" do
-  version "1.3.54"
-  sha256 "76e4fc5dd3f83dd4e91462ec706be0397be45a92f9f51fe3a7444b7c72c301c5"
+  version "1.4.0"
 
-  url "https://downloads.sourceforge.net/fldigi/fldigi/flrig-#{version}_x86_64.dmg"
+  if MacOS.version <= :catalina
+    sha256 "339fa8271bbff84c904e15c0b220ddf44323312a718624cfce807ed6b3da6edb"
+
+    url "https://downloads.sourceforge.net/fldigi/fldigi/flrig-#{version}_hs.dmg"
+  else
+    sha256 "131a95b38fea8a2e978094093db7871af81467f5513c6b5f6070ce6d711a3149"
+
+    url "https://downloads.sourceforge.net/fldigi/fldigi/flrig-#{version}_bs.dmg"
+  end
+
   name "flrig"
   desc "Ham radio rig control"
   homepage "https://sourceforge.net/projects/fldigi/files/flrig/"
@@ -10,7 +18,7 @@ cask "flrig" do
   livecheck do
     url "https://sourceforge.net/projects/fldigi/rss?path=/flrig"
     strategy :page_match
-    regex(/flrig-(\d+(?:\.\d+)*)_x86_64\.dmg/i)
+    regex(/flrig[._-]v?(\d+(?:\.\d+)+)\w*\.dmg/i)
   end
 
   app "flrig.app"
