a9d4730849e1e0a771ac3c9e0f9643df8dfb1be5
Leandro Facchinetti <git@leafac.com>
2021-06-24 03:37:48 +0100

    reaper-arm: Add ARM version of REAPER for compatibility with VLC and enable video editing (#107338)
    * reaper-arm: Add ARM version of REAPER for compatibility with VLC and enable video editing
    
    ### Why Is This Necessary If `reaper.rb` Already Exists?
    
    Video editing requires compatibility with VLC, [which is installed with ARM](https://github.com/Homebrew/homebrew-cask/blob/77b86126e8fe50329d80c228c39681aaf46fa353/Casks/vlc.rb#L12). But [audio plugins may not work with ARM](https://www.reaper.fm/download.php), so I think it makes sense to continue pointing `reaper.rb` to x64 and have a separate Cask instead.
    
    * reaper: Consolidate the reaper & reaper-arm Casks

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -5,10 +5,14 @@ cask "reaper" do
     sha256 "9cafc2dcda0ea11daea92f6558aea6eb3aa58917b91a50e81e2a623e3fda0f5f"
 
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64.dmg"
-  else
+  elsif Hardware::CPU.intel?
     sha256 "c29fca6eb8a9365a3b0a665eea546a428e4aec8bb4ac21e6d0f6f784c5530ca1"
 
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64_catalina.dmg"
+  else
+    sha256 "ca819c60acb44909e2ee1c5709621463bd5aa88e08dfa6d0843c2677b647806e"
+
+    url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}-beta_arm64.dmg"
   end
 
   name "REAPER"
