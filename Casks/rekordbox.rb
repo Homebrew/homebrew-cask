31c88c9542f17a073272e3d10f44488936fce743
Oli Walker <oliwalker@btinternet.com>
2021-06-24 13:26:26 +0100

    Update rekordbox from 6.5.1,20210301195555 to 6.5.2,20210618162921 (#107663)

--- Casks/rekordbox.rb
+++ Casks/rekordbox.rb
@@ -1,6 +1,6 @@
 cask "rekordbox" do
-  version "6.5.1,20210301195555"
-  sha256 "8a1aaa925b9aa9f2b8997b62a426968a1017189fc6f8043a55d6dc3e9fd94c4e"
+  version "6.5.2,20210618162921"
+  sha256 "90a3911b05329ee222dc30379b78dcfdaddc196e2d0facbb991c545543815646"
 
   url "https://cdn.rekordbox.com/files/#{version.after_comma}/Install_rekordbox_#{version.before_comma.dots_to_underscores}.pkg_.zip"
   appcast "https://rekordbox.com/en/download/"
