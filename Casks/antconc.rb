d248f66477bf661b5eaf79fbf3c86a4f1734248f
Michael Cho <cho-m@tuta.io>
2021-06-23 22:47:23 -0700

    antconc: add livecheck

--- Casks/antconc.rb
+++ Casks/antconc.rb
@@ -3,11 +3,14 @@ cask "antconc" do
   sha256 "faac413a4bd914feec6609b96fcf368fa6e91baeedbd15303a8a0594a38db7dd"
 
   url "https://www.laurenceanthony.net/software/antconc/releases/AntConc#{version.no_dots}/AntConc.zip"
-  appcast "https://www.laurenceanthony.net/software/antconc/releases/",
-          must_contain: version.no_dots
   name "AntConc"
   desc "Corpus analysis toolkit for concordancing and text analysis"
   homepage "https://www.laurenceanthony.net/software/antconc/"
+
+  livecheck do
+    url :homepage
+    regex(/>\s*Mac[^<]*\((\d+(?:\.\d+)+)\)\s*</i)
+  end
 
   app "AntConc.app"
 
