3a7ef771a3bf3ab10849c2b9b116c4b477ad1337
Miccal Matthews <miccal.matthews@gmail.com>
2021-06-24 10:41:25 +0800

    Update deepnest.rb

--- Casks/deepnest.rb
+++ Casks/deepnest.rb
@@ -3,13 +3,12 @@ cask "deepnest" do
   sha256 "602c97f2becfddd808bfea8cab1cd7c20503494fa17df2ee55b3cec65a45c7eb"
 
   url "https://deepnest.io/Deepnest-#{version}-mac.zip"
-  name "deepnest"
-  desc "/opt/homebrew/Library/Taps/homebrew/homebrew-cask/Casks/deepnest.rb`"
+  name "Deepnest"
+  desc "Nesting application for CNC machines"
   homepage "https://deepnest.io/"
 
   livecheck do
     url :homepage
-    strategy :page_match
     regex(/href=.*?Deepnest-(\d+(?:\.\d+)*)-mac\.zip/i)
   end
 
