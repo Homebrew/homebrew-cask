36d160bee07dbd5ab88e6bcd4996f6f4f70fa33e
stephaneuh <stephane.ris@icloud.com>
2021-06-24 17:08:52 -0400

    adding kafkaexplorer cask

---
+++ Casks/kafkaexplorer.rb
@@ -0,0 +1,13 @@
+cask "kafkaexplorer" do
+  version "1.1"
+  sha256 :no_check
+
+  url "https://github.com/stephaneuh/kafkaexplorer/blob/develop/releases/Kafkaexplorer_1.1.dmg"
+  name "Kafka Explorer"
+  desc "A free, easy, straight to the point, graphical tool to explore Kafka topics and produce messages. Fully integrated with Confluent Schema Registry for AVRO schemas and JSON schemas on the fly deserialization."
+  homepage "https://github.com/stephaneuh/kafkaexplorer"
+
+  app "kafkaexplorer #{version.major}.app"
+
+  zap trash: "~/.kafkatool#{version.major}"
+end
