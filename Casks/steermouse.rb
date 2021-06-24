3b46f3d727cb26474577a47e7e8757eca917e649
Michael Cho <cho-m@tuta.io>
2021-06-24 11:32:03 -0700

    Update steermouse from 5.5.6 to 5.5.7

--- Casks/steermouse.rb
+++ Casks/steermouse.rb
@@ -1,9 +1,10 @@
 cask "steermouse" do
-  version "5.5.6"
-  sha256 "80f60ad3920ef7c9c31cdd793fea0427ee3a0deb12e80bf4473101d8dfd3b366"
+  version "5.5.7"
+  sha256 "497376308f0ef9385a73611bdb012a93c9406f2fcf5e318ab12d1e653383e124"
 
   url "https://plentycom.jp/ctrl/files_sm/SteerMouse#{version}.dmg"
   name "SteerMouse"
+  desc "Customize mouse buttons, wheels and cursor speed"
   homepage "https://plentycom.jp/en/steermouse/"
 
   livecheck do
