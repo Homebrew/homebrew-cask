ef377619907f870f207968e064bf675500c446e2
Michael Cho <cho-m@tuta.io>
2021-06-24 17:51:29 -0700

    Update ckan from 1.30.2 to 1.30.4

--- Casks/ckan.rb
+++ Casks/ckan.rb
@@ -1,6 +1,6 @@
 cask "ckan" do
-  version "1.30.2"
-  sha256 "136ec251c2846af36578d7ff745912ae255f546597f340642d16533a44bb48c0"
+  version "1.30.4"
+  sha256 "2b81d18259dc11a3043c165b1d6e95b1290103eb56f67b2b5d1f1dee1ca3fc9a"
 
   url "https://github.com/KSP-CKAN/CKAN/releases/download/v#{version}/CKAN.dmg"
   name "Comprehensive Kerbal Archive Network"
