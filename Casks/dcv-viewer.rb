bd803957dd4e0f26264027b62dae57e3dabd3408
Michael Cho <cho-m@tuta.io>
2021-06-24 20:04:13 -0700

    Update dcv-viewer from 2021.1.3450 to 2021.1.3473 (#107707)

--- Casks/dcv-viewer.rb
+++ Casks/dcv-viewer.rb
@@ -1,13 +1,13 @@
 cask "dcv-viewer" do
-  version "2021.1.3450"
+  version "2021.1.3473"
 
   if Hardware::CPU.intel?
-    sha256 "517dae0934f9c71868195014fd91828b96dea2b6df35cd8cbedab693e1352684"
+    sha256 "1c06ec60ae147e379cad3fcb6ffce9ee545b30084d04b765e7f0e26e89db839c"
 
     url "https://d1uj6qtbmh3dt5.cloudfront.net/#{version.major_minor}/Clients/nice-dcv-viewer-#{version}.x86_64.dmg",
         verified: "d1uj6qtbmh3dt5.cloudfront.net/"
   else
-    sha256 "348bef801a5b44806914727efec6cba014c8e1abd70e5d8481831ab3b336b58d"
+    sha256 "de6ca6683097e04210b278ccdd1e68c28ae526d2b116d99875d83c0b13211f83"
 
     url "https://d1uj6qtbmh3dt5.cloudfront.net/#{version.major_minor}/Clients/nice-dcv-viewer-#{version}.arm64.dmg",
         verified: "d1uj6qtbmh3dt5.cloudfront.net/"
