9eceb4e6ae81d8e5c3cdb8e47502c4cc0f78475b
Michael Cho <cho-m@tuta.io>
2021-06-24 20:56:59 -0700

    eaccess: add livecheck and add download ID to version (#107712)

--- Casks/eaccess.rb
+++ Casks/eaccess.rb
@@ -1,12 +1,23 @@
 cask "eaccess" do
-  version "1.13.3"
-  sha256 :no_check
+  version "1.13.3,97899"
+  sha256 "b496682ccaa4b90bdb7a9ae81b631e714235f81481d985c3c752c313dc7d7058"
 
-  url "https://glutz.com/service/downloads/?dwnldid=97899"
-  appcast "https://glutz.com/service/downloads/soft-und-firmware/"
+  url "https://glutz.com/service/downloads/?dwnldid=#{version.after_comma}"
   name "eAccess Desktop"
   desc "Software for eAccess devices"
   homepage "https://glutz.com/service/download/software-and-firmware/"
+
+  livecheck do
+    url "https://glutz.com/download-center/category-assets/26303"
+    strategy :page_match do |page|
+      JSON.parse(page)["assets"].map do |asset|
+        v = asset["title"][/Desktop\s+MacOS\s+v?(\d+(?:\.\d+)+)/i, 1]
+        "#{v},#{asset["external_id"]}" if v
+      end.compact
+    end
+  end
+
+  depends_on macos: ">= :el_capitan"
 
   app "eAccess Desktop.app"
   binary "#{appdir}/eAccess Desktop.app/Contents/MacOS/eAccessServer"
