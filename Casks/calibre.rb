d19ed7247f0f04443891811548dee52c9bd07f5e
xxyzz <gitpull@protonmail.com>
2021-06-25 12:53:56 +0800

    Update calibre from 5.21.0 to 5.22.0 (#107719)

--- Casks/calibre.rb
+++ Casks/calibre.rb
@@ -3,8 +3,8 @@ cask "calibre" do
     version "3.48.0"
     sha256 "68829cd902b8e0b2b7d5cf7be132df37bcc274a1e5720b4605d2dd95f3a29168"
   else
-    version "5.21.0"
-    sha256 "a0ea64694fd5b41d88b1fe3c7f66c69d3f9a5be6794c9c64f67f1c472692632f"
+    version "5.22.0"
+    sha256 "f3c5ab671bdf9a19389202c5857d5ef2be5e656a353496d172b7fde875e7a165"
   end
 
   url "https://download.calibre-ebook.com/#{version}/calibre-#{version}.dmg"
