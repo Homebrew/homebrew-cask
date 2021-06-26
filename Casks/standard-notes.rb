8bb646ae12dfb6f5b0e2c3bf0b69150b7e476d4b
blazarmilkywayflybee <3058623+blazarmilkywayflybee@users.noreply.github.com>
2021-06-26 13:05:55 +1000

    Update standard-notes from 3.8.9 to 3.8.10 (#107745)
    Co-authored-by: Blazar <2w3ioecb@anonaddy.me1>

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -1,6 +1,6 @@
 cask "standard-notes" do
-  version "3.8.9"
-  sha256 "42541f6967b51f96f7a2a109f3e301d8042da0418da68ea1679da3f5782ae3be"
+  version "3.8.10"
+  sha256 "d3b8aae593bd1036b86570b9d899c2c2b99438e4fff4c74f4ca02490dc11f2f3"
 
   url "https://github.com/standardnotes/desktop/releases/download/v#{version}/Standard-Notes-#{version}-mac.zip",
       verified: "github.com/standardnotes/desktop/"
