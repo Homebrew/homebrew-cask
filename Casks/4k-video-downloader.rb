a7948f391ebcaaff9335a1de7d220f45e2360c53
Bevan Kay <email@bevankay.me>
2021-06-26 13:41:14 +1000

    Update 4k-video-downloader from 4.16.3 to 4.16.4

--- Casks/4k-video-downloader.rb
+++ Casks/4k-video-downloader.rb
@@ -1,6 +1,6 @@
 cask "4k-video-downloader" do
-  version "4.16.3"
-  sha256 "031217f8c7d2c89552736e544e1179ced462523c0419462787e76b46f57e1988"
+  version "4.16.4"
+  sha256 "161ccf7116690a5ebf02d39a6218afb7f49858d14bfdeced5f008365abbd01e9"
 
   url "https://dl.4kdownload.com/app/4kvideodownloader_#{version.major_minor_patch}.dmg"
   name "4K Video Downloader"
