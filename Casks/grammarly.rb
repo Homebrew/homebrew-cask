8579761fcdecf93b3c23525ec781865bbf868e36
Bevan Kay <email@bevankay.me>
2021-06-26 14:01:47 +1000

    grammarly: add version and livecheck

--- Casks/grammarly.rb
+++ Casks/grammarly.rb
@@ -1,5 +1,5 @@
 cask "grammarly" do
-  version :latest
+  version "1.5.72"
   sha256 :no_check
 
   url "https://download-editor.grammarly.com/osx/Grammarly.dmg"
@@ -8,6 +8,13 @@ cask "grammarly" do
   homepage "https://www.grammarly.com/"
 
   app "Grammarly.app"
+
+  livecheck do
+    url :url
+    strategy :extract_plist do |versions|
+      versions.values.map(&:short_version).compact.first
+    end 
+  end
 
   zap trash: [
     "~/Library/Application Support/Grammarly",
