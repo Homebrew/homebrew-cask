bea2ffccf8d0ae5075baafdbaea1e100f6fa527f
Roman Ternavskyi <rternavskyi@gmail.com>
2021-06-28 20:11:40 +0300

    Fix style

--- Casks/maxon-app.rb
+++ Casks/maxon-app.rb
@@ -4,29 +4,29 @@ cask "maxon-app" do
 
   url "https://installer.maxon.net/installer/RG_installers/Maxon_App_#{version}_Mac.dmg"
   name "Maxon App"
-  desc "Download the Maxon App to install, use, and try all our products."
+  desc "Download the Maxon App to install, use, and try all our products"
   homepage "https://www.maxon.net/en/downloads/"
 
   installer script: {
-    executable: "#{staged_path}/Maxon App Installer.app/Contents/Scripts/install.sh"
+    executable: "#{staged_path}/Maxon App Installer.app/Contents/Scripts/install.sh",
   }
 
   uninstall delete: "#{staged_path}/#{token}"
 
   zap trash: [
-      "~/Library/Application Support/Maxon",
-      "~/Library/Application Support/Red Giant",
-      "~/Library/Saved Application State/net.maxon.app-manager.savedState",
-      "~/Library/Preferences/Maxon",
-      "~/Library/Preferences/net.maxon.app-manager.plist",
-      "~/Library/Caches/net.maxon.app-manager",
-      "/Users/Shared/Maxon",
-      "/Users/Shared/Red Giant",
-      "/Library/Application Support/Maxon",
-      "/Library/Application Support/Red Giant",
-      "/Library/Logs/Maxon",
-      "/Library/Logs/Red Giant",
-      "/Library/LaunchDaemons/net.maxon.deployservice.plist",
-      "/Library/PrivilegedHelperTools/net.maxon.deployservice",
+    "~/Library/Application Support/Maxon",
+    "~/Library/Application Support/Red Giant",
+    "~/Library/Saved Application State/net.maxon.app-manager.savedState",
+    "~/Library/Preferences/Maxon",
+    "~/Library/Preferences/net.maxon.app-manager.plist",
+    "~/Library/Caches/net.maxon.app-manager",
+    "/Users/Shared/Maxon",
+    "/Users/Shared/Red Giant",
+    "/Library/Application Support/Maxon",
+    "/Library/Application Support/Red Giant",
+    "/Library/Logs/Maxon",
+    "/Library/Logs/Red Giant",
+    "/Library/LaunchDaemons/net.maxon.deployservice.plist",
+    "/Library/PrivilegedHelperTools/net.maxon.deployservice",
   ]
 end
