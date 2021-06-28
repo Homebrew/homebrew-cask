236aa1baa839d6f7cf418cb1f40b20cf94ea6586
David Yantis <404NetworkError@users.noreply.github.com>
2021-06-27 23:45:21 -0400

    Update tg-pro from 2.57,11737 to 2.58,12255 (#107795)

--- Casks/tg-pro.rb
+++ Casks/tg-pro.rb
@@ -1,6 +1,6 @@
 cask "tg-pro" do
-  version "2.57,11737"
-  sha256 "e6a16b693e2e9d92a0ab04ae3fed6617732030c1224cb7556adaa4179bd5e516"
+  version "2.58,12255"
+  sha256 "de24253f820c9fd2c8f7d17328fd44dfc6e2d2cbefdf98fe3454a97e1022e04b"
 
   url "https://www.tunabellysoftware.com/resources/TG%20Pro%20#{version.before_comma}.dmg"
   name "TG Pro"
