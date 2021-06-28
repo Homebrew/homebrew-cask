f0b52b04f038ef504ba9f35460b816e2c877a48b
404NetworkError <404NetworkError@users.noreply.github.com>
2021-06-27 20:35:29 -0400

    Update c0re100-qbittorrent from 4.3.5.10 to 4.3.6.10

--- Casks/c0re100-qbittorrent.rb
+++ Casks/c0re100-qbittorrent.rb
@@ -1,6 +1,6 @@
 cask "c0re100-qbittorrent" do
-  version "4.3.5.10"
-  sha256 "dc5c8d84219c6737ec0401ef7cade9a26b44f35cc1fb5ef0b740761eb48cce5b"
+  version "4.3.6.10"
+  sha256 "af37301c934e0ea3ddd7d1e2378cb47693399cfbe27b0d0cc7cf0f0c79a3c1f4"
 
   url "https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-#{version}/qBittorrent-#{version}.dmg"
   name "qBittorrent Enhanced Edition"
