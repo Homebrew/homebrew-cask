f624d268f0795e3c8fe1f768141c691cb77e6e6f
Curudel <33662559+Curudel@users.noreply.github.com>
2021-06-28 13:38:27 +0200

    Update microsoft-azure-storage-explorer from 1.19.1 to 1.20.0 (#107805)

--- Casks/microsoft-azure-storage-explorer.rb
+++ Casks/microsoft-azure-storage-explorer.rb
@@ -1,6 +1,6 @@
 cask "microsoft-azure-storage-explorer" do
-  version "1.19.1"
-  sha256 "f586dde6cf896f58ef60737abfcedd80529a8f51ee2ace175d773ec5234c2db6"
+  version "1.20.0"
+  sha256 "034a5a1616d27d1bd3bdfa8f1f0e256a5d0b88d687dffe1ae02fbb8c36d4a4c2"
 
   url "https://github.com/microsoft/AzureStorageExplorer/releases/download/V#{version}/Mac_StorageExplorer.zip",
       verified: "github.com/microsoft/AzureStorageExplorer/"
