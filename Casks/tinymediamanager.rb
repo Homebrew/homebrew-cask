92748ae22c7f13c3256905efcfaa135a45e28635
Santiago Maturana Arce <santi2912@icloud.com>
2021-06-28 12:43:09 +0200

    Update tinymediamanager from 4.1.5.1 to 4.1.6 (#107802)

--- Casks/tinymediamanager.rb
+++ Casks/tinymediamanager.rb
@@ -1,6 +1,6 @@
 cask "tinymediamanager" do
-  version "4.1.5.1"
-  sha256 "1556507310579ae5ddfed0acf76fa9c3ac7c5b597dfd26a90bb9615ff44e5644"
+  version "4.1.6"
+  sha256 "26a1546b75bf13769840da2d7a7e79ccef2423d141685eb19ee8af7aa650be6d"
 
   url "https://release.tinymediamanager.org/v#{version.major}/dist/tmm_#{version}_mac.zip"
   name "tinyMediaManager"
