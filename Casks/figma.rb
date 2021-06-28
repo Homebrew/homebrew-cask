54e7567df175fa98f037bde7431000b6da03b6e6
Ondřej Führer <ondrejfuhrer@users.noreply.github.com>
2021-06-28 11:40:12 +0200

    Update figma from 98.13.0 to 99.0.0 (#107788)

--- Casks/figma.rb
+++ Casks/figma.rb
@@ -1,7 +1,8 @@
 cask "figma" do
+  version "99.0.0"
+
   if Hardware::CPU.intel?
-    version "98.13.0"
-    sha256 "213753d01235bc39925553bb78d8d079553885516cadf2e18702ccc6da46a8a8"
+    sha256 "9c94aa3dc73a74900814241c9795ac7663472b44eb3f88e8596ef62ecb5de5c3"
 
     url "https://desktop.figma.com/mac/Figma-#{version}.zip"
 
@@ -10,8 +11,7 @@ cask "figma" do
       regex(%r{/Figma[._-]v?(\d+(?:\.\d+)+)\.zip}i)
     end
   else
-    version "98.11.0"
-    sha256 "85bf1fc63a799b46c0d8e2e041033b54537cca5ee5dcb27af3b6016f080ca7c0"
+    sha256 "5eb4d1073ad671dc53ec1a00225e6adf1f8645b1d43a6774514cd96009f5fdd3"
 
     url "https://desktop.figma.com/mac-arm/Figma-#{version}.zip"
 
