095965d40c4ff344054567c8474b9831a83fae57
Dario Vladović <d.vladimyr@gmail.com>
2021-06-29 01:07:19 +0200

    Update buttercup from 2.9.0 to 2.9.1 (#107819)

--- Casks/buttercup.rb
+++ Casks/buttercup.rb
@@ -1,6 +1,6 @@
 cask "buttercup" do
-  version "2.9.0"
-  sha256 "21e02c421c6b4f5355e2124e9d87eb5890f25591e36698b0a81fb234a8deda1f"
+  version "2.9.1"
+  sha256 "7fd03f02874d4b9bc89588d05f0b0f519c0020457cc484074bcf7eb59de0d8ea"
 
   url "https://github.com/buttercup/buttercup-desktop/releases/download/v#{version}/Buttercup-mac-x64-#{version}.dmg",
       verified: "github.com/buttercup/buttercup-desktop/"
@@ -13,5 +13,16 @@ cask "buttercup" do
     strategy :github_latest
   end
 
+  auto_updates true
+
   app "Buttercup.app"
+
+  zap trash: [
+    "~/Library/Application Support/Buttercup",
+    "~/Library/Application Support/Buttercup-nodejs",
+    "~/Library/Logs/Buttercup-nodejs",
+    "~/Library/Preferences/Buttercup-nodejs",
+    "~/Library/Preferences/pw.buttercup.desktop.plist",
+    "~/Library/Saved Application State/pw.buttercup.desktop.savedState",
+  ]
 end
