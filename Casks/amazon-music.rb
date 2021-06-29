a65f64b6bc92af6874c26b37d768a1b349950b88
Sambhav Aggarwal <nuclearsam@gmail.com>
2021-06-29 13:06:26 +0530

    Updated amazon-music from 8.4.0.2249 to 8.6.0.2271
    Updated amazon-music from 8.4.0.2249 to 8.6.0.2271 and updated the SHA256. The newer version 8.6.0.2271 is compatible with Big Sur on M1.

--- Casks/amazon-music.rb
+++ Casks/amazon-music.rb
@@ -1,6 +1,6 @@
 cask "amazon-music" do
-  version "8.4.0.2249,224949_d5c94ee480b0c856a9257863319df96e"
-  sha256 "c7d001f1be8a37bd14bca1eee3851c3235327fb43d45700726ddbb5eb322c783"
+  version "8.6.0.2271,22710610_e91247c784977e6c258edc2ea5d1a151"
+  sha256 "85d69d9a46296c43936344c1dccb009cbba90ca85c3d7b60620540aff3155ef8"
 
   url "https://morpho-releases.s3-us-west-2.amazonaws.com/mac/#{version.after_comma}/Amazon+Music+Installer.dmg",
       verified: "morpho-releases.s3-us-west-2.amazonaws.com/mac/"
