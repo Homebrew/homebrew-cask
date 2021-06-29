8930d81cd2ee7a402dbaf781545afd5a12451e66
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-06-29 20:13:02 +0900

    Update macdive from 2.13.1 to 2.14.0

--- Casks/macdive.rb
+++ Casks/macdive.rb
@@ -1,6 +1,6 @@
 cask "macdive" do
-  version "2.13.1"
-  sha256 "8d2522e2cacfcd416095153f7bc930bba12d3de6803db2e63d6cfc2672367ef7"
+  version "2.14.0"
+  sha256 "581ef4f6cdfcb26dcd9f1e2bfbe311cfdc35a1d69c7c05de110213b37f126d42"
 
   url "https://www.mac-dive.com/downloads/MacDive_#{version}.dmg"
   name "MacDive"
