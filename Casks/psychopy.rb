01d767a366e26efcaee7702e451d5b78a340cc51
Miccal Matthews <miccal.matthews@gmail.com>
2021-06-29 13:19:15 +0800

    Update psychopy.rb

--- Casks/psychopy.rb
+++ Casks/psychopy.rb
@@ -2,7 +2,6 @@ cask "psychopy" do
   version "2021.2.0.1"
   sha256 "91c8208bb3f4e0df9ea16ede81dcd6b901814975c6698485c54dd46b9344a3bc"
 
-  # url "https://github.com/psychopy/psychopy/releases/download/#{version}/StandalonePsychoPy-#{version}-macOS.dmg"
   url "https://github.com/psychopy/psychopy/releases/download/#{version.major_minor_patch}/StandalonePsychoPy-#{version}-macOS.dmg"
   name "PsychoPy"
   desc "For running psychology and neuroscience experiments"
