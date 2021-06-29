2f363441878966977cb1ea13600619e8ad40c8c9
Hannes Braun <hannesbraun@mail.de>
2021-06-29 19:14:10 +0200

    Update qlc-plus from 4.12.3 to 4.12.4 (#107844)

--- Casks/qlc-plus.rb
+++ Casks/qlc-plus.rb
@@ -1,15 +1,15 @@
 cask "qlc-plus" do
-  version "4.12.3"
-  sha256 "31c0779de17719bb4b6acb6e14ffd83f3475baec5f3e75911d6ea55943e33988"
+  version "4.12.4"
+  sha256 "63f744f7b55ba9b992aa481cda9049b24f9f568072404a71afe4a4f0a7b71469"
 
   url "https://qlcplus.org/downloads/#{version}/QLC+_#{version}.dmg"
   name "Q Light Controller+"
+  desc "Control DMX or analog lighting systems"
   homepage "https://qlcplus.org/"
 
   livecheck do
     url "https://github.com/mcallegari/qlcplus"
-    strategy :git
-    regex(/^QLC\+_(\d+(?:\.\d+)*)$/)
+    regex(/^QLC\+[._-]v?(\d+(?:\.\d+)+)$/i)
   end
 
   app "QLC+.app"
