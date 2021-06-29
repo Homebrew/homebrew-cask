20efdd26599ae06bec793ffd1673a6d17452f181
Xiaokai Xia <xia@xiaokai.me>
2021-06-29 11:01:02 +0800

    Update psychopy.rb
    Fix a little error of the url.

--- Casks/psychopy.rb
+++ Casks/psychopy.rb
@@ -1,8 +1,9 @@
 cask "psychopy" do
-  version "2021.2.0"
-  sha256 "35990de0b942bd3e0ebd5775d469ae4d4d5a9ea7f672fd640dfd1dddfe88a175"
+  version "2021.2.0.1"
+  sha256 "91c8208bb3f4e0df9ea16ede81dcd6b901814975c6698485c54dd46b9344a3bc"
 
-  url "https://github.com/psychopy/psychopy/releases/download/#{version}/StandalonePsychoPy-#{version}-macOS.dmg"
+  # url "https://github.com/psychopy/psychopy/releases/download/#{version}/StandalonePsychoPy-#{version}-macOS.dmg"
+  url "https://github.com/psychopy/psychopy/releases/download/#{version}/StandalonePsychoPy-2021.2.0.1-macOS.dmg"
   name "PsychoPy"
   desc "For running psychology and neuroscience experiments"
   homepage "https://github.com/psychopy/psychopy"
