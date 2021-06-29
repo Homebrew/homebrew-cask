d1eca61d09c4dc668d61bc44966197ef94cbbc9e
Jeff <31933684+upkit@users.noreply.github.com>
2021-06-30 06:15:50 +0800

    Update eudic from 4.0.2,1051 to 4.1.1,1054 (#107833)
    * Update eudic from 4.0.2,1051 to 4.1.1,1054
    
    * Update eudic.rb
    
    * update livecheck
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/eudic.rb
+++ Casks/eudic.rb
@@ -1,17 +1,18 @@
 cask "eudic" do
-  version "4.0.2,1051"
-  sha256 "ca8a9de73f9d856ed7bf67e9576a597dc29710b28d195358d4164467f221ca15"
+  version "4.1.1,1054"
+  sha256 :no_check
 
-  url "https://static.frdic.com/pkg/eudicmac.dmg?v=#{version.after_comma}",
+  url "https://static.frdic.com/pkg/eudicmac.dmg",
       verified:   "static.frdic.com/",
       user_agent: :fake
   name "Eudic"
   name "欧路词典"
+  desc "European dictionary"
   homepage "https://www.eudic.net/v4/en/app/eudic"
 
   livecheck do
-    url "https://www.eudic.net/update/eudic_mac.xml"
-    strategy :sparkle
+    url :url
+    strategy :extract_plist
   end
 
   app "Eudic.app"
