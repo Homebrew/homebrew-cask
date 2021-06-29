3fe0ed40682142499beff8c4253170b53e3e813a
Miccal Matthews <miccal.matthews@gmail.com>
2021-06-29 10:17:36 +0800

    Update retroarch-metal.rb

--- Casks/retroarch-metal.rb
+++ Casks/retroarch-metal.rb
@@ -1,5 +1,5 @@
 cask "retroarch-metal" do
-  version "1.9.5"
+  version "1.9.6"
   sha256 "fefca7c4933462460ec3b9cf1406c63d00753e2fcfddbe0661eb8356cce49bda"
 
   url "https://buildbot.libretro.com/stable/#{version}/apple/osx/universal/RetroArch_Metal.dmg"
@@ -12,9 +12,7 @@ cask "retroarch-metal" do
     regex(%r{href=["']?/stable/v?(\d+(?:\.\d+)+)/?["' >]}i)
   end
 
-  conflicts_with cask: [
-    "retroarch",
-  ]
+  conflicts_with cask: "retroarch"
 
   app "RetroArch.app"
 
