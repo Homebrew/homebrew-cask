6cceca204bc4f21a277e01d9042251486bf08f25
Brad Beyenhof <bbeyenhof@icloud.com>
2021-06-30 11:57:45 -0500

    Update yakyak from 1.5.11.4 to 1.5.11.5

--- Casks/yakyak.rb
+++ Casks/yakyak.rb
@@ -1,6 +1,6 @@
 cask "yakyak" do
-  version "1.5.11.4"
-  sha256 "65adc84101e41bf998c31890a3b63b5f6ae927df3c8189d17c580d21a8460d3f"
+  version "1.5.11.5"
+  sha256 "a0f37f802b591f3f856c729a3db1537ecbbff37ec7eaa732bce09244ae824de0"
 
   url "https://github.com/yakyak/yakyak/releases/download/v#{version}/yakyak-#{version}-osx-x64.zip"
   name "Yakyak"
