92d0fffd910a865632ca6a50e53d17e626314baa
aae42 <abrossia@gmail.com>
2021-06-30 02:52:55 -0400

    Initialize cinc-workstation cask (#107860)
    Free-as-in-Beer version of chef-workstation
    
    https://www.cinc.sh/start/workstation/

---
+++ Casks/cinc-workstation.rb
@@ -0,0 +1,27 @@
+cask "cinc-workstation" do
+  version "21.6.497"
+  sha256 "c685394602a7c1d864508d3d90d34c0c6228a90825664418cfb4e8a401ddb614"
+
+  url "http://downloads.cinc.sh/files/unstable/cinc-workstation/#{version}/mac_os_x/10.15/cinc-workstation-#{version}-1.x86_64.dmg"
+  name "Cinc Workstation"
+  desc "Free-as-in-Beer all-in-one installer for Chef infrastructure management tools"
+  homepage "https://cinc.sh/start/workstation/"
+
+  livecheck do
+    url "https://omnitruck.cinc.sh/unstable/cinc-workstation/metadata?p=mac_os_x&pv=10.15&m=x86_64&v=latest"
+    regex(/version\s*(\d+(?:\.\d+)+)/i)
+  end
+
+  depends_on macos: ">= :mojave"
+
+  pkg "cinc-workstation-#{version}-1.x86_64.pkg"
+
+  uninstall pkgutil:   "com.cinc-project.pkg.cinc-workstation",
+            launchctl: "io.chef.chef-workstation.app",
+            script:    {
+              executable: "/opt/cinc-workstation/bin/uninstall_chef_workstation",
+              sudo:       true,
+            }
+
+  zap trash: "~/.cinc-workstation/"
+end
