0092a4e6466147fd649c757ef7c52e4698898a0d
Mykola Rybak <mykola.rybak@gmail.com>
2021-06-30 20:27:17 +0300

    Update spatterlight from 0.8.0 to 0.8.1

--- Casks/spatterlight.rb
+++ Casks/spatterlight.rb
@@ -1,6 +1,6 @@
 cask "spatterlight" do
-  version "0.8.0"
-  sha256 "2d475ab50e91a92241eab025b5fc07f6b0676def009d7f666e47f5e09723c71a"
+  version "0.8.1"
+  sha256 "6c21af59cc40930487c6d3405de9c460b6394df76b42762d6492b479244ff98c"
 
   url "https://github.com/angstsmurf/spatterlight/releases/download/v#{version}/Spatterlight.zip",
       verified: "github.com/angstsmurf/spatterlight/"
