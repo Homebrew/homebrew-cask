83bd353e63ff2d067e039266902684972cf15c28
Cameron Malek <cam@cameronmalek.com>
2021-07-01 13:58:45 -0500

    Update backblaze from 7.0.2.496 to 7.0.2.498

--- Casks/backblaze.rb
+++ Casks/backblaze.rb
@@ -1,6 +1,6 @@
 cask "backblaze" do
-  version "7.0.2.496"
-  sha256 "1809273e772d236b7056f41c7bf6025673bef30e8f84d302d1c7403d24920fac"
+  version "7.0.2.498"
+  sha256 "a39d9d1014d796c7b356413771fea1d277b00073ef43fe9be50ebbb56f7ef323"
 
   url "https://secure.backblaze.com/api/install_backblaze?file=bzinstall-mac-#{version}.zip"
   name "Backblaze"
