26e9f8bce40162e5600f09dcfc701beac59d59a0
Salomon Popp <hi@salomonpopp.me>
2021-07-02 00:59:48 +0200

    Update plexamp from 3.4.7 to 3.5.0 (#107930)

--- Casks/plexamp.rb
+++ Casks/plexamp.rb
@@ -1,6 +1,6 @@
 cask "plexamp" do
-  version "3.4.7"
-  sha256 "d2384a493a08f07fe8fe7f185cda5080a54a9b9dc9c50c639e3a01974a13fb7a"
+  version "3.5.0"
+  sha256 "e72d312332a94ec948674877672eeeb73151b920be123902658e925662c93fd9"
 
   url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}.dmg",
       verified: "plexamp.plex.tv/"
