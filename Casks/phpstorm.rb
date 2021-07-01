2040f635c3ec920fef5a6ca86d9906d882fdb356
Bevan Kay <email@bevankay.me>
2021-07-02 07:31:26 +1000

    Update phpstorm from 2021.1.4,211.7628.25 to 2021.1.4,211.7628.25 (#107912)

--- Casks/phpstorm.rb
+++ Casks/phpstorm.rb
@@ -1,12 +1,12 @@
 cask "phpstorm" do
-  version "2021.1.3,211.7442.50"
+  version "2021.1.4,211.7628.25"
 
   if Hardware::CPU.intel?
-    sha256 "88df5e4b0933f3795c010bd230cc5f1778fbd432d626981e8d0627886593694e"
+    sha256 "3d19209814b08db70c5add5b14501ffd4ab6ff9c6696e672e757be6b7ea08329"
 
     url "https://download.jetbrains.com/webide/PhpStorm-#{version.before_comma}.dmg"
   else
-    sha256 "ded8a97c43dcac8668063059e51537fd8d7a65438ea9e8d07af53afaf48ad6d4"
+    sha256 "c9df401adaa3991cef6297f979e700ffddfdaa231943b84f7e1deef44bd2c9d3"
 
     url "https://download.jetbrains.com/webide/PhpStorm-#{version.before_comma}-aarch64.dmg"
   end
