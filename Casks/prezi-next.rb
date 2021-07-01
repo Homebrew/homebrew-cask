41a8c55422724e4560be5f29d6266f306828d065
Bevan Kay <email@bevankay.me>
2021-07-02 07:39:50 +1000

    Update prezi-next from 1.50.0,26964 to 1.51.0,27016 (#107907)

--- Casks/prezi-next.rb
+++ Casks/prezi-next.rb
@@ -1,15 +1,18 @@
 cask "prezi-next" do
-  version "1.50.0,26964"
-  sha256 "ea6c8e9ed792c7f526a988592eb9721ace497cca91666da5e025ad9535963918"
+  version "1.51.0,27016"
+  sha256 "b7990bb9825eb51226d536bdc4e8c122138de9ff5f9ee4fb598fd32dd476d196"
 
   url "https://desktopassets.prezi.com/mac/pitch/releases/Prezi_Next_#{version.before_comma}.dmg"
   name "Prezi Next"
+  desc "Presentation software"
   homepage "https://prezi.com/"
 
   livecheck do
     url "https://prezidesktop.s3.amazonaws.com/assets/mac/pitch/updates/prezi-business.xml"
     strategy :sparkle
   end
+
+  depends_on macos: ">= :el_capitan"
 
   app "Prezi Next.app"
 end
