f3e1769b7bb770e2ca56680827de20bde2c04e14
Bevan Kay <email@bevankay.me>
2021-07-01 21:57:03 +1000

    shift: add livecheck (#107908)

--- Casks/shift.rb
+++ Casks/shift.rb
@@ -3,10 +3,14 @@ cask "shift" do
   sha256 "f9412ce974fcf179d7a09c7e5b0fa68f853954e7515ec6572d3b12b82448d9ec"
 
   url "https://update.tryshift.com/download/version/#{version}/osx_64"
-  appcast "https://tryshift.com/download/?platform=mac"
   name "Shift"
   desc "Workstation to streamline your accounts, apps, and workflows"
   homepage "https://tryshift.com/"
+
+  livecheck do
+    url "https://tryshift.com/download/?platform=mac"
+    regex(%r{href=.*?version/(\d+(?:[._-]\d+)+)[._-]stable/osx[._-]64}i)
+  end
 
   app "Shift.app"
 end
