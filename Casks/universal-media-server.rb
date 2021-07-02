bea2f1cc96760f5ec7bfdaa9b33ad67907f75809
Michael Cho <cho-m@tuta.io>
2021-07-01 18:19:35 -0700

    Update universal-media-server from 10.5.0 to 10.6.0

--- Casks/universal-media-server.rb
+++ Casks/universal-media-server.rb
@@ -1,12 +1,14 @@
 cask "universal-media-server" do
-  version "10.5.0"
-  sha256 "63b6cf6bbb66f1fdea2367de8d4c0e5a7979e2f440dd0aaf9dc51332865b0405"
+  version "10.6.0"
+  sha256 "bc05e570a69785ffc832e876f19c705f65a2c129d99fb952743b960ece97dacb"
 
   url "https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/#{version}/UMS-#{version}.dmg",
       verified: "github.com/UniversalMediaServer/UniversalMediaServer/"
   name "Universal Media Server"
   desc "Media server supporting DLNA, UPnP and HTTP(S)"
   homepage "https://www.universalmediaserver.com/"
+
+  depends_on macos: ">= :catalina"
 
   app "Universal Media Server.app"
 
