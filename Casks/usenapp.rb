f87e47acfc447e07458743d77135546128341429
Michael Cho <cho-m@tuta.io>
2021-07-01 18:16:49 -0700

    Update usenapp from 1.07.5,323 to 1.08.8,335

--- Casks/usenapp.rb
+++ Casks/usenapp.rb
@@ -1,6 +1,6 @@
 cask "usenapp" do
-  version "1.07.5,323"
-  sha256 "8a48336ee80049b6d1072d5a88a9f1ae9fa2ace1e65e0bf5b9930105cde89c67"
+  version "1.08.8,335"
+  sha256 "382cc56ef81cfa196410ac56faddee438a73d0b6172e846b9025aff28155faa6"
 
   url "https://www.usenapp.com/download/Usenapp-#{version.before_comma}.dmg"
   name "Usenapp"
