8540fe9bbd442ae94097db5d761bb0064dbea578
exuanbo <exuanbo@protonmail.com>
2021-07-02 11:45:16 +0100

    Update flutter from 2.2.2 to 2.2.3

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -1,6 +1,6 @@
 cask "flutter" do
-  version "2.2.2"
-  sha256 "f965a3c72a0511de39d40067fc17fde842ef7d9cc3c48b2345924d05ce4e8ef0"
+  version "2.2.3"
+  sha256 "9f9a56224282257fc79ae8979aedddf5400a95b7ad436bd09e0e7aac2a1905ca"
 
   url "https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_#{version}-stable.zip",
       verified: "storage.googleapis.com/flutter_infra_release/"
