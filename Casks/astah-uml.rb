d0ac822d6b2b4c283d5880371918878ad244ee96
Michael Cho <cho-m@tuta.io>
2021-07-01 17:02:12 -0700

    Update astah-uml from 8.3.0,b9757d to 8.4.0,8fdff6

--- Casks/astah-uml.rb
+++ Casks/astah-uml.rb
@@ -1,6 +1,6 @@
 cask "astah-uml" do
-  version "8.3.0,b9757d"
-  sha256 "421e5281f0a652beda6a94ef1f4be9ce3945786e41bde7c70cf0839af2874df7"
+  version "8.4.0,8fdff6"
+  sha256 "0015a09556c281c76c40dc99476bc75be8e41e990ecde12e2e2ffab1fb4c7cd6"
 
   url "https://cdn.change-vision.com/files/astah-uml-#{version.before_comma.dots_to_underscores}-#{version.after_comma}-MacOs.dmg",
       verified: "cdn.change-vision.com/files/"
