5b8b56bff9e9bad2e8aa0a28f0fcb6c3923b50e4
Michael Cho <cho-m@tuta.io>
2021-07-01 19:40:48 -0700

    Update wire from 3.25.4095 to 3.26.4145 (#107957)

--- Casks/wire.rb
+++ Casks/wire.rb
@@ -1,6 +1,6 @@
 cask "wire" do
-  version "3.25.4095"
-  sha256 "b20cd46e219ebb29ab6e2387ed8acc40ac899f3bf94a5ebce6f4f0a1fbaaeb05"
+  version "3.26.4145"
+  sha256 "c259fc7c81cf6286cab54e06b7c00e4e4509f13a64549c25ad3c57f1932267b2"
 
   url "https://github.com/wireapp/wire-desktop/releases/download/macos%2F#{version}/Wire.pkg",
       verified: "github.com/wireapp/wire-desktop/"
@@ -8,11 +8,9 @@ cask "wire" do
   desc "Collaboration platform focusing on security"
   homepage "https://wire.com/"
 
-  # We need to check all releases since not all releases are for macOS.
   livecheck do
-    url "https://github.com/wireapp/wire-desktop/releases"
-    strategy :page_match
-    regex(%r{href=.*?/macos%2F(\d+(?:\.\d+)*)/Wire\.pkg}i)
+    url :url
+    regex(%r{^macos[/._-]v?(\d+(?:\.\d+)+)$}i)
   end
 
   pkg "Wire.pkg"
