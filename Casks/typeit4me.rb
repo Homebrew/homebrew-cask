1f028c29f44be4f74023a0bdca64f2fc577fd87e
Michael Cho <cho-m@tuta.io>
2021-07-01 19:36:25 -0700

    Update typeit4me from 6.3.2,239 to 6.3.3,244 (#107981)
    * Update typeit4me from 6.3.2,239 to 6.3.3,244
    
    * Update typeit4me.rb

--- Casks/typeit4me.rb
+++ Casks/typeit4me.rb
@@ -1,9 +1,10 @@
 cask "typeit4me" do
-  version "6.3.2,239"
-  sha256 "5aeda373a3d4593077e5b86fd958140a6a252f484600ed5d7e1850046df9b77c"
+  version "6.3.3,244"
+  sha256 "cc4440d858a85ef6d2c890f3eb7fccb6e0ecec796ad827fe84f7725c8741ab06"
 
   url "https://ettoresoftware.store/download/TypeIt4Me#{version.before_comma}b#{version.after_comma}.zip"
   name "TypeIt4Me"
+  desc "Text expander"
   homepage "https://ettoresoftware.store/mac-apps/typeit4me/"
 
   livecheck do
@@ -13,5 +14,5 @@ cask "typeit4me" do
 
   auto_updates true
 
-  app "TypeIt4Meb#{version.after_comma}notarised.app", target: "TypeIt4Me.app"
+  app "TypeIt4Me#{version.before_comma}b#{version.after_comma}.app", target: "TypeIt4Me.app"
 end
