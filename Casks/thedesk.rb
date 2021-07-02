af47a8dc1b4694da6ebf7c0f4433aa5b5d4f571f
Michael Cho <cho-m@tuta.io>
2021-07-01 19:34:27 -0700

    Update thedesk from 22.2.0 to 22.3.0 (#107991)

--- Casks/thedesk.rb
+++ Casks/thedesk.rb
@@ -1,6 +1,6 @@
 cask "thedesk" do
-  version "22.2.0"
-  sha256 "acbb039a15ff3d3030552189457b7835f6945731b1ec6c664dd2b838acd9b6e4"
+  version "22.3.0"
+  sha256 "2d8f6aa4e21d034c5f3bd80fc448bb9c84bce4c649ef7186736064690e73d4e7"
 
   url "https://github.com/cutls/TheDesk/releases/download/v#{version}/TheDesk-#{version}.dmg",
       verified: "github.com/cutls/TheDesk/"
