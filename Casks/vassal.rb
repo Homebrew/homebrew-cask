88c5fe5dd96aac07d5d0c9eb8d9df97c8b8db8aa
Michael Cho <cho-m@tuta.io>
2021-07-01 19:38:15 -0700

    Update vassal from 3.5.7 to 3.5.8 (#107971)

--- Casks/vassal.rb
+++ Casks/vassal.rb
@@ -1,6 +1,6 @@
 cask "vassal" do
-  version "3.5.7"
-  sha256 "6006529023d92c8bb853a473c38205d0a51a079b27c00efa51b25a70d0dec858"
+  version "3.5.8"
+  sha256 "247eecec789d14a9b752248407c3a8b054fa7f08e98e97a51f79d9afe254a60a"
 
   url "https://github.com/vassalengine/vassal/releases/download/#{version}/VASSAL-#{version}-macos.dmg",
       verified: "github.com/vassalengine/vassal/"
