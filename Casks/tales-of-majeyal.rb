ce5dcd19b7d41334d3c5c7c370ccd1f048958d20
Michael Cho <cho-m@tuta.io>
2021-07-01 20:07:56 -0700

    Update tales-of-majeyal from 1.7.3 to 1.7.4

--- Casks/tales-of-majeyal.rb
+++ Casks/tales-of-majeyal.rb
@@ -1,6 +1,6 @@
 cask "tales-of-majeyal" do
-  version "1.7.3"
-  sha256 "8243d094c70ee8880433df312f9527da8fb30ce470fdde323d9a5ebe932cd88a"
+  version "1.7.4"
+  sha256 "287aa14e56ac0a90f1a6b085bfe0dcf1ea346db8ac8317249bfab24c642a3af2"
 
   url "https://te4.org/dl/t-engine/t-engine4-osx-#{version}.zip"
   name "Tales of Maj'Eyal"
@@ -9,7 +9,7 @@ cask "tales-of-majeyal" do
 
   livecheck do
     url "https://te4.org/download"
-    regex(%r{href=.*?/t-engine4-osx[._-]v?(\d+(?:\.\d+)+)\.zip}i)
+    regex(/href=.*?t-engine4[._-]osx[._-]v?(\d+(?:\.\d+)+)\.zip/i)
   end
 
   # Renamed for clarity: app name is inconsistent with its branding.
