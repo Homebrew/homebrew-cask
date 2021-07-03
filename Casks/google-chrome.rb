842aa80701227b1569cab37be577f7a6ae6b7e03
Dario Vladovic <d.vladimyr@gmail.com>
2021-07-03 02:30:45 +0200

    google-chrome: alternative download for Intel chips

--- Casks/google-chrome.rb
+++ Casks/google-chrome.rb
@@ -2,7 +2,12 @@ cask "google-chrome" do
   version "91.0.4472.114"
   sha256 :no_check
 
-  url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
+  if Hardware::CPU.intel?
+    url "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg"
+  else
+    url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
+  end
+
   name "Google Chrome"
   desc "Web browser"
   homepage "https://www.google.com/chrome/"
