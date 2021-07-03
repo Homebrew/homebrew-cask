10110c2aaadb6efbfce6de733d3dd851ed81f1f6
Georg <Technofrikus@gmx.net>
2021-07-03 10:00:07 +0200

    run brew fix
    I would have run the fix command, but my git is acting up or I am doing something wrong. So when I try to push my local changes this happens:
    
    tf@Mac-Pro homebrew-cask % git push Technofrikus vial-branch
    To github.com:Technofrikus/homebrew-cask.git
     ! [rejected]              vial-branch -> vial-branch (fetch first)
    error: failed to push some refs to 'github.com:Technofrikus/homebrew-cask.git'
    hint: Updates were rejected because the remote contains work that you do
    hint: not have locally. This is usually caused by another repository pushing
    hint: to the same ref. You may want to first integrate the remote changes
    hint: (e.g., 'git pull ...') before pushing again.
    hint: See the 'Note about fast-forwards' in 'git push --help' for details.
    
    I was not able to solve this, so I just added the zap stanza by hand on github directly. Didnt think of copying the complete text from my local file and just pasting it after running brew fix. So that is what I did now. Hope this solves everything.

--- Casks/vial.rb
+++ Casks/vial.rb
@@ -3,10 +3,16 @@ cask "vial" do
   sha256 "7f8861cce8782afa5f68267e3b28293517cd8f0aee8b70f621b8c208b79b5cb1"
 
   url "https://github.com/vial-kb/vial-gui/releases/download/v#{version}/Vial-v#{version}.dmg",
-        verified: "https://github.com/vial-kb/vial-gui"
+      verified: "https://github.com/vial-kb/vial-gui"
   name "vial"
   desc "Configurator of compatible keyboards in real time"
   homepage "https://get.vial.today/"
+
+  livecheck do
+    url "https://github.com/vial-kb/vial-gui.git"
+  end
+
+  app "Vial.app"
 
   zap trash: [
     "~/Library/Application Support/Vial",
@@ -14,6 +20,4 @@ cask "vial" do
     "~/Library/Preferences/Vial.plist",
     "~/Library/Preferences/com.vial.Vial.plist",
   ]
-  
-  app "Vial.app"
 end
