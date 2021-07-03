39a6ce8e2349167c81189f94b12e61532253dd69
Georg <Technofrikus@gmx.net>
2021-07-03 15:28:27 +0200

    removed the livechange code
    Not sure why the fix command added the frozen string line, but I hope this is correct.

--- Casks/vial.rb
+++ Casks/vial.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 cask "vial" do
   version "0.3"
   sha256 "7f8861cce8782afa5f68267e3b28293517cd8f0aee8b70f621b8c208b79b5cb1"
@@ -7,10 +9,6 @@ cask "vial" do
   name "vial"
   desc "Configurator of compatible keyboards in real time"
   homepage "https://get.vial.today/"
-
-  livecheck do
-    url "https://github.com/vial-kb/vial-gui.git"
-  end
 
   app "Vial.app"
 
