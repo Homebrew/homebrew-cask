f338c92bcd9038edd4a52ac5dd5f44582434d931
Bevan Kay <email@bevankay.me>
2021-07-04 00:42:33 +1000

    Update aleo-studio from 0.13.0 to 0.14.0 (#108042)

--- Casks/aleo-studio.rb
+++ Casks/aleo-studio.rb
@@ -1,6 +1,6 @@
 cask "aleo-studio" do
-  version "0.13.0"
-  sha256 "9e42587f18a773e0033f1da8915fcdda52b0f8b68be676e0c517594c5b7ee194"
+  version "0.14.0"
+  sha256 "4a1b2cc81c873fa9c10b7bc19b7b92493f4c317ccd2c518b42a34bb99684fe61"
 
   url "https://aleo-studio-releases.sfo2.digitaloceanspaces.com/latest/Aleo%20Studio-#{version}.pkg",
       verified: "aleo-studio-releases.sfo2.digitaloceanspaces.com/"
