a5c996e4c4eed12043bad90873f4838860758c84
disrupted <hi@salomonpopp.me>
2021-07-03 23:18:02 +0200

    Cosmetic: swap order

--- Casks/plexamp.rb
+++ Casks/plexamp.rb
@@ -1,14 +1,14 @@
 cask "plexamp" do
   version "3.5.0"
 
-  if Hardware::CPU.arm?
-    url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}-arm64.dmg",
-        verified: "plexamp.plex.tv/"
-    sha256 "ead85dd518814718ce57b47a2cf76d0f290a9fd6465c88369d33964a67dbb76d"
-  else
+  if Hardware::CPU.intel?
+    sha256 "6eff736103de0ed89d81600f82e541deecc5f8d76d878320b41ad87ffe4bd87d"
     url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}.dmg",
         verified: "plexamp.plex.tv/"
-    sha256 "6eff736103de0ed89d81600f82e541deecc5f8d76d878320b41ad87ffe4bd87d"
+  else
+    sha256 "ead85dd518814718ce57b47a2cf76d0f290a9fd6465c88369d33964a67dbb76d"
+    url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}-arm64.dmg",
+        verified: "plexamp.plex.tv/"
   end
 
   name "Plexamp"
