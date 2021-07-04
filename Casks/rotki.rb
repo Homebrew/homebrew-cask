00e9a1972d9ec0aae044f717d6e5c7621b1a6789
Michael Cho <cho-m@tuta.io>
2021-07-03 20:10:33 -0700

    Update rotki from 1.17.1 to 1.18.1

--- Casks/rotki.rb
+++ Casks/rotki.rb
@@ -1,6 +1,6 @@
 cask "rotki" do
-  version "1.17.1"
-  sha256 "bb0d969db0e3e409a254cba3c46a5294dbe460f028f498acdd6ec00373e4d9cf"
+  version "1.18.1"
+  sha256 "354cfbdc96d2711af01f7a8161f5310d65286f3af1d3ecf2d1779575563af954"
 
   url "https://github.com/rotki/rotki/releases/download/v#{version}/rotki-darwin-v#{version}.dmg",
       verified: "github.com/rotki/rotki/"
