2fd8c35e7387b94d776e95a9843caaaac0ced2b7
Michael Cho <cho-m@tuta.io>
2021-07-03 20:15:25 -0700

    Update min from 1.19.2 to 1.20.0

--- Casks/min.rb
+++ Casks/min.rb
@@ -1,9 +1,18 @@
 cask "min" do
-  version "1.19.2"
-  sha256 "600c849a8cc33485b467f9d78e013a310ee93b4c1d70dce2e591e68ff1eca109"
+  version "1.20.0"
 
-  url "https://github.com/minbrowser/min/releases/download/v#{version}/Min-v#{version}-darwin-x64.zip",
-      verified: "github.com/minbrowser/min/"
+  if Hardware::CPU.intel?
+    sha256 "03fedef8fa72c38e7362ea4f31b815efde32dfb3254507d3cc24c1d36625d12e"
+
+    url "https://github.com/minbrowser/min/releases/download/v#{version}/Min-v#{version}-darwin-x64.zip",
+        verified: "github.com/minbrowser/min/"
+  else
+    sha256 "f299f8cfd841e4a43613a3dbbe953997d7082c67e58cb4fdbdd03bbb26ca4b36"
+
+    url "https://github.com/minbrowser/min/releases/download/v#{version}/Min-v#{version}-darwin-arm64.zip",
+        verified: "github.com/minbrowser/min/"
+  end
+
   name "Min"
   desc "Minimal browser that protects privacy"
   homepage "https://minbrowser.github.io/min/"
