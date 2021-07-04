53247f9be774a17dc5029ff9d8db5a5cebe2aae3
Michael Cho <cho-m@tuta.io>
2021-07-03 19:46:58 -0700

    Update sirimote from 1.3.9,27 to 1.4,30

--- Casks/sirimote.rb
+++ Casks/sirimote.rb
@@ -1,15 +1,18 @@
 cask "sirimote" do
-  version "1.3.9,27"
+  version "1.4,30"
   sha256 :no_check
 
   url "https://eternalstorms.at/sirimote/SiriMote.zip"
   name "SiriMote"
+  desc "Control your computer with your Apple TV Siri Remote"
   homepage "https://eternalstorms.at/sirimote"
 
   livecheck do
     url "https://eternalstorms.at/sirimote/updatefeed.xml"
     strategy :sparkle
   end
+
+  depends_on macos: ">= :sierra"
 
   app "SiriMote.app"
 end
