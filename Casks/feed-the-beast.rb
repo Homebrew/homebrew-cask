bec59c857ede240a7be8b419f80513c6f1233c52
Bevan Kay <email@bevankay.me>
2021-07-04 16:44:56 +1000

    Update feed-the-beast from 202104081147-534c2a1ea3 to 202105051818-5b1f30277f (#108095)

--- Casks/feed-the-beast.rb
+++ Casks/feed-the-beast.rb
@@ -1,6 +1,6 @@
 cask "feed-the-beast" do
-  version "202104081147-534c2a1ea3"
-  sha256 "6d3be8b29e0ae4d8c66e179d65eba0bd0ebf8a285cee2651d54f7e8778daa814"
+  version "202105051818-5b1f30277f"
+  sha256 "6674177e2bd8cb608c5574b3077744762cd49dec89186c073e217dedc78fb275"
 
   url "https://apps.modpacks.ch/FTBApp/release/#{version}-release/FTBA_macos_#{version}-release.dmg",
       verified: "apps.modpacks.ch/FTBApp/"
