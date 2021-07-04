6c7ea57fbf04e07c0fab199b4a5ee0f29c562297
Bevan Kay <email@bevankay.me>
2021-07-04 17:45:51 +1000

    Update runjs from 2.0.0 to 2.0.1 (#108106)

--- Casks/runjs.rb
+++ Casks/runjs.rb
@@ -1,13 +1,13 @@
 cask "runjs" do
-  version "2.0.0"
+  version "2.0.1"
 
   if Hardware::CPU.intel?
-    sha256 "ced55fe78b8486ce7e3627a76b5dd9a4cd8b5385565ab563fcafc530fbfb3fde"
+    sha256 "71db49c0d1e7308fa1b391b81e3098e5c168a013c9e1a7caf3054f374604f242"
 
     url "https://github.com/lukehaas/runjs/releases/download/v#{version}/RunJS-#{version}.dmg",
         verified: "github.com/lukehaas/runjs/"
   else
-    sha256 "e1c67d3a0149adad1d0beeacc79e17e154fd5830e3575aa2766b2ccd3f896a0b"
+    sha256 "f1868b503318a0fc0f4095bf67fd19f5da41a8abc47049e5a1f98ee9bc81053f"
 
     url "https://github.com/lukehaas/runjs/releases/download/v#{version}/RunJS-#{version}-arm64.dmg",
         verified: "github.com/lukehaas/runjs/"
