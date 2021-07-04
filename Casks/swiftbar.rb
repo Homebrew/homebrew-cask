1326c8ddd5dcc56d111ddaf9d7b29cf86e5db1b7
Michael Cho <cho-m@tuta.io>
2021-07-03 20:55:16 -0700

    swiftbar: add livecheck to ignore beta release (#108062)

--- Casks/swiftbar.rb
+++ Casks/swiftbar.rb
@@ -3,10 +3,17 @@ cask "swiftbar" do
   sha256 "8679bf45eac4c9eb497bc72998a34c1a925a87f75aa28941f1a0872b943ccdad"
 
   url "https://github.com/swiftbar/SwiftBar/releases/download/v#{version}/SwiftBar.zip",
-      verified: "github.com/swiftbar/SwiftBar/releases/"
+      verified: "github.com/swiftbar/SwiftBar/"
   name "SwiftBar"
-  desc "Powerful menu bar customization tool"
+  desc "Menu bar customization tool"
   homepage "https://swiftbar.app/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :catalina"
 
   app "SwiftBar.app"
 
