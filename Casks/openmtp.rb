a366ef375281c928fc4214e8def48ab1925864ab
Fáng <haofanfang@gmail.com>
2021-07-04 14:13:26 +0800

    Update openmtp from 3.1.10 to 3.1.15 (#108101)
    * Update openmtp from 3.1.10 to 3.1.15
    
    * Update openmtp.rb
    
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>

--- Casks/openmtp.rb
+++ Casks/openmtp.rb
@@ -1,10 +1,11 @@
 cask "openmtp" do
-  version "3.1.10"
-  sha256 "b19dca7b9d4811b7c353dfcfd66fefded365dd80691a596f4e5031c100deed58"
+  version "3.1.15"
+  sha256 "9e1002759ab0b8b0c82799c6eae951e53a0dcb5410c0276226fff7cd35d58ef7"
 
   url "https://github.com/ganeshrvel/openmtp/releases/download/v#{version}/OpenMTP-#{version}-mac.zip",
       verified: "github.com/ganeshrvel/openmtp/"
   name "OpenMTP"
+  desc "Android file transfer"
   homepage "https://openmtp.ganeshrvel.com/"
 
   livecheck do
