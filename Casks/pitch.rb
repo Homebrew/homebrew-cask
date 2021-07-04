b96f7e295fc3bc0e7471faa196419bfca1dc47d1
Michael Cho <cho-m@tuta.io>
2021-07-03 20:54:48 -0700

    Update pitch from 1.30.0,1614780 to 1.33.1,1691766 (#108064)

--- Casks/pitch.rb
+++ Casks/pitch.rb
@@ -1,6 +1,6 @@
 cask "pitch" do
-  version "1.30.0,1614780"
-  sha256 "689dca263abefde41745350fcbba84f4c5b3f9ffd35805851494e16e3d8a5bc7"
+  version "1.33.1,1691766"
+  sha256 "a822aaa9134dd2a63e4297c2dfcbe4e7137cdf006581e777878faff59b19995b"
 
   url "https://desktop-app-builds.pitch.com/Pitch-#{version.before_comma}-ci#{version.after_comma}.dmg"
   name "Pitch"
