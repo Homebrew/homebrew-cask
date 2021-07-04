9b206a1068c17fe578436ef57e01bc7c73bab399
Bevan Kay <email@bevankay.me>
2021-07-04 16:45:11 +1000

    Update electron from 13.1.4 to 13.1.5 (#108094)

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,12 +1,12 @@
 cask "electron" do
-  version "13.1.4"
+  version "13.1.5"
 
   if Hardware::CPU.intel?
-    sha256 "3d9980f8062766be3cfcfd54b9fd9f469993df06d538a2324c4879eae1284c84"
+    sha256 "62e0f1464c76881c44ef16f8a0644f861c32c5664170db1d1cf263b884f05742"
     url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip",
         verified: "github.com/electron/electron/"
   else
-    sha256 "e961b7d361c378e4adab7903211ac7200684cb91044c3b300acde04ca24d0468"
+    sha256 "ecc2cb9ccdd8ca6dbb0e6a55315d4bc47446bac1836ac93824c7753ad86e6d3f"
     url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-arm64.zip",
         verified: "github.com/electron/electron/"
   end
