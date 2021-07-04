e9f80860921fac7bc70b6ef95116dd1d81e59a72
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-07-04 16:47:26 +0900

    Update pyfa from 2.33.0 to 2.36.0 (#108109)

--- Casks/pyfa.rb
+++ Casks/pyfa.rb
@@ -1,6 +1,6 @@
 cask "pyfa" do
-  version "2.33.0"
-  sha256 "c5d3732d70033488da61a48cbed8089f9026bbf578791e79bcb0ed32706bbeb6"
+  version "2.36.0"
+  sha256 "e91839401285517ab5498701e146b23ca00fabf30f111458315be893bedb3db5"
 
   url "https://github.com/pyfa-org/Pyfa/releases/download/v#{version}/pyfa-v#{version}-mac.zip"
   name "pyfa"
