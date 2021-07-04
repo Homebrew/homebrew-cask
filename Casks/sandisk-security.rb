cf8138b3349b7e3feb91968b45b4e674d0194bfd
Walter Purcaro <vuolter@gmail.com>
2021-07-04 20:18:08 +0200

    Add SanDisk Security v1.0.0.13

---
+++ Casks/sandisk-security.rb
@@ -0,0 +1,27 @@
+cask "sandisk-security" do
+  version "1.0.0.13"
+  sha256 :no_check
+
+  url "https://downloads.sandisk.com/downloads/sandisk-security-mac-1-0-0-13.dmg"
+  name "SanDisk Security"
+  desc "Password-protect the new (V2) versions of the SanDisk Extreme Portable SSD line"
+  homepage "https://kb.sandisk.com/app/answers/detail/a_id/23374/~/sandisk-security-feature---general-faq"
+
+  installer script: {
+    executable: "exec/SanDisk Security Installer.app/Contents/MacOS/SanDisk Security Installer",
+    args:       ["-silent"],
+    sudo:       true,
+  }
+
+  uninstall script: {
+    executable: "exec/SanDisk Security Installer.app/Contents/MacOS/SanDisk Security Installer",
+    args:       ["-uninstall", "-silent"],
+    sudo:       true,
+  }
+
+  zap trash: [
+    "~/Library/Caches/com.wdc.branded.sandisksecurity",
+    "~/Library/Preferences/com.wdc.branded.sandisksecurity.plist",
+    "~/Library/Caches/com.westerndigital.SanDiskSecurityInstaller",
+  ]
+end
