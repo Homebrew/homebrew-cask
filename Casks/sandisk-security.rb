831e0f799b15d05356094119caa5396826a52d3e
Bevan Kay <email@bevankay.me>
2021-07-05 10:56:38 +1000

    Add livecheck, and version url

--- Casks/sandisk-security.rb
+++ Casks/sandisk-security.rb
@@ -2,10 +2,18 @@ cask "sandisk-security" do
   version "1.0.0.13"
   sha256 :no_check
 
-  url "https://downloads.sandisk.com/downloads/sandisk-security-mac-1-0-0-13.dmg"
+  url "https://downloads.sandisk.com/downloads/sandisk-security-mac-#{version.dots_to_hyphens}.dmg"
   name "SanDisk Security"
   desc "Password-protect the new (V2) versions of the SanDisk Extreme Portable SSD line"
   homepage "https://kb.sandisk.com/app/answers/detail/a_id/23374/~/sandisk-security-feature---general-faq"
+
+  livecheck do
+    url :homepage
+    strategy :page_match do |page|
+      page.scan(/href=.*?sandisk[._-]security[-_.]mac[-_.]v?(\d+(?:[-.]\d+)+)\.dmg/i)
+          .map { |match| match[0].tr("-", ".") }
+    end
+  end
 
   installer script: {
     executable: "exec/SanDisk Security Installer.app/Contents/MacOS/SanDisk Security Installer",
