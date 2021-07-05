72b3d1ca876966cc067b8180dc6c04698982d529
Yishen Miao <mys721tx@gmail.com>
2021-07-04 23:50:06 -0700

    Bumping renpy to 7.4.6 (#108124)
    * Bumping renpy to 7.4.6
    
    Bumps renpy to 7.4.6.
    
    * add livecheck
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/renpy.rb
+++ Casks/renpy.rb
@@ -1,11 +1,16 @@
 cask "renpy" do
-  version "7.4.4"
-  sha256 "b06491573cb59d2fd0c511c550ddb82b22a78a0ef34680370f280d47ff9683a6"
+  version "7.4.6"
+  sha256 "3a3b8608f8150be8e50ab5f27d371fc0f828554874c96c31b1cf77e300953cb3"
 
   url "https://www.renpy.org/dl/#{version}/renpy-#{version}-sdk.zip"
   name "Ren'Py"
   desc "Visual novel engine in Python"
   homepage "https://www.renpy.org/"
+
+  livecheck do
+    url "https://www.renpy.org/latest.html"
+    regex(/href=.*?renpy[._-]v?(\d+(?:\.\d+)+)[._-]sdk\.dmg/i)
+  end
 
   suite "renpy-#{version}-sdk"
 end
