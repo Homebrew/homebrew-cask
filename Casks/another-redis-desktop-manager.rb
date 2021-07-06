09fc36f549af4e29df4a4181a970f1b97b15d144
Bevan Kay <email@bevankay.me>
2021-07-06 13:20:46 +1000

    Update another-redis-desktop-manager from 1.4.5 to 1.4.8

--- Casks/another-redis-desktop-manager.rb
+++ Casks/another-redis-desktop-manager.rb
@@ -1,6 +1,6 @@
 cask "another-redis-desktop-manager" do
-  version "1.4.5"
-  sha256 "b93af8c19ffa44cdbce697034e1a9319b6c031424cf0952a9905e63ea4505ba1"
+  version "1.4.8"
+  sha256 "ecb6b50ff8d22e96fda124ffe16556f0e002cfbfe44c43f3f4c7a4c0e93001ba"
 
   url "https://github.com/qishibo/AnotherRedisDesktopManager/releases/download/v#{version}/Another-Redis-Desktop-Manager.#{version}.dmg"
   name "Another Redis Desktop Manager"
