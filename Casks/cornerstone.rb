25333ee63337547b19aebe3539681c421bc81ab3
Bevan Kay <email@bevankay.me>
2021-07-07 04:02:24 +1000

    cornerstone: update url and homepage (#108159)

--- Casks/cornerstone.rb
+++ Casks/cornerstone.rb
@@ -2,14 +2,14 @@ cask "cornerstone" do
   version "4.2"
   sha256 "c18a4ac6d21e3908942d438e23cfbf31635ad268d638960ca7862680aca4247a"
 
-  url "https://www.zennaware.com/cornerstone/downloads/Cornerstone-#{version}.zip"
+  url "https://cornerstone.assembla.com/downloads/Cornerstone-#{version}.zip"
   name "Cornerstone"
-  homepage "https://www.zennaware.com/cornerstone/"
+  desc "Subversion client"
+  homepage "https://cornerstone.assembla.com/"
 
   livecheck do
     url :homepage
-    strategy :page_match
-    regex(%r{href=.*?/Cornerstone-(\d+(?:\.\d+)*)\.zip}i)
+    regex(/href=.*?Cornerstone[._-]v?(\d+(?:\.\d+)*)\.zip/i)
   end
 
   depends_on macos: ">= :mojave"
