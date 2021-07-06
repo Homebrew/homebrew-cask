123574ecf45f698de3ca05cc90bb484fd48a4ac3
Bevan Kay <email@bevankay.me>
2021-07-07 09:56:13 +1000

    Update forklift.rb

--- Casks/forklift.rb
+++ Casks/forklift.rb
@@ -1,5 +1,5 @@
 cask "forklift" do
-  version "3.5.3"
+  version "3.5.3,215"
   sha256 "c2148aad98a88550d6655a13afe5faaf9419a25802d448c1ce6ad794a147b463"
 
   url "https://download.binarynights.com/ForkLift#{version.before_comma}.zip"
