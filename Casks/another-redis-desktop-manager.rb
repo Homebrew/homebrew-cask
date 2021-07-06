c8fe6eb7793fbb43561e17d0022627aa1f958253
Michael Cho <cho-m@tuta.io>
2021-07-06 10:46:24 -0700

    another-redis-desktop-manager: add M1-arm64 version

--- Casks/another-redis-desktop-manager.rb
+++ Casks/another-redis-desktop-manager.rb
@@ -1,8 +1,16 @@
 cask "another-redis-desktop-manager" do
   version "1.4.8"
-  sha256 "ecb6b50ff8d22e96fda124ffe16556f0e002cfbfe44c43f3f4c7a4c0e93001ba"
 
-  url "https://github.com/qishibo/AnotherRedisDesktopManager/releases/download/v#{version}/Another-Redis-Desktop-Manager.#{version}.dmg"
+  if Hardware::CPU.intel?
+    sha256 "ecb6b50ff8d22e96fda124ffe16556f0e002cfbfe44c43f3f4c7a4c0e93001ba"
+
+    url "https://github.com/qishibo/AnotherRedisDesktopManager/releases/download/v#{version}/Another-Redis-Desktop-Manager.#{version}.dmg"
+  else
+    sha256 "e93e2309b512ac93c6ecdcf842fc5f4d7977721f86fd76c58d5921ef13da8e32"
+
+    url "https://github.com/qishibo/AnotherRedisDesktopManager/releases/download/v#{version}/Another-Redis-Desktop-Manager-M1-arm64-#{version}.dmg"
+  end
+
   name "Another Redis Desktop Manager"
   desc "Redis desktop manager"
   homepage "https://github.com/qishibo/AnotherRedisDesktopManager/"
