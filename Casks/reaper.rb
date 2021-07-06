c6288daa3c7c010fa4e389a2a16d09085f7a0171
Bevan Kay <email@bevankay.me>
2021-07-07 09:55:49 +1000

    Update reaper from 6.30 to 6.31

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,16 +1,16 @@
 cask "reaper" do
-  version "6.30"
+  version "6.31"
 
   if MacOS.version <= :mojave
     sha256 "9cafc2dcda0ea11daea92f6558aea6eb3aa58917b91a50e81e2a623e3fda0f5f"
 
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64.dmg"
   elsif Hardware::CPU.intel?
-    sha256 "ad98c9083da03888aa51b2d95c3208979fb8d5c5ffea32289d25a6e972b92fa3"
+    sha256 "33b68d500c370dd3736842a0c5e1279e054a50621c88f51cc8f3b5595903f42e"
 
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64_catalina.dmg"
   else
-    sha256 "9d51b258ad692b0c773139f8aa78c44520ba01140cb275d57a6d355fa1089671"
+    sha256 "42b68fc08dc0058ba78172879b675a331e1f7e3cb787d51b9bcbd9bb8e34cbfa"
 
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}-beta_arm64.dmg"
   end
