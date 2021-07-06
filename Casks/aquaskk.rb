1a78525eb719698b322873f60645af53cecc5912
Bevan Kay <email@bevankay.me>
2021-07-06 14:41:16 +1000

    update livecheck to skip github pre-release

--- Casks/aquaskk.rb
+++ Casks/aquaskk.rb
@@ -1,10 +1,15 @@
 cask "aquaskk" do
-  version "4.7.4"
-  sha256 "0ab193d091ebdd00391184019e5c566f43b8a40303329fc28be2aa5b104115ec"
+  version "4.7.3"
+  sha256 "bf3300ceb8f5fd2433e244de0f43f1962073b5048f1536ef58e617f7c4785abd"
 
   url "https://github.com/codefirst/aquaskk/releases/download/#{version}/AquaSKK-#{version}.pkg"
   name "AquaSKK"
   homepage "https://github.com/codefirst/aquaskk"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   pkg "AquaSKK-#{version}.pkg"
 
