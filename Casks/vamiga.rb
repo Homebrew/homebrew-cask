7e2061ab8ce1a5117a77b4f908da8d483e25336f
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-07-07 21:33:33 +0900

    Update vamiga from 1.0.1 to 1.0.2 (#108202)

--- Casks/vamiga.rb
+++ Casks/vamiga.rb
@@ -1,6 +1,6 @@
 cask "vamiga" do
-  version "1.0.1"
-  sha256 "fde211d92aa9eabb8cbdd379b805ac103d03002d473d8563cf194379ca143d59"
+  version "1.0.2"
+  sha256 "844ba969a499c039429fda46b47c2e066e14e955cdc62e06832e76c2f8c0b6bf"
 
   url "https://github.com/dirkwhoffmann/vAmiga/releases/download/v#{version}/vAmiga.app.zip",
       verified: "github.com/dirkwhoffmann/vAmiga/"
