eefb368b0e9144fc986ec8b100662fc1be5127db
Nick Lockwood <nick@charcoaldesign.co.uk>
2021-07-07 23:00:57 +0100

    Update swiftformat-for-xcode from 0.48.6 to 0.48.7

--- Casks/swiftformat-for-xcode.rb
+++ Casks/swiftformat-for-xcode.rb
@@ -1,6 +1,6 @@
 cask "swiftformat-for-xcode" do
-  version "0.48.6"
-  sha256 "fa23ff3d459c153c5e93931333dbd48355b85942544fa95eaa9ba51213ba21eb"
+  version "0.48.7"
+  sha256 "bd62918fa047c58deddf68604a0865644f25a1bfec008588d0ae4a2a2208e5e5"
 
   url "https://github.com/nicklockwood/SwiftFormat/archive/#{version}.zip"
   name "SwiftFormat for Xcode"
