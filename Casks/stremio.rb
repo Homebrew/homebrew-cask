46351c5f5a4f0f26b4ac862db2ac44e40fbd2c38
Eloy Aldea <eloy.aldea@gmail.com>
2021-07-07 09:38:31 +0200

    Update stremio from 4.4.137 to 4.4.141

--- Casks/stremio.rb
+++ Casks/stremio.rb
@@ -1,6 +1,6 @@
 cask "stremio" do
-  version "4.4.137"
-  sha256 "cea038eb7b5c45552276823c2a5776f334be03c580d57862b6a85258aa606c73"
+  version "4.4.141"
+  sha256 "ec41ba76004bcc8b4da93d8ef8483ea5ff6037fc83b862c592d5ca57bc34b5a5"
 
   url "https://dl.strem.io/shell-osx/v#{version}/Stremio+#{version}.dmg"
   name "Stremio"
