203c5b6fed7aeb96d91f3b69036cd342814cab38
Nick Lockwood <nicklockwood@users.noreply.github.com>
2021-07-08 08:25:02 +0100

    Update swiftformat-for-xcode from 0.48.6 to 0.48.7 (#108214)
    * Update swiftformat-for-xcode from 0.48.6 to 0.48.7
    
    * Update depends_on
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/swiftformat-for-xcode.rb
+++ Casks/swiftformat-for-xcode.rb
@@ -1,13 +1,13 @@
 cask "swiftformat-for-xcode" do
-  version "0.48.6"
-  sha256 "fa23ff3d459c153c5e93931333dbd48355b85942544fa95eaa9ba51213ba21eb"
+  version "0.48.7"
+  sha256 "bd62918fa047c58deddf68604a0865644f25a1bfec008588d0ae4a2a2208e5e5"
 
   url "https://github.com/nicklockwood/SwiftFormat/archive/#{version}.zip"
   name "SwiftFormat for Xcode"
   desc "Xcode Extension for reformatting Swift code"
   homepage "https://github.com/nicklockwood/SwiftFormat"
 
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :mojave"
 
   app "SwiftFormat-#{version}/EditorExtension/SwiftFormat for Xcode.app"
 
