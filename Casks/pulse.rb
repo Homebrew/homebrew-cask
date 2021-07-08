289dd0968a80c2a660fd811067e046ac1784a65c
Vítor Galvão <info@vitorgalvao.com>
2021-07-08 12:55:10 +0100

    Update pulse.rb

--- Casks/pulse.rb
+++ Casks/pulse.rb
@@ -2,7 +2,7 @@ cask "pulse" do
   version "0.15.3"
   sha256 "cfba399c46f90d11402a04137ff968ad7daf465450b5c7a3607f0f57d17904ad"
 
-  url "https://github.com/kean/Pulse/releases/download/#{version}/Pulse.app.zip"
+  url "https://github.com/kean/Pulse/releases/download/#{version}/Pulse.app.zip",
       verified: "github.com/kean/Pulse/"
   name "Pulse"
   desc "Logger and Network Inspector"
