0504cb5e9ac3c3e1ee310e3bcc192383e7cd5155
Peter Lewis <peter@peterlew.is>
2021-07-08 12:42:15 +0100

    Correct cask name

--- Casks/pulse.rb
+++ Casks/pulse.rb
@@ -3,7 +3,7 @@ cask "pulse" do
   sha256 "cfba399c46f90d11402a04137ff968ad7daf465450b5c7a3607f0f57d17904ad"
 
   url "https://github.com/kean/Pulse/releases/download/#{version}/Pulse.app.zip"
-  name "pulse"
+  name "Pulse"
   desc "Logger and Network Inspector"
   homepage "https://kean.blog/pulse/home"
 
