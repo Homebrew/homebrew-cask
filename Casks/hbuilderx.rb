db96b449a5e9a1ed986a36e600847ea7cedeabcc
Bevan Kay <email@bevankay.me>
2021-07-08 21:25:10 +1000

    hbuilderx: update version, add livecheck (#108234)
    * hbuilderx: update version, add livecheck
    
    * use json strategy

--- Casks/hbuilderx.rb
+++ Casks/hbuilderx.rb
@@ -1,13 +1,19 @@
 cask "hbuilderx" do
-  version "3.1.4.20210305"
-  sha256 "eb1ef0d331824e8d9021ecd38e607933d3806aa2505431d1d86d9c4cba797729"
+  version "3.1.18.20210609"
+  sha256 "c85921dfced7e45248da2a32a7f209667a3df307f97578e0e4983ebaa81ba70a"
 
   url "https://download1.dcloud.net.cn/download/HBuilderX.#{version}.dmg",
       verified: "download1.dcloud.net.cn/"
-  appcast "https://update.dcloud.net.cn/hbuilderx/changelog/#{version}.html"
   name "HBuilderX"
   desc "HTML editor"
   homepage "https://www.dcloud.io/hbuilderx.html"
+
+  livecheck do
+    url "https://download1.dcloud.net.cn/hbuilderx/release.json"
+    strategy :page_match do |page|
+      JSON.parse(page)["version"]
+    end
+  end
 
   app "HBuilderX.app"
 end
