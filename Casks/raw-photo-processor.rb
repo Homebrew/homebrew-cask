508968abb804eec1b5dd7d64cd42a2d7e1a49b85
frihamn <hello@prudens.eu>
2021-07-08 03:21:27 +0200

    Update raw-photo-processor from 1915Beta to 1922Beta (#108201)
    * Update raw-photo-processor from 1915Beta to 1922Beta
    
    * Add desc
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/raw-photo-processor.rb
+++ Casks/raw-photo-processor.rb
@@ -1,11 +1,12 @@
 cask "raw-photo-processor" do
   # Betas of this software are release quality: https://groups.google.com/d/msg/raw-photo-processor/PJyyP2JwIwI/dn3CFknuCwAJ
-  version "1915Beta"
-  sha256 "a621be34bc51a0072a132a852f4f562aae1a10fffbd2f701fd09931707262cdf"
+  version "1922Beta"
+  sha256 "8409146cecf9541d2b7a66c1dfc67f32f1197accb5ef3874055e92f71bd056e9"
 
   url "https://www.raw-photo-processor.com/RPP/RPP64_#{version}.zip"
   appcast "https://groups.google.com/forum/feed/raw-photo-processor/msgs/rss.xml?num=50"
   name "Raw Photo Processor"
+  desc "Process raw photos"
   homepage "https://www.raw-photo-processor.com/RPP/Overview.html"
 
   app "Raw Photo Processor 64.app"
