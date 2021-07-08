53bfa0d0bdb37a74429584ac50469477772877b2
Peter Lewis <peter@peterlew.is>
2021-07-08 12:45:45 +0100

    Add `verified` parameter
    Verified from website both in the menu as well as mentioned [here](https://kean.blog/pulse/guides/pulse-macos)

--- Casks/pulse.rb
+++ Casks/pulse.rb
@@ -3,6 +3,7 @@ cask "pulse" do
   sha256 "cfba399c46f90d11402a04137ff968ad7daf465450b5c7a3607f0f57d17904ad"
 
   url "https://github.com/kean/Pulse/releases/download/#{version}/Pulse.app.zip"
+      verified: "github.com/kean/Pulse/"
   name "Pulse"
   desc "Logger and Network Inspector"
   homepage "https://kean.blog/pulse/home"
