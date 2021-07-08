214c8d18cf52a2a343a46d239ae9eb2de4174c78
jd-a <16205186+jd-a@users.noreply.github.com>
2021-07-08 08:02:07 +0200

    Update JabRef to 5.3
    Correction of hash

--- Casks/jabref.rb
+++ Casks/jabref.rb
@@ -1,6 +1,6 @@
 cask "jabref" do
   version "5.3"
-  sha256 "f6a93cef6453176da25b7ecf81fed805339330113b56cef523de1827f7625d5"
+  sha256 "f6a93cef6453176da25b7ecf81fed805339330113b56cef523de1827f7625d5e"
 
   url "https://github.com/JabRef/jabref/releases/download/v#{version}/JabRef-#{version}.dmg",
       verified: "github.com/JabRef/jabref/"
