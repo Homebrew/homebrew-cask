d5ea69d97209939dd5a20c32b26ed40bd7de7dc9
Dario Vladovic <d.vladimyr@gmail.com>
2021-07-08 15:27:43 +0200

    proxyman: use vendor download url & add livecheck

--- Casks/proxyman.rb
+++ Casks/proxyman.rb
@@ -1,12 +1,16 @@
 cask "proxyman" do
-  version "2.29.0"
+  version "2.29.0,22900"
   sha256 "c2b1239eae2f3efbb0b9a7dcd111deb746d8b66d402bdd33bd0cd72183d27c3a"
 
-  url "https://github.com/ProxymanApp/Proxyman/releases/download/#{version}/Proxyman_#{version}.dmg",
-      verified: "github.com/ProxymanApp/Proxyman/"
+  url "https://download.proxyman.io/#{version.after_comma}/Proxyman_#{version.before_comma}.dmg"
   name "Proxyman"
   desc "Modern and intuitive HTTP Debugging Proxy app"
   homepage "https://proxyman.io/"
+
+  livecheck do
+    url "https://proxyman.io/osx/version.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
 
