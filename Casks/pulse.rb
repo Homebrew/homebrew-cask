4bccd2fcbd4a3c8dbba1a517feeded5bf8c55594
Peter Lewis <peter@peterlew.is>
2021-07-08 12:13:53 +0100

    Remove appcast

--- Casks/pulse.rb
+++ Casks/pulse.rb
@@ -3,7 +3,6 @@ cask "pulse" do
   sha256 "cfba399c46f90d11402a04137ff968ad7daf465450b5c7a3607f0f57d17904ad"
 
   url "https://github.com/kean/Pulse/releases/download/#{version}/Pulse.app.zip"
-  appcast "https://github.com/kean/Pulse/releases.atom"
   name "pulse"
   desc "Logger and Network Inspector"
   homepage "https://github.com/kean/Pulse"
