3f80e5e7a733c27c650f60800603b20ef55d3b09
Jakub Lański <jakub.lanski@roche.com>
2021-07-09 08:34:21 +0200

    Update noxappplayer from 3.8.5.5,20210513:a9853a4a5ba64f9b96c1e3bf6df15e3b to 3.8.5.6,20210617:7b95569e63e7493ea3daf9fae4574e4c (#108281)

--- Casks/noxappplayer.rb
+++ Casks/noxappplayer.rb
@@ -1,6 +1,6 @@
 cask "noxappplayer" do
-  version "3.8.5.5,20210513:a9853a4a5ba64f9b96c1e3bf6df15e3b"
-  sha256 "1668997e8163e4521d379e1b9406fddceb901eb8fca92daf347b1534b99770f7"
+  version "3.8.5.6,20210617:7b95569e63e7493ea3daf9fae4574e4c"
+  sha256 "ec816784862cab820cc0b93f73eca6783628e6eb88607e17285545edc523a976"
 
   url "https://res06.bignox.com/full/#{version.after_comma.before_colon}/#{version.after_colon}.dmg?filename=NoxInstaller_#{version.before_comma}_en.dmg"
   name "NoxAppPlayer"
