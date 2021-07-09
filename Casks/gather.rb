889ebf08b68f884fe30ca8363020a3b199d795a3
Wietse de Vries <wietsedvries@gmail.com>
2021-07-09 09:36:12 +0200

    Update gather from 0.1.0 to 0.1.1

--- Casks/gather.rb
+++ Casks/gather.rb
@@ -1,6 +1,6 @@
 cask "gather" do
-  version "0.1.0"
-  sha256 "4ce518efdbc7355113745bd439e3fb04b573eb3eb25f68c3aa3f3165dc4728f5"
+  version "0.1.1"
+  sha256 "b562ac189c8118d79b67c12211cee697d2776ad46578507ff46cd5798d6999c2"
 
   url "https://github.com/gathertown/gather-town-desktop-releases/releases/download/v#{version}/Gather-#{version}-mac.zip",
       verified: "github.com/gathertown/gather-town-desktop-releases"
