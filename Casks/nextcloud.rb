e596a34a88d296406f601020327d3d70a8a8ae4b
tiiiecherle <tiiiecherle@gmail.com>
2021-07-09 10:08:26 +0200

    Update nextcloud from 3.2.3 to 3.2.4

--- Casks/nextcloud.rb
+++ Casks/nextcloud.rb
@@ -6,8 +6,8 @@ cask "nextcloud" do
     url "https://github.com/nextcloud/desktop/releases/download/v#{version.major_minor_patch}/Nextcloud-#{version}.pkg",
         verified: "github.com/nextcloud/desktop/"
   else
-    version "3.2.3"
-    sha256 "186ab0477dbae676a69a7cdbd7e3a4ec096e1157ef411a5e8a256c1709a93e5e"
+    version "3.2.4"
+    sha256 "124d09e3064be3bb1656f714f93e27f87a03297a219601bd97e3b68d3c9561b5"
 
     url "https://github.com/nextcloud/desktop/releases/download/v#{version}/Nextcloud-#{version}.pkg",
         verified: "github.com/nextcloud/desktop/"
