a7c0a50456a7894e8107792374a8a03915f1eb25
Zhenfu Shi <szf1234@me.com>
2021-07-09 13:05:24 +0800

    Update makemkv from 1.16.3 to 1.16.4 (#108276)

--- Casks/makemkv.rb
+++ Casks/makemkv.rb
@@ -1,6 +1,6 @@
 cask "makemkv" do
-  version "1.16.3"
-  sha256 "a62b0b22a52622e8591bf936efc26fee41787fe618f062698870e775642c00d3"
+  version "1.16.4"
+  sha256 "eb992408b020477fa136d84ab7696ea1c855cab4381c35b79980698148436bc2"
 
   url "https://www.makemkv.com/download/makemkv_v#{version}_osx.dmg"
   name "MakeMKV"
