e53fe11e8a7b89bb804e54a10d554129ed805b46
Yishen Miao <mys721tx@gmail.com>
2021-07-09 16:35:46 -0700

    Bumping phd2 to 2.6.10 (#108302)
    Bumps phd2 to 2.6.10.

--- Casks/phd2.rb
+++ Casks/phd2.rb
@@ -1,6 +1,6 @@
 cask "phd2" do
-  version "2.6.9"
-  sha256 "6c891a7251df226c9090012dc537e36e7fe03119079935fa4daca7d197ee197a"
+  version "2.6.10"
+  sha256 "13ed63233c6937b74e1a4de2241ed49650755b8e90e4f98fda83e714ee204717"
 
   url "https://openphdguiding.org/PHD2-#{version}-OSX-64.zip"
   name "PHD2"
