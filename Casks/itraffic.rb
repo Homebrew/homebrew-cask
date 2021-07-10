d29160e42b3b9a5c1a905e2d09df6e0f0296ab98
foam <282332576@qq.com>
2021-07-10 20:54:50 +0800

    Add Itraffic.app Latest (#108320)
    * Add Itraffic.app Latest
    
    create the Itraffic.app in brew cask
    
    * itraffic: add zap option
    
    * itraffic: append zap list
    
    * itraffic: add option depends_on
    
    * itraffic: fix style

---
+++ Casks/itraffic.rb
@@ -0,0 +1,20 @@
+cask "itraffic" do
+  version "0.1.3"
+  sha256 "b047f02588c4ad2f527b6a073bbfac3403aebb738ab760778c24cc619f731e58"
+
+  url "https://github.com/foamzou/ITraffic-monitor-for-mac/releases/download/v#{version}/ITraffic-v#{version}.zip"
+  name "itraffic"
+  desc "Monitor for displaying process traffic on status bar"
+  homepage "https://github.com/foamzou/ITraffic-monitor-for-mac"
+
+  depends_on macos: ">= :catalina"
+
+  app "ITraffic.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.foamzou.ITrafficMonitorForMac",
+    "~/Library/Application Support/ITraffic",
+    "~/Library/Containers/com.foamzou.ITrafficMonitorForMac",
+    "~/Library/Preferences/com.foamzou.ITrafficMonitorForMac.plist",
+  ]
+end
