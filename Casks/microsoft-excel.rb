f0e8ad98a262703bd5462db7df1e5c19f1dad978
Dario Vladovic <d.vladimyr@gmail.com>
2021-07-13 22:48:34 +0200

    microsoft-excel: update livecheck
    Use Microsoft AutoUpdate feed.

--- Casks/microsoft-excel.rb
+++ Casks/microsoft-excel.rb
@@ -9,8 +9,8 @@ cask "microsoft-excel" do
   homepage "https://products.office.com/en-US/excel"
 
   livecheck do
-    url "https://go.microsoft.com/fwlink/p/?linkid=525135"
-    strategy :header_match
+    url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/0409XCEL2019-chk.xml"
+    regex(%r{Version</key>\s*<string>(\d+(?:\.\d+)*)</}i)
   end
 
   auto_updates true
