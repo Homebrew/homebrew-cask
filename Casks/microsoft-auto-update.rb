470b2366c9c9ac5569798bd49be074d01dd3f4bc
Dario Vladovic <d.vladimyr@gmail.com>
2021-07-13 23:10:18 +0200

    Update microsoft-auto-update from 4.36.21061301 to 4.37.21071101

--- Casks/microsoft-auto-update.rb
+++ Casks/microsoft-auto-update.rb
@@ -1,6 +1,6 @@
 cask "microsoft-auto-update" do
-  version "4.36.21061301"
-  sha256 "0680414ea9553fef6198b8414cfababb6ed5b22a571138b98f6f3e4190630db3"
+  version "4.37.21071101"
+  sha256 "2352ba7722abcc92e9c05e2d50b1b6f71fef7d91306d6e4ef765f042688f2bc4"
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_AutoUpdate_#{version}_Updater.pkg",
       verified: "officecdn-microsoft-com.akamaized.net/"
