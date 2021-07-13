6d4918a520bf4fffc6af60c9839cce9d10968129
Dario Vladovic <d.vladimyr@gmail.com>
2021-07-14 01:20:17 +0200

    microsoft-edge: alternative download method for Intel chips

--- Casks/microsoft-edge.rb
+++ Casks/microsoft-edge.rb
@@ -1,9 +1,16 @@
 cask "microsoft-edge" do
   version "91.0.864.67"
-  sha256 "2bb1e36d9c69ef8020031e056942b222bd0b56fc82c361da75e0e4c0c5ac6abd"
 
-  url "https://officecdn-microsoft-com.akamaized.net/pr/03ADF619-38C6-4249-95FF-4A01C0FFC962/MacAutoupdate/MicrosoftEdge-#{version}.pkg",
-      verified: "officecdn-microsoft-com.akamaized.net/"
+  if Hardware::CPU.intel?
+    url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdge-#{version}.pkg",
+        verified: "officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/"
+    sha256 "078bb879499625eb0952d62c0b1990ba858ebeb308653373ba76e5c9afd1a69f"
+  else
+    url "https://officecdn-microsoft-com.akamaized.net/pr/03ADF619-38C6-4249-95FF-4A01C0FFC962/MacAutoupdate/MicrosoftEdge-#{version}.pkg",
+        verified: "officecdn-microsoft-com.akamaized.net/pr/03ADF619-38C6-4249-95FF-4A01C0FFC962/MacAutoupdate/"
+    sha256 "2bb1e36d9c69ef8020031e056942b222bd0b56fc82c361da75e0e4c0c5ac6abd"
+  end
+
   name "Microsoft Edge"
   desc "Web browser"
   homepage "https://www.microsoft.com/edge"
