55e4c8af7bb333a18b3b90fd5ebc9fb72e06a5ae
Curudel <andreas.florin.1988@outlook.com>
2021-07-13 21:28:17 +0200

    Update microsoft-excel from 16.50.21061301 to 16.51.21071101

--- Casks/microsoft-excel.rb
+++ Casks/microsoft-excel.rb
@@ -1,6 +1,6 @@
 cask "microsoft-excel" do
-  version "16.50.21061301"
-  sha256 "95c69d6d7734a04d7fe3e3debc65a59b939b3db1eb7fadcbb5049d18dc8933ac"
+  version "16.51.21071101"
+  sha256 "a20606ccdf3aee321188b6604497d309f488371d62265e382c4608c4011333bb"
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Excel_#{version}_Installer.pkg",
       verified: "officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/"
