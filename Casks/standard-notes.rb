065e4d8a27a9df6557abe8368fa1559c607f8868
Blazar <2w3ioecb@anonaddy.me1>
2021-07-15 06:02:06 +1000

    Update standard-notes from 3.8.16 to 3.8.17

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -1,6 +1,6 @@
 cask "standard-notes" do
-  version "3.8.16"
-  sha256 "b8d1f5863052d1e0407363d52f67401ffbc1d20127efe0a6f1808a086c552d08"
+  version "3.8.17"
+  sha256 "1db9819c3acda5bbc256c59536ff3664ebacf9691fcf950fef95b7f5f22a35e5"
 
   url "https://github.com/standardnotes/desktop/releases/download/v#{version}/Standard-Notes-#{version}-mac.zip",
       verified: "github.com/standardnotes/desktop/"
