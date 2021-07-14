4394f4f9e0fe8a77eed6b152e43b076379d7b090
Ralph Martin <ralph@cs.cf.ac.uk>
2021-07-14 04:48:54 +0100

    Update smartgit from 20.2.5 to 21.1.0 (#108403)
    * Update smartgit from 20.2.5 to 21.1.0
    
    * update livecheck
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/smartgit.rb
+++ Casks/smartgit.rb
@@ -1,6 +1,6 @@
 cask "smartgit" do
-  version "20.2.5"
-  sha256 "7674b546af9544499599fe82f8806df8001525ff0d3e19cda8ef125452d969d0"
+  version "21.1.0"
+  sha256 "6969e3b4fb00b5bf7b255ceb858c567a4bcda8967a728f3d18669d9fc84e5cd5"
 
   url "https://www.syntevo.com/downloads/smartgit/smartgit-macosx-#{version.dots_to_underscores}.dmg"
   name "SmartGit"
@@ -8,9 +8,12 @@ cask "smartgit" do
   homepage "https://www.syntevo.com/smartgit/"
 
   livecheck do
-    url "https://www.syntevo.com/smartgit/changelog.txt"
-    strategy :page_match
-    regex(/SmartGit\s*(\d+(?:\.\d+)*)/i)
+    url "https://www.syntevo.com/smartgit/download/"
+    regex(/href=.*?smartgit[._-]macosx[._-]v?(\d+(?:[._]\d+)+)\.dmg/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex)
+          .map { |match| match[0].tr("_", ".") }
+    end
   end
 
   app "SmartGit.app"
