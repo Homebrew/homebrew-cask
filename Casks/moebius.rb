a673e2e2d4a8b75611b64f53ac43fd379ef035ca
Miccal Matthews <miccal.matthews@gmail.com>
2021-07-15 08:28:27 +0800

    Update moebius.rb

--- Casks/moebius.rb
+++ Casks/moebius.rb
@@ -1,14 +1,17 @@
 cask "moebius" do
   version "1.0.29"
-  sha256 :no_check
+  sha256 "014e355767fa27796a6f5f5778b608a3a802ba064655c23776fa89f0dd1163ba"
 
-  url "https://github.com/blocktronics/moebius/releases/download/1.0.29/Moebius.dmg",
+  url "https://github.com/blocktronics/moebius/releases/download/#{version}/Moebius.dmg",
       verified: "github.com/blocktronics/moebius"
   name "Moebius"
-  desc "ANSI Editor"
+  desc "ANSI editor"
   homepage "https://blocktronics.github.io/moebius/"
 
   app "Moebius.app"
 
-  zap trash: "~/Library/Application Support/Moebius; ~/Library/Preferences/org.andyherbert.moebius.plist"
+  zap trash: [
+    "~/Library/Application Support/Moebius",
+    "~/Library/Preferences/org.andyherbert.moebius.plist",
+  ]
 end
