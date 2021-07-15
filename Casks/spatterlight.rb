2d78c9276144e3e3610632bb106ac0da3ab5e1b3
Mykola Rybak <mykola.rybak@gmail.com>
2021-07-16 01:06:21 +0300

    Update spatterlight from 0.8.2 to 0.8.3 (#108494)
    * Update spatterlight from 0.8.2 to 0.8.3
    
    * add zap stanza
    
    * fix style
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/spatterlight.rb
+++ Casks/spatterlight.rb
@@ -1,6 +1,6 @@
 cask "spatterlight" do
-  version "0.8.2"
-  sha256 "f9f063745a7f48d6b2be319586ac2babd9e1d95e16a150abe52e9b179fda192c"
+  version "0.8.3"
+  sha256 "e49bb02502c4675f59c2635c1649ca1a41e111c06fdcbcbf7b41a478efbeb747"
 
   url "https://github.com/angstsmurf/spatterlight/releases/download/v#{version}/Spatterlight.zip",
       verified: "github.com/angstsmurf/spatterlight/"
@@ -11,4 +11,14 @@ cask "spatterlight" do
   depends_on macos: ">= :catalina"
 
   app "Spatterlight.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/net.ccxvii.spatterlight.SpatterlightQuickLook",
+    "~/Library/Application Scripts/net.ccxvii.spatterlight.SpatterlightThumbnailer",
+    "~/Library/Application Scripts/net.ccxvii.spatterlight.iFictionQuickLook",
+    "~/Library/Containers/net.ccxvii.spatterlight.SpatterlightQuickLook",
+    "~/Library/Containers/net.ccxvii.spatterlight.SpatterlightThumbnailer",
+    "~/Library/Containers/net.ccxvii.spatterlight.iFictionQuickLook",
+    "~/Library/Preferences/net.ccxvii.spatterlight.plist",
+  ]
 end
