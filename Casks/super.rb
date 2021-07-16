a2988f180e6ab99a76593cf971ad8d646f87db4d
Nick Mitchell <nickm@us.ibm.com>
2021-07-16 12:53:07 -0400

    Add Super.app v1.4.1
    This PR adds a recipe for Super.app

---
+++ Casks/super.rb
@@ -0,0 +1,27 @@
+cask "super" do
+  version "1.4.1"
+
+  name "Super"
+  desc "CLI for the Serverless Supercomputer"
+  homepage "https://github.com/IBM/super"
+
+  if Hardware::CPU.intel?
+    url "https://github.com/IBM/super/releases/download/v#{version}/Super-darwin-x64.tar.bz2"
+    sha256 "dff9e85bbb5dee7b997eec55051e2d6008709c3e45b943296104a4438c6f46c5"
+    app "Super-darwin-x64/Super.app"
+  else
+    url "https://github.com/IBM/super/releases/download/v#{version}/Super-darwin-amd64.tar.bz2"
+    sha256 "e5b725b29ed141b876895195f8fc561abc1b8385196a60b1663a602704391cf0"
+    app "Super-darwin-amd64/Super.app"
+  end
+
+  livecheck do
+    url :url
+    strategy :git
+    regex(/^v(\d+(?:\.\d+)*)$/)
+  end
+
+  binary "#{appdir}/Super.app/Contents/Resources/super"
+
+  zap trash: "~/Library/Application\ Support/Super"
+end
