f2abd61ca6e53ad36338f53126421fd2f495b5a1
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-07-16 21:17:55 +0900

    Update eqmac from 1.0.0 to 1.1.0 (#108527)

--- Casks/eqmac.rb
+++ Casks/eqmac.rb
@@ -1,6 +1,6 @@
 cask "eqmac" do
-  version "1.0.0"
-  sha256 "f0cf0831da9fc08f7c2f267b6a691173b32b4aa688625afcc43cade6d5f65a7f"
+  version "1.1.0"
+  sha256 "9e924a6328ffbb54acf84322135abfd2d812197151fea6033179ecc108bfdb6a"
 
   url "https://github.com/bitgapp/eqMac/releases/download/v#{version}/eqMac.pkg",
       verified: "github.com/bitgapp/eqMac/"
