7b650713e9463d741c1fd36f02450aa49fe4564c
NetSarang Computer <NetSarangComputer@MacBook-Pro.local>
2021-07-16 11:10:57 +0900

    Update PortX.app version 1.1.4

--- Casks/portx.rb
+++ Casks/portx.rb
@@ -1,17 +1,22 @@
 cask "portx" do
-  version :latest
-  sha256 :no_check
+  version "1.1.4"
+  sha256 "2c612c434fa76b943af6d583293354063b3180334d976bcb804ade1606bbbd07"
 
-  url "https://cdn.netsarang.net/0ac7ea20/PortX-latest.dmg",
+  url "https://cdn.netsarang.net/0ac7ea20/PortX-#{version}.dmg",
       verified: "cdn.netsarang.net/"
   name "portx"
   desc "SSH Client"
   homepage "https://portx.online/en/portx/"
 
+  livecheck do
+    url :homepage
+    regex(/PortX-(\d+(?:\.\d+)*)\.dmg/i)
+  end
+
   app "PortX.app"
 
   zap trash: [
     "~/Library/Application Support/PortX",
-    "~/Library/Logs/PortX",
+    "~/Library/Logs/PortX"
   ]
 end
