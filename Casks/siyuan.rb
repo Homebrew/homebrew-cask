dfc302b681eb69752fc7c356f4d7176a4d481467
Kunzhe Huang <hkunzhe24@gmail.com>
2021-07-17 23:03:20 +0800

    Update siyuan from 1.1.83 to 1.2.0 (#108511)

--- Casks/siyuan.rb
+++ Casks/siyuan.rb
@@ -1,14 +1,8 @@
 cask "siyuan" do
-  version "1.1.83"
+  version "1.2.0"
+  sha256 "b750471c066d6c5a7e4ccc3c25eb9f430070ecff016acfd295b953ec46dac452"
 
-  if Hardware::CPU.intel?
-    sha256 "e3d602b82b2af2987c7c26589a94a98cc9c11b7f8789b5dc574df805791737e9"
-    url "https://github.com/siyuan-note/siyuan/releases/download/v#{version}/siyuan-#{version}-mac.dmg"
-  else
-    sha256 "28700a13550eda4637ce3119642bcb36860205fa4f0661b7a0fe2ea93678a106"
-    url "https://github.com/siyuan-note/siyuan/releases/download/v#{version}/siyuan-#{version}-mac-arm64.dmg"
-  end
-
+  url "https://github.com/siyuan-note/siyuan/releases/download/v#{version}/siyuan-#{version}-mac.dmg"
   name "SiYuan"
   desc "Local-first personal knowledge management system"
   homepage "https://github.com/siyuan-note/siyuan"
