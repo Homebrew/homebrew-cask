ea27dd643c4c9082d048c7cfd509b261756a0b9e
Justin Hamilton <jwhamilton99@gmail.com>
2021-07-16 22:14:06 -0400

    Update menu-bar-splitter from 2.0 to 2.0.1 and update description

--- Casks/menu-bar-splitter.rb
+++ Casks/menu-bar-splitter.rb
@@ -1,10 +1,10 @@
 cask "menu-bar-splitter" do
-  version "2.0"
+  version "2.0.1"
   sha256 "4f1db97404fe48eaef14b8fd05c938a6ca9e6c6cd546730e25e85b709ed8fa8a"
 
   url "https://github.com/jwhamilton99/menu-bar-splitter/releases/download/#{version}/menu-bar-splitter-v#{version}.zip"
   name "Menu Bar Splitter"
-  desc "Utility for keeping menu bar organized"
+  desc "A macOS utility for keeping your menu bar organized."
   homepage "https://github.com/jwhamilton99/menu-bar-splitter"
 
   depends_on macos: ">= :catalina"
