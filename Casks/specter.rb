e82d3bd7f2cabf4316d5034caf016ea16ca0e611
wikiwoka <wikiwoka@protonmail.com>
2021-07-17 15:13:03 +0200

    Update specter from 1.5.0 to 1.5.0

--- Casks/specter.rb
+++ Casks/specter.rb
@@ -1,6 +1,6 @@
 cask "specter" do
-  version "1.4.6"
-  sha256 "32bce09e7147f7b37588318ba2fc1397410cc069a484486040effab4dde4b82f"
+  version "1.5.0"
+  sha256 "63ec10c26e51704ca84a249ccd15edf5d14891d22f7f29153fbd4d5b52c4a21d"
 
   url "https://github.com/cryptoadvance/specter-desktop/releases/download/v#{version}/SpecterDesktop-v#{version}.dmg",
       verified: "github.com/cryptoadvance/specter-desktop/"
