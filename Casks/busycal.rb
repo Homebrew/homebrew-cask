9461439973abfee5218ad163e9945c361299be95
Bevan Kay <email@bevankay.me>
2021-07-18 16:03:13 +1000

    update busycal.rb

--- Casks/busycal.rb
+++ Casks/busycal.rb
@@ -21,15 +21,15 @@ cask "busycal" do
 
   pkg "BusyCal Installer.pkg"
 
-  uninstall pkgutil: "com.busymac.busycal#{version.major}.pkg",
-            quit:    "N4RA379GBW.com.busymac.busycal#{version.major}.alarm",
-            signal:  ["TERM", "com.busymac.busycal#{version.major}"]
+  uninstall pkgutil: "com.busymac.busycal#{version.minor}.pkg",
+            quit:    "N4RA379GBW.com.busymac.busycal#{version.minor}.alarm",
+            signal:  ["TERM", "com.busymac.busycal#{version.minor}"]
 
   zap trash: [
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.busymac.busycal#{version.major}.sfl*",
-    "~/Library/Containers/com.busymac.busycal#{version.major}",
-    "~/Library/Containers/N4RA379GBW.com.busymac.busycal#{version.major}.alarm",
-    "~/Library/Group Containers/com.busymac.busycal#{version.major}",
-    "~/Library/Group Containers/N4RA379GBW.com.busymac.busycal#{version.major}",
+    "~/Library/Containers/com.busymac.busycal#{version.minor}",
+    "~/Library/Containers/N4RA379GBW.com.busymac.busycal#{version.minor}.alarm",
+    "~/Library/Group Containers/com.busymac.busycal#{version.minor}",
+    "~/Library/Group Containers/N4RA379GBW.com.busymac.busycal#{version.minor}",
   ]
 end
