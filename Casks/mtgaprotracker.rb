97919231a91baff08f1132f1b29aa8ac936e38a8
Bevan Kay <email@bevankay.me>
2021-07-19 08:29:43 +1000

    Update mtgaprotracker from 2.1.24 to 2.1.27 (#108715)

--- Casks/mtgaprotracker.rb
+++ Casks/mtgaprotracker.rb
@@ -1,6 +1,6 @@
 cask "mtgaprotracker" do
-  version "2.1.24"
-  sha256 "c22e28f4b5135011bb6c6440ab0e3a04e1180895ff1d00cf965d3d5c74821764"
+  version "2.1.27"
+  sha256 "96c47b4612fac3860c1836689ff4ada3043144ee41f3413c8c1805134f210bd7"
 
   url "https://github.com/Razviar/mtgap/releases/download/v#{version}/mtgaprotracker.dmg",
       verified: "github.com/Razviar/mtgap/"
