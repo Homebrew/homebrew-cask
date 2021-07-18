b553fedb219e7ad3b83926591d7d3e447dd7ba9b
Alireza Asadollahi <aliasadollahi@aol.com>
2021-07-18 07:16:26 +0000

    Update appcast url

--- Casks/vmware-horizon-client.rb
+++ Casks/vmware-horizon-client.rb
@@ -3,7 +3,7 @@ cask "vmware-horizon-client" do
   sha256 "59820608c6e6515b1a4bece79f586dbee2eabbadf34c958a27e2635c031df5e5"
 
   url "https://download3.vmware.com/software/view/viewclients/#{version.after_comma}/VMware-Horizon-Client-#{version.before_comma}.dmg"
-  appcast "https://my.vmware.com/channel/public/api/v1.0/dlg/details?locale=en_US&downloadGroup=#{version.after_comma}_MAC_2103"
+  appcast "https://my.vmware.com/channel/public/api/v1.0/dlg/details?locale=en_US&downloadGroup=#{version.after_comma}_MAC_2106"
   name "VMware Horizon Client"
   desc "Virtual machine client"
   homepage "https://www.vmware.com/"
