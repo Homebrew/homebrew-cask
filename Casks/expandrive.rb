d5b864ccc13e2efcb51e92b93f96e3f044d8e2e6
Bevan Kay <email@bevankay.me>
2021-07-18 17:16:58 +1000

    Update expandrive from 7.7.9 to 2021.6.4

--- Casks/expandrive.rb
+++ Casks/expandrive.rb
@@ -1,6 +1,6 @@
 cask "expandrive" do
-  version "7.7.9"
-  sha256 "b2bfcc474626e4ea0be1ff1ab987ba0dcd9259530909ad3021f6088d6f3e5de6"
+  version "2021.6.4"
+  sha256 "0b3a624999cdefd9e546a9c1e49b692913284f44be8bf9ab94e2288abaa50768"
 
   url "https://updates.expandrive.com/apps/expandrive#{version.major}/v/#{version.dots_to_hyphens}/update_download"
   name "ExpanDrive"
