28e2d4b8b104e52b70ca8cd961a69eb166f13104
Phil Hannent <phil@elmstonesystems.co.uk>
2021-07-18 14:21:25 +0100

    add the verified parameter

--- Casks/lynx.rb
+++ Casks/lynx.rb
@@ -2,7 +2,8 @@ cask "lynx" do
   version "7.3.7.0"
   sha256 :no_check
 
-  url "https://download.saharasupport.com/lynx7/production/macx/Lynx7-install.dmg"
+  url "https://download.saharasupport.com/lynx7/production/macx/Lynx7-install.dmg",
+      verified: "download.saharasupport.com"
   name "LYNX Whiteboard by Clevertouch"
   desc "Cross platform presentation and productivity App"
   homepage "https://www.lynxcloud.app/"
@@ -17,7 +18,7 @@ cask "lynx" do
   pkg "Lynx7.pkg"
 
   uninstall pkgutil: [
-      "uk.co.cleverproducts.lynx",
-      "com.clevertouch.lynx",
+    "uk.co.cleverproducts.lynx",
+    "com.clevertouch.lynx",
   ]
 end
