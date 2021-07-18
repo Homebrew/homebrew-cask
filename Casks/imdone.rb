2a692f613db3f8bff713d8552aca021c24892782
Bevan Kay <email@bevankay.me>
2021-07-18 19:59:28 +1000

    Update imdone from 1.18.5 to 1.19.3

--- Casks/imdone.rb
+++ Casks/imdone.rb
@@ -1,6 +1,6 @@
 cask "imdone" do
-  version "1.18.5"
-  sha256 "9271ecd4b27461bb8f7119b74fc0743ad0816b535025db81d8a679b82f2e32b0"
+  version "1.19.3"
+  sha256 "188b005bd1d48e1318fabd80e1d8e46ff0229df235c168a153e0e1d7426a7e69"
 
   url "https://imdone.io/downloads/imdone-#{version}.dmg"
   name "imdone"
