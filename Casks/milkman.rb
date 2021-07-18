fc1e3087a6ab0ace25204ae9dfd5a018dd329afa
Bevan Kay <email@bevankay.me>
2021-07-19 08:32:26 +1000

    Update milkman from 5.2.0 to 5.3.0 (#108699)

--- Casks/milkman.rb
+++ Casks/milkman.rb
@@ -1,6 +1,6 @@
 cask "milkman" do
-  version "5.2.0"
-  sha256 "0ac66d97f7cfa506123862276c24f9f24e649ab29140e5474a59c2d28c67fd3a"
+  version "5.3.0"
+  sha256 "5952955f5c0027435460b35bcb64019b843eaf1edbd7af2be8eb8febe845fcba"
 
   url "https://github.com/warmuuh/milkman/releases/download/#{version}/milkman-dist-appbundle-macos64-bin.tgz"
   name "Milkman"
