4198326b9539df23c3bffc9de026360606e03191
Bevan Kay <email@bevankay.me>
2021-07-18 15:42:35 +1000

    Update banksiagui from 0.45 to 0.46

--- Casks/banksiagui.rb
+++ Casks/banksiagui.rb
@@ -1,6 +1,6 @@
 cask "banksiagui" do
-  version "0.45"
-  sha256 "005930300796a330a80d70233b53d97fca6915bbe1dc4d27597d066a8f584415"
+  version "0.46"
+  sha256 "f6baa89895329076c7309841fcd4cae5ff17fdf48cce07d300991e95cfad7b07"
 
   url "https://banksiagui.com/dl/BanksiaGui-#{version}-mac.zip"
   name "BanksiaGui"
