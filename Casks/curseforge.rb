467b123b035e56fd13f082431bcf302561e97d4c
Bevan Kay <email@bevankay.me>
2021-07-19 07:36:35 +1000

    curseforge: add version, livecheck and auto_updates (#108553)
    * curseforge: add version, livecheck and auto_updates
    
    * curseforge: update url, add checksum

--- Casks/curseforge.rb
+++ Casks/curseforge.rb
@@ -1,11 +1,18 @@
 cask "curseforge" do
-  version :latest
-  sha256 :no_check
+  version "0.177.1-3"
+  sha256 "69030db3046326be4a17271caa1d616480c3b21f5e698695f2108df1c16dd320"
 
-  url "https://curseforge.overwolf.com/downloads/curseforge-latest.dmg"
+  url "https://curseforge.overwolf.com/electron/mac/CurseForge-#{version}.dmg"
   name "CurseForge"
   desc "Download and manage your addons and mods"
   homepage "https://curseforge.overwolf.com/"
+
+  livecheck do
+    url "https://curseforge.overwolf.com/electron/mac/latest-mac.yml"
+    strategy :electron_builder
+  end
+
+  auto_updates true
 
   app "CurseForge.app"
 
