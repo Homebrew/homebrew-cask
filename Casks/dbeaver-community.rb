3673dca4e4c316708c1f5acbbae5064fa26472af
Bevan Kay <email@bevankay.me>
2021-07-19 08:28:37 +1000

    Update dbeaver-community from 21.1.2 to 21.1.3 (#108722)

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,6 +1,6 @@
 cask "dbeaver-community" do
-  version "21.1.2"
-  sha256 "6c56313d2d5c153cee1b6479008d9c1723829ed1066913571d6d40f335d4616e"
+  version "21.1.3"
+  sha256 "39fed978f06a83ca7a50dcd64ec9251ceab7f148370a5ad6e1c0d2a0f8d8afe3"
 
   url "https://dbeaver.io/files/#{version}/dbeaver-ce-#{version}-macos.dmg"
   name "DBeaver Community Edition"
