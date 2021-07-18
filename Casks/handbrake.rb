c47e0e45a3f5732655a725ececa6e3f06ce87463
Luflosi <Luflosi@users.noreply.github.com>
2021-07-18 17:33:43 +0200

    Update handbrake from 1.3.3 to 1.4.0 (#108667)

--- Casks/handbrake.rb
+++ Casks/handbrake.rb
@@ -1,6 +1,6 @@
 cask "handbrake" do
-  version "1.3.3"
-  sha256 "500cb924de7a3cc3a95cac4e69f303fc0623d3524fe59ac4ee4160ff43d56af6"
+  version "1.4.0"
+  sha256 "1dd4d6be8b25e12fd4fbc370dc084a0855243f3d8e157611ba7dcdba3a41d1a6"
 
   url "https://github.com/HandBrake/HandBrake/releases/download/#{version}/HandBrake-#{version}.dmg",
       verified: "github.com/HandBrake/HandBrake/"
