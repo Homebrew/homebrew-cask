bca2e1226eccffe7f523c110dfee6bfa0b3f1042
Bevan Kay <email@bevankay.me>
2021-07-19 07:58:21 +1000

    Update mountain-duck from 4.6.2,18221 to 4.7.0,18302

--- Casks/mountain-duck.rb
+++ Casks/mountain-duck.rb
@@ -1,6 +1,6 @@
 cask "mountain-duck" do
-  version "4.6.2,18221"
-  sha256 "01f47701216b7165efa15993348d78b0fcdf65a7588978c594c4f3741d7e022d"
+  version "4.7.0,18302"
+  sha256 "8e49bfd30dc02d46fe01a0b01fd6123275b5ba3e0b25b6b803f7e8247f0cba65"
 
   url "https://dist.mountainduck.io/Mountain%20Duck-#{version.before_comma}.#{version.after_comma}.zip"
   name "Mountain Duck"
