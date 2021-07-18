564ba4d6863592db5eb394dabcf4694fccba2f5a
Bevan Kay <email@bevankay.me>
2021-07-19 09:31:49 +1000

    Update nault from 1.12.0 to 1.14.0

--- Casks/nault.rb
+++ Casks/nault.rb
@@ -1,6 +1,6 @@
 cask "nault" do
-  version "1.12.0"
-  sha256 "61afdb81afb0c83a06c4debcaad8389c15a9e60842a683fb21bb1dddb7717de1"
+  version "1.14.0"
+  sha256 "261f4b9b86cbff29a4856c1182c3e904ac9838a2190c9c7a7907da9b64bd2ad1"
 
   url "https://github.com/Nault/Nault/releases/download/v#{version}/Nault-#{version}-Mac.dmg"
   name "Nault"
