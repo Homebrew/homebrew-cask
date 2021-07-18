1d8af48243fb9406499d16b856dd1a4540c524b7
Jeff <31933684+upkit@users.noreply.github.com>
2021-07-18 23:32:25 +0800

    Update utools from 1.3.5 to 2.0.7 (#108664)

--- Casks/utools.rb
+++ Casks/utools.rb
@@ -1,15 +1,15 @@
 cask "utools" do
-  version "1.3.5"
-  sha256 "49a085440c9a38faabfb17b148f530ab8abf7f946599681d3523f2f94d07cc5d"
+  version "2.0.7"
+  sha256 "e27d1e8e272df5a55af8bee37dae8b6e3a3ea6ccbc89536abd7e269096e9779b"
 
-  url "https://res.u-tools.cn/currentversion/uTools-#{version}.dmg",
-      verified: "res.u-tools.cn/"
+  url "https://publish.u-tools.cn/version2/uTools-#{version}.dmg",
+      verified: "publish.u-tools.cn/"
   name "uTools"
   desc "Plug-in productivity tool set"
   homepage "https://u.tools/index.html"
 
   livecheck do
-    url "https://res.u-tools.cn/currentversion/public-mac.yml"
+    url "https://publish.u-tools.cn/version2/public-mac.yml"
     strategy :electron_builder
   end
 
