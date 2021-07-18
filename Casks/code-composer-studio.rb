fe5971cfce98edce6ca3bba87fd86a2b9d61fe9b
Bevan Kay <email@bevankay.me>
2021-07-18 16:39:59 +1000

    Update code-composer-studio from 10.3.1.00003 to 10.4.0.00006 (#108588)

--- Casks/code-composer-studio.rb
+++ Casks/code-composer-studio.rb
@@ -1,6 +1,6 @@
 cask "code-composer-studio" do
-  version "10.3.1.00003"
-  sha256 "02a78731bd617d7e29ab306e70f2faaf9cabfcc53402dc7d5f5649f048be535d"
+  version "10.4.0.00006"
+  sha256 "819579753408fa0e1ee0be7a1e2bea6a1e470907e45174ab5fe64fb62ad6d14c"
 
   url "https://software-dl.ti.com/ccs/esd/CCSv10/CCS_#{version.major_minor_patch.dots_to_underscores}/exports/ccs_installer_osx_#{version}.dmg"
   name "Code Composer Studio (CCS)"
