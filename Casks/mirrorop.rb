795744a9bddcd76e81203023d5533293e7a88322
Bevan Kay <email@bevankay.me>
2021-07-19 08:31:20 +1000

    Update mirrorop from 2.5.3.87 to 2.5.4.89 (#108704)

--- Casks/mirrorop.rb
+++ Casks/mirrorop.rb
@@ -1,6 +1,6 @@
 cask "mirrorop" do
-  version "2.5.3.87"
-  sha256 "d505869d9737418f7a39e9b11d8a9934508830866e02746504c36d2612567bc9"
+  version "2.5.4.89"
+  sha256 "898376b108bcf517f6fedd34b10aca87a12a9a243584fc982893f550536bf7d0"
 
   url "https://www.barco.com/services/website/en/TdeFiles/Download?FileNumber=R33050100&TdeType=3&MajorVersion=#{version.major}&MinorVersion=#{version.minor}&PatchVersion=#{version.patch}&BuildVersion=#{version.split(".")[-1]}"
   name "MirrorOp Sender"
