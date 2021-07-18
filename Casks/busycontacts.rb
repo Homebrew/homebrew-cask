3f14f0dc3cba466a52dc1253558d40a30bcf07b5
Bevan Kay <email@bevankay.me>
2021-07-18 16:41:06 +1000

    Update busycontacts from 1.5.2.1,2021-04-15-15-21 to 1.6.0,2021-07-16-16-21 (#108581)

--- Casks/busycontacts.rb
+++ Casks/busycontacts.rb
@@ -1,6 +1,6 @@
 cask "busycontacts" do
-  version "1.5.2.1,2021-04-15-15-21"
-  sha256 "c8255ca675c423b6dbfc32ebca87dd8b77e72da6b170887aa55c4293b989bf17"
+  version "1.6.0,2021-07-16-16-21"
+  sha256 "9b95a95f0c3d56a819a0547d2081081a17e0ab6f0bf3c2aefb3328133edd1679"
 
   url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bct-#{version.before_comma}-#{version.after_comma}.zip",
       verified: "7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/"
