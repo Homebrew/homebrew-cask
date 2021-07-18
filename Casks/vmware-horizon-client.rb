8a31f9be319d95fd1b39caaf3502884d3ca873a1
Alireza Asadollahi <aliasadollahi@aol.com>
2021-07-18 21:46:17 +0000

    Update vmware-horizon-client from 2103-8.2.0-17759009,CART22FQ1 to 2106-8.3.0-18251973,CART22FQ2 (#108559)
    * Update vmware-horizon-client from 2103-8.2.0-17759009,CART22FQ1 to 2106-8.3.0-18251973,CART22FQ2
    
    * Update appcast url

--- Casks/vmware-horizon-client.rb
+++ Casks/vmware-horizon-client.rb
@@ -1,9 +1,9 @@
 cask "vmware-horizon-client" do
-  version "2103-8.2.0-17759009,CART22FQ1"
-  sha256 "ee6b085e3632d0d1a9aae6a58ca6d02b299ed8bbb82062eb804ebe04b30ed519"
+  version "2106-8.3.0-18251973,CART22FQ2"
+  sha256 "59820608c6e6515b1a4bece79f586dbee2eabbadf34c958a27e2635c031df5e5"
 
   url "https://download3.vmware.com/software/view/viewclients/#{version.after_comma}/VMware-Horizon-Client-#{version.before_comma}.dmg"
-  appcast "https://my.vmware.com/channel/public/api/v1.0/dlg/details?locale=en_US&downloadGroup=#{version.after_comma}_MAC_2103"
+  appcast "https://my.vmware.com/channel/public/api/v1.0/dlg/details?locale=en_US&downloadGroup=#{version.after_comma}_MAC_2106"
   name "VMware Horizon Client"
   desc "Virtual machine client"
   homepage "https://www.vmware.com/"
