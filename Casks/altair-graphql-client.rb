3794b40c58b78bd90c1c290c7c96658b380e1edf
Bevan Kay <email@bevankay.me>
2021-07-18 15:29:55 +1000

    Update altair-graphql-client from 4.0.6 to 4.0.7 (#108568)

--- Casks/altair-graphql-client.rb
+++ Casks/altair-graphql-client.rb
@@ -1,13 +1,13 @@
 cask "altair-graphql-client" do
-  version "4.0.6"
+  version "4.0.7"
 
   if Hardware::CPU.intel?
-    sha256 "a4f4d867d1bfad2d2c5e468ab21b10726ef7ec40508b4859b2d27e006c775c51"
+    sha256 "79461a75c1f1b04c1114be510ad45360cba9b4f79da8c5b74012e9b50a4039cb"
 
     url "https://github.com/imolorhe/altair/releases/download/v#{version}/altair_#{version}_mac.zip",
         verified: "github.com/imolorhe/altair/"
   else
-    sha256 "6ececd190e56d1357dbbfbe5ef22646d7deebaa60c17736d0939662110af5f48"
+    sha256 "d8571c8fc7e74d632179b5fe316f7886941fcc01a1c848b289cdf070724a0ea4"
 
     url "https://github.com/imolorhe/altair/releases/download/v#{version}/altair_#{version}_arm64_mac.zip",
         verified: "github.com/imolorhe/altair/"
