e42c44108bdb148750cf73f24dc21f44bdd291ff
Bevan Kay <email@bevankay.me>
2021-07-19 07:20:03 +1000

    Update lynx.rb

--- Casks/lynx.rb
+++ Casks/lynx.rb
@@ -2,20 +2,20 @@ cask "lynx" do
   version "7.3.7.0"
   sha256 :no_check
 
-  url "https://download.saharasupport.com/lynx7/production/macx/Lynx7-install.dmg",
+  url "https://download.saharasupport.com/lynx#{version.major}/production/macx/Lynx#{version.major}-install.dmg",
       verified: "download.saharasupport.com"
   name "LYNX Whiteboard by Clevertouch"
   desc "Cross platform presentation and productivity App"
   homepage "https://www.lynxcloud.app/"
 
   livecheck do
-    url "https://downloads.saharasupport.com/lynx7/production/macx/version.txt"
+    url "https://downloads.saharasupport.com/lynx#{version.major}/production/macx/version.txt"
     regex(/(\d+(?:[._-]\d+)+)/)
   end
 
   auto_updates true
 
-  pkg "Lynx7.pkg"
+  pkg "Lynx#{version.major}.pkg"
 
   uninstall pkgutil: [
     "uk.co.cleverproducts.lynx",
