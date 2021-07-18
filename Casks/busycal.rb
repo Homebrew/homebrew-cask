a8fd754b980b2aa76f91e3f44988711958649d9e
Bevan Kay <email@bevankay.me>
2021-07-19 07:37:23 +1000

    Update busycal from 3.12.6.1,2021-04-17-05-08 to 2021.3.1,2021-07-18-01-16 (#108580)
    * Update busycal from 3.12.6.1,2021-04-17-05-08 to 2021.3.1,2021-07-18-01-16
    
    * update busycal.rb

--- Casks/busycal.rb
+++ Casks/busycal.rb
@@ -1,6 +1,6 @@
 cask "busycal" do
-  version "3.12.6.1,2021-04-17-05-08"
-  sha256 "cd6252d562a879ce8aba4f8739b9867a9b462d6fb0c3117732857589fbd1ab19"
+  version "2021.3.1,2021-07-18-01-16"
+  sha256 "d809c7bf5b4d599b67071ed13bc2632f32258a3cd1ac81e976bffa0439bb8f29"
 
   url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bcl-#{version.before_comma}-#{version.after_comma}.zip",
       verified: "7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/"
@@ -21,15 +21,15 @@ cask "busycal" do
 
   pkg "BusyCal Installer.pkg"
 
-  uninstall pkgutil: "com.busymac.busycal#{version.major}.pkg",
-            quit:    "N4RA379GBW.com.busymac.busycal#{version.major}.alarm",
-            signal:  ["TERM", "com.busymac.busycal#{version.major}"]
+  uninstall pkgutil: "com.busymac.busycal#{version.minor}.pkg",
+            quit:    "N4RA379GBW.com.busymac.busycal#{version.minor}.alarm",
+            signal:  ["TERM", "com.busymac.busycal#{version.minor}"]
 
   zap trash: [
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.busymac.busycal#{version.major}.sfl*",
-    "~/Library/Containers/com.busymac.busycal#{version.major}",
-    "~/Library/Containers/N4RA379GBW.com.busymac.busycal#{version.major}.alarm",
-    "~/Library/Group Containers/com.busymac.busycal#{version.major}",
-    "~/Library/Group Containers/N4RA379GBW.com.busymac.busycal#{version.major}",
+    "~/Library/Containers/com.busymac.busycal#{version.minor}",
+    "~/Library/Containers/N4RA379GBW.com.busymac.busycal#{version.minor}.alarm",
+    "~/Library/Group Containers/com.busymac.busycal#{version.minor}",
+    "~/Library/Group Containers/N4RA379GBW.com.busymac.busycal#{version.minor}",
   ]
 end
