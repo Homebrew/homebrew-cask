c9200802aa2b7e407a1aef01c7047ddb2c02ce73
Bevan Kay <email@bevankay.me>
2021-07-18 20:53:40 +1000

    Update krisp from 1.22.7 to 1.23.7

--- Casks/krisp.rb
+++ Casks/krisp.rb
@@ -1,6 +1,6 @@
 cask "krisp" do
-  version "1.22.7"
-  sha256 "504ab202842d9b3fc6c291effe564710e779c543ba4b25a29697459bb44a47b1"
+  version "1.23.7"
+  sha256 "491740e7158de1c2ebf0f848d5c0c1f6d498f939b9520a98d758d6a1824eb035"
 
   url "https://cdn.krisp.ai/mac/release/v#{version.major}.#{version.minor}/krisp_#{version}.pkg"
   name "Krisp"
