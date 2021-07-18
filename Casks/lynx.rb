7ddd1bbfb89cef5879766275165a9817ec4c06d3
Phil Hannent <phil@elmstonesystems.co.uk>
2021-07-18 13:17:37 +0100

    Update Lynx from 6.6.6 to 7.3.7

--- Casks/lynx.rb
+++ Casks/lynx.rb
@@ -1,20 +1,23 @@
 cask "lynx" do
-  version "6.6.6.0"
+  version "7.3.7.0"
   sha256 :no_check
 
-  url "https://download.saharasupport.com/lynx/production/macx/Lynx-install.dmg"
-  name "Lynx by Clevertouch"
-  desc "Clevertouch software installer"
-  homepage "https://download.saharasupport.com/"
+  url "https://download.saharasupport.com/lynx7/production/macx/Lynx7-install.dmg"
+  name "LYNX Whiteboard by Clevertouch"
+  desc "Cross platform presentation and productivity App"
+  homepage "https://www.lynxcloud.app/"
 
   livecheck do
-    url "https://downloads.saharasupport.com/lynx/production/macx/version.txt"
+    url "https://downloads.saharasupport.com/lynx7/production/macx/version.txt"
     regex(/(\d+(?:[._-]\d+)+)/)
   end
 
   auto_updates true
 
-  pkg "Lynx.pkg"
+  pkg "Lynx7.pkg"
 
-  uninstall pkgutil: "uk.co.cleverproducts.lynx"
+  uninstall pkgutil: [
+      "uk.co.cleverproducts.lynx",
+      "com.clevertouch.lynx",
+  ]
 end
