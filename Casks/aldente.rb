a3033f2a5d9e873199714f11cefc339230722980
Bevan Kay <email@bevankay.me>
2021-07-18 19:48:06 +1000

    fix style

--- Casks/aldente.rb
+++ Casks/aldente.rb
@@ -1,8 +1,8 @@
 cask "aldente" do
   version "1.04"
   sha256 "205b44132a8cd5d3777e564092b31732cca26dfb3e90893275537420502da538"
-  url "https://github.com/davidwernhart/AlDente/releases/download/#{version}/AlDente_#{version}.dmg"
 
+  url "https://github.com/davidwernhart/AlDente/releases/download/#{version}/AlDente_#{version}.dmg"
   name "AlDente"
   desc "Menu bar tool to limit maximum charging percentage"
   homepage "https://github.com/davidwernhart/AlDente"
@@ -14,9 +14,10 @@ cask "aldente" do
               "/Library/LaunchDaemons/com.apphousekitchen.aldente-pro.helper.plist",
               "/Library/PrivilegedHelperTools/com.apphousekitchen.aldente-pro.helper",
             ]
+
   zap trash: [
     "~/Library/Application Support/AlDente",
     "~/Library/HTTPStorages/com.apphousekitchen.aldente-pro.binarycookies",
-    "~/Library/Caches/com.apphousekitchen.aldente-pro"
-      ]
+    "~/Library/Caches/com.apphousekitchen.aldente-pro",
+  ]
 end
