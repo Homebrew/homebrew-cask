f44989588aa914744aa6bda50f956faf455f389d
Bevan Kay <email@bevankay.me>
2021-07-19 08:32:45 +1000

    Update metabase from 0.39.4 to 0.40.1 (#108693)

--- Casks/metabase.rb
+++ Casks/metabase.rb
@@ -1,6 +1,6 @@
 cask "metabase" do
-  version "0.39.4"
-  sha256 "ac6a066db6fd571d9b13bd398ed7336a72f7e166fd347e4ec4e6b55c5bed4385"
+  version "0.40.1"
+  sha256 "1dc8acf5f902de38401b14f705d0a9cc1ce11c594ae6d9f23c278be595673fc7"
 
   url "https://s3.amazonaws.com/downloads.metabase.com/v#{version}/Metabase.zip",
       verified: "s3.amazonaws.com/downloads.metabase.com/"
