368c90c5dc2456f13e7b9a795562f633be81b812
MoonFruit <dkmoonfruit@gmail.com>
2021-07-19 13:16:19 +0800

    Update telegram-desktop from 2.8.3 to 2.8.10 (#108751)
    * Update telegram-desktop from 2.8.3 to 2.8.9
    
    * update url, update livecheck
    
    * Update telegram-desktop.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/telegram-desktop.rb
+++ Casks/telegram-desktop.rb
@@ -1,16 +1,16 @@
 cask "telegram-desktop" do
-  version "2.8.3"
-  sha256 "73d0330a8a77d1d80ed3258b6df1de210848387aec3573918e373d0661717b11"
+  version "2.8.10"
+  sha256 "a2ab50c9ed9789af0be5f996134339c89c1e4f040a04438177533ceeb2697180"
 
-  url "https://github.com/telegramdesktop/tdesktop/releases/download/v#{version}/tsetup.#{version}.dmg",
-      verified: "github.com/telegramdesktop/tdesktop/"
+  url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg",
+      verified: "updates.tdesktop.com/tmac/"
   name "Telegram Desktop"
   desc "Desktop client for Telegram messenger"
   homepage "https://desktop.telegram.org/"
 
   livecheck do
-    url :url
-    strategy :github_latest
+    url "https://telegram.org/dl/desktop/mac"
+    strategy :header_match
   end
 
   auto_updates true
