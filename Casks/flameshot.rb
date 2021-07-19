1ffba7363f1b13cc46d15f173b2ed2690216587e
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-07-19 20:17:32 +0900

    Update flameshot from 0.9.0 to 0.10.0

--- Casks/flameshot.rb
+++ Casks/flameshot.rb
@@ -1,6 +1,6 @@
 cask "flameshot" do
-  version "0.9.0"
-  sha256 "fb593aaf5f060c323207eaf33444b9a6e5e2dd62734f71367876104ff35a9d03"
+  version "0.10.0"
+  sha256 "8433c67c89a6c1346abea46e0e01d57ed87f610b5d5a293c244974a61828c8a4"
 
   url "https://github.com/flameshot-org/flameshot/releases/download/v#{version}/flameshot.dmg",
       verified: "github.com/flameshot-org/flameshot/"
