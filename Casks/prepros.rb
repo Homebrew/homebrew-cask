f8ee332327c3ac4dd720f0ea04884d5f45126016
Bevan Kay <email@bevankay.me>
2021-07-19 16:01:12 +1000

    Update prepros from 7.3.41 to 7.3.48

--- Casks/prepros.rb
+++ Casks/prepros.rb
@@ -1,6 +1,6 @@
 cask "prepros" do
-  version "7.3.41"
-  sha256 "6186afc68d32b2744bb740b21e9c1234ff5b6ae751ccd35ca081027013e6f1b7"
+  version "7.3.48"
+  sha256 "138501cef2671c697065df0f1f9aa9a80434562ac220882c9282698e3f635fe6"
 
   url "https://downloads.prepros.io/v#{version.major}/Prepros-#{version}.zip"
   name "Prepros"
