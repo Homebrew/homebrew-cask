5dd285686cf21adf3defb1ad533fd5f7ddf4376c
Bevan Kay <email@bevankay.me>
2021-07-19 16:38:45 +1000

    Update raindropio from 5.3.33 to 5.3.36

--- Casks/raindropio.rb
+++ Casks/raindropio.rb
@@ -1,13 +1,13 @@
 cask "raindropio" do
-  version "5.3.33"
+  version "5.3.36"
 
   if Hardware::CPU.intel?
-    sha256 "22aadd354e7baa4e40a47ff6be285425a2326ecd47ace4fbe5ddcd0e375b3384"
+    sha256 "741730eba1d69239243d15e49c8a8fa89d0b599e68e81e7f0e4ea5f31e54d881"
 
     url "https://github.com/raindropio/desktop/releases/download/v#{version}/Raindrop-x64.dmg",
         verified: "github.com/raindropio/desktop/"
   else
-    sha256 "560bfc997d5ef881e779304fe255e5d66fbefb50d0c05c368f08f195b9d54c1c"
+    sha256 "c9e78910f94a6c0ae9f3fb988c19143fdc1b82a2fa452f03e0617bab81f50c63"
 
     url "https://github.com/raindropio/desktop/releases/download/v#{version}/Raindrop-arm64.dmg",
         verified: "github.com/raindropio/desktop/"
