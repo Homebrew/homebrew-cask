480a8d806e7d745e45881be1c4bd50ae63e0445f
Reno <25192197+singularitti@users.noreply.github.com>
2021-07-19 18:52:05 -0400

    Update typeface from 3.1.0,2852 to 3.2.0,2900 (#108832)

--- Casks/typeface.rb
+++ Casks/typeface.rb
@@ -1,6 +1,6 @@
 cask "typeface" do
-  version "3.1.0,2852"
-  sha256 "69de5cab22d3bce752574326f23b0315e69e52805a8c3b8cc3cff15d76428d36"
+  version "3.2.0,2900"
+  sha256 "5e66efdad214c4fbe3cd5b7de0c87b58ae5d47a0edc9a4ce65b6d503a4c640de"
 
   url "https://dcdn.typefaceapp.com/Typeface-#{version.before_comma}-#{version.after_comma}/Typeface-#{version.before_comma}-#{version.after_comma}.dmg"
   name "Typeface"
