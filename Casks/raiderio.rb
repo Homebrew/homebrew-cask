bfec55b621fd682647133c6b2407cae34f119c9b
Bevan Kay <email@bevankay.me>
2021-07-19 16:35:26 +1000

    Update raiderio from 2.1.7 to 2.1.8

--- Casks/raiderio.rb
+++ Casks/raiderio.rb
@@ -1,6 +1,6 @@
 cask "raiderio" do
-  version "2.1.7"
-  sha256 "c9522e472cc1a81fb85376f3df3a063ec800d8b3714a7b88b8418f9cfc904e9a"
+  version "2.1.8"
+  sha256 "b8ca6c2f6f9a2bd3e4710f8e718e91604d942eeb90649c7836f43ae4a30fa5f6"
 
   url "https://github.com/RaiderIO/raiderio-client-builds/releases/download/v#{version}/RaiderIO_Client.dmg",
       verified: "github.com/RaiderIO/raiderio-client-builds/"
