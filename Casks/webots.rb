32f968f9215d3871ab1cc7a0834df453c21675df
Benjamin Délèze <benjamin.deleze@cyberbotics.com>
2021-07-19 11:28:50 +0200

    Update webots from R2021a to R2021b (#108811)

--- Casks/webots.rb
+++ Casks/webots.rb
@@ -1,6 +1,6 @@
 cask "webots" do
-  version "R2021a"
-  sha256 "94ba34a3e1393e117a50c3864253f3e108f144cde30b334eee02e331036a3d36"
+  version "R2021b"
+  sha256 "fc118caa7e39f9c1415d7b9aa2809c351c7981495d2b9e8d21959a09e3c76525"
 
   url "https://github.com/cyberbotics/webots/releases/download/#{version}/webots-#{version}.dmg",
       verified: "github.com/cyberbotics/webots/"
