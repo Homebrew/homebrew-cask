70e6a619ec4cdd727db50834cd7995d0b97ef285
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-07-19 20:19:59 +0900

    Update front from 3.27.3 to 3.27.5

--- Casks/front.rb
+++ Casks/front.rb
@@ -1,6 +1,6 @@
 cask "front" do
-  version "3.27.3"
-  sha256 "3b951e7682b257c1021497385ab07910058bb25b3823853fa30125d752d0d6a5"
+  version "3.27.5"
+  sha256 "af436e525177799efabf762b394bcc41407547bdb6db39b1558c04d97582e333"
 
   url "https://dl.frontapp.com/desktop/builds/#{version}/Front-#{version}.zip"
   name "Front"
