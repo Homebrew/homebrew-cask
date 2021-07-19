b20435dfeadae653e1e1bbf1c999f36d8612db25
Bevan Kay <email@bevankay.me>
2021-07-19 16:35:11 +1000

    Update qlab from 4.6.9,4609 to 4.6.10,4610 (#108790)

--- Casks/qlab.rb
+++ Casks/qlab.rb
@@ -1,6 +1,6 @@
 cask "qlab" do
-  version "4.6.9,4609"
-  sha256 "f2367d11ff74fbbd24f52c05c5c7b6bf65ffc5c3768138300ad8760821429d80"
+  version "4.6.10,4610"
+  sha256 "659da450d83e1f30d9515190c873ec95b95bf9a2e231e8bda2566819d32c176e"
 
   url "https://qlab.app/downloads/archive/QLab-#{version.before_comma}.zip"
   name "QLab"
