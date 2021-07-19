6cfc3878ce0de4f1ca34905892b1a26605e7427e
MoonFruit <dkmoonfruit@gmail.com>
2021-07-19 11:45:55 +0800

    Update tencent-meeting from 2.15.2.416 to 2.15.3.400 (#108752)

--- Casks/tencent-meeting.rb
+++ Casks/tencent-meeting.rb
@@ -1,12 +1,12 @@
 cask "tencent-meeting" do
   if Hardware::CPU.intel?
-    version "2.15.2.416,d028e1cbe56689560f23fc0cc220e19e"
-    sha256 "f0d4e4cd44b6477f549630888ee62e0afc3cbc5af6521fd7367bccf5e2a20413"
+    version "2.15.3.400,e7589f75224aa90911114449ac00a955"
+    sha256 "a3546cb7d87469cdb4a14070e0bd4796ec2cf728a9087c3666333a58dc68c319"
     url "https://updatecdn.meeting.qq.com/#{version.after_comma}/TencentMeeting_0300000000_#{version.before_comma}.publish.x86_64.dmg",
         verified: "qq.com/"
   else
-    version "2.13.3.435,19289abd97629d65ad8033a6d226c3c0"
-    sha256 "41180897931182331f8db8c88fc18e12cb37e225566a11fbd0af3f8ce6fa13f6"
+    version "2.15.3.400,6511c21f2ffa0b6c3c6521ca681a2f38"
+    sha256 "aa648bcc734deef2e6699e565439a6c3529574ac8e5f76c0d4113b91581d99ee"
     url "https://updatecdn.meeting.qq.com/#{version.after_comma}/TencentMeeting_0300000000_#{version.before_comma}.publish.arm64.dmg",
         verified: "qq.com/"
   end
