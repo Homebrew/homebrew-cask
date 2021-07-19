3c6b932648622770da91577bbf3a509ddbb42e82
Harry Marr <harry.marr@gmail.com>
2021-07-19 11:18:18 -0400

    Update Vitals to v0.6

--- Casks/vitals.rb
+++ Casks/vitals.rb
@@ -1,6 +1,6 @@
 cask "vitals" do
-  version "0.5"
-  sha256 "98b4f4e63f444d950c0e0bbfbcbbef3af4b1920da50addb98020552a696ef342"
+  version "0.6"
+  sha256 "499fe68c4060102c03ab9eb3e0cbd57cfb6999f036d7ef057cc1634ae659a6fe"
 
   url "https://github.com/hmarr/vitals/releases/download/v#{version}/vitals-v#{version}.zip"
   name "Vitals"
