5e705d589848f80da36e5c829c9df44b3ac2a3b9
Bevan Kay <email@bevankay.me>
2021-07-19 16:17:36 +1000

    Update polkadot-js from 0.92.3 to 0.94.1 (#108774)

--- Casks/polkadot-js.rb
+++ Casks/polkadot-js.rb
@@ -1,6 +1,6 @@
 cask "polkadot-js" do
-  version "0.92.3"
-  sha256 "9053695edd6bb29dcd36871db06487cdd8e76e964cadc192f989f9763c726b83"
+  version "0.94.1"
+  sha256 "c686a8439829062b1bfe07bdd061653c183b9975a9d5c411b5ef94029e41eccb"
 
   url "https://github.com/polkadot-js/apps/releases/download/v#{version}/Polkadot-JS-Apps-mac-#{version}.dmg",
       verified: "github.com/polkadot-js/apps/"
