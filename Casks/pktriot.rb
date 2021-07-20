3fceb38003a64aee3a4d8e222265fcfdc14a68af
Joshua Mulliken <joshua@mulliken.net>
2021-07-19 21:22:10 -0400

    Add Packetriot client v0.10.10

---
+++ Casks/pktriot.rb
@@ -0,0 +1,11 @@
+cask "pktriot" do
+  version "0.10.10"
+  sha256 "e98b9e307e2bc72cfac1cdad23eb81489db3ba0e140a6463cd486bebaff297de"
+
+  url "https://download.packetriot.com/macos/pktriot-#{version}.macos.tar.gz"
+  name "pktriot"
+  desc "Instantly and securely host server applications and static websites on the Internet."
+  homepage "https://packetriot.com"
+
+  binary "pktriot-#{version}/pktriot"
+end
