95a5d2974f541c3288bd9beeda9417ad71846028
Bevan Kay <email@bevankay.me>
2021-07-20 12:28:23 +1000

    Update syncmate from 8.1.475 to 8.2.491 (#108884)

--- Casks/syncmate.rb
+++ Casks/syncmate.rb
@@ -1,6 +1,6 @@
 cask "syncmate" do
-  version "8.1.475"
-  sha256 "991e806647abe4bbcf84f37872d64ed9dc11856e6f3270fd74ceb293a7415be7"
+  version "8.2.491"
+  sha256 "ef5c573984a93c45e97fa09d796845dd027d9dbd285056de07a3ec0cfe05772b"
 
   url "https://cdn.electronic.us/products/syncmate/mac/update/SyncMate_#{version}.zip",
       verified: "cdn.electronic.us/products/syncmate/"
