28580b09b04a2dfc4b4c6c7984caad089b7675dc
Bevan Kay <email@bevankay.me>
2021-07-20 11:46:34 +1000

    Update studio-3t from 2021.3.1 to 2021.4.1 (#108872)

--- Casks/studio-3t.rb
+++ Casks/studio-3t.rb
@@ -1,6 +1,6 @@
 cask "studio-3t" do
-  version "2021.3.1"
-  sha256 "78821b784ae3608aac491b3e7e9570cb68277f44bd1876e0b1d0442c1d56832d"
+  version "2021.4.1"
+  sha256 "37617fc9a1c4357ba5d30b2cc13b9ab00b2c83db04e22dd0b309b6f6fcd267cb"
 
   url "https://download.studio3t.com/studio-3t/mac/#{version}/Studio-3T.dmg"
   name "Studio 3T"
