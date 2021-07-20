749fe9b49dad2af86f6e386c7936b4d987bec321
Bevan Kay <email@bevankay.me>
2021-07-20 12:28:26 +1000

    Update syncovery from 9.35f to 9.38c (#108885)

--- Casks/syncovery.rb
+++ Casks/syncovery.rb
@@ -1,12 +1,12 @@
 cask "syncovery" do
-  version "9.35f"
+  version "9.38c"
 
   if Hardware::CPU.intel?
-    sha256 "2c32237c0d7888237f99d8f3ee2715c1204645f7ba4dc30520e1a3ae45af82f9"
+    sha256 "37772687b155b440dd469565b6a3082da71fbdafd8c59e5e5754b78db2facc55"
 
     url "https://www.syncovery.com/release/SyncoveryMac#{version}.dmg"
   else
-    sha256 "994b58125ccb1f01f875d9853b8021b1a86d3dd5d3c17f9bffeeb407a2355f67"
+    sha256 "6f25e37017311a6a7a9dfd915eb400de782a8ddac8cc14a26a80224543172afd"
 
     url "https://www.syncovery.com/release/SyncoveryMac#{version}-Apple.dmg"
   end
