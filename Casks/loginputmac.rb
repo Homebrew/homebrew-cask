bd99153c8dc9f9b2a977d0f5900fefc291534bae
R0uter <lomyr.jo@gmail.com>
2021-07-20 19:26:57 +1000

    Update loginputmac from 2.4.6,23561 to 2.4.7,23582

--- Casks/loginputmac.rb
+++ Casks/loginputmac.rb
@@ -1,6 +1,6 @@
 cask "loginputmac" do
-  version "2.4.6,23561"
-  sha256 "afdeed2e0b20c1806dbd4ac098bcdfc4b6f9b27edcee9ae96b74d4330c776708"
+  version "2.4.7,23582"
+  sha256 "7863bf4590cd57b0af5ac25599da3a923f32aad87d24fe0a43c6b468f1a1fc5c"
 
   url "https://loginput-mac2.content-delivery.top/loginputmac#{version.major}_latest.pkg",
       verified: "loginput-mac2.content-delivery.top/"
