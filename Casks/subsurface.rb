dc7ec5675255feb7ff1993c5be918bfd89500e5b
Bevan Kay <email@bevankay.me>
2021-07-20 11:51:33 +1000

    Update subsurface from 5.0.1 to 5.0.2 (#108875)

--- Casks/subsurface.rb
+++ Casks/subsurface.rb
@@ -1,11 +1,11 @@
 cask "subsurface" do
-  version "5.0.1"
+  version "5.0.2"
 
   if MacOS.version <= :mojave
     sha256 "34274051fe008486eccd01b31fcfdc793724276b505438184b35393ffd1199fe"
     url "https://subsurface-divelog.org/downloads/Subsurface-#{version}-10.13+14.dmg"
   else
-    sha256 "2d444c806ce8b3c51fb078e9f87b7b8418c2a9d1b1b3e9a6356ecc8bbd32f2c8"
+    sha256 "9d23dd5a5f0c4f4b2e3cd32ebe2ff2baa10b5d7e5d1cac0887024b7df2adb30b"
     url "https://subsurface-divelog.org/downloads/Subsurface-#{version}-10.15+11.0.dmg"
   end
 
