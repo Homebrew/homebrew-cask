95145edcc2c06ada504ea70ca50436f332632752
Bevan Kay <email@bevankay.me>
2021-07-20 12:28:00 +1000

    Update superslicer from 2.2.53.4,20201111 to 2.3.56.7,210710 (#108878)

--- Casks/superslicer.rb
+++ Casks/superslicer.rb
@@ -1,6 +1,6 @@
 cask "superslicer" do
-  version "2.2.53.4,20201111"
-  sha256 "04c07dbd71395134cc0d491d4219722aa2b9387d4a28f57b46cc52de672c2cf6"
+  version "2.3.56.7,210710"
+  sha256 "53d7f49d3162427d6355cf6cc8398a875d40de3b84274d548598f51be117c83e"
 
   url "https://github.com/supermerill/SuperSlicer/releases/download/#{version.before_comma}/SuperSlicer_#{version.before_comma}_macos_#{version.after_comma}.dmg"
   name "SuperSlicer"
