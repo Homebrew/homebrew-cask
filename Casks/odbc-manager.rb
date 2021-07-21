897122678d6e55ce98a082dfb4f3edbba8fcff55
James Fenwick <j.fenwick@me.com>
2021-07-21 11:11:29 +0100

    Add livecheck stanza and specify current version

--- Casks/odbc-manager.rb
+++ Casks/odbc-manager.rb
@@ -1,11 +1,17 @@
 cask "odbc-manager" do
-  version :latest
+  version "1.0.21"
   sha256 :no_check
 
   url "http://www.odbcmanager.net/downloads/ODBC_Manager64_Installer.dmg"
   name "ODBC Manager"
   desc "Free ODBC administrator"
   homepage "http://www.odbcmanager.net/"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(/Download ODBC Manager (\d+(?:\.\d+)*)/i)
+  end
 
   conflicts_with cask: "actual-odbc-pack"
 
