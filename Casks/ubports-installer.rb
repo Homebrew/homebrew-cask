250f556104a8bc857ed3b124186da230822cc981
Miccal Matthews <miccal.matthews@gmail.com>
2021-07-21 10:48:32 +0800

    Update ubports-installer.rb

--- Casks/ubports-installer.rb
+++ Casks/ubports-installer.rb
@@ -2,7 +2,8 @@ cask "ubports-installer" do
   version "0.8.8-beta"
   sha256 "57b296031cf7d2f72384d7b43e336a5b629ef01e3af9afbd7b8a3a622c6c8bf9"
 
-  url "https://github.com/ubports/ubports-installer/releases/download/#{version}/ubports-installer_#{version}_mac.dmg", verified: "github.com/ubports/ubports-installer/"
+  url "https://github.com/ubports/ubports-installer/releases/download/#{version}/ubports-installer_#{version}_mac.dmg",
+      verified: "github.com/ubports/ubports-installer/"
   name "ubports-installer"
   desc "Application to install ubports on mobile devices"
   homepage "https://ubports.com/"
