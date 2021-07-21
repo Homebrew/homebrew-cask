826b08effce31b594884b30b392c63fa2687858d
Hannes Braun <hannesbraun@mail.de>
2021-07-21 10:48:17 +0200

    Update visualvm from 2.0.7 to 2.1

--- Casks/visualvm.rb
+++ Casks/visualvm.rb
@@ -1,6 +1,6 @@
 cask "visualvm" do
-  version "2.0.7"
-  sha256 "50352c34f76ea7ab9368d2e381b499bfbe7f9d8ba96feb49ad436e33084647cc"
+  version "2.1"
+  sha256 "89c014f4b01c65c802ad0db913d5645b889c68e26974106ca91fa5e12730e926"
 
   url "https://github.com/oracle/visualvm/releases/download/#{version}/VisualVM_#{version.no_dots}.dmg",
       verified: "github.com/oracle/visualvm/"
