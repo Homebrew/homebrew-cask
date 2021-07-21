a37d1a150a46543393a28d13a032013697ff6560
Joshua Mulliken <joshua@mulliken.net>
2021-07-21 08:52:44 -0400

    Add zap stanza to installer

--- Casks/ubports-installer.rb
+++ Casks/ubports-installer.rb
@@ -14,4 +14,10 @@ cask "ubports-installer" do
   end
 
   app "ubports-installer.app"
+
+  zap trash: [
+               "~/Library/Application Support/ubports-installer",
+               "~/Library/Preferences/com.ubports.installer.plist",
+               "~/Library/Saved Application State/com.ubports.installer.savedState"
+             ]
 end
