3b0b5d5155821b7f6eeb671a2785b48e4e5ec16f
Reno <25192197+singularitti@users.noreply.github.com>
2021-07-22 19:04:04 -0400

    Update fvim from 0.2.348,81a158c to 0.2.392,g646a86f (#109026)

--- Casks/fvim.rb
+++ Casks/fvim.rb
@@ -1,6 +1,6 @@
 cask "fvim" do
-  version "0.2.348,81a158c"
-  sha256 "6e5ef4c8f9b04dd65e872eef2b3e7b4444fc1f1e797aeea5bae0062b66d5dda7"
+  version "0.2.392,g646a86f"
+  sha256 "0eda16db10ad9709ae429ae7ace6346b0521e5dc71660f50bf1a0cfbce77f1a9"
 
   url "https://github.com/yatli/fvim/releases/download/v#{version.before_comma}%2B#{version.after_comma}/fvim-osx.zip"
   name "FVim"
@@ -10,7 +10,7 @@ cask "fvim" do
   livecheck do
     url :url
     strategy :github_latest do |page|
-      match = page.match(%r{href=.*?/tag/v?(\d+(?:\.\d+)+)%2B([0-9a-f]+)["' >]}i)
+      match = page.match(%r{href=.*?/tag/v?(\d+(?:\.\d+)+)%2B(g?\h+)["' >]}i)
       "#{match[1]},#{match[2]}"
     end
   end
