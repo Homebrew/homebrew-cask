af5ea6340682239a24321f34044a47efb201a742
_sam <3804518+aahung@users.noreply.github.com>
2021-07-22 01:51:27 -0700

    Add Gif Bubble v0.2.0

---
+++ Casks/gifbubble.rb
@@ -0,0 +1,11 @@
+cask "gifbubble" do
+  version "0.2.0"
+  sha256 :no_check
+
+  url "https://github.com/aahung/gif-bubble/releases/download/v0.2.0/Gif.Bubble.app.zip"
+  name "Gif Bubble"
+  desc "Gif Search from menu bar"
+  homepage "https://github.com/aahung/gif-bubble"
+
+  app "Gif Bubble.app"
+end
