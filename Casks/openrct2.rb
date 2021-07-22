192fd82b4aef7bf2be4d809c0256870eea1f2598
Eric Knibbe <enk3@outlook.com>
2021-07-22 12:20:58 -0400

    Update openrct2 from 0.3.3 to 0.3.4

--- Casks/openrct2.rb
+++ Casks/openrct2.rb
@@ -3,8 +3,8 @@ cask "openrct2" do
     version "0.2.6"
     sha256 "0073933b486da10b181bc8a226a140badc64c7cd93f681d769c17b5715221a85"
   else
-    version "0.3.3"
-    sha256 "60477520caded0bb4a139947a0d5f2df05fabdff8b59aa08b4dd09c91a160c21"
+    version "0.3.4"
+    sha256 "33b0f11c754e1db5e7df4982a6476e051b430c6da5dd4a94b8211e1b300d7c11"
   end
 
   url "https://github.com/OpenRCT2/OpenRCT2/releases/download/v#{version}/OpenRCT2-#{version}-macos-x86-64.zip",
