956fdcfbfd5a7b71eb9be938c8ad7119d19e0119
tiiiecherle <tiiiecherle@gmail.com>
2021-07-22 17:33:34 +0200

    Update libreoffice from 7.1.4 to 7.1.5 (#109018)

--- Casks/libreoffice.rb
+++ Casks/libreoffice.rb
@@ -1,6 +1,6 @@
 cask "libreoffice" do
-  version "7.1.4"
-  sha256 "e85714bae026e8f1cce9c2dfdc9dc9d3d4dadc058d1aac208a9cfc2c98fa419f"
+  version "7.1.5"
+  sha256 "6b3e5741ba787f70c65411d7e1128b24d621c46e883b1fc61530bc10a38a18c5"
 
   url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg",
       verified: "documentfoundation.org/"
