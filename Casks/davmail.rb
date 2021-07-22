93bd6032f76fa793ffa66f574d2bec14cde8e7ba
Joseph Reagle <596814+reagle@users.noreply.github.com>
2021-07-22 11:42:14 -0400

    Update davmail from 5.5.1-3299 to 6.0.0-3375 (#109014)
    * Update davmail from 5.5.1-3299 to 6.0.0-3375
    
    * Update davmail.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/davmail.rb
+++ Casks/davmail.rb
@@ -1,11 +1,12 @@
 cask "davmail" do
-  version "5.5.1-3299"
-  sha256 "d906cc113a055bd3c494fe917907df2562c4bee94726aa08ed13cdb519b9dbe9"
+  version "6.0.0-3375"
+  sha256 "a212b6f3e06c9a145b3626bc1dcfc527f36d625530e53adf2ef379b9c6e9d354"
 
   url "https://downloads.sourceforge.net/davmail/DavMail-MacOSX-#{version}.app.zip",
       verified: "downloads.sourceforge.net/davmail/"
   appcast "https://sourceforge.net/projects/davmail/rss"
   name "DavMail"
+  desc "Use any mail/calendar client with an Exchange server"
   homepage "https://davmail.sourceforge.io/"
 
   app "DavMail.app"
