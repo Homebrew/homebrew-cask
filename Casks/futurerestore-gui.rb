21f6d1b22927ea6fef53c2a72e2d7daa65bc319b
CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
2021-07-23 18:48:59 -0400

    Update futurerestore-gui.rb

--- Casks/futurerestore-gui.rb
+++ Casks/futurerestore-gui.rb
@@ -2,7 +2,7 @@ cask "futurerestore-gui" do
   version "1.90"
   sha256 "05e237fa9ad404be4b0fe2ff7b2cf862062ba98e34345ad0e39b1532fc062def"
 
-  url "https://github.com/CoocooFroggy/FutureRestore-GUI/releases/download/v#{version}/FutureRestore-GUI-Mac-#{version}.dmg"
+  url "https://github.com/CoocooFroggy/FutureRestore-GUI/releases/download/#{version}/FutureRestore-GUI-Mac-#{version}.dmg"
   name "FutureRestore GUI"
   desc "Graphical interface for FutureRestore"
   homepage "https://github.com/CoocooFroggy/FutureRestore-GUI/"
