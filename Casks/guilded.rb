1a568a53769bdc3b07f544f9743f3866d71b3938
Bevan Kay <email@bevankay.me>
2021-07-23 17:26:34 +1000

    update version, update livecheck

--- Casks/guilded.rb
+++ Casks/guilded.rb
@@ -1,8 +1,8 @@
 cask "guilded" do
-  version "1.0.9104602-release"
+  version "1.0.9104602"
   sha256 "40f6245caa63144c043eafbcff1c794f0bad08cfbbecca59a79bf0841587def8"
 
-  url "https://s3-us-west-2.amazonaws.com/www.guilded.gg/AppBuilds/mac/Guilded-#{version}.dmg",
+  url "https://s3-us-west-2.amazonaws.com/www.guilded.gg/AppBuilds/mac/Guilded-#{version}-release.dmg",
       verified: "s3-us-west-2.amazonaws.com/www.guilded.gg/AppBuilds/mac/"
   name "Guilded"
   desc "Group chat platform"
