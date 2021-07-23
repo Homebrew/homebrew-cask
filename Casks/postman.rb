2c5d8cbb1eb3fb6311a065b0fbd472c61c7bbac1
MoonFruit <dkmoonfruit@gmail.com>
2021-07-23 17:24:49 +0800

    Update postman from 8.8.0 to 8.9.0

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,6 +1,6 @@
 cask "postman" do
-  version "8.8.0"
-  sha256 "ed9d22020c21b379f11f231e5f78996eec271c936d731b9093dfc7e160d559d7"
+  version "8.9.0"
+  sha256 "c90462e1bcd92cbfd713068a676d24b5124d830dbe8ba6c12d6b5ee8ae6f6495"
 
   url "https://dl.pstmn.io/download/version/#{version}/osx64",
       verified: "dl.pstmn.io/download/version/"
