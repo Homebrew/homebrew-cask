1f8d9b4a5b19a1c9a5011e562b4102bda43ccce6
Reno <25192197+singularitti@users.noreply.github.com>
2021-07-23 13:41:51 -0400

    Update fork from 2.9.3 to 2.10

--- Casks/fork.rb
+++ Casks/fork.rb
@@ -1,6 +1,6 @@
 cask "fork" do
-  version "2.9.3"
-  sha256 "240462647bf50fd041478e5e033159d7c7de179f8d6877d4ac0caa501f84fbb2"
+  version "2.10"
+  sha256 "cb1da0ed3d03d318172acd768b5dda7584b80f9f811c88f3bcfc9e616a9274a3"
 
   url "https://forkapp.ams3.cdn.digitaloceanspaces.com/mac/Fork-#{version}.dmg",
       verified: "forkapp.ams3.cdn.digitaloceanspaces.com/mac/"
