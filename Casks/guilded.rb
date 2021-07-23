a2915076d69e39b78d774e69c166c57a6d257acc
Abheek D <67982792+ADawesomeguy@users.noreply.github.com>
2021-07-23 09:31:44 -0500

    Add Guilded v1.0.9104602-release (#109036)
    * Add Guilded v1.0.9104602-release
    
    * add livecheck, update url, add checksum
    
    * update version, update livecheck
    
    * update livecheck
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

---
+++ Casks/guilded.rb
@@ -0,0 +1,26 @@
+cask "guilded" do
+  version "1.0.9104602"
+  sha256 "40f6245caa63144c043eafbcff1c794f0bad08cfbbecca59a79bf0841587def8"
+
+  url "https://s3-us-west-2.amazonaws.com/www.guilded.gg/AppBuilds/mac/Guilded-#{version}-release.dmg",
+      verified: "s3-us-west-2.amazonaws.com/www.guilded.gg/AppBuilds/mac/"
+  name "Guilded"
+  desc "Group chat platform"
+  homepage "https://www.guilded.gg/"
+
+  livecheck do
+    url "https://s3-us-west-2.amazonaws.com/www.guilded.gg/AppBuilds/mac/release-mac.yml"
+    strategy :electron_builder do |data|
+      data["version"].tr("-release", "")
+    end
+  end
+
+  app "Guilded.app"
+
+  zap trash: [
+    "~/Library/Application Support/Guilded",
+    "~/Library/Logs/Guilded",
+    "~/Library/Preferences/com.electron.guilded.plist",
+    "~/Library/Saved Application State/com.electron.guilded.savedState",
+  ]
+end
