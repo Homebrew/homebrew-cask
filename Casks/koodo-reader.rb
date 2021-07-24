19dc87c0f7b583bae63dab27969811a09da58f63
Reno <25192197+singularitti@users.noreply.github.com>
2021-07-24 02:57:36 -0400

    Update koodo-reader from 1.2.7 to 1.2.9

--- Casks/koodo-reader.rb
+++ Casks/koodo-reader.rb
@@ -1,13 +1,13 @@
 cask "koodo-reader" do
-  version "1.2.7"
+  version "1.2.9"
 
   if Hardware::CPU.intel?
-    sha256 "ad5c839644db62cb8edbbd3b2f5aa9b74d28154bf5afe35497057bd48a35578b"
+    sha256 "9bc446735f2d04d9f3bd2f21124f6e6f3c92c5ed43af2f2196666dab6a0e4b2e"
 
     url "https://github.com/troyeguo/koodo-reader/releases/download/v#{version}/Koodo-Reader-#{version}.dmg",
         verified: "github.com/troyeguo/koodo-reader/"
   else
-    sha256 "2ec9bce429a0fa81cdd81d1c1bc1fe69dd0bc0e332ad2c42d2a3be0b704eb21e"
+    sha256 "8d3e5c9a6442dbe9727eaef070c86464c37fd34f63f52c3f36cdf9943df0a7b9"
 
     url "https://github.com/troyeguo/koodo-reader/releases/download/v#{version}/Koodo-Reader-#{version}-arm64.dmg",
         verified: "github.com/troyeguo/koodo-reader/"
