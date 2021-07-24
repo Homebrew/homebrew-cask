d2a4ff8c2e50345ae1cb3f779d716de5d47a8b32
Sebastian Danielsson <sebastian.danielsson@protonmail.com>
2021-07-24 19:23:28 +0200

    Add Hush.app v1.0

---
+++ Casks/hush.rb
@@ -0,0 +1,14 @@
+# typed: false
+# frozen_string_literal: true
+
+cask "hush" do
+  version "1.0"
+  sha256 "ed5050e0806d633a717807f90e28acf4b6c2ebcd789b68d3b1c4d461aba0dfc7"
+
+  url "https://github.com/oblador/hush/releases/download/v#{version}/Hush.dmg"
+  name "Hush"
+  desc "Block nags to accept cookies and privacy invasive tracking in Safari"
+  homepage "https://github.com/oblador/hush"
+
+  app "Hush.app"
+end
