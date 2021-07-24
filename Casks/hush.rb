fea9550ad929bb609c955d96dadeefbc7d489119
Sebastian Danielsson <sebastian.danielsson@protonmail.com>
2021-07-24 23:20:01 +0200

    Add zap trash

--- Casks/hush.rb
+++ Casks/hush.rb
@@ -11,4 +11,11 @@ cask "hush" do
   homepage "https://github.com/oblador/hush"
 
   app "Hush.app"
+
+  zap trash: [
+  "~/Library/Application Scripts/se.oblador.Hush",
+  "~/Library/Application Scripts/se.oblador.Hush.ContentBlocker",
+  "~/Library/Containers/se.oblador.Hush",
+  "~/Library/Containers/se.oblador.Hush.ContentBlocker",
+]
 end
