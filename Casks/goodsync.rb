4e5c3e00b885ca067b32d438e55a54231c576939
Brandon Pickett <pickettbd@gmail.com>
2021-07-24 12:49:33 -0400

    Update goodsync from 11.7.7 to 11.7.8

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,6 +1,6 @@
 cask "goodsync" do
-  version "11.7.7"
-  sha256 "11a013541f024e0fa7ae9353a857965df9beb8c57b2107d28c6bac14e329aca8"
+  version "11.7.8"
+  sha256 "4b85c6f5894188f759e71901767f260942e1c16fdeb3172f5d4bda164451361d"
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
   name "GoodSync"
