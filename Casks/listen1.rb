c1ec1d0679d17626e068ee8d965d30705dea6477
刘伟琦 <liuweiqi@Wills-MacBook-Pro.local>
2021-07-25 18:29:20 +0800

    Update listen1 from 2.21.5 to 2.21.6

--- Casks/listen1.rb
+++ Casks/listen1.rb
@@ -1,10 +1,9 @@
 cask "listen1" do
   # NOTE: "1" is not a version number, but an intrinsic part of the product name
-  version "2.21.5"
-  sha256 "93cddb7b193c65368892887e151ae93f1db9b500b9948c4560cf237d804fa44a"
+  version "2.21.6"
+  sha256 "fd32f611ebbad961ecebfeaa53b1873d0fb74f2dbd25a48c67e410019f94267f"
 
-  url "https://github.com/listen1/listen1_desktop/releases/download/v#{version}/Listen1_#{version}_mac.dmg",
-      verified: "github.com/listen1/listen1_desktop/"
+  url "https://github.com/listen1/listen1_desktop/releases/download/v#{version}/Listen1_#{version}_mac_x64.dmg"
   name "Listen 1"
   desc "Search and play songs from a variety of online sources"
   homepage "https://listen1.github.io/listen1/"
