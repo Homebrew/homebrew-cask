6a285423092c9fc753797698cc6c9fee3525ab31
David Lanouette <David.Lanouette@GMail.com>
2021-07-25 19:01:22 -0400

    Update 4k-video-downloader to 4.17.0 (#109117)
    Co-authored-by: David Lanouette <David.Lanouette@RedHat.com>

--- Casks/4k-video-downloader.rb
+++ Casks/4k-video-downloader.rb
@@ -1,6 +1,6 @@
 cask "4k-video-downloader" do
-  version "4.16.4"
-  sha256 "161ccf7116690a5ebf02d39a6218afb7f49858d14bfdeced5f008365abbd01e9"
+  version "4.17.0"
+  sha256 "6fa1c6767470201d76122f9d8c11e383eb4225014ce17b79035b37e433f45508"
 
   url "https://dl.4kdownload.com/app/4kvideodownloader_#{version.major_minor_patch}.dmg"
   name "4K Video Downloader"
