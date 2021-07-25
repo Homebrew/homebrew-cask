6e9e01ec6bda3685ee5a3acb8e901197712a8c28
FnControlOption <70830482+FnControlOption@users.noreply.github.com>
2021-07-25 00:28:09 -0700

    Update adobe-connect from 11,2021.4.36 to 11,2021.6.27

--- Casks/adobe-connect.rb
+++ Casks/adobe-connect.rb
@@ -1,14 +1,19 @@
 cask "adobe-connect" do
-  version "11,2021.4.36"
-  sha256 "fe56b2e5e000f6f0ca80ecbae8b59798cb0bb9c944f39a556711465a9f4561d6"
+  version "11,2021.6.27"
+  sha256 "4037e55aadfe96964eb200c57913dbc51e3c6ecec64bfe9fbf212febcc73fedf"
 
-  url "https://download.adobe.com/pub/connect/updaters/meeting/#{version.before_comma}/AdobeConnect_#{version.after_comma.dots_to_underscores}.dmg"
+  url "https://download.adobe.com/pub/connect/updaters/meeting/#{version.before_comma.dots_to_underscores}/AdobeConnect_#{version.after_comma.dots_to_underscores}.dmg"
   name "Adobe Connect"
   desc "Virtual meeting client"
   homepage "https://www.adobe.com/products/adobeconnect.html"
 
   livecheck do
-    skip "No version information available"
+    url "https://www.adobe.com/go/ConnectMac11Plus"
+    regex(%r{/(\d+(?:_\d+)*)/AdobeConnect[._-](\d+(?:_\d+)*)\.dmg}i)
+    strategy :header_match do |headers, regex|
+      match = headers["location"].match(regex)
+      "#{match[1].tr("_", ".")},#{match[2].tr("_", ".")}"
+    end
   end
 
   auto_updates true
