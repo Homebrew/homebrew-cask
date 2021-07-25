873a0047c56299fcf8a43e2aff8af95ead51eedd
FnControlOption <70830482+FnControlOption@users.noreply.github.com>
2021-07-25 10:39:42 -0700

    Update appgate-sdp-client from 5.3.3 to 5.4.2

--- Casks/appgate-sdp-client.rb
+++ Casks/appgate-sdp-client.rb
@@ -1,15 +1,31 @@
 cask "appgate-sdp-client" do
-  version "5.3.3"
-  sha256 "935c87fcec29c6c7ab28ced0b3da8bb98db7f6b51303c3d651c53b14fc17fcbd"
+  if MacOS.version <= :high_sierra
+    version "5.3.3"
+    sha256 "935c87fcec29c6c7ab28ced0b3da8bb98db7f6b51303c3d651c53b14fc17fcbd"
+  else
+    version "5.4.2"
+    sha256 "d0880947d92035cb2783f995f69404085096326b24d8d58ba7f83a0411ee8efe"
+
+    livecheck do
+      url :homepage
+      strategy :page_match do |page|
+        page_regex = %r{href=["']?/(support/software-defined-perimeter-support/sdp-v?\d+(?:-\d+)*)["' >]}i
+        page.scan(page_regex).flat_map do |match|
+          version_page = Homebrew::Livecheck::Strategy.page_content("https://www.appgate.com/#{match[0]}")
+          version_regex = %r{href=.*?/Appgate[._-]SDP[._-](\d+(?:\.\d+)*)[._-]Installer\.dmg}i
+          version_page[:content].scan(version_regex).map(&:first)
+        end
+      end
+    end
+  end
 
   url "https://bin.appgate-sdp.com/#{version.major_minor}/client/AppGate-SDP-#{version}-Installer.dmg",
       verified: "bin.appgate-sdp.com/"
-  appcast "https://www.appgate.com/support/software-defined-perimeter-support/sdp-v#{version.major}-#{version.minor}"
   name "AppGate SDP Client for macOS"
   desc "Software-defined perimeter for secure network access"
-  homepage "https://www.appgate.com/software-defined-perimeter/support"
+  homepage "https://www.appgate.com/support/software-defined-perimeter-support"
 
-  depends_on macos: ">= :el_capitan"
+  depends_on macos: ">= :high_sierra"
 
   pkg "AppGate SDP Installer.pkg"
 
