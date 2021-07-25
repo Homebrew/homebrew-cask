929f4c85cdd02d8998114e08d8f6b46c3427d142
6weiqi <callmeliuweiqi@gmail.com>
2021-07-25 18:54:27 +0800

    Add 'verified' parameter in listen1 cask

--- Casks/listen1.rb
+++ Casks/listen1.rb
@@ -4,6 +4,7 @@ cask "listen1" do
   sha256 "fd32f611ebbad961ecebfeaa53b1873d0fb74f2dbd25a48c67e410019f94267f"
 
   url "https://github.com/listen1/listen1_desktop/releases/download/v#{version}/Listen1_#{version}_mac_x64.dmg"
+      verified: "github.com/listen1/listen1_desktop/"
   name "Listen 1"
   desc "Search and play songs from a variety of online sources"
   homepage "https://listen1.github.io/listen1/"
