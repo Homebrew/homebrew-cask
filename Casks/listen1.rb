c687da47a5312aa96b9b008a08bd50e32b067f48
Bevan Kay <email@bevankay.me>
2021-07-25 21:04:45 +1000

    Update listen1.rb

--- Casks/listen1.rb
+++ Casks/listen1.rb
@@ -3,7 +3,7 @@ cask "listen1" do
   version "2.21.6"
   sha256 "fd32f611ebbad961ecebfeaa53b1873d0fb74f2dbd25a48c67e410019f94267f"
 
-  url "https://github.com/listen1/listen1_desktop/releases/download/v#{version}/Listen1_#{version}_mac_x64.dmg"
+  url "https://github.com/listen1/listen1_desktop/releases/download/v#{version}/Listen1_#{version}_mac_x64.dmg",
       verified: "github.com/listen1/listen1_desktop/"
   name "Listen 1"
   desc "Search and play songs from a variety of online sources"
