22ac1d62921e69a192b5710bd371530c11d638f7
Bevan Kay <email@bevankay.me>
2021-07-25 22:52:41 +1000

    Update electerm from 1.13.4 to 1.14.0 (#109103)

--- Casks/electerm.rb
+++ Casks/electerm.rb
@@ -1,8 +1,8 @@
 cask "electerm" do
-  version "1.13.4"
-  sha256 "1f192bc7570fcd0e8b77b746547cde0afcb23a8d875bbd1212b4fb4ba1321ece"
+  version "1.14.0"
+  sha256 "4a12d769792d3f4a8b314de2ff3323650ac9c673688f7d5b904790ea0ef234ef"
 
-  url "https://github.com/electerm/electerm/releases/download/#{version}/electerm-#{version}-mac.dmg"
+  url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac-x64.dmg"
   name "electerm"
   desc "Terminal/ssh/serial port/sftp client"
   homepage "https://github.com/electerm/electerm/"
