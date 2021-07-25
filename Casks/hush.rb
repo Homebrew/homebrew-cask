18e0fb23eafea7c0fd40be96b04d43134cfa2acb
Miccal Matthews <miccal.matthews@gmail.com>
2021-07-25 08:30:28 +0800

    Update hush.rb

--- Casks/hush.rb
+++ Casks/hush.rb
@@ -2,10 +2,11 @@ cask "hush" do
   version "1.0"
   sha256 "ed5050e0806d633a717807f90e28acf4b6c2ebcd789b68d3b1c4d461aba0dfc7"
 
-  url "https://github.com/oblador/hush/releases/download/v#{version}/Hush.dmg"
+  url "https://github.com/oblador/hush/releases/download/v#{version}/Hush.dmg",
+      verified: "github.com/oblador/hush/"
   name "Hush"
   desc "Block nags to accept cookies and privacy invasive tracking in Safari"
-  homepage "https://github.com/oblador/hush"
+  homepage "https://oblador.github.io/hush/"
 
   depends_on macos: ">= :big_sur"
 
