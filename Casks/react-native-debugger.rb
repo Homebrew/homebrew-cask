693645a7819994b8834372627ddf8b4236e05dbe
Jhen-Jie Hong <developer@jhen.me>
2021-07-26 11:06:38 +0800

    Update react-native-debugger (v0.12.0) (#109129)

--- Casks/react-native-debugger.rb
+++ Casks/react-native-debugger.rb
@@ -1,6 +1,6 @@
 cask "react-native-debugger" do
-  version "0.11.9"
-  sha256 "6aaabe96215f99e282db56e008b69142204625a4598dd3df393a250c994fba46"
+  version "0.12.0"
+  sha256 "370bd7d511b15362c544051e5e3e04c86201182ded22c51a082437db9e92d024"
 
   url "https://github.com/jhen0409/react-native-debugger/releases/download/v#{version}/rn-debugger-macos-x64.zip"
   name "React Native Debugger"
