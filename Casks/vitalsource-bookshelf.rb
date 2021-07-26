b191a16262e78b644fdd02f00384c159d282673e
Bevan Kay <email@bevankay.me>
2021-07-27 09:26:45 +1000

    vitalsource-bookshelf: update version, add zap, update depends_on, add verified url parameter

--- Casks/vitalsource-bookshelf.rb
+++ Casks/vitalsource-bookshelf.rb
@@ -2,7 +2,8 @@ cask "vitalsource-bookshelf" do
   version "10.0.0.1365"
   sha256 "c21310b8614c892b074afc107f3eb9f287089970df3c2f4799b3459e47732097"
 
-  url "https://downloads.vitalbook.com/vsti/bookshelf/#{version.major_minor}/mac/bookshelf/VitalSource-Bookshelf_#{version}.dmg"
+  url "https://downloads.vitalbook.com/vsti/bookshelf/#{version.major_minor}/mac/bookshelf/VitalSource-Bookshelf_#{version}.dmg",
+      verified: "downloads.vitalbook.com/vsti/bookshelf"
   name "VitalSource Bookshelf"
   desc "Access eTextbooks"
   homepage "https://www.vitalsource.com/bookshelf-features"
@@ -12,7 +13,13 @@ cask "vitalsource-bookshelf" do
     regex(/href=.*?VitalSource-Bookshelf[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :high_sierra"
 
   app "VitalSource Bookshelf.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.vitalsource.bookshelf",
+    "~/Library/Logs/Vitalsource Bookshelf",
+    "~/Library/Preferences/com.vitalsource.bookshelf.plist",
+  ]
 end
