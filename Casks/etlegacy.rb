2a9e73f63a6dfe96ffd825afdbf2827b51a6ae86
Sebastian Danielsson <sebastian.danielsson@protonmail.com>
2021-07-26 21:30:36 +0200

    Add etlegacy v2.77.1

---
+++ Casks/etlegacy.rb
@@ -0,0 +1,23 @@
+cask "etlegacy" do
+  version "2.77.1"
+  sha256 :no_check
+
+  url "https://www.etlegacy.com/download/file/258"
+  name "ET: Legacy"
+  desc "Open source project based on the code of Wolfenstein: Enemy Territory"
+  homepage "https://www.etlegacy.com/"
+
+  depends_on macos: ">= :big_sur"
+  depends_on formula: "wget"
+
+  suite "ETLegacy"
+  installer script: {
+    executable: "wget",
+    args:       ["https://mirror.etlegacy.com/etmain/pak0.pk3", "-P", "#{caskroom_path}/#{version}/ETLegacy/etmain", "https://mirror.etlegacy.com/etmain/pak1.pk3", "-P", "#{caskroom_path}/#{version}/ETLegacy/etmain", "https://mirror.etlegacy.com/etmain/pak2.pk3", "-P", "#{caskroom_path}/#{version}/ETLegacy/etmain"],
+  }
+
+  uninstall delete: [
+    "~/Library/Application Support/etlegacy",
+    "~/Library/Saved Application State/com.etlegacy.etl.savedState",
+  ]
+end
