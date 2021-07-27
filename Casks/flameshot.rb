842aabcfc7a2951e673ded7e671348d2295b8968
pebosi <mail@pebosi.net>
2021-07-28 00:43:55 +0200

    Update flameshot.rb (#109197)

--- Casks/flameshot.rb
+++ Casks/flameshot.rb
@@ -1,6 +1,6 @@
 cask "flameshot" do
-  version "0.10.0"
-  sha256 "8433c67c89a6c1346abea46e0e01d57ed87f610b5d5a293c244974a61828c8a4"
+  version "0.10.1"
+  sha256 "c4d20d790ee2922506e92ef47d08986f87f4cb11dbabcb126b23ac2c8392b48f"
 
   url "https://github.com/flameshot-org/flameshot/releases/download/v#{version}/flameshot.dmg",
       verified: "github.com/flameshot-org/flameshot/"
