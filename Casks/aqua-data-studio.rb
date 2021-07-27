9d5abe6a67557fc9bdbb626bc223ded7d551fe10
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-07-26 20:27:29 -0700

    Update aqua-data-studio from 21.0.3 to 22.0.0 (#109121)
    * Update aqua-data-studio from 21.0.3 to 22.0.0
    
    * livecheck
    
    * return empty arrays
    
    * Update Casks/aqua-data-studio.rb
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>
    
    * Update Casks/aqua-data-studio.rb
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>
    
    * Update aqua-data-studio.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/aqua-data-studio.rb
+++ Casks/aqua-data-studio.rb
@@ -1,11 +1,38 @@
 cask "aqua-data-studio" do
-  version "21.0.3"
-  sha256 "2a092e86eed401d632da60632db42049d65e4255486e97b6e7c31122b130b842"
+  version "22.0.0"
+  sha256 "c190a2c52d21001d473af35dfb8a5b487d61795d1694ec9fa5159000762c4cb1"
 
   url "https://downloads.aquafold.com/v#{version}/osx/ads-osx-#{version}.tar.gz"
   name "Aquafold Aqua Data Studio"
   desc "Database IDE with data management and visual analytics"
   homepage "https://www.aquafold.com/aquadatastudio"
+
+  livecheck do
+    url "https://www.aquaclusters.com/app/home/project/public/aquadatastudio/wikibook/changelog/page/0/Home"
+    regex(%r{href=["']?([^"' >]*?/changelog/page/Version[._-]?(\d+(?:\.\d+)+)/[^"' >]*?)["' >]}i)
+    strategy :page_match do |page, regex|
+      changelog_matches = page.scan(regex)
+      next [] if changelog_matches.blank?
+
+      changelog_matches.uniq!(&:second)
+      changelog_matches.sort_by! { |match| Version.new(match.second) }
+
+      # Assume the last-sorted version is newest
+      changelog_path, changelog_version = changelog_matches.last
+
+      # Check the changelog of the newest version to identify patch versions
+      changelog_page = Homebrew::Livecheck::Strategy.page_content(URI.join(url, changelog_path))
+      next [] if changelog_page[:content].blank?
+
+      versions = changelog_page[:content].scan(/>\s*?v?(\d+(?:\.\d+)+)/i)
+      next versions.flatten if versions.present?
+
+      # Append a patch version of 0 if the newest changelog version is just a
+      # major/minor version (almost certain to be true)
+      changelog_version += ".0" if changelog_version.count(".") < 2
+      changelog_version
+    end
+  end
 
   app "Aqua Data Studio.app"
 end
