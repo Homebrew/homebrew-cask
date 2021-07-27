ef5ec5e34bb1f37a409b5d251428e0fdc0af08e7
Miccal Matthews <miccal.matthews@gmail.com>
2021-07-27 11:04:07 +0800

    Update 3dgenceslicer.rb

--- Casks/3dgenceslicer.rb
+++ Casks/3dgenceslicer.rb
@@ -1,8 +1,8 @@
 cask "3dgenceslicer" do
-  version "4.0,2.4.2"
+  version "2.4.2,4.0"
   sha256 "2f790a6fc5729bbc45f7dce6a884a3a8cce66b84950d6223e8e41e90b064d8c1"
 
-  url "https://cloud.3dgence.com/downloads/slicer/3DGence%20Slicer%20#{version.before_comma}%20(v.#{version.after_comma}).dmg"
+  url "https://cloud.3dgence.com/downloads/slicer/3DGence%20Slicer%20#{version.after_comma}%20(v.#{version.before_comma}).dmg"
   name "3DGence Slicer"
   desc "Prepare files for 3D printing based on CAD models for 3DGence printers"
   homepage "https://3dgence.com/"
@@ -14,10 +14,10 @@ cask "3dgenceslicer" do
     )
     strategy :page_match do |page, regex|
       page.scan(regex).map do |match|
-        "#{match[0]},#{match[1]}"
+        "#{match[1]},#{match[0]}"
       end
     end
   end
 
-  app "3DGence Slicer #{version.before_comma}.app"
+  app "3DGence Slicer #{version.after_comma}.app"
 end
