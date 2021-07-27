0218bed7bad9a00ac54bc1d3ef01a62650f33ae1
Seeker <meaningseeking@protonmail.com>
2021-07-27 16:37:12 -0700

    Update 7777 from 1.0.5 to 1.0.7 (#109094)
    * Update 7777 from 1.0.5 to 1.0.7
    
    * add livecheck
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/7777.rb
+++ Casks/7777.rb
@@ -1,11 +1,16 @@
 cask "7777" do
-  version "1.0.5"
-  sha256 "844ce9f32d17516cf29a45a728910f5b2e0424ce12dfee7df934e93f0b9c70b2"
+  version "1.0.7"
+  sha256 "e386d78cf6853f176c06ed99a26e688cf3434ee365da0eb1a4c6b5cd0137a438"
 
   url "https://releases.port7777.com/#{version}/macos/7777"
   name "7777"
   desc "Remote AWS database on local port 7777"
   homepage "https://port7777.com/"
+
+  livecheck do
+    url "https://releases.port7777.com/latest/version.txt"
+    regex(/(\d+(?:\.\d+)+)/i)
+  end
 
   binary "7777"
 end
