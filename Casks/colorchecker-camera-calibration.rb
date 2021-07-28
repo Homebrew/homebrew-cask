e07e7843aa4cc8fc5a890d8c3231c8a982be941a
FnControlOption <70830482+FnControlOption@users.noreply.github.com>
2021-07-28 08:09:30 -0700

    Update colorchecker-camera-calibration from 2.1.0 to 2.3.0

--- Casks/colorchecker-camera-calibration.rb
+++ Casks/colorchecker-camera-calibration.rb
@@ -1,12 +1,17 @@
 cask "colorchecker-camera-calibration" do
-  version "2.1.0"
-  sha256 "8e574d3fb707576ea8e230bbd6608d1bf3dd17537d38bb56be9356d1047e124d"
+  version "2.3.0"
+  sha256 "a7d6d0335447b8ad85de935e3f7a37ec69895cddb8282c4c55a960d4b332db17"
 
   url "https://downloads.xrite.com/downloads/software/ColorCheckerPassport/Mac/v#{version}/ColorCheckerCameraCalibration.zip",
       verified: "downloads.xrite.com/downloads/software/ColorCheckerPassport/Mac/"
   name "ColorChecker Camera Calibration"
   desc "Software to build custom camera profiles"
   homepage "https://xritephoto.com/ph_product_overview.aspx?ID=2632&action=overview"
+
+  livecheck do
+    url :homepage
+    regex(/>\s*v?(\d+(?:\.\d+)*)\s*</i)
+  end
 
   pkg "ColorChecker Camera Calibration.pkg"
 
