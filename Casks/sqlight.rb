c35aad1dd3a7d49787e5d2092d483ee7d12213be
Miccal Matthews <miccal.matthews@gmail.com>
2021-07-28 08:33:26 +0800

    Update sqlight.rb

--- Casks/sqlight.rb
+++ Casks/sqlight.rb
@@ -18,6 +18,9 @@ cask "sqlight" do
       js_file_path = page[%r{src=["']?(/com-aurvan-satva-ui-reactjs-release/static/js/main\.\w+\.chunk\.js)["' >]}i, 1]
       next [] if js_file_path.blank?
 
+      js_file_data = Homebrew::Livecheck::Strategy.page_content("https://www.aurvan.com/#{js_file_path}")
+      next [] if js_file_data[:content].blank?
+
       js_file_data[:content].scan(regex).map { |match| match&.first&.gsub("_", ".") }
     end
   end
