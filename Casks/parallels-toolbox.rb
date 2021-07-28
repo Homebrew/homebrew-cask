5b3d71944b49b789331de04c7e77d864d270e3a9
MoonFruit <dkmoonfruit@gmail.com>
2021-07-28 10:46:56 +0800

    Update parallels-toolbox from 4.5.0-3860 to 5.0.0-4023

--- Casks/parallels-toolbox.rb
+++ Casks/parallels-toolbox.rb
@@ -1,6 +1,6 @@
 cask "parallels-toolbox" do
-  version "4.5.0-3860"
-  sha256 "ac84fdb201250d67e4253bd76f991dd03a3c48d9b3520ef4508805cf79dc45da"
+  version "5.0.0-4023"
+  sha256 "355ca35ff947513b1fd3fe705aff53b7a2d0a316b892210a012d3cb31965754a"
 
   url "https://download.parallels.com/toolbox/v#{version.major}/#{version}/ParallelsToolbox-#{version}.dmg"
   name "Parallels Toolbox"
