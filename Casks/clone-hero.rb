f227ac7ed0fb3c4aae815bdc86576df44b79237e
FnControlOption <70830482+FnControlOption@users.noreply.github.com>
2021-07-28 07:31:02 -0700

    clone-hero: add livecheck

--- Casks/clone-hero.rb
+++ Casks/clone-hero.rb
@@ -3,9 +3,14 @@ cask "clone-hero" do
   sha256 "ae8102f503596822c45a2b3c12e48e218473f1525772bbe824139ce48a23a5ea"
 
   url "https://dl.clonehero.net/clonehero-v.#{version.sub(/^\d+\./, "")}/clonehero-mac.dmg"
-  appcast "https://clonehero.net/"
   name "Clone Hero"
+  desc "Guitar Hero clone"
   homepage "https://clonehero.net/"
+
+  livecheck do
+    url :homepage
+    regex(%r{href=["']?/releases/v?(\d+(?:\.\d+)*)["' >]}i)
+  end
 
   app "Clone Hero.app"
 
