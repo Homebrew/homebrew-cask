8a4ab25057ce259b508432221c895d615bdadc8a
Jeremy Mayeres <jeremy@jerr.dev>
2021-07-28 13:06:58 +0300

    Update monitorcontrol URL

--- Casks/monitorcontrol.rb
+++ Casks/monitorcontrol.rb
@@ -2,10 +2,10 @@ cask "monitorcontrol" do
   version "2.2.0"
   sha256 "02e029ad8e799fb3b1a1fe036fd309058bcaabddb94c5fb6859714cdbf78d868"
 
-  url "https://github.com/the0neyouseek/MonitorControl/releases/download/v#{version}/MonitorControl.#{version}.dmg"
+  url "https://github.com/MonitorControl/MonitorControl/releases/download/v#{version}/MonitorControl.#{version}.dmg"
   name "MonitorControl"
   desc "Tool to control external monitor brightness & volume"
-  homepage "https://github.com/the0neyouseek/MonitorControl"
+  homepage "https://github.com/MonitorControl/MonitorControl"
 
   depends_on macos: ">= :sierra"
 
