c2ef2da28818e8e806a3e238eb63a70a75c3352e
FnControlOption <70830482+FnControlOption@users.noreply.github.com>
2021-07-29 10:01:55 -0700

    Add Clock Signal 2021-07-16

---
+++ Casks/clock-signal.rb
@@ -0,0 +1,20 @@
+cask "clock-signal" do
+  version "2021-07-16"
+  sha256 "98a6a33445500a22d746ed15f37f3c65ed52f1215f7b300977b4689ccb064ef2"
+
+  url "https://github.com/TomHarte/CLK/releases/download/#{version}/Clock.Signal.macOS.#{version}.zip"
+  name "Clock Signal"
+  name "CLK"
+  desc "Latency-hating emulator of 8- and 16-bit platforms"
+  homepage "https://github.com/TomHarte/CLK"
+
+  app "Clock Signal.app"
+
+  uninstall quit: "TH.Clock-Signal"
+
+  zap trash: [
+    "~/Library/Application Scripts/TH.Clock-Signal",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/th.clock-signal.sfl*",
+    "~/Library/Containers/TH.Clock-Signal",
+  ]
+end
