582e17784019cda042ac4459e7f1ff36e2679fec
lucky <53379023+x13a@users.noreply.github.com>
2021-07-29 07:45:23 +0300

    update android-studio from 4.2.2.0,202.7486908 to 2020.3.1.22 (#109262)
    * update android-studio from 4.2.2.0,202.7486908 to 2020.3.1.22
    
    * Update android-studio.rb
    
    Co-authored-by: lucky <>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/android-studio.rb
+++ Casks/android-studio.rb
@@ -1,19 +1,23 @@
 cask "android-studio" do
-  version "4.2.2.0,202.7486908"
-  sha256 "63b8e5bed9a772c19398604bd7c897a543d4164a534775ad44a91110f49532c7"
+  version "2020.3.1.22"
 
-  url "https://dl.google.com/dl/android/studio/install/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.dmg",
-      verified: "google.com/dl/android/studio/"
+  if Hardware::CPU.intel?
+    sha256 "42722b671fcda03e6e02b712828b5484e4af7563c5172b583252c9070c173d4b"
+    url "https://redirector.gvt1.com/edgedl/android/studio/install/#{version}/android-studio-#{version}-mac.dmg",
+        verified: "redirector.gvt1.com/edgedl/android/studio/"
+  else
+    sha256 "dc383ea7d7a6585451bced011f7db178068b9744319a9093cb05ac855b2c81ff"
+    url "https://redirector.gvt1.com/edgedl/android/studio/ide-zips/#{version}/android-studio-#{version}-mac_arm.zip",
+        verified: "redirector.gvt1.com/edgedl/android/studio/"
+  end
+
   name "Android Studio"
   desc "Tools for building Android applications"
   homepage "https://developer.android.com/studio/"
 
   livecheck do
     url :homepage
-    strategy :page_match do |page|
-      match = page.match(%r{href=.*?/(\d+(?:\.\d+)*)/android-studio-ide-(\d+(?:\.\d+)*)-mac\.dmg}i)
-      "#{match[1]},#{match[2]}"
-    end
+    regex(/android-studio-(\d+(?:\.\d+)*)-mac/i)
   end
 
   auto_updates true
