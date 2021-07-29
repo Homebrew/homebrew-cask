ae39fdc626c12c6d8f8525b932ad39667613329d
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-07-29 15:52:53 -0700

    duplicacy: discontinued (#109288)

--- Casks/duplicacy.rb
+++ Casks/duplicacy.rb
@@ -5,8 +5,18 @@ cask "duplicacy" do
   url "https://acrosync.com/duplicacy/duplicacy_gui_osx_#{version}.dmg",
       verified: "acrosync.com/duplicacy/"
   name "Duplicacy"
+  desc "Cloud backup tool"
   homepage "https://duplicacy.com/"
+
+  livecheck do
+    url "https://duplicacy.com/download.html"
+    regex(/href=.*?duplicacy[._-]?gui[._-]?osx[._-]?v?(\d+(?:\.\d+)*)\.dmg/i)
+  end
 
   app "Duplicacy.app"
   binary "#{appdir}/Duplicacy.app/Contents/Resources/duplicacy_osx_x64_#{version}", target: "duplicacy"
+
+  caveats do
+    discontinued
+  end
 end
