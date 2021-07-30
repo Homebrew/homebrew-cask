ac1f6d6bab8cdd3f2214f2d6312a0110aef469a4
Robert Blair <12276457+robertleeblairjr@users.noreply.github.com>
2021-07-29 19:05:33 -0500

    Update origin from 10.5.95.47231 to 10.5.102.48654 (#109294)
    * Update origin from 10.5.95.47231 to 10.5.102.48654
    
    * Update origin.rb
    
    * Update origin.rb
    
    * Update origin.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/origin.rb
+++ Casks/origin.rb
@@ -1,27 +1,27 @@
 cask "origin" do
-  version "10.5.95.47231"
-  sha256 "63e8ad69682e286cbeb835c3e43f6345de220cbdf2f8696c997b417e578cfc2b"
+  version "10.5.102.48654"
+  sha256 :no_check
 
-  url "https://origin-a.akamaihd.net/Origin-Client-Download/origin/mac/live/OriginUpdate_#{version.dots_to_underscores}.zip",
-      verified: "origin-a.akamaihd.net/"
-  appcast "https://api1.origin.com/autopatch/2/upgradeFrom/#{version}/en_US/PROD?platform=MAC&osVersion=10.14.0"
+  url "https://origin-a.akamaihd.net/Origin-Client-Download/origin/mac/live/Origin.dmg",
+      verified: "origin-a.akamaihd.net/Origin-Client-Download/"
   name "Origin"
+  desc "Play PC games and connect with your friends"
   homepage "https://www.origin.com/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   auto_updates true
 
   app "Origin.app"
 
-  preflight do
-    # There is no enclosing .app folder, just the 'Contents' of the app in the appcast download
-    FileUtils.mv(staged_path.children, staged_path.join("Origin.app").tap(&:mkpath))
-  end
-
   zap trash: [
     "~/Library/Application Support/Origin",
-    "~/Library/Saved Application State/com.ea.Origin.savedState",
-    "~/Library/LaunchAgents/com.ea.origin.WebHelper.plist",
     "~/Library/Caches/com.ea.Origin",
     "~/Library/Caches/Origin",
+    "~/Library/LaunchAgents/com.ea.origin.WebHelper.plist",
+    "~/Library/Saved Application State/com.ea.Origin.savedState",
   ]
 end
