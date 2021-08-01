9b6f0afabaa60d58a9705a2eca19d97f650eb1bf
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-01 17:37:43 +0800

    Update powder.rb

--- Casks/powder.rb
+++ Casks/powder.rb
@@ -3,9 +3,14 @@ cask "powder" do
   sha256 :no_check
 
   url "https://powdertoy.co.uk/Download/powder.dmg"
-  appcast "https://powdertoy.co.uk/"
   name "Powder Toy"
+  desc "Physics sandbox game"
   homepage "https://powdertoy.co.uk/"
+
+  livecheck do
+    url :homepage
+    regex(%r{macOS</dt>\n\t*<dd class="Version">(\d+(?:\.\d+)*)}i)
+  end
 
   app "Powder.app"
 end
