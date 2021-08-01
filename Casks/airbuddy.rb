5a21d90bb11967c6fc5db58f628a2addf5b1f9f3
Grettir Asmundarson <30250+grettir@users.noreply.github.com>
2021-08-01 14:40:36 -0600

    Update airbuddy from 2.4.1,333 to 2.4.2,335 (#109363)
    Co-authored-by: Grettir Asmundarson <grettir@users.noreply.github.com>

--- Casks/airbuddy.rb
+++ Casks/airbuddy.rb
@@ -1,6 +1,6 @@
 cask "airbuddy" do
-  version "2.4.1,333"
-  sha256 "27e7fda96e299a3239f5cac2b623cd68f82279077f11650a6112601e6374892d"
+  version "2.4.2,335"
+  sha256 "700ba65a1cb3135cc79870f85ea6868ab296db61791faefde11476333b241a66"
 
   url "https://su.airbuddy.app/kCRSAmcjBc/AirBuddy_v#{version.before_comma}-#{version.after_comma}.dmg"
   name "AirBuddy"
