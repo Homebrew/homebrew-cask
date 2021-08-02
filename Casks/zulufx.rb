fa8e3926eb4de153802a10b55c2a8352a3489d8c
Robert Blair <12276457+robertleeblairjr@users.noreply.github.com>
2021-08-02 11:10:43 -0500

    Update zulufx.rb
    Update URLs.

--- Casks/zulufx.rb
+++ Casks/zulufx.rb
@@ -5,17 +5,17 @@ cask "zulufx" do
     sha256 "1190fdfe30ce1c6a1e746084a9e325a045fbf63ba0cc2dde3ccb74959e3a38b0"
 
     url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-fx-jdk#{version.before_comma}-macosx_x64.dmg",
-        referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
+        referer: "https://www.azul.com/downloads/"
   else
     sha256 "50c6b1ddb154ba743da3851d09ff87e063dab096d5350c2e4a74f7e1cdfa976a"
 
     url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-fx-jdk#{version.before_comma}-macosx_aarch64.dmg",
-        referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
+        referer: "https://www.azul.com/downloads/"
   end
 
   name "ZuluFX"
   desc "Azul ZuluFX Java Standard Edition Development Kit"
-  homepage "https://www.azul.com/downloads/zulu/zulu-mac/"
+  homepage "https://www.azul.com/downloads/"
 
   depends_on macos: ">= :sierra"
 
