92cc8eaa4556b8e68bdbd0d9ad00db6961f289e2
Takeshi Owaki <owaki@kumanomi.org>
2021-08-03 04:38:40 +0900

    Update jedit-omega from 2.39 to 2.40 (#109384)

--- Casks/jedit-omega.rb
+++ Casks/jedit-omega.rb
@@ -3,8 +3,8 @@ cask "jedit-omega" do
     version "1.32"
     sha256 "251697fe6b76419b2cef41a89a2cb7d2e3f8caab0bc1ae82aed6c14a366fdad9"
   else
-    version "2.39"
-    sha256 "3a6db023daa8015fb6636aecd6b6745b25378d971990d70171c1e23748a9bce8"
+    version "2.40"
+    sha256 "0ab607a5f2cea7952e9a33f2c842725b276a7d183e53fe7541280772431f2e9e"
   end
 
   url "http://download.artman21.co.jp/product/JeditOmega/JeditOmega#{version.no_dots}.dmg",
