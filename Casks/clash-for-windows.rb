d2a18a8616676f7ba8a02b5094c0a138a40c3d33
FAN VINGA <fanalcest@gmail.com>
2021-08-04 00:38:40 +0800

    Update clash-for-windows from 0.16.4 to 0.17.0

--- Casks/clash-for-windows.rb
+++ Casks/clash-for-windows.rb
@@ -1,12 +1,12 @@
 cask "clash-for-windows" do
-  version "0.16.4"
+  version "0.17.0"
 
   if Hardware::CPU.intel?
     url "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/#{version}/Clash.for.Windows-#{version}.dmg"
-    sha256 "84e7d989e749bede21f67a2dc4ee80129f131502e50d29073ba851622c6b8158"
+    sha256 "96e96786e2305b8b2fa7d021275993113bf261e79925ca482a696dc6c9a8a5b6"
   else
     url "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/#{version}/Clash.for.Windows-#{version}-arm64.dmg"
-    sha256 "3706148d0243ed7d7fa14a5d4bbb52e80ff2a9372c46809983e8ec240e69ade2"
+    sha256 "cdc2768ae13101f2e05b5b4f9da4b7fa8afb747fff653556b4ffcced33bb51cb"
   end
 
   name "Clash for Windows"
