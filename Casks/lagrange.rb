4aa3b5166351395c867f658fc48f3a3dce9c75fe
Jaakko Keränen <jaakko.keranen@iki.fi>
2021-08-03 11:10:11 +0300

    Update lagrange from 1.6.1 to 1.6.2 (#109396)

--- Casks/lagrange.rb
+++ Casks/lagrange.rb
@@ -1,13 +1,13 @@
 cask "lagrange" do
-  version "1.6.1"
+  version "1.6.2"
 
   if Hardware::CPU.intel?
-    sha256 "c1fd881115280371208b1983ecda16085e3a184bfcced235ab04aeb0b0833012"
+    sha256 "60107ba9a7fc45881ba5150408dab0c30c9e1bd44de49ea7a9e6c677db2d7819"
 
     url "https://github.com/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_macos10.13-x86_64.tbz",
         verified: "github.com/skyjake/lagrange/"
   else
-    sha256 "1fddea20f60ea6fb50d53da7aca488b0ff5aa4649b9f3ba6eb7d35838e6d95a2"
+    sha256 "b5e2190b43fbaddb5d48fe0f8cc0c2c3b64a4f8e319841a641a9b041e137f499"
 
     url "https://github.com/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_macos11.0-arm64.tbz",
         verified: "github.com/skyjake/lagrange/"
