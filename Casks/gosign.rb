bb0d490df779db644cb8d7272a07e0fa6f1fcd6f
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-03 14:05:25 +0800

    Update and rename gosign-desktop.rb to gosign.rb

--- Casks/gosign.rb
+++ Casks/gosign.rb
@@ -1,11 +1,16 @@
-cask "gosign-desktop" do
-  version :latest
+cask "gosign" do
+  version "1.1.6"
   sha256 :no_check
 
   url "https://rinnovofirma.infocert.it/gosign/download/darwin/latest"
   name "GoSign Desktop"
-  desc "Firma digitale Italia"
+  desc "Digital signature and time stamp app"
   homepage "https://www.firma.infocert.it/prodotti/gosign.php"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   depends_on macos: ">= :high_sierra"
 
