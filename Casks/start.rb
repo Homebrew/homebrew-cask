1bdf5de48b0c53fd24b8cbce971fc998e1e73e17
zuisong <378818573@qq.com>
2021-08-03 14:21:18 +0800

    Update start from 0.293.3-100 to 0.297.2-121 (#109395)
    Co-authored-by: chenjian <com.me@foxmail.com>

--- Casks/start.rb
+++ Casks/start.rb
@@ -1,12 +1,18 @@
 cask "start" do
-  version "0.292.3-89"
-  sha256 "1708e1818d0a67afebdac5e089c48fcc77d51e0f132b11c7808bad861273dba5"
+  version "0.297.2-121"
+  sha256 "423416d983f69f7440038a7b733250d81f03272dbf27ba76a2715415450ca9c3"
 
-  url "https://imgcdn.start.qq.com/cdn/mac.client/installer/START-Installer-#{version}.dmg?"
+  url "https://imgcdn.start.qq.com/cdn/mac.client/installer/START-Installer-#{version}.dmg"
   name "START"
   name "腾讯云游戏"
   desc "Tencent cloud gaming platform"
   homepage "https://start.qq.com/"
+
+  livecheck do
+    url "https://api.start.qq.com/cfg/get?biztypes=macos-update-info"
+    strategy :page_match
+    regex(%r{.*/START-Installer-([.\d\-]+)\.dmg}i)
+  end
 
   auto_updates true
   depends_on macos: ">= :sierra"
