46c6b524b42cfed65340b997986ff4e0aa5c4f04
Juraj Bubniak <juraj.bubniak@gmail.com>
2021-08-04 10:08:14 +0200

    Update mattermost to support apple silicon build. (#109458)

--- Casks/mattermost.rb
+++ Casks/mattermost.rb
@@ -1,8 +1,14 @@
 cask "mattermost" do
   version "4.7.1"
-  sha256 "e4c580ebb9b6c55da2569cf0ee6dbcbed413310213a9bf86e6cdf7b716226a93"
 
-  url "https://releases.mattermost.com/desktop/#{version}/mattermost-desktop-#{version}-mac.zip"
+  if Hardware::CPU.intel?
+    sha256 "e4c580ebb9b6c55da2569cf0ee6dbcbed413310213a9bf86e6cdf7b716226a93"
+    url "https://releases.mattermost.com/desktop/#{version}/mattermost-desktop-#{version}-mac.zip"
+  else
+    sha256 "241dcc2e8840958415a827cdc25f8306615265f23927ec34573d597f1348928b"
+    url "https://releases.mattermost.com/desktop/#{version}/mattermost-desktop-#{version}-mac-m1.zip"
+  end
+
   name "Mattermost"
   desc "Open-source, self-hosted Slack-alternative"
   homepage "https://about.mattermost.com/"
