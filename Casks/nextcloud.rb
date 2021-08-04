c3b3dd606cffce8fa968fa213b6d9028ef29bcad
Michele Franzin <michele.franzin@seesaw.it>
2021-08-04 17:13:56 +0200

    Typo in binary name (#109466)
    If a case sensitive filesystem is present, the following error occur:
    
    ...
    installer: The upgrade was successful.
    ==> Purging files for version 3.3.0 of Cask nextcloud
    Error: It seems the symlink source '/Applications/nextcloud.app/Contents/MacOS/nextcloudcmd' is not there.

--- Casks/nextcloud.rb
+++ Casks/nextcloud.rb
@@ -20,7 +20,7 @@ cask "nextcloud" do
   depends_on macos: ">= :yosemite"
 
   pkg "Nextcloud-#{version}.pkg"
-  binary "#{appdir}/nextcloud.app/Contents/MacOS/nextcloudcmd"
+  binary "#{appdir}/Nextcloud.app/Contents/MacOS/nextcloudcmd"
 
   uninstall pkgutil: "com.nextcloud.desktopclient"
 
