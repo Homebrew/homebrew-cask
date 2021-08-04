a11b129ecb76fbbd1b9bc2299ddb1ca8145d9720
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-04 11:39:56 +0800

    Update kdocs.rb

--- Casks/kdocs.rb
+++ Casks/kdocs.rb
@@ -1,15 +1,19 @@
 cask "kdocs" do
-  version "v2.13.0"
-  sha256 "cc0aa869986ff7302b1743d3deb34a0f9cf801a5e0bb5e6060209269efb12419"
+  version "2.13.0,1004"
+  sha256 "f2c860e6a79732570c27020fe53335b1ef3fd5201e1f3d0e3c93af07e3b1347f"
 
-  url "https://qn.cache.wpscdn.cn/kdocs/apk/kdesktopmac/KDocs_1001_#{version}.dmg", verified: "wpscdn.cn"
+  url "https://qn.cache.wpscdn.cn/kdocs/apk/kdesktopmac/KDocs_#{version.after_comma}_#{version.before_comma}.dmg",
+      verified: "qn.cache.wpscdn.cn/kdocs/apk/kdesktopmac/"
   name "金山文档"
   desc "Online collaborate editor for Word, Excel and PPT documents"
   homepage "https://www.kdocs.cn/"
 
   livecheck do
     url "https://www.kdocs.cn/kd/api/configure/list?idList=appOfficial"
-    regex(%r{.*/apk/kdesktopmac/KDocs_\d+_([v.\d]+)\.dmg}i)
+    strategy :page_match do |page|
+      match = page.match(/kdocs[._-](\d+(?:\.\d+)*)[._-]v?(\d+(?:\.\d+)*)\.dmg/i)
+      "#{match[2]},#{match[1]}"
+    end
   end
 
   app "金山文档.app"
