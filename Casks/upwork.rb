348b88108018daff4ec133c5335f98a54e6cf44d
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-04 08:57:28 +0800

    Update upwork.rb

--- Casks/upwork.rb
+++ Casks/upwork.rb
@@ -1,14 +1,14 @@
 cask "upwork" do
-  version "5.6.4.1,814e9843307a4269"
-  sha256 "875ea9c68acd3405f1b32bb3c5bf94ebb95ff6b6cd873b6bc698e4b330ba5ecb"
+  version "5.6.7.13,9f0e0a44a59e4331"
+  sha256 "ee4d1e24afbe60e6534204e96593990f365732d2a667a5f6b36e1c9359b77734"
 
-  url "https://updates-desktopapp.upwork.com/binaries/v#{version.before_comma.dots_to_underscores}_#{version.after_comma}/Upwork.dmg"
+  url "https://upwork-usw2-desktopapp.upwork.com/binaries/v#{version.before_comma.dots_to_underscores}_#{version.after_comma}/Upwork.dmg"
   name "Upwork"
   desc "Work marketplace for freelancing"
   homepage "https://www.upwork.com/"
 
   livecheck do
-    url "https://updates-desktopapp.upwork.com/binaries/versions-mac.json"
+    url "https://upwork-usw2-desktopapp.upwork.com/binaries/versions-mac.json"
     strategy :page_match do |page|
       match = page.match(%r{/v(\d+(?:_\d+)*)_([^/]+)/Upwork\.dmg}i)
       "#{match[1].tr("_", ".")},#{match[2]}"
