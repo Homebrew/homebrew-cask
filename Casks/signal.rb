992680468d6a86bd11f148e3ebb55d3be72331d4
Blazar <2w3ioecb@anonaddy.me1>
2021-08-04 10:42:09 +1000

    Update signal from 5.11.0 to 5.12.0

--- Casks/signal.rb
+++ Casks/signal.rb
@@ -1,6 +1,6 @@
 cask "signal" do
-  version "5.11.0"
-  sha256 "cc39be2e6faa328218fe9819d601577ac9ddd7868b65f0be6bcccbe1f31cff16"
+  version "5.12.0"
+  sha256 "ad77456637e184acb58ad4b184329ff97ec1ef8da1235744e44a842ba53df43d"
 
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{version}.dmg"
   name "Signal"
