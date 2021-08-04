f84528ce0e5a0e7b5493b48fb5e4e2cb4b410998
Tom Forbes <tom@tomforb.es>
2021-08-04 01:38:18 +0100

    Update alfaview from 8.22.0 to 8.24.0 (#109437)

--- Casks/alfaview.rb
+++ Casks/alfaview.rb
@@ -1,6 +1,6 @@
 cask "alfaview" do
-  version "8.22.0"
-  sha256 "522c9181686ce0aaed01d866e979a5563067b5e1b6951892a7d6dc74dc8a7686"
+  version "8.24.0"
+  sha256 "6a8bd1df489d50357c2277984b331a25c2673a5481c41cdc81d186deb9c4f5f6"
 
   url "https://assets.alfaview.com/stable/mac/alfaview-mac-production-#{version}.dmg"
   name "Alfaview"
