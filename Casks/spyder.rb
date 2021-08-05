27d241eb47fb6b8837331340cc2eb7d82119fecb
Dustin Wheeler <3818510+mskblackbelt@users.noreply.github.com>
2021-08-05 11:45:01 -0400

    Update spyder from 5.1.0 to 5.1.1

--- Casks/spyder.rb
+++ Casks/spyder.rb
@@ -1,6 +1,6 @@
 cask "spyder" do
-  version "5.1.0"
-  sha256 "9de911c9d563af93b37a6883ed686213b5e9d557914f1df5e8f59071363e11f5"
+  version "5.1.1"
+  sha256 "643fd900e985c6e14e2bffbc758e949304ac14d7d26ebebd64c0ca1148aeae7d"
 
   url "https://github.com/spyder-ide/spyder/releases/download/v#{version}/Spyder.dmg",
       verified: "github.com/spyder-ide/spyder/"
