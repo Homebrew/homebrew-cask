951c081dfa4d3c272ff76a5b4a75319ff3148f94
shottr <88459899+shottr@users.noreply.github.com>
2021-08-05 16:42:56 -0400

    Add Shottr.app v1.4.6 (#109481)
    * Add Shottr.app v1.4.6
    
    * Update shottr.rb
    
    * Update shottr.rb
    
    * Add Shottr.app v1.4.6
    
    Updated cask with Zap instructions
    
    * Update shottr.rb
    
    Co-authored-by: max <frip.nown@gmail.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/shottr.rb
@@ -0,0 +1,25 @@
+cask "shottr" do
+  version "1.4.6"
+  sha256 "c3afa816927e8755f64b9dbe5f2d9a71643277c8c9d45fc5447d664843c4e9ae"
+
+  url "https://shottr.cc/dl/Shottr-#{version}.dmg"
+  name "Shottr"
+  desc "Screenshot measurement and annotation tool"
+  homepage "https://shottr.cc/"
+
+  livecheck do
+    url :homepage
+    regex(/Shottr-(\d+(?:\.\d+)*)\.dmg/i)
+  end
+
+  depends_on macos: ">= :catalina"
+
+  app "Shottr.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/cc.ffitch.shottr",
+    "~/Library/Application Scripts/cc.ffitch.shottr-LaunchAtLoginHelper",
+    "~/Library/Containers/cc.ffitch.shottr",
+    "~/Library/Containers/cc.ffitch.shottr-LaunchAtLoginHelper",
+  ]
+end
