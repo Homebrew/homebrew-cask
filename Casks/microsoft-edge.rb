d4b02068befc48e06e4db98d8eb0dc5db0de9b5f
César Román <thecesrom@gmail.com>
2021-08-05 16:49:10 -0700

    Update microsoft-edge from 92.0.902.62 to 92.0.902.67

--- Casks/microsoft-edge.rb
+++ Casks/microsoft-edge.rb
@@ -1,14 +1,14 @@
 cask "microsoft-edge" do
-  version "92.0.902.62"
+  version "92.0.902.67"
 
   if Hardware::CPU.intel?
     url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdge-#{version}.pkg",
         verified: "officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/"
-    sha256 "b8de07c6c8cd01d91a818320f7c031c85f178320549a071a05696c3635e382d7"
+    sha256 "afb14750572b49c2ad5f25b637ffb3d637107e3db094b97b14a386a9f85a9af7"
   else
     url "https://officecdn-microsoft-com.akamaized.net/pr/03ADF619-38C6-4249-95FF-4A01C0FFC962/MacAutoupdate/MicrosoftEdge-#{version}.pkg",
         verified: "officecdn-microsoft-com.akamaized.net/pr/03ADF619-38C6-4249-95FF-4A01C0FFC962/MacAutoupdate/"
-    sha256 "baf0d28fddea6f1e62d6d772c1269339e1da81464a771d8640d2556f77d03214"
+    sha256 "8ed41b289a0505c8882ae1e77c1542aa5d8c365d21b6514e406a4a8707da7fd8"
   end
 
   name "Microsoft Edge"
