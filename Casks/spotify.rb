e34e05b7a502c753628aa71e980f717a25a3b809
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-06 04:49:08 +0800

    Update spotify.rb

--- Casks/spotify.rb
+++ Casks/spotify.rb
@@ -1,17 +1,31 @@
 cask "spotify" do
-  version "1.1.65.643.g2d707698,1.1.65.643.g2d707698-28"
-  sha256 :no_check
+  if Hardware::CPU.intel?
+    version "1.1.65.643.g2d707698,1.1.65.643.g2d707698-28"
+    sha256 :no_check
 
-  url "https://download.scdn.co/Spotify.dmg",
-      verified: "scdn.co/"
+    url "https://download.scdn.co/Spotify.dmg",
+        verified: "scdn.co/"
+
+    livecheck do
+      url :url
+      strategy :extract_plist
+    end
+  else
+    version "1.1.63.488.g27383f12,1.1.63.488.g27383f12-868"
+    sha256 :no_check
+
+    url "https://download.scdn.co/SpotifyBetaARM64.dmg",
+        verified: "scdn.co/"
+
+    livecheck do
+      url :url
+      strategy :extract_plist
+    end
+  end
+
   name "Spotify"
   desc "Music streaming service"
   homepage "https://www.spotify.com/"
-
-  livecheck do
-    url :url
-    strategy :extract_plist
-  end
 
   auto_updates true
 
