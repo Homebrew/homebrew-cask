a5947b89168b1df80cba8af5a114bb7d494a5773
max <frip.nown@gmail.com>
2021-08-04 22:57:15 -0400

    Add Shottr.app v1.4.6

---
+++ Casks/shottr.rb
@@ -0,0 +1,11 @@
+cask "shottr" do
+  version "1.4.6"
+  sha256 "c3afa816927e8755f64b9dbe5f2d9a71643277c8c9d45fc5447d664843c4e9ae"
+
+  url "https://shottr.cc/dl/Shottr-#{version}.dmg"
+  name "Shottr"
+  desc "Screenshot measurement and annotation tool"
+  homepage "https://shottr.cc/"
+
+  app "Shottr.app"
+end
