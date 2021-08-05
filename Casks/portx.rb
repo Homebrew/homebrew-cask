6ab4fd8e2b9f85ffeb33374dd2225f1c3a727f8a
NetSarang Computer <83996870+NetSarangComputer@users.noreply.github.com>
2021-08-05 19:29:19 +0900

    Updated PortX.app v1.1.5 (#109485)
    * Updated PortX.app v1.1.5
    
    * Update Casks/portx.rb
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    
    * portx: use actual SHA instead of placeholder
    
    * Update portx.rb
    
    Co-authored-by: NetSarang Computer <NetSarangComputer@MacBook-Pro.local>
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/portx.rb
+++ Casks/portx.rb
@@ -1,8 +1,8 @@
 cask "portx" do
-  version "1.1.4,10.2"
-  sha256 :no_check
+  version "1.1.5,8"
+  sha256 "0a6554d35f92413a7a3ecd37d037587dab4f45f8bd49483ff371825f77b9e03c"
 
-  url "https://cdn.netsarang.net/0ac7ea20/PortX-latest.dmg",
+  url "https://cdn.netsarang.net/0ac7ea20/PortX-#{version.before_comma}.dmg",
       verified: "cdn.netsarang.net/"
   name "portx"
   desc "SSH Client"
