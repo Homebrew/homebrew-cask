566fc079967fddd69a3a1eaa7e3135f037386d92
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-06 05:16:07 +0800

    spotify: add arm version (#109509)
    * Update spotify.rb
    
    * Update spotify.rb

--- Casks/spotify.rb
+++ Casks/spotify.rb
@@ -1,9 +1,18 @@
 cask "spotify" do
-  version "1.1.65.643.g2d707698,1.1.65.643.g2d707698-28"
-  sha256 :no_check
+  if Hardware::CPU.intel?
+    version "1.1.65.643.g2d707698,1.1.65.643.g2d707698-28"
+    sha256 :no_check
 
-  url "https://download.scdn.co/Spotify.dmg",
-      verified: "scdn.co/"
+    url "https://download.scdn.co/Spotify.dmg",
+        verified: "scdn.co/"
+  else
+    version "1.1.63.488.g27383f12,1.1.63.488.g27383f12-868"
+    sha256 :no_check
+
+    url "https://download.scdn.co/SpotifyBetaARM64.dmg",
+        verified: "scdn.co/"
+  end
+
   name "Spotify"
   desc "Music streaming service"
   homepage "https://www.spotify.com/"
