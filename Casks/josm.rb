0c51dc4a4bef7106630b34ed27f58a9bf400d25d
Dan Cojocaru <dan.cojocaru@3pillarglobal.com>
2021-08-05 10:10:26 +0300

    Update josm from 18118 to 18118

--- Casks/josm.rb
+++ Casks/josm.rb
@@ -1,6 +1,6 @@
 cask "josm" do
-  version "18004"
-  sha256 "412561f34e372bf7fb80f123a2c1a8fc3363d5def92d41707fa10c78793af1f3"
+  version "18118"
+  sha256 "278d087f601438d76a6835e732fcc81b12c945a20178bb041fcd25f2307d3b44"
 
   url "https://github.com/openstreetmap/josm/releases/download/#{version}-tested/JOSM-macOS-java16.zip",
       verified: "github.com/openstreetmap/josm/"
