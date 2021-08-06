e04ba330a4e25e34dfd3b68f45d2adad94c5c777
eloyaldea <35566945+eloyaldea@users.noreply.github.com>
2021-08-06 16:18:03 +0200

    Update streamlabs-obs from 1.3.0 to 1.3.1 (#109532)

--- Casks/streamlabs-obs.rb
+++ Casks/streamlabs-obs.rb
@@ -1,6 +1,6 @@
 cask "streamlabs-obs" do
-  version "1.3.0"
-  sha256 "6c704f14df515c4b17c18e030d0db53a558b74717ce23503bfb79a400cc1c182"
+  version "1.3.1"
+  sha256 "f988925405e7bc7b5ba6d2245232a32c684f537596379e751b681dc13e053950"
 
   url "https://slobs-cdn.streamlabs.com/Streamlabs+OBS-#{version}.dmg"
   name "Streamlabs OBS"
