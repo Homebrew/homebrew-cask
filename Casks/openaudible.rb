ef8dbf2874e54df5d2cba3d7fca298076c8a632e
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-08-06 22:08:36 +0900

    Update openaudible from 3.0.1 to 3.0.2

--- Casks/openaudible.rb
+++ Casks/openaudible.rb
@@ -1,6 +1,6 @@
 cask "openaudible" do
-  version "3.0.1"
-  sha256 "fc7485f9b2c5e5500a14f57962189ab376e395514d075be214cdbae67928bca6"
+  version "3.0.2"
+  sha256 "72d44ccdb8210fbe332a7e554bf8efc3e945064589af3ef4f739ebe32e3b6f67"
 
   url "https://github.com/openaudible/openaudible/releases/download/v#{version}/OpenAudible_#{version}.dmg",
       verified: "github.com/openaudible/"
