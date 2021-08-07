c8576c80e4f4f3303e060716fa23877b0dfaeb9a
min <min@doiob.cn>
2021-08-08 01:59:27 +0800

    Update electron-fiddle from 0.25.2 to 0.26.0 (#109553)

--- Casks/electron-fiddle.rb
+++ Casks/electron-fiddle.rb
@@ -1,13 +1,13 @@
 cask "electron-fiddle" do
-  version "0.25.2"
+  version "0.26.0"
 
   if Hardware::CPU.intel?
-    sha256 "e220f4764ebf3e17f5989e228ae0b550e5315e0f3bd90af457009a92ef4072dd"
+    sha256 "194e36302952db1234e3dc067f43ab2b3eef0fca46c3bf6353d081b7d1a06d34"
 
     url "https://github.com/electron/fiddle/releases/download/v#{version}/Electron.Fiddle-darwin-x64-#{version}.zip",
         verified: "github.com/electron/fiddle/"
   else
-    sha256 "599d63a6a9eb154a938971670afd4c053eda2d145f3f552192f39ed22e89bda1"
+    sha256 "edd7d9739b076f0c5145a02169c1b29018314fd93a6c368b1ca686d2d1ae25b2"
 
     url "https://github.com/electron/fiddle/releases/download/v#{version}/Electron.Fiddle-darwin-arm64-#{version}.zip",
         verified: "github.com/electron/fiddle/"
