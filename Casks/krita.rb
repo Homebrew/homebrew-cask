55fed0f52de6ef8f4ea7da8befe0943524c375b6
i0ntempest <szf1234@me.com>
2021-08-08 01:08:34 +0800

    Update krita from 4.4.5 to 4.4.7

--- Casks/krita.rb
+++ Casks/krita.rb
@@ -1,6 +1,6 @@
 cask "krita" do
-  version "4.4.5"
-  sha256 "1b0e6ecb539043f6522da6f5192c7fcdaae3641fbe5d4a766e4c7834288efc71"
+  version "4.4.7"
+  sha256 "d1284670ed43205a30364b85fb90f72e0c7389baf50b1c92c6ca665025cd0cbe"
 
   url "https://cdn.download.kde.org/stable/krita/#{version.major_minor_patch}/krita-#{version}.dmg",
       verified: "cdn.download.kde.org/stable/krita/"
