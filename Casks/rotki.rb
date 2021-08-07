4c082a35251f7b6966eb58435d33e0197ed864ca
Dhruba Basu <7675102+dhrubabasu@users.noreply.github.com>
2021-08-07 16:01:45 -0400

    Update rotki from 1.18.1 to 1.20.0 (#109555)

--- Casks/rotki.rb
+++ Casks/rotki.rb
@@ -1,8 +1,8 @@
 cask "rotki" do
-  version "1.18.1"
-  sha256 "354cfbdc96d2711af01f7a8161f5310d65286f3af1d3ecf2d1779575563af954"
+  version "1.20.0"
+  sha256 "ed60d90a654174004c953a515056ca5b8d02e1b39ddedc82272a868389531da4"
 
-  url "https://github.com/rotki/rotki/releases/download/v#{version}/rotki-darwin-v#{version}.dmg",
+  url "https://github.com/rotki/rotki/releases/download/v#{version}/rotki-darwin_x64-v#{version}.dmg",
       verified: "github.com/rotki/rotki/"
   name "Rotki"
   desc "Portfolio tracking and accounting tool"
