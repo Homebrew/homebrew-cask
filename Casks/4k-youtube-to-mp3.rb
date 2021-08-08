1f02db97cd2fb37aad005844ce9ce13bc65e7252
Jeffrey C <990135+spaz926@users.noreply.github.com>
2021-08-08 06:51:58 -0500

    Update 4k-youtube-to-mp3 from 4.2.0 to 4.2.1 (#109567)

--- Casks/4k-youtube-to-mp3.rb
+++ Casks/4k-youtube-to-mp3.rb
@@ -1,7 +1,7 @@
 cask "4k-youtube-to-mp3" do
   # NOTE: "3" is not a version number, but an intrinsic part of the product name
-  version "4.2.0"
-  sha256 "85756abd956530c8b318f265a3636a5a901eb262871263c1a37b11a500b1c38a"
+  version "4.2.1"
+  sha256 "ac8b4f23a97e1ccb686faee4c7deeedce386621e2a074d899152262a1b0c46ce"
 
   url "https://dl.4kdownload.com/app/4kyoutubetomp3_#{version}.dmg"
   name "4K YouTube to MP3"
