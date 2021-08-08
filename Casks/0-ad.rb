6f01d8bde3c6810b5c58583d9ea72c99b4659558
André David <adavidzh@users.noreply.github.com>
2021-08-08 23:27:40 +0200

    Draft: Update 0-ad to 0.0.25-alpha (#109580)
    * Update 0-ab to 0.0.25-alpha
    
    Computed `sha256` on file that matches the published `md5` and `sha1`.
    
    * Update 0-ad.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/0-ad.rb
+++ Casks/0-ad.rb
@@ -1,13 +1,6 @@
 cask "0-ad" do
-  if MacOS.version <= :el_capitan
-    version "0.0.23b-alpha"
-    sha256 "5fae1331bb1778bf16e49460fb850270b6cd76bee6483078ff27582e71ab263f"
-    app "0 A.D.app"
-  else
-    version "0.0.24b-alpha"
-    sha256 "483bfdff249ff93435a4638631b47b4037b43e44719ba2efdc10f7f76771fc33"
-    app "0 A.D..app"
-  end
+  version "0.0.25-alpha"
+  sha256 "8be21a9a2859580729ea90ab7a53c36663368d7d77e22bbded8a48164a51bc0d"
 
   url "https://releases.wildfiregames.com/0ad-#{version}-osx64.dmg",
       verified: "releases.wildfiregames.com/"
@@ -19,4 +12,8 @@ cask "0-ad" do
     url "https://play0ad.com/download/mac/"
     regex(/href=.*?0ad[._-]v?(.*?)-osx64\.dmg/i)
   end
+
+  depends_on macos: ">= :sierra"
+
+  app "0 A.D..app"
 end
