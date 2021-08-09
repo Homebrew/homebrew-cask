40e95796dd80ea207dacde61d914e0e974c09428
Bevan Kay <email@bevankay.me>
2021-08-09 10:23:53 +1000

    add livecheck skip in conditional

--- Casks/secretive.rb
+++ Casks/secretive.rb
@@ -2,6 +2,10 @@ cask "secretive" do
   if MacOS.version <= :catalina
     version "1.0.3"
     sha256 "d8522c153f20cd03513e6815bdb46be98eae0db2b2a45d30f60b25a6609d1657"
+
+    livecheck do
+      skip "Legacy version for MacOS Catalina"
+    end
   else
     version "2.1.1"
     sha256 "24a79464506080c2fb7f20671678f727f5e2ebf436839c8b90ad953d305fd999"
