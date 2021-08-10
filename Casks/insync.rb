5275658fcfa56dcc0797bc2e9f379d2e397a7784
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-10 21:00:51 +0800

    Update insync from 3.4.2.40983 to 3.5.0.50109 (#109645)

--- Casks/insync.rb
+++ Casks/insync.rb
@@ -1,6 +1,6 @@
 cask "insync" do
-  version "3.4.2.40983"
-  sha256 "870b6c6d105d480cf1f69e44e3d9cba72df3c67bf52f261e54d1033303db5a6b"
+  version "3.5.0.50109"
+  sha256 "3446e3dcd06b6927231388d8a68d26214e0d0fe000ab04072d566da5d095aaa0"
 
   url "http://s.insynchq.com/builds/Insync-#{version}.dmg"
   name "Insync"
