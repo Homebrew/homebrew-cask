eb1775cb18d4b6ba631fb6b653efd8937b6b1531
miccal <miccal.matthews@gmail.com>
2021-08-10 21:01:42 +0800

    Update nault from 1.14.0 to 1.15.0

--- Casks/nault.rb
+++ Casks/nault.rb
@@ -1,6 +1,6 @@
 cask "nault" do
-  version "1.14.0"
-  sha256 "261f4b9b86cbff29a4856c1182c3e904ac9838a2190c9c7a7907da9b64bd2ad1"
+  version "1.15.0"
+  sha256 "5c13bacbd888a7d3d4065c0e04caa8d699c6b3d2e92b57a9673cd040dbe52333"
 
   url "https://github.com/Nault/Nault/releases/download/v#{version}/Nault-#{version}-Mac.dmg"
   name "Nault"
