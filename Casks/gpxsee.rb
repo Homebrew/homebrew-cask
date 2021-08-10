c1922ecbb5d4eceab4f9fd5c5de8161f17eb67fe
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-10 21:02:07 +0800

    Update gpxsee from 9.3 to 9.4 (#109648)

--- Casks/gpxsee.rb
+++ Casks/gpxsee.rb
@@ -1,6 +1,6 @@
 cask "gpxsee" do
-  version "9.3"
-  sha256 "1f43ef1f5502c01346748c0525c4f6749b4320ac8d6c71960b9683d5e3b31621"
+  version "9.4"
+  sha256 "af9ef4efaeb33022bcf2963e4e34a66780bf5e9420d8e2269c356042483fd64d"
 
   url "https://downloads.sourceforge.net/gpxsee/Mac%20OS%20X/GPXSee-#{version}.dmg",
       verified: "sourceforge.net/gpxsee/Mac%20OS%20X/"
