ceec38ceeb13a71c9b64c008e893ede8399bdb65
Caden Haustein <cadenhaustein@gmail.com>
2021-08-10 05:18:17 +0000

    Update Librewolf from 90.0.2-1 to 91.0-1 (#109602)

--- Casks/librewolf.rb
+++ Casks/librewolf.rb
@@ -1,7 +1,7 @@
 cask "librewolf" do
   if Hardware::CPU.intel?
-    version "90.0.2-1,cf9d3890330b86823b100271bd82beda"
-    sha256 "d0cc9a78c1e584250bbe069d61f80da8af0d730abd920aef0071704cf5a7930b"
+    version "91.0-1,d828003a7a327267b75b578e8b49e5fe"
+    sha256 "7fe75921efbaff4032d02953fab6239c5c0827956e51fcd34d5b8d75b5aff806"
 
     url "https://gitlab.com/librewolf-community/browser/macos/uploads/#{version.after_comma}/librewolf-#{version.before_comma}.dmg",
         verified: "gitlab.com/librewolf-community/browser/macos/"
@@ -16,8 +16,8 @@ cask "librewolf" do
       end
     end
   else
-    version "90.0.2-1,ad1de7d5d4d7bfc3bc6fbd65e6e75a87"
-    sha256 "589ee6cb055585b005de04fcd39c7d9aedf025f371fa8fd2a83b1b53401e1011"
+    version "91.0-1,ffbdf10070a2e7797344dd3bc7734fbd"
+    sha256 "ffe85de1444da6d2a1de1723e372729dfdccd3f72c06a2ab5cadd8e4a22e82bc"
 
     url "https://gitlab.com/librewolf-community/browser/macos/uploads/#{version.after_comma}/librewolf-#{version.before_comma}_aarch64_exp.dmg",
         verified: "gitlab.com/librewolf-community/browser/macos/"
