7f268e24fee322dfd25b2ed66f2ea819e55d585c
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-10 21:12:47 +0800

    Update google-web-designer.rb

--- Casks/google-web-designer.rb
+++ Casks/google-web-designer.rb
@@ -4,6 +4,7 @@ cask "google-web-designer" do
 
   url "https://dl.google.com/webdesigner/mac/shell/googlewebdesigner_mac.dmg"
   name "Google Web Designer"
+  desc "Create interactive HTML5-based designs and motion graphics"
   homepage "https://www.google.com/webdesigner/"
 
   livecheck do
