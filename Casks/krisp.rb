9434a41b98babba26eb5caf5205652a341166a36
Jeffrey C <990135+spaz926@users.noreply.github.com>
2021-08-10 08:11:27 -0500

    Update krisp from 1.23.7 to 1.24.7 (#109651)

--- Casks/krisp.rb
+++ Casks/krisp.rb
@@ -1,6 +1,6 @@
 cask "krisp" do
-  version "1.23.7"
-  sha256 "491740e7158de1c2ebf0f848d5c0c1f6d498f939b9520a98d758d6a1824eb035"
+  version "1.24.7"
+  sha256 "03da782c0bf2b1e4787ffbeb326a5921bf5294981418bd2d995f541d26f8723b"
 
   url "https://cdn.krisp.ai/mac/release/v#{version.major}.#{version.minor}/krisp_#{version}.pkg"
   name "Krisp"
