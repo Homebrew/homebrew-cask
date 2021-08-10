9b1cbdf488f94bacc7f49e5857c2cbdc78ccfcca
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-10 22:05:14 +0800

    Update pycharm-edu from 2021.1.2,211.7628.16 to 2021.1.3,211.7628.48 (#109668)

--- Casks/pycharm-edu.rb
+++ Casks/pycharm-edu.rb
@@ -1,12 +1,12 @@
 cask "pycharm-edu" do
-  version "2021.1.2,211.7628.16"
+  version "2021.1.3,211.7628.48"
 
   if Hardware::CPU.intel?
-    sha256 "74ea0fca6aefbf7c7916a5872f1d4fd107dff87e8604dc2057f785fc22124c30"
+    sha256 "5d910d1d81417dc117c964f6cb3adcda66df7dc1c8e77a6cef2050c094817872"
 
     url "https://download.jetbrains.com/python/pycharm-edu-#{version.before_comma}.dmg"
   else
-    sha256 "fd5057f8cb2b3f1028058c6456af75b2d6e7bd261356fec421502de77ccba1a3"
+    sha256 "70bf84f6d006d39eeb0e9c0cbba93fb683baa67bbe0339dffc03d7351c21bf12"
 
     url "https://download.jetbrains.com/python/pycharm-edu-#{version.before_comma}-aarch64.dmg"
   end
