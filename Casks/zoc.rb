2b7c43cff11a0309009b60dc58bd0ae074520cf8
rjpontefract <rjpontefract@users.noreply.github.com>
2021-08-10 21:28:37 +1200

    Update zoc from 8.02.6 to 8.02.7 (#109611)

--- Casks/zoc.rb
+++ Casks/zoc.rb
@@ -1,6 +1,6 @@
 cask "zoc" do
-  version "8.02.6"
-  sha256 "921986933f66c82583a8ca51512b0c6a215e169998ca5198ba905993f78398e2"
+  version "8.02.7"
+  sha256 "3b4363b186213f9516db2b4e02131bdce5633843b6f5915e93caa7fd2c13e7a6"
 
   url "https://www.emtec.com/downloads/zoc/zoc#{version.no_dots}.dmg"
   name "ZOC"
