bf951524fe81d1ea92a8e01c1d317309a6c1c4e6
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-10 22:21:51 +0800

    Update workflowy from 1.3.5-18094 to 1.3.6-723 (#109680)

--- Casks/workflowy.rb
+++ Casks/workflowy.rb
@@ -1,6 +1,6 @@
 cask "workflowy" do
-  version "1.3.5-18094"
-  sha256 "38a2d80afaa2372a6bda6f22facbab43ce7d091a01262edaffa40495369b51a9"
+  version "1.3.6-723"
+  sha256 "a45e7a645c9f5ed9cd9684b1ff8ec63663abab4e2f093f7d3f5b22df78f09ed9"
 
   url "https://github.com/workflowy/desktop/releases/download/v#{version}/WorkFlowy.zip",
       verified: "github.com/workflowy/desktop/"
