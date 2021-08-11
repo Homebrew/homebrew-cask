fc72974053c4282684c98f40206b4b47184b7d0c
Thomas Walter <tw@b-a-l-u.de>
2021-08-12 00:29:22 +0200

    Update jitsi-meet from 2.8.9 to 2.8.10

--- Casks/jitsi-meet.rb
+++ Casks/jitsi-meet.rb
@@ -1,6 +1,6 @@
 cask "jitsi-meet" do
-  version "2.8.9"
-  sha256 "0421f487459ca86cde6696c708cb5cc6e5db39dd913334089244d8e486af20cb"
+  version "2.8.10"
+  sha256 "1fe1df3b41611cdc0ee99bebd3e7f1750564e1d824464335da6a7485eca72afb"
 
   url "https://github.com/jitsi/jitsi-meet-electron/releases/download/v#{version}/jitsi-meet.dmg"
   name "Jitsi Meet"
