aeeb10981aec0cbe1200b5fd2fc11ffaa1125f1c
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-11 22:08:09 +0800

    Update vym.rb

--- Casks/vym.rb
+++ Casks/vym.rb
@@ -3,13 +3,11 @@ cask "vym" do
   sha256 "17587f3ad5550795f6224d09dd1bff659cb525ba77f6482b2d35138a16da1e80"
 
   url "https://downloads.sourceforge.net/vym/osx11-vym-#{version}-by%20shemeshg.zip"
-  name "vym"
-  desc "- view your mind"
+  name "VYM (View Your Mind)"
+  desc "Generate and manipulate maps which show your thoughts"
   homepage "https://sourceforge.net/projects/vym/"
 
   app "vym.app"
 
-  zap trash: [
-    "~/Library/Preferences/com.insilmaril.vym.plist",
-  ]
+  zap trash: "~/Library/Preferences/com.insilmaril.vym.plist"
 end
