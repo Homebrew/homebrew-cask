eb03ae670b3e2374fb03eb272b94eff0c3a74aa2
George Adams <georgeadams1995@gmail.com>
2021-08-11 14:15:54 +0100

    fix makehuman broken link

--- Casks/makehuman.rb
+++ Casks/makehuman.rb
@@ -1,8 +1,8 @@
 cask "makehuman" do
-  version "1.1.1"
-  sha256 "0f5626b1878f8f4233be431917e740806b141522f9f4b3da7809e8e7798ee2d8"
+  version "1.2.0"
+  sha256 "dbbaecc1286f20f65216f39dcb3021522ec89b6acd05c0ec769dfa22aa129c08"
 
-  url "https://download.tuxfamily.org/makehuman/releases/#{version}/makehuman-#{version}-osx.dmg",
+  url "https://download.tuxfamily.org/makehuman/releases/makehuman-community-#{version}-macos.zip",
       verified: "tuxfamily.org/makehuman/"
   appcast "https://download.tuxfamily.org/makehuman/releases/"
   name "MakeHuman"
