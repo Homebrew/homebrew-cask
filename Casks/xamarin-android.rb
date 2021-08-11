b55b7584090172b39ff178e4b2c3fe3fce7325b8
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-11 09:34:13 +0800

    Update xamarin-android.rb

--- Casks/xamarin-android.rb
+++ Casks/xamarin-android.rb
@@ -3,10 +3,14 @@ cask "xamarin-android" do
   sha256 "a2efde72d9d75eacf65a9e2dacc4516875a738646c7f098b764e130dab4e0949"
 
   url "https://dl.xamarin.com/MonoforAndroid/Mac/xamarin.android-#{version}.pkg"
-  appcast "https://docs.microsoft.com/en-us/xamarin/android/release-notes/",
-          must_contain: version.major_minor
   name "Xamarin.Android"
+  desc "Gives .NET developers complete access to Android SDK's"
   homepage "https://www.xamarin.com/platform"
+
+  livecheck do
+    url "https://software.xamarin.com/Service/Updates?v=2&pvd1ec039f-f3db-468b-a508-896d7c382999=0"
+    regex(%r{/xamarin\.android[._-]v?(\d+(?:\.\d+)+)\.pkg}i)
+  end
 
   pkg "xamarin.android-#{version}.pkg"
 
