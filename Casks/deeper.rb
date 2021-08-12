1846feba27cdaca54ee519ac3ec8ee1f76a626f1
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-12 21:13:38 +0800

    Update deeper.rb

--- Casks/deeper.rb
+++ Casks/deeper.rb
@@ -1,17 +1,5 @@
 cask "deeper" do
-  if MacOS.version <= :yosemite
-    version "2.0.4"
-    sha256 "70a8ae37e6a62541a03b1b144ff92bac38585ce936b1acc12ce484416db13b8f"
-  elsif MacOS.version <= :el_capitan
-    version "2.1.4"
-    sha256 "3dc9607644872da14a0b6f20722e36d0cb6cb7ab2528f86de1cf059086cf2848"
-  elsif MacOS.version <= :sierra
-    version "2.2.3"
-    sha256 "33fee21b65279e4459b6469dbc68f0c6df91663ed26d6b62042b21883efda0ed"
-  elsif MacOS.version <= :high_sierra
-    version "2.3.3"
-    sha256 "08ac5820428bcce74548786e8fda947edfaa31cf4a822d5c443835e73a11dd3b"
-  elsif MacOS.version <= :mojave
+  if MacOS.version <= :mojave
     version "2.4.8"
     sha256 "13dbe7bd680963aca91c40a4fd1e16648b63538f3213692db22dd91a3e3f2c89"
   elsif MacOS.version <= :catalina
