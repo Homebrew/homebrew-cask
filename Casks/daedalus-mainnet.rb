6b66be63a5c810924f15946d15a6ef6c0f9a7f29
yottalogical <email@yottalogical.com>
2021-08-12 09:45:54 -0400

    Update daedalus-mainnet from 4.2.0 to 4.2.1 (#109759)

--- Casks/daedalus-mainnet.rb
+++ Casks/daedalus-mainnet.rb
@@ -1,6 +1,6 @@
 cask "daedalus-mainnet" do
-  version "4.2.0,18540"
-  sha256 "66c27653da9109aea22842ed77a425dbef47f9408eeed76fa3c18c66e95bc3ca"
+  version "4.2.1,18759"
+  sha256 "bc1ce1a977a5ce548b3a886ffb1314e28e4452002ce66569033a72ab0c357673"
 
   url "https://update-cardano-mainnet.iohk.io/daedalus-#{version.before_comma}-mainnet-#{version.after_comma}.pkg",
       verified: "update-cardano-mainnet.iohk.io/"
