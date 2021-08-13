e1f98f7299f39b8f6d5554312615404db86275b4
Hendrik Emerson <hendrik.emerson@haufe-lexware.net>
2021-08-13 14:35:30 +0200

    Update seadrive from 2.0.15 to 2.0.16

--- Casks/seadrive.rb
+++ Casks/seadrive.rb
@@ -1,6 +1,6 @@
 cask "seadrive" do
-  version "2.0.15"
-  sha256 "b8fcd6dff8b42a3248533c4f07cb8c4b507c5ba628912e771a10b288d067a5d7"
+  version "2.0.16"
+  sha256 "81069853d5caa015e296e2d040ed09fda5759039dd1783c1f72b5f57dd098df0"
 
   url "https://download.seadrive.org/seadrive-#{version}.dmg",
       verified: "download.seadrive.org/"
