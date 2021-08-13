a4fb35bc34df3727c117af564d089d425e6a8ca3
Jeffrey C <990135+spaz926@users.noreply.github.com>
2021-08-13 05:22:08 -0500

    Update exodus from 21.8.5 to 21.8.13

--- Casks/exodus.rb
+++ Casks/exodus.rb
@@ -1,11 +1,11 @@
 cask "exodus" do
-  version "21.8.5"
+  version "21.8.13"
 
   if Hardware::CPU.intel?
-    sha256 "a43b36bd40d5ce093093d903bddfac9e49ec3a018ef9f3ed654a926a50f6c142"
+    sha256 "90b518cd13563884666d12ffcb6e9a37c13beb3a342874b5ab41912c96427864"
     url "https://downloads.exodus.com/releases/exodus-macos-#{version}.dmg"
   else
-    sha256 "f9384363c50abeeb72d3a1eed9fc4ac71d587f287b0047b1a962fdd4cc20708b"
+    sha256 "15e36cb51e41d20af2119c8188310b4751ead60e4411c8ca74e562e686532305"
     url "https://downloads.exodus.com/releases/exodus-macos-arm64-#{version}.dmg"
   end
 
