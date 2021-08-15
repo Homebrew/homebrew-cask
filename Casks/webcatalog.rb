00310688f9c8d37fa61b43cd0d21e116b2a294eb
Bevan Kay <email@bevankay.me>
2021-08-15 22:15:02 +1000

    Update webcatalog from 36.1.0 to 36.2.0 (#109840)

--- Casks/webcatalog.rb
+++ Casks/webcatalog.rb
@@ -1,13 +1,13 @@
 cask "webcatalog" do
-  version "36.1.0"
+  version "36.2.0"
 
   if Hardware::CPU.intel?
-    sha256 "0756ecb4e4ead496f84a45a9c212d3496718d2535a424eef8d758504b79a9056"
+    sha256 "e7aea8f69937d514e3ca09c8a47f08278ee1ec1633f115f5cdd87984f5093837"
 
     url "https://github.com/webcatalog/webcatalog-app/releases/download/v#{version}/WebCatalog-#{version}.dmg",
         verified: "github.com/webcatalog/webcatalog-app/"
   else
-    sha256 "9a210190f0a3c4e7b12ce850cc10a716ce6fb1ded76fdc0a85a15399a59d56f9"
+    sha256 "7d8ce4688f311408db123c89e426d75264dc5ea647f7aac9c7c298a23d833574"
 
     url "https://github.com/webcatalog/webcatalog-app/releases/download/v#{version}/WebCatalog-#{version}-arm64.dmg",
         verified: "github.com/webcatalog/webcatalog-app/"
