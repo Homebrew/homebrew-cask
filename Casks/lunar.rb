ff57b5b41fbe672457f0d2e20290c30540952161
Alin Panaitiu <alin.panaitiu@gmail.com>
2021-08-17 00:19:00 +0300

    Update lunar (#109904)

--- Casks/lunar.rb
+++ Casks/lunar.rb
@@ -1,6 +1,6 @@
 cask "lunar" do
   version "4.8.2"
-  sha256 "02c19d8198070a5d3d8afdba53d6b4ce0d48407f8524de296c6ad883ba3e7a23"
+  sha256 "69b14fe5124ca0d4857e68a904c648e61e69ea5c499d0ecaaff6fa67b74e5261"
 
   url "https://static.lunar.fyi/releases/Lunar-#{version}.dmg"
   name "Lunar"
