2093c7319b77659be4fac9ea2762cf3071a98098
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-16 13:56:42 +0800

    Update atlauncher from 3.4.6.0 to 3.4.6.2 (#109862)

--- Casks/atlauncher.rb
+++ Casks/atlauncher.rb
@@ -1,6 +1,6 @@
 cask "atlauncher" do
-  version "3.4.6.0"
-  sha256 "e008e6ff61bfe0c2a29bdad91f2451c670374285ece0c00f0193409d3d289086"
+  version "3.4.6.2"
+  sha256 "13f993ed6ec790009400e9eafdc6388af76726b9614ce3b42c32b71af9f610ab"
 
   url "https://github.com/ATLauncher/ATLauncher/releases/download/v#{version}/ATLauncher-#{version}.zip",
       verified: "github.com/ATLauncher/ATLauncher/"
