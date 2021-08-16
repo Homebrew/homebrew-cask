048785e1c0995e4eec508e22e2e7a02bfcd7c0c5
miccal <miccal.matthews@gmail.com>
2021-08-16 14:27:54 +0800

    Update bunch from 1.4.1,112 to 1.4.4,118

--- Casks/bunch.rb
+++ Casks/bunch.rb
@@ -1,6 +1,6 @@
 cask "bunch" do
-  version "1.4.1,112"
-  sha256 "b6be8964e6e4f3372ee1353b4c86c1aa8ae93f28c5464a4818d69515be0ebc5d"
+  version "1.4.4,118"
+  sha256 "1c9a1bd3c224e34235dc88c03ae57fd061b089cc67c7059b4358046e3dc862de"
 
   url "https://brettterpstra.com/updates/bunch/Bunch#{version.before_comma}#{version.after_comma}.dmg",
       verified: "brettterpstra.com/updates/bunch/"
