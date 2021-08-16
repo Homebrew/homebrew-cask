1ea29cbd9f069ab46fa9422e66d3f02c0ce8f0ea
Reno <25192197+singularitti@users.noreply.github.com>
2021-08-15 21:26:51 -0400

    Update videofusion from 1.4.1.1156.0 to 1.6.0.1791.0 (#109852)
    * Update videofusion from 1.4.1.1156.0 to 1.6.0.1791.0
    
    * Update videofusion.rb
    
    * Update videofusion.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/videofusion.rb
+++ Casks/videofusion.rb
@@ -1,8 +1,8 @@
 cask "videofusion" do
-  version "1.4.1.1156.0"
-  sha256 "bd5993eddafc0fcde4f8769a2ed9cdfc4535820bd34c9e71ac1ef6e9e05e40a8"
+  version "1.6.0.1791.0"
+  sha256 "13014b117c13eae7bad979f4c878d12cc6b4a364159d76bcf7805e32700b9d0e"
 
-  url "https://lf3-faceucdn-tos.pstatp.com/obj/faceu-packages/Jianying_#{version.dots_to_underscores}.pkg",
+  url "https://lf3-faceucdn-tos.pstatp.com/obj/faceu-packages/Jianying_#{version.dots_to_underscores}.dmg",
       verified: "lf3-faceucdn-tos.pstatp.com/obj/faceu-packages/"
   name "VideoFusion"
   name "剪映专业版"
@@ -12,18 +12,13 @@ cask "videofusion" do
   livecheck do
     url "https://lf3-beecdn.bytetos.com/obj/ies-fe-bee/bee_prod/biz_80/bee_prod_80_bee_publish_3563.json"
     strategy :page_match do |page|
-      JSON.parse(page)["mac_download_pkg"]["channel_default"][/(\d+(?:_\d+)+)\.pkg/i, 1].tr("_", ".")
+      JSON.parse(page)["mac_download_pkg"]["channel_default"][/(\d+(?:_\d+)+)\.dmg/i, 1].tr("_", ".")
     end
   end
 
   depends_on macos: ">= :mojave"
 
-  pkg "Jianying_#{version.dots_to_underscores}.pkg"
-
-  uninstall pkgutil: [
-    "com.lemon.lvpro",
-    "com.mygreatcompany.pkg.lvpro",
-  ]
+  app "VideoFusion-macOS.app"
 
   zap trash: [
     "~/Library/Caches/com.lemon.ee.lv",
