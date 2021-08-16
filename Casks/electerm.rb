3aeaac32cbe06097349d5adf2f71dd8d221fa525
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-08-16 21:22:08 +0900

    Update electerm from 1.15.3 to 1.16.1 (#109893)

--- Casks/electerm.rb
+++ Casks/electerm.rb
@@ -1,6 +1,6 @@
 cask "electerm" do
-  version "1.15.3"
-  sha256 "5b074a94902045ab0bd85ed88619ece9c251c73b3c221c74cd9275081a77674b"
+  version "1.16.1"
+  sha256 "4f3dda65ca909add7f2657d7c6a2dca54801808dfdbd572825c8257926e32da8"
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac-x64.dmg"
   name "electerm"
