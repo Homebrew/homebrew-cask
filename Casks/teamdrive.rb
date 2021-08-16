c8cb7608dda5b155735484d68a20233dc022d39d
Henning Schwentner <hs@wps.de>
2021-08-16 20:15:12 +0200

    Tweak appcast URL

--- Casks/teamdrive.rb
+++ Casks/teamdrive.rb
@@ -4,7 +4,7 @@ cask "teamdrive" do
 
   url "https://download.teamdrive.net/#{version}/TMDR/mac/Install-TeamDrive-#{version}_TMDR.dmg",
       verified: "teamdrive.net/"
-  appcast "https://teamdrive.com/en/downloads/",
+  appcast "https://teamdrive.com/en/downloads",
           must_contain: version.major_minor_patch
   name "TeamDrive"
   desc "Secure cloud storage service"
