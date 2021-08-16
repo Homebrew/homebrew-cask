d624a4418f63f1819baeba9cf439f34695b8f243
Guillaume <hi@255kb.dev>
2021-08-16 12:03:34 +0200

    Update Mockoon to 1.15.0 (#109884)

--- Casks/mockoon.rb
+++ Casks/mockoon.rb
@@ -1,6 +1,6 @@
 cask "mockoon" do
-  version "1.14.1"
-  sha256 "1cb5ace608f6229a8bb2de2a0e3866d6bbe81868609bf48b84de3edefef9fd74"
+  version "1.15.0"
+  sha256 "c68b7bb8a927521eea25d5e73d0d8402cb02499b86ad4f3900cefbe6e2be74d4"
 
   url "https://github.com/mockoon/mockoon/releases/download/v#{version}/mockoon.setup.#{version}.dmg",
       verified: "github.com/mockoon/mockoon/"
