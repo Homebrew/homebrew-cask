9818f93cc8c213e29417376c05e3b5550957efb9
Bevan Kay <email@bevankay.me>
2021-08-17 20:55:22 +1000

    Update pktriot from 0.10.10 to 0.11.0 (#110052)

--- Casks/pktriot.rb
+++ Casks/pktriot.rb
@@ -1,6 +1,6 @@
 cask "pktriot" do
-  version "0.10.10"
-  sha256 "e98b9e307e2bc72cfac1cdad23eb81489db3ba0e140a6463cd486bebaff297de"
+  version "0.11.0"
+  sha256 "bf519dbde486cd4f24761c2dde417637c9b27f3f471a5d77738e450d552ebde5"
 
   url "https://download.packetriot.com/macos/pktriot-#{version}.macos.tar.gz"
   name "pktriot"
