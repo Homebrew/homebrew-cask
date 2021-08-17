851a6dc528b03e23b61a6905c122481c23e520e8
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-17 23:48:45 +0800

    Update invesalius.rb

--- Casks/invesalius.rb
+++ Casks/invesalius.rb
@@ -1,8 +1,8 @@
 cask "invesalius" do
-  version "3.1.1"
-  sha256 "678b2f028c66b805a9865524eba49852f0c7f7e7d231a965ffa78b0ced47b629"
+  version "3.1.99995"
+  sha256 "e9ee3a7548cee911355440fb87362a14080e4ec8372b85fdb6dd118cb294d173"
 
-  url "https://github.com/invesalius/invesalius3/releases/download/v#{version}/InVesalius-#{version}.dmg",
+  url "https://github.com/invesalius/invesalius3/releases/download/v#{version}/InVesalius.#{version}.dmg",
       verified: "github.com/invesalius/invesalius3/"
   name "InVesalius"
   homepage "https://www.cti.gov.br/en/invesalius"
