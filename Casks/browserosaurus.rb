2368cef8a6eed10d0ae6f5595c91c31a82311b9a
Will Stone <654103+will-stone@users.noreply.github.com>
2021-08-17 15:57:14 +0100

    Update Browserosaurus homepage (#110109)
    * Update Browserosaurus homepage
    
    * Remove "verified" params from download URLs

--- Casks/browserosaurus.rb
+++ Casks/browserosaurus.rb
@@ -3,17 +3,15 @@ cask "browserosaurus" do
 
   if Hardware::CPU.intel?
     sha256 "c22210139cd473fcba095605fc94221d8fe2eaaed07469b7d0bbaa363a953f96"
-    url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-darwin-x64-#{version}.zip",
-        verified: "github.com/will-stone/browserosaurus/"
+    url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-darwin-x64-#{version}.zip"
   else
     sha256 "f1e16692e42115d99fab14c1001ce37bf53db10919bb360e8bd4b3b34cdcf348"
-    url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-darwin-arm64-#{version}.zip",
-        verified: "github.com/will-stone/browserosaurus/"
+    url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-darwin-arm64-#{version}.zip"
   end
 
   name "Browserosaurus"
   desc "Open-source browser prompter"
-  homepage "https://wstone.io/browserosaurus/"
+  homepage "https://github.com/will-stone/browserosaurus"
 
   auto_updates true
 
