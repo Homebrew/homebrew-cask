c340f7a6dcd4aa836043eff869890a041dd22935
Bevan Kay <email@bevankay.me>
2021-08-17 16:20:21 +1000

    Update millie from 2.18.0 to 2.18.3 (#110017)

--- Casks/millie.rb
+++ Casks/millie.rb
@@ -1,6 +1,6 @@
 cask "millie" do
-  version "2.18.0"
-  sha256 "5dcd5d7e5ffbd40f3cc1b831fa0ac420494960e21cb121a91c25adeccca6a734"
+  version "2.18.3"
+  sha256 "98a0b5300cc7c84a465f85d0fc997025325492bf1253a86ef841684a43b6dc1d"
 
   url "https://apis.millie.co.kr/v1/download/installer/mac/Millie-#{version}.dmg"
   name "millie"
