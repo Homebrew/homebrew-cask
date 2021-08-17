2069f45bad4c1222cd51a45f892ced7a5a716f74
Bevan Kay <email@bevankay.me>
2021-08-17 11:57:02 +1000

    Update droplr from 5.9.5,464 to 59.1.1,470 (#109951)

--- Casks/droplr.rb
+++ Casks/droplr.rb
@@ -1,6 +1,6 @@
 cask "droplr" do
-  version "5.9.5,464"
-  sha256 "bd381271637598bb4225c3f79a0eb7cc0515fb839f52b947fcaf1bd95e2ca948"
+  version "59.1.1,470"
+  sha256 "5c96b2cfb5cbd12cc03dbd07c64e17c41b97e18c588275fa0b9311d63f6dbd31"
 
   url "https://files.droplr.com/apps/mac/Droplr#{version.before_comma.no_dots}-#{version.after_comma}.zip"
   name "Droplr"
