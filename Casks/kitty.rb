d003d24417b852a59a7b0d9378579d43f92bb7c7
Peter Lithammer <peter.lithammer@gmail.com>
2021-08-17 08:30:02 +0200

    Update kitty from 0.23.0 to 0.23.1

--- Casks/kitty.rb
+++ Casks/kitty.rb
@@ -1,6 +1,6 @@
 cask "kitty" do
-  version "0.23.0"
-  sha256 "0759c59f98d925c0e2902b232157e65c20288cdadc1264e9cd2afbc9c2a5adf0"
+  version "0.23.1"
+  sha256 "a0987a7d00497a2251e7990f7a3128aa96155730abc322c3cd2648bd5a839e0f"
 
   url "https://github.com/kovidgoyal/kitty/releases/download/v#{version}/kitty-#{version}.dmg"
   name "kitty"
