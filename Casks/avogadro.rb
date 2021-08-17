a9ad73a33021286f1fc099b130b7502e1086ad37
Bevan Kay <email@bevankay.me>
2021-08-17 10:23:49 +1000

    Update avogadro from 1.94.0 to 1.95.0

--- Casks/avogadro.rb
+++ Casks/avogadro.rb
@@ -1,6 +1,6 @@
 cask "avogadro" do
-  version "1.94.0"
-  sha256 "305d81a2dfcd47f3ee96ca072847bd8fe0d2de2c9a9e21d213a9a79da481e40e"
+  version "1.95.0"
+  sha256 "a8f67fdce30b7984cce850e6046ef7507104bd2a3a197b0ab645d9a4be597ccf"
 
   url "https://github.com/OpenChemistry/avogadrolibs/releases/download/#{version}/Avogadro2-#{version}-Darwin.dmg",
       verified: "github.com/OpenChemistry/avogadrolibs/"
