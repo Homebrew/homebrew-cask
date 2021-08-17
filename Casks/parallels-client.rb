4ae83dd70b036ae60b26d86daa7fda00872d0ce1
Bevan Kay <email@bevankay.me>
2021-08-17 20:55:07 +1000

    Update parallels-client from 18.1.0,22708 to 18.1.0.1,22712 (#110048)
    * Update parallels-client from 18.1.0,22708 to 18.1.0.1,22712
    
    * add desc

--- Casks/parallels-client.rb
+++ Casks/parallels-client.rb
@@ -1,9 +1,10 @@
 cask "parallels-client" do
-  version "18.1.0,22708"
-  sha256 "dea9583c7e1af302c8ad8f4362bac7318022ba5bc3dd7edcc63fa0ae9f956649"
+  version "18.1.0.1,22712"
+  sha256 "216c851551cd63fe16fd4f6293a26e26083ddec6919ff4f5585d957968079e23"
 
   url "https://download.parallels.com/ras/v#{version.major}/#{version.before_comma.major_minor_patch}.#{version.after_comma}/RasClient-Mac-Notarized-#{version.before_comma.major_minor_patch}-#{version.after_comma}.pkg"
   name "Parallels Client"
+  desc "RDP client"
   homepage "https://www.parallels.com/products/ras/features/rdp-client/"
 
   livecheck do
