1af1cb0ba26488f8c945f99d03028bb9ca566b1a
Bevan Kay <email@bevankay.me>
2021-08-17 20:17:48 +1000

    Update openloco from 21.07 to 21.08

--- Casks/openloco.rb
+++ Casks/openloco.rb
@@ -1,6 +1,6 @@
 cask "openloco" do
-  version "21.07"
-  sha256 "f288b75f37c0157b28cf783d25da898c38f0aa201c9818ea0a61df79fc3449d0"
+  version "21.08"
+  sha256 "d4fb13137c8ce463e307551ee41af7cd9290e751ddf7f09f2592d5bec8d9016d"
 
   url "https://github.com/OpenLoco/OpenLoco/releases/download/v#{version}/openloco-v#{version}-macos.zip"
   name "OpenLoco"
