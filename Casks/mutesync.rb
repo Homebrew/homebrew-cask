757bd9761518a6f05dd4ca6fe0740581c5806d97
Bevan Kay <email@bevankay.me>
2021-08-17 16:16:39 +1000

    Update mutesync from 4.7.1 to 4.7.3

--- Casks/mutesync.rb
+++ Casks/mutesync.rb
@@ -1,6 +1,6 @@
 cask "mutesync" do
-  version "4.7.1"
-  sha256 "30791ba4668ba8b8574c752449a51302a479a353a706d48f520e763d93c25007"
+  version "4.7.3"
+  sha256 "289b2cc11a398c1df7b4cc6e7409330a59f99cce4e0bdd45d6ae02140b8ad932"
 
   url "https://mutesync.s3-us-west-2.amazonaws.com/mutesync-#{version}.dmg",
       verified: "mutesync.s3-us-west-2.amazonaws.com/"
