5164473d82e9b37d9c89500ddf047dcbf28c6e67
Zhenfu Shi <szf1234@me.com>
2021-08-17 20:22:56 +0800

    Update qv2ray.rb

--- Casks/qv2ray.rb
+++ Casks/qv2ray.rb
@@ -2,7 +2,8 @@ cask "qv2ray" do
   version "2.7.0"
   sha256 "457e1b70989515ddb9a1bd4c0635d4ea7d5eda17435bd63908151621b1235a97"
 
-  url "https://github.com/Qv2ray/Qv2ray/releases/download/v#{version}/Qv2ray-v#{version}-macOS-x64.dmg"
+  url "https://github.com/Qv2ray/Qv2ray/releases/download/v#{version}/Qv2ray-v#{version}-macOS-x64.dmg",
+      verified: "github.com/Qv2ray/Qv2ray/"
   name "Qv2ray"
   desc "V2Ray GUI client with extensive protocol support"
   homepage "https://qv2ray.net/"
