2e178b4fb947ac8ffdee0bb8d4abd0359f0b6d75
Bevan Kay <email@bevankay.me>
2021-08-17 11:28:30 +1000

    Update cog from 1557,63d1346c to 1570,3f6cb1bc (#109933)

--- Casks/cog.rb
+++ Casks/cog.rb
@@ -1,6 +1,6 @@
 cask "cog" do
-  version "1557,63d1346c"
-  sha256 "eeeb08236ddff47b8b58c3e16df452374e82e20ee937ddbc1786b127c8c87e28"
+  version "1570,3f6cb1bc"
+  sha256 "ac32874a03f879f179d2e69d543e73ff97ec1754452167fdccc08349bdc37f08"
 
   url "https://f.losno.co/cog/Cog-#{version.after_comma}.zip"
   name "Cog"
