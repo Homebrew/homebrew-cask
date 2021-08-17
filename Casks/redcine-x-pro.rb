b3beb5d0bcc9f3169ddfc249a8bc0b682d11cdb8
Bevan Kay <bevanjkay@gmail.com>
2021-08-17 22:12:49 +1000

    update redcine-x-pro.rb

--- Casks/redcine-x-pro.rb
+++ Casks/redcine-x-pro.rb
@@ -1,9 +1,10 @@
 cask "redcine-x-pro" do
-  version "52.3,50721"
-  sha256 "c813718f091b7211ea057db1f119bce8d40fac0e0dcfb9e07882880a27e22a11"
+  version "53.0,51001"
+  sha256 "d44cbc953f536bd82a33e1434b2521f3cce8ce22c2c4abb33bf29404e39c0c38"
 
   url "https://downloads.red.com/software/rcx/mac/release/#{version.major}.#{version.after_comma}/REDCINE-X_PRO_Build_#{version.before_comma}.pkg"
   name "REDCINE-X PRO"
+  desc "Transcode and manipulate REDCODE RAW footage"
   homepage "https://www.red.com/"
 
   livecheck do
