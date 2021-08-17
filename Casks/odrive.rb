5b051d678da1737cbd1d49175b80ec35f23c05eb
Bevan Kay <email@bevankay.me>
2021-08-17 20:34:18 +1000

    Update odrive from 7125 to 7131 (#110038)

--- Casks/odrive.rb
+++ Casks/odrive.rb
@@ -1,6 +1,6 @@
 cask "odrive" do
-  version "7125"
-  sha256 "024818b9dccae1b5c05ad89f91e3fb3935b999f7ebb3cfc027928821c733b604"
+  version "7131"
+  sha256 "9c80e6e6c4f734b78c790ab6aa942f9d251b00e86c6c0dfdcdab017f56f7c56a"
 
   url "https://d3huse1s6vwzq6.cloudfront.net/odrivesync.#{version}.pkg",
       verified: "d3huse1s6vwzq6.cloudfront.net/"
