1d8125b6b5d0a0c4ab830dd5bfc605a6ee9dc1e6
Bevan Kay <email@bevankay.me>
2021-08-17 12:23:03 +1000

    Update drivethrurpg from 3.0.1.3 to 3.1.5.0 (#109950)

--- Casks/drivethrurpg.rb
+++ Casks/drivethrurpg.rb
@@ -1,6 +1,6 @@
 cask "drivethrurpg" do
-  version "3.0.1.3"
-  sha256 "d12c39314aae8ef357e3204a1bf013319ebf3085fc47400c613345b5b2cdfc9e"
+  version "3.1.5.0"
+  sha256 "7d24ad2e09d744ad4408ca815f7454905634ad8778013324bd74ba15bc749068"
 
   url "https://dtrpg-library-app.s3.us-east-2.amazonaws.com/DriveThruRPG_v#{version}.dmg",
       verified: "dtrpg-library-app.s3.us-east-2.amazonaws.com/"
