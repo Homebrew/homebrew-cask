d3a1be4f31ab778f7cd081a3422b2e48c2f286f7
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-18 18:50:45 +0800

    Update qtum.rb

--- Casks/qtum.rb
+++ Casks/qtum.rb
@@ -1,8 +1,8 @@
 cask "qtum" do
-  version "0.20.1"
-  sha256 "cf909f0292020fd140011e79f22894639e1bf013ed83021a55b9ba8324ddbc33"
+  version "0.20.3"
+  sha256 "f59f3666902d0840d5687746ca1336f943deda3bca0de4e08b2e611ef21a26f6"
 
-  url "https://github.com/qtumproject/qtum/releases/download/mainnet-ignition-v#{version}/qtum-#{version}-osx-unsigned.dmg",
+  url "https://github.com/qtumproject/qtum/releases/download/mainnet-fastlane-v#{version}/qtum-#{version}-osx-unsigned.dmg",
       verified: "github.com/qtumproject/qtum/"
   name "Qtum"
   desc "Cryptocurrency wallet"
