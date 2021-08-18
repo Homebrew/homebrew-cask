e9353645bff44ab06a2c157df0d64dc1e0680051
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-18 19:15:09 +0800

    Update runjs.rb

--- Casks/runjs.rb
+++ Casks/runjs.rb
@@ -3,7 +3,7 @@ cask "runjs" do
   sha256 "0c6f1bcc1c95ccbc43588837d164faaccdc8fe8743695cc56e1a600c36050b4b"
 
   url "https://github.com/lukehaas/RunJS/releases/download/v#{version}/RunJS-#{version}-universal.dmg",
-      verified: "github.com/lukehaas/runjs/"
+      verified: "github.com/lukehaas/RunJS/"
   name "RunJS"
   desc "JavaScript playground that auto-evaluates as code is typed"
   homepage "https://runjs.app/"
