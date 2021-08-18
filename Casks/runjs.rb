9555c28606e3535252f63d5c06b4d95ec094c241
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-18 19:23:46 +0800

    runjs 2.1.1 (#110140)
    * Update runjs.rb
    
    * Update runjs.rb
    
    * Update runjs.rb

--- Casks/runjs.rb
+++ Casks/runjs.rb
@@ -1,21 +1,17 @@
 cask "runjs" do
-  version "2.0.1"
+  version "2.1.1"
+  sha256 "0c6f1bcc1c95ccbc43588837d164faaccdc8fe8743695cc56e1a600c36050b4b"
 
-  if Hardware::CPU.intel?
-    sha256 "71db49c0d1e7308fa1b391b81e3098e5c168a013c9e1a7caf3054f374604f242"
-
-    url "https://github.com/lukehaas/runjs/releases/download/v#{version}/RunJS-#{version}.dmg",
-        verified: "github.com/lukehaas/runjs/"
-  else
-    sha256 "f1868b503318a0fc0f4095bf67fd19f5da41a8abc47049e5a1f98ee9bc81053f"
-
-    url "https://github.com/lukehaas/runjs/releases/download/v#{version}/RunJS-#{version}-arm64.dmg",
-        verified: "github.com/lukehaas/runjs/"
-  end
-
+  url "https://github.com/lukehaas/RunJS/releases/download/v#{version}/RunJS-#{version}-universal.dmg",
+      verified: "github.com/lukehaas/RunJS/"
   name "RunJS"
   desc "JavaScript playground that auto-evaluates as code is typed"
   homepage "https://runjs.app/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   auto_updates true
   depends_on macos: ">= :yosemite"
