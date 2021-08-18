627c47a83a163d331c73194b08a9c5eedca7c3b3
Jacob Ford <j@cobford.com>
2021-08-17 23:16:10 -0400

    Add the basics, figure out download URL

---
+++ Casks/clay.rb
@@ -0,0 +1,11 @@
+cask "clay" do
+  version "1.4.9"
+  sha256 "ba68525b208df6c7d99fc7e5e5116f0036435ba042340eaa610776fd04ca230e"
+
+  url "https://assets.clay.earth/desktop/mac/Clay-#{version}-arm64.dmg"
+  name "clay"
+  desc "Private rolodex to remember people better"
+  homepage "https://clay.earth"
+
+  app "Clay.app"
+end
