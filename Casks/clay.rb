003e818cb7a519d03ce87639c7febfc25a6ede04
Jacob Ford <j@cobford.com>
2021-08-17 23:57:18 -0400

    Add Intel version

--- Casks/clay.rb
+++ Casks/clay.rb
@@ -1,8 +1,14 @@
 cask "clay" do
   version "1.4.9"
-  sha256 "ba68525b208df6c7d99fc7e5e5116f0036435ba042340eaa610776fd04ca230e"
 
-  url "https://assets.clay.earth/desktop/mac/Clay-#{version}-arm64.dmg"
+  if Hardware::CPU.intel?
+    sha256 "58a41027a551eaf430d4589e032dd972c8734bbda6a1fe33db037c44adf2a0fe"
+    url "https://assets.clay.earth/desktop/mac/Clay-#{version}.dmg"
+  else
+    sha256 "ba68525b208df6c7d99fc7e5e5116f0036435ba042340eaa610776fd04ca230e"
+    url "https://assets.clay.earth/desktop/mac/Clay-#{version}-arm64.dmg"
+  end
+
   name "clay"
   desc "Private rolodex to remember people better"
   homepage "https://clay.earth/"
