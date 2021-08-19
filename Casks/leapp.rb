f45696430764547d5d7f9126b68411472da3b47f
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-19 08:29:02 +0800

    Update leapp.rb

--- Casks/leapp.rb
+++ Casks/leapp.rb
@@ -3,7 +3,7 @@ cask "leapp" do
   sha256 "00c72c4c5e8138e65bbdc08fe75676688275b453335d9ba001a1fe38372ae281"
 
   url "https://asset.noovolari.com/latest/Leapp-#{version}-mac.zip",
-      verified: "asset.noovolari.com"
+      verified: "asset.noovolari.com/"
   name "Leapp"
   desc "Cloud credentials manager"
   homepage "https://www.leapp.cloud/"
