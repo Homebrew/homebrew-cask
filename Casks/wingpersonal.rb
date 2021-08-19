11779c663ea22cdfe0394e96476f908f7c48b053
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-19 08:45:27 +0800

    Update wingpersonal.rb

--- Casks/wingpersonal.rb
+++ Casks/wingpersonal.rb
@@ -1,14 +1,26 @@
 cask "wingpersonal" do
-  version "7.2.7.0"
-  sha256 "772faf1b9238b8e376ac0dc56678c437cf147f0ef3dfb647a095b1ce367c8be4"
+  version "8.0.2.0"
 
-  url "https://wingware.com/pub/wing-personal/#{version}/wing-personal-#{version}.dmg"
-  appcast "https://www.wingware.com/downloads/wing-personal"
+  if Hardware::CPU.intel?
+    sha256 "2a4b953bcc849286bfbe2958dc0ab41d3ed2507e4f79a04d319baae8458181d5"
+
+    url "https://wingware.com/pub/wing-personal/#{version}/wing-personal-#{version}-intel.dmg"
+  else
+    sha256 "1d242c6215354e0b8464005a6ea24855a39d27c92a536a39e10faea3570f955f"
+
+    url "https://wingware.com/pub/wing-personal/#{version}/wing-personal-#{version}-arm64.dmg"
+  end
+
   name "WingPersonal"
   desc "Free Python IDE designed for students and hobbyists"
   homepage "https://www.wingware.com/"
 
-  depends_on macos: ">= :sierra"
+  livecheck do
+    url "https://wingware.com/pub/wing-personal/"
+    regex(%r{href="(\d+(?:\.\d+)*)/"}i)
+  end
+
+  depends_on macos: ">= :high_sierra"
 
   app "WingPersonal.app"
 end
