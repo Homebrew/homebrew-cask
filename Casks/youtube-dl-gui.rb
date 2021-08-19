0d2bb87055dab99f4919a624df99c37b672beed8
Zhenfu Shi <szf1234@me.com>
2021-08-19 11:47:32 +0800

    Update youtube-dl-gui.rb

--- Casks/youtube-dl-gui.rb
+++ Casks/youtube-dl-gui.rb
@@ -3,11 +3,11 @@ cask "youtube-dl-gui" do
   sha256 "2317e4dfb3f2930de3afba2d5794521e8887150b9a4928ec1829654ef22ad9e7"
 
   url "https://github.com/jely2002/youtube-dl-gui/releases/download/v#{version}/Open-Video-Downloader-#{version}.dmg"
-  name "YouTube Downloader GUI"
+  name "Open Video Downloader"
   desc "Cross-platform GUI for youtube-dl made in Electron and node.js"
   homepage "https://github.com/jely2002/youtube-dl-gui"
 
-  app "YouTube Downloader GUI.app"
+  app "Open Video Downloader.app"
 
   zap trash: [
     "~/Library/Preferences/com.jelleglebbeek.youtube-dl-gui.plist",
