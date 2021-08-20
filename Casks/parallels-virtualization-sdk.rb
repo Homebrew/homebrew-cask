4c5b8e780ac5ec1ab5eeee325c84daf0ff494878
Josh Tilles <MerelyAPseudonym@gmail.com>
2021-08-20 19:46:09 -0400

    Update parallels-virtualization-sdk from 16.5.0-50692 to 17.0.0-51461

--- Casks/parallels-virtualization-sdk.rb
+++ Casks/parallels-virtualization-sdk.rb
@@ -1,22 +1,11 @@
 cask "parallels-virtualization-sdk" do
-  if Hardware::CPU.intel?
-    version "16.5.0-49183"
-    sha256 "5d4bcf6d6f0e341c5f33380f8601170c9f41b05a0f720050622bfc979e017e79"
+  version "17.0.0-51461"
+  sha256 "be916765c32aa20a2bece6903c1bfa56b8ffbb0bc1333bcbb239faeec0a79237"
 
-    livecheck do
-      url "https://www.parallels.com/download/pvsdk/intel/"
-      regex(%r{/parallelsvirtualizationsdk-?(\d+(?:\.\d+)+-\d+)(?:-mac)?\.dmg}i)
-      strategy :header_match
-    end
-  else
-    version "16.5.0-50692"
-    sha256 "3a796228f1ea27f6261a56af89342e1e3292194628e0dd4042d296eb83f0f18e"
-
-    livecheck do
-      url "https://www.parallels.com/download/pvsdk/m1/"
-      regex(%r{/parallelsvirtualizationsdk-?(\d+(?:\.\d+)+-\d+)(?:-mac)?\.dmg}i)
-      strategy :header_match
-    end
+  livecheck do
+    url "https://www.parallels.com/download/pvsdk/"
+    regex(%r{/parallelsvirtualizationsdk-?(\d+(?:\.\d+)+-\d+)(?:-mac)?\.dmg}i)
+    strategy :header_match
   end
 
   url "https://download.parallels.com/desktop/v#{version.major}/#{version}/ParallelsVirtualizationSDK-#{version}-mac.dmg"
