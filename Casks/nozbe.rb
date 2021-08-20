4cfbaf1a01a65026471dbb714e017dce688ab90e
mlewak <85615562+mlewak@users.noreply.github.com>
2021-08-20 15:04:17 +0200

    update nozbe to 3.20.1 and add livecheck

--- Casks/nozbe.rb
+++ Casks/nozbe.rb
@@ -1,11 +1,21 @@
 cask "nozbe" do
-  version "3.13.0"
-  sha256 "384a15bd9b34935b7f0f152ebdd5c8653e0aed86e01c4e7844acfe34769cc4c9"
+  version "3.20.1"
+  sha256 "1cf4461e172b66b486c24e1dbc6177559e921bf10e100b74262a165e44cee876"
 
   url "https://files.nozbe.com/#{version.no_dots}/release/Nozbe.app.zip"
   name "Nozbe"
   desc "Task manager"
   homepage "https://nozbe.com/"
+
+  livecheck do
+    url "https://nozbe.com/mac"
+    strategy :header_match do |headers|
+      # example: location: https://files2.nozbe.com/3201/release/Nozbe.app.zip
+      match = headers["location"].match(%r{/v?(\d)(\d+)(\d+)/release/Nozbe}i)
+      # not perfect, will fail when          it'll have to distinguish between 3.20.10 and 3.201.0
+      "#{match[1]}\.#{match[2]}\.#{match[3]}"
+    end
+  end
 
   depends_on macos: ">= :sierra"
 
