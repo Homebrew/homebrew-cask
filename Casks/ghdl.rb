01ace5d64409dc4bb2744c74d489c5301eb3aef1
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-21 13:24:18 +0800

    Update ghdl.rb

--- Casks/ghdl.rb
+++ Casks/ghdl.rb
@@ -1,5 +1,5 @@
 cask "ghdl" do
-  version "1.0.0"
+  version :latest
   sha256 :no_check
 
   url "https://github.com/ghdl/ghdl/releases/download/nightly/ghdl-macos-10.15-mcode.tgz"
@@ -8,8 +8,7 @@ cask "ghdl" do
   homepage "https://github.com/ghdl/ghdl/"
 
   livecheck do
-    url :url
-    strategy :github_latest
+    skip "unversioned URL"
   end
 
   binary "bin/ghdl"
