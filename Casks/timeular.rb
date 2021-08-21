af8886655ca00714fd84497b4ffa9db09976a890
Bevan Kay <email@bevankay.me>
2021-08-21 15:19:35 +1000

    Update timeular from 3.9.3 to 3.9.6

--- Casks/timeular.rb
+++ Casks/timeular.rb
@@ -1,6 +1,6 @@
 cask "timeular" do
-  version "3.9.3"
-  sha256 "f4dd8333fe1fb466e7ba5bbd4c28e3eb6cf86f501411966eab79fc93adab3ba0"
+  version "3.9.6"
+  sha256 "1e34843001fd7df5fae9f043b6f849ccff5dfc8edff76da4bbf6b45403a40392"
 
   url "https://timeular-desktop-packages.s3.amazonaws.com/mac/production/Timeular-#{version}.dmg",
       verified: "timeular-desktop-packages.s3.amazonaws.com/"
