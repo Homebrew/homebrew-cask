660e3140374519a2f3e9f4e36029beea45dfddd1
miccal <miccal.matthews@gmail.com>
2021-08-21 15:02:42 +0800

    Update lbry from 0.51.1 to 0.51.2

--- Casks/lbry.rb
+++ Casks/lbry.rb
@@ -1,6 +1,6 @@
 cask "lbry" do
-  version "0.51.1"
-  sha256 "8e910d31fff8af3fc626cafc46363927c0e65cc1c85c9f09ea2f618dbe52a68e"
+  version "0.51.2"
+  sha256 "f497927baa8f8b1c1a1c8b0bcf5eabbc89b2bb5ed9a7bc448612df69d7292ba6"
 
   url "https://github.com/lbryio/lbry-desktop/releases/download/v#{version}/LBRY_#{version}.dmg",
       verified: "github.com/lbryio/lbry-desktop/"
