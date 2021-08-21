5944784d3160026bba374a7397510c31fc6e3a04
Christos Zisopoulos <christos@mac.com>
2021-08-22 00:10:41 +0200

    Update camo-studio from 1.3.6,4830 to 1.3.8,5146

--- Casks/camo-studio.rb
+++ Casks/camo-studio.rb
@@ -1,6 +1,6 @@
 cask "camo-studio" do
-  version "1.3.6,4830"
-  sha256 "3ab89aaa0d9fc2785a394d0d6a2d4ebdafddabf2cf9f67380c9417cf6991fa6e"
+  version "1.3.8,5146"
+  sha256 "46cde65ddd2307cb61403180448669fb9cc86779e06f171173ca8174a0055d80"
 
   url "https://reincubate.com/res/labs/camo/CamoStudioMac-#{version.before_comma}-#{version.after_comma}-Release.app.zip"
   name "Camo Studio"
