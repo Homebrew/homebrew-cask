9e02ce28d9b438b113afb3f66502e7037b29bda2
Bevan Kay <email@bevankay.me>
2021-08-21 21:37:19 +1000

    cozy-drive: bump version, update url (#110368)

--- Casks/cozy-drive.rb
+++ Casks/cozy-drive.rb
@@ -1,9 +1,9 @@
 cask "cozy-drive" do
-  version "3.28.1"
-  sha256 "0da0f3535c8b71c383b0c66dbe78257d7e581fa457e22dc212d6ff5945c01214"
+  version "3.29.0"
+  sha256 "7fc2e1af51a329e387bbdaf1ec5f275fbc6ee40c987e47b8a5f478d1bf3e9956"
 
-  url "https://nuts.cozycloud.cc/download/channel/stable/CozyDrive-#{version}.dmg",
-      verified: "nuts.cozycloud.cc/"
+  url "https://github.com/cozy-labs/cozy-desktop/releases/download/v#{version}/Cozy-Drive-#{version}.dmg",
+      verified: "github.com/cozy-labs/cozy-desktop"
   name "Cozy Drive"
   desc "File synchronization for Cozy Cloud"
   homepage "https://cozy.io/"
