fdaa3384cd1fe42aeaa9dd793b965ed930389525
Bevan Kay <email@bevankay.me>
2021-08-21 23:24:23 +1000

    Update ishowu-instant from 1.4.7,1375 to 1.4.8,1379 (#110399)

--- Casks/ishowu-instant.rb
+++ Casks/ishowu-instant.rb
@@ -1,6 +1,6 @@
 cask "ishowu-instant" do
-  version "1.4.7,1375"
-  sha256 "2b5303d61e8c0806f574e16aca50678e7521fc7d383755fa581ea8f5256e8dda"
+  version "1.4.8,1379"
+  sha256 "653a5d0a16715f86bbe74e29d37b6a77d3cd0f5d2945eb6d94cb20349bb00128"
 
   url "https://www.shinywhitebox.com/downloads/instant/iShowU_Instant_#{version.before_comma}.dmg"
   name "iShowU Instant"
