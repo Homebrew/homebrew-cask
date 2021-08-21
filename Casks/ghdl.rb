c9f366764acbe2e79a8924c6c44a544d322cf2fe
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-21 14:22:11 +0800

    ghdl :latest (#110218)
    * Update ghdl.rb
    
    * Update ghdl.rb
    
    * Update ghdl.rb

--- Casks/ghdl.rb
+++ Casks/ghdl.rb
@@ -1,15 +1,11 @@
 cask "ghdl" do
-  version "0.37"
-  sha256 "cb870085dd55167eda162b2d8b0020b30574b47fa8f8dfc7a1fd6aa3eb32ee91"
+  version :latest
+  sha256 :no_check
 
-  url "https://github.com/ghdl/ghdl/releases/download/v#{version}/ghdl-#{version}-macosx-mcode.tgz"
+  url "https://github.com/ghdl/ghdl/releases/download/nightly/ghdl-macos-10.15-mcode.tgz"
   name "ghdl"
+  desc "VHDL 2008/93/87 simulator"
   homepage "https://github.com/ghdl/ghdl/"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
 
   binary "bin/ghdl"
 end
