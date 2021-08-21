bd8cb32f43c79eec9c204286087bf6916d993db0
Bevan Kay <email@bevankay.me>
2021-08-21 12:56:54 +1000

    fix style

--- Casks/parallels-virtualization-sdk.rb
+++ Casks/parallels-virtualization-sdk.rb
@@ -2,16 +2,16 @@ cask "parallels-virtualization-sdk" do
   version "17.0.0-51461"
   sha256 "be916765c32aa20a2bece6903c1bfa56b8ffbb0bc1333bcbb239faeec0a79237"
 
+  url "https://download.parallels.com/desktop/v#{version.major}/#{version}/ParallelsVirtualizationSDK-#{version}-mac.dmg"
+  name "Parallels Virtualization SDK"
+  desc "Desktop virtualization development kit"
+  homepage "https://www.parallels.com/products/desktop/download/"
+
   livecheck do
     url "https://www.parallels.com/download/pvsdk/"
     regex(%r{/parallelsvirtualizationsdk-?(\d+(?:\.\d+)+-\d+)(?:-mac)?\.dmg}i)
     strategy :header_match
   end
-
-  url "https://download.parallels.com/desktop/v#{version.major}/#{version}/ParallelsVirtualizationSDK-#{version}-mac.dmg"
-  name "Parallels Virtualization SDK"
-  desc "Desktop virtualization development kit"
-  homepage "https://www.parallels.com/products/desktop/download/"
 
   pkg "Parallels Virtualization SDK.pkg"
 
