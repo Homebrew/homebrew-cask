2381490ce957a88d7876a4e18ebdfbe383485914
Bevan Kay <email@bevankay.me>
2021-08-22 15:00:26 +1000

    Update loom from 0.97.0 to 0.98.0 (#110414)

--- Casks/loom.rb
+++ Casks/loom.rb
@@ -1,6 +1,6 @@
 cask "loom" do
-  version "0.97.0"
-  sha256 "a06f33c13efec0233e0147fa5daea728a16726ee40cdfa732d46d4ff477e468e"
+  version "0.98.0"
+  sha256 "e6ba7eefb59b8a595c26bf30a4bb3ca204970e95c2cfc3d8703edf8c4297fae6"
 
   url "https://cdn.loom.com/desktop-packages/Loom-#{version}.dmg"
   name "Loom"
