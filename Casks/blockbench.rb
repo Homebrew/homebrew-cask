16811f4c697f62476b9986564ee1792979918bb5
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-22 22:38:08 +0800

    Update blockbench.rb

--- Casks/blockbench.rb
+++ Casks/blockbench.rb
@@ -5,16 +5,16 @@ cask "blockbench" do
     sha256 "e8c0b70e7cf7659186580a6de7dc8aee00167a37f211a6e59cbf693b87178129"
 
     url "https://github.com/JannisX11/blockbench/releases/download/v#{version}/Blockbench_x64_#{version}.dmg",
-        verified: "github.com/JannisX11/blockbench"
+        verified: "github.com/JannisX11/blockbench/"
   else
     sha256 "d6bc651b1d4f1f184a8dfc8856ce92a51217f177b206edee9e33113c5ed4a047"
 
     url "https://github.com/JannisX11/blockbench/releases/download/v#{version}/Blockbench_arm64_#{version}.dmg",
-        verified: "github.com/JannisX11/blockbench"
+        verified: "github.com/JannisX11/blockbench/"
   end
 
   name "Blockbench"
-  desc "Free, modern 3D model editor for boxy models and pixel art textures"
+  desc "3D model editor for boxy models and pixel art textures"
   homepage "https://www.blockbench.net/"
 
   livecheck do
