9bab742859441bfd46aaaa002f250b2cf4c102cf
Bevan Kay <email@bevankay.me>
2021-08-22 15:11:36 +1000

    Update melodics from 2.1.6397,B594381D-4D17-4ECF-A343-BA44C8C3D29E to 2.1.6404,96A38A3A-5D8C-463B-8C1F-C1F03B6407E2 (#110416)

--- Casks/melodics.rb
+++ Casks/melodics.rb
@@ -1,6 +1,6 @@
 cask "melodics" do
-  version "2.1.6397,B594381D-4D17-4ECF-A343-BA44C8C3D29E"
-  sha256 "bcd27581c94b01f0155fa2c7379a7d6ea769ac6889f501c76b594c6d5460b584"
+  version "2.1.6404,96A38A3A-5D8C-463B-8C1F-C1F03B6407E2"
+  sha256 "0bd96a2787e48a7af4c9f51557cb40adc375b6968c0e88d7bf331604f84e9dec"
 
   url "https://web-cdn.melodics.com/download/#{version.after_comma}.zip"
   name "Melodics"
