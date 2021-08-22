900d6e0a1e14bb66b2753c4bc67f82c0b5d51eaf
Balu <tw@b-a-l-u.de>
2021-08-22 17:51:24 +0200

    Fixing regex

--- Casks/fsnotes.rb
+++ Casks/fsnotes.rb
@@ -11,7 +11,7 @@ cask "fsnotes" do
   livecheck do
     url :url
     strategy :github_latest
-    regex(/href=.*?\/tag\/v?(\d+)["' >]/i)
+    regex(%r{href=.*?/tag/v?(\d+)["' >]}i)
   end
 
   app "FSNotes.app"
