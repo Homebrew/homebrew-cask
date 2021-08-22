c50552083d1c01779d533227d87353c42c58265a
Bevan Kay <email@bevankay.me>
2021-08-22 17:27:56 +1000

    Update solar2d from 2021.3651 to 2021.3652

--- Casks/solar2d.rb
+++ Casks/solar2d.rb
@@ -1,6 +1,6 @@
 cask "solar2d" do
-  version "2021.3651"
-  sha256 "375ef128a268c2fde976e2879313a98956e370488fcdf777c7728478714b13e7"
+  version "2021.3652"
+  sha256 "c0f5331754aac8685d7990e34059479ee44ccf3bae0a59ac36b17a0f7735990a"
 
   url "https://github.com/coronalabs/corona/releases/download/#{version.minor}/Solar2D-macOS-#{version}.dmg",
       verified: "github.com/coronalabs/corona/"
