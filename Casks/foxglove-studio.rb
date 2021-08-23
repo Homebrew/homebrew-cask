d05f3db9eada084c5b9a584d40db408f5975df2b
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-23 18:24:05 +0800

    Revert "Delete foxglove-studio.rb (#110525)" (#110550)
    This reverts commit 09eed3cabe14a888415f28f27b350fa0a52b5285.

---
+++ Casks/foxglove-studio.rb
@@ -0,0 +1,27 @@
+cask "foxglove-studio" do
+  version "0.15.0"
+  sha256 "006668243c851d1dd4676fbb53f12580a4edbaafb9299e05a860150d4e6a509f"
+
+  url "https://github.com/foxglove/studio/releases/download/v#{version}/foxglove-studio-#{version}-mac-universal.dmg",
+      verified: "github.com/foxglove/studio/"
+  name "Foxglove Studio"
+  desc "Visualization and debugging tool for robotics"
+  homepage "https://foxglove.dev/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "Foxglove Studio.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/dev.foxglove.studio.quicklook",
+    "~/Library/Application Support/Foxglove Studio",
+    "~/Library/Caches/dev.foxglove.studio",
+    "~/Library/Caches/dev.foxglove.studio.ShipIt",
+    "~/Library/Containers/dev.foxglove.studio.quicklook",
+    "~/Library/Preferences/dev.foxglove.studio.plist",
+    "~/Library/Saved Application State/dev.foxglove.studio.savedState",
+  ]
+end
