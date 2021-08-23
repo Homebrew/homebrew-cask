022f0d9cd2a37b44e38cf8bbe82971fb1b396ddd
Johannes Klein <jo@temka.de>
2021-08-24 01:59:11 +0200

    Update sky-ticket.rb (#110563)
    * Update sky-ticket.rb
    
    version url and add proper livecheck
    
    * add sha256

--- Casks/sky-ticket.rb
+++ Casks/sky-ticket.rb
@@ -1,15 +1,18 @@
 cask "sky-ticket" do
-  version "8.3.0"
-  sha256 :no_check
+  version "8.6.0"
+  sha256 "fbe2853557895244f4b3f2fd09d2ef9cf960e92f7b26b36c9521036f927cc422"
 
-  url "https://web.static.nowtv.com/watch/player/skyticket/de/latest/SkyTicket-Mac.dmg",
+  url "https://web.static.nowtv.com/watch/player/skyticket/de/#{version}/SkyTicket-Mac.dmg",
       verified: "web.static.nowtv.com/watch/player/skyticket/"
   name "Sky Ticket Player"
   desc "Stream Sky Ticket content"
   homepage "https://skyticket.sky.de/"
 
   livecheck do
-    skip "unversioned URL"
+    url "https://web.static.nowtv.com/watch/player/skyticket/de/latest/update.json"
+    strategy :page_match do |page|
+      JSON.parse(page)["platforms"]["darwin"]["version"]
+    end
   end
 
   app "Sky Ticket.app"
