ab25af8b086d3361418dd34289aedcb68b74d1f6
Jacob Ford <j@cobford.com>
2021-08-22 20:48:30 -0400

    Add mimestream (#110511)
    * Add mimestream
    
    (supercedes https://github.com/Homebrew/homebrew-cask-versions/pull/11734\)
    
    * Update mimestream.rb
    
    * Update mimestream.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/mimestream.rb
@@ -0,0 +1,22 @@
+cask "mimestream" do
+  version "0.26.2"
+  sha256 "0644cbb3d97b8b9a00bc31013a559a607e52a102e1d3e8a72b7f2dfe76583cd3"
+
+  url "https://storage.googleapis.com/mimestream-releases/Mimestream_#{version}.dmg",
+      verified: "storage.googleapis.com/mimestream-releases/"
+  name "Mimestream"
+  desc "Native app email client for Gmail"
+  homepage "https://mimestream.com/"
+
+  livecheck do
+    url "https://storage.googleapis.com/mimestream-releases/"
+    regex(/Mimestream[._-]?(\d+(?:\.\d+)*)\.dmg/i)
+  end
+
+  app "Mimestream.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.mimestream.Mimestream",
+    "~/Library/Containers/com.mimestream.Mimestream",
+  ]
+end
