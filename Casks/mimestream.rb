a3b8857d62c5231173e6c3d2e8858c4e8eb19262
Jacob Ford <j@cobford.com>
2021-08-22 21:24:05 -0400

    Switch Mimestream back to sparkle livecheck strategy (#110529)
    * Add mimestream
    
    (supercedes https://github.com/Homebrew/homebrew-cask-versions/pull/11734\)
    
    * Update mimestream.rb
    
    * Update mimestream.rb
    
    * Switch livecheck to Sparkle strategy
    
    * style --fix has showed be an even better way
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/mimestream.rb
+++ Casks/mimestream.rb
@@ -9,8 +9,8 @@ cask "mimestream" do
   homepage "https://mimestream.com/"
 
   livecheck do
-    url "https://storage.googleapis.com/mimestream-releases/"
-    regex(/Mimestream[._-]?(\d+(?:\.\d+)*)\.dmg/i)
+    url "https://mimestream.com/appcast.xml"
+    strategy :sparkle, &:short_version
   end
 
   app "Mimestream.app"
