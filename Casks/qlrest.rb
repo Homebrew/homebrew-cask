89a2299c1a61f4e6e84d2c1c26fe132e33b026ff
Bevan Kay <email@bevankay.me>
2021-08-25 20:42:28 +1000

    qlrest: add version, add livecheck

--- Casks/qlrest.rb
+++ Casks/qlrest.rb
@@ -1,10 +1,15 @@
 cask "qlrest" do
-  version :latest
+  version "2.0.0"
   sha256 :no_check
 
   url "https://github.com/downloads/cluther/qlrest/QLRest.qlgenerator.zip"
   name "QLRest"
   homepage "https://github.com/cluther/qlrest"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   qlplugin "QLRest.qlgenerator"
 end
