7753cfafb5ba8bc75460c268ba0ea17466af1de3
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-25 11:02:18 +0800

    Update gpxsee from 9.4 to 9.5 (#110597)
    * Update gpxsee from 9.4 to 9.5
    
    * Update gpxsee.rb

--- Casks/gpxsee.rb
+++ Casks/gpxsee.rb
@@ -1,11 +1,11 @@
 cask "gpxsee" do
-  version "9.4"
-  sha256 "af9ef4efaeb33022bcf2963e4e34a66780bf5e9420d8e2269c356042483fd64d"
+  version "9.5"
+  sha256 "ef0e6c3ce1c6ac04054ebfb6279b71960590d8cd9ecb2ffeb60e322d9cba2284"
 
   url "https://downloads.sourceforge.net/gpxsee/Mac%20OS%20X/GPXSee-#{version}.dmg",
       verified: "sourceforge.net/gpxsee/Mac%20OS%20X/"
   name "GPXSee"
-  desc "GPS log file viewer and analyze"
+  desc "GPS log file viewer and analyzer"
   homepage "https://www.gpxsee.org/"
 
   app "GPXSee.app"
