c9e593657ae07e6eb2182f6f94e433f4a8513151
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-25 17:05:41 +0800

    Update epoccam.rb

--- Casks/epoccam.rb
+++ Casks/epoccam.rb
@@ -2,12 +2,12 @@ cask "epoccam" do
   version "3.3"
   sha256 "a676c29ea85cac2d7568de554f946bfa7523a8acd49f488034c1a0a1a50d8d39"
 
-  url "https://edge.elgato.com/egc/macos/epoccam/EpocCam_Installer_#{version.major}_#{version.minor}.pkg"
+  url "https://edge.elgato.com/egc/macos/epoccam/EpocCam_Installer_#{version.dots_to_underscores}.pkg"
   name "EpocCam"
   desc "Turn your phone into a webcam"
   homepage "https://www.elgato.com/epoccam"
 
-  pkg "EpocCam_Installer_#{version.major}_#{version.minor}.pkg"
+  pkg "EpocCam_Installer_#{version.dots_to_underscores}.pkg"
 
   uninstall_preflight do
     set_ownership "/Library/Audio/Plug-Ins/HAL/VirtualMic.driver"
