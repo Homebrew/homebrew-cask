bb882361c84240124fbe853c284f2f4c3d064045
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-25 12:08:15 +0800

    Update trilium-notes from 0.47.6 to 0.47.7 (#110607)

--- Casks/trilium-notes.rb
+++ Casks/trilium-notes.rb
@@ -1,6 +1,6 @@
 cask "trilium-notes" do
-  version "0.47.6"
-  sha256 "1cb4c46db1c8f05c484beef9ccf9bcc3972b32d10639c9cb225ba0e3a0046053"
+  version "0.47.7"
+  sha256 "3b847b03d2d090643fd262003fde8796df60d14b30f1e02980de80b553f1db1b"
 
   url "https://github.com/zadam/trilium/releases/download/v#{version}/trilium-mac-x64-#{version}.zip"
   name "Trilium Notes"
