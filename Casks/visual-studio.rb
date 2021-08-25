9979af6a3970b97543bf6fd102b85aad14aad8a1
tuzi3040 <28290621+tuzi3040@users.noreply.github.com>
2021-08-26 06:20:45 +0800

    Update visual-studio from 8.10.7.17 to 8.10.8.0 (#110629)

--- Casks/visual-studio.rb
+++ Casks/visual-studio.rb
@@ -1,6 +1,6 @@
 cask "visual-studio" do
-  version "8.10.7.17"
-  sha256 "9b92ba7de0f141ec6713798b645ae4705a0e12a0b260defd0b4a14f7d4c41c15"
+  version "8.10.8.0"
+  sha256 "7c476282537565ca8600200ffa83d3caa94f0301ce78e3b6c3172e60994be3bb"
 
   url "https://dl.xamarin.com/VsMac/VisualStudioForMac-#{version}.dmg",
       verified: "dl.xamarin.com/VsMac/"
