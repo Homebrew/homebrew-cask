976c97500a807a92ef14a83c2d015e6fea30e163
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-26 06:23:00 +0800

    Update clover.rb

--- Casks/clover.rb
+++ Casks/clover.rb
@@ -3,9 +3,9 @@ cask "clover" do
   sha256 "1f46b0d69d90627e07cc15e481a3ce5f68ede92eecb971b250c66a2674f346ac"
 
   url "https://clover-desktop-app.s3.amazonaws.com/Clover-#{version}.dmg",
-      verified: "clover-desktop-app.s3.amazonaws.com"
+      verified: "clover-desktop-app.s3.amazonaws.com/"
   name "clover"
-  desc "Notes, whiteboarding, todos, and a daily planner in one simple tool"
+  desc "Notes, whiteboarding, todos, and a daily planner in one tool"
   homepage "https://cloverapp.com/"
 
   livecheck do
