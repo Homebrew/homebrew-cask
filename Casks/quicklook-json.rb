03d6ab12c67e2b9e9cb41d101f0ecffd25e1fe2a
Bevan Kay <email@bevankay.me>
2021-08-25 20:17:49 +1000

    quicklook-json: add version, add livecheck

--- Casks/quicklook-json.rb
+++ Casks/quicklook-json.rb
@@ -1,10 +1,15 @@
 cask "quicklook-json" do
-  version :latest
+  version "2,1.0"
   sha256 :no_check
 
   url "http://www.sagtau.com/media/QuickLookJSON.qlgenerator.zip"
   name "quick look JSON"
   homepage "http://www.sagtau.com/quicklookjson.html"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   qlplugin "QuickLookJSON.qlgenerator"
 end
