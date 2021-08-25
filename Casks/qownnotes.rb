3c0926b6e8b07cf932ed699986677b41396bacff
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-25 12:08:38 +0800

    Update qownnotes from 21.8.8 to 21.8.11 (#110600)

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,6 +1,6 @@
 cask "qownnotes" do
-  version "21.8.8"
-  sha256 "dd90fd3b5c824be52464048c46a5942929ba61d3b69a4f94d5d5448e12013c0d"
+  version "21.8.11"
+  sha256 "02c359321ed529a9b494fbd316438200da8b52de7b027aee140a9ed3e27285b3"
 
   url "https://github.com/pbek/QOwnNotes/releases/download/v#{version}/QOwnNotes.dmg",
       verified: "github.com/pbek/QOwnNotes/"
