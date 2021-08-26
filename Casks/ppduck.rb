70e55ef363395b0c9481fe0458520da49bc2bc58
shensven <shensven2016@gmail.com>
2021-08-26 11:58:33 +0800

    ppduck: binary URL update

--- Casks/ppduck.rb
+++ Casks/ppduck.rb
@@ -2,7 +2,7 @@ cask "ppduck" do
   version "3.10.11"
   sha256 "294335e27a3af4ce65392fa350cd4042ca040b84d2e7de6814bdb1cb2fc647d7"
 
-  url "http://download.ppduck.com/PPDuck#{version.major}_#{version}.dmg"
+  url "https://download.ppduck.com/PPDuck#{version.major}_#{version}.dmg"
   name "PPDuck"
   homepage "https://www.ppduck.com/"
 
