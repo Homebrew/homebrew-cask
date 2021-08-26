7a700ff22f2058166c9b764dfa433b265c2b522a
shensven <shensven2016@gmail.com>
2021-08-26 12:16:07 +0800

    ppduck: add description

--- Casks/ppduck.rb
+++ Casks/ppduck.rb
@@ -4,6 +4,7 @@ cask "ppduck" do
 
   url "https://download.ppduck.com/PPDuck#{version.major}_#{version}.dmg"
   name "PPDuck"
+  desc "PP鸭整合了业内最优秀的数种开源的图片压缩算法，会自动根据图片特征自动选择压缩参数。只需要将图片拖入PP鸭，就能自动批量压缩，省时省心。"
   homepage "https://www.ppduck.com/"
 
   app "PPDuck#{version.major}.app"
