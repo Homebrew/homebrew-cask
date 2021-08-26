884f398370f67eaef534808edb03bf4d3c04a90b
jackkke <32036121+jackkke@users.noreply.github.com>
2021-08-26 17:57:59 +0800

    Update electerm.rb (#110651)

--- Casks/electerm.rb
+++ Casks/electerm.rb
@@ -1,6 +1,6 @@
 cask "electerm" do
-  version "1.16.3"
-  sha256 "492023ad38dad77c0d29c55bb053a20912a92355224ebda5ef2d288de12015ed"
+  version "1.16.5"
+  sha256 "0bb6d4ba1a1513b598a5899cd21faac0d7f28a025299175a9467be76a996db0b"
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac-x64.dmg"
   name "electerm"
