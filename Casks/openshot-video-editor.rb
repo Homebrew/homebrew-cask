7bff67cde268a139e1ebe1e9e5dfaa9f9f7e026e
pebosi <mail@pebosi.net>
2021-08-27 10:14:04 +0200

    Update to 2.6.0

--- Casks/openshot-video-editor.rb
+++ Casks/openshot-video-editor.rb
@@ -1,6 +1,6 @@
 cask "openshot-video-editor" do
-  version "2.5.1"
-  sha256 "cc17586bb7241545659220994c765ae89eb584f6be007069cba519cd4c1793a4"
+  version "2.6.0"
+  sha256 "bbf3dd98fc2445135c9ffb15bb4941c98bf8962e4c1ca1fde6383f6c7c9c72eb"
 
   url "https://github.com/OpenShot/openshot-qt/releases/download/v#{version}/OpenShot-v#{version}-x86_64.dmg",
       verified: "github.com/OpenShot/openshot-qt/"
