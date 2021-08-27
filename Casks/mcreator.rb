d86640bc6b5818ebe877d7f6eb922c6cc70c0dda
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-27 13:54:41 +0800

    mcreator 2021.2.35315 (#110692)
    * Update mcreator.rb
    
    * Update mcreator.rb
    
    * Update mcreator.rb

--- Casks/mcreator.rb
+++ Casks/mcreator.rb
@@ -1,16 +1,19 @@
 cask "mcreator" do
-  version "2021.1"
-  sha256 "05827fd0e2d52303a618ffaa254ee73af3e7c4c493e66dcec66b7659315f4ea5"
+  version "2021.2.35315"
+  sha256 "6e86636c958d42fd45879d2faffcdea47a39ffceb4f8f1a1f9c4e9ebe3326e6d"
 
-  url "https://mcreator.net/repository/#{version.dots_to_hyphens}/MCreator%20#{version}%20Mac%2064bit.dmg"
+  url "https://github.com/MCreator/MCreator/releases/download/#{version}/MCreator.#{version.major_minor}.Mac.64bit.dmg",
+      verified: "github.com/MCreator/MCreator/"
   name "MCreator"
   desc "Software used to make Minecraft Java Edition mods"
   homepage "https://mcreator.net/"
 
   livecheck do
-    url "https://mcreator.net/download"
-    strategy :page_match
-    regex(/MCreator.*?(\d+(?:\.\d+)*)/i)
+    url "https://github.com/MCreator/MCreator/releases/"
+    regex(%r{(\d+(?:\.\d+)*)/MCreator\.(\d+(?:\.\d+)*)\.Mac\.64bit\.dmg}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| match[0].to_s }
+    end
   end
 
   app "MCreator.app"
