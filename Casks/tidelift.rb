d5ee45693c8ecb3417023cd500a8ab88b15a69e1
Tyrel Souza <tyrelsouza@gmail.com>
2021-08-27 13:56:35 -0400

    Add tidelift CLI v1.0.0
    Initial addition of Tidelift CLI version 1.0.0

---
+++ Casks/tidelift.rb
@@ -0,0 +1,20 @@
+cask "tidelift" do
+  version "1.0.0"
+  sha256 "6c369dc4577b0d345ea4e79204af7468b39392959612c49c69e4c1ed0f012896"
+
+  url "https://download.tidelift.com/cli/#{version}/darwin/tidelift"
+  name "Tidelift CLI"
+  desc "Tool to interact with the Tidelift system"
+  homepage "https://tidelift.com/cli"
+
+  livecheck do
+    url "https://download.tidelift.com/cli/index.html"
+    regex(%r{^.*/cli/([^/]+)/darwin/tidelift">}i)
+  end
+
+  binary "tidelift", target: "tidelift"
+
+  postflight do
+    set_permissions "#{staged_path}/tidelift", "+x"
+  end
+end
