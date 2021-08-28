eb432549179ed21ce19c31942e5c534d379a16c1
Geoff Hutchison <geoff.hutchison@gmail.com>
2021-08-27 22:57:17 -0400

    Update avogadro from 1.95.0 to 1.95.1 (#110708)
    * Update avogadro from 1.95.0 to 1.95.1
    
    * Added zap stanza

--- Casks/avogadro.rb
+++ Casks/avogadro.rb
@@ -1,6 +1,6 @@
 cask "avogadro" do
-  version "1.95.0"
-  sha256 "a8f67fdce30b7984cce850e6046ef7507104bd2a3a197b0ab645d9a4be597ccf"
+  version "1.95.1"
+  sha256 "2fc23987f0955f19f3d58380e5e42bb58ee00a9c383953a2fb6caa9f95922f39"
 
   url "https://github.com/OpenChemistry/avogadrolibs/releases/download/#{version}/Avogadro2-#{version}-Darwin.dmg",
       verified: "github.com/OpenChemistry/avogadrolibs/"
@@ -9,4 +9,10 @@ cask "avogadro" do
   homepage "https://avogadro.cc/"
 
   app "Avogadro2.app"
+
+  zap trash: [
+    "~/Library/Preferences/org.openchemistry.Avogadro.plist",
+    "~/Library/Saved Application State/cc.avogadro.savedState",
+    "~/Library/Application Support/OpenChemistry/Avogadro",
+  ]
 end
