ae3da8f2602352189e83b644a3609516559f111a
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-08-29 00:31:37 +0900

    Update rotki from 1.20.0 to 1.20.1 (#110732)

--- Casks/rotki.rb
+++ Casks/rotki.rb
@@ -1,6 +1,6 @@
 cask "rotki" do
-  version "1.20.0"
-  sha256 "ed60d90a654174004c953a515056ca5b8d02e1b39ddedc82272a868389531da4"
+  version "1.20.1"
+  sha256 "d2ff80ac83a72886c70fa314dd7540c70fd053fc6148bf79f34286c2f8466b5e"
 
   url "https://github.com/rotki/rotki/releases/download/v#{version}/rotki-darwin_x64-v#{version}.dmg",
       verified: "github.com/rotki/rotki/"
