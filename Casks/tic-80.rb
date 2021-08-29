6cd94a87ab248e6e987364fd58714816c0c66bc0
FnControlOption <70830482+FnControlOption@users.noreply.github.com>
2021-08-29 08:16:29 -0700

    Add TIC-80 0.90.1723

---
+++ Casks/tic-80.rb
@@ -0,0 +1,19 @@
+cask "tic-80" do
+  version "0.90.1723"
+  sha256 "c72b1c51a97c44ddabd2b2d14597275c42e0aedef383de71a48495d8a22ae02e"
+
+  url "https://github.com/nesbox/TIC-80/releases/download/v#{version}/tic80-v#{version.major_minor}-mac.dmg",
+      verified: "github.com/nesbox/TIC-80/"
+  name "TIC-80"
+  desc "Fantasy computer for making, playing and sharing tiny games"
+  homepage "https://tic80.com/"
+
+  app "tic80.app"
+
+  uninstall quit: "com.nesbox.tic"
+
+  zap trash: [
+    "~/Library/Application Support/com.nesbox.tic",
+    "~/Library/Saved Application State/com.nesbox.tic.savedState",
+  ]
+end
