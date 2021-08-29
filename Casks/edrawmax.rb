afebb2c1aa30512af5c85a3826c8627dce152ea5
雷子昂 <lei@leiziangdeMacBook-Pro.local>
2021-08-29 11:36:51 +0800

    Update edrawmax from latest to latest

--- Casks/edrawmax.rb
+++ Casks/edrawmax.rb
@@ -2,18 +2,18 @@ cask "edrawmax" do
   version :latest
   sha256 :no_check
 
-  language "CN" do
+  language "zh", "CN" do
     url "https://cc-download.edrawsoft.cn/edraw-max_cn_full5381.dmg"
     homepage "https://www.edrawsoft.cn/"
+    app "亿图图示.app"
   end
   language "en", default: true do
     url "https://download.edrawsoft.com/edraw-max_full5380.dmg"
     homepage "https://www.edrawsoft.com/"
+    app "Wondershare EdrawMax.app"
   end
 
   name "EdrawMax"
-
-  app "EdrawMax.app"
 
   zap trash: [
     "~/Library/Edraw",
