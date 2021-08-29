215edf65337eb603b6909bee98b2305b0d2fd469
ashendon <ashendon@users.noreply.github.com>
2021-08-29 10:03:44 -0400

    Update electron from 13.2.2 to 13.2.3

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,12 +1,12 @@
 cask "electron" do
-  version "13.2.2"
+  version "13.2.3"
 
   if Hardware::CPU.intel?
-    sha256 "276916b97960d9e49944b9c6dcd35746feb3b55e86747b4ed0e0eb569c47ec0f"
+    sha256 "c02f116484a5e1495d9f7451638bc0d3dea8fa2fde2e4c9c88a17cff98192ddc"
     url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip",
         verified: "github.com/electron/electron/"
   else
-    sha256 "1bc30f12cf6ff3cedde5f90e138b604cd610176be63bb87a9f023146c2f7c81d"
+    sha256 "ef375063e30bc51bbcbe16fb7e5d85933eb60888ccc159c391aefc4f6d616faa"
     url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-arm64.zip",
         verified: "github.com/electron/electron/"
   end
