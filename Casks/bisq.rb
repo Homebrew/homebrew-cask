808e5c78a0190a54e4f00ce96c2748d667561a92
Hannes Braun <hannesbraun@mail.de>
2021-08-30 15:03:05 +0200

    Update bisq from 1.7.2 to 1.7.3 (#110778)

--- Casks/bisq.rb
+++ Casks/bisq.rb
@@ -1,6 +1,6 @@
 cask "bisq" do
-  version "1.7.2"
-  sha256 "cdcec4df053b6e7ad86407f1c13da31898ae52755c3f22d35fe0cf9b06b1ad0e"
+  version "1.7.3"
+  sha256 "027b290b3ae9b41cb65fe06eb1f3e3991bea2873991cd782073a2a4ce36f7447"
 
   url "https://github.com/bisq-network/bisq/releases/download/v#{version}/Bisq-#{version}.dmg",
       verified: "github.com/bisq-network/bisq/"
