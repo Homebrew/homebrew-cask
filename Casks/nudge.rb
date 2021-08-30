1fadceb81844d89ffe3024d527a36ea09e09735b
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-30 23:40:26 +0800

    Update nudge.rb

--- Casks/nudge.rb
+++ Casks/nudge.rb
@@ -3,11 +3,14 @@ cask "nudge" do
   sha256 "ec1f345dbc03a93a76dc764cc1a5cd60826472537c66e1de9f1a0e404bb9951d"
 
   url "https://github.com/macadmins/nudge/releases/download/v.#{version}/Nudge-#{version}.pkg"
-  name "nudge"
+  name "Nudge"
   desc "Application for enforcing os updates"
   homepage "https://github.com/macadmins/nudge"
 
+  depends_on macos: ">= :big_sur"
+
   pkg "Nudge-#{version}.pkg"
+  binary "/Applications/Utilities/Nudge.app/Contents/MacOS/Nudge", target: "nudge"
 
   uninstall pkgutil: "com.github.macadmins.Nudge"
 
