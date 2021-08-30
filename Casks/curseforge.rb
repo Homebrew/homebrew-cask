548c6e55c1d45e41661ba758353ec9a18a13d1b6
Bevan Kay <email@bevankay.me>
2021-08-30 21:14:08 +1000

    Update curseforge from 0.180.1-1 to 0.181.1-1 (#110774)

--- Casks/curseforge.rb
+++ Casks/curseforge.rb
@@ -1,6 +1,6 @@
 cask "curseforge" do
-  version "0.180.1-1"
-  sha256 "d33435ae7d2e556980f397fac91cd7c25c01f14f23973fccfcc45d6d405e2abb"
+  version "0.181.1-1"
+  sha256 "b1e8d3cb6622432ac441a5f361aa9de1c6ca44fa82582abe1d67c3b6af04e938"
 
   url "https://curseforge.overwolf.com/electron/mac/CurseForge-#{version}.dmg"
   name "CurseForge"
