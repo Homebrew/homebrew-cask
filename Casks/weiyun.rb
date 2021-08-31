e7a278e3db0e9159162877a19f17b84cd42cec96
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-31 23:30:41 +0800

    Update weiyun.rb

--- Casks/weiyun.rb
+++ Casks/weiyun.rb
@@ -4,9 +4,14 @@ cask "weiyun" do
 
   url "https://dldir1.qq.com/weiyun/electron-update/release/mac/Weiyun-mac-x64-#{version}.dmg",
       verified: "dldir1.qq.com/weiyun/"
-  appcast "https://qzonestyle.gtimg.cn/qzone/qzactStatics/configSystem/data/65/config1.js"
-  name "weiyun"
+  name "Weiyun"
+  desc "Document backup and online management"
   homepage "https://www.weiyun.com/"
+
+  livecheck do
+    url "https://qzonestyle.gtimg.cn/qzone/qzactStatics/configSystem/data/65/config1.js"
+    regex(/Weiyun-mac-x64-(\d+(?:\.\d+)*)\.dmg/i)
+  end
 
   app "Weiyun.app"
 
