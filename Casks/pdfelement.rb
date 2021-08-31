f51bc9643c76a9c1e0671a5b4089f8c198326faa
Timo Früh <timo.frueh@icloud.com>
2021-08-31 18:36:06 +0200

    Update pdfelement from 7.6.7,3180 to wondershare-pdfelement.rb

--- Casks/pdfelement.rb
+++ Casks/pdfelement.rb
@@ -1,5 +1,5 @@
 cask "pdfelement" do
-  version "7.6.7,3180"
+  version "wondershare-pdfelement.rb"
   sha256 :no_check
 
   url "https://download.wondershare.com/cbs_down/mac-pdfelement_full5237.dmg"
