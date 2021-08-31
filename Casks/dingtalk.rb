8f1fe0a36b61a716fdff67fffa5ff8beb4883759
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-31 10:54:12 +0800

    Update dingtalk from 6.0.22.7 to 6.0.26.8 (#110810)

--- Casks/dingtalk.rb
+++ Casks/dingtalk.rb
@@ -1,6 +1,6 @@
 cask "dingtalk" do
-  version "6.0.22.7"
-  sha256 "8ef84199f75ac6a80d444d6c38d6100a3578c14cdcc78da2959ac67c7f22b186"
+  version "6.0.26.8"
+  sha256 "02cbba85907e5d13554fb5e272cd8f99c2849851e2345ae5e9304e7290f33dda"
 
   url "https://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/DingTalk_v#{version}.dmg"
   name "DingTalk"
