630476a283cdc81eb1a76eb3996939ac4f07df4f
Jacob Ford <j@cobford.com>
2021-08-30 20:41:28 -0400

    Update losslesscut from 3.38.0 to 3.39.0

--- Casks/losslesscut.rb
+++ Casks/losslesscut.rb
@@ -1,6 +1,6 @@
 cask "losslesscut" do
-  version "3.38.0"
-  sha256 "73a0e67bc7da1f71a58796813c2c6ff8576fbd595ceb3544879734d303462e9f"
+  version "3.39.0"
+  sha256 "f950c793088d94629cbd7192437be514bca03f180708e087aefeab12e191f276"
 
   url "https://github.com/mifi/lossless-cut/releases/download/v#{version}/LosslessCut-mac.dmg"
   name "LosslessCut"
