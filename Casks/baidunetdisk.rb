62b2d26fc7ce4079df99139f68d29e644da3ead6
MoonFruit <dkmoonfruit@gmail.com>
2021-08-31 16:55:05 +0800

    Update baidunetdisk from 3.9.3 to 3.9.5 (#110845)

--- Casks/baidunetdisk.rb
+++ Casks/baidunetdisk.rb
@@ -1,6 +1,6 @@
 cask "baidunetdisk" do
-  version "3.9.3"
-  sha256 "f0edacc0ae5d845e4b7f2d783267647d2c2b5562b1d02b18f65593ca05d7efe8"
+  version "3.9.5"
+  sha256 "2d2957d64ccadef5c9cc69a794102902d51bf3eadaa4f23ac45f0aa0f3a5657d"
 
   url "https://wppkg.baidupcs.com/issue/netdisk/MACguanjia/BaiduNetdisk_mac_#{version}.dmg",
       verified: "baidupcs.com/issue/netdisk/MACguanjia/"
