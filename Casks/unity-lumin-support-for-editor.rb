8ac83c95e03c5fa38535e7ec14413c39989b1e7f
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-31 15:01:07 +0800

    Update unity-lumin-support-for-editor.rb

--- Casks/unity-lumin-support-for-editor.rb
+++ Casks/unity-lumin-support-for-editor.rb
@@ -4,10 +4,21 @@ cask "unity-lumin-support-for-editor" do
 
   url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Lumin-Support-for-Editor-#{version.before_comma}.pkg",
       verified: "download.unity3d.com/download_unity/"
-  appcast "https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json"
   name "Unity Lumin Build Support"
   desc "Lumin target support for Unity"
   homepage "https://unity.com/products"
+
+  livecheck do
+    url "https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json"
+    strategy :page_match do |page|
+      page.scan(%r{
+        /download_unity/(\h+)/MacEditorTargetInstaller
+        /UnitySetup-Lumin-Support-for-Editor-(\d+(?:\.\d+)*[a-z]*\d*)\.pkg
+      }ix).map do |match|
+        "#{match[1]},#{match[0]}"
+      end
+    end
+  end
 
   depends_on cask: "unity"
 
