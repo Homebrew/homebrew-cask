6b4f58c15d4b39a0a1756a55460dbbf08b9cd56d
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-31 10:56:01 +0800

    Update gretl from 2021b to 2021c (#110813)

--- Casks/gretl.rb
+++ Casks/gretl.rb
@@ -1,6 +1,6 @@
 cask "gretl" do
-  version "2021b"
-  sha256 "13091151f21969255ba26a414cfed286f2fa1b6ba4b106fefd516a752d451c14"
+  version "2021c"
+  sha256 "1252c3ab200b78b92cc64f747ce1ffaaa455b370c6b54e34fe5f4f61f8faccf9"
 
   url "https://downloads.sourceforge.net/gretl/gretl-#{version}-macos-intel.pkg",
       verified: "downloads.sourceforge.net/gretl/"
