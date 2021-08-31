5b44af46cccbfba98dc8c37cf61d0fd8fbfe6a07
Tai Zhou <21986946+Tai-Zhou@users.noreply.github.com>
2021-08-31 23:46:18 +0800

    Update version and url for weiyun (#110847)
    * Update weiyun.rb
    
    * Update weiyun.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/weiyun.rb
+++ Casks/weiyun.rb
@@ -1,12 +1,17 @@
 cask "weiyun" do
-  version "3.0.6.409_0"
-  sha256 "c723d0f872dbcb74fc745a8dafeb9701adb3ef28b495595082a18bf9139efadd"
+  version "5.2.1109"
+  sha256 "23339bc991d6938b5e71f5431e477fee5708318b7bc532af9a676cfeb2b9cec5"
 
-  url "https://dldir1.qq.com/weiyun/Weiyun_Mac_#{version}.dmg",
+  url "https://dldir1.qq.com/weiyun/electron-update/release/mac/Weiyun-mac-x64-#{version}.dmg",
       verified: "dldir1.qq.com/weiyun/"
-  appcast "https://qzonestyle.gtimg.cn/qzone/qzactStatics/configSystem/data/65/config1.js"
-  name "weiyun"
+  name "Weiyun"
+  desc "Document backup and online management"
   homepage "https://www.weiyun.com/"
+
+  livecheck do
+    url "https://qzonestyle.gtimg.cn/qzone/qzactStatics/configSystem/data/65/config1.js"
+    regex(/Weiyun-mac-x64-(\d+(?:\.\d+)*)\.dmg/i)
+  end
 
   app "Weiyun.app"
 
