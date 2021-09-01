c6c599ccbd6e77a6dde6dfe08e5fb23522f64e6b
Miccal Matthews <miccal.matthews@gmail.com>
2021-09-01 10:29:50 +0800

    Update whatsapp from 2.2132.6 to 2.2134.9 (#110873)

--- Casks/whatsapp.rb
+++ Casks/whatsapp.rb
@@ -1,6 +1,6 @@
 cask "whatsapp" do
-  version "2.2132.6"
-  sha256 "8033eacb6c837fc762e3771669fd95f96fd9f31cb5a34d0f0333fdda39918de2"
+  version "2.2134.9"
+  sha256 "e4393ef81b9a47a97c2c598f012de0d2423bf2361dee5ef710dd59fd4bf50101"
 
   url "https://web.whatsapp.com/desktop/mac/files/release-#{version}.zip"
   name "WhatsApp"
