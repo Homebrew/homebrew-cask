1f095f62aa06189b248a12916e25f47a3a42c3ca
Miccal Matthews <miccal.matthews@gmail.com>
2021-09-01 18:46:37 +0800

    Update smplayer.rb

--- Casks/smplayer.rb
+++ Casks/smplayer.rb
@@ -3,9 +3,9 @@ cask "smplayer" do
   sha256 "f08c20e20090ddc1633fda543d2ec5d3428ad92ebecb864ef239c5a7de15812b"
 
   url "https://github.com/smplayer-dev/smplayer/releases/download/v#{version}/smplayer-#{version}.dmg",
-      verified: "github.com/smplayer-dev/smplayer"
+      verified: "github.com/smplayer-dev/smplayer/"
   name "SMPlayer"
-  desc "Free Media Player with built-in codecs"
+  desc "Media player with built-in codecs"
   homepage "https://www.smplayer.info/"
 
   app "SMPlayer.app"
