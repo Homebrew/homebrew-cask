5e3dbb4f99fd2ce5109b811343f267a24dcdef82
zianglei <zianglei@126.com>
2021-09-01 08:41:50 +0800

    Update edrawmax language and app name (#110743)
    * Update edrawmax from latest to latest
    
    * Update and rename edrawmax.rb to wondershare-edrawmax.rb
    
    Co-authored-by: 雷子昂 <lei@leiziangdeMacBook-Pro.local>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/wondershare-edrawmax.rb
+++ Casks/wondershare-edrawmax.rb
@@ -1,19 +1,25 @@
-cask "edrawmax" do
-  version :latest
+cask "wondershare-edrawmax" do
+  version "11.1.0"
   sha256 :no_check
 
-  language "CN" do
+  language "zh", "CN" do
     url "https://cc-download.edrawsoft.cn/edraw-max_cn_full5381.dmg"
     homepage "https://www.edrawsoft.cn/"
+    app "亿图图示.app"
   end
   language "en", default: true do
     url "https://download.edrawsoft.com/edraw-max_full5380.dmg"
     homepage "https://www.edrawsoft.com/"
+    app "Wondershare EdrawMax.app"
   end
 
   name "EdrawMax"
+  desc "Diagram software"
 
-  app "EdrawMax.app"
+  livecheck do
+    url "https://www.edrawsoft.com/whats-new/edrawmax.html"
+    regex(/EdrawMax\s*V?(\d+(?:\.\d+)*)/i)
+  end
 
   zap trash: [
     "~/Library/Edraw",
