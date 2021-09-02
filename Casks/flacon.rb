2627f5ba5c2582aaaeca0d3a89f0ef0168779e61
Bevan Kay <email@bevankay.me>
2021-09-02 10:25:21 +1000

    Update flacon.rb

--- Casks/flacon.rb
+++ Casks/flacon.rb
@@ -3,7 +3,7 @@ cask "flacon" do
   sha256 "4c44c4b1235e0edb3ffd563676ee189eb8380a3dbadff4272056607bcff71498"
 
   url "https://github.com/flacon/flacon/releases/download/v#{version}/Flacon_#{version}.dmg",
-      verified: "github.com/flacon/flacon"
+      verified: "github.com/flacon/flacon/"
   name "Flacon"
   desc "Open source audio file encoder"
   homepage "https://flacon.github.io/"
