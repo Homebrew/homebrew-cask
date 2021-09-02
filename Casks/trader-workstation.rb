c39e61bb637705f2bed6273ef556a5786c082835
John Pitchko <johnpitchko@users.noreply.github.com>
2021-09-02 03:32:27 -0400

    Update Trader Workstation version to 10.10.2g (#110890)

--- Casks/trader-workstation.rb
+++ Casks/trader-workstation.rb
@@ -1,25 +1,25 @@
 cask "trader-workstation" do
-  version "981.3c"
+  version "10.10.2g"
   sha256 :no_check
 
-  url "https://download2.interactivebrokers.com/installers/tws/stable-standalone/tws-stable-standalone-macosx-x64.dmg"
+  url "https://download2.interactivebrokers.com/installers/tws/latest/tws-latest-macosx-x64.dmg"
   name "Trader Workstation"
   desc "Trading software"
   homepage "https://www.interactivebrokers.com/"
 
   livecheck do
-    url "https://download2.interactivebrokers.com/installers/tws/stable-standalone/version.json"
+    url "https://download2.interactivebrokers.com/installers/tws/latest/version.json"
     regex(/"buildVersion"\s*:\s*"(\d+(?:\.\d+)+[a-z]*)"/i)
   end
 
   installer script: {
-    executable: "#{staged_path}/Trader Workstation #{version.major} Installer.app/Contents/MacOS/JavaApplicationStub",
+    executable: "#{staged_path}/Trader Workstation Installer.app/Contents/MacOS/JavaApplicationStub",
     args:       ["-q"],
   }
 
   uninstall quit:   "com.install4j.5889-6375-8446-2021.22",
             script: {
-              executable: "/Applications/Trader Workstation #{version.major}/Trader Workstation #{version.major} Uninstaller.app/Contents/MacOS/JavaApplicationStub",
+              executable: "/Applications/Trader Workstation/Trader Workstation Uninstaller.app/Contents/MacOS/JavaApplicationStub",
               args:       ["-q"],
             }
 end
