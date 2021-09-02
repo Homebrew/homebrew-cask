fa7bd739afbb2532e01b494c259d586abc6b4a20
Reno <25192197+singularitti@users.noreply.github.com>
2021-09-02 09:05:05 -0400

    Update permute from 3.6.7,2433 to 3.6.8,2438

--- Casks/permute.rb
+++ Casks/permute.rb
@@ -1,6 +1,6 @@
 cask "permute" do
-  version "3.6.7,2433"
-  sha256 "a18ce15527f16ce4c66375802c40cbac5b0d9980d465b28bd700375de3b6f2ea"
+  version "3.6.8,2438"
+  sha256 "bbf662a6f18b0dca5ca14ba455fb97bb6f176af33dbe475e3151b924cdb92a14"
 
   url "https://trial.charliemonroe.net/permute/v#{version.major}/Permute_#{version.major}_#{version.after_comma}.dmg"
   name "Permute"
