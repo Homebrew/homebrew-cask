a6c7ab46d4baa71d6ebbffa5145ef32976f8382e
eloyaldea <35566945+eloyaldea@users.noreply.github.com>
2021-09-03 15:53:58 +0200

    Update spotify from 1.1.66.580.gbd43cbc9,1.1.66.580.gbd43cbc9-18 to 1.1.67.586.gbb5ef64e,1.1.67.586.gbb5ef64e-22 (#110947)

--- Casks/spotify.rb
+++ Casks/spotify.rb
@@ -1,6 +1,6 @@
 cask "spotify" do
   if Hardware::CPU.intel?
-    version "1.1.66.580.gbd43cbc9,1.1.66.580.gbd43cbc9-18"
+    version "1.1.67.586.gbb5ef64e,1.1.67.586.gbb5ef64e-22"
     sha256 :no_check
 
     url "https://download.scdn.co/Spotify.dmg",
