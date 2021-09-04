884b1d38956e39c013c760dabe97e85886b81d7c
Ignacio Sanchez Gines <nacho@geardome.com>
2021-09-04 11:51:23 +0200

    Update gearsystem from 3.4.0 to 3.4.1

--- Casks/gearsystem.rb
+++ Casks/gearsystem.rb
@@ -1,6 +1,6 @@
 cask "gearsystem" do
-  version "3.4.0"
-  sha256 "da7143dbb2816a6de2a979531965ebf581f416202fb4267544a03023d4121ac2"
+  version "3.4.1"
+  sha256 "b5a9a2dc62d721448d315f7116e08433257ee5f849362f570ebe99eb7f425336"
 
   url "https://github.com/drhelius/Gearsystem/releases/download/gearsystem-#{version}/Gearsystem-#{version}-macOS.zip"
   name "Gearsystem"
