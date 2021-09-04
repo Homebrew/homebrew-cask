049e77018b0e49fbb948083f4028e07e2c2d9771
Mathias <3380095+mathiashro@users.noreply.github.com>
2021-09-04 19:12:12 +0200

    Bump version of JOSM cask from 18118 to 18191
    Bump to current stable release ("tested").

--- Casks/josm.rb
+++ Casks/josm.rb
@@ -1,6 +1,6 @@
 cask "josm" do
-  version "18118"
-  sha256 "278d087f601438d76a6835e732fcc81b12c945a20178bb041fcd25f2307d3b44"
+  version "18191"
+  sha256 "221c4c0a5c2c1b58d7282f9300b87342a30bd693c45f7ef1173d76b74a340b7d"
 
   url "https://github.com/JOSM/josm/releases/download/#{version}-tested/JOSM-macOS-java16-#{version}.zip",
       verified: "github.com/JOSM/josm/"
