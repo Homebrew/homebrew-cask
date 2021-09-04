3a91b32f5bb9638e4280bd5f1866b04a0065e984
Jacob Ford <j@cobford.com>
2021-09-04 03:55:35 -0400

    Fix slate download URL to only working one

--- Casks/slate.rb
+++ Casks/slate.rb
@@ -2,7 +2,7 @@ cask "slate" do
   version "1.0.25"
   sha256 :no_check
 
-  url "http://slate.ninjamonkeysoftware.com/Slate.dmg",
+  url "https://www.ninjamonkeysoftware.com/slate/versions/slate-#{version}.tar.gz",
       verified: "slate.ninjamonkeysoftware.com/"
   name "Slate"
   desc "Window management application (replacement for Divvy/SizeUp/ShiftIt)"
