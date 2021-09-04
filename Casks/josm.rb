949dda206ea5e3b10fb073f2bdbf072a02f59778
Mathias <3380095+mathiashro@users.noreply.github.com>
2021-09-04 17:25:27 +0200

    Update JOSM cask url: project moved to 'JOSM' org
    Recently the JOSM project moved from 'openstreetmap' GitHub organization to the dedicated 'JOSM'. This PR is to prepare upgrade to current tested-18191 release.

--- Casks/josm.rb
+++ Casks/josm.rb
@@ -2,7 +2,7 @@ cask "josm" do
   version "18118"
   sha256 "278d087f601438d76a6835e732fcc81b12c945a20178bb041fcd25f2307d3b44"
 
-  url "https://github.com/openstreetmap/josm/releases/download/#{version}-tested/JOSM-macOS-java16.zip",
+  url "https://github.com/JOSM/josm/releases/download/#{version}-tested/JOSM-macOS-java16.zip",
       verified: "github.com/openstreetmap/josm/"
   name "JOSM"
   desc "Extensible editor for OpenStreetMap"
