3d064a7ef5231c482fdae5f8d2e44703d6ecfb7a
Jacob Ford <j@cobford.com>
2021-09-05 17:58:26 -0400

    Fix slate download URL (#110991)
    * Fix slate download URL to only working one
    
    * Update verified domain
    
    * Add back sha256 for versioned URL
    
    * Update slate.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/slate.rb
+++ Casks/slate.rb
@@ -1,11 +1,11 @@
 cask "slate" do
   version "1.0.25"
-  sha256 :no_check
+  sha256 "c19a27fda6cd61efd9c7b21949646bbc20919f18e99bf2bee24874cbc1acba43"
 
-  url "http://slate.ninjamonkeysoftware.com/Slate.dmg",
-      verified: "slate.ninjamonkeysoftware.com/"
+  url "https://www.ninjamonkeysoftware.com/slate/versions/slate-#{version}.tar.gz",
+      verified: "ninjamonkeysoftware.com/"
   name "Slate"
-  desc "Window management application (replacement for Divvy/SizeUp/ShiftIt)"
+  desc "Window management application"
   homepage "https://github.com/jigish/slate"
 
   livecheck do
