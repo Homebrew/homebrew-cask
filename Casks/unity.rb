5ef76645bd932521bb2836383916fb5741d44432
Miccal Matthews <miccal.matthews@gmail.com>
2021-09-06 09:05:05 +0800

    Update unity from 2021.1.18f1,25bdc3efbc2d to 2021.1.19f1,5f5eb8bbdc25 (#110999)

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -1,6 +1,6 @@
 cask "unity" do
-  version "2021.1.18f1,25bdc3efbc2d"
-  sha256 "d44dbdcf241c5b984c7c0cabb83979df1549e2630f033e76d46d43444ca9bbdb"
+  version "2021.1.19f1,5f5eb8bbdc25"
+  sha256 "fc499d7adb7a1c3387e564e438578a9083e5a5c61ef43a6de325e896c5d1e093"
 
   url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorInstaller/Unity-#{version.before_comma}.pkg",
       verified: "download.unity3d.com/download_unity/"
