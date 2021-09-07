7c909fb51fafc399135ed4d7d6847795770cc6f0
Reno <25192197+singularitti@users.noreply.github.com>
2021-09-07 18:13:38 -0400

    Update fvim from 0.3.412,g3b521db to 0.3.423,g6e28667 (#111066)

--- Casks/fvim.rb
+++ Casks/fvim.rb
@@ -1,6 +1,6 @@
 cask "fvim" do
-  version "0.3.412,g3b521db"
-  sha256 "f14e9841dd5836cb2d5fda8c6938fc557df877b0191315bc131794f853a7ace1"
+  version "0.3.423,g6e28667"
+  sha256 "618c0e288291986a46b15236007eee52015e5afe5d32ee441d3f2eaef3902eb6"
 
   url "https://github.com/yatli/fvim/releases/download/v#{version.before_comma}%2B#{version.after_comma}/fvim-osx.zip"
   name "FVim"
