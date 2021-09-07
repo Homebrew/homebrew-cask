66945ac8e1ff1c250309d6eed1eeaff0173d45d1
Miccal Matthews <miccal.matthews@gmail.com>
2021-09-07 14:33:40 +0800

    Update sensei from 1.4.11,94 to 1.4.12,95 (#111045)
    * Update sensei from 1.4.11,94 to 1.4.12,95
    
    * Update sensei.rb

--- Casks/sensei.rb
+++ Casks/sensei.rb
@@ -1,5 +1,5 @@
 cask "sensei" do
-  version "1.4.11,94"
+  version "1.4.12,95"
   sha256 :no_check
 
   url "https://cindori.s3.amazonaws.com/Sensei.dmg",
@@ -19,16 +19,16 @@ cask "sensei" do
   app "Sensei.app"
 
   uninstall delete:    [
-    "/Library/LaunchDaemons/org.cindori.SenseiHelper.plist",
-    "/Library/LaunchDaemons/org.cindori.SenseiDaemon.plist",
     "/Library/LaunchAgents/org.cindori.SenseiMonitor.plist",
+    "/Library/LaunchDaemons/org.cindori.SenseiDaemon.plist",
+    "/Library/LaunchDaemons/org.cindori.SenseiHelper.plist",
     "/Library/PrivilegedHelperTools/org.cindori.SenseiTool",
   ],
             launchctl: "org.cindori.SenseiTool"
 
   zap trash: [
-    "~/Library/Application Support/Sensei",
     "~/Library/Application Support/org.cindori.Sensei",
+    "~/Library/Application Support/Sensei",
     "~/Library/Caches/org.cindori.Sensei",
     "~/Library/Cookies/org.cindori.Sensei.binarycookies",
     "~/Library/Preferences/org.cindori.Sensei.plist",
