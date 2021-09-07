80a250f23be350663325d9983f61078a330165b9
Ian Robert O'Brien <ian.r.obrien@protonmail.com>
2021-09-07 13:17:09 +0200

    Update cryptomator from 1.5.17 to 1.5.18

--- Casks/cryptomator.rb
+++ Casks/cryptomator.rb
@@ -1,6 +1,6 @@
 cask "cryptomator" do
-  version "1.5.17"
-  sha256 "a5c6729185dc231929de864a053da69cd895708c8e6610bf32c5d4a38c7587bb"
+  version "1.5.18"
+  sha256 "612396100e1169d5e0e187e7efbef37df5a64eb061268eff289332705fc7c080"
 
   url "https://github.com/cryptomator/cryptomator/releases/download/#{version}/Cryptomator-#{version}.dmg",
       verified: "github.com/cryptomator/cryptomator/"
