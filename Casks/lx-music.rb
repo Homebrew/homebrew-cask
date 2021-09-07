b6950e0aa27280cb24b16c2e79d05c62128d1bc7
Miccal Matthews <miccal.matthews@gmail.com>
2021-09-07 14:33:20 +0800

    Update lx-music from 1.12.2 to 1.13.0 (#111039)

--- Casks/lx-music.rb
+++ Casks/lx-music.rb
@@ -1,12 +1,12 @@
 cask "lx-music" do
-  version "1.12.2"
+  version "1.13.0"
 
   if Hardware::CPU.intel?
-    sha256 "5c5825cff999905422a3082510d6a2ff65f22b4cf6d7a13ae56c9109a35515d8"
+    sha256 "024a8353e3abc17670df88c0120326dcfc42c59f12c567e2fbb24a91567a000a"
 
     url "https://github.com/lyswhut/lx-music-desktop/releases/download/v#{version}/lx-music-desktop-#{version}.dmg"
   else
-    sha256 "4823d8977429c9d6ba0bd87900c54bd9348e28a380186f1de2642d8d321ebf0b"
+    sha256 "fb3881bf68788ba53f090a9bca2eacf3c1aa40ef415e18fbf81283b3c7497d77"
 
     url "https://github.com/lyswhut/lx-music-desktop/releases/download/v#{version}/lx-music-desktop-#{version}-arm64.dmg"
   end
