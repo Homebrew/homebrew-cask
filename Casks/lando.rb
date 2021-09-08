1270d9f740e6551b646d2d44fd38d92b13b484dd
philltran <web@philltran.com>
2021-09-08 00:15:54 -0400

    Update lando from 3.3.0 to 3.3.1

--- Casks/lando.rb
+++ Casks/lando.rb
@@ -1,13 +1,13 @@
 cask "lando" do
-  version "3.3.0"
+  version "3.3.1"
 
   if Hardware::CPU.intel?
-    sha256 "2bf46c831c1eea7848a07e30bb09734c9b269667d2b128de5eea03ac9726ea09"
+    sha256 "d9e672d8a5db6d62100ee0b80f9a7a010b426ea7bbe0052ac500c5ab285d0cd3"
 
     url "https://github.com/lando/lando/releases/download/v#{version}/lando-x64-v#{version}.dmg",
         verified: "github.com/lando/lando/"
   else
-    sha256 "36d796da1dbc89750cf4f714e0ad9ec15ef19a560cff9bb52e0e015a237a8321"
+    sha256 "9c300e52f373eb6af380039061c36bcf25c4fff6f313e94526347cbec19ad13f"
 
     url "https://github.com/lando/lando/releases/download/v#{version}/lando-arm64-v#{version}.dmg",
         verified: "github.com/lando/lando/"
