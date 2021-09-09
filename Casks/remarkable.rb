ee952b19c6e601c9eaf5d13fcf37dc900bf232bf
Miccal Matthews <miccal.matthews@gmail.com>
2021-09-10 07:33:06 +0800

    Update remarkable.rb

--- Casks/remarkable.rb
+++ Casks/remarkable.rb
@@ -1,12 +1,21 @@
 cask "remarkable" do
-  version "2.8.2.170"
+  version "2.8.2.170,11141120"
   sha256 "7669f1e8dd1b13fd5305af8a215b17b33b7ff6cc218a218c9992b7dd83223465"
 
-  url "https://downloads.remarkable.com/desktop/production/mac/reMarkable-#{version}.dmg"
+  url "https://eu-central-1.linodeobjects.com/remarkable-#{version.major}/sparkle/reMarkableMacOs/Prod/#{version.after_comma}/reMarkable-#{version.before_comma}.dmg"
   name "Remarkable"
   desc "Desktop application"
   homepage "https://remarkable.com/"
 
+  livecheck do
+    url "https://get-updates.cloud.remarkable.engineering/sparkle/reMarkableMacOs/Prod/appcast.xml"
+    regex(%r{reMarkableMacOs/Prod/(\d+)/reMarkable-(\d+(?:\.\d+)*)\.dmg}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[1]},#{match[0]}" }
+    end
+  end
+
+  auto_updates true
   depends_on macos: ">= :high_sierra"
 
   app "reMarkable.app"
