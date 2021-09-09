d92bc711a496e9ea63d6d362bc4935fe963f4cae
小雨 <614422099@QQ.com>
2021-09-09 17:55:54 +0800

    Update lens support Apple Silicon (#111123)
    * Update lens support Apple Silicon
    
    * fix audit
    
    * fix style ci
    
    * fix style ci
    
    * fix style ci

--- Casks/lens.rb
+++ Casks/lens.rb
@@ -1,8 +1,14 @@
 cask "lens" do
   version "5.2.0-latest.20210908.1"
-  sha256 "6055e4c451bae97c0ec7f337bddf9afeec95098b153849c29eed6378dbdb9728"
 
-  url "https://api.k8slens.dev/binaries/Lens-#{version}.dmg"
+  if Hardware::CPU.intel?
+    sha256 "6055e4c451bae97c0ec7f337bddf9afeec95098b153849c29eed6378dbdb9728"
+    url "https://api.k8slens.dev/binaries/Lens-#{version}.dmg"
+  else
+    sha256 "d353a2d163af2799562fce0b94f3c7b8f210023a3b82c071a3a593901a25e4b5"
+    url "https://api.k8slens.dev/binaries/Lens-#{version}-arm64.dmg"
+  end
+
   name "Lens"
   desc "Kubernetes IDE"
   homepage "https://k8slens.dev/"
