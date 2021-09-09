75003ab5a2e8206aa7f6ba64941158f961478f53
Bevan Kay <email@bevankay.me>
2021-09-10 09:50:17 +1000

    Update evernote from 10.20.4,2893 to 10.21.5,2931 (#111145)
    * Update evernote from 10.20.4,2893 to 10.21.5,2931
    
    * Update evernote.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/evernote.rb
+++ Casks/evernote.rb
@@ -15,8 +15,8 @@ cask "evernote" do
 
     url "https://cdn1.evernote.com/mac-smd/public/Evernote_RELEASE_#{version}.dmg"
   else
-    version "10.20.4,2893"
-    sha256 "f8053ca01036a09491f17b846d4d2884f449931f9b09854ac3ea57d71cabeb17"
+    version "10.21.5,2931"
+    sha256 "f68a7e1e0a63b7cb90960cd45cfb82aa61767f5111a362d1989421eabd8455e1"
 
     url "https://cdn1.evernote.com/boron/mac/builds/Evernote-#{version.before_comma}-mac-ddl-ga-#{version.after_comma}.dmg"
   end
@@ -49,10 +49,10 @@ cask "evernote" do
     "~/Library/Application Support/com.evernote.EvernoteHelper",
     "~/Library/Application Support/Caches/evernote-client-updater",
     "~/Library/Caches/com.evernote.Evernote",
+    "~/Library/Cookies/com.evernote.Evernote.binarycookies",
+    "~/Library/Logs/Evernote",
     "~/Library/Preferences/com.evernote.Evernote.plist",
     "~/Library/Preferences/com.evernote.EvernoteHelper.plist",
-    "~/Library/Logs/Evernote",
-    "~/Library/Cookies/com.evernote.Evernote.binarycookies",
     "~/Library/Saved Application State/com.evernote.Evernote.savedState",
   ]
 end
