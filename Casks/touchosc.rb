b1c046bbd67ec525cb44f4e31ff0d2fe75d1131c
Bevan Kay <email@bevankay.me>
2021-09-09 12:48:24 +1000

    touchosc: add zap, update livecheck, bump version (#111118)
    * touchosc: add zap, update livecheck, bump version
    
    * formatting update, add auto_updates

--- Casks/touchosc.rb
+++ Casks/touchosc.rb
@@ -1,16 +1,27 @@
 cask "touchosc" do
-  version "1.0.5.108"
-  sha256 "b1b76240bf302635f8dd4d02200edb166e536ef08a96acfc870af38401efc5fe"
+  version "1.0.5,109"
+  sha256 "ec7e563b5c2a1a267386f3f9941aea63953d0823e1f8c356e875059ad6893822"
 
-  url "https://hexler.net/pub/touchosc/touchosc-#{version}-macos.dmg"
+  url "https://hexler.net/pub/touchosc/touchosc-#{version.before_comma}.#{version.after_comma}-macos.dmg"
   name "touchosc"
   desc "MIDI and OSC Controller Software"
   homepage "https://hexler.net/touchosc"
 
   livecheck do
-    url "https://hexler.net/touchosc/releases"
-    regex(%r{href=.*?/touchosc-(\d+(?:\.\d+)*)-macos\.dmg}i)
+    url "https://hexler.net/touchosc/appcast/macos/"
+    strategy :sparkle
   end
 
+  auto_updates true
+
   app "TouchOSC.app"
+
+  zap trash: [
+    "~/Documents/TouchOSC",
+    "~/Library/Application Support/net.hexler.lex",
+    "~/Library/Caches/net.hexler.lex",
+    "~/Library/Logs/net.hexler.lex",
+    "~/Library/Preferences/net.hexler.lex.plist",
+    "~/Library/Saved Application State/net.hexler.lex.savedState",
+  ]
 end
