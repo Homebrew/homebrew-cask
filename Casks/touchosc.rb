0e97c8876e18e2bb2d4d8b27fed68b4da976bed3
Bevan Kay <email@bevankay.me>
2021-09-09 12:26:36 +1000

    formatting update, add auto_updates

--- Casks/touchosc.rb
+++ Casks/touchosc.rb
@@ -2,7 +2,7 @@ cask "touchosc" do
   version "1.0.5,109"
   sha256 "ec7e563b5c2a1a267386f3f9941aea63953d0823e1f8c356e875059ad6893822"
 
-  url "https://hexler.net/pub/touchosc/touchosc-#{version.tr(",", ".")}-macos.dmg"
+  url "https://hexler.net/pub/touchosc/touchosc-#{version.before_comma}.#{version.after_comma}-macos.dmg"
   name "touchosc"
   desc "MIDI and OSC Controller Software"
   homepage "https://hexler.net/touchosc"
@@ -11,6 +11,8 @@ cask "touchosc" do
     url "https://hexler.net/touchosc/appcast/macos/"
     strategy :sparkle
   end
+
+  auto_updates true
 
   app "TouchOSC.app"
 
