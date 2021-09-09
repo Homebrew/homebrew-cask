22df6a610e58259fc21dd38c8cbe9774baf40b99
Bevan Kay <bevanjkay@gmail.com>
2021-09-09 11:17:57 +1000

    Update url, homepage, livecheck

--- Casks/ultimaker-cura.rb
+++ Casks/ultimaker-cura.rb
@@ -2,14 +2,15 @@ cask "ultimaker-cura" do
   version "4.11.0"
   sha256 "301b709643e3816c71445af1263855ba8fa45d4c583cebcf039be2f9150c7c89"
 
-  url "https://github.com/Ultimaker/Cura/releases/download/#{version}/Ultimaker_Cura-#{version}.dmg"
+  url "https://github.com/Ultimaker/Cura/releases/download/#{version}/Ultimaker_Cura-#{version}.dmg",
+      verified: "github.com/Ultimaker/Cura/"
   name "Ultimaker Cura"
   name "Cura"
   desc "3D printer and slicing GUI"
-  homepage "https://ultimaker.com/en/products/cura-software"
+  homepage "https://ultimaker.com/software/ultimaker-cura"
 
   livecheck do
-    url "https://github.com/Ultimaker/Cura"
+    url :url
     strategy :github_latest
   end
 
