17c8e6fb197e0f99d78475ad93b35342e3dc3a59
Miccal Matthews <miccal.matthews@gmail.com>
2021-09-10 07:34:00 +0800

    Update remarkable.rb

--- Casks/remarkable.rb
+++ Casks/remarkable.rb
@@ -2,7 +2,8 @@ cask "remarkable" do
   version "2.8.2.170,11141120"
   sha256 "7669f1e8dd1b13fd5305af8a215b17b33b7ff6cc218a218c9992b7dd83223465"
 
-  url "https://eu-central-1.linodeobjects.com/remarkable-#{version.major}/sparkle/reMarkableMacOs/Prod/#{version.after_comma}/reMarkable-#{version.before_comma}.dmg"
+  url "https://eu-central-1.linodeobjects.com/remarkable-#{version.major}/sparkle/reMarkableMacOs/Prod/#{version.after_comma}/reMarkable-#{version.before_comma}.dmg",
+      verified: "eu-central-1.linodeobjects.com/"
   name "Remarkable"
   desc "Desktop application"
   homepage "https://remarkable.com/"
