1c434b0ddb758cbcad5659285dc735ecf9f88737
Nick Mitchell <starpit@users.noreply.github.com>
2021-09-10 19:51:06 -0400

    Upgrade kui from 10.4.6 to 10.4.13 (#111171)

--- Casks/kui.rb
+++ Casks/kui.rb
@@ -1,9 +1,18 @@
 cask "kui" do
-  version "10.4.6"
-  sha256 "99ab0bef38acad6a29d8ecd914b6e1f7202290414dff5f5269566791f731f2f2"
+  version "10.4.13"
 
-  url "https://github.com/IBM/kui/releases/download/v#{version}/Kui-darwin-x64.tar.bz2",
-      verified: "github.com/IBM/kui/"
+  if Hardware::CPU.intel?
+    url "https://github.com/kubernetes-sigs/kui/releases/download/v#{version}/Kui-darwin-x64.tar.bz2",
+        verified: "github.com/kubernetes-sigs/kui/"
+    sha256 "7adfc6807700549b3f2b299f181e5f7c63394c0319879314082484d7822a28af"
+    app "Kui-darwin-x64/Kui.app"
+  else
+    url "https://github.com/kubernetes-sigs/kui/releases/download/v#{version}/Kui-darwin-arm64.tar.bz2",
+        verified: "github.com/kubernetes-sigs/kui/"
+    sha256 "ff2982297a4f55f834521460401ff53e8b4825bef11cf2aa7aafb916947b0948"
+    app "Kui-darwin-arm64/Kui.app"
+  end
+
   name "Kui"
   desc "Hybrid command-line/UI development experience for cloud-native development"
   homepage "https://kui.tools/"
@@ -13,6 +22,7 @@ cask "kui" do
     strategy :github_latest
   end
 
-  app "Kui-darwin-x64/Kui.app"
   binary "#{appdir}/Kui.app/Contents/Resources/kubectl-kui"
+
+  zap trash: "~/Library/Application Support/Kui"
 end
