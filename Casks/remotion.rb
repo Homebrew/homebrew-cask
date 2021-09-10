7fcdd5ebc63713823c30173a30f7cfb9cbaf761a
Steve Huff <shuff@vecna.org>
2021-09-10 16:17:18 -0400

    Add Remotion 1.56.1

---
+++ Casks/remotion.rb
@@ -0,0 +1,11 @@
+cask "remotion" do
+  version "1.56.1"
+  sha256 "5fe4335a5dac88c44f83ec27573206c908c3d7113ab265cc9ebdcc14257ad45e"
+
+  url "https://updates.remotion.com/installers/Remotion%20#{version}.dmg"
+  name "remotion"
+  desc "Virtual office that puts your hybrid team right on your desktop"
+  homepage "https://remotion.com/"
+
+  app "Remotion.app"
+end
