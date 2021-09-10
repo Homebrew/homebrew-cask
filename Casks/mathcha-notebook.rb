84711a6f70e323ccc3fb0dda115098b11200cd40
Bevan Kay <email@bevankay.me>
2021-09-11 08:39:39 +1000

    version url, update livecheck, add auto_updates

--- Casks/mathcha-notebook.rb
+++ Casks/mathcha-notebook.rb
@@ -1,19 +1,18 @@
 cask "mathcha-notebook" do
   version "1.0.423"
-  sha256 :no_check
+  sha256 "75b2431e5178fff9382ebc68027d8d7334bf340a75629d562b97f80d06f611f0"
 
-  url "https://notebook-downloads.mathcha.io/public/Mathcha%20Notebook-latest-mac.zip"
+  url "https://notebook-downloads.mathcha.io/public/Mathcha%20Notebook-#{version}-mac.zip"
   name "Mathcha Notebook"
   desc "Mathematics editor"
   homepage "https://www.mathcha.io/"
 
   livecheck do
     url "https://notebook-downloads.mathcha.io/public/latest-mac.yml"
-    regex(/Mathcha Notebook-(\d+(?:\.\d+)+)-mac.zip/i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| (match[0]).to_s }
-    end
+    strategy :electron_builder
   end
+
+  auto_updates true
 
   app "Mathcha Notebook.app"
 
