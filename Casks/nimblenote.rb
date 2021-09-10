d14141d2e787a6ab69a53174fa648eb10ccbbf9f
David Winter <i@djw.me>
2021-09-10 17:39:01 +0100

    Add nimblenote.app v3.1.1

---
+++ Casks/nimblenote.rb
@@ -0,0 +1,19 @@
+cask "nimblenote" do
+  version "3.1.1"
+  sha256 "7dbdaa2d79baa13493e28c2ab0ba38671a6e2874dfa07c09278c8ebc278f775a"
+
+  url "https://github.com/nimblenote/nimblenote/releases/latest/download/nimblenote-#{version}.dmg", verified: "https://github.com/nimblenote/nimblenote"
+  name "nimblenote"
+  desc "Keyboard-driven note taking"
+  homepage "https://nimblenote.app/"
+
+  livecheck do
+    url "https://github.com/nimblenote/nimblenote/releases/latest/download/release.json"
+    strategy :page_match
+    regex(/"version":"(\d+\.\d+\.\d+)"/i)
+  end
+
+  auto_updates true
+
+  app "nimblenote.app"
+end
