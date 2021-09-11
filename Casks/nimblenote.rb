8c5216749f83527d3893f56061e18dd5f0802c59
Miccal Matthews <miccal.matthews@gmail.com>
2021-09-11 13:47:01 +0800

    Update nimblenote.rb

--- Casks/nimblenote.rb
+++ Casks/nimblenote.rb
@@ -2,16 +2,11 @@ cask "nimblenote" do
   version "3.1.1"
   sha256 "7dbdaa2d79baa13493e28c2ab0ba38671a6e2874dfa07c09278c8ebc278f775a"
 
-  url "https://github.com/nimblenote/nimblenote/releases/latest/download/nimblenote-#{version}.dmg", verified: "https://github.com/nimblenote/nimblenote"
-  name "nimblenote"
+  url "https://github.com/nimblenote/nimblenote/releases/latest/download/nimblenote-#{version}.dmg",
+      verified: "github.com/nimblenote/nimblenote/"
+  name "Nimblenote"
   desc "Keyboard-driven note taking"
   homepage "https://nimblenote.app/"
-
-  livecheck do
-    url "https://github.com/nimblenote/nimblenote/releases/latest/download/release.json"
-    strategy :page_match
-    regex(/"version":"(\d+\.\d+\.\d+)"/i)
-  end
 
   auto_updates true
 
