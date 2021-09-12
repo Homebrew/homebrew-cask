1c1fd9caa0a3c408bc4c646fb8429bd208c62f5a
Miccal Matthews <miccal.matthews@gmail.com>
2021-09-12 19:54:15 +0800

    Update metabase from 0.40.3.1 to 0.40.4 (#111237)

--- Casks/metabase.rb
+++ Casks/metabase.rb
@@ -1,6 +1,6 @@
 cask "metabase" do
-  version "0.40.3.1"
-  sha256 "c43b04ae06b9536ad86be7144ff5c4257aad5438ee8359c8acd7d5db18631266"
+  version "0.40.4"
+  sha256 "d1450a953dedf6da03de002eeab3615195c2720a1c0d9883c8ba52012f8ecd10"
 
   url "https://s3.amazonaws.com/downloads.metabase.com/v#{version}/Metabase.zip",
       verified: "s3.amazonaws.com/downloads.metabase.com/"
