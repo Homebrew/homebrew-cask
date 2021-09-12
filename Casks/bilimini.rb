faed084373ad3c856f0ff7ee56e6e0cca727a992
Alexander Hamann <ah0703@gmail.com>
2021-09-12 14:16:11 +0200

    Update bilimini from 1.5.4 to 1.5.5

--- Casks/bilimini.rb
+++ Casks/bilimini.rb
@@ -1,6 +1,6 @@
 cask "bilimini" do
-  version "1.5.4"
-  sha256 "c09105eb8872cc9652b06fefae0cd48bebd79591ae5df702da1f5017acab3365"
+  version "1.5.5"
+  sha256 "ef09cb6365bcd16b34841e529d1c58325e14ed54d95f9096d1ae224125a0bc0d"
 
   url "https://github.com/chitosai/bilimini/releases/download/v#{version}/bilimini-#{version}-mac.zip"
   name "bilimini"
