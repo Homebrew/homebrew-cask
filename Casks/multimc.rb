2a946d13ebe8c7a4f4919a5b4d538c50fcff5136
Bevan Kay <email@bevankay.me>
2021-09-13 10:57:47 +1000

    update livecheck, and version formatting

--- Casks/multimc.rb
+++ Casks/multimc.rb
@@ -1,11 +1,16 @@
 cask "multimc" do
-  version "0.6.13"
+  version "0.6.13.2518"
   sha256 :no_check
 
   url "https://files.multimc.org/downloads/mmc-stable-osx64.tar.gz"
   name "Multi MC"
   desc "Minecraft launcher"
   homepage "https://multimc.org/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   auto_updates true
 
