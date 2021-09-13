e2af949d255b0ef4f25d1f88bac6ffe78740af9a
Bevan Kay <email@bevankay.me>
2021-09-14 09:18:07 +1000

    Update spyder from 5.1.3 to 5.1.4 (#111255)

--- Casks/spyder.rb
+++ Casks/spyder.rb
@@ -1,6 +1,6 @@
 cask "spyder" do
-  version "5.1.3"
-  sha256 "7784dc2354a0023a038098d37e5e7a11b7f6b081f1db68c768ed193b22883f61"
+  version "5.1.4"
+  sha256 "96d9fba14b621b69724db7b7d06dffcfc3bf025eb377b6f43b7fc21866ff062d"
 
   url "https://github.com/spyder-ide/spyder/releases/download/v#{version}/Spyder.dmg",
       verified: "github.com/spyder-ide/spyder/"
