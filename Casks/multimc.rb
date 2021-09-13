76c74e0d186d4602628b4cd543338505477f5f5f
Bevan Kay <email@bevankay.me>
2021-09-13 12:05:10 +1000

    Update Casks/multimc.rb
    Co-authored-by: haykam821 <24855774+haykam821@users.noreply.github.com>

--- Casks/multimc.rb
+++ Casks/multimc.rb
@@ -2,7 +2,7 @@ cask "multimc" do
   version "0.6.13"
   sha256 :no_check
 
-  url "https://files.multimc.org/downloads/mmc-stable-osx64-5.5.12.tar.gz"
+  url "https://files.multimc.org/downloads/mmc-stable-osx64-5.15.2.tar.gz"
   name "Multi MC"
   desc "Minecraft launcher"
   homepage "https://multimc.org/"
