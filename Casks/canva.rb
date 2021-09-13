023da43b3dac4a0087490d0b7194c16382a92058
Bevan Kay <email@bevankay.me>
2021-09-13 11:59:09 +1000

    Update canva from 1.30.0 to 1.31.0 (#111251)

--- Casks/canva.rb
+++ Casks/canva.rb
@@ -1,6 +1,6 @@
 cask "canva" do
-  version "1.30.0"
-  sha256 "7e7fab6ad7c203dab25b02acb820e66dbae5ff38cd2a78637f6cf9f8a59687b5"
+  version "1.31.0"
+  sha256 "932662073ddb115ba65d9ccafc90144ce284010e111437fc811585d8c98fdf2e"
 
   url "https://desktop-release.canva-deploy.com/Canva-#{version}.dmg",
       verified: "desktop-release.canva-deploy.com/"
