a21bc76566b5a4dd092f523321be2bad1d6775b8
MoonFruit <dkmoonfruit@gmail.com>
2021-09-14 10:21:28 +0800

    Update wechatwork from 3.1.15.90292 to 3.1.16.90294

--- Casks/wechatwork.rb
+++ Casks/wechatwork.rb
@@ -1,6 +1,6 @@
 cask "wechatwork" do
-  version "3.1.15.90292"
-  sha256 "0fec83e0d4e7d20eed886734602469990eb3c1e5738c73ca77fd6bd20a97fd46"
+  version "3.1.16.90294"
+  sha256 "4cf36ee92fca3de884d26ae83f3bf28250aa3a59eb76f51bfe244ea4aa4201c5"
 
   url "https://dldir1.qq.com/wework/work_weixin/WeCom_#{version}.dmg"
   name "WeChat Work"
