f1199d2dbdd55a1b448e4fe7ff29221cc6b82ed8
neutric <neutric@users.noreply.github.com>
2021-09-14 10:52:28 +0200

    Update chemdoodle from 11.6.0 to 11.7.0 (#111286)

--- Casks/chemdoodle.rb
+++ Casks/chemdoodle.rb
@@ -1,6 +1,6 @@
 cask "chemdoodle" do
-  version "11.6.0"
-  sha256 "c00c91c09af5d909b4856038f549eb424eb5044761353d77cca1bb9bd05c62fc"
+  version "11.7.0"
+  sha256 "2ae7a71a9628956c6d6b324bdb3a0564235f8de0d7b1c9c060f2cb36d7aada87"
 
   url "https://www.ichemlabs.com/downloads/ChemDoodle-macos-#{version}.dmg"
   name "ChemDoodle"
