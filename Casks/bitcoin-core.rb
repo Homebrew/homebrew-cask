3898920ceb8ca6873dbeb533bc9bb74fab76886a
nthumann <me@n-thumann.de>
2021-09-14 18:07:26 +0200

    Change bitcoin-core URL to signed DMG

--- Casks/bitcoin-core.rb
+++ Casks/bitcoin-core.rb
@@ -2,7 +2,7 @@ cask "bitcoin-core" do
   version "0.21.1"
   sha256 "2df15131cd18fd1941adc26f014012b437ccaadab39f1f5dc10282a68e8f9923"
 
-  url "https://bitcoincore.org/bin/bitcoin-core-#{version}/bitcoin-#{version}-osx.dmg"
+  url "https://bitcoincore.org/bin/bitcoin-core-#{version}/bitcoin-#{version}-osx-signed.dmg"
   name "Bitcoin Core"
   desc "Bitcoin client and wallet"
   homepage "https://bitcoincore.org/"
