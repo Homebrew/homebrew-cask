3564019b3340d04ac9717799c5dafa8592660481
Roman Ternavskyi <rternavskyi@gmail.com>
2021-09-15 15:57:24 +0300

    add major_minor method

--- Casks/maxon.rb
+++ Casks/maxon.rb
@@ -1,8 +1,8 @@
 cask "maxon" do
-  version "2.0"
+  version "2.0.0"
   sha256 "b45c6c593c942c99c5f1554cb9f6bf9efef7741773d4b2d17a6832135b4c45d1"
 
-  url "https://installer.maxon.net/installer/RG_installers/Maxon_App_#{version}_Mac.dmg"
+  url "https://installer.maxon.net/installer/RG_installers/Maxon_App_#{version.major_minor}_Mac.dmg"
   name "Maxon App"
   desc "Install, use, and try Maxon products"
   homepage "https://www.maxon.net/en/downloads/"
