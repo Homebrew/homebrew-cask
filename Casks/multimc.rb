58cebafd5e631887f185392323360a1ee24614d0
Bevan Kay <email@bevankay.me>
2021-09-15 12:21:27 +1000

    multimc: update url, add depends_on (#111253)
    * multimc: update url, add depends_on
    
    * Update Casks/multimc.rb
    
    Co-authored-by: haykam821 <24855774+haykam821@users.noreply.github.com>
    
    Co-authored-by: haykam821 <24855774+haykam821@users.noreply.github.com>

--- Casks/multimc.rb
+++ Casks/multimc.rb
@@ -2,7 +2,7 @@ cask "multimc" do
   version "0.6.13"
   sha256 :no_check
 
-  url "https://files.multimc.org/downloads/mmc-stable-osx64.tar.gz"
+  url "https://files.multimc.org/downloads/mmc-stable-osx64-5.15.2.tar.gz"
   name "Multi MC"
   desc "Minecraft launcher"
   homepage "https://multimc.org/"
@@ -15,6 +15,7 @@ cask "multimc" do
   end
 
   auto_updates true
+  depends_on macos: ">= :high_sierra"
 
   app "MultiMC.app"
 
