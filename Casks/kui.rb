0b63ce864a967ca4bb097578928aa5196791a711
Nick Mitchell <starpit@users.noreply.github.com>
2021-09-15 10:53:30 -0400

    bump kui from 10.5.2 to 10.5.4 (#111336)
    * bump kui from 10.5.2 to 10.5.4
    
    This PR also updates the homepage, and removes the now-superfluous verified bit
    
    * Update kui.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/kui.rb
+++ Casks/kui.rb
@@ -1,26 +1,19 @@
 cask "kui" do
-  version "10.5.2"
+  version "10.5.4"
 
   if Hardware::CPU.intel?
-    url "https://github.com/kubernetes-sigs/kui/releases/download/v#{version}/Kui-darwin-x64.tar.bz2",
-        verified: "github.com/kubernetes-sigs/kui/"
-    sha256 "3e55829a1cd1deed292c3129fc29f536a961f41353a638faf2f72b365854d2ec"
+    url "https://github.com/kubernetes-sigs/kui/releases/download/v#{version}/Kui-darwin-x64.tar.bz2"
+    sha256 "4e3d43945fac6c25cc3c51fafdf8eed4b1c6454226907ddf5ebe0d96077acf4f"
     app "Kui-darwin-x64/Kui.app"
   else
-    url "https://github.com/kubernetes-sigs/kui/releases/download/v#{version}/Kui-darwin-arm64.tar.bz2",
-        verified: "github.com/kubernetes-sigs/kui/"
-    sha256 "a21bc0685e7c18f428afaca4b6c03ff50abe240d5fdc06272b3642525a85665b"
+    url "https://github.com/kubernetes-sigs/kui/releases/download/v#{version}/Kui-darwin-arm64.tar.bz2"
+    sha256 "9542ea0585b299be2505de1b7a690ab8b4800f7a433df5e0226c7e8a5dc61607"
     app "Kui-darwin-arm64/Kui.app"
   end
 
   name "Kui"
   desc "Hybrid command-line/UI development experience for cloud-native development"
-  homepage "https://kui.tools/"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
+  homepage "https://github.com/kubernetes-sigs/kui"
 
   binary "#{appdir}/Kui.app/Contents/Resources/kubectl-kui"
 
