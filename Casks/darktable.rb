1b9a816c1ed668fb1fbf1211385ffa837eb08d31
Bevan Kay <email@bevankay.me>
2021-09-16 19:39:13 +1000

    Update darktable from 3.6.0.3 to 3.6.1.5 (#111370)
    * Update darktable from 3.6.0.3 to 3.6.1.5
    
    * Update darktable.rb

--- Casks/darktable.rb
+++ Casks/darktable.rb
@@ -1,6 +1,6 @@
 cask "darktable" do
-  version "3.6.0.3"
-  sha256 "12c9319d68dac5b1b5602bf470254467b90f5c5b9d2e93ea081d42884cfd4f80"
+  version "3.6.1.5"
+  sha256 "0fa0cd49971fdf76f690be095600250ad7777c2317f969c7135f035bc7b5f0ee"
 
   url "https://github.com/darktable-org/darktable/releases/download/release-#{version.major_minor_patch}/darktable-#{version}.dmg",
       verified: "github.com/darktable-org/darktable/"
@@ -13,8 +13,6 @@ cask "darktable" do
     strategy :github_latest
     regex(/href=.*?darktable[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
-
-  conflicts_with cask: "homebrew/cask-versions/darktable-dev"
 
   app "darktable.app"
 end
