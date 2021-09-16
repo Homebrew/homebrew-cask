a9911985a797d30db053ad692b9e1d4dd93d90a6
Miccal Matthews <miccal.matthews@gmail.com>
2021-09-16 21:12:43 +0800

    Update remix-ide.rb

--- Casks/remix-ide.rb
+++ Casks/remix-ide.rb
@@ -4,20 +4,14 @@ cask "remix-ide" do
 
   url "https://github.com/ethereum/remix-desktop/releases/download/v#{version}/Remix-IDE-#{version}.dmg",
       verified: "github.com/ethereum/remix-desktop/"
-  name "remix-ide"
+  name "Remix IDE desktop"
   desc "Desktop version of Remix web IDE used for Ethereum smart contract development"
-  homepage "https://remix-project.org/"
-
-  livecheck do
-    url "https://github.com/ethereum/remix-desktop/releases/download"
-    strategy :page_match
-    regex(%r{href=.*?/Remix-IDE-(\d+(?:\.\d+)*)\.zip}i)
-  end
+  homepage "https://remix-project.org/"\
 
   app "Remix IDE.app"
 
   zap trash: [
-    "/Library/Preferences/org.ethereum.remix-ide.plist",
+    "~/Library/Preferences/org.ethereum.remix-ide.plist",
     "~/Library/Saved Application State/org.ethereum.remix-ide.savedState",
   ]
 end
