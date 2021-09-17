ae7f9ebf47b2ce50828e472aa5500d4a77c29082
River <46098567+itsRiver@users.noreply.github.com>
2021-09-18 03:36:43 +0800

    Update one-switch from 1.21,322 to 1.21.1,324

--- Casks/one-switch.rb
+++ Casks/one-switch.rb
@@ -1,6 +1,6 @@
 cask "one-switch" do
-  version "1.21,322"
-  sha256 "6618e0cbcdc4dd6dc32703303a176aac919f9b3c840938df99d1c7241904ccb9"
+  version "1.21.1,324"
+  sha256 "de71a497b276a319c13fc17f788ec9fe24a73a0d71e60cc218e2488888f7a357"
 
   url "https://fireball.studio/api/release_manager/downloads/studio.fireball.OneSwitch/#{version.after_comma}.zip"
   name "One Switch"
