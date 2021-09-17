82597280d8aa3d2b6ff347ca281cd5c49937959a
Bevan Kay <email@bevankay.me>
2021-09-17 22:48:39 +1000

    Update metasploit from 6.1.6,20210912102848 to 6.1.7,20210917102824 (#111430)

--- Casks/metasploit.rb
+++ Casks/metasploit.rb
@@ -1,6 +1,6 @@
 cask "metasploit" do
-  version "6.1.6,20210912102848"
-  sha256 "af57397aedde9bad10742da1f9fefb3f0b75e3cb144df8fc4eb048a91525140e"
+  version "6.1.7,20210917102824"
+  sha256 "3c57a85a12159596e6016585b8a7cff69dcf09e39151ac4b83f93c10caaf21b1"
 
   url "https://osx.metasploit.com/metasploit-framework-#{version.before_comma}%2B#{version.after_comma}-1rapid7-1.pkg"
   name "Metasploit Framework"
