043a912a7c22f917c3b4a9c976ca8112d9e85bfb
Alexander Hamann <ah0703@gmail.com>
2021-09-17 23:14:02 +0200

    Update foxmail from 1.5.4.94534 to 1.5.4.94544

--- Casks/foxmail.rb
+++ Casks/foxmail.rb
@@ -1,6 +1,6 @@
 cask "foxmail" do
-  version "1.5.4.94534"
-  sha256 "53b109bb479d13a3e53d668c18f78095d1780b26c38baea40da4d13e4ef75a5f"
+  version "1.5.4.94544"
+  sha256 "1bf3d95357cb98b9c163b8c63d78a9d7e6e9bd65085b7bf7a6bca0835fff0749"
 
   url "https://dldir1.qq.com/foxmail/MacFoxmail/Foxmail_for_Mac_#{version}.dmg",
       verified: "dldir1.qq.com/foxmail/MacFoxmail/"
