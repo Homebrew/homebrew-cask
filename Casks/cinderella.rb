d5d275683143ffd1cee0a5338450e5bb974a77e4
kortenkamp <kortenkamp@cinderella.de>
2021-09-18 02:26:42 +0200

    Update cinderella from 3.0b.2035 to 3.0b.2036 (#111433)

--- Casks/cinderella.rb
+++ Casks/cinderella.rb
@@ -1,6 +1,6 @@
 cask "cinderella" do
-  version "3.0b.2035"
-  sha256 "f7b33bcc1c412fc159f4510e367d556b33fe1fba7576181767f605ed7213ce8b"
+  version "3.0b.2036"
+  sha256 "73fbc901db7b0b3039e24e5625311b2ae813ee026074b51140358da1adfb7ce1"
 
   url "https://beta.cinderella.de/Cinderella-#{version}.dmg"
   name "Cinderella"
