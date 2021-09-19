2077e753139e1e5f6acf9d76a2cdeaab13b676f9
Bevan Kay <email@bevankay.me>
2021-09-20 09:21:27 +1000

    Update hot.rb

--- Casks/hot.rb
+++ Casks/hot.rb
@@ -1,6 +1,6 @@
 cask "hot" do
   version "1.4.0"
-  sha256 "604e3c125805ccb2006e040d06fbcb29b7e6b6957ce78832bf9533b6d25dc00a"
+  sha256 "9ddcd3fa98ae754d91ab7260c32663d7fe1bd25bba9ca5dc179e0e72ee1cba1f"
 
   url "https://github.com/macmade/Hot/releases/download/#{version}/Hot.app.zip"
   name "Hot"
