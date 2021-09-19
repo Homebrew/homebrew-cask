39667739c32fc5a09b89176b4f177e1f1f2d7de6
Bevan Kay <email@bevankay.me>
2021-09-19 22:43:57 +1000

    Update lens from 5.2.2-latest.20210917.1 to 5.2.2-latest.20210918.1 (#111481)

--- Casks/lens.rb
+++ Casks/lens.rb
@@ -1,11 +1,11 @@
 cask "lens" do
-  version "5.2.2-latest.20210917.1"
+  version "5.2.2-latest.20210918.1"
 
   if Hardware::CPU.intel?
-    sha256 "ef2b849b95ef68a0245ded4f39eb09ec4735e7565da9a38d9e1deea05560379d"
+    sha256 "c857d61d2963461dd759d132bfded30cc8bee732410e3d0746b4b3693367fe4a"
     url "https://api.k8slens.dev/binaries/Lens-#{version}.dmg"
   else
-    sha256 "e8d159222da3c6c70cd163b32a09152255c56e64ba11faa417a1c5dc8d7f850e"
+    sha256 "c9f09ef6d527afc0d158e31172b678be2b693b6f843b9c9c2f0bd1c559bd9126"
     url "https://api.k8slens.dev/binaries/Lens-#{version}-arm64.dmg"
   end
 
