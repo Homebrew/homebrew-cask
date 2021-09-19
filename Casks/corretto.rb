13a3c0ab52fcc606890515102da1cb733f1b393f
Miccal Matthews <miccal.matthews@gmail.com>
2021-09-19 08:24:43 +0800

    Update corretto.rb

--- Casks/corretto.rb
+++ Casks/corretto.rb
@@ -3,29 +3,37 @@ cask "corretto" do
 
   if Hardware::CPU.intel?
     sha256 "9363489f7fef6e109a977225e72dd5063d5e5dc5b1466f17f875eca4db8f4bae"
+
+    url "https://corretto.aws/downloads/resources/#{version.sub(/-\d+/, "")}/amazon-corretto-#{version}-macosx-x64.pkg"
+
+    livecheck do
+      url "https://corretto.aws/downloads/latest/amazon-corretto-#{version.major}-x64-macos-jdk.pkg"
+      strategy :header_match do |headers|
+        headers["location"][%r{/amazon-corretto-(\d+(?:\.\d+)*)-macosx-x64\.pkg}i, 1]
+      end
+    end
   else
     sha256 "64083bd91f60bb9481c61c9484aacbd59d6776bb5fb3229d3ea2c6a2236d0a8b"
+
+    url "https://corretto.aws/downloads/resources/#{version.sub(/-\d+/, "")}/amazon-corretto-#{version}-macosx-aarch64.pkg"
+
+    livecheck do
+      url "https://corretto.aws/downloads/latest/amazon-corretto-#{version.major}-aarch64-macos-jdk.pkg"
+      strategy :header_match do |headers|
+        headers["location"][%r{/amazon-corretto-(\d+(?:\.\d+)*)-macosx-aarch64\.pkg}i, 1]
+      end
+    end
   end
 
-  arch_string = if Hardware::CPU.intel?
-    "x64"
-  else
-    "aarch64"
-  end
-
-  url "https://corretto.aws/downloads/resources/#{version.sub(/-\d+/, "")}/amazon-corretto-#{version}-macosx-#{arch_string}.pkg"
   name "AWS Corretto JDK"
   desc "OpenJDK distribution from Amazon"
   homepage "https://corretto.aws/"
 
-  livecheck do
-    url "https://corretto.aws/downloads/latest/amazon-corretto-#{version.major}-#{arch_string}-macos-jdk.pkg"
-    strategy :header_match do |headers|
-      headers["location"][%r{/amazon-corretto-(\d+(?:\.\d+)*)-macosx-#{arch_string}\.pkg}i, 1]
-    end
+  if Hardware::CPU.intel?
+    pkg "amazon-corretto-#{version}-macosx-x64.pkg"
+  else
+    pkg "amazon-corretto-#{version}-macosx-aarch64.pkg"
   end
-
-  pkg "amazon-corretto-#{version}-macosx-#{arch_string}.pkg"
 
   uninstall pkgutil: "com.amazon.corretto.#{version.major}"
 end
