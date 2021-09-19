44c0a9b82c869290eaf0e705706381a9c7ff2182
Zach Heineman <zach@heineman.info>
2021-09-19 10:27:09 -0700

    Update Loom version
    Not sure which other fields need adjusting (or how to do it).

--- Casks/loom.rb
+++ Casks/loom.rb
@@ -1,5 +1,5 @@
 cask "loom" do
-  version "0.98.0"
+  version "0.99.8"
   sha256 "e6ba7eefb59b8a595c26bf30a4bb3ca204970e95c2cfc3d8703edf8c4297fae6"
 
   url "https://cdn.loom.com/desktop-packages/Loom-#{version}.dmg"
