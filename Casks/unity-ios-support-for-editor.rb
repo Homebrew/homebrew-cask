88a5d815bc4f60e82a0c4bdcfe56393e3279176d
Miccal Matthews <miccal.matthews@gmail.com>
2021-09-20 09:20:40 +0800

    Update unity-ios-support-for-editor from 2021.1.20f1,be552157821d to 2021.1.21f1,f2d5d3c59f8c (#111497)

--- Casks/unity-ios-support-for-editor.rb
+++ Casks/unity-ios-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask "unity-ios-support-for-editor" do
-  version "2021.1.20f1,be552157821d"
-  sha256 "9d789114ce5a6169537029a26154b74c1b90e711f61828660d8156e23bb1d677"
+  version "2021.1.21f1,f2d5d3c59f8c"
+  sha256 "c22bfa05cc6ac0edf1f69357c630a95edcc7d7b78bee54ec669c79322142ba18"
 
   url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-#{version.before_comma}.pkg",
       verified: "download.unity3d.com/download_unity/"
