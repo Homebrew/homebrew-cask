e863b6b8d3a629454d631dba14761284ca5d88d7
Miccal Matthews <miccal.matthews@gmail.com>
2021-09-20 15:17:36 +0800

    Update yuque.rb (#111502)

--- Casks/yuque.rb
+++ Casks/yuque.rb
@@ -1,17 +1,28 @@
 cask "yuque" do
-  version "0.9.29"
-  sha256 "6ca1e4fc924b82e8cdd0e127e64ff25adeede6cb65089efaf6921b8cdf463136"
+  version "0.9.22"
 
-  url "https://app.nlark.com/yuque-desktop/Yuque-#{version}.dmg",
-      verified: "app.nlark.com/yuque-desktop/"
+  if Hardware::CPU.intel?
+    sha256 "386fc9bb46e47a6e31b3199d49f239f4dd2506db56074d3d14a3f25824d0dd69"
+
+    url "https://app.nlark.com/yuque-desktop/Yuque-#{version}.dmg",
+        verified: "app.nlark.com/yuque-desktop/"
+  else
+    sha256 "edef6078a07a37c479c334459d5f61187ef53e0969e37a42c6963a96779348a0"
+
+    url "https://app.nlark.com/yuque-desktop/arm64/Yuque-#{version}.dmg",
+        verified: "app.nlark.com/yuque-desktop/arm64/"
+  end
+
   name "Yuque"
   name "语雀"
   desc "Cloud knowledge base"
   homepage "https://www.yuque.com/"
 
+  # The stable version is that listed on the download page. See:
+  #   https://github.com/Homebrew/homebrew-cask/pull/111472
   livecheck do
-    url "https://www.yuque.com/yuque/yuque-desktop/changelog"
-    regex(/下载地址(\d+(?:\.\d+)*)/i)
+    url "https://www.yuque.com/install/desktop"
+    regex(/desktopDownloadVersion%22%3A%22(\d+(?:\.\d+)*)/i)
   end
 
   app "语雀.app"
