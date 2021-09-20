ea66c0703f1a348a4dfd543855942539e66641ed
i0ntempest <szf1234@me.com>
2021-09-20 05:49:07 -0400

    Update maintenance from 2.7.8 to 2.7.9

--- Casks/maintenance.rb
+++ Casks/maintenance.rb
@@ -18,8 +18,8 @@ cask "maintenance" do
     version "2.7.1"
     sha256 "833e658f862f0a58dc6a073c70a67bed071b835167f73fc24e80386a36bfd38b"
   else
-    version "2.7.8"
-    sha256 "fe33f99802e97fa83493fd7882ca146f2b3e38e2c27d93d6e20c7e7f640b56b6"
+    version "2.7.9"
+    sha256 "b8f75093c85e9983b808a59e0ffb12a7f5a28cd8967ae43cc0c810769bdfcf81"
   end
 
   url "https://www.titanium-software.fr/download/#{MacOS.version.to_s.delete(".")}/Maintenance.dmg"
