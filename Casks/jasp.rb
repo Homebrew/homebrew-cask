85dc3339bcdec9a4c6182746d5d05fd053b6be91
Bevan Kay <email@bevankay.me>
2021-09-22 09:34:36 +1000

    jasp: add livecheck, add zap (#111577)

--- Casks/jasp.rb
+++ Casks/jasp.rb
@@ -3,12 +3,24 @@ cask "jasp" do
   sha256 "df1b186c23c739654d4af67ab225aa09a446e165efb2a70a9fee474506efadf9"
 
   url "https://static.jasp-stats.org/JASP-#{version}.dmg"
-  appcast "https://jasp-stats.org/download/",
-          must_contain: version.sub(/(.0)+$/, "")
   name "JASP"
+  desc "Statistical analysis application"
   homepage "https://jasp-stats.org/"
+
+  livecheck do
+    url "https://jasp-stats.org/thank-you-for-downloading-jasp-macos/"
+    regex(/href=.*?JASP[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+  end
 
   depends_on macos: ">= :high_sierra"
 
   app "JASP.app"
+
+  zap trash: [
+    "~/.JASP",
+    "~/Library/Application Support/JASP",
+    "~/Library/Caches/JASP",
+    "~/Library/Preferences/org.jasp-stats.JASP.plist",
+    "~/Library/Saved Application State/org.jasp-stats.jasp.savedState",
+  ]
 end
