4de63b5bb50eb27027277f45cc36978041c56851
Thomas J. Faulkenberry <faulkenberry@tarleton.edu>
2021-09-21 18:09:19 -0500

    Update JASP to version 0.15.0.0 (#111568)
    * Update JASP to version 0.15.0.0
    
    Note: beginning with version 0.15.0.0, JASP no longer has separate download for pre-Catalina
    
    * fix style offences
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/jasp.rb
+++ Casks/jasp.rb
@@ -1,15 +1,8 @@
 cask "jasp" do
-  version "0.14.1.0"
+  version "0.15.0.0"
+  sha256 "df1b186c23c739654d4af67ab225aa09a446e165efb2a70a9fee474506efadf9"
 
-  if MacOS.version <= :mojave
-    sha256 "8fe3cf08058576e54c78e1d87a2cfe159dead894c26aca37fd6fa610a5fb3cf7"
-
-    url "https://static.jasp-stats.org/JASP-#{version}-preCatalina.dmg"
-  else
-    sha256 "ea682a95e51f9f0cd3d92b49eac18831c38de1e03f90020321cfce3eba5a0ee6"
-
-    url "https://static.jasp-stats.org/JASP-#{version}-postMojave.dmg"
-  end
+  url "https://static.jasp-stats.org/JASP-#{version}.dmg"
   appcast "https://jasp-stats.org/download/",
           must_contain: version.sub(/(.0)+$/, "")
   name "JASP"
