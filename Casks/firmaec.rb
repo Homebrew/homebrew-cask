d17721c149c63004a273d31dc7a7111dfb960aff
Serge Bibauw <sbibauw@gmail.com>
2021-09-21 13:07:22 -0500

    Add FirmaEC.app v2.10.0

---
+++ Casks/firmaec.rb
@@ -0,0 +1,24 @@
+cask "firmaec" do
+  version "2.10.0"
+  sha256 :no_check
+
+  url "https://www.firmadigital.gob.ec/firmaec/FirmaEC.zip"
+  name "FirmaEC"
+  desc "Firma electrónica en Ecuador"
+  homepage "https://www.firmadigital.gob.ec"
+
+  livecheck do
+    url "https://www.firmadigital.gob.ec/registro-de-cambios-de-firmaecchangelog/"
+    regex(/Versión (\d+(?:\.\d+)+) \(/i)
+  end
+
+  container type: :zip
+
+  app "FirmaEC/FirmaEC.app"
+
+  zap trash: [
+    "~/firmadigital.log",
+    "~/firmadigital.log.lck",
+    "~/firmadigital.properties"
+  ]
+end
