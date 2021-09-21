140c7c0e247bcf8d9feb24ec1d481f637b8d8d3e
Bevan Kay <email@bevankay.me>
2021-09-22 08:58:33 +1000

    fix style offences

--- Casks/jasp.rb
+++ Casks/jasp.rb
@@ -1,10 +1,8 @@
 cask "jasp" do
   version "0.15.0.0"
-  
   sha256 "df1b186c23c739654d4af67ab225aa09a446e165efb2a70a9fee474506efadf9"
-  
+
   url "https://static.jasp-stats.org/JASP-#{version}.dmg"
-  
   appcast "https://jasp-stats.org/download/",
           must_contain: version.sub(/(.0)+$/, "")
   name "JASP"
