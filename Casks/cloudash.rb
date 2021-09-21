f461d3ef647f059ecf92be8121b63af2a845c96f
George Boot <884482+georgeboot@users.noreply.github.com>
2021-09-21 22:19:14 +0200

    Add cloudash cask
    Update cloudash.rb

---
+++ Casks/cloudash.rb
@@ -0,0 +1,12 @@
+cask "cloudash" do
+  version "1.0.2"
+  sha256 "b8e72678c88a04bc9def11f2dfe18bc99941a53004802a2a53cec227b82042a9"
+
+  url "https://github.com/cloudashdev/cloudash/releases/download/#{version}/Cloudash-#{version}.dmg",
+      verified: "github.com/cloudashdev/cloudash/"
+  name "cloudash"
+  desc "Monitoring and troubleshooting desktop app for serverless architectures"
+  homepage "https://cloudash.dev/"
+
+  app "Cloudash.app"
+end
