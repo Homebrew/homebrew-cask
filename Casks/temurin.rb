a03522cc514ca5cb87d135157bfde8cc8ed69803
George Adams <georgeadams1995@gmail.com>
2021-09-22 10:41:49 +0100

    update temurin to 17+35 (#111592)
    * update temurin to 17+35
    
    * Update temurin.rb
    
    * Update temurin.rb

--- Casks/temurin.rb
+++ Casks/temurin.rb
@@ -1,8 +1,8 @@
 cask "temurin" do
-  version "16.0.2,7"
-  sha256 "3348e66ae287c70e48935d435b62cc6dec65558f84e6d60ac283dbb78f81ef7d"
+  version "17,35"
+  sha256 "a45c33691f0508a95ff291c88713088e060376e7b4e9cac03d083225b68d8f78"
 
-  url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.before_comma}%2B#{version.after_comma}/OpenJDK#{version.major}U-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma.major}.pkg",
+  url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.before_comma}%2B#{version.after_comma}/OpenJDK#{version.major}-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma.major}.pkg",
       verified: "github.com/adoptium/"
   name "Eclipse Temurin Java Development Kit"
   desc "JDK from the Eclipse Foundation (Adoptium)"
@@ -18,7 +18,7 @@ cask "temurin" do
     end
   end
 
-  pkg "OpenJDK#{version.major}U-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma.major}.pkg"
+  pkg "OpenJDK#{version.major}-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma.major}.pkg"
 
   uninstall pkgutil: "net.temurin.#{version.major}.jdk"
 end
