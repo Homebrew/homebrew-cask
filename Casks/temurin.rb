8cf5a65e017812daa962eef9515cfeb5192ee426
George Adams <georgeadams1995@gmail.com>
2021-09-22 09:04:20 +0100

    Update temurin.rb

--- Casks/temurin.rb
+++ Casks/temurin.rb
@@ -2,7 +2,7 @@ cask "temurin" do
   version "17,35"
   sha256 "a45c33691f0508a95ff291c88713088e060376e7b4e9cac03d083225b68d8f78"
 
-  url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.before_comma}%2B#{version.after_comma}/OpenJDK#{version.major}U-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma.major}.pkg",
+  url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.before_comma}%2B#{version.after_comma}/OpenJDK#{version.major}-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma.major}.pkg",
       verified: "github.com/adoptium/"
   name "Eclipse Temurin Java Development Kit"
   desc "JDK from the Eclipse Foundation (Adoptium)"
