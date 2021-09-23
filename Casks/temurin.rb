88d742eab59dc8ed2e2f90c1c7a46bfb66cd857f
Sean Molenaar <SMillerDev@users.noreply.github.com>
2021-09-23 17:01:18 +0200

    temurin: simplify arch settings

--- Casks/temurin.rb
+++ Casks/temurin.rb
@@ -1,16 +1,14 @@
 cask "temurin" do
+  arch = Hardware::CPU.intel? ? "x64" : "aarch64"
+  
   version "17,35"
-
+  url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.before_comma}%2B#{version.after_comma}/OpenJDK#{version.major}-jdk_#{arch}_mac_hotspot_#{version.before_comma}_#{version.after_comma.major}.pkg",
+        verified: "github.com/adoptium/"
+  
   if Hardware::CPU.intel?
     sha256 "a45c33691f0508a95ff291c88713088e060376e7b4e9cac03d083225b68d8f78"
-
-    url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.before_comma}%2B#{version.after_comma}/OpenJDK#{version.major}-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma.major}.pkg",
-        verified: "github.com/adoptium/"
   else
     sha256 "b0f779a7acf252a0838d9b4a423726d80cd6930713cfc517d2296fa36e4c95d4"
-
-    url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.before_comma}%2B#{version.after_comma}/OpenJDK#{version.major}-jdk_aarch64_mac_hotspot_#{version.before_comma}_#{version.after_comma.major}.pkg",
-        verified: "github.com/adoptium/"
   end
 
   name "Eclipse Temurin Java Development Kit"
@@ -27,11 +25,7 @@ cask "temurin" do
     end
   end
 
-  if Hardware::CPU.intel?
-    pkg "OpenJDK#{version.major}-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma.major}.pkg"
-  else
-    pkg "OpenJDK#{version.major}-jdk_aarch64_mac_hotspot_#{version.before_comma}_#{version.after_comma.major}.pkg"
-  end
+  pkg "OpenJDK#{version.major}-jdk_#{arch}_mac_hotspot_#{version.before_comma}_#{version.after_comma.major}.pkg"
 
   uninstall pkgutil: "net.temurin.#{version.major}.jdk"
 end
