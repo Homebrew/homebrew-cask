b05524601b397331ca0dd8133eb93c40e4221e85
Bevan Kay <email@bevankay.me>
2021-09-24 00:17:42 +1000

    Update flipper from 0.111.0 to 0.112.0 (#111648)

--- Casks/flipper.rb
+++ Casks/flipper.rb
@@ -1,6 +1,6 @@
 cask "flipper" do
-  version "0.111.0"
-  sha256 "8451ca0cf2b1f16a1ecc4a6154a34ec83afa5dd22a857072234b20e92a53da56"
+  version "0.112.0"
+  sha256 "19be9bf7736a2481b17a14218397b5c19cbf666668580b0e36027de810d1cd24"
 
   url "https://github.com/facebook/flipper/releases/download/v#{version}/Flipper-mac.dmg",
       verified: "github.com/facebook/flipper/"
