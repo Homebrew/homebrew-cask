b0c148e721e52bfac8ca32c0d49bfd1ed5a2d306
Reno <25192197+singularitti@users.noreply.github.com>
2021-09-22 20:37:30 -0400

    Update visual-studio-code from 1.60.1 to 1.60.2 (#111615)

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -1,11 +1,11 @@
 cask "visual-studio-code" do
-  version "1.60.1"
+  version "1.60.2"
 
   if Hardware::CPU.intel?
-    sha256 "27b36fa7391fe7053bd161d2d2f579627afee4d8dc63421e2b14f0ce88a257b3"
+    sha256 "40ea0d829a8a7144a074bdf84da7b08b147dd2d3a33b2eca757b85b33b3c1b01"
     url "https://update.code.visualstudio.com/#{version}/darwin/stable"
   else
-    sha256 "b366cfbf425b6434ede85a3450e2899b3c4585b48601b5200eb00a33cdb3a819"
+    sha256 "531855edba11ddcc2bfceec5e63173be080af86dcb9ec8af519d0239097e6093"
     url "https://update.code.visualstudio.com/#{version}/darwin-arm64/stable"
   end
 
