1a8c8dc1c6b7a6145a18169d88d9c474062ec4c6
George Adams <georgeadams1995@gmail.com>
2021-09-23 15:39:09 +0100

    Add aarch64 temurin build

--- Casks/temurin.rb
+++ Casks/temurin.rb
@@ -1,13 +1,18 @@
 cask "temurin" do
   version "17,35"
-  sha256 "a45c33691f0508a95ff291c88713088e060376e7b4e9cac03d083225b68d8f78"
+  
+  if Hardware::CPU.intel?
+    sha256 "a45c33691f0508a95ff291c88713088e060376e7b4e9cac03d083225b68d8f78"
 
-  url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.before_comma}%2B#{version.after_comma}/OpenJDK#{version.major}-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma.major}.pkg",
-      verified: "github.com/adoptium/"
-  name "Eclipse Temurin Java Development Kit"
-  desc "JDK from the Eclipse Foundation (Adoptium)"
-  homepage "https://adoptium.net/"
+    url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.before_comma}%2B#{version.after_comma}/OpenJDK#{version.major}-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma.major}.pkg",
+        verified: "github.com/adoptium/"
+  else
+    sha256 "b0f779a7acf252a0838d9b4a423726d80cd6930713cfc517d2296fa36e4c95d4"
 
+    url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.before_comma}%2B#{version.after_comma}/OpenJDK#{version.major}-jdk_aarch64_mac_hotspot_#{version.before_comma}_#{version.after_comma.major}.pkg",
+        verified: "github.com/adoptium/"
+  end
+  
   livecheck do
     url :url
     strategy :git do |tags|
@@ -17,6 +22,10 @@ cask "temurin" do
       end.compact
     end
   end
+  
+  name "Eclipse Temurin Java Development Kit"
+  desc "JDK from the Eclipse Foundation (Adoptium)"
+  homepage "https://adoptium.net/"
 
   pkg "OpenJDK#{version.major}-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma.major}.pkg"
 
