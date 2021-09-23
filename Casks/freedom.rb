9c8ef4507360a8385a1e2a9c71dc83b17bbaa4a0
Jason Tedor <jason@tedor.me>
2021-09-23 13:13:57 -0400

    Update Casks/freedom.rb

--- Casks/freedom.rb
+++ Casks/freedom.rb
@@ -1,5 +1,5 @@
 cask "freedom" do
-  version "2.5.9,1922.1"
+  version "2.5.9,1922"
   sha256 "2a3a5197f6add7cfd9be4b5ec0a665f4480cc2467149cf2824b04a0c42358f0c"
 
   url "https://cdn.freedom.to/installers/updates/mac/#{version.before_comma}/Freedom.zip"
