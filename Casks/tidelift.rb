c770f7e05d229483fd88725852a6b79dead9095c
Tyrel Souza <tyrelsouza@gmail.com>
2021-09-23 20:04:54 -0400

    Add tidelift CLI v1.0.2 (#111569)
    * Add tidelift CLI v1.0.0
    
    Initial addition of Tidelift CLI version 1.0.0
    
    * Update tidelift.rb
    
    * Update version in PR
    
    tidelift version 1.0.2 disables selfupdate as per https://docs.brew.sh/Acceptable-Formulae#we-dont-like-tools-that-upgrade-themselves
    
    * Version 1.0.2 not 1.0.1

---
+++ Casks/tidelift.rb
@@ -0,0 +1,20 @@
+cask "tidelift" do
+  version "1.0.2"
+  sha256 "76db78ec88177bad5822eab22eb0c058bec2f61f1816876d94092de0bba42201"
+
+  url "https://download.tidelift.com/cli/#{version}/darwin_homebrew/tidelift"
+  name "Tidelift CLI"
+  desc "Tool to interact with the Tidelift system"
+  homepage "https://tidelift.com/cli"
+
+  livecheck do
+    url "https://download.tidelift.com/cli/index.html"
+    regex(%r{href=.*?/cli/(\d+(?:\.\d+)+)/darwin/tidelift}i)
+  end
+
+  binary "tidelift"
+
+  postflight do
+    set_permissions "#{staged_path}/tidelift", "+x"
+  end
+end
