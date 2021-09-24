c6e44289c3293f53b7492ebb680d82b7f411775f
Rod Begbie <rod@begbie.com>
2021-09-24 02:33:37 +0200

    Add Dropbox Capture - https://dropbox.com/capture (#111659)
    * Add Dropbox Capture - https://dropbox.com/capture
    
    * add zap stanza
    
    * Update dropbox-capture.rb
    
    Co-authored-by: Rod Begbie <rod@Rods-MacBook-Air.local>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/dropbox-capture.rb
@@ -0,0 +1,26 @@
+cask "dropbox-capture" do
+  version "51.0.0"
+  sha256 :no_check
+
+  url "https://clientupdates.dropboxstatic.com/dbx-releng/dropbox_capture/mac/Dropbox_Capture.dmg",
+      verified: "clientupdates.dropboxstatic.com/dbx-releng/dropbox_capture/mac/"
+  name "Dropbox Capture"
+  desc "Share your work and ideas with video messages and screenshots"
+  homepage "https://dropbox.com/capture/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
+
+  app "Dropbox Capture.app"
+
+  zap trash: [
+    "~/Library/Application Support/Dropbox-Capture",
+    "~/Library/Caches/Dropbox-Capture",
+    "~/Library/Caches/com.electron.dropbox-capture",
+    "~/Library/Caches/com.electron.dropbox-capture.ShipIt",
+    "~/Library/Logs/Dropbox-Capture",
+    "~/Library/Preferences/com.electron.dropbox-capture.plist",
+  ]
+end
