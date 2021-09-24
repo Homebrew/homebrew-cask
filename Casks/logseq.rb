24b538bfb039e3655fa43a2183eb62ce818d7c56
Miccal Matthews <miccal.matthews@gmail.com>
2021-09-25 04:55:06 +0800

    Update logseq.rb

--- Casks/logseq.rb
+++ Casks/logseq.rb
@@ -5,18 +5,13 @@ cask "logseq" do
     sha256 "67d3a85847f3e23a7c3b25dfc057b6cfe8cad35a9e47b4254cd3368caafe7e9e"
     url "https://github.com/logseq/logseq/releases/download/#{version}/logseq-darwin-x64-#{version}.dmg"
   else
-    sha256 "53aecf32469e223900c2a5a8c470ec30dc3174e764088b8bae356d20f93ed6cf"
+    sha256 "4ef12ed8a9f60a09f3cb12e889c7cd7c9c876f90fd0693feee3e9ecea5d3302a"
     url "https://github.com/logseq/logseq/releases/download/#{version}/logseq-darwin-arm64-#{version}.dmg"
   end
 
   name "Logseq"
   desc "Privacy-first, open-source platform for knowledge sharing and management"
   homepage "https://github.com/logseq/logseq"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
 
   auto_updates true
 
