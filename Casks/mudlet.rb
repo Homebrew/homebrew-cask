ad7e3edc2e10e6d04f6cb97174a35f34825a1f8e
Mykola Rybak <mykola.rybak@gmail.com>
2021-09-24 19:55:37 +0300

    Update mudlet from 4.12.0 to 4.13.1

--- Casks/mudlet.rb
+++ Casks/mudlet.rb
@@ -1,6 +1,6 @@
 cask "mudlet" do
-  version "4.12.0"
-  sha256 "079cc52457fdeeba273985e94f6cb9e2a4e4a4f8746acb3f92ddb9658c8b8b89"
+  version "4.13.1"
+  sha256 "a96bc99c75bd40f85c0a23d675ab29bddb46d344e56ac6cfc4e4393529f7594a"
 
   url "https://www.mudlet.org/wp-content/files/Mudlet-#{version}.dmg"
   name "Mudlet"
