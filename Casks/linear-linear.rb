ec0515826e08a599c07e84274e5788b3d4775fcb
Ryan Schlesinger <ryan@ryanschlesinger.com>
2021-09-24 10:21:36 -0700

    Update linear-linear from 1.3.4 to 1.4.0

--- Casks/linear-linear.rb
+++ Casks/linear-linear.rb
@@ -1,5 +1,5 @@
 cask "linear-linear" do
-  version "1.3.4"
+  version "1.4.0"
   sha256 :no_check
 
   if Hardware::CPU.intel?
@@ -12,9 +12,18 @@ cask "linear-linear" do
   desc "App to manage software development and track bugs"
   homepage "https://linear.app/"
 
-  livecheck do
-    url "https://desktop.linear.app/mac/dmg/x64"
-    strategy :header_match
+  if Hardware::CPU.intel?
+    livecheck do
+      url "https://desktop.linear.app/mac/dmg/x64"
+      strategy :header_match
+      regex(/Linear (\d+(?:\.\d+)*?)(?:-x64)?.dmg/)
+    end
+  else
+    livecheck do
+      url "https://desktop.linear.app/mac/dmg/arm64"
+      strategy :header_match
+      regex(/Linear (\d+(?:\.\d+)*?)(?:-arm64)?.dmg/)
+    end
   end
 
   auto_updates true
