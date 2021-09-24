f33d31ccfd3a8c979fd5bfaeb25eea93c80425b4
Serge Bibauw <sbibauw@gmail.com>
2021-09-23 19:01:23 -0500

    Add FirmaEC.app v2.10.0 (#111567)
    * Add FirmaEC.app v2.10.0
    
    * fix syntax issues in FirmaEC
    
    * Update firmaec.rb
    
    * Include FirmaECTransversal.app
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/firmaec.rb
@@ -0,0 +1,23 @@
+cask "firmaec" do
+  version "2.10.0"
+  sha256 :no_check
+
+  url "https://www.firmadigital.gob.ec/firmaec/FirmaEC.zip"
+  name "FirmaEC"
+  desc "Sign documents through digital certificates"
+  homepage "https://www.firmadigital.gob.ec/"
+
+  livecheck do
+    url "https://www.firmadigital.gob.ec/registro-de-cambios-de-firmaecchangelog/"
+    regex(/Versión\s*(\d+(?:\.\d+)+)/i)
+  end
+
+  app "FirmaEC/FirmaEC.app"
+  app "FirmaEC/FirmaECTransversal.app"
+
+  zap trash: [
+    "~/firmadigital.log",
+    "~/firmadigital.log.lck",
+    "~/firmadigital.properties",
+  ]
+end
