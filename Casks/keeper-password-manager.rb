8937229b35a6cba2df243b0f327d50cda74ae700
neutric <ah0703@googlemail.com>
2021-09-25 19:22:09 +0200

    Update keeper-password-manager from 16.1.1,202106301833 to 16.2.2,202109241850, add desc

--- Casks/keeper-password-manager.rb
+++ Casks/keeper-password-manager.rb
@@ -1,9 +1,10 @@
 cask "keeper-password-manager" do
-  version "16.1.1,202106301833"
+  version "16.2.2,202109241850"
   sha256 :no_check
 
   url "https://keepersecurity.com/desktop_electron/Darwin/KeeperSetup.dmg"
   name "Keeper Password Manager"
+  desc "Password manager application and digital vault"
   homepage "https://keepersecurity.com/"
 
   livecheck do
