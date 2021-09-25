d28e82425221e603e335fe9b8c36f0806028f08f
Daniel Kean <64756591+danielkean@users.noreply.github.com>
2021-09-25 10:18:20 +0100

    Update bitwarden to use the universal binary on Apple silicon

--- Casks/bitwarden.rb
+++ Casks/bitwarden.rb
@@ -1,9 +1,16 @@
 cask "bitwarden" do
   version "1.28.3"
-  sha256 "19d4845b817b8b5098ac4927446c07adf8461b985e6b18ac3102a0313949f15f"
 
-  url "https://github.com/bitwarden/desktop/releases/download/v#{version}/Bitwarden-#{version}-mac.zip",
-      verified: "github.com/bitwarden/desktop/"
+  if Hardware::CPU.intel?
+    sha256 "19d4845b817b8b5098ac4927446c07adf8461b985e6b18ac3102a0313949f15f"
+    url "https://github.com/bitwarden/desktop/releases/download/v#{version}/Bitwarden-#{version}-mac.zip",
+        verified: "github.com/bitwarden/desktop/"
+  else
+    sha256 "04fdf5f15a046d00f47bdf40483abf73626cb50728256c18ae1397012ceb22b7"
+    url "https://github.com/bitwarden/desktop/releases/download/v#{version}/Bitwarden-#{version}-universal.pkg",
+        verified: "github.com/bitwarden/desktop/"
+  end
+
   name "Bitwarden"
   desc "Desktop password and login vault"
   homepage "https://bitwarden.com/"
