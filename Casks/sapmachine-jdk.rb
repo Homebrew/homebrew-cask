898422b1c48cfb6bfe9b64b82a41ca29595341f4
ryenus <ryenus@gmail.com>
2021-09-25 15:47:19 +0800

    Update sapmachine-jdk from 16.0.2 to 17 (#111730)
    And replace 'osx' with 'macos' in the url.

--- Casks/sapmachine-jdk.rb
+++ Casks/sapmachine-jdk.rb
@@ -1,8 +1,8 @@
 cask "sapmachine-jdk" do
-  version "16.0.2"
-  sha256 "d385e29399a35156ecaa23fbeed08b876c4b9bfcf076640169bde95196ee1d78"
+  version "17"
+  sha256 "0b29168998bf0a72107aca7eff5a1d80a9dacbe5af5cbdbf6026a746ff093dfe"
 
-  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_osx-x64_bin.dmg",
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_macos-x64_bin.dmg",
       verified: "github.com/SAP/SapMachine/"
   name "SapMachine OpenJDK Development Kit"
   desc "OpenJDK release maintained and supported by SAP"
@@ -11,7 +11,7 @@ cask "sapmachine-jdk" do
   livecheck do
     url :url
     strategy :github_latest
-    regex(%r{href=.*/sapmachine-jdk-(\d+(?:\.\d+)*(?:\+\d+(?:\.\d+)*)?)_osx-x64_bin\.dmg}i)
+    regex(%r{href=.*/sapmachine-jdk-(\d+(?:\.\d+)*(?:\+\d+(?:\.\d+)*)?)_macos-x64_bin\.dmg}i)
   end
 
   artifact "sapmachine-jdk-#{version}.jdk", target: "/Library/Java/JavaVirtualMachines/sapmachine-jdk-#{version}.jdk"
