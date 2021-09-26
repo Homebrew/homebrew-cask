3d9dd6ea94fa790896a737b39582ae18e3dbb17d
Daniel Kean <64756591+danielkean@users.noreply.github.com>
2021-09-26 10:10:05 +0100

    Revert "Update bitwarden to use the universal binary on Apple silicon (#111732)" (#111755)

--- Casks/bitwarden.rb
+++ Casks/bitwarden.rb
@@ -1,8 +1,8 @@
 cask "bitwarden" do
   version "1.28.3"
-  sha256 "04fdf5f15a046d00f47bdf40483abf73626cb50728256c18ae1397012ceb22b7"
+  sha256 "19d4845b817b8b5098ac4927446c07adf8461b985e6b18ac3102a0313949f15f"
 
-  url "https://github.com/bitwarden/desktop/releases/download/v#{version}/Bitwarden-#{version}-universal.pkg",
+  url "https://github.com/bitwarden/desktop/releases/download/v#{version}/Bitwarden-#{version}-mac.zip",
       verified: "github.com/bitwarden/desktop/"
   name "Bitwarden"
   desc "Desktop password and login vault"
@@ -15,21 +15,21 @@ cask "bitwarden" do
 
   auto_updates true
 
-  pkg "Bitwarden-#{version}-universal.pkg"
+  app "Bitwarden.app"
 
-  uninstall pkgutil: [
+  uninstall quit: [
     "com.bitwarden.desktop",
     "com.bitwarden.desktop.helper",
   ]
 
   zap trash: [
+    "~/Library/Logs/Bitwarden",
     "~/Library/Application Support/Bitwarden",
     "~/Library/Caches/com.bitwarden.desktop",
     "~/Library/Caches/com.bitwarden.desktop.ShipIt",
-    "~/Library/Logs/Bitwarden",
     "~/Library/Preferences/ByHost/com.bitwarden.desktop.ShipIt.*.plist",
-    "~/Library/Preferences/com.bitwarden.desktop.helper.plist",
     "~/Library/Preferences/com.bitwarden.desktop.plist",
+    "~/Library/Preferences/com.bitwarden.desktop.helper.plist",
     "~/Library/Saved Application State/com.bitwarden.desktop.savedState",
   ]
 end
