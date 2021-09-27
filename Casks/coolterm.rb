677698b2e756f7b547f0e4a4b1b5b3979317b4ed
Jacob Ford <j@cobford.com>
2021-09-27 09:58:03 -0400

    Add coolterm livecheck

--- Casks/coolterm.rb
+++ Casks/coolterm.rb
@@ -7,5 +7,10 @@ cask "coolterm" do
   desc "Serial port terminal"
   homepage "https://freeware.the-meiers.org/"
 
+  livecheck do
+    url "https://freeware.the-meiers.org/#CoolTerm"
+    regex(/<center>([0-9\.]+)<br>05\/31\/2021<\/center>/i)
+  end
+
   app "CoolTermMac/CoolTerm.app"
 end
