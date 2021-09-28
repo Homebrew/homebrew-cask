31a460e6ec522d10fcb2d7be46bfb22601bd8cff
Bevan Kay <email@bevankay.me>
2021-09-28 15:16:15 +1000

    fontstand: update livecheck strategy

--- Casks/fontstand.rb
+++ Casks/fontstand.rb
@@ -1,5 +1,5 @@
 cask "fontstand" do
-  version "2.4.0,105"
+  version "2.4.0"
   sha256 :no_check
 
   url "https://fontstand.com/apps/download/69"
@@ -9,7 +9,13 @@ cask "fontstand" do
 
   livecheck do
     url :url
-    strategy :extract_plist
+    regex(/Fontstand[._-]v?(\d+(?:[.-]\d+)+)\.zip/i)
+    strategy :header_match do |headers, regex|
+      match = headers["content-disposition"].match(regex)[1]
+      next if match.blank?
+
+      match.tr("-",".")
+    end
   end
 
   app "Fontstand.app"
