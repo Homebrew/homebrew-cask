069690f343880f6c81d3ac9ecd7c6425940c235a
Jacob Ford <j@cobford.com>
2021-09-27 21:10:09 -0400

    Better token reference

--- Casks/thetimemachinemechanic.rb
+++ Casks/thetimemachinemechanic.rb
@@ -1,10 +1,9 @@
-cask "t2m2" do
+cask "thetimemachinemechanic" do
   version "1.19,2021.09"
   sha256 "aa41043dd8bff576c27173859ed55766fad947d17f28938f10aa8fc764cfb16c"
 
   url "https://eclecticlightdotcom.files.wordpress.com/#{version.after_comma.major}/#{version.after_comma.minor}/t2m2#{version.before_comma.no_dots}.zip",
       verified: "eclecticlightdotcom.files.wordpress.com/"
-  name "t2m2"
   name "The Time Machine Mechanic"
   desc "Time Machine log viewer & status inspector"
   homepage "https://eclecticlight.co/consolation-t2m2-and-log-utilities/"
