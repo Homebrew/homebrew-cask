fc54a63f5d9074b8dccfc88553a119a433f701c7
Jacob Ford <j@cobford.com>
2021-09-27 21:32:23 -0400

    Add coolterm livecheck (#111787)
    * Add coolterm livecheck
    
    * Brew style
    
    * Fix livecheck
    
    * style fix
    
    * Switch livecheck to app's own source, update version
    
    * Better version regex
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>
    
    * Switch livecheck to HTTPS
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/coolterm.rb
+++ Casks/coolterm.rb
@@ -1,11 +1,16 @@
 cask "coolterm" do
-  version "1.9.0"
+  version "1.9.0.3.1.948"
   sha256 :no_check
 
   url "https://freeware.the-meiers.org/CoolTermMac.zip"
   name "CoolTerm"
   desc "Serial port terminal"
   homepage "https://freeware.the-meiers.org/"
+
+  livecheck do
+    url "https://freeware.the-meiers.org/version/CoolTerm.ver"
+    regex(/^\s*v?(\d+(?:\.\d+)+)\s*$/i)
+  end
 
   app "CoolTermMac/CoolTerm.app"
 end
