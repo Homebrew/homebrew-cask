21cf2cf6aab7026e7d9a87434c369b59949af3dd
Miccal Matthews <miccal.matthews@gmail.com>
2021-09-28 09:15:40 +0800

    Update sapmachine-jdk.rb

--- Casks/sapmachine-jdk.rb
+++ Casks/sapmachine-jdk.rb
@@ -1,19 +1,21 @@
 cask "sapmachine-jdk" do
   version "17"
 
+  if Hardware::CPU.intel?
+    sha256 "0b29168998bf0a72107aca7eff5a1d80a9dacbe5af5cbdbf6026a746ff093dfe"
+
+    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_macos-x64_bin.dmg",
+        verified: "github.com/SAP/SapMachine/"
+  else
+    sha256 "e993513eb8837399507009ebbc8f980de6fd57ce55196a2e102bfd7fb617530f"
+
+    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_macos-aarch64_bin.dmg",
+        verified: "github.com/SAP/SapMachine/"
+  end
+
   name "SapMachine OpenJDK Development Kit"
   desc "OpenJDK distribution from SAP"
   homepage "https://sapmachine.io/"
-
-  if Hardware::CPU.intel?
-    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_macos-x64_bin.dmg",
-        verified: "github.com/SAP/SapMachine/"
-    sha256 "0b29168998bf0a72107aca7eff5a1d80a9dacbe5af5cbdbf6026a746ff093dfe"
-  else
-    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_macos-aarch64_bin.dmg",
-        verified: "github.com/SAP/SapMachine/"
-    sha256 "e993513eb8837399507009ebbc8f980de6fd57ce55196a2e102bfd7fb617530f"
-  end
 
   livecheck do
     url :url
