7ef7736bc199972aeaba2198508e1ab39a356fcc
neutric <ah0703@googlemail.com>
2021-09-29 19:17:18 +0200

    Bump mamp from 6.4 to 6.5

--- Casks/mamp.rb
+++ Casks/mamp.rb
@@ -1,8 +1,14 @@
 cask "mamp" do
-  version "6.4"
-  sha256 "619354e7ca14500352245524de93c41b8ec3ab2d55ad85f62049f0e442f5acc7"
+  version "6.5"
 
-  url "https://downloads.mamp.info/MAMP-PRO/releases/#{version}/MAMP_MAMP_PRO_#{version}.pkg"
+  if Hardware::CPU.intel?
+    sha256 "957894db1b8ab3406f1c8248b89392807158ccec349bef8f2f56111052a008df"
+    url "https://downloads.mamp.info/MAMP-PRO/releases/#{version}/MAMP_MAMP_PRO_#{version}-Intel-x86.pkg"
+  else
+    sha256 "82bea0c53849e8d2a73900bcc2bb108d1775e58b6ab18380d1370a61c4c58a56"
+    url "https://downloads.mamp.info/MAMP-PRO/releases/#{version}/MAMP_MAMP_PRO_#{version}-M1-arm.pkg"
+  end
+
   name "MAMP"
   desc "Web development solution with Apache, Nginx, PHP & MySQL"
   homepage "https://www.mamp.info/"
@@ -10,13 +16,17 @@ cask "mamp" do
   livecheck do
     url "https://www.mamp.info/en/downloads/"
     strategy :page_match
-    regex(%r{href=.*?/MAMP_MAMP_PRO_(\d+(?:\.\d+)*)\.pkg}i)
+    regex(%r{href=.*?/MAMP_MAMP_PRO_(\d+(?:\.\d+)*).*\.pkg}i)
   end
 
   auto_updates true
-  depends_on macos: ">= :yosemite"
+  depends_on macos: ">= :sierra"
 
-  pkg "MAMP_MAMP_PRO_#{version}.pkg"
+  if Hardware::CPU.intel?
+    pkg "MAMP_MAMP_PRO_#{version}-Intel-x86.pkg"
+  else
+    pkg "MAMP_MAMP_PRO_#{version}-M1-arm.pkg"
+  end
 
   postflight do
     set_ownership ["/Applications/MAMP", "/Applications/MAMP PRO"]
