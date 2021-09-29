d906942748985cbbe8d20e1aa0002b76fb27a105
Bevan Kay <email@bevankay.me>
2021-09-29 23:53:15 +1000

    Update drawio from 15.3.7 to 15.3.8

--- Casks/drawio.rb
+++ Casks/drawio.rb
@@ -1,12 +1,12 @@
 cask "drawio" do
-  version "15.3.7"
+  version "15.3.8"
 
   if Hardware::CPU.intel?
-    sha256 "d2d40b21956cdcab0fed0ddf9e432436dc1c68935c509463a4eac67756049239"
+    sha256 "b7f4e5c39a1cd17a4edd86e1116946148530e295a6b71fae09d46be9d0ab2902"
     url "https://github.com/jgraph/drawio-desktop/releases/download/v#{version}/draw.io-x64-#{version}.dmg",
         verified: "github.com/jgraph/drawio-desktop/"
   else
-    sha256 "cc0a0e818febfe53803d7b3457e79494b5a17329e8212594dae4941bee1d07e0"
+    sha256 "6ac97d8e00378c73b509bf012ba3274b16fd8ddcb16d440433ed8fa841db0e53"
     url "https://github.com/jgraph/drawio-desktop/releases/download/v#{version}/draw.io-arm64-#{version}.dmg",
         verified: "github.com/jgraph/drawio-desktop/"
   end
