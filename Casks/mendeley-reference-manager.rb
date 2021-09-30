d82e2ca81bb432a91c809242351e6819dfd52c63
neutric <ah0703@googlemail.com>
2021-09-30 10:42:51 +0200

    Update mendeley-reference-manager from 2.56.1 to 2.58.0

--- Casks/mendeley-reference-manager.rb
+++ Casks/mendeley-reference-manager.rb
@@ -1,6 +1,6 @@
 cask "mendeley-reference-manager" do
-  version "2.56.1"
-  sha256 "1c7752dc0596e9a834960559aecfcad866b38cd09332634f32097b2600740546"
+  version "2.58.0"
+  sha256 "52413ada862b4bfc19af4ad4b2a15310bdde35781c5d970b624805576ff9ed35"
 
   url "https://static.mendeley.com/bin/desktop/mendeley-reference-manager-#{version}.dmg"
   name "Mendeley Reference Manager"
