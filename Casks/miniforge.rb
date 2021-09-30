7501edbd6fea087d9ed38041789fc2d13c0e7aa2
Bevan Kay <email@bevankay.me>
2021-09-30 10:37:22 +1000

    Update miniforge from 4.10.3-5 to 4.10.3-6 (#111854)

--- Casks/miniforge.rb
+++ Casks/miniforge.rb
@@ -1,8 +1,8 @@
 cask "miniforge" do
-  version "4.10.3-5"
+  version "4.10.3-6"
 
   if Hardware::CPU.intel?
-    sha256 "6f324566af60279518c2d03ffd69f605b335f87f72e6e7e4c2f383808e41e212"
+    sha256 "eabb50e2594d55eeb2a74fa05a919be876ec364e8064e1623ab096f39d6b6dd1"
     url "https://github.com/conda-forge/miniforge/releases/download/#{version}/Miniforge3-#{version}-MacOSX-x86_64.sh"
 
     installer script: {
@@ -10,7 +10,7 @@ cask "miniforge" do
       args:       ["-b", "-p", "#{caskroom_path}/base"],
     }
   else
-    sha256 "77989d3186c21ec8de9ff22ef076e815c465c026cef087601b2bec5d02cfd889"
+    sha256 "4ba889103eb119e9d12e45b5fba3bcdef45877d9a886657f14f1ebe71a9b5a63"
     url "https://github.com/conda-forge/miniforge/releases/download/#{version}/Miniforge3-#{version}-MacOSX-arm64.sh"
 
     installer script: {
