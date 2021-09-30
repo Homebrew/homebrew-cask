e3ba71c0c52d4168bae8e49f02cfab1276b9cef4
Bevan Kay <email@bevankay.me>
2021-09-30 10:49:06 +1000

    add zap, add desc, add livecheck

--- Casks/idagio.rb
+++ Casks/idagio.rb
@@ -4,7 +4,22 @@ cask "idagio" do
 
   url "https://dl.idagio.com/IDAGIO-#{version}.dmg"
   name "IDAGIO"
+  desc "Classical music streaming app"
   homepage "https://www.idagio.com/"
 
+  livecheck do
+    url "https://www.idagio.com/download"
+    regex(/href=.*?IDAGIO[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+  end
+
+  auto_updates true
+
   app "IDAGIO.app"
+
+  zap trash: [
+    "~/Library/Application Support/IDAGIO",
+    "~/Library/Logs/IDAGIO",
+    "~/Library/Preferences/com.idagio.desktop.plist",
+    "~/Library/Saved Application State/com.idagio.desktop.savedState",
+  ]
 end
