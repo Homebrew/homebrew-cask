a3595da1fdfb43fe961d854604b44dbf4e3fedd9
Zhenfu Shi <szf1234@me.com>
2021-09-30 23:13:31 -0400

    Update jupyterlab from 3.1.12-2 to 3.1.13-1 (#111927)

--- Casks/jupyterlab.rb
+++ Casks/jupyterlab.rb
@@ -1,6 +1,6 @@
 cask "jupyterlab" do
-  version "3.1.12-2"
-  sha256 "0aa7ca1aa088bd5712f77ad9d924fd89eba9a5413f0672d28822a103454894ce"
+  version "3.1.13-1"
+  sha256 "bce13d3022217a72f874a8b654834aaf3f5df4d820fecd40ca113c1a67499ea1"
 
   url "https://github.com/jupyterlab/jupyterlab_app/releases/download/v#{version}/JupyterLab-Setup-macOS.pkg"
   name "JupyterLab App"
