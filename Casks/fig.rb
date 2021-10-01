7b73c947adebe4ea98c29e3ee82fc8efabc17af5
Matt Schrage <matthewschrage@gmail.com>
2021-09-30 17:37:14 -0700

    Add Fig.app latest (#111877)
    * Add Fig.app latest
    
    * Fix https and add additional delete stanzas
    
    * fig: additional linting fixes
    
    * fig: use builtin function to delete input method
    
    * fig: linting
    
    * Update fig.rb
    
    * Update fig.rb
    
    Co-authored-by: Matt Schrage <matt@fig.io>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/fig.rb
@@ -0,0 +1,27 @@
+cask "fig" do
+  version "1.0.49,270"
+  sha256 "2209f614d7d09081755a9906539712aed0f23bc395fe1b1e508b506806faaaf4"
+
+  url "https://versions.withfig.com/fig%20#{version.before_comma}.dmg",
+      verified: "versions.withfig.com/"
+  name "fig"
+  desc "Reimagine your terminal"
+  homepage "https://fig.io/"
+
+  livecheck do
+    url "https://versions.withfig.com/appcast.xml"
+    strategy :sparkle
+  end
+
+  depends_on macos: ">= :high_sierra"
+
+  app "Fig.app"
+
+  uninstall script: "#{appdir}/Fig.app/Contents/Resources/uninstall.sh"
+
+  zap trash: [
+    "~/Library/Application Support/com.mschrage.fig",
+    "~/Library/Caches/com.mschrage.fig",
+    "~/Library/Preferences/com.mschrage.fig.plist",
+  ]
+end
