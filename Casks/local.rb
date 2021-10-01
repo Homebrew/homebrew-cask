fdec25a9559e3d0e776d54693126b73fb301048a
Andy Fragen <andy@thefragens.com>
2021-10-01 12:13:12 -0700

    Update Local from 6.1.2 to 6.1.3 (#111950)

--- Casks/local.rb
+++ Casks/local.rb
@@ -1,6 +1,6 @@
 cask "local" do
-  version "6.1.2,5473"
-  sha256 "c862a65a3f33cdc46c0a13ae598a93ebc6b557867bf64e6d67d25da177cbf1fb"
+  version "6.1.3,5515"
+  sha256 "37b6d0a83c941f1cac0dc87024c37c1c620e2e7ddbba6448db6b5b30f13a810c"
 
   url "https://cdn.localwp.com/releases-stable/#{version.before_comma}+#{version.after_comma}/local-#{version.before_comma}-mac.dmg"
   name "Local"
