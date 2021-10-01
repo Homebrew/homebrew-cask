428d9ddbfac005e81908b035195df71e507ebeb2
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-01 09:03:17 +0800

    Update sage from 9.4,1.2 to 9.4,1.2.1 (#111918)
    * Update sage from 9.4,1.2 to 9.4,1.2.1
    
    * Update sage.rb
    
    * Update sage.rb

--- Casks/sage.rb
+++ Casks/sage.rb
@@ -1,6 +1,6 @@
 cask "sage" do
-  version "9.4,1.2"
-  sha256 "70f315b7a47d95b45fe8902838fbe6dd3cfdfe4eb761dba2df644092d640cf68"
+  version "9.4,1.2.1"
+  sha256 "4def064f5bf95fe32c83570db6094d08e7820ff535379c07609e69b500be3f83"
 
   url "https://github.com/3-manifolds/Sage_macOS/releases/download/v#{version.after_comma}/SageMath-#{version.before_comma}.dmg",
       verified: "github.com/3-manifolds/Sage_macOS/"
@@ -30,6 +30,7 @@ cask "sage" do
             pkgutil: [
               "org.computop.SageMath.#{version.before_comma.dots_to_underscores}.bin",
               "org.computop.SageMath.#{version.before_comma.dots_to_underscores}.share",
+              "org.computop.SageMath.#{version.before_comma.dots_to_underscores}.texlive",
             ]
 
   zap trash: [
