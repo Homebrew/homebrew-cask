a8d187306a2035614e458f531493444b795626db
John Benninghoff <jbenninghoff@mac.com>
2021-10-01 18:03:14 -0500

    Update duet from 2.3.3.3 to 2.3.3.6 (#111938)
    * Update duet from 2.3.3.3 to 2.3.3.6
    
    * Update duet.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/duet.rb
+++ Casks/duet.rb
@@ -1,6 +1,6 @@
 cask "duet" do
-  version "2.3.3.3"
-  sha256 "c2d0d4805b404b35a40d288170f147d12dca40fda564caa9d2e91729d2506ba3"
+  version "2.3.3.6"
+  sha256 "da81197ee50f59319e467782c0320ef038c71b8d35450277b3c58525145f666f"
 
   url "https://duet.nyc3.cdn.digitaloceanspaces.com/Mac/#{version.major_minor.dots_to_underscores}/duet-#{version.dots_to_hyphens}.zip",
       verified: "duet.nyc3.cdn.digitaloceanspaces.com/Mac/"
@@ -23,11 +23,11 @@ cask "duet" do
             kext: "com.karios.driver.DuetDisplay"
 
   zap trash: [
-    "~/Library/Preferences/com.kairos.duet*.plist",
     "~/Library/Application Support/com.kairos.duet*",
     "~/Library/Caches/com.crashlytics.data/com.kairos.duet*",
-    "~/Library/Caches/io.fabric.sdk.mac.data/com.kairos.duet*",
     "~/Library/Caches/com.kairos.duet*",
+    "~/Library/Caches/io.fabric.sdk.mac.data/com.kairos.duet*",
+    "~/Library/Preferences/com.kairos.duet*.plist",
   ],
       rmdir: [
         "~/Library/Caches/com.crashlytics.data",
