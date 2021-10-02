e5be9ed3deb99ddc32f8287e50057ca8b3bcca57
John Benninghoff <jbenninghoff@mac.com>
2021-10-02 14:41:48 -0500

    Update marsedit from 4.5.3,9721 to 4.5.5,9750 (#112001)

--- Casks/marsedit.rb
+++ Casks/marsedit.rb
@@ -1,6 +1,6 @@
 cask "marsedit" do
-  version "4.5.3,9721"
-  sha256 "ba3f4f9efa6aba03546c9d7df6693a21b54f8873db613fb2315348ce66ec45f9"
+  version "4.5.5,9750"
+  sha256 "03bd0354ccd8616cccd0bd35b1cd4c571430910f78f0c0c8a6d31e288fb18709"
 
   url "https://redsweater.com/marsedit/MarsEdit#{version.before_comma}.zip"
   name "MarsEdit"
