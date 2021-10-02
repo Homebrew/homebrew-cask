4afcea1ee0a44e06193e01bf8696640086fa6e9d
Yurii Kolesnykov <root@yurikoles.com>
2021-10-02 18:12:21 +0300

    Update qt-creator from 5.0.1 to 5.0.2

--- Casks/qt-creator.rb
+++ Casks/qt-creator.rb
@@ -1,6 +1,6 @@
 cask "qt-creator" do
-  version "5.0.1"
-  sha256 "e560d17ff36c94a6ad8389fbf19391acbb9da95ee2765f585f16035c9079e20d"
+  version "5.0.2"
+  sha256 "858d0b5180fba9f72d29a21e53393dc9d83c791f4e34288d83e697fd08c8ef4e"
 
   url "https://download.qt.io/official_releases/qtcreator/#{version.major_minor}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
   name "Qt Creator"
