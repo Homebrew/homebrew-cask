59d64b8c39a29c8c3d11c7db54ceecf4ef006fc5
Marek Hrabe <marekhrabe@me.com>
2021-10-02 13:33:38 +0200

    Update mockuuups-studio from 3.2.1 to 3.3.0

--- Casks/mockuuups-studio.rb
+++ Casks/mockuuups-studio.rb
@@ -1,17 +1,33 @@
 cask "mockuuups-studio" do
-  version "3.2.1"
-  sha256 "f97a2b4a62b4f0e3bcf1cd1a00e1b744de2c31cff651656ddd7f097680124878"
+  version "3.3.0"
 
-  url "https://binaries.mockuuups.com/Mockuuups%20Studio-#{version}-mac.zip",
-      verified: "mockuuups.com/"
+  if Hardware::CPU.intel?
+    sha256 "bee40bd4b1f5ea28d3e76ec52364eb5092daca2e1324c769213da3b36a317b80"
+
+    url "https://binaries.mockuuups.com/Mockuuups%20Studio-#{version}-mac.zip",
+        verified: "mockuuups.com/"
+
+    livecheck do
+      url "https://mockuuups.studio/download/mac-x64/"
+      strategy :header_match
+    end
+  else
+    sha256 "25a8651323b219ba3ae9a83a63965641c4a0bca2e46d3f6aeb14adacd6726963"
+
+    url "https://binaries.mockuuups.com/Mockuuups%20Studio-#{version}-arm64-mac.zip",
+        verified: "mockuuups.com/"
+
+    livecheck do
+      url "https://mockuuups.studio/download/apple-sillicon/"
+      strategy :header_match
+    end
+  end
+
   name "Mockuuups Studio"
   desc "Allows designers and marketers to drag and drop visuals into scenes"
   homepage "https://mockuuups.studio/"
 
-  livecheck do
-    url "https://mockuuups.studio/download/mac/"
-    strategy :header_match
-  end
+  auto_updates true
 
   app "Mockuuups Studio.app"
 
