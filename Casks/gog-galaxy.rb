8a0232b4ce75c2d7c525d5662914ad903ce846e0
zsoltion <zsoltion@users.noreply.github.com>
2021-10-02 10:02:13 +0200

    Update gog-galaxy from 2.0.41.110 to 2.0.43.66

--- Casks/gog-galaxy.rb
+++ Casks/gog-galaxy.rb
@@ -2,14 +2,14 @@ cask "gog-galaxy" do
   version "2.0.43.66"
   sha256 "b8c28915979f9d90912baffa184d2a04ed73f874a9454325128919861e917246"
 
-  url "https://cdn.gog.com/open/galaxy/client/#{version.chomp("a")}/galaxy_client_#{version}.pkg"
+  url "https://cdn.gog.com/open/galaxy/client/#{version}/galaxy_client_#{version}.pkg"
   name "GOG Galaxy"
   desc "Game client"
   homepage "https://www.gog.com/galaxy"
 
   livecheck do
     url "https://gog.com/galaxy"
-    regex(%r{href=.*?/galaxy_client_(\d+(?:\.\d+)*)\a.pkg}i)
+    regex(%r{href=.*?/galaxy_client_(\d+(?:\.\d+)*)a.pkg}i)
   end
 
   auto_updates true
