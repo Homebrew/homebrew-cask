24ca4757f01459d2915975b9f952795bf0a184d5
neutric <ah0703@googlemail.com>
2021-10-04 00:17:55 +0200

    Update opensesame from 3.3.9 to 3.3.10

--- Casks/opensesame.rb
+++ Casks/opensesame.rb
@@ -1,9 +1,15 @@
 cask "opensesame" do
-  version "3.3.9"
-  sha256 "6891a59b25f7ae340bdba53e55faec71e00cf8a67897b496a271ac3a3134ae06"
+  version "3.3.10"
 
-  url "https://github.com/smathot/OpenSesame/releases/download/release%2F#{version}/opensesame_#{version}-py37-macos-1.dmg",
-      verified: "github.com/smathot/OpenSesame/"
+  if Hardware::CPU.intel?
+    sha256 "ec1b624224608cc32ae796c68fa596ad6e69bd1b921ccc2b8c9b0da962212bdf"
+    url "https://github.com/open-cogsci/OpenSesame/releases/download/release%2F#{version}/opensesame_#{version}-py37-macos-x64-1.dmg",
+        verified: "github.com/open-cogsci/OpenSesame/"
+  else
+    sha256 "562476872c9dc8276b8637cc7fc8f8c21edbccec71cbf54a437eb8aa34cdf2ea"
+    url "https://github.com/open-cogsci/OpenSesame/releases/download/release%2F#{version}/opensesame_#{version}-py37-macos-arm-1.dmg",
+        verified: "github.com/open-cogsci/OpenSesame/"
+  end
   name "OpenSesame"
   desc "Graphical experiment builder for the social sciences"
   homepage "https://osdoc.cogsci.nl/"
@@ -13,5 +19,5 @@ cask "opensesame" do
     regex(%r{^release[/._-]v?(\d+(?:\.\d+)+)$}i)
   end
 
-  app "opensesame.app"
+  app "OpenSesame.app"
 end
