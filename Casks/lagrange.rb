c9380843d40acbbb7c73876a5420687bba579247
Jaakko Keränen <jaakko.keranen@iki.fi>
2021-10-03 13:42:45 +0300

    lagrange: Fixed macOS 10.13 download URL

--- Casks/lagrange.rb
+++ Casks/lagrange.rb
@@ -4,7 +4,7 @@ cask "lagrange" do
   if Hardware::CPU.intel?
     sha256 "a67bda7454ed6060a09d938deed006c4b4078af28559191f792e19a6904554d8"
 
-    url "https://github.com/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}-macos10.13-x86_64.tbz",
+    url "https://github.com/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_macos10.13-x86_64.tbz",
         verified: "github.com/skyjake/lagrange/"
   else
     sha256 "5acb33653df9d7803ae1147fd1243ea80f22593412dd06385087cf6ac9258716"
