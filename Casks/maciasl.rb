bb5b34ca83ddd92454ca918e79d399336616f5d2
Naveen Kumar <naveenk2908@gmail.com>
2021-10-05 04:28:42 +0530

    Update maciasl.rb (#112079)
    * Update maciasl.rb
    
    * Remove livecheck strategy
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/maciasl.rb
+++ Casks/maciasl.rb
@@ -7,6 +7,15 @@ cask "maciasl" do
   desc "ACPI Machine Language (AML) compiler and IDE"
   homepage "https://github.com/acidanthera/MaciASL"
 
+  auto_updates true
+
   app "MaciASL.app"
   binary "#{appdir}/MaciASL.app/Contents/MacOS/iasl-stable", target: "iasl"
+
+  uninstall quit: "org.acidanthera.MaciASL"
+
+  zap trash: [
+    "~/Library/Preferences/org.acidanthera.MaciASL.plist",
+    "~/Library/Saved Application State/org.acidanthera.MaciASL.savedState",
+  ]
 end
