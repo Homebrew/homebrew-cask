81c02302702701310a6cd9071d3ecf2a92eea837
xxyzz <gitpull@protonmail.com>
2021-10-04 18:29:45 +0800

    Add kindle-previewer zap stanza (#112063)
    * Add kindle-previewer zap stanza
    
    * Update kindle-previewer.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/kindle-previewer.rb
+++ Casks/kindle-previewer.rb
@@ -1,14 +1,15 @@
 cask "kindle-previewer" do
-  version "3.52"
+  version "3.58.0"
   sha256 :no_check
 
-  url "https://kindlepreviewer3.s3.amazonaws.com/KindlePreviewerInstaller.pkg",
-      verified: "kindlepreviewer3.s3.amazonaws.com/"
+  url "https://d2bzeorukaqrvt.cloudfront.net/KindlePreviewerInstaller.pkg",
+      verified: "d2bzeorukaqrvt.cloudfront.net/"
   name "Kindle Previewer"
-  homepage "https://www.amazon.com/gp/feature.html?ie=UTF8&docId=1003018611"
+  desc "Preview and audit Kindle eBooks"
+  homepage "https://www.amazon.com/Kindle-Previewer/b?ie=UTF8&node=21381691011"
 
   livecheck do
-    url :homepage
+    url "https://s3.amazonaws.com/kindlepreviewer/UG_ReleaseNotes_EN.txt"
     strategy :page_match
     regex(/Kindle\s*Previewer\s*(\d+(?:\.\d+)*)/i)
   end
@@ -17,4 +18,9 @@ cask "kindle-previewer" do
 
   uninstall launchctl: "com.amazon.KindlePreviewerUpdater",
             pkgutil:   "Amazon.Kindle.Previewer.pkg"
+
+  zap trash: [
+    "/Library/LaunchDaemons/com.amazon.KindlePreviewerUpdater.plist",
+    "~/.kindle",
+  ]
 end
