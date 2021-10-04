67e1aa2e58ba9bfba6115e1103ce52206dc40e65
Naveen Kumar <naveenk2908@gmail.com>
2021-10-04 15:54:19 +0530

    Add PlistEDPlus.app v1.0.64 (#112050)
    * Add PlistEDPlus.app v1.0.64
    
    * Update and rename plist-edplus.rb to plistedplus.rb
    
    * Update plistedplus.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/plistedplus.rb
@@ -0,0 +1,26 @@
+cask "plistedplus" do
+  version "1.0.64"
+  sha256 "43bb8f8a58710724065a7849084c3a2fdc3c3c35071486f9a7fcb08e74a5c92d"
+
+  url "https://github.com/ic005k/PlistEDPlus/releases/download/#{version}/PlistEDPlus_Mac.dmg"
+  name "PlistEDPlus"
+  desc "Plist file editor"
+  homepage "https://github.com/ic005k/PlistEDPlus"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "PlistEDPlus.app"
+
+  uninstall quit: "z.PlistEDPlus"
+
+  zap trash: [
+    "~/Library/Preferences/z.PlistEDPlus.plist",
+    "~/Library/Saved Application State/z.PlistEDPlus.savedState",
+  ]
+end
