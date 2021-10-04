4ef09c3108c660c791956ef446333801679bfad6
Prasannajit Acharya <prasannajit.acharya.kanhu@gmail.com>
2021-10-04 10:51:04 +0530

    Add support for ARM64 builds of Dbeaver Community for Apple Silicon

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,8 +1,14 @@
 cask "dbeaver-community" do
-  version "21.2.1"
-  sha256 "cee5952febdcb970fc56a1658ccb8de99ebf26093d1a0fdbec36fc6983bf0b98"
+  version "21.2.2"
 
-  url "https://dbeaver.io/files/#{version}/dbeaver-ce-#{version}-macos.dmg"
+  if Hardware::CPU.intel?
+    sha256 "985489c389c9cf56d8bd795ac72598d6c4361eb71c1cc13942bc838734befed8"
+    url "https://dbeaver.io/files/#{version}/dbeaver-ce-#{version}-macos-x86_64.dmg"
+  else
+    sha256 "3190457fe92c5f1c1668b8b8813e632f6cbbc0e0ce48264b326ef6531fcbe6a3"
+    url "https://dbeaver.io/files/#{version}/dbeaver-ce-#{version}-macos-aarch64.dmg"
+  end
+
   name "DBeaver Community Edition"
   desc "Universal database tool and SQL client"
   homepage "https://dbeaver.io/"
@@ -13,4 +19,6 @@ cask "dbeaver-community" do
   end
 
   app "DBeaver.app"
+
+  zap trash: "~/Library/Preferences/org.jkiss.dbeaver.core.product.plist"
 end
