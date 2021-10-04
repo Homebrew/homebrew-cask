bebfbfe538f6a0ced345a803a6cf4eb2af297751
Vidhan Bhatt <me@vidhan.io>
2021-10-03 20:02:30 -0400

    Update the kite .dmg url (#112010)
    * Update the kite .dmg url
    
    * Update verified URL

--- Casks/kite.rb
+++ Casks/kite.rb
@@ -2,8 +2,8 @@ cask "kite" do
   version "0.20210610.0"
   sha256 "a72a98e77c5d3410f8d92e2bb1458afa5d4edd51a021491f8f6f2d5995170914"
 
-  url "https://draqv87tt43s0.cloudfront.net/mac/#{version}/Kite.dmg",
-      verified: "draqv87tt43s0.cloudfront.net/"
+  url "https://kitedownloadss.b-cdn.net/mac/#{version}/Kite.dmg",
+      verified: "kitedownloadss.b-cdn.net/"
   name "Kite"
   desc "Add code completions to all your code editors"
   homepage "https://kite.com/"
