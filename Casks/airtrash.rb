07662c29fe4f45e7b0779f67c9d4165cf655882a
Erik Weber <erik.weber@norsk-tipping.no>
2021-10-04 12:05:53 +0200

    Add airtrash zap and desc stanza (#112060)
    * Add zap stanza for airtrash
    
    * Add desc stanza to airtrash

--- Casks/airtrash.rb
+++ Casks/airtrash.rb
@@ -4,7 +4,13 @@ cask "airtrash" do
 
   url "https://github.com/maciejczyzewski/airtrash/releases/download/#{version}/airtrash-#{version}.dmg"
   name "airtrash"
+  desc "Clone of Apple's Airdrop - easy P2P file transfer"
   homepage "https://github.com/maciejczyzewski/airtrash/"
 
   app "Airtrash.app"
+
+  zap trash: [
+    "~/Library/Application Support/airtrash",
+    "~/Library/Preferences/maciejczyzewski.airtrash.plist",
+  ]
 end
