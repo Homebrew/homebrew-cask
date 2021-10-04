afab9b03b32d956d669e2825f1e27a5c2dbbe1eb
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-04 12:29:36 +0800

    Update ricochet-refresh.rb

--- Casks/ricochet-refresh.rb
+++ Casks/ricochet-refresh.rb
@@ -4,9 +4,15 @@ cask "ricochet-refresh" do
 
   url "https://github.com/blueprint-freespeech/ricochet-refresh/releases/download/v#{version}-release/ricochet-refresh-#{version}-macos-x86_64.dmg",
       verified: "github.com/blueprint-freespeech/ricochet-refresh/"
-  name "ricochet-refresh"
+  name "Ricochet Refresh"
   desc "Private and anonymous instant messaging over tor"
   homepage "https://www.ricochetrefresh.net/"
+
+  livecheck do
+    url "https://github.com/blueprint-freespeech/ricochet-refresh/releases"
+    strategy :page_match
+    regex(/ricochet[._-]refresh[._-]v?(\d+(?:\.\d+)*)[._-]macos[._-]x86[._-]64\.dmg/i)
+  end
 
   app "Ricochet Refresh.app"
 
