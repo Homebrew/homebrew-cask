e5b63da13ea4045fc6f02c3771baa4c02eef32fc
George Adams <george.adams@microsoft.com>
2021-10-05 21:20:56 +0100

    simplify multiarch logic

--- Casks/freeplane.rb
+++ Casks/freeplane.rb
@@ -1,16 +1,14 @@
 cask "freeplane" do
+  arch = Hardware::CPU.intel? ? "intel" : "apple"
+
   version "1.9.10"
 
+  url "https://downloads.sourceforge.net/freeplane/Freeplane-#{version}-#{arch}.dmg",
+      verified: "downloads.sourceforge.net/freeplane/"
   if Hardware::CPU.intel?
     sha256 "67b6ee390800ce93ec7aa7255f5a7669efda41ce904d164af623c20d5c71d0c2"
-
-    url "https://downloads.sourceforge.net/freeplane/Freeplane-#{version}-intel.dmg",
-        verified: "downloads.sourceforge.net/freeplane/"
   else
     sha256 "375895d3087f69b235d8f5d3c3fa0294abb3fc5999e32941568e342bd47264ed"
-
-    url "https://downloads.sourceforge.net/freeplane/Freeplane-#{version}-apple.dmg",
-        verified: "downloads.sourceforge.net/freeplane/"
   end
 
   name "Freeplane"
