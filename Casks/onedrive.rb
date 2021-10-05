a0640944269776188ef61505989ddf6b5acdd38a
arnetkachev <arnetkachev@users.noreply.github.com>
2021-10-05 23:08:06 +0300

    Update onedrive from 21.160.0808.0002 to 21.180.0905.0007 (#112126)

--- Casks/onedrive.rb
+++ Casks/onedrive.rb
@@ -1,6 +1,6 @@
 cask "onedrive" do
-  version "21.160.0808.0002"
-  sha256 "19097b282167d66bf666f046ed7bc0fed2df8a59d278f4a2d86ab7522afc223b"
+  version "21.180.0905.0007"
+  sha256 "888d375f41508cce36772616de7dfd03a12acba5615c1640189daa37273ed578"
 
   url "https://oneclient.sfx.ms/Mac/Direct/#{version}/OneDrive.pkg",
       verified: "oneclient.sfx.ms/Mac/Direct/"
