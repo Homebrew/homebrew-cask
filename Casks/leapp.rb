0518e1011ddd5d5871c6da5178212adb1b62af77
Timo Sand <>
2021-10-05 20:19:35 +0300

    Change URL to be determined via block
    Signed-off-by: Timo Sand <>

--- Casks/leapp.rb
+++ Casks/leapp.rb
@@ -2,8 +2,18 @@ cask "leapp" do
   version "0.7.3"
   sha256 "eff8a4e372491bf50a2a06ec7a6c7970528770eb651f1966c030056d04f742fb"
 
-  url "https://asset.noovolari.com/latest/Leapp-#{version}-mac.zip",
-      verified: "asset.noovolari.com/"
+  # URL is different for 'latest' release and previous releases
+  url do
+    require "net/http"
+    latest_url = "https://asset.noovolari.com/latest/Leapp-#{version}-mac.zip"
+    default_url = "https://asset.noovolari.com/#{version}/Leapp-#{version}-mac.zip"
+    url = URI.parse(default_url)
+    req = Net::HTTP.new(url.host, url.port)
+    req.use_ssl = true
+    res = req.request_head(url.path)
+    res.code == "404" || res.code == "403" ? latest_url : default_url
+  end
+
   name "Leapp"
   desc "Cloud credentials manager"
   homepage "https://www.leapp.cloud/"
