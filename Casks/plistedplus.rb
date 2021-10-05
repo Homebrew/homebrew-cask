d9972df92c9b1f4a476c520c84f87bcf1ba33d8e
neutric <ah0703@googlemail.com>
2021-10-05 18:54:14 +0200

    Update plistedplus from 1.0.64 to 1.0.65

--- Casks/plistedplus.rb
+++ Casks/plistedplus.rb
@@ -1,6 +1,6 @@
 cask "plistedplus" do
-  version "1.0.64"
-  sha256 "43bb8f8a58710724065a7849084c3a2fdc3c3c35071486f9a7fcb08e74a5c92d"
+  version "1.0.65"
+  sha256 "8752c338159b7e48e4d2fafe37e9f58ce299a5eef764d38267af26384ad3c083"
 
   url "https://github.com/ic005k/PlistEDPlus/releases/download/#{version}/PlistEDPlus_Mac.dmg"
   name "PlistEDPlus"
