e9beb7e646e174e757069e2cb308b502e2e7e7a4
Wesley Muthemba <chegeamunga@hotmail.co.uk>
2021-10-05 09:54:32 -0700

    Update gog-galaxy.rb

--- Casks/gog-galaxy.rb
+++ Casks/gog-galaxy.rb
@@ -1,6 +1,6 @@
 cask "gog-galaxy" do
   version "2.0.43.66"
-  sha256 "b8c28915979f9d90912baffa184d2a04ed73f874a9454325128919861e917246"
+  sha256 "6de8e5f8f14a2d51898d5add47c1c72bd54e8d2ed54809d027c4fd9837529183"
 
   url "https://cdn.gog.com/open/galaxy/client/#{version}/galaxy_client_#{version}.pkg"
   name "GOG Galaxy"
