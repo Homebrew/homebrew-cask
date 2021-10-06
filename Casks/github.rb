fdd09e8864d25d8805fd96a3a6985d5cdef3faf9
tuzi3040 <28290621+tuzi3040@users.noreply.github.com>
2021-10-06 19:57:19 +0800

    Update github from 2.9.3-2c86f4d1 to 2.9.4-24101633 (#112221)
    * Update github from 2.9.3-2c86f4d1 to 2.9.4-24101633
    
    * Update github.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/github.rb
+++ Casks/github.rb
@@ -1,12 +1,12 @@
 cask "github" do
-  version "2.9.3-2c86f4d1"
+  version "2.9.4-24101633"
 
   if Hardware::CPU.intel?
-    sha256 "0697984f41d8349e9a58d003ebab684082b79ae2aa8782c29d4d23487d7f80d3"
+    sha256 "a7e2f0749b440d401a053b14abd4833e2087552711a835067e7d3d25e3f09cb9"
     url "https://desktop.githubusercontent.com/github-desktop/releases/#{version}/GitHubDesktop-x64.zip",
         verified: "githubusercontent.com/"
   else
-    sha256 "2c3c58abc055dea3275d363b14a516044aebefc103d55bd6537093be236f452d"
+    sha256 "63d199d31fa82b8e39f0d3412a07232053477a5906b18d0327914e2618910035"
     url "https://desktop.githubusercontent.com/github-desktop/releases/#{version}/GitHubDesktop-arm64.zip",
         verified: "githubusercontent.com/"
   end
@@ -28,18 +28,18 @@ cask "github" do
   binary "#{appdir}/GitHub Desktop.app/Contents/Resources/app/static/github.sh", target: "github"
 
   zap trash: [
-    "~/Library/Application Support/GitHub Desktop",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.github.GitHubClient.sfl*",
     "~/Library/Application Support/com.github.GitHubClient",
     "~/Library/Application Support/com.github.GitHubClient.ShipIt",
+    "~/Library/Application Support/GitHub Desktop",
     "~/Library/Application Support/ShipIt_stderr.log",
     "~/Library/Application Support/ShipIt_stdout.log",
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.github.GitHubClient.sfl*",
     "~/Library/Caches/com.github.GitHubClient",
     "~/Library/Caches/com.github.GitHubClient.ShipIt",
+    "~/Library/Logs/GitHub Desktop",
+    "~/Library/Preferences/ByHost/com.github.GitHubClient.ShipIt.*.plist",
     "~/Library/Preferences/com.github.GitHubClient.helper.plist",
     "~/Library/Preferences/com.github.GitHubClient.plist",
-    "~/Library/Preferences/ByHost/com.github.GitHubClient.ShipIt.*.plist",
-    "~/Library/Logs/GitHub Desktop",
   ],
       rmdir: "~/.config/git"
 end
