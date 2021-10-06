f44deba1ed7ac43a904c6fb87863c11063e9f4b7
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-06 15:08:07 +0800

    Update guilded from 1.0.9105465 to 1.0.9109710 (#112160)

--- Casks/guilded.rb
+++ Casks/guilded.rb
@@ -1,6 +1,6 @@
 cask "guilded" do
-  version "1.0.9105465"
-  sha256 "dbdf672f6cf05e549cf428cbc1b64a6a4d25c1a15c00b4c40297b23ce8751018"
+  version "1.0.9109710"
+  sha256 "300133fa6a3b968dcbd9b7776a2684fc0089f58e8fa8328ca83c065dfeada285"
 
   url "https://s3-us-west-2.amazonaws.com/www.guilded.gg/AppBuilds/mac/Guilded-#{version}-release.dmg",
       verified: "s3-us-west-2.amazonaws.com/www.guilded.gg/AppBuilds/mac/"
