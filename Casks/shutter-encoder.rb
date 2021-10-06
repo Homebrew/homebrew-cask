bf3ae39be25dd36ef5aaa655b1d0fd68095a5812
George Adams <george.adams@microsoft.com>
2021-10-06 08:41:34 +0100

    simplify multiarch logic

--- Casks/shutter-encoder.rb
+++ Casks/shutter-encoder.rb
@@ -1,18 +1,13 @@
 cask "shutter-encoder" do
+  arch = Hardware::CPU.intel? ? "64bits" : "Silicon"
+
   version "15.5"
 
+  url "https://www.shutterencoder.com/Shutter%20Encoder%20#{version}%20Mac%20#{arch}.pkg"
   if Hardware::CPU.intel?
     sha256 "7b97d3347cb1031c5335fb8fa413637940905f9eea380a680e0749b3df7315c4"
-
-    url "https://www.shutterencoder.com/Shutter%20Encoder%20#{version}%20Mac%2064bits.pkg"
-
-    pkg "Shutter Encoder #{version} Mac 64bits.pkg"
   else
     sha256 "3371bdf03691cf4785c0236471ada5ca76ce3c84f37a745a976f8bbb78af962f"
-
-    url "https://www.shutterencoder.com/Shutter%20Encoder%20#{version}%20Apple%20Silicon.pkg"
-
-    pkg "Shutter Encoder #{version} Apple Silicon.pkg"
   end
 
   name "Shutter Encoder"
@@ -23,6 +18,8 @@ cask "shutter-encoder" do
     url "https://www.shutterencoder.com/changelog.txt"
     regex(/^\s*Version\s*(\d+(?:\.\d+)*)/i)
   end
+
+  pkg "Shutter Encoder #{version} Mac #{arch}.pkg"
 
   uninstall pkgutil: "com.paulpacifico.shutterencoder",
             quit:    "com.paulpacifico.shutterencoder"
