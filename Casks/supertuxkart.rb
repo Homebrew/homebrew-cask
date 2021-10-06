432194450f45a7296671442371695fee1be65646
Randall <17261190+ran-dall@users.noreply.github.com>
2021-10-06 14:01:59 -0700

    Update SHA for SuperTux v1.3 (#112240)

--- Casks/supertuxkart.rb
+++ Casks/supertuxkart.rb
@@ -1,6 +1,6 @@
 cask "supertuxkart" do
   version "1.3"
-  sha256 "2b1911cd782b0ac96d66c008c90e23a72720bc865176376220d1c297ab67ad22"
+  sha256 "50d24b00dbe009003ae3ba1e7a2cfea2b1e998ac762bee9382acef43e490fea5"
 
   url "https://github.com/supertuxkart/stk-code/releases/download/#{version}/SuperTuxKart-#{version}-mac.zip",
       verified: "github.com/supertuxkart/stk-code"
