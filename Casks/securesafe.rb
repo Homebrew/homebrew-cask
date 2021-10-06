e644c5714c71a661b476646b21b6261921d86c12
dswiss-desktop-app <38659191+dswiss-desktop-app@users.noreply.github.com>
2021-10-06 15:55:12 +0200

    Update securesafe.rb
    version 2.12.0 released

--- Casks/securesafe.rb
+++ Casks/securesafe.rb
@@ -2,8 +2,7 @@ cask "securesafe" do
   version "2.12.0"
   sha256 "87e64f2f10e58173c239267cfbe7c4975eb5598b33eb6ce7441e8ae2323b0aa6"
 
-  url "https://www.securesafe.com/userdata/downloads/securesafe-#{version}.pkg",
-      verified: "securesafe.com/userdata/downloads/"
+  url "https://www.securesafe.com/userdata/downloads/securesafe-#{version}.pkg"
   name "SecureSafe"
   desc "Highly secure online storage with password manager"
   homepage "https://www.securesafe.com/"
