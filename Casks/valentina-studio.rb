c59c3c2e575113c3ebc4652905a8931b9a26257e
miccal <miccal.matthews@gmail.com>
2021-10-06 15:31:31 +0800

    Update valentina-studio from 11.4.1 to 11.4.6

--- Casks/valentina-studio.rb
+++ Casks/valentina-studio.rb
@@ -1,6 +1,6 @@
 cask "valentina-studio" do
-  version "11.4.1"
-  sha256 "0df3d9d5f1ca7d29e50e0e79842016d010016ee00ef70655a71c4964061a3fba"
+  version "11.4.6"
+  sha256 "8de11eaae16a060fa7be4af69c1f4fc71b919279b8613ed1c7b47d461a84001c"
 
   url "https://valentina-db.com/download/prev_releases/#{version}/mac_64/vstudio_x64_#{version.major}_mac.dmg"
   name "Valentina Studio"
