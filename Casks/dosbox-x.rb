aa3c860323f34cf40e454d5b847d951ab30d2d97
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-07 07:56:08 +0800

    Update dosbox-x.rb

--- Casks/dosbox-x.rb
+++ Casks/dosbox-x.rb
@@ -1,8 +1,6 @@
 cask "dosbox-x" do
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 
-  url "https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v#{version.before_comma}/dosbox-x-macosx-#{arch}-#{version.after_comma}.zip",
-      verified: "github.com/joncampbell123/dosbox-x/"
   if Hardware::CPU.intel?
     version "0.83.18,20211001082411"
     sha256 "c6a9fb6e410b5db7d1d8b55662a9137dec6c46098edacb3da3ff333fa62a9f82"
@@ -11,6 +9,8 @@ cask "dosbox-x" do
     sha256 "3178fe181e6725bd2671f7dcef9b1e99e7399d94bda6ea22a8ccf111885dd7f1"
   end
 
+  url "https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v#{version.before_comma}/dosbox-x-macosx-#{arch}-#{version.after_comma}.zip",
+      verified: "github.com/joncampbell123/dosbox-x/"
   name "DOSBox-X"
   desc "Fork of the DOSBox project"
   homepage "https://dosbox-x.com/"
