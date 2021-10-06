0c7c6e3e12dc86adb260d18f21fd188b58722ece
miccal <miccal.matthews@gmail.com>
2021-10-06 15:02:34 +0800

    Update opera-gx from 78.0.4093.214 to 79.0.4143.73

--- Casks/opera-gx.rb
+++ Casks/opera-gx.rb
@@ -1,6 +1,6 @@
 cask "opera-gx" do
-  version "78.0.4093.214"
-  sha256 "1f0d7bd23127397d9c83e83984e8d23dbf157704d7b8ff4edf7a8ef72bdce0df"
+  version "79.0.4143.73"
+  sha256 "7893aa8f89d4b402c0a555dd853badb27e9961be9aed987855461b653e2c7366"
 
   url "https://get.geo.opera.com/pub/opera_gx/#{version}/mac/Opera_GX_#{version}_Setup.dmg"
   name "Opera GX"
