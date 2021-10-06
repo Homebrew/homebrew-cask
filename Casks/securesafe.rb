5422fb91eb851961afe0cebf3589c572069b5a9a
dswiss-desktop-app <38659191+dswiss-desktop-app@users.noreply.github.com>
2021-10-06 16:22:52 +0200

    Update securesafe.rb
    version 2.12.0 released

--- Casks/securesafe.rb
+++ Casks/securesafe.rb
@@ -7,12 +7,6 @@ cask "securesafe" do
   desc "Highly secure online storage with password manager"
   homepage "https://www.securesafe.com/"
 
-  livecheck do
-    url "https://www.securesafe.com/userdata/downloads"
-    strategy :page_match
-    regex(/securesafe-(\d+(?:\.\d+)*)\.pkg/i)
-  end
-
   depends_on macos: ">= :mojave"
 
   pkg "securesafe-#{version}.pkg"
