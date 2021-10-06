214c98b5c1e19a58565f8caff9e379b226e6a2de
neutric <ah0703@googlemail.com>
2021-10-06 23:35:52 +0200

    paperspace: remove livecheck relying on external site for POST request functionality

--- Casks/paperspace.rb
+++ Casks/paperspace.rb
@@ -7,12 +7,5 @@ cask "paperspace" do
   desc "Desktop app for the Paperspace cloud computing platform"
   homepage "https://www.paperspace.com/app/"
 
-  livecheck do
-    url "https://www.macupdater.net/cgi-bin/extract_text/send_post_request_data.cgi?url=https://api.paperspace.io/receivers/latestVersion&data={%22component%22:%22paperspaceInstaller%22,%22os%22:%22mac%22}&headers={%22user-agent%22:%22Mozilla%22,%22content-type%22:%22application/json%22}"
-    strategy :page_match do |page|
-      JSON.parse(page.split("sasd")[0])["version"]
-    end
-  end
-
   app "Paperspace.app"
 end
