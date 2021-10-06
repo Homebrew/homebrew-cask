8fa977e8fc15efe09a9d9f0a69dd69c608ec5ca5
Naveen Kumar <naveenk2908@gmail.com>
2021-10-06 15:42:16 +0530

    Updated 'PlistEDPlus.app' to '1.0.66'. (#112207)
    Added sierra or older version app.

--- Casks/plistedplus.rb
+++ Casks/plistedplus.rb
@@ -1,8 +1,14 @@
 cask "plistedplus" do
-  version "1.0.65"
-  sha256 "8752c338159b7e48e4d2fafe37e9f58ce299a5eef764d38267af26384ad3c083"
+  version "1.0.66"
 
-  url "https://github.com/ic005k/PlistEDPlus/releases/download/#{version}/PlistEDPlus_Mac.dmg"
+  if MacOS.version <= :sierra
+    sha256 "8c65897f3177f43992949693f00fee1847f6780f701b08ae969546455814a7c2"
+    url "https://github.com/ic005k/PlistEDPlus/releases/download/#{version}/PlistEDPlus_Mac10.12.and.below.dmg"
+  else
+    sha256 "b758ecfc1fb4b80e5c6aada3b9f7b8bbcd8d4d8c866cf0a005999d77c9a9b96d"
+    url "https://github.com/ic005k/PlistEDPlus/releases/download/#{version}/PlistEDPlus_Mac.dmg"
+  end
+
   name "PlistEDPlus"
   desc "Plist file editor"
   homepage "https://github.com/ic005k/PlistEDPlus"
@@ -12,14 +18,24 @@ cask "plistedplus" do
     strategy :github_latest
   end
 
-  depends_on macos: ">= :high_sierra"
-
   app "PlistEDPlus.app"
 
-  uninstall quit: "z.PlistEDPlus"
+  if MacOS.version <= :sierra
+    uninstall quit: "com.yourcompany.PlistEDPlus"
+  else
+    uninstall quit: "z.PlistEDPlus"
+  end
 
-  zap trash: [
-    "~/Library/Preferences/z.PlistEDPlus.plist",
-    "~/Library/Saved Application State/z.PlistEDPlus.savedState",
-  ]
+  if MacOS.version <= :sierra
+    zap trash: [
+      "~/Library/Preferences/com.github-com-ic005k.PlistEdPlus.plist",
+      "~/Library/Preferences/com.yourcompany.PlistEDPlus.plist",
+      "~/Library/Saved Application State/com.yourcompany.PlistEDPlus.savedState",
+    ]
+  else
+    zap trash: [
+      "~/Library/Preferences/z.PlistEDPlus.plist",
+      "~/Library/Saved Application State/z.PlistEDPlus.savedState",
+    ]
+  end
 end
