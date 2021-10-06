abc201a78456c40a1da689bd6f83028d2f95e200
Diego <jahndiego@mailbox.org>
2021-10-06 23:49:17 +0200

    Update tandem from 1.8.818 to 2.0.1001 (#112237)

--- Casks/tandem.rb
+++ Casks/tandem.rb
@@ -1,12 +1,12 @@
 cask "tandem" do
-  version "1.8.818"
+  version "2.0.1001"
 
   if Hardware::CPU.intel?
-    sha256 "228971e3798e3e658c05fbd04e01991e1f452087c9f22ecea708446f6592cbf0"
+    sha256 "3aa0926745eb589656908de80dd68c7c6725791d5366efe8517f725a8891232b"
     url "https://download.todesktop.com/200527auaqaacsy/Tandem%20#{version}-x64.dmg",
         verified: "download.todesktop.com/200527auaqaacsy/"
   else
-    sha256 "5a08640944af10a4191f6649604cfae6beb942e0b0a7a75ddc401ba0ec09327d"
+    sha256 "c87bd5a231852cab6d87a07489d932af413e2e4877abb16130e154426215ca44"
     url "https://download.todesktop.com/200527auaqaacsy/Tandem%20#{version}-arm64.dmg",
         verified: "download.todesktop.com/200527auaqaacsy/"
   end
