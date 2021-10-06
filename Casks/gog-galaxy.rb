64658ad0593e93a4256c49761e82bd278ffdfe22
neutric <neutric@users.noreply.github.com>
2021-10-06 02:42:04 +0200

    Update gog-galaxy from 2.0.43.66 to 2.0.43.66a (#112119)
    * Revert "Update gog-galaxy from 2.0.41.110 to 2.0.43.66 (#111977)"
    
    This reverts commit 1626bed1623516c0b724012cd8197afdb3a4231a.
    
    * Update gog-galaxy from 2.0.41.110 to 2.0.43.66a
    
    * Update gog-galaxy.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/gog-galaxy.rb
+++ Casks/gog-galaxy.rb
@@ -1,15 +1,15 @@
 cask "gog-galaxy" do
-  version "2.0.43.66"
-  sha256 "b8c28915979f9d90912baffa184d2a04ed73f874a9454325128919861e917246"
+  version "2.0.43.66a"
+  sha256 "349e9c0e8fc3c3ac582c1ac2d43066bcfb7eeac9362d0baf3ed05e7dc5aa49d5"
 
-  url "https://cdn.gog.com/open/galaxy/client/#{version}/galaxy_client_#{version}.pkg"
+  url "https://cdn.gog.com/open/galaxy/client/#{version.chomp("a")}/galaxy_client_#{version}.pkg"
   name "GOG Galaxy"
   desc "Game client"
   homepage "https://www.gog.com/galaxy"
 
   livecheck do
-    url "https://gog.com/galaxy"
-    regex(%r{href=.*?/galaxy_client_(\d+(?:\.\d+)*)a.pkg}i)
+    url :homepage
+    regex(%r{href=.*?/galaxy_client_(\d+(?:\.\d+)*a?)\.pkg}i)
   end
 
   auto_updates true
@@ -18,9 +18,9 @@ cask "gog-galaxy" do
 
   uninstall delete:    "/Applications/GOG Galaxy.app",
             launchctl: [
+              "com.gog.galaxy.autoLauncher",
               "com.gog.galaxy.ClientService",
               "com.gog.galaxy.commservice",
-              "com.gog.galaxy.autoLauncher",
             ]
 
   zap trash: [
