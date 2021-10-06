73a30b1a31fcc75802f8049f1f1c80b84019a1f6
neutric <ah0703@googlemail.com>
2021-10-06 02:23:34 +0200

    Update paperspace from 10.1.0.1685 to 11.3.0.3468

--- Casks/paperspace.rb
+++ Casks/paperspace.rb
@@ -1,11 +1,18 @@
 cask "paperspace" do
-  version "10.1.0.1685"
-  sha256 "6d7bef9f569a3c8a99b716068cc3ff6d8d5e603536eab593a4359b8866d0dfaa"
+  version "11.3.0.3468"
+  sha256 "3550b55422f03434b1a47d1e94d5352ae78467a9491e42e97ac1d7b15721fcf6"
 
   url "https://assets.paperspace.com/native-app/prod/darwin/Paperspace-#{version}.dmg"
-  appcast "https://www.macupdater.net/cgi-bin/extract_text/send_post_request_data.cgi?url=https://api.paperspace.io/receivers/latestVersion&data={%22component%22:%22paperspaceInstaller%22,%22os%22:%22mac%22}&headers={%22user-agent%22:%22Mozilla%22,%22content-type%22:%22application/json%22}"
   name "Paperspace"
-  homepage "https://www.paperspace.com/"
+  desc "Desktop app for the Paperspace cloud computing platform"
+  homepage "https://www.paperspace.com/app/"
+
+  livecheck do
+    url "https://www.macupdater.net/cgi-bin/extract_text/send_post_request_data.cgi?url=https://api.paperspace.io/receivers/latestVersion&data={%22component%22:%22paperspaceInstaller%22,%22os%22:%22mac%22}&headers={%22user-agent%22:%22Mozilla%22,%22content-type%22:%22application/json%22}"
+    strategy :page_match do |page|
+      JSON.parse(page.split("sasd")[0])["version"]
+    end
+  end
 
   app "Paperspace.app"
 end
