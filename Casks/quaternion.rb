a06092b82396b566fba60e12983137206b9875c6
Aaron Raimist <aaron@raim.ist>
2021-10-07 18:12:41 -0500

    Update quaternion from 0.0.95 to 0.0.95.1

--- Casks/quaternion.rb
+++ Casks/quaternion.rb
@@ -1,6 +1,6 @@
 cask "quaternion" do
-  version "0.0.95"
-  sha256 "47c6ae19a1cb7cb266f277d79672297b8eb8b911e0ee7cf527f91533463d546d"
+  version "0.0.95.1"
+  sha256 "d2f80cfd17ab25b4062032afa76aa7bf509d32fb72c91858ab04347e216df9a7"
 
   url "https://github.com/quotient-im/Quaternion/releases/download/#{version}/quaternion-#{version}.dmg"
   name "Quaternion"
