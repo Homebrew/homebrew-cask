50a5ba6f709c4889be6bb299774fe53398b7bb5e
Bevan Kay <email@bevankay.me>
2021-10-07 18:48:28 +1100

    use universal binary

--- Casks/pushplaylabs-sidekick.rb
+++ Casks/pushplaylabs-sidekick.rb
@@ -1,35 +1,21 @@
 cask "pushplaylabs-sidekick" do
-  if Hardware::CPU.intel?
-    version "92.11.5.13224,4bad716"
-    sha256 "22b7eeae43ef8c5fb4feb01ec0810d3da6e8f95bb7552f2648275963513ab8f6"
+  version "92.11.5.13226,6bc0c48"
+  sha256 "30ccd7071169c062fe227bcc049f0edbe62bfa07ef74100f4d4fd93002a9272c"
 
-    url "https://sidekick-cdn-production.meetsidekick.com/builds/sidekick-mac-release-x64-#{version.before_comma}-#{version.after_comma}-df.dmg"
-
-    livecheck do
-      url "https://api.meetsidekick.com/downloads/df/mac"
-      strategy :header_match do |headers|
-        match = headers["location"].match(/[_-](\d+(?:\.\d+)+)[_-](\h+)[._-]df\.dmg/i)
-        "#{match[1]},#{match[2]}"
-      end
-    end
-  else
-    version "92.11.5.13226,6bc0c48"
-    sha256 "30ccd7071169c062fe227bcc049f0edbe62bfa07ef74100f4d4fd93002a9272c"
-
-    url "https://sidekick-cdn-production.meetsidekick.com/builds/sidekick-mac-release-universal-#{version.before_comma}-#{version.after_comma}-df.dmg"
-
-    livecheck do
-      url "https://api.meetsidekick.com/downloads/macm1"
-      strategy :header_match do |headers|
-        match = headers["location"].match(/[_-](\d+(?:\.\d+)+)[_-](\h+)[._-]df\.dmg/i)
-        "#{match[1]},#{match[2]}"
-      end
-    end
-  end
-
+  url "https://sidekick-cdn-production.meetsidekick.com/builds/sidekick-mac-release-universal-#{version.before_comma}-#{version.after_comma}-df.dmg"
   name "Sidekick"
   desc "Browser designed for modern work"
   homepage "https://www.meetsidekick.com/"
+
+  livecheck do
+    url "https://api.meetsidekick.com/downloads/macm1"
+    strategy :header_match do |headers|
+      match = headers["location"].match(/[_-](\d+(?:\.\d+)+)(\h+)[._-]df\.dmg/i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   app "Sidekick.app"
 
