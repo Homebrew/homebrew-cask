6ebe14b566e0b7bf347b70964098c1987e7e67e6
xxyzz <gitpull@protonmail.com>
2021-10-08 15:53:29 +0800

    Update kindle-previewer to 3.59.0 (#112286)

--- Casks/kindle-previewer.rb
+++ Casks/kindle-previewer.rb
@@ -1,5 +1,5 @@
 cask "kindle-previewer" do
-  version "3.58.0"
+  version "3.59.0"
   sha256 :no_check
 
   url "https://d2bzeorukaqrvt.cloudfront.net/KindlePreviewerInstaller.pkg",
@@ -9,7 +9,7 @@ cask "kindle-previewer" do
   homepage "https://www.amazon.com/Kindle-Previewer/b?ie=UTF8&node=21381691011"
 
   livecheck do
-    url "https://s3.amazonaws.com/kindlepreviewer/UG_ReleaseNotes_EN.txt"
+    url :homepage
     strategy :page_match
     regex(/Kindle\s*Previewer\s*(\d+(?:\.\d+)*)/i)
   end
