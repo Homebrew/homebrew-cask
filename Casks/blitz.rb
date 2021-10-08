c55b29de694990a3cdb93bc3661822bcfd575f97
Hannes Braun <hannesbraun@mail.de>
2021-10-08 18:30:59 +0200

    Update blitz from 1.15.50 to 1.15.52 (#112304)

--- Casks/blitz.rb
+++ Casks/blitz.rb
@@ -1,6 +1,6 @@
 cask "blitz" do
-  version "1.15.50"
-  sha256 "b67fb3fe5d99325e2383b1ead37cee4fab5f7161de4dd771c82103f6387aaf18"
+  version "1.15.52"
+  sha256 "b1565d4571892c4a4a3b8d35ff966d682d901e30b325575048a45da52a81c226"
 
   url "https://blitz-stable.blitz.gg/Blitz-#{version}.dmg"
   name "Blitz"
