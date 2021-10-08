826466af14890e2359054e9e569ae3391d1acf4b
Connor Adams <connorads@users.noreply.github.com>
2021-10-08 17:52:46 +0100

    Remove comma and format

--- Casks/pensela.rb
+++ Casks/pensela.rb
@@ -1,11 +1,11 @@
 cask "pensela" do
-    version "1.2.3"
-    sha256 "5ded6cba1dfa551e8e963b0d0e9d6849fe37e41c4bdfdd47ceb71ab79ee2d5ee"
+  version "1.2.3"
+  sha256 "5ded6cba1dfa551e8e963b0d0e9d6849fe37e41c4bdfdd47ceb71ab79ee2d5ee"
 
-    url "https://github.com/weiameili/Pensela/releases/download/v#{version}/Pensela-#{version}.dmg",
-    name "Pensela"
-    desc "The Swiss Army Knife of Screen Annotation Tools"
-    homepage "https://github.com/weiameili/Pensela"
+  url "https://github.com/weiameili/Pensela/releases/download/v#{version}/Pensela-#{version}.dmg"
+  name "Pensela"
+  desc "The Swiss Army Knife of Screen Annotation Tools"
+  homepage "https://github.com/weiameili/Pensela"
 
-    app "Pensela.app"
+  app "Pensela.app"
 end
