bd00133b861fb8743abea52c2f8fc3916295b53a
viking1304 <viking1304@gmail.com>
2021-10-08 19:10:52 +0200

    Fix dmg filename

--- Casks/samsung-dex.rb
+++ Casks/samsung-dex.rb
@@ -2,7 +2,7 @@ cask "samsung-dex" do
   version "20210720162553369"
   sha256 "2e45b967a25f5ad04a401a88a4b35e837123c6b9323f6c7c3d6ba0755f4397a6"
 
-  url "https://downloadcenter.samsung.com/content/SW/#{version[0..5]}/#{version}/SamsungDeXSetupMac.dmg"
+  url "https://downloadcenter.samsung.com/content/SW/#{version[0..5]}/#{version}/SamsungDeXSetup.dmg"
   name "Samsung DeX"
   desc "Extend some Samsung devices into a desktop-like experience"
   homepage "https://www.samsung.com/us/explore/dex/"
