c372eadb8df8699bab2f8101b5c496442160e398
tiiiecherle <tiiiecherle@gmail.com>
2021-10-08 18:33:19 +0200

    Update macfuse from 4.1.2 to 4.2.1 (#112295)

--- Casks/macfuse.rb
+++ Casks/macfuse.rb
@@ -1,6 +1,6 @@
 cask "macfuse" do
-  version "4.1.2"
-  sha256 "9ff344de38bad5ac5eca5194da00a2ba35dd6afdb9039133b5446168f8edafdb"
+  version "4.2.1"
+  sha256 "bd3abba91a8c16005eb3937ad492e49275e60db429bbecd30d54769c191e0699"
 
   url "https://github.com/osxfuse/osxfuse/releases/download/macfuse-#{version}/macfuse-#{version}.dmg",
       verified: "github.com/osxfuse/osxfuse/"
