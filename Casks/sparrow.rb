57316e3d906a015c33a5370a7c753f3678cc9930
Brendan Murphy <brendan@thesimplekid.com>
2021-10-08 12:27:07 -0400

    Update sparrow from 1.5.0 to 1.5.1 (#112317)

--- Casks/sparrow.rb
+++ Casks/sparrow.rb
@@ -1,6 +1,6 @@
 cask "sparrow" do
-  version "1.5.0"
-  sha256 "a4deab8cb164f0375344462be80ab2a59a5177029efc50cea4f458e6de956bee"
+  version "1.5.1"
+  sha256 "f6e2279642d2bfbcee253fdc5d0c47957b742a2bbdb81817138457fa6a506a12"
 
   url "https://github.com/sparrowwallet/sparrow/releases/download/#{version}/Sparrow-#{version}.dmg",
       verified: "github.com/sparrowwallet/sparrow/"
