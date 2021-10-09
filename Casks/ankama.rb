fd85338741152a39d3d3bb6db2e9d0af9cdc3b61
manghao <>
2021-10-09 23:47:01 +0200

    lint

--- Casks/ankama.rb
+++ Casks/ankama.rb
@@ -5,20 +5,20 @@ cask "ankama" do
   language "en", default: true do
     desc "Discover the portal you can use to download all of Ankama's PC games"
     homepage "https://www.ankama.com/en/launcher"
+    "en"
   end
-
   language "fr" do
     desc "Découvre le portail qui te permet de télécharger tous les jeux PC d'Ankama"
     homepage "https://www.ankama.com/fr/launcher"
+    "fr"
   end
-
   language "es" do
     desc "Descubre el portal para descargar todos los juegos de PC de Ankama"
     homepage "https://www.ankama.com/es/lanzar"
+    "es"
   end
 
   url "https://download.ankama.com/launcher/full/mac/"
-
   name "Ankama Launcher"
 
   depends_on macos: ">= :sierra"
