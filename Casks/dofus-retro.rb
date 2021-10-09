fc36648ca62cb2b7ac2076429a13ef461ab04fdd
manghao <>
2021-10-09 23:47:52 +0200

    lint

--- Casks/dofus-retro.rb
+++ Casks/dofus-retro.rb
@@ -7,31 +7,26 @@ cask "dofus-retro" do
     homepage "https://www.dofus.com/fr/mmorpg/telecharger"
     "fr"
   end
-
   language "es" do
     desc "¡Juega a DOFUS Retro para ver cómo empezó la leyenda!"
     homepage "https://www.dofus.com/es/mmorpg/descargar"
     "es"
   end
-
   language "de" do
     desc "Spielt DOFUS Retro und erlebt, wie die Legende begann!"
     homepage "https://www.dofus.com/de/mmorpg/download"
     "de"
   end
-
   language "it" do
     desc "Gioca a DOFUS Retro per vedere come è iniziata la leggenda!"
     homepage "https://www.dofus.com/it/mmorpg/download"
     "it"
   end
-
   language "pt" do
     desc "Jogue DOFUS Retro para ver como a lenda começou!"
     homepage "https://www.dofus.com/pt/mmorpg/baixar"
     "pt"
   end
-
   language "en", default: true do
     desc "Play DOFUS Retro to see how the legend began!"
     homepage "https://www.dofus.com/en/mmorpg/download"
@@ -40,7 +35,6 @@ cask "dofus-retro" do
 
   url "https://dofusretro.cdn.ankama.com/installers/release/dofus-retro-mac64.tar.gz",
       verified: "dofusretro.cdn.ankama.com/"
-
   name "Dofus Retro"
 
   depends_on macos: ">= :yosemite"
