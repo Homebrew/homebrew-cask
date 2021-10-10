4cda471a0e09889895e41e9e2576ed504203d9a7
naveenkrdy <naveenk2908@gmail.com>
2021-10-10 06:43:21 +0530

    Update cask 'kaleidoscope.app' to 'v3'

--- Casks/kaleidoscope.rb
+++ Casks/kaleidoscope.rb
@@ -1,31 +1,29 @@
 cask "kaleidoscope" do
-  version "2.4.2,1455:sep-7-2021"
-  sha256 "d2ccd4c63781f836f2b5018ba51c286829929d43d74ea6300144e7c7a50f911a"
+  version "3.0.2"
+  sha256 "ff89bee907b911678009ebdcf75423e940b11d48b0379d22a7873d562a91f500"
 
-  url "https://updates.kaleidoscope.app/v2/prod/Kaleidoscope-#{version.before_comma}-#{version.after_comma.before_colon}-#{version.after_colon}.app.zip"
+  url "https://updates.kaleidoscope.app/v3/prod/Kaleidoscope-#{version}-2013.app.zip"
   name "Kaleidoscope"
   desc "Spot and merge differences in text and image files or folders"
   homepage "https://www.kaleidoscope.app/"
 
   livecheck do
-    url "https://updates.kaleidoscope.app/v#{version.major}/prod/appcast"
-    strategy :sparkle do |item|
-      match = item.url.match(%r{/Kaleidoscope-(\d+(?:\.\d+)*)-(\d+)-(\w+(?:-\d+)*)\.app\.zip}i)
-      "#{match[1]},#{match[2]}:#{match[3]}"
-    end
+    url "https://updates.kaleidoscope.app/v#{version}/prod/appcast"
+    strategy :sparkle
   end
 
   auto_updates true
-  depends_on macos: ">= :high_sierra"
+  depends_on macos: ">= :big_sur"
 
   app "Kaleidoscope.app"
   binary "#{appdir}/Kaleidoscope.app/Contents/Resources/bin/ksdiff"
 
   zap trash: [
-    "~/Library/Application Support/com.blackpixel.kaleidoscope",
-    "~/Library/Caches/com.blackpixel.kaleidoscope",
-    "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.blackpixel.kaleidoscope",
-    "~/Library/Preferences/com.blackpixel.kaleidoscope.plist",
-    "~/Library/Saved Application State/com.blackpixel.kaleidoscope.savedState",
+    "~/Library/Application Support/Kaleidoscope",
+    "~/Library/Application Support/app.kaleidoscope.v3",
+    "~/Library/Caches/app.kaleidoscope.v3",
+    "~/Library/Preferences/app.kaleidoscope.v3.plist",
+    "~/Library/Saved Application State/app.kaleidoscope.v3.savedState",
+    "~/Library/WebKit/app.kaleidoscope.v3",
   ]
 end
