fd833379903d7e27ec64204a6bd7b287dc7b9ca9
miccal <miccal.matthews@gmail.com>
2021-10-11 13:22:10 +0800

    Update logisim-evolution from 3.6.0 to 3.6.1

--- Casks/logisim-evolution.rb
+++ Casks/logisim-evolution.rb
@@ -1,6 +1,6 @@
 cask "logisim-evolution" do
-  version "3.6.0"
-  sha256 "35196d7d6293858bc37a449264f0b076340a9c6e84ce38704cbcbf784c28daf6"
+  version "3.6.1"
+  sha256 "1b38fb8d2f21fc06e0b0cdc1b6a576b46ad52e9173d0f9adb7343df2ba577fc7"
 
   url "https://github.com/reds-heig/logisim-evolution/releases/download/v#{version}/Logisim-evolution-#{version}.dmg"
   name "Logisim Evolution"
