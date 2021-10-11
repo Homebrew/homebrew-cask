e246d4597d746665d4571ce17204c629ec73c587
Jeff <31933684+upkit@users.noreply.github.com>
2021-10-11 18:13:33 +0800

    Update utools from 2.1.0 to 2.3.2 (#112412)
    * Update utools from 2.1.0 to 2.3.2
    
    * Update utools.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/utools.rb
+++ Casks/utools.rb
@@ -1,6 +1,6 @@
 cask "utools" do
-  version "2.1.0"
-  sha256 "3cc48b309b696ca5c92f11e0d32d96edf25ce0772ff5e279365f09e7c402bc03"
+  version "2.3.2"
+  sha256 "30f34614e963bba79a65b2a7192c848101bcec9b21610ed055951a0bcd4c7600"
 
   url "https://publish.u-tools.cn/version2/uTools-#{version}.dmg",
       verified: "publish.u-tools.cn/"
@@ -9,8 +9,8 @@ cask "utools" do
   homepage "https://u.tools/index.html"
 
   livecheck do
-    url "https://publish.u-tools.cn/version2/latest-mac.yml?noCache=true"
-    strategy :electron_builder
+    url :homepage
+    regex(/uTools[._-]v?(\d+(?:\.\d+)*)\.dmg/i)
   end
 
   auto_updates true
