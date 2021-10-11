c9194c5923cd44ee6fc351654c8f6b1c9f96297f
miccal <miccal.matthews@gmail.com>
2021-10-11 12:50:03 +0800

    Update 010-editor from 12.0 to 12.0.1

--- Casks/010-editor.rb
+++ Casks/010-editor.rb
@@ -1,6 +1,6 @@
 cask "010-editor" do
-  version "12.0"
-  sha256 "f1b2503e7115fc816f497267c611a9af23df6aa7bd08b6398ba29b80869db2be"
+  version "12.0.1"
+  sha256 "336cb1b7e055d3638e562e63539e90db4a795f738d67ca377c2e9dfac8670924"
 
   url "https://download.sweetscape.com/010EditorMac64Installer#{version}.dmg"
   name "010 Editor"
