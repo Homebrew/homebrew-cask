eb100a28d8ddea409be1ab10cf01733df1eaf177
miccal <miccal.matthews@gmail.com>
2021-10-11 13:43:35 +0800

    Update retroarch from 1.9.10 to 1.9.11

--- Casks/retroarch.rb
+++ Casks/retroarch.rb
@@ -1,6 +1,6 @@
 cask "retroarch" do
-  version "1.9.10"
-  sha256 "b02660085c494f2f96fcd477e791023768cd79da512d815950c75eae09ef8d54"
+  version "1.9.11"
+  sha256 "0fb36685203bf589406e5aa3d821d998902037c2f3b49836e952c66bef48516c"
 
   url "https://buildbot.libretro.com/stable/#{version}/apple/osx/x86_64/RetroArch.dmg"
   name "RetroArch"
