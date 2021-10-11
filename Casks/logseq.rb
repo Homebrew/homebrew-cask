98f37df840f769f89fcca212070251d4702ffb40
Bryan Sakowski <bgs113@users.noreply.github.com>
2021-10-11 10:09:47 -0500

    Update logseq from 0.4.2 to 0.4.3 (#112424)
    * Update logseq from 0.4.2 to 0.4.3
    
    * simplify multiarch logic
    
    Co-authored-by: George Adams <george.adams@microsoft.com>

--- Casks/logseq.rb
+++ Casks/logseq.rb
@@ -1,12 +1,13 @@
 cask "logseq" do
-  version "0.4.2"
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
+  version "0.4.3"
+
+  url "https://github.com/logseq/logseq/releases/download/#{version}/logseq-darwin-#{arch}-#{version}.dmg"
   if Hardware::CPU.intel?
-    sha256 "e8bf8d623ac4076f521bf72e1f3c60a206f05359c2066e28f7a1f1a130b95cde"
-    url "https://github.com/logseq/logseq/releases/download/#{version}/logseq-darwin-x64-#{version}.dmg"
+    sha256 "5ad1f9f2469cb3bbd7fe8e1aefddc4f61da1d4096db3f16a7e7dd0feda4f7e95"
   else
-    sha256 "e5053facc669a3a7e5e5d8d17f732186adb6b9b04227428efabeb70e110ae6c1"
-    url "https://github.com/logseq/logseq/releases/download/#{version}/logseq-darwin-arm64-#{version}.dmg"
+    sha256 "79f6192a9755b24cb3db82801f040eef1e894a6802dc9bbcf192b15a98b17e6e"
   end
 
   name "Logseq"
