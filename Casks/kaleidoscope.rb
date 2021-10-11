f883fd825d393f6dfa9c8b4d4e49ecfcb9a78de2
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-11 09:05:19 +0800

    Update kaleidoscope.rb

--- Casks/kaleidoscope.rb
+++ Casks/kaleidoscope.rb
@@ -1,33 +1,23 @@
 cask "kaleidoscope" do
-  if MacOS.version <= :catalina
-    version "2.4.2,1455:sep-7-2021"
-    sha256 "d2ccd4c63781f836f2b5018ba51c286829929d43d74ea6300144e7c7a50f911a"
-    url "https://updates.kaleidoscope.app/v2/prod/Kaleidoscope-#{version.before_comma}-#{version.after_comma.before_colon}-#{version.after_colon}.app.zip"
+  version "3.0.2,2013"
+  sha256 "ff89bee907b911678009ebdcf75423e940b11d48b0379d22a7873d562a91f500"
 
-    livecheck do
-      url "https://updates.kaleidoscope.app/v#{version.major}/prod/appcast"
-      strategy :sparkle do |item|
-        match = item.url.match(%r{/Kaleidoscope-(\d+(?:\.\d+)*)-(\d+)-(\w+(?:-\d+)*)\.app\.zip}i)
-        "#{match[1]},#{match[2]}:#{match[3]}"
-      end
-    end
-  else
-    version "3.0.2,2013"
-    sha256 "ff89bee907b911678009ebdcf75423e940b11d48b0379d22a7873d562a91f500"
-    url "https://updates.kaleidoscope.app/v#{version.major}/prod/Kaleidoscope-#{version.before_comma}-#{version.after_comma}.app.zip"
-
-    livecheck do
-      url "https://updates.kaleidoscope.app/v#{version.major}/prod/appcast"
-      strategy :sparkle
-    end
-  end
-
+  url "https://updates.kaleidoscope.app/v#{version.major}/prod/Kaleidoscope-#{version.before_comma}-#{version.after_comma}.app.zip"
   name "Kaleidoscope"
   desc "Spot and merge differences in text and image files or folders"
   homepage "https://www.kaleidoscope.app/"
 
+  livecheck do
+    url "https://updates.kaleidoscope.app/v#{version.major}/prod/appcast"
+    regex(/Kaleidoscope-(\d+(?:\.\d+)*)-(\d+)\.app\.zip/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
+    end
+  end
+
   auto_updates true
-  depends_on macos: ">= :high_sierra"
+  conflicts_with cask: "homebrew/cask-versions/kaleidoscope2"
+  depends_on macos: ">= :big_sur"
 
   app "Kaleidoscope.app"
   binary "#{appdir}/Kaleidoscope.app/Contents/Resources/bin/ksdiff"
