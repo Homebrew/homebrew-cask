51f47f89cea82185fdfb2743f23272a5fdc65350
jeff <i-v5@qq.com>
2021-10-11 15:35:08 +0800

    Update utools from 2.1.0 to 2.3.2

--- Casks/utools.rb
+++ Casks/utools.rb
@@ -1,6 +1,6 @@
 cask "utools" do
-  version "2.1.0"
-  sha256 "3cc48b309b696ca5c92f11e0d32d96edf25ce0772ff5e279365f09e7c402bc03"
+  version "2.3.2"
+  sha256 "30f34614e963bba79a65b2a7192c848101bcec9b21610ed055951a0bcd4c7600"
 
   url "https://publish.u-tools.cn/version2/uTools-#{version}.dmg",
       verified: "publish.u-tools.cn/"
