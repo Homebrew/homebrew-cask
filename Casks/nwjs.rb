c53a146fbfff36915b9b44f96d3e83efb49de4fb
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-11 14:26:38 +0800

    Update nwjs from 0.57.0 to 0.57.1 (#112399)

--- Casks/nwjs.rb
+++ Casks/nwjs.rb
@@ -1,6 +1,6 @@
 cask "nwjs" do
-  version "0.57.0"
-  sha256 "b364fc26f0980cf8963368ec9d076f4d2b37d8f562fe006fbf3e8a0d2d68f7de"
+  version "0.57.1"
+  sha256 "384fa622633bb9fd22625a75bb13a0eef4a828535dd94eab913893ea3149b33f"
 
   url "https://dl.nwjs.io/v#{version}/nwjs-sdk-v#{version}-osx-x64.zip"
   name "NW.js"
