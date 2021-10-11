d92ec43acbb2c94b6fe35726620fe8c68605c5b9
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-11 14:26:51 +0800

    Update metabase from 0.40.5 to 0.41.0 (#112398)

--- Casks/metabase.rb
+++ Casks/metabase.rb
@@ -1,6 +1,6 @@
 cask "metabase" do
-  version "0.40.5"
-  sha256 "e81948cc08ee643101a72ad4eb39e39e1e019accd549983f4d1af891ba9e53bf"
+  version "0.41.0"
+  sha256 "9347d7f5b238ec75beb1ddf9b5aa518b010009a64d78c8ccd5de13568bcbb2c3"
 
   url "https://s3.amazonaws.com/downloads.metabase.com/v#{version}/Metabase.zip",
       verified: "s3.amazonaws.com/downloads.metabase.com/"
