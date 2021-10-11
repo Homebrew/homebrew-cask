c37c6bcc82b601d2d54dc070a6aa222ae2eede34
Naveen Kumar <naveenk2908@gmail.com>
2021-10-11 06:45:47 +0530

    Update cask `kaleidoscope.app` to `v3` (#112361)
    * Update cask 'kaleidoscope.app' to 'v3'
    
    * add if block for previous version
    
    * add if block for previous version
    
    * Update kaleidoscope.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/kaleidoscope.rb
+++ Casks/kaleidoscope.rb
@@ -1,31 +1,38 @@
 cask "kaleidoscope" do
-  version "2.4.2,1455:sep-7-2021"
-  sha256 "d2ccd4c63781f836f2b5018ba51c286829929d43d74ea6300144e7c7a50f911a"
+  version "3.0.2,2013"
+  sha256 "ff89bee907b911678009ebdcf75423e940b11d48b0379d22a7873d562a91f500"
 
-  url "https://updates.kaleidoscope.app/v2/prod/Kaleidoscope-#{version.before_comma}-#{version.after_comma.before_colon}-#{version.after_colon}.app.zip"
+  url "https://updates.kaleidoscope.app/v#{version.major}/prod/Kaleidoscope-#{version.before_comma}-#{version.after_comma}.app.zip"
   name "Kaleidoscope"
   desc "Spot and merge differences in text and image files or folders"
   homepage "https://www.kaleidoscope.app/"
 
   livecheck do
     url "https://updates.kaleidoscope.app/v#{version.major}/prod/appcast"
-    strategy :sparkle do |item|
-      match = item.url.match(%r{/Kaleidoscope-(\d+(?:\.\d+)*)-(\d+)-(\w+(?:-\d+)*)\.app\.zip}i)
-      "#{match[1]},#{match[2]}:#{match[3]}"
+    regex(/Kaleidoscope-(\d+(?:\.\d+)*)-(\d+)\.app\.zip/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
     end
   end
 
   auto_updates true
-  depends_on macos: ">= :high_sierra"
+  conflicts_with cask: "homebrew/cask-versions/kaleidoscope2"
+  depends_on macos: ">= :big_sur"
 
   app "Kaleidoscope.app"
   binary "#{appdir}/Kaleidoscope.app/Contents/Resources/bin/ksdiff"
 
   zap trash: [
+    "~/Library/Application Support/app.kaleidoscope.v*",
     "~/Library/Application Support/com.blackpixel.kaleidoscope",
+    "~/Library/Application Support/Kaleidoscope",
     "~/Library/Caches/com.blackpixel.kaleidoscope",
+    "~/Library/Caches/app.kaleidoscope.v*",
     "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.blackpixel.kaleidoscope",
+    "~/Library/Preferences/app.kaleidoscope.v*.plist",
     "~/Library/Preferences/com.blackpixel.kaleidoscope.plist",
+    "~/Library/Saved Application State/app.kaleidoscope.v*.savedState",
     "~/Library/Saved Application State/com.blackpixel.kaleidoscope.savedState",
+    "~/Library/WebKit/app.kaleidoscope.v*",
   ]
 end
