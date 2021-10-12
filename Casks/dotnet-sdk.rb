adeb8b205c4e610dada7b152ff5a8eec78602862
Dmytro Kyshchenko <sys2712@gmail.com>
2021-10-13 00:18:59 +0300

    Update dotnet-sdk from 5.0.401,846d5680-b804-4903-8d8d-255804bcfaeb:436101afc96998f75efb452f5ded3c1a to 5.0.402,88bc1553-e90f-4a4f-9574-65d9a5065cd2:1d5646e1abb8b4d4a61ba0b0be976047 (#112479)

--- Casks/dotnet-sdk.rb
+++ Casks/dotnet-sdk.rb
@@ -1,6 +1,6 @@
 cask "dotnet-sdk" do
-  version "5.0.401,846d5680-b804-4903-8d8d-255804bcfaeb:436101afc96998f75efb452f5ded3c1a"
-  sha256 "fe93ad11180f6d482832a846f982f439173dafeed7ffe2747c2ca270cbc63b63"
+  version "5.0.402,88bc1553-e90f-4a4f-9574-65d9a5065cd2:1d5646e1abb8b4d4a61ba0b0be976047"
+  sha256 "525d0ff470e3d0e16b06787e72bdaf34df385007ee9125aa6f30eced69ef6a61"
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
   name ".NET SDK"
