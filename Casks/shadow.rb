996bbdc3d9e8fa0c46c13ef18c2ece8f0ee68635
Alexander Lauster <alexander@macwinlin.de>
2021-10-12 21:29:08 +0200

    Update Shadow to use ARM version when needed
    - Add contition to use ARM client on Apple Silicon
    - Update to latest version
    - Add description (copy from Shadow website)

--- Casks/shadow.rb
+++ Casks/shadow.rb
@@ -1,10 +1,16 @@
 cask "shadow" do
-  version "5.0.809.6322"
+  version "5.0.1020"
   sha256 :no_check
 
-  url "https://update.shadow.tech/launcher/prod/mac/Shadow.dmg"
   name "Shadow"
+  desc "Your gaming PC powered by Cloud technology"
   homepage "https://shadow.tech/"
+
+  if Hardware::CPU.intel?
+    url "https://update.shadow.tech/launcher/prod/mac/x64/Shadow.dmg"
+  else
+    url "https://update.shadow.tech/launcher/prod/mac/arm64/Shadow.dmg"
+  end
 
   app "Shadow.app"
 
