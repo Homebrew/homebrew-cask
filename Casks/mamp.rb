4e41cabac26cafa8398ee37216d380fdcd7fb1ea
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-13 11:04:54 +0800

    mamp: various updates (#112494)
    * Update mamp.rb
    
    * Update mamp.rb

--- Casks/mamp.rb
+++ Casks/mamp.rb
@@ -1,12 +1,13 @@
 cask "mamp" do
+  arch = Hardware::CPU.intel? ? "Intel-x86" : "M1-arm"
+
   version "6.6"
 
+  url "https://downloads.mamp.info/MAMP-PRO/releases/#{version}/MAMP_MAMP_PRO_#{version}-#{arch}.pkg"
   if Hardware::CPU.intel?
     sha256 "306b101a84251655b8e1d50ef1c4d59901d300f85d0e03910701d8d418d4a4d4"
-    url "https://downloads.mamp.info/MAMP-PRO/releases/#{version}/MAMP_MAMP_PRO_#{version}-Intel-x86.pkg"
   else
     sha256 "5d83861bcade99d6562bfb92c4a95f895f9e89496ca4c33380223ab9133640e6"
-    url "https://downloads.mamp.info/MAMP-PRO/releases/#{version}/MAMP_MAMP_PRO_#{version}-M1-arm.pkg"
   end
 
   name "MAMP"
@@ -22,11 +23,7 @@ cask "mamp" do
   auto_updates true
   depends_on macos: ">= :sierra"
 
-  if Hardware::CPU.intel?
-    pkg "MAMP_MAMP_PRO_#{version}-Intel-x86.pkg"
-  else
-    pkg "MAMP_MAMP_PRO_#{version}-M1-arm.pkg"
-  end
+  pkg "MAMP_MAMP_PRO_#{version}-#{arch}.pkg"
 
   postflight do
     set_ownership ["/Applications/MAMP", "/Applications/MAMP PRO"]
