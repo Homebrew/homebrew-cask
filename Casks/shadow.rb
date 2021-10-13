a0092157659c28336838be5f0c2db8338884765c
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-13 08:04:58 +0800

    Update shadow.rb

--- Casks/shadow.rb
+++ Casks/shadow.rb
@@ -1,22 +1,20 @@
 cask "shadow" do
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
+
   version "5.0.1020"
   sha256 :no_check
 
+  url "https://update.shadow.tech/launcher/prod/mac/#{arch}/Shadow.dmg"
+      verified: "commondatastorage.googleapis.com/chromium-browser-snapshots/"
   name "Shadow"
   desc "Your gaming PC powered by Cloud technology"
   homepage "https://shadow.tech/"
 
-  if Hardware::CPU.intel?
-    url "https://update.shadow.tech/launcher/prod/mac/x64/Shadow.dmg"
-  else
-    url "https://update.shadow.tech/launcher/prod/mac/arm64/Shadow.dmg"
-  end
-
   app "Shadow.app"
 
   zap trash: [
+    "~/Library/Application Support/Shadow",
     "~/Library/Preferences/com.electron.shadow.helper.plist",
     "~/Library/Preferences/com.electron.shadow.plist",
-    "~/Library/Application Support/Shadow",
   ]
 end
