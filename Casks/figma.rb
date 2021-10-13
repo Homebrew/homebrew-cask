0e6cb31ba0260e698c4c93361b00c50ae0121d0d
Bevan Kay <email@bevankay.me>
2021-10-13 17:22:17 +1100

    figma: various updates

--- Casks/figma.rb
+++ Casks/figma.rb
@@ -1,29 +1,23 @@
 cask "figma" do
+  arch = Hardware::CPU.intel? ? "mac" : "mac-arm"
+
   version "102.9.0"
 
   if Hardware::CPU.intel?
     sha256 "88d30dc1fd870d32ae705828cafe77ace5120dab28a157824ba58405910cffbc"
-
-    url "https://desktop.figma.com/mac/Figma-#{version}.zip"
-
-    livecheck do
-      url "https://desktop.figma.com/mac/RELEASE.json"
-      regex(%r{/Figma[._-]v?(\d+(?:\.\d+)+)\.zip}i)
-    end
   else
     sha256 "b1c1b23b1f31e98f11383582c32a84c1a2ea16a93cd09f1da91b2326328d30c1"
-
-    url "https://desktop.figma.com/mac-arm/Figma-#{version}.zip"
-
-    livecheck do
-      url "https://desktop.figma.com/mac-arm/RELEASE.json"
-      regex(%r{/Figma[._-]v?(\d+(?:\.\d+)+)\.zip}i)
-    end
   end
 
+  url "https://desktop.figma.com/#{arch}/Figma-#{version}.zip"
   name "Figma"
   desc "Collaborative team software"
   homepage "https://www.figma.com/"
+
+  livecheck do
+    url "https://desktop.figma.com/#{arch}/RELEASE.json"
+    regex(%r{/Figma[._-]v?(\d+(?:\.\d+)+)\.zip}i)
+  end
 
   auto_updates true
 
