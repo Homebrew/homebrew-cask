3bc0157fcc8672c5cac227f004fc416fd51c4455
Bevan Kay <email@bevankay.me>
2021-10-13 14:11:45 +1100

    Update adobe-acrobat-pro from 21.007.20096 to 21.007.20099 (#112495)

--- Casks/adobe-acrobat-pro.rb
+++ Casks/adobe-acrobat-pro.rb
@@ -1,5 +1,5 @@
 cask "adobe-acrobat-pro" do
-  version "21.007.20096"
+  version "21.007.20099"
   sha256 :no_check
 
   url "https://trials.adobe.com/AdobeProducts/APRO/Acrobat_HelpX/osx10/Acrobat_DC_Web_WWMUI.dmg",
