7ddf675c4514923118a753fd94c641ba37fdadf4
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-13 10:17:15 +0800

    Update djl-bench.rb

--- Casks/djl-bench.rb
+++ Casks/djl-bench.rb
@@ -2,8 +2,7 @@ cask "djl-bench" do
   version "0.13.0"
   sha256 "4d33d58fee830b19b009ded9328cddc158bf7d89251435d379603bdf61faa63f"
 
-  url "https://publish.djl.ai/djl-bench/#{version}/benchmark-#{version}.zip",
-      verified: "publish.djl.ai/djl-bench"
+  url "https://publish.djl.ai/djl-bench/#{version}/benchmark-#{version}.zip"
   name "djl-bench"
   desc "Universal machine learning model benchmark tool"
   homepage "https://djl.ai/"
