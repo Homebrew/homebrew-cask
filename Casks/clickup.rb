93919568b190518b7ed7a0f4fc2d3b6ee28514b0
Bevan Kay <email@bevankay.me>
2021-10-13 13:58:39 +1100

    Update clickup from 3.0.2 to 3.0.3

--- Casks/clickup.rb
+++ Casks/clickup.rb
@@ -1,14 +1,14 @@
 cask "clickup" do
-  version "3.0.2"
+  version "3.0.3"
 
   if Hardware::CPU.intel?
     url "https://download.todesktop.com/210531zdwwjv8ke/ClickUp%20#{version}-x64.dmg",
         verified: "download.todesktop.com/210531zdwwjv8ke/"
-    sha256 "3ccd6ccf5121afe591c79ed674a8090bfff90c551237d288c6fb24ae0b5b2501"
+    sha256 "27cf93a049e7fbf5689c39f2a6dd7dd8a3d428b907af142c1702653597773082"
   else
     url "https://download.todesktop.com/210531zdwwjv8ke/ClickUp%20#{version}-arm64.dmg",
         verified: "download.todesktop.com/210531zdwwjv8ke/"
-    sha256 "8a792730e506cfc80cb344d14a74d2b049b046be18a1e19b49e7ace9237d82d0"
+    sha256 "32829cb826bb440c9f563cbcf6934747f358fd6679a7c8032c6b6bada014f1d7"
   end
 
   name "ClickUp"
