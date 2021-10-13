369bea9e58e5d3474353d57020cc34af21ebbe8f
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-14 07:50:47 +0800

    Update zettlr.rb

--- Casks/zettlr.rb
+++ Casks/zettlr.rb
@@ -1,14 +1,14 @@
 cask "zettlr" do
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
+
   version "1.8.9"
 
+  url "https://github.com/Zettlr/Zettlr/releases/download/v#{version}/Zettlr-#{version}-#{arch}.dmg",
+      verified: "github.com/Zettlr/Zettlr/"
   if Hardware::CPU.intel?
     sha256 "4030d9b3a6f7fd7b7fc8a033f4512b09671965293821f892ae4a5554b5f22150"
-    url "https://github.com/Zettlr/Zettlr/releases/download/v#{version}/Zettlr-#{version}-x64.dmg",
-        verified: "github.com/Zettlr/Zettlr/"
   else
     sha256 "9308a45a7f0b84f55cef26d8004d3e9a48224fe41eaa8490853cbe3295f36ee6"
-    url "https://github.com/Zettlr/Zettlr/releases/download/v#{version}/Zettlr-#{version}-arm64.dmg",
-        verified: "github.com/Zettlr/Zettlr/"
   end
 
   name "Zettlr"
