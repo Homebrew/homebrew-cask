b2896fd6daf3c233c419c99a39a6b90767f65d45
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-13 08:40:13 +0800

    Update microsoft-edge from 94.0.992.38 to 94.0.992.47 (#112489)

--- Casks/microsoft-edge.rb
+++ Casks/microsoft-edge.rb
@@ -1,14 +1,14 @@
 cask "microsoft-edge" do
-  version "94.0.992.38"
+  version "94.0.992.47"
 
   if Hardware::CPU.intel?
     url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdge-#{version}.pkg",
         verified: "officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/"
-    sha256 "fb1bd12401ea3e38bc5cb7ed24cae4772ed5bd56c6d00506b3dd8e84d4ef1e69"
+    sha256 "eb33a78567d10e807cace157c2011cf5d8440e3ae7581938485ee3b6332ba210"
   else
     url "https://officecdn-microsoft-com.akamaized.net/pr/03ADF619-38C6-4249-95FF-4A01C0FFC962/MacAutoupdate/MicrosoftEdge-#{version}.pkg",
         verified: "officecdn-microsoft-com.akamaized.net/pr/03ADF619-38C6-4249-95FF-4A01C0FFC962/MacAutoupdate/"
-    sha256 "3860e2548c86280d707c6f32d662565fa630ae8068be0fe51b13c1a2e24bad0a"
+    sha256 "bab296267980c045d8a5888f228ce2f6fa98c9fbcda4c5f59dc452e2d16b31a5"
   end
 
   name "Microsoft Edge"
