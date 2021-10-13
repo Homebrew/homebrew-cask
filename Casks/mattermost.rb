3db23432934ac66c67477018bf74d2dd9713998f
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-14 07:15:49 +0800

    Update mattermost.rb

--- Casks/mattermost.rb
+++ Casks/mattermost.rb
@@ -1,12 +1,13 @@
 cask "mattermost" do
+  arch = Hardware::CPU.intel? ? "x64" : "m1"
+
   version "5.0.0"
 
+  url "https://releases.mattermost.com/desktop/#{version}/mattermost-desktop-#{version}-mac-#{arch}.zip"
   if Hardware::CPU.intel?
     sha256 "78529aea98c0a6d98ce68da76ee4b982bfcfb8a7078d4a553341ad3e79f3cc6e"
-    url "https://releases.mattermost.com/desktop/#{version}/mattermost-desktop-#{version}-mac-x64.zip"
   else
     sha256 "cae46ab7c963fb8ececef42f204b3e10583f5446ebb8bd2331dfac0d6eb36bbd"
-    url "https://releases.mattermost.com/desktop/#{version}/mattermost-desktop-#{version}-mac-m1.zip"
   end
 
   name "Mattermost"
@@ -16,7 +17,7 @@ cask "mattermost" do
   livecheck do
     url "https://docs.mattermost.com/install/desktop-app-install.html#macos-10-9"
     strategy :page_match
-    regex(%r{href=.*?/mattermost-desktop-(\d+(?:\.\d+)*)-mac-x64\.dmg}i)
+    regex(%r{href=.*?/mattermost-desktop-(\d+(?:\.\d+)*)-mac-#{arch}\.dmg}i)
   end
 
   app "Mattermost.app"
