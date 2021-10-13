1c906d7a1f8e252110e282ef891b3d93a6403aa9
miccal <miccal.matthews@gmail.com>
2021-10-13 19:17:09 +0800

    Update wing-personal from 8.0.2.0 to 8.0.4.0

--- Casks/wing-personal.rb
+++ Casks/wing-personal.rb
@@ -1,12 +1,12 @@
 cask "wing-personal" do
-  version "8.0.2.0"
+  version "8.0.4.0"
 
   if Hardware::CPU.intel?
-    sha256 "2a4b953bcc849286bfbe2958dc0ab41d3ed2507e4f79a04d319baae8458181d5"
+    sha256 "b64920cae3308f38396e3ffd5e7345c872f44db4462cce34fe6f016179da2392"
 
     url "https://wingware.com/pub/wing-personal/#{version}/wing-personal-#{version}-intel.dmg"
   else
-    sha256 "1d242c6215354e0b8464005a6ea24855a39d27c92a536a39e10faea3570f955f"
+    sha256 "e1461efb56477d4137a3a35063a94f4738241ebf2bf73caaca620ebd04f6a2dc"
 
     url "https://wingware.com/pub/wing-personal/#{version}/wing-personal-#{version}-arm64.dmg"
   end
