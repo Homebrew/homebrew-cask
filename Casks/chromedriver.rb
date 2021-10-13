496fe2422aae69bd9f7a1c8a0db1b700803de4eb
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-14 07:47:32 +0800

    Update chromedriver.rb

--- Casks/chromedriver.rb
+++ Casks/chromedriver.rb
@@ -1,16 +1,14 @@
 cask "chromedriver" do
+  arch = Hardware::CPU.intel? ? "mac64" : "mac64_m1"
+
   version "94.0.4606.61"
 
+  url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_#{arch}.zip",
+      verified: "chromedriver.storage.googleapis.com/"
   if Hardware::CPU.intel?
     sha256 "3b91860aa4d71330e68cac11da586ac39adcead361ae95b3fa856fd3da5383ac"
-
-    url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_mac64.zip",
-        verified: "chromedriver.storage.googleapis.com/"
   else
     sha256 "f32d6de39c14d531268ab5882c5144253f3bdb84fc210fd08c435f13bd5a7dce"
-
-    url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_mac64_m1.zip",
-        verified: "chromedriver.storage.googleapis.com/"
   end
 
   name "ChromeDriver"
