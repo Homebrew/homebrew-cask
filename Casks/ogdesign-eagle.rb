b4626171fd033673e6434f8c3e361466ede4e69c
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-14 15:21:31 +0800

    Update ogdesign-eagle.rb

--- Casks/ogdesign-eagle.rb
+++ Casks/ogdesign-eagle.rb
@@ -1,17 +1,16 @@
 cask "ogdesign-eagle" do
+  arch = Hardware::CPU.intel? ? "build" : "M1-build"
+
   version "2.0,37"
 
+  url "https://eagleapp.s3-accelerate.amazonaws.com/releases/Eagle-#{version.before_comma}-#{arch}#{version.after_comma}.dmg",
+      verified: "eagleapp.s3-accelerate.amazonaws.com/"
   if Hardware::CPU.intel?
     sha256 "de2035bb67ed397e278f8accd841f3a0ae9f6a5d802d1a9f6a7536a999beca26"
-    url "https://eagleapp.s3-accelerate.amazonaws.com/releases/Eagle-#{version.before_comma}-build#{version.after_comma}.dmg",
-        verified: "eagleapp.s3-accelerate.amazonaws.com/"
   else
     sha256 "bc6315aa3092412ecad2093fbffa5e6fad7ccb37cb1f556725c04e236a015773"
-    url "https://eagleapp.s3-accelerate.amazonaws.com/releases/Eagle-#{version.before_comma}-M1-build#{version.after_comma}.dmg",
-        verified: "eagleapp.s3-accelerate.amazonaws.com/"
   end
 
-  appcast "https://eagle.cool/changelog"
   name "Eagle"
   desc "Organize all your reference images in one place"
   homepage "https://eagle.cool/macOS"
