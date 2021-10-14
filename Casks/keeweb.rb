fea32c244ebb86c56316b3b20c692c877f501cd7
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-14 15:25:27 +0800

    Update keeweb.rb

--- Casks/keeweb.rb
+++ Casks/keeweb.rb
@@ -1,14 +1,14 @@
 cask "keeweb" do
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
+
   version "1.18.7"
 
+  url "https://github.com/keeweb/keeweb/releases/download/v#{version}/KeeWeb-#{version}.mac.x64.dmg",
+      verified: "github.com/keeweb/keeweb/"
   if Hardware::CPU.intel?
     sha256 "f99146aebc34b59ec5ea56ffde2048c860feb69d69b958643efd7485fa7a0135"
-    url "https://github.com/keeweb/keeweb/releases/download/v#{version}/KeeWeb-#{version}.mac.x64.dmg",
-        verified: "github.com/keeweb/keeweb/"
   else
     sha256 "6e4870b1660b91e735eaf30e7d751c7bb8dfae623d5b6c47899bd4d5ab1e6cae"
-    url "https://github.com/keeweb/keeweb/releases/download/v#{version}/KeeWeb-#{version}.mac.arm64.dmg",
-        verified: "github.com/keeweb/keeweb/"
   end
 
   name "KeeWeb"
