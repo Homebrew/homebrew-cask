c0169329a8b6fbddcd4f350d716669cd1dbbce5c
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-14 17:46:42 +0800

    Update drawio.rb

--- Casks/drawio.rb
+++ Casks/drawio.rb
@@ -1,14 +1,14 @@
 cask "drawio" do
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
+
   version "15.4.0"
 
+  url "https://github.com/jgraph/drawio-desktop/releases/download/v#{version}/draw.io-#{arch}-#{version}.dmg",
+      verified: "github.com/jgraph/drawio-desktop/"
   if Hardware::CPU.intel?
     sha256 "58e628ebae2c203c54d7e1ab031ffe572126d100507d06558cf48eb7455cd6cf"
-    url "https://github.com/jgraph/drawio-desktop/releases/download/v#{version}/draw.io-x64-#{version}.dmg",
-        verified: "github.com/jgraph/drawio-desktop/"
   else
     sha256 "9baee8d9dce105a22e5d9c04589007fcd0a482e6d72416b033e5e4485a42ed2a"
-    url "https://github.com/jgraph/drawio-desktop/releases/download/v#{version}/draw.io-arm64-#{version}.dmg",
-        verified: "github.com/jgraph/drawio-desktop/"
   end
 
   name "draw.io Desktop"
