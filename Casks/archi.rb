37ebca368b4e886a5bbabeb8e64da073379f3dc4
arnetkachev <arnetkachev@users.noreply.github.com>
2021-10-14 15:54:19 +0300

    Update archi from 4.8.1 to 4.9.0 (#112636)

--- Casks/archi.rb
+++ Casks/archi.rb
@@ -1,8 +1,15 @@
 cask "archi" do
-  version "4.8.1"
-  sha256 "c67f1a72a35393d9dabbef375529341b2f9f5006bf7bf4c7f242c661644a956c"
+  arch = Hardware::CPU.intel? ? "" : "-Silicon"
 
-  url "https://www.archimatetool.com/downloads/archi/?dl=#{version}/Archi-Mac-#{version}.dmg",
+  version "4.9.0"
+
+  if Hardware::CPU.intel?
+    sha256 "72d90bdd79762ccdf2daf463d6ea950a863df33cde5e1e5ae054ee8ee5e43d04"
+  else
+    sha256 "317a72129385f80829d1cb20d21a5fed76764afeb26f1eb4fc2f6e566f80b56d"
+  end
+
+  url "https://www.archimatetool.com/downloads/archi/?d1=#{version}/Archi-Mac#{arch}-#{version}.dmg",
       using: :post
   name "Archi"
   name "ArchiMate Tool"
@@ -20,7 +27,8 @@ cask "archi" do
   app "Archi.app"
 
   zap trash: [
-    "~/Library/Application Support/Archi#{version.major}",
+    "~/Library/Application Support/Archi*",
+    "~/Library/Caches/com.archimatetool.editor",
     "~/Library/Preferences/com.archimatetool.editor.plist",
     "~/Library/Saved Application State/com.archimatetool.editor.savedState",
   ]
