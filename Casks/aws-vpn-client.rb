af874b37ee4448405e286d1a3e6be7d9be4e8bf6
Marc Wensauer <marc@dreambox.com>
2021-10-13 18:08:09 -0700

    Address brew style warnings

--- Casks/aws-vpn-client.rb
+++ Casks/aws-vpn-client.rb
@@ -2,12 +2,13 @@ cask "aws-vpn-client" do
   version "1.3.5"
   sha256 :no_check
 
+  # Release notes: https://docs.aws.amazon.com/vpn/latest/clientvpn-user/release-notes.html#release-notes-macos
+
   url "https://d20adtppz83p9s.cloudfront.net/OSX/1.3.5/AWS_VPN_Client.pkg",
       verified: "d20adtppz83p9s.cloudfront.net/"
   name "AWS Client VPN"
-  desc "A managed client-based VPN service that enables you to securely access AWS resources and resources in your on-premises network."
+  desc "Managed client-based VPN service to securely access AWS resources"
   homepage "https://aws.amazon.com/vpn/"
-  # Release notes: https://docs.aws.amazon.com/vpn/latest/clientvpn-user/release-notes.html#release-notes-macos
 
   pkg "AWS_VPN_Client.pkg"
 
