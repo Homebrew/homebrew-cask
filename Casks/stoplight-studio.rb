3f589047c9cc388222cdc4904e45bc09e1798366
miccal <miccal.matthews@gmail.com>
2021-10-14 17:30:00 +0800

    Update stoplight-studio from 2.3.0,5931.git-67616e9 to 2.3.0,6547.git-2cf5b62

--- Casks/stoplight-studio.rb
+++ Casks/stoplight-studio.rb
@@ -1,13 +1,13 @@
 cask "stoplight-studio" do
-  version "2.3.0,5931.git-67616e9"
+  version "2.3.0,6547.git-2cf5b62"
 
   if Hardware::CPU.intel?
-    sha256 "aac31ad905e62e0fcdb3fdb24f54702be128105d9a45a4a7c81efac9f993a829"
+    sha256 "9ee898c6d1b440fb58c7b568c8f31c3a5923425b6a70c7c4e8f8be1f519cb2e7"
 
     url "https://github.com/stoplightio/studio/releases/download/v#{version.before_comma}-stable.#{version.after_comma}/stoplight-studio-mac.dmg",
         verified: "github.com/stoplightio/studio/"
   else
-    sha256 "75dfef898970ddc78200b10a7e1e7187fd4106826a7fc64b9146b02aec041acf"
+    sha256 "4e520f53154e1195404dec8b49b6196fef70fc96a1c4766e335cbca54e5ba027"
 
     url "https://github.com/stoplightio/studio/releases/download/v#{version.before_comma}-stable.#{version.after_comma}/stoplight-studio-mac-arm64.dmg",
         verified: "github.com/stoplightio/studio/"
