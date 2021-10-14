db4d96b957d6ceba719afe7a7f059601bdd1788f
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-14 11:53:03 +0800

    Update blockbench.rb (#112581)

--- Casks/blockbench.rb
+++ Casks/blockbench.rb
@@ -1,16 +1,14 @@
 cask "blockbench" do
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
+
   version "3.9.3"
 
+  url "https://github.com/JannisX11/blockbench/releases/download/v#{version}/Blockbench_#{arch}_#{version}.dmg",
+      verified: "github.com/JannisX11/blockbench/"
   if Hardware::CPU.intel?
     sha256 "e8c0b70e7cf7659186580a6de7dc8aee00167a37f211a6e59cbf693b87178129"
-
-    url "https://github.com/JannisX11/blockbench/releases/download/v#{version}/Blockbench_x64_#{version}.dmg",
-        verified: "github.com/JannisX11/blockbench/"
   else
     sha256 "d6bc651b1d4f1f184a8dfc8856ce92a51217f177b206edee9e33113c5ed4a047"
-
-    url "https://github.com/JannisX11/blockbench/releases/download/v#{version}/Blockbench_arm64_#{version}.dmg",
-        verified: "github.com/JannisX11/blockbench/"
   end
 
   name "Blockbench"
