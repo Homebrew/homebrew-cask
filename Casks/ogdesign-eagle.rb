df9651a2152dcd79bdd6524e5618de1f4bb46f77
miccal <miccal.matthews@gmail.com>
2021-10-14 15:17:09 +0800

    Update ogdesign-eagle from 2.0,8 to 2.0,37

--- Casks/ogdesign-eagle.rb
+++ Casks/ogdesign-eagle.rb
@@ -1,12 +1,12 @@
 cask "ogdesign-eagle" do
-  version "2.0,8"
+  version "2.0,37"
 
   if Hardware::CPU.intel?
-    sha256 "fd487e03ab89bc85d6a9dff65286f8e2b20a91cfd66d2dfe20624536505af891"
+    sha256 "de2035bb67ed397e278f8accd841f3a0ae9f6a5d802d1a9f6a7536a999beca26"
     url "https://eagleapp.s3-accelerate.amazonaws.com/releases/Eagle-#{version.before_comma}-build#{version.after_comma}.dmg",
         verified: "eagleapp.s3-accelerate.amazonaws.com/"
   else
-    sha256 "66d1bcd58ec4efd2426ee40aefe834fa2e367f40e90b798f4625123a8967cef6"
+    sha256 "bc6315aa3092412ecad2093fbffa5e6fad7ccb37cb1f556725c04e236a015773"
     url "https://eagleapp.s3-accelerate.amazonaws.com/releases/Eagle-#{version.before_comma}-M1-build#{version.after_comma}.dmg",
         verified: "eagleapp.s3-accelerate.amazonaws.com/"
   end
