aae92016cee0577fcb97fdba637db23d506238f8
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-14 12:25:08 +0800

    Update dcv-viewer.rb

--- Casks/dcv-viewer.rb
+++ Casks/dcv-viewer.rb
@@ -1,16 +1,14 @@
 cask "dcv-viewer" do
+  arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
+
   version "2021.2.3776"
 
+  url "https://d1uj6qtbmh3dt5.cloudfront.net/#{version.major_minor}/Clients/nice-dcv-viewer-#{version}.#{arch}.dmg",
+      verified: "d1uj6qtbmh3dt5.cloudfront.net/"
   if Hardware::CPU.intel?
     sha256 "0d72af77d33d472c9f6523036809e5936f516851d1c788a909674dc857fe0e78"
-
-    url "https://d1uj6qtbmh3dt5.cloudfront.net/#{version.major_minor}/Clients/nice-dcv-viewer-#{version}.x86_64.dmg",
-        verified: "d1uj6qtbmh3dt5.cloudfront.net/"
   else
     sha256 "86061eeacf8c59fa71a08eb00cf65e918193e2007d47224820049431c2b370bb"
-
-    url "https://d1uj6qtbmh3dt5.cloudfront.net/#{version.major_minor}/Clients/nice-dcv-viewer-#{version}.arm64.dmg",
-        verified: "d1uj6qtbmh3dt5.cloudfront.net/"
   end
 
   name "NICE DCV Viewer"
@@ -19,7 +17,7 @@ cask "dcv-viewer" do
 
   livecheck do
     url :homepage
-    regex(%r{href=.*?/nice-dcv-viewer[._-]v?(\d+(?:\.\d+)+)[._-]x86_64\.dmg}i)
+    regex(%r{href=.*?/nice-dcv-viewer[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}\.dmg}i)
   end
 
   depends_on macos: ">= :mojave"
