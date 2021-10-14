6d03c06260a639dd7c673efa85f8b4f7d5dc3d00
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-14 11:37:50 +0800

    Update nota.rb

--- Casks/nota.rb
+++ Casks/nota.rb
@@ -1,16 +1,14 @@
 cask "nota" do
+  arch = Hardware::CPU.intel? ? "mac" : "arm64-mac"
+
   version "0.33.0"
 
+  url "https://github.com/notaapp/releases/releases/download/#{version}/Nota-#{version}-#{arch}.zip",
+      verified: "github.com/notaapp/releases/"
   if Hardware::CPU.intel?
     sha256 "6c86abf1886b2b7ac96220e6f5ea11d851d50c5457f284bd2d401991d07b233a"
-
-    url "https://github.com/notaapp/releases/releases/download/#{version}/Nota-#{version}-mac.zip",
-        verified: "github.com/notaapp/releases/"
   else
     sha256 "dfd6ed72b9cae7f61df43dc887f9c433ce97f4d72b4ebf8487594f89d91ce034"
-
-    url "https://github.com/notaapp/releases/releases/download/#{version}/Nota-#{version}-arm64-mac.zip",
-        verified: "github.com/notaapp/releases/"
   end
 
   name "Nota"
