72179caa0b2d86d604c1669477cd12e0404321ef
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-14 08:55:37 +0800

    Update raindropio.rb (#112573)

--- Casks/raindropio.rb
+++ Casks/raindropio.rb
@@ -1,21 +1,25 @@
 cask "raindropio" do
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
+
   version "5.4.2"
 
+  url "https://github.com/raindropio/desktop/releases/download/v#{version}/Raindrop-#{arch}.dmg",
+      verified: "github.com/raindropio/desktop/"
   if Hardware::CPU.intel?
     sha256 "0247825f9c23cf0cdc24dc1ada4b63a9113c76010a60355b52d432e3c1faada1"
-
-    url "https://github.com/raindropio/desktop/releases/download/v#{version}/Raindrop-x64.dmg",
-        verified: "github.com/raindropio/desktop/"
   else
     sha256 "001d27bf87dcf46517e400f146227a330db16d34376bca2f996cbb668ee9d6a3"
-
-    url "https://github.com/raindropio/desktop/releases/download/v#{version}/Raindrop-arm64.dmg",
-        verified: "github.com/raindropio/desktop/"
   end
 
   name "Raindrop.io"
   desc "All-in-one bookmark manager"
   homepage "https://raindrop.io/"
+
+  livecheck do
+    url "https://github.com/raindropio/desktop/releases"
+    strategy :page_match
+    regex(%r{v?(\d+(?:\.\d+)*)/Raindrop[._-]#{arch}\.dmg}i)
+  end
 
   auto_updates true
 
