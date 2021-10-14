4053015b1459d85c07cb3ea15e5e206cd190b8ca
Marc Wensauer <marc@dreambox.com>
2021-10-13 17:59:41 -0700

    aws-vpn-client: use specific version

--- Casks/aws-vpn-client.rb
+++ Casks/aws-vpn-client.rb
@@ -1,11 +1,12 @@
 cask "aws-vpn-client" do
-  version :latest
+  version "1.3.5"
   sha256 :no_check
 
-  url "https://d20adtppz83p9s.cloudfront.net/OSX/latest/AWS_VPN_Client.pkg",
+  url "https://d20adtppz83p9s.cloudfront.net/OSX/1.3.5/AWS_VPN_Client.pkg",
       verified: "d20adtppz83p9s.cloudfront.net/"
   name "AWS Client VPN"
   homepage "https://aws.amazon.com/vpn/"
+  # Release notes: https://docs.aws.amazon.com/vpn/latest/clientvpn-user/release-notes.html#release-notes-macos
 
   pkg "AWS_VPN_Client.pkg"
 
