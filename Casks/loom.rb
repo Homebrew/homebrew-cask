ccb3fede64f8356578ab3184fd7ac409d098ea17
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-14 15:05:14 +0800

    Update loom.rb

--- Casks/loom.rb
+++ Casks/loom.rb
@@ -1,14 +1,14 @@
 cask "loom" do
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
+
   version "0.102.0"
 
+  url "https://cdn.loom.com/desktop-packages/Loom-#{version}#{arch}.dmg",
+      verified: ""
   if Hardware::CPU.intel?
     sha256 "141d4740c91ea4c4809d9fd0f749c23e8841cf979b59d6fdc6499925189c8323"
-
-    url "https://cdn.loom.com/desktop-packages/Loom-#{version}.dmg"
   else
     sha256 "e1249d8816ac68a619d1d524f8d4e790326e83735fbd61e5ddf89570a17b85ee"
-
-    url "https://cdn.loom.com/desktop-packages/Loom-#{version}-arm64.dmg"
   end
 
   name "Loom"
