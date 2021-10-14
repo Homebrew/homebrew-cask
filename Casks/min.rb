9903619780c71e6ea678c072a5dce8aeee3ba1b2
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-14 15:09:49 +0800

    Update min.rb

--- Casks/min.rb
+++ Casks/min.rb
@@ -1,16 +1,14 @@
 cask "min" do
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
+
   version "1.21.0"
 
+  url "https://github.com/minbrowser/min/releases/download/v#{version}/Min-v#{version}-darwin-#{arch}.zip",
+      verified: "github.com/minbrowser/min/"
   if Hardware::CPU.intel?
     sha256 "4b81d90a2dc6fa5799219409af93ba2ff19e851742c9cfce73bafc3105d8ad5e"
-
-    url "https://github.com/minbrowser/min/releases/download/v#{version}/Min-v#{version}-darwin-x64.zip",
-        verified: "github.com/minbrowser/min/"
   else
     sha256 "31160e1f8dd6662d58e8cb8570c7814ddc631266060ce45a2f9466a9879cd520"
-
-    url "https://github.com/minbrowser/min/releases/download/v#{version}/Min-v#{version}-darwin-arm64.zip",
-        verified: "github.com/minbrowser/min/"
   end
 
   name "Min"
