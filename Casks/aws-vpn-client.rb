f2fd3060278bf0c2d7ee91c8e0b74d74d044a952
Bevan Kay <email@bevankay.me>
2021-10-14 12:16:02 +1100

    version url, add livecheck, add sha256

--- Casks/aws-vpn-client.rb
+++ Casks/aws-vpn-client.rb
@@ -1,14 +1,17 @@
 cask "aws-vpn-client" do
   version "1.3.5"
-  sha256 :no_check
+  sha256 "b91d74fa5d528fddfbb21f803be8649db1a2891b421a24338a67486d80ff7a67"
 
-  # Release notes: https://docs.aws.amazon.com/vpn/latest/clientvpn-user/release-notes.html#release-notes-macos
-
-  url "https://d20adtppz83p9s.cloudfront.net/OSX/1.3.5/AWS_VPN_Client.pkg",
+  url "https://d20adtppz83p9s.cloudfront.net/OSX/#{version}/AWS_VPN_Client.pkg",
       verified: "d20adtppz83p9s.cloudfront.net/"
   name "AWS Client VPN"
   desc "Managed client-based VPN service to securely access AWS resources"
   homepage "https://aws.amazon.com/vpn/"
+
+  livecheck do
+    url "https://docs.aws.amazon.com/vpn/latest/clientvpn-user/release-notes.html#release-notes-macos"
+    regex(%r{href=.*?v?(\d+(?:\.\d+)+)/AWS_VPN_Client\.pkg}i)
+  end
 
   pkg "AWS_VPN_Client.pkg"
 
