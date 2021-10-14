26b1c2c8e663ce3e27a4db38a774f3bee6dd1379
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-14 11:51:23 +0800

    Update calmly-writer.rb

--- Casks/calmly-writer.rb
+++ Casks/calmly-writer.rb
@@ -1,14 +1,13 @@
 cask "calmly-writer" do
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
+
   version "2.0.34"
 
+  url "https://www.calmlywriter.com/releases/#{arch}/Calmly%20Writer-#{version}.pkg"
   if Hardware::CPU.intel?
     sha256 "da4216eb6328358e5043292ac6a241aa7d30353df64acfbde78c02e3e27774d2"
-
-    url "https://www.calmlywriter.com/releases/x64/Calmly%20Writer-#{version}.pkg"
   else
     sha256 "f72907c250e0666476e30341685c0b3364cbcc92d84c376a4cbe63594e53151e"
-
-    url "https://www.calmlywriter.com/releases/arm64/Calmly%20Writer-#{version}.pkg"
   end
 
   name "Calmly Writer"
@@ -16,7 +15,7 @@ cask "calmly-writer" do
   homepage "https://calmlywriter.com/"
 
   livecheck do
-    url "https://calmlywriter.com/releases/x64/download.php"
+    url "https://calmlywriter.com/releases/#{arch}/download.php"
     strategy :header_match
   end
 
