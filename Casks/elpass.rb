76c4e3f522857f6e21b766fc6d3934cbffcfb318
LB <liubx1988@gmail.com>
2021-10-14 17:36:55 +0800

    Update elpass from 1.3.5,363 to 1.4.1,400

--- Casks/elpass.rb
+++ Casks/elpass.rb
@@ -1,6 +1,6 @@
 cask "elpass" do
-  version "1.3.5,363"
-  sha256 "326ca7831ca16e6469be4bc8843d8338804e86d0d6cf15dd3fd25aff5347f842"
+  version "1.4.1,400"
+  sha256 "ba62571f86d1d3b94bbf7f54b5cafecdbfca4f7c10995ce71b50cd2872faf628"
 
   url "https://elpass.app/macos/Elpass-#{version.before_comma}-#{version.after_comma}.zip"
   name "Elpass"
