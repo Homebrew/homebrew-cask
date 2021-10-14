2649a3ab9b2c5239cb7278d961947889bc046804
miccal <miccal.matthews@gmail.com>
2021-10-14 20:17:55 +0800

    Update libreoffice from 7.2.1 to 7.2.2

--- Casks/libreoffice.rb
+++ Casks/libreoffice.rb
@@ -1,13 +1,13 @@
 cask "libreoffice" do
-  version "7.2.1"
+  version "7.2.2"
 
   if Hardware::CPU.intel?
-    sha256 "b83e841360fa6ee9e42bfc1579ce9ae135972d6e7a01e79d1b975c2b855ff152"
+    sha256 "dc2fd0577e3ee4f99c79d235a6efcd8fecc7069d24090c4eaea69e0fad8245ae"
 
     url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg",
         verified: "documentfoundation.org/"
   else
-    sha256 "890136b6735f50bd9fe014fd49ff56e995b04ad3b5115f441ca0cfafd50becc7"
+    sha256 "83954ea5bae605aba26fb0d731e080ee04e9e748a3148c43620af6ef2154f611"
 
     url "http://download.documentfoundation.org/libreoffice/stable/#{version}/mac/aarch64/LibreOffice_#{version}_MacOS_aarch64.dmg",
         verified: "documentfoundation.org/"
