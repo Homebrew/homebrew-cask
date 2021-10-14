1ed657a61b44c6abdd1a8163be76ee42a077d930
Hannes Braun <hannesbraun@mail.de>
2021-10-15 00:51:29 +0200

    Update gramps from 5.1.4,2 to 5.1.4,3

--- Casks/gramps.rb
+++ Casks/gramps.rb
@@ -1,6 +1,6 @@
 cask "gramps" do
-  version "5.1.4,2"
-  sha256 "dc8e2518a8d17650edaada09ee1fa6b1e42ca440bd14c5fbd0d35d93499e5550"
+  version "5.1.4,3"
+  sha256 "2e780ada1aeea7803094f190afecde6ed321840974a8925a8f711ba9f879c3bd"
 
   url "https://github.com/gramps-project/gramps/releases/download/v#{version.before_comma}/Gramps-Intel-#{version.before_comma}-#{version.after_comma}.dmg",
       verified: "github.com/gramps-project/gramps/"
