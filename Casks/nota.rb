20559b13391c15f79a1aa0e204c97cb6751b418d
miccal <miccal.matthews@gmail.com>
2021-10-14 11:36:09 +0800

    Update nota from 0.31.0 to 0.33.0

--- Casks/nota.rb
+++ Casks/nota.rb
@@ -1,13 +1,13 @@
 cask "nota" do
-  version "0.31.0"
+  version "0.33.0"
 
   if Hardware::CPU.intel?
-    sha256 "95fa52e74389411e779d0cb0c96bb7583537ef3674df3f387084b8c5461c4731"
+    sha256 "6c86abf1886b2b7ac96220e6f5ea11d851d50c5457f284bd2d401991d07b233a"
 
     url "https://github.com/notaapp/releases/releases/download/#{version}/Nota-#{version}-mac.zip",
         verified: "github.com/notaapp/releases/"
   else
-    sha256 "6731b6c0c534997e5b576cc0b9253b304f1e058b363c24723673dcb77ca1d927"
+    sha256 "dfd6ed72b9cae7f61df43dc887f9c433ce97f4d72b4ebf8487594f89d91ce034"
 
     url "https://github.com/notaapp/releases/releases/download/#{version}/Nota-#{version}-arm64-mac.zip",
         verified: "github.com/notaapp/releases/"
