792e0e82b02b5fa29592f7fd3ad22ea165e125f1
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-10-15 15:08:01 +0200

    Fix whitespace

--- Casks/pdfify.rb
+++ Casks/pdfify.rb
@@ -1,7 +1,7 @@
 cask "pdfify" do
   version "3.3.3-136"
   sha256 "08c72b5e4bf83b1e948ec87caf74965191c505ed81eb08f6f7dce3741ccb49d0"
- 
+
   url "https://pdfify.app/get/cask/download/macos/PDFify-#{version}.zip"
   name "PDFify"
   desc "Create searchable and smaller PDF"
@@ -12,7 +12,7 @@ cask "pdfify" do
     strategy :page_match
     regex(%r{href=.*?/PDFify-(\d+(?:\.\d+)*-\d+)\.zip}i)
   end
- 
+
   depends_on macos: ">= :high_sierra"
 
   app "PDFify.app"
