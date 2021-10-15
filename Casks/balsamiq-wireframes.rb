abab8df26c2d5b596115f14e98152bbbd6313daa
Michael Cho <cho-m@tuta.io>
2021-10-14 23:55:02 -0700

    Update balsamiq-wireframes from 4.3.2 to 4.3.3

--- Casks/balsamiq-wireframes.rb
+++ Casks/balsamiq-wireframes.rb
@@ -1,6 +1,6 @@
 cask "balsamiq-wireframes" do
-  version "4.3.2"
-  sha256 "0ade50afa661a524b37a31cefaa22af66dbcdfa8bd5e83dc8d9283fb4e95e4c5"
+  version "4.3.3"
+  sha256 "aa2225b2b8d511e1a6cc68a427bd35da8422df7571dda1bac3f055967f1f962b"
 
   url "https://builds.balsamiq.com/bwd/Balsamiq%20Wireframes%20#{version}.dmg"
   name "Balsamiq Wireframes"
