aa2cc07868068f46dc3c13b5a50d5e29306d3cb7
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-15 17:58:35 +0800

    Update loom from 0.100.3 to 0.102.0 (#112595)
    * Update loom from 0.100.3 to 0.102.0
    
    * Update loom.rb
    
    * Update loom.rb

--- Casks/loom.rb
+++ Casks/loom.rb
@@ -1,14 +1,13 @@
 cask "loom" do
-  version "0.100.3"
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
 
+  version "0.102.0"
+
+  url "https://cdn.loom.com/desktop-packages/Loom-#{version}#{arch}.dmg"
   if Hardware::CPU.intel?
-    sha256 "c23f725dd41918bc3defc888662fc8ccf0e734115badc5ff852bc60f5f44dcbb"
-
-    url "https://cdn.loom.com/desktop-packages/Loom-#{version}.dmg"
+    sha256 "141d4740c91ea4c4809d9fd0f749c23e8841cf979b59d6fdc6499925189c8323"
   else
-    sha256 "f526b4ea7ac13ccaa6f84081615bf8f6c2af4cf13f3754f252636c04524deaa8"
-
-    url "https://cdn.loom.com/desktop-packages/Loom-#{version}-arm64.dmg"
+    sha256 "e1249d8816ac68a619d1d524f8d4e790326e83735fbd61e5ddf89570a17b85ee"
   end
 
   name "Loom"
