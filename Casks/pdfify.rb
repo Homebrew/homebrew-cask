51f99c537983d82a4a41eefd574e53cc31c1fb7e
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-10-15 14:58:56 +0200

    Add PDFify v3.3.3

---
+++ Casks/pdfify.rb
@@ -0,0 +1,18 @@
+cask "pdfify" do
+  version "3.3.3-136"
+  sha256 "08c72b5e4bf83b1e948ec87caf74965191c505ed81eb08f6f7dce3741ccb49d0"
+ 
+  name "PDFify"
+  url "https://pdfify.app/get/cask/download/macos/PDFify-#{version}.zip"
+  homepage "https://pdfify.app/"
+
+  livecheck do
+    url "https://pdfify.app/updater-macos"
+    strategy :page_match
+    regex(%r{href=.*?/PDFify-(\d+(?:\.\d+)*-\d+)\.zip}i)
+  end
+ 
+  depends_on macos: ">= :high_sierra"
+
+  app "PDFify.app"
+end
