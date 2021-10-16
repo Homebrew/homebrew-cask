27c4f91abe4ccc8dc4e4c8746d558533a5382309
525056 <89318644+525056@users.noreply.github.com>
2021-10-16 11:40:28 +0200

    Update trezor-suite from 21.9.2 to 21.10.2

--- Casks/trezor-suite.rb
+++ Casks/trezor-suite.rb
@@ -1,8 +1,14 @@
 cask "trezor-suite" do
-  version "21.9.2"
-  sha256 "6423cbc17171a89e7bf801411e5db5d7018b84071df46ba667873ba598ae2af6"
+  version "21.10.2"
 
-  url "https://suite.trezor.io/web/static/desktop/Trezor-Suite-#{version}-mac.dmg"
+  if Hardware::CPU.intel?
+    sha256 "f2043d03f8bd71373f09634cedf44425702398b3453ab6d541683a8accd58d2a"
+    url "https://suite.trezor.io/web/static/desktop/Trezor-Suite-#{version}-mac-x64.dmg"
+  else
+    sha256 "5a7eeac02cfa9efa4fb411b49cbe1fe7a763e8e1f043a300def1532d4da836da"
+    url "https://suite.trezor.io/web/static/desktop/Trezor-Suite-#{version}-mac-arm64.dmg"
+  end
+
   name "TREZOR Suite"
   desc "Companion app for the Trezor hardware wallet"
   homepage "https://suite.trezor.io/"
