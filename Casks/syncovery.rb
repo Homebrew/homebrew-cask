8bd0b6423e33caa719c0011e04226efd4030adf4
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-16 15:40:40 +0800

    Update syncovery.rb

--- Casks/syncovery.rb
+++ Casks/syncovery.rb
@@ -1,14 +1,13 @@
 cask "syncovery" do
+  arch = Hardware::CPU.intel? ? "" : "-Apple"
+
   version "9.39e"
 
+  url "https://www.syncovery.com/release/SyncoveryMac#{version}#{arch}.dmg"
   if Hardware::CPU.intel?
     sha256 "0fd98c967548c011eef41c1d87159211d66528863993f1c451ba72c6be248771"
-
-    url "https://www.syncovery.com/release/SyncoveryMac#{version}.dmg"
   else
     sha256 "9b70beaccc8f95d710064943d45e60c52618f242653a13df6eecdea09ce47fe5"
-
-    url "https://www.syncovery.com/release/SyncoveryMac#{version}-Apple.dmg"
   end
 
   name "Syncovery"
@@ -17,7 +16,7 @@ cask "syncovery" do
 
   livecheck do
     url "https://www.syncovery.com/download/mac/"
-    regex(%r{href=.*?/SyncoveryMac(\d+(?:\.\d+)*[a-z]?)\.dmg}i)
+    regex(%r{href=.*?/SyncoveryMac(\d+(?:\.\d+)*[a-z]?)#{arch}\.dmg}i)
   end
 
   pkg "SyncoveryMac (double-click to install).pkg"
