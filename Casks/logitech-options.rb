309fdc8b09d32026743b0b0d37476913b83b2e11
wilmouths <wilmouthsteven@gmail.com>
2021-10-16 12:54:52 +0200

    style: lint after brew style

--- Casks/logitech-options.rb
+++ Casks/logitech-options.rb
@@ -3,7 +3,7 @@ cask "logitech-options" do
   sha256 :no_check
 
   url "https://download01.logi.com/web/ftp/pub/techsupport/options/options_installer.zip",
-    verified: 'download01.logi.com/'
+      verified: "download01.logi.com/"
   name "Logitech Options"
   desc "Powerful and easy-to-use application that aims to enhance your Logitech devices"
   homepage "https://www.logitech.com/product/options"
@@ -16,11 +16,12 @@ cask "logitech-options" do
 
   zap trash: [
     "~/Library/Application Support/Logitech",
-    "/Applications/Logi Options.app",
     "~/Library/Preferences/com.logitech.Logi-Options.plist",
     "~/Library/Preferences/com.logitech.manager.daemon.plist",
     "~/Library/Preferences/com.logitech.manager.devinfo.plist",
     "~/Library/Preferences/com.logitech.manager.setting.*.plist",
     "~/Library/Saved Application State/com.logitech.Logi-Options.savedState",
+    "/Applications/Logi Options.app",
+    "/Library/Application Support/Logitech.localized",
   ]
 end
