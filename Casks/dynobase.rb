8a7ee9c2d4901d278d0697e144f751c79b6ed275
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-16 15:38:43 +0800

    Update dynobase.rb

--- Casks/dynobase.rb
+++ Casks/dynobase.rb
@@ -1,16 +1,14 @@
 cask "dynobase" do
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
+
   version "1.7.1"
 
+  url "https://github.com/Dynobase/dynobase/releases/download/#{version}/Dynobase-#{version}#{arch}.dmg",
+      verified: "github.com/Dynobase/dynobase/"
   if Hardware::CPU.intel?
     sha256 "b8f2f1a65e4e5e8a6290f0b4ede3f9ccda3d198ed2142c5b059825cdb3699419"
-
-    url "https://github.com/Dynobase/dynobase/releases/download/#{version}/Dynobase-#{version}.dmg",
-        verified: "github.com/Dynobase/dynobase/"
   else
     sha256 "261d7e395aaa94ec8cb8fde540c8093ca2b2d35a8f3f77b10b286be9572a5ac3"
-
-    url "https://github.com/Dynobase/dynobase/releases/download/#{version}/Dynobase-#{version}-arm64.dmg",
-        verified: "github.com/Dynobase/dynobase/"
   end
 
   name "Dynobase"
