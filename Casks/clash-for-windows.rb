a89de3d4a9faf54de0c7dd50435b7ab71e32f828
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-16 15:37:24 +0800

    Update clash-for-windows.rb

--- Casks/clash-for-windows.rb
+++ Casks/clash-for-windows.rb
@@ -1,11 +1,12 @@
 cask "clash-for-windows" do
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
+
   version "0.18.4"
 
+  url "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/#{version}/Clash.for.Windows-#{version}#{arch}.dmg"
   if Hardware::CPU.intel?
-    url "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/#{version}/Clash.for.Windows-#{version}.dmg"
     sha256 "ff54b1a008c4ef5e3cbbac661e842e1e4390625a5343b9c20e981ca87add2ef3"
   else
-    url "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/#{version}/Clash.for.Windows-#{version}-arm64.dmg"
     sha256 "2e13eba179ba63491d51fe95978e66b5bf95657083cdbe0f7512918f180b22c5"
   end
 
