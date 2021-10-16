10269f06e28eeb92e26952f4de6f6ff02bd76251
Bevan Kay <email@bevankay.me>
2021-10-16 11:32:26 +1100

    Update proxyman from 2.34.0,23400 to 2.34.1,23410 (#112733)

--- Casks/proxyman.rb
+++ Casks/proxyman.rb
@@ -1,6 +1,6 @@
 cask "proxyman" do
-  version "2.34.0,23400"
-  sha256 "3530be0842ce9c1f0a3f264e911cb36dec01ac7b654c709b97ed408a133f9181"
+  version "2.34.1,23410"
+  sha256 "8d65b3bd2e8f7fad605e549fa372fde9c3a3773f4868b2101fa88b64fd129c69"
 
   url "https://download.proxyman.io/#{version.after_comma}/Proxyman_#{version.before_comma}.dmg"
   name "Proxyman"
