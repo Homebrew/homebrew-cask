754d78b06029d01b676aa79f64390f729aead02a
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-16 22:12:16 +0800

    Update microsoft-powerpoint.rb

--- Casks/microsoft-powerpoint.rb
+++ Casks/microsoft-powerpoint.rb
@@ -2,8 +2,8 @@ cask "microsoft-powerpoint" do
   version "16.54.21101001"
   sha256 "75a57c82b46d0e2558c454f19610576b7a48baf1ccc5cd1fa61b69cca5bf0bd1"
 
-  url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_PowerPoint_#{version}_Installer.pkg",
-      verified: "officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/"
+  url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_PowerPoint_#{version}_Installer.pkg",
+      verified: "officecdnmac.microsoft.com/"
   name "Microsoft PowerPoint"
   desc "Presentation software"
   homepage "https://products.office.com/en-US/powerpoint"
