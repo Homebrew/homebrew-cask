e8f51bcd66fb904704b7d8195a36643d561325ef
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-16 14:10:41 +0800

    Update ferdi.rb

--- Casks/ferdi.rb
+++ Casks/ferdi.rb
@@ -1,16 +1,14 @@
 cask "ferdi" do
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
+
   version "5.6.2"
 
+  url "https://github.com/getferdi/ferdi/releases/download/v#{version}/Ferdi-#{version}#{arch}.dmg",
+      verified: "github.com/getferdi/ferdi/"
   if Hardware::CPU.intel?
     sha256 "bfad74f6b22364f8ee80a3faf1b386707e76949c5eea68a47df11d298ee0bbc8"
-
-    url "https://github.com/getferdi/ferdi/releases/download/v#{version}/Ferdi-#{version}.dmg",
-        verified: "github.com/getferdi/ferdi/"
-  else
+ else
     sha256 "51dfa80046037bd53b3ace758b29b8a8f809b7493388cbcf96a408895ea089ea"
-
-    url "https://github.com/getferdi/ferdi/releases/download/v#{version}/Ferdi-#{version}-arm64.dmg",
-        verified: "github.com/getferdi/ferdi/"
   end
 
   name "Ferdi"
@@ -18,9 +16,8 @@ cask "ferdi" do
   homepage "https://getferdi.com/"
 
   livecheck do
-    url "https://github.com/getferdi/ferdi/releases/"
-    strategy :page_match
-    regex(/Ferdi[._-](\d+(?:\.\d+)*)\.dmg/i)
+    url :url
+    strategy :github_latest
   end
 
   auto_updates true
