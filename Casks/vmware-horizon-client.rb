b69371aec996f9066b6aefadf265d44786494214
Bevan Kay <email@bevankay.me>
2021-10-16 19:06:04 +1100

    possible livecheck

--- Casks/vmware-horizon-client.rb
+++ Casks/vmware-horizon-client.rb
@@ -3,10 +3,33 @@ cask "vmware-horizon-client" do
   sha256 "9a53ae726faf61353d45ad21573518d51d3c81fc20d0cc9dca952be1bae0a166"
 
   url "https://download3.vmware.com/software/view/viewclients/#{version.after_comma}/VMware-Horizon-Client-#{version.before_comma}.dmg"
-  appcast "https://my.vmware.com/channel/public/api/v1.0/dlg/details?locale=en_US&downloadGroup=#{version.after_comma}_MAC_2106.1"
   name "VMware Horizon Client"
   desc "Virtual machine client"
   homepage "https://www.vmware.com/"
+
+  livecheck do
+    url "https://customerconnect.vmware.com/channel/public/api/v1.0/products/getRelatedDLGList?locale=en_US&category=desktop_end_user_computing&product=vmware_horizon_clients&version=horizon_8&dlgType=PRODUCT_BINARY"
+    regex(%r{/([^/]+)/VMware-Horizon-Client-v?(\d+(?:[.-]\d+)+)\.dmg}i)
+    strategy :page_match do |page|
+      mac_json_info = JSON.parse(page)["dlgEditionsLists"].select { |item| item["name"].match(/mac/i) }&.first
+      api_item = mac_json_info["dlgList"]&.first
+      next if api_item.blank?
+
+      download_group = api_item["code"]
+      product_id = api_item["productId"]
+      pid = api_item["releasePackageId"]
+      next if download_group.blank? || product_id.blank? || pid.blank?
+
+      url = "https://customerconnect.vmware.com/channel/public/api/v1.0/dlg/details?locale=en_US&downloadGroup=#{download_group}&productId=#{product_id}&rPId=#{pid}"
+      download_item = Homebrew::Livecheck::Strategy.page_content(url)
+      next if download_item[:content].blank?
+
+      match = download_item[:content].match(regex)
+      next if match.blank?
+
+      "#{match[2]},#{match[1]}"
+    end
+  end
 
   depends_on macos: ">= :sierra"
 
