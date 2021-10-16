2ee25933ffe4440fd7c48056e788205c000e8662
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-16 15:47:19 +0800

    Update stack-stack.rb

--- Casks/stack-stack.rb
+++ Casks/stack-stack.rb
@@ -4,8 +4,7 @@ cask "stack-stack" do
   if Hardware::CPU.intel?
     sha256 "682af014498759c38723666d7fbaa67915fc08636d5b3c4e73e302b4b14f7cc7"
 
-    url "https://stack-v1.s3.amazonaws.com/builds/prod/Stack-#{version}.dmg",
-        verified: "stack-v1.s3.amazonaws.com/"
+    url "https://binaries.getstack.app/builds/prod/Stack-#{version}.dmg"
   else
     sha256 "b2e866d059c9736810a172d02d91b443807aa4b3d9df6dffd50914b341ef2740"
 
@@ -17,7 +16,7 @@ cask "stack-stack" do
   homepage "https://getstack.app/"
 
   livecheck do
-    url "https://stack-v1.s3.amazonaws.com/builds/prod/latest-mac.yml"
+    url "https://binaries.getstack.app/builds/prod/latest-mac.yml"
     strategy :electron_builder
   end
 
