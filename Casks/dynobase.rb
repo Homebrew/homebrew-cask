3a2be06496433c28efbc1b83731f5ac2e3893567
miccal <miccal.matthews@gmail.com>
2021-10-16 15:33:12 +0800

    Update dynobase from 1.6.0 to 1.7.1

--- Casks/dynobase.rb
+++ Casks/dynobase.rb
@@ -1,13 +1,13 @@
 cask "dynobase" do
-  version "1.6.0"
+  version "1.7.1"
 
   if Hardware::CPU.intel?
-    sha256 "9274091011226abc1b2e6c210350cd701f3d7308efdc34ca0c045fc68162971a"
+    sha256 "b8f2f1a65e4e5e8a6290f0b4ede3f9ccda3d198ed2142c5b059825cdb3699419"
 
     url "https://github.com/Dynobase/dynobase/releases/download/#{version}/Dynobase-#{version}.dmg",
         verified: "github.com/Dynobase/dynobase/"
   else
-    sha256 "39181015a6540a1c6c2f1b457c213029f0fe8051aa633064b5283c0b2a81713b"
+    sha256 "261d7e395aaa94ec8cb8fde540c8093ca2b2d35a8f3f77b10b286be9572a5ac3"
 
     url "https://github.com/Dynobase/dynobase/releases/download/#{version}/Dynobase-#{version}-arm64.dmg",
         verified: "github.com/Dynobase/dynobase/"
