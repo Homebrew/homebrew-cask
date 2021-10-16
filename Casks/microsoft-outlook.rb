45aa9b9b497b140b0d100a61b123ebf17fd8cc77
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-16 22:24:02 +0800

    Update microsoft-outlook.rb (#112767)

--- Casks/microsoft-outlook.rb
+++ Casks/microsoft-outlook.rb
@@ -2,8 +2,8 @@ cask "microsoft-outlook" do
   version "16.54.21101001"
   sha256 "c7b3ced52462b611a9762941088fa05e42d79b26349ca62b705a9bcbce00b41e"
 
-  url "https://officecdn-microsoft-com.akamaized.net/db/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Outlook_#{version}_Installer.pkg",
-      verified: "officecdn-microsoft-com.akamaized.net/db/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/"
+  url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Outlook_#{version}_Installer.pkg",
+      verified: "officecdnmac.microsoft.com/"
   name "Microsoft Outlook"
   desc "Email client"
   homepage "https://products.office.com/en-us/outlook/email-and-calendar-software-microsoft-outlook"
