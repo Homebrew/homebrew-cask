d857e8b7ac83161cd7b2e04047729a448831e12d
Erik Minekus <tsunami@tsunami-productions.nl>
2021-10-17 17:34:19 +0200

    Update querious from 3.1.3 to 3.1.4

--- Casks/querious.rb
+++ Casks/querious.rb
@@ -1,6 +1,6 @@
 cask "querious" do
-  version "3.1.3"
-  sha256 "84ea48bef83be094e0e77f4aca95edbf4eb4aea2db3dadbec0fe97deb63b2181"
+  version "3.1.4"
+  sha256 "53a7457ea0e3945f40744fdd5178d65504e153bfe3e6b6b07340b1a56272917f"
 
   url "https://www.araelium.com/querious/downloads/versions/Querious#{version}.zip"
   name "Querious #{version.major}"
