5f033868004a96e3fcf030a1304856d6ba8c7687
Jacob Ford <j@cobford.com>
2021-10-17 01:49:34 -0400

    Update sketch from 77,131065 to 78,131919 (#112792)
    * Update sketch from 77,131065 to 78,131919
    
    * Update sketch.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/sketch.rb
+++ Casks/sketch.rb
@@ -1,6 +1,6 @@
 cask "sketch" do
-  version "77,131065"
-  sha256 "621e21f128b7ca96a5f9f0e360322bb7c98a41a3b025509231afea364a839a21"
+  version "78,131919"
+  sha256 "e930775b8229beb500c2bbd5649019d002b99969ab3b292f32eb7e2f112d2d09"
 
   url "https://download.sketchapp.com/sketch-#{version.before_comma}-#{version.after_comma}.zip"
   name "Sketch"
@@ -20,14 +20,14 @@ cask "sketch" do
   zap trash: [
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.bohemiancoding.sketch3.sfl*",
     "~/Library/Application Support/com.bohemiancoding.sketch3",
+    "~/Library/Autosave Information/com.bohemiancoding.sketch3.plist",
     "~/Library/Caches/com.bohemiancoding.sketch3",
     "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.bohemiancoding.sketch3",
+    "~/Library/Cookies/com.bohemiancoding.sketch3.binarycookies",
+    "~/Library/HTTPStorages/com.bohemiancoding.sketch3.binarycookies",
     "~/Library/Logs/com.bohemiancoding.sketch3",
     "~/Library/Preferences/com.bohemiancoding.sketch3.LSSharedFileList.plist",
     "~/Library/Preferences/com.bohemiancoding.sketch3.plist",
-    "~/Library/Cookies/com.bohemiancoding.sketch3.binarycookies",
-    "~/Library/HTTPStorages/com.bohemiancoding.sketch3.binarycookies",
     "~/Library/Saved Application State/com.bohemiancoding.sketch3.savedState",
-    "~/Library/Autosave Information/com.bohemiancoding.sketch3.plist",
   ]
 end
