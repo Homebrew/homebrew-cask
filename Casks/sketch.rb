558fca0ba5a42c7559a98e217bccd8d186db867d
Bevan Kay <email@bevankay.me>
2021-10-17 16:38:48 +1100

    Update sketch.rb

--- Casks/sketch.rb
+++ Casks/sketch.rb
@@ -20,14 +20,14 @@ cask "sketch" do
   zap trash: [
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.bohemiancoding.sketch3.sfl*",
     "~/Library/Application Support/com.bohemiancoding.sketch3",
+    "~/Library/Autosave Information/com.bohemiancoding.sketch3.plist",
     "~/Library/Caches/com.bohemiancoding.sketch3",
     "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.bohemiancoding.sketch3",
+    "~/Library/Cookies/com.bohemiancoding.sketch3.binarycookies",
+    "~/Library/HTTPStorages/com.bohemiancoding.sketch3.binarycookies",
     "~/Library/Logs/com.bohemiancoding.sketch3",
     "~/Library/Preferences/com.bohemiancoding.sketch3.LSSharedFileList.plist",
     "~/Library/Preferences/com.bohemiancoding.sketch3.plist",
-    "~/Library/Cookies/com.bohemiancoding.sketch3.binarycookies",
-    "~/Library/HTTPStorages/com.bohemiancoding.sketch3.binarycookies",
     "~/Library/Saved Application State/com.bohemiancoding.sketch3.savedState",
-    "~/Library/Autosave Information/com.bohemiancoding.sketch3.plist",
   ]
 end
