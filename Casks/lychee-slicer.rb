277b8f69862265931d919760292fa1dda02b0ac2
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-18 07:46:03 +0800

    Update lychee-slicer.rb

--- Casks/lychee-slicer.rb
+++ Casks/lychee-slicer.rb
@@ -3,9 +3,9 @@ cask "lychee-slicer" do
   sha256 "afeb2fc1fd5756a6c902900cf762cef1e4fb92f913d1247ec2bfdc89d23a616f"
 
   url "https://mango-lychee.nyc3.cdn.digitaloceanspaces.com/LycheeSlicer-#{version}.dmg",
-      verified: "mango-lychee.nyc3.cdn.digitaloceanspaces.com"
+      verified: "mango-lychee.nyc3.cdn.digitaloceanspaces.com/"
   name "lychee-slicer"
-  desc "Powerful and versatile slicer for Resin 3D printers"
+  desc "Slicer for Resin 3D printers"
   homepage "https://mango3d.io/lychee-slicer-3-for-sla-3d-printers/"
 
   livecheck do
