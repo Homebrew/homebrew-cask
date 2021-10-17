b7d74baaa29aa8c6197010949b7d0a8bdb2232f5
Janek Łukaszewicz <janluk@google.com>
2021-10-17 23:21:57 +0200

    Add UVtools.app 2.23.6

---
+++ Casks/uvtools.rb
@@ -0,0 +1,11 @@
+cask "uvtools" do
+  version "2.23.6"
+  sha256 "936bc0db28a2eafa944245ceb8ab9f996c1afea72ad70fdb941eb91588f7d326"
+
+  url "https://github.com/sn4k3/UVtools/releases/download/v#{version}/UVtools_osx-x64_v#{version}.zip"
+  name "UVtools"
+  desc "MSLA/DLP, file analysis, calibration, repair, conversion and manipulation"
+  homepage "https://github.com/sn4k3/UVtools"
+
+  app "UVtools.app"
+end
