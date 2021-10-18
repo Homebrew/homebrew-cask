7335a4794d130c20e2a4e382d32e8e9f4c00ef68
miccal <miccal.matthews@gmail.com>
2021-10-18 14:36:56 +0800

    Update smplayer from 21.8.0 to 21.8.0.9999

--- Casks/smplayer.rb
+++ Casks/smplayer.rb
@@ -1,6 +1,6 @@
 cask "smplayer" do
-  version "21.8.0"
-  sha256 "f08c20e20090ddc1633fda543d2ec5d3428ad92ebecb864ef239c5a7de15812b"
+  version "21.8.0.9999"
+  sha256 "9265fe2bd2cf4eb9c47f77798fec5d58c93892c22dff7e2397f8865a970345be"
 
   url "https://github.com/smplayer-dev/smplayer/releases/download/v#{version}/smplayer-#{version}.dmg",
       verified: "github.com/smplayer-dev/smplayer/"
