369a252d33a3fb29cec9e4ec9c786c4df3f33eb2
arnetkachev <arnetkachev@users.noreply.github.com>
2021-10-18 23:35:58 +0300

    keka: various fixes

--- Casks/keka.rb
+++ Casks/keka.rb
@@ -8,15 +8,24 @@ cask "keka" do
   desc "File archiver"
   homepage "https://www.keka.io/"
 
+  livecheck do
+    url "https://u.keka.io"
+    strategy :sparkle, &:short_version
+  end
+
   auto_updates true
   conflicts_with cask: "homebrew/cask-versions/keka-beta"
 
   app "Keka.app"
 
   zap trash: [
+    "~/Library/Application Scripts/com.aone.keka",
+    "~/Library/Application Scripts/com.aone.keka.KekaFinderIntegration",
     "~/Library/Application Support/Keka",
     "~/Library/Caches/com.aone.keka",
     "~/Library/Containers/com.aone.keka",
+    "~/Library/Containers/com.aone.keka.KekaFinderIntegration",
+    "~/Library/Group Containers/4FG648TM2A.group.com.aone.keka",
     "~/Library/Preferences/com.aone.keka.plist",
     "~/Library/Saved Application State/com.aone.keka.savedState",
   ]
