10f6abf5d09ebd1bd77a52f0e8cce1ab157e74d7
Isa Farnik <if_then@hey.com>
2021-10-18 10:15:52 -0700

    inso: use zip instead of pkg

--- Casks/inso.rb
+++ Casks/inso.rb
@@ -1,8 +1,8 @@
 cask "inso" do
-  version "2.3.3-alpha.2"
-  sha256 "d2ee9b7e50f5e9c7ce75fd68405525f0155bc26efcd7aba2a43d9480b6134e56"
+  version "2.3.3-alpha.4"
+  sha256 "620f39604216f2519ed16d48e411823ff1f19589156c7c2a1e1adc3198f517ce"
 
-  url "https://github.com/Kong/insomnia/releases/download/lib%40#{version}/inso-macos-latest-#{version}.pkg",
+  url "https://github.com/Kong/insomnia/releases/download/lib%40#{version}/inso-macos-#{version}.zip",
       verified: "github.com/Kong/insomnia/"
   name "inso"
   desc "CLI HTTP and GraphQL Client"
@@ -11,14 +11,10 @@ cask "inso" do
   livecheck do
     url "https://github.com/Kong/insomnia/releases"
     strategy :page_match
-    regex(/href=.*?inso-macos-(?:latest-)*(\d+(?:\.\d+).*?)\.pkg/i)
+    regex(/href=.*?inso-macos-(?:latest-)*(\d+(?:\.\d+).*?)\.zip/i)
   end
 
   auto_updates true
 
-  pkg "inso-macos-latest-#{version}.pkg"
-
-  uninstall pkgutil: [
-    "com.insomnia.inso",
-  ]
+  binary "inso"
 end
