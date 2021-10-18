3b5699e9f7d9595397f897b37c9e8b1504fb7391
miccal <miccal.matthews@gmail.com>
2021-10-18 13:29:38 +0800

    Update electron from 15.1.2 to 15.2.0

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,14 +1,14 @@
 cask "electron" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "15.1.2"
+  version "15.2.0"
 
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-#{arch}.zip",
       verified: "github.com/electron/electron/"
   if Hardware::CPU.intel?
-    sha256 "f95f70790fa689684ec2d4822451e45968a2307ccd674a4378a89667f1e2c50d"
+    sha256 "32d3d4e5f7dbb8fe035a7b91dc64c042eb930461424784d4c450e06768e7162d"
   else
-    sha256 "edcfb7e83c5edad21b05d9babb4a2840157e68dbe4278c3ab3b609ece22e1372"
+    sha256 "a7982607416ca2d30d7f48fbc3b16ab072c46170b117123a5e9763f85227a5cb"
   end
 
   name "Electron"
