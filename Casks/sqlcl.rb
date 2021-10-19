6565b9d59e3845c2c548ec9838d5e8748c761e9c
Kris <kris.rice@oracle.com>
2021-10-19 10:14:15 -0400

    brew style added a \n

--- Casks/sqlcl.rb
+++ Casks/sqlcl.rb
@@ -6,7 +6,7 @@ cask "sqlcl" do
   name "sqlcl"
   desc "Oracle SQLcl is the modern command-line interface for the Oracle Database"
   homepage "https://www.oracle.com/sqlcl"
-  
+
   stage_only true
 
   caveats do
