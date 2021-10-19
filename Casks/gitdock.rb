178288bc366336fd05f54fa21db49e4326865d62
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-19 16:40:25 +0800

    Update gitdock.rb

--- Casks/gitdock.rb
+++ Casks/gitdock.rb
@@ -3,9 +3,15 @@ cask "gitdock" do
   sha256 "f9fecb7d026d1acaea71bf62439bda604d30688383993f34dd343283d44a4d52"
 
   url "https://gitlab.com/api/v4/projects/28462485/packages/generic/gitdock/#{version}/GitDock.dmg"
-  name "gitdock"
-  desc "Is an app that displays all your GitLab activities in one place"
+  name "GitDock"
+  desc "Displays all your GitLab activities in one place"
   homepage "https://gitlab.com/mvanremmerden/gitdock"
+
+  livecheck do
+    url "https://gitlab.com/api/v4/projects/28462485/releases"
+    strategy :page_match
+    regex(%r{gitdock/(\d+(?:\.\d+)*)/GitDock\.dmg}i)
+  end
 
   app "GitDock.app"
 end
