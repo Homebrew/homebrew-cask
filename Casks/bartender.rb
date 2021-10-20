e2781425d2142e2632ec0e7585c6f71f398bcfe4
Bevan Kay <email@bevankay.me>
2021-10-21 10:25:26 +1100

    update bartender url

--- Casks/bartender.rb
+++ Casks/bartender.rb
@@ -10,9 +10,9 @@ cask "bartender" do
     url "https://macbartender.com/B2/updates/#{version.before_comma.dots_to_hyphens}/Bartender%20#{version.major}.zip",
         referer: "https://www.macbartender.com"
   else
-    version "4.1.6,41006"
-    sha256 "3d9811e70d3c4f9158185988b03ec5b2f153bd0605cb77e12b847b40899c3bcb"
-    url "https://macbartender.com/B2/updates/B4Latest/Bartender%20#{version.major}.dmg",
+    version "4.1.5,41005"
+    sha256 "983b93098dc0cfff703b034e07c8399f8bceab4c096d90f79f0e2238ffa08131"
+    url "https://macbartender.com/B2/updates/#{version.before_comma.major}-#{version.before_comma.minor}-#{version.before_comma.patch.rjust(2, '0')}/Bartender%20#{version.major}.dmg",
         referer: "https://www.macbartender.com"
   end
 
