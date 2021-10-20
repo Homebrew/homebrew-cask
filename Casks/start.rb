e7da3d2b2b8d7b146767a4b5dcba833c6357aed8
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-20 18:27:30 +0800

    Update start.rb

--- Casks/start.rb
+++ Casks/start.rb
@@ -1,30 +1,25 @@
 cask "start" do
+  url_arch = Hardware::CPU.intel? ? "" : "m1/"
+  livecheck_arch = Hardware::CPU.intel? ? "" : "-arm"
+
   if Hardware::CPU.intel?
     version "0.298.2-127"
     sha256 "51e92e5f335a6fb84de3c85e4be2bd88b8abfece394f1555bfa60362ab264166"
-
-    url "https://imgcdn.start.qq.com/cdn/mac.client/installer/START-Installer-#{version}.dmg"
-
-    livecheck do
-      url "https://api.start.qq.com/cfg/get?biztypes=macos-update-info"
-      regex(%r{.*/START-Installer-([.\d\-]+)\.dmg}i)
-    end
   else
     version "0.298.2-14"
     sha256 "369337b4b8da24c5874ef00f49113453a59f0f8b7e0149f0a6e8a90855991fa2"
-
-    url "https://imgcdn.start.qq.com/cdn/mac.client/installer/m1/START-Installer-#{version}.dmg"
-
-    livecheck do
-      url "https://api.start.qq.com/cfg/get?biztypes=macos-update-info-arm"
-      regex(%r{.*/START-Installer-([.\d\-]+)\.dmg}i)
-    end
   end
 
+  url "https://imgcdn.start.qq.com/cdn/mac.client/installer/#{url_arch}START-Installer-#{version}.dmg"
   name "START"
   name "腾讯云游戏"
   desc "Tencent cloud gaming platform"
   homepage "https://start.qq.com/"
+
+  livecheck do
+    url "https://api.start.qq.com/cfg/get?biztypes=macos-update-info#{livecheck_arch}"
+    regex(%r{.*/START-Installer-([.\d\-]+)\.dmg}i)
+  end
 
   auto_updates true
   depends_on macos: ">= :sierra"
