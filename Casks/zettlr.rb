623d0c30e4499a6a906fba1711f256c0bbb9dd07
Hendrik Erz <hendrik@zettlr.com>
2021-10-20 10:04:01 +0200

    Update zettlr from 2.0.0 to 2.0.1

--- Casks/zettlr.rb
+++ Casks/zettlr.rb
@@ -1,13 +1,13 @@
 cask "zettlr" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "2.0.0"
+  version "2.0.1"
 
   url "https://github.com/Zettlr/Zettlr/releases/download/v#{version}/Zettlr-#{version}-#{arch}.dmg"
   if Hardware::CPU.intel?
-    sha256 "af7f989046c93e955513522089d102435ccba4c63b1947dfd0d12151f8373ee9"
+    sha256 "b758a7b1aea7e1238ff666b96cfbc2d136e5b06f51fa6b02152ae4f415236ad8"
   else
-    sha256 "132f008f3ae8b6b83dd8d65f84eab85722ef43f3243f67fa47589418e274c7c1"
+    sha256 "f23d612685ef1abeaf8b0cbf8bce466c0303e9e2e12eaf0ac8c1975cdc01aa43"
   end
 
   name "Zettlr"
