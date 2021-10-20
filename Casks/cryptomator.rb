d9149f346e57739d22228561bd024dc13fb7414e
yibo <7483137+xyb994@users.noreply.github.com>
2021-10-19 20:00:13 -0400

    Update cryptomator from 1.5.19 to 1.6.0 (#112940)

--- Casks/cryptomator.rb
+++ Casks/cryptomator.rb
@@ -1,6 +1,6 @@
 cask "cryptomator" do
-  version "1.5.19"
-  sha256 "6576c1425e1388d83be525f240a8140c8a73e10dfe2a3ecd65ff452f7c0d6c63"
+  version "1.6.0"
+  sha256 "75334656783bfcfe1c522d291b4671fd852615439523e5140aa3c48418d4c880"
 
   url "https://github.com/cryptomator/cryptomator/releases/download/#{version}/Cryptomator-#{version}.dmg",
       verified: "github.com/cryptomator/cryptomator/"
