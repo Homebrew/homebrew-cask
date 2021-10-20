75e8e9b348e6241fe9d55475f595b80aaff68591
Bevan Kay <email@bevankay.me>
2021-10-20 22:50:45 +1100

    sha256 :no_check

--- Casks/oracle-jdk.rb
+++ Casks/oracle-jdk.rb
@@ -2,14 +2,9 @@ cask "oracle-jdk" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
 
   version "17.0.1"
+  sha256 :no_check   # required as upstream package is updated in place
 
-  url "https://download.oracle.com/java/#{version.major}/latest/jdk-#{version}_macos-#{arch}_bin.dmg"
-  if Hardware::CPU.intel?
-    sha256 "1cddd0c4b505bc78459e4353d6dadb81c4a9c1f15934d87531bef7d4c52d8e51"
-  else
-    sha256 "5d88b9c8e51650b46545bcb95842025a58d80c068f9d8f8062a8b6276620d8d3"
-  end
-
+  url "https://download.oracle.com/java/#{version.major}/latest/jdk-#{version.major}_macos-#{arch}_bin.dmg"
   name "Oracle Java Standard Edition Development Kit"
   desc "JDK from Oracle"
   homepage "https://www.oracle.com/java/technologies/downloads/"
