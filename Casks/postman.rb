329928910c62109aa3a72c95d03c875c54d6bc86
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2021-10-21 06:58:15 +0500

    feat: add Apple Silicon support
    add Apple Silicon support with different support
    
    https://github.com/postmanlabs/postman-app-support/issues/9346#issuecomment-947573206

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,12 +1,20 @@
 cask "postman" do
-  version "9.1.1"
-  sha256 "8b84a23b699aa9b7f83488d1ca5b05a309f4cda1c26d0e4a90b69a459af19cbd"
-
-  url "https://dl.pstmn.io/download/version/#{version}/osx64",
-      verified: "dl.pstmn.io/download/version/"
+  
   name "Postman"
   desc "Collaboration platform for API development"
   homepage "https://www.postman.com/"
+  
+  if Hardware::CPU.intel?
+    version "9.1.1"
+    url "https://dl.pstmn.io/download/version/#{version}/osx64",
+      verified: "dl.pstmn.io/download/version/"
+    sha256 "8b84a23b699aa9b7f83488d1ca5b05a309f4cda1c26d0e4a90b69a459af19cbd"
+  else
+    version "9.0.7"
+    url "https://dl.pstmn.io/download/version/#{version}/osx_arm64",
+      verified: "dl.pstmn.io/download/version/"
+    sha256 "33a3b4686ddf873cfefa9301b763da411265c94ccd880b2607e7177129ffc8f2"
+  end
 
   livecheck do
     url "https://dl.pstmn.io/api/version/notes?from=#{version}&channel=stable&platform=osx"
