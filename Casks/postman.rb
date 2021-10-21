cf458459c4b71de54a025e5f6317008c76ff6033
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2021-10-21 07:05:53 +0500

    fix: this may fix linting issue

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -7,12 +7,12 @@ cask "postman" do
   if Hardware::CPU.intel?
     version "9.1.1"
     url "https://dl.pstmn.io/download/version/#{version}/osx64",
-      verified: "dl.pstmn.io/download/version/"
+       verified: "dl.pstmn.io/download/version/"
     sha256 "8b84a23b699aa9b7f83488d1ca5b05a309f4cda1c26d0e4a90b69a459af19cbd"
   else
     version "9.0.7"
     url "https://dl.pstmn.io/download/version/#{version}/osx_arm64",
-      verified: "dl.pstmn.io/download/version/"
+       verified: "dl.pstmn.io/download/version/"
     sha256 "33a3b4686ddf873cfefa9301b763da411265c94ccd880b2607e7177129ffc8f2"
   end
 
