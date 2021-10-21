576d6a2d3a5e02cfeea0521ea0af2fff1cdaeab5
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-22 07:29:19 +0800

    Update tinkerwell.rb

--- Casks/tinkerwell.rb
+++ Casks/tinkerwell.rb
@@ -1,16 +1,15 @@
 cask "tinkerwell" do
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
+
   version "2.18.0"
 
   if Hardware::CPU.intel?
     sha256 "8c162dc50eb19928c2313c449d0112b67495fd3a1ac89a538d5bb29c77bf33b2"
-
-    url "https://download.tinkerwell.app/tinkerwell/Tinkerwell-#{version}.dmg"
   else
     sha256 "ef8f2336cad0206a3a196f00faa4f975f2f241b44a20763cf54d35b9514ad3a5"
-
-    url "https://download.tinkerwell.app/tinkerwell/Tinkerwell-#{version}-arm64.dmg"
   end
 
+  url "https://download.tinkerwell.app/tinkerwell/Tinkerwell-#{version}#{arch}.dmg"
   name "Tinkerwell"
   desc "Tinker tool for PHP and Laravel developers"
   homepage "https://tinkerwell.app/"
