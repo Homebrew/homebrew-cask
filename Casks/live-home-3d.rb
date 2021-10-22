781980175d4fa2f331f0f503e347084a5a05f608
Jacob Ford <j@cobford.com>
2021-10-22 01:49:47 -0400

    Add livecheck

--- Casks/live-home-3d.rb
+++ Casks/live-home-3d.rb
@@ -7,5 +7,10 @@ cask "live-home-3d" do
   desc "Home & floorplan designer & renderer"
   homepage "https://www.livehome3d.com/mac/live-home-3d"
 
+  livecheck do
+    url "https://www.belightsoft.com/download/updates/appcast_lh3d.xml"
+    strategy :sparkle
+  end
+
   app "Live Home 3D.app"
 end
