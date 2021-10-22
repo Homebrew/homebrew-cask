b421acca467d3ed04cddc99ee3f91dd81cbf8fdf
Jacob Ford <j@cobford.com>
2021-10-22 01:30:45 -0400

    Create cask basics

---
+++ Casks/live-home-3d.rb
@@ -0,0 +1,11 @@
+cask "live-home-3d" do
+  version "4.1.3"
+  sha256 ""
+
+  url "https://s3.amazonaws.com/belightsoft/LiveHome3DStd.dmg"
+  name "live-home-3d"
+  desc "Home & floorplan designer & renderer"
+  homepage "https://www.livehome3d.com/mac/live-home-3d"
+
+  app "Live Home 3D.app"
+end
