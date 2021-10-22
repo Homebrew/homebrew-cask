4d9802ac2e4a3f9455a0f812d9800dd0c98106bb
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-23 00:11:23 +0800

    Update visual-studio-code.rb

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -3,13 +3,13 @@ cask "visual-studio-code" do
 
   version "1.61.2"
 
-  url "https://update.code.visualstudio.com/#{version}/#{arch}/stable"
   if Hardware::CPU.intel?
     sha256 "6481bebaead19a42a46007392ed82b75c1967e8bf34b468bbbb107e3a22a3d83"
   else
     sha256 "7e7d86c51b89c3bd5984a530ab2bae5e4c3d4960e3c1ac562cd7e52258cfd51e"
   end
 
+  url "https://update.code.visualstudio.com/#{version}/#{arch}/stable"
   name "Microsoft Visual Studio Code"
   name "VS Code"
   desc "Open-source code editor"
