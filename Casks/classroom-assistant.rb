5bc60971c2edc927b30591bde7f5988f3c6dd9ee
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-10-22 21:58:08 +0900

    Update classroom-assistant from 2.0.3 to 2.0.4 (#113053)

--- Casks/classroom-assistant.rb
+++ Casks/classroom-assistant.rb
@@ -1,6 +1,6 @@
 cask "classroom-assistant" do
-  version "2.0.3"
-  sha256 "ffb76331ef43696a8ee6acb4e9afb1e8f2819ca3d3c61fbc8cf9bfd0d018deee"
+  version "2.0.4"
+  sha256 "976da27e92ff1c2c711bda57a4d3cb5be7733f14a02d8b7f730526d3b8e9ea65"
 
   url "https://github.com/education/classroom-assistant/releases/download/v#{version}/Classroom.Assistant-darwin-x64-#{version}.zip"
   name "GitHub Classroom Assistant"
