314ecd6f928e09a47407b3dc3aa7da28dbf9789f
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-23 00:22:54 +0800

    Update yesplaymusic.rb

--- Casks/yesplaymusic.rb
+++ Casks/yesplaymusic.rb
@@ -1,16 +1,15 @@
 cask "yesplaymusic" do
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
+
   version "0.4.2"
 
   if Hardware::CPU.intel?
     sha256 "76731a76d0c32e3530d653082b8f2ae56339d6c239f8ffef44647d7f6259846a"
-
-    url "https://github.com/qier222/YesPlayMusic/releases/download/v#{version}/YesPlayMusic-mac-#{version}.dmg"
   else
     sha256 "7dea2ea672c1f79a60d6d4ca0862d02f39e2be5fd2ccc0f5cb52a97fdb515ccb"
-
-    url "https://github.com/qier222/YesPlayMusic/releases/download/v#{version}/YesPlayMusic-mac-#{version}-arm64.dmg"
   end
 
+  url "https://github.com/qier222/YesPlayMusic/releases/download/v#{version}/YesPlayMusic-mac-#{version}#{arch}.dmg"
   name "YesPlayMusic"
   desc "Third-party NetEase cloud player"
   homepage "https://github.com/qier222/YesPlayMusic"
