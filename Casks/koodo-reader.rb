3878500a8cb493f04aacb3df8921475c869c2caf
miccal <miccal.matthews@gmail.com>
2021-10-24 05:05:06 +0800

    Update koodo-reader from 1.3.2 to 1.3.3

--- Casks/koodo-reader.rb
+++ Casks/koodo-reader.rb
@@ -1,13 +1,13 @@
 cask "koodo-reader" do
-  version "1.3.2"
+  version "1.3.3"
 
   if Hardware::CPU.intel?
-    sha256 "dea649fb92b577721a36e4ed5e717c76f55e0b13af5ed0fd0176a0ef145cf9e5"
+    sha256 "e09aec2ac4b235833f5b32d81a66eef4cd40f207d721b04a5068e74d2bedb321"
 
     url "https://github.com/troyeguo/koodo-reader/releases/download/v#{version}/Koodo-Reader-#{version}.dmg",
         verified: "github.com/troyeguo/koodo-reader/"
   else
-    sha256 "d98fe82a32a7e7d32a07fa6f50c197a841a21a6f88db15f0e664e289c0df9cab"
+    sha256 "c10862678683c1df0699010b9179289654e51bc1eac6ce857bed3b30dba4c0b2"
 
     url "https://github.com/troyeguo/koodo-reader/releases/download/v#{version}/Koodo-Reader-#{version}-arm64.dmg",
         verified: "github.com/troyeguo/koodo-reader/"
