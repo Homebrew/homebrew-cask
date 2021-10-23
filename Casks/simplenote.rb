003bddffd3032ed2b097be1dbbc9ee0264bd57a3
neutric <5984479+neutric@users.noreply.github.com>
2021-10-23 02:59:14 +0200

    Update simplenote from 2.20.0 to 2.21.0 (#113086)

--- Casks/simplenote.rb
+++ Casks/simplenote.rb
@@ -1,6 +1,6 @@
 cask "simplenote" do
-  version "2.20.0"
-  sha256 "cfdc01db72a8235d5ebcd563f91fc1aa2f427385430298549f8f18e04e14e369"
+  version "2.21.0"
+  sha256 "431e83a0982414a3932f4e4b408a9ae125f84118be1f810c11a84e2d8c1aa740"
 
   url "https://github.com/Automattic/simplenote-electron/releases/download/v#{version}/Simplenote-macOS-#{version}.dmg"
   name "Simplenote"
