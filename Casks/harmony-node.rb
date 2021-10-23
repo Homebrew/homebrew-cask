a33bd35c07e9b6475365353144b2a7e91a7a5c8d
omhmichaels <omhmichaels@gmail.com>
2021-10-23 10:50:52 -0500

    harmony one node cask added

---
+++ Casks/harmony-node.rb
@@ -0,0 +1,19 @@
+cask "harmony-one" do
+    version "4.3.0"
+    sha256 "a8fd1a63f46886d51605c71fef15a7dabe5509dd35ce71ce5df2cc9ccad7860d"
+  
+    url "https://github.com/harmony-one/harmony/releases/download/v#{version}/harmony-macos-4.3.0.zip"
+    name "Harmony One Blockchain Node"
+    desc "Network node for the Harmony blockchain"
+    homepage "https://github.com/harmony-one/harmony"
+  
+    
+
+ 
+    binary "harmony"
+
+    caveats do
+        unsigned_accessibility
+      end
+  end
+  
