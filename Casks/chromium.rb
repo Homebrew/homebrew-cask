6fb4796e0c7e4dec61d9af440adfcd47aaf3bb90
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-24 05:19:06 +0800

    Update chromium.rb

--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -3,14 +3,14 @@ cask "chromium" do
 
   version "934300"
 
-  url "https://commondatastorage.googleapis.com/chromium-browser-snapshots/#{arch}/#{version}/chrome-mac.zip",
-      verified: "commondatastorage.googleapis.com/chromium-browser-snapshots/"
   if Hardware::CPU.intel?
     sha256 "024d99d7fa056251d5b0496e955939eb74751735ec3c1c6581ec3d3abaed5fda"
   else
     sha256 "6f1a76e56bea0453afa881b6d2481982e409ab59a5883f78dab7bd8a51c6b561"
   end
 
+  url "https://commondatastorage.googleapis.com/chromium-browser-snapshots/#{arch}/#{version}/chrome-mac.zip",
+      verified: "commondatastorage.googleapis.com/chromium-browser-snapshots/"
   name "Chromium"
   desc "Free and open-source web browser"
   homepage "https://www.chromium.org/Home"
