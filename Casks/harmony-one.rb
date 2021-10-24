5a4f5e2f197f13502ae56c379749e3d49abd48c7
omhmichaels <omhmichaels@gmail.com>
2021-10-24 17:59:43 -0500

    Added shim for dylib issues

--- Casks/harmony-one.rb
+++ Casks/harmony-one.rb
@@ -2,7 +2,7 @@ cask "harmony-one" do
     version "4.3.0"
     sha256 "a8fd1a63f46886d51605c71fef15a7dabe5509dd35ce71ce5df2cc9ccad7860d"
   
-    url "https://github.com/harmony-one/harmony/releases/download/v#{version}/harmony-macos-4.3.0.zip"
+    url "https://github.com/harmony-one/harmony/releases/download/v#{version}/harmony-macos-#{version}.zip"
 
     name "Harmony One Blockchain Node"
     desc "Network node for the Harmony blockchain"
