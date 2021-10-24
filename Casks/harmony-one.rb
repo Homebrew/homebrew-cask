f5b3143eeb5af05a9f5553f3ca38a30c6251f4f4
omhmichaels <omhmichaels@gmail.com>
2021-10-24 01:16:27 -0500

    Fixing Libs

--- Casks/harmony-one.rb
+++ Casks/harmony-one.rb
@@ -17,7 +17,7 @@ cask "harmony-one" do
     artifact "lib/", target: "#{HOMEBREW_PREFIX}/harmony-one/#{version}/"
 
     postflight do
-        set_ownership [ "#{HOMEBREW_PREFIX}/harmony-one/#{version}/"]
+        #set_ownership [ "#{HOMEBREW_PREFIX}/harmony-one/#{version}/"]
 
         system_command "/bin/export", ["DYLD_FALLBACK_LIBRARY_PATH=#{HOMEBREW_PREFIX}/harmony-one/#{version}/lib/:$DYLD_FALLBACK_LIBRARY_PATH"]
         system_command "/bin/export", ["PATH=#{HOMEBREW_PREFIX}/harmony-one/#{version}/:$PATH"]
