067d3d73d77cca1d6b5e9368e6d6773b520e748a
omhmichaels <omhmichaels@gmail.com>
2021-10-24 17:51:02 -0500

    Added shim for dylib issues

--- Casks/harmony-one.rb
+++ Casks/harmony-one.rb
@@ -2,7 +2,8 @@ cask "harmony-one" do
     version "4.3.0"
     sha256 "a8fd1a63f46886d51605c71fef15a7dabe5509dd35ce71ce5df2cc9ccad7860d"
   
-    url "https://github.com/harmony-one/harmony/releases/download/v#{version}/harmony-macos-4.3.0.zip"
+    url "https://github.com/harmony-one/harmony/releases/download/v#{version}/harmony-macos-#{version}.zip"
+
     name "Harmony One Blockchain Node"
     desc "Network node for the Harmony blockchain"
     homepage "https://github.com/harmony-one/harmony"
@@ -11,27 +12,31 @@ cask "harmony-one" do
 
   
 
-    
+    # Copy artifacts to the Application Directory
     artifact "./", target: "#{HOMEBREW_PREFIX}/harmony-one"
     
+    
     # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
-    shimscript = "#{HOMEBREW_PREFIX}/harmony-one/harmony-shim.sh"
+    # Attempt to correctly linking these dylibs. 
+    # Guidance appreciated if theres is a better method.
+    shimscript = "#{staged_path}/harmony-shim.sh"
+
     binary shimscript, target: "#{HOMEBREW_PREFIX}/bin/harmony-one"
 
     preflight do
-        #system_command "/bin/mkdir", args: ["-p", "#{HOMEBREW_PREFIX}/harmony-one/"], sudo: true
         File.write shimscript, <<~EOS
             #!/bin/sh
+            cd /usr/local/harmony-one/
             ./harmony
         EOS
     end
 
 
-    
+    # Delete application directory created    
     uninstall delete: ["#{HOMEBREW_PREFIX}/harmony-one"]
 
     caveats do
         unsigned_accessibility
       end
-  end
+end
   
