4c28a472f520e1cb5b1c0fdfa881caaf33a5cda8
omhmichaels <omhmichaels@gmail.com>
2021-10-23 20:13:35 -0500

    Fixing Libs

--- Casks/harmony-node.rb
+++ Casks/harmony-node.rb
@@ -1,4 +1,4 @@
-cask "harmony-node" do
+cask "harmony-one" do
     version "4.3.0"
     sha256 "a8fd1a63f46886d51605c71fef15a7dabe5509dd35ce71ce5df2cc9ccad7860d"
   
@@ -13,13 +13,13 @@ cask "harmony-node" do
 
 
 
-    binary "/usr/local/bin/harmony-node/harmony"
-    artifact "./lib/*", target: "/usr/local/lib/"
+    binary "/usr/local/bin/harmony-node/harmony-one"
+    artifact "./lib/*", target: "/usr/local/lib/harmony-one"
     
-    #postflight do
-        #set_ownership [ "#{HOMEBREW_PREFIX}/bin/harmony-node"]
-        #system_command ""
-        #end
+    postflight do
+        set_ownership [ "#{HOMEBREW_PREFIX}/bin/harmony-one", "/usr/local/lib/harmony-one"]
+        system_command "/bin/ln", args: ["-s", ""], sudo: false
+    end
     
     uninstall delete: ["#{HOMEBREW_PREFIX}/bin/harmony-node"]
 
