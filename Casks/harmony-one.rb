a4601bc51fab1aab37c15cc2ba12e704a377ef88
omhmichaels <omhmichaels@gmail.com>
2021-10-24 17:43:04 -0500

    Added shim for dylib issues

--- Casks/harmony-one.rb
+++ Casks/harmony-one.rb
@@ -2,13 +2,13 @@ cask "harmony-one" do
     version "4.3.0"
     sha256 "a8fd1a63f46886d51605c71fef15a7dabe5509dd35ce71ce5df2cc9ccad7860d"
   
-    url "https://github.com/harmony-one/harmony/releases/download/v#{version}/harmony-macos-#{version}.zip"
+    url "https://github.com/harmony-one/harmony/releases/download/v#{version}/harmony-macos-4.3.0.zip"
 
     name "Harmony One Blockchain Node"
     desc "Network node for the Harmony blockchain"
     homepage "https://github.com/harmony-one/harmony"
 
-
+    
 
   
 
@@ -17,17 +17,17 @@ cask "harmony-one" do
     
     
     # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
-    # Could not think of a more effective way to go about correctly linking these dylibs. Guidance appreciated if theres is a better method.
+    # Attempt to correctly linking these dylibs. 
+    # Guidance appreciated if theres is a better method.
     shimscript = "#{staged_path}/harmony-shim.sh"
 
     binary shimscript, target: "#{HOMEBREW_PREFIX}/bin/harmony-one"
 
     preflight do
-
         File.write shimscript, <<~EOS
             #!/bin/sh
             cd /usr/local/harmony-one/
-            ./harmony "$@"
+            ./harmony
         EOS
     end
 
@@ -35,18 +35,13 @@ cask "harmony-one" do
     # Delete application directory created    
     uninstall delete: ["#{HOMEBREW_PREFIX}/harmony-one"]
 
-=begin
-    
-    # Due to Macos Security you will have to verify the unsigned binary as well as each of the dylib libraries.     
-    # After Successfull Completion of Security Validation you will get the following error unless you have created a wallet and have its corresponding keys:
-        "ERROR when loading bls key: stat ./.hmy/blskeys: no such file or dirZZZZZZectory"
-=end
-
+    # Verify the unsigned binary as well as each of the dylib libraries.  
+    # After validation the following error will occcurs
+    #  ERROR when loading bls 
+    # key: stat ./.hmy/blskeys: no such file or directory
+    # Create a wallet and have its corresponding keys to fix
     caveats do
         unsigned_accessibility
       end
-
-
-
   end
   
