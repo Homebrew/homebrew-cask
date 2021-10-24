c576180dd3df621b608cfcce6dce4a82a4e66fc2
omhmichaels <omhmichaels@gmail.com>
2021-10-24 01:51:09 -0500

    Fixing Libs

--- Casks/harmony-one.rb
+++ Casks/harmony-one.rb
@@ -11,11 +11,12 @@ cask "harmony-one" do
 
   
 
+    binary "harmony"
 
-    artifact "./", target: "#{HOMEBREW_PREFIX}/harmony-one/#{version}/"
-
-
-
+    postflight do
+        system_command "/bin/cp", args: ["-s", "#{staged_path}/harmony-one/#{version}/lib/*.dylib", "#{HOMEBREW_PREFIX}/lib/"]
+    end
+       
     uninstall delete: ["#{HOMEBREW_PREFIX}/harmony-one"]
 
     caveats do
