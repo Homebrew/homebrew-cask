cf15783b4c3c950f4323068e76582baaa034f9fa
omhmichaels <omhmichaels@gmail.com>
2021-10-24 01:10:41 -0500

    Fixing Libs

--- Casks/harmony-one.rb
+++ Casks/harmony-one.rb
@@ -1,6 +1,4 @@
 cask "harmony-one" do
-
-
     version "4.3.0"
     sha256 "a8fd1a63f46886d51605c71fef15a7dabe5509dd35ce71ce5df2cc9ccad7860d"
   
@@ -9,25 +7,24 @@ cask "harmony-one" do
     desc "Network node for the Harmony blockchain"
     homepage "https://github.com/harmony-one/harmony"
   
-
-    binary "harmony"
-
-    dylibs = Dir.glob('lib/*.dylib')
     
-    preflight do
-        Dir.glob('lib/*.dylib') do |dylib_filename|
-            # Do work on files ending in .dylib
-            File.symlink  "dylib_filename" "#{HOMEBREW_PREFIX}/lib"
-          end
-       
-    end 
 
   
 
 
 
+    binary "harmony", target: "#{HOMEBREW_PREFIX}/harmony-one/#{version}/"
+    artifact "lib/", target: "#{HOMEBREW_PREFIX}/harmony-one/#{version}/"
+
+    postflight do
+        set_ownership [ "#{HOMEBREW_PREFIX}/harmony-one/#{version}/"]
+
+        system_command "/bin/export", ["DYLD_FALLBACK_LIBRARY_PATH=#{HOMEBREW_PREFIX}/harmony-one/#{version}/lib/:$DYLD_FALLBACK_LIBRARY_PATH"]
+        system_command "/bin/export", ["PATH=#{HOMEBREW_PREFIX}/harmony-one/#{version}/:$PATH"]]
+
+    end
     
-    uninstall delete: ["#{HOMEBREW_PREFIX}/bin/harmony", "#{HOMEBREW_PREFIX}/lib/harmony-one"]
+    uninstall delete: ["#{HOMEBREW_PREFIX}/bin/harmony-one", "#{HOMEBREW_PREFIX}/lib/harmony-one"]
 
     caveats do
         unsigned_accessibility
