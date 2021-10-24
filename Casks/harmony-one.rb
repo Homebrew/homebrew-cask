7f6d5ac0c1b1f3ebf6e26f93fc116b135f201273
omhmichaels <omhmichaels@gmail.com>
2021-10-24 18:27:09 -0500

    Added shim for dylib issues

--- Casks/harmony-one.rb
+++ Casks/harmony-one.rb
@@ -1,33 +1,27 @@
 cask "harmony-one" do
+
     version "4.3.0"
     sha256 "a8fd1a63f46886d51605c71fef15a7dabe5509dd35ce71ce5df2cc9ccad7860d"
-  
     url "https://github.com/harmony-one/harmony/releases/download/v#{version}/harmony-macos-#{version}.zip"
-
     name "Harmony One Blockchain Node"
     desc "Network node for the Harmony blockchain"
     homepage "https://github.com/harmony-one/harmony"
 
-    
 
-  
-
-    # Copy artifacts to the Application Directory
-    artifact "./", target: "#{HOMEBREW_PREFIX}/harmony-one"
-    
-    
     # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
     # Attempt to correctly linking these dylibs. 
     # Guidance appreciated if theres is a better method.
     shimscript = "#{staged_path}/harmony-shim.sh"
-
     binary shimscript, target: "#{HOMEBREW_PREFIX}/bin/harmony-one"
+    # Copy artifacts to the Application Directory
+    artifact "./", target: "#{HOMEBREW_PREFIX}/harmony-one"
+    
 
     preflight do
         File.write shimscript, <<~EOS
             #!/bin/sh
             cd /usr/local/harmony-one/
-            ./harmony
+            ./harmony "$@"
         EOS
     end
 
@@ -42,6 +36,5 @@ cask "harmony-one" do
     # Create a wallet and have its corresponding keys to fix
     caveats do
         unsigned_accessibility
-      end
+    end
 end
-  
