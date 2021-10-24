3d337f1e0461ad8b9f76177d0936b6486d5dc47a
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-25 00:22:23 +0800

    Update mailtrackerblocker.rb

--- Casks/mailtrackerblocker.rb
+++ Casks/mailtrackerblocker.rb
@@ -22,7 +22,5 @@ cask "mailtrackerblocker" do
   uninstall pkgutil: "com.onefatgiraffe.mailtrackerblocker",
             delete:  "/Library/Mail/Bundles/MailTrackerBlocker.mailbundle"
 
-  zap trash: [
-    "~/Library/Containers/com.apple.mail/Data/Library/Application Support/com.onefatgiraffe.mailtrackerblocker",
-  ]
+  zap trash: "~/Library/Containers/com.apple.mail/Data/Library/Application Support/com.onefatgiraffe.mailtrackerblocker"
 end
