229577566f29d6ca22b408570c2bcced7545088f
omhmichaels <omhmichaels@gmail.com>
2021-10-24 17:49:23 -0500

    Added shim for dylib issues

--- Casks/harmony-one.rb
+++ Casks/harmony-one.rb
@@ -3,45 +3,35 @@ cask "harmony-one" do
     sha256 "a8fd1a63f46886d51605c71fef15a7dabe5509dd35ce71ce5df2cc9ccad7860d"
   
     url "https://github.com/harmony-one/harmony/releases/download/v#{version}/harmony-macos-4.3.0.zip"
-
     name "Harmony One Blockchain Node"
     desc "Network node for the Harmony blockchain"
     homepage "https://github.com/harmony-one/harmony"
 
-
+    
 
   
 
-    # Copy artifacts to the Application Directory
+    
     artifact "./", target: "#{HOMEBREW_PREFIX}/harmony-one"
     
-    
     # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
-    # Could not think of a more effective way to go about correctly linking these dylibs. Guidance appreciated if theres is a better method.
-    shimscript = "#{staged_path}/harmony-shim.sh"
-
+    shimscript = "#{HOMEBREW_PREFIX}/harmony-one/harmony-shim.sh"
     binary shimscript, target: "#{HOMEBREW_PREFIX}/bin/harmony-one"
 
     preflight do
-
+        #system_command "/bin/mkdir", args: ["-p", "#{HOMEBREW_PREFIX}/harmony-one/"], sudo: true
         File.write shimscript, <<~EOS
             #!/bin/sh
-            cd /usr/local/harmony-one/
-            ./harmony "$@"
+            ./harmony
         EOS
     end
 
 
-    # Delete application directory created    
+    
     uninstall delete: ["#{HOMEBREW_PREFIX}/harmony-one"]
 
-    # Due to Macos Security you will have to verify the unsigned binary as well as each of the dylib libraries. 
     caveats do
         unsigned_accessibility
       end
-    # After Successfull Completion of Security Validation you will get the following error unless you have created a wallet and have its corresponding keys:
-    # "ERROR when loading bls key: stat ./.hmy/blskeys: no such file or directory"
-
-
   end
   
