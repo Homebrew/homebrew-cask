538f319262e4aa98f2694094753635311c818bab
omhmichaels <omhmichaels@gmail.com>
2021-10-24 02:08:09 -0500

    Fixing Libs

--- Casks/harmony-one.rb
+++ Casks/harmony-one.rb
@@ -14,9 +14,7 @@ cask "harmony-one" do
     binary "harmony"
 
     postflight do
-        system_command "/bin/ln", args: ["-fs", "/usr/local/Caskroom/#{version}/lib/*", "#{HOMEBREW_PREFIX}/lib/"]
-    end
-       
+        system_command "/bin/cp", args: ["#{staged_path}/harmony-one/#{version}/lib/*", "#{HOMEBREW_PREFIX}/lib/"]
     uninstall delete: ["#{HOMEBREW_PREFIX}/harmony-one"]
 
     caveats do
