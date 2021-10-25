08cf7da2bd8bb63c1112d5d3001d2ccb8c518a44
Macario Patrick <17728506+MacarioPatrick@users.noreply.github.com>
2021-10-25 19:48:43 -0400

    Update onyx.rb

--- Casks/onyx.rb
+++ Casks/onyx.rb
@@ -19,7 +19,7 @@ cask "onyx" do
     sha256 "0dd8119ad3441c5130ca584ac90ad450c272aab8b577925561a2536da48d2d54"
   else
     version "4.0.1"
-    sha256 "d8b1613df2aacca0f827d4f98b7e6ec372c2030cc09ff3291f9660271f008c4d"
+    sha256 "e6fd409dbdb08197d7ba3bb34fd28712ea6320c9eb9cd04942f12c496c5e9c07"
   end
 
   url "https://www.titanium-software.fr/download/#{MacOS.version.to_s.delete(".")}/OnyX.dmg"
