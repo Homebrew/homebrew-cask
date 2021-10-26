54a244e70f323ef8e83e118f88aa87182c39f4b8
Jakub Lanski <jakub.lanski@roche.com>
2021-10-26 22:54:04 +0200

    Update iterm2 from 3.4.11 to 3.4.12

--- Casks/iterm2.rb
+++ Casks/iterm2.rb
@@ -4,8 +4,8 @@ cask "iterm2" do
     version "3.3.12"
     sha256 "6811b520699e8331b5d80b5da1e370e0ed467e68bc56906f08ecfa986e318167"
   else
-    version "3.4.11"
-    sha256 "0d27df194a23f1c7fa13fb0e1319afa687c7c8816647af0f6754bb82a7055d6b"
+    version "3.4.12"
+    sha256 "2083a4d0a37e62a7c18fca5eafdaea246bca8cccc98094c33b38e9946342fb2a"
   end
 
   url "https://iterm2.com/downloads/stable/iTerm2-#{version.dots_to_underscores}.zip"
