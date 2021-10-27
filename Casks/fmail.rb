d0dfc65a0de2491cf30fc6cbec2f2dfeeebca71a
Alex Jarvis <github@alexjarvis.net>
2021-10-26 20:34:16 -0500

    Add FMail.app v2.3.6

---
+++ Casks/fmail.rb
@@ -0,0 +1,11 @@
+cask "fmail" do
+  version "2.3.6"
+  sha256 :no_check
+
+  url "https://arievanboxel.fr/fmail/updates/fmail.dmg"
+  name "FMail"
+  desc "Unofficial native application for Fastmail"
+  homepage "https://arievanboxel.fr/fmail/en/"
+
+  app "FMail.app"
+end
