51cae83f42e91a04594469dd71413191bdd0a1c0
Bevan Kay <email@bevankay.me>
2021-10-27 14:52:35 +1100

    Add topnotch 1.1 (#113255)
    * Add topnotch 1.1
    
    * update livecheck
    
    * fix style

---
+++ Casks/topnotch.rb
@@ -0,0 +1,24 @@
+cask "topnotch" do
+  version "1.1"
+  sha256 "47d5e0c89996759fe4e2f6973c4e2fa552ddb966c47cc80d46a72f8c9073bd0d"
+
+  url "https://updates.topnotch.app/TopNotch-#{version}.dmg"
+  name "topnotch"
+  desc "Utility to hide the notch"
+  homepage "https://topnotch.app/"
+
+  livecheck do
+    url "https://updates.topnotch.app/appcast.xml"
+    strategy :sparkle
+  end
+
+  app "TopNotch.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/pl.maketheweb.TopNotch-LaunchAtLoginHelper",
+    "~/Library/Caches/pl.maketheweb.TopNotch",
+    "~/Library/Containers/pl.maketheweb.TopNotch-LaunchAtLoginHelper",
+    "~/Library/Preferences/pl.maketheweb.TopNotch.plist",
+    "~/Library/WebKit/pl.maketheweb.TopNotch",
+  ]
+end
