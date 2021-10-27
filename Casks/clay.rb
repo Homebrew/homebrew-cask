1ccfe0e9a9a6e0284c2a7b888bbc03abfa660a80
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-28 07:00:13 +0800

    Update clay from 1.6.1 to 1.6.3 (#113286)
    * Update clay from 1.6.1 to 1.6.3
    
    * Update clay.rb

--- Casks/clay.rb
+++ Casks/clay.rb
@@ -1,14 +1,15 @@
 cask "clay" do
-  version "1.6.1"
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
+
+  version "1.6.3"
 
   if Hardware::CPU.intel?
-    sha256 "c62ad6a7ae16c3b4ac6462fa3f4c022c036423f2d027eab429bab133ad56761c"
-    url "https://assets.clay.earth/desktop/mac/Clay-#{version}.dmg"
+    sha256 "b931d44e3dd4fef1e04b4ed1791cf05f96b2759f399226d8a660184c5eab090a"
   else
-    sha256 "732bd70180ddb0b623827030aea7385fc9b9efba4a506827e92dc5f2cb004a82"
-    url "https://assets.clay.earth/desktop/mac/Clay-#{version}-arm64.dmg"
+    sha256 "04775efa3008e7f7556b97f71f2d90fbd9370cd142a523d2e65a1e6f90efbfb1"
   end
 
+  url "https://assets.clay.earth/desktop/mac/Clay-#{version}#{arch}.dmg"
   name "clay"
   desc "Private rolodex to remember people better"
   homepage "https://clay.earth/"
