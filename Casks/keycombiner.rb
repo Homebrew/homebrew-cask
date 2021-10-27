1351adb6e06c9a11d233b065df905a18d5180c8f
Benjamin Carr, PhD <ben.carr@astrazeneca.com>
2021-10-27 15:11:49 -0400

    Add KeyCombiner 0.3.4 Cask

---
+++ Casks/keycombiner.rb
@@ -0,0 +1,11 @@
+cask "keycombiner" do
+  version "0.3.4"
+  sha256 "86194712bc26b5310da6c6a5c3df69764f15ddbe43622ec05449aba106f395b9"
+
+  url "https://keycombiner.com/download/KeyCombiner-#{version}.dmg"
+  name "keycombiner"
+  desc "Instant Shortcut Lookup with KeyCombiner Desktop"
+  homepage "https://keycombiner.com/"
+
+  app "KeyCombiner.app"
+end
