d25016ba09b9fd59ab6e600453c83090ad0aeb35
Elf Sundae <elf.sundae@gmail.com>
2021-10-28 06:55:57 +0800

    Update thunder from 4.1.1.65037 to 4.1.1.65042 (#113278)
    * Update thunder from 4.1.1.65037 to 4.1.1.65042
    
    * Update thunder.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/thunder.rb
+++ Casks/thunder.rb
@@ -1,6 +1,6 @@
 cask "thunder" do
-  version "4.1.1.65037"
-  sha256 "68d92e414bf90ef3c5f3efebdf142aa5f59bcb25f7a33b5db68d40447b358467"
+  version "4.1.1.65042"
+  sha256 "099720c9a6dc6cf26ae4324750a1d286f5b01a33fc15d8ed5fe1e2e6215696d7"
 
   url "https://down.sandai.net/mac/thunder_#{version}.dmg",
       verified: "down.sandai.net/"
@@ -10,8 +10,8 @@ cask "thunder" do
   homepage "https://www.xunlei.com/"
 
   livecheck do
-    url "https://static-xl9-ssl.xunlei.com/json/mac_preferences.json"
-    regex(/appcast[._-]v?(\d+(?:\.\d+)+)\.xml/i)
+    url "https://down.sandai.net/mac/thunder/appcast.xml"
+    regex(/thunder[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
   auto_updates true
