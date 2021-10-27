4353b1d6ba5a69a93bf64d6e79ae3446e2e470d5
neutric <5984479+neutric@users.noreply.github.com>
2021-10-27 09:32:05 +0200

    monitorcontrol: skip livecheck for legacy version

--- Casks/monitorcontrol.rb
+++ Casks/monitorcontrol.rb
@@ -2,6 +2,9 @@ cask "monitorcontrol" do
   if MacOS.version <= :catalina
     version "3.1.1"
     sha256 "41fa75f378e2a1d0ec0b90fae9cfe4f6859d1628fecf32d23ad7b5447beb78e1"
+    livecheck do
+      skip "Legacy version for macOS Catalina and older"
+    end
   else
     version "4.0.0"
     sha256 "8e71a1f2bcdc2a5e95b22d8f4575032415d46b54ded998728e8fd23963fcc356"
