3a8aa09543dd9d058aca14f6fab4398f5b2eb2b2
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-28 06:48:42 +0800

    Update clay.rb

--- Casks/clay.rb
+++ Casks/clay.rb
@@ -1,14 +1,15 @@
 cask "clay" do
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
+
   version "1.6.3"
 
   if Hardware::CPU.intel?
     sha256 "b931d44e3dd4fef1e04b4ed1791cf05f96b2759f399226d8a660184c5eab090a"
-    url "https://assets.clay.earth/desktop/mac/Clay-#{version}.dmg"
   else
     sha256 "04775efa3008e7f7556b97f71f2d90fbd9370cd142a523d2e65a1e6f90efbfb1"
-    url "https://assets.clay.earth/desktop/mac/Clay-#{version}-arm64.dmg"
   end
 
+  url "https://assets.clay.earth/desktop/mac/Clay-#{version}#{arch}.dmg"
   name "clay"
   desc "Private rolodex to remember people better"
   homepage "https://clay.earth/"
