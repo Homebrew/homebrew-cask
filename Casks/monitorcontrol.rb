1e6a7dfb97181f5748d93668198d62debbbea481
neutric <5984479+neutric@users.noreply.github.com>
2021-10-27 13:44:33 +0200

    Revert "monitorcontrol: add previous version for compatibility with Catalina … (#113257)"
    This reverts commit 36601ffb60c452d307476dc6c5a2cb5febe1deb1.

--- Casks/monitorcontrol.rb
+++ Casks/monitorcontrol.rb
@@ -1,22 +1,12 @@
 cask "monitorcontrol" do
-  if MacOS.version <= :catalina
-    version "3.1.1"
-    sha256 "41fa75f378e2a1d0ec0b90fae9cfe4f6859d1628fecf32d23ad7b5447beb78e1"
-
-    livecheck do
-      skip "Legacy version for macOS Catalina and older"
-    end
-  else
-    version "4.0.0"
-    sha256 "8e71a1f2bcdc2a5e95b22d8f4575032415d46b54ded998728e8fd23963fcc356"
-  end
+  version "4.0.0"
+  sha256 "8e71a1f2bcdc2a5e95b22d8f4575032415d46b54ded998728e8fd23963fcc356"
 
   url "https://github.com/MonitorControl/MonitorControl/releases/download/v#{version}/MonitorControl.#{version}.dmg"
   name "MonitorControl"
   desc "Tool to control external monitor brightness & volume"
   homepage "https://github.com/MonitorControl/MonitorControl"
 
-  auto_updates true
   depends_on macos: ">= :sierra"
 
   app "MonitorControl.app"
