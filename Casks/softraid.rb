d8a8632b6af4c9661908186e2ddae2b9a001c1a4
David Kimura <dave@k-innovations.net>
2021-10-27 07:47:58 -0400

    Update softraid from 5.8.4 to 6.2

--- Casks/softraid.rb
+++ Casks/softraid.rb
@@ -1,14 +1,15 @@
 cask "softraid" do
-  version "5.8.4"
-  sha256 "5d5384403cd2e6b32e62cfe70bddbb82ce1ef4db88b0b197f5e08764e9a586c5"
+  version "6.2"
+  sha256 "44a9f6c203ebb78d8dd203d471f0561da344407aae2cbba4bf1ed921583ca4a9"
 
-  url "https://download.owcdigital.com/softraid/mac/#{version.major}/softraid/SoftRAID%20#{version}.dmg",
-      verified: "download.owcdigital.com/softraid/"
+  url "https://downloads.owc.com/softraid/mac/#{version.major}/SoftRAID%20#{version}.dmg",
+      verified: "downloads.owc.com/softraid/"
   name "SoftRAID"
   homepage "https://www.softraid.com/"
+  desc "Powerful and intuitive software RAID utility"
 
   livecheck do
-    url "https://download.owcdigital.com/softraid5/mac"
+    url "https://download.owcdigital.com/softraid6/mac"
     strategy :header_match
   end
 
