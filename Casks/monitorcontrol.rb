a2dba7dac8aa08823daff87c3a2d33c7a500a23e
neutric <5984479+neutric@users.noreply.github.com>
2021-10-27 09:08:19 +0200

    monitorcontrol: add previous version for compatibility with Catalina and older

--- Casks/monitorcontrol.rb
+++ Casks/monitorcontrol.rb
@@ -1,12 +1,18 @@
 cask "monitorcontrol" do
-  version "4.0.0"
-  sha256 "8e71a1f2bcdc2a5e95b22d8f4575032415d46b54ded998728e8fd23963fcc356"
+  if MacOS.version <= :catalina
+    version "3.1.1"
+    sha256 "41fa75f378e2a1d0ec0b90fae9cfe4f6859d1628fecf32d23ad7b5447beb78e1"
+  else
+    version "4.0.0"
+    sha256 "8e71a1f2bcdc2a5e95b22d8f4575032415d46b54ded998728e8fd23963fcc356"
+  end
 
   url "https://github.com/MonitorControl/MonitorControl/releases/download/v#{version}/MonitorControl.#{version}.dmg"
   name "MonitorControl"
   desc "Tool to control external monitor brightness & volume"
   homepage "https://github.com/MonitorControl/MonitorControl"
 
+  auto_updates true
   depends_on macos: ">= :sierra"
 
   app "MonitorControl.app"
