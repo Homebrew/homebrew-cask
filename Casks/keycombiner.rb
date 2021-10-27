bd9129b0bd3e5f54c24a6716076e2c2f3d902584
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-28 07:03:05 +0800

    Update keycombiner.rb

--- Casks/keycombiner.rb
+++ Casks/keycombiner.rb
@@ -4,8 +4,13 @@ cask "keycombiner" do
 
   url "https://keycombiner.com/download/KeyCombiner-#{version}.dmg"
   name "keycombiner"
-  desc "Instant Shortcut Lookup with KeyCombiner Desktop"
+  desc "Instant shortcut lookup"
   homepage "https://keycombiner.com/"
+
+  livecheck do
+    url "https://keycombiner.com/download/latest-mac.yml"
+    strategy :electron_builder
+  end
 
   app "KeyCombiner.app"
 end
