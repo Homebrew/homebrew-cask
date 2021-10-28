39438aa188c892c0a0b54b0dc43c22a0335fdbc2
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-28 12:00:25 +0800

    Update natron.rb

--- Casks/natron.rb
+++ Casks/natron.rb
@@ -1,8 +1,8 @@
 cask "natron" do
-  version "2.4.0"
-  sha256 "cdcb9ec4a193b10e25d7bdc9d5e265b3e5224a9b1822b5b3f25bcf9440dde52e"
+  version "2.4.1"
+  sha256 "c7434206bb81700bf62644d9d0e3c46f576dc002a125923afee751eddcfc8406"
 
-  url "https://github.com/NatronGitHub/Natron/releases/download/v#{version}/Natron-#{version}-OSX-64.dmg",
+  url "https://github.com/NatronGitHub/Natron/releases/download/v#{version}/Natron-#{version}-macOS11-x86_64.dmg",
       verified: "github.com/NatronGitHub/Natron/"
   name "Natron"
   desc "Open-source node-graph based video compositing software"
