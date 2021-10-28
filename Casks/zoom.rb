a62bc45b5be5e985fdaccedab40c315ef9549187
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-28 19:00:08 +0800

    Update zoom.rb

--- Casks/zoom.rb
+++ Casks/zoom.rb
@@ -1,16 +1,15 @@
 cask "zoom" do
+  arch = Hardware::CPU.intel? ? "" : "arm64/"
+
   version "5.8.3.2240"
 
   if Hardware::CPU.intel?
     sha256 "ae0f4683875ef0a4358ef274c66e9a3b39861497e25e0894b2b56d698d0cd47e"
-
-    url "https://cdn.zoom.us/prod/#{version}/Zoom.pkg"
   else
     sha256 "ac5bdd2824e47c27560f3aff9a9a24d24b31707c27663ea8a85e7bb0d2bc462a"
-
-    url "https://cdn.zoom.us/prod/#{version}/arm64/Zoom.pkg"
   end
 
+  url "https://cdn.zoom.us/prod/#{version}/#{arch}Zoom.pkg"
   name "Zoom.us"
   desc "Video communication and virtual meeting platform"
   homepage "https://www.zoom.us/"
