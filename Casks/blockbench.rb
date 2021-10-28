2110f0760354ae7a75932ddb7070d21b3cae8a68
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-28 11:53:17 +0800

    Update blockbench from 4.0.1 to 4.0.3 (#113304)
    * Update blockbench from 4.0.1 to 4.0.3
    
    * Update blockbench.rb

--- Casks/blockbench.rb
+++ Casks/blockbench.rb
@@ -1,16 +1,16 @@
 cask "blockbench" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "4.0.1"
+  version "4.0.3"
+
+  if Hardware::CPU.intel?
+    sha256 "6b33246780c78b3ecff79bdea89471afc18bf6da2e07b9d21d3ffb3401bcb699"
+  else
+    sha256 "f8b23d2e8ac76719516729ee700e994c51f1926ba0ce0e13480cec2df2eb44d3"
+  end
 
   url "https://github.com/JannisX11/blockbench/releases/download/v#{version}/Blockbench_#{arch}_#{version}.dmg",
       verified: "github.com/JannisX11/blockbench/"
-  if Hardware::CPU.intel?
-    sha256 "562e5a56281a61b8d9533d541f06e89d55fc85659a67d744673b81f0dfd65da5"
-  else
-    sha256 "2acd9a3bb54474e97cc3fc6a64bd2e6824f7a3397c1b4b31bf9fddfd27f486a4"
-  end
-
   name "Blockbench"
   desc "3D model editor for boxy models and pixel art textures"
   homepage "https://www.blockbench.net/"
