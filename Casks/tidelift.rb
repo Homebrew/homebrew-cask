6682932143166032991a1192a5527f0037332967
Tieg Zaharia <tieg@tidelift.com>
2021-10-28 10:39:39 -0400

    Update tidelift from 1.1.1 to 1.2.0

--- Casks/tidelift.rb
+++ Casks/tidelift.rb
@@ -1,6 +1,6 @@
 cask "tidelift" do
-  version "1.1.1"
-  sha256 "b7f38bbf65730579bd6707e47f04bcf1352bdc3bb113525b983634f04b0e73f5"
+  version "1.2.0"
+  sha256 "61af7166eaea2b0ecaa3f36ce779dbd97a4f3916192f90d9fd096a5ad0fb0b96"
 
   url "https://download.tidelift.com/cli/#{version}/darwin_homebrew/tidelift"
   name "Tidelift CLI"
