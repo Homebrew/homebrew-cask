aa1bfe93c92b4f9c1fc10b7ddbcfe4b8c3685267
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-28 17:34:53 +0800

    Update chromedriver.rb

--- Casks/chromedriver.rb
+++ Casks/chromedriver.rb
@@ -3,14 +3,14 @@ cask "chromedriver" do
 
   version "95.0.4638.54"
 
-  url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_#{arch}.zip",
-      verified: "chromedriver.storage.googleapis.com/"
   if Hardware::CPU.intel?
     sha256 "94a4f2ff4a853878169eeb94648a0ffd413a8c217b98da26e20728870243b1fb"
   else
     sha256 "df84d80e3111abd493634934e4f9033db6329f318abeabdd1cabb8763fd6dc29"
   end
 
+  url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_#{arch}.zip",
+      verified: "chromedriver.storage.googleapis.com/"
   name "ChromeDriver"
   desc "Automated testing of webapps for Google Chrome"
   homepage "https://sites.google.com/chromium.org/driver/"
