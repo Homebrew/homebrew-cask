54e42ffcd0e5a1bbf66d82cb37e9004cc9b70aae
Shoichi Aizawa <shoichiaizawa@gmail.com>
2021-10-29 08:14:59 +0900

    Update postman from 9.1.2 to 9.1.3 (#113387)

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,12 +1,12 @@
 cask "postman" do
   arch = Hardware::CPU.intel? ? "osx64" : "osx_arm64"
 
-  version "9.1.2"
+  version "9.1.3"
 
   if Hardware::CPU.intel?
-    sha256 "1d8badc2b848a52549eafe8ac03bb85577a44c4ae9aae818b557314f62f698ca"
+    sha256 "1c543ec9c1b30c12d85d58b93177fd72dfa6482049dab0cefeb0a822aae5d086"
   else
-    sha256 "2677a0027a714798a994b60298dec728a6b3e0357e9324b8fb93212d6f7bccb4"
+    sha256 "4b551a350b5f76a30f5ab00ceab7ba7eef280f3b5a5a466b353cf06a3659ad7e"
   end
 
   url "https://dl.pstmn.io/download/version/#{version}/#{arch}",
