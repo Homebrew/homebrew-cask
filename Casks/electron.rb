675ddb2b9d5a9037f85a5c18f1b0a75e43bd22da
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-28 11:30:41 +0800

    Update electron.rb

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -3,14 +3,14 @@ cask "electron" do
 
   version "15.3.0"
 
-  url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-#{arch}.zip",
-      verified: "github.com/electron/electron/"
   if Hardware::CPU.intel?
     sha256 "667da0d56bab968d28c41c1f53b901fb685e56d1f56faf33912a240a0397bab5"
   else
     sha256 "9cf5caafc7cbf0e91483e24d5a696b81618d293a5eccd4d122c57baaab88724c"
   end
 
+  url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-#{arch}.zip",
+      verified: "github.com/electron/electron/"
   name "Electron"
   desc "Build desktop apps with JavaScript, HTML, and CSS"
   homepage "https://electronjs.org/"
