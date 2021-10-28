a8eb6a89001ce6ca4381a8e5595b7fa1e9dfa30b
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-28 11:51:43 +0800

    Update min from 1.21.0 to 1.22.0 (#113318)
    * Update min from 1.21.0 to 1.22.0
    
    * Update min.rb

--- Casks/min.rb
+++ Casks/min.rb
@@ -1,16 +1,16 @@
 cask "min" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "1.21.0"
+  version "1.22.0"
+
+  if Hardware::CPU.intel?
+    sha256 "a29adf556203cbf78673193de54ca31f1d17c9cde039afab8f9b2210b6bc6ac9"
+  else
+    sha256 "07a8ca5773d00eebf0df6be4156de22c6a3c20310b27558410d12bdb1178bd36"
+  end
 
   url "https://github.com/minbrowser/min/releases/download/v#{version}/Min-v#{version}-darwin-#{arch}.zip",
       verified: "github.com/minbrowser/min/"
-  if Hardware::CPU.intel?
-    sha256 "4b81d90a2dc6fa5799219409af93ba2ff19e851742c9cfce73bafc3105d8ad5e"
-  else
-    sha256 "31160e1f8dd6662d58e8cb8570c7814ddc631266060ce45a2f9466a9879cd520"
-  end
-
   name "Min"
   desc "Minimal browser that protects privacy"
   homepage "https://minbrowser.github.io/min/"
