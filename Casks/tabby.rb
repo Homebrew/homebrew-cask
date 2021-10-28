8f6f741ceadc5879ae8aa4a41136147cab37c848
miccal <miccal.matthews@gmail.com>
2021-10-28 11:20:51 +0800

    Update tabby from 1.0.159 to 1.0.160

--- Casks/tabby.rb
+++ Casks/tabby.rb
@@ -1,14 +1,14 @@
 cask "tabby" do
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 
-  version "1.0.159"
+  version "1.0.160"
 
   url "https://github.com/Eugeny/tabby/releases/download/v#{version}/tabby-#{version}-macos-#{arch}.zip",
       verified: "github.com/Eugeny/tabby/"
   if Hardware::CPU.intel?
-    sha256 "1fc3e8e9297e9c8ba6481e66947bc4f17902ed4ddc090ccbb20f2dc385e3f155"
+    sha256 "6cf3bfffa6799de2f32c5cfa99af412d7e43b30b7784c0a060d455c8ad1ad4eb"
   else
-    sha256 "0f3e85e6f236216d8ac1d5e3f4dcd680cb18264da04d3235d885359e1068d4eb"
+    sha256 "fc1ce9635893a82bade79a57f392bf57b442b3ba59eb67dc712d6270d53bd593"
   end
 
   name "Tabby"
