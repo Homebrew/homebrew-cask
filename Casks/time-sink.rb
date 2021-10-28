8b3ba793322574b8286cb8acb4d7cdd314952d97
neutric <5984479+neutric@users.noreply.github.com>
2021-10-28 11:38:03 +0200

    Update time-sink from 2.1.0 to 2.2.3

--- Casks/time-sink.rb
+++ Casks/time-sink.rb
@@ -1,13 +1,16 @@
 cask "time-sink" do
-  version "2.1.0"
-  sha256 "6a2cec3828d7d3bf244825419dc82e3d5e0a6b640736c401c3b56305ddfef0ba"
+  version "2.2.3"
+  sha256 "560ebfb30b2203e3376241954c6fe118f6de47f89e27052c06128655852f5b93"
 
   url "https://manytricks.com/download/_do_not_hotlink_/timesink#{version.no_dots}.dmg"
-  appcast "https://manytricks.com/timesink/appcast",
-          must_contain: version.chomp(".0")
   name "Time Sink"
   desc "Tracks how you spend your time on your computer"
   homepage "https://manytricks.com/timesink/"
+
+  livecheck do
+    url "https://manytricks.com/timesink/releasenotes/?rref=appcast"
+    regex(/<h2>Time\s+Sink\s+v?(\d+(?:\.\d+)+)[-\s]/i)
+  end
 
   auto_updates true
 
