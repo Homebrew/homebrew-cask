e793213522c12316d9e6962c4aeccb88262cd97e
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-28 18:50:53 +0800

    Update calmly-writer.rb

--- Casks/calmly-writer.rb
+++ Casks/calmly-writer.rb
@@ -3,13 +3,13 @@ cask "calmly-writer" do
 
   version "2.0.35"
 
-  url "https://www.calmlywriter.com/releases/#{arch}/Calmly%20Writer-#{version}.pkg"
   if Hardware::CPU.intel?
     sha256 "93f0671535951fcf9c62fe922280b8a1161df2b837cb193ce5d28056f5c751c8"
   else
     sha256 "bc517bcf86072f057fc46ae8364bb3bc33dd5bed40431548f8c8cd5c5741b032"
   end
 
+  url "https://www.calmlywriter.com/releases/#{arch}/Calmly%20Writer-#{version}.pkg"
   name "Calmly Writer"
   desc "Word processor with markdown formatting and select themes"
   homepage "https://calmlywriter.com/"
