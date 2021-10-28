52985350a04f6046e7c91abe4fa0db1f8675d4fe
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-28 12:07:58 +0800

    Update hydrus-network.rb

--- Casks/hydrus-network.rb
+++ Casks/hydrus-network.rb
@@ -1,12 +1,17 @@
 cask "hydrus-network" do
-  version "458"
-  sha256 "3f8864cdfeec98ec0dce8dbb7a63e51c8e7a76cf1d737fd1fe4b08e727810350"
+  version "459.2"
+  sha256 "164603c7594a9d2ea75ecfd1b068c56c190ed77a4b420d062dee525639cdc2bc"
 
   url "https://github.com/hydrusnetwork/hydrus/releases/download/v#{version}/Hydrus.Network.#{version}.-.macOS.-.App.dmg",
       verified: "github.com/hydrusnetwork/hydrus/"
   name "hydrus-network"
   desc "Personal booru-style media tagger to organize your media"
   homepage "https://hydrusnetwork.github.io/hydrus/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "Hydrus Network.app"
 
