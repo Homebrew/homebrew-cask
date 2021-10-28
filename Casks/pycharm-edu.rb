18de1a3a3ff7ee718461e8218a269db9fc2f56c5
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-29 04:48:44 +0800

    Update pycharm-edu.rb

--- Casks/pycharm-edu.rb
+++ Casks/pycharm-edu.rb
@@ -1,16 +1,15 @@
 cask "pycharm-edu" do
+  arch = Hardware::CPU.intel? ? "" : "-aarch64"
+
   version "2021.2.3,212.5457.63"
 
   if Hardware::CPU.intel?
     sha256 "2ce7877299d7e218323cd3bb97fc44b05bc54467bfffc25b46bbcffa97caace6"
-
-    url "https://download.jetbrains.com/python/pycharm-edu-#{version.before_comma}.dmg"
   else
     sha256 "0bb8d3407ef1e0fa4502dc750997b707d47c56d1c621256cecabf80637dc8996"
-
-    url "https://download.jetbrains.com/python/pycharm-edu-#{version.before_comma}-aarch64.dmg"
   end
 
+  url "https://download.jetbrains.com/python/pycharm-edu-#{version.before_comma}#{arch}.dmg"
   name "Jetbrains PyCharm Educational Edition"
   name "PyCharm Edu"
   desc "Professional IDE for scientific and web Python development"
