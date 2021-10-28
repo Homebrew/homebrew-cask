c79866db3b446df20f0174e2a995372724a9cf2b
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-28 11:50:01 +0800

    Update wavebox.rb

--- Casks/wavebox.rb
+++ Casks/wavebox.rb
@@ -3,14 +3,14 @@ cask "wavebox" do
 
   version "10.95.29.2"
 
-  url "https://download.wavebox.app/stable/#{arch}/Install%20Wavebox%20#{version}.dmg",
-      verified: "download.wavebox.app/"
   if Hardware::CPU.intel?
     sha256 "bdca3fbf02a7f70d052e29c204c2426c61c6a4da5784960462a823a00c9381df"
   else
     sha256 "5f20b3cb4a74ea8f82ee3585991898af11024af23c79facdbb88ff05ca18fb29"
   end
 
+  url "https://download.wavebox.app/stable/#{arch}/Install%20Wavebox%20#{version}.dmg",
+      verified: "download.wavebox.app/"
   name "Wavebox"
   desc "Web browser"
   homepage "https://wavebox.io/"
