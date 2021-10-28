17318df781c4e924c65e25bb4f3fbadf265f94a6
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-28 18:44:41 +0800

    Update zulufx.rb

--- Casks/zulufx.rb
+++ Casks/zulufx.rb
@@ -4,14 +4,14 @@ cask "zulufx" do
 
   version "15.0.5,15.36.13-ca"
 
-  url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-fx-jdk#{version.before_comma}-macosx_#{arch}.dmg",
-      referer: "https://www.azul.com/downloads/"
   if Hardware::CPU.intel?
     sha256 "2d6b24911b2d8f5e363a564090edb8f6cc941a5e02a43d98b2d2edaa23f5243a"
   else
     sha256 "22658b7103474047c7f6ed736dc6b177ee8098231cd8e2d26d10eedf640f0a0f"
   end
 
+  url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-fx-jdk#{version.before_comma}-macosx_#{arch}.dmg",
+      referer: "https://www.azul.com/downloads/"
   name "ZuluFX"
   desc "Azul ZuluFX Java Standard Edition Development Kit"
   homepage "https://www.azul.com/downloads/"
