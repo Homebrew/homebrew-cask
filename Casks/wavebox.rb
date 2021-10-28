3593e7ee7b11dfb73fda5b385a8e9161620d6f18
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-28 12:00:48 +0800

    Update wavebox from 10.94.24.2 to 10.95.29.2 (#113337)
    * Update wavebox from 10.94.24.2 to 10.95.29.2
    
    * Update wavebox.rb

--- Casks/wavebox.rb
+++ Casks/wavebox.rb
@@ -1,16 +1,16 @@
 cask "wavebox" do
   arch = Hardware::CPU.intel? ? "mac" : "macarm64"
 
-  version "10.94.24.2"
+  version "10.95.29.2"
+
+  if Hardware::CPU.intel?
+    sha256 "bdca3fbf02a7f70d052e29c204c2426c61c6a4da5784960462a823a00c9381df"
+  else
+    sha256 "5f20b3cb4a74ea8f82ee3585991898af11024af23c79facdbb88ff05ca18fb29"
+  end
 
   url "https://download.wavebox.app/stable/#{arch}/Install%20Wavebox%20#{version}.dmg",
       verified: "download.wavebox.app/"
-  if Hardware::CPU.intel?
-    sha256 "ed0e751ce2474019ec045b8b7f5537be34c3289a32e591f0e3a9dbf7b95ae3d0"
-  else
-    sha256 "82c8474e80147f88ea3aeddea4acd2293ff64e9d24bcfd8d7c3202fef38eaecc"
-  end
-
   name "Wavebox"
   desc "Web browser"
   homepage "https://wavebox.io/"
