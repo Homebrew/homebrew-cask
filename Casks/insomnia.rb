74b62391eb4771db146a531c8cc20d0d4b16dca8
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-28 11:52:58 +0800

    Update insomnia.rb

--- Casks/insomnia.rb
+++ Casks/insomnia.rb
@@ -9,8 +9,7 @@ cask "insomnia" do
   homepage "https://insomnia.rest/"
 
   livecheck do
-    url :url
-    strategy :github_latest
+    url "https://github.com/Kong/insomnia/releases/"
     regex(/href=.*?Insomnia[._-]Core[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
