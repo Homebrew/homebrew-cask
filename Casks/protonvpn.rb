9a491c87b8b43d60a5d33707c1d5d0eae59c525f
Bevan Kay <email@bevankay.me>
2021-10-30 10:55:53 +1100

    Update protonvpn from 2.2.1 to 2.2.2

--- Casks/protonvpn.rb
+++ Casks/protonvpn.rb
@@ -1,6 +1,6 @@
 cask "protonvpn" do
-  version "2.2.1"
-  sha256 "7edce86180155738777970116a3579e668ad9537cab4020f1cf89c027a17c1e3"
+  version "2.2.2"
+  sha256 "763f1d682c64a272d127394791deccb93784d5c9b32dcc681ee8ec3f7a116274"
 
   url "https://protonvpn.com/download/ProtonVPN_mac_v#{version}.dmg"
   name "ProtonVPN"
