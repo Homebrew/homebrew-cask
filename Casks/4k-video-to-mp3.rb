261f6d2c65a652a97842e38804bd7c405f8c6d82
miccal <miccal.matthews@gmail.com>
2021-10-30 06:27:20 +0800

    Livecheck updates

--- Casks/4k-video-to-mp3.rb
+++ Casks/4k-video-to-mp3.rb
@@ -10,7 +10,7 @@ cask "4k-video-to-mp3" do
   livecheck do
     url "https://www.4kdownload.com/download"
     strategy :page_match
-    regex(%r{href=.*?/4kvideotomp3_(\d+(?:\.\d+)*)\.dmg}i)
+    regex(%r{href=.*?/4kvideotomp3_(\d+(?:\.\d+)+)\.dmg}i)
   end
 
   app "4K Video to MP3.app"
