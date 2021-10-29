31c3f1226166c1fa94dd5821214d005ebaf33c10
neutric <5984479+neutric@users.noreply.github.com>
2021-10-29 21:19:52 +0200

    Update valentina-studio from 11.4.6 to 11.4.7

--- Casks/valentina-studio.rb
+++ Casks/valentina-studio.rb
@@ -1,6 +1,6 @@
 cask "valentina-studio" do
-  version "11.4.6"
-  sha256 "8de11eaae16a060fa7be4af69c1f4fc71b919279b8613ed1c7b47d461a84001c"
+  version "11.4.7"
+  sha256 "525c2300ef7c2283807527d235d2e4921cb1deab3c35017618286abaa8fba13e"
 
   url "https://valentina-db.com/download/prev_releases/#{version}/mac_64/vstudio_x64_#{version.major}_mac.dmg"
   name "Valentina Studio"
