4a2681da4260124071c9135a9c490fa2cb3f3fd0
zuisong <378818573@qq.com>
2021-10-29 11:42:26 +0800

    Update ximalaya from 2.0.3,1632464709 to 2.1.0,1635299887 (#113399)
    Co-authored-by: chenjian <com.me@foxmail.com>

--- Casks/ximalaya.rb
+++ Casks/ximalaya.rb
@@ -1,6 +1,6 @@
 cask "ximalaya" do
-  version "2.0.3,1632464709"
-  sha256 "44b9cd9099ad1f07fcb25e3873d0e71c5f0e5b3d6c96fe20faeceea5e49623be"
+  version "2.1.0,1635299887"
+  sha256 "15f14921f8c0a3c94809fbeae9cdf73654ad7c40e5af7f1567e4d9e55714f146"
 
   url "https://s1.xmcdn.com/yx/ximalaya-pc-client/#{version.before_comma}/download/mac-x64/Ximalaya_#{version.before_comma}_#{version.after_comma}.dmg",
       verified: "s1.xmcdn.com/"
