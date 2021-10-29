caa3a78cab5068c7ef220dad93bffc1c28824d5c
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-30 06:02:09 +0800

    Update ghost-browser.rb

--- Casks/ghost-browser.rb
+++ Casks/ghost-browser.rb
@@ -1,14 +1,15 @@
 cask "ghost-browser" do
+  arch = Hardware::CPU.intel? ? "" : "_arm64"
+
   version "2.1.3.6"
 
   if Hardware::CPU.intel?
     sha256 "67e4402e90dae718ff83e515404841fbf73742fb94b71ca5aadc78f312b5945b"
-    url "https://downloads.ghostbrowser.com/GhostBrowser-#{version}.dmg"
   else
     sha256 "86380279f1ebe097237d4c9a18baa844358ec83e96593bba8fbf10a3374aacf6"
-    url "https://downloads.ghostbrowser.com/GhostBrowser-#{version}_arm64.dmg"
   end
 
+  url "https://downloads.ghostbrowser.com/GhostBrowser-#{version}#{arch}.dmg"
   name "Ghost Browser"
   desc "Web browser"
   homepage "https://ghostbrowser.com/download/"
