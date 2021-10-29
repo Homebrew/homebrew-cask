07baff13a01eefad315b8a279fa58d68a2a8aaab
neutric <5984479+neutric@users.noreply.github.com>
2021-10-29 13:01:20 +0200

    Update uu-booster from 2.4.0,200 to 2.6.4,228

--- Casks/uu-booster.rb
+++ Casks/uu-booster.rb
@@ -1,6 +1,6 @@
 cask "uu-booster" do
-  version "2.4.0,200"
-  sha256 "7729f6d9018ecb5a0fbe8b658f5060accb87216b6954d85913a906e6a5adece2"
+  version "2.6.4,228"
+  sha256 "836ef54e35f412288c90e56bf8ac1ef58585002d37dccf90fc1185b4722ae0d0"
 
   url "https://uu.gdl.netease.com/UU-macOS-#{version.before_comma}(#{version.after_comma}).dmg",
       verified: "uu.gdl.netease.com/"
