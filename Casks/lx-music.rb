f5d99efe5b0d79dd2214eb6d6b4751fde22d4cc2
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-29 16:10:44 +0800

    Update lx-music.rb

--- Casks/lx-music.rb
+++ Casks/lx-music.rb
@@ -1,16 +1,15 @@
 cask "lx-music" do
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
+
   version "1.15.0"
 
   if Hardware::CPU.intel?
     sha256 "b967e834d742c54c3c2692be52ed212df316ed8ee9af6677f7652eb82282f5f7"
-
-    url "https://github.com/lyswhut/lx-music-desktop/releases/download/v#{version}/lx-music-desktop-#{version}.dmg"
   else
     sha256 "d6b2eb82fc70174389b39d7c0b6c002841d8b19357c6b90139d873510bd3f87a"
-
-    url "https://github.com/lyswhut/lx-music-desktop/releases/download/v#{version}/lx-music-desktop-#{version}-arm64.dmg"
   end
 
+  url "https://github.com/lyswhut/lx-music-desktop/releases/download/v#{version}/lx-music-desktop-#{version}#{arch}.dmg"
   name "LX Music Assistant Desktop Edition"
   name "洛雪音乐助手桌面版"
   desc "Music app base on Electron & Vue"
