0d0d0f59654507ea20ca0b6cb953efb9752ed615
Samuel Inman <Samuel.Q.Inman@gmail.com>
2021-10-28 19:34:58 -0500

    Added livecheck stanza and omitted errant version from check

--- Casks/hydrus-network.rb
+++ Casks/hydrus-network.rb
@@ -1,6 +1,6 @@
 cask "hydrus-network" do
-  version "458"
-  sha256 "3f8864cdfeec98ec0dce8dbb7a63e51c8e7a76cf1d737fd1fe4b08e727810350"
+  version "459.2"
+  sha256 "164603c7594a9d2ea75ecfd1b068c56c190ed77a4b420d062dee525639cdc2bc"
 
   url "https://github.com/hydrusnetwork/hydrus/releases/download/v#{version}/Hydrus.Network.#{version}.-.macOS.-.App.dmg",
       verified: "github.com/hydrusnetwork/hydrus/"
@@ -13,4 +13,12 @@ cask "hydrus-network" do
   zap trash: [
     "~/Library/Hydrus/",
   ]
+  livecheck do
+    url "https://github.com/hydrusnetwork/hydrus.git"
+    strategy :git do |tags|
+      # Software dev released a hotfix for docker, but has no build artifacts for any clients
+      # so we're manually omitting it here.
+      tags.map { |tag| tag=="v459.3"? nil : tag }.compact
+    end
+  end
 end
