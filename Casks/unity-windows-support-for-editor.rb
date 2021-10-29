a341b7281c5c0ec6c9d06bd9c7eaf8d67793a705
neutric <5984479+neutric@users.noreply.github.com>
2021-10-29 11:15:26 +0200

    Update unity-windows-support-for-editor from 2021.1.24f1,6667702a1e7c to 2021.2.0f1,4bf1ec4b23c9

--- Casks/unity-windows-support-for-editor.rb
+++ Casks/unity-windows-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask "unity-windows-support-for-editor" do
-  version "2021.1.24f1,6667702a1e7c"
-  sha256 "a68dbbc9e1c812d0c5c5387f7d2fae0749116c9c777a5f4543a139387ad12806"
+  version "2021.2.0f1,4bf1ec4b23c9"
+  sha256 "c910076eb89454b78b42ca43e904e63aa48f92c95f6b079bbe5ec3d19d696612"
 
   url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-#{version.before_comma}.pkg",
       verified: "download.unity3d.com/download_unity/"
