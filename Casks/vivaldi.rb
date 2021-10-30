b3791c47bf3ac9c018ede3fda358deb45f5f56ea
neutric <5984479+neutric@users.noreply.github.com>
2021-10-30 10:46:24 +0200

    Update vivaldi from 4.3.2439.56 to 4.3.2439.63 (#113485)

--- Casks/vivaldi.rb
+++ Casks/vivaldi.rb
@@ -1,6 +1,6 @@
 cask "vivaldi" do
-  version "4.3.2439.56"
-  sha256 "322bd71a7616bb89cfc727ad2489de5bbfe24d9ef84fdf4430d95d6db4bd034d"
+  version "4.3.2439.63"
+  sha256 "3da32247c36fd3aa8b3ab9b34c2a7676f59cd68a55102752c2894567efd3fb56"
 
   url "https://downloads.vivaldi.com/stable/Vivaldi.#{version}.universal.dmg"
   name "Vivaldi"
