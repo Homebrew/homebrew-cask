d60bc40246228c7f7b3eecf8267a51651b967890
Felix Honer <felix.honer@novatec-gmbh.de>
2021-10-30 11:07:14 +0200

    Update microsoft-edge from 95.0.1020.38 to 95.0.1020.40 (#113487)
    * Update microsoft-edge from 95.0.1020.38 to 95.0.1020.40
    
    * Update microsoft-edge.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/microsoft-edge.rb
+++ Casks/microsoft-edge.rb
@@ -2,16 +2,16 @@ cask "microsoft-edge" do
   folder = Hardware::CPU.intel? ? "C1297A47-86C4-4C1F-97FA-950631F94777" : "03adf619-38c6-4249-95ff-4a01c0ffc962"
   linkid = Hardware::CPU.intel? ? "2069148" : "2093504"
 
-  version "95.0.1020.38"
+  version "95.0.1020.40"
+
+  if Hardware::CPU.intel?
+    sha256 "cc865bc4848cbe07a318cfce1df7f6586a0af2ca99e3c5c87b057ec1cc6845fc"
+  else
+    sha256 "9a73d1970656a7f7a3ba654a892a7085adaedefa566e32340bfdbffb1191faa2"
+  end
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/#{folder}/MacAutoupdate/MicrosoftEdge-#{version}.pkg",
       verified: "officecdn-microsoft-com.akamaized.net/"
-  if Hardware::CPU.intel?
-    sha256 "fd360a764f5a9bb877c579a0d7b4c7429a2790ae71dc9b654b3807f9c522985a"
-  else
-    sha256 "89f120344a9839fcfa213e2d562bce6edecfe6693aab00d633b3e74f3a712f6e"
-  end
-
   name "Microsoft Edge"
   desc "Web browser"
   homepage "https://www.microsoft.com/edge"
