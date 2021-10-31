ae39c00b12d1d05938a7b25e1bd972fedc4b0684
neutric <5984479+neutric@users.noreply.github.com>
2021-10-31 02:35:53 +0200

    Update visicut from 1.9-83-g2dc5fd7f to 1.9-113,g02234deb (#113516)

--- Casks/visicut.rb
+++ Casks/visicut.rb
@@ -1,16 +1,20 @@
 cask "visicut" do
-  version "1.9-83-g2dc5fd7f"
-  sha256 "a0d5cb13f9e4133b8721ce94603fb0ea6b9eeb7943fab79ab2f82cae6df4982a"
+  version "1.9-113,g02234deb"
+  sha256 "1370d05f4b251a3518915723bc20b2b06a7539f3f63204c10c2d9b15c4a3b419"
 
-  url "https://download.visicut.org/files/master/MacOSX/VisiCutMac-#{version}.zip"
+  url "https://download.visicut.org/files/master/MacOSX/VisiCutMac-#{version.before_comma}-#{version.after_comma}.zip"
   name "VisiCut"
   desc "Prepare, save and send jobs to Lasercutters"
   homepage "https://visicut.org/"
 
   livecheck do
     url "https://download.visicut.org"
-    strategy :page_match
-    regex(%r{href=.*?/VisiCutMac-(\d+(?:\.\d+)*-\d+-g\d+dc\d+fd\d+f)\.zip}i)
+    strategy :page_match do |page|
+      match = page.match(%r{href=.*?/VisiCutMac[._-]v?(\d+(?:\.\d+)+-\d+)-([a-z\d]+)\.zip}i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
   end
 
   app "VisiCut.app"
