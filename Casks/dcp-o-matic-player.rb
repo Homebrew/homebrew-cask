9317fb53f0967ef441a3a0dbc9e5229fb1fa4df6
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-31 09:46:57 +0800

    Update dcp-o-matic-player from 2.14.54 to 2.14.55 (#113525)
    * Update dcp-o-matic-player from 2.14.54 to 2.14.55
    
    * Update dcp-o-matic-player.rb

--- Casks/dcp-o-matic-player.rb
+++ Casks/dcp-o-matic-player.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-player" do
-  version "2.14.54"
-  sha256 "c979f2d2a4ba9b7b4ea3239e01387d9eb300b65f05e9fdf7db37ee90d9e880f7"
+  version "2.14.55"
+  sha256 "228084e6963f66c847d35254c55eefdf723bedbb3db689e876d1081ef68723c5"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.9-player&version=#{version}"
   name "DCP-o-matic Player"
@@ -10,7 +10,7 @@ cask "dcp-o-matic-player" do
   livecheck do
     url "https://dcpomatic.com/download"
     strategy :page_match
-    regex(/href=.*?id=osx[0-9._-]*player[^"']*?version=(\d+(?:\.\d+)*)/i)
+    regex(/href=.*?id=osx[0-9._-]*player[^"']*?version=(\d+(?:\.\d+)+)/i)
   end
 
   app "DCP-o-matic #{version.major} Player.app"
