61934359d5a65165aca8be5f6d8d5aa1fe90e2a6
neutric <5984479+neutric@users.noreply.github.com>
2021-11-01 00:31:18 +0100

    vesta: remove appcast

--- Casks/vesta.rb
+++ Casks/vesta.rb
@@ -3,7 +3,6 @@ cask "vesta" do
   sha256 "5e7110f4c09a426fd6abc0db2b6ef1fc8288e8d4ffa393a22338d93c10d792ef"
 
   url "https://jp-minerals.org/vesta/archives/#{version}/VESTA.dmg"
-  appcast "https://jp-minerals.org/vesta/en/changes.html"
   name "VESTA"
   desc "Visualization for electronic and structural analysis"
   homepage "https://jp-minerals.org/vesta/en/"
