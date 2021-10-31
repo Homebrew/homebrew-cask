dcfb8042b9f188b2d49eb6e840832def84c96413
miccal <miccal.matthews@gmail.com>
2021-10-31 09:20:52 +0800

    Update doxie from 2.13 to 2.14

--- Casks/doxie.rb
+++ Casks/doxie.rb
@@ -1,6 +1,6 @@
 cask "doxie" do
-  version "2.13"
-  sha256 "166a521160a6a6056cf81437fa4720f3400bb5e59b8fbdd5fe4f6729a96391a0"
+  version "2.14"
+  sha256 "94b01a798705e792ebc932f7a997f8ad18fd8dfe01a9aba7a661b533306e1510"
 
   url "https://download.getdoxie.com/Doxie-#{version}.dmg"
   name "Doxie"
