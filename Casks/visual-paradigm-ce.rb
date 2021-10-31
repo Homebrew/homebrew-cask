68efa857d86e68b6eceb7a39ba3b429e8fd9aba4
neutric <5984479+neutric@users.noreply.github.com>
2021-10-31 02:45:12 +0200

    Update visual-paradigm-ce from 16.3,20210827 to 16.3,20211004 (#113517)
    * Update visual-paradigm-ce from 16.3,20210827 to 16.3,20211004
    
    * Update visual-paradigm-ce.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/visual-paradigm-ce.rb
+++ Casks/visual-paradigm-ce.rb
@@ -1,6 +1,6 @@
 cask "visual-paradigm-ce" do
-  version "16.3,20210827"
-  sha256 "ff745a82be43ce5d46825aaf883d4db9233360151304728c8f0535bd73ed7c4b"
+  version "16.3,20211004"
+  sha256 "ccb5c4be07fd1b43c35c3b4f678adde926f7a0e6e1e7b3cfd6c013b42ea007a4"
 
   url "https://www.visual-paradigm.com/downloads/vpce/Visual_Paradigm_CE_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
   name "Visual Paradigm Community Edition"
@@ -10,7 +10,7 @@ cask "visual-paradigm-ce" do
   livecheck do
     url "https://www.visual-paradigm.com/downloads/vpce/checksum.html"
     strategy :header_match do |headers|
-      match = headers["location"].match(%r{/vpce(\d+(?:\.\d+)*)/(\d+)/checksum\.html}i)
+      match = headers["location"].match(%r{/vpce(\d+(?:\.\d+)+)/(\d+)/checksum\.html}i)
       next if match.blank?
 
       "#{match[1]},#{match[2]}"
