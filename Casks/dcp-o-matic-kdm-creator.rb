d765ff0e41ea5aad63d8b08b6de616e7d97870d2
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-31 09:53:47 +0800

    Update dcp-o-matic-kdm-creator from 2.14.54 to 2.14.55 (#113528)
    * Update dcp-o-matic-kdm-creator from 2.14.54 to 2.14.55
    
    * Update dcp-o-matic-kdm-creator.rb

--- Casks/dcp-o-matic-kdm-creator.rb
+++ Casks/dcp-o-matic-kdm-creator.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-kdm-creator" do
-  version "2.14.54"
-  sha256 "f1be4e7f4aa1432cd30e0569c0b76692ae3764ab8217bd14ddb21def6dc4133f"
+  version "2.14.55"
+  sha256 "2d4983d2dd755ebaffe987aed5aaece3f358af3827f43b2f98df960c881395ce"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.9-kdm&version=#{version}"
   name "DCP-o-matic KDM Creator"
@@ -10,7 +10,7 @@ cask "dcp-o-matic-kdm-creator" do
   livecheck do
     url "https://dcpomatic.com/download"
     strategy :page_match
-    regex(/stable\s*release:\s*(\d+(?:\.\d+)*)/i)
+    regex(/stable\s*release:\s*(\d+(?:\.\d+)+)/i)
   end
 
   app "DCP-o-matic #{version.major} KDM Creator.app"
