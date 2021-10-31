31b74ad79fb689bb969487517789f69701a97d19
Zhenfu Shi <szf1234@me.com>
2021-10-31 04:14:50 -0400

    Update whatsyoursign.rb

--- Casks/whatsyoursign.rb
+++ Casks/whatsyoursign.rb
@@ -2,7 +2,8 @@ cask "whatsyoursign" do
   version "2.0.1"
   sha256 "6e40562a2034b109bfc15fcb7b107d132396f420fe39abed49cc32338489ec29"
 
-  url "https://github.com/objective-see/WhatsYourSign/releases/download/v#{version}/WhatsYourSign_#{version}.zip"
+  url "https://github.com/objective-see/WhatsYourSign/releases/download/v#{version}/WhatsYourSign_#{version}.zip",
+      verified: "github.com/objective-see/"
   name "What's Your Sign?"
   desc "Shows a files cryptographic signing information"
   homepage "https://objective-see.com/products/whatsyoursign.html"
