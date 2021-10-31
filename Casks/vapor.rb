59f386f7c4614adc9942321f29074c366a1c9f29
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-01 07:43:17 +0800

    Update vapor.rb

--- Casks/vapor.rb
+++ Casks/vapor.rb
@@ -2,12 +2,12 @@ cask "vapor" do
   version "3.5.0"
   sha256 "16e321be63def7c16002bdc09f2ab403fe9ad4f85595a2f24d9069e8d6530dab"
 
-  url "https://github.com/NCAR/VAPOR/releases/download/#{version}/VAPOR3-#{version}-Darwin.dmg"
+  url "https://github.com/NCAR/VAPOR/releases/download/#{version}/VAPOR#{version.major}-#{version}-Darwin.dmg"
   name "VAPOR"
   desc "Visualization and analysis platform"
   homepage "https://github.com/NCAR/VAPOR"
 
   app "vapor.app"
 
-  # zap not needed
+  # No zap stanza required
 end
