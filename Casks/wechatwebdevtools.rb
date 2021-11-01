cffc5b97cd6af71ca71dc39c570ccb375374c3e9
neutric <5984479+neutric@users.noreply.github.com>
2021-11-01 02:15:28 +0100

    Update wechatwebdevtools from 1.05.2108150 to 1.05.2110110 (#113564)

--- Casks/wechatwebdevtools.rb
+++ Casks/wechatwebdevtools.rb
@@ -1,6 +1,6 @@
 cask "wechatwebdevtools" do
-  version "1.05.2108150"
-  sha256 "4b1b26e1e6dcf4d299940df1cf77057ef26498638ae37afab89706cf8b45bea7"
+  version "1.05.2110110"
+  sha256 "a886285be55cf03bee8666846fdb5760dda58a71a5c3cd6c47afb41d7fecdf00"
 
   url "https://dldir1.qq.com/WechatWebDev/release/p-ae42ee2cde4d42ee80ac60b35f183a99/wechat_devtools_#{version}.dmg"
   name "wechat web devtools"
