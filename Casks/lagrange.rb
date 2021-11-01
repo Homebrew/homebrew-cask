624de2dde78aafa4d8315c6682d309abfb56abf4
Jaakko Keränen <jaakko.keranen@iki.fi>
2021-11-01 10:32:28 +0200

    Update lagrange from 1.7.3 to 1.8.0 (#113578)
    * Update lagrange from 1.7.3 to 1.8.0
    
    * Update lagrange.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/lagrange.rb
+++ Casks/lagrange.rb
@@ -1,16 +1,16 @@
 cask "lagrange" do
   arch = Hardware::CPU.intel? ? "macos10.13-x86_64" : "macos11.0-arm64"
 
-  version "1.7.3"
+  version "1.8.0"
+
+  if Hardware::CPU.intel?
+    sha256 "a2baf320a0c120ad807e0933a1d4aec910954a43c9ef9cacf491dee17a5e35bf"
+  else
+    sha256 "2f9c58c069b3427979d43f5eb5835d024206844f00b1dcc9016003009452c584"
+  end
 
   url "https://github.com/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_#{arch}.tbz",
       verified: "github.com/skyjake/lagrange/"
-  if Hardware::CPU.intel?
-    sha256 "a86ee463c539dd99858d2e49e70a585bbe1b76610c6d841707e818aafc14da2b"
-  else
-    sha256 "dbaf309bb60a4db9a04dccaba05f836d3388aea8ff2877c308677b1162832855"
-  end
-
   name "Lagrange"
   desc "Desktop GUI client for browsing Geminispace"
   homepage "https://gmi.skyjake.fi/lagrange/"
