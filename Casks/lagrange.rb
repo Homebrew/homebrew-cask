b6dccf1a27814f88d38200764273b182a836a3ad
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-01 16:12:23 +0800

    Update lagrange.rb

--- Casks/lagrange.rb
+++ Casks/lagrange.rb
@@ -3,14 +3,14 @@ cask "lagrange" do
 
   version "1.8.0"
 
-  url "https://github.com/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_#{arch}.tbz",
-      verified: "github.com/skyjake/lagrange/"
   if Hardware::CPU.intel?
     sha256 "a2baf320a0c120ad807e0933a1d4aec910954a43c9ef9cacf491dee17a5e35bf"
   else
     sha256 "2f9c58c069b3427979d43f5eb5835d024206844f00b1dcc9016003009452c584"
   end
 
+  url "https://github.com/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_#{arch}.tbz",
+      verified: "github.com/skyjake/lagrange/"
   name "Lagrange"
   desc "Desktop GUI client for browsing Geminispace"
   homepage "https://gmi.skyjake.fi/lagrange/"
