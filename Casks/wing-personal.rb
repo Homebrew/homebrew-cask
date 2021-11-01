12f4808b932d78f76126432c5ab4fcd8b012f780
neutric <5984479+neutric@users.noreply.github.com>
2021-11-01 02:27:22 +0100

    Update wing-personal from 8.0.4.0 to 8.1.0.0 (#113566)
    * Update wing-personal from 8.0.4.0 to 8.1.0.0
    
    * Update wing-personal.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/wing-personal.rb
+++ Casks/wing-personal.rb
@@ -1,22 +1,22 @@
 cask "wing-personal" do
   arch = Hardware::CPU.intel? ? "intel" : "arm64"
 
-  version "8.0.4.0"
+  version "8.1.0.0"
 
-  url "https://wingware.com/pub/wing-personal/#{version}/wing-personal-#{version}-#{arch}.dmg"
   if Hardware::CPU.intel?
-    sha256 "b64920cae3308f38396e3ffd5e7345c872f44db4462cce34fe6f016179da2392"
+    sha256 "99d0a06694265e04b74394ef6b1fc3c8c7449fa1106091a2743c51ed5c0170d4"
   else
-    sha256 "e1461efb56477d4137a3a35063a94f4738241ebf2bf73caaca620ebd04f6a2dc"
+    sha256 "942a09fcba631bfba4a40340c34e9ab0bd88040e8229500860139a97f5cac934"
   end
 
+  url "https://wingware.com/pub/wing-personal/#{version}/wing-personal-#{version}-#{arch}.dmg"
   name "Wing Personal"
   desc "Free Python IDE designed for students and hobbyists"
   homepage "https://www.wingware.com/"
 
   livecheck do
     url "https://wingware.com/pub/wing-personal/"
-    regex(%r{href="(\d+(?:\.\d+)*)/"}i)
+    regex(%r{href="(\d+(?:\.\d+)+)/"}i)
   end
 
   depends_on macos: ">= :high_sierra"
