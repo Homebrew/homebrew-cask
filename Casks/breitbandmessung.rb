1335c539529a0e2cff1da1e830a06295e955d279
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-03 07:42:55 +0800

    Update breitbandmessung.rb

--- Casks/breitbandmessung.rb
+++ Casks/breitbandmessung.rb
@@ -4,10 +4,17 @@ cask "breitbandmessung" do
 
   url "https://download.breitbandmessung.de/bbm/Breitbandmessung-mac.dmg"
   name "Breitbandmessung"
-  desc "Official internet speed test from the german Bundesnetzagentur"
+  desc "Official internet speed test from the German Bundesnetzagentur"
   homepage "https://www.breitbandmessung.de/"
+
+  livecheck do
+    url "https://download.breitbandmessung.de/bbm/latest-mac.yml"
+    strategy :electron_builder
+  end
+
+  depends_on macos: ">= :sierra"
 
   app "Breitbandmessung.app"
 
-  zap trash: "~/Library/Application\ Support/Breitbandmessung"
+  zap trash: "~/Library/Application Support/Breitbandmessung"
 end
