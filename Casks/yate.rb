1a5824788125f98a39028ec180e4383ea3717151
neutric <5984479+neutric@users.noreply.github.com>
2021-11-02 23:11:31 +0100

    Update yate from 6.6.1.1,10542 to 6.6.1.3,10544 (#113655)
    * Update yate from 6.6.1.1,10542 to 6.6.1.3,10544
    
    * yate: add zap

--- Casks/yate.rb
+++ Casks/yate.rb
@@ -1,5 +1,5 @@
 cask "yate" do
-  version "6.6.1.1,10542"
+  version "6.6.1.3,10544"
   sha256 :no_check
 
   url "https://2manyrobots.com/Updates/Yate/Yate.zip"
@@ -15,4 +15,9 @@ cask "yate" do
   auto_updates true
 
   app "Yate.app"
+
+  zap trash: [
+    "~/Library/Application Support/Yate",
+    "~/Library/Preferences/com.2manyrobots.Yate.plist",
+  ]
 end
