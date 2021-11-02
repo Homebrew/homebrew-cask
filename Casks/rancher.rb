3a64ed67b3f9e7cfcae3a461c02b0adb3003b2b8
commitay <commitay@users.noreply.github.com>
2021-11-03 08:09:28 +1000

    Update rancher from 0.6.0 to 0.6.1 (#113660)
    * Update rancher from 0.6.0 to 0.6.1
    
    * Update rancher.rb

--- Casks/rancher.rb
+++ Casks/rancher.rb
@@ -1,6 +1,6 @@
 cask "rancher" do
-  version "0.6.0"
-  sha256 "6be631de519a3d8d657047b43d6f5981c7f00a44509a0c4ddede3f9fdb85344f"
+  version "0.6.1"
+  sha256 "181b46b6da8906e8bbbe0bc67e962a44b5abc13aa4b9bea54a0fbc9520795649"
 
   url "https://github.com/rancher-sandbox/rancher-desktop/releases/download/v#{version}/Rancher.Desktop-#{version}.dmg",
       verified: "github.com/rancher-sandbox/rancher-desktop/"
@@ -24,9 +24,12 @@ cask "rancher" do
 
   zap trash: [
     "~/.kuberlr",
+    "~/Library/Application Support/Caches/rancher-desktop-updater",
     "~/Library/Application Support/rancher-desktop",
+    "~/Library/Caches/io.rancherdesktop.app*",
     "~/Library/Caches/rancher-desktop",
     "~/Library/Logs/rancher-desktop",
+    "~/Library/Preferences/ByHost/io.rancherdesktop.app*",
     "~/Library/Preferences/io.rancherdesktop.app.plist",
     "~/Library/Preferences/rancher-desktop",
     "~/Library/Saved Application State/io.rancherdesktop.app.savedState",
