ca2fdf2feb6975460482dd8bd2a041b8ee073c58
Amory Meltzer <Amorymeltzer@gmail.com>
2021-11-02 11:54:13 -0400

    Update hot from 1.5.0 to 1.6.0

--- Casks/hot.rb
+++ Casks/hot.rb
@@ -1,6 +1,6 @@
 cask "hot" do
-  version "1.5.0"
-  sha256 "e17e1a2c0be97825b75cbb3ff193d125fb45b28447aefeb8407f2f5a0d6b556e"
+  version "1.6.0"
+  sha256 "55c19f765d9eb9b15de4a9200ea9c1636707555cf5488cf2bc82d3136d81ae96"
 
   url "https://github.com/macmade/Hot/releases/download/#{version}/Hot.app.zip"
   name "Hot"
