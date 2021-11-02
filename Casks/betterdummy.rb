8accc24c80b5ebc6a7655b58769e0f764aa6faa6
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-02 09:55:22 +0800

    Update betterdummy.rb

--- Casks/betterdummy.rb
+++ Casks/betterdummy.rb
@@ -1,16 +1,11 @@
 cask "betterdummy" do
-  version "1.0.7,416"
+  version "1.0.7"
   sha256 "f21bbe804f42c31eb7f6dd82ffd3ea855b9a4847b4afb5ba717fdba55f45b7c1"
 
-  url "https://github.com/waydabber/BetterDummy/releases/download/v#{version.before_comma}/BetterDummy-v#{version.before_comma}.zip"
+  url "https://github.com/waydabber/BetterDummy/releases/download/v#{version}/BetterDummy-v#{version}.zip"
   name "betterdummy"
   desc "Dummy Display for Apple Silicon Macs to achieve custom resolutions"
   homepage "https://github.com/waydabber/BetterDummy"
-
-  livecheck do
-    url "https://github.com/waydabber/BetterDummy/raw/main/docs/appcast.xml"
-    strategy :sparkle
-  end
 
   app "BetterDummy.app"
 
