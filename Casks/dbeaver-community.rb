4fe618eea268542d06a42a4ca94f9d3229604c28
Prasannajit Acharya <prasannajit.acharya.kanhu@gmail.com>
2021-11-02 23:59:32 +0530

    Update dbeaver-community from 21.2.3 to 21.2.4 (#113653)

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,13 +1,13 @@
 cask "dbeaver-community" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
-  version "21.2.3"
+  version "21.2.4"
 
   url "https://dbeaver.io/files/#{version}/dbeaver-ce-#{version}-macos-#{arch}.dmg"
   if Hardware::CPU.intel?
-    sha256 "269711ee5f8902fc3f2bff314d6b7377c38316b8474db3851857d63fbe5663c4"
+    sha256 "86635e17f6afab4000296cdc1917530452c8a972caa9befdadd0ea12ace3be20"
   else
-    sha256 "6c476e0e64c10233812d0a2976284991ec8258f2147e0165b65398dca2f52b91"
+    sha256 "fa5baf4ba48c461149ee67cee855b60d7781b9f072cc9fcb41aed79c7c76e7d4"
   end
 
   name "DBeaver Community Edition"
