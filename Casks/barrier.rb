986ee13e16542b7484b01738bb658dd931d99480
Bevan Kay <email@bevankay.me>
2021-11-03 09:45:26 +1100

    Update barrier from 2.3.3 to 2.4.0

--- Casks/barrier.rb
+++ Casks/barrier.rb
@@ -1,6 +1,6 @@
 cask "barrier" do
-  version "2.3.3"
-  sha256 "fc6a002d796453eba7a77b9df107af0f972b6e807268e880c88950bde819a40d"
+  version "2.4.0"
+  sha256 "af938d17dcea5701da7a990705acbd0686dfedfdbcd64721666ae0bef7644ba9"
 
   url "https://github.com/debauchee/barrier/releases/download/v#{version}/Barrier-#{version}-Release.dmg"
   name "Barrier"
