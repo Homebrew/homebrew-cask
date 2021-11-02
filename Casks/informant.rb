df5dcae109cda4b82c8457acd0b1c2699d7f96c1
Ty Irvine <39813066+tyirvine@users.noreply.github.com>
2021-11-01 18:46:38 -0600

    Update informant.rb

--- Casks/informant.rb
+++ Casks/informant.rb
@@ -2,8 +2,7 @@ cask "informant" do
   version "0.09.4"
   sha256 "072d26903902652e6c8eae3b36e611675d95baaa11dd7c29c7664c854776bda0"
 
-  url "https://github.com/tyirvine/Informant/releases/download/#{version}/Informant.zip",
-      verified: "https://github.com/tyirvine/Informant/"
+  url "https://github.com/tyirvine/Informant/releases/download/#{version}/Informant.zip"
   name "Informant"
   desc "Inspect files with a single click"
   homepage "https://github.com/tyirvine/Informant"
