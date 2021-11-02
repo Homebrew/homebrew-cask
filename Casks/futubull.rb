b7eb0a8de0abea19b3cd57e726d4b4372f8f8df1
BleedingSky <3053264+BleedingSky@users.noreply.github.com>
2021-11-02 09:33:50 +0800

    Update futubull from 11.10.1518,202109271021 to 11.11.1618,202110281756 (#113612)
    * Update futubull from 11.10.1518,202109271021 to 11.11.1618,202110281756
    
    * Update futubull.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/futubull.rb
+++ Casks/futubull.rb
@@ -1,6 +1,6 @@
 cask "futubull" do
-  version "11.10.1518,202109271021"
-  sha256 "9a1192d26865c85d7535ec65830a00d5a2b802fed5633bc25497aeb5212d39ff"
+  version "11.11.1618,202110281756"
+  sha256 "6fe9fce68437220b0a4b7b84f763edbfbbe1f5d84b4e005a293dc0675c58672b"
 
   url "https://softwarefile.futunn.com/FTNNForMac_#{version.before_comma}_#{version.after_comma}_Website.dmg"
   name "Futubull"
@@ -11,7 +11,7 @@ cask "futubull" do
   livecheck do
     url "https://www.futunn.com/download/history?client=11"
     strategy :page_match do |page|
-      page.scan(%r{/FTNNForMac[._-]v?(\d+(?:\.\d+)*)[_-](\d+)[._-]Website\.dmg}i).map do |match|
+      page.scan(%r{/FTNNForMac[._-]v?(\d+(?:\.\d+)+)[_-](\d+)[._-]Website\.dmg}i).map do |match|
         "#{match[0]},#{match[1]}"
       end
     end
