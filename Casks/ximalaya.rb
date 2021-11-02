889a98bf2759109b8b58001d308cbff2a03e76aa
zuisong <378818573@qq.com>
2021-11-02 12:53:21 +0800

    Update ximalaya from 2.1.0,1635299887 to 2.1.1,1635745919 (#113626)
    * Update ximalaya from 2.1.0,1635299887 to 2.1.1,1635745919
    
    * Update ximalaya.rb
    
    Co-authored-by: chenjian <com.me@foxmail.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/ximalaya.rb
+++ Casks/ximalaya.rb
@@ -1,6 +1,6 @@
 cask "ximalaya" do
-  version "2.1.0,1635299887"
-  sha256 "15f14921f8c0a3c94809fbeae9cdf73654ad7c40e5af7f1567e4d9e55714f146"
+  version "2.1.1,1635745919"
+  sha256 "1c948a9f318246c39bc3a3e4163408d86141ce0a1543d6379a63b1298827f7ae"
 
   url "https://s1.xmcdn.com/yx/ximalaya-pc-client/#{version.before_comma}/download/mac-x64/Ximalaya_#{version.before_comma}_#{version.after_comma}.dmg",
       verified: "s1.xmcdn.com/"
@@ -12,7 +12,7 @@ cask "ximalaya" do
   livecheck do
     url "https://www.ximalaya.com/down/lite?client=mac"
     strategy :header_match do |headers|
-      match = headers["location"].match(%r{/Ximalaya[_-](\d+(?:\.\d+)*)[_-](\d+)\.dmg}i)
+      match = headers["location"].match(%r{/Ximalaya[_-](\d+(?:\.\d+)+)[_-](\d+)\.dmg}i)
       next if match.blank?
 
       "#{match[1]},#{match[2]}"
