19109d49660005a7629951340850f7cf8d33e8ef
Peng Guanwen <pg999w@outlook.com>
2021-11-02 10:19:46 +0800

    Create betterdummy.rb (#113585)
    * Create betterdummy.rb
    
    * Add zap
    
    * Update betterdummy.rb
    
    * Update betterdummy.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/betterdummy.rb
@@ -0,0 +1,18 @@
+cask "betterdummy" do
+  version "1.0.7"
+  sha256 "f21bbe804f42c31eb7f6dd82ffd3ea855b9a4847b4afb5ba717fdba55f45b7c1"
+
+  url "https://github.com/waydabber/BetterDummy/releases/download/v#{version}/BetterDummy-v#{version}.zip"
+  name "betterdummy"
+  desc "Dummy Display for Apple Silicon Macs to achieve custom resolutions"
+  homepage "https://github.com/waydabber/BetterDummy"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "BetterDummy.app"
+
+  zap trash: "~/Library/Preferences/me.waydabber.BetterDummy.plist"
+end
