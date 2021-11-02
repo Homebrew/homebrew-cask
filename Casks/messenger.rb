7b676bb611f0d253bac468a3f3518d7d41850a25
Howard Rappaport <howie@rewardsmith.com>
2021-11-02 11:18:42 -0400

    Update messenger from 128.10.74,327944632 to 128.12.74,328840638

--- Casks/messenger.rb
+++ Casks/messenger.rb
@@ -1,6 +1,6 @@
 cask "messenger" do
-  version "128.10.74,327944632"
-  sha256 "6e59ee32018184c649c70205e0e83b3e576ad202f20568f4a7249f80bd6869d9"
+  version "128.12.74,328840638"
+  sha256 "74355b6f1e0229661ab612afb4b63a3655a3935e20b0db9643a25c2fdcea8606"
 
   url "https://www.facebook.com/messenger/desktop/update/#{version.after_comma}.zip",
       verified: "facebook.com/messenger/desktop/"
