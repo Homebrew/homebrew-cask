761bcdeba03bf2d3759ce6b3628dff9e57552f14
Jacob Ford <j@cobford.com>
2021-11-01 23:40:52 -0400

    Update clay from 1.6.4 to 1.6.5 (#113624)
    * Update clay from 1.6.4 to 1.6.5
    
    * Clay auto-updates itself now (silently)

--- Casks/clay.rb
+++ Casks/clay.rb
@@ -1,12 +1,12 @@
 cask "clay" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  version "1.6.4"
+  version "1.6.5"
 
   if Hardware::CPU.intel?
-    sha256 "0f0061f68598913d843bde886385759a4214ce2c0a01c638cb5866ffd6f9f8f5"
+    sha256 "be50e8e9871eed9a8c91eacae504b31e85751b27559e7099454f4fa09c03f0a1"
   else
-    sha256 "579fa1f48fc3eae304214c133373792c8f2cf71615ef68106400f65450dd98d3"
+    sha256 "6142ab460eddbd299840773868b5c3f098d90e68eaa2ec6d5a0ff0a8a424e79d"
   end
 
   url "https://assets.clay.earth/desktop/mac/Clay-#{version}#{arch}.dmg"
@@ -18,6 +18,8 @@ cask "clay" do
     url "https://assets.clay.earth/desktop/mac/latest-mac.yml"
     strategy :electron_builder
   end
+
+  auto_updates true
 
   app "Clay.app"
 
