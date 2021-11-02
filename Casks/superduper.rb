82f02a18de3f74d2575d5b392df0e531dd3d8577
Josh Tilles <MerelyAPseudonym@gmail.com>
2021-11-02 18:23:52 -0400

    Update superduper from 3.3.1,121 to 3.5,122 (#113662)
    * Update superduper from 3.3.1,121 to 3.5,122
    
    * Update superduper.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/superduper.rb
+++ Casks/superduper.rb
@@ -1,14 +1,17 @@
 cask "superduper" do
-  version "3.3.1,121"
+  version "3.5,122"
   sha256 :no_check
 
   url "https://shirtpocket.s3.amazonaws.com/SuperDuper/SuperDuper!.dmg",
       verified: "shirtpocket.s3.amazonaws.com/SuperDuper/"
-  appcast "https://versioncheck.blacey.com/superduper/version.xml?VSN=100",
-          must_contain: version.after_comma
   name "SuperDuper!"
   desc "Backup, recovery and cloning software"
   homepage "https://www.shirt-pocket.com/SuperDuper/SuperDuperDescription.html"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   app "SuperDuper!.app"
 
