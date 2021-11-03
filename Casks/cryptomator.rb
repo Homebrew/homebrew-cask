1606c84ebdf258b4adb49ec7f901e2ef88a3faf7
Daniel Neto <jdaniel.npereira@gmail.com>
2021-11-03 10:14:15 +0000

    Update cryptomator from 1.6.2 to 1.6.3

--- Casks/cryptomator.rb
+++ Casks/cryptomator.rb
@@ -1,6 +1,6 @@
 cask "cryptomator" do
-  version "1.6.2"
-  sha256 "5eb93817950df592730ad492c47ddb913d3868af4545a235e0bbebd7d528c257"
+  version "1.6.3"
+  sha256 "a19e2db2f8ba20c3b0aead2cbde827594f576af8721ee73bf070e0e9710c44e6"
 
   url "https://github.com/cryptomator/cryptomator/releases/download/#{version}/Cryptomator-#{version}.dmg",
       verified: "github.com/cryptomator/cryptomator/"
