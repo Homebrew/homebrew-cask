91573ced0207db7f31366d39271f02c2127bf460
River <46098567+itsRiver@users.noreply.github.com>
2021-11-03 21:44:46 +0800

    Update telegram from 8.1.4,222346 to 8.2,222899

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,6 +1,6 @@
 cask "telegram" do
-  version "8.1.4,222346"
-  sha256 "30260124add2a5bc0cc8d7c56d668a511c60f9ed20601eee83b950dca78643c8"
+  version "8.2,222899"
+  sha256 "461d8242f193708be5dc09fce6a457f28070e66399451a80c8c2898767a47afb"
 
   url "https://osx.telegram.org/updates/Telegram-#{version.before_comma}.#{version.after_comma}.app.zip"
   name "Telegram for macOS"
