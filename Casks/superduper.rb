141890f5d574f7415e0afa073672ef16e7c55961
neutric <5984479+neutric@users.noreply.github.com>
2021-11-03 02:48:30 +0100

    superduper: add auto_updates, add to zap (#113676)
    * Update superduper from 3.3.1,121 to 3.5,122
    
    * Update superduper.rb
    
    * superduper: add auto_updates, add to zap
    
    * Update superduper.rb
    
    * superduper: update livecheck
    
    Co-authored-by: Josh Tilles <MerelyAPseudonym@gmail.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/superduper.rb
+++ Casks/superduper.rb
@@ -9,11 +9,26 @@ cask "superduper" do
   homepage "https://www.shirt-pocket.com/SuperDuper/SuperDuperDescription.html"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url "https://s3.amazonaws.com/shirtpocket/SuperDuper/superduperinfo.rtf"
+    strategy :page_match do |page|
+      match = page.match(/SuperDuper!\s*v?(\d+(?:\.\d+)+)\s*\(v(\d+)\)/i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
   end
+
+  auto_updates true
 
   app "SuperDuper!.app"
 
-  zap trash: "~/Library/Application Support/SuperDuper!"
+  zap trash: [
+    "~/Library/Application Support/SuperDuper!",
+    "~/Library/Caches/com.blacey.SuperDuper",
+    "~/Library/Preferences/com.blacey.SuperDuper.plist",
+    "~/Library/Preferences/com.paradigmasoft.vstudio.plist",
+    "~/Library/Preferences/com.paradigmasoft.VStudio",
+    "~/Library/Saved Application State/com.blacey.SuperDuper.savedState",
+    "~/Library/Services/Run SuperDuper! Settings.workflow",
+  ]
 end
