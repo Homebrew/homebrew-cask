0c8bffb5fcf4c5b459730011b9250ad5792a3069
Jakub Warczarek <jakub.warczarek@gmail.com>
2021-11-03 11:12:49 +0100

    Install AWS session-manager-plugin from notarized pkg instead of zip

--- Casks/session-manager-plugin.rb
+++ Casks/session-manager-plugin.rb
@@ -1,9 +1,9 @@
 cask "session-manager-plugin" do
   version "1.2.279.0"
-  sha256 "15b8b932a09f05d723db27e13a928c42b048802e6fb638f8ba69a82aa8f33a9d"
+  sha256 "8f14d12aa528d27ba787b20e7ea8492e07cc80321de51843ecd44436dcd64dc1"
 
-  url "https://session-manager-downloads.s3.amazonaws.com/plugin/#{version}/mac/sessionmanager-bundle.zip",
-      verified: "session-manager-downloads.s3.amazonaws.com/"
+  url "https://s3.amazonaws.com/session-manager-downloads/plugin/#{version}/mac/session-manager-plugin.pkg",
+      verified: "s3.amazonaws.com/session-manager-downloads/"
   name "Session Manager Plugin for the AWS CLI"
   desc "Plugin for AWS CLI to start and end sessions that connect to managed instances"
   homepage "https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html"
@@ -13,5 +13,8 @@ cask "session-manager-plugin" do
     regex(%r{<td>\s*v?(\d+(?:\.\d+)+)\s*</td>}i)
   end
 
-  binary "sessionmanager-bundle/bin/session-manager-plugin"
+  pkg "session-manager-plugin.pkg"
+  binary "/usr/local/sessionmanagerplugin/bin/session-manager-plugin"
+
+  uninstall pkgutil: "session-manager-plugin"
 end
