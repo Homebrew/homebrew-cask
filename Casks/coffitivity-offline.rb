c38eb40372c64bd3afdbdfb9633bcfa73a0960f9
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-04 05:32:05 +0800

    Update coffitivity-offline.rb

--- Casks/coffitivity-offline.rb
+++ Casks/coffitivity-offline.rb
@@ -3,7 +3,7 @@ cask "coffitivity-offline" do
   sha256 "f4d0f9cf0bc7da08ec6bada4ce67bd501c51ebe5c04be193895d91c51318790f"
 
   url "https://github.com/siwalikm/coffitivity-offline/releases/download/v#{version}/Coffitivity.Offline-#{version}.dmg",
-      verified: "github.com/siwalikm/coffitivity-offline"
+      verified: "github.com/siwalikm/coffitivity-offline/"
   name "Coffitivity Offline"
   desc "Ambient sound generator"
   homepage "https://coffitivity-offline.siwalik.in/"
