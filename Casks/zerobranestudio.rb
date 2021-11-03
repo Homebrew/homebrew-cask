0f41b63eb0fcb51dfc8575822a11107e01023060
neutric <5984479+neutric@users.noreply.github.com>
2021-11-03 14:40:21 +0100

    zerobranestudio: add desc+livecheck

--- Casks/zerobranestudio.rb
+++ Casks/zerobranestudio.rb
@@ -3,9 +3,14 @@ cask "zerobranestudio" do
   sha256 "61fb075da4d93edeef16e73e1d66c6655c527a11c8f52f29af03906565459c90"
 
   url "https://download.zerobrane.com/ZeroBraneStudioEduPack-#{version}-macos.dmg"
-  appcast "https://raw.githubusercontent.com/pkulchenko/ZeroBraneStudio/master/CHANGELOG.md"
   name "ZeroBrane Studio"
+  desc "Lua IDE"
   homepage "https://studio.zerobrane.com/"
+
+  livecheck do
+    url "https://studio.zerobrane.com/download"
+    regex(/href=.*?ZeroBraneStudioEduPack[._-]?v?(\d+(?:\.\d+)+)[._-]macos\.dmg/i)
+  end
 
   app "ZeroBraneStudio.app"
 end
