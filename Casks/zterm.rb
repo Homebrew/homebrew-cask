4621e23a4efa33aeecc413d085b3bcb2aa35cf69
neutric <5984479+neutric@users.noreply.github.com>
2021-11-03 16:15:13 +0100

    zterm: add livecheck and depends_on (#113695)
    * zterm: add livecheck
    
    * zterm: add depends_on

--- Casks/zterm.rb
+++ Casks/zterm.rb
@@ -3,10 +3,16 @@ cask "zterm" do
   sha256 "711b1e4322970d2ece65ab5d51e18f2e634c2f43847db9a7a98e4326e1b8217c"
 
   url "https://www.dalverson.com/zterm/ZTerm#{version}.dmg"
-  appcast "https://www.dalverson.com/zterm/"
   name "ZTerm"
   desc "Terminal emulation program"
   homepage "https://www.dalverson.com/zterm/"
+
+  livecheck do
+    url :homepage
+    regex(/href.*?ZTerm[._-]?v?(\d+(?:\.\d+)+)\.dmg/i)
+  end
+
+  depends_on macos: "<= :mojave"
 
   app "ZTerm.app"
 end
