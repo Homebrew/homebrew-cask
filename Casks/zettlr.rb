1e8590237ca554f16ceaf7ad4da41685e367314c
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-04 05:29:26 +0800

    Update zettlr.rb

--- Casks/zettlr.rb
+++ Casks/zettlr.rb
@@ -3,13 +3,13 @@ cask "zettlr" do
 
   version "2.0.2"
 
-  url "https://github.com/Zettlr/Zettlr/releases/download/v#{version}/Zettlr-#{version}-#{arch}.dmg"
   if Hardware::CPU.intel?
     sha256 "3d111e4328e06f4e23f6ef418ab91c4081f316861c1fef3117d70a16fb92b7b7"
   else
     sha256 "e5b40a584207323eb92b98c303f53767b4db9c908a4d0e50aaab9eff909c918d"
   end
 
+  url "https://github.com/Zettlr/Zettlr/releases/download/v#{version}/Zettlr-#{version}-#{arch}.dmg"
   name "Zettlr"
   desc "Open-source markdown editor"
   homepage "https://github.com/Zettlr/Zettlr/"
