2e1b4ebd92a5fcdc91dc110063e8698b9c16a9af
Sam Doran <sdoran@redhat.com>
2021-11-04 19:13:40 -0400

    Update keyboard-maestro from 9.2 to 10.0 (#113705)
    * Update keyboard-maestro from 9.2 to 10.0
    
    * Use correct hash
    
    * Add trailing "0" when the version is 10 or greater
    
    * Account for .Z in the actual version
    
    The .0 only needs to be added for versions >= 10
    that are not already X.Y.Z.
    
    * Correct syntax
    
    * Update keyboard-maestro.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/keyboard-maestro.rb
+++ Casks/keyboard-maestro.rb
@@ -1,8 +1,8 @@
 cask "keyboard-maestro" do
-  version "9.2"
-  sha256 "a3a25d0663074e726e4d89692196f2e4a5b7ae013b5f3ed9eb1aae784d457c5d"
+  version "10.0,1000"
+  sha256 "04946ffc9606f2ea22e964ab3d05123d144ffa5f00e32a3081fe76d7ec4802ee"
 
-  url "https://files.stairways.com/keyboardmaestro-#{version.no_dots}.zip",
+  url "https://files.stairways.com/keyboardmaestro-#{version.after_comma}.zip",
       verified: "stairways.com/"
   name "Keyboard Maestro"
   desc "Automation software"
@@ -10,8 +10,12 @@ cask "keyboard-maestro" do
 
   livecheck do
     url "https://files.stairways.com/index.html"
-    strategy :page_match
-    regex(/href=.*?\.zip.*?Keyboard\s*Maestro\s*(\d+(\.\d+)*)/i)
+    strategy :page_match do |page|
+      match = page.match(/href=.*?keyboardmaestro[._-]v?(\d+)\.zip.*?Maestro\s*(\d+(?:\.\d+)+)/i)
+      next if match.blank?
+
+      "#{match[2]},#{match[1]}"
+    end
   end
 
   auto_updates true
