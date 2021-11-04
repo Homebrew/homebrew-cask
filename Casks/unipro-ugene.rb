02f34e0c85c40445a0232c480069a47045dd2a38
romainstuder <romainstuder@users.noreply.github.com>
2021-11-04 13:31:43 +0000

    Add ugene (#113749)
    * Add ugene
    
    * Update and rename ugene.rb to unipro-ugene.rb
    
    * Rename ugene->unipro-gene
    
    * Add zap
    
    * Remove extra empty line
    
    * Update unipro-ugene.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/unipro-ugene.rb
@@ -0,0 +1,17 @@
+cask "unipro-ugene" do
+  version "40.1"
+  sha256 "e603b3d15ed4d9ce2f42852d6e8aa201b39c91d384cf8d7cd6727f73a436bfc6"
+
+  url "https://github.com/ugeneunipro/ugene/releases/download/#{version}/ugene-#{version}-mac-x86-64.dmg",
+      verified: "github.com/ugeneunipro/ugene/"
+  name "Ugene"
+  desc "Free open-source cross-platform bioinformatics software"
+  homepage "https://ugene.net/"
+
+  app "Unipro UGENE.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.unipro.UGENE.plist",
+    "~/Library/Preferences/net.ugene.ugene.plist",
+  ]
+end
