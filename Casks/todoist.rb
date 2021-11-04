8580b809cae6cdd6e96d7a652c0e8c13c12d59c7
Nihaal Sangha <nihaal.git@gmail.com>
2021-11-04 03:33:53 +0000

    Add `zap` to Todoist

--- Casks/todoist.rb
+++ Casks/todoist.rb
@@ -13,4 +13,27 @@ cask "todoist" do
   end
 
   app "Todoist.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/S3DD273774.com.todoist.mac-beta.Todoist.extensions",
+    "~/Library/Application Scripts/S3DD273774.com.todoist.mac-dev.Todoist.extensions",
+    "~/Library/Application Scripts/S3DD273774.com.todoist.mac.Todoist",
+    "~/Library/Application Scripts/S3DD273774.com.todoist.mac.Todoist.extensions",
+    "~/Library/Application Scripts/com.todoist.mac.Todoist",
+    "~/Library/Application Scripts/com.todoist.mac.Todoist.TodoistSafari",
+    "~/Library/Application Scripts/com.todoist.mac.Todoist.TodoistShare",
+    "~/Library/Application Scripts/com.todoist.mac.Todoist.TodoistToday",
+    "~/Library/Application Support/Todoist",
+    "~/Library/Containers/com.todoist.mac.Todoist",
+    "~/Library/Containers/com.todoist.mac.Todoist.TodoistSafari",
+    "~/Library/Containers/com.todoist.mac.Todoist.TodoistShare",
+    "~/Library/Containers/com.todoist.mac.Todoist.TodoistToday",
+    "~/Library/Group Containers/S3DD273774.com.todoist.mac-beta.Todoist.extensions",
+    "~/Library/Group Containers/S3DD273774.com.todoist.mac-dev.Todoist.extensions",
+    "~/Library/Group Containers/S3DD273774.com.todoist.mac.Todoist",
+    "~/Library/Group Containers/S3DD273774.com.todoist.mac.Todoist.extensions",
+    "~/Library/Logs/Todoist",
+    "~/Library/Preferences/com.todoist.mac.Todoist.plist",
+    "~/Library/Saved Application State/com.todoist.mac.Todoist.savedState",
+  ]
 end
