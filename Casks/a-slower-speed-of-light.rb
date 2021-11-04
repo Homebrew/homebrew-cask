263a2d30dc1f823a5ab9328b4afa2985b6d10f4c
neutric <5984479+neutric@users.noreply.github.com>
2021-11-04 21:08:59 +0100

    Update a-slower-speed-of-light from summer12 to 2020

--- Casks/a-slower-speed-of-light.rb
+++ Casks/a-slower-speed-of-light.rb
@@ -1,10 +1,16 @@
 cask "a-slower-speed-of-light" do
-  version "summer12"
-  sha256 "4163053a6caa6d258475aa29209fb863fe63139c8d3048661f8a9bc11ffd187c"
+  version "2020"
+  sha256 "207513b33b7198c0f3106de510a13960434cb1c177fccc062d4e53cfbf981543"
 
-  url "https://web.mit.edu/gambit/#{version}/speedoflight/beta/A_Slower_Speed_of_Light.dmg"
+  url "https://web.mit.edu/gambit/summer12/speedoflight/#{version}/A_Slower_Speed_of_Light.dmg"
   name "A Slower Speed of Light"
+  desc "First-person game"
   homepage "http://gamelab.mit.edu/games/a-slower-speed-of-light/"
+
+  livecheck do
+    url :homepage
+    regex(/title=.*?Download\s*v?(\d+(?:\.\d+)*)\s*For\s*Mac"/i)
+  end
 
   app "A Slower Speed of Light.app"
 end
