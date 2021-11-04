60fd4b0fb1beca857e7e1ef3c9da8fe166c77700
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-04 09:30:46 +0800

    Update shotcut.rb

--- Casks/shotcut.rb
+++ Casks/shotcut.rb
@@ -3,14 +3,14 @@ cask "shotcut" do
 
   version "21.10.31"
 
-  url "https://github.com/mltframework/shotcut/releases/download/v#{version}/shotcut-#{arch}-#{version.no_dots}.dmg",
-      verified: "github.com/mltframework/shotcut/"
   if Hardware::CPU.intel?
     sha256 "37c7afd53931711b8bbb37a0490e4052b972b97455140a558f8f3896189d6a2f"
   else
     sha256 "1dca894a0733afc43445de194a69e3bf41f72acfde90bef6bc96e6f050b49611"
   end
 
+  url "https://github.com/mltframework/shotcut/releases/download/v#{version}/shotcut-#{arch}-#{version.no_dots}.dmg",
+      verified: "github.com/mltframework/shotcut/"
   name "Shotcut"
   desc "Video editor"
   homepage "https://www.shotcut.org/"
