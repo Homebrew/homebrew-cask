6b427c51f8455786d724c7d6ec212275b5202005
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-04 08:05:05 +0800

    Update and rename ugene.rb to unipro-ugene.rb

--- Casks/unipro-ugene.rb
+++ Casks/unipro-ugene.rb
@@ -1,10 +1,10 @@
-cask "ugene" do
+cask "unipro-ugene" do
   version "40.1"
   sha256 "e603b3d15ed4d9ce2f42852d6e8aa201b39c91d384cf8d7cd6727f73a436bfc6"
 
   url "https://github.com/ugeneunipro/ugene/releases/download/#{version}/ugene-#{version}-mac-x86-64.dmg",
-      verified: "github.com/ugeneunipro/ugene"
-  name "ugene"
+      verified: "github.com/ugeneunipro/ugene/"
+  name "Ugene"
   desc "Free open-source cross-platform bioinformatics software"
   homepage "https://ugene.net/"
 
