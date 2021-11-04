28c05585b851d1d6397b472aa2f24b6f3129e146
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-04 10:53:00 +0800

    Update neo4j.rb

--- Casks/neo4j.rb
+++ Casks/neo4j.rb
@@ -3,7 +3,8 @@ cask "neo4j" do
   version "1.4.9"
   sha256 "e7ff9c37a37984d534d34627e3431928c98724a59138efb211ca91f0e182d527"
 
-  url "https://dist.neo4j.org/neo4j-desktop/mac/Neo4j%20Desktop-#{version}.dmg"
+  url "https://dist.neo4j.org/neo4j-desktop/mac/Neo4j%20Desktop-#{version}.dmg",
+      verified: "dist.neo4j.org/neo4j-desktop/mac/"
   name "Neo4j Desktop"
   desc "Developer IDE or Management Environment for Neo4j instances"
   homepage "https://neo4j.com/download/"
@@ -11,7 +12,7 @@ cask "neo4j" do
   livecheck do
     url "https://neo4j.com/download-center/#desktop"
     strategy :page_match
-    regex(%r{href=.*?/neo4j-desktop/.*?flavour=osx.*?release=(\d+(?:\.\d+)*)}i)
+    regex(%r{href=.*?/neo4j-desktop/.*?flavour=osx.*?release=(\d+(?:\.\d+)+)}i)
   end
 
   depends_on macos: ">= :yosemite"
