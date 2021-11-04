dc0f9796bce2733aafa0b6be1b405c097528ef1f
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-05 07:03:25 +0800

    Update keyboard-maestro.rb

--- Casks/keyboard-maestro.rb
+++ Casks/keyboard-maestro.rb
@@ -1,13 +1,8 @@
 cask "keyboard-maestro" do
-  version "10.0"
+  version "10.0,1000"
   sha256 "04946ffc9606f2ea22e964ab3d05123d144ffa5f00e32a3081fe76d7ec4802ee"
 
-  # Versions >= 10 need to be X.Y.Z to avoid conflicting with 1.0.Z versions.
-  # The version in the package may be X.Y or X.Y.Z.
-  url_version = version.clone
-  url_version.concat(".0") if version.major.to_i >= 10 && version.split(".").length < 3
-
-  url "https://files.stairways.com/keyboardmaestro-#{url_version.no_dots}.zip",
+  url "https://files.stairways.com/keyboardmaestro-#{version.after_comma}.zip",
       verified: "stairways.com/"
   name "Keyboard Maestro"
   desc "Automation software"
@@ -15,8 +10,12 @@ cask "keyboard-maestro" do
 
   livecheck do
     url "https://files.stairways.com/index.html"
-    strategy :page_match
-    regex(/href=.*?\.zip.*?Keyboard\s*Maestro\s*(\d+(\.\d+)*)/i)
+    strategy :page_match do |page|
+      match = page.match(/href=.*?keyboardmaestro[._-]v?(\d+)\.zip.*?Maestro\s*(\d+(?:\.\d+)+)/i)
+      next if match.blank?
+
+      "#{match[2]},#{match[1]}"
+    end
   end
 
   auto_updates true
