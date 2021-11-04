5e52b997d5becad95f6379e2d98c13c91bf66b84
neutric <5984479+neutric@users.noreply.github.com>
2021-11-05 00:36:04 +0100

    td-agent: add livecheck

--- Casks/td-agent.rb
+++ Casks/td-agent.rb
@@ -4,9 +4,13 @@ cask "td-agent" do
 
   url "https://s3.amazonaws.com/packages.treasuredata.com/#{version.major}/macosx/td-agent-#{version}.dmg",
       verified: "s3.amazonaws.com/packages.treasuredata.com/"
-  appcast "https://td-agent-package-browser.herokuapp.com/#{version.major}/macosx"
   name "td-agent"
   homepage "https://www.fluentd.org/"
+
+  livecheck do
+    url "https://td-agent-package-browser.herokuapp.com/#{version.major}/macosx"
+    regex(/href=.*?td-agent[._-]?v?(\d+(?:\.\d+)+)\.dmg/i)
+  end
 
   pkg "td-agent-#{version}.pkg"
 
