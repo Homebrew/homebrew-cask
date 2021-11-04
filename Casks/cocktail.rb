d810eeffaf90336cb544a92a057080ca17539cf7
owine <owine@users.noreply.github.com>
2021-11-04 16:12:48 -0500

    Cocktail: Fix livecheck on Big Sur

--- Casks/cocktail.rb
+++ Casks/cocktail.rb
@@ -64,6 +64,11 @@ cask "cocktail" do
     sha256 "1f684f7a06ed9d5c44513434f87070dafa000bee2311c580ceff5727ae5a3d88"
 
     url "https://www.maintain.se/downloads/Cocktail#{version.major}BSE.dmg"
+    
+    livecheck do
+      url :homepage
+      strategy :page_match
+      regex(/macOS\s*11(?:\.\d+)*.*?(\d+(?:\.\d+)+)/i)
   else
     version "15.0"
     sha256 "8398c07b9f009d3d2a6554b646107c1e8c40938a2b98e5c5af9493d07fadf8e4"
