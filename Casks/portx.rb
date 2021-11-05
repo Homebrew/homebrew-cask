b7fccd846466f052493c95d78a20db0a0c4bf0ae
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-05 13:10:53 +0800

    Update portx.rb

--- Casks/portx.rb
+++ Casks/portx.rb
@@ -1,8 +1,8 @@
 cask "portx" do
   version "2.0.3,10.11"
-  sha256 "88d0e9e59f28820cbc825e0ab170b4b84c9fdcfd5f03b8d2b2c5c34daaa173dd"
+  sha256 :no_check
 
-  url "https://cdn.netsarang.net/0ac7ea20/PortX-#{version.before_comma}.dmg",
+  url "https://cdn.netsarang.net/0ac7ea20/PortX-latest.dmg",
       verified: "cdn.netsarang.net/"
   name "portx"
   desc "SSH Client"
