014a9637a1739fc7f1d572589b91a5b08effe98b
zuisong <378818573@qq.com>
2021-11-06 00:24:11 +0800

    Update betterdummy from 1.0.7 to 1.0.8 (#113854)
    Co-authored-by: chenjian <com.me@foxmail.com>

--- Casks/betterdummy.rb
+++ Casks/betterdummy.rb
@@ -1,6 +1,6 @@
 cask "betterdummy" do
-  version "1.0.7"
-  sha256 "f21bbe804f42c31eb7f6dd82ffd3ea855b9a4847b4afb5ba717fdba55f45b7c1"
+  version "1.0.8"
+  sha256 "c1df44d6f84662278a36047720405748d5982b54f70776180820ed99f74bf879"
 
   url "https://github.com/waydabber/BetterDummy/releases/download/v#{version}/BetterDummy-v#{version}.zip"
   name "betterdummy"
