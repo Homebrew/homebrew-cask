f06d92ec634b315a0dc55c9dc35f931b307bb752
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-05 17:05:54 +0800

    Update ray.rb

--- Casks/ray.rb
+++ Casks/ray.rb
@@ -1,5 +1,6 @@
 cask "ray" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
+  folder = Hardware::CPU.intel? ? "" : "arm64/"
 
   version "1.18.1"
 
@@ -9,7 +10,7 @@ cask "ray" do
     sha256 "6dff7448441e54d6c73a20943cec1d76609e1b706a96cc8da9c71c5e0c8f98de"
   end
 
-  url "https://ray-app.s3.eu-west-1.amazonaws.com/arm64/Ray-#{version}#{arch}.dmg",
+  url "https://ray-app.s3.eu-west-1.amazonaws.com/#{folder}Ray-#{version}#{arch}.dmg",
       verified: "ray-app.s3.eu-west-1.amazonaws.com/"
   name "Ray"
   desc "Debug with Ray to fix problems faster"
