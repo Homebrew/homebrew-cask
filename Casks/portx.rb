6da090118abf418fef8fc5a9b3825c91459e27fd
NetSarangComputer <bigseungho@netsarang.com>
2021-11-05 13:29:48 +0900

    Updated PortX.app Version 2.0.3

--- Casks/portx.rb
+++ Casks/portx.rb
@@ -1,6 +1,6 @@
 cask "portx" do
-  version "2.0.1,10.8"
-  sha256 "a980f1b4814cf03c9c58b6dd4820f4fcee3503cd9f55fc24ccb3cea4c461d362"
+  version "2.0.3,10.11"
+  sha256 "88d0e9e59f28820cbc825e0ab170b4b84c9fdcfd5f03b8d2b2c5c34daaa173dd"
 
   url "https://cdn.netsarang.net/0ac7ea20/PortX-#{version.before_comma}.dmg",
       verified: "cdn.netsarang.net/"
