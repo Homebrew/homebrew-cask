96a0eae4341e567a1db5bd2e61443bb07fc7aaae
NetSarang Computer <83996870+NetSarangComputer@users.noreply.github.com>
2021-11-05 14:21:27 +0900

    Updated PortX.app Version 2.0.3 (#113842)
    * Updated PortX.app Version 2.0.3
    
    * Update portx.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/portx.rb
+++ Casks/portx.rb
@@ -1,8 +1,8 @@
 cask "portx" do
-  version "2.0.1,10.8"
-  sha256 "a980f1b4814cf03c9c58b6dd4820f4fcee3503cd9f55fc24ccb3cea4c461d362"
+  version "2.0.3,10.11"
+  sha256 :no_check
 
-  url "https://cdn.netsarang.net/0ac7ea20/PortX-#{version.before_comma}.dmg",
+  url "https://cdn.netsarang.net/0ac7ea20/PortX-latest.dmg",
       verified: "cdn.netsarang.net/"
   name "portx"
   desc "SSH Client"
