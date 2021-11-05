f2a640f12cd5f37ee003cfd83d5d05556359dabc
César Román <thecesrom@gmail.com>
2021-11-04 22:13:06 -0700

    Update azure-data-studio from 1.33.0 to 1.33.1 (#113839)
    * Update azure-data-studio from 1.33.0 to 1.33.1
    
    * Update azure-data-studio.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/azure-data-studio.rb
+++ Casks/azure-data-studio.rb
@@ -1,6 +1,6 @@
 cask "azure-data-studio" do
-  version "1.33.0"
-  sha256 "6e89cd7cca288c02d6fed21f82fe2fe1f114c4e3ed8283842686adcc528e44fa"
+  version "1.33.1"
+  sha256 "73ff269b941929e650be6e8e73203185a0949c86cd072fa42436a141ffeed2ed"
 
   url "https://azuredatastudio-update.azurewebsites.net/#{version}/darwin/stable",
       verified: "azuredatastudio-update.azurewebsites.net/"
@@ -11,7 +11,7 @@ cask "azure-data-studio" do
   livecheck do
     url "https://azuredatastudio-update.azurewebsites.net/api/update/darwin/stable/VERSION"
     strategy :page_match
-    regex(/"productVersion"\s*:\s*"(\d+(:?\.\d+)*)"/)
+    regex(/"productVersion"\s*:\s*"(\d+(:?\.\d+)+)"/)
   end
 
   auto_updates true
