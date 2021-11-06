17d8f606ab4c5f7f183e047cf569e6c180e44999
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-06 23:50:24 +0800

    Update prezi-classic.rb

--- Casks/prezi-classic.rb
+++ Casks/prezi-classic.rb
@@ -10,7 +10,7 @@ cask "prezi-classic" do
   livecheck do
     url "https://prezidesktop.s3.amazonaws.com/assets/mac/pd6/updates/prezi-classic.xml"
     strategy :page_match do |page|
-      match = page.match(/<enclosure.*?sparkle:version="(\d+).*?sparkle:shortVersionString="(\d+(?:\.\d+)*)"/im)
+      match = page.match(/<enclosure.*?sparkle:version="(\d+).*?sparkle:shortVersionString="(\d+(?:\.\d+)+)"/im)
       next if match.blank?
 
       "#{match[2]},#{match[1]}"
