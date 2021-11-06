86b3cbbe4a948748fe1a81f3baa5a8ed65dfd9cc
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-07 06:55:41 +0800

    Update actual.rb

--- Casks/actual.rb
+++ Casks/actual.rb
@@ -1,10 +1,18 @@
 cask "actual" do
-  version :latest
-  sha256 :no_check
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  url "https://download.actualbudget.com/download/dmg"
+  version "0.0.143"
+
+  if Hardware::CPU.intel?
+    sha256 "ab5baf1c99c0be468f2a7a4cab85108559f1d2ddf3073bb4966fb79a884d11b5"
+  else
+    sha256 "1cdd81c8df398eb899ff63904ffce942c3562b4fc6a28f71ccfe1c536945ea96"
+  end
+
+  url "https://github.com/actualbudget/releases/releases/download/#{version}/Actual-#{version}#{arch}.dmg",
+      verified: "github.com/actualbudget/releases/"
   name "Actual"
-  desc "Super fast privacy-focused app for managing your finances"
+  desc "Privacy-focused app for managing your finances"
   homepage "https://actualbudget.com/"
 
   app "Actual.app"
