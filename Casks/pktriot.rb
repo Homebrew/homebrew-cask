dd9aa57cf3d38edf51ec8ee720c6b336630d2247
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-11-06 18:57:41 +0900

    Update pktriot from 0.11.2 to 0.12.0 (#113889)
    * Update pktriot from 0.11.2 to 0.12.0
    
    * Update pktriot.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/pktriot.rb
+++ Casks/pktriot.rb
@@ -1,6 +1,6 @@
 cask "pktriot" do
-  version "0.11.2"
-  sha256 "14ddacd77569abbb7f60db400b05b09c1c18734214a390d365276b630f7a1680"
+  version "0.12.0"
+  sha256 "8b4b28bfa9f05db5857116709d20e52db9d89e3d99be69e651f9d1cddc63734f"
 
   url "https://download.packetriot.com/macos/pktriot-#{version}.macos.tar.gz"
   name "pktriot"
@@ -10,7 +10,7 @@ cask "pktriot" do
   livecheck do
     url "https://packetriot.com/downloads"
     strategy :page_match
-    regex(/href=.*?pktriot[._-](\d+(?:\.\d+)*)\.macos\.t/i)
+    regex(/href=.*?pktriot[._-](\d+(?:\.\d+)+)\.macos\.t/i)
   end
 
   binary "pktriot-#{version}/pktriot"
