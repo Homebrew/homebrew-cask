0f5283568bb765ae563e620d9aa63fb66e450ccd
neutric <5984479+neutric@users.noreply.github.com>
2021-11-06 10:39:40 +0100

    Update birdfont from 4.19.5 to 4.20.1 (#113877)
    * Update birdfont from 4.19.5 to 4.20.1
    
    * birdfont: update legacy version

--- Casks/birdfont.rb
+++ Casks/birdfont.rb
@@ -3,16 +3,16 @@ cask "birdfont" do
     version "4.17.0"
     sha256 "74c778e3b7598a66b9ddb11b189ba6a21d68263fb0499a965dcedd0ac987b051"
   elsif MacOS.version <= :catalina
-    version "4.19.4"
-    sha256 "1a925a1f4bd1eee0c82e2a916f1c5f6c84344b7d69968a82ffe95eff002bb9eb"
+    version "4.20.0"
+    sha256 "10262559c24becf576cd8cc027fef458ec9f726a7ac2b173669506672dcf14ab"
 
     livecheck do
       url "https://birdfont.org/purchase.php"
       regex(%r{Mac\s*OS\s*10\.15.*?/birdfont[._-]v?(\d+(?:\.\d+)+)[._-]free\.dmg}i)
     end
   else
-    version "4.19.5"
-    sha256 "c6440b1712b8eab4cbc6526ddaae58ef6399c2d5e6bcbed712534138180205e4"
+    version "4.20.1"
+    sha256 "ebc718a984346ed1ed93551d1b7ad79d6b848492e6dde0723f2f2121a550bf20"
 
     livecheck do
       url "https://birdfont.org/purchase.php"
