ea85d3217c91f1254dbf0b255d61060c5576d198
neutric <5984479+neutric@users.noreply.github.com>
2021-11-06 23:49:07 +0100

    Update origin from 10.5.104.48966 to 10.5.106.49298 (#113937)

--- Casks/origin.rb
+++ Casks/origin.rb
@@ -1,5 +1,5 @@
 cask "origin" do
-  version "10.5.104.48966"
+  version "10.5.106.49298"
   sha256 :no_check
 
   url "https://origin-a.akamaihd.net/Origin-Client-Download/origin/mac/live/Origin.dmg",
