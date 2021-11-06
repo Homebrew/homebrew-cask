44422ea724426f56cb773d017583d5ae09411783
Jacob Ford <j@cobford.com>
2021-11-06 13:29:27 -0400

    Update mimestream from 0.30.2 to 0.30.3

--- Casks/mimestream.rb
+++ Casks/mimestream.rb
@@ -1,6 +1,6 @@
 cask "mimestream" do
-  version "0.30.2"
-  sha256 "c9bc5534bf1ecde08bcac12204128662273b727dc574248abbecc314a85d5078"
+  version "0.30.3"
+  sha256 "fbc3f3e15003729ad3e9ed433297de8156e6cd43abebae4f4c99b289c7e027cc"
 
   url "https://storage.googleapis.com/mimestream-releases/Mimestream_#{version}.dmg",
       verified: "storage.googleapis.com/mimestream-releases/"
