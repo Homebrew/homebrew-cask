fe055616f004597519ca074c6b50b5497a6eac10
denkristoffer <denkristoffer@users.noreply.github.com>
2021-11-06 20:56:44 +0100

    Add Actual.app Latest

---
+++ Casks/actual.rb
@@ -0,0 +1,11 @@
+cask "actual" do
+  version :latest
+  sha256 :no_check
+
+  url "https://download.actualbudget.com/download/dmg"
+  name "Actual"
+  desc "Super fast privacy-focused app for managing your finances"
+  homepage "https://actualbudget.com/"
+
+  app "Actual.app"
+end
