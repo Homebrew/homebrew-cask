43c61aa5d31459cede866734dfd562f69afcd9fc
Loyio Hex <i@loyio.me>
2021-11-06 23:50:45 +0800

    Update cctalk.rb (#113906)

--- Casks/cctalk.rb
+++ Casks/cctalk.rb
@@ -1,6 +1,6 @@
 cask "cctalk" do
-  version "7.8.8.3"
-  sha256 "08c58e8501176dbce024ea6440075fa7413bb3fc76bd38bf3b411264daf4686d"
+  version "7.9.0.3"
+  sha256 "37b59274ec2db2fd9e45ca113982691529452813acc0ced3e5f30b95249b1f05"
 
   url "https://cc.hjfile.cn/cc/#{version}/8/1/103/#{version}.dmg",
       verified: "cc.hjfile.cn/"
