7a0f8944027c55f2c4d985265918686311b8ea19
Prasannajit Acharya <prasannajit.acharya.kanhu@gmail.com>
2021-11-06 19:53:32 +0530

    Update ray from 1.18.1 to 1.18.3

--- Casks/ray.rb
+++ Casks/ray.rb
@@ -2,12 +2,12 @@ cask "ray" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
   folder = Hardware::CPU.intel? ? "" : "arm64/"
 
-  version "1.18.1"
+  version "1.18.3"
 
   if Hardware::CPU.intel?
-    sha256 "66809f2935f21dd1890a7c04e59371f95d96efc22ac1d16ab216f86761f29444"
+    sha256 "b2ba99ed35304def9a545641c51e2452404a589e22ed1747a82157972555094d"
   else
-    sha256 "6dff7448441e54d6c73a20943cec1d76609e1b706a96cc8da9c71c5e0c8f98de"
+    sha256 "28b636dc58347935275926cdf022c27c7a060e7d2fb3c0733734c431a7754acd"
   end
 
   url "https://ray-app.s3.eu-west-1.amazonaws.com/#{folder}Ray-#{version}#{arch}.dmg",
