cb4d12ad473acf6fbab87a9eff1aaaa2d08b362d
neutric <5984479+neutric@users.noreply.github.com>
2021-11-06 10:38:20 +0100

    Update busycontacts from 1.6.1,2021-10-26-07-08 to 1.6.2,2021-11-05-00-56 (#113874)

--- Casks/busycontacts.rb
+++ Casks/busycontacts.rb
@@ -1,6 +1,6 @@
 cask "busycontacts" do
-  version "1.6.1,2021-10-26-07-08"
-  sha256 "2872ad96d5147489b7bd8b6e07ecd59b7cd8555ad3e88ecc058f89819881df3b"
+  version "1.6.2,2021-11-05-00-56"
+  sha256 "1f53fecafed0861019c20ee85ee4862ea3090008f0e6c92224e2be885585ade8"
 
   url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bct-#{version.before_comma}-#{version.after_comma}.zip",
       verified: "7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/"
