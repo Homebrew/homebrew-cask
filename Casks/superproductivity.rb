043d5337a867b972e869d3687021f4380014e78c
Christian Werner <cwerner1@users.noreply.github.com>
2021-11-06 13:05:36 +0100

    Update superproductivity from 7.6.0 to 7.7.0 (#113900)

--- Casks/superproductivity.rb
+++ Casks/superproductivity.rb
@@ -1,6 +1,6 @@
 cask "superproductivity" do
-  version "7.6.0"
-  sha256 "672bda58a7dfcbe3c198fcb9cb3d031bc900582d9ee8285a4523ce1adc7e8cb1"
+  version "7.7.0"
+  sha256 "c1b8de8717056e7db8532b001f8388108dbddd8e222ad57ea7362dbf3fe06278"
 
   url "https://github.com/johannesjo/super-productivity/releases/download/v#{version}/superProductivity-#{version}-mac.zip",
       verified: "github.com/johannesjo/super-productivity/"
