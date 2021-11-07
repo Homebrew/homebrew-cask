2e90930e1060f4e57f86d2d948507ea292c2282f
stolen data <djinn@stolendata.net>
2021-11-07 18:51:28 +0100

    Adjust the build's macOS version requirement
    This build of 0.34.0 requires macOS Mojave or newer

--- Casks/mpv.rb
+++ Casks/mpv.rb
@@ -15,7 +15,7 @@ cask "mpv" do
   end
 
   conflicts_with formula: "mpv"
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :mojave"
 
   app "mpv.app"
   binary "#{appdir}/mpv.app/Contents/MacOS/mpv"
