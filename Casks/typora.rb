77ac33aa734be135d85636394c3c9294b5c219be
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-07 19:10:07 +0800

    Update typora.rb

--- Casks/typora.rb
+++ Casks/typora.rb
@@ -10,7 +10,7 @@ cask "typora" do
   livecheck do
     url "https://www.typora.io/download/dev_update.xml"
     strategy :page_match
-    regex(/Typora-(\d+(?:\.\d+)*)\.dmg/i)
+    regex(/Typora-(\d+(?:\.\d+)+)\.dmg/i)
   end
 
   auto_updates true
@@ -19,9 +19,9 @@ cask "typora" do
   app "Typora.app"
 
   zap trash: [
+    "~/Library/Application Support/abnerworks.Typora",
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/abnerworks.typora.sfl*",
     "~/Library/Application Support/Typora",
-    "~/Library/Application Support/abnerworks.Typora",
     "~/Library/Caches/abnerworks.Typora",
     "~/Library/Cookies/abnerworks.Typora.binarycookies",
     "~/Library/Preferences/abnerworks.Typora.plist",
