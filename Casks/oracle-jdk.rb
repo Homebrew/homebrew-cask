6c8e632763cfd3d91a47a8846c9c61b39e99b4a2
Youfu Zhang <zhangyoufu@gmail.com>
2021-11-08 13:51:18 +0800

    Use archive URL instead of latest URL for oracle-jdk

--- Casks/oracle-jdk.rb
+++ Casks/oracle-jdk.rb
@@ -2,9 +2,9 @@ cask "oracle-jdk" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
 
   version "17.0.1"
-  sha256 :no_check   # required as upstream package is updated in place
+  sha256 "1cddd0c4b505bc78459e4353d6dadb81c4a9c1f15934d87531bef7d4c52d8e51"
 
-  url "https://download.oracle.com/java/#{version.major}/latest/jdk-#{version.major}_macos-#{arch}_bin.dmg"
+  url "https://download.oracle.com/java/#{version.major}/archive/jdk-#{version}_macos-#{arch}_bin.dmg"
   name "Oracle Java Standard Edition Development Kit"
   desc "JDK from Oracle"
   homepage "https://www.oracle.com/java/technologies/downloads/"
