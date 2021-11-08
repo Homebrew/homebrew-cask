f3128fc0ae9118432cc410e5566f61776ad14c21
neutric <5984479+neutric@users.noreply.github.com>
2021-11-09 00:48:57 +0100

    Update chromedriver from 95.0.4638.54 to 95.0.4638.69 (#114025)

--- Casks/chromedriver.rb
+++ Casks/chromedriver.rb
@@ -1,12 +1,12 @@
 cask "chromedriver" do
   arch = Hardware::CPU.intel? ? "mac64" : "mac64_m1"
 
-  version "95.0.4638.54"
+  version "95.0.4638.69"
 
   if Hardware::CPU.intel?
-    sha256 "94a4f2ff4a853878169eeb94648a0ffd413a8c217b98da26e20728870243b1fb"
+    sha256 "7d70a8c0441ef9b572ac17ecbb2f1976a1689b2ed04aebb7e5586fe097ddf830"
   else
-    sha256 "df84d80e3111abd493634934e4f9033db6329f318abeabdd1cabb8763fd6dc29"
+    sha256 "11b8e73ea5c1a01a17ae484c63155fb5a9674a3799c0225184aec305a5f705fe"
   end
 
   url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_#{arch}.zip",
