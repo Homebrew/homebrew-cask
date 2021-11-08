4936985d20e272b5aa8bbe589160b311232cc134
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-08 11:35:22 -0800

    Update dotnet from 5.0.11 to 6.0.0 (#114016)

--- Casks/dotnet.rb
+++ Casks/dotnet.rb
@@ -1,8 +1,15 @@
 cask "dotnet" do
-  version "5.0.11,f241f934-a4cc-400a-8d03-a5ab50c25fea:1c8600ad088a42b157c073454e80039a"
-  sha256 "4f8ea31f5d15d72b375356325970fca205ade9f7f18193fcea1749e7895994ce"
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-runtime-#{version.before_comma}-osx-x64.pkg"
+  if Hardware::CPU.intel?
+    version "6.0.0,03e62824-4061-45dc-8777-93e82205148d:81489074c065675bd713a9cbeb995bef"
+    sha256 "fedbddb71e18d68279433e22326220225b3ee45209966fce818ac34349eefef4"
+  else
+    version "6.0.0,1bc784af-565c-42d5-8db2-9988c8d2b65f:644cea0fe2f712ad297b6eb49147f446"
+    sha256 "dab12623b00707a8e2c5e29438de7a9e738880111d59a5be215fda4a427a9d59"
+  end
+
+  url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-runtime-#{version.before_comma}-osx-#{arch}.pkg"
   name ".Net Runtime"
   desc "Developer platform"
   homepage "https://www.microsoft.com/net/core#macos"
@@ -13,7 +20,7 @@ cask "dotnet" do
   livecheck do
     url "https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/#{version.major_minor}/releases.json"
     strategy :page_match do |page|
-      page.scan(%r{/download/pr/([^/]+)/([^/]+)/dotnet-runtime-v?(\d+(?:\.\d+)+)-osx-x64\.pkg}i).map do |match|
+      page.scan(%r{/download/pr/([^/]+)/([^/]+)/dotnet-runtime-v?(\d+(?:\.\d+)+)-osx-#{arch}\.pkg}i).map do |match|
         "#{match[2]},#{match[0]}:#{match[1]}"
       end
     end
@@ -24,9 +31,9 @@ cask "dotnet" do
     "homebrew/cask-versions/dotnet-preview",
     "homebrew/cask-versions/dotnet-sdk-preview",
   ]
-  depends_on macos: ">= :high_sierra"
+  depends_on macos: ">= :mojave"
 
-  pkg "dotnet-runtime-#{version.before_comma}-osx-x64.pkg"
+  pkg "dotnet-runtime-#{version.before_comma}-osx-#{arch}.pkg"
   binary "/usr/local/share/dotnet/dotnet"
 
   uninstall pkgutil: "com.microsoft.dotnet.*",
