32935636bbeb0826103af1ab12c89e5ce55bb55a
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-08 14:44:12 +0800

    Update oracle-jdk.rb

--- Casks/oracle-jdk.rb
+++ Casks/oracle-jdk.rb
@@ -2,7 +2,12 @@ cask "oracle-jdk" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
 
   version "17.0.1"
-  sha256 "1cddd0c4b505bc78459e4353d6dadb81c4a9c1f15934d87531bef7d4c52d8e51"
+
+  if Hardware::CPU.intel?
+    sha256 "1cddd0c4b505bc78459e4353d6dadb81c4a9c1f15934d87531bef7d4c52d8e51"
+  else
+    sha256 "5d88b9c8e51650b46545bcb95842025a58d80c068f9d8f8062a8b6276620d8d3"
+  end
 
   url "https://download.oracle.com/java/#{version.major}/archive/jdk-#{version}_macos-#{arch}_bin.dmg"
   name "Oracle Java Standard Edition Development Kit"
