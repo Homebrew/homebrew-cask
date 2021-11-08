20a22ae69ded3da2d413b0685183f637925dfb9f
Eric Knibbe <enk3@outlook.com>
2021-11-08 18:48:05 -0500

    Update openttd from 12.0 to 12.1 (#114026)

--- Casks/openttd.rb
+++ Casks/openttd.rb
@@ -1,6 +1,11 @@
 cask "openttd" do
-  version "12.0"
-  sha256 "67466d41afe806db5ffa7d9115d4c633713075d0cd839d3b419e285efea42ac0"
+  if MacOS.version <= :high_sierra
+    version "1.11.2"
+    sha256 "44f7e08b806124cce8d99c5adc906eb4280ba8057609b8c309080d63fcfb17c2"
+  else
+    version "12.1"
+    sha256 "e7f7f84a6372a2ef11756c727b92c4778592ea9eb899168e4da772ed6a7e1640"
+  end
 
   url "https://proxy.binaries.openttd.org/openttd-releases/#{version}/openttd-#{version}-macos-universal.zip"
   name "OpenTTD"
