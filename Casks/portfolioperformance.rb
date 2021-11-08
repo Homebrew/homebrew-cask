5a22494365ab3870d503efa34eaa9c8d149a34a3
Martin Schulze <github.com@mhhs.de>
2021-11-08 10:29:37 +0100

    Update PortfolioPerformance for Apple M1 & latest version (#113998)
    * portfolioperformance update for Apple M1 & latest version
    
    * Update portfolioperformance.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/portfolioperformance.rb
+++ Casks/portfolioperformance.rb
@@ -1,8 +1,15 @@
 cask "portfolioperformance" do
-  version "0.55.0"
-  sha256 "1e9f94f4a6792980e55993f57dc5f7a59713b3335c5dafbbf3b06aeea4b5416b"
+  arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
-  url "https://github.com/buchen/portfolio/releases/download/#{version}/PortfolioPerformance-#{version}.dmg",
+  version "0.56.0"
+
+  if Hardware::CPU.intel?
+    sha256 "171b72c885c3ccd29992bc79638d2a97c6eb39b6cfdb5511bd50383f26ccaea7"
+  else
+    sha256 "82ffe92a1d92d7596d7bb9f1e12209f0d78d651ce040bd889a1b98dbb02aec2f"
+  end
+
+  url "https://github.com/buchen/portfolio/releases/download/#{version}/PortfolioPerformance-#{version}-#{arch}.dmg",
       verified: "github.com/buchen/portfolio/"
   name "Portfolio Performance"
   desc "Calculate the overall performance of an investment portfolio"
