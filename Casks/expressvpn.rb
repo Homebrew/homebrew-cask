eaef6b4493cdffa5859e3230d2d53fbeb7af3915
Bevan Kay <email@bevankay.me>
2021-11-09 15:08:48 +1100

    Update expressvpn from 10.7.0.13 to 10.8.0.2 (#114041)

--- Casks/expressvpn.rb
+++ Casks/expressvpn.rb
@@ -1,6 +1,6 @@
 cask "expressvpn" do
-  version "10.7.0.13"
-  sha256 "9c1a9fbb9ed72d956f873b7e5c9912e541abcd6f98ceed18ae5a1a25fcd28840"
+  version "10.8.0.2"
+  sha256 "31da59779c3e5e388ef84e5186791a81dab8c272c8d1d0809d633850d5f80fb9"
 
   url "https://www.expressvpn.works/clients/mac/expressvpn_mac_#{version}_release.pkg"
   name "ExpressVPN"
