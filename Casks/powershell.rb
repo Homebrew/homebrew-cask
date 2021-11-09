0d781e9913443a0a9836454fc678fa3068d6d8fa
Alex Harper <30801293+mcx808@users.noreply.github.com>
2021-11-09 10:45:57 +0000

    Update powershell.rb (#114048)
    Updated to select arm64 package. 
    (Ruby newbie, guessed the syntax from another package in the repo).

--- Casks/powershell.rb
+++ Casks/powershell.rb
@@ -1,8 +1,15 @@
 cask "powershell" do
   version "7.2.0"
-  sha256 "f73682927bdadb127cf0c18641d5efe9ea12e53053e65d5c54f06fd4ef413a4a"
 
-  url "https://github.com/PowerShell/PowerShell/releases/download/v#{version}/powershell-#{version}-osx-x64.pkg"
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
+
+  if Hardware::CPU.intel?
+    sha256 "f73682927bdadb127cf0c18641d5efe9ea12e53053e65d5c54f06fd4ef413a4a"
+  else
+    sha256 "6293A48CEAD807DD3C9F932176021533254BD6883754C5574E36A1775D797959"
+  end
+
+  url "https://github.com/PowerShell/PowerShell/releases/download/v#{version}/powershell-#{version}-osx-#{arch}.pkg"
   name "PowerShell"
   desc "Command-line shell and scripting language"
   homepage "https://github.com/PowerShell/PowerShell"
@@ -15,7 +22,7 @@ cask "powershell" do
 
   depends_on macos: ">= :high_sierra"
 
-  pkg "powershell-#{version}-osx-x64.pkg"
+  pkg "powershell-#{version}-osx-#{arch}.pkg"
 
   uninstall pkgutil: "com.microsoft.powershell"
 
