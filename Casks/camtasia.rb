ac18339a01061818f6d455a5358a2516a11748c6
neutric <5984479+neutric@users.noreply.github.com>
2021-11-09 03:12:28 +0100

    Update camtasia from 2021.0.5 to 2021.0.6 (#114031)
    * Update camtasia from 2021.0.5 to 2021.0.6
    
    * camtasia: add zap+update homepage
    
    * update livecheck
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/camtasia.rb
+++ Casks/camtasia.rb
@@ -1,14 +1,14 @@
 cask "camtasia" do
-  version "2021.0.5"
-  sha256 "916aba24ba18a8822e39e5902d9f98e561560a21e9fb6eab835405fdafa0d375"
+  version "2021.0.6"
+  sha256 "1527ca511437bc2b385e48a42059f944c531f73bf9e00fc491238e35bc2514b5"
 
   url "https://download.techsmith.com/camtasiamac/releases/#{version.major[-2..]}#{version.minor_patch.no_dots}/Camtasia.dmg"
   name "Camtasia"
   desc "Screen recorder and video editor"
-  homepage "https://www.techsmith.com/camtasia.html"
+  homepage "https://www.techsmith.com/video-editor.html"
 
   livecheck do
-    url "https://support.techsmith.com/hc/en-us/articles/115006624748-Camtasia-Mac-Version-History"
+    url "https://support.techsmith.com/api/v2/help_center/en-us/articles/115006624748"
     regex(/Camtasia\s*\(Mac\)\s*(\d+(?:\.\d+)+)/i)
   end
 
@@ -16,4 +16,10 @@ cask "camtasia" do
   depends_on macos: ">= :mojave"
 
   app "Camtasia #{version.major}.app"
+
+  zap trash: [
+    "~/Library/Application Support/TechSmith/Camtasia#{version.major}",
+    "~/Library/Preferences/com.techsmith.camtasia2021.plist",
+    "/Users/Shared/TechSmith/Camtasia",
+  ]
 end
