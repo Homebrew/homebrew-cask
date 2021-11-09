3de8554c7dd5e4d0a1b45afa8b9c02e4acbba5f3
neutric <5984479+neutric@users.noreply.github.com>
2021-11-09 01:36:57 +0100

    Update camerabag-photo from 2021.2.1 to 2021.4.0 (#114030)
    * Update camerabag-photo from 2021.2.1 to 2021.4.0
    
    * camerabag-photo: add depends_on
    
    * camerabag-photo: add zap
    
    * camerabag-photo: tweak livecheck regex

--- Casks/camerabag-photo.rb
+++ Casks/camerabag-photo.rb
@@ -1,6 +1,6 @@
 cask "camerabag-photo" do
-  version "2021.2.1"
-  sha256 "e43d53a1059fe7bada8524b3e8894e7a9e3a51cbdbb4021f7cf94dca4bcaa5bd"
+  version "2021.4.0"
+  sha256 "d43a4857f998a23043ae16c0a3368732b4dd422e586b8ab7606e3920858a8d59"
 
   url "https://nevercenter.com/camerabag/download/filearchive/Install_CameraBag_Photo_#{version.dots_to_underscores}_mac.dmg"
   name "CameraBag"
@@ -9,11 +9,15 @@ cask "camerabag-photo" do
 
   livecheck do
     url "https://nevercenter.com/camerabag/download/?filetype=mac"
-    regex(/href=.*?Install[._-]?CameraBag[._-]?Photo[._-]?v?(\d+(?:[._]\d+)*)[._-]?mac\.dmg/i)
+    regex(/href=.*?Install[._-]?CameraBag[._-]?Photo[._-]?v?(\d+(?:[._]\d+)+)[._-]?mac\.dmg/i)
     strategy :page_match do |page, regex|
       page.scan(regex).map { |match| match[0].tr("_", ".") }
     end
   end
 
+  depends_on macos: ">= :catalina"
+
   app "CameraBag Photo.app"
+
+  zap trash: "~/Library/Saved Application State/com.nevercenter.camerabagphoto.savedState"
 end
