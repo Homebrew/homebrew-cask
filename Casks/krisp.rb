4422dcfea03147ca8ef78ee43d78840d13aca06b
Amory Meltzer <Amorymeltzer@gmail.com>
2021-11-10 08:22:54 -0500

    Update krisp from 1.28.7 to 1.29.6

--- Casks/krisp.rb
+++ Casks/krisp.rb
@@ -1,6 +1,6 @@
 cask "krisp" do
-  version "1.28.7"
-  sha256 "7aace940042a3347e1b72e5f315494446f63574e8f1120ebf2d4bd1b1f2026d1"
+  version "1.29.6"
+  sha256 "c7672b116219c1a2134a7a24267428a09975bb849aeae1d35b7200159e917c5d"
 
   url "https://cdn.krisp.ai/mac/release/v#{version.major}.#{version.minor}/krisp_#{version}.pkg"
   name "Krisp"
