473444ea339a3f17afd90e69a8bfbaa3fccc6aeb
Bevan Kay <email@bevankay.me>
2021-11-11 10:56:39 +1100

    Bump version, add zap, add depends_on, version url, add sha256

--- Casks/diskcatalogmaker.rb
+++ Casks/diskcatalogmaker.rb
@@ -1,8 +1,8 @@
 cask "diskcatalogmaker" do
-  version "8.3.9"
-  sha256 :no_check
+  version "8.4.1"
+  sha256 "01c60578e6919268517d4c32ebe7a58e9669aec4e3f250877f5ce9f2e6eeb035"
 
-  url "https://download.diskcatalogmaker.com/zip/DiskCatalogMaker.zip"
+  url "https://download.diskcatalogmaker.com/download/zip/DiskCatalogMaker#{version.no_dots}.zip"
   name "DiskCatalogMaker"
   desc "Toast-bundled version of DiskCatalogMaker"
   homepage "https://diskcatalogmaker.com/"
@@ -17,5 +17,17 @@ cask "diskcatalogmaker" do
     end
   end
 
+  depends_on macos: ">= :el_capitan"
+
   app "DiskCatalogMaker.app"
+
+  zap trash: [
+    "~/Library/Application Support/DiskCatalogMaker",
+    "~/Library/Application Support/com.mac.fujisoft.DiskCatalogMaker",
+    "~/Library/Caches/DiskCatalogMaker",
+    "~/Library/Caches/com.mac.fujisoft.DiskCatalogMaker",
+    "~/Library/Logs/DiskCatalogMaker",
+    "~/Library/Preferences/com.mac.fujisoft.DiskCatalogMaker.plist",
+    "~/Library/Saved Application State/com.mac.fujisoft.DiskCatalogMaker.savedState",
+  ]
 end
