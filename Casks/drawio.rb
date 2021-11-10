7ca6cd83158921a3d3d214452a76a54b7dc0466d
Hannes Braun <hannesbraun@mail.de>
2021-11-11 00:30:49 +0100

    Update drawio from 15.4.0 to 15.7.3 (#114132)

--- Casks/drawio.rb
+++ Casks/drawio.rb
@@ -1,14 +1,14 @@
 cask "drawio" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "15.4.0"
+  version "15.7.3"
 
   url "https://github.com/jgraph/drawio-desktop/releases/download/v#{version}/draw.io-#{arch}-#{version}.dmg",
       verified: "github.com/jgraph/drawio-desktop/"
   if Hardware::CPU.intel?
-    sha256 "58e628ebae2c203c54d7e1ab031ffe572126d100507d06558cf48eb7455cd6cf"
+    sha256 "62e6a8afd22a100021f73fbbefb21c1751dc91a5df23da5b2a8bc7a9ccc2763c"
   else
-    sha256 "9baee8d9dce105a22e5d9c04589007fcd0a482e6d72416b033e5e4485a42ed2a"
+    sha256 "f3340df9bddcd813c7b3fbdf3cd90dfcbd930d1cfffbb9c071148cb8c9a1c729"
   end
 
   name "draw.io Desktop"
