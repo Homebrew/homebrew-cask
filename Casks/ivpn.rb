d588b96ac5c6e45860b3ab8043459579b2a3c51e
drewfravert <drew@fravert.org>
2021-11-09 18:43:41 -0600

    Update ivpn from 3.3.30 to 3.4.0

--- Casks/ivpn.rb
+++ Casks/ivpn.rb
@@ -1,6 +1,6 @@
 cask "ivpn" do
-  version "3.3.30"
-  sha256 "7155967dda8f53580ab2d158fa57b447efe0c40a29f28b884bf33fc0f8fcb12d"
+  version "3.4.0"
+  sha256 "ca9d45f7df2eb95fa5f57ada9012d6add95113635b74f21df36c40725687b3f2"
 
   url "https://repo.ivpn.net/macos/bin/IVPN-#{version}.dmg"
   name "IVPN"
