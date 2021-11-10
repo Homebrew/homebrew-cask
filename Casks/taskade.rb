5d05c23a7abc79a89969edc8a7068f1ee0abf3b9
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-11-10 20:35:25 +0900

    Update taskade from 4.0.0 to 4.1.1

--- Casks/taskade.rb
+++ Casks/taskade.rb
@@ -1,6 +1,6 @@
 cask "taskade" do
-  version "4.0.0"
-  sha256 "749dbb7e06a7b9796b33c709b534d79a89e12458d010865739fb0ad36aed2835"
+  version "4.1.1"
+  sha256 "cf1d4d37e3cc2215d2822fa87114920d718d03cdaccb1b802fc5cb84831a0d97"
 
   url "https://apps.taskade.com/updates/Taskade-#{version}-universal.dmg"
   name "Taskade"
