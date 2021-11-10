cfb4aedb1a37ac4facaaa8848f7fd1115e4d4d95
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-11 05:15:41 +0800

    Update pichon.rb

--- Casks/pichon.rb
+++ Casks/pichon.rb
@@ -1,11 +1,16 @@
 cask "pichon" do
-  version "1.0.8.1,10"
+  version "1.0.8.1"
   sha256 :no_check
 
   url "https://desktop.icons8.com/updates/pichon/Pichon.dmg"
   name "Pichon"
   desc "Search utility for icons8"
   homepage "https://icons8.com/"
+
+  livecheck do
+    url "https://desktop.icons8.com/updates/pichon/cast.xml"
+    strategy :sparkle
+  end
 
   app "Pichon.app"
 
