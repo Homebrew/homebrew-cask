8d0ca7de7e317c693427de3beea0323643b0517c
Veronica <nanodesu@machiavelli.local>
2021-11-10 11:56:37 +0300

    Update yggdrasil from 0.4.2 to 0.4.2

--- Casks/yggdrasil.rb
+++ Casks/yggdrasil.rb
@@ -1,13 +1,13 @@
 cask "yggdrasil" do
-  version "0.4.0"
-  sha256 "2425d0bdef84de6c91865e330ec4d27266a5712b015b6c3c771a9745d011e4c7"
+  version "0.4.2"
+  sha256 "fe317aa64a31ff47d6f27d2ac81694c6bf0aafacd96bee02ede1add8a6a2e7a4"
 
-  url "https://github.com/yggdrasil-network/yggdrasil-go/releases/download/v#{version}/yggdrasil-#{version}-macos-amd64.pkg"
+  url "https://github.com/yggdrasil-network/yggdrasil-go/releases/download/v#{version}/yggdrasil-#{version}-macos-arm64.pkg"
   name "Yggdrasil"
   desc "End-to-end encrypted IPv6 networking to connect worlds"
   homepage "https://github.com/yggdrasil-network/yggdrasil-go"
 
-  pkg "yggdrasil-#{version}-macos-amd64.pkg"
+  pkg "yggdrasil-#{version}-macos-arm64.pkg"
 
   uninstall launchctl: "yggdrasil",
             pkgutil:   "io.github.yggdrasil-network.pkg"
