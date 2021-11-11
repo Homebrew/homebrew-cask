3e648ee80ba96953232a2ee9fa4351dcba0d87d0
J-rg <j_rg@live.de>
2021-11-11 16:13:15 +0100

    Update audacity from 3.1.0 to 3.1.1

--- Casks/audacity.rb
+++ Casks/audacity.rb
@@ -1,6 +1,6 @@
 cask "audacity" do
-  version "3.1.0"
-  sha256 "0a74e6c52c5b8e22b327ed3741e72ea7e3ae87d55c4cdfa370050c4013556d66"
+  version "3.1.1"
+  sha256 "84b0e9590bf0b418d1722aa04a933658840c0945e9c4e52daacb9c2e36ab80a6"
 
   url "https://github.com/audacity/audacity/releases/download/Audacity-#{version}/audacity-macos-#{version}-Intel.dmg",
       verified: "github.com/audacity/audacity/"
