cd24235e1e3fae57ede14f83622e785f433fd98e
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-11 01:47:13 -0800

    plex-media-server: replace dash in version string with comma (#114153)
    * plex-media-server: replace dash in version string with comma
    
    * Fix livecheck

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -1,16 +1,18 @@
 cask "plex-media-server" do
-  version "1.24.5.5173-8dcc73a59"
+  version "1.24.5.5173,8dcc73a59"
   sha256 "2eff6614f4ac9397988bd368855e98226e75c20c09a583ff18fedcd4fe3ce752"
 
-  url "https://downloads.plex.tv/plex-media-server-new/#{version}/macos/PlexMediaServer-#{version}-x86_64.zip"
+  url "https://downloads.plex.tv/plex-media-server-new/#{version.before_comma}-#{version.after_comma}/macos/PlexMediaServer-#{version.before_comma}-#{version.after_comma}-x86_64.zip"
   name "Plex Media Server"
   desc "Home media server"
   homepage "https://www.plex.tv/"
 
   livecheck do
     url "https://plex.tv/api/downloads/5.json"
-    strategy :page_match
-    regex(%r{href=.*?/PlexMediaServer-(\d+(?:\.\d+)*-[\da-f]+)-x86_64\.zip}i)
+    regex(%r{href=.*?/PlexMediaServer-(\d+(?:\.\d+)*)-([\da-f]+)-x86_64\.zip}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
+    end
   end
 
   auto_updates true
