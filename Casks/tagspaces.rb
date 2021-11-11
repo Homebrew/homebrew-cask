cf7adb2b0cd79a2d591e7821e4d50d9ea1534dc5
Lucas Freitas <4260591+lucasff@users.noreply.github.com>
2021-11-11 04:32:14 +0100

    Update tagspaces.rb

--- Casks/tagspaces.rb
+++ Casks/tagspaces.rb
@@ -1,12 +1,12 @@
 cask "tagspaces" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "4.0.4"
+  version "4.0.6"
 
   if Hardware::CPU.intel?
-    sha256 "de7a1e1fd37115d680424ffdd60e14edd7c309a105d351410e284658af303660"
+    sha256 "836cba027c72bc8fa9e5a2fd661d28bb3ff76ada1e90bda630049d605a0842e3"
   else
-    sha256 "5c467d1755e0f7d1589abb83921bf46f3b4749ba8ab0687065d4860254bfa38f"
+    sha256 "61666f22ceff186a9aa7ba9365ca259ee4d336c14c658f8e89c49a82c40c5df0"
   end
 
   url "https://github.com/tagspaces/tagspaces/releases/download/v#{version}/tagspaces-mac-#{arch}-#{version}.dmg",
