b936c5a883bdcb195480135338e9ca9192002b65
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-11 01:46:50 -0800

    lens: replace version substring '-latest.' with comma (#114151)
    * lens: replace version substring '-latest.' with comma
    
    * Fix livecheck

--- Casks/lens.rb
+++ Casks/lens.rb
@@ -1,7 +1,7 @@
 cask "lens" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  version "5.2.7-latest.20211110.1"
+  version "5.2.7,20211110.1"
 
   if Hardware::CPU.intel?
     sha256 "51312def38e7c00030a285204ac47f6822ad1b31b93b0d8369947e4ab05cb07f"
@@ -9,14 +9,16 @@ cask "lens" do
     sha256 "8f220e3541c6e5a6d1524532e3a9cafe7e9c33fa9ae0a27c7646500df6af63fe"
   end
 
-  url "https://api.k8slens.dev/binaries/Lens-#{version}#{arch}.dmg"
+  url "https://api.k8slens.dev/binaries/Lens-#{version.before_comma}-latest.#{version.after_comma}#{arch}.dmg"
   name "Lens"
   desc "Kubernetes IDE"
   homepage "https://k8slens.dev/"
 
   livecheck do
     url "https://lens-binaries.s3.amazonaws.com/ide/latest-mac.yml"
-    strategy :electron_builder
+    strategy :electron_builder do |data|
+      data["version"].sub("-latest.", ",")
+    end
   end
 
   auto_updates true
