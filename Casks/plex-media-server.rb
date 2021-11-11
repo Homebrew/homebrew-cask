e910242145d886fd09a9530cc65feba25ed50212
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-10 22:02:40 -0800

    plex-media-server: replace dash in version string with comma

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -1,8 +1,8 @@
 cask "plex-media-server" do
-  version "1.24.5.5173-8dcc73a59"
+  version "1.24.5.5173,8dcc73a59"
   sha256 "2eff6614f4ac9397988bd368855e98226e75c20c09a583ff18fedcd4fe3ce752"
 
-  url "https://downloads.plex.tv/plex-media-server-new/#{version}/macos/PlexMediaServer-#{version}-x86_64.zip"
+  url "https://downloads.plex.tv/plex-media-server-new/#{version.before_comma}-#{version.after_comma}/macos/PlexMediaServer-#{version.before_comma}-#{version.after_comma}-x86_64.zip"
   name "Plex Media Server"
   desc "Home media server"
   homepage "https://www.plex.tv/"
