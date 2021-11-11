8623da6aeeab4f9e1fc76fc0a39ff97cbd65cebe
Mohammad Momayezi <mhdmyz@MHDMYZs-MacBook-Pro.local>
2021-11-11 18:20:40 +0330

    Update miniforge from 4.10.3-7 to 4.10.3-8

--- Casks/miniforge.rb
+++ Casks/miniforge.rb
@@ -1,13 +1,13 @@
 cask "miniforge" do
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 
-  version "4.10.3-7"
+  version "4.10.3-8"
 
   url "https://github.com/conda-forge/miniforge/releases/download/#{version}/Miniforge3-#{version}-MacOSX-#{arch}.sh"
   if Hardware::CPU.intel?
-    sha256 "a25c1b381b20873ed856ce675a7a2ccf48f1d6782a5cdce9f06496e6ffa7883f"
+    sha256 "9c123f45da81878b3b3f221dcb7595f6420cac0310235316a7deba93cf12bbe3"
   else
-    sha256 "3cd1f11743f936ba522709eb7a173930c299ac681671a909b664222329a56290"
+    sha256 "ed1f245d8effb463c9bd5d1cde034e7e291684e0668885a87deb34303dafced5"
   end
 
   name "miniforge"
