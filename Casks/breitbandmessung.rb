3e756cda83a7aa5a05051fedcb104b5cf7278723
ZepDonald <63632671+ZepDonald@users.noreply.github.com>
2021-11-11 23:34:14 +0100

    Update breitbandmessung from 2.0.3 to 2.0.4 (#114174)
    * Update breitbandmessung from 2.0.3 to 2.0.4
    
    * version url, add sha256
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/breitbandmessung.rb
+++ Casks/breitbandmessung.rb
@@ -1,8 +1,8 @@
 cask "breitbandmessung" do
-  version "2.0.3"
-  sha256 :no_check
+  version "2.0.4"
+  sha256 "e2ed49a11dcd57cef794873e4bc7a3ba2cfb04de0896957daac91c11cb83bdce"
 
-  url "https://download.breitbandmessung.de/bbm/Breitbandmessung-mac.dmg"
+  url "https://download.breitbandmessung.de/bbm/Breitbandmessung-#{version}-mac.dmg"
   name "Breitbandmessung"
   desc "Official internet speed test from the German Bundesnetzagentur"
   homepage "https://www.breitbandmessung.de/"
