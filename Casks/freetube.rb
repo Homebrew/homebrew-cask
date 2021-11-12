d33702d7f8586143c8f2ccc8ce35274f56ec1bfa
Clément L <porkepix@gmail.com>
2021-11-12 11:55:17 +0100

    Update freetube from 0.15.0 to 0.15.1 (#114214)

--- Casks/freetube.rb
+++ Casks/freetube.rb
@@ -1,6 +1,6 @@
 cask "freetube" do
-  version "0.15.0"
-  sha256 "928115aaf71ef84743852051810dd30d221c425d49c3bf6a6f1390e6f47e1478"
+  version "0.15.1"
+  sha256 "33c663f726be44c5ea570a4545afc3581190ad9d1a37499e931db87639a1b400"
 
   url "https://github.com/FreeTubeApp/FreeTube/releases/download/v#{version}-beta/freetube-#{version}-mac.dmg"
   name "FreeTube"
