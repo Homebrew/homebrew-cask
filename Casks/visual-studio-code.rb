63800b7f80bb25348796f22177f54b73fd120b71
César Román <thecesrom@gmail.com>
2021-11-11 17:52:28 -0800

    Update visual-studio-code from 1.62.1 to 1.62.2 (#114188)
    * Update visual-studio-code from 1.62.1 to 1.62.2
    
    * Update visual-studio-code.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -1,12 +1,12 @@
 cask "visual-studio-code" do
   arch = Hardware::CPU.intel? ? "darwin" : "darwin-arm64"
 
-  version "1.62.1"
+  version "1.62.2"
 
   if Hardware::CPU.intel?
-    sha256 "77c4e891d7cf837ac5ceb718237a85dab7501cec8dadf659dce2849d783c0dad"
+    sha256 "3c05dda98d93fb187de46869f45d842e3265e6f48bb7f5cca170830cd036276a"
   else
-    sha256 "42f1a841d1aa403c99caa0b89a5119c2d8ba978615b9eca4c39de4336094d8de"
+    sha256 "1ab74c80b2310764b16ff51e43df7d8103a354ff7b4f3df923aabc7300c13b62"
   end
 
   url "https://update.code.visualstudio.com/#{version}/#{arch}/stable"
@@ -18,7 +18,7 @@ cask "visual-studio-code" do
   livecheck do
     url "https://update.code.visualstudio.com/api/update/#{arch}/stable/VERSION"
     strategy :page_match
-    regex(/"productVersion"\s*:\s*"(\d+(:?\.\d+)*)"/)
+    regex(/"productVersion"\s*:\s*"(\d+(:?\.\d+)+)"/)
   end
 
   auto_updates true
