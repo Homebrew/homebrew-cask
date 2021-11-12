93b43f96c4cc1fa713ca1d60e08b423b5703bd06
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-11 23:27:08 -0800

    bettertouchtool: replace dash with comma in version string (#114206)

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -1,8 +1,8 @@
 cask "bettertouchtool" do
-  version "3.622-1771"
+  version "3.622,1771"
   sha256 "5a032b0ab107741c0bf3f322892cce0d36f4e19f9ed4f44e98b61cad4502d037"
 
-  url "https://folivora.ai/releases/btt#{version}.zip"
+  url "https://folivora.ai/releases/btt#{version.before_comma}-#{version.after_comma}.zip"
   name "BetterTouchTool"
   desc "Tool to customize input devices and automate computer systems"
   homepage "https://folivora.ai/"
@@ -10,9 +10,10 @@ cask "bettertouchtool" do
   livecheck do
     url "https://folivora.ai/releases/"
     strategy :page_match do |page|
-      page.scan(/btt(\d+(?:.\d+)*)\.zip.*?(\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2})/i)
+      page.scan(/btt(\d+(?:[._-]\d+)*)\.zip.*?(\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2})/i)
           .max_by { |(_, time)| Time.parse(time) }
           .first
+          .tr("-", ",")
     end
   end
 
