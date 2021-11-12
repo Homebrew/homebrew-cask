fe2a22a4bdf9ef4595473c4e866991c5a62aae71
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-11 23:45:01 -0800

    anka-build-cloud-controller-and-registry: replace dash with comma in version string (#114209)
    * anka-build-cloud-controller-and-registry: replace dash with comma in version string
    
    * Update anka-build-cloud-controller-and-registry.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/anka-build-cloud-controller-and-registry.rb
+++ Casks/anka-build-cloud-controller-and-registry.rb
@@ -1,21 +1,26 @@
 cask "anka-build-cloud-controller-and-registry" do
-  version "1.20.0-035872f5"
+  version "1.20.0,035872f5"
   sha256 "cf375336267ce1712995d26fad9a0aa690c95d6e98ee87b9bdb8caae814eb6c3"
 
-  url "https://downloads.veertu.com/anka/AnkaControllerRegistry-#{version}.pkg"
+  url "https://downloads.veertu.com/anka/AnkaControllerRegistry-#{version.before_comma}-#{version.after_comma}.pkg"
   name "Anka Build Cloud Controller & Registry"
   desc "Virtual machine management GUI/API and registry"
   homepage "https://veertu.com/"
 
   livecheck do
     url "https://veertu.com/downloads/ankacontroller-registry-mac-latest"
-    strategy :header_match
-    regex(/AnkaControllerRegistry[._-]?v?(\d+(?:\.\d+)*[._-]\h+)\.pkg/i)
+    regex(/AnkaControllerRegistry[._-]?v?(\d+(?:\.\d+)+)[._-](\h+)\.pkg/i)
+    strategy :header_match do |headers, regex|
+      match = headers["location"]&.match(regex)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
   end
 
   depends_on macos: ">= :yosemite"
 
-  pkg "AnkaControllerRegistry-#{version}.pkg"
+  pkg "AnkaControllerRegistry-#{version.before_comma}-#{version.after_comma}.pkg"
 
   uninstall script: {
     executable: "/Library/Application Support/Veertu/Anka/tools/controller/uninstall.sh",
