b15bfbd4d4ab0ce9c9af3c47ab0ac41339205e29
neutric <5984479+neutric@users.noreply.github.com>
2021-11-12 02:47:44 +0100

    Update electerm from 1.17.3 to 1.17.15 (#114200)
    * Update electerm from 1.17.3 to 1.17.15
    
    * electerm: add auto_updates

--- Casks/electerm.rb
+++ Casks/electerm.rb
@@ -1,18 +1,20 @@
 cask "electerm" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "1.17.3"
+  version "1.17.15"
 
   if Hardware::CPU.intel?
-    sha256 "74dd8b394970eaed784c7d4eda145b9b9e02dbae787ec9bddeb3133d72f560e3"
+    sha256 "56244952a86554096495c9105b7ed9e19523526ae66f03670a019e17a1b88087"
   else
-    sha256 "2fa227cfe7f449d3cdda13251a2c2432523362e95d047e63b66d74923af87342"
+    sha256 "fae3ac9cb70dc89c4caae525d77f3736ba3c4118d2f7be769db4bddb8390688a"
   end
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac-#{arch}.dmg"
   name "electerm"
   desc "Terminal/ssh/serial port/sftp client"
   homepage "https://github.com/electerm/electerm/"
+
+  auto_updates true
 
   app "electerm.app"
 
