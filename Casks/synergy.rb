e60e8f7a056c9f028c34520aa936fa02b8665ce8
Russell Jancewicz <russell.jancewicz@gmail.com>
2021-11-12 18:43:26 -0500

    add support for synergy on apple m1 (#114239)
    * add support for synergy on apple m1
    
    * Update synergy.rb
    
    * Update synergy.rb
    
    Co-authored-by: Russell Jancewicz <rjancewicz@MacBook-Pro.local>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/synergy.rb
+++ Casks/synergy.rb
@@ -1,8 +1,15 @@
 cask "synergy" do
-  version "1.14.1,37bbd3c0"
-  sha256 "fa3628e5956bb043395828449e7073c2ac561bbd6d50b933bc08e38e68a0e596"
+  arch = Hardware::CPU.intel? ? "x86-64" : "arm64"
 
-  url "https://binaries.symless.com/synergy/v#{version.before_comma.major}-core-standard/#{version.before_comma}-stable.#{version.after_comma}/synergy_#{version.before_comma}-stable.#{version.after_comma}_macos_x86-64.dmg"
+  version "1.14.1,37bbd3c0"
+
+  if Hardware::CPU.intel?
+    sha256 "fa3628e5956bb043395828449e7073c2ac561bbd6d50b933bc08e38e68a0e596"
+  else
+    sha256 "2b3358b8f94de63a74d1b29ea8a70f3e06b110b521a909403ec908ead51a8252"
+  end
+
+  url "https://binaries.symless.com/synergy/v#{version.before_comma.major}-core-standard/#{version.before_comma}-stable.#{version.after_comma}/synergy_#{version.before_comma}-stable.#{version.after_comma}_macos_#{arch}.dmg"
   name "Synergy"
   desc "Keyboard and mouse sharing tool - open-source core"
   homepage "https://symless.com/synergy"
