23dd74c0d7f5a843c2e3f57d31040f43e57a4c80
neutric <5984479+neutric@users.noreply.github.com>
2021-11-12 09:28:11 +0100

    Update enfusegui from 2.1.3 to 3.1.2

--- Casks/enfusegui.rb
+++ Casks/enfusegui.rb
@@ -1,10 +1,10 @@
 cask "enfusegui" do
-  version "2.1.3"
-  sha256 "da762b7be32b9d264ffc121b43fae4b039dc797994be50e31f448bccf0d4b908"
+  version "3.1.2"
+  sha256 "4a96ab245d8fafaea90d5f3e86df4d7094928ec2d920ae25c9b86b317fc53f8d"
 
-  url "http://software.bergmark.com/enfusegui/files/2.1/EnfuseGUI-#{version}.dmg"
+  url "https://swipeware.com/apps/enfusegui/v#{version.major}/EnfuseGUI-#{version}.dmg"
   name "EnfuseGUI"
-  homepage "http://software.bergmark.com/enfusegui/Main.html"
+  homepage "https://swipeware.com/applications/enfusegui/"
 
   app "EnfuseGUI.app"
 end
