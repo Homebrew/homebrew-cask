18353176020b577fb0dbf0ef12153a8b200154d9
neutric <5984479+neutric@users.noreply.github.com>
2021-11-13 01:57:27 +0100

    freeyourmusic: update livecheck url

--- Casks/freeyourmusic.rb
+++ Casks/freeyourmusic.rb
@@ -9,7 +9,7 @@ cask "freeyourmusic" do
   homepage "https://freeyourmusic.com/"
 
   livecheck do
-    url "https://stamp-live.s3.amazonaws.com/latest-mac.yml"
+    url "https://s3.nl-ams.scw.cloud/fym-app-production/latest-mac.yml"
     strategy :electron_builder
   end
 
