0e0f6f890964d9f75a3151afa742c5ab47f6593d
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-12 17:10:48 -0800

    feed-the-beast: replace dash in version string with comma

--- Casks/feed-the-beast.rb
+++ Casks/feed-the-beast.rb
@@ -1,8 +1,8 @@
 cask "feed-the-beast" do
-  version "202111051727-fc8797cf31"
+  version "202111051727,fc8797cf31"
   sha256 "f169e80c3bf6289ca5f810ba44b4d0fe92720150eb8b301ab631078b01a4b205"
 
-  url "https://apps.modpacks.ch/FTBApp/release/#{version}-release/FTBA_macos_#{version}-release.dmg",
+  url "https://apps.modpacks.ch/FTBApp/release/#{version.before_comma}-#{version.after_comma}-release/FTBA_macos_#{version.before_comma}-#{version.after_comma}-release.dmg",
       verified: "apps.modpacks.ch/FTBApp/"
   name "Feed the Beast"
   desc "Minecraft mod downloader and manager"
@@ -10,7 +10,10 @@ cask "feed-the-beast" do
 
   livecheck do
     url "https://www.feed-the-beast.com/app_release.xml"
-    regex(/FTBA[._-]macos[._-](\d+-\h+)[._-]release\.dmg/i)
+    regex(/FTBA[._-]macos[._-](\d+)[._-](\h+)[._-]release\.dmg/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
+    end
   end
 
   app "FTBApp.app"
