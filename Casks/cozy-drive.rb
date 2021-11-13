cd46139b55e26eb5b30116a311d736360d774cab
Porkepix <porkepix@gmail.com>
2021-11-13 01:59:43 +0100

    Update cozy-drive from 3.30.1 to 3.31.0

--- Casks/cozy-drive.rb
+++ Casks/cozy-drive.rb
@@ -1,6 +1,6 @@
 cask "cozy-drive" do
-  version "3.30.1"
-  sha256 "5a48cb95dff4bac04f5ab904dd08cc85c0d8b7c49ef5d9d09a97338b6695eebd"
+  version "3.31.0"
+  sha256 "ddba5b4ed3679c167c80cdfae6f0c7e2876549fd8f8d35bfde84314ea983d1d1"
 
   url "https://github.com/cozy-labs/cozy-desktop/releases/download/v#{version}/Cozy-Drive-#{version}.dmg",
       verified: "github.com/cozy-labs/cozy-desktop"
