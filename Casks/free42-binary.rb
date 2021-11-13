54ab0ec944185abe11ea26ebb3e721e1d74636d9
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-13 13:40:20 +0800

    Update free42-binary.rb

--- Casks/free42-binary.rb
+++ Casks/free42-binary.rb
@@ -16,7 +16,7 @@ cask "free42-binary" do
 
   zap trash: [
     "~/Library/Application Support/Free42",
-    "~/Library/Preferences/com.thomasokken.Free42.plist",
     "~/Library/Caches/com.thomasokken.Free42",
+    "~/Library/Preferences/com.thomasokken.Free42.plist",
   ]
 end
