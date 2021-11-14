ec9d177f3fb38da679ca8735b8ee17e01cd3fe16
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-15 06:08:45 +0800

    Update kdocs.rb

--- Casks/kdocs.rb
+++ Casks/kdocs.rb
@@ -11,7 +11,7 @@ cask "kdocs" do
   livecheck do
     url "https://www.kdocs.cn/kd/api/configure/list?idList=appOfficial"
     strategy :page_match do |page|
-      match = page.match(/kdocs[._-](\d+(?:\.\d+)*)[._-]v?(\d+(?:\.\d+)*)\.dmg/i)
+      match = page.match(/kdocs[._-](\d+(?:\.\d+)+)[._-]v?(\d+(?:\.\d+)*)\.dmg/i)
       next if match.blank?
 
       "#{match[2]},#{match[1]}"
