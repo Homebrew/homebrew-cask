13fec9d03d5cd27766de4d6a14c896c99128891f
neutric <5984479+neutric@users.noreply.github.com>
2021-11-14 01:09:35 +0100

    Update geany from 1.38 to 1.38,3

--- Casks/geany.rb
+++ Casks/geany.rb
@@ -1,8 +1,8 @@
 cask "geany" do
-  version "1.38"
-  sha256 "8467c87377672e271dab0144cb96f0c4b46b48ce6d5df5223bb39b5a32350f84"
+  version "1.38,3"
+  sha256 "eb2dfa9ff71af5214f0bdb6dfbf487a212a1f95a16ab035c5d46a5b024a48c97"
 
-  url "https://download.geany.org/geany-#{version}_osx.dmg"
+  url "https://download.geany.org/geany-#{version.before_comma}_osx#{version.after_comma&.prepend("-")}.dmg"
   name "Geany"
   desc "Fast and lightweight IDE"
   homepage "https://www.geany.org/"
