65ad5ba2b79aad29d9c6118317bae5130aa11b57
neutric <5984479+neutric@users.noreply.github.com>
2021-11-14 01:34:23 +0100

    Update glyphs from 3.0.4,3099 to 3.0.4,3100

--- Casks/glyphs.rb
+++ Casks/glyphs.rb
@@ -1,6 +1,6 @@
 cask "glyphs" do
-  version "3.0.4,3099"
-  sha256 "2e22fe9d1b087f5f61bbfce5a77e09518024164701f0fc841f9dcc2a3eb9b4f7"
+  version "3.0.4,3100"
+  sha256 "ea029ba5e3fb1051aaa3a96903ab70e3fdd785f7da03063eec91ee8000af4d55"
 
   url "https://updates.glyphsapp.com/Glyphs#{version.before_comma}-#{version.after_comma}.zip"
   name "Glyphs"
