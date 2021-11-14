2bbdc064a1cea344dfd262c3961e3938304bb835
neutric <5984479+neutric@users.noreply.github.com>
2021-11-14 05:34:28 +0100

    Update geogebra from 6.0.672.0 to 6.0.674.0 (#114286)
    * Update geogebra from 6.0.672.0 to 6.0.674.0
    
    * geogebra: add missing uninstall quit item

--- Casks/geogebra.rb
+++ Casks/geogebra.rb
@@ -1,6 +1,6 @@
 cask "geogebra" do
-  version "6.0.672.0"
-  sha256 "3221cb797498b51e4ff317731b1598cf094522e57d9b76330542af1d25020d88"
+  version "6.0.674.0"
+  sha256 "1ba161c334287588ab847f54abeb53f181de2b308154165c74a2f692b1681354"
 
   url "https://download.geogebra.org/installers/#{version.major_minor}/GeoGebra-Classic-#{version.major}-MacOS-Portable-#{version.dots_to_hyphens}.zip"
   name "GeoGebra"
@@ -17,7 +17,10 @@ cask "geogebra" do
 
   app "GeoGebra Classic #{version.major}.app"
 
-  uninstall quit:       "org.geogebra.mathapps",
+  uninstall quit:       [
+    "com.apple.systemevents",
+    "org.geogebra.mathapps",
+  ],
             login_item: "GeoGebra",
             pkgutil:    "org.geogebra6.mac"
 
