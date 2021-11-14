948fd44c47c040cb2bcd2406960fd78003efd93d
Andreas Florin <33662559+Curudel@users.noreply.github.com>
2021-11-14 05:49:38 +0100

    Update microsoft-edge from 95.0.1020.44 to 95.0.1020.53 (#114277)

--- Casks/microsoft-edge.rb
+++ Casks/microsoft-edge.rb
@@ -2,12 +2,12 @@ cask "microsoft-edge" do
   folder = Hardware::CPU.intel? ? "C1297A47-86C4-4C1F-97FA-950631F94777" : "03adf619-38c6-4249-95ff-4a01c0ffc962"
   linkid = Hardware::CPU.intel? ? "2069148" : "2093504"
 
-  version "95.0.1020.44"
+  version "95.0.1020.53"
 
   if Hardware::CPU.intel?
-    sha256 "266bc98cdc10b922f4947f858c5f18d2108c175036d0ae97b4db9ef183254d2d"
+    sha256 "c9c86cc7b7d207ff7fb297962af4e32d641a10d292aa312c52b8f35db823a976"
   else
-    sha256 "42e593235489916ff678ade0637b0668a5538a1a9acec965e512490c31c30134"
+    sha256 "30b044685131e9a417ab757ce2356e421a300e8043575979dd2e69cdda6722b7"
   end
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/#{folder}/MacAutoupdate/MicrosoftEdge-#{version}.pkg",
