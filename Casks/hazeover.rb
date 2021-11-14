d0035b08ecfb6e7969cfe3d1e7af68083cc02a50
neutric <5984479+neutric@users.noreply.github.com>
2021-11-15 00:23:29 +0100

    hazeover: add legacy version

--- Casks/hazeover.rb
+++ Casks/hazeover.rb
@@ -1,16 +1,30 @@
 cask "hazeover" do
-  version "1.9.1,1089"
-  sha256 :no_check
+  if MacOS.version <= :big_sur
+    version "1.8.9,1050"
+    sha256 "de5e4a660cc30276155606b539d1ae58684115a3983d69598f1505fcad499a87"
 
-  url "https://hazeover.com/HazeOver.dmg"
+    url "https://hazeover.com/get/HazeOver_10.13.dmg"
+
+    livecheck do
+      skip "Legacy version for Big Sur and below"
+    end
+  else
+    version "1.9.1,1089"
+    sha256 :no_check
+
+    url "https://hazeover.com/HazeOver.dmg"
+
+    livecheck do
+      url "https://hazeover.com/updates.xml"
+      strategy :sparkle
+    end
+  end
+
   name "HazeOver"
   desc "Windows manager and desktop organizer"
   homepage "https://hazeover.com/"
 
-  livecheck do
-    url "https://hazeover.com/updates.xml"
-    strategy :sparkle
-  end
+  depends_on macos: ">= :high_sierra"
 
   app "HazeOver.app"
 
