ebfc7ecd9245b18143a50a17a7497689c6b3382b
neutric <5984479+neutric@users.noreply.github.com>
2021-11-14 02:10:31 +0100

    grads: add alternative name + desc

--- Casks/grads.rb
+++ Casks/grads.rb
@@ -4,6 +4,8 @@ cask "grads" do
 
   url "ftp://cola.gmu.edu/grads/#{version.major_minor}/grads-#{version}-bin-darwin17.5.tar.gz"
   name "Grid Analysis and Display System"
+  name "GrADS"
+  desc "Access, manipulate, and visualize earth science data"
   homepage "http://cola.gmu.edu/grads/grads.php"
 
   livecheck do
