5d5715af9f505c81399c78f0036d3b8ebf23aaca
neutric <5984479+neutric@users.noreply.github.com>
2021-11-14 05:33:57 +0100

    Update geomap from 3.6.12 to 3.6.14 (#114287)
    * Update geomap from 3.6.12 to 3.6.14
    
    * geomap: add desc
    
    * geomap: tweak livecheck regex

--- Casks/geomap.rb
+++ Casks/geomap.rb
@@ -1,15 +1,15 @@
 cask "geomap" do
-  version "3.6.12"
+  version "3.6.14"
   sha256 :no_check
 
   url "http://www.geomapapp.org/MapApp/GeoMapApp.dmg"
   name "GeoMapApp"
+  desc "Browse, visualize and analyze geoscience data sets"
   homepage "http://www.geomapapp.org/"
 
   livecheck do
     url :homepage
-    strategy :page_match
-    regex(/GeoMapApp\s*is.*?(\d+(?:\.\d+)*)</i)
+    regex(/GeoMapApp\s*is.*?(\d+(?:\.\d+)+)\s*</i)
   end
 
   app "GeoMapApp.app"
