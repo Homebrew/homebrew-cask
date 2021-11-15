1d136cc4943736c4bfdbfa0a1518988cffafba91
Leilei Chui <46757452+LeileiChui@users.noreply.github.com>
2021-11-15 20:47:25 +0800

    Update vnote.rb
    version 3.10.0

--- Casks/vnote.rb
+++ Casks/vnote.rb
@@ -1,6 +1,6 @@
 cask "vnote" do
-  version "3.9.0"
-  sha256 "edaab49ce7f458206424a5f02e20854c4fed06d23a791b4a8bf3a22dddc9fab5"
+  version "3.10.0"
+  sha256 "5d9a2d89f7b0d87a99c0bc27630540cbdd04400f7c5363e0641aead33fd4a0db"
 
   url "https://github.com/vnotex/vnote/releases/download/v#{version}/vnote-mac-x64_v#{version}.zip",
       verified: "github.com/vnotex/vnote/"
