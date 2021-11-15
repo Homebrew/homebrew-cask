b257d6978e7b376471d5edcc0424fbece931da78
MoonFruit <dkmoonfruit@gmail.com>
2021-11-15 16:16:33 +0800

    Update telegram-desktop from 3.2.4 to 3.2.5 (#114357)

--- Casks/telegram-desktop.rb
+++ Casks/telegram-desktop.rb
@@ -1,6 +1,6 @@
 cask "telegram-desktop" do
-  version "3.2.4"
-  sha256 "860f3d6c67dd47cba01fbd8bcbda714d7c6d821d9e9db788a9c007f605b17799"
+  version "3.2.5"
+  sha256 "8d265d4c4fdb0863a3e134f13561c7a54a357bddce691e03f4d0d4d879a65995"
 
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg",
       verified: "updates.tdesktop.com/tmac/"
