956ebbbcec26ef4291bd0ceb1c91cb448b1162b2
neutric <5984479+neutric@users.noreply.github.com>
2021-11-16 00:34:59 +0100

    Update ibm-cloud-cli from 2.0.2 to 2.2.0

--- Casks/ibm-cloud-cli.rb
+++ Casks/ibm-cloud-cli.rb
@@ -1,6 +1,6 @@
 cask "ibm-cloud-cli" do
-  version "2.0.2"
-  sha256 "d0449e3e6a71e903dda57036c3e3c8b4edea9a18af0d04decfa23fcf4cb9b79e"
+  version "2.2.0"
+  sha256 "1d7f052f0673016e7e3693c51eccaa00d14543b9502f5e24ec21193535242ad1"
 
   url "https://download.clis.cloud.ibm.com/ibm-cloud-cli/#{version}/IBM_Cloud_CLI_#{version}.pkg"
   name "IBM Cloud CLI"
