5b9e6a805364d2e2abff215900aefc5f4fbfed40
neutric <5984479+neutric@users.noreply.github.com>
2021-11-15 01:03:54 +0100

    Update hook from 3.2.1 to 3.3.1

--- Casks/hook.rb
+++ Casks/hook.rb
@@ -1,6 +1,6 @@
 cask "hook" do
-  version "3.2.1,2021.08"
-  sha256 "f5aee8d4ae6d2c1279f249b2d18656c96b91de3a2c1c5a8fe557554e1bf9aee1"
+  version "3.3.1,2021.10"
+  sha256 "30919d674f5197c33d87ce5a659375112d24e42d7f79d9b30f38f6e3abd4e1b9"
 
   url "https://hookproductivity.com/wp-content/uploads/#{version.after_comma.major}/#{version.after_comma.minor}/Hook-productivity-app-#{version.before_comma}.dmg_.zip",
       user_agent: :fake
