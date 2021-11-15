90066f29e46809a0c08c4e344c22fe6245db5f49
Elsie Hupp <9206310+elsiehupp@users.noreply.github.com>
2021-11-15 13:23:21 -0500

    Fix typos and style

--- Casks/qlmarkdown.rb
+++ Casks/qlmarkdown.rb
@@ -1,8 +1,8 @@
 cask "qlmarkdown" do
-  if MacOS.version < catalina
+  if MacOS.version < :catalina
     version "1.3.6"
     sha256 "810853c000dd5c3e18978070abb7f595ad52ddfa568fccb428d28b513d1810ab"
-  
+
     url "https://github.com/toland/qlmarkdown/releases/download/v#{version}/QLMarkdown.qlgenerator.zip"
     name "QLMarkdown"
     desc "QuickLook generator for Markdown files (macOS Mojave and earlier)"
@@ -12,15 +12,15 @@ cask "qlmarkdown" do
   else
     version "1.0.5"
     sha256 "d0d040a8cb4df32561366eefd908fbcf69c3aaf87e319681777bf18812002679"
-  
+
     url "https://github.com/sbarex/QLMarkdown/releases/download/#{version}/QLMarkdown.zip"
     name "QLMarkdown"
     desc "QuickLook generator for Markdown files (macOS Catalina and later)"
     homepage "https://github.com/sbarex/QLMarkdown"
-  
+
     auto_updates true
     depends_on macos: ">= :catalina"
-  
+
     app "QLMarkdown.app"
   end
 
