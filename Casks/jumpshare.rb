4e474a79e823f56b2a9ade2f09fdf0c41f537367
neutric <5984479+neutric@users.noreply.github.com>
2021-11-17 00:25:22 +0100

    Update jumpshare from 2.7.2 to 2.7.3

--- Casks/jumpshare.rb
+++ Casks/jumpshare.rb
@@ -1,8 +1,9 @@
 cask "jumpshare" do
-  version "2.7.2,102"
-  sha256 "9acd6e172a8edbf48b5cca4552d4a210539654517d741ad4f5ef209b37349faa"
+  version "2.7.3,103"
+  sha256 "f9f1ddb4fdaafa64a817358805952836d8bc147f7a504a0a8417e7e17b3057a1"
 
-  url "https://apps.jumpshare.com/desktop/mac/updates/Jumpshare-#{version.before_comma}.tar.bz2"
+  url "https://d21hi1or3tbtjm.cloudfront.net//desktop/mac/updates/Jumpshare-#{version.before_comma}.tar.bz2",
+      verified: "d21hi1or3tbtjm.cloudfront.net/"
   name "Jumpshare"
   desc "File sharing, screen recording, and screenshot capture app"
   homepage "https://jumpshare.com/"
