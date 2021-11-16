89030ee8a978023f631732255e42ff6e95849005
Bevan Kay <email@bevankay.me>
2021-11-16 15:44:54 +1100

    Update fs-uae-launcher from 3.1.42 to 3.1.43

--- Casks/fs-uae-launcher.rb
+++ Casks/fs-uae-launcher.rb
@@ -1,12 +1,12 @@
 cask "fs-uae-launcher" do
   arch = Hardware::CPU.intel? ? "x86-64" : "ARM64"
 
-  version "3.1.42"
+  version "3.1.43"
 
   if Hardware::CPU.intel?
-    sha256 "c84db7c5bf04b183f86fcce6828d6e85968ddc3c350aef0a0abbfe68f506756e"
+    sha256 "c810dc3cfaa022baaa2672203c230bcb76ca279995bf8bcb0a133a9a7bff3bd2"
   else
-    sha256 "a06656df30572e76623b7cad29b93eece0c7938029889574c36b65315a508ad1"
+    sha256 "aa7941947c40180a7150fb1487af318bcf4a855086cccc972f862b7c8c06803e"
   end
 
   url "https://fs-uae.net/files/FS-UAE-Launcher/Stable/#{version}/FS-UAE-Launcher_#{version}_macOS_#{arch}.tar.xz"
