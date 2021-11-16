9cb3a5490881aa14de4142f441b53a7688bb23e1
Bevan Kay <email@bevankay.me>
2021-11-16 17:00:35 +1100

    Update fig from 1.0.52,307 to 1.0.52,311 (#114446)

--- Casks/fig.rb
+++ Casks/fig.rb
@@ -1,6 +1,6 @@
 cask "fig" do
-  version "1.0.52,307"
-  sha256 "b002dae7e82d71e5c1d953e3dc63dec312a5bc2f87140c00ab3f22f4e70d16df"
+  version "1.0.52,311"
+  sha256 "817e9460c818c3b07181a1f95906256df78aeced983530d26944d74abf3dbdb7"
 
   url "https://versions.withfig.com/fig%20#{version.after_comma}.dmg",
       verified: "versions.withfig.com/"
