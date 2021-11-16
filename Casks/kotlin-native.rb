3a22911a53b900be47e40bc43ef2b19e9e2a1344
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-16 17:59:22 +0800

    Update kotlin-native.rb

--- Casks/kotlin-native.rb
+++ Casks/kotlin-native.rb
@@ -3,14 +3,14 @@ cask "kotlin-native" do
 
   version "1.6.0"
 
-  url "https://github.com/JetBrains/kotlin/releases/download/v#{version}/kotlin-native-macos-#{arch}-#{version}.tar.gz",
-      verified: "github.com/JetBrains/kotlin/"
   if Hardware::CPU.intel?
     sha256 "70957e74ff7fa491e5271e791cca9deca0bf388584ccf5ca9a86c264b059f9e7"
   else
     sha256 "1cb23a13763c97fe6f0208d3dfd7b3042118842195de9763c750757cbe7ba8ee"
   end
 
+  url "https://github.com/JetBrains/kotlin/releases/download/v#{version}/kotlin-native-macos-#{arch}-#{version}.tar.gz",
+      verified: "github.com/JetBrains/kotlin/"
   name "Kotlin Native"
   desc "LLVM backend for Kotlin"
   homepage "https://kotlinlang.org/docs/reference/native-overview.html"
