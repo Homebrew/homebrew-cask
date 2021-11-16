8a91f02f9b0777a71888ceaa935b0e275ad69cbd
Christian Kraemer <christian.kraemer@gmail.com>
2021-11-16 10:59:46 +0100

    Update qownnotes from 21.11.4 to 21.11.5 (#114453)
    Co-authored-by: Christian Kraemer <christian.kraemer@proximity.de>

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,6 +1,6 @@
 cask "qownnotes" do
-  version "21.11.4"
-  sha256 "f59d38e8d9ff6ae5b56b0ba61a790679cd52f7b5b0e79a24b3dc098751ed9e21"
+  version "21.11.5"
+  sha256 "5bcbf6279fe191b3f1e20014739928345697a8659c430a9a4e08a8244aa20b5f"
 
   url "https://github.com/pbek/QOwnNotes/releases/download/v#{version}/QOwnNotes.dmg",
       verified: "github.com/pbek/QOwnNotes/"
