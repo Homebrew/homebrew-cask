bc301322674d7ad3e5bff2a2e33a201ed3dc1e57
Bevan Kay <email@bevankay.me>
2021-11-16 14:55:54 +1100

    Update dynobase from 1.7.3 to 1.7.4

--- Casks/dynobase.rb
+++ Casks/dynobase.rb
@@ -1,14 +1,14 @@
 cask "dynobase" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  version "1.7.3"
+  version "1.7.4"
 
   url "https://github.com/Dynobase/dynobase/releases/download/#{version}/Dynobase-#{version}#{arch}.dmg",
       verified: "github.com/Dynobase/dynobase/"
   if Hardware::CPU.intel?
-    sha256 "11575efac58861f851260c87f46f298992725794b7ee25a6dcde4304e5a7687d"
+    sha256 "f559304065faf9cf203797093fc24bd7edca948e226249a0b92dde037bddf568"
   else
-    sha256 "33d15bf8242ef348c649f4de2de04fd70c1e0accf926df8b34121eb66f1812fd"
+    sha256 "091a711bbfbbe3b8537e9406ccaa1ca8895222d9ca86bb87aac1e2302c5f63d9"
   end
 
   name "Dynobase"
