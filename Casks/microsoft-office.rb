dcc1fc2a291fa566d69614fa12b25f36e75ba7b7
Bevan Kay <email@bevankay.me>
2021-11-17 09:52:28 +1100

    Update microsoft-office from 16.54.21101001 to 16.55.21111400 (#114484)

--- Casks/microsoft-office.rb
+++ Casks/microsoft-office.rb
@@ -1,6 +1,6 @@
 cask "microsoft-office" do
-  version "16.54.21101001"
-  sha256 "963962717a9f246b433426edf3da6d6e718c2880e80da71cc204dd5704d52169"
+  version "16.55.21111400"
+  sha256 "3f81906af35ee2d9cc55fa8bdcd6e752cf5cbdf5b6de0115f9dc64811cc45898"
 
   url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Office_#{version}_Installer.pkg",
       verified: "officecdnmac.microsoft.com/"
