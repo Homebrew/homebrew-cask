54550fe56e8f06f7422e14f6690018b6257ddd9e
Bevan Kay <email@bevankay.me>
2021-11-16 14:17:04 +1100

    Update codeql from 2.7.0 to 2.7.1 (#114429)

--- Casks/codeql.rb
+++ Casks/codeql.rb
@@ -1,6 +1,6 @@
 cask "codeql" do
-  version "2.7.0"
-  sha256 "ecfee5d2b1ee1ee0c8ceeadde35ab6245c4f7919cc19778381ec8cf9a5b187f8"
+  version "2.7.1"
+  sha256 "c9d8b025fc1dc8bd991435218708b8b007a325e3f2bd51af8e45039ae01b7f10"
 
   url "https://github.com/github/codeql-cli-binaries/releases/download/v#{version}/codeql-osx64.zip"
   name "CodeQL"
