7ea738a0f49532e0fa10baea49c70c94361f81dc
Bevan Kay <email@bevankay.me>
2021-11-16 13:24:44 +1100

    Update blockbench from 4.0.3 to 4.0.4

--- Casks/blockbench.rb
+++ Casks/blockbench.rb
@@ -1,12 +1,12 @@
 cask "blockbench" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "4.0.3"
+  version "4.0.4"
 
   if Hardware::CPU.intel?
-    sha256 "6b33246780c78b3ecff79bdea89471afc18bf6da2e07b9d21d3ffb3401bcb699"
+    sha256 "b1ee2d8681b1a0cd975d72d924b72d36c0560c4a0774fbdca218f8b0c6a70475"
   else
-    sha256 "f8b23d2e8ac76719516729ee700e994c51f1926ba0ce0e13480cec2df2eb44d3"
+    sha256 "b14e5b529d5f2c7fbfb1c2651d7d5d2f8e8106eedc573d3d7987a2d80d1838bc"
   end
 
   url "https://github.com/JannisX11/blockbench/releases/download/v#{version}/Blockbench_#{arch}_#{version}.dmg",
