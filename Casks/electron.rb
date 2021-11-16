2e364b0670dfaaeb2da09f5a6b6998a251c7f923
Bevan Kay <email@bevankay.me>
2021-11-16 15:16:43 +1100

    Update electron from 15.3.1 to 16.0.0

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,12 +1,12 @@
 cask "electron" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "15.3.1"
+  version "16.0.0"
 
   if Hardware::CPU.intel?
-    sha256 "a91b089b5dc5b1279966511344b805ec84869b6cd60af44f800b363bba25b915"
+    sha256 "f95847ea721cbf8632a72bec504137554aa262d111aaa5e2e30a74df66ff6c97"
   else
-    sha256 "89fafef20426c0a3a2326172d222d29fc5d24d64c8910b7abddf61bd2c6a6700"
+    sha256 "0c71b070cf6bcfa7dc4dad65ee14af926d5dda73e4a2f96e68cff844df8aba1d"
   end
 
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-#{arch}.zip",
