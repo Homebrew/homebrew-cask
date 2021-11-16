aaea7e9233ebc5b969dffc96c86134eee452428a
Bevan Kay <email@bevankay.me>
2021-11-17 09:35:40 +1100

    Revert "add github_latest livecheck strategy"
    This reverts commit fa030723ac60f4e176fa3f5e9a2ddb272e1c2c6f.

--- Casks/trilium-notes.rb
+++ Casks/trilium-notes.rb
@@ -1,16 +1,11 @@
 cask "trilium-notes" do
-  version "0.48.6"
-  sha256 "65285c36c0627335447c7507657b2615891457c0ff26d2f753d68863071cf3f7"
+  version "0.48.5"
+  sha256 "d5ce56581a167b900de9db228b14afa3fc3a8ce239c5b08dbd8f3505e9f427d2"
 
   url "https://github.com/zadam/trilium/releases/download/v#{version}/trilium-mac-x64-#{version}.zip"
   name "Trilium Notes"
   desc "Personal knowledge base"
   homepage "https://github.com/zadam/trilium"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
 
   app "trilium-mac-x64/Trilium Notes.app"
 end
