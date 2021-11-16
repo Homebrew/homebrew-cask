dd1f11ac6f5b0f17e9bf77d35c84ee50d5559fc0
zuisong <com.me@foxmail.com>
2021-11-16 17:44:15 +0800

    Update youku from 1.9.5.3.111210 to 1.9.5.4.11151

--- Casks/youku.rb
+++ Casks/youku.rb
@@ -1,6 +1,6 @@
 cask "youku" do
-  version "1.9.5.3.111210"
-  sha256 "63ff1a2ba0e2db79b077701ba19b16aea79fdb90b99c293cedf7244b17d7b90e"
+  version "1.9.5.4.11151"
+  sha256 "0d9bdbe8158deeb710fcaddda9895aee779cb8d9d0ed8dd62b1f9ab7f18f2158"
 
   url "https://pcclient.download.youku.com/ikumac/youkumac_#{version}.dmg"
   name "Youku"
