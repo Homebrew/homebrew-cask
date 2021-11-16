a4fa46167e84c2c056ef865d4eaa478cd2ac8428
Bevan Kay <email@bevankay.me>
2021-11-16 13:43:19 +1100

    Update bria from 6.4.2 to 6.5.1 (#114425)

--- Casks/bria.rb
+++ Casks/bria.rb
@@ -1,6 +1,6 @@
 cask "bria" do
-  version "6.4.2,107895"
-  sha256 "d9208cbc89077d757830b5ad34b9a5d2e710fbcb925f365a031bd735e52dba8e"
+  version "6.5.1,108815"
+  sha256 "84c1a1cea97ce406fdb7d5b6ce0fc98c76d69a5ed4f707fa08f15b7306d5fcf5"
 
   url "https://counterpath.s3.amazonaws.com/downloads/Bria_#{version.before_comma}_#{version.after_comma}.dmg",
       verified: "counterpath.s3.amazonaws.com/"
