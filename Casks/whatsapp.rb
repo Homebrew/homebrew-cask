9f6e8295d9054d9edfb67b246d03b8d73b2831cb
Bevan Kay <email@bevankay.me>
2021-11-18 09:25:12 +1100

    Update whatsapp from 2.2142.12 to 2.2144.11

--- Casks/whatsapp.rb
+++ Casks/whatsapp.rb
@@ -1,6 +1,6 @@
 cask "whatsapp" do
-  version "2.2142.12"
-  sha256 "ab28ed4ac8deb360a50d11a3c73d6961abfbd63816bd5086d8bb2783bc2da890"
+  version "2.2144.11"
+  sha256 "f9c85253e2123989e4c3cd979eb31ab721c8ec2fb26eb980f133606e7e4fff6f"
 
   url "https://web.whatsapp.com/desktop/mac/files/release-#{version}.zip"
   name "WhatsApp"
