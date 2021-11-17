e8a7ed6304508c1f2c7dca8dd0509409b6b102dc
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-17 04:02:08 -0800

    spotify: minimize version string (#114328)
    * spotify: minimize version string
    
    * Update Casks/spotify.rb
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/spotify.rb
+++ Casks/spotify.rb
@@ -2,9 +2,9 @@ cask "spotify" do
   arch = Hardware::CPU.intel? ? "" : "ARM64"
 
   if Hardware::CPU.intel?
-    version "1.1.72.439.gc253025e,1.1.72.439.gc253025e-34"
+    version "1.1.72.439,c253025e,34"
   else
-    version "1.1.69.612.gb7409abc,1.1.69.612.gb7409abc-13"
+    version "1.1.69.612,b7409abc,13"
   end
 
   sha256 :no_check
@@ -17,7 +17,12 @@ cask "spotify" do
 
   livecheck do
     url :url
-    strategy :extract_plist
+    strategy :extract_plist do |items|
+      match = items["com.spotify.client"].version.match(/^(\d+(?:\.\d+)+)[._-]g(\h+)[._-](\d+)$/i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]},#{match[3]}"
+    end
   end
 
   auto_updates true
