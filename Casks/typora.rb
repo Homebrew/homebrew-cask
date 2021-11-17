96635d335cdad455bb23a80f6769987cb89d742e
Reno <25192197+singularitti@users.noreply.github.com>
2021-11-17 17:19:01 -0500

    Update typora from 0.11.16 to 0.11.18 (#114532)

--- Casks/typora.rb
+++ Casks/typora.rb
@@ -1,6 +1,6 @@
 cask "typora" do
-  version "0.11.16"
-  sha256 "ae0dd0c1defc47193f3e9fa2e60e5e6ff138a944ca942c2cf1457a62d722434c"
+  version "0.11.18"
+  sha256 "cc8c9b07f4584f81b214d4a2cbc270cb1171d7262f0f7ffdd466f8b74b2da38e"
 
   url "https://www.typora.io/download/Typora-#{version}.dmg"
   name "Typora"
