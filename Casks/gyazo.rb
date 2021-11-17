2bc952c2e9ba1039352771caf902ff0d9d09b9ce
Bevan Kay <email@bevankay.me>
2021-11-17 11:13:37 +1100

    Update gyazo.rb

--- Casks/gyazo.rb
+++ Casks/gyazo.rb
@@ -4,6 +4,7 @@ cask "gyazo" do
 
   url "https://files.gyazo.com/setup/Gyazo-#{version}.dmg"
   name "Nota Gyazo GIF"
+  desc "Screenshot and screen recording tool"
   homepage "https://gyazo.com/"
 
   livecheck do
