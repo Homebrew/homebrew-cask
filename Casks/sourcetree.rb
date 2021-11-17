f593f796295b644657879304b66c80b8a8fea8cc
Slobodan Marković <sloba.markovic@gmail.com>
2021-11-17 19:06:16 +0100

    Update sourcetree from 4.1.3 to 4.1.4

--- Casks/sourcetree.rb
+++ Casks/sourcetree.rb
@@ -12,8 +12,8 @@ cask "sourcetree" do
     url "https://product-downloads.atlassian.com/software/sourcetree/ga/Sourcetree_#{version.before_comma}_#{version.after_comma}.zip",
         verified: "atlassian.com/software/sourcetree/"
   else
-    version "4.1.3,239"
-    sha256 "a5206c04579fe6e68a35e2bb8e73aceb6a9106800ecf67047e10b3f5c93e3823"
+    version "4.1.4,240"
+    sha256 "f88725f6980e70a4dbbb3b08010d994480b8d91b1768e8033748076d7fe04ac1"
 
     url "https://product-downloads.atlassian.com/software/sourcetree/ga/Sourcetree_#{version.before_comma}_#{version.after_comma}.zip",
         verified: "atlassian.com/software/sourcetree/"
