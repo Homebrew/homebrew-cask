6be4a541654748e0df54c14d64680789087b0cad
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-17 03:51:45 -0800

    Update surge from 4.2.3 to 4.2.4 (#114321)
    * Update surge from 4.2.3 to 4.2.4
    
    * Better livecheck

--- Casks/surge.rb
+++ Casks/surge.rb
@@ -1,8 +1,8 @@
 cask "surge" do
-  version "4.2.3-1357-0803594c82248360a95722089650f7f7"
-  sha256 "af4fa9222c62415772888d15e0dc6e9a980a686b50eb4ffec6220c2837b5d7dc"
+  version "4.2.4,1399,4f3c53abfe45fce5646d84af11e589c4"
+  sha256 "c19ceb77c5a52afc5b1f3616bf3076e93a74931183457fc628b1210578df04ac"
 
-  url "https://dl.nssurge.com/mac/v#{version.major}/Surge-#{version}.zip"
+  url "https://dl.nssurge.com/mac/v#{version.major}/Surge-#{version.tr(",", "-")}.zip"
   name "Surge"
   desc "Network toolbox"
   homepage "https://nssurge.com/"
@@ -10,7 +10,10 @@ cask "surge" do
   livecheck do
     url "https://www.nssurge.com/mac/v#{version.major}/appcast-signed.xml"
     strategy :sparkle do |item|
-      item.url[/-(\d+(?:\.\d+)*-\d+-[0-9a-f]+)\.zip/i, 1]
+      match = item.url.match(/[._-](\d+(?:\.\d+)+)[._-](\d+)[._-](\h+)\.zip/i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]},#{match[3]}"
     end
   end
 
