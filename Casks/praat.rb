0373405004e80232b01c760ae6eaa58c54d83cd8
J-rg <j_rg@live.de>
2021-11-18 03:07:34 +0100

    Update praat from 6.1.56 to 6.2.01

--- Casks/praat.rb
+++ Casks/praat.rb
@@ -1,6 +1,6 @@
 cask "praat" do
-  version "6.1.56"
-  sha256 "7449914af507357c4271b67b32574e7198ff3941df8ec30cb9961e0dfc30767c"
+  version "6.2.01"
+  sha256 "ba3ea13b41828facfc9513d7d19273135d9e55f6ad333bf8d148bcc9a66766d4"
 
   url "https://github.com/praat/praat/releases/download/v#{version}/praat#{version.no_dots}_mac.dmg",
       verified: "github.com/praat/praat/"
