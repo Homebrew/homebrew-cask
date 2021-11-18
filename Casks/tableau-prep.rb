b5b59d947c4b50e2343781561533eccfade59403
Anton Möller <anton.myller@gmail.com>
2021-11-18 17:21:47 +0100

    Update tableau-prep from 2021.3.1 to 2021.3.2 (#114585)
    * Update tableau-prep from 2021.3.1 to 2021.3.2
    
    * Update tableau-prep.rb
    
    Co-authored-by: Anton Möller <anton@esportal.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/tableau-prep.rb
+++ Casks/tableau-prep.rb
@@ -1,6 +1,6 @@
 cask "tableau-prep" do
-  version "2021.3.1"
-  sha256 "a3d96065b916aa429c7e1283d0082f71592a887f84931ceca426ff2783bb1a3f"
+  version "2021.3.2"
+  sha256 "cbce3818b3ebf7e214ea2483a29e1e75af3da20d225a6819ec10f5f231d3e35f"
 
   url "https://downloads.tableau.com/esdalt/tableau_prep/#{version}/TableauPrep-#{version.dots_to_hyphens}.dmg"
   name "Tableau Prep"
@@ -11,7 +11,7 @@ cask "tableau-prep" do
   livecheck do
     url "https://www.tableau.com/downloads/prep/mac"
     strategy :header_match do |headers|
-      headers["location"][/-(\d+-\d+-\d+)\.dmg/i, 1].tr("-", ".")
+      headers["location"][/TableauPrep-(\d+(?:-\d+)+)\.dmg/i, 1].tr("-", ".")
     end
   end
 
@@ -21,14 +21,14 @@ cask "tableau-prep" do
 
   uninstall pkgutil: [
     "com.amazon.redshiftodbc",
-    "simba.sparkodbc",
     "com.simba.sparkodbc",
     "com.simba.sqlserverodbc",
-    "com.tableausoftware.Maestro.app",
     "com.tableausoftware.desktopShortcut",
-    "com.tableausoftware.telemetry",
     "com.tableausoftware.FLEXNet.11.*",
+    "com.tableausoftware.Maestro.app",
     "com.tableausoftware.oracle",
     "com.tableausoftware.postgresql",
+    "com.tableausoftware.telemetry",
+    "simba.sparkodbc",
   ]
 end
