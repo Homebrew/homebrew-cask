256eda93c0225c4932801a3653e7cc03c50f7b9d
loop99 <loop99@infoseek.jp>
2021-11-19 08:47:17 +0900

    Add Disk Diet.app v5.5 (#114584)
    * Add Disk Diet.app v5.5
    
    * Add a zap stanza

---
+++ Casks/disk-diet.rb
@@ -0,0 +1,24 @@
+cask "disk-diet" do
+  version "5.5,1474"
+  sha256 "a7f1a0c2c3b7186dc9844a60e5dded90aca4e758f7502c174452bf90cabaf67e"
+
+  url "https://www.tunabellysoftware.com/resources/Disk%20Diet%20#{version.before_comma}.dmg"
+  name "Disk Diet"
+  desc "Free up disk space"
+  homepage "https://www.tunabellysoftware.com/disk_diet/"
+
+  livecheck do
+    url "https://www.tunabellysoftware.com/resources/sparkle/diskdiet.xml"
+    strategy :sparkle
+  end
+
+  app "Disk Diet.app"
+
+  zap trash: [
+    "/Library/LaunchDaemons/com.tunabellysoftware.DiskDietHelper.plist",
+    "/Library/PrivilegedHelperTools/com.tunabellysoftware.DiskDietHelper",
+    "~/Library/Application Support/Disk Diet",
+    "~/Library/Caches/com.tunabellysoftware.diskdiet",
+    "~/Library/Preferences/com.tunabellysoftware.diskdiet.plist",
+  ]
+end
