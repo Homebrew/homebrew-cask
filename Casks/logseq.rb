c59885eee4cbcd113b871a2e1c1716ef5c36f302
EINDEX <eindex.me@outlook.com>
2021-11-18 03:17:44 +0000

    Update logseq from 0.4.6 to 0.4.7

--- Casks/logseq.rb
+++ Casks/logseq.rb
@@ -1,12 +1,12 @@
 cask "logseq" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "0.4.6"
+  version "0.4.7"
 
   if Hardware::CPU.intel?
-    sha256 "08d37337fe52bbdfa3d34a4fbd9034bb45712cfcbfb9c7d93310a05db68aadf2"
+    sha256 "5be35c798aec7d772d8bdbcd0f1531c082dc8573cf306fba94123eb3bbf3c3ad"
   else
-    sha256 "fd0b5ded124f417e9d5813478ce39408212291747d4279b6b7867c6c8c64560f"
+    sha256 "b81b9dd9bb48a9d572a2e7f9f2589bf1189d9ca72ab206d770a6bd0731ad6611"
   end
 
   url "https://github.com/logseq/logseq/releases/download/#{version}/logseq-darwin-#{arch}-#{version}.dmg"
