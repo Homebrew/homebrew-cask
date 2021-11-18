5be658aa9c719aaa31943b6755b9deb66e9fcc75
MoonFruit <dkmoonfruit@gmail.com>
2021-11-18 14:45:16 +0800

    Update uu-booster from 2.6.4 to 2.6.5

--- Casks/uu-booster.rb
+++ Casks/uu-booster.rb
@@ -1,6 +1,6 @@
 cask "uu-booster" do
-  version "2.6.4,228"
-  sha256 "836ef54e35f412288c90e56bf8ac1ef58585002d37dccf90fc1185b4722ae0d0"
+  version "2.6.5,233"
+  sha256 "8fae51e2584a704e775c0f33e292499c4a6126f21646bd5f3b4c5aca3a539b32"
 
   url "https://uu.gdl.netease.com/UU-macOS-#{version.before_comma}(#{version.after_comma}).dmg",
       verified: "uu.gdl.netease.com/"
