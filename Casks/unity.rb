b1542fe962a30a8b344801f40999dffa4f3bbd0d
neutric <5984479+neutric@users.noreply.github.com>
2021-11-18 17:15:53 +0100

    Update unity from 2021.2.2f1 to 2021.2.3f1

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -1,6 +1,6 @@
 cask "unity" do
-  version "2021.2.2f1,5e2b1e92c7f8"
-  sha256 "3673cdeafdab79a26591f880a02efdc4ef8f725ab10369c8c8c4830e94fd8460"
+  version "2021.2.3f1,32358a8527b4"
+  sha256 "2cffe05bd6dc7e8ca066c870308aabda1ef33410287f8267d28986dfce44c1d2"
 
   url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorInstaller/Unity-#{version.before_comma}.pkg",
       verified: "download.unity3d.com/download_unity/"
@@ -11,7 +11,7 @@ cask "unity" do
   livecheck do
     url "https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json"
     strategy :page_match do |page|
-      page.scan(%r{/download_unity/(\h+)/MacEditorInstaller/Unity-(\d+(?:\.\d+)*[a-z]*\d*)\.pkg}i).map do |match|
+      page.scan(%r{/download_unity/(\h+)/MacEditorInstaller/Unity-(\d+(?:\.\d+)+[a-z]*\d*)\.pkg}i).map do |match|
         "#{match[1]},#{match[0]}"
       end
     end
