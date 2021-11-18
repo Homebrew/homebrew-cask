6b29838bedaf7fff4d9c6932f029eb7c67225284
raycastbot <68011578+raycastbot@users.noreply.github.com>
2021-11-19 00:57:22 +0100

    Update raycast from 1.25.4 to 1.25.5 (#114587)
    * Update raycast from 1.25.4 to 1.25.5
    
    * Update raycast.rb
    
    Co-authored-by: runner <runner@Mac-1637252538056.local>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/raycast.rb
+++ Casks/raycast.rb
@@ -1,5 +1,5 @@
 cask "raycast" do
-  version "1.25.4"
+  version "1.25.5"
   sha256 :no_check
 
   url "https://api.raycast.app/v2/download"
@@ -10,7 +10,7 @@ cask "raycast" do
   livecheck do
     url :url
     strategy :header_match
-    regex(/Raycast_v?(\d+(?:\.\d+)*)_universal\.dmg/i)
+    regex(/Raycast_v?(\d+(?:\.\d+)+)_universal\.dmg/i)
   end
 
   auto_updates true
