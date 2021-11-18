5db586f7aeb258bc5eed1e0d36e72d62b23c5acd
neutric <5984479+neutric@users.noreply.github.com>
2021-11-19 00:55:19 +0100

    Update lark from 4.6.6 to 4.10.14 (#114601)
    * Update lark from 4.6.6 to 4.10.14
    
    * lark: tweak livecheck regex
    
    * lark: update arm64 branch

--- Casks/lark.rb
+++ Casks/lark.rb
@@ -2,11 +2,11 @@ cask "lark" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
   if Hardware::CPU.intel?
-    version "4.6.6,55e44a"
-    sha256 "d01a443e8e48076aa3bf683286e9c2082b24bf95a439894f9f7c95d2f9cfd960"
+    version "4.10.14,a89957"
+    sha256 "1df03dc07768e8678cb443aa8aff2b61509b2fdeb9b842c0815d1e01d02bcf17"
   else
-    version "4.6.6,2cc004"
-    sha256 "0f1788c0695d6b2901cef1a9e4337a8cae4ae46f6acebd10eef9c10543e87654"
+    version "4.10.14,40a957"
+    sha256 "fb8f06d56ac036a40f7bba037e01f4428fe9a54ae9d6540d0c4639c0a4213dc4"
   end
 
   url "https://sf16-va.larksuitecdn.com/obj/lark-artifact-storage/#{version.after_comma}/Lark-darwin_#{arch}-#{version.before_comma}-signed.dmg",
@@ -18,7 +18,7 @@ cask "lark" do
   livecheck do
     url "https://www.larksuite.com/api/downloads"
     strategy :page_match do |page|
-      match = page.match(%r{/lark-artifact-storage/(\w+)/Lark-darwin_#{arch}-(\d+(?:\.\d+)*)-signed\.dmg}i)
+      match = page.match(%r{/lark-artifact-storage/(\h+)/Lark-darwin_#{arch}[._-]v?(\d+(?:\.\d+)+)-signed\.dmg}i)
       next if match.blank?
 
       "#{match[2]},#{match[1]}"
