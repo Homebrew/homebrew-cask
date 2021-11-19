0e4d6601e861223b1eaddd7b6eb6cd3bf8f03e42
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-19 08:17:28 +0800

    Update webex-meetings.rb

--- Casks/webex-meetings.rb
+++ Casks/webex-meetings.rb
@@ -2,9 +2,20 @@ cask "webex-meetings" do
   version :latest
   sha256 :no_check
 
-  url "https://akamaicdn.webex.com/client/webexapp.dmg"
+  if Hardware::CPU.intel?
+    url "https://akamaicdn.webex.com/client/webexapp.dmg"
+  else
+    url "https://akamaicdn.webex.com/client/Cisco_Webex_Meetings.pkg"
+  end
+
   name "Webex Meetings"
+  desc "Video communication and virtual meeting platform"
   homepage "https://www.webex.com/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   pkg "Cisco_Webex_Meetings.pkg"
 
