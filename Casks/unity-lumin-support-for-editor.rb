09ff133b5f4854d7649bb207ddf9d9a4b512464a
neutric <5984479+neutric@users.noreply.github.com>
2021-11-19 01:16:00 +0100

    Update unity-lumin-support-for-editor from 2021.2.2f1 to 2021.2.3f1

--- Casks/unity-lumin-support-for-editor.rb
+++ Casks/unity-lumin-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask "unity-lumin-support-for-editor" do
-  version "2021.2.2f1,5e2b1e92c7f8"
-  sha256 "f6bb4f7621d513c6cedcf1bfe03ac11638fb963b4d4653b84f772863860027d8"
+  version "2021.2.3f1,32358a8527b4"
+  sha256 "bea4d5857003d3fcd8f5f11c4105c5ee94be8b90677afee6df6abb7aeaf7d262"
 
   url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Lumin-Support-for-Editor-#{version.before_comma}.pkg",
       verified: "download.unity3d.com/download_unity/"
@@ -13,7 +13,7 @@ cask "unity-lumin-support-for-editor" do
     strategy :page_match do |page|
       page.scan(%r{
         /download_unity/(\h+)/MacEditorTargetInstaller
-        /UnitySetup-Lumin-Support-for-Editor-(\d+(?:\.\d+)*[a-z]*\d*)\.pkg
+        /UnitySetup-Lumin-Support-for-Editor-(\d+(?:\.\d+)+[a-z]*\d*)\.pkg
       }ix).map do |match|
         "#{match[1]},#{match[0]}"
       end
