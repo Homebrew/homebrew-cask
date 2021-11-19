7651a83700bb5e73ed2440efb3a67205e14ac00c
neutric <5984479+neutric@users.noreply.github.com>
2021-11-19 01:22:36 +0100

    Update unity-windows-support-for-editor from 2021.2.2f1 to 2021.2.3f1

--- Casks/unity-windows-support-for-editor.rb
+++ Casks/unity-windows-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask "unity-windows-support-for-editor" do
-  version "2021.2.2f1,5e2b1e92c7f8"
-  sha256 "d2e2d2b5a0ea47279228b2dbddc6205ad695e5815b522a025f9cabcdcac7b3b0"
+  version "2021.2.3f1,32358a8527b4"
+  sha256 "c8759e325e761c2c81a9d8e3322b313872af6e9e91b656c5fc1b33cfeb2c36db"
 
   url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-#{version.before_comma}.pkg",
       verified: "download.unity3d.com/download_unity/"
@@ -13,7 +13,7 @@ cask "unity-windows-support-for-editor" do
     strategy :page_match do |page|
       page.scan(%r{
         /download_unity/(\h+)/MacEditorTargetInstaller
-        /UnitySetup-Windows-Mono-Support-for-Editor-(\d+(?:\.\d+)*[a-z]*\d*)\.pkg
+        /UnitySetup-Windows-Mono-Support-for-Editor-(\d+(?:\.\d+)+[a-z]*\d*)\.pkg
       }ix).map do |match|
         "#{match[1]},#{match[0]}"
       end
