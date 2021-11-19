fa82e4b5705aac84d2fe091befa93a11ac6229a3
neutric <5984479+neutric@users.noreply.github.com>
2021-11-19 01:25:25 +0100

    Update unity-linux-support-for-editor from 2021.2.2f1 to 2021.2.3f1 (#114610)

--- Casks/unity-linux-support-for-editor.rb
+++ Casks/unity-linux-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask "unity-linux-support-for-editor" do
-  version "2021.2.2f1,5e2b1e92c7f8"
-  sha256 "f398bdd1c8281fdc7ed44a01760f386c0171196935bea56176640c4cb6f375ba"
+  version "2021.2.3f1,32358a8527b4"
+  sha256 "a39d56430ed30e352d698dbca0d0b1ab97878f054812d0eb545b8829f975295d"
 
   url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Linux-Mono-Support-for-Editor-#{version.before_comma}.pkg",
       verified: "download.unity3d.com/download_unity/"
@@ -13,7 +13,7 @@ cask "unity-linux-support-for-editor" do
     strategy :page_match do |page|
       page.scan(%r{
         /download_unity/(\h+)/MacEditorTargetInstaller
-        /UnitySetup-Linux-Mono-Support-for-Editor-(\d+(?:\.\d+)*[a-z]*\d*)\.pkg
+        /UnitySetup-Linux-Mono-Support-for-Editor-(\d+(?:\.\d+)+[a-z]*\d*)\.pkg
       }ix).map do |match|
         "#{match[1]},#{match[0]}"
       end
