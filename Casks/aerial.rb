c152027756fec802d3d55711e68c33619fed1ab7
Alain Silberstein <emerek@web.de>
2021-11-20 13:15:57 +0100

    Update aerial from 2.3.3 to 2.3.3

--- Casks/aerial.rb
+++ Casks/aerial.rb
@@ -1,8 +1,8 @@
 cask "aerial" do
   version "2.3.3"
-  sha256 "857c2fb27733ce8ba8fb6cadb4642e1994b14f800f335fbe058ff4ae5cb294cd"
+  sha256 :no_check
 
-  url "https://github.com/JohnCoates/Aerial/releases/download/v#{version}/Aerial.saver.zip"
+  url "https://github.com/JohnCoates/Aerial/releases/download/v2.3.3/Aerial.saver.zip"
   name "Aerial Screensaver"
   desc "Apple TV Aerial screensaver"
   homepage "https://github.com/JohnCoates/Aerial"
