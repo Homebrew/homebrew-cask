3e1b6a6b9726a26410021b5ec029b44ae8b48c61
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-21 06:34:19 +0800

    Update burp-suite.rb

--- Casks/burp-suite.rb
+++ Casks/burp-suite.rb
@@ -14,18 +14,7 @@ cask "burp-suite" do
     end
   end
 
-  installer script: {
-    executable: "Burp Suite Community Edition.app/Contents/MacOS/JavaApplicationStub",
-    args:       ["-q"],
-    sudo:       true,
-  }
-
-  postflight do
-    set_ownership "/Applications/Burp Suite Community Edition.app"
-    set_permissions "/Applications/Burp Suite Community Edition.app", "a+rX"
-  end
-
-  uninstall delete: "/Applications/Burp Suite Community Edition.app"
+  app "Burp Suite Community Edition.app"
 
   zap trash: "~/.BurpSuite"
 end
