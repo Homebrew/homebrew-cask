891832b914ebb173af7300334f784a2237433457
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-21 05:25:22 +0800

    Update remarkable.rb

--- Casks/remarkable.rb
+++ Casks/remarkable.rb
@@ -9,10 +9,7 @@ cask "remarkable" do
 
   livecheck do
     url "https://get-updates.cloud.remarkable.engineering/sparkle/reMarkableMacOs/Prod/appcast.xml"
-    regex(%r{reMarkableMacOs/(Dev|Prod)/\d+/reMarkable-(\d+(?:\.\d+)*)\.dmg}i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| match[1].to_s }
-    end
+    regex(/reMarkable-(\d+(?:\.\d+)+)\.dmg/i)
   end
 
   auto_updates true
