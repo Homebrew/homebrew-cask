7654c66862aac648415c5ffa621befb3a22bd2e5
Yannic Haupenthal <yhaupenthal@posteo.de>
2021-11-20 19:31:48 +0700

    Fix version, url, livecheck and app

--- Casks/revolver-office.rb
+++ Casks/revolver-office.rb
@@ -1,11 +1,19 @@
 cask "revolver-office" do
-  version "8123"
+  version "8.12.3"
   sha256 "fae87bb58908319bf0bb7da62fc30bed67195ac30e691e7de55a6a17e0dbc8d0"
 
-  url "https://download.revolver.info/r8/12/3/mac/revolveroffice#{version}.dmg"
+  url "https://download.revolver.info/r#{version.major}/#{version.minor}/#{version.patch}/mac/revolveroffice#{version.no_dots}.dmg"
   name "Revolver Office"
   desc "Project management tool"
   homepage "https://www.revolver.info/"
 
-  app "Revolver Office"
+  livecheck do
+    url "https://www.revolver.info/downloads/"
+    strategy :page_match do |page|
+      page.scan(/href=.*?r?(\d+(?:\/\d+)+)\/mac\/revolveroffice?.*?\.dmg/i)
+          .map { |match| match&.first&.gsub(/\//, "\.") }
+    end
+  end
+
+  app "Revolver Office.app"
 end
