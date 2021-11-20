f2e45ee7bf277be7545825b50a18c4e86dd2bc36
Yannic Haupenthal <yhaupenthal@posteo.de>
2021-11-20 18:39:18 +0700

    Add Revolver Office.app 8.12.3

---
+++ Casks/revolver-office.rb
@@ -0,0 +1,11 @@
+cask "revolver-office" do
+  version "8123"
+  sha256 "fae87bb58908319bf0bb7da62fc30bed67195ac30e691e7de55a6a17e0dbc8d0"
+
+  url "https://download.revolver.info/r8/12/3/mac/revolveroffice#{version}.dmg"
+  name "Revolver Office"
+  desc "Project management tool"
+  homepage "https://www.revolver.info/"
+
+  app "Revolver Office"
+end
