afe29aef6d7130a1ea63b63e92373ab87ebf2fb4
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-21 06:32:37 +0800

    Update defold.rb

--- Casks/defold.rb
+++ Casks/defold.rb
@@ -1,38 +1,21 @@
 cask "defold" do
-  version "1.2.189,229b4dbc78495615871b3a7679e79d75d018c27a"
-  sha256 :no_check
+  version "1.2.189"
+  sha256 "78a2d397b2c77e68bf8435fa5901b486ddb50fa043569ac9b763c59442c7168b"
 
-  # Yes, this is fine, editor-alpha is how their stable channel is called
-  release_channel = "editor-alpha"
-  release_info_url = "http://d.defold.com/#{release_channel}/info.json"
-
-  url release_info_url do |json_content|
-    require "json"
-    version_sha = JSON.parse(json_content)["sha1"]
-    "https://d.defold.com/archive/editor-alpha/#{version_sha}/editor-alpha/editor2/Defold-x86_64-darwin.dmg"
-  end
+  url "https://github.com/defold/defold/releases/download/#{version}/Defold-x86_64-darwin.dmg",
+      verified: "github.com/defold/defold/"
   name "Defold"
-  desc "Free to use game engine for development of desktop, mobile and web games"
+  desc "Game engine for development of desktop, mobile and web games"
   homepage "https://defold.com/"
 
-  livecheck do
-    url release_info_url
-    strategy :page_match do |json_content|
-      require "json"
-      info = JSON.parse(json_content)
-      "#{info["version"]},#{info["sha1"]}"
-    end
-  end
-
   auto_updates true
-  depends_on macos: ">= :el_capitan"
 
   app "Defold.app"
 
   zap trash: [
     "~/Library/Application Support/Defold",
-    "~/Library/Preferences/com.defold.editor.plist",
     "~/Library/Caches/com.defold.editor",
+    "~/Library/Preferences/com.defold.editor.plist",
     "~/Library/Saved Application State/com.defold.editor.savedState",
   ]
 end
