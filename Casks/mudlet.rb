77f814403d2513581f5c852bb39228e377f42fa7
neutric <5984479+neutric@users.noreply.github.com>
2021-11-22 00:39:09 +0100

    Update mudlet from 4.13.1 to 4.14.0

--- Casks/mudlet.rb
+++ Casks/mudlet.rb
@@ -1,6 +1,6 @@
 cask "mudlet" do
-  version "4.13.1"
-  sha256 "a96bc99c75bd40f85c0a23d675ab29bddb46d344e56ac6cfc4e4393529f7594a"
+  version "4.14.0"
+  sha256 "fbea26f85aae4713a6c10f39c67b01ad35778a1626d70c97c12e311d1faf1fd7"
 
   url "https://www.mudlet.org/wp-content/files/Mudlet-#{version}.dmg"
   name "Mudlet"
@@ -9,7 +9,6 @@ cask "mudlet" do
 
   livecheck do
     url "https://github.com/Mudlet/Mudlet"
-    strategy :git
   end
 
   depends_on macos: ">= :high_sierra"
