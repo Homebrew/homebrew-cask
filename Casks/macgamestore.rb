72f02165c3c24f34d3119187bc8338aa01a3105c
neutric <5984479+neutric@users.noreply.github.com>
2021-11-21 05:19:53 +0100

    Update macgamestore from 4.3.2 to 4.3.4 (#114685)
    * Update macgamestore from 4.3.2 to 4.3.4
    
    * macgamestore: tweak livecheck regex

--- Casks/macgamestore.rb
+++ Casks/macgamestore.rb
@@ -1,6 +1,6 @@
 cask "macgamestore" do
-  version "4.3.2,6082"
-  sha256 "46b48ec39c4dbf1dce6d249b7de28e4f74765b0eca97a95a65bd06d9276da3d8"
+  version "4.3.4,6084"
+  sha256 "9f4b86ba8726de7c00fa129392b5b3cfdb0af7b682dfedfa1e0b00e08ea0cf11"
 
   url "https://www.macgamestore.com/api_clientapp/clientupdates/public/core6/MacGameStore_#{version.before_comma}_#{version.after_comma}.tgz"
   name "MacGameStore"
@@ -10,7 +10,7 @@ cask "macgamestore" do
   livecheck do
     url "https://www.macgamestore.com/api_clientapp/clientupdates/public/update.xml"
     strategy :sparkle do |item|
-      match = item.url.match(%r{/MacGameStore_(\d+(?:\.\d+)*)_(\d+)\.tgz}i)
+      match = item.url.match(%r{/MacGameStore_(\d+(?:\.\d+)+)_(\d+)\.t}i)
       next if match.blank?
 
       "#{match[1]},#{match[2]}"
@@ -22,7 +22,7 @@ cask "macgamestore" do
   app "MacGameStore.app"
 
   zap trash: [
-    "~/Library/Application Support/MacGameStore.com",
     "/Applications/MacGameStore",
+    "~/Library/Application Support/MacGameStore.com",
   ]
 end
