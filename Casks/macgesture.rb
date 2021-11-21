616e2a9c6e783ecf4cb1d1f629e2c34811bbc5c5
neutric <5984479+neutric@users.noreply.github.com>
2021-11-21 05:19:34 +0100

    Update macgesture from 3.0.0 to 3.1.0 (#114686)
    * Update macgesture from 3.0.0 to 3.1.0
    
    * macgesture: switch to Git livecheck strategy
    
    * macgesture: add auto_updates
    
    * macgesture: add zap

--- Casks/macgesture.rb
+++ Casks/macgesture.rb
@@ -1,16 +1,19 @@
 cask "macgesture" do
-  version "3.0.0"
-  sha256 "bd3e2638dc9eef8d1a6a9945792d7285c3cf85d47cc12bd8b9000bec83c43831"
+  version "3.1.0"
+  sha256 "9be2282365015bac05c5017f6c93225c9c451cef51bd1864d73b9970d559ad48"
 
   url "https://github.com/MacGesture/MacGesture/releases/download/#{version}/MacGesture-#{version}.zip"
   name "MacGesture"
   desc "Utility to set up global mouse gestures"
   homepage "https://github.com/MacGesture/MacGesture"
 
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
+  auto_updates true
 
   app "MacGesture.app"
+
+  zap trash: [
+    "~/Library/Caches/com.codefalling.MacGesture",
+    "~/Library/Preferences/com.codefalling.MacGesture.plist",
+    "~/Library/WebKit/com.codefalling.MacGesture",
+  ]
 end
