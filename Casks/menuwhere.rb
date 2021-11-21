46f451e497ca81898d343dedbc0f9a208ab63177
neutric <5984479+neutric@users.noreply.github.com>
2021-11-21 02:13:32 +0100

    menuwhere: fix sha256

--- Casks/menuwhere.rb
+++ Casks/menuwhere.rb
@@ -1,6 +1,6 @@
 cask "menuwhere" do
   version "2.1.1,22"
-  sha256 :no_check
+  sha256 "2068b91b41bae33c40168c6092522a028e2e42d6a415844d0c404aa51b81d9b4"
 
   url "https://manytricks.com/download/_do_not_hotlink_/menuwhere#{version.before_comma.no_dots}.dmg"
   name "Menuwhere"
