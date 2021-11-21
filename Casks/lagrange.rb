70ee8c2783a560246a8331375db69a3513579a67
Jaakko Keränen <jaakko.keranen@iki.fi>
2021-11-21 16:07:51 +0200

    Update lagrange from 1.8.2 to 1.8.3

--- Casks/lagrange.rb
+++ Casks/lagrange.rb
@@ -1,12 +1,12 @@
 cask "lagrange" do
   arch = Hardware::CPU.intel? ? "macos10.13-x86_64" : "macos11.0-arm64"
 
-  version "1.8.2"
+  version "1.8.3"
 
   if Hardware::CPU.intel?
-    sha256 "be6cf80002c9ec80184188e01db457d7e1f8758c1a26aa5796c69987c3019711"
+    sha256 "dab3aaa9b1855f87757fa88f603521e8b0be5bb87fa5aaad1d62081e01d33f17"
   else
-    sha256 "39182aa5d16c826f4ea8dcc0e6f0a9f898009b95fa47464af330d353dc48e015"
+    sha256 "b8a8e3b2e92161f1670fbbaac75a6903de92324181bb2833ad894a822ce81116"
   end
 
   url "https://github.com/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_#{arch}.tbz",
