a2f193081deeede2a1237001e8a92d0a071d1c20
neutric <5984479+neutric@users.noreply.github.com>
2021-11-22 07:11:19 +0100

    Update mendeley-reference-manager from 2.61.0 to 2.61.1 (#114726)

--- Casks/mendeley-reference-manager.rb
+++ Casks/mendeley-reference-manager.rb
@@ -1,6 +1,6 @@
 cask "mendeley-reference-manager" do
-  version "2.61.0"
-  sha256 "400d24fced11b5715a0291793bfe1878558f4d570ac706c714f1402dff3d0e38"
+  version "2.61.1"
+  sha256 "f1f9759f1ed68119a5cfeccf2e1e2f2864951b79203716971c21fedb05de9cb2"
 
   url "https://static.mendeley.com/bin/desktop/mendeley-reference-manager-#{version}.dmg"
   name "Mendeley Reference Manager"
