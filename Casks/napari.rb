8935a614281de6f6cc9a4560f43c73f0d9110e2e
neutric <5984479+neutric@users.noreply.github.com>
2021-11-23 00:55:20 +0100

    Update napari from 0.4.10 to 0.4.12

--- Casks/napari.rb
+++ Casks/napari.rb
@@ -1,8 +1,8 @@
 cask "napari" do
-  version "0.4.10"
-  sha256 "1fc7f58ee85ec9a51a3f16f38118b380c194bc15e94f12294009100c5a3004c6"
+  version "0.4.12"
+  sha256 "fd76d7b5a7990e3f6deb06785d685c9f14672c175b2e5f9a95bbd90ab1a3519e"
 
-  url "https://github.com/napari/napari/releases/download/v#{version}/napari-#{version}-macOS.zip",
+  url "https://github.com/napari/napari/releases/download/v#{version}/napari-#{version}-macOS-x86_64.zip",
       verified: "github.com/napari/napari/"
   name "napari"
   desc "Multi-dimensional image viewer for Python"
