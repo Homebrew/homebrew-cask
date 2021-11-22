ebac8bb92afdc57456213a6c415209cdd7f6453b
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-22 17:54:32 +0800

    Update altair-graphql-client.rb

--- Casks/altair-graphql-client.rb
+++ Casks/altair-graphql-client.rb
@@ -3,14 +3,14 @@ cask "altair-graphql-client" do
 
   version "4.2.1"
 
-  url "https://github.com/imolorhe/altair/releases/download/v#{version}/altair_#{version}_#{arch}_mac.zip",
-      verified: "github.com/imolorhe/altair/"
   if Hardware::CPU.intel?
     sha256 "852c45b630119d100e3d4bbd269f3eff1d422d16f8ee4680ae599a66e79b6561"
   else
     sha256 "e6082291eaa67568fd5b9bb307dc7487d897c73252b8ca05e841572862705e08"
   end
 
+  url "https://github.com/imolorhe/altair/releases/download/v#{version}/altair_#{version}_#{arch}_mac.zip",
+      verified: "github.com/imolorhe/altair/"
   name "Altair GraphQL Client"
   desc "GraphQL client"
   homepage "https://altair.sirmuel.design/"
