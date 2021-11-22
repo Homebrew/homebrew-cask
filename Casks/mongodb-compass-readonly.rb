327321b776009bc77de99a88bd3e3dac6f66ef4c
neutric <5984479+neutric@users.noreply.github.com>
2021-11-22 07:11:11 +0100

    Update mongodb-compass-readonly from 1.28.4 to 1.29.4 (#114737)
    * Update mongodb-compass-readonly from 1.28.4 to 1.29.4
    
    * mongodb-compass-readonly: add zap

--- Casks/mongodb-compass-readonly.rb
+++ Casks/mongodb-compass-readonly.rb
@@ -1,6 +1,6 @@
 cask "mongodb-compass-readonly" do
-  version "1.28.4"
-  sha256 "10a9969107f3cab7f602967501312c58d7fc4fb9a02c55e5fb6beb63d1d4f78e"
+  version "1.29.4"
+  sha256 "5d6f5512392398b3632345da3c23d2719d961495335eb85808659b147510201d"
 
   url "https://downloads.mongodb.com/compass/mongodb-compass-readonly-#{version}-darwin-x64.dmg"
   name "MongoDB Compass Readonly"
@@ -13,4 +13,13 @@ cask "mongodb-compass-readonly" do
   end
 
   app "MongoDB Compass Readonly.app"
+
+  zap trash: [
+    "~/.mongodb/compass",
+    "~/Library/Application Support/MongoDB Compass Readonly",
+    "~/Library/Caches/com.mongodb.compass.readonly",
+    "~/Library/Caches/com.mongodb.compass.readonly.ShipIt",
+    "~/Library/Preferences/com.mongodb.compass.readonly.plist",
+    "~/Library/Saved Application State/com.mongodb.compass.readonly.savedState",
+  ]
 end
