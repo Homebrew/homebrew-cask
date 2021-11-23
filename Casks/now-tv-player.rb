47d78b8ed20a2a775e06522bbea95b7b31ccbea5
neutric <5984479+neutric@users.noreply.github.com>
2021-11-23 01:27:38 +0100

    Update now-tv-player from 8.7.0 to 8.8.0 (#114771)
    * Update now-tv-player from 8.7.0 to 8.8.0
    
    * now-tv-player: add zap

--- Casks/now-tv-player.rb
+++ Casks/now-tv-player.rb
@@ -1,5 +1,5 @@
 cask "now-tv-player" do
-  version "8.7.0"
+  version "8.8.0"
   sha256 :no_check
 
   url "https://web.static.nowtv.com/watch/player/nowtv/gb/latest/NOWTVPlayer-Mac.dmg"
@@ -13,4 +13,12 @@ cask "now-tv-player" do
   end
 
   app "NOW TV Player.app"
+
+  zap trash: [
+    "~/Library/Application Support/NOW TV Player",
+    "~/Library/Logs/NOW TV Player",
+    "~/Library/Preferences/com.electron.now-tv-player.helper.plist",
+    "~/Library/Preferences/com.electron.now-tv-player.plist",
+    "~/Library/Saved Application State/com.electron.now-tv-player.savedState",
+  ]
 end
