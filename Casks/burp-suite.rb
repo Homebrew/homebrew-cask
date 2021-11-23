b1cf19c56163fb5ef95bd9c2c6cecf5550905116
Thierry KHAMPHOUSONE <khamphousone.thierry@outlook.fr>
2021-11-23 17:05:20 +0100

    Update burp-suite.rb

--- Casks/burp-suite.rb
+++ Casks/burp-suite.rb
@@ -1,6 +1,6 @@
 cask "burp-suite" do
   version "2021.10.2"
-  sha256 "2b4a12431c06a38e3d050bc09792bdfbebafc4be697e89fae62138fb67765cd2"
+  sha256 "b15d38fc9a66982a0b991373206f5bc5e76730e5628d965dd5fa0126d64e04bf"
 
   url "https://portswigger.net/burp/releases/download?product=community&version=#{version}&type=MacOsx"
   name "Burp Suite Community Edition"
