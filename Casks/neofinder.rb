8d6d7d2abb6e7163961807f88de06a8d205eaefe
neutric <5984479+neutric@users.noreply.github.com>
2021-11-23 14:45:11 +0100

    Update neofinder from 7.8.1 to 8.0 (#114799)
    * Update neofinder from 7.8.1 to 8.0
    
    * neofinder: add depends_on
    
    * neofinder: add zap

--- Casks/neofinder.rb
+++ Casks/neofinder.rb
@@ -1,8 +1,8 @@
 cask "neofinder" do
-  version "7.8.1"
-  sha256 "5a54256c5c7abc98b8d2d777b7168eec3a5d5b7c48b4414ef1d71ea8f82b84cc"
+  version "8.0"
+  sha256 "2eb216809f5c76d06228a19407296b2b5763a87e545d4c55638ec65ee7cd64c0"
 
-  url "https://www.wfs-apps.de/updates/neofinder.#{version}.zip",
+  url "https://www.wfs-apps.de/updates/neofinder-mac.#{version}.zip",
       verified: "wfs-apps.de/"
   name "NeoFinder"
   desc "Digital media asset manager"
@@ -13,5 +13,16 @@ cask "neofinder" do
     strategy :sparkle
   end
 
+  depends_on macos: ">= :el_capitan"
+
   app "NeoFinder.app"
+
+  zap trash: [
+    "~/Library/Application Support/CrashReporter/NeoFinder_*",
+    "~/Library/Application Support/NeoFinder",
+    "~/Library/Caches/de.wfs-apps.neofinder",
+    "~/Library/Caches/de.wfs-apps.neofinder.quicklaunch.cache",
+    "~/Library/Preferences/de.wfs-apps.neofinder.plist",
+    "~/Library/Preferences/de.wfs-apps.neofinder.statusBar.plist",
+  ]
 end
