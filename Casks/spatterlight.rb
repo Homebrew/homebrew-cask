0b5da42726345b5fba6f0e1ee63c0f0d27bbce21
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-24 18:52:08 +0800

    Update spatterlight.rb

--- Casks/spatterlight.rb
+++ Casks/spatterlight.rb
@@ -6,19 +6,15 @@ cask "spatterlight" do
       verified: "github.com/angstsmurf/spatterlight/"
   name "Spatterlight"
   desc "Play most kinds of interactive fiction game files"
-  homepage "http://ccxvii.net/spatterlight/"
+  homepage "https://ccxvii.net/spatterlight/"
 
   depends_on macos: ">= :catalina"
 
   app "Spatterlight.app"
 
   zap trash: [
-    "~/Library/Application Scripts/net.ccxvii.spatterlight.SpatterlightQuickLook",
-    "~/Library/Application Scripts/net.ccxvii.spatterlight.SpatterlightThumbnailer",
-    "~/Library/Application Scripts/net.ccxvii.spatterlight.iFictionQuickLook",
-    "~/Library/Containers/net.ccxvii.spatterlight.SpatterlightQuickLook",
-    "~/Library/Containers/net.ccxvii.spatterlight.SpatterlightThumbnailer",
-    "~/Library/Containers/net.ccxvii.spatterlight.iFictionQuickLook",
+    "~/Library/Application Scripts/net.ccxvii.spatterlight.*",
+    "~/Library/Containers/net.ccxvii.spatterlight.*",
     "~/Library/Preferences/net.ccxvii.spatterlight.plist",
   ]
 end
