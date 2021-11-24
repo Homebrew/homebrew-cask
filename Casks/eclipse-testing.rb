7334c6f736dd96008d644c4357de1643a04277ef
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-24 13:59:57 -0800

    Update eclipse-testing from 4.18.0 to 4.21.0

--- Casks/eclipse-testing.rb
+++ Casks/eclipse-testing.rb
@@ -1,11 +1,15 @@
 cask "eclipse-testing" do
-  version "4.18.0,2020-12:R"
-  sha256 "f3882e04f5b84d714b234cc2868ef07b633f13e2a7ce2145796e9b067b603e0e"
+  version "4.21.0,2021-09:R"
+  sha256 "ad167befe1e88df397c50cac7a3ad9fa894720810f80118b3ea0fa3666cd263b"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-testing-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name "Eclipse for Testers"
   desc "Eclipse IDE for testing purposes"
   homepage "https://eclipse.org/"
+
+  livecheck do
+    cask "eclipse-ide"
+  end
 
   # Renamed to avoid conflict with other Eclipse.
   app "Eclipse.app", target: "Eclipse Testing.app"
