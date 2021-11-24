06f8b1c43ced0c1e27a2812636f86b32d6d279fe
neutric <5984479+neutric@users.noreply.github.com>
2021-11-25 00:30:31 +0100

    Update openloco from 21.08 to 21.10

--- Casks/openloco.rb
+++ Casks/openloco.rb
@@ -1,6 +1,6 @@
 cask "openloco" do
-  version "21.08"
-  sha256 "d4fb13137c8ce463e307551ee41af7cd9290e751ddf7f09f2592d5bec8d9016d"
+  version "21.10"
+  sha256 "b35173556b5e15a059a9b66c86ea091a863c31850bbb6ed51363d209f923bc2a"
 
   url "https://github.com/OpenLoco/OpenLoco/releases/download/v#{version}/openloco-v#{version}-macos.zip"
   name "OpenLoco"
