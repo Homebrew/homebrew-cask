99bc7be76cf2e8b5f1e9a72ddcf0fb4092bdce68
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-24 12:51:56 -0800

    Update temurin.rb

--- Casks/temurin.rb
+++ Casks/temurin.rb
@@ -16,7 +16,7 @@ cask "temurin" do
   homepage "https://adoptium.net/"
 
   livecheck do
-    url "https://api.adoptium.net/v3/info/release_versions?release_type=ga&architecture=x64&image_type=jdk&jvm_impl=hotspot&os=mac&page=0&page_size=1&project=jdk&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse"
+    url "https://api.adoptium.net/v3/info/release_versions?release_type=ga&architecture=#{arch}&image_type=jdk&jvm_impl=hotspot&os=mac&page=0&page_size=1&project=jdk&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse"
     strategy :page_match do |page|
       JSON.parse(page)["versions"].map do |version|
         match = version["openjdk_version"].match(/^(\d+(?:\.\d+)*)\+(\d+(?:\.\d+)*)$/i)
