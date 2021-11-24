7b9c4a826d4f17ad7f4399177899cfe60a2d5966
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-24 12:49:11 -0800

    Use version-agnostic URL for livecheck

--- Casks/temurin.rb
+++ Casks/temurin.rb
@@ -16,10 +16,10 @@ cask "temurin" do
   homepage "https://adoptium.net/"
 
   livecheck do
-    url "https://api.adoptium.net/v3/assets/feature_releases/#{version.csv[0].major}/ga?architecture=#{arch}&image_type=jdk&jvm_impl=hotspot&os=mac&page=0&page_size=1&project=jdk&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse"
+    url "https://api.adoptium.net/v3/info/release_versions?release_type=ga&architecture=x64&image_type=jdk&jvm_impl=hotspot&os=mac&page=0&page_size=1&project=jdk&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse"
     strategy :page_match do |page|
-      JSON.parse(page).map do |release|
-        match = release["release_name"].match(/^jdk-(\d+(?:\.\d+)*)\+(\d+(?:\.\d+)*)$/i)
+      JSON.parse(page)["versions"].map do |version|
+        match = version["openjdk_version"].match(/^(\d+(?:\.\d+)*)\+(\d+(?:\.\d+)*)$/i)
         next if match.blank?
 
         "#{match[1]},#{match[2]}"
