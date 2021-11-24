f2033a66df68f5968b9ae79b0f65fc0b29ce996a
Robert Blair <12276457+robertleeblairjr@users.noreply.github.com>
2021-11-24 11:10:22 -0600

    Update origin from 10.5.106.49298 to 10.5.107.49426 (#114858)

--- Casks/origin.rb
+++ Casks/origin.rb
@@ -1,5 +1,5 @@
 cask "origin" do
-  version "10.5.106.49298"
+  version "10.5.107.49426"
   sha256 :no_check
 
   url "https://origin-a.akamaihd.net/Origin-Client-Download/origin/mac/live/Origin.dmg",
