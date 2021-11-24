f6a79970347940fa1595ea2c57e92d06750a1534
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-24 14:03:34 -0800

    Update eclipse-javascript from 4.18.0 to 4.21.0

--- Casks/eclipse-javascript.rb
+++ Casks/eclipse-javascript.rb
@@ -1,11 +1,15 @@
 cask "eclipse-javascript" do
-  version "4.18.0,2020-12:R"
-  sha256 "28869cc65cac7277d6afaa5d7deb49bd34884e6da6840e5df02dafa07b7babed"
+  version "4.21.0,2021-09:R"
+  sha256 "16e412c92f115a900dd497d989c718bd6e315d6be7c9c6ac000139b8de56bd53"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-javascript-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name "Eclipse IDE for JavaScript and Web Developers"
   desc "Eclipse IDE for JavaScript and web developers"
   homepage "https://eclipse.org/"
+
+  livecheck do
+    cask "eclipse-ide"
+  end
 
   # Renamed to avoid conflict with other Eclipse.
   app "Eclipse.app", target: "Eclipse JavaScript.app"
