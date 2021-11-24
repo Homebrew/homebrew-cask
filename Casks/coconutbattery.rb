297659e5eb942db92beebe020e79865cf8474d82
tiiiecherle <tiiiecherle@gmail.com>
2021-11-24 21:50:16 +0100

    Update coconutbattery from 3.9.6,404cb123 to 3.9.6,233080c3 (#114866)
    * Update coconutbattery from 3.9.6,404cb123 to 3.9.6,233080c3
    
    * Update coconutbattery.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/coconutbattery.rb
+++ Casks/coconutbattery.rb
@@ -5,10 +5,10 @@ cask "coconutbattery" do
 
     url "https://www.coconut-flavour.com/downloads/coconutBattery_#{version}.zip"
   else
-    version "3.9.6,404cb123"
-    sha256 "52a6a06b33ced49eb324c2aa0a7c9b56717eb9361efeaeada9fd5f2179d63fed"
+    version "3.9.6,233080c3"
+    sha256 "fd1dff203b48f74b626bfefc2e5071be87f25755897bd65cdb4a57bdc5df8c09"
 
-    url "https://www.coconut-flavour.com/downloads/coconutBattery_#{version.before_comma.no_dots}_#{version.after_comma}.zip"
+    url "https://www.coconut-flavour.com/downloads/coconutBattery_#{version.csv[0].no_dots}_#{version.csv[1]}.zip"
 
     livecheck do
       url "https://coconut-flavour.com/updates/coconutBattery.xml"
@@ -34,8 +34,8 @@ cask "coconutbattery" do
     "~/Library/Application Support/coconutBattery",
     "~/Library/Caches/com.coconut-flavour.coconutBattery*",
     "~/Library/Group Containers/*.coconut-flavour.coconutBattery",
-    "~/Library/Preferences/com.coconut-flavour.coconutBattery.plist",
     "~/Library/Preferences/com.coconut-flavour.coconutBattery-Menu.plist",
+    "~/Library/Preferences/com.coconut-flavour.coconutBattery.plist",
     "~/Library/Saved Application State/com.coconut-flavour.coconutBattery.savedState",
   ]
 end
