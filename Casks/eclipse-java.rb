77cad864c1f4689bc0e6d068d622286b37a838a1
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-25 14:46:00 -0800

    eclipse-*: delete `:R` suffix in version string (#114916)

--- Casks/eclipse-java.rb
+++ Casks/eclipse-java.rb
@@ -1,21 +1,14 @@
 cask "eclipse-java" do
-  version "4.21.0,2021-09:R"
+  version "4.21.0,2021-09"
   sha256 "5fb0137aff46c6d8b36615670aa86dcfc4b653b19fd1bd0d1bbeae8f1c1cf9ce"
 
-  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-java-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
+  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-java-#{version.csv.second}-R-macosx-cocoa-x86_64.dmg&r=1"
   name "Eclipse IDE for Java Developers"
   desc "Eclipse IDE for Java developers"
   homepage "https://eclipse.org/"
 
   livecheck do
-    url "https://www.eclipse.org/downloads/packages/"
-    strategy :page_match do |page|
-      page.scan(/Eclipse IDE (\d+-\d+) R Packages/i).map do |release|
-        version_page = Homebrew::Livecheck::Strategy.page_content("https://projects.eclipse.org/releases/#{release[0]}")[:content]
-        version = version_page.scan(%r{href="/projects/eclipse/releases/(\d+(?:\.\d+)*)"}i)
-        "#{version[0][0]},#{release[0]}:R"
-      end
-    end
+    cask "eclipse-ide"
   end
 
   # Renamed to avoid conflict with other Eclipse.
--- Casks/eclipse-javascript.rb
+++ Casks/eclipse-javascript.rb
@@ -1,8 +1,8 @@
 cask "eclipse-javascript" do
-  version "4.18.0,2020-12:R"
+  version "4.18.0,2020-12"
   sha256 "28869cc65cac7277d6afaa5d7deb49bd34884e6da6840e5df02dafa07b7babed"
 
-  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-javascript-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
+  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-javascript-#{version.csv.second}-R-macosx-cocoa-x86_64.dmg&r=1"
   name "Eclipse IDE for JavaScript and Web Developers"
   desc "Eclipse IDE for JavaScript and web developers"
   homepage "https://eclipse.org/"
