6cf20aec0d059c49067da676601558b0813df4e4
Jan Steeg <jsteeg@posteo.de>
2021-11-25 23:00:10 +0000

    Update mountain-duck from 4.8.2 to 4.9.0 (#114938)
    * Update mountain-duck from 4.8.2 to 4.9.0
    
    * Update mountain-duck.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/mountain-duck.rb
+++ Casks/mountain-duck.rb
@@ -1,8 +1,8 @@
 cask "mountain-duck" do
-  version "4.8.2,18835"
-  sha256 "84f2e599e612639f4275f6633bcae6ecc489f0c839f20a66128cd7a09768d102"
+  version "4.9.0,18884"
+  sha256 "9787859b90b1a9509e259e01dab2f1e7bcb8b65079808fc4fa7bdf1f8425f762"
 
-  url "https://dist.mountainduck.io/Mountain%20Duck-#{version.before_comma}.#{version.after_comma}.zip"
+  url "https://dist.mountainduck.io/Mountain%20Duck-#{version.csv.first}.#{version.csv.second}.zip"
   name "Mountain Duck"
   desc "Mounts servers and cloud storages as a disk on the desktop"
   homepage "https://mountainduck.io/"
@@ -22,7 +22,7 @@ cask "mountain-duck" do
     "~/Library/Caches/io.mountainduck",
     "~/Library/Containers/io.mountainduck.findersync",
     "~/Library/Group Containers/G69SCX94XU.duck",
-    "~/Library/Preferences/io.mountainduck.plist",
     "~/Library/Preferences/G69SCX94XU.duck.plist",
+    "~/Library/Preferences/io.mountainduck.plist",
   ]
 end
