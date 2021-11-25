314237d49df0a69df7977b7b9c89b3f66e21a619
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-26 00:19:20 +0800

    Update izotope-product-portal.rb

--- Casks/izotope-product-portal.rb
+++ Casks/izotope-product-portal.rb
@@ -4,17 +4,17 @@ cask "izotope-product-portal" do
 
   url "https://s3.amazonaws.com/izotopedownloads/product_download/iZotope_Product_Portal_v#{version.tr(".", "_")}.dmg",
       verified: "https://s3.amazonaws.com/izotopedownloads/"
-  name "izotope-product-portal"
+  name "Izotope product portal"
   desc "Professional audio software for audio recording, mixing, broadcast and others"
   homepage "https://www.izotope.com/en/products/downloads.html"
 
   livecheck do
     url "https://www.izotope.com/in-app/pp/download/mac"
     strategy :header_match do |headers|
-      loc = headers["location"][/iZotope_Product_Portal_v(\d+(?:_[\d,\w]+)+)\.dmg/i, 1]
-      next if loc.blank?
+      match = headers["location"][/iZotope[._-]Product[._-]Portal[._-]v?(\d+(?:[._-]\d+)+\w?)\.dmg/i, 1]
+      next if match.blank?
 
-      loc.tr("_", ".")
+      match.tr("_", ".")
     end
   end
 
