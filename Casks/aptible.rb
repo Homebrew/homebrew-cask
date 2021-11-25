892f02f6323c5a6f7008e60c3dd15c1417658b43
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-25 00:30:54 -0800

    Use `version.csv` instead of `version.after_comma.before_comma`

--- Casks/aptible.rb
+++ Casks/aptible.rb
@@ -2,7 +2,7 @@ cask "aptible" do
   version "0.19.1,20210802230457,251"
   sha256 "693e38e36edcd5289df8c0456ecfd7e812c668fc368b32ca9cc54fe90698530a"
 
-  url "https://omnibus-aptible-toolbelt.s3.amazonaws.com/aptible/omnibus-aptible-toolbelt/master/#{version.after_comma.after_comma}/pkg/aptible-toolbelt-#{version.before_comma}%2B#{version.after_comma.before_comma}-mac-os-x.10.11.6-1.pkg",
+  url "https://omnibus-aptible-toolbelt.s3.amazonaws.com/aptible/omnibus-aptible-toolbelt/master/#{version.csv.third}/pkg/aptible-toolbelt-#{version.csv.first}%2B#{version.csv.second}-mac-os-x.10.11.6-1.pkg",
       verified: "omnibus-aptible-toolbelt.s3.amazonaws.com/"
   name "Aptible Toolbelt"
   desc "Command-line tool for Aptible Deploy, an audit-ready App Deployment Platform"
@@ -10,16 +10,15 @@ cask "aptible" do
 
   livecheck do
     url :homepage
-    strategy :page_match do |page|
-      page.scan(%r{href=.*?/(\d+)/pkg/aptible-toolbelt[._-]v?(\d+(?:\.\d+)+)%2B(\d+)[._-]mac}i).map do |match|
-        "#{match[1]},#{match[2]},#{match[0]}"
-      end
+    regex(%r{href=.*?/(\d+)/pkg/aptible-toolbelt[._-]v?(\d+(?:\.\d+)+)%2B(\d+)[._-]mac}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[1]},#{match[2]},#{match[0]}" }
     end
   end
 
   depends_on formula: "libu2f-host"
 
-  pkg "aptible-toolbelt-#{version.before_comma}+#{version.after_comma.before_comma}-mac-os-x.10.11.6-1.pkg"
+  pkg "aptible-toolbelt-#{version.csv.first}+#{version.csv.second}-mac-os-x.10.11.6-1.pkg"
 
   uninstall pkgutil: "com.aptible.toolbelt"
 end
