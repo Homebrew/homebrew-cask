3f6dc86231eb8e96a51e75e0dd53500afd6f55c6
Bevan Kay <email@bevankay.me>
2021-11-25 17:37:41 +1100

    update url, update uninstall

--- Casks/soothe-2.rb
+++ Casks/soothe-2.rb
@@ -1,24 +1,30 @@
 cask "soothe-2" do
+  # NOTE: "2" is not a version number, but an intrinsic part of the product name
   version "1.2.2"
   sha256 "9f49f4a958bde5e2898cabbd803f2409ea5763a10e528de5fb752bd2388dddd9"
 
-  url "https://oeksound.ams3.cdn.digitaloceanspaces.com/soothe2_v#{version.delete(".")}_Mac.dmg"
-  verified "https://oeksound.ams3.cdn.digitaloceanspaces.com/"
+  url "https://oeksound.ams3.cdn.digitaloceanspaces.com/soothe2_v#{version.delete(".")}_Mac.dmg",
+      verified: "oeksound.ams3.cdn.digitaloceanspaces.com/"
   name "Soothe 2"
   desc "Dynamic resonance suppressor"
   homepage "https://oeksound.com/plugins/soothe2/"
 
   livecheck do
     url "https://oeksound.com/changelog/soothe2/"
-    strategy :page_match
     regex(%r{href=.*?/soothe2-(\d+(?:\.\d+)+)\.pkg}i)
   end
 
   depends_on macos: ">= :yosemite"
 
-  pkg "/Volumes/soothe2/soothe2.pkg"
+  pkg "soothe2.pkg"
 
-  uninstall ""
+  uninstall pkgutil: [
+    "com.oeksound.soothe2.common",
+    "com.oeksound.soothe2.vst3",
+    "com.oeksound.soothe2.vst",
+    "com.oeksound.soothe2.au",
+    "com.oeksound.soothe2.aax",
+  ]
 
   zap trash: [
     "~/Library/Application Support/oeksound/soothe2/config.xml",
