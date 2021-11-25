9c7149aede8dfedede1a55789023667c3984939d
Benjamin Battin <58034262+Benjamin51380@users.noreply.github.com>
2021-11-25 16:00:26 +0100

    Update drovio from 3.1.6 to 3.1.7 (#114924)

--- Casks/drovio.rb
+++ Casks/drovio.rb
@@ -1,6 +1,6 @@
 cask "drovio" do
-  version "3.1.6"
-  sha256 "eea3a9d3344cfe392537b1ec4d1190131db422153ba3d49edacae006741d031f"
+  version "3.1.7"
+  sha256 "78d1a64844256c6969179b7e623cb4d7adb5911773e87978267f13016b5a39f2"
 
   url "https://repository.drovio.com/stable/drovio/macos/#{version.major}.x/#{version}/drovio.dmg"
   name "Drovio"
