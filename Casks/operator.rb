e310abfcb9ffb373fca48c4c67fb2ab522d2c5df
neutric <5984479+neutric@users.noreply.github.com>
2021-11-25 05:00:34 +0100

    Update operator from 1.3.2 to 1.3.3 (#114888)

--- Casks/operator.rb
+++ Casks/operator.rb
@@ -1,6 +1,6 @@
 cask "operator" do
-  version "1.3.2"
-  sha256 "a845adb0d07e6d6fd5892ee9249161a7c3b0aba615a33e61334023af9676ace1"
+  version "1.3.3"
+  sha256 "00314bc08e8e96b02aa798b10812cb6d3cd4e3d10a417c80159800f02e39deec"
 
   url "https://s3.amazonaws.com/operator.versions/release-builds/#{version}/Operator-#{version}-mac.zip",
       verified: "s3.amazonaws.com/operator.versions/"
