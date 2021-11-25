f437d83aa99f728fccb0e5869caed3d4d4ad5413
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-25 23:24:09 +0800

    Update libreoffice from 7.2.2 to 7.2.3 (#114925)
    * Update libreoffice from 7.2.2 to 7.2.3
    
    * Update libreoffice.rb

--- Casks/libreoffice.rb
+++ Casks/libreoffice.rb
@@ -2,12 +2,12 @@ cask "libreoffice" do
   arch = Hardware::CPU.intel? ? "x86-64" : "aarch64"
   folder = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
-  version "7.2.2"
+  version "7.2.3"
 
   if Hardware::CPU.intel?
-    sha256 "dc2fd0577e3ee4f99c79d235a6efcd8fecc7069d24090c4eaea69e0fad8245ae"
+    sha256 "0dbadebbe1fe648b0f409200a83719869186c07cfa7a71bd328d5b26260fda62"
   else
-    sha256 "83954ea5bae605aba26fb0d731e080ee04e9e748a3148c43620af6ef2154f611"
+    sha256 "42033fb19cd8be0d09039e53347ef44f75eaa194f2d86b01723763ad19e2a07a"
   end
 
   url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/#{folder}/LibreOffice_#{version}_MacOS_#{arch}.dmg",
@@ -19,7 +19,7 @@ cask "libreoffice" do
   livecheck do
     url "https://download.documentfoundation.org/libreoffice/stable/"
     strategy :page_match
-    regex(%r{href="(\d+(?:\.\d+)*)/"}i)
+    regex(%r{href="(\d+(?:\.\d+)+)/"}i)
   end
 
   conflicts_with cask: "homebrew/cask-versions/libreoffice-still"
@@ -48,8 +48,8 @@ cask "libreoffice" do
   end
 
   zap trash: [
-    "~/Library/Application Support/LibreOffice",
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.libreoffice.script.sfl*",
+    "~/Library/Application Support/LibreOffice",
     "~/Library/Preferences/org.libreoffice.script.plist",
     "~/Library/Saved Application State/org.libreoffice.script.savedState",
   ]
