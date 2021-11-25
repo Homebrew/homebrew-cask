8de48debcaf0349cafb2db3faba5fd1a1f46ecdd
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-25 12:26:50 +0800

    Update notch-simulator.rb

--- Casks/notch-simulator.rb
+++ Casks/notch-simulator.rb
@@ -3,8 +3,8 @@ cask "notch-simulator" do
   sha256 "7e884cc68b0dcf163a963ebe4ffdaf7ce2a5700a0caa0d1d874115d5e6c03d43"
 
   url "https://github.com/megabitsenmzq/Notch-Simulator/releases/download/#{version}/Notch.Simulator.zip"
-  name "notch-simulator"
-  desc "Simulate the notch on the MacBook Pro 2021"
+  name "Notch Simulator"
+  desc "Simulate the notch on the MacBook Pro"
   homepage "https://github.com/megabitsenmzq/Notch-Simulator"
 
   app "Notch Simulator.app"
