59b0d49be436b4d09a43ad038d3b8978a3494b31
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-25 00:32:27 -0800

    Update hazeover.rb

--- Casks/hazeover.rb
+++ Casks/hazeover.rb
@@ -3,7 +3,7 @@ cask "hazeover" do
     version "1.8.9,1050,10.13"
     sha256 "d6b18a0a4c45e809cc9dd168626bbab6be378ed9ba38c8bde16c39fa44239adf"
 
-    url "https://hazeover.com/get/HazeOver_#{version.after_colon}.dmg"
+    url "https://hazeover.com/get/HazeOver_#{version.csv.third}.dmg"
 
     livecheck do
       skip "Legacy version for Big Sur and below"
