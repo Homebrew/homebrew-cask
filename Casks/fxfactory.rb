878cf083af2b2a41eac6d860ce1a6fdab428e0a4
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-25 15:47:24 +0800

    Update fxfactory.rb

--- Casks/fxfactory.rb
+++ Casks/fxfactory.rb
@@ -2,7 +2,7 @@ cask "fxfactory" do
   version "7.2.6,6943"
   sha256 "b565a889b2878cc4cca17d2e38597a3628902be7eb775dca3bd94f94808ec8d2"
 
-  url "https://fxfactory.s3.amazonaws.com/noiseindustries/fxfactory/FxFactory_#{version.major_minor_patch.no_dots}_#{version.after_comma}.zip",
+  url "https://fxfactory.s3.amazonaws.com/noiseindustries/fxfactory/FxFactory_#{version.before_comma.no_dots}_#{version.after_comma}.zip",
       verified: "fxfactory.s3.amazonaws.com/noiseindustries/"
   name "FxFactory"
   desc "Browse, install and purchase effects and plugins from a huge catalog"
@@ -13,17 +13,13 @@ cask "fxfactory" do
     regex(%r{href=.*?/fxfactory-(\d+(?:\.\d+)+)\.pkg}i)
   end
 
-  pkg "Install FxFactory #{version.major_minor_patch}.pkg"
+  pkg "Install FxFactory #{version.before_comma}.pkg"
 
-  uninstall pkgutil: [
-     "com.fxfactory.pkg",
-   ]
-
-  zap trash: [
-    "/Library/Application Support/FxFactory",
-    "/Library/LaunchDaemons/com.fxfactory.FxFactory.helper.plist",
-    "/Library/PrivilegedHelperTools/com.fxfactory.FxFactory.helper",
-    "~/Documents/FxFactory_726_6943.zip",
-    "~/Documents/Install FxFactory 7.2.6.pkg",
-  ]
+  uninstall pkgutil:   "com.fxfactory.pkg",
+            launchctl: "com.fxfactory.FxFactory.helper"
+            delete:    [
+              "/Library/Application Support/FxFactory",
+              "/Library/LaunchDaemons/com.fxfactory.FxFactory.helper.plist",
+              "/Library/PrivilegedHelperTools/com.fxfactory.FxFactory.helper",
+            ]
 end
