3d376dbf5e7964e539acb82f3aa3ad4c335f3911
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-25 11:52:14 +0800

    Update pushplaylabs-sidekick.rb

--- Casks/pushplaylabs-sidekick.rb
+++ Casks/pushplaylabs-sidekick.rb
@@ -6,8 +6,8 @@ cask "pushplaylabs-sidekick" do
     version "94.13.2.14440,f15ef51"
     sha256 "5ac5ed226074550d6df863c89c481067e70f39e68fdf98492437f99028dcbf46"
   else
-    version "94.13.2.14309,d50457d"
-    sha256 "c80fe9037c05abb1004922954e4b8c58ec62c0c5f0a1839b93e7ad31bedff5e1"
+    version "94.13.2.14442,5a79b42"
+    sha256 "0c7411a02be7a673c03d5df30075b3f5bc516431bbf876f44c6c38daab21ed74"
   end
 
   url "https://sidekick-cdn-production.meetsidekick.com/builds/sidekick-mac-release-#{arch}-#{version.before_comma}-#{version.after_comma}-df.dmg"
