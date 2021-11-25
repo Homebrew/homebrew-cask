e4e6da27590cda22f628a8ef057a482a53d60972
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-25 16:38:48 +0800

    Update burp-suite-professional.rb

--- Casks/burp-suite-professional.rb
+++ Casks/burp-suite-professional.rb
@@ -1,6 +1,6 @@
 cask "burp-suite-professional" do
-  version "2021.10.1"
-  sha256 "1dda661700871cd0917ff9a56d4094ed6473dad0503444380bee23706214e31b"
+  version "2021.10"
+  sha256 "8b857b3cce7a484b12406c9f75d8f39c33864572d6c8d87350d08d72f54eb350"
 
   url "https://portswigger.net/burp/releases/download?product=pro&version=#{version}&type=MacOsx"
   name "Burp Suite Professional"
@@ -8,13 +8,34 @@ cask "burp-suite-professional" do
   homepage "https://portswigger.net/burp/pro"
 
   livecheck do
-    url "https://portswigger.net/burp/releases/professional/latest"
-    strategy :header_match do |headers|
-      headers["location"][%r{/professional[._-]community[._-]v?(\d+(?:-\d+)+)\?}i, 1].tr("-", ".")
+    url "https://portswigger.net/burp/releases/data"
+    strategy :page_match do |page|
+      all_versions = JSON.parse(page)["ResultSet"]["Results"]
+      next if all_versions.blank?
+
+      all_versions.map do |item|
+        item["version"] if
+              item["releaseChannels"].include?("Stable") &&
+              item["categories"].include?("Professional") &&
+              item["builds"].any? do |build|
+                build["ProductPlatform"] == "MacOsx"
+              end
+      end.compact
     end
   end
 
-  app "Burp Suite Professional.app"
+  installer script: {
+    executable: "Burp Suite Professional Installer.app/Contents/MacOS/JavaApplicationStub",
+    args:       ["-q"],
+    sudo:       true,
+  }
+
+  postflight do
+    set_ownership "/Applications/Burp Suite Professional.app"
+    set_permissions "/Applications/Burp Suite Professional.app", "a+rX"
+  end
+
+  uninstall delete: "/Applications/Burp Suite Professional.app"
 
   zap trash: "~/.BurpSuite"
 end
