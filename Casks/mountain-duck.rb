714b0be289859b5d65cd48d1e585bf79a676cde8
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-26 06:48:29 +0800

    Update mountain-duck.rb

--- Casks/mountain-duck.rb
+++ Casks/mountain-duck.rb
@@ -2,7 +2,7 @@ cask "mountain-duck" do
   version "4.9.0,18884"
   sha256 "9787859b90b1a9509e259e01dab2f1e7bcb8b65079808fc4fa7bdf1f8425f762"
 
-  url "https://dist.mountainduck.io/Mountain%20Duck-#{version.before_comma}.#{version.after_comma}.zip"
+  url "https://dist.mountainduck.io/Mountain%20Duck-#{version.csv.first}.#{version.csv.second}.zip"
   name "Mountain Duck"
   desc "Mounts servers and cloud storages as a disk on the desktop"
   homepage "https://mountainduck.io/"
@@ -22,7 +22,7 @@ cask "mountain-duck" do
     "~/Library/Caches/io.mountainduck",
     "~/Library/Containers/io.mountainduck.findersync",
     "~/Library/Group Containers/G69SCX94XU.duck",
-    "~/Library/Preferences/io.mountainduck.plist",
     "~/Library/Preferences/G69SCX94XU.duck.plist",
+    "~/Library/Preferences/io.mountainduck.plist",
   ]
 end
