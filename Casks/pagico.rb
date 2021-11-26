ac569b35b5e01187400fd0aa96e592133786a7a0
neutric <5984479+neutric@users.noreply.github.com>
2021-11-26 08:52:15 +0100

    pagico: update url

--- Casks/pagico.rb
+++ Casks/pagico.rb
@@ -2,7 +2,7 @@ cask "pagico" do
   version "10.0,20211124"
   sha256 "cc44123c40a7cc844211000c2f1bfc08438c00cd7803c4d88b557c74b56c4c46"
 
-  url "https://www.pagico.com/downloads/Pagico_macOS_r#{version.after_comma}.dmg"
+  url "https://www.pagico.com/downloads/Pagico_macOS_r#{version.csv.second}.dmg"
   name "Pagico"
   desc "Manage all your tasks, files, and notes"
   homepage "https://www.pagico.com/"
