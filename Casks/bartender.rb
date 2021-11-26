25e67cd49c2947287430d78adb3bee99f3d75f42
ZepDonald <63632671+ZepDonald@users.noreply.github.com>
2021-11-27 00:22:07 +0100

    Bumped version of Bartender to 4.1.21,41021 (#114959)
    * Bumped version of Bartender to 4.1.21,41021
    
    * Update bartender.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/bartender.rb
+++ Casks/bartender.rb
@@ -7,11 +7,11 @@ cask "bartender" do
   elsif MacOS.version <= :catalina
     version "3.1.25,31125"
     sha256 "bd31aa2aab3262ed08f5c421d504b82893b5dfc546f37fc98eae23bc47494c8a"
-    url "https://macbartender.com/B2/updates/#{version.before_comma.dots_to_hyphens}/Bartender%20#{version.major}.zip",
+    url "https://macbartender.com/B2/updates/#{version.csv.first.dots_to_hyphens}/Bartender%20#{version.major}.zip",
         referer: "https://www.macbartender.com"
   else
-    version "4.1.20,41020"
-    sha256 "6ae47a34fbfbc662d0aea947c232b9583d7ca5d9936e085d9a4aaad75fe83c60"
+    version "4.1.21,41021"
+    sha256 "87bf7e3861a359ff703423efd6a3a2134261268426619c05ef90ef189d2f7804"
     url "https://macbartender.com/B2/updates/#{version.csv.first.major}-#{version.csv.first.minor}-#{version.csv.first.patch.rjust(2, "0")}/Bartender%20#{version.major}.dmg",
         referer: "https://www.macbartender.com"
   end
