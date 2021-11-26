6d6ed5af26de028f35e07c32daaa7f6256b898d6
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-27 07:27:53 +0800

    Update volley.rb

--- Casks/volley.rb
+++ Casks/volley.rb
@@ -1,12 +1,17 @@
 cask "volley" do
-  version "1.0.2"
+  version "1.0.63"
   sha256 :no_check
 
   url "https://pieces.volley.app/downloads/Volley.dmg",
       verified: "pieces.volley.app/downloads/"
   name "Volley"
-  desc "Marco Pollo like app built for quic asynchronous team communication"
+  desc "Asynchronous video messaging app"
   homepage "https://www.volleyapp.com/"
+
+  livecheck do
+    url "https://s3.amazonaws.com/pieces.volley.app/downloads/latest-mac.yml"
+    strategy :electron_builder
+  end
 
   app "Volley.app"
 end
