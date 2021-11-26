74efb298318857094e144b15e6dd554d3cc4d678
Yugoslavskiy Daniil <yugoslavskiy@gmail.com>
2021-11-26 14:42:53 +0100

    add manasecurity.com: macOS vulnerability management for individuals

---
+++ Casks/mana-security.rb
@@ -0,0 +1,19 @@
+cask "mana-security" do
+  version "2.4.0"
+  sha256 :no_check
+
+  url "https://download.manasecurity.com/"
+  name "Mana Security"
+  desc "macOS vulnerability management for individuals"
+  homepage "https://www.manasecurity.com/"
+
+  livecheck do
+    url "https://download.manasecurity.com/"
+    strategy :header_match
+  end
+
+  depends_on macos: ">= :big_sur"
+
+  app "Mana Security.app"
+
+end
