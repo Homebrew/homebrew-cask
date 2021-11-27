548a99d50f7268197e7f43f0fa4190b5c726e681
neutric <5984479+neutric@users.noreply.github.com>
2021-11-28 00:29:06 +0100

    preferencecleaner: update urls

--- Casks/preferencecleaner.rb
+++ Casks/preferencecleaner.rb
@@ -2,9 +2,9 @@ cask "preferencecleaner" do
   version "2.0"
   sha256 "800f1df5ca2519eaf66e0efc383a7a3f76018f8f8760058c3cee08b5ec75bd8e"
 
-  url "https://www.echomist.co.uk/software/downloads/PreferenceCleaner_#{version}.dmg"
+  url "https://echomist.co.uk/software/downloads/PreferenceCleaner_#{version}.dmg"
   name "PreferenceCleaner"
-  homepage "https://www.echomist.co.uk/software/PreferenceCleaner.php"
+  homepage "https://echomist.co.uk/software/PreferenceCleaner.php"
 
   livecheck do
     url :homepage
