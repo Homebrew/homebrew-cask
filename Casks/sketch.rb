48b6d2510b7adf3606a094834e688372e1cd2a8d
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-27 17:30:19 +0800

    Update sketch.rb

--- Casks/sketch.rb
+++ Casks/sketch.rb
@@ -2,7 +2,7 @@ cask "sketch" do
   version "80.1,134476"
   sha256 "2ede3107c0c4578c224a0bb32f11a483677c6b68ee09340aa8a76642ee0f267f"
 
-  url "https://download.sketch.com/sketch-#{version.before_comma}-#{version.after_comma}.zip"
+  url "https://download.sketch.com/sketch-#{version.csv.first}-#{version.csv.second}.zip"
   name "Sketch"
   desc "Digital design and prototyping platform"
   homepage "https://www.sketch.com/"
