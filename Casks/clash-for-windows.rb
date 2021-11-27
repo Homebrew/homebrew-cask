c7cd08af482b444004327730e82c9408a83447f4
SupremeSir <haofanfang@gmail.com>
2021-11-27 12:00:35 +0800

    Upgrade clash-for-windows to 0.18.10

--- Casks/clash-for-windows.rb
+++ Casks/clash-for-windows.rb
@@ -1,12 +1,12 @@
 cask "clash-for-windows" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  version "0.18.9"
+  version "0.18.10"
 
   if Hardware::CPU.intel?
-    sha256 "33ca1004ba19d425f1b1b82b1e29e42df2ac0fcce5a7ed159314b88d6faa11d1"
+    sha256 "d10c75861339fa35d7125021d4cfe0a1e78176d1bf85ec8b6acdb875235f249d"
   else
-    sha256 "0822306dec816772dc9fa0ed9ab317a4f8ca029e537edaaac2add7f1941a35cc"
+    sha256 "6b7b128a91e96b74f4a9050f5d63501a6af48f6f6d72ec2c151750f79603bb59"
   end
 
   url "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/#{version}/Clash.for.Windows-#{version}#{arch}.dmg"
