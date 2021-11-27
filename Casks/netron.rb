a68d00f1bddadeabe9a9fdbb2e70faa0465cf458
Bevan Kay <email@bevankay.me>
2021-11-27 17:05:26 +1100

    Update netron from 5.3.8 to 5.3.9 (#114986)

--- Casks/netron.rb
+++ Casks/netron.rb
@@ -1,6 +1,6 @@
 cask "netron" do
-  version "5.3.8"
-  sha256 "b20be16f5772b241c7bc462da78e940f9494dcfaafc4747929e65f70b50edba4"
+  version "5.3.9"
+  sha256 "5d44e475ecff3a369d1d66fd9574b617d6079226f6b1dc086d8d8ac211be6528"
 
   url "https://github.com/lutzroeder/netron/releases/download/v#{version}/Netron-#{version}-mac.zip"
   name "Netron"
