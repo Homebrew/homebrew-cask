51220b1678b12e03f5d413344f5f6920129990a2
Bevan Kay <email@bevankay.me>
2021-11-29 10:18:37 +1100

    Update holavpn.rb

--- Casks/holavpn.rb
+++ Casks/holavpn.rb
@@ -2,7 +2,7 @@ cask "holavpn" do
   version "2.39,1.190.697"
   sha256 "e24b3b92d08cd515413da9161e25761a5c7ff5682f864806568be0891eb4e205"
 
-  url "https://cdn4.hola.org/static/HolaVPN#{version.before_comma.major}E-#{version.before_comma}-#{version.after_comma}-noc.dmg"
+  url "https://cdn4.hola.org/static/HolaVPN#{version.csv.first.major}E-#{version.csv.first}-#{version.csv.second}-noc.dmg"
   name "Hola VPN"
   desc "Peer-to-peer VPN"
   homepage "https://hola.org/"
@@ -10,7 +10,7 @@ cask "holavpn" do
   livecheck do
     url "https://hola.org/download"
     strategy :page_match do |page|
-      page.scan(/href=.*?HolaVPN#{version.before_comma.major}E[._-]v?(\d+(?:\.\d+)+)[_-](\d+(?:\.\d+)+)-noc\.dmg/i).map do |match|
+      page.scan(/href=.*?HolaVPN#{version.csv.first.major}E[._-]v?(\d+(?:\.\d+)+)[_-](\d+(?:\.\d+)+)-noc\.dmg/i).map do |match|
         "#{match[0]},#{match[1]}"
       end
     end
@@ -18,12 +18,12 @@ cask "holavpn" do
 
   depends_on macos: ">= :catalina"
 
-  app "HolaVPN#{version.before_comma.major}E.app"
+  app "HolaVPN#{version.csv.first.major}E.app"
 
   zap trash: [
-    "~/Documents/holavpn#{version.before_comma.major}e.db*",
-    "~/Library/Caches/org.hola.hola#{version.before_comma.major}e-nosdk",
-    "~/Library/Preferences/org.hola.hola#{version.before_comma.major}e-nosdk.plist",
-    "~/Library/Saved Application State/org.hola.hola#{version.before_comma.major}e-nosdk~iosmac.savedState",
+    "~/Documents/holavpn#{version.csv.first.major}e.db*",
+    "~/Library/Caches/org.hola.hola#{version.csv.first.major}e-nosdk",
+    "~/Library/Preferences/org.hola.hola#{version.csv.first.major}e-nosdk.plist",
+    "~/Library/Saved Application State/org.hola.hola#{version.csv.first.major}e-nosdk~iosmac.savedState",
   ]
 end
