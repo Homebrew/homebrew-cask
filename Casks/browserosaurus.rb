05c1613fce5614284121e50d8e4409bccb6e12e1
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-28 10:26:55 +0800

    Update browserosaurus.rb

--- Casks/browserosaurus.rb
+++ Casks/browserosaurus.rb
@@ -3,13 +3,13 @@ cask "browserosaurus" do
 
   version "16.0.6"
 
-  url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-darwin-#{arch}-#{version}.zip"
   if Hardware::CPU.intel?
     sha256 "ebd249e841a143cf8856c2da9d1d8e2f909d4270f316e23c4db1bfc300e86729"
   else
     sha256 "69407bbc18e39a4e387d346b96b8a61a9f1846de43f104f3fd227513e26d39dc"
   end
 
+  url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-darwin-#{arch}-#{version}.zip"
   name "Browserosaurus"
   desc "Open-source browser prompter"
   homepage "https://github.com/will-stone/browserosaurus"
