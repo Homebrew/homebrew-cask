ed3a381ba39735b73f3b2065737165f339f19680
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-28 08:48:11 +0800

    Update valkyrie.rb

--- Casks/valkyrie.rb
+++ Casks/valkyrie.rb
@@ -2,11 +2,16 @@ cask "valkyrie" do
   version "2.5.6"
   sha256 "6b73e8f9eb7fca202feea27545163d08d237ea1b23289716a1d4d0928f392c0d"
 
-  url "https://github.com/NPBruce/valkyrie/releases/download/release/2.5/v#{version}/valkyrie-macos-#{version}.tar.gz",
+  url "https://github.com/NPBruce/valkyrie/releases/download/release/#{version.major_minor}/v#{version}/valkyrie-macos-#{version}.tar.gz",
       verified: "github.com/NPBruce/valkyrie"
   name "Valkyrie"
   desc "GM for Fantasy Flight Board Games"
   homepage "https://npbruce.github.io/valkyrie/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "Valkyrie.app"
 end
