435cd280cd1e6708244b43325902891545a24cb0
neutric <5984479+neutric@users.noreply.github.com>
2021-11-29 01:18:09 +0100

    Update pdfsam-basic from 4.2.7 to 4.2.8 (#115042)

--- Casks/pdfsam-basic.rb
+++ Casks/pdfsam-basic.rb
@@ -1,11 +1,11 @@
 cask "pdfsam-basic" do
-  version "4.2.7"
-  sha256 "54d78d00ab1286446d0dac88f15d9e82d442bb67a9dfb4b39bd5f2ab1ca78954"
+  version "4.2.8"
+  sha256 "87dfc43ea095b97648a0c28e2a7e3cf9cd0cc68d3ef31a7b42c26513e087afed"
 
   url "https://github.com/torakiki/pdfsam/releases/download/v#{version}/PDFsam-#{version}.dmg",
       verified: "github.com/torakiki/pdfsam/"
   name "PDFsam Basic"
-  desc "Extractas pages, splits, merges, mixes and rotates PDF files"
+  desc "Extracts pages, splits, merges, mixes and rotates PDF files"
   homepage "https://pdfsam.org/"
 
   app "PDFsam Basic.app"
