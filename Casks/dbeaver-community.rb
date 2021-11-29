8511b9e7e19015a4c7dcf8b52aba58ae74f10ab1
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-29 08:27:43 +0800

    Update dbeaver-community.rb

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -3,13 +3,13 @@ cask "dbeaver-community" do
 
   version "21.3.0"
 
-  url "https://dbeaver.io/files/#{version}/dbeaver-ce-#{version}-macos-#{arch}.dmg"
   if Hardware::CPU.intel?
     sha256 "89fb02ae1ef5b4237c2873bf73649122db406b4af23b05a7ac139e480b8bc710"
   else
     sha256 "a9ab347b43cd906f55302800d8631462919697009a1d1ed05df0db66af101103"
   end
 
+  url "https://dbeaver.io/files/#{version}/dbeaver-ce-#{version}-macos-#{arch}.dmg"
   name "DBeaver Community Edition"
   desc "Universal database tool and SQL client"
   homepage "https://dbeaver.io/"
