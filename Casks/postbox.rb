639cc24a9eec2a11a7fe7b059142af92be470694
neutric <5984479+neutric@users.noreply.github.com>
2021-11-29 01:14:48 +0100

    Update postbox from 7.0.49 to 7.0.52 (#115049)
    * Update postbox from 7.0.49 to 7.0.52
    
    * postbox: tweak livecheck regex

--- Casks/postbox.rb
+++ Casks/postbox.rb
@@ -1,6 +1,6 @@
 cask "postbox" do
-  version "7.0.49"
-  sha256 "ebd636f47002328d15011d3cba964cea3ea8d9929993373146894b0e466e2196"
+  version "7.0.52"
+  sha256 "c59789bfb6273a04e61365c402e2bd1e2fff96fdb4f8494358cff65ce90b7abb"
 
   url "https://d3nx85trn0lqsg.cloudfront.net/mac/postbox-#{version}-mac64.dmg",
       verified: "d3nx85trn0lqsg.cloudfront.net/mac/"
@@ -10,8 +10,7 @@ cask "postbox" do
 
   livecheck do
     url "https://www.postbox-inc.com/download/success-mac"
-    strategy :page_match
-    regex(%r{href=.*?/postbox-(\d+(?:\.\d+)*)-mac64\.dmg}i)
+    regex(%r{href=.*?/postbox[._-]v?(\d+(?:\.\d+)+)[._-]mac64\.dmg}i)
   end
 
   auto_updates true
