e893f8fe0ebc689916f7d4b4a100a95f0cc51da9
yugoslavskiy <daniil@yugoslavskiy.com>
2021-11-29 01:25:41 +0100

    add manasecurity.com: macOS vulnerability management for individuals (#114949)
    * add manasecurity.com: macOS vulnerability management for individuals
    
    * add sha256 manasecurity.com: macOS vulnerability management for individuals
    
    * fix description
    
    * recover sha256 :no_check`
    
    * fix issue with extra newline
    
    * add zap stanza and uninstall section as @miccal requested
    
    * add manasecurity.com: macOS vulnerability management for individuals
    
    * add sha256 manasecurity.com: macOS vulnerability management for individuals
    
    * fix description
    
    * recover sha256 :no_check`
    
    * fix issue with extra newline
    
    * add zap stanza and uninstall section as @miccal requested
    
    * add uninstall quit
    
    * Update mana-security.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

---
+++ Casks/mana-security.rb
@@ -0,0 +1,28 @@
+cask "mana-security" do
+  version "2.4.0"
+  sha256 :no_check
+
+  url "https://download.manasecurity.com/"
+  name "Mana Security"
+  desc "Vulnerability Management for individuals"
+  homepage "https://www.manasecurity.com/"
+
+  livecheck do
+    url "https://download.manasecurity.com/"
+    strategy :header_match
+  end
+
+  depends_on macos: ">= :big_sur"
+
+  app "Mana Security.app"
+
+  uninstall login_item: "Mana Security"
+
+  zap trash: [
+    "~/Library/Application Support/Mana Security/",
+    "~/Library/Caches/com.manasecurity*",
+    "~/Library/Logs/Mana Security",
+    "~/Library/Preferences/com.manasecurity*",
+    "~/Library/Saved Application State/com.manasecurity*",
+  ]
+end
