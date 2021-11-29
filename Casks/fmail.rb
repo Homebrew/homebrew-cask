a953f2fbb2bb714808cd71356802b7cf15189495
Alex Jarvis <github@alexjarvis.net>
2021-11-29 17:57:59 -0600

    Update fmail.rb
    Add version-specific URL and SHA256

--- Casks/fmail.rb
+++ Casks/fmail.rb
@@ -1,8 +1,8 @@
 cask "fmail" do
   version "2.4.5,93"
-  sha256 :no_check  # required as upstream package is updated in place, including versioned sparkle url
+  sha256 "9043e30ffe364836ca40ab913ceb46730aa2c3bca152e9146d8564d6298978f6"
 
-  url "https://arievanboxel.fr/fmail/updates/fmail.dmg"
+  url "https://arievanboxel.fr/fmail/sparkle/FMail_245.zip"
   name "FMail"
   desc "Unofficial native application for Fastmail"
   homepage "https://arievanboxel.fr/fmail/en/"
