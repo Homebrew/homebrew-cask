053184779382dd2abcc39d54f93a9a8b6f48e359
Guillaume <hi@255kb.dev>
2021-11-30 17:59:30 +0100

    Update mockoon to v1.17.0 Add Mockoon zap trash info

--- Casks/mockoon.rb
+++ Casks/mockoon.rb
@@ -1,14 +1,21 @@
 cask "mockoon" do
-  version "1.16.0"
-  sha256 "1021e9e6a34729186524b3b00abc8a6b8509cd2bc06f765d516f3b18a984c0da"
+  version "1.17.0"
+  sha256 "4c639e3e3d2c5e7239c79e46b3c5b4fab3f685e612ac64535fe719101f64c85e"
 
   url "https://github.com/mockoon/mockoon/releases/download/v#{version}/mockoon.setup.#{version}.dmg",
       verified: "github.com/mockoon/mockoon/"
   name "Mockoon"
-  desc "Utility to run mock APIs locally"
+  desc "Easily create mock APIs locally."
   homepage "https://mockoon.com/"
 
   auto_updates true
 
   app "Mockoon.app"
+  
+  zap trash: [
+    "~/Library/Application Support/mockoon",
+    "~/Library/Logs/Mockoon",
+    "~/Library/Preferences/com.mockoon.app.plist",
+    "~/Library/Saved Application State/com.mockoon.app.savedState"
+  ]
 end
