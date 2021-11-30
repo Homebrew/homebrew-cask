19eb2d9191500e9ba1548eb1cde0a89b9e7aa95f
neutric <5984479+neutric@users.noreply.github.com>
2021-11-30 01:29:20 +0100

    Update profind from 1.9.2 to 1.9.3 (#115086)
    * Update profind from 1.9.2 to 1.9.3
    
    * profind: add desc
    
    * profind: add zap

--- Casks/profind.rb
+++ Casks/profind.rb
@@ -1,9 +1,10 @@
 cask "profind" do
-  version "1.9.2,1921"
-  sha256 "eb21a5a769a7bd29c1411aa86578d6bf3da67748d18b9cb146f1638d7db5abe1"
+  version "1.9.3,1930"
+  sha256 "37e53e1f371c165c8e10ad719f95ccb837b6b7ce6015ea610c5ea1d4d06ea3a0"
 
-  url "https://www.zeroonetwenty.com/profind/downloads/ProFind#{version.before_comma.no_dots}.dmg"
+  url "https://www.zeroonetwenty.com/profind/downloads/ProFind#{version.csv.first.no_dots}.dmg"
   name "ProFind"
+  desc "File search app"
   homepage "https://www.zeroonetwenty.com/profind/"
 
   livecheck do
@@ -14,4 +15,12 @@ cask "profind" do
   depends_on macos: ">= :high_sierra"
 
   app "ProFind.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.zeroonetwenty.ProFind",
+    "~/Library/Application Support/com.zeroonetwenty.ProFind",
+    "~/Library/Caches/com.zeroonetwenty.ProFind",
+    "~/Library/Preferences/com.zeroonetwenty.ProFind.plist",
+    "~/Library/Saved Application State/com.zeroonetwenty.ProFind.savedState",
+  ]
 end
