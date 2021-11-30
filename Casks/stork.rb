67b4116aaab974e3f780a1d36c88a566b8470ca2
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-30 18:17:05 +0800

    Update stork.rb

--- Casks/stork.rb
+++ Casks/stork.rb
@@ -2,7 +2,7 @@ cask "stork" do
   version "1.0.0,1290"
   sha256 "7dff76f84a45dbe85cb90af97aee9b0c17f98b63375ff64793043d358816df2c"
 
-  url "https://downloads.stork.ai/macos/release/Stork-#{version.before_comma}.#{version.after_comma}.dmg"
+  url "https://downloads.stork.ai/macos/release/Stork-#{version.csv.first}.#{version.csv.second}.dmg"
   name "Stork for macOS"
   desc "Messenger with a powerful features for hybrid work"
   homepage "https://stork.ai/"
