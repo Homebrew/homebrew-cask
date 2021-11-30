ff69aeb5dbedf67561c7824e7e494ac6cb689f38
Stephen <stephengroat@users.noreply.github.com>
2021-11-30 11:32:15 -0800

    Add AppFlowy 0.0.2

---
+++ Casks/appflowy.rb
@@ -0,0 +1,12 @@
+cask "appflowy" do
+  version "0.0.2"
+  sha256 "3d5177015cc568eab83a2e1d4ff71fe28ad31d63834dc3aa32c23742b09b6b29"
+
+  url "https://github.com/AppFlowy-IO/appflowy/releases/download/#{version}/AppFlowy-MacOS.zip",
+      verified: "github.com/AppFlowy-IO/appflowy/"
+  name "appflowy"
+  desc "Open-source alternative to Notion"
+  homepage "https://www.appflowy.io/"
+
+  app "AppFlowy.app"
+end
