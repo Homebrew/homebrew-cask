dd66a0553680f5b8c8c5df39049d2c43025b6d1a
Alex Jarvis <github@alexjarvis.net>
2021-11-29 18:22:07 -0600

    Update fmail from 2.4.4 to 2.4.5 (#115073)
    * Update fmail from 2.4.4 to 2.4.5
    
    * Update fmail.rb
    
    * Update fmail.rb
    
    Add version-specific URL and SHA256
    
    * Update fmail.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/fmail.rb
+++ Casks/fmail.rb
@@ -1,8 +1,8 @@
 cask "fmail" do
-  version "2.4.4,92"
-  sha256 :no_check
+  version "2.4.5,93"
+  sha256 "9043e30ffe364836ca40ab913ceb46730aa2c3bca152e9146d8564d6298978f6"
 
-  url "https://arievanboxel.fr/fmail/updates/fmail.dmg"
+  url "https://arievanboxel.fr/fmail/sparkle/FMail_#{version.csv.first.no_dots}.zip"
   name "FMail"
   desc "Unofficial native application for Fastmail"
   homepage "https://arievanboxel.fr/fmail/en/"
