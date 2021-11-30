f980ce725f4aae5499073a3c4817e16be9fcc42b
Bevan Kay <email@bevankay.me>
2021-11-30 21:39:53 +1100

    Add Grammarly Desktop App 1.0.0.5

---
+++ Casks/grammarly-desktop.rb
@@ -0,0 +1,22 @@
+cask "grammarly-desktop" do
+  version "1.0.0.5"
+  sha256 :no_check
+
+  url "https://download-mac.grammarly.com/Grammarly.dmg"
+  name "Grammarly Desktop"
+  desc "Grammarly for desktop"
+  homepage "https://www.grammarly.com/desktop"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
+
+  app "Grammarly Installer.app", target: "Grammarly Desktop.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.grammarly.ProjectLlama",
+    "~/Library/Caches/com.grammarly.ProjectLlama",
+    "~/Library/Preferences/com.grammarly.ProjectLlama.plist",
+  ]
+end
