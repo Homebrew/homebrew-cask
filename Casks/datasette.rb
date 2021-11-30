0891a464267b0fa1932d3e2ebce29e8352cada10
Bevan Kay <email@bevankay.me>
2021-11-30 11:15:13 +1100

    rename datasette-desktop to datasette

--- Casks/datasette.rb
+++ Casks/datasette.rb
@@ -1,9 +1,9 @@
-cask "datasette-desktop" do
+cask "datasette" do
   version "0.2.1"
   sha256 "76325f6ef6342de922ce49bcbc2e68f5d05bd17534f9a765a20847c6a4d5297b"
 
   url "https://github.com/simonw/datasette-app/releases/download/#{version}/Datasette-mac.app.zip"
-  name "Datasette Desktop"
+  name "Datasette"
   desc "Desktop application that wraps Datasette"
   homepage "https://github.com/simonw/datasette-app"
 
