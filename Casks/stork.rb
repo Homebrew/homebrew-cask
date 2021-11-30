3ca7e76b9717e70f7a4499f1e2325d55e6b44666
Pimenov Sergey <pimenov.sergei@gmail.com>
2021-11-29 23:55:00 -0800

    revert version preprocessing

--- Casks/stork.rb
+++ Casks/stork.rb
@@ -2,16 +2,14 @@ cask "stork" do
   version "1.0.0,1290"
   sha256 "7dff76f84a45dbe85cb90af97aee9b0c17f98b63375ff64793043d358816df2c"
 
-  url "https://downloads.stork.ai/macos/release/Stork-#{version.before_comma}.#{version.after_comma.delete("^0-9")}.dmg"
+  url "https://downloads.stork.ai/macos/release/Stork-#{version.before_comma}.#{version.after_comma}.dmg"
   name "Stork for macOS"
   desc "Messenger with a powerful features for hybrid work"
   homepage "https://stork.ai/"
 
   livecheck do
     url "https://downloads.stork.ai/macos/release/app_cast.xml"
-    strategy :sparkle do |item|
-      "#{item.short_version},#{item.version.delete("^0-9")}"
-    end
+    strategy :sparkle
   end
 
   auto_updates true
