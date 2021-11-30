ddde41a589efb1a593709f73f2d731b3a6152860
Darren Kavanagh <dkav@users.noreply.github.com>
2021-11-29 16:57:56 -0800

    Remove auto_updates flag (#115078)
    * Remove auto_updates flag
    
    Auto updates is not working for Gimp. See https://gitlab.gnome.org/GNOME/gimp/-/issues/7325 for more info. This change reverts https://github.com/Homebrew/homebrew-cask/commit/3cc865bcd435aac4f1da800d651d79096bda5f00#diff-74a1d191600404f203e12426fa83a0a4d7db3e09cb490b902806b161491c84d6
    
    * Update gimp.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/gimp.rb
+++ Casks/gimp.rb
@@ -11,10 +11,8 @@ cask "gimp" do
   livecheck do
     url "https://www.gimp.org/downloads/"
     strategy :page_match
-    regex(%r{href=.*?/gimp-(\d+(?:\.\d+)*)-x86_64\.dmg}i)
+    regex(%r{href=.*?/gimp-(\d+(?:\.\d+)+)-x86_64\.dmg}i)
   end
-
-  auto_updates true
 
   app "GIMP-#{version.major_minor}.app"
   binary "#{appdir}/GIMP-#{version.major_minor}.app/Contents/MacOS/gimp"
@@ -24,8 +22,8 @@ cask "gimp" do
   end
 
   zap trash: [
-    "~/Library/Preferences/org.gimp.gimp-#{version.major_minor}:.plist",
     "~/Library/Application Support/Gimp",
+    "~/Library/Preferences/org.gimp.gimp-#{version.major_minor}:.plist",
     "~/Library/Saved Application State/org.gimp.gimp-#{version.major_minor}:.savedState",
   ]
 end
