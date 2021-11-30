3cdc3330b71bad28c94705465de1cedbf747b79b
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-30 09:03:05 +0800

    Update datasette.rb

--- Casks/datasette.rb
+++ Casks/datasette.rb
@@ -2,10 +2,11 @@ cask "datasette" do
   version "0.2.1"
   sha256 "76325f6ef6342de922ce49bcbc2e68f5d05bd17534f9a765a20847c6a4d5297b"
 
-  url "https://github.com/simonw/datasette-app/releases/download/#{version}/Datasette-mac.app.zip"
+  url "https://github.com/simonw/datasette-app/releases/download/#{version}/Datasette-mac.app.zip",
+      verified: "github.com/simonw/datasette-app/"
   name "Datasette"
   desc "Desktop application that wraps Datasette"
-  homepage "https://github.com/simonw/datasette-app"
+  homepage "https://datasette.io/desktop"
 
   app "Datasette.app"
 
