c8f63466589c6d91319e2b7d7b900c66001f2873
Guillaume <hi@255kb.dev>
2021-12-01 17:00:19 +0100

    Replace Mockoon v1.17.0 binary by universal package

--- Casks/mockoon.rb
+++ Casks/mockoon.rb
@@ -1,8 +1,8 @@
 cask "mockoon" do
   version "1.17.0"
-  sha256 "4c639e3e3d2c5e7239c79e46b3c5b4fab3f685e612ac64535fe719101f64c85e"
+  sha256 "4805ed66883ba0565f589e63b99c1e3c149308484cb30a369e4e879d1d974885"
 
-  url "https://github.com/mockoon/mockoon/releases/download/v#{version}/mockoon.setup.#{version}.dmg",
+  url "https://github.com/mockoon/mockoon/releases/download/v#{version}/mockoon.setup.#{version}.universal.dmg",
       verified: "github.com/mockoon/mockoon/"
   name "Mockoon"
   desc "Create mock APIs locally"
