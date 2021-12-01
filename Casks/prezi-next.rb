105ca7e23f3ce8a43effbb0fdca9819a2f7ffe9a
neutric <5984479+neutric@users.noreply.github.com>
2021-12-01 12:17:40 +0100

    Update prezi-next from 1.52.0 to 1.53.1 (#115129)

--- Casks/prezi-next.rb
+++ Casks/prezi-next.rb
@@ -1,8 +1,8 @@
 cask "prezi-next" do
-  version "1.52.0,27050"
-  sha256 "07e7c3fea8ba11593a427b139919133c34d9c00ed7da8df9d44373b2f4a22b39"
+  version "1.53.1,27105"
+  sha256 "51dfc718cda334b028843c50e4f905102a8bb1f8cdc362d849c32f7c255fe8b8"
 
-  url "https://desktopassets.prezi.com/mac/pitch/releases/Prezi_Next_#{version.before_comma}.dmg"
+  url "https://desktopassets.prezi.com/mac/pitch/releases/Prezi_Next_#{version.csv.first}.dmg"
   name "Prezi Next"
   desc "Presentation software"
   homepage "https://prezi.com/"
