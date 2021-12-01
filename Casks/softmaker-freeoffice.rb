3fe1f672869ec64562bf17bc61254512b2647c3e
Bevan Kay <email@bevankay.me>
2021-12-01 22:09:01 +1100

    Replace shasum with `:no_check`

--- Casks/softmaker-freeoffice.rb
+++ Casks/softmaker-freeoffice.rb
@@ -1,6 +1,6 @@
 cask "softmaker-freeoffice" do
   version "2021"
-  sha256 "721c5bc20eb343f87f5c373e61cb4a20f619fe1a98d61b921d75f7da8bcac593"
+  sha256 :no_check # required as upstream package is updated in place
 
   url "https://www.softmaker.net/down/softmaker-freeoffice-#{version}.pkg",
       verified: "softmaker.net/"
