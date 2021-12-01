079c760750f7b10bdd19d42f9404ec60860c70ec
Bevan Kay <email@bevankay.me>
2021-12-02 09:00:07 +1100

    rename retrace.rb

---
+++ Casks/re-trace.rb
@@ -0,0 +1,15 @@
+cask "re-trace" do
+    version "1.0.1-beta.0"
+    sha256 "28db7777ae1b63b875eb7d6ceeed2a677cbcff55960375c0af22ea5e044e42ad"
+
+    url "https://github.com/Staffbase/retrace/releases/download/v#{version}/RE-Trace-#{version}.dmg"
+    name "RE-Trace"
+    desc "Micro-journaling app"
+    homepage "https://github.com/Staffbase/retrace/"
+  
+    depends_on macos: ">= :sierra"
+    
+    app "RE-Trace.app"
+  
+    zap trash: "~/Library/Application Support/RE-Trace"
+  end
