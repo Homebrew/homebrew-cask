cfaafb8767ff696fd2e8d0bd89ab9f7118b578b7
Charles Siu <chunheisiu@gmail.com>
2021-12-01 23:14:26 +0800

    Update feishu from 5.0.9 to 5.1.5

--- Casks/feishu.rb
+++ Casks/feishu.rb
@@ -5,8 +5,8 @@ cask "feishu" do
     version "5.1.5,2b840e"
     sha256 "afa2572a5a6b9998e2ce791cec2a3de71d65cee24f9b304064c4f0ce6dfbf551"
   else
-    version "5.0.9,fd8d60"
-    sha256 "f3a4711ddf7c95c63c3ec45842c7726856da4ac6619653a7bbd9f62644711b38"
+    version "5.1.5,e462a1"
+    sha256 "5fa04655c2819796b23e1578e9bafe78017acab64441ea9ad533699e333dd58a"
   end
 
   url "https://sf3-cn.feishucdn.com/obj/ee-appcenter/#{version.after_comma}/Feishu-darwin_#{arch}-#{version.before_comma}-signed.dmg",
