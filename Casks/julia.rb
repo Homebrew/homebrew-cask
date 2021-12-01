b4d6886287e450732f7e64ee6a4697c4731ad70c
Bevan Kay <email@bevankay.me>
2021-12-01 20:02:18 +1100

    update conditionals

--- Casks/julia.rb
+++ Casks/julia.rb
@@ -1,17 +1,19 @@
 cask "julia" do
+  arch = Hardware::CPU.intel? ? "mac64" : "macaarch64"
+  arch_folder = Hardware::CPU.intel? ? "x64" : "aarch64"
+
   version "1.7.0"
 
+  if Hardware::CPU.intel?
+    sha256 "9a7919448e13ba9cefb0f0fe8178ca089333c86e2722f1e482a1dc8c0e2f03b6"
+  else
+    sha256 "6852aab9a40a3265551eb85ad19ff16c3ba5410c852f5e7949972cb9911d473a"
+  end
+
+  url "https://julialang-s3.julialang.org/bin/mac/#{arch_folder}/#{version.major_minor}/julia-#{version}-#{arch}.dmg"
   name "Julia"
   desc "Programming language for technical computing"
   homepage "https://julialang.org/"
-
-  if Hardware::CPU.arm?
-    url "https://julialang-s3.julialang.org/bin/mac/aarch64/macaarch64/julia-#{version}-macaarch64.dmg"
-    sha256 "6852aab9a40a3265551eb85ad19ff16c3ba5410c852f5e7949972cb9911d473a"
-  else
-    url "https://julialang-s3.julialang.org/bin/mac/x64/#{version.major_minor}/julia-#{version}-mac64.dmg"
-    sha256 "9a7919448e13ba9cefb0f0fe8178ca089333c86e2722f1e482a1dc8c0e2f03b6"
-  end
 
   livecheck do
     url "https://julialang.org/downloads/"
