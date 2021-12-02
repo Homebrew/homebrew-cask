35b29effeec75a25bcbf1303365d1ae1e6da5479
neutric <5984479+neutric@users.noreply.github.com>
2021-12-02 13:16:14 +0100

    Update quickbooks from 20.0.5.866 to 20.0.6.880 (#115176)
    * Update quickbooks from 20.0.5.866 to 20.0.6.880
    
    * quickbooks: make livecheck fail gracefully
    
    * quickbooks: add desc

--- Casks/quickbooks.rb
+++ Casks/quickbooks.rb
@@ -1,20 +1,23 @@
 cask "quickbooks" do
-  version "20.0.5.866,2021"
+  version "20.0.6.880,2021"
   sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://http-download.intuit.com/http.intuit/Downloads/#{version.after_comma}/Latest/QuickBooksMac#{version.after_comma}.dmg"
+  url "https://http-download.intuit.com/http.intuit/Downloads/#{version.csv.second}/Latest/QuickBooksMac#{version.csv.second}.dmg"
   name "QuickBooks Desktop"
+  desc "Accounting software"
   homepage "https://quickbooks.intuit.com/desktop/mac/"
 
   livecheck do
     url "https://http-download.intuit.com/http.intuit/Downloads/Mac/QuickBooks/020/66E6085F-B670-45F7-88DB-D19C9CBD5855/app/appcast.xml"
     strategy :sparkle do |item|
       year = item.url[%r{/QuickBooks_(\d+)\.tgz}i, 1]
+      next if year.blank?
+
       "#{item.short_version},#{year}"
     end
   end
 
   depends_on macos: ">= :mojave"
 
-  app "QuickBooks #{version.after_comma}.app"
+  app "QuickBooks #{version.csv.second}.app"
 end
