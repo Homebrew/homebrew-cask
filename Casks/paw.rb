802562cece67b5d0eba34633e2e3a3788a14b5b0
Bevan Kay <email@bevankay.me>
2021-12-02 23:26:00 +1100

    Update paw.rb

--- Casks/paw.rb
+++ Casks/paw.rb
@@ -2,7 +2,7 @@ cask "paw" do
   version "3.3.2,3003002002"
   sha256 "a4bccd7646be6d310c44e3f74dbe8f3ace63dd00b02bd7fe8dbf5bc6286f3f7b"
 
-  url "https://cdn-builds.paw.cloud/paw/Paw-#{version.before_comma}.zip"
+  url "https://cdn-builds.paw.cloud/paw/Paw-#{version.csv.first}.zip"
   name "Paw"
   desc "HTTP client that helps testing and describing APIs"
   homepage "https://paw.cloud/"
