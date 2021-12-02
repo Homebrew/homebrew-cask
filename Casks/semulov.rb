94b1e56c5924a130683cd27ebcd6b58b77ae7c82
Minku Lee <public@premi.st>
2021-12-03 00:06:27 +0900

    Update Semulov from 2.4.1 to 2.6.0

--- Casks/semulov.rb
+++ Casks/semulov.rb
@@ -1,11 +1,11 @@
 cask "semulov" do
-  version "2.4.1"
-  sha256 :no_check
+  version "2.6.0"
+  sha256 "fc25b015251562df79c89143b2fb123fc793d780e3f1eb9990d6d5723c870dc6"
 
-  url "https://kainjow.com/downloads/Semulov.zip"
+  url "https://github.com/kainjow/Semulov/releases/download/v2.6/Semulov.zip"
   name "Semulov"
   desc "Access mounted and unmounted volumes from the menubar"
-  homepage "https://kainjow.com/"
+  homepage "https://github.com/kainjow/Semulov"
 
   livecheck do
     url "https://kainjow.com/updates/semulov.xml"
