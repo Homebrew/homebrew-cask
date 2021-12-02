085c296f71206a4d2d33b3feaa852f24adad9f41
Salomon Popp <hi@salomonpopp.me>
2021-12-02 13:33:14 +0100

    Update paw from 3.3.1 to 3.3.2 (#115199)
    * Update paw from 3.3.1 to 3.3.2
    
    * Add sparkle version
    
    * Update paw.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/paw.rb
+++ Casks/paw.rb
@@ -1,8 +1,8 @@
 cask "paw" do
-  version "3.3.1,3003001001"
-  sha256 "b04f5b3c104ae097d4463160a99e297091461a63690745923a4cd738ab62641a"
+  version "3.3.2,3003002002"
+  sha256 "a4bccd7646be6d310c44e3f74dbe8f3ace63dd00b02bd7fe8dbf5bc6286f3f7b"
 
-  url "https://cdn-builds.paw.cloud/paw/Paw-#{version.before_comma}.zip"
+  url "https://cdn-builds.paw.cloud/paw/Paw-#{version.csv.first}.zip"
   name "Paw"
   desc "HTTP client that helps testing and describing APIs"
   homepage "https://paw.cloud/"
