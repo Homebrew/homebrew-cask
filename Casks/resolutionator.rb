22cac5aaebd4c0ad28c94084609c9a6355297c28
neutric <5984479+neutric@users.noreply.github.com>
2021-12-03 02:59:44 +0100

    Update resolutionator from 2.1.0 to 2.2.0 (#115226)

--- Casks/resolutionator.rb
+++ Casks/resolutionator.rb
@@ -1,8 +1,8 @@
 cask "resolutionator" do
-  version "2.1.0,118"
-  sha256 "3318c5754f935b3e052ff5c20ef33d90896b14149c367404b89bcd9e26810645"
+  version "2.2.0,122"
+  sha256 "9f2fd542957f70fd45a64aca8804399b44b12d30cc58d9dabda1b8924348769c"
 
-  url "https://manytricks.com/download/_do_not_hotlink_/resolutionator#{version.before_comma.no_dots}.dmg"
+  url "https://manytricks.com/download/_do_not_hotlink_/resolutionator#{version.csv.first.no_dots}.dmg"
   name "Resolutionator"
   desc "Use any of your display's available resolutions"
   homepage "https://manytricks.com/resolutionator/"
