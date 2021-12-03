15dbb259532c8908f9cab15518ef41dedfb0e447
shensven <shensven2016@gmail.com>
2021-12-03 22:48:21 +0800

    Update flipper from 0.122.0 to 0.123.0 (Manually)

--- Casks/flipper.rb
+++ Casks/flipper.rb
@@ -1,6 +1,6 @@
 cask "flipper" do
-  version "0.122.0"
-  sha256 "1c5216a503f14dd515d6da5bd19ac23764e9171e98aa871a7744e7f8cd025456"
+  version "0.123.0"
+  sha256 "f450830ddf4c2394758dde492ba07d43bcf1e32e836a0bee694f0e56edf80951"
 
   url "https://github.com/facebook/flipper/releases/download/v#{version}/Flipper-mac.dmg",
       verified: "github.com/facebook/flipper/"
