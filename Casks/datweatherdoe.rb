e8a5ff4d0c843273342c8162f33a273cc96a32f4
Inder Dhir <inderdhir@hotmail.com>
2021-12-02 22:40:14 -0500

    Update datweatherdoe from 2.1.5 to 2.1.6

--- Casks/datweatherdoe.rb
+++ Casks/datweatherdoe.rb
@@ -1,6 +1,6 @@
 cask "datweatherdoe" do
-  version "2.1.5"
-  sha256 "79255736cda1e416366fe2953fbfe10419cc842772cd9060c9e041fef4e729a0"
+  version "2.1.6"
+  sha256 "9ffc97df11876a28b87aa11bc511c930fc79c02ee9512f7eb9e68aca75246bf4"
 
   url "https://github.com/inderdhir/DatWeatherDoe/releases/download/#{version}/DatWeatherDoe-#{version}.dmg"
   name "DatWeatherDoe"
