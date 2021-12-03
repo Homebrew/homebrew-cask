ebf5c86ff9248381c3a08e4d02f9cbad456ac4c2
Fuad Saud <fuadfsaud@gmail.com>
2021-12-03 11:07:38 -0300

    Add SpaceId.app v1.4

---
+++ Casks/spaceid.rb
@@ -0,0 +1,25 @@
+cask "spaceid" do
+  version "1.4"
+  sha256 "6b867f758b2037f2c5f908ec0311d73a7e47f4da179cdde952c914fd2b33528e"
+
+  url "https://github.com/dshnkao/SpaceId/releases/download/v#{version}/SpaceId.app.zip"
+  name "spaceid"
+  desc "Menu bar indicator showing the currently selected space"
+  homepage "https://github.com/dshnkao/SpaceId/"
+
+  livecheck do
+    url "https://github.com/dshnkao/SpaceId/releases.atom"
+    strategy :page_match
+    regex(%r{href="https://github.com/dshnkao/SpaceId/releases/tag/v?(.*?)"})
+  end
+
+  app "SpaceId.app"
+
+  preflight do
+    set_permissions "#{staged_path}/SpaceId.app", "0755"
+  end
+
+  uninstall login_item: "SpaceId"
+
+  zap trash: "~/Library/Preferences/com.dshnkao.SpaceId.plist"
+end
