31bdada6b948fb513bd20269154972fa5ef66ead
Reno <25192197+singularitti@users.noreply.github.com>
2021-12-03 00:08:41 -0500

    Update koodo-reader from 1.3.7 to 1.3.8 (#115241)

--- Casks/koodo-reader.rb
+++ Casks/koodo-reader.rb
@@ -1,12 +1,12 @@
 cask "koodo-reader" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  version "1.3.7"
+  version "1.3.8"
 
   if Hardware::CPU.intel?
-    sha256 "afcace5634f004dc142a4c1183d94bd0672f850093ddd03f164053fe18e8facd"
+    sha256 "3cac7b590a391bbf5940aee782072ad440676ceefba88a7eb5835ff1ef548895"
   else
-    sha256 "9fb2bd33e46403dd04ef60788d32b66c356dbe273be8f6e5ef6cbbe42bf1d29f"
+    sha256 "0733eb7924997928fc9a869c15f6aaa2d6421f67a8a6a78f68a4bc2a2b1e8216"
   end
 
   url "https://github.com/troyeguo/koodo-reader/releases/download/v#{version}/Koodo-Reader-#{version}#{arch}.dmg",
