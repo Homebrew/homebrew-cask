825bc1a88e5e392682349b328d0761b11933d714
Ömer Balyali <omerbalyali@gmail.com>
2021-12-03 14:02:39 +0100

    Add Soundtoys 5 Plugins v5.3.6

---
+++ Casks/soundtoys.rb
@@ -0,0 +1,18 @@
+cask "soundtoys" do
+  version "5.3.6,16152"
+  sha256 "859676347419937c6d77c2eb47dca2b262574db056138b7028e2e3792080bb27"
+
+  url "https://storage.googleapis.com/soundtoys-download/versions/version_#{version.major}_#{version.minor}_#{version.patch}_#{version.after_comma}/Soundtoys#{version.major}_#{version.major}.#{version.minor}.#{version.patch}.#{version.after_comma}.dmg",
+      verified: "https://storage.googleapis.com/soundtoys-download/"
+  name "Soundtoys #{version.major}"
+  desc "Audio Effects Plugins"
+  homepage "https://www.soundtoys.com/product/soundtoys-#{version.major}/"
+
+  livecheck do
+    skip "No version information available"
+  end
+
+  pkg "Install Soundtoys #{version.major}.pkg"
+
+  uninstall pkgutil: "com.soundtoys.*"
+end
