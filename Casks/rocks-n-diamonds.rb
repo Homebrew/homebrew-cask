fbe0fe7f6b78a8319e9207835540984ac35978cc
neutric <5984479+neutric@users.noreply.github.com>
2021-12-05 00:07:30 +0100

    rocks-n-diamonds: add livecheck

--- Casks/rocks-n-diamonds.rb
+++ Casks/rocks-n-diamonds.rb
@@ -3,9 +3,13 @@ cask "rocks-n-diamonds" do
   sha256 "9166e69872c83561fc3d1e4025e17cc151c6ad2ebbd083505ee680f7259763a7"
 
   url "https://www.artsoft.org/RELEASES/macosx/rocksndiamonds/rocksndiamonds-#{version}.dmg"
-  appcast "https://www.artsoft.org/RELEASES/macosx/rocksndiamonds/"
   name "Rocks'n'Diamonds"
   homepage "https://www.artsoft.org/rocksndiamonds/"
+
+  livecheck do
+    url "https://www.artsoft.org/RELEASES/macosx/rocksndiamonds/"
+    regex(/href=.*?rocksndiamonds[._-]?v?(\d+(?:\.\d+)+)\.dmg/i)
+  end
 
   app "Rocks'n'Diamonds.app"
 end
