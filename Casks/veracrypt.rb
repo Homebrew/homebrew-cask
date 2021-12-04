949c565c9099fffdb6f28a5ba25346c05f056bd2
Bevan Kay <email@bevankay.me>
2021-12-04 23:46:51 +1100

    update livecheck

--- Casks/veracrypt.rb
+++ Casks/veracrypt.rb
@@ -2,16 +2,15 @@ cask "veracrypt" do
   version "1.25.4"
   sha256 "0af273555cbd82573f0f3182c178fefe61436e7260f4dc1a778efd9e786802ea"
 
-  url "https://launchpad.net/veracrypt/trunk/#{version.downcase}/+download/VeraCrypt_#{version}.dmg",
+  url "https://launchpad.net/veracrypt/trunk/#{version}/+download/VeraCrypt_#{version}.dmg",
       verified: "launchpad.net/veracrypt/trunk/"
   name "VeraCrypt"
   desc "Disk encryption software focusing on security based on TrueCrypt"
   homepage "https://www.veracrypt.fr/"
 
   livecheck do
-    url "https://github.com/veracrypt/VeraCrypt/releases"
-    strategy :github_latest
-    regex(%r{href=.*?/VeraCrypt_(\d+(?:\.\d+)*[^/]*?)\.dmg}i)
+    url "https://www.veracrypt.fr/en/Downloads.html"
+    regex(/href=.*?VeraCrypt[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
   depends_on cask: "macfuse"
