870f13e9ab4fb96d1c18de824b409a002b99e244
Reno <25192197+singularitti@users.noreply.github.com>
2021-12-03 19:24:56 -0500

    Update gluemotion from 2.0.2,fee459b0-05dd-46e6-a9c2-d2c397c298fd to 2.0.2,fee459b0-05dd-46e6-a9c2-d2c397c298fd (#115281)
    * Update gluemotion from 2.0.2,fee459b0-05dd-46e6-a9c2-d2c397c298fd to 2.0.2,fee459b0-05dd-46e6-a9c2-d2c397c298fd
    
    * Fix gluemotion.rb

--- Casks/gluemotion.rb
+++ Casks/gluemotion.rb
@@ -1,15 +1,21 @@
 cask "gluemotion" do
-  version "2.0.1,6055"
-  sha256 "b801e9fde09f34e76491621fa494fc7e85a689751ab9e7dfb144c54997c4f4ea"
+  version "2.0.2,4e1c690b-af5e-4bf3-bdeb-ca6f8cebbc66"
+  sha256 "6a80e14f6fbeb69d18fa8226c45e7d756174660d68956ad7c82c478ba4fcd6c9"
 
-  url "https://neededapps.com/appcasts/gluemotion/versions/GM-#{version.before_comma}.zip"
+  url "https://neededapps.nyc3.digitaloceanspaces.com/media/public/#{version.csv.second}.zip",
+      verified: "neededapps.nyc3.digitaloceanspaces.com"
   name "GlueMotion"
   desc "Create and correct time lapse movies"
   homepage "https://neededapps.com/gluemotion/"
 
   livecheck do
     url "https://neededapps.com/appcasts/gluemotion/changelog.xml"
-    strategy :sparkle
+    strategy :sparkle do |item|
+      hex = item.url[%r{/([\h-]+)\.zip}i, 1]
+      next if hex.blank?
+
+      "#{item.short_version},#{hex}"
+    end
   end
 
   app "GlueMotion.app"
