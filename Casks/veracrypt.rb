a65e08220e0717c7d0dffaca975477b2b18c4dfc
tiiiecherle <tiiiecherle@gmail.com>
2021-12-04 21:27:30 +0100

    Update veracrypt from 1.24-Update8 to 1.25.4 (#115299)

--- Casks/veracrypt.rb
+++ Casks/veracrypt.rb
@@ -1,17 +1,16 @@
 cask "veracrypt" do
-  version "1.24-Update8"
-  sha256 "9edeae9fbf7f1b2eb91ce55ff82481bfd4c099e85c3c88cb6d6dd91b460a3a6b"
+  version "1.25.4"
+  sha256 "0af273555cbd82573f0f3182c178fefe61436e7260f4dc1a778efd9e786802ea"
 
-  url "https://launchpad.net/veracrypt/trunk/#{version.downcase}/+download/VeraCrypt_#{version}.dmg",
+  url "https://launchpad.net/veracrypt/trunk/#{version}/+download/VeraCrypt_#{version}.dmg",
       verified: "launchpad.net/veracrypt/trunk/"
   name "VeraCrypt"
   desc "Disk encryption software focusing on security based on TrueCrypt"
   homepage "https://www.veracrypt.fr/"
 
   livecheck do
-    url "https://github.com/veracrypt/VeraCrypt/releases"
-    strategy :github_latest
-    regex(%r{href=.*?/VeraCrypt_(\d+(?:\.\d+)*[^/]*?)\.dmg}i)
+    url "https://www.veracrypt.fr/en/Downloads.html"
+    regex(/href=.*?VeraCrypt[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
   depends_on cask: "macfuse"
