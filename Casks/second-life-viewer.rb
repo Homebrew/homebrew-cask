a3e477eec5d6e39bfb22be54e8541bf293f23073
Bevan Kay <email@bevankay.me>
2021-12-06 10:08:39 +1100

    Update livecheck

--- Casks/second-life-viewer.rb
+++ Casks/second-life-viewer.rb
@@ -11,6 +11,8 @@ cask "second-life-viewer" do
     url "https://secondlife.com/support/downloads/"
     strategy :page_match do |page|
       v = page[%r{href=.*?/Second_Life_(\d+(?:_\d+)*)_x86_64\.dmg}i, 1]
+      next if v.blank?
+
       v.tr("_", ".")
     end
   end
