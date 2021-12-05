a81495d7f1b1638debabb95f0c314fc161d6691b
Bevan Kay <email@bevankay.me>
2021-12-05 22:25:55 +1100

    Update fastscripts.rb

--- Casks/fastscripts.rb
+++ Casks/fastscripts.rb
@@ -2,13 +2,13 @@ cask "fastscripts" do
   version "3.0.2,1524"
   sha256 "fed63c476458b6a12e202ce480016912fdd55dac575f979df684d69c1bfc13a8"
 
-  url "https://redsweater.com/fastscripts/FastScripts#{version.before_comma}.zip"
+  url "https://redsweater.com/fastscripts/FastScripts#{version.csv.first}.zip"
   name "FastScripts"
   desc "Tool for running time-saving scripts"
   homepage "https://redsweater.com/fastscripts/"
 
   livecheck do
-    url "https://redsweater.com/fastscripts/appcast3.php"
+    url "https://redsweater.com/fastscripts/appcast#{version.major}.php"
     strategy :sparkle
   end
 
