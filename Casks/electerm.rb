9884fa6377658f865ee809db8fd1c78aa524f7e3
zuisong <com.me@foxmail.com>
2021-12-05 22:57:55 +0800

    Update electerm from 1.17.16 to 1.17.19

--- Casks/electerm.rb
+++ Casks/electerm.rb
@@ -1,12 +1,12 @@
 cask "electerm" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "1.17.16"
+  version "1.17.19"
 
   if Hardware::CPU.intel?
-    sha256 "8772cc34181fccc82cd3ca7ba5998093f8c844deca4bcc85648d98007ab492b1"
+    sha256 "87de8ec164ee9b2c3e4366a4926aff8b0f04976bb6c8ef2520501be1369547df"
   else
-    sha256 "bf4c94514b4c442a17dd2f9aec2924be65f5e2ed01ba436bdc2e73f246027181"
+    sha256 "4e9ee6e7c7f43518ba299f3a050397e8d059eb20b31274f7488ac3208ecd3d9f"
   end
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac-#{arch}.dmg"
