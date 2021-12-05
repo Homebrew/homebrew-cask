ab27ca8bc51e96cbf2d63c4960c175d122f7b856
Bevan Kay <email@bevankay.me>
2021-12-05 22:16:07 +1100

    Update marsedit.rb

--- Casks/marsedit.rb
+++ Casks/marsedit.rb
@@ -2,7 +2,7 @@ cask "marsedit" do
   version "4.5.6,9796"
   sha256 "547f873c43c4c7802c654bfe3e4f38375f697dc715138f641ffa64f70854f911"
 
-  url "https://redsweater.com/marsedit/MarsEdit#{version.before_comma}.zip"
+  url "https://redsweater.com/marsedit/MarsEdit#{version.csv.first}.zip"
   name "MarsEdit"
   desc "Tool to write, preview and publish blogs"
   homepage "https://redsweater.com/marsedit/"
