44dfe9878dbc48f33c070e3051f714a31a6241ad
Richard Lee <14349+dlackty@users.noreply.github.com>
2021-12-05 19:57:41 +0800

    Add Ludwig 1.1.7 (#115292)
    * Add Ludwig 1.1.7
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

---
+++ Casks/ludwig.rb
@@ -0,0 +1,25 @@
+cask "ludwig" do
+  version "1.1.7"
+  sha256 "808ba9ffabf18aa8b2032bf620dd80f39429efadb1c771d9f091f6c415c1737a"
+
+  url "https://desktop.ludwig.guru/download/#{version}/osx_64"
+  name "ludwig"
+  desc "Sentence search engine app that helps you write better English"
+  homepage "https://ludwig.guru/"
+
+  livecheck do
+    url "https://desktop.ludwig.guru/download/latest/osx_64"
+    strategy :header_match
+  end
+
+  app "Ludwig.app"
+
+  zap trash: [
+    "~/Library/Application Support/Ludwig",
+    "~/Library/Caches/guru.ludwig",
+    "~/Library/Caches/guru.ludwig.ShipIt",
+    "~/Library/Logs/Ludwig",
+    "~/Library/Preferences/guru.ludwig.plist",
+    "~/Library/Saved Application State/guru.ludwig.savedState",
+  ]
+end
