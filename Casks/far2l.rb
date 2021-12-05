41c2cb22bb7d0e0987514f6ea152f13213063b13
Bevan Kay <email@bevankay.me>
2021-12-05 22:14:33 +1100

    update livecheck

--- Casks/far2l.rb
+++ Casks/far2l.rb
@@ -2,24 +2,24 @@ cask "far2l" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
   if MacOS.version <= :mojave
     version "far2l-2.3.211204-48654cc,2021-12-04_alpha"
-    url "https://github.com/elfmz/far2l/releases/download/v#{version.after_comma}/far2l-#{version.before_comma}-alpha-MacOS-10.11.dmg"
+    url "https://github.com/elfmz/far2l/releases/download/v#{version.csv.second}/far2l-#{version.csv.first}-alpha-MacOS-10.11.dmg"
     sha256 "113c55b19e8427258bea955415ff9282d18a4669df5f459ca8340bb5ce1726f2"
 
     livecheck do
       url "https://github.com/elfmz/far2l/releases"
-      regex(%r{/v(.*)/far2l-(.*)-alpha-MacOS-10\.11\.dmg}i)
+      regex(%r{/v([^/]+)/far2l-(.+)-alpha-MacOS-10\.11\.dmg}i)
       strategy :page_match do |page, regex|
         page.scan(regex).map { |match| "#{match[1]},#{match[0]}" }
       end
     end
   else
     version "2.3.211204-48654cc4,2021-12-04_alpha"
-    url "https://github.com/elfmz/far2l/releases/download/v#{version.after_comma}/far2l-#{version.before_comma}-alpha-MacOS-10.15.dmg"
+    url "https://github.com/elfmz/far2l/releases/download/v#{version.csv.second}/far2l-#{version.csv.first}-alpha-MacOS-10.15.dmg"
     sha256 "b2e43058b9d77306670afe61d579cf2031827328706dab54e34d3f7597b7fa01"
 
     livecheck do
       url "https://github.com/elfmz/far2l/releases"
-      regex(%r{/v(.*)/far2l-(.*)-alpha-MacOS-10\.15\.dmg}i)
+      regex(%r{/v([^/]+)/far2l-(.+)-alpha-MacOS-10\.15\.dmg}i)
       strategy :page_match do |page, regex|
         page.scan(regex).map { |match| "#{match[1]},#{match[0]}" }
       end
