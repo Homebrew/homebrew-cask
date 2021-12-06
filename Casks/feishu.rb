a6283773ba65d2bf01a7667171101d1d0f06395a
zuisong <378818573@qq.com>
2021-12-06 19:55:31 +0800

    Update feishu from 5.1.5 to 5.1.8 (#115366)
    Co-authored-by: zuisong <com.me@foxmail.com>

--- Casks/feishu.rb
+++ Casks/feishu.rb
@@ -2,11 +2,11 @@ cask "feishu" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
   if Hardware::CPU.intel?
-    version "5.1.5,2b840e"
-    sha256 "afa2572a5a6b9998e2ce791cec2a3de71d65cee24f9b304064c4f0ce6dfbf551"
+    version "5.1.8,5123d2"
+    sha256 "89321ed01503f6a51f1d5def358c8938cdf3dfff00692d1eb0d6bb3053b644db"
   else
-    version "5.1.5,e462a1"
-    sha256 "5fa04655c2819796b23e1578e9bafe78017acab64441ea9ad533699e333dd58a"
+    version "5.1.8,e0b28b"
+    sha256 "07abf13d5e083a607711278556e41b64effd245aafad9010ebb17a5b642f156a"
   end
 
   url "https://sf3-cn.feishucdn.com/obj/ee-appcenter/#{version.after_comma}/Feishu-darwin_#{arch}-#{version.before_comma}-signed.dmg",
