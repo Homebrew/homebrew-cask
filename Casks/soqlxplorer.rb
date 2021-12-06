6553720f11e6a182b709d83cb16b259599fd4a54
neutric <5984479+neutric@users.noreply.github.com>
2021-12-07 00:52:08 +0100

    Update soqlxplorer from 4.2 to 4.3

--- Casks/soqlxplorer.rb
+++ Casks/soqlxplorer.rb
@@ -1,6 +1,6 @@
 cask "soqlxplorer" do
-  version "4.2"
-  sha256 "3060930f6ad67fd677a072e4bbde016dd351d7b8a503bd09991448db92abc35c"
+  version "4.3"
+  sha256 "a3fbae13436041b3669aaaf44f88528510f67e62bf119a8d2bfca7aca67a0bfa"
 
   url "https://www.pocketsoap.com/osx/soqlx/SoqlXplorer_v#{version}.zip"
   name "SoqlXplorer"
