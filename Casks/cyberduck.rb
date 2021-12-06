0f63dfd15b6169e19e18f5c781100361e31e3bc4
Bevan Kay <email@bevankay.me>
2021-12-07 08:18:47 +1100

    Update cyberduck.rb

--- Casks/cyberduck.rb
+++ Casks/cyberduck.rb
@@ -2,7 +2,7 @@ cask "cyberduck" do
   version "8.1.1,36550"
   sha256 "e8488fe0b5d97fd061419a49747f1de4a3162379d14a59dff53254bc85c74288"
 
-  url "https://update.cyberduck.io/Cyberduck-#{version.before_comma}.#{version.after_comma}.zip"
+  url "https://update.cyberduck.io/Cyberduck-#{version.csv.first}.#{version.csv.second}.zip"
   name "Cyberduck"
   desc "Server and cloud storage browser"
   homepage "https://cyberduck.io/"
