f77afab5378a670f89a26cc7302a0c1ce7610588
xurc <xurc@Xus-MacBook-Pro.local>
2021-12-06 15:11:17 +0800

    Update curio from 15 to 19

--- Casks/curio.rb
+++ Casks/curio.rb
@@ -1,6 +1,6 @@
 cask "curio" do
-  version "15,15008"
-  sha256 "9e0c4176fd94ef9535bfb02289e8b8e43fc2cb4c3fe1135d2e3171ddd84fd9b3"
+  version "19,19006"
+  sha256 "e2ee57fbc298573e83da89f25b7c3f11aa71ddd6c1d76ad4d81d3b5782623141"
 
   url "https://www.zengobi.com/downloads/Curio#{version.after_comma.no_dots}.zip"
   name "Curio"
