8b9bcbba46889423e67dd5acca3e6949bea3aefa
ShredderWalnut <34428332+ShredderWalnut@users.noreply.github.com>
2021-12-06 16:09:53 +0800

    Update Curio from 15 to 19

--- Casks/curio.rb
+++ Casks/curio.rb
@@ -2,7 +2,7 @@ cask "curio" do
   version "19,19006"
   sha256 "e2ee57fbc298573e83da89f25b7c3f11aa71ddd6c1d76ad4d81d3b5782623141"
 
-  url "https://www.zengobi.com/downloads/Curio#{version.after_comma.no_dots}.zip"
+  url "https://www.zengobi.com/curio/downloads/Curio#{version.after_comma.no_dots}.zip"
   name "Curio"
   desc "Note-taking and organizing app"
   homepage "https://zengobi.com/curio/"
