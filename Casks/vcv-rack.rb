478eb5f15f0e49c1e9255428289cbaaf39c94a8d
ShredderWalnut <34428332+ShredderWalnut@users.noreply.github.com>
2021-12-06 21:33:07 +0800

    Update vcv-rack from 1.1.6 to 2.0.1 (#115376)
    * Update vcv-rack.rb
    
    * Update vcv-rack.rb
    
    * Update vcv-rack.rb
    
    * fix style
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/vcv-rack.rb
+++ Casks/vcv-rack.rb
@@ -1,17 +1,18 @@
 cask "vcv-rack" do
-  version "1.1.6"
-  sha256 "79078ca77dff41beab2247cadf22d1a9d4112b3981f8a4084923b75519664a50"
+  version "2.0.1"
+  sha256 "a0c07c03a198f052851cdaaff20e3baf8b5cc5d4ccc493eacd59e6d754de0bb2"
 
-  url "https://vcvrack.com/downloads/Rack-#{version}-mac.zip"
+  url "https://vcvrack.com/downloads/RackFree-#{version}-mac.pkg"
   name "VCV Rack"
   desc "Open-source virtual modular synthesizer"
   homepage "https://vcvrack.com/"
 
   livecheck do
     url "https://vcvrack.com/Rack.html"
-    strategy :page_match
-    regex(%r{href=.*?/Rack-(\d+(?:\.\d+)*)-mac\.zip}i)
+    regex(%r{href=.*?/RackFree[._-]v?(\d+(?:\.\d+)+)-mac\.pkg}i)
   end
 
-  app "Rack.app"
+  pkg "RackFree-#{version}-mac.pkg"
+
+  uninstall pkgutil: ["com.vcvrack.rack"]
 end
