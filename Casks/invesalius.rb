2332e8738d25259f9f93beab0de19b91e1b12f01
Bevan Kay <email@bevankay.me>
2021-12-08 00:05:32 +1100

    Update invesalius from 3.1.99995 to 3.1.99996

--- Casks/invesalius.rb
+++ Casks/invesalius.rb
@@ -1,6 +1,6 @@
 cask "invesalius" do
-  version "3.1.99995"
-  sha256 "e9ee3a7548cee911355440fb87362a14080e4ec8372b85fdb6dd118cb294d173"
+  version "3.1.99996"
+  sha256 "dac9b2ad8875b04862d0945906892b163ca3fbf9923138cb180aae181c7a2cab"
 
   url "https://github.com/invesalius/invesalius3/releases/download/v#{version}/InVesalius.#{version}.dmg"
   name "InVesalius"
