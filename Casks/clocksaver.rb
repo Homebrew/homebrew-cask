98e27e48500dd4844067b6f6071c705c13f2d7b0
neutric <5984479+neutric@users.noreply.github.com>
2021-12-07 20:43:39 +0100

    clocksaver: update checksum

--- Casks/clocksaver.rb
+++ Casks/clocksaver.rb
@@ -1,6 +1,6 @@
 cask "clocksaver" do
   version "0.7.0"
-  sha256 "9497e774695cadfb25fb6ad1b5689a110f806041b34d3b536b862d653de8bca4"
+  sha256 "a5559b1bcb20ae5bc473e16e6842618aa3a51ed17e9dc583a069ed1af5931180"
 
   url "https://github.com/soffes/Clock.saver/releases/download/v#{version}/Clock.saver-#{version}.zip"
   name "Clock.saver screensaver"
