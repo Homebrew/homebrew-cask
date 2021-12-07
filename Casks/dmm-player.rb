a8ff39dd7c954c16565b28c6ccf5953a678e703b
Bevan Kay <email@bevankay.me>
2021-12-07 22:07:54 +1100

    Update dmm-player from 2.1.8 to 2.1.9

--- Casks/dmm-player.rb
+++ Casks/dmm-player.rb
@@ -1,6 +1,6 @@
 cask "dmm-player" do
-  version "2.1.8"
-  sha256 "ac45719e1fe4d225fe813d2abdff3e5f34fb23253f3d1f960fe981c68e8c1e3b"
+  version "2.1.9"
+  sha256 "420aaef55b881cd6f9a648a71e9604d2a8db758db835baff24b62e8ea3a64090"
 
   url "https://portalapp.dmm.com/dmmplayerv#{version.major}/dmm/#{version.dots_to_underscores}/DMMPlayerV#{version.major}Installer_#{version.dots_to_underscores}.pkg"
   name "DMM Player"
