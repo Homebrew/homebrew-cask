c80526546dd784dea5c839acc7407b91883e13fd
neutric <5984479+neutric@users.noreply.github.com>
2021-12-07 01:09:02 +0100

    Update sparkle from 1.27.0 to 1.27.1

--- Casks/sparkle.rb
+++ Casks/sparkle.rb
@@ -1,6 +1,6 @@
 cask "sparkle" do
-  version "1.27.0"
-  sha256 "458bcdbc364836ed50fc604c1a85fca1653de80d5f05d3e532563bafdb2b111b"
+  version "1.27.1"
+  sha256 "20d729f8ca4d07911da31fe0296880cd3176b71738deaf2b7f8de2d96cf63f4e"
 
   url "https://github.com/sparkle-project/Sparkle/releases/download/#{version}/Sparkle-#{version}.tar.xz",
       verified: "github.com/sparkle-project/Sparkle/"
