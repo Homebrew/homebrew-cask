1764a97e40ba131b2ce9a5353680f544305d4d29
miccal <miccal.matthews@gmail.com>
2021-12-07 13:42:39 +0800

    Update kstars from 3.5.5 to 3.5.6

--- Casks/kstars.rb
+++ Casks/kstars.rb
@@ -1,6 +1,6 @@
 cask "kstars" do
-  version "3.5.5"
-  sha256 "8cc6e4478f5d34f12997734d6731e5d343ec82025c432a9ad6007d900874f626"
+  version "3.5.6"
+  sha256 "d4e348a174136bc82d77802030da2c18cb57f62752b62486dfe8e62de0fdddfa"
 
   url "https://www.indilib.org/jdownloads/kstars/kstars-#{version}.dmg",
       verified: "indilib.org/jdownloads/kstars/"
