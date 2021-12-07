5d3c7ca6b95e893021b4a45964b5c24f71bebcac
Bevan Kay <email@bevankay.me>
2021-12-07 18:06:52 +1100

    Update coin-wallet from 5.1.2 to 5.1.3

--- Casks/coin-wallet.rb
+++ Casks/coin-wallet.rb
@@ -1,6 +1,6 @@
 cask "coin-wallet" do
-  version "5.1.2"
-  sha256 "040da3fbe40b1937ecf7e97a8713c43a94dd6683742db4d915c548d646274522"
+  version "5.1.3"
+  sha256 "7bc2f8dc8b9075449fcc4e44e69e85f105fd27c293043b329d0bee5ba91f6e4a"
 
   url "https://github.com/CoinSpace/CoinSpace/releases/download/v#{version}/Coin.Wallet.dmg",
       verified: "github.com/CoinSpace/CoinSpace/"
