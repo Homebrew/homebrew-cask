7eb546a843476c0653c8d5eb0a323a44ab9b9ae3
neutric <5984479+neutric@users.noreply.github.com>
2021-12-07 01:09:10 +0100

    Update sound-siphon from 3.3.4 to 3.3.5 (#115400)
    * Update sound-siphon from 3.3.4 to 3.3.5
    
    * Update livecheck strategy
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/sound-siphon.rb
+++ Casks/sound-siphon.rb
@@ -1,5 +1,5 @@
 cask "sound-siphon" do
-  version "3.3.4"
+  version "3.3.5"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://staticz.com/download/5876/"
@@ -8,8 +8,8 @@ cask "sound-siphon" do
   homepage "https://staticz.com/soundsiphon/"
 
   livecheck do
-    url "http://staticz.net/updates/soundsiphon#{version.major}.rss"
-    strategy :sparkle
+    url :url
+    strategy :header_match
   end
 
   depends_on macos: ">= :mojave"
