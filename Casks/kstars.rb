0db6160d22113650c1655fcc7db7859ab1e12c82
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-07 14:21:44 +0800

    Update kstars from 3.5.5 to 3.5.6 (#115438)
    * Update kstars from 3.5.5 to 3.5.6
    
    * Update kstars.rb

--- Casks/kstars.rb
+++ Casks/kstars.rb
@@ -1,6 +1,6 @@
 cask "kstars" do
-  version "3.5.5"
-  sha256 "8cc6e4478f5d34f12997734d6731e5d343ec82025c432a9ad6007d900874f626"
+  version "3.5.6"
+  sha256 "d4e348a174136bc82d77802030da2c18cb57f62752b62486dfe8e62de0fdddfa"
 
   url "https://www.indilib.org/jdownloads/kstars/kstars-#{version}.dmg",
       verified: "indilib.org/jdownloads/kstars/"
@@ -10,8 +10,7 @@ cask "kstars" do
 
   livecheck do
     url :homepage
-    strategy :page_match
-    regex(%r{href=.*?/kstars-(\d+(?:\.\d+)*)\.dmg}i)
+    regex(%r{href=.*?/kstars-(\d+(?:\.\d+)+)\.dmg}i)
   end
 
   depends_on macos: ">= :high_sierra"
