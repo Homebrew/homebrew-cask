d8c64d632d5b082d78b644df00acbebcc99e5902
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-07 14:21:04 +0800

    Update electronmail from 4.13.0 to 4.13.1 (#115423)

--- Casks/electronmail.rb
+++ Casks/electronmail.rb
@@ -1,6 +1,6 @@
 cask "electronmail" do
-  version "4.13.0"
-  sha256 "05c4d22acaff106acd3919131c80c53fa2e7850e303c9585af295b22a19afb5c"
+  version "4.13.1"
+  sha256 "0fe992b404b2f2cbf84bf1d73ae2ee01d3029c4dc74f1b7cb39a2af32754bb34"
 
   url "https://github.com/vladimiry/ElectronMail/releases/download/v#{version}/electron-mail-#{version}-mac-x64.dmg"
   name "ElectronMail"
