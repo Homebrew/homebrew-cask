56e0438fcde53935bed3174950f30e7b76fde980
Bevan Kay <email@bevankay.me>
2021-12-08 09:34:56 +1100

    Update kdocs.rb

--- Casks/kdocs.rb
+++ Casks/kdocs.rb
@@ -2,7 +2,7 @@ cask "kdocs" do
   version "3.0.4,1001"
   sha256 "c25b4f7edeed4099d0551748c14512a65a47be75904db432f8b249a4b5c322ee"
 
-  url "https://qn.cache.wpscdn.cn/kdocs/apk/kdesktopmac/KDocs_#{version.after_comma}_v#{version.before_comma}.dmg",
+  url "https://qn.cache.wpscdn.cn/kdocs/apk/kdesktopmac/KDocs_#{version.csv.second}_v#{version.csv.first}.dmg",
       verified: "qn.cache.wpscdn.cn/kdocs/apk/kdesktopmac/"
   name "金山文档"
   desc "Online collaborate editor for Word, Excel and PPT documents"
