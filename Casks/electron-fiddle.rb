3a1dd7c1e44069a0a2f7a8d3d8ba3cacdd60a97b
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-07 14:18:01 +0800

    Update electron-fiddle.rb

--- Casks/electron-fiddle.rb
+++ Casks/electron-fiddle.rb
@@ -3,14 +3,14 @@ cask "electron-fiddle" do
 
   version "0.27.2"
 
-  url "https://github.com/electron/fiddle/releases/download/v#{version}/Electron.Fiddle-darwin-#{arch}-#{version}.zip",
-      verified: "github.com/electron/fiddle/"
   if Hardware::CPU.intel?
     sha256 "d5dc8c4ff806ff919a75985ecffae6b056a99ed953308ed0bcc523831ca96905"
   else
     sha256 "a8c9e377dfa53fb3c6c218b3bedb9196e75ca482c662f3537b9ba3eb8a4cf1e8"
   end
 
+  url "https://github.com/electron/fiddle/releases/download/v#{version}/Electron.Fiddle-darwin-#{arch}-#{version}.zip",
+      verified: "github.com/electron/fiddle/"
   name "Electron Fiddle"
   desc "Create and play with small Electron experiments"
   homepage "https://www.electronjs.org/fiddle"
