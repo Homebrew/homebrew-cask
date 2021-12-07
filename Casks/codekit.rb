0c2e73634cf274de24b9f748c84920bc78000798
Bevan Kay <email@bevankay.me>
2021-12-07 21:38:33 +1100

    Update codekit.rb

--- Casks/codekit.rb
+++ Casks/codekit.rb
@@ -2,7 +2,7 @@ cask "codekit" do
   version "3.15.1,34264"
   sha256 "6c10b66ee5ed76bfa84d695267fa1bd32e8adf0892da2d6e8742ec4745350366"
 
-  url "https://codekitapp.com/binaries/codekit-#{version.after_comma}.zip"
+  url "https://codekitapp.com/binaries/codekit-#{version.csv.second}.zip"
   name "CodeKit"
   desc "App for building websites"
   homepage "https://codekitapp.com/"
