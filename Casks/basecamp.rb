e8d37797de4e572a31b7e966e6f521a741d894bb
Bevan Kay <email@bevankay.me>
2021-12-07 22:09:29 +1100

    Update basecamp from 3 to 3,2.2.9 (#115476)
    * Update basecamp from 3 to 3,2.2.9
    
    * Update Casks/basecamp.rb
    
    Co-authored-by: George Adams <george.adams@microsoft.com>
    
    Co-authored-by: George Adams <george.adams@microsoft.com>

--- Casks/basecamp.rb
+++ Casks/basecamp.rb
@@ -1,13 +1,31 @@
 cask "basecamp" do
-  version "3"
-  sha256 :no_check # required as upstream package is updated in-place
+  version "3,2.2.9"
+  sha256 "4fb1f1a26a8217cd092b723de8afcb8727ecc1d36f207aca37d3822d54582783"
 
-  url "https://bc3-desktop.s3.amazonaws.com/mac/basecamp#{version}.dmg",
+  url "https://bc#{version.major}-desktop.s3.amazonaws.com/mac/basecamp#{version.major}-#{version.csv.second}.zip",
       verified: "bc3-desktop.s3.amazonaws.com/"
   name "Basecamp"
+  desc "All-In-One Toolkit for Working Remotely"
   homepage "https://basecamp.com/help/#{version}/guides/apps/mac"
+
+  livecheck do
+    url "https://bc#{version.major}-desktop.s3.amazonaws.com/mac/updates.json"
+    strategy :page_match do |page|
+      minor_version = JSON.parse(page)["version"]
+      major_version = page.match(/basecamp(\d)/i)
+      next if major_version.blank? || minor_version.blank?
+
+      "#{major_version[1]},#{minor_version}"
+    end
+  end
 
   auto_updates true
 
-  app "Basecamp #{version}.app"
+  app "Basecamp #{version.major}.app"
+
+  zap trash: [
+    "~/Library/Application Support/Basecamp*",
+    "~/Library/Preferences/com.basecamp.basecamp*.plist",
+    "~/Library/Saved Application State/com.basecamp.basecamp*.savedState",
+  ]
 end
