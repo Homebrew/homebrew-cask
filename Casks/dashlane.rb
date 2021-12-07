84de76946f57e9a08f6766115f299c5e35c29e83
Bevan Kay <email@bevankay.me>
2021-12-07 22:14:20 +1100

    Update dashlane from 6.2140.0.50277 to 6.2148.0.51413 (#115504)

--- Casks/dashlane.rb
+++ Casks/dashlane.rb
@@ -1,6 +1,6 @@
 cask "dashlane" do
-  version "6.2140.0.50277"
-  sha256 "ca0d462d311f8c6ca134ed24bf64774f05cedfebadd99b915f58c650210f1dfc"
+  version "6.2148.0.51413"
+  sha256 "fdc2f01040b7bf5f98d64f7412be483a7d7724365d97be892f0be48b82242c04"
 
   url "https://d3mfqat9ni8wb5.cloudfront.net/releases/#{version.major_minor_patch}/#{version}/release/Dashlane.dmg",
       verified: "d3mfqat9ni8wb5.cloudfront.net/releases/"
