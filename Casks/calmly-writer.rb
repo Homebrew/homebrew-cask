b8c0f1b3c336eacbe2d35c4c7a7de7e0edaa726c
Bevan Kay <email@bevankay.me>
2021-12-07 21:35:33 +1100

    Update calmly-writer from 2.0.36 to 2.0.37 (#115480)

--- Casks/calmly-writer.rb
+++ Casks/calmly-writer.rb
@@ -1,12 +1,12 @@
 cask "calmly-writer" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "2.0.36"
+  version "2.0.37"
 
   if Hardware::CPU.intel?
-    sha256 "08d57fdedee42b67aca3caae9815a867736c45960dfde95b191b76cfcd460950"
+    sha256 "5426fbcd4d33244df6d61ea4eac2fcd4941d162013a3e1e51b1997dad55d4a4a"
   else
-    sha256 "3cc5240a4c43aad475a0a995eaed21ec8d72db07f886ab0cd132505f26524d8f"
+    sha256 "ff1417572712f1d5940da670332291fe911ab4ee25e8832216fff25f1f72cadd"
   end
 
   url "https://www.calmlywriter.com/releases/#{arch}/Calmly%20Writer-#{version}.pkg"
