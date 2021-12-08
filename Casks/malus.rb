627b79585ef2a827e750cd6ddaa0795895f42c66
i0ntempest <szf1234@me.com>
2021-12-08 04:11:23 -0500

    Update malus from 3.0.0 to 3.0.1

--- Casks/malus.rb
+++ Casks/malus.rb
@@ -1,8 +1,8 @@
 cask "malus" do
-  version "3.0.0"
-  sha256 "8cae0a7abdbf207741d21f7a9937fe87dfc9a1798c9c29c34098eacef8358eda"
+  version "3.0.1"
+  sha256 "bb716003e32db4cd3527a9eaee87dbc6f4e1cb8152269362f132e3f7f02c7731"
 
-  url "https://download.getmalus.com/uploads/malus-mac-#{version.dots_to_underscores}.dmg"
+  url "https://download.getmalus.com/uploads/Malus#{version.dots_to_underscores}.dmg"
   name "Malus"
   desc "Proxy to help accessing various online media resources/services"
   homepage "https://getmalus.com/"
