85020e6e5f197a10b1a94a7c438c3b8117883982
miccal <miccal.matthews@gmail.com>
2021-12-08 22:50:43 +0800

    Update davmail from 6.0.0-3375 to 6.0.1-3390

--- Casks/davmail.rb
+++ Casks/davmail.rb
@@ -1,6 +1,6 @@
 cask "davmail" do
-  version "6.0.0-3375"
-  sha256 "a212b6f3e06c9a145b3626bc1dcfc527f36d625530e53adf2ef379b9c6e9d354"
+  version "6.0.1-3390"
+  sha256 "917bafe8b5466cbd2a9ac282bf0ef5590f3cc83e099c88ce729897b60fd9bc89"
 
   url "https://downloads.sourceforge.net/davmail/DavMail-MacOSX-#{version}.app.zip",
       verified: "downloads.sourceforge.net/davmail/"
