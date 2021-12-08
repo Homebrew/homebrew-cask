a560089091f5cd252cbf561a557e5dac4b150b6e
neutric <5984479+neutric@users.noreply.github.com>
2021-12-08 13:02:16 +0100

    Update sqlworkbenchj from 127 to 128 (#115563)
    * Update sqlworkbenchj from 127 to 128
    
    * sqlworkbenchj: add desc
    
    * sqlworkbenchj: specify minimum Java version

--- Casks/sqlworkbenchj.rb
+++ Casks/sqlworkbenchj.rb
@@ -1,9 +1,10 @@
 cask "sqlworkbenchj" do
-  version "127"
+  version "128"
   sha256 "2173c7f00172bef3fed23e7f57e168a9d156c72c311af032ba469139f80d9fe9"
 
   url "https://www.sql-workbench.eu/Workbench-Build#{version}-Mac.tgz"
   name "SQL Workbench/J"
+  desc "DBMS-independent SQL query tool"
   homepage "https://www.sql-workbench.eu/"
 
   livecheck do
@@ -14,6 +15,6 @@ cask "sqlworkbenchj" do
   app "SQLWorkbenchJ.app"
 
   caveats do
-    depends_on_java
+    depends_on_java "11+"
   end
 end
