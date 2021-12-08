38755ad234dd15cf22b149f0cbd15bfd414d7f75
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-08 22:11:09 +0800

    Update trezor-suite.rb

--- Casks/trezor-suite.rb
+++ Casks/trezor-suite.rb
@@ -3,13 +3,13 @@ cask "trezor-suite" do
 
   version "21.12.1"
 
-  url "https://suite.trezor.io/web/static/desktop/Trezor-Suite-#{version}-mac-#{arch}.dmg"
   if Hardware::CPU.intel?
     sha256 "fdf2e06efe885740272eb94ef87830d6c1aeae3456f7af2a8b2ac9a2fae818d0"
   else
     sha256 "dfee8c5abc4ac243c40edb6faf73f8541c1c4701e8f3a38c0a7d2591ac09e190"
   end
 
+  url "https://suite.trezor.io/web/static/desktop/Trezor-Suite-#{version}-mac-#{arch}.dmg"
   name "TREZOR Suite"
   desc "Companion app for the Trezor hardware wallet"
   homepage "https://suite.trezor.io/"
