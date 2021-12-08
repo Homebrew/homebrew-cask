af0d88a5862689a68faba79b9079049611857201
Cap.雪ノ下八幡 <CaptainYukinoshitaHachiman@protonmail.com>
2021-12-08 17:24:26 +0800

    Add SchildiChat.app v1.9.5-sc.2 (#115574)
    * Add SchildiChat.app v1.9.5-sc.2
    
    * Update schildichat.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/schildichat.rb
@@ -0,0 +1,14 @@
+cask "schildichat" do
+  version "1.9.5-sc.2"
+  sha256 "2ddcc6ad487fe41c9f738d1f9071cd982d80921fd4fc271ad55e3b2b7407beff"
+
+  url "https://github.com/SchildiChat/schildichat-desktop/releases/download/v#{version}/SchildiChat-#{version}-universal_by_nyantec.dmg",
+      verified: "github.com/SchildiChat/schildichat-desktop/"
+  name "SchildiChat"
+  desc "Matrix client based on Element with a more traditional IM experience"
+  homepage "https://schildi.chat/desktop/"
+
+  app "SchildiChat.app"
+
+  zap trash: "~/Library/Application Support/SchildiChat"
+end
