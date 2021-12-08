7a16ad5b9c306e9fa7c10d25989fcd36a06d6ffc
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-08 08:33:26 +0800

    Update supermjograph.rb

--- Casks/supermjograph.rb
+++ Casks/supermjograph.rb
@@ -3,16 +3,10 @@ cask "supermjograph" do
   sha256 "d8044df2da0114af8dddecafac015a1489a244499541c0a69be1b23c7cd65da5"
 
   url "https://downloads.sourceforge.net/mjograph/SuperMjograph/SuperMjograph-#{version}.zip",
-      verified: "downloads.sourceforge.net/mjograph"
+      verified: "downloads.sourceforge.net/mjograph/SuperMjograph/"
   name "SuperMjograph"
-  desc "Is a tool for generating scientific graphs from data"
+  desc "Generate scientific graphs from data"
   homepage "https://www.mjograph.net/"
-
-  livecheck do
-    url "https://sourceforge.net/projects/mjograph/files/latest/download"
-    strategy :header_match
-    regex(/SuperMjograph-(\d+(?:\.\d+)*)\.zip/i)
-  end
 
   app "SuperMjograph.app"
 end
