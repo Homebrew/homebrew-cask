85569a3abbc054f40bb1fbda210cc906f654ccb4
miccal <miccal.matthews@gmail.com>
2021-12-08 22:39:37 +0800

    Update tev from 1.17 to 1.19

--- Casks/tev.rb
+++ Casks/tev.rb
@@ -1,6 +1,6 @@
 cask "tev" do
-  version "1.17"
-  sha256 "306f5f044d9ba7aa7aea1b227d6da8d649b92aa10341a019cce08473c9c99f38"
+  version "1.19"
+  sha256 "198b8c5d410cb1f5f55be3fd91b45fd4161d8d1e542486c5af6a7dc3b093bc32"
 
   url "https://github.com/Tom94/tev/releases/download/v#{version}/tev.dmg"
   name "tev"
