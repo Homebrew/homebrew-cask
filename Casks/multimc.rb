be57d85cee96a962c3c37c138ec2821eb56bdeeb
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-08 22:43:52 +0800

    Update multimc.rb

--- Casks/multimc.rb
+++ Casks/multimc.rb
@@ -2,13 +2,13 @@ cask "multimc" do
   version "0.6.14"
   sha256 :no_check
 
-  url "https://files.multimc.org/downloads/mmc-stable-osx64.tar.gz"
+  url "https://files.multimc.org/downloads/mmc-stable-osx64-5.15.2.tar.gz"
   name "Multi MC"
   desc "Minecraft launcher"
   homepage "https://multimc.org/"
 
   livecheck do
-    url "https://files.multimc.org/update/osx64/stable/index.json"
+    url "https://files.multimc.org/update/osx64-5.15.2/stable/index.json"
     strategy :page_match do |page|
       JSON.parse(page)["Versions"][0]["Name"]
     end
