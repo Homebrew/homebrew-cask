5d226087ef9fac8c2c5d58857b811034b797cfa7
neutric <5984479+neutric@users.noreply.github.com>
2021-12-08 02:08:18 +0100

    Update sunlogincontrol from 3.0 to 5.3.1.40594

--- Casks/sunlogincontrol.rb
+++ Casks/sunlogincontrol.rb
@@ -1,8 +1,8 @@
 cask "sunlogincontrol" do
-  version "3.0"
-  sha256 "87ff12a9192ed809c2c8668e63cf44b1101544e52f43e8cc912ed2b20acf077f"
+  version "5.3.1.40594"
+  sha256 "27d8e5a6fbda5340f3a0d5fc913f08bea17a50ddd3f3b57c3e66d13e6ba7b735"
 
-  url "https://download.oray.com/sunlogin/mac/SunloginControl#{version}.dmg"
+  url "https://down.oray.com/sunlogin/mac/SunloginRemote_#{version}.dmg"
   name "SunloginControl"
   name "向日葵控制端"
   homepage "https://sunlogin.oray.com/"
