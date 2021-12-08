2d78f3dc52f1f4350b7272f66151a746d205d53a
Branch Vincent <branchevincent@gmail.com>
2021-12-08 17:07:39 -0500

    Update insomnia from 2021.6.0 to 2021.7.0

--- Casks/insomnia.rb
+++ Casks/insomnia.rb
@@ -1,6 +1,6 @@
 cask "insomnia" do
-  version "2021.6.0"
-  sha256 "7c732cc1d8fc83ebabfb0b295db6cc1a77b82686d290d8d37c88969feebfba9a"
+  version "2021.7.0"
+  sha256 "686e0c82e4e3ecfa10b94629ff59f5c41c5bb23619d561d4bbfe096f6d4d50c2"
 
   url "https://github.com/Kong/insomnia/releases/download/core%40#{version}/Insomnia.Core-#{version}.dmg",
       verified: "github.com/Kong/insomnia/"
