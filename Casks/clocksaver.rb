c675bd7badb7cd4bdfa3caa6188a72b46b404874
neutric <5984479+neutric@users.noreply.github.com>
2021-12-08 10:04:50 +0100

    Update clocksaver from 0.7.0 to 0.7.1

--- Casks/clocksaver.rb
+++ Casks/clocksaver.rb
@@ -1,8 +1,8 @@
 cask "clocksaver" do
-  version "0.7.0"
-  sha256 "a5559b1bcb20ae5bc473e16e6842618aa3a51ed17e9dc583a069ed1af5931180"
+  version "0.7.1"
+  sha256 "a2dbe86718c2721578084585580b29f5f294d098e009a47873fae9b8248c6ad8"
 
-  url "https://github.com/soffes/Clock.saver/releases/download/v#{version}/Clock.saver-#{version}.zip"
+  url "https://github.com/soffes/Clock.saver/releases/download/v#{version}/Clock.saver.zip"
   name "Clock.saver screensaver"
   homepage "https://github.com/soffes/Clock.saver"
 
