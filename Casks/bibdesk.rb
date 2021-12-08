76b6de7dd92fc2d9e36a4226bb1defe0b6346ee8
Anselm Hahn <Anselm.Hahn@gmail.com>
2021-12-08 19:11:54 +0100

    Update bibdesk from 1.8.8,5799 to 1.8.8

--- Casks/bibdesk.rb
+++ Casks/bibdesk.rb
@@ -1,5 +1,5 @@
 cask "bibdesk" do
-  version "1.8.8,5799"
+  version "1.8.8"
   sha256 "1047a95188660abb3cc1d66ae0d6f9b4be2f2f5574a4bc4a00438947c785dc73"
 
   url "https://downloads.sourceforge.net/bibdesk/BibDesk/BibDesk-#{version.csv.first}/BibDesk-#{version.csv.first}.dmg",
