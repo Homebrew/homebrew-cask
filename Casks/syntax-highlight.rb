7aadeff44b7da69ae3943bcb07c5246b0f94f6de
Reno <25192197+singularitti@users.noreply.github.com>
2021-12-08 17:49:27 -0500

    Update syntax-highlight from 2.1.4 to 2.1.5 (#115600)

--- Casks/syntax-highlight.rb
+++ Casks/syntax-highlight.rb
@@ -1,6 +1,6 @@
 cask "syntax-highlight" do
-  version "2.1.4"
-  sha256 "8aa627e3e3ceb54f7644eb75ede57c18b989b03e31f6d92c02934eb6cd82e8d1"
+  version "2.1.5"
+  sha256 "5c64648e4eb049b0044dd961ce55c122978bc2382a55ad8c505514eaa245b6f7"
 
   url "https://github.com/sbarex/SourceCodeSyntaxHighlight/releases/download/#{version}/Syntax.Highlight.zip"
   name "Syntax Highlight"
