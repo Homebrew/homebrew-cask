a3fcb49b94614cbb4bbd632873ba207924cb49d6
Bevan Kay <email@bevankay.me>
2021-12-08 23:01:27 +1100

    update livecheck, version url, add shasum

--- Casks/superhuman.rb
+++ Casks/superhuman.rb
@@ -2,16 +2,22 @@ cask "superhuman" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
   version "11.0.36"
-  sha256 :no_check
 
-  url "https://download.superhuman.com/Superhuman#{arch}.dmg"
+  if Hardware::CPU.intel?
+    sha256 "ccc9c8526757dad96fab06ab29e7a0b955a2f9658553212c4679262afef020e5"
+  else
+    sha256 "0285fcabbc6e7dafc3f94185e1be0c32804f9bea692136707cf3cfafb245d22b"
+  end
+
+  url "https://storage.googleapis.com/download.superhuman.com/supertron-update/Superhuman-#{version}#{arch}-latest-mac.zip",
+      verified: "storage.googleapis.com/download.superhuman.com/"
   name "Superhuman"
   desc "Email client"
   homepage "https://superhuman.com/"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url "https://storage.googleapis.com/download.superhuman.com/supertron-update/latest-mac.yml"
+    strategy :electron_builder
   end
 
   app "Superhuman.app"
