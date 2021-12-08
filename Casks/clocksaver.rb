0adaca2ca633bc4ec73b22642c0fec0fc999a847
Bevan Kay <email@bevankay.me>
2021-12-08 22:49:41 +1100

    Update clocksaver.rb

--- Casks/clocksaver.rb
+++ Casks/clocksaver.rb
@@ -3,8 +3,8 @@ cask "clocksaver" do
   sha256 "a2dbe86718c2721578084585580b29f5f294d098e009a47873fae9b8248c6ad8"
 
   url "https://github.com/soffes/Clock.saver/releases/download/v#{version}/Clock.saver.zip"
-  desc "Screensavers inspired by Braun watches"
   name "Clock.saver screensaver"
+  desc "Screensavers inspired by Braun watches"
   homepage "https://github.com/soffes/Clock.saver"
 
   screen_saver "Clock.saver"
