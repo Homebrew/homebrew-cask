fb060e66152a7c2461dea7105f8fa4528993e6f8
neutric <5984479+neutric@users.noreply.github.com>
2021-12-09 07:52:06 +0100

    Update subsync from 0.16.0 to 0.17.0 (#115606)
    * Update subsync from 0.16.0 to 0.17.0
    
    * subsync: fix livecheck
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/subsync.rb
+++ Casks/subsync.rb
@@ -1,12 +1,18 @@
 cask "subsync" do
-  version "0.17"
+  version "0.17.0"
   sha256 "8d81f4d8da99b5f6b023da3fd100fccadb0c2b07143e495eb57bd22bfa5a78bd"
 
-  url "https://github.com/sc0ty/subsync/releases/download/#{version}/subsync-#{version}.0-mac-x86_64.dmg",
+  url "https://github.com/sc0ty/subsync/releases/download/#{version.major_minor}/subsync-#{version}-mac-x86_64.dmg",
       verified: "github.com/sc0ty/subsync/"
   name "subsync"
   desc "Subtitle speech synchronizer"
   homepage "https://subsync.online/"
+
+  livecheck do
+    url "https://github.com/sc0ty/subsync/releases/"
+    strategy :page_match
+    regex(%r{href=.*?/subsync[._-]v?(\d+(?:\.\d+)+)-mac-x86_64\.dmg}i)
+  end
 
   app "subsync.app"
   # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
