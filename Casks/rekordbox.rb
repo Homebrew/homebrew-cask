333e07b17c26df732150a3d95778df2f8efc39a5
Oli Walker <oliwalker@btinternet.com>
2021-12-09 23:56:27 +0000

    Update rekordbox from 6.6.0 to 6.6.1 (#115661)
    * Update rekordbox from 6.6.0 to 6.6.1
    
    * Update rekordbox.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/rekordbox.rb
+++ Casks/rekordbox.rb
@@ -1,8 +1,8 @@
 cask "rekordbox" do
-  version "6.6.0,20211110092655"
-  sha256 "3daa63c1487fb7f3742bea566b30f5eca6001e0f2dff2b751ab0aaeafb09d7a0"
+  version "6.6.1,20211125100827"
+  sha256 "7d9e0c0d2a9a71c50c9d01770718f878041b5badf3166c22ad85e148ea1fdc44"
 
-  url "https://cdn.rekordbox.com/files/#{version.after_comma}/Install_rekordbox_#{version.before_comma.dots_to_underscores}.pkg_.zip"
+  url "https://cdn.rekordbox.com/files/#{version.csv.second}/Install_rekordbox_#{version.csv.first.dots_to_underscores}.pkg_.zip"
   appcast "https://rekordbox.com/en/download/"
   name "rekordbox"
   desc "Free Dj app to prepare and manage your music files"
@@ -11,7 +11,7 @@ cask "rekordbox" do
   auto_updates true
   depends_on macos: ">= :high_sierra"
 
-  pkg "Install_rekordbox_#{version.before_comma.dots_to_underscores}.pkg"
+  pkg "Install_rekordbox_#{version.csv.first.dots_to_underscores}.pkg"
 
   uninstall pkgutil: "com.pioneer.rekordbox.#{version.major}.*",
             delete:  "/Applications/rekordbox #{version.major}"
