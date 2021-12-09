4827db1efbadda16e594ed0636bb18c4af8aed09
ShredderWalnut <34428332+ShredderWalnut@users.noreply.github.com>
2021-12-10 07:51:03 +0800

    Update vcv-rack from 2.0.2 to 2.0.3 (#115643)
    * Update vcv-rack from 2.0.2 to 2.0.3
    
    * Update vcv-rack.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/vcv-rack.rb
+++ Casks/vcv-rack.rb
@@ -1,6 +1,6 @@
 cask "vcv-rack" do
-  version "2.0.2"
-  sha256 "f449713ad8c5f453487f0d706c76ee80e530559cbfa96a6f2b3f8ef77acf829c"
+  version "2.0.3"
+  sha256 "ba50e7617d8c23ed4583404206f6b5931f68a6e892329c3a92f816d21ee8d58e"
 
   url "https://vcvrack.com/downloads/RackFree-#{version}-mac.pkg"
   name "VCV Rack"
@@ -9,10 +9,10 @@ cask "vcv-rack" do
 
   livecheck do
     url "https://vcvrack.com/Rack.html"
-    regex(%r{href=.*?/RackFree[._-]v?(\d+(?:\.\d+)+)-mac\.pkg}i)
+    regex(%r{href=.*?/RackFree[._-]v?(\d+(?:\.\d+)+)[._-]mac\.pkg}i)
   end
 
   pkg "RackFree-#{version}-mac.pkg"
 
-  uninstall pkgutil: ["com.vcvrack.rack"]
+  uninstall pkgutil: "com.vcvrack.rack"
 end
