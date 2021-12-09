fe835c88bd295b99534d83238b03c7c2c857777a
Glitch <iGlitch@users.noreply.github.com>
2021-12-09 05:29:40 +0000

    Add arm64 builds for Cryptomator
    Using https://github.com/Homebrew/homebrew-cask/pull/115628#pullrequestreview-827203688 instead.

--- Casks/cryptomator.rb
+++ Casks/cryptomator.rb
@@ -1,8 +1,15 @@
 cask "cryptomator" do
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
+  
   version "1.6.4"
-  sha256 "7c763610523dc6ced2d51d5e25fdb15ccd93366a107b8959460f7796eef83dc2"
+  
+  if Hardware::CPU.intel?
+    sha256 "7c763610523dc6ced2d51d5e25fdb15ccd93366a107b8959460f7796eef83dc2"
+  else
+    sha256 "1e7fc2bb09bbe914317c9b1459415a250d8f7baaa4cb51735c2f1a1e8d4605da"
+  end
 
-  url "https://github.com/cryptomator/cryptomator/releases/download/#{version}/Cryptomator-#{version}.dmg",
+  url "https://github.com/cryptomator/cryptomator/releases/download/#{version}/Cryptomator-#{version}#{arch}.dmg",
       verified: "github.com/cryptomator/cryptomator/"
   name "Cryptomator"
   desc "Multi-platform client-side cloud file encryption tool"
@@ -24,3 +31,4 @@ cask "cryptomator" do
     "~/Library/Preferences/org.cryptomator.plist",
   ]
 end
+
