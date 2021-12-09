89bc6566d9fffd79742d1c2e593989b2576b2159
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-09 08:06:09 +0800

    Update vassal.rb

--- Casks/vassal.rb
+++ Casks/vassal.rb
@@ -1,8 +1,15 @@
 cask "vassal" do
-  version "3.5.8"
-  sha256 "247eecec789d14a9b752248407c3a8b054fa7f08e98e97a51f79d9afe254a60a"
+  arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
-  url "https://github.com/vassalengine/vassal/releases/download/#{version}/VASSAL-#{version}-macos.dmg",
+  version "3.6.1"
+
+  if Hardware::CPU.intel?
+    sha256 "cfc72844e7241cd89ed7ff583429d238fb6660e0898d18594fce325487a046e8"
+  else
+    sha256 "d7d04138e55747fa38387f6b4133afa8a080cc501863ed5cf50c055089ee2ca3"
+  end
+
+  url "https://github.com/vassalengine/vassal/releases/download/#{version}/VASSAL-#{version}-macos-#{arch}.dmg",
       verified: "github.com/vassalengine/vassal/"
   name "VASSAL"
   desc "Board game engine"
