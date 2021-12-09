1d1918893e24913002865b784641d5e74a585e9b
neutric <5984479+neutric@users.noreply.github.com>
2021-12-10 00:32:11 +0100

    Update timemator from 2.8.3 to 2.8.5

--- Casks/timemator.rb
+++ Casks/timemator.rb
@@ -1,9 +1,9 @@
 cask "timemator" do
-  version "2.8.3,133"
-  sha256 :no_check
+  version "2.8.5,137"
+  sha256 "768be009497a117018245d5d4c3854e28c14d893dbc8f060d2c97f0aed6ad4ef"
 
-  url "https://catforce-timemator.s3.amazonaws.com/releases/Timemator.dmg",
-      verified: "catforce-timemator.s3.amazonaws.com/"
+  url "https://s3-eu-west-1.amazonaws.com/catforce-timemator/releases/Timemator_#{version.csv.first}.dmg",
+      verified: "s3-eu-west-1.amazonaws.com/catforce-timemator/"
   name "Timemator"
   desc "Automatic time-tracking application"
   homepage "https://timemator.com/"
