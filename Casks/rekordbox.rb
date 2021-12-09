cf15ef523d142a1c6335b00c9adf61963127a549
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-10 07:50:46 +0800

    Update rekordbox.rb

--- Casks/rekordbox.rb
+++ Casks/rekordbox.rb
@@ -2,7 +2,7 @@ cask "rekordbox" do
   version "6.6.1,20211125100827"
   sha256 "7d9e0c0d2a9a71c50c9d01770718f878041b5badf3166c22ad85e148ea1fdc44"
 
-  url "https://cdn.rekordbox.com/files/#{version.after_comma}/Install_rekordbox_#{version.before_comma.dots_to_underscores}.pkg_.zip"
+  url "https://cdn.rekordbox.com/files/#{version.csv.second}/Install_rekordbox_#{version.csv.first.dots_to_underscores}.pkg_.zip"
   appcast "https://rekordbox.com/en/download/"
   name "rekordbox"
   desc "Free Dj app to prepare and manage your music files"
@@ -11,7 +11,7 @@ cask "rekordbox" do
   auto_updates true
   depends_on macos: ">= :high_sierra"
 
-  pkg "Install_rekordbox_#{version.before_comma.dots_to_underscores}.pkg"
+  pkg "Install_rekordbox_#{version.csv.first.dots_to_underscores}.pkg"
 
   uninstall pkgutil: "com.pioneer.rekordbox.#{version.major}.*",
             delete:  "/Applications/rekordbox #{version.major}"
