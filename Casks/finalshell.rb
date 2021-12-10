5af94397c7865bb19b093dd68d81039ca322cd4b
liheng <924068818@qq.com>
2021-12-10 09:04:04 +0800

    fix check error! add pkgutil remove step. change url http to https. format code style.

--- Casks/finalshell.rb
+++ Casks/finalshell.rb
@@ -2,18 +2,20 @@ cask "finalshell" do
   version "3.9.2"
   sha256 :no_check
 
-  url "http://www.hostbuf.com/downloads/finalshell_install.pkg"
+  url "https://www.hostbuf.com/downloads/finalshell_install.pkg"
   name "finalshell"
-  desc "一体化的的服务器,网络管理软件,不仅是ssh客户端,还是功能强大的开发,运维工具,充分满足开发,运维需求."
-  homepage "http://www.hostbuf.com/"
+  desc "一体化的的服务器,网络管理软件,不仅是ssh客户端,还是功能强大的开发,运维工具,充分满足开发,运维需求"
+  homepage "https://www.hostbuf.com/"
 
   pkg "finalshell_install.pkg"
 
-  uninstall quit:[
-    "finalshellinstall.all"
+  uninstall quit:    [
+    "finalshellinstall.all",
   ],
-  delete:    [
-    "/Applications/FinalShell.app",
-  ],
-  rmdir:  "~/Library/FinalShell/"
+            delete:  [
+              "/Applications/FinalShell.app",
+            ],
+            pkgutil: "finalshellinstall.all",
+
+            rmdir:   "~/Library/FinalShell/"
 end
