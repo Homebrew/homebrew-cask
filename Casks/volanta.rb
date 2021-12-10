704ab8960489d3560b30913cb4d508d5d8e5a48a
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-10 23:52:07 +0800

    Update volanta.rb

--- Casks/volanta.rb
+++ Casks/volanta.rb
@@ -2,7 +2,7 @@ cask "volanta" do
   version "1.1.11,58470a63"
   sha256 "51b9e61379ff029b7dad28feca4056c8a6d75ab8aaf20421f150bccc4eda8523"
 
-  url "https://cdn.volanta.app/software/volanta-app/#{version.before_comma}-#{version.after_comma}/volanta-#{version.before_comma}.dmg"
+  url "https://cdn.volanta.app/software/volanta-app/#{version.csv.first}-#{version.csv.second}/volanta-#{version.csv.first}.dmg"
   name "Volanta"
   desc "Personal flight tracker"
   homepage "https://volanta.app/roadmap/"
