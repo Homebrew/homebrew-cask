792f60e95ce85808337d6120aef8fe0b52ca309a
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-10 23:55:50 +0800

    Update visual-paradigm-ce.rb

--- Casks/visual-paradigm-ce.rb
+++ Casks/visual-paradigm-ce.rb
@@ -2,7 +2,7 @@ cask "visual-paradigm-ce" do
   version "16.3,20211201"
   sha256 "21c62c868ea881c56791e2e6855eafcf6b2791f56bc6d01c99bfde7fe04f7a09"
 
-  url "https://www.visual-paradigm.com/downloads/vpce/Visual_Paradigm_CE_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
+  url "https://www.visual-paradigm.com/downloads/vpce/Visual_Paradigm_CE_#{version.csv.first.dots_to_underscores}_#{version.csv.second}_OSX_WithJRE.dmg"
   name "Visual Paradigm Community Edition"
   desc "All-in-one UML, SysML, BPMN Modeling Platform for Agile"
   homepage "https://www.visual-paradigm.com/"
