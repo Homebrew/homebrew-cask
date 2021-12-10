5d3f81ff4023b15a815c19b8bc76876f98a2d273
neutric <5984479+neutric@users.noreply.github.com>
2021-12-10 09:23:14 +0100

    Update the-battle-for-wesnoth from 1.16.0 to 1.16.1 (#115657)

--- Casks/the-battle-for-wesnoth.rb
+++ Casks/the-battle-for-wesnoth.rb
@@ -1,6 +1,6 @@
 cask "the-battle-for-wesnoth" do
-  version "1.16.0"
-  sha256 "20dd3a085794fd90fe63e19e43b0797d1f76aaa14f58446c4df0f99ecb9c7227"
+  version "1.16.1"
+  sha256 "a12c312fd21d4f2622c5340211696ea4b92c88ac1f56fe61585e39f545d7fed5"
 
   url "https://downloads.sourceforge.net/wesnoth/Wesnoth_#{version}.dmg",
       verified: "sourceforge.net/wesnoth/"
