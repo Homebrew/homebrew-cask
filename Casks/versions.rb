9a4fbba509dfe793c700d18d4df337b375f4da4f
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-10 23:51:13 +0800

    Update versions.rb

--- Casks/versions.rb
+++ Casks/versions.rb
@@ -2,7 +2,7 @@ cask "versions" do
   version "2.0.9,2020"
   sha256 "7eaca9270f8f78d60b2008abc42ad9465b6feda3e1e5aea524c5f3b027465d20"
 
-  url "https://updates.versionsapp.com/v#{version.major}/prod/Versions-#{version.before_comma}-#{version.after_comma}.zip"
+  url "https://updates.versionsapp.com/v#{version.major}/prod/Versions-#{version.csv.first}-#{version.csv.second}.zip"
   name "Versions"
   desc "Subversion client"
   homepage "https://versionsapp.com/"
