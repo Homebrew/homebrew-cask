93a1675a683896a7c97b280de329442e14d82e03
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-11 12:15:21 +0800

    Update xstation5.rb

--- Casks/xstation5.rb
+++ Casks/xstation5.rb
@@ -1,14 +1,17 @@
-# typed: false
-# frozen_string_literal: true
-
 cask "xstation5" do
+  # note: "5" is not a version number, but an intrinsic part of the product name
   version "2.34.0-Build.3"
   sha256 :no_check
 
-  url "https://www.xtb.com/pl/xstation5/xStation5-MacOS.dmg"
+  url "https://desktopxstation5.xtb.com/prod/update/mac/xStation5-MacOS.dmg"
   name "xStation5"
   desc "Desktop trading platform"
   homepage "https://www.xtb.com/"
+
+  livecheck do
+    url "https://xs5.xopenhub.pro/xtb_prod/desktop/mac/latest-mac.yml"
+    strategy :electron_builder
+  end
 
   app "xStation5.app"
 end
