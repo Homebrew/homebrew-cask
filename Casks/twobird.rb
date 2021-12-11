87c05ab20e77a4d2fdcf0b64811148a17e56bf0c
neutric <5984479+neutric@users.noreply.github.com>
2021-12-11 01:33:32 +0100

    twobird: add arch conditional

--- Casks/twobird.rb
+++ Casks/twobird.rb
@@ -1,8 +1,10 @@
 cask "twobird" do
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
+
   version "1.0.45"
   sha256 :no_check
 
-  url "https://www.twobird.com/download/mac-x64"
+  url "https://www.twobird.com/download/mac-#{arch}"
   name "Twobird"
   desc "Email client with collaborative notes"
   homepage "https://www.twobird.com/"
