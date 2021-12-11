d36413a92b532ef8f7dc34cce7d9ea16d96f82a4
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-11 12:18:44 +0800

    Update emmetapp.rb

--- Casks/emmetapp.rb
+++ Casks/emmetapp.rb
@@ -5,16 +5,24 @@ cask "emmetapp" do
   url "https://s3.amazonaws.com/emmet.app/emmetapp.dmg",
       verified: "s3.amazonaws.com/emmet.app/"
   name "emmetapp"
-  desc "Window manager you dream about"
+  desc "Tiling and stacking window manager and window resizing tool"
   homepage "https://emmetapp.com/"
 
+  livecheck do
+    url "https://s3.amazonaws.com/emmet.app/appcast.xml"
+    strategy :sparkle
+  end
+
   auto_updates true
-  depends_on macos: ">= :catalina"
+  depends_on macos: [
+    :catalina,
+    :big_sur,
+  ]
 
   app "emmetapp.app"
 
   zap trash: [
-    "~/Library/Application\ Scripts/com.speedyhill.emmetapp-LaunchAtLoginHelper",
+    "~/Library/Application Scripts/com.speedyhill.emmetapp-LaunchAtLoginHelper",
     "~/Library/Containers/com.speedyhill.emmetapp-LaunchAtLoginHelper",
     "~/Library/Group Containers/2BUA8C4S2C.com.agilebits",
     "~/Library/Preferences/com.speedyhill.emmetapp.plist",
