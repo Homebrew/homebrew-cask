150d38439052e764bdaa4048b1221ad2e21763e1
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-12 23:09:14 +0800

    Update appium-inspector.rb

--- Casks/appium-inspector.rb
+++ Casks/appium-inspector.rb
@@ -4,20 +4,15 @@ cask "appium-inspector" do
 
   url "https://github.com/appium/appium-inspector/releases/download/v#{version}/Appium-Inspector-#{version}-mac.zip"
   name "Appium Inspector GUI"
-  desc "GUI inspector for mobile apps and more, powered by a Appium server"
-  homepage "https://github.com/appium/appium-inspector"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
+  desc "GUI inspector for mobile apps"
+  homepage "https://github.com/appium/appium-inspector/"
 
   app "Appium Inspector.app"
 
   zap trash: [
     "~/Library/Application Support/appium-inspector",
-    "~/Library/Saved Application State/io.appium.inspector.savedState",
-    "~/Library/Preferences/io.appium.inspector.plist",
     "~/Library/Logs/Appium Inspector",
+    "~/Library/Preferences/io.appium.inspector.plist",
+    "~/Library/Saved Application State/io.appium.inspector.savedState",
   ]
 end
