9bb521a50663d5607f7a4453a99626d70957fa35
Bevan Kay <email@bevankay.me>
2021-12-13 09:06:56 +1100

    Update kekaexternalhelper.rb

--- Casks/kekaexternalhelper.rb
+++ Casks/kekaexternalhelper.rb
@@ -2,7 +2,7 @@ cask "kekaexternalhelper" do
   version "1.1.2,1.2.50"
   sha256 "4fb629045b9d887a6adb3280e791ff8ac7843b40b475bb88ece5c25c6cd5d70f"
 
-  url "https://github.com/aonez/Keka/releases/download/v#{version.after_comma}/KekaExternalHelper-v#{version.before_comma}.zip"
+  url "https://github.com/aonez/Keka/releases/download/v#{version.csv.second}/KekaExternalHelper-v#{version.csv.first}.zip"
   name "Keka External Helper"
   name "KekaDefaultApp"
   desc "Helper application for the Keka file archiver"
