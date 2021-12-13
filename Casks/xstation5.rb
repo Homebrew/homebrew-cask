6fd8eadcd75a38ff70882b9a2ad8e859d210d868
Bevan Kay <email@bevankay.me>
2021-12-13 11:40:06 +1100

    update livecheck url, use versioned url, add shasum

--- Casks/xstation5.rb
+++ Casks/xstation5.rb
@@ -1,15 +1,15 @@
 cask "xstation5" do
   # NOTE: "5" is not a version number, but an intrinsic part of the product name
   version "2.35.0-Build.3"
-  sha256 :no_check
+  sha256 "55b1e8699da5285e2e2efe69c7cdd0fa76a9119cd98f961f731903c7dda7bcd5"
 
-  url "https://desktopxstation5.xtb.com/prod/update/mac/xStation5-MacOS.dmg"
+  url "https://desktopxstation5.xtb.com/prod/update/mac/xStation5-#{version}-mac.zip"
   name "xStation5"
   desc "Desktop trading platform"
   homepage "https://www.xtb.com/"
 
   livecheck do
-    url "https://xs5.xopenhub.pro/xtb_prod/desktop/mac/latest-mac.yml"
+    url "https://desktopxstation5.xtb.com/prod/update/mac/latest-mac.yml"
     strategy :electron_builder
   end
 
