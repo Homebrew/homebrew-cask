5438501573d8f13320073a2a99de7f824ca48ca8
Rory Powis <rpowis@users.noreply.github.com>
2021-12-13 14:12:42 +0100

    Bump Centered.app to v3.4.0

--- Casks/centered.rb
+++ Casks/centered.rb
@@ -1,5 +1,5 @@
 cask "centered" do
-  version "3.3.1"
+  version "3.4.0"
   sha256 "a498db40f8a7f5f5c5a6a3d868c5046527746a006ee2e554afc970873e44f078"
 
   url "https://d3ni5jxar6n4fz.cloudfront.net/Centered-#{version}-universal.dmg",
