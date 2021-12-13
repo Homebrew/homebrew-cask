8b3a6c3e63f447a6b27dbe3d70e9e5fa6397d1e3
neutric <5984479+neutric@users.noreply.github.com>
2021-12-14 00:05:56 +0100

    Update freac from 1.1.5 to 1.1.5,a

--- Casks/freac.rb
+++ Casks/freac.rb
@@ -1,15 +1,15 @@
 cask "freac" do
-  version "1.1.5"
+  version "1.1.5,a"
 
   if MacOS.version <= :catalina
     sha256 "0e3d14e789b6d413db519348bfdade657af4324054ac3a47f6b389131422f1d7"
 
-    url "https://github.com/enzo1982/freac/releases/download/v#{version}/freac-#{version}-macos10.dmg",
+    url "https://github.com/enzo1982/freac/releases/download/v#{version.csv.first}/freac-#{version.delete(",")}-macos10.dmg",
         verified: "github.com/enzo1982/freac/"
   else
-    sha256 "3c310de32cbb5827deac721f758446556dfa4e19e9d345360b8250d1bb0d23a1"
+    sha256 "131b4037c90d996e2a798ec62a413aeefffbc7ee02d1616b807bd0708c3c8854"
 
-    url "https://github.com/enzo1982/freac/releases/download/v#{version}/freac-#{version}-macos11.dmg",
+    url "https://github.com/enzo1982/freac/releases/download/v#{version.csv.first}/freac-#{version.delete(",")}-macos11.dmg",
         verified: "github.com/enzo1982/freac/"
   end
 
