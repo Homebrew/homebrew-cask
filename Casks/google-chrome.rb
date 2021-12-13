c15505cbf41da26244e3914fb4e7110c61ad2e69
LanikSJ <laniksj@gmail.com>
2021-12-13 11:50:41 -0800

    Update google-chrome from 96.0.4664.93 to 96.0.4664.110

--- Casks/google-chrome.rb
+++ Casks/google-chrome.rb
@@ -1,7 +1,7 @@
 cask "google-chrome" do
   arch = Hardware::CPU.intel? ? "" : "universal/"
 
-  version "96.0.4664.93"
+  version "96.0.4664.110"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/#{arch}stable/GGRO/googlechrome.dmg"
