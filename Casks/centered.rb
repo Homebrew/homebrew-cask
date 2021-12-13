6f5af3439c175e8bf975c99b1dbb0a59bf92bbfe
Rory Powis <rpowis@users.noreply.github.com>
2021-12-13 15:02:16 +0100

    Add Centered.app v3.3.1 (#115693)
    * Add Centered.app v3.3.1
    
    * Update centered.rb
    
    * Update centered.rb
    
    * Add zap stanza for Centered
    
    * Bump Centered.app to v3.4.0
    
    * Fix updated SHA for Centered.app
    
    * Fix formatting of centered cask
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/centered.rb
@@ -0,0 +1,25 @@
+cask "centered" do
+  version "3.4.0"
+  sha256 "63d42c9f671a54443d39edb82a592ce1ba0ee671b503e8357e8adb61b2a01871"
+
+  url "https://d3ni5jxar6n4fz.cloudfront.net/Centered-#{version}-universal.dmg",
+      verified: "d3ni5jxar6n4fz.cloudfront.net/"
+  name "Centered"
+  desc "Flow state app for productivity"
+  homepage "https://www.centered.app/"
+
+  livecheck do
+    url "https://centered-desktop-release.s3.amazonaws.com/latest-mac.yml"
+    strategy :electron_builder
+  end
+
+  auto_updates true
+
+  app "Centered.app"
+
+  zap trash: [
+    "~/Library/Application Support/Centered",
+    "~/Library/Preferences/app.centered.mac.prod.plist",
+    "~/Library/Saved Application State/app.centered.mac.prod.savedState",
+  ]
+end
