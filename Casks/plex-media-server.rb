f955d7256ca5816ea2ab4560c1a03cba9bb08102
Bevan Kay <email@bevankay.me>
2021-12-14 09:58:32 +1100

    Update plex-media-server.rb

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -2,7 +2,7 @@ cask "plex-media-server" do
   version "1.25.2.5319,c43dc0277"
   sha256 "7f32ebf43aaf70a1e41b68044ce05c8a9d4771353f3cd21ef20a65a2ee41f10a"
 
-  url "https://downloads.plex.tv/plex-media-server-new/#{version.before_comma}-#{version.after_comma}/macos/PlexMediaServer-#{version.before_comma}-#{version.after_comma}-x86_64.zip"
+  url "https://downloads.plex.tv/plex-media-server-new/#{version.csv.first}-#{version.csv.second}/macos/PlexMediaServer-#{version.csv.first}-#{version.csv.second}-x86_64.zip"
   name "Plex Media Server"
   desc "Home media server"
   homepage "https://www.plex.tv/"
