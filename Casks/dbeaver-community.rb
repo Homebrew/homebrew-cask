4e42ecdb00be5382d54baa32e5db54d222cd8520
Jannik Reichenau <jannik.reichenau@schnittwerk.com>
2021-12-13 13:56:39 +0100

    Update dbeaver-community from 21.3.1 to 21.3.1

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -4,7 +4,7 @@ cask "dbeaver-community" do
   version "21.3.1"
 
   if Hardware::CPU.intel?
-    sha256 "1a7418a74e1105f02bfb0a66f625515aba1617d771d4ef38276ef2f3414d768c"
+    sha256 "3b32f460ed303d533c984362558b99a52dedf9307e051e406a893a8295969791"
   else
     sha256 "926603429c07fabea704b5d6ea2f75ac94dc60fd5463b9600ae3960732f712ef"
   end
