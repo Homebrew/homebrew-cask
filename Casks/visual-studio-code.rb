f965fa2892fd782cc18889a7e8b3344c980f2d65
Clément L <porkepix@gmail.com>
2021-12-14 22:43:46 +0100

    Update visual-studio-code from 1.63.0 to 1.63.1 (#115853)
    * Update visual-studio-code from 1.63.0 to 1.63.1
    
    * Update visual-studio-code.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -1,12 +1,12 @@
 cask "visual-studio-code" do
   arch = Hardware::CPU.intel? ? "darwin" : "darwin-arm64"
 
-  version "1.63.0"
+  version "1.63.1"
 
   if Hardware::CPU.intel?
-    sha256 "926bc81f6c61af4ebe01b897e3c908324d8469be6fea552493e383899f1fcfbb"
+    sha256 "2f8ee67844973c36405eb7f3568aa8cac01efa69f7c3d5d17f8db92661d76f8e"
   else
-    sha256 "a516bf67588613274708860da2c2a1507ff5b90515960431a1ad9d1f8481b9b2"
+    sha256 "ff9a15f50ce35313a59700527b6f7ea06ef3dedf9ac9f3cf88475cba97c4e821"
   end
 
   url "https://update.code.visualstudio.com/#{version}/#{arch}/stable"
@@ -17,7 +17,6 @@ cask "visual-studio-code" do
 
   livecheck do
     url "https://update.code.visualstudio.com/api/update/#{arch}/stable/VERSION"
-    strategy :page_match
     regex(/"productVersion"\s*:\s*"(\d+(:?\.\d+)+)"/)
   end
 
@@ -27,14 +26,14 @@ cask "visual-studio-code" do
   binary "#{appdir}/Visual Studio Code.app/Contents/Resources/app/bin/code"
 
   zap trash: [
+    "~/.vscode",
     "~/Library/Application Support/Code",
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.microsoft.vscode.sfl*",
-    "~/Library/Caches/com.microsoft.VSCode",
     "~/Library/Caches/com.microsoft.VSCode.ShipIt",
+    "~/Library/Caches/com.microsoft.VSCode",
     "~/Library/Preferences/ByHost/com.microsoft.VSCode.ShipIt.*.plist",
     "~/Library/Preferences/com.microsoft.VSCode.helper.plist",
     "~/Library/Preferences/com.microsoft.VSCode.plist",
     "~/Library/Saved Application State/com.microsoft.VSCode.savedState",
-    "~/.vscode",
   ]
 end
