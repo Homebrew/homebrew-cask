000ba769eb26c50147a2ab84155acfa733046c56
Andrew Dunning <adunning@users.noreply.github.com>
2021-12-14 09:03:13 +0000

    Oxygen XML Editor: use unversioned URL
    Builds can change without a version update; see #28632 and #115739.

--- Casks/oxygen-xml-editor.rb
+++ Casks/oxygen-xml-editor.rb
@@ -1,23 +1,13 @@
 cask "oxygen-xml-editor" do
-  arch = Hardware::CPU.intel? ? "" : "-aarch64"
+  arch = Hardware::CPU.intel? ? "" : "-openjdk-aarch64"
 
-  version "24.0"
+  version :latest
+  sha256 :no_check
 
-  if Hardware::CPU.intel?
-    sha256 "0621c6833c82ebd7931a198ce5a973cb2646d4b0dc827224cfa6abbd53383997"
-  else
-    sha256 "68f071bf58e08d48e227d9bd65b0a55211305680d6ecee87de0d726c20129251"
-  end
-
-  url "https://archives.oxygenxml.com/Oxygen/Editor/InstData#{version}/MacOSX/VM/oxygen-openjdk#{arch}.dmg"
+  url "https://www.oxygenxml.com/InstData/Editor/MacOSX/VM/oxygen#{arch}.dmg"
   name "oXygen XML Editor"
   desc "Tools for XML editing, including Oxygen XML Developer and Author"
   homepage "https://www.oxygenxml.com/xml_editor.html"
-
-  livecheck do
-    url "https://www.oxygenxml.com/rssBuildID.xml"
-    regex(/Oxygen\sXML\sEditor\sversion\s(\d+(?:\.\d+)+)/i)
-  end
 
   suite "Oxygen XML Editor"
 end
