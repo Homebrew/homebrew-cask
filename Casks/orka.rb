cca8ada1b16438aa26b0052a1f8fa5672aca1ed4
radeva <vesita@gmail.com>
2021-12-14 23:43:34 +0200

    Update orka from 1.6.0 to 1.7.0 (#115851)
    * Update orka from 1.6.0 to 1.7.0
    
    * Update orka.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/orka.rb
+++ Casks/orka.rb
@@ -1,6 +1,6 @@
 cask "orka" do
-  version "1.6.0"
-  sha256 "accc2136bcd68c8b83463d9e4a77d755479a3e90b111452d8717b87160343708"
+  version "1.7.0"
+  sha256 "0e07e9726a07d72ccd11230a9b29d3440e8f11ee3703d9be6ec3b5c7015378b4"
 
   url "https://cli-builds-public.s3-eu-west-1.amazonaws.com/official/#{version}/macos/orka.pkg",
       verified: "cli-builds-public.s3-eu-west-1.amazonaws.com/official/"
@@ -10,8 +10,7 @@ cask "orka" do
 
   livecheck do
     url "https://orkadocs.macstadium.com/docs/downloads"
-    strategy :page_match
-    regex(%r{href=.*?/official/(\d+(?:\.\d+)*)/macos/orka\.pkg}i)
+    regex(%r{href=.*?/official/(\d+(?:\.\d+)+)/macos/orka\.pkg}i)
   end
 
   pkg "orka.pkg"
