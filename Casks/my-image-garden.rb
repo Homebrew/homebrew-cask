4d77a54980136588b676489e4fa52ad5896a4ad7
Ross Goldberg <484615+rgoldberg@users.noreply.github.com>
2021-12-14 03:43:57 -0500

    Upgraded my-image-garden to version 3.6.6,04

--- Casks/my-image-garden.rb
+++ Casks/my-image-garden.rb
@@ -1,13 +1,14 @@
 cask "my-image-garden" do
-  version "3.6.5"
-  sha256 "d40260bb5786738a40560b1a6834c4b284c7c9986ae7ce2d96c90e820d7f8f73"
+  version "3.6.6,04"
+  sha256 "1b83b4695cd8d84c7807a06a5ba4c7e46cc5cfd73bdaf606a327e7341f573bb2"
 
-  url "https://gdlp01.c-wss.com/gds/2/0200006062/02/mmig-mac-#{version.dots_to_underscores}-ea11.dmg",
+  url "https://gdlp01.c-wss.com/gds/2/0200006062/#{version.csv.second}/mmig-mac-#{version.csv.first.dots_to_underscores}-ea11.dmg",
       verified: "c-wss.com/"
   name "Canon My Image Garden"
+  desc "Photo editing and printing tool"
   homepage "https://support-asia.canon-asia.com/?personal"
 
-  pkg "My Image Garden V#{version.no_dots}.pkg"
+  pkg "My Image Garden V#{version.csv.first.no_dots}.pkg"
 
   uninstall pkgutil: "jp.co.canon.MyImageGarden",
             quit:    "jp.co.canon.MyImageGarden"
