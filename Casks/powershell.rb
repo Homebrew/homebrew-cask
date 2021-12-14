612fd3e7ae12f3b0f1c864a558b8279d4e8b6043
tuzi3040 <28290621+tuzi3040@users.noreply.github.com>
2021-12-15 03:15:12 +0800

    powershell: fix sha256 to lowercase

--- Casks/powershell.rb
+++ Casks/powershell.rb
@@ -6,7 +6,7 @@ cask "powershell" do
   if Hardware::CPU.intel?
     sha256 "f73682927bdadb127cf0c18641d5efe9ea12e53053e65d5c54f06fd4ef413a4a"
   else
-    sha256 "6293A48CEAD807DD3C9F932176021533254BD6883754C5574E36A1775D797959"
+    sha256 "6293a48cead807dd3c9f932176021533254bd6883754c5574e36a1775d797959"
   end
 
   url "https://github.com/PowerShell/PowerShell/releases/download/v#{version}/powershell-#{version}-osx-#{arch}.pkg"
