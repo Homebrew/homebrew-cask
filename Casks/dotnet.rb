897599c0e4504f7dcc3ef8375797a2b433299af4
Jeffrey C <990135+spaz926@users.noreply.github.com>
2021-12-15 06:52:07 -0600

    Update dotnet from 6.0.0 to 6.0.1

--- Casks/dotnet.rb
+++ Casks/dotnet.rb
@@ -2,8 +2,8 @@ cask "dotnet" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
   if Hardware::CPU.intel?
-    version "6.0.0,03e62824-4061-45dc-8777-93e82205148d,81489074c065675bd713a9cbeb995bef"
-    sha256 "fedbddb71e18d68279433e22326220225b3ee45209966fce818ac34349eefef4"
+    version "6.0.1,6824b342-4659-40a1-ab73-25dd43e5e225,e57b1bfa437cf152f2d7064246bfb653"
+    sha256 "0be7228ce53c4766d19b48afad0769d1b420c5c4f38a81e5cae37aa3ed72cdbc"
   else
     version "6.0.0,1bc784af-565c-42d5-8db2-9988c8d2b65f,644cea0fe2f712ad297b6eb49147f446"
     sha256 "dab12623b00707a8e2c5e29438de7a9e738880111d59a5be215fda4a427a9d59"
