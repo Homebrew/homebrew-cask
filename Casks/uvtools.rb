784bfa443c0a8e846fc39a9df5e8561164e7f08b
neutric <5984479+neutric@users.noreply.github.com>
2021-12-15 16:56:25 +0100

    Update uvtools from 2.23.6 to 2.26.0

--- Casks/uvtools.rb
+++ Casks/uvtools.rb
@@ -1,6 +1,6 @@
 cask "uvtools" do
-  version "2.23.6"
-  sha256 "936bc0db28a2eafa944245ceb8ab9f996c1afea72ad70fdb941eb91588f7d326"
+  version "2.26.0"
+  sha256 "91583dd07c4e8585075f228ea06de59b77b1d46df7af7b8dc98017bd1ee137fb"
 
   url "https://github.com/sn4k3/UVtools/releases/download/v#{version}/UVtools_osx-x64_v#{version}.zip"
   name "UVtools"
