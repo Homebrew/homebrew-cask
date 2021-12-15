aa6b4b705c91622974ef8a50c4bdbb5d377f5e54
Yusuf Kursad Kaya <ykursadkaya@gmail.com>
2021-12-15 18:07:44 +0300

    Update postman from 9.4.1 to 9.5.0

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,12 +1,12 @@
 cask "postman" do
   arch = Hardware::CPU.intel? ? "osx64" : "osx_arm64"
 
-  version "9.4.1"
+  version "9.5.0"
 
   if Hardware::CPU.intel?
-    sha256 "800e4afab118a06723fb08e2297bee326b42f602c0ac2711cc77376025fefdb9"
+    sha256 "52f3f5f2dcb80d0e6070ff474d78c8f8e117a8f1683c460d8ada9013d2e7a2cf"
   else
-    sha256 "144b1884e6747d0e4fb987ddb24999274832fe3d3fa96eeadc6a0be44615a698"
+    sha256 "0c039244b6afe60fb836acb2a784d8e642af5a845ba70b3cee8785099cf5edeb"
   end
 
   url "https://dl.pstmn.io/download/version/#{version}/#{arch}",
