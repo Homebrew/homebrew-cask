6ef6b63c3cb6ad91b08b5ff0b21b1edf0a137d42
Benjamin Mironer <bmironer@gmail.com>
2021-12-16 09:56:16 +0100

    Update silnite from 5 to 6

--- Casks/silnite.rb
+++ Casks/silnite.rb
@@ -1,6 +1,6 @@
 cask "silnite" do
-  version "5,2020.08"
-  sha256 "bd4a314918710af6a2eaeccb8dbf26724b5075a178118f33a719161af697eb7c"
+  version "6,2021.12"
+  sha256 "932117c39f351e6933f295a52b222cde4bf18d3a069dadc9bd5cffc4caab965a"
 
   url "https://eclecticlightdotcom.files.wordpress.com/#{version.after_comma.major}/#{version.after_comma.minor}/silnite#{version.before_comma}.zip",
       verified: "eclecticlightdotcom.files.wordpress.com/"
