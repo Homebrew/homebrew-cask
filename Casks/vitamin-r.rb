96395d7b6b269ac7409acdb4fe83c43be3549c23
neutric <5984479+neutric@users.noreply.github.com>
2021-12-16 14:56:50 +0100

    Update vitamin-r from 3.28 to 3.31

--- Casks/vitamin-r.rb
+++ Casks/vitamin-r.rb
@@ -3,8 +3,8 @@ cask "vitamin-r" do
     version "2.58"
     sha256 "c6c631430b44359aa022d9ca5ca6e98dbdf7258f2ceae0353f344a035682661e"
   else
-    version "3.28"
-    sha256 "5b3403deb748ea87b8ba1e1923b1277da642a9d3ec9e66fd5be0ac1a634ecb0a"
+    version "3.31"
+    sha256 "6c5ce3926060b7e3616527fc3c1f0d2a5cd8a0be4d9f5496c099a33be993312b"
   end
 
   url "https://www.publicspace.net/download/signedVitamin#{version.major}.zip"
