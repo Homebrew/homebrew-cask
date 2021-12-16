b5f33e239ac6e4284eb0a1e01b90ce5ca04bfb04
Thomas <thomassth@users.noreply.github.com>
2021-12-17 00:22:26 +0800

    Create macro-recorder.rb

---
+++ Casks/macro-recorder.rb
@@ -0,0 +1,11 @@
+cask "macro-recorder" do
+  version "2.0.69"
+  sha256 :no_check
+
+  url "https://www.macrorecorder.com/MacroRecorderSetup.dmg"
+  name "macro-recorder"
+  desc "Record mouse and keyboard actions"
+  homepage "https://www.macrorecorder.com/"
+
+  app "MacroRecorder.app"
+end
