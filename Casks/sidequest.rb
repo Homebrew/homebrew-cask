3e21545df8a66e6e96ba6f5c32bd0828bf4b63c1
Sergio Ortega Sanchez <ortega@tune-is.de>
2021-12-17 12:27:24 +0100

    Update sidequest from 0.10.25 to 0.10.26 (#115956)

--- Casks/sidequest.rb
+++ Casks/sidequest.rb
@@ -1,6 +1,6 @@
 cask "sidequest" do
-  version "0.10.25"
-  sha256 "5940c61d203e27bb76306e8be11614bd5b7a1b857500cd9970d84e46da48761f"
+  version "0.10.26"
+  sha256 "92d7c4eaf165765c210035ce60bc0cfb1d75a93eb646e153ace68b2d52097ec4"
 
   url "https://github.com/SideQuestVR/SideQuest/releases/download/v#{version}/SideQuest-#{version}.dmg",
       verified: "github.com/SideQuestVR/SideQuest/"
