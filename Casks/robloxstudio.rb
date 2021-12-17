b491cbefdc4e02fb9948eece8c0180ced71ae7d1
Bevan Kay <email@bevankay.me>
2021-12-17 23:05:01 +1100

    Update and rename roblox-studio.rb to robloxstudio.rb

--- Casks/robloxstudio.rb
+++ Casks/robloxstudio.rb
@@ -1,12 +1,17 @@
-cask "roblox-studio" do
+cask "robloxstudio" do
   version :latest
   sha256 :no_check
 
   url "https://setup.rbxcdn.com/mac/RobloxStudio.dmg",
       verified: "setup.rbxcdn.com"
-  name "roblox-studio"
+  name "RobloxStudio"
   desc "Building tool for Roblox"
   homepage "https://www.roblox.com/create"
+  
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   app "RobloxStudio.app"
 end
