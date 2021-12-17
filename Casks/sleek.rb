53ba50fddeee5d3b77c311b576ddc27ef3d72dc3
ransome <11188741+ransome1@users.noreply.github.com>
2021-12-17 13:45:05 +0100

    Update sleek.rb

--- Casks/sleek.rb
+++ Casks/sleek.rb
@@ -2,7 +2,7 @@ cask "sleek" do
   version "1.1.5"
   sha256 "3b074604e8c261499462f12a97818df9edf4dcbd1034d14611a72153f8dcb4da"
   url "https://github.com/ransome1/sleek/releases/download/v#{version}/sleek-#{version}-mac.dmg"
-  
+
   name "sleek"
   desc "Todo app based on todo.txt"
   homepage "https://github.com/ransome1/sleek"
