27351b77ddff166962a6fda6ebcc6b2c07dca66b
Adrian Thomas <thomas@equinux.com>
2021-12-17 16:18:48 +0000

    Add VPN Tracker 365 21.7.3

--- Casks/vpntracker.rb
+++ Casks/vpntracker.rb
@@ -4,11 +4,10 @@ cask "vpntracker" do
 
   url "https://www.vpntracker.com/goto/HPdownload/VPNT365Latest"
   name "VPN Tracker 365"
-  desc "The ultimate VPN client for macOS with IPsec, L2TP, OpenVPN, PPTP, SonicWALL SSL, AnyConnect SSL, Fortinet SSL and SSTP protocol support."
+  desc "VPN client: IPsec, L2TP, OpenVPN, PPTP, SSTP, SonicWALL/AnyConnect/Fortinet SSL"
   homepage "https://vpntracker.com/"
 
   depends_on macos: ">= :el_capitan"
 
   app "VPN Tracker 365.app"
-
 end
