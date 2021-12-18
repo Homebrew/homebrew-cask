e0c1c8ccb9f52139b288d5866767634413814d76
Yusuf Kürşad Kaya <ykursadkaya@gmail.com>
2021-12-18 04:22:02 +0300

    Update skim from 1.6.6 to 1.6.7 (#116031)
    * Update skim from 1.6.6 to 1.6.7
    
    * Update skim.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/skim.rb
+++ Casks/skim.rb
@@ -1,8 +1,8 @@
 cask "skim" do
-  version "1.6.6,136"
-  sha256 "cac1ab8e3374631628de31e2402297372f72f14f693e4cb557bdff92e9e71e10"
+  version "1.6.7,137"
+  sha256 "03a448e8c491f207af3fb45b8524247f74e8e6be9f26c00ea29ac3a164d728eb"
 
-  url "https://downloads.sourceforge.net/skim-app/Skim/Skim-#{version.before_comma}/Skim-#{version.before_comma}.dmg",
+  url "https://downloads.sourceforge.net/skim-app/Skim/Skim-#{version.csv.first}/Skim-#{version.csv.first}.dmg",
       verified: "downloads.sourceforge.net/skim-app/"
   name "Skim"
   desc "PDF reader and note-taking application"
