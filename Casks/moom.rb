8b7edc7df62c09587f6e9f473791da5d09af3dae
Bevan Kay <email@bevankay.me>
2021-12-18 11:38:13 +1100

    Update moom.rb

--- Casks/moom.rb
+++ Casks/moom.rb
@@ -2,7 +2,7 @@ cask "moom" do
   version "3.2.24,3341"
   sha256 "f65b4a70ed4de66374c91d0a1a6f00adbd7089c21a92c354fd8c485422cc20df"
 
-  url "https://manytricks.com/download/_do_not_hotlink_/moom#{version.before_comma.no_dots}.dmg"
+  url "https://manytricks.com/download/_do_not_hotlink_/moom#{version.csv.first.no_dots}.dmg"
   name "Moom"
   desc "Utility to move and zoom windows—on one display"
   homepage "https://manytricks.com/moom/"
