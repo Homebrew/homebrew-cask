78d3ef96ab1b39efe1623beaef480464cbf8fa02
Dmitrij Podabed <iam@itfarrier.com>
2021-12-19 01:05:28 +0300

    Add PD Runner.app v0.3.5

---
+++ Casks/pd-runner.rb
@@ -0,0 +1,11 @@
+cask "pd-runner" do
+  version "0.3.5"
+  sha256 "c0b96c361891c6d40854da77a05cb412a7e3e22cc349f7fef5abd35534fa0150"
+
+  url "https://github.com/lihaoyun6/PD-Runner/releases/download/#{version}/PD-Runner_v#{version}_Universal2.dmg"
+  name "pd-runner"
+  desc "Tool for Parallels Desktop can start VMs even the trial period has expired"
+  homepage "https://github.com/lihaoyun6/PD-Runner"
+
+  app "PD Runner.app"
+end
