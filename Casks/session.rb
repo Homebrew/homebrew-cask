14f2257a748ec78aacc3e4ebff6722332ae11e8d
neutric <5984479+neutric@users.noreply.github.com>
2021-12-18 16:39:13 +0100

    session: update url based on redirect

--- Casks/session.rb
+++ Casks/session.rb
@@ -2,8 +2,8 @@ cask "session" do
   version "1.7.6"
   sha256 "7056ed33a377737cd68ae9d229c0aa1f1e449ce5fcf77395d8eae323f85be478"
 
-  url "https://github.com/loki-project/session-desktop/releases/download/v#{version}/session-desktop-mac-#{version}.dmg",
-      verified: "github.com/loki-project/session-desktop/"
+  url "https://github.com/oxen-io/session-desktop/releases/download/v#{version}/session-desktop-mac-#{version}.dmg",
+      verified: "github.com/oxen-io/session-desktop/"
   name "Session"
   desc "Onion routing based messenger"
   homepage "https://getsession.org/"
