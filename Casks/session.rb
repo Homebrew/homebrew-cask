bab56290f066abe187817add677d7590999514eb
neutric <5984479+neutric@users.noreply.github.com>
2021-12-18 16:36:35 +0100

    Update session from 1.7.4 to 1.7.6

--- Casks/session.rb
+++ Casks/session.rb
@@ -1,6 +1,6 @@
 cask "session" do
-  version "1.7.4"
-  sha256 "7b01c6e2b8a327aadc272da4063783e692a3dd64942ab1338439de70fdbf010c"
+  version "1.7.6"
+  sha256 "7056ed33a377737cd68ae9d229c0aa1f1e449ce5fcf77395d8eae323f85be478"
 
   url "https://github.com/loki-project/session-desktop/releases/download/v#{version}/session-desktop-mac-#{version}.dmg",
       verified: "github.com/loki-project/session-desktop/"
