c1e66a75671cbfd17c37b25d482aa9c2212b2a35
Jacob Ford <j@cobford.com>
2021-12-18 22:32:22 -0500

    Update URL & livecheck for Universal License

--- Casks/softorino-youtube-converter.rb
+++ Casks/softorino-youtube-converter.rb
@@ -2,13 +2,13 @@ cask "softorino-youtube-converter" do
   version "3.4.9"
   sha256 "e0cf1a43c811fcfb7e712c21c3a137def62c7a6c2577aa25398c2670155a4232"
 
-  url "https://shining.softorino.com/shine_uploads/softorinoyoutubeconverter2mac_#{version}.dmg"
+  url "https://ushining.softorino.com/shine_uploads/softorinoyoutubeconverter2mac_#{version}.dmg"
   name "Softorino YouTube Converter"
   desc "YouTube downloader and converter"
   homepage "https://softorino.com/youtube-converter/"
 
   livecheck do
-    url "https://shining.softorino.com/appcast.php?abbr=syc2m"
+    url "https://api.softorino.com/live/app-manager/v3/syc2/mac/updates"
     strategy :sparkle
   end
 
