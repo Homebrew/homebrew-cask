295c1ad8079f8d60adcc0418e78b9d2787e6ca50
Bevan Kay <email@bevankay.me>
2021-12-20 08:42:06 +1100

    Update loginputmac.rb

--- Casks/loginputmac.rb
+++ Casks/loginputmac.rb
@@ -2,7 +2,7 @@ cask "loginputmac" do
   version "2.5.2,24120"
   sha256 "582a93566cbb8bf79bfbfbae5bd404985506e59c57479a202ecb88f5e4aa630f"
 
-  url "https://loginput-mac2.totest.top/LogInputMac#{version.major}.app#{version.after_comma}.zip",
+  url "https://loginput-mac2.totest.top/LogInputMac#{version.major}.app#{version.csv.after}.zip",
       verified: "loginput-mac2.totest.top/"
   name "LoginputMac"
   desc "Chinese input method"
