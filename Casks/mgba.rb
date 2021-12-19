56c5d8fe8214898f695c878fddf042691a9cb881
dbrookman <53625739+dbrookman@users.noreply.github.com>
2021-12-19 16:40:45 -0500

    Update mgba from 0.9.2 to 0.9.3 (#116064)

--- Casks/mgba.rb
+++ Casks/mgba.rb
@@ -1,8 +1,8 @@
 cask "mgba" do
-  version "0.9.2"
-  sha256 "f58eb506066b5586bb0cfe20fc170bde2ca5c1de4eb4b324b19dc1cbe5ed71d0"
+  version "0.9.3"
+  sha256 "38d91f4861d9e05a4e26fea0667061399b488e8783c9d60ea35fbf801b1cfde8"
 
-  url "https://github.com/mgba-emu/mgba/releases/download/#{version}/mGBA-#{version}-osx.tar.xz",
+  url "https://github.com/mgba-emu/mgba/releases/download/#{version}/mGBA-#{version}-osx.dmg",
       verified: "github.com/mgba-emu/mgba/"
   name "mGBA"
   desc "Game Boy Advance emulator"
@@ -13,7 +13,7 @@ cask "mgba" do
     strategy :github_latest
   end
 
-  app "mGBA-#{version}-osx/Applications/mGBA.app"
+  app "mGBA.app"
 
   zap trash: [
     "~/Library/Preferences/com.endrift.mgba-qt.plist",
