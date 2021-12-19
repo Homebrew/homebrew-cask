d31492560682af0ef36308f40cb387c100045083
Jacob Ford <j@cobford.com>
2021-12-18 23:30:49 -0500

    Add version & SHA

--- Casks/waltr-heic-converter.rb
+++ Casks/waltr-heic-converter.rb
@@ -1,6 +1,6 @@
 cask "waltr-heic-converter" do
-  version ""
-  sha256 ""
+  version "1.0.2"
+  sha256 "6e716143b73cf314e8fd1728a466ede9d7b99b1ec2364b55a7dad818ffcd1ab2"
 
   url "https://dl.devmate.com/com.softorino.WaltrHeicConverter/#{version}/1537972843/WALTRHEICConverter-#{version}.zip"
   name "WALTR HEIC Converter"
