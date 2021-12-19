f1da5ea6ff7deae9ecaf225d4f6f432fcfa7239b
Jacob Ford <j@cobford.com>
2021-12-18 23:30:27 -0500

    Find versioned URL (but has timestamp)

--- Casks/waltr-heic-converter.rb
+++ Casks/waltr-heic-converter.rb
@@ -2,7 +2,7 @@ cask "waltr-heic-converter" do
   version ""
   sha256 ""
 
-  url "https://dl.devmate.com/com.softorino.WaltrHeicConverter/WALTRHEICConverter.dmg"
+  url "https://dl.devmate.com/com.softorino.WaltrHeicConverter/#{version}/1537972843/WALTRHEICConverter-#{version}.zip"
   name "WALTR HEIC Converter"
   desc "Drag-and-drop HEIC to JPEG image converter"
   homepage "https://softorino.com/heic-converter/"
