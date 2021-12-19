7323aa12f0ea21409931e1164ee9e0d4fc971413
Jacob Ford <j@cobford.com>
2021-12-18 22:49:27 -0500

    Create folder-colorizer

---
+++ Casks/folder-colorizer.rb
@@ -0,0 +1,11 @@
+cask "folder-colorizer" do
+  version "4.0.0"
+  sha256 "4f9ff6d49b34b01f874e88af6c83138dc198f8444113a28c3780c78c73868806"
+
+  url "https://ushining.softorino.com/shine_uploads/foldercolorizermac_#{version}.dmg"
+  name "Folder Colorizer"
+  desc "Folder icon editor & manager"
+  homepage "https://softorino.com/folder-colorizer-mac/"
+
+  app "Folder Colorizer.app"
+end
