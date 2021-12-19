c49b2104f6b1e1b808916138e8afc0a5079b1ed8
Jacob Ford <j@cobford.com>
2021-12-18 22:15:50 -0500

    Update download & livecheck URLs for Universal License

--- Casks/waltr-pro.rb
+++ Casks/waltr-pro.rb
@@ -2,13 +2,13 @@ cask "waltr-pro" do
   version "3.7.98"
   sha256 "3818e93ac674c54b199148ebbfb5c2742051cb6580db514fb76a26dbc4a35282"
 
-  url "https://shining.softorino.com/shine_uploads/waltrpromac_#{version}.dmg"
+  url "https://ushining.softorino.com/shine_uploads/waltrpromac_#{version}.dmg"
   name "WALTR PRO"
   desc "Media conversion and direct transfer tool for Apple devices"
   homepage "https://softorino.com/waltr/"
 
   livecheck do
-    url "https://api.softorino.com/v1/app-manager/waltrpro/mac/updates"
+    url "https://api.softorino.com/live/app-manager/v3/waltrpro/mac/updates"
     strategy :sparkle
   end
 
