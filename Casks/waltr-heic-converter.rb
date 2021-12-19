51c5e2e5c077299ef38b70be18439d65d3e7dda8
Jacob Ford <j@cobford.com>
2021-12-18 23:21:58 -0500

    Get basics, still no versioned URL

---
+++ Casks/waltr-heic-converter.rb
@@ -0,0 +1,11 @@
+cask "waltr-heic-converter" do
+  version ""
+  sha256 ""
+
+  url "https://dl.devmate.com/com.softorino.WaltrHeicConverter/WALTRHEICConverter.dmg"
+  name "WALTR HEIC Converter"
+  desc "Drag-and-drop HEIC to JPEG image converter"
+  homepage "https://softorino.com/heic-converter/"
+
+  app "WALTR HEIC Converter.app"
+end
