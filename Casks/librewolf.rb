ded4d0906d687471f722552502b6326fd52bc264
neutric <5984479+neutric@users.noreply.github.com>
2021-12-19 22:52:21 +0100

    librewolf: fix livecheck

--- Casks/librewolf.rb
+++ Casks/librewolf.rb
@@ -9,7 +9,7 @@ cask "librewolf" do
     sha256 "b0fbb663823e8c2f6b4aa44341d1567b31d079ea59dcc1e00c2cdbb0ca0e6226"
   end
 
-  url "https://gitlab.com/librewolf-community/browser/macos/uploads/#{version.csv.second}/librewolf-#{version.csv.first}#{arch}.dmg",
+  url "https://gitlab.com/librewolf-community/browser/macos/uploads/#{version.csv.third}/librewolf-#{version.csv.first}-#{version.csv.second}#{arch}.dmg",
       verified: "gitlab.com/librewolf-community/browser/macos/"
   name "LibreWolf"
   desc "Web browser"
@@ -17,10 +17,10 @@ cask "librewolf" do
 
   livecheck do
     url "https://gitlab.com/api/v4/projects/13853965/releases"
-    regex(%r{/(\w+)/librewolf[._-](\d+(?:\.\d+)+(?:-\d+)?)#{arch}\.dmg}i)
+    regex(%r{/(\w+)/librewolf[._-](\d+(?:\.\d+)+)-(\d+)#{arch}\.dmg}i)
     strategy :page_match do |page, regex|
       page.scan(regex).map do |match|
-        "#{match[1]},#{match[0]}"
+        "#{match[1]},#{match[2]},#{match[0]}"
       end
     end
   end
