b911b3554e3f7121ff8f2496d9521c182848fd33
neutric <5984479+neutric@users.noreply.github.com>
2021-12-20 01:16:33 +0100

    Update xampp from 8.0.13-0 to 8.1.0-0

--- Casks/xampp.rb
+++ Casks/xampp.rb
@@ -1,6 +1,6 @@
 cask "xampp" do
-  version "8.0.13-0"
-  sha256 "585b4e804511053f0fafac2e37fdc4c0eb6400ddd56724acb5af2ca71b6eb603"
+  version "8.1.0-0"
+  sha256 "36ab95fc9ac74ce64463c63baa97f9833badbace82535ebdca7f996a19f3b3be"
 
   url "https://downloadsapachefriends.global.ssl.fastly.net/xampp-files/#{version.split("-").first}/xampp-osx-#{version}-installer.dmg",
       verified: "downloadsapachefriends.global.ssl.fastly.net/xampp-files/"
