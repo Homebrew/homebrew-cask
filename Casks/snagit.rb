deeddd363e7deb7bbfd70c4c8ab520fc9631a80f
Carson Greene <c.greene@techsmith.com>
2021-12-20 10:14:29 -0500

    Update snagit from 2022.0.0 to 2022.0.1

--- Casks/snagit.rb
+++ Casks/snagit.rb
@@ -1,6 +1,6 @@
 cask "snagit" do
-  version "2022.0.0"
-  sha256 "e76bcab969d730ce6657cfe868bde50ff0622f99a33efc29d389cae9a4da52ac"
+  version "2022.0.1"
+  sha256 "b4b5b0e6a8eb66ab63f4a27f73ee62269be2c77e4b097708996d51b6d92f94ed"
 
   url "https://download.techsmith.com/snagitmac/releases/#{version.major[-2..]}#{version.minor_patch.no_dots}/snagit.dmg"
   name "Snagit"
@@ -8,10 +8,9 @@ cask "snagit" do
   homepage "https://www.techsmith.com/screen-capture.html"
 
   livecheck do
-    url "https://www.techsmith.com/api/v/1/products/getallversions/100"
-    strategy :page_match do |page|
-      v = JSON.parse(page).first
-      "20#{v["Major"]}.#{v["Minor"]}.#{v["Maintenance"]}"
+    url "https://www.techsmith.com/redirect.asp?target=sufeedurl&product=snagitmac&ver=#{version}&lang=enu&os=mac"
+    strategy :sparkle do |item|
+      "#{item.short_version},#{item.version}:#{item.url[%r{/(\d+)/[^/]+\.zip}i, 1]}"
     end
   end
 
