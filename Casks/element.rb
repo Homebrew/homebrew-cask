323c0b4f7c4ace055a5680fc7c5806b0773c9c22
Hannes Braun <hannesbraun@mail.de>
2021-12-21 10:37:06 +0100

    Update element from 1.9.7 to 1.9.8 (#116136)

--- Casks/element.rb
+++ Casks/element.rb
@@ -1,6 +1,6 @@
 cask "element" do
-  version "1.9.7"
-  sha256 "ac2259663600b2384545ab1cf9a3578d3b8f081469c473fbeb078fa11cb0acda"
+  version "1.9.8"
+  sha256 "57347910e9321417dda64d3a8631d76180dec11ab5126f77352162badfb1dd30"
 
   url "https://packages.riot.im/desktop/install/macos/Element-#{version}-universal.dmg",
       verified: "packages.riot.im/desktop/"
