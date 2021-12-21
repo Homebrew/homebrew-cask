7727f0adb8cb702a6bfa7f9ee37f489d6d164028
jeff <i-v5@qq.com>
2021-12-21 14:11:00 +0800

    Update switchhosts from 4.0.3.6070 to 4.1.0.6076

--- Casks/switchhosts.rb
+++ Casks/switchhosts.rb
@@ -1,13 +1,13 @@
 cask "switchhosts" do
-  version "4.0.3.6070"
+  version "4.1.0.6076"
 
   if Hardware::CPU.intel?
-    sha256 "bba1430d6253ee0a27a474f3d8ad090c639b0a5ab81ec1d0ff42abdf3781c394"
-    url "https://github.com/oldj/SwitchHosts/releases/download/v#{version.major_minor_patch}/SwitchHosts_#{version}.dmg",
+    sha256 "ba0e47d5d255f610a578641306d6aa8f13797d2b8da86c3b04ca08cccd774886"
+    url "https://github.com/oldj/SwitchHosts/releases/download/v#{version.major_minor_patch}/SwitchHosts_mac_x64_#{version}.dmg",
         verified: "github.com/oldj/SwitchHosts/"
   else
-    sha256 "e7c02ff1a5a69d456cd65384c7a5351dfed3354a17f05694efbb6cbbde1d6f7d"
-    url "https://github.com/oldj/SwitchHosts/releases/download/v#{version.major_minor_patch}/SwitchHosts_arm64_#{version}.dmg",
+    sha256 "f7360db875930fb4065970bbab274aea98218ce2d2c85c833cd42cd03a10bdc2"
+    url "https://github.com/oldj/SwitchHosts/releases/download/v#{version.major_minor_patch}/SwitchHosts_mac_arm64_#{version}.dmg",
         verified: "github.com/oldj/SwitchHosts/"
   end
 
@@ -18,7 +18,7 @@ cask "switchhosts" do
   livecheck do
     url :url
     strategy :github_latest
-    regex(%r{/SwitchHosts_(\d+(?:\.\d+)*)\.dmg}i)
+    regex(%r{/SwitchHosts_installer_(\d+(?:\.\d+)*)\.exe}i)
   end
 
   app "SwitchHosts.app"
