da4fcae2f0a11a8afa3bd2ada448df2341d3b4be
Reno <25192197+singularitti@users.noreply.github.com>
2021-12-21 04:43:42 -0500

    Update typeface from 3.4.1 to 3.5.0 (#116135)
    * Update typeface from 3.4.1 to 3.5.0
    
    * Update typeface.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/typeface.rb
+++ Casks/typeface.rb
@@ -1,8 +1,8 @@
 cask "typeface" do
-  version "3.4.1,3046"
-  sha256 "7706eca286f2b59d97a1e217ca64b44b9dd25f4fdc2acdefe463091eb1b2a3dd"
+  version "3.5.0,3122"
+  sha256 "a736843b8a4a9efe211684f66d32ade6f092f0fff041305729198f2ce62d5433"
 
-  url "https://dcdn.typefaceapp.com/Typeface-#{version.before_comma}-#{version.after_comma}/Typeface-#{version.before_comma}-#{version.after_comma}.dmg"
+  url "https://dcdn.typefaceapp.com/Typeface-#{version.csv.first}-#{version.csv.second}/Typeface-#{version.csv.first}-#{version.csv.second}.dmg"
   name "Typeface"
   desc "Font manager application"
   homepage "https://typefaceapp.com/"
