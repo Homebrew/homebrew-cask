a9577285e92654e70c5f714828cd48ce5cc30971
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-22 05:57:33 +0800

    Update robloxstudio.rb

--- Casks/robloxstudio.rb
+++ Casks/robloxstudio.rb
@@ -3,11 +3,11 @@ cask "robloxstudio" do
   sha256 :no_check
 
   url "https://setup.rbxcdn.com/mac/RobloxStudio.dmg",
-      verified: "setup.rbxcdn.com"
+      verified: "setup.rbxcdn.com/"
   name "RobloxStudio"
   desc "Building tool for Roblox"
   homepage "https://www.roblox.com/create"
-  
+
   livecheck do
     url :url
     strategy :extract_plist
