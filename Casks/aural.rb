efdb21a134c9ea49282c21b8ee42d8b5d1a032e2
Bevan Kay <email@bevankay.me>
2021-12-22 12:41:38 +1100

    Update aural from 3.3.1 to 3.4.1 (#116270)

--- Casks/aural.rb
+++ Casks/aural.rb
@@ -1,8 +1,8 @@
 cask "aural" do
-  version "3.3.1"
-  sha256 "ad6fd5b77b20d39417888ec6cb96749b3b7952affd143039f4fa746b7c35bf88"
+  version "3.4.1"
+  sha256 "0433a7daa2fcb6aeeac363ca495d6af0763a7353217e1bf4a6711ac3d64bf494"
 
-  url "https://github.com/maculateConception/aural-player/releases/download/#{version}/AuralPlayer-#{version}.dmg"
+  url "https://github.com/maculateConception/aural-player/releases/download/v#{version}/AuralPlayer-#{version}.dmg"
   name "Aural Player"
   desc "Audio player inspired by Winamp"
   homepage "https://github.com/maculateConception/aural-player"
