7956ea534c15c9e5c07edce7401c93e3ec465008
Thomas Walter <tw@ba.lu>
2021-12-22 09:00:41 +0100

    Update prusaslicer from 2.3.3 to 2.4.0

--- Casks/prusaslicer.rb
+++ Casks/prusaslicer.rb
@@ -1,6 +1,6 @@
 cask "prusaslicer" do
-  version "2.3.3,202107161034"
-  sha256 "02b5820a371f4eb7b74ae7acfc4f0c32181c2eada4d00b97853d0559edff6462"
+  version "2.4.0,202112211607"
+  sha256 "b044b198aa97a8d53db9b012baf1db5fc2bdd8a2c6bf881cbbec5ceb725f3570"
 
   url "https://github.com/prusa3d/PrusaSlicer/releases/download/version_#{version.before_comma}/PrusaSlicer-#{version.before_comma}+universal-#{version.after_comma}.dmg",
       verified: "github.com/prusa3d/PrusaSlicer/"
