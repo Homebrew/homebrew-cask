c9f3d8e1465946d1ba6039596ed27d14f8b5acb1
owine <owine@users.noreply.github.com>
2021-12-22 15:41:44 -0600

    Update honto from 6.44.0 to 6.54.0 (#116343)
    * Update honto from 6.44.0 to 6.54.0
    
    * Update honto.rb
    
    * add livecheck
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/honto.rb
+++ Casks/honto.rb
@@ -1,11 +1,17 @@
 cask "honto" do
-  version "6.44.0,20200124"
+  version "6.54.0,20211105"
   sha256 :no_check
 
   url "https://dl.honto.jp/macapp/honto.dmg"
   name "honto view app"
   name "hontoビューアアプリ"
+  desc "Ebook reader for the honto store"
   homepage "https://honto.jp/ebook/dlinfo.html"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   app "honto.app"
 end
