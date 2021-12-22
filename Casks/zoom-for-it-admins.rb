4764101a530bc27e2c3b0869fa97934fc0ed0d6f
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-22 08:23:42 +0800

    Update zoom-for-it-admins from 5.8.6.2879 to 5.9.0.3377 (#116231)
    * Update zoom-for-it-admins from 5.8.6.2879 to 5.9.0.3377
    
    * Update zoom-for-it-admins.rb

--- Casks/zoom-for-it-admins.rb
+++ Casks/zoom-for-it-admins.rb
@@ -1,6 +1,6 @@
 cask "zoom-for-it-admins" do
-  version "5.8.6.2879"
-  sha256 "4ede3b09d40bc9b119d4fcb51ca7edd6bd3f7d6c6b303c6b5f1583dd4d5db69b"
+  version "5.9.0.3377"
+  sha256 "d7e4402e1b78cc832b090469e192b023a88b1c78b30e74a654b494c6aaa60ef0"
 
   url "https://cdn.zoom.us/prod/#{version}/ZoomInstallerIT.pkg"
   name "Zoom for IT Admins"
@@ -8,8 +8,7 @@ cask "zoom-for-it-admins" do
   homepage "https://support.zoom.us/hc/en-us/articles/115001799006-Mass-Deployment-with-Preconfigured-Settings-for-Mac"
 
   livecheck do
-    url "https://zoom.us/client/latest/Zoom.pkg"
-    strategy :header_match
+    cask "zoom"
   end
 
   # Do not add `auto_updates`. While supporting an auto-update mechanism, this software is more inconvenient than most
@@ -34,8 +33,8 @@ cask "zoom-for-it-admins" do
     "~/.zoomus",
     "~/Desktop/Zoom",
     "~/Documents/Zoom",
-    "~/Library/Application Support/CloudDocs/session/containers/iCloud.us.zoom.videomeetings",
     "~/Library/Application Support/CloudDocs/session/containers/iCloud.us.zoom.videomeetings.plist",
+    "~/Library/Application Support/CloudDocs/session/containers/iCloud.us.zoom.videomeetings",
     "~/Library/Application Support/CrashReporter/zoom.us*",
     "~/Library/Application Support/zoom.us",
     "~/Library/Caches/us.zoom.xos",
@@ -45,10 +44,10 @@ cask "zoom-for-it-admins" do
     "~/Library/Logs/zoominstall.log",
     "~/Library/Logs/ZoomPhone",
     "~/Library/Mobile Documents/iCloud~us~zoom~videomeetings",
-    "~/Library/Preferences/ZoomChat.plist",
     "~/Library/Preferences/us.zoom.airhost.plist",
     "~/Library/Preferences/us.zoom.xos.Hotkey.plist",
     "~/Library/Preferences/us.zoom.xos.plist",
+    "~/Library/Preferences/ZoomChat.plist",
     "~/Library/Safari/PerSiteZoomPreferences.plist",
     "~/Library/SafariTechnologyPreview/PerSiteZoomPreferences.plist",
     "~/Library/Saved Application State/us.zoom.xos.savedState",
