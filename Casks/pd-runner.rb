a519d2efad5d3a22e61f429d2478e2ff827020b2
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-22 09:28:18 +0800

    Update pd-runner.rb

--- Casks/pd-runner.rb
+++ Casks/pd-runner.rb
@@ -3,15 +3,16 @@ cask "pd-runner" do
   sha256 "c0b96c361891c6d40854da77a05cb412a7e3e22cc349f7fef5abd35534fa0150"
 
   url "https://github.com/lihaoyun6/PD-Runner/releases/download/#{version}/PD-Runner_v#{version}_Universal2.dmg"
-  name "pd-runner"
-  desc "Tool for Parallels Desktop can start VMs even the trial period has expired"
-  homepage "https://github.com/lihaoyun6/PD-Runner"
+  name "PD Runner"
+  desc "VM launcher for Parallels Desktop"
+  homepage "https://github.com/lihaoyun6/PD-Runner/"
 
   app "PD Runner.app"
 
-  zap trash: [
+  uninstall delete: [
     "/Library/LaunchDaemons/com.lihaoyun6.PD-Runner-Helper.plist",
     "/Library/PrivilegedHelperTools/com.lihaoyun6.PD-Runner-Helper",
-    "~/Library/Preferences/com.lihaoyun6.PD-Runner.plist",
   ]
+
+  zap trash: "~/Library/Preferences/com.lihaoyun6.PD-Runner.plist"
 end
