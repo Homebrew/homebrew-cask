f2c1f83480b8c5f27c7ff4427f2eea777a20dbd5
Bevan Kay <email@bevankay.me>
2021-12-23 08:29:50 +1100

    add desc, add livecheck

--- Casks/freedome.rb
+++ Casks/freedome.rb
@@ -4,7 +4,13 @@ cask "freedome" do
 
   url "https://download.sp.f-secure.com/freedome/installer/2/Freedome.pkg"
   name "F-Secure Freedome"
+  desc "VPN client"
   homepage "https://www.f-secure.com/en_US/web/home_us/freedome"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   pkg "Freedome.pkg"
 
