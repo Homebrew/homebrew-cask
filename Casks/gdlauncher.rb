9175d5407425b092ee713fa91e33242332f45bba
Bevan Kay <email@bevankay.me>
2021-12-22 12:46:59 +1100

    Update gdlauncher from 1.1.18 to 1.1.20 (#116271)

--- Casks/gdlauncher.rb
+++ Casks/gdlauncher.rb
@@ -1,6 +1,6 @@
 cask "gdlauncher" do
-  version "1.1.18"
-  sha256 "d46ef4f262c4dd254b31cb7d979d31f125523d50adc488031c73075e25dd8223"
+  version "1.1.20"
+  sha256 "fbabd716b486e9eae1da5cf963d7018abacb4ccf8a4196888aceee87333f91a5"
 
   url "https://github.com/gorilla-devs/GDLauncher/releases/download/v#{version}/GDLauncher-mac-setup.dmg",
       verified: "github.com/gorilla-devs/GDLauncher/"
