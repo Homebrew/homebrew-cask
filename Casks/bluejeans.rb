62997d145573a2b213dea62dc54e18bc22b990e9
Bevan Kay <email@bevankay.me>
2021-12-22 17:52:45 +1100

    Update bluejeans from 2.32.0.170 to 2.33.0.309 (#116282)

--- Casks/bluejeans.rb
+++ Casks/bluejeans.rb
@@ -1,6 +1,6 @@
 cask "bluejeans" do
-  version "2.32.0.170"
-  sha256 "cdb79ef23b00edbb11a9027478478f09b543feb3972cb32d5903dd4138fffa4f"
+  version "2.33.0.309"
+  sha256 "918930dabfd7d93b67981fe2c1aae3c896486286152d7665c6a3a4a3657ae9b4"
 
   url "https://swdl.bluejeans.com/desktop-app/mac/#{version.major_minor_patch}/#{version}/BlueJeansInstaller.pkg"
   name "BlueJeans"
