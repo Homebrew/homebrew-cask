ab7e6266a9c6b6839b33938158f4282bcea1870d
Adrián Ruiz <adrianruizarroyo@icloud.com>
2021-12-22 02:36:05 +0100

    Fix URL for autofirma (#116137)
    * Fix URL structure in autofirma and add SHA256
    
    * Fix issues to satisfy audit
    
    * Add livecheck
    
    * Fix style
    
    * Follow Cookbook guidelines for desc stanza

--- Casks/autofirma.rb
+++ Casks/autofirma.rb
@@ -1,11 +1,22 @@
 cask "autofirma" do
   version "1.6.5"
-  sha256 :no_check
+  sha256 "4564893f65a1566ea82c131a63bb56b44dbd2bcc218b5ba501ebb6dc22634c79"
 
-  url "https://estaticos.redsara.es/comunes/autofirma/currentversion/AutoFirma_Mac.zip",
+  url "https://estaticos.redsara.es/comunes/autofirma/#{version.major}/#{version.minor}/#{version.patch}/AutoFirma_Mac.zip",
       verified: "estaticos.redsara.es/comunes/autofirma/"
   name "AutoFirma"
+  desc "Digital signature editor and validator"
   homepage "https://firmaelectronica.gob.es/Home/Descargas.htm"
+
+  livecheck do
+    url :homepage
+    strategy :page_match do |page|
+      match = page.match(%r{href=.*?/(\d+)/(\d+)/(\d+)/AutoFirma_Mac.zip}i)
+      next if match.blank?
+
+      "#{match[1]}.#{match[2]}.#{match[3]}"
+    end
+  end
 
   pkg "AutoFirma_#{version.dots_to_underscores}.pkg"
 
