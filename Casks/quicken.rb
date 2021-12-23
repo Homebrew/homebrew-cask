98df7c8dcd4be11af0bc73761fcec9ac77bd28d3
Chris Bell <chris@seven24.ca>
2021-12-23 16:58:16 -0500

    Update quicken from 6.4.4 to 6.5.0 (#116378)

--- Casks/quicken.rb
+++ Casks/quicken.rb
@@ -1,6 +1,6 @@
 cask "quicken" do
-  version "6.4.4,604.41834.100"
-  sha256 "10cc878dcd2d27c493025a0c02458d57622e17b2dae5b529c1d28384a7ebf5b8"
+  version "6.5.0,605.42621.100"
+  sha256 "5db328708e3a1b955483d783d01819ff26a14c9449988e17f3d98b5742f67d09"
 
   url "https://download.quicken.com/mac/Quicken/001/Release/031A96D9-EFE6-4520-8B6A-7F465DDAA3E4/Quicken-#{version.csv.second}/Quicken-#{version.csv.second}.zip"
   name "Quicken"
