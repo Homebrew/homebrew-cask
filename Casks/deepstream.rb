64fef9295b6699c00ef221f3b1b3bf263057e5b6
neutric <5984479+neutric@users.noreply.github.com>
2021-12-23 01:26:49 +0100

    deepstream: add desc

--- Casks/deepstream.rb
+++ Casks/deepstream.rb
@@ -5,6 +5,7 @@ cask "deepstream" do
   url "https://github.com/deepstreamIO/deepstream.io/releases/download/v#{version}/deepstream.io-mac-#{version}.pkg",
       verified: "github.com/deepstreamIO/deepstream.io/"
   name "deepstream"
+  desc "Data-sync realtime server"
   homepage "https://deepstream.io/"
 
   pkg "deepstream.io-mac-#{version}.pkg"
