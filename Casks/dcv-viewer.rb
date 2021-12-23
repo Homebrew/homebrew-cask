8111433bf123911fadb830339aa43324b1c0d066
neutric <5984479+neutric@users.noreply.github.com>
2021-12-23 01:58:42 +0100

    Update dcv-viewer from 2021.2.3797 to 2021.3.3829 (#116356)
    * Update dcv-viewer from 2021.2.3797 to 2021.3.3829
    
    * dcv-viewer: add zap

--- Casks/dcv-viewer.rb
+++ Casks/dcv-viewer.rb
@@ -1,14 +1,14 @@
 cask "dcv-viewer" do
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 
-  version "2021.2.3797"
+  version "2021.3.3829"
 
   url "https://d1uj6qtbmh3dt5.cloudfront.net/#{version.major_minor}/Clients/nice-dcv-viewer-#{version}.#{arch}.dmg",
       verified: "d1uj6qtbmh3dt5.cloudfront.net/"
   if Hardware::CPU.intel?
-    sha256 "169e7ce10f16a248d5f32cc01adac2a88944924c176b68734c1ed6b8e40fc5c6"
+    sha256 "aaca37ccc846f33dc71735556e3e533a55525818490ce05c767f1526b227aece"
   else
-    sha256 "561031d5751cdeaf6a283d45b6c074ee694968a19d1447a51258b60d2b13c0d8"
+    sha256 "8d71305c0a606929f568d688e2f0ac19beafb99fefd0b29b9306eb25398e7bb4"
   end
 
   name "NICE DCV Viewer"
@@ -23,4 +23,6 @@ cask "dcv-viewer" do
   depends_on macos: ">= :mojave"
 
   app "DCV Viewer.app"
+
+  zap trash: "~/Library/Preferences/com.nicesoftware.dcvviewer.plist"
 end
