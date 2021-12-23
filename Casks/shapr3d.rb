dfd46ab0c0eafbf332dd948e15c7234620636b33
Aaron Rumpler <80610725+Aaron-Rumpler@users.noreply.github.com>
2021-12-24 11:08:12 +1300

    Add shapr3d (#116380)
    * Add shapr3d
    
    * Add livecheck for shapr3d

---
+++ Casks/shapr3d.rb
@@ -0,0 +1,23 @@
+cask "shapr3d" do
+  version "5.31.0.2105"
+  sha256 "d7e7f3fb583a556dd928c3146b536b81d105d524c6364525a4af5756fb76e459"
+
+  url "https://download.shapr3d.com/mac/Shapr3D-#{version}.dmg"
+  name "Shapr3D"
+  desc "Parametric 3D CAD"
+  homepage "https://www.shapr3d.com/"
+
+  livecheck do
+    url "https://download.shapr3d.com/mac/latest"
+    strategy :header_match
+  end
+
+  depends_on macos: ">= :big_sur"
+
+  app "Shapr3D.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.shapr3d.shapr",
+    "~/Library/Containers/com.shapr3d.shapr",
+  ]
+end
