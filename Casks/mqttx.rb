becc670dc124e91bfa615ec76d9213ffe3bee182
Jianyun Zhao <zhaojianyun88@outlook.com>
2021-12-24 13:10:40 +0800

    change mqttx app to universal app (#116409)
    * change mqttx app to universal app
    
    * Update mqttx.rb
    
    * Update mqttx.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/mqttx.rb
+++ Casks/mqttx.rb
@@ -1,8 +1,15 @@
 cask "mqttx" do
-  version "1.7.0"
-  sha256 "50732d42411d297b529b788c6b37469c050974b3f684a6e8310840014af26ee1"
+  arch = Hardware::CPU.intel? ? "" : "universal-"
 
-  url "https://github.com/emqx/MQTTX/releases/download/v#{version}/MQTTX-#{version}-mac.zip",
+  version "1.7.0"
+
+  if Hardware::CPU.intel?
+    sha256 "50732d42411d297b529b788c6b37469c050974b3f684a6e8310840014af26ee1"
+  else
+    sha256 "68754d3dd83ba947ce08a5a763e6a844f01b00275322eb9f88d757d99383825e"
+  end
+
+  url "https://github.com/emqx/MQTTX/releases/download/v#{version}/MQTTX-#{version}-#{arch}mac.zip",
       verified: "github.com/emqx/MQTTX/"
   name "MQTTX"
   desc "Cross-platform MQTT 5.0 Desktop Client"
