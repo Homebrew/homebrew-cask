36adb19edb3bcef85c200ce16d855c667ad574e4
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-24 12:40:02 +0800

    Update and rename mailsuite.rb to smallcubed-mailsuite.rb

--- Casks/smallcubed-mailsuite.rb
+++ Casks/smallcubed-mailsuite.rb
@@ -1,12 +1,19 @@
-cask "mailsuite" do
+cask "smallcubed-mailsuite" do
   version "2021.8.1"
   sha256 "f76468eb4826d8270ba239e8ec73f10ca25dbe57831f33944fab72a2a4404995"
 
   url "https://s3.amazonaws.com/media.smallcubed.com/release/scs/MailSuite_#{version}.dmg",
-      verified: "s3.amazonaws.com/media.smallcubed.com"
-  name "mailsuite"
+      verified: "s3.amazonaws.com/media.smallcubed.com/"
+  name "MailSuite"
   desc "Manage Mail like a Maven"
   homepage "https://smallcubed.com/scs/"
+
+  livecheck do
+    url "https://smallcubed.com/download/scs"
+    srategy :header_match
+  end
+
+  depends_on macos: ">= :big_sur"
 
   app "SmallCubed MailSuite.app"
 end
