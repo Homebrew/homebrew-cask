ebd99f4bf339dfd53c14c3d6cc3266574190e226
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-25 00:01:28 +0800

    Update ximalaya.rb

--- Casks/ximalaya.rb
+++ Casks/ximalaya.rb
@@ -2,8 +2,8 @@ cask "ximalaya" do
   version "2.3.0,1640311700"
   sha256 "a744f346c58f4603da1be8e2b026cf06a8ba0e43e2e0a91315f08d4e18a0e945"
 
-  url "https://s1.xmcdn.com/yx/ximalaya-pc-client/#{version.before_comma}/download/mac-x64/Ximalaya_#{version.before_comma}_#{version.after_comma}.dmg",
-      verified: "s1.xmcdn.com/"
+  url "https://s1.xmcdn.com/yx/ximalaya-pc-client/#{version.csv.first}/download/mac-x64/Ximalaya_#{version.csv.first}_#{version.csv.second}.dmg",
+      verified: "s1.xmcdn.com/yx/ximalaya-pc-client/"
   name "ximalaya"
   name "喜马拉雅"
   desc "Platform for podcasting and audio-sharing"
