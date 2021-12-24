b332c39ec79f1ccfc769b79540214f887d819b2c
neutric <5984479+neutric@users.noreply.github.com>
2021-12-25 00:13:44 +0100

    fabfilter-timeless: tweak livecheck regex

--- Casks/fabfilter-timeless.rb
+++ Casks/fabfilter-timeless.rb
@@ -10,7 +10,7 @@ cask "fabfilter-timeless" do
   livecheck do
     url "https://www.fabfilter.com/download"
     strategy :page_match do |page|
-      match = page.match(%r{href=".*?/fftimeless(\d)(\d\d)\.dmg}i)
+      match = page.match(/fftimeless(\d)(\d+)\.dmg/i)
       next if match.blank?
 
       "#{match[1]}.#{match[2]}"
@@ -21,7 +21,5 @@ cask "fabfilter-timeless" do
 
   pkg "FabFilter Timeless #{version} Installer.pkg"
 
-  uninstall pkgutil: [
-    "com.fabfilter.Timeless.#{version.major}",
-  ]
+  uninstall pkgutil: "com.fabfilter.Timeless.#{version.major}"
 end
