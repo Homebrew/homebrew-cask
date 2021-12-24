92f0a00a8f4991c19b5c1c900fbffa38595f89f6
neutric <5984479+neutric@users.noreply.github.com>
2021-12-24 01:44:48 +0100

    Update eclipse-jee from 4.21.0 to 4.22.0 (#116394)

--- Casks/eclipse-jee.rb
+++ Casks/eclipse-jee.rb
@@ -1,6 +1,6 @@
 cask "eclipse-jee" do
-  version "4.21.0,2021-09"
-  sha256 "d1fcddd1bf899da1f1b18425a462c59f0b8c7adc12bb7b628bd9c931d22f034b"
+  version "4.22.0,2021-12"
+  sha256 "3a4d32951ddb2b1a5e87ed31a4fa49bf16516407152f49c1ad82b18931fc9b57"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-jee-#{version.csv.second}-R-macosx-cocoa-x86_64.dmg&r=1"
   name "Eclipse IDE for Java EE Developers"
