381c91e722d967046ec83ace58ff610e3f6fb4a3
Gabriel Gerlero <gerlero@users.noreply.github.com>
2021-12-23 22:03:27 -0300

    Update paraview from 5.9.1 to 5.10

--- Casks/paraview.rb
+++ Casks/paraview.rb
@@ -1,6 +1,6 @@
 cask "paraview" do
-  version "5.9.1"
-  sha256 "8e7648b6380bc0688971fae17da010e63d75bc67f899286fd8173bb6910cc404"
+  version "5.10"
+  sha256 "6b65522284f72c60a211e0ba1b8146c103d75fcf0c280a9c2b4e108c1fca337c"
 
   url "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v#{version.major_minor}&type=binary&os=macOS&downloadFile=ParaView-#{version}-MPI-OSX10.13-Python3.8-64bit.dmg",
       user_agent: :fake
