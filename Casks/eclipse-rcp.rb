278a47bb78dac244e211ffd62b6561cd29844d31
neutric <5984479+neutric@users.noreply.github.com>
2021-12-24 01:53:33 +0100

    Update eclipse-rcp from 4.21.0 to 4.22.0 (#116396)

--- Casks/eclipse-rcp.rb
+++ Casks/eclipse-rcp.rb
@@ -1,6 +1,6 @@
 cask "eclipse-rcp" do
-  version "4.21.0,2021-09"
-  sha256 "c0bd9807a84114f1cf472b7bbc4432526b495a228850c17755f82a07dec667d2"
+  version "4.22.0,2021-12"
+  sha256 "e64b09c51f71327f6822faaf9e459e76a5bebf48ed599876ca55732ff33d89f7"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-rcp-#{version.csv.second}-R-macosx-cocoa-x86_64.dmg&r=1"
   name "Eclipse for RCP and RAP Developers"
