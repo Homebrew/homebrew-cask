fc4e7f57641dd7c4ba439049f14e46c8151fd840
neutric <5984479+neutric@users.noreply.github.com>
2021-12-25 00:17:07 +0100

    fabfilter-pro-q: tweak livecheck regex

--- Casks/fabfilter-pro-q.rb
+++ Casks/fabfilter-pro-q.rb
@@ -10,7 +10,7 @@ cask "fabfilter-pro-q" do
   livecheck do
     url "https://www.fabfilter.com/download"
     strategy :page_match do |page|
-      match = page.match(%r{href=".*?/ffproq(\d)(\d\d)\.dmg}i)
+      match = page.match(/ffproq(\d)(\d+)\.dmg/i)
       next if match.blank?
 
       "#{match[1]}.#{match[2]}"
@@ -21,7 +21,5 @@ cask "fabfilter-pro-q" do
 
   pkg "FabFilter Pro-Q #{version} Installer.pkg"
 
-  uninstall pkgutil: [
-    "com.fabfilter.Pro-Q.#{version.major}",
-  ]
+  uninstall pkgutil: "com.fabfilter.Pro-Q.#{version.major}"
 end
