d5c76fb2ad84599288df4116c94ace7ead4fa396
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-24 10:00:11 +0800

    paraview 5.10.0 (#116404)

--- Casks/paraview.rb
+++ Casks/paraview.rb
@@ -1,12 +1,17 @@
 cask "paraview" do
-  version "5.9.1"
-  sha256 "8e7648b6380bc0688971fae17da010e63d75bc67f899286fd8173bb6910cc404"
+  version "5.10.0"
+  sha256 "346833f38ef82d9313fcb203396b901da41e968a8d7078e2f128a345d2d6bafc"
 
-  url "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v#{version.major_minor}&type=binary&os=macOS&downloadFile=ParaView-#{version}-MPI-OSX10.13-Python3.8-64bit.dmg",
+  url "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v#{version.major_minor}&type=binary&os=macOS&downloadFile=ParaView-#{version}-MPI-OSX10.13-Python3.9-x86_64.dmg",
       user_agent: :fake
-  appcast "https://www.paraview.org/files/listing.txt"
   name "ParaView"
+  desc "Data analysis and visualization application"
   homepage "https://www.paraview.org/"
+
+  livecheck do
+    url "https://www.paraview.org/files/listing.txt"
+    regex(/ParaView[._-](\d+(?:\.\d+)+)[._-]MPI[._-]OSX10\.13[._-]Python3\.9[._-]x86[._-]64\.dmg/i)
+  end
 
   depends_on macos: ">= :sierra"
 
