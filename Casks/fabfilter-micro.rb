da3fe98ecfe728b3d85456a15b65a1ae3d3480fd
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-24 08:47:42 +0800

    Update fabfilter-micro.rb

--- Casks/fabfilter-micro.rb
+++ Casks/fabfilter-micro.rb
@@ -10,7 +10,7 @@ cask "fabfilter-micro" do
   livecheck do
     url "https://www.fabfilter.com/download"
     strategy :page_match do |page|
-      match = page.match(%r{href=.*?/ffmicro(\d)(\d\d)\.dmg}i)
+      match = page.match(/ffmicro(\d)(\d+)\.dmg/i)
       next if match.blank?
 
       "#{match[1]}.#{match[2]}"
@@ -21,7 +21,5 @@ cask "fabfilter-micro" do
 
   pkg "FabFilter Micro #{version} Installer.pkg"
 
-  uninstall pkgutil: [
-    "com.fabfilter.Micro.#{version.major}",
-  ]
+  uninstall pkgutil: "com.fabfilter.Micro.#{version.major}"
 end
