32b0f4cc59587ef6b799fe23b97cb23473ac25e1
neutric <5984479+neutric@users.noreply.github.com>
2021-12-25 01:17:50 +0100

    Update flacon from 8.1.0 to 8.2.0 (#116446)

--- Casks/flacon.rb
+++ Casks/flacon.rb
@@ -1,6 +1,6 @@
 cask "flacon" do
-  version "8.1.0"
-  sha256 "31747658041e96dfa31100c8581955b024fa8097fe9b721162314bbbdba4d251"
+  version "8.2.0"
+  sha256 "245cae6df3834119382ce87379db35da7d8bf846afd5501e647b0939f63db46f"
 
   url "https://github.com/flacon/flacon/releases/download/v#{version}/Flacon_#{version}.dmg",
       verified: "github.com/flacon/flacon/"
