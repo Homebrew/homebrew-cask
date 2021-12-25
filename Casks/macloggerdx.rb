9725137a89ad8316783865600d30ea7d26e6d96e
Eric Macauley <eric@macauley.me>
2021-12-24 23:47:55 -0800

    Add MacLoggerDX Latest

---
+++ Casks/macloggerdx.rb
@@ -0,0 +1,11 @@
+cask "macloggerdx" do
+  version :latest
+  sha256 :no_check
+
+  url "https://www.dogparksoftware.com/files/MacLoggerDX.dmg"
+  name "MacLoggerDX"
+  desc "Ham Radio Logging and Rig Control Software"
+  homepage "https://www.dogparksoftware.com/MacLoggerDX.html"
+
+  app "MacLoggerDX.app"
+end
