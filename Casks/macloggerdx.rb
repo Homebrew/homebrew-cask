441ea49ecdf7b03d7c8dd9383a14d26b280f1ae9
Eric Macauley <eric@macauley.me>
2021-12-25 15:40:39 -0800

    Change from unversioned to version 6.36, add zap

--- Casks/macloggerdx.rb
+++ Casks/macloggerdx.rb
@@ -1,11 +1,21 @@
 cask "macloggerdx" do
-  version :latest
-  sha256 :no_check
+  version "6.36"
+  sha256 "25ac048ed04a23c7b9a478d445847d7960e2146c67e83f7c8cd98ce770d3eb74"
 
-  url "https://www.dogparksoftware.com/files/MacLoggerDX.dmg"
+  url "https://www.dogparksoftware.com/files/MacLoggerDX#{version.delete(".")}.dmg"
   name "MacLoggerDX"
   desc "Ham Radio Logging and Rig Control Software"
   homepage "https://www.dogparksoftware.com/MacLoggerDX.html"
 
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(/Download: v(\d+(?:\.\d+)*)[^a-z]+/i)
+  end
+
   app "MacLoggerDX.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.dogparksoftware.MacLoggerDX*.plist",
+  ]
 end
