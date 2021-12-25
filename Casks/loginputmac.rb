495d4db2493c3c3d8af5827672f965b93cade8f4
R0uter <R0uter@users.noreply.github.com>
2021-12-26 01:27:22 +1000

    fix style

--- Casks/loginputmac.rb
+++ Casks/loginputmac.rb
@@ -2,8 +2,9 @@ cask "loginputmac" do
   version "2.5.3,24161"
   sha256 "9ad1e436283dbb6a60d040c205da3a31e5d97e999f53c6e6ed01682bb36e5857"
 
-  # The install source has to be a PKG file otherwise both install and update will be failed. 
-  # Inputmethod (IME) is a special application that lives in `/Library/Input\ Method` not `/Application`, and needs privilege to write in, 
+  # The install source has to be a PKG file otherwise both install and update will be failed.
+  # Inputmethod (IME) is a special application that lives in `/Library/Input\ Method` not `/Application`,
+  # and needs privilege to write in.
   # Pls DO NOT change the path into `.app` again, those files only for sparkle update framework tho.
   
   url "https://loginput-mac2.totest.top/loginputmac#{version.major}_latest.pkg",
