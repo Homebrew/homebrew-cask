14d43ecc49908008851178e57360a1c5a8cdeb50
R0uter <R0uter@users.noreply.github.com>
2021-12-26 01:06:31 +1000

    fix a bug that input method can not be installed
    Since https://github.com/Homebrew/homebrew-cask/pull/112003, this cask been broken until now my user reported to me...
    The install source has to be a PKG file otherwise both install and update will be failed. Inputmethod (IME) is a special application that lives in `/Library/Input\ Method` not `/Application`, and needs privilege to write in, pls DO not change the path into `.app` again, those file only for sparkle update frame tho.

--- Casks/loginputmac.rb
+++ Casks/loginputmac.rb
@@ -2,7 +2,11 @@ cask "loginputmac" do
   version "2.5.3,24161"
   sha256 "9ad1e436283dbb6a60d040c205da3a31e5d97e999f53c6e6ed01682bb36e5857"
 
-  url "https://loginput-mac2.totest.top/LogInputMac#{version.major}.app#{version.csv.second}.zip",
+  # The install source has to be a PKG file otherwise both install and update will be failed. 
+  # Inputmethod (IME) is a special application that lives in `/Library/Input\ Method` not `/Application`, and needs privilege to write in, 
+  # Pls DO NOT change the path into `.app` again, those files only for sparkle update framework tho.
+  
+  url "https://loginput-mac2.totest.top/loginputmac#{version.major}_latest.pkg",
       verified: "loginput-mac2.totest.top/"
   name "LoginputMac"
   desc "Chinese input method"
@@ -15,5 +19,8 @@ cask "loginputmac" do
 
   auto_updates true
 
-  app "LogInputMac#{version.major}.app"
+  pkg "loginputmac#{version.major}_latest.pkg"
+
+  uninstall pkgutil: "com.logcg.pkg.LoginputMac#{version.major}",
+            quit:    "com.logcg.inputmethod.LogInputMac#{version.major}"
 end
