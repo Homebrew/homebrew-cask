18e74b662f8d8498b54445528ff743a430a7b878
neutric <5984479+neutric@users.noreply.github.com>
2021-12-26 04:41:32 +0100

    Update fluent-reader from 1.0.2 to 1.1.0 (#116466)
    * Update fluent-reader from 1.0.2 to 1.1.0
    
    * fluent-reader: fix typo

--- Casks/fluent-reader.rb
+++ Casks/fluent-reader.rb
@@ -1,6 +1,6 @@
 cask "fluent-reader" do
-  version "1.0.2"
-  sha256 "48ea4f7b9b9524fb01cd872664eae1be1cd9723e912ceb49f9b5104c1d1d2452"
+  version "1.1.0"
+  sha256 "a9fbf42b38ae9c0ff5985c9ed76115e6bffa5098cd76f1f57a43dcfe548c07b2"
 
   url "https://github.com/yang991178/fluent-reader/releases/download/v#{version}/Fluent.Reader.#{version}.dmg",
       verified: "github.com/yang991178/fluent-reader/"
@@ -16,7 +16,7 @@ cask "fluent-reader" do
   app "Fluent Reader.app"
 
   zap trash: [
-    "~/Library//Application Support/fluent-reader",
+    "~/Library/Application Support/fluent-reader",
     "~/Library/Preferences/me.hyliu.fluentreader.plist",
   ]
 end
