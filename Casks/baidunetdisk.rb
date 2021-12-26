856f19f6b9cecf8b405f5bdbe4d16c6a0c7aaf8f
Zhang Maiyun <myzhang1029@hotmail.com>
2021-12-26 12:51:41 -0800

    baidunetdisk: update zap

--- Casks/baidunetdisk.rb
+++ Casks/baidunetdisk.rb
@@ -19,10 +19,14 @@ cask "baidunetdisk" do
   app "BaiduNetdisk_mac.app"
 
   zap trash: [
+    "~/Library/Application Scripts/com.baidu.BaiduNetdisk-mac.FinderSync",
+    "~/Library/Application Support/baidunetdisk",
     "~/Library/Application Support/com.baidu.BaiduNetdisk-mac",
     "~/Library/Caches/com.baidu.BaiduNetdisk-mac",
     "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.baidu.BaiduNetdisk-mac",
+    "~/Library/Containers/com.baidu.BaiduNetdisk-mac.FinderSync",
     "~/Library/Cookies/com.baidu.BaiduNetdisk-mac.binarycookies",
+    "~/Library/HTTPStorages/com.baidu.BaiduNetdisk-mac",
     "~/Library/Preferences/com.baidu.BaiduNetdisk-mac.plist",
     "~/Library/sapi/wappass.baidu.com",
     "~/Library/Saved Application State/com.baidu.BaiduNetdisk-mac.savedState",
