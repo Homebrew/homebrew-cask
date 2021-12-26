418319f5793ea76c5babe712d7db0d8c7f013ca1
Alexander Ronald Altman <alexanderaltman@me.com>
2021-12-25 21:20:54 -1000

    kicad 6.0.0-0 (#116472)
    * kicad 6.0.0-0
    
    * Update kicad.rb
    
    * Update kicad.rb
    
    * Update kicad.rb
    
    * Update kicad.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/kicad.rb
+++ Casks/kicad.rb
@@ -1,8 +1,8 @@
 cask "kicad" do
-  version "5.1.12-1"
-  sha256 "c2380302f0ddce72123aa0ae4a6561b3acc168c3c04fc6b3019ee3325dcd65e3"
+  version "6.0.0-0"
+  sha256 "ac8d93c9e4d3eb03446f18e6803f92168cf34f35080595425d0c5cb448c2a743"
 
-  url "https://kicad-downloads.s3.cern.ch/osx/stable/kicad-unified-#{version}-10_14.dmg",
+  url "https://kicad-downloads.s3.cern.ch/osx/stable/kicad-unified-#{version}.dmg",
       verified: "kicad-downloads.s3.cern.ch/"
   name "KiCad"
   desc "Electronics design automation suite"
@@ -10,28 +10,23 @@ cask "kicad" do
 
   livecheck do
     url "https://kicad-downloads.s3.cern.ch/?delimiter=/&prefix=osx/stable/"
-    regex(/kicad-unified-(\d+(?:.\d+)*)-10_14\.dmg/i)
+    regex(/kicad-unified-(\d+(?:.\d+)*)\.dmg/i)
   end
 
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= :catalina"
 
-  app "KiCad/kicad.app",            target: "KiCad/kicad.app"
-  app "KiCad/bitmap2component.app", target: "KiCad/bitmap2component.app"
-  app "KiCad/eeschema.app",         target: "KiCad/eeschema.app"
-  app "KiCad/gerbview.app",         target: "KiCad/gerbview.app"
-  app "KiCad/pcb_calculator.app",   target: "KiCad/pcb_calculator.app"
-  app "KiCad/pcbnew.app",           target: "KiCad/pcbnew.app"
-  app "KiCad/pl_editor.app",        target: "KiCad/pl_editor.app"
-  artifact "kicad/help",            target: "/Library/Application Support/kicad/help"
-  artifact "kicad/library",         target: "/Library/Application Support/kicad/library"
-  artifact "kicad/modules",         target: "/Library/Application Support/kicad/modules"
-  artifact "kicad/share",           target: "/Library/Application Support/kicad/share"
-  artifact "kicad/template",        target: "/Library/Application Support/kicad/template"
+  suite "KiCad"
+  artifact "demos", target: "/Library/Application Support/kicad/demos"
 
-  uninstall rmdir: [
-    "/Library/Application Support/kicad",
+  uninstall delete: [
     "#{appdir}/KiCad",
+    "/Library/Application Support/kicad/demos",
   ]
 
-  zap trash: "~/Library/Preferences/kicad"
+  zap trash: [
+    "/Library/Application Support/kicad",
+    "~/Library/Application Support/kicad",
+    "~/Library/Preferences/kicad",
+    "~/Library/Preferences/org.kicad-pcb.*",
+  ]
 end
