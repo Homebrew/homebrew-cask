08caf7c04b8b6bdab4d1ffc2f9456150d3bb7823
Bevan Kay <email@bevankay.me>
2021-12-27 10:59:55 +1100

    Update stats from 2.6.24 to 2.6.25 (#116491)

--- Casks/stats.rb
+++ Casks/stats.rb
@@ -1,6 +1,6 @@
 cask "stats" do
-  version "2.6.24"
-  sha256 "beb127a74dd5edd783236109c999c928cc55fdf561f1ea4466ced8ff83ddbe5e"
+  version "2.6.25"
+  sha256 "4e3313bddb3e1c50639fc637d1a492c70c25bfa8efa58d1aee173cdb2be5b30b"
 
   url "https://github.com/exelban/stats/releases/download/v#{version}/Stats.dmg"
   name "Stats"
