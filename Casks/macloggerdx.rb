4613b0692aeeb2605c30a398732b72a6cd92c697
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-26 11:50:35 +0800

    Update macloggerdx.rb

--- Casks/macloggerdx.rb
+++ Casks/macloggerdx.rb
@@ -2,20 +2,19 @@ cask "macloggerdx" do
   version "6.36"
   sha256 "25ac048ed04a23c7b9a478d445847d7960e2146c67e83f7c8cd98ce770d3eb74"
 
-  url "https://www.dogparksoftware.com/files/MacLoggerDX#{version.delete(".")}.dmg"
+  url "https://www.dogparksoftware.com/files/MacLoggerDX#{version.no_dots}.dmg"
   name "MacLoggerDX"
-  desc "Ham Radio Logging and Rig Control Software"
+  desc "Ham radio logging and rig control software"
   homepage "https://www.dogparksoftware.com/MacLoggerDX.html"
 
   livecheck do
     url :homepage
-    strategy :page_match
-    regex(/Download: v(\d+(?:\.\d+)*)[^a-z]+/i)
+    regex(/Download:\s*v?(\d+(?:\.\d+)+)[^a-z]+/i)
   end
+
+  depends_on macos: ">= :high_sierra"
 
   app "MacLoggerDX.app"
 
-  zap trash: [
-    "~/Library/Preferences/com.dogparksoftware.MacLoggerDX*.plist",
-  ]
+  zap trash: "~/Library/Preferences/com.dogparksoftware.MacLoggerDX*.plist"
 end
