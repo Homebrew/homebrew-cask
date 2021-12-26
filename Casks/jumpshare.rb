0b70f43245f035af1bc4344cc80ac20b0df482f4
neutric <5984479+neutric@users.noreply.github.com>
2021-12-26 04:43:21 +0100

    jumpshare: fix typo (#116471)

--- Casks/jumpshare.rb
+++ Casks/jumpshare.rb
@@ -2,7 +2,7 @@ cask "jumpshare" do
   version "2.7.3,103"
   sha256 "f9f1ddb4fdaafa64a817358805952836d8bc147f7a504a0a8417e7e17b3057a1"
 
-  url "https://d21hi1or3tbtjm.cloudfront.net//desktop/mac/updates/Jumpshare-#{version.before_comma}.tar.bz2",
+  url "https://d21hi1or3tbtjm.cloudfront.net/desktop/mac/updates/Jumpshare-#{version.csv.first}.tar.bz2",
       verified: "d21hi1or3tbtjm.cloudfront.net/"
   name "Jumpshare"
   desc "File sharing, screen recording, and screenshot capture app"
