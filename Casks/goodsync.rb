6e9c1b11c98dac2ed04eea4931806bc9c57dbe28
neutric <5984479+neutric@users.noreply.github.com>
2021-12-27 00:57:04 +0100

    Update goodsync from 11.9.1 to 11.9.8

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,6 +1,6 @@
 cask "goodsync" do
-  version "11.9.1"
-  sha256 "6010f2fd75a5611ed1500da0fa484a8a0914a1b08995f5176d2137448e253d19"
+  version "11.9.8"
+  sha256 "058c9b9c3eb864bbbcad7529fa4db1b2d3e7714e143d0b5c6d83a8f680642750"
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
   name "GoodSync"
