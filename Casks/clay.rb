43f2c92bd92570cf97db43ddade2c64fd0c3fcf8
Jacob Ford <j@cobford.com>
2021-12-26 15:16:09 -0500

    Update clay from 1.6.9 to 1.7.1 (#116478)

--- Casks/clay.rb
+++ Casks/clay.rb
@@ -1,12 +1,12 @@
 cask "clay" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  version "1.6.9"
+  version "1.7.1"
 
   if Hardware::CPU.intel?
-    sha256 "60b240684871b5de052e9022a62953e462e4956b843ab022213ed69d9659f0b6"
+    sha256 "e2e6366095cf41e854ca15b4563e3ff6b9efc1a6775378fea82b0baf673c2a70"
   else
-    sha256 "47bd827c3bb6f7505ee63396d2ea88ff21a3c2a38cba5d4a1acb4895f3a02c5b"
+    sha256 "220963032f271c9c243dd46a4de3bd12168f26b2e2ef490bee5a415665517955"
   end
 
   url "https://assets.clay.earth/desktop/mac/Clay-#{version}#{arch}.dmg"
