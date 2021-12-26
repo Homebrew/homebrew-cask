c808082b6e2032d19314128856b0028dec2331f4
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-27 04:16:59 +0800

    Update shutter-encoder.rb

--- Casks/shutter-encoder.rb
+++ Casks/shutter-encoder.rb
@@ -3,13 +3,13 @@ cask "shutter-encoder" do
 
   version "15.7"
 
-  url "https://www.shutterencoder.com/Shutter%20Encoder%20#{version}%20Mac%20#{arch}.pkg"
   if Hardware::CPU.intel?
     sha256 "14bf5c1eb5f6979d1975fe1977cc57ec06515c07937da1c92ca458826037d449"
   else
     sha256 "4a39a50b7b7e4e601b1d4f11ff5e9959f39c8651d5533c962af6d52ce2f0ad1b"
   end
 
+  url "https://www.shutterencoder.com/Shutter%20Encoder%20#{version}%20Mac%20#{arch}.pkg"
   name "Shutter Encoder"
   desc "Video, audio and image converter"
   homepage "https://www.shutterencoder.com/"
