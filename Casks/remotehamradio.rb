6ec41cbc40fdcb1221c60bdb882a55a29d9843e7
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-26 11:57:05 +0800

    Update remotehamradio.rb

--- Casks/remotehamradio.rb
+++ Casks/remotehamradio.rb
@@ -2,15 +2,15 @@ cask "remotehamradio" do
   version "2.2.1"
   sha256 "f042a12c30227723eebdaaaa348ebb6b3ade4dde4126beac2514fd9a7dc10499"
 
-  url "https://s3.amazonaws.com/rhr-electron/desktop-releases/RemoteHamRadio-#{version}-mac.zip", verified: "s3.amazonaws.com/rhr-electron"
+  url "https://s3.amazonaws.com/rhr-electron/desktop-releases/RemoteHamRadio-#{version}-mac.zip",
+      verified: "s3.amazonaws.com/rhr-electron/desktop-releases/"
   name "RemoteHamRadio"
   desc "Desktop console app for RemoteHamRadio service"
   homepage "https://www.remotehamradio.com/"
 
   livecheck do
     url "http://update.remotehamradio.com/desktop/download?platform=Macintosh"
-    strategy :page_match
-    regex(%r{href=.*?/RemoteHamRadio-(\d+(?:\.\d+)*)-mac\.zip}i)
+    regex(/href=.*?RemoteHamRadio[._-]v?(\d+(?:\.\d+)+)-mac\.zip/i)
   end
 
   app "RemoteHamRadio.app"
