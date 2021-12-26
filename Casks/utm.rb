17170fa451c7a443b3691b20845e721fcc0af07d
Zhang Maiyun <myzhang1029@hotmail.com>
2021-12-26 12:51:42 -0800

    utm: update zap

--- Casks/utm.rb
+++ Casks/utm.rb
@@ -20,5 +20,10 @@ cask "utm" do
   zap trash: [
     "~/Library/Preferences/com.utmapp.UTM.plist",
     "~/Library/Saved Application State/com.utmapp.UTM.savedState",
+    "~/Library/Application Scripts/com.utmapp.QEMUHelper",
+    "~/Library/Application Scripts/com.utmapp.UTM",
+    "~/Library/Containers/com.utmapp.QEMUHelper",
+    "~/Library/Containers/com.utmapp.UTM",
+    "~/Library/Group Containers/*.com.utmapp.UTM",
   ]
 end
