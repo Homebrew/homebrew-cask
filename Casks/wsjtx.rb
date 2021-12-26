32de6afb857e5063f01d679f0590745bc31cb81a
Eric Macauley <macauley@users.noreply.github.com>
2021-12-25 23:30:01 -0800

    Add WSJTX version 2.5.3 (#116459)
    * Add WSJTX version 2.5.3
    
    * Update wsjtx.rb
    
    * Add zap stanza
    
    * Update wsjtx.rb
    
    Co-authored-by: Eric Macauley <eric@macauley.me>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/wsjtx.rb
@@ -0,0 +1,35 @@
+cask "wsjtx" do
+  version "2.5.3"
+  sha256 "595a90359a16551a4ed36aaaffb4d3efc5fce03754a1e3093812813ad783a2be"
+
+  url "https://physics.princeton.edu/pulsar/k1jt/wsjtx-#{version}-Darwin.dmg"
+  name "WSJT-X"
+  desc "Ham radio app implementing communication protocols for weak-signal conditions"
+  homepage "https://physics.princeton.edu/pulsar/k1jt/wsjtx.html"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?wsjtx-(\d+(?:\.\d+)+)-Darwin\.dmg/i)
+  end
+
+  depends_on macos: ">= :high_sierra"
+
+  app "wsjtx.app"
+  artifact "com.wsjtx.sysctl.plist", target: "/Library/LaunchDaemons/com.wsjtx.sysctl.plist"
+
+  postflight do
+    set_ownership("/Library/LaunchDaemons/com.wsjtx.sysctl.plist", user: "root", group: "wheel")
+  end
+
+  uninstall delete: "/Library/LaunchDaemons/com.wsjtx.sysctl.plist"
+
+  zap trash: [
+    "~/Library/Application Support/WSJT-X",
+    "~/Library/Preferences/WSJT-X.ini",
+    "~/Library/Saved Application State/org.k1jt.wsjtx.savedState",
+  ]
+
+  caveats do
+    reboot
+  end
+end
