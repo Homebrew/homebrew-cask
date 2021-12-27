14a3a0c62e3ff16700529536c43a9ae15ad25891
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-27 16:31:01 +0800

    Update linearmouse.rb

--- Casks/linearmouse.rb
+++ Casks/linearmouse.rb
@@ -3,7 +3,7 @@ cask "linearmouse" do
   sha256 "2bf0fabfc3ac48f041bf443bbd3033353bf898f2860d2c963f6df405da613fed"
 
   url "https://github.com/lujjjh/LinearMouse/releases/download/v#{version}/LinearMouse.dmg",
-      verified: "github.com/lujjjh/LinearMouse"
+      verified: "github.com/lujjjh/LinearMouse/"
   name "LinearMouse"
   desc "Customize mouse behavior"
   homepage "https://linearmouse.org/"
