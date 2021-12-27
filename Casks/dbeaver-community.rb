5365352bc219e4a00914cfcdcedf69b60dcecdfb
Alex Harano <alexandre.harano@voxy.com>
2021-12-27 10:39:03 -0300

    Update dbeaver-community

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -4,7 +4,7 @@ cask "dbeaver-community" do
   version "21.3.2"
 
   if Hardware::CPU.intel?
-    sha256 "eb42fd96c8d013a9962464e481758868b03ad906d7a07d5d32c74f9f828804bb"
+    sha256 "8278253d121e1c74645ed302263dd340f61fadd075c9d676732c248b47011f26"
   else
     sha256 "663a64321f364c20f0da4e614eac1c1a88065ed595db146fa22b85d245f65ec3"
   end
