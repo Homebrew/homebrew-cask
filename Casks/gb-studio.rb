31cabf854f1a590fec3bb11e2c0feb34920b249e
neutric <5984479+neutric@users.noreply.github.com>
2021-12-27 02:37:30 +0100

    Update gb-studio from 3.0.1 to 3.0.2 (#116495)

--- Casks/gb-studio.rb
+++ Casks/gb-studio.rb
@@ -1,8 +1,8 @@
 cask "gb-studio" do
-  version "3.0.1"
-  sha256 "84b376656a9c7a3325a558368d8627d8d559b346997e961e051b909799b0fc62"
+  version "3.0.2"
+  sha256 "902799ecf53a622e644e86ebfc27b3dbaf0bb9eb20eb5e71f5594c53a632152d"
 
-  url "https://github.com/chrismaltby/gb-studio/releases/download/v#{version}/GB-Studio-Mac-#{version}.zip",
+  url "https://github.com/chrismaltby/gb-studio/releases/download/v#{version}/gb-studio-mac.zip",
       verified: "github.com/chrismaltby/gb-studio/"
   name "GB Studio"
   desc "Drag and drop retro game creator"
