d941c8020a539617eefb5560e67216946b9f009d
viking1304 <viking1304@gmail.com>
2021-12-27 20:03:23 +0100

    Fix dbeaver-community checksums (#116520)

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -4,9 +4,9 @@ cask "dbeaver-community" do
   version "21.3.2"
 
   if Hardware::CPU.intel?
-    sha256 "eb42fd96c8d013a9962464e481758868b03ad906d7a07d5d32c74f9f828804bb"
+    sha256 "8278253d121e1c74645ed302263dd340f61fadd075c9d676732c248b47011f26"
   else
-    sha256 "663a64321f364c20f0da4e614eac1c1a88065ed595db146fa22b85d245f65ec3"
+    sha256 "5810939ec9e2d0176d7e837abab2b2f4b21a1adc11f56c0099969c9491c3ad47"
   end
 
   url "https://dbeaver.io/files/#{version}/dbeaver-ce-#{version}-macos-#{arch}.dmg"
