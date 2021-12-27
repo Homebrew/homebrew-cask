a002573c0cedac053d799948a1065ae44dd25a61
neutric <5984479+neutric@users.noreply.github.com>
2021-12-27 06:10:44 +0100

    Update sequel-ace from 3.4.3 to 3.4.4 (#116502)

--- Casks/sequel-ace.rb
+++ Casks/sequel-ace.rb
@@ -1,6 +1,6 @@
 cask "sequel-ace" do
-  version "3.4.3,20019"
-  sha256 "59c9e4f15c0acf1e0c5ee22cc7372085ae4794777684ebbb7b900f77c9b8441f"
+  version "3.4.4,20021"
+  sha256 "1cf0c76343df066c8b285f979f939d40c6084df7695f652d9397a1424173e71b"
 
   url "https://github.com/Sequel-Ace/Sequel-Ace/releases/download/production/#{version.csv.first}-#{version.csv.second}/Sequel-Ace-#{version.csv.first}.zip"
   name "Sequel Ace"
