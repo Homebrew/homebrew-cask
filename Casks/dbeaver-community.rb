8339d02592c87b30ea9b66330529eca7225f5f11
frodera <219724+frodera@users.noreply.github.com>
2021-12-27 22:26:57 +1100

    Update dbeaver-community from 21.3.1 to 21.3.2 (#116505)

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,12 +1,12 @@
 cask "dbeaver-community" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
-  version "21.3.1"
+  version "21.3.2"
 
   if Hardware::CPU.intel?
-    sha256 "3b32f460ed303d533c984362558b99a52dedf9307e051e406a893a8295969791"
+    sha256 "eb42fd96c8d013a9962464e481758868b03ad906d7a07d5d32c74f9f828804bb"
   else
-    sha256 "c3ce8911baf8ec8e98a014e90a6459cad77e36dbb31a93110d8a81f747de9104"
+    sha256 "663a64321f364c20f0da4e614eac1c1a88065ed595db146fa22b85d245f65ec3"
   end
 
   url "https://dbeaver.io/files/#{version}/dbeaver-ce-#{version}-macos-#{arch}.dmg"
