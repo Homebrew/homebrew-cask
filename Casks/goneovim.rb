39bcb8fafc13c65dd71060ea659b3d8478993d1f
neutric <5984479+neutric@users.noreply.github.com>
2021-12-28 01:05:46 +0100

    Update goneovim from 0.4.13 to 0.5.0

--- Casks/goneovim.rb
+++ Casks/goneovim.rb
@@ -1,16 +1,8 @@
 cask "goneovim" do
-  version "0.4.13"
+  version "0.5.0"
+  sha256 "d54f729d1af12e5e0083b77d49004aec038baa09035402c4ceae8e7b1947663e"
 
-  if MacOS.version <= :catalina
-    sha256 "78910bc16ca31ca8dc50de60d415c5041067a9485b9b56f3b2449073dc7aa121"
-    url "https://github.com/akiyosi/goneovim/releases/download/v#{version}/Goneovim-v#{version}-macos.tar.bz2"
-    app "Goneovim-v#{version}-macos/goneovim.app"
-  else
-    sha256 "c0599ecbe330ab244ed9b6eb42b934f1b10fef63af8a19cac1a1f21e5ecf7788"
-    url "https://github.com/akiyosi/goneovim/releases/download/v#{version}/Goneovim-v#{version}-macos11.tar.bz2"
-    app "Goneovim-v#{version}-macos11/goneovim.app"
-  end
-
+  url "https://github.com/akiyosi/goneovim/releases/download/v#{version}/Goneovim-v#{version}-macos.tar.bz2"
   name "Goneovim"
   desc "Neovim GUI written in Golang, using a Golang qt backend"
   homepage "https://github.com/akiyosi/goneovim"
@@ -21,6 +13,8 @@ cask "goneovim" do
   end
 
   depends_on formula: "neovim"
+
+  app "Goneovim-latest-macos/goneovim.app"
 
   zap trash: [
     "~/Library/Saved Application State/com.ident.goneovim.savedState",
