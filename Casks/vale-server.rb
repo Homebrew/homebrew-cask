f2dc8972dd0f86ae2d8b2121d901457cf312d419
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-28 08:57:04 +0800

    Update vale-server.rb

--- Casks/vale-server.rb
+++ Casks/vale-server.rb
@@ -3,19 +3,20 @@ cask "vale-server" do
   sha256 "a10067f4321386f49c5c79426cd078bc8cfea96684682a88809ad9f9c49d58a0"
 
   url "https://github.com/errata-ai/errata.ai/releases/download/v#{version.major}.0.0/Vale-Server-#{version}-macos.dmg",
-      verified: "github.com/errata-ai/errata.ai"
+      verified: "github.com/errata-ai/errata.ai/"
   name "Vale Server"
-  desc "Cross-platform desktop application that brings your editorial style guide to life"
+  desc "Cross-platform desktop application"
   homepage "https://errata.ai/vale-server/"
 
   livecheck do
-    url "https://github.com/errata-ai/errata.ai"
-    strategy :github_latest
+    url "https://github.com/errata-ai/errata.ai/releases/"
+    strategy :page_match
+    regex(/Vale[._-]Server[._-]v?(\d+(?:\.\d+)+)[._-]macos\.dmg/i)
   end
+
+  depends_on macos: ">= :high_sierra"
 
   app "Vale Server.app"
 
-  zap trash: [
-    "~/Library/Application Support/Vale Server",
-  ]
+  zap trash: "~/Library/Application Support/Vale Server"
 end
