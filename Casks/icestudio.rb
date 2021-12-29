25a8911824fc521f892b057eab98251ef18abf6a
neutric <5984479+neutric@users.noreply.github.com>
2021-12-29 01:27:00 +0100

    icestudio: add zap

--- Casks/icestudio.rb
+++ Casks/icestudio.rb
@@ -5,8 +5,17 @@ cask "icestudio" do
   url "https://github.com/FPGAwars/icestudio/releases/download/v#{version}/icestudio-#{version}-osx64.dmg",
       verified: "github.com/FPGAwars/icestudio/"
   name "icestudio"
-  desc "Visual editor for open FPGA boards"
+  desc "Visual editor for open FPGA board"
   homepage "https://icestudio.io/"
 
   app "icestudio.app"
+
+  zap trash: [
+    "~/.icestudio",
+    "~/icestudio.log",
+    "~/Library/Application Support/icestudio",
+    "~/Library/Caches/icestudio",
+    "~/Library/Preferences/com.nw-builder.icestudio.plist",
+    "~/Library/Saved Application State/com.nw-builder.icestudio.savedState",
+  ]
 end
