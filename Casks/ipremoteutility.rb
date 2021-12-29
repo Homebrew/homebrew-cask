a9362d151c4f6f22e292eec29d0b1117dd9b4f24
neutric <5984479+neutric@users.noreply.github.com>
2021-12-29 03:14:28 +0100

    Update ipremoteutility from 1.8.7 to 1.8.9 (#116574)
    * Update ipremoteutility from 1.8.7 to 1.8.9
    
    * ipremoteutility: tweak livecheck regex
    
    * ipremoteutility: add desc

--- Casks/ipremoteutility.rb
+++ Casks/ipremoteutility.rb
@@ -1,15 +1,15 @@
 cask "ipremoteutility" do
-  version "1.8.7"
-  sha256 "bbcfb710919bdb17bd7416663b0c5965251b45d4267c46fe299566da6bf54435"
+  version "1.8.9"
+  sha256 "6ebee95ba683199b309e0341858fed2618e6b47b88b63f30a4a4470d54e419cb"
 
   url "https://www.flandersscientific.com/ip-remote/release/IPRemoteUtility-#{version}-macOSX.zip"
   name "Flanders IP Remote Utility"
+  desc "Management of Flanders Scientific hardware"
   homepage "https://www.flandersscientific.com/ip-remote/"
 
   livecheck do
     url :homepage
-    strategy :page_match
-    regex(%r{href=.*?/IPRemoteUtility-(\d+(?:\.\d+)*)-macOSX\.zip}i)
+    regex(/href=.*?IPRemoteUtility[._-]v?(\d+(?:\.\d+)+)-macOSX\.zip/i)
   end
 
   container nested: "IPRemoteUtility-#{version}-macOSX/IPRemoteUtility-#{version}.dmg"
