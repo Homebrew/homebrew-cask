edb633e73758ef813ed93d517313722db3a68e39
Bevan Kay <email@bevankay.me>
2021-12-30 09:15:17 +1100

    Update shimo.rb

--- Casks/shimo.rb
+++ Casks/shimo.rb
@@ -2,7 +2,7 @@ cask "shimo" do
   version "5.0.4,8890"
   sha256 "0f624d29248c2cb3d77ede2cc8ce8e30cdddaa7358d444571dd2ec1932f6958c"
 
-  url "https://downloads.mailbutler.io/Shimo_#{version.before_comma}_#{version.after_comma}.zip",
+  url "https://downloads.mailbutler.io/Shimo_#{version.csv.first}_#{version.csv.second}.zip",
       verified: "downloads.mailbutler.io/"
   name "Shimo"
   desc "VPN client for secure internet access and private browsing"
