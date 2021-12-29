7157522fb2234537e6c850b2a0bdc6c28bba441a
Alexandre Rey <39006939+alxrey@users.noreply.github.com>
2021-12-29 04:56:34 +0100

    Update macspice from 3.1.24 to 3.1.25 and complete missing fields (#116545)
    * Update macspice from 3.1.24 to 3.1.25
    
    * Add desc field
    
    * Update URL
    
    * Update macspice.rb
    
    * Update macspice.rb
    
    * Update macspice.rb
    
    * Update macspice.rb
    
    * Update macspice.rb
    
    * Update macspice.rb
    
    * Update macspice.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/macspice.rb
+++ Casks/macspice.rb
@@ -1,9 +1,10 @@
 cask "macspice" do
-  version "3.1.24,341"
-  sha256 "486baee366773f3589c870d9294e1028c118e29f9bf4dc824322e37f2782475f"
+  version "3.1.25,343"
+  sha256 "22f82c32b6e79916b123a7d1f5ded71a9a30cffc5ab63262b43a0d11db3c45f9"
 
-  url "https://www.macspice.com/mirror/binaries/v#{version.before_comma}/MacSpice3f5.dmg"
+  url "https://www.macspice.com/binaries/v#{version.csv.first}/MacSpice3f5.dmg"
   name "MacSpice"
+  desc "Electronic circuit simulator"
   homepage "https://www.macspice.com/"
 
   livecheck do
