80e1e04d221a0048e1eac4983468028397e4bb3c
neutric <5984479+neutric@users.noreply.github.com>
2021-12-29 01:11:33 +0100

    idafree: move url stanza

--- Casks/idafree.rb
+++ Casks/idafree.rb
@@ -3,14 +3,13 @@ cask "idafree" do
 
   version "7.7"
 
-  url "https://out7.hex-rays.com/files/#{arch}#{version.no_dots}_mac.app.zip",
-      verified: ""
   if Hardware::CPU.intel?
     sha256 "1141a984567f3eab8bda2b372a15d03757fa06919f67e007e1a948d52acc8377"
   else
     sha256 "161b7ef7b7415e03dd95e401c32173b6711074b632d46de8a89115999ecd83a6"
   end
 
+  url "https://out7.hex-rays.com/files/#{arch}#{version.no_dots}_mac.app.zip"
   name "IDA Free"
   desc "Binary code analysis tool"
   homepage "https://hex-rays.com/ida-free/"
