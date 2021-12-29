df176bc6da74cc17b7cefa7b36ad02ab6a7eb1e5
Marc Mogdanz <20729218+MarcMogdanz@users.noreply.github.com>
2021-12-29 23:16:44 +0100

    Update prisma-studio from 0.438.0 to 0.448.0 (#116599)

--- Casks/prisma-studio.rb
+++ Casks/prisma-studio.rb
@@ -1,6 +1,6 @@
 cask "prisma-studio" do
-  version "0.438.0"
-  sha256 "5414ea9c42b01b16ee7f6aef6410e999a8ad43a03f49f22a17deb11ce2154982"
+  version "0.448.0"
+  sha256 "4a9f0cc426a14962e4ba212cbfc6c626811942aedc127e14b771adb86df828eb"
 
   url "https://github.com/prisma/studio/releases/download/v#{version}/Prisma-Studio.dmg",
       verified: "github.com/prisma/studio/"
