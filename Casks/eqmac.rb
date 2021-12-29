bee4a0739c2b2fdc2453ff44a07330a9859fb97e
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-29 16:36:00 +0800

    Update eqmac from 1.3.2 to 1.4.0 (#116584)

--- Casks/eqmac.rb
+++ Casks/eqmac.rb
@@ -1,6 +1,6 @@
 cask "eqmac" do
-  version "1.3.2"
-  sha256 "b51dbb3f343e5ffa779f5084fe9de279671100e441a8bbfdc676ce3591c88093"
+  version "1.4.0"
+  sha256 "c62f487da493d1e8c2e255a803eb92ccb5ed535b20bf73c8fe99856e21382554"
 
   url "https://github.com/bitgapp/eqMac/releases/download/v#{version}/eqMac.pkg",
       verified: "github.com/bitgapp/eqMac/"
