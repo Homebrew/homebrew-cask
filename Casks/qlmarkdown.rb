0ed76adf9a05d02abe227b9520cf68102dcc8024
Yusuf Kürşad Kaya <ykursadkaya@gmail.com>
2021-12-30 01:19:44 +0300

    Update qlmarkdown from 1.0.9 to 1.0.10 (#116602)

--- Casks/qlmarkdown.rb
+++ Casks/qlmarkdown.rb
@@ -1,6 +1,6 @@
 cask "qlmarkdown" do
-  version "1.0.9"
-  sha256 "a3c507f9da728d53f816b44a1c5987c3e2ee63b974b6d831ef2b44c0fbb3d74c"
+  version "1.0.10"
+  sha256 "bc336b4e1c69d7d88eb54e54e792305516d2775e6c32705dbc9bf7eb3f58ec25"
 
   url "https://github.com/sbarex/QLMarkdown/releases/download/#{version}/QLMarkdown.zip"
   name "sbarex-qlmarkdown"
