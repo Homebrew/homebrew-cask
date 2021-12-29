5f98ab06ff6c323742dc9efcbc055852abbff70b
neutric <5984479+neutric@users.noreply.github.com>
2021-12-29 01:32:40 +0100

    immersed: update urls following redirect

--- Casks/immersed.rb
+++ Casks/immersed.rb
@@ -2,14 +2,13 @@ cask "immersed" do
   version "16.1,134"
   sha256 :no_check
 
-  url "https://immersed.com/dl/Immersed.dmg",
-      verified: "immersed.com/dl/"
+  url "https://immersed.com/dl/Immersed.dmg"
   name "Immersed"
   desc "Desktop Agent for VR Office"
-  homepage "https://immersedvr.com/"
+  homepage "https://immersed.com/"
 
   livecheck do
-    url "https://immersedvr.com/dl/appcast.xml"
+    url "https://immersed.com/dl/appcast.xml"
     strategy :sparkle
   end
 
