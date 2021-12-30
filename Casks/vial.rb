f9a15494c99853b20bf250d857539fb4dc1f7461
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-30 19:47:39 +0800

    Update vial.rb

--- Casks/vial.rb
+++ Casks/vial.rb
@@ -3,7 +3,7 @@ cask "vial" do
   sha256 "901fde94be7cddfa75c53c114c9e9d9e0f537490f8bad0e291584caa20a4732f"
 
   url "https://github.com/vial-kb/vial-gui/releases/download/v#{version}/Vial-v#{version}.dmg",
-      verified: "https://github.com/vial-kb/vial-gui"
+      verified: "https://github.com/vial-kb/vial-gui/"
   name "Vial"
   desc "Configurator of compatible keyboards in real time"
   homepage "https://get.vial.today/"
@@ -13,7 +13,7 @@ cask "vial" do
   zap trash: [
     "~/Library/Application Support/Vial",
     "~/Library/Caches/Vial",
-    "~/Library/Preferences/Vial.plist",
     "~/Library/Preferences/com.vial.Vial.plist",
+    "~/Library/Preferences/Vial.plist",
   ]
 end
