8873817c981d8797970e8758c57e1ca79607e2e3
Clément L <porkepix@gmail.com>
2021-12-31 00:43:22 +0100

    Update itunes-volume-control from 1.6.8 to 1.7.2 (#116686)
    * Update itunes-volume-control from 1.6.8 to 1.7.2
    
    * Update itunes-volume-control.rb
    
    * Update itunes-volume-control.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/itunes-volume-control.rb
+++ Casks/itunes-volume-control.rb
@@ -1,20 +1,21 @@
 cask "itunes-volume-control" do
-  version "1.6.8"
-  sha256 "93386a0ffcc7f96fbb6de42c2d1f04339e3e62f9ce7bc99e106ff73c7345440c"
+  version "1.7.2"
+  sha256 "9fb6a5fb0df8ca6aa135defd284bf23049cb128b621b906795378f0a043c047e"
 
   url "http://quantum-technologies.iap.uni-bonn.de/alberti/iTunesVolumeControl/iTunesVolumeControl-v#{version}.zip",
       verified: "uni-bonn.de/alberti/iTunesVolumeControl/"
   name "iTunes Volume Control"
-  homepage "https://github.com/alberti42/iTunes-Volume-Control"
+  desc "Control the volume of Apple Music and Spotify using keyboard volume keys"
+  homepage "https://github.com/alberti42/Volume-Control"
 
   livecheck do
-    url "https://github.com/alberti42/iTunes-Volume-Control#versions"
+    url "https://github.com/alberti42/Volume-Control#versions"
     strategy :page_match
     regex(%r{href=.*?/iTunesVolumeControl-v?(\d+(?:\.\d+)*)\.zip}i)
   end
 
   auto_updates true
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= :high_sierra"
 
-  app "iTunes Volume Control.app"
+  app "Volume Control.app"
 end
