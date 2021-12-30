3f9eecdd9b5ca2e40db330e8fefaf100594f35d6
Eric Macauley <eric@macauley.me>
2021-12-30 14:42:53 -0800

    Remove user_agent

--- Casks/gridtracker.rb
+++ Casks/gridtracker.rb
@@ -2,8 +2,7 @@ cask "gridtracker" do
   version "1.21.1217"
   sha256 "fd32335a82f663be0df1d03f5990c5ba2fa2df7790624e6ad30d862311efc350"
 
-  url "https://download.gridtracker.org/v#{version}/GridTracker-#{version}-mac-x64.zip",
-      user_agent: :fake
+  url "https://download.gridtracker.org/v#{version}/GridTracker-#{version}-mac-x64.zip"
   name "GridTracker"
   desc "Warehouse of amateur radio information presented in an easy to use interface"
   homepage "https://gridtracker.org/grid-tracker/"
