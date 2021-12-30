3aa233d5d67cf9770975919d4fddfc397f6414bb
ShredderWalnut <34428332+ShredderWalnut@users.noreply.github.com>
2021-12-30 21:41:10 +0800

    Update curio from 19 to 20

--- Casks/curio.rb
+++ Casks/curio.rb
@@ -1,6 +1,6 @@
 cask "curio" do
-  version "19,19006"
-  sha256 "e2ee57fbc298573e83da89f25b7c3f11aa71ddd6c1d76ad4d81d3b5782623141"
+  version "20,20009"
+  sha256 "d1cb21d05345cc77dd31726ba0b3cac665e81974681959401f9c0d5d7cc21c20"
 
   url "https://www.zengobi.com/downloads/Curio#{version.csv.second.no_dots}.zip"
   name "Curio"
