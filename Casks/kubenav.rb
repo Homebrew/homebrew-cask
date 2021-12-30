72d3a0a1339044b745d8c521c27d95c6d4acd7fc
Clément L <porkepix@gmail.com>
2021-12-30 02:49:37 +0100

    Update kubenav from 3.7.2 to 3.9.0 (#116634)
    * Update kubenav from 3.7.2 to 3.9.0
    
    * Update kubenav.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/kubenav.rb
+++ Casks/kubenav.rb
@@ -1,16 +1,16 @@
 cask "kubenav" do
   arch = Hardware::CPU.intel? ? "amd64" : "arm64"
 
-  version "3.7.2"
+  version "3.9.0"
+
+  if Hardware::CPU.intel?
+    sha256 "81dfd86497a1a5081cec1c4a544eb7959218db0107ddc3758b0b3e5367db950f"
+  else
+    sha256 "ad3d0778d85cad9271ba5942f9bc6de0130edcb28d1ecc9cfdb0ee3e4c040222"
+  end
 
   url "https://github.com/kubenav/kubenav/releases/download/#{version}/kubenav-darwin-#{arch}.zip",
       verified: "github.com/kubenav/kubenav/"
-  if Hardware::CPU.intel?
-    sha256 "6977025db6c093f5e7f7cd72bd027cec9b23ddfbe76a44343a84a4e1d7a43fa1"
-  else
-    sha256 "267047ccca59c3ff7f9721fa86bed80016fe7f8ee4392ec6b2afdda3178a083c"
-  end
-
   name "kubenav"
   desc "Navigator for your Kubernetes clusters right in your pocket"
   homepage "https://kubenav.io/"
