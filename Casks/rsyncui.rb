c27574db6ea2280ab4ec73589376229685e92b5b
Thomas Evensen <22427340+rsyncOSX@users.noreply.github.com>
2021-12-30 09:06:25 +0100

    Create rsyncui.rb

---
+++ Casks/rsyncui.rb
@@ -0,0 +1,19 @@
+cask "rsyncui" do
+  version "1.2.5"
+  sha256 "94f9b005f9f9966260c94f44573704cffa1075632e7c5ade9150b97370d1531c"
+
+  url "https://github.com/rsyncOSX/RsyncUI/releases/download/v#{version}/RsyncUI.#{version}.dmg"
+  name "RsyncUI"
+  desc "GUI for rsync"
+  homepage "https://github.com/rsyncOSX/RsyncUI"
+
+  depends_on macos: ">= :catalina"
+
+  app "RsyncUI.app"
+
+  zap trash: [
+    "~/Library/Caches/no.blogspot.RsyncUI",
+    "~/Library/Preferences/no.blogspot.RsyncUI.plist",
+    "~/Library/Saved Application State/no.blogspot.RsyncUI.savedState",
+  ]
+end
