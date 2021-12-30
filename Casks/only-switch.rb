f6d98f7595220888eae833bd89663cdd72e18b58
Reno <25192197+singularitti@users.noreply.github.com>
2021-12-30 06:27:44 -0500

    Add cask "only-switch" (#116658)
    * Add cask "only-switch" v1.5
    
    * Update "only-switch" to v1.6
    
    * Update only-switch.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/only-switch.rb
@@ -0,0 +1,20 @@
+cask "only-switch" do
+  version "1.6"
+  sha256 "0731ade444b81802d7a1b8ef9e683453daa059954431a7f047c4ab16a370b208"
+
+  url "https://github.com/jacklandrin/OnlySwitch/releases/download/release_#{version}/OnlySwitch.dmg"
+  name "OnlySwitch"
+  desc "System and utility switches"
+  homepage "https://github.com/jacklandrin/OnlySwitch"
+
+  depends_on macos: ">= :monterey"
+
+  app "Only Switch.app"
+
+  zap trash: [
+    "~/Library/Application Support/OnlySwitch",
+    "~/Library/Caches/jacklandrin.OnlySwitch",
+    "~/Library/OnlySwitch",
+    "~/Library/Preferences/jacklandrin.OnlySwitch.plist",
+  ]
+end
