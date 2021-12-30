8d4c1be7e287567029017450f6f6ee6bf772c81e
Clément L <porkepix@gmail.com>
2021-12-30 03:57:50 +0100

    Update openvanilla from 1.6.1 to 1.6.2 (#116638)
    * Update openvanilla from 1.6.1 to 1.6.2
    
    * Update openvanilla.rb
    
    * Update openvanilla.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/openvanilla.rb
+++ Casks/openvanilla.rb
@@ -1,11 +1,23 @@
 cask "openvanilla" do
-  version "1.6.1,3273"
-  sha256 "248c2a84f95f40ffea2fff71310814103ae70d87a829e136713ff801b39a29f2"
+  version "1.6.2,3300"
+  sha256 "2aa30f038376edf3c725f9654e6c939ee733104ac50e33f5161853da3353331e"
 
   url "https://github.com/openvanilla/openvanilla/releases/download/#{version.before_comma}/OpenVanilla-Installer-Mac-#{version.before_comma}.zip",
       verified: "github.com/openvanilla/openvanilla/"
   name "OpenVanilla"
+  desc "Provides common input methods"
   homepage "https://openvanilla.org/"
+
+  livecheck do
+    url "https://raw.githubusercontent.com/openvanilla/openvanilla/master/Source/Mac/OpenVanilla-Info.plist"
+    strategy :page_match do |page|
+      shortversion = page.match(/CFBundleShortVersionString.*?\n.*?(\d+(?:\.\d+)+)/i)
+      version = page.match(/CFBundleVersion.*?\n.*?(\d+(?:\.\d+)*)/i)
+      next if shortversion.blank? || version.blank?
+
+      "#{shortversion[1]},#{version[1]}"
+    end
+  end
 
   container nested: "OpenVanillaInstaller.app/Contents/Resources/NotarizedArchives/OpenVanilla-r#{version.after_comma}.zip"
 
