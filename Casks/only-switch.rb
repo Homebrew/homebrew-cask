313ab5fe3418e2b2bdda0dda2eb7c3de3c824454
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-30 19:20:23 +0800

    Update only-switch.rb

--- Casks/only-switch.rb
+++ Casks/only-switch.rb
@@ -3,8 +3,8 @@ cask "only-switch" do
   sha256 "0731ade444b81802d7a1b8ef9e683453daa059954431a7f047c4ab16a370b208"
 
   url "https://github.com/jacklandrin/OnlySwitch/releases/download/release_#{version}/OnlySwitch.dmg"
-  name "only-switch"
-  desc "All-in-one system and utility switches"
+  name "OnlySwitch"
+  desc "System and utility switches"
   homepage "https://github.com/jacklandrin/OnlySwitch"
 
   depends_on macos: ">= :monterey"
@@ -12,9 +12,9 @@ cask "only-switch" do
   app "Only Switch.app"
 
   zap trash: [
-    "~/Library/OnlySwitch",
-    "~/Library/Caches/jacklandrin.OnlySwitch",
     "~/Library/Application Support/OnlySwitch",
+    "~/Library/Caches/jacklandrin.OnlySwitch",
+    "~/Library/OnlySwitch",
     "~/Library/Preferences/jacklandrin.OnlySwitch.plist",
   ]
 end
