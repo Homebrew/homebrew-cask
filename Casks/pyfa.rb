218e080d490e2b188b0ea40eaedda7540539f17a
Clément L <porkepix@gmail.com>
2021-12-30 01:43:04 +0100

    Update pyfa from 2.38.1 to 2.39.1 (#116628)

--- Casks/pyfa.rb
+++ Casks/pyfa.rb
@@ -1,6 +1,6 @@
 cask "pyfa" do
-  version "2.38.1"
-  sha256 "a17fe7a8790f429cabf4712b1b6ec288a63474dd8a14314a8b1082a1bc39cdad"
+  version "2.39.1"
+  sha256 "634a08f745b812f3482d199890d1818c1ae06b7259a6a1b1b43764349f63d0fb"
 
   url "https://github.com/pyfa-org/Pyfa/releases/download/v#{version}/pyfa-v#{version}-mac.zip"
   name "pyfa"
