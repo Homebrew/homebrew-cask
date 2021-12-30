1f414056fb6c9e1b90472922261fead20c2e7f67
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-30 19:29:42 +0800

    Update fastmarks.rb

--- Casks/fastmarks.rb
+++ Casks/fastmarks.rb
@@ -2,8 +2,8 @@ cask "fastmarks" do
   version "1.1.0,5"
   sha256 "fbcb15f2b78e471022769039fbaa7fd20185533891f06d5820fc5158230def8e"
 
-  url "https://get.retina.studio/fastmarks/fastmarks_#{version.after_comma}.zip"
-  name "fastmarks"
+  url "https://get.retina.studio/fastmarks/fastmarks_#{version.csv.second}.zip"
+  name "Fastmarks"
   desc "Search and open web browser bookmarks"
   homepage "https://retina.studio/fastmarks/"
 
