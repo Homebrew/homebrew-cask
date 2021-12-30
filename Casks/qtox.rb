401bb12b2083e92a25dff43700a99279dfbea195
Clément L <porkepix@gmail.com>
2021-12-30 05:11:28 +0100

    Update qtox from 1.17.3 to 1.17.4 (#116645)
    * Update qtox from 1.17.3 to 1.17.4
    
    * Update qtox.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/qtox.rb
+++ Casks/qtox.rb
@@ -1,10 +1,11 @@
 cask "qtox" do
-  version "1.17.3"
-  sha256 "63ee6eb013487a86abbaeb23ad61eca7b74c6ceed0e56fa7e7d9bee6522615b4"
+  version "1.17.4"
+  sha256 "e049e2e359a0a711cf5957a5d9ba20c482cd5cff97048a1ed792f5394d3c61c8"
 
   url "https://github.com/qTox/qTox/releases/download/v#{version}/qTox.dmg",
       verified: "github.com/qTox/qTox/"
   name "qTox"
+  desc "Instant messaging and video conferencing app"
   homepage "https://qtox.github.io/"
 
   app "qTox.app"
