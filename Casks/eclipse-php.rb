b9ea11ab5f73a1520dd2eddc888ed9764f2db6de
Porkepix <porkepix@gmail.com>
2021-12-30 02:36:40 +0100

    Update eclipse-php from 4.21.0 to 4.22.0

--- Casks/eclipse-php.rb
+++ Casks/eclipse-php.rb
@@ -1,6 +1,6 @@
 cask "eclipse-php" do
-  version "4.21.0,2021-09"
-  sha256 "a2b28c5be6a4b6645fba58e1e0eb6f7a0ae1161c1475a72ea9df721420bed1ff"
+  version "4.22.0,2021-12"
+  sha256 "9a6fb55f6bbc51401324241cf5598d228de4b417c12eadaa40ec85fba41164e0"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-php-#{version.csv.second}-R-macosx-cocoa-x86_64.dmg&r=1"
   name "Eclipse IDE for PHP Developers"
