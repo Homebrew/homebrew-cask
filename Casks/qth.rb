04330a6b3dd4e971c3c1b8d34df48188ce870c8b
Eric Macauley <macauley@users.noreply.github.com>
2021-12-30 15:36:50 -0800

    Add QTH version 0.6.3 (#116690)
    * Add QTH version 0.6.3
    
    * Update qth.rb
    
    Co-authored-by: Eric Macauley <eric@macauley.me>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/qth.rb
@@ -0,0 +1,23 @@
+cask "qth" do
+  version "0.6.3"
+  sha256 "8120174a4c34d47b61444d32efc87ad29b9b1949a6624b3de3bb8554e37a1131"
+
+  url "https://www.w8wjb.com/qth/QTH-#{version}.dmg"
+  name "QTH"
+  desc "APRS client application"
+  homepage "https://www.w8wjb.com/wp/qth/"
+
+  livecheck do
+    url "https://www.w8wjb.com/wp/qth/qth-app-changelog/"
+    regex(%r{href=.*?/QTH[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
+  end
+
+  app "QTH.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.w8wjb.QTH",
+    "~/Library/Caches/com.apple.helpd/Generated/com.w8wjb.QTH.help*",
+    "~/Library/Logs/com.w8wjb.QTH",
+    "~/Library/Preferences/com.w8wjb.QTH.plist",
+  ]
+end
