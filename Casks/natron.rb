6ecbd76cf56719127a6e22b6047bd87d0b76a18f
Bevan Kay <email@bevankay.me>
2021-12-30 11:31:13 +1100

    Update natron.rb

--- Casks/natron.rb
+++ Casks/natron.rb
@@ -2,7 +2,7 @@ cask "natron" do
   version "2.4.2"
 
   if MacOS.version <= :mojave
-    sha256 "88fcb9f9659e35e267a6f11f79b7bd94b05471a2b82c639d5d7ea4871c9a0661"
+    sha256 "437979f4b94e718d7e7eb399715f851734900b7453bd3fe6a41d3a43e733a95a"
     url "https://github.com/NatronGitHub/Natron/releases/download/vsha/Natron-#{version}-OSX109-x86_64.dmg",
         verified: "github.com/NatronGitHub/Natron/"
   elsif MacOS.version <= :catalina
@@ -10,8 +10,8 @@ cask "natron" do
     url "https://github.com/NatronGitHub/Natron/releases/download/v#{version}/Natron-#{version}-macOS1015-x86_64.dmg",
         verified: "github.com/NatronGitHub/Natron/"
   else
-    sha256 "c7434206bb81700bf62644d9d0e3c46f576dc002a125923afee751eddcfc8406"
-    url "https://github.com/NatronGitHub/Natron/releases/download/v#{version}/Natron-#{version}-macOS11-x86_64.dmg",
+    sha256 "2801d03d6829532953b78eafb42d543d6c56ba338eb53435fa20ff89ba2d7a65"
+    url "https://github.com/NatronGitHub/Natron/releases/download/v#{version}/Natron-#{version}-macOS12-x86_64.dmg",
         verified: "github.com/NatronGitHub/Natron/"
   end
 
