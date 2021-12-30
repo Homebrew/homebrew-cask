566f7a1ee9ba327956322b735f8b46e9179cc58c
neutric <5984479+neutric@users.noreply.github.com>
2021-12-30 01:11:45 +0100

    Update jami from 202111051146 to 202112151254

--- Casks/jami.rb
+++ Casks/jami.rb
@@ -1,6 +1,6 @@
 cask "jami" do
-  version "202111051146"
-  sha256 "1898c6bb955925e3342baeb88c13b9e121cd1d2b42658597888970a7c4c1f776"
+  version "202112151254"
+  sha256 "e0d5aa095c56cf2403b4e6b83903c250851d3a7997487cd089744a7eb1f524a2"
 
   url "https://dl.jami.net/mac_osx/jami-#{version}.dmg"
   name "Jami"
