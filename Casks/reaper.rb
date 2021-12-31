2c40eeca9f8ad8ffb95d5de2e88a62aea522e76e
Bevan Kay <email@bevankay.me>
2021-12-31 13:40:30 +1100

    reaper: fix <= :mojave shasum mismatch (#116707)

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -2,7 +2,7 @@ cask "reaper" do
   version "6.43"
 
   if MacOS.version <= :mojave
-    sha256 "899e32ecc25666da5b6237c76a69f71258ca7e4bdf3443f6bc62606cfc5240cb"
+    sha256 "49f400ede001b934e5b6d2eba2e439b352be1b34697764c1e6f275d12292a5eb"
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64.dmg"
   else
     sha256 "45a0b13e4abcd523feb05c3b51991e4f986fc709f1fc326ce4b9e3ee6d7c49a3"
