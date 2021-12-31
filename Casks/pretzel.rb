00dc3a3885c993ddb3d8a91124b24819246488ac
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-12-31 19:23:27 +0900

    Update pretzel from 2.9.4 to 2.9.7 (#116708)

--- Casks/pretzel.rb
+++ Casks/pretzel.rb
@@ -1,6 +1,6 @@
 cask "pretzel" do
-  version "2.9.4"
-  sha256 "3d8b59ab6790ebe87207473bbfb2180e9d7ed4e712e17dbf92c3041fbf32b906"
+  version "2.9.7"
+  sha256 "418e8ba7527888fda068f8d9a4e31bd5f891fc5517732094f90d313d71a3e667"
 
   url "https://download.pretzel.rocks/Pretzel-#{version}-mac.zip"
   name "Pretzel"
