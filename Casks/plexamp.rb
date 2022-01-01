67c25a5cf38826553199d7623bac76e688aebf27
disrupted <hi@salomonpopp.me>
2022-01-01 16:19:48 +0100

    Move livecheck out of conditional

--- Casks/plexamp.rb
+++ Casks/plexamp.rb
@@ -5,25 +5,21 @@ cask "plexamp" do
 
     url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}.dmg",
         verified: "plexamp.plex.tv/"
-
-    livecheck do
-      url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/latest-mac.yml"
-      strategy :electron_builder
-    end
   else
     sha256 "765ae9021dd7a2e4129bf48c5f26517e9dea8e1a93941bfc36e81544fa71136a"
 
     url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}-arm64.dmg",
         verified: "plexamp.plex.tv/"
-
-    livecheck do
-      skip "No version information available"
-    end
   end
 
   name "Plexamp"
   desc "Music player focusing on visuals"
   homepage "https://plexamp.com/"
+
+  livecheck do
+    url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/latest-mac.yml"
+    strategy :electron_builder
+  end
 
   app "Plexamp.app"
 end
