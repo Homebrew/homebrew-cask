c3ee0fe0d96b1f681d71ecc6bfa1d303801bc665
kytwb <412895+kytwb@users.noreply.github.com>
2022-01-01 08:51:01 +0100

    Update ferdi from 5.6.5 to 5.6.6 (#116720)

--- Casks/ferdi.rb
+++ Casks/ferdi.rb
@@ -1,12 +1,12 @@
 cask "ferdi" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  version "5.6.5"
+  version "5.6.6"
 
   if Hardware::CPU.intel?
-    sha256 "11a58dc5d2ddcbc7e1daa585a3eacee5b622a178ab94f2e41016d57bc74340f7"
+    sha256 "d3d94d28b67fadb225b602e8788f624694d34741de49cadf6d77c6f8491c274b"
   else
-    sha256 "cbc6c6561d9f5ec4ea1b7033bab20777ebf92ac0143ac65f78eea52e310b8283"
+    sha256 "00f6e4863547ad9e3027442d132d4589521b37d576c57750127266334846f1f8"
   end
 
   url "https://github.com/getferdi/ferdi/releases/download/v#{version}/Ferdi-#{version}#{arch}.dmg",
