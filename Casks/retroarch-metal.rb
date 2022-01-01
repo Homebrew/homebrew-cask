58e77224e520f1ff735470c43681219e7eb560dc
Porkepix <porkepix@gmail.com>
2022-01-01 01:24:49 +0100

    Update retroarch-metal from 1.9.13 to 1.9.14

--- Casks/retroarch-metal.rb
+++ Casks/retroarch-metal.rb
@@ -1,6 +1,6 @@
 cask "retroarch-metal" do
-  version "1.9.13"
-  sha256 "0103379e15ec8bc0ecf00213eda1bc672b91b734d9188e19dd80c920d7becdd0"
+  version "1.9.14"
+  sha256 "d9bd2c827c061d20360b59799797f62832bade633f35545fc288ad5a1e14cda0"
 
   url "https://buildbot.libretro.com/stable/#{version}/apple/osx/universal/RetroArch_Metal.dmg"
   name "RetroArch"
