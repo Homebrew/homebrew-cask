afca151fe9fc8d8865d703963a48f874f639be3c
Scott Drennan <dttocs@pobox.com>
2022-01-01 12:37:31 -0400

    Add DirEqual 4.0
    Allow DirEqual, https://naarakstudio.com/direqual/ to be installed via brew.

---
+++ Casks/direqual.rb
@@ -0,0 +1,11 @@
+cask "direqual" do
+  version "4.0"
+  sha256 :no_check
+
+  url "https://naarakstudio.com/download/DirEqual.dmg"
+  name "DirEqual"
+  desc "Advanced directory compare utility"
+  homepage "https://naarakstudio.com/direqual/"
+
+  app "DirEqual.app"
+end
