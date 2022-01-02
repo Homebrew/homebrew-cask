0958030a3e27aab73fdcc8668ea88ba8167dc147
nrlquaker <ustynov.vy@gmail.com>
2022-01-02 10:12:21 +0200

    Update nrlquaker-winbox to 3.32.0 (#116752)

--- Casks/nrlquaker-winbox.rb
+++ Casks/nrlquaker-winbox.rb
@@ -1,6 +1,6 @@
 cask "nrlquaker-winbox" do
-  version "3.31.0"
-  sha256 "431c87b4426d09b7c05de57e0c4a9fd21a41e84c3dca8050d45b3d984e1d0cef"
+  version "3.32.0"
+  sha256 "7db9d1169b1e1b961ec5793372fe4341adf91ffe9fbdc9d2a31fb18522f048e3"
 
   url "https://github.com/nrlquaker/winbox-mac/releases/download/v#{version}/Winbox-mac-#{version}.zip"
   name "Winbox-mac"
