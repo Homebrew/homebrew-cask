75be6ca0f26f8faf561523408be7b93180ad439b
Aleksandr Fedotov <mezonix@gmail.com>
2022-01-02 18:22:27 +0300

    Update kkbox.rb

--- Casks/kkbox.rb
+++ Casks/kkbox.rb
@@ -1,11 +1,28 @@
 cask "kkbox" do
-  version "5.2.88-1c04d41"
-  sha256 "789e386baff3ad386eb2275322eef8ce85c90908a5e37d23121f1ee83fa93fb4"
+  version "21.12.10"
+  sha256 "96b5fbe1a1c13b75eef940a75e2a3f8ab7a28918d28c7c44e4de24879ce199d4"
 
-  url "https://download.kkbox.com/files/KKBOX-#{version}.dmg"
+  url "https://dl-universal.kfs.io/#{version}/KKBOX-#{version}.dmg",
+      verified: "dl-universal.kfs.io/"
   name "KKBOX"
   desc "Music streaming service"
   homepage "https://www.kkbox.com/"
 
+  depends_on macos: ">= :el_capitan"
+
   app "KKBOX.app"
+
+  zap trash: [
+    "~/Library/Application Support/KKBOX",
+    "~/Library/Application Support/com.kkbox.KKBOX",
+    "~/Library/Caches/KKBOX",
+    "~/Library/Caches/com.kkbox.KKBOX",
+    "~/Library/Caches/com.kkbox.electron-app",
+    "~/Library/Caches/com.kkbox.electron-app.ShipIt",
+    "~/Library/HTTPStorages/com.kkbox.KKBOX",
+    "~/Library/HTTPStorages/com.kkbox.electron-app",
+    "~/Library/Preferences/com.kkbox.KKBOX.plist",
+    "~/Library/Preferences/com.kkbox.electron-app.plist",
+    "~/Library/Saved Application State/com.kkbox.electron-app.savedState",
+  ]
 end
