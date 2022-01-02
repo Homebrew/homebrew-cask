69b1df52b61b0f525b5b02f04740e3aa1607d903
Scott Drennan <dttocs@pobox.com>
2022-01-01 22:14:21 -0400

    Update cask DirEqual to include zap stanza

--- Casks/direqual.rb
+++ Casks/direqual.rb
@@ -8,4 +8,12 @@ cask "direqual" do
   homepage "https://naarakstudio.com/direqual/"
 
   app "DirEqual.app"
+
+  zap trash: [
+    "~/Library/Caches/com.naarak.DirEqual",
+    "~/Library/Containers/com.naarak.DirEqual",
+    "~/Library/Application Scripts/com.naarak.DirEqual",
+    "~/Library/Preferences/com.naarak.DirEqual.plist",
+    "~/Library/WebKit/com.naarak.DirEqual",
+  ]
 end
