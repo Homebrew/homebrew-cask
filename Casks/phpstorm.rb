a3a63b29da20526f49edc1fc3ca53aa392e97f95
Hannes Braun <hannesbraun@mail.de>
2022-01-02 18:55:49 +0100

    Update phpstorm from 2021.3 to 2021.3.1 (#116801)
    * Update phpstorm from 2021.3 to 2021.3.1
    
    * Update phpstorm.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/phpstorm.rb
+++ Casks/phpstorm.rb
@@ -1,16 +1,15 @@
 cask "phpstorm" do
-  version "2021.3,213.5744.279"
+  arch = Hardware::CPU.intel? ? "" : "-aarch64"
+
+  version "2021.3.1,213.6461.83"
 
   if Hardware::CPU.intel?
-    sha256 "3baedcc1cfac39b49a87d284e01c539dd044cf48dae9aa8cd82f5860a3874fba"
-
-    url "https://download.jetbrains.com/webide/PhpStorm-#{version.before_comma}.dmg"
+    sha256 "389ae36007b66b75f8e0880981b77a72f33199e2feee39c8aaca6027b266d085"
   else
-    sha256 "763ce71f924bb760c14281797aa31fd3d2eaa43ad2c3a9135ee476a25a5fa46e"
-
-    url "https://download.jetbrains.com/webide/PhpStorm-#{version.before_comma}-aarch64.dmg"
+    sha256 "e6d5f13c596100445411e2b406093768b13004c593d5081b9628ef128f1c313c"
   end
 
+  url "https://download.jetbrains.com/webide/PhpStorm-#{version.csv.first}#{arch}.dmg"
   name "JetBrains PhpStorm"
   desc "PHP IDE by JetBrains"
   homepage "https://www.jetbrains.com/phpstorm/"
@@ -42,7 +41,7 @@ cask "phpstorm" do
     "~/Library/Application Support/PhpStorm#{version.major_minor}",
     "~/Library/Caches/PhpStorm#{version.major_minor}",
     "~/Library/Logs/PhpStorm#{version.major_minor}",
-    "~/Library/Preferences/PhpStorm#{version.major_minor}",
     "~/Library/Preferences/jetbrains.phpstorm.*.plist",
+    "~/Library/Preferences/PhpStorm#{version.major_minor}",
   ]
 end
