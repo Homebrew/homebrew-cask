74f6dd06a7f2d56433cda2d2c0c84147b6732bad
Scott Drennan <scottd@pobox.com>
2022-01-02 04:45:20 -0400

    Add DirEqual 4.0 (#116750)
    * Add DirEqual 4.0
    
    Allow DirEqual, https://naarakstudio.com/direqual/ to be installed via brew.
    
    * Update cask DirEqual to include zap stanza
    
    * Update direqual.rb
    
    Co-authored-by: Scott Drennan <dttocs@pobox.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/direqual.rb
@@ -0,0 +1,24 @@
+cask "direqual" do
+  version "4.0,40009"
+  sha256 :no_check
+
+  url "https://naarakstudio.com/download/DirEqual.dmg"
+  name "DirEqual"
+  desc "Advanced directory compare utility"
+  homepage "https://naarakstudio.com/direqual/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
+
+  app "DirEqual.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.naarak.DirEqual",
+    "~/Library/Caches/com.naarak.DirEqual",
+    "~/Library/Containers/com.naarak.DirEqual",
+    "~/Library/Preferences/com.naarak.DirEqual.plist",
+    "~/Library/WebKit/com.naarak.DirEqual",
+  ]
+end
