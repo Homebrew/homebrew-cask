7955e303dfa2d6f9e107858cc4e09d33e88f20a1
Aleksandr Fedotov <mezonix@gmail.com>
2022-01-02 21:06:46 +0300

    Update kkbox.rb (#116783)
    * Update kkbox.rb
    
    * Update kkbox.rb
    
    * Add livecheck
    
    * Update url
    
    * Update regex
    
    * Update kkbox.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/kkbox.rb
+++ Casks/kkbox.rb
@@ -1,10 +1,33 @@
 cask "kkbox" do
-  version "5.2.88-1c04d41"
-  sha256 "789e386baff3ad386eb2275322eef8ce85c90908a5e37d23121f1ee83fa93fb4"
+  version "21.12.10"
+  sha256 "96b5fbe1a1c13b75eef940a75e2a3f8ab7a28918d28c7c44e4de24879ce199d4"
 
-  url "https://download.kkbox.com/files/KKBOX-#{version}.dmg"
+  url "https://dl-universal.kfs.io/#{version}/KKBOX-#{version}.dmg",
+      verified: "dl-universal.kfs.io/"
   name "KKBOX"
+  desc "Music streaming service"
   homepage "https://www.kkbox.com/"
 
+  livecheck do
+    url "https://www.kkbox.com/tw/en/download"
+    regex(/href=.*?KKBOX[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+  end
+
+  depends_on macos: ">= :el_capitan"
+
   app "KKBOX.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.kkbox.KKBOX",
+    "~/Library/Application Support/KKBOX",
+    "~/Library/Caches/com.kkbox.electron-app.ShipIt",
+    "~/Library/Caches/com.kkbox.electron-app",
+    "~/Library/Caches/com.kkbox.KKBOX",
+    "~/Library/Caches/KKBOX",
+    "~/Library/HTTPStorages/com.kkbox.electron-app",
+    "~/Library/HTTPStorages/com.kkbox.KKBOX",
+    "~/Library/Preferences/com.kkbox.electron-app.plist",
+    "~/Library/Preferences/com.kkbox.KKBOX.plist",
+    "~/Library/Saved Application State/com.kkbox.electron-app.savedState",
+  ]
 end
