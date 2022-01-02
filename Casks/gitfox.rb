5e595356bdc9589d8ebac1bc7f8bce17ea964a18
Reno <25192197+singularitti@users.noreply.github.com>
2022-01-02 16:48:35 -0500

    Update gitfox from 2.1.0 to 2.2.0

--- Casks/gitfox.rb
+++ Casks/gitfox.rb
@@ -1,6 +1,6 @@
 cask "gitfox" do
-  version "2.1.0,6488"
-  sha256 "e259834edbd4fe749bca1780edefcb19459e99514dabac56f9dc0d4b6d05b234"
+  version "2.2.0,6602"
+  sha256 "d39992b2814dbafd781248dd051e8cfa30582593a0ffd7df24bbba7a20a8d620"
 
   url "https://storage.googleapis.com/gitfox/builds/retail/#{version.after_comma}/Gitfox.#{version.after_comma}.zip",
       verified: "storage.googleapis.com/gitfox/"
