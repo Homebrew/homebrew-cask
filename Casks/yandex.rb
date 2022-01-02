3bd6444b4225003a50769fa34baed09a0f76bafc
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-03 03:26:16 +0800

    Update yandex.rb

--- Casks/yandex.rb
+++ Casks/yandex.rb
@@ -1,18 +1,13 @@
 cask "yandex" do
   arch = Hardware::CPU.intel? ? "" : "?arch=arm64"
 
-  version "21.11.0.2054,0.2054"
+  version :latest
   sha256 :no_check
 
   url "https://browser.yandex.ru/download#{arch}"
   name "Yandex.Browser"
   desc "Web browser"
   homepage "https://browser.yandex.ru/"
-
-  livecheck do
-    url :url
-    strategy :extract_plist
-  end
 
   app "Yandex.app"
 end
