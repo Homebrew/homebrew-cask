3875146076700d1bf99483f5d44db4513b3f67d4
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-02 16:15:06 +0800

    Update plexamp.rb

--- Casks/plexamp.rb
+++ Casks/plexamp.rb
@@ -1,18 +1,16 @@
 cask "plexamp" do
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
+
   version "3.9.0"
 
   if Hardware::CPU.intel?
     sha256 "a40e58af21255fc9b9c1a2363165ee2f0fb7708e68ca88c2e14d1124f78dc41b"
-
-    url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}.dmg",
-        verified: "plexamp.plex.tv/"
   else
     sha256 "765ae9021dd7a2e4129bf48c5f26517e9dea8e1a93941bfc36e81544fa71136a"
-
-    url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}-arm64.dmg",
-        verified: "plexamp.plex.tv/"
   end
 
+  url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}#{arch}.dmg",
+      verified: "plexamp.plex.tv/"
   name "Plexamp"
   desc "Music player focusing on visuals"
   homepage "https://plexamp.com/"
