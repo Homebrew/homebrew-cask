4d0a22425b6ce499e15b605e683f54d7d3577510
owine <owine@users.noreply.github.com>
2022-01-02 02:23:25 -0600

    Update cocktail from 15.1 to 15.2 (#116736)
    * Update cocktail from 15.1 to 15.2
    
    * Update cocktail.rb

--- Casks/cocktail.rb
+++ Casks/cocktail.rb
@@ -60,8 +60,8 @@ cask "cocktail" do
       strategy :sparkle
     end
   elsif MacOS.version <= :big_sur
-    version "14.4"
-    sha256 "1f684f7a06ed9d5c44513434f87070dafa000bee2311c580ceff5727ae5a3d88"
+    version "14.4.1"
+    sha256 "10a7f6731d5055a097a0dda665583ec19eaf7001618f37e2c826b07408bc2752"
 
     url "https://www.maintain.se/downloads/Cocktail#{version.major}BSE.dmg"
 
@@ -71,8 +71,8 @@ cask "cocktail" do
       regex(/macOS\s*11(?:\.\d+)*.*?(\d+(?:\.\d+)+)/i)
     end
   else
-    version "15.1"
-    sha256 "41bafa4d359557b00c637ee68f5b106843dd9b4e1bfa3b203bcd56f2d6dc42e8"
+    version "15.2"
+    sha256 "ce90ffe2089149a241e92503998ae5d65593d82bb70fbadea3c9cfc9f032e353"
 
     url "https://www.maintain.se/downloads/Cocktail#{version.major}ME.dmg"
 
