48f448139626b4da0cc811f68434aa1cb1a95995
李锋 <2994263+ng-life@users.noreply.github.com>
2022-01-03 02:06:10 +0800

    Update vnote.rb (#116776)
    fix version path

--- Casks/vnote.rb
+++ Casks/vnote.rb
@@ -2,7 +2,7 @@ cask "vnote" do
   version "3.11.0"
   sha256 "47fe4387248a82b655940013aeac8bd1dd540c23ce301f2e7e399163a40c6dc4"
 
-  url "https://github.com/vnotex/vnote/releases/download/#{version}/vnote-mac-x64_v#{version}.zip",
+  url "https://github.com/vnotex/vnote/releases/download/v#{version}/vnote-mac-x64_v#{version}.zip",
       verified: "github.com/vnotex/vnote/"
   name "VNote"
   desc "Note-taking platform"
