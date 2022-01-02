3e7c45db899c088a5ea67dcc27ba47be7bb2631d
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-03 02:18:02 +0800

    Update ssdreporter-free.rb

--- Casks/ssdreporter-free.rb
+++ Casks/ssdreporter-free.rb
@@ -2,7 +2,7 @@ cask "ssdreporter-free" do
   version "1.6.0,1542"
   sha256 "18744721b68bfd78a8e0884d213f4a29aa123865dde12efa92fd7dc2fa1ed23a"
 
-  url "https://www.corecode.io/downloads/ssdreporter_#{version.before_comma}.zip"
+  url "https://www.corecode.io/downloads/ssdreporter_#{version.csv.first}.zip"
   name "SSDReporter Free"
   desc "SSD health monitoring tool"
   homepage "https://www.corecode.io/ssdreporter/"
