0b34fa5d961aecb99a4977ffe10417bdee791ca8
Salomon Popp <hi@salomonpopp.me>
2022-01-02 09:21:34 +0100

    Update plexamp from 3.5.0 to 3.9.0 (#116747)
    * Update plexamp from 3.5.0 to 3.9.0
    
    * Manually update Intel version
    
    * Move version out of conditional
    
    * Move livecheck out of conditional
    
    * Fix style
    
    * Update plexamp.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/plexamp.rb
+++ Casks/plexamp.rb
@@ -1,30 +1,24 @@
 cask "plexamp" do
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
+
+  version "3.9.0"
+
   if Hardware::CPU.intel?
-    version "3.8.2"
-    sha256 "6fd7bcfd1ccce7249912bfa4d42947d703e02e827e4fc3eb6c199cd7e06da627"
-
-    url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}.dmg",
-        verified: "plexamp.plex.tv/"
-
-    livecheck do
-      url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/latest-mac.yml"
-      strategy :electron_builder
-    end
+    sha256 "a40e58af21255fc9b9c1a2363165ee2f0fb7708e68ca88c2e14d1124f78dc41b"
   else
-    version "3.5.0"
-    sha256 "ead85dd518814718ce57b47a2cf76d0f290a9fd6465c88369d33964a67dbb76d"
-
-    url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}-arm64.dmg",
-        verified: "plexamp.plex.tv/"
-
-    livecheck do
-      skip "No version information available"
-    end
+    sha256 "765ae9021dd7a2e4129bf48c5f26517e9dea8e1a93941bfc36e81544fa71136a"
   end
 
+  url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}#{arch}.dmg",
+      verified: "plexamp.plex.tv/"
   name "Plexamp"
   desc "Music player focusing on visuals"
   homepage "https://plexamp.com/"
+
+  livecheck do
+    url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/latest-mac.yml"
+    strategy :electron_builder
+  end
 
   app "Plexamp.app"
 end
