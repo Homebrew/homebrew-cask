005c4d1a3920db06be6c1071f9a4c5588d1cb766
neutric <5984479+neutric@users.noreply.github.com>
2022-01-02 09:11:12 +0100

    Update lark from 4.10.14 to 5.2.10 (#116757)

--- Casks/lark.rb
+++ Casks/lark.rb
@@ -2,14 +2,14 @@ cask "lark" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
   if Hardware::CPU.intel?
-    version "4.10.14,a89957"
-    sha256 "1df03dc07768e8678cb443aa8aff2b61509b2fdeb9b842c0815d1e01d02bcf17"
+    version "5.2.10,6acc59"
+    sha256 "91ead4c00a8219a8f80eada571a5350258073460a457c364af40f3592e56f091"
   else
     version "4.10.14,40a957"
     sha256 "fb8f06d56ac036a40f7bba037e01f4428fe9a54ae9d6540d0c4639c0a4213dc4"
   end
 
-  url "https://sf16-va.larksuitecdn.com/obj/lark-artifact-storage/#{version.after_comma}/Lark-darwin_#{arch}-#{version.before_comma}-signed.dmg",
+  url "https://sf16-va.larksuitecdn.com/obj/lark-artifact-storage/#{version.csv.second}/Lark-darwin_#{arch}-#{version.csv.first}-signed.dmg",
       verified: "sf16-va.larksuitecdn.com/obj/lark-artifact-storage/"
   name "Lark"
   desc "Project management software"
