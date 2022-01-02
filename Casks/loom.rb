684cf3137b78d0e34a05cc95898c17b08613113e
neutric <5984479+neutric@users.noreply.github.com>
2022-01-02 09:09:56 +0100

    Update loom from 0.110.3 to 0.112.1 (#116761)
    * Update loom from 0.110.3 to 0.112.1
    
    * loom: add zap

--- Casks/loom.rb
+++ Casks/loom.rb
@@ -1,12 +1,12 @@
 cask "loom" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  version "0.110.3"
+  version "0.112.1"
 
   if Hardware::CPU.intel?
-    sha256 "604f494c50d0958a5c182097929535022e396279211c4afcb7dd37520c775a2e"
+    sha256 "216667496fc31dbd4c1513c487da53fd25a08ea24e5bceec3d7ad53ff8c7340c"
   else
-    sha256 "26f8c4b93db1b6219f80c91022ac3413d26862ce1dfd273e23ae162a82420edf"
+    sha256 "6c981563138d5b341bd8a6371e90a6810fad8754cd607c27c40ea79c95c50918"
   end
 
   url "https://cdn.loom.com/desktop-packages/Loom-#{version}#{arch}.dmg"
@@ -22,4 +22,11 @@ cask "loom" do
   auto_updates true
 
   app "Loom.app"
+
+  zap trash: [
+    "~/Library/Application Support/Loom",
+    "~/Library/Logs/Loom",
+    "~/Library/Preferences/com.loom.desktop.plist",
+    "~/Library/Saved Application State/com.loom.desktop.savedState",
+  ]
 end
