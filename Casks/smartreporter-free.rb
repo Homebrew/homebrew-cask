6751c087dde22ba2a3330d5bc7e37dd25e93acf5
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-03 02:17:27 +0800

    Update smartreporter-free.rb

--- Casks/smartreporter-free.rb
+++ Casks/smartreporter-free.rb
@@ -2,13 +2,13 @@ cask "smartreporter-free" do
   version "3.2.0,2572"
   sha256 "c6217997c0f615d29512ec7cc017cfaee039e42678a9c69f8fe1de15b9804378"
 
-  url "https://www.corecode.io/downloads/smartreporter_#{version.before_comma}.zip"
+  url "https://www.corecode.io/downloads/smartreporter_#{version.csv.first}.zip"
   name "SMARTReporter Free"
   desc "Drive failure monitoring tool"
   homepage "https://www.corecode.io/smartreporter/"
 
   livecheck do
-    url "https://www.corecode.io/smartreporter/smartreporter3.xml"
+    url "https://www.corecode.io/smartreporter/smartreporter#{version.major}.xml"
     strategy :sparkle
   end
 
