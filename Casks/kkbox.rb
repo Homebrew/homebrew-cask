04f51e8328eed8a1e79d6b4932965acf1487bc0f
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-03 02:01:54 +0800

    Update kkbox.rb

--- Casks/kkbox.rb
+++ Casks/kkbox.rb
@@ -18,16 +18,16 @@ cask "kkbox" do
   app "KKBOX.app"
 
   zap trash: [
-    "~/Library/Application Support/KKBOX",
     "~/Library/Application Support/com.kkbox.KKBOX",
-    "~/Library/Caches/KKBOX",
-    "~/Library/Caches/com.kkbox.KKBOX",
-    "~/Library/Caches/com.kkbox.electron-app",
+    "~/Library/Application Support/KKBOX",
     "~/Library/Caches/com.kkbox.electron-app.ShipIt",
-    "~/Library/HTTPStorages/com.kkbox.KKBOX",
+    "~/Library/Caches/com.kkbox.electron-app",
+    "~/Library/Caches/com.kkbox.KKBOX",
+    "~/Library/Caches/KKBOX",
     "~/Library/HTTPStorages/com.kkbox.electron-app",
-    "~/Library/Preferences/com.kkbox.KKBOX.plist",
+    "~/Library/HTTPStorages/com.kkbox.KKBOX",
     "~/Library/Preferences/com.kkbox.electron-app.plist",
+    "~/Library/Preferences/com.kkbox.KKBOX.plist",
     "~/Library/Saved Application State/com.kkbox.electron-app.savedState",
   ]
 end
