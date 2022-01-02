1abc5e166a57d1b7dd0ef470416965f0ca1d653c
neutric <5984479+neutric@users.noreply.github.com>
2022-01-02 01:26:06 +0100

    Update loaf from 1.1.17 to 1.1.18

--- Casks/loaf.rb
+++ Casks/loaf.rb
@@ -1,6 +1,6 @@
 cask "loaf" do
-  version "1.1.17"
-  sha256 "ea16a72c45ba0e5eef5e025c66cdc6a18ebb7c8b25f092e38e42a5929b20eeac"
+  version "1.1.18"
+  sha256 "3caa44f6c262a1e8f21e07b243d6f9f22268eac833764c558df5a01bb201ac3b"
 
   url "https://github.com/philipardeljan/getloaf/releases/download/v#{version}/loaf.dmg",
       verified: "github.com/philipardeljan/getloaf/"
