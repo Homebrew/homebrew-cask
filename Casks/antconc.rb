3bd35ab8fd67a8847634cdcb11a8d9a233670b54
hyuraku <32809703+hyuraku@users.noreply.github.com>
2022-01-04 00:05:07 +0900

    Update antconc from 3.5.9 to 4.0.2 (#116836)
    * Update antconc from 3.5.9 to 4.0.2
    
    * Update antconc.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/antconc.rb
+++ Casks/antconc.rb
@@ -1,8 +1,8 @@
 cask "antconc" do
-  version "3.5.9"
-  sha256 "faac413a4bd914feec6609b96fcf368fa6e91baeedbd15303a8a0594a38db7dd"
+  version "4.0.2"
+  sha256 "a05c6ac057349f7e1588d2542a4d41f158ec4b445ec71cce42560a233936af0a"
 
-  url "https://www.laurenceanthony.net/software/antconc/releases/AntConc#{version.no_dots}/AntConc.zip"
+  url "https://www.laurenceanthony.net/software/antconc/releases/AntConc#{version.no_dots}/AntConc.dmg"
   name "AntConc"
   desc "Corpus analysis toolkit for concordancing and text analysis"
   homepage "https://www.laurenceanthony.net/software/antconc/"
