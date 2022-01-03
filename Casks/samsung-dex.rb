77ecb684a9463d85c14e56b7d6e710936a569e39
neutric <5984479+neutric@users.noreply.github.com>
2022-01-03 01:07:18 +0100

    Update samsung-dex from 20211108133743295 to 20211210154322368

--- Casks/samsung-dex.rb
+++ Casks/samsung-dex.rb
@@ -1,8 +1,8 @@
 cask "samsung-dex" do
-  version "20211108133743295"
-  sha256 "88279231aabd632dc196f6b1da191a830416ccd6498d59eb7f1d431f6fbebac6"
+  version "20211210154322368"
+  sha256 "6bf45739e81ad7970ae86147b64bc1df3392154b00672243fb666c62c983a9f6"
 
-  url "https://downloadcenter.samsung.com/content/SW/#{version[0..5]}/#{version}/SamsungDeXSetup.dmg"
+  url "https://downloadcenter.samsung.com/content/SW/#{version[0..5]}/#{version}/SamsungDeXSetupMac.dmg"
   name "Samsung DeX"
   desc "Extend some Samsung devices into a desktop-like experience"
   homepage "https://www.samsung.com/us/explore/dex/"
