5b1a0c2eb14dc9f9e41e48499875681351067f2b
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-03 23:05:41 +0800

    Update data-rescue.rb

--- Casks/data-rescue.rb
+++ Casks/data-rescue.rb
@@ -2,7 +2,7 @@ cask "data-rescue" do
   version "6.0.6,6213.62.03"
   sha256 "2e4186325d13a8442fe7ef4b3f9c3d75578d2cbcf911537f85ca07e898970884"
 
-  url "https://downloads.prosofteng.com/dr/Data_Rescue_#{version.before_comma}.dmg"
+  url "https://downloads.prosofteng.com/dr/Data_Rescue_#{version.csv.first}.dmg"
   name "Data Rescue #{version.major}"
   desc "Data recovery software"
   homepage "https://www.prosofteng.com/data-rescue-recovery-software/"
