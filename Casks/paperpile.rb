da70a4ebba3da2c3baa37bd5c2eb6280e12272d3
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-03 13:56:57 +0800

    Update paperpile.rb

--- Casks/paperpile.rb
+++ Casks/paperpile.rb
@@ -1,14 +1,15 @@
 cask "paperpile" do
   version "0.7.2"
-  sha256 :no_check
+  sha256 "d512ad93042c580cdf6343f466dd3d83c1108b2badec400d0d9ad0dc49a14807"
 
-  url "https://paperpile.com/download/desktop/macos-latest"
+  url "https://cdn.paperpile.com/download/desktop/Paperpile-#{version}.dmg"
   name "Paperpile"
   desc "Citation plugin for Microsoft Word"
   homepage "https://paperpile.com/word-plugin/"
 
   livecheck do
-    skip "No version information available"
+    url "https://paperpile-desktop.s3.amazonaws.com/production/latest-mac.yml"
+    strategy :electron_builder
   end
 
   auto_updates true
