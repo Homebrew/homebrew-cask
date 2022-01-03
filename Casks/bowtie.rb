b779df230d4099230809317dec816f628c2bb6d3
Aleksandr Fedotov <mezonix@gmail.com>
2022-01-03 17:27:51 +0300

    Update bowtie.rb

--- Casks/bowtie.rb
+++ Casks/bowtie.rb
@@ -2,7 +2,7 @@ cask "bowtie" do
   version "1.5,1500"
   sha256 "d8406b066851c0730ca052036bedd5ded82019403de1fd58b579da34cfa4a948"
 
-  url "http://bowtieapp.com/bowtie-#{version.before_comma}.zip"
+  url "http://bowtieapp.com/bowtie-#{version.csv.first}.zip"
   name "Bowtie"
   desc "Control your music with customizable shortcuts"
   homepage "http://bowtieapp.com/"
