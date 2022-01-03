916158cd3b4e2713de931328b4379a454e86c384
Aleksandr Fedotov <mezonix@gmail.com>
2022-01-03 17:46:45 +0300

    Update ti-connect-ce (#116852)

--- Casks/ti-connect-ce.rb
+++ Casks/ti-connect-ce.rb
@@ -2,7 +2,7 @@ cask "ti-connect-ce" do
   version "5.6.3.2278,BC4AE19C4CDF4270B388F7645D4A50ED"
   sha256 "066f86e1a62fe6444a5bd998afe6f95c93f34deadf35d325320ac65d47438488"
 
-  url "https://education.ti.com/download/en/ed-tech/68CEDD34FDC94622B4DBD173E6A0D8C3/#{version.after_comma}/TIConnectCE-#{version.before_comma}.dmg"
+  url "https://education.ti.com/download/en/ed-tech/68CEDD34FDC94622B4DBD173E6A0D8C3/#{version.csv.second}/TIConnectCE-#{version.csv.first}.dmg"
   name "TI Connect™ CE"
   desc "Connectivity software for the TI-84 Plus family of graphing calculators"
   homepage "https://education.ti.com/en/products/computer-software/ti-connect-ce-sw"
