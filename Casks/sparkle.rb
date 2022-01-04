7710e276f5ae6e7c9b61540e6136d5c6b16f6431
Clément L <porkepix@gmail.com>
2022-01-04 03:19:48 +0100

    Update sparkle from 1.27.1 to 2.0.0 (#116864)

--- Casks/sparkle.rb
+++ Casks/sparkle.rb
@@ -1,6 +1,6 @@
 cask "sparkle" do
-  version "1.27.1"
-  sha256 "20d729f8ca4d07911da31fe0296880cd3176b71738deaf2b7f8de2d96cf63f4e"
+  version "2.0.0"
+  sha256 "7ca516157e2f7c021313aa3e1427a18b5940b482fcb4f08120dabc90b85b0268"
 
   url "https://github.com/sparkle-project/Sparkle/releases/download/#{version}/Sparkle-#{version}.tar.xz",
       verified: "github.com/sparkle-project/Sparkle/"
