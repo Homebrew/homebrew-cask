cfd9c68223f2d4566551aa0f57868073277520bb
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-04 11:53:15 +0800

    Update vimr.rb

--- Casks/vimr.rb
+++ Casks/vimr.rb
@@ -2,15 +2,20 @@ cask "vimr" do
   version "0.36.0,20220102.161018"
   sha256 "b1361cd3b811b04702f924cdd1bca571778c56180b44c18d5a12b5e6d21a207f"
 
-  url "https://github.com/qvacua/vimr/releases/download/v#{version.before_comma}-#{version.after_comma}/VimR-v#{version.before_comma}.tar.bz2",
+  url "https://github.com/qvacua/vimr/releases/download/v#{version.csv.first}-#{version.csv.second}/VimR-v#{version.csv.first}.tar.bz2",
       verified: "github.com/qvacua/vimr/"
   name "VimR"
   desc "GUI for the Neovim text editor"
   homepage "http://vimr.org/"
 
   livecheck do
-    url :url
-    regex(/^v?(\d+(?:[.-]\d+)+)$/i)
+    url "https://github.com/qvacua/vimr/releases/latest"
+    strategy :page_match do |page|
+      match = page.match(/v?(\d+(?:\.\d+)+)[._-](\d+(?:\.\d+)*)/i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
   end
 
   auto_updates true
