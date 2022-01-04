1f894a98fd89bb1814e6067e02eeda80f0ada6d8
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-04 10:13:37 +0800

    Update jump.rb

--- Casks/jump.rb
+++ Casks/jump.rb
@@ -2,7 +2,7 @@ cask "jump" do
   version "8.8.28,80828"
   sha256 "51c47eef515cef38da22c9e06b1b9ecfcfa152d14022cdbc016e364f7385d16e"
 
-  url "https://mirror.jumpdesktop.com/downloads/jdm/JumpDesktopMac-#{version.before_comma}.zip"
+  url "https://mirror.jumpdesktop.com/downloads/jdm/JumpDesktopMac-#{version.csv.first}.zip"
   name "Jump Desktop"
   desc "Remote desktop application"
   homepage "https://jumpdesktop.com/#jdmac"
