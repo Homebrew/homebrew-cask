b692250595705dfd7a9e0cc48dcb8ad28f416fbd
Aleksandr Fedotov <mezonix@gmail.com>
2022-01-04 05:19:08 +0300

    Update edfbrowser.rb (#116866)

--- Casks/edfbrowser.rb
+++ Casks/edfbrowser.rb
@@ -2,7 +2,7 @@ cask "edfbrowser" do
   version "1.84,81b147ef06488445bd7ef8fe4b036648"
   sha256 "27217b7cc2f770921e51c31f16c2958ab5de8d61b762d889692df83bd3f61ccc"
 
-  url "https://gitlab.com/whitone/EDFbrowser/uploads/#{version.after_comma}/EDFbrowser-#{version.before_comma}.dmg",
+  url "https://gitlab.com/whitone/EDFbrowser/uploads/#{version.after_comma}/EDFbrowser-#{version.csv.first}.dmg",
       verified: "gitlab.com/whitone/EDFbrowser/"
   name "EDFbrowser"
   desc "EDF+ and BDF+ viewer and toolbox"
