612976d4d782fcf07d1ec4014a8faa0c67428183
Reno <25192197+singularitti@users.noreply.github.com>
2022-01-03 22:58:38 -0500

    Update vimr from 0.35.0 to 0.36.0 (#116903)
    * Update vimr from 0.35.0 to 0.36.0
    
    * Update vimr.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/vimr.rb
+++ Casks/vimr.rb
@@ -1,16 +1,21 @@
 cask "vimr" do
-  version "0.35.0-20211105.213803"
-  sha256 "bb0ea1ba949bee1c4a41bb459f7cbe48651a092878be4874f12a6d5200b18a7d"
+  version "0.36.0,20220102.161018"
+  sha256 "b1361cd3b811b04702f924cdd1bca571778c56180b44c18d5a12b5e6d21a207f"
 
-  url "https://github.com/qvacua/vimr/releases/download/v#{version}/VimR-v#{version}.tar.bz2",
+  url "https://github.com/qvacua/vimr/releases/download/v#{version.csv.first}-#{version.csv.second}/VimR-v#{version.csv.first}.tar.bz2",
       verified: "github.com/qvacua/vimr/"
   name "VimR"
   desc "GUI for the Neovim text editor"
   homepage "http://vimr.org/"
 
   livecheck do
-    url :url
-    regex(/^v?(\d+(?:[.-]\d+)+)$/i)
+    url "https://github.com/qvacua/vimr/releases/latest"
+    strategy :page_match do |page|
+      match = page.match(/v?(\d+(?:\.\d+)+)[._-](\d+(?:\.\d+)*)/i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
   end
 
   auto_updates true
