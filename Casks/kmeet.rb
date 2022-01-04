42196118bdfd9a15b96ba133e1ce0ff1164d7e35
Nicolas Hedger <649677+nhedger@users.noreply.github.com>
2022-01-04 22:51:59 +0100

    Add kMeet v1.1.34 (#116931)
    * Add kMeet v1.1.34
    
    * Fix style
    
    * Fix typo
    
    * Sort zaps alphabetically
    
    * Use string instead of single-line array

---
+++ Casks/kmeet.rb
@@ -0,0 +1,24 @@
+cask "kmeet" do
+  version "1.1.34"
+  sha256 :no_check
+
+  url "https://download.storage.infomaniak.com/meet/kMeet.dmg"
+  name "kMeet"
+  desc "Client for the kMeet videoconferencing solution"
+  homepage "https://kmeet.infomaniak.com/"
+
+  livecheck do
+    url "https://download.storage.infomaniak.com/meet/latest-mac.yml"
+    strategy :electron_builder
+  end
+
+  app "kMeet.app"
+
+  uninstall quit: "com.infomaniak.meet"
+
+  zap trash: [
+    "~/Library/Application Support/Caches/kmeet-electron-updater",
+    "~/Library/Application Support/kMeet",
+    "~/Library/Logs/kMeet",
+  ]
+end
