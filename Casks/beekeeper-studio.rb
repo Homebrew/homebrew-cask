e2ab46002dec337ae05ddf493955f4dcdf62145e
Branch Vincent <branchevincent@gmail.com>
2022-01-04 16:44:39 -0500

    Update beekeeper-studio from 3.0.1 to 3.0.3 (#116927)

--- Casks/beekeeper-studio.rb
+++ Casks/beekeeper-studio.rb
@@ -1,6 +1,6 @@
 cask "beekeeper-studio" do
-  version "3.0.1"
-  sha256 "cf9a3d6c2f187149f76153ac445d4943db00193bccd7b9d20509665760a946e6"
+  version "3.0.3"
+  sha256 "8463b6702becb46c27176f957997c68de835cc9cf8f5f30031c6ee996afa2700"
 
   url "https://github.com/beekeeper-studio/beekeeper-studio/releases/download/v#{version}/Beekeeper-Studio-#{version}.dmg",
       verified: "github.com/beekeeper-studio/beekeeper-studio/"
