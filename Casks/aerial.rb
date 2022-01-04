cab3c81de81dd5d17cc96b3dfa5401dc8d170038
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-04 11:14:24 +0800

    Update aerial.rb (#116897)

--- Casks/aerial.rb
+++ Casks/aerial.rb
@@ -7,9 +7,18 @@ cask "aerial" do
   desc "Apple TV Aerial screensaver"
   homepage "https://github.com/JohnCoates/Aerial"
 
+  conflicts_with cask: "homebrew/cask-versions/aerial-beta"
   depends_on macos: ">= :sierra"
 
   screen_saver "Aerial.saver"
 
-  zap trash: "~/Library/Caches/Aerial"
+  zap trash: [
+    "~/Library/Application Support/Aerial",
+    "~/Library/Caches/Aerial",
+    "~/Library/Containers/com.apple.ScreenSaver.Engine.legacyScreenSaver.x86-64/Data/Library/Caches/Aerial",
+    "~/Library/Containers/com.apple.ScreenSaver.Engine.legacyScreenSaver/Data/Library/Application Support/Aerial",
+    "~/Library/Containers/com.apple.ScreenSaver.Engine.legacyScreenSaver/Data/Library/Caches/Aerial",
+    "~/Library/Preferences/ByHost/com.JohnCoates.Aerial*",
+    "~/Library/Screen Savers/Aerial.saver",
+  ]
 end
