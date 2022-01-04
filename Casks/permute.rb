745361ebe3ecd0aa156d3a67798e8a2d4331134e
ShredderWalnut <34428332+ShredderWalnut@users.noreply.github.com>
2022-01-04 15:39:30 +0800

    Update permute.rb (#116907)
    * Update permute.rb
    
    * Update permute.rb
    
    * Update permute.rb

--- Casks/permute.rb
+++ Casks/permute.rb
@@ -2,14 +2,13 @@ cask "permute" do
   version "3.8,2553"
   sha256 "5dd20d58fbb51c48b3945feb2a0164e94caa1c80d8ebbc050d68bf50bed5c473"
 
-  url "https://charliemonroesoftware.com/trial/permute/v#{version.major}/Permute_#{version.major}_#{version.csv.second}.dmg",
-      verified: "charliemonroesoftware.com/"
+  url "https://software.charliemonroe.net/trial/permute/v#{version.major}/Permute_#{version.major}_#{version.csv.second}.dmg"
   name "Permute"
   desc "Converts and edits video, audio or image files"
   homepage "https://software.charliemonroe.net/permute/"
 
   livecheck do
-    url "https://charliemonroesoftware.com/trial/permute/updates_#{version.major}.xml"
+    url "https://software.charliemonroe.net/trial/permute/v#{version.major}/updates.xml"
     strategy :sparkle
   end
 
