d4f2276a0cc14d2fb721a134b270c582d90bf635
neutric <5984479+neutric@users.noreply.github.com>
2022-01-04 04:24:51 +0100

    captain: update url and add desc

--- Casks/captain.rb
+++ Casks/captain.rb
@@ -2,9 +2,9 @@ cask "captain" do
   version "9.0.0"
   sha256 :no_check
 
-  url "https://www.dropbox.com/s/pkayzy14unr4r0a/Captain.dmg?dl=1",
-      verified: "dropbox.com/s/pkayzy14unr4r0a/"
+  url "https://getcaptain.co/downloads/Captain.dmg"
   name "Captain"
+  desc "Manage Docker containers from the menu bar"
   homepage "https://getcaptain.co/"
 
   app "Captain.app"
