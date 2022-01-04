59935f5bf10f7515427338268e72263a77531f46
neutric <5984479+neutric@users.noreply.github.com>
2022-01-04 02:14:23 +0100

    Update macspice from 3.1.25 to 3.2.1

--- Casks/macspice.rb
+++ Casks/macspice.rb
@@ -1,8 +1,8 @@
 cask "macspice" do
-  version "3.1.25,343"
-  sha256 "22f82c32b6e79916b123a7d1f5ded71a9a30cffc5ab63262b43a0d11db3c45f9"
+  version "3.2.1,351"
+  sha256 "85e8ec002c9a1af823bd18766176d3e77b51173fa91e482da6ada30bf84950d7"
 
-  url "https://www.macspice.com/binaries/v#{version.csv.first}/MacSpice3f5.dmg"
+  url "https://www.macspice.com/mirror/binaries/v#{version.csv.first}/MacSpice3f5.dmg"
   name "MacSpice"
   desc "Electronic circuit simulator"
   homepage "https://www.macspice.com/"
