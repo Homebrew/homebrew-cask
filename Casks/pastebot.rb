097dc7be94eb08aeb74e402cdbe39fc485950a46
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-05 07:08:48 +0800

    pastebot 2.4.1 (#116948)

--- Casks/pastebot.rb
+++ Casks/pastebot.rb
@@ -1,12 +1,17 @@
 cask "pastebot" do
-  version "2.4"
-  sha256 "da7b93ca13110fcc4f2adc267c287a62692d25c7a4b09f14e849c5283bb898af"
+  version "2.4.1"
+  sha256 "8a169360a98ee67816fca9c6bcde423de71606e3e70ec19d9bcf48ea6a75f77f"
 
   url "https://tapbots.net/pastebot#{version.major}/Pastebot.dmg",
       verified: "tapbots.net/"
   name "Pastebot"
   desc "Workflow application to improve productivity"
   homepage "https://tapbots.com/pastebot/"
+
+  livecheck do
+    url "https://tapbots.net/pastebot2/update.plist"
+    regex(%r{<key>shortVersion</key>.*\n.*<string>(\d+(?:\.\d+)+)</string>}i)
+  end
 
   depends_on macos: ">= :mojave"
 
