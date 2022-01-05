6d03ff8a7a0f4dcf70eae7130f555a906c6b9b23
Clément L <porkepix@gmail.com>
2022-01-05 13:28:22 +0100

    Update electron from 16.0.5 to 16.0.6 (#116975)

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,12 +1,12 @@
 cask "electron" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "16.0.5"
+  version "16.0.6"
 
   if Hardware::CPU.intel?
-    sha256 "0bb7f2c506d8c3e9ef7ec4049baf87a3365cbf80c569f6eb98ddc1a2ddb653e6"
+    sha256 "250489ca6b1bec91f4d59766c1d4f65407f016765395517a6dfd5625e93d646f"
   else
-    sha256 "de4eac412a942a0b238792a38c0c80691d1a7ef2eba850c15619c0db9da89f1a"
+    sha256 "a4416627a3055308259689fd156a7d4fd704983420a20e8939409b08195af204"
   end
 
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-#{arch}.zip",
