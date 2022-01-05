2b9413c0f5fb64185e7ee85c403cb2f29191df08
Porkepix <porkepix@gmail.com>
2022-01-05 14:08:19 +0100

    Update nimblenote from 3.1.2 to 3.2.1

--- Casks/nimblenote.rb
+++ Casks/nimblenote.rb
@@ -1,6 +1,6 @@
 cask "nimblenote" do
-  version "3.1.2"
-  sha256 "37d8a004d36e03c9d98c4d7ab989d05644ce66c65dc897152cbe39e50a5258dd"
+  version "3.2.1"
+  sha256 "934feef24473724bbf8800337fb2f9898835581f55182e7fb8a7fdf4bcc943f5"
 
   url "https://github.com/nimblenote/nimblenote/releases/download/v#{version}/nimblenote-#{version}.dmg",
       verified: "github.com/nimblenote/nimblenote/"
