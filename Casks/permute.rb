14e14a2d163222c35a237debd8f15215dc67a84a
Reno <25192197+singularitti@users.noreply.github.com>
2022-01-05 07:22:18 -0500

    Update permute from 3.8 to 3.8.1 (#116973)

--- Casks/permute.rb
+++ Casks/permute.rb
@@ -1,6 +1,6 @@
 cask "permute" do
-  version "3.8,2553"
-  sha256 "5dd20d58fbb51c48b3945feb2a0164e94caa1c80d8ebbc050d68bf50bed5c473"
+  version "3.8.1,2556"
+  sha256 "6891802dd99bdaecb01d5d3fc7bcc99c05a97e164d9e4403d3f4890c16783cf5"
 
   url "https://software.charliemonroe.net/trial/permute/v#{version.major}/Permute_#{version.major}_#{version.csv.second}.dmg"
   name "Permute"
