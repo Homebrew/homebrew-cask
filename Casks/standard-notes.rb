10f497c1f96079c1a89218b09b5a5d12c66864c5
Jakob Yanagibashi <jeifour@users.noreply.github.com>
2022-01-05 18:47:46 +0100

    Upgrade standard-notes from 3.9.10 to 3.9.12

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -1,9 +1,18 @@
 cask "standard-notes" do
-  version "3.9.10"
-  sha256 "6a1af9b49a75429128d55c7d56e4c30fff2e14773c817dba416eb7d6898303f3"
+  version "3.9.12"
 
-  url "https://github.com/standardnotes/desktop/releases/download/v#{version}/Standard-Notes-#{version}-mac-x64.zip",
-      verified: "github.com/standardnotes/desktop/"
+  if Hardware::CPU.intel?
+    sha256 "63f808938e05aa0487fc3f89c5db40cf7c63141f3963af41fcb97aa29265b6ae"
+
+    url "https://github.com/standardnotes/desktop/releases/download/v#{version}/Standard-Notes-#{version}-mac-x64.zip",
+        verified: "github.com/standardnotes/desktop/"
+  else
+    sha256 "9584a1d2cce20979b24d0f8ff9df041fd636f281d2a9dca290a1ab524e9a85a6"
+
+    url "https://github.com/standardnotes/desktop/releases/download/v#{version}/Standard-Notes-#{version}-mac-arm64.zip",
+        verified: "github.com/standardnotes/desktop/"
+  end
+
   name "Standard Notes"
   desc "Free, open-source, and completely encrypted notes app"
   homepage "https://standardnotes.org/"
