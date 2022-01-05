00c1662e0f5f815f8f23814921b4390814e24af6
neutric <5984479+neutric@users.noreply.github.com>
2022-01-06 00:18:13 +0100

    Update movist-pro from 2.6.7 to 2.8.1

--- Casks/movist-pro.rb
+++ Casks/movist-pro.rb
@@ -1,8 +1,8 @@
 cask "movist-pro" do
-  version "2.6.7,174"
-  sha256 "6e19719e17f93b09337094ba03af6c28cb4a9e4e15e5b9f012e1471cc7948b1e"
+  version "2.8.1,181"
+  sha256 "3cf62512c6ef5fd4d8d9f0ee47a14c99d8e85a1384e5cc73217f20dd31cc55a7"
 
-  url "https://s3.ap-northeast-2.amazonaws.com/update.cocoable.com/MovistPro_#{version.before_comma}.dmg",
+  url "https://s3.ap-northeast-2.amazonaws.com/update.cocoable.com/MovistPro_#{version.csv.first}.dmg",
       verified: "s3.ap-northeast-2.amazonaws.com/update.cocoable.com/"
   name "Movist Pro"
   desc "Media player"
