7e5ecd715438c87f788b36296b1af0c86891dcf8
neutric <5984479+neutric@users.noreply.github.com>
2022-01-06 00:20:12 +0100

    Update missive from 10.5.0 to 10.15.0

--- Casks/missive.rb
+++ Casks/missive.rb
@@ -1,6 +1,6 @@
 cask "missive" do
-  version "10.5.0"
-  sha256 "e1ba7e4f127f49fffa7c70980ff2269ba089cd2751718d1382ef52bd8b8dac36"
+  version "10.15.0"
+  sha256 "fa953a8a4459b4a52ae36861f2c701833615fd2878a00ce5ad65735fd3878f3f"
 
   url "https://d1weoh55n7oehu.cloudfront.net/#{version}/Missive-#{version}.dmg",
       verified: "d1weoh55n7oehu.cloudfront.net/"
