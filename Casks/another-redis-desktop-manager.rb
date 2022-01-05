ba82979f578e52ce3b6f4fa7698370063f047b68
Porkepix <porkepix@gmail.com>
2022-01-05 15:12:00 +0100

    Update another-redis-desktop-manager from 1.5.0 to 1.5.1

--- Casks/another-redis-desktop-manager.rb
+++ Casks/another-redis-desktop-manager.rb
@@ -1,12 +1,12 @@
 cask "another-redis-desktop-manager" do
-  version "1.5.0"
+  version "1.5.1"
 
   if Hardware::CPU.intel?
-    sha256 "ef3f82d87064d2c4673285647732878070bbdf709db25a84a099b01ae918660a"
+    sha256 "fea712bdb336935ac4f00aebcc5005d9c7ffc141cfa8996156df31f72a7571b2"
 
     url "https://github.com/qishibo/AnotherRedisDesktopManager/releases/download/v#{version}/Another-Redis-Desktop-Manager.#{version}.dmg"
   else
-    sha256 "00a5dd87fa8915d1ae96d1b70a33a056efdd79711561773ca208ec71ca72a29e"
+    sha256 "564d1f534186f97244bb4d4ec667e27f2faf7bc741f01dc8adc905ece541f074"
 
     url "https://github.com/qishibo/AnotherRedisDesktopManager/releases/download/v#{version}/Another-Redis-Desktop-Manager-M1-arm64-#{version}.dmg"
   end
