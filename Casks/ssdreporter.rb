cb121fb3630b69e5bb2fe1e3c488469c1d9a5139
Bevan Kay <email@bevankay.me>
2022-01-05 14:38:53 +1100

    rename cask token

--- Casks/ssdreporter.rb
+++ Casks/ssdreporter.rb
@@ -1,9 +1,9 @@
-cask "ssdreporter-free" do
+cask "ssdreporter" do
   version "1.6.0,1575"
   sha256 "d176e7c0d63d220ac061fde998cd9ad0563d437b60019bc46acd720327359cd3"
 
   url "https://www.corecode.io/downloads/ssdreporter_#{version.csv.first}.zip"
-  name "SSDReporter Free"
+  name "SSDReporter"
   desc "SSD health monitoring tool"
   homepage "https://www.corecode.io/ssdreporter/"
 
