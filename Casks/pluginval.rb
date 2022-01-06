eac5df3dd6294c4578c4e1f3701cebac66eb5a19
Bevan Kay <email@bevankay.me>
2022-01-07 09:21:37 +1100

    Update pluginval.rb

--- Casks/pluginval.rb
+++ Casks/pluginval.rb
@@ -5,18 +5,13 @@ cask "pluginval" do
   url "https://github.com/Tracktion/pluginval/releases/download/v#{version}/pluginval_macOS.zip",
       verified: "https://github.com/Tracktion/pluginval/"
   name "pluginval"
-  desc "Is a cross-platform plugin validator and tester application"
+  desc "Cross-platform plugin validator and tester application"
   homepage "https://www.tracktion.com/develop/pluginval"
-
-  livecheck do
-    url :stable
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
-  end
 
   app "pluginval.app"
 
   zap trash: [
+    "~/Library/Application Support/pluginval/pluginval.xml",
     "~/Library/Caches/pluginval/pluginval_crash.txt",
-    "~/Library/Application\ Support/pluginval/pluginval.xml",
   ]
 end
