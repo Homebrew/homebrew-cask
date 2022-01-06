6dc6d2a611a0d83ea576fbdf9459b7cb30eb4cdc
Porkepix <porkepix@gmail.com>
2022-01-06 03:35:39 +0100

    Update jgrennison-openttd from 0.44.2 to 0.45.0

--- Casks/jgrennison-openttd.rb
+++ Casks/jgrennison-openttd.rb
@@ -1,6 +1,6 @@
 cask "jgrennison-openttd" do
-  version "0.44.2"
-  sha256 "f727fe52a202677ccc223071a938858dc57ba3b99db9b5f3f4b44f3993375757"
+  version "0.45.0"
+  sha256 "825adcfdf36a477dde6999230b37fcf4113e0f0d204c478d5aecdd064951bcce"
 
   url "https://github.com/JGRennison/OpenTTD-patches/releases/download/jgrpp-#{version}/openttd-jgrpp-#{version}-macos-universal.dmg"
   name "JGR's OpenTTD Patchpack"
