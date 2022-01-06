c4ba63d322641c29116ec72dccd9bdeb706ae3be
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-07 06:32:45 +0800

    Update lunasea.rb

--- Casks/lunasea.rb
+++ Casks/lunasea.rb
@@ -1,11 +1,23 @@
 cask "lunasea" do
-  version "6.0.0"
-  sha256 :no_check
+  version "6.0.0,60000006"
+  sha256 "10ad53618b83ca1b94ef0190805911f54c73865fd477695191991c41223454d2"
 
-  url "https://github.com/CometTools/LunaSea/releases/download/6.0.0%2B60000006/LunaSea-macOS-60000006.zip"
+  url "https://github.com/CometTools/LunaSea/releases/download/#{version.csv.first}%2B#{version.csv.second}/LunaSea-macOS-#{version.csv.second}.zip"
   name "LunaSea"
-  desc "Is a fully featured, open source self-hosted controller"
-  homepage "https://github.com/CometTools/LunaSea"
+  desc "Open source self-hosted remote control for media software"
+  homepage "https://github.com/CometTools/LunaSea/"
+
+  livecheck do
+    url :url
+    strategy :git do |tags|
+      tags.map do |tag|
+        match = tag.match(/^v?(\d+(?:\.\d+)+)[+-](\d+(?:\.\d+)*)$/i)
+        next if match.blank?
+
+        "#{match[1]},#{match[2]}"
+      end
+    end
+  end
 
   app "LunaSea.app"
 end
