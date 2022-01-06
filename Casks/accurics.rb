da3a891ad037d458c0d60ed78dfdf7390e0dc51f
Willie <willie@accurics.com>
2022-01-05 16:28:34 -0800

    Update accurics from 1.0.29 to 1.0.30 (#116996)
    Co-authored-by: Willie Sana <wsana@tenable.com>

--- Casks/accurics.rb
+++ Casks/accurics.rb
@@ -1,6 +1,6 @@
 cask "accurics" do
-  version "1.0.29"
-  sha256 "8fcb49cbbdac474f6fd157691cb5feb506f4be50e43378a5c67cf306bd078d7d"
+  version "1.0.30"
+  sha256 "b6cb02513aca9fec505930759e50e9c7b97c0267534e73a6031ef891ce91d49c"
 
   url "https://downloads.accurics.com/cli/#{version}/accurics-cli.dmg",
       user_agent: :fake
