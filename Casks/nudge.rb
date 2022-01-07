c7a76dfc8f3f1cd5f69bc9a5bee31827f2b0162c
neutric <5984479+neutric@users.noreply.github.com>
2022-01-07 03:16:34 +0100

    Update nudge from 1.1.2.10212021172015 to 1.1.3.11292021162608 (#117047)
    * Update nudge from 1.1.2.10212021172015 to 1.1.3.11292021162608
    
    * nudge: switch livecheck strategy to avoid pre-releases

--- Casks/nudge.rb
+++ Casks/nudge.rb
@@ -1,11 +1,16 @@
 cask "nudge" do
-  version "1.1.2.10212021172015"
-  sha256 "c95fe9c8718cdfc08911be6b549ceef341a3800e9e09c3e6b2ec98cf1fc08ee3"
+  version "1.1.3.11292021162608"
+  sha256 "d0ece3ccffd8dbfa0b6c065bd7ce91305f9ca2347b30091ee53a483484f19144"
 
   url "https://github.com/macadmins/nudge/releases/download/v#{version}/Nudge-#{version}.pkg"
   name "Nudge"
   desc "Application for enforcing OS updates"
   homepage "https://github.com/macadmins/nudge"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   depends_on macos: ">= :big_sur"
 
