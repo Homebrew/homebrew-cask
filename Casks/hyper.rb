09f85c900e709df50f05bed68aae834f1cc4496d
Bevan Kay <email@bevankay.me>
2022-01-08 08:04:48 +1100

    Update hyper from 3.1.4 to 3.1.5 (#117083)

--- Casks/hyper.rb
+++ Casks/hyper.rb
@@ -1,14 +1,14 @@
 cask "hyper" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "3.1.4"
+  version "3.1.5"
 
   url "https://github.com/vercel/hyper/releases/download/v#{version}/Hyper-#{version}-mac-#{arch}.zip",
       verified: "github.com/vercel/hyper/"
   if Hardware::CPU.intel?
-    sha256 "f6038c7da47f98f12520c5af5aa08d6ef171897eda0c49b2cc8a189bf0301ffe"
+    sha256 "3d87e611190678a4b7240c6f4e373f8dffe6b838f0fb235b33be134931e87da0"
   else
-    sha256 "339bce09a3b68b865ea8157feaaeac13f2ac80a2e661d95f9c5b6a1191ede5ac"
+    sha256 "ab7dd0833e87311fc53fe32390377cbbf99387e3668b18c77082aa58da1a89fd"
   end
 
   name "Hyper"
