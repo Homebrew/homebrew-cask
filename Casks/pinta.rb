27daf0da5672a77bcb73b3bb2cb70042d23f824b
Andrew Carr <7977719+athisun@users.noreply.github.com>
2022-01-08 06:49:17 +1000

    Update pinta from 1.7.1 to 2.0.1 (#117073)
    * Update pinta from 1.7.1 to 2.0.1
    
    * Add missing zap file
    
    * Update zap files

--- Casks/pinta.rb
+++ Casks/pinta.rb
@@ -1,14 +1,17 @@
 cask "pinta" do
-  version "1.7.1"
-  sha256 "f4146af00919c64bb544461d896bfab98a6249183f3d47d78af4b2b125c9ee8e"
+  version "2.0.1"
+  sha256 "118f6fd52270d00de80d7ffaafa0b17d3abfe0c9c370f36db0d1ef75438ce07d"
 
-  url "https://github.com/PintaProject/Pinta/releases/download/#{version}/Pinta.app.zip",
+  url "https://github.com/PintaProject/Pinta/releases/download/#{version}/Pinta.dmg",
       verified: "github.com/PintaProject/Pinta/"
   name "Pinta"
   desc "Simple Gtk# Paint Program"
   homepage "https://www.pinta-project.com/"
 
-  depends_on cask: "mono-mdk"
-
   app "Pinta.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.ximian.pinta.plist",
+    "~/Library/Saved Application State/com.ximian.pinta.savedState",
+  ]
 end
