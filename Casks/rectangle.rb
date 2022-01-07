a37ebbe255908b234d02dee3aeeee0a5cd0180ce
Daniel Schultz <slifty@gmail.com>
2022-01-06 23:56:51 -0500

    Update rectangle.rb

--- Casks/rectangle.rb
+++ Casks/rectangle.rb
@@ -1,5 +1,5 @@
 cask "rectangle" do
-  version "0.50"
+  version "0.50,55"
   sha256 "b1323721795da2401736a60b300472d0d6c6727f5072992d27e794315556467c"
 
   url "https://github.com/rxhanson/Rectangle/releases/download/v#{version.csv.first}/Rectangle#{version.csv.first}.dmg",
