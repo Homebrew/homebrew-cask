9b0c0a8b28bb86538cbd8566561056d17e9c5d14
Ben Rockwood <benr@cuddletech.com>
2022-01-07 01:08:28 -0800

    Mondoo Client v5.20.0

---
+++ Casks/mondoo-cli.rb
@@ -0,0 +1,17 @@
+cask "mondoo-cli" do
+  version "5.20.0"
+  sha256 "52932b4c2d5e8355e745edb96de20811e0c42a9c61338d17b4cab3da95fc0399"
+
+  url "https://releases.mondoo.io/mondoo/#{version}/mondoo_#{version}_darwin_universal.pkg"
+  name "Mondoo"
+  desc "Mondoo Client CLI for the Mondoo Policy as Code Platform"
+  homepage "https://mondoo.io/"
+
+  pkg "mondoo_#{version}_darwin_universal.pkg"
+  
+  uninstall script: {
+                      executable: "/Library/Mondoo/#{version}/uninstall.sh",
+                      sudo:       true,
+                    }
+end
+
