cd23dcc930165a746bcb3151312c1e112f1466bc
Bevan Kay <email@bevankay.me>
2022-01-08 08:01:19 +1100

    Update kdeconnect.rb

--- Casks/kdeconnect.rb
+++ Casks/kdeconnect.rb
@@ -1,16 +1,15 @@
 cask "kdeconnect" do
   version "21.12.1-816"
-  sha256 :no_check
+  sha256 "8775967bf73470c3064724235608e982208bdee8f8d719ecd03bb9313205cf5c"
 
-  url "https://binary-factory.kde.org/job/kdeconnect-kde_Release_macos/lastSuccessfulBuild/artifact/kdeconnect-kde-21.12.1-816-macos-64-clang.dmg"
+  url "https://binary-factory.kde.org/job/kdeconnect-kde_Release_macos/lastSuccessfulBuild/artifact/kdeconnect-kde-#{version}-macos-64-clang.dmg"
   name "KDE Connect"
   desc "Enabling communication between all your devices"
   homepage "https://kdeconnect.kde.org/"
 
   livecheck do
     url "https://binary-factory.kde.org/job/kdeconnect-kde_Release_macos/lastSuccessfulBuild/artifact/"
-    strategy :page_match
-    regex(/href=.*?kdeconnect-kde-([\d.]+)-macos-64-clang.dmg/i)
+    regex(/href=.*?kdeconnect-kde[._-]v?(\d+(?:[.-]\d+)+)-macos-64-clang.dmg/i)
   end
 
   app "kdeconnect-indicator.app", target: "KDE Connect.app"
@@ -19,10 +18,10 @@ cask "kdeconnect" do
 
   zap trash: [
     "~/Library/Application Support/kpeoplevcard",
-    "~/Library/Preferences/org.kde.kdeconnect.plist",
     "~/Library/Preferences/kdeconnect",
     "~/Library/Preferences/kdeconnect_runcommand",
     "~/Library/Preferences/kdeconnect_sendnotifications",
     "~/Library/Preferences/kdeconnect_share",
+    "~/Library/Preferences/org.kde.kdeconnect.plist",
   ]
 end
