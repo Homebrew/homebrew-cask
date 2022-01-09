4d5bc20e52761889647cb7c21b19e3f7a6749312
Clément L <porkepix@gmail.com>
2022-01-09 01:28:02 +0100

    Update hackintool from 3.8.3 to 3.8.4 (#117105)

--- Casks/hackintool.rb
+++ Casks/hackintool.rb
@@ -1,6 +1,6 @@
 cask "hackintool" do
-  version "3.8.3"
-  sha256 "bdb87ae295ed7219e65ba033f7cbf35017fc8059a4d1a4e10020673eb4f50ca7"
+  version "3.8.4"
+  sha256 "b17b515c7eb86137878a03840fe389e8e15993712be4f5a2beb5c0a277020c94"
 
   url "https://github.com/headkaze/Hackintool/releases/download/#{version}/Hackintool.zip"
   name "Hackintool"
