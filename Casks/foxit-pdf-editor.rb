60d184e995b215e8aff3155c90e890eac35a3936
Tomáš Holý <vantomas@vantomas.net>
2022-01-09 01:46:20 +0100

    Add Foxit PDF Editor.app v11.1.0 (#116546)
    * Add Foxit PDF Editor.app v11.1.0
    
    * Update foxit-pdf-editor.rb
    
    Co-authored-by: Tomáš Holý <vantomas@gmail.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/foxit-pdf-editor.rb
@@ -0,0 +1,36 @@
+cask "foxit-pdf-editor" do
+  version "11.1.0"
+  sha256 "aa9a0fbc605e294507e7b2bfa5f653ae302d1df9104b3437cba842ac80c1d8ee"
+
+  url "https://cdn01.foxitsoftware.com/pub/foxit/phantomPDF/desktop/mac/#{version.major}.x/#{version.major_minor}/ML/FoxitPDFEditor#{version.no_dots}.L10N.Setup.pkg",
+      verified: "cdn01.foxitsoftware.com/pub/foxit/phantomPDF/desktop/mac/"
+  name "Foxit PDF Editor"
+  desc "PDF Editor"
+  homepage "https://www.foxit.com/pdf-editor/"
+
+  livecheck do
+    url "https://www.foxit.com/downloads/latest.html?product=Foxit-PDF-Editor-Mac&platform=Mac-OS-X"
+    strategy :header_match do |headers|
+      match = headers["location"].match(%r{/(\d+(?:\.\d+)*)/ML/FoxitPDFEditor(\d+)\.L10N\.Setup\.pkg}i)
+      next if match.blank?
+
+      "#{match[1]}.#{match[2].delete_prefix(match[1].delete("."))}"
+    end
+  end
+
+  pkg "FoxitPDFEditor#{version.no_dots}.L10N.Setup.pkg"
+
+  uninstall pkgutil:   "com.foxit.pkg.pdfeditor",
+            delete:    [
+              "/Applications/Foxit PDF Editor.app",
+              "/Library/Application Support/Foxit Software/FoxitService/FoxitPDFEditorUpdateService.app",
+              "/Library/LaunchDaemons/com.foxit.PDFEditorUpdateService.plist",
+            ],
+            launchctl: "com.foxit.PDFEditorUpdateService"
+
+  zap trash: [
+    "~/Library/Application Support/Foxit Software/Foxit PDF Editor",
+    "~/Library/Caches/com.foxit-software.Foxit PDF Editor",
+    "~/Library/Preferences/com.foxit-software.Foxit PDF Editor.plist",
+  ]
+end
