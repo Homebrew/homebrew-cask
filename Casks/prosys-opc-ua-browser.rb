49da5ee2c5f4ffd9c1811d03774022021092777f
neutric <5984479+neutric@users.noreply.github.com>
2022-01-10 00:36:10 +0100

    prosys-opc-ua-browser: update livecheck

--- Casks/prosys-opc-ua-browser.rb
+++ Casks/prosys-opc-ua-browser.rb
@@ -1,16 +1,20 @@
 cask "prosys-opc-ua-browser" do
-  version "4.1.0-354"
+  version "4.1.0,354"
   sha256 "e289747b31d2603d579e0495cd559c60a1c558f054461bbfb8fa3aa373a0261a"
 
-  url "https://www.prosysopc.com/opcua/apps/UaBrowser/dist/#{version}/prosys-opc-ua-browser-macos-#{version}.dmg"
+  url "https://www.prosysopc.com/opcua/apps/UaBrowser/dist/#{version.csv.first}-#{version.csv.second}/prosys-opc-ua-browser-macos-#{version.csv.first}-#{version.csv.second}.dmg"
   name "Prosys OPC UA Browser"
   desc "Browse and visualize data from OPC UA servers"
   homepage "https://www.prosysopc.com/products/opc-ua-browser/"
 
   livecheck do
     url "https://downloads.prosysopc.com/opc-ua-browser-downloads.php"
-    strategy :page_match
-    regex(%r{href=.*?/prosys-opc-ua-browser-macos-(\d+(?:\.\d+)*-\d+)\.dmg}i)
+    strategy :page_match do |page|
+      match = page.match(/href=.*?prosys-opc-ua-browser-macos[._-]v?(\d+(?:\.\d+)+)-(\d+)\.dmg/i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
   end
 
   installer script: {
