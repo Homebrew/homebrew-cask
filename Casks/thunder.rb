bc7ce06f14656027e85a87d509befc12cbc8bb15
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-10 16:10:09 +0800

    Update thunder.rb

--- Casks/thunder.rb
+++ Casks/thunder.rb
@@ -3,15 +3,15 @@ cask "thunder" do
   sha256 "0f9cdcdfd8159f300512e7e4ff94d6aab28d9f842f1db02c973356f9e60b09d8"
 
   url "https://down.sandai.net/mac/thunder_#{version}.dmg",
-      verified: "down.sandai.net/"
+      verified: "down.sandai.net/mac/"
   name "Thunder"
   name "迅雷"
   desc "VPN and WiFi proxy"
   homepage "https://www.xunlei.com/"
 
   livecheck do
-    url "https://static-xl9-ssl.xunlei.com/json/mac_preferences.json"
-    regex(/appcast[._-]v?(\d+(?:\.\d+)+)\.xml/i)
+    url "https://dl.xunlei.com"
+    regex(/thunder[._-](\d+(?:\.\d+)+)\.dmg/i)
   end
 
   auto_updates true
