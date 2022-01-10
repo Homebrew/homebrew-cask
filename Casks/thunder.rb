e74c6da44dbcb36815aff2b2a43727509d981516
SvenFE <shensven2016@gmail.com>
2022-01-10 16:16:29 +0800

    Update thunder from 4.1.3.65141 to 4.2.0.65203 (#117170)
    * Update thunder from 4.1.3.65141 to 4.2.0.65203
    
    * Update thunder.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/thunder.rb
+++ Casks/thunder.rb
@@ -1,17 +1,17 @@
 cask "thunder" do
-  version "4.1.3.65141"
-  sha256 "8350126c140213b80677812124209060b7ce364ce6983e561d5f6e7201495aed"
+  version "4.2.0.65203"
+  sha256 "0f9cdcdfd8159f300512e7e4ff94d6aab28d9f842f1db02c973356f9e60b09d8"
 
   url "https://down.sandai.net/mac/thunder_#{version}.dmg",
-      verified: "down.sandai.net/"
+      verified: "down.sandai.net/mac/"
   name "Thunder"
   name "迅雷"
   desc "VPN and WiFi proxy"
   homepage "https://www.xunlei.com/"
 
   livecheck do
-    url "https://static-xl9-ssl.xunlei.com/json/mac_preferences.json"
-    regex(/appcast[._-]v?(\d+(?:\.\d+)+)\.xml/i)
+    url "https://dl.xunlei.com"
+    regex(/thunder[._-](\d+(?:\.\d+)+)\.dmg/i)
   end
 
   auto_updates true
