a6e5fd165901e53de702c3fc891ee8f50e07079e
viking1304 <viking1304@gmail.com>
2022-01-10 12:41:59 +0100

    Add Jump Desktop Connect v4.1.3

---
+++ jump-desktop-connect.rb
@@ -0,0 +1,13 @@
+cask "jump-desktop-connect" do
+  version "4.1.3"
+  sha256 :no_check
+
+  url "https://mirror.jumpdesktop.com/downloads/connect/JumpDesktopConnect.dmg"
+  name "Jump Desktop Connect"
+  desc "High Performance Desktop Sharing using a fraction of bandwidth of RDP/VNC"
+  homepage "https://jumpdesktop.com/"
+
+  pkg ".jdc.sparkle_guided.pkg"
+
+  uninstall pkgutil: "com.p5sys.jump.connect"
+end
