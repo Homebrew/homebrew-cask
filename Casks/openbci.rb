00217ea845bedb73f775a07aeaa54b1f948ea250
Hybras <24651269+hybras@users.noreply.github.com>
2022-01-09 23:09:42 -0500

    Add OpenBci v5.0.9 (#117101)
    * Add OpenBci v5.0.8
    
    * Update openbci.rb
    
    * Add zap stanza for OpenBCI
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/openbci.rb
@@ -0,0 +1,24 @@
+cask "openbci" do
+  version "5.0.9,2021-11-06_00-16-07"
+  sha256 "216fc1d47ebb5b158d7bbe7f71caf78f754d4a41c8cc9b5914bd2b01afd0cfcc"
+
+  url "https://github.com/OpenBCI/OpenBCI_GUI/releases/download/v#{version.csv.first}/openbcigui_v#{version.csv.first}_#{version.csv.second}_macosx.dmg",
+      verified: "github.com/OpenBCI/OpenBCI_GUI/"
+  name "OpenBCI"
+  desc "Connect to OpenBCI hardware, visualize and stream physiological data"
+  homepage "https://openbci.com/"
+
+  livecheck do
+    url "https://github.com/OpenBCI/OpenBCI_GUI/releases/latest"
+    strategy :page_match do |page|
+      match = page.match(/openbcigui[._-]v?(\d+(?:\.\d+)+)[._-](.+)[._-]macosx\.dmg/i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
+  end
+
+  app "OpenBCI_GUI.app"
+
+  zap trash: "~/Documents/OpenBCI_GUI"
+end
