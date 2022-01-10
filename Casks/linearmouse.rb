bb9a07db0d77b17d80b96c9cb18ba7cff46597b7
Jiahao Lu <lujjjh@gmail.com>
2022-01-11 05:29:50 +0800

    Update linearmouse from 0.3.3 to 0.3.4 (#117184)

--- Casks/linearmouse.rb
+++ Casks/linearmouse.rb
@@ -1,9 +1,9 @@
 cask "linearmouse" do
-  version "0.3.3"
-  sha256 "769dc71bd30956f3af31690c3b2d4ee8334e3c319f86fd63b3f84ad6fc35ecfe"
+  version "0.3.4"
+  sha256 "2a9a94e48d2e2c5bfa683dd0abf1795b2277e606e2739e39c699a9cd528de87b"
 
-  url "https://github.com/lujjjh/LinearMouse/releases/download/v#{version}/LinearMouse.dmg",
-      verified: "github.com/lujjjh/LinearMouse/"
+  url "https://github.com/linearmouse/linearmouse/releases/download/v#{version}/LinearMouse.dmg",
+      verified: "github.com/linearmouse/linearmouse/"
   name "LinearMouse"
   desc "Customize mouse behavior"
   homepage "https://linearmouse.org/"
@@ -11,6 +11,10 @@ cask "linearmouse" do
   depends_on macos: ">= :catalina"
 
   app "LinearMouse.app"
+
+  uninstall quit: [
+    "com.lujjjh.LinearMouse",
+  ]
 
   zap trash: "~/Library/Preferences/com.lujjjh.LinearMouse.plist"
 end
