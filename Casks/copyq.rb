06c7605c57caeeff8a7d764c449ea6f7fe65df33
neutric <5984479+neutric@users.noreply.github.com>
2022-01-11 00:12:37 +0100

    copyq: add legacy version (#117207)

--- Casks/copyq.rb
+++ Casks/copyq.rb
@@ -1,6 +1,15 @@
 cask "copyq" do
-  version "6.0.1"
-  sha256 "b1fa44d9fc3db1c9f270e031e891201d0bbac36faffc3be6c994a7515328cec6"
+  if MacOS.version <= :catalina
+    version "5.0.0"
+    sha256 "7201ff51d1258c8eae03580262a96bbee7d65c6e2133b0d5d6f10f95f031edd4"
+
+    livecheck do
+      skip "Legacy version for Catalina and earlier"
+    end
+  else
+    version "6.0.1"
+    sha256 "b1fa44d9fc3db1c9f270e031e891201d0bbac36faffc3be6c994a7515328cec6"
+  end
 
   url "https://github.com/hluk/CopyQ/releases/download/v#{version}/CopyQ.dmg.zip",
       verified: "github.com/hluk/CopyQ/"
