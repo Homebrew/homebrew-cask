2e779bb96382824e7a4c9171d197fe10d547261c
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-12 07:52:54 +0800

    Update screens-connect.rb

--- Casks/screens-connect.rb
+++ Casks/screens-connect.rb
@@ -2,7 +2,7 @@ cask "screens-connect" do
   version "4.11.1,21658"
   sha256 "a61c1f5be03406b02309d78adee51963f268564548a379bbfc12d560a32b7362"
 
-  url "https://updates.edovia.com/com.edovia.screens.connect.mac/ScreensConnect_#{version.before_comma}b#{version.after_comma}.zip"
+  url "https://updates.edovia.com/com.edovia.screens.connect.mac/ScreensConnect_#{version.csv.first}b#{version.csv.second}.zip"
   name "Screens Connect"
   desc "Remote desktop software"
   homepage "https://edovia.com/en/screens-connect/"
