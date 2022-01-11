4aefe4ad713e7c6c13bff57d59de9d14b76b23ef
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-11 10:17:00 +0800

    Update doughnut.rb

--- Casks/doughnut.rb
+++ Casks/doughnut.rb
@@ -1,16 +1,11 @@
 cask "doughnut" do
-  version "1.1.1,58"
+  version "1.1.1"
   sha256 "e50a64f6325bc88378f04848213ff745360d6a84a7533e7ad93b5abe53015ba9"
 
-  url "https://github.com/dyerc/Doughnut/releases/download/v#{version.csv.first}/Doughnut-#{version.csv.first}.dmg"
+  url "https://github.com/dyerc/Doughnut/releases/download/v#{version}/Doughnut-#{version}.dmg"
   name "Doughnut"
-  desc "Podcast client (podcatcher)"
-  homepage "https://github.com/dyerc/Doughnut"
-
-  livecheck do
-    url "https://raw.githubusercontent.com/dyerc/Doughnut/master/appcast.xml"
-    strategy :sparkle
-  end
+  desc "Podcast client"
+  homepage "https://github.com/dyerc/Doughnut/"
 
   app "Doughnut.app"
 
