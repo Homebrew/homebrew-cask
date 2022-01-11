d49ae71f207d57706a2624442b257be707314a0c
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-11 08:56:48 +0800

    Update athens.rb

--- Casks/athens.rb
+++ Casks/athens.rb
@@ -1,10 +1,17 @@
 cask "athens" do
-  version "1.0.0"
-  sha256 "322744478e3277216a129dd162e04ae24587d71eedafa6681b30a976b2521c9b"
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  url "https://github.com/athensresearch/athens/releases/download/v#{version}/Athens-#{version}.dmg",
+  version "1.0.0"
+
+  if Hardware::CPU.intel?
+    sha256 "322744478e3277216a129dd162e04ae24587d71eedafa6681b30a976b2521c9b"
+  else
+    sha256 "c5bd8759f3f6f10a91ed950acc25d51cc9bc5e70515515e36a1e3fec2579e375"
+  end
+
+  url "https://github.com/athensresearch/athens/releases/download/v#{version}/Athens-#{version}#{arch}.dmg",
       verified: "github.com/athensresearch/athens/"
-  name "athens"
+  name "Athens"
   desc "Self-hosted knowledge graph"
   homepage "https://www.athensresearch.org/"
 
