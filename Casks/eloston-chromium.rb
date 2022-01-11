a66fce0cee1d413d8cdd51c7634d3f859d884290
dbrookman <53625739+dbrookman@users.noreply.github.com>
2022-01-10 21:58:08 -0500

    Add epoch time to arm64 eloston-chromium version (#117201)
    * Add epoch time to arm64 eloston-chromium version
    
    Accounts for the current arm64 version being suffixed with the Epoch
    time.
    
    * Update eloston-chromium.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/eloston-chromium.rb
+++ Casks/eloston-chromium.rb
@@ -2,13 +2,27 @@ cask "eloston-chromium" do
   if Hardware::CPU.intel?
     version "97.0.4692.71-1.2_x86-64"
     sha256 "324d339576b17029a692d20c80dbee4e81b72b92183b1196532dc4d72d7d51a5"
+
+    url "https://github.com/kramred/ungoogled-chromium-macos/releases/download/#{version}/ungoogled-chromium_#{version}-macos.dmg",
+        verified: "github.com/kramred/ungoogled-chromium-macos/"
   else
-    version "96.0.4664.110-1.1_arm64"
+    version "97.0.4692.71-1.1_arm64,1641722599"
     sha256 "875065219a9f3cbd88dc290b1a98c4780dc84228939fc37bfdaba675ead99b3c"
+
+    url "https://github.com/kramred/ungoogled-chromium-macos/releases/download/#{version.csv.first}__#{version.csv.second}/ungoogled-chromium_#{version.csv.first}-macos.dmg",
+        verified: "github.com/kramred/ungoogled-chromium-macos/"
+
+    livecheck do
+      url "https://github.com/kramred/ungoogled-chromium-macos/releases/"
+      strategy :page_match do |page|
+        match = page.match(%r{releases/download/(.+)[._-][._-](\d+)/ungoogled[._-]chromium[._-](.+)[._-]macos\.dmg}i)
+        next if match.blank?
+
+        "#{match[1]},#{match[2]}"
+      end
+    end
   end
 
-  url "https://github.com/kramred/ungoogled-chromium-macos/releases/download/#{version}/ungoogled-chromium_#{version}-macos.dmg",
-      verified: "github.com/kramred/ungoogled-chromium-macos/"
   name "Ungoogled Chromium"
   desc "Google Chromium, sans integration with Google"
   homepage "https://ungoogled-software.github.io/ungoogled-chromium-binaries/"
