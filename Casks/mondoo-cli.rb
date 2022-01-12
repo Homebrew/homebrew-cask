5ca70efa152cf65df67318d7556b80ee0f5e44f3
Ben Rockwood <benr@cuddletech.com>
2022-01-11 22:31:54 -0800

    Update to 5.21.0 for LiveCheck

--- Casks/mondoo-cli.rb
+++ Casks/mondoo-cli.rb
@@ -1,6 +1,6 @@
 cask "mondoo-cli" do
-  version "5.20.0"
-  sha256 "52932b4c2d5e8355e745edb96de20811e0c42a9c61338d17b4cab3da95fc0399"
+  version "5.21.0"
+  sha256 "a90b5e7c5e5efcded69b6ba3a00ffd6950c2dd181d9df92d25a6cd1f86a65afc"
 
   url "https://releases.mondoo.io/mondoo/#{version}/mondoo_#{version}_darwin_universal.pkg"
   name "Mondoo"
