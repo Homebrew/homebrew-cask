b89d7031f5374cb8ecd7784ba8428254cded23c8
neutric <5984479+neutric@users.noreply.github.com>
2022-01-12 01:36:37 +0100

    Update navicat-premium-essentials from 15.0.32 to 15.0.33 (#117235)

--- Casks/navicat-premium-essentials.rb
+++ Casks/navicat-premium-essentials.rb
@@ -1,5 +1,5 @@
 cask "navicat-premium-essentials" do
-  version "15.0.32"
+  version "15.0.33"
   sha256 :no_check
 
   url "http://download.navicat.com/download/navicatess#{version.major_minor.no_dots}_premium_en.dmg"
