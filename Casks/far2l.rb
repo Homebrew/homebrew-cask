7463b59064f3dc84b0184119761e29834c8673b6
Yurii Kolesnykov <root@yurikoles.com>
2022-01-12 22:42:12 +0200

    Update far2l from 2.3.2 to 2.4.0
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/far2l.rb
+++ Casks/far2l.rb
@@ -1,34 +1,24 @@
 cask "far2l" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "2.3.2,_2.3.2"
-
-  if MacOS.version <= :mojave
-    url "https://github.com/elfmz/far2l/releases/download/v#{version.csv.second}/far2l-#{version.csv.first}-alpha-MacOS-10.11.dmg"
-    sha256 "eabf42a5cee8332d9f12ed0818453c729219db13368ec3834377ab818ff5ee1c"
-
-    livecheck do
-      url "https://github.com/elfmz/far2l/releases"
-      regex(%r{/v([^/]+)/far2l-(\d+(?:\.\d+)+)-alpha-MacOS-10\.11\.dmg}i)
-      strategy :page_match do |page, regex|
-        page.scan(regex).map { |match| "#{match[1]},#{match[0]}" }
-      end
-    end
-  else
-    url "https://github.com/elfmz/far2l/releases/download/v#{version.csv.second}/far2l-#{version.csv.first}-alpha-MacOS-10.15.dmg"
-    sha256 "1058a7f7d0b4da0620faae424158bd5398ee1bb6647ce22e9e6e4a6dc1a6408b"
-
-    livecheck do
-      url "https://github.com/elfmz/far2l/releases"
-      regex(%r{/v([^/]+)/far2l-(\d+(?:\.\d+)+)-alpha-MacOS-10\.15\.dmg}i)
-      strategy :page_match do |page, regex|
-        page.scan(regex).map { |match| "#{match[1]},#{match[0]}" }
-      end
-    end
-  end
+  version "2.4.0"
 
   name "far2l"
+  if MacOS.version <= :mojave
+    url "https://github.com/elfmz/far2l/releases/download/v_#{version}/far2l-#{version}-beta-MacOS-10.11.dmg"
+    sha256 "37e7bf7cf3b21221a09e7ffd0eec03da543721d66e04142705dbd7248e54db46"
+  else
+    url "https://github.com/elfmz/far2l/releases/download/v_#{version}/far2l-#{version}-beta-MacOS-10.15.dmg"
+    sha256 "92192c8aa2866cbb445bcb072d10d660f8145caeefb4aa35b8d0418ac7c31b8e"
+  end
+
   desc "Unix fork of FAR Manager v2"
   homepage "https://github.com/elfmz/far2l"
+
+  livecheck do
+    url "https://github.com/elfmz/far2l/releases"
+    regex(%r{/v_([^/]+)/far2l-(\d+(?:\.\d+)+)-beta-MacOS-10\.11\.dmg}i)
+    strategy :page_match
+  end
 
   depends_on macos: ">= :el_capitan"
 
