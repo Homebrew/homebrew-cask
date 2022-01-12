14f0c7262248f606151219187960567b0e392fc7
Bevan Kay <email@bevankay.me>
2022-01-13 10:21:41 +1100

    adjust livecheck

--- Casks/tencent-lemon.rb
+++ Casks/tencent-lemon.rb
@@ -1,16 +1,15 @@
 cask "tencent-lemon" do
-  version "5.0.4"
+  version "5.0.4_2"
   sha256 "e88a19b581fbf64ca80a21c390c697e635214cb451767f8a9e891dfc421f5beb"
 
-  url "https://webcdn.m.qq.com/lemon/version/LemonV#{version}_2.dmg"
+  url "https://webcdn.m.qq.com/lemon/version/LemonV#{version}.dmg"
   name "Tencent Lemon Cleaner"
   desc "Cleanup and system status tool"
   homepage "https://lemon.qq.com/"
 
   livecheck do
     url "https://lemon.guanjia.qq.com/latest/package"
-    strategy :page_match
-    regex(/"latest":{"version":"(\d+(?:\.\d+)+)"/i)
+    regex(/"downloadUrl":"https.*LemonV(\d+(?:[._]\d+)+)\.dmg/i)
   end
 
   app "Tencent Lemon.app"
