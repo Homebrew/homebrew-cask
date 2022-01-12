44372a7c59e01db3004ec39a1e1db84f44511e31
doeber <47430879+doeber@users.noreply.github.com>
2022-01-13 00:33:26 +0100

    Update signal from 5.27.1 to 5.28.0 (#117292)

--- Casks/signal.rb
+++ Casks/signal.rb
@@ -1,12 +1,12 @@
 cask "signal" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "5.27.1"
+  version "5.28.0"
 
   if Hardware::CPU.intel?
-    sha256 "83cdd68c0bcfbe0afb3ab794531f3122ad3732b58c9517ff8b83796b6c170723"
+    sha256 "48cc3c8fa1fe36726b5ad8b687047cc7c7f22beca6fc9a6272df13d138311a9f"
   else
-    sha256 "600121095295d7b7d54b38d0a984ca220e94ceb4ef902afded1cb1591d2bff71"
+    sha256 "ddb333d8829f6996d31b6f40fcd7d6396ba7ce83f4f2a219e1f4a8dcf8b49de7"
   end
 
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{arch}-#{version}.dmg"
