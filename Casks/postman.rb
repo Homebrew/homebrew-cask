a061edb0cabf80650fafcf82cc20ef578b2580b1
neutric <5984479+neutric@users.noreply.github.com>
2022-01-12 01:38:09 +0100

    Update postman from 9.8.2 to 9.8.3 (#117227)

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,12 +1,12 @@
 cask "postman" do
   arch = Hardware::CPU.intel? ? "osx64" : "osx_arm64"
 
-  version "9.8.2"
+  version "9.8.3"
 
   if Hardware::CPU.intel?
-    sha256 "aa9478e9c046893c5e04ec944191a7ab5627a71c133f03f10727e916951b747a"
+    sha256 "4ac6da3cb5dce5930a0cd29a1f42657a85234fb30ea27fb41f54c86312dc0cbc"
   else
-    sha256 "e74ca5fb5a6198751f241d1c06c052d83d3edd648ffe0994dc537340b785d9c2"
+    sha256 "1a5281f4ac88e15e4424f0387f2cf9d8ebc20c4194d84606f0c9119572a63407"
   end
 
   url "https://dl.pstmn.io/download/version/#{version}/#{arch}",
