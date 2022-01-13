3457a6858c0a5da5d61be1dfde92b49df8cb30d3
neutric <5984479+neutric@users.noreply.github.com>
2022-01-13 02:17:33 +0100

    Update recaf from 2.21.8 to 2.21.11 (#117301)

--- Casks/recaf.rb
+++ Casks/recaf.rb
@@ -1,6 +1,6 @@
 cask "recaf" do
-  version "2.21.8"
-  sha256 "9b3fa9f1bd1697db3ef5bcff93283d172b6c6edba84a35a3cfa214bc17a2bdd7"
+  version "2.21.11"
+  sha256 "d5b3b481fdd43e81686b236b986242393d53dd9e623223af160e4114e617a4ea"
 
   url "https://github.com/Col-E/Recaf/releases/download/#{version}/recaf-#{version}-J8-jar-with-dependencies.jar",
       verified: "github.com/Col-E/Recaf/"
