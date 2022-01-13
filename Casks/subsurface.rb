489231fe74e407075cbc9c3afe83e3fd79089a81
tpak <tpak@users.noreply.github.com>
2022-01-13 13:16:22 -0700

    Update subsurface from 5.0.4 to 5.0.5

--- Casks/subsurface.rb
+++ Casks/subsurface.rb
@@ -1,11 +1,11 @@
 cask "subsurface" do
-  version "5.0.4"
+  version "5.0.5"
 
   if MacOS.version <= :mojave
     sha256 "4a194ee98f97c0d15e8e99d0646e00ebfae67b34644d97fd3a77b05179042655"
     url "https://subsurface-divelog.org/downloads/Subsurface-#{version}-10.13+14.dmg"
   else
-    sha256 "610c34fea09bb6759ed63db570e13b9118ab6306cd13f2fb78288b7c07d455cd"
+    sha256 "b80919d25d6a998df20064032d7fc06661cbd2aedbf0a024a2357178897f7bf9"
     url "https://subsurface-divelog.org/downloads/Subsurface-#{version}-10.15+11.0.dmg"
   end
 
