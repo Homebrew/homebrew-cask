6da7a08571bb38e8227ee1aee985d435cb664246
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-13 10:28:34 +0800

    Update far2l.rb

--- Casks/far2l.rb
+++ Casks/far2l.rb
@@ -2,7 +2,6 @@ cask "far2l" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
   version "2.4.0"
 
-  name "far2l"
   if MacOS.version <= :mojave
     url "https://github.com/elfmz/far2l/releases/download/v_#{version}/far2l-#{version}-beta-MacOS-10.11.dmg"
     sha256 "37e7bf7cf3b21221a09e7ffd0eec03da543721d66e04142705dbd7248e54db46"
@@ -11,13 +10,14 @@ cask "far2l" do
     sha256 "92192c8aa2866cbb445bcb072d10d660f8145caeefb4aa35b8d0418ac7c31b8e"
   end
 
+  name "far2l"
   desc "Unix fork of FAR Manager v2"
   homepage "https://github.com/elfmz/far2l"
 
   livecheck do
     url "https://github.com/elfmz/far2l/releases"
-    regex(%r{/v_([^/]+)/far2l-(\d+(?:\.\d+)+)-beta-MacOS-10\.11\.dmg}i)
     strategy :page_match
+    regex(/far2l[._-](\d+(?:\.\d+)+)[._-](alpha|beta)[._-]MacOS[._-]10\.(11|15)\.dmg/i)
   end
 
   depends_on macos: ">= :el_capitan"
