b71d2f203853cc376dd8bb691685ac990b6259b0
Chris Tirpak <tpak@users.noreply.github.com>
2022-01-13 14:36:20 -0700

    add additional version checks
    simplify livecheck 
    check O version and get the correct image for older OS's

--- Casks/subsurface.rb
+++ Casks/subsurface.rb
@@ -4,6 +4,9 @@ cask "subsurface" do
   if MacOS.version <= :mojave
     sha256 "d33cc7bd277be1559e67e5bd08d512c0a901430dd2685df278e06fa17c21f3c3"
     url "https://subsurface-divelog.org/downloads/Subsurface-#{version}-10.13+14.dmg"
+  elsif MacOS.release <= :big_sur
+      sha256 "b80919d25d6a998df20064032d7fc06661cbd2aedbf0a024a2357178897f7bf9"
+      url "https://subsurface-divelog.org/downloads/Subsurface-#{version}-10.15+11.dmg"  
   else
     sha256 "b80919d25d6a998df20064032d7fc06661cbd2aedbf0a024a2357178897f7bf9"
     url "https://subsurface-divelog.org/downloads/Subsurface-#{version}.dmg"
@@ -16,7 +19,7 @@ cask "subsurface" do
   livecheck do
     url "https://subsurface-divelog.org/downloads/"
     strategy :page_match
-    regex(%r{href=.*?/Subsurface-(\d+(?:\.\d+)*)-10\.15\+11\.0\.dmg}i)
+    regex(%r{href=.*?/Subsurface-(\d+(?:\.\d+)*).dmg}i)
   end
 
   app "Subsurface.app"
