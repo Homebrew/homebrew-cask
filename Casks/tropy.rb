1c68d2563d1dd4a60a75a35ed1b064934672b311
Sylvester Keil <sylvester@keil.or.at>
2022-01-13 09:44:47 +0100

    Add Tropy arm64 builds

--- Casks/tropy.rb
+++ Casks/tropy.rb
@@ -1,9 +1,16 @@
 cask "tropy" do
   version "1.11.1"
-  sha256 "4351ebdf5284f6f4f3d5707f1ba19659cd12c291d70d031b81182f6a30d2819d"
 
-  url "https://github.com/tropy/tropy/releases/download/v#{version}/tropy-#{version}.dmg",
-      verified: "github.com/tropy/tropy/"
+  if Hardware::CPU.intel?
+    sha256 "4351ebdf5284f6f4f3d5707f1ba19659cd12c291d70d031b81182f6a30d2819d"
+    url "https://github.com/tropy/tropy/releases/download/v#{version}/tropy-#{version}.dmg",
+        verified: "github.com/tropy/tropy/"
+  else
+    sha256 "289b8e92f03d5917c72d56a2f5b021233e42f4ed50ab0d9c6464d98bb8a5514d"
+    url "https://github.com/tropy/tropy/releases/download/v#{version}/tropy-#{version}-arm64.dmg",
+        verified: "github.com/tropy/tropy/"
+  end
+
   name "Tropy"
   desc "Research photo management"
   homepage "https://tropy.org/"
