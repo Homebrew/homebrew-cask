bd1bda1cbee3f235e47408d8073a004f5f42390e
Sylvester Keil <sylvester@keil.or.at>
2022-01-14 01:58:17 +0100

    Add Tropy arm64 build (#117329)
    * Add Tropy arm64 builds
    
    * Update tropy.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/tropy.rb
+++ Casks/tropy.rb
@@ -1,8 +1,15 @@
 cask "tropy" do
-  version "1.11.1"
-  sha256 "4351ebdf5284f6f4f3d5707f1ba19659cd12c291d70d031b81182f6a30d2819d"
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  url "https://github.com/tropy/tropy/releases/download/v#{version}/tropy-#{version}.dmg",
+  version "1.11.1"
+
+  if Hardware::CPU.intel?
+    sha256 "4351ebdf5284f6f4f3d5707f1ba19659cd12c291d70d031b81182f6a30d2819d"
+  else
+    sha256 "289b8e92f03d5917c72d56a2f5b021233e42f4ed50ab0d9c6464d98bb8a5514d"
+  end
+
+  url "https://github.com/tropy/tropy/releases/download/v#{version}/tropy-#{version}#{arch}.dmg",
       verified: "github.com/tropy/tropy/"
   name "Tropy"
   desc "Research photo management"
@@ -17,9 +24,9 @@ cask "tropy" do
 
   zap trash: [
     "~/Library/Application Support/Tropy",
-    "~/Library/Caches/Tropy",
-    "~/Library/Caches/org.tropy.tropy",
     "~/Library/Caches/org.tropy.tropy.ShipIt",
+    "~/Library/Caches/org.tropy.tropy",
+    "~/Library/Caches/Tropy",
     "~/Library/Logs/Tropy",
     "~/Library/Preferences/org.tropy.tropy.plist",
     "~/Library/Saved Application State/org.tropy.tropy.savedState",
