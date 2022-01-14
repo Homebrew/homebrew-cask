bc75bf4b7fdde13b6d15dc90f7d0c135c79f367a
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-14 08:17:08 +0800

    Update subsurface.rb

--- Casks/subsurface.rb
+++ Casks/subsurface.rb
@@ -5,8 +5,8 @@ cask "subsurface" do
     sha256 "d33cc7bd277be1559e67e5bd08d512c0a901430dd2685df278e06fa17c21f3c3"
     url "https://subsurface-divelog.org/downloads/Subsurface-#{version}-10.13+14.dmg"
   elsif MacOS.version <= :big_sur
-      sha256 "b80919d25d6a998df20064032d7fc06661cbd2aedbf0a024a2357178897f7bf9"
-      url "https://subsurface-divelog.org/downloads/Subsurface-#{version}-10.15+11.0.dmg"  
+    sha256 "b80919d25d6a998df20064032d7fc06661cbd2aedbf0a024a2357178897f7bf9"
+    url "https://subsurface-divelog.org/downloads/Subsurface-#{version}-10.15+11.0.dmg"
   else
     sha256 "b80919d25d6a998df20064032d7fc06661cbd2aedbf0a024a2357178897f7bf9"
     url "https://subsurface-divelog.org/downloads/Subsurface-#{version}.dmg"
@@ -15,9 +15,10 @@ cask "subsurface" do
   name "Subsurface"
   desc "Open source divelog program"
   homepage "https://subsurface-divelog.org/"
-  
+
   livecheck do
     url "https://subsurface-divelog.org/downloads/"
+    regex(/href=.*?Subsurface[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
   app "Subsurface.app"
