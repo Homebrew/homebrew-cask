b96f068718194920a1f18a63d9c98a8521a581c4
Michael Johnston <mjohnston16@gmail.com>
2022-01-13 16:34:01 -0800

    Update cron from 1.107.0 to 1.108.0 (#117362)
    * Update cron from 1.107.0 to 1.108.0
    
    Also makes minor updates to name and description.
    
    * Update cron.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/cron.rb
+++ Casks/cron.rb
@@ -1,17 +1,17 @@
 cask "cron" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "1.107.0"
+  version "1.108.0"
 
   if Hardware::CPU.intel?
-    sha256 "3f2335578382db0b1201d1b98e07960d4667e32c9293ea795111b55cf6da6cfd"
+    sha256 "08475e7b860162599d280917e7ba8971e05e51f197652cb1e077b72775e746d6"
   else
-    sha256 "ac49a33af84549d1dbef88201182b94e9a5b0dc4326a1178e5fe806540598282"
+    sha256 "ab6aee49fe4f0a3cba80ce84ea4e3dbafa020bddc499e81521bd9e7b2b84fb27"
   end
 
   url "https://download.todesktop.com/210303leazlircz/Cron%20#{version}-#{arch}-mac.zip",
       verified: "download.todesktop.com/210303leazlircz/"
-  name "cron"
+  name "Cron Calendar"
   desc "Calendar for professionals and teams"
   homepage "https://cron.com/"
 
