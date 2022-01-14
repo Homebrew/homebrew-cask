92bcf490011d8ef89e8ada01531c89b871770376
neutric <5984479+neutric@users.noreply.github.com>
2022-01-14 14:12:54 +0100

    Update unity-webgl-support-for-editor from 2021.2.7f1 to 2021.2.8f1 (#117395)

--- Casks/unity-webgl-support-for-editor.rb
+++ Casks/unity-webgl-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask "unity-webgl-support-for-editor" do
-  version "2021.2.7f1,6bd9e232123f"
-  sha256 "1efef8f7ef96b80bb450d07ccb86b608a3ff77f870f8deb36d27868b080d29e6"
+  version "2021.2.8f1,d0e5f0a7b06a"
+  sha256 "26d48ee7dec7e7466ea89678adb1adb6273397d79b2fa37ee1c8b3164ed61674"
 
   url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorTargetInstaller/UnitySetup-WebGL-Support-for-Editor-#{version.csv.first}.pkg",
       verified: "download.unity3d.com/download_unity/"
