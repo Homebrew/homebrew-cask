7cdb7713abe246f6c4df7579b2a86d22b3514750
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-14 16:22:54 +0800

    Update yuque.rb

--- Casks/yuque.rb
+++ Casks/yuque.rb
@@ -1,18 +1,17 @@
 cask "yuque" do
+  arch = Hardware::CPU.intel? ? "" : "arm64/"
+
   version "1.0.3"
 
   if Hardware::CPU.intel?
     sha256 "b81398d9bc7b241deeb19e73ccea9c48229ac791617dc4cc8de518b553cdf978"
-
-    url "https://app.nlark.com/yuque-desktop/Yuque-#{version}.dmg",
         verified: "app.nlark.com/yuque-desktop/"
   else
     sha256 "58ca2a09b575ebc118bcb9c800a3c72b1bb5142ec17b1376f1bfc842ffbc5328"
-
-    url "https://app.nlark.com/yuque-desktop/arm64/Yuque-#{version}.dmg",
-        verified: "app.nlark.com/yuque-desktop/arm64/"
   end
 
+  url "https://app.nlark.com/yuque-desktop/#{arch}Yuque-#{version}.dmg",
+      verified: "app.nlark.com/yuque-desktop/"
   name "Yuque"
   name "语雀"
   desc "Cloud knowledge base"
@@ -22,7 +21,7 @@ cask "yuque" do
   #   https://github.com/Homebrew/homebrew-cask/pull/111472
   livecheck do
     url "https://www.yuque.com/install/desktop"
-    regex(/desktopDownloadVersion%22%3A%22(\d+(?:\.\d+)*)/i)
+    regex(/desktopDownloadVersion%22%3A%22(\d+(?:\.\d+)+)/i)
   end
 
   app "语雀.app"
