52b7a96fdd359bc7aaa708b79c8df439d605db68
Aaron Rumpler <80610725+Aaron-Rumpler@users.noreply.github.com>
2022-01-15 21:35:50 +1300

    Added scroll, renamed hookshot to rectangle-pro, and updated middle to 1.6.6
    Also includes miscellaneous improvements for rectangle, rectangle-pro, multitouch, middle, hightop, and hyperkey

---
+++ Casks/rectangle-pro.rb
@@ -0,0 +1,31 @@
+cask "rectangle-pro" do
+  version "2.0,100"
+  sha256 "b151ad1b63168f6f98b8c3792329eb6abb2131ab7d3c6e794cb9d61960f3e7c7"
+
+  url "https://rectangleapp.com/pro/downloads/Rectangle%20Pro%20#{version.csv.first}.dmg"
+  name "Rectangle Pro"
+  desc "Window snapping tool"
+  homepage "https://rectangleapp.com/pro/"
+
+  livecheck do
+    url "https://rectangleapp.com/pro/downloads/updates.xml"
+    strategy :sparkle
+  end
+
+  auto_updates true
+  depends_on macos: ">= :sierra"
+
+  app "Rectangle Pro.app"
+
+  uninstall quit: "com.knollsoft.Hookshot"
+
+  zap trash: [
+    "~/Library/Application Support/Rectangle Pro",
+    "~/Library/Caches/com.knollsoft.Hookshot",
+    "~/Library/Cookies/com.knollsoft.Hookshot",
+    "~/Library/Cookies/com.knollsoft.Hookshot.*",
+    "~/Library/HTTPStorages/com.knollsoft.Hookshot",
+    "~/Library/HTTPStorages/com.knollsoft.Hookshot.*",
+    "~/Library/Preferences/com.knollsoft.Hookshot.plist",
+  ]
+end
