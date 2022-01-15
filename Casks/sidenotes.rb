25233af18ea1751afb26ce5277609fcac92d5a61
neutric <5984479+neutric@users.noreply.github.com>
2022-01-15 13:39:37 +0100

    Update sidenotes from 1.4.4 to 1.4.5 (#117431)
    * Update sidenotes from 1.4.4 to 1.4.5
    
    * sidenotes: raise minimum OS version

--- Casks/sidenotes.rb
+++ Casks/sidenotes.rb
@@ -1,6 +1,6 @@
 cask "sidenotes" do
-  version "1.4.4,173"
-  sha256 "974d46beed9aac39ce8650db885289bf12deb2fa8078fa1457600873136a10ef"
+  version "1.4.5,179"
+  sha256 "5b16a7125235b218085044cecfdf72a29adcd6923274af493feb8ea02e547865"
 
   url "https://www.apptorium.com/public/products/sidenotes/releases/SideNotes-#{version.csv.first}.zip"
   name "SideNotes"
@@ -12,7 +12,7 @@ cask "sidenotes" do
     strategy :sparkle
   end
 
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= :catalina"
 
   app "SideNotes.app"
 end
