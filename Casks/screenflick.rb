1c3d340f243d35f45631f1dc21024cf80066e328
neutric <5984479+neutric@users.noreply.github.com>
2022-01-15 06:42:42 +0100

    Update screenflick from 3.0.7 to 3.0.8 (#117425)
    * Update screenflick from 3.0.7 to 3.0.8
    
    * screenflick: tweak livecheck regex

--- Casks/screenflick.rb
+++ Casks/screenflick.rb
@@ -1,6 +1,6 @@
 cask "screenflick" do
-  version "3.0.7"
-  sha256 "9f76efcdcc81a6e99e1fe6b56bab3fb46f48b326b09cd22aa434cbb0c91abcfa"
+  version "3.0.8"
+  sha256 "5676d12877f11a9186600ee0179de842af810a77aaffc784eb1bedd465d91183"
 
   url "https://store.araelium.com/screenflick/downloads/versions/Screenflick#{version}.zip"
   name "Screenflick"
@@ -9,7 +9,7 @@ cask "screenflick" do
 
   livecheck do
     url "https://arweb-assets.s3.amazonaws.com/downloads/screenflick/updates.json"
-    regex(%r{/Screenflick(\d+(?:\.\d+)+)\.zip}i)
+    regex(/Screenflick(\d+(?:\.\d+)+)\.zip/i)
   end
 
   depends_on macos: ">= :mojave"
