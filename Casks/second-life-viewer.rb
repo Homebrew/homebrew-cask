466ef9a43b9c3409c9e6327a24754d4d32043266
neutric <5984479+neutric@users.noreply.github.com>
2022-01-15 13:38:50 +0100

    Update second-life-viewer from 6.5.0.565607 to 6.5.2.567427 (#117429)

--- Casks/second-life-viewer.rb
+++ Casks/second-life-viewer.rb
@@ -1,6 +1,6 @@
 cask "second-life-viewer" do
-  version "6.5.0.565607"
-  sha256 "11f83fef6ffc15bd404d1306c41b7b8a9a739259afbd2b4a0463dd0f4390a469"
+  version "6.5.2.567427"
+  sha256 "728c07a96ffdfc4027f9fd1ac40dc4915c1ae09375157bcb51bbbbb925b55f3d"
 
   url "http://download.cloud.secondlife.com/Viewer_#{version.major}/Second_Life_#{version.dots_to_underscores}_x86_64.dmg"
   name "Linden Lab Second Life Viewer"
