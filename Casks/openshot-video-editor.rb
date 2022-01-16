1d667b8719885bd64c95a09fd07bb7e90b61f46c
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-16 16:04:12 +0800

    Update openshot-video-editor.rb (#117496)

--- Casks/openshot-video-editor.rb
+++ Casks/openshot-video-editor.rb
@@ -9,4 +9,8 @@ cask "openshot-video-editor" do
   homepage "https://openshot.org/"
 
   app "OpenShot Video Editor.app"
+
+  postflight do
+    set_permissions "#{appdir}/OpenShot Video Editor.app/Contents", "a+rx"
+  end
 end
