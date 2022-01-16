93d878a96814f8922a6d03a779bd5d86bbc33cc0
Aleksandr Fedotov <mezonix@gmail.com>
2022-01-16 13:31:47 +0300

    Update adguard-vpn.rb

--- Casks/adguard-vpn.rb
+++ Casks/adguard-vpn.rb
@@ -15,6 +15,7 @@ cask "adguard-vpn" do
   end
 
   auto_updates true
+  conflicts_with cask: "homebrew/cask-versions/adguard-vpn-nightly"
   depends_on macos: ">= :sierra"
 
   pkg "AdGuard VPN.pkg"
@@ -26,10 +27,7 @@ cask "adguard-vpn" do
               "/Library/Application Support/AdGuard Software/com.adguard.mac.vpn",
               "/Library/Application Support/com.adguard.mac.vpn",
             ],
-            rmdir:     [
-              "/Library/Application Support/AdGuard Software",
-              "/Library/Application Support/AdGuard Software/com.adguard.mac",
-            ]
+            rmdir:     "/Library/Application Support/AdGuard Software"
 
   zap trash: [
     "/Library/Logs/com.adguard.mac.vpn",
