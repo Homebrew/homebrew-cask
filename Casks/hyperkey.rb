24fb72c0345da964a32b47238429f07593eadba1
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-16 15:03:36 +0800

    Update hyperkey.rb

--- Casks/hyperkey.rb
+++ Casks/hyperkey.rb
@@ -1,15 +1,15 @@
 cask "hyperkey" do
-  version "0.11,11"
+  version "0.11"
   sha256 "b7ec7e59d8f5355ab7cfd7df93c84c9c38144da175c11dc8effd863b371a6583"
 
-  url "https://hyperkey.app/downloads/Hyperkey#{version.csv.first}.dmg"
+  url "https://hyperkey.app/downloads/Hyperkey#{version}.dmg"
   name "Hyperkey"
   desc "Convert your caps lock key or any of your modifier keys to the hyper key"
   homepage "https://hyperkey.app/"
 
   livecheck do
     url "https://hyperkey.app/downloads/updates.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   auto_updates true
