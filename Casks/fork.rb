bff7f6cfee94137a113affa10ccb70a9908805a1
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-17 04:57:38 +0800

    Update fork.rb

--- Casks/fork.rb
+++ Casks/fork.rb
@@ -10,25 +10,25 @@ cask "fork" do
   livecheck do
     url "https://git-fork.com/update/feed.xml"
     strategy :sparkle do |item|
-      item.url[%r{/Fork-(\d+(?:\.\d+)*)\.dmg}i, 1]
+      item.url[%r{/Fork-(\d+(?:\.\d+)+)\.dmg}i, 1]
     end
   end
 
   auto_updates true
-  conflicts_with cask: "fork-dev"
+  conflicts_with cask: "homebrew/cask-versions/fork-dev"
 
   app "Fork.app"
   binary "#{appdir}/Fork.app/Contents/Resources/fork_cli", target: "fork"
 
   zap trash: [
-    "~/Library/Application Support/Fork",
-    "~/Library/Application Support/com.DanPristupov.Fork",
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.danpristupov.fork.sfl2",
+    "~/Library/Application Support/com.DanPristupov.Fork",
+    "~/Library/Application Support/Fork",
     "~/Library/Caches/com.DanPristupov.Fork",
     "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.DanPristupov.Fork",
     "~/Library/Cookies/com.DanPristupov.Fork.binarycookies",
-    "~/Library/HTTPStorages/com.DanPristupov.Fork",
     "~/Library/HTTPStorages/com.DanPristupov.Fork.binarycookies",
+    "~/Library/HTTPStorages/com.DanPristupov.Fork",
     "~/Library/Logs/Fork.log",
     "~/Library/Preferences/com.DanPristupov.Fork.plist",
     "~/Library/Saved Application State/com.DanPristupov.Fork.savedState",
