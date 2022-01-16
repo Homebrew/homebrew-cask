a5822fc8da4e7024a616696cb13d0b9bb806ae48
Aaron Rumpler <80610725+Aaron-Rumpler@users.noreply.github.com>
2022-01-16 18:12:39 +1300

    Improved zap stanza for hyperkey and moved to Sparkle livecheck

--- Casks/hyperkey.rb
+++ Casks/hyperkey.rb
@@ -1,26 +1,32 @@
 cask "hyperkey" do
-  version "0.11"
+  version "0.11,11"
   sha256 "b7ec7e59d8f5355ab7cfd7df93c84c9c38144da175c11dc8effd863b371a6583"
 
-  url "https://hyperkey.app/downloads/Hyperkey#{version}.dmg"
-  name "hyperkey"
+  url "https://hyperkey.app/downloads/Hyperkey#{version.csv.first}.dmg"
+  name "Hyperkey"
   desc "Convert your caps lock key or any of your modifier keys to the hyper key"
   homepage "https://hyperkey.app/"
 
   livecheck do
-    url :homepage
-    regex(/href=.+(\d+(:?\.\d+)).dmg/i)
+    url "https://hyperkey.app/downloads/updates.xml"
+    strategy :sparkle
   end
 
+  auto_updates true
   depends_on macos: ">= :sierra"
 
   app "Hyperkey.app"
+
+  uninstall quit: "com.knollsoft.Hyperkey"
 
   zap trash: [
     "~/Library/Application Scripts/com.knollsoft.HyperkeyLauncher",
     "~/Library/Application Support/Hyperkey",
     "~/Library/Caches/com.knollsoft.Hyperkey",
     "~/Library/Containers/com.knollsoft.HyperkeyLauncher",
+    "~/Library/Cookies/com.knollsoft.Hyperkey.binarycookies",
+    "~/Library/HTTPStorages/com.knollsoft.Hyperkey",
+    "~/Library/HTTPStorages/com.knollsoft.Hyperkey.binarycookies",
     "~/Library/Preferences/com.knollsoft.Hyperkey.plist",
   ]
 end
