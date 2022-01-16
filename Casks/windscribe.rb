dc766debb4e3f54cbfbe3e2bd9fb868cf4a25383
Alireza Habibzadeh <alirezahabib80@gmail.com>
2022-01-16 21:54:07 +0100

    windscribe: update zap (#117519)
    * Added a zap
    
    Added a file left over from Windscribe after uninstalling it with --zap
    
    * Update windscribe.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/windscribe.rb
+++ Casks/windscribe.rb
@@ -9,8 +9,7 @@ cask "windscribe" do
 
   livecheck do
     url "https://windscribe.com/changelog/mac"
-    strategy :page_match
-    regex(/Windscribe\.dmg">\s*v(\d+(?:\.\d+)*)/i)
+    regex(/Windscribe\.dmg">\s*v(\d+(?:\.\d+)+)/i)
   end
 
   installer manual: "WindscribeInstaller.app"
@@ -28,14 +27,14 @@ cask "windscribe" do
               "/Applications/Windscribe.app",
               "/Library/PrivilegedHelperTools/com.windscribe.helper.macos",
               "/Library/LaunchDaemons/com.windscribe.helper.macos.plist",
-              "/private/var/run/windscribe_helper_socket2",
               "/usr/local/bin/windscribe-cli",
             ]
 
   zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.windscribe.launcher.macos.sfl*",
     "~/Library/Application Support/Windscribe",
-    "~/Library/Preferences/com.windscribe.Windscribe2.plist",
     "~/Library/Preferences/com.aaa.windscribe.windscribe.plist",
+    "~/Library/Preferences/com.windscribe.Windscribe2.plist",
     "~/Library/Saved Application State/com.windscribe.gui.macos.savedState",
   ]
 end
