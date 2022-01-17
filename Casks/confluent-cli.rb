652ff9e0e1e1f09afebc3d0b7c54fd4103732db9
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-17 21:18:34 +0800

    Update confluent-cli.rb

--- Casks/confluent-cli.rb
+++ Casks/confluent-cli.rb
@@ -3,13 +3,14 @@ cask "confluent-cli" do
   sha256 "7fe81e0d007df112a6679178ac302a7c414b3f1417d69046b2e773dd7d669691"
 
   url "https://s3-us-west-2.amazonaws.com/confluent.cloud/confluent-cli/archives/#{version}/confluent_v#{version}_darwin_amd64.tar.gz",
-      verified: "https://s3-us-west-2.amazonaws.com/confluent.cloud/confluent-cli/"
-  name "confluent-cli"
+      verified: "s3-us-west-2.amazonaws.com/confluent.cloud/confluent-cli/archives/"
+  name "Confluent CLI"
   desc "Enables developers to manage Confluent Cloud or Confluent Platform"
   homepage "https://docs.confluent.io/confluent-cli/current/overview.html"
 
   livecheck do
-    skip "No version information available"
+    url "https://s3-us-west-2.amazonaws.com/confluent.cloud/confluent-cli/archives/latest/confluent_latest_darwin_amd64.tar.gz"
+    strategy :header_match
   end
 
   binary "confluent/confluent"
