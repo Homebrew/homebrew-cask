e9251538caa850bef3221fa63a1b452b9fca7aea
Clément L <porkepix@gmail.com>
2022-01-17 14:14:00 +0100

    Update fontgoggles from 1.4.2 to 1.4.3 (#117563)

--- Casks/fontgoggles.rb
+++ Casks/fontgoggles.rb
@@ -1,6 +1,6 @@
 cask "fontgoggles" do
-  version "1.4.2"
-  sha256 "42382f2e5a44fd484feac7229559525cb9a2c07ff1348219449684479b0db36c"
+  version "1.4.3"
+  sha256 "46b623de8801808fae44a6872cfd55a553cf080fc31918a74b514c26907a78b8"
 
   url "https://github.com/justvanrossum/fontgoggles/releases/download/v#{version}/FontGoggles.dmg",
       verified: "github.com/justvanrossum/fontgoggles/"
