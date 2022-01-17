6832d5a8ee421da771ae7cbc6e59ab6acb6edf3b
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-17 15:31:20 +0800

    Update wine-stable.rb

--- Casks/wine-stable.rb
+++ Casks/wine-stable.rb
@@ -12,7 +12,7 @@ cask "wine-stable" do
   homepage "https://wiki.winehq.org/MacOS"
 
   livecheck do
-    url "https://github.com/Gcenx/macOS_Wine_builds/releases/latest"
+    url "https://github.com/Gcenx/macOS_Wine_builds/releases/"
     strategy :page_match
     regex(/wine[._-]stable[._-]v?(\d+(?:\.\d+)*)[._-]osx64\.tar\.xz/i)
   end
