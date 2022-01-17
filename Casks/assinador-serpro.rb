2db8a0fc8fdf49a3b1d0ecb7fae29d3b73bcdf14
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-17 16:34:21 +0800

    Update assinador-serpro from 3.0.0 to 3.0.1 (#117560)
    * Update assinador-serpro from 3.0.0 to 3.0.1
    
    * Update assinador-serpro.rb
    
    * Update assinador-serpro.rb

--- Casks/assinador-serpro.rb
+++ Casks/assinador-serpro.rb
@@ -1,15 +1,14 @@
 cask "assinador-serpro" do
-  version "3.0.0"
-  sha256 "a981db5b5a6a250c24565f031aa58a9fb2723ac4cbfcd3db74dbb7cdd53ca6df"
+  version "3.0.1"
+  sha256 "ec1ffcb42fefd8419a1c9fc0f7e880d2cb44c469fc6ef3dfcf6795b8a7a97201"
 
   url "https://www.serpro.gov.br/links-fixos-superiores/assinador-digital/assinador-serpro/arquivos/assinador-serpro-#{version.dots_to_hyphens}-instalador.dmg"
-  name "assinador-serpro"
+  name "Assinador Serpro"
   desc "Validate and sign documents using digital certificates"
   homepage "https://www.serpro.gov.br/links-fixos-superiores/assinador-digital/assinador-serpro"
 
   livecheck do
     url :homepage
-    strategy :page_match
     regex(/Assinador\sSerpro\s(\d+(?:\.\d+)+)/i)
   end
 
