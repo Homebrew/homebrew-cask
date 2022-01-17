8e4d41fe98c3b9dd843c1de914ef4af6d1a0ebde
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-17 16:19:20 +0800

    Update assinador-serpro.rb

--- Casks/assinador-serpro.rb
+++ Casks/assinador-serpro.rb
@@ -3,7 +3,7 @@ cask "assinador-serpro" do
   sha256 "ec1ffcb42fefd8419a1c9fc0f7e880d2cb44c469fc6ef3dfcf6795b8a7a97201"
 
   url "https://www.serpro.gov.br/links-fixos-superiores/assinador-digital/assinador-serpro/arquivos/assinador-serpro-#{version.dots_to_hyphens}-instalador.dmg"
-  name "assinador-serpro"
+  name "Assinador Serpro"
   desc "Validate and sign documents using digital certificates"
   homepage "https://www.serpro.gov.br/links-fixos-superiores/assinador-digital/assinador-serpro"
 
