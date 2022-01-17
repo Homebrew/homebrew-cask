df6906ae33d37eb0ee6fdf1821e05ab79b6fd873
Aaron Rumpler <80610725+Aaron-Rumpler@users.noreply.github.com>
2022-01-17 16:19:49 +1300

    scroll: remove trailing slash from homepage url (#117544)

--- Casks/scroll.rb
+++ Casks/scroll.rb
@@ -5,7 +5,7 @@ cask "scroll" do
   url "https://ryanhanson.dev/scroll/downloads/Scroll#{version.csv.first}.dmg"
   name "Scroll"
   desc "Configure scrolling on Trackpad and Magic Mouse"
-  homepage "https://ryanhanson.dev/scroll/"
+  homepage "https://ryanhanson.dev/scroll"
 
   livecheck do
     url "https://ryanhanson.dev/scroll/downloads/updates.xml"
