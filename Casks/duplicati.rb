61e1fa6ee8cc715d9a3f7075d98c43a017b63568
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-17 14:06:43 +0800

    duplicati 2.0.6.100,canary,2021-08-11 (#117546)

--- Casks/duplicati.rb
+++ Casks/duplicati.rb
@@ -1,21 +1,21 @@
 cask "duplicati" do
-  version "2.0.6.3,beta:2021-06-17"
-  sha256 "7a26fd69b7016e88a23ff03474eb78e174da463c4967b90c0b54f07a94027e18"
+  version "2.0.6.100,canary,2021-08-11"
+  sha256 "434cdedbd15b3562cf943b55114515b7b00bdaa1cccf06ea22b9f625bd3f0a02"
 
-  url "https://github.com/duplicati/duplicati/releases/download/v#{version.csv.first}-#{version.csv.first}_#{version.csv.second.before_colon}_#{version.after_colon}/duplicati-#{version.csv.first}_#{version.csv.second.before_colon}_#{version.after_colon}.dmg",
+  url "https://github.com/duplicati/duplicati/releases/download/v#{version.csv.first}-#{version.csv.first}_#{version.csv.second}_#{version.csv.third}/duplicati-#{version.csv.first}_#{version.csv.second}_#{version.csv.third}.dmg",
       verified: "github.com/duplicati/duplicati/"
   name "Duplicati"
-  desc "Store securely encrypted backups in the cloud!"
+  desc "Store securely encrypted backups in the cloud"
   homepage "https://www.duplicati.com/"
 
   livecheck do
     url :url
     strategy :git do |tags|
       tags.map do |tag|
-        match = tag.match(/^v(\d+(?:\.\d+)+)-(?:\d+(?:\.\d+)*)_(stable|beta)_(\d+(?:-\d+)*)$/i)
+        match = tag.match(/^v(\d+(?:\.\d+)+)-(?:\d+(?:\.\d+)+)_(beta|canary|experimental)_(\d+(?:-\d+)*)$/i)
         next if match.blank?
 
-        "#{match[1]},#{match[2]}:#{match[3]}" if match
+        "#{match[1]},#{match[2]},#{match[3]}" if match
       end.compact
     end
   end
@@ -25,7 +25,7 @@ cask "duplicati" do
   app "Duplicati.app"
 
   zap trash: [
-    "~/Library/Application Support/Duplicati",
     "~/.config/Duplicati",
+    "~/Library/Application Support/Duplicati",
   ]
 end
