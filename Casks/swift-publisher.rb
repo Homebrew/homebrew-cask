5b5fc5f09e8408948cbe61414b11c0dda4ea3a2f
neutric <5984479+neutric@users.noreply.github.com>
2022-01-17 01:32:57 +0100

    Update swift-publisher from 5.6.1 to 5.6.2 (#117542)
    * Update swift-publisher from 5.6.1 to 5.6.2
    
    * swift-publisher: update homepage following redirect

--- Casks/swift-publisher.rb
+++ Casks/swift-publisher.rb
@@ -1,12 +1,12 @@
 cask "swift-publisher" do
-  version "5.6.1,4762"
+  version "5.6.2,4777"
   sha256 :no_check
 
   url "https://belightsoft.s3.amazonaws.com/SwiftPublisher.dmg",
       verified: "belightsoft.s3.amazonaws.com/"
   name "Swift Publisher"
   desc "Page layout and desktop publishing application"
-  homepage "https://www.belightsoft.com/swift-publisher/"
+  homepage "https://www.swiftpublisher.com/"
 
   livecheck do
     url "https://www.belightsoft.com/download/updates/appcast_SwiftPublisher#{version.major}.xml"
