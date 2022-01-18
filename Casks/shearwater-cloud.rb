6786441a2bce47dcfcda8795adf43c602d52b00c
tpak <tpak@users.noreply.github.com>
2022-01-17 19:32:44 -0700

    bump shearwater-cloud version from 2.6.2 to 2.8.2

--- Casks/shearwater-cloud.rb
+++ Casks/shearwater-cloud.rb
@@ -1,6 +1,6 @@
 cask "shearwater-cloud" do
-  version "2.6.2"
-  sha256 "200ab24b520c385c2bfd6abcf5a3a6cca1f45799e6ab84152adaaf727bf9afcf"
+  version "2.8.2"
+  sha256 "f9a4b3a9ee1dc1f74b31355bc025c77c14e738fdc96c1881564bf0e48e488d69"
 
   url "https://www.shearwater.com/wp-content/downloads/ShearwaterCloudInstaller_#{version}.dmg"
   name "Shearwater Cloud"
