401f00efe2aad90d8a959a2f53d8d93b838d70d9
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-19 06:39:32 +0800

    Update and rename surge-xt-synthesizer.rb to surge-xt.rb

--- Casks/surge-xt.rb
+++ Casks/surge-xt.rb
@@ -1,4 +1,4 @@
-cask "surge-xt-synthesizer" do
+cask "surge-xt" do
   version "1.0.0"
   sha256 "d0a481745e7526b02ba41267be9bf61a90377f0fed80ffcebc655777cd9c5a4f"
 
@@ -11,19 +11,18 @@ cask "surge-xt-synthesizer" do
   pkg "surge-xt-macOS-#{version}.pkg"
 
   uninstall pkgutil: [
-    "com.surge-synth-team.surge-xt.app.pkg",
-    "com.surge-synth-team.surge-xt.component.pkg",
-    "com.surge-synth-team.surge-xt.vst3.pkg",
     "com.surge-synth-team.surge-xt-fx.app.pkg",
     "com.surge-synth-team.surge-xt-fx.component.pkg",
     "com.surge-synth-team.surge-xt-fx.vst3.pkg",
+    "com.surge-synth-team.surge-xt.app.pkg",
+    "com.surge-synth-team.surge-xt.component.pkg",
     "com.surge-synth-team.surge-xt.resources.pkg",
+    "com.surge-synth-team.surge-xt.vst3.pkg",
   ],
-            delete:
-                     [
-                       "/Applications/Surge XT Effects.app",
-                       "/Applications/Surge XT.app",
-                     ]
+            delete:  [
+              "/Applications/Surge XT Effects.app",
+              "/Applications/Surge XT.app",
+            ]
 
-  # no zap stanza required
+  zap rmdir: "~/Documents/Surge XT"
 end
