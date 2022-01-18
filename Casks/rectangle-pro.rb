b125dd8ac07b5c9ca33a96940e28963742d06883
ShredderWalnut <34428332+ShredderWalnut@users.noreply.github.com>
2022-01-19 05:43:32 +0800

    Update rectangle-pro from 2.0 to 2.1 (#117618)

--- Casks/rectangle-pro.rb
+++ Casks/rectangle-pro.rb
@@ -1,6 +1,6 @@
 cask "rectangle-pro" do
-  version "2.0,100"
-  sha256 "b151ad1b63168f6f98b8c3792329eb6abb2131ab7d3c6e794cb9d61960f3e7c7"
+  version "2.1,101"
+  sha256 "64e070c3bdd54cefddb6d5bfc2c2c2318479b42928bcbaa2e6e9e08e4caabf2d"
 
   url "https://rectangleapp.com/pro/downloads/Rectangle%20Pro%20#{version.csv.first}.dmg"
   name "Rectangle Pro"
