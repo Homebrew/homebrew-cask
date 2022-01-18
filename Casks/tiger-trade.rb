6fec7b7b488a57b772e8cd533314de2f722aa2b4
neutric <5984479+neutric@users.noreply.github.com>
2022-01-19 00:52:05 +0100

    Update tiger-trade from 7.6.1 to 7.8.1

--- Casks/tiger-trade.rb
+++ Casks/tiger-trade.rb
@@ -1,6 +1,6 @@
 cask "tiger-trade" do
-  version "7.6.1,20211130,AC74EA"
-  sha256 "48c5fcaa06055436088e0d9736d50f4ea08a8274e5d57b38240daa19152716c2"
+  version "7.8.1,20220106,1034F7"
+  sha256 "342892164099845ef757b394b81715658eb24825b0ef7b7a0e9089895b45cb17"
 
   url "https://s.tigerfintech.com/desktop/cdn/f/TigerTrade_#{version.tr(",", "_")}.dmg",
       verified: "s.tigerfintech.com/"
