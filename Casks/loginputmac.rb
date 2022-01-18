4065e043fd95a49a504b5b6cdab410a9144287ea
R0uter <R0uter@users.noreply.github.com>
2022-01-18 14:40:03 +1000

    Update loginputmac from 2.5.3 to 2.5.4 (#117602)

--- Casks/loginputmac.rb
+++ Casks/loginputmac.rb
@@ -1,6 +1,6 @@
 cask "loginputmac" do
-  version "2.5.3,24161"
-  sha256 "a6111d21be1f28e7a8ea748c7b98d5168432c20d47ad6abe93b05867429f58f5"
+  version "2.5.4,24201"
+  sha256 "5db8aec4ab41249ac12be68858adeaa173257cdbb037ab48f350a2146203f4f2"
 
   url "https://loginput-mac2.totest.top/loginputmac#{version.major}_latest.pkg",
       verified: "loginput-mac2.totest.top/"
