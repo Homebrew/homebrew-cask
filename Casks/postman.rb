35a22025c53ecf31c53c8d3cd2759d5d9d9563a6
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2022-01-19 03:06:15 +0500

    Update postman from 9.9.0 to 9.9.3 (#117614)
    * Update postman from 9.9.0 to 9.9.3
    
    * update livecheck
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,12 +1,12 @@
 cask "postman" do
   arch = Hardware::CPU.intel? ? "osx64" : "osx_arm64"
 
-  version "9.9.0"
+  version "9.9.3"
 
   if Hardware::CPU.intel?
-    sha256 "365bcd99027e785640097af1541be785ee173ebb24bb1d21fe223ba0c36cdecc"
+    sha256 "5e81ddb6688c623e784bf59b57b8bebae3ddfa9a57f3988964e7588dee68d95b"
   else
-    sha256 "fd973c296bb18a8c842ae1b6ebbfbdc3454a6c7c65925caca7710bd436690db2"
+    sha256 "386a544678a5cf0317623a37dcfc1629c16559fdb7c970d150d430e85e48dd87"
   end
 
   url "https://dl.pstmn.io/download/version/#{version}/#{arch}",
@@ -16,8 +16,11 @@ cask "postman" do
   homepage "https://www.postman.com/"
 
   livecheck do
-    url "https://dl.pstmn.io/api/version/notes/"
-    regex(/Postman\s*v?(\d+(?:\.\d+)+)/i)
+    url "https://dl.pstmn.io/api/version/"
+    strategy :page_match do |page|
+      stable_versions = JSON.parse(page).filter { |v| v["channel"] == "stable" }
+      stable_versions.map { |v| v["name"] }
+    end
   end
 
   auto_updates true
