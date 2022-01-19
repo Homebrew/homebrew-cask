ee757a9e0964bcb7b5080a2daf5a505ce623be5e
Anton Möller <anton.myller@gmail.com>
2022-01-19 11:24:00 +0100

    Update tableau-prep from 2021.4.3 to 2021.4.4 (#117671)
    Co-authored-by: Anton Möller <anton@esportal.com>

--- Casks/tableau-prep.rb
+++ Casks/tableau-prep.rb
@@ -1,6 +1,6 @@
 cask "tableau-prep" do
-  version "2021.4.3"
-  sha256 "4cd19f79f87a0621565c2067521484fc77b44a0c23fd80a19336d17926b4b3b4"
+  version "2021.4.4"
+  sha256 "bf589d9740c683e69f23de90acd9d9284530f47e92ddbccae20f163e68723b18"
 
   url "https://downloads.tableau.com/esdalt/tableau_prep/#{version}/TableauPrep-#{version.dots_to_hyphens}.dmg"
   name "Tableau Prep"
