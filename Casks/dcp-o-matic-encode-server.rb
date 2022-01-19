d9f5ee69cb4d6342334195c7b6a42a39b0acdead
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-19 17:52:23 +0800

    Update dcp-o-matic-encode-server.rb

--- Casks/dcp-o-matic-encode-server.rb
+++ Casks/dcp-o-matic-encode-server.rb
@@ -8,9 +8,7 @@ cask "dcp-o-matic-encode-server" do
   homepage "https://dcpomatic.com/"
 
   livecheck do
-    url "https://dcpomatic.com/download"
-    strategy :page_match
-    regex(/stable\s*release:\s*(\d+(?:\.\d+)+)/i)
+    cask "dcp-o-matic"
   end
 
   app "DCP-o-matic #{version.major} Encode Server.app"
