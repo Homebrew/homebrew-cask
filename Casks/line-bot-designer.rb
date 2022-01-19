5d4792a3dfa9ff65a91ee6e5aa25885dc6c635f1
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-19 18:28:15 +0800

    Update line-bot-designer.rb (#117672)

--- Casks/line-bot-designer.rb
+++ Casks/line-bot-designer.rb
@@ -3,14 +3,14 @@ cask "line-bot-designer" do
   sha256 "d4b80baaa73d0947f6b6be2c2530877edc67d4bbe208a5cc1ad0c509b8f505b1"
 
   url "https://d.line-scdn.net/r/devcenter/bot-designer/LINE%20Bot%20Designer-#{version}.dmg",
-      verified: "d.line-scdn.net/"
+      verified: "d.line-scdn.net/r/devcenter/bot-designer/"
   name "LINE Bot Designer"
+  desc "Prototype LINE bots"
   homepage "https://developers.line.biz/en/"
 
   livecheck do
-    url "https://developers.line.biz/en/bot-designer/download/"
-    strategy :page_match
-    regex(%r{href=.*?/LINE%20Bot%20Designer-(\d+(?:\.\d+)*)\.dmg}i)
+    url "https://developers.line.biz/en/docs/messaging-api/download-bot-designer/"
+    regex(/LINE%20Bot%20Designer[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
   app "LINE Bot Designer.app"
