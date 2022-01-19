a03f744599b84db9d966e7143d9185e188770ec0
owine <owine@users.noreply.github.com>
2022-01-19 16:26:43 -0600

    Update google-chrome from 97.0.4692.71 to 97.0.4692.99

--- Casks/google-chrome.rb
+++ Casks/google-chrome.rb
@@ -1,7 +1,7 @@
 cask "google-chrome" do
   arch = Hardware::CPU.intel? ? "" : "universal/"
 
-  version "97.0.4692.71"
+  version "97.0.4692.99"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/#{arch}stable/GGRO/googlechrome.dmg"
