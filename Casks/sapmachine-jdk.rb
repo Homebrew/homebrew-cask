b6a28195a8cd852d7c956193604de175f56a5dbf
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-19 21:09:32 +0800

    Update sapmachine-jdk.rb

--- Casks/sapmachine-jdk.rb
+++ Casks/sapmachine-jdk.rb
@@ -3,14 +3,14 @@ cask "sapmachine-jdk" do
 
   version "17.0.2"
 
-  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_macos-#{arch}_bin.dmg",
-      verified: "github.com/SAP/SapMachine/"
   if Hardware::CPU.intel?
     sha256 "c00b7a32f7be3e1a400d815c22d0e254cdab0e8f421cb6d63ef8a50c8e8a501a"
   else
     sha256 "7502b95cf9e691197e21637a8e4acfca3a0955aa11f36e47824bf0083085bfc2"
   end
 
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_macos-#{arch}_bin.dmg",
+      verified: "github.com/SAP/SapMachine/"
   name "SapMachine OpenJDK Development Kit"
   desc "OpenJDK distribution from SAP"
   homepage "https://sapmachine.io/"
@@ -18,7 +18,7 @@ cask "sapmachine-jdk" do
   livecheck do
     url :url
     strategy :github_latest
-    regex(%r{href=.*/sapmachine-jdk-(\d+(?:\.\d+)*(?:\+\d+(?:\.\d+)*)?)_macos-(aarch64|x64)_bin\.dmg}i)
+    regex(%r{href=.*/sapmachine-jdk-(\d+(?:\.\d+)*(?:\+\d+(?:\.\d+)*)?)_macos-#{arch}_bin\.dmg}i)
   end
 
   artifact "sapmachine-jdk-#{version}.jdk", target: "/Library/Java/JavaVirtualMachines/sapmachine-jdk-#{version}.jdk"
