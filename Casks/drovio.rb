9678d8c7da6fe54976bda79fdbf5efeb32602a5c
Benjamin Battin <benjamin.battin@use-together.com>
2022-01-20 08:05:18 +0100

    Handling both x86_64 and arm64 packaging for Drovio

--- Casks/drovio.rb
+++ Casks/drovio.rb
@@ -1,14 +1,21 @@
 cask "drovio" do
-  version "3.2.0"
-  sha256 "e9c371acba7b23c1a532c39486ea1d7d1307b74228dfe26c4347138289b6aa8e"
+  arch_package_folder = Hardware::CPU.intel? ? "macos" : "macos_silicon"
 
-  url "https://repository.drovio.com/stable/drovio/macos/#{version.major}.x/#{version}/drovio.dmg"
+  version "3.2.0"
+
+  if Hardware::CPU.intel?
+    sha256 "e9c371acba7b23c1a532c39486ea1d7d1307b74228dfe26c4347138289b6aa8e"
+  else
+    sha256 "de9f2df2b590111595cd1530a9bc63dada10f2bc2e8d6a25559a4290dbc479c0"
+  end
+
+  url "https://repository.drovio.com/stable/drovio/#{arch_package_folder}/#{version.major}.x/#{version}/drovio.dmg"
   name "Drovio"
   desc "Remote pair programming and team collaboration tool"
   homepage "https://www.drovio.com/"
 
   livecheck do
-    url "https://repository.drovio.com/stable/drovio/macos/latest_version/release.json"
+    url "https://repository.drovio.com/stable/drovio/#{arch_package_folder}/latest_version/release.json"
     regex(/"version"\s*:\s*"(\d+(?:\.\d+)+)"/i)
   end
 
