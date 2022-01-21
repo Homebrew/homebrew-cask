9e9e48182c31d116a7814483fada0c82b4dbc7df
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-21 16:47:18 +0800

    Update audiogridder-server.rb (#117806)

--- Casks/audiogridder-server.rb
+++ Casks/audiogridder-server.rb
@@ -3,20 +3,19 @@ cask "audiogridder-server" do
 
   version "1.1.1"
 
-  url "https://audiogridder.com/releases/AudioGridderServer_#{version}_macOS-#{arch}.pkg"
   if Hardware::CPU.intel?
     sha256 "072e52bd56fedb1c481961beacca99096e3a65ba36339bdd78155fd1f1d133ea"
   else
     sha256 "26be020b2ba57d7a0102a152099fcc5e993ce4bbcb23f47d80d5d9f24421658b"
   end
 
+  url "https://audiogridder.com/releases/AudioGridderServer_#{version}_macOS-#{arch}.pkg"
   name "AudioGridder Server"
   desc "VST2/VST3/AU DSP Server"
   homepage "https://audiogridder.com/"
 
   livecheck do
     url "https://audiogridder.com/releases/latest.txt"
-    strategy :page_match
     regex(/(\d+(?:\.\d+)+)/)
   end
 
@@ -25,9 +24,9 @@ cask "audiogridder-server" do
   uninstall pkgutil: "com.e47.pkg.server"
 
   zap trash: [
-    "~/.audiogridder/audiogridderserver.cfg",
-    "~/.audiogridder/audiogridderserver.cache",
     "~/.audiogridder/audiogridder.winpos",
+    "~/.audiogridder/audiogridderserver.cache",
+    "~/.audiogridder/audiogridderserver.cfg",
     "~/Library/Logs/AudioGridder/Master",
     "~/Library/Logs/AudioGridder/Scan",
     "~/Library/Logs/AudioGridder/Server",
