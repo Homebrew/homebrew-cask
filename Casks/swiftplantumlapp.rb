ff4b6cc76724974509c9d0b80a803fff823c4ac9
MarcoEidinger <eidingermarco@gmail.com>
2022-01-21 15:00:54 -0800

    Update swiftplantumlapp from 1.2.1 to 1.2.2 (#117837)

--- Casks/swiftplantumlapp.rb
+++ Casks/swiftplantumlapp.rb
@@ -1,6 +1,6 @@
 cask "swiftplantumlapp" do
-  version "1.2.1"
-  sha256 "44dc78f5ef2795cb881d4ffa028a9b728c6000b0effbce3e294815d77bda46da"
+  version "1.2.2"
+  sha256 "e1a188e1b93526fa96b728a3d356eab78fd5d66b9c219c28b45f9ae4e4c35f6e"
 
   url "https://github.com/MarcoEidinger/SwiftPlantUML-Xcode-Extension/releases/download/#{version}/SwiftPlantUMLApp.zip"
   name "swiftplantumlapp"
