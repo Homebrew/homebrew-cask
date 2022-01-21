dd7693a04176a56c5c937072638feff7de6e7757
JBallin <JBallin@users.noreply.github.com>
2022-01-20 22:25:37 -0800

    Update gather from 0.1.6 to 0.1.7 (#117756)

--- Casks/gather.rb
+++ Casks/gather.rb
@@ -1,6 +1,6 @@
 cask "gather" do
-  version "0.1.6"
-  sha256 "7f11ea97ae173182134438c48d415bd1fa78f8fd7067b7c55ff02647091073c1"
+  version "0.1.7"
+  sha256 "9824bc0de4e85c045d51938f78bb2416050602205d6e7aa929e19389e388d1fe"
 
   url "https://github.com/gathertown/gather-town-desktop-releases/releases/download/v#{version}/Gather-#{version}-mac.zip",
       verified: "github.com/gathertown/gather-town-desktop-releases"
