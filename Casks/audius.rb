15591ca7affd1e8fccf6504ed8e90af3bae89e57
Hannes Braun <hannesbraun@mail.de>
2022-01-21 15:13:46 +0100

    Update audius from 0.24.38 to 0.24.39 (#117815)

--- Casks/audius.rb
+++ Casks/audius.rb
@@ -1,6 +1,6 @@
 cask "audius" do
-  version "0.24.38"
-  sha256 "c342d6a656f47282486baa860db1487fec24083745a2bea18cfb9e378e6ade26"
+  version "0.24.39"
+  sha256 "339d4854454bbced6067c973c1d0d93bd97ace1d4dfd5b7ce391d3c51a9ae1e2"
 
   url "https://download.audius.co/Audius-#{version}.dmg"
   name "Audius"
