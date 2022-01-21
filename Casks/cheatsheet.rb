377804d1692296a220c9a719df010dc49ec80d67
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2022-01-21 07:57:02 +0100

    CheatSheet: Update homepage URL (#117774)

--- Casks/cheatsheet.rb
+++ Casks/cheatsheet.rb
@@ -2,11 +2,10 @@ cask "cheatsheet" do
   version "1.6.2"
   sha256 "8e80029a61b5b53b7b263409295f4769733d8158f3fe58bffe1cce84bc371b6f"
 
-  url "https://mediaatelier.com/CheatSheet/CheatSheet_#{version}.zip",
-      verified: "mediaatelier.com/CheatSheet/"
+  url "https://mediaatelier.com/CheatSheet/CheatSheet_#{version}.zip"
   name "CheatSheet"
   desc "Tool to list all active shortcuts of the current application"
-  homepage "https://www.cheatsheetapp.com/CheatSheet/"
+  homepage "https://www.mediaatelier.com/CheatSheet/"
 
   livecheck do
     url "https://mediaatelier.com/CheatSheet/feed.php"
