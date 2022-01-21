daf722a1cedacf50abfaf569be0df75827e15ad4
jack <jacklandrin@hotmail.com>
2022-01-21 15:46:16 +0100

    Update only-switch from v2.1.2 to v2.1.3 (#117832)
    * update only-switch to v1.7
    
    * Update only-switch from v1.7 to v1.8
    
    * Update only-switch from v1.8 to v1.8.1
    
    * Update only-switch from v1.8.1 to v1.9
    
    * Update only-switch from v1.9 to v2.0
    
    * Update only-switch from v2.0 to v2.1
    
    * delete extra empty line
    
    * Update only-switch from v2.1 to v2.1.1
    
    * Update only-switch from v2.1.1 to v2.1.2
    
    * Update only-switch from v2.1.2 to v2.1.3
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/only-switch.rb
+++ Casks/only-switch.rb
@@ -1,6 +1,6 @@
 cask "only-switch" do
-  version "2.1.2"
-  sha256 "437d55528c003c6cdd1fe6f668e12269a54c49662e6f66fab24ecbcf3ca056cb"
+  version "2.1.3"
+  sha256 "3411a338aca1f7bad391971e0862420918dd98e66aec39436d618ea8374d5d20"
 
   url "https://github.com/jacklandrin/OnlySwitch/releases/download/release_#{version}/OnlySwitch.dmg"
   name "OnlySwitch"
