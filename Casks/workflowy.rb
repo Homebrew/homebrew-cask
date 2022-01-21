0ee02cd25a10b52bd8b69b089275d979b0a3ea96
neutric <5984479+neutric@users.noreply.github.com>
2022-01-22 00:55:29 +0100

    Update workflowy from 1.3.7-2795 to 1.3.7-3060 (#117863)

--- Casks/workflowy.rb
+++ Casks/workflowy.rb
@@ -1,6 +1,6 @@
 cask "workflowy" do
-  version "1.3.7-2795"
-  sha256 "ca3c83077fdae070af50b626bd5b85ba90cb27a14005549f352a241147b5086a"
+  version "1.3.7-3060"
+  sha256 "259bf7f5b8202664c38d3bea5c46657063e02ded124fb3a1748aa09f3164e7f2"
 
   url "https://github.com/workflowy/desktop/releases/download/v#{version}/WorkFlowy.zip",
       verified: "github.com/workflowy/desktop/"
