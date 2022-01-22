a41fb9f5da799c25e28c85ea0f8f15ff53a9ef38
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-22 23:16:07 +0800

    Update cryo.rb

--- Casks/cryo.rb
+++ Casks/cryo.rb
@@ -3,14 +3,13 @@ cask "cryo" do
   sha256 "1399b7b0cd61b27858237dc4f21907069b1431ae9135d3203c64b206d6060dd9"
 
   url "https://cryonet.io/downloads/macos/cryo_#{version}_macos.zip"
-  name "cryo"
+  name "Cryo"
   desc "Visual file manager"
   homepage "https://cryonet.io/"
 
   livecheck do
     url :homepage
-    strategy :page_match
-    regex(%r{href=.*?/cryo_(\d+(?:\.\d+)+)_macos\.zip}i)
+    regex(%r{href=.*?/cryo[._-]v?(\d+(?:\.\d+)+)[._-]macos\.zip}i)
   end
 
   app "cryo.app"
