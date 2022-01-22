8b6f181b5133b8a3db7e9b78e8987251aa21a4f7
Alexander Ronald Altman <alexanderaltman@me.com>
2022-01-22 15:58:15 -0800

    Update keybase from 5.9.0,20211216221146,9b33c9201f to 5.9.0,20220121192730,95a3939b3a (#117920)
    * Update keybase from 5.9.0,20211216221146,9b33c9201f to 5.9.0,20220121192730,95a3939b3a
    
    * Update keybase.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/keybase.rb
+++ Casks/keybase.rb
@@ -1,6 +1,6 @@
 cask "keybase" do
-  version "5.9.0,20211216221146,9b33c9201f"
-  sha256 "17f0c419665e98c26eb705bbcf591bd222125ab19f3085f3dbf5e3926cdd7a22"
+  version "5.9.0,20220121192730,95a3939b3a"
+  sha256 "00a113f296545b464135764c51a66e0ab792785004cf37f6e3f9cc220990c267"
 
   url "https://prerelease.keybase.io/darwin-updates/Keybase-#{version.csv.first}-#{version.csv.second}%2B#{version.csv.third}.zip"
   name "Keybase"
@@ -10,7 +10,7 @@ cask "keybase" do
   livecheck do
     url "https://prerelease.keybase.io/update-darwin-prod-v2.json"
     strategy :page_match do |page|
-      match = page.match(/Keybase-(\d+(?:\.\d+)*)-(\d+)%2B([0-9a-f]+)\.zip/i)
+      match = page.match(/Keybase[._-]v?(\d+(?:\.\d+)+)[._-](\d+)%2B([0-9a-f]+)\.zip/i)
       next if match.blank?
 
       "#{match[1]},#{match[2]},#{match[3]}"
@@ -26,7 +26,10 @@ cask "keybase" do
                    args: ["install-auto"]
   end
 
-  uninstall delete:    "/Library/PrivilegedHelperTools/keybase.Helper",
+  uninstall delete:    [
+    "/Library/Logs/keybase*",
+    "/Library/PrivilegedHelperTools/keybase.Helper",
+  ],
             launchctl: "keybase.Helper",
             signal:    [
               ["TERM", "keybase.Electron"],
@@ -44,9 +47,7 @@ cask "keybase" do
     "~/Library/Caches/Keybase",
     "~/Library/Group Containers/keybase",
     "~/Library/Logs/Keybase*",
-    "~/Library/Logs/keybase*",
     "~/Library/Preferences/keybase*",
-    "/Library/Logs/keybase*",
   ],
       rmdir: "/keybase"
 end
