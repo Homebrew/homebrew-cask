ea704bde1a06c3161f6e5003955f1e0228e93e91
Clément L <porkepix@gmail.com>
2022-01-22 15:32:30 +0100

    Update bdash from 1.12.2 to 1.13.0 (#117879)

--- Casks/bdash.rb
+++ Casks/bdash.rb
@@ -1,6 +1,6 @@
 cask "bdash" do
-  version "1.12.2"
-  sha256 "c851262cbfe3e542d2722138ef7f327482ce4c8c02caf64d4a4db2c590a52513"
+  version "1.13.0"
+  sha256 "f2c3ea33f774fe3f9fe4c35a34d03017a89bff566b73f5e5ad40937f7512c89d"
 
   url "https://github.com/bdash-app/bdash/releases/download/v#{version}/Bdash-#{version}-mac.zip"
   name "Bdash"
