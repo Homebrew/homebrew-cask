e13d83013e1290930cc018bd921d278c628402a9
neutric <5984479+neutric@users.noreply.github.com>
2022-01-23 00:51:07 +0100

    yu-writer: update urls and switch livecheck strategy (#117926)
    * yu-writer: update url and homepage following redirect
    
    * yu-writer: switch livecheck strategy to Git
    
    * yu-writer: add desc

--- Casks/yu-writer.rb
+++ Casks/yu-writer.rb
@@ -1,21 +1,12 @@
 cask "yu-writer" do
-  version "0.5.3,beta"
+  version "0.5.3"
   sha256 "4fff4042c6ac7c047097c5e6d59a8a1c3f9dacfbdcadb3121904426413b38e06"
 
-  url "https://github.com/hemashushu/yu-writer.site/releases/download/v#{version.csv.first}/yu-writer-#{version.csv.second}-#{version.csv.first}-macos.dmg",
-      verified: "github.com/hemashushu/yu-writer.site/"
+  url "https://github.com/ivarptr/yu-writer.site/releases/download/v#{version}/yu-writer-beta-#{version}-macos.dmg",
+      verified: "github.com/ivarptr/yu-writer.site"
   name "Yu Writer"
-  homepage "https://hemashushu.github.io/yu-writer.site/"
-
-  livecheck do
-    url :homepage
-    strategy :page_match do |page|
-      match = page.match(%r{href=.*?/yu-writer-(?:([a-z]+)-)?(\d+(?:\.\d+)*)-macos\.dmg}i)
-      next if match.blank?
-
-      "#{match[2]},#{match[1]}"
-    end
-  end
+  desc "Markdown editor"
+  homepage "https://ivarptr.github.io/yu-writer.site/"
 
   app "Yu Writer.app"
 
