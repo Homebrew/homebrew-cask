73bf62b47a02908a37a855b22ae83076625f2db3
Reno <25192197+singularitti@users.noreply.github.com>
2022-01-22 18:42:57 -0500

    Update mathcha-notebook from 1.0.427 to 1.0.428 (#117915)

--- Casks/mathcha-notebook.rb
+++ Casks/mathcha-notebook.rb
@@ -1,6 +1,6 @@
 cask "mathcha-notebook" do
-  version "1.0.427"
-  sha256 "7d7e3642cdcab1a375e3ceed0815a22b49ec7efcde29f4901e38f82c84a73359"
+  version "1.0.428"
+  sha256 "1333c5470ad6c3387bb947354293f69bf4f5a7a88085da44a71382fa396dd338"
 
   url "https://notebook-downloads.mathcha.io/public/Mathcha%20Notebook-#{version}-mac.zip"
   name "Mathcha Notebook"
