0f1added35a0d37a5bfb396448d548bb44bfc9aa
Eric Macauley <213539+macauley@users.noreply.github.com>
2022-01-21 22:33:27 -0800

    Update qth from 0.8.0 to 0.8.1 (#117873)
    Co-authored-by: Eric Macauley <macauley@users.noreply.github.com>

--- Casks/qth.rb
+++ Casks/qth.rb
@@ -1,6 +1,6 @@
 cask "qth" do
-  version "0.8.0"
-  sha256 "384733f3dba9a5e9e86d304c11bc196ba5901c8d44404dddb41cf02bdb909027"
+  version "0.8.1"
+  sha256 "7223a1fafc08f702af4aef8210d21316f7a2d8073f1f7092f013e4053d26fc2a"
 
   url "https://www.w8wjb.com/qth/QTH-#{version}.dmg"
   name "QTH"
