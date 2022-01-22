70a8c763dbd69398444df557c18714070c2940ba
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-22 23:17:59 +0800

    Update crystax-ndk.rb

--- Casks/crystax-ndk.rb
+++ Casks/crystax-ndk.rb
@@ -9,8 +9,7 @@ cask "crystax-ndk" do
 
   livecheck do
     url :homepage
-    strategy :page_match
-    regex(%r{href=.*?/crystax-ndk-(\d+(?:\.\d+)+)-darwin-x86_64\.tar\.xz}i)
+    regex(%r{href=.*?/crystax[._-]ndk[._-]v?(\d+(?:\.\d+)+)[._-]darwin[._-]x86[._-]64\.tar\.xz}i)
   end
 
   conflicts_with cask: "android-ndk"
