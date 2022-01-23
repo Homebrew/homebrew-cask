49bd36b6a72bf460bb170556a7e24ead832ea1c2
ymadd <Kb_yamato@protonmail.com>
2022-01-24 03:13:30 +0900

    Update sigmaos from 0.15.2.1-b to 0.15.2.1-b

--- Casks/sigmaos.rb
+++ Casks/sigmaos.rb
@@ -1,6 +1,6 @@
 cask "sigmaos" do
-  version "0.15.1.1-b"
-  sha256 "3793b4f322ccae26ae4cbe2d740bdfcfaeb241defba09296ce326daa97baf43b"
+  version "0.15.2.1-b"
+  sha256 "897d87df22cbac5f27b99a1542f011f1ae8dc6445eb5a5e7716231fd5f3bc2f5"
 
   url "https://releases.sigmaos.com/macos/SigmaOS-#{version}.dmg"
   name "sigmaos"
