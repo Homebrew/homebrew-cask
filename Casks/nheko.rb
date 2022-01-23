d419dec6c7e1c2f487ed1d4c129a1938c93b46c0
friedrichweise <account@weise.io>
2022-01-23 12:03:27 +0100

    Update nheko from 0.9.0 to 0.9.1 – style fixes

--- Casks/nheko.rb
+++ Casks/nheko.rb
@@ -2,7 +2,7 @@ cask "nheko" do
   version "0.9.1,dfab0c92"
   sha256 "a795ebee28df116997b2da9d20db132a8a07a35d438eb9bf9be98467f1bb2f27"
 
-  url "https://github.com/Nheko-Reborn/nheko/releases/download/v#{version.before_comma}/nheko-v#{version.before_comma}-#{version.after_comma}-#{version.after_comma}.dmg",
+  url "https://github.com/Nheko-Reborn/nheko/releases/download/v#{version.csv.first}/nheko-v#{version.csv.first}-#{version.csv.second}-#{version.csv.second}.dmg",
       verified: "github.com/Nheko-Reborn/nheko/"
   name "Nheko"
   desc "Desktop client for the Matrix protocol"
