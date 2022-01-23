bdf4c3b0cf8739980ee291038d4422937c3bb9ee
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-23 10:58:57 +0800

    Update final-fantasy-xiv-online.rb

--- Casks/final-fantasy-xiv-online.rb
+++ Casks/final-fantasy-xiv-online.rb
@@ -3,15 +3,14 @@ cask "final-fantasy-xiv-online" do
   sha256 "a187414e0a0a101b995f08706d1bd341992258e72d535409f1136daaed413f17"
 
   url "https://gdl.square-enix.com/ffxiv/inst/#{version}/FINAL_FANTASY_XIV_ONLINE_x64.dmg",
-      verified: "square-enix.com"
+      verified: "square-enix.com/ffxiv/inst/"
   name "Final Fantasy XIV"
   desc "Story-driven massively multiplayer online role-playing game"
   homepage "https://www.finalfantasyxiv.com/"
 
   livecheck do
     url "https://na.finalfantasyxiv.com/mac/download/"
-    strategy :page_match
-    regex(%r{href="https://gdl.square-enix.com/ffxiv/inst/([^/]*)/FINAL_FANTASY_XIV_ONLINE_x64.dmg"}i)
+    regex(%r{href="https://gdl.square-enix.com/ffxiv/inst/([^/]*)/FINAL[._-]FANTASY[._-]XIV[._-]ONLINE[._-]x64.dmg"}i)
   end
 
   auto_updates true
