bdb7d0e6bbc03b06cbfab28421055a631568af7f
friedrichweise <account@weise.io>
2022-01-23 11:59:34 +0100

    Update nheko from 0.9.0 to 0.9.1

--- Casks/nheko.rb
+++ Casks/nheko.rb
@@ -1,8 +1,8 @@
 cask "nheko" do
-  version "0.9.0"
-  sha256 "7065b579903098dd098707812a7e6e325860517e01e70e6ee9b205f2efaa2aaf"
+  version "0.9.1,dfab0c92"
+  sha256 "a795ebee28df116997b2da9d20db132a8a07a35d438eb9bf9be98467f1bb2f27"
 
-  url "https://github.com/Nheko-Reborn/nheko/releases/download/v#{version}/nheko-v#{version}.dmg",
+  url "https://github.com/Nheko-Reborn/nheko/releases/download/v#{version.before_comma}/nheko-v#{version.before_comma}-#{version.after_comma}-#{version.after_comma}.dmg",
       verified: "github.com/Nheko-Reborn/nheko/"
   name "Nheko"
   desc "Desktop client for the Matrix protocol"
