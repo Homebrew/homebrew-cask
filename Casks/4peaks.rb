c6f36518a70749aa7592f0c396d498da51cdefd5
Bevan Kay <email@bevankay.me>
2022-01-24 10:53:23 +1100

    4peaks: add livecheck

--- Casks/4peaks.rb
+++ Casks/4peaks.rb
@@ -7,5 +7,11 @@ cask "4peaks" do
   name "4Peaks"
   homepage "https://nucleobytes.com/4peaks/index.html"
 
+  livecheck do
+    url :homepage
+    # <div class="product-version">v 1.8</div>
+    regex(/"product-version">v?\s+?(\d+(?:\.\d+)+)[\s<]/i)
+  end
+
   app "4Peaks.app"
 end
