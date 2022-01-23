452e94bf184b69e5b4ecd84fda87b631a30b87f8
tiiiecherle <tiiiecherle@gmail.com>
2022-01-23 23:04:30 +0100

    Update angry-ip-scanner from 3.8.1 to 3.8.2 (#117982)

--- Casks/angry-ip-scanner.rb
+++ Casks/angry-ip-scanner.rb
@@ -1,12 +1,12 @@
 cask "angry-ip-scanner" do
   arch = Hardware::CPU.intel? ? "X86" : "Arm64"
 
-  version "3.8.1"
+  version "3.8.2"
 
   if Hardware::CPU.intel?
-    sha256 "e60b16e124c84e2368a0bb9dd85a282a163ad1cc9946745ab14adcca5075d13f"
+    sha256 "e37228d7dc5e4766d9070bca5f0d616ae04887d7f6ee7b30cc8ea5a0190c7441"
   else
-    sha256 "b9e13383878ef7999d46b18b41d6699ce5c406af071ec849235bdd103025e3e5"
+    sha256 "bc811e6d2c2df7fd2826ba0545a5a27f53d6da1420abfb8ff5ff8e0427a9317d"
   end
 
   url "https://github.com/angryip/ipscan/releases/download/#{version}/ipscan-mac#{arch}-#{version}.zip",
