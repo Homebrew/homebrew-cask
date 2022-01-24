2d615ae3722659c6d61b4677da9778c246058024
Colin Rundel <rundel@gmail.com>
2022-01-23 16:35:55 -0800

    Initial cask for quarto (#118002)
    * Initial caskfile for quarto
    
    * Update quarto.rb
    
    * Add zap stanza
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/quarto.rb
@@ -0,0 +1,18 @@
+cask "quarto" do
+  version "0.3.34"
+  sha256 "f1a2a9d537f2352362e814b4c9b325ecea30dbfd183b5fc67bec51ea393090fe"
+
+  url "https://github.com/quarto-dev/quarto-cli/releases/download/v#{version}/quarto-#{version}-macos.pkg",
+      verified: "github.com/quarto-dev/quarto-cli/"
+  name "quarto"
+  desc "Scientific and technical publishing system built on Pandoc"
+  homepage "https://www.quarto.org/"
+
+  depends_on macos: ">= :el_capitan"
+
+  pkg "quarto-#{version}-macos.pkg"
+
+  uninstall pkgutil: "org.rstudio.quarto"
+
+  zap trash: "~/Library/Caches/quarto"
+end
