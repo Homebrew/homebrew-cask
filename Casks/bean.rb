f8172b3431aedec72695c81487558a5473d468d0
hyuraku <32809703+hyuraku@users.noreply.github.com>
2022-01-24 12:30:25 +0900

    Update bean from 3.4.5 to 3.4.6 (#118027)
    * Update bean from 3.4.5 to 3.4.6
    
    * Update bean.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/bean.rb
+++ Casks/bean.rb
@@ -1,6 +1,6 @@
 cask "bean" do
-  version "3.4.5"
-  sha256 "6c72fe309be3f1ae7985c6b75cf5ef39091abe18046c3eb7cf11884c85c4b183"
+  version "3.4.6"
+  sha256 "d966fcf0ba1de1f779e57421d819771b78d0f6ad74d4b3760fa5d0dca9e12495"
 
   url "https://www.bean-osx.com/releases/Bean-Install-#{version.dots_to_hyphens}.zip"
   name "Bean"
@@ -10,7 +10,7 @@ cask "bean" do
   livecheck do
     url :homepage
     strategy :page_match do |page|
-      v = page[%r{href=.*?/Bean-Install-(\d+(?:-\d+)*)\.zip}i, 1]
+      v = page[%r{href=.*?/Bean[._-]Install[._-]v?(\d+(?:-\d+)+)\.zip}i, 1]
       v.tr("-", ".")
     end
   end
