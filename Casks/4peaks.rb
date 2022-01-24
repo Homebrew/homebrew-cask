645ba9d2ff208292d37adc812fb6a18ac08b062a
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-24 08:32:25 +0800

    Update 4peaks.rb

--- Casks/4peaks.rb
+++ Casks/4peaks.rb
@@ -2,7 +2,7 @@ cask "4peaks" do
   version "1.8"
   sha256 :no_check
 
-  url "http://downloads.nucleobytes.com/4peaks.dmg"
+  url "https://downloads.nucleobytes.com/4peaks.dmg"
   name "4Peaks"
   desc "Visualize and edit DNA sequence trace files"
   homepage "https://nucleobytes.com/4peaks/index.html"
