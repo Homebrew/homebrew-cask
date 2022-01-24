4f201257bbceb4cb0b24a395b04ce834cd2ed7df
Bevan Kay <email@bevankay.me>
2022-01-24 11:36:58 +1100

    4peaks: add livecheck (#118012)
    * 4peaks: add livecheck
    
    * update livecheck
    
    * Update 4peaks.rb
    
    * Update 4peaks.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/4peaks.rb
+++ Casks/4peaks.rb
@@ -2,10 +2,15 @@ cask "4peaks" do
   version "1.8"
   sha256 :no_check
 
-  url "http://downloads.nucleobytes.com/4peaks.dmg"
-  appcast "https://nucleobytes.com/4peaks/index.html"
+  url "https://downloads.nucleobytes.com/4peaks.dmg"
   name "4Peaks"
+  desc "Visualize and edit DNA sequence trace files"
   homepage "https://nucleobytes.com/4peaks/index.html"
+
+  livecheck do
+    url :homepage
+    regex(/"product-version">v?\s+?(\d+(?:\.\d+)+)[\s<]/i)
+  end
 
   app "4Peaks.app"
 end
