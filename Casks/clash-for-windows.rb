7340806ac8c4d5ce1fcb533e6ace7bfc3fb97fa9
6david9 <686160+6david9@users.noreply.github.com>
2022-01-24 16:35:22 +0800

    Update clash-for-windows from 0.19.5 to 0.19.6 (#118061)

--- Casks/clash-for-windows.rb
+++ Casks/clash-for-windows.rb
@@ -1,12 +1,12 @@
 cask "clash-for-windows" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  version "0.19.5"
+  version "0.19.6"
 
   if Hardware::CPU.intel?
-    sha256 "fb2b4e0ca56e40c311b9b1094c53c9245155c4ebe94dca85ebb36ff9c0b411f2"
+    sha256 "d0d386776691b762ff2f2bf196850b2c71a2edf5ca5e349905ca568501428aeb"
   else
-    sha256 "76ea6312e43dd43b8696fe97fc21d267ae5e43a619ec642a05e571cbb8b6895a"
+    sha256 "708a641fafc0e102a3eed7cd7bea7b09686fc8e13e8a07269123dc44a6ba8639"
   end
 
   url "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/#{version}/Clash.for.Windows-#{version}#{arch}.dmg"
