1a72914dff019e4c7fa778c3df04b63a09ec79bb
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-25 13:34:29 +0800

    Update navicat-for-postgresql from 15.0.33 to 16.0.7 (#118107)
    * Update navicat-for-postgresql from 15.0.33 to 16.0.7
    
    * Update navicat-for-postgresql.rb
    
    * Update navicat-for-postgresql.rb

--- Casks/navicat-for-postgresql.rb
+++ Casks/navicat-for-postgresql.rb
@@ -1,5 +1,5 @@
 cask "navicat-for-postgresql" do
-  version "15.0.33"
+  version "16.0.7"
   sha256 :no_check
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_pgsql_en.dmg"
@@ -8,9 +8,10 @@ cask "navicat-for-postgresql" do
   homepage "https://www.navicat.com/products/navicat-for-postgresql"
 
   livecheck do
-    url "https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20for%20PostgreSQL&appLang=en"
-    strategy :sparkle
+    cask "navicat-premium"
   end
+
+  depends_on macos: ">= :mojave"
 
   app "Navicat for PostgreSQL.app"
 end
