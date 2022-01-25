c4a7776ea2c612b5863aec40da4ab11f76a8880e
Dmitry Chirkin <josser@nixsolutions.com>
2022-01-25 07:59:42 +0200

    Upgrade decrediton from 1.6.3 to 1.7.0 (#118113)
    fixed style

--- Casks/decrediton.rb
+++ Casks/decrediton.rb
@@ -1,11 +1,23 @@
 cask "decrediton" do
-  version "1.6.3"
-  sha256 "bca8e9fac99bded3cdc71bd1db1ac49c7c4a242750777189a48bd467baefbf97"
+  arch = Hardware::CPU.intel? ? "amd64" : "arm64"
 
-  url "https://github.com/decred/decred-binaries/releases/download/v#{version}/decrediton-v#{version}.dmg"
+  version "1.7.0"
+
+  if Hardware::CPU.intel?
+    sha256 "71600c35e284ae0c85b3b18de74977fbb241bb8c99346cd32987f167502d9037"
+  else
+    sha256 "28d2aedd3afe4781264acad2983ad12647b88d358d2238cc2ea2975c293397ce"
+  end
+
+  url "https://github.com/decred/decred-binaries/releases/download/v#{version}/decrediton-#{arch}-v#{version}.dmg"
   name "Decrediton"
   desc "Wallet GUI for decred autonomous digital currency"
   homepage "https://github.com/decred/decrediton"
 
   app "decrediton.app"
+
+  zap trash: [
+    "~/Library/Application Support/decrediton",
+    "~/Library/Preferences/com.Electron.Decrediton.plist",
+  ]
 end
