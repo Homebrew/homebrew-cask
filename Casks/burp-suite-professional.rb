1f779e1d4b543109d85048134769bc2ecf768f8d
daffyDukk <21338799+daffyDukk@users.noreply.github.com>
2022-01-25 15:26:59 +0000

    Added apple silicon support for burp-suite-professional (#118132)
    * Added Apple silicon support
    
    Changed the cask to check for and install hte proper version on Apple
    M1 macs
    
    * changes from the brew styles command
    
    * Added Aplle silicon support for burp-suite-professional
    
    * Update burp-suite.rb
    
    * Update burp-suite-professional.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/burp-suite-professional.rb
+++ Casks/burp-suite-professional.rb
@@ -1,8 +1,15 @@
 cask "burp-suite-professional" do
-  version "2021.12.1"
-  sha256 "f555221aeea3ed6ce2b2377cc997b352c4f4614fe897baeb1c9518a6bdd641a9"
+  arch = Hardware::CPU.intel? ? "MacOsx" : "MacOsArm64"
 
-  url "https://portswigger.net/burp/releases/download?product=pro&version=#{version}&type=MacOsx"
+  version "2021.12.1"
+
+  if Hardware::CPU.intel?
+    sha256 "f555221aeea3ed6ce2b2377cc997b352c4f4614fe897baeb1c9518a6bdd641a9"
+  else
+    sha256 "fe96d42f185372ca9e7ff1881705dce91a5b59a33de2e0ff7481734b9d42d4e7"
+  end
+
+  url "https://portswigger.net/burp/releases/download?product=pro&version=#{version}&type=#{arch}"
   name "Burp Suite Professional"
   desc "Web security testing toolkit"
   homepage "https://portswigger.net/burp/pro"
@@ -18,7 +25,7 @@ cask "burp-suite-professional" do
               item["releaseChannels"].include?("Stable") &&
               item["categories"].include?("Professional") &&
               item["builds"].any? do |build|
-                build["ProductPlatform"] == "MacOsx"
+                build["ProductPlatform"] == arch.to_s
               end
       end.compact
     end
