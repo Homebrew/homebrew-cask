3aa301acbe6ccab5e125cd309cca124c0926a837
Kreeblah <kreeblah@gmail.com>
2022-01-24 18:55:00 -0800

    rar: Update to 6.10, fix livecheck, and add arch checks (#118088)

--- Casks/rar.rb
+++ Casks/rar.rb
@@ -1,16 +1,22 @@
 cask "rar" do
-  version "6.0.2"
-  sha256 "6da67bd6f617206b36e5fecf274ba3a0652bb166519852e1bc32342a8564b6c8"
+  arch = Hardware::CPU.intel? ? "x64" : "arm"
 
-  url "https://www.rarlab.com/rar/rarosx-#{version}.tar.gz"
+  version "6.10"
+
+  if Hardware::CPU.intel?
+    sha256 "40c119a9e6e1f7883d2d3333205f1c6c928080ea57f93bf6797d5996c2131eb2"
+  else
+    sha256 "db20d61544d5a569de718eac5f652a9105579db9f48ecc4ade78556a3ac24c1d"
+  end
+
+  url "https://www.rarlab.com/rar/rarmacos-#{arch}-#{version.no_dots}.tar.gz"
   name "RAR Archiver"
   desc "Archive manager for data compression and backups"
   homepage "https://www.rarlab.com/"
 
   livecheck do
     url "https://www.rarlab.com/download.htm"
-    strategy :page_match
-    regex(%r{href=.*?/rarosx-(\d+(:?\.\d+)*)\.tar\.gz}i)
+    regex(/>s*RAR\s+v?(\d+(:?\.\d+)+)\s+for\s+macOS/i)
   end
 
   binary "rar/rar"
