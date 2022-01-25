29e4a3e9db031a526105edda1651c199cf9e4598
Clément L <porkepix@gmail.com>
2022-01-25 11:19:31 +0100

    Update bdash from 1.14.0 to 1.14.1 (#118120)

--- Casks/bdash.rb
+++ Casks/bdash.rb
@@ -1,6 +1,6 @@
 cask "bdash" do
-  version "1.14.0"
-  sha256 "9c61d5919ff4d125a1f98ac7fe64a94d44abc5af621e4584d7edc03f4474b710"
+  version "1.14.1"
+  sha256 "cc494654ecfc0a19aa7a1dca3bab40e9debf3e77a10deb5a924eaa1d078a0aee"
 
   url "https://github.com/bdash-app/bdash/releases/download/v#{version}/Bdash-#{version}-mac.zip"
   name "Bdash"
