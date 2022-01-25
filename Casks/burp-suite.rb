22eb92b26e45c63ce7814addd877e804bf5f3fd3
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-25 23:13:10 +0800

    Update burp-suite.rb

--- Casks/burp-suite.rb
+++ Casks/burp-suite.rb
@@ -1,15 +1,8 @@
 cask "burp-suite" do
   version "2021.12.1"
+  sha256 "bec817afc59faa60b20ba4761af471b0b09925dac128c30326438d13e746d2e6"
 
-  arch = Hardware::CPU.intel? ? "MacOsx" : "MacOsArm64"
-
-  if Hardware::CPU.intel?
-    sha256 "bec817afc59faa60b20ba4761af471b0b09925dac128c30326438d13e746d2e6"
-  else
-    sha256 "65d6cfed455dd5ee43512513257c2a4f8da8b6b48e1a6ff983997d26f26794c9"
-  end
-
-  url "https://portswigger.net/burp/releases/download?product=community&version=#{version}&type=#{arch}"
+  url "https://portswigger.net/burp/releases/download?product=community&version=#{version}&type=MacOsx"
   name "Burp Suite Community Edition"
   desc "Web security testing toolkit"
   homepage "https://portswigger.net/burp/"
@@ -25,11 +18,7 @@ cask "burp-suite" do
               item["releaseChannels"].include?("Stable") &&
               item["categories"].include?("Community") &&
               item["builds"].any? do |build|
-                build["ProductPlatform"] == if Hardware::CPU.intel?
-                  "MacOsx"
-                else
-                  "MacOsArm64"
-                end
+                build["ProductPlatform"] == "MacOsx"
               end
       end.compact
     end
