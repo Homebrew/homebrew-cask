0e30dba772ca54f72e31417e788dbeefd447bc34
Nick Mitchell <starpit@users.noreply.github.com>
2022-01-24 22:19:02 -0500

    bump kui from 11.0.0 to 11.1.0 (#118092)
    * bump kui from 11.0.0 to 11.1.1
    
    * Update kui.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/kui.rb
+++ Casks/kui.rb
@@ -1,23 +1,18 @@
 cask "kui" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "11.0.0"
+  version "11.1.1"
 
-  url "https://github.com/kubernetes-sigs/kui/releases/download/v#{version}/Kui-darwin-#{arch}.tar.bz2"
   if Hardware::CPU.intel?
-    sha256 "4d639c57cf682caf0ed4d3b8bd4856dfbed18ab145d18fd236c9461228824616"
+    sha256 "f15ee63ab7bf2be7b6e8de147e2f3af1881417a41c2809b1d62c93a0835bd588"
   else
-    sha256 "2ac2d9fc7e28f33155594f32308bce52c51ff27c38e8a688cc050afe6b1dbfaf"
+    sha256 "a0a1067a5e60070917caa738bf5868d3f6b72d7dcea85cd785bca2e16e70ed05"
   end
 
+  url "https://github.com/kubernetes-sigs/kui/releases/download/v#{version}/Kui-darwin-#{arch}.tar.bz2"
   name "Kui"
   desc "Hybrid command-line/UI development experience for cloud-native development"
   homepage "https://github.com/kubernetes-sigs/kui"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
 
   app "Kui-darwin-#{arch}/Kui.app"
   binary "#{appdir}/Kui.app/Contents/Resources/kubectl-kui"
