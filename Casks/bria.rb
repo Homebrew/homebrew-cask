f207dba2c7568814a1b05b6583615f0834678a85
neutric <5984479+neutric@users.noreply.github.com>
2022-01-26 07:22:57 +0100

    Update bria from 6.5.1 to 6.5.2 (#118168)
    * Update bria from 6.5.1 to 6.5.2
    
    * bria: tweak livecheck regex

--- Casks/bria.rb
+++ Casks/bria.rb
@@ -1,6 +1,6 @@
 cask "bria" do
-  version "6.5.1,108815"
-  sha256 "84c1a1cea97ce406fdb7d5b6ce0fc98c76d69a5ed4f707fa08f15b7306d5fcf5"
+  version "6.5.2,109387"
+  sha256 "0cf8ae8f637c59b0820f6ccc192d48fcf833af1ef90379850af84f834061dc8b"
 
   url "https://counterpath.s3.amazonaws.com/downloads/Bria_#{version.csv.first}_#{version.csv.second}.dmg",
       verified: "counterpath.s3.amazonaws.com/"
@@ -11,7 +11,7 @@ cask "bria" do
   livecheck do
     url "https://www.counterpath.com/Bria#{version.major}forMac"
     strategy :header_match do |headers|
-      match = headers["location"].match(%r{/Bria[._-]v?(\d+(?:\.\d+)+)[_-](\d+)\.dmg}i)
+      match = headers["location"].match(/Bria[._-]v?(\d+(?:\.\d+)+)[_-](\d+)\.dmg/i)
       next if match.blank?
 
       "#{match[1]},#{match[2]}"
