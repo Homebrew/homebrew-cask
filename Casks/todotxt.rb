e8c5c7b332f183f1696dec579637c618a76e130c
Seth Taylor <sethiuslmaximus@gmail.com>
2022-01-26 20:12:41 -0600

    add desc and zap stanzas to todotxt (#118221)

--- Casks/todotxt.rb
+++ Casks/todotxt.rb
@@ -5,7 +5,13 @@ cask "todotxt" do
   url "https://github.com/mjdescy/TodoTxtMac/releases/download/#{version}/TodoTxtMac.app.zip",
       verified: "github.com/mjdescy/TodoTxtMac/"
   name "TodoTxtMac"
+  desc "Minimalist, keyboard-driven to-do manager"
   homepage "https://mjdescy.github.io/TodoTxtMac/"
 
   app "TodoTxtMac.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.descy.michael.todotxtmac.sfl2",
+    "~/Library/Preferences/com.descy.michael.TodoTxtMac.plist",
+  ]
 end
