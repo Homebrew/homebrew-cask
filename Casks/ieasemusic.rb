85b47ad73c6df670f73c3e4a1c4a77940001cbf2
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-27 10:10:23 +0800

    Update ieasemusic.rb

--- Casks/ieasemusic.rb
+++ Casks/ieasemusic.rb
@@ -7,11 +7,6 @@ cask "ieasemusic" do
   desc "Third-party NetEase cloud music player"
   homepage "https://github.com/trazyn/ieaseMusic"
 
-  livecheck do
-    url :url
-    strategy :git
-  end
-
   auto_updates true
 
   app "ieaseMusic.app"
