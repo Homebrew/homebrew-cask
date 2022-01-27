d6ad573088bfab811c9129238ed5f56d85bdc965
neutric <5984479+neutric@users.noreply.github.com>
2022-01-27 05:56:32 +0100

    Update android-platform-tools from 31.0.3 to 32.0.0 (#118244)
    * Update android-platform-tools from 31.0.3 to 32.0.0
    
    * android-platform-tools: switch to strategy :header_match default

--- Casks/android-platform-tools.rb
+++ Casks/android-platform-tools.rb
@@ -1,8 +1,8 @@
 cask "android-platform-tools" do
-  version "31.0.3,e8b2b4cbe47c728c1e54c5f524440b52d4e1a33c"
-  sha256 "773c08cfa31cec1bb4568ce5b374366e6310a5ffc21875024604a0f65bc831b1"
+  version "32.0.0"
+  sha256 "29f0163a8f5c2cea7b3bedf3ccaa9374513a2cd653f436b0debdb7c4974ba525"
 
-  url "https://dl.google.com/android/repository/#{version.csv.second}.platform-tools_r#{version.csv.first}-darwin.zip",
+  url "https://dl.google.com/android/repository/platform-tools_r#{version}-darwin.zip",
       verified: "google.com/android/repository/"
   name "Android SDK Platform-Tools"
   desc "Android SDK component"
@@ -10,12 +10,7 @@ cask "android-platform-tools" do
 
   livecheck do
     url "https://dl.google.com/android/repository/platform-tools-latest-darwin.zip"
-    strategy :header_match do |headers|
-      match = headers["location"].match(%r{/([0-9a-f]{40,}).platform-tools_r([.0-9]+)-darwin})
-      next if match.blank?
-
-      "#{match[2]},#{match[1]}"
-    end
+    strategy :header_match
   end
 
   binary "#{staged_path}/platform-tools/adb"
