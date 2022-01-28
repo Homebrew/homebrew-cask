8992a64527acdc5cf30a823f256cf056f57e7836
Yusuf Kürşad Kaya <ykursadkaya@gmail.com>
2022-01-28 11:19:50 +0300

    Update temurin from 17.0.1 to 17.0.2 (#118301)
    * Update temurin from 17.0.1 to 17.0.2
    
    * Update temurin.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/temurin.rb
+++ Casks/temurin.rb
@@ -1,16 +1,16 @@
 cask "temurin" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
 
-  version "17.0.1,12"
+  version "17.0.2,8"
+
+  if Hardware::CPU.intel?
+    sha256 "b82d3ba3ff4c453bd954979a20e67754478294da8650e3b9e50f6d1a7cf5c628"
+  else
+    sha256 "0904c11eb5f7b53aac53e6279992ee2a900c9517b26659dee3704b2d11c149cd"
+  end
 
   url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.csv.first}%2B#{version.csv.second}/OpenJDK#{version.major}U-jdk_#{arch}_mac_hotspot_#{version.csv.first}_#{version.csv.second.major}.pkg",
       verified: "github.com/adoptium/"
-  if Hardware::CPU.intel?
-    sha256 "6318bac0268115f5740abc0113026356c3d784169ef9c5c94df2c3e3744505de"
-  else
-    sha256 "918b8d7796bf8761369a4b2d5c8c518e83eacd96347f6dc4bdd03a4792de024c"
-  end
-
   name "Eclipse Temurin Java Development Kit"
   desc "JDK from the Eclipse Foundation (Adoptium)"
   homepage "https://adoptium.net/"
