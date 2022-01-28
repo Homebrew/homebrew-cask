a8f092403690063f28bd26ada3d57b56a84f7094
Clément L <porkepix@gmail.com>
2022-01-28 12:41:17 +0100

    Update drawio from 16.4.0 to 16.4.11 (#118309)
    * Update drawio from 16.4.0 to 16.4.11
    
    * Update drawio.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/drawio.rb
+++ Casks/drawio.rb
@@ -1,16 +1,16 @@
 cask "drawio" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "16.4.0"
+  version "16.4.11"
+
+  if Hardware::CPU.intel?
+    sha256 "993c5831e83541377c5cf4de60275ec580a5e51ad7052fa64446ca85145c5723"
+  else
+    sha256 "9d232c0336866ac79c2045468b4f49344e81f11bc9504fd5ba7036482fffc4f9"
+  end
 
   url "https://github.com/jgraph/drawio-desktop/releases/download/v#{version}/draw.io-#{arch}-#{version}.dmg",
       verified: "github.com/jgraph/drawio-desktop/"
-  if Hardware::CPU.intel?
-    sha256 "489fa00cf5fa0cc7342c56fe8b2e839136757aab88e0962a4503abd2cfdab4f5"
-  else
-    sha256 "ed61f043609213ac53359d76a1dba6f5587b788e9e8b30967779712b3ec6a7e3"
-  end
-
   name "draw.io Desktop"
   desc "Draw.io is free online diagram software"
   homepage "https://www.diagrams.net/"
