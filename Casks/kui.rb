8e28ea31eea6025ed543563204399f487059b092
Nick Mitchell <starpit@users.noreply.github.com>
2022-01-28 01:25:26 -0500

    Update kui from 11.1.2 to 11.1.3 (#118280)

--- Casks/kui.rb
+++ Casks/kui.rb
@@ -1,12 +1,12 @@
 cask "kui" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "11.1.2"
+  version "11.1.3"
 
   if Hardware::CPU.intel?
-    sha256 "b7eedf22ee1ca7d9c9d371868107a311c202c0642a1c70c8dd5f1c5061a5e98e"
+    sha256 "82e1af66daaaa05d3380c6d2adc95560eb940a146b7366eacb1d3c5f7c667463"
   else
-    sha256 "2ada4297b97791618168b3b7ac8aa36dacc10151d5a4f1482950b0a5373a99c8"
+    sha256 "88bd1cffc47ad19b685ef4761c8a75b4ae145a262c8a9ea686b425c22766273c"
   end
 
   url "https://github.com/kubernetes-sigs/kui/releases/download/v#{version}/Kui-darwin-#{arch}.tar.bz2"
