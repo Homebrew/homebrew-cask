011a906da69ce8a4264d95539639593462f10983
r33drichards <57335981+r33drichards@users.noreply.github.com>
2022-01-27 19:30:31 -0800

    Update brev-cli.rb

--- Casks/brev-cli.rb
+++ Casks/brev-cli.rb
@@ -1,6 +1,10 @@
+# typed: false
+# frozen_string_literal: true
+
 cask "brev-cli" do
   version "0.6.12"
   sha256 :no_check
+
   if OS.mac?
     if Hardware::CPU.intel?
       url "https://github.com/brevdev/brev-cli/releases/download/v0.6.12/brev-cli_0.6.12_darwin_amd64.tar.gz"
@@ -22,5 +26,6 @@ cask "brev-cli" do
   name "brev-cli"
   desc "CLI tool for managing workspaces provided by brev.dev"
   homepage "https://github.com/brevdev/brev-cli/"
+
   binary "brev"
 end
