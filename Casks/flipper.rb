56d9e3e13632781fa0fb2a307f48e5239383b154
Steven Conaway <steven.conaway@icloud.com>
2022-01-29 15:10:26 -0500

    flipper: v0.131.1

--- Casks/flipper.rb
+++ Casks/flipper.rb
@@ -1,6 +1,6 @@
 cask "flipper" do
-  version "0.130.0"
-  sha256 "e359457b07ccdb08eab6868aeb89216e79f146b1addce6b079366a8e932e5268"
+  version "0.131.1"
+  sha256 "8c3a53d3d2ac7f7c3dca3d398203a3cbb91476ac3a6283ff1c05a3d2f38664ca"
 
   url "https://github.com/facebook/flipper/releases/download/v#{version}/Flipper-mac.dmg",
       verified: "github.com/facebook/flipper/"
