fb08fea42cf7bbc643bf91e7f6544a7d2e6af322
neutric <5984479+neutric@users.noreply.github.com>
2022-01-29 02:51:44 +0100

    Update clockify from 2.7.10 to 2.7.11 (#118348)

--- Casks/clockify.rb
+++ Casks/clockify.rb
@@ -1,5 +1,5 @@
 cask "clockify" do
-  version "2.7.10,350"
+  version "2.7.11,355"
   sha256 :no_check
 
   url "https://clockify-resources.s3.eu-central-1.amazonaws.com/downloads/ClockifyDesktop.zip",
