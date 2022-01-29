eba1ef057358da2f5543fbd4e90cc470d6f339b9
Clément L <porkepix@gmail.com>
2022-01-29 02:54:56 +0100

    Update drawio from 16.4.11 to 16.5.1 (#118339)

--- Casks/drawio.rb
+++ Casks/drawio.rb
@@ -1,12 +1,12 @@
 cask "drawio" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "16.4.11"
+  version "16.5.1"
 
   if Hardware::CPU.intel?
-    sha256 "993c5831e83541377c5cf4de60275ec580a5e51ad7052fa64446ca85145c5723"
+    sha256 "fc1fbda14530d4d4fc70df6884a2bf8c3d619e03b287da88f530e98b23ce2b4e"
   else
-    sha256 "9d232c0336866ac79c2045468b4f49344e81f11bc9504fd5ba7036482fffc4f9"
+    sha256 "c00999e84691401a9e721a4a1772eb8d9b4318d69847003c2578ea548c779467"
   end
 
   url "https://github.com/jgraph/drawio-desktop/releases/download/v#{version}/draw.io-#{arch}-#{version}.dmg",
