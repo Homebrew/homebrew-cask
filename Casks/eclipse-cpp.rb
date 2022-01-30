1c176eab7e9eba74ca554bc95d29e3da41e66518
Andreya <6035860+andreyakostov@users.noreply.github.com>
2022-01-31 00:59:09 +0200

    Add Apple silicon support for eclipse-cpp (#118401)
    * Add Apple silicon support for eclipse-cpp
    
    * Update eclipse-cpp.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/eclipse-cpp.rb
+++ Casks/eclipse-cpp.rb
@@ -1,8 +1,15 @@
 cask "eclipse-cpp" do
-  version "4.22.0,2021-12"
-  sha256 "7d2af4314a610793e70a0e54260fadb3aa0bd2307f8fe368e866fb22da9226cf"
+  arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
-  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-cpp-#{version.csv.second}-R-macosx-cocoa-x86_64.dmg&r=1"
+  version "4.22.0,2021-12"
+
+  if Hardware::CPU.intel?
+    sha256 "7d2af4314a610793e70a0e54260fadb3aa0bd2307f8fe368e866fb22da9226cf"
+  else
+    sha256 "867ac694f6bf8f315a846c9d95fdf2d42ac91782e89bd079fd6d2415b8b9147c"
+  end
+
+  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-cpp-#{version.csv.second}-R-macosx-cocoa-#{arch}.dmg&r=1"
   name "Eclipse IDE for C/C++ Developers"
   desc "Eclipse IDE for C and C++ developers"
   homepage "https://eclipse.org/"
