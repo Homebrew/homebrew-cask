fb0fbe6cd912e61948a8181537d02bd53ce7219b
Loïc PÉRON <44115467+nicerloop@users.noreply.github.com>
2022-01-30 11:13:14 +0100

    Update keystore-explorer from 5.5.0 to 5.5.1 (#118388)
    * Update keystore-explorer from 5.5.0 to 5.5.1
    
    * Update keystore-explorer.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/keystore-explorer.rb
+++ Casks/keystore-explorer.rb
@@ -1,10 +1,11 @@
 cask "keystore-explorer" do
-  version "5.5.0"
-  sha256 "ec50f0e15a2c3fa3077285f928cbfcf98a0dd5fdd3646b7e4bf1e79f8d0e2153"
+  version "5.5.1"
+  sha256 "0f109ca88c6a7950993efcaed50e39d8a7306075f7f5e60a2652015fc14fcefc"
 
   url "https://github.com/kaikramer/keystore-explorer/releases/download/v#{version}/kse-#{version.no_dots}.dmg",
       verified: "github.com/kaikramer/keystore-explorer/"
   name "KeyStore Explorer"
+  desc "GUI replacement for the Java command-line utilities keytool and jarsigner"
   homepage "https://keystore-explorer.org/"
 
   app "KeyStore Explorer.app"
