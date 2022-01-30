8a37ecd69887490f078f1ba4291903105d3c9e53
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-31 06:48:49 +0800

    Update eclipse-dsl.rb

--- Casks/eclipse-dsl.rb
+++ Casks/eclipse-dsl.rb
@@ -1,4 +1,6 @@
 cask "eclipse-dsl" do
+  arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
+
   version "4.22.0,2021-12"
 
   if Hardware::CPU.intel?
@@ -6,8 +8,6 @@ cask "eclipse-dsl" do
   else
     sha256 "33dc8f7bb75659dffc850790db1e9027e63adfea4cf91295918a6dcaa0819ca0"
   end
-
-  arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-dsl-#{version.csv.second}-R-macosx-cocoa-#{arch}.dmg&r=1"
   name "Eclipse IDE for Java and DSL Developers"
