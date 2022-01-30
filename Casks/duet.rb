4482b4c709aef09ef95473843c8b4e9bec757a08
Loïc PÉRON <44115467+nicerloop@users.noreply.github.com>
2022-01-30 11:15:36 +0100

    Update duet from 2.4.0.2 to 2.4.2.0 (#118387)
    * Update duet from 2.4.0.2 to 2.4.2.0
    
    * Update duet.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/duet.rb
+++ Casks/duet.rb
@@ -1,6 +1,6 @@
 cask "duet" do
-  version "2.4.0.2"
-  sha256 "1cc8df6b8eab6a6019a15225380e8b8c703e3098acdc9482a99e6284e6ad62db"
+  version "2.4.2.0"
+  sha256 "e2b1df4a1ade77bd96374f3b1ad0995dec849e5c85ffef31c43d9ed0686779e5"
 
   url "https://duet.nyc3.cdn.digitaloceanspaces.com/Mac/#{version.major_minor.dots_to_underscores}/duet-#{version.dots_to_hyphens}.zip",
       verified: "duet.nyc3.cdn.digitaloceanspaces.com/Mac/"
@@ -11,7 +11,7 @@ cask "duet" do
   livecheck do
     url "https://updates.duetdisplay.com/latestMac"
     strategy :header_match do |headers|
-      headers["location"][/-(\d+(?:-\d+)*)\.zip/i, 1].tr("-", ".")
+      headers["location"][/duet[._-]v?(\d+(?:-\d+)+)\.zip/i, 1].tr("-", ".")
     end
   end
 
