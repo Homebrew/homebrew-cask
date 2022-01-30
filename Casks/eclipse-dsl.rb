8832d7dc4387ab6bc312deacbdd01fb810e54c18
Andreya <6035860+andreyakostov@users.noreply.github.com>
2022-01-31 00:56:06 +0200

    Add Apple silicon support for eclipse-dsl (#118402)
    * Add Apple silicon support for eclipse-dsl
    
    * Update eclipse-dsl.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/eclipse-dsl.rb
+++ Casks/eclipse-dsl.rb
@@ -1,8 +1,15 @@
 cask "eclipse-dsl" do
-  version "4.22.0,2021-12"
-  sha256 "46ca25ded90512de578ba8879db1fb18a9bc98498f9b091dce84732cf7c04a78"
+  arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
-  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-dsl-#{version.csv.second}-R-macosx-cocoa-x86_64.dmg&r=1"
+  version "4.22.0,2021-12"
+
+  if Hardware::CPU.intel?
+    sha256 "46ca25ded90512de578ba8879db1fb18a9bc98498f9b091dce84732cf7c04a78"
+  else
+    sha256 "33dc8f7bb75659dffc850790db1e9027e63adfea4cf91295918a6dcaa0819ca0"
+  end
+
+  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-dsl-#{version.csv.second}-R-macosx-cocoa-#{arch}.dmg&r=1"
   name "Eclipse IDE for Java and DSL Developers"
   desc "Eclipse IDE for Java and DSL developers"
   homepage "https://eclipse.org/"
