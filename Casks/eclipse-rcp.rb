98a0aa5daa51b42b4bc0f1c3b6bb57e5ffc9cf37
Andreya <6035860+andreyakostov@users.noreply.github.com>
2022-01-31 00:51:00 +0200

    Add Apple silicon support for eclipse-rcp (#118410)
    * Add Apple silicon support for eclipse-rcp
    
    * Update eclipse-rcp.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/eclipse-rcp.rb
+++ Casks/eclipse-rcp.rb
@@ -1,8 +1,15 @@
 cask "eclipse-rcp" do
-  version "4.22.0,2021-12"
-  sha256 "e64b09c51f71327f6822faaf9e459e76a5bebf48ed599876ca55732ff33d89f7"
+  arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
-  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-rcp-#{version.csv.second}-R-macosx-cocoa-x86_64.dmg&r=1"
+  version "4.22.0,2021-12"
+
+  if Hardware::CPU.intel?
+    sha256 "e64b09c51f71327f6822faaf9e459e76a5bebf48ed599876ca55732ff33d89f7"
+  else
+    sha256 "852a41ea297428e240ff97266d2f5deff9719e2352f464a322613b17dbbc34d7"
+  end
+
+  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-rcp-#{version.csv.second}-R-macosx-cocoa-#{arch}.dmg&r=1"
   name "Eclipse for RCP and RAP Developers"
   desc "Eclipse IDE for RCP and RAP developers"
   homepage "https://eclipse.org/"
