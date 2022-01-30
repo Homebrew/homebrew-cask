ef569e5e2a18617b125dfa2cba8c5ab839ab46d9
Andreya <6035860+andreyakostov@users.noreply.github.com>
2022-01-31 00:55:33 +0200

    Add Apple silicon support for eclipse-php (#118408)
    * Add Apple silicon support for eclipse-php
    
    * Update eclipse-php.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/eclipse-php.rb
+++ Casks/eclipse-php.rb
@@ -1,8 +1,15 @@
 cask "eclipse-php" do
-  version "4.22.0,2021-12"
-  sha256 "9a6fb55f6bbc51401324241cf5598d228de4b417c12eadaa40ec85fba41164e0"
+  arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
-  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-php-#{version.csv.second}-R-macosx-cocoa-x86_64.dmg&r=1"
+  version "4.22.0,2021-12"
+
+  if Hardware::CPU.intel?
+    sha256 "9a6fb55f6bbc51401324241cf5598d228de4b417c12eadaa40ec85fba41164e0"
+  else
+    sha256 "63224429da78386946381c6cec4808d07b5485f9878246dccd9713a331012b6c"
+  end
+
+  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-php-#{version.csv.second}-R-macosx-cocoa-#{arch}.dmg&r=1"
   name "Eclipse IDE for PHP Developers"
   desc "Eclipse IDE for PHP developers"
   homepage "https://eclipse.org/"
