69708652988e1bcd56ef5d78bbee1f97da58de87
Andreya <6035860+andreyakostov@users.noreply.github.com>
2022-01-31 01:03:16 +0200

    Add Apple silicon support for eclipse-platform (#118409)
    * Add Apple silicon support for eclipse-platform
    
    * Update eclipse-platform.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/eclipse-platform.rb
+++ Casks/eclipse-platform.rb
@@ -1,8 +1,15 @@
 cask "eclipse-platform" do
-  version "4.22,202111241800"
-  sha256 "923dca09cebf4ebc97fdf7151f2dc984b8c7c425a452df7167e94e7aba473cc2"
+  arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
-  url "https://www.eclipse.org/downloads/download.php?file=/eclipse/downloads/drops#{version.major}/R-#{version.csv.first}-#{version.csv.second}/eclipse-SDK-#{version.csv.first}-macosx-cocoa-x86_64.dmg&r=1"
+  version "4.22,202111241800"
+
+  if Hardware::CPU.intel?
+    sha256 "923dca09cebf4ebc97fdf7151f2dc984b8c7c425a452df7167e94e7aba473cc2"
+  else
+    sha256 "4c9ac2e2be62776f408016e1935d80a0ca3783f1a19e4aa7f013cdb66e93fc0a"
+  end
+
+  url "https://www.eclipse.org/downloads/download.php?file=/eclipse/downloads/drops#{version.major}/R-#{version.csv.first}-#{version.csv.second}/eclipse-SDK-#{version.csv.first}-macosx-cocoa-#{arch}.dmg&r=1"
   name "Eclipse SDK"
   desc "SDK for the Eclipse IDE"
   homepage "https://eclipse.org/"
