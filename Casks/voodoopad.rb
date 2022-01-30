b7caf95345c4eafc8b83f4ca84590282e1bff781
John Benninghoff <jbenninghoff@mac.com>
2022-01-30 16:47:00 -0600

    Update voodoopad from 5.3.0 to 5.4.0 (#118406)
    * Update voodoopad from 5.3.0 to 5.4.0
    
    * Update VoodooPad homepage, desc

--- Casks/voodoopad.rb
+++ Casks/voodoopad.rb
@@ -1,11 +1,12 @@
 cask "voodoopad" do
-  version "5.3.0,6450"
-  sha256 "a2eb6a042df78170a88a9909fb40ee92bc407b4c95f414cac5b1fb8ddef390ce"
+  version "5.4.0,6608"
+  sha256 "17f9bf20ade564f59cc65f7f6ca63419635f8350d1dee7e64ca9b8ba46fd9043"
 
   url "https://voodoopad.s3.amazonaws.com/VoodooPad-#{version.csv.first}.zip",
       verified: "voodoopad.s3.amazonaws.com/"
   name "VoodooPad"
-  homepage "http://www.voodoopad.com/"
+  desc "Notes organizer"
+  homepage "https://www.voodoopad.com/"
 
   livecheck do
     url "https://www.primatelabs.com/appcast/voodoopad#{version.major}.xml"
