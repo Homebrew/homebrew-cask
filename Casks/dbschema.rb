faff250a8e0a37357428ca558dd0eed5bf39859c
neutric <5984479+neutric@users.noreply.github.com>
2022-01-31 02:03:04 +0100

    Update dbschema from 8.4.5 to 8.5.0 (#118418)
    * Update dbschema from 8.4.5 to 8.5.0
    
    * dbschema: tweak livecheck regex
    
    * update livecheck
    
    * dbschema: use sha256
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/dbschema.rb
+++ Casks/dbschema.rb
@@ -1,6 +1,6 @@
 cask "dbschema" do
-  version "8.4.5"
-  sha256 :no_check # required as upstream package is updated in-place
+  version "8.5.0"
+  sha256 "265eaee98e233ff672566c711fa43c0fae5491a55116e7644c3c734ae477dfba"
 
   url "https://www.dbschema.com/download/DbSchema_macos_#{version.dots_to_underscores}.tgz"
   name "DbSchema"
@@ -10,7 +10,9 @@ cask "dbschema" do
   livecheck do
     url "https://www.dbschema.com/download.html"
     strategy :page_match do |page|
-      v = page[%r{href=.*?/DbSchema[._-]macos[._-]v?(\d+(?:_\d+)+)\.t}i, 1]
+      v = page[/href=.*?DbSchema[._-]macos[._-]v?(\d+(?:_\d+)+)\.t/i, 1]
+      next if v.blank?
+
       v.tr("_", ".")
     end
   end
