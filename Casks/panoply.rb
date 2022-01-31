1faf7de2932401a9f1f355808d5237975b990358
Timothy Gebhard <timothygebhard@users.noreply.github.com>
2022-02-01 00:13:25 +0100

    Update panoply.rb (#118444)
    Bump version from 5.0.0 to 5.0.1

--- Casks/panoply.rb
+++ Casks/panoply.rb
@@ -1,6 +1,6 @@
 cask "panoply" do
-  version "5.0.0"
-  sha256 "6311f41e9771830789da505f095b0013bdad78ae38c8cd16ce9d5d5f05c6792f"
+  version "5.0.1"
+  sha256 "6ce5e83467c0597c060fca7c2998335da9c0a5856ff19c171c76a6eea21a8e50"
 
   url "https://www.giss.nasa.gov/tools/panoply/download/PanoplyMacOS-#{version}.dmg"
   name "Panoply netCDF, HDF and GRIB Data Viewer"
