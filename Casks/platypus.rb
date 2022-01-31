3e2eacfc005b9573100fb020af9072765737fb1f
neutric <5984479+neutric@users.noreply.github.com>
2022-01-31 22:23:53 +0100

    Revert "platypus: fix livecheck (#115047)"
    This reverts commit 961fd0a77fc5d828f2e9d8f269246a36fbc5a898.

--- Casks/platypus.rb
+++ Casks/platypus.rb
@@ -1,5 +1,5 @@
 cask "platypus" do
-  version "5.3"
+  version "5.3,1113"
   sha256 "efc66e943e6327896d0c1b82b0c1798c9ea17cffa03581e4949541c30d9833b0"
 
   url "https://sveinbjorn.org/files/software/platypus/platypus#{version.csv.first}.zip"
@@ -8,7 +8,8 @@ cask "platypus" do
   homepage "https://sveinbjorn.org/platypus"
 
   livecheck do
-    url "https://github.com/sveinbjornt/Platypus"
+    url "https://sveinbjorn.org/files/appcasts/PlatypusAppcast.xml"
+    strategy :sparkle
   end
 
   auto_updates true
@@ -16,8 +17,8 @@ cask "platypus" do
   app "Platypus.app"
 
   zap trash: [
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.sveinbjorn.platypus.sfl2",
     "~/Library/Application Support/Platypus",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.sveinbjorn.platypus.sfl2",
     "~/Library/Caches/org.sveinbjorn.Platypus",
     "~/Library/Preferences/org.sveinbjorn.Platypus.plist",
   ]
