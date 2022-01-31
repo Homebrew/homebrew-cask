079d4253c685496fd0f0515a86784c4036f1f03f
Clément L <porkepix@gmail.com>
2022-02-01 00:14:10 +0100

    Update shotcut from 21.12.24 to 22.01.30 (#118441)

--- Casks/shotcut.rb
+++ Casks/shotcut.rb
@@ -1,12 +1,12 @@
 cask "shotcut" do
   arch = Hardware::CPU.intel? ? "macos" : "macos-ARM64"
 
-  version "21.12.24"
+  version "22.01.30"
 
   if Hardware::CPU.intel?
-    sha256 "994586b634078af0ffd7d946dfe00167120f942f87d39019b48071aad3a06833"
+    sha256 "ec4346088a1049aa30b95ebce31f194d7054484afbd6b53db1932228112e0c5e"
   else
-    sha256 "98eb1c0b529024810dabd6097d6051683bee70c01962dfba23905c79ab0f4dff"
+    sha256 "140eb77224998f85e52358399d4ec39d427bf3bc03a1dc2034415c48396df3b9"
   end
 
   url "https://github.com/mltframework/shotcut/releases/download/v#{version}/shotcut-#{arch}-#{version.no_dots}.dmg",
