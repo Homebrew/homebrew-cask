89dba57a632922ac39fb1c3452e559615cdf949f
eloyaldea <35566945+eloyaldea@users.noreply.github.com>
2022-02-01 00:08:51 +0100

    Update routeconverter.rb

--- Casks/routeconverter.rb
+++ Casks/routeconverter.rb
@@ -3,13 +3,11 @@ cask "routeconverter" do
   sha256 :no_check
 
   url "https://static.routeconverter.com/download/RouteConverterMac.app.zip"
-  appcast "https://static.routeconverter.com/download/previous-releases/",
-          must_contain: version.major_minor
   name "RouteConverter"
-  desc "RouteConverter is a free, user friendly GPS tool to display, edit, enrich and convert routes, tracks and waypoints."
+  desc "A free, user friendly GPS tool to display, edit, enrich and convert routes, tracks and waypoints."
   homepage "https://www.routeconverter.com/"
 
   auto_updates true
 
-  app "RouteConverterMacOpenSource.app"
+  app "RouteConverterMac.app"
 end
