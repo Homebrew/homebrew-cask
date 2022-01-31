b20656d790754634226b623abe5ddd300ae63acf
neutric <5984479+neutric@users.noreply.github.com>
2022-01-31 01:57:27 +0100

    dbschema: use sha256

--- Casks/dbschema.rb
+++ Casks/dbschema.rb
@@ -1,6 +1,6 @@
 cask "dbschema" do
   version "8.5.0"
-  sha256 :no_check # required as upstream package is updated in-place
+  sha256 "265eaee98e233ff672566c711fa43c0fae5491a55116e7644c3c734ae477dfba"
 
   url "https://www.dbschema.com/download/DbSchema_macos_#{version.dots_to_underscores}.tgz"
   name "DbSchema"
@@ -12,7 +12,7 @@ cask "dbschema" do
     strategy :page_match do |page|
       v = page[/href=.*?DbSchema[._-]macos[._-]v?(\d+(?:_\d+)+)\.t/i, 1]
       next if v.blank?
-      
+
       v.tr("_", ".")
     end
   end
