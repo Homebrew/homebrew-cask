75b80ce61db3aae5bc9ad2985fdd8aab037ecb32
Matt Coster <opensource@mtcoster.net>
2022-02-01 15:08:24 +0000

    Update ableton-live from 11.0.12 to 11.1
    Includes:
     * ableton-live-intro
     * ableton-live-lite
     * ableton-live-standard
     * ableton-live-suite

--- Casks/ableton-live-standard.rb
+++ Casks/ableton-live-standard.rb
@@ -1,8 +1,14 @@
 cask "ableton-live-standard" do
-  version "11.0.12"
-  sha256 "5195d68cbc0fc926bfd8c293d1bee7566eac4ae5b4af1fbb195e84945dcea838"
+  arch = MacOS.version >= :mojave ? "universal" : "intel"
+  version "11.1"
 
-  url "https://cdn-downloads.ableton.com/channels/#{version}/ableton_live_standard_#{version}_64.dmg"
+  if MacOS.version >= :mojave
+    sha256 "0f7589cbca877b0ba7233c36c0c8a0f1b6360221fe9cc2da3ad2425a1bebee59"
+  else
+    sha256 "1da2aeb846cd75c7ee7a98a62b61eaf6cf4f8adddd1dec0b9abf392b4ab23f44"
+  end
+
+  url "https://cdn-downloads.ableton.com/channels/#{version}/ableton_live_standard_#{version}_#{arch}.dmg"
   name "Ableton Live Standard"
   desc "Sound and music editor"
   homepage "https://www.ableton.com/en/live/"
