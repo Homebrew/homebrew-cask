9335417229240502eee7a9f3cc752f6d1e9cdd5c
Mihail Yaremenko <m50421@outlook.com>
2022-02-01 19:16:45 +0200

    Update Microsoft Office apps casks to support older macOS versions (#118276)
    * Update Microsoft-outlook to load older versions on older versions of macOS
    
    * Update microsoft-auto-update.rb
    
    * Update microsoft-outlook.rb
    
    * Cleanup microsoft-outlook.rb
    
    * Add shasum of MAU, cleanup
    
    * Update Microsoft Excel cask to load older versions on old macOS versions
    
    * Update Microsoft-word to load older version on older macOS versions
    
    * Add sha256 for 16.54
    
    * Update powerpoint to load older versions on old macOS versions

--- Casks/microsoft-powerpoint.rb
+++ Casks/microsoft-powerpoint.rb
@@ -1,6 +1,20 @@
 cask "microsoft-powerpoint" do
-  version "16.57.22011101"
-  sha256 "0071a45a7855e0f7053060719403c665ae31c4f1331019fab91970d0c78af50f"
+  if MacOS.version <= :el_capitan
+    version "16.16.20101200"
+    sha256 "0c898068408082124f7fe45717e3fb4b4f5647b609b54dc5fa6c90e295f499c3"
+  elsif MacOS.version <= :sierra
+    version "16.30.19101301"
+    sha256 "d0b16f96bb390a225c52808952a66f0e02bf3f355234cbe733b250d37bb44c72"
+  elsif MacOS.version <= :high_sierra
+    version "16.43.20110804"
+    sha256 "a89e0aed18e5b1e56293b1f9eaccc3e3f5089eb37a9eec64bb6f3a3fa90587eb"
+  elsif MacOS.version <= :mojave
+    version "16.54.21101001"
+    sha256 "75a57c82b46d0e2558c454f19610576b7a48baf1ccc5cd1fa61b69cca5bf0bd1"
+  else
+    version "16.57.22011101"
+    sha256 "0071a45a7855e0f7053060719403c665ae31c4f1331019fab91970d0c78af50f"
+  end
 
   url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_PowerPoint_#{version}_Installer.pkg",
       verified: "officecdnmac.microsoft.com/"
@@ -16,7 +30,7 @@ cask "microsoft-powerpoint" do
   auto_updates true
   conflicts_with cask: "microsoft-office"
   depends_on cask: "microsoft-auto-update"
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :yosemite"
 
   pkg "Microsoft_PowerPoint_#{version}_Installer.pkg",
       choices: [
