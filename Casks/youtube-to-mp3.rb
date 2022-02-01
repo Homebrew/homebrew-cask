12ebe76502892943e5d473c98c0c56556bc41e9f
kvorobyov <vorobyov@gmail.com>
2022-02-01 17:56:35 +0600

    Update YouTube to MP3 v.3.9.9.68 and links (#118506)
    their main website is having connection issues so they moved to a mirror website until the thing get resolved

--- Casks/youtube-to-mp3.rb
+++ Casks/youtube-to-mp3.rb
@@ -1,11 +1,11 @@
 cask "youtube-to-mp3" do
-  version "3.9.9.66"
+  version "3.9.9.68"
   sha256 :no_check
 
-  url "https://www.mediahuman.com/files/YouTubeToMP3.dmg"
+  url "https://www.mediahuman.net/files/YouTubeToMP3.dmg"
   name "MediaHuman YouTube to MP3 Converter"
   desc "Downloads music from playlists or channels"
-  homepage "https://www.mediahuman.com/youtube-to-mp3/"
+  homepage "https://www.mediahuman.net/youtube-to-mp3/"
 
   livecheck do
     url :homepage
