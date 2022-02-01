f7c8ef4ea110224a37ba71572ef54ae2a94852b4
singularitti <singularitti@outlook.com>
2022-01-31 20:20:08 -0500

    Update lingon-x from 8.4.1 to 8.4.2

--- Casks/lingon-x.rb
+++ Casks/lingon-x.rb
@@ -3,8 +3,8 @@ cask "lingon-x" do
     version "6.6.5"
     sha256 "b0231b1a98dcc8f5c4234b419c9f5331407b8cce29b33f0ea2e32b12595adfa8"
   else
-    version "8.4.1"
-    sha256 "1f9d8e1ddde9fe231bac021d9bc4310e9f43971be09ac6499b37dd22ec5b3e52"
+    version "8.4.2"
+    sha256 "6ce95055ec7adb678e75b740799668add19816a004e27625361c4b0c1f3ae981"
   end
 
   url "https://www.peterborgapps.com/downloads/LingonX#{version.major}.zip"
