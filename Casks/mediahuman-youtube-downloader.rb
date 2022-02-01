0dd40cff10698d5963f29432137a95fd3f97b725
kvorobyov <vorobyov@gmail.com>
2022-02-01 17:56:54 +0600

    Update YouTube Downloader v3.9.9.68 and links (#118507)
    their main website is having connection issues so they have moved to a mirror website until the things get resolved

--- Casks/mediahuman-youtube-downloader.rb
+++ Casks/mediahuman-youtube-downloader.rb
@@ -1,11 +1,11 @@
 cask "mediahuman-youtube-downloader" do
-  version "3.9.9.64"
+  version "3.9.9.68"
   sha256 :no_check
 
-  url "https://www.mediahuman.com/files/YouTubeDownloader.dmg"
+  url "https://www.mediahuman.net/files/YouTubeDownloader.dmg"
   name "MediaHuman YouTube Downloader"
   desc "YouTube videos downloader"
-  homepage "https://www.mediahuman.com/youtube-video-downloader/"
+  homepage "https://www.mediahuman.net/youtube-video-downloader/"
 
   livecheck do
     url :homepage
