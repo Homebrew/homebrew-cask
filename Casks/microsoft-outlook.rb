d35a7bebd2657a46ae6f41d162c6976d482c575f
Mihail Yaremenko <m50421@outlook.com>
2022-02-01 19:16:45 +0200

    Update Microsoft Office apps casks to support older macOS versions (#118276)
    * Update Microsoft-outlook to load older versions on older versions of macOS
    
    * Update microsoft-auto-update.rb
    
    * Update microsoft-outlook.rb
    
    * Cleanup microsoft-outlook.rb
    
    * Add shasum of MAU, cleanup
    
    * Update Microsoft Excel cask to load older versions on old macOS versions
    
    * Update Microsoft-word to load older version on older macOS versions
    
    * Add sha256 for 16.54
    
    * Update powerpoint to load older versions on old macOS versions

--- Casks/microsoft-outlook.rb
+++ Casks/microsoft-outlook.rb
@@ -1,6 +1,20 @@
 cask "microsoft-outlook" do
-  version "16.57.22011101"
-  sha256 "63dd37ee10f934abf8ee08f0baad986b9a92d3fc9d9435e4b40d20ca413d39d3"
+  if MacOS.version <= :el_capitan
+    version "16.16.20101200"
+    sha256 "aafedfe466b7bf10f96fdfbf6b0f9bcf84e94a5097e5fccb3740d3d0cc666f26"
+  elsif MacOS.version <= :sierra
+    version "16.30.19101301"
+    sha256 "a131eb9ea7d0f498376f678198b27eab3139ec264a3a4d873be522ec8fe48845"
+  elsif MacOS.version <= :high_sierra
+    version "16.43.20110804"
+    sha256 "0e53acefafc25d1eebbf257f343de0d0a5258099c154f7ba5d99aa709fb50d08"
+  elsif MacOS.version <= :mojave
+    version "16.54.21101001"
+    sha256 "c7b3ced52462b611a9762941088fa05e42d79b26349ca62b705a9bcbce00b41e"
+  else
+    version "16.57.22011101"
+    sha256 "63dd37ee10f934abf8ee08f0baad986b9a92d3fc9d9435e4b40d20ca413d39d3"
+  end
 
   url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Outlook_#{version}_Installer.pkg",
       verified: "officecdnmac.microsoft.com/"
@@ -16,7 +30,7 @@ cask "microsoft-outlook" do
   auto_updates true
   conflicts_with cask: "microsoft-office"
   depends_on cask: "microsoft-auto-update"
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :yosemite"
 
   pkg "Microsoft_Outlook_#{version}_Installer.pkg",
       choices: [
