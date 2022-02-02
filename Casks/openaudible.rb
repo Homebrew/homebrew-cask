767f23d5f513c46bd17325da4d5aad14e99e2e94
Matt Henkel <guildencrantz@menagerie.cc>
2022-02-02 11:55:26 -0700

    Update OpenAudible from 3.3 -> 3.4.1
    This includes adding support for aarch64

--- Casks/openaudible.rb
+++ Casks/openaudible.rb
@@ -1,6 +1,13 @@
 cask "openaudible" do
-  version "3.3"
-  sha256 "4c59d640368b605f5abe2ef907bfe2faa3d69e64ae5b615c9f25c6e8be614c89"
+  arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
+
+  version "3.4.1"
+
+  if Hardware::CPU.intel?
+    sha256 "27a6785dbef62f16bfe20894fe3c09f1ae11f84ef75113a36ded9d074fc1e9b4"
+  else
+    sha256 "b7389a6f1f6cae5a72e76f3e5598cbd3b048fdfdec9b5fea03738fe1a3b59e8a"
+  end
 
   url "https://github.com/openaudible/openaudible/releases/download/v#{version}/OpenAudible_#{version}.dmg",
       verified: "github.com/openaudible/"
