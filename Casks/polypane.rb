ba608a1930046b93852be85cb815bbcb31074c4c
Miccal Matthews <miccal.matthews@gmail.com>
2022-02-02 11:10:27 +0800

    Update polypane.rb

--- Casks/polypane.rb
+++ Casks/polypane.rb
@@ -1,9 +1,16 @@
 cask "polypane" do
-  version "8.0.0"
-  sha256 "4faac4a9b9fbb8c3805ee890809394fe92ee9aed326cf98557b33803fd83fc92"
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  url "https://github.com/firstversionist/polypane/releases/download/v#{version}/Polypane-#{version}-universal.dmg",
-      verified: "github.com/firstversionist/polypane"
+  version "8.0.0"
+
+  if Hardware::CPU.intel?
+    sha256 "5c8cfa59f24e9fc8e3c54b43a2fca62644fadc6a6b7fa34ebaa986ee8cc5babc"
+  else
+    sha256 "f6626a6777cdf0e0e7e34f30521ad54fadd6393f73fd3d04fe971ba3d8103f18"
+  end
+
+  url "https://github.com/firstversionist/polypane/releases/download/v#{version}/Polypane-v#{version}#{arch}.dmg",
+      verified: "github.com/firstversionist/polypane/"
   name "Polypane"
   desc "Browser for ambitious developers"
   homepage "https://polypane.app/"
@@ -12,8 +19,8 @@ cask "polypane" do
 
   zap trash: [
     "~/Library/Application Support/Polypane",
-    "~/Library/Caches/com.firstversionist.polypane",
     "~/Library/Caches/com.firstversionist.polypane.ShipIt",
+    "~/Library/Caches/com.firstversionist.polypane",
     "~/Library/Logs/Polypane",
     "~/Library/Preferences/com.firstversionist.polypane.plist",
     "~/Library/Saved Application State/com.firstversionist.polypane.savedState",
