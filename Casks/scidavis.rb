2337025369f0ce42f0fb0c75d907202b7f0dad4f
Clément L <porkepix@gmail.com>
2022-02-03 01:41:52 +0100

    Update scidavis from 1.26 to 2.7 (#118560)
    * Update scidavis from 1.26 to 2.7
    
    * Update scidavis.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/scidavis.rb
+++ Casks/scidavis.rb
@@ -1,15 +1,16 @@
 cask "scidavis" do
-  version "1.26"
-  sha256 "bc49df3fc712cdcfee7804bf5e6b6e3d378e4178e07e6f664e05795b93829a1b"
+  version "2.7"
+  sha256 "d248cd2aea443ada2f450e952527edef7ba8cd3aa1d01250ee706f682180f79c"
 
   url "https://downloads.sourceforge.net/scidavis/scidavis-#{version}-mac-dist.dmg",
       verified: "downloads.sourceforge.net/scidavis/"
-  name "scidavis"
+  name "SciDAVis"
+  desc "Application for scientific data analysis and visualization"
   homepage "https://scidavis.sourceforge.io/"
 
   livecheck do
     url "https://sourceforge.net/projects/scidavis/rss.xml"
-    regex(%r{/scidavis-(\d+(?:\.\d+)*)-mac-dist\.dmg}i)
+    regex(%r{/scidavis[._-]v?(\d+(?:\.\d+)+)[._-]mac[._-]dist\.dmg}i)
   end
 
   depends_on macos: ">= :yosemite"
