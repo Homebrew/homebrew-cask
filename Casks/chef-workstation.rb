0ad9e30f41d27185ded020bb5c007f1dac14f465
Vikram Karve <vikram.karve@progress.com>
2022-02-03 16:35:50 +0530

    define variable per architecture for easy find-replace
    Signed-off-by: Vikram Karve <vikram.karve@progress.com>

--- Casks/chef-workstation.rb
+++ Casks/chef-workstation.rb
@@ -2,12 +2,15 @@ cask "chef-workstation" do
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
   macos_version = Hardware::CPU.intel? ? "10.15" : "11"
 
+  sha256_intel = "b957dca3a7be28373a718e95f88290c5be928985ac08b37b8c83b1ac1330d4db"
+  sha256_arm = "728ff9458a7f73618a9a34ccd5cb41962ce061f2d67ef2e58e19dd0251c528a5"
+
   version "22.1.778"
 
   if Hardware::CPU.intel?
-    sha256 "b957dca3a7be28373a718e95f88290c5be928985ac08b37b8c83b1ac1330d4db"
+    sha256 sha256_intel.to_s
   else
-    sha256 "728ff9458a7f73618a9a34ccd5cb41962ce061f2d67ef2e58e19dd0251c528a5"
+    sha256 sha256_arm.to_s
   end
 
   url "https://packages.chef.io/files/stable/chef-workstation/#{version}/mac_os_x/#{macos_version}/chef-workstation-#{version}-1.#{arch}.dmg"
