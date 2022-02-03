6249a77485514c2d7f0d2435d21726f496bfe319
Clément L <porkepix@gmail.com>
2022-02-03 15:14:41 +0100

    Update trilium-notes from 0.49.5 to 0.50.1 (#118609)

--- Casks/trilium-notes.rb
+++ Casks/trilium-notes.rb
@@ -1,6 +1,6 @@
 cask "trilium-notes" do
-  version "0.49.5"
-  sha256 "748a18a8d0fdf218f5be93639c482d0cea974bfa70f96b9955d6e51ed7fe63c6"
+  version "0.50.1"
+  sha256 "1915fec61289550b00567b70e3f7cd4e6120fa50577f8f73e6d424b320f5b686"
 
   url "https://github.com/zadam/trilium/releases/download/v#{version}/trilium-mac-x64-#{version}.zip"
   name "Trilium Notes"
