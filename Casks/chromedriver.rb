898f52bd8a4d01801d53ec0f35d13287d2323ec6
Natasha Dowse <dowsena@gmail.com>
2022-02-03 21:20:40 +1300

    Update chromedriver from 97.0.4692.71 to 98.0.4758.48

--- Casks/chromedriver.rb
+++ Casks/chromedriver.rb
@@ -1,12 +1,12 @@
 cask "chromedriver" do
   arch = Hardware::CPU.intel? ? "mac64" : "mac64_m1"
 
-  version "97.0.4692.71"
+  version "98.0.4758.48"
 
   if Hardware::CPU.intel?
-    sha256 "7ae949b20216aa9dda278564d877d2c815ded013442cf0b64f429d1676b361ff"
+    sha256 "02cd8dee4f7089ebefef017df32453d9194a1b6de00aa7dcb01e1bebb0dea526"
   else
-    sha256 "4af03d3ea3b96dfa4281916b3aa0682dd92f2a938e7bf0f00a7e70997339f64b"
+    sha256 "713777f69a4fa109902c7443b5f96012c2c7d954fd454c186de8abc8535d9612"
   end
 
   url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_#{arch}.zip",
