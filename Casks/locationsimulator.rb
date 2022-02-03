07c99a1c263b7443ab81e4c3164c9f81b4a3ac36
Schlaubischlump <Schlaubischlump@users.noreply.github.com>
2022-02-03 18:01:41 +0100

    Fix description

--- Casks/locationsimulator.rb
+++ Casks/locationsimulator.rb
@@ -4,7 +4,7 @@ cask "locationsimulator" do
 
   url "https://github.com/Schlaubischlump/LocationSimulator/releases/download/v#{version}/LocationSimulator.app.zip"
   name "LocationSimulator"
-  desc "MacOS 10.15.X / 11.X / 12.X application to spoof your iOS / iPadOS or iPhoneSimulator device location."
+  desc "Application to spoof your iOS / iPadOS or iPhoneSimulator device location."
   homepage "https://github.com/Schlaubischlump/LocationSimulator"
 
   depends_on macos: ">= :catalina"
