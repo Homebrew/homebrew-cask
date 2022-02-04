b7ca1f524ed254f037e46dd75ac7908478b6ae5c
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2022-02-04 07:58:22 +0500

    Update Framer to 2022.5.0 (#118653)

--- Casks/framer.rb
+++ Casks/framer.rb
@@ -1,7 +1,7 @@
 cask "framer" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "2021.46.1"
+  version "2022.5.0"
   sha256 :no_check
 
   url "https://updates.framer.com/electron/darwin/#{arch}/Framer.zip"
