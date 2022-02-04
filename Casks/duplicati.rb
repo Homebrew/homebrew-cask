1f9633fd9d2e9b320a34f275ab5413bedf8b03af
Paul DelRe <pdelre@gmail.com>
2022-02-04 09:55:57 -0500

    update `version`, `url`, and checksum to latest stable release

--- Casks/duplicati.rb
+++ Casks/duplicati.rb
@@ -1,9 +1,8 @@
 cask "duplicati" do
-  version "2.0.6.100,canary,2021-08-11"
-  sha256 "434cdedbd15b3562cf943b55114515b7b00bdaa1cccf06ea22b9f625bd3f0a02"
+  version "2.0.6.3_beta_2021-06-17"
+  sha256 "7a26fd69b7016e88a23ff03474eb78e174da463c4967b90c0b54f07a94027e18"
 
-  url "https://github.com/duplicati/duplicati/releases/download/v#{version.csv.first}-#{version.csv.first}_#{version.csv.second}_#{version.csv.third}/duplicati-#{version.csv.first}_#{version.csv.second}_#{version.csv.third}.dmg",
-      verified: "github.com/duplicati/duplicati/"
+  url "https://updates.duplicati.com/beta/duplicati-#{version}.dmg"
   name "Duplicati"
   desc "Store securely encrypted backups in the cloud"
   homepage "https://www.duplicati.com/"
