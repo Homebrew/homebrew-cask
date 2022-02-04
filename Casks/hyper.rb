bcb63e4fb57dc86ca6ac54fa62e60fde4fd576ca
Clément L <porkepix@gmail.com>
2022-02-04 18:02:05 +0100

    Update hyper from 3.1.5 to 3.2.0 (#118666)
    * Update hyper from 3.1.5 to 3.2.0
    
    * Update hyper.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/hyper.rb
+++ Casks/hyper.rb
@@ -1,16 +1,16 @@
 cask "hyper" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "3.1.5"
+  version "3.2.0"
+
+  if Hardware::CPU.intel?
+    sha256 "00d801a79b52a6955b437c7b5052fec99ecca05528aafd3aee71a9191224613a"
+  else
+    sha256 "f05a98816960f49d9674422cbf649a123f10fbb2e3a570d0c46ff2f4d5db3f96"
+  end
 
   url "https://github.com/vercel/hyper/releases/download/v#{version}/Hyper-#{version}-mac-#{arch}.zip",
       verified: "github.com/vercel/hyper/"
-  if Hardware::CPU.intel?
-    sha256 "3d87e611190678a4b7240c6f4e373f8dffe6b838f0fb235b33be134931e87da0"
-  else
-    sha256 "ab7dd0833e87311fc53fe32390377cbbf99387e3668b18c77082aa58da1a89fd"
-  end
-
   name "Hyper"
   desc "Terminal built on web technologies"
   homepage "https://hyper.is/"
@@ -27,12 +27,12 @@ cask "hyper" do
   binary "#{appdir}/Hyper.app/Contents/Resources/bin/hyper"
 
   zap trash: [
-    "~/.hyper.js",
     "~/.hyper_plugins",
+    "~/.hyper.js",
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/co.zeit.hyper.sfl*",
     "~/Library/Application Support/Hyper",
-    "~/Library/Caches/co.zeit.hyper",
     "~/Library/Caches/co.zeit.hyper.ShipIt",
+    "~/Library/Caches/co.zeit.hyper",
     "~/Library/Cookies/co.zeit.hyper.binarycookies",
     "~/Library/Logs/Hyper",
     "~/Library/Preferences/ByHost/co.zeit.hyper.ShipIt.*.plist",
