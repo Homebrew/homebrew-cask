022ff70d0dea4e10342be4eed8e6f45a68e905b4
Saurabh Kumar <557509+sa1@users.noreply.github.com>
2022-02-03 21:59:15 -0500

    Update beeper from 2.18.17 to 2.19.15 (#118642)

--- Casks/beeper.rb
+++ Casks/beeper.rb
@@ -1,7 +1,7 @@
 cask "beeper" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "2.18.17"
+  version "2.19.15"
   sha256 :no_check
 
   url "https://download.beeper.com/mac/dmg/#{arch}"
