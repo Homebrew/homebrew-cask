a047f9119584baaa0a92af46bcd4c116aadd67c2
Miccal Matthews <miccal.matthews@gmail.com>
2022-02-04 11:25:18 +0800

    Update arctype.rb

--- Casks/arctype.rb
+++ Casks/arctype.rb
@@ -2,7 +2,8 @@ cask "arctype" do
   version "0.9.45"
   sha256 "304aa13eac8b6c336d9715c5c3e629614a8ffb886a291d6972e0b2c6a18c0594"
 
-  url "https://arctype-downloads.s3-us-west-2.amazonaws.com/updates/Arctype-#{version}.dmg"
+  url "https://arctype-downloads.s3-us-west-2.amazonaws.com/updates/Arctype-#{version}.dmg",
+      verified: "arctype-downloads.s3-us-west-2.amazonaws.com/updates/"
   name "Arctype"
   desc "SQL client and database management tool"
   homepage "https://arctype.com/"
