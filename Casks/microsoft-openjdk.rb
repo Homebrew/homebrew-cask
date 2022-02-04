e78ed3eb90c2a5194bb68edfde0ecd20d61bb1e4
Derek Keeler <d3r3kk@users.noreply.github.com>
2022-02-03 19:18:13 -0800

    Update Microsoft build of OpenJDK to Jan-PSU 17.0.2.8.1 (#118613)
    * Update Microsoft build of OpenJDK to Jan-PSU 17.0.2.8.1
    
    * Style fix (whitespace)
    
    * Update microsoft-openjdk.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/microsoft-openjdk.rb
+++ Casks/microsoft-openjdk.rb
@@ -1,15 +1,15 @@
 cask "microsoft-openjdk" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
 
-  version "17.0.1.12.1"
+  version "17.0.2.8.1"
 
   if Hardware::CPU.intel?
-    sha256 "f39e87d5c76c7e108e8e24645f294b5921dd5a431c8a62a58578dd83dfcb8cae"
+    sha256 "1b83dc134c06190b262e3a19cfc21bd2e471ed2fc6fae84e838b0bc82ec21313"
   else
-    sha256 "d149d45d0c1e149c878de231d2c37110876f4501816195e23aec132fa26e5536"
+    sha256 "de47aaecdc8362e59345d27777cc443e3d011f2758aae20c4f5a9289f9b015a4"
   end
 
-  url "https://aka.ms/download-jdk/microsoft-jdk-#{version}-macOS-#{arch}.pkg",
+  url "https://aka.ms/download-jdk/microsoft-jdk-#{version}-macos-#{arch}.pkg",
       verified: "aka.ms/download-jdk/"
   name "Microsoft Build of OpenJDK"
   desc "OpenJDK distribution from Microsoft"
