52bd16b46b5f7363f633668c52587afd1e41646f
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2022-02-04 05:46:12 +0500

    fix: fix lint errors

--- Casks/arctype.rb
+++ Casks/arctype.rb
@@ -6,14 +6,14 @@ cask "arctype" do
   name "arctype"
   desc "SQL client and database management tool that's fun to use"
   homepage "https://arctype.com/"
-
-  auto_updates true
-  depends_on macos: ">= :high_sierra"
   
   livecheck do
     url "https://arctype-downloads.s3.amazonaws.com/updates/latest-mac.yml"
     strategy :electron_builder
   end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
 
   app "Arctype.app"
 
@@ -21,6 +21,6 @@ cask "arctype" do
     "~/Library/Application Support/Arctype",
     "~/Library/Logs/Arctype",
     "~/Library/Preferences/com.arctype.sql.plist",
-    "~/Library/Saved Application State/com.arctype.sql.savedState",
+    "~/Library/Saved Application State/com.arctype.sql.savedState"
   ]
 end
