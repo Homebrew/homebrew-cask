468484edbe269877998b8183d953646952392203
Risang Baskoro <risangbaskoro@gmail.com>
2022-02-04 09:58:04 +0700

    Update vscodium from 1.63.2 to 1.64.0 (#118654)

--- Casks/vscodium.rb
+++ Casks/vscodium.rb
@@ -1,6 +1,6 @@
 cask "vscodium" do
-  version "1.63.2"
-  sha256 "a126a7cea58e34ca05fb99611f25687dfdd114720b129089d1f905ce87267dd8"
+  version "1.64.0"
+  sha256 "f4d970febb6f877742dc827c6e6ed0cd0ce3b704e9d67331ac1b01ab37c2b67b"
 
   url "https://github.com/VSCodium/vscodium/releases/download/#{version}/VSCodium.x64.#{version}.dmg"
   name "VSCodium"
