07a06a1e0a67d1901d00d82822f758dc53ff1918
Paul Mucur <mudge@mudge.name>
2022-02-04 16:56:20 +0000

    Update raspberry-pi-imager from 1.7 to 1.7.1 (#118665)

--- Casks/raspberry-pi-imager.rb
+++ Casks/raspberry-pi-imager.rb
@@ -1,6 +1,6 @@
 cask "raspberry-pi-imager" do
-  version "1.7"
-  sha256 "4014abb4a9ab26473eaba4e1b4d71bd48c677f70f26fa5ba5f04ae9938a62438"
+  version "1.7.1"
+  sha256 "11e5e7d5f92727db988937d58f7d755fee441d8b9e715519d0be0ee241c8b075"
 
   url "https://downloads.raspberrypi.org/imager/imager_#{version}.dmg"
   name "Raspberry Pi Imager"
