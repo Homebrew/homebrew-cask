7325ed18d191c31421d12f9a7a0bdd567c41147c
Max Beckenbauer <max.bec92@gmail.com>
2022-02-04 19:40:06 +0100

    Update flutter from 2.8.1 to 2.10.0 (#118682)

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -1,6 +1,6 @@
 cask "flutter" do
-  version "2.8.1"
-  sha256 "90a9f4e05b9e7c22e6c646a2d275a5d6b48497cf7e24accbb62b0b9692687dbb"
+  version "2.10.0"
+  sha256 "f2bc398d624c24213cf02a4385cbb0755bcc5806947908e46e04a8dafca8fe58"
 
   url "https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_#{version}-stable.zip",
       verified: "storage.googleapis.com/flutter_infra_release/"
