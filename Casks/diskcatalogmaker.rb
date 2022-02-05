ec69853f8630caf1c3ea1db000252dd588bed6de
neutric <5984479+neutric@users.noreply.github.com>
2022-02-05 12:00:33 +0100

    Update diskcatalogmaker from 8.4.5 to 8.4.6 (#118696)
    * Update diskcatalogmaker from 8.4.5 to 8.4.6
    
    * diskcatalogmaker: sort zap

--- Casks/diskcatalogmaker.rb
+++ Casks/diskcatalogmaker.rb
@@ -1,6 +1,6 @@
 cask "diskcatalogmaker" do
-  version "8.4.5"
-  sha256 "1352f8244f0348f57c9c4536756e1c24578619c148e8f780aa95eeab7fedc049"
+  version "8.4.6"
+  sha256 "0e4509c9d1f94fe3d0b320d6abdc5ceed9d4d0a4d2cf3f03fbb376d90db84d7f"
 
   url "https://download.diskcatalogmaker.com/download/zip/DiskCatalogMaker#{version.no_dots}.zip"
   name "DiskCatalogMaker"
@@ -22,10 +22,10 @@ cask "diskcatalogmaker" do
   app "DiskCatalogMaker.app"
 
   zap trash: [
-    "~/Library/Application Support/DiskCatalogMaker",
     "~/Library/Application Support/com.mac.fujisoft.DiskCatalogMaker",
-    "~/Library/Caches/DiskCatalogMaker",
+    "~/Library/Application Support/DiskCatalogMaker",
     "~/Library/Caches/com.mac.fujisoft.DiskCatalogMaker",
+    "~/Library/Caches/DiskCatalogMaker",
     "~/Library/Logs/DiskCatalogMaker",
     "~/Library/Preferences/com.mac.fujisoft.DiskCatalogMaker.plist",
     "~/Library/Saved Application State/com.mac.fujisoft.DiskCatalogMaker.savedState",
