565f4f4c010c18c0c0bf5c551e21cee9b60836b3
Reno <25192197+singularitti@users.noreply.github.com>
2022-02-05 09:17:23 -0500

    Add `zap` stanza for `recents` (#118711)
    * Update recents from 2.2.1,6244 to 2.2.1,6244
    
    * Update recents.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/recents.rb
+++ Casks/recents.rb
@@ -3,11 +3,29 @@ cask "recents" do
   sha256 :no_check
 
   url "https://recentsapp.com/releases/Recents_latest.zip"
-  appcast "https://api.appcenter.ms/v0.1/public/sparkle/apps/74f5ee9e-bf2d-4be3-b92a-3e8766433b8b"
   name "Recents"
+  desc "File launcher"
   homepage "https://recentsapp.com/"
 
-  depends_on macos: ">= :high_sierra"
+  livecheck do
+    url "https://api.appcenter.ms/v0.1/public/sparkle/apps/74f5ee9e-bf2d-4be3-b92a-3e8766433b8b"
+    strategy :page_match do |page|
+      match = page.match(/Version\s(\d+(?:\.\d+)+)\s\((\d*)\)/i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
+  end
+
+  depends_on macos: ">= :mojave"
 
   app "Recents.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.lapier.Recents",
+    "~/Library/Caches/com.lapier.Recents",
+    "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.lapier.Recents",
+    "~/Library/HTTPStorages/com.lapier.Recents",
+    "~/Library/Preferences/com.lapier.Recents.plist",
+  ]
 end
