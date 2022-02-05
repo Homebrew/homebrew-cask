f9265fafb4db44e631a9c2c234fdc39e3b08b3c8
Porkepix <porkepix@gmail.com>
2022-02-05 16:28:33 +0100

    Update vnote from 3.12.0 to 3.12.888

--- Casks/vnote.rb
+++ Casks/vnote.rb
@@ -1,6 +1,6 @@
 cask "vnote" do
-  version "3.12.0"
-  sha256 "549e12eb324f854fe6c47874bd9c542220d9b62abe3964f0203dc132a961741b"
+  version "3.12.888"
+  sha256 "215909e76abb18aabdea664c58135606746e59872e6ac8d6063afe6857ea8c20"
 
   url "https://github.com/vnotex/vnote/releases/download/v#{version}/vnote-mac-x64_v#{version}.zip",
       verified: "github.com/vnotex/vnote/"
