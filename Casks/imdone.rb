d2907ab8a4cb2f22e11c7cf3e402a8fc0de17d3f
hyuraku <32809703+hyuraku@users.noreply.github.com>
2022-02-05 15:24:54 +0900

    Update imdone from 1.23.2 to 1.25.5

--- Casks/imdone.rb
+++ Casks/imdone.rb
@@ -1,6 +1,6 @@
 cask "imdone" do
-  version "1.23.2"
-  sha256 "faa0acacd7a33d8766baf03e91877e1e116cf34ace079fd59c12a8afd9135815"
+  version "1.25.5"
+  sha256 "cb0803415155161edc2cece2a898860b189bd7c277843fbd8be4eb764d0f7a39"
 
   url "https://imdone.io/downloads/imdone-#{version}.dmg"
   name "imdone"
