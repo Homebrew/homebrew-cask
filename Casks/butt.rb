f72fc7d70707048202b2350852ed293bb680d6ef
Hannes Braun <hannesbraun@mail.de>
2022-02-06 21:47:33 +0100

    Update butt from 0.1.32 to 0.1.33

--- Casks/butt.rb
+++ Casks/butt.rb
@@ -1,6 +1,6 @@
 cask "butt" do
-  version "0.1.32"
-  sha256 "8a5181b77a00d9f8f13c3b47b68d8609d7d919f44a0405c0dfdbe635f69cf3b3"
+  version "0.1.33"
+  sha256 "c003e71bd943ffbff047a605b781bf3816fdf92be006069d74e72fbc1d133f1e"
 
   url "https://downloads.sourceforge.net/butt/butt-#{version}.dmg",
       verified: "sourceforge.net/butt/"
