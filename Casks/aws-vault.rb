681037f7747c21fca3f547bab92705b0eae1d193
Moulick Aggarwal <moulickaggarwal@gmail.com>
2022-02-07 01:30:22 +0530

    Update aws-vault from 6.4.0 to 6.5.0

--- Casks/aws-vault.rb
+++ Casks/aws-vault.rb
@@ -1,13 +1,13 @@
 cask "aws-vault" do
   arch = Hardware::CPU.intel? ? "amd64" : "arm64"
 
-  version "6.4.0"
+  version "6.5.0"
 
   url "https://github.com/99designs/aws-vault/releases/download/v#{version}/aws-vault-darwin-#{arch}.dmg"
   if Hardware::CPU.intel?
-    sha256 "01161cc1ffb7ecbdf0513c68ba7a3b4e215b29eb8f3a337593282c58633615b8"
+    sha256 "14ded0a4911b23c95211b32945c4a48e7d4b71d0f0b6466902985636f6b8558d"
   else
-    sha256 "49857c8fbb57be0e5f0b4a00bc95be3b199678d1b6d0c12378e51e0a6bd24268"
+    sha256 "d1b506e7a94e68dfca3dc227b465e99f36dcf1efd88cc5a9d28839ea6d47133f"
   end
 
   name "aws-vault"
