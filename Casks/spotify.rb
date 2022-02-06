8409f69662c8e40ab50fc43e254ededd7c868eea
goodness-from-me <42966950+goodness-from-me@users.noreply.github.com>
2022-02-06 16:25:00 +0200

    Update spotify from 1.1.77.643 to 1.1.78.765 (#118740)

--- Casks/spotify.rb
+++ Casks/spotify.rb
@@ -4,7 +4,7 @@ cask "spotify" do
   if Hardware::CPU.intel?
     version "1.1.78.765,5ea20b00,16"
   else
-    version "1.1.77.643,3c4c6fc6,18"
+    version "1.1.78.765,5ea20b00,11"
   end
 
   sha256 :no_check
