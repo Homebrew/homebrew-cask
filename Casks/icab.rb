76f056986957c13214d34a89db6df8b9a2b2b8d8
Seth Taylor <sethiuslmaximus@gmail.com>
2022-02-06 08:21:56 -0600

    Update icab from 6.0.9 to 6.0.15 (#118734)

--- Casks/icab.rb
+++ Casks/icab.rb
@@ -1,6 +1,6 @@
 cask "icab" do
-  version "6.0.9"
-  sha256 "78747b9a08ab5ba6bf231e26828f7c9087797a382b76e31b6965cf86d7241f30"
+  version "6.0.15"
+  sha256 "4b2bc5388ab70988e50c23b32837442f6d3eb29cb57a48358da6d46787f162d9"
 
   url "https://icab.clauss-net.de/icab/iCab_#{version.major_minor_patch}.zip",
       verified: "icab.clauss-net.de/"
