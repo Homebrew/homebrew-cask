847d3054fff03e0c207749e0df9cc788a329573b
ZepDonald <63632671+ZepDonald@users.noreply.github.com>
2022-02-06 15:29:51 +0100

    Update ff-works from 2.6.6 to 3.0.1 (#118744)

--- Casks/ff-works.rb
+++ Casks/ff-works.rb
@@ -1,8 +1,8 @@
 cask "ff-works" do
-  version "2.6.6"
+  version "3.0.1"
   sha256 :no_check
 
-  url "https://www.ffworks.net/ffWorks.dmg"
+  url "https://www.ffworks.net/Downloads/ffWorks.dmg"
   name "ff·Works"
   desc "Video-encoding and transcoding app"
   homepage "https://www.ffworks.net/"
