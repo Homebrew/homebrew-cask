a84aed4c5d0ac160c58c406067927b6bbedb5716
neutric <5984479+neutric@users.noreply.github.com>
2022-02-06 02:59:17 +0100

    jdownloader: add livecheck

--- Casks/jdownloader.rb
+++ Casks/jdownloader.rb
@@ -1,11 +1,16 @@
 cask "jdownloader" do
-  version :latest
+  version "45328"
   sha256 :no_check
 
   url "http://installer.jdownloader.org/clean/JD2Setup.dmg",
       user_agent: :fake
   name "JDownloader"
   homepage "https://jdownloader.org/"
+
+  livecheck do
+    url "https://svn.jdownloader.org/build.php"
+    regex(/Revision:.*?(\d+)[\s<]/i)
+  end
 
   preflight do
     system_command "#{staged_path}/JDownloader Installer.app/Contents/MacOS/JavaApplicationStub",
