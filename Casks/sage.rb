a3e818618b9539543225703eda3d5cd3e6eb7407
Deirdre Connolly <durumcrustulum@gmail.com>
2022-02-05 22:31:25 -0500

    Upgrade to Sage 9.5 release, support M1  (#118683)
    * Upgrade to Sage 9.5 release
    
    * Update arm sha256
    
    * Update x64 sha256
    
    * Use git livecheck strat
    
    * Update sage.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/sage.rb
+++ Casks/sage.rb
@@ -1,17 +1,24 @@
 cask "sage" do
-  version "9.4,1.2.2"
-  sha256 "d59e16c3ec816f5b1ca0a9ad1e77fe5710d6f6d65f84495c64a0e846d9df4da8"
+  arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 
-  url "https://github.com/3-manifolds/Sage_macOS/releases/download/v#{version.csv.second}/SageMath-#{version.csv.first}%2B.dmg",
+  version "9.5,1.3"
+
+  if Hardware::CPU.intel?
+    sha256 "21606aa7476b70a01de6594e37d74625436ac6307ccafe19ff1146d2315189fd"
+  else
+    sha256 "4640428a7b41b829a1a18d6f5ecf9d22d2ae12c50d4b791fcebb532a369d70e0"
+  end
+
+  url "https://github.com/3-manifolds/Sage_macOS/releases/download/v#{version.csv.second}/SageMath-#{version.csv.first}_#{arch}.dmg",
       verified: "github.com/3-manifolds/Sage_macOS/"
   name "Sage"
   desc "Mathematics software system"
   homepage "https://www.sagemath.org/"
 
   livecheck do
-    url "https://github.com/3-manifolds/Sage_macOS/releases/latest"
+    url "https://github.com/3-manifolds/Sage_macOS/releases"
     strategy :page_match do |page|
-      match = page.match(%r{href=.*?/v?(\d+(?:\.\d+)+)/SageMath-(\d+(?:\.\d+)+)\+?\.dmg}i)
+      match = page.match(%r{href=.*?/v?(\d+(?:\.\d+)+)/SageMath[._-]v?(\d+(?:\.\d+)+)\+?[._-]#{arch}\.dmg}i)
       next if match.blank?
 
       "#{match[2]},#{match[1]}"
