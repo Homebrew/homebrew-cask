9efd119eb4e5fa34d52c61a0dcaa495e1483ef3a
Miccal Matthews <miccal.matthews@gmail.com>
2022-02-07 09:05:57 +0800

    Update openineditor-lite.rb

--- Casks/openineditor-lite.rb
+++ Casks/openineditor-lite.rb
@@ -8,8 +8,9 @@ cask "openineditor-lite" do
   homepage "https://github.com/Ji4n1ng/OpenInTerminal"
 
   livecheck do
-    url "https://raw.githubusercontent.com/Ji4n1ng/OpenInTerminal/master/Resources/README-Lite.md"
-    regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)*)}i)
+    url "https://github.com/Ji4n1ng/OpenInTerminal/releases"
+    strategy :page_match
+    regex(%r{v?(\d+(?:\.\d+)+)/OpenInEditor[._-]Lite\.app\.zip}i)
   end
 
   app "OpenInEditor-Lite.app"
