d6f3d48561197136619df3b2bcb1989f670cf045
Clément L <porkepix@gmail.com>
2022-02-07 22:36:06 +0100

    Update gramps from 5.1.4 to 5.1.5 (#118797)
    * Update gramps from 5.1.4 to 5.1.5
    
    * Update gramps.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/gramps.rb
+++ Casks/gramps.rb
@@ -1,6 +1,6 @@
 cask "gramps" do
-  version "5.1.4,5"
-  sha256 "b50494b5b851121989695c1ed41f0512de5da6e907dc94fe6cd06b95149f7ac6"
+  version "5.1.5,1"
+  sha256 "16e7416fd1aa48f4e67190123572a02f795b75e1af3b88d67b4dde9a8e1e7dfe"
 
   url "https://github.com/gramps-project/gramps/releases/download/v#{version.csv.first}/Gramps-Intel-#{version.csv.first}-#{version.csv.second}.dmg",
       verified: "github.com/gramps-project/gramps/"
@@ -11,7 +11,7 @@ cask "gramps" do
   livecheck do
     url "https://github.com/gramps-project/gramps/releases/latest"
     strategy :page_match do |page|
-      match = page.match(%r{href=.*?/Gramps-Intel-(\d+(?:.\d+)*)-(\d+)\.dmg}i)
+      match = page.match(%r{href=.*?/Gramps[._-]Intel[._-]v?(\d+(?:.\d+)+)[._-](\d+)\.dmg}i)
       next if match.blank?
 
       "#{match[1]},#{match[2]}"
