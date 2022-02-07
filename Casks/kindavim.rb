99efe5e413a1239783fa0a813402cb139abb195a
Guill Lo <guill.bout@gmail.com>
2022-02-07 21:44:39 +0800

    remove livecheck, use :latest version

--- Casks/kindavim.rb
+++ Casks/kindavim.rb
@@ -1,16 +1,11 @@
 cask "kindavim" do
-  version ":latest"
+  version :latest
   sha256 :no_check
 
   url "https://kindavim.app/releases/kindaVim.zip"
   name "kindaVim"
   desc "Use Vim in input fields and non input fields"
   homepage "https://kindavim.app/"
-
-  livecheck do
-    url "https://kindavim.app/releases/appcast.xml"
-    strategy :sparkle
-  end
 
   depends_on macos: ">= :monterey"
 
