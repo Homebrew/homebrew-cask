f88f2ec90618c260deeb65b9aa9921ad077b1e08
Miccal Matthews <miccal.matthews@gmail.com>
2022-02-07 11:20:07 +0800

    Update icab.rb

--- Casks/icab.rb
+++ Casks/icab.rb
@@ -3,9 +3,7 @@ cask "icab" do
   sha256 "4b2bc5388ab70988e50c23b32837442f6d3eb29cb57a48358da6d46787f162d9"
 
   url "https://icab.clauss-net.de/icab/iCab_#{version}.zip",
-      verified: "icab.clauss-net.de/"
-  appcast "http://www.icab.de/download.html",
-          must_contain: version.major_minor
+      verified: "icab.clauss-net.de/icab/"
   name "iCab"
   desc "Alternative web browser"
   homepage "http://www.icab.de/"
