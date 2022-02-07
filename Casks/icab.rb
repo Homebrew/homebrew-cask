89ea2a882b83612810370d2d47917ab78c82312e
Seth Taylor <sethiuslmaximus@gmail.com>
2022-02-06 21:25:17 -0600

    Update icab.rb (#118770)
    * Update icab.rb
    
    * Update icab.rb
    
    * Update icab.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/icab.rb
+++ Casks/icab.rb
@@ -2,15 +2,26 @@ cask "icab" do
   version "6.0.15"
   sha256 "4b2bc5388ab70988e50c23b32837442f6d3eb29cb57a48358da6d46787f162d9"
 
-  url "https://icab.clauss-net.de/icab/iCab_#{version.major_minor_patch}.zip",
-      verified: "icab.clauss-net.de/"
-  appcast "http://www.icab.de/download.html",
-          must_contain: version.major_minor
+  url "https://icab.clauss-net.de/icab/iCab_#{version}.zip",
+      verified: "icab.clauss-net.de/icab/"
   name "iCab"
   desc "Alternative web browser"
   homepage "http://www.icab.de/"
 
+  livecheck do
+    url "http://www.icab.de/download.html"
+    regex(/iCab\sv?(\d+(?:\.\d+)+)/i)
+  end
+
   depends_on macos: ">= :high_sierra"
 
-  app "iCab #{version.major_minor_patch}/iCab.app"
+  app "iCab #{version}/iCab.app"
+
+  zap trash: [
+    "~/Library/Application Support/iCab",
+    "~/Library/Caches/com.apple.helpd/Generated/iCab.help*#{version}",
+    "~/Library/Caches/de.icab.iCab",
+    "~/Library/Preferences/de.icab.iCab.plist",
+    "~/Library/Preferences/iCab",
+  ]
 end
