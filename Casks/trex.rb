f99a91315d6172b73aad9ca8d78f2eec590910f5
Miccal Matthews <miccal.matthews@gmail.com>
2022-02-07 09:11:33 +0800

    Update trex.rb

--- Casks/trex.rb
+++ Casks/trex.rb
@@ -3,15 +3,10 @@ cask "trex" do
   sha256 "035ffda6acc996002518f9731bb566fe3de6fb840ba0cd9a7b6722f3a9f608c2"
 
   url "https://github.com/amebalabs/TRex/releases/download/v#{version}/TRex.zip",
-      verified: "github.com/amebalabs/TRex/releases/"
+      verified: "github.com/amebalabs/TRex/"
   name "TRex"
   desc "Easy to use text extraction tool"
   homepage "https://trex.ameba.co/"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
 
   depends_on macos: ">= :big_sur"
 
