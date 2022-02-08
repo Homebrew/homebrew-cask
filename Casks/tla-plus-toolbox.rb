fc69779b94b9e0b98c2992ed40659bb9d2b871bd
Andrew Yefanov <1134togo@gmail.com>
2022-02-08 14:52:47 +0300

    Update tla-plus-toolbox version from 1.7.1(not installable) to 1.8.0 (#118850)
    * update tla-plus-toolbox version to 1.8.0
    
    * Update tla-plus-toolbox.rb
    
    * Update tla-plus-toolbox.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/tla-plus-toolbox.rb
+++ Casks/tla-plus-toolbox.rb
@@ -1,19 +1,12 @@
 cask "tla-plus-toolbox" do
-  version "1.7.1"
-  sha256 "78e7d0ecbcba63ef7f13f9f315bfb0365a4f1d952caaa11a0a6f86a1bae5ac99"
+  version "1.8.0"
+  sha256 "8a593a8ddb69c6b6f05a53fc0353235b5abf8cd243c8fb8c8accbaf0d59cd6d7"
 
   url "https://github.com/tlaplus/tlaplus/releases/download/v#{version}/TLAToolbox-#{version}-macosx.cocoa.x86_64.zip",
       verified: "github.com/tlaplus/tlaplus/"
   name "TLA+ Toolbox"
   desc "IDE for TLA+"
   homepage "https://lamport.azurewebsites.net/tla/toolbox.html"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
-
-  conflicts_with cask: "homebrew/cask-versions/tla-plus-toolbox-nightly"
 
   app "TLA+ Toolbox.app"
 end
