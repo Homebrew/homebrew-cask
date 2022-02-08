1a8fd278ee74efe181292ca3be7bbb9821e5e285
Miccal Matthews <miccal.matthews@gmail.com>
2022-02-08 18:54:34 +0800

    Update weka.rb

--- Casks/weka.rb
+++ Casks/weka.rb
@@ -1,16 +1,16 @@
 cask "weka" do
+  arch = Hardware::CPU.intel? ? "osx" : "arm-osx"
+
   version "3.8.6"
 
   if Hardware::CPU.intel?
-    url "https://downloads.sourceforge.net/weka/weka-#{version.dots_to_hyphens}-azul-zulu-osx.dmg",
-        verified: "sourceforge.net/weka/"
     sha256 "282d5ff81960d1ae43ee6e4e8eaa7ae8c341666a214e859728e15214af80383b"
   else
-    url "https://downloads.sourceforge.net/weka/weka-#{version.dots_to_hyphens}-azul-zulu-arm-osx.dmg",
-        verified: "sourceforge.net/weka/"
     sha256 "85de453da3bb41c952da48eee0bf574d1da7ee4fa6fd3a8e2d7a81a89d55d3d7"
   end
 
+  url "https://downloads.sourceforge.net/weka/weka-#{version.dots_to_hyphens}-azul-zulu-#{arch}.dmg",
+      verified: "sourceforge.net/weka/"
   name "Weka"
   desc "Collection of machine learning algorithms for data mining tasks"
   homepage "https://www.cs.waikato.ac.nz/ml/weka/"
@@ -18,7 +18,7 @@ cask "weka" do
   livecheck do
     url "https://waikato.github.io/weka-wiki/downloading_weka/"
     strategy :page_match do |page|
-      page[%r{href=.*?/weka[._-]v?(\d+(?:-\d+)+)[._-]azul[._-]zulu[._-]osx\.dmg}i, 1].tr("-", ".")
+      page[%r{href=.*?/weka[._-]v?(\d+(?:-\d+)+)[._-]azul[._-]zulu[._-]#{arch}\.dmg}i, 1].tr("-", ".")
     end
   end
 
