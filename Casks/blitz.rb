03a7ecb479f27e72003a84a97b7b1700a7d181ec
Hannes Braun <hannesbraun@mail.de>
2022-02-08 12:11:58 +0100

    Update blitz from 1.16.22 to 1.16.23 (#118861)

--- Casks/blitz.rb
+++ Casks/blitz.rb
@@ -1,6 +1,6 @@
 cask "blitz" do
-  version "1.16.22"
-  sha256 "386fef79d511921681eab830f09ab01e6c66443a88a806bbdf232a909901d48c"
+  version "1.16.23"
+  sha256 "cc5c9640ee888416782edd09bc041967a9f7960dedc5a3e1c10e2427e6b0b1a0"
 
   url "https://blitz-stable.blitz.gg/Blitz-#{version}.dmg"
   name "Blitz"
