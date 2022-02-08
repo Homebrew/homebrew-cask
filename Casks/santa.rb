d617670ae482e6baaddefe869ea3a77138a127c3
Clément L <porkepix@gmail.com>
2022-02-08 12:28:16 +0100

    Update santa from 2021.8 to 2022.1 (#118864)

--- Casks/santa.rb
+++ Casks/santa.rb
@@ -1,6 +1,6 @@
 cask "santa" do
-  version "2021.8"
-  sha256 "f27b5fcd3efdbc43fb7f1139f85175e74dc8ae5cf2afd48d3d89d0ba59aa9248"
+  version "2022.1"
+  sha256 "0df631a29ee8a6902c0c228533068403d8c998c8a7dc36bca7779047f60d8ebd"
 
   url "https://github.com/google/santa/releases/download/#{version}/santa-#{version}.dmg"
   name "Santa"
