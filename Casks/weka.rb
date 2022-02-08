5c6866080bfb6881e13be666576254403b05d020
Adrian Ho <aho+bruteforce@03s.net>
2022-02-08 18:19:11 +0800

    weka: fix checksum, add ARM installer
    Upstream replaced DMGs because they forgot to codesign their binaries: https://list.waikato.ac.nz/hyperkitty/list/wekalist@list.waikato.ac.nz/thread/LKCEXGMQJGNAARXHDY4Z6WCLY4NN4MSA/

--- Casks/weka.rb
+++ Casks/weka.rb
@@ -1,9 +1,16 @@
 cask "weka" do
   version "3.8.6"
-  sha256 "092284e53006f37c5c43bab1f4a082771375c7db9d749963498a5e4bda0e146c"
 
-  url "https://downloads.sourceforge.net/weka/weka-#{version.dots_to_hyphens}-azul-zulu-osx.dmg",
-      verified: "sourceforge.net/weka/"
+  if Hardware::CPU.intel?
+    url "https://downloads.sourceforge.net/weka/weka-#{version.dots_to_hyphens}-azul-zulu-osx.dmg",
+        verified: "sourceforge.net/weka/"
+    sha256 "282d5ff81960d1ae43ee6e4e8eaa7ae8c341666a214e859728e15214af80383b"
+  else
+    url "https://downloads.sourceforge.net/weka/weka-#{version.dots_to_hyphens}-azul-zulu-arm-osx.dmg",
+        verified: "sourceforge.net/weka/"
+    sha256 "85de453da3bb41c952da48eee0bf574d1da7ee4fa6fd3a8e2d7a81a89d55d3d7"
+  end
+
   name "Weka"
   desc "Collection of machine learning algorithms for data mining tasks"
   homepage "https://www.cs.waikato.ac.nz/ml/weka/"
