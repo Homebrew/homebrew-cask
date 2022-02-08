2029681a2a6f042c9049998f16fe99d1646c25bb
Jonathan Scholz <contact@jonathan-scholz.de>
2022-02-08 15:50:40 +0100

    Use GitHub release to install protonmail-bridge (#118869)
    * protonmail-bridge: use GitHub release download URL
    
    Using the GitHub release to download because the
    generic URL may lead to Homebrew reporting that an
    old version is installed when it actually
    downloads the latest version.
    
    * protonmail-bridge: bump version to 2.1.0

--- Casks/protonmail-bridge.rb
+++ Casks/protonmail-bridge.rb
@@ -1,8 +1,9 @@
 cask "protonmail-bridge" do
-  version "1.8.12"
-  sha256 :no_check
+  version "2.1.0"
+  sha256 "423e51a97fac39dbfb4130806aeb98642a0a80906da4090961ad8916691daef4"
 
-  url "https://protonmail.com/download/Bridge-Installer.dmg"
+  url "https://github.com/ProtonMail/proton-bridge/releases/download/v#{version}/Bridge-Installer.dmg",
+      verified: "github.com/ProtonMail/proton-bridge/"
   name "ProtonMail Bridge"
   desc "Bridges ProtonMail to email clients supporting IMAP and SMTP protocols"
   homepage "https://protonmail.com/bridge/"
