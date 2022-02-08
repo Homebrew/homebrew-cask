01365bd498f2182b4758842f8297837b2cb4a644
neutric <5984479+neutric@users.noreply.github.com>
2022-02-08 01:07:32 +0100

    gogs: add arm version

--- Casks/gogs.rb
+++ Casks/gogs.rb
@@ -1,10 +1,18 @@
 cask "gogs" do
-  version "0.12.4"
-  sha256 "92d0cb113697a47ee75fa41bc27ab561383ade7929a38e9b6313d160ee053293"
+  arch = Hardware::CPU.intel? ? "amd64" : "arm64"
 
-  url "https://github.com/gogs/gogs/releases/download/v#{version}/gogs_#{version}_darwin_amd64.zip",
+  version "0.12.4"
+
+  if Hardware::CPU.intel?
+    sha256 "92d0cb113697a47ee75fa41bc27ab561383ade7929a38e9b6313d160ee053293"
+  else
+    sha256 "d462e156094ab2642551aa5e036413c303299235d28bc58952518ac901cb2f5d"
+  end
+
+  url "https://github.com/gogs/gogs/releases/download/v#{version}/gogs_#{version}_darwin_#{arch}.zip",
       verified: "github.com/gogs/gogs/"
   name "Go Git Service"
+  desc "Self-hosted Git service"
   homepage "https://gogs.io/"
 
   # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
