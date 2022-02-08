4a18586c65b954079a51fe00b980f4e9dcbc0182
neutric <5984479+neutric@users.noreply.github.com>
2022-02-08 01:31:26 +0100

    Update gcs from 4.36.0 to 4.37.1 (#118829)

--- Casks/gcs.rb
+++ Casks/gcs.rb
@@ -1,6 +1,6 @@
 cask "gcs" do
-  version "4.36.0"
-  sha256 "4dde37ee57b16b130371481836f994fe2792ed4b6dcfccbfabd6035cfb6611f1"
+  version "4.37.1"
+  sha256 "5bf8c91a4315b3b0893aa9b70c6f0a46cbffbe20430fb0325ac1ba8501ea5308"
 
   url "https://github.com/richardwilkes/gcs/releases/download/v#{version}/GCS-#{version}.dmg",
       verified: "github.com/richardwilkes/gcs/"
