09ff76d43543c25913bb4c05b3977ac268e12157
Aaron Lee <aaron@onefatgiraffe.com>
2022-02-09 00:52:12 +0900

    Update mailtrackerblocker from 0.5.8 to 0.6.0 (#118871)
    Co-authored-by: runner <runner@Mac-1644329721421.local>

--- Casks/mailtrackerblocker.rb
+++ Casks/mailtrackerblocker.rb
@@ -1,6 +1,6 @@
 cask "mailtrackerblocker" do
-  version "0.5.8"
-  sha256 "121d3aebceacbc3cc3ccca8bc6fda8c425173462ed821732d13250a50dfaf21d"
+  version "0.6.0"
+  sha256 "2fac9926338f1d023521afa93c3cbcc537129a8636aa583ff12d2a5d763af1fb"
 
   url "https://github.com/apparition47/MailTrackerBlocker/releases/download/#{version}/MailTrackerBlocker.pkg",
       verified: "github.com/apparition47/MailTrackerBlocker/"
