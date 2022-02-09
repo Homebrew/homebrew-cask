7b7cf261117735596773971865510a60bb34a579
neutric <5984479+neutric@users.noreply.github.com>
2022-02-09 01:59:06 +0100

    Update hoststool from 2.7.0 to 2.8.0 and update name

--- Casks/hoststool.rb
+++ Casks/hoststool.rb
@@ -1,13 +1,13 @@
 cask "hoststool" do
-  version "2.7.0"
-  sha256 "52a8c777210dff249e5d37c839ec711910e3c87f68dfe127cb0b9b4719736782"
+  version "2.8.0"
+  sha256 "20f87eca89f92957e9d35c1854318659998a5663038162778016d9e2d581a07d"
 
-  url "https://github.com/ZzzM/HostsX/releases/download/#{version}/HostsToolForMac.zip"
-  name "Hosts tool for Mac"
+  url "https://github.com/ZzzM/HostsX/releases/download/#{version}/HostsX.dmg"
+  name "HostsX"
   desc "Local hosts update tool"
   homepage "https://github.com/ZzzM/HostsX"
 
   depends_on macos: ">= :sierra"
 
-  app "HostsToolForMac.app"
+  app "HostsX.app"
 end
