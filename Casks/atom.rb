ddceb1e5f157ddc1453bd187389fb6daf5680eb9
eloyaldea <35566945+eloyaldea@users.noreply.github.com>
2022-02-09 10:38:47 +0100

    Update atom from 1.58.0 to 1.59.0 (#118879)

--- Casks/atom.rb
+++ Casks/atom.rb
@@ -1,6 +1,6 @@
 cask "atom" do
-  version "1.58.0"
-  sha256 "28d8cf487e68271de87e0427d8b566609e29d40be9ff0a76c09ec72883a8e836"
+  version "1.59.0"
+  sha256 "9ad3101d2bf1f4fdf547174d838e8efc81c899fa29db063e1edadda1f0fa42d3"
 
   url "https://github.com/atom/atom/releases/download/v#{version}/atom-mac.zip",
       verified: "github.com/atom/atom/"
