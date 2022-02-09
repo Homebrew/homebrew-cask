4b1dd41d7ca5ce7f1aa53fbabf758e3be994c985
fabrizio <fxbrit@tuta.io>
2022-02-09 09:38:13 +0000

    Update LibreWolf from v96.0.3-1 to v97.0-1 (#118884)
    * Update LibreWolf from v95.0.2-1 to v96.0-1
    
    * Update LibreWolf from v96.0-1 to v96.0.1-1
    
    * Update LibreWolf from v96.0.1-1 to v96.0.2-1
    
    * Update LibreWolf from v96.0.3-1 to v97.0-1

--- Casks/librewolf.rb
+++ Casks/librewolf.rb
@@ -1,15 +1,15 @@
 cask "librewolf" do
-  arch = Hardware::CPU.intel? ? "" : "_aarch64_exp"
+  arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    version "96.0.3,1,dc7f916e3934e6e332b2b7c5fcf30175"
-    sha256 "3a45392bac62cf11e1153fd83cdedeb90b8acfc93d9e31d97f7853625973a187"
+    version "97.0,1,45070518c865ab1af2cd2c40fe9c3143"
+    sha256 "9db7ac26ec5f833127a588c99feeeaada4004a165117d113e479fb75ca291438"
   else
-    version "96.0.3,1,50ab139fe29f3ae1b0e3d3488386d145"
-    sha256 "ce576f5cce6fa2fe78bb5afe4706358be1b72d3b30630617a8768d99e9ca4fce"
+    version "97.0,1,006eaf7e31707df8e0ef2a70ff74a3ca"
+    sha256 "adfd5852ab4ea07c6a2e489ea7dee3fa2311f66e4f75f462b14d98535a3aea09"
   end
 
-  url "https://gitlab.com/librewolf-community/browser/macos/uploads/#{version.csv.third}/librewolf-#{version.csv.first}-#{version.csv.second}#{arch}.dmg",
+  url "https://gitlab.com/librewolf-community/browser/macos/uploads/#{version.csv.third}/librewolf-#{version.csv.first}-#{version.csv.second}.en-US.mac.#{arch}.dmg",
       verified: "gitlab.com/librewolf-community/browser/macos/"
   name "LibreWolf"
   desc "Web browser"
@@ -17,7 +17,7 @@ cask "librewolf" do
 
   livecheck do
     url "https://gitlab.com/api/v4/projects/13853965/releases"
-    regex(%r{/(\w+)/librewolf[._-](\d+(?:\.\d+)+)-(\d+)#{arch}\.dmg}i)
+    regex(%r{/(\w+)/librewolf[._-](\d+(?:\.\d+)+)-(\d+)\.en-US\.mac\.#{arch}\.dmg}i)
     strategy :page_match do |page, regex|
       page.scan(regex).map do |match|
         "#{match[1]},#{match[2]},#{match[0]}"
