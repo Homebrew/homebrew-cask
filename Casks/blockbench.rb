70177ff03b8cde1063efd2cbc64b5bb4e948ef39
Mihail Yaremenko <m50421@outlook.com>
2022-02-10 11:44:43 +0200

    Update zap of `Blockbench` (#118947)
    * Update zap of `Blockbench`
    
    * Update `blockbench` to 4.1.4

--- Casks/blockbench.rb
+++ Casks/blockbench.rb
@@ -1,12 +1,12 @@
 cask "blockbench" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "4.1.3"
+  version "4.1.4"
 
   if Hardware::CPU.intel?
-    sha256 "e1bc36b9e394275eda57b33749f8305524fb675f2d52c8fd27f70b92a97fe641"
+    sha256 "0b6a78e15385fbaa633ef78464056a032a78bae26e4b83956b56dbc5a1aa9112"
   else
-    sha256 "cbbc1d63e518c8e344945650192df7161f23dfb91fd655f3ad2efc387407efdd"
+    sha256 "cac91e38b64e7414b9e5dee610d5ca49b6d13b3baf9b9d645b442ced11ad368f"
   end
 
   url "https://github.com/JannisX11/blockbench/releases/download/v#{version}/Blockbench_#{arch}_#{version}.dmg",
@@ -22,5 +22,9 @@ cask "blockbench" do
 
   app "Blockbench.app"
 
-  zap trash: "~/Library/Application Support/Blockbench"
+  zap trash: [
+    "~/Library/Application Support/Blockbench",
+    "~/Library/Preferences/blockbench.plist",
+    "~/Library/Saved Application State/blockbench.savedState",
+  ]
 end
