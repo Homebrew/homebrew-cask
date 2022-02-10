536c11f1127a72009f3375a84dffae75b30e1f74
eloyaldea <35566945+eloyaldea@users.noreply.github.com>
2022-02-10 10:34:24 +0100

    Update altserver from 1.4.8 to 1.4.9 (#118922)

--- Casks/altserver.rb
+++ Casks/altserver.rb
@@ -1,6 +1,6 @@
 cask "altserver" do
-  version "1.4.8,60"
-  sha256 "4c6c6bc6c0d409aa6eef98cb157f1aa4375439fde6dc2a395af79fdd43530054"
+  version "1.4.9,62"
+  sha256 "50b26b0ac3c472456d1a9d04b4790a5978a8be755f2cf0568b128d6473f91bf4"
 
   url "https://cdn.altstore.io/file/altstore/altserver/#{version.csv.first.dots_to_underscores}.zip"
   name "AltServer"
