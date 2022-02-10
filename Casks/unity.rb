7bf85bba36edad854c348e8d90827e248af75888
neutric <5984479+neutric@users.noreply.github.com>
2022-02-10 23:26:40 +0100

    Update unity from 2021.2.10f1 to 2021.2.11f1 (#118982)

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -1,12 +1,12 @@
 cask "unity" do
   arch = Hardware::CPU.intel? ? "" : "Arm64"
 
-  version "2021.2.10f1,ee872746220e"
+  version "2021.2.11f1,e50cafbb4399"
 
   if Hardware::CPU.intel?
-    sha256 "79012f77a11e442ddc5c19e4ab4a9906f6840f77b5c065f36459819656eb6fef"
+    sha256 "305ddb8502e4628fe193b549a0d957aa35519f45a12f6564034f595e129a0ddd"
   else
-    sha256 "36a840329d7531226131f7fdb43d883dead3f86463411a6e25ef9a7d2bfd206d"
+    sha256 "e71960e2999042a255f1a8d6dd8ef66c2440e427e1e4b98ff85481da1ace3dc7"
   end
 
   url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorInstaller#{arch}/Unity-#{version.csv.first}.pkg",
