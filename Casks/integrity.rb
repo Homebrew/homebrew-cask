7f9978c998e1e7891917c25dc178d4c9d6383d19
neutric <5984479+neutric@users.noreply.github.com>
2022-02-12 00:02:18 +0100

    integrity: livecheck: ignore beta versions

--- Casks/integrity.rb
+++ Casks/integrity.rb
@@ -9,7 +9,7 @@ cask "integrity" do
 
   livecheck do
     url "https://peacockmedia.software/mac/integrity/version_history.html"
-    regex(/<h3>v?(\d+(?:\.\d+)+)\s/i)
+    regex(/<h3>v?(\d+(?:\.\d+)+)\s(?!.*beta)/i)
   end
 
   app "Integrity.app"
