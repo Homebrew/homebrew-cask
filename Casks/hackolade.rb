9fdbb8c7979c6f45d0e1b1d042bfa7063a156625
Aaron Jheng <wentworth@outlook.com>
2022-02-11 14:07:11 +0800

    Add Hackolade (new Cask) (#119011)
    * Add Hackolade (new Cask)
    
    * Update hackolade.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/hackolade.rb
@@ -0,0 +1,27 @@
+cask "hackolade" do
+  version "5.4.3"
+  sha256 :no_check
+
+  url "https://s3-eu-west-1.amazonaws.com/hackolade/current/Hackolade-mac-setup-signed.pkg",
+      verified: "s3-eu-west-1.amazonaws.com/hackolade/current/"
+  name "Hackolade"
+  desc "Data Modeling for NoSQL databases, storage formats, REST APIs, and JSON in RDBMS"
+  homepage "https://hackolade.com/"
+
+  livecheck do
+    url "https://hackolade.com/download.html"
+    regex(/Current\sversion:\sv?(\d+(?:\.\d+)+)/i)
+  end
+
+  depends_on macos: ">= :yosemite"
+
+  pkg "Hackolade-mac-setup-signed.pkg"
+
+  uninstall pkgutil: "com.hackolade.pkg.Hackolade"
+
+  zap trash: [
+    "~/Library/Application Support/Hackolade",
+    "~/Library/Preferences/com.electron.hackolade.plist",
+    "~/Library/Saved Application State/com.electron.hackolade.savedState",
+  ]
+end
