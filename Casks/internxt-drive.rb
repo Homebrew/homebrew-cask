5219e543d25c76214911ca1f572e7166088902cc
neutric <5984479+neutric@users.noreply.github.com>
2022-02-11 09:40:01 +0100

    Update internxt-drive from 1.5.0 to 1.5.2 (#118997)

--- Casks/internxt-drive.rb
+++ Casks/internxt-drive.rb
@@ -1,6 +1,6 @@
 cask "internxt-drive" do
-  version "1.5.0"
-  sha256 "ec6e28deed738c7b3f743db53a1542035d6a9665f98ee945f3d509f3f56ad88e"
+  version "1.5.2"
+  sha256 "9f4b2e712b238b9c61ea6791ff44e2e20e0c4d66287c6af789b4d7faeea1c02c"
 
   url "https://github.com/internxt/drive-desktop/releases/download/v#{version}/internxt-drive-#{version}.dmg",
       verified: "https://github.com/internxt/drive-desktop/"
