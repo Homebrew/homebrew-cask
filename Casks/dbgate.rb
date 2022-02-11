a6ba64c0d0be26a096742c95e80e891e0659a87f
Clément L <porkepix@gmail.com>
2022-02-11 10:45:43 +0100

    Update dbgate from 4.6.1 to 4.6.3 (#119023)

--- Casks/dbgate.rb
+++ Casks/dbgate.rb
@@ -1,12 +1,12 @@
 cask "dbgate" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "4.6.1"
+  version "4.6.3"
 
   if Hardware::CPU.intel?
-    sha256 "1b1a6b4e08831d77fe41d93dcc4a5ce7c74fccef5eaf1e09d4a0a2df5264264d"
+    sha256 "a2fddfc25ebd1255de382753a6195afa7310272197b883978e8904c703a6dc82"
   else
-    sha256 "a844d3294cbe25b2881437140a2343a219c447bd9ad9037deabb947f91df5753"
+    sha256 "a2fda46293280c849864a05d1a4cb1bf91f18519d8ac69b88a89895cf76f7b27"
   end
 
   url "https://github.com/dbgate/dbgate/releases/download/v#{version}/dbgate-#{version}-mac_#{arch}.dmg",
