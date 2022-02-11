eb66cd39ea91a43425f1b0e38175184d34d22ce1
Miccal Matthews <miccal.matthews@gmail.com>
2022-02-11 14:01:11 +0800

    Update hackolade.rb

--- Casks/hackolade.rb
+++ Casks/hackolade.rb
@@ -1,19 +1,23 @@
 cask "hackolade" do
-  version :latest
+  version "5.4.3"
   sha256 :no_check
 
   url "https://s3-eu-west-1.amazonaws.com/hackolade/current/Hackolade-mac-setup-signed.pkg",
-      verified: "https://s3-eu-west-1.amazonaws.com/hackolade/"
+      verified: "s3-eu-west-1.amazonaws.com/hackolade/current/"
   name "Hackolade"
   desc "Data Modeling for NoSQL databases, storage formats, REST APIs, and JSON in RDBMS"
   homepage "https://hackolade.com/"
+
+  livecheck do
+    url "https://hackolade.com/download.html"
+    regex(/Current\sversion:\sv?(\d+(?:\.\d+)+)/i)
+  end
 
   depends_on macos: ">= :yosemite"
 
   pkg "Hackolade-mac-setup-signed.pkg"
 
-  uninstall pkgutil: "com.hackolade.pkg.Hackolade",
-            delete:  "/Applications/Hackolade.app"
+  uninstall pkgutil: "com.hackolade.pkg.Hackolade"
 
   zap trash: [
     "~/Library/Application Support/Hackolade",
