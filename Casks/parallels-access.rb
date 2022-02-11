d5e58430a383e8874df5b2ea67433ef2ae570710
Gabor Papp <p3g2or@gmail.com>
2022-02-11 10:41:29 +0100

    Update parallels-access from 5.5.0-36378 to 7.0.0-39895 (#119019)
    * Update parallels-access from 5.5.0-36378 to 7.0.0-39895
    
    * add com.parallels.mobile.audioloader to uninstall launchctl
    
    * Update parallels-access.rb
    
    * Update parallels-access.rb
    
    Co-authored-by: Gabor Papp <gabor.papp@zooshgroup.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/parallels-access.rb
+++ Casks/parallels-access.rb
@@ -1,10 +1,19 @@
 cask "parallels-access" do
-  version "5.5.0-36378"
-  sha256 "8a7e85adae4b9402fb413557c9e6f0913d8cf02f81ac286d0bc846ac17ad1e07"
+  version "7.0.0-39895"
+  sha256 "c80dae90a90aea8fd1ec81da914c8b4258e33ff5d49f1d381b58eed130e4f91e"
 
   url "https://download.parallels.com/pmobile/v#{version.major}/#{version}/ParallelsAccess-#{version}-mac.dmg"
   name "Parallels Access"
+  desc "Simplest remote access to your computer from anywhere"
   homepage "https://www.parallels.com/products/access/"
+
+  livecheck do
+    url "https://download.parallels.com/website_links/pmobile/3/builds-en_US.json"
+    strategy :page_match do |page|
+      scan = page.scan(/ParallelsAccess[._-]v?(\d+(?:\.\d+)+)-(\d+)[._-]mac\.dmg/i)
+      scan.map { |v| "#{v[0]}-#{v[1]}" }
+    end
+  end
 
   # This .dmg cannot be extracted normally
   # Original discussion: https://github.com/Homebrew/homebrew-cask/issues/26872
@@ -21,14 +30,13 @@ cask "parallels-access" do
   end
 
   uninstall launchctl: [
-    "com.parallels.mobile.startgui.launchagent",
+    "com.parallels.mobile.audioloader",
     "com.parallels.mobile.dispatcher.launchdaemon",
     "com.parallels.mobile.kextloader.launchdaemon",
     "com.parallels.mobile.prl_deskctl_agent.launchagent",
+    "com.parallels.mobile.startgui.launchagent",
   ],
-            quit:      [
-              "com.parallels.inputmethod.ParallelsIM",
-            ],
+            quit:      "com.parallels.inputmethod.ParallelsIM",
             signal:    [
               ["TERM", "com.parallels.mobile"],
               ["TERM", "com.parallels.mobile.prl_deskctl_agent"],
@@ -42,8 +50,8 @@ cask "parallels-access" do
   zap trash: [
     "~/Library/Cookies/com.parallels.mobile.prl_deskctl_agent.binarycookies",
     "~/Library/Group Containers/4C6364ACXT.com.parallels.Access",
-    "~/Library/Preferences/com.parallels.Parallels Access.plist",
-    "~/Library/Preferences/com.parallels.Parallels Access.plist.sdb",
     "~/Library/Preferences/com.parallels.mobile.plist",
+    "~/Library/Preferences/com.parallels.Parallels Access.plist.sdb",
+    "~/Library/Preferences/com.parallels.Parallels Access.plist",
   ]
 end
