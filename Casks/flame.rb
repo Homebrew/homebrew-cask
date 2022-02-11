d1e555160b2a8bd08499504589656c6e1c4417cb
Sam Ford <1584702+samford@users.noreply.github.com>
2022-02-10 23:28:55 -0500

    Update flame from 2.6.0 to 3.0.0,80 (#119013)

--- Casks/flame.rb
+++ Casks/flame.rb
@@ -1,8 +1,9 @@
 cask "flame" do
-  version "2.6.0"
-  sha256 "fc86b75a0f2be52be3ec6ce2e260fa816fffe8e0d14caf87c3e8050ed0ec0b1a"
+  version "3.0.0,80"
+  sha256 "cc09216769022fc4acb20eaddd37898ace0d3a9ca8d9f53fbc06cdbfd45868e0"
 
-  url "https://github.com/tominsam/flametouch/releases/download/#{version}/Flame_#{version}.zip",
+  # Tag version format is 1.2.3(45) but zip file uses 1.2.3.45
+  url "https://github.com/tominsam/flametouch/releases/download/#{version.csv.first}(#{version.csv.second})/Flame.#{version.csv.first}.#{version.csv.second}.zip",
       verified: "github.com/tominsam/flametouch/"
   name "Flame"
   desc "Rendezvous service browser for iPhone / iPod touch"
@@ -10,8 +11,12 @@ cask "flame" do
 
   livecheck do
     url :url
-    strategy :git
-    regex(/^(\d+(?:\.\d+)*)$/i)
+    regex(%r{href=.*?/tag/v?(\d+(?:\.\d+){1,2})(?:\s*[(._-](\d+)\)?)?["' >]}i)
+    strategy :github_latest do |page, regex|
+      page.scan(regex).map do |match|
+        match[1].present? ? "#{match[0]},#{match[1]}" : match[0]
+      end
+    end
   end
 
   depends_on macos: ">= :catalina"
