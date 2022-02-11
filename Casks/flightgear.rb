a526c86054c3b3c9fdb54a960576833f37330f90
miccal <miccal.matthews@gmail.com>
2022-02-11 11:21:34 +0800

    Update flightgear from 2020.3.11 to 2020.3.12

--- Casks/flightgear.rb
+++ Casks/flightgear.rb
@@ -1,6 +1,6 @@
 cask "flightgear" do
-  version "2020.3.11"
-  sha256 "f6a04a20c9d5e45ec9e97a2cf506db477f186bac2693e051ede4e924cb0c517d"
+  version "2020.3.12"
+  sha256 "f731684c9c90228a9cb84ff3e0775a50cefde8c96011711fe3ab84a370689beb"
 
   url "https://downloads.sourceforge.net/flightgear/FlightGear-#{version}.dmg",
       verified: "sourceforge.net/flightgear/"
