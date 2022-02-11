9a12d13ae01b6df97d218a21bd2c7ca370fd192f
Miccal Matthews <miccal.matthews@gmail.com>
2022-02-11 08:35:30 +0800

    Update threema-work.rb

--- Casks/threema-work.rb
+++ Casks/threema-work.rb
@@ -1,19 +1,18 @@
-# typed: false
-# frozen_string_literal: true
-
 cask "threema-work" do
   version "1.1.0"
   sha256 :no_check
 
   url "https://releases.threema.ch/web-electron/v1/release/Threema-Work-Latest.dmg"
-  name "threema-work"
-  desc "End-to-end encrypted instant messaging application (business version)"
+  name "Threema Work"
+  desc "End-to-end encrypted instant messaging application"
   homepage "https://work.threema.ch/"
 
   livecheck do
     url "https://work.threema.ch/en/changelog#chl=1"
     regex(/Threema\s*Work\s*(\d+(?:\.\d+)+)\s*for\s*Desktop/i)
   end
+
+  depends_on macos: ">= :el_capitan"
 
   app "Threema Work.app"
 
