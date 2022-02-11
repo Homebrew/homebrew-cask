3e10bad27e37a0a13dc92e79ba24749d3b2eb904
Clément L <porkepix@gmail.com>
2022-02-11 13:57:57 +0100

    Update vassal from 3.6.4 to 3.6.5 (#119026)

--- Casks/vassal.rb
+++ Casks/vassal.rb
@@ -1,12 +1,12 @@
 cask "vassal" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
-  version "3.6.4"
+  version "3.6.5"
 
   if Hardware::CPU.intel?
-    sha256 "1aa3b948894e1d7e1a6566ccf7f82ddc2e85574b32b8a38e3f2b62a25ca52bcf"
+    sha256 "631e90f037b86421a0341ae5299be2e80301317f958c18a558f11e46efe92f7b"
   else
-    sha256 "483f795d66c135c1cdc5edba2de5acb997ca72d687bf573e175f5f1c678d7d73"
+    sha256 "348ca9e9330404210cf0c04404bfa7f9aa87f638b53cc458f8d0e9de254a1d3d"
   end
 
   url "https://github.com/vassalengine/vassal/releases/download/#{version}/VASSAL-#{version}-macos-#{arch}.dmg",
