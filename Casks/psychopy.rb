97fbbfbb919503e0add7f51824ba52db8bc4c908
Clément L <porkepix@gmail.com>
2022-02-12 21:40:01 +0100

    Update psychopy from 2021.2.3 to 2022.1.0 (#119073)
    * Update psychopy from 2021.2.3 to 2022.1.0
    
    * Update psychopy.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/psychopy.rb
+++ Casks/psychopy.rb
@@ -1,16 +1,16 @@
 cask "psychopy" do
-  version "2021.2.3"
-  sha256 "1af534c3478600e551d63594a493a0ca17a3188a9c662d0caf1881d0f767334a"
+  version "2022.1.0"
+  sha256 "49e8874dafe8110977da6814637a89950ea9e9b8e59a6a07bf1f6fa36b9fa52b"
 
   url "https://github.com/psychopy/psychopy/releases/download/#{version.major_minor_patch}/StandalonePsychoPy-#{version}-macOS.dmg"
   name "PsychoPy"
-  desc "For running psychology and neuroscience experiments"
+  desc "Create experiments in behavioral science"
   homepage "https://github.com/psychopy/psychopy"
 
   livecheck do
     url :url
     strategy :github_latest
-    regex(%r{href=.+/StandalonePsychoPy-v?(\d+(?:\.\d+)*)-macOS\.dmg}i)
+    regex(%r{href=.+/StandalonePsychoPy[._-]v?(\d+(?:\.\d+)+)[._-]macOS\.dmg}i)
   end
 
   app "PsychoPy.app"
