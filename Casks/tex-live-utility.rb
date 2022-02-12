6a74cda966aa3fd4dda3a5abcc9cf58c81a4ef97
Reno <25192197+singularitti@users.noreply.github.com>
2022-02-12 15:13:51 -0500

    Update tex-live-utility from 1.52 to 1.53 (#119066)
    * Update tex-live-utility from 1.52 to 1.53
    
    * Update tex-live-utility.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/tex-live-utility.rb
+++ Casks/tex-live-utility.rb
@@ -1,6 +1,6 @@
 cask "tex-live-utility" do
-  version "1.52"
-  sha256 "479c0895466580f98e1fc63628eb4f533773f90e85ee48fa70e2edb0307684a2"
+  version "1.53"
+  sha256 "64caa374c9a34d0b7a513248aa42cf4c9b0dff207010ee4b4cd2fd5b56a956df"
 
   url "https://github.com/amaxwell/tlutility/releases/download/#{version}/TeX.Live.Utility.app-#{version}.zip"
   name "TeX Live Utility"
@@ -17,10 +17,10 @@ cask "tex-live-utility" do
   app "TeX Live Utility.app"
 
   zap trash: [
-    "~/Library/Caches/com.googlecode.mactlmgr.tlu",
     "~/Library/Application Support/TeX Live Utility",
+    "~/Library/Caches/com.apple.helpd/SDMHelpData/Other/English/HelpSDMIndexFile/TeX Live Utility Help*",
+    "~/Library/Caches/com.googlecode.mactlmgr.tlu",
     "~/Library/Preferences/com.googlecode.mactlmgr.tlu.plist",
     "~/Library/Saved Application State/com.googlecode.mactlmgr.tlu.savedState",
-    "~/Library/Caches/com.apple.helpd/SDMHelpData/Other/English/HelpSDMIndexFile/TeX Live Utility Help*",
   ]
 end
