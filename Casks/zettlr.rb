7555023349ab7c24c4a8c268235cb9d4aac67c53
Wolfram Esser <github@derwok.de>
2022-02-12 13:03:47 +0100

    Update zettlr from 2.2.0 to 2.2.1

--- Casks/zettlr.rb
+++ Casks/zettlr.rb
@@ -1,12 +1,12 @@
 cask "zettlr" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "2.2.0"
+  version "2.2.1"
 
   if Hardware::CPU.intel?
-    sha256 "899102af5f6dd475148da8ad5dc6303251ff41f740c6c3d058994e4924bc3f7f"
+    sha256 "16ca47bd540977a4c489e6886bcc1c48cfd048972d326b9e58afdf81c05fb495"
   else
-    sha256 "21d0b3bdf918ee0328edfe9e96809f55f2648ba0928e75e573e892a65c867ec9"
+    sha256 "243f6a6872e20cdab4fe9702f497e7fd348c1b0a5d558f605229117ebbc505ae"
   end
 
   url "https://github.com/Zettlr/Zettlr/releases/download/v#{version}/Zettlr-#{version}-#{arch}.dmg"
