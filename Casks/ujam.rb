be48de2baa2276c73c8c4532071b5d92b8e4cfaa
aaronk6 <aaronk6@users.noreply.github.com>
2022-02-12 17:23:25 +0100

    Fix style

--- Casks/ujam.rb
+++ Casks/ujam.rb
@@ -1,18 +1,18 @@
 cask "ujam" do
   version "0.3.3"
   sha256 "8d03723b2d35e290ac52b7e015a27831e4160d95891e37b0c9fabe762b3f5f34"
-  
+
   url "https://software.ujam.com/ujamapp/UJAM-#{version}.dmg"
   name "UJAM"
   desc "Administration tool for UJAM products"
   homepage "https://www.ujam.com/"
 
-  app "UJAM.app"
-
   livecheck do
     url "https://software.ujam.com/ujamapp/latest-mac.yml"
     regex(/version:\s+(\d+(?:\.\d+)+)/i)
   end
+
+  app "UJAM.app"
 
   zap trash: "~/Library/Application Support/ujamapp"
 end
