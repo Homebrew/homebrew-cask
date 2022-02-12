1b47013a54f7833ddca5c8e5f5d3bddbf647c175
MoonFruit <dkmoonfruit@gmail.com>
2022-02-12 22:12:03 +0800

    Update wechatwork from 4.0.0.90428 to 4.0.0.90438 (#119061)

--- Casks/wechatwork.rb
+++ Casks/wechatwork.rb
@@ -1,6 +1,6 @@
 cask "wechatwork" do
-  version "4.0.0.90428"
-  sha256 "339aa8297467945550ab7fa3d6bd6b0d83a1477bfadc47bdf7d15f2cbfc479d3"
+  version "4.0.0.90438"
+  sha256 "c8043a3408757d1d5629b1d618f7278f04a09a0f2ea8a48f5fd2d81e8118700a"
 
   url "https://dldir1.qq.com/wework/work_weixin/WeCom_#{version}.dmg"
   name "WeChat Work"
