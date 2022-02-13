ed701694d7c3d655d0a73a35298571fbb7ec9203
Zhenfu Shi <szf1234@me.com>
2022-02-13 20:27:53 +0800

    Update opencore-configurator from 2.56.0.0 to 2.57.0.0 (#119081)

--- Casks/opencore-configurator.rb
+++ Casks/opencore-configurator.rb
@@ -1,5 +1,5 @@
 cask "opencore-configurator" do
-  version "2.56.0.0"
+  version "2.57.0.0"
   sha256 :no_check
 
   url "https://mackie100projects.altervista.org/apps/opencoreconf/download-new-build.php?version=last",
