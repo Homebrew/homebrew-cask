4037c157dacc51ecd9a9763c95ee77fad6e31b57
neutric <5984479+neutric@users.noreply.github.com>
2022-02-13 03:28:36 +0100

    keycombiner: fix name capitalization

--- Casks/keycombiner.rb
+++ Casks/keycombiner.rb
@@ -3,7 +3,7 @@ cask "keycombiner" do
   sha256 "9e9e6b24f82f74009470415f2484f58ce045eedfdf138588791feb552f8ad61c"
 
   url "https://keycombiner.com/download/KeyCombiner-#{version}.dmg"
-  name "keycombiner"
+  name "KeyCombiner"
   desc "Instant shortcut lookup"
   homepage "https://keycombiner.com/"
 
