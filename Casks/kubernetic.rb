9a770f1c70771f41c45c0443b4c34e2db046d310
neutric <5984479+neutric@users.noreply.github.com>
2022-02-14 00:40:18 +0100

    Update kubernetic from 3.1.0 to 3.2.0 (#119143)

--- Casks/kubernetic.rb
+++ Casks/kubernetic.rb
@@ -1,6 +1,6 @@
 cask "kubernetic" do
-  version "3.1.0"
-  sha256 "aabfb1497d3d3407adc0040c8af83b189cf395d74021c508653d5d9385ed4132"
+  version "3.2.0"
+  sha256 "220b15f754bccdafe5355d2b3989a8dc44113c4e42a30826cb63ceccec0ed0e3"
 
   url "https://kubernetic.s3.amazonaws.com/Kubernetic-#{version}.dmg",
       verified: "kubernetic.s3.amazonaws.com/"
