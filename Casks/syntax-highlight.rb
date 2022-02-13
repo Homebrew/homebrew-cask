a5d85b6afaf1224ed7480374ace255afe28eedfb
Bevan Kay <email@bevankay.me>
2022-02-13 23:30:50 +1100

    Update syntax-highlight from 2.1.7 to 2.1.8

--- Casks/syntax-highlight.rb
+++ Casks/syntax-highlight.rb
@@ -1,6 +1,6 @@
 cask "syntax-highlight" do
-  version "2.1.7"
-  sha256 "1db3cb33fd74b4fcced26602b715bb83c703704dabd7e153550c8abc0c6f6050"
+  version "2.1.8"
+  sha256 "2c142af5ad3f48539f74e007f2f280a5cba32839a444d2ae541c6e673a3ae48b"
 
   url "https://github.com/sbarex/SourceCodeSyntaxHighlight/releases/download/#{version}/Syntax.Highlight.zip"
   name "Syntax Highlight"
