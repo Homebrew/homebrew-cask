e0bd5d0cb559c9948274568d72a7134667fed4ca
Sam Ford <1584702+samford@users.noreply.github.com>
2022-02-14 18:37:26 -0500

    Update fpc-src-laz from 3.2.0-2,2.0.12 to 3.2.2-20210709,2.2.0 (#119176)

--- Casks/fpc-src-laz.rb
+++ Casks/fpc-src-laz.rb
@@ -1,24 +1,22 @@
 cask "fpc-src-laz" do
-  version "3.2.0-2,2.0.12"
-  sha256 "8f85c873c3a224961a98fd24e97320a2134bbcc7f91c6d8153e53b36beb7fe74"
+  version "3.2.2-20210709,2.2.0"
+  sha256 "c0ed6b9261679ba040cdf07f4f5d13d915184a4164b1addf5a81e19b0738e87a"
 
-  url "https://downloads.sourceforge.net/lazarus/Lazarus%20macOS%20x86-64/Lazarus%20#{version.csv.second}/fpc-src-#{version.csv.first}-laz.pkg",
+  url "https://downloads.sourceforge.net/lazarus/Lazarus%20macOS%20x86-64/Lazarus%20#{version.csv.second}/fpc-src-#{version.csv.first}-macosx.dmg",
       verified: "sourceforge.net/lazarus/"
   name "Pascal compiler source files for Lazarus"
   desc "Pascal compiler source files for Lazarus"
   homepage "https://www.lazarus-ide.org/"
 
   livecheck do
-    url "https://sourceforge.net/projects/lazarus/rss"
-    strategy :page_match do |page|
-      match = page.match(%r{Lazarus%20(\d+(?:.\d+)*)/fpc-src-(\d+(?:.\d+)*)-laz\.pkg}i)
-      next if match.blank?
-
-      "#{match[2]},#{match[1]}"
+    url "https://sourceforge.net/projects/lazarus/rss?path=/Lazarus%20macOS%20x86-64"
+    regex(%r{url=.*?/Lazarus(?:%20|[._-])v?(\d+(?:\.\d+)+)/fpc-src[._-]v?(\d+(?:[.-]\d+)+)[^"' >]+?\.(?:dmg|pkg)}i)
+    strategy :sourceforge do |page, regex|
+      page.scan(regex).map { |match| "#{match[1]},#{match[0]}" }
     end
   end
 
-  pkg "fpc-src-#{version.csv.first}-laz.pkg"
+  pkg "fpcsrc-#{version.csv.first}.pkg"
 
   uninstall pkgutil: [
     "org.freepascal.fpc.source",
