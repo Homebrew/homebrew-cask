f97441fcb3e6197ca7ed2189ae4a15de19cfa27a
sandergol <30435939+sandergol@users.noreply.github.com>
2022-02-14 12:30:10 +0300

    Upgrade Murus.app to v2.2.3 (#119156)

--- Casks/murus.rb
+++ Casks/murus.rb
@@ -1,6 +1,6 @@
 cask "murus" do
-  version "2.2.2"
-  sha256 "17a213957039ce0e16a98f5128ca75a8d74b1ab89c4172e1cdefe770f8fffe4d"
+  version "2.2.3"
+  sha256 "855aca40b79d7e72a8cd2610d53c55eea2d81b456aad0f95ddf4c07e1ad03807"
 
   url "https://github.com/TheMurusTeam/Murus/releases/download/v#{version}/murus-#{version}.zip",
       verified: "github.com/TheMurusTeam/Murus/"
