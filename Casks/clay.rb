b2711b11b56d099c18c6b9416ad7ba47d660fd42
Jacob Ford <j@cobford.com>
2022-02-15 03:12:46 -0500

    Update clay from 1.8.1 to 1.8.3 (#119174)

--- Casks/clay.rb
+++ Casks/clay.rb
@@ -1,12 +1,12 @@
 cask "clay" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  version "1.8.1"
+  version "1.8.3"
 
   if Hardware::CPU.intel?
-    sha256 "8d683d4d5edef370b9adf78d985dd9190474144b4278f227f489a2a04bd1f840"
+    sha256 "29d59007b76685142267bf15134cf63719e02328bf93d1fe7a7611b7d9d73fb2"
   else
-    sha256 "63a3d5da50c14d5fb4cf2215bbd86bf88c3f68a5fff3e9ca7fdd7cefc7fbb39b"
+    sha256 "3228d7d05db41baf9f90520526d82968956c1ce9ecfba15a17a45d6064cc6e5a"
   end
 
   url "https://assets.clay.earth/desktop/mac/Clay-#{version}#{arch}.dmg"
