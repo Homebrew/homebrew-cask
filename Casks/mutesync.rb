5d27afe89c26c5b878f8dae551f3f1e2102a7577
Giovanni Merlino <gmerlino@unime.it>
2022-02-15 15:42:46 +0100

    Update mutesync from 4.11.2 to 4.12.5 (#119222)

--- Casks/mutesync.rb
+++ Casks/mutesync.rb
@@ -1,6 +1,6 @@
 cask "mutesync" do
-  version "4.11.2"
-  sha256 "4e9184531bbddbf833a4983f23d2df1bff7f7c0171dc09400de957940f860b28"
+  version "4.12.5"
+  sha256 "1ea435c97d036fd66e72ac7f94e6d11627597957f30288bde2802295f6dbe717"
 
   url "https://mutesync.s3-us-west-2.amazonaws.com/mutesync-#{version}.dmg",
       verified: "mutesync.s3-us-west-2.amazonaws.com/"
