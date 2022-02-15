fc61766488bbba92caea702e185589c2f2e45322
neutric <5984479+neutric@users.noreply.github.com>
2022-02-15 02:32:15 +0100

    Update lark from 5.2.10 to 5.2.23 (#119191)

--- Casks/lark.rb
+++ Casks/lark.rb
@@ -2,8 +2,8 @@ cask "lark" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
   if Hardware::CPU.intel?
-    version "5.2.10,6acc59"
-    sha256 "91ead4c00a8219a8f80eada571a5350258073460a457c364af40f3592e56f091"
+    version "5.2.23,5b373f"
+    sha256 "fb1e17ee5d6cc9a92a86d3453ed4309dc500eaee59dcd2e9b5fde17d3acb4cd2"
   else
     version "4.10.14,40a957"
     sha256 "fb8f06d56ac036a40f7bba037e01f4428fe9a54ae9d6540d0c4639c0a4213dc4"
