9690a34f28b11619a1577dc29ba0c17b4c3fd075
Miccal Matthews <miccal.matthews@gmail.com>
2022-02-16 02:01:54 +0800

    Update paragon-camptune.rb

--- Casks/paragon-camptune.rb
+++ Casks/paragon-camptune.rb
@@ -1,11 +1,20 @@
 cask "paragon-camptune" do
-  version :latest
+  version "10.17.262"
   sha256 :no_check
 
   url "https://dl.paragon-software.com/demo/camptune_demo_u.dmg"
-  name "paragon-camptune"
+  name "Paragon CampTune"
   desc "Manage disk space on Macs with Boot Camp"
   homepage "https://www.paragon-software.com/home/camptune/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist do |versions|
+      versions.values.map(&:short_version).compact.first
+    end
+  end
+
+  depends_on macos: ">= :sierra"
 
   app "Paragon CampTune.app"
 end
