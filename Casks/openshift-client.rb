d2b0eb683ff500b0ff06d2e4b1d4524173789823
Miccal Matthews <miccal.matthews@gmail.com>
2022-02-15 09:32:55 +0800

    Update openshift-client.rb

--- Casks/openshift-client.rb
+++ Casks/openshift-client.rb
@@ -2,13 +2,13 @@ cask "openshift-client" do
   version "4.9.21"
   sha256 "583e18caa5377fc269a0d4889c3866ddb119947a1cfa8c1f5fcd3a442768f8cf"
 
-  url "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/#{version}/openshift-client-mac.tar.gz"
+  url "https://mirror.openshift.com/pub/openshift-v#{version.major}/clients/ocp/#{version}/openshift-client-mac.tar.gz"
   name "Openshift Client"
   desc "OpenShift Command-line client, part of OKD"
   homepage "https://www.openshift.com/"
 
   livecheck do
-    url "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/"
+    url "https://mirror.openshift.com/pub/openshift-v#{version.major}/clients/ocp/"
     regex(%r{href=["']?v?(\d+(?:\.\d+)+)/?["' >]}i)
   end
 
