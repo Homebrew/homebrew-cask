172df6a5f9b4142dca49dbb0afb73c09b55ef77d
LangLangBart <92653266+LangLangBart@users.noreply.github.com>
2022-02-15 18:41:01 +0100

    [Coteditor] Adding a conditional for macOS Catalina (#119208)
    * Coteditor 4.1.0 no longer supports Catalina
    
    Adding a conditional for Catalina
    
    * coteditor: skip livecheck on older macOS releases
    
    * Update coteditor.rb
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/coteditor.rb
+++ Casks/coteditor.rb
@@ -8,6 +8,9 @@ cask "coteditor" do
   elsif MacOS.version <= :mojave
     version "3.9.7"
     sha256 "be34d4f800e73cc8363d8b83e1b257a06176dc85d345d680149b108f51686cf2"
+  elsif MacOS.version <= :catalina
+    version "4.0.9"
+    sha256 "969e891f4a36146c317150806fee01559d177f956734595c73537affc8897e79"
   else
     version "4.1.0"
     sha256 "134796c3515ccffade82e914e95d4b570709dbf581b9016ffbee72edf01f0ee0"
@@ -20,9 +23,13 @@ cask "coteditor" do
   homepage "https://coteditor.com/"
 
   livecheck do
-    url :url
-    strategy :github_latest
-    regex(%r{href=.*?/tag/v?(\d+(?:[.-]\d+)+)["' >]}i)
+    if MacOS.version >= :big_sur
+      url :url
+      regex(%r{href=.*?/tag/v?(\d+(?:[.-]\d+)+)["' >]}i)
+      strategy :github_latest
+    else
+      skip "version is static on older macOS releases"
+    end
   end
 
   auto_updates true
