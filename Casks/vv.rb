c2dfdc965387c01d4626c30cc174b8b3dc901aa4
Clément L <porkepix@gmail.com>
2022-02-15 23:59:19 +0100

    Update vv from 2.4.9 to 2.4.10 (#119165)

--- Casks/vv.rb
+++ Casks/vv.rb
@@ -1,6 +1,6 @@
 cask "vv" do
-  version "2.4.9"
-  sha256 "cba8568123b666cc25b8427d629c1afed3bf85fab9a59913cc7669ebc38005d4"
+  version "2.4.10"
+  sha256 "b6060d1105745640728fd7cdcc6a312a1d497a29cb30e5575cfb3318cd9577e8"
 
   url "https://github.com/vv-vim/vv/releases/download/v#{version}/VV-#{version}.dmg"
   name "VV"
