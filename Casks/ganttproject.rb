c3cfd7cb6523e0a22afb341a198ddfda15693e18
Andreas Eitel <github-aneitel@online.de>
2022-02-16 23:32:05 +0100

    update ganttproject from 3.1.3100 to 3.2.3200

--- Casks/ganttproject.rb
+++ Casks/ganttproject.rb
@@ -1,6 +1,6 @@
 cask "ganttproject" do
-  version "3.1.3200"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  version "3.2.3200"
+  sha256 "6dde4a151cabdf69fe84a3ea6d4b943233c8f9cb181bdac9168a6748e74778a6"
   
   url "https://github.com/bardsoftware/ganttproject/releases/download/ganttproject-#{version}/ganttproject-#{version}.dmg",
       verified: "github.com/bardsoftware/ganttproject/"
