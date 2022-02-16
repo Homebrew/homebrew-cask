87be3cd79d41bcc9e1243ee2ca0832e18a562782
ann0see <20726856+ann0see@users.noreply.github.com>
2022-02-16 12:33:50 +0100

    Provide legacy artifact for Jamulus (#119262)
    Related to https://github.com/Homebrew/discussions/discussions/2961#discussioncomment-2184802
    Co-authored-by: neutric <neutric@users.noreply.github.com>

--- Casks/jamulus.rb
+++ Casks/jamulus.rb
@@ -1,8 +1,15 @@
 cask "jamulus" do
   version "3.8.1"
-  sha256 "79fffe5c72c38260e6e5edff07ecccb355995c502d8a4a8c710eeccff8284624"
 
-  url "https://downloads.sourceforge.net/llcon/jamulus_#{version}_mac.dmg",
+  if MacOS.version <= :sierra
+    sha256 "d425ab355ae03849638829f68a71b8497139b5919cb3ce52a9f6742ba2101d62"
+    suffix = "_legacy"
+  else
+    sha256 "79fffe5c72c38260e6e5edff07ecccb355995c502d8a4a8c710eeccff8284624"
+    suffix = ""
+  end
+
+  url "https://downloads.sourceforge.net/llcon/jamulus_#{version}_mac#{suffix}.dmg",
       verified: "downloads.sourceforge.net/llcon/"
   name "Jamulus"
   desc "Play music online with friends"
