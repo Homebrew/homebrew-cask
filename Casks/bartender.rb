c987e557dd7f28cdaf86d919db9693c58328eac8
ROGERIO PRADO DE JESUS <rogeriopradoj@gmail.com>
2022-02-16 08:57:38 -0300

    Update bartender from 4.1.31,41031 to 4.1.31

--- Casks/bartender.rb
+++ Casks/bartender.rb
@@ -10,7 +10,7 @@ cask "bartender" do
     url "https://macbartender.com/B2/updates/#{version.csv.first.dots_to_hyphens}/Bartender%20#{version.major}.zip",
         referer: "https://www.macbartender.com"
   else
-    version "4.1.31,41031"
+    version "4.1.31"
     sha256 "3e3adda6f5321fd404aa6048fbf151e5f6690051e2dd03c6ee408a007869636b"
     url "https://macbartender.com/B2/updates/#{version.csv.first.major}-#{version.csv.first.minor}-#{version.csv.first.patch.rjust(2, "0")}/Bartender%20#{version.major}.dmg",
         referer: "https://www.macbartender.com"
