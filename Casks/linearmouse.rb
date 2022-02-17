3f21ca0a37076b96292ffb547396ec906904d707
Jiahao Lu <lujjjh@gmail.com>
2022-02-17 17:04:46 +0800

    Update linearmouse from 0.4.3 to 0.5.1 (#119312)

--- Casks/linearmouse.rb
+++ Casks/linearmouse.rb
@@ -1,6 +1,6 @@
 cask "linearmouse" do
-  version "0.4.3"
-  sha256 "70d25deaf27ef5d7e6635aae9894227351e08b910c5959c1d0d400d3152fbaf2"
+  version "0.5.1"
+  sha256 "10c9e2e886a8e57e881f72f34218a3d6b04c203bc903056205b3d9c881bc4d57"
 
   url "https://github.com/linearmouse/linearmouse/releases/download/v#{version}/LinearMouse.dmg",
       verified: "github.com/linearmouse/linearmouse/"
