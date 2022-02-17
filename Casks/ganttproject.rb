3618c463c8479e29d9627b21cd64d73e9e8b927d
Clément L <porkepix@gmail.com>
2022-02-17 03:09:55 +0100

    Update ganttproject from 3.1.3100 to 3.2.3200 (#119263)

--- Casks/ganttproject.rb
+++ Casks/ganttproject.rb
@@ -1,6 +1,6 @@
 cask "ganttproject" do
-  version "3.1.3100"
-  sha256 "397f7d41be9786bd1aab8bca2429df4d2474c6cfeea3566263d026554ffb5507"
+  version "3.2.3200"
+  sha256 "6dde4a151cabdf69fe84a3ea6d4b943233c8f9cb181bdac9168a6748e74778a6"
 
   url "https://github.com/bardsoftware/ganttproject/releases/download/ganttproject-#{version}/ganttproject-#{version}.dmg",
       verified: "github.com/bardsoftware/ganttproject/"
