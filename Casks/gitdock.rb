2ffca72fbad052cb97152767dc8dcde098c37306
Marcel van Remmerden <maclbb@hotmail.de>
2022-02-17 10:06:02 +0100

    Update gitdock from 0.1.20 to 0.1.21 (#119271)

--- Casks/gitdock.rb
+++ Casks/gitdock.rb
@@ -1,6 +1,6 @@
 cask "gitdock" do
-  version "0.1.20"
-  sha256 "5297f2e59d9ae24edd6b35f590977fd4e521546b27bc2136135063a2de2a4449"
+  version "0.1.21"
+  sha256 "ce806c905921e47dba02029c15cb7bfb8c95626a8571f4e99d53ae345ba5a21c"
 
   url "https://gitlab.com/mvanremmerden/gitdock/-/releases/v#{version}/downloads/GitDock-#{version}.dmg"
   name "GitDock"
