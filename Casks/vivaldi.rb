58080db10c8aed2f790753c06af48864443a32c6
owine <owine@users.noreply.github.com>
2022-02-17 03:05:49 -0600

    Update vivaldi from 5.1.2567.46 to 5.1.2567.49 (#119274)

--- Casks/vivaldi.rb
+++ Casks/vivaldi.rb
@@ -1,6 +1,6 @@
 cask "vivaldi" do
-  version "5.1.2567.46"
-  sha256 "02d3faddf13923c3791e6cc89c486b35306f87659e24f9a344bbb34f1a750590"
+  version "5.1.2567.49"
+  sha256 "d8783d4b00758526c4b70f0345d1b0cb94a533d1dd92daed962634aee49bc18f"
 
   url "https://downloads.vivaldi.com/stable/Vivaldi.#{version}.universal.dmg"
   name "Vivaldi"
