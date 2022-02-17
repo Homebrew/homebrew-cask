4dacfca1a96c84ef3e0f220c6b77930639d5a07b
bruggesser <bruggesser@gmx.net>
2022-02-17 02:54:15 +0100

    Update openwebstart from 1.5.1 to 1.5.2 (#119276)
    * Update openwebstart from 1.5.1 to 1.5.2
    
    * Update openwebstart.rb
    
    * Update openwebstart.rb
    
    * Update openwebstart.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/openwebstart.rb
+++ Casks/openwebstart.rb
@@ -1,6 +1,6 @@
 cask "openwebstart" do
-  version "1.5.1"
-  sha256 "5836d95e3bc326ba985e5fa6ea63bdf8efe93973115b27ab6a530368e798a6c2"
+  version "1.5.2"
+  sha256 "7e53421fd008b8ed18e1f805921e37e7622d72aa3a65f137a8ca808d36e0b696"
 
   url "https://github.com/karakun/OpenWebStart/releases/download/v#{version}/OpenWebStart_macos_#{version.dots_to_underscores}.dmg",
       verified: "github.com/karakun/OpenWebStart/"
@@ -20,9 +20,10 @@ cask "openwebstart" do
   end
 
   uninstall script: {
-    executable: "/Applications/OpenWebStart/OpenWebStart Uninstaller.app/Contents/MacOS/JavaApplicationStub",
-    args:       ["-c"],
-    sudo:       true,
+    executable:   "/Applications/OpenWebStart/OpenWebStart Uninstaller.app/Contents/MacOS/JavaApplicationStub",
+    args:         ["-q"],
+    sudo:         true,
+    print_stderr: false,
   },
             delete: "/Applications/OpenWebStart"
 
