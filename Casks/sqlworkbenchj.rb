5cdfed283b224b48a9590ea6427b8ce627c05f1f
Miccal Matthews <miccal.matthews@gmail.com>
2022-02-17 11:22:18 +0800

    Update sqlworkbenchj.rb (#119300)

--- Casks/sqlworkbenchj.rb
+++ Casks/sqlworkbenchj.rb
@@ -1,15 +1,15 @@
 cask "sqlworkbenchj" do
-  version "128"
+  version "124"
   sha256 "2173c7f00172bef3fed23e7f57e168a9d156c72c311af032ba469139f80d9fe9"
 
-  url "https://www.sql-workbench.eu/Workbench-Build#{version}-Mac.tgz"
+  url "https://www.sql-workbench.eu/archive/Workbench-Build#{version}-Mac.tgz"
   name "SQL Workbench/J"
   desc "DBMS-independent SQL query tool"
   homepage "https://www.sql-workbench.eu/"
 
   livecheck do
-    url "https://www.sql-workbench.eu/downloads.html"
-    regex(/Current\s+stable\s+release:\s+Build\s+(\d+)/i)
+    url "https://www.sql-workbench.eu/download-archive.html"
+    regex(/Workbench[._-]Build(\d+)[._-]Mac\.tgz/i)
   end
 
   app "SQLWorkbenchJ.app"
