8109f6ea16a0800dbaffa3e6b9a0eb7efd9ac2d8
Kenneth C. Arnold <kenneth.arnold@gmail.com>
2022-02-18 08:13:59 -0500

    Update xournal-plus-plus from 1.1.0 to 1.1.1 (#119341)
    Automatic version bump failed because the maintainers switched tag naming conventions,
    see 'Note to distro maintainers' at https://github.com/xournalpp/xournalpp/releases/tag/v1.1.1

--- Casks/xournal-plus-plus.rb
+++ Casks/xournal-plus-plus.rb
@@ -1,8 +1,8 @@
 cask "xournal-plus-plus" do
-  version "1.1.0"
-  sha256 "fe713a9fb3d796a71bbeb6eefac24c11e8c3e4a907dc90cb6c0ea90695d18246"
+  version "1.1.1"
+  sha256 "9368414dc210ce6746735cf4def6692a1dee05e847ee3fb5b3a06b0fe45ef963"
 
-  url "https://github.com/xournalpp/xournalpp/releases/download/#{version}/xournalpp-#{version}-macos.zip"
+  url "https://github.com/xournalpp/xournalpp/releases/download/v#{version}/xournalpp-#{version}-macos.zip"
   name "Xournal++"
   desc "Handwriting notetaking software"
   homepage "https://github.com/xournalpp/xournalpp"
