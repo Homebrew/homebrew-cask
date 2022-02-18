5f09faa29f29e83982318e502974ad2e20ab9824
owine <owine@users.noreply.github.com>
2022-02-18 07:15:19 -0600

    Update fantastical from 3.6 to 3.6.1 (#119344)

--- Casks/fantastical.rb
+++ Casks/fantastical.rb
@@ -1,6 +1,6 @@
 cask "fantastical" do
-  version "3.6,1392"
-  sha256 "160e12f70c1dd43e603caa9ff81ffed03a8987c3127ad377d06c97eea8de6c25"
+  version "3.6.1,1402"
+  sha256 "94fa6ade86848cb6a0a25d11f52d76e447325cf1545e3b66f8cc1e7c804941e5"
 
   url "https://cdn.flexibits.com/Fantastical_#{version.csv.first}.zip"
   name "Fantastical"
