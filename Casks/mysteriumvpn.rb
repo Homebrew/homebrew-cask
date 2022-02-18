2f6514ff95a2806ff9cff87c7f890a5483343d53
otkd <7527203+otkd@users.noreply.github.com>
2022-02-18 02:33:03 -0500

    Add MysteriumVPN.app v10.5.3
    Signed-off-by: otkd <7527203+otkd@users.noreply.github.com>

---
+++ Casks/mysteriumvpn.rb
@@ -0,0 +1,25 @@
+cask "mysteriumvpn" do
+  version "10.5.3"
+  sha256 "a05b1803b2eaca44d54067f9ee2c0f76b4d5baa2a305e9805c773c4b4d65de6c"
+
+  url "https://github.com/mysteriumnetwork/mysterium-vpn-desktop/releases/download/#{version}/MysteriumVPN-#{version}.dmg",
+      verified: "github.com/mysteriumnetwork/mysterium-vpn-desktop/"
+  name "Mysterium VPN"
+  desc "Safe, secure and affordable decentralized VPN"
+  homepage "https://www.mysteriumvpn.com/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "MysteriumVPN.app"
+
+  zap trash: [
+    "~/Library/Logs/MysteriumVPN",
+    "~/.mysterium",
+    "/var/log/myst_supervisor.log",
+    "~/Library/Preferences/network.mysterium.mysterium-vpn-desktop.plist",
+    "~/Library/Saved Application State/network.mysterium.mysterium-vpn-desktop.savedState",
+  ]
+end
