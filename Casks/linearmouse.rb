43e36cb33d8b8975bdb66d45d04b5fb307888e5c
Jiahao Lu <lujjjh@gmail.com>
2022-02-18 21:09:10 +0800

    Update linearmouse from 0.5.1 to 0.5.2 (#119362)

--- Casks/linearmouse.rb
+++ Casks/linearmouse.rb
@@ -1,6 +1,6 @@
 cask "linearmouse" do
-  version "0.5.1"
-  sha256 "10c9e2e886a8e57e881f72f34218a3d6b04c203bc903056205b3d9c881bc4d57"
+  version "0.5.2"
+  sha256 "11321739d4be8406bbefe5a0e19db402c34ea79b8c36fe160da9724f20673993"
 
   url "https://github.com/linearmouse/linearmouse/releases/download/v#{version}/LinearMouse.dmg",
       verified: "github.com/linearmouse/linearmouse/"
@@ -8,6 +8,7 @@ cask "linearmouse" do
   desc "Customize mouse behavior"
   homepage "https://linearmouse.org/"
 
+  auto_updates true
   depends_on macos: ">= :catalina"
 
   app "LinearMouse.app"
