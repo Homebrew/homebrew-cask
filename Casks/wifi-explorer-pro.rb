86304505c0abd5827e2f129206be618f403c5733
Zhenfu Shi <szf1234@me.com>
2022-02-18 21:05:04 +0800

    Update wifi-explorer-pro from 3.4.2 to 3.4.3 (#119357)

--- Casks/wifi-explorer-pro.rb
+++ Casks/wifi-explorer-pro.rb
@@ -1,6 +1,6 @@
 cask "wifi-explorer-pro" do
-  version "3.4.2,21"
-  sha256 "07144100479dadb01ac5f4fe435bb36b717c8b454ea71a744e6f57b3a1be1dc8"
+  version "3.4.3,22"
+  sha256 "52f6e211e96f5b3f356ca04a7f3c03dcfb5518ea64bbab2168b563d32ab5d0cd"
 
   url "https://www.intuitibits.com/appcasts/WiFiExplorerPro_#{version.csv.first}.zip"
   name "WiFi Explorer Pro"
