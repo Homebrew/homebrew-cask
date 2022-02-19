2f731ee816ea1e3b85f86336a2ebc9dd47a0bae7
Jaakko Keränen <jaakko.keranen@iki.fi>
2022-02-19 17:07:45 +0200

    Update lagrange from 1.10.5 to 1.10.6 (#119431)

--- Casks/lagrange.rb
+++ Casks/lagrange.rb
@@ -1,12 +1,12 @@
 cask "lagrange" do
   arch = Hardware::CPU.intel? ? "macos10.13-x86_64" : "macos11.0-arm64"
 
-  version "1.10.5"
+  version "1.10.6"
 
   if Hardware::CPU.intel?
-    sha256 "493c032c4224bb0f2c4a512a6958d39cf3b2d6c9d0683bb8173203017c1dabf0"
+    sha256 "d5c1ddb4f0004c2aa05fe416a30bbe0cb0252a43b4f5de64ef8a0d95c99cd63d"
   else
-    sha256 "4f21e8937f5f607f069c0f262bedb34cd52beefe89468f7d5c6cbbd38cda7ada"
+    sha256 "85496e7f2a25b4853c1a06bab6dea57293ad15e94ac94f58de02c6df97b2c3cb"
   end
 
   url "https://github.com/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_#{arch}.tbz",
