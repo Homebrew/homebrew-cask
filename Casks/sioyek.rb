0c4bdb834a674f4478c72cf9309b1781f89e660d
Alan Chen <github@zenan.ch>
2022-02-19 11:37:07 -0600

    Update to 1.1.0

--- Casks/sioyek.rb
+++ Casks/sioyek.rb
@@ -1,5 +1,5 @@
 cask "sioyek" do
-  version "1.0.0"
+  version "1.1.0"
   sha256 "e75e42158e0901120bbb975fc8402bdfab092cc2b736f3ce5039f0f89e15fc04"
 
   url "https://github.com/ahrm/sioyek/releases/download/v#{version}/sioyek-release-mac.zip",
