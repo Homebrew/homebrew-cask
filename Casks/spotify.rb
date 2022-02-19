e8207ed5c50d51ebaae9180aa3f4ff416bf1f15f
Branch Vincent <branchevincent@gmail.com>
2022-02-19 09:53:03 -0500

    Update spotify from 1.1.78.765 to 1.1.79.763 (#119381)

--- Casks/spotify.rb
+++ Casks/spotify.rb
@@ -2,7 +2,7 @@ cask "spotify" do
   arch = Hardware::CPU.intel? ? "" : "ARM64"
 
   if Hardware::CPU.intel?
-    version "1.1.78.765,5ea20b00,16"
+    version "1.1.79.763,c2965cdf,21"
   else
     version "1.1.78.765,5ea20b00,11"
   end
