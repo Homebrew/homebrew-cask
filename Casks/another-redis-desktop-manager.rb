336cb093a7dbdff3ea1c8b5440f7bc4b6c5e589c
marchpane <89104892+marchpane@users.noreply.github.com>
2022-02-19 15:52:36 +0100

    Fix SHA256 Value (#119385)

--- Casks/another-redis-desktop-manager.rb
+++ Casks/another-redis-desktop-manager.rb
@@ -6,7 +6,7 @@ cask "another-redis-desktop-manager" do
   if Hardware::CPU.intel?
     sha256 "a734bd5c095eeb29863a3d4c05b6d1d2c9d1ba64fab7b2f7007a5eff50b50835"
   else
-    sha256 "2c7febc52dea1e860d4ba6ec528e9a8e7516df9f0067cd188c978f479f593334"
+    sha256 "653b977987407b42fd82fb5fe1830583d7d1fbb8bf32b5ae70f3ed96f238fd92"
   end
 
   url "https://github.com/qishibo/AnotherRedisDesktopManager/releases/download/v#{version}/Another-Redis-Desktop-Manager#{arch}#{version}.dmg"
