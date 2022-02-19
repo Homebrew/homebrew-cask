75075ae4ab25d57b5d1516cb64d54768f17590f6
Porkepix <porkepix@gmail.com>
2022-02-19 13:44:39 +0100

    Update natron from 2.4.2 to 2.4.3

--- Casks/natron.rb
+++ Casks/natron.rb
@@ -1,12 +1,12 @@
 cask "natron" do
-  version "2.4.2"
+  version "2.4.3"
 
   if MacOS.version <= :mojave
     sha256 "437979f4b94e718d7e7eb399715f851734900b7453bd3fe6a41d3a43e733a95a"
     url "https://github.com/NatronGitHub/Natron/releases/download/vsha/Natron-#{version}-OSX109-x86_64.dmg",
         verified: "github.com/NatronGitHub/Natron/"
   elsif MacOS.version <= :catalina
-    sha256 "c9cf39ced992e15c6bcbc6bb1c960e36398561c5858b216f07413ccf3335efe8"
+    sha256 "89ca3cca577a49023c03bb617be2a694e71fd4b8f1b1a2bbf83560f3f1450d7a"
     url "https://github.com/NatronGitHub/Natron/releases/download/v#{version}/Natron-#{version}-macOS1015-x86_64.dmg",
         verified: "github.com/NatronGitHub/Natron/"
   else
