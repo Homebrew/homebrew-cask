9398afe5fcc8b52709ae3185f67ba0c76b8d3877
Slobodan Marković <smarkovic@users.noreply.github.com>
2022-02-19 19:35:08 +0100

    Update webex from 42.2.0.21338 to 42.2.0.21486 (#119433)
    Co-authored-by: Slobodan Marković <sloba.markovic@gmail.com>

--- Casks/webex.rb
+++ Casks/webex.rb
@@ -1,7 +1,7 @@
 cask "webex" do
   arch = Hardware::CPU.intel? ? "TeamsDesktop-MACOS" : "Desktop-MACOS-Apple-Silicon"
 
-  version "42.2.0.21338"
+  version "42.2.0.21486"
   sha256 :no_check
 
   url "https://binaries.webex.com/Webex#{arch}-Gold/Webex.dmg"
