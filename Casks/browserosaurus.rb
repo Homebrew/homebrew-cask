4abaa7d1184440af337604157988f606733d742f
Clément L <porkepix@gmail.com>
2022-02-20 23:52:23 +0100

    Update browserosaurus from 17.2.0 to 17.3.0 (#119452)

--- Casks/browserosaurus.rb
+++ Casks/browserosaurus.rb
@@ -1,12 +1,12 @@
 cask "browserosaurus" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "17.2.0"
+  version "17.3.0"
 
   if Hardware::CPU.intel?
-    sha256 "4933cb7262f69f2c6d3847b4d2df5fbf6307d60f45a1ae72e853415ae39a0e6a"
+    sha256 "23e51064c8a0eb6745932f14f4c2ab81e8241771e9f80fd8f11bbeb863276907"
   else
-    sha256 "c45b1205fa7cecb83691c02f868dce779721e77aa056af07c0d02cc0970928a6"
+    sha256 "db04b58ea91abff53bec0a266af39486ebb7483f3740572b55b4515a85d8fc90"
   end
 
   url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-darwin-#{arch}-#{version}.zip"
