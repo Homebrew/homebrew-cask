043f8d83f4a01ef98aae7a020b61ef18cf4b0f0d
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2022-02-21 03:52:31 +0500

    Update arctype to v0.9.49 (#119451)

--- Casks/arctype.rb
+++ Casks/arctype.rb
@@ -1,6 +1,6 @@
 cask "arctype" do
-  version "0.9.48"
-  sha256 "8294b6d5c68575cf349fa249793615d63a4452dd8905ffcbc65f7700b50f84fb"
+  version "0.9.49"
+  sha256 "e024eedd954d7f0dfc60330b9186a0e50bcfb3ef93f58a02d112d9c696c336b1"
 
   url "https://arctype-downloads.s3-us-west-2.amazonaws.com/updates/Arctype-#{version}.dmg",
       verified: "arctype-downloads.s3-us-west-2.amazonaws.com/updates/"
