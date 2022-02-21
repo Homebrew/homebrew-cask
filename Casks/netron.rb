edacfb0c44cd13a117c026b8d7b4467f70836cb4
Lutz <lutzroeder@users.noreply.github.com>
2022-02-21 22:39:04 +0000

    Update Netron to 5.5.9

--- Casks/netron.rb
+++ Casks/netron.rb
@@ -1,16 +1,11 @@
 cask "netron" do
-  version "5.5.7"
-  sha256 "5b243272066d978616d2d0ea7f0899b7924d94b4c9169eb386c4bc75918f4994"
+  version "5.5.9"
+  sha256 "e0ede5d9e22fdf049e91a566806e53f40add53b4c6bbf12282f3c4ddc4d87061"
 
   url "https://github.com/lutzroeder/netron/releases/download/v#{version}/Netron-#{version}-mac.zip"
   name "Netron"
   desc "Visualizer for neural network, deep learning, and machine learning models"
   homepage "https://github.com/lutzroeder/netron"
-
-  livecheck do
-    url :url
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
-  end
 
   auto_updates true
 
