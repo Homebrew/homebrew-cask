e6008398fd090b3426bd4d0ad78d8e2ed3c2f2f9
Porkepix <porkepix@gmail.com>
2022-02-21 13:04:51 +0100

    jamulus-3.8.2: update legacy hash

--- Casks/jamulus.rb
+++ Casks/jamulus.rb
@@ -2,7 +2,7 @@ cask "jamulus" do
   version "3.8.2"
 
   if MacOS.version <= :sierra
-    sha256 "d425ab355ae03849638829f68a71b8497139b5919cb3ce52a9f6742ba2101d62"
+    sha256 "e6492ec6355166d88d48be7d7d3a242739c112841c1f826451be7a5adb0cbd00"
     suffix = "_legacy"
   else
     sha256 "d1f85e0ca04a2a648890a84bb3740ec36477f6f28e6cc3a35883aef7e655be50"
