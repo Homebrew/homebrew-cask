d665b4c8fba168bd1c37a6284a4c1abe3f08c8e2
ShredderWalnut <34428332+ShredderWalnut@users.noreply.github.com>
2022-02-21 18:48:09 +0800

    Update reaper from 6.48 to 6.49 (#119475)

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,11 +1,11 @@
 cask "reaper" do
-  version "6.48"
+  version "6.49"
 
   if MacOS.version <= :mojave
     sha256 "7eb0fb7df77e8ab2e2b14b49d45b08f9c1973421fc5a8f5672c00778feb122bd"
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64.dmg"
   else
-    sha256 "b6a429060265aa5eb149a0cbd16e8e478f78bdfc559210d88e14b6dbd31c2475"
+    sha256 "92138faf09e32c1b265246d9742587693802a1e483bd7acb4b80ab3ad995eb89"
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}_universal.dmg"
   end
 
