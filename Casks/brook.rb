da41cd6815e9bffc5eb23964f7aaac631585cc03
Clément L <porkepix@gmail.com>
2022-02-21 11:56:54 +0100

    Update brook from 20210701 to 20220401 (#119482)

--- Casks/brook.rb
+++ Casks/brook.rb
@@ -1,6 +1,6 @@
 cask "brook" do
-  version "20210701"
-  sha256 "731ba0b0923296cc904553d9e74049b0ff6af04cf4dd720db61f7339e7122f4f"
+  version "20220401"
+  sha256 "077f27ae594131cbe6d28acb631feb1b5a58cc175157c1b1fe86597c38fe7748"
 
   url "https://github.com/txthinking/brook/releases/download/v#{version}/Brook.dmg"
   name "Brook"
