25d260fc2b489a4644a386ba66f7132794257c95
Depal1 <47154119+Depal1@users.noreply.github.com>
2022-02-21 21:43:53 -0600

    Add Eliot.app v2.1

---
+++ Casks/eliot.rb
@@ -0,0 +1,17 @@
+cask "eliot" do
+  version "2.1"
+  sha256 "c7e79f6e58a57dacfdbfdd4c0f15a632163c7c50d3bf0ae1adc3376bece128d7"
+
+  url "https://dl.sv.nongnu.org/releases/eliot/releases/#{version}/eliot-#{version}.dmg"
+  name "Eliot"
+  desc "Open source Scrabble game"
+  homepage "https://www.nongnu.org/eliot/fr/index.html"
+
+  app "Eliot.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.eliot.eliot.plist",
+    "~/Library/Preferences/org.nongnu.savannah.eliot.plist",
+    "~/Library/Saved Application State/org.nongnu.savannah.eliot.savedState",
+  ]
+end
