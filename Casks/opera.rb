747185df3dc445c5c0ccf674c0cc599656362776
Adon <13709042+adonh@users.noreply.github.com>
2022-02-22 05:14:14 -0500

    Update opera from 83.0.4254.54 to 84.0.4316.14 (#119524)

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask "opera" do
-  version "83.0.4254.54"
-  sha256 "6dd9fed0695e742875ea5a69357ea02935941eff1a1f2ca75af1801ad9d6b27c"
+  version "84.0.4316.14"
+  sha256 "70dc18bf11e7b1541b8e5017138cfd3855b22e2daf74e9d679803775b4e37033"
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   name "Opera"
