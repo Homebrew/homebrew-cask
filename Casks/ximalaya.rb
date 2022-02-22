72109d0c4d80fe4d9d95a5444ee9b7dc4936c64b
MoonFruit <dkmoonfruit@gmail.com>
2022-02-22 21:35:00 +0800

    Update ximalaya from 2.4.0 to 2.5.0 (#119539)

--- Casks/ximalaya.rb
+++ Casks/ximalaya.rb
@@ -1,6 +1,6 @@
 cask "ximalaya" do
-  version "2.4.0,1642348063"
-  sha256 "efcf214b1563fd5f139fd00a6165fb1f7b20af6dfdfc3e03737ede199e7407e4"
+  version "2.5.0,1645498451"
+  sha256 "ae46314cfc96ddd6ab7a50cf5181ab30c38cdac49fc84dc5e3b37f48aa920828"
 
   url "https://s1.xmcdn.com/yx/ximalaya-pc-client/#{version.csv.first}/download/mac-x64/Ximalaya_#{version.csv.first}_#{version.csv.second}.dmg",
       verified: "s1.xmcdn.com/yx/ximalaya-pc-client/"
