658eb4e0eb25273eec6593a8ded649ea3a8cb3f5
neutric <5984479+neutric@users.noreply.github.com>
2022-02-23 09:26:08 +0100

    Update marvel from 10.9.7 to 10.9.8 (#119560)

--- Casks/marvel.rb
+++ Casks/marvel.rb
@@ -1,6 +1,6 @@
 cask "marvel" do
-  version "10.9.7,5072"
-  sha256 "87e24bb48022ab88c91d9a9509220943728004d6578df68cd2b52a9c5b7724fd"
+  version "10.9.8,5092"
+  sha256 "772c5695aadd20d54cbb73a329a33625fc6e53ea167b6c9a441b16186abcc1a1"
 
   url "https://storage.googleapis.com/sketch-plugin/#{version.csv.first}/Marvel.zip",
       verified: "storage.googleapis.com/sketch-plugin/"
