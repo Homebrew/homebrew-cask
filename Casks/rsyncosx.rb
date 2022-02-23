9884d577ba45f41892cdd3737c28967f9ac3e4f2
Clément L <porkepix@gmail.com>
2022-02-23 14:33:01 +0100

    Update rsyncosx from 6.6.9 to 6.7.1 (#119535)

--- Casks/rsyncosx.rb
+++ Casks/rsyncosx.rb
@@ -1,6 +1,6 @@
 cask "rsyncosx" do
-  version "6.6.9"
-  sha256 "71de1445959f1c7bf7ea47c03e36dbc463100735fdeaa5fd92ed823c2f9f99df"
+  version "6.7.1"
+  sha256 "24a5f26e1d29ee915b8549525a32998c55cba86d24be4beb6ec4faf4b9081b38"
 
   url "https://github.com/rsyncOSX/RsyncOSX/releases/download/v#{version}/RsyncOSX.#{version}.dmg"
   name "RsyncOSX"
