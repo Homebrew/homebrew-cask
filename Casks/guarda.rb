df31a1ab7fa2be0fd46732c9f194402ac7dcbec9
Jonathan Stanley <jonathanstanley@users.noreply.github.com>
2022-02-23 00:58:55 -0500

    Create guarda.rb

---
+++ Casks/guarda.rb
@@ -0,0 +1,11 @@
+cask "guarda" do
+  version "1.0.20"
+  sha256 "69e88c2ccb05ec2f8004045f0d0940ae578b51992975e6b8bdbda78e0e95e044"
+
+  url "https://github.com/guardaco/guarda-desktop-releases/releases/download/v#{version}/Guarda-#{version}.dmg", verified: "github.com/guardaco/"
+  name "guarda"
+  desc "Multi-chain, non-custodial, crypto wallet"
+  homepage "https://guarda.com/"
+
+  app "Guarda.app"
+end
