72302ceb0feba74ea3097507348ad81f73d8aa08
Mykola Rybak <mykola.rybak@gmail.com>
2022-02-24 01:19:15 +0200

    Fix style

--- Casks/gargoyle.rb
+++ Casks/gargoyle.rb
@@ -2,6 +2,7 @@ cask "gargoyle" do
   arch = Hardware::CPU.intel? ? "mac-intel" : "mac-arm64"
 
   version "2022.1"
+
   if Hardware::CPU.intel?
     sha256 "68448156799315e9204079559b451b6012f8e2d7e1368e32412b1f32f01157e4"
   else
