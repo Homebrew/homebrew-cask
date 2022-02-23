8ffa96754bdc7f2af5d778d807e6132169fff62f
Aren Patel <aren55555@users.noreply.github.com>
2022-02-23 00:25:07 -0800

    Update gather from 0.1.7 to 0.2.0 (#119556)

--- Casks/gather.rb
+++ Casks/gather.rb
@@ -1,6 +1,6 @@
 cask "gather" do
-  version "0.1.7"
-  sha256 "9824bc0de4e85c045d51938f78bb2416050602205d6e7aa929e19389e388d1fe"
+  version "0.2.0"
+  sha256 "32cbb0f4806a1f6ed8d6aebd87970e14fde332143107ca8a3eab5a2b60e4e5ed"
 
   url "https://github.com/gathertown/gather-town-desktop-releases/releases/download/v#{version}/Gather-#{version}-mac.zip",
       verified: "github.com/gathertown/gather-town-desktop-releases"
