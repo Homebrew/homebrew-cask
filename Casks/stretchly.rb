935863952b3045f0dd3ffa7692faf2583674bfb5
Clément L <porkepix@gmail.com>
2022-02-23 09:58:11 +0100

    Update stretchly from 1.9.0 to 1.10.0 (#119550)

--- Casks/stretchly.rb
+++ Casks/stretchly.rb
@@ -1,6 +1,6 @@
 cask "stretchly" do
-  version "1.9.0"
-  sha256 "ca4f100f7291e5a0ddde06cec7295f843f6ae06df6551808c412f537eaaf2d15"
+  version "1.10.0"
+  sha256 "d5bff265d0d3142af98629b648ec4d39d4058aaad4d55a8f4780f1682ce1a471"
 
   url "https://github.com/hovancik/stretchly/releases/download/v#{version}/stretchly-#{version}.dmg",
       verified: "github.com/hovancik/stretchly/"
