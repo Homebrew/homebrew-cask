0a56aa5f076bd06019d20d80158a159e69514d52
neutric <5984479+neutric@users.noreply.github.com>
2022-02-23 23:23:25 +0100

    Update mu-editor from 1.0.3 to 1.1.1 (#119565)
    * Update mu-editor from 1.0.3 to 1.1.1
    
    * mu-editor: update app stanza
    
    * mu-editor: add zap

--- Casks/mu-editor.rb
+++ Casks/mu-editor.rb
@@ -1,12 +1,18 @@
 cask "mu-editor" do
-  version "1.0.3"
-  sha256 "b1815923ab38d00a5ec512912e50694e29c17f087989cd21aa52c88ce10452b0"
+  version "1.1.1"
+  sha256 "3cb81559e97ee8813d126810fb11f981b32981d22e1767204f43b1a33f6e9f11"
 
-  url "https://github.com/mu-editor/mu/releases/download/#{version}/mu-editor.dmg",
+  url "https://github.com/mu-editor/mu/releases/download/v#{version}/MuEditor-osx-#{version}.dmg",
       verified: "github.com/mu-editor/mu/"
   name "Mu"
   desc "Small, simple editor for beginner Python programmers"
   homepage "https://codewith.mu/"
 
-  app "mu-editor.app"
+  app "Mu Editor.app"
+
+  zap rmdir: "~/mu_code",
+      trash: [
+        "~/Library/Application Support/mu",
+        "~/Library/Logs/mu",
+      ]
 end
