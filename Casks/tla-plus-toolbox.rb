83a5485cbf5d886b3cad1b5cf7343a1618475933
Paul Swartz <paul@paulswartz.net>
2022-02-24 08:38:18 -0500

    fix: update SHA256 for tla-plus-toolbox
    Not sure where the initial SHA256 came from, but it does not match what's currently coming from https://github.com/tlaplus/tlaplus/releases

--- Casks/tla-plus-toolbox.rb
+++ Casks/tla-plus-toolbox.rb
@@ -1,6 +1,6 @@
 cask "tla-plus-toolbox" do
   version "1.8.0"
-  sha256 "0353bd93f318b90608f1d1bc0c7bc6b43f4631476f22b90d30be793e156bdb03"
+  sha256 "81cd2276215ceda37ca629f30886c82bdce5987133d585d3fcadd47cd3df861e"
 
   url "https://github.com/tlaplus/tlaplus/releases/download/v#{version}/TLAToolbox-#{version}-macosx.cocoa.x86_64.zip",
       verified: "github.com/tlaplus/tlaplus/"
