7404a19afbd86f659ae3ba50969bca76d2090264
neutric <5984479+neutric@users.noreply.github.com>
2022-02-24 09:29:10 +0100

    Update navicat-for-mysql from 16.0.7 to 16.0.8 (#119597)

--- Casks/navicat-for-mysql.rb
+++ Casks/navicat-for-mysql.rb
@@ -1,8 +1,8 @@
 cask "navicat-for-mysql" do
-  version "16.0.7"
+  version "16.0.8"
   sha256 :no_check
 
-  url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_mysql_en.dmg"
+  url "https://download.navicat.com/download/navicat#{version.major_minor.no_dots}_mysql_en.dmg"
   name "Navicat for MySQL"
   desc "Database administration and development tool"
   homepage "https://www.navicat.com/products/navicat-for-mysql"
