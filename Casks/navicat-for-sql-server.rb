f43ec192fed5021eb95afd6953cac06cdd518550
neutric <5984479+neutric@users.noreply.github.com>
2022-02-24 09:29:53 +0100

    Update navicat-for-sql-server from 16.0.7 to 16.0.8 (#119600)

--- Casks/navicat-for-sql-server.rb
+++ Casks/navicat-for-sql-server.rb
@@ -1,8 +1,8 @@
 cask "navicat-for-sql-server" do
-  version "16.0.7"
+  version "16.0.8"
   sha256 :no_check
 
-  url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_sqlserver_en.dmg"
+  url "https://download.navicat.com/download/navicat#{version.major_minor.no_dots}_sqlserver_en.dmg"
   name "Navicat for SQL Server"
   desc "Database administration and development tool for SQL-server"
   homepage "https://www.navicat.com/products/navicat-for-sqlserver"
