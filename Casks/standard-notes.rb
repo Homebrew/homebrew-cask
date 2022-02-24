00f753dd76aec3ead9bac1050cf2f9b1dccc88bf
Jakob Fritz Yanagibashi <heikegani1672@jy11.de>
2022-02-24 02:21:24 +0100

    Update standard-notes from 3.11.1 to 3.11.2

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -1,12 +1,12 @@
 cask "standard-notes" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "3.11.1"
+  version "3.11.2"
 
   if Hardware::CPU.intel?
-    sha256 "deef7872ce29ac59c7c926f443899da9895ffb289a9c5ad4a8bedc5121a67e3b"
+    sha256 "62ddb5913d85ce371f78379b5bdb9cbbfa6d80d99b5b7de65da5b9cbf1bdd58a"
   else
-    sha256 "c476e4d222a47331a58fc38dc6a1b45cbc9314c91ad1d4249dbe64ef9d755ec3"
+    sha256 "5a91b83eb4e03748fbbd67ac09142bc2d2647562534b0a0aaccc384a8e692dd4"
   end
 
   url "https://github.com/standardnotes/desktop/releases/download/v#{version}/Standard-Notes-#{version}-mac-#{arch}.zip",
