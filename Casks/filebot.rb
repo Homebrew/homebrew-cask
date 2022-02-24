9a032e4790522210a879b538b56e07038fa42548
Bevan Kay <email@bevankay.me>
2022-02-24 21:03:25 +1100

    filebot: bump version, add arch conditional, update zap, update livecheck

--- Casks/filebot.rb
+++ Casks/filebot.rb
@@ -1,15 +1,22 @@
 cask "filebot" do
-  version "4.9.4"
-  sha256 "d9bb3fb4c275336512c3c52c9db333a75ca2b2f9919bf271a86ae91eae160d9f"
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  url "https://get.filebot.net/filebot/FileBot_#{version}/FileBot_#{version}.app.tar.xz"
+  version "4.9.5"
+
+  if Hardware::CPU.intel?
+    sha256 "8f98cd152102dd752433dac9f7939376bb4a5bc5c8080722ecbb1b17e1dccdc9"
+  else
+    sha256 "7387674b10adae30ea69288a6828b35391d0fd32e067412d11efea2be363233b"
+  end
+
+  url "https://get.filebot.net/filebot/FileBot_#{version}/FileBot_#{version}_#{arch}.app.tar.xz"
   name "FileBot"
   desc "Tool for organizing and renaming movies, TV shows, anime or music"
   homepage "https://www.filebot.net/"
 
   livecheck do
     url "https://www.filebot.net/download.html"
-    regex(/href=.*?FileBot[._-]v?(\d+(?:\.\d+)+)\.app\.t/i)
+    regex(/href=.*?FileBot[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}\.pkg/i)
   end
 
   app "FileBot.app"
@@ -17,5 +24,12 @@ cask "filebot" do
   binary "#{appdir}/FileBot.app/Contents/Resources/bash_completion.d/filebot_completion",
          target: "#{HOMEBREW_PREFIX}/etc/bash_completion.d/filebot"
 
-  zap trash: "~/Library/Preferences/net.filebot.ui.plist"
+  zap trash: [
+    "~/Library/Application Scripts/net.filebot.FileBot",
+    "~/Library/Containers/net.filebot.FileBot",
+    "~/Library/Preferences/net.filebot.FileBot.app.plist",
+    "~/Library/Preferences/net.filebot.ui.plist",
+    "~/Library/Saved Application State/net.filebot.FileBot.app.savedState",
+    "~/Library/Saved Application State/net.filebot.FileBot.savedState",
+  ]
 end
