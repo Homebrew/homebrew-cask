50b1c6377719b4e62377661f58ae604931f20bf4
Bevan Kay <email@bevankay.me>
2022-02-25 00:49:03 +1100

    Update pushplaylabs-sidekick from 96.15.1.15334 to 96.15.1.16727 (#119626)
    * Update pushplaylabs-sidekick from 96.15.1.15334 to 96.15.1.16727
    
    * bump intel version
    
    * rebump intel version 🤷
    
    * Update pushplaylabs-sidekick.rb

--- Casks/pushplaylabs-sidekick.rb
+++ Casks/pushplaylabs-sidekick.rb
@@ -3,11 +3,11 @@ cask "pushplaylabs-sidekick" do
   livecheck_folder = Hardware::CPU.intel? ? "mac" : "macm1"
 
   if Hardware::CPU.intel?
-    version "96.15.1.15332,51480cd"
-    sha256 "b937cc826571bde71d508864816b90a7f9d159fcbf8c8f11c60f3cf2d73ec5a4"
+    version "96.15.1.16725,adcf8c9"
+    sha256 "b73b2e3c9ea18e48809fce791155244a114692b210e097d990bd1066ce23248c"
   else
-    version "96.15.1.15334,215651a"
-    sha256 "f88073740f1077d293ca2afec37d6939cd43c924d998d40a2aae59df8de8bedc"
+    version "96.15.1.16727,60e4139"
+    sha256 "21af615ef4e52e3e07f8811d2d8d5cfbc22b0ba7e377e9782f873e7d522c283e"
   end
 
   url "https://sidekick-cdn-production.meetsidekick.com/builds/sidekick-mac-release-#{arch}-#{version.csv.first}-#{version.csv.second}-df.dmg"
