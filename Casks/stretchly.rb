2be5e903ac7a5d7d9921044c095832e0468d5661
Ben Limmer <ben@benlimmer.com>
2022-02-24 01:27:06 -0700

    Add stretchly M1 support (#119568)

--- Casks/stretchly.rb
+++ Casks/stretchly.rb
@@ -1,12 +1,23 @@
 cask "stretchly" do
-  version "1.10.0"
-  sha256 "d5bff265d0d3142af98629b648ec4d39d4058aaad4d55a8f4780f1682ce1a471"
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  url "https://github.com/hovancik/stretchly/releases/download/v#{version}/stretchly-#{version}.dmg",
+  version "1.10.0"
+
+  if Hardware::CPU.intel?
+    sha256 "d5bff265d0d3142af98629b648ec4d39d4058aaad4d55a8f4780f1682ce1a471"
+  else
+    sha256 "b27a6a4d20c3da67c7868088d7e464414eca5f7a010806c708716ec2f0e6deba"
+  end
+
+  url "https://github.com/hovancik/stretchly/releases/download/v#{version}/stretchly-#{version}#{arch}.dmg",
       verified: "github.com/hovancik/stretchly/"
   name "Stretchly"
   desc "Break time reminder app"
   homepage "https://hovancik.net/stretchly/"
 
   app "Stretchly.app"
+
+  caveats <<~EOS
+    This application is not signed. See https://github.com/hovancik/stretchly#application-signing for details.
+  EOS
 end
