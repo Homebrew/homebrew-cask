30b3dd790b21082aa104ae43adab5c11aa472cd5
Alexander Ronald Altman <alexanderaltman@me.com>
2022-02-23 18:12:41 -0800

    gcc-arm-embedded 11.2-2022.02 update
    Also contains a new gcc-aarch64-embedded cask, with the same version.

--- Casks/gcc-arm-embedded.rb
+++ Casks/gcc-arm-embedded.rb
@@ -1,20 +1,23 @@
 cask "gcc-arm-embedded" do
   # Exists as a cask because it is impractical as a formula:
   # https://github.com/Homebrew/homebrew-core/pull/45780#issuecomment-569246452
-  version "10.3-2021.10"
-  sha256 "e3888a1d0af798be7f98d67a3e6dc4fc96d92d5b57fc635e0c021a4a36087b5d"
+  version "11.2-2022.02"
+  gcc_version = "11.2.1"
+  sha256 "105ac7f9b2be62c55d6853a099a92488c16f08bb296a694ef8430e5ddf8dead8"
 
-  url "https://developer.arm.com/-/media/Files/downloads/gnu-rm/#{version}/gcc-arm-none-eabi-#{version}-mac.pkg"
+  url "https://developer.arm.com/-/media/Files/downloads/gnu/#{version}/binrel/gcc-arm-#{version}-darwin-x86_64-arm-none-eabi.pkg"
   name "GCC ARM Embedded"
   desc "Pre-built GNU bare-metal toolchain for 32-bit Arm processors"
-  homepage "https://developer.arm.com/tools-and-software/open-source-software/developer-tools/gnu-toolchain/gnu-rm"
+  homepage "https://developer.arm.com/tools-and-software/open-source-software/developer-tools/gnu-toolchain"
 
   livecheck do
-    url "https://developer.arm.com/tools-and-software/open-source-software/developer-tools/gnu-toolchain/gnu-rm/downloads"
-    regex(/href=.*?gcc-arm-none-eabi-(\d+\.\d+-\d+\.\d+)-mac.pkg/i)
+    url "https://developer.arm.com/tools-and-software/open-source-software/developer-tools/gnu-toolchain/downloads"
+    regex(/href=.*?gcc-arm-(\d+\.\d+-\d+\.\d+)-darwin-(?:\w+)-arm-none-eabi.pkg/i)
   end
 
-  pkg "gcc-arm-none-eabi-#{version}-mac.pkg"
+  depends_on arch: :x86_64
+
+  pkg "gcc-arm-#{version}-darwin-x86_64-arm-none-eabi.pkg"
   binary "#{appdir}/ARM/bin/arm-none-eabi-addr2line"
   binary "#{appdir}/ARM/bin/arm-none-eabi-ar"
   binary "#{appdir}/ARM/bin/arm-none-eabi-as"
@@ -24,7 +27,7 @@ cask "gcc-arm-embedded" do
   binary "#{appdir}/ARM/bin/arm-none-eabi-elfedit"
   binary "#{appdir}/ARM/bin/arm-none-eabi-g++"
   binary "#{appdir}/ARM/bin/arm-none-eabi-gcc"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-gcc-10.3.1"
+  binary "#{appdir}/ARM/bin/arm-none-eabi-gcc-#{gcc_version}"
   binary "#{appdir}/ARM/bin/arm-none-eabi-gcc-ar"
   binary "#{appdir}/ARM/bin/arm-none-eabi-gcc-nm"
   binary "#{appdir}/ARM/bin/arm-none-eabi-gcc-ranlib"
@@ -33,8 +36,7 @@ cask "gcc-arm-embedded" do
   binary "#{appdir}/ARM/bin/arm-none-eabi-gcov-tool"
   binary "#{appdir}/ARM/bin/arm-none-eabi-gdb"
   binary "#{appdir}/ARM/bin/arm-none-eabi-gdb-add-index"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-gdb-add-index-py"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-gdb-py"
+  binary "#{appdir}/ARM/bin/arm-none-eabi-gfortran"
   binary "#{appdir}/ARM/bin/arm-none-eabi-gprof"
   binary "#{appdir}/ARM/bin/arm-none-eabi-ld"
   binary "#{appdir}/ARM/bin/arm-none-eabi-ld.bfd"
@@ -48,6 +50,18 @@ cask "gcc-arm-embedded" do
   binary "#{appdir}/ARM/bin/arm-none-eabi-strings"
   binary "#{appdir}/ARM/bin/arm-none-eabi-strip"
 
-  uninstall pkgutil: "gcc.arm-none-eabi-#{version[/^(\d{2})/]}",
-            delete:  "/Applications/ARM"
+  uninstall pkgutil: "gcc-arm-#{version}-darwin-x86_64-arm-none-eabi",
+            delete:  [
+              "/Applications/ARM/#{version}-darwin-x86_64-arm-none-eabi-manifest",
+              "/Applications/ARM/bin/arm-none-eabi-*",
+              "/Applications/ARM/arm-none-eabi",
+              "/Applications/ARM/lib/gcc/arm-none-eabi",
+              "/Applications/ARM/libexec/gcc/arm-none-eabi",
+              "/Applications/ARM/share/doc/arm-none-eabi",
+              "/Applications/ARM/share/gcc-arm-none-eabi",
+              "/Applications/ARM/share/man/man1/arm-none-eabi-*.1",
+              "/Applications/ARM/share/man/man5/arm-none-eabi-gdbinit.5",
+            ]
+
+  zap delete: "/Application/ARM"
 end
