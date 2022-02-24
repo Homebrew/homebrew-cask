b336d185e6e5bd33b299c8861c733c37e2f326d8
neutric <5984479+neutric@users.noreply.github.com>
2022-02-24 09:30:27 +0100

    Update navicat-premium from 16.0.7 to 16.0.8 (#119602)

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -1,8 +1,8 @@
 cask "navicat-premium" do
-  version "16.0.7"
+  version "16.0.8"
   sha256 :no_check
 
-  url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_premium_en.dmg"
+  url "https://download.navicat.com/download/navicat#{version.major_minor.no_dots}_premium_en.dmg"
   name "Navicat Premium"
   desc "Database administration and development tool"
   homepage "https://www.navicat.com/products/navicat-premium"
