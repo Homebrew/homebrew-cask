8a1bd5d815f280401e52f246c023e25021c22fff
neutric <5984479+neutric@users.noreply.github.com>
2022-02-24 01:14:16 +0100

    Update natron from 2.4.2 to 2.4.3 (#119594)
    * Update natron from 2.4.2 to 2.4.3
    
    * natron: update other sha256s
    
    * Trigger CI

--- Casks/natron.rb
+++ Casks/natron.rb
@@ -1,16 +1,16 @@
 cask "natron" do
-  version "2.4.2"
+  version "2.4.3"
 
   if MacOS.version <= :mojave
-    sha256 "437979f4b94e718d7e7eb399715f851734900b7453bd3fe6a41d3a43e733a95a"
-    url "https://github.com/NatronGitHub/Natron/releases/download/vsha/Natron-#{version}-OSX109-x86_64.dmg",
+    sha256 "3439b15559ef8f2e0f52aafb2973be1584922a35ec398d20a74011ec838b9bf0"
+    url "https://github.com/NatronGitHub/Natron/releases/download/v#{version}/Natron-#{version}-OSX109-x86_64.dmg",
         verified: "github.com/NatronGitHub/Natron/"
   elsif MacOS.version <= :catalina
-    sha256 "c9cf39ced992e15c6bcbc6bb1c960e36398561c5858b216f07413ccf3335efe8"
+    sha256 "89ca3cca577a49023c03bb617be2a694e71fd4b8f1b1a2bbf83560f3f1450d7a"
     url "https://github.com/NatronGitHub/Natron/releases/download/v#{version}/Natron-#{version}-macOS1015-x86_64.dmg",
         verified: "github.com/NatronGitHub/Natron/"
   else
-    sha256 "2801d03d6829532953b78eafb42d543d6c56ba338eb53435fa20ff89ba2d7a65"
+    sha256 "7b4e6aac31e507f9be3eb38468fbcc5dd9b2bdbcb4b2de4a4fca67346da5bb20"
     url "https://github.com/NatronGitHub/Natron/releases/download/v#{version}/Natron-#{version}-macOS12-x86_64.dmg",
         verified: "github.com/NatronGitHub/Natron/"
   end
