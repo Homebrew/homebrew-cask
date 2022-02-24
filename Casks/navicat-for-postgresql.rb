7bda73496c11bcdaa50b2f5ab8e8e440d6b7fcf8
neutric <5984479+neutric@users.noreply.github.com>
2022-02-24 09:29:39 +0100

    Update navicat-for-postgresql from 16.0.7 to 16.0.8 (#119599)

--- Casks/navicat-for-postgresql.rb
+++ Casks/navicat-for-postgresql.rb
@@ -1,8 +1,8 @@
 cask "navicat-for-postgresql" do
-  version "16.0.7"
+  version "16.0.8"
   sha256 :no_check
 
-  url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_pgsql_en.dmg"
+  url "https://download.navicat.com/download/navicat#{version.major_minor.no_dots}_pgsql_en.dmg"
   name "Navicat for PostgreSQL"
   desc "Database administration and development tool for PostgreSQL"
   homepage "https://www.navicat.com/products/navicat-for-postgresql"
