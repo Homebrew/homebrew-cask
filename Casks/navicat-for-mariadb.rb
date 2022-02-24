9dc0fec54b1039785a38d09b86d29f28c49c4fb3
neutric <5984479+neutric@users.noreply.github.com>
2022-02-24 01:21:52 +0100

    Update navicat-for-mariadb from 16.0.7 to 16.0.8

--- Casks/navicat-for-mariadb.rb
+++ Casks/navicat-for-mariadb.rb
@@ -1,8 +1,8 @@
 cask "navicat-for-mariadb" do
-  version "16.0.7"
+  version "16.0.8"
   sha256 :no_check
 
-  url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_mariadb_en.dmg"
+  url "https://download.navicat.com/download/navicat#{version.major_minor.no_dots}_mariadb_en.dmg"
   name "Navicat for MariaDB"
   desc "Database management and administration tool for MariaDB"
   homepage "https://www.navicat.com/products/navicat-for-mariadb"
