1c1bc9535ba840b2733391cbd1efe38edd09f4dc
neutric <5984479+neutric@users.noreply.github.com>
2022-02-24 01:25:07 +0100

    Update navicat-for-oracle from 16.0.7 to 16.0.8

--- Casks/navicat-for-oracle.rb
+++ Casks/navicat-for-oracle.rb
@@ -1,8 +1,8 @@
 cask "navicat-for-oracle" do
-  version "16.0.7"
+  version "16.0.8"
   sha256 :no_check
 
-  url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_ora_en.dmg"
+  url "https://download.navicat.com/download/navicat#{version.major_minor.no_dots}_ora_en.dmg"
   name "Navicat for Oracle"
   desc "Database administration and development tool for Oracle"
   homepage "https://www.navicat.com/products/navicat-for-oracle"
