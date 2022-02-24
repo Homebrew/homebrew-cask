f157bc9b0bb3898beb24f434db74a4a6aca61393
Bevan Kay <email@bevankay.me>
2022-02-25 00:16:30 +1100

    Update 4k-youtube-to-mp3 from 4.4.3 to 4.4.4 (#119627)

--- Casks/4k-youtube-to-mp3.rb
+++ Casks/4k-youtube-to-mp3.rb
@@ -1,7 +1,7 @@
 cask "4k-youtube-to-mp3" do
   # NOTE: "3" is not a version number, but an intrinsic part of the product name
-  version "4.4.3"
-  sha256 "c9753763d57800a8810e1a04c1f1b639c0b742401e3e2fafb1adf4a7f89331f1"
+  version "4.4.4"
+  sha256 "55c0dd7232de6d8c67e2971cd11389159564d12bdd13544efa299c4eed9ce6da"
 
   url "https://dl.4kdownload.com/app/4kyoutubetomp3_#{version}.dmg"
   name "4K YouTube to MP3"
