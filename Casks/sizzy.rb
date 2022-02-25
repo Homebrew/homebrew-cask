a11234ed5ae175adc491e1ad4014d28e7cba81c5
Michael Deck <6399755+likeadeckofcards@users.noreply.github.com>
2022-02-25 15:03:01 -0500

    Fix syntax for automated checks

--- Casks/sizzy.rb
+++ Casks/sizzy.rb
@@ -1,14 +1,13 @@
 cask "sizzy" do
   version :latest
+  sha256 :no_check
 
   if Hardware::CPU.intel?
-    sha256 :no_check
     url "https://proxy.sizzy.co/updates/download/mac?arch=intel"
   else
-    sha256 :no_check
     url "https://proxy.sizzy.co/updates/download/mac?arch=arm64"
   end
-  
+
   name "Sizzy"
   desc "Tool to simulate responsive designs on multiple devices"
   homepage "https://sizzy.co/"
