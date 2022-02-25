b96a172e2498c66d86bfad96f81a28912bca37eb
Depal1 <47154119+Depal1@users.noreply.github.com>
2022-02-25 06:19:02 -0600

    Add Flycast.app v1.2 (#119571)
    * Add Flycast.app v1.2
    
    * Update flycast.rb
    
    * Update flycast.rb

---
+++ Casks/flycast.rb
@@ -0,0 +1,17 @@
+cask "flycast" do
+  version "1.2"
+  sha256 "144b0b603bbd7bdc707cd9839ca550d665c62a1f4d1c09181ab02fc641058f89"
+
+  url "https://github.com/flyinghead/flycast/releases/download/v#{version}/flycast-macOS-#{version}.zip"
+  name "Flycast"
+  desc "Dreamcast, Naomi and Atomiswave emulator"
+  homepage "https://github.com/flyinghead/flycast"
+
+  livecheck do
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  app "Flycast.app"
+
+  zap rmdir: "~/.flycast"
+end
