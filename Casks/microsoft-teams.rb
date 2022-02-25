fc2d6f83a639546529020e45ea9fe0410555630d
Miccal Matthews <miccal.matthews@gmail.com>
2022-02-25 11:30:21 +0800

    Update microsoft-teams from 1.5.00.2570 to 1.5.00.4683 (#119660)
    * Update microsoft-teams from 1.5.00.2570 to 1.5.00.4683
    
    * Update microsoft-teams.rb
    
    * Update microsoft-teams.rb

--- Casks/microsoft-teams.rb
+++ Casks/microsoft-teams.rb
@@ -1,9 +1,9 @@
 cask "microsoft-teams" do
-  version "1.5.00.2570"
-  sha256 "16d1e733a8f7361226a75272991cdfcc50e0387fb657cea80923bc811e8697bf"
+  version "1.5.00.4683"
+  sha256 "6c0d24f71b3a7e296361bdb53ab84caf1fb8b95ffda69d8a0850cc56a415037e"
 
   url "https://statics.teams.cdn.office.net/production-osx/#{version}/Teams_osx.pkg",
-      verified: "statics.teams.cdn.office.net"
+      verified: "statics.teams.cdn.office.net/production-osx/"
   name "Microsoft Teams"
   desc "Meet, chat, call, and collaborate in just one place"
   homepage "https://teams.microsoft.com/downloads"
@@ -34,5 +34,6 @@ cask "microsoft-teams" do
     "~/Library/Preferences/com.microsoft.teams.plist",
     "~/Library/Saved Application State/com.microsoft.teams.savedState",
     "~/Library/WebKit/com.microsoft.teams",
-  ]
+  ],
+      rmdir: "~/Library/Application Support/Microsoft"
 end
