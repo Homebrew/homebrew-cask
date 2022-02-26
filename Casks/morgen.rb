5c776684eea3ace5e2619c425f74d726bd8679f4
Richard Green <rdg@wait.fm>
2022-02-26 17:58:00 +0000

    morgen 2.4.4 (new cask)

---
+++ Casks/morgen.rb
@@ -0,0 +1,12 @@
+cask "morgen" do
+  version "2.4.4"
+  sha256 :no_check
+
+  url "https://dl.todesktop.com/210203cqcj00tw1/mac/dmg/x64",
+      verified: "https://dl.todesktop.com/210203cqcj00tw1/mac/dmg/x64"
+  name "Morgen"
+  desc "All-in-one Calendars, Tasks and Scheduler"
+  homepage "https://morgen.so"
+
+  app "Morgen.app"
+end
