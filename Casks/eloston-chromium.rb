c78eb2cb7c16cc9897940127e7ca6d35caf0c49a
Bevan Kay <email@bevankay.me>
2022-02-26 17:31:43 +1100

    bump both arm and intel

--- Casks/eloston-chromium.rb
+++ Casks/eloston-chromium.rb
@@ -5,8 +5,8 @@ cask "eloston-chromium" do
     version "98.0.4758.109-1.1,1645793736"
     sha256 "5dde5e6059d66e05914f9016a268863f0f87d3ee3c93b932ecbc81949e1a6af0"
   else
-    version "98.0.4758.102-1.1,1645022010"
-    sha256 "5a117fa7d3fd5bded48e2cba8268d7e2beb224207ab2cf78d2f902cbcaececd7"
+    version "98.0.4758.109-1.1,1645734398"
+    sha256 "ef8e963223e7467f1d7fd9c678707202219d403553ec83a9a96c2058d0166671"
   end
 
   url "https://github.com/kramred/ungoogled-chromium-macos/releases/download/#{version.csv.first}_#{arch}__#{version.csv.second}/ungoogled-chromium_#{version.csv.first}_#{arch}-macos.dmg",
