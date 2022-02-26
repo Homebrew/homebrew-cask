7dd3c81d28f16c3a4c77fe0a1d91ed96933fb973
Johannes Klein <jo@temka.de>
2022-02-26 07:37:50 +0100

    Update eloston-chromium from 98.0.4758.102-1.1 to 98.0.4758.109-1.1 (#119674)
    * Update eloston-chromium from 98.0.4758.102-1.1 to 98.0.4758.109-1.1
    
    * bump both arm and intel
    
    Co-authored-by: Johannes Klein <johannes.klein@lineupr.com>
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/eloston-chromium.rb
+++ Casks/eloston-chromium.rb
@@ -2,11 +2,11 @@ cask "eloston-chromium" do
   arch = Hardware::CPU.intel? ? "x86-64" : "arm64"
 
   if Hardware::CPU.intel?
-    version "98.0.4758.102-1.1,1644985332"
-    sha256 "d32215f5e61495eaba255d8d77bd6e4a742db4d1e3b3f5dc1bb8cbb9b9669e71"
+    version "98.0.4758.109-1.1,1645793736"
+    sha256 "5dde5e6059d66e05914f9016a268863f0f87d3ee3c93b932ecbc81949e1a6af0"
   else
-    version "98.0.4758.102-1.1,1645022010"
-    sha256 "5a117fa7d3fd5bded48e2cba8268d7e2beb224207ab2cf78d2f902cbcaececd7"
+    version "98.0.4758.109-1.1,1645734398"
+    sha256 "ef8e963223e7467f1d7fd9c678707202219d403553ec83a9a96c2058d0166671"
   end
 
   url "https://github.com/kramred/ungoogled-chromium-macos/releases/download/#{version.csv.first}_#{arch}__#{version.csv.second}/ungoogled-chromium_#{version.csv.first}_#{arch}-macos.dmg",
