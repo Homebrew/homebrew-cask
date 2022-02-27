752f50599c3ca27850da4a53f51f9f59b013ea11
neutric <5984479+neutric@users.noreply.github.com>
2022-02-27 02:56:39 +0100

    nota: sort stanzas

--- Casks/nota.rb
+++ Casks/nota.rb
@@ -3,14 +3,14 @@ cask "nota" do
 
   version "0.34.3"
 
-  url "https://github.com/notaapp/releases/releases/download/#{version}/Nota-#{version}-#{arch}.zip",
-      verified: "github.com/notaapp/releases/"
   if Hardware::CPU.intel?
     sha256 "66d044ba1c6f73438609856c1224ece470a4568569b85fc2c6b460095e39a48a"
   else
     sha256 "60e08e46c350c63c66792ea7b45e6b56c80bcdcf2b7669d08562be17108e66f0"
   end
 
+  url "https://github.com/notaapp/releases/releases/download/#{version}/Nota-#{version}-#{arch}.zip",
+      verified: "github.com/notaapp/releases/"
   name "Nota"
   desc "Markdown files editor"
   homepage "https://nota.md/"
