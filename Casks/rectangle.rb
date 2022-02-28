a32d861d3c4649901afca651d072cec77976e969
Remigiusz Żętkowski <remigiusz.zetkowski@gmail.com>
2022-03-01 00:07:19 +0100

    Update Rectangle to version 0.53 (#119794)

--- Casks/rectangle.rb
+++ Casks/rectangle.rb
@@ -1,6 +1,6 @@
 cask "rectangle" do
-  version "0.51,57"
-  sha256 "ed0c1e4634471f3c095ed1331611f8fb859f47febcdc9c2c4aba670f7db3a5bb"
+  version "0.53,59"
+  sha256 "759e614c00668cdb0ae00acccbd1dbe7484291bd97ec5722c74520c537ba4273"
 
   url "https://github.com/rxhanson/Rectangle/releases/download/v#{version.csv.first}/Rectangle#{version.csv.first}.dmg",
       verified: "github.com/rxhanson/Rectangle/"
