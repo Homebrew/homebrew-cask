4d182764f362b7b187b3ff61ffea4d2c4636050f
Miccal Matthews <miccal.matthews@gmail.com>
2022-02-28 13:03:34 +0800

    Update miniforge.rb

--- Casks/miniforge.rb
+++ Casks/miniforge.rb
@@ -3,13 +3,13 @@ cask "miniforge" do
 
   version "4.11.0-4"
 
-  url "https://github.com/conda-forge/miniforge/releases/download/#{version}/Miniforge3-#{version}-MacOSX-#{arch}.sh"
   if Hardware::CPU.intel?
     sha256 "5f0019f6f671f3a41352b53ee7001dfbc6d7806a62d3691159c60d493ed82ea2"
   else
     sha256 "7b3e3c29f8cc4a6a13e53f20e155f7bd30216e071c6028f699582b23bff06e60"
   end
 
+  url "https://github.com/conda-forge/miniforge/releases/download/#{version}/Miniforge3-#{version}-MacOSX-#{arch}.sh"
   name "miniforge"
   desc "Minimal installer for conda specific to conda-forge"
   homepage "https://github.com/conda-forge/miniforge"
