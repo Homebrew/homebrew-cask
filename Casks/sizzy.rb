6900ef0ef4d796548714e7a6cdb9d27ed842b8bb
Michael Deck <6399755+likeadeckofcards@users.noreply.github.com>
2022-02-28 18:52:43 -0500

    Update sizzy for arm support (#119679)
    * Update sizzy from 55.2.0 to 59.0.0
    
    * Updating the cask to provide Apple Silicon support
    
    Updated the url string to provide support for Apple Silicon and remove the specific version from the cask as the url does not allow the user to specify a version to download. Also, the livecheck is not necessary as the url provided will always give you the latest version.
    
    * Fix syntax for automated checks
    
    * Remove auto updates stanza
    
    * Update arch flag to interpolated variable
    
    * Remove trailing whitespace
    
    * Put livecheck and auto_updates back in
    
    * Update sizzy.rb
    
    * Fix livecheck
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/sizzy.rb
+++ Casks/sizzy.rb
@@ -1,8 +1,10 @@
 cask "sizzy" do
-  version "55.2.0"
+  arch = Hardware::CPU.intel? ? "intel" : "arm64"
+
+  version "59.0.0"
   sha256 :no_check
 
-  url "https://sizzy.co/get-app"
+  url "https://proxy.sizzy.co/updates/download/mac?arch=#{arch}"
   name "Sizzy"
   desc "Tool to simulate responsive designs on multiple devices"
   homepage "https://sizzy.co/"
@@ -10,6 +12,7 @@ cask "sizzy" do
   livecheck do
     url :url
     strategy :header_match
+    regex(/Sizzy[._-]v?(\d+(?:\.\d+)+)(?:[._-]#{arch})?\.dmg/i)
   end
 
   auto_updates true
