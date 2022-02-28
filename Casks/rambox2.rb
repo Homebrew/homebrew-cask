2d9d8b99cfa72dc2a50b7ee4c85d4675777fa0b3
Pierre-Luc Paour <github@paour.com>
2022-02-28 10:25:54 +0100

    Add Rambox version 2.0.1
    A new cask is created, since Rambox (CE) 0.x still exists, and no migration is handled to version 2.

---
+++ Casks/rambox2.rb
@@ -0,0 +1,36 @@
+cask "rambox2" do
+  version "2.0.1"
+  sha256 "cd7f3a0016fc4b46813d70dc3e2c2ebf00b59cedb858a2acea7e8abf977a80fa"
+
+  url "https://github.com/ramboxapp/download/releases/download/v#{version}/Rambox-#{version}-mac.zip",
+      verified: "github.com/ramboxapp/download/"
+  name "Rambox"
+  desc "Free and Open Source messaging and emailing app"
+  homepage "https://rambox.pro/"
+
+  app "Rambox.app"
+
+  zap trash: [
+    "~/Library/Application Support/CrashReporter/Rambox Helper_*.plist",
+    "~/Library/Application Support/CrashReporter/Rambox_*.plist",
+    "~/Library/Application Support/Rambox",
+    "~/Library/Caches/com.grupovrs.ramboxce",
+    "~/Library/Caches/com.grupovrs.ramboxce.ShipIt",
+    "~/Library/Caches/com.grupovrs.rambox",
+    "~/Library/Caches/com.grupovrs.rambox.ShipIt",
+    "~/Library/Caches/com.saenzramiro.rambox",
+    "~/Library/Logs/Rambox",
+    "~/Library/Preferences/ByHost/com.grupovrs.ramboxce.ShipIt.*.plist",
+    "~/Library/Preferences/com.grupovrs.ramboxce.helper.plist",
+    "~/Library/Preferences/com.grupovrs.ramboxce.plist",
+    "~/Library/Preferences/ByHost/com.grupovrs.rambox.ShipIt.*.plist",
+    "~/Library/Preferences/com.grupovrs.rambox.helper.plist",
+    "~/Library/Preferences/com.grupovrs.rambox.plist",
+    "~/Library/Preferences/com.rambox.helper.plist",
+    "~/Library/Preferences/com.rambox.plist",
+    "~/Library/Saved Application State/com.grupovrs.ramboxce.savedState",
+    "~/Library/Saved Application State/com.rambox.savedState",
+    "~/Library/Saved Application State/com.saenzramiro.rambox.savedState",
+    "~/Library/WebKit/com.saenzramiro.rambox",
+  ]
+end
