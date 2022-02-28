5b6b6f398d353f99ee2767b1441d7c3ab53d41cc
Bevan Kay <email@bevankay.me>
2022-03-01 10:28:12 +1100

    Update sizzy.rb

--- Casks/sizzy.rb
+++ Casks/sizzy.rb
@@ -1,7 +1,7 @@
 cask "sizzy" do
   arch = Hardware::CPU.intel? ? "intel" : "arm64"
 
-  version :latest
+  version "59.0.0"
   sha256 :no_check
 
   url "https://proxy.sizzy.co/updates/download/mac?arch=#{arch}"
@@ -12,6 +12,7 @@ cask "sizzy" do
   livecheck do
     url :url
     strategy :header_match
+    regex(/Sizzy[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}\.dmg/i)
   end
 
   auto_updates true
