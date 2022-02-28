5ee9ec28b4e23b8690c986172c17a8b086ac0de7
owine <owine@users.noreply.github.com>
2022-02-28 15:41:57 -0600

    Update microsoft-edge from 98.0.1108.56 to 98.0.1108.62

--- Casks/microsoft-edge.rb
+++ Casks/microsoft-edge.rb
@@ -2,12 +2,12 @@ cask "microsoft-edge" do
   folder = Hardware::CPU.intel? ? "C1297A47-86C4-4C1F-97FA-950631F94777" : "03adf619-38c6-4249-95ff-4a01c0ffc962"
   linkid = Hardware::CPU.intel? ? "2069148" : "2093504"
 
-  version "98.0.1108.56"
+  version "98.0.1108.62"
 
   if Hardware::CPU.intel?
-    sha256 "7ab569106dd266d852ab75a2f795d64b6722bf335bf8a58bebdf8e488d47520e"
+    sha256 "5686213b227d0af11757596c5828e720784b7b9caea6c44412c366a65ec3d006"
   else
-    sha256 "bb893b1299fe1a85e1147532196d04f2260ecc74bdf0c028558027d470d27469"
+    sha256 "4430cd7dcab4f6539413cf5ebf39cc7b84e0c7ac22fe787f4fd840fbbef9f2ad"
   end
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/#{folder}/MacAutoupdate/MicrosoftEdge-#{version}.pkg",
