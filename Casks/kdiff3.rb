6f589cbf1faac5143ec2a2b01bd51735ce323d3d
fanninpm <fanninpm@miamioh.edu>
2022-02-28 00:06:31 -0500

    Update kdiff3 from 1.9.3 to 1.9.4 (#119765)
    * Update kdiff3 from 1.9.3 to 1.9.4
    
    * Update kdiff3.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/kdiff3.rb
+++ Casks/kdiff3.rb
@@ -1,16 +1,16 @@
 cask "kdiff3" do
   # NOTE: "3" is not a version number, but an intrinsic part of the product name
-  version "1.9.3"
-  sha256 "502e7631364aa1eda7ed3caed634137e5d848255b5147f46a5c615956dfb4a79"
+  version "1.9.4"
+  sha256 "9cfb02c73cba644d9f778947a98d0e61a08db027b15149059e7df99566a19862"
 
-  url "https://download.kde.org/stable/kdiff3/kdiff3-#{version}-macos-64.dmg"
+  url "https://download.kde.org/stable/kdiff3/kdiff3-#{version}-macos-64-clang.dmg"
   name "KDiff3"
   desc "Utility for comparing and merging files and directories"
   homepage "https://invent.kde.org/sdk/kdiff3"
 
   livecheck do
     url "https://download.kde.org/stable/kdiff3/"
-    regex(/href=["']?kdiff3[._-]?v?(\d+(?:\.\d+)+)[._-]?macos[._-]?64\.dmg/i)
+    regex(/href=["']?kdiff3[._-]v?(\d+(?:\.\d+)+)[._-]macos[._-]64[._-]clang\.dmg/i)
   end
 
   app "kdiff3.app"
