fd04144813ffd45b69ed0d3e1073ccb81cc134a4
Bevan Kay <email@bevankay.me>
2022-03-01 10:38:29 +1100

    Fix livecheck

--- Casks/sizzy.rb
+++ Casks/sizzy.rb
@@ -12,7 +12,7 @@ cask "sizzy" do
   livecheck do
     url :url
     strategy :header_match
-    regex(/Sizzy[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}\.dmg/i)
+    regex(/Sizzy[._-]v?(\d+(?:\.\d+)+)(?:[._-]#{arch})?\.dmg/i)
   end
 
   auto_updates true
