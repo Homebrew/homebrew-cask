11efbe246ebc63c7a685b9cf92eec7b9d1464b40
Michael Deck <6399755+likeadeckofcards@users.noreply.github.com>
2022-02-28 08:34:20 -0500

    Update arch flag to interpolated variable

--- Casks/sizzy.rb
+++ Casks/sizzy.rb
@@ -1,13 +1,10 @@
 cask "sizzy" do
+  arch = Hardware::CPU.intel? ? "intel" : "arm64"
+  
   version :latest
   sha256 :no_check
 
-  if Hardware::CPU.intel?
-    url "https://proxy.sizzy.co/updates/download/mac?arch=intel"
-  else
-    url "https://proxy.sizzy.co/updates/download/mac?arch=arm64"
-  end
-
+  url "https://proxy.sizzy.co/updates/download/mac?arch=#{arch}"
   name "Sizzy"
   desc "Tool to simulate responsive designs on multiple devices"
   homepage "https://sizzy.co/"
