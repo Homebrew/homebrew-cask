40c9dbd837e80dfb9a0144c4094d9c09dc08205e
Miccal Matthews <miccal.matthews@gmail.com>
2022-02-28 17:20:53 +0800

    Update chromium.rb

--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -1,24 +1,14 @@
 cask "chromium" do
+  version :latest
+  sha256 :no_check
+
   arch = Hardware::CPU.intel? ? "Mac" : "Mac_Arm"
 
-  if Hardware::CPU.intel?
-    version "973267"
-    sha256 "09a229cfc3039aaf4e1b125b5b75d1cda4f57d72a1df449f2c682be9ad0173b1"
-  else
-    version "965870"
-    sha256 "0dc69da41fa9b13b57dc78fa8c28595d1973afe21011a8de7a3e30cbc839452f"
-  end
-
-  url "https://commondatastorage.googleapis.com/chromium-browser-snapshots/#{arch}/#{version}/chrome-mac.zip",
-      verified: "commondatastorage.googleapis.com/chromium-browser-snapshots/"
+  url "https://download-chromium.appspot.com/dl/#{arch}?type=snapshots",
+      verified: "download-chromium.appspot.com/dl/"
   name "Chromium"
   desc "Free and open-source web browser"
   homepage "https://www.chromium.org/Home"
-
-  livecheck do
-    url "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/#{arch}%2FLAST_CHANGE?alt=media"
-    regex(/v?(\d+(?:\.\d+)*)/i)
-  end
 
   conflicts_with cask: [
     "eloston-chromium",
