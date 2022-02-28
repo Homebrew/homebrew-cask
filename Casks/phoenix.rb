435cfa7032b9c705596c95e912edf48565ccf45d
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2022-02-28 07:29:01 +0200

    Upgrade Phoenix to 3.0.0 (#119714)
    * Upgrade Phoenix to 3.0.0
    
    * Update phoenix.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/phoenix.rb
+++ Casks/phoenix.rb
@@ -1,18 +1,18 @@
 cask "phoenix" do
-  version "2.6.8"
-  sha256 "b779791ae673d676d9084d88d2c075d6a8cdd5b11e97d6ac8287dfa6df5c7f55"
+  version "3.0.0"
+  sha256 "9fbe966e2f82f8702a431a183fb2a8bc5866233433fb34bc7570df39bf069049"
 
   url "https://github.com/kasper/phoenix/releases/download/#{version}/phoenix-#{version}.tar.gz"
   name "Phoenix"
   desc "Window and app manager scriptable with JavaScript"
   homepage "https://github.com/kasper/phoenix/"
 
-  depends_on macos: ">= :yosemite"
+  depends_on macos: ">= :el_capitan"
 
   app "Phoenix.app"
 
   zap trash: [
-    "~/.phoenix.js",
     "~/Library/Application Support/Phoenix/storage.json",
+    "~/Library/Preferences/org.khirviko.Phoenix.plist",
   ]
 end
