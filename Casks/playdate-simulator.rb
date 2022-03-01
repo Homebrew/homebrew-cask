e7770771ab5e59ce4215da7008cb17902852a270
Miccal Matthews <miccal.matthews@gmail.com>
2022-03-01 09:44:44 +0800

    Update and rename playdate-sdk.rb to playdate-simulator.rb

--- Casks/playdate-simulator.rb
+++ Casks/playdate-simulator.rb
@@ -1,18 +1,16 @@
-cask "playdate-sdk" do
+cask "playdate-simulator" do
   version "1.9.0"
   sha256 "c61e3ba45b65034f89690e5ad22b2ebd24e03df6d376b72e0fae62b003d104ff"
 
   url "https://download-keycdn.panic.com/playdate_sdk/PlaydateSDK-#{version}.pkg",
-      verified: "panic.com"
+      verified: "panic.com/playdate_sdk/"
   name "Playdate SDK"
-  desc "Playdate Lua and C APIs, docs and SSimulator for local development"
+  desc "Playdate Lua and C APIs, docs and Simulator for local development"
   homepage "https://play.date/dev/"
 
   livecheck do
     url :homepage
-    strategy :page_match do |page|
-      page.scan(%r{<span class="actionInfo">\s*Playdate SDK (\d+.\d+.\d+)\s*</span>}i)
-          .map { |match| match&.first }
+    regex(/Playdate\sSDK\s(\d+(?:\.\d+)+)/i)
     end
   end
 
