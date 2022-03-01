b76b4f24d8e1329173259dd172ea4cdfb79271c8
Reno <25192197+singularitti@users.noreply.github.com>
2022-02-28 19:23:25 -0500

    Update metaimage from 1.9.8,6091 to 1.9.8,b0f8d13a-8233-4fb9-acca-8246f4947a80 (#119758)
    * Update metaimage from 1.9.8,b0f8d13a-8233-4fb9-acca-8246f4947a80 to 1.9.8,b0f8d13a-8233-4fb9-acca-8246f4947a80
    
    * Update Casks/metaimage.rb strategy
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/metaimage.rb
+++ Casks/metaimage.rb
@@ -1,15 +1,22 @@
 cask "metaimage" do
-  version "1.9.8,6091"
+  version "1.9.8,b0f8d13a-8233-4fb9-acca-8246f4947a80"
   sha256 "12f5f575f679083706547c8adfd12c141c680a948313f3e47fd011842bf012d2"
 
-  url "https://neededapps.com/appcasts/metaimage/versions/MI-#{version.csv.first}.zip"
+  url "https://neededapps.nyc3.digitaloceanspaces.com/media/public/#{version.csv.second}.zip",
+      verified: "neededapps.nyc3.digitaloceanspaces.com/media/public/"
   name "MetaImage"
   desc "Editor to read, write and edit images metadata"
   homepage "https://neededapps.com/metaimage/"
 
   livecheck do
     url "https://neededapps.com/appcasts/metaimage/changelog.xml"
-    strategy :sparkle
+    regex(%r{/([^/]+)\.zip$}i)
+    strategy :sparkle do |item, regex|
+      filename = item.url[regex, 1]
+      next if filename.blank?
+
+      "#{item.short_version},#{filename}"
+    end
   end
 
   depends_on macos: ">= :high_sierra"
