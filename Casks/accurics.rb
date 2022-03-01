c55b805775f140a915a3737b9ab82b90e8943c54
Alexander Ronald Altman <alexanderaltman@me.com>
2022-03-01 09:03:42 -0800

    Update accurics from 1.0.32 to 1.0.34 (#119840)

--- Casks/accurics.rb
+++ Casks/accurics.rb
@@ -1,6 +1,6 @@
 cask "accurics" do
-  version "1.0.32"
-  sha256 "d338e8e5d73c2ab64a9a0743ac25c8211c5c5f852daf48e44f81d49bd5cc9747"
+  version "1.0.34"
+  sha256 "5ee7d65344778d006103aa953e33f9a72a55dec67cbea35c07391844d719b10e"
 
   url "https://downloads.accurics.com/cli/#{version}/accurics-cli.dmg",
       user_agent: :fake
