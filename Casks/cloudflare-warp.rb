ee64e39006f0a49ea89e164fc6843bbb1e66410d
Miccal Matthews <miccal.matthews@gmail.com>
2022-03-01 10:11:39 +0800

    Update cloudflare-warp.rb

--- Casks/cloudflare-warp.rb
+++ Casks/cloudflare-warp.rb
@@ -1,5 +1,5 @@
 cask "cloudflare-warp" do
-  version "2022.2.248.0"
+  version "2022.2.248.0,20220223.7"
   sha256 :no_check
 
   url "https://1111-releases.cloudflareclient.com/mac/Cloudflare_WARP.zip",
