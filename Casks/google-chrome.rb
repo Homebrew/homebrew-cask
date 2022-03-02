9765b92c3f29ab309af6ce5bc6bc3bd9d8677846
MoonFruit <dkmoonfruit@gmail.com>
2022-03-02 16:59:04 +0800

    google-chrome: use only the universal url (#119894)

--- Casks/google-chrome.rb
+++ Casks/google-chrome.rb
@@ -1,10 +1,8 @@
 cask "google-chrome" do
-  arch = Hardware::CPU.intel? ? "" : "universal/"
-
   version "99.0.4844.51"
   sha256 :no_check
 
-  url "https://dl.google.com/chrome/mac/#{arch}stable/GGRO/googlechrome.dmg"
+  url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
   name "Google Chrome"
   desc "Web browser"
   homepage "https://www.google.com/chrome/"
