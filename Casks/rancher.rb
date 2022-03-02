a59d1e961ef5affac4aa2229fdd930d477da0ac1
commitay <commitay@users.noreply.github.com>
2022-03-03 08:10:57 +1000

    Update rancher from 1.0.1 to 1.1.0

--- Casks/rancher.rb
+++ Casks/rancher.rb
@@ -1,12 +1,12 @@
 cask "rancher" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
-  version "1.0.1"
+  version "1.1.0"
 
   if Hardware::CPU.intel?
-    sha256 "01dcabe09dca068853da7a3966a6f9895fa7537eeef296f0363da64f42ad78fc"
+    sha256 "7e8bca5ba20e0e3623b977edc459dd29e95dac833ddd47fcbdefc6e0585f8d13"
   else
-    sha256 "b5a3af6c95355efe05dcee8afbf57d8af170e1356d502b1bb9f2f3f58a2bd147"
+    sha256 "e8cf434e5a25249a1e735c61c37cd2bfda678c3711299797d8ccd235e6857b52"
   end
 
   url "https://github.com/rancher-sandbox/rancher-desktop/releases/download/v#{version}/Rancher.Desktop-#{version}.#{arch}.dmg",
