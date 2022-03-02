622aff18d86f6bdc821e9864c03804dce9ba2fa8
neutric <5984479+neutric@users.noreply.github.com>
2022-03-02 10:10:49 +0100

    gcc-*-embedded: fix zap (#119875)
    * gcc-aarch64-embedded: fix zap
    
    * gcc-arm-embedded: fix zap
    
    * gcc-arm-embedded: switch to zap trash
    
    * gcc-aarch64-embedded: switch to zap trash

--- Casks/gcc-aarch64-embedded.rb
+++ Casks/gcc-aarch64-embedded.rb
@@ -60,5 +60,5 @@ cask "gcc-aarch64-embedded" do
               "/Applications/ARM/share/man/man5/aarch64-none-elf-gdbinit.5",
             ]
 
-  zap delete: "/Application/ARM"
+  zap trash: "/Applications/ARM"
 end
