5b518df39546a5ca61d21c79c544b4c28a5aefd7
Miccal Matthews <miccal.matthews@gmail.com>
2022-03-03 05:02:24 +0800

    Update rambox.rb (#119915)

--- Casks/rambox.rb
+++ Casks/rambox.rb
@@ -1,12 +1,14 @@
 cask "rambox" do
-  version "0.7.9"
-  sha256 "237315fd3d5790e84b1d531024d44fd8fb29066123b02f2580840d069edf1dc2"
+  version "2.0.1"
+  sha256 "cd7f3a0016fc4b46813d70dc3e2c2ebf00b59cedb858a2acea7e8abf977a80fa"
 
-  url "https://github.com/ramboxapp/community-edition/releases/download/#{version}/Rambox-#{version}-mac.zip",
-      verified: "github.com/ramboxapp/community-edition/"
+  url "https://github.com/ramboxapp/download/releases/download/v#{version}/Rambox-#{version}-mac.zip",
+      verified: "github.com/ramboxapp/download/"
   name "Rambox"
   desc "Free and Open Source messaging and emailing app"
   homepage "https://rambox.pro/"
+
+  conflicts_with cask: "homebrew/cask-versions/rambox-ce"
 
   app "Rambox.app"
 
@@ -14,8 +16,8 @@ cask "rambox" do
     "~/Library/Application Support/CrashReporter/Rambox Helper_*.plist",
     "~/Library/Application Support/CrashReporter/Rambox_*.plist",
     "~/Library/Application Support/Rambox",
-    "~/Library/Caches/com.grupovrs.ramboxce",
     "~/Library/Caches/com.grupovrs.ramboxce.ShipIt",
+    "~/Library/Caches/com.grupovrs.ramboxce",
     "~/Library/Caches/com.saenzramiro.rambox",
     "~/Library/Logs/Rambox",
     "~/Library/Preferences/ByHost/com.grupovrs.ramboxce.ShipIt.*.plist",
