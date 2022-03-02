c28044970f92e12be8d33cd7a554fe011ef6f2a9
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2022-03-02 17:33:02 +0800

    Update dropbox-capture from 69.0.0 to 71.0.0 (#119892)

--- Casks/dropbox-capture.rb
+++ Casks/dropbox-capture.rb
@@ -1,5 +1,5 @@
 cask "dropbox-capture" do
-  version "69.0.0"
+  version "71.0.0"
   sha256 :no_check
 
   url "https://clientupdates.dropboxstatic.com/dbx-releng/dropbox_capture/mac/Dropbox_Capture.dmg",
