e6cb51a9c290c7392eff8b3b852d8ebdb2719519
insomnia-infra <88453862+insomnia-infra@users.noreply.github.com>
2022-03-02 10:13:13 +0100

    Update inso from 2.4.1 to 2.5.0 (#119854)
    Co-authored-by: runner <runner@Mac-1646160055825.local>

--- Casks/inso.rb
+++ Casks/inso.rb
@@ -1,6 +1,6 @@
 cask "inso" do
-  version "2.4.1"
-  sha256 "ef9510f32d5d5093f6589fe22d6629c2aa780315966381fc94f83519f2553c2d"
+  version "2.5.0"
+  sha256 "d4b7768be9f612255f6f1b5619dcb5c67ab2e800e43ff4308354cdd2c084644e"
 
   url "https://github.com/Kong/insomnia/releases/download/lib%40#{version}/inso-macos-#{version}.zip",
       verified: "github.com/Kong/insomnia/"
