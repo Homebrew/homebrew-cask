67e6c8db823c06eb359e570489f08554fb8d5f1e
Bevan Kay <email@bevankay.me>
2022-03-04 10:10:19 +1100

    Update reaper from 6.49 to 6.50

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,11 +1,11 @@
 cask "reaper" do
-  version "6.49"
+  version "6.50"
 
   if MacOS.version <= :mojave
     sha256 "7eb0fb7df77e8ab2e2b14b49d45b08f9c1973421fc5a8f5672c00778feb122bd"
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64.dmg"
   else
-    sha256 "92138faf09e32c1b265246d9742587693802a1e483bd7acb4b80ab3ad995eb89"
+    sha256 "1fb3eb979b1a6c1f6ed5c11fb7c63952d69e2cd75d53c15b6b18f47be52e3e3d"
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}_universal.dmg"
   end
 
