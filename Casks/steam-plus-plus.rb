2fb8a1d26e16ff8b579f5d87bea5283fcb1e5743
Zhenfu Shi <szf1234@me.com>
2022-03-03 16:11:00 -0500

    Update steam-plus-plus from 2.6.5 to 2.6.9 (#119974)

--- Casks/steam-plus-plus.rb
+++ Casks/steam-plus-plus.rb
@@ -1,6 +1,6 @@
 cask "steam-plus-plus" do
-  version "2.6.5"
-  sha256 "b9e23a09c1ddfe254fda43dba16db387514834e67509569f1631e3a325e4ec90"
+  version "2.6.9"
+  sha256 "c03e9eb6f0bc25d7ad20ee1a28f88477b2f5c19866be2db93673b6c31a348114"
 
   url "https://github.com/BeyondDimension/SteamTools/releases/download/#{version}/Steam++_macos_x64_v#{version}.dmg",
       verified: "github.com/BeyondDimension/SteamTools/"
