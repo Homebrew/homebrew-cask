47c4549657f921cfce73a4b9cc5324211dfeda6c
Brad Murray <brad@bradmurray.com>
2022-03-03 16:09:56 -0500

    Update iina-plus to 0.6.2 (#119971)

--- Casks/iina-plus.rb
+++ Casks/iina-plus.rb
@@ -1,6 +1,6 @@
 cask "iina-plus" do
-  version "0.6.0,21122622"
-  sha256 "0df90c4079c20c6573d52e505f4317fdd4a2c6910315cdeaa13b5598c1c0d71c"
+  version "0.6.2,22022810"
+  sha256 "861a92665a748bf3e7ffe9712b7943059ad1e16d52df842a3cad037d9790b696"
 
   url "https://github.com/xjbeta/iina-plus/releases/download/#{version.csv.first}(#{version.csv.second})/iina+.#{version.csv.first}.dmg"
   name "IINA+"
