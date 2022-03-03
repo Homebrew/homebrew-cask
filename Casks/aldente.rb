f6646338ff615891e0139e895b4dcf406d107913
Leopoldo Pla <leopoldoplasempere@gmail.com>
2022-03-03 22:10:39 +0100

    Update aldente from 1.14 to 1.15 (#119973)

--- Casks/aldente.rb
+++ Casks/aldente.rb
@@ -1,6 +1,6 @@
 cask "aldente" do
-  version "1.14"
-  sha256 "e7e78183aa1a4331592d43059114ff2a027959b5846a5148cd3c1bbe3b33ebfe"
+  version "1.15"
+  sha256 "37d09129482250f2c840e0e0990648cbba329b4676a05d6e12d64713dd96c26a"
 
   url "https://github.com/davidwernhart/AlDente/releases/download/#{version}/AlDente.dmg"
   name "AlDente"
