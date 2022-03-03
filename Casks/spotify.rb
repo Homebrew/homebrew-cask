9c5f8a5394555891b2c10c24da22f414a23df238
Johannes Klein <jo@temka.de>
2022-03-03 16:56:19 +0100

    Update spotify from 1.1.79.763 to 1.1.80.699 (#119960)
    * Update spotify from 1.1.79.763 to 1.1.80.699
    
    * Update spotify.rb
    
    Co-authored-by: Johannes Klein <johannes.klein@lineupr.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/spotify.rb
+++ Casks/spotify.rb
@@ -2,9 +2,9 @@ cask "spotify" do
   arch = Hardware::CPU.intel? ? "" : "ARM64"
 
   if Hardware::CPU.intel?
-    version "1.1.79.763,c2965cdf,21"
+    version "1.1.80.699,c3dac750,9"
   else
-    version "1.1.79.763,c2965cdf,18"
+    version "1.1.80.699,gc3dac750,7"
   end
 
   sha256 :no_check
