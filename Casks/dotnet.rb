0fcfc21c787778946b8cbfda3f2a972e06cc9a38
Bevan Kay <email@bevankay.me>
2022-03-03 16:41:12 +1100

    Update dotnet from 6.0.1 to 6.0.2

--- Casks/dotnet.rb
+++ Casks/dotnet.rb
@@ -5,8 +5,8 @@ cask "dotnet" do
     version "6.0.1,6824b342-4659-40a1-ab73-25dd43e5e225,e57b1bfa437cf152f2d7064246bfb653"
     sha256 "0be7228ce53c4766d19b48afad0769d1b420c5c4f38a81e5cae37aa3ed72cdbc"
   else
-    version "6.0.1,51646583-741c-481e-b598-f13dc719cdf4,3adaa0faa24326fd7cc2265e957339bf"
-    sha256 "5c8ac515cf93cd8ac11f4c2baff2c23a1c10fed359e4979db60233f6a0c79133"
+    version "6.0.2,0d80c9b0-764f-4a13-8858-bc51e7b54b8f,7980e983431cfbbc0b28fb85f7770034"
+    sha256 "66fe2879d14d796c43a99d7bea7a520f42c8b52bf7f9a125d6c79f7285174464"
   end
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv.second}/#{version.csv.third}/dotnet-runtime-#{version.csv.first}-osx-#{arch}.pkg"
