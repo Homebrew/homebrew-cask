7f29690b73f6102e5bd25d4602905941bd3f2681
Clément L <porkepix@gmail.com>
2022-03-03 12:34:55 +0100

    Update tabby from 1.0.172 to 1.0.173 (#119955)

--- Casks/tabby.rb
+++ Casks/tabby.rb
@@ -1,12 +1,12 @@
 cask "tabby" do
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 
-  version "1.0.172"
+  version "1.0.173"
 
   if Hardware::CPU.intel?
-    sha256 "820561f72611dc1ee6486b7ab99db37ee603329bc2c95c9b8ec1d24e15e15714"
+    sha256 "6c3be59ce05e49de03ebe298e2978e20ddfe2b9d99912a5320067c6fcce89ffa"
   else
-    sha256 "0f131d7d664c6594cc29b7faee4045e1ca39491d142857e157d47988f4c21d96"
+    sha256 "817ef7bc292d2adb61d942bb5e0c22f1b21952864d5219ea352434a946e90824"
   end
 
   url "https://github.com/Eugeny/tabby/releases/download/v#{version}/tabby-#{version}-macos-#{arch}.zip",
