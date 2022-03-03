8fb4d25bf00b436716e0a7f54cefaee0e2d98c08
neutric <5984479+neutric@users.noreply.github.com>
2022-03-03 16:41:17 +0100

    Update opensesame from 3.3.10 to 3.3.11 (#119965)

--- Casks/opensesame.rb
+++ Casks/opensesame.rb
@@ -1,16 +1,9 @@
 cask "opensesame" do
-  arch = Hardware::CPU.intel? ? "x64" : "arm"
+  version "3.3.11"
+  sha256 "b33134a0fe03a15ac2de0cce9dee495fec96ac9da50352af0b906dac54cb9fc0"
 
-  version "3.3.10"
-
-  url "https://github.com/open-cogsci/OpenSesame/releases/download/release%2F#{version}/opensesame_#{version}-py37-macos-#{arch}-1.dmg",
+  url "https://github.com/open-cogsci/OpenSesame/releases/download/release%2F#{version}/opensesame_#{version}-py37-macos-x64-1.dmg",
       verified: "github.com/open-cogsci/OpenSesame/"
-  if Hardware::CPU.intel?
-    sha256 "ec1b624224608cc32ae796c68fa596ad6e69bd1b921ccc2b8c9b0da962212bdf"
-  else
-    sha256 "562476872c9dc8276b8637cc7fc8f8c21edbccec71cbf54a437eb8aa34cdf2ea"
-  end
-
   name "OpenSesame"
   desc "Graphical experiment builder for the social sciences"
   homepage "https://osdoc.cogsci.nl/"
