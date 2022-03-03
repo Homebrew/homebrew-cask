f4c6c72d2b2c84aa9b0c02f482ed03e886d6b275
Johannes Klein <jo@temka.de>
2022-03-03 10:00:43 +0100

    Update dropbox from 142.4.4197 to 143.4.4161 (#119948)
    Co-authored-by: Johannes Klein <johannes.klein@lineupr.com>

--- Casks/dropbox.rb
+++ Casks/dropbox.rb
@@ -1,7 +1,7 @@
 cask "dropbox" do
   arch = Hardware::CPU.intel? ? "" : "&arch=arm64"
 
-  version "142.4.4197"
+  version "143.4.4161"
   sha256 :no_check
 
   url "https://www.dropbox.com/download?plat=mac&full=1#{arch}"
