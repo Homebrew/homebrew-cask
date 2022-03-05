820ebbaa02a66c0691ecc2dcc8271cff2b0c8ca4
Hannes Braun <hannesbraun@mail.de>
2022-03-05 12:24:11 +0100

    reaper: fix sha256 for old macOS versions (#120012)

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -2,7 +2,7 @@ cask "reaper" do
   version "6.50"
 
   if MacOS.version <= :mojave
-    sha256 "7eb0fb7df77e8ab2e2b14b49d45b08f9c1973421fc5a8f5672c00778feb122bd"
+    sha256 "fcb2d3a6a7ab455426cf13c8c6f49aa97fe986556464a0dbd34766c25f40b971"
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64.dmg"
   else
     sha256 "1fb3eb979b1a6c1f6ed5c11fb7c63952d69e2cd75d53c15b6b18f47be52e3e3d"
