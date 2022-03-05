faa95adc289cb7cb47bf3f81ec584c81af9bcb9f
neutric <5984479+neutric@users.noreply.github.com>
2022-03-05 22:56:13 +0100

    pdf-converter-master: update sha256

--- Casks/pdf-converter-master.rb
+++ Casks/pdf-converter-master.rb
@@ -1,6 +1,6 @@
 cask "pdf-converter-master" do
   version "6.2.0"
-  sha256 "3134b19c8f234b1d0e948fec3115008ada48973f3a8c48ccdd8fd4e8bad7b923"
+  sha256 "627013bbffbb7beafc864bff64e961f776879b7b5dcc8faf1934d6dd4337f8dd"
 
   url "https://www.lightenpdf.com/upload/download/pdf-converter-master-#{version.no_dots}.dmg"
   name "PDF Converter Master"
