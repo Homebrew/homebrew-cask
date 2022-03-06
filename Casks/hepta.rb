ba38394cb7ee2238e19e8a30be089ea8282c912e
Bevan Kay <email@bevankay.me>
2022-03-06 22:52:38 +1100

    Update hepta from 0.97.0 to 0.98.0

--- Casks/hepta.rb
+++ Casks/hepta.rb
@@ -1,12 +1,12 @@
 cask "hepta" do
   arch = Hardware::CPU.intel? ? "mac" : "arm64-mac"
 
-  version "0.97.0"
+  version "0.98.0"
 
   if Hardware::CPU.intel?
-    sha256 "25c9b8d0f71fa95bb311e14b84b791dae057c44bc448e64b4382331fbaa5b844"
+    sha256 "d046008612504e727176cf95267702945e290786b0acb1de4660af9848584417"
   else
-    sha256 "d416e9bc14227f4fe841baf8b7ee204dce4bdb910bd9914b7b8c751a4a5908a3"
+    sha256 "10ef70c8dd84254b7b07b2b3200ac38ff22644a82f311bd1bb6d5ac3456c0a23"
   end
 
   url "https://github.com/heptameta/project-meta/releases/download/v#{version}/Hepta-#{version}-#{arch}.zip",
