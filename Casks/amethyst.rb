a9ac7a24a1b0fe8ec307f5a89bf9c16231b9a22a
Clément L <porkepix@gmail.com>
2022-03-06 12:46:07 +0100

    Update amethyst from 0.15.5 to 0.15.6 (#120059)

--- Casks/amethyst.rb
+++ Casks/amethyst.rb
@@ -4,8 +4,8 @@ cask "amethyst" do
     sha256 "9fd1ac2cfb8159b2945a4482046ee6d365353df617f4edbabc4e8cadc448c1e7"
     url "https://ianyh.com/amethyst/versions/Amethyst-#{version}.zip"
   else
-    version "0.15.5"
-    sha256 "a3d39e9c36ff13ac6f7e0c656c741acd285124ef53a03264fe03efc5906ce683"
+    version "0.15.6"
+    sha256 "e459b9ce81078b8251549168c0d8473c069a14e817f3a8f386ec84b0f220955a"
     url "https://github.com/ianyh/Amethyst/releases/download/v#{version}/Amethyst.zip",
         verified: "github.com/ianyh/Amethyst/"
   end
