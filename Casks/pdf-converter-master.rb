f7938b7c483fe77dac708123a41fc6de8f02823e
neutric <5984479+neutric@users.noreply.github.com>
2022-03-06 12:54:01 +0100

    pdf-converter-master: update sha256 (#120079)
    * pdf-converter-master: update sha256
    
    * pdf-converter-master: tweak livecheck
    
    * pdf-converter-master: add desc

--- Casks/pdf-converter-master.rb
+++ Casks/pdf-converter-master.rb
@@ -1,15 +1,15 @@
 cask "pdf-converter-master" do
   version "6.2.0"
-  sha256 "3134b19c8f234b1d0e948fec3115008ada48973f3a8c48ccdd8fd4e8bad7b923"
+  sha256 "627013bbffbb7beafc864bff64e961f776879b7b5dcc8faf1934d6dd4337f8dd"
 
   url "https://www.lightenpdf.com/upload/download/pdf-converter-master-#{version.no_dots}.dmg"
   name "PDF Converter Master"
+  desc "Document converter"
   homepage "https://www.lightenpdf.com/pdf-converter-mac.html"
 
   livecheck do
     url :homepage
-    strategy :page_match
-    regex(/Version\s*(\d+(?:\.\d+)*)/i)
+    regex(/Version\s*v?(\d+(?:\.\d+)+)/i)
   end
 
   app "PDF Converter Master.app"
