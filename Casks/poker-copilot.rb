500c741ee63ab3050abfc9ce3ea72e80f9920122
Bevan Kay <email@bevankay.me>
2022-03-07 10:09:50 +1100

    Update poker-copilot.rb

--- Casks/poker-copilot.rb
+++ Casks/poker-copilot.rb
@@ -2,7 +2,7 @@ cask "poker-copilot" do
   version "7.26,8855"
   sha256 "32fedd6ac7d39a03c58a376f2247b82cc8b7473d7ae38dc91c674c14f2f51ad4"
 
-  url "https://static.pokercopilot.com/pokercopilot_macos_#{version.dots_to_underscores}.dmg"
+  url "https://static.pokercopilot.com/pokercopilot_macos_#{version.csv.first.dots_to_underscores}_build_#{version.csv.second}.dmg"
   name "Poker Copilot"
   desc "Online poker HUD and tracking software"
   homepage "https://pokercopilot.com/"
