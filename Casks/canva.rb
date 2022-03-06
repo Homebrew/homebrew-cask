959f9d4c0916bfd99b41aaa6a74058857a7f1e62
Bevan Kay <email@bevankay.me>
2022-03-06 23:01:21 +1100

    add arch conditional formatting

--- Casks/canva.rb
+++ Casks/canva.rb
@@ -1,17 +1,16 @@
 cask "canva" do
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
+
   version "1.40.0"
 
   if Hardware::CPU.intel?
     sha256 "7417c7be06c9ddab613d80397344c3974e129309712068dbb017d83fd8478309"
-
-    url "https://desktop-release.canva-deploy.com/Canva-#{version}.dmg",
-        verified: "desktop-release.canva-deploy.com/"
   else
     sha256 "2960f5b59eacab720afeb84074cef9ab4b883c86dded07fce8a793634a6abf49"
-
-    url "https://desktop-release.canva-deploy.com/Canva-#{version}-arm64.dmg",
-        verified: "desktop-release.canva-deploy.com/"
   end
+
+  url "https://desktop-release.canva-deploy.com/Canva-#{version}#{arch}.dmg",
+      verified: "desktop-release.canva-deploy.com/"
   name "Canva"
   desc "Design tool"
   homepage "https://www.canva.com/"
