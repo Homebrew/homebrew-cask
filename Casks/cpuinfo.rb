68040a5aa26de3016e39abc918ab45b99dcafd50
Porkepix <porkepix@gmail.com>
2022-03-06 14:01:32 +0100

    Update cpuinfo from 1.4.7 to 1.5.1

--- Casks/cpuinfo.rb
+++ Casks/cpuinfo.rb
@@ -1,5 +1,5 @@
 cask "cpuinfo" do
-  version "1.4.7"
+  version "1.5.1"
   sha256 "e6037c41db55d6032bfb0c5f6ed6b62d8303a58b1a13a972522253aeebef427a"
 
   url "https://github.com/yusukeshibata/cpuinfo/raw/#{version}/dist/cpuinfo.zip"
