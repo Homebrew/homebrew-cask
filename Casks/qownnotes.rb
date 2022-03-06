3696548e657b9744c0f7604376322ca754cf9c4c
Clément L <porkepix@gmail.com>
2022-03-06 12:49:30 +0100

    Update qownnotes from 22.3.0 to 22.3.1 (#120066)

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,6 +1,6 @@
 cask "qownnotes" do
-  version "22.3.0"
-  sha256 "30eb10f0ecb5567dce3c38e33fdd9ed1bef20b729f43d441030a52d4c74e8fa0"
+  version "22.3.1"
+  sha256 "8f97aa3ccbe74731bcc23fb74d8a8651898b4e2d8b6bdb15aabfd721f71a1521"
 
   url "https://github.com/pbek/QOwnNotes/releases/download/v#{version}/QOwnNotes.dmg",
       verified: "github.com/pbek/QOwnNotes/"
