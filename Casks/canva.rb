a2ce94ea6e8490fbacec8b75269da43fe277ee5a
Richard Lee <14349+dlackty@users.noreply.github.com>
2022-03-06 17:09:33 +0800

    canva: add arm version

--- Casks/canva.rb
+++ Casks/canva.rb
@@ -1,9 +1,17 @@
 cask "canva" do
   version "1.40.0"
-  sha256 "7417c7be06c9ddab613d80397344c3974e129309712068dbb017d83fd8478309"
 
-  url "https://desktop-release.canva-deploy.com/Canva-#{version}.dmg",
-      verified: "desktop-release.canva-deploy.com/"
+  if Hardware::CPU.intel?
+    sha256 "7417c7be06c9ddab613d80397344c3974e129309712068dbb017d83fd8478309"
+
+    url "https://desktop-release.canva-deploy.com/Canva-#{version}.dmg",
+        verified: "desktop-release.canva-deploy.com/"
+  else
+    sha256 "2960f5b59eacab720afeb84074cef9ab4b883c86dded07fce8a793634a6abf49"
+
+    url "https://desktop-release.canva-deploy.com/Canva-#{version}-arm64.dmg",
+        verified: "desktop-release.canva-deploy.com/"
+  end
   name "Canva"
   desc "Design tool"
   homepage "https://www.canva.com/"
