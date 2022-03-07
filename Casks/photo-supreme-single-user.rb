6252298799a95ba31783c7a2b6771895d35a1685
neutric <5984479+neutric@users.noreply.github.com>
2022-03-07 02:10:12 +0100

    Update photo-supreme-single-user from 4 to 6

--- Casks/photo-supreme-single-user.rb
+++ Casks/photo-supreme-single-user.rb
@@ -1,5 +1,5 @@
 cask "photo-supreme-single-user" do
-  version "4"
+  version "6"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://trial.idimager.com/PhotoSupreme_V#{version}.pkg"
