7e041d21889e30bbd3d54f2711d6c827274b65a0
Thomas Montague <montague.thomas@gmail.com>
2022-03-07 17:12:03 -0500

    Update openshift-client to 4.10.2 and add support for arm64.

--- Casks/openshift-client.rb
+++ Casks/openshift-client.rb
@@ -1,8 +1,9 @@
 cask "openshift-client" do
-  version "4.9.23"
-  sha256 "cf82ac4052746b457d0185acc09ecd544628cf242b6263c562533d248217fa1e"
+  version "4.10.2"
+  sha256 Hardware::CPU.intel? ? "0e1a7d132b83d3e7c54c58192f6ee87229ad0581f468ebcd0b21844611bc1322" : "634448755b9f22664d5dcb1a42ef9f004121ade3a537c047e4db57f96f0618d6"
 
-  url "https://mirror.openshift.com/pub/openshift-v#{version.major}/clients/ocp/#{version}/openshift-client-mac.tar.gz"
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
+  url "https://mirror.openshift.com/pub/openshift-v#{version.major}/clients/ocp/#{version}/openshift-client-mac#{arch}.tar.gz"
   name "Openshift Client"
   desc "Red Hat OpenShift Container Platform command-line client"
   homepage "https://www.openshift.com/"
