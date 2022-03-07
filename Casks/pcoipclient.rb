eab1e50adbf1f783c23583f6517dddeebb8e258f
neutric <5984479+neutric@users.noreply.github.com>
2022-03-07 02:12:16 +0100

    Update pcoipclient from 21.03.0 to 22.01.2 (#120125)
    * Update pcoipclient from 21.03.0 to 22.01.2
    
    * pcoipclient: update livecheck
    
    * pcoipclient: add license

--- Casks/pcoipclient.rb
+++ Casks/pcoipclient.rb
@@ -1,16 +1,15 @@
 cask "pcoipclient" do
-  version "21.03.0"
-  sha256 "4dd5b21afccf49778b551c6b6cbf155d92b9905a0b951d86c7b4205c89cdb182"
+  version "22.01.2"
+  sha256 "e49852eb5f2f26a241c0438fd8971a6edcf314662c942e6f956aaee45653788e"
 
-  url "https://downloads.teradici.com/mac/stable/pcoip-client_#{version}.dmg"
+  url "https://dl.teradici.com/DeAdBCiUYInHcSTy/pcoip-client/raw/names/pcoip-client-dmg/versions/#{version}/pcoip-client_#{version}.dmg"
   name "Teradici PCoIP Software Client for macOS"
   desc "Client for VM agents and remote workstation cards"
   homepage "https://docs.teradici.com/find/product/software-and-mobile-clients/"
 
   livecheck do
-    url "https://downloads.teradici.com/mac/stable/"
-    strategy :page_match
-    regex(/href=.*?pcoip-client_(\d+(?:\.\d+)*)\.dmg/i)
+    url "https://dl.teradici.com/DeAdBCiUYInHcSTy/pcoip-client/raw/names/pcoip-client-dmg/versions/latest/pcoip-client_latest.dmg"
+    strategy :header_match
   end
 
   app "PCoIPClient.app"
@@ -25,4 +24,8 @@ cask "pcoipclient" do
     "~/Library/Preferences/com.teradici.Teradici PCoIP Client.plist",
     "~/Library/Preferences/com.teradici.PCoIP Client Connection Info.plist",
   ]
+
+  caveats do
+    license "https://docs.teradici.com/reference/eula/teradici-end-user-license-agreement"
+  end
 end
