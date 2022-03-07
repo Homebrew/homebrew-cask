f35e7c98827d2eaf886cdc48676e5b173f80549d
neutric <5984479+neutric@users.noreply.github.com>
2022-03-07 09:29:22 +0100

    Update universal-media-server from 10.17.0 to 10.17.1 (#120135)

--- Casks/universal-media-server.rb
+++ Casks/universal-media-server.rb
@@ -1,6 +1,6 @@
 cask "universal-media-server" do
-  version "10.17.0"
-  sha256 "4c9e2f786cf298a4c971e588dd7c8235b70e5414cb1fbd7931dddac72b2c8e0c"
+  version "10.17.1"
+  sha256 "ba53389875787be9ba78c7c69cb9d47c99887d142d520e2c89caa54748c8ad27"
 
   url "https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/#{version}/UMS-#{version}.dmg",
       verified: "github.com/UniversalMediaServer/UniversalMediaServer/"
