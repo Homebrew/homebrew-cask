ef183662f671805caa3e4532a075c21a1a5bd278
Thomas Montague <montague.thomas@gmail.com>
2022-03-07 17:23:49 -0500

    Fix style.

--- Casks/openshift-client.rb
+++ Casks/openshift-client.rb
@@ -1,6 +1,11 @@
 cask "openshift-client" do
   version "4.10.2"
-  sha256 Hardware::CPU.intel? ? "0e1a7d132b83d3e7c54c58192f6ee87229ad0581f468ebcd0b21844611bc1322" : "634448755b9f22664d5dcb1a42ef9f004121ade3a537c047e4db57f96f0618d6"
+
+  if Hardware::CPU.intel?
+    sha256 "0e1a7d132b83d3e7c54c58192f6ee87229ad0581f468ebcd0b21844611bc1322"
+  else
+    sha256 "634448755b9f22664d5dcb1a42ef9f004121ade3a537c047e4db57f96f0618d6"
+  end
 
   arch = Hardware::CPU.intel? ? "" : "-arm64"
   url "https://mirror.openshift.com/pub/openshift-v#{version.major}/clients/ocp/#{version}/openshift-client-mac#{arch}.tar.gz"
