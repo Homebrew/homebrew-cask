5066ff69232e1f75f918871c9b3ebf93ee7bed1f
Hannes Braun <hannesbraun@mail.de>
2022-03-08 17:43:25 +0100

    Update bisq from 1.8.2 to 1.8.4

--- Casks/bisq.rb
+++ Casks/bisq.rb
@@ -1,6 +1,6 @@
 cask "bisq" do
-  version "1.8.2"
-  sha256 "d3526d754be0c99166cbd2c237642718e4c9d81e6eb7b540987156588b074822"
+  version "1.8.4"
+  sha256 "70e0e5a843092b5b2d0679a4179b3a6da4adea5ab3bb8d3f594cc8c395dffb36"
 
   url "https://github.com/bisq-network/bisq/releases/download/v#{version}/Bisq-#{version}.dmg",
       verified: "github.com/bisq-network/bisq/"
