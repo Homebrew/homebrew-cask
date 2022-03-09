bd034d93a22cc5e09369130734b414248be431b3
Porkepix <porkepix@gmail.com>
2022-03-09 10:46:29 +0100

    Update arctype from 0.9.49 to 0.9.53

--- Casks/arctype.rb
+++ Casks/arctype.rb
@@ -1,6 +1,6 @@
 cask "arctype" do
-  version "0.9.49"
-  sha256 "e024eedd954d7f0dfc60330b9186a0e50bcfb3ef93f58a02d112d9c696c336b1"
+  version "0.9.53"
+  sha256 "bbcf92950728cb6b3a867e06dbfca22efbe7868b482031aae6445bcc59316405"
 
   url "https://arctype-downloads.s3-us-west-2.amazonaws.com/updates/Arctype-#{version}.dmg",
       verified: "arctype-downloads.s3-us-west-2.amazonaws.com/updates/"
