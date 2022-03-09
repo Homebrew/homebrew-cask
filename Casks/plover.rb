7d7059b47bd9ae6462425e2483f7470dca13e085
neutric <5984479+neutric@users.noreply.github.com>
2022-03-09 10:59:38 +0100

    Update plover from 3.1.1 to 4.0.0.dev10 (#120183)
    * Update plover from 3.1.1 to 4.0.0.dev10
    
    * plover: fix livecheck
    
    * plover: add caveats

--- Casks/plover.rb
+++ Casks/plover.rb
@@ -1,8 +1,8 @@
 cask "plover" do
-  version "3.1.1"
-  sha256 "c45c1c6cf797de62923d20e6e46aec0d946027b6b3cca4df2fc2db4e3183516b"
+  version "4.0.0.dev10"
+  sha256 "a19c59ec874569e6f5a765d15627bcf439664148859360a805305d985baaf193"
 
-  url "https://github.com/openstenoproject/plover/releases/download/v#{version}/plover-#{version}-py2-mac.dmg",
+  url "https://github.com/openstenoproject/plover/releases/download/v#{version}/plover-#{version}-macosx_10_13_x86_64.dmg",
       verified: "github.com/openstenoproject/plover/"
   name "Plover"
   desc "Stenotype engine"
@@ -11,9 +11,15 @@ cask "plover" do
   livecheck do
     url :url
     strategy :github_latest
+    regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+[\w.]+)["' >]}i)
   end
 
   app "Plover.app"
 
   zap trash: "~/Library/Application Support/plover/"
+
+  caveats <<~EOS
+    Version 4 is a major change and the configuration file it creates is not compatible with
+    Plover 3 or earlier. Please backup your plover.cfg.
+  EOS
 end
