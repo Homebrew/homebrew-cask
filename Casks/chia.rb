63a8711a30b9c45c4fb80695131bab5d94312d5d
Tobias "Knight" S <tobias@tobiassachs.de>
2022-03-09 23:59:16 +0100

    Update chia from 1.2.11 to 1.3.0 (#120261)

--- Casks/chia.rb
+++ Casks/chia.rb
@@ -1,6 +1,6 @@
 cask "chia" do
-  version "1.2.11"
-  sha256 "f9f58e3d7d420408bc66b06b40e3289185a96707fa52d75631cd1e7a2b9e48a1"
+  version "1.3.0"
+  sha256 "9ab146c13d489352de27af4be35f3972de06b1c30ccecbcde40a7aba58a853bb"
 
   url "https://github.com/Chia-Network/chia-blockchain/releases/download/#{version}/Chia-#{version}.dmg",
       verified: "github.com/Chia-Network/chia-blockchain/"
