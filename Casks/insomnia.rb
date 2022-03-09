14334055f9810ea5f81a52ae159c3a76fbf6155c
insomnia-infra <88453862+insomnia-infra@users.noreply.github.com>
2022-03-09 20:23:03 +0000

    Update insomnia from 2022.1.0 to 2022.1.1

--- Casks/insomnia.rb
+++ Casks/insomnia.rb
@@ -1,6 +1,6 @@
 cask "insomnia" do
-  version "2022.1.0"
-  sha256 "f6d2422e3bb34c3d9e272cd89199a451740ddf922e0f2e0107aff20e1809e9e5"
+  version "2022.1.1"
+  sha256 "d6835f3ac7e40992c3992f2a124770f2cbcbc7431661d3b721e1cbed570af033"
 
   url "https://github.com/Kong/insomnia/releases/download/core%40#{version}/Insomnia.Core-#{version}.dmg",
       verified: "github.com/Kong/insomnia/"
