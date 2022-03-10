6c5ff889f17575e102320e4be7b94b66e211778d
Yusuf Kürşad Kaya <ykursadkaya@gmail.com>
2022-03-10 13:05:37 +0300

    Update authy from 2.0.0 to 2.1.0 (#120266)

--- Casks/authy.rb
+++ Casks/authy.rb
@@ -1,6 +1,6 @@
 cask "authy" do
-  version "2.0.0"
-  sha256 "73f4f6df942d0fbaeee7da134b538da8e468e641c33948769e2e7b68c47eea63"
+  version "2.1.0"
+  sha256 "988f53a6d18fce648f5c6f6055cb7db644f186667a3d7fce6571f288f94e70e6"
 
   url "https://authy-electron-repository-production.s3.amazonaws.com/authy/stable/#{version}/darwin/x64/Authy%20Desktop-#{version}.dmg",
       verified: "authy-electron-repository-production.s3.amazonaws.com/"
