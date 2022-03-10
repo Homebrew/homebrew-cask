2bc9e3871115965dfd91e5fc583ce1bc756875b3
neutric <5984479+neutric@users.noreply.github.com>
2022-03-10 11:16:18 +0100

    Update tencent-docs from 2.2.33 to 2.2.35 (#120284)

--- Casks/tencent-docs.rb
+++ Casks/tencent-docs.rb
@@ -1,8 +1,10 @@
 cask "tencent-docs" do
-  version "2.2.33"
+  arch = Hardware::CPU.intel? ? "X64" : "Arm64"
+
+  version "2.2.35"
   sha256 :no_check
 
-  url "https://down.qq.com/qqweb/mac_docs/MacTencentDocs.dmg"
+  url "https://down.qq.com/qqweb/mac_docs/MacTencentDocs#{arch}.dmg"
   name "Tencent Docs"
   name "腾讯文档"
   desc "Online editor for Word, Excel and PPT documents"
