80ae1617e2cb3fe177dce96c456ff456d0cb4cf1
hw <hussainweb@gmail.com>
2022-03-10 13:59:36 -0500

    Update logseq from 0.6.2 to 0.6.3

--- Casks/logseq.rb
+++ Casks/logseq.rb
@@ -1,12 +1,12 @@
 cask "logseq" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "0.6.2"
+  version "0.6.3"
 
   if Hardware::CPU.intel?
-    sha256 "022312ff93e2ea9127f9225f4194e92ccf6502dc1be4a01f5e7d3d587a59a02d"
+    sha256 "cd86be0210273750c77958f2e24bbf056735ff4748af2780818fa538cdf2240a"
   else
-    sha256 "4a69a1e475585f458afa6a359ac5a9763d154511c335d47cc9ce50004e58ae78"
+    sha256 "302ce938571da71ce2f4c0c1fff2fc7059cbe50e219f6192bfdb583bc272df64"
   end
 
   url "https://github.com/logseq/logseq/releases/download/#{version}/logseq-darwin-#{arch}-#{version}.dmg"
