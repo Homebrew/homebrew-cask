8543da078de66baf18055bbbf366ee486abe9bfb
neutric <5984479+neutric@users.noreply.github.com>
2022-03-11 13:34:19 +0100

    Update pitch from 1.57.3 to 1.66.0 (#120335)

--- Casks/pitch.rb
+++ Casks/pitch.rb
@@ -1,6 +1,6 @@
 cask "pitch" do
-  version "1.57.3,2546277"
-  sha256 "89cae692935d3a8117d0e78bd421b91be4c3f1e36e53d692c6b51e94da029e2d"
+  version "1.66.0,3145359"
+  sha256 "1957b00da9c2e4d8607aa8264bde946bb389a4941c6d0b886f8e851c6ab15e75"
 
   url "https://desktop-app-builds.pitch.com/Pitch-#{version.csv.first}-ci#{version.csv.second}.dmg"
   name "Pitch"
