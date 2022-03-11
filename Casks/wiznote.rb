0d870f0f9ed9212aa8bc2f61edf0f6b0f9ef393c
Aaron Jheng <wentworth@outlook.com>
2022-03-11 11:10:05 +0800

    Update wiznote from 0.1.66 to 0.1.67

--- Casks/wiznote.rb
+++ Casks/wiznote.rb
@@ -1,16 +1,15 @@
 cask "wiznote" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "0.1.66"
+  version "0.1.67"
 
   if Hardware::CPU.intel?
-    sha256 "2616f22d08cf8ecaedd8b0133de51bd60b2ad9d9e81d38594a9e99143af504f8"
+    sha256 "2aa02bc6cd0d508ce2857d3b2d60693b592a5ea64d1cba146fc723f0a0e85da4"
   else
-    sha256 "c64c544274fc13c032e1f5e030e7176b2ed7eaedb4f4899eef83109cf2181975"
+    sha256 "b313e10bfd8b0b125b932cfcc2328b7720535ab027894ec4e4612e7dab6e5ceb"
   end
 
-  url "https://wizteam-generic.pkg.coding.net/client/wiznote-autoupdate/wiznote-desktop-#{version}-mac-#{arch}.dmg",
-      verified: "wizteam-generic.pkg.coding.net/client/wiznote-autoupdate/"
+  url "https://get.wiz.cn/x/wiznote-desktop-#{version}-mac-#{arch}.dmg"
   name "WizNote"
   desc "Note-taking application"
   homepage "https://www.wiz.cn/"
