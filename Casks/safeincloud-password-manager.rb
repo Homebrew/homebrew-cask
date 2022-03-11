956505bb2171fb122584fde457b5c5b195924e8e
Fabian Pfütsch <54020707+fpfuetsch@users.noreply.github.com>
2022-03-11 17:42:41 +0100

    Add checksum and description

--- Casks/safeincloud-password-manager.rb
+++ Casks/safeincloud-password-manager.rb
@@ -1,9 +1,10 @@
 cask "safeincloud-password-manager" do
   version "21.4.2,210402"
-  sha256 :no_check
+  sha256 "1a47926c8c73abbf6e4246060be9ab081466045c920bd975cd0a6872dca902b9"
 
   url "https://www.safe-in-cloud.com/download/SafeInCloud.dmg"
   name "SafeInCloud Password Manager"
+  desc "Cross-platform AES-256 password manager"
   homepage "https://www.safe-in-cloud.com/"
 
   livecheck do
