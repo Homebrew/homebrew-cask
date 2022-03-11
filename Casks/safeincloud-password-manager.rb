f8a11f934fecdd996f07d6d8b6958f16707263b8
Fabian Pfütsch <54020707+fpfuetsch@users.noreply.github.com>
2022-03-11 22:04:44 +0100

    Update safeincloud-password-manager from 21.0.5 to 21.4.2 (#120250)

--- Casks/safeincloud-password-manager.rb
+++ Casks/safeincloud-password-manager.rb
@@ -1,9 +1,10 @@
 cask "safeincloud-password-manager" do
-  version "21.0.5,210005"
+  version "21.4.2,210402"
   sha256 :no_check
 
   url "https://www.safe-in-cloud.com/download/SafeInCloud.dmg"
   name "SafeInCloud Password Manager"
+  desc "Cross-platform AES-256 password manager"
   homepage "https://www.safe-in-cloud.com/"
 
   livecheck do
