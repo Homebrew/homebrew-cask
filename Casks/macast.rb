51718b118e53e07b8ce4b16991db393dd102ed36
Bevan Kay <email@bevankay.me>
2022-03-12 17:47:48 +1100

    Update macast.rb

--- Casks/macast.rb
+++ Casks/macast.rb
@@ -3,7 +3,7 @@ cask "macast" do
   sha256 "076500271e727f11f02eebb2731d2e6e80cf80d5f077fc1191293660312e2cfa"
 
   url "https://github.com/xfangfang/Macast/releases/download/v#{version}/Macast-MacOS-v#{version}.dmg"
-  name "macast"
+  name "Macast"
   desc "DLNA Media Renderer"
   homepage "https://github.com/xfangfang/Macast"
 
