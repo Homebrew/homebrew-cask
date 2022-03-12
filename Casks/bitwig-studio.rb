41861288b779068f211aaa82e21567be437641b9
Simon Metson <simon.metson@enterprisedb.com>
2022-03-12 07:10:59 +0000

    Update bitwig-studio from 4.1.6 to 4.2 (#120379)
    Co-authored-by: Simon Metson <drsm79@Simons-Air.5abwr>

--- Casks/bitwig-studio.rb
+++ Casks/bitwig-studio.rb
@@ -1,6 +1,6 @@
 cask "bitwig-studio" do
-  version "4.1.6"
-  sha256 "12f9dd8e32fca36773a4d130fb4f0164431d9410fce8698f75404f2acd9d9e56"
+  version "4.2"
+  sha256 "3798ff4d388e591c8321ef0053220bdb5fbe9961473f0a8947c11c6812eded91"
 
   url "https://downloads.bitwig.com/stable/#{version}/Bitwig%20Studio%20#{version}.dmg"
   name "Bitwig Studio"
