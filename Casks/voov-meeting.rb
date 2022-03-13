397ea1dc04eb41c0498b958967e432bd49119b73
peeweep <peeweep@0x0.ee>
2022-03-13 21:54:51 +0800

    Update voov-meeting from 1.7.1.510 to 3.3.0.520

--- Casks/voov-meeting.rb
+++ Casks/voov-meeting.rb
@@ -1,22 +1,28 @@
 cask "voov-meeting" do
-  version "1.7.1.510,1410000198"
-  sha256 "7e43ecc30d0eb99ab511bb8709b89d6c3916a5a05b638d1c4980210669ac669a"
+  version "3.3.0.520,1410000198"
+  sha256 :no_check
 
-  url "https://down.qq.com/download/VooVMeeting_#{version.csv.second}_#{version.csv.first}.publish.dmg",
-      verified: "down.qq.com/download/"
+  if Hardware::CPU.intel?
+    url "https://updatecdn.meeting.qq.com/e3a8df8a8f31495d20d4dea1ad978336/VooVMeeting_#{version.csv.second}_#{version.csv.first}.publish.x86_64.dmg",
+        verified: "updatecdn.meeting.qq.com"
+  else
+    url "https://updatecdn.meeting.qq.com/e187c2b2db4619068591b905c134362d/VooVMeeting_#{version.csv.second}_#{version.csv.first}.publish.arm64.dmg",
+        verified: "updatecdn.meeting.qq.com"
+  end
+
   name "VooV Meeting"
   name "Tencent Meeting International Version"
   desc "Cross-border video conferencing software"
   homepage "https://voovmeeting.com/"
 
   livecheck do
-    url "https://voovmeeting.com/download/darwin"
-    strategy :header_match do |headers|
-      match = headers["location"].match(/_(\d+)_(\d+(?:\.\d+)*)\.publish\.dmg/)
-      next if match.blank?
-
-      "#{match[2]},#{match[1]}"
-    end
+    # instance = Hardware::CPU.intel? ? "mac" : "mac_arm64"
+    #
+    # curl \
+    #   'https://voovmeeting.com/wemeet-webapi/v2/config/query-download-info'\
+    #   --data-raw
+    #   '[{"instance":"mac_arm64","type":"1410000198"}]'
+    skip "only works with POST request"
   end
 
   app "VooV Meeting.app"
