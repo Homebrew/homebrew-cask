06dfeabd6d2783fa62073efa5aa8ada5a80d43ef
neutric <5984479+neutric@users.noreply.github.com>
2022-03-13 06:50:23 +0100

    Update pyfa from 2.39.3 to 2.40.0 (#120422)

--- Casks/pyfa.rb
+++ Casks/pyfa.rb
@@ -1,6 +1,6 @@
 cask "pyfa" do
-  version "2.39.3"
-  sha256 "f9c0ea71fe10f99d734766625bff4c3e59954f62d3c2c6edee0e84b67a5c7fce"
+  version "2.40.0"
+  sha256 "9abe511bdf3e31208053e0414b453edc0f96c10b52e2a70938d1bdeee08faa8c"
 
   url "https://github.com/pyfa-org/Pyfa/releases/download/v#{version}/pyfa-v#{version}-mac.zip"
   name "pyfa"
