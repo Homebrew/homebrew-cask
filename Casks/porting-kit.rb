e77144a9581aec14fbfb66158943471e0b76960e
ZepDonald <63632671+ZepDonald@users.noreply.github.com>
2022-03-13 06:46:57 +0100

    Update porting-kit from 4.2.41 to 5.0.5 (#120412)

--- Casks/porting-kit.rb
+++ Casks/porting-kit.rb
@@ -1,6 +1,6 @@
 cask "porting-kit" do
-  version "4.2.41"
-  sha256 "57ba837dd380ff731217476ee698e1879696aa205c47c33b82a9f0f7f6a7be84"
+  version "5.0.5"
+  sha256 "a1985298a96264d5bdedeee294931a53218daaf7c71bc40d052f5f532c009632"
 
   url "https://github.com/vitor251093/porting-kit-releases/releases/download/v#{version}/Porting-Kit-#{version}.dmg",
       verified: "github.com/vitor251093/porting-kit-releases/"
