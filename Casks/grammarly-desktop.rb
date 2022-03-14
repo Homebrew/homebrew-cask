6efd768dc3346b7cd2dad9ac723d354b0b512f56
Luca Palli <luca@palli.ch>
2022-03-14 23:12:28 +0100

    Update grammarly-desktop from 1.0.4.6 to 1.0.4.8 (#120492)

--- Casks/grammarly-desktop.rb
+++ Casks/grammarly-desktop.rb
@@ -1,6 +1,6 @@
 cask "grammarly-desktop" do
-  version "1.0.4.6"
-  sha256 "93845a4c5c0d56df60b70764bde1a35319c761b1930c441e207b9c89ba034199"
+  version "1.0.4.8"
+  sha256 "05e1857f8c790114475527407086a942cfd9c6a40d646f118c7496a552b24ae3"
 
   url "https://download-mac.grammarly.com/versions/#{version}/Grammarly.dmg"
   name "Grammarly Desktop"
