3c46e55ccbed7dcc5d7765661b322255d02829dc
neutric <5984479+neutric@users.noreply.github.com>
2022-03-14 22:56:35 +0100

    Update tencent-meeting from 3.5.3.446 to 3.5.4.418

--- Casks/tencent-meeting.rb
+++ Casks/tencent-meeting.rb
@@ -2,8 +2,8 @@ cask "tencent-meeting" do
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 
   if Hardware::CPU.intel?
-    version "3.5.3.446,3547be73ca3e01870a51a0f2b74700d7"
-    sha256 "1d47d1bea902bd4ea0ab663f766d2c041eb6153239a08d62f997ae033f8036e3"
+    version "3.5.4.418,c5549c1521cf625035f918c41719a72f"
+    sha256 "ad7b17e29a97229c6f891fe791fa397034ac5b04ec6035763456a490c89ff0b1"
   else
     version "3.5.3.446,a5ce26ed13217ff63ccdb3fec1ae0659"
     sha256 "be4f2fb454675cd7fc78a9a8f28bb2993ee42964eae9b476a567d8e472ed7378"
