225faa217e2385eee9aa966b83412b1f37db28f2
Ondřej Führer <ondrejfuhrer@users.noreply.github.com>
2022-03-14 16:23:01 +0100

    Update metasploit from 6.1.34,20220312112922 to 6.1.34,20220314102916 (#120481)

--- Casks/metasploit.rb
+++ Casks/metasploit.rb
@@ -1,6 +1,6 @@
 cask "metasploit" do
-  version "6.1.34,20220312112922"
-  sha256 "5faffb7f2ec69e41d4e7f9f42af7b2d54d76ce4c9565be633a6e5aa2835c0d5d"
+  version "6.1.34,20220314102916"
+  sha256 "e4573978ff6d346bbd39de7e61ae8d71f2633dae6f5e77e5506ba6a6e2352be1"
 
   url "https://osx.metasploit.com/metasploit-framework-#{version.csv.first}%2B#{version.csv.second}-1rapid7-1.x86_64.pkg"
   name "Metasploit Framework"
