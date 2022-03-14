ef1fd4f0a356630b8e8d188a536fa3a501144376
Matt Schrage <matthewschrage@gmail.com>
2022-03-14 01:33:49 -0700

    Update fig cask (#120384)
    * fix: update uninstall executable
    
    * fix: conform to brew style guide
    
    * Update hash & version
    
    * Update fig.rb
    
    * fig: update path to executable
    
    * Update fig.rb
    
    * Update order of zapped folders to be alphabetical

--- Casks/fig.rb
+++ Casks/fig.rb
@@ -1,6 +1,6 @@
 cask "fig" do
-  version "1.0.55,383"
-  sha256 "c4ab58b7f7f529d7fae4ed996c86921cc9c940ccbaf2ae8ddc24d28db9945678"
+  version "1.0.56,403"
+  sha256 "080a94b3f4020c6071eeadc44294732c51d1f185ccbc3535a54fb15d5564eeee"
 
   url "https://versions.withfig.com/fig%20#{version.csv.second}.dmg",
       verified: "versions.withfig.com/"
@@ -17,16 +17,37 @@ cask "fig" do
   depends_on macos: ">= :high_sierra"
 
   app "Fig.app"
+  binary "#{appdir}/Fig.app/Contents/MacOS/fig-darwin-universal", target: "fig"
 
-  uninstall script: "#{appdir}/Fig.app/Contents/Resources/config/tools/uninstall-script.sh"
+  uninstall script:
+                       {
+                         executable: "#{appdir}/Fig.app/Contents/MacOS/fig-darwin-universal",
+                         args:       ["app", "uninstall"],
+                       },
+            launchctl:
+                       [
+                         "io.fig.launcher",
+                         "io.fig.uninstall",
+                         "io.fig.dotfiles-daemon",
+                       ],
+            quit:
+                       [
+                         "com.mschrage.fig",
+                         "io.fig.input-method.cursor",
+                       ]
 
   zap trash: [
     "~/.fig",
     "~/.fig.dotfiles.bak",
     "~/Library/Application Support/com.mschrage.fig",
+    "~/Library/Application Support/fig",
     "~/Library/Caches/com.mschrage.fig",
     "~/Library/Caches/fig",
     "~/Library/Preferences/com.mschrage.fig.*",
     "~/Library/WebKit/com.mschrage.fig",
   ]
+
+  caveats <<~EOS
+    Please launch the Fig application to finish setup...
+  EOS
 end
