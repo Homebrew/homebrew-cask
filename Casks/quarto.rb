4dcc1861ad299f1887ab652da8160c02d0c32694
Colin Rundel <rundel@gmail.com>
2022-03-14 14:40:40 -0400

    Update quarto from 0.9.88 to 0.9.90

--- Casks/quarto.rb
+++ Casks/quarto.rb
@@ -1,6 +1,6 @@
 cask "quarto" do
-  version "0.9.88"
-  sha256 "1681b8bc6c0b1f9a9b4e77382b593683db40b2f5a3844b09809442e0095c9c2a"
+  version "0.9.90"
+  sha256 "f96603798d3f590f41b1ea088a213c819b3a81a9ba8ce3f95b1386ce6b549b8a"
 
   url "https://github.com/quarto-dev/quarto-cli/releases/download/v#{version}/quarto-#{version}-macos.pkg",
       verified: "github.com/quarto-dev/quarto-cli/"
