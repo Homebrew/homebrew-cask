225137dd80bc1400d60f05c6bc9442b1df203816
neutric <5984479+neutric@users.noreply.github.com>
2022-03-14 06:12:25 +0100

    Update unity-android-support-for-editor from 2021.2.14f1 to 2021.2.15f1 (#120452)

--- Casks/unity-android-support-for-editor.rb
+++ Casks/unity-android-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask "unity-android-support-for-editor" do
-  version "2021.2.14f1,bcb93e5482d2"
-  sha256 "88e127d8fd827842e17a1adcd2e0858ae03c30245c8ae683d0da9ae1f4619ee2"
+  version "2021.2.15f1,3b8567f665bb"
+  sha256 "7ef27c05ae5d554b0ce3206866d5d2c3b82e7e3331b8005aa4ea991e751fd93b"
 
   url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-#{version.csv.first}.pkg",
       verified: "download.unity3d.com/download_unity/"
