baffe91a77a94a27cec5af94d7d4266b4de48c88
neutric <5984479+neutric@users.noreply.github.com>
2022-03-16 14:06:27 +0100

    Update navicat-for-sqlite from 16.0.8 to 16.0.11

--- Casks/navicat-for-sqlite.rb
+++ Casks/navicat-for-sqlite.rb
@@ -1,5 +1,5 @@
 cask "navicat-for-sqlite" do
-  version "16.0.8"
+  version "16.0.11"
   sha256 :no_check
 
   url "https://download.navicat.com/download/navicat#{version.major_minor.no_dots}_sqlite_en.dmg"
