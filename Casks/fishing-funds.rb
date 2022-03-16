bdaa29448207a4df1d3dc55eb79796a8ebbaa9b3
1zilc <dywzzjx@163.com>
2022-03-16 13:59:57 +0800

    Update fishing-funds from 5.6.0 to 5.7.1

--- Casks/fishing-funds.rb
+++ Casks/fishing-funds.rb
@@ -1,12 +1,12 @@
 cask "fishing-funds" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  version "5.6.0"
+  version "5.7.1"
 
   if Hardware::CPU.intel?
-    sha256 "8a130e705576de17cf2c01b527546a0eaad02a51a8a41f27223e9e600a28b005"
+    sha256 "9bf33b0b8efeebe78e22f8c066bc43fefb0209112dba7327886e09ba1ae0107d"
   else
-    sha256 "72a806351ef31345abb0b6b7ba5446e26cb0047737d550d5ae296f0a999f7022"
+    sha256 "d0e82ed7e7bfe9ce2844346879736b64b2dee73f33f684cbcc6bc9d8962390c1"
   end
 
   url "https://github.com/1zilc/fishing-funds/releases/download/v#{version}/Fishing-Funds-#{version}#{arch}.dmg",
