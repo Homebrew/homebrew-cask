788c7d4c8489f09a447b977316b556a62c34c20b
darklore <958690+darklore@users.noreply.github.com>
2022-03-16 20:02:14 +0900

    Update codeql from 2.8.0 to 2.8.3 (#120545)

--- Casks/codeql.rb
+++ Casks/codeql.rb
@@ -1,6 +1,6 @@
 cask "codeql" do
-  version "2.8.0"
-  sha256 "39de8d8dca05820744ff27510af7bd9ef1a3150c0512b150c9fd808c72e01d43"
+  version "2.8.3"
+  sha256 "5773a36c273e210a96f0bd8977d4b76a3a6835aa8bc5df4b59394f3518cc7a77"
 
   url "https://github.com/github/codeql-cli-binaries/releases/download/v#{version}/codeql-osx64.zip"
   name "CodeQL"
