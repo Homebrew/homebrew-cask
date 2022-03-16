3e532c827768ffd37f18a8e31795b02c267c015c
tuzi3040 <28290621+tuzi3040@users.noreply.github.com>
2022-03-17 03:59:21 +0800

    Update powershell from 7.2.1 to 7.2.2 (#120579)

--- Casks/powershell.rb
+++ Casks/powershell.rb
@@ -1,12 +1,12 @@
 cask "powershell" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "7.2.1"
+  version "7.2.2"
 
   if Hardware::CPU.intel?
-    sha256 "23046dbada76832e441b838170e3e36ada54a69c4f3ae25e210be554e659fce4"
+    sha256 "289093be09b6bbfbf0f41f59f1881bdc97620d979d8ef846a9e110caa5f07a7a"
   else
-    sha256 "034bdb6ca923036a4d4e2a065810f9cf96b07ea5781bc1c81330417433dd1ea6"
+    sha256 "2174808f72ff5457f13530a7121608c050dc2203b1a21fec057f27a10ce5fbd7"
   end
 
   url "https://github.com/PowerShell/PowerShell/releases/download/v#{version}/powershell-#{version}-osx-#{arch}.pkg"
