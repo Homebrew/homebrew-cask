972e4bc6b16ac1e5e11bc3d9114b07453b688d0c
Nils Streedain <tannins_berets_0@icloud.com>
2022-03-16 00:35:18 -0700

    Add Balena Etcher builds for Apple Silicon

--- Casks/balenaetcher.rb
+++ Casks/balenaetcher.rb
@@ -1,9 +1,18 @@
 cask "balenaetcher" do
+  arch = Hardware::CPU.intel? ? "" : "arm64/"
+  
   version "1.7.7"
-  sha256 "61a8835661e22854204cbd0c9948312f2641be8e02eea43d5c507f369b167972"
-
-  url "https://github.com/balena-io/etcher/releases/download/v#{version}/balenaEtcher-#{version}.dmg",
+  
+  if Hardware::CPU.intel?
+    sha256 "61a8835661e22854204cbd0c9948312f2641be8e02eea43d5c507f369b167972"
+    url "https://github.com/balena-io/etcher/releases/download/v#{version}/balenaEtcher-#{version}.dmg",
       verified: "github.com/balena-io/etcher/"
+  else
+    sha256 "f273b2ea65a0edcf0334230b62234e81e617880ee2e99db12fa93424d7a8921a"
+    url "https://github.com/Augmentedjs/balena-io-etcher-builds/releases/download/v#{version}/balenaEtcher-#{version}.dmg",
+      verified: "github.com/Augmentedjs/balena-io-etcher-builds/"
+  end
+
   name "Etcher"
   desc "Tool to flash OS images to SD cards & USB drives"
   homepage "https://balena.io/etcher"
