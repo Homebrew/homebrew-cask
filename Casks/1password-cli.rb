71b2445de6c49feb0cef6885dacde80497fc7c5f
Jameel Al-Aziz <me@jalaziz.io>
2022-03-15 18:25:47 -0700

    Update 1password-cli from 1.12.4 to 2.0.0

--- Casks/1password-cli.rb
+++ Casks/1password-cli.rb
@@ -1,15 +1,15 @@
 cask "1password-cli" do
-  version "1.12.4"
-  sha256 "a484de66fa64252c51f3a90d6b8a1dad70bd4537b786466f897b99badb51a1ee"
+  version "2.0.0"
+  sha256 "0c2f6177346343d88d8db7ba3df469317cc37889eae2b61201ada70c740c4d1a"
 
-  url "https://cache.agilebits.com/dist/1P/op/pkg/v#{version}/op_apple_universal_v#{version}.pkg",
-      verified: "cache.agilebits.com/dist/1P/op/pkg/"
+  url "https://cache.agilebits.com/dist/1P/op2/pkg/v#{version}/op_apple_universal_v#{version}.pkg",
+      verified: "cache.agilebits.com/dist/1P/op2/pkg/"
   name "1Password CLI"
   desc "Command-line helper for the 1Password password manager"
-  homepage "https://support.1password.com/command-line/"
+  homepage "https://developer.1password.com/docs/cli"
 
   livecheck do
-    url "https://app-updates.agilebits.com/product_history/CLI"
+    url "https://app-updates.agilebits.com/product_history/CLI2"
     regex(%r{href=.*?/op_apple_universal[._-]v?(\d+(?:\.\d+)+)\.pkg}i)
   end
 
