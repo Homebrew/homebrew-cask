07d5a95bffa8bcf6fb2a2ea282bc2b7b548ac8ae
peeweep <peeweep@0x0.ee>
2022-03-16 09:43:18 +0800

    voov-meeting: add md5 to version, clean comment.

--- Casks/voov-meeting.rb
+++ Casks/voov-meeting.rb
@@ -1,27 +1,23 @@
 cask "voov-meeting" do
-  version "3.3.2.510,1410000198"
-  sha256 :no_check
+  arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 
   if Hardware::CPU.intel?
-    url "https://updatecdn.meeting.qq.com/255661c36b01c1f58129754358b192ac/VooVMeeting_#{version.csv.second}_#{version.csv.first}.publish.x86_64.dmg",
-        verified: "updatecdn.meeting.qq.com"
+    version "3.3.2.510,1410000198,255661c36b01c1f58129754358b192ac"
+    sha256 "e33c32b2af728c3ac0277156ef75e781fcec48f7191dc93c81669cde9b13dd64"
   else
-    url "https://updatecdn.meeting.qq.com/4c95e5f9ac1a586edab01d761aa8e0be/VooVMeeting_#{version.csv.second}_#{version.csv.first}.publish.arm64.dmg",
-        verified: "updatecdn.meeting.qq.com"
+    version "3.3.2.510,1410000198,4c95e5f9ac1a586edab01d761aa8e0be"
+    sha256 "062e9ed7bf86dd64087100b595b3bd2e406ceb0aa5d1cf288513b05424bde783"
   end
+  url "https://updatecdn.meeting.qq.com/#{version.csv.third}/VooVMeeting_#{version.csv.second}_#{version.csv.first}.publish.#{arch}.dmg",
+      verified: "updatecdn.meeting.qq.com"
 
   name "VooV Meeting"
   name "Tencent Meeting International Version"
   desc "Cross-border video conferencing software"
   homepage "https://voovmeeting.com/"
 
+  # See https://github.com/Homebrew/homebrew-cask/pull/120458#issuecomment-1068393782
   livecheck do
-    # instance = Hardware::CPU.intel? ? "mac" : "mac_arm64"
-    #
-    # curl \
-    #   'https://voovmeeting.com/wemeet-webapi/v2/config/query-download-info'\
-    #   --data-raw
-    #   '[{"instance":"mac_arm64","type":"1410000198"}]'
     skip "only works with POST request"
   end
 
