4257679f66271257a57d09f7bc978e3f9fbe2900
Nils Streedain <tannins_berets_0@icloud.com>
2022-03-16 01:17:43 -0700

    Reran 'brew style --fix balenEtcher' locally

--- Casks/balenaetcher.rb
+++ Casks/balenaetcher.rb
@@ -1,15 +1,14 @@
 cask "balenaetcher" do
-  
   version "1.7.7"
-  
+
   if Hardware::CPU.intel?
     sha256 "61a8835661e22854204cbd0c9948312f2641be8e02eea43d5c507f369b167972"
     url "https://github.com/balena-io/etcher/releases/download/v#{version}/balenaEtcher-#{version}.dmg",
-      verified: "github.com/balena-io/etcher/"
+        verified: "github.com/balena-io/etcher/"
   else
     sha256 "f273b2ea65a0edcf0334230b62234e81e617880ee2e99db12fa93424d7a8921a"
     url "https://github.com/Augmentedjs/balena-io-etcher-builds/releases/download/v#{version}/balenaEtcher-#{version}.dmg",
-      verified: "github.com/Augmentedjs/balena-io-etcher-builds/"
+        verified: "github.com/Augmentedjs/balena-io-etcher-builds/"
   end
 
   name "Etcher"
