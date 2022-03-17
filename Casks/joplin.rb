86e493a213d096e243f97dc6f9913f9eeab3af66
Takahiro Yoshimura <takahiro_y@monolithworks.co.jp>
2022-03-17 22:32:31 +0900

    Update joplin from 2.7.14 to 2.7.15

--- Casks/joplin.rb
+++ Casks/joplin.rb
@@ -1,6 +1,6 @@
 cask "joplin" do
-  version "2.7.14"
-  sha256 "be3a26b754d3a382c18c72ccb98b72ad50485442659f562d886525d7246b75e1"
+  version "2.7.15"
+  sha256 "08f0ffdb1e45c472fd0ac633f446495f94988851b3eff7e39ed3a50cc2876040"
 
   url "https://github.com/laurent22/joplin/releases/download/v#{version}/Joplin-#{version}.dmg",
       verified: "github.com/laurent22/joplin/"
