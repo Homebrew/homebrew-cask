445719cc0e1ab10518cc552ef32299e7b067646e
Matthew Gregg <matthew.gregg97@icloud.com>
2022-03-18 11:36:17 +0000

    Update regex to ignore preview release

--- Casks/android-ndk.rb
+++ Casks/android-ndk.rb
@@ -10,7 +10,7 @@ cask "android-ndk" do
 
   livecheck do
     url "https://developer.android.com/ndk/downloads"
-    regex(/Latest\s+r(\d+[a-z]?)/i)
+    regex(/Latest\s+r(\d+[a-z]?)\b(?!\s+Preview)/i)
   end
 
   conflicts_with cask: "crystax-ndk"
