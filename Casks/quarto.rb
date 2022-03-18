d958f8499ff47d7cb745f934cd99679d60969a89
Clément L <porkepix@gmail.com>
2022-03-18 07:15:14 +0100

    Update quarto from 0.9.108 to 0.9.112 (#120644)

--- Casks/quarto.rb
+++ Casks/quarto.rb
@@ -1,6 +1,6 @@
 cask "quarto" do
-  version "0.9.108"
-  sha256 "b1ba47fbba15dd90a157785c81fa304a655011ee7fd4aa226136f2f795b209a2"
+  version "0.9.112"
+  sha256 "f780dbcc6c7168c36321ad443657f3bac9205be1fe65c15328ea9e1037b481bf"
 
   url "https://github.com/quarto-dev/quarto-cli/releases/download/v#{version}/quarto-#{version}-macos.pkg",
       verified: "github.com/quarto-dev/quarto-cli/"
