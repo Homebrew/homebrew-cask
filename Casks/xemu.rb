1c07f63779f7555895119177184abb6a75591db6
Clément L <porkepix@gmail.com>
2022-03-18 10:19:15 +0100

    Update xemu from 0.6.2-80-g5d02cdf512 to 0.6.2-84-gcd5d3df626 (#120611)

--- Casks/xemu.rb
+++ Casks/xemu.rb
@@ -1,6 +1,6 @@
 cask "xemu" do
-  version "0.6.2-80-g5d02cdf512"
-  sha256 "5bdb5692d8bdc697a396de87bba1038ba0855cdff5d2375bfb03fa21bbd9184d"
+  version "0.6.2-84-gcd5d3df626"
+  sha256 "e715e6d01e9ccf5b59c2b468d654d5d981f640cd662cb4b2df7f736f2ca60783"
 
   url "https://github.com/mborgerson/xemu/releases/download/gh-release%2F#{version}/xemu-macos-universal-release.zip",
       verified: "github.com/mborgerson/xemu/"
