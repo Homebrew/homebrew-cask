0f1dd5b37d56523ced1873a520b4084880db5b3a
Schlaubischlump <Schlaubischlump@users.noreply.github.com>
2022-03-18 07:16:06 +0100

    Update LocationSimulator from 0.1.8.2 to 0.1.8.3 (#120631)

--- Casks/locationsimulator.rb
+++ Casks/locationsimulator.rb
@@ -1,6 +1,6 @@
 cask "locationsimulator" do
-  version "0.1.8.2"
-  sha256 "d30a2e8a1b8a67ef33ba7bcb72ae6588eb6b49930da0044c4dacc135d4f693b2"
+  version "0.1.8.3"
+  sha256 "f5301061dad2ba485d38feb39a23cfefbfd48957473fa86939c510d34a9adadb"
 
   url "https://github.com/Schlaubischlump/LocationSimulator/releases/download/v#{version}/LocationSimulator.app.zip"
   name "LocationSimulator"
