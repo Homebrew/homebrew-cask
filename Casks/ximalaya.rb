15bc8e46a2265629a95a8449762219255fb032ef
zuisong <378818573@qq.com>
2022-03-18 17:15:12 +0800

    Update ximalaya from 2.5.0 to 2.6.0 (#120613)
    Co-authored-by: zuisong <com.me@foxmail.com>

--- Casks/ximalaya.rb
+++ Casks/ximalaya.rb
@@ -1,6 +1,6 @@
 cask "ximalaya" do
-  version "2.5.0,1645498451"
-  sha256 "ae46314cfc96ddd6ab7a50cf5181ab30c38cdac49fc84dc5e3b37f48aa920828"
+  version "2.6.0,1647423365"
+  sha256 "acf5eb744fba26ce092b8794c0a5868e2a29a00f1a5fb4429832cb991863b6d0"
 
   url "https://s1.xmcdn.com/yx/ximalaya-pc-client/#{version.csv.first}/download/mac-x64/Ximalaya_#{version.csv.first}_#{version.csv.second}.dmg",
       verified: "s1.xmcdn.com/yx/ximalaya-pc-client/"
