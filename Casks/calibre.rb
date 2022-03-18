2f6a4ac8f2ca8d547e6c790052fe041c9fedde4f
xxyzz <gitpull@protonmail.com>
2022-03-18 14:14:23 +0800

    Update calibre from 5.38.0 to 5.39.0 (#120651)

--- Casks/calibre.rb
+++ Casks/calibre.rb
@@ -3,8 +3,8 @@ cask "calibre" do
     version "3.48.0"
     sha256 "68829cd902b8e0b2b7d5cf7be132df37bcc274a1e5720b4605d2dd95f3a29168"
   else
-    version "5.38.0"
-    sha256 "5aabd6a02ddb8aa91b6237fc8ff449ca1a10e021425188d663bc20ae3119bb78"
+    version "5.39.0"
+    sha256 "eea74dad8bc3b3ed7de7ad0f2f8f32be72a56a1f2267332dd079ae0c5863e5e3"
   end
 
   url "https://download.calibre-ebook.com/#{version}/calibre-#{version}.dmg"
