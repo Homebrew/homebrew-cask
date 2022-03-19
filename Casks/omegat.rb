58ab6c9a553f877546c6c86d510fdc94e66e1bd1
Porkepix <porkepix@gmail.com>
2022-03-20 00:08:09 +0100

    Update omegat from 4.3.2 to 4.3.3

--- Casks/omegat.rb
+++ Casks/omegat.rb
@@ -1,6 +1,6 @@
 cask "omegat" do
-  version "4.3.2"
-  sha256 "e6fef5e4fca21ce7922b0c5fcb5a31039f87bee50b0357e12365ffb272c75210"
+  version "4.3.3"
+  sha256 "7ed557bcd88972be1761bcd96b6f575b54f1eafd93b25b1440a5b3eda845fad8"
 
   url "https://downloads.sourceforge.net/omegat/OmegaT%20-%20Standard/OmegaT%20#{version.major_minor_patch}/OmegaT_#{version}_Mac_Notarized.zip",
       verified: "downloads.sourceforge.net/omegat/"
