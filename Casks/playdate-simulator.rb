0451ca6480bfacc5cdfb7c0050f1954bb443a300
neutric <5984479+neutric@users.noreply.github.com>
2022-03-20 10:26:15 +0100

    Update playdate-simulator from 1.9.1 to 1.9.3 (#120727)
    * Update playdate-simulator from 1.9.1 to 1.9.3
    
    * playdate-simulator: switch livecheck strategy
    
    * playdate-simulator: add license
    
    * playdate-simulator: update pkg stanza

--- Casks/playdate-simulator.rb
+++ Casks/playdate-simulator.rb
@@ -1,21 +1,25 @@
 cask "playdate-simulator" do
-  version "1.9.1"
-  sha256 "efb490b187bc012576a10d53f43dc62917ee7f03a302e680bbd9a9696ff4226a"
+  version "1.9.3"
+  sha256 "7a0335ee0d855883a7b01e40e3f1b6f98fe1c5d239406683d22f0160491ce2d6"
 
-  url "https://download-keycdn.panic.com/playdate_sdk/PlaydateSDK-#{version}.pkg",
+  url "https://download-keycdn.panic.com/playdate_sdk/PlaydateSDK-#{version}.zip",
       verified: "download-keycdn.panic.com/playdate_sdk/"
   name "Playdate SDK"
   desc "Playdate Lua and C APIs, docs and Simulator for local development"
   homepage "https://play.date/dev/"
 
   livecheck do
-    url :homepage
-    regex(/Playdate\sSDK\s(\d+(?:\.\d+)+)/i)
+    url "https://download.panic.com/playdate_sdk/PlaydateSDK-latest.zip"
+    strategy :header_match
   end
 
   depends_on macos: ">= :catalina"
 
-  pkg "PlaydateSDK-#{version}.pkg"
+  pkg "PlaydateSDK.pkg"
 
   uninstall pkgutil: "date.play.sdk"
+
+  caveats do
+    license "https://play.date/dev/sdk-license"
+  end
 end
