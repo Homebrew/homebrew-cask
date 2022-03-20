de50703cb0047a5f93a4ab71076a116fa1c69b94
Sam Ford <1584702+samford@users.noreply.github.com>
2022-03-20 11:47:38 -0400

    eiskaltdcpp: update livecheck (#120748)

--- Casks/eiskaltdcpp.rb
+++ Casks/eiskaltdcpp.rb
@@ -8,7 +8,8 @@ cask "eiskaltdcpp" do
   homepage "https://sourceforge.net/projects/eiskaltdcpp/"
 
   livecheck do
-    regex(/EiskaltDC%2B%2B[._-]v?(\d+(?:\.\d+)+)[._-]x86_64\.dmg/i)
+    url "https://sourceforge.net/projects/eiskaltdcpp/rss?path=/macOS"
+    regex(%r{url=.*?/EiskaltDC%2B%2B[._-]v?(\d+(?:\.\d+)+)(?:-[^"']+?)?\.dmg}i)
   end
 
   app "EiskaltDC++.app"
