746af7777ee0da020b003ec30e6bbaf8101394f4
Meet Godhani <meetgodhani23@gmail.com>
2022-03-20 05:26:04 -0400

    Update raven-reader from 1.0.72 to 1.0.72 (#120438)
    * Update raven-reader from 1.0.72 to 1.0.72
    
    * Added rave-reader to secure connection audit skiplist
    
    * Update secure_connection_audit_skiplist.json
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/raven-reader.rb
+++ Casks/raven-reader.rb
@@ -1,6 +1,6 @@
 cask "raven-reader" do
-  version "1.0.71"
-  sha256 "eb08c819ac003428de4cf85fb13bbda0f9288c3e3f6aab7cf378e3fa869dcdd0"
+  version "1.0.72"
+  sha256 "344f514a58a2c400501f4908f898e98cacf6056e6ba2140667c0c575f3ed408a"
 
   url "https://download.helloefficiency.com/ravenreader/Raven%20Reader-#{version}.dmg",
       verified: "download.helloefficiency.com/ravenreader/"
@@ -16,4 +16,13 @@ cask "raven-reader" do
   auto_updates true
 
   app "Raven Reader.app"
+
+  zap trash: [
+    "~/Library/Application Support/Raven Reader",
+    "~/Library/Caches/org.helloefficiency.ravenreader",
+    "~/Library/Caches/org.helloefficiency.ravenreader.ShipIt",
+    "~/Library/Logs/Raven Reader",
+    "~/Library/Preferences/org.helloefficiency.ravenreader.plist",
+    "~/Library/Saved Application State/org.helloefficiency.ravenreader.savedState",
+  ]
 end
