5d8b373b65f1a6c8e42373aecf2b1c52be9fa4ab
neutric <5984479+neutric@users.noreply.github.com>
2022-03-20 10:23:51 +0100

    Update plasticscm-cloud-edition from 10.0.16.6656 to 11.0.16.6718 (#120726)

--- Casks/plasticscm-cloud-edition.rb
+++ Casks/plasticscm-cloud-edition.rb
@@ -1,6 +1,6 @@
 cask "plasticscm-cloud-edition" do
-  version "10.0.16.6656"
-  sha256 "c3376c60f15bdafe810605b927529f0746cc83d621c597a96c0fffdd67e6d02a"
+  version "11.0.16.6718"
+  sha256 "82e8c843d5845cf2710ff8b8e959b49ea3809a03f37d8140c4eb2f9d8afc4a99"
 
   url "https://s3.eu-west-2.amazonaws.com/plastic-releases/releases/#{version}/plasticscm/osx/plasticscm-cloud-#{version}.pkg.zip",
       verified: "s3.eu-west-2.amazonaws.com/plastic-releases/"
