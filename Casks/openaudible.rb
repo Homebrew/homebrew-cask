08d45726a321989d249748bed1cee325c9391923
Jacob Ford <j@cobford.com>
2022-03-20 23:17:25 -0400

    Update openaudible from 3.5.2 to 3.5.3 (#120758)

--- Casks/openaudible.rb
+++ Casks/openaudible.rb
@@ -1,12 +1,12 @@
 cask "openaudible" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
-  version "3.5.2"
+  version "3.5.3"
 
   if Hardware::CPU.intel?
-    sha256 "35274d767cdb9500473febee8a59c82d82a2d4615249343d959cdfc9d237941c"
+    sha256 "bdb1b3098986a7963fe90dc58561fd7275667358b711109238ba97efdac0ff8c"
   else
-    sha256 "868e62eae4e4f5af9aa1d3a907e464946d5a3f9586fbb3b135b9c33404f99b01"
+    sha256 "09874b95ca2a4f8d36209d6995f3386c1920eac39c5a38287e56f7a7b72afb12"
   end
 
   url "https://github.com/openaudible/openaudible/releases/download/v#{version}/OpenAudible_#{version}_#{arch}.dmg",
