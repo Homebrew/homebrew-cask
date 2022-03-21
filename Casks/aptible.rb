5c3536178d2da6faafbefa53cab68b96b0a6b7f6
Alex Kubacki <usernotfound@gmail.com>
2022-03-21 16:34:40 -0600

    Update aptible from 0.19.1,20210802230457,251 to 0.19.3,20220314154920,340,10.15.7-1

--- Casks/aptible.rb
+++ Casks/aptible.rb
@@ -1,8 +1,8 @@
 cask "aptible" do
-  version "0.19.1,20210802230457,251"
-  sha256 "693e38e36edcd5289df8c0456ecfd7e812c668fc368b32ca9cc54fe90698530a"
+  version "0.19.3,20220314154920,340,10.15.7-1"
+  sha256 "e50d899e25172facbac1489a6e24a4d17431631485ca15be8b026be1c99480e8"
 
-  url "https://omnibus-aptible-toolbelt.s3.amazonaws.com/aptible/omnibus-aptible-toolbelt/master/#{version.csv.third}/pkg/aptible-toolbelt-#{version.csv.first}%2B#{version.csv.second}-mac-os-x.10.11.6-1.pkg",
+  url "https://omnibus-aptible-toolbelt.s3.amazonaws.com/aptible/omnibus-aptible-toolbelt/master/#{version.csv.third}/pkg/aptible-toolbelt-#{version.csv.first}%2B#{version.csv.second}-mac-os-x.#{version.csv.fourth}.pkg",
       verified: "omnibus-aptible-toolbelt.s3.amazonaws.com/"
   name "Aptible Toolbelt"
   desc "Command-line tool for Aptible Deploy, an audit-ready App Deployment Platform"
@@ -10,15 +10,15 @@ cask "aptible" do
 
   livecheck do
     url :homepage
-    regex(%r{href=.*?/(\d+)/pkg/aptible-toolbelt[._-]v?(\d+(?:\.\d+)+)%2B(\d+)[._-]mac}i)
+    regex(%r{href=.*?/(\d+)/pkg/aptible-toolbelt[._-]v?(\d+(?:\.\d+)+)%2B(\d+)[._-]mac-os-x\.?(\d+(?:\.\d+)+-\d)\.pkg}i)
     strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| "#{match[1]},#{match[2]},#{match[0]}" }
+      page.scan(regex).map { |match| "#{match[1]},#{match[2]},#{match[0]},#{match[3]}" }
     end
   end
 
-  depends_on formula: "libu2f-host"
+  depends_on formula: "libfido2"
 
-  pkg "aptible-toolbelt-#{version.csv.first}+#{version.csv.second}-mac-os-x.10.11.6-1.pkg"
+  pkg "aptible-toolbelt-#{version.csv.first}+#{version.csv.second}-mac-os-x.#{version.csv.fourth}.pkg"
 
   uninstall pkgutil: "com.aptible.toolbelt"
 end
