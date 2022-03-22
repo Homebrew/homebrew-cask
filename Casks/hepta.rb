1f0d4082142692b8b61755951dc5585523c6a91c
Reno <25192197+singularitti@users.noreply.github.com>
2022-03-22 05:05:32 -0400

    Update hepta from 0.105.0 to 0.110.0 (#120793)

--- Casks/hepta.rb
+++ Casks/hepta.rb
@@ -1,12 +1,12 @@
 cask "hepta" do
   arch = Hardware::CPU.intel? ? "mac" : "arm64-mac"
 
-  version "0.105.0"
+  version "0.110.0"
 
   if Hardware::CPU.intel?
-    sha256 "3a7fbadaf2c34545a94e7487a312b65f1a37d6540ba50edaa468ee64d9ebbc3d"
+    sha256 "6ea733fb756972fcd57deee2ed1f469770fabed841ad76e0b423e03d25fe56d7"
   else
-    sha256 "99bdd5816ef7b17ace6887d517862debf23770f9e8edb8b96ff41085a108940b"
+    sha256 "2cd1952adc090af3b85dac02074ad890461387365804aaccf8060409738b0a67"
   end
 
   url "https://github.com/heptameta/project-meta/releases/download/v#{version}/Hepta-#{version}-#{arch}.zip",
