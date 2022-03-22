1bccb6275e3b472bff130acd90764f6ae58fdc7a
Prasannajit Acharya <prasannajit.acharya.kanhu@gmail.com>
2022-03-22 14:36:32 +0530

    Update ray from 2.0.0 to 2.0.1 (#120777)

--- Casks/ray.rb
+++ Casks/ray.rb
@@ -2,12 +2,12 @@ cask "ray" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
   folder = Hardware::CPU.intel? ? "" : "arm64/"
 
-  version "2.0.0"
+  version "2.0.1"
 
   if Hardware::CPU.intel?
-    sha256 "5e734cd6b75ab00c8df2748d419af294792daae9da7922926a57cf8f653ce514"
+    sha256 "d2a2eda635147a34d136312505f77a3e09123289aa0919e2e942935e379bc7a9"
   else
-    sha256 "5eec7112a2884035d7ce854143869217586b0bb9bd7b823087869a8a76e52f3a"
+    sha256 "75dd6a77e02f9c6c84b1bb95918f706d97a3a3fc6ae8fa0e74c1c0155ba12e78"
   end
 
   url "https://ray-app.s3.eu-west-1.amazonaws.com/#{folder}Ray-#{version}#{arch}.dmg",
