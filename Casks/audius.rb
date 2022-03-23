115466d42c23f85d9cefd87893fa1750ac9199f7
Hannes Braun <hannesbraun@mail.de>
2022-03-23 13:24:56 +0100

    Update audius from 1.1.2 to 1.1.3 (#120869)

--- Casks/audius.rb
+++ Casks/audius.rb
@@ -1,6 +1,6 @@
 cask "audius" do
-  version "1.1.2"
-  sha256 "d162e8c3e8acf5c63968dbef4a3528ae06182c48bcc7cbba656a05df960ba5d4"
+  version "1.1.3"
+  sha256 "a46f1581c654be6c8a59f3fc1064e0b12eb432df22f9beafd1fad11c11f2d29f"
 
   url "https://download.audius.co/Audius-#{version}.dmg"
   name "Audius"
