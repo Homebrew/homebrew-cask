ab474f2a4cf7d67c7a616b70c7766455573d0bb6
Lex.Chen <ctyytc@gmail.com>
2022-03-23 16:05:08 +0800

    Update springtoolsuite.rb to add aarch64 (#120768)
    * Update springtoolsuite.rb to add aarch64
    
    Update springtoolsuite.rb to add aarch64
    
    * Update springtoolsuite.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/springtoolsuite.rb
+++ Casks/springtoolsuite.rb
@@ -1,8 +1,15 @@
 cask "springtoolsuite" do
-  version "4.14.0,4.23.0"
-  sha256 "973b0b6a332e60649bf2b989f0b2f7f8986f9138641e4cfa11e7ff276a2cc598"
+  arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
-  url "https://download.springsource.com/release/STS#{version.major}/#{version.csv.first}.RELEASE/dist/e#{version.csv.second.major_minor}/spring-tool-suite-#{version.major}-#{version.csv.first}.RELEASE-e#{version.csv.second}-macosx.cocoa.x86_64.dmg",
+  version "4.14.0,4.23.0"
+
+  if Hardware::CPU.intel?
+    sha256 "973b0b6a332e60649bf2b989f0b2f7f8986f9138641e4cfa11e7ff276a2cc598"
+  else
+    sha256 "c3d1d80669382688d49b2fc6d06b1f54e30fac0371d020ca07dbdf52472b69d0"
+  end
+
+  url "https://download.springsource.com/release/STS#{version.major}/#{version.csv.first}.RELEASE/dist/e#{version.csv.second.major_minor}/spring-tool-suite-#{version.major}-#{version.csv.first}.RELEASE-e#{version.csv.second}-macosx.cocoa.#{arch}.dmg",
       verified: "download.springsource.com/release/"
   name "Spring Tool Suite"
   desc "Next generation tooling for Spring Boot"
@@ -12,7 +19,7 @@ cask "springtoolsuite" do
     url :homepage
     strategy :page_match do |page|
       match = page.match(
-        %r{href=.*?/spring-tool-suite-\d+-(\d+(?:\.\d+)+)\.RELEASE-e(\d+(?:\.\d+)+)-macosx\.cocoa\.x86_64\.dmg}i,
+        %r{href=.*?/spring-tool-suite-\d+-(\d+(?:\.\d+)+)\.RELEASE-e(\d+(?:\.\d+)+)-macosx\.cocoa\.#{arch}\.dmg}i,
       )
       next if match.blank?
 
