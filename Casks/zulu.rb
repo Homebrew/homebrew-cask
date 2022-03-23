84468c537f3eee95eea72dead8ec20a392881155
Henry Chu <henrytech@outlook.com>
2022-03-23 15:31:56 +0800

    Update zulu from 17.0.2 to 18.0.0 (#120851)

--- Casks/zulu.rb
+++ Casks/zulu.rb
@@ -2,12 +2,12 @@ cask "zulu" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
   choice = Hardware::CPU.intel? ? "x86" : "arm"
 
-  version "17.0.2,17.32.13-ca"
+  version "18.0.0,18.28.13-ca"
 
   if Hardware::CPU.intel?
-    sha256 "73d9fd535a690c1fdcc9099edbb7f9e9b8897e30573c0be95360e6896804c203"
+    sha256 "a349702f247729466471acaf5b33eda291ad615233ad5224a7130d7eb4a6c497"
   else
-    sha256 "74d8e059865fdb8cfbc202bbd93a05c5a67a2d12947b09e30d637dd9b7e4011f"
+    sha256 "1b690d087e0b0f0b61cc7ac0f8b9f7d0fd6269f0132067330a106f205f288eed"
   end
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}.dmg",
