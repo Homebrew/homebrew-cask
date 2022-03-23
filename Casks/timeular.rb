ea144546b5edec25e41f75acf9ecc4d5d0ab659c
Steve Huff <shuff@vecna.org>
2022-03-23 03:29:04 -0400

    Update timeular from 4.2.0 to 4.6.0 (#120829)

--- Casks/timeular.rb
+++ Casks/timeular.rb
@@ -1,6 +1,6 @@
 cask "timeular" do
-  version "4.2.0"
-  sha256 "b8c5281f139f662d82bdc99cdb9b580a0d0c6b5ffd32eeb10ca19179f1cce786"
+  version "4.6.0"
+  sha256 "4dfefa8351b66810ea472ae145f05bd3dda4e77d81a6c0d663a2d408c9a0d5a6"
 
   url "https://timeular-desktop-packages.s3.amazonaws.com/mac/production/Timeular-#{version}.dmg",
       verified: "timeular-desktop-packages.s3.amazonaws.com/"
