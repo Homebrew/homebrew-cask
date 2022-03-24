6a93b7a4b8e342aab8eb3c4f4e7a224b15ae555c
YICAI YANG <macing_wang@hapigo.com>
2022-03-24 21:21:23 +0800

    Fixed verified download url syntax error
    HapiGo is a great productivity tool with many features on macOS for developers.

--- Casks/hapigo.rb
+++ Casks/hapigo.rb
@@ -2,7 +2,7 @@ cask "hapigo" do
   version "2.5.1"
   sha256 "6393038f16c30e00ac6f033787a261510b56f76e0bb4d32ae38fa08cd837047f"
 
-  url "https://github.com/HapiGo/HapiGo/releases/download/#{version}/HapiGo.dmg"
+  url "https://github.com/HapiGo/HapiGo/releases/download/#{version}/HapiGo.dmg",
       verified: "github.com/HapiGo/HapiGo/"
   name "HapiGo"
   desc "脑力工作者必备工具"
