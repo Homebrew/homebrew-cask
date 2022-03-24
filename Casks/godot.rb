f3c0d323e622f1c238bffc2e8ec70d683bb7cf80
Scott Foster <scott@imfoster.com>
2022-03-24 04:06:50 -0500

    godot 3.4.4 bump (#120888)

--- Casks/godot.rb
+++ Casks/godot.rb
@@ -1,6 +1,6 @@
 cask "godot" do
-  version "3.4.3"
-  sha256 "4b444137f84b7d7368ef845300e180e6919e01aba49133c17bd0ce722af73505"
+  version "3.4.4"
+  sha256 "b318654b9adf858f2525595b04f9e52cb686e86700b7be2f913ff2557d2ae3de"
 
   url "https://downloads.tuxfamily.org/godotengine/#{version}/Godot_v#{version}-stable_osx.universal.zip",
       verified: "downloads.tuxfamily.org/godotengine/"
