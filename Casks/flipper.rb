e25db6a07b8b1f8e1b3b805d36e3d92a30f25aa4
Carlos Moreno <clmoreno@users.noreply.github.com>
2022-03-24 05:31:08 -0600

    Update flipper from 0.139.0 to 0.140.0 (#120885)
    Co-authored-by: Carlos Moreno <cmoreno@protelecom.com.gt>

--- Casks/flipper.rb
+++ Casks/flipper.rb
@@ -1,6 +1,6 @@
 cask "flipper" do
-  version "0.139.0"
-  sha256 "9a343795fe99ba6282df8670334e97b1df336ca1b8f97c170c984f95b45dc2f0"
+  version "0.140.0"
+  sha256 "4db6ea69f400db1f360cbbc3dddab2547a20d8b78d414181d82827a698efc544"
 
   url "https://github.com/facebook/flipper/releases/download/v#{version}/Flipper-mac.dmg",
       verified: "github.com/facebook/flipper/"
