3de4ec3b0914a8046b58e17f9427e3574a86de5f
eloyaldea <eloy.aldea@gmail.com>
2022-03-24 09:45:17 +0100

    Update futurerestore-gui from 1.97.1 to 1.98.00

--- Casks/futurerestore-gui.rb
+++ Casks/futurerestore-gui.rb
@@ -1,6 +1,6 @@
 cask "futurerestore-gui" do
-  version "1.97.1"
-  sha256 "da8621c2aebbe80437cbedcd1c809ad8d322e24e5018d1af99b3704169cd28cb"
+  version "1.98.00"
+  sha256 "268e23082f9212fda3c083205c3a4ed0c19b491cf010fcfa803c3ecd29ae87bd"
 
   url "https://github.com/CoocooFroggy/FutureRestore-GUI/releases/download/v#{version}/FutureRestore-GUI-Mac-#{version}.dmg"
   name "FutureRestore GUI"
