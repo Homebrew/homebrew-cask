de9eb2a85c6d9724b0883ccc6bf14f3797371940
Essam Almansouri <eralmansouri@gmail.com>
2022-03-24 20:23:28 -0700

    Waterfox: Add ARM support

--- Casks/waterfox.rb
+++ Casks/waterfox.rb
@@ -1,15 +1,21 @@
 cask "waterfox" do
+  suffix = Hardware::CPU.intel? ? "Setup" : "ARM.Setup"
+  version "4.0.8"
+
+  url "https://github.com/WaterfoxCo/Waterfox/releases/download/G#{version}/Waterfox.G#{version}.#{suffix}.dmg", verified: "github.com/WaterfoxCo/Waterfox/"
   name "Waterfox"
   desc "Web browser"
-  homepage "https://www.waterfox.net/"
-  version "4.0.8"
-  sha256 "fe49d8da775d0d1ae7914cd51cf59fa9032cf02d398ac8891f2b722f4c2799b0"
+  if Hardware::CPU.intel?
+    sha256 "fe49d8da775d0d1ae7914cd51cf59fa9032cf02d398ac8891f2b722f4c2799b0"
+  else
+    sha256 "ee65166b3da3ff442e2ba0a9953844e5d4db386e0780ce41f3a39b842a191ce2"
+  end
 
-  url "https://github.com/WaterfoxCo/Waterfox/releases/download/G#{version}/Waterfox.G#{version}.Setup.dmg"
+  homepage "https://www.waterfox.net/"
 
   livecheck do
     url "https://www.waterfox.net/download/"
-    regex(%r{href=.*?/Waterfox\.G(\d+(?:\.\d+)+)\.Setup\.dmg}i)
+    regex(%r{href=.*?/Waterfox\.G(\d+(?:\.\d+)+)\.#{suffix}\.dmg}i)
   end
 
   depends_on macos: ">= :yosemite"
