0f94f90e25e48c9fca116171e541842c24b570b5
Essam Almansouri <eralmansouri@gmail.com>
2022-03-24 20:10:59 -0700

    Update Waterfox to V4.0.8

--- Casks/waterfox.rb
+++ Casks/waterfox.rb
@@ -1,15 +1,15 @@
 cask "waterfox" do
-  version "3.2.6"
-  sha256 "1aa0442868c503a2b214c023364d6b86e7a3cb6eda94c80d73e19d627996ee3d"
-
-  url "https://cdn.waterfox.net/releases/osx64/installer/Waterfox%20G#{version}%20Setup.dmg"
   name "Waterfox"
   desc "Web browser"
   homepage "https://www.waterfox.net/"
+  version "4.0.8"
+  sha256 "fe49d8da775d0d1ae7914cd51cf59fa9032cf02d398ac8891f2b722f4c2799b0"
+
+  url "https://github.com/WaterfoxCo/Waterfox/releases/download/G#{version}/Waterfox.G#{version}.Setup.dmg"
 
   livecheck do
     url "https://www.waterfox.net/download/"
-    regex(%r{href=.*?/Waterfox%20G(\d+(?:\.\d+)+)%20Setup\.dmg}i)
+    regex(%r{href=.*?/Waterfox\.G(\d+(?:\.\d+)+)\.Setup\.dmg}i)
   end
 
   depends_on macos: ">= :yosemite"
