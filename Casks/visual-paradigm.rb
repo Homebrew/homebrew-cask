a226f57a3701b73f1798912281fc12dbf5b8ecc0
neutric <5984479+neutric@users.noreply.github.com>
2022-03-25 17:22:54 +0100

    Update visual-paradigm from 16.3,20220215 to 16.3,20220323 (#120972)

--- Casks/visual-paradigm.rb
+++ Casks/visual-paradigm.rb
@@ -1,6 +1,6 @@
 cask "visual-paradigm" do
-  version "16.3,20220215"
-  sha256 "1c6cb9a5970d11ea03ee7eabeeaccb862e570300607d00995d5fab8cff0152ab"
+  version "16.3,20220323"
+  sha256 "a7629aba4b2ea8ba2942774b38522b6ef5f61e23fb0e8d83c954ac82dc869651"
 
   url "https://www.visual-paradigm.com/downloads/vp#{version.csv.first}/#{version.csv.second}/Visual_Paradigm_#{version.csv.first.dots_to_underscores}_#{version.csv.second}_OSX_WithJRE.dmg"
   name "Visual Paradigm"
