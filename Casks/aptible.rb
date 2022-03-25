5c5577de3f0cf924f9296242e0b6a708e8c6536d
Alex Kubacki <usernotfound@gmail.com>
2022-03-25 11:06:32 -0600

    Update aptible from 0.19.3,20220314154920,340 to 0.19.3,20220324183731,340 (#120930)

--- Casks/aptible.rb
+++ Casks/aptible.rb
@@ -1,6 +1,6 @@
 cask "aptible" do
-  version "0.19.3,20220314154920,340"
-  sha256 "e50d899e25172facbac1489a6e24a4d17431631485ca15be8b026be1c99480e8"
+  version "0.19.3,20220324183731,340"
+  sha256 "4ecbe4b716ec6f725338164159da2d8acbe964e603085f1f6919a3bb0a74e76c"
 
   url "https://omnibus-aptible-toolbelt.s3.amazonaws.com/aptible/omnibus-aptible-toolbelt/master/#{version.csv.third}/pkg/aptible-toolbelt-#{version.csv.first}%2B#{version.csv.second}-mac-os-x.10.15.7-1.pkg",
       verified: "omnibus-aptible-toolbelt.s3.amazonaws.com/"
