6627247600cb405bdd72076bed7e0e1732f2d04e
Pierre Pirault <pierre.pirault@outlook.com>
2022-03-25 12:22:18 -0400

    Update tidelift from 1.7.0 to 1.9.0 (#120988)

--- Casks/tidelift.rb
+++ Casks/tidelift.rb
@@ -1,6 +1,6 @@
 cask "tidelift" do
-  version "1.8.0"
-  sha256 "adfcca6189f6a9da7e5a8eb75d62c2c2eaa76e46616bbb1e240ac5ccd3cf16a9"
+  version "1.9.0"
+  sha256 "d6fe43b092a5caafe2d15f7c97cce2c25753d7a7a410b450f11db4e053b3152f"
 
   url "https://download.tidelift.com/cli/#{version}/darwin_homebrew/tidelift"
   name "Tidelift CLI"
