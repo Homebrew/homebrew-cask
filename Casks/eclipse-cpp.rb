cba3c1abdd2eeff3a453bc4401454184aaa5b31d
Hannes Braun <hannesbraun@mail.de>
2022-03-27 18:38:21 +0200

    Update eclipse-cpp from 4.22.0 to 4.23.0 (#121045)

--- Casks/eclipse-cpp.rb
+++ Casks/eclipse-cpp.rb
@@ -1,12 +1,12 @@
 cask "eclipse-cpp" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
-  version "4.22.0,2021-12"
+  version "4.23.0,2022-03"
 
   if Hardware::CPU.intel?
-    sha256 "7d2af4314a610793e70a0e54260fadb3aa0bd2307f8fe368e866fb22da9226cf"
+    sha256 "b8ec6894a3989bfda5861c5cd9d772d7fb6d37354230d6a1f730c77b76e701f2"
   else
-    sha256 "867ac694f6bf8f315a846c9d95fdf2d42ac91782e89bd079fd6d2415b8b9147c"
+    sha256 "12f1c47951d0263f22a2dd10734767bb41a46b39d4e22e6402aa1c8b8e2cadea"
   end
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-cpp-#{version.csv.second}-R-macosx-cocoa-#{arch}.dmg&r=1"
