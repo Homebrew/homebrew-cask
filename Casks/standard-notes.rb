13588f1b2576172fe09b64d8062c9edbed6ed433
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2022-03-27 20:48:49 +0800

    Update standard-notes from 3.13.0 to 3.14.0 (#121015)

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -1,12 +1,12 @@
 cask "standard-notes" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "3.13.0"
+  version "3.14.0"
 
   if Hardware::CPU.intel?
-    sha256 "c171773d0cf91a5c11706e3161ac002a62aa7f39fad70187295e6794d696381a"
+    sha256 "c0a8e51d4dc55578ae601fba7bafe2851fd0c20fe7054ed8d132ed8c41c941a5"
   else
-    sha256 "4d5fb36c6868b3e149bdf3241448badfe00b245d81e3d2c57bbcba80d411ba3c"
+    sha256 "999e6a347b320506ee0392e74d7370e26977ff1b0a386dc7921dc74d115c6484"
   end
 
   url "https://github.com/standardnotes/desktop/releases/download/v#{version}/Standard-Notes-#{version}-mac-#{arch}.zip",
