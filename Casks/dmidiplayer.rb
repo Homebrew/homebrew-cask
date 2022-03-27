5a661f236b494f71767262d9d341e16b7076bd2c
Aaron Mansheim <minopret@gmail.com>
2022-03-27 01:46:36 -0400

    Update dmidiplayer from 1.5.2 to 1.5.1

--- Casks/dmidiplayer.rb
+++ Casks/dmidiplayer.rb
@@ -1,12 +1,17 @@
 cask "dmidiplayer" do
-  version "1.5.2"
-  sha256 "1f96beab927b75a0065f09720269db0bc61401a7674c359be9f06bdf9a76826a"
+  version "1.5.1"
+  sha256 "48a22c73143dc7fb57ea9de19e9ecb03d32a88c33fbf6e44f071b3a129f113a5"
 
   url "https://downloads.sourceforge.net/dmidiplayer/#{version}/dmidiplayer-#{version}-mac-x64.dmg",
       verified: "downloads.sourceforge.net/dmidiplayer/"
   name "dmidiplayer"
   desc "Multiplatform MIDI File Player"
   homepage "https://dmidiplayer.sourceforge.io/"
+
+  livecheck do
+    url "https://sourceforge.net/projects/dmidiplayer/rss?path=/"
+    regex(%r{url=.*?/dmidiplayer[._-]v?(\d+(?:\.\d+)+)-mac-x64\.dmg}i)
+  end
 
   depends_on formula: "fluid-synth"
   depends_on macos: ">= :sierra"
