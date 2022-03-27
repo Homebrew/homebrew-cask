2a38f7bbd4dddecf3d66cbc7fa26233df00ec2ae
Clément L <porkepix@gmail.com>
2022-03-27 14:10:17 +0200

    Update archipelago from 3.16.1 to 4.0.0 (#120996)

--- Casks/archipelago.rb
+++ Casks/archipelago.rb
@@ -1,6 +1,6 @@
 cask "archipelago" do
-  version "3.16.1"
-  sha256 "b0b761d1293bc6876da2c3e5630e04957d299a4167f7296c4e1e4fde953d362d"
+  version "4.0.0"
+  sha256 "b0e72e180663d913de71a4c24f795ddf632f0f084405bd2195c25b84f5dac3dd"
 
   url "https://github.com/npezza93/archipelago/releases/download/v#{version}/Archipelago-#{version}.dmg"
   name "Archipelago"
