b9a8faeb93b67d22571df8a77c99775a5d5f0277
Joe Wicentowski <joewiz@gmail.com>
2022-03-27 07:17:03 -0400

    Add Mellel 5.app v5.1.0 (51012) (#120775)
    * Add Mellel 5.app v5.1.0 (51012)
    
    * Update livecheck
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>

---
+++ Casks/mellel.rb
@@ -0,0 +1,36 @@
+cask "mellel" do
+  version "5.1.0,51012"
+  sha256 "89fb9e8240ad407fbf3ca8f2949356ec58af7acf1d5f23884f534399d3dfeda4"
+
+  url "https://d1riogbqt3a9uw.cloudfront.net/mellel_#{version.csv.second}.dmg",
+      verified: "d1riogbqt3a9uw.cloudfront.net"
+  name "Mellel"
+  desc "Advanced word processor built for long and complex documents"
+  homepage "https://www.mellel.com/"
+
+  livecheck do
+    url "http://www.mellelupdate.com/mellelupdate/latest_update.xml"
+    regex(%r{<full-version>v?(\d+(?:\.\d{1,2})+)(?:\.(\d{3,}))?</full-version>}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map do |match|
+        match[1].present? ? "#{match[0]},#{match[1]}" : match[0]
+      end
+    end
+  end
+
+  auto_updates true
+
+  app "Mellel #{version.major}.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.redlex.mellel",
+    "~/Library/Application Support/Mellel #{version.major}",
+    "~/Library/Application Support/com.redlex.mellel#{version.major}",
+    "~/Library/Caches/com.redlex.mellel#{version.major}",
+    "~/Library/HTTPStorages/com.redlex.mellel#{version.major}",
+    "~/Library/HTTPStorages/com.redlex.mellel#{version.major}.binarycookies",
+    "~/Library/Preferences/com.redlex.mellel#{version.major}.plist",
+    "~/Library/Saved Application State/com.redlex.MellelUpdater.savedState",
+    "~/Library/Saved Application State/com.redlex.mellel#{version.major}.savedState",
+  ]
+end
