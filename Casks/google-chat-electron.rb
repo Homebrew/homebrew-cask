45de2a4c344468fd81ddc64822da9bc64a61ec55
Leopoldo Pla <leopoldoplasempere@gmail.com>
2022-03-27 18:44:46 +0200

    Update google-chat-electron from 2.14.3 to 2.14.4 (#121038)

--- Casks/google-chat-electron.rb
+++ Casks/google-chat-electron.rb
@@ -1,6 +1,6 @@
 cask "google-chat-electron" do
-  version "2.14.3"
-  sha256 "8f6dc8efea85cf3a32b9e90cab3253e7a920b5575e8a52db48035399e07f437e"
+  version "2.14.4"
+  sha256 "4d974ff1fd374ebb8a9befdcb019c073cbc29fef0b76b3d2737d2a68a42a2d15"
 
   url "https://github.com/ankurk91/google-chat-electron/releases/download/#{version}/google-chat-electron-#{version}-darwin-x64.zip"
   name "google-chat-electron"
