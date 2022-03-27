38834991d981b7919763cde0d4110c01289a29d6
Sam Ford <1584702+samford@users.noreply.github.com>
2022-03-27 12:37:56 -0400

    Update isyncer from 3.8.0 to 3.9.0 (#121046)

--- Casks/isyncer.rb
+++ Casks/isyncer.rb
@@ -1,6 +1,6 @@
 cask "isyncer" do
-  version "3.8.0"
-  sha256 "a3a3df7662cbf21f22b7ed78603b3b900b249635555b5b94ad11bf561d417348"
+  version "3.9.0"
+  sha256 "552e558f99e4ccdec9e2ec1adfda650851676ac0769fd9ceafcd7803b7a7de37"
 
   url "https://www.isyncer.de/system/iSyncerV#{version}mac-installer.tgz"
   name "iSyncer"
@@ -9,7 +9,7 @@ cask "isyncer" do
 
   livecheck do
     url "https://www.isyncer.de/en/releases/"
-    regex(%r{href=.*?/iSyncerv?(\d+(?:\.\d+)+)mac[._-]installer\.tgz}i)
+    regex(/href=.*?iSyncer[._-]?v?(\d+(?:\.\d+)+)[._-]?mac[._-]installer\.t/i)
   end
 
   pkg "iSyncer-installer-#{version}.pkg"
