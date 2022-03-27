a5340988508f66a1b58616573e5ac7c3533e09ab
Francisco Rodera <trek1s@gmail.com>
2022-03-27 20:34:30 +1100

    Update eloston-chromium from 99.0.4844.74-1.1 to 99.0.4844.84-1.1

--- Casks/eloston-chromium.rb
+++ Casks/eloston-chromium.rb
@@ -2,8 +2,8 @@ cask "eloston-chromium" do
   arch = Hardware::CPU.intel? ? "x86-64" : "arm64"
 
   if Hardware::CPU.intel?
-    version "99.0.4844.74-1.1,1648005568"
-    sha256 "2c3f98e86f3769339964d8f0e39040a27e5e032eec093dbc01178ac98b00c2d9"
+    version "99.0.4844.84-1.1,1648358143"
+    sha256 "404dd6bded3a5eff98bb2587296fef743871eaea52e9d7d6aab60e4ed8c8a309"
   else
     version "99.0.4844.74-1.1,1647530108"
     sha256 "a9a18f53cdf8da9864210a403266e6ea5f30226393ed83568b3f3af6e5961ca0"
