b534ed11b25cbb3a1701f9d7b1114cf86717a069
Aditya Mangalampalli <aditya.mangalampalli@gmail.com>
2022-03-27 15:03:31 -0700

    Update ferdi from 5.8.0 to 5.8.1

--- Casks/ferdi.rb
+++ Casks/ferdi.rb
@@ -1,12 +1,12 @@
 cask "ferdi" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  version "5.8.0"
+  version "5.8.1"
 
   if Hardware::CPU.intel?
-    sha256 "c9d31d0a4a58e3448ae8af4dd92b76878c85b384414297ccd553e57c444a7f9f"
+    sha256 "fa802e2627dc2c9c5ccface1f9c830c5a2e0e9ae7a7339716651010e39928a50"
   else
-    sha256 "7fcddbac8868c45bf457118cb2d25e2ceb0aeea2ab0662ebfcdb6c07d9bb2241"
+    sha256 "ec7ccceba08f1c581290d6ce4f5fa5478bed2c713c592d0298856f7b2719f35d"
   end
 
   url "https://github.com/getferdi/ferdi/releases/download/v#{version}/Ferdi-#{version}#{arch}.dmg",
