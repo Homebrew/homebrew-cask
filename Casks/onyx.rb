476a6fa47cf489554c5686fce34e9c4567e3ad10
owine <owine@users.noreply.github.com>
2022-03-27 21:26:32 -0500

    Update onyx from 4.1.6 to 4.1.7

--- Casks/onyx.rb
+++ Casks/onyx.rb
@@ -21,8 +21,8 @@ cask "onyx" do
     version "4.0.2"
     sha256 "18659c76972690bb58f1c1b850ae2c6c9991fdeb9c22790daa1b18c9fa74cb8e"
   else
-    version "4.1.6"
-    sha256 "e1fd1d0793761164180c0b96edda818de0e7a786dd3fd3b45e41bb4a251ce717"
+    version "4.1.7"
+    sha256 "a7f9d2438ae3346d5d9651cd066c7b20f0d00ae9dc692ecde61b00b5859ba68c"
   end
 
   url "https://www.titanium-software.fr/download/#{MacOS.version.to_s.delete(".")}/OnyX.dmg"
