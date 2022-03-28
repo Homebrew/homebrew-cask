971fc9b3ebd7d7e53b98c3a197f1bd09c47b6220
Matt Coster <opensource@mtcoster.net>
2022-03-28 09:09:42 +0100

    Update feed-the-beast from 202203051730 to 202203251547

--- Casks/feed-the-beast.rb
+++ Casks/feed-the-beast.rb
@@ -1,6 +1,6 @@
 cask "feed-the-beast" do
-  version "202203051730,9df8355f62"
-  sha256 "dd9e988483bbee2b29789720fb3422b392c57761553d3439a2834f1889389be3"
+  version "202203251547,a5e4f8b1e5"
+  sha256 "2c2666097d627284d8ca8fef321916fcfd70618ebae23f887f5024c017b310bd"
 
   url "https://apps.modpacks.ch/FTBApp/release/#{version.csv.first}-#{version.csv.second}-release/FTBA_macos_#{version.csv.first}-#{version.csv.second}-release.dmg",
       verified: "apps.modpacks.ch/FTBApp/"
