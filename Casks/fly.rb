5a357f230e86fde0cea106d9bd411ef1e2a9a475
Porkepix <porkepix@gmail.com>
2022-03-29 01:01:18 +0200

    Update fly from 7.7.0 to 7.7.1

--- Casks/fly.rb
+++ Casks/fly.rb
@@ -1,6 +1,6 @@
 cask "fly" do
-  version "7.7.0"
-  sha256 "19d71f6c57fbaac9abcddfbf17d99047bf6b9bee7db2da60ef1fc6e4ecf754db"
+  version "7.7.1"
+  sha256 "356226256041e12a58f3494ed6f9266218d6653aecb8cadef998b88c8213810b"
 
   url "https://github.com/concourse/concourse/releases/download/v#{version}/fly-#{version}-darwin-amd64.tgz"
   name "fly"
