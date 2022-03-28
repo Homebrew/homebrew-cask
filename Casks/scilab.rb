ab53ce61e956650311f0cd8d5c6f0db29b3d8bc7
André David <andre.david@cern.ch>
2022-03-28 19:14:31 +0200

    scilab 6.1.1 arm64

--- Casks/scilab.rb
+++ Casks/scilab.rb
@@ -1,9 +1,17 @@
 cask "scilab" do
-  version "6.1.1"
-  sha256 "922e879979fe4e1fde83be4b3df02070c0930a56a75cdeb9b5ef46ae29f0ef57"
+  arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
+  prefix = Hardware::CPU.intel? ? "" : "accelerate-" # there is also an "openblas-" version
 
-  url "https://www.utc.fr/~mottelet/scilab/download/#{version}/scilab-#{version}-x86_64.dmg",
-      verified: "utc.fr/~mottelet/scilab/"
+  version "6.1.1"
+
+  if Hardware::CPU.intel?
+    sha256 "922e879979fe4e1fde83be4b3df02070c0930a56a75cdeb9b5ef46ae29f0ef57"
+  else
+    sha256 "2f87710fc47c6d8e6777ee280ece589342e536c17290b9c033ea0dfcef3b4912"
+  end
+
+  url "https://www.utc.fr/~mottelet/scilab/download/#{version}/scilab-#{version}-#{prefix}#{arch}.dmg",
+      verified: "utc.fr/~mottelet/scilab/"  
   name "Scilab"
   desc "Software for numerical computation"
   homepage "https://www.scilab.org/"
