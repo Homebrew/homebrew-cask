06d7969051fad3e9c41ef35c102391e8a6dd5a1f
Guillaume Rischard <git@stereo.lu>
2022-03-28 01:56:45 -0400

    Update lunar from 5.5.2 to 5.5.3 (#121068)

--- Casks/lunar.rb
+++ Casks/lunar.rb
@@ -1,6 +1,6 @@
 cask "lunar" do
-  version "5.5.2"
-  sha256 "44b9b809c9e39d2815d8e1500e9464ee5e235f8d2e582e477c44948956a2214a"
+  version "5.5.3"
+  sha256 "46e33e7a213d270f51aeefdcfa3b7d34803cf81ef012d221c3a79f9d098719e5"
 
   url "https://static.lunar.fyi/releases/Lunar-#{version}.dmg"
   name "Lunar"
