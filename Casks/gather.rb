5524002983324fff6d56de70b2c88a00675403b8
Hideki Igarashi <hideki.develop@gmail.com>
2022-03-28 14:57:39 +0900

    Update gather from 0.2.0 to 0.2.2 (#121058)

--- Casks/gather.rb
+++ Casks/gather.rb
@@ -1,6 +1,6 @@
 cask "gather" do
-  version "0.2.0"
-  sha256 "32cbb0f4806a1f6ed8d6aebd87970e14fde332143107ca8a3eab5a2b60e4e5ed"
+  version "0.2.2"
+  sha256 "5ba41eacaf4f27bba26e23473dd3a2bd400a326fe026c25fde05e6a5bc36ce77"
 
   url "https://github.com/gathertown/gather-town-desktop-releases/releases/download/v#{version}/Gather-#{version}-mac.zip",
       verified: "github.com/gathertown/gather-town-desktop-releases"
