e66aa088e4caee99a0752b4d608a7d129fdebf3d
Bevan Kay <email@bevankay.me>
2022-03-28 17:27:58 +1100

    Update mixed-in-key-live.rb

--- Casks/mixed-in-key-live.rb
+++ Casks/mixed-in-key-live.rb
@@ -3,7 +3,7 @@ cask "mixed-in-key-live" do
   sha256 :no_check
 
   url "https://builds.mixedinkey.com/download/55/release/latest?key=public"
-  name "Mixed in Key Live"
+  name "Mixed In Key Live"
   desc "Get the Key and BPM of any audio, instantly"
   homepage "https://mixedinkey.com/live"
 
@@ -14,17 +14,17 @@ cask "mixed-in-key-live" do
 
   auto_updates true
 
-  pkg "#{name.first}.pkg"
+  pkg "Mixed In Key Live.pkg"
 
   bundle_id = "com.mixedinkey.MIK-Live"
   uninstall quit: bundle_id, pkgutil: "com.mixedinkey.Mixed_In_Key_Live.pkg"
 
-  zap trash: %W[
-    ~/Library/Application Support/#{bundle_id}
-    ~/Library/Application Support/Mixedinkey
-    ~/Library/Caches/#{bundle_id}
-    ~/Library/HTTPStorages/#{bundle_id}
-    ~/Library/Preferences/#{bundle_id}.plist
-    ~/Library/Saved Application State/#{bundle_id}.savedState
+  zap trash: [
+    "~/Library/Application Support/#{bundle_id}",
+    "~/Library/Application Support/Mixedinkey",
+    "~/Library/Caches/#{bundle_id}",
+    "~/Library/HTTPStorages/#{bundle_id}",
+    "~/Library/Preferences/#{bundle_id}.plist",
+    "~/Library/Saved Application State/#{bundle_id}.savedState",
   ]
 end
