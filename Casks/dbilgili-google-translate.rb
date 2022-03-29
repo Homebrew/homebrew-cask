dd91aba7484f06fe4e11d1010f20dbe604a0e2e6
Paul Su <sujianbo@gmail.com>
2022-03-29 11:20:05 +0800

    Add Translator.app (dbilgili/GoogleTranslate) v1.0.2

---
+++ Casks/dbilgili-google-translate.rb
@@ -0,0 +1,11 @@
+cask "dbilgili-google-translate" do
+  version "1.0.2"
+  sha256 "f600da9489fe562348b0cd11c41934e59888e5ba3bf63c6edcf15753b979b2fc"
+
+  url "https://github.com/dbilgili/GoogleTranslate/releases/download/#{version}/Translator-#{version}.dmg.zip"
+  name "dbilgili-google-translate"
+  desc "A basic macOS menu bar app to access Google Translate quickly"
+  homepage "https://github.com/dbilgili/GoogleTranslate"
+
+  app "Translator.app"
+end
