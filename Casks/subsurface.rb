ab7f9a3f479091a3602937e418ae5a6855255caf
Piotr Kedra <piotr.kedra@schibsted.com>
2022-03-29 20:35:23 +0200

    Update subsurface from 5.0.6 to 5.0.8

--- Casks/subsurface.rb
+++ Casks/subsurface.rb
@@ -1,14 +1,14 @@
 cask "subsurface" do
-  version "5.0.6"
+  version "5.0.8"
 
   if MacOS.version <= :mojave
     sha256 "b718acc583a986bc36df77b7b076dac0e19f193f6579c55ff802d448cea7c2b3"
     url "https://subsurface-divelog.org/downloads/Subsurface-#{version}-10.13+14.dmg"
   elsif MacOS.version <= :big_sur
-    sha256 "6fd55c2b58b855fbee001d32ca114271fc5c8a6c0399680860b80df8e40939c6"
+    sha256 "8bdce1b52ebf3128c43c09b59f02ff2de7130761448e777d06fb539236f4b862"
     url "https://subsurface-divelog.org/downloads/Subsurface-#{version}-10.15+11.0.dmg"
   else
-    sha256 "6fd55c2b58b855fbee001d32ca114271fc5c8a6c0399680860b80df8e40939c6"
+    sha256 "8bdce1b52ebf3128c43c09b59f02ff2de7130761448e777d06fb539236f4b862"
     url "https://subsurface-divelog.org/downloads/Subsurface-#{version}.dmg"
   end
 
