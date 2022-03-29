f7b34ee8f732760ba6f02631191db030f52e75ec
xiaolanglanglang <2145773+xiaolanglanglang@users.noreply.github.com>
2022-03-29 17:06:39 +0800

    Update tabby from 1.0.173 to 1.0.174 (#121123)

--- Casks/tabby.rb
+++ Casks/tabby.rb
@@ -1,12 +1,12 @@
 cask "tabby" do
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 
-  version "1.0.173"
+  version "1.0.174"
 
   if Hardware::CPU.intel?
-    sha256 "6c3be59ce05e49de03ebe298e2978e20ddfe2b9d99912a5320067c6fcce89ffa"
+    sha256 "bcb01d55706df4ca2cc3e0674cec93cade02774cebec369d8224cd5058a16a5e"
   else
-    sha256 "817ef7bc292d2adb61d942bb5e0c22f1b21952864d5219ea352434a946e90824"
+    sha256 "8627d36b8436b872a3c529effc8e54750c7962c36763f8309cd2f680415a9b6d"
   end
 
   url "https://github.com/Eugeny/tabby/releases/download/v#{version}/tabby-#{version}-macos-#{arch}.zip",
