840bc41b926985d6d21cd878141cec598dc6ac9b
owine <owine@users.noreply.github.com>
2022-03-29 13:51:04 -0500

    Update rocket-chat from 3.8.0 to 3.8.1

--- Casks/rocket-chat.rb
+++ Casks/rocket-chat.rb
@@ -1,8 +1,8 @@
 cask "rocket-chat" do
-  version "3.8.0"
-  sha256 "e97cfedd3fec4b4385542bea9608b5d94549a28176a9b0451c84135c45c2e5f5"
+  version "3.8.1"
+  sha256 "fba96ac3db50df8bb1fa67a28cd1fc8956e15ef842831afbbdc9e3211366bc08"
 
-  url "https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/#{version}/rocketchat-#{version}.dmg",
+  url "https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/#{version}/rocketchat-#{version}-mac.dmg",
       verified: "github.com/RocketChat/Rocket.Chat.Electron/"
   name "Rocket.Chat"
   desc "Official desktop client for Rocket.Chat"
