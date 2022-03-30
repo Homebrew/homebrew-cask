1329c84b6584c8df67ae118f4566e39457fe550a
rzbz <46098567+rzbz@users.noreply.github.com>
2022-03-30 10:18:13 +0800

    Update bob from 0.8.0 to 0.8.1 (#121138)

--- Casks/bob.rb
+++ Casks/bob.rb
@@ -1,6 +1,6 @@
 cask "bob" do
-  version "0.8.0"
-  sha256 "042626d373942e6ec2c19dd859e9db82a5c8cc1c9223edf3cba57548ae28a241"
+  version "0.8.1"
+  sha256 "853990df3918df866ab593e24d5545c38d5743f3e251f2745c637938ee752885"
 
   url "https://github.com/ripperhe/Bob/releases/download/v#{version}/Bob.zip"
   name "Bob"
