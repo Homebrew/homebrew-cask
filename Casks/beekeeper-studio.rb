108931d403236f3abf2e4e2517aee3cf1d8537a0
Aayush Kapoor <xeoneux@icloud.com>
2022-03-30 07:58:54 +0530

    beekeeper-studio apple silicon support (#121124)

--- Casks/beekeeper-studio.rb
+++ Casks/beekeeper-studio.rb
@@ -1,8 +1,15 @@
 cask "beekeeper-studio" do
   version "3.3.6"
-  sha256 "efe0974855a68db5da6e02f8b9a163e500e234a0dfaf2c0a026f76e66294ba52"
 
-  url "https://github.com/beekeeper-studio/beekeeper-studio/releases/download/v#{version}/Beekeeper-Studio-#{version}.dmg",
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
+
+  if Hardware::CPU.intel?
+    sha256 "efe0974855a68db5da6e02f8b9a163e500e234a0dfaf2c0a026f76e66294ba52"
+  else
+    sha256 "20e96c547366b6c4a2244edd475d9f3a1c420d4f32101c9968a50ce32254cc11"
+  end
+
+  url "https://github.com/beekeeper-studio/beekeeper-studio/releases/download/v#{version}/Beekeeper-Studio-#{version}#{arch}.dmg",
       verified: "github.com/beekeeper-studio/beekeeper-studio/"
   name "Beekeeper Studio"
   desc "Cross platform SQL editor and database management app"
