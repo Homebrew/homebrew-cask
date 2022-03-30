f98226f62135b2a20d36315acc0630d32c2bf864
goranpjevic <67331732+goranpjevic@users.noreply.github.com>
2022-03-30 09:35:19 +0200

    Update dyalog from 18.0.40684 to 18.2.45405 (#121144)

--- Casks/dyalog.rb
+++ Casks/dyalog.rb
@@ -1,6 +1,6 @@
 cask "dyalog" do
-  version "18.0.40684"
-  sha256 "226477ef353999203bd3c40d840480f2de23cac950a38bc150bf5c78a1b9b436"
+  version "18.2.45405"
+  sha256 "66273ab94d6ba7dbb4aa8a03b75aed3d3ad3754a81b943d7afb4b6b366a98747"
 
   url "https://www.dyalog.com/uploads/php/download.dyalog.com/download.php?file=#{version.major_minor}/mac_64_#{version}_unicode.pkg"
   name "Dyalog APL"
