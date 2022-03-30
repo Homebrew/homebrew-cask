76167feed2c6bbbc6fcfd421b93d5e1161a16847
Francis McKenzie <info@macca.tech>
2022-03-30 10:25:30 +0200

    Update postgrespreferencepane from 2.6 to 2.7 (#121156)
    * Update postgrespreferencepane from 2.6 to 2.7
    
    * Add desc stanza to postgrespreferencepane cask

--- Casks/postgrespreferencepane.rb
+++ Casks/postgrespreferencepane.rb
@@ -1,9 +1,10 @@
 cask "postgrespreferencepane" do
-  version "2.6"
-  sha256 "c04f7d3e2563454292a806c4d67d661e560c61c29fe45b196324e071e60130a9"
+  version "2.7"
+  sha256 "970510169d0e37fb3feca6fa700d26fa2421aabcd19fc17db58eb0dca89ebb14"
 
   url "https://github.com/MaccaTech/PostgresPrefs/releases/download/v#{version}/PostgresPrefs-v#{version}.dmg"
   name "PostgresPrefs"
+  desc "Preference Pane for controlling PostgreSQL database servers"
   homepage "https://github.com/MaccaTech/PostgresPrefs"
 
   prefpane "PostgreSQL.prefPane"
