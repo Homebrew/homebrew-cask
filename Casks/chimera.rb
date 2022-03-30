7ba5cd93118c28da395525d1a58dbb55e4328e47
Anselm Hahn <Anselm.Hahn@gmail.com>
2022-03-30 15:17:50 +0200

    Updating the downloading the link

--- Casks/chimera.rb
+++ Casks/chimera.rb
@@ -1,8 +1,7 @@
 cask "chimera" do
   version "1.16"
   sha256 "1ab2072085e2fbed6d3be354bb66a9344273ab1f8735bc24c4f43d524b1b137c"
-
-  url "https://www.cgl.ucsf.edu/chimera/cgi-bin/secure/chimera-get.py?ident=OHeQer2WTaxn%2F%2BtrtntD5OBguUNERNj83x903g7ihe4rqA%3D%3D&file=mac64%2Fchimera-#{version}-mac64.dmg&choice=Notified"
+  url "https://www.cgl.ucsf.edu/chimera/cgi-bin/secure/chimera-get.py?ident=OHeQer2WTaxn%2F%2BtrtntC7uBgvUNERNj83xx10gPggO4u&file=mac64%2Fchimera-#{version}-mac64.dmg&choice=Notified"
   name "Chimera"
   desc "Visualization and analysis of molecular structures"
   homepage "https://www.cgl.ucsf.edu/chimera/"
