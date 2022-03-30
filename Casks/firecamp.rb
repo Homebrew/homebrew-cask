84cf7cd490b9e4504b083a98b9710ce083fe393c
Nishchit <dnishchit@gmail.com>
2022-03-30 07:47:43 +0530

    Firecamp upgraded to v2.6.1 (#121135)

--- Casks/firecamp.rb
+++ Casks/firecamp.rb
@@ -1,6 +1,6 @@
 cask "firecamp" do
-  version "2.6.0"
-  sha256 "0a4b4807ae459f55d55a9a7012389128f0f3aec41c48c98e13d3034ec396dc35"
+  version "2.6.1"
+  sha256 "8b7df7244da0e577a7a5793822af6144548a65024e6b1f8166513ec2435ccb08"
 
   url "https://firecamp.ams3.digitaloceanspaces.com/versions/mac/Firecamp-#{version}.dmg",
       verified: "firecamp.ams3.digitaloceanspaces.com/"
