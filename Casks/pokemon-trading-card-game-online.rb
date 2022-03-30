ab116a7b15caefe6ebf06e610579c3e6dea62660
Robert Blair <12276457+robertleeblairjr@users.noreply.github.com>
2022-03-29 21:08:53 -0500

    Update pokemon-trading-card-game-online from 2.78.0.5176 to 2.88.0.5535 (#121159)

--- Casks/pokemon-trading-card-game-online.rb
+++ Casks/pokemon-trading-card-game-online.rb
@@ -1,5 +1,5 @@
 cask "pokemon-trading-card-game-online" do
-  version "2.78.0.5176"
+  version "2.88.0.5535"
   sha256 :no_check
 
   url "https://tcgo-installer.s3.amazonaws.com/PokemonInstaller_Mac.dmg",
