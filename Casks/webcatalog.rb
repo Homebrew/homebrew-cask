dcc637cce6a26e8c1c716ce86b074c98464bf1ef
Bevan Kay <email@bevankay.me>
2022-03-30 23:04:20 +1100

    Update webcatalog from 41.4.0 to 41.4.0 (#121183)

--- Casks/webcatalog.rb
+++ Casks/webcatalog.rb
@@ -1,21 +1,20 @@
 cask "webcatalog" do
-  version "41.2.1"
-  sha256 "1cd63f5417a1e5192af38f00d6dbe8476754619815d2221055bb195b7078360f"
+  version "41.4.0"
+  sha256 "753a3c989222ab706ac067e9533a27a06782eec52ef485f63d74be59850f8967"
 
-  url "https://github.com/webcatalog/webcatalog-app/releases/download/v#{version}/WebCatalog-classic-#{version}-universal.dmg",
-      verified: "github.com/webcatalog/webcatalog-app/"
+  url "https://cdn-2.webcatalog.io/webcatalog/WebCatalog-#{version}-universal.dmg"
   name "WebCatalog"
   desc "Tool to run web apps like desktop apps"
-  homepage "https://webcatalog.app/"
+  homepage "https://webcatalog.io/"
 
   livecheck do
-    url :url
-    strategy :github_latest
+    url "https://cdn-2.webcatalog.io/webcatalog/latest-mac.yml"
+    strategy :electron_builder
   end
 
   auto_updates true
 
-  app "WebCatalog Classic.app"
+  app "WebCatalog.app"
 
   zap trash: [
     "~/Library/Application Support/WebCatalog",
