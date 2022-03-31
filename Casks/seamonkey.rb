f18f607575552fb25cd2d3b53afd422404074032
Jakob Yanagibashi <git22@jakob.page>
2022-03-31 09:46:58 +0200

    Update seamonkey from 2.53.11 to 2.53.11.1 (#121196)

--- Casks/seamonkey.rb
+++ Casks/seamonkey.rb
@@ -1,28 +1,28 @@
 cask "seamonkey" do
-  version "2.53.11"
+  version "2.53.11.1"
 
   language "de" do
-    sha256 "0abe96a48b0ef764eec1d3185cbdcdddb6541618401afe22203884705ef35038"
+    sha256 "a41fab15f62c6b103b2eeeae89bd53e1edacf213bd64311eed9b65574044f50e"
     "de"
   end
   language "en-GB" do
-    sha256 "1055012271698385d2ace746b739cbc96fad25b9817a8063da8b8c27c3d33a30"
+    sha256 "ca9782ff7faa9bbe39f3e62df37aacf2c1f4d245863d0f5bd2eee8351f6c9add"
     "en-GB"
   end
   language "en-US", default: true do
-    sha256 "c5f3cadb834323b813fefca9110bf7ba78b507bbdc7dd06de5b797d12e2aac57"
+    sha256 "9a77392979b87347cd2f322bdf55b20b0367655c1210d73bc3ebe827c1a8c0c8"
     "en-US"
   end
   language "fr" do
-    sha256 "e004daef88557140f43709fe256aa96b42b1d756324c1b49c7d9d6e8e3016f16"
+    sha256 "99fc1188b9bc45419574fbbdf351b5e8bad2982922640718c503051a0897abbc"
     "fr"
   end
   language "it" do
-    sha256 "75e723a24f07a00af9e4b4b93cc1015a78bbb906a8e5e79638c2ede41b4ba09c"
+    sha256 "87cb3a762f5b7981860cae46b82c6d008b7596ca92636014b86599c771c4440b"
     "it"
   end
   language "ru" do
-    sha256 "de04600e3b422251c8ce628835dcff1528e9e0eb1111321e306f6c9842cf1220"
+    sha256 "05571febd004c78245d37e7ddfc29aab6f9602422acd464c833d127bd9b18b58"
     "ru"
   end
 
