7e1a39cafbc10242ec79c508b7dfd790b6091aae
peeweep <peeweep@0x0.ee>
2022-03-31 15:44:14 +0800

    Update wechatwork from 4.0.2.90474 to 4.0.3.90492 (#121191)

--- Casks/wechatwork.rb
+++ Casks/wechatwork.rb
@@ -1,6 +1,6 @@
 cask "wechatwork" do
-  version "4.0.2.90474"
-  sha256 "af2ecb6694e8ee5677a9d7d37790adcf86e0e5cdab5d1eb419e5678a3c531f8c"
+  version "4.0.3.90492"
+  sha256 "c7306cf208cbdc1e4b0dbcfd96f981d04dff5875616ada7d1d369c214be39a4e"
 
   url "https://dldir1.qq.com/foxmail/wecom-mac/update/WeCom_#{version}.dmg"
   name "WeChat Work"
