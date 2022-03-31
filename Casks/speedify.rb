f53ee7a6a298c25298fd76e97ff40585574abdb2
Caleb Jasik <calebjasik@jasik.xyz>
2022-03-30 20:34:58 -0500

    Use versioned url for Speedify cask (#120920)

--- Casks/speedify.rb
+++ Casks/speedify.rb
@@ -1,14 +1,14 @@
 cask "speedify" do
   version "11.9.2,8960"
-  sha256 :no_check
+  sha256 "71c0f67a5301e065a696e18f6373d57c8ced97675dc6d0cb1aa84c299a520bed"
 
-  url "https://downloads.speedify.com/speedify.php?platform=osx"
+  url "https://downloads.speedify.com/Speedify-#{version.csv.first}.#{version.csv.second}.dmg"
   name "Speedify"
   desc "VPN client"
   homepage "https://speedify.com/"
 
   livecheck do
-    url :url
+    url "https://downloads.speedify.com/speedify.php?platform=osx"
     strategy :extract_plist
   end
 
