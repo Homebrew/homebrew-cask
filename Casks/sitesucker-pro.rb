f36e796ef0c6de2f70a3b8bd0599521d4a58ddf4
i0ntempest <szf1234@me.com>
2022-03-31 00:03:51 -0400

    Update sitesucker-pro from 4.4 to 4.4.1

--- Casks/sitesucker-pro.rb
+++ Casks/sitesucker-pro.rb
@@ -3,8 +3,8 @@ cask "sitesucker-pro" do
     version "3.2.7"
     sha256 "dd61a113ad86b580e0faf97b4aa86290e038bb3e098f2d19e67fc9e194ce1a3e"
   else
-    version "4.4"
-    sha256 "b09c6b0b469ecd477919419294fc995af3ebfee68c87d7d63abf35776b48442b"
+    version "4.4.1"
+    sha256 "897f608e66caf241b1c19ae8c7b3c08845f6d4d8ca7beeff0d3f980a80951deb"
   end
 
   url "https://ricks-apps.com/osx/sitesucker/archive/#{version.major}.x/#{version.major_minor}.x/#{version}/SiteSucker_Pro_#{version}.dmg"
