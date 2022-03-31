adbfef6559c9d59fb66f70d19f1db3c90df5491e
Luca Palli <luca@palli.ch>
2022-03-31 11:01:59 +0200

    Update miro from 0.7.5 to 0.7.7 (#121201)

--- Casks/miro.rb
+++ Casks/miro.rb
@@ -1,7 +1,7 @@
 cask "miro" do
   arch = Hardware::CPU.intel? ? "darwin" : "darwin-arm64"
 
-  version "0.7.5"
+  version "0.7.7"
   sha256 :no_check
 
   url "https://desktop.miro.com/platforms/#{arch}/Miro.dmg"
