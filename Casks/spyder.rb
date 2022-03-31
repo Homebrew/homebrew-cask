d9c074a81b6af00a54a73b8ce59c829666fe5fa2
Clément L <porkepix@gmail.com>
2022-03-31 13:17:16 +0200

    Update spyder from 5.2.2 to 5.3.0 (#121236)

--- Casks/spyder.rb
+++ Casks/spyder.rb
@@ -1,6 +1,6 @@
 cask "spyder" do
-  version "5.2.2"
-  sha256 "b06abfb82f11da86f2b16624393adff976a48bbc99d19dbc98cef2f27b57659e"
+  version "5.3.0"
+  sha256 "979ab649c478f578be8ce3e4260e1f53350076a3b94fa3ee5444cf0a5a4a9bd9"
 
   url "https://github.com/spyder-ide/spyder/releases/download/v#{version}/Spyder.dmg",
       verified: "github.com/spyder-ide/spyder/"
