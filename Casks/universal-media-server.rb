ea6b5d3deea231a84a85ad264e1570ce9cf3d1f0
neutric <5984479+neutric@users.noreply.github.com>
2022-04-01 15:17:39 +0200

    Update universal-media-server from 10.18.0 to 10.19.0 (#121273)

--- Casks/universal-media-server.rb
+++ Casks/universal-media-server.rb
@@ -1,6 +1,6 @@
 cask "universal-media-server" do
-  version "10.18.0"
-  sha256 "302e0e332c056f3565e31a6d5a7487718048db3357aa554b8f34f2bc0b9eedc0"
+  version "10.19.0"
+  sha256 "fb4d196c498da31f407a2f1d697227aa4198eaa506f75dc8988303fff234fa33"
 
   url "https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/#{version}/UMS-#{version}.dmg",
       verified: "github.com/UniversalMediaServer/UniversalMediaServer/"
