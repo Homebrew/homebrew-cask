cf24a1d135b6a7db90ef5cfb31224983f10ec899
Yishen Miao <mys721tx@gmail.com>
2022-03-31 18:56:52 -0700

    Bumping PreForm to 3.23.1
    Bumps PreForm to 3.23.1.

--- Casks/preform.rb
+++ Casks/preform.rb
@@ -1,6 +1,6 @@
 cask "preform" do
-  version "3.23.0,106_16635"
-  sha256 "d4f6257279b1654e1f6f5ce434b3ffcc78595a8401dd3f915c16145fd7b0aed7"
+  version "3.23.1,109_17301"
+  sha256 "9d17f6627947095797fa42c1bac8028cf614d37e4f8b8becac1fcc416da65f45"
 
   url "https://s3.amazonaws.com/FormlabsReleases/Release/#{version.csv.first}/PreForm_mac_#{version.csv.first}_release_releaser_#{version.csv.second}.dmg",
       verified: "s3.amazonaws.com/FormlabsReleases/"
