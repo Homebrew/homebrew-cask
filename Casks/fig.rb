39f2a5e2d18de2e7ee7423fd104b0e122851591a
Matt Schrage <matthewschrage@gmail.com>
2022-04-01 06:23:06 -0700

    Update fig from 1.0.56,414 to 1.0.56,421 (#121290)
    Co-authored-by: runner <runner@Mac-1648784978738.local>

--- Casks/fig.rb
+++ Casks/fig.rb
@@ -1,6 +1,6 @@
 cask "fig" do
-  version "1.0.56,414"
-  sha256 "2e830bfefedcf30738782565d277dd81092f7cab699523c022fea0307bdd8e11"
+  version "1.0.56,421"
+  sha256 "95f0dfe35b132edcf67f9a2964324131a1122e47a0f84dc7851cd43b5a6b27b0"
 
   url "https://versions.withfig.com/fig%20#{version.csv.second}.dmg",
       verified: "versions.withfig.com/"
