0e53f5701a94e8b5c36ce35d36e67a92fca6619a
owine <owine@users.noreply.github.com>
2022-04-01 06:31:39 -0500

    Update mountain-duck from 4.10.3 to 4.11.0 (#121255)

--- Casks/mountain-duck.rb
+++ Casks/mountain-duck.rb
@@ -1,6 +1,6 @@
 cask "mountain-duck" do
-  version "4.10.3,19145"
-  sha256 "1cf3c05b2a74cac448e1336502f75bf1f347aee23d602f6db8a1a6e7bbfe518c"
+  version "4.11.0,19424"
+  sha256 "d23f5a1e971ca1591dfc1369aecc97610bb61c645e4704b41b00d5445cc44929"
 
   url "https://dist.mountainduck.io/Mountain%20Duck-#{version.csv.first}.#{version.csv.second}.zip"
   name "Mountain Duck"
