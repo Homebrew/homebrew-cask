54a2264a4c75235528ae0462ce4a89c2971e1ed0
Clément L <porkepix@gmail.com>
2022-04-01 15:21:14 +0200

    Update pyzo from 4.12.0 to 4.12.3 (#121264)

--- Casks/pyzo.rb
+++ Casks/pyzo.rb
@@ -1,6 +1,6 @@
 cask "pyzo" do
-  version "4.12.0"
-  sha256 "eccd93c7a0d618b8cea0492bee1751bdba9ebead489806ca238c2f5919d91d6e"
+  version "4.12.3"
+  sha256 "5a6a92cbf86e9c3ad7976147ee2bd7b14bbb68ff79038cdc6f9c08f13b58cc47"
 
   url "https://github.com/pyzo/pyzo/releases/download/v#{version}/pyzo-#{version}-macos_x86_64.dmg",
       verified: "github.com/pyzo/pyzo/"
