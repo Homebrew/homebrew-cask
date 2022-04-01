fcf79af590acf2256de601ccdb29f3c83ef605ef
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2022-04-01 17:10:12 +0200

    ganttproject 3.2.3230 (botched upstream release)
    It looks like the upstream maintainer was not thorough in re-releasing 3.2.3200.
    - Add `version_of_github_release` to paper over this
    - Use it to construct `url`
    - Update sha256

--- Casks/ganttproject.rb
+++ Casks/ganttproject.rb
@@ -1,8 +1,14 @@
 cask "ganttproject" do
-  version "3.2.3200"
-  sha256 "6dde4a151cabdf69fe84a3ea6d4b943233c8f9cb181bdac9168a6748e74778a6"
+  version "3.2.3230"
+  sha256 "4e6ebd308378d3d1936050ff6407630692d5b8833f05f139428b052cf91982dc"
 
-  url "https://github.com/bardsoftware/ganttproject/releases/download/ganttproject-#{version}/ganttproject-#{version}.dmg",
+  ## Normally `version_of_github_release` is the same as `version`, so
+  ## one would say:
+  # version_of_github_release = version()
+  ## ... But currently this isn't the case:
+  version_of_github_release = "3.2.3200"
+
+  url "https://github.com/bardsoftware/ganttproject/releases/download/ganttproject-#{version_of_github_release}/ganttproject-#{version}.dmg",
       verified: "github.com/bardsoftware/ganttproject/"
   name "GanttProject"
   desc "Gantt chart and project management application"
