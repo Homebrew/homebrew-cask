4b32f861aeeb427e60267cf18965f3dacf333950
neutric <5984479+neutric@users.noreply.github.com>
2022-04-01 15:20:04 +0200

    Update navicat-for-sql-server from 16.0.11 to 16.0.12 (#121280)

--- Casks/navicat-for-sql-server.rb
+++ Casks/navicat-for-sql-server.rb
@@ -1,5 +1,5 @@
 cask "navicat-for-sql-server" do
-  version "16.0.11"
+  version "16.0.12"
   sha256 :no_check
 
   url "https://download.navicat.com/download/navicat#{version.major_minor.no_dots}_sqlserver_en.dmg"
