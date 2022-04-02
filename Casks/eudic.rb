5e85e62ca88ee0a0cf28d78256a24443a22572d6
Minghua Zheng <m.zheng2@outlook.com>
2022-04-02 20:16:29 +0100

    Update eudic from 4.1.5 to 4.2.0

--- Casks/eudic.rb
+++ Casks/eudic.rb
@@ -2,7 +2,7 @@ cask "eudic" do
   version "4.2.0,1070"
   sha256 "b6feebf8824fa71ac8bb521749707f336d36aaf6cae9fde6b4803cb3a668ef59"
 
-  url "https://static.frdic.com/pkg/eudicmac.dmg?v=#{version.csv.second}",
+  url "https://static.frdic.com/pkg/eudicmac.dmg",
       verified:   "static.frdic.com/",
       user_agent: :fake
   name "Eudic"
@@ -11,8 +11,8 @@ cask "eudic" do
   homepage "https://www.eudic.net/v4/en/app/eudic"
 
   livecheck do
-    url "https://www.eudic.net/update/eudic_mac.xml"
-    strategy :sparkle
+    url :url
+    strategy :extract_plist
   end
 
   app "Eudic.app"
