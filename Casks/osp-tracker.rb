72908d4ac57b843caef3b7452e62289e7a968498
Gabriel Gerlero <gerlero@users.noreply.github.com>
2022-04-02 17:56:27 -0300

    Add Tracker.app v6.0.7

---
+++ Casks/osp-tracker.rb
@@ -0,0 +1,27 @@
+cask "osp-tracker" do
+  version "6.0.7"
+  sha256 "df0e0ccd2e2631fee0dc4c6863a7b015215c05eb620a9d913958f8cb8ac68ebe"
+
+  url "https://physlets.org/tracker/installers/download.php?file=Tracker-#{version}-osx-installer.dmg"
+  name "Tracker"
+  desc "Video analysis and modeling tool for physics education"
+  homepage "https://physlets.org/tracker/"
+
+  livecheck do
+    url "https://physlets.org/tracker/"
+    strategy :page_match
+    regex(/href="?.*?file=Tracker-(\d+(?:\.\d+)+)-osx-installer\.dmg"?/i)
+  end
+
+  installer script: {
+    executable: "Tracker-#{version}-osx-installer.app/Contents/MacOS/installbuilder.sh",
+    args:       ["--mode", "unattended"],
+    sudo:       true,
+  }
+
+  uninstall script: {
+    executable: "/usr/local/tracker/uninstall_Tracker.app/Contents/MacOS/installbuilder.sh",
+    args:       ["--mode", "unattended"],
+    sudo:       true,
+  }
+end
