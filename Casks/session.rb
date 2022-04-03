52621a003463fff6ecdfcbedeac5c2de67fc832c
Clément L <porkepix@gmail.com>
2022-04-03 15:26:15 +0200

    Update session from 1.7.6 to 1.7.9 (#121351)

--- Casks/session.rb
+++ Casks/session.rb
@@ -1,6 +1,6 @@
 cask "session" do
-  version "1.7.6"
-  sha256 "7056ed33a377737cd68ae9d229c0aa1f1e449ce5fcf77395d8eae323f85be478"
+  version "1.7.9"
+  sha256 "8e88dc5f37fbf6efbbe83cf73f2b5b3d33c7b501dc2b3adb4e4b06bb2b4574e9"
 
   url "https://github.com/oxen-io/session-desktop/releases/download/v#{version}/session-desktop-mac-#{version}.dmg",
       verified: "github.com/oxen-io/session-desktop/"
