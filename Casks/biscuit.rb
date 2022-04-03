9fde62375ac9b328dcf372666748411a9811bb29
Alexander Ronald Altman <alexanderaltman@me.com>
2022-04-03 16:19:57 -0700

    Update biscuit from 1.2.24 to 1.2.25

--- Casks/biscuit.rb
+++ Casks/biscuit.rb
@@ -1,13 +1,13 @@
 cask "biscuit" do
-  version "1.2.24"
+  version "1.2.25"
 
   if Hardware::CPU.intel?
-    sha256 "3bcecb7a99765eb05b827454fe5ba5e07c8573ce96e895c65d8edc9a2af2cd35"
+    sha256 "0f3f705f7a29f3dbbb8206a1e0b9234a95b5f8cc924ce34e8c51af3dd5bb9aa6"
 
     url "https://github.com/agata/dl.biscuit/releases/download/v#{version}/Biscuit-#{version}.dmg",
         verified: "github.com/agata/dl.biscuit/"
   else
-    sha256 "0b47026f817d5d27a00c5d59a29d430e7aab9235ca1dbbbba922a7d55a95d30a"
+    sha256 "24be52aa4643c3ee348525f2f3e0ec9a849cb71599027fdeb1b0c4686a644d53"
 
     url "https://github.com/agata/dl.biscuit/releases/download/v#{version}/Biscuit-#{version}-arm64.dmg",
         verified: "github.com/agata/dl.biscuit/"
