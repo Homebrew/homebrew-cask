13d6346a82b11c6f86f1843213401b73ec340669
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2022-04-03 15:25:14 +0200

    Update tiled from 1.8.2 to 1.8.4 (#121362)

--- Casks/tiled.rb
+++ Casks/tiled.rb
@@ -1,6 +1,6 @@
 cask "tiled" do
-  version "1.8.2"
-  sha256 "8f01e6840211ab6713174a4e48478ebd9bfe7aae0b27fb3668c1b665ffb29943"
+  version "1.8.4"
+  sha256 "82b66466a05a12c8682132a5b16dc7eaa3b3c2e82fd4d7db18a7ebc7568b9a27"
 
   url "https://github.com/bjorn/tiled/releases/download/v#{version}/Tiled-#{version}-macos.zip",
       verified: "github.com/bjorn/tiled/"
