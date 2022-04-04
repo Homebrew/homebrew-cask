ff7130e98a1526592d69397a3c23df4d3a5502b5
Bevan Kay <email@bevankay.me>
2022-04-04 17:40:29 +1000

    add livecheck, bump version

--- Casks/anypointstudio.rb
+++ Casks/anypointstudio.rb
@@ -1,15 +1,16 @@
 cask "anypointstudio" do
-  version "7.11.1"
-  sha256 "417be2b9b60674eb1709e97d1566c23529cf3920afc0ab9d3eec0faca54446b6"
+  version "7.12.0"
+  sha256 "03560aa3e96b5467305d87c5961703c92285b92f396b6be861c6b0ceb91b167c"
 
-  url "https://mule-studio.s3.amazonaws.com/#{version}-U1/AnypointStudio-#{version}-macos64.zip",
+  url "https://mule-studio.s3.amazonaws.com/#{version}-GA/AnypointStudio-#{version}-macos64.zip",
       verified: "mule-studio.s3.amazonaws.com/"
   name "Anypoint Studio"
   desc "Eclipse-based IDE for designing and testing Mule applications"
   homepage "https://www.mulesoft.com/platform/studio"
 
   livecheck do
-    skip "No version information available"
+    url "https://docs.mulesoft.com/release-notes/studio/anypoint-studio"
+    regex(/Anypoint\s+Studio\s+v?(\d+(?:\.\d+)+)/i)
   end
 
   app "AnypointStudio.app"
