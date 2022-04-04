656c7396170ed1101b4104065b9a77dce6701d65
Bevan Kay <email@bevankay.me>
2022-04-04 19:08:14 +1000

    Update lunar from 5.5.3 to 5.5.4

--- Casks/lunar.rb
+++ Casks/lunar.rb
@@ -1,6 +1,6 @@
 cask "lunar" do
-  version "5.5.3"
-  sha256 "46e33e7a213d270f51aeefdcfa3b7d34803cf81ef012d221c3a79f9d098719e5"
+  version "5.5.4"
+  sha256 "cf0dcd553b1ffdc2caf12ea9d13b811cba617f41ba59081e2a4b848e0e7b06fe"
 
   url "https://static.lunar.fyi/releases/Lunar-#{version}.dmg"
   name "Lunar"
