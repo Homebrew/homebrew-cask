d8ffb43152acaa8fe91702f5530fbfba756372e9
Daniel Bayley <daniel.bayley@me.com>
2022-04-03 18:51:19 -0700

    Add BetterMouse 1.2.833 (#121247)
    * Add BetterMouse 1.2.833
    
    * update zap
    
    * bump version
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

---
+++ Casks/bettermouse.rb
@@ -0,0 +1,27 @@
+cask "bettermouse" do
+  version "1.2.858"
+  sha256 "6a4bef1e80cd0e8e733f8976961e33c95da073376e554ab755d22ac67b513d56"
+
+  url "https://better-mouse.com/wp-content/uploads/BetterMouse.#{version}.zip"
+  name "BetterMouse"
+  desc "Utility improving 3rd party mouse performance and functionalities"
+  homepage "https://better-mouse.com/"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(/Version (\d+(?:\.\d+)+)/)
+  end
+
+  app "BetterMouse.app"
+
+  uninstall quit: bundle_id = "com.naotanhaocan.BetterMouse"
+
+  zap trash: [
+    "~/Library/Application Support/BetterMouse",
+    "~/Library/Caches/#{bundle_id}",
+    "~/Library/HTTPStorages/#{bundle_id}*",
+    "~/Library/Preferences/#{bundle_id}.plist",
+    "~/Library/Saved Application State/#{bundle_id}.savedState",
+  ]
+end
