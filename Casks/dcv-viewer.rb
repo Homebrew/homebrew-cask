f9162044d68c6b11cc23b356ff557ce901d1f905
Alexander Ronald Altman <alexanderaltman@me.com>
2022-04-03 21:06:23 -0700

    Update dcv-viewer from 2022.0.3929 to 2022.0.3973 (#121470)

--- Casks/dcv-viewer.rb
+++ Casks/dcv-viewer.rb
@@ -1,12 +1,12 @@
 cask "dcv-viewer" do
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 
-  version "2022.0.3929"
+  version "2022.0.3973"
 
   if Hardware::CPU.intel?
-    sha256 "4b596d5c6709eb92f88120dbd80bb21d4f7ff8ce0754d616d42470283f75f0e7"
+    sha256 "4192d79260933dfe0dd60858b088f2f286b91b24749c69336fc3e455666d7f39"
   else
-    sha256 "ea4cbb17eb819b7551c100e7a39c2fcb6b8d106632c418b89fd32ec1cced8e1f"
+    sha256 "150bdc8fcfa12fb4a0647e4fcd7b05142ea47b65bf3b4e400e92e2584a175c39"
   end
 
   url "https://d1uj6qtbmh3dt5.cloudfront.net/#{version.major_minor}/Clients/nice-dcv-viewer-#{version}.#{arch}.dmg",
