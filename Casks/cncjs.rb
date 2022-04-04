19d14996ce733b628a5f664342d8119eb2d32338
Alexander Ronald Altman <alexanderaltman@me.com>
2022-04-03 19:15:49 -0700

    Update cncjs from 1.9.23 to 1.9.24

--- Casks/cncjs.rb
+++ Casks/cncjs.rb
@@ -1,6 +1,6 @@
 cask "cncjs" do
-  version "1.9.23"
-  sha256 "2de514db5ade63c858cd2f4ca768cbc3b5002940cb559a3d4d2d0740b28282c1"
+  version "1.9.24"
+  sha256 "79b35d79f369f3ed021caa61ca4565ae03848e8d03b1929f35126cca168d44e7"
 
   url "https://github.com/cncjs/cncjs/releases/download/v#{version}/cncjs-app-#{version}-macos.x64.dmg",
       verified: "github.com/cncjs/cncjs/"
