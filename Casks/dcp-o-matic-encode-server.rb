bc07b1123706bb763ae2de1714d103026d04ac9f
Alexander Ronald Altman <alexanderaltman@me.com>
2022-04-03 20:05:47 -0700

    Update dcp-o-matic-encode-server from 2.14.57 to 2.16.5

--- Casks/dcp-o-matic-encode-server.rb
+++ Casks/dcp-o-matic-encode-server.rb
@@ -1,8 +1,8 @@
 cask "dcp-o-matic-encode-server" do
-  version "2.14.57"
-  sha256 "5a52cfa03a64b3c0e124f01a4d858480cbbb3a0d660b511f62b2403f8c870ba4"
+  version "2.16.5"
+  sha256 "70acd5eb2b2a09d82bf0d615bae0754ba6750da734c4575f4918efdf4d3c1417"
 
-  url "https://dcpomatic.com/dl.php?id=osx-10.9-server&version=#{version}"
+  url "https://dcpomatic.com/dl.php?id=osx-10.10-server&version=#{version}"
   name "DCP-o-matic Encode Server"
   desc "Convert video, audio and subtitles into DCP (Digital Cinema Democratized)"
   homepage "https://dcpomatic.com/"
