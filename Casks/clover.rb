4a1ee3f63fabf0d89f014429ca1af52541560785
Alexander Ronald Altman <alexanderaltman@me.com>
2022-04-03 21:08:31 -0700

    Update clover from 1.1.0 to 1.1.2 (#121450)

--- Casks/clover.rb
+++ Casks/clover.rb
@@ -1,6 +1,6 @@
 cask "clover" do
-  version "1.1.0"
-  sha256 "ab778b4937c4dff320f8e106500daaa0d12536158964a902a307f7004938b546"
+  version "1.1.2"
+  sha256 "364d87485c5f99ce7419d534c82fd2900fb34b5e043a4e5b19a899b19ae77f56"
 
   url "https://clover-desktop-app.s3.amazonaws.com/Clover-#{version}.dmg",
       verified: "clover-desktop-app.s3.amazonaws.com/"
