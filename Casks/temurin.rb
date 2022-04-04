f0cde2092ed0fe542979635e4485e3a1eab3b66b
Shelley Lambert <slambert@gmail.com>
2022-04-04 07:24:40 -0400

    JDK18 release
    Signed-off-by: Shelley Lambert <slambert@gmail.com>

--- Casks/temurin.rb
+++ Casks/temurin.rb
@@ -1,12 +1,12 @@
 cask "temurin" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
 
-  version "17.0.2,8"
+  version "18,36"
 
   if Hardware::CPU.intel?
-    sha256 "b82d3ba3ff4c453bd954979a20e67754478294da8650e3b9e50f6d1a7cf5c628"
+    sha256 "cdef36a561d7550fb505a9c36fedf363af16a5984c92e74f5abaa25172c12537"
   else
-    sha256 "0904c11eb5f7b53aac53e6279992ee2a900c9517b26659dee3704b2d11c149cd"
+    sha256 ""
   end
 
   url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.csv.first}%2B#{version.csv.second}/OpenJDK#{version.major}U-jdk_#{arch}_mac_hotspot_#{version.csv.first}_#{version.csv.second.major}.pkg",
