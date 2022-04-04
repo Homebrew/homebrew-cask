d0c9b09ba140a9b5453a8f14318b2a573845befe
Alexander Ronald Altman <alexanderaltman@me.com>
2022-04-03 20:09:58 -0700

    Update dcp-o-matic-batch-converter from 2.14.57 to 2.16.5

--- Casks/dcp-o-matic-batch-converter.rb
+++ Casks/dcp-o-matic-batch-converter.rb
@@ -1,8 +1,8 @@
 cask "dcp-o-matic-batch-converter" do
-  version "2.14.57"
-  sha256 "6c5c5b246c86e640f0579ad07a69b59d6d3b0aaf6cf20a2995c24fcb961a8c87"
+  version "2.16.5"
+  sha256 "76ac85848b3677d0c3336717559d79dccbf09dfaef589964b1fb29d78b7b802d"
 
-  url "https://dcpomatic.com/dl.php?id=osx-10.9-batch&version=#{version}"
+  url "https://dcpomatic.com/dl.php?id=osx-10.10-batch&version=#{version}"
   name "DCP-o-matic Batch converter"
   desc "Convert video, audio and subtitles into DCP (Digital Cinema Democratized)"
   homepage "https://dcpomatic.com/"
