0dc74a64ca8f2005c4f617c5a49cdfb2d7cfeaee
Bevan Kay <email@bevankay.me>
2022-04-04 10:12:55 +1000

    update livecheck

--- Casks/ganttproject.rb
+++ Casks/ganttproject.rb
@@ -1,8 +1,8 @@
 cask "ganttproject" do
-  version "3.2.3230"
+  version "3.2.3200,3.2.3230"
   sha256 "4e6ebd308378d3d1936050ff6407630692d5b8833f05f139428b052cf91982dc"
 
-  url "https://github.com/bardsoftware/ganttproject/releases/download/ganttproject-#{version}/ganttproject-#{version}.dmg",
+  url "https://github.com/bardsoftware/ganttproject/releases/download/ganttproject-#{version.csv.first}/ganttproject-#{version.csv.second}.dmg",
       verified: "github.com/bardsoftware/ganttproject/"
   name "GanttProject"
   desc "Gantt chart and project management application"
@@ -10,8 +10,13 @@ cask "ganttproject" do
 
   livecheck do
     url :url
-    strategy :github_latest
-    regex(/href=.*?ganttproject[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+    regex(%r{href=.*ganttproject[._-]v?(\d+(?:\.\d+)+)/ganttproject[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
+    strategy :github_latest do |page, regex|
+      match = page.match(regex)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
   end
 
   app "GanttProject.app"
