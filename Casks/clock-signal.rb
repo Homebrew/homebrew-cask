e6202cefa8a55f9c35cf3d0d06e04d771bc48a22
Alexander Ronald Altman <alexanderaltman@me.com>
2022-04-03 19:06:10 -0700

    Update clock-signal from 2022-03-13 to 2022-03-26

--- Casks/clock-signal.rb
+++ Casks/clock-signal.rb
@@ -1,6 +1,6 @@
 cask "clock-signal" do
-  version "2022-03-13"
-  sha256 "d91ff16ea71fe8084a171b12a97860235d570c39f5a0632b73379a5c659145f8"
+  version "2022-03-26"
+  sha256 "b460f8de31fee6c9bcfb35b0064efe46cacaa6de5e08e0f15787d3ac6875577a"
 
   url "https://github.com/TomHarte/CLK/releases/download/#{version}/Clock.Signal.macOS.#{version}.zip"
   name "Clock Signal"
