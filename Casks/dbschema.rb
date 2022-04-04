f39e307179b0f2ea3f7f29f5172247e97b56c63e
Alexander Ronald Altman <alexanderaltman@me.com>
2022-04-03 21:06:37 -0700

    Update dbschema from 8.5.2 to 8.5.2 (#121468)

--- Casks/dbschema.rb
+++ Casks/dbschema.rb
@@ -1,5 +1,5 @@
 cask "dbschema" do
-  version "8.5.1"
+  version "8.5.2"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.dbschema.com/download/DbSchema_macos_#{version.dots_to_underscores}.tgz"
