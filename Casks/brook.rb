1c48b07161a47b255adce07f06e9f6070effb31d
Alexander Ronald Altman <alexanderaltman@me.com>
2022-04-03 17:36:16 -0700

    Update brook from 20220404 to 20220406 (#121426)

--- Casks/brook.rb
+++ Casks/brook.rb
@@ -1,6 +1,6 @@
 cask "brook" do
-  version "20220404"
-  sha256 "8efb20bc66022b1bfa981bf2b30079b1d29a5409edeb5c0a234c0e66325ff15c"
+  version "20220406"
+  sha256 "29737fbc9d56ee0827df98e58fa10d532c427b39f4345c0d284d9290ef114482"
 
   url "https://github.com/txthinking/brook/releases/download/v#{version}/Brook.dmg"
   name "Brook"
