049da76eb9b38107eebdf2313eaa384067c9134a
Alexander Ronald Altman <alexanderaltman@me.com>
2022-04-03 21:04:48 -0700

    Update drovio from 3.2.3 to 3.2.4 (#121490)

--- Casks/drovio.rb
+++ Casks/drovio.rb
@@ -1,12 +1,12 @@
 cask "drovio" do
   arch = Hardware::CPU.intel? ? "macos" : "macos_silicon"
 
-  version "3.2.3"
+  version "3.2.4"
 
   if Hardware::CPU.intel?
-    sha256 "0a248aee1d2435846bad06fdbbc8662459c66c7dde2ba2ee97d014b5420921c4"
+    sha256 "cd989df4768fff060c15cf1f434cf935ea921a7bc017b17502d9e7aa5e648b3b"
   else
-    sha256 "3e40520c1edbd560e9d9ecaebd3d888eebbe71c6d43210a87bffa1142863be57"
+    sha256 "0c6333b1125542b51111e4b99a5fe00948d0672bbfa01ba1062a30b1df7886cf"
   end
 
   url "https://repository.drovio.com/stable/drovio/#{arch}/#{version.major}.x/#{version}/drovio.dmg"
