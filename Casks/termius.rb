fe568b1c5a2d63d4c2a2dea5ad9cd5deddd4f24e
Matt Coster <opensource@mtcoster.net>
2022-04-04 09:35:58 +0100

    Update termius from 7.37.0 to 7.37.2 (#121505)

--- Casks/termius.rb
+++ Casks/termius.rb
@@ -1,7 +1,7 @@
 cask "termius" do
   arch = Hardware::CPU.intel? ? "mac" : "mac-arm64"
 
-  version "7.37.0"
+  version "7.37.2"
   sha256 :no_check
 
   url "https://autoupdate.termius.com/#{arch}/Termius.dmg"
