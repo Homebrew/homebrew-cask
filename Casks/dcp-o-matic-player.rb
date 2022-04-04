0fe9af7a2bffd944d9609b13b4283bc617a6af92
Alexander Ronald Altman <alexanderaltman@me.com>
2022-04-03 21:05:52 -0700

    Update dcp-o-matic-player from 2.14.57 to 2.16.5 (#121477)

--- Casks/dcp-o-matic-player.rb
+++ Casks/dcp-o-matic-player.rb
@@ -1,8 +1,8 @@
 cask "dcp-o-matic-player" do
-  version "2.14.57"
-  sha256 "a8c9dc2f1ee081d34da0045ccd4fe49ffec21ba9ff81de52a7c0c1aee36e9ab8"
+  version "2.16.5"
+  sha256 "aaac9049c7fa353b3a81eace6bb928d3d28e3cdab57db36e0ee7b0e76b022ad9"
 
-  url "https://dcpomatic.com/dl.php?id=osx-10.9-player&version=#{version}"
+  url "https://dcpomatic.com/dl.php?id=osx-10.10-player&version=#{version}"
   name "DCP-o-matic Player"
   desc "Play Digital Cinema Packages"
   homepage "https://dcpomatic.com/"
