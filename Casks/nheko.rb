b82d14eb475d76ed24c6604d30e00c9cf26d673c
Bevan Kay <email@bevankay.me>
2022-04-04 10:13:58 +1000

    Update nheko.rb

--- Casks/nheko.rb
+++ Casks/nheko.rb
@@ -2,7 +2,7 @@ cask "nheko" do
   version "0.9.3"
   sha256 "f51e814b9d31fda776866aad9a8d62b61175c565b5a783de9c2f87a47e8c13fb"
 
-  url "https://github.com/Nheko-Reborn/nheko/releases/download/v#{version.csv.first}/nheko-v#{version.csv.first}.dmg",
+  url "https://github.com/Nheko-Reborn/nheko/releases/download/v#{version}/nheko-v#{version}.dmg",
       verified: "github.com/Nheko-Reborn/nheko/"
   name "Nheko"
   desc "Desktop client for the Matrix protocol"
