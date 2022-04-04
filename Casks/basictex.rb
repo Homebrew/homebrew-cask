b4146ef056f92fb9766948ea830670a8f3670fef
Miccal Matthews <miccal.matthews@gmail.com>
2022-04-05 04:32:04 +0800

    Update basictex.rb

--- Casks/basictex.rb
+++ Casks/basictex.rb
@@ -2,7 +2,7 @@ cask "basictex" do
   version "2022.0314"
   sha256 "5ef0678318c2b947b78b77c0cddae09e763359596c6c1fd0362f5cdca9714b78"
 
-  url "http://mirror.ctan.org/systems/mac/mactex/mactex-basictex-#{version.no_dots}.pkg",
+  url "https://mirror.ctan.org/systems/mac/mactex/mactex-basictex-#{version.no_dots}.pkg",
       verified: "mirror.ctan.org/systems/mac/mactex/"
   name "BasicTeX"
   desc "Compact TeX distribution as alternative to the full TeX Live / MacTeX"
