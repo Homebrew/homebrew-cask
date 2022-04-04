bb755802b43831ebe7f2a2bcccb71c28516b9d8f
Kenneth Chew <79120643+kthchew@users.noreply.github.com>
2022-04-04 05:26:49 +0000

    Add PolyMC.app v1.1.1 (#121473)
    * Add PolyMC.app v1.1.1
    
    * Remove livecheck from PolyMC.app cask

---
+++ Casks/polymc.rb
@@ -0,0 +1,21 @@
+cask "polymc" do
+  version "1.1.1"
+  sha256 "5f40a9db742b1910825f88980c24e5bb40673b127e5794a4d09729356e5a9778"
+
+  url "https://github.com/PolyMC/PolyMC/releases/download/#{version}/PolyMC-macOS-#{version}.tar.gz",
+      verified: "github.com/PolyMC/PolyMC/"
+  name "PolyMC"
+  desc "Minecraft launcher"
+  homepage "https://polymc.org/"
+
+  depends_on macos: ">= :sierra"
+
+  app "PolyMC.app"
+
+  zap trash: [
+    "~/Library/Application Support/PolyMC/metacache",
+    "~/Library/Application Support/PolyMC/PolyMC-*.log",
+    "~/Library/Application Support/PolyMC/polymc.cfg",
+    "~/Library/Saved Application State/org.polymc.PolyMC.savedState",
+  ]
+end
