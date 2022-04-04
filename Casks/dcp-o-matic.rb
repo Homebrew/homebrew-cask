e7958ace41186b8a43578a9bcdf0264cd68d80a4
Alexander Ronald Altman <alexanderaltman@me.com>
2022-04-03 21:06:11 -0700

    Update dcp-o-matic from 2.14.57 to 2.16.5 (#121474)

--- Casks/dcp-o-matic.rb
+++ Casks/dcp-o-matic.rb
@@ -1,8 +1,8 @@
 cask "dcp-o-matic" do
-  version "2.14.57"
-  sha256 "ba9155b7d5abf71a4c589ab4c59d9d8b8b929261c2ede4168fca6de19163144e"
+  version "2.16.5"
+  sha256 "b28143c0008248cfdd3674edf1b2c2e033a6c9976ccec86cd296f435c0dbb55d"
 
-  url "https://dcpomatic.com/dl.php?id=osx-10.9-main&version=#{version}"
+  url "https://dcpomatic.com/dl.php?id=osx-10.10-main&version=#{version}"
   name "DCP-o-matic"
   desc "Convert video, audio and subtitles into DCP (Digital Cinema Democratized)"
   homepage "https://dcpomatic.com/"
