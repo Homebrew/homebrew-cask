e0aa95dd2dac0ecc3bc129b4b1b0b0f8bb6b2fea
Lex.Chen <ctyytc@gmail.com>
2022-04-05 23:45:34 +0800

    Update staruml.rb to add arm64 support (#121566)
    Update staruml.rb to add arm64 support

--- Casks/staruml.rb
+++ Casks/staruml.rb
@@ -1,15 +1,22 @@
 cask "staruml" do
-  version "5.0.1"
-  sha256 "31d72acb8d12e255fb67afc536f952bae7b4400702df67b515adf564100ec87d"
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  url "https://staruml.io/download/releases-v#{version.major}/StarUML-#{version}.dmg"
+  version "5.0.1"
+
+  if Hardware::CPU.intel?
+    sha256 "31d72acb8d12e255fb67afc536f952bae7b4400702df67b515adf564100ec87d"
+  else
+    sha256 "259d32508bd843e4defb6ec1b46e016d8408432b4095c60ec045f1cf2e721a88"
+  end
+
+  url "https://staruml.io/download/releases-v#{version.major}/StarUML-#{version}#{arch}.dmg"
   name "StarUML"
   desc "Software modeler"
   homepage "https://staruml.io/"
 
   livecheck do
     url "https://staruml.io/"
-    regex(%r{href=.*?/StarUML-(\d+(?:\.\d+)*)\.dmg}i)
+    regex(%r{href=.*?/StarUML-(\d+(?:\.\d+)*)(-arm64)?\.dmg}i)
   end
 
   app "StarUML.app"
