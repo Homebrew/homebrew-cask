dcf6a8308767260a8be5ae4202c1fd43a9ed6120
Bevan Kay <email@bevankay.me>
2022-04-05 11:08:06 +1000

    Update qlmarkdown from 1.0.14 to 1.0.15 (#121540)

--- Casks/qlmarkdown.rb
+++ Casks/qlmarkdown.rb
@@ -1,6 +1,6 @@
 cask "qlmarkdown" do
-  version "1.0.14"
-  sha256 "ecf7ff8f6463531e757f664c7fac5a3981229284451fffa6175eb6205e9dcd75"
+  version "1.0.15"
+  sha256 "f8ad590d9d5cc081d3e8f9ecf559e41991aa8a72fd41ed4e7bac70e8e804a2c5"
 
   url "https://github.com/sbarex/QLMarkdown/releases/download/#{version}/QLMarkdown.zip"
   name "sbarex-qlmarkdown"
