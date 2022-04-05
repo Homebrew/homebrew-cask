b289585f4203388e480677b8cd9bd28bd47dc5dd
MoonFruit <dkmoonfruit@gmail.com>
2022-04-05 18:58:11 +0800

    Update mongodb-compass from 1.31.0 to 1.31.1

--- Casks/mongodb-compass.rb
+++ Casks/mongodb-compass.rb
@@ -1,6 +1,6 @@
 cask "mongodb-compass" do
-  version "1.31.0"
-  sha256 "58ec06854aaad8f1639707e3adc9b2214906edc8b52265b703f50d6a0f3dcd51"
+  version "1.31.1"
+  sha256 "ed5fd8b3c78146545c50534ce7c49d7305e1373ec514fb393115c01951903875"
 
   url "https://downloads.mongodb.com/compass/mongodb-compass-#{version}-darwin-x64.dmg"
   name "MongoDB Compass"
