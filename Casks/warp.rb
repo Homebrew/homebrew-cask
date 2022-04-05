6767d5cd3c977b94154a1233380833abd4307c74
Rod Begbie <rod@begbie.com>
2022-04-05 14:27:41 -0700

    Add Warp terminal emulator (#121582)

---
+++ Casks/warp.rb
@@ -0,0 +1,25 @@
+cask "warp" do
+  version "0.2022.04.01.01.37.stable_03"
+  sha256 "c2a51c76b02d4734ef319b3fabaac38a3cf9b41c1f762ddb497196e500f736b3"
+
+  url "https://warp-releases.storage.googleapis.com/stable/v#{version}/Warp.dmg",
+      verified: "warp-releases.storage.googleapis.com"
+  name "Warp"
+  desc "Rust-based terminal"
+  homepage "https://www.warp.dev/"
+
+  livecheck do
+    url "https://storage.googleapis.com/warp-releases/channel_versions.json"
+    regex(/v(\d+(?:\.\d+)+\.stable_\d+)/i)
+  end
+
+  auto_updates true
+
+  app "Warp.app"
+
+  zap trash: [
+    "~/Library/Preferences/dev.warp.Warp-Stable.plist",
+    "~/Library/Application Support/dev.warp.Warp-Stable",
+    "~/Library/Logs/warp.log",
+  ]
+end
