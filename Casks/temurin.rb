57d923d82d437069857f3cfd10d4378312bca944
Shelley Lambert <slambert@gmail.com>
2022-04-05 20:21:58 -0400

    Add aarch64 mac
    Signed-off-by: Shelley Lambert <slambert@gmail.com>

--- Casks/temurin.rb
+++ Casks/temurin.rb
@@ -6,7 +6,7 @@ cask "temurin" do
   if Hardware::CPU.intel?
     sha256 "cdef36a561d7550fb505a9c36fedf363af16a5984c92e74f5abaa25172c12537"
   else
-    sha256 ""
+    sha256 "ac0beb8843f4bc360656ad828fdc9b06937a2ea67cb4d5ef6e3c37c09f5501d4"
   end
 
   url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.csv.first}%2B#{version.csv.second}/OpenJDK#{version.major}U-jdk_#{arch}_mac_hotspot_#{version.csv.first}_#{version.csv.second.major}.pkg",
