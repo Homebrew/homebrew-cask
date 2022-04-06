4889856e38c635274d216b8f43eb2dea41baf8e2
Clément L <porkepix@gmail.com>
2022-04-06 02:50:07 +0200

    Update lens from 5.4.4 to 5.4.5 (#121587)

--- Casks/lens.rb
+++ Casks/lens.rb
@@ -1,12 +1,12 @@
 cask "lens" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  version "5.4.4,20220325.1"
+  version "5.4.5,20220405.1"
 
   if Hardware::CPU.intel?
-    sha256 "ce9d23035038a4a6c9c83a5c27d02b2f6614265fef9d95797e6d68c5b5bd063b"
+    sha256 "e3d6b6f705a5552d8fb9cee2215e9a8822b8fcb0001ed314b7921feb00568b90"
   else
-    sha256 "df4103f6e6d811fe554f6ef19efc9076347c53b0e3286ff0424f448f57ff8c3e"
+    sha256 "c0a66a0eb32395a24f6f4c895cbee47886032594fdb51b47136a673943635401"
   end
 
   url "https://api.k8slens.dev/binaries/Lens-#{version.csv.first}-latest.#{version.csv.second}#{arch}.dmg"
