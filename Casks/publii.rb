c1a0c3206cb1f3c5464523c0ea80a20103521c47
Tin Kyaw <tin@lsvp.com>
2022-04-06 11:21:55 -0700

    Update publii

--- Casks/publii.rb
+++ Casks/publii.rb
@@ -1,6 +1,6 @@
 cask "publii" do
   version "0.38.3"
-  sha256 "24d3dc60779eba8ea366613bc6318e3bb1739984a4281544743a86fce9398f8a"
+  sha256 "d395fcfb83f7a99c40d3c14f8e42d7cd576170ac245f1a68d1a34a7a4e8c3455"
 
   url "https://cdn.getpublii.com/Publii-#{version}.dmg"
   name "Publii"
