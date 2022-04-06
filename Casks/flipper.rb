8c718565d3ddf016c6e3a926a4eb24ff2588ae15
Sean Molenaar <smillerdev@me.com>
2022-04-06 16:45:54 +0200

    Update flipper from 0.141.0 to 0.142.0

--- Casks/flipper.rb
+++ Casks/flipper.rb
@@ -1,6 +1,6 @@
 cask "flipper" do
-  version "0.141.0"
-  sha256 "7241ce8e9a0fa25176d4c35c95fd12ddf68dbd741e832f543eba034a221941f0"
+  version "0.142.0"
+  sha256 "a955981e18971d7283cefececb4e5149571e0cbf4666d3a70cc32d34c68e8107"
 
   url "https://github.com/facebook/flipper/releases/download/v#{version}/Flipper-mac.dmg",
       verified: "github.com/facebook/flipper/"
