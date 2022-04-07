cb76275283c7ea5510179bce0e43e23b668b4038
AkiraFukushima <h3.poteto@gmail.com>
2022-04-08 00:16:19 +0900

    Update whalebird to 4.5.3 (#121650)

--- Casks/whalebird.rb
+++ Casks/whalebird.rb
@@ -1,6 +1,6 @@
 cask "whalebird" do
-  version "4.5.2"
-  sha256 "5f1cfdc32abbcef4872bd7ee77c2fb69a9416a6a06ee64e09ecdf95d8dd90154"
+  version "4.5.3"
+  sha256 "c1bd84e7d92cff2ea4e54ed483589be260e5b644e88d2526a8125b7ac66ccb5c"
 
   url "https://github.com/h3poteto/whalebird-desktop/releases/download/#{version}/Whalebird-#{version}-darwin-universal.dmg",
       verified: "github.com/h3poteto/whalebird-desktop/"
