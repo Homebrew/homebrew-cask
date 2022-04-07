fb2f5cb5d82b8e99f39d738a6f36021ca6788aae
zuisong <378818573@qq.com>
2022-04-07 16:00:31 +0800

    Update electerm from 1.20.10 to 1.21.9 (#121614)
    Co-authored-by: zuisong <com.me@foxmail.com>

--- Casks/electerm.rb
+++ Casks/electerm.rb
@@ -1,12 +1,12 @@
 cask "electerm" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "1.20.10"
+  version "1.21.9"
 
   if Hardware::CPU.intel?
-    sha256 "41dc20386dcf5f3512239bb5fc948eecebd8efda155a023c1e9d728854540c59"
+    sha256 "021b893afbc28912d8060cb305756e186be8288a8c42ecbce4742c3e9b8ccc93"
   else
-    sha256 "52852d991e98f56532f13fe64cf25be90270a1e29492e44bc0c12cfb44d1ad0f"
+    sha256 "f42680507f237258f5b664bbf6cb535072f0d2e67daadee5197a217c64730767"
   end
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac-#{arch}.dmg"
