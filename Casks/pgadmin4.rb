7fb83878bea787ed7a2cdeb85383748d8e611408
leonschmidlin <leon@schmidl.in>
2022-04-07 17:13:46 +0200

    Update pgadmin4 from 6.7 to 6.8 (#121656)
    Co-authored-by: LeonAdmin <leonadmin@MacBook-Pro.local>

--- Casks/pgadmin4.rb
+++ Casks/pgadmin4.rb
@@ -1,6 +1,6 @@
 cask "pgadmin4" do
-  version "6.7"
-  sha256 "39ccc32418c59334fc893d863427697f220f3f9ea8da04450cd65d90107db085"
+  version "6.8"
+  sha256 "0c7ff6b7ed2b868c3b591c92c4cc3127149ffe76d85fc85464c1bbecaa6aa9e4"
 
   url "https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v#{version}/macos/pgadmin4-#{version}.dmg",
       verified: "ftp.postgresql.org/pub/pgadmin/pgadmin4/"
