a0c4c9caf0e9411082a2a6032cc78265047b59a7
Johannes Klein <jo@temka.de>
2022-04-07 17:16:08 +0200

    Update eloston-chromium from 100.0.4896.60-1.1 to 100.0.4896.75-1.1 (#121649)
    Co-authored-by: Johannes Klein <johannes.klein@lineupr.com>

--- Casks/eloston-chromium.rb
+++ Casks/eloston-chromium.rb
@@ -2,8 +2,8 @@ cask "eloston-chromium" do
   arch = Hardware::CPU.intel? ? "x86-64" : "arm64"
 
   if Hardware::CPU.intel?
-    version "100.0.4896.60-1.1,1648783271"
-    sha256 "c856a0793beda8d1e3ad38909b89ea27b213171b8f81c297f8dc642c38f2d1cc"
+    version "100.0.4896.75-1.1,1649306153"
+    sha256 "9a006d8d1d0b755f8e9a50aff38196f55d167f6f0c2bb9667e82e182a60f9d1b"
   else
     version "100.0.4896.60-1.1,1648761745"
     sha256 "0ebbada73656223a0f5ea944c43c70ca18c66266bb7d2e73f56a72dfbbc6e2ae"
