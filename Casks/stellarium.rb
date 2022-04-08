947d9d5718f23acac2e1043060ca492292340142
Amory Meltzer ☀ <Amorymeltzer@gmail.com>
2022-04-08 15:14:29 -0400

    stellarium: update to version 0.22 (#121718)
    Now includes arm64 and x86 versions

--- Casks/stellarium.rb
+++ Casks/stellarium.rb
@@ -1,8 +1,15 @@
 cask "stellarium" do
-  version "0.21.3"
-  sha256 "d6c7eb5770ec53366d73297742a18feda800e4efad664afe2600efc851b3589c"
+  arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 
-  url "https://github.com/Stellarium/stellarium/releases/download/v#{version.major_minor_patch}/Stellarium-#{version}.zip",
+  version "0.22.0"
+
+  if Hardware::CPU.intel?
+    sha256 "8e55d08d87769c2f02d807d0480839560ad00b93eae74ab2d39a1d5a0541b03d"
+  else
+    sha256 "b9fed25a9272de8300155ad656a527e5878fc1e9fd6a091a14f30830442bbd7d"
+  end
+
+  url "https://github.com/Stellarium/stellarium/releases/download/v#{version.major_minor_patch}/Stellarium-#{version}-#{arch}.zip",
       verified: "github.com/Stellarium/stellarium/"
   name "Stellarium"
   desc "Tool to render realistic skies in real time on the screen"
