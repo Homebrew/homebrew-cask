4f3c51e65b6d9526d3682daa0a1a4bd89b9ae91c
Clément L <porkepix@gmail.com>
2022-04-08 21:15:02 +0200

    Update uvtools from 3.2.1 to 3.2.2 (#121720)

--- Casks/uvtools.rb
+++ Casks/uvtools.rb
@@ -1,6 +1,6 @@
 cask "uvtools" do
-  version "3.2.1"
-  sha256 "d4b050e1a357932d8e2be1d437cc88af528332da6ea51213c7eefddc1da4deb6"
+  version "3.2.2"
+  sha256 "a9da1354ba1fd3f5eddfe2e75c5ecd546985600ed7e0e75c804a7877c44d88a1"
 
   url "https://github.com/sn4k3/UVtools/releases/download/v#{version}/UVtools_osx-x64_v#{version}.zip"
   name "UVtools"
