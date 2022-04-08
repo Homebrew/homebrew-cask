f451a9195a93d65f57ae76116f2f9688fae08f40
Clément L <porkepix@gmail.com>
2022-04-08 21:13:20 +0200

    Update pritunl from 1.2.3124.65 to 1.2.3128.59 (#121714)

--- Casks/pritunl.rb
+++ Casks/pritunl.rb
@@ -1,12 +1,12 @@
 cask "pritunl" do
   arch = Hardware::CPU.intel? ? "" : ".arm64"
 
-  version "1.2.3124.65"
+  version "1.2.3128.59"
 
   if Hardware::CPU.intel?
-    sha256 "518a4339380f54be522e686ef57efc8d3762e0b34911dae58a7aa60fb21e4c10"
+    sha256 "55e02a03f8bc4b5acc3214e91976095eae5035b8e0b276774c7df89d3a759063"
   else
-    sha256 "16a41e8ab9b034c6d59854de811ae8b753017ea2ddcf567b5850f3f08785e591"
+    sha256 "30cd469ee1a63ebbdaf724b771d6b029d24c615a82f4a6269870fefa6525de8a"
   end
 
   url "https://github.com/pritunl/pritunl-client-electron/releases/download/#{version}/Pritunl#{arch}.pkg.zip",
