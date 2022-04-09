7908068ed9244657c765558abd5538ffd5faab9f
Luflosi <Luflosi@users.noreply.github.com>
2022-04-09 19:06:43 +0200

    Update ipfs from 0.20.4 to 0.20.5 (#121741)

--- Casks/ipfs.rb
+++ Casks/ipfs.rb
@@ -1,6 +1,6 @@
 cask "ipfs" do
-  version "0.20.4"
-  sha256 "5078eefb80c3002d8e48817b029d72c8465be1fb6194ba85599fc80243b385bf"
+  version "0.20.5"
+  sha256 "bad9909e64e92b907017c97e847569fc8c8d41645eca4e007ebb60ff8037cf70"
 
   url "https://github.com/ipfs-shipyard/ipfs-desktop/releases/download/v#{version}/IPFS-Desktop-#{version}.dmg"
   name "IPFS Desktop"
