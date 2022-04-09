92fbe8cb52c6f3f7e0bf8368fd36ba115458299c
Moulick Aggarwal <moulickaggarwal@gmail.com>
2022-04-10 00:44:15 +0530

    Update macfuse from 4.2.4 to 4.2.5

--- Casks/macfuse.rb
+++ Casks/macfuse.rb
@@ -1,6 +1,6 @@
 cask "macfuse" do
-  version "4.2.4"
-  sha256 "82a2c30b3a7bf56aa2755c0c192fb50d9eecc3fe42505ab4e8679b50306188bd"
+  version "4.2.5"
+  sha256 "a94af08049fe480453e02acb57334f7c8dcc47d68f3f3fe341bf5aadda418dff"
 
   url "https://github.com/osxfuse/osxfuse/releases/download/macfuse-#{version}/macfuse-#{version}.dmg",
       verified: "github.com/osxfuse/osxfuse/"
