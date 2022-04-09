de2796604c1c6c4e4138410b936d2a061ff7d2e1
Yurii Kolesnykov <root@yurikoles.com>
2022-04-09 20:04:31 +0300

    Update badlion-client from 3.6.4 to 3.8.0 (#121758)

--- Casks/badlion-client.rb
+++ Casks/badlion-client.rb
@@ -1,6 +1,6 @@
 cask "badlion-client" do
-  version "3.6.4"
-  sha256 "5df3ebf0d99e944c8a54ccc904984a3d3d071930fd1410b7af623341e39b6782"
+  version "3.8.0"
+  sha256 "1bee231fa918710d46fb66a6bc65ada14d5d825675dcd9d8609f2ddb37440cc1"
 
   url "https://client-updates.badlion.net/Badlion%20Client-#{version}.dmg"
   name "Badlion Client"
