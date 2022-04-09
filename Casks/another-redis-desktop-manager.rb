87ac8150d6f2717464e73e138a790040a21a9ecf
Yurii Kolesnykov <root@yurikoles.com>
2022-04-09 18:23:04 +0300

    Update another-redis-desktop-manager from 1.5.2 to 1.5.5

--- Casks/another-redis-desktop-manager.rb
+++ Casks/another-redis-desktop-manager.rb
@@ -1,12 +1,12 @@
 cask "another-redis-desktop-manager" do
   arch = Hardware::CPU.intel? ? "." : "-M1-arm64-"
 
-  version "1.5.2"
+  version "1.5.5"
 
   if Hardware::CPU.intel?
-    sha256 "a734bd5c095eeb29863a3d4c05b6d1d2c9d1ba64fab7b2f7007a5eff50b50835"
+    sha256 "b4b5ced319af3a048f3e4fc1e48b6f6186cd2aa2bbd17148faa48350911723aa"
   else
-    sha256 "653b977987407b42fd82fb5fe1830583d7d1fbb8bf32b5ae70f3ed96f238fd92"
+    sha256 "00bf73c9195faaca5f4c27d1be7c7baaa5efe5e4903b07768a8cdd82542b6bc8"
   end
 
   url "https://github.com/qishibo/AnotherRedisDesktopManager/releases/download/v#{version}/Another-Redis-Desktop-Manager#{arch}#{version}.dmg"
