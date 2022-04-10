5897c1fe4399da82dbad90b7d5a98b923e1bbcb4
Anuj More <anujmorex@gmail.com>
2022-04-11 01:59:56 +0800

    Auto-fix style
    ```
    brew style --fix Casks/gdevelop.rb
    ```

--- Casks/gdevelop.rb
+++ Casks/gdevelop.rb
@@ -7,11 +7,10 @@ cask "gdevelop" do
   sha256 "be81dc06ba56333cffb95ff86ea64066b1acbddb09ddd2f74a841759e9a00b53"
 
   url "https://github.com/4ian/GDevelop/releases/download/v#{version}/GDevelop-#{major_version}-#{version}.dmg",
-    verified: "github.com/4ian/GDevelop/"
-
+      verified: "github.com/4ian/GDevelop/"
   name "GDevelop"
-  desc "An open-source, cross-platform game engine designed to be used by everyone"
-  homepage "https://gdevelop.io"
+  desc "Open-source, cross-platform game engine designed to be used by everyone"
+  homepage "https://gdevelop.io/"
 
   app "GDevelop #{major_version}.app"
 end
