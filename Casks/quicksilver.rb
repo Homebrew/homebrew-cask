4f1d4fedba20ab389897483372b3ee7caf054eea
Tim Visher <tim.visher@gmail.com>
2022-04-11 09:13:05 -0400

    Update quicksilver from 2.0.0 to 2.0.3 (#121662)
    * Update quicksilver from 2.0.0 to 2.0.2
    
    * to be squashed: Use github releases for livecheck
    
    * Update Casks/quicksilver.rb
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    
    * Revert "Update Casks/quicksilver.rb"
    
    This reverts commit e7a49792094d068e03e6ba7ec615703da6ae0c4c.
    
    * Update quicksilver from 2.0.2 to 2.0.3
    
    * Update quicksilver.rb
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/quicksilver.rb
+++ Casks/quicksilver.rb
@@ -1,15 +1,16 @@
 cask "quicksilver" do
-  version "2.0.0"
-  sha256 "a0a60697bbd6960604a1f7b199f5273ea0fa28d2d41d27c0df398ea289fcddcb"
+  version "2.0.3"
+  sha256 "cf7f7bc8fac114c9931049715f88297705f4161ffe3d473b261435574ca7b82a"
 
-  url "https://qsapp.com/archives/downloads/Quicksilver%20#{version}.dmg"
+  url "https://github.com/quicksilver/Quicksilver/releases/download/v#{version}/Quicksilver.#{version}.dmg",
+      verified: "github.com/quicksilver/Quicksilver"
   name "Quicksilver"
   desc "Productivity application"
   homepage "https://qsapp.com/"
 
   livecheck do
-    url "https://qsapp.com/archives/"
-    regex(%r{href=.*?/Quicksilver%20(\d+(?:\.\d+)*)\.dmg}i)
+    url :url
+    strategy :github_latest
   end
 
   auto_updates true
