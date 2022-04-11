f5d967a7f208ecb70f6954ca4ef92015f2d6b5ec
Anuj More <anujmorex@gmail.com>
2022-04-11 21:31:36 +0800

    Remove major_version method and replace with version.major

--- Casks/gdevelop.rb
+++ Casks/gdevelop.rb
@@ -1,16 +1,12 @@
 cask "gdevelop" do
-  def major_version
-    "5"
-  end
-
   version "5.0.131"
   sha256 "be81dc06ba56333cffb95ff86ea64066b1acbddb09ddd2f74a841759e9a00b53"
 
-  url "https://github.com/4ian/GDevelop/releases/download/v#{version}/GDevelop-#{major_version}-#{version}.dmg",
+  url "https://github.com/4ian/GDevelop/releases/download/v#{version}/GDevelop-#{version.major}-#{version}.dmg",
       verified: "github.com/4ian/GDevelop/"
   name "GDevelop"
   desc "Open-source, cross-platform game engine designed to be used by everyone"
   homepage "https://gdevelop.io/"
 
-  app "GDevelop #{major_version}.app"
+  app "GDevelop #{version.major}.app"
 end
