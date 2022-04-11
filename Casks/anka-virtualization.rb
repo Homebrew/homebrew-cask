c805010920cc5c4f4e983fb927a8397c982c02b2
Nathan Pierce <norsegaud@gmail.com>
2022-04-11 15:02:30 -0400

    quick fix

--- Casks/anka-virtualization.rb
+++ Casks/anka-virtualization.rb
@@ -18,7 +18,7 @@ cask "anka-virtualization" do
   pkg "Anka-#{version}.pkg"
 
   uninstall launchctl: [
-    "com.veertu.anka.guestaddons.pkg"
+    "com.veertu.anka.guestaddons.pkg",
     "com.veertu.anka.agent.pkg",
     "com.veertu.anka.ankakbd",
     "com.veertu.anka.ankanetd",
