9ca3bb7018d3cc7c2e897a904c2f685b1771e1ac
Willie Sana <wsana@tenable.com>
2022-04-11 18:28:25 +0000

    updated sha

--- Casks/accurics.rb
+++ Casks/accurics.rb
@@ -1,6 +1,6 @@
 cask "accurics" do
   version "1.0.35"
-  sha256 "cb7f53ef9bce8de5ee333aab501e90751a2d67c403b11adde3e10248ecf9362e"
+  sha256 "ea1c33ec5b2c97880f024564f8f046e51de9e92f2e7ad02167c23aae3a6fc2ff"
 
   url "https://downloads.accurics.com/cli/#{version}/accurics-cli.dmg",
       user_agent: :fake
