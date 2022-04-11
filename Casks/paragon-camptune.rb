018e52a2aaeecacd0819e65bb47daa2415172c2e
Yurii Kolesnykov <root@yurikoles.com>
2022-04-11 16:10:22 +0300

    paragon-camptune: update uninstall (#121819)
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/paragon-camptune.rb
+++ Casks/paragon-camptune.rb
@@ -18,6 +18,13 @@ cask "paragon-camptune" do
 
   app "Paragon CampTune.app"
 
+  uninstall delete: [
+    "/Library/Application Support/Paragon Software/com.paragon-software.camptunex.helper",
+    "/Library/Application Support/Paragon Software/com.paragon-software.camptunex.helper-data",
+    "/Library/Application Support/Paragon Software/uc.camptunex",
+  ],
+            rmdir:  "/Library/Application Support/Paragon Software/"
+
   zap trash: [
     "~/Library/Application Support/com.paragon-software.camptunex",
     "~/Library/HTTPStorages/com.paragon-software.camptunex",
