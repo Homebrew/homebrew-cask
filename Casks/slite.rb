36a81b236cd5dc9c636a798a3a6c4abe63e39c62
Arnaud Rinquin <me@arnaud.rocks>
2022-04-11 15:38:07 +0200

    Update slite from 1.2.4 to 1.2.10 (#121829)
    * Update slite from 1.2.10 to 1.2.10
    
    * update url, livecheck
    
    Co-authored-by: admin <admin@admins-mbp.home>
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/slite.rb
+++ Casks/slite.rb
@@ -1,15 +1,23 @@
 cask "slite" do
-  version "64"
-  sha256 :no_check
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  url "https://download.slite.com/mac"
+  version "1.2.11"
+
+  if Hardware::CPU.intel?
+    sha256 "6f925e8ff26dbc89e1344e4e07a93304000c7558ff68537b644db13788d0e4a6"
+  else
+    sha256 "fe5df18780cb0c51a4d5997423d3ddc55a0b3e60c261451e01dd4d39d63cfd81"
+  end
+
+  url "https://download.todesktop.com/20062929x31pwfi/Slite%20#{version}-#{arch}-mac.zip",
+      verified: "download.todesktop.com/20062929x31pwfi/"
   name "Slite"
   desc "Team communication and collaboration software"
   homepage "https://slite.com/"
 
   livecheck do
-    url "https://download.slite.com/mac"
-    strategy :header_match
+    url "https://download.todesktop.com/20062929x31pwfi/latest-mac.yml"
+    strategy :electron_builder
   end
 
   app "Slite.app"
