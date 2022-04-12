dad1a4f8f599c6d46119ede7d008460604fcc2ff
hyuraku <32809703+hyuraku@users.noreply.github.com>
2022-04-12 21:46:30 +0900

    Update recaf from 2.21.12 to 2.21.13 (#121863)

--- Casks/recaf.rb
+++ Casks/recaf.rb
@@ -1,6 +1,6 @@
 cask "recaf" do
-  version "2.21.12"
-  sha256 "523599424c776d8a2cad629cb4234833b217ef1944542919cd197916b58aa6ee"
+  version "2.21.13"
+  sha256 "9d6cff1f9d4a7363027f53f4e85c8b74e235a2884463b7475fc1b83aee2d000e"
 
   url "https://github.com/Col-E/Recaf/releases/download/#{version}/recaf-#{version}-J8-jar-with-dependencies.jar",
       verified: "github.com/Col-E/Recaf/"
