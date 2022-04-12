1665dd4cf54d32404d89ccb11366260b91a189a5
Bevan Kay <email@bevankay.me>
2022-04-12 22:29:10 +1000

    update livecheck, version url

--- Casks/youdaonote.rb
+++ Casks/youdaonote.rb
@@ -1,13 +1,18 @@
 cask "youdaonote" do
   version "7.0.10"
-  sha256 :no_check
+  sha256 "1234"
 
-  url "https://artifact.lx.netease.com/download/ynote-electron/%E6%9C%89%E9%81%93%E4%BA%91%E7%AC%94%E8%AE%B0.dmg",
+  url "https://artifact.lx.netease.com/download/ynote-electron/%E6%9C%89%E9%81%93%E4%BA%91%E7%AC%94%E8%AE%B0-#{version}.zip",
       verified: "https://artifact.lx.netease.com/download/ynote-electron/"
   name "youdaonote"
   name "有道云笔记"
   desc "Multi-platform note application"
   homepage "https://note.youdao.com/"
+
+  livecheck do
+    url "https://artifact.lx.netease.com/download/ynote-electron/latest-mac.yml"
+    strategy :electron_builder
+  end
 
   app "有道云笔记.app"
 
