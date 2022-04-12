17cd3b055a438645c94e513ba31f021c376da6e1
Jackson Chen <50755746+JacksonChen666@users.noreply.github.com>
2022-04-12 09:42:55 +0200

    Update ricochet-refresh from 3.0.10 to 3.0.11 (#121843)

--- Casks/ricochet-refresh.rb
+++ Casks/ricochet-refresh.rb
@@ -1,6 +1,6 @@
 cask "ricochet-refresh" do
-  version "3.0.10"
-  sha256 "c9751b6f78365777f083d619aa36947bc83ae9043b4b74f099f884b70d8398ef"
+  version "3.0.11"
+  sha256 "dc1145c0e05709eb01b2dfdf41b8a0603516889aad4b359d5fe5d09c072f4d0e"
 
   url "https://github.com/blueprint-freespeech/ricochet-refresh/releases/download/v#{version}-release/ricochet-refresh-#{version}-macos-x86_64.dmg",
       verified: "github.com/blueprint-freespeech/ricochet-refresh/"
