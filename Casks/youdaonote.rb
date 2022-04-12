2deb90ff0193dad636713ecf8fd8a83a3b86ac19
warrentak <warrentak18@gmail.com>
2022-04-12 18:20:48 +0800

    update youdaonote to the lastest version(v7.0.10).

--- Casks/youdaonote.rb
+++ Casks/youdaonote.rb
@@ -4,7 +4,6 @@ cask "youdaonote" do
 
   url "https://artifact.lx.netease.com/download/ynote-electron/%E6%9C%89%E9%81%93%E4%BA%91%E7%AC%94%E8%AE%B0.dmg",
       verified: "https://artifact.lx.netease.com/download/ynote-electron/"
-
   name "youdaonote"
   name "有道云笔记"
   desc "Multi-platform note application"
@@ -13,11 +12,7 @@ cask "youdaonote" do
   app "有道云笔记.app"
 
   zap trash: [
-    "~/Library/Caches/com.crashlytics.data/com.youdao.YoudaoDict",
-    "~/Library/Caches/com.youdao.YoudaoDict",
-    "~/Library/Caches/io.fabric.sdk.mac.data/com.youdao.YoudaoDict",
     "~/Library/Containers/com.youdao.note.YoudaoNoteMac",
     "~/Library/Saved Application State/com.youdao.YoudaoDict.savedState",
-    "~/Library/com.youdao.YoudaoDict",
   ]
 end
