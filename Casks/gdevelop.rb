a69ecd20aea561fbe0b5da9f9d373c8a33e49c91
Anuj More <anujmorex@gmail.com>
2022-04-13 16:33:36 +0800

    Add GDevelop 5 cask (#121796)
    * Add GDevelop 5 cask
    
    * Auto-fix style
    
    ```
    brew style --fix Casks/gdevelop.rb
    ```
    
    * Remove major_version method and replace with version.major
    
    * Add zap stanza
    
    * Update gdevelop.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

---
+++ Casks/gdevelop.rb
@@ -0,0 +1,19 @@
+cask "gdevelop" do
+  version "5.0.131"
+  sha256 "be81dc06ba56333cffb95ff86ea64066b1acbddb09ddd2f74a841759e9a00b53"
+
+  url "https://github.com/4ian/GDevelop/releases/download/v#{version}/GDevelop-#{version.major}-#{version}.dmg",
+      verified: "github.com/4ian/GDevelop/"
+  name "GDevelop"
+  desc "Open-source, cross-platform game engine designed to be used by everyone"
+  homepage "https://gdevelop.io/"
+
+  app "GDevelop #{version.major}.app"
+
+  zap trash: [
+    "~/Library/Application Support/GDevelop #{version.major}",
+    "~/Library/Logs/GDevelop #{version.major}",
+    "~/Library/Preferences/com.gdevelop-app.ide.plist",
+    "~/Library/Saved Application State/com.gdevelop-app.ide.savedState",
+  ]
+end
