2ad79aaa8c42d6e16413b18fa8bf29cde61c2638
Porkepix <porkepix@gmail.com>
2022-04-13 19:14:08 +0200

    Update codeql from 2.8.4 to 2.8.5

--- Casks/codeql.rb
+++ Casks/codeql.rb
@@ -1,6 +1,6 @@
 cask "codeql" do
-  version "2.8.4"
-  sha256 "fb8a2562bbca9601a6cf4bbad079abdd6b067075d6816c20698cd68f8810e642"
+  version "2.8.5"
+  sha256 "eff5e16b68382f2b918a4be03a9701df70b68b3a1db394aba0edd61809e11f79"
 
   url "https://github.com/github/codeql-cli-binaries/releases/download/v#{version}/codeql-osx64.zip"
   name "CodeQL"
