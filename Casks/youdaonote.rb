7c9fa95607fc3331f875c793506798970f64b9f8
warrentak <warrentak18@gmail.com>
2022-04-13 10:36:43 +0800

    commit modified cask

--- Casks/youdaonote.rb
+++ Casks/youdaonote.rb
@@ -1,13 +1,18 @@
 cask "youdaonote" do
   version "7.0.10"
-  sha256 :no_check
+  sha256 "505de6377dd8f7af881c837779de16b9af34b80534c223fac16e5b7e126ab028"
 
-  url "https://artifact.lx.netease.com/download/ynote-electron/%E6%9C%89%E9%81%93%E4%BA%91%E7%AC%94%E8%AE%B0.dmg",
-      verified: "https://artifact.lx.netease.com/download/ynote-electron/"
+  url "https://artifact.lx.netease.com/download/ynote-electron/%E6%9C%89%E9%81%93%E4%BA%91%E7%AC%94%E8%AE%B0-#{version}.zip",
+      user_agent: :fake,
+      verified:   "https://artifact.lx.netease.com/download/ynote-electron/"
   name "youdaonote"
   name "有道云笔记"
   desc "Multi-platform note application"
   homepage "https://note.youdao.com/"
+
+  livecheck do
+    skip "No version information available"
+  end
 
   app "有道云笔记.app"
 
