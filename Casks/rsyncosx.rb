046688294973d591336f4873a170514bd68d0215
Thomas Evensen <thomeven@gmail.com>
2022-04-13 10:49:55 +0200

    Update rsyncosx.rb

--- Casks/rsyncosx.rb
+++ Casks/rsyncosx.rb
@@ -1,6 +1,6 @@
 cask "rsyncosx" do
-  version "6.7.1"
-  sha256 "24a5f26e1d29ee915b8549525a32998c55cba86d24be4beb6ec4faf4b9081b38"
+  version "6.7.2"
+  sha256 "64bbfc22c0d36bf8742d6ad7852b02be317a11372852ce532a907584898ee14b"
 
   url "https://github.com/rsyncOSX/RsyncOSX/releases/download/v#{version}/RsyncOSX.#{version}.dmg"
   name "RsyncOSX"
