6a9905e69b531bb5d388757cd950a052e67e6b39
tuzi3040 <28290621+tuzi3040@users.noreply.github.com>
2022-04-13 14:13:29 +0800

    Update dotnet-sdk from 6.0.201 to 6.0.202 (#121926)

--- Casks/dotnet-sdk.rb
+++ Casks/dotnet-sdk.rb
@@ -2,11 +2,11 @@ cask "dotnet-sdk" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
   if Hardware::CPU.intel?
-    version "6.0.201,a5e0f5da-6088-451c-a341-b751c0d418c7,9fe3a31273888fe23cbe71cac32fa35c"
-    sha256 "a6199cee00bb381b00847cf2e4e7a2192935e2a03c8892a3368a5b3479f3868f"
+    version "6.0.202,4332c16b-5a65-4adf-b25d-f6a46ef2b335,1a1edc2dab547161e2448390c3d4f56d"
+    sha256 "e3378b6c90c6ae98b026db14ae7a7703dc01eae79c00d5e455cd73365a54c3f9"
   else
-    version "6.0.201,2e20d654-1371-4c8f-a0dd-e81bac07549e,7b63667ab1941110bf9e684dc66b590d"
-    sha256 "a219339edb3156c84bfc684efc5a1061d528b2e10b870763bb1119f925249135"
+    version "6.0.202,605dfd20-32ac-470a-b366-cbb293ce4cfe,9c1eec28d0e6068721026a5f77af2e60"
+    sha256 "7d5c2f37cfc2fee080deac1d972f8413c84d97532157a346bac72f607b91a7c5"
   end
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv.second}/#{version.csv.third}/dotnet-sdk-#{version.csv.first}-osx-#{arch}.pkg"
