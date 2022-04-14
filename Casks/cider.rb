9c1bd1862b21c00e462e3b117632b8ddf15315fd
Piotrek Rybiec <piotrrybiec28@gmail.com>
2022-04-14 18:51:11 +0200

    fix style requirements

--- Casks/cider.rb
+++ Casks/cider.rb
@@ -1,14 +1,13 @@
 cask "cider" do
   version "1.14.3"
-  sha256 "bc99ba04122f17cd0a8f4cd6413b13d83ec369d4c3a21cedb75fd6990144618f"
-   
-  url "https://github.com/ciderapp/Cider/releases/download/macos-beta/Cider.dmg"
+  sha256 :no_check
+
+  url "https://github.com/ciderapp/Cider/releases/download/macos-beta/Cider.dmg", verified: "https://github.com/ciderapp/Cider"
   name "Cider"
-  desc "An open source and community oriented Apple Music client for Windows, Linux, macOS, and more."
-  homepage "https://cider.sh"
+  desc "Open source and community oriented Apple Music"
+  homepage "https://cider.sh/"
 
   auto_updates true
 
   app "Cider.app"
 end
-
