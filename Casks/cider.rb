1e1b993ec18c27ce8587dcb72c853a42e82a687f
Piotrek Rybiec <piotrrybiec28@gmail.com>
2022-04-14 18:36:45 +0200

    Create a cask for Cider

---
+++ Casks/cider.rb
@@ -0,0 +1,14 @@
+cask "cider" do
+  version "beta"
+  sha256 "bc99ba04122f17cd0a8f4cd6413b13d83ec369d4c3a21cedb75fd6990144618f"
+   
+  url "https://github.com/ciderapp/Cider/releases/download/macos-beta/Cider.dmg"
+  name "Cider"
+  desc "An open source and community oriented Apple Music client for Windows, Linux, macOS, and more."
+  homepage "https://cider.sh"
+
+  auto_updates true
+
+  app "Cider.app"
+end
+
