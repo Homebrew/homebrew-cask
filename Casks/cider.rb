5f008f26eac1f3b6311568148680fc5d9891da35
a1eaiactaest <55894077+a1eaiactaest@users.noreply.github.com>
2022-04-14 20:15:07 +0200

    version

--- Casks/cider.rb
+++ Casks/cider.rb
@@ -1,5 +1,5 @@
 cask "cider" do
-  version "1.14.3"
+  version "1.4.3"
   sha256 :no_check
 
   url "https://github.com/ciderapp/Cider/releases/download/macos-beta/Cider.dmg", verified: "https://github.com/ciderapp/Cider"
