5cc364cf9ead21fd20694efea10305abb0df1094
otkd <7527203+otkd@users.noreply.github.com>
2022-04-14 12:51:21 -0400

    Update nym-wallet.app url
    Signed-off-by: otkd <7527203+otkd@users.noreply.github.com>

--- Casks/nym-wallet.rb
+++ Casks/nym-wallet.rb
@@ -2,7 +2,7 @@ cask "nym-wallet" do
   version "1.0.2"
   sha256 "9f5a58e2e06645fce48aa3dd439b0557385b95c0c270fe2b941d36928c2efbbb"
 
-  url "https://github.com/nymtech/nym/releases/download/nym-wallet-#{version}/nym-wallet_#{version}_x64_macos_11.dmg",
+  url "https://github.com/nymtech/nym/releases/download/nym-wallet-v#{version}/nym-wallet_#{version}_x64_macos_11.dmg",
       verified: "github.com/nymtech/nym/"
   name "Nym Wallet"
   desc "Use to bond coins or interact with your Nym node or delegate to other nodes"
