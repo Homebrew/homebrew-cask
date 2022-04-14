899626db43528ddca6a63867ac23b5dd62639303
wjlmelody <30622992+wjlmelody@users.noreply.github.com>
2022-04-14 21:28:07 +0800

    Update wechatwork.rb

--- Casks/wechatwork.rb
+++ Casks/wechatwork.rb
@@ -1,25 +1,25 @@
 cask "wechatwork" do
-    if Hardware::CPU.intel?
-        version "4.0.3.90492"
-        sha256 "c7306cf208cbdc1e4b0dbcfd96f981d04dff5875616ada7d1d369c214be39a4e"
-    else
-        version "4.0.3.99066"
-        sha256 "16ef032ea837b86d5010abdb3a371f70402fd733d3aa1b465a520fe1011999d4"
-    end
+  if Hardware::CPU.intel?
+    version "4.0.3.90492"
+    sha256 "c7306cf208cbdc1e4b0dbcfd96f981d04dff5875616ada7d1d369c214be39a4e"
+  else
+    version "4.0.3.99066"
+    sha256 "16ef032ea837b86d5010abdb3a371f70402fd733d3aa1b465a520fe1011999d4"
+  end
 
-    url "https://dldir1.qq.com/foxmail/wecom-mac/update/WeCom_#{version}.dmg"
-    name "WeChat Work"
-    name "企业微信"
-    desc "Messaging and calling application"
-    homepage "https://work.weixin.qq.com/"
+  url "https://dldir1.qq.com/foxmail/wecom-mac/update/WeCom_#{version}.dmg"
+  name "WeChat Work"
+  name "企业微信"
+  desc "Messaging and calling application"
+  homepage "https://work.weixin.qq.com/"
   
-    livecheck do
-        skip "No version information available"
-    end
+  livecheck do
+    skip "No version information available"
+  end
   
-    auto_updates true
+  auto_updates true
   
-    app "企业微信.app"
+  app "企业微信.app"
   
-    uninstall quit: "com.tencent.WeWorkMac"
+  uninstall quit: "com.tencent.WeWorkMac"
 end
