022d4b8db0bfe0a9335d563283b57f9a0c7e0aad
Ray Yin <ray.yin@hobsons.com>
2022-04-14 10:27:10 -0400

    Update retroarch from 1.10.3 to 1.10.3

--- Casks/retroarch.rb
+++ Casks/retroarch.rb
@@ -1,6 +1,6 @@
 cask "retroarch" do
   version "1.10.3"
-  sha256 "d003c7b2fe9199337bcbafd868c2eddb18df69ecfdf1afb25b39102f3e6001c8"
+  sha256 "e5b15a0fa7e9379b91d75abe4f3a92fe5c626bd1a7c24c07c9b91f6cf952969b"
 
   url "https://buildbot.libretro.com/stable/#{version}/apple/osx/x86_64/RetroArch.dmg"
   name "RetroArch"
