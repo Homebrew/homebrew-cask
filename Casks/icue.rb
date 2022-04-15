109288a0cf16c599f26b6942eb7516597ae7291c
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-04-15 23:30:21 +0800

    Add iCUE v4.22.203

---
+++ Casks/icue.rb
@@ -0,0 +1,24 @@
+cask "icue" do
+  version "4.22.203"
+  sha256 "526c4d4b220d83574c80a320a9b94d9a06aa67182054fa4933e7b22805ee4d65"
+
+  url "https://downloads.corsair.com/Files/CUE/iCUE-#{version}-release.dmg"
+  name "com.corsair.cue.3"
+  desc "Orchestrate compatible CORSAIR peripherals"
+  homepage "https://www.corsair.com/uk/en/icue-mac"
+
+  livecheck do
+    skip "No version information available"
+  end
+
+  auto_updates true
+
+  app "iCUE.app"
+
+  uninstall quit: "com.corsair.cue.3"
+
+  zap trash: [
+    "~/Library/Preferences/com.corsair.cue.3.plist",
+    "~/Library/Saved Application State/com.corsair.cue.3.savedState",
+  ]
+end
