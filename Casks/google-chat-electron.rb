0b227d81ea71fce3e5bcbf12d334804173e0c94f
Adam Coulter <ads@me.com>
2022-04-15 16:26:28 +1000

    [google-chat-electron] add apple silicon support (#121925)

--- Casks/google-chat-electron.rb
+++ Casks/google-chat-electron.rb
@@ -1,8 +1,15 @@
 cask "google-chat-electron" do
   version "2.15.0"
-  sha256 "a757dc557990f32a9be8b21fd81995748e8a2ecc56c772681a3cb427000986fb"
 
-  url "https://github.com/ankurk91/google-chat-electron/releases/download/#{version}/google-chat-electron-#{version}-darwin-x64.zip"
+  if Hardware::CPU.intel?
+    sha256 "a757dc557990f32a9be8b21fd81995748e8a2ecc56c772681a3cb427000986fb"
+    arch = "darwin-x64"
+  else
+    sha256 "aadb3e0db0053330fa0543556781d28dc6ca4e32e0a5060b18778e6d5c346cbd"
+    arch = "darwin-arm64"
+  end
+
+  url "https://github.com/ankurk91/google-chat-electron/releases/download/#{version}/google-chat-electron-#{version}-#{arch}.zip"
   name "google-chat-electron"
   desc "Standalone app for Google Chat"
   homepage "https://github.com/ankurk91/google-chat-electron"
