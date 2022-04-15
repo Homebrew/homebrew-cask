d24ba61175d54b0b4ee7494f4dc53003d81ce375
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-04-15 11:15:09 +0800

    Update i4tools.rb

--- Casks/i4tools.rb
+++ Casks/i4tools.rb
@@ -2,7 +2,7 @@ cask "i4tools" do
   version "1.13.005"
   sha256 "0d2cf2a8c23b9068fab8e8b71c872104df87200539dd942f8bc7cb6d65c4c3dc"
 
-  url "http://d.updater.i4.cn/i4mac/20220413/0/i4Tools_#{version}_#{version}.dmg"
+  url "http://d-updater.i4.cn/i4mac/20220413/0/i4Tools_#{version}_#{version}.dmg"
 
   name "i4Tools"
   name "爱思助手"
