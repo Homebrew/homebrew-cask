6780a9718b77b0c49509bba3b8a5a0cc78d5a77d
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-04-16 23:48:12 +0800

    Update cnkiexpress.rb

--- Casks/cnkiexpress.rb
+++ Casks/cnkiexpress.rb
@@ -3,7 +3,8 @@ cask "cnkiexpress" do
   sha256 "0d2459f85d7949277545a869eba5fe18ab2d0273cfd3291a690ff8a5a3942cd3"
 
   url "https://download.cnki.net/cnkiexpress/\%E5\%85\%A8\%E7\%90\%83\%E5\%AD\%A6\%E6\%9C\%AF\%E5\%BF\%AB\%E6\%8A\%A5-#{version}.dmg"
-  name "com.cnki.cnkiexpress"
+  name "cnkiexpress"
+  name "全球学术快报"
   desc "Academic application"
   homepage "https://cajviewer.cnki.net/index.html"
 
