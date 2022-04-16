3417b47e01aefdf88a9fb0b8054d45bbcea01a92
Jameel Al-Aziz <me@jalaziz.io>
2022-04-15 19:02:50 -0700

    Add Leapp arm64 support

--- Casks/leapp.rb
+++ Casks/leapp.rb
@@ -1,8 +1,15 @@
 cask "leapp" do
-  version "0.11.0"
-  sha256 "d8395dce82ee759ead020212d782abcd56ceaaa88d37ba875e3d13589c7203bf"
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  url "https://asset.noovolari.com/#{version}/Leapp-#{version}.dmg",
+  version "0.11.0"
+
+  if Hardware::CPU.intel?
+    sha256 "d8395dce82ee759ead020212d782abcd56ceaaa88d37ba875e3d13589c7203bf"
+  else
+    sha256 "51021c3b4e005122e4827d7c2faa4770fa3e7d6ce6b91840ecf3cd9fe93f2ece"
+  end
+
+  url "https://asset.noovolari.com/#{version}/Leapp-#{version}#{arch}.dmg",
       verified: "asset.noovolari.com/"
   name "Leapp"
   desc "Cloud credentials manager"
