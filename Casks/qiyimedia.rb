f2d98766be799a14fb8455683adec6b405ad25e1
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-04-17 16:06:34 +0800

    Update qiyimedia from 13.3.5 to 13.4.0

--- Casks/qiyimedia.rb
+++ Casks/qiyimedia.rb
@@ -1,5 +1,5 @@
 cask "qiyimedia" do
-  version "13.3.5"
+  version "13.4.0"
   sha256 :no_check
 
   url "https://static-d.iqiyi.com/ext/common/iQIYIMedia_271.dmg"
@@ -18,10 +18,7 @@ cask "qiyimedia" do
   app "爱奇艺.app"
 
   zap trash: [
-    "~/Library/Application Scripts/com.iqiyi.player.QYWidget",
     "~/Library/Application Scripts/com.iqiyi.player",
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.iqiyi.player.sfl*",
-    "~/Library/Containers/com.iqiyi.player.QYWidget",
-    "~/Library/Group Containers/group.com.qiyi",
+    "~/Library/Containers/com.iqiyi.player",
   ]
 end
