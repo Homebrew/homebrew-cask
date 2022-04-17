931b770ee774ee237446f69e90598ab9c5a38829
Kenneth Chew <kenneth.c0@protonmail.com>
2022-04-17 19:41:55 -0400

    Update polymc from 1.1.1 to 1.2.0

--- Casks/polymc.rb
+++ Casks/polymc.rb
@@ -1,6 +1,6 @@
 cask "polymc" do
-  version "1.1.1"
-  sha256 "5f40a9db742b1910825f88980c24e5bb40673b127e5794a4d09729356e5a9778"
+  version "1.2.0"
+  sha256 "1ea6755f689cdef6273582976e1e712ee3cefbdcc22e859e2715c08464595e23"
 
   url "https://github.com/PolyMC/PolyMC/releases/download/#{version}/PolyMC-macOS-#{version}.tar.gz",
       verified: "github.com/PolyMC/PolyMC/"
@@ -8,7 +8,7 @@ cask "polymc" do
   desc "Minecraft launcher"
   homepage "https://polymc.org/"
 
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :high_sierra"
 
   app "PolyMC.app"
 
