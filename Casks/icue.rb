da28ee0e19111a9be8323d4c5b85d7f016c188be
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-04-17 23:22:52 +0800

    Update icue.rb

--- Casks/icue.rb
+++ Casks/icue.rb
@@ -9,12 +9,7 @@ cask "icue" do
 
   livecheck do
     url "https://www.corsair.com/uk/en/downloads"
-    strategy :page_match do |page|
-      match = page.match(%r{(?<=data-url="https://downloads.corsair.com/Files/CUE/iCUE-)(\d+(?:\.\d+)+)(?=-release.dmg")}i)
-      next if match.blank?
-
-      "#{match[1]}"
-    end
+    regex(%r{(?<=data-url="https://downloads.corsair.com/Files/CUE/iCUE-)(4(?:\.\d+)+)(?=-release.dmg")}i)
   end
 
   auto_updates true
