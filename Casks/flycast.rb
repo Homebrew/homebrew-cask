bca2fb892449a81c67cc16b071cec749d537349d
Depal1 <47154119+Depal1@users.noreply.github.com>
2022-04-18 04:04:33 -0600

    Update flycast from 1.2 to 1.3 (#122083)

--- Casks/flycast.rb
+++ Casks/flycast.rb
@@ -1,6 +1,6 @@
 cask "flycast" do
-  version "1.2"
-  sha256 "144b0b603bbd7bdc707cd9839ca550d665c62a1f4d1c09181ab02fc641058f89"
+  version "1.3"
+  sha256 "492682b106b2ff44508e2697975316cbabf1758da90edfc62af0dc2be178e452"
 
   url "https://github.com/flyinghead/flycast/releases/download/v#{version}/flycast-macOS-#{version}.zip"
   name "Flycast"
