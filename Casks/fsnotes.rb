69854b6654da05b15aa9bd32f53cbebdaded1515
Balu <tw@b-a-l-u.de>
2022-04-18 15:12:47 +0200

    Update fsnotes from 5.3.8 to 5.3.9 (#122122)
    Co-authored-by: Thomas Walter <tw@ba.lu>

--- Casks/fsnotes.rb
+++ Casks/fsnotes.rb
@@ -1,6 +1,6 @@
 cask "fsnotes" do
-  version "5.3.8"
-  sha256 "353ae5a53375ab55388511e2e98f4d49eb8c875c581f25de8b3d90592bba3f76"
+  version "5.3.9"
+  sha256 "72e298852bc0e0016e3af038a6e197220a41b1e10718d5bcf4af57e1c17acd9b"
 
   url "https://github.com/glushchenko/fsnotes/releases/download/#{version}/FSNotes_#{version}.zip",
       verified: "github.com/glushchenko/fsnotes/"
