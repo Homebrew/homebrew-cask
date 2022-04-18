7adf46b4e43923e70a7a7ffc4ecf6ee5da30e0d8
lipu <59558393+lipu-ted@users.noreply.github.com>
2022-04-18 23:12:26 +0200

    Update lx-music from 1.19.0 to 1.20.0 (#122159)

--- Casks/lx-music.rb
+++ Casks/lx-music.rb
@@ -1,12 +1,12 @@
 cask "lx-music" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  version "1.19.0"
+  version "1.20.0"
 
   if Hardware::CPU.intel?
-    sha256 "5b74b064cef37e3baa60ef53e49fa77d64e21dd3514ab477bf45b27310f812a9"
+    sha256 "50f6e5fa2124f6872ddaa0291f742e93cefd8d83560c9bf41eb9578f3cf3ed08"
   else
-    sha256 "cec46d809868ac7dcded4f5237ab4a606216c2b232cefe8a03841114fe90175f"
+    sha256 "f5574a6ada5a0652357eb1fbfd9e2cc7d326d25cdab4d76f9bff660992fed1b2"
   end
 
   url "https://github.com/lyswhut/lx-music-desktop/releases/download/v#{version}/lx-music-desktop-#{version}#{arch}.dmg"
