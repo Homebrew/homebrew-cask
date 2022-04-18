beaa74cde66b40a9a1b949ef7a1a34dccf85b986
Reno <25192197+singularitti@users.noreply.github.com>
2022-04-18 06:23:19 -0400

    Update keysmith from 1.18.0 to 1.18.1 (#122108)

--- Casks/keysmith.rb
+++ Casks/keysmith.rb
@@ -1,6 +1,6 @@
 cask "keysmith" do
-  version "1.18.0,93"
-  sha256 "1c49037d93448228bed33304a89bdd0a530112042f390860ee8a6a10a715aa5f"
+  version "1.18.1,94"
+  sha256 "687721e156990564c0864aa84980f29314a8e703f259cfebd9859d9b6dd8fd26"
 
   url "https://keysmith.app/versions/Keysmith-#{version.csv.first}.dmg"
   name "Keysmith"
