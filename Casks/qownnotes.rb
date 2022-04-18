58c69a46f367ce9effe44ae2c686e1f088052bd9
Christian Kraemer <christian.kraemer@gmail.com>
2022-04-18 12:04:51 +0200

    Update qownnotes from 22.4.0 to 22.4.1 (#122084)

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,6 +1,6 @@
 cask "qownnotes" do
-  version "22.4.0"
-  sha256 "d0fbcb87bc7fa5dbdee22ea19aee8322d55222d74c0590021de46cda84b8b474"
+  version "22.4.1"
+  sha256 "530a338e60305c32ec9f2aca1256bfa57cf9c2b69106526cd9843aebad153726"
 
   url "https://github.com/pbek/QOwnNotes/releases/download/v#{version}/QOwnNotes.dmg",
       verified: "github.com/pbek/QOwnNotes/"
