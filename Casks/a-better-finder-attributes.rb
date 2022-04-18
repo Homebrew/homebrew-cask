4520905921b0d24232c42dd4e3a43e14eb593d21
Reno <25192197+singularitti@users.noreply.github.com>
2022-04-18 06:22:11 -0400

    Update a-better-finder-attributes from 7.18 to 7.19 (#122102)

--- Casks/a-better-finder-attributes.rb
+++ Casks/a-better-finder-attributes.rb
@@ -1,6 +1,6 @@
 cask "a-better-finder-attributes" do
-  version "7.18"
-  sha256 "9502f12fa1a310911e0af1c28aec0047eaa95cfd145ea91ef8ed1645f699dc9c"
+  version "7.19"
+  sha256 "1ebda25ac2f5d6372819856f0dbb7b58336e1115aa0d4307eb3b196b9e4bcb10"
 
   url "https://www.publicspace.net/download/signedABFA#{version.major}.zip"
   name "A Better Finder Attributes"
