3a43458cb2319c0e7307d9232d229aae995e33fd
Clément L <porkepix@gmail.com>
2022-04-19 01:19:24 +0200

    Update easy-move-plus-resize from 1.4.2 to 1.5.0 (#122168)

--- Casks/easy-move-plus-resize.rb
+++ Casks/easy-move-plus-resize.rb
@@ -1,6 +1,6 @@
 cask "easy-move-plus-resize" do
-  version "1.4.2"
-  sha256 "d44077b4065abba210c37fd32051a88fc217730309d7507096aac27cc2d9a9fb"
+  version "1.5.0"
+  sha256 "94e83f27c3b9c0ffdecc96c7f04d34cc7fb95e4e71c7b5f19c4faae83b2e91eb"
 
   url "https://github.com/dmarcotte/easy-move-resize/releases/download/#{version}/Easy.Move+Resize.app.zip"
   name "Easy Move+Resize"
