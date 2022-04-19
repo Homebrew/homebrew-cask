69a990521451a41cd5f2a33b8371020bba556ee8
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-04-19 20:33:18 +0800

    Update imgotv.rb

--- Casks/imgotv.rb
+++ Casks/imgotv.rb
@@ -22,6 +22,6 @@ cask "imgotv" do
     "~/Library/Caches/com.mgtv.pcclientx",
     "~/Library/Containers/com.mgtv.MGTV-macOS",
     "~/Library/Preferences/com.mgtv.pcclientx.plist",
-  ],
-      rmdir: "~/Library/Saved Application State/com.mgtv.pcclientx.savedState"
+    "~/Library/Saved Application State/com.mgtv.pcclientx.savedState",
+  ]
 end
