a9f432dd330aa0aa2e3f05a5b71646fb715f5ad0
zuisong <com.me@foxmail.com>
2022-04-19 23:42:21 +0800

    Update visualvm from 2.1.2 to 2.1.3

--- Casks/visualvm.rb
+++ Casks/visualvm.rb
@@ -1,6 +1,6 @@
 cask "visualvm" do
-  version "2.1.2"
-  sha256 "f2f3dc4b1bb799b8c234c47be99fe65028d1995176e639e3c777d87fb76c53a5"
+  version "2.1.3"
+  sha256 "26076f6ff2fc0f5f9bc188d9d2b51f179c97e069c15a1a2e564b0aeb0f8428d8"
 
   url "https://github.com/oracle/visualvm/releases/download/#{version}/VisualVM_#{version.no_dots}.dmg",
       verified: "github.com/oracle/visualvm/"
