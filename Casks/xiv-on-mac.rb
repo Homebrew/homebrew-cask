9e26ef22b7c3713bc64424e40ce74c478e52169c
Jakob Yanagibashi <git22@jakob.page>
2022-04-19 20:03:40 +0200

    Add XIV on Mac

---
+++ Casks/xiv-on-mac.rb
@@ -0,0 +1,22 @@
+cask "xiv-on-mac" do
+  version "3.5.5"
+  sha256 "c1b8f160168d0678ea1ce8a3372fdaace888b93b7d01421ce2ddc15c6c8f5a29"
+
+  url "https://softwareupdate.xivmac.com/sites/default/files/update_data/XIV%20on%20Mac#{version}.tar.xz"
+  name "xiv-on-mac"
+  desc "Wine wrapper, setup tool and launcher for FFXIV"
+  homepage "https://www.xivmac.com/"
+
+  livecheck do
+    url "https://softwareupdate.xivmac.com/sites/default/files/update_data/xivonmac_appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  app "XIV on Mac.app"
+
+  zap trash: [
+    "~/Library/Caches/dezent.XIV-on-Mac",
+    "~/Library/HTTPStorages/dezent.XIV-on-Mac",
+    "~/Library/Preferences/dezent.XIV-on-Mac.plist",
+  ]
+end
