116ae76fcf2a6eb7853138aa44bc9d41ee6a9cba
Bevan Kay <email@bevankay.me>
2022-04-19 10:48:44 +1000

    Update datweatherdoe.rb

--- Casks/datweatherdoe.rb
+++ Casks/datweatherdoe.rb
@@ -1,5 +1,5 @@
 cask "datweatherdoe" do
-  if MacOS.version < :big_sur
+  if MacOS.version <= :catalina
     version "2.2.0"
     sha256 "0e7c7a9a770f3f7e10a17610e4e8670f3c7050a872b1cb2947bfbbbfda94174f"
   else
@@ -8,7 +8,11 @@ cask "datweatherdoe" do
   end
 
   url "https://github.com/inderdhir/DatWeatherDoe/releases/download/#{version}/DatWeatherDoe-#{version}.dmg"
-  if MacOS.version < :big_sur
+  name "DatWeatherDoe"
+  desc "Menu bar weather app"
+  homepage "https://github.com/inderdhir/DatWeatherDoe"
+
+  if MacOS.version <= :catalina
     livecheck do
       skip "Legacy version for Catalina and earlier"
     end
@@ -18,10 +22,6 @@ cask "datweatherdoe" do
       strategy :github_latest
     end
   end
-
-  name "DatWeatherDoe"
-  desc "Menu bar weather app"
-  homepage "https://github.com/inderdhir/DatWeatherDoe"
 
   app "DatWeatherDoe.app"
 
