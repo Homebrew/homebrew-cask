b6918390b606f917587c0202b611170e25276368
Tin H Kyaw <tinhkyaw@users.noreply.github.com>
2022-04-18 23:06:11 -0700

    Update preference-manager.rb

--- Casks/preference-manager.rb
+++ Casks/preference-manager.rb
@@ -1,5 +1,5 @@
 cask "preference-manager" do
-  version "4.5.3"
+  version "4.5.3.0"
   sha256 "16a71f08a8e2f3fae5b8c4634e67f8378a0947fb8f85f87e6e63641bc5441e49"
 
   url "https://www.digitalrebellion.com/download/prefman?version=#{version.no_dots}"
