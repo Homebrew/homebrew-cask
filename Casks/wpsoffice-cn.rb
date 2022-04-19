459b032bd26626eccbbc56fcce8c98c3c5eaa78a
Bevan Kay <email@bevankay.me>
2022-04-19 14:38:24 +1000

    update livecheck

--- Casks/wpsoffice-cn.rb
+++ Casks/wpsoffice-cn.rb
@@ -20,11 +20,11 @@ cask "wpsoffice-cn" do
   livecheck do
     url :homepage
     strategy :page_match do |page|
-      v = page.match(%r{href=["'].*?WPS_Office[._-](\d+(?:\.\d+)+).*\.dmg["']}i).match(%r{(\d+(?:\.\d+)+)(?=\()}i)
-      id = page.match(%r{href=["'].*?WPS_Office[._-].*\((\d+)\).*\.dmg["']}i).match(%r{(?<=\()\d+(?=\))}i)
+      v = page.match(/href=["'].*?WPS_Office[._-](\d+(?:\.\d+)+).*\.dmg["']/i)
+      id = page.match(/href=["'].*?WPS_Office[._-].*\((\d+)\).*\.dmg["']/i)
       next if v.blank? || id.blank?
 
-      "#{v},#{id}"
+      "#{v[1]},#{id[1]}"
     end
   end
 
