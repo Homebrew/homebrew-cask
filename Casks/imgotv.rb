5d859ca7a4d34179815ff46a413874c3e9e7e691
Bevan Kay <email@bevankay.me>
2022-04-19 22:40:29 +1000

    Update imgotv.rb

--- Casks/imgotv.rb
+++ Casks/imgotv.rb
@@ -1,8 +1,8 @@
 cask "imgotv" do
-  version "6.4.6"
+  version "6.4.6_1"
   sha256 "af68f7ec41444eb274ce7bc4d8e2c6b39b36f2d20b8299d507b0a3cd41e1b967"
 
-  url "https://download.imgo.tv/app/mac/#{version}_1/mgtv-mango.dmg",
+  url "https://download.imgo.tv/app/mac/#{version}/mgtv-mango.dmg",
       verified: "download.imgo.tv/"
   name "芒果TV"
   desc "Mango TV video app"
@@ -11,7 +11,7 @@ cask "imgotv" do
   livecheck do
     url "https://pcconf.api.mgtv.com/getPcDownloadUrl?source=mango2"
     strategy :header_match
-    regex(%r{/app/mac/(\d+(?:\.\d+)+)_1/mgtv[._-]mango\.dmg}i)
+    regex(%r{/app/mac/(\d+(?:[._]\d+)+)/mgtv[._-]mango\.dmg}i)
   end
 
   depends_on macos: ">= :yosemite"
