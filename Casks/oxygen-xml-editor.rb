45b0dfcf06288974cc427988e6d98427e0981202
Joe Wicentowski <joewiz@gmail.com>
2022-04-20 13:42:46 -0400

    Update oxygen-xml-editor from 24.1,2022030807 to 24.1,2022041507
    And supply sha256 values for added security

--- Casks/oxygen-xml-editor.rb
+++ Casks/oxygen-xml-editor.rb
@@ -1,8 +1,14 @@
 cask "oxygen-xml-editor" do
   arch = Hardware::CPU.intel? ? "" : "-aarch64"
 
-  version "24.1,2022030807"
-  sha256 :no_check
+  version "24.1,2022041507"
+  sha256
+
+  if Hardware::CPU.intel?
+    sha256 "4b46b522084a3b824395aec638a3e081146ad4a4e3686c68b7621b84e5da1efa"
+  else
+    sha256 "a07c150ebd8a127d31ed4b2922c60134923c4fe36018a56cc0884dcee2feba9a"
+  end
 
   url "https://www.oxygenxml.com/InstData/Editor/MacOSX/VM/oxygen-openjdk#{arch}.dmg"
   name "oXygen XML Editor"
