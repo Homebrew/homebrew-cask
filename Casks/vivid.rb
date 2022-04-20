e7333e6d672a7ad59202cd4571b27cac3b77e357
Reno <singularitti@outlook.com>
2022-04-19 20:11:07 -0400

    Update vivid from 1.2.1 to 1.4

--- Casks/vivid.rb
+++ Casks/vivid.rb
@@ -1,6 +1,6 @@
 cask "vivid" do
-  version "1.2.1,18"
-  sha256 "e4352bb2c2c18374a52be94e0fbb23ce84263ddedc6eb36209b0e31afbe2a794"
+  version "1.4,20"
+  sha256 "db696c0e803be7ad8cf3a18ab76e8078280691ed64f61bf0ae5688ce81be9c5b"
 
   url "https://lumen-digital.com/apps/vivid/releases/Vivid#{version.csv.first}.zip",
       verified: "https://lumen-digital.com/apps/vivid"
