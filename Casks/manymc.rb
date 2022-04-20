28b795f45fd27f62117e51a333e337c0ceffb40d
meowmeowcat <68463158+meowmeowmeowcat@users.noreply.github.com>
2022-04-20 15:03:01 +0800

    Add ManyMC.app v0.0.7

---
+++ Casks/manymc.rb
@@ -0,0 +1,29 @@
+cask "manymc" do
+  version "0.0.7"
+  sha256 "3b49d0cec2fa58dfa055e138739c53654189fc44c79a829191092f4a1d42f4f8"
+
+  url "https://github.com/MinecraftMachina/ManyMC/releases/download/v#{version}/ManyMC.zip"
+  name "ManyMC"
+  desc "Familiar Minecraft Launcher with native support for arm64 (M1)"
+  homepage "https://github.com/MinecraftMachina/ManyMC"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on arch: :arm64
+  depends_on macos: ">= :big_sur"
+
+  app "ManyMC.app"
+
+  zap trash: [
+    "~/Library/Application Support/ManyMC",
+    "~/Library/Preferences/org.multimc.ManyMC.plist",
+    "~/Library/Saved Application State/org.multimc.ManyMC.savedState",
+  ]
+
+  caveats do
+    depends_on_java "17+"
+  end
+end
