3aa49cce378518a1ba6ea42eef0899d568ce4a7a
lipu <59558393+lipu-ted@users.noreply.github.com>
2022-04-20 14:48:34 +0200

    Update franz from 5.9.1 to 5.9.2 (#122244)

--- Casks/franz.rb
+++ Casks/franz.rb
@@ -1,12 +1,12 @@
 cask "franz" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  version "5.9.1"
+  version "5.9.2"
 
   if Hardware::CPU.intel?
-    sha256 "ff177691e82bd29b9caa57dd602bd955d00accd68d3d5147a015f6ac4eb3f5bc"
+    sha256 "9b6907c132624c645e9d39bc9822a239b0e3a7507d40bcc97bf2be6ca3fc171f"
   else
-    sha256 "48e2e918f43aec4b2c47822a9eb53e7053c1c7ce54a1642012a851a54661ad16"
+    sha256 "700342d6d3a6b532e5e7a3767c8b97d9bcfc9eb0959098f02d3b13462fdcfb7d"
   end
 
   url "https://github.com/meetfranz/franz/releases/download/v#{version}/franz-#{version}#{arch}.dmg",
