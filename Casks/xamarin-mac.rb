928a12cc33d26a6e61a40eab62e14a383571dd54
Yurii Kolesnykov <root@yurikoles.com>
2022-04-20 09:33:19 +0300

    Update xamarin-mac from 8.6.0.3 to 8.8.0.3 (#122230)

--- Casks/xamarin-mac.rb
+++ Casks/xamarin-mac.rb
@@ -1,6 +1,6 @@
 cask "xamarin-mac" do
-  version "8.6.0.3"
-  sha256 "876b434908d1787441b8648782b1e2b4ddd51531c5df62a9cd02a68f48b56084"
+  version "8.8.0.3"
+  sha256 "590be3e7d09888ec6dc9bdc58d0df8ff2de1ecf224638bd4da5c771438f44ab3"
 
   url "https://dl.xamarin.com/XamarinforMac/Mac/xamarin.mac-#{version}.pkg"
   name "Xamarin Mac"
