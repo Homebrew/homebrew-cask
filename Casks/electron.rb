af9f48c724db432f23f07208d7d42c7f061e4584
Clément L <porkepix@gmail.com>
2022-04-22 01:04:04 +0200

    Update electron from 18.0.4 to 18.1.0 (#122303)

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,12 +1,12 @@
 cask "electron" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "18.0.4"
+  version "18.1.0"
 
   if Hardware::CPU.intel?
-    sha256 "7077834b6235fe8825620eea88fbd9de9c5c11001abbbe6b2b9f4357a791d4b4"
+    sha256 "24dd64a66b820c9553c5e5570907da6c98e808d33fac98072b9c2a8f1659cb14"
   else
-    sha256 "19b47adefcd730bbdbef7d749b39df8a8ae4905883eafb5e2bb3154fe4b02eb6"
+    sha256 "97adf13306c9b3b304d3e9ddf68f5f7fb9b79c9a1342114e3671182f3cc9e808"
   end
 
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-#{arch}.zip",
