e72f7cb250135de0141554c791a3afa8d3627f1c
MoonFruit <dkmoonfruit@gmail.com>
2022-04-21 14:14:56 +0800

    Update ximalaya from 2.6.0 to 2.7.1 (#122293)

--- Casks/ximalaya.rb
+++ Casks/ximalaya.rb
@@ -1,6 +1,6 @@
 cask "ximalaya" do
-  version "2.6.0,1647423365"
-  sha256 "acf5eb744fba26ce092b8794c0a5868e2a29a00f1a5fb4429832cb991863b6d0"
+  version "2.7.1,1649843270"
+  sha256 "d9a07da0550b2d944d458f764e36878171b015f1f5851f55e45eb352380f7e66"
 
   url "https://s1.xmcdn.com/yx/ximalaya-pc-client/#{version.csv.first}/download/mac-x64/Ximalaya_#{version.csv.first}_#{version.csv.second}.dmg",
       verified: "s1.xmcdn.com/yx/ximalaya-pc-client/"
