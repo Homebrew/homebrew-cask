514651248a21a03d836921e1cbc631546ba0a6fd
Alexander Ronald Altman <alexanderaltman@me.com>
2022-04-22 23:48:10 -0700

    Update dcp-o-matic-player from 2.16.5 to 2.16.8 (#122395)

--- Casks/dcp-o-matic-player.rb
+++ Casks/dcp-o-matic-player.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-player" do
-  version "2.16.5"
-  sha256 "aaac9049c7fa353b3a81eace6bb928d3d28e3cdab57db36e0ee7b0e76b022ad9"
+  version "2.16.8"
+  sha256 "4ab420792607615e91d67f30e19bed86cc5126756dec681e762364fd9f4d0bf7"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-player&version=#{version}"
   name "DCP-o-matic Player"
