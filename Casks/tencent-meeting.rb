d1dbbb91d0be8e058c71b20cf287a3c62ee3fac8
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-04-23 15:51:50 +0800

    Update tencent-meeting.rb

--- Casks/tencent-meeting.rb
+++ Casks/tencent-meeting.rb
@@ -2,10 +2,10 @@ cask "tencent-meeting" do
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 
   if Hardware::CPU.intel?
-    version "3.6.6.420,9381555779dbb853ba1ec6397531236b"
+    version "3.7.6.404,9381555779dbb853ba1ec6397531236b"
     sha256 "239466642ff2a93a2b20115e232208b8567b02aa4dd97676bd1ec47822a3d103"
   else
-    version "3.6.6.420,1acb0448975b0a0a9f51e6ed8bbff923"
+    version "3.7.6.404,1acb0448975b0a0a9f51e6ed8bbff923"
     sha256 "0001567b780cf8fe87229b8a21bc06a6e482a73e549ad938a97bc68a4c8c1771"
   end
 
