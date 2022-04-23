e4ac4af7a43603771cbd913e473e6313997cd61a
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-04-23 14:42:16 +0800

    Update wechatwork from 4.0.3.90492 to 4.0.3.99066 (#122025)
    * Update wechatwork from 4.0.3.90492 to 4.0.3.99066
    
    * Update wechatwork.rb
    
    * Update wechatwork.rb
    
    * Update wechatwork.rb

--- Casks/wechatwork.rb
+++ Casks/wechatwork.rb
@@ -1,6 +1,11 @@
 cask "wechatwork" do
-  version "4.0.3.90492"
-  sha256 "c7306cf208cbdc1e4b0dbcfd96f981d04dff5875616ada7d1d369c214be39a4e"
+  if Hardware::CPU.intel?
+    version "4.0.3.90492"
+    sha256 "c7306cf208cbdc1e4b0dbcfd96f981d04dff5875616ada7d1d369c214be39a4e"
+  else
+    version "4.0.3.99066"
+    sha256 "16ef032ea837b86d5010abdb3a371f70402fd733d3aa1b465a520fe1011999d4"
+  end
 
   url "https://dldir1.qq.com/foxmail/wecom-mac/update/WeCom_#{version}.dmg"
   name "WeChat Work"
@@ -9,8 +14,13 @@ cask "wechatwork" do
   homepage "https://work.weixin.qq.com/"
 
   livecheck do
-    url "https://work.weixin.qq.com/wework_admin/commdownload?platform=mac"
-    strategy :header_match
+    if Hardware::CPU.intel?
+      url "https://work.weixin.qq.com/wework_admin/commdownload?platform=mac"
+      strategy :header_match
+    else
+      url :homepage
+      regex(%r{href=.*/wecom-mac/update/WeCom[-_.](\d+(?:\.\d+)+)\.dmg}i)
+    end
   end
 
   auto_updates true
@@ -18,4 +28,19 @@ cask "wechatwork" do
   app "企业微信.app"
 
   uninstall quit: "com.tencent.WeWorkMac"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.tencent.WeWorkMac",
+    "~/Library/Application Scripts/88L2Q4487U.com.tencent.WeWorkMac",
+    "~/Library/Application Scripts/88L2Q4487U.com.tencent.WeWorkMacDebug",
+    "~/Library/Application Scripts/88L2Q4487U.com.tencent.WeWorkMacUIDev",
+    "~/Library/Application Scripts/88L2Q4487U.com.tencent.WeWorkMac.dev",
+    "~/Library/Application Scripts/88L2Q4487U.com.tencent.WeWorkMac.IPCHelper",
+    "~/Library/Containers/com.tencent.WeWorkMac",
+    "~/Library/Containers/88L2Q4487U.com.tencent.WeWorkMac",
+    "~/Library/Containers/88L2Q4487U.com.tencent.WeWorkMacDebug",
+    "~/Library/Containers/88L2Q4487U.com.tencent.WeWorkMacUIDev",
+    "~/Library/Containers/88L2Q4487U.com.tencent.WeWorkMac.dev",
+    "~/Library/Containers/88L2Q4487U.com.tencent.WeWorkMac.IPCHelper",
+  ]
 end
