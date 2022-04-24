17d820c1b4f44a9c3ed4467dfa253f91880e5f5f
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-04-24 20:21:37 +0800

    Update videofusion.rb

--- Casks/videofusion.rb
+++ Casks/videofusion.rb
@@ -2,15 +2,15 @@ cask "videofusion" do
   version "2.9.6.7019.0"
   sha256 "d5e1deb0cc2b27ffd2311267b2cbbd7c96624c1238b6d015e839784d13953ff5"
 
-  url "https://lf6-package.vlabstatic.com/obj/faceu-packages/Jianying_#{version.dots_to_underscores}.dmg",
-      verified: "lf6-package.vlabstatic.com/obj/faceu-packages/"
+  url "https://lf3-package.vlabstatic.com/obj/faceu-packages/Jianying_#{version.dots_to_underscores}.dmg",
+      verified: "lf3-package.vlabstatic.com/obj/faceu-packages/"
   name "VideoFusion"
   name "剪映专业版"
   desc "Video editor"
   homepage "https://lv.ulikecam.com/"
 
   livecheck do
-    url "https://lf6-beecdn.bytetos.com/obj/ies-fe-bee/bee_prod/biz_80/bee_prod_80_bee_publish_3563.json"
+    url "https://lf3-beecdn.bytetos.com/obj/ies-fe-bee/bee_prod/biz_80/bee_prod_80_bee_publish_3563.json"
     strategy :page_match do |page|
       JSON.parse(page)["mac_download_pkg"]["channel_default"][/(\d+(?:_\d+)+)\.dmg/i, 1].tr("_", ".")
     end
