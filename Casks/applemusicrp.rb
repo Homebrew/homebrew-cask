11a9fa41d594bd91417f25c9a8ad43093938f947
wxllow <35239049+wxllow@users.noreply.github.com>
2022-04-23 20:14:27 -0400

    Added applemusicrp

---
+++ Casks/applemusicrp.rb
@@ -0,0 +1,11 @@
+cask "applemusicrp" do
+    version "3.0.0"
+    sha256 "6c676cb60d3ab3bcb9380556fa8bf29f1b29b7f9edb6ad6ba5ce4eec760c6d24"
+  
+    url "https://github.com/wxllow/applemusicrp/releases/download/#{version}/AppleMusicRP.dmg"
+    name "AppleMusicRP"
+    desc "Discord Rich Presence for Apple Music w/ Album Artwork!"
+    homepage "https://github.com/wxllow/applemusicrp"
+  
+    app "applemusicrp.app"
+  end
