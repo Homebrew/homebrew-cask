086fc1061dd34d4b442799afa0cb6f5cb620b923
James Mead <github@floehopper.org>
2022-04-25 05:12:20 +0100

    Fix URL for ZEsarUX release & add desc to cask file (#122408)
    * Fix URL for ZEsarUX release
    
    It appears that the URL has changed slightly and this was preventing me
    from installing the cask.
    
    * Add desc to ZEsarUX cask file
    
    As recommended by `brew audit`.
    
    * Fix livecheck for ZEsarUX
    
    The previous configuration using the github_latest strategy was failing,
    because it was including a release candidate. Using the default git
    strategy as recommended in the docs [1] in combination with a regex as
    suggested in the docs [2] seems to work.
    
    [1]: https://docs.brew.sh/Brew-Livecheck#general-guidelines
    [2]: https://docs.brew.sh/Brew-Livecheck#git-tags
    
    * update livecheck
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/zesarux.rb
+++ Casks/zesarux.rb
@@ -2,13 +2,15 @@ cask "zesarux" do
   version "10.0"
   sha256 "df45b2d1cf72bcb75ed13d398dfffbf33d293b3d8eccc2545cd61f30fab15b07"
 
-  url "https://github.com/chernandezba/zesarux/releases/download/#{version}/ZEsarUX_macos-#{version}.dmg"
+  url "https://github.com/chernandezba/zesarux/releases/download/ZEsarUX-#{version}/ZEsarUX_macos-#{version}.dmg"
   name "ZEsarUX"
+  desc "ZX machines emulator"
   homepage "https://github.com/chernandezba/zesarux"
 
   livecheck do
     url :url
     strategy :github_latest
+    regex(/href=.*?ZEsarUX[._-]macos[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
   app "ZEsarUX.app"
