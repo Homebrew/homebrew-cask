182b6edc439eaab07eedb18b4b445b17d091aabc
Bevan Kay <email@bevankay.me>
2022-04-25 14:46:06 +1000

    Update cnkiexpress.rb

--- Casks/cnkiexpress.rb
+++ Casks/cnkiexpress.rb
@@ -1,6 +1,6 @@
 cask "cnkiexpress" do
   version "0.1.21"
-  sha256 "0d2459f85d7949277545a869eba5fe18ab2d0273cfd3291a690ff8a5a3942cd3"
+  sha256 "65aa05aa50c7d1a26c9e653739ac05d2fd3db459d2c90ea2ece1877626862435"
 
   url "https://download.cnki.net/cnkiexpress/\%E5\%85\%A8\%E7\%90\%83\%E5\%AD\%A6\%E6\%9C\%AF\%E5\%BF\%AB\%E6\%8A\%A5-#{version}.dmg"
   name "CNKIExpress"
