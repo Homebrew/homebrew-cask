335726b8660420aca7d22fa82f798ca4b6a1300e
Roman Ternavskyi <rternavskyi@gmail.com>
2022-04-25 22:14:00 +0300

    add comma

--- Casks/autodesk-fusion360.rb
+++ Casks/autodesk-fusion360.rb
@@ -8,7 +8,7 @@ cask "autodesk-fusion360" do
   homepage "https://www.autodesk.com/products/fusion-360"
 
   installer script: {
-    executable: "#{staged_path}/Install Autodesk Fusion 360.app/Contents/MacOS/Fusion 360 Client Downloader"
+    executable: "#{staged_path}/Install Autodesk Fusion 360.app/Contents/MacOS/Fusion 360 Client Downloader",
   }
 
   uninstall quit:   [
