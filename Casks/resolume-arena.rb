c96a4c1b48b20c94d5f669896464c0fa30a49ff9
Bevan Kay <email@bevankay.me>
2022-04-25 13:48:43 +1000

    Update resolume-arena from 7.11.1 to 7.11.2

--- Casks/resolume-arena.rb
+++ Casks/resolume-arena.rb
@@ -1,6 +1,6 @@
 cask "resolume-arena" do
-  version "7.11.1,13682"
-  sha256 "f20db281922e12d5f5a5973b0f39be3c707aecce14d8f69dcd35682eeac79542"
+  version "7.11.2,13714"
+  sha256 "a80da5cfcfc800be40d313df99d89f06148900648174cbdb8658944bc53e534b"
 
   url "https://dd5sgwxv3xok.cloudfront.net/Resolume_Arena_#{version.major_minor_patch.dots_to_underscores}_rev_#{version.csv.second}_Installer.dmg",
       verified: "dd5sgwxv3xok.cloudfront.net/"
