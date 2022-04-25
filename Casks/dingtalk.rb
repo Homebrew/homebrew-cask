b5d15feacea6d5c1683d555a0570623bef056e0f
zuisong <com.me@foxmail.com>
2022-04-25 22:08:42 +0800

    dingtalk 6.5.10.4
    * Update dingtalk from 6.5.0.6 to 6.5.10.4
    * add m1 support
    
    Closes #122468.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/dingtalk.rb
+++ Casks/dingtalk.rb
@@ -1,15 +1,27 @@
 cask "dingtalk" do
-  version "6.5.0.6"
-  sha256 "b0e6de497260ab121b4a8bc372734395490c0223c08bb2b53fca0de32d11c1e5"
+  if Hardware::CPU.intel?
+    version "6.5.10.4"
+    sha256 "18ee3da613a0269aa7de12dfad6074efd76f154932561a5fe0913cddefd13e48"
+    url "https://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/DingTalk_v#{version}.dmg"
+  else
+    version "6.5.2.5"
+    sha256 "80b04b7535b83cd9ad2def6b2880de921f415e64d68c11e41cfdd7cc644005bf"
+    url "https://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/M1-Beta/DingTalk-ARM64-#{version}.dmg"
+  end
 
-  url "https://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/DingTalk_v#{version}.dmg"
   name "DingTalk"
   name "钉钉"
   desc "Teamwork app by Alibaba Group"
   homepage "https://www.dingtalk.com/"
 
   livecheck do
-    url "https://www.dingtalk.com/mac/d/"
+    livecheck_url = if Hardware::CPU.intel?
+      "https://www.dingtalk.com/mac/d/"
+    else
+      "https://www.dingtalk.com/mac/d/qd=2022mac-m1"
+    end
+    url livecheck_url
+
     strategy :header_match
   end
 
