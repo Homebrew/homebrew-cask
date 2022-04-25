962e5c6be8238d7239b7c61079602268439996db
Loyio Hex <i@loyio.me>
2022-04-25 10:19:28 +0800

    Update notion-enhanced.rb

--- Casks/notion-enhanced.rb
+++ Casks/notion-enhanced.rb
@@ -1,13 +1,13 @@
 cask "notion-enhanced" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
   version "2.0.18-1"
-  
+
   if Hardware::CPU.intel?
     sha256 "e54a37053ed52a42ecbb4ed22a0ce50498ecc1efb3bff5b134099a56a8569309"
   else
     sha256 "5437e9edc439b03e3e11ca1f19a3576c817437b91571dcbd810d364053a8926f"
   end
-  
+
   url "https://github.com/notion-enhancer/notion-repackaged/releases/download/v#{version}/Notion-Enhanced-#{version}#{arch}.dmg",
       verified: "github.com/notion-enhancer"
   name "Notion Enhanced"
