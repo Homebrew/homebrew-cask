a06c488264af8ea4bd6579f012929d5918fef90c
i0ntempest <szf1234@me.com>
2022-04-25 14:27:52 -0400

    browserosaurus 18.1.0
    Update browserosaurus from 18.0.0 to 18.1.0
    
    Closes #122475.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/browserosaurus.rb
+++ Casks/browserosaurus.rb
@@ -1,12 +1,12 @@
 cask "browserosaurus" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "18.0.0"
+  version "18.1.0"
 
   if Hardware::CPU.intel?
-    sha256 "aed67a417be836b5851295f8bf4ceba599639d9db54627db497acf05d11c2b31"
+    sha256 "6a8bdd83e52afdee6e16fc98a9657ce322d454f0bd1d251368f121084c0a8322"
   else
-    sha256 "67ac434995b075b68fa4014648cb5788d92a97ef2f78aba42ec88720ca01fbc0"
+    sha256 "efa400ebdf9cbaa5d4e544456ab93b1e29ac2fa49972089bdbe956551b188423"
   end
 
   url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-darwin-#{arch}-#{version}.zip"
