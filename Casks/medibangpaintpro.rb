67312cd9b04016c399c4c73672ad4894d448a8c1
Reno <25192197+singularitti@users.noreply.github.com>
2022-04-25 00:19:40 -0400

    Update medibangpaintpro from 27.2 to 28.0 (#122407)

--- Casks/medibangpaintpro.rb
+++ Casks/medibangpaintpro.rb
@@ -1,6 +1,6 @@
 cask "medibangpaintpro" do
-  version "27.2"
-  sha256 "5f8f4f88222d900a30e8ce84c4d3aa968a08b09ed55e1f975271030233f47d85"
+  version "28.0"
+  sha256 "d9d6bd4e8807f96958413743f06a14521aec09500d7a1d66b5ee17c6fd4a5998"
 
   url "https://medibangpaint.com/static/installer/MediBangPaintPro/MediBangPaintPro-#{version}.dmg"
   name "MediBang Paint Pro"
