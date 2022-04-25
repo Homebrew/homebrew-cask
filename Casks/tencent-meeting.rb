908e321e79e87380b0ae5bab02551db1e4de4f23
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-04-25 12:20:48 +0800

    Update tencent-meeting from 3.6.6.420 to 3.7.6.404 (#122400)
    * Update tencent-meeting from 3.6.6.420 to 3.7.6.404
    
    * Update tencent-meeting.rb

--- Casks/tencent-meeting.rb
+++ Casks/tencent-meeting.rb
@@ -2,11 +2,11 @@ cask "tencent-meeting" do
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 
   if Hardware::CPU.intel?
-    version "3.6.6.420,68df5eca7502c04be9efe9e55ef7be6e"
-    sha256 "1c90896d72cdffc3644a282af27494ebad4d23050cd692838d54bead25b3b81c"
+    version "3.7.6.404,9381555779dbb853ba1ec6397531236b"
+    sha256 "239466642ff2a93a2b20115e232208b8567b02aa4dd97676bd1ec47822a3d103"
   else
-    version "3.6.6.420,cdc3937d8bdd9217115a265d5b4f3b47"
-    sha256 "dd4fae08015b5ad9c35eb5abe7b6d873226e9279b17550642f3df8905ea6e019"
+    version "3.7.6.404,1acb0448975b0a0a9f51e6ed8bbff923"
+    sha256 "0001567b780cf8fe87229b8a21bc06a6e482a73e549ad938a97bc68a4c8c1771"
   end
 
   url "https://updatecdn.meeting.qq.com/cos/#{version.csv.second}/TencentMeeting_0300000000_#{version.csv.first}.publish.#{arch}.dmg",
