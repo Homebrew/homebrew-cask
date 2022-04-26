9ed6063cc4feed025b6f5e859c1ca120b8acce32
Alexey Loginov <nvmker@gmail.com>
2022-04-26 22:52:31 +0300

    Fixed url and added sha256 checksum

--- Casks/cycling74-max.rb
+++ Casks/cycling74-max.rb
@@ -1,8 +1,9 @@
 cask "cycling74-max" do
   version "8.3.0_220426"
-  sha256 :no_check
+  sha256 "1b106cde409b5c1abda33d23959f7bedd66c5f37cc3c7a69f5663331d59f6b86"
 
-  url "https://akiaj5esl75o5wbdcv2a-maxmspjitter.s3.amazonaws.com/Max830_220426.dmg"
+  url "https://akiaj5esl75o5wbdcv2a-maxmspjitter.s3.amazonaws.com/Max#{version.no_dots}.dmg",
+    verified: "akiaj5esl75o5wbdcv2a-maxmspjitter.s3.amazonaws.com/"
   name "Cycling ‘74 Max"
   name "Ableton Max for Live"
   desc "Flexible space to create your own interactive software"
