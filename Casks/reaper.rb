b9d0baaf23986f683ea26160986ec5fbc5a5db2d
Bevan Kay <email@bevankay.me>
2022-04-26 10:15:42 +1000

    reaper 6.56
    Update reaper from 6.55 to 6.56
    
    Closes #122505.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,11 +1,11 @@
 cask "reaper" do
-  version "6.55"
+  version "6.56"
 
   if MacOS.version <= :mojave
     sha256 "f13921dfe8a110fd9c4641762c9ac0bdceb8c5bb0c59e17ec58f8b7b68adfa9b"
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64.dmg"
   else
-    sha256 "f84b360f06ea446e1b50e14278b63c22e69a98ed40609c0b855648e91e42a98a"
+    sha256 "b0df797e6e94c90fd6b001486f22baf4f4283d089b1ff962e894275f1433ec45"
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}_universal.dmg"
   end
 
