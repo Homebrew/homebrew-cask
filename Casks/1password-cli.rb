401b131a945e11fbacf028e58550606a2415aadf
Branch Vincent <branchevincent@gmail.com>
2022-04-25 20:39:36 -0400

    1password-cli 2.1.0
    Update 1password-cli from 2.0.2 to 2.1.0
    
    Closes #122513.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/1password-cli.rb
+++ Casks/1password-cli.rb
@@ -1,6 +1,6 @@
 cask "1password-cli" do
-  version "2.0.2"
-  sha256 "3f9aaccb892213f0cc26726bdc450731c6f42a8519c8ed814393c8a2c3db9c8f"
+  version "2.1.0"
+  sha256 "0a7dfff83be4e615bdaefc6ee08f60847492d7259201e42ade0c7ee9dbc58889"
 
   url "https://cache.agilebits.com/dist/1P/op2/pkg/v#{version}/op_apple_universal_v#{version}.pkg",
       verified: "cache.agilebits.com/dist/1P/op2/pkg/"
