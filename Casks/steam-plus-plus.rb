1d62f17464a63e17c9fdd84a08ba41570f7428b9
i0ntempest <szf1234@me.com>
2022-04-26 15:43:50 -0400

    steam-plus-plus 2.7.0
    Update steam-plus-plus from 2.6.9 to 2.7.0
    
    Closes #122556.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/steam-plus-plus.rb
+++ Casks/steam-plus-plus.rb
@@ -1,8 +1,15 @@
 cask "steam-plus-plus" do
-  version "2.6.9"
-  sha256 "c03e9eb6f0bc25d7ad20ee1a28f88477b2f5c19866be2db93673b6c31a348114"
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  url "https://github.com/BeyondDimension/SteamTools/releases/download/#{version}/Steam++_macos_x64_v#{version}.dmg",
+  version "2.7.0"
+
+  if Hardware::CPU.intel?
+    sha256 "c9d0e92d64a857fb0199e2e55cad4fa82dd92c7f4989b52b918db3f0da62c3d6"
+  else
+    sha256 "2eedab66805d621f4fe54617f7476a8fc7388c05e7710fea0a999c57c86d84f6"
+  end
+
+  url "https://github.com/BeyondDimension/SteamTools/releases/download/#{version}/Steam++_macos_#{arch}_v#{version}.dmg",
       verified: "github.com/BeyondDimension/SteamTools/"
   name "Steam++"
   desc "Steam helper tools"
