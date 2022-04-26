f2881c2ff014e1c95b46921502b7466ffe2d4c05
neutric <5984479+neutric@users.noreply.github.com>
2022-04-26 13:18:35 +0200

    Update unity from 2021.3.0f1 to 2021.3.1f1 (#122489)
    * Update unity from 2021.3.0f1 to 2021.3.1f1
    
    * Trigger CI
    
    * Trigger CI

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -1,12 +1,12 @@
 cask "unity" do
   arch = Hardware::CPU.intel? ? "" : "Arm64"
 
-  version "2021.3.0f1,6eacc8284459"
+  version "2021.3.1f1,3b70a0754835"
 
   if Hardware::CPU.intel?
-    sha256 "0d626fbdf452a6e4a73a336cc6f0e36729e7d8d7e648eefe32ba0828a8527983"
+    sha256 "9ced483d8c948b57cf4b14a8b83569c464629762b4af21595cd515e77f1033f1"
   else
-    sha256 "24e686d2e3e5210a919f5b933308ac71e709d45430acf4c288ad5bf2b77d962e"
+    sha256 "bd6554c6e0fe6358f373ae13ef0e15c5e6c1d9c5210be73d36b77c2903e961d1"
   end
 
   url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorInstaller#{arch}/Unity-#{version.csv.first}.pkg",
