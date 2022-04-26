fb78c91ddeb7fd7c6e0293bd7e111ccbad0c10e1
Porkepix <porkepix@gmail.com>
2022-04-26 12:28:48 +0200

    codeql 2.9.0
    Update codeql from 2.8.5 to 2.9.0
    
    Closes #122535.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/codeql.rb
+++ Casks/codeql.rb
@@ -1,6 +1,6 @@
 cask "codeql" do
-  version "2.8.5"
-  sha256 "eff5e16b68382f2b918a4be03a9701df70b68b3a1db394aba0edd61809e11f79"
+  version "2.9.0"
+  sha256 "a8c6ee9ae462dd001b76f7cef4fe57a37255944d6cf4e832f6f14c9ea60a8f89"
 
   url "https://github.com/github/codeql-cli-binaries/releases/download/v#{version}/codeql-osx64.zip"
   name "CodeQL"
