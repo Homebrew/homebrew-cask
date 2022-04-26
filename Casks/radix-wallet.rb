56c6b646e4afdb876fd2dbb1b0d81d1406e42571
Coby Chapple <coby.c@rdx.works>
2022-04-26 15:34:17 +0100

    Correct name field for Radix Wallet

--- Casks/radix-wallet.rb
+++ Casks/radix-wallet.rb
@@ -3,7 +3,7 @@ cask "radix-wallet" do
   sha256 "aeac5f36a4a672d24108f4e05b48e0e59e4202b769de1ca253b86d0c47a9102b"
 
   url "https://wallet.radixdlt.com/files/Radix-Wallet-#{version}.dmg"
-  name "radix-wallet"
+  name "Radix Wallet"
   desc "Desktop wallet for the Radix DLT cryptocurrency"
   homepage "https://wallet.radixdlt.com/"
 
