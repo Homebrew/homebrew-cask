c2983824f0d8efaaced4786cf5d1a566ba46ebcc
owine <owine@users.noreply.github.com>
2022-04-27 12:33:57 -0500

    gotomeeting 19950
    Update gotomeeting from 19932 to 19950
    
    Closes #122611.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/gotomeeting.rb
+++ Casks/gotomeeting.rb
@@ -1,6 +1,6 @@
 cask "gotomeeting" do
-  version "19932"
-  sha256 "391573efc2043bf4f63be45e3540db531a9c631286d3b8504e5f3029618c825c"
+  version "19950"
+  sha256 "64c3b31b35f27027a93a092f3a1c36cf23d551b4bccd384fa45bdca4a672c91c"
 
   url "https://builds.cdn.getgo.com/builds/g2m/#{version}/GoToMeeting.dmg",
       verified: "builds.cdn.getgo.com/builds/g2m/"
