3fd865c8ef0d0ce8a8a5f282cfbd486ecfe9eb79
John Benninghoff <jbenninghoff@mac.com>
2022-04-27 08:25:25 -0500

    Update endnote from 20.2 to 20.3

--- Casks/endnote.rb
+++ Casks/endnote.rb
@@ -1,5 +1,5 @@
 cask "endnote" do
-  version "20.2"
+  version "20.3"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://download.endnote.com/downloads/#{version.major}/EndNote#{version.major}Installer.dmg"
