910f0a5291442c2f15a595895861ed9092b89be5
Vin E. R. Yap <vineryap@gmail.com>
2022-04-28 10:28:57 +0900

    Added arm64 sha256

--- Casks/vscodium.rb
+++ Casks/vscodium.rb
@@ -2,7 +2,11 @@ cask "vscodium" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
   version "1.66.2"
-  sha256 "7163ee27353cba1d8477da3a39001db0c67a71bef19799af3e16ff9367c93e10"
+  if Hardware::CPU.intel?
+    sha256 "7163ee27353cba1d8477da3a39001db0c67a71bef19799af3e16ff9367c93e10"
+  else
+    sha256 "3685ade606cb51065895938081b42aa6fd15c92ff5e258f7181be7df1920c549"
+  end
 
   url "https://github.com/VSCodium/vscodium/releases/download/#{version}/VSCodium.#{arch}.#{version}.dmg"
   name "VSCodium"
