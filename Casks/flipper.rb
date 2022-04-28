1ec0e6ef927d2e019e6a3fd60bcd2b828e92c7cc
Carlos Moreno <cmoreno@protelecom.com.gt>
2022-04-27 21:45:12 -0600

    flipper 0.144.0
    Update flipper from 0.143.0 to 0.144.0
    
    Closes #122634.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/flipper.rb
+++ Casks/flipper.rb
@@ -1,6 +1,6 @@
 cask "flipper" do
-  version "0.143.0"
-  sha256 "74b9e4d62d18e5aaf96efb49227127fb763963cb2559c8f9cc376d75d495e2ac"
+  version "0.144.0"
+  sha256 "3971b818418f00520be81525822d92510aae1c4d6be9f737c52f66c16739d052"
 
   url "https://github.com/facebook/flipper/releases/download/v#{version}/Flipper-mac.dmg",
       verified: "github.com/facebook/flipper/"
