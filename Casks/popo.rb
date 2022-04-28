82df219963b21c226d8dade508389bbcb1b7fec6
Shiki <53338814+shikiroot@users.noreply.github.com>
2022-04-28 09:24:32 +0800

    update popo from 3.47.1 to 3.48.0

--- Casks/popo.rb
+++ Casks/popo.rb
@@ -1,6 +1,6 @@
 cask "popo" do
-  version "3.47.1"
-  sha256 "ad40151d56d4965bc6728cfb13bc10676a0e3fc49f9370ab51cd08315de83375"
+  version "3.48.0"
+  sha256 "3f9ef89810baf10b1d50fc675e5d191bf3ce8ed2db886d498400d2c73ad7fd3c"
 
   url "https://popo.netease.com/file/popomac/POPO-setup_#{version.dots_to_underscores}.dmg"
   name "NetEase POPO"
