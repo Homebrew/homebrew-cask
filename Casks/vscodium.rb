afd103634a40bebe38a934d74f7513cb8ec01dd7
Vin E. R. Yap <vineryap@gmail.com>
2022-04-28 10:01:10 +0900

    vscodium: rebuild
    * Added vscodium arm64
    * Added arm64 sha256
    * Fixed style
    
    Closes #122621.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/vscodium.rb
+++ Casks/vscodium.rb
@@ -1,8 +1,15 @@
 cask "vscodium" do
-  version "1.66.2"
-  sha256 "7163ee27353cba1d8477da3a39001db0c67a71bef19799af3e16ff9367c93e10"
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  url "https://github.com/VSCodium/vscodium/releases/download/#{version}/VSCodium.x64.#{version}.dmg"
+  version "1.66.2"
+
+  if Hardware::CPU.intel?
+    sha256 "7163ee27353cba1d8477da3a39001db0c67a71bef19799af3e16ff9367c93e10"
+  else
+    sha256 "3685ade606cb51065895938081b42aa6fd15c92ff5e258f7181be7df1920c549"
+  end
+
+  url "https://github.com/VSCodium/vscodium/releases/download/#{version}/VSCodium.#{arch}.#{version}.dmg"
   name "VSCodium"
   desc "Binary releases of VS Code without MS branding/telemetry/licensing"
   homepage "https://github.com/VSCodium/vscodium"
