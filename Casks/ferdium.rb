874bb174c2f27e5a54262e9ab808b9f3bbd58d15
Aditya Mangalampalli <aditya.mangalampalli@gmail.com>
2022-04-27 18:55:33 -0700

    Adding Ferdium formula

---
+++ Casks/ferdium.rb
@@ -0,0 +1,42 @@
+cask "ferdium" do
+    arch = Hardware::CPU.intel? ? "" : "-arm64"
+
+    version "6.0.0-nightly.12"
+
+    if Hardware::CPU.intel?
+      sha256 "505b4a96acc6236a520c6ed75264f8f2e7e5a5458b7472dd058fe7359b6b139e"
+    else
+      sha256 "ec7ccceba08f1c581290d6ce4f5fa5478bed2c713c592d0298856f7b2719f35d"
+    end
+
+    url "https://github.com/ferdium/ferdium-app/releases/download/v#{version}/Ferdium-#{version}#{arch}.dmg",
+        verified: "github.com/ferdium/ferdium-app/"
+    name "Ferdium"
+    desc "The hard-fork of @getferdi, adding awesome features and removing the unwanted owner."
+    homepage "https://ferdium.org/"
+
+    livecheck do
+      url :url
+      strategy :github_latest
+    end
+
+    auto_updates true
+
+    app "Ferdium.app"
+
+    uninstall quit:   "com.ferdium.ferdium-app",
+              delete: "/Library/Logs/DiagnosticReports/Ferdium Helper_.*wakeups_resource.diag"
+
+    zap trash: [
+      "~/Library/Application Support/Caches/ferdium-updater",
+      "~/Library/Application Support/Ferdium",
+      "~/Library/Caches/com.ferdium.ferdium-app",
+      "~/Library/Caches/com.ferdium.ferdium-app.ShipIt",
+      "~/Library/Logs/Ferdium",
+      "~/Library/Preferences/ByHost/com.ferdium.ferdium-app.ShipIt.*.plist",
+      "~/Library/Preferences/com.electron.ferdium.helper.plist",
+      "~/Library/Preferences/com.electron.ferdium.plist",
+      "~/Library/Preferences/com.ferdium.ferdium-app.plist",
+      "~/Library/Saved Application State/com.ferdium.ferdium-app.savedState",
+    ]
+  end
