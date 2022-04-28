ed278d62b7065c32fdcd40374f7e7003c2c59c8c
nitrocode <nitrocode@users.noreply.github.com>
2022-04-28 10:34:11 -0500

    leapp 0.12.0
    Update leapp from 0.11.0 to 0.12.0
    
    Closes #122665.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/leapp.rb
+++ Casks/leapp.rb
@@ -1,12 +1,12 @@
 cask "leapp" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  version "0.11.0"
+  version "0.12.0"
 
   if Hardware::CPU.intel?
-    sha256 "d8395dce82ee759ead020212d782abcd56ceaaa88d37ba875e3d13589c7203bf"
+    sha256 "53c88bdc7a015067408ba5438f4c3565601f9ae3341cab9800dd69806a245c7d"
   else
-    sha256 "51021c3b4e005122e4827d7c2faa4770fa3e7d6ce6b91840ecf3cd9fe93f2ece"
+    sha256 "e98f7590172db95a9d5c7a6ac0eb4c12adfbce588036b5e8d0995aca3db16b6d"
   end
 
   url "https://asset.noovolari.com/#{version}/Leapp-#{version}#{arch}.dmg",
