0bf04fe294a1424251448e4c5b3bb10db33d76f1
Matt Coster <opensource@mtcoster.net>
2022-04-28 09:37:08 +0100

    protokol 0.4.4

---
+++ Casks/protokol.rb
@@ -0,0 +1,28 @@
+cask "protokol" do
+  version "0.4.4,86"
+  sha256 "8a8cff15d5a81fc3aa3e86fc4f8d42561ad416849c6f6f643c3a7ecf4eb6f08d"
+
+  url "https://hexler.net/pub/protokol/protokol-#{version.csv.first}.#{version.csv.second}-macos.dmg"
+  name "protokol"
+  desc "MIDI and OSC Monitor"
+  homepage "https://hexler.net/protokol"
+
+  livecheck do
+    url "https://hexler.net/protokol/appcast/macos/"
+    strategy :sparkle
+  end
+
+  auto_updates true
+
+  app "Protokol.app"
+
+  zap rmdir: "~/Documents/Protokol",
+      trash: [
+        "~/Library/Application Support/net.hexler.Protokol",
+        "~/Library/Caches/net.hexler.Protokol",
+        "~/Library/HTTPStorages/net.hexler.Protokol",
+        "~/Library/HTTPStorages/net.hexler.Protokol.binarycookies",
+        "~/Library/Logs/net.hexler.Protokol",
+        "~/Library/Preferences/net.hexler.Protokol.plist",
+      ]
+end
