4d5903ab7ba79dadcdb7be84653300bd17f7df7a
neutric <5984479+neutric@users.noreply.github.com>
2022-04-29 01:01:56 +0200

    Update pagico from 10,20220301 to 10,20220420

--- Casks/pagico.rb
+++ Casks/pagico.rb
@@ -1,6 +1,6 @@
 cask "pagico" do
-  version "10,20220301"
-  sha256 "88982ef29d86b3d01b57af620e2b399fbf8342163e0b0bb815fd309d14f9bf13"
+  version "10,20220420"
+  sha256 "bba0edef168ebe1e56c9a9ec11b97d469eb4012602b31382b9d40d0fcdfafa42"
 
   url "https://www.pagico.com/downloads/Pagico_macOS_r#{version.csv.second}.dmg"
   name "Pagico"
