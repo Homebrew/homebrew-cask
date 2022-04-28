ec30a5e06dfa432000d241ad9c4f147aad9d67aa
Vin E. R. Yap <vineryap@gmail.com>
2022-04-28 10:01:10 +0900

    Added vscodium arm64

--- Casks/vscodium.rb
+++ Casks/vscodium.rb
@@ -1,8 +1,10 @@
 cask "vscodium" do
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
+
   version "1.66.2"
   sha256 "7163ee27353cba1d8477da3a39001db0c67a71bef19799af3e16ff9367c93e10"
 
-  url "https://github.com/VSCodium/vscodium/releases/download/#{version}/VSCodium.x64.#{version}.dmg"
+  url "https://github.com/VSCodium/vscodium/releases/download/#{version}/VSCodium.#{arch}.#{version}.dmg"
   name "VSCodium"
   desc "Binary releases of VS Code without MS branding/telemetry/licensing"
   homepage "https://github.com/VSCodium/vscodium"
