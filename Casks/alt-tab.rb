5798bd47c1a4d30c8a6d4e51f5c2d1b4a443dd79
Reno <25192197+singularitti@users.noreply.github.com>
2022-04-28 22:17:16 -0400

    Update alt-tab from 6.33.0 to 6.34.0 (#122663)

--- Casks/alt-tab.rb
+++ Casks/alt-tab.rb
@@ -1,6 +1,6 @@
 cask "alt-tab" do
-  version "6.33.0"
-  sha256 "401b05de707497bb306ab1d33292a72a6bf2f66dc79811b32029e41247523f06"
+  version "6.34.0"
+  sha256 "85791a9f5fec9f3fca4da9e2a709e73d9019bdc470067cab97142174da678ceb"
 
   url "https://github.com/lwouis/alt-tab-macos/releases/download/v#{version}/AltTab-#{version}.zip"
   name "AltTab"
