0e8b1f203520a24e3ea695f03ddbe7cf762d3509
Porkepix <porkepix@gmail.com>
2022-04-29 22:59:14 +0200

    Update amitv87-pip from 2.00 to 2.03

--- Casks/amitv87-pip.rb
+++ Casks/amitv87-pip.rb
@@ -1,6 +1,6 @@
 cask "amitv87-pip" do
-  version "2.00"
-  sha256 "f8ed6b7b1c8e800c5ca5b571f512edbbf21bd9641179e6e4cce7e6605b16f7f4"
+  version "2.03"
+  sha256 "b06ab33ce89bb24ea77539d31272a07e03a571f84df0eb97e4d542bb3b6c0bc2"
 
   url "https://github.com/amitv87/PiP/releases/download/v#{version}/PiP-#{version}.dmg"
   name "PiP"
