5d1c0ea73a38f741e7a518af178e5e5961460294
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-04-29 19:29:57 +0800

    Update wpsoffice-cn.rb

--- Casks/wpsoffice-cn.rb
+++ Casks/wpsoffice-cn.rb
@@ -18,7 +18,7 @@ cask "wpsoffice-cn" do
     url :homepage
     regex(/href=.*?WPS_Office[._-]v?(\d+(?:\.\d+)+)[_(](\d+)[_)]*#{arch}\.dmg/i)
     strategy :page_match do |page|
-      page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
+      page.scan(regex).map {|match| "#{match[0]},#{match[1]}"}
     end
   end
 
