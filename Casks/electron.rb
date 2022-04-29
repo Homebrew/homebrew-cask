0c3bb30e9dd523ea20c46207be31fe3330758029
Porkepix <porkepix@gmail.com>
2022-04-29 10:23:42 +0200

    electron 18.2.0
    Update electron from 18.1.0 to 18.2.0
    
    Closes #122710.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,12 +1,12 @@
 cask "electron" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "18.1.0"
+  version "18.2.0"
 
   if Hardware::CPU.intel?
-    sha256 "24dd64a66b820c9553c5e5570907da6c98e808d33fac98072b9c2a8f1659cb14"
+    sha256 "43b4443bee220e11bd9ed50c9daecce9d85b1f2827c386c16ced1f7dbf2d9760"
   else
-    sha256 "97adf13306c9b3b304d3e9ddf68f5f7fb9b79c9a1342114e3671182f3cc9e808"
+    sha256 "64ec6623664f8e2f0925714778dc64f8556db28dea4060507ee6de65a692e0f9"
   end
 
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-#{arch}.zip",
