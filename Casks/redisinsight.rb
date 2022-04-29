21c328cc4bb4ca8d35c8be25436986ab64fd48e8
Bevan Kay <email@bevankay.me>
2022-04-29 22:33:51 +1000

    Update redisinsight.rb

--- Casks/redisinsight.rb
+++ Casks/redisinsight.rb
@@ -4,7 +4,7 @@ cask "redisinsight" do
   version "2.0.6"
   sha256 :no_check
 
-  url "https://download.redisinsight.redis.com/latest/RedisInsight-v2-mac-#{arch}.dmg"
+  url "https://download.redisinsight.redis.com/latest/RedisInsight-v#{version.major}-mac-#{arch}.dmg"
   name "RedisInsight"
   desc "GUI for streamlined Redis application development"
   homepage "https://redis.com/redis-enterprise/redis-insight/"
@@ -14,10 +14,10 @@ cask "redisinsight" do
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
-  app "RedisInsight-v2.app", target: "RedisInsight.app"
+  app "RedisInsight-v#{version.major}.app", target: "RedisInsight.app"
 
   zap trash: [
-    "~/Library/Preferences/org.RedisLabs.RedisInsight-V2.plist",
-    "~/Library/Saved Application State/org.RedisLabs.RedisInsight-V2.savedState",
+    "~/Library/Preferences/org.RedisLabs.RedisInsight-V#{version.major}.plist",
+    "~/Library/Saved Application State/org.RedisLabs.RedisInsight-V#{version.major}.savedState",
   ]
 end
