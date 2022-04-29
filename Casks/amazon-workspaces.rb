84b8293cbf4e51b2ca9c688e44b397408c012358
Eric Martin <eric@ericdoesinfosec.com>
2022-04-29 09:14:15 -0400

    versioning to 7.0.7.2248

--- Casks/amazon-workspaces.rb
+++ Casks/amazon-workspaces.rb
@@ -1,8 +1,8 @@
 cask "amazon-workspaces" do
-  version "rolling"
-  sha256 :no_check
+  version "4.0.7.2248"
+  sha256 "6f4f61172dc62b75cb1de3f8a5afbc2df691ff5326c1c69bcaea4f59d80a6cc3"
 
-  url "https://d2td7dqidlhjx7.cloudfront.net/prod/global/osx/WorkSpaces.pkg",
+  url "https://d2td7dqidlhjx7.cloudfront.net/prod/global/osx/WorkSpaces_AllProducts_#{version.split(".")[-1]}.zip",
       verified: "d2td7dqidlhjx7.cloudfront.net/prod/global/osx/"
   name "Amazon Workspaces"
   desc "Cloud native persistent desktop virtualization"
