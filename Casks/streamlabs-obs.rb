3b2d3ef058918a4bb41d9ffa28ea9abefcaa87af
eloyaldea <eloy.aldea@gmail.com>
2022-04-29 16:19:43 +0200

    Update streamlabs-obs from 1.7.0 to 1.8.3

--- Casks/streamlabs-obs.rb
+++ Casks/streamlabs-obs.rb
@@ -1,6 +1,6 @@
 cask "streamlabs-obs" do
-  version "1.7.0"
-  sha256 "d9b0c84edfb32c851dfcec1cd58e9f431fcc40adcb241041f819bec2cb18d015"
+  version "1.8.3"
+  sha256 "0da6ca1c185b38f31c7d54f389338694c3ee6ec000f0945980fc26e829f0f449"
 
   url "https://slobs-cdn.streamlabs.com/Streamlabs+Desktop-#{version}.dmg"
   name "Streamlabs Desktop"
