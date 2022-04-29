0cd09de056a54ed73d5584aa223baec1216504f5
peeweep <peeweep@0x0.ee>
2022-04-29 05:57:27 +0000

    Update wechatwork from 4.0.3.99066 to 4.0.6.90539
    Signed-off-by: peeweep <peeweep@0x0.ee>

--- Casks/wechatwork.rb
+++ Casks/wechatwork.rb
@@ -1,7 +1,7 @@
 cask "wechatwork" do
   if Hardware::CPU.intel?
-    version "4.0.3.90492"
-    sha256 "c7306cf208cbdc1e4b0dbcfd96f981d04dff5875616ada7d1d369c214be39a4e"
+    version "4.0.6.90539"
+    sha256 "2ead9f3083492edf07bec85c4e41f0a8ae8219869786708e883b80d2c35c7238"
   else
     version "4.0.3.99066"
     sha256 "16ef032ea837b86d5010abdb3a371f70402fd733d3aa1b465a520fe1011999d4"
