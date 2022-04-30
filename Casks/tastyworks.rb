cd521e6d780ec67fb94d05b316f81f9753225f7e
Brady Owens <brady@fastglass.net>
2022-04-29 21:00:09 -0400

    Tastyworks upgrade to 1.22

--- Casks/tastyworks.rb
+++ Casks/tastyworks.rb
@@ -1,16 +1,11 @@
 cask "tastyworks" do
-  version "1.14.2"
-  sha256 "09dec162db321904a25d196134da699dc7d79a1c17fa7134d81fc11677802f1b"
+  version "1.22.0"
+  sha256 "e7e56d8aed91594c6259f47fffb0326c3ca7bda2eb0ad2a10c487d819b11ee26"
 
   url "https://download.tastyworks.com/desktop-#{version.major}.x.x/#{version}/tastyworks-#{version}.dmg"
   name "tastyworks"
   desc "Desktop trading platform for the tastyworks brokerage"
   homepage "https://tastyworks.com/"
-
-  livecheck do
-    url "https://tastyworks.com/component---src-pages-technology-js-c0073caebcf082ded47f.js"
-    regex(%r{/tastyworks-(\d+(?:\.\d+)*)}i)
-  end
 
   auto_updates true
 
