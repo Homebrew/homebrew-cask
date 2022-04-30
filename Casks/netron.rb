05ae7083074e354192c755d8f51c565b9c2f8d3d
Lutz <lutzroeder@users.noreply.github.com>
2022-04-30 02:03:00 +0000

    netron 5.7.5
    Update Netron to 5.7.5
    
    Closes #122753.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/netron.rb
+++ Casks/netron.rb
@@ -1,6 +1,6 @@
 cask "netron" do
-  version "5.7.4"
-  sha256 "6badda1aeb1274e9f0243fff4a1ca4acdff34a2cf3c38fb6d816a14cc424258e"
+  version "5.7.5"
+  sha256 "496892ebd3e17a703c60a2945ec221242d673159761a456da4eb4caa8ad9ca2f"
 
   url "https://github.com/lutzroeder/netron/releases/download/v#{version}/Netron-#{version}-mac.zip"
   name "Netron"
