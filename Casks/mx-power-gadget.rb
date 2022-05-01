596ec7b5135c9ebb45663d6c461675e8424ce13d
Marcin Mitura <mitura07@wp.pl>
2022-05-01 16:28:12 +0200

    Adding cask for Mx Power Gadget

---
+++ Casks/mx-power-gadget.rb
@@ -0,0 +1,11 @@
+cask "mx-power-gadget" do
+    version "1.2"
+    sha256 "702f6ee1e533334bd6ffa42c168b20ad4d39a4c9a197fd0ece7e4b196bfdb929"
+  
+    url "https://www.seense.com/menubarstats/mxpg/updateapp/mxpg.zip"
+    name "Mx Power Gadget"
+    desc "Power management and monitoring for Apple Mx processors"
+    homepage "https://www.seense.com/menubarstats/mxpg/"
+  
+    app "Mx Power Gadget.app"
+  end
