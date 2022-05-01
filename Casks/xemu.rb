5ea4afe834a01c4e962896818abc5e367d533752
Marcin Mitura <mitura07@wp.pl>
2022-05-01 15:48:34 +0200

    Update xemu formulae to v0.6.5 (#122757)
    * Update xemu formulae to v0.6.5
    
    * Update xemu.rb
    
    * Typo
    
    * Fixed url for brew audit
    
    * Remove livecheck

--- Casks/xemu.rb
+++ Casks/xemu.rb
@@ -1,17 +1,12 @@
 cask "xemu" do
-  version "0.6.3-10-g292c9703de"
-  sha256 "1ac9603cb819f76dcd1e10fed53683c6237b4c4c33d202f056d4d9fa6deb69d4"
+  version "0.6.5"
+  sha256 "16964c0e86a95b7c4320c75503198f4f576eb3160c1edb0c2ad07a76e9009ba1"
 
-  url "https://github.com/mborgerson/xemu/releases/download/gh-release%2F#{version}/xemu-macos-universal-release.zip",
+  url "https://github.com/mborgerson/xemu/releases/download/v#{version}/xemu-macos-universal-release.zip",
       verified: "github.com/mborgerson/xemu/"
   name "Xemu"
   desc "Original Xbox Emulator"
   homepage "https://xemu.app/"
-
-  livecheck do
-    url :url
-    regex(%r{^gh-release/(.*)$}i)
-  end
 
   app "Xemu.app"
 
