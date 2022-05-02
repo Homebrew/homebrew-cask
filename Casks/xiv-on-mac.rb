87878a06a2a5efc5ca89db43aa5e975a1becdfa8
Jakob Yanagibashi <git22@jakob.page>
2022-05-03 01:52:22 +0200

    Add "XIV on Mac" v3.6.2 (#122208)
    * Add XIV on Mac
    
    * Add comma after last item in Zap stanza
    
    * Add macOS version requirement and auto-update info
    
    * Update "XIV on Mac" to v3.5.6
    
    * Update "XIV on Mac" to v3.5.7
    
    * Update "XIV on Mac" to v3.5.8
    
    * Update "XIV on Mac" to v3.6
    
    * Update "XIV on Mac" to v3.6.2
    
    * Change name to "XIV on Mac"
    
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>
    
    * Sort "XIV on Mac" zap stanza alphabetically
    
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>

---
+++ Casks/xiv-on-mac.rb
@@ -0,0 +1,26 @@
+cask "xiv-on-mac" do
+  version "3.6.2"
+  sha256 "c60ae1731d6debc4e67bd88bdc64b31039d542bd5e6b6d30015bfbae3c8cd455"
+
+  url "https://softwareupdate.xivmac.com/sites/default/files/update_data/XIV%20on%20Mac#{version}.tar.xz"
+  name "XIV on Mac"
+  desc "Wine wrapper, setup tool and launcher for FFXIV"
+  homepage "https://www.xivmac.com/"
+
+  livecheck do
+    url "https://softwareupdate.xivmac.com/sites/default/files/update_data/xivonmac_appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+  depends_on macos: ">= :big_sur"
+
+  app "XIV on Mac.app"
+
+  zap trash: [
+    "~/Library/Application Support/XIV on Mac",
+    "~/Library/Caches/dezent.XIV-on-Mac",
+    "~/Library/HTTPStorages/dezent.XIV-on-Mac",
+    "~/Library/Preferences/dezent.XIV-on-Mac.plist",
+  ]
+end
