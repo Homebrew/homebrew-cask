67aaa8de010e0e4cd4c184e989280b446dddbf60
Marcin Mitura <mitura07@wp.pl>
2022-05-02 08:23:28 +0200

    typos

--- Casks/mx-power-gadget.rb
+++ Casks/mx-power-gadget.rb
@@ -1,7 +1,7 @@
 cask "mx-power-gadget" do
     version "1.2,20220322"
     sha256 "702f6ee1e533334bd6ffa42c168b20ad4d39a4c9a197fd0ece7e4b196bfdb929"
-  
+    
     url "https://www.seense.com/menubarstats/mxpg/updateapp/mxpg.zip"
     name "Mx Power Gadget"
     desc "Power management and monitoring for Apple Mx processors"
@@ -18,4 +18,4 @@ cask "mx-power-gadget" do
         "~/Library/Caches/com.fabriceleyne.MxPowerGadget",
         "~/Library/Preferences/com.fabriceleyne.MxPowerGadget.plist",
       ]
-  end
+end
