0c5fbaa300700ba4af6f778b09f6416efecba0e4
core_contingency <ccontingency@gmail.com>
2022-05-02 10:09:24 -0700

    Update checksum of kstars

--- Casks/kstars.rb
+++ Casks/kstars.rb
@@ -1,6 +1,6 @@
 cask "kstars" do
   version "3.5.8"
-  sha256 "1404d114c4340b95ca0bdb1a5320e4db06b1fac6e55ee09500102137714c8173"
+  sha256 "877cf01526f51b8ef6a439dafe2cb7664a26dd97149221d1cde5b589cbf80c62"
 
   url "https://www.indilib.org/jdownloads/kstars/kstars-#{version}.dmg",
       verified: "indilib.org/jdownloads/kstars/"
