b0c2a56677147370537536e1170e26c0fd78af84
Marcin Mitura <mitura07@wp.pl>
2022-05-02 08:31:08 +0200

    Update mx-power-gadget.rb

--- Casks/mx-power-gadget.rb
+++ Casks/mx-power-gadget.rb
@@ -1,7 +1,7 @@
 cask "mx-power-gadget" do
     version "1.2,20220322"
     sha256 "702f6ee1e533334bd6ffa42c168b20ad4d39a4c9a197fd0ece7e4b196bfdb929"
-    
+
     url "https://www.seense.com/menubarstats/mxpg/updateapp/mxpg.zip"
     name "Mx Power Gadget"
     desc "Power management and monitoring for Apple Mx processors"
@@ -17,5 +17,5 @@ cask "mx-power-gadget" do
     zap trash: [
         "~/Library/Caches/com.fabriceleyne.MxPowerGadget",
         "~/Library/Preferences/com.fabriceleyne.MxPowerGadget.plist",
-      ]
+    ]
 end
