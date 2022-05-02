84a334bcfc370c3678dee3bf7b6227c022ddce47
Lawrence <lawrencews@jhu.edu>
2022-05-02 08:58:27 -0400

    firefox: rebuild
    Update firefox from 99.0.1 to 99.0.1
    
    Closes #122824.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/firefox.rb
+++ Casks/firefox.rb
@@ -1,6 +1,30 @@
 cask "firefox" do
   version "99.0.1"
 
+  language "af" do
+    sha256 "62dc390d46a3ddb3ae98f861b639751903e279f468970d0a5549192e0085b5fd"
+    "af"
+  end
+  language "ar" do
+    sha256 "d76ee05cd76d6205cb6bb018e9b15e80cccc820318eefcea3de7f6f5b7ac6b0d"
+    "ar"
+  end
+  language "be" do
+    sha256 "60182bcf9da8f587e293f504c42a86ba7dc07c4d95a9fcbf6607904da9cf59a5"
+    "be"
+  end
+  language "bg" do
+    sha256 "69a1dcae5b59ba0a141126c341b3dfe063541d17f57c0123f756821a546c1424"
+    "bg"
+  end
+  language "bn" do
+    sha256 "4442927b361b1afbe75c647058912baa9a7738dad9735b064242f9929b0e3565"
+    "bn"
+  end
+  language "ca" do
+    sha256 "3485406768ba30eec02e4605e90bf2dfb302f27fbcd6a215108c36f71358a3e5"
+    "ca"
+  end
   language "cs" do
     sha256 "a292fb70e1c24da224b6a7677068e2cb33957f4ee920aaa1e3a94158414e55f0"
     "cs"
@@ -37,6 +61,14 @@ cask "firefox" do
     sha256 "f3e93953edde7a8f4d297b1eacaa93601a6b3c686b98fe04082c55927b5e9625"
     "es-ES"
   end
+  language "fa" do
+    sha256 "e840fb424b9764a58c30e1879820f25a03366c0933cfc386ada10de319a815a5"
+    "fa"
+  end
+  language "ff" do
+    sha256 "39f835166a562224525d7ef476c0eb06691cc48d56ad5ce0faabc18b55a0f6a0"
+    "ff"
+  end
   language "fi" do
     sha256 "9098638fa395209660bf7c72608232c1c109e5699ba589b6ffcf4f9a91ed59e6"
     "fi"
@@ -48,6 +80,22 @@ cask "firefox" do
   language "gl" do
     sha256 "df4d63e7ed164dade73f7172bb9ab05d7e0e5b7af69630ff1cbabbaa64480d40"
     "gl"
+  end
+  language "gn" do
+    sha256 "688f511f7d1ccd5b7e9e485c8ec005b6c6afa973c3770d940c354ad4b98041a4"
+    "gn"
+  end
+  language "gu" do
+    sha256 "e0834cdb14e2830152133a7e8d4546b2e175c765b661af84f2e72333d54a727f"
+    "gu-IN"
+  end
+  language "he" do
+    sha256 "e789e5c058492ec3a5238edd0515faa51f8613ff75c246bc4a5c55a0ee57795e"
+    "he"
+  end
+  language "hi" do
+    sha256 "121497e0fb3192b4b21ef66acbec70b297e935f596cbb863847c9235009f162a"
+    "hi-IN"
   end
   language "in" do
     sha256 "121497e0fb3192b4b21ef66acbec70b297e935f596cbb863847c9235009f162a"
@@ -61,13 +109,33 @@ cask "firefox" do
     sha256 "9e64782bfc05f8452541b089f825b2bded24b9fc49558188c4f33e8862393b63"
     "ja-JP-mac"
   end
+  language "ka" do
+    sha256 "d32ceecf2ea231bb94d29cd5c506a6363c8c66b90100d29327aa174d8129516d"
+    "ka"
+  end
   language "ko" do
     sha256 "46a88d7584082bbf5c1a87e5a4e37e88538a877a1f7931ae12563503e5db7df3"
     "ko"
   end
+  language "mr" do
+    sha256 "e037fdc41725c58fe7c37783492db402c42632952666b9e8c16adbdddd36ae77"
+    "mr"
+  end
+  language "my" do
+    sha256 "d202309822902089ef61c48015a0e90458075a98efd2d4d1ae1158f78c365f43"
+    "my"
+  end
+  language "ne" do
+    sha256 "33cc24577497ec8c31ec84c850b4e7c15685cb3036af373abf15e410362e45b5"
+    "ne-NP"
+  end
   language "nl" do
     sha256 "9536cb1739cc95ca0081c56135bb40bb9fa15296c67529de63df4a0f2197dfe1"
     "nl"
+  end
+  language "pa-IN" do
+    sha256 "e14e0c26413643890eef4d8c957058a876022dd8c52140df4ad2c7fd5e0cfaa7"
+    "pa-IN"
   end
   language "pl" do
     sha256 "3bad4a0b8b8cd8985e309c0030dc795f7a83a94a1e285036d7b88d11a3ef8872"
@@ -85,9 +153,37 @@ cask "firefox" do
     sha256 "9d5888fe8fb8d1b79daf23c62fcf8595efe24afb9e3c5726c4f8521981cdd264"
     "ru"
   end
+  language "si" do
+    sha256 "12c175006324944577167b7fefaa562a428ccd608f7d5a90c8d5514f30747bc2"
+    "si"
+  end
+  language "sq" do
+    sha256 "7d063de7a09db2829abedc0a319c983592a0cad8fe24a0b6fe83799966e61269"
+    "sq"
+  end
+  language "sr" do
+    sha256 "c54e8b4bd4ebc6f61895ab4817b10ebe9b62df1f2a98183ff2618f8f3c05fa20"
+    "sr"
+  end
   language "sv" do
     sha256 "c5887d3800c23a6a979b184dd5753c6c78a40b90a4718faddeb3c7482aee7ce9"
     "sv-SE"
+  end
+  language "ta" do
+    sha256 "de865bb3800615cdbd036582711633d8e41c783daffa807cd3c377fa72c2b92a"
+    "ta"
+  end
+  language "te" do
+    sha256 "234031717f57be7e6f48242afec16f6f9171c4d6a645a316c684d1c2a7967561"
+    "te"
+  end
+  language "th" do
+    sha256 "d24a7c37eae6ed7f74a1e791268622371cddf9e72e2315208b892cbdfbcda6fc"
+    "th"
+  end
+  language "tl" do
+    sha256 "ff2ee27e1165862cab18a6aae112fa8675b6de708e4cb25e357043128e561f3c"
+    "tl"
   end
   language "tr" do
     sha256 "3473af1a0eb916f8680aca51dbf2e2e3107144f8cbf7d12f475b030ff15b447c"
@@ -96,6 +192,10 @@ cask "firefox" do
   language "uk" do
     sha256 "f4d0eb6251d7bab52423b1f8d5decf5cae89f457c13cc8adf602480eac913ab7"
     "uk"
+  end
+  language "ur" do
+    sha256 "307374f5121f285b5085ef67a66ed56276712e91eaccf28e2f64326c660755a4"
+    "ur"
   end
   language "zh-TW" do
     sha256 "eb65500492d1040e1d77f0dc2be53cc19ff8473b55b69277de5731586bc80e97"
