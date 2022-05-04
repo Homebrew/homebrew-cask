be03af882a67607c0e067c71c20234c785eab687
Rezart Qelibari <entwickler-kontakt@astzweig.de>
2022-05-04 14:17:01 +0200

    Shorten verified url string for nitro-pdf-pro.rb

--- Casks/nitro-pdf-pro.rb
+++ Casks/nitro-pdf-pro.rb
@@ -3,7 +3,7 @@ cask "nitro-pdf-pro" do
   sha256 "2b59fba1eedec9f2c01c2d1c19544beeca23e7f943f5dcf495674b4466e1a2d2"
 
   url "https://downloads.gonitro.com/macos/Nitro%20PDF%20Pro%20Retail_#{version}.dmg",
-      verified: "https://downloads.gonitro.com/macos/Nitro%20PDF%20Pro%20Retail_#{version}.dmg"
+      verified: "downloads.gonitro.com/macos/"
   name "Nitro PDF Pro"
   desc "PDF editing software"
   homepage "https://pdfpen.com/"
