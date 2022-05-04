e98b58467272b2cc58e7be4d9ac0019536e2fb68
Hannes Braun <hannesbraun@mail.de>
2022-05-05 01:26:05 +0200

    reaper: update sha256

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -2,7 +2,7 @@ cask "reaper" do
   version "6.56"
 
   if MacOS.version <= :mojave
-    sha256 "f13921dfe8a110fd9c4641762c9ac0bdceb8c5bb0c59e17ec58f8b7b68adfa9b"
+    sha256 "fb8df581e0dc55b0acae64400567323266037d6241f71bf5d271d77e60583f12"
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64.dmg"
   else
     sha256 "b0df797e6e94c90fd6b001486f22baf4f4283d089b1ff962e894275f1433ec45"
