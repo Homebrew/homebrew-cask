6663e23cdaa66f34a9df57a5c7afb8a011e04533
Porkepix <porkepix@gmail.com>
2022-05-04 13:35:02 +0200

    lagrange 1.13.2
    Update lagrange from 1.13.0 to 1.13.2
    
    Closes #122900.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/lagrange.rb
+++ Casks/lagrange.rb
@@ -1,12 +1,12 @@
 cask "lagrange" do
   arch = Hardware::CPU.intel? ? "macos10.13-x86_64" : "macos11.0-arm64"
 
-  version "1.13.0"
+  version "1.13.2"
 
   if Hardware::CPU.intel?
-    sha256 "0fae406c0007fc737e156cb23c2baf26a5dd500923ba02d4cece504bd7055a0d"
+    sha256 "e465d99babdb18f0504baf0f38c646d9971f804e0c1f0a98effcde307d3d79f5"
   else
-    sha256 "05733704b63b7903bc2b369ec3fad06e6d18e8dcd5a1631eb79ac54269ce02f0"
+    sha256 "19b1e702a8bc082fa462aada2b329d9c4c891ba79c4fc04731bd314078801ca3"
   end
 
   url "https://github.com/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_#{arch}.tbz",
