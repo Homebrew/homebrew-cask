aef3f8cbde0bc5c42c3a5ada8c9ea5bc6e9a8c53
fxbrit <fxbrit@tuta.io>
2022-05-05 19:01:32 +0200

    fix versions

--- Casks/librewolf.rb
+++ Casks/librewolf.rb
@@ -2,10 +2,10 @@ cask "librewolf" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    version "100.0.1,4,80b24f36c1618f7d9655c1bbc180653f"
+    version "100.0,1,80b24f36c1618f7d9655c1bbc180653f"
     sha256 "076efaaacc55c131c4e7023fd744f7baa56e323a4caed493115ecdc79c15ca1a"
   else
-    version "100.0.1,4,704954eac358497aad3338b7c6f125be"
+    version "100.0,1,704954eac358497aad3338b7c6f125be"
     sha256 "0f1b09c67dbbfd505ba0103a0fad99d2db77e37725513be6e911adc6eb06f4e2"
   end
 
