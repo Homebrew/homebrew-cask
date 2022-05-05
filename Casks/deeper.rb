195509a84953ed8b9d7ef5eb65d7f8281876e09a
i0ntempest <szf1234@me.com>
2022-05-04 22:45:21 -0400

    Update deeper from 2.7.7 to 2.7.8

--- Casks/deeper.rb
+++ Casks/deeper.rb
@@ -21,8 +21,8 @@ cask "deeper" do
     version "2.7.0"
     sha256 "695a1572294fb535bbf266f11cc1d3e1838995d762b4d841f2f7d77801e7a546"
   else
-    version "2.7.7"
-    sha256 "e87c69c5f444334a1a4bc58f73462c8073cadb96a3b9f3ab786e9359472e72e8"
+    version "2.7.8"
+    sha256 "d6e0cb3cb8248f00497fb2dcb3481f2ff71ba05f1499ea2423110e28298babbb"
   end
 
   url "https://www.titanium-software.fr/download/#{MacOS.version.to_s.delete(".")}/Deeper.dmg"
