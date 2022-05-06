45cb53ba237cdf3d8885e302e70e36d975279820
Stef de Goey <stefdegoey@outlook.com>
2022-05-06 12:56:09 +0200

    Simplified livecheck regex

--- Casks/touch-portal.rb
+++ Casks/touch-portal.rb
@@ -9,7 +9,7 @@ cask "touch-portal" do
 
   livecheck do
     url :homepage
-    regex(/\.dmg.*\n.*<br><span style="font-size: 20px;">v?(\d+(?:\.\d+)*)/i)
+    regex(/\.dmg.*\n.*v?(\d+(?:\.\d+)*)/i)
   end
 
   app "TouchPortal.app"
