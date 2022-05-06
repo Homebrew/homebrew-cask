c8cc503f322e48abf25b270069aa751f1c51cf3c
neutric <5984479+neutric@users.noreply.github.com>
2022-05-06 05:12:22 +0200

    quicksync 4.4.3,123
    Update quicksync from 4.3.3 to 4.4.3
    
    Closes #122985.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/quicksync.rb
+++ Casks/quicksync.rb
@@ -1,8 +1,8 @@
 cask "quicksync" do
-  version "4.3.3"
-  sha256 "9458bee719ca6d19ef84bfb4f1c309e8c509c88c626a71bde1c58b1f02854f38"
+  version "4.4.3,123"
+  sha256 "fb7d84bad2e917114148f2b9574fecaaaec1715311eb7dd67030bb8833264e33"
 
-  url "https://cms.gigaset.com/macsw/gqs/leopard/QuickSync_#{version}.zip"
+  url "https://cms.gigaset.com/macsw/gqs/leopard/QuickSync_#{version.csv.first}.zip"
   name "QuickSync"
   desc "Syncing software for Gigaset products"
   homepage "https://www.gigaset.com/en_en/cms/start/service/support-detail/gigaset-quicksync-mac-version.html"
