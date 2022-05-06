a9eb17374c3d680370f99e628ddc8f0ab728e829
Porkepix <porkepix@gmail.com>
2022-05-06 10:36:58 +0200

    amitv87-pip 2.11
    Update amitv87-pip from 2.10 to 2.11
    
    Closes #122991.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/amitv87-pip.rb
+++ Casks/amitv87-pip.rb
@@ -1,6 +1,6 @@
 cask "amitv87-pip" do
-  version "2.10"
-  sha256 "e1f4ee01d63b3d2df06e884d9762f46780e2b6d487b9fcc4e9c469f6d7950339"
+  version "2.11"
+  sha256 "150340a76f856e939ef9c042aa9af6f232dfb21fe3b1a454ff4d7257567da063"
 
   url "https://github.com/amitv87/PiP/releases/download/v#{version}/PiP-#{version}.dmg"
   name "PiP"
