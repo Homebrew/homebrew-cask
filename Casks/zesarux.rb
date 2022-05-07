909408578117308828580e1e62bcbae27ddd8bfb
Clément L <porkepix@gmail.com>
2022-05-07 12:38:32 +0200

    Update zesarux from 10.0 to 10.1 (#123033)

--- Casks/zesarux.rb
+++ Casks/zesarux.rb
@@ -1,6 +1,6 @@
 cask "zesarux" do
-  version "10.0"
-  sha256 "df45b2d1cf72bcb75ed13d398dfffbf33d293b3d8eccc2545cd61f30fab15b07"
+  version "10.1"
+  sha256 "f315abdd014219793ecde1f8aa2268e2d76126db3ea00b46a913ca1717250b68"
 
   url "https://github.com/chernandezba/zesarux/releases/download/ZEsarUX-#{version}/ZEsarUX_macos-#{version}.dmg"
   name "ZEsarUX"
