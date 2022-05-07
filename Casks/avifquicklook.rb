e12dfd3f5d38a90f038beb0c3f7a7a20b9903edc
Bevan Kay <email@bevankay.me>
2022-05-07 23:17:41 +1000

    Update avifquicklook.rb

--- Casks/avifquicklook.rb
+++ Casks/avifquicklook.rb
@@ -3,7 +3,7 @@ cask "avifquicklook" do
   sha256 "44cb0a387f8e1a2dcb18a9be9efc473d3a6c5935f61bd02adb7f95e683233250"
 
   url "https://github.com/dreampiggy/AVIFQuickLook/releases/download/#{version}/AVIFQuickLook.qlgenerator.zip"
-  name "avifquicklook"
+  name "AVIFQuickLook"
   desc "QuickLook Plugin for AVIF images"
   homepage "https://github.com/dreampiggy/AVIFQuickLook"
 
