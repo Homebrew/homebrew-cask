d576721144d90a0ef3f5a0b46373365289e7accc
Marcin Mitura <mitura07@wp.pl>
2022-05-08 12:58:23 +0200

    Add AetherSX2 cask

---
+++ Casks/aethersx2.rb
@@ -0,0 +1,17 @@
+cask "aethersx2" do
+  version ""
+  sha256 :no_check
+
+  url "https://aethersx2.com/archive/desktop/mac/AetherSX2-alpha-1860-mac.zip"
+  name "AetherSX2"
+  desc "Sony PlayStation 2 emulator for ARM based Macs"
+  homepage "https://aethersx2.com/"
+
+  app "AetherSX2.app"
+
+  zap trash: [
+    "~/Library/Application Support/AetherSX2",
+    "~/Library/Preferences/com.aethersx2.mac.plist",
+    "~/Library/Saved Application State/com.aethersx2.mac.savedState",
+  ]
+end
