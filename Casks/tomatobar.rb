023761a5db7527b0a51fa28d4efdda9d348d9e53
Philipp Müller <pmueller.wf@gmail.com>
2022-05-08 12:09:00 +0200

    Update Casks/tomatobar.rb
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>

--- Casks/tomatobar.rb
+++ Casks/tomatobar.rb
@@ -3,7 +3,7 @@ cask "tomatobar" do
   sha256 "ff4d465f0d70cb2e234918a650d3844352d0179e9f3742666ae98d909988dc23"
 
   url "https://github.com/ivoronin/TomatoBar/releases/download/v#{version}/TomatoBar-v#{version}.zip"
-  name "tomato-bar"
+  name "TomatoBar"
   desc "Menu bar pomodoro timer"
   homepage "https://github.com/ivoronin/TomatoBar"
 
