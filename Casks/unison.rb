4587aa908e3f3585ae9c8289ea39696d7f844528
Porkepix <porkepix@gmail.com>
2022-05-09 12:22:33 +0200

    unison: rebuild
    Update unison from 2.52.0 to 2.52.1
    
    Closes #123107.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/unison.rb
+++ Casks/unison.rb
@@ -6,8 +6,8 @@ cask "unison" do
     url "https://github.com/bcpierce00/unison/releases/download/#{version}/Unison-OS-X-#{version}.zip",
         verified: "github.com/bcpierce00/unison/"
   else
-    version "2.52.0,4.12.1"
-    sha256 "473d26c269ce63390426c32e7e43cc135424475670c5d1d7ee36783886c66ade"
+    version "2.52.1,4.14.0"
+    sha256 "d2c87075e460d74641ae8a955703de5cf5344d9affdf4e2f6a8fc5d971c4246c"
 
     url "https://github.com/bcpierce00/unison/releases/download/v#{version.csv.first}/Unison-v#{version.csv.first}.ocaml-#{version.csv.second}.macos-10.15.app.tar.gz",
         verified: "github.com/bcpierce00/unison/"
