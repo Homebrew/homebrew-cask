2f0d1452d3e3cc04c0d9f6c20fdcd722173fb49e
Jake Good <jake@developstuff.com>
2022-05-10 07:47:42 -0500

    Update playdate-simulator from 1.10.0 to 1.11.0

--- Casks/playdate-simulator.rb
+++ Casks/playdate-simulator.rb
@@ -1,6 +1,6 @@
 cask "playdate-simulator" do
-  version "1.10.0"
-  sha256 "92ae1df03f4b663c892d59db8610bce79b19c3a7f5444a7ca86bec386e04ab15"
+  version "1.11.0"
+  sha256 "05c3d1e73067ee578bd6e7bcdca254b3b202ad35ad726ca7225051df1c4fdf67"
 
   url "https://download-keycdn.panic.com/playdate_sdk/PlaydateSDK-#{version}.zip",
       verified: "download-keycdn.panic.com/playdate_sdk/"
