8ff2177bfab9057e4d61241a287ce622e2fd5493
Zihua Li <i@zihua.li>
2022-05-10 08:29:42 +0800

    Add Medis.app v2.8.3 (#123039)
    * Add Medis.app v2.8.3
    
    * Update medis.rb
    
    * update livecheck
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

---
+++ Casks/medis.rb
@@ -0,0 +1,23 @@
+cask "medis" do
+  version "2.8.3"
+  sha256 "eacc9138cf6014ce07c16c7ed75a0372074b4bf424ebc956649ae7b744dfb53f"
+
+  url "https://api.getmedis.com/medis-#{version}.zip"
+  name "Medis"
+  desc "Modern GUI for Redis"
+  homepage "https://getmedis.com/"
+
+  livecheck do
+    url "https://api.appcenter.ms/v0.1/public/sparkle/apps/d33d1248-5582-4304-b45c-cb40d51da2ab"
+    strategy :sparkle, &:short_version
+  end
+
+  app "Medis.app"
+
+  zap trash: [
+    "~/Library/Application Support/li.zihua.medis*",
+    "~/Library/Caches/li.zihua.medis*",
+    "~/Library/HTTPStorages/li.zihua.medis*",
+    "~/Library/Preferences/li.zihua.medis*.plist",
+  ]
+end
