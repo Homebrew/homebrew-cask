3f7664950df53af5b2661ff384a31f9ccd2718a7
tiiiecherle <tiiiecherle@gmail.com>
2022-05-11 07:46:52 +0200

    Update coteditor from 4.2.0 to 4.2.1

--- Casks/coteditor.rb
+++ Casks/coteditor.rb
@@ -12,8 +12,8 @@ cask "coteditor" do
     version "4.0.9"
     sha256 "969e891f4a36146c317150806fee01559d177f956734595c73537affc8897e79"
   else
-    version "4.2.0"
-    sha256 "0c381f58db6a634b28a9bf88733661b826392d8052513c65221882f36e12c885"
+    version "4.2.1"
+    sha256 "824f89477daa6ed853ef775b6ee3dfceb13f5b9b836335b7a39479965029de3c"
   end
 
   url "https://github.com/coteditor/CotEditor/releases/download/#{version}/CotEditor_#{version}.dmg",
