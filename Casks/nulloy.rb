65d1f2944d25155381daa49e4375670978f2a9cc
Josh <linkingparents@gmail.com>
2022-05-11 15:05:17 -0300

    Update nulloy from 0.9.1 to 0.9.5

--- Casks/nulloy.rb
+++ Casks/nulloy.rb
@@ -1,6 +1,6 @@
 cask "nulloy" do
-  version "0.9.1"
-  sha256 "a32ec59f32dc3ebe473cebce8f6a44f03d2a72d5062615e0e187d9b0a0815f0d"
+  version "0.9.5"
+  sha256 "386d2935b427460e44bcd46b0b2e6ca1015f066b7d5e65097815d86bf4aee0ac"
 
   url "https://github.com/nulloy/nulloy/releases/download/#{version}/Nulloy-#{version}-x86_64.dmg",
       verified: "github.com/nulloy/nulloy/"
