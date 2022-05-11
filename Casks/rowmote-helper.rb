0e60dd29aad5f3732b143e39a402f8e516506cfb
neutric <5984479+neutric@users.noreply.github.com>
2022-05-11 23:58:31 +0200

    Update rowmote-helper from 4.2.4 to 4.2.5

--- Casks/rowmote-helper.rb
+++ Casks/rowmote-helper.rb
@@ -1,6 +1,6 @@
 cask "rowmote-helper" do
-  version "4.2.4"
-  sha256 "552cdb8acd46758caa1944b46e5d3f2b53f4cbf1da70b195fc35a18804370cfd"
+  version "4.2.5"
+  sha256 "9646af824fcfcbddbf71591408bd02137b65554650418574ddc5ba0c45cd59f4"
 
   url "https://regularrateandrhythm.com/rowmote-pro/rh/rowmote-helper-#{version}.zip"
   name "Rowmote Helper"
