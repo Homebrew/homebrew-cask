f161d966679572cdec4db6e54e5f82376e6bc348
David Crosby <dave@dafyddcrosby.com>
2022-05-11 09:00:48 -0600

    remove -intel from version

--- Casks/mindforger.rb
+++ Casks/mindforger.rb
@@ -1,8 +1,8 @@
 cask "mindforger" do
-  version "1.54.0-20220307.225449-intel"
+  version "1.54.0-20220307.225449"
   sha256 "234b50d22d126eedaefcac5f3d331d1d913b2140b3ea4869f92042f2a5216c26"
 
-  url "https://github.com/dvorka/mindforger/releases/download/#{version.split("-").first}/mindforger-#{version}.dmg",
+  url "https://github.com/dvorka/mindforger/releases/download/#{version.split("-").first}/mindforger-#{version}-intel.dmg",
       verified: "github.com/dvorka/mindforger/"
   name "MindForger"
   desc "Thinking notebook and Markdown IDE"
@@ -10,7 +10,7 @@ cask "mindforger" do
 
   livecheck do
     url :url
-    regex(%r{href=.*?/mindforger-(\d+\.\d+\.\d+-\d{8}\.\d{6}-intel)\.dmg}i)
+    regex(%r{href=.*?/mindforger-(\d+\.\d+\.\d+-\d{8}\.\d{6})-intel\.dmg}i)
     strategy :github_latest do |page, regex|
       match = page.match(regex)
       next if match.blank?
