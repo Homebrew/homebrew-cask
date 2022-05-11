29ac1cda2e10f2365ac56f0686e174f6cce11c41
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-11 08:30:32 +0800

    Update jquake.rb

--- Casks/jquake.rb
+++ Casks/jquake.rb
@@ -1,16 +1,16 @@
 cask "jquake" do
+  arch = Hardware::CPU.intel? ? "intel" : "arm64"
+
   version "1.6.2"
 
   if Hardware::CPU.intel?
     sha256 "1a2ce77c84d730457b9e1309e4edc64ecd0c6021f3cfeb7ae124ab7a76eb2688"
-    arch = "intel"
   else
     sha256 "c02f0e078bebdce582ee9c04d7facf530c072c682a0f85240393f93cc80b4d92"
-    arch = "arm64"
   end
 
   url "https://fleneindre.github.io/downloads/JQuake_#{version}_mac_#{arch}.dmg",
-      verified: "fleneindre.github.io/"
+      verified: "fleneindre.github.io/downloads/"
   name "JQuake"
   desc "Real-time earthquake monitoring software for Japan"
   homepage "https://jquake.net/"
@@ -21,6 +21,8 @@ cask "jquake" do
       JSON.parse(page)["version"]
     end
   end
+
+  depends_on macos: ">= :catalina"
 
   app "JQuake.app"
 
