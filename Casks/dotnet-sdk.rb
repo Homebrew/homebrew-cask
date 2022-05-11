f63e1e2a531f19a253dd456e59589aa91128d91f
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2022-05-11 17:01:45 +0100

    Update dotnet-sdk from 6.0.202 to 6.0.300

--- Casks/dotnet-sdk.rb
+++ Casks/dotnet-sdk.rb
@@ -5,8 +5,8 @@ cask "dotnet-sdk" do
     version "6.0.300,a048efaf-c4ee-4c22-a987-2bda2a58253c,3ddde00d1b2cf420a0d870df26bc56f8"
     sha256 "c5a9890bf2b83e8f6b307f9e8a8704d0d31c20be19ff3785978a1bb31ff788cb"
   else
-    version "6.0.202,605dfd20-32ac-470a-b366-cbb293ce4cfe,9c1eec28d0e6068721026a5f77af2e60"
-    sha256 "7d5c2f37cfc2fee080deac1d972f8413c84d97532157a346bac72f607b91a7c5"
+    version "6.0.300,9e14be2d-f676-41ca-8483-ca43bb9c2fc6,7c647915c92303c11a444f88c66f174f"
+    sha256 "409ce62821c3ae3ae8fd800c48f7d109dfffcf11b6462dd2466a5398bd94a4df"
   end
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv.second}/#{version.csv.third}/dotnet-sdk-#{version.csv.first}-osx-#{arch}.pkg"
