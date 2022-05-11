3c74d24146184a4371be6f7c228a81ad211c80bb
Aditya Mangalampalli <aditya.mangalampalli@gmail.com>
2022-05-10 18:50:12 -0700

    Adding Ferdium 6.0.0-nightly.12 formula (#122631)
    * Adding Ferdium formula
    
    * Fix description
    
    * Update ferdium.rb
    
    * Fixed formatting
    
    * Update ferdium.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/ferdium.rb
@@ -0,0 +1,37 @@
+cask "ferdium" do
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
+
+  version "6.0.0-nightly.31"
+
+  if Hardware::CPU.intel?
+    sha256 "a56b30db3678b1d80ac88f25fec1b6c66c90753ab9fe191344ae68bd7b3fcc1a"
+  else
+    sha256 "8901c1f4df92c19c5c8db8f9d734d44796410cec336f7fd1306b85baac2565fa"
+  end
+
+  url "https://github.com/ferdium/ferdium-app/releases/download/v#{version}/Ferdium-#{version}#{arch}.dmg",
+      verified: "github.com/ferdium/ferdium-app/"
+  name "Ferdium"
+  desc "Multi-platform multi-messaging app"
+  homepage "https://ferdium.org/"
+
+  auto_updates true
+
+  app "Ferdium.app"
+
+  uninstall quit:   "com.ferdium.ferdium-app",
+            delete: "/Library/Logs/DiagnosticReports/Ferdium Helper_.*wakeups_resource.diag"
+
+  zap trash: [
+    "~/Library/Application Support/Caches/ferdium-updater",
+    "~/Library/Application Support/Ferdium",
+    "~/Library/Caches/com.ferdium.ferdium-app",
+    "~/Library/Caches/com.ferdium.ferdium-app.ShipIt",
+    "~/Library/Logs/Ferdium",
+    "~/Library/Preferences/ByHost/com.ferdium.ferdium-app.ShipIt.*.plist",
+    "~/Library/Preferences/com.electron.ferdium.helper.plist",
+    "~/Library/Preferences/com.electron.ferdium.plist",
+    "~/Library/Preferences/com.ferdium.ferdium-app.plist",
+    "~/Library/Saved Application State/com.ferdium.ferdium-app.savedState",
+  ]
+end
