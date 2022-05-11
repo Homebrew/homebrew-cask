50b3b283b4e8b7de0b16ec0e66014a4db944747d
miccal <miccal.matthews@gmail.com>
2022-05-11 17:11:59 +0800

    inkdrop 5.5.1
    Update inkdrop from 5.4.3 to 5.5.1
    
    Closes #123229.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/inkdrop.rb
+++ Casks/inkdrop.rb
@@ -1,12 +1,12 @@
 cask "inkdrop" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "5.4.3"
+  version "5.5.1"
 
   if Hardware::CPU.intel?
-    sha256 "40e15dbd8df80764f8776caf593b6f3214d479ea752016ea313b1abaed5c2325"
+    sha256 "c81e7f26d2b3984058309d65aaad2451d710838fd3f9c9ebb50eb465e70b8133"
   else
-    sha256 "ed016a680c71e4526ae9c54a045bba0b0226f27d676a9402b844e23205b1e938"
+    sha256 "83f386ccc721378ba100435938fc53e8105ba572e1b39e5b9615b532f9ab3bb4"
   end
 
   url "https://d3ip0rje8grhnl.cloudfront.net/v#{version}/Inkdrop-#{version}-#{arch}-Mac.zip",
