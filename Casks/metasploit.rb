da0cadb67fdf0974a73760d6838007680c9e3c1e
neutric <5984479+neutric@users.noreply.github.com>
2022-05-13 00:03:34 +0200

    Update metasploit from 6.1.42,20220506133353 to 6.1.42,20220512102909

--- Casks/metasploit.rb
+++ Casks/metasploit.rb
@@ -1,6 +1,6 @@
 cask "metasploit" do
-  version "6.1.42,20220506133353"
-  sha256 "9dcc8acdc6c92929661ecde9b36cd4d927bd9a3a2a4e7d859c9a9311b67d0af4"
+  version "6.1.42,20220512102909"
+  sha256 "b0ff36621d2576cf8353c9047fe850bf5beff35cfb8b56e7c49d8adad128b8cd"
 
   url "https://osx.metasploit.com/metasploit-framework-#{version.csv.first}%2B#{version.csv.second}-1rapid7-1.x86_64.pkg"
   name "Metasploit Framework"
