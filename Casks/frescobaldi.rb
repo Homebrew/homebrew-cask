2713d5445ea73c14606ff9f492c27f0428e53e2d
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-12 13:24:21 +0800

    frescobaldi: rebuild
    * Update frescobaldi.rb
    * Update frescobaldi.rb
    
    Closes #123361.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/frescobaldi.rb
+++ Casks/frescobaldi.rb
@@ -5,12 +5,13 @@ cask "frescobaldi" do
   url "https://github.com/frescobaldi/frescobaldi/releases/download/v#{version}/Frescobaldi-#{version}-x86_64.dmg",
       verified: "github.com/frescobaldi/frescobaldi/"
   name "Frescobaldi"
-  desc "LilyPond Editor"
+  desc "LilyPond editor"
   homepage "https://frescobaldi.org/"
 
   livecheck do
-    url :url
-    strategy :github_latest
+    url "https://github.com/frescobaldi/frescobaldi/releases/"
+    strategy :page_match
+    regex(/Frescobaldi[._-]v?(\d+(?:\.\d+)+)[._-]x86[._-]64\.dmg/i)
   end
 
   app "Frescobaldi.app"
