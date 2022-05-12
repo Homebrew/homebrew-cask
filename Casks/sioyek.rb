5ed8b779228db089254ff0f3bfd49d5ed2e1b662
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-12 15:14:21 +0800

    sioyek 1.2.0
    * Update sioyek.rb
    * Update sioyek.rb
    
    Closes #123367.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/sioyek.rb
+++ Casks/sioyek.rb
@@ -1,12 +1,17 @@
 cask "sioyek" do
-  version "1.1.0"
-  sha256 "9b4e0d30bcc49a9006fc96d45b540fef8a8c17061b8875f01d41c31219ca0c2b"
+  version "1.2.0"
+  sha256 "79974b0f419047046ad15f42410a8197a905ec22c0b293454f8f98323828a9d2"
 
   url "https://github.com/ahrm/sioyek/releases/download/v#{version}/sioyek-release-mac.zip",
       verified: "github.com/ahrm/sioyek/"
   name "Sioyek"
   desc "PDF viewer designed for reading research papers and technical books"
   homepage "https://sioyek.info/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   container nested: "build/sioyek.dmg"
 
