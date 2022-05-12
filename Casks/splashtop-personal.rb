73ec054b8e1136829a641ad8c50f7c355c80f3d6
Bevan Kay <email@bevankay.me>
2022-05-12 13:29:44 +1000

    Update splashtop-personal from 3.4.6.1 to 3.4.8.5

--- Casks/splashtop-personal.rb
+++ Casks/splashtop-personal.rb
@@ -1,6 +1,6 @@
 cask "splashtop-personal" do
-  version "3.4.6.1"
-  sha256 "39e202c3a5376256d55ca26ef6bd6d5aca046039251fec077e5d094e087f8b0e"
+  version "3.4.8.5"
+  sha256 "80b8c3171d796daf8ccb46e9f9b1ff5f519ba297202ace383d99080ce8625668"
 
   url "https://d17kmd0va0f0mp.cloudfront.net/macclient/STP/Splashtop_Personal_Mac_v#{version}.dmg",
       verified: "d17kmd0va0f0mp.cloudfront.net/"
