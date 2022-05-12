64a87091dc4eff48c1b7e723af9f170963ff1d1c
miccal <miccal.matthews@gmail.com>
2022-05-12 09:40:35 +0800

    Update blockbench from 4.2.2 to 4.2.4

--- Casks/blockbench.rb
+++ Casks/blockbench.rb
@@ -1,12 +1,12 @@
 cask "blockbench" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "4.2.2"
+  version "4.2.4"
 
   if Hardware::CPU.intel?
-    sha256 "560e82e5661f3b6fe6379227670a61dbe44bfcf98b9c653133f47ce504fea867"
+    sha256 "7717e118f14ca70d2d3abad644f20d603c9e3ced12fb776ae09c76cdc04a7b03"
   else
-    sha256 "d7fb768b2f1a7bee89f3d6cf768b2280c00caccc4187d94852cb87ff786f5f26"
+    sha256 "e4c8c36211aeccccc6f6ff4d756b9c56dcf1b9facb18bd7f5aafc80302e92295"
   end
 
   url "https://github.com/JannisX11/blockbench/releases/download/v#{version}/Blockbench_#{arch}_#{version}.dmg",
