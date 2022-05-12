c7eba3d42676cf40da0583b7dc11b8ef4491bc82
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-12 10:20:28 +0800

    Update guitar-pro.rb

--- Casks/guitar-pro.rb
+++ Casks/guitar-pro.rb
@@ -2,7 +2,7 @@ cask "guitar-pro" do
   version "8"
   sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://alt-downloads.guitar-pro.com/gp7/stable/guitar-pro-#{version.major}-setup.pkg"
+  url "https://alt-downloads.guitar-pro.com/gp#{version.major}/stable/guitar-pro-#{version.major}-setup.pkg"
   name "Guitar Pro"
   desc "Sheet music editor software for guitar, bass, keyboards, drums and more"
   homepage "https://www.guitar-pro.com/"
