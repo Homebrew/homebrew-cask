edd3799055e9e47e0ed024c627612a79e930c6e0
Baocang Nie <baocang@develop.local>
2022-05-12 17:19:01 +0800

    Update wechatwebdevtools from 1.05.2203070 to 1.05.2204250

--- Casks/wechatwebdevtools.rb
+++ Casks/wechatwebdevtools.rb
@@ -1,12 +1,12 @@
 cask "wechatwebdevtools" do
-  version "1.05.2203070"
-  sha256 "8704f9b8f07bf823268906ee004c5c77e34773aa86b17b3260cec9f3dc663a30"
+  version "1.05.2204250"
+  sha256 "d46828cfa36bc0a084503f671e6f3bdfde6031f4b73e90c5dc9e8dae9d62c559"
 
   url "https://dldir1.qq.com/WechatWebDev/release/p-ae42ee2cde4d42ee80ac60b35f183a99/wechat_devtools_#{version}.dmg"
-  name "wechat web devtools"
-  name "微信web开发者工具"
-  desc "Weixin DevTools for Official Account and Mini Program development"
-  homepage "https://mp.weixin.qq.com/debug/wxadoc/dev/devtools/download.html"
+  name "Wechat DevTools"
+  name "微信开发者工具"
+  desc "Wechat DevTools for Official Account and Mini Program development"
+  homepage "https://developers.weixin.qq.com/miniprogram/dev/devtools/download.html"
 
   livecheck do
     url "https://developers.weixin.qq.com/miniprogram/dev/devtools/download.html"
@@ -18,8 +18,8 @@ cask "wechatwebdevtools" do
   app "wechatwebdevtools.app"
 
   zap trash: [
-    "~/Library/Application Support/微信web开发者工具",
-    "~/Library/Caches/微信web开发者工具",
+    "~/Library/Application Support/微信开发者工具",
+    "~/Library/Caches/微信开发者工具",
     "~/Library/Preferences/com.tencent.wechat.devtools.plist",
     "~/Library/Preferences/com.tencent.webplusdevtools.plist",
     "~/Library/Saved Application State/com.tencent.wechat.devtools.savedState",
