6adb2dadf2e5f3447dce8113da4c26f638e0e966
Marc Doeberin <github@doeberin.com>
2022-05-13 07:37:22 +0200

    signal 5.43.0
    Update signal from 5.42.0 to 5.43.0
    
    Closes #123472.
    
    Signed-off-by: Miccal Matthews <6127163+miccal@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/signal.rb
+++ Casks/signal.rb
@@ -1,12 +1,12 @@
 cask "signal" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "5.42.0"
+  version "5.43.0"
 
   if Hardware::CPU.intel?
-    sha256 "db9c7ccd06d93e6a8d6048964a88a46478bba4e8d427e5661c105b47b377b677"
+    sha256 "bc7613403848750df876b50d1d4e83ef87283662b48cc1758e0170f951e9b485"
   else
-    sha256 "a35da7b4d85e17fce670a3867ee3a204f308ebb2ee8123b0887a6adcdfe93d58"
+    sha256 "4d984f9d3b1757e134c98a561a98bdec5ca8fe1edb5493387d2c3f23c0ae6e4d"
   end
 
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{arch}-#{version}.dmg"
