3ccdedb45357d701dc8aa365228b60c74c8ad5d8
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-05-12 22:41:30 -0400

    Update dcp-o-matic-batch-converter from 2.16.8 to 2.16.10

--- Casks/dcp-o-matic-batch-converter.rb
+++ Casks/dcp-o-matic-batch-converter.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-batch-converter" do
-  version "2.16.8"
-  sha256 "520500814c1caffa1a734ba74f6868a9cc8ca62796354d5d4068df980928f085"
+  version "2.16.10"
+  sha256 "d62b8c59305628565c3603988338f3edc9312adddefc0c2335708b3bd68f2333"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-batch&version=#{version}"
   name "DCP-o-matic Batch converter"
