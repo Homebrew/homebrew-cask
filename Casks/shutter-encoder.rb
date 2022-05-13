64744e587718fe3a1b8c2c28a08833dc1cff4b52
Porkepix <porkepix@gmail.com>
2022-05-13 11:31:05 +0200

    Update shutter-encoder from 15.9 to 16.0

--- Casks/shutter-encoder.rb
+++ Casks/shutter-encoder.rb
@@ -1,12 +1,12 @@
 cask "shutter-encoder" do
   arch = Hardware::CPU.intel? ? "64bits" : "Silicon"
 
-  version "15.9"
+  version "16.0"
 
   if Hardware::CPU.intel?
-    sha256 "8a2cad539a8d8628ad2c86321372ebdfd5291b6a32ff4a9124854e04db9f0ff7"
+    sha256 "651986b16223cb9a08b65fb5c57f4547d92623bb40485c24e7257c5a115da5eb"
   else
-    sha256 "519c9912396a1e9158b15947718ab31031eb7b5314fa0ee0f74a088a0d4fc248"
+    sha256 "58e85131558d462e173009960c91646a254cee0dcb9ee37ed040cc7a0abfc28c"
   end
 
   url "https://www.shutterencoder.com/Shutter%20Encoder%20#{version}%20Mac%20#{arch}.pkg"
