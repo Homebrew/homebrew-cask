21a68dfd67b59f4ac87b2874bafb9c4fea196b3f
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-05-13 17:54:04 -0400

    Update beeper from 3.9.14 to 3.10.13

--- Casks/beeper.rb
+++ Casks/beeper.rb
@@ -1,7 +1,7 @@
 cask "beeper" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "3.9.14"
+  version "3.10.13"
   sha256 :no_check
 
   url "https://download.beeper.com/mac/dmg/#{arch}"
