d5d853f3c9ae68c220815e562276c1adb898f810
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-13 08:59:31 +0800

    Update and rename ti-smartview-ce-84-plus.rb to ti-smartview-ce-for-the-ti-84-plus-family.rb

--- Casks/ti-smartview-ce-for-the-ti-84-plus-family.rb
+++ Casks/ti-smartview-ce-for-the-ti-84-plus-family.rb
@@ -1,9 +1,9 @@
-cask "ti-smartview-ce-84-plus" do
+cask "ti-smartview-ce-for-the-ti-84-plus-family" do
   version "5.6.0.2092,0D7B013A0B0846408659344F4C28BBA8"
   sha256 "15ba387d524161a21b8e5ef1060b495a563e2a5594e64737c0f102851f786088"
 
   url "https://education.ti.com/download/en/ed-tech/BE8220257AA241148986628D6EE332E5/#{version.csv.second}/TI-SmartView-CE-84-#{version.csv.first}.dmg"
-  name "TI SmartView™ CE Emulator Software for the TI-84 Plus Family"
+  name "TI SmartView CE Emulator Software for the TI-84 Plus Family"
   desc "Software to emulate the TI 84 Plus family of calculators"
   homepage "https://education.ti.com/en/software/details/en/BE8220257AA241148986628D6EE332E5/ti-smartview-ce-for-ti-84-plus-family"
 
