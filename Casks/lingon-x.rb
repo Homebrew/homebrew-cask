a2fea4b793532146ce73471b71788a1ce9887029
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-05-13 19:38:21 -0400

    Update lingon-x from 8.4.4 to 8.4.5

--- Casks/lingon-x.rb
+++ Casks/lingon-x.rb
@@ -3,8 +3,8 @@ cask "lingon-x" do
     version "6.6.5"
     sha256 "b0231b1a98dcc8f5c4234b419c9f5331407b8cce29b33f0ea2e32b12595adfa8"
   else
-    version "8.4.4"
-    sha256 "f95190a809a128b0396c73eb9e6ee47db6cef409b05e325705e85a8e4f94f8b2"
+    version "8.4.5"
+    sha256 "a7202068b8146a86511f17e9c5384b30523eaa1c0886d0794fe574d8e7ae7f32"
   end
 
   url "https://www.peterborgapps.com/downloads/LingonX#{version.major}.zip"
