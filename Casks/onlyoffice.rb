edb9bd83b8e08c4d60cc0a640a56626bd06bf9c2
miccal <miccal.matthews@gmail.com>
2022-05-13 18:46:41 +0800

    Update onlyoffice from 7.0.1 to 7.1.0

--- Casks/onlyoffice.rb
+++ Casks/onlyoffice.rb
@@ -1,12 +1,12 @@
 cask "onlyoffice" do
   arch = Hardware::CPU.intel? ? "x86_64" : "arm"
 
-  version "7.0.1"
+  version "7.1.0"
 
   if Hardware::CPU.intel?
-    sha256 "84dd81ba9ee6ee54409133f7014240c0c23f7c2dfe5f1f8b334e5e52cf93e161"
+    sha256 "97ec1035147d9587f20f87fa1b92b14947cad2dc304f3a7f71c12cb3d8535237"
   else
-    sha256 "e33c84ec414ec918208d1168d7dc25190faf7ab2d7d114e535b7f9a111d00360"
+    sha256 "1aebbfe203b51ca3c400b1e9232df4290210f2f22d35a361b91792eb18c8a5a2"
   end
 
   url "https://github.com/ONLYOFFICE/DesktopEditors/releases/download/v#{version}/ONLYOFFICE-#{arch}.dmg",
