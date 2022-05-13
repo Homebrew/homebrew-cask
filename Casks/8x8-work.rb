e5ba33a9bbb6a2e20dd439c85558c7b2acbf7074
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-05-12 21:48:39 -0400

    Update 8x8-work from 7.17.3-3 to 7.18.2-2

--- Casks/8x8-work.rb
+++ Casks/8x8-work.rb
@@ -1,6 +1,6 @@
 cask "8x8-work" do
-  version "7.17.3-3"
-  sha256 "d1d074010a56b6ee06cfcfe050ce469f30ba9f5a17aeb07035d8222a45124510"
+  version "7.18.2-2"
+  sha256 "c65339d536df05abdc93c4d32c68a8c5d9b9a3c1afc1a4916817ddaa84d81334"
 
   url "https://vod-updates.8x8.com/ga/work-dmg-v#{version}.dmg"
   name "8x8_work"
