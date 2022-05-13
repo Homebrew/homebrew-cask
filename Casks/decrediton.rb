a6fabb5496b8c5057b7e34d4da0a1604e58ccf43
miccal <miccal.matthews@gmail.com>
2022-05-13 18:06:05 +0800

    Update decrediton from 1.7.1 to 1.7.2

--- Casks/decrediton.rb
+++ Casks/decrediton.rb
@@ -1,12 +1,12 @@
 cask "decrediton" do
   arch = Hardware::CPU.intel? ? "amd64" : "arm64"
 
-  version "1.7.1"
+  version "1.7.2"
 
   if Hardware::CPU.intel?
-    sha256 "09da70f24412d48339ad97fedf394c1758eb2af97f7e83c5d4f4a27fc198f5b9"
+    sha256 "2cff49751267a1a284df4fc73295e6a729f0f2a1858187772c0ae61981e6a5bd"
   else
-    sha256 "729a515318ad1866a0c08a0d885e66afa144c88f8a0f761ba22174234d3c6308"
+    sha256 "7a68aaa273564e8d7b96af4624c0c476415f6977ab446288127010cf340f2a38"
   end
 
   url "https://github.com/decred/decred-binaries/releases/download/v#{version}/decrediton-#{arch}-v#{version}.dmg"
