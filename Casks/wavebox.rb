f16975461fce3503b56071d39af31e11a0d6cc95
miccal <miccal.matthews@gmail.com>
2022-05-13 18:58:31 +0800

    wavebox 10.101.21.2
    Update wavebox from 10.101.16.2 to 10.101.21.2
    
    Closes #123497.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/wavebox.rb
+++ Casks/wavebox.rb
@@ -1,12 +1,12 @@
 cask "wavebox" do
   arch = Hardware::CPU.intel? ? "mac" : "macarm64"
 
-  version "10.101.16.2"
+  version "10.101.21.2"
 
   if Hardware::CPU.intel?
-    sha256 "7a0dc83cd95a6a15dd9f0e58c49b9eafe68a41fce144df80ff4260325a1304f3"
+    sha256 "670e14cb8929fdc3880c9d70784638e8813850c83d7406783f2c45a7e7f33c94"
   else
-    sha256 "017136fdf715afaa04983adad33ad42083a76beeb48765d057fdd7b5af2c60ee"
+    sha256 "0c2fee9c334bb059d7b069ea2c40864ae87b9909413ae0d78002241010ee8201"
   end
 
   url "https://download.wavebox.app/stable/#{arch}/Install%20Wavebox%20#{version}.dmg",
