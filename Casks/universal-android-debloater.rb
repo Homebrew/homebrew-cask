0e7bf9be7be7d429f6b751da01d2210036a5cf4a
mhmdanas <triallax@tutanota.com>
2022-05-14 17:10:11 +0300

    universal-android-debloater 0.5 (new cask)

---
+++ Casks/universal-android-debloater.rb
@@ -0,0 +1,16 @@
+cask "universal-android-debloater" do
+  version "0.5"
+  sha256 "2c029731a354e44f1afeec3284d0c2017a35e7ac1f2ddc09da0c4c2e7525aac8"
+
+  url "https://github.com/0x192/universal-android-debloater/releases/download/#{version}/uad_gui-macos-opengl.tar.gz"
+  name "Universal Android Debloater"
+  name "UAD"
+  desc "Cross-platform GUI which uses ADB to debloat non-rooted Android devices"
+  homepage "https://github.com/0x192/universal-android-debloater"
+
+  depends_on cask: "android-platform-tools"
+
+  binary "uad_gui-macos-opengl", target: "uad"
+
+  zap trash: "~/Library/Caches/uad"
+end
