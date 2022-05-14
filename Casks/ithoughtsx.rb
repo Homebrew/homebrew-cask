c6bf3ebced93250dd131c5032eaff871b5a2f3a1
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-14 11:58:38 +0800

    Update ithoughtsx.rb

--- Casks/ithoughtsx.rb
+++ Casks/ithoughtsx.rb
@@ -3,7 +3,7 @@ cask "ithoughtsx" do
   sha256 :no_check
 
   url "https://cdn.toketaware.com?download=iThoughtsX.zip",
-      referer:    "https://www.toketaware.com/"
+      referer: "https://www.toketaware.com/"
   name "iThoughtsX"
   desc "Mind mapping tool"
   homepage "https://www.toketaware.com/ithoughts-osx"
