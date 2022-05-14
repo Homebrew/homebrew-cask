3f6ffc73e5972edb80d57e8ca21e512524e1728b
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-05-13 20:10:00 -0400

    paddle-easydl 1.2.0,1200
    Update paddle-easydl from 1.1.4 to 1.2.0
    
    Closes #123668.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/paddle-easydl.rb
+++ Casks/paddle-easydl.rb
@@ -1,6 +1,6 @@
 cask "paddle-easydl" do
-  version "1.1.4,1168"
-  sha256 "a31a592ba0aec4d24abeb8443aee11fd886c79e32366d60ee0afffab2d8d2051"
+  version "1.2.0,1200"
+  sha256 "ca43f45dbb926731d374bcb276b5535b4fc85d3d97b8b152349d44b62ad6e596"
 
   url "https://aip-static.cdn.bcebos.com/paddle-desktop/releases/#{version.csv.first}/%E9%A3%9E%E6%A1%A8EasyDL-#{version.csv.first}.#{version.csv.second}.dmg",
       verified: "aip-static.cdn.bcebos.com/paddle-desktop"
