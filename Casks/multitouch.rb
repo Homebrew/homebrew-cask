2b749cecb385b9a1f376a5ef74c998af487efa61
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-05-13 20:01:15 -0400

    multitouch 1.27.4,159
    Update multitouch from 1.26.4 to 1.27.4
    
    Closes #123657.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/multitouch.rb
+++ Casks/multitouch.rb
@@ -1,6 +1,6 @@
 cask "multitouch" do
-  version "1.26.4,152"
-  sha256 "bc7feb8d6ad84917863477395e5a13f97638df6998e30a845905b2b7a71684a2"
+  version "1.27.4,159"
+  sha256 "34b1e5a6eb2e529260eef04714a278449756ee9101b215c9ac9dc044b47ca7b1"
 
   url "https://multitouch.app/downloads/multitouch#{version.csv.first}.dmg"
   name "Multitouch"
