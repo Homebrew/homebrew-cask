74f85618b3216449d9c1ecf4b438e0747f123eaa
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-05-14 08:55:23 -0400

    superhuman 12.1.3
    Update superhuman from 11.0.41 to 12.1.3
    
    Closes #123716.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/superhuman.rb
+++ Casks/superhuman.rb
@@ -1,12 +1,12 @@
 cask "superhuman" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  version "11.0.41"
+  version "12.1.3"
 
   if Hardware::CPU.intel?
-    sha256 "5b991b667e69f3652a292224fc8119f3a551a2094c60f03c6fcbb9026df5240c"
+    sha256 "69f0fa2311b8af1f5038739715738261656f2b7662f09114e64469f47a6c0a4f"
   else
-    sha256 "3c5da3c8fcfff44de38483c1da1e91cc6fbe92eec0e7321c2f8e99f5014d47dd"
+    sha256 "2e6d6e43b5656b962fb60124ec5ad55fb9d6fb85112fe313e602c499b5d05da0"
   end
 
   url "https://storage.googleapis.com/download.superhuman.com/supertron-update/Superhuman-#{version}#{arch}-latest-mac.zip",
