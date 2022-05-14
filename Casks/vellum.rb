d9d9b2f602a85102c85ad7d93e9e81845022c1c6
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-05-14 09:26:11 -0400

    Update vellum from 3.1 to 3.2.1

--- Casks/vellum.rb
+++ Casks/vellum.rb
@@ -1,6 +1,6 @@
 cask "vellum" do
-  version "3.1,31000"
-  sha256 "527ac943cceed5ba2f98d38a8a3c2b20c194f72239d5a27ddb5fca2520f7c05e"
+  version "3.2.1,32100"
+  sha256 "c4328a489e4499a4281f05abce77e776c1902ee4de25c9016c2c2b477cd032e6"
 
   url "https://180g.s3.amazonaws.com/downloads/Vellum-#{version.csv.second}.zip",
       verified: "180g.s3.amazonaws.com/downloads/"
