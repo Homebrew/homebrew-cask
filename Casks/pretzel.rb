fd89a09c2bd623348233afa22a648a30438c355d
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-05-13 20:16:02 -0400

    pretzel 2.11.1
    Update pretzel from 2.11.0 to 2.11.1
    
    Closes #123681.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/pretzel.rb
+++ Casks/pretzel.rb
@@ -1,6 +1,6 @@
 cask "pretzel" do
-  version "2.11.0"
-  sha256 "42ac5387eb82a5e318b3d73b6afc5279b4e77bde16159503ddad3dfd3fe6622e"
+  version "2.11.1"
+  sha256 "898a5f0129d40cdbf578fd29772766251bc4dcef63214b3038174ce0902f9239"
 
   url "https://download.pretzel.rocks/Pretzel-#{version}-mac.zip"
   name "Pretzel"
