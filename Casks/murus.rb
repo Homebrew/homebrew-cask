113e10ee4b354ccd42fd9d9c8b7d1f75f1fcb387
sandergol <sandergol@outlook.com>
2022-05-14 16:25:23 +0300

    Upgrade Murus.app to v2.3.2

--- Casks/murus.rb
+++ Casks/murus.rb
@@ -2,7 +2,7 @@ cask "murus" do
   version "2.3.2"
   sha256 "7a1d3ef8961ae04c1ec3d8de2245fa88a4f5fc4e35c746f6ca80cd16f6171e23"
 
-  url "https://github.com/TheMurusTeam/Murus/releases/download/v#{version}/murus-#{version}.zip",
+  url "https://github.com/TheMurusTeam/Murus/releases/download/v#{version}/Murus.#{version}.zip",
       verified: "github.com/TheMurusTeam/Murus/"
   name "Murus Firewall"
   desc "Firewall app"
