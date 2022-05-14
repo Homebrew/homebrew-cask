7c2ca73a22f2049aedd09c99ddebb2702331eafe
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-05-13 20:05:48 -0400

    openvanilla 1.6.4,3310
    Update openvanilla from 1.6.2 to 1.6.4
    
    Closes #123666.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/openvanilla.rb
+++ Casks/openvanilla.rb
@@ -1,6 +1,6 @@
 cask "openvanilla" do
-  version "1.6.2,3300"
-  sha256 "2aa30f038376edf3c725f9654e6c939ee733104ac50e33f5161853da3353331e"
+  version "1.6.4,3310"
+  sha256 "06e5cf778bd19ea05d85d037d472dffd8ffad6cc1dfb51814435db14f1eb139e"
 
   url "https://github.com/openvanilla/openvanilla/releases/download/#{version.csv.first}/OpenVanilla-Installer-Mac-#{version.csv.first}.zip",
       verified: "github.com/openvanilla/openvanilla/"
