28e8c57140613a0c39ebc5c79b1b4a481aacb7af
sandergol <sandergol@outlook.com>
2022-05-14 16:08:52 +0300

    murus 2.3.2
    * Upgrade Murus.app to v2.3.2
    * Upgrade Murus.app to v2.3.2
    
    Closes #123741.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/murus.rb
+++ Casks/murus.rb
@@ -1,8 +1,8 @@
 cask "murus" do
-  version "2.3.1"
-  sha256 "9a05f4dc974c2a0b5043206eb677ceabdb26d2fa1aa6d30f7db84166ebcb3ecc"
+  version "2.3.2"
+  sha256 "7a1d3ef8961ae04c1ec3d8de2245fa88a4f5fc4e35c746f6ca80cd16f6171e23"
 
-  url "https://github.com/TheMurusTeam/Murus/releases/download/v#{version}/murus-#{version}.zip",
+  url "https://github.com/TheMurusTeam/Murus/releases/download/v#{version}/Murus.#{version}.zip",
       verified: "github.com/TheMurusTeam/Murus/"
   name "Murus Firewall"
   desc "Firewall app"
