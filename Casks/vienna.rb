13a25efbd0ba6f37fb5d56b287d3df3d335e54e3
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-16 07:19:37 +0800

    Update vienna.rb

--- Casks/vienna.rb
+++ Casks/vienna.rb
@@ -1,5 +1,5 @@
 cask "vienna" do
-  version "3.7.3"
+  version "3.7.3,7560"
   sha256 "73ccf2bcea6884099708b323966a2307df8f0208264fda58ba4426bd87530d9d"
 
   url "https://github.com/ViennaRSS/vienna-rss/releases/download/v%2F#{version.csv.first}/Vienna#{version.csv.first}.tar.gz",
@@ -18,8 +18,8 @@ cask "vienna" do
   app "Vienna.app"
 
   zap trash: [
-    "~/Library/Application Support/Vienna",
     "~/Library/Application Scripts/uk.co.opencommunity.vienna2",
+    "~/Library/Application Support/Vienna",
     "~/Library/Caches/uk.co.opencommunity.vienna2",
     "~/Library/Cookies/uk.co.opencommunity.vienna2.binarycookies",
     "~/Library/HTTPStorages/uk.co.opencommunity.vienna2.binarycookies",
