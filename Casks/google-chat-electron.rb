9b7b37bc6e3883adefc8bae5a175091c1d1faf73
TAK <39761181+toco8@users.noreply.github.com>
2022-05-15 11:54:26 +0900

    Add depends_on stanza to google-chat-electron (#123776)
    * Update google-chat-electron.rb
    
    * Update google-chat-electron.rb
    
    * Update google-chat-electron.rb

--- Casks/google-chat-electron.rb
+++ Casks/google-chat-electron.rb
@@ -1,18 +1,20 @@
 cask "google-chat-electron" do
+  arch = Hardware::CPU.intel? ? "darwin-x64" : "darwin-arm64"
+
   version "2.15.1"
 
   if Hardware::CPU.intel?
     sha256 "671b42125a6100fdf64b07ecde1cd91d8ba1b3caf5530746f96cf4621516eee3"
-    arch = "darwin-x64"
   else
     sha256 "cf490d915018e12cfaca061cb0aef0ac013600b8c112b06a6219cf377268e1fb"
-    arch = "darwin-arm64"
   end
 
   url "https://github.com/ankurk91/google-chat-electron/releases/download/#{version}/google-chat-electron-#{version}-#{arch}.zip"
   name "google-chat-electron"
   desc "Standalone app for Google Chat"
   homepage "https://github.com/ankurk91/google-chat-electron"
+
+  depends_on macos: ">= :catalina"
 
   app "google-chat-electron.app"
 
