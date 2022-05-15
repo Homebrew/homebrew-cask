981d1d98e37a21a68fe0ae0eb133e6296646bd4e
neutric <5984479+neutric@users.noreply.github.com>
2022-05-15 04:52:46 +0200

    Update sqlpro-for-mysql from 2021.53 to 2022.15 (#123781)
    * Update sqlpro-for-mysql from 2021.53 to 2022.15
    
    * sqlpro-for-mysql: sort zap

--- Casks/sqlpro-for-mysql.rb
+++ Casks/sqlpro-for-mysql.rb
@@ -1,8 +1,8 @@
 cask "sqlpro-for-mysql" do
-  version "2021.53"
-  sha256 "53b718c7a4b6ee6271030cdb1959bdd9e2917371e5348bcf0d567d26614b6a72"
+  version "2022.15"
+  sha256 "05a738a786e8a6119331de4dec8fa1fda98ea47bb4a9702950a15453a3ca26dd"
 
-  url "https://d3fwkemdw8spx3.cloudfront.net/mysql/SQLProMySQL.#{version}.zip",
+  url "https://d3fwkemdw8spx3.cloudfront.net/mysql/SQLProMySQL.#{version}.app.zip",
       verified: "d3fwkemdw8spx3.cloudfront.net/mysql/"
   name "SQLPro for MySQL"
   desc "MySQL & MariaDB database client"
@@ -16,7 +16,7 @@ cask "sqlpro-for-mysql" do
   app "SQLPro for MySQL.app"
 
   zap trash: [
-    "~/Library/Containers/com.hankinsoft.osx.mysql",
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.hankinsoft.osx.mysql.sfl*",
+    "~/Library/Containers/com.hankinsoft.osx.mysql",
   ]
 end
