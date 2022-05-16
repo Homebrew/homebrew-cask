9c5d8178bcfc1b0b36e65fc6097a9691fe4b4385
Ted Kremenek <kremenek@apple.com>
2022-05-16 15:27:50 -0700

    Add Apple Silicon support for uvtools cask

--- Casks/uvtools.rb
+++ Casks/uvtools.rb
@@ -2,7 +2,8 @@ cask "uvtools" do
   version "3.4.2"
   sha256 "6ef3effafe23d46f848dd7f31b21a86bc757cd527b22268c99811d8b8a3aea95"
 
-  url "https://github.com/sn4k3/UVtools/releases/download/v#{version}/UVtools_osx-x64_v#{version}.zip"
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
+  url "https://github.com/sn4k3/UVtools/releases/download/v#{version}/UVtools_osx-#{arch}_v#{version}.zip"
   name "UVtools"
   desc "MSLA/DLP, file analysis, calibration, repair, conversion and manipulation"
   homepage "https://github.com/sn4k3/UVtools"
