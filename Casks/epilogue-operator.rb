b8ab1cdf7dc9a1041e88d57defce320b866575f8
Bevan Kay <email@bevankay.me>
2022-05-16 23:45:37 +1000

    Update epilogue-operator from 0.7.1 to 0.8.0

--- Casks/epilogue-operator.rb
+++ Casks/epilogue-operator.rb
@@ -1,15 +1,16 @@
 cask "epilogue-operator" do
-  version "0.7.1"
+  version "0.8.0"
   sha256 :no_check
 
-  url "https://www.epilogue.co/downloads/operator/osx"
+  url "https://epilogue.nyc3.cdn.digitaloceanspaces.com/releases/software/Operator/release/mac/Operator.dmg",
+      verified: "epilogue.nyc3.cdn.digitaloceanspaces.com/releases/software/Operator/"
   name "Epilogue Operator"
   desc "Play and manage Game Boy cartridges on your computer"
   homepage "https://www.epilogue.co/"
 
   livecheck do
     url :url
-    strategy :header_match
+    strategy :extract_plist
   end
 
   app "Operator.app"
