31e9adf8a48e8ffcac2640e5f54f3102c7cb4371
neutric <5984479+neutric@users.noreply.github.com>
2022-05-17 00:05:57 +0200

    visual-paradigm 16.3,20220513
    Update visual-paradigm from 16.3,20220503 to 16.3,20220513
    
    Closes #123854.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/visual-paradigm.rb
+++ Casks/visual-paradigm.rb
@@ -1,6 +1,6 @@
 cask "visual-paradigm" do
-  version "16.3,20220503"
-  sha256 "27b7afbf5697502f53845da7a760b705b3f766003e9fc5800549ff6e8d82422c"
+  version "16.3,20220513"
+  sha256 "9c01ce149ddb558e78cc8d2af3d8894d1d9a4cfe70cba57ee243096f2ccdaf91"
 
   url "https://www.visual-paradigm.com/downloads/vp#{version.csv.first}/#{version.csv.second}/Visual_Paradigm_#{version.csv.first.dots_to_underscores}_#{version.csv.second}_OSX_WithJRE.dmg"
   name "Visual Paradigm"
