0190df27c9e68a78a0e50989a3c02789d222c7cd
Bevan Kay <email@bevankay.me>
2022-05-16 19:19:04 +1000

    Update bria from 6.5.2 to 6.5.3

--- Casks/bria.rb
+++ Casks/bria.rb
@@ -1,6 +1,6 @@
 cask "bria" do
-  version "6.5.2,109387"
-  sha256 "0cf8ae8f637c59b0820f6ccc192d48fcf833af1ef90379850af84f834061dc8b"
+  version "6.5.3,110165"
+  sha256 "199a4e4ffa06119f9142d8b03b579db52ffadb2f10fe8b3ff99952751158b215"
 
   url "https://counterpath.s3.amazonaws.com/downloads/Bria_#{version.csv.first}_#{version.csv.second}.dmg",
       verified: "counterpath.s3.amazonaws.com/"
@@ -8,5 +8,26 @@ cask "bria" do
   desc "Softphone application"
   homepage "https://www.counterpath.com/bria-solo/"
 
+  livecheck do
+    url "https://www.counterpath.com/downloads/Bria6ForMac.php"
+    regex(/Bria_(\d+(?:\.\d+)+)_(\d+)\.dmg/i)
+    strategy :header_match do |headers, regex|
+      match = headers["location"].match(regex)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
+  end
+
+  auto_updates true
+
   app "Bria.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.CounterPath_Corporation.Bria",
+    "~/Library/Caches/com.CounterPath_Corporation.Bria",
+    "~/Library/HTTPStorages/com.CounterPath_Corporation.Bria",
+    "~/Library/Preferences/com.CounterPath_Corporation.Bria.plist",
+    "~/Library/WebKit/com.CounterPath_Corporation.Bria",
+  ]
 end
