2cf95df37ab645f838a0be93c70b25178bd7bb74
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-16 17:05:33 +0800

    smartsynchronize 4.3.0 (#123823)
    Update smartsynchronize.rb

--- Casks/smartsynchronize.rb
+++ Casks/smartsynchronize.rb
@@ -1,8 +1,8 @@
 cask "smartsynchronize" do
-  version "4.2.1"
-  sha256 "e4cf1c0b335f23b1dab9c7f8e1be802f6bac5a160ddbc7b57f4cf3605e44fabd"
+  version "4.3.0"
+  sha256 "22041d221c32b23e6be76a089e72ddc7fe1f52fac190fac949f29fff7b05d498"
 
-  url "https://www.syntevo.com/downloads/smartsynchronize/smartsynchronize-macos-#{version.dots_to_underscores}.dmg"
+  url "https://www.syntevo.com/downloads/smartsynchronize/smartsynchronize-x86_64-#{version.dots_to_underscores}.dmg"
   name "SmartSynchronize"
   desc "File and directory compare tool"
   homepage "https://www.syntevo.com/smartsynchronize/"
@@ -10,7 +10,9 @@ cask "smartsynchronize" do
   livecheck do
     url "https://www.syntevo.com/smartsynchronize/download/"
     strategy :page_match do |page|
-      v = page[%r{href=.*?/smartsynchronize-macos-(\d+(?:_\d+)*)\.dmg}i, 1]
+      v = page[/smartsynchronize[._-]x86[._-]64[._-]v?(\d+(?:_\d+)+)\.dmg/i, 1]
+      next if v.blank?
+
       v.tr("_", ".")
     end
   end
