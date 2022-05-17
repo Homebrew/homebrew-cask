f959d950b438685d0d08677838aa8278291fe2e8
Jameel Al-Aziz <247849+jalaziz@users.noreply.github.com>
2022-05-17 03:24:24 -0700

    Add arm64 support for smartsynchronize (#123889)
    * Add arm64 support for smartsynchronize
    
    * Update smartsynchronize.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/smartsynchronize.rb
+++ Casks/smartsynchronize.rb
@@ -1,8 +1,15 @@
 cask "smartsynchronize" do
-  version "4.3.0"
-  sha256 "22041d221c32b23e6be76a089e72ddc7fe1f52fac190fac949f29fff7b05d498"
+  arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
-  url "https://www.syntevo.com/downloads/smartsynchronize/smartsynchronize-x86_64-#{version.dots_to_underscores}.dmg"
+  version "4.3.0"
+
+  if Hardware::CPU.intel?
+    sha256 "22041d221c32b23e6be76a089e72ddc7fe1f52fac190fac949f29fff7b05d498"
+  else
+    sha256 "e52c0b57bd4c31023cb0dc84f0f45369e27ee8b96fc6b20768b2f8831469db9d"
+  end
+
+  url "https://www.syntevo.com/downloads/smartsynchronize/smartsynchronize-#{arch}-#{version.dots_to_underscores}.dmg"
   name "SmartSynchronize"
   desc "File and directory compare tool"
   homepage "https://www.syntevo.com/smartsynchronize/"
@@ -10,7 +17,7 @@ cask "smartsynchronize" do
   livecheck do
     url "https://www.syntevo.com/smartsynchronize/download/"
     strategy :page_match do |page|
-      v = page[/smartsynchronize[._-]x86[._-]64[._-]v?(\d+(?:_\d+)+)\.dmg/i, 1]
+      v = page[/smartsynchronize[._-]#{arch}[._-]v?(\d+(?:_\d+)+)\.dmg/i, 1]
       next if v.blank?
 
       v.tr("_", ".")
