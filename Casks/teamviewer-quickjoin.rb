e2ef24fb6fece7d1644b232c413e318db842d054
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-17 18:40:21 +0800

    Update teamviewer-quickjoin.rb

--- Casks/teamviewer-quickjoin.rb
+++ Casks/teamviewer-quickjoin.rb
@@ -1,12 +1,18 @@
 cask "teamviewer-quickjoin" do
-  version "15.16.8,1"
-  sha256 :no_check
+  version "15"
+  sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://download.teamviewer.com/download/TeamViewerQJ.dmg"
+  url "https://download.teamviewer.com//download/version_#{version}x/TeamViewerQJ.dmg"
   name "TeamViewer QuickJoin"
   name "TeamViewer QJ"
   desc "Standalone TeamViewer app for joining presentations and meetings"
   homepage "https://www.teamviewer.com/"
+
+  livecheck do
+    url "https://dl.teamviewer.com/download/TeamViewerQJ.dmg"
+    strategy :header_match
+    regex(%r{/version[._-]v?(\d+)x/TeamViewerQJ\.dmg}/i)
+  end
 
   # Renamed for consistency: app name is different in the Finder and in a shell.
   app "TeamViewerQJ.app", target: "TeamViewer QuickJoin.app"
