9fb4bce7021376965a663184c8385a63b52e3067
tiiiecherle <tiiiecherle@gmail.com>
2022-05-17 19:24:03 +0200

    Update openwebstart and add new arm version

--- Casks/openwebstart.rb
+++ Casks/openwebstart.rb
@@ -1,8 +1,13 @@
 cask "openwebstart" do
-  version "1.5.2"
-  sha256 "7e53421fd008b8ed18e1f805921e37e7622d72aa3a65f137a8ca808d36e0b696"
+  arch = Hardware::CPU.intel? ? "x64" : "aarch64"
+  version "1.6.0"
+  if Hardware::CPU.intel?
+    sha256 "c2a71ce2e22adbf96aab869f0c96b2d26f839eace0d6692bf861070334fb2898"
+  else
+    sha256 "c4b738e7a0ddc76d6a40a913bede9f205d421b7b596b329ecb96438a7d048b72"
+  end
 
-  url "https://github.com/karakun/OpenWebStart/releases/download/v#{version}/OpenWebStart_macos_#{version.dots_to_underscores}.dmg",
+  url "https://github.com/karakun/OpenWebStart/releases/download/v#{version}/OpenWebStart_macos-#{arch}_#{version.dots_to_underscores}.dmg",
       verified: "github.com/karakun/OpenWebStart/"
   name "OpenWebStart"
   desc "Tool to run Java Web Start-based applications after the release of Java 11"
