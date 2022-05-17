9c9e7b23da3157480286404602d5fcc8c9935d04
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-17 11:56:40 +0800

    Update and rename megax.rb to mega.rb

---
+++ Casks/mega.rb
@@ -0,0 +1,35 @@
+cask "mega" do
+  version "11.0.11"
+  sha256 "0bdc3ed14bd73d3bbaea28044ab593a33c7f0ce1e477cf754a774095f3ba9c1c"
+
+  url "ttps://megasoftware.net/do_force_download/MEGA_#{version}_installer.pkg"
+  name "MEGA"
+  name "Molecular Evolutionary Genetics Analysis"
+  desc "Molecular evolution statistical analysis and construction of phylogenetic trees"
+  homepage "https://megasoftware.net/"
+
+  livecheck do
+    url "https://megasoftware.net/history"
+    regex(/MEGA\s\d+\sversion\s(\d+(?:\.\d+)+)/i)
+  end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  pkg "MEGA_#{version}_installer.pkg"
+
+  uninstall quit:    "com.company.MEGA#{version.major}",
+            pkgutil: "com.igem.pkg.MEGA#{version.major}",
+            delete:  [
+              "/Applications/MEGA#{version.major}.app",
+              "/usr/local/bin/megacc",
+              "/usr/local/share/man/man1/mega#{version.major}.1",
+              "/usr/local/share/man/man1/megacc.1",
+            ]
+
+  zap trash: [
+    "~/Library/Preferences/com.company.MEGA#{version.major} Helper.plist",
+    "~/Library/Preferences/com.company.MEGA#{version.major}.plist",
+    "~/Library/Saved Application State/com.company.MEGA#{version.major}.savedState",
+  ]
+end
