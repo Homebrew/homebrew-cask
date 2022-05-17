ef632e2777d13a172f85d07e20d838fb596f3fbc
Bevan Kay <email@bevankay.me>
2022-05-17 23:36:08 +1000

    Update localdev from 0.12.0 to 1.3.1

--- Casks/localdev.rb
+++ Casks/localdev.rb
@@ -1,11 +1,16 @@
 cask "localdev" do
-  version "0.12.0"
-  sha256 "e5e8d9620cd5c67e5a497855b20dde44ff8d482edeae984363d7ba1eed7d296b"
+  version "1.3.1"
+  sha256 "946f49215c4559f28ebf8b183698f986c3de0baa8d1e10d2cdd1fbe44fb9d9f2"
 
   url "https://pantheon-localdev.s3.amazonaws.com/Localdev-v#{version}.dmg",
       verified: "pantheon-localdev.s3.amazonaws.com/"
   name "Localdev"
   homepage "https://pantheon.io/localdev"
+
+  livecheck do
+    url "https://pantheon-localdev.s3.amazonaws.com/latest-mac.yml"
+    strategy :electron_builder
+  end
 
   app "Localdev.app"
 
