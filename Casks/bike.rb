3b8db5d1c6d4e2e0f124c519d3ab9f56675dd5f7
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-18 05:35:35 +0800

    Update bike.rb

--- Casks/bike.rb
+++ Casks/bike.rb
@@ -1,19 +1,19 @@
 cask "bike" do
-  version "51"
+  version "1.0,51"
   sha256 "1e2d017a53660cb970d8dfeb10aec765d0f44046ed87d375b35bf8246a2caa18"
 
-  url "https://www.hogbaysoftware.com/bike/releases/#{version}.dmg"
+  url "https://www.hogbaysoftware.com/bike/releases/#{version.csv.second}.dmg"
   name "Bike"
   desc "Record and process your ideas"
   homepage "https://www.hogbaysoftware.com/bike/"
 
   livecheck do
-    url "https://www.hogbaysoftware.com/bike/releases/"
-    regex(/href=.*?(\d+).dmg"/i)
+    url "https://www.hogbaysoftware.com/bike/releases/index.xml"
+    strategy :sparkle
   end
 
   auto_updates true
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= :big_sur"
 
   app "Bike.app"
 
