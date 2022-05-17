cd2f662017a5c0d4805675d30ae714f7f9980328
Tomas Klohna <Klohto@users.noreply.github.com>
2022-05-17 23:43:43 +0200

    Add Bike app from Hog Bay Software (#123908)
    * Add Bike app from Hog Bay Software
    
    * Add macOS Mojave dependency check
    
    * Add zap stanza
    
    * Update bike.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/bike.rb
@@ -0,0 +1,24 @@
+cask "bike" do
+  version "1.0,51"
+  sha256 "1e2d017a53660cb970d8dfeb10aec765d0f44046ed87d375b35bf8246a2caa18"
+
+  url "https://www.hogbaysoftware.com/bike/releases/#{version.csv.second}.dmg"
+  name "Bike"
+  desc "Record and process your ideas"
+  homepage "https://www.hogbaysoftware.com/bike/"
+
+  livecheck do
+    url "https://www.hogbaysoftware.com/bike/releases/index.xml"
+    strategy :sparkle
+  end
+
+  auto_updates true
+  depends_on macos: ">= :big_sur"
+
+  app "Bike.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.hogbaysoftware.Bike",
+    "~/Library/Containers/com.hogbaysoftware.Bike",
+  ]
+end
