806403a12985ba681e8b5d7f7d740023a5c405e1
Tomas Klohna <Klohto@users.noreply.github.com>
2022-05-17 19:03:21 +0200

    Add Bike app from Hog Bay Software

---
+++ Casks/bike.rb
@@ -0,0 +1,18 @@
+cask "bike" do
+  version "51"
+  sha256 "1e2d017a53660cb970d8dfeb10aec765d0f44046ed87d375b35bf8246a2caa18"
+
+  url "https://www.hogbaysoftware.com/bike/releases/#{version}.dmg"
+  name "Bike"
+  desc "Record and process your ideas"
+  homepage "https://www.hogbaysoftware.com/bike/"
+
+  livecheck do
+    url "https://www.hogbaysoftware.com/bike/releases/"
+    regex(/href=.*?(\d+).dmg"/i)
+  end
+
+  auto_updates true
+
+  app "Bike.app"
+end
