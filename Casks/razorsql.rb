dbee22f26ac73a0a73c88ff6849a38e55af5f6db
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-17 19:07:29 +0800

    Update razorsql.rb

--- Casks/razorsql.rb
+++ Casks/razorsql.rb
@@ -3,7 +3,7 @@ cask "razorsql" do
   sha256 "6c2fdb01b8ed53de80fdbc912ec652d56d2add1780227cc0e679a10bc1b2c9e6"
 
   url "https://s3.dualstack.us-east-1.amazonaws.com/downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}.dmg",
-      verified: "s3.dualstack.us-east-1.amazonaws.com/downloads.razorsql.com/downloads/"
+      verified: "s3.dualstack.us-east-1.amazonaws.com/"
   appcast "https://razorsql.com/updates.html"
   name "RazorSQL"
   desc "SQL query tool and SQL editor"
