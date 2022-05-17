e3dc2e0ee263644fd397231719441850275afcce
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-17 14:44:27 +0800

    Update sync-my-l2p.rb

--- Casks/sync-my-l2p.rb
+++ Casks/sync-my-l2p.rb
@@ -2,7 +2,7 @@ cask "sync-my-l2p" do
   version "2.5.1a"
   sha256 "e7dd0afce765444f229c287bf40b67fe00a4692e1a441cda62fb9b88efc13351"
 
-  url "https://github.com/rwthmoodle/Sync-my-L2P/releases/download/v#{version}/SyncMyL2P-#{version.chomp(".a")}-osx.dmg",
+  url "https://github.com/rwthmoodle/Sync-my-L2P/releases/download/v#{version}/SyncMyL2P-#{version.chomp("a")}-osx.dmg",
       verified: "github.com/rwthmoodle/Sync-my-L2P/"
   name "Sync-my-L2P"
   desc "Synchronizes your documents from the L2P and Moodle of RWTH Aachen"
