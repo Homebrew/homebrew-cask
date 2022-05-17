22720cffb1e21dc70bd4965db3df950a23c514ad
Bevan Kay <email@bevankay.me>
2022-05-17 23:15:29 +1000

    Update lifesize from 2.216.2840 to 2.216.2893

--- Casks/lifesize.rb
+++ Casks/lifesize.rb
@@ -1,11 +1,27 @@
 cask "lifesize" do
-  version "2.216.2840"
-  sha256 "75427ddb8e8238d8f93634a0b345cdc90c11c2669fde22c5e66f22c3f2eead65"
+  version "2.216.2893"
+  sha256 "a624026c2a8b26b5812492cb491829ae941924def663ab842c1483a6398a0394"
 
-  url "https://download.lifesizecloud.com/Lifesize-#{version}.dmg",
+  url "https://download.lifesizecloud.com/Lifesize-#{version}-mac.zip",
       verified: "download.lifesizecloud.com/"
   name "lifesize"
   homepage "https://www.lifesize.com/"
 
+  livecheck do
+    url "https://download.lifesizecloud.com/update?version=0.0.0"
+    regex(/Lifesize[._-]v?(\d+(?:\.\d+)+)-mac\.zip/i)
+  end
+
   app "Lifesize.app"
+
+  zap trash: [
+    "~/Library/Application Support/Lifesize",
+    "~/Library/Caches/com.lifesize.cloud",
+    "~/Library/Caches/com.lifesize.cloud.ShipIt",
+    "~/Library/HTTPStorages/com.lifesize.cloud",
+    "~/Library/LaunchAgents/com.lifesize.LifesizeAppService.agent.plist",
+    "~/Library/Logs/Lifesize",
+    "~/Library/Preferences/com.lifesize.cloud.plist",
+    "~/Library/Saved Application State/com.lifesize.cloud.savedState",
+  ]
 end
