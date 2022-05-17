3c6c35fc95d72ac80d0634ae651647393db99b50
Bevan Kay <email@bevankay.me>
2022-05-18 07:09:16 +1000

    razorsql: various updates (#123896)
    Update razorsql from 10.0.3 to 10.0.3

--- Casks/razorsql.rb
+++ Casks/razorsql.rb
@@ -1,14 +1,29 @@
 cask "razorsql" do
-  version "9.2.7"
-  sha256 "683b5124c21166b4747cc6e165ba0a709d932634fa8d881d7a8aca1f3b39b959"
+  arch = Hardware::CPU.intel? ? "" : "_aarch64"
 
-  url "https://s3.dualstack.us-east-1.amazonaws.com/downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}.dmg",
+  version "10.0.3"
+
+  if Hardware::CPU.intel?
+    sha256 "6c2fdb01b8ed53de80fdbc912ec652d56d2add1780227cc0e679a10bc1b2c9e6"
+  else
+    sha256 "11d21dc0e5316b80e4a539c0837ed51e9644946166bbfcb856004a2a586dcd62"
+  end
+
+  url "https://s3.dualstack.us-east-1.amazonaws.com/downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}#{arch}.dmg",
       verified: "s3.dualstack.us-east-1.amazonaws.com/"
-  appcast "https://razorsql.com/updates.html"
   name "RazorSQL"
+  desc "SQL query tool and SQL editor"
   homepage "https://razorsql.com/"
 
-  depends_on macos: ">= :high_sierra"
+  livecheck do
+    url "https://razorsql.com/download_mac.html"
+    regex(/href=.*?razorsql[._-]?v?(\d+(?:[._]\d+)+)#{arch}\.dmg/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| match[0].tr("_", ".") }
+    end
+  end
+
+  depends_on macos: ">= :mojave"
 
   app "RazorSQL.app"
 
