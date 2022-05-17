e0e4fca41ea985d82449a5b8ae83e66a00e0f347
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2022-05-18 07:47:51 +0800

    dropbox-capture 77.0.8
    Update dropbox-capture from 77.0.0 to 77.0.8
    
    Closes #123931.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/dropbox-capture.rb
+++ Casks/dropbox-capture.rb
@@ -1,5 +1,5 @@
 cask "dropbox-capture" do
-  version "77.0.0"
+  version "77.0.8"
   sha256 :no_check
 
   url "https://clientupdates.dropboxstatic.com/dbx-releng/dropbox_capture/mac/Dropbox_Capture.dmg",
