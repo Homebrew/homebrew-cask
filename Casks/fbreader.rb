3a473cee4e5012bec74a5b4eca4f7179aba6c3a5
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-18 09:00:27 +0800

    Update fbreader.rb

--- Casks/fbreader.rb
+++ Casks/fbreader.rb
@@ -1,9 +1,8 @@
 cask "fbreader" do
-  version "0.9.0"
+  version "0.9.0,0.9.20180813"
   sha256 :no_check
 
   url "https://fbreader.org/files/macos/FBReaderMacOS.dmg"
-  appcast "https://fbreader.org/macos/"
   name "FBReader"
   desc "Book reader"
   homepage "https://fbreader.org/macos/"
