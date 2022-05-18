eb411c19ccff9e3108d8df432193d1c636a32adf
miccal <miccal.matthews@gmail.com>
2022-05-18 08:16:46 +0800

    sioyek 1.3.0
    Update sioyek from 1.2.0 to 1.3.0
    
    Closes #123945.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/sioyek.rb
+++ Casks/sioyek.rb
@@ -1,6 +1,6 @@
 cask "sioyek" do
-  version "1.2.0"
-  sha256 "79974b0f419047046ad15f42410a8197a905ec22c0b293454f8f98323828a9d2"
+  version "1.3.0"
+  sha256 "52c64fee7184798d9f9c5d65d4eb1725b677ca59f14b1ec7e3943794e20a70c4"
 
   url "https://github.com/ahrm/sioyek/releases/download/v#{version}/sioyek-release-mac.zip",
       verified: "github.com/ahrm/sioyek/"
