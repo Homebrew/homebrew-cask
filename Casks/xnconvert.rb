d516296cc1d4ee320b81c9c1b648a757ac701938
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-18 11:52:41 +0800

    xnconvert 1.94
    Update xnconvert.rb
    
    Closes #123967.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/xnconvert.rb
+++ Casks/xnconvert.rb
@@ -1,11 +1,16 @@
 cask "xnconvert" do
-  version "1.92"
+  version "1.94"
   sha256 :no_check
 
-  url "https://download.xnview.com/XnConvert-mac-x64.dmg"
+  url "https://download.xnview.com/XnConvert-mac.dmg"
   name "XnSoft XnConvert"
   desc "Image-converter and resizer tool"
   homepage "https://www.xnview.com/en/xnconvert/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   app "XnConvert.app"
 end
