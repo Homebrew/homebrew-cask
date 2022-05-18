ac0e96a13c390550e9e90e0715f734a015d7abfe
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-18 11:29:13 +0800

    Update qgroundcontrol.rb

--- Casks/qgroundcontrol.rb
+++ Casks/qgroundcontrol.rb
@@ -1,12 +1,17 @@
 cask "qgroundcontrol" do
-  version "4.1.4"
+  version "4.2.1"
   sha256 :no_check
 
-  url "https://qgroundcontrol.s3.amazonaws.com/latest/QGroundControl.dmg",
-      verified: "qgroundcontrol.s3.amazonaws.com/latest/"
+  url "https://d176tv9ibo4jno.cloudfront.net/latest/QGroundControl.dmg",
+      verified: "d176tv9ibo4jno.cloudfront.net/latest/"
   name "QGroundControl"
   desc "Ground control station for drones"
   homepage "http://qgroundcontrol.com/"
+
+  livecheck do
+    url "https://github.com/mavlink/qgroundcontrol/releases/"
+    strategy :github_latest
+  end
 
   app "qgroundcontrol.app"
 end
