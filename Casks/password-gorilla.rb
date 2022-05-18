85db827daabd918ce6f159f61636929a1a4a3447
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-18 12:26:00 +0800

    Update password-gorilla.rb

--- Casks/password-gorilla.rb
+++ Casks/password-gorilla.rb
@@ -1,12 +1,16 @@
 cask "password-gorilla" do
-  version "1.5.3.7.3"
+  version "15373"
   sha256 "51c443fb58a3628c2a45bd3160096abb9b017f33e6a08628636168f996ad0414"
 
-  url "https://gorilla.dp100.com/downloads/gorilla.mac.#{version.no_dots}.zip",
+  url "http://gorilla.dp100.com/downloads/gorilla.mac.#{version}.zip",
       verified: "gorilla.dp100.com/"
-  appcast "https://gorilla.dp100.com/downloads/"
   name "Password Gorilla"
   homepage "https://github.com/zdia/gorilla"
+
+  livecheck do
+    url "https://gorilla.dp100.com/downloads/"
+    regex(/gorilla\.mac\.(\d+)\.zip/i)
+  end
 
   app "Password Gorilla.app"
 end
