3bd9f1cf5025d444be4e80ac5cf586762b21e561
Bevan Kay <email@bevankay.me>
2022-05-18 18:51:34 +1000

    maczip4win: add livecheck, add desc (#123979)
    Update maczip4win from 0.4.1 to 0.4.1

--- Casks/maczip4win.rb
+++ Casks/maczip4win.rb
@@ -4,7 +4,16 @@ cask "maczip4win" do
 
   url "https://ynomura.com/MacZip4win_#{version.dots_to_underscores}.zip"
   name "MacZip4Win"
+  desc "Create zip files for use on Windows"
   homepage "https://ynomura.com/wp/?page_id=116"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?MacZip4win[._-]v?(\d+(?:[._]\d+)+)\.zip/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| match[0].tr("_", ".") }
+    end
+  end
 
   app "MacZip4win #{version}/MacZip4Win.app"
 end
