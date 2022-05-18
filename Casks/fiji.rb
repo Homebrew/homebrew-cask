bedc42e0013fce667593e29651080b6f1f439c00
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-18 09:39:37 +0800

    fiji 20220414-1745
    Update fiji.rb
    
    Closes #123953.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/fiji.rb
+++ Casks/fiji.rb
@@ -1,12 +1,17 @@
 cask "fiji" do
-  version "1.0"
-  sha256 :no_check
+  version "20220414-1745"
+  sha256 "99b90c323d9840615d2c3f34295a28beff5f3fa9be4a2b283f3481645c332c83"
 
-  url "https://downloads.imagej.net/fiji/latest/fiji-macosx.zip",
-      verified: "downloads.imagej.net/fiji/"
+  url "https://downloads.imagej.net/fiji/archive/#{version}/fiji-macosx.zip",
+      verified: "downloads.imagej.net/fiji/archive/"
   name "Fiji"
   desc "Open-source image processing package"
   homepage "https://fiji.sc/"
+
+  livecheck do
+    url "https://downloads.imagej.net/fiji/archive/"
+    regex(/(\d{8}-\d{4})/i)
+  end
 
   app "Fiji.app"
 end
