550fa365a9eda1238aeab3318436d01a939f0afb
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-18 09:52:05 +0800

    Update helium.rb

--- Casks/helium.rb
+++ Casks/helium.rb
@@ -2,8 +2,7 @@ cask "helium" do
   version "1.0.0"
   sha256 "30abcdcb04e53f24948897acfd24899c7cdfca564b71b023224ae13f11365bbd"
 
-  url "https://github.com/koush/CarbonResources/releases/download/v#{version}/carbon-mac.zip",
-      verified: "github.com/koush/CarbonResources/"
+  url "https://github.com/koush/CarbonResources/releases/download/v#{version}/carbon-mac.zip"
   name "Helium"
   homepage "https://github.com/koush/support-wiki/wiki/Helium-Desktop-Installer-and-Android-App"
 
