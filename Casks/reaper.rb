34b83ecb17de649abcbd9f1c02c6d0d4c737a6ee
Bevan Kay <email@bevankay.me>
2022-05-19 12:36:20 +1000

    Update reaper.rb

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -3,7 +3,7 @@ cask "reaper" do
 
   if MacOS.version <= :mojave
     sha256 "76d9f5cbcb6e34588e144c77f3fe8d4a06125b8dcd2c81e86c5ab0b288753acc"
-    url "https://www.reaper.fm/files/6.x/reaper658_x86_64.dmg"
+    url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64.dmg"
   else
     sha256 "6777d7b3e7a5d4f8d8f3018d3c0f996a83df50577678c830b16f915a82e1bf0c"
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}_universal.dmg"
