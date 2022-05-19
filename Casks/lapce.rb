35c3d59b0110cb63d1c187ba82f9d7aa52ea615c
Porkepix <porkepix@gmail.com>
2022-05-20 00:40:56 +0200

    Update lapce from 0.1.0 to 0.1.1

--- Casks/lapce.rb
+++ Casks/lapce.rb
@@ -1,6 +1,6 @@
 cask "lapce" do
-  version "0.1.0"
-  sha256 "5b9d43218a4e0e1df59de07c2aaea67674a06a7af47fc80aa24a01775f6bbc66"
+  version "0.1.1"
+  sha256 "d71f4bb930f77af62990dae34e28456dbf217d3dfc2b5867b93c4b98ba9e0e81"
 
   url "https://github.com/lapce/lapce/releases/download/v#{version}/Lapce-macos.dmg",
       verified: "github.com/lapce/lapce/"
