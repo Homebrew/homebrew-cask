45f388312191cdaa406a90a0fd1a4736a454fd4e
mstgrv <mstgrv@gmail.com>
2022-05-19 12:09:50 +1000

    Update reaper from 6.57 to 6.58

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,11 +1,11 @@
 cask "reaper" do
-  version "6.57"
+  version "6.58"
 
   if MacOS.version <= :mojave
     sha256 "b79d96bfef028712ba5610cefd75fbb3e35fc8812676fa999ca61d4378e810c3"
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64.dmg"
   else
-    sha256 "b4a0a94e721777d863d86bf88c37c7a72ff4cb2442751f5dbd39d5dddc102f6a"
+    sha256 "6777d7b3e7a5d4f8d8f3018d3c0f996a83df50577678c830b16f915a82e1bf0c"
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}_universal.dmg"
   end
 
