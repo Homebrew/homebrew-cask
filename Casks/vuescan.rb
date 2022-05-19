987690f6ae62da3488a760b93f51fe12216f4963
Gregor Zurowski <gregor@zurowski.net>
2022-05-19 09:19:32 +0200

    vuescan 9.7.87
    Update vuescan from 9.7.86 to 9.7.87
    
    Closes #124032.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,5 +1,5 @@
 cask "vuescan" do
-  version "9.7.86"
+  version "9.7.87"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.hamrick.com/files/vuea64#{version.major_minor.no_dots}.dmg"
