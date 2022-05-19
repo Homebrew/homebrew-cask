a0aa4c093db2a523b53032394b45dbb15c1c36ea
ZoltanBalazs <60389717+ZoltanBalazs@users.noreply.github.com>
2022-05-19 13:28:39 +0200

    Ghidra 10.1.3 20220519, fix binaries quarantine

--- Casks/ghidra.rb
+++ Casks/ghidra.rb
@@ -1,5 +1,5 @@
 cask "ghidra" do
-  version "10.1.3,20220421"
+  version "10.1.3,20220519"
   sha256 "9c73b6657413686c0af85909c20581e764107add2a789038ebc6eca49dc4e812"
 
   url "https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_#{version.csv.first}_build/ghidra_#{version.csv.first}_PUBLIC_#{version.csv.second}.zip",
@@ -21,6 +21,10 @@ cask "ghidra" do
   preflight do
     # Log4j misinterprets comma in staged_path as alternative delimiter
     FileUtils.mv(staged_path, "#{caskroom_path}/#{version.csv.first}-#{version.csv.second}")
+    # Remove the quarantine flag, otherwise 'decompile' and 'demangler_gnu_...' gets blocked
+    system_command "xattr",
+               args: ["-dr", "com.apple.quarantine", "#{caskroom_path}/#{version.csv.first}-#{version.csv.second}"]
+
   end
 
   uninstall_preflight do
