d3a844ee610d09230dc1c8bedc3a72cdd03871b0
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-05-20 07:50:05 +0800

    Add bilibili-mac with official version of 1.1.2

---
+++ Casks/bilibili-mac.rb
@@ -0,0 +1,25 @@
+cask "bilibili-mac" do
+  version "1.1.2"
+  sha256 :no_check
+
+  url "https://dl.hdslb.com/mobile/fixed/pc_electron_mac/bili_mac.dmg",
+      verified: "dl.hdslb.com/"
+  name "Bilibili"
+  name "哔哩哔哩"
+  desc "Official bilibili video streaming and sharing platform"
+  homepage "https://app.bilibili.com/"
+
+  livecheck do
+    skip "No version information available"
+  end
+
+  auto_updates true
+
+  app "哔哩哔哩.app"
+
+  zap trash: [
+    "~/Library/Application Support/bilibili/",
+    "~/Library/Saved Application State/com.bilibili.bilibiliPC.savedState/",
+    "~/Library/Preferences/com.bilibili.bilibiliPC.plist",
+  ]
+end
