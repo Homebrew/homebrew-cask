a1aa09e9c8a7e3c256aaa4a59f9f4810830ccf61
alokedesai <aloke@warp.dev>
2022-05-20 15:58:42 -0400

    Update Warp Cask to use /download endpoint
    The Warp cask was directly hitting a GCP endpoint. However, the Warp team is making changes and this URL may not be publically available in the future. To fix, I've changed this cask to hit the `/download` endpoint directly, which will return the most up-to-date version of Warp.

--- Casks/warp.rb
+++ Casks/warp.rb
@@ -1,16 +1,16 @@
 cask "warp" do
   version "0.2022.05.16.09.01.stable_01"
-  sha256 "fc912d946eb0ce0b44d8fa5c1ec004112a8a482653bae734251e4fed5c9bfb34"
+  sha256 :no_check
 
-  url "https://warp-releases.storage.googleapis.com/stable/v#{version}/Warp.dmg",
-      verified: "warp-releases.storage.googleapis.com"
+  url "https://app.warp.dev/download"
   name "Warp"
   desc "Rust-based terminal"
   homepage "https://www.warp.dev/"
 
   livecheck do
-    url "https://storage.googleapis.com/warp-releases/channel_versions.json"
+    url :url
     regex(/v(\d+(?:\.\d+)+\.stable_\d+)/i)
+    strategy :header_match
   end
 
   auto_updates true
