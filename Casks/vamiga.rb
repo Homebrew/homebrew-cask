2e419034f344c1de260003ac0d9abc3233aa6bdd
Daniel Sattler <ds@Mini-von-Daniel.fritz.box>
2022-05-20 09:31:21 +0200

    Update vamiga from 2.0 to 2.0.1

--- Casks/vamiga.rb
+++ Casks/vamiga.rb
@@ -1,6 +1,6 @@
 cask "vamiga" do
-  version "2.0"
-  sha256 "ccbb7bc0b807ce471f50327721c47f54a861fac96ce05704194827ee6708a32e"
+  version "2.0.1"
+  sha256 "7b985aef6622689abf300c5e61fd8d90fff2cad69045074317d24e748d5a5a55"
 
   url "https://github.com/dirkwhoffmann/vAmiga/releases/download/v#{version}/vAmiga.app.zip",
       verified: "github.com/dirkwhoffmann/vAmiga/"
