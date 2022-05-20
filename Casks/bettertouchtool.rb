d2e5b2b01cf93a62668fbf75b06175db64e708aa
Sam Ford <1584702+samford@users.noreply.github.com>
2022-05-19 23:18:03 -0400

    bettertouchtool: update livecheck

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -9,11 +9,9 @@ cask "bettertouchtool" do
 
   livecheck do
     url "https://folivora.ai/releases/"
-    strategy :page_match do |page|
-      page.scan(/btt(\d+(?:[._-]\d+)*)\.zip.*?(\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2})/i)
-          .max_by { |(_, time)| Time.parse(time) }
-          .first
-          .tr("-", ",")
+    regex(/btt(\d+(?:[._-]\d+)*)\.zip.*?(\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2})/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).max_by { |match| Time.parse(match[1]) }&.first&.tr("-", ",")
     end
   end
 
