65eb952bc4ddedef45ddce7950febbf7a0de4a56
Eric Liu <eric@Erics-MBP.localdomain>
2022-05-21 00:54:31 -0500

    input-source-pro 2.0.5-beta

---
+++ Casks/input-source-pro.rb
@@ -0,0 +1,26 @@
+cask "input-source-pro" do
+  version "2.0.5-beta"
+  sha256 "4afc880bae4bf78e713b4d302a2ab63ccb04f710c49ea0bff3089489b797334a"
+
+  url "https://inputsource.pro/beta/Input%20Source%20Pro%20#{version}.dmg"
+  name "input-source-pro"
+  desc "Tool for multi-language users"
+  homepage "https://inputsource.pro/"
+
+  livecheck do
+    url "https://inputsource.pro/beta/appcast.xml"
+    strategy :page_match
+    regex(%r{<title>(.*)</title>}i)
+  end
+
+  auto_updates true
+  depends_on macos: ">= :big_sur"
+
+  app "Input Source Pro.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.runjuu.Input-Source-Pro.plist",
+    "~/Library/Application Support/Input Source Pro",
+    "~/Library/Caches/com.runjuu.Input-Source-Pro",
+  ]
+end
