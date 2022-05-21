bceb25d3983fd02244f7e46505086218e9cdc7aa
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-22 05:55:19 +0800

    Update oso-cloud.rb

--- Casks/oso-cloud.rb
+++ Casks/oso-cloud.rb
@@ -1,9 +1,11 @@
 cask "oso-cloud" do
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
+
   version :latest
   sha256 :no_check
 
-  url "https://d3i4cc4dqewpo9.cloudfront.net/latest/oso_cli_mac_osx_#{arch}", verified: "d3i4cc4dqewpo9.cloudfront.net"
+  url "https://d3i4cc4dqewpo9.cloudfront.net/latest/oso_cli_mac_osx_#{arch}",
+      verified: "d3i4cc4dqewpo9.cloudfront.net"
   name "OSO Cloud CLI"
   desc "Tool for interacting with OSO Cloud"
   homepage "https://cloud-docs.osohq.com/get-started/quickstart"
