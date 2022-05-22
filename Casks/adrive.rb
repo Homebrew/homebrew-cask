59c500fb9aa51efcc9143599599f969274fba22f
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-05-23 07:40:05 +0800

    Update adrive with version 3.3.0

--- Casks/adrive.rb
+++ Casks/adrive.rb
@@ -1,18 +1,23 @@
 cask "adrive" do
-  version :latest
+  version "3.3.0"
   sha256 :no_check
 
   url "https://yunpan.aliyun.com/downloads/apps/desktop/aDrive.dmg",
       verified: "yunpan.aliyun.com/"
   name "Aliyundrive"
-  desc "阿里云盘"
+  name "阿里云盘"
+  desc "Intelligent cloud storage platform"
   homepage "https://www.aliyundrive.com/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   app "aDrive.app"
 
   zap trash: [
     "~/Library/Application Support/aDrive",
-    "~/Library/Logs/aDrive",
     "~/Library/Preferences/com.alicloud.smartdrive.plist",
     "~/Library/Saved Application State/com.alicloud.smartdrive.savedState",
   ]
