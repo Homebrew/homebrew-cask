c6fb559f263d6eb05d6159c6fdf22302a8274e71
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-05-22 11:23:14 +0800

    Update and rename adrive.rb to aliyundrive.rb

--- Casks/aliyundrive.rb
+++ Casks/aliyundrive.rb
@@ -1,18 +1,23 @@
-cask "adrive" do
-  version :latest
+cask "aliyundrive" do
+  version "3.3.0"
   sha256 :no_check
 
   url "https://yunpan.aliyun.com/downloads/apps/desktop/aDrive.dmg",
       verified: "yunpan.aliyun.com/"
   name "Aliyundrive"
+  name "阿里云盘"
   desc "阿里云盘"
   homepage "https://www.aliyundrive.com/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   app "aDrive.app"
 
   zap trash: [
     "~/Library/Application Support/aDrive",
-    "~/Library/Logs/aDrive",
     "~/Library/Preferences/com.alicloud.smartdrive.plist",
     "~/Library/Saved Application State/com.alicloud.smartdrive.savedState",
   ]
