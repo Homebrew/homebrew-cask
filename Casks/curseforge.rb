dc51ec49dbfc8f5e26d9a03749faeda72d415580
Bevan Kay <email@bevankay.me>
2022-05-23 22:12:53 +1000

    Update curseforge from 0.195.2-1 to 0.198.1-28

--- Casks/curseforge.rb
+++ Casks/curseforge.rb
@@ -1,6 +1,6 @@
 cask "curseforge" do
-  version "0.195.2-1"
-  sha256 "3a36d5d5eeaebd93022bc8f2d4922fa8e8198ef22d6aeb516e2a8ed869a2e62f"
+  version "0.198.1-28"
+  sha256 "2e23d380c009187518ed35fba84c31015d004c28a8f970ccfa4b280cd22ec5a7"
 
   url "https://curseforge.overwolf.com/electron/mac/CurseForge-#{version}.dmg"
   name "CurseForge"
