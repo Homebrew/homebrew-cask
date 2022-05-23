f3de39d9c42309c5c410f7f2700a3b1f519162e5
Bevan Kay <email@bevankay.me>
2022-05-23 10:39:39 +1000

    Update webull from 5.8.0 to 5.9.0 (#124166)
    * Update webull from 5.8.0 to 5.9.0
    
    * Update webull.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/webull.rb
+++ Casks/webull.rb
@@ -1,15 +1,20 @@
 cask "webull" do
-  version "5.10.0"
-  sha256 "4c4c641149ccab2701f19f154aa9ace2c8570423a676246db04582c4889ef767"
+  dl_arch, livecheck_arch = Hardware::CPU.intel? ? ["us", "qt_mac_global"] : ["u_M1", "qt_m1_global"]
 
-  dl_arch = Hardware::CPU.arm? ? "u_M1" : "us"
+  if Hardware::CPU.intel?
+    version "5.10.0"
+    sha256 "4c4c641149ccab2701f19f154aa9ace2c8570423a676246db04582c4889ef767"
+  else
+    version "5.9.0"
+    sha256 "f93d33869f5f76fec7f1f65f73e38703e72391d2051fcaf928ac36802d7dbd05"
+  end
+
   url "https://u1sweb.webullfinance.com/us/desktop/Webull%20Desktop%20#{version}_#{dl_arch}.dmg",
-      verified: "u1sweb.webullfinance.com"
+      verified: "u1sweb.webullfinance.com/us/desktop/"
   name "Webull"
   desc "Desktop client for Webull Financial LLC"
   homepage "https://webull.com/"
 
-  livecheck_arch = Hardware::CPU.arm? ? "qt_m1_global" : "qt_mac_global"
   livecheck do
     url "https://infoapi.webullfintech.com/api/operation/appver/last?platform=#{livecheck_arch}&osv=10.15"
     strategy :page_match do |page|
