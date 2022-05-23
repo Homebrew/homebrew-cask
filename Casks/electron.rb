7a231971ef2ea4df81d398fd3478fc4ac80ce921
Porkepix <porkepix@gmail.com>
2022-05-24 01:17:23 +0200

    Update electron from 18.2.4 to 19.0.0

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,12 +1,12 @@
 cask "electron" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "18.2.4"
+  version "19.0.0"
 
   if Hardware::CPU.intel?
-    sha256 "573f5b4f82434bd68b46f92bf1db6f5b2825b0a91da7b4f733eee55227f2babc"
+    sha256 "1e0427e18b97273786a52c4472f508c336685c1d90c196ff57116db7f0ecf2be"
   else
-    sha256 "e5265d9e628d11ee2f831e752feb54c3417e0a52b189223db5571dde6eaf713e"
+    sha256 "f3cf6015155167fd0f9ff0958cd9465b66f2be0eea3ecf035a596a20e90a0c96"
   end
 
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-#{arch}.zip",
