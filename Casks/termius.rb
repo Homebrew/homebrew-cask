4659fe24febfe05e532fd5374f4908d01821ee17
luca <luca@palli.ch>
2022-05-23 10:35:51 +0200

    Update termius from 7.40.2 to 7.41.2

--- Casks/termius.rb
+++ Casks/termius.rb
@@ -1,7 +1,7 @@
 cask "termius" do
   arch = Hardware::CPU.intel? ? "mac" : "mac-arm64"
 
-  version "7.40.2"
+  version "7.41.2"
   sha256 :no_check
 
   url "https://autoupdate.termius.com/#{arch}/Termius.dmg"
