1caa88ce33291c7b1f25cdcae030a3e251d26165
Christian Kraemer <christian.kraemer@proximity.de>
2022-05-23 08:29:42 +0200

    Update free-gpgmail from 6,2021.3.1, to 6,2022.1.1,

--- Casks/free-gpgmail.rb
+++ Casks/free-gpgmail.rb
@@ -6,8 +6,8 @@ cask "free-gpgmail" do
     version "5,2021.3.1,-signed"
     sha256 "ee0b1a313afedb5cdf8a9703b00f35040b3109877247bb342c92a60b53944c9a"
   elsif MacOS.version >= :monterey
-    version "6,2021.3.1,"
-    sha256 "d15ae3bad7a40c98a078b5be0891dd38d43240cde7acbfd5f7ac03fafad4bbb1"
+    version "6,2022.1.1,"
+    sha256 "c2c0f54b97e10af583b1db007006d4fa56531879390424aa5ab573d8b0ce66cf"
   end
 
   url "https://github.com/Free-GPGMail/Free-GPGMail/releases/download/v#{version.csv.second}/Free-GPGMail_#{version.csv.first}-#{version.csv.second}#{version.csv.third}.mailbundle.zip"
