9a5d1b681d3ab7ddc4bf72b0cb9bcdc9dfee50b6
Porkepix <porkepix@gmail.com>
2022-05-23 09:53:38 +0200

    Update liteide from 37.4 to 38.0

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -1,6 +1,6 @@
 cask "liteide" do
-  version "37.4"
-  sha256 "a86e48592782e018edb693528ed60c4bcdbd0bac33ec9fd09f44cfff4ed7e77a"
+  version "38.0"
+  sha256 "a48f7ab142144ba0c3b7fee1ea9cb2c03a999d48f0a2e486270cb2cd6e6762d8"
 
   url "https://github.com/visualfc/liteide/releases/download/x#{version}/liteidex#{version}.macos-qt5.15.2.zip"
   name "LiteIDE"
