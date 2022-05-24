fc610a2e0c1b56f7d881b97732c3b53cc5a82389
Yuiham <Yuiham@users.noreply.github.com>
2022-05-24 14:29:51 +0800

    Update input-source-pro from 2.0.5-beta to 2.0.6-beta (#124222)

--- Casks/input-source-pro.rb
+++ Casks/input-source-pro.rb
@@ -1,6 +1,6 @@
 cask "input-source-pro" do
-  version "2.0.5-beta,306"
-  sha256 "4afc880bae4bf78e713b4d302a2ab63ccb04f710c49ea0bff3089489b797334a"
+  version "2.0.6-beta,307"
+  sha256 "7663ec0a28da9e8ab58b5afe3f19de20524dd65af7334127993d2ca811d91e2e"
 
   url "https://inputsource.pro/beta/Input%20Source%20Pro%20#{version.csv.first}.dmg"
   name "Input Source Pro"
