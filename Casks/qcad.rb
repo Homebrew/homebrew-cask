4ef1011e86792c9fb6ff250396366232e4c860fe
Porkepix <porkepix@gmail.com>
2022-05-24 14:27:51 +0200

    qcad 3.27.6
    Update qcad from 3.27.5 to 3.27.6
    
    Closes #124238.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/qcad.rb
+++ Casks/qcad.rb
@@ -1,5 +1,5 @@
 cask "qcad" do
-  version "3.27.5"
+  version "3.27.6"
 
   if Hardware::CPU.intel?
     if MacOS.version <= :high_sierra
@@ -11,7 +11,7 @@ cask "qcad" do
         regex(/qcad[._-]v?(\d+(?:\.\d+)+)[._-]trial[._-]macos[._-]10\.10[._-]10\.13\.dmg/i)
       end
     else
-      sha256 "faf82b51897fe6d50380ef0dac094633d59a6afdf195bc2c8d0946773121bdd8"
+      sha256 "b3861329672b1a44d72de8fe02cc696a955dad6cd7c1cd18da9e064b7ef0f816"
       url "https://www.qcad.org/archives/qcad/qcad-#{version}-trial-macos-10.14-12.dmg"
 
       livecheck do
@@ -20,7 +20,7 @@ cask "qcad" do
       end
     end
   else
-    sha256 "93833ed0d950ceae4a5a693fda65ae6d3212371d543b33e2a4434e4077a558e8"
+    sha256 "a49ffa3adf3087ad9ac59de7fc54c7ea5ef131d6ca2950265916bacc86d3c405"
     url "https://www.qcad.org/archives/qcad/qcad-#{version}-trial-macos-11-12-arm64.dmg"
 
     livecheck do
