ad84982d14ed0586c3debeb3f5b2f87cbc64085b
Porkepix <porkepix@gmail.com>
2022-05-24 14:11:23 +0200

    xemu 0.7.18
    Update xemu from 0.7.17 to 0.7.18
    
    Closes #124234.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/xemu.rb
+++ Casks/xemu.rb
@@ -1,6 +1,6 @@
 cask "xemu" do
-  version "0.7.17"
-  sha256 "340d704e3dadc484f77de06502172c51222df6a9564b54a9548ff2416fb1b893"
+  version "0.7.18"
+  sha256 "fd5e1f2c289ee4a656edd797446676688b1898c33b5e8b9cf523bdfbfed5c1bb"
 
   url "https://github.com/mborgerson/xemu/releases/download/v#{version}/xemu-macos-universal-release.zip",
       verified: "github.com/mborgerson/xemu/"
