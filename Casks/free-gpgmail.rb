6d0cb446a508b2897d9f173fc26d031e7880436b
Christian Kraemer <christian.kraemer@gmail.com>
2022-05-24 02:13:04 +0200

    Update free-gpgmail from 6,2021.3.1, to 6,2022.1.1, (#124172)
    * Update free-gpgmail from 6,2021.3.1, to 6,2022.1.1,
    
    * free-gpgmail: update versions for older macOS
    
    * free-gpgmail: update livecheck
    
    Co-authored-by: Christian Kraemer <christian.kraemer@proximity.de>
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/free-gpgmail.rb
+++ Casks/free-gpgmail.rb
@@ -1,13 +1,13 @@
 cask "free-gpgmail" do
   if MacOS.version <= :catalina
-    version "5,2021.3.1,-unsigned"
-    sha256 "3a3c9290622cc3fe8d4761050363917d009f9fa923eb32d436364f0ae9a161b9"
+    version "5,2022.1,"
+    sha256 "bfcb6765ec1ec7141a73d4f464654ebd34d3a8abc418debcdb3a4c272f92b903"
   elsif MacOS.version <= :big_sur
-    version "5,2021.3.1,-signed"
-    sha256 "ee0b1a313afedb5cdf8a9703b00f35040b3109877247bb342c92a60b53944c9a"
+    version "5,2022.1,_signed"
+    sha256 "2dbce4008294f4e06e3c45828a788f7a9586de6f1e9c3d6c1b65de888f09b401"
   elsif MacOS.version >= :monterey
-    version "6,2021.3.1,"
-    sha256 "d15ae3bad7a40c98a078b5be0891dd38d43240cde7acbfd5f7ac03fafad4bbb1"
+    version "6,2022.1.1,"
+    sha256 "c2c0f54b97e10af583b1db007006d4fa56531879390424aa5ab573d8b0ce66cf"
   end
 
   url "https://github.com/Free-GPGMail/Free-GPGMail/releases/download/v#{version.csv.second}/Free-GPGMail_#{version.csv.first}-#{version.csv.second}#{version.csv.third}.mailbundle.zip"
@@ -19,19 +19,15 @@ cask "free-gpgmail" do
   # cask `version` (based on the execution environment). As such, this won't
   # surface a new major version and that will need to be handled manually.
   livecheck do
-    url :url
-    regex(/href=.*?Free-GPGMail[._-]v?(\d+)-(\d+(?:\.\d+)+)(-[^"' >]+?)?[._-]mailbundle\.zip/i)
-    strategy :github_latest do |page, regex|
+    url "https://github.com/Free-GPGMail/Free-GPGMail/releases?q=prerelease%3Afalse"
+    regex(/href=.*?Free-GPGMail[._-]v?(\d+)[_-](\d+(?:\.\d+)+)([_-][^"' >]+?)?[._-]mailbundle\.zip/i)
+    strategy :page_match do |page, regex|
+      version_suffix = version.csv.third&.sub(/^[_-]/, "")
       page.scan(regex).map do |match|
         next if match[0] != version.csv.first
+        next if match[2]&.sub(/^[_-]/, "") != version_suffix
 
-        if match[2].present?
-          next if version.csv.third.present? && match[2] != version.csv.third
-
-          "#{match[0]},#{match[1]},#{match[2]}"
-        else
-          "#{match[0]},#{match[1]},"
-        end
+        "#{match[0]},#{match[1]},#{match[2]}"
       end
     end
   end
