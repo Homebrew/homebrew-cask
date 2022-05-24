978a4f90dc0d55d649d80f634f1584ee66a77190
Bevan Kay <email@bevankay.me>
2022-05-24 21:00:34 +1000

    Update pushplaylabs-sidekick from 98.20.1.19370 to 100.22.2.19926

--- Casks/pushplaylabs-sidekick.rb
+++ Casks/pushplaylabs-sidekick.rb
@@ -6,8 +6,8 @@ cask "pushplaylabs-sidekick" do
     version "98.17.2.19625,69f5243"
     sha256 "c99732a666b4d25388bf244dc889b532e928622daf2468302e733b740cdf0632"
   else
-    version "98.20.1.19370,18dfa6e"
-    sha256 "6453f3c41b3e4c401d2243ab7e46816f2581c1fe9f1ddcc0f45774feb0aa42fd"
+    version "100.22.2.19926,abd48ab"
+    sha256 "30d99ae034b657fcf9082f78523e6523fd7846493135df269af697ffaa9dc8b7"
   end
 
   url "https://fast-cdn.meetsidekick.com/builds/sidekick-mac-release-#{arch}-#{version.csv.first}-#{version.csv.second}-df.dmg"
