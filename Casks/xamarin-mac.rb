b493952b9611d91cb8e354c9a9800a8d439eca5f
Yurii Kolesnykov <root@yurikoles.com>
2022-05-24 16:48:44 +0300

    xamarin-mac 8.10.0.1
    Update xamarin-mac from 8.8.0.3 to 8.10.0.1
    
    Closes #124244.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/xamarin-mac.rb
+++ Casks/xamarin-mac.rb
@@ -1,6 +1,6 @@
 cask "xamarin-mac" do
-  version "8.8.0.3"
-  sha256 "590be3e7d09888ec6dc9bdc58d0df8ff2de1ecf224638bd4da5c771438f44ab3"
+  version "8.10.0.1"
+  sha256 "3d0ded560db6946545467983a4c4a6190320595995c0e3d5c9ad03a95e573ec8"
 
   url "https://dl.xamarin.com/XamarinforMac/Mac/xamarin.mac-#{version}.pkg"
   name "Xamarin Mac"
