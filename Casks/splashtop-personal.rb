291d607660caa1ab8336e1b4b471a6512859b6ea
miccal <miccal.matthews@gmail.com>
2022-05-25 14:40:46 +0800

    splashtop-personal 3.5.0.3
    Update splashtop-personal from 3.4.8.5 to 3.5.0.3
    
    Closes #124284.
    
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/splashtop-personal.rb
+++ Casks/splashtop-personal.rb
@@ -1,6 +1,6 @@
 cask "splashtop-personal" do
-  version "3.4.8.5"
-  sha256 "80b8c3171d796daf8ccb46e9f9b1ff5f519ba297202ace383d99080ce8625668"
+  version "3.5.0.3"
+  sha256 "f0c89f99e2dc07de9c68e64457cdbad509ac7cde303f3bd65e02adb9654e27b6"
 
   url "https://d17kmd0va0f0mp.cloudfront.net/macclient/STP/Splashtop_Personal_Mac_v#{version}.dmg",
       verified: "d17kmd0va0f0mp.cloudfront.net/"
