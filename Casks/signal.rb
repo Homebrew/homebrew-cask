e3d0729e50e13640d684e636cf1d5a8d3af7aaff
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-05-25 13:52:14 -0700

    Update signal from 5.43.0 to 5.44.0

--- Casks/signal.rb
+++ Casks/signal.rb
@@ -1,12 +1,12 @@
 cask "signal" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "5.43.0"
+  version "5.44.0"
 
   if Hardware::CPU.intel?
-    sha256 "bc7613403848750df876b50d1d4e83ef87283662b48cc1758e0170f951e9b485"
+    sha256 "368f85177c865e793d230a46a2a7be2b60c16b179bc95b2ded8863fec74e84a9"
   else
-    sha256 "4d984f9d3b1757e134c98a561a98bdec5ca8fe1edb5493387d2c3f23c0ae6e4d"
+    sha256 "078cb761dc6135a7215102ba300464fd5dbfabcd5e13f336b8819030a56952f2"
   end
 
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{arch}-#{version}.dmg"
