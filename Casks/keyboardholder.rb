428a328ca6b95ba08033e1188d0db8a857aab13e
leaves chen <leaves615@gmail.com>
2022-05-25 16:53:26 +0800

    upgrade v1.6.2
    fix url does not match homepage.

--- Casks/keyboardholder.rb
+++ Casks/keyboardholder.rb
@@ -2,7 +2,8 @@ cask "keyboardholder" do
   version "1.6.2"
   sha256 "1f381cb3025e1ed8d67ce8e178d5fc4b99565c7401b07586fb844b0c128e5fb0"
 
-  url "https://github.com/leaves615/KeyboardHolder/releases/download/v#{version}/KeyboardHolder-#{version}.zip"
+  url "https://github.com/leaves615/KeyboardHolder/releases/download/v#{version}/KeyboardHolder-#{version}.zip",
+      verified: "github.com/leaves615/KeyboardHolder/"
   name "KeyboardHolder"
   desc "Switch input method per application"
   homepage "http://keyboardholder.leavesc.com/"
