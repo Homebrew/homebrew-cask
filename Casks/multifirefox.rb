0e77830f87f1bf56f2755caf2e6d567adff7301e
miccal <miccal.matthews@gmail.com>
2022-05-25 14:24:52 +0800

    multifirefox 2.3.3
    * Update multifirefox from 2.4.0 to 2.3.3
    * Update multifirefox.rb
    
    Closes #124279.
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/multifirefox.rb
+++ Casks/multifirefox.rb
@@ -1,10 +1,11 @@
 cask "multifirefox" do
-  version "2.4.0"
-  sha256 "7b0cac19469d6a222b4254c0c6ef27cafe847ce89e1efb398d3637f1425645dc"
+  version "2.3.3"
+  sha256 "da08ec6b31a928549783aefcd54664fcf26e107bf09bb1d5439e769a2273b537"
 
   url "https://mff_sparkle.s3.amazonaws.com/MultiFirefox_#{version}.zip",
       verified: "mff_sparkle.s3.amazonaws.com/"
   name "MultiFirefox"
+  desc "Launcher utility to run multiple versions of Firefox side-by-side"
   homepage "https://davemartorana.com/multifirefox/"
 
   livecheck do
