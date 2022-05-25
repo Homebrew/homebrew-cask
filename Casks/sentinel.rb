0c4f4fd2889fcaec678f26416e5cba0c869c96df
Sean Molenaar <SMillerDev@users.noreply.github.com>
2022-05-25 11:46:04 +0200

    sentinel: add arm

--- Casks/sentinel.rb
+++ Casks/sentinel.rb
@@ -1,8 +1,13 @@
 cask "sentinel" do
   version "0.18.10"
-  sha256 "c79d982cf24bbce38bcdbf7deb752ff629d76bba33c9f2b04f48be957113885d"
-
-  url "https://releases.hashicorp.com/sentinel/#{version}/sentinel_#{version}_darwin_amd64.zip"
+  if Hardware::CPU.intel?
+    sha256 "c79d982cf24bbce38bcdbf7deb752ff629d76bba33c9f2b04f48be957113885d"
+  else
+    sha256 "5b116ef4dabaa58677f7a496b55511b091e3fd486d1ed7167e485e9f169f5a42"
+  end
+  
+  arch = Hardware::CPU.intel? ? "amd" : "arm"
+  url "https://releases.hashicorp.com/sentinel/#{version}/sentinel_#{version}_darwin_#{arch}64.zip"
   name "Sentinel"
   desc "Language and framework for policy as code"
   homepage "https://docs.hashicorp.com/sentinel"
