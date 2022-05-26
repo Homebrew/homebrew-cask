43a647ce114a1b5d916dfb7529113ceb3964f9b4
runner <runner@Mac-1653600692087.local>
2022-05-26 21:48:53 +0000

    Update warp from 0.2022.05.16.09.01.stable_01 to 0.2022.05.23.09.07.stable_01

--- Casks/warp.rb
+++ Casks/warp.rb
@@ -1,6 +1,6 @@
 cask "warp" do
-  version "0.2022.05.16.09.01.stable_01"
-  sha256 "fc912d946eb0ce0b44d8fa5c1ec004112a8a482653bae734251e4fed5c9bfb34"
+  version "0.2022.05.23.09.07.stable_01"
+  sha256 "157cc5c6cd5baea2c92e2e675465ca3eba22b236efc0af081db04492107e1320"
 
   url "https://warp-releases.storage.googleapis.com/stable/v#{version}/Warp.dmg",
       verified: "warp-releases.storage.googleapis.com"
