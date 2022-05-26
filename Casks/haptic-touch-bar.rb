ebcde773873bf781cb849c1c18c2abfb86d98cb7
Sam Ford <1584702+samford@users.noreply.github.com>
2022-05-26 16:29:26 -0400

    haptic-touch-bar: update version, url, livecheck

--- Casks/haptic-touch-bar.rb
+++ Casks/haptic-touch-bar.rb
@@ -1,16 +1,21 @@
 cask "haptic-touch-bar" do
-  version "2.4.0,1540815050"
+  version "2.4.0,240,1540815050"
   sha256 "c7b044f1516bb0912e863e2e3a3fd080fa06833eaddc0f056caf55a5e9df94b5"
 
-  url "https://dl.devmate.com/com.bopsoft.HapticTouchBar/#{version.csv.first.no_dots}/#{version.csv.second}/HapticTouchBar-#{version.csv.first.no_dots}.zip",
+  url "https://dl.devmate.com/com.bopsoft.HapticTouchBar/#{version.csv.second}/#{version.csv.third}/HapticTouchBar-#{version.csv.second}.zip",
       verified: "dl.devmate.com/com.bopsoft.HapticTouchBar/"
   name "Haptic Touch Bar"
+  desc "Add haptic feedback to Touch Bar buttons"
   homepage "https://www.haptictouchbar.com/"
 
   livecheck do
     url "https://updates.devmate.com/com.bopsoft.HapticTouchBar.xml"
-    strategy :sparkle do |item|
-      "#{item.short_version},#{item.url[%r{/(\d+)/HapticTouchBar-\d+\.zip}i, 1]}"
+    regex(%r{/(\d+)/HapticTouchBar\d*?[_-]v?(\d+(?:\.\d+)*)\.(?:dmg|zip)}i)
+    strategy :sparkle do |item, regex|
+      match = item.url.match(regex)
+      next if match.blank?
+
+      "#{item.short_version},#{match[2]},#{match[1]}"
     end
   end
 
