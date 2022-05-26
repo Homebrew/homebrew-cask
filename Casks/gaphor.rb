d5475414587f8f701866e0a2139ffe6c6a873d37
hyuraku <32809703+hyuraku@users.noreply.github.com>
2022-05-26 21:53:41 +0900

    gaphor 2.10.0
    Update gaphor from 2.9.2 to 2.10.0
    
    Closes #124371.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/gaphor.rb
+++ Casks/gaphor.rb
@@ -1,6 +1,6 @@
 cask "gaphor" do
-  version "2.9.2"
-  sha256 "47ac7f5c302e5860687cebd59c46c7d2e4358c8c7d6e26491c7353cfc42a0ee6"
+  version "2.10.0"
+  sha256 "a757d2dc0fb197ee9aea564c47754ac753be68fa851dc9605cf79b96ae6c6ed0"
 
   url "https://github.com/gaphor/gaphor/releases/download/#{version}/Gaphor-#{version}.dmg",
       verified: "github.com/gaphor/gaphor/"
