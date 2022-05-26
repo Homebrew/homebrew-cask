3546df2f6ec7fd566bae4bf347685d179739a42c
Leopoldo Pla <leopoldoplasempere@gmail.com>
2022-05-26 10:16:13 +0200

    google-chat-electron 2.16.1
    Update google-chat-electron from 2.15.1 to 2.16.1
    
    Closes #124362.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/google-chat-electron.rb
+++ Casks/google-chat-electron.rb
@@ -1,12 +1,12 @@
 cask "google-chat-electron" do
   arch = Hardware::CPU.intel? ? "darwin-x64" : "darwin-arm64"
 
-  version "2.15.1"
+  version "2.16.1"
 
   if Hardware::CPU.intel?
-    sha256 "671b42125a6100fdf64b07ecde1cd91d8ba1b3caf5530746f96cf4621516eee3"
+    sha256 "54f1345d817a04554464d75c089c086c7d65f67ef19e1e17f560d07bbf3fddd3"
   else
-    sha256 "cf490d915018e12cfaca061cb0aef0ac013600b8c112b06a6219cf377268e1fb"
+    sha256 "b1039641b0eb8f92eb01500b918d2868285dc7fea461a0af62ea412b59598a6b"
   end
 
   url "https://github.com/ankurk91/google-chat-electron/releases/download/#{version}/google-chat-electron-#{version}-#{arch}.zip"
