7763d4ff74a3e0ba44d910c952adb682fda563c9
Sam Ford <1584702+samford@users.noreply.github.com>
2022-05-26 15:56:00 -0400

    dictionaries: update livecheck

--- Casks/dictionaries.rb
+++ Casks/dictionaries.rb
@@ -10,8 +10,12 @@ cask "dictionaries" do
 
   livecheck do
     url "https://updates.devmate.com/io.dictionaries.Dictionaries.xml"
-    strategy :sparkle do |item|
-      "#{item.short_version},#{item.version},#{item.url[%r{/(\d+)/Dictionaries-\d+\.zip}i, 1]}"
+    regex(%r{/(\d+)/Dictionaries\d*?[_-]v?(\d+(?:\.\d+)*)\.(?:dmg|zip)}i)
+    strategy :sparkle do |item, regex|
+      match = item.url.match(regex)
+      next if match.blank?
+
+      "#{item.short_version},#{match[2]},#{match[1]}"
     end
   end
 
