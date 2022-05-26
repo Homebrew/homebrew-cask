563c285886151d67cf063afb57076458e1c5e622
Bevan Kay <email@bevankay.me>
2022-05-26 15:23:17 +1000

    Update piezo.rb

--- Casks/piezo.rb
+++ Casks/piezo.rb
@@ -8,7 +8,7 @@ cask "piezo" do
   homepage "https://rogueamoeba.com/piezo/"
 
   livecheck do
-    url "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&system=1231&bundleid=com.rogueamoeba.Piezo&platform=osx&version=0"
+    url "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&system=1231&bundleid=com.rogueamoeba.Piezo&platform=osx&version=#{version.no_dots}8000"
     strategy :sparkle
   end
 
