c092823efd999d73c53ef4af9b747d9796166f46
miccal <miccal.matthews@gmail.com>
2022-05-26 12:12:48 +0800

    rectangle-pro 2.4.2,113
    Update rectangle-pro from 2.4.1 to 2.4.2
    
    Closes #124345.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/rectangle-pro.rb
+++ Casks/rectangle-pro.rb
@@ -1,6 +1,6 @@
 cask "rectangle-pro" do
-  version "2.4.1,112"
-  sha256 "7bf17bb82c130fb629ad579a7c9e4e1a15541b6da3245c5504e333e4be7736b1"
+  version "2.4.2,113"
+  sha256 "43429d81ea859267c9222309da634123f2b6ce0514e4c1ce2bb466a2437f4c70"
 
   url "https://rectangleapp.com/pro/downloads/Rectangle%20Pro%20#{version.csv.first}.dmg"
   name "Rectangle Pro"
