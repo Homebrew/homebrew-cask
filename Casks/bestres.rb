81c8fbf95bc545d240ba5a3ec20dff7d2f48bec0
Sam Ford <1584702+samford@users.noreply.github.com>
2022-05-26 15:55:59 -0400

    bestres: update livecheck

--- Casks/bestres.rb
+++ Casks/bestres.rb
@@ -10,8 +10,12 @@ cask "bestres" do
 
   livecheck do
     url "https://updates.devmate.com/com.icyberon.BestRes.xml"
-    strategy :sparkle do |item|
-      "#{item.short_version},#{item.version},#{item.url[%r{/(\d+)/BestRes-\d+\.zip}i, 1]}"
+    regex(%r{/(\d+)/BestRes\d*?[_-]v?(\d+(?:\.\d+)*)\.(?:dmg|zip)}i)
+    strategy :sparkle do |item, regex|
+      match = item.url.match(regex)
+      next if match.blank?
+
+      "#{item.short_version},#{match[2]},#{match[1]}"
     end
   end
 
