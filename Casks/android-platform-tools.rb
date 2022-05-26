a664313d3dd77e1c4a52be90627792bef9f316e7
neutric <5984479+neutric@users.noreply.github.com>
2022-05-26 09:00:47 +0200

    android-platform-tools 33.0.2
    Update android-platform-tools from 33.0.1 to 33.0.2
    
    Closes #124357.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/android-platform-tools.rb
+++ Casks/android-platform-tools.rb
@@ -1,6 +1,6 @@
 cask "android-platform-tools" do
-  version "33.0.1"
-  sha256 "7b2acfb3ae02aff2cd679d9df9c125d46543d0db2e95b6b249c25ec6160fddf0"
+  version "33.0.2"
+  sha256 "d347860b1f41187a779f70afde8e1d675ded55a30c304d29e7716e2b32195d09"
 
   url "https://dl.google.com/android/repository/platform-tools_r#{version}-darwin.zip",
       verified: "google.com/android/repository/"
