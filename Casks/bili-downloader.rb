e36f93290cac3e64c6f8a6c3ba034438af9718b1
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-26 09:03:30 +0800

    Update bili-downloader.rb

--- Casks/bili-downloader.rb
+++ Casks/bili-downloader.rb
@@ -4,17 +4,10 @@ cask "bili-downloader" do
 
   url "https://github.com/JimmyLiang-lzm/biliDownloader_GUI/releases/download/V#{version}/BiliDownloader_for_MacOS_X.dmg"
   name "BiliDownloader"
-  desc "B站视频下载程序，BiliBili视频下载器，支持下载B站视频、交互\\互动视频，支持下载区域限制视频资源。"
+  desc "BiliBili media downloader"
   homepage "https://github.com/JimmyLiang-lzm/biliDownloader_GUI"
-
-  livecheck do
-    url "https://github.com/JimmyLiang-lzm/biliDownloader_GUI/releases/latest"
-    strategy :github_latest
-  end
 
   app "biliDownloader_GUI.app"
 
-  zap trash: [
-    "~/Library/Saved Application State/biliDownloader_GUI.savedState",
-  ]
+  zap trash: "~/Library/Saved Application State/biliDownloader_GUI.savedState"
 end
