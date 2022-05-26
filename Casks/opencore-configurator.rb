85afb4aac7ff8bc739e968cdbf81c4ef5fd5ff48
i0ntempest <szf1234@me.com>
2022-05-25 22:34:35 -0400

    opencore-configurator 2.61.0.0
    Update opencore-configurator from 2.60.0.1 to 2.61.0.0
    
    Closes #124341.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/opencore-configurator.rb
+++ Casks/opencore-configurator.rb
@@ -1,5 +1,5 @@
 cask "opencore-configurator" do
-  version "2.60.0.1"
+  version "2.61.0.0"
   sha256 :no_check
 
   url "https://mackie100projects.altervista.org/apps/opencoreconf/download-new-build.php?version=last",
