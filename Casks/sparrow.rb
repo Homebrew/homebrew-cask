a5a757b16698b2b6f290ad9a54736a333c561fb6
Brendan Murphy <brendan@thesimplekid.com>
2022-05-27 09:12:29 -0400

    Update sparrow.rb

--- Casks/sparrow.rb
+++ Casks/sparrow.rb
@@ -4,9 +4,9 @@ cask "sparrow" do
   version "1.6.5"
 
   if Hardware::CPU.intel?
-    sha256 "baa086bc20f200a3a38ee8f35053c4b064b43d1db5f8240ea5e700256eb56e03"
+    sha256 "3fd0f2563365da4b573c8aad9a8170451b08c116f84dbb156a02c92344acd411"
   else
-    sha256 "c43606663bd176c8a335fb9121b5e706b396d604149c6c4f732cfbf00d1d44c4"
+    sha256 "baa086bc20f200a3a38ee8f35053c4b064b43d1db5f8240ea5e700256eb56e03"
   end
 
   url "https://github.com/sparrowwallet/sparrow/releases/download/#{version}/Sparrow-#{version}-#{arch}.dmg",
