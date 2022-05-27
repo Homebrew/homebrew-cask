2f0bae3f19c37a3f7e1cdb021ebe7e93c7606783
cryptoAlgorithm <cryptoalgro@gmail.com>
2022-05-27 11:08:32 +0800

    Fix homepage and version to pass audit checks

--- Casks/swiftcord.rb
+++ Casks/swiftcord.rb
@@ -1,11 +1,11 @@
 cask "swiftcord" do
-  version "0.4.0"
+  version "0.4.0,7"
   sha256 "391c57db0800fc30aa9500d83752b143e2f7b9a0253ffbad2a81554ce0531caa"
 
-  url "https://github.com/SwiftcordApp/Swiftcord/releases/download/v#{version}/Swiftcord.#{version}.dmg"
+  url "https://github.com/SwiftcordApp/Swiftcord/releases/download/v#{version.before_comma}/Swiftcord.#{version.before_comma}.dmg"
   name "Swiftcord"
   desc "Native Discord client built in Swift & SwiftUI"
-  homepage ""
+  homepage "https://github.com/SwiftcordApp/Swiftcord"
 
   livecheck do
     url "https://raw.githubusercontent.com/SwiftcordApp/Swiftcord/main/appcast.xml"
