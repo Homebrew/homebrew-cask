fc5a93d1c685c505e3121344ead1f30ea5bda3ae
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-05-26 20:47:20 -0700

    Update version, URL, OS dependency, zap paths for paragon-extfs

--- Casks/paragon-extfs.rb
+++ Casks/paragon-extfs.rb
@@ -1,10 +1,14 @@
 cask "paragon-extfs" do
-  version :latest
+  version "11.6.237"
   sha256 :no_check
 
   url "https://dl.paragon-software.com/demo/trial_extfs.dmg"
-  name "Paragon ExtFS"
-  homepage "https://www.paragon-software.com/ufsdhome/extfs-mac/"
+  name "extFS for Mac by Paragon Software"
+  desc "Read/write support for ext2/3/4 formatted volumes"
+  homepage "https://www.paragon-software.com/home/extfs-mac/"
+
+  auto_updates true
+  depends_on macos: ">= :sierra"
 
   installer manual: "FSInstaller.app"
 
@@ -17,5 +21,12 @@ cask "paragon-extfs" do
               ["KILL", "com.paragon-software.extfs.notification-agent"],
             ]
 
-  zap trash: "~/Library/Preferences/com.paragon-software.extfs.fsapp.plist"
+  zap trash: [
+    "~/Library/Application Support/com.paragon-software.extfs.*",
+    "~/Library/Caches/com.paragon-software.extfs.fsapp",
+    "~/Library/HTTPStorages/com.paragon-software.extfs.*",
+    "~/Library/Preferences/com.paragon-software.extfs.fsapp.plist",
+    "~/Library/Saved Application State/com.paragon-software.extfs.fsapp.savedState",
+    "~/Library/WebKit/com.paragon-software.extfs.fsapp",
+  ]
 end
