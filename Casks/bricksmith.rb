062ecd2bbb8a8c8f72fb11488c45a00746946984
neutric <5984479+neutric@users.noreply.github.com>
2022-05-28 23:58:36 +0200

    Update bricksmith from 3.0 to 3.1.0

--- Casks/bricksmith.rb
+++ Casks/bricksmith.rb
@@ -1,8 +1,8 @@
 cask "bricksmith" do
-  version "3.0"
-  sha256 "bf7dcd7508d2d6e37f6079080e8ae51095e14d05eb507ad85ac4bbaf5b9ae7c5"
+  version "3.1.0"
+  sha256 "7ea1991b68ae9a75e3107f4df0f01a9980e17f889536fd7cae0ba8c6f56b7d67"
 
-  url "https://downloads.sourceforge.net/bricksmith/bricksmith/Bricksmith%20#{version}/BricksmithComplete#{version}.zip",
+  url "https://downloads.sourceforge.net/bricksmith/bricksmith/Bricksmith%20#{version.major_minor}/BricksmithComplete#{version.major_minor}.zip",
       verified: "downloads.sourceforge.net/bricksmith/"
   name "Bricksmith"
   homepage "https://bricksmith.sourceforge.io/"
