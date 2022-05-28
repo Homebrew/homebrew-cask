f06195956acfb12f1aa392d2c95f86d94bb5d143
neutric <5984479+neutric@users.noreply.github.com>
2022-05-29 00:13:09 +0200

    Update browserosaurus from 18.1.0 to 18.2.0

--- Casks/browserosaurus.rb
+++ Casks/browserosaurus.rb
@@ -1,12 +1,12 @@
 cask "browserosaurus" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "18.1.0"
+  version "18.2.0"
 
   if Hardware::CPU.intel?
-    sha256 "6a8bdd83e52afdee6e16fc98a9657ce322d454f0bd1d251368f121084c0a8322"
+    sha256 "e51fb5c8d7bc6f155b58697091187478b6a8ecce47a118a0e2190b34dd9307e8"
   else
-    sha256 "efa400ebdf9cbaa5d4e544456ab93b1e29ac2fa49972089bdbe956551b188423"
+    sha256 "274ee4ed373acc7dd450b230ae888c27ec40cd9a5494e126711daa3f3b0aa349"
   end
 
   url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-darwin-#{arch}-#{version}.zip"
