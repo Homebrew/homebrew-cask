1475452985b71c2d7d970a499cc67a7a8e15a268
Brad Murray <brad@bradmurray.com>
2022-05-29 10:47:42 -0400

    Fixed change in URL from source

--- Casks/shutter-encoder.rb
+++ Casks/shutter-encoder.rb
@@ -1,15 +1,15 @@
 cask "shutter-encoder" do
-  arch = Hardware::CPU.intel? ? "64bits" : "Silicon"
+  arch = Hardware::CPU.intel? ? "Mac%2064bits" : "Apple%20Silicon"
 
   version "16.0"
 
   if Hardware::CPU.intel?
     sha256 "651986b16223cb9a08b65fb5c57f4547d92623bb40485c24e7257c5a115da5eb"
   else
-    sha256 "58e85131558d462e173009960c91646a254cee0dcb9ee37ed040cc7a0abfc28c"
+    sha256 "6dd2f9a0c36b2480fbebc08e4b05bed6a3809002820bc9416e2a1696f8c302b0"
   end
 
-  url "https://www.shutterencoder.com/Shutter%20Encoder%20#{version}%20Mac%20#{arch}.pkg"
+  url "https://www.shutterencoder.com/Shutter%20Encoder%20#{version}%20#{arch}.pkg"
   name "Shutter Encoder"
   desc "Video, audio and image converter"
   homepage "https://www.shutterencoder.com/"
@@ -19,7 +19,7 @@ cask "shutter-encoder" do
     regex(/^\s*Version\s*(\d+(?:\.\d+)+)/i)
   end
 
-  pkg "Shutter Encoder #{version} Mac #{arch}.pkg"
+  pkg "Shutter Encoder #{version} #{arch}.pkg"
 
   uninstall pkgutil: "com.paulpacifico.shutterencoder",
             quit:    "com.paulpacifico.shutterencoder"
