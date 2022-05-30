4a9bcc5408025740ea1e570d71dd3c030cc8d458
Meet Godhani <meetgodhani23@gmail.com>
2022-05-29 20:45:43 -0400

    Update raven-reader from 1.0.72 to 1.0.73 (#124492)

--- Casks/raven-reader.rb
+++ Casks/raven-reader.rb
@@ -1,6 +1,6 @@
 cask "raven-reader" do
-  version "1.0.72"
-  sha256 "344f514a58a2c400501f4908f898e98cacf6056e6ba2140667c0c575f3ed408a"
+  version "1.0.73"
+  sha256 "c5ded48436c096e9b2e9ff19d94fc2aa22306dd2eab7f0ba031c5e945a0d062d"
 
   url "https://download.helloefficiency.com/ravenreader/Raven%20Reader-#{version}.dmg",
       verified: "download.helloefficiency.com/ravenreader/"
