dbc94ab1b38007d2c4595f245dd279221ba5e66c
Sam Ford <1584702+samford@users.noreply.github.com>
2022-05-29 23:07:29 -0400

    intune-company-portal latest
    * intune-company-portal: use unversioned url
    * Update uninstall stanza
    
    Closes #124515.
    
    Co-authored-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/intune-company-portal.rb
+++ Casks/intune-company-portal.rb
@@ -1,24 +1,16 @@
 cask "intune-company-portal" do
-  version "2.12.210101"
-  sha256 "9b327c4b47129b4bb25e0de4ca16387187e8d7f134f2ff4c58c197f7c26b4abc"
+  version :latest
+  sha256 :no_check
 
-  url "https://officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/CompanyPortal_#{version}-Installer.pkg"
+  url "https://officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/CompanyPortal-Installer.pkg"
   name "Company Portal"
   desc "App to manage access to corporate apps, data, and resources"
   homepage "https://docs.microsoft.com/en-us/mem/intune/user-help/enroll-your-device-in-intune-macos-cp"
 
-  livecheck do
-    url "https://go.microsoft.com/fwlink/?linkid=853070"
-    strategy :header_match do |headers|
-      headers["location"][%r{/CompanyPortal_(\d+(?:\.\d+)*)-Installer\.pkg}i, 1]
-    end
-  end
-
-  auto_updates true
   depends_on cask: "microsoft-auto-update"
   depends_on macos: ">= :mojave"
 
-  pkg "CompanyPortal_#{version}-Installer.pkg",
+  pkg "CompanyPortal-Installer.pkg",
       choices: [
         {
           "choiceIdentifier" => "com.microsoft.package.Microsoft_AutoUpdate.app", # Office16_autoupdate_updater.pkg
@@ -27,10 +19,11 @@ cask "intune-company-portal" do
         },
       ]
 
-  uninstall pkgutil: [
-    "com.microsoft.CompanyPortalMac",
-    "com.microsoft.CompanyPortal",
-  ],
+  uninstall quit:    "com.microsoft.autoupdate2",
+            pkgutil: [
+              "com.microsoft.CompanyPortalMac",
+              "com.microsoft.CompanyPortal",
+            ],
             delete:  [
               "/Applications/Company Portal.app",
             ]
