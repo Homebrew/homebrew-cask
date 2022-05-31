7c8ca95cd61345089c44ffd9be01b0a9c5f28a48
Bevan Kay <email@bevankay.me>
2022-05-31 11:29:56 +1000

    Update simply-fortran from 2 to 3.24.3735 (#124524)
    * Update simply-fortran from 2 to 3.24.3735
    
    * Update simply-fortran.rb
    
    * Update simply-fortran.rb
    
    * Update simply-fortran.rb

--- Casks/simply-fortran.rb
+++ Casks/simply-fortran.rb
@@ -1,20 +1,30 @@
 cask "simply-fortran" do
-  if MacOS.version <= :mojave
-    version "3.15.3382"
-
-    sha256 "45441f0f222822af0a03dcb3da898f07252f739d71a88d534af5dd5f459f8da5"
-    url "https://download.simplyfortran.com/#{version.major_minor}/macos/simplyfortran-#{version}.legacy.dmg"
+  url_string = if MacOS.version <= :big_sur
+    "-x86_64.legacy"
   else
-    version "3.15.3384"
-
-    sha256 "b9f5e479097643089b5a6999498d3bafbea2f682e5fb8a73fa7d1d11e1dfdf16"
-    url "https://download.simplyfortran.com/#{version.major_minor}/macos/simplyfortran-#{version}.dmg"
+    Hardware::CPU.intel? ? "-x86_64" : "-arm64"
   end
-  appcast "https://simplyfortran.com/download/?platform=macos",
-          must_contain: version.major_minor
+
+  if MacOS.version <= :big_sur
+    version "3.24.3735"
+    sha256 "1b854b1bf5d982d43385cea735b0112ffa17f900e2ed8e42da1c01a9df5ca804"
+  elsif Hardware::CPU.intel?
+    version "3.24.3734"
+    sha256 "b4913da2ef5ff4c962f29ed897c9afcb1cbcad7dee0647683227ada899b965a2"
+  else
+    version "3.24.3735"
+    sha256 "c548c5911b5e23699bcfee2756da5d8680f8654a4833437542524e6cc2eb992a"
+  end
+
+  url "https://download.simplyfortran.com/#{version.major_minor}/macos/simplyfortran-#{version}#{url_string}.dmg"
   name "Simply Fortran"
   desc "Fortran development environment"
   homepage "https://simplyfortran.com/"
+
+  livecheck do
+    url "https://simplyfortran.com/download/?platform=macos"
+    regex(/href=.*?simplyfortran[._-]v?(\d+(?:\.\d+)+)#{url_string}\.dmg/i)
+  end
 
   app "Simply Fortran.app"
 end
