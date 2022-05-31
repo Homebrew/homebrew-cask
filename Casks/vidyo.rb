f6c89dd0f5ff17cc61eaab9c944855189101939e
Bevan Kay <email@bevankay.me>
2022-05-31 21:47:48 +1000

    Update vidyo from 3.6.18.0009 to 22.2.0.17850

--- Casks/vidyo.rb
+++ Casks/vidyo.rb
@@ -1,10 +1,18 @@
 cask "vidyo" do
-  version "3.6.18.0009"
-  sha256 "6f1d1dcc8e707befc07e3f251ecad007e027016f4203b71396c45384b9a1dd5e"
+  version "22.2.0.17850"
+  sha256 "da00ed1a052c7b49100475825f440748c2cf8db516c374957cd082b57c98d952"
 
-  url "https://client-downloads.vidyocloud.com/VidyoDesktopInstaller-macosx-TAG_VD_#{version.dots_to_underscores}.dmg"
+  url "https://client-downloads.vidyocloud.com/VidyoConnectInstaller-macosx-TAG_VCOND_#{version.dots_to_underscores}.dmg"
   name "Vidyo"
-  homepage "https://vidyocloud.com/"
+  homepage "https://vidyocloud.com/download.html?lang=en"
+
+  livecheck do
+    url :homepage
+    regex(/https.*?VidyoConnectInstaller[._-]macosx[._-]TAG[._-]VCOND[._-]v?(\d+(?:[._]\d+)+)\.dmg/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| match.first.tr("_", ".") }
+    end
+  end
 
   depends_on macos: ">= :sierra"
 
