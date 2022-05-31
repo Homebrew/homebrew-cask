40fed5c8ecb9fe6a380c6a7fd97868c852798430
Guillaume <hi@255kb.dev>
2022-05-31 13:59:10 +0200

    mockoon 1.19.0
    * Update Mockoon to v1.19.0
    * Update mockoon.rb
    * Update mockoon.rb
    
    Closes #124607.
    
    Co-authored-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/mockoon.rb
+++ Casks/mockoon.rb
@@ -1,12 +1,19 @@
 cask "mockoon" do
-  version "1.18.1"
-  sha256 "4e891dce08f0b8adc2d7026e52f56a8ff2a23ff110eb2955a846d40775ae0fb8"
+  version "1.19.0"
+  sha256 "563478a999649d6815e516740d3277dff27365fe062f3e590aac21842eb6bb76"
 
   url "https://github.com/mockoon/mockoon/releases/download/v#{version}/mockoon.setup.#{version}.universal.dmg",
       verified: "github.com/mockoon/mockoon/"
   name "Mockoon"
   desc "Create mock APIs in seconds"
   homepage "https://mockoon.com/"
+
+  # a regex is required to skip tags that relate to mockoon cli
+  livecheck do
+    url :url
+    strategy :git
+    regex(/^v?(\d+(?:\.\d+)+)/i)
+  end
 
   auto_updates true
 
