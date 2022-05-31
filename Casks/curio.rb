21f88d79c87906153a385fda3505d49654ad9565
Bevan Kay <email@bevankay.me>
2022-05-31 12:35:26 +1000

    Update curio.rb

--- Casks/curio.rb
+++ Casks/curio.rb
@@ -1,5 +1,5 @@
 cask "curio" do
-  version "22,22102"
+  version "22.1,22102"
   sha256 "1e4fc3def7251ed79078b83ad2f0f6235bf5a2f9ad8d6e9de6753f748293e9f9"
 
   url "https://www.zengobi.com/downloads/Curio#{version.csv.second.no_dots}.zip"
@@ -9,9 +9,7 @@ cask "curio" do
 
   livecheck do
     url "https://www.zengobi.com/curio/appcasts/Curio#{version.major}.xml"
-    strategy :sparkle do |item|
-      item.version.to_s
-    end
+    strategy :sparkle
   end
 
   depends_on macos: ">= :mojave"
