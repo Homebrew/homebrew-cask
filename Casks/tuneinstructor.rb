18a12c68ede1b44e0de4fce8ff41f08ed32f5a5b
Bevan Kay <email@bevankay.me>
2022-05-31 21:34:48 +1000

    Update tuneinstructor from 3.7v5,17467 to 3.7v5

--- Casks/tuneinstructor.rb
+++ Casks/tuneinstructor.rb
@@ -1,11 +1,16 @@
 cask "tuneinstructor" do
-  version "3.7v5,17467"
+  version "3.7v5"
   sha256 "bbd0255f809da259b6c4a9d7f7fc27de2bceb5dc7caca32adc61769d9d98f9de"
 
   url "https://www.tune-instructor.de/resources/downloads/TuneInstructor#{version.csv.first}.zip"
-  appcast "https://www.tune-instructor.de/de/download.html"
   name "Tune•Instructor"
+  desc "Menu bar control for Apple Music"
   homepage "https://www.tune-instructor.de/com/start.html"
+
+  livecheck do
+    url "https://www.tune-instructor.de/de/download.html"
+    regex(/href=.*?TuneInstructor[._-]?v?(\d+(?:[.v]\d+)+)\.zip/i)
+  end
 
   depends_on macos: ">= :catalina"
 
