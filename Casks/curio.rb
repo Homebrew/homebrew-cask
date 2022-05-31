8dbdc336fe34ad21d4cea5b56ad38e82bcadc3ae
ShredderWalnut <34428332+ShredderWalnut@users.noreply.github.com>
2022-05-31 10:40:06 +0800

    Update curio from 21 to 22 (#124529)
    * Update curio from 21 to 22
    
    * Update curio.rb
    
    * curio: fix livecheck
    
    * Update curio.rb
    
    * Update curio.rb
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/curio.rb
+++ Casks/curio.rb
@@ -1,6 +1,6 @@
 cask "curio" do
-  version "21,21006"
-  sha256 "e46f943ee66cbab7708c1eb30339539ccf8312c24f878f298ea0b678f52727b1"
+  version "22.1,22102"
+  sha256 "1e4fc3def7251ed79078b83ad2f0f6235bf5a2f9ad8d6e9de6753f748293e9f9"
 
   url "https://www.zengobi.com/downloads/Curio#{version.csv.second.no_dots}.zip"
   name "Curio"
@@ -8,13 +8,8 @@ cask "curio" do
   homepage "https://zengobi.com/curio/"
 
   livecheck do
-    url "https://www.zengobi.com/curio/download/"
-    strategy :header_match do |headers|
-      match = headers["location"].match(/Curio(\d+)\.zip/i)
-      next if match.blank?
-
-      "#{match[1][0..1]},#{match[1]}"
-    end
+    url "https://www.zengobi.com/curio/appcasts/Curio#{version.major}.xml"
+    strategy :sparkle
   end
 
   depends_on macos: ">= :mojave"
