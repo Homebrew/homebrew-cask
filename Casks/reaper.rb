ae1b02a9ccc40329c7291afec63d5dacff394afa
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-31 10:05:42 +0800

    Update reaper.rb

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -2,7 +2,7 @@ cask "reaper" do
   version "6.59"
 
   if MacOS.version <= :mojave
-    sha256 "76d9f5cbcb6e34588e144c77f3fe8d4a06125b8dcd2c81e86c5ab0b288753acc"
+    sha256 "6c389154be9593777224038e989e4f52a11e931dc4d59481f9e9036c14a49dfd"
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64.dmg"
   else
     sha256 "8679107de0276e1ee077083709c041b1fb342ca72d91cadb8519cadd77fa72d3"
