2c1206679b7c589a398786fc2c41f319c7980889
Porkepix <porkepix@gmail.com>
2022-06-01 18:20:59 +0200

    Update miniforge from 4.12.0-0 to 4.12.0-2

--- Casks/miniforge.rb
+++ Casks/miniforge.rb
@@ -1,12 +1,12 @@
 cask "miniforge" do
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 
-  version "4.12.0-0"
+  version "4.12.0-2"
 
   if Hardware::CPU.intel?
-    sha256 "3bfcd52dd1acafc712b6fb042d4b019c6f09ea3ba62710f722f4e9ebcde7f67c"
+    sha256 "37007407ab504fb8bd3af68ff821c0819ad2f016087b9c45f1e95a910c92531e"
   else
-    sha256 "e52cb92d620c5a408afef9ac8b5d2e964e2c72211c69cc41a2bb4d6af0a26001"
+    sha256 "24181b1a42c6bb9704e28ac4ecb234f3c86d882a7db408948692bc5792a2f713"
   end
 
   url "https://github.com/conda-forge/miniforge/releases/download/#{version}/Miniforge3-#{version}-MacOSX-#{arch}.sh"
