924daeeb914dd1b699c88ee5c821b9b3c075b59b
Bevan Kay <email@bevankay.me>
2022-06-01 16:47:34 +1000

    prezi-video 2.7.0
    * Update prezi-video from 1.13.0 to 2.7.0
    * Update prezi-video.rb
    
    Closes #124656.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/prezi-video.rb
+++ Casks/prezi-video.rb
@@ -1,13 +1,21 @@
 cask "prezi-video" do
-  version "1.13.0"
-  sha256 "477a3d199b1f108e3e1e394a93787fde89f499dea273937c0c1f5fd410b66410"
+  version "2.7.0"
+  sha256 "94a6a182fdbec06f52a66111637fa8db15eb0d795beec78f2067dfb988a51a41"
 
-  url "https://desktopassets.prezi.com/mac/prezi-video/releases/Prezi_Video_#{version}.dmg"
+  url "https://desktopassets.prezi.com/mac/prezi-video/releases/Prezi_Video_#{version}.zip"
   name "Prezi Video"
   desc "Lets you interact with your content live as you stream or record"
   homepage "https://prezi.com/video/"
 
-  pkg "Install Prezi Video.pkg"
+  livecheck do
+    url "https://s3.amazonaws.com/prezidesktop/assets/mac/prezi-video/updates/prezi-video.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
+  pkg "Prezi_Video.pkg"
 
   uninstall quit:      "com.prezi.PreziCast",
             launchctl: "com.prezi.prezivideo.vcam.assistant",
