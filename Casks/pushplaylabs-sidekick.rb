160264b3d858debfede2768bcb74fe0dd89f3279
Bevan Kay <email@bevankay.me>
2022-06-01 12:11:10 +1000

    pushplaylabs-sidekick 100.22.3.19960,0c40532
    * Update pushplaylabs-sidekick from 100.22.2.19926 to 100.22.3.19962
    * Update pushplaylabs-sidekick.rb
    * Update pushplaylabs-sidekick.rb
    
    Closes #124637.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/pushplaylabs-sidekick.rb
+++ Casks/pushplaylabs-sidekick.rb
@@ -3,11 +3,11 @@ cask "pushplaylabs-sidekick" do
   livecheck_folder = Hardware::CPU.intel? ? "mac" : "macm1"
 
   if Hardware::CPU.intel?
-    version "100.22.2.19924,58f5ad3"
-    sha256 "8627ac36f8a8b746968c4b2f0ec347912f17535230de294305df3b324305d54a"
+    version "100.22.3.19960,0c40532"
+    sha256 "d08cb275f5050f4157a90d1d501beb59488bd66be370d1b41b1341d030c29db6"
   else
-    version "100.22.2.19926,abd48ab"
-    sha256 "30d99ae034b657fcf9082f78523e6523fd7846493135df269af697ffaa9dc8b7"
+    version "100.22.3.19962,d931e5c"
+    sha256 "a5aa4b2607a9b69990d08852e7b15acf8737e8dc7bc61b19cb6da44682885e8b"
   end
 
   url "https://fast-cdn.meetsidekick.com/builds/sidekick-mac-release-#{arch}-#{version.csv.first}-#{version.csv.second}-df.dmg"
