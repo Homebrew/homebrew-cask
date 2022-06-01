a8029a5e285014a479d69dff410f7822677fd1b7
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-06-01 23:08:06 +0800

    Update tencent-meeting from 3.7.8.428 to 3.8.4.421

--- Casks/tencent-meeting.rb
+++ Casks/tencent-meeting.rb
@@ -2,11 +2,11 @@ cask "tencent-meeting" do
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 
   if Hardware::CPU.intel?
-    version "3.7.8.428,1429a132028e21c73ae97dff10ccebbd"
-    sha256 "f7f591b947f3b8861ae5ab5319a18699c10f0b000bcc4a3ccb14bdd14c3c7713"
+    version "3.8.4.421,2ddd0b88cb3d4c0250da7dda8d18ce80"
+    sha256 "92f78e3fc97d7d7d1e9268c3d204078c921622ca08bfbd40581f2e4c8986aee1"
   else
-    version "3.7.8.428,b554ca7731015ff9819dd9090a36aea4"
-    sha256 "490c6ddc2c09822f6c043a2f1bd45b21c160606bf5acddd947a649a43f83108d"
+    version "3.8.4.421,54df800f5fdb8d593977ada9a4ef867a"
+    sha256 "d2f5a1bfa6121411ef8f45ebc5abffb209c0b7f6ab94fda3b689f53bbc3a32f5"
   end
 
   url "https://updatecdn.meeting.qq.com/cos/#{version.csv.second}/TencentMeeting_0300000000_#{version.csv.first}.publish.#{arch}.dmg",
@@ -17,10 +17,6 @@ cask "tencent-meeting" do
   homepage "https://meeting.tencent.com/"
 
   livecheck do
-    # curl -H 'Content-Type: application/json' \
-    #   --data-binary \
-    #   '[{"instance":"mac","type":"0300000000"},{"instance":"mac_arm64","type":"0300000000"}]' \
-    #   https://meeting.tencent.com/wemeet-webapi/v2/config/query-download-info
     skip "No version information available"
   end
 
