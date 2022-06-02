a44d7472b3be864234a56fb71874940ac4f2add8
Eduard Bosch <eduardboschbertra@gmail.com>
2022-06-02 15:55:12 +0200

    Use qlcolorcode for m1 on arm64

--- Casks/qlcolorcode.rb
+++ Casks/qlcolorcode.rb
@@ -1,11 +1,18 @@
 cask "qlcolorcode" do
-  version "4.1.0"
-  sha256 "a0f7a97a20cd85eebaef15e399ad29c47711dae2e33e9f51de034f3c8a7a251f"
+  version Hardware::CPU.intel? ? "4.1.0" : "4.1.2+m1"
 
-  url "https://github.com/anthonygelibert/QLColorCode/releases/download/release-#{version}/QLColorCode.qlgenerator.zip"
   name "QLColorCode"
   desc "QuickLook plug-in that renders source code with syntax highlighting"
-  homepage "https://github.com/anthonygelibert/QLColorCode"
+
+  if Hardware::CPU.intel?
+    url "https://github.com/anthonygelibert/QLColorCode/releases/download/release-#{version}/QLColorCode.qlgenerator.zip"
+    sha256 "a0f7a97a20cd85eebaef15e399ad29c47711dae2e33e9f51de034f3c8a7a251f"
+    homepage "https://github.com/anthonygelibert/QLColorCode"
+  else
+    url "https://github.com/jpc/QLColorCode/releases/download/release-#{version}/QLColorCode-#{version}.zip"
+    sha256 "2cd375ed04ad7c164ebfbdf5ea9dbf9dc99bbb104b044fd70fe389dc2a836e91"
+    homepage "https://github.com/jpc/QLColorCode"
+  end
 
   depends_on macos: ">= :mojave"
 
