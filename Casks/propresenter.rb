bddf8897e59b424f203d4fb29f91b62da7f37c4a
Bevan Kay <email@bevankay.me>
2022-06-03 09:14:43 +1000

    Update propresenter from 7.9.1 to 7.9.2

--- Casks/propresenter.rb
+++ Casks/propresenter.rb
@@ -1,6 +1,6 @@
 cask "propresenter" do
-  version "7.9.1,118030598"
-  sha256 "4e30cae8879b65ba40879b81206e2df1b513d409cf1e128c151fb057670783a3"
+  version "7.9.2,118030852"
+  sha256 "61e77b2035c6350c80e8c94464dbeb74dc8cb0466410c74cb910ac9cda2a2cd3"
 
   url "https://renewedvision.com/downloads/propresenter/mac/ProPresenter_#{version.csv.first}_#{version.csv.second}.zip"
   name "ProPresenter"
