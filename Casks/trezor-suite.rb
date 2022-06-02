5fda1dbca264c5dafbe7d4c9385a95353b4ab7f8
owine <owine@users.noreply.github.com>
2022-06-02 13:12:26 -0500

    trezor-suite 22.5.3
    Update trezor-suite from 22.5.2 to 22.5.3
    
    Closes #124758.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/trezor-suite.rb
+++ Casks/trezor-suite.rb
@@ -1,12 +1,12 @@
 cask "trezor-suite" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "22.5.2"
+  version "22.5.3"
 
   if Hardware::CPU.intel?
-    sha256 "f5bba9bcee3239ee8ff5ddb0fccf70408e9be5bbfbc2a2c412994e9e37d3d73a"
+    sha256 "d7e2b538c443155a6c3d43d5213d003fab86161da4f1bdcf3c2708fee83d4fd0"
   else
-    sha256 "d3efd6807572ca184081c95ce66989efe4a64ab965b33d0b85c0b99aee65632a"
+    sha256 "f423d6f7b1684e740bb1425f8a8a429818006102f5ab0f5332f495c12aae96dd"
   end
 
   url "https://suite.trezor.io/web/static/desktop/Trezor-Suite-#{version}-mac-#{arch}.dmg"
