89ef4ec42d3bbdb4c865ca4b6bb51a4caa781dec
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-02 20:18:06 -0700

    Remove completions that no longer exist

--- Casks/docker.rb
+++ Casks/docker.rb
@@ -33,15 +33,11 @@ cask "docker" do
   ]
 
   app "Docker.app"
-  binary "#{appdir}/Docker.app/Contents/Resources/etc/docker.bash-completion",
-         target: "#{HOMEBREW_PREFIX}/etc/bash_completion.d/docker"
   binary "#{appdir}/Docker.app/Contents/Resources/etc/docker-compose.bash-completion",
          target: "#{HOMEBREW_PREFIX}/etc/bash_completion.d/docker-compose"
-  binary "#{appdir}/Docker.app/Contents/Resources/etc/docker.zsh-completion",
-         target: "#{HOMEBREW_PREFIX}/share/zsh/site-functions/_docker"
   binary "#{appdir}/Docker.app/Contents/Resources/etc/docker-compose.zsh-completion",
          target: "#{HOMEBREW_PREFIX}/share/zsh/site-functions/_docker_compose"
-  binary "#{appdir}/Docker.app/Contents/Resources/etc/docker.fish-completion",
+  binary "#{appdir}/Docker.app/Contents/Resources/etc/docker.fish",
          target: "#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/docker.fish"
   binary "#{appdir}/Docker.app/Contents/Resources/etc/docker-compose.fish-completion",
          target: "#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/docker-compose.fish"
