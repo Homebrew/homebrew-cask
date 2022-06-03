6ae3e13030939e560d985e98c9f663b69b774d73
neutric <5984479+neutric@users.noreply.github.com>
2022-06-03 03:08:38 +0200

    Update dynobase from 1.9.3 to 1.10.0

--- Casks/dynobase.rb
+++ Casks/dynobase.rb
@@ -1,15 +1,15 @@
 cask "dynobase" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  version "1.9.3"
+  version "1.10.0"
 
   if Hardware::CPU.intel?
-    sha256 "2b3f8b01c6c1a4f84dd1566181910c20156484fa6a593f16e87f7f5d0dc52d79"
+    sha256 "a30a7c51e65c40b579dd15dca1e3dfe09a42aa49197f5133ef7fc0556917ca8f"
   else
-    sha256 "899ae8cc8076c70f15bed821693813b9679880669e9b5d55df58527dd8926f7e"
+    sha256 "4fead01c0652554cfacb5ae5aed33f1afbab676712809e318811cbe70661c599"
   end
 
-  url "https://github.com/Dynobase/dynobase/releases/download/#{version}/Dynobase-#{version}#{arch}.dmg",
+  url "https://github.com/Dynobase/dynobase/releases/download/v#{version}/Dynobase-#{version}#{arch}.dmg",
       verified: "github.com/Dynobase/dynobase/"
   name "Dynobase"
   desc "GUI Client for DynamoDB"
