983a98b3c91ad946452a93750a7b723f7be23bdf
neutric <5984479+neutric@users.noreply.github.com>
2022-06-03 02:49:33 +0200

    Update dropzone from 4.3.1 to 4.3.5

--- Casks/dropzone.rb
+++ Casks/dropzone.rb
@@ -1,6 +1,6 @@
 cask "dropzone" do
-  version "4.3.1,1550"
-  sha256 "4d9b4868327432501a039abe458b54fa5e94de582d806519ef0f3943b8c403d6"
+  version "4.3.5,1594"
+  sha256 "475a72094e400aaf2d24480ffd460df33767e99750f3257c037d6453ad2bea93"
 
   url "https://aptonic.com/releases/Dropzone-#{version.csv.first}.zip"
   name "Dropzone"
