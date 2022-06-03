8814692d5a37a8eade35d1c37d9466ef71ac3fba
Brad Murray <brad@bradmurray.com>
2022-06-02 20:02:44 -0400

    Update android-messages-desktop.rb

--- Casks/android-messages-desktop.rb
+++ Casks/android-messages-desktop.rb
@@ -2,7 +2,7 @@ cask "android-messages-desktop" do
   version "5.3.4"
   sha256 "f1259938832ed6bfcd3fabce185b8d20f932300e4489e4a0da96bf5671b5a522"
 
-  url "https://github.com/OrangeDrangon/android-messages-desktop/releases/download/v#{version}/AndroidMessages-v#{version}-mac-universal.dmg"
+  url "https://github.com/OrangeDrangon/android-messages-desktop/releases/download/v#{version}/Android-Messages-v#{version}-mac-universal.zip"
   name "Android Messages Desktop"
   desc "Desktop client for Android Messages"
   homepage "https://github.com/OrangeDrangon/android-messages-desktop"
