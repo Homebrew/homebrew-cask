208ade56e60f31f35eaf06de96574d1aa4a48610
Bevan Kay <email@bevankay.me>
2022-06-04 17:10:32 +1000

    Update mysql-connector-python from 8.0.23 to 8.0.29

--- Casks/mysql-connector-python.rb
+++ Casks/mysql-connector-python.rb
@@ -1,11 +1,23 @@
 cask "mysql-connector-python" do
-  version "8.0.23"
-  sha256 "640178dec7b72cae7caa6d07c4eb1ad7afe0c5e097dde56e1c6a2ef5351bf496"
+  arch = Hardware::CPU.intel? ? "x86-64bit" : "arm64"
 
-  url "https://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-#{version}-macos10.15.dmg"
+  version "8.0.29"
+
+  if Hardware::CPU.intel?
+    sha256 "028437530f54db39d91d3830e8966af5ab9d1540867e1ee14bd428ed7fc21b54"
+  else
+    sha256 "59c5e0a509cefa7ed8daa4aee228de8d38898d0a68a7469898e8aa1a2a425053"
+  end
+
+  url "https://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-#{version}-macos12-#{arch}.dmg"
   name "MySQL Connector for Python"
   desc "Self-contained Python driver for communicating with MySQL servers"
   homepage "https://dev.mysql.com/downloads/connector/python/"
+
+  livecheck do
+    url "https://dev.mysql.com/downloads/connector/python/?tpl=files&os=33"
+    regex(/href=.*?mysql[._-]connector[._-]python[._-]v?(\d+(?:\.\d+)+)[._-]macos12[._-]#{arch}\.dmg/i)
+  end
 
   # despite the "10.15" in the download name, this is compatible with 10.14 and above
   depends_on macos: ">= :mojave"
