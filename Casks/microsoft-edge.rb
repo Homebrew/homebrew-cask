c03a37edd879f207f5c73fac92eedb9aa91e91fe
Reno <singularitti@outlook.com>
2022-06-03 23:06:03 -0400

    microsoft-edge 102.0.1245.33
    Update microsoft-edge from 102.0.1245.30 to 102.0.1245.33
    
    Closes #124832.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/microsoft-edge.rb
+++ Casks/microsoft-edge.rb
@@ -2,12 +2,12 @@ cask "microsoft-edge" do
   folder = Hardware::CPU.intel? ? "C1297A47-86C4-4C1F-97FA-950631F94777" : "03adf619-38c6-4249-95ff-4a01c0ffc962"
   linkid = Hardware::CPU.intel? ? "2069148" : "2093504"
 
-  version "102.0.1245.30"
+  version "102.0.1245.33"
 
   if Hardware::CPU.intel?
-    sha256 "296f70a750243a7ede298ab01e527413bba2eba2b5db433e52c3efd9df96f5b4"
+    sha256 "f5848b80343c8d71d8f15716e8ed66a98ba88bc5ab71b836529d86fc4da55809"
   else
-    sha256 "10adc25ebd34837e24a4f9bd3c4777430178d51e86a8729cd62172cbbbd39270"
+    sha256 "42008b2520d79fa8b77e5db276b36162573df6b467198a0649ceac34f05691ed"
   end
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/#{folder}/MacAutoupdate/MicrosoftEdge-#{version}.pkg",
@@ -36,6 +36,7 @@ cask "microsoft-edge" do
 
   zap trash: [
     "~/Library/Application Support/Microsoft Edge",
+    "~/Library/Application Support/Microsoft/EdgeUpdater",
     "~/Library/Caches/Microsoft Edge",
     "~/Library/Preferences/com.microsoft.edgemac.plist",
     "~/Library/Saved Application State/com.microsoft.edgemac.savedState",
