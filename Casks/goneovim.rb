664bc84e07aa253420ec95b48fe58e6061019b56
Porkepix <porkepix@gmail.com>
2022-06-05 12:50:44 +0200

    goneovim 0.6.1
    * Update goneovim from 0.5.1 to 0.6.1
    * Update goneovim.rb
    
    Closes #124899.
    
    Co-authored-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/goneovim.rb
+++ Casks/goneovim.rb
@@ -1,6 +1,6 @@
 cask "goneovim" do
-  version "0.5.1"
-  sha256 "0e00d37872534bf2c495a5ad01c6a2b63790f96cd4a864e7335821d3694774d4"
+  version "0.6.1"
+  sha256 "870083f880f66619566e8f7a574c562e7dd3f71fc0e86b6706d12e85e76dda73"
 
   url "https://github.com/akiyosi/goneovim/releases/download/v#{version}/Goneovim-v#{version}-macos.tar.bz2"
   name "Goneovim"
@@ -14,7 +14,7 @@ cask "goneovim" do
 
   depends_on formula: "neovim"
 
-  app "Goneovim-latest-macos/goneovim.app"
+  app "Goneovim-v#{version}-macos/goneovim.app"
   # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
   shimscript = "#{staged_path}/goneovim.wrapper.sh"
   binary shimscript, target: "goneovim"
