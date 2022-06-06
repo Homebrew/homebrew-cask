5a0731fc9151ffc556f2d29626a0a914980617f8
Bevan Kay <email@bevankay.me>
2022-06-07 09:24:08 +1000

    Update icab.rb

--- Casks/icab.rb
+++ Casks/icab.rb
@@ -15,7 +15,7 @@ cask "icab" do
 
   depends_on macos: ">= :high_sierra"
 
-  app "iCab #{version.major_minor}/iCab.app"
+  app "iCab #{version}/iCab.app"
 
   zap trash: [
     "~/Library/Application Support/iCab",
