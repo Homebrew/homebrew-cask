4743581e864e1845cdce47d36b4b0f4cc81363d2
alokedesai <aloke@warp.dev>
2022-06-06 09:09:44 -0400

    change download url

--- Casks/warp.rb
+++ Casks/warp.rb
@@ -2,8 +2,7 @@ cask "warp" do
   version "0.2022.05.16.09.01.stable_01"
   sha256 "fc912d946eb0ce0b44d8fa5c1ec004112a8a482653bae734251e4fed5c9bfb34"
 
-  url "https://warp-releases.storage.googleapis.com/stable/v#{version}/Warp.dmg",
-      verified: "warp-releases.storage.googleapis.com"
+  url "https://app.warp.dev/download/brew?version=v#{version}"
   name "Warp"
   desc "Rust-based terminal"
   homepage "https://www.warp.dev/"
