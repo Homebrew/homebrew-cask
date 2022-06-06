7234c47d879d1975056f11f656a9df51a64e9cfe
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2022-06-06 22:12:00 +0800

    Add Apple Silicon version, and update URL
    Looks like previous URL not working anymore. Also there is Apple Silicon version.

--- Casks/dropbox-capture.rb
+++ Casks/dropbox-capture.rb
@@ -1,9 +1,10 @@
 cask "dropbox-capture" do
+  arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
   version "78.0.2"
   sha256 :no_check
 
-  url "https://clientupdates.dropboxstatic.com/dbx-releng/dropbox_capture/mac/Dropbox_Capture.dmg",
-      verified: "clientupdates.dropboxstatic.com/dbx-releng/dropbox_capture/mac/"
+  url "https://edge.dropboxstatic.com/dbx-releng/products/dropbox-capture/#{version}/mac.#{arch}/Dropbox_Capture.dmg",
+      verified: "edge.dropboxstatic.com/dbx-releng/products/dropbox-capture/"
   name "Dropbox Capture"
   desc "Share your work and ideas with video messages and screenshots"
   homepage "https://dropbox.com/capture/"
