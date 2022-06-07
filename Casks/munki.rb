44e9c545a47a51cffad84e347cc100d20df720a5
Porkepix <porkepix@gmail.com>
2022-06-08 01:27:07 +0200

    munki 5.7.1.4434
    Update munki from 5.7.0.4429 to 5.7.1.4434
    
    Closes #125006.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/munki.rb
+++ Casks/munki.rb
@@ -1,6 +1,6 @@
 cask "munki" do
-  version "5.7.0.4429"
-  sha256 "72fe04d40e9c54ed248c7e7af6fc2fd2ce753eb9ed89a512dbd5b5a999fc5d26"
+  version "5.7.1.4434"
+  sha256 "3b00461db482934bdc6849dc9f8db03e5359dc32f99fe88cacd3f234d4f88ba1"
 
   url "https://github.com/munki/munki/releases/download/v#{version.major_minor_patch}/munkitools-#{version}.pkg",
       verified: "github.com/munki/munki/"
