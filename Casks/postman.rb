9b3ddcbb40d295fda967f5ce4689e971a6f5db5f
MoonFruit <dkmoonfruit@gmail.com>
2022-06-08 06:20:54 +0800

    Update postman from 9.19.0 to 9.19.3 (#124571)
    * Update postman from 9.19.0 to 9.19.3
    
    * Update postman.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,22 +1,17 @@
 cask "postman" do
-  arch = Hardware::CPU.intel? ? "osx64" : "osx_arm64"
+  arch = Hardware::CPU.intel? ? "osx_64" : "osx_arm64"
 
-  version "9.19.0"
+  version "9.20.3"
+  sha256 :no_check
 
-  if Hardware::CPU.intel?
-    sha256 "629bf940f0466a6684144e6cd7fc2352e7d005491a8d0d70d4968de1e32f1856"
-  else
-    sha256 "3d0f3e1cfee53135d08a2ba5624f187a6b2b7d9746e9d39700622947fae0fd07"
-  end
-
-  url "https://dl.pstmn.io/download/version/#{version}/#{arch}",
-      verified: "dl.pstmn.io/download/version/"
+  url "https://dl.pstmn.io/download/latest/#{arch}",
+      verified: "dl.pstmn.io/download/latest/"
   name "Postman"
   desc "Collaboration platform for API development"
   homepage "https://www.postman.com/"
 
   livecheck do
-    url "https://dl.pstmn.io/download/latest/#{arch}"
+    url :url
     strategy :header_match
   end
 
