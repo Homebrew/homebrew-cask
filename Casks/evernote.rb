4ecfe689365faaee16952f6ac07c926be6cd575d
LanikSJ <laniksj@gmail.com>
2022-06-07 16:02:35 -0700

    evernote: rebuild
    Update evernote from 10.38.1 to 10.38.3
    
    Closes #125002.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/evernote.rb
+++ Casks/evernote.rb
@@ -15,8 +15,8 @@ cask "evernote" do
 
     url "https://cdn1.evernote.com/mac-smd/public/Evernote_RELEASE_#{version}.dmg"
   else
-    version "10.38.1,3408,83c94402ae"
-    sha256 "eb1e0562304af2d44a4e78ead32fe96e4ded0c3d3b25c0cf264a9409e445f263"
+    version "10.38.3,3437,801d2ba198"
+    sha256 "a9aa4b8b602fd8a866dd3082ac398864a44e7955ed005635eaeea69514345815"
 
     url "https://cdn1.evernote.com/boron/mac/builds/Evernote-#{version.csv.first}-mac-ddl-ga-#{version.csv.second}-#{version.csv.third}.dmg"
   end
