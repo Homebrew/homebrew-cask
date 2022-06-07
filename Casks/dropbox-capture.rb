86382949a265363b12b325358509820f417c1fce
Bevan Kay <email@bevankay.me>
2022-06-07 18:44:07 +1000

    Add shasum, update livecheck

--- Casks/dropbox-capture.rb
+++ Casks/dropbox-capture.rb
@@ -1,7 +1,13 @@
 cask "dropbox-capture" do
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
+
   version "78.0.2"
-  sha256 :no_check
+
+  if Hardware::CPU.intel?
+    sha256 "de8d64f5a3c3057796d23f9e1804ab9495f451d9b767fa0359cd8663670554a9"
+  else
+    sha256 "2fe7b0e743cf706d88c2045b8d77624d1bdc4d86229aaa1eb7118646b8740aa1"
+  end
 
   url "https://edge.dropboxstatic.com/dbx-releng/products/dropbox-capture/#{version}/mac.#{arch}/Dropbox_Capture.dmg",
       verified: "edge.dropboxstatic.com/dbx-releng/products/dropbox-capture/"
@@ -10,8 +16,8 @@ cask "dropbox-capture" do
   homepage "https://dropbox.com/capture/"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url "https://dropbox.com/capture/download"
+    strategy :header_match
   end
 
   auto_updates true
