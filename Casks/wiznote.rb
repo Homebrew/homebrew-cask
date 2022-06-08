5b79119bf5eb95139c37848659cec3ef93f051d4
Aaron Jheng <wentworth@outlook.com>
2022-06-08 17:17:29 +0800

    Update wiznote from 0.1.78 to 0.1.81 (#125023)

--- Casks/wiznote.rb
+++ Casks/wiznote.rb
@@ -1,12 +1,12 @@
 cask "wiznote" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "0.1.78"
+  version "0.1.81"
 
   if Hardware::CPU.intel?
-    sha256 "60cef0ea6aa5902834703f29feea3530bd490a6abebece31c6d5f305da404dff"
+    sha256 "156a972c7b86dee5d7d6498e546b5328bac02e9ab9d73e064a2063c0a64e7679"
   else
-    sha256 "58621faf168a2aa65d56b89efb34269dd31a3b04715b7f73cf82a6d297a0e34e"
+    sha256 "56b4329ad573c9fd71fa604c7a4e9bf71a2d505e7af3c17fa38e12e9e95bd51b"
   end
 
   url "https://get.wiz.cn/x/wiznote-desktop-#{version}-mac-#{arch}.dmg"
