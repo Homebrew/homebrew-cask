acd6a3cd7c15c38355755890174d3bec099e2e07
Aditya Dalal <adidalal@users.noreply.github.com>
2022-06-08 01:10:04 -0400

    syncovery 9.47q
    Update syncovery from 9.47i to 9.47q
    
    Closes #125020.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/syncovery.rb
+++ Casks/syncovery.rb
@@ -1,12 +1,12 @@
 cask "syncovery" do
   arch = Hardware::CPU.intel? ? "" : "-Apple"
 
-  version "9.47i"
+  version "9.47q"
 
   if Hardware::CPU.intel?
-    sha256 "5dd31de3d5079cadd4c19470d2277ef8744101b7cc90291829bf92afb6108e3c"
+    sha256 "77fb7bf43bc8804dfc2a8aef89605f8f007a42a19cd7139f24975c808da0555a"
   else
-    sha256 "cab53d204cb29aed6a6934fd749ba742d5d12d445ed097825b14c04bb27855af"
+    sha256 "110bd3d9337de29a7222fe37e51a0916a2f555b4ec06c2afc5381f6f8b1fee75"
   end
 
   url "https://www.syncovery.com/release/SyncoveryMac#{version}#{arch}.dmg"
