a47e2407440eaf531d41e4c3b9c2bbce56afa23a
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-09 13:04:58 -0700

    keybase 6.0.1,20220609162338,0fc9feea3d
    Update keybase from 5.9.2 to 6.0.1
    
    Closes #125209.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/keybase.rb
+++ Casks/keybase.rb
@@ -1,6 +1,6 @@
 cask "keybase" do
-  version "5.9.2,20220131221715,a25f15e42b"
-  sha256 "dfdba447f395862a6efa38b061b202b49e0a1c549c8a24c53fb189567564e376"
+  version "6.0.1,20220609162338,0fc9feea3d"
+  sha256 "eda297371c83ce8f58e93be20a19ed9112e4e6755022ed709a8180bdfbd1449d"
 
   url "https://prerelease.keybase.io/darwin-updates/Keybase-#{version.csv.first}-#{version.csv.second}%2B#{version.csv.third}.zip"
   name "Keybase"
