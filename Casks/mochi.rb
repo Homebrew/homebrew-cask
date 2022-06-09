3d3170ae17dd37f12ced21fd0afaab36eb440664
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-09 13:18:56 -0700

    mochi 1.14.2
    Update mochi from 1.13.13 to 1.14.2
    
    Closes #125224.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/mochi.rb
+++ Casks/mochi.rb
@@ -1,6 +1,6 @@
 cask "mochi" do
-  version "1.13.13"
-  sha256 "c2c95117d51a97f3cdf7c35a2e07f602d81cff630bf19a139428a8d784d07dba"
+  version "1.14.2"
+  sha256 "25b5abe75a9d28a37ff3b2f9a0e4c32636e8a7d2d3af8eb8e4daf37db9c7dcbb"
 
   url "https://mochi.cards/releases/Mochi-#{version}.dmg"
   name "Mochi"
