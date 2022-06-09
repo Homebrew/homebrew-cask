a9a4d61741952ead2640fa04cd07df01aa9dcd55
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-09 14:26:29 -0700

    Update tiger-trade from 7.13.2 to 7.15.1

--- Casks/tiger-trade.rb
+++ Casks/tiger-trade.rb
@@ -1,6 +1,6 @@
 cask "tiger-trade" do
-  version "7.13.2,20220429,417A80"
-  sha256 "e327d9ffc6c430b303664c7126436f4fb1b6f58a1a9e8cad326f240929e4eb1c"
+  version "7.15.1,20220609,5CF571"
+  sha256 "4d4f5c4a3016186044ab1b535d792cbdc1e9e2688a90236e59ddfed954da3772"
 
   url "https://s.tigerfintech.com/desktop/cdn/f/TigerTrade_#{version.tr(",", "_")}.dmg",
       verified: "s.tigerfintech.com/"
