48273b0082d9297ef6ac25fc68f5a863f766e10c
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-09 16:02:48 -0700

    fix missed version

--- Casks/simply-fortran.rb
+++ Casks/simply-fortran.rb
@@ -9,7 +9,7 @@ cask "simply-fortran" do
     version "3.25.3754"
     sha256 "eefcde488f42eaed99dd64215b7ec05bb2daf6c824418cede10c63a74128a220"
   elsif Hardware::CPU.intel?
-    version "3.24.3734"
+    version "3.25.3754"
     sha256 "b4913da2ef5ff4c962f29ed897c9afcb1cbcad7dee0647683227ada899b965a2"
   else
     version "3.25.3754"
