63a0aac7f0aa10990b2dc224f7d986f30e55274d
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-06-09 12:02:53 -0400

    Update listen1 from 2.21.7 to 2.23.0

--- Casks/listen1.rb
+++ Casks/listen1.rb
@@ -2,14 +2,14 @@ cask "listen1" do
   # NOTE: "1" is not a version number, but an intrinsic part of the product name
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "2.21.7"
+  version "2.23.0"
 
   url "https://github.com/listen1/listen1_desktop/releases/download/v#{version}/Listen1_#{version}_mac_#{arch}.dmg",
       verified: "github.com/listen1/listen1_desktop/"
   if Hardware::CPU.intel?
-    sha256 "cb34ffe4af595eaf205ed0e6c6c7738ba46bdda1e9280c17129e38bc41c6bcdb"
+    sha256 "9574e20c3e7332be7496ff63c3919a659f5626212648841e8943786143245c61"
   else
-    sha256 "84e6f5aeeec61404db32a0f6a4dc326f56b2f068e2dd033188868fb7ed2f6073"
+    sha256 "f678d40a4635a826978fd940bc6e3b1e7bca55791fa50a13a7d55998ebe70ff0"
   end
 
   name "Listen 1"
