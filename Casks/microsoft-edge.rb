684ccb1904588c112954009d1773c977052fc5b9
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-09 13:11:05 -0700

    Update microsoft-edge from 102.0.1245.33 to 102.0.1245.39

--- Casks/microsoft-edge.rb
+++ Casks/microsoft-edge.rb
@@ -2,12 +2,12 @@ cask "microsoft-edge" do
   folder = Hardware::CPU.intel? ? "C1297A47-86C4-4C1F-97FA-950631F94777" : "03adf619-38c6-4249-95ff-4a01c0ffc962"
   linkid = Hardware::CPU.intel? ? "2069148" : "2093504"
 
-  version "102.0.1245.33"
+  version "102.0.1245.39"
 
   if Hardware::CPU.intel?
-    sha256 "f5848b80343c8d71d8f15716e8ed66a98ba88bc5ab71b836529d86fc4da55809"
+    sha256 "ec48251f586bb3bcbf90e4b94ac1553787a335b1997add1e6742e767ac3eb355"
   else
-    sha256 "42008b2520d79fa8b77e5db276b36162573df6b467198a0649ceac34f05691ed"
+    sha256 "91c742288b8a4549cea14fa5237396ebd0ade2f761634fad114294f4f3547767"
   end
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/#{folder}/MacAutoupdate/MicrosoftEdge-#{version}.pkg",
