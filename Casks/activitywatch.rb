d3b431da6e4549fad6c25fd2f4c7ad4527bbeb6e
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-09 09:27:09 -0700

    Update activitywatch from 0.11.0 to 0.12.0b2

--- Casks/activitywatch.rb
+++ Casks/activitywatch.rb
@@ -1,6 +1,6 @@
 cask "activitywatch" do
-  version "0.11.0"
-  sha256 "91deabd5844968467b5b6b3423e12654d4e339ba309f333c25d991a2e689fd96"
+  version "0.12.0b2"
+  sha256 "208a9803aeee108c9c697be0a5f250e1d01b5410138efe38fa85544f9e5219ae"
 
   url "https://github.com/ActivityWatch/activitywatch/releases/download/v#{version}/activitywatch-v#{version}-macos-x86_64.dmg",
       verified: "github.com/ActivityWatch/activitywatch/"
