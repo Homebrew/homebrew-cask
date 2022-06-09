2ac8534331151907b38c9117ff76a8ceaea8ab6b
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-09 14:07:57 -0700

    Update specter from 1.9.4 to 1.10.2

--- Casks/specter.rb
+++ Casks/specter.rb
@@ -1,6 +1,6 @@
 cask "specter" do
-  version "1.9.4"
-  sha256 "0846a4e0279ea67ad5297d4b29f3bc1130418361bf47a01528402e6d8b8ace79"
+  version "1.10.2"
+  sha256 "4c87ec3b567e459e35df3b93407879ee5c137e78b433ec8d463f8b863eb2b4e5"
 
   url "https://github.com/cryptoadvance/specter-desktop/releases/download/v#{version}/Specter-v#{version}.dmg",
       verified: "github.com/cryptoadvance/specter-desktop/"
