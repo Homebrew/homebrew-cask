36ab82307cddb36421eaedc4fdd8d5963eb741ff
Porkepix <porkepix@gmail.com>
2022-06-09 17:12:02 +0200

    Update iina-plus from 0.6.12 to 0.6.13

--- Casks/iina-plus.rb
+++ Casks/iina-plus.rb
@@ -1,6 +1,6 @@
 cask "iina-plus" do
-  version "0.6.12"
-  sha256 "ce1809d46402f7e2faa5a7b6a73ad3a841f0f236f2bd966d0831dc105519bf78"
+  version "0.6.13"
+  sha256 "a62a48f111ef3f3a3448cf9dbe04b72824f499bc4c31c35a2ac6a432975a1157"
 
   url "https://github.com/xjbeta/iina-plus/releases/download/#{version}/IINA+.#{version}.dmg"
   name "IINA+"
