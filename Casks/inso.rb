90731d0c044222b8ae0ac57dc42010037a3553ea
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-09 13:01:05 -0700

    inso 3.1.0
    Update inso from 3.0.0 to 3.1.0
    
    Closes #125208.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/inso.rb
+++ Casks/inso.rb
@@ -1,6 +1,6 @@
 cask "inso" do
-  version "3.0.0"
-  sha256 "510449c29582dd58276aba44497e05781b86df872ac8bdbf4491371cd4ecc50b"
+  version "3.1.0"
+  sha256 "c748238f34bba0cea755e4a4fad164efd816013e401aaaac715c2c8c209b3257"
 
   url "https://github.com/Kong/insomnia/releases/download/lib%40#{version}/inso-macos-#{version}.zip",
       verified: "github.com/Kong/insomnia/"
