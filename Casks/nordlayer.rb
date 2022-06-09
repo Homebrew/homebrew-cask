8dbf001ca032ceeab4dbf5ef53ac6865bcb46f1a
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-09 13:21:58 -0700

    nordlayer 2.3.0,1896
    Update nordlayer from 2.2.0 to 2.3.0
    
    Closes #125230.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/nordlayer.rb
+++ Casks/nordlayer.rb
@@ -1,6 +1,6 @@
 cask "nordlayer" do
-  version "2.2.0,1823"
-  sha256 "d8d701346ef29a04418c83c8aafc99d3ec322583485cdb9aeea68cf91cc840ff"
+  version "2.3.0,1896"
+  sha256 "6b61b2d0e8c37e48c2dd7fc5f3fd9ecb7a929a49dbe9b817bec7e0a75af647da"
 
   url "https://downloads.nordlayer.com/mac/latest/NordLayer_v#{version.csv.first}.zip"
   name "NordLayer"
