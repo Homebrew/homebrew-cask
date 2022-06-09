aec418feea556cb9f3a153356eb9c57cc65b24ec
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-09 13:25:52 -0700

    Update paintcode from 3.5.3 to 3.5.4

--- Casks/paintcode.rb
+++ Casks/paintcode.rb
@@ -1,6 +1,6 @@
 cask "paintcode" do
-  version "3.5.3,35014"
-  sha256 "543e9249228a0040d1d4eeef30c2ed66342b828c0aac511c186764fc9b13547b"
+  version "3.5.4,35015"
+  sha256 "7e8598e9d0f4c8d84207a02a9deecf126c116e0ab158c35a54b9bb1f17da46fc"
 
   url "https://www.pixelcut.com/paintcode#{version.major}/paintcode_#{version.csv.first}.zip",
       verified: "pixelcut.com/"
