1b0e9847981c77a6369fbff532aadfbd113e65d2
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-09 14:01:54 -0700

    shearwater-cloud 2.9.1
    Update shearwater-cloud from 2.8.3 to 2.9.1
    
    Closes #125289.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/shearwater-cloud.rb
+++ Casks/shearwater-cloud.rb
@@ -1,6 +1,6 @@
 cask "shearwater-cloud" do
-  version "2.8.3"
-  sha256 "cb3c5d88d0444c817137650a6f839935a8e9cee57294d6698fc38470df81b290"
+  version "2.9.1"
+  sha256 "ef8c0b31d4c1b72255a659fc725ebc2ac627a290ed78642a1e9e38a87ed4f670"
 
   url "https://www.shearwater.com/wp-content/downloads/ShearwaterCloudInstaller_#{version}.dmg"
   name "Shearwater Cloud"
