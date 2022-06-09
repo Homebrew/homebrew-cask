3cd35d68bb81d23c7a8f28d099986fcff667b109
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-09 13:35:35 -0700

    principle 6.17,6021
    Update principle from 6.15 to 6.17
    
    Closes #125265.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/principle.rb
+++ Casks/principle.rb
@@ -1,6 +1,6 @@
 cask "principle" do
-  version "6.15,6017"
-  sha256 "457891dd5ad80b6af7acc0d675895351687298411fe4ca6d340584fdbfcff2f4"
+  version "6.17,6021"
+  sha256 "ccfb918bd020f030afa2e3079a1a5fb372e1f07a6485cf782123697652d2ed5a"
 
   url "https://principleformac.com/download/Principle_#{version.csv.first.dots_to_underscores}.zip"
   name "Principle"
