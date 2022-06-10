13210ab519479f27cdbe28a8203ce24145d19a54
neutric <5984479+neutric@users.noreply.github.com>
2022-06-11 01:46:12 +0200

    mongodb-realm-studio 12.0.0
    * Update mongodb-realm-studio from 11.1.2 to 12.0.0
    * Update mongodb-realm-studio.rb
    
    Closes #125405.
    
    Co-authored-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/mongodb-realm-studio.rb
+++ Casks/mongodb-realm-studio.rb
@@ -1,16 +1,16 @@
 cask "mongodb-realm-studio" do
-  version "11.1.2"
-  sha256 "6552fe80de8e4606d24090bd8f0ed843fcde57fdd859271498a418e60b78c1a7"
+  version "12.0.0"
+  sha256 "451e06769b0fa2c1ec0fa94d7892aaafdc01b9a954133e5e6c4a85132344dfd5"
 
-  url "https://github.com/realm/realm-studio/releases/download/v#{version}/MongoDB.Realm.Studio-#{version}.dmg",
+  url "https://github.com/realm/realm-studio/releases/download/v#{version}/Realm.Studio-#{version}.dmg",
       verified: "github.com/realm/realm-studio/"
-  name "MongoDB Realm Studio"
+  name "Realm Studio"
   desc "Tool for the Realm Database and Realm Platform"
   homepage "https://realm.io/products/realm-studio/"
 
   auto_updates true
 
-  app "MongoDB Realm Studio.app"
+  app "Realm Studio.app"
 
   zap delete: [
     "~/Library/Application Support/Realm Studio",
