0996bf2e4ec3e3524a3f484fbd2fe0e0c277d3db
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-11 14:26:33 -0700

    hbuilderx 3.4.15.20220610
    Update hbuilderx from 3.4.7.20220422 to 3.4.15.20220610
    
    Closes #125462.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/hbuilderx.rb
+++ Casks/hbuilderx.rb
@@ -1,6 +1,6 @@
 cask "hbuilderx" do
-  version "3.4.7.20220422"
-  sha256 "fb4ca6e7021fbd18ca075312a79446d9e2d93346ad141488587ac09f8747308d"
+  version "3.4.15.20220610"
+  sha256 "24f0f58d4d45208b2f826606be8efda4f86a5183d8fb5eb2a696195290aef2d6"
 
   url "https://download1.dcloud.net.cn/download/HBuilderX.#{version}.dmg",
       verified: "download1.dcloud.net.cn/"
