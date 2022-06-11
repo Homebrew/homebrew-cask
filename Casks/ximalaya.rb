fe228a83dd78b1c2ff4cf8b5b7ed06eb1767fefd
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-06-11 11:18:54 -0400

    ximalaya 3.0.0,1654661122
    Update ximalaya from 2.9.0 to 3.0.0
    
    Closes #125452.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/ximalaya.rb
+++ Casks/ximalaya.rb
@@ -1,6 +1,6 @@
 cask "ximalaya" do
-  version "2.9.0,1652688817"
-  sha256 "7cbac7f7219c039e5a233033c55bf6555af611db917d380a86ba4e33f78771c3"
+  version "3.0.0,1654661122"
+  sha256 "3b86078245565fcfb98d373e5a55732c77ee1c8cfabc39cb744a1bf4dd1c85f2"
 
   url "https://s1.xmcdn.com/yx/ximalaya-pc-client/#{version.csv.first}/download/mac-x64/Ximalaya_#{version.csv.first}_#{version.csv.second}_x64.dmg",
       verified: "s1.xmcdn.com/yx/ximalaya-pc-client/"
