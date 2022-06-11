9058bca0fb51d37395cbb949114a726c7b06d5d5
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-06-11 08:26:53 -0400

    Update 3dgenceslicer from 3.1.1 to 3.2.0

--- Casks/3dgenceslicer.rb
+++ Casks/3dgenceslicer.rb
@@ -1,8 +1,8 @@
 cask "3dgenceslicer" do
-  version "3.1.1,4.0"
-  sha256 "53f5e56af69d969cd4aec5360c6846857ab95d93e8d7326322ab8b372122a20c"
+  version "3.2.0,4.0"
+  sha256 "fc9b95d9602c3f2a5b89e5eb539f1a4e75cf8d0d04763c9122a4ee394721d3ca"
 
-  url "https://cloud.3dgence.com/downloads/slicer/3DGence%20Slicer%20#{version.csv.second}%20(v.#{version.csv.first}).dmg"
+  url "https://cloud.3dgence.com/downloads/slicer/3DGenceSlicer-#{version.csv.second}-(v#{version.csv.first}).dmg"
   name "3DGence Slicer"
   desc "Prepare files for 3D printing based on CAD models for 3DGence printers"
   homepage "https://3dgence.com/"
