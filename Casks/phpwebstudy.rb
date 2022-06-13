05df1e3963a025029ac97b0673d98b36e7d9fa8f
徐鹏飞 <250881478@qq.com>
2022-06-14 05:01:37 +0800

    Add PhpWebStudy.app v1.0.14 (#125507)
    * Add PhpWebStudy.app v1.0.14
    
    * Add PhpWebStudy.app v1.0.14
    
    * Update Casks/phpwebstudy.rb
    
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>
    
    * Update phpwebstudy.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>

---
+++ Casks/phpwebstudy.rb
@@ -0,0 +1,25 @@
+cask "phpwebstudy" do
+  version "1.0.14"
+  sha256 "e7d4ef8a485e69513ac0f860be013dd3405c0bcb2d474aa3bdeba4caf4257f05"
+
+  url "https://github.com/xpf0000/PhpWebStudy/releases/download/v#{version}/PhpWebStudy-#{version}-mac.zip",
+      verified: "github.com/xpf0000/PhpWebStudy/"
+  name "PhpWebStudy"
+  desc "PHP and Web develop environment manager"
+  homepage "https://www.macphpstudy.com/"
+
+  livecheck do
+    url "https://raw.githubusercontent.com/xpf0000/PhpWebStudy/master/latest-mac.yml"
+    strategy :electron_builder
+  end
+
+  auto_updates true
+
+  app "PhpWebStudy.app"
+
+  zap trash: [
+    "~/Library/Application Support/PhpWebStudy",
+    "~/Library/Logs/PhpWebStudy",
+    "~/Library/PhpWebStudy",
+  ]
+end
