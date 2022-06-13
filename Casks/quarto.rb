19c5d5e1e0896a45cc2041183136fc0368c76508
Porkepix <porkepix@gmail.com>
2022-06-13 02:21:31 +0200

    Update quarto from 0.9.573 to 0.9.577

--- Casks/quarto.rb
+++ Casks/quarto.rb
@@ -1,6 +1,6 @@
 cask "quarto" do
-  version "0.9.573"
-  sha256 "83486b9758704c628d83dbacca9f9c4ef6609b66c075f399d7b94b18e238ed9d"
+  version "0.9.577"
+  sha256 "9e9b869b2095d7107bc6e1625e551bdbefde93d45df794522e5c84a820f42ac3"
 
   url "https://github.com/quarto-dev/quarto-cli/releases/download/v#{version}/quarto-#{version}-macos.pkg",
       verified: "github.com/quarto-dev/quarto-cli/"
