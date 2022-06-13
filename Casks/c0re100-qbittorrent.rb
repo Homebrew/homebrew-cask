3915292f8c8c178989389e18c04041d5a14a39ed
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-12 23:29:28 -0700

    Bump c0re100-qbittorrent

--- Casks/c0re100-qbittorrent.rb
+++ Casks/c0re100-qbittorrent.rb
@@ -1,8 +1,8 @@
 cask "c0re100-qbittorrent" do
-  version "4.4.3.11"
-  sha256 "1b3038dc497c320dfd4eee2ee8fa280d49c1c2cea10b2e00314c237ba61c6271"
+  version "4.4.3.12"
+  sha256 "b99f9700f83622655d40d1588f4aa02f908ff26850bea49c142dcad7c3ef91d4"
 
-  url "https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-#{version}/qBittorrent-Enhanced-#{version}-macOS-universal.dmg"
+  url "https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-#{version}/qBittorrent-Enhanced-Edition-release-#{version}-macOS-universal.dmg"
   name "qBittorrent Enhanced Edition"
   desc "Bittorrent client"
   homepage "https://github.com/c0re100/qBittorrent-Enhanced-Edition"
