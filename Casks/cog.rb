9000bd6cabde5af3d2fb64e3fc1aae43e5b84986
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-13 14:24:37 -0700

    cog 2266,e5ff6cd2
    Update cog from 2261 to 2266
    
    Closes #125567.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/cog.rb
+++ Casks/cog.rb
@@ -1,6 +1,6 @@
 cask "cog" do
-  version "2261,b675ced7"
-  sha256 "fb17d4540d92a93518bc617e239267cc1fc597eba979e054162b942206388c52"
+  version "2266,e5ff6cd2"
+  sha256 "faaa9ac7aa69323e9a35a60effd200a5fa2085c00ba8026f93f0f642f5192e2c"
 
   url "https://cogcdn.cog.losno.co/Cog-#{version.csv.second}.zip"
   name "Cog"
