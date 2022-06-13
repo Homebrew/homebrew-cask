04fd491927dc18784a82b0e9c480814009a8b155
Miccal Matthews <miccal.matthews@gmail.com>
2022-06-14 04:54:43 +0800

    Update phpwebstudy.rb

--- Casks/phpwebstudy.rb
+++ Casks/phpwebstudy.rb
@@ -2,7 +2,8 @@ cask "phpwebstudy" do
   version "1.0.14"
   sha256 "e7d4ef8a485e69513ac0f860be013dd3405c0bcb2d474aa3bdeba4caf4257f05"
 
-  url "https://github.com/xpf0000/PhpWebStudy/releases/download/v#{version}/PhpWebStudy-#{version}-mac.zip", verified: "github.com/xpf0000/PhpWebStudy/"
+  url "https://github.com/xpf0000/PhpWebStudy/releases/download/v#{version}/PhpWebStudy-#{version}-mac.zip",
+      verified: "github.com/xpf0000/PhpWebStudy/"
   name "PhpWebStudy"
   desc "PHP and Web develop environment manager"
   homepage "https://www.macphpstudy.com/"
