df5230c72bb05f22b512bcf4375458cd5cdeeafc
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-13 13:01:45 -0700

    nano 23.3
    Update nano from 23.1 to 23.3
    
    Closes #125545.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/nano.rb
+++ Casks/nano.rb
@@ -1,6 +1,6 @@
 cask "nano" do
-  version "23.1"
-  sha256 "922aa8020a4225cd3c1a961badcb5115d9e06ce06dea9b98e1a1d5b243428793"
+  version "23.3"
+  sha256 "20057926a464c25090d69c9ce783d8211d91ed94fa0e3772340f8fb7ba1604f7"
 
   url "https://github.com/nanocurrency/nano-node/releases/download/V#{version}/nano-node-V#{version}-Darwin.dmg",
       verified: "github.com/nanocurrency/nano-node/"
