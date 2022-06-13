091aef483c3707413262e7b79ec201cd293393b0
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-13 13:08:59 -0700

    tuxera-ntfs 2021.1
    Update tuxera-ntfs from 2021 to 2021.1
    
    Closes #125561.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/tuxera-ntfs.rb
+++ Casks/tuxera-ntfs.rb
@@ -1,5 +1,5 @@
 cask "tuxera-ntfs" do
-  version "2021"
+  version "2021.1"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://tuxera.com/mac/tuxerantfs_#{version}.dmg"
