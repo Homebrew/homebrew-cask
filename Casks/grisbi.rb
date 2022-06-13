e392a17af73e8bd070fc37ffe1e8930e92d49d71
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-13 11:06:02 -0700

    Fix URL

--- Casks/grisbi.rb
+++ Casks/grisbi.rb
@@ -2,7 +2,7 @@ cask "grisbi" do
   version "2.0.5"
   sha256 "75620ff25fd60b9fe8fe3422aed494e23d83a0a6f75d483f6f01f04d183ab21b"
 
-  url "https://downloads.sourceforge.net/grisbi/#{version.csv.second}/#{version}/Grisbi-#{version}.dmg",
+  url "https://downloads.sourceforge.net/grisbi/#{version.major_minor}.x/#{version}/Grisbi-#{version}.dmg",
       verified: "downloads.sourceforge.net/grisbi"
   name "Grisbi"
   desc "Personal financial management program"
