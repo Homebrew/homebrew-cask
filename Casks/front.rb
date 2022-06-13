48257d85747b188dd5cb3c5873f46521d7294cbd
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-12 22:26:32 -0700

    front 3.34.13
    * Split arch, add min OS, update URL
    * Fix url verification
    
    Closes #125518.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/front.rb
+++ Casks/front.rb
@@ -1,16 +1,26 @@
 cask "front" do
-  version "3.29.5"
-  sha256 "3c59771b33893e82f54461db11c758ae709de54454b9b8d3251d603f8adb8f44"
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  url "https://dl.frontapp.com/desktop/builds/#{version}/Front-#{version}.zip"
+  version "3.34.13"
+
+  if Hardware::CPU.intel?
+    sha256 "6ee6b7aea0abd0e94222a6f847314a2217bebc23d0963f286fe354495ab70535"
+  else
+    sha256 "7702ce89d5093d56f0781c4f02dd61a0fb49af0455a4958bda0f470ad361f1a5"
+  end
+
+  url "https://dl.frontapp.com/desktop/builds/#{version}/Front-#{version}-#{arch}.zip",
+      verified: "dl.frontapp.com/desktop/builds"
   name "Front"
   desc "Customer communication platform"
-  homepage "https://frontapp.com/"
+  homepage "https://front.com/"
 
   livecheck do
     url "https://dl.frontapp.com/desktop/updates/latest/mac/latest-mac.yml"
     strategy :electron_builder
   end
+
+  depends_on macos: ">= :el_capitan"
 
   app "Front.app"
 
