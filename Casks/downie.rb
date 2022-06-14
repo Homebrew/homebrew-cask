e4cc44627dd1649f6fd8858265ff5bee3d92a8c2
Reno <singularitti@outlook.com>
2022-06-14 11:53:34 -0400

    downie 4.5.5,4415
    Update downie from 4.5.4 to 4.5.5
    
    Closes #125609.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,6 +1,6 @@
 cask "downie" do
-  version "4.5.4,4409"
-  sha256 "8eeab4ef00f1b53f0646434e3806de6665b73da68fdb3c9e69f4da79adf3fc26"
+  version "4.5.5,4415"
+  sha256 "5e749b1972c104e23854bee217042abbe9ffa2945aaed2e4d86cd8cbf45d3661"
 
   url "https://software.charliemonroe.net/trial/downie/v#{version.major}/Downie_#{version.major}_#{version.csv.second}.dmg"
   name "Downie"
