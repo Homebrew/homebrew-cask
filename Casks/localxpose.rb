8071a2e63b06c9b98be7d92a70f89934c11c8a85
Miccal Matthews <miccal.matthews@gmail.com>
2022-06-15 06:22:09 +0800

    Update localxpose.rb

--- Casks/localxpose.rb
+++ Casks/localxpose.rb
@@ -1,10 +1,11 @@
 cask "localxpose" do
   arch = Hardware::CPU.intel? ? "amd64" : "arm64"
+
   version "22.6.3"
   sha256 :no_check
 
   url "https://api.localxpose.io/api/v2/downloads/loclx-darwin-#{arch}.zip"
-  name "localxpose"
+  name "LocalXpose"
   desc "Reverse proxy that enables you to expose your localhost to the internet"
   homepage "https://localxpose.io/"
 
