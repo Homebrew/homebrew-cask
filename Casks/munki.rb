102c151b613f4670b005605b06f2352cb8aa98bc
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-14 10:43:52 -0700

    Update munki from 5.7.1.4434 to 5.7.2.4439

--- Casks/munki.rb
+++ Casks/munki.rb
@@ -1,6 +1,6 @@
 cask "munki" do
-  version "5.7.1.4434"
-  sha256 "3b00461db482934bdc6849dc9f8db03e5359dc32f99fe88cacd3f234d4f88ba1"
+  version "5.7.2.4439"
+  sha256 "515cf72d2ae00249fd1d604ec413832dc55f6b001591453b3ccfc2821ccf2a5b"
 
   url "https://github.com/munki/munki/releases/download/v#{version.major_minor_patch}/munkitools-#{version}.pkg",
       verified: "github.com/munki/munki/"
