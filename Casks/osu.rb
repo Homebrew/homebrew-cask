944e9724d8b1f3eaad581444674c3e6a87236be5
Jackson Chen <jackson@jacksonchen666.com>
2022-06-15 15:48:47 +0200

    Update osu from 2022.612.0 to 2022.615.0

--- Casks/osu.rb
+++ Casks/osu.rb
@@ -1,12 +1,12 @@
 cask "osu" do
   arch = Hardware::CPU.intel? ? "Intel" : "Apple.Silicon"
 
-  version "2022.612.0"
+  version "2022.615.0"
 
   if Hardware::CPU.intel?
-    sha256 "b17cee05b9438988db24df316950da4672ec20d074f6588e933864b13bf9da24"
+    sha256 "bd2250c36c255c1d51e42183408362dd562fddfa3838a1ac2413fe01fd4e722a"
   else
-    sha256 "51eed6ca412528d0e554f4ccec9cf8daddf63aa08414fe0a39b2dd70395cdf1b"
+    sha256 "a397fff60469c5ac6e0e458f7167ba73cdb4482ee4fde37cc35115e2060c99a5"
   end
 
   url "https://github.com/ppy/osu/releases/download/#{version}/osu.app.#{arch}.zip"
