d14ecf33b6f0fab8594f5e4f8207bfaffe2eced2
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-14 21:15:40 -0700

    Update quarto from 0.9.583 to 0.9.584

--- Casks/quarto.rb
+++ Casks/quarto.rb
@@ -1,6 +1,6 @@
 cask "quarto" do
-  version "0.9.583"
-  sha256 "8d7661fb5eb1138debb54ac9a645e0b720ddac4a100badbce532b4503145d8e7"
+  version "0.9.584"
+  sha256 "a731a623d7596020db57c5e26070ab97c4b7bb50a27a330ac37cbd6666e613e2"
 
   url "https://github.com/quarto-dev/quarto-cli/releases/download/v#{version}/quarto-#{version}-macos.pkg",
       verified: "github.com/quarto-dev/quarto-cli/"
