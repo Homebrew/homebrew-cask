5b957827e02b4583377ddba31a5dc02107217746
Lonre Wang <lonrevip@gmail.com>
2022-06-15 23:37:01 +0800

    Update insomnia from 2022.4.0 to 2022.4.1

--- Casks/insomnia.rb
+++ Casks/insomnia.rb
@@ -1,6 +1,6 @@
 cask "insomnia" do
-  version "2022.4.0"
-  sha256 "58b55161f56e1c24a4ea69e626d653a956b075888742a85a93e6cd487000713c"
+  version "2022.4.1"
+  sha256 "8c3fb0d06f0aa0f826936b8a94d9c27eca7a9888b7cb6066cedb30cd5852e88f"
 
   url "https://github.com/Kong/insomnia/releases/download/core%40#{version}/Insomnia.Core-#{version}.dmg",
       verified: "github.com/Kong/insomnia/"
