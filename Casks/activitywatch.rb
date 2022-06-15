c234e1a26490bcb715cb7888c514ac59b9f0219e
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-14 20:31:54 -0700

    Fix livecheck & zap stanza

--- Casks/activitywatch.rb
+++ Casks/activitywatch.rb
@@ -3,12 +3,21 @@ cask "activitywatch" do
   sha256 "91deabd5844968467b5b6b3423e12654d4e339ba309f333c25d991a2e689fd96"
 
   url "https://github.com/ActivityWatch/activitywatch/releases/download/v#{version}/activitywatch-v#{version}-macos-x86_64.dmg",
-      verified: "github.com/ActivityWatch/activitywatch/"
+      verified: "github.com/ActivityWatch/activitywatch/releases/download/"
   name "ActivityWatch"
   desc "Time tracker"
   homepage "https://activitywatch.net/"
 
+  livecheck do
+    url "https://activitywatch.net/downloads/"
+    regex(%r{href=.*?/activitywatch-v([0-9]+(\.[0-9]+)+)-macos-x86_64\.dmg}i)
+  end
+
   app "ActivityWatch.app"
 
-  zap trash: "~/Library/Application Support/activitywatch"
+  zap trash: [
+    "~/Library/Application Support/activitywatch",
+    "~/Library/Caches/activitywatch",
+    "~/Library/Logs/activitywatch",
+  ]
 end
