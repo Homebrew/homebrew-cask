99ca4e3fce727269d8f95cd80353399b46d1d65b
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-15 09:18:37 -0700

    Update url

--- Casks/eclipse-installer.rb
+++ Casks/eclipse-installer.rb
@@ -4,12 +4,12 @@ cask "eclipse-installer" do
   version "4.24.0,2022-06"
 
   if Hardware::CPU.intel?
-    sha256 "125e008c6da26301c12c9c6ddaa309090333e9371b152deea507c0cefeeb035e"
+    sha256 "66f363702ebc292f957767853f24429158721d446f1dffbcdb2cec518456e4d0"
   else
-    sha256 "b093b562ba17398075f5cdcca617494436a19819d0d3c4c445adc0890c2ca3e6"
+    sha256 "c83cf7237889b5df8c5d11d74fcac03e89eaaa110f476eaa6353c5abe374e5c9"
   end
 
-  url "https://eclipse.org/downloads/download.php?file=/oomph/epp/#{version.csv.second}/R/eclipse-inst-#{arch}.tar.gz&r=1"
+  url "https://eclipse.org/downloads/download.php?file=/oomph/epp/#{version.csv.second}/R/eclipse-inst-jre-mac-#{arch}.dmg&r=1"
   name "Eclipse Installer"
   name "Eclipse IDE installer"
   desc "Install and update your Eclipse Development Environment"
