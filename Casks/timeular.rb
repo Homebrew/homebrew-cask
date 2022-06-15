b66d5e060b4d37b31aff32c6909675faac5d30f7
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-15 09:58:36 -0700

    timeular 4.11.0
    Update timeular from 4.10.0 to 4.11.0
    
    Closes #125757.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/timeular.rb
+++ Casks/timeular.rb
@@ -1,6 +1,6 @@
 cask "timeular" do
-  version "4.10.0"
-  sha256 "44e631652f0def855ec9a891f606b6073ca354251ae2b2004addd80cb289f274"
+  version "4.11.0"
+  sha256 "d498085fdf91eba95bc7e454bec18328b60ed5a974d1729ff90b910b90f72f10"
 
   url "https://timeular-desktop-packages.s3.amazonaws.com/mac/production/Timeular-#{version}.dmg",
       verified: "timeular-desktop-packages.s3.amazonaws.com/"
