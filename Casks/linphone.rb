92e55b826cbb1ac330df45b0ff1237744678cd74
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-15 09:31:51 -0700

    Update linphone from 4.4.5 to 4.4.6

--- Casks/linphone.rb
+++ Casks/linphone.rb
@@ -1,6 +1,6 @@
 cask "linphone" do
-  version "4.4.5"
-  sha256 "e7669b4d633d0a4a1ea6b3fe272ea7672a9b32c1cb9349827e4a43290b733a28"
+  version "4.4.6"
+  sha256 "924ff57893962f4cfa16352279dd167aa74471d20a7bfd03a624de0a00c71776"
 
   url "https://download.linphone.org/releases/macosx/app/Linphone-#{version}-mac.dmg"
   name "Linphone"
