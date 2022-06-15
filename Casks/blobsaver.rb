363a1fe3424805b271ec57a1bab19ed57d980736
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-14 18:36:27 -0700

    blobsaver 3.2.1
    Update blobsaver from 3.2.0 to 3.2.1
    
    Closes #125691.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/blobsaver.rb
+++ Casks/blobsaver.rb
@@ -1,12 +1,12 @@
 cask "blobsaver" do
   arch = Hardware::CPU.intel? ? "intel" : "arm"
 
-  version "3.2.0"
+  version "3.2.1"
 
   if Hardware::CPU.intel?
-    sha256 "f60846080a452e4c0bebf263770b5d371e5bab3487b80a7df220217bba042582"
+    sha256 "341dad87e5a36f7f6c1f14050a4f44128afa2da5651abea761af59054d5029c8"
   else
-    sha256 "de78183eb7451c3f82c0ed2c165a3b2c9f6623dd42272e303293f541520c2f5d"
+    sha256 "0e510dd7315af465a9f1f333e7f303e16871fa9964ae9c1632145d38b6e32e04"
   end
 
   url "https://github.com/airsquared/blobsaver/releases/download/v#{version}/blobsaver-#{arch}-#{version}.dmg"
