02fa5b3443289804c55beec906318bc155a2c655
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-15 09:23:52 -0700

    Fix Intel

--- Casks/eclipse-installer.rb
+++ Casks/eclipse-installer.rb
@@ -4,7 +4,7 @@ cask "eclipse-installer" do
   version "4.24.0,2022-06"
 
   if Hardware::CPU.intel?
-    sha256 "66f363702ebc292f957767853f24429158721d446f1dffbcdb2cec518456e4d0"
+    sha256 "cca180dfd69704557c848df87198652ea848cc028a69973bc929b51ea6d95efd"
   else
     sha256 "c83cf7237889b5df8c5d11d74fcac03e89eaaa110f476eaa6353c5abe374e5c9"
   end
