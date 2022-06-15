6e885cb879f6aaecb65b0fce7ca7aa72818e6181
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-14 21:03:59 -0700

    Fix verified URL

--- Casks/activitywatch.rb
+++ Casks/activitywatch.rb
@@ -3,7 +3,7 @@ cask "activitywatch" do
   sha256 "91deabd5844968467b5b6b3423e12654d4e339ba309f333c25d991a2e689fd96"
 
   url "https://github.com/ActivityWatch/activitywatch/releases/download/v#{version}/activitywatch-v#{version}-macos-x86_64.dmg",
-      verified: "github.com/ActivityWatch/activitywatch/releases/download/"
+      verified: "github.com/ActivityWatch/activitywatch/"
   name "ActivityWatch"
   desc "Time tracker"
   homepage "https://activitywatch.net/"
