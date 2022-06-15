528a62e0ba104cf351f0d0cc6a204dffffbacbd8
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-15 09:31:43 -0700

    Update lark from 5.11.9 to 5.11.11

--- Casks/lark.rb
+++ Casks/lark.rb
@@ -5,8 +5,8 @@ cask "lark" do
     version "5.11.9,53384f"
     sha256 "d01c98cd81ee06d8a902a474ee9e85c0df75080a8a63b7a83b1f5c50ad014d64"
   else
-    version "5.11.9,ce2d63"
-    sha256 "f9ab4b660c47d5287f80f234670deca55fa8229be70fd46db641b6aa84d11367"
+    version "5.11.11,b7417a"
+    sha256 "cbfb082b329cd5947dbde17a4b27ea40e348bd32dd487f70fac936d48d3a17a1"
   end
 
   url "https://sf16-va.larksuitecdn.com/obj/lark-artifact-storage/#{version.csv.second}/Lark-darwin_#{arch}-#{version.csv.first}-signed.dmg",
