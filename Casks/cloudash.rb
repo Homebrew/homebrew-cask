0e46c31fb3d6463550faa2ade3df1f7f4af83525
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-15 17:37:22 -0700

    Update cloudash from 1.16.0 to 1.16.1 (#125716)

--- Casks/cloudash.rb
+++ Casks/cloudash.rb
@@ -1,6 +1,6 @@
 cask "cloudash" do
-  version "1.16.0"
-  sha256 "37afafbfcf7b431c7db9d4f67116c2b4ff4cfcf74a009ae93cef361796d5047a"
+  version "1.16.1"
+  sha256 "099c8ff8cc2a676bc11f1ab39829cd1c25b8aec405fa2c8a3c4f3ad0a0ac84f5"
 
   url "https://github.com/cloudashdev/cloudash/releases/download/#{version}/Cloudash-#{version}.dmg",
       verified: "github.com/cloudashdev/cloudash/"
