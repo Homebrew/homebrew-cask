056d1f83d41298945b34d32ce928539b73d7bdfb
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-16 16:44:17 -0700

    Fix leading 0

--- Casks/cloudflare-warp.rb
+++ Casks/cloudflare-warp.rb
@@ -1,5 +1,5 @@
 cask "cloudflare-warp" do
-  version "2022.05.342.0,20220616.9"
+  version "2022.5.342.0,20220616.9"
   sha256 :no_check
 
   url "https://1111-releases.cloudflareclient.com/mac/Cloudflare_WARP.zip",
