24951abad895f25f7be793f7a87546e8043e33a7
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-15 17:37:03 -0700

    Update busycal from 2022.2.3,2022-06-15-00-38 to 2022.2.3,2022-06-15-11-28 (#125715)

--- Casks/busycal.rb
+++ Casks/busycal.rb
@@ -1,6 +1,6 @@
 cask "busycal" do
-  version "2022.2.3,2022-06-15-00-38"
-  sha256 "8a81a234f8be1c1cdd48dbe8ecd9766de37f74b4c39404532fc41738e147b094"
+  version "2022.2.3,2022-06-15-11-28"
+  sha256 "7237c1a2ec7b01359a5786fcac545ae11db90c28c1ae0fd9eae3ce8efff356bc"
 
   url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bcl-#{version.csv.first}-#{version.csv.second}.zip",
       verified: "7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/"
