4064e44e4b1d98f58f8ac25a07ca847ba40aca87
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-16 16:15:44 -0700

    cloudflare-warp 2022.5.342.0,20220616.9
    * Update cloudflare-warp from 2022.5.310.0 to 2022.05.342.0
    * Fix leading 0
    
    Closes #125881.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/cloudflare-warp.rb
+++ Casks/cloudflare-warp.rb
@@ -1,5 +1,5 @@
 cask "cloudflare-warp" do
-  version "2022.5.310.0,20220606.16"
+  version "2022.5.342.0,20220616.9"
   sha256 :no_check
 
   url "https://1111-releases.cloudflareclient.com/mac/Cloudflare_WARP.zip",
