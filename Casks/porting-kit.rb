1f92c8de4fd8c24254bef12e3e3654ce44b795c8
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-15 23:23:11 -0700

    Update porting-kit from 5.3.1 to 5.4.0

--- Casks/porting-kit.rb
+++ Casks/porting-kit.rb
@@ -1,6 +1,6 @@
 cask "porting-kit" do
-  version "5.3.1"
-  sha256 "11d2cac61b7ad3151219571f8856d5239fb2ab0441a719f9f280b992f1854561"
+  version "5.4.0"
+  sha256 "0ec8e0ec515204a6272cf6e4462abc705ac284bf91eb26ba1be5121f34d61b77"
 
   url "https://github.com/vitor251093/porting-kit-releases/releases/download/v#{version}/Porting-Kit-#{version}.dmg",
       verified: "github.com/vitor251093/porting-kit-releases/"
