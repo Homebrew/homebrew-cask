afc53f126274e0fad6630a7e643c94d7f3927e89
Bo Anderson <mail@boanderson.me>
2022-06-16 03:20:36 +0100

    powerphotos: remove Yosemite support

--- Casks/powerphotos.rb
+++ Casks/powerphotos.rb
@@ -1,9 +1,5 @@
 cask "powerphotos" do
-  if MacOS.version <= :yosemite
-    version "1.0.6"
-    sha256 "927c1095858d259b9469c86d20ce39cf0bfc350ad0b64ae8ba0ca0557b632305"
-    url "https://www.fatcatsoftware.com/powerphotos/PowerPhotos_#{version.no_dots}.zip"
-  elsif MacOS.version <= :el_capitan
+  if MacOS.version <= :el_capitan
     version "1.2.3"
     sha256 "b07eb9f8801fb397d55e3dd7e0569dbef5d3265debaf3ee68247062901d93fcb"
     url "https://www.fatcatsoftware.com/powerphotos/PowerPhotos_#{version.no_dots}.zip"
