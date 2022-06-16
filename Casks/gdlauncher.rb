0316c247a4ec98df469514ec93a9ef1fc68ed0b8
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-15 22:34:31 -0700

    Update gdlauncher from 1.1.23 to 1.1.24

--- Casks/gdlauncher.rb
+++ Casks/gdlauncher.rb
@@ -1,6 +1,6 @@
 cask "gdlauncher" do
-  version "1.1.23"
-  sha256 "4910194889b5a192ab422a36c4d6e7c3eede19d8859eccf72e4bdc4e3b794936"
+  version "1.1.24"
+  sha256 "9fe08dd65563ec9acdf6927ce65869a922ee6733d891b07462c662e4b573b6b6"
 
   url "https://github.com/gorilla-devs/GDLauncher/releases/download/v#{version}/GDLauncher-mac-setup.dmg",
       verified: "github.com/gorilla-devs/GDLauncher/"
