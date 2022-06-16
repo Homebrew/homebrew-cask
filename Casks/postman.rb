8a009cb59aee769e1aa87f01e8ab7f4b35922eca
Bevan Kay <email@bevankay.me>
2022-06-16 10:35:31 +1000

    Downgrade postman from 9.20.3 to 9.19.0 (#125712)
    Update postman from 9.19.0 to 9.19.0

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,18 +1,25 @@
 cask "postman" do
-  arch = Hardware::CPU.intel? ? "osx_64" : "osx_arm64"
+  arch = Hardware::CPU.intel? ? "osx64" : "osx_arm64"
 
-  version "9.20.3"
-  sha256 :no_check
+  version "9.19.0"
 
-  url "https://dl.pstmn.io/download/latest/#{arch}",
-      verified: "dl.pstmn.io/download/latest/"
+  if Hardware::CPU.intel?
+    sha256 "629bf940f0466a6684144e6cd7fc2352e7d005491a8d0d70d4968de1e32f1856"
+  else
+    sha256 "3d0f3e1cfee53135d08a2ba5624f187a6b2b7d9746e9d39700622947fae0fd07"
+  end
+
+  url "https://dl.pstmn.io/download/version/#{version}/#{arch}",
+      verified: "dl.pstmn.io/download/version/"
   name "Postman"
   desc "Collaboration platform for API development"
   homepage "https://www.postman.com/"
 
   livecheck do
-    url :url
-    strategy :header_match
+    url "https://dl.pstmn.io/api/version/latest"
+    strategy :page_match do |page|
+      JSON.parse(page)["version"]
+    end
   end
 
   auto_updates true
