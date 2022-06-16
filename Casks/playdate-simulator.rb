617d27b8708cfba2d993b94accff368cab53d844
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-15 23:22:46 -0700

    Update playdate-simulator from 1.11.1 to 1.12.0

--- Casks/playdate-simulator.rb
+++ Casks/playdate-simulator.rb
@@ -1,6 +1,6 @@
 cask "playdate-simulator" do
-  version "1.11.1"
-  sha256 "5c05b96020fd8922d1941f631220170e07456b6b63c1b82a576ae406b64647f9"
+  version "1.12.0"
+  sha256 "04a2ab267ade03a6acc77ff18afd5198002a681ddad648e823c0439c89a63131"
 
   url "https://download-keycdn.panic.com/playdate_sdk/PlaydateSDK-#{version}.zip",
       verified: "download-keycdn.panic.com/playdate_sdk/"
