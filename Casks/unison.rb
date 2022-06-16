38619e13e0881a4020161d2b41303b7a5d2fa511
Bo Anderson <mail@boanderson.me>
2022-06-16 03:20:37 +0100

    unison: remove Yosemite support

--- Casks/unison.rb
+++ Casks/unison.rb
@@ -1,18 +1,9 @@
 cask "unison" do
-  if MacOS.version <= :yosemite
-    version "2.48.3"
-    sha256 "d578196d8b38f35c1e0410a1c86ff4e115a91f7eb211201db7a940a3a3e0f099"
+  version "2.52.1,4.14.0"
+  sha256 "d2c87075e460d74641ae8a955703de5cf5344d9affdf4e2f6a8fc5d971c4246c"
 
-    url "https://github.com/bcpierce00/unison/releases/download/#{version}/Unison-OS-X-#{version}.zip",
-        verified: "github.com/bcpierce00/unison/"
-  else
-    version "2.52.1,4.14.0"
-    sha256 "d2c87075e460d74641ae8a955703de5cf5344d9affdf4e2f6a8fc5d971c4246c"
-
-    url "https://github.com/bcpierce00/unison/releases/download/v#{version.csv.first}/Unison-v#{version.csv.first}.ocaml-#{version.csv.second}.macos-10.15.app.tar.gz",
-        verified: "github.com/bcpierce00/unison/"
-  end
-
+  url "https://github.com/bcpierce00/unison/releases/download/v#{version.csv.first}/Unison-v#{version.csv.first}.ocaml-#{version.csv.second}.macos-10.15.app.tar.gz",
+      verified: "github.com/bcpierce00/unison/"
   name "Unison"
   desc "File synchronizer"
   homepage "https://www.cis.upenn.edu/~bcpierce/unison/"
