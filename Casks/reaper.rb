ea4cf2e16e249e949090ceaf0073651388ab47b0
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-17 12:53:24 -0700

    reaper 6.61
    * Update reaper from 6.60 to 6.61
    * Update reaper.rb
    
    Closes #125955.
    
    Co-authored-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,11 +1,11 @@
 cask "reaper" do
-  version "6.60"
+  version "6.61"
 
   if MacOS.version <= :mojave
-    sha256 "800738b63831b720266688ec7eeae7349664cd38c88d48e13a3b7aa6b5a04d0f"
+    sha256 "fe4945b88d3a334feba809e2cb1863b8b9eb57e9afd67b1b1c4feb53baaa79c0"
     url "https://dlcf.reaper.fm/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64.dmg"
   else
-    sha256 "938cc8919f50c7e221a9828a8600736c688d104bbdafc68c25f5da8e3cf30a6f"
+    sha256 "bd41f738b7e93fba0c29e654b090d7ba82ed3c1f9fea08c48842f4f22f9d3917"
     url "https://dlcf.reaper.fm/#{version.major}.x/reaper#{version.major_minor.no_dots}_universal.dmg"
   end
 
