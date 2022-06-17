0f1a44dac03f883705444d45d0061d86ff2b6806
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-17 09:20:26 -0700

    clash-for-windows 0.19.21
    Update clash-for-windows from 0.19.20 to 0.19.21
    
    Closes #125927.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/clash-for-windows.rb
+++ Casks/clash-for-windows.rb
@@ -1,12 +1,12 @@
 cask "clash-for-windows" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  version "0.19.20"
+  version "0.19.21"
 
   if Hardware::CPU.intel?
-    sha256 "7aa0965780f53f1492ebd2eb20eb9a54c773ce2a5fa510873b36d6595058cc17"
+    sha256 "5a0fc0b2f0608ed76429a24d35e192f9c076466dab401c3c389634fc126e465b"
   else
-    sha256 "a0fb8200b654e4d27ba344694a606ce7f66076210697a98318b23bec10551bde"
+    sha256 "3392deb81a1631c0073bf59c55d04deb7b1e8f30dc7bc021db97946bc8f89b18"
   end
 
   url "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/#{version}/Clash.for.Windows-#{version}#{arch}.dmg"
