b3ec0bf41981bcc4eacda2b1155a7acce1173d1f
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-16 17:06:37 -0700

    Update lingon-x from 8.4.5 to 8.4.6

--- Casks/lingon-x.rb
+++ Casks/lingon-x.rb
@@ -3,8 +3,8 @@ cask "lingon-x" do
     version "6.6.5"
     sha256 "b0231b1a98dcc8f5c4234b419c9f5331407b8cce29b33f0ea2e32b12595adfa8"
   else
-    version "8.4.5"
-    sha256 "a7202068b8146a86511f17e9c5384b30523eaa1c0886d0794fe574d8e7ae7f32"
+    version "8.4.6"
+    sha256 "968894272a09fd455430db6e042e541324546d025e34b55b8c50b73702abcde6"
   end
 
   url "https://www.peterborgapps.com/downloads/LingonX#{version.major}.zip"
