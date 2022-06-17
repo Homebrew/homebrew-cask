78fa2968928cc4e869d495a421c96debca537b54
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-17 10:36:02 -0700

    wing-personal 8.3.2.0
    Update wing-personal from 8.3.1.0 to 8.3.2.0
    
    Closes #125946.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/wing-personal.rb
+++ Casks/wing-personal.rb
@@ -1,12 +1,12 @@
 cask "wing-personal" do
   arch = Hardware::CPU.intel? ? "intel" : "arm64"
 
-  version "8.3.1.0"
+  version "8.3.2.0"
 
   if Hardware::CPU.intel?
-    sha256 "afc480575ffb6b683e7434b1c8a3202a113e55ad76c8073caf4182ec6292df7b"
+    sha256 "9eae9bb82491cb1bce82ae407a39ac90f5ddfa1ff20bbcb33428091a1f462258"
   else
-    sha256 "72c73019cecbf8f2a2e470997b6e9612f9041eca387cdca76d4b7266af34e49b"
+    sha256 "437193ca239d26085c7025a2c9b60acb5147c448bd7138485a3e50e5a510db2e"
   end
 
   url "https://wingware.com/pub/wing-personal/#{version}/wing-personal-#{version}-#{arch}.dmg"
