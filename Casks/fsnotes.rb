8c9f4513bf7201162979a51ee8ac820de9cae59c
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-17 12:37:42 -0700

    Update fsnotes from 5.4.4 to 5.4.5

--- Casks/fsnotes.rb
+++ Casks/fsnotes.rb
@@ -1,6 +1,6 @@
 cask "fsnotes" do
-  version "5.4.4"
-  sha256 "b4b1f03d186ba9e8685f6fdfc7f3d8cc8c8701fc9971c4098fefe336a4082b28"
+  version "5.4.5"
+  sha256 "4a13e711373d100d51337f29b445ab0d214f6601684779e4f9cb790e703c5ab2"
 
   url "https://github.com/glushchenko/fsnotes/releases/download/#{version}/FSNotes_#{version}.zip",
       verified: "github.com/glushchenko/fsnotes/"
