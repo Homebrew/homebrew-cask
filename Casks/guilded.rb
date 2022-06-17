5454742250562dfedac62d72d40468fc32c4cdd5
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-17 15:46:33 -0700

    guilded 1.0.9141143
    Update guilded from 1.0.9136306 to 1.0.9141143
    
    Closes #125959.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/guilded.rb
+++ Casks/guilded.rb
@@ -1,6 +1,6 @@
 cask "guilded" do
-  version "1.0.9136306"
-  sha256 "dbe7db204f5405b5f3736831dcc6ecb82b9af5a09ab6f7dd01afb864c491f2c4"
+  version "1.0.9141143"
+  sha256 "254227b339d5ea93b7563c744a9f4bb3ec975b766fabfa7820756a8a37b7fc72"
 
   url "https://s3-us-west-2.amazonaws.com/www.guilded.gg/AppBuilds/mac/Guilded-#{version}-release.dmg",
       verified: "s3-us-west-2.amazonaws.com/www.guilded.gg/AppBuilds/mac/"
