194393439fa9b5e7faad8a1f389c4cb9cb3f4327
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-16 18:33:10 -0700

    Update dropbox from 150.4.5000 to 151.3.4214

--- Casks/dropbox.rb
+++ Casks/dropbox.rb
@@ -1,7 +1,7 @@
 cask "dropbox" do
   arch = Hardware::CPU.intel? ? "" : "&arch=arm64"
 
-  version "150.4.5000"
+  version "151.3.4214"
   sha256 :no_check
 
   url "https://www.dropbox.com/download?plat=mac&full=1#{arch}"
