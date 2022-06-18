afdb3c3a3f9105f3d7884cb81dde2c6478e74eba
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-17 21:16:43 -0700

    quicken 6.8.0,608.44820.100
    Update quicken from 6.7.1 to 6.8.0
    
    Closes #125980.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/quicken.rb
+++ Casks/quicken.rb
@@ -1,6 +1,6 @@
 cask "quicken" do
-  version "6.7.1,607.44146.100"
-  sha256 "d7f2e316e4af15e1d04b86d59aa9f9193589ef95ed8eb281b3b758e43d94ca74"
+  version "6.8.0,608.44820.100"
+  sha256 "6f6e11349ec7b0379bbab145d998ffecc299471aafd344587179837f85351290"
 
   url "https://download.quicken.com/mac/Quicken/001/Release/031A96D9-EFE6-4520-8B6A-7F465DDAA3E4/Quicken-#{version.csv.second}/Quicken-#{version.csv.second}.zip"
   name "Quicken"
