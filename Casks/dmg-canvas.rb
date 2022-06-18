29e084067a7571022432e431b7ff4d5aee7918e6
JounQin <admin@1stg.me>
2022-06-18 23:00:44 +0800

    DMG Canvas 3.0

---
+++ Casks/dmg-canvas.rb
@@ -0,0 +1,11 @@
+cask "dmg-canvas" do
+  version "3.0"
+  sha256 :no_check
+
+  url "https://www.araelium.com/dmgcanvas/downloads/DMGCanvas.dmg"
+  name "DMG Canvas"
+  desc "Stylized disk images made easy"
+  homepage "https://www.araelium.com/dmgcanvas"
+
+  app "DMG Canvas.app"
+end
