99aba1a366f479a42334a21ce614c396174c9901
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-06-18 10:54:32 -0400

    Update quaternion from 0.0.95.1 to 0.0.95.1-flatpak

--- Casks/quaternion.rb
+++ Casks/quaternion.rb
@@ -1,6 +1,6 @@
 cask "quaternion" do
-  version "0.0.95.1"
-  sha256 "d2f80cfd17ab25b4062032afa76aa7bf509d32fb72c91858ab04347e216df9a7"
+  version "0.0.95.1-flatpak"
+  sha256 "2d657e54bc7549015274df073881933327b3a17ddf816af951e8ee05347918d9"
 
   url "https://github.com/quotient-im/Quaternion/releases/download/#{version}/quaternion-#{version}.dmg"
   name "Quaternion"
