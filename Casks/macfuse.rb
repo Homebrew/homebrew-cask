6be5ddc106fb7c8899e548fab235d99454a3ac82
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-17 21:00:41 -0700

    macfuse 4.4.0
    Update macfuse from 4.2.5 to 4.4.0
    
    Closes #125978.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/macfuse.rb
+++ Casks/macfuse.rb
@@ -1,6 +1,6 @@
 cask "macfuse" do
-  version "4.2.5"
-  sha256 "a94af08049fe480453e02acb57334f7c8dcc47d68f3f3fe341bf5aadda418dff"
+  version "4.4.0"
+  sha256 "db347d5a57d32d8556e82386eea9ef155fd877890eb3db547e49bb137d976a43"
 
   url "https://github.com/osxfuse/osxfuse/releases/download/macfuse-#{version}/macfuse-#{version}.dmg",
       verified: "github.com/osxfuse/osxfuse/"
