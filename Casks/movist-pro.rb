364a72bff67c107a780d629578be31dea3655857
Aditya Dalal <adidalal@users.noreply.github.com>
2022-06-19 13:14:36 -0400

    movist-pro 2.8.2,187
    * Update movist-pro from 2.8.1 to 2.8.2
    * Update movist-pro.rb
    
    Closes #126054.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/movist-pro.rb
+++ Casks/movist-pro.rb
@@ -1,6 +1,6 @@
 cask "movist-pro" do
-  version "2.8.1,181"
-  sha256 "3cf62512c6ef5fd4d8d9f0ee47a14c99d8e85a1384e5cc73217f20dd31cc55a7"
+  version "2.8.2,187"
+  sha256 "fc23bb91d9862df9e0692c0a61c530886b75d668fd4443e480310af9a8e0bd70"
 
   url "https://s3.ap-northeast-2.amazonaws.com/update.cocoable.com/MovistPro_#{version.csv.first}.dmg",
       verified: "s3.ap-northeast-2.amazonaws.com/update.cocoable.com/"
