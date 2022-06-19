b06cc655f63b796ba7de405f0834d0b28344269e
Aditya Dalal <adidalal@users.noreply.github.com>
2022-06-19 13:49:09 -0400

    Update movist-pro.rb

--- Casks/movist-pro.rb
+++ Casks/movist-pro.rb
@@ -1,5 +1,5 @@
 cask "movist-pro" do
-  version "2.8.2"
+  version "2.8.2,187"
   sha256 "fc23bb91d9862df9e0692c0a61c530886b75d668fd4443e480310af9a8e0bd70"
 
   url "https://s3.ap-northeast-2.amazonaws.com/update.cocoable.com/MovistPro_#{version.csv.first}.dmg",
