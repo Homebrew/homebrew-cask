a1dcf58860dd1e1998e20223075404b99c85dabc
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-19 12:23:15 -0700

    Update rwts-pdfwriter from 1.0.2 to 1.0.3

--- Casks/rwts-pdfwriter.rb
+++ Casks/rwts-pdfwriter.rb
@@ -1,5 +1,5 @@
 cask "rwts-pdfwriter" do
-  version "1.0.2"
+  version "1.0.3"
   sha256 "990e055ff136dd3a53d37a4656604131d6f947f270ea531a9534c92b7ab2c2a1"
 
   url "https://github.com/rodyager/RWTS-PDFwriter/releases/download/v#{version}/RWTS-PDFwriter.pkg"
