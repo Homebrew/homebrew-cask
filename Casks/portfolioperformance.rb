898aa25d331bd31a07896c69e669de15ef977965
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-19 12:18:18 -0700

    Update portfolioperformance from 0.58.4 to 0.58.5

--- Casks/portfolioperformance.rb
+++ Casks/portfolioperformance.rb
@@ -1,12 +1,12 @@
 cask "portfolioperformance" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
-  version "0.58.4"
+  version "0.58.5"
 
   if Hardware::CPU.intel?
-    sha256 "62b6f49a894fd1653f6d032c885dfefc5abf39fdd8f6a9e89d4c6efd9345314d"
+    sha256 "7afcf97ec3e3153d943f39a6093e841553f24950a246a5af1268594731022d3e"
   else
-    sha256 "15f7ba8879e64fdf868ddf81efd648ebccae254698c1c0ee88eab525cf0c15d8"
+    sha256 "6b6f2cc5d4ffed52c0507807e1024bea1028e1d8f827928a7be9f3626f7da566"
   end
 
   url "https://github.com/buchen/portfolio/releases/download/#{version}/PortfolioPerformance-#{version}-#{arch}.dmg",
