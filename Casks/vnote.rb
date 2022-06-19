d445de982deae2bd60c36241d1bc15c62692f51b
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-19 12:35:36 -0700

    vnote 3.13.1
    Update vnote from 3.13.0 to 3.13.1
    
    Closes #126071.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/vnote.rb
+++ Casks/vnote.rb
@@ -1,6 +1,6 @@
 cask "vnote" do
-  version "3.13.0"
-  sha256 "0b30575e3e71bd920836af3d4a5a0de61fbf55cc3a157af0e7cacdc987288d9f"
+  version "3.13.1"
+  sha256 "a07e31e1a39bac5bd535f84d75bc76147e194b955634970a803e33a7e7dbbab1"
 
   url "https://github.com/vnotex/vnote/releases/download/v#{version}/vnote-mac-x64_v#{version}.zip",
       verified: "github.com/vnotex/vnote/"
