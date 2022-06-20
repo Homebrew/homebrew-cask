9f65f6356ee0c8713486649519eb8f264dd1b169
ShredderWalnut <34428332+ShredderWalnut@users.noreply.github.com>
2022-06-20 05:54:01 +0000

    Create tailscale.rb (#125508)
    * Create tailscale.rb
    
    * Update and rename tailscale.rb to Casks/tailscale.rb
    
    * Update tailscale.rb
    
    * Update tailscale.rb
    
    * Various updates
    
    * Update tailscale.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

---
+++ Casks/tailscale.rb
@@ -0,0 +1,30 @@
+cask "tailscale" do
+  version "1.26.0"
+  sha256 "16dd0f68f736ed76e46bc75e95bc86c7da4da811da08e2079fcc03aeceeec4c7"
+
+  url "https://pkgs.tailscale.com/stable/Tailscale-#{version}-macos.zip"
+  name "Tailscale"
+  desc "Mesh VPN based on Wireguard"
+  homepage "https://tailscale.com/"
+
+  livecheck do
+    url "https://pkgs.tailscale.com/stable/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  app "Tailscale.app"
+
+  uninstall login_item: "Tailscale"
+
+  zap trash: [
+    "Library/Tailscale",
+    "~/Library/Application Scripts/*.io.tailscale.ipn.macsys",
+    "~/Library/Application Scripts/io.tailscale.ipn.macsys.share-extension",
+    "~/Library/Application Scripts/io.tailscale.ipn.macsys",
+    "~/Library/Containers/io.tailscale.ipn.macos.network-extension",
+    "~/Library/Containers/io.tailscale.ipn.macsys.share-extension",
+    "~/Library/Containers/io.tailscale.ipn.macsys",
+    "~/Library/Containers/Tailscale",
+    "~/Library/Group Containers/*.io.tailscale.ipn.macsys",
+  ]
+end
