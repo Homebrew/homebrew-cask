99db08dddb94868c403996760e8823e20fe12a50
JounQin <admin@1stg.me>
2022-06-20 10:23:53 +0800

    Upgrade MiaoYan from 0.5.0 to 0.5.1

--- Casks/miaoyan.rb
+++ Casks/miaoyan.rb
@@ -1,12 +1,19 @@
 cask "miaoyan" do
-  version "0.5.0"
-  sha256 "b95f8c3b64bd1aaa3e441ee72bd0ce3042b3882a2fe3deabce8e027fcf63310a"
+  version "0.5.1"
+  sha256 "fd0a8c14bba2bfd488771a351b103a95759694cb5a73e7e33dc4e7f5b1ef3ab0"
 
-  url "https://github.com/tw93/MiaoYan/releases/download/V#{version}/MiaoYan-V#{version}.dmg",
+  url "https://github.com/tw93/MiaoYan/releases/download/V#{version}/MiaoYan.dmg",
       verified: "github.com/tw93/MiaoYan"
   name "MiaoYan"
   desc "⛷ Lightweight Markdown app to help you write great sentences"
-  homepage "https://miaoyan.vercel.app/"
+  homepage "https://miaoyan.app/"
 
   app "MiaoYan.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.tw93.MiaoYan",
+    "~/Library/Caches/com.tw93.MiaoYan",
+    "~/Library/HTTPStorages/com.tw93.MiaoYan",
+    "~/Library/Preferences/com.tw93.MiaoYan.plist",
+  ]
 end
