22fe33eb4d68126062e6ad179bc6a0cfbfc801d4
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-20 15:59:41 -0700

    busycal 2022.2.3,2022-06-21-01-20
    * Update busycal from 2022.2.3,2022-06-18-04-07 to 2022.2.3,2022-06-21-01-20
    * Fix uninstall
    
    Closes #126134.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/busycal.rb
+++ Casks/busycal.rb
@@ -1,6 +1,6 @@
 cask "busycal" do
-  version "2022.2.3,2022-06-18-04-07"
-  sha256 "2fba73a19946092e21b833c3b8761b59834db5ded680fb5d4afb9c5c20a24445"
+  version "2022.2.3,2022-06-21-01-20"
+  sha256 "77be84d55739c2a7c5da3b6e76864ff2a899727c04aaaae1adf7c0ba13bbda93"
 
   url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bcl-#{version.csv.first}-#{version.csv.second}.zip",
       verified: "7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/"
@@ -25,7 +25,8 @@ cask "busycal" do
 
   uninstall pkgutil: "com.busymac.busycal3.pkg",
             quit:    "N4RA379GBW.com.busymac.busycal3.alarm",
-            signal:  ["TERM", "com.busymac.busycal3"]
+            signal:  ["TERM", "com.busymac.busycal3"],
+            delete:  "/Applications/BusyCal.app"
 
   zap trash: [
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.busymac.busycal#{version.major}.sfl*",
