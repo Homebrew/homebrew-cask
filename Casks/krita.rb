d081e5e34be9c40e286987fa9c8d31a33df143dd
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-19 20:34:54 -0700

    Fix livecheck

--- Casks/krita.rb
+++ Casks/krita.rb
@@ -2,15 +2,15 @@ cask "krita" do
   version "5.0.6"
   sha256 "53edf2519aaad0f2d3e3b858d7bd8dc2e8215ed9b3bfea8de0516527c54aadba"
 
-  url "https://cdn.download.kde.org/stable/krita/#{version.major_minor_patch}/krita-#{version}.dmg",
-      verified: "cdn.download.kde.org/stable/krita/"
+  url "https://download.kde.org/stable/krita/#{version.major_minor_patch}/krita-#{version}.dmg",
+      verified: "download.kde.org/stable/krita/"
   name "Krita"
   desc "Free and open-source painting and sketching program"
   homepage "https://krita.org/"
 
   livecheck do
-    url "https://download.kde.org/stable/krita/"
-    regex(%r{href=['"]?(\d+(?:\.\d+)+)/['"]?}i)
+    url "https://krita.org/en/download/krita-desktop/"
+    regex(/href=.*?krita[._-](\d+(?:\.\d+)+)\.dmg/i)
   end
 
   depends_on macos: ">= :sierra"
