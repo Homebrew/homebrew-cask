52c2d1cfa38c4917ad62dfc9abfb9fc19ffa83b2
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-20 07:26:29 -0700

    Update ableton-live-intro from 11.1.5 to 11.1.6

--- Casks/ableton-live-intro.rb
+++ Casks/ableton-live-intro.rb
@@ -1,9 +1,9 @@
 cask "ableton-live-intro" do
   arch = MacOS.version >= :mojave ? "universal" : "intel"
-  version "11.1.5"
+  version "11.1.6"
 
   if MacOS.version >= :mojave
-    sha256 "06d6801bfc79ece8949318d3d187016d7be73e2642fdfeffa6a261b1c81e0a09"
+    sha256 "ffa68423dcaaa7b1668563655e4c5a85c3cd5adbb9bd58f5c19e7ab7331282bd"
   else
     sha256 "91064b1551ded54c841d61d8f30a538c6f2f177d1caf2a39f823ea952093f38e"
   end
