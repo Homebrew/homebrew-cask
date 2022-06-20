dcf62aa52fca18838af12c6ab7f8c8c4a8c37beb
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-20 16:18:30 -0700

    elephicon 1.9.8
    Update elephicon from 1.9.7 to 1.9.8
    
    Closes #126145.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/elephicon.rb
+++ Casks/elephicon.rb
@@ -1,12 +1,12 @@
 cask "elephicon" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "1.9.7"
+  version "1.9.8"
 
   if Hardware::CPU.intel?
-    sha256 "a67a15d952ebc5c0b2262f23968a5b12c69b74725f05533667f7674964b5a41c"
+    sha256 "c8aa6361edda29a5d884842e03232fc4146480befb10b9223a2e60c298286e8f"
   else
-    sha256 "c1bfddb4b4104b836857b808b5a7909471aac090a4a12a227b4e3d23445b0b6a"
+    sha256 "ffc544f7ec719111aa0f85b033398220fef121e5518ed548b44accc244583fac"
   end
 
   url "https://github.com/sprout2000/elephicon/releases/download/v#{version}/Elephicon-#{version}-darwin-#{arch}.zip"
