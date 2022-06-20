cdfe1fe87ba66f0ae918f28ee36fd22d66b93e73
Bram Vandeperre <bra@dixa.com>
2022-06-20 14:29:43 +0200

    Update dixa.rb

--- Casks/dixa.rb
+++ Casks/dixa.rb
@@ -1,7 +1,7 @@
 cask "dixa" do
   version "4.0.9"
-  sha256 "525b958ddf0de299296a58bce9493649586a82da687511b6d9d4ac222927a2f5"
-  url "https://github.com/todbot/Blink1Control2/releases/download/v#{version}/dixa-#{version}.dmg",
+  sha256 "3391dc12b8d67324f5f5d3b68964d58fb51de53df2cc4b2b3cb98626d959d98b"
+  url "https://github.com/dixahq/dixa-desktop-app-release/releases/download/v#{version}/dixa-#{version}.dmg",
       verified: "github.com/dixahq/dixa-desktop-app-release"
   name "Dixa"
   desc "Dixa is a born global customer service tech company on a mission to solve customer chaos, eliminate bad customer service and build strong bonds between companies and their customers - much like the connection between friends."
