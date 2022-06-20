e8a1b7e442eaceb105d8b4cdef4c09d1c322d37b
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-20 12:41:26 -0700

    Update gns3 from 2.2.32 to 2.2.33

--- Casks/gns3.rb
+++ Casks/gns3.rb
@@ -1,7 +1,7 @@
 cask "gns3" do
   # NOTE: "3" is not a version number, but an intrinsic part of the product name
-  version "2.2.32"
-  sha256 "d6cd2bbb7e4f28487a14a0d6b3e72e2677298e909a58b1ae0822789b906a6f23"
+  version "2.2.33"
+  sha256 "ad136d5c73039238d83c0a505d207e252393039e2fc6ba83be9ee191195bb500"
 
   url "https://github.com/GNS3/gns3-gui/releases/download/v#{version}/GNS3-#{version}.dmg",
       verified: "github.com/GNS3/gns3-gui/"
