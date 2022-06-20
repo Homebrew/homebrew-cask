be3c41b5cdcfd017090892a121d3975bc6359ae9
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-06-20 10:35:48 -0400

    tradingview 1.0.10
    Update tradingview from 1.0.8 to 1.0.10
    
    Closes #126117.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/tradingview.rb
+++ Casks/tradingview.rb
@@ -1,6 +1,6 @@
 cask "tradingview" do
-  version "1.0.8"
-  sha256 "5ea7c32be38feb4829c822a4d289485bbc0c51afe314e7c7904fa980a8262856"
+  version "1.0.10"
+  sha256 "a5f0345ef7af611831d619a3b0bc94fbdd2cd4c6caf424ebc53fd4d3b3b5ed54"
 
   url "https://tvd-packages.tradingview.com/stable/#{version}/darwin/TradingView.dmg"
   name "TradingView Desktop"
