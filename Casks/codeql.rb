8fe3f12ef766bd65df2397abdb788658987f069a
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-06-20 09:08:42 -0400

    Update codeql from 2.9.3 to 2.9.4

--- Casks/codeql.rb
+++ Casks/codeql.rb
@@ -1,6 +1,6 @@
 cask "codeql" do
-  version "2.9.3"
-  sha256 "cadd1dcced83481f2c278350eb25745d28e96df344dccc9d39e759af017a0ab5"
+  version "2.9.4"
+  sha256 "b5394222a0e48dfa565c367918b1b0dc00b4bb69a3be1c2bdcb1d3a5ed73c490"
 
   url "https://github.com/github/codeql-cli-binaries/releases/download/v#{version}/codeql-osx64.zip"
   name "CodeQL"
