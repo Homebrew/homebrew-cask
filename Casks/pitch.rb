e6dd6ec12d011465f69bd89dd5410e491626d27e
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-20 21:21:42 -0700

    pitch 1.79.1,stable.3,4103985
    * Fix URL & bump version
    * pitch: update livecheck and version
    
    Closes #126165.
    
    Co-authored-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/pitch.rb
+++ Casks/pitch.rb
@@ -1,17 +1,17 @@
 cask "pitch" do
-  version "1.73.0,3651924"
-  sha256 "a72ab96f429a518282242af9dd8ea80d754d0a12968dc14a4151e07ac1758bcf"
+  version "1.79.1,stable.3,4103985"
+  sha256 "0ea9eb4cf0c9e60a14278c54a6d61f77622a122aa2abfd18df16e9ebfd568e74"
 
-  url "https://desktop-app-builds.pitch.com/Pitch-#{version.csv.first}-ci#{version.csv.second}.dmg"
+  url "https://desktop-app-builds.pitch.com/Pitch-#{version.csv.first}-#{version.csv.second}-ci#{version.csv.third}.dmg"
   name "Pitch"
   desc "Collaborative presentation software"
   homepage "https://pitch.com/"
 
   livecheck do
     url "https://desktop-app-builds.pitch.com/latest-mac.yml"
-    regex(/Pitch[._-]v?(\d+(?:\.\d+)+).*?[._-]ci(\d+)\.dmg/i)
+    regex(/Pitch[._-]v?(\d+(?:\.\d+)+)-([^-]+)-ci(\d+)\.dmg/i)
     strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
+      page.scan(regex).map { |match| "#{match.first},#{match.second},#{match.third}" }
     end
   end
 
