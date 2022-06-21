3bf028e8e00af12545e65ec106ffaf444aa92e38
owine <owine@users.noreply.github.com>
2022-06-21 12:44:19 -0500

    BetterDisplay: New Cask

---
+++ Casks/betterdisplay.rb
@@ -0,0 +1,26 @@
+cask "betterdisplay" do
+  version "1.2.6"
+  sha256 "2d9d919ee35d73ac0e09a89b1a2b2ddd9ddfe9a966986ebdd7a0fb27febc0d42"
+
+  url "https://github.com/waydabber/BetterDisplay/releases/download/v#{version}/BetterDisplay-v#{version}.dmg"
+  name "BetterDisplay"
+  desc "Display Management, Custom Resolutions, Brightness Adjustment and Dummy Displays for Macs (formerly BetterDummy)"
+  homepage "https://github.com/waydabber/BetterDisplay"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+
+  app "BetterDisplay.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/me.waydabber.BetterDiaplayHelper",
+    "~/Library/Application Scripts/me.waydabber.BetterDisplay",
+    "~/Library/Containers/me.waydabber.BetterDisplayHelper",
+    "~/Library/Containers/me.waydabber.BetterDisplay",
+    "~/Library/Preferences/me.waydabber.BetterDisplay.plist",
+  ]
+end
