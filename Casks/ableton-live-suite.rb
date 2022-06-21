1371192035b9810f1e09f7857ac6d500bb8a718c
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-21 08:30:17 -0700

    Bump pre-Mojave

--- Casks/ableton-live-suite.rb
+++ Casks/ableton-live-suite.rb
@@ -5,7 +5,7 @@ cask "ableton-live-suite" do
   if MacOS.version >= :mojave
     sha256 "41e2fc386c47e395bc2cdf397238ac3cb2fa0ace9bb1cbb18bfa3358778dfdc1"
   else
-    sha256 "0e1779d22c087993a4e5f11f9f2a83a429dccfafa92c5e7aa05c87219de39c90"
+    sha256 "ae6f2c978009d7baa86b53d41be8a5eafda27bcb3a0bf102ebff629b4ebe091f"
   end
 
   url "https://cdn-downloads.ableton.com/channels/#{version}/ableton_live_suite_#{version}_#{arch}.dmg"
