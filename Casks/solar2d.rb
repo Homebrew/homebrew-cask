dc5cef3ca20d847104e7d1ad9ed9698733802330
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-21 14:53:10 -0700

    Update solar2d from 2022.3673 to 2022.3674

--- Casks/solar2d.rb
+++ Casks/solar2d.rb
@@ -1,6 +1,6 @@
 cask "solar2d" do
-  version "2022.3673"
-  sha256 "05ced962b7b3748deed3bdf76666acddbc281605a4ef7cda61dea45d7a39c1ad"
+  version "2022.3674"
+  sha256 "4616b1e080dd69adb9590a16f48385c4823e289db9f724265c603a3d3b467df2"
 
   url "https://github.com/coronalabs/corona/releases/download/#{version.minor}/Solar2D-macOS-#{version}.dmg",
       verified: "github.com/coronalabs/corona/"
