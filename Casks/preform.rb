bcf0ba109d32e28d492df24bb04dae90420775e1
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-20 21:44:45 -0700

    Fix hash

--- Casks/preform.rb
+++ Casks/preform.rb
@@ -1,6 +1,6 @@
 cask "preform" do
   version "3.25.0,137_20268"
-  sha256 "432c827a4c416d3e5a705e320a529874fa4f2eeefe3ee15c302ea64132df8cce"
+  sha256 "979306afc732233c0705e17928e9fb12617508008823f09fef1e963a0c24fcf6"
 
   url "https://downloads.formlabs.com/PreForm/Release/#{version.csv.first}/PreForm_mac_#{version.csv.first}_release_releaser_#{version.csv.second}.dmg"
   name "PreForm"
