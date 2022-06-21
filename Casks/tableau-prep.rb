80723804fbb53cdf8ce21a553de13e8a3cf9ef9a
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-21 14:58:55 -0700

    Update tableau-prep from 2022.1.2 to 2022.1.3

--- Casks/tableau-prep.rb
+++ Casks/tableau-prep.rb
@@ -1,6 +1,6 @@
 cask "tableau-prep" do
-  version "2022.1.2"
-  sha256 "9880706682ff933c4a8c566ecefc6b3b0c5d833b0b781a02e71e6a015fbc0bb8"
+  version "2022.1.3"
+  sha256 "41e47c5564c38b07f9c64be99be8e8f97a437b9a232e2cd04c81a94116790f79"
 
   url "https://downloads.tableau.com/esdalt/tableau_prep/#{version}/TableauPrep-#{version.dots_to_hyphens}.dmg"
   name "Tableau Prep"
