5db93b1d3d16b44b129acc4dd0de686c2c0a0b86
Tin Htet Kyaw <tin@tins-air.lan>
2022-06-21 09:07:53 -0700

    ukelele 3.5.6,355
    Update ukelele from 3.5.5 to 3.5.6
    
    Closes #126194.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/ukelele.rb
+++ Casks/ukelele.rb
@@ -1,6 +1,6 @@
 cask "ukelele" do
-  version "3.5.5,351"
-  sha256 "7a4309b0694aa5e802efc877260e71171fe8762802386b362c0fdbb183b39c61"
+  version "3.5.6,355"
+  sha256 "5cbf69cb40821aede0692eef80425f43991012304113e517fc9220b396085067"
 
   url "https://software.sil.org/downloads/r/ukelele/Ukelele_#{version.csv.first}.dmg"
   name "Ukelele"
