8b02fc0c34204c43278e1bb775fd5b523e7a622c
JounQin <admin@1stg.me>
2022-06-21 16:43:33 +0800

    MiaoYan 0.6.0 (#125999)
    * MiaoYan 0.5.0
    
    * Upgrade MiaoYan from 0.5.0 to 0.5.1
    
    * Update miaoyan.rb
    
    * MiaoYan 0.6.0
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

---
+++ Casks/miaoyan.rb
@@ -0,0 +1,19 @@
+cask "miaoyan" do
+  version "0.6.0"
+  sha256 "aeed12fa16a8b19430a72d75b7aa77d2cafed018afc99967a10027f6c613511c"
+
+  url "https://github.com/tw93/MiaoYan/releases/download/V#{version}/MiaoYan.dmg",
+      verified: "github.com/tw93/MiaoYan"
+  name "MiaoYan"
+  desc "Markdown editor"
+  homepage "https://miaoyan.app/"
+
+  app "MiaoYan.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.tw93.MiaoYan",
+    "~/Library/Caches/com.tw93.MiaoYan",
+    "~/Library/HTTPStorages/com.tw93.MiaoYan",
+    "~/Library/Preferences/com.tw93.MiaoYan.plist",
+  ]
+end
