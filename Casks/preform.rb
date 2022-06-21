e6b6aa715e9b221c753204c0bbf51e1c03b87441
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-20 21:44:02 -0700

    Fix URL

--- Casks/preform.rb
+++ Casks/preform.rb
@@ -1,9 +1,8 @@
 cask "preform" do
-  version "3.24.2,125_19236"
+  version "3.25.0,137_20268"
   sha256 "432c827a4c416d3e5a705e320a529874fa4f2eeefe3ee15c302ea64132df8cce"
 
-  url "https://s3.amazonaws.com/FormlabsReleases/Release/#{version.csv.first}/PreForm_mac_#{version.csv.first}_release_releaser_#{version.csv.second}.dmg",
-      verified: "s3.amazonaws.com/FormlabsReleases/"
+  url "https://downloads.formlabs.com/PreForm/Release/#{version.csv.first}/PreForm_mac_#{version.csv.first}_release_releaser_#{version.csv.second}.dmg"
   name "PreForm"
   desc "3D printing setup, management, and monitoring"
   homepage "https://formlabs.com/tools/preform/"
