839cc0bae110381e1147208be5d22f15c15c85b2
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-22 14:24:26 -0700

    Bump Intel

--- Casks/eloston-chromium.rb
+++ Casks/eloston-chromium.rb
@@ -2,8 +2,8 @@ cask "eloston-chromium" do
   arch = Hardware::CPU.intel? ? "x86-64" : "arm64"
 
   if Hardware::CPU.intel?
-    version "102.0.5005.115-1.1,1654930392"
-    sha256 "6cd1f1ae3c453ff957290d9b99ca8b2036cbaa71a33f344d3131d2dbd19f8d21"
+    version "103.0.5060.53-1.1,1655884980"
+    sha256 "1003f91a00da4b3051c0ffc87913efb267ae9b799421b1a82ba506ff08e46910"
   else
     version "103.0.5060.53-1.1,1655922667"
     sha256 "9da5ef44539f75ae656eac5f03156a02b7c9554804af355d6058b0f69460bba1"
