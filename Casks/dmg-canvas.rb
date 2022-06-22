44cd510811846f829fc59289ba1672ddd2944492
Miccal Matthews <miccal.matthews@gmail.com>
2022-06-22 10:51:30 +0800

    Update dmg-canvas.rb

--- Casks/dmg-canvas.rb
+++ Casks/dmg-canvas.rb
@@ -2,7 +2,8 @@ cask "dmg-canvas" do
   version "3.0.17,300026"
   sha256 :no_check
 
-  url "https://arweb-assets.s3.amazonaws.com/downloads/dmgcanvas/DMGCanvas.dmg"
+  url "https://arweb-assets.s3.amazonaws.com/downloads/dmgcanvas/DMGCanvas.dmg",
+      verified: "arweb-assets.s3.amazonaws.com/downloads/dmgcanvas/"
   name "DMG Canvas"
   desc "Stylized disk images made easy"
   homepage "https://www.araelium.com/dmgcanvas"
