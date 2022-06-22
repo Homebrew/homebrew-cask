2e40a54a5710c470f08c57bd2034973a01d1b6a2
linbo0518 <linbo0518@gmail.com>
2022-06-22 11:41:49 +0800

    fig: checksum update
    Update fig from 1.0.58,462 to 1.0.58,462
    
    Closes #126271.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/fig.rb
+++ Casks/fig.rb
@@ -1,6 +1,6 @@
 cask "fig" do
   version "1.0.58,462"
-  sha256 "baa0293228b6779dbb0cc75b3c482817df9587a82d51f9c208b05aeb7aa38a37"
+  sha256 "17668d02ab9e21a41c5db0e73cccd1b9774e2d41054d7ffc16ad31f45a8f2ed8"
 
   url "https://versions.withfig.com/fig%20#{version.csv.second}.dmg",
       verified: "versions.withfig.com/"
