90e5150bdeb7b5fd3799dec268bdfe61364a27fd
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-22 09:55:26 -0700

    Update hightop from 1.3 to 1.3.1

--- Casks/hightop.rb
+++ Casks/hightop.rb
@@ -1,6 +1,6 @@
 cask "hightop" do
-  version "1.3,35"
-  sha256 "cc62867ed3b2f7c91e654166fbc59696661b67fc8df39677c4cf6d2f80138050"
+  version "1.3.1,36"
+  sha256 "e3dfa37d6a14a4d8d61a0640a2c8d95e393cabca3043eeb6b56a209fba2ed53f"
 
   url "https://hightop.app/downloads/HighTop#{version.csv.first}.dmg"
   name "HighTop"
