83ce21cff23b953c447c9b1bf997c55b4b5b79f2
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-06-21 23:49:57 -0400

    Update standard-notes from 3.20.0 to 3.22.11 (#126110)
    * Update standard-notes from 3.20.0 to 3.22.11
    
    * Rename from standardnotes/desktop to standardnotes/app
    
    * Fix capitalization
    
    * Update tag name
    
    * Update standard-notes.rb
    
    * Update standard-notes.rb
    
    * Update standard-notes.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -1,19 +1,24 @@
 cask "standard-notes" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "3.20.0"
+  version "3.22.11"
 
   if Hardware::CPU.intel?
-    sha256 "a8fd8b9f980853908b9b7e140ccb4a3132a753a834aa522aaef415a0fee9a406"
+    sha256 "03a20c8b09c3273aaf1c4c89406e9a2e293b1bd23f0c5dd6dba80379b07109ac"
   else
-    sha256 "9547daa99ad1397ffebf27c1a85329280252ff48155612c689a356cc18ab2943"
+    sha256 "0957ea6f00124dba2edf4ef427343e917c5f1e96f86d915a4c65aef763868127"
   end
 
-  url "https://github.com/standardnotes/desktop/releases/download/v#{version}/Standard-Notes-#{version}-mac-#{arch}.zip",
-      verified: "github.com/standardnotes/desktop/"
+  url "https://github.com/standardnotes/app/releases/download/%40standardnotes%2Fdesktop%40#{version}/standard-notes-#{version}-mac-#{arch}.zip",
+      verified: "github.com/standardnotes/app/"
   name "Standard Notes"
   desc "Free, open-source, and completely encrypted notes app"
   homepage "https://standardnotes.org/"
+
+  livecheck do
+    url "https://github.com/standardnotes/app/releases/latest"
+    strategy :header_match
+  end
 
   auto_updates true
 
