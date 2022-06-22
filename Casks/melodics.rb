8fd42c655eca399af932fecbaf695caa69367173
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-21 20:30:22 -0700

    melodics 2.1.7877,B24E6F7D-25CA-4942-99D4-81341E295DB9
    Update melodics from 2.1.7757 to 2.1.7877
    
    Closes #126265.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/melodics.rb
+++ Casks/melodics.rb
@@ -1,6 +1,6 @@
 cask "melodics" do
-  version "2.1.7757,7F688BFF-D739-45FB-8289-DA11CAA287CF"
-  sha256 "1977f023fab31def03e8f8591fa074b534018ed98fd586691681c5e3b527a862"
+  version "2.1.7877,B24E6F7D-25CA-4942-99D4-81341E295DB9"
+  sha256 "bf660a329fc6def7f4a3023c2f11143eb48370a52778e36bd7a667e8b8ffcf98"
 
   url "https://web-cdn.melodics.com/download/#{version.csv.second}.zip"
   name "Melodics"
