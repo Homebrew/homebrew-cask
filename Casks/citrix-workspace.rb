3a4b7a1583a3bddd9df9d79c4e7952dcbbcd3f80
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-21 19:53:08 -0700

    citrix-workspace: Fix livecheck & correct version (#126262)
    Fix livecheck & correct version

--- Casks/citrix-workspace.rb
+++ Casks/citrix-workspace.rb
@@ -1,5 +1,5 @@
 cask "citrix-workspace" do
-  version "22.06.0.39,2206"
+  version "22.04.0.44,2204"
   sha256 :no_check
 
   url "https://downloadplugins.citrix.com/Mac/CitrixWorkspaceApp.dmg"
@@ -8,7 +8,7 @@ cask "citrix-workspace" do
   homepage "https://www.citrix.com/"
 
   livecheck do
-    url "https://www.citrix.com/downloads/workspace-app/mac/workspace-app-for-mac-latest.html"
+    url "https://www.citrix.com/downloads/workspace-app/mac/workspace-app-for-mac-latest1.html"
     strategy :page_match do |page|
       match = page.match(/Version:\s(\d+(?:\.\d+)+)\s\((\d+(?:\.\d+)*)\)/i)
       next if match.blank?
