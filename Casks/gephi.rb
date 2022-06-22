dfb18a593a658d7fc76f42b142f6bc38493c76a9
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-22 14:13:28 -0700

    gephi 0.9.6
    Update gephi from 0.9.5 to 0.9.6
    
    Closes #126341.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/gephi.rb
+++ Casks/gephi.rb
@@ -1,6 +1,6 @@
 cask "gephi" do
-  version "0.9.5"
-  sha256 "a0d9f63e7b385f6c19d5016dc16808253401fd09b3d143d4cb0894323c78a8b7"
+  version "0.9.6"
+  sha256 "dcbea35143687f2c691aa399f776f8aa02996138adb5f9170bf383de3192a387"
 
   url "https://github.com/gephi/gephi/releases/download/v#{version}/gephi-#{version}-macos-x64.dmg",
       verified: "github.com/gephi/gephi/"
