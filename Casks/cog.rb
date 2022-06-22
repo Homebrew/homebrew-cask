54eddd16beafa7b948f82056d1119de084d7e345
Miccal Matthews <miccal.matthews@gmail.com>
2022-06-23 04:54:48 +0800

    Update cog.rb

--- Casks/cog.rb
+++ Casks/cog.rb
@@ -1,17 +1,15 @@
 cask "cog" do
-  version "2395,271b9b34"
+  version "271b9b34"
   sha256 "a93eb11727c44f0a1c36aa998882aa80f326f11cea159e1b2ebcb76582f9a38d"
 
-  url "https://cogcdn.cog.losno.co/Cog-#{version.csv.second}.zip"
+  url "https://cogcdn.cog.losno.co/Cog-#{version}.zip"
   name "Cog"
   desc "Audio player"
   homepage "https://cog.losno.co/"
 
   livecheck do
     url "https://cogcdn.cog.losno.co/mercury.xml"
-    strategy :sparkle do |item|
-      item.version.split("-g", 2).join(",")
-    end
+    regex(/Cog[._-]v?(.+)\.zip/i)
   end
 
   auto_updates true
