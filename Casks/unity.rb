2344c79a23da8f39826691d08ca0d2c8db17321f
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-23 09:59:49 -0700

    unity 2022.1.6f1,e49a51cf6290
    Update unity from 2022.1.5f1 to 2022.1.6f1
    
    Closes #126417.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -1,12 +1,12 @@
 cask "unity" do
   arch = Hardware::CPU.intel? ? "" : "Arm64"
 
-  version "2022.1.5f1,feea5ec8f162"
+  version "2022.1.6f1,e49a51cf6290"
 
   if Hardware::CPU.intel?
-    sha256 "5093df3663d7356e156ccc116072457625010e57107371c4fce5c0230009018f"
+    sha256 "e278aaa89b281aa17da37d3c0bf47725c26016d781c1c2341d729233ce9dfaef"
   else
-    sha256 "6e75c0fda9d2b61d00458dafc62d887cc1368bbce666e384d1a05819dad4e48d"
+    sha256 "7cb010fbac5245c8ff60530acfc9ab1be69277bea70847960b3517c91f926097"
   end
 
   url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorInstaller#{arch}/Unity-#{version.csv.first}.pkg",
