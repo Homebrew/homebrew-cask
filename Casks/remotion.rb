23af040e8388511607d6bcd40ab4879c7995b492
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-22 18:28:18 -0700

    Update remotion from 1.84.4 to 1.85.0

--- Casks/remotion.rb
+++ Casks/remotion.rb
@@ -1,6 +1,6 @@
 cask "remotion" do
-  version "1.84.4"
-  sha256 "954ef60dcea5b1b4921207cffbdfab7ede47c069341f2d1e0f1f0a28e6ffaeb8"
+  version "1.85.0"
+  sha256 "cd7148c9144fbf5ec94bb231525bd1196b2b7d77a9176b6e44baf288a190b40d"
 
   url "https://updates.remotion.com/installers/Remotion%20#{version}.dmg"
   name "remotion"
