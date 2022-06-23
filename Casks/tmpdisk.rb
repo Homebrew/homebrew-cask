4e2259f871821c8a4f18c49d24fbc6144e3dbb70
Toni Förster <toni.foerster@gmail.com>
2022-06-23 12:18:23 +0200

    Add TmpDisk.app v2.0.2

---
+++ Casks/tmpdisk.rb
@@ -0,0 +1,18 @@
+cask "tmpdisk" do
+  version "2.0.2"
+  sha256 "bc8e09b55c20e24850d31a2ff797567dd854a7c514c10f293d53005dade304ef"
+
+  url "https://github.com/imothee/tmpdisk/releases/download/v#{version}/TmpDisk.dmg"
+  name "TmpDisk"
+  desc "Ram Disk Management App"
+  homepage "https://github.com/imothee/tmpdisk"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  app "TmpDisk.app"
+
+  zap trash: ["~/Library/Preferences/com.imothee.TmpDisk.plist"]
+end
