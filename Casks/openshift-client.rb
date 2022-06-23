e6e2990736b5468ac02bf00378ffc06ea76feb33
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-23 09:24:12 -0700

    openshift-client 4.10.20
    Update openshift-client from 4.10.18 to 4.10.20
    
    Closes #126393.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/openshift-client.rb
+++ Casks/openshift-client.rb
@@ -1,10 +1,10 @@
 cask "openshift-client" do
-  version "4.10.18"
+  version "4.10.20"
 
   if Hardware::CPU.intel?
-    sha256 "285c307491d8ffd19c065a942515fda78e53f95289d4b4985aa4c92439f7f339"
+    sha256 "9382fa90968e1977fe3b81a18e099b6755f3f8b2aaec7e028abb25d65e621332"
   else
-    sha256 "4a6efa36a00e6de6f630ca00703a559edb276fa511db7b841fab33b3af12f43c"
+    sha256 "aa0c96671aaa2e54a5f88484416fa63abf203e91c18216ab6551b91f83009e8f"
   end
 
   arch = Hardware::CPU.intel? ? "" : "-arm64"
