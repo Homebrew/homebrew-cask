e9c9622a6ad1cc6f1ea11dab9d50e10a2905323d
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-23 22:40:25 -0700

    winzip 9.0.5520
    * Bump version, add livecheck
    * Update Casks/winzip.rb
    * Update winzip.rb
    
    Closes #126453.
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>
    Co-authored-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/winzip.rb
+++ Casks/winzip.rb
@@ -1,10 +1,24 @@
 cask "winzip" do
-  version "7.0.4565"
-  sha256 "e65aab9f4619a190da6fc29a01864049cf409e5ae6c962d840d96bbd25d98899"
+  version "9.0.5520"
+  sha256 "2b7f2b5322ffe709151b9790aaba6231ce54e423ceca5ea9433977e3079da528"
 
   url "https://download.winzip.com/winzipmacedition#{version.major}.dmg"
   name "WinZip"
+  desc "File archiving tool"
   homepage "https://www.winzip.com/mac/en/winzip.html"
+
+  livecheck do
+    url "https://www.winzip.com/en/download/"
+    regex(/href=.*?winzipmacedition[._-]?v?(\d+)\.dmg/i)
+    strategy :page_match do |page, regex|
+      major_version = page[regex, 1]
+      next if major_version.blank?
+
+      cask = CaskLoader.load("winzip")
+      download_url = "https://download.winzip.com/winzipmacedition#{major_version}.dmg"
+      Homebrew::Livecheck::Strategy::ExtractPlist.find_versions(cask: cask, url: download_url)[:matches].values
+    end
+  end
 
   app "WinZip.app"
 end
