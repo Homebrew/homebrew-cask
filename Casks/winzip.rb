5f3b67f3b2a378208ac08bea7da9df61fe4a8f05
p-linnane <105994585+p-linnane@users.noreply.github.com>
2022-06-23 22:40:25 -0700

    Bump version, add livecheck

--- Casks/winzip.rb
+++ Casks/winzip.rb
@@ -1,10 +1,16 @@
 cask "winzip" do
-  version "7.0.4565"
-  sha256 "e65aab9f4619a190da6fc29a01864049cf409e5ae6c962d840d96bbd25d98899"
+  version "9.0.5520"
+  sha256 "2b7f2b5322ffe709151b9790aaba6231ce54e423ceca5ea9433977e3079da528"
 
   url "https://download.winzip.com/winzipmacedition#{version.major}.dmg"
   name "WinZip"
+  desc "File archiving tool"
   homepage "https://www.winzip.com/mac/en/winzip.html"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   app "WinZip.app"
 end
