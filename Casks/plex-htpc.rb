72722c24bd87f52928436fd2de909cfeeed0f153
Eric Park <me@ericswpark.com>
2022-06-24 13:21:08 +0900

    Add Plex HTPC v1.19.1.3076-b24239e5 (#126447)

---
+++ Casks/plex-htpc.rb
@@ -0,0 +1,28 @@
+cask "plex-htpc" do
+  version "1.19.1.3076,b24239e5"
+  sha256 "bc4212a6ca0c778c57f3b0072565de04441fad598f29602354e430d68fac6f0c"
+
+  url "https://downloads.plex.tv/htpc/#{version.csv.first}-#{version.csv.second}/macos/PlexHTPC-#{version.csv.first}-#{version.csv.second}-x86_64.zip"
+  name "Plex HTPC"
+  desc "Home Theater PC media player"
+  homepage "https://www.plex.tv/"
+
+  livecheck do
+    url "https://plex.tv/api/downloads/7.json"
+    regex(/"version"\s*:\s*"(\d(?:\.\d+)*)-([a-f0-9]{8})"/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "Plex HTPC.app"
+
+  zap trash: [
+    "~/Library/Application Support/Plex HTPC",
+    "~/Library/Caches/Plex HTPC",
+    "~/Library/Logs/Plex HTPC",
+  ]
+end
