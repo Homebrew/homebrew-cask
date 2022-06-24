106f7fb96ec68b16c0e933a378434251e38f1ef5
p-linnane <plinnanedev@gmail.com>
2022-06-24 09:57:00 -0700

    Update waterfox from 4.1.3 to 4.1.3.1

--- Casks/waterfox.rb
+++ Casks/waterfox.rb
@@ -1,12 +1,12 @@
 cask "waterfox" do
   arch = Hardware::CPU.intel? ? "Setup" : "ARM.Setup"
 
-  version "4.1.3"
+  version "4.1.3.1"
 
   if Hardware::CPU.intel?
-    sha256 "c077e9377c75d86d20ce85523e6557d3550730a3995c08395cb279b20152d86f"
+    sha256 "efd62ffb526efc3546b9bd6a4f7482918a4a913bc3bfdb54a5dc76a97a5b8f28"
   else
-    sha256 "88cce369851a02ee76585c124a3078f8f523968aeb95a5ff19506e892cab1f64"
+    sha256 "26dada4e24709232294102130e8d3333c40491fcdfa65a4b395b1a9ff9b95451"
   end
 
   url "https://github.com/WaterfoxCo/Waterfox/releases/download/G#{version}/Waterfox.G#{version}.#{arch}.dmg", verified: "github.com/WaterfoxCo/Waterfox/"
