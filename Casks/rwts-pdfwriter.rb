eec6ad82a6695f2cf6df370d92dd467581362d22
Nuri Jung <jnooree@snu.ac.kr>
2022-06-24 13:15:48 +0900

    rwts-pdfwriter: Fix sha256 sum

--- Casks/rwts-pdfwriter.rb
+++ Casks/rwts-pdfwriter.rb
@@ -1,6 +1,6 @@
 cask "rwts-pdfwriter" do
   version "2.0"
-  sha256 "46ff8a69b5aa150bdbecda4ec4f0999d017b9645aa3b794e355916bc350d29cd"
+  sha256 "8e653359009f07f107a02b02ac99d198528c87fc280964c772f90fed7d9557d3"
 
   url "https://github.com/rodyager/RWTS-PDFwriter/releases/download/v#{version}/RWTS-PDFwriter.pkg"
   name "RWTS PDFwriter"
