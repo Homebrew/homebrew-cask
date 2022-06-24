cec97cafd435db8a75642671c0b66ee41ac47dfa
Miccal Matthews <miccal.matthews@gmail.com>
2022-06-25 07:18:30 +0800

    Update mmex.rb

--- Casks/mmex.rb
+++ Casks/mmex.rb
@@ -3,14 +3,10 @@ cask "mmex" do
   sha256 "664be47534cafaacb75819279941e0839b28cf294b688e01e305c8199086d125"
 
   url "https://github.com/moneymanagerex/moneymanagerex/releases/download/v#{version}/mmex-#{version}-Darwin.dmg",
-      verified: "github.com/moneymanagerex/moneymanagerex/releases/download"
+      verified: "github.com/moneymanagerex/moneymanagerex/"
   name "Money Manager Ex"
   desc "Money management application"
   homepage "https://www.moneymanagerex.org/"
-
-  livecheck do
-    url :url
-  end
 
   app "MMEX.app"
 
