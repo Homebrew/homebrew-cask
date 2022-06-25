8c90b2fe1374c0304d8300dcca42dff60907dd42
thk-cheng <kenneth.cheng.tsun.him@gmail.com>
2022-06-25 12:44:16 +0100

    Update telegram-desktop from 4.0.1 to 4.0.2

--- Casks/telegram-desktop.rb
+++ Casks/telegram-desktop.rb
@@ -1,6 +1,6 @@
 cask "telegram-desktop" do
-  version "4.0.1"
-  sha256 "2a3ba3a320753aa5047490d1f743f6099672da3ec88d1c3e34fe29f299d400aa"
+  version "4.0.2"
+  sha256 "26bd233f5e5340e2705a6a3c45b9f67c658cbcca1241343e4e5f5ee078058c17"
 
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg",
       verified: "updates.tdesktop.com/tmac/"
