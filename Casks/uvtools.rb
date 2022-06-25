eb24db8c87d6c4fa30efd91e7270216c115a79d4
Porkepix <porkepix@gmail.com>
2022-06-25 14:31:16 +0200

    uvtools 3.5.2
    Update uvtools from 3.5.1 to 3.5.2
    
    Closes #126534.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/uvtools.rb
+++ Casks/uvtools.rb
@@ -1,12 +1,12 @@
 cask "uvtools" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "3.5.1"
+  version "3.5.2"
 
   if Hardware::CPU.intel?
-    sha256 "fd0a666b49d4ae9d8ebcaeb93c508a37d62148e4ddaa5e3f9e8fbff688088e75"
+    sha256 "119f6fda3e754964b789a3c4914b1ab10216b4b0faf978023d7b50a6e16d283e"
   else
-    sha256 "6f1ea56f74faa2637da6c5349b9e2fa135ebc2c002d24ae67f9cdac51d49f72c"
+    sha256 "91712b216dd82aa72a55f370f12022fc2b22070b3fe383327be0683317250b4e"
   end
 
   url "https://github.com/sn4k3/UVtools/releases/download/v#{version}/UVtools_osx-#{arch}_v#{version}.zip"
