357cd2c64172d6fb7308002d4e6969f37e4a1ebe
p-linnane <plinnanedev@gmail.com>
2022-06-25 10:27:00 -0700

    Update beeper from 3.14.77 to 3.14.82

--- Casks/beeper.rb
+++ Casks/beeper.rb
@@ -1,7 +1,7 @@
 cask "beeper" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "3.14.77"
+  version "3.14.82"
   sha256 :no_check
 
   url "https://download.beeper.com/mac/dmg/#{arch}"
