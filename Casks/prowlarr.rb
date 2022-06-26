123ee77d09e1ad5b79a5d42645a3d7b42c857057
neutric <5984479+neutric@users.noreply.github.com>
2022-06-26 02:24:30 +0200

    Update prowlarr from 0.3.0.1730 to 0.4.0.1832

--- Casks/prowlarr.rb
+++ Casks/prowlarr.rb
@@ -1,12 +1,12 @@
 cask "prowlarr" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "0.3.0.1730"
+  version "0.4.0.1832"
 
   if Hardware::CPU.intel?
-    sha256 "04183921b0c396bb75dc48e414ecb703a27677c516b40a68dc5c51607c5f6ce6"
+    sha256 "df6d1883d0d1d5bd252463b761e169ae4d0315ccce5af0a5ec9f7385770783f5"
   else
-    sha256 "44b0dd8d80cc0341e7a4814cb7e4c53a26a651e94a1d559b14c6af1f84f6a0d4"
+    sha256 "9550f45acffaf48c56ca007866e813bbc573b93646e564a5d6defbc577799b71"
   end
 
   url "https://github.com/Prowlarr/Prowlarr/releases/download/v#{version}/Prowlarr.develop.#{version}.osx-app-core-#{arch}.zip",
