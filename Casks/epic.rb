282bd558c37831e458c86153785f90e223c1855a
neutric <5984479+neutric@users.noreply.github.com>
2022-06-27 01:08:20 +0200

    epic 103.0.5060.53
    Update epic from 91.0.4472.114 to 103.0.5060.53
    
    Closes #126583.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/epic.rb
+++ Casks/epic.rb
@@ -1,15 +1,15 @@
 cask "epic" do
-  arch = Hardware::CPU.intel? ? "mac" : "macarm"
+  arch = Hardware::CPU.intel? ? "" : "m1"
 
-  version "91.0.4472.114"
+  version "103.0.5060.53"
 
   if Hardware::CPU.intel?
-    sha256 "52f388b4da04a26a7c5c732eaf8688e0f2bd0a6c34cd10dffe781d38ae974053"
+    sha256 "d3e352ae9cd7aa342ff8d9b89f87be9b94e7887c38ac5fb9b92e9e2089a721ba"
   else
-    sha256 "79951fee34ba9b9bffea5e03b8217776467e65a737d2054ae38b4f76114acb71"
+    sha256 "445d5344376c5f586350fb53ed04f7535dd48c48029e0f4a18a5b9be45995b8f"
   end
 
-  url "https://cdn.epicbrowser.com/v#{version.major}/#{arch}/epic_#{version}.dmg"
+  url "https://cdn.epicbrowser.com/v100/#{arch}/epic_#{version}.dmg"
   name "Epic Privacy Browser"
   desc "Private, secure web browser"
   homepage "https://www.epicbrowser.com/"
