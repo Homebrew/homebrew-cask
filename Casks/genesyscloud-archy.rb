a2bf7fbc99fe430cbd5d9d29ecadb8add9de9ed0
Bevan Kay <email@bevankay.me>
2022-06-28 09:57:20 +1000

    Update genesyscloud-archy.rb

--- Casks/genesyscloud-archy.rb
+++ Casks/genesyscloud-archy.rb
@@ -3,14 +3,13 @@ cask "genesyscloud-archy" do
   sha256 "8e5773e76e77d382153afae5111ce9795f0fa91e7bdeed6c5838226b93585501"
 
   url "https://sdk-cdn.mypurecloud.com/archy/#{version}/archy-macos.zip", verified: "https://sdk-cdn.mypurecloud.com/archy/"
-  name "genesyscloud-archy"
-  desc "Genesys Cloud Architect YAML processor that lets you create Architect flows"
+  name "Archy"
+  desc "YAML processor"
   homepage "https://developer.genesys.cloud/devapps/archy/"
 
   livecheck do
     url "https://sdk-cdn.mypurecloud.com/archy/versions.json"
-    strategy :page_match
-    regex(/(\d+(?:\.\d+)*)/i)
+    regex(/"version"\s*:\s*"(\d+(?:\.\d+)+)"/i)
   end
 
   binary "archyBin/archy-macos-#{version}", target: "archy"
