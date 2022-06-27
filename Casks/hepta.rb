8167c4d02f01430984a0937e9954bd4ff39613df
p-linnane <plinnanedev@gmail.com>
2022-06-26 19:51:34 -0700

    Update hepta from 0.161.0 to 0.162.0

--- Casks/hepta.rb
+++ Casks/hepta.rb
@@ -1,12 +1,12 @@
 cask "hepta" do
   arch = Hardware::CPU.intel? ? "mac" : "arm64-mac"
 
-  version "0.161.0"
+  version "0.162.0"
 
   if Hardware::CPU.intel?
-    sha256 "91d0be1d64680a422dd1c2eb6a6ee015b824ed99abe99dff135c6cddedd1aca8"
+    sha256 "678fc87d944bfd27e1fa5aaeb01e46c39f30dc029af65e84128b42210a5f99f5"
   else
-    sha256 "f5d1fe8e4ccdcbdb24f4d4d58ae35081c4cc68a2c16879fa5a615c17cc4c1921"
+    sha256 "17bf485295a7b47f22b8e8f6f05a18a4177a61940be2958c744fd2962f13b24e"
   end
 
   url "https://github.com/heptameta/project-meta/releases/download/v#{version}/Hepta-#{version}-#{arch}.zip",
