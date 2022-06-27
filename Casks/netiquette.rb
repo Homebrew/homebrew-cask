77cfba5a41e8f10724f8ef27be4a2b0b01589723
i0ntempest <szf1234@me.com>
2022-06-27 17:51:48 -0400

    Update netiquette from 2.0.0 to 2.1.0

--- Casks/netiquette.rb
+++ Casks/netiquette.rb
@@ -1,6 +1,6 @@
 cask "netiquette" do
-  version "2.0.0"
-  sha256 "54b6ae3da6ffe7644077608083cc40939e4ec4bd8fd0436f4a12f6ec48a4a4fb"
+  version "2.1.0"
+  sha256 "e2bb8220a9216d6317735e1c67c90b56352449c520eac865f2e50db85f7b7a57"
 
   url "https://github.com/objective-see/Netiquette/releases/download/v#{version}/Netiquette_#{version}.zip",
       verified: "github.com/objective-see/"
