875a5c802fd27858049d5504a43386d8d51ce6ad
p-linnane <plinnanedev@gmail.com>
2022-06-28 12:13:50 -0700

    ferdium 6.0.0-nightly.80
    Update ferdium from 6.0.0-nightly.78 to 6.0.0-nightly.80
    
    Closes #126689.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/ferdium.rb
+++ Casks/ferdium.rb
@@ -1,12 +1,12 @@
 cask "ferdium" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  version "6.0.0-nightly.78"
+  version "6.0.0-nightly.80"
 
   if Hardware::CPU.intel?
-    sha256 "ea4175af55a953f650ad70dd09c6bef29d3c23836c24e7291bdd513cab8ef400"
+    sha256 "cd34a5399cae710c020c6fb4d0d3b767d49d49746312278617b59fcce7975e40"
   else
-    sha256 "7ee0c49e3fb74b3a1f16d3499a6e2b5caa3b104bee11fe6e715773c2a8fb59b3"
+    sha256 "1bb8f78bea195b7ba86e0975a4545316e0569cde031d48e00f601829629a8015"
   end
 
   url "https://github.com/ferdium/ferdium-app/releases/download/v#{version}/Ferdium-#{version}#{arch}.dmg",
