f82a0539e0fcd23dcaba004d5e166728438f14e1
Miccal Matthews <miccal.matthews@gmail.com>
2022-06-28 10:08:30 +0800

    Update and rename genesyscloud-archy.rb to archy.rb

--- Casks/archy.rb
+++ Casks/archy.rb
@@ -1,8 +1,9 @@
-cask "genesyscloud-archy" do
+cask "archy" do
   version "2.8.0"
   sha256 "8e5773e76e77d382153afae5111ce9795f0fa91e7bdeed6c5838226b93585501"
 
-  url "https://sdk-cdn.mypurecloud.com/archy/#{version}/archy-macos.zip", verified: "https://sdk-cdn.mypurecloud.com/archy/"
+  url "https://sdk-cdn.mypurecloud.com/archy/#{version}/archy-macos.zip",
+      verified: "sdk-cdn.mypurecloud.com/archy/"
   name "Archy"
   desc "YAML processor"
   homepage "https://developer.genesys.cloud/devapps/archy/"
