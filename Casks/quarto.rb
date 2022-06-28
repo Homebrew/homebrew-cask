d54a9ef52b951b7e3da4a4aa6880f233e5ba0d50
p-linnane <plinnanedev@gmail.com>
2022-06-28 12:52:32 -0700

    quarto 0.9.629
    Update quarto from 0.9.626 to 0.9.629
    
    Closes #126704.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/quarto.rb
+++ Casks/quarto.rb
@@ -1,6 +1,6 @@
 cask "quarto" do
-  version "0.9.626"
-  sha256 "96492fb461c290ab9ca6d4b89b679614a24890038a665c71509d9acd232526e8"
+  version "0.9.629"
+  sha256 "6beff6a3eb5b9e695ea78920742642115e39b8133a1208597ab028b06481c523"
 
   url "https://github.com/quarto-dev/quarto-cli/releases/download/v#{version}/quarto-#{version}-macos.pkg",
       verified: "github.com/quarto-dev/quarto-cli/"
