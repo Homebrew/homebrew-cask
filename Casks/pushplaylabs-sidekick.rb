1d5704998cf9fd41d49335e29e53ab2bc3c26ac2
p-linnane <plinnanedev@gmail.com>
2022-06-28 12:51:48 -0700

    pushplaylabs-sidekick 100.24.4.20852,8b362d0
    * Update pushplaylabs-sidekick from 100.24.3.20708 to 100.24.4.20854
    * Bump Intel
    
    Closes #126703.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/pushplaylabs-sidekick.rb
+++ Casks/pushplaylabs-sidekick.rb
@@ -3,11 +3,11 @@ cask "pushplaylabs-sidekick" do
   livecheck_folder = Hardware::CPU.intel? ? "mac" : "macm1"
 
   if Hardware::CPU.intel?
-    version "100.24.3.20706,03817c5"
-    sha256 "a1709c9f091ad5c53e0d319978620f98902ec7316c359bc4f0fc072419947a2b"
+    version "100.24.4.20852,8b362d0"
+    sha256 "8d1bb8f94c3ac3be6bf5e93fa4a6287a91e5fa39d472f4bac4bceee6a240fdeb"
   else
-    version "100.24.3.20708,885a1ed"
-    sha256 "79c53409bb0b8031def7315871fa5f15331a12441eca6121826cceb372a365e6"
+    version "100.24.4.20854,be44cdd"
+    sha256 "f154d7ec098220fa4660d25c8b90f9886037b127c655d772c5b6283e0699f52a"
   end
 
   url "https://fast-cdn.meetsidekick.com/builds/sidekick-mac-release-#{arch}-#{version.csv.first}-#{version.csv.second}-df.dmg"
