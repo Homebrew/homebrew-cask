fc906554812c86bd0308a17fe8968279c10df43d
p-linnane <plinnanedev@gmail.com>
2022-06-28 11:53:43 -0700

    Update bloodhound from 4.1.0 to 4.1.1

--- Casks/bloodhound.rb
+++ Casks/bloodhound.rb
@@ -1,6 +1,6 @@
 cask "bloodhound" do
-  version "4.1.0"
-  sha256 "88e73ed9922003522ffe4b79167dc818716adc62dc6c739f6c7422ae209bd233"
+  version "4.1.1"
+  sha256 "2c45a08d77284fedd8074a5664e7c2424a94c6d131540010da2e73d8f520ebe7"
 
   url "https://github.com/BloodHoundAD/BloodHound/releases/download/#{version}/BloodHound-darwin-x64.zip"
   name "bloodhound"
