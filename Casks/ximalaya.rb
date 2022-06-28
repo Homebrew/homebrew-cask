bd9923f562c507390f7aa373b61cfc902a637380
neutric <5984479+neutric@users.noreply.github.com>
2022-06-28 09:23:46 +0200

    Update ximalaya from 3.0.0 to 3.0.1

--- Casks/ximalaya.rb
+++ Casks/ximalaya.rb
@@ -1,6 +1,6 @@
 cask "ximalaya" do
-  version "3.0.0,1654661122"
-  sha256 "3b86078245565fcfb98d373e5a55732c77ee1c8cfabc39cb744a1bf4dd1c85f2"
+  version "3.0.1,1656297120"
+  sha256 "a8cffac06945ff8e4ef21b2611329ab9f5adfd481cf9bf509ba887f6dfc4396f"
 
   url "https://s1.xmcdn.com/yx/ximalaya-pc-client/#{version.csv.first}/download/mac-x64/Ximalaya_#{version.csv.first}_#{version.csv.second}_x64.dmg",
       verified: "s1.xmcdn.com/yx/ximalaya-pc-client/"
