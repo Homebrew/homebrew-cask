bd81769abbd16652eb48337e3c49abd2aa5a8c13
neutric <5984479+neutric@users.noreply.github.com>
2022-06-28 10:39:57 +0200

    Update clip-studio-paint from 1.12.1 to 1.12.3

--- Casks/clip-studio-paint.rb
+++ Casks/clip-studio-paint.rb
@@ -1,6 +1,6 @@
 cask "clip-studio-paint" do
-  version "1.12.1"
-  sha256 "c849af4f7120d713d0e5759b3825393c760cba25db563719ff3a1d9d75568692"
+  version "1.12.3"
+  sha256 "8bb6ca81070ae3a5fd948c218d4e59a90bbb758206c3b3f143feba9fb7083919"
 
   url "https://vd.clipstudio.net/clipcontent/paint/app/#{version.no_dots}/CSP_#{version.no_dots}m_app.pkg"
   name "Clip Studio Paint"
