66e04c3cc169c74d158f084713718dcd08956baa
Miccal Matthews <miccal.matthews@gmail.com>
2022-06-28 10:16:46 +0800

    Update weektodo.rb

--- Casks/weektodo.rb
+++ Casks/weektodo.rb
@@ -2,7 +2,7 @@ cask "weektodo" do
   version "1.8.0"
   sha256 "4a103e80ad590849ce10167016da1f3abb611918984d427e8e56c0f852ee75d9"
 
-  url "https://github.com/Zuntek/WeekToDoWeb/releases/download/v#{version}/WeekToDo-#{version}.dmg"
+  url "https://github.com/Zuntek/WeekToDoWeb/releases/download/v#{version}/WeekToDo-#{version}.dmg",
       verified: "github.com/Zuntek/WeekToDoWeb/"
   name "WeekToDo"
   desc "Weekly planner app focused on privacy"
