8408245178feda1b856f2cf6c5a1b9559151ee32
Steffen Schmidt <43960349+stsmdt@users.noreply.github.com>
2022-06-28 04:15:38 +0200

    Add genesyscloud archy v2.8.0 (#126610)
    * Add genesyscloud archy v2.8.0
    
    * Update genesyscloud-archy.rb
    
    * Update and rename genesyscloud-archy.rb to archy.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/archy.rb
@@ -0,0 +1,17 @@
+cask "archy" do
+  version "2.8.0"
+  sha256 "8e5773e76e77d382153afae5111ce9795f0fa91e7bdeed6c5838226b93585501"
+
+  url "https://sdk-cdn.mypurecloud.com/archy/#{version}/archy-macos.zip",
+      verified: "sdk-cdn.mypurecloud.com/archy/"
+  name "Archy"
+  desc "YAML processor"
+  homepage "https://developer.genesys.cloud/devapps/archy/"
+
+  livecheck do
+    url "https://sdk-cdn.mypurecloud.com/archy/versions.json"
+    regex(/"version"\s*:\s*"(\d+(?:\.\d+)+)"/i)
+  end
+
+  binary "archyBin/archy-macos-#{version}", target: "archy"
+end
