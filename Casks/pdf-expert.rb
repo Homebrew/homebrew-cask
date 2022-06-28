86e58498a5bcf18416f1184744fec301197dbf44
Reno <singularitti@outlook.com>
2022-06-28 15:08:58 -0400

    pdf-expert: rebuild
    Update pdf-expert from 2.5.21,761 to 2.5.21,761
    
    Closes #126682.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/pdf-expert.rb
+++ Casks/pdf-expert.rb
@@ -18,4 +18,17 @@ cask "pdf-expert" do
   depends_on macos: ">= :high_sierra"
 
   app "PDF Expert.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.readdle.pdfexpert-mac.sfl2",
+    "~/Library/Application Support/com.readdle.PDFExpert-Mac",
+    "~/Library/Application Support/PDF Expert",
+    "~/Library/Caches/com.readdle.PDFExpert-Installer",
+    "~/Library/Caches/com.readdle.PDFExpert-Mac",
+    "~/Library/HTTPStorages/com.readdle.PDFExpert-Installer",
+    "~/Library/HTTPStorages/com.readdle.PDFExpert-Mac.binarycookies",
+    "~/Library/HTTPStorages/com.readdle.PDFExpert-Mac",
+    "~/Library/PDF Expert",
+    "~/Library/Preferences/com.readdle.PDFExpert-Mac.plist",
+  ]
 end
