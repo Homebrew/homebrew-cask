79182395e305548b527eb834d41ff32e95d0edc3
p-linnane <plinnanedev@gmail.com>
2022-06-28 23:28:39 -0700

    mucommander: Fix livecheck (#126746)
    Fix livecheck

--- Casks/mucommander.rb
+++ Casks/mucommander.rb
@@ -2,11 +2,17 @@ cask "mucommander" do
   version "1.0.0-1"
   sha256 "7af739c4807202ce56dac95ab980ae0f3ca6abf06393b6ab68382cc67301bae9"
 
-  url "https://github.com/mucommander/mucommander/releases/download/#{version}/mucommander-#{version}.dmg",
+  url "https://github.com/mucommander/mucommander/releases/download/#{version}/muCommander-#{version}.dmg",
       verified: "github.com/mucommander/mucommander/"
   name "muCommander"
   desc "File manager with a dual-pane interface"
   homepage "https://www.mucommander.com/"
+
+  livecheck do
+    url "https://github.com/mucommander/mucommander/releases"
+    strategy :page_match
+    regex(/href=.*?mucommander[._-]v?(\d+(?:\.\d+)+-?\d?)\.dmg/i)
+  end
 
   app "muCommander.app"
 end
