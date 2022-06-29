fc6173dd5153c196b706ad9160f07b822d53ffde
p-linnane <plinnanedev@gmail.com>
2022-06-29 09:23:52 -0700

    webull 6.0.2,600802
    Fix livecheck & URL
    
    Closes #126778.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/webull.rb
+++ Casks/webull.rb
@@ -1,15 +1,15 @@
 cask "webull" do
-  dl_arch, livecheck_arch = Hardware::CPU.intel? ? ["us", "qt_mac_global"] : ["us_M1", "qt_m1_global"]
+  dl_arch, livecheck_arch = Hardware::CPU.intel? ? ["us_x64", "qt_mac_global"] : ["us_arm64", "qt_m1_global"]
 
-  version "6.0.1"
+  version "6.0.2,600802"
 
   if Hardware::CPU.intel?
-    sha256 "bf9d449d643b9b4af45fab4854cbbc87ea995fb5fd874837cd8dac2921be4990"
+    sha256 "01f3b0f215716f50a4caa93fe7f1ccdf3469bceeca10fd6cb9d1ab67d86f9658"
   else
-    sha256 "82351a2775cc6efb820ed93970b8ebf69fea0de951ed64659985bebc958be330"
+    sha256 "aadd015041696474ef3a80887fc8ecd89108736f2e49748a207fa51d86c6b84b"
   end
 
-  url "https://u1sweb.webullfinance.com/us/desktop/Webull%20Desktop%20#{version}_#{dl_arch}.dmg",
+  url "https://u1sweb.webullfinance.com/us/desktop/Webull%20Desktop_#{version.csv.first}_#{dl_arch}_signed_#{version.csv.second}.dmg",
       verified: "u1sweb.webullfinance.com/us/desktop/"
   name "Webull"
   desc "Desktop client for Webull Financial LLC"
@@ -17,12 +17,13 @@ cask "webull" do
 
   livecheck do
     url "https://infoapi.webullfintech.com/api/operation/appver/last?platform=#{livecheck_arch}&osv=10.15"
-    strategy :page_match do |page|
-      JSON.parse(page)["latestAppVersion"]
+    regex(/Webull%20Desktop[._-](\d+(?:\.\d+)+).*?[._-](\d+)\.dmg/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match.first},#{match.second}" }
     end
   end
 
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :mojave"
 
   app "Webull Desktop.app"
 
