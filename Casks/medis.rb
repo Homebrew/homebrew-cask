823a5c43bd43fc9424bbe16e36c8147338b492d5
Zihua Li <i@zihua.li>
2022-06-29 17:37:37 +0800

    medis 2.8.12
    * Update medis from 2.8.10 to 2.8.12
    * Add depends_on for Medis
    * Update medis.rb
    
    Closes #126755.
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/medis.rb
+++ Casks/medis.rb
@@ -1,6 +1,6 @@
 cask "medis" do
-  version "2.8.10"
-  sha256 "3c4c92bf4987b3f280c3a41766341b5dcd1e6fc1af87218dbc84c279e285ea05"
+  version "2.8.12"
+  sha256 "badfb6ae54dde6485d6b576cd3f9c64aa6f16940f904c2a851deb80d79816dbc"
 
   url "https://api.getmedis.com/medis-#{version}.zip"
   name "Medis"
@@ -11,6 +11,8 @@ cask "medis" do
     url "https://api.appcenter.ms/v0.1/public/sparkle/apps/d33d1248-5582-4304-b45c-cb40d51da2ab"
     strategy :sparkle, &:short_version
   end
+
+  depends_on macos: ">= :big_sur"
 
   app "Medis.app"
 
