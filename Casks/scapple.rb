57c81ebdede7c55352944bf7044854e1cda4d0ba
Reno <singularitti@outlook.com>
2022-06-29 15:26:54 -0400

    Update scapple from 1.4.1,8168 to 1.4.1,8168

--- Casks/scapple.rb
+++ Casks/scapple.rb
@@ -16,4 +16,14 @@ cask "scapple" do
   depends_on macos: ">= :catalina"
 
   app "Scapple.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.literatureandlatte.scapple.sfl2",
+    "~/Library/Application Support/Scapple",
+    "~/Library/Autosave Information/com.literatureandlatte.scapple.plist",
+    "~/Library/Caches/com.literatureandlatte.scapple",
+    "~/Library/HTTPStorages/com.literatureandlatte.scapple.binarycookies",
+    "~/Library/HTTPStorages/com.literatureandlatte.scapple",
+    "~/Library/Preferences/com.literatureandlatte.scapple.plist",
+  ]
 end
