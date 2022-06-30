16861d53a7185628a1ac7466789848fe31be7cb4
p-linnane <plinnanedev@gmail.com>
2022-06-30 16:59:34 -0700

    Update p4v from 2022.1 to 2022.2

--- Casks/p4v.rb
+++ Casks/p4v.rb
@@ -1,6 +1,6 @@
 cask "p4v" do
-  version "2022.1,2286077"
-  sha256 "f695a3e11c3b3386b891512a96f407d9f9b0ab280f6c365b2dbfc26019816b52"
+  version "2022.2,2304646"
+  sha256 "d72c017e516d37508e0433dcf94fa96e208781a24a161bb545eee3100cfb963b"
 
   url "https://cdist2.perforce.com/perforce/r#{version.major[-2..]}.#{version.minor}/bin.macosx1015x86_64/P4V.dmg"
   name "Perforce Helix Visual Client"
