2369a8b1860ae237d601c17c3fb451292e9574f4
Mike McQuaid <mike@mikemcquaid.com>
2022-06-30 08:34:48 +0100

    league-of-legends: brew style --fix
    For https://github.com/Homebrew/brew/pull/13477

--- Casks/league-of-legends.rb
+++ Casks/league-of-legends.rb
@@ -13,17 +13,17 @@ cask "league-of-legends" do
   uninstall delete: "/Applications/League of Legends.app"
 
   zap trash: [
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.riotgames.maccontainer.sfl*",
-    "~/Library/Application Support/Riot Games/Install League of Legends",
-    "~/Library/Application Support/Riot Games/League of Legends",
-    "~/Library/Caches/com.riotgames.LeagueofLegends.LeagueClient",
-    "~/Library/Logs/Riot Games/Install League of Legends na",
-    "~/Library/Preferences/com.riotgames.LeagueofLegends.LeagueClientUxHelper.plist",
-    "~/Library/Saved Application State/com.riotgames.LeagueofLegends.GameClient.savedState",
-    "~/Library/Saved Application State/com.riotgames.LeagueofLegends.LeagueClientUx.savedState",
-    "/Users/Shared/Riot Games/Metadata/league_of_legends.live.game_patch",
-    "/Users/Shared/Riot Games/Metadata/league_of_legends.live",
-  ],
+        "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.riotgames.maccontainer.sfl*",
+        "~/Library/Application Support/Riot Games/Install League of Legends",
+        "~/Library/Application Support/Riot Games/League of Legends",
+        "~/Library/Caches/com.riotgames.LeagueofLegends.LeagueClient",
+        "~/Library/Logs/Riot Games/Install League of Legends na",
+        "~/Library/Preferences/com.riotgames.LeagueofLegends.LeagueClientUxHelper.plist",
+        "~/Library/Saved Application State/com.riotgames.LeagueofLegends.GameClient.savedState",
+        "~/Library/Saved Application State/com.riotgames.LeagueofLegends.LeagueClientUx.savedState",
+        "/Users/Shared/Riot Games/Metadata/league_of_legends.live.game_patch",
+        "/Users/Shared/Riot Games/Metadata/league_of_legends.live",
+      ],
       rmdir: [
         "~/Documents/League of Legends",
         "~/Library/Application Support/Riot Games",
