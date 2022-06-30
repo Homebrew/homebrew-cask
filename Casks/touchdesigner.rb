015a2fd25f2dced38aa37c4551893444d109f0c0
p-linnane <plinnanedev@gmail.com>
2022-06-29 20:09:04 -0700

    Bump version & split arch

--- Casks/touchdesigner.rb
+++ Casks/touchdesigner.rb
@@ -1,18 +1,25 @@
 cask "touchdesigner" do
-  version "2022.25370"
-  sha256 "02952854ffa0f4d9c8f4a171e4a6f142dd48d1bc39129578959a129ddab67264"
+  arch = Hardware::CPU.intel? ? "intel" : "arm64"
 
-  url "https://download.derivative.ca/TouchDesigner.#{version}.intel.dmg"
+  version "2022.25370"
+
+  if Hardware::CPU.intel?
+    sha256 "cf0a32363378adda9caf2449de013885a7ee60e9501472fcdfa94817783063ce"
+  else
+    sha256 "02952854ffa0f4d9c8f4a171e4a6f142dd48d1bc39129578959a129ddab67264"
+  end
+
+  url "https://download.derivative.ca/TouchDesigner.#{version}.#{arch}.dmg"
   name "Derivative TouchDesigner"
   desc "Tool for creating dynamic digital art"
   homepage "https://derivative.ca/"
 
   livecheck do
-    url "https://derivative.ca/download/"
-    regex(/href=.*?TouchDesigner[._-]?v?(\d+(?:\.\d+)+)\.intel\.dmg/i)
+    url "https://derivative.ca/colorbox/download/release-download/39"
+    regex(/href=.*?TouchDesigner[._-]?v?(\d+(?:\.\d+)+)\.#{arch}\.dmg/i)
   end
 
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :mojave"
 
   app "TouchDesigner.app"
 end
