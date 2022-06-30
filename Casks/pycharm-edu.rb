fd2e7e5fecae03c12940db46f0913fb3c6b61786
Mike McQuaid <mike@mikemcquaid.com>
2022-06-30 08:34:57 +0100

    pycharm-edu: brew style --fix
    For https://github.com/Homebrew/brew/pull/13477

--- Casks/pycharm-edu.rb
+++ Casks/pycharm-edu.rb
@@ -31,8 +31,7 @@ cask "pycharm-edu" do
 
   uninstall_postflight do
     ENV["PATH"].split(File::PATH_SEPARATOR).map { |path| File.join(path, "charm") }.each do |path|
-      if File.exist?(path) &&
-         File.readlines(path).grep(/# see com.intellij.idea.SocketLock for the server side of this interface/).any?
+      if File.readlines(path).grep(/# see com.intellij.idea.SocketLock for the server side of this interface/).any?
         File.delete(path)
       end
     end
