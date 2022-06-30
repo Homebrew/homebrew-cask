5707e4a795568351364c994184c489b79fa7e00a
Mike McQuaid <mike@mikemcquaid.com>
2022-06-30 08:34:39 +0100

    detail: brew style --fix
    For https://github.com/Homebrew/brew/pull/13477

--- Casks/detail.rb
+++ Casks/detail.rb
@@ -19,25 +19,25 @@ cask "detail" do
   pkg "Detail.pkg"
 
   uninstall pkgutil: [
-    "co.detail.DAL",
-    "co.detail.HAL",
-    "co.detail.mac",
-  ],
+              "co.detail.DAL",
+              "co.detail.HAL",
+              "co.detail.mac",
+            ],
             delete:  [
               "/Library/CoreMediaIO/Plug-Ins/DAL/Detail Camera.plugin",
               "/Library/Audio/Plug-Ins/HAL/Detail Audio.driver",
             ]
 
   zap trash: [
-    "~/Library/Application Support/co.detail.mac",
-    "~/Library/Application Support/Detail",
-    "~/Library/Caches/co.detail.mac",
-    "~/Library/Caches/com.plausiblelabs.crashreporter.data/co.detail.mac",
-    "~/Library/co.detail.library",
-    "~/Library/HTTPStorages/co.detail.mac",
-    "~/Library/Preferences/co.detail.mac.plist",
-    "~/Library/Saved Application State/co.detail.mac.savedState",
-    "~/Library/WebKit/co.detail.mac",
-  ],
+        "~/Library/Application Support/co.detail.mac",
+        "~/Library/Application Support/Detail",
+        "~/Library/Caches/co.detail.mac",
+        "~/Library/Caches/com.plausiblelabs.crashreporter.data/co.detail.mac",
+        "~/Library/co.detail.library",
+        "~/Library/HTTPStorages/co.detail.mac",
+        "~/Library/Preferences/co.detail.mac.plist",
+        "~/Library/Saved Application State/co.detail.mac.savedState",
+        "~/Library/WebKit/co.detail.mac",
+      ],
       rmdir: "~/Documents/Detail"
 end
