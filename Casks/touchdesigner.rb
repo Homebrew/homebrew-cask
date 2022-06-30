f5f47009276b7dc2ccdefdbb671b19dfdd6a1bff
p-linnane <plinnanedev@gmail.com>
2022-06-29 20:27:25 -0700

    Fix hashes

--- Casks/touchdesigner.rb
+++ Casks/touchdesigner.rb
@@ -6,7 +6,7 @@ cask "touchdesigner" do
   if Hardware::CPU.intel?
     sha256 "02952854ffa0f4d9c8f4a171e4a6f142dd48d1bc39129578959a129ddab67264"
   else
-    sha256 "02952854ffa0f4d9c8f4a171e4a6f142dd48d1bc39129578959a129ddab67264"
+    sha256 "2cb17ea355544842bc30f08c5a15673a04c019688b731973583ac55afa3a65ea"
   end
 
   url "https://download.derivative.ca/TouchDesigner.#{version}.#{arch}.dmg"
