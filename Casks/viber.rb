b17c2b38d0f500fa19f00b8a77daee54f1451529
Mike McQuaid <mike@mikemcquaid.com>
2022-06-30 08:35:07 +0100

    viber: brew style --fix
    For https://github.com/Homebrew/brew/pull/13477

--- Casks/viber.rb
+++ Casks/viber.rb
@@ -18,14 +18,14 @@ cask "viber" do
   app "Viber.app"
 
   zap trash: [
-    "~/Library/Application Scripts/com.viber.osx.macvibershare",
-    "~/Library/Application Support/com.viber.osx",
-    "~/Library/Application Support/ViberPC",
-    "~/Library/Caches/Viber Media S.à r.l",
-    "~/Library/Caches/com.viber.osx",
-    "~/Library/Containers/com.viber.osx.macvibershare",
-    "~/Library/Preferences/com.viber.*.plist",
-    "~/Library/Saved Application State/com.viber.osx.savedState",
-  ],
+        "~/Library/Application Scripts/com.viber.osx.macvibershare",
+        "~/Library/Application Support/com.viber.osx",
+        "~/Library/Application Support/ViberPC",
+        "~/Library/Caches/Viber Media S.à r.l",
+        "~/Library/Caches/com.viber.osx",
+        "~/Library/Containers/com.viber.osx.macvibershare",
+        "~/Library/Preferences/com.viber.*.plist",
+        "~/Library/Saved Application State/com.viber.osx.savedState",
+      ],
       rmdir: "~/Documents/ViberDownloads"
 end
