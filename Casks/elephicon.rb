77ab02e6f7401f11cc0430f9fc6fb567e402fca2
p-linnane <plinnanedev@gmail.com>
2022-06-30 16:39:07 -0700

    Update elephicon from 1.9.10 to 1.9.11

--- Casks/elephicon.rb
+++ Casks/elephicon.rb
@@ -1,12 +1,12 @@
 cask "elephicon" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "1.9.10"
+  version "1.9.11"
 
   if Hardware::CPU.intel?
-    sha256 "e0bd45d34a517a935efb496f50646e91a6200a5779fde3cf7d24cf52d4833097"
+    sha256 "0fdfd918643be4e3585c316ec4db8549879b301473b3b1ef60d5f9af2f79c213"
   else
-    sha256 "5583a2ceb121a6a492b6b3fddaf6067bf7ec3c8e8e4aa13e4544e916b6eb78f5"
+    sha256 "20b92f26e3911659fce37905e8a042a4651ccf8f51a6b3136faf30beed1d2855"
   end
 
   url "https://github.com/sprout2000/elephicon/releases/download/v#{version}/Elephicon-#{version}-darwin-#{arch}.zip"
