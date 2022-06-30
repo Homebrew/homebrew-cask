9c86a417c600a88228e6fcdfa9f6dcdcd38a8bf1
p-linnane <plinnanedev@gmail.com>
2022-06-30 12:40:06 -0700

    Fix completions

--- Casks/docker.rb
+++ Casks/docker.rb
@@ -33,15 +33,15 @@ cask "docker" do
   ]
 
   app "Docker.app"
-  binary "#{appdir}/Docker.app/Contents/Resources/etc/docker",
+  binary "#{appdir}/Docker.app/Contents/Resources/etc/docker.bash-completion",
          target: "#{HOMEBREW_PREFIX}/etc/bash_completion.d/docker"
   binary "#{appdir}/Docker.app/Contents/Resources/etc/docker-compose.bash-completion",
          target: "#{HOMEBREW_PREFIX}/etc/bash_completion.d/docker-compose"
-  binary "#{appdir}/Docker.app/Contents/Resources/etc/_docker",
+  binary "#{appdir}/Docker.app/Contents/Resources/etc/docker.zsh-completion",
          target: "#{HOMEBREW_PREFIX}/share/zsh/site-functions/_docker"
   binary "#{appdir}/Docker.app/Contents/Resources/etc/docker-compose.zsh-completion",
          target: "#{HOMEBREW_PREFIX}/share/zsh/site-functions/_docker_compose"
-  binary "#{appdir}/Docker.app/Contents/Resources/etc/docker.fish",
+  binary "#{appdir}/Docker.app/Contents/Resources/etc/docker.fish-completion",
          target: "#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/docker.fish"
   binary "#{appdir}/Docker.app/Contents/Resources/etc/docker-compose.fish-completion",
          target: "#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/docker-compose.fish"
@@ -61,12 +61,6 @@ cask "docker" do
               "/usr/local/bin/kubectl",
               "/usr/local/bin/notary",
               "/usr/local/bin/vpnkit",
-              "#{HOMEBREW_PREFIX}/share/zsh/site-functions/_docker",
-              "#{HOMEBREW_PREFIX}/share/zsh/site-functions/_docker_compose",
-              "#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/docker.fish",
-              "#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/docker-compose.fish",
-              "#{HOMEBREW_PREFIX}/etc/bash_completion.d/docker",
-              "#{HOMEBREW_PREFIX}/etc/bash_completion.d/docker-compose",
             ],
             launchctl: [
               "com.docker.helper",
