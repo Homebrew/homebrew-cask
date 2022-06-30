5ee89fb6ea46c529542dee781e5e19db1180fb0f
Porkepix <porkepix@gmail.com>
2022-06-30 11:54:16 +0200

    Update electerm from 1.21.88 to 1.21.91

--- Casks/electerm.rb
+++ Casks/electerm.rb
@@ -1,12 +1,12 @@
 cask "electerm" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "1.21.88"
+  version "1.21.91"
 
   if Hardware::CPU.intel?
-    sha256 "2e8a6c53a32d7e1060289980e3b7f0501ca53bc5ea38db366dc0bf168eecc21f"
+    sha256 "fbf13263d56f62eb993921729b496c83708dcdb900b7e571f00e8069188534c3"
   else
-    sha256 "1b5ab2aa52a9ed42f8b4d695524ca393e5838869c7f4ecc0742997ccc19dd1a7"
+    sha256 "6b8a83c5eec2df9b2661c596648340cac500e1cdf782907ec46ac83ea6e97183"
   end
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac-#{arch}.dmg"
