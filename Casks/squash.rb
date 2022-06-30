ac676a2729916a93cd488961ead0d1a6a7b0da31
Jacob Ford <j@cobford.com>
2022-06-30 14:40:10 -0400

    Add verified CDN URL

--- Casks/squash.rb
+++ Casks/squash.rb
@@ -2,7 +2,8 @@ cask "squash" do
   version "3.0,826"
   sha256 "e2560c7c89a3cd2fd1b87c34955cde8b520f02bb4866a1dd88a8837e1574a936"
 
-  url "https://dl.devant-cdn.io/v1/app/4ad73d1f-7ab7-4f7f-b9df-8d2d906ef718/Squash-#{version.csv.second}.zip/Squash.zip"
+  url "https://dl.devant-cdn.io/v1/app/4ad73d1f-7ab7-4f7f-b9df-8d2d906ef718/Squash-#{version.csv.second}.zip/Squash.zip",
+      verified: "dl.devant-cdn.io/v1/app/4ad73d1f-7ab7-4f7f-b9df-8d2d906ef718/"
   name "squash"
   desc "Batch image processor, resizer, and converter"
   homepage "https://www.realmacsoftware.com/squash/"
