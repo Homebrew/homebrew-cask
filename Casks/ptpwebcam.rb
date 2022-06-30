1c79c2b573ee978012e58740995e57371f07f0a2
Mike McQuaid <mike@mikemcquaid.com>
2022-06-30 08:34:57 +0100

    ptpwebcam: brew style --fix
    For https://github.com/Homebrew/brew/pull/13477

--- Casks/ptpwebcam.rb
+++ Casks/ptpwebcam.rb
@@ -16,13 +16,13 @@ cask "ptpwebcam" do
   pkg "PTP_Webcam-v#{version}.pkg"
 
   uninstall pkgutil:   [
-    "org.ptpwebcam.pkg.EnableChrome",
-    "org.ptpwebcam.pkg.EnableSkype",
-    "org.ptpwebcam.pkg.EnableTeams",
-    "org.ptpwebcam.pkg.EnableZoom",
-    "org.ptpwebcam.pkg.PTPWebcam",
-    "org.ptpwebcam.pkg.RemoveEOSWebcam",
-  ],
+              "org.ptpwebcam.pkg.EnableChrome",
+              "org.ptpwebcam.pkg.EnableSkype",
+              "org.ptpwebcam.pkg.EnableTeams",
+              "org.ptpwebcam.pkg.EnableZoom",
+              "org.ptpwebcam.pkg.PTPWebcam",
+              "org.ptpwebcam.pkg.RemoveEOSWebcam",
+            ],
             launchctl: [
               "org.ptpwebcam.PtpWebcamAgent",
               "org.ptpwebcam.PtpWebcamAssistant",
