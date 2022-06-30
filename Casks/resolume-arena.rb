2dd39f73c3aad8b286d21751eb7103c2745d7fef
Mike McQuaid <mike@mikemcquaid.com>
2022-06-30 08:34:59 +0100

    resolume-arena: brew style --fix
    For https://github.com/Homebrew/brew/pull/13477

--- Casks/resolume-arena.rb
+++ Casks/resolume-arena.rb
@@ -19,14 +19,14 @@ cask "resolume-arena" do
   pkg "Resolume Arena Installer.pkg"
 
   uninstall pkgutil:   [
-    "com.resolume.pkg.ResolumeArena.*",
-    "com.resolume.pkg.ResolumeDXV",
-    "com.resolume.pkg.ResolumeQuickLook",
-    "com.resolume.pkg.ResolumeAlley",
-    "com.resolume.pkg.ResolumeWireNodes",
-    "com.resolume.pkg.ResolumeCommon",
-    "com.resolume.pkg.ResolumeWire",
-  ],
+              "com.resolume.pkg.ResolumeArena.*",
+              "com.resolume.pkg.ResolumeDXV",
+              "com.resolume.pkg.ResolumeQuickLook",
+              "com.resolume.pkg.ResolumeAlley",
+              "com.resolume.pkg.ResolumeWireNodes",
+              "com.resolume.pkg.ResolumeCommon",
+              "com.resolume.pkg.ResolumeWire",
+            ],
             delete:    "/Applications/Resolume Arena #{version.major}",
             signal:    ["TERM", "com.resolume.arena"],
             launchctl: "com.resolume.arena"
