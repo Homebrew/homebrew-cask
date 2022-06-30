348fe73480f3d0c5ec29e1f4a08989977b4047d1
Mike McQuaid <mike@mikemcquaid.com>
2022-06-30 08:34:43 +0100

    github: brew style --fix
    For https://github.com/Homebrew/brew/pull/13477

--- Casks/github.rb
+++ Casks/github.rb
@@ -29,18 +29,18 @@ cask "github" do
   binary "#{appdir}/GitHub Desktop.app/Contents/Resources/app/static/github.sh", target: "github"
 
   zap trash: [
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.github.GitHubClient.sfl*",
-    "~/Library/Application Support/com.github.GitHubClient",
-    "~/Library/Application Support/com.github.GitHubClient.ShipIt",
-    "~/Library/Application Support/GitHub Desktop",
-    "~/Library/Application Support/ShipIt_stderr.log",
-    "~/Library/Application Support/ShipIt_stdout.log",
-    "~/Library/Caches/com.github.GitHubClient",
-    "~/Library/Caches/com.github.GitHubClient.ShipIt",
-    "~/Library/Logs/GitHub Desktop",
-    "~/Library/Preferences/ByHost/com.github.GitHubClient.ShipIt.*.plist",
-    "~/Library/Preferences/com.github.GitHubClient.helper.plist",
-    "~/Library/Preferences/com.github.GitHubClient.plist",
-  ],
+        "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.github.GitHubClient.sfl*",
+        "~/Library/Application Support/com.github.GitHubClient",
+        "~/Library/Application Support/com.github.GitHubClient.ShipIt",
+        "~/Library/Application Support/GitHub Desktop",
+        "~/Library/Application Support/ShipIt_stderr.log",
+        "~/Library/Application Support/ShipIt_stdout.log",
+        "~/Library/Caches/com.github.GitHubClient",
+        "~/Library/Caches/com.github.GitHubClient.ShipIt",
+        "~/Library/Logs/GitHub Desktop",
+        "~/Library/Preferences/ByHost/com.github.GitHubClient.ShipIt.*.plist",
+        "~/Library/Preferences/com.github.GitHubClient.helper.plist",
+        "~/Library/Preferences/com.github.GitHubClient.plist",
+      ],
       rmdir: "~/.config/git"
 end
