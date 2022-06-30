e3eeb80155c71fb9d7821ddfe2ad79b3526d08f9
Arik Hadas <arik.hadas1@gmail.com>
2022-06-30 09:24:54 +0300

    Update mucommander from 1.0.1-1 to 1.0.1-1 (#126862)

--- Casks/mucommander.rb
+++ Casks/mucommander.rb
@@ -1,6 +1,6 @@
 cask "mucommander" do
-  version "1.0.0-1"
-  sha256 "7af739c4807202ce56dac95ab980ae0f3ca6abf06393b6ab68382cc67301bae9"
+  version "1.0.1-1"
+  sha256 "1a2df75b5defa1b69ec2e4a2f9d50d3b8b9c9bcfc312d96dd807fefe807fa38f"
 
   url "https://github.com/mucommander/mucommander/releases/download/#{version}/muCommander-#{version}.dmg",
       verified: "github.com/mucommander/mucommander/"
