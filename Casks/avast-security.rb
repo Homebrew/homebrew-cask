5ee1cd039e5dbcace347f499f585c4387e7a41af
Mike McQuaid <mike@mikemcquaid.com>
2022-06-30 08:34:32 +0100

    avast-security: brew style --fix
    For https://github.com/Homebrew/brew/pull/13477

--- Casks/avast-security.rb
+++ Casks/avast-security.rb
@@ -16,10 +16,10 @@ cask "avast-security" do
   pkg "Install Avast Security.pkg"
 
   uninstall script:    {
-    executable:   "/Applications/Avast.app/Contents/Backend/hub/uninstall.sh",
-    must_succeed: false, # A non-0 exit code may be given even if the uninstall succeeds (https://github.com/Homebrew/homebrew-cask/issues/21740#issuecomment-224094946).
-    sudo:         true,
-  },
+              executable:   "/Applications/Avast.app/Contents/Backend/hub/uninstall.sh",
+              must_succeed: false, # A non-0 exit code may be given even if the uninstall succeeds (https://github.com/Homebrew/homebrew-cask/issues/21740#issuecomment-224094946).
+              sudo:         true,
+            },
             launchctl: [
               "com.avast.hub.schedule",
               "com.avast.hub.xpc",
