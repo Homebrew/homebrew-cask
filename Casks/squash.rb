31922d8bb0934809396542b0bfd16db04208b683
Jacob Ford <j@cobford.com>
2022-06-30 14:19:21 -0400

    squash 3.0,826 (new cask)
    * Cask basics
    * Add livecheck
    * Add zap (based solely on AppCleaner, ignoring Sparkle stuff)
    * Add verified CDN URL
    
    Closes #126906.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Casks/squash.rb
@@ -0,0 +1,24 @@
+cask "squash" do
+  version "3.0,826"
+  sha256 "e2560c7c89a3cd2fd1b87c34955cde8b520f02bb4866a1dd88a8837e1574a936"
+
+  url "https://dl.devant-cdn.io/v1/app/4ad73d1f-7ab7-4f7f-b9df-8d2d906ef718/Squash-#{version.csv.second}.zip/Squash.zip",
+      verified: "dl.devant-cdn.io/v1/app/4ad73d1f-7ab7-4f7f-b9df-8d2d906ef718/"
+  name "squash"
+  desc "Batch image processor, resizer, and converter"
+  homepage "https://www.realmacsoftware.com/squash/"
+
+  livecheck do
+    url "https://update.devant.io/v1/feed/4ad73d1f-7ab7-4f7f-b9df-8d2d906ef718/"
+    strategy :sparkle do |item|
+      "#{item.short_version},#{item.version}"
+    end
+  end
+
+  app "Squash.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.realmacsoftware.squash3",
+    "~/Library/Containers/com.realmacsoftware.squash3",
+  ]
+end
