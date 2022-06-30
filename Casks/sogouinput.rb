e805f6aae67419507c755abe878d0fd231f93119
Mike McQuaid <mike@mikemcquaid.com>
2022-06-30 08:35:02 +0100

    sogouinput: brew style --fix
    For https://github.com/Homebrew/brew/pull/13477

--- Casks/sogouinput.rb
+++ Casks/sogouinput.rb
@@ -23,24 +23,24 @@ cask "sogouinput" do
   installer manual: "sogou_mac_#{version.csv.first}.app"
 
   uninstall delete:    [
-    "/Library/Input Methods/SogouInput.app",
-    "/Library/QuickLook/SogouSkinFileQuickLook.qlgenerator",
-  ],
+              "/Library/Input Methods/SogouInput.app",
+              "/Library/QuickLook/SogouSkinFileQuickLook.qlgenerator",
+            ],
             launchctl: "com.sogou.SogouServices"
 
   zap trash: [
-    "~/.sogouinput",
-    "~/Library/Application Support/Sogou/EmojiPanel",
-    "~/Library/Application Support/Sogou/InputMethod",
-    "~/Library/Caches/com.sogou.inputmethod.sogou",
-    "~/Library/Caches/com.sogou.SGAssistPanel",
-    "~/Library/Caches/com.sogou.SogouPreference",
-    "~/Library/Caches/SogouServices",
-    "~/Library/Cookies/com.sogou.inputmethod.sogou.binarycookies",
-    "~/Library/Cookies/com.sogou.SogouPreference.binarycookies",
-    "~/Library/Cookies/SogouServices.binarycookies",
-    "~/Library/Preferences/com.sogou.SogouPreference.plist",
-    "~/Library/Saved Application State/com.sogou.SogouInstaller.savedState",
-  ],
+        "~/.sogouinput",
+        "~/Library/Application Support/Sogou/EmojiPanel",
+        "~/Library/Application Support/Sogou/InputMethod",
+        "~/Library/Caches/com.sogou.inputmethod.sogou",
+        "~/Library/Caches/com.sogou.SGAssistPanel",
+        "~/Library/Caches/com.sogou.SogouPreference",
+        "~/Library/Caches/SogouServices",
+        "~/Library/Cookies/com.sogou.inputmethod.sogou.binarycookies",
+        "~/Library/Cookies/com.sogou.SogouPreference.binarycookies",
+        "~/Library/Cookies/SogouServices.binarycookies",
+        "~/Library/Preferences/com.sogou.SogouPreference.plist",
+        "~/Library/Saved Application State/com.sogou.SogouInstaller.savedState",
+      ],
       rmdir: "~/Library/Application Support/Sogou"
 end
