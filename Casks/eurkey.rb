af803bc577c93acd921b85e0842143363b948421
Henning Pohlmeyer <hpohlmeyer@users.noreply.github.com>
2022-06-30 08:25:31 +0200

    Change EurKey source to up-to-date repo

--- Casks/eurkey.rb
+++ Casks/eurkey.rb
@@ -2,8 +2,8 @@ cask "eurkey" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/jonasdiemer/EurKEY-Mac/archive/master.zip",
-      verified: "github.com/jonasdiemer/EurKEY-Mac/"
+  url "https://github.com/lbschenkel/EurKEY-Mac/archive/master.zip",
+      verified: "github.com/lbschenkel/EurKEY-Mac"
   name "EurKEY keyboard layout"
   desc "Keyboard Layout for Europeans, Coders and Translators"
   homepage "https://eurkey.steffen.bruentjen.eu/"
