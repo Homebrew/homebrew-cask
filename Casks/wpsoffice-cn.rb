ed659ce3a7dbb6a4d27cea41d24e2c34da5b5d7f
p-linnane <patrick@linnane.io>
2022-07-01 11:13:46 -0700

    Update wpsoffice-cn from 4.4.0 to 4.4.1

--- Casks/wpsoffice-cn.rb
+++ Casks/wpsoffice-cn.rb
@@ -1,12 +1,12 @@
 cask "wpsoffice-cn" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "4.4.0,7333"
+  version "4.4.1,7380"
 
   if Hardware::CPU.intel?
-    sha256 "2d65ae71fe6476627b3b65f7e3f4d07919c16d5d3191e43c95b5a502151f502d"
+    sha256 "74afd52ab1030a4a9c7454ac1d2020ff83afdcd0e9460fb6a15b36e90433e9f7"
   else
-    sha256 "02987bdafa7dac3b374cb82cea7307663db6d112b24ef412687541fa45222025"
+    sha256 "5b073d4d4a549eea8039173b09c9fec93b690a2d0253c42acebffaff7f755435"
   end
 
   url "https://package.mac.wpscdn.cn/mac_wps_pkg/#{version.csv.first}/WPS_Office_#{version.csv.first}(#{version.csv.second})_#{arch}.dmg",
