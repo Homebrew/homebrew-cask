5a6824030be4e36b1885836de76c8f60848ef87b
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-07-02 12:08:32 +0800

    Update cloud189.rb

--- Casks/cloud189.rb
+++ Casks/cloud189.rb
@@ -1,9 +1,8 @@
 cask "cloud189" do
   version "1.1.0"
-  sha256 "d109d1f996654321b86ff014f5ab8cc86ec9a144518605e9889db0ed6922c935"
+  sha256 :no_check
 
-  url "https://download.cloud.189.cn/file/downloadFile.action?dlt=4&dt=1&expired=1766930486508&sk=297953828&ufi=61304212366059386&zyc=60&token=cloud3&sig=gfhWf6DndIlD28kChBJM1d48E18%3D",
-      verified: "download.cloud.189.cn"
+  url "https://download.cloud.189.cn/file/downloadFile.action?dlt=4&dt=1&expired=1766930486508&sk=297953828&ufi=61304212366059386&zyc=60&token=cloud3&sig=gfhWf6DndIlD28kChBJM1d48E18%3D"
   name "Cloud189"
   name "天翼云盘"
   desc "Public cloud storage service"
