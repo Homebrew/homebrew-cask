75af4b8af0e7e378d1ffff70d2897ce3347c00e1
p-linnane <patrick@linnane.io>
2022-07-02 15:46:23 -0700

    Update buttercup from 2.15.0 to 2.15.1

--- Casks/buttercup.rb
+++ Casks/buttercup.rb
@@ -1,6 +1,6 @@
 cask "buttercup" do
-  version "2.15.0"
-  sha256 "c20c25d4bff16c713ec3e3f9b8a5e1420a2ad55e6890c78a18bedf7d82bbd6fc"
+  version "2.15.1"
+  sha256 "64fc115def15267dc75be44bf42a92f9db1fec359a910caa0c3a73f1e670df06"
 
   url "https://github.com/buttercup/buttercup-desktop/releases/download/v#{version}/Buttercup-mac-x64-#{version}.dmg",
       verified: "github.com/buttercup/buttercup-desktop/"
