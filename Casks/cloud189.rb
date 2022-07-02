aabf7f1f4844391aed2a800ac39d965b9cd0d8d9
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-07-02 11:48:13 +0800

    cloud189 1.1.0 (new cask)
    * Create cloud189.rb
    * Update cloud189.rb
    * Update cloud189.rb
    
    Closes #126999.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Casks/cloud189.rb
@@ -0,0 +1,23 @@
+cask "cloud189" do
+  version "1.1.0"
+  sha256 :no_check
+
+  url "https://download.cloud.189.cn/file/downloadFile.action?dlt=4&dt=1&expired=1766930486508&sk=297953828&ufi=61304212366059386&zyc=60&token=cloud3&sig=gfhWf6DndIlD28kChBJM1d48E18%3D"
+  name "Cloud189"
+  name "天翼云盘"
+  desc "Public cloud storage service"
+  homepage "https://cloud.189.cn/web/static/download-client/index.html"
+
+  livecheck do
+    skip "No version information available"
+  end
+
+  app "天翼云盘.app"
+
+  uninstall quit: "com.cnki.cnkiexpress"
+
+  zap trash: [
+    "~/Library/Application Support/com.21cn.cloud-dist",
+    "~/Library/Preferences/21cn.cloud.dist.plist",
+  ]
+end
