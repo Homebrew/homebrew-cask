f51fc1bb90e38e15e1891451953a7d5e9f941374
Miccal Matthews <miccal.matthews@gmail.com>
2022-07-03 22:39:29 +0800

    Update bing-wallpaper.rb

--- Casks/bing-wallpaper.rb
+++ Casks/bing-wallpaper.rb
@@ -1,8 +1,8 @@
 cask "bing-wallpaper" do
-  version "1.0.9"
+  version "1.0.9,17"
   sha256 "b55b4d4533ed53f8d3e27229f8d414aa2a780f6fb2354e384dc6be0237c9c14b"
 
-  url "https://download.microsoft.com/download/9/1/1/911276db-dcd3-4129-9639-375613697b11/Mac/Installer/#{version}/Bing/Flight1/MW011/Defaults/Bing%20Wallpaper.pkg"
+  url "https://download.microsoft.com/download/9/1/1/911276db-dcd3-4129-9639-375613697b11/Mac/Installer/#{version.csv.first}/Bing/Flight1/MW011/Defaults/Bing%20Wallpaper.pkg"
   name "Bing Wallpaper"
   desc "Use the Bing daily image as your wallpaper"
   homepage "https://bingwallpaper.microsoft.com/"
@@ -16,9 +16,13 @@ cask "bing-wallpaper" do
 
   pkg "Bing Wallpaper.pkg"
 
-  uninstall pkgutil: "com.microsoft.msbwpackage",
-            quit:    [
+  uninstall pkgutil:   "com.microsoft.msbwpackage",
+            quit:      [
               "com.microsoft.autoupdate2",
               "com.microsoft.msbwdefaults",
+            ],
+            launchctl: [
+              "com.microsoft.msbwapp",
+              "com.microsoft.msbwupdater",
             ]
 end
