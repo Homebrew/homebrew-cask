8f9589e0079a8b646e69a35ccd73818d28a8d848
Tse Kit Yam <tsekityam@users.noreply.github.com>
2022-07-03 12:02:54 +0800

    Add Bing Wallpaper v1.0.9

---
+++ Casks/bing-wallpaper.rb
@@ -0,0 +1,20 @@
+cask "bing-wallpaper" do
+  version "1.0.9"
+  sha256 "b55b4d4533ed53f8d3e27229f8d414aa2a780f6fb2354e384dc6be0237c9c14b"
+
+  url "https://download.microsoft.com/download/9/1/1/911276db-dcd3-4129-9639-375613697b11/Mac/Installer/#{version}/Bing/Flight1/MW011/Defaults/Bing%20Wallpaper.pkg"
+  name "Bing Wallpaper"
+  desc "Bing Wallpaper on macOS"
+  homepage "https://bingwallpaper.microsoft.com/"
+
+  pkg "Bing Wallpaper.pkg"
+
+  uninstall pkgutil: [
+              "com.microsoft.msbwpackage",
+            ],
+
+            quit:    [
+              "com.microsoft.autoupdate2",
+              "com.microsoft.msbwdefaults",
+            ]
+end
