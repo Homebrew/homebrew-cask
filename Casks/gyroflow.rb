c639f886df62d9c987b362af4d8b32b05effaff4
Daniil Kivenko <dkivenko@welltory.app>
2022-07-04 02:06:36 +0400

    add Gyroflow.app v1.1.0

---
+++ Casks/gyroflow.rb
@@ -0,0 +1,14 @@
+cask "gyroflow" do
+  version "1.1.0"
+  sha256 "21b0df549a5f2378464c83d1a7b4d55f7040b65e61db5fd37d876665fc666050"
+
+  url "https://github.com/gyroflow/gyroflow/releases/download/v#{version}/Gyroflow-mac-universal.dmg",
+      verified: "github.com/gyroflow/gyroflow/"
+  name "Gyroflow"
+  desc "Video stabilization using gyroscope data"
+  homepage "https://gyroflow.xyz/"
+
+  app "Gyroflow.app"
+
+  zap trash: "~/Library/Caches/Gyroflow/"
+end
