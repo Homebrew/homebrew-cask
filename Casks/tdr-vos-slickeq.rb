860b212f34bf97e9dd0c6cbf659ea0f8c5ccba6b
Tobias Fertig <tobias.fertig@fhws.de>
2022-07-03 16:24:41 +0200

    tdr-vos-slickeq 1.3.7 (new cask)

---
+++ Casks/tdr-vos-slickeq.rb
@@ -0,0 +1,22 @@
+cask "tdr-vos-slickeq" do
+  version "1.3.7"
+  sha256 "c663b854bfaa322d62c74ed287dfba845dd82a20b95fcaa64ed0771e123a0ab2"
+
+  url "https://www.tokyodawn.net/labs/SlickEQ/#{version}/TDR%20VOS%20SlickEQ.zip?x24775"
+  name "tdr-vos-slickeq"
+  desc "Mixing equalizer designed for musical flexibility and impeccable sound"
+  homepage "https://www.tokyodawn.net/tdr-vos-slickeq/"
+
+  livecheck do
+    skip "No version information available"
+  end
+
+  pkg "TDR VOS SlickEQ.pkg"
+
+  uninstall pkgutil: [
+    "com.TokyoDawnLabs.TDRVOSSlickEQ.VST3",
+    "com.TokyoDawnLabs.TDRVOSSlickEQ.VST",
+    "com.TokyoDawnLabs.TDRVOSSlickEQ.AAX",
+    "com.TokyoDawnLabs.TDRVOSSlickEQ.AU",
+  ]
+end
