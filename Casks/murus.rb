52e6c616d9d92e0ae4d8507963f6ea1e149f693d
Patrick Linnane <patrick@linnane.io>
2022-07-02 22:40:55 -0700

    murus: Fix URL & bump version (#127022)

--- Casks/murus.rb
+++ Casks/murus.rb
@@ -1,8 +1,8 @@
 cask "murus" do
-  version "2.3.2"
-  sha256 "7a1d3ef8961ae04c1ec3d8de2245fa88a4f5fc4e35c746f6ca80cd16f6171e23"
+  version "2.3.3"
+  sha256 "94b4fa108766fefa60c5d7f00b75790dcc08c17e324a19cd64ac5069f2141bc3"
 
-  url "https://github.com/TheMurusTeam/Murus/releases/download/v#{version}/Murus.#{version}.zip",
+  url "https://github.com/TheMurusTeam/Murus/releases/download/v#{version}/murus-#{version}.zip",
       verified: "github.com/TheMurusTeam/Murus/"
   name "Murus Firewall"
   desc "Firewall app"
