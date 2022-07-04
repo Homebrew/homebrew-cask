96369d0ce8121b51f486696bd340cc160a851a58
Ralph Rooding <ralph@ralphrooding.com>
2022-07-04 15:06:22 +0200

    Update yousician.rb
    Added zap

--- Casks/yousician.rb
+++ Casks/yousician.rb
@@ -8,4 +8,15 @@ cask "yousician" do
   homepage "https://yousician.com/"
 
   app "Yousician Launcher.app"
+  
+  zap trash: [
+    "~/Library/Application Support/unity.Yousician.Yousician",
+    "~/Library/Caches/com.yousician.launcher",
+    "~/Library/HTTPStorages/com.yousician.launcher",
+    "~/Library/HTTPStorages/com.yousician.launcher.binarycookies",
+    "~/Library/HTTPStorages/unity.Yousician.Yousician",
+    "~/Library/Logs/Yousician",
+    "~/Library/Preferences/unity.Yousician.Yousician.plist",
+    "~/Library/Saved Application State/com.yousician.launcher.savedState",
+  ]
 end
