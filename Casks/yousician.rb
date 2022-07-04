ef690525c6f10866d2d622ad7d7e50fcca38aafe
Ralph Rooding <ralph@ralphrooding.com>
2022-07-04 13:46:35 +0200

    Added a cask for Yousician

---
+++ Casks/yousician.rb
@@ -0,0 +1,11 @@
+cask "yousician" do
+  version :latest
+  sha256 :no_check
+
+  url "https://public.yousician.com/Builds/Yousician.dmg"
+  name "Yousician"
+  desc "Is a fun way to learn the guitar, piano, bass, ukulele or singing"
+  homepage "https://yousician.com/"
+
+  app "Yousician Launcher.app"
+end
