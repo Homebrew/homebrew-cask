2b5459e492e490d452981d730ca059a5015e4909
p-linnane <patrick@linnane.io>
2022-07-04 10:27:22 -0700

    Update syncovery from 9.47v to 9.47w

--- Casks/syncovery.rb
+++ Casks/syncovery.rb
@@ -1,12 +1,12 @@
 cask "syncovery" do
   arch = Hardware::CPU.intel? ? "" : "-Apple"
 
-  version "9.47v"
+  version "9.47w"
 
   if Hardware::CPU.intel?
-    sha256 "e377e440ba97738ad98643e934b7945b14bc3eba13082c93637ac3ea1c069298"
+    sha256 "51dde7d0d28d90167eea9696a021a7adc153c6b730678b9a11bbd4b05f129ac0"
   else
-    sha256 "586aa8154077bdbc54a5b36d9d6c50d9ec508492b733bf7472293cb7a878b9bb"
+    sha256 "402861d78da8902888f1bedaaa790bf0d2e10876c1fd060854f096a8fda02427"
   end
 
   url "https://www.syncovery.com/release/SyncoveryMac#{version}#{arch}.dmg"
