5f382cf840d3d1226ded2e5a6cb56e6d21e52eea
neutric <5984479+neutric@users.noreply.github.com>
2022-07-04 14:59:01 +0200

    valentina-studio 12.4.4
    Update valentina-studio from 12.4.3 to 12.4.4
    
    Closes #127090.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/valentina-studio.rb
+++ Casks/valentina-studio.rb
@@ -1,6 +1,6 @@
 cask "valentina-studio" do
-  version "12.4.3"
-  sha256 "2c3612ddb9fc13007681271665ac3fcf5320fa8556f299213fca5e68e274837c"
+  version "12.4.4"
+  sha256 "db84eed207d4a9967fd65add66aafd3b15c03eb302e9ca0279436a1c27577310"
 
   url "https://valentina-db.com/download/prev_releases/#{version}/mac_64/vstudio_x64_#{version.major}_mac.dmg"
   name "Valentina Studio"
