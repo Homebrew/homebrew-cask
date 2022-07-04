737c60ff8067406dea1245504830acbd418943b0
neutric <5984479+neutric@users.noreply.github.com>
2022-07-04 15:53:26 +0200

    obs-websocket: update url and homepage following redirect

--- Casks/obs-websocket.rb
+++ Casks/obs-websocket.rb
@@ -2,10 +2,10 @@ cask "obs-websocket" do
   version "5.0.0"
   sha256 "f7cb0dfc2a9cfa2cdd9793ec28ee9fdab79d6a4fe339a95a2a602133bc5cd6ad"
 
-  url "https://github.com/Palakis/obs-websocket/releases/download/#{version}/obs-websocket-#{version}-macOS.pkg"
+  url "https://github.com/obsproject/obs-websocket/releases/download/#{version}/obs-websocket-#{version}-macOS.pkg"
   name "obs-websocket"
   desc "Remote-control OBS Studio through WebSockets"
-  homepage "https://github.com/Palakis/obs-websocket"
+  homepage "https://github.com/obsproject/obs-websocket"
 
   livecheck do
     url :url
