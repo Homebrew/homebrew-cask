8d765002fe770507b83f15512205851533879277
Jared Grove <jared@jaredgrove.com>
2022-07-05 03:58:35 -0400

    add Gamma Control.app 6.3.3

---
+++ Casks/gamma-control.rb
@@ -0,0 +1,13 @@
+cask "gamma-control" do
+    version "6.3.3"
+    sha256 "81729e238e39666f1c6eda8f9c4476fbc02c81f394c4003a184aa41fdb9b87ea"
+  
+    url "https://littoral.michelf.ca/apps/gamma-control/gamma-control-#{version}.zip"
+    appcast "https://michelf.ca/projects/gamma-control/version-history/"
+    name "Gamma Control"
+    desc "Per-screen color adjustments"
+    homepage "https://michelf.ca/projects/gamma-control/"
+  
+    app "Gamma Control.app"
+  end
+  
