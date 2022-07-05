2b39eb7efee2b84693d5f625879e3968b7c77894
Jared Grove <jared@jaredgrove.com>
2022-07-05 04:10:22 -0400

    add livecheck, rm appcast

--- Casks/gamma-control.rb
+++ Casks/gamma-control.rb
@@ -3,10 +3,15 @@ cask "gamma-control" do
     sha256 "81729e238e39666f1c6eda8f9c4476fbc02c81f394c4003a184aa41fdb9b87ea"
   
     url "https://littoral.michelf.ca/apps/gamma-control/gamma-control-#{version}.zip"
-    appcast "https://michelf.ca/projects/gamma-control/version-history/"
     name "Gamma Control"
     desc "Per-screen color adjustments"
     homepage "https://michelf.ca/projects/gamma-control/"
+  
+    livecheck do
+      url :homepage
+      strategy :page_match
+      regex(%r{href=.*?/gamma-control-(\d+(?:\.\d+)*)\.zip}i)
+    end
   
     app "Gamma Control.app"
   end
