7ba00760b508067588493a52edb09dd973a82f37
p-linnane <patrick@linnane.io>
2022-07-05 09:55:15 -0700

    wifi-explorer-pro 3.4.7,26
    Update wifi-explorer-pro from 3.4.6 to 3.4.7
    
    Closes #127186.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/wifi-explorer-pro.rb
+++ Casks/wifi-explorer-pro.rb
@@ -1,6 +1,6 @@
 cask "wifi-explorer-pro" do
-  version "3.4.6,25"
-  sha256 "6863a0d39d3afef55b7b8e369a7e07b348e864c62e9d7aa6888a4749606841ee"
+  version "3.4.7,26"
+  sha256 "2a31418915ae525e10958bb39d0a4cc4867d2c53aca1d8ef92c35c38fd491355"
 
   url "https://www.intuitibits.com/downloads/WiFiExplorerPro_#{version.csv.first}.pkg"
   name "WiFi Explorer Pro"
