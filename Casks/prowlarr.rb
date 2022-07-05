71227c8a15b7bc4ba9d2c4f63bafa58af5911bae
neutric <5984479+neutric@users.noreply.github.com>
2022-07-05 11:56:10 +0200

    prowlarr 0.4.2.1879
    Update prowlarr from 0.4.1.1838 to 0.4.2.1879
    
    Closes #127154.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/prowlarr.rb
+++ Casks/prowlarr.rb
@@ -1,12 +1,12 @@
 cask "prowlarr" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "0.4.1.1838"
+  version "0.4.2.1879"
 
   if Hardware::CPU.intel?
-    sha256 "efd651667fce5febedb2c7786081531fb5ff8b712718aed0b8c65b67fe27ef49"
+    sha256 "cd7a2870c65c1028b1ab3bcb036c654d42d8629d8f13e2ef786c9b93e1d82a15"
   else
-    sha256 "3619d00df38a87456b78af09ec8ea033680b26dab9f9fb478f6dcc33f7e19b41"
+    sha256 "256279aebd7b293c7f1dbb8335574a7dd518e76da077a0885361b4b8b8bf0d0b"
   end
 
   url "https://github.com/Prowlarr/Prowlarr/releases/download/v#{version}/Prowlarr.develop.#{version}.osx-app-core-#{arch}.zip",
