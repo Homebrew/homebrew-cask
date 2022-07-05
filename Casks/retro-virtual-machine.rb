4a9d3cf9e780ce9a02585e5fcf4415e0e177d940
p-linnane <patrick@linnane.io>
2022-07-04 22:40:08 -0700

    Adjust URL & switch to HTTP

--- Casks/retro-virtual-machine.rb
+++ Casks/retro-virtual-machine.rb
@@ -2,14 +2,14 @@ cask "retro-virtual-machine" do
   version "2.0,1.r7"
   sha256 "75e94f2df589ead3fb1eab529713312a17dc16e6b2ba547594cd9d5975def566"
 
-  url "https://retrovirtualmachine.ams3.digitaloceanspaces.com/release/beta#{version.csv.second.major}/macos/RetroVirtualMachine.#{version.csv.first}.beta-#{version.csv.second}.macos.dmg",
-      verified: "retrovirtualmachine.ams3.digitaloceanspaces.com/"
+  url "https://static.retrovm.org/release/beta#{version.csv.second.major}/macos/RetroVirtualMachine.#{version.csv.first}.beta-#{version.csv.second}.macos.dmg",
+      verified: "static.retrovm.org/release/"
   name "Retro Virtual Machine"
   desc "ZX Spectrum and Amstrad CPC emulator"
-  homepage "https://www.retrovirtualmachine.org/en/"
+  homepage "http://www.retrovirtualmachine.org/en/"
 
   livecheck do
-    url "https://www.retrovirtualmachine.org/en/downloads"
+    url "http://www.retrovirtualmachine.org/en/downloads"
     strategy :page_match do |page|
       match = page.match(/RetroVirtualMachine\.(\d+(?:\.\d+)*)\.beta-(\d+(?:\..\d+)*)\.macos\.dmg/i)
       next if match.blank?
