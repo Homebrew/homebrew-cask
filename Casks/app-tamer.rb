b8d9aa5b128a65b40876169193b3478f82f44267
p-linnane <patrick@linnane.io>
2022-07-04 21:50:35 -0700

    app-tamer 2.7.3,11460
    Update app-tamer from 2.7.2 to 2.7.3
    
    Closes #127137.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/app-tamer.rb
+++ Casks/app-tamer.rb
@@ -1,6 +1,6 @@
 cask "app-tamer" do
-  version "2.7.2,11413"
-  sha256 "a8b497a498dedd075a8261ae59eefb6b1b50cfcfd04dd6ad66f2538bce50bbc3"
+  version "2.7.3,11460"
+  sha256 "c9a1ddb1e2f49238f1fc74a628c0502925ac0147dbb0bffc1ad5eb4e1c9427ee"
 
   url "https://www.stclairsoft.com/download/AppTamer-#{version.csv.first}.dmg"
   name "AppTamer"
