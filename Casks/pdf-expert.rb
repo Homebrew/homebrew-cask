95b584a467613d97077f465503c76726d9cae12f
ShredderWalnut <34428332+ShredderWalnut@users.noreply.github.com>
2022-07-05 23:57:06 +0800

    Revert "Update pdf-expert.rb"
    This reverts commit ca9ecff22ef2210eb5305ed7f05f05056bd0a371.

--- Casks/pdf-expert.rb
+++ Casks/pdf-expert.rb
@@ -1,14 +1,15 @@
 cask "pdf-expert" do
-  version "3.0.23,844"
-  sha256 "23c3ce60f27b36edfbe5727a7e4c17d737ce2df1aa17b735924cff1443adebb6"
+  version "2.5.21,761"
+  sha256 "7906dc0b154c928a2b6c5c341336a1fec5f9c6ba059de08b8c91eca160f94ff6"
 
-  url "https://downloads.pdfexpert.com/pem#{version.major}/versions/#{version.after_comma}/PDFExpert.zip",
+  url "https://d1ke680phyeohy.cloudfront.net/versions/#{version.csv.second}/PDFExpert.dmg",
+      verified: "d1ke680phyeohy.cloudfront.net/versions/"
   name "PDF Expert"
   desc "PDF reader, editor and annotator"
   homepage "https://pdfexpert.com/"
 
   livecheck do
-    url "https://downloads.pdfexpert.com/paid/appcast.xml"
+    url "https://d1ke680phyeohy.cloudfront.net/release/appcast.xml"
     strategy :sparkle
   end
 
