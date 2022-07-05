1b1571e6654ad6f222b9b4768663b938e6b3e328
Jared Grove <jared@jaredgrove.com>
2022-07-05 04:24:00 -0400

    fix trailing whitespace

--- Casks/gamma-control.rb
+++ Casks/gamma-control.rb
@@ -1,18 +1,17 @@
 cask "gamma-control" do
     version "6.3.3"
     sha256 "81729e238e39666f1c6eda8f9c4476fbc02c81f394c4003a184aa41fdb9b87ea"
-  
+
     url "https://littoral.michelf.ca/apps/gamma-control/gamma-control-#{version}.zip"
     name "Gamma Control"
     desc "Per-screen color adjustments"
     homepage "https://michelf.ca/projects/gamma-control/"
-  
+
     livecheck do
       url :homepage
       strategy :page_match
       regex(%r{href=.*?/gamma-control-(\d+(?:\.\d+)*)\.zip}i)
     end
-  
+
     app "Gamma Control.app"
-  end
-  
+end
