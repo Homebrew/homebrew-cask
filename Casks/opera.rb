979547fe6fed54d8895235efaa4231e673ef97e2
Lex.Chen <ctyytc@gmail.com>
2022-07-05 23:23:59 +0800

    opera 88.0.4412.74
    Update opera from 88.0.4412.27 to 88.0.4412.74
    
    Closes #127160.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask "opera" do
-  version "88.0.4412.53"
-  sha256 "96202123f8fe0203a9c430a61fcad6a4bd0ed6d5c6f409b3f5eae5446c816165"
+  version "88.0.4412.74"
+  sha256 "15d391d24d72c37df19702bd50cefb4cf113e18a1d367a105984b439b6bacce8"
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   name "Opera"
