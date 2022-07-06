c10def2a5596dadab5a4712f1a4864499c616387
Yaroslav Slepukhin <yaroslavslepukhin@gmail.com>
2022-07-06 14:06:53 +0400

    set correct pkg filename for mac arm

--- Casks/ringcentral.rb
+++ Casks/ringcentral.rb
@@ -1,5 +1,8 @@
 cask "ringcentral" do
+  arch = Hardware::CPU.intel? ? "" : "-arm"
+
   version "22.2.22"
+
   if Hardware::CPU.intel?
     url "https://app.ringcentral.com/download/RingCentral.pkg"
   else
@@ -16,7 +19,7 @@ cask "ringcentral" do
     strategy :electron_builder
   end
 
-  pkg "RingCentral.pkg"
+  pkg "RingCentral#{arch}.pkg"
 
   uninstall delete:  "/Applications/RingCentral.app",
             quit:    "RingCentral",
