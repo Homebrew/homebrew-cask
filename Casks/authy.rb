efb0558b84be27e3afdbbe87b97096184423dc02
owine <owine@users.noreply.github.com>
2022-07-05 20:02:05 -0500

    Update authy from 2.2.0 to 2.2.1 (#127159)

--- Casks/authy.rb
+++ Casks/authy.rb
@@ -1,6 +1,6 @@
 cask "authy" do
-  version "2.2.0"
-  sha256 "80492691069b092dbaaec229249a3dfb1d9dcd27fe47b83913782be4dfc35a8c"
+  version "2.2.1"
+  sha256 "88663f7e83cec5a39c4336df9fb395b30447431c8902d0769211f1e31006d2db"
 
   url "https://authy-electron-repository-production.s3.amazonaws.com/authy/stable/#{version}/darwin/x64/Authy%20Desktop-#{version}.dmg",
       verified: "authy-electron-repository-production.s3.amazonaws.com/"
