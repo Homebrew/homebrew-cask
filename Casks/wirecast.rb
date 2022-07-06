95890823dbc26875c3e2f8c7bfdb7784939f8d48
Miccal Matthews <miccal.matthews@gmail.com>
2022-07-06 09:36:22 +0800

    Update wirecast.rb

--- Casks/wirecast.rb
+++ Casks/wirecast.rb
@@ -17,17 +17,17 @@ cask "wirecast" do
   app "Wirecast.app"
 
   uninstall delete: [
-              "/Library/LaunchDaemons/net.telestream.LicensingHelper.plist",
-              "/Library/Preferences/net.telestream.wirecast.plist",
-              "/Library/PriviledgedHelperTools/net.telestream.LicensingHelper",
-            ]
+    "/Library/LaunchDaemons/net.telestream.LicensingHelper.plist",
+    "/Library/Preferences/net.telestream.wirecast.plist",
+    "/Library/PriviledgedHelperTools/net.telestream.LicensingHelper",
+  ]
 
   zap trash: [
-        "~/Library/Caches/net.telestream.wirecast/",
-        "~/Library/HTTPStorages/net.telestream.wirecast",
-        "~/Library/Logs/Wirecast",
-        "~/Library/Preferences/Wirecast",
-        "~/Library/Preferences/net.telestream.wirecast.plist",
-        "~/Library/Saved Applicate State/net.telestream.wirecast.savedState",
-      ]
+    "~/Library/Caches/net.telestream.wirecast/",
+    "~/Library/HTTPStorages/net.telestream.wirecast",
+    "~/Library/Logs/Wirecast",
+    "~/Library/Preferences/Wirecast",
+    "~/Library/Preferences/net.telestream.wirecast.plist",
+    "~/Library/Saved Applicate State/net.telestream.wirecast.savedState",
+  ]
 end
