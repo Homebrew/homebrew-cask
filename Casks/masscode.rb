3ce46bb384a45aa64814483a1c2e672157005666
Anton Reshetov <reshetov.art@gmail.com>
2022-07-06 14:42:00 +0300

    FIx hash sum for arch

--- Casks/masscode.rb
+++ Casks/masscode.rb
@@ -3,9 +3,9 @@ cask "masscode" do
 
   version "2.7.0"
   if Hardware::CPU.intel?
-    sha256 "75364a04fdb5548ef1e7d56d009e59dda77c62f89af8a74dbf008a52f2b96c11"
-  else
     sha256 "58dbb36f2a18dca9e8fb54f1d0996915f31448eb4b56dcb352ddbf8ebc228d60"
+  else
+    sha256 "75364a04fdb5548ef1e7d56d009e59dda77c62f89af8a74dbf008a52f2b96c11"
   end
 
   url "https://github.com/massCodeIO/massCode/releases/download/v#{version}/massCode-#{version}#{arch}.dmg",
