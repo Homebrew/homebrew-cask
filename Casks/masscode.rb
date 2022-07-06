6d58666a45bf5d4a27533663f86f56f1d06b0efb
Anton Reshetov <reshetov.art@gmail.com>
2022-07-06 13:07:50 +0300

    Add x86 version

--- Casks/masscode.rb
+++ Casks/masscode.rb
@@ -1,8 +1,14 @@
 cask "masscode" do
-  version "2.7.0"
-  sha256 "75364a04fdb5548ef1e7d56d009e59dda77c62f89af8a74dbf008a52f2b96c11"
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  url "https://github.com/massCodeIO/massCode/releases/download/v#{version}/massCode-#{version}-arm64.dmg",
+  version "2.7.0"
+  if Hardware::CPU.intel?
+    sha256 "75364a04fdb5548ef1e7d56d009e59dda77c62f89af8a74dbf008a52f2b96c11"
+  else
+    sha256 "58dbb36f2a18dca9e8fb54f1d0996915f31448eb4b56dcb352ddbf8ebc228d60"
+  end
+
+  url "https://github.com/massCodeIO/massCode/releases/download/v#{version}/massCode-#{version}#{arch}.dmg",
       verified: "https://github.com/massCodeIO/massCode/"
   name "massCode"
   desc "Open-source code snippets manager for developers"
