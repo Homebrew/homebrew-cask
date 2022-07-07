a78a670ddbb5536ca432ec3e431e33cf5639096b
p-linnane <patrick@linnane.io>
2022-07-07 10:41:34 -0700

    chrysalis 0.10.4
    Update chrysalis from 0.10.3 to 0.10.4
    
    Closes #127358.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/chrysalis.rb
+++ Casks/chrysalis.rb
@@ -1,6 +1,6 @@
 cask "chrysalis" do
-  version "0.10.3"
-  sha256 "6a7b2f150c636058d568b68b196c23a881072d1cf83bb726d30760a691091938"
+  version "0.10.4"
+  sha256 "d3bd5aae7b4e3204032835b8b8f2d7896068aff92ecb41e7f20db7aa0ad0e232"
 
   url "https://github.com/keyboardio/Chrysalis/releases/download/v#{version}/Chrysalis-#{version}.dmg"
   name "Chrysalis"
