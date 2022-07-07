2d649de23696c30e308cac0ddb4a90a9eca2ff98
p-linnane <patrick@linnane.io>
2022-07-07 09:58:17 -0700

    Update stellarium from 0.22.1 to 0.22.2

--- Casks/stellarium.rb
+++ Casks/stellarium.rb
@@ -1,12 +1,12 @@
 cask "stellarium" do
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 
-  version "0.22.1"
+  version "0.22.2"
 
   if Hardware::CPU.intel?
-    sha256 "a3993feda9b9401f5838ff1c43f1d3c22d9c9ef2c085015f4e6b4a9447b73924"
+    sha256 "5e8c2ee315f8c00a393e7bd22461f9b48355538cec39e1bb771e92a5795bcfb4"
   else
-    sha256 "a9a1bb92b6ec8df2722da57c1d3e5162f9ca0d4b12fbc642c7ee0d947ffa9b53"
+    sha256 "74acc44f96597d11d92f94015efd19cd18c2ce76e850d90f44c40d636f72ea5f"
   end
 
   url "https://github.com/Stellarium/stellarium/releases/download/v#{version.major_minor_patch}/Stellarium-#{version}-#{arch}.zip",
