79f22bd76acc3f1f7c7e4ec9e7707c6758d02dff
Philipp Jetschina <kontakt@jetschina.de>
2022-07-07 23:55:06 +0200

    fix livecheck error

--- Casks/calhash.rb
+++ Casks/calhash.rb
@@ -2,9 +2,17 @@ cask "calhash" do
   if MacOS.version <= :catalina
     version "1.0.5"
     sha256 "bb15c18839837bdeda203a8a916b09b4f2b971e5bba10b603f290130939d3bf7"
+
+    livecheck do
+      skip "Legacy version for Catalina"
+    end
   elsif MacOS.version <= :big_sur
     version "1.1.1"
     sha256 "66b2de6f514119b3ddf915310b94486d24d3cc6733c74af97e367b76cb2d7478"
+
+    livecheck do
+      skip "Legacy version for Big Sur"
+    end
   else
     version "1.1.7"
     sha256 "bbcbbc3d3f47751d5f021603691404e0886ae999e85812c2fe55b0e98d104a04"
