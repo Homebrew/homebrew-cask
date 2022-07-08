d45c5ee704d72c69b9e024246a63dff463101392
p-linnane <patrick@linnane.io>
2022-07-08 08:34:39 -0700

    Update arctype from 0.9.70 to 0.9.71

--- Casks/arctype.rb
+++ Casks/arctype.rb
@@ -1,6 +1,6 @@
 cask "arctype" do
-  version "0.9.70"
-  sha256 "e4ff60cbf7b034ff160e0b24be73ebc5887c919774d0092605a3052bdd504356"
+  version "0.9.71"
+  sha256 "4acde50a37535f445a33c99460b92f5995eabdb513446146246bcd1792b5dc90"
 
   url "https://arctype-downloads.s3-us-west-2.amazonaws.com/updates/Arctype-#{version}.dmg",
       verified: "arctype-downloads.s3-us-west-2.amazonaws.com/updates/"
