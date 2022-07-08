5cb0811ec4d0708c8eecdcd40c60a609c7d861ab
p-linnane <patrick@linnane.io>
2022-07-08 09:59:28 -0700

    Update masscode from 2.7.1 to 2.8.0

--- Casks/masscode.rb
+++ Casks/masscode.rb
@@ -1,12 +1,12 @@
 cask "masscode" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  version "2.7.1"
+  version "2.8.0"
 
   if Hardware::CPU.intel?
-    sha256 "4f91b64ceed0b5c73e9873264deb85ff054ec5b3912baf34982421b7eb3edf4c"
+    sha256 "9b1f8f0f702c0a5fe3a96acb8c8db9d32c9a5c89398472d865f0d2dbcd8d09e1"
   else
-    sha256 "283fab2a8c0179a54da9a5560221ddaa4328ebef3c5014a43a13c56126f3b8a0"
+    sha256 "d2748ab60275094a2ea714108ba83d519c0393137396f4373436469da3b50b0b"
   end
 
   url "https://github.com/massCodeIO/massCode/releases/download/v#{version}/massCode-#{version}#{arch}.dmg",
