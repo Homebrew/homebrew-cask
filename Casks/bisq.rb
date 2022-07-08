4915431be9346dea09ff4056a4d7d3f61bfb4f4f
p-linnane <patrick@linnane.io>
2022-07-08 12:51:08 -0700

    bisq 1.9.4
    Update bisq from 1.9.2 to 1.9.4
    
    Closes #127452.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/bisq.rb
+++ Casks/bisq.rb
@@ -1,6 +1,6 @@
 cask "bisq" do
-  version "1.9.2"
-  sha256 "79a32de9f2686611be397a7a368fe905c004bcaced676b538e45058972c13732"
+  version "1.9.4"
+  sha256 "5a3d5ccbe9dfdfb774ba197c8788985d9cf900cfab5aea38daa5e4d20abd9108"
 
   url "https://github.com/bisq-network/bisq/releases/download/v#{version}/Bisq-#{version}.dmg",
       verified: "github.com/bisq-network/bisq/"
