20f99b8ac87ef4f8bf2047763764dbc407e2b6c8
Sophon <sophonci117@gmail.com>
2022-07-08 16:57:12 +0800

    LemonLime 0.3.3

---
+++ Casks/lemonlime.rb
@@ -0,0 +1,11 @@
+cask "lemonlime" do
+  version "0.3.3"
+  sha256 "d3da9c4eeaa11d7c61e5da9bf5b48aade2c2446f7c6332e095d81f9cf6726b54"
+
+  url "https://github.com/Project-LemonLime/Project_LemonLime/releases/download/#{version}/lemon-Release.dmg"
+  name "lemonlime"
+  desc "Lightweight evaluation system based on Lemon + LemonPlus for OI(Olympiad in Informatics) competition"
+  homepage "https://github.com/Project-LemonLime/Project_LemonLime"
+
+  app "lemon.app"
+end
