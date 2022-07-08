40a801c28999f28efbe1fc306d9f3d728f3ad47d
p-linnane <patrick@linnane.io>
2022-07-08 10:19:30 -0700

    Update quarto from 0.9.655 to 0.9.658

--- Casks/quarto.rb
+++ Casks/quarto.rb
@@ -1,6 +1,6 @@
 cask "quarto" do
-  version "0.9.655"
-  sha256 "b95ef1e017bedb92451589298b3e0616f2972d38578e0c4e4b859b269ce324a6"
+  version "0.9.658"
+  sha256 "59d5c04f3b345349003428c8be6f62633c3791de2b5a20cc5783f4b9c1098939"
 
   url "https://github.com/quarto-dev/quarto-cli/releases/download/v#{version}/quarto-#{version}-macos.pkg",
       verified: "github.com/quarto-dev/quarto-cli/"
