9a93787532713a3abbfdc40e5856c30361751ab9
Porkepix <porkepix@gmail.com>
2022-07-08 11:14:58 +0200

    packetproxy 2.1.9
    Update packetproxy from 2.1.8 to 2.1.9
    
    Closes #127403.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/packetproxy.rb
+++ Casks/packetproxy.rb
@@ -1,6 +1,6 @@
 cask "packetproxy" do
-  version "2.1.8"
-  sha256 "4ae04b7951291d651dc8ee56eb5a7526fabb734641100babf4d108537604541c"
+  version "2.1.9"
+  sha256 "781a3b32177dfdf77c8f5015fa980a08f0b4b227437ea3eaa3a3fb6a95d9e3af"
 
   url "https://github.com/DeNA/PacketProxy/releases/download/#{version}/PacketProxy-#{version}-Installer-Mac-Signed.dmg"
   name "PacketProxy"
