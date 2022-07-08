b59f19853792d92af456b91d938da9656b86b1fc
p-linnane <patrick@linnane.io>
2022-07-08 12:36:13 -0700

    Bump version, split by arch, fix livecheck

--- Casks/shift.rb
+++ Casks/shift.rb
@@ -1,15 +1,22 @@
 cask "shift" do
-  version "7.2.18"
-  sha256 "6d779264fad6736675f56c74a9cbd8d778baba7234123f42d7182cfb15667cc0"
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  url "https://update.tryshift.com/download/version/#{version}/osx_64"
+  version "8.0.44.425"
+
+  if Hardware::CPU.intel?
+    sha256 "9d120330a5d3b912f05d1250a87e96226b585c1c089046ed98f77ae978f42fa5"
+  else
+    sha256 "dd2667a827a7ee58f66fdc68b71efa8cc4b93c7559b72c059fbda08f78fbc54e"
+  end
+
+  url "https://updates.tryshift.com/v#{version.major_minor_patch}/stable/shift-v#{version}-stable-#{arch}.dmg"
   name "Shift"
   desc "Workstation to streamline your accounts, apps, and workflows"
   homepage "https://tryshift.com/"
 
   livecheck do
     url "https://tryshift.com/download/?platform=mac"
-    regex(%r{href=.*?version/(\d+(?:[._-]\d+)+)[._-]stable/osx[._-]64}i)
+    regex(/href=.*?(\d+(?:[._-]\d+)+)[._-]stable.*?\.dmg/i)
   end
 
   app "Shift.app"
