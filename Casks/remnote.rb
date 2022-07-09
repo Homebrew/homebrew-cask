d08e304a026e297b65d78bc1d930f9ef4cd19d44
p-linnane <patrick@linnane.io>
2022-07-09 09:54:26 -0700

    remnote 1.8.5
    Update remnote from 1.8.3 to 1.8.5
    
    Closes #127489.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/remnote.rb
+++ Casks/remnote.rb
@@ -1,6 +1,6 @@
 cask "remnote" do
-  version "1.8.3"
-  sha256 "bdcf87672f4836b18d8566de8cc71d004660719f2586f09b8fadc279342c65bd"
+  version "1.8.5"
+  sha256 "3c388748b1ab6dd17deab99d9fa2ec3dc5dac74eebd052ebb9af96f9a4f5a622"
 
   url "https://download.remnote.io/RemNote-#{version}.dmg",
       verified: "remnote.io"
