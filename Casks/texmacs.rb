35fffd764d72200d3d9c912bbaf17d2ccf3e5fcb
Ilmari Vacklin <ilmari.vacklin@iki.fi>
2022-07-09 21:53:58 +0300

    Add TeXmacs ARM variant

--- Casks/texmacs.rb
+++ Casks/texmacs.rb
@@ -1,8 +1,15 @@
 cask "texmacs" do
-  version "2.1.2"
-  sha256 "db1f9a525554d76794e0339cc19fb4d45eee79bce5f7a176c8dc8b8667181b08"
+  arch = Hardware::CPU.intel? ? "" : "-arm"
 
-  url "https://ftp.texmacs.org/TeXmacs/tmftp/macos/TeXmacs-#{version}.dmg"
+  version "2.1.2"
+
+  if Hardware::CPU.intel?
+    sha256 "db1f9a525554d76794e0339cc19fb4d45eee79bce5f7a176c8dc8b8667181b08"
+  else
+    sha256 "d0a5abf3dafba31073ffc3bbfbeff5300453954a54495affee8a16a2f9196587"
+  end
+
+  url "https://ftp.texmacs.org/TeXmacs/tmftp/macos/TeXmacs-#{version}#{arch}.dmg"
   name "GNU TeXmacs"
   desc "Scientific editing platform"
   homepage "https://www.texmacs.org/"
