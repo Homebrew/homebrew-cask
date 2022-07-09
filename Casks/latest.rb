47e7a75385cf403cefeb39e9ebda35aa73e224b5
p-linnane <patrick@linnane.io>
2022-07-09 09:34:09 -0700

    latest 0.8.1,708
    Update latest from 0.8 to 0.8.1
    
    Closes #127485.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/latest.rb
+++ Casks/latest.rb
@@ -1,6 +1,6 @@
 cask "latest" do
-  version "0.8,487"
-  sha256 "7740a16afde28845c5f1dee951208e2fadf6a1e214668e980164394c8ad34169"
+  version "0.8.1,708"
+  sha256 "8563072816b20a014f90107ca8fb781e38affb31d027c90894adbab5090a960e"
 
   url "https://max.codes/latest/#{version.csv.first}.zip"
   name "Latest"
