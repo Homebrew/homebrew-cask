08ecec01d64cbda8bde5bcbc1d1e32235796ffae
Jared Grove <jared@jaredgrove.com>
2022-07-09 15:33:35 -0400

    add "Black Light Pro.app" v1.2

---
+++ Casks/black-light-pro.rb
@@ -0,0 +1,21 @@
+cask "black-light-pro" do
+  version "1.2"
+  sha256 "0f97fba22628e1a4c5684d6271fa0477cf97a9e7361dd92fecdd04c3f2f8dbf4"
+
+  url "https://littoral.michelf.ca/apps/black-light-pro/black-light-pro-#{version}.zip"
+  name "Black Light Pro"
+  desc "Color effects on a schedule"
+  homepage "https://michelf.ca/software/black-light-pro/"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?black-light-pro[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+  end
+
+  app "BLack Light Pro.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/ca.michelf.BlackLight.Pro",
+    "~/Library/Containers/ca.michelf.BlackLight.Pro",
+  ]
+end
