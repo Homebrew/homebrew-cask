00c917173e781617684fc0fd064730bfcb298c38
Sam Ford <1584702+samford@users.noreply.github.com>
2022-07-09 23:16:45 -0400

    djview: add livecheck (#127503)

--- Casks/djview.rb
+++ Casks/djview.rb
@@ -1,13 +1,22 @@
 cask "djview" do
-  version "4.12,2"
+  version "4.12,3.5.28,2"
   sha256 "c65460282d7d43c239d262551d415e0cf5873d4b8cbd845e434f2134cdd0a859"
 
-  url "https://downloads.sourceforge.net/djvu/DjVuLibre-3.5.28%2BDjView-#{version.csv.first}-universal-#{version.csv.second}.dmg",
+  url "https://downloads.sourceforge.net/djvu/DjVuLibre-#{version.csv.second}%2BDjView-#{version.csv.first}-universal-#{version.csv.third}.dmg",
       verified: "downloads.sourceforge.net/djvu/"
-  appcast "https://sourceforge.net/projects/djvu/rss"
   name "DjView"
   desc "DjVu viewer and browser plugin"
   homepage "https://djvu.sourceforge.io/"
+
+  livecheck do
+    url "https://sourceforge.net/projects/djvu/rss?path=/DjVuLibre_MacOS"
+    regex(%r{url=.*?/DjVuLibre[._-](\d+(?:\.\d+)+)%2BDjView[._-](\d+(?:\.\d+)+)[._-]universal(?:-(\d+))?\.dmg}i)
+    strategy :sourceforge do |page, regex|
+      page.scan(regex).map do |match|
+        match[2].present? ? "#{match[1]},#{match[0]},#{match[2]}" : "#{match[1]},#{match[0]}"
+      end
+    end
+  end
 
   app "DjView.app"
 
