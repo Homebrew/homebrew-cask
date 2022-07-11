08139b2e27c74f21bba0918e9578840e9a8b0638
p-linnane <patrick@linnane.io>
2022-07-11 08:56:51 -0700

    grammarly-desktop 1.8.0.0
    Update grammarly-desktop from 1.7.0.1 to 1.8.0.0
    
    Closes #127559.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/grammarly-desktop.rb
+++ Casks/grammarly-desktop.rb
@@ -1,6 +1,6 @@
 cask "grammarly-desktop" do
-  version "1.7.0.1"
-  sha256 "915188f4761a28e77749ed312d1ceebc9adb139eafefec47481cd5095f36ede0"
+  version "1.8.0.0"
+  sha256 "a791a530ac77359674b2e5ea931f260fd0864c8659349936f4372235e23cf40f"
 
   url "https://download-mac.grammarly.com/versions/#{version}/Grammarly.dmg"
   name "Grammarly Desktop"
