25dfa357a1cf6499e3da2c1051ff6e09b023c8b7
Bevan Kay <email@bevankay.me>
2022-07-12 09:28:00 +1000

    Update grammarly-desktop from 1.8.0.0 to 1.8.0.1

--- Casks/grammarly-desktop.rb
+++ Casks/grammarly-desktop.rb
@@ -1,6 +1,6 @@
 cask "grammarly-desktop" do
-  version "1.8.0.0"
-  sha256 "a791a530ac77359674b2e5ea931f260fd0864c8659349936f4372235e23cf40f"
+  version "1.8.0.1"
+  sha256 "bb1bb95963546b749746bfd735a382af61c08e0c3b11c745b6c20e6aea540014"
 
   url "https://download-mac.grammarly.com/versions/#{version}/Grammarly.dmg"
   name "Grammarly Desktop"
