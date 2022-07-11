1b3ce3fad8e261bfc3dd0e251a15e1209046654f
PMheart <17109513+PMheart@users.noreply.github.com>
2022-07-11 21:13:41 +0200

    Update desmume.rb

--- Casks/desmume.rb
+++ Casks/desmume.rb
@@ -1,11 +1,12 @@
 cask "desmume" do
-  version "0.9.11"
-  sha256 "c3ff7024e0905f1c1da390d0fa793a3dc42d026ceded333958f3b79243a11db1"
+  version_tag "release_0_9_13"
+  version "0.9.13"
+  sha256 "d42e4bbf8f96b6bfdb3c8be6cf469b606a3b105352460636b1051b8dd0365ebc"
 
-  url "https://downloads.sourceforge.net/desmume/desmume-#{version}-mac.dmg"
+  url "https://github.com/TASEmulators/desmume/releases/download/#{version_tag}/desmume-#{version}-macOS.dmg"
   name "DeSmuME"
   desc "Nintendo DS emulator"
-  homepage "https://sourceforge.net/projects/desmume/"
+  homepage "https://github.com/TASEmulators/desmume/"
 
   app "DeSmuME.app"
 
