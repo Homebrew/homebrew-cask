d716204f64fa19ba4760a16a2557fb0d06e8ade6
neutric <5984479+neutric@users.noreply.github.com>
2022-07-12 14:20:39 +0200

    postman-agent 0.4.7
    Update postman-agent from 0.4.6 to 0.4.7
    
    Closes #127637.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/postman-agent.rb
+++ Casks/postman-agent.rb
@@ -1,12 +1,12 @@
 cask "postman-agent" do
   arch = Hardware::CPU.intel? ? "osx_64" : "osx_arm64"
 
-  version "0.4.6"
+  version "0.4.7"
 
   if Hardware::CPU.intel?
-    sha256 "09d4b0d7eb9946662ba942ffe0dd2d1612858ed3a7cfff153150951d9e9c94d4"
+    sha256 "d479f55963ce5fe56f67fb03f349732790511c3c6943e256e6193bae15498af1"
   else
-    sha256 "ee5f3087d4719730bc4a160082cfd4db4544cc92b42f4ca1c6635c0824efa242"
+    sha256 "e76fb6b2c1198c9591be5a711d22d9391a6859ad059b45af111f0f385be16190"
   end
 
   url "https://dl-agent.pstmn.io/download/version/#{version}/#{arch}",
