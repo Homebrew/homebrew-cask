2c23c3224ce86ea8124191f37639d7936c1ec70f
p-linnane <patrick@linnane.io>
2022-07-12 16:32:41 -0700

    Bump Intel

--- Casks/dotnet.rb
+++ Casks/dotnet.rb
@@ -2,8 +2,8 @@ cask "dotnet" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
   if Hardware::CPU.intel?
-    version "6.0.6,bc450f6d-2309-4352-b940-e4ac89261afe,e6a64009f3829f7042153a772edfa4ea"
-    sha256 "5079d835dfd80b8ba1bd997eaf4459d45b3a621b84abbef64a6c3bf7b1a94104"
+    version "6.0.7,3da50ef8-7b22-4f08-95e9-3641416fdc08,ee91bcdbbbb117803e315f673c9d19f1"
+    sha256 "9aba5aca71ada681a77a9058a5d5c2fdfed7d6a197ba416c88e8093906abbdeb"
   else
     version "6.0.7,ba8b5982-2d85-4b0f-a933-13b1dcca3e2d,a367458c007ef00bf1544754578cf54c"
     sha256 "31140e6597f8943ee4b4cf25fd560fa4db586ab76770bb9a578ed6efab694ba4"
