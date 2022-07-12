647b2c537dcb31c635a659287103442d188cb46f
p-linnane <patrick@linnane.io>
2022-07-11 20:38:31 -0700

    Update purei-play from 2022-06-28 to 2022-07-11

--- Casks/purei-play.rb
+++ Casks/purei-play.rb
@@ -1,6 +1,6 @@
 cask "purei-play" do
-  version "2022-06-28,8e5796e6"
-  sha256 "cf65b5fdcd1680087c49e219749e076b442d8c6a639ca94ec9927cbf6aced81b"
+  version "2022-07-11,a4860d8c"
+  sha256 "554018f7c5870bd5e1b4a27793ef823dc9a2aa7a256f3130d08b5d85288d5366"
 
   url "https://s3.us-east-2.amazonaws.com/playbuilds/#{version.csv.second}/Play.dmg",
       verified: "https://s3.us-east-2.amazonaws.com/playbuilds/"
