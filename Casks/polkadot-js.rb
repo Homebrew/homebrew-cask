b8e03106dff5c76db2b4c8f8e69aa3a418cf3091
neutric <5984479+neutric@users.noreply.github.com>
2022-07-12 14:18:16 +0200

    Update polkadot-js from 0.116.1 to 0.117.2

--- Casks/polkadot-js.rb
+++ Casks/polkadot-js.rb
@@ -1,6 +1,6 @@
 cask "polkadot-js" do
-  version "0.116.1"
-  sha256 "9b823acd49724a9c2f9691da488a425dec970de129027145c3578a420a64bd86"
+  version "0.117.2"
+  sha256 "3242fa4c1133bde74a1f78cd49175e078de9d230a7f9f34c27e41ee8f5a9250c"
 
   url "https://github.com/polkadot-js/apps/releases/download/v#{version}/Polkadot-JS-Apps-mac-#{version}.dmg",
       verified: "github.com/polkadot-js/apps/"
