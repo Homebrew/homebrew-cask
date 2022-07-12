a6357ad2f3445b721d8081ae72a7891647eb2f66
p-linnane <patrick@linnane.io>
2022-07-12 16:13:47 -0700

    Update blitz from 1.17.7 to 1.17.9

--- Casks/blitz.rb
+++ Casks/blitz.rb
@@ -1,6 +1,6 @@
 cask "blitz" do
-  version "1.17.7"
-  sha256 "7615057b548be13a054991b9db6d7bac918c25ab5b68dd5624b798d56f8fe90d"
+  version "1.17.9"
+  sha256 "8d6e64e6fd35a39d281758d6b81385f543b7033f48d253afd8eb10768403836c"
 
   url "https://blitz-stable.blitz.gg/Blitz-#{version}.dmg"
   name "Blitz"
