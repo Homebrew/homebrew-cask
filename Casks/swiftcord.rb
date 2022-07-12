9cd407eb79fbadc7b225f367a9e040b87c71bfb3
p-linnane <patrick@linnane.io>
2022-07-11 20:50:25 -0700

    Update swiftcord from 0.4.3 to 0.4.4

--- Casks/swiftcord.rb
+++ Casks/swiftcord.rb
@@ -1,6 +1,6 @@
 cask "swiftcord" do
-  version "0.4.3,10"
-  sha256 "10413e0c7ead0ad4d5a56b744dc25baa4651a8b7e25523d97f46620fe171a206"
+  version "0.4.4,11"
+  sha256 "03515e39775b6c9523553ec53ec16bdc63a0dbcab602c19aed9ebc779f309986"
 
   url "https://github.com/SwiftcordApp/Swiftcord/releases/download/v#{version.csv.first}/Swiftcord.#{version.csv.first}.dmg"
   name "Swiftcord"
