79c5ab53088a4debff3c87882cb965a238da0499
p-linnane <patrick@linnane.io>
2022-07-13 08:46:13 -0700

    quarto 1.0.18
    Update quarto from 1.0.16 to 1.0.18
    
    Closes #127760.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/quarto.rb
+++ Casks/quarto.rb
@@ -1,6 +1,6 @@
 cask "quarto" do
-  version "1.0.16"
-  sha256 "77a4e05d951d4f289c4bd9b00e91d36a6ca196bd8dfdaddd491828d701ce2983"
+  version "1.0.18"
+  sha256 "0e737b0b843225b6360e31baa059a37a43f19f38a7fd1c119552d0ab12f8fa35"
 
   url "https://github.com/quarto-dev/quarto-cli/releases/download/v#{version}/quarto-#{version}-macos.pkg",
       verified: "github.com/quarto-dev/quarto-cli/"
