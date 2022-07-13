c5e5275f60c79b7a843b9e899e440ac946c5a91a
p-linnane <patrick@linnane.io>
2022-07-13 08:16:31 -0700

    busycal 2022.3.1,2022-07-13-14-52
    Update busycal from 2022.3.1,2022-07-12-18-31 to 2022.3.1,2022-07-13-14-52
    
    Closes #127732.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/busycal.rb
+++ Casks/busycal.rb
@@ -1,6 +1,6 @@
 cask "busycal" do
-  version "2022.3.1,2022-07-12-18-31"
-  sha256 "4facb506045b5279dcc7217755cac59d2e80c90c0296fbf69d5009702c40ebe6"
+  version "2022.3.1,2022-07-13-14-52"
+  sha256 "4a05b192cca6dd6e7de8a352485970bb7dc89e678ae09c98adfcff97e8a5c755"
 
   url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bcl-#{version.csv.first}-#{version.csv.second}.zip",
       verified: "7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/"
