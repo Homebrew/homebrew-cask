d69acce320d6fdd25e9cd7065d651913cdcd3332
p-linnane <patrick@linnane.io>
2022-07-13 10:58:00 -0700

    daedalus-testnet 4.12.0,22058
    * Update daedalus-testnet from 4.11.0 to 4.12.0
    * Fix pkg name
    
    Closes #127787.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/daedalus-testnet.rb
+++ Casks/daedalus-testnet.rb
@@ -1,8 +1,8 @@
 cask "daedalus-testnet" do
-  version "4.11.0,21857"
-  sha256 "c00efb7b0d524490af3fa3b9f8a95cb16b1964f3eab789a6b7e21467daeca8ec"
+  version "4.12.0,22058"
+  sha256 "afe54c0a371445dbac5517c484e43434151707c08a925196c58b01d2f8984f65"
 
-  url "https://updates-cardano-testnet.s3.amazonaws.com/daedalus-#{version.csv.first}-testnet-#{version.csv.second}.pkg",
+  url "https://updates-cardano-testnet.s3.amazonaws.com/daedalus-#{version.csv.first}-testnet-#{version.csv.second}-x86_64-darwin.pkg",
       verified: "updates-cardano-testnet.s3.amazonaws.com/"
   name "Daedalus Testnet"
   desc "Cryptocurrency wallet for test ada on the Cardano Testnet blockchain"
@@ -12,7 +12,7 @@ cask "daedalus-testnet" do
     url "https://updates-cardano-testnet.s3.amazonaws.com/daedalus-latest-version.json"
     strategy :page_match do |page|
       version = page.match(/"version":"(\d+(?:\.\d+)+)"/)[1]
-      build = page.match(/(\d+(?:\.\d+)*)\.pkg/)[1]
+      build = page.match(/testnet-(\d+(?:\.\d+)*).*?\.pkg/)[1]
       next if version.blank? || build.blank?
 
       "#{version},#{build}"
@@ -22,7 +22,7 @@ cask "daedalus-testnet" do
   auto_updates true
   depends_on macos: ">= :high_sierra"
 
-  pkg "daedalus-#{version.csv.first}-testnet-#{version.csv.second}.pkg"
+  pkg "daedalus-#{version.csv.first}-testnet-#{version.csv.second}-x86_64-darwin.pkg"
 
   uninstall pkgutil: "org.Daedalustestnet.pkg"
 
