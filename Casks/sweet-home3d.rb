013328d650ed517c02ad2e5c50a6e25318cc8c5e
p-linnane <patrick@linnane.io>
2022-07-13 08:58:19 -0700

    sweet-home3d 7.0
    Update sweet-home3d from 6.6 to 7.0
    
    Closes #127764.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/sweet-home3d.rb
+++ Casks/sweet-home3d.rb
@@ -1,6 +1,6 @@
 cask "sweet-home3d" do
-  version "6.6"
-  sha256 "0423fb6e7fabc8bcfd0ea8260e4de671199cba3bede827c011149a17db90a038"
+  version "7.0"
+  sha256 "26f0242e435ce2230f051f1342a6071e2bc1ea8b33d34dec24e630f0fc435c5a"
 
   url "https://downloads.sourceforge.net/sweethome3d/SweetHome3D/SweetHome3D-#{version}/SweetHome3D-#{version}-macosx.dmg",
       verified: "sourceforge.net/sweethome3d/"
