6ec78a14f24d849b8c762c20da6187a7add06170
p-linnane <patrick@linnane.io>
2022-07-13 11:24:03 -0700

    sonic-pi 4.0.1
    Update sonic-pi from 4.0.0 to 4.0.1
    
    Closes #127791.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/sonic-pi.rb
+++ Casks/sonic-pi.rb
@@ -1,8 +1,8 @@
 cask "sonic-pi" do
-  version "4.0.0"
-  sha256 "c33327eccf940863ec35ad4c9954b4c66a91add17b7a84f259cfd936af8815c7"
+  version "4.0.1"
+  sha256 "ff7e7119644125f1b6c9ab926e2f1d5cbdc2e025e8b4cbc2ab7be58e0015dfca"
 
-  url "https://sonic-pi.net/files/releases/v#{version}/Sonic-Pi-for-Mac-x64-v#{version}.dmg"
+  url "https://sonic-pi.net/files/releases/v#{version}/Sonic-Pi-for-Mac-x64-v#{version.dots_to_hyphens}.dmg"
   name "Sonic Pi"
   desc "Code-based music creation and performance tool"
   homepage "https://sonic-pi.net/"
