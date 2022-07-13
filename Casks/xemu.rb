4d353255b019bd1393a07d47f53f3674b2bf41d1
p-linnane <patrick@linnane.io>
2022-07-13 15:34:20 -0700

    Update xemu from 0.7.58 to 0.7.59

--- Casks/xemu.rb
+++ Casks/xemu.rb
@@ -1,6 +1,6 @@
 cask "xemu" do
-  version "0.7.58"
-  sha256 "2d230316c73b76a06910365d5a415d8f53d19103c5bf1b6d6289546619a98241"
+  version "0.7.59"
+  sha256 "01697159d6f71cbca16098fb57eb7298bfbdd14608e89397e71f8b0b4a4dc5fc"
 
   url "https://github.com/mborgerson/xemu/releases/download/v#{version}/xemu-macos-universal-release.zip",
       verified: "github.com/mborgerson/xemu/"
