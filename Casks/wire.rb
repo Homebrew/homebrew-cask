d7a1f910cdfbcfa1e5bcf5d3f7939c25f841fe21
p-linnane <patrick@linnane.io>
2022-07-13 09:01:45 -0700

    Update wire from 3.27.4357 to 3.28.4393

--- Casks/wire.rb
+++ Casks/wire.rb
@@ -1,6 +1,6 @@
 cask "wire" do
-  version "3.27.4357"
-  sha256 "e891c2a7cc95c5f72b51962278c12438c51240ead887559d58912ffd5d7a3076"
+  version "3.28.4393"
+  sha256 "bc3fff0f495d1cd9bb6d607425bd9e37ba93f5325a0f8b74419bbcce9d82880f"
 
   url "https://github.com/wireapp/wire-desktop/releases/download/macos%2F#{version}/Wire.pkg",
       verified: "github.com/wireapp/wire-desktop/"
