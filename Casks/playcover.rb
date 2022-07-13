dbf2a11ce317f7f4c8eb59ff93582c01531b51b5
Hayato Fujii <hayato.fujii@gmail.com>
2022-07-12 22:18:50 -0300

    playcover: fix cask styling

--- Casks/playcover.rb
+++ Casks/playcover.rb
@@ -1,12 +1,11 @@
 cask "playcover" do
+  version "1.0.6"
+  sha256 "c34399f5786096e30d0998939cdb0dbf1edb9bf57fbe842d2d5d1f51427cce9a"
+
+  url "https://github.com/PlayCover/PlayCover/releases/download/#{version}/PlayCover_#{version}.dmg"
   name "PlayCover"
   desc "Sideload iOS apps and games"
   homepage "https://github.com/PlayCover/PlayCover"
-
-  version "1.0.6"
-  sha256 "c34399f5786096e30d0998939cdb0dbf1edb9bf57fbe842d2d5d1f51427cce9a"
-  url "https://github.com/PlayCover/PlayCover/releases/download/#{version}/PlayCover_#{version}.dmg",
-      verified: "github.com/PlayCover/PlayCover/"
 
   depends_on arch: :arm64
   depends_on macos: :monterey
