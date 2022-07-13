1453dae455133f8a231bc2739364182b7f0b6899
p-linnane <patrick@linnane.io>
2022-07-13 09:40:05 -0700

    Update brackets from 2.0.1 to 2.1.2

--- Casks/brackets.rb
+++ Casks/brackets.rb
@@ -1,17 +1,17 @@
 cask "brackets" do
-  version "2.0.1"
-  sha256 "2aa93fc36011bea42554ec3c118f4e1c5d5518b92094b89b57c20f5ba1a1ec66"
+  version "2.1.2"
+  sha256 "6399d43315e0c8921c11d27325b697d26d8a524bfc1455a83173a13a6c6048a2"
 
-  url "https://github.com/brackets-cont/brackets/releases/download/v#{version}/Brackets.#{version}.signed.dmg",
+  url "https://github.com/brackets-cont/brackets/releases/download/v#{version}-release/brackets.#{version}.dmg",
       verified: "github.com/brackets-cont/brackets/"
   name "Brackets"
   desc "Open-source code editor for web-developement"
   homepage "https://brackets.io/"
 
   livecheck do
-    url :url
+    url "https://github.com/brackets-cont/brackets/releases"
     strategy :github_latest
-    regex(%r{href=.*?/Brackets\.(\d+(?:\.\d+)+)\.signed.dmg}i)
+    regex(%r{href=.*?/brackets\.(\d+(?:\.\d+)+)\.dmg}i)
   end
 
   app "Brackets.app"
