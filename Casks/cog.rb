38f5b85c51b9ec25177cb279a5977901f998fbbb
p-linnane <patrick@linnane.io>
2022-07-14 07:58:23 -0700

    Update cog from 2602 to 2617

--- Casks/cog.rb
+++ Casks/cog.rb
@@ -1,6 +1,6 @@
 cask "cog" do
-  version "2602,c4e97531"
-  sha256 "9b6b6d1094e01968af71f3f3a472f187301b75b74e9a3dba47e6189e1dc4bfea"
+  version "2617,af64f93e"
+  sha256 "4b4d9e3cb3bd714da859bd76098d57eddff99f724c2aa9c1230a699518fc065d"
 
   url "https://cogcdn.cog.losno.co/Cog-#{version.csv.second}.zip"
   name "Cog"
