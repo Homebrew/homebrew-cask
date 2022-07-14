4a4e1d8dee26813c73c80c49d2c2e32d51c0859f
owine <owine@users.noreply.github.com>
2022-07-14 18:30:57 -0500

    Update cloudflare-warp from 2022.5.342.0 to 2022.7.175.0 (#127878)

--- Casks/cloudflare-warp.rb
+++ Casks/cloudflare-warp.rb
@@ -1,5 +1,5 @@
 cask "cloudflare-warp" do
-  version "2022.5.342.0,20220616.9"
+  version "2022.7.175.0,20220714.10"
   sha256 :no_check
 
   url "https://1111-releases.cloudflareclient.com/mac/Cloudflare_WARP.zip",
