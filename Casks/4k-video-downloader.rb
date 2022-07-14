50f464a0a6f078320d918543b179f38aaa05e6c2
p-linnane <patrick@linnane.io>
2022-07-14 07:53:08 -0700

    Update 4k-video-downloader from 4.20.4 to 4.21.0

--- Casks/4k-video-downloader.rb
+++ Casks/4k-video-downloader.rb
@@ -1,6 +1,6 @@
 cask "4k-video-downloader" do
-  version "4.20.4"
-  sha256 "af4f0395e04b9d1af50fa025cf099056d106ae3533d371a211187c46bc5658af"
+  version "4.21.0"
+  sha256 "026419289403d3af830e8c09dea30d572d14b5ea74fb52912ba7945f3d87c7a1"
 
   url "https://dl.4kdownload.com/app/4kvideodownloader_#{version.major_minor_patch}.dmg"
   name "4K Video Downloader"
