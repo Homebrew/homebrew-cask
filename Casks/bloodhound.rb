ffba7ab46ff3c85acbf78f0150fe4e2b97de6f41
Viktor Ashirov <vashirov@redhat.com>
2022-07-14 09:53:30 +0200

    BloodHound: add suppport for arm64 builds

--- Casks/bloodhound.rb
+++ Casks/bloodhound.rb
@@ -1,11 +1,28 @@
 cask "bloodhound" do
-  version "4.1.1"
-  sha256 "2c45a08d77284fedd8074a5664e7c2424a94c6d131540010da2e73d8f520ebe7"
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  url "https://github.com/BloodHoundAD/BloodHound/releases/download/#{version}/BloodHound-darwin-x64.zip"
+  version "4.1.1"
+
+  if Hardware::CPU.intel?
+    sha256 "2c45a08d77284fedd8074a5664e7c2424a94c6d131540010da2e73d8f520ebe7"
+  else
+    sha256 "45d8e1aafeb7361581def6751c201b11cd2a9761f1c19d17925f72fda78328bc"
+  end
+
+  url "https://github.com/BloodHoundAD/BloodHound/releases/download/#{version}/BloodHound-darwin-#{arch}.zip"
   name "bloodhound"
   desc "Six Degrees of Domain Admin"
   homepage "https://github.com/BloodHoundAD/BloodHound"
 
-  app "BloodHound-darwin-x64/BloodHound.app"
+  app "BloodHound-darwin-#{arch}/BloodHound.app"
+
+  caveats <<~EOS
+    According to https://github.com/BloodHoundAD/BloodHound/issues/504
+    this app will not work with quarantine attributes.
+    To use this cask, install it with:
+
+      brew install --cask --no-quarantine #{token}
+
+    WARNING: quarantining is a macOS security feature and you disable it at your own risk!
+  EOS
 end
