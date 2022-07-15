7af763a9e17ab0eaea6aafea6d83144dfeafefe5
p-linnane <patrick@linnane.io>
2022-07-14 21:12:54 -0700

    Update ujam from 0.3.8 to 0.3.9

--- Casks/ujam.rb
+++ Casks/ujam.rb
@@ -1,6 +1,6 @@
 cask "ujam" do
-  version "0.3.8"
-  sha256 "fcbd041434c2379837c1a287099c9eb5adf7981df71298593a68a5a5e1c74a18"
+  version "0.3.9"
+  sha256 "f1b0deab764665a104d68447047e6ef77510bf6dc0b23a794583e83897f385c4"
 
   url "https://software.ujam.com/ujamapp/UJAM-#{version}.dmg"
   name "UJAM"
