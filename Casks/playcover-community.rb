40b9c4d8876cd3ca7bb8c6987f7726517c3c19cc
Hayato Fujii <hayato.fujii@gmail.com>
2022-07-15 14:44:11 -0300

    playcover-community: version-bump to 1.0.7

--- Casks/playcover-community.rb
+++ Casks/playcover-community.rb
@@ -1,6 +1,6 @@
 cask "playcover-community" do
-  version "1.0.6"
-  sha256 "c34399f5786096e30d0998939cdb0dbf1edb9bf57fbe842d2d5d1f51427cce9a"
+  version "1.0.7"
+  sha256 "83577fb7683091e018b186ace8f9d85c50fdf7e2c4f665589f26b9e36d4b6e1f"
 
   url "https://github.com/PlayCover/PlayCover/releases/download/#{version}/PlayCover_#{version}.dmg"
   name "PlayCover"
@@ -9,7 +9,6 @@ cask "playcover-community" do
 
   depends_on arch: :arm64
   depends_on macos: :monterey
-  depends_on formula: "ldid"
 
   app "PlayCover.app"
 
