3c03ada10f475e4d3025dec3a9c797f6c2845363
p-linnane <patrick@linnane.io>
2022-07-15 08:59:06 -0700

    Update miaoyan from 0.8.0 to 0.9.0

--- Casks/miaoyan.rb
+++ Casks/miaoyan.rb
@@ -1,6 +1,6 @@
 cask "miaoyan" do
-  version "0.8.0"
-  sha256 "c0201ff91a1d8d66e59ea7bfe63b0209ea8bc843c9e89b9bb9889d62c86ce9a3"
+  version "0.9.0"
+  sha256 "a40722e3dd775faa17ecb9f61cd629f112e84363d7509b5a6e8c254686113373"
 
   url "https://github.com/tw93/MiaoYan/releases/download/V#{version}/MiaoYan.dmg",
       verified: "github.com/tw93/MiaoYan"
