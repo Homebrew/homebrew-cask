dbab41d796b3b7987c0e61c20be6ce12f81ff669
p-linnane <patrick@linnane.io>
2022-07-14 17:53:23 -0700

    Update appium-inspector from 2022.6.1 to 2022.7.1

--- Casks/appium-inspector.rb
+++ Casks/appium-inspector.rb
@@ -1,6 +1,6 @@
 cask "appium-inspector" do
-  version "2022.6.1"
-  sha256 "7ef8836838143406d46e72b04dba00125ce4b2cea1d439187bf173f76d2eef65"
+  version "2022.7.1"
+  sha256 "dfa8c6cf829ff4c17b4a3e449e0a3d7528dfe41b1cccc117e2cbe8dc3499da14"
 
   url "https://github.com/appium/appium-inspector/releases/download/v#{version}/Appium-Inspector-#{version}-universal-mac.zip"
   name "Appium Inspector GUI"
