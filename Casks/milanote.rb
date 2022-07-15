c18ce3ced0e4f395713202b8ab6bb697cdefa8e0
p-linnane <patrick@linnane.io>
2022-07-15 08:44:42 -0700

    Switch to 3.1.11

--- Casks/milanote.rb
+++ Casks/milanote.rb
@@ -1,6 +1,6 @@
 cask "milanote" do
-  version "3.1.12"
-  sha256 "1eafc8743e5536458100b340ee3624106c155971b95381f2197eebd12fefec23"
+  version "3.1.11"
+  sha256 "e3921f61feebeaaa5c1fd890f2decf6adf6ec05509df59593a8ac6c75c499dd8"
 
   url "https://milanote-app-releases.s3.amazonaws.com/Milanote-#{version}.dmg",
       verified: "milanote-app-releases.s3.amazonaws.com/"
