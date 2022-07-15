d26c411223d86d536ce9b6a68d732e649e76824c
Porkepix <porkepix@gmail.com>
2022-07-15 12:00:38 +0200

    sonixd 0.15.3
    Update sonixd from 0.15.2 to 0.15.3
    
    Closes #127913.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/sonixd.rb
+++ Casks/sonixd.rb
@@ -1,12 +1,12 @@
 cask "sonixd" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "0.15.2"
+  version "0.15.3"
 
   if Hardware::CPU.intel?
-    sha256 "7c31a36472b176a13087588ed6d6b80df983be1fbc4c92fa6692a4f10b275c50"
+    sha256 "384889e0abccdd75771624f874d812a1ed49f50e2f51af03e0bcb286940059dc"
   else
-    sha256 "35b7c9a1355d14867c1873411385ba401cfed6ba2941be2ce77143c2609ad62d"
+    sha256 "f2382d64fa216c7fb7734043c77be5b45e1dd5f214da3cca0a5d3c34c2e7acda"
   end
 
   url "https://github.com/jeffvli/sonixd/releases/download/v#{version}/Sonixd-#{version}-mac-#{arch}.dmg"
