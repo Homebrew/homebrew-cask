1b20ceea36eb36fe2bb9cfa0f8b6396e935617b7
neutric <5984479+neutric@users.noreply.github.com>
2022-07-16 03:40:45 +0200

    Update gather from 0.2.2 to 0.3.0

--- Casks/gather.rb
+++ Casks/gather.rb
@@ -1,6 +1,6 @@
 cask "gather" do
-  version "0.2.2"
-  sha256 "5ba41eacaf4f27bba26e23473dd3a2bd400a326fe026c25fde05e6a5bc36ce77"
+  version "0.3.0"
+  sha256 "f941db9ae549215fd2aad10c246630b321f9406ca71ac7dc3d02579499714976"
 
   url "https://github.com/gathertown/gather-town-desktop-releases/releases/download/v#{version}/Gather-#{version}-mac.zip",
       verified: "github.com/gathertown/gather-town-desktop-releases"
