67ca4a89b8ff18d89c263fbd185be9ada12892aa
Wang Hailong <whlsxl@users.noreply.github.com>
2022-07-16 22:13:25 +0800

    Update WindTerm from 2.4.0 to 2.4.1

--- Casks/windterm.rb
+++ Casks/windterm.rb
@@ -1,8 +1,8 @@
 cask "windterm" do
-  version "2.4.0"
+  version "2.4.1"
   sha256 "8dcf18887b4c2b43c39c6c7b32ca093df986f9192334021891488b5a2256b903"
 
-  url "https://github.com/kingToolbox/WindTerm/releases/download/#{version}/WindTerm_#{version}_Mac_Portable_x86_64.dmg"
+  url "https://github.com/kingToolbox/WindTerm/releases/download/2.4.0/WindTerm_#{version}_Mac_Portable_x86_64.dmg"
   name "WindTerm"
   desc "SSH/SFTP/Shell/Telnet/Serial terminal"
   homepage "https://github.com/kingToolbox/WindTerm"
