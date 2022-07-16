d7e5984e880fa593e0fd63e9f6a10a3af5c261a7
p-linnane <patrick@linnane.io>
2022-07-15 22:38:42 -0700

    swiftformat-for-xcode 0.49.13
    Update swiftformat-for-xcode from 0.49.12 to 0.49.13
    
    Closes #127987.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/swiftformat-for-xcode.rb
+++ Casks/swiftformat-for-xcode.rb
@@ -1,6 +1,6 @@
 cask "swiftformat-for-xcode" do
-  version "0.49.12"
-  sha256 "8ea67ecd6d862171cee323d5a78bd1e34357a4bfce136370d02759331098cd2c"
+  version "0.49.13"
+  sha256 "789994e7404f3b79182b6f677128ed3c732e14a75910ce0896903370a0456e2f"
 
   url "https://github.com/nicklockwood/SwiftFormat/releases/download/#{version}/SwiftFormat.for.Xcode.app.zip"
   name "SwiftFormat for Xcode"
