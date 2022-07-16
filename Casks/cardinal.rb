58162910aedc49b100728f321650db84cb615eb2
p-linnane <patrick@linnane.io>
2022-07-15 19:57:02 -0700

    Update cardinal from 22.06 to 22.07

--- Casks/cardinal.rb
+++ Casks/cardinal.rb
@@ -1,6 +1,6 @@
 cask "cardinal" do
-  version "22.06"
-  sha256 "9a0f861bb97cf8ff6708b845462c78fa1acd5c3dbfa3025a3824d6d4929c1f18"
+  version "22.07"
+  sha256 "2d9207193a82bdcd0c237f667579d86db44664d96cabac98687e901d3485f0e6"
 
   url "https://github.com/DISTRHO/Cardinal/releases/download/#{version}/Cardinal-macOS-universal-#{version}.pkg"
   name "Cardinal"
