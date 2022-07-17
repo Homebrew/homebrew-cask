b944e8a3ba1d67269181ed744112a5a3181c1e6d
Reno <singularitti@outlook.com>
2022-07-16 22:09:26 -0400

    Update blockblock from 2.1.4 to 2.1.4

--- Casks/blockblock.rb
+++ Casks/blockblock.rb
@@ -41,5 +41,9 @@ cask "blockblock" do
   desc "Monitors common persistence locations"
   homepage "https://objective-see.com/products/blockblock.html"
 
-  zap trash: "~/Library/Preferences/com.objectiveSee.BlockBlock.plist"
+  zap trash: [
+    "~/Library/Caches/com.objective-see.blockblock.helper",
+    "~/Library/Preferences/com.objectiveSee.BlockBlock.plist",
+    "~/Library/Preferences/com.objective-see.blockblock.helper.plist",
+  ]
 end
