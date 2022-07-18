e66ee440bbeb7839e1bc86410591b0a7270c8f26
p-linnane <patrick@linnane.io>
2022-07-18 09:48:02 -0700

    Update sonic-pi from 4.0.2 to 4.0.3

--- Casks/sonic-pi.rb
+++ Casks/sonic-pi.rb
@@ -1,6 +1,6 @@
 cask "sonic-pi" do
-  version "4.0.2"
-  sha256 "2b20d5855b41a5a9e56aec1a268d6466f5f392ae5a08f6cf63c8151d9c85b7ab"
+  version "4.0.3"
+  sha256 "68c31ce8679759aac565794d962970b86cdd36dce5b59c9d92c9a8810bec2119"
 
   url "https://sonic-pi.net/files/releases/v#{version}/Sonic-Pi-for-Mac-x64-v#{version.dots_to_hyphens}.dmg"
   name "Sonic Pi"
