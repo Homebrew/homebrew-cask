e9558d31bef528aa780435d61cd398c529db1b55
p-linnane <patrick@linnane.io>
2022-07-17 18:53:20 -0700

    Add desc

--- Casks/macsvg.rb
+++ Casks/macsvg.rb
@@ -1,10 +1,11 @@
 cask "macsvg" do
-  version "1.2.0"
-  sha256 "c25105533150f5f9d28e6b08ac20f346f49999f70af55fa0a3aa4d6093aa37ec"
+  version "1.1.7"
+  sha256 "a0dbf7636296accf0cee1df7635e8f2a379d32bca48f055030143e0b11091bc8"
 
-  url "https://github.com/dsward2/macSVG/releases/download/v#{version}/macSVG-v#{version.major_minor}.zip",
+  url "https://github.com/dsward2/macSVG/releases/download/v#{version}/macSVG-v#{version}.zip",
       verified: "github.com/dsward2/macSVG/"
   name "macSVG"
+  desc "App for designing HTML5 Scalable Vector Graphics"
   homepage "https://macsvg.org/"
 
   depends_on macos: ">= :mojave"
