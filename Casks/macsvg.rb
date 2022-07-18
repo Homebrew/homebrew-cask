0d0f880d9eceb6145d1d5784cc69109fe7afea7e
Bevan Kay <email@bevankay.me>
2022-07-18 12:07:21 +1000

    Update url

--- Casks/macsvg.rb
+++ Casks/macsvg.rb
@@ -2,7 +2,7 @@ cask "macsvg" do
   version "1.2.0"
   sha256 "c25105533150f5f9d28e6b08ac20f346f49999f70af55fa0a3aa4d6093aa37ec"
 
-  url "https://github.com/dsward2/macSVG/releases/download/v#{version}/macSVG-v#{version.major_minor}.zip",
+  url "https://github.com/dsward2/macSVG/releases/download/v#{version}/macSVG-v#{version.chomp(".0")}.zip",
       verified: "github.com/dsward2/macSVG/"
   name "macSVG"
   desc "App for designing HTML5 Scalable Vector Graphics"
