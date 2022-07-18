2ddb0f3767687ae824c01d85daba1ac939348efe
p-linnane <patrick@linnane.io>
2022-07-18 11:55:11 -0700

    Fix URL & livecheck

--- Casks/pablodraw.rb
+++ Casks/pablodraw.rb
@@ -2,14 +2,14 @@ cask "pablodraw" do
   version "3.2.1"
   sha256 "91a89ac33ba3e064a7a2b44d5c7c271cbd1303882874e7ce159b8fb058589301"
 
-  url "http://download.picoe.ca/pablodraw/#{version.major_minor}/PabloDrawMac-#{version}.zip"
+  url "https://github.com/cwensley/pablodraw/releases/download/#{version}/PabloDrawMac-#{version}.zip"
   name "PabloDraw"
   desc "Ansi/Ascii text and RIPscrip editor/viewer"
-  homepage "http://picoe.ca/products/pablodraw/"
+  homepage "https://github.com/cwensley/pablodraw/"
 
   livecheck do
-    url :homepage
-    regex(/href=.*?PabloDrawMac[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+    url "https://github.com/cwensley/pablodraw/releases/"
+    strategy :github_latest
   end
 
   app "PabloDraw.app"
