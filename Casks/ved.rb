b9bd855d255c06ca1379f32978614d19e3c36c2f
Adrthegamedev <46429482+Adrthegamedev@users.noreply.github.com>
2022-07-19 14:31:25 +0200

    Add Ved.app v1.9.1

---
+++ Casks/ved.rb
@@ -0,0 +1,23 @@
+cask "ved" do
+  version "1.9.1"
+  sha256 "9ba3ea2de7b1c644bdd44be776ec10dea0411de57f8fa6bdfafd810a1d510766"
+
+  url "https://tolp.nl/ved/files/download/mac/ved_#{version}_mac.dmg"
+  name "ved"
+  desc "External level editor for VVVVVV"
+  homepage "https://tolp.nl/ved/"
+
+  livecheck do
+    url "tolp.nl/ved/files/download/mac/"
+    strategy :page_match
+    regex(/href=.*?ved[ ._-]v?(\d+(?:\.\d+)+)[ ._-]mac\.dmg/i)
+  end
+
+  app "Ved.app"
+
+  zap trash: [
+    "~/Library/Application Support/ved",
+    "~/Library/Caches/nl.tolp.ved",
+    "~/Library/Saved Application State/nl.tolp.ved.savedState",
+  ]
+end
