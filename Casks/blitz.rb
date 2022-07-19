2653f03fd5b9038f93f7086e8c33665877df7bbe
p-linnane <patrick@linnane.io>
2022-07-19 08:50:49 -0700

    blitz 1.17.15
    Update blitz from 1.17.13 to 1.17.15
    
    Closes #128182.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/blitz.rb
+++ Casks/blitz.rb
@@ -1,6 +1,6 @@
 cask "blitz" do
-  version "1.17.13"
-  sha256 "2da85f736a5b6ba6d8099aea1739f59fb99d6b4b4a0f4243412aa8665997d952"
+  version "1.17.15"
+  sha256 "6cccd4767ed131126d1966469d2fa175c451dc4717bd30cd20ad7c44943a3154"
 
   url "https://blitz-stable.blitz.gg/Blitz-#{version}.dmg"
   name "Blitz"
