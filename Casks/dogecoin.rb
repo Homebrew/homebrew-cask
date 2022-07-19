8511e0722fb6e90988a104296112352a98b15e87
p-linnane <patrick@linnane.io>
2022-07-18 21:44:55 -0700

    Adjust livecheck

--- Casks/dogecoin.rb
+++ Casks/dogecoin.rb
@@ -8,6 +8,11 @@ cask "dogecoin" do
   desc "Cryptocurrency"
   homepage "https://dogecoin.com/"
 
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
   app "Dogecoin-Qt.app"
 
   preflight do
