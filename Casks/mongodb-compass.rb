1dde8c423b34a76a285f57ad7cb6915c2b6e1b10
p-linnane <patrick@linnane.io>
2022-07-19 08:01:10 -0700

    mongodb-compass 1.32.4
    Update mongodb-compass from 1.32.3 to 1.32.4
    
    Closes #128163.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/mongodb-compass.rb
+++ Casks/mongodb-compass.rb
@@ -1,6 +1,6 @@
 cask "mongodb-compass" do
-  version "1.32.3"
-  sha256 "877b124536ee5b25653aed5cfa9c7c7522be8d8b5f541f0f0f4ded1414cf2ae7"
+  version "1.32.4"
+  sha256 "e9fb3a4002fffbac064931df8835d3971a33f3ab13b7c6835b0aac80382625fa"
 
   url "https://downloads.mongodb.com/compass/mongodb-compass-#{version}-darwin-x64.dmg"
   name "MongoDB Compass"
