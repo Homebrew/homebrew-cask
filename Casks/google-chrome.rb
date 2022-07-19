5c3944b66909b523362b590008d840038b42dbd3
miccal <miccal.matthews@gmail.com>
2022-07-20 04:21:45 +0800

    Update google-chrome from 103.0.5060.114 to 103.0.5060.134

--- Casks/google-chrome.rb
+++ Casks/google-chrome.rb
@@ -1,5 +1,5 @@
 cask "google-chrome" do
-  version "103.0.5060.114"
+  version "103.0.5060.134"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
