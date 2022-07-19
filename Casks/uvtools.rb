212131d861e22d2daf41710f7a00ac26f4a6af10
p-linnane <patrick@linnane.io>
2022-07-18 22:31:51 -0700

    Update uvtools from 3.5.4 to 3.5.5

--- Casks/uvtools.rb
+++ Casks/uvtools.rb
@@ -1,12 +1,12 @@
 cask "uvtools" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "3.5.4"
+  version "3.5.5"
 
   if Hardware::CPU.intel?
-    sha256 "016ec54a9618a6dd675973f1821e38886147b7b1cf450f9a76751076fb65121f"
+    sha256 "ceef9ca70b2e7dfaab97abfbe1f9b28dd8983e160cbb789037e2402180705ee1"
   else
-    sha256 "a90c5895984e8ac33b666805ca6900a09c18ce81f55e0f716a043cfb0c5e741d"
+    sha256 "792efa831d8106e80fc440d5c9f21137f5edde456b236ad91fb6eba9af9fdbb7"
   end
 
   url "https://github.com/sn4k3/UVtools/releases/download/v#{version}/UVtools_osx-#{arch}_v#{version}.zip"
