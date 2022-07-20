e05c4555e3bc43f07144b433e29ac13c81cda18d
p-linnane <patrick@linnane.io>
2022-07-20 13:59:42 -0700

    Update operator from 1.6.0 to 1.7.0

--- Casks/operator.rb
+++ Casks/operator.rb
@@ -1,6 +1,6 @@
 cask "operator" do
-  version "1.6.0"
-  sha256 "3da2ed81d1a0c2cac8ef5ff9d24698a4c4dc0a1986901969e534b8d694b01642"
+  version "1.7.0"
+  sha256 "f6c30b4fa5cb371264e92be051798b4decdd1438873eb833faa8fe46ce0a4b4b"
 
   url "https://s3.amazonaws.com/operator.versions/release-builds/#{version}/prelude-operator-#{version}-x64.dmg",
       verified: "s3.amazonaws.com/operator.versions/"
