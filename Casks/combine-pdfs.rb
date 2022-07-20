d75770656c484878f968d0c2e11106928f502717
p-linnane <patrick@linnane.io>
2022-07-20 07:47:24 -0700

    Update combine-pdfs from 5.5.2 to 5.6.0

--- Casks/combine-pdfs.rb
+++ Casks/combine-pdfs.rb
@@ -1,6 +1,6 @@
 cask "combine-pdfs" do
-  version "5.5.2"
-  sha256 "2fee2e174fd02525dc599e52d13d14f6c8b5f0a56c76e04c5821ad33d3801651"
+  version "5.6.0"
+  sha256 "51c00784839deb5c41969f2d438e60dc0ee0de63e273079f01ed4f93f61550c8"
 
   url "https://www.monkeybreadsoftware.de/Software/CombinePDFs/CombinePDFs#{version.no_dots}.zip"
   name "Combine PDFs"
