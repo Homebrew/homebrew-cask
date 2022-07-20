2d19d2ba589c59503f3718781a31cad2835a6b17
p-linnane <patrick@linnane.io>
2022-07-20 09:23:40 -0700

    Update app-fair from 0.8.79 to 0.8.83

--- Casks/app-fair.rb
+++ Casks/app-fair.rb
@@ -1,6 +1,6 @@
 cask "app-fair" do
-  version "0.8.79"
-  sha256 "53c5100a5709447a6a85de6c1ae8f9a7062af82ee0397471e5838f2f51a9bffa"
+  version "0.8.83"
+  sha256 "5d5d7ed75607dbca493431155da84fcb1b19d057a1388aa650c48f0e3d7b10d0"
 
   url "https://github.com/App-Fair/App/releases/download/#{version}/App-Fair-macOS.zip",
       verified: "github.com/App-Fair/App/"
