d140b5644febabcd4e3926289698c960206828ae
neutric <5984479+neutric@users.noreply.github.com>
2022-07-20 18:21:39 +0200

    Update unity-ios-support-for-editor from 2022.1.9f1 to 2022.1.10f1

--- Casks/unity-ios-support-for-editor.rb
+++ Casks/unity-ios-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask "unity-ios-support-for-editor" do
-  version "2022.1.9f1,07e076b6d414"
-  sha256 "b91372cfa38343c832912d03d27da2ece509a8436ffa6088a80dfbb3ce61cd9a"
+  version "2022.1.10f1,9aa0f82c4f96"
+  sha256 "582e18f87bf32a041797835420f5e066b56a0b6965e0187fb71556fafb42bcfe"
 
   url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-#{version.csv.first}.pkg",
       verified: "download.unity3d.com/download_unity/"
