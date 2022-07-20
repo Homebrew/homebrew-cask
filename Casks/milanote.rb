265f46e5290d2e424ab7fd7b822a17a8af0158ac
Patrick Linnane <patrick@linnane.io>
2022-07-19 17:59:12 -0700

    Update milanote from 3.1.11 to 3.1.12 (#128062)
    * Update milanote from 3.1.11 to 3.1.12
    
    * Switch livecheck strategy

--- Casks/milanote.rb
+++ Casks/milanote.rb
@@ -1,6 +1,6 @@
 cask "milanote" do
-  version "3.1.11"
-  sha256 "e3921f61feebeaaa5c1fd890f2decf6adf6ec05509df59593a8ac6c75c499dd8"
+  version "3.1.12"
+  sha256 "1eafc8743e5536458100b340ee3624106c155971b95381f2197eebd12fefec23"
 
   url "https://milanote-app-releases.s3.amazonaws.com/Milanote-#{version}.dmg",
       verified: "milanote-app-releases.s3.amazonaws.com/"
@@ -9,8 +9,8 @@ cask "milanote" do
   homepage "https://www.milanote.com/"
 
   livecheck do
-    url "https://www.milanote.com/download-desktop-app"
-    regex(/href=.*?Milanote[._-]?v?(\d+(?:\.\d+)+)\.dmg/i)
+    url "https://milanote-app-releases.s3.amazonaws.com/latest-mac.yml"
+    strategy :electron_builder
   end
 
   app "Milanote.app"
