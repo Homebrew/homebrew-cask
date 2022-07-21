3cdd624a1c5fa5bbb95012f939d8b59d1033b0e0
p-linnane <patrick@linnane.io>
2022-07-21 08:46:39 -0700

    brave-browser 1.41.100.0,141.100
    Update brave-browser from 1.41.99.0 to 1.41.100.0
    
    Closes #128344.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/brave-browser.rb
+++ Casks/brave-browser.rb
@@ -2,12 +2,12 @@ cask "brave-browser" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
   folder = Hardware::CPU.intel? ? "stable" : "stable-arm64"
 
-  version "1.41.99.0,141.99"
+  version "1.41.100.0,141.100"
 
   if Hardware::CPU.intel?
-    sha256 "a758668680235fc06a3477ec0c9df4f4474d8b7464ac48d6ca07d8ec52b8b92b"
+    sha256 "e0b4f5ddafa8e478b5f0e8fd36e8e042718e8cef394005efedb7cb6cd7ea0f92"
   else
-    sha256 "ce7ac2924107d5ba9805328f48414b061b300e10b2b9b9acaa9192f0dd3fd96e"
+    sha256 "2678abc985e8722403bba515f6272e72f81fbd704a8c3b3d1273d07e46aec71c"
   end
 
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.csv.second}/Brave-Browser-#{arch}.dmg",
