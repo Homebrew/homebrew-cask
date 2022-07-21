e382616f70569c04cba91f52a3896cbb435d6c71
p-linnane <patrick@linnane.io>
2022-07-21 08:51:14 -0700

    dcv-viewer 2022.1.4251
    Update dcv-viewer from 2022.1.4241 to 2022.1.4251
    
    Closes #128349.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/dcv-viewer.rb
+++ Casks/dcv-viewer.rb
@@ -1,12 +1,12 @@
 cask "dcv-viewer" do
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 
-  version "2022.1.4241"
+  version "2022.1.4251"
 
   if Hardware::CPU.intel?
-    sha256 "117c07d73ee22f7c6ff666e357bb02b2365e2ccca4cbf825d7478e57d5854d37"
+    sha256 "b2058fc5e2d909f6b5203d2782145f2b8a084b4a5b18323cf6b3eadea5f8e1f7"
   else
-    sha256 "7d96d4af131e36b0f9e33e24360e3020e715ed0fa78054c33bb1c491cec991c8"
+    sha256 "73cc916295366cd232a5eedeccf5ea0eb9f9e83bf5179abb0b5864066b291506"
   end
 
   url "https://d1uj6qtbmh3dt5.cloudfront.net/#{version.major_minor}/Clients/nice-dcv-viewer-#{version}.#{arch}.dmg",
