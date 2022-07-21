d7a7b45db2eb74b6c69f60aed37a58afbbdd8b40
qeeqez <qeeqez@gmail.com>
2022-07-21 23:05:13 +0300

    Update sia-ui from 1.5.6 to 1.5.8

--- Casks/sia-ui.rb
+++ Casks/sia-ui.rb
@@ -1,6 +1,6 @@
 cask "sia-ui" do
-  version "1.5.6"
-  sha256 "fc45e726130ec43026c3ec556290c8b86bfac8fb13e3bc14bde3a1dbf7370e49"
+  version "1.5.8"
+  sha256 "45e8b60f1b5a27a87e60c230fef7cf9ef3b728ec02439b508c9443b1f2a788a0"
 
   url "https://sia.tech/releases/Sia-UI-v#{version}.dmg"
   name "Sia-UI"
