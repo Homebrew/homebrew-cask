0cf391060af5476bba775b45ecf527df5b53f4d3
p-linnane <patrick@linnane.io>
2022-07-22 14:43:47 -0700

    figma 116.2.3
    Update figma from 116.2.2 to 116.2.3
    
    Closes #128453.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/figma.rb
+++ Casks/figma.rb
@@ -1,12 +1,12 @@
 cask "figma" do
   arch = Hardware::CPU.intel? ? "mac" : "mac-arm"
 
-  version "116.2.2"
+  version "116.2.3"
 
   if Hardware::CPU.intel?
-    sha256 "514b8499edb424a3a4bfff103141aa4b9d47436feb5fc41b36c0ce51a0a4ce58"
+    sha256 "a47315bef8e22bc4552be805f8509144cf56ae6e1323cdb0db74ae8e76109470"
   else
-    sha256 "6c3fef2c7c39cafd9de0571b896c93abe4b5c111f9adb2778a4026243e3ad229"
+    sha256 "11cbd7ab52c302d8b601742daee0153e7a18b43ee241ba45dbe2ef28abfcba63"
   end
 
   url "https://desktop.figma.com/#{arch}/Figma-#{version}.zip"
