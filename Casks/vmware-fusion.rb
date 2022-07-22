53479dbd11d6f2ac1e867cf35481ba486d0ed796
p-linnane <patrick@linnane.io>
2022-07-21 20:09:24 -0700

    vmware-fusion 12.2.4,20071091
    Update vmware-fusion from 12.2.3 to 12.2.4
    
    Closes #128411.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/vmware-fusion.rb
+++ Casks/vmware-fusion.rb
@@ -1,8 +1,8 @@
 cask "vmware-fusion" do
-  version "12.2.3,19436697"
-  sha256 "8cd01bd0e7d18b32bfa159bcce54bd885151e3d92d97e5d4a20bcbc09a1c3f4b"
+  version "12.2.4,20071091"
+  sha256 "0b0516f4d5f70e759ae08a40d2e14f487c0b66d84ee467e38972ad013e1f6c7f"
 
-  url "https://download3.vmware.com/software/FUS-#{version.csv.first.no_dots}-New/VMware-Fusion-#{version.csv.first}-#{version.csv.second}_x86.dmg"
+  url "https://download3.vmware.com/software/FUS-#{version.csv.first.no_dots}/VMware-Fusion-#{version.csv.first}-#{version.csv.second}_x86.dmg"
   name "VMware Fusion"
   desc "Create, manage, and run virtual machines"
   homepage "https://www.vmware.com/products/fusion.html"
