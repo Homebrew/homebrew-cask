7fdaec402ee5cb16239667b2208a6c26643775d3
p-linnane <patrick@linnane.io>
2022-07-21 19:55:48 -0700

    switchresx 4.12.1
    Update switchresx from 4.12 to 4.12.1
    
    Closes #128405.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/switchresx.rb
+++ Casks/switchresx.rb
@@ -1,5 +1,5 @@
 cask "switchresx" do
-  version "4.12"
+  version "4.12.1"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.madrau.com/data/switchresx/SwitchResX#{version.major}.zip"
