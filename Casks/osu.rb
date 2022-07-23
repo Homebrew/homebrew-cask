ad040e96e3b92f395ec05aab50677d0c77b0b885
p-linnane <patrick@linnane.io>
2022-07-22 22:42:04 -0700

    osu 2022.723.0
    Update osu from 2022.719.0 to 2022.723.0
    
    Closes #128460.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/osu.rb
+++ Casks/osu.rb
@@ -1,12 +1,12 @@
 cask "osu" do
   arch = Hardware::CPU.intel? ? "Intel" : "Apple.Silicon"
 
-  version "2022.719.0"
+  version "2022.723.0"
 
   if Hardware::CPU.intel?
-    sha256 "ceaf6e355b2418c43cc0b4ee0a80ff174df9d4c9ee1850bddd199246d1f2cb14"
+    sha256 "878418bd8cbc58e4872f59d723d7ce61a970f20d8738ed63a8bf6054e976d9ee"
   else
-    sha256 "6e50882db13f566bc9edc70481f3878b9518cc89b030e184355884941eb4c95b"
+    sha256 "8a10c3d27564f2fdddf94ea7b5d24be21a534522144718d6447a76364fb7c387"
   end
 
   url "https://github.com/ppy/osu/releases/download/#{version}/osu.app.#{arch}.zip"
