69291713a7d2bf7cfff6d54c1d59c514b03ebd34
Reno <singularitti@outlook.com>
2022-07-23 18:39:27 -0400

    Update rectangle from 0.57,63 to 0.57,63

--- Casks/rectangle.rb
+++ Casks/rectangle.rb
@@ -24,6 +24,7 @@ cask "rectangle" do
     "~/Library/Application Scripts/com.knollsoft.RectangleLauncher",
     "~/Library/Application Support/Rectangle",
     "~/Library/Caches/com.knollsoft.Rectangle",
+    "~/Library/HTTPStorages/com.knollsoft.Rectangle",
     "~/Library/Containers/com.knollsoft.RectangleLauncher",
     "~/Library/Preferences/com.knollsoft.Rectangle.plist",
   ]
