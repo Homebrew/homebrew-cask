0cb1862321f3a849c336b0adf97f5d8fa788b674
Leandro Toledo <leandrotoledodesouza@gmail.com>
2022-07-23 13:00:48 -0400

    Update superslicer.rb to reflect ARM download url

--- Casks/superslicer.rb
+++ Casks/superslicer.rb
@@ -1,12 +1,12 @@
 cask "superslicer" do
   arch = Hardware::CPU.intel? ? "" : "arm_"
 
-  version "2.4.58.3,220714"
-
   if Hardware::CPU.intel?
+    version "2.4.58.3,220714"
     sha256 "4fb4a65d2f56bf75867257ea40a7b32515fbb598e992c69ee8a94ef4579781c5"
   else
-    sha256 "a389e53f4c8c3228933618f71842a16c96033a1dd74199337641df99f6b7f5b1"
+    version "2.4.58.3,220715"
+    sha256 "c1cfb77477e0ddaddbafbdfa9e31a345a8da590790350aca23c180fd081c645a"
   end
 
   url "https://github.com/supermerill/SuperSlicer/releases/download/#{version.csv.first}/SuperSlicer_#{version.csv.first}_macos_#{arch}#{version.csv.second}.dmg"
@@ -17,7 +17,7 @@ cask "superslicer" do
   livecheck do
     url "https://github.com/supermerill/SuperSlicer/releases/latest"
     strategy :page_match do |page|
-      match = page.match(%r{href=.*?/SuperSlicer_(\d+(?:\.\d+)+)_macos_(\d+)\.dmg}i)
+      match = page.match(%r{href=.*?/SuperSlicer_(\d+(?:\.\d+)+)_macos_#{arch}(\d+)\.dmg}i)
       next if match.blank?
 
       "#{match[1]},#{match[2]}"
