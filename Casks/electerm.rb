ad12ed7730ecd6a1c756d35df9fd5ba2a16435cd
p-linnane <patrick@linnane.io>
2022-07-23 07:56:44 -0700

    electerm 1.22.1
    Update electerm from 1.21.93 to 1.22.1
    
    Closes #128469.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/electerm.rb
+++ Casks/electerm.rb
@@ -1,12 +1,12 @@
 cask "electerm" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "1.21.93"
+  version "1.22.1"
 
   if Hardware::CPU.intel?
-    sha256 "356f6cc5b032370c3488394e2b7b9f9fb3a0ee0a1b43239eecf65d2167a60345"
+    sha256 "a499412a8d57f3a76b777987548353242fefae6c557b3c08352b9c4417f9e226"
   else
-    sha256 "f3e8bb0a8fb0da8b2e68a7f1414336d0664433f52328156a3fe185a33c2c176e"
+    sha256 "4cd06d56497f57d62451cc7cfad6f698927a7e5cf130bee4f54082e0a42fc1c0"
   end
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac-#{arch}.dmg"
