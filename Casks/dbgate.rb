4c6caf74642ea86355c040002e4842e700ce0f3f
Porkepix <porkepix@gmail.com>
2022-07-24 11:44:06 +0200

    Update dbgate from 5.0.8 to 5.0.9

--- Casks/dbgate.rb
+++ Casks/dbgate.rb
@@ -1,6 +1,6 @@
 cask "dbgate" do
-  version "5.0.8"
-  sha256 "b80c7c79b37fb142bb46bed44675cf6ac3328402d80d3e91adc52faac7128469"
+  version "5.0.9"
+  sha256 "c0f410d3116b5b1388ce6a15d59c5aa6a0be7d25011a2eefb4e47ec3aa889a26"
 
   url "https://github.com/dbgate/dbgate/releases/download/v#{version}/dbgate-#{version}-mac_universal.dmg",
       verified: "github.com/dbgate/dbgate/"
