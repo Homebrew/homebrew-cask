000765ff08220095ca62956918c55483ec69ca61
Lutz <lutzroeder@users.noreply.github.com>
2022-07-24 22:52:25 +0000

    netron 5.9.6
    Update Netron to 5.9.6
    
    Closes #128512.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/netron.rb
+++ Casks/netron.rb
@@ -1,6 +1,6 @@
 cask "netron" do
-  version "5.9.5"
-  sha256 "25a960519fa4d74ceb300cbe89c9e8a7f3cea29593d9efd229366492b946f325"
+  version "5.9.6"
+  sha256 "bf376d2c7a543ddc790af801e06f5058c82e7e427edc30b402ae25e395ca360a"
 
   url "https://github.com/lutzroeder/netron/releases/download/v#{version}/Netron-#{version}-mac.zip"
   name "Netron"
