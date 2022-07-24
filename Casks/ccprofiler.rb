d714aee19720be281fdd88321fa723e495ab2e99
Bevan Kay <email@bevankay.me>
2022-07-24 19:36:24 +1000

    Update ccprofiler.rb

--- Casks/ccprofiler.rb
+++ Casks/ccprofiler.rb
@@ -2,7 +2,7 @@ cask "ccprofiler" do
   version "1.1.1"
   sha256 "d841a8f473b041ddb95516f6fd1afbae754be866c3df0e9820ca81b21162eb41"
 
-  url "https://downloads.xrite.com/Downloads/Software/ccProfiler/v#{version.major_minor_patch}/mac/ccProfiler.zip",
+  url "https://downloads.xrite.com/Downloads/Software/ccProfiler/v#{version}/mac/ccProfiler.zip",
       verified: "xrite.com"
   name "ccProfiler"
   desc "Monitor colibration for Calibrite ColorChecker Display Pro & Plus"
