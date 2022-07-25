dca9aaf751a89ad9aa9bdd89146aec4de5403e04
Patrick Linnane <patrick@linnane.io>
2022-07-25 15:22:33 -0700

    Update strawberry from 1.0.6 to 1.0.7

--- Casks/strawberry.rb
+++ Casks/strawberry.rb
@@ -1,6 +1,6 @@
 cask "strawberry" do
-  version "1.0.6"
-  sha256 "ced3a157d95f5afa3511284ac5a89e3a431470ab1a8cfc578e6dfc8d1d2245e5"
+  version "1.0.7"
+  sha256 "0b8d1bbf41747d7d7a26bcee9bb5a5b9d098b2e6be43e7833fdecb541fb74780"
 
   url "https://github.com/strawberrymusicplayer/strawberry/releases/download/#{version}/strawberry-#{version}-x86_64.dmg",
       verified: "github.com/strawberrymusicplayer/strawberry/"
