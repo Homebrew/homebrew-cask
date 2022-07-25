1c6a18d90246179c22f4e0ab7cc8ce3e574bd89f
Marcell Purham <mpurham@cybercell.local>
2022-07-25 14:57:55 -0500

    New cask: Polypad

---
+++ Casks/polypad.rb
@@ -0,0 +1,17 @@
+cask "polypad" do
+  version "1.3.0"
+  sha256 "8640fca759485073b686d76fdda6c8c532bce480c60ea7516947aa8799420e0d"
+
+  url "https://get.polypad.io/download/mac"
+  name "Polypad"
+  desc "Scriptable Textpad for Developers"
+  homepage "https://polypad.io/"
+
+  livecheck do
+    url "https://polypad-releases.s3.us-west-1.amazonaws.com/mac/latest-mac.yml"
+    strategy :electron_builder
+  end
+
+  app "Polypad.app"
+end
+
