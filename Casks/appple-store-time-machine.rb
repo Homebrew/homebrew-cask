1601bc155dda3d25c0dda0565014c0c73c324c95
Jacob Ford <j@cobford.com>
2022-07-25 10:42:10 -0400

    Cask basics

---
+++ Casks/appple-store-time-machine.rb
@@ -0,0 +1,11 @@
+cask "appple-store-time-machine" do
+  version "1.0"
+  sha256 ""
+
+  url "https://files.departmentmap.store/file/AppleStoreTimeMachine/Shop+Different.dmg"
+  name "appple-store-time-machine"
+  desc "3D reconstruction of Apple Retail Stores on their opening days"
+  homepage "https://departmentmap.store/timemachine/"
+
+  app ""
+end
