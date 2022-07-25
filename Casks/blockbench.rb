d4b78b86d62b08c7009f04be67c32bc0d00fd3f7
Patrick Linnane <patrick@linnane.io>
2022-07-25 15:19:10 -0700

    blockbench 4.3.1
    Update blockbench from 4.3.0 to 4.3.1
    
    Closes #128575.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/blockbench.rb
+++ Casks/blockbench.rb
@@ -1,12 +1,12 @@
 cask "blockbench" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "4.3.0"
+  version "4.3.1"
 
   if Hardware::CPU.intel?
-    sha256 "6d02a072a25277cadb11899c0d32639a758a2b584172644009f24a0f4feb0e51"
+    sha256 "6a18203516ec827f119a78f637825448a3931fd6caad0d81992087478ac83d9e"
   else
-    sha256 "adf989575936a4f95e4d746ff563479c1d888f85751e4a8df26ef80857a557be"
+    sha256 "c121000d3c330bb8080977806f90a746a5c85680957dc736f8389f4fcc0fc145"
   end
 
   url "https://github.com/JannisX11/blockbench/releases/download/v#{version}/Blockbench_#{arch}_#{version}.dmg",
