40580999f2fecf394db3671519bc6e5769bda9af
Jacob Ford <j@cobford.com>
2022-07-25 16:04:15 -0400

    Add shasum & app

--- Casks/apple-store-time-machine.rb
+++ Casks/apple-store-time-machine.rb
@@ -1,11 +1,11 @@
 cask "apple-store-time-machine" do
   version "1.0"
-  sha256 ""
+  sha256 "ccd9cdc75bbe1766a260bba5cbd5055fca9717b170fe6d010c7b352f672ad3d3"
 
   url "https://files.departmentmap.store/file/AppleStoreTimeMachine/Shop+Different.dmg"
   name "appple-store-time-machine"
   desc "3D reconstruction of Apple Retail Stores on their opening days"
   homepage "https://departmentmap.store/timemachine/"
 
-  app ""
+  app "Shop Different.app"
 end
