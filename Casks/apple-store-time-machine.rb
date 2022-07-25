d7e5d81929a89bfb70afdbfaca2ab49744529f9d
Jacob Ford <j@cobford.com>
2022-07-25 16:09:53 -0400

    Name app

--- Casks/apple-store-time-machine.rb
+++ Casks/apple-store-time-machine.rb
@@ -3,7 +3,7 @@ cask "apple-store-time-machine" do
   sha256 "ccd9cdc75bbe1766a260bba5cbd5055fca9717b170fe6d010c7b352f672ad3d3"
 
   url "https://files.departmentmap.store/file/AppleStoreTimeMachine/Shop+Different.dmg"
-  name "appple-store-time-machine"
+  name "Apple Store Time Machine"
   desc "3D reconstruction of Apple Retail Stores on their opening days"
   homepage "https://departmentmap.store/timemachine/"
 
