d22f71440bc3d844d6e7840e3bb5cd4961ce89a5
p-linnane <patrick@linnane.io>
2022-07-25 09:57:57 -0700

    Fix sha256

--- Casks/ferdium.rb
+++ Casks/ferdium.rb
@@ -4,9 +4,9 @@ cask "ferdium" do
   version "6.0.0"
 
   if Hardware::CPU.intel?
-    sha256 "0fa9f37f3ccdca4562f080d260390f3942ad4c9afcab8a2e34d970c3d3bd0edd"
-  else
     sha256 "131c29c39275143d294b022581556cf9ac2ed458efc13fea06f677205d854da1"
+  else
+    sha256 "c9b4a05a0654e57065df2b2d56738178d83345f4de157ef72daa950b48e51188"
   end
 
   url "https://github.com/ferdium/ferdium-app/releases/download/v#{version}/Ferdium-mac-#{version}-#{arch}.dmg",
