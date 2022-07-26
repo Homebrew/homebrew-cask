f77f1e2da56d0a6aff84f184e01d126ccce8ea85
Mr-TTOT <51176964+Mr-TTOT@users.noreply.github.com>
2022-07-26 03:47:22 -0600

    Create ccprofiler cask (#128490)
    * ccprofiler
    
    Create cask
    
    * ccprofiler
    
    Fix syntax errors
    
    * Update ccprofiler.rb
    
    * Update ccprofiler.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

---
+++ Casks/ccprofiler.rb
@@ -0,0 +1,28 @@
+cask "ccprofiler" do
+  version "1.1.1"
+  sha256 "d841a8f473b041ddb95516f6fd1afbae754be866c3df0e9820ca81b21162eb41"
+
+  url "https://downloads.xrite.com/Downloads/Software/ccProfiler/v#{version}/mac/ccProfiler.zip",
+      verified: "xrite.com"
+  name "ccProfiler"
+  desc "Monitor colibration for Calibrite ColorChecker Display Pro & Plus"
+  homepage "https://calibrite.com/"
+
+  livecheck do
+    url "https://calibrite.com/us/software-downloads/"
+    regex(%r{href=.*?v?(\d+(?:\.\d+)+)/mac/ccProfiler\.zip}i)
+  end
+
+  depends_on macos: ">= :catalina"
+
+  pkg "ccProfiler.pkg"
+
+  uninstall pkgutil: [
+    "com.xrite.xritedeviceservices.installer.pkg",
+    "com.calibrite.ccprofiler.pkg",
+  ]
+
+  caveats do
+    reboot
+  end
+end
