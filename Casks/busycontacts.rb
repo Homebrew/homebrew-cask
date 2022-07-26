ad36a046204f62d6b75140a21c19b88e745f634c
Patrick Linnane <patrick@linnane.io>
2022-07-25 20:56:41 -0700

    Bump to 2022.3.1,2022-07-26-06-29

--- Casks/busycontacts.rb
+++ Casks/busycontacts.rb
@@ -1,6 +1,6 @@
 cask "busycontacts" do
-  version "2022.3.1,2022-07-26-04-35"
-  sha256 "6a50cb066377726c9b19155c26ed0c3c0df18f8fb4d9149f859f46da01fd8138"
+  version "2022.3.1,2022-07-26-06-29"
+  sha256 "8cb06341046e39a104c62c08f9be4266305aacf8a4955549a641cd797c4d8cd2"
 
   url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bct-#{version.csv.first}-#{version.csv.second}.zip",
       verified: "7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/"
