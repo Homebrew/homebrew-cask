29ffe5780feaa9884fcc640ec3f663622429e13e
Patrick Linnane <patrick@linnane.io>
2022-07-25 18:55:48 -0700

    Update busycontacts from 1.6.7 to 2022.3.1

--- Casks/busycontacts.rb
+++ Casks/busycontacts.rb
@@ -1,6 +1,6 @@
 cask "busycontacts" do
-  version "1.6.7,2022-06-21-00-37"
-  sha256 "315e24659f187bc98b3758d217f745fecc7a93411200a99e388b99515f6ebf42"
+  version "2022.3.1,2022-07-26-04-35"
+  sha256 "6a50cb066377726c9b19155c26ed0c3c0df18f8fb4d9149f859f46da01fd8138"
 
   url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bct-#{version.csv.first}-#{version.csv.second}.zip",
       verified: "7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/"
