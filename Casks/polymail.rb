3007baf08c7b3cf721cb78953ef58c8bfd05e429
Honza Hrubý <41996274+honzahruby@users.noreply.github.com>
2022-07-26 17:05:29 +0200

    Fix styling

--- Casks/polymail.rb
+++ Casks/polymail.rb
@@ -1,13 +1,13 @@
 cask "polymail" do
-  version "2.2.6"
   arch = Hardware::CPU.intel? ? "" : "-arm64"
   
+  version "2.2.6"
+
   if Hardware::CPU.intel?
     sha256 "7f0545b2a99ab9fa515b6f78437a53226e6e1f788a8ed411e6bbd3fff5c09249"
   else
     sha256 "e5d7ddf964e817ca604612d85ffe26381eb9d3f73962b40af32b5ef071833a2b"
   end
-  
 
   url "https://sparkle-updater.polymail.io/macos#{arch}/builds/Polymail-v#{version}.zip"
   name "Polymail"
