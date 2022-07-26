aea52be3757ad9397b4f284bece4aa12913eadea
Patrick Linnane <patrick@linnane.io>
2022-07-25 18:55:48 -0700

    busycontacts 2022.3.1,2022-07-26-06-29
    * Update busycontacts from 1.6.7 to 2022.3.1
    * Bump to 2022.3.1,2022-07-26-06-29
    
    Closes #128586.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/busycontacts.rb
+++ Casks/busycontacts.rb
@@ -1,6 +1,6 @@
 cask "busycontacts" do
-  version "1.6.7,2022-06-21-00-37"
-  sha256 "315e24659f187bc98b3758d217f745fecc7a93411200a99e388b99515f6ebf42"
+  version "2022.3.1,2022-07-26-06-29"
+  sha256 "8cb06341046e39a104c62c08f9be4266305aacf8a4955549a641cd797c4d8cd2"
 
   url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bct-#{version.csv.first}-#{version.csv.second}.zip",
       verified: "7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/"
