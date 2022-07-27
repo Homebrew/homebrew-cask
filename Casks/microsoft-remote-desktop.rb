b9f75ed2f2926830d5c8749537c4b81c84e788fe
Patrick Linnane <patrick@linnane.io>
2022-07-26 22:08:43 -0700

    Update microsoft-remote-desktop from 10.7.7 to 10.7.8 (#128669)
    * Update microsoft-remote-desktop from 10.7.7 to 10.7.8
    
    * Quit errorreporting

--- Casks/microsoft-remote-desktop.rb
+++ Casks/microsoft-remote-desktop.rb
@@ -1,6 +1,6 @@
 cask "microsoft-remote-desktop" do
-  version "10.7.7"
-  sha256 "f109329c038819c4bd75d667572154435aa96ce73e047e8a1bff0c9bc05f6973"
+  version "10.7.8"
+  sha256 "9eac38256f1e152e04545c342159bfa165e11a44761384d2820c3fd0db31eaf6"
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Remote_Desktop_#{version}_installer.pkg",
       verified: "officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/"
@@ -29,6 +29,7 @@ cask "microsoft-remote-desktop" do
             quit:      [
               "com.microsoft.autoupdate2",
               "com.microsoft.autoupdate.fba",
+              "com.microsoft.errorreporting",
             ]
 
   zap trash: [
