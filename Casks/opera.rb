1f0dffe8ae37ef32b7e5ec68d19da78dd3cfafd6
Patrick Linnane <patrick@linnane.io>
2022-07-27 11:43:18 -0700

    Update opera from 89.0.4447.51 to 89.0.4447.71

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask "opera" do
-  version "89.0.4447.51"
-  sha256 "92f62abf5adcd16fec123ac6dd3c95513ebf3e5d4778c76eac7a8bef848e20c2"
+  version "89.0.4447.71"
+  sha256 "2b866c502323134dda2dd15426fc279a786b9176a5e038ff753fafbfafc2cb48"
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   name "Opera"
