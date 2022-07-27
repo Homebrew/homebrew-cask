e612506373de51d74c59ba58a910158249c94dc3
Patrick Linnane <patrick@linnane.io>
2022-07-26 17:48:02 -0700

    mplab-xc32: rebuild
    Fix URL & livecheck
    
    Closes #128670.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/mplab-xc32.rb
+++ Casks/mplab-xc32.rb
@@ -2,14 +2,14 @@ cask "mplab-xc32" do
   version "4.10"
   sha256 "6ffbd3e1b6922e1e2b661d63b1b5058528e60c52b2d25d04a8a551859f35cdf4"
 
-  url "https://ww1.microchip.com/downloads/en/DeviceDoc/xc32-v#{version}-osx-installer.dmg"
+  url "https://ww1.microchip.com/downloads/aemDocuments/documents/DEV/ProductDocuments/SoftwareTools/xc32-v#{version}-osx-installer.dmg"
   name "MPLab XC32 Compiler"
   desc "Compiler for 32-bit PIC and SAM MCUs and MPUs"
   homepage "https://www.microchip.com/mplab/compilers"
 
   livecheck do
-    url "https://www.microchip.com/en-us/tools-resources/develop/mplab-xc-compilers"
-    regex(%r{href=.*?downloads/en/DeviceDoc/xc32[._-]v?(\d+(?:\.\d+)+)-osx-installer\.dmg}i)
+    url "https://www.microchip.com/en-us/tools-resources/develop/mplab-xc-compilers/downloads-documentation"
+    regex(%r{href=.*?ProductDocuments/SoftwareTools/xc32[._-]v?(\d+(?:\.\d+)+)-osx-installer\.dmg}i)
   end
 
   installer script: {
