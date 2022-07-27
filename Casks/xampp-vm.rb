8942ac946345a8e943ce25e6103828c0165d123b
Hans <hamawulf+github@posteo.net>
2022-07-27 09:28:17 +0200

    fix url

--- Casks/xampp-vm.rb
+++ Casks/xampp-vm.rb
@@ -2,7 +2,7 @@ cask "xampp-vm" do
   version "8.1.6-0"
   sha256 "11be67f9c8bd955a07bbc93af9cf126fda20ba2b60eef66f8c92bb655776cedb"
 
-  url "https://www.apachefriends.org/xampp-files/#{version.split("-").first}/xampp-osx-#{version}-vm.dmg"
+  url "https://downloadsapachefriends.global.ssl.fastly.net/#{version.split("-").first}/xampp-osx-#{version}-vm.dmg"
   name "XAMPP-VM"
   desc "Virtual machine with apache distribution containing MySQL, PHP, and Perl"
   homepage "https://www.apachefriends.org/index.html"
