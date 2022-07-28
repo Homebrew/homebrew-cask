736ffcb8585e83dee652a2d687594c91acb1880b
Patrick Linnane <patrick@linnane.io>
2022-07-28 11:28:01 -0700

    Update goodsync from 11.11.5 to 11.11.6

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,5 +1,5 @@
 cask "goodsync" do
-  version "11.11.5"
+  version "11.11.6"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
