39cb48971b5ba36ba5b1af2e80d4055abeca5af5
Patrick Linnane <patrick@linnane.io>
2022-07-28 12:41:22 -0700

    Update electronmail from 5.0.1 to 5.0.2

--- Casks/electronmail.rb
+++ Casks/electronmail.rb
@@ -1,6 +1,6 @@
 cask "electronmail" do
-  version "5.0.1"
-  sha256 "ebf13fc69f51d7517e256a7c078b6f2dbb87e4f14e845c2eb50d4a1d4284452a"
+  version "5.0.2"
+  sha256 "72365ccfe7e80d412f7fcb4a702699f2a0cba4a6d6239b9ec18116ec4db4f7ce"
 
   url "https://github.com/vladimiry/ElectronMail/releases/download/v#{version}/electron-mail-#{version}-mac-x64.dmg"
   name "ElectronMail"
