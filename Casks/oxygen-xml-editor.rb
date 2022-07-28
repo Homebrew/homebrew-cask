1b8bdd40221e663f76072a5c475c37b1636e596f
Patrick Linnane <patrick@linnane.io>
2022-07-28 09:00:42 -0700

    Update oxygen-xml-editor from 24.1,2022062007 to 24.1,2022072507

--- Casks/oxygen-xml-editor.rb
+++ Casks/oxygen-xml-editor.rb
@@ -1,7 +1,7 @@
 cask "oxygen-xml-editor" do
   arch = Hardware::CPU.intel? ? "" : "-aarch64"
 
-  version "24.1,2022062007"
+  version "24.1,2022072507"
   sha256 :no_check
 
   url "https://www.oxygenxml.com/InstData/Editor/MacOSX/VM/oxygen-openjdk#{arch}.dmg"
