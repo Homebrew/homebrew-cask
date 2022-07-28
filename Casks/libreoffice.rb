26b0f2a7f88474894f06273ae0c0f390cb1e4d2a
Rylan Polster <rslpolster@gmail.com>
2022-07-28 16:02:38 -0400

    libreoffice: use `on_{system}` blocks

--- Casks/libreoffice.rb
+++ Casks/libreoffice.rb
@@ -1,12 +1,13 @@
 cask "libreoffice" do
-  arch, folder = Hardware::CPU.intel? ? ["x86-64", "x86_64"] : ["aarch64", "aarch64"]
-
+  arch = "aarch64"
+  folder = "aarch64"
   version "7.3.5"
+  sha256 "dfd30d5d520959ee1b65a620d748a0ac5535d1a9e811f83fa35a14dc8d3d736d"
 
-  if Hardware::CPU.intel?
+  on_intel do
+    arch = "x86-64"
+    folder = "x86_64"
     sha256 "9534e444b3102a653f3d0c0e3fb54efb20caf2c91ce121b2db00d2531ab8b7fa"
-  else
-    sha256 "dfd30d5d520959ee1b65a620d748a0ac5535d1a9e811f83fa35a14dc8d3d736d"
   end
 
   url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/#{folder}/LibreOffice_#{version}_MacOS_#{arch}.dmg",
