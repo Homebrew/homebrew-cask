4386b43a8f6d6884da4ece1ed6b3499b77ca0583
Rylan Polster <rslpolster@gmail.com>
2022-07-28 16:02:38 -0400

    ngrok: use `on_{system}` blocks

--- Casks/ngrok.rb
+++ Casks/ngrok.rb
@@ -1,12 +1,12 @@
 cask "ngrok" do
-  arch = Hardware::CPU.intel? ? "amd64" : "arm64"
+  arch = "arm64"
+  version "3.0.6,91H3cQoKGUw,a"
+  sha256 "f17dd2755abc3db298bfac5279c0d633d08fc96e69708a332d9c65b57328a9f6"
 
-  if Hardware::CPU.intel?
+  on_intel do
+    arch = "amd64"
     version "3.0.6,mStkVrofaG9,a"
     sha256 "26695c41b93e6437b797fcc0f6e78242ec8c665e8e57f436aa6305b9237e3ac1"
-  else
-    version "3.0.6,91H3cQoKGUw,a"
-    sha256 "f17dd2755abc3db298bfac5279c0d633d08fc96e69708a332d9c65b57328a9f6"
   end
 
   url "https://bin.equinox.io/#{version.csv.third}/#{version.csv.second}/ngrok-v#{version.major}-#{version.csv.first}-stable-darwin-#{arch}.zip",
