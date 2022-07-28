f5d24b5f2d10d1e5f24b65565e16feb587c9f502
Rylan Polster <rslpolster@gmail.com>
2022-07-28 16:02:38 -0400

    iterm2: use `on_{system}` blocks

--- Casks/iterm2.rb
+++ Casks/iterm2.rb
@@ -1,11 +1,11 @@
 cask "iterm2" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  if MacOS.version <= :high_sierra
+  version "3.4.16"
+  sha256 "b0941a008ead9f680e9f4937698b9b849acbb4e30ed1f3f100e3616cd6d49c0b"
+
+  on_high_sierra :or_older do
     version "3.3.12"
     sha256 "6811b520699e8331b5d80b5da1e370e0ed467e68bc56906f08ecfa986e318167"
-  else
-    version "3.4.16"
-    sha256 "b0941a008ead9f680e9f4937698b9b849acbb4e30ed1f3f100e3616cd6d49c0b"
   end
 
   url "https://iterm2.com/downloads/stable/iTerm2-#{version.dots_to_underscores}.zip"
