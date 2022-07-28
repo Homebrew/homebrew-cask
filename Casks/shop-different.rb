945bc8bab7217082440ef7e08f9200fa281dc582
Jacob Ford <j@cobford.com>
2022-07-28 05:37:46 -0400

    Add @MichaelSteeber's Apple Store Time Machine (#128569)

---
+++ Casks/shop-different.rb
@@ -0,0 +1,17 @@
+cask "shop-different" do
+  version "1.0"
+  sha256 :no_check
+
+  url "https://files.departmentmap.store/file/AppleStoreTimeMachine/Shop+Different.dmg"
+  name "Apple Store Time Machine"
+  desc "3D reconstruction of Apple Retail Stores on their opening days"
+  homepage "https://departmentmap.store/timemachine/"
+
+  app "Shop Different.app"
+
+  zap trash: [
+    "~/Library/Logs/Michael Steeber/Shop Different",
+    "~/Library/Preferences/com.MichaelSteeber.ShopDifferent.plist",
+    "~/Library/Saved Application State/com.MichaelSteeber.ShopDifferent.savedState",
+  ]
+end
