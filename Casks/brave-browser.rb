372007a16f6251c7548818e198370d8ca6358cee
Rylan Polster <rslpolster@gmail.com>
2022-07-28 16:02:37 -0400

    brave-browser: use `on_{system}` blocks

--- Casks/brave-browser.rb
+++ Casks/brave-browser.rb
@@ -1,13 +1,13 @@
 cask "brave-browser" do
-  arch = Hardware::CPU.intel? ? "x64" : "arm64"
-  folder = Hardware::CPU.intel? ? "stable" : "stable-arm64"
-
+  arch = "arm64"
+  folder = "stable-arm64"
   version "1.41.100.0,141.100"
+  sha256 "2678abc985e8722403bba515f6272e72f81fbd704a8c3b3d1273d07e46aec71c"
 
-  if Hardware::CPU.intel?
+  on_intel do
+    arch = "x64"
+    folder = "stable"
     sha256 "e0b4f5ddafa8e478b5f0e8fd36e8e042718e8cef394005efedb7cb6cd7ea0f92"
-  else
-    sha256 "2678abc985e8722403bba515f6272e72f81fbd704a8c3b3d1273d07e46aec71c"
   end
 
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.csv.second}/Brave-Browser-#{arch}.dmg",
