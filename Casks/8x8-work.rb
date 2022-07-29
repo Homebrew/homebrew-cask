ef07dfd92dd4a67c9dc9c2dc3413245c37096760
Patrick Linnane <patrick@linnane.io>
2022-07-28 21:54:18 -0700

    Update 8x8-work from 7.20.3-3 to 7.21.3-4 (#128774)

--- Casks/8x8-work.rb
+++ Casks/8x8-work.rb
@@ -1,6 +1,6 @@
 cask "8x8-work" do
-  version "7.20.3-3"
-  sha256 "8933dc89cb1a988a3e78c98dbdcf4ce52ce82b6c7c61ddb9891c38f10883c771"
+  version "7.21.3-4"
+  sha256 "03c51835ac312f96e50c87e44ef7a50aed4d9f5c6de97f4c3b701202d7db71bc"
 
   url "https://vod-updates.8x8.com/ga/work-dmg-v#{version}.dmg"
   name "8x8_work"
