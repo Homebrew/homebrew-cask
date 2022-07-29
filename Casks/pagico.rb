f671e1b7ed58f42ca3c3d6adccba002818667572
Patrick Linnane <patrick@linnane.io>
2022-07-28 21:53:07 -0700

    Update pagico from 10,20220727 to 10,20220728 (#128786)

--- Casks/pagico.rb
+++ Casks/pagico.rb
@@ -1,6 +1,6 @@
 cask "pagico" do
-  version "10,20220727"
-  sha256 "7440fb6a328d6e2c518dff714cc07daa5d08f7f5128c37c42b4fd6c3d4f9a0b2"
+  version "10,20220728"
+  sha256 "e9220216d81273f7a061d88ae68117ef392195071400375d0df3e0b6c66766f3"
 
   url "https://www.pagico.com/downloads/Pagico_macOS_r#{version.csv.second}.dmg"
   name "Pagico"
