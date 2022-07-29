009d04934eea8e2feda156691e4e99e11e3f8a18
Patrick Linnane <patrick@linnane.io>
2022-07-29 09:42:00 -0700

    Update wifispoof from 3.8.4.1 to 3.8.5

--- Casks/wifispoof.rb
+++ Casks/wifispoof.rb
@@ -1,6 +1,6 @@
 cask "wifispoof" do
-  version "3.8.4.1"
-  sha256 "a580ae3c542c4704016216438ad63b34e87168e0a0b3a1bcc720ccbaa5cc26ac"
+  version "3.8.5"
+  sha256 "aad9f195108e1ab0329ebacc04d1c69a90a8ca1c08f28ee4ffe8d0ecf4312a3c"
 
   url "https://sweetpproductions.com/products/wifispoof#{version.major}/WiFiSpoof#{version.major}.dmg",
       verified: "sweetpproductions.com/products/"
