4b279a48db0b1d7f4348746d26a8999bd1be927d
Jens Hoffmann <jehoffma@gmail.com>
2022-07-31 01:24:33 +0200

    Add ImHex v1.19.3

---
+++ Casks/imhex.rb
@@ -0,0 +1,23 @@
+cask "imhex" do
+  version "1.19.3"
+  sha256 "2bce4b8e5617b91bf771ed58654b9fcd88f9a195d63e83eb82fdc6514d97610c"
+
+  url "https://github.com/WerWolv/ImHex/releases/download/v#{version}/imhex-#{version}-Darwin.dmg",
+      verified: "github.com/WerWolv/ImHex"
+  name "ImHex"
+  desc "Hex editor for reverse engineers"
+  homepage "https://imhex.werwolv.net/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "imhex.app"
+
+  zap trash: [
+    "~/Library/Application Support/imhex",
+    "~/Library/Saved Application State/net.WerWolv.ImHex.savedState",
+    "~/Library/Preferences/net.WerWolv.ImHex.plist",
+  ]
+end
