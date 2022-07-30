3e62fe10cbf996a8646ef020a63d98b333f0034b
Patrick Linnane <patrick@linnane.io>
2022-07-30 15:24:00 -0700

    Update Casks/anypointstudio.rb
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Casks/anypointstudio.rb
+++ Casks/anypointstudio.rb
@@ -2,8 +2,8 @@ cask "anypointstudio" do
   version "7.13"
   sha256 "0787195aa9d4adfdb5c1c01cc6e3d245eceb89828caed6c25b485f04871ab8e6"
 
-  foldersuffix = version.patch == "" ? ".0-GA" : "-U#{version.patch}"
-  zipsuffix = version.patch == "" ? ".0" : ""
+  foldersuffix = version.patch.blank? ? ".0-GA" : "-U#{version.patch}"
+  zipsuffix = version.patch.blank? ? ".0" : ""
 
   url "https://mule-studio.s3.amazonaws.com/#{version}#{foldersuffix}/AnypointStudio-#{version}#{zipsuffix}-macos64.zip",
       verified: "mule-studio.s3.amazonaws.com/"
