e67d10b5f6755dd2827f3e3db002fa2fe0fef87f
Jacob Ford <j@cobford.com>
2022-07-30 11:26:38 -0400

    Update shop-different from 1.0 to 1.0.1

--- Casks/shop-different.rb
+++ Casks/shop-different.rb
@@ -1,8 +1,8 @@
 cask "shop-different" do
-  version "1.0"
+  version "1.0.1"
   sha256 :no_check
 
-  url "https://files.departmentmap.store/file/AppleStoreTimeMachine/Shop+Different.dmg"
+  url "https://files.departmentmap.store/file/AppleStoreTimeMachine/Shop+Different+#{version}.dmg"
   name "Apple Store Time Machine"
   desc "3D reconstruction of Apple Retail Stores on their opening days"
   homepage "https://departmentmap.store/timemachine/"
