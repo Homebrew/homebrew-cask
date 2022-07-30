c188c24739b36143b3634306476e62f78986b713
Patrick Linnane <patrick@linnane.io>
2022-07-29 21:00:50 -0700

    Update hbuilderx from 3.4.18.20220630 to 3.5.3.20220729

--- Casks/hbuilderx.rb
+++ Casks/hbuilderx.rb
@@ -1,6 +1,6 @@
 cask "hbuilderx" do
-  version "3.4.18.20220630"
-  sha256 "4d083aaaeedf021b4d2e12502dc6f630b2ff071696618ac36574b178d4e98a07"
+  version "3.5.3.20220729"
+  sha256 "5786de23ae4be40066a645bc190fa12a58d87cd8344c79f3d7cb6a0a66e0d44a"
 
   url "https://download1.dcloud.net.cn/download/HBuilderX.#{version}.dmg",
       verified: "download1.dcloud.net.cn/"
