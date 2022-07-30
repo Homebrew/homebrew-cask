b5742c940d9b97d29d1dc802d220d9f916a88280
Bevan Kay <email@bevankay.me>
2022-07-30 12:23:53 +1000

    Update livebook.rb

--- Casks/livebook.rb
+++ Casks/livebook.rb
@@ -1,28 +1,21 @@
 cask "livebook" do
+  arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
+
   version "0.6.3"
 
   if Hardware::CPU.intel?
-    arch = "x86_64"
     sha256 "ebdabcf39edd640092aa5d2ccc985a4cb3426ac1cfb674b8751c48b7e26aba04"
   else
-    arch = "aarch64"
     sha256 "f3d205950b4cfeec57a3594f724bc4751661c4ff8fd8410cc046480c31d00840"
   end
 
   url "https://github.com/livebook-dev/livebook/releases/download/v#{version}/LivebookInstall-#{version}-macos-#{arch}.dmg",
       verified: "github.com/livebook-dev/livebook"
   name "Livebook"
-  desc "Interactive & collaborative code notebooks in Elixir"
+  desc "Code notebook for Elixir Developers"
   homepage "https://livebook.dev/"
-
-  livecheck do
-    url :stable
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
-  end
 
   app "Livebook.app"
 
-  zap trash: [
-    "~/Library/Application Support/livebook",
-  ]
+  zap trash: "~/Library/Application Support/livebook"
 end
