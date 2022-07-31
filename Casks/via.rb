7b4aadab5724c4c531605fdc01534e89c9eefe70
Patrick Linnane <patrick@linnane.io>
2022-07-30 21:29:57 -0700

    Update via from 2.0.4 to 2.0.5

--- Casks/via.rb
+++ Casks/via.rb
@@ -1,6 +1,6 @@
 cask "via" do
-  version "2.0.4"
-  sha256 "761500d5a40d0b7e64b0ccb94f69b42d40e9b3c2f48f41894cbee0dd7b13008f"
+  version "2.0.5"
+  sha256 "43f06ec38157600bedd95a3bee5c0c9a5c754a24cf39331523edc5b70459ad73"
 
   url "https://github.com/the-via/releases/releases/download/v#{version}/via-#{version}-mac.dmg",
       verified: "github.com/the-via/releases/"
