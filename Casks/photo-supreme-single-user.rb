b9487ca1bfa2836ae81e8328088885ff239e6c49
Patrick Linnane <patrick@linnane.io>
2022-07-31 10:01:11 -0700

    Update photo-supreme-single-user from 7.3.0.4453 to 7.3.0.4457 (#128934)

--- Casks/photo-supreme-single-user.rb
+++ Casks/photo-supreme-single-user.rb
@@ -1,5 +1,5 @@
 cask "photo-supreme-single-user" do
-  version "7.3.0.4453"
+  version "7.3.0.4457"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://trial.idimager.com/PhotoSupreme_V#{version.major}.pkg"
