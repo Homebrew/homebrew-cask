d350950c407828d1278e9ba151485caba420d161
neutric <5984479+neutric@users.noreply.github.com>
2022-08-01 01:00:58 +0200

    Update sweet-home3d from 7.0 to 7.0.2

--- Casks/sweet-home3d.rb
+++ Casks/sweet-home3d.rb
@@ -1,6 +1,6 @@
 cask "sweet-home3d" do
-  version "7.0"
-  sha256 "26f0242e435ce2230f051f1342a6071e2bc1ea8b33d34dec24e630f0fc435c5a"
+  version "7.0.2"
+  sha256 "d4e14e26e4cd25d5c65165835ac1d1c212012857ca333270298270fb6071952b"
 
   url "https://downloads.sourceforge.net/sweethome3d/SweetHome3D/SweetHome3D-#{version}/SweetHome3D-#{version}-macosx.dmg",
       verified: "sourceforge.net/sweethome3d/"
