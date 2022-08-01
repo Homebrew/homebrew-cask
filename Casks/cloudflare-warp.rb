a66949e14ee526b1f25a3b32f46d6fdf3524bb83
owine <owine@users.noreply.github.com>
2022-08-01 15:42:54 -0500

    cloudflare-warp 2022.7.422.0,20220801.16
    Update cloudflare-warp from 2022.7.175.0 to 2022.7.422.0
    
    Closes #129003.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/cloudflare-warp.rb
+++ Casks/cloudflare-warp.rb
@@ -1,5 +1,5 @@
 cask "cloudflare-warp" do
-  version "2022.7.175.0,20220714.10"
+  version "2022.7.422.0,20220801.16"
   sha256 :no_check
 
   url "https://1111-releases.cloudflareclient.com/mac/Cloudflare_WARP.zip",
