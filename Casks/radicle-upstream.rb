9d0a589f0398f078238e5777f74e8792a420963b
Rūdolfs Ošiņš <rudolfs@osins.org>
2022-08-01 10:38:24 +0200

    radicle-upstream: checksum update
    Update radicle-upstream from 0.3.2 to 0.3.2
    
    Closes #128954.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/radicle-upstream.rb
+++ Casks/radicle-upstream.rb
@@ -1,6 +1,6 @@
 cask "radicle-upstream" do
   version "0.3.2"
-  sha256 "f197b3858ed84506019afd25a3e1b5d61b9858cea4f4e460a21c0fd5aafc0d8e"
+  sha256 "d52d9c101c4c59fb414ef736c49e5f751ee1be5b407d3bf26e097bb01ffca29d"
 
   url "https://releases.radicle.xyz/radicle-upstream-#{version}.dmg"
   name "Radicle Upstream"
