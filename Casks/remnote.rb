ec08e58bb41238515de19eb5c7c09a06a8687001
Patrick Linnane <patrick@linnane.io>
2022-08-01 15:46:56 -0700

    Update remnote from 1.8.8 to 1.8.10

--- Casks/remnote.rb
+++ Casks/remnote.rb
@@ -1,6 +1,6 @@
 cask "remnote" do
-  version "1.8.8"
-  sha256 "f793e0b57ca709f0db9bbd788a635d5a53c971da27068a804c315818e591850a"
+  version "1.8.10"
+  sha256 "09ffeb8cc1b70370ee4ed740760d220fd22fb6ec3d187f045ee9f2952ea36148"
 
   url "https://download.remnote.io/RemNote-#{version}.dmg",
       verified: "remnote.io"
