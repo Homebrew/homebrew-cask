70d1b9e10bde8b14a0b95ee16c02ce9170462128
Patrick Linnane <patrick@linnane.io>
2022-08-01 10:39:59 -0700

    Update visual-paradigm from 16.3 to 17.0

--- Casks/visual-paradigm.rb
+++ Casks/visual-paradigm.rb
@@ -1,6 +1,6 @@
 cask "visual-paradigm" do
-  version "16.3,20220622"
-  sha256 "550e2860ad34d2633cee6ea15cb1fc9c74368a59285203f91dc4f0d5f206d153"
+  version "17.0,20220801"
+  sha256 "224ae7ee55a81fc6e49a4f9b0018e42584bcce75c6115d1dbeec8d9c3d691f83"
 
   url "https://www.visual-paradigm.com/downloads/vp#{version.csv.first}/#{version.csv.second}/Visual_Paradigm_#{version.csv.first.dots_to_underscores}_#{version.csv.second}_OSX_WithJRE.dmg"
   name "Visual Paradigm"
