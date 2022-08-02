6ff0902effeb77621d9f2563b4a04cf87d47492e
Bevan Kay <email@bevankay.me>
2022-08-03 09:05:54 +1000

    Update v2ray-unofficial.rb

--- Casks/v2ray-unofficial.rb
+++ Casks/v2ray-unofficial.rb
@@ -7,6 +7,11 @@ cask "v2ray-unofficial" do
   desc "GUI client that supports Shadowsocks(R), V2Ray, and Trojan protocols"
   homepage "https://github.com/Dr-Incognito/V2Ray-Desktop"
 
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
   app "V2Ray-Desktop.app"
 
   zap trash: [
