657ca71e1dfa4be45001d99a98cc3048d737362b
Francisco Solis <fran@theprogramsrc.xyz>
2022-08-01 22:43:06 -0400

    Added DuckDuckGo Cask v0.27.0 (#128916)
    * Added DuckDuckGo Cask v0.27.0
    
    * Added zap stanza
    
    * Update duckduckgo.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

---
+++ Casks/duckduckgo.rb
@@ -0,0 +1,28 @@
+cask "duckduckgo" do
+  version "0.27.0"
+  sha256 "a1605dfff0e9d9ff88b56a8c1bc074e87419238df7ae5b599ad54f8282958892"
+
+  url "https://staticcdn.duckduckgo.com/macos-desktop-browser/duckduckgo-#{version}.dmg"
+  name "DuckDuckGo"
+  desc "Web browser focusing on privacy"
+  homepage "https://duckduckgo.com/"
+
+  livecheck do
+    url "https://staticcdn.duckduckgo.com/macos-desktop-browser/appcast.xml"
+    strategy :sparkle
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
+  app "DuckDuckGo.app"
+
+  zap trash: [
+    "~/Library/Caches/com.duckduckgo.macos.browser",
+    "~/Library/Containers/com.duckduckgo.macos.browser",
+    "~/Library/HTTPStorages/com.duckduckgo.macos.browser",
+    "~/Library/Preferences/com.duckduckgo.macos.browser.plist",
+    "~/Library/Saved Application State/com.duckduckgo.macos.browser.savedState",
+    "~/Library/WebKit/com.duckduckgo.macos.browser",
+  ]
+end
