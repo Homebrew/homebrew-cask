ba85da5b8e0946f7cf3288d54b71aecbf25e020e
neutric <5984479+neutric@users.noreply.github.com>
2022-08-02 12:36:57 +0200

    Update ogdesign-eagle from 3.0,7 to 3.0,8

--- Casks/ogdesign-eagle.rb
+++ Casks/ogdesign-eagle.rb
@@ -1,12 +1,12 @@
 cask "ogdesign-eagle" do
   arch = Hardware::CPU.intel? ? "" : "M1-"
 
-  version "3.0,7"
+  version "3.0,8"
 
   if Hardware::CPU.intel?
-    sha256 "9b35d210f1a75efd313053272cb38f19650fdf4c51f940c690407fbc36ddb3ca"
+    sha256 "bbb33440aa0ac425c25d2ad29f61a479ab7431cc9aa2f170d16a2397cc1bc0c8"
   else
-    sha256 "a6e94273cfe3a05b4e559a60a021306d6720c288920014e18d7f75f6daebb351"
+    sha256 "6ceb0f6427731385584e02816123db4125d8a730becd3cd1225f3aee7d9e6c40"
   end
 
   url "https://r2-app.eagle.cool/releases/Eagle-#{version.csv.first}-#{arch}build#{version.csv.second}.dmg"
