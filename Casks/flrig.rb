577bcb48ae2870e0be796df0f63c5ca94694b3c4
Patrick Linnane <patrick@linnane.io>
2022-08-02 11:22:40 -0700

    Fix sha256

--- Casks/flrig.rb
+++ Casks/flrig.rb
@@ -2,7 +2,7 @@ cask "flrig" do
   version "1.4.7"
 
   if MacOS.version <= :catalina
-    sha256 "f589e2c9df666a16b102e4786f48c71ba5ba1b91e23fa22e588811e944f182c9"
+    sha256 "aacbef604d070d16493f0b22d9a02772314fe25fd8131b19604ae5621a1d9211"
     url "https://downloads.sourceforge.net/fldigi/fldigi/flrig-#{version}_HS.dmg"
   else
     sha256 "e83e95e004778b66ea9079d608cda009c1996a0ac975a4258fcd68ae0d23479b"
