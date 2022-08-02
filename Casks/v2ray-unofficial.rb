d20307c6805003c2adcfcd1fa0a46441edf0d6d0
JounQin <admin@1stg.me>
2022-08-03 07:17:03 +0800

    Add new cask `v2ray-unofficial` (#127296)
    * Add new cask `v2ray-desktop`
    
    * Update Casks/v2ray-desktop.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>
    
    * Update Casks/v2ray-desktop.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>
    
    * Update and rename v2ray-desktop.rb to v2ray-unofficial.rb
    
    * Update v2ray-unofficial.rb
    
    * Update v2ray-unofficial.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

---
+++ Casks/v2ray-unofficial.rb
@@ -0,0 +1,21 @@
+cask "v2ray-unofficial" do
+  version "2.3.1"
+  sha256 "c1f900950e4650190dc2bf67ad8fc51b4602be214b4807233d1175dfca855ea6"
+
+  url "https://github.com/Dr-Incognito/V2Ray-Desktop/releases/download/#{version}/V2Ray-Desktop-v#{version}-macOS-x86_64.dmg"
+  name "V2Ray Desktop"
+  desc "GUI client that supports Shadowsocks(R), V2Ray, and Trojan protocols"
+  homepage "https://github.com/Dr-Incognito/V2Ray-Desktop"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "V2Ray-Desktop.app"
+
+  zap trash: [
+    "~/Library/Preferences/V2Ray-Desktop",
+    "~/Library/Saved Application State/com.yourcompany.V2Ray-Desktop.savedState",
+  ]
+end
