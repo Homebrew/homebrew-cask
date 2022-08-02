667b2af0b8519b992c62839ea7dc01c76f0a95f6
Patrick Linnane <patrick@linnane.io>
2022-08-02 08:35:01 -0700

    Update electerm from 1.22.24 to 1.22.27

--- Casks/electerm.rb
+++ Casks/electerm.rb
@@ -1,12 +1,12 @@
 cask "electerm" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "1.22.24"
+  version "1.22.27"
 
   if Hardware::CPU.intel?
-    sha256 "71012283cc671ee4c1153ae8358fd298c914ccc1463b98043fcdd13f09a4751f"
+    sha256 "23fbe666d1bd8550bc1bb008b3abec62f5f31c09eb5a5fb74eb4a7dea22eca7d"
   else
-    sha256 "7e15c57447306fe325f94bbdeee3c54b87de745ee0412ccfef90bba27850efd7"
+    sha256 "ec627f5f2783926da240a4e7ab6f38d4bf09dd1a5de93579399e4af05a111ca3"
   end
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac-#{arch}.dmg"
