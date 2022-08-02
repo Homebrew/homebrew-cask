be8f9bca31e7e03eb8bb135650c02529a04338cd
Patrick Linnane <patrick@linnane.io>
2022-08-02 08:37:47 -0700

    Update flying-carpet from 5.0 to 6.0

--- Casks/flying-carpet.rb
+++ Casks/flying-carpet.rb
@@ -1,6 +1,6 @@
 cask "flying-carpet" do
-  version "5.0"
-  sha256 "83ac946b4cf9be1164c361bcbcb8a195bb636686a0eb3efa471d3888951b4264"
+  version "6.0"
+  sha256 "34effd0ab3b9c784bb05f1b7fd464a2141be4e1d1020ba894d54d16ac1c22ef6"
 
   url "https://github.com/spieglt/FlyingCarpet/releases/download/v#{version}/FlyingCarpetMac.zip"
   name "Flying Carpet"
