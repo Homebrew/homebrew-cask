b896421fc9a6ef0b610d6fdd83a6a84e2bc925ac
Patrick Linnane <patrick@linnane.io>
2022-08-03 11:41:05 -0700

    electron 20.0.1
    Update electron from 20.0.0 to 20.0.1
    
    Closes #129168.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,12 +1,12 @@
 cask "electron" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "20.0.0"
+  version "20.0.1"
 
   if Hardware::CPU.intel?
-    sha256 "e493320e3410bf8cb7ac5b17a5d58a0a8a50b8637303a7c1d9342c08af6cd180"
+    sha256 "2ceea21ba4f79e1e9bea6d01e31a6339288a3d98adf23ac52a728bb22e55794c"
   else
-    sha256 "0492396b3476735ca679c0e87d86e94b4a1630b5cdce3e4cf155ff443aff4932"
+    sha256 "42f251973d4ae90941f82898d4a2e21d7e4c899e920bc671aba03eedb871ee10"
   end
 
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-#{arch}.zip",
