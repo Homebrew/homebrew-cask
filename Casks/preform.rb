bd8737c03e71985ab46fa9cabd292b6947ce8c75
Patrick Linnane <patrick@linnane.io>
2022-08-03 10:10:53 -0700

    Update preform from 3.26.0 to 3.26.1

--- Casks/preform.rb
+++ Casks/preform.rb
@@ -1,6 +1,6 @@
 cask "preform" do
-  version "3.26.0,153_21258"
-  sha256 "e21c0aeb5084f5fff83e4ef93b78e2b26005c4e1040743ab1c55db52a6762723"
+  version "3.26.1,154_21934"
+  sha256 "5696f8610a1f9c56c511502a7c2e4d354af1a744ab468466b8e5377a0e4012a1"
 
   url "https://downloads.formlabs.com/PreForm/Release/#{version.csv.first}/PreForm_mac_#{version.csv.first}_release_releaser_#{version.csv.second}.dmg"
   name "PreForm"
