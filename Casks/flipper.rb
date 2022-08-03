87eaf54849a795ec40f3cb5cda1d8ae9b9460aed
SvenFE <shensven2016@gmail.com>
2022-08-03 21:19:16 +0800

    flipper 0.157.0
    Update flipper from 0.156.0 to 0.157.0
    
    Closes #129122.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/flipper.rb
+++ Casks/flipper.rb
@@ -1,6 +1,6 @@
 cask "flipper" do
-  version "0.156.0"
-  sha256 "9b5cbc778b08c9a3bd479d8bf8ce36f344356868f4fb85020f347835631b0595"
+  version "0.157.0"
+  sha256 "e431e1d3643c8f7bb6f7bbaf6be34504bbc2cac2bb402865b3b93e748827635b"
 
   url "https://github.com/facebook/flipper/releases/download/v#{version}/Flipper-mac.dmg",
       verified: "github.com/facebook/flipper/"
