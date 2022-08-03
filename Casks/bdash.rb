3611f5874c0ad764cf9f036a096a11f650d16166
Patrick Linnane <patrick@linnane.io>
2022-08-03 09:21:24 -0700

    Update bdash from 1.15.1 to 1.15.2

--- Casks/bdash.rb
+++ Casks/bdash.rb
@@ -1,6 +1,6 @@
 cask "bdash" do
-  version "1.15.1"
-  sha256 "f0a4c1a16a9406dcf4ba13feca9359e1a7acea1de3346c621ad4dd58fe2a7778"
+  version "1.15.2"
+  sha256 "df64fb50a41b469303389e05323016adc48c84a3a712a7cc1863deedb2adb9d9"
 
   url "https://github.com/bdash-app/bdash/releases/download/v#{version}/Bdash-#{version}-mac.zip"
   name "Bdash"
