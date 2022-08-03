63ff879d577cc683d5b20a981d23ad0d88463ea5
Kapeli <support@kapeli.com>
2022-08-03 13:20:12 +0300

    dash: rebuild
    Update dash from 6.2.4 to 6.3.0
    
    Closes #129115.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/dash.rb
+++ Casks/dash.rb
@@ -3,8 +3,8 @@ cask "dash" do
     version "4.6.7"
     sha256 "e2b5eb996645b25f12ccae15e24b1b0d8007bc5fed925e14ce7be45a2b693fb6"
   else
-    version "6.2.4,996"
-    sha256 "336ac944cf473a9832b703d8eaa3df8016831ce6c15e0062338d1d23c6dc0e40"
+    version "6.3.0,998"
+    sha256 "b1f8b8a7d1cd3fb698cffc6783753487d9546393c82ce01bf1907665f801e889"
   end
 
   url "https://kapeli.com/downloads/v#{version.major}/Dash.zip"
