f5fcb49d0d6e4aacd07857b46a2344595c794867
Patrick Linnane <patrick@linnane.io>
2022-08-03 09:45:01 -0700

    jamulus: checksum update
    Add arm64, bump min os ver
    
    Closes #129140.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/jamulus.rb
+++ Casks/jamulus.rb
@@ -1,12 +1,15 @@
 cask "jamulus" do
   version "3.9.0"
 
-  if MacOS.version <= :sierra
-    sha256 "e6492ec6355166d88d48be7d7d3a242739c112841c1f826451be7a5adb0cbd00"
+  if MacOS.version <= :mojave
+    sha256 "156555cdbd1ff618375f7c9d03a4da5fbd7d19ed57d67db8675ba6f57170e228"
     suffix = "_legacy"
-  else
+  elsif Hardware::CPU.intel?
     sha256 "65f04e749c997bde9e1d814cd0423dd6a3fb6868ed1c62e5f8a8f9f2a1a5160c"
     suffix = ""
+  else
+    sha256 "353ed99c0497f2b2b668a910ab4f954a7ba51b8bca8c205c8f1c47db98e87fd5"
+    suffix = "_arm64"
   end
 
   url "https://downloads.sourceforge.net/llcon/jamulus_#{version}_mac#{suffix}.dmg",
@@ -15,7 +18,7 @@ cask "jamulus" do
   desc "Play music online with friends"
   homepage "https://jamulus.io/"
 
-  depends_on macos: ">= :el_capitan"
+  depends_on macos: ">= :catalina"
 
   app "Jamulus.app"
   app "JamulusServer.app"
