13e1e475d735538b074f25e5ffa731e862465306
Quinn Bellmore <quinnzbellz@gmail.com>
2022-08-02 21:02:55 -0400

    add moderndeck cask

---
+++ Casks/moderndeck.rb
@@ -0,0 +1,21 @@
+cask "moderndeck" do
+  version "9.4.5"
+  sha256 "0033851c0c8435e567bdf85a99aff8f7979d7982b84bd77ed4dc07d62eb602d6"
+
+  url "https://github.com/dangeredwolf/ModernDeck/releases/download/v#{version}/ModernDeck-universal.dmg",
+      verified: "github.com/dangeredwolf/ModernDeck"
+  name "ModernDeck"
+  desc "ModernDeck takes TweetDeck to the next level with a Material-inspired theme, customization and performance enhancements, and more. Available as a desktop app or a browser extension."
+  homepage "https://moderndeck.app/"
+
+livecheck do
+    url :url
+    strategy :github_latest
+  end
+auto_updates true
+  depends_on macos: ">= :el_capitan"
+
+  app "ModernDeck.app"
+end
+
+
