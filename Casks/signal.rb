317324e83e24c0bc9896e32bb62aa6dd46b2d64b
Patrick Linnane <patrick@linnane.io>
2022-08-03 15:40:30 -0700

    Update signal from 5.52.0 to 5.53.0

--- Casks/signal.rb
+++ Casks/signal.rb
@@ -1,12 +1,12 @@
 cask "signal" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "5.52.0"
+  version "5.53.0"
 
   if Hardware::CPU.intel?
-    sha256 "2c3b795c5682a0270e647e5ca47dc697239f26078e15648b4721d22bea98dfe3"
+    sha256 "371d508892f8e96e8bc6d60164148fdf0a8fde1e7e7e82cf8fb4fbb83a1b2b26"
   else
-    sha256 "6787520e639add9f646614549275491e3d6061878894185069faffb4657d7b73"
+    sha256 "eab98c584e405781a587890006c59e02c9a1b22d66705f3fc03064d247c04fde"
   end
 
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{arch}-#{version}.dmg"
