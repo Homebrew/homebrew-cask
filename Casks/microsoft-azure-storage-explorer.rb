b7061311e1d0e15711a36bd483b5fa27ba2d1c63
Curudel <andreas.florin.1988@outlook.com>
2022-08-04 08:14:15 +0200

    microsoft-azure-storage-explorer 1.25.0
    Update microsoft-azure-storage-explorer from 1.24.3 to 1.25.0
    
    Closes #129189.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/microsoft-azure-storage-explorer.rb
+++ Casks/microsoft-azure-storage-explorer.rb
@@ -1,6 +1,6 @@
 cask "microsoft-azure-storage-explorer" do
-  version "1.24.3"
-  sha256 "9fe3801fd3f0ba52c18b6beb9eebe74c026112312ec223ad6b9e948addaab96f"
+  version "1.25.0"
+  sha256 "be037c1750dc4f0f9ca7b04e1f404930487045f202ba929d01a2f6e663bd1988"
 
   url "https://github.com/microsoft/AzureStorageExplorer/releases/download/v#{version}/Mac_StorageExplorer.zip",
       verified: "github.com/microsoft/AzureStorageExplorer/"
