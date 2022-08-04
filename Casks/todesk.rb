926f6cd337cf1b4b493254f1f61e19cf0c5c9b58
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-08-04 14:48:29 +0800

    Update todesk.rb

--- Casks/todesk.rb
+++ Casks/todesk.rb
@@ -14,7 +14,7 @@ cask "todesk" do
 
   auto_updates true
 
-  pkg "ToDesk_#{version}.pkg"
+  pkg "ToDeskk_#{version}.pkg"
 
   uninstall delete:    "/Applications/ToDesk.app",
             launchctl: [
