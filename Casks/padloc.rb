7bcac3b1c97642a0440275bc66b62b45c047f3c5
John <john@iamjohnhenry.com>
2022-08-04 12:29:04 -0700

    padloc: rebuild
    * Update location for installed dmg
    * Specify 'electron' directly
    * Format URL
    
    Closes #129200.
    
    Signed-off-by: Markus Reiter <me@reitermark.us>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/padloc.rb
+++ Casks/padloc.rb
@@ -2,7 +2,7 @@ cask "padloc" do
   version "4.0.0"
   sha256 "b1d2bb585b07133289b4de2a5cba61569af65b47664f39cf200e461179a1ee99"
 
-  url "https://github.com/padloc/padloc/releases/download/v#{version}/Padloc-#{version}.dmg",
+  url "https://github.com/padloc/padloc/releases/download/v#{version}/padloc_#{version}_macos_electron.dmg",
       verified: "github.com/padloc/padloc/"
   name "Padloc"
   desc "Modern password manager"
