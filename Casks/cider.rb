ff31f8a70768cc8a0aaa32499e972760a4873185
Piotrek Rybiec <piotrrybiec28@gmail.com>
2022-08-05 14:34:24 +0200

    correct repo now

--- Casks/cider.rb
+++ Casks/cider.rb
@@ -1,10 +1,10 @@
 cask "cider" do
-  version "1.4.3"
+  version "v1.5.4-beta.34"
   sha256 :no_check
 
-  url "https://github.com/ciderapp/Cider/releases/download/macos-beta/Cider.dmg", verified: "https://github.com/ciderapp/Cider"
+  url "https://github.com/ciderapp/cider-releases/releases/download/#{version}/Cider-#{version}-universal.dmg", verified: "https://github.com/ciderapp/cider-releases"
   name "Cider"
-  desc "Open source and community oriented Apple Music"
+  desc "Open source and community oriented Apple Music client"
   homepage "https://cider.sh/"
 
   auto_updates true
