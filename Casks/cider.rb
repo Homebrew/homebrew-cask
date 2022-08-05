044929a44ed6566509913097ae243da266dd8333
Piotrek Rybiec <piotrrybiec28@gmail.com>
2022-08-05 14:42:31 +0200

    last updates

--- Casks/cider.rb
+++ Casks/cider.rb
@@ -1,8 +1,8 @@
 cask "cider" do
-  version "v1.5.4-beta.34"
+  version "1.5.4-beta.34"
   sha256 :no_check
 
-  url "https://github.com/ciderapp/cider-releases/releases/download/#{version}/Cider-#{version}-universal.dmg", verified: "https://github.com/ciderapp/cider-releases"
+  url "https://github.com/ciderapp/cider-releases/releases/download/v#{version}/Cider-#{version}-universal.dmg", verified: "https://github.com/ciderapp/cider-releases"
   name "Cider"
   desc "Open source and community oriented Apple Music client"
   homepage "https://cider.sh/"
