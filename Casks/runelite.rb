dbcc04a8f2ad37c17d323bbc4050198357c8f3ad
neutric <5984479+neutric@users.noreply.github.com>
2022-08-07 00:11:52 +0200

    Update runelite from 2.4.3 to 2.4.4

--- Casks/runelite.rb
+++ Casks/runelite.rb
@@ -2,8 +2,8 @@ cask "runelite" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
 
   if Hardware::CPU.intel?
-    version "2.4.3"
-    sha256 "0e39d850278a0ea5599fe9d80af2feba785261c4fb60c6254d594047298eb722"
+    version "2.4.4"
+    sha256 "62f8a097ddbc6dffcee85c6e3b8b2f386b97395e878f575198dad1386df47c28"
   else
     version "2.4.2"
     sha256 "6deceeb1460259622d0f6d35e2527f94d1613b5a5bc40ebade25b617a970a411"
