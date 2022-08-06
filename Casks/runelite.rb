761f7314fad5e7d4c4be54cf0105770539565caf
neutric <5984479+neutric@users.noreply.github.com>
2022-08-07 00:33:39 +0200

    runelite: fix style

--- Casks/runelite.rb
+++ Casks/runelite.rb
@@ -1,11 +1,11 @@
 cask "runelite" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
 
+  version "2.4.4"
+
   if Hardware::CPU.intel?
-    version "2.4.4"
     sha256 "62f8a097ddbc6dffcee85c6e3b8b2f386b97395e878f575198dad1386df47c28"
   else
-    version "2.4.4"
     sha256 "b233def430723a3f6b7229fefb2361a198aeafbdc485358295f1d662f97df766"
   end
 
