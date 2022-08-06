8211d7d21724656c30deee9b71fdec4a692a2573
Daniel Sattler <ds@MBP-von-Daniel.fritz.box>
2022-08-06 11:58:07 +0200

    Add arm64 builds to darktable
    This adds the arm64 builds to darktable

--- Casks/darktable.rb
+++ Casks/darktable.rb
@@ -1,8 +1,15 @@
 cask "darktable" do
+  arch = Hardware::CPU.intel? ? "" : "_arm64"
+  
   version "4.0.0"
-  sha256 "addab784af18bafa303340e754c00084c126e61c3d5b93006f8e6d602f838203"
+  
+  if Hardware::CPU.intel?
+    sha256 "addab784af18bafa303340e754c00084c126e61c3d5b93006f8e6d602f838203"
+  else
+    sha256 "5bcd8e088065bc20815022f494ca1ca0613446562843f378ad60f68ae6917cb7"
+  end
 
-  url "https://github.com/darktable-org/darktable/releases/download/release-#{version.major_minor_patch}/darktable-#{version}.dmg",
+  url "https://github.com/darktable-org/darktable/releases/download/release-#{version.major_minor_patch}/darktable-#{version}#{arch}.dmg",
       verified: "github.com/darktable-org/darktable/"
   name "darktable"
   desc "Photography workflow application and raw developer"
