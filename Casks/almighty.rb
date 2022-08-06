f3d2760751847e7b41a239b716d992091ad48dce
neutric <5984479+neutric@users.noreply.github.com>
2022-08-07 01:43:57 +0200

    Update almighty from 2.3.3 to 2.3.5

--- Casks/almighty.rb
+++ Casks/almighty.rb
@@ -1,8 +1,8 @@
 cask "almighty" do
-  version "2.3.3,43"
+  version "2.3.5,46"
   sha256 :no_check
 
-  url "https://almighty-app.s3.eu-north-1.amazonaws.com/Almighty.app.zip",
+  url "https://almighty-app.s3.eu-north-1.amazonaws.com/Almighty.zip",
       verified: "almighty-app.s3.eu-north-1.amazonaws.com/"
   name "almighty"
   desc "Settings and tweaks configurator"
