87570c76c52121861b3ed5e19b80a6949fa72030
Daniel Sattler <ds@Mini-von-Daniel.fritz.box>
2022-08-07 18:39:41 +0200

    schism-tracker 20220807
    Update schism-tracker from 20220506 to 20220807
    
    Closes #129322.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/schism-tracker.rb
+++ Casks/schism-tracker.rb
@@ -1,6 +1,6 @@
 cask "schism-tracker" do
-  version "20220506"
-  sha256 "f2265e58e5c932ec35cde29d5133dd87f45d851dc0ad4bfcb2666305455fb1d5"
+  version "20220807"
+  sha256 "8f8e63d4d4f520b9a640246abaaea384e9b5a2466c8597bde77100beb549571c"
 
   url "https://github.com/schismtracker/schismtracker/releases/download/#{version}/schismtracker-#{version}-macos.zip"
   name "Schism Tracker"
