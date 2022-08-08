874ec4fdc2ee3fa06cba0633458ecf1374053414
cyao1234 <94928179+cheyao@users.noreply.github.com>
2022-08-08 14:47:59 +0200

    Create winebottler.rb

---
+++ Casks/winebottler.rb
@@ -0,0 +1,17 @@
+cask "8x8-meet" do
+  version "1.8.6"
+  sha256 :no_check
+
+  url "https://winebottler.kronenberg.org/combo/builds/WineBottlerCombo_1.8.6.dmg"
+  name "WineBottler"
+  desc "Run Windows-based Programs on a Mac"
+  homepage "https://winebottler.kronenberg.org/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
+
+  app "Wine.app"
+  app "WineBottler.app"
+end
