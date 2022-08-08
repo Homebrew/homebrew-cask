5c26c6b0eb0e1b031e05c68a3f4983f51ed5bf5a
William Entriken <github.com@phor.net>
2022-08-08 09:51:21 -0400

    corelocationcli 4.0.1
    Update corelocationcli from 4.0.0 to 4.0.1
    
    Closes #129346.
    
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/corelocationcli.rb
+++ Casks/corelocationcli.rb
@@ -1,6 +1,6 @@
 cask "corelocationcli" do
-  version "4.0.0"
-  sha256 "73772a3bfad8b530925f1db19be3c80af504521f5cf903990e7534ae5fabc484"
+  version "4.0.1"
+  sha256 "356210adf039316e7a464231efa0f3eade5bf6db96f3e6579cc81de5c44dcc9b"
 
   url "https://github.com/fulldecent/corelocationcli/releases/download/#{version}/CoreLocationCLI.zip"
   name "Core Location CLI"
