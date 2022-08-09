ad85d6033175eac404acf8bde0ab4d454f10331a
zuisong <19145952+zuisong@users.noreply.github.com>
2022-08-09 14:58:16 +0800

    Update shortcat from 0.10.0 to 0.10.1 (#129351)
    Co-authored-by: zuisong <com.me@foxmail.com>

--- Casks/shortcat.rb
+++ Casks/shortcat.rb
@@ -1,6 +1,6 @@
 cask "shortcat" do
-  version "0.10.0,163"
-  sha256 "0c1dab0eea7423b5550d3bd4586b8e0fd4c69a044d8514c07b426358127a6143"
+  version "0.10.1,166"
+  sha256 "3d7f4c6a266439774d22599e3f9811a807d6934fa2fc1a4e9c7d5c176693a196"
 
   url "https://files.shortcat.app/releases/v#{version.csv.first}/Shortcat.zip"
   name "Sproutcube Shortcat"
