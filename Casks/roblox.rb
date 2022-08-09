23aafc0e5dc98303bf7a1dca0921f808253cc8e2
Aaron Liu <aaronliu0130@gmail.com>
2022-08-09 10:55:25 -0400

    Update version and livecheck
    i hate regexes

--- Casks/roblox.rb
+++ Casks/roblox.rb
@@ -1,5 +1,5 @@
 cask "roblox" do
-  version "1.2.2.5360459,6e3d2e7c823142ae"
+  version "0.538.0.5380364,17f793e683ec4fd2"
   sha256 "eefc95fb85c888cbefaa16212779954c46482197442a82555f2e622135a28f06"
 
   url "https://setup.rbxcdn.com/mac/version-#{version.csv.second}-Roblox.dmg",
@@ -10,16 +10,10 @@ cask "roblox" do
 
   livecheck do
     url "https://clientsettingscdn.roblox.com/v1/client-version/MacPlayer"
-    regex(/version[._-]([^"]+)/i)
+    regex(/version":"([^"]+)".*version[._-]([^"]+)/i)
     strategy :page_match do |page, regex|
-      client_version = page[regex, 1]
-      next if client_version.blank?
-
-      cask = CaskLoader.load("roblox")
-      download_url = "https://setup.rbxcdn.com/mac/version-#{client_version}-Roblox.dmg"
-      main_version = Homebrew::Livecheck::Strategy::ExtractPlist.find_versions(cask: cask, url: download_url)[:matches].values.first
-      next if main_version.blank?
-
+      main_version = page[regex, 1]
+      client_version = page[regex, 2]
       "#{main_version},#{client_version}"
     end
   end
