e1b8a2393d23120d0bbd05b82590ff856c086a85
neutric <5984479+neutric@users.noreply.github.com>
2022-08-10 02:03:30 +0200

    Update tortoisehg from 6.2 to 6.2.1

--- Casks/tortoisehg.rb
+++ Casks/tortoisehg.rb
@@ -1,6 +1,6 @@
 cask "tortoisehg" do
-  version "6.2"
-  sha256 "0b1da8a0fddf2f7549117d87f9d0256eaabe76c57afb9ff448049519ab5b2191"
+  version "6.2.1"
+  sha256 "a2401171b309e98171ef7cb823d98ea28770c3bae9eb95a1fbfe6ab3c9327a73"
 
   url "https://www.mercurial-scm.org/release/tortoisehg/macos/TortoiseHg-#{version}-mac-x64-qt5.dmg",
       verified: "mercurial-scm.org/release/tortoisehg/"
