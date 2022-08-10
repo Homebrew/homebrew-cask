c77dff4fa70fb1ed7b7499eb0be513107de81b4b
neutric <5984479+neutric@users.noreply.github.com>
2022-08-10 02:02:24 +0200

    Update thorium from 2.0.0 to 2.1.0

--- Casks/thorium.rb
+++ Casks/thorium.rb
@@ -1,6 +1,6 @@
 cask "thorium" do
-  version "2.0.0"
-  sha256 "d628bd2956b2f57a610ba75b94d510f2af2d3fe8f9f621ff4922d0a1eb134193"
+  version "2.1.0"
+  sha256 "924fe26c6ddb12a65c05de7573138ef10a3a482801341f5aecf39619d5d5db53"
 
   url "https://github.com/edrlab/thorium-reader/releases/download/v#{version}/Thorium-#{version}.dmg",
       verified: "github.com/edrlab/thorium-reader/"
