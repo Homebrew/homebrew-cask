18c7abb7ca797afc713877f844831ee8fb35f0ef
Carlos Moreno <cmoreno@protelecom.com.gt>
2022-08-10 06:46:48 -0600

    Update flipper from 0.157.0 to 0.158.0

--- Casks/flipper.rb
+++ Casks/flipper.rb
@@ -1,6 +1,6 @@
 cask "flipper" do
-  version "0.157.0"
-  sha256 "e431e1d3643c8f7bb6f7bbaf6be34504bbc2cac2bb402865b3b93e748827635b"
+  version "0.158.0"
+  sha256 "80612c7d9a0e9689a0b9dbd1826a76f3ca2fcd0a86761c510027924a5ce35d95"
 
   url "https://github.com/facebook/flipper/releases/download/v#{version}/Flipper-mac.dmg",
       verified: "github.com/facebook/flipper/"
