b6dcb5168c94cba8c5b10799eb3633b98fb0b343
neutric <5984479+neutric@users.noreply.github.com>
2022-08-10 03:06:57 +0200

    Update naver-whale from 3.15.136.29 to 3.16.138.22

--- Casks/naver-whale.rb
+++ Casks/naver-whale.rb
@@ -1,7 +1,7 @@
 cask "naver-whale" do
   arch = Hardware::CPU.intel? ? "" : "ARM64"
 
-  version "3.15.136.29"
+  version "3.16.138.22"
   sha256 :no_check
 
   url "http://update.whale.naver.net/downloads/installers/NaverWhale#{arch}.dmg",
