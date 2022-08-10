98ef6b013a4aa5c39ceac82efe1586020271ba47
Rylan Polster <rslpolster@gmail.com>
2022-08-10 17:54:13 -0400

    listen1: use `arch` and `on_arch_conditional` DSLs

--- Casks/listen1.rb
+++ Casks/listen1.rb
@@ -1,6 +1,6 @@
 cask "listen1" do
   # NOTE: "1" is not a version number, but an intrinsic part of the product name
-  arch = Hardware::CPU.intel? ? "x64" : "arm64"
+  arch arm: "arm64", intel: "x64"
 
   version "2.24.0"
 
