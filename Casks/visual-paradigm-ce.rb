8ebafa9ed17b91f8166c09f88cab8d1ab2af9c9f
Lex.Chen <ctyytc@gmail.com>
2022-08-10 15:32:55 +0800

    visual-paradigm-ce: rebuild
    Update visual-paradigm-ce to add AArch64 architecture in version 17.0
    
    Closes #129448.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/visual-paradigm-ce.rb
+++ Casks/visual-paradigm-ce.rb
@@ -1,8 +1,15 @@
 cask "visual-paradigm-ce" do
-  version "17.0,20220801"
-  sha256 "f7d4db8b65982054ea2ee803b3c92e60ea5e7b9816fa4536f763e7061e17458c"
+  arch = Hardware::CPU.intel? ? "WithJRE" : "AArch64"
 
-  url "https://www.visual-paradigm.com/downloads/vpce/Visual_Paradigm_CE_#{version.csv.first.dots_to_underscores}_#{version.csv.second}_OSX_WithJRE.dmg"
+  version "17.0,20220801"
+
+  if Hardware::CPU.intel?
+    sha256 "f7d4db8b65982054ea2ee803b3c92e60ea5e7b9816fa4536f763e7061e17458c"
+  else
+    sha256 "3bed108ac6f210cc04bc13b5cc80399d88961505793282bf0a9bc33c0d1cc046"
+  end
+
+  url "https://www.visual-paradigm.com/downloads/vpce/Visual_Paradigm_CE_#{version.csv.first.dots_to_underscores}_#{version.csv.second}_OSX_#{arch}.dmg"
   name "Visual Paradigm Community Edition"
   desc "All-in-one UML, SysML, BPMN Modeling Platform for Agile"
   homepage "https://www.visual-paradigm.com/"
