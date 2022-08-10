7c7fd7fe3e8277756da62949aa58576d39f131c6
neutric <5984479+neutric@users.noreply.github.com>
2022-08-10 03:23:45 +0200

    Update mailspring from 1.10.3 to 1.10.4

--- Casks/mailspring.rb
+++ Casks/mailspring.rb
@@ -1,6 +1,6 @@
 cask "mailspring" do
-  version "1.10.3"
-  sha256 "59f09175d6ffd8253a23201a5703f7d9d9bec6753692876163790c3c991e413a"
+  version "1.10.4"
+  sha256 "d93c6dcd0cf569aced174e8ffe3fbb24634a8ac04d774b16c784f4ba90838167"
 
   url "https://github.com/Foundry376/Mailspring/releases/download/#{version}/Mailspring.zip",
       verified: "github.com/Foundry376/Mailspring/"
