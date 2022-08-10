e32af2c28926ca714c722e03c035f1168c644668
Rylan Polster <rslpolster@gmail.com>
2022-08-10 17:54:09 -0400

    github: use `arch` and `on_arch_conditional` DSLs

--- Casks/github.rb
+++ Casks/github.rb
@@ -1,6 +1,6 @@
 cask "github" do
-  arch = Hardware::CPU.intel? ? "x64" : "arm64"
-  platform = Hardware::CPU.intel? ? "darwin" : "darwin-arm64"
+  arch arm: "arm64", intel: "x64"
+  platform = on_arch_conditional arm: "darwin-arm64", intel: "darwin"
 
   version "3.0.5-25b66588"
 
