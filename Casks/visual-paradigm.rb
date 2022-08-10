c2b72154c926b15ba2782336caff87c6271a8924
Lex.Chen <ctyytc@gmail.com>
2022-08-10 15:37:56 +0800

    Update visual-paradigm to add AArch64 architecture in version 17.0

--- Casks/visual-paradigm.rb
+++ Casks/visual-paradigm.rb
@@ -1,8 +1,15 @@
 cask "visual-paradigm" do
-  version "17.0,20220801"
-  sha256 "224ae7ee55a81fc6e49a4f9b0018e42584bcce75c6115d1dbeec8d9c3d691f83"
+  arch = Hardware::CPU.intel? ? "WithJRE" : "AArch64"
 
-  url "https://www.visual-paradigm.com/downloads/vp#{version.csv.first}/#{version.csv.second}/Visual_Paradigm_#{version.csv.first.dots_to_underscores}_#{version.csv.second}_OSX_WithJRE.dmg"
+  version "17.0,20220801"
+
+  if Hardware::CPU.intel?
+    sha256 "224ae7ee55a81fc6e49a4f9b0018e42584bcce75c6115d1dbeec8d9c3d691f83"
+  else
+    sha256 "626e638df254baf90ef77fed388101817d872e8bcafb79f163bde6ef06ca333d"
+  end
+
+  url "https://www.visual-paradigm.com/downloads/vp#{version.csv.first}/#{version.csv.second}/Visual_Paradigm_#{version.csv.first.dots_to_underscores}_#{version.csv.second}_OSX_#{arch}.dmg"
   name "Visual Paradigm"
   desc "UML CASE Tool supporting UML 2, SysML and Business Process Modeling Notation"
   homepage "https://www.visual-paradigm.com/"
