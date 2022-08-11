3b1e1c6b3f201cd08a1ef5c27816136499e609fc
Dmitriy Vorobiev <plus79166302110@gmail.com>
2022-08-11 06:42:23 +0000

    translate description

--- Casks/chromium-gost.rb
+++ Casks/chromium-gost.rb
@@ -2,9 +2,9 @@ cask "chromium-gost" do
   version "103.0.5060.134"
   sha256 "f7bcca0f328fae408773700dae023aa036e909d48022157b0c6627b860a1ea78"
 
-  url "https://github.com/deemru/Chromium-Gost/releases/download/#{version}/chromium-gost-103.0.5060.134-macos-amd64.tar.bz2"
+  url "https://github.com/deemru/Chromium-Gost/releases/download/#{version}/chromium-gost-#{version}-macos-amd64.tar.bz2"
   name "Chromium-Gost"
-  desc "Браузер Chromium с поддержкой алгоритмов ГОСТ"
+  desc "Open source web browser based on Chromium with support for GOST cryptographic algorithms"
   homepage "https://github.com/deemru/Chromium-Gost"
 
   app "Chromium-Gost.app"
