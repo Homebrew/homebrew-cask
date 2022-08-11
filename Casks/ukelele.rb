f42150d7ce75871d72e7a328ae690658395afd10
Tin H Kyaw <tinhkyaw@users.noreply.github.com>
2022-08-11 10:16:40 -0700

    Update ukelele from 3.5.6 to 3.5.7 (#129538)

--- Casks/ukelele.rb
+++ Casks/ukelele.rb
@@ -1,6 +1,6 @@
 cask "ukelele" do
-  version "3.5.6,355"
-  sha256 "5cbf69cb40821aede0692eef80425f43991012304113e517fc9220b396085067"
+  version "3.5.7,359"
+  sha256 "631994a31763c7dc383d429969a7b3d98975ad98dcf30dc51b1421fc93fc05e7"
 
   url "https://software.sil.org/downloads/r/ukelele/Ukelele_#{version.csv.first}.dmg"
   name "Ukelele"
