feec98aea7d73613c820bf8a2ba80fe4b0073af1
Patrick Linnane <patrick@linnane.io>
2022-08-11 15:05:12 -0700

    Update cellprofiler from 4.2.1 to 4.2.3

--- Casks/cellprofiler.rb
+++ Casks/cellprofiler.rb
@@ -1,6 +1,6 @@
 cask "cellprofiler" do
-  version "4.2.1"
-  sha256 "38622f987d965da8115f6abe6f809fb7313b360272c8f983baa1597f5f3d50a3"
+  version "4.2.3"
+  sha256 "d52fe1adba9eaa4e16341487059152fd8d4c09f17ae20f71400be4002de8d864"
 
   url "https://cellprofiler-releases.s3.amazonaws.com/CellProfiler-macOS-#{version}.zip",
       verified: "cellprofiler-releases.s3.amazonaws.com/"
