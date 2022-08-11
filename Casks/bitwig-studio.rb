6e2e4949ec9073c73cfa71a4842706115c5d9ea0
neutric <5984479+neutric@users.noreply.github.com>
2022-08-11 17:17:03 +0000

    Update bitwig-studio from 4.3.2 to 4.3.4 (#129518)

--- Casks/bitwig-studio.rb
+++ Casks/bitwig-studio.rb
@@ -1,6 +1,6 @@
 cask "bitwig-studio" do
-  version "4.3.2"
-  sha256 "69d3157e9525ac28467b3a6c2c82f5bca13566ae1af3cf8a7812a04de259e66b"
+  version "4.3.4"
+  sha256 "008c8fc9f7d24e6e2557a56834478399eb529ed1a0dcef52ddd3432c85a493f8"
 
   url "https://downloads.bitwig.com/stable/#{version}/Bitwig%20Studio%20#{version}.dmg"
   name "Bitwig Studio"
