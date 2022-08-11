bffc35e57fe5b1079325e85134d639fda17adfe5
Patrick Linnane <patrick@linnane.io>
2022-08-11 16:54:46 -0700

    Bump to 4.2.4

--- Casks/cellprofiler.rb
+++ Casks/cellprofiler.rb
@@ -1,9 +1,9 @@
 cask "cellprofiler" do
-  version "4.2.3"
-  sha256 "d52fe1adba9eaa4e16341487059152fd8d4c09f17ae20f71400be4002de8d864"
+  version "4.2.4"
+  sha256 "09661c3318496c0e6c3e61450f67d311e24866a861effa344a8462772977e19b"
 
-  url "https://cellprofiler-releases.s3.amazonaws.com/CellProfiler-macOS-#{version}.zip",
-      verified: "cellprofiler-releases.s3.amazonaws.com/"
+  url "https://github.com/CellProfiler/CellProfiler/releases/download/v#{version}/CellProfiler-macOS-#{version}.zip",
+      verified: "github.com/CellProfiler/CellProfiler/"
   name "CellProfiler"
   desc "Open-source application for biological image analysis"
   homepage "https://cellprofiler.org/"
