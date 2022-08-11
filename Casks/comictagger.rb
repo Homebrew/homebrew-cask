e6747310b1ba11eeef841fbd3efd6cb81d70b20f
Patrick Linnane <patrick@linnane.io>
2022-08-11 15:02:24 -0700

    comictagger 1.4.5
    Update comictagger from 1.4.3 to 1.4.5
    
    Closes #129559.
    
    Signed-off-by: Markus Reiter <me@reitermark.us>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/comictagger.rb
+++ Casks/comictagger.rb
@@ -1,6 +1,6 @@
 cask "comictagger" do
-  version "1.4.3"
-  sha256 "3f3ab89c748c7cbf99aef0d563021a7a1bb1d492f34d28c5caef7539a313ea4e"
+  version "1.4.5"
+  sha256 "8bf80a097a28ce1a37c7aaa99cdd27571553cc3fed30bebb8b236ee715d78411"
 
   url "https://github.com/davide-romanini/comictagger/releases/download/#{version}/ComicTagger-#{version}-osx-10.15.7-x86_64.app.zip"
   name "ComicTagger"
