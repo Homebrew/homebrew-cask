33456246d82bf58d5c57c01ed334f1bdbba398fb
Patrick Linnane <patrick@linnane.io>
2022-08-11 17:04:44 -0700

    geogebra 6.0.724.0
    Update geogebra from 6.0.723.0 to 6.0.724.0
    
    Closes #129579.
    
    Signed-off-by: Markus Reiter <me@reitermark.us>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/geogebra.rb
+++ Casks/geogebra.rb
@@ -1,6 +1,6 @@
 cask "geogebra" do
-  version "6.0.723.0"
-  sha256 "463ca067c5187e0b639b72bef577b2f1bf73c394c9a1a88071c547e3e1c1888c"
+  version "6.0.724.0"
+  sha256 "22e70186ed5a387b0efdfb96efdcde2a1e4374175357e89f8b8b6b207645ac9d"
 
   url "https://download.geogebra.org/installers/#{version.major_minor}/GeoGebra-Classic-#{version.major}-MacOS-Portable-#{version.dots_to_hyphens}.zip"
   name "GeoGebra"
