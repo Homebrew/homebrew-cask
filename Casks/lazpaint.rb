a9ecd0b028c20c8129b2ed411a8b139e1a669186
Patrick Linnane <patrick@linnane.io>
2022-08-12 22:28:48 -0700

    Fix version

--- Casks/lazpaint.rb
+++ Casks/lazpaint.rb
@@ -1,6 +1,6 @@
 cask "lazpaint" do
-  version "7.1.6"
-  sha256 "75088f275e71d6dbd7a76c6ba98083722f2f535fa7f7fa4f839b8f8650f1d862"
+  version "7.2"
+  sha256 "593067a23a3ee43fc852be16a0048daa305a5751ab7afd6f3dfd27e1d6c43971"
 
   url "https://github.com/bgrabitmap/lazpaint/releases/download/v#{version}/lazpaint#{version}_macos64.dmg",
       verified: "github.com/bgrabitmap/lazpaint/"
