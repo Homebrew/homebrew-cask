21c5c54f2dfc80af18353438d7e2f43550d359a1
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-08-13 09:53:46 +0800

    Update todesk.rb

--- Casks/todesk.rb
+++ Casks/todesk.rb
@@ -2,7 +2,7 @@ cask "todesk" do
   version "4.3.1.0"
   sha256 "2ed0a1e07b3c70060f2cfe11a265795ef41ea2912f40d5bc3d66481537587f86"
 
-  url "https://dl.todesk.com/macos/ToDesk_v#{version}.pkg"
+  url "https://dl.todesk.com/macos/ToDesk_#{version}.pkg"
   name "ToDesk"
   desc "Remote control software"
   homepage "https://www.todesk.com/"
@@ -14,7 +14,7 @@ cask "todesk" do
 
   auto_updates true
 
-  pkg "ToDesk_v#{version}.pkg"
+  pkg "ToDesk_#{version}.pkg"
 
   uninstall delete:    "/Applications/ToDesk.app",
             launchctl: [
