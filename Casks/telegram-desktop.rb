2c0dadf48f87b755819bf820d33d9228a04a865a
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2022-08-13 05:26:13 +0500

    telegram-desktop 4.1.0
    Upgrade Telegram Desktop
    
    Closes #129624.
    
    Signed-off-by: Markus Reiter <me@reitermark.us>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/telegram-desktop.rb
+++ Casks/telegram-desktop.rb
@@ -1,6 +1,6 @@
 cask "telegram-desktop" do
-  version "4.0.2"
-  sha256 "26bd233f5e5340e2705a6a3c45b9f67c658cbcca1241343e4e5f5ee078058c17"
+  version "4.1.0"
+  sha256 "16c1bd8bd18df7c7dd97500fd71321351446f6fea27339d0b97a9e7069df2694"
 
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg",
       verified: "updates.tdesktop.com/tmac/"
