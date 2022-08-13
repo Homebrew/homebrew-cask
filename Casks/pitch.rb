4518c72330a1e4bffd07fd1334654b2512ba56e0
Patrick Linnane <patrick@linnane.io>
2022-08-12 22:36:51 -0700

    Update pitch from 1.86.0 to 1.87.2

--- Casks/pitch.rb
+++ Casks/pitch.rb
@@ -1,6 +1,6 @@
 cask "pitch" do
-  version "1.86.0,stable.2,4494312"
-  sha256 "05939048892d1dfa082bb95248a23eec3c479c7de1d3dbf70bbdd1690923ee55"
+  version "1.87.2,stable.1,4545046"
+  sha256 "5fd0bfa2449ed3f29e68e74e798981f96681380480fb831108cac4be074c07da"
 
   url "https://desktop-app-builds.pitch.com/Pitch-#{version.csv.first}-#{version.csv.second}-ci#{version.csv.third}.dmg"
   name "Pitch"
