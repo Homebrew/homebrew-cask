effcb8a2a0c075838c539f69c021312dca0d5ca4
Patrick Linnane <patrick@linnane.io>
2022-08-13 13:25:25 -0700

    cleaneronepro 6.6.4,4950
    Update cleaneronepro from 6.6.3 to 6.6.4
    
    Closes #129689.
    
    Signed-off-by: Markus Reiter <me@reitermark.us>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/cleaneronepro.rb
+++ Casks/cleaneronepro.rb
@@ -1,6 +1,6 @@
 cask "cleaneronepro" do
-  version "6.6.3,4941"
-  sha256 "0f2da1a31eceb73615b5cf6d07bb6ee524eab9bd209e2c13e4160abcc3a525ce"
+  version "6.6.4,4950"
+  sha256 "f612824e8c76230e4b2e0528c8204c487e8a922f9544fe2ca9176ac52faf4446"
 
   url "https://files.trendmicro.com/products/CleanerOnePro/COPG0001/CleanerOnePro_#{version.csv.first.dots_to_underscores}.zip"
   name "cleaneronepro"
