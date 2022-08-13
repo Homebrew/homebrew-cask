3c0d02a8f638367bad155287164077d0619ad7a2
Patrick Linnane <patrick@linnane.io>
2022-08-12 22:24:34 -0700

    mailspring 1.10.5
    Update mailspring from 1.10.4 to 1.10.5
    
    Closes #129663.
    
    Signed-off-by: Markus Reiter <me@reitermark.us>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/mailspring.rb
+++ Casks/mailspring.rb
@@ -1,6 +1,6 @@
 cask "mailspring" do
-  version "1.10.4"
-  sha256 "d93c6dcd0cf569aced174e8ffe3fbb24634a8ac04d774b16c784f4ba90838167"
+  version "1.10.5"
+  sha256 "28a040ad3afc360202e09fbaebb39e5a5d99c0766bf9dd6c349000d0758169ca"
 
   url "https://github.com/Foundry376/Mailspring/releases/download/#{version}/Mailspring.zip",
       verified: "github.com/Foundry376/Mailspring/"
