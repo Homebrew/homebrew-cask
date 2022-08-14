568c79d2201e04127939c04c6232e538fc53bd70
SSAakaash <ssaakaash@icloud.com>
2022-08-14 18:14:20 +0530

    Add 'TL Legacy.app' v148.2

--- Casks/tl-legacy.rb
+++ Casks/tl-legacy.rb
@@ -1,11 +1,11 @@
 cask "tl-legacy" do
   version "148.2"
-  sha256 "e02f5adf1beac248a57f2dbd32fd46617125f00011b7ed04bff6236574f4e66e"
+  sha256 :no_check
 
   url "https://tlaun.ch/macos"
   name "tl-legacy"
-  desc "Good old fast and easy launcher for Minecraft."
-  homepage "https://tlaun.ch"
+  desc "Good old fast and easy launcher for Minecraft"
+  homepage "https://tlaun.ch/"
 
   app "TL Legacy.app"
 end
