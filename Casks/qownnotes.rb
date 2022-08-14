539b3a61511da743683d16a26b1508b79d488838
Patrick Linnane <patrick@linnane.io>
2022-08-14 09:28:30 -0700

    qownnotes 22.8.1
    Update qownnotes from 22.8.0 to 22.8.1
    
    Closes #129721.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,6 +1,6 @@
 cask "qownnotes" do
-  version "22.8.0"
-  sha256 "49b744fbbb2ee394043b202957793b7b880072cc831cdd285faf30cba8a0c194"
+  version "22.8.1"
+  sha256 "93d3e60616415f86b0d2d273c88276fd3ef8b2a476736ce150b04092194caa7d"
 
   url "https://github.com/pbek/QOwnNotes/releases/download/v#{version}/QOwnNotes.dmg",
       verified: "github.com/pbek/QOwnNotes/"
