6ceddafec2da28f3e895603086ca0d45d8db342a
AndrewC <803618+andrewcrook@users.noreply.github.com>
2022-08-15 16:50:11 +0100

    Processing 3.5.4->4.0.1 and new github repo

--- Casks/processing.rb
+++ Casks/processing.rb
@@ -1,9 +1,9 @@
 cask "processing" do
-  version "3.5.4,0270"
+  version "4.0.1,1286"
   sha256 "4d64fe42a6c5c0863cc82e93a036e73731999ee9448be45bc322f91b0010bb6b"
 
-  url "https://github.com/processing/processing/releases/download/processing-#{version.csv.second}-#{version.csv.first}/processing-#{version.csv.first}-macosx.zip",
-      verified: "github.com/processing/processing/"
+  url "https://github.com/processing/processing4/releases/download/processing-#{version.csv.second}-#{version.csv.first}/processing-#{version.csv.first}-macosx.zip",
+      verified: "github.com/processing/processing4/"
   name "Processing"
   desc "Flexible software sketchbook and a language for learning how to code"
   homepage "https://processing.org/"
@@ -19,6 +19,7 @@ cask "processing" do
 
   conflicts_with cask: [
     "homebrew/cask-versions/processing2",
+    "homebrew/cask-versions/processing3",
     "homebrew/cask-versions/processing-beta",
   ]
 
