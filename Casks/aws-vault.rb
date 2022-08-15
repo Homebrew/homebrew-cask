704a35a2cfc9d0094afc7674ad07fad5555e9ed1
Rylan Polster <rslpolster@gmail.com>
2022-08-14 23:02:00 -0400

    aws-vault: use `on_intel` and `on_arm` blocks

--- Casks/aws-vault.rb
+++ Casks/aws-vault.rb
@@ -4,9 +4,10 @@ cask "aws-vault" do
   version "6.6.0"
 
   url "https://github.com/99designs/aws-vault/releases/download/v#{version}/aws-vault-darwin-#{arch}.dmg"
-  if Hardware::CPU.intel?
+  on_intel do
     sha256 "eeb7b8dbdba7bb18fcf32e1c422683d5fd444c6c3b5eb41e760bbfcb2036df6c"
-  else
+  end
+  on_arm do
     sha256 "7c624f16a60f6d48900d7aa7f17add991376f1d280e8a5168094e19098797c35"
   end
 
