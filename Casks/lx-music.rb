652ab5b50c7fa4af88aef5d8e33483d2e89b299f
Rylan Polster <rslpolster@gmail.com>
2022-08-14 23:44:37 -0400

    lx-music: use `on_intel` and `on_arm` blocks

--- Casks/lx-music.rb
+++ Casks/lx-music.rb
@@ -3,9 +3,10 @@ cask "lx-music" do
 
   version "1.22.1"
 
-  if Hardware::CPU.intel?
+  on_intel do
     sha256 "e74158b77f2da4a9b6e0530f7fa7a9f34c66b479922d8708eb06fb475dd3a218"
-  else
+  end
+  on_arm do
     sha256 "462ffb39ae422debc1d23b70f0ea123dc995627d4a3a7b526d0e0d07511ea3fd"
   end
 
