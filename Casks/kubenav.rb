f584f627a515093a2ff46adcb9c687f875eb9e87
Rylan Polster <rslpolster@gmail.com>
2022-08-14 23:44:36 -0400

    kubenav: use `on_intel` and `on_arm` blocks

--- Casks/kubenav.rb
+++ Casks/kubenav.rb
@@ -3,9 +3,10 @@ cask "kubenav" do
 
   version "3.9.0"
 
-  if Hardware::CPU.intel?
+  on_intel do
     sha256 "81dfd86497a1a5081cec1c4a544eb7959218db0107ddc3758b0b3e5367db950f"
-  else
+  end
+  on_arm do
     sha256 "ad3d0778d85cad9271ba5942f9bc6de0130edcb28d1ecc9cfdb0ee3e4c040222"
   end
 
