59b4874f7af990776bc9552774d5b9951f0af330
Rylan Polster <rslpolster@gmail.com>
2022-08-14 23:02:01 -0400

    blockbench: use `on_intel` and `on_arm` blocks

--- Casks/blockbench.rb
+++ Casks/blockbench.rb
@@ -3,9 +3,10 @@ cask "blockbench" do
 
   version "4.3.1"
 
-  if Hardware::CPU.intel?
+  on_intel do
     sha256 "6a18203516ec827f119a78f637825448a3931fd6caad0d81992087478ac83d9e"
-  else
+  end
+  on_arm do
     sha256 "c121000d3c330bb8080977806f90a746a5c85680957dc736f8389f4fcc0fc145"
   end
 
