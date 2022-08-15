97d0c4fad805ab7e2430a3d681d9624deb93d7b9
jack <jacklandrin@hotmail.com>
2022-08-15 23:47:30 +0200

    update only-switch from 2.3.6 to 2.3.7

--- Casks/only-switch.rb
+++ Casks/only-switch.rb
@@ -1,6 +1,6 @@
 cask "only-switch" do
   version "2.3.7"
-  sha256 "558e105e965bb23fb218135894aa5717400a14f35a786a18306daadaabf6b73d"
+  sha256 "2a9ea3c7777560244535a370c1093f859643bafe772a9d272e11f9dd3efa0791"
 
   url "https://github.com/jacklandrin/OnlySwitch/releases/download/release_#{version}/OnlySwitch.dmg"
   name "OnlySwitch"
