55a61d052ac83eb3712ca03960798f115aec985b
Rylan Polster <rslpolster@gmail.com>
2022-08-14 23:44:41 -0400

    openshift-client: use `on_intel` and `on_arm` blocks

--- Casks/openshift-client.rb
+++ Casks/openshift-client.rb
@@ -3,9 +3,10 @@ cask "openshift-client" do
 
   version "4.11.0"
 
-  if Hardware::CPU.intel?
+  on_intel do
     sha256 "8fcfb2d9cc81894dda9a6fbd91dda77cec598aaeee668abf05c4903a51c1138d"
-  else
+  end
+  on_arm do
     sha256 "9c5875208ce1da5a0463fb847942bbdd08116dbd122a2e0ebb6167e5ebeec127"
   end
 
