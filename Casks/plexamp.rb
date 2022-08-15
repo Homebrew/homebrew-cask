af7bb34001ba3f79d9b5043be5acca117d0a531a
Rylan Polster <rslpolster@gmail.com>
2022-08-14 23:44:41 -0400

    plexamp: use `on_intel` and `on_arm` blocks

--- Casks/plexamp.rb
+++ Casks/plexamp.rb
@@ -3,9 +3,10 @@ cask "plexamp" do
 
   version "4.3.0"
 
-  if Hardware::CPU.intel?
+  on_intel do
     sha256 "1ead84054eb80d2ed1232ac3b3768fa082685a2c4f5e2018c4c8414b3e6447ab"
-  else
+  end
+  on_arm do
     sha256 "63a74f995963b536b1baaee256a32513c674189b6ef543badf4bf6f5b7ed6cba"
   end
 
