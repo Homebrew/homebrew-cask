07d195ce868eb7466e69a8b0cd08ff03968f0ef7
Miccal Matthews <miccal.matthews@gmail.com>
2022-08-15 12:44:14 +0800

    Update desmume.rb

--- Casks/desmume.rb
+++ Casks/desmume.rb
@@ -2,18 +2,24 @@ cask "desmume" do
   version "0.9.13"
   sha256 "d42e4bbf8f96b6bfdb3c8be6cf469b606a3b105352460636b1051b8dd0365ebc"
 
-  url "https://github.com/TASEmulators/desmume/releases/download/release_#{version.tr(".","_")}/desmume-#{version}-macOS.dmg",
+  url "https://github.com/TASEmulators/desmume/releases/download/release_#{version.tr(".", "_")}/desmume-#{version}-macOS.dmg",
       verified: "github.com/TASEmulators/desmume/"
   name "DeSmuME"
   desc "Nintendo DS emulator"
   homepage "https://desmume.org/"
 
+  livecheck do
+    url "https://github.com/TASEmulators/desmume/releases/"
+    strategy :page_match
+    regex(/desmume[._-]v?(\d+(?:\.\d+)+)[._-]macOS\.dmg/i)
+  end
+
   app "DeSmuME.app"
 
   zap trash: [
-    "~/Library/Application Support/DeSmuME",
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.desmume.desmume.sfl*",
-    "~/Library/Saved Application State/org.desmume.DeSmuME.savedState",
+    "~/Library/Application Support/DeSmuME",
     "~/Library/Preferences/org.desmume.DeSmuME.plist",
+    "~/Library/Saved Application State/org.desmume.DeSmuME.savedState",
   ]
 end
