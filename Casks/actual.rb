228d9a35b7d1df07763badd77301a8f9901ce71b
Rylan Polster <rslpolster@gmail.com>
2022-08-14 23:01:59 -0400

    actual: use `on_intel` and `on_arm` blocks

--- Casks/actual.rb
+++ Casks/actual.rb
@@ -3,9 +3,10 @@ cask "actual" do
 
   version "0.0.148"
 
-  if Hardware::CPU.intel?
+  on_intel do
     sha256 "95159e54c011aba02f64b0d126a497b99544e1086429a11e67587e3ffc0533d4"
-  else
+  end
+  on_arm do
     sha256 "df9e2139a3a5b1355f1ed1a28863115ac574eb15ec856d6e95de13b02bc26ae0"
   end
 
