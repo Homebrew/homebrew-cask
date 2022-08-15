079aac9b9b0da272d9aa9d2b6fe5ab08ec9fd349
Rylan Polster <rslpolster@gmail.com>
2022-08-14 23:44:28 -0400

    dotnet-sdk: use `on_intel` and `on_arm` blocks

--- Casks/dotnet-sdk.rb
+++ Casks/dotnet-sdk.rb
@@ -1,10 +1,11 @@
 cask "dotnet-sdk" do
   arch arm: "arm64", intel: "x64"
 
-  if Hardware::CPU.intel?
+  on_intel do
     version "6.0.400,2382b0d3-f865-4818-b3aa-47a94ccedba8,cd1eb0d061a1e1d8d0373603c4e82a97"
     sha256 "c31cde7a2a0f4f63e91229d3122ae5bb7f758b0cf3093b486b0e683f6d2a62a3"
-  else
+  end
+  on_arm do
     version "6.0.400,a92daceb-cf41-4c37-8e70-8a158889a9b2,c027605fb8d8b51ee0892e10ed874ab8"
     sha256 "adb224aa775120fdaf88b48a890466853e8a8a0719c9de0199d79451b41ba7d3"
   end
