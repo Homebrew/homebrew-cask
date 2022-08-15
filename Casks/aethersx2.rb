8fd3edaae15440e00b58e5b658779d48107ff11c
Roman Zhuzha <roman.zhuzha@getbux.com>
2022-08-15 19:17:17 +0200

    improve version for AetherSX2

--- Casks/aethersx2.rb
+++ Casks/aethersx2.rb
@@ -1,15 +1,15 @@
 cask "aethersx2" do
-  version "2278"
+  version "alpha-2278"
   sha256 "717764c8a76f48315d4c880b32dfb815cc3538e7947a53a8c76e027fda881168"
 
-  url "https://aethersx2.com/archive/desktop/mac/AetherSX2-alpha-#{version}-mac.zip"
+  url "https://aethersx2.com/archive/desktop/mac/AetherSX2-#{version}-mac.zip"
   name "AetherSX2"
   desc "Sony PlayStation 2 emulator for ARM based Macs"
   homepage "https://aethersx2.com/"
 
   livecheck do
     url "https://www.aethersx2.com/archive/?dir=desktop/mac"
-    regex(/AetherSX2[._-]alpha[._-](\d+)[._-]mac\.zip/i)
+    regex(/AetherSX2[._-]((?:v\d+(?:\.\d+)|alpha)[._-]\d+)[._-]mac\.zip/i)
   end
 
   depends_on arch: :arm64
