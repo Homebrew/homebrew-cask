e230bcf2b628920de44cf4f2834ac7e82157e2ee
Rylan Polster <rslpolster@gmail.com>
2022-08-14 23:44:42 -0400

    polypane: use `on_intel` and `on_arm` blocks

--- Casks/polypane.rb
+++ Casks/polypane.rb
@@ -3,9 +3,10 @@ cask "polypane" do
 
   version "10.0.1"
 
-  if Hardware::CPU.intel?
+  on_intel do
     sha256 "303929e6821f4d5c124c10c257104c5fd90fd4a6cbd13c68a288dee4dcc1fd00"
-  else
+  end
+  on_arm do
     sha256 "bf3933c7461d952e19fab0e444712c24567abc64680092bff108993c15a8a205"
   end
 
