1d706f16f10cc7e03bad564ce168db2c714f05cd
Patrick Linnane <patrick@linnane.io>
2022-08-15 09:31:18 -0700

    Fix Intel sha256

--- Casks/hepta.rb
+++ Casks/hepta.rb
@@ -4,7 +4,7 @@ cask "hepta" do
   version "0.185.1"
 
   on_intel do
-    sha256 "1d2cca07e9b7d8e67e4291577431e43a6fb0fc8060a4b38d8f2855fab3fef09c"
+    sha256 "fa7b5e300e255a077bffce05a29d91ebb5ad4e16f46940a80e0259d7ba904605"
   end
   on_arm do
     sha256 "60c426a03e9c32b44a9b289ccdefb6d30ad28c1f40930fdaac9fbfea28827c3e"
