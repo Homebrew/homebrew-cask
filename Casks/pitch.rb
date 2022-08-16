6d228486f141db5c67323cb9a4ea499cffdebefd
Patrick Linnane <patrick@linnane.io>
2022-08-16 08:44:14 -0700

    pitch 1.88.1,stable.1,4556826
    Update pitch from 1.87.2 to 1.88.1
    
    Closes #129836.
    
    Signed-off-by: Markus Reiter <me@reitermark.us>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/pitch.rb
+++ Casks/pitch.rb
@@ -1,6 +1,6 @@
 cask "pitch" do
-  version "1.87.2,stable.2,4549888"
-  sha256 "c9439ecc713832f6da71f70dd937041d15816af0f3aa2554f72473022bf2c2a2"
+  version "1.88.1,stable.1,4556826"
+  sha256 "e002252cd43a6755bd5289471162e4fd532c8a440e2a276f2f79a6c71af17b76"
 
   url "https://desktop-app-builds.pitch.com/Pitch-#{version.csv.first}-#{version.csv.second}-ci#{version.csv.third}.dmg"
   name "Pitch"
