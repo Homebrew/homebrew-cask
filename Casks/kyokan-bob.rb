b6f1c9354946e66c104ff2e4214f6aec930a6fde
Bevan Kay <email@bevankay.me>
2022-08-16 23:43:30 +1000

    Update kyokan-bob.rb

--- Casks/kyokan-bob.rb
+++ Casks/kyokan-bob.rb
@@ -1,11 +1,12 @@
 cask "kyokan-bob" do
+  arch arm: "arm64", intel: "x86"
+  
   version "1.0.0"
 
-  if Hardware::CPU.intel?
-    arch = "x86"
+  on_intel do
     sha256 "f4e962e38d8679a5abe7baf181d12ea679bd90876fe991a4c3c01b21f10195c8"
-  else
-    arch = "arm64"
+  end
+  on_arm do
     sha256 "03c913e5ea5d8779122b4da3a9ef0d63307ce84a7a86e84feae53af82bb6fd36"
   end
 
@@ -14,11 +15,6 @@ cask "kyokan-bob" do
   name "Bob Wallet"
   desc "Handshake wallet GUI for managing transactions, name auctions, and DNS records"
   homepage "https://bobwallet.io/"
-
-  livecheck do
-    url :url
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
-  end
 
   app "Bob.app"
 
