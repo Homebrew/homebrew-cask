e5bbb54849549b4cd17bd0b1a649d5c8f288075e
Patrick Linnane <patrick@linnane.io>
2022-08-16 08:15:17 -0700

    hepta 0.185.2
    * Update hepta from 0.185.1 to 0.185.2
    * Fix Intel sha256
    
    Closes #129819.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/hepta.rb
+++ Casks/hepta.rb
@@ -1,13 +1,13 @@
 cask "hepta" do
   arch arm: "arm64-mac", intel: "mac"
 
-  version "0.185.1"
+  version "0.185.2"
 
   on_intel do
-    sha256 "fa7b5e300e255a077bffce05a29d91ebb5ad4e16f46940a80e0259d7ba904605"
+    sha256 "302ba70b1d8c65a43d28e36f011812831c480bd2e45eaf1bb4662b85af6e26cc"
   end
   on_arm do
-    sha256 "60c426a03e9c32b44a9b289ccdefb6d30ad28c1f40930fdaac9fbfea28827c3e"
+    sha256 "00c0dea79789300e0e327b8f10b4502963431bd9be9c1ac21cdedbf4a24382f3"
   end
 
   url "https://github.com/heptameta/project-meta/releases/download/v#{version}/Hepta-#{version}-#{arch}.zip",
