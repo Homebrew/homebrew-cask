54a422cbad36460ce035148aec487c8664d5e29d
Dmitriy Vorobiev <plus79166302110@gmail.com>
2022-08-16 03:25:26 +0300

    Add Chromium-GOST v.103.0.5060.134 (#129343)
    * Add Chromium-GOST v.103.0.5060.134
    
    * translate description
    
    * Reduced the number of characters in the description
    
    * add zap

---
+++ Casks/chromium-gost.rb
@@ -0,0 +1,16 @@
+cask "chromium-gost" do
+  version "103.0.5060.134"
+  sha256 "f7bcca0f328fae408773700dae023aa036e909d48022157b0c6627b860a1ea78"
+
+  url "https://github.com/deemru/Chromium-Gost/releases/download/#{version}/chromium-gost-#{version}-macos-amd64.tar.bz2"
+  name "Chromium-Gost"
+  desc "Browser based on Chromium with support for GOST cryptographic algorithms"
+  homepage "https://github.com/deemru/Chromium-Gost"
+
+  app "Chromium-Gost.app"
+
+  zap trash: [
+    "~/Library/Application Support/Chromium",
+    "~/Library/Caches/Chromium",
+  ]
+end
