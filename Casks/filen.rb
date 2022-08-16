18dae3b30a77a25cc5b5fa90dad5000b5cdecd70
Nebow <81814662+Nebow@users.noreply.github.com>
2022-08-16 13:39:51 +0000

    Add Filen.app 2.0.4

---
+++ Casks/filen.rb
@@ -0,0 +1,25 @@
+cask "filen" do
+  arch arm: "arm64", intel: "x64"
+
+  version "2.0.4"
+  sha256 :no_check
+
+  url "https://cdn.filen.io/desktop/release/filen_#{arch}.dmg"
+  name "Filen"
+  desc "Desktop client for Filen.io"
+  homepage "https://filen.io/"
+
+  livecheck do
+    url "https://github.com/FilenCloudDienste/filen-desktop/releases/latest/"
+    strategy :github_latest
+  end
+
+  app "Filen.app"
+
+  zap trash: [
+    "~/Library/Preferences/io.filen.desktop.plist",
+    "~/Library/Application Support/filen-desktop",
+    "~/Library/Saved Application State/io.filen.desktop.savedState",
+    "~/Library/Logs/filen-desktop",
+  ]
+end
