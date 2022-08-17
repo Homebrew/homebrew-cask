b5ff4b90c134881404a65b9524ca66e1b7680ca6
Roman Zhuzha <sanyer@users.noreply.github.com>
2022-08-17 17:32:48 +0200

    move 'v' outside the version

--- Casks/aethersx2.rb
+++ Casks/aethersx2.rb
@@ -1,15 +1,15 @@
 cask "aethersx2" do
-  version "v1.1-2596"
+  version "1.1-2596"
   sha256 "bb97a8c29edd80430d813889455c2d745b522d4b0f73caa6d0eca37d94d35f3c"
 
-  url "https://aethersx2.com/archive/desktop/mac/AetherSX2-#{version}-mac.zip"
+  url "https://aethersx2.com/archive/desktop/mac/AetherSX2-v#{version}-mac.zip"
   name "AetherSX2"
   desc "Sony PlayStation 2 emulator for ARM based Macs"
   homepage "https://aethersx2.com/"
 
   livecheck do
     url "https://www.aethersx2.com/archive/?dir=desktop/mac"
-    regex(/AetherSX2[._-](v\d+(?:\.\d+)[._-]\d+)[._-]mac\.zip/i)
+    regex(/AetherSX2[._-]v?(\d+(?:\.\d+)[._-]\d+)[._-]mac\.zip/i)
   end
 
   depends_on arch: :arm64
