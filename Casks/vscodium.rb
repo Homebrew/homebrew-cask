ae9ae71e28e7dc5e853853147618b0eb9267d862
Patrick Linnane <patrick@linnane.io>
2022-08-16 20:01:04 -0700

    vscodium 1.70.1.22228
    * Update vscodium from 1.70.1 to 1.70.1.22228
    * Fix Intel sha256
    
    Closes #129896.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/vscodium.rb
+++ Casks/vscodium.rb
@@ -1,13 +1,13 @@
 cask "vscodium" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.70.1"
+  version "1.70.1.22228"
 
   on_intel do
-    sha256 "3f7123c87c6486016512ab70714bf7928a419accc207b9a4caddd3e7e6e44fa8"
+    sha256 "8f39c5183f9454b0d226a3c6544fe5d390b3a1d04dc16ecec9b37ba9d4927024"
   end
   on_arm do
-    sha256 "49d8fd0606d09aa29fb42fafe6da7f5cc92150e73d45eb53527aa844e59ce52d"
+    sha256 "b7ca5e15160fce428dc49ba6020623c34ab0e64587481c2fc0a1973f3d852413"
   end
 
   url "https://github.com/VSCodium/vscodium/releases/download/#{version}/VSCodium.#{arch}.#{version}.dmg"
