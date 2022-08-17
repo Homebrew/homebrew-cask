b8a0de23f701f94f31424b92a077f6abae4407c6
Depal1 <47154119+Depal1@users.noreply.github.com>
2022-08-17 09:08:14 -0600

    Update playcover-community to 1.1.0

--- Casks/playcover-community.rb
+++ Casks/playcover-community.rb
@@ -1,14 +1,15 @@
 cask "playcover-community" do
-  version "1.0.7"
-  sha256 "83577fb7683091e018b186ace8f9d85c50fdf7e2c4f665589f26b9e36d4b6e1f"
+  version "1.1.0"
+  sha256 "e23266483763bf6aaf81f539a4b29927e89ef4ea18a98db411c3978350a079d1"
 
   url "https://github.com/PlayCover/PlayCover/releases/download/#{version}/PlayCover_#{version}.dmg"
   name "PlayCover"
   desc "Sideload iOS apps and games"
   homepage "https://github.com/PlayCover/PlayCover"
 
+  auto_updates true
   depends_on arch: :arm64
-  depends_on macos: :monterey
+  depends_on macos: ">= :monterey"
 
   app "PlayCover.app"
 
