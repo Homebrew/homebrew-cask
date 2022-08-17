96661db05f1eb8285a1f5d21fd266f881cf94e2e
Patrick Linnane <patrick@linnane.io>
2022-08-16 19:26:07 -0700

    insync 3.7.11.50381
    Update insync from 3.7.9.50368 to 3.7.11.50381
    
    Closes #129882.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/insync.rb
+++ Casks/insync.rb
@@ -1,8 +1,8 @@
 cask "insync" do
-  version "3.7.9.50368"
-  sha256 "c329885afe3eeba1118ffa4087706529ef09b932c0144c9f91dd26611efb2631"
+  version "3.7.11.50381"
+  sha256 "ed6e955467ce025fc6a8fd3b0fc89426eb010a392de7d6198ce932c2659f124c"
 
-  url "http://s.insynchq.com/builds/Insync-#{version}.dmg"
+  url "http://cdn.insynchq.com/builds/mac/Insync-#{version}.dmg"
   name "Insync"
   desc "Manage your Google Drive and OneDrive files"
   homepage "https://www.insynchq.com/"
