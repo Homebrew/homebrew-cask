0129abd3613ee58bb155e759e71936a462e52c66
Patrick Linnane <patrick@linnane.io>
2022-08-17 08:49:17 -0700

    Fix Intel sha256

--- Casks/brave-browser.rb
+++ Casks/brave-browser.rb
@@ -5,7 +5,7 @@ cask "brave-browser" do
   version "1.42.97.0,142.97"
 
   on_intel do
-    sha256 "3fa4ae81ea619140906164d114526419a783bb34017a69c5de6b4114eee0df00"
+    sha256 "fc7e8409f24b5bec4cebf8fd61bbf57069b5acf5eaa69c0db3100b80b26a18fc"
   end
   on_arm do
     sha256 "0f9871c5dec32b0caf4bf26c29101ae333fec870926312e039baec9c5182d756"
