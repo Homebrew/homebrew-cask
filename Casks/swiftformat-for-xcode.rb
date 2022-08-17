c6ff9bcc03c8ea3621eceac319e044bfade563f4
Patrick Linnane <patrick@linnane.io>
2022-08-16 19:46:01 -0700

    Update swiftformat-for-xcode from 0.49.16 to 0.49.17

--- Casks/swiftformat-for-xcode.rb
+++ Casks/swiftformat-for-xcode.rb
@@ -1,6 +1,6 @@
 cask "swiftformat-for-xcode" do
-  version "0.49.16"
-  sha256 "86038303bfc3111287a185eeb27c1b81c5757c08dadd9c7ffb90bb02cdfef28c"
+  version "0.49.17"
+  sha256 "4b61b36b78ffc23b9b71c3c50873c65fa9dcb612e8a4a1badcdd6d73d207a873"
 
   url "https://github.com/nicklockwood/SwiftFormat/releases/download/#{version}/SwiftFormat.for.Xcode.app.zip"
   name "SwiftFormat for Xcode"
