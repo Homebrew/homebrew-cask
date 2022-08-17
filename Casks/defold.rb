dd659b29642913ffd3ebc7e57f84ed9ec2815c9a
Patrick Linnane <patrick@linnane.io>
2022-08-17 09:00:18 -0700

    Update defold from 1.3.4 to 1.3.5

--- Casks/defold.rb
+++ Casks/defold.rb
@@ -1,6 +1,6 @@
 cask "defold" do
-  version "1.3.4"
-  sha256 "5a22302ab5aabd61d3023b188242a9c3464f72aefeff555a6a0ec3ca9de755ba"
+  version "1.3.5"
+  sha256 "4f07a004761370bd1c781c3f966a27b2b8e22f16f654c66424255c2e47dbed6c"
 
   url "https://github.com/defold/defold/releases/download/#{version}/Defold-x86_64-darwin.dmg",
       verified: "github.com/defold/defold/"
