4ccd27c7baa72005f0f70141507cfef02999417d
Patrick Linnane <patrick@linnane.io>
2022-08-18 08:48:43 -0700

    clion 2022.2.1,222.3739.54
    * Update clion from 2022.2 to 2022.2.1
    * Fix Intel sha256
    
    Closes #129980.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/clion.rb
+++ Casks/clion.rb
@@ -1,13 +1,13 @@
 cask "clion" do
   arch arm: "-aarch64"
 
-  version "2022.2,222.3345.126"
+  version "2022.2.1,222.3739.54"
 
   on_intel do
-    sha256 "27f92a2513ff55a3c354e46e0562c900dbab68e26455be51c3eeb42995aa2ac1"
+    sha256 "ba2a8fcf9a1f080ca6b2ef832b13104c440077b9e6a2bb6e26bc97bdea373208"
   end
   on_arm do
-    sha256 "b25375ef2d6daa75c83d624633e18850529f0a6a72ae54bbb6a22f9343cc2c86"
+    sha256 "af36f7f9a98881d37d89757083875494c472e60d14bd70fe0d08533d284dd4e1"
   end
 
   url "https://download.jetbrains.com/cpp/CLion-#{version.csv.first}#{arch}.dmg"
