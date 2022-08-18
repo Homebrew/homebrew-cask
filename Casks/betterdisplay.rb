dbaf194d2214d368bee27cd9a069a8ac04b57be2
Patrick Linnane <patrick@linnane.io>
2022-08-18 11:08:11 -0700

    betterdisplay 1.2.9
    Update betterdisplay from 1.2.8 to 1.2.9
    
    Closes #130029.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/betterdisplay.rb
+++ Casks/betterdisplay.rb
@@ -1,6 +1,6 @@
 cask "betterdisplay" do
-  version "1.2.8"
-  sha256 "708a884c77ab8353da9625e88fbabce0173d6e6a097e3648cab46ccca654414a"
+  version "1.2.9"
+  sha256 "c6ff91d8c050753ed80c8082e8bdc608ddaa7f32a3cb7bbce390a88aa459abab"
 
   url "https://github.com/waydabber/BetterDisplay/releases/download/v#{version}/BetterDisplay-v#{version}.dmg",
       verified: "github.com/waydabber/BetterDisplay"
