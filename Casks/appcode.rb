f34ef542e591d344f615e5acb62f471193cffef0
MoonFruit <dkmoonfruit@gmail.com>
2022-08-18 10:35:58 +0800

    Update appcode from 2022.2 to 2022.2.1

--- Casks/appcode.rb
+++ Casks/appcode.rb
@@ -1,10 +1,10 @@
 cask "appcode" do
   arch arm: "-aarch64"
 
-  version "2022.2,222.3345.144"
+  version "2022.2.1,222.3739.57"
 
   on_intel do
-    sha256 "68f929177be0b90bebec53615750a765cb10c917925286a1d82a71e7c3e3b737"
+    sha256 "00b749b4bf78a815b68b39e2bd4baf0f05700b84d7f2c099a542f86e194a0b35"
   end
   on_arm do
     sha256 "94f2e968ff184b94be516e26cf750c35e5e3d13a3a4edba583f8b1aab05473cc"
