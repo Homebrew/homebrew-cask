289b743eb9d905a7e24e23fba877505a42d17250
Patrick Linnane <patrick@linnane.io>
2022-08-18 09:10:23 -0700

    hashbackup 3039
    Update hashbackup from 3038 to 3039
    
    Closes #129994.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/hashbackup.rb
+++ Casks/hashbackup.rb
@@ -1,6 +1,6 @@
 cask "hashbackup" do
-  version "3038"
-  sha256 "b3a4a251ad2e0f72d09750544127bc098d21f23c3b5d82b69ed988b27cbd2abb"
+  version "3039"
+  sha256 "1f72c9ab204a81e5a84d05b081e34298a4fbbf84f3ef11d28d6ca1ee026f343f"
 
   url "http://upgrade.hashbackup.com/#{version}/hb.r#{version}.Darwin.x86_64.bz2"
   name "HashBackup"
