59a64350b13714ad012a28843cfa6000dd198449
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-18 22:19:10 +0800

    Add ARM64 artefact for Julia

--- Casks/julia.rb
+++ Casks/julia.rb
@@ -1,8 +1,12 @@
 cask "julia" do
-  version "1.8.0"
-  sha256 "29880bb57614c9c597edf5384a351b3f0299fa097b9eb62cd640c93cde58677b"
+  arch arm: "aarch64", intel: "x64"
+  midfix = on_arch_conditional arm: arch
 
-  url "https://julialang-s3.julialang.org/bin/mac/x64/#{version.major_minor}/julia-#{version}-mac64.dmg"
+  version "1.8.0"
+  sha256 arm:   "8e113a42fe4eac7b75023136674452f6f09590717755417cd137258707e96540",
+         intel: "29880bb57614c9c597edf5384a351b3f0299fa097b9eb62cd640c93cde58677b"
+
+  url "https://julialang-s3.julialang.org/bin/mac/#{arch}/#{version.major_minor}/julia-#{version}-mac#{midfix}64.dmg"
   name "Julia"
   desc "Programming language for technical computing"
   homepage "https://julialang.org/"
