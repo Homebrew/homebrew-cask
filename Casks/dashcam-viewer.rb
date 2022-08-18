fd1d156023022465e37547c545d4aca1f101fe70
Patrick Linnane <patrick@linnane.io>
2022-08-18 08:59:54 -0700

    dashcam-viewer 3.8.7
    Update dashcam-viewer from 3.8.6 to 3.8.7
    
    Closes #129985.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/dashcam-viewer.rb
+++ Casks/dashcam-viewer.rb
@@ -1,6 +1,6 @@
 cask "dashcam-viewer" do
-  version "3.8.6"
-  sha256 "9ced96bdde0028b8ad95dbfb5d5751d1f3b6f9d9039628ec2b2056e29efe7f02"
+  version "3.8.7"
+  sha256 "3337a39794289e2897e712b667d0ba1e320a37910325274388d64d03b60a3c26"
 
   url "https://filedn.com/l2s8TAtm4VASBX72ds0zYD8/dcv/Dashcam_Viewer_v#{version}.dmg",
       verified: "filedn.com/l2s8TAtm4VASBX72ds0zYD8/dcv/"
