1811e450ae5f119d06242090c3ebe2f435b9d83a
Patrick Linnane <patrick@linnane.io>
2022-08-18 09:56:31 -0700

    Update qqlive from 2.51.1.53393 to 2.52.0.53407

--- Casks/qqlive.rb
+++ Casks/qqlive.rb
@@ -1,6 +1,6 @@
 cask "qqlive" do
-  version "2.51.1.53393"
-  sha256 "0be7ad69575aaddaf6d7de29f25a9da6cff47df30ef5219bfe5ea2d3293fc838"
+  version "2.52.0.53407"
+  sha256 "28c3868ba7f965175ac378702056c3daaddc7cb8d7873e4252b030ac5c819456"
 
   url "https://dldir1.qq.com/qqtv/mac/QQLive_distribute_universal_#{version}.dmg"
   name "QQLive"
