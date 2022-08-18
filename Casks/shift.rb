842a29caf3973e4c76617d2790f4c7f9aaa4c5d7
chiefmikey <wolfemikl@gmail.com>
2022-08-17 22:38:11 -0600

    shift 8.0.48.473
    Update shift from 8.0.46.443 to 8.0.48.473
    
    Closes #129959.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/shift.rb
+++ Casks/shift.rb
@@ -1,10 +1,10 @@
 cask "shift" do
   arch arm: "arm64", intel: "x64"
 
-  version "8.0.46.443"
+  version "8.0.48.473"
 
   on_intel do
-    sha256 "510c15f5c6c012710eea3bb7d4706f9fc26e069a7021a43bb6fa8e0b49cdf8ca"
+    sha256 "50cfaa6b97aefab0af61e26c53dbb0925467fffbc0de50434739769096b105a7"
   end
   on_arm do
     sha256 "5e07cb8f12b64f9f23b0971b1ec8127a9c204272ace6cb2c2084c5c46b71ac58"
