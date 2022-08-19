cf9319e3befb7947ee4b9db30a5c74d7ea1e88c6
Neil Hawkins <neilh@cogapp.com>
2022-08-19 10:38:26 +0100

    Update screaming-frog-seo-spider from 16.7 to 17.0

--- Casks/screaming-frog-seo-spider.rb
+++ Casks/screaming-frog-seo-spider.rb
@@ -1,6 +1,6 @@
 cask "screaming-frog-seo-spider" do
-  version "16.7"
-  sha256 "6379947949b879a7a9a88da1c2b5022491cd9865f7d5bb4d88aae7be3fd6df18"
+  version "17.0"
+  sha256 "db3d53f9796fdf19096bf512df0ac401178b205c3e06660f7d60ed9efa48e574"
 
   url "https://download.screamingfrog.co.uk/products/seo-spider/ScreamingFrogSEOSpider-#{version}.dmg"
   name "Screaming Frog SEO Spider"
