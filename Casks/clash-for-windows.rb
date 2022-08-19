b379f5d65083d05f629f065aba8023f9e606b721
Patrick Linnane <patrick@linnane.io>
2022-08-19 07:26:57 -0700

    clash-for-windows 0.19.27
    Update clash-for-windows from 0.19.26 to 0.19.27
    
    Closes #130057.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/clash-for-windows.rb
+++ Casks/clash-for-windows.rb
@@ -1,13 +1,13 @@
 cask "clash-for-windows" do
   arch arm: "-arm64"
 
-  version "0.19.26"
+  version "0.19.27"
 
   on_intel do
-    sha256 "6572b20a7cee1ecea9826bf0f5bcc04dd93771cb26b205b64cb28646be6313eb"
+    sha256 "b70a89a3b24c6c744abf95a0007cddd69b298fe0c2c3cf3750fc5c04f77da131"
   end
   on_arm do
-    sha256 "c859cce3e780cf7a3c52be32ea96d266ad3b1e9925e1c4438e3e7fb70f6b8b77"
+    sha256 "73da9d4331ef69e56713565de7078555302df25c953f62ce45428465c84057c2"
   end
 
   url "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/#{version}/Clash.for.Windows-#{version}#{arch}.dmg"
