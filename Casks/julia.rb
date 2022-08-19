732e532d2f5031c1b8b7779a0f114a7bba03162a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-19 16:54:41 +0800

    Update julia from 1.7.3 to 1.8.0 (#129974)
    * Update julia from 1.7.3 to 1.8.0
    
    * Add ARM64 artefact for Julia
    
    * Update julia.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/julia.rb
+++ Casks/julia.rb
@@ -1,15 +1,24 @@
 cask "julia" do
-  version "1.7.3"
-  sha256 "67799ec06cf57bca80a8a1c6e5a170485d3bfc0461b217699b0730da37d0656f"
+  arch arm: "aarch64", intel: "x64"
+  midfix = on_arch_conditional arm: arch
 
-  url "https://julialang-s3.julialang.org/bin/mac/x64/#{version.major_minor}/julia-#{version}-mac64.dmg"
+  version "1.8.0"
+
+  on_intel do
+    sha256 "29880bb57614c9c597edf5384a351b3f0299fa097b9eb62cd640c93cde58677b"
+  end
+  on_arm do
+    sha256 "8e113a42fe4eac7b75023136674452f6f09590717755417cd137258707e96540"
+  end
+
+  url "https://julialang-s3.julialang.org/bin/mac/#{arch}/#{version.major_minor}/julia-#{version}-mac#{midfix}64.dmg"
   name "Julia"
   desc "Programming language for technical computing"
   homepage "https://julialang.org/"
 
   livecheck do
     url "https://julialang.org/downloads/"
-    regex(/href=.*?julia[._-]v?(\d+(?:\.\d+)+)[._-]mac64\.dmg/i)
+    regex(/href=.*?julia[._-]v?(\d+(?:\.\d+)+)[._-]mac#{midfix}64\.dmg/i)
   end
 
   app "Julia-#{version.major_minor}.app"
