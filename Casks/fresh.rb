408e09d80388cffa4ade9edeb6b0bef17241049c
Miccal Matthews <miccal.matthews@gmail.com>
2022-08-20 07:58:39 +0800

    Update fresh.rb

--- Casks/fresh.rb
+++ Casks/fresh.rb
@@ -1,11 +1,18 @@
 cask "fresh" do
-  version "4.0.4"
+  version "4.0.4,404.101"
   sha256 :no_check
 
   url "https://ironicsoftware.com/downloads/Fresh.zip"
-  name "Ironic Software Fresh"
+  name "Fresh"
   desc "Keep your recently modified files at hand and up-to-date"
   homepage "https://ironicsoftware.com/fresh/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
+
+  depends_on macos: ">= :high_sierra"
 
   app "Fresh.app"
 end
