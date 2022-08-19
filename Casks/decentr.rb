2e71f4cb3d35111dead153436882f045aac3f226
Miccal Matthews <miccal.matthews@gmail.com>
2022-08-19 16:53:03 +0800

    Update decentr.rb

--- Casks/decentr.rb
+++ Casks/decentr.rb
@@ -3,8 +3,12 @@ cask "decentr" do
 
   version "1.3.2"
 
-  on_arm   { sha256 "a15e91def4242b950b23f46dd2d0a18e2348c5f0c7e030b82e4acdcb301c1511" }
-  on_intel { sha256 "9429047b44478fcc6bc48ac5bca73f9064a992e3b07c6ad33a8868f5cfa6d375" }
+  on_intel do
+    sha256 "9429047b44478fcc6bc48ac5bca73f9064a992e3b07c6ad33a8868f5cfa6d375"
+  end
+  on_arm do
+    sha256 "a15e91def4242b950b23f46dd2d0a18e2348c5f0c7e030b82e4acdcb301c1511"
+  end
 
   url "https://decentr.net/files/MacOS_#{arch}_Decentr_#{version}.zip"
   name "Decentr"
@@ -13,7 +17,7 @@ cask "decentr" do
 
   livecheck do
     url :homepage
-    regex(/href=.*?MacOS_#{arch}_Decentr_v?(\d+(?:\.\d+)+)\.zip/i)
+    regex(/href=.*?MacOS[._-]#{arch}[._-]Decentr[._-]v?(\d+(?:\.\d+)+)\.zip/i)
   end
 
   app "Decentr.app"
