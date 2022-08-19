d16a0d6ac8fe739168e2dfaa74edb11d29e8eb9a
Patrick Linnane <patrick@linnane.io>
2022-08-19 10:07:42 -0700

    v2rayu: Add arm64, bump to 3.3.0 (#130117)
    Add arm64, bump to 3.3.0

--- Casks/v2rayu.rb
+++ Casks/v2rayu.rb
@@ -1,8 +1,16 @@
 cask "v2rayu" do
-  version "3.2.0"
-  sha256 "c22301909180efb6750def820952c25e0b7fbe5838de3cbfac1bdd219ececae5"
+  arch arm: "arm64", intel: "64"
 
-  url "https://github.com/yanue/V2rayU/releases/download/#{version}/V2rayU.dmg"
+  version "3.3.0"
+
+  on_intel do
+    sha256 "f20de5fae2308ac4c528d1e6ce9fd1aa071cfc05245d19924408640b2bbfd62b"
+  end
+  on_arm do
+    sha256 "9fb1aa5a2e8d5dc0243c6f5c6b99ee5ac44cf233d0a0280badc5a9709a22421c"
+  end
+
+  url "https://github.com/yanue/V2rayU/releases/download/#{version}/V2rayU-#{arch}.dmg"
   name "V2rayU"
   desc "Collection of tools to build a dedicated basic communication network"
   homepage "https://github.com/yanue/V2rayU"
