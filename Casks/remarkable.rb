cec3f9686e31b935d60a108b6e9b49b5a8b97c8c
Patrick Linnane <patrick@linnane.io>
2022-08-19 08:52:35 -0700

    remarkable 2.14.3.623,40828928
    Update remarkable from 2.14.2.612 to 2.14.3.623
    
    Closes #130086.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/remarkable.rb
+++ Casks/remarkable.rb
@@ -1,8 +1,8 @@
 cask "remarkable" do
-  version "2.14.2.612,40108032"
-  sha256 "e70aef4218e4ae27a0df095d32f489aa9d89cc2bb3de1da6ab2989d1bb4c6300"
+  version "2.14.3.623,40828928"
+  sha256 "0bad462af1a1abdc4787e13b1c97a7eaa20aadb2465c733bde33dfcb0f03bfe3"
 
-  url "https://updates-download.cloud.remarkable.engineering/sparkle/reMarkableMacOs/Staging_9fb83f4b-c3d3-4d4d-83ca-8970001ea9a3/#{version.csv.second}/reMarkable-#{version.csv.first}.dmg",
+  url "https://updates-download.cloud.remarkable.engineering/sparkle/reMarkableMacOs/Prod/#{version.csv.second}/reMarkable-#{version.csv.first}.dmg",
       verified: "updates-download.cloud.remarkable.engineering/sparkle/reMarkableMacOs/"
   name "Remarkable"
   desc "View, Screen Share, organize, import, and download files to a reMarkable device"
