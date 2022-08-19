532279054e4ee1f7b9818514bd9d4051216a90c5
Patrick Linnane <patrick@linnane.io>
2022-08-19 12:13:25 -0700

    Fix URL

--- Casks/poi.rb
+++ Casks/poi.rb
@@ -7,7 +7,7 @@ cask "poi" do
     sha256 "5eff9c247998274c8aff80cec4935794c9080ea1d31a90d26d12d2284cd35dfb"
   end
   on_arm do
-    sha256 "5eff9c247998274c8aff80cec4935794c9080ea1d31a90d26d12d2284cd35dfb"
+    sha256 "80dab844d0f974f5aed73f964fcaf8a4e9d7874068afe09aab7fd96363cb2131"
   end
 
   url "https://github.com/poooi/poi/releases/download/v#{version}/poi-#{version}#{arch}.dmg",
