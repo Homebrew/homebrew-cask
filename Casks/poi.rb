a1b879545c14c40f46422f39445eeefd31f2841e
Patrick Linnane <patrick@linnane.io>
2022-08-19 11:51:36 -0700

    poi 10.9.1
    * Add arm64, bump to 10.9.1
    * Fix URL
    * Fix URL
    
    Closes #130126.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/poi.rb
+++ Casks/poi.rb
@@ -1,12 +1,22 @@
 cask "poi" do
-  version "10.9.0"
-  sha256 "c6deb80f7fe7dc6e34ed21a01d11be38c54affd043d7e73677aa5c4dd15f0ed7"
+  arch arm: "-arm64", intel: ""
 
-  url "https://github.com/poooi/poi/releases/download/v#{version}/poi-#{version}.dmg",
+  version "10.9.1"
+
+  on_intel do
+    sha256 "5eff9c247998274c8aff80cec4935794c9080ea1d31a90d26d12d2284cd35dfb"
+  end
+  on_arm do
+    sha256 "80dab844d0f974f5aed73f964fcaf8a4e9d7874068afe09aab7fd96363cb2131"
+  end
+
+  url "https://github.com/poooi/poi/releases/download/v#{version}/poi-#{version}#{arch}.dmg",
       verified: "github.com/poooi/poi/"
   name "poi"
   desc "Scalable KanColle browser and tool"
   homepage "https://poi.moe/"
+
+  depends_on macos: ">= :high_sierra"
 
   app "poi.app"
 
