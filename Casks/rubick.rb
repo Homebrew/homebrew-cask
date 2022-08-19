d8a5ef518ec5607ad49bf4037c43cdc9f9a7bb09
Miccal Matthews <miccal.matthews@gmail.com>
2022-08-19 16:51:31 +0800

    Update and rename rubick2.rb to rubick.rb

--- Casks/rubick.rb
+++ Casks/rubick.rb
@@ -1,14 +1,14 @@
-cask "rubick2" do
+cask "rubick" do
   version "2.0.4"
   sha256 "aa0a705366f2d176d281dac1f36545355724c22e899e91eb22d006ae685ee8d2"
 
-  url "https://github.com/rubickCenter/rubick/releases/download/v#{version}/rubick2-#{version}.dmg",
+  url "https://github.com/rubickCenter/rubick/releases/download/v#{version}/rubick#{version.major}-#{version}.dmg",
       verified: "github.com/rubickCenter/rubick/"
-  name "rubick2"
-  desc "🔧 Electron based open source toolbox, free integration of rich plug-ins"
+  name "Rubick"
+  desc "Electron-based plug-in toolbox"
   homepage "https://rubickcenter.github.io/rubick/"
 
-  app "rubick2.app"
+  app "rubick#{version.major}.app"
 
   zap trash: [
     "~/rubick",
