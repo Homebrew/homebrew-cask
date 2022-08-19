1f22f9580eb9375de792675727508dd8d0bbd5c1
Patrick Linnane <patrick@linnane.io>
2022-08-19 09:40:25 -0700

    Add livecheck

--- Casks/yandex-music-unofficial.rb
+++ Casks/yandex-music-unofficial.rb
@@ -16,6 +16,11 @@ cask "yandex-music-unofficial" do
   desc "Unofficial app for Yandex Music"
   homepage "https://yandex-music.juvs.dev/"
 
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
   auto_updates true
 
   app "Yandex Music Unofficial.app"
