1ba9b2bf4ddd0763c00645ae101fd1793e81a208
Daniel Bayley <daniel.bayley@me.com>
2022-08-19 10:01:42 +0100

    Add Decentr 1.3.2 (#129870)
    * Add Decentr 1.3.2
    
    * Update decentr.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/decentr.rb
@@ -0,0 +1,31 @@
+cask "decentr" do
+  arch arm: "M1", intel: "x64"
+
+  version "1.3.2"
+
+  on_intel do
+    sha256 "9429047b44478fcc6bc48ac5bca73f9064a992e3b07c6ad33a8868f5cfa6d375"
+  end
+  on_arm do
+    sha256 "a15e91def4242b950b23f46dd2d0a18e2348c5f0c7e030b82e4acdcb301c1511"
+  end
+
+  url "https://decentr.net/files/MacOS_#{arch}_Decentr_#{version}.zip"
+  name "Decentr"
+  desc "Web3 blockchain/metaverse browser"
+  homepage "https://decentr.net/"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?MacOS[._-]#{arch}[._-]Decentr[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+  end
+
+  app "Decentr.app"
+
+  zap trash: [
+    "~/Library/Application Support/Decentr",
+    "~/Library/Caches/Decentr",
+    "~/Library/Preferences/com.Decentr.Decentr.plist",
+    "~/Library/Saved Application State/com.Decentr.Decentr.savedState",
+  ]
+end
