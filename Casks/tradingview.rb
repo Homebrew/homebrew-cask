7e3c8371ea3533e4e952c0245a946db81d557d66
Patrick Linnane <patrick@linnane.io>
2022-08-19 09:10:13 -0700

    tradingview 1.0.15
    Update tradingview from 1.0.14 to 1.0.15
    
    Closes #130096.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/tradingview.rb
+++ Casks/tradingview.rb
@@ -1,6 +1,6 @@
 cask "tradingview" do
-  version "1.0.14"
-  sha256 "a1a9fb32717608072fac57ed616ebce3ef26cafb04ecf3b474a7a53242f280e8"
+  version "1.0.15"
+  sha256 "17bd9ec7ad5ffa9a12a7905ecea5d55d0497d97cdaf98a20ba2f383f045bea75"
 
   url "https://tvd-packages.tradingview.com/stable/#{version}/darwin/TradingView.dmg"
   name "TradingView Desktop"
