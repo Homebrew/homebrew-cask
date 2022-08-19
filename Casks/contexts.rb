a21ccccca95570c86b3ac63ade41a3937967a11d
Luís Portela Afonso <lcportellaa@gmail.com>
2022-08-19 11:14:17 +0100

    Update contexts from 3.8.1 to 3.9.0

--- Casks/contexts.rb
+++ Casks/contexts.rb
@@ -3,8 +3,8 @@ cask "contexts" do
     version "3.7.1,371"
     sha256 "de5e4a660cc30276155606b539d1ae58684115a3983d69598f1505fcad499a87"
   else
-    version "3.8.1,381"
-    sha256 "f4ca05c6abb14f6193b158645d74ca9d2672aabba7e07cc1c6cc3af18aa44907"
+    version "3.9.0"
+    sha256 "c86a214d5ee7718453d0fb6442b3ef9a7fc02c5c1582b6d28ef0c2e621a6b6aa"
   end
 
   url "https://contexts.co/releases/Contexts-#{version.csv.first}.dmg"
