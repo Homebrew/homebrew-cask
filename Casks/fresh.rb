7bfe5aa9cc6f355ccb099c56d546f461739cbfaf
Scott Willeke <scott@willeke.com>
2022-08-19 12:26:57 -0700

    fresh: add Ironic Software Fresh.app v4.0.4

---
+++ Casks/fresh.rb
@@ -0,0 +1,11 @@
+cask "fresh" do
+  version "4.0.4"
+  sha256 :no_check
+
+  url "https://ironicsoftware.com/downloads/Fresh.zip"
+  name "Ironic Software Fresh"
+  desc "Keep your recently modified files at hand and up-to-date"
+  homepage "https://ironicsoftware.com/fresh/"
+
+  app "Fresh.app"
+end
