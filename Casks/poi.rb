ad03be8b3996e0bf7a970158200ad0a8afbe67b4
Patrick Linnane <patrick@linnane.io>
2022-08-19 11:42:27 -0700

    Update poi from 10.9.0 to 10.9.1

--- Casks/poi.rb
+++ Casks/poi.rb
@@ -1,6 +1,6 @@
 cask "poi" do
-  version "10.9.0"
-  sha256 "c6deb80f7fe7dc6e34ed21a01d11be38c54affd043d7e73677aa5c4dd15f0ed7"
+  version "10.9.1"
+  sha256 "5eff9c247998274c8aff80cec4935794c9080ea1d31a90d26d12d2284cd35dfb"
 
   url "https://github.com/poooi/poi/releases/download/v#{version}/poi-#{version}.dmg",
       verified: "github.com/poooi/poi/"
