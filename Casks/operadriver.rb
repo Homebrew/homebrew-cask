182d65deb5b09cd5bf1eedf004fa0a2f24e49465
Patrick Linnane <patrick@linnane.io>
2022-08-19 08:41:44 -0700

    Update operadriver from 103.0.5060.66 to 104.0.5112.81

--- Casks/operadriver.rb
+++ Casks/operadriver.rb
@@ -1,6 +1,6 @@
 cask "operadriver" do
-  version "103.0.5060.66"
-  sha256 "1f6c9c52837bf8fc743f995e006ba54c5a365cbd77a5668ab997e2733cfb9cc3"
+  version "104.0.5112.81"
+  sha256 "222f813f2a440729e4058dba98812460c9c20d6f0037b8b415c0d9601f516bec"
 
   url "https://github.com/operasoftware/operachromiumdriver/releases/download/v.#{version}/operadriver_mac64.zip"
   name "OperaChromiumDriver"
