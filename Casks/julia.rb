064a1956f94ee3c60a7897a50c6352a965874db7
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-19 18:25:06 +0800

    julia: remove `midfix` (#130045)
    * julia: remove `midfix`
    
    This is just confusing, and we don't need it.
    
    * Update julia.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/julia.rb
+++ Casks/julia.rb
@@ -1,6 +1,5 @@
 cask "julia" do
   arch arm: "aarch64", intel: "x64"
-  midfix = on_arch_conditional arm: "aarch"
 
   version "1.8.0"
 
@@ -11,14 +10,14 @@ cask "julia" do
     sha256 "8e113a42fe4eac7b75023136674452f6f09590717755417cd137258707e96540"
   end
 
-  url "https://julialang-s3.julialang.org/bin/mac/#{arch}/#{version.major_minor}/julia-#{version}-mac#{midfix}64.dmg"
+  url "https://julialang-s3.julialang.org/bin/mac/#{arch}/#{version.major_minor}/julia-#{version}-mac#{arch.delete_prefix("x")}.dmg"
   name "Julia"
   desc "Programming language for technical computing"
   homepage "https://julialang.org/"
 
   livecheck do
     url "https://julialang.org/downloads/"
-    regex(/href=.*?julia[._-]v?(\d+(?:\.\d+)+)[._-]mac#{midfix}64\.dmg/i)
+    regex(/href=.*?julia[._-]v?(\d+(?:\.\d+)+)[._-]mac#{arch.delete_prefix("x")}\.dmg/i)
   end
 
   app "Julia-#{version.major_minor}.app"
