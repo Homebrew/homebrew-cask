d2da3c1a456266c93c6a113e8e19cb387a36de5f
Aaron Jheng <wentworth@outlook.com>
2022-08-20 16:58:40 +0800

    hackolade: add arm build (#130140)

--- Casks/hackolade.rb
+++ Casks/hackolade.rb
@@ -1,8 +1,16 @@
 cask "hackolade" do
-  version "6.4.1"
-  sha256 :no_check
+  arch arm: "ARM64", intel: ""
 
-  url "https://s3-eu-west-1.amazonaws.com/hackolade/current/Hackolade-mac-setup-signed.pkg",
+  version "6.4.1"
+
+  on_intel do
+    sha256 "df11bee9e746cf00c57bebdeba016c9dc660fee7bcd56ae4d68a9d8ee61d0210"
+  end
+  on_arm do
+    sha256 "cb8a87760e10e3e38777a59c68305b5be2ff453a529080eb95372f4b17fcf08b"
+  end
+
+  url "https://s3-eu-west-1.amazonaws.com/hackolade/previous/v#{version}/Hackolade-mac#{arch}-setup-signed.pkg",
       verified: "s3-eu-west-1.amazonaws.com/hackolade/"
   name "Hackolade"
   desc "Data Modeling for NoSQL databases, storage formats, REST APIs, and JSON in RDBMS"
@@ -13,7 +21,7 @@ cask "hackolade" do
     regex(/Current\sversion:\sv?(\d+(?:\.\d+)+)/i)
   end
 
-  pkg "Hackolade-mac-setup-signed.pkg"
+  pkg "Hackolade-mac#{arch}-setup-signed.pkg"
 
   uninstall pkgutil: "com.hackolade.pkg.Hackolade"
 
