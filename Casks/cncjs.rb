682a32a77cdb79295fc79df3ba66cf70169a1c86
Patrick Linnane <patrick@linnane.io>
2022-08-19 20:42:00 -0700

    Update cncjs from 1.9.26 to 1.9.27

--- Casks/cncjs.rb
+++ Casks/cncjs.rb
@@ -1,6 +1,6 @@
 cask "cncjs" do
-  version "1.9.26"
-  sha256 "65978898ce35c5a71c9c4999addf7db6b769022f177d53457ecd6ca032b07798"
+  version "1.9.27"
+  sha256 "3e112f318498c5b9fdc160e21a31c2322e89db947585e36bf8c7434009e90a63"
 
   url "https://github.com/cncjs/cncjs/releases/download/v#{version}/cncjs-app-#{version}-macos.x64.dmg",
       verified: "github.com/cncjs/cncjs/"
