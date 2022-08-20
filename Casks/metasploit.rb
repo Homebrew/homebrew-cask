e7b7539c54e87e878e5b6f00a5ac351280b4412d
Patrick Linnane <patrick@linnane.io>
2022-08-20 10:18:34 -0700

    Update metasploit from 6.2.14,20220819103116 to 6.2.14,20220820103024 (#130142)

--- Casks/metasploit.rb
+++ Casks/metasploit.rb
@@ -1,6 +1,6 @@
 cask "metasploit" do
-  version "6.2.14,20220819103116"
-  sha256 "b11bcbf3595990027a4e40dad29528c03164dbbe8f75822ca820f14eb5ecaa47"
+  version "6.2.14,20220820103024"
+  sha256 "2d2e9b5d50223f397a932ebfa57ce638e39c24a66e48942c759f6fd053cdc411"
 
   url "https://osx.metasploit.com/metasploit-framework-#{version.csv.first}%2B#{version.csv.second}-1rapid7-1.x86_64.pkg"
   name "Metasploit Framework"
