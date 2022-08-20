4332589d1b72f903e162168c9c5d950084dba3d4
Patrick Linnane <patrick@linnane.io>
2022-08-20 11:57:23 -0700

    Update itsycal from 0.13.2 to 0.13.3

--- Casks/itsycal.rb
+++ Casks/itsycal.rb
@@ -6,8 +6,8 @@ cask "itsycal" do
     version "0.11.17"
     sha256 "fda1ba5611deaf4d5b834118b3af37ea9c5d08d1f8c813d04e7dd0552a270e11"
   else
-    version "0.13.2,2203"
-    sha256 "4dc971e5275e5eda8d4c48c5c7cbab3483c5c64ef2873f162aad002524c69c9c"
+    version "0.13.3,2219"
+    sha256 "2cdc6d47a6dcd6231b0911d6ac2305d186a9e78990b2cc55e523d0852778dcec"
   end
 
   url "https://itsycal.s3.amazonaws.com/Itsycal-#{version.csv.first}.zip",
