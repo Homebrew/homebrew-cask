82996e55dad85863b43d8e40ae5dc959deadcf87
Patrick Linnane <patrick@linnane.io>
2022-08-22 12:37:21 -0700

    Update ngrok from 3.0.6 to 3.0.7

--- Casks/ngrok.rb
+++ Casks/ngrok.rb
@@ -6,8 +6,8 @@ cask "ngrok" do
     sha256 "26695c41b93e6437b797fcc0f6e78242ec8c665e8e57f436aa6305b9237e3ac1"
   end
   on_arm do
-    version "3.0.6,91H3cQoKGUw,a"
-    sha256 "f17dd2755abc3db298bfac5279c0d633d08fc96e69708a332d9c65b57328a9f6"
+    version "3.0.7,3qk13uQpAwh,a"
+    sha256 "c0294214fab0ef18111b2e00c2f76c0c90ec291f0fca43cf46a6745b6d9765ec"
   end
 
   url "https://bin.equinox.io/#{version.csv.third}/#{version.csv.second}/ngrok-v#{version.major}-#{version.csv.first}-stable-darwin-#{arch}.zip",
