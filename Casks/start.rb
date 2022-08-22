d00827f1a1254d458addd7d93942c10cd8f31c36
Patrick Linnane <patrick@linnane.io>
2022-08-21 21:35:45 -0700

    start 0.301.10076-5
    Update start from 0.301.29-77 to 0.301.10076-3
    
    Closes #130192.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/start.rb
+++ Casks/start.rb
@@ -1,17 +1,18 @@
 cask "start" do
-  arch arm: "m1/"
+  arch arm: "arm", intel: "x86"
+  folder_arch = on_arch_conditional arm: "m1/"
   livecheck_arch = on_arch_conditional arm: "-arm"
 
   on_intel do
-    version "0.301.29-193"
-    sha256 "fcbfb60dd562ef3b1c5c0198f987b0dbdd95b287b9019b3a63e1a671cb25d313"
+    version "0.301.10076-5"
+    sha256 "fa5a1e08ff26fdc6c6253b0547297c98435c13c6c17f73ca1e025f9dcd37aa33"
   end
   on_arm do
-    version "0.301.29-77"
-    sha256 "097d7b55bf13761f8d0142a5a486077205bbcb49dd65e55ba6ed667095d26a2c"
+    version "0.301.10076-3"
+    sha256 "040c0a944372a102f10fae078baa1d6e941feb3edf2371d70cd9fa375f384096"
   end
 
-  url "https://imgcdn.start.qq.com/cdn/mac.client/installer/#{arch}START-Installer-#{version}.dmg"
+  url "https://imgcdn.start.qq.com/cdn/mac.client/installer/#{folder_arch}START-Installer-#{arch}-#{version}.dmg"
   name "START"
   name "腾讯云游戏"
   desc "Tencent cloud gaming platform"
@@ -19,7 +20,7 @@ cask "start" do
 
   livecheck do
     url "https://api.start.qq.com/cfg/get?biztypes=macos-update-info#{livecheck_arch}"
-    regex(/START-Installer[._-]v?(\d+(?:[.-]\d+)+)\.dmg/i)
+    regex(/START-Installer[._-]#{arch}[._-]v?(\d+(?:[.-]\d+)+)\.dmg/i)
   end
 
   auto_updates true
