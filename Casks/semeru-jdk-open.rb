d85fe5ea8d24d5d870b8c19cc9980b0d1de16d1b
Patrick Linnane <patrick@linnane.io>
2022-08-22 13:07:18 -0700

    Add arm64, bump to 18.0.2+9,openj9-0.33.1

--- Casks/semeru-jdk-open.rb
+++ Casks/semeru-jdk-open.rb
@@ -1,8 +1,16 @@
 cask "semeru-jdk-open" do
-  version "18.0.2+9,openj9-0.33.0"
-  sha256 "96b3b66ef8dacfe555f6fc5b4003d1d225a2f7aaa7bb420a07fce24f87fa9536"
+  arch arm: "aarch64", intel: "x64"
 
-  url "https://github.com/ibmruntimes/semeru#{version.major}-binaries/releases/download/jdk-#{version.csv.first}_#{version.csv.second}/ibm-semeru-open-jdk_x64_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg",
+  version "18.0.2+9,openj9-0.33.1"
+
+  on_intel do
+    sha256 "9cc0bd4f66b68631ff43fed90fd05867bf3c69ab3d78daa6de3f77f6d6f99645"
+  end
+  on_arm do
+    sha256 "b6dd0bca76fb3d606fdc2ad1dc75612354c4c515262e8450c326944a41be11d6"
+  end
+
+  url "https://github.com/ibmruntimes/semeru#{version.major}-binaries/releases/download/jdk-#{version.csv.first}_#{version.csv.second}/ibm-semeru-open-jdk_#{arch}_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg",
       verified: "github.com/ibmruntimes/semeru#{version.major}-binaries/"
   name "IBM Semeru Runtime (JDK) Open Edition"
   desc "Production-ready JDK with the OpenJDK class libraries and the Eclipse OpenJ9 JVM"
@@ -18,7 +26,7 @@ cask "semeru-jdk-open" do
     end
   end
 
-  pkg "ibm-semeru-open-jdk_x64_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg"
+  pkg "ibm-semeru-open-jdk_#{arch}_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg"
 
   uninstall pkgutil: "net.ibm-semeru-open.#{version.major}.jdk"
 end
