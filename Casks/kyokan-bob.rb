3fb495db5205ff999899a3424f25426b31099afc
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:34 -0400

    kyokan-bob: use `sha256 arm: "...", intel: "..."`

--- Casks/kyokan-bob.rb
+++ Casks/kyokan-bob.rb
@@ -2,13 +2,8 @@ cask "kyokan-bob" do
   arch arm: "arm64", intel: "x86"
 
   version "1.0.0"
-
-  on_intel do
-    sha256 "f4e962e38d8679a5abe7baf181d12ea679bd90876fe991a4c3c01b21f10195c8"
-  end
-  on_arm do
-    sha256 "03c913e5ea5d8779122b4da3a9ef0d63307ce84a7a86e84feae53af82bb6fd36"
-  end
+  sha256 arm:   "03c913e5ea5d8779122b4da3a9ef0d63307ce84a7a86e84feae53af82bb6fd36",
+         intel: "f4e962e38d8679a5abe7baf181d12ea679bd90876fe991a4c3c01b21f10195c8"
 
   url "https://github.com/kyokan/bob-wallet/releases/download/v#{version}/Bob-#{version}-#{arch}.dmg",
       verified: "github.com/kyokan/bob-wallet/"
