e580381161ea7256ea4def6062865b220bb19802
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:45 -0400

    superproductivity: use `sha256 arm: "...", intel: "..."`

--- Casks/superproductivity.rb
+++ Casks/superproductivity.rb
@@ -2,13 +2,8 @@ cask "superproductivity" do
   arch arm: "-arm64"
 
   version "7.11.5"
-
-  on_intel do
-    sha256 "5c6b50db6109939ad1f902c4d2123aae81bce0299095ebc31249e9e6ca452a6f"
-  end
-  on_arm do
-    sha256 "67665f0d3786c6d3be8705af67f1ffcfc987b38f61415f07ea940b1e95e8dd47"
-  end
+  sha256 arm:   "67665f0d3786c6d3be8705af67f1ffcfc987b38f61415f07ea940b1e95e8dd47",
+         intel: "5c6b50db6109939ad1f902c4d2123aae81bce0299095ebc31249e9e6ca452a6f"
 
   url "https://github.com/johannesjo/super-productivity/releases/download/v#{version}/superProductivity-#{version}#{arch}.dmg",
       verified: "github.com/johannesjo/super-productivity/"
