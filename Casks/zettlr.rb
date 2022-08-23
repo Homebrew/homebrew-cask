56c44a16c226787f042cd769f6a463db5dceb701
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:50 -0400

    zettlr: use `sha256 arm: "...", intel: "..."`

--- Casks/zettlr.rb
+++ Casks/zettlr.rb
@@ -2,13 +2,8 @@ cask "zettlr" do
   arch arm: "arm64", intel: "x64"
 
   version "2.3.0"
-
-  on_intel do
-    sha256 "73ee8fbcf481b1cf68a3456f3c215be8a7d8835342220cc44fba6fa0440f37fc"
-  end
-  on_arm do
-    sha256 "0236d873d0d1477e480334fe8e68b795a8a7eb61cdbbf72d300143773e98955d"
-  end
+  sha256 arm:   "0236d873d0d1477e480334fe8e68b795a8a7eb61cdbbf72d300143773e98955d",
+         intel: "73ee8fbcf481b1cf68a3456f3c215be8a7d8835342220cc44fba6fa0440f37fc"
 
   url "https://github.com/Zettlr/Zettlr/releases/download/v#{version}/Zettlr-#{version}-#{arch}.dmg"
   name "Zettlr"
