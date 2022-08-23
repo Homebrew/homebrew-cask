0acf3adb3d5c3bf2a17ceacf0eddaed1d8a4d1b8
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:31 -0400

    headlamp: use `sha256 arm: "...", intel: "..."`

--- Casks/headlamp.rb
+++ Casks/headlamp.rb
@@ -2,13 +2,8 @@ cask "headlamp" do
   arch arm: "arm64", intel: "x64"
 
   version "0.12.0"
-
-  on_intel do
-    sha256 "b94592a566b8bd297d8221bd34a57a6add83d1a6fa5f27fb2bfe2ff688803c3d"
-  end
-  on_arm do
-    sha256 "b4d11c4b2ed8fc9f273dfe487f4ceb51dd743fc2408e6c7150f7b43d8f692de1"
-  end
+  sha256 arm:   "b4d11c4b2ed8fc9f273dfe487f4ceb51dd743fc2408e6c7150f7b43d8f692de1",
+         intel: "b94592a566b8bd297d8221bd34a57a6add83d1a6fa5f27fb2bfe2ff688803c3d"
 
   url "https://github.com/kinvolk/headlamp/releases/download/v#{version}/Headlamp-#{version}-mac-#{arch}.dmg",
       verified: "github.com/kinvolk/headlamp/"
