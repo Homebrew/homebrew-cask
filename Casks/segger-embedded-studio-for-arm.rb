11ed2d8747ec556aa52397d49408fb859e7387e8
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:43 -0400

    segger-embedded-studio-for-arm: use `sha256 arm: "...", intel: "..."`

--- Casks/segger-embedded-studio-for-arm.rb
+++ Casks/segger-embedded-studio-for-arm.rb
@@ -2,15 +2,10 @@ cask "segger-embedded-studio-for-arm" do
   arch arm: "arm64", intel: "x64"
 
   version "6.34"
+  sha256 arm:   "4a5ab3761dca0c75cfe902253a5c93ce8b1b32161b386593319a812cf7efacff",
+         intel: "4c0b2ed14f46c5df4f560b1f8d0411d4070cd965cbe02801f55c7f93686c574e"
 
   url "https://www.segger.com/downloads/embedded-studio/Setup_EmbeddedStudio_ARM_v#{version.no_dots}_macos_#{arch}.dmg"
-  on_intel do
-    sha256 "4c0b2ed14f46c5df4f560b1f8d0411d4070cd965cbe02801f55c7f93686c574e"
-  end
-  on_arm do
-    sha256 "4a5ab3761dca0c75cfe902253a5c93ce8b1b32161b386593319a812cf7efacff"
-  end
-
   name "SEGGER Embedded Studio for ARM"
   desc "IDE for embedded systems"
   homepage "https://www.segger.com/products/development-tools/embedded-studio"
