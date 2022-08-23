ad812fe55cd2ae2bd6a776f7f426a4c6e1fff216
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:26 -0400

    decrediton: use `sha256 arm: "...", intel: "..."`

--- Casks/decrediton.rb
+++ Casks/decrediton.rb
@@ -2,13 +2,8 @@ cask "decrediton" do
   arch arm: "arm64", intel: "amd64"
 
   version "1.7.3"
-
-  on_intel do
-    sha256 "106a0d417163c9c247b5a658e7dda691290af8b9651dcecb99f83cfcda5683a8"
-  end
-  on_arm do
-    sha256 "b29286210fe17fe159b5ccb24f0be572da9235acc6b9093356091d52a218848d"
-  end
+  sha256 arm:   "b29286210fe17fe159b5ccb24f0be572da9235acc6b9093356091d52a218848d",
+         intel: "106a0d417163c9c247b5a658e7dda691290af8b9651dcecb99f83cfcda5683a8"
 
   url "https://github.com/decred/decred-binaries/releases/download/v#{version}/decrediton-#{arch}-v#{version}.dmg"
   name "Decrediton"
