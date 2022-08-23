c3416bb99a6c7bbc9b8e10f3efd2f93aaf40cb9b
Dan <dan@wadoku.de>
2022-08-24 01:00:11 +0200

    Create rpcs3.rb

---
+++ Casks/rpcs3.rb
@@ -0,0 +1,29 @@
+cask "rpcs3" do
+  version "0.0.24-14135,60fc90bb8e8c04629521094a9c8fb2cad9955829,60fc90bb"
+  sha256 "1a7382910064c9a204b5f2d7f22b17f3222f7b0d7fd82991c3a287ca3ce3f5fb"
+
+  url "https://github.com/RPCS3/rpcs3-binaries-mac/releases/download/build-#{version.csv.second}/rpcs3-v#{version.csv.first}-#{version.csv.third}_macos.dmg",
+      verified: "github.com/RPCS3/rpcs3-binaries-mac"
+  name "RPCS3"
+  desc "PlayStation 3 emulator and debugger"
+  homepage "https://rpcs3.net/"
+
+  livecheck do
+    url "https://rpcs3.net/download"
+    regex(%r{href=.*/rpcs3-binaries-mac/releases/download/build-(.*)/rpcs3-v(\d+\.\d+\.\d+-\d+)-(.*)_macos.dmg}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map do |match|
+        "#{match[1]},#{match[0]},#{match[2]}"
+      end
+    end
+  end
+
+  app "RPCS3.app"
+
+  zap trash: [
+    "~/Library/Application Support/rpcs3",
+    "~/Library/Caches/rpcs3",
+    "~/Library/Preferences/net.rpcs3.rpcs3.plist",
+    "~/Library/Saved Application State/net.rpcs3.rpcs3.savedState",
+  ]
+end
