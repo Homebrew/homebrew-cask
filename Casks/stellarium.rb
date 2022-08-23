1dba7869af76151c4a2adbc885e871e28d20e861
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:44 -0400

    stellarium: use `sha256 arm: "...", intel: "..."`

--- Casks/stellarium.rb
+++ Casks/stellarium.rb
@@ -2,13 +2,8 @@ cask "stellarium" do
   arch arm: "arm64", intel: "x86_64"
 
   version "0.22.2"
-
-  on_intel do
-    sha256 "5e8c2ee315f8c00a393e7bd22461f9b48355538cec39e1bb771e92a5795bcfb4"
-  end
-  on_arm do
-    sha256 "74acc44f96597d11d92f94015efd19cd18c2ce76e850d90f44c40d636f72ea5f"
-  end
+  sha256 arm:   "74acc44f96597d11d92f94015efd19cd18c2ce76e850d90f44c40d636f72ea5f",
+         intel: "5e8c2ee315f8c00a393e7bd22461f9b48355538cec39e1bb771e92a5795bcfb4"
 
   url "https://github.com/Stellarium/stellarium/releases/download/v#{version.major_minor_patch}/Stellarium-#{version}-#{arch}.zip",
       verified: "github.com/Stellarium/stellarium/"
