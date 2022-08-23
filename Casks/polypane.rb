4c0136660885aaccf80e941dd8f7b980e10e378e
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:39 -0400

    polypane: use `sha256 arm: "...", intel: "..."`

--- Casks/polypane.rb
+++ Casks/polypane.rb
@@ -2,13 +2,8 @@ cask "polypane" do
   arch arm: "-arm64"
 
   version "10.0.1"
-
-  on_intel do
-    sha256 "303929e6821f4d5c124c10c257104c5fd90fd4a6cbd13c68a288dee4dcc1fd00"
-  end
-  on_arm do
-    sha256 "bf3933c7461d952e19fab0e444712c24567abc64680092bff108993c15a8a205"
-  end
+  sha256 arm:   "bf3933c7461d952e19fab0e444712c24567abc64680092bff108993c15a8a205",
+         intel: "303929e6821f4d5c124c10c257104c5fd90fd4a6cbd13c68a288dee4dcc1fd00"
 
   url "https://github.com/firstversionist/polypane/releases/download/v#{version}/Polypane-#{version}#{arch}.dmg",
       verified: "github.com/firstversionist/polypane/"
