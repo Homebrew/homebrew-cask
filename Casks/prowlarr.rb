8fbb3be740ceb38b50d3a3a213a93986e4b6b212
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:40 -0400

    prowlarr: use `sha256 arm: "...", intel: "..."`

--- Casks/prowlarr.rb
+++ Casks/prowlarr.rb
@@ -2,13 +2,8 @@ cask "prowlarr" do
   arch arm: "arm64", intel: "x64"
 
   version "0.4.4.1947"
-
-  on_intel do
-    sha256 "2ed10f8e52d2541decf293f690ace57c162fc55aac6395218884e2e68eb4694d"
-  end
-  on_arm do
-    sha256 "9b6da817a4bc94452177ecdeb08e3d9624a82db2a138451ce1e4cac9c5a974b0"
-  end
+  sha256 arm:   "9b6da817a4bc94452177ecdeb08e3d9624a82db2a138451ce1e4cac9c5a974b0",
+         intel: "2ed10f8e52d2541decf293f690ace57c162fc55aac6395218884e2e68eb4694d"
 
   url "https://github.com/Prowlarr/Prowlarr/releases/download/v#{version}/Prowlarr.develop.#{version}.osx-app-core-#{arch}.zip",
       verified: "github.com/Prowlarr/Prowlarr/"
