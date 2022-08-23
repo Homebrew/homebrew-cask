7c355677431bfd7fdc4e22393235afb723cd27d4
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:37 -0400

    molotov: use `sha256 arm: "...", intel: "..."`

--- Casks/molotov.rb
+++ Casks/molotov.rb
@@ -3,13 +3,8 @@ cask "molotov" do
   arch_folder = on_arch_conditional arm: "m1/"
 
   version "4.5.0"
-
-  on_intel do
-    sha256 "9366230de159a4b5dffe29e1823b6e3bc0d6e6fd2ddc2c4f1b9e9764319cf995"
-  end
-  on_arm do
-    sha256 "4b5303345be7bfcf4f90440424c52f5fd06d912c500969d576cef4eba159d82d"
-  end
+  sha256 arm:   "4b5303345be7bfcf4f90440424c52f5fd06d912c500969d576cef4eba159d82d",
+         intel: "9366230de159a4b5dffe29e1823b6e3bc0d6e6fd2ddc2c4f1b9e9764319cf995"
 
   url "https://desktop-auto-upgrade.molotov.tv/mac/#{arch_folder}Molotov-#{version}#{arch}-mac.zip"
   name "Molotov"
