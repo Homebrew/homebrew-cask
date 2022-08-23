5bf692a1b6340a6bafdfe6e9ae6f99f935a96afb
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:48 -0400

    webull: use `sha256 arm: "...", intel: "..."`

--- Casks/webull.rb
+++ Casks/webull.rb
@@ -3,13 +3,8 @@ cask "webull" do
   livecheck_arch = on_arch_conditional arm: "qt_m1_global", intel: "qt_mac_global"
 
   version "6.2.5"
-
-  on_intel do
-    sha256 "5a6296a6c6e2f40f6d104ee3ef66dc3ae1f86ac59a76188d5dcfa13cebff3c12"
-  end
-  on_arm do
-    sha256 "47de9ca9e844212861dc6a31df00b189b6b05d782152993835cef40900e88994"
-  end
+  sha256 arm:   "47de9ca9e844212861dc6a31df00b189b6b05d782152993835cef40900e88994",
+         intel: "5a6296a6c6e2f40f6d104ee3ef66dc3ae1f86ac59a76188d5dcfa13cebff3c12"
 
   url "https://u1sweb.webullfintech.com/us/desktop/Webull%20Desktop_#{version}_#{arch}signed.dmg",
       verified: "u1sweb.webullfintech.com/us/desktop/"
