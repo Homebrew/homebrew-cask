4eb7723e9032ed37f3ca70331090b988c66a9f0f
Rylan Polster <rslpolster@gmail.com>
2022-08-23 13:56:37 -0400

    r: use arch and macOS version DSLs

--- Casks/r.rb
+++ Casks/r.rb
@@ -1,16 +1,18 @@
 cask "r" do
-  if MacOS.version <= :sierra
+  arch arm: "-arm64"
+  folder = on_arch_conditional arm: "big-sur-arm64/base/", intel: "base/"
+
+  version "4.2.1"
+  sha256 arm:   "9a746270ef98bf038e3cbbc6e962fd3b7081786c49c59d37398261749ba5c693",
+         intel: "972d1e514baa873e4cda238f18163230fcd44d507f4b89d98cf5409e08b16bf9"
+
+  url "https://cloud.r-project.org/bin/macosx/#{folder}R-#{version}#{arch}.pkg"
+
+  on_sierra :or_older do
     version "3.6.3.nn"
     sha256 "f2b771e94915af0fe0a6f042bc7a04ebc84fb80cb01aad5b7b0341c4636336dd"
+
     url "https://cloud.r-project.org/bin/macosx/R-#{version}.pkg"
-  elsif Hardware::CPU.intel?
-    version "4.2.1"
-    sha256 "972d1e514baa873e4cda238f18163230fcd44d507f4b89d98cf5409e08b16bf9"
-    url "https://cloud.r-project.org/bin/macosx/base/R-#{version}.pkg"
-  else
-    version "4.2.1"
-    sha256 "9a746270ef98bf038e3cbbc6e962fd3b7081786c49c59d37398261749ba5c693"
-    url "https://cloud.r-project.org/bin/macosx/big-sur-arm64/base/R-#{version}-arm64.pkg"
   end
 
   name "R"
@@ -24,11 +26,7 @@ cask "r" do
 
   depends_on macos: ">= :el_capitan"
 
-  if Hardware::CPU.intel?
-    pkg "R-#{version}.pkg"
-  else
-    pkg "R-#{version}-arm64.pkg"
-  end
+  pkg "R-#{version}#{arch}.pkg"
 
   uninstall pkgutil: [
               "org.r-project*",
