ee99cbb6b83c2889074a51f6d921e72d545ed785
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:37 -0400

    onlyoffice: use `sha256 arm: "...", intel: "..."`

--- Casks/onlyoffice.rb
+++ Casks/onlyoffice.rb
@@ -2,13 +2,8 @@ cask "onlyoffice" do
   arch arm: "arm", intel: "x86_64"
 
   version "7.1.1"
-
-  on_intel do
-    sha256 "f0384110b6ebafb046c6df0d730edc5c6687564c760f84bb9e812a7f836c3abc"
-  end
-  on_arm do
-    sha256 "0d17c802acdb2bc7aa9af7bec05fdae1db6691a9df72e948ce010077c993b4b0"
-  end
+  sha256 arm:   "0d17c802acdb2bc7aa9af7bec05fdae1db6691a9df72e948ce010077c993b4b0",
+         intel: "f0384110b6ebafb046c6df0d730edc5c6687564c760f84bb9e812a7f836c3abc"
 
   url "https://github.com/ONLYOFFICE/DesktopEditors/releases/download/v#{version}/ONLYOFFICE-#{arch}.dmg",
       verified: "github.com/ONLYOFFICE/DesktopEditors/"
