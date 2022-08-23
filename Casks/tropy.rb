3668a22fa73f75419810b131248489c4c04f5fd9
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:46 -0400

    tropy: use `sha256 arm: "...", intel: "..."`

--- Casks/tropy.rb
+++ Casks/tropy.rb
@@ -2,13 +2,8 @@ cask "tropy" do
   arch arm: "-arm64"
 
   version "1.12.0"
-
-  on_intel do
-    sha256 "ef8e62028cd991ed8e668797035286f4e5220190a9d70591fe7e853be90b862d"
-  end
-  on_arm do
-    sha256 "d730a8747596a1a6d0c869cadbdc3bdff5ed371e05b205fad6b397dc5a4b872f"
-  end
+  sha256 arm:   "d730a8747596a1a6d0c869cadbdc3bdff5ed371e05b205fad6b397dc5a4b872f",
+         intel: "ef8e62028cd991ed8e668797035286f4e5220190a9d70591fe7e853be90b862d"
 
   url "https://github.com/tropy/tropy/releases/download/v#{version}/tropy-#{version}#{arch}.dmg",
       verified: "github.com/tropy/tropy/"
