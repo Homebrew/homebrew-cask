ad5b6a11c51c81374bf9f9fa37061ae75be42e87
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:44 -0400

    slite: use `sha256 arm: "...", intel: "..."`

--- Casks/slite.rb
+++ Casks/slite.rb
@@ -2,13 +2,8 @@ cask "slite" do
   arch arm: "arm64", intel: "x64"
 
   version "1.2.14"
-
-  on_intel do
-    sha256 "8103be5d53d349cf5070ec98e74df8d01ded0f93e8c84b9d18b18c19c6210b76"
-  end
-  on_arm do
-    sha256 "a74ad9345c3dba1874ec682280996be21b2c517824879a1f9fc0ee9896c940df"
-  end
+  sha256 arm:   "a74ad9345c3dba1874ec682280996be21b2c517824879a1f9fc0ee9896c940df",
+         intel: "8103be5d53d349cf5070ec98e74df8d01ded0f93e8c84b9d18b18c19c6210b76"
 
   url "https://download.todesktop.com/20062929x31pwfi/Slite%20#{version}-#{arch}-mac.zip",
       verified: "download.todesktop.com/20062929x31pwfi/"
