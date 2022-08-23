bdc99582094d9b10975e5647bc9047eff7385794
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:33 -0400

    jasper: use `sha256 arm: "...", intel: "..."`

--- Casks/jasper.rb
+++ Casks/jasper.rb
@@ -2,13 +2,8 @@ cask "jasper" do
   arch arm: "arm64", intel: "x64"
 
   version "1.1.1"
-
-  on_intel do
-    sha256 "8c30e53b39ab87c94eff5c76c52b7403125efeb33b76e0cc6250c134aaf55fe3"
-  end
-  on_arm do
-    sha256 "860a6d48cfc4542a0060746e10d2bfe06a3da66a0a3aeef028f02454afb3cd66"
-  end
+  sha256 arm:   "860a6d48cfc4542a0060746e10d2bfe06a3da66a0a3aeef028f02454afb3cd66",
+         intel: "8c30e53b39ab87c94eff5c76c52b7403125efeb33b76e0cc6250c134aaf55fe3"
 
   url "https://github.com/jasperapp/jasper/releases/download/v#{version}/jasper_v#{version}_mac_#{arch}.zip",
       verified: "github.com/jasperapp/jasper/"
