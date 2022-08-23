20460807d8693fc2b29e03e03dc5a5548e8fe6e3
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:30 -0400

    gather: use `sha256 arm: "...", intel: "..."`

--- Casks/gather.rb
+++ Casks/gather.rb
@@ -2,13 +2,8 @@ cask "gather" do
   arch arm: "-arm64"
 
   version "0.3.6"
-
-  on_intel do
-    sha256 "8c4b6c82ceb7797db4e1203867d4268b6c093fe69bd5f068a95b7fb5a8b5eff6"
-  end
-  on_arm do
-    sha256 "170743882eb60a66a6cd7fddea3fd106cd86ca9b35bd373175f75bf1e88386df"
-  end
+  sha256 arm:   "170743882eb60a66a6cd7fddea3fd106cd86ca9b35bd373175f75bf1e88386df",
+         intel: "8c4b6c82ceb7797db4e1203867d4268b6c093fe69bd5f068a95b7fb5a8b5eff6"
 
   url "https://github.com/gathertown/gather-town-desktop-releases/releases/download/v#{version}/Gather-#{version}#{arch}-mac.zip",
       verified: "github.com/gathertown/gather-town-desktop-releases"
