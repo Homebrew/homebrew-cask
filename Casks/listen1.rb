8bb735d5f245fad737f694f7a52430e999689c1d
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:35 -0400

    listen1: use `sha256 arm: "...", intel: "..."`

--- Casks/listen1.rb
+++ Casks/listen1.rb
@@ -3,16 +3,11 @@ cask "listen1" do
   arch arm: "arm64", intel: "x64"
 
   version "2.24.0"
+  sha256 arm:   "8fd3e5c832dbdd02cbb9c68edcc902dd6749e67ea62d4d98ff155890c2aee954",
+         intel: "2327464903b24cdd620d1ecbad8e039cd1f6f200f4286b05d6d799e7cc125a29"
 
   url "https://github.com/listen1/listen1_desktop/releases/download/v#{version}/Listen1_#{version}_mac_#{arch}.dmg",
       verified: "github.com/listen1/listen1_desktop/"
-  on_intel do
-    sha256 "2327464903b24cdd620d1ecbad8e039cd1f6f200f4286b05d6d799e7cc125a29"
-  end
-  on_arm do
-    sha256 "8fd3e5c832dbdd02cbb9c68edcc902dd6749e67ea62d4d98ff155890c2aee954"
-  end
-
   name "Listen 1"
   desc "Search and play songs from a variety of online sources"
   homepage "https://listen1.github.io/listen1/"
