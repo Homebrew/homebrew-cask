ecafdd6e6bb267a995c18f078fe3939641094659
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:24 -0400

    clash-for-windows: use `sha256 arm: "...", intel: "..."`

--- Casks/clash-for-windows.rb
+++ Casks/clash-for-windows.rb
@@ -2,13 +2,8 @@ cask "clash-for-windows" do
   arch arm: "-arm64"
 
   version "0.19.27"
-
-  on_intel do
-    sha256 "b70a89a3b24c6c744abf95a0007cddd69b298fe0c2c3cf3750fc5c04f77da131"
-  end
-  on_arm do
-    sha256 "73da9d4331ef69e56713565de7078555302df25c953f62ce45428465c84057c2"
-  end
+  sha256 arm:   "73da9d4331ef69e56713565de7078555302df25c953f62ce45428465c84057c2",
+         intel: "b70a89a3b24c6c744abf95a0007cddd69b298fe0c2c3cf3750fc5c04f77da131"
 
   url "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/#{version}/Clash.for.Windows-#{version}#{arch}.dmg"
   name "Clash for Windows"
