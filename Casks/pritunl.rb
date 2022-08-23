55c357d9901483daf05afce034b219f4f223af5b
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:40 -0400

    pritunl: use `sha256 arm: "...", intel: "..."`

--- Casks/pritunl.rb
+++ Casks/pritunl.rb
@@ -2,13 +2,8 @@ cask "pritunl" do
   arch arm: ".arm64"
 
   version "1.2.3236.80"
-
-  on_intel do
-    sha256 "47281dab7501d10db66249636f29bec2d4c9b2e9f4971f9fc9967b97f20a60d9"
-  end
-  on_arm do
-    sha256 "d26c375da9b16febace3fea6781405086d3e45129256ad638d54fdb688253f29"
-  end
+  sha256 arm:   "d26c375da9b16febace3fea6781405086d3e45129256ad638d54fdb688253f29",
+         intel: "47281dab7501d10db66249636f29bec2d4c9b2e9f4971f9fc9967b97f20a60d9"
 
   url "https://github.com/pritunl/pritunl-client-electron/releases/download/#{version}/Pritunl#{arch}.pkg.zip",
       verified: "github.com/pritunl/pritunl-client-electron/"
