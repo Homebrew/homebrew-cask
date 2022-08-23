d360e58b7f1c2759d90bc4f38a88f06afb4e31d6
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:26 -0400

    dcv-viewer: use `sha256 arm: "...", intel: "..."`

--- Casks/dcv-viewer.rb
+++ Casks/dcv-viewer.rb
@@ -2,13 +2,8 @@ cask "dcv-viewer" do
   arch arm: "arm64", intel: "x86_64"
 
   version "2022.1.4279"
-
-  on_intel do
-    sha256 "036bab4870ffb2f0240f8813910a1f72e3718b4fa552cda8311f20cd8ed25e7d"
-  end
-  on_arm do
-    sha256 "9d9929a66d8e9e7b43e1e455c246f4da465a28945880416efb35e70066fea33a"
-  end
+  sha256 arm:   "9d9929a66d8e9e7b43e1e455c246f4da465a28945880416efb35e70066fea33a",
+         intel: "036bab4870ffb2f0240f8813910a1f72e3718b4fa552cda8311f20cd8ed25e7d"
 
   url "https://d1uj6qtbmh3dt5.cloudfront.net/#{version.major_minor}/Clients/nice-dcv-viewer-#{version}.#{arch}.dmg",
       verified: "d1uj6qtbmh3dt5.cloudfront.net/"
