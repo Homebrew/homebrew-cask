2c7a719f31d4158a16fe74a4117a62e5fb1c0ea3
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:35 -0400

    macintoshjs: use `sha256 arm: "...", intel: "..."`

--- Casks/macintoshjs.rb
+++ Casks/macintoshjs.rb
@@ -2,15 +2,10 @@ cask "macintoshjs" do
   arch arm: "arm64", intel: "x64"
 
   version "1.1.0"
+  sha256 arm:   "0af95bbcc075f939d6c3e1fdcdab0d8da2760e5546aecfbc82767e326640740c",
+         intel: "4ca41517f15c594e718da622074a2160754cb8f1293cb1fad908f6d0ae585384"
 
   url "https://github.com/felixrieseberg/macintosh.js/releases/download/v#{version}/macintosh.js-darwin-#{arch}-#{version}.zip"
-  on_intel do
-    sha256 "4ca41517f15c594e718da622074a2160754cb8f1293cb1fad908f6d0ae585384"
-  end
-  on_arm do
-    sha256 "0af95bbcc075f939d6c3e1fdcdab0d8da2760e5546aecfbc82767e326640740c"
-  end
-
   name "macintosh.js"
   desc "Virtual Apple Macintosh with System 8, running in Electron"
   homepage "https://github.com/felixrieseberg/macintosh.js"
