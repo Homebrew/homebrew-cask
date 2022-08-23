777aab9aae77c50c8ed754891bcf0b6c5cd7e7d2
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:42 -0400

    roam-research: use `sha256 arm: "...", intel: "..."`

--- Casks/roam-research.rb
+++ Casks/roam-research.rb
@@ -2,13 +2,8 @@ cask "roam-research" do
   arch arm: "-arm64"
 
   version "0.0.15"
-
-  on_intel do
-    sha256 "c270b957cb7ba0c9fa7b7ff8ff9a2fbe2ed3c8dba24ee2584dfa487022e1cc51"
-  end
-  on_arm do
-    sha256 "4407df5ac97d3013dbf113d09da33d01264d94f8f4c30d5546afe237e6af5834"
-  end
+  sha256 arm:   "4407df5ac97d3013dbf113d09da33d01264d94f8f4c30d5546afe237e6af5834",
+         intel: "c270b957cb7ba0c9fa7b7ff8ff9a2fbe2ed3c8dba24ee2584dfa487022e1cc51"
 
   url "https://roam-electron-deploy.s3.us-east-2.amazonaws.com/Roam+Research-#{version}#{arch}.dmg",
       verified: "roam-electron-deploy.s3.us-east-2.amazonaws.com"
