045e755750fe376282dd006b78d239a11b18eab1
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:35 -0400

    lx-music: use `sha256 arm: "...", intel: "..."`

--- Casks/lx-music.rb
+++ Casks/lx-music.rb
@@ -2,13 +2,8 @@ cask "lx-music" do
   arch arm: "-arm64"
 
   version "1.22.2"
-
-  on_intel do
-    sha256 "7d44b3c072fb0cb6e6e5949f8087222da0393782cbfebf896872516ab0e96b2a"
-  end
-  on_arm do
-    sha256 "8f30eb1264e300d0bcd543d61b7d84dcfd06c8e372cc7daed1c4645aeba81bc4"
-  end
+  sha256 arm:   "8f30eb1264e300d0bcd543d61b7d84dcfd06c8e372cc7daed1c4645aeba81bc4",
+         intel: "7d44b3c072fb0cb6e6e5949f8087222da0393782cbfebf896872516ab0e96b2a"
 
   url "https://github.com/lyswhut/lx-music-desktop/releases/download/v#{version}/lx-music-desktop-#{version}#{arch}.dmg"
   name "LX Music Assistant Desktop Edition"
