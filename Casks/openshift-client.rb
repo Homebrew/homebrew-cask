fbcb5d17d1cf755f9c0e179192890a5e0d9d98f5
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:38 -0400

    openshift-client: use `sha256 arm: "...", intel: "..."`

--- Casks/openshift-client.rb
+++ Casks/openshift-client.rb
@@ -2,13 +2,8 @@ cask "openshift-client" do
   arch arm: "-arm64"
 
   version "4.11.1"
-
-  on_intel do
-    sha256 "7ddf8dcee068e91f1f67bed64b9a10b178a151ca8ba6b4dc76a06324f2fa51ce"
-  end
-  on_arm do
-    sha256 "157d5f950353f43835ba4c4cc918bc9d90bd61715a370dac3875dfcc916d5f27"
-  end
+  sha256 arm:   "157d5f950353f43835ba4c4cc918bc9d90bd61715a370dac3875dfcc916d5f27",
+         intel: "7ddf8dcee068e91f1f67bed64b9a10b178a151ca8ba6b4dc76a06324f2fa51ce"
 
   url "https://mirror.openshift.com/pub/openshift-v#{version.major}/clients/ocp/#{version}/openshift-client-mac#{arch}.tar.gz"
   name "Openshift Client"
