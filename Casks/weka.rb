e3967e832ac9ee7669209e9a4b7152d528b09d78
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:49 -0400

    weka: use `sha256 arm: "...", intel: "..."`

--- Casks/weka.rb
+++ Casks/weka.rb
@@ -2,13 +2,8 @@ cask "weka" do
   arch arm: "arm-osx", intel: "osx"
 
   version "3.8.6"
-
-  on_intel do
-    sha256 "282d5ff81960d1ae43ee6e4e8eaa7ae8c341666a214e859728e15214af80383b"
-  end
-  on_arm do
-    sha256 "85de453da3bb41c952da48eee0bf574d1da7ee4fa6fd3a8e2d7a81a89d55d3d7"
-  end
+  sha256 arm:   "85de453da3bb41c952da48eee0bf574d1da7ee4fa6fd3a8e2d7a81a89d55d3d7",
+         intel: "282d5ff81960d1ae43ee6e4e8eaa7ae8c341666a214e859728e15214af80383b"
 
   url "https://downloads.sourceforge.net/weka/weka-#{version.dots_to_hyphens}-azul-zulu-#{arch}.dmg",
       verified: "sourceforge.net/weka/"
