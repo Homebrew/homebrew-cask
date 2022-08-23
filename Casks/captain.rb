040eca689b6f6d6473d0830e4de67c634f533b79
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:24 -0400

    captain: use `sha256 arm: "...", intel: "..."`

--- Casks/captain.rb
+++ Casks/captain.rb
@@ -2,13 +2,8 @@ cask "captain" do
   arch arm: "-arm64"
 
   version "10.0.3"
-
-  on_intel do
-    sha256 "32209eaa21e8917470d37456388395d79aefac911bceed044011284fb222d186"
-  end
-  on_arm do
-    sha256 "a0a380f4d38d7330bfea534ed8ecc44ebf48971843549017aa5551bd659fb121"
-  end
+  sha256 arm:   "a0a380f4d38d7330bfea534ed8ecc44ebf48971843549017aa5551bd659fb121",
+         intel: "32209eaa21e8917470d37456388395d79aefac911bceed044011284fb222d186"
 
   url "https://github.com/RickWong/Captain/releases/download/v#{version}/Captain-#{version}#{arch}.dmg",
       verified: "github.com/RickWong/Captain"
