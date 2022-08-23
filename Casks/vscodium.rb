457be832bf2df56b7210ec7477c792c5d73b23c2
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:48 -0400

    vscodium: use `sha256 arm: "...", intel: "..."`

--- Casks/vscodium.rb
+++ Casks/vscodium.rb
@@ -2,13 +2,8 @@ cask "vscodium" do
   arch arm: "arm64", intel: "x64"
 
   version "1.70.2.22230"
-
-  on_intel do
-    sha256 "117c099ce22c430dad0c8ca4daa56020e7b0cf0679d984c64d8d74790a8f2f8d"
-  end
-  on_arm do
-    sha256 "423bf7fbcce09c84072c7a0decec9eb3efda9a0e3efe2da31078618958fbb504"
-  end
+  sha256 arm:   "423bf7fbcce09c84072c7a0decec9eb3efda9a0e3efe2da31078618958fbb504",
+         intel: "117c099ce22c430dad0c8ca4daa56020e7b0cf0679d984c64d8d74790a8f2f8d"
 
   url "https://github.com/VSCodium/vscodium/releases/download/#{version}/VSCodium.#{arch}.#{version}.dmg"
   name "VSCodium"
