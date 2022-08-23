2e4cb3b554a091ebf533ccb4649dfe23053e9815
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:27 -0400

    eclipse-java: use `sha256 arm: "...", intel: "..."`

--- Casks/eclipse-java.rb
+++ Casks/eclipse-java.rb
@@ -2,13 +2,8 @@ cask "eclipse-java" do
   arch arm: "aarch64", intel: "x86_64"
 
   version "4.24.0,2022-06"
-
-  on_intel do
-    sha256 "46636f6d9e82394412cc6660358c84200499fa79c89dda52c77c39e81a9592f7"
-  end
-  on_arm do
-    sha256 "387983833dd7f8296e4bcdba063258a6c9194dba2cb0ed6cddf4b4157369b445"
-  end
+  sha256 arm:   "387983833dd7f8296e4bcdba063258a6c9194dba2cb0ed6cddf4b4157369b445",
+         intel: "46636f6d9e82394412cc6660358c84200499fa79c89dda52c77c39e81a9592f7"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-java-#{version.csv.second}-R-macosx-cocoa-#{arch}.dmg&r=1"
   name "Eclipse IDE for Java Developers"
