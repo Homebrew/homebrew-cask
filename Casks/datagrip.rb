573160d1edb825d4f947593d9db008bc8f2c2517
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:25 -0400

    datagrip: use `sha256 arm: "...", intel: "..."`

--- Casks/datagrip.rb
+++ Casks/datagrip.rb
@@ -2,13 +2,8 @@ cask "datagrip" do
   arch arm: "-aarch64"
 
   version "2022.2.2,222.3739.67"
-
-  on_intel do
-    sha256 "b8494c758971e81c8f02f99caf53c0e05a7305cb988886c0229e135d9241065f"
-  end
-  on_arm do
-    sha256 "22e2aae4e6114dc8f5546fafa184c429a1b429f4147beac1a6e6fee1b5862d3f"
-  end
+  sha256 arm:   "22e2aae4e6114dc8f5546fafa184c429a1b429f4147beac1a6e6fee1b5862d3f",
+         intel: "b8494c758971e81c8f02f99caf53c0e05a7305cb988886c0229e135d9241065f"
 
   url "https://download.jetbrains.com/datagrip/datagrip-#{version.csv.first}#{arch}.dmg"
   name "DataGrip"
