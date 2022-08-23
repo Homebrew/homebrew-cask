c1d1e8724e074b0673f6c78a289d93466a2b62d0
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:43 -0400

    shift: use `sha256 arm: "...", intel: "..."`

--- Casks/shift.rb
+++ Casks/shift.rb
@@ -2,13 +2,8 @@ cask "shift" do
   arch arm: "arm64", intel: "x64"
 
   version "8.0.48.473"
-
-  on_intel do
-    sha256 "50cfaa6b97aefab0af61e26c53dbb0925467fffbc0de50434739769096b105a7"
-  end
-  on_arm do
-    sha256 "5e07cb8f12b64f9f23b0971b1ec8127a9c204272ace6cb2c2084c5c46b71ac58"
-  end
+  sha256 arm:   "5e07cb8f12b64f9f23b0971b1ec8127a9c204272ace6cb2c2084c5c46b71ac58",
+         intel: "50cfaa6b97aefab0af61e26c53dbb0925467fffbc0de50434739769096b105a7"
 
   url "https://updates.tryshift.com/v#{version.major_minor_patch}/stable/shift-v#{version}-stable-#{arch}.dmg"
   name "Shift"
