96ae482272e3fb8606e5808fc6ebb199931ecc9c
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:29 -0400

    ferdium: use `sha256 arm: "...", intel: "..."`

--- Casks/ferdium.rb
+++ Casks/ferdium.rb
@@ -2,13 +2,8 @@ cask "ferdium" do
   arch arm: "arm64", intel: "x64"
 
   version "6.0.0"
-
-  on_intel do
-    sha256 "131c29c39275143d294b022581556cf9ac2ed458efc13fea06f677205d854da1"
-  end
-  on_arm do
-    sha256 "c9b4a05a0654e57065df2b2d56738178d83345f4de157ef72daa950b48e51188"
-  end
+  sha256 arm:   "c9b4a05a0654e57065df2b2d56738178d83345f4de157ef72daa950b48e51188",
+         intel: "131c29c39275143d294b022581556cf9ac2ed458efc13fea06f677205d854da1"
 
   url "https://github.com/ferdium/ferdium-app/releases/download/v#{version}/Ferdium-mac-#{version}-#{arch}.dmg",
       verified: "github.com/ferdium/ferdium-app/"
