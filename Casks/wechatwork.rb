7264af0c9316906ea2249ad97831244470339b62
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:00:57 -0400

    wechatwork: replace arch conditionals with DSLs

--- Casks/wechatwork.rb
+++ Casks/wechatwork.rb
@@ -1,8 +1,11 @@
 cask "wechatwork" do
-  if Hardware::CPU.intel?
+  arch arm: "_arm64"
+
+  on_intel do
     version "4.0.12.90612"
     sha256 "20b887dda7f3c6cb74a12506dece04b8e6adf28c9c80d455ed6ae21f470e7b25"
-  else
+  end
+  on_arm do
     version "4.0.12.99158"
     sha256 "33e9b07a42983aba31dfef0511008be43395735b85f05372128a22da8a88740a"
   end
@@ -14,11 +17,7 @@ cask "wechatwork" do
   homepage "https://work.weixin.qq.com/"
 
   livecheck do
-    if Hardware::CPU.intel?
-      url "https://work.weixin.qq.com/wework_admin/commdownload?platform=mac"
-    else
-      url "https://work.weixin.qq.com/wework_admin/commdownload?platform=mac_arm64"
-    end
+    url "https://work.weixin.qq.com/wework_admin/commdownload?platform=mac#{arch}"
     strategy :header_match
   end
 
