2e2be393c7bb9277265a81b8c16ac751b145520c
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:23 -0400

    blobsaver: use `sha256 arm: "...", intel: "..."`

--- Casks/blobsaver.rb
+++ Casks/blobsaver.rb
@@ -2,13 +2,8 @@ cask "blobsaver" do
   arch arm: "arm", intel: "intel"
 
   version "3.2.1"
-
-  on_intel do
-    sha256 "341dad87e5a36f7f6c1f14050a4f44128afa2da5651abea761af59054d5029c8"
-  end
-  on_arm do
-    sha256 "0e510dd7315af465a9f1f333e7f303e16871fa9964ae9c1632145d38b6e32e04"
-  end
+  sha256 arm:   "0e510dd7315af465a9f1f333e7f303e16871fa9964ae9c1632145d38b6e32e04",
+         intel: "341dad87e5a36f7f6c1f14050a4f44128afa2da5651abea761af59054d5029c8"
 
   url "https://github.com/airsquared/blobsaver/releases/download/v#{version}/blobsaver-#{arch}-#{version}.dmg"
   name "blobsaver"
