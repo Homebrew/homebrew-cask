d4998d5ac071db93a11924d5436020aa5d8ff6e8
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:47 -0400

    turbovnc-viewer: use `sha256 arm: "...", intel: "..."`

--- Casks/turbovnc-viewer.rb
+++ Casks/turbovnc-viewer.rb
@@ -2,13 +2,8 @@ cask "turbovnc-viewer" do
   arch arm: "arm64", intel: "x86_64"
 
   version "3.0.1"
-
-  on_intel do
-    sha256 "0b9f083d753dc65c0a023d8d9c6c95da997184645ea216b60a8e1db72f8baccb"
-  end
-  on_arm do
-    sha256 "76f451817884185f5832dc794831fbfb62d50104a6b416d4c78c6d55ea4b0f12"
-  end
+  sha256 arm:   "76f451817884185f5832dc794831fbfb62d50104a6b416d4c78c6d55ea4b0f12",
+         intel: "0b9f083d753dc65c0a023d8d9c6c95da997184645ea216b60a8e1db72f8baccb"
 
   url "https://downloads.sourceforge.net/turbovnc/#{version}/TurboVNC-#{version}-#{arch}.dmg",
       verified: "sourceforge.net/turbovnc/"
