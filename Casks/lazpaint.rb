06e54843bb83511b91ef588b615a2dd9b5190d04
Patrick Linnane <patrick@linnane.io>
2022-08-23 07:49:57 -0700

    Update lazpaint from 7.1.6 to 7.2.2

--- Casks/lazpaint.rb
+++ Casks/lazpaint.rb
@@ -1,6 +1,6 @@
 cask "lazpaint" do
-  version "7.1.6"
-  sha256 "75088f275e71d6dbd7a76c6ba98083722f2f535fa7f7fa4f839b8f8650f1d862"
+  version "7.2.2"
+  sha256 "f7abc6fde628ea057ea9076acc156449b9e2e92d9d49f596cb6368850cc22c51"
 
   url "https://github.com/bgrabitmap/lazpaint/releases/download/v#{version}/lazpaint#{version}_macos64.dmg",
       verified: "github.com/bgrabitmap/lazpaint/"
