68b6d4cad92eb07db72b39cf24776183dfb0f538
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:46 -0400

    tempo: use `sha256 arm: "...", intel: "..."`

--- Casks/tempo.rb
+++ Casks/tempo.rb
@@ -2,15 +2,10 @@ cask "tempo" do
   arch arm: "release-arm64", intel: "release"
 
   version "6.0.0"
+  sha256 arm:   "67260ec4260e30da28abc20d847282f08e09d8bce00730594c598caf3a63bdb7",
+         intel: "acfe86aedce23077a34de27717f38920bf4eafb740841088ab31b685fec4e3af"
 
   url "https://download.yourtempo.co/#{arch}/Tempo-#{version}.dmg"
-  on_intel do
-    sha256 "acfe86aedce23077a34de27717f38920bf4eafb740841088ab31b685fec4e3af"
-  end
-  on_arm do
-    sha256 "67260ec4260e30da28abc20d847282f08e09d8bce00730594c598caf3a63bdb7"
-  end
-
   name "Tempo"
   desc "Email client that delivers all email in batches"
   homepage "https://www.yourtempo.co/"
