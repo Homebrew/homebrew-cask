a0d272430e38d5563a4e4fad51eda5f529d983a5
Rylan Polster <rslpolster@gmail.com>
2022-08-23 13:56:37 -0400

    anki: use arch and macOS version DSLs

--- Casks/anki.rb
+++ Casks/anki.rb
@@ -2,22 +2,19 @@ cask "anki" do
   arch arm: "apple", intel: "intel"
 
   version "2.1.54"
+  sha256 arm:   "44e229181dd6db5b6d5a3f9c4647f4ae92c5acee0b47af99c8646c6a5cf927e1",
+         intel: "92eb39f59f4e7b8b1b025178b337a0ef9d619521144748a32b3e2b8df1c45a00"
 
-  if Hardware::CPU.intel?
-    if MacOS.version >= :mojave
-      sha256 "92eb39f59f4e7b8b1b025178b337a0ef9d619521144748a32b3e2b8df1c45a00"
-      qtversion = 6
-    else
-      sha256 "edc44e5862384bb1c419033267c78167809253090c0302f07114c00c223db07a"
-      qtversion = 5
-    end
-  else
-    sha256 "44e229181dd6db5b6d5a3f9c4647f4ae92c5acee0b47af99c8646c6a5cf927e1"
-    qtversion = 6
+  url "https://github.com/ankitects/anki/releases/download/#{version}/anki-#{version}-mac-#{arch}-qt6.dmg",
+      verified: "github.com/ankitects/anki/"
+
+  on_high_sierra :or_older do
+    sha256 "edc44e5862384bb1c419033267c78167809253090c0302f07114c00c223db07a"
+
+    url "https://github.com/ankitects/anki/releases/download/#{version}/anki-#{version}-mac-#{arch}-qt5.dmg",
+        verified: "github.com/ankitects/anki/"
   end
 
-  url "https://github.com/ankitects/anki/releases/download/#{version}/anki-#{version}-mac-#{arch}-qt#{qtversion}.dmg",
-      verified: "github.com/ankitects/anki/"
   name "Anki"
   desc "Memory training application"
   homepage "https://apps.ankiweb.net/"
