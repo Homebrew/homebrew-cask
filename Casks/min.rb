1679b1bdb5eb390a2a5fa0c223d584dabb62dfe4
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:37 -0400

    min: use `sha256 arm: "...", intel: "..."`

--- Casks/min.rb
+++ Casks/min.rb
@@ -2,13 +2,8 @@ cask "min" do
   arch arm: "arm64", intel: "x86"
 
   version "1.25.1"
-
-  on_intel do
-    sha256 "48c21f8d702907b8627b3f4cf939b2447b09e8ed1f06d0dbe8f5a91451a76471"
-  end
-  on_arm do
-    sha256 "88d2515aa2b7c53e590f6291038e895a1f33bdfaec5bde748e0b1df1e3e0d53f"
-  end
+  sha256 arm:   "88d2515aa2b7c53e590f6291038e895a1f33bdfaec5bde748e0b1df1e3e0d53f",
+         intel: "48c21f8d702907b8627b3f4cf939b2447b09e8ed1f06d0dbe8f5a91451a76471"
 
   url "https://github.com/minbrowser/min/releases/download/v#{version}/min-v#{version}-mac-#{arch}.zip",
       verified: "github.com/minbrowser/min/"
