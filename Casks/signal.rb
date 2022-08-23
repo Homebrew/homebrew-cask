aef8162ac62844383fb1ba77c80b63a1f0bcd007
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:43 -0400

    signal: use `sha256 arm: "...", intel: "..."`

--- Casks/signal.rb
+++ Casks/signal.rb
@@ -2,13 +2,8 @@ cask "signal" do
   arch arm: "arm64", intel: "x64"
 
   version "5.55.0"
-
-  on_intel do
-    sha256 "0997f557924dbffdc477bc8088b3512776a8382c5eae30e191d0e730012fbbd4"
-  end
-  on_arm do
-    sha256 "18ee0df71f071d7c524d1273b8e93f04a4a479d20b753729a7c8bcf077636ed3"
-  end
+  sha256 arm:   "18ee0df71f071d7c524d1273b8e93f04a4a479d20b753729a7c8bcf077636ed3",
+         intel: "0997f557924dbffdc477bc8088b3512776a8382c5eae30e191d0e730012fbbd4"
 
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{arch}-#{version}.dmg"
   name "Signal"
