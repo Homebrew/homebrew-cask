d4888357dd1afc2df2fe120a6af03fa4f9423292
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:36 -0400

    mat: use `sha256 arm: "...", intel: "..."`

--- Casks/mat.rb
+++ Casks/mat.rb
@@ -2,13 +2,8 @@ cask "mat" do
   arch arm: "aarch64", intel: "x86_64"
 
   version "1.13.0.20220615"
-
-  on_intel do
-    sha256 "5e866ce672f2d800b902f017edc266406ef1c895e6defde0aac5d895d7966b98"
-  end
-  on_arm do
-    sha256 "55a7113181a5aea34d83a471752f81f27988720e2f58edc090e6e2565f020532"
-  end
+  sha256 arm:   "55a7113181a5aea34d83a471752f81f27988720e2f58edc090e6e2565f020532",
+         intel: "5e866ce672f2d800b902f017edc266406ef1c895e6defde0aac5d895d7966b98"
 
   url "https://download.eclipse.org/mat/#{version.major_minor_patch}/rcp/MemoryAnalyzer-#{version}-macosx.cocoa.#{arch}.dmg"
   name "Eclipse Memory Analyzer"
