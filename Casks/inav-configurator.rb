5e21a9602015ede162976eb4dbd6890b69013b26
Patrick Linnane <patrick@linnane.io>
2022-08-23 07:47:25 -0700

    Update inav-configurator from 5.0.0 to 5.1.0

--- Casks/inav-configurator.rb
+++ Casks/inav-configurator.rb
@@ -1,6 +1,6 @@
 cask "inav-configurator" do
-  version "5.0.0"
-  sha256 "f510bae6c7f5f7eded718b651b8d2a808fb0d068333a587e969e445cf1952b5d"
+  version "5.1.0"
+  sha256 "75d386c0b655ca67b0a76511e3717fd55113fa1426586c1353b8939c6dfdd953"
 
   url "https://github.com/iNavFlight/inav-configurator/releases/download/#{version}/INAV-Configurator_macOS_#{version}.zip"
   name "INAV Configurator"
