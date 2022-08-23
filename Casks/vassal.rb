30968dc780490e264b93627c91afbf63e3d89d30
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:47 -0400

    vassal: use `sha256 arm: "...", intel: "..."`

--- Casks/vassal.rb
+++ Casks/vassal.rb
@@ -2,13 +2,8 @@ cask "vassal" do
   arch arm: "aarch64", intel: "x86_64"
 
   version "3.6.7"
-
-  on_intel do
-    sha256 "d5c01584a97aaee4aef222b0f07ada24ada823a7a515a79c357084b1ecddd8a2"
-  end
-  on_arm do
-    sha256 "0b1e00f631aba7b8d79185d8e96065e32af6dae994d8461e1a3fe5f513d9f66e"
-  end
+  sha256 arm:   "0b1e00f631aba7b8d79185d8e96065e32af6dae994d8461e1a3fe5f513d9f66e",
+         intel: "d5c01584a97aaee4aef222b0f07ada24ada823a7a515a79c357084b1ecddd8a2"
 
   url "https://github.com/vassalengine/vassal/releases/download/#{version}/VASSAL-#{version}-macos-#{arch}.dmg",
       verified: "github.com/vassalengine/vassal/"
