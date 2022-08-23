4252fe775944cf687b13c3fbac5e140f4545434f
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:30:44 -0400

    springtoolsuite: use `sha256 arm: "...", intel: "..."`

--- Casks/springtoolsuite.rb
+++ Casks/springtoolsuite.rb
@@ -2,13 +2,8 @@ cask "springtoolsuite" do
   arch arm: "aarch64", intel: "x86_64"
 
   version "4.15.3,4.24.0"
-
-  on_intel do
-    sha256 "9e69a29355e039ce9a0bb9441a515aabb212ef04131d45e9537710225a026b32"
-  end
-  on_arm do
-    sha256 "8ad1d2c34d7c1b9b6854f89fbb65503463d41ce5953dd433a932da08d103e63f"
-  end
+  sha256 arm:   "8ad1d2c34d7c1b9b6854f89fbb65503463d41ce5953dd433a932da08d103e63f",
+         intel: "9e69a29355e039ce9a0bb9441a515aabb212ef04131d45e9537710225a026b32"
 
   url "https://download.springsource.com/release/STS#{version.major}/#{version.csv.first}.RELEASE/dist/e#{version.csv.second.major_minor}/spring-tool-suite-#{version.major}-#{version.csv.first}.RELEASE-e#{version.csv.second}-macosx.cocoa.#{arch}.dmg",
       verified: "download.springsource.com/release/"
