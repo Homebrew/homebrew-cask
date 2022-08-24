a0b7783fcc1969e58f76e41e78e4aac7b39bb6ca
Patrick Linnane <patrick@linnane.io>
2022-08-24 09:09:14 -0700

    Update shapr3d from 5.192.0.3827 to 5.200.0.3889

--- Casks/shapr3d.rb
+++ Casks/shapr3d.rb
@@ -1,6 +1,6 @@
 cask "shapr3d" do
-  version "5.192.0.3827"
-  sha256 "e923906bb2f27d4529385848ac4626447c3c62619d5efed9a95d588dc8626535"
+  version "5.200.0.3889"
+  sha256 "68e45d674f1af72007347d09613e2c417afa671eaacecf0311a5291063deac7f"
 
   url "https://download.shapr3d.com/mac/Shapr3D-#{version}.dmg"
   name "Shapr3D"
