262d340e6ffc7c17dce72f4f239696b134fe40d6
Patrick Linnane <patrick@linnane.io>
2022-08-24 16:21:57 -0700

    Add arm64, bump to 1.7.7

--- Casks/asciidocfx.rb
+++ Casks/asciidocfx.rb
@@ -1,8 +1,11 @@
 cask "asciidocfx" do
-  version "1.7.6"
-  sha256 "271f0ba474bc007069e62c7e586c741da082dd342099ba0bfa2feab23074930f"
+  arch arm: "_M1"
 
-  url "https://github.com/asciidocfx/AsciidocFX/releases/download/v#{version}/AsciidocFX_Mac.dmg",
+  version "1.7.7"
+  sha256 arm:   "1f6779d46ff4a7452e53fbea2a5d7fbe72e5ad55a823307595ba936403cfd2b0",
+         intel: "84c2e92655551c2a02ed400815b6a004631ba6f2db9be6a0ab86e9be6b838f9f"
+
+  url "https://github.com/asciidocfx/AsciidocFX/releases/download/v#{version}/AsciidocFX_Mac#{arch}.dmg",
       verified: "github.com/asciidocfx/AsciidocFX/"
   name "AsciidocFX"
   desc "Asciidoc Editor and Toolchain to build books, documents and slides"
