31aac3128caa96336c45d66702eadc45a245ae09
Miccal Matthews <miccal.matthews@gmail.com>
2022-08-24 10:58:21 +0800

    Update cider.rb

--- Casks/cider.rb
+++ Casks/cider.rb
@@ -3,7 +3,7 @@ cask "cider" do
   sha256 "9cb5d9b3e4aeddfd2f6623f981f6dd52c112c7192e29a44adea2739fec51bd4d"
 
   url "https://github.com/ciderapp/cider-releases/releases/download/v#{version}/Cider-#{version}-universal.dmg",
-       verified: "github.com/ciderapp/cider-releases"
+      verified: "github.com/ciderapp/cider-releases"
   name "Cider"
   desc "Open source and community oriented Apple Music client"
   homepage "https://cider.sh/"
