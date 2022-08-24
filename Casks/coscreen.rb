75b0b27a926dcf30ef0dfc30b083461769751c88
Patrick Linnane <patrick@linnane.io>
2022-08-24 16:32:00 -0700

    coscreen 4.0.69
    Update coscreen from 4.0.67 to 4.0.69
    
    Closes #130376.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/coscreen.rb
+++ Casks/coscreen.rb
@@ -1,9 +1,9 @@
 cask "coscreen" do
   arch arm: "arm64", intel: "x64"
 
-  version "4.0.67"
-  sha256 arm:   "0cec123742472ed4010650a928b25ce4a05ac596cc8ffe231618dda144c197e1",
-         intel: "b59f60e405b713f3fd81cdae656fed4d2f3d8c23e70555f4368e52d9e90a7aa2"
+  version "4.0.69"
+  sha256 arm:   "4d832da41eaac137f10729c82dd9ebe65af50c65afade9cf8e3e0c684c1b8c4c",
+         intel: "4d0641c2094dc0e9e850ace16de5a133db6295af6aff9d451e358c14b5787f39"
 
   url "https://update.coscreen.org/CoScreen-#{version}-stable-#{arch}.dmg",
       verified: "https://update.coscreen.org/"
