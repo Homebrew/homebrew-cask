c9d9b6142111bdefe6ceac1b8ddbd19fb75e3529
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-08-24 15:00:47 +0900

    steermouse 5.6.4
    Update steermouse from 5.6.3 to 5.6.4
    
    Closes #130324.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/steermouse.rb
+++ Casks/steermouse.rb
@@ -1,6 +1,6 @@
 cask "steermouse" do
-  version "5.6.3"
-  sha256 "33868bfe6a4a5edca653ab9c7ddaceec95d80e06fd6e05cdd139ce9cf137ad57"
+  version "5.6.4"
+  sha256 "db388f7072c6af4922038a7b5b4c6b59f81391a222e889fd53cebd5c83e96c29"
 
   url "https://plentycom.jp/ctrl/files_sm/SteerMouse#{version}.dmg"
   name "SteerMouse"
