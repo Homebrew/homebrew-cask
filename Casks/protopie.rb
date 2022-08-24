cd688bd4dbbbac25c22035a5397a0c4d9cdd6b55
Patrick Linnane <patrick@linnane.io>
2022-08-24 08:47:07 -0700

    Update protopie from 7.1.0 to 7.2.0

--- Casks/protopie.rb
+++ Casks/protopie.rb
@@ -1,6 +1,6 @@
 cask "protopie" do
-  version "7.1.0"
-  sha256 "68376e4c939360b1cef32408918fb0704e47a6f3ad6fb20d2dabe1079416ac52"
+  version "7.2.0"
+  sha256 "bb6d1451393160581d144c8cb37cd4457b4334cc75bc9959010b56fb99ff06a1"
 
   url "https://release.protopie.io/ProtoPie-#{version}.dmg"
   name "ProtoPie"
