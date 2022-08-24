2500d9c1533c8066cef37825956b8eddefc2d15a
Patrick Linnane <patrick@linnane.io>
2022-08-24 08:50:58 -0700

    Update quicken from 6.8.3 to 6.9.0

--- Casks/quicken.rb
+++ Casks/quicken.rb
@@ -1,6 +1,6 @@
 cask "quicken" do
-  version "6.8.3,608.44884.100"
-  sha256 "6ac78f40b9e616f9a08b118b9a0d63089468fc2639979b3d18105a1eb8d33110"
+  version "6.9.0,609.45401.100"
+  sha256 "b49567ad5a2612747a19c77e9194db4be781de30ba6046e41a8b50a88340ec77"
 
   url "https://download.quicken.com/mac/Quicken/001/Release/031A96D9-EFE6-4520-8B6A-7F465DDAA3E4/Quicken-#{version.csv.second}/Quicken-#{version.csv.second}.zip"
   name "Quicken"
