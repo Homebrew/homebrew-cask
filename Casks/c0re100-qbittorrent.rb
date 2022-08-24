70d27e839ed18cd4df18a12eeffcad227fa32d36
Patrick Linnane <patrick@linnane.io>
2022-08-23 21:20:14 -0700

    c0re100-qbittorrent 4.4.4.10
    Update c0re100-qbittorrent from 4.4.3.12 to 4.4.4.10
    
    Closes #130314.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/c0re100-qbittorrent.rb
+++ Casks/c0re100-qbittorrent.rb
@@ -1,6 +1,6 @@
 cask "c0re100-qbittorrent" do
-  version "4.4.3.12"
-  sha256 "b99f9700f83622655d40d1588f4aa02f908ff26850bea49c142dcad7c3ef91d4"
+  version "4.4.4.10"
+  sha256 "604db17a56d03b3ccd8df19737c137991cd20fc123ad0fdc4b780751c931be83"
 
   url "https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-#{version}/qBittorrent-Enhanced-Edition-release-#{version}-macOS-universal.dmg"
   name "qBittorrent Enhanced Edition"
