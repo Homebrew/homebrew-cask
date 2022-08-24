4d4d645b3a285c6469a80f30ff8463e8400054e9
Miccal Matthews <miccal.matthews@gmail.com>
2022-08-24 10:35:51 +0800

    Update cider.rb

--- Casks/cider.rb
+++ Casks/cider.rb
@@ -1,11 +1,17 @@
 cask "cider" do
   version "1.5.4-beta.99"
-  sha256 :no_check
+  sha256 "9cb5d9b3e4aeddfd2f6623f981f6dd52c112c7192e29a44adea2739fec51bd4d"
 
-  url "https://github.com/ciderapp/cider-releases/releases/download/v#{version}/Cider-#{version}-universal.dmg", verified: "https://github.com/ciderapp/cider-releases"
+  url "https://github.com/ciderapp/cider-releases/releases/download/v#{version}/Cider-#{version}-universal.dmg",
+       verified: "github.com/ciderapp/cider-releases"
   name "Cider"
   desc "Open source and community oriented Apple Music client"
   homepage "https://cider.sh/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   auto_updates true
 
@@ -13,8 +19,8 @@ cask "cider" do
 
   zap trash: [
     "~/Library/Application Support/Cider",
+    "~/Library/Logs/Cider",
     "~/Library/Preferences/cider.plist",
     "~/Library/Saved Application State/cider.savedState",
-    "~/Library/Logs/Cider",
   ]
 end
