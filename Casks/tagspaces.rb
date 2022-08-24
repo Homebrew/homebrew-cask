19a8e40ba99bbef637b725f154aa338efb69ae97
Patrick Linnane <patrick@linnane.io>
2022-08-24 09:13:34 -0700

    tagspaces 4.5.2
    Update tagspaces from 4.4.3 to 4.5.2
    
    Closes #130355.
    
    Signed-off-by: Markus Reiter <me@reitermark.us>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/tagspaces.rb
+++ Casks/tagspaces.rb
@@ -1,9 +1,9 @@
 cask "tagspaces" do
   arch arm: "arm64", intel: "x64"
 
-  version "4.4.3"
-  sha256 arm:   "62faf39ce08575421d3a9d67763399c2ee4a1bca09ebae3401b747fa0867e8dd",
-         intel: "f40a73828459b5dd5cd31063bd0cfdfeef4736e2a6594951ca1658060bd6965d"
+  version "4.5.2"
+  sha256 arm:   "6f8ade0bb3a637557689885c68068ba30625e91f00fdb33c9ebd465824380cc8",
+         intel: "222ccfc3a4884f2909ff739da7d58e1fe4d746924763bd3aa573df242a0dbbe7"
 
   url "https://github.com/tagspaces/tagspaces/releases/download/v#{version}/tagspaces-mac-#{arch}-#{version}.dmg",
       verified: "github.com/tagspaces/tagspaces/"
