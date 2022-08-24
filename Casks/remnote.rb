ab3f4eb392c6da04cca4efc30519d82326773906
Benedikt Müller <d12bb@posteo.de>
2022-08-24 12:49:46 +0200

    remnote: add arm64 version
    Closes #130326.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/remnote.rb
+++ Casks/remnote.rb
@@ -1,8 +1,11 @@
 cask "remnote" do
-  version "1.8.17"
-  sha256 "6ca0a1a6555e843322f9d27c05c4c1039ea71bef2b15d224cc71a6ee63aa5aa1"
+  arch arm: "-arm64", intel: ""
 
-  url "https://download.remnote.io/RemNote-#{version}.dmg",
+  version "1.8.17"
+  sha256 arm:   "e03df3f1e18d5e4f2d9edd04a93c9cb1a8991e8b5fc926b5bb0bf3ff03ed4bf2",
+         intel: "6ca0a1a6555e843322f9d27c05c4c1039ea71bef2b15d224cc71a6ee63aa5aa1"
+
+  url "https://download.remnote.io/RemNote-#{version}#{arch}.dmg",
       verified: "remnote.io"
   name "RemNote"
   desc "Spaced-repetition powered note-taking tool"
