964a1f7a0d36c915e53fccd8ba2a1c741de4f724
Patrick Linnane <patrick@linnane.io>
2022-08-25 11:05:30 -0700

    plasticscm-cloud-edition 11.0.16.7348
    Update plasticscm-cloud-edition from 11.0.16.7303 to 11.0.16.7348
    
    Closes #130431.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/plasticscm-cloud-edition.rb
+++ Casks/plasticscm-cloud-edition.rb
@@ -1,6 +1,6 @@
 cask "plasticscm-cloud-edition" do
-  version "11.0.16.7303"
-  sha256 "59f55f96ac1a5a056c7d1d9a052154ff7e5d6d2e2e7a7968f1f9960c2a444fc7"
+  version "11.0.16.7348"
+  sha256 "0da758aaba05ca917e750ba5ce2afd3f8575e3026819630a7e14077f1fc48d3e"
 
   url "https://s3.eu-west-2.amazonaws.com/plastic-releases/releases/#{version}/plasticscm/osx/plasticscm-cloud-#{version}.pkg.zip",
       verified: "s3.eu-west-2.amazonaws.com/plastic-releases/"
