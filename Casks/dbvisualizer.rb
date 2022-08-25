230e43ed740547c8e64bfc6d9da9b92bede39983
Patrick Linnane <patrick@linnane.io>
2022-08-25 11:29:09 -0700

    dbvisualizer 13.0.5
    Update dbvisualizer from 13.0.4 to 13.0.5
    
    Closes #130439.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/dbvisualizer.rb
+++ Casks/dbvisualizer.rb
@@ -1,6 +1,6 @@
 cask "dbvisualizer" do
-  version "13.0.4"
-  sha256 "498f24d5add6fec44197461c66ec16ccabeb9da40a931dd2426533a30c1a418e"
+  version "13.0.5"
+  sha256 "6b27c46b4d99b74f19add91b2b16d9289d0df4b1d99e2afd920d7ebeca79a440"
 
   url "https://www.dbvis.com/product_download/dbvis-#{version}/media/dbvis_macos_#{version.dots_to_underscores}_jre.dmg"
   name "DbVisualizer"
