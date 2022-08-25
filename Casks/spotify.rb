9eaf6fc5c799016e022b399b85274216e669063a
Aliaksandr Maiseyenka <WolfMeister@users.noreply.github.com>
2022-08-25 16:17:10 +0200

    spotify 1.1.92.647,a4397eb7,7
    * Update spotify from 1.1.92.647,ga4397eb,7 to 1.1.92.647,ga4397eb,7
    * spotify/fix: Fixed version of the application
    
    Closes #130413.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/spotify.rb
+++ Casks/spotify.rb
@@ -1,7 +1,7 @@
 cask "spotify" do
   arch arm: "ARM64"
 
-  version "1.1.91.824,07f1e963,9"
+  version "1.1.92.647,a4397eb7,7"
   sha256 :no_check
 
   url "https://download.scdn.co/Spotify#{arch}.dmg",
