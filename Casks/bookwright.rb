64eadbbfaa25d65dc811373b5c39a8924ba3ceb1
Miccal Matthews <miccal.matthews@gmail.com>
2022-08-25 12:07:19 +0800

    Update Casks/bookwright.rb
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/bookwright.rb
+++ Casks/bookwright.rb
@@ -7,11 +7,13 @@ cask "bookwright" do
   desc "Make a book with this tool and the Blurb printing service"
   homepage "https://www.blurb.com/bookwright"
 
+  # If this URL only provides information for a specific major version, this
+  # check will be unable to detect new major versions and these will need to
+  # be identified manually outside of livecheck. In that scenario, the check
+  # should theoretically work as expected again after updating the cask version.
   livecheck do
-    # url "https://www.blurb.com/bookwright-install-mac?platform=MacOS&bwversion=#{version}&download=true"
-    # regex(/href=.+www.blurb.com\/booksmart_download\/bookwright?platform=mac&bwversion=(\d+\.\d+\.\d+)/i)
-
-    skip "Unable to get a direct link, it is added via JavaScript within the page."
+    url "https://www.blurb.com/bookwright_v#{version.major}/versions.json"
+    regex(/["']version["']:\s*?["']v?(\d+(?:\.\d+)+)["']/i)
   end
 
   app "BookWright.app"
