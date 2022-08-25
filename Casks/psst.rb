a1d077ceeb0ad1fbb12ba478eba067afdfafd7e3
chiefmikey <wolfemikl@gmail.com>
2022-08-25 03:05:44 -0600

    Update psst from 0.1.0,20220816.085916 to 0.1.0,20220825.085407

--- Casks/psst.rb
+++ Casks/psst.rb
@@ -1,5 +1,5 @@
 cask "psst" do
-  version "0.1.0,20220816.085916"
+  version "0.1.0,20220825.085407"
   sha256 :no_check
 
   url "https://nightly.link/jpochyla/psst/workflows/build/master/Psst-x64.dmg.zip",
