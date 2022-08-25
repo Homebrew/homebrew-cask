7e2c48ac5ff58d75d25045fbb7ae7bd67326392e
Miccal Matthews <miccal.matthews@gmail.com>
2022-08-25 12:15:12 +0800

    Update cider.rb

--- Casks/cider.rb
+++ Casks/cider.rb
@@ -1,6 +1,6 @@
 cask "cider" do
-  version "1.5.4-beta.99"
-  sha256 "9cb5d9b3e4aeddfd2f6623f981f6dd52c112c7192e29a44adea2739fec51bd4d"
+  version "1.5.6-beta.8"
+  sha256 "2e37c7b4a40c66ee703cdf38649822d4ccd1efca6740be3cf486101139c804fd"
 
   url "https://github.com/ciderapp/cider-releases/releases/download/v#{version}/Cider-#{version}-universal.dmg",
       verified: "github.com/ciderapp/cider-releases"
