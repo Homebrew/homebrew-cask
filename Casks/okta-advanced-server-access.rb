1cc3a3faa67cadfb6675afb905a2875068eecf68
LanikSJ <LanikSJ@users.noreply.github.com>
2022-08-24 17:04:34 -0700

    Update okta-advanced-server-access from 1.61.0 to 1.62.3 (#130379)

--- Casks/okta-advanced-server-access.rb
+++ Casks/okta-advanced-server-access.rb
@@ -1,6 +1,6 @@
 cask "okta-advanced-server-access" do
-  version "1.61.0"
-  sha256 "098ee39ccd772f6bb47c995c1b16326f9a769b1c180af4efcf85178c98f106b9"
+  version "1.62.3"
+  sha256 "15ac485f0c2c9446e6bd9cf44a783dd17eda578abf7f6967e4b407db7f104e41"
 
   url "https://dist.scaleft.com/client-tools/mac/v#{version}/ScaleFT-#{version}.pkg",
       verified: "scaleft.com/"
