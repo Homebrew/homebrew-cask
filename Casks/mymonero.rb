88621401a20883618de80596d1887144937fa37a
Patrick Linnane <patrick@linnane.io>
2022-08-25 10:00:03 -0700

    mymonero 1.3.2
    Update mymonero from 1.3.0 to 1.3.2
    
    Closes #130424.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/mymonero.rb
+++ Casks/mymonero.rb
@@ -1,6 +1,6 @@
 cask "mymonero" do
-  version "1.3.0"
-  sha256 "a3213956a46ec49daf9e982ea297b346a552429fb94d2e53cb43aa6acceaa6ca"
+  version "1.3.2"
+  sha256 "77719a53fe8b5ab19d632ce512836a4caad0e01f9f08ca56fed03ae094264755"
 
   url "https://github.com/mymonero/mymonero-app-js/releases/download/v#{version}/MyMonero-#{version}.dmg",
       verified: "github.com/mymonero/mymonero-app-js/"
