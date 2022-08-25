7e7ebef01a9fe0901f9e12e2fbe9b49f4c3aaff8
Patrick Linnane <patrick@linnane.io>
2022-08-25 11:09:02 -0700

    sidequest 0.10.32
    Update sidequest from 0.10.31 to 0.10.32
    
    Closes #130433.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/sidequest.rb
+++ Casks/sidequest.rb
@@ -1,9 +1,9 @@
 cask "sidequest" do
   arch arm: "-arm64"
 
-  version "0.10.31"
-  sha256 arm:   "9d060ae49beb33bb70cbc0253f5ef5b33275ffc146b23d2461b60bf084ae08fa",
-         intel: "6639bf7b29b7cdf274c667d4ff490d600b3732d8323cd052703080a4b4ada80f"
+  version "0.10.32"
+  sha256 arm:   "76e77a38972ddda0a79ae6cd7fdd4ccc3e9d1faf657d7d0480ac91b387781a97",
+         intel: "fd8b8e6ca7a07af501c1660edff52d3e1d888527c57425e42ba9f38ea1c7f75f"
 
   url "https://github.com/SideQuestVR/SideQuest/releases/download/v#{version}/SideQuest-#{version}#{arch}.dmg",
       verified: "github.com/SideQuestVR/SideQuest/"
