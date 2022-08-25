b7e8430bcaaac99fa29982c022c2fbd605c5fa2a
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-08-25 15:37:17 +0800

    Update tencent-meeting.rb

--- Casks/tencent-meeting.rb
+++ Casks/tencent-meeting.rb
@@ -1,16 +1,17 @@
 cask "tencent-meeting" do
+  version "3.10.7.413"
   arch arm: "arm64", intel: "x86_64"
 
   on_intel do
-    version "3.10.7.413,2012f9451007318aea154d31ae6ff91c"
+    ver "2012f9451007318aea154d31ae6ff91c"
     sha256 "197fef8145de764344740c66ee438bbddaf95f8d3ee9fbd7ee90fc303281ec8c"
   end
   on_arm do
-    version "3.10.7.413,f543792179e0b5f5696974a5939435bc"
+    ver "f543792179e0b5f5696974a5939435bc"
     sha256 "678d9918cbda90948900c56d57e10e378e92ec78b2687e8f75c0791f20f478a3"
   end
 
-  url "https://updatecdn.meeting.qq.com/cos/#{version.csv.second}/TencentMeeting_0300000000_#{version.csv.first}.publish.#{arch}.dmg",
+  url "https://updatecdn.meeting.qq.com/cos/#{ver}/TencentMeeting_0300000000_#{version}.publish.#{arch}.dmg",
       verified: "updatecdn.meeting.qq.com/cos/"
   name "Tencent Meeting"
   name "腾讯会议"
