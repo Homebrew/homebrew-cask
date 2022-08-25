5c37603fbd196e499a458fa18ec9aae13792ccc0
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-08-25 16:14:50 +0800

    Update tencent-meeting.rb

--- Casks/tencent-meeting.rb
+++ Casks/tencent-meeting.rb
@@ -1,18 +1,16 @@
 cask "tencent-meeting" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "3.10.7.413"
-
   on_intel do
-    ver "2012f9451007318aea154d31ae6ff91c"
+    version "3.10.7.413,2012f9451007318aea154d31ae6ff91c"
     sha256 "197fef8145de764344740c66ee438bbddaf95f8d3ee9fbd7ee90fc303281ec8c"
   end
   on_arm do
-    ver "f543792179e0b5f5696974a5939435bc"
+    version "3.10.7.413,f543792179e0b5f5696974a5939435bc"
     sha256 "678d9918cbda90948900c56d57e10e378e92ec78b2687e8f75c0791f20f478a3"
   end
 
-  url "https://updatecdn.meeting.qq.com/cos/#{ver}/TencentMeeting_0300000000_#{version}.publish.#{arch}.dmg",
+  url "https://updatecdn.meeting.qq.com/cos/#{version.csv.second}/TencentMeeting_0300000000_#{version.csv.first}.publish.#{arch}.dmg",
       verified: "updatecdn.meeting.qq.com/cos/"
   name "Tencent Meeting"
   name "腾讯会议"
@@ -20,8 +18,7 @@ cask "tencent-meeting" do
   homepage "https://meeting.tencent.com/"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    skip "No version information available"
   end
 
   auto_updates true
