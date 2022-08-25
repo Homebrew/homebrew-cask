bbf3c768c37471cfa75b3429b7440afab528ade8
chiefmikey <wolfemikl@gmail.com>
2022-08-25 03:41:47 -0600

    Update ssh-config-editor from 2.5.1 to 2.6-b

--- Casks/ssh-config-editor.rb
+++ Casks/ssh-config-editor.rb
@@ -1,6 +1,6 @@
 cask "ssh-config-editor" do
-  version "2.5.1,96"
-  sha256 "a91a0db47e5a858abe1788baea135d1e306f335bb295468066621cffd996a3bf"
+  version "2.6-b,97"
+  sha256 "5adbf9a1dc3466195f43e0fc4c955f124b16b149ae349a29e7d9368aa6472505"
 
   url "https://hejki.org/download/ssheditor/SSHConfigEditor-#{version.csv.second}.dmg"
   name "SSH Config Editor"
