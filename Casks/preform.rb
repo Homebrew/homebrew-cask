d746fa36401d4de278760304c4aa36daa69252b2
Patrick Linnane <patrick@linnane.io>
2022-08-25 11:03:51 -0700

    preform 3.27.0,162_23329
    Update preform from 3.26.2 to 3.27.0
    
    Closes #130430.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/preform.rb
+++ Casks/preform.rb
@@ -1,6 +1,6 @@
 cask "preform" do
-  version "3.26.2,155_22353"
-  sha256 "547cd2fd327d32d29ae90f85fa289a8a153c3ea32ad04d1f8cd877086bd11993"
+  version "3.27.0,162_23329"
+  sha256 "24b48ba2862d7c2c8c25f7a0307d190b5dd0fbc58dafc4477c4cf791520e9093"
 
   url "https://downloads.formlabs.com/PreForm/Release/#{version.csv.first}/PreForm_mac_#{version.csv.first}_release_releaser_#{version.csv.second}.dmg"
   name "PreForm"
