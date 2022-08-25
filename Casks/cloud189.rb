74e5193a4a65f45ae15386310a84a96733369914
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-08-25 14:58:16 +0800

    Update cloud189 with sha256 and livecheck

--- Casks/cloud189.rb
+++ Casks/cloud189.rb
@@ -1,6 +1,6 @@
 cask "cloud189" do
   version "1.1.0"
-  sha256 :no_check
+  sha256 "d109d1f996654321b86ff014f5ab8cc86ec9a144518605e9889db0ed6922c935"
 
   url "https://download.cloud.189.cn/file/downloadFile.action?dlt=4&dt=1&expired=1766930486508&sk=297953828&ufi=61304212366059386&zyc=60&token=cloud3&sig=gfhWf6DndIlD28kChBJM1d48E18%3D"
   name "Cloud189"
@@ -9,7 +9,8 @@ cask "cloud189" do
   homepage "https://cloud.189.cn/web/static/download-client/index.html"
 
   livecheck do
-    skip "No version information available"
+    url :url
+    strategy :header_match
   end
 
   app "天翼云盘.app"
