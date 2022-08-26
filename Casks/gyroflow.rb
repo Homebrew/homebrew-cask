dcddbbe35348799521041e8d78329d15d31ba770
Mykola Rybak <mykola.rybak@gmail.com>
2022-08-26 12:28:46 +0300

    gyroflow 1.2.0
    Update gyroflow from 1.1.0 to 1.2.0
    
    Closes #130460.
    
    Signed-off-by: Markus Reiter <me@reitermark.us>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/gyroflow.rb
+++ Casks/gyroflow.rb
@@ -1,6 +1,6 @@
 cask "gyroflow" do
-  version "1.1.0"
-  sha256 "21b0df549a5f2378464c83d1a7b4d55f7040b65e61db5fd37d876665fc666050"
+  version "1.2.0"
+  sha256 "d387bf070358e0e37ce7b1f3c36438195f75e9ca484b405b412b219cbb309892"
 
   url "https://github.com/gyroflow/gyroflow/releases/download/v#{version}/Gyroflow-mac-universal.dmg",
       verified: "github.com/gyroflow/gyroflow/"
