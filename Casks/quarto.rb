daffeeb27d34e10611efea8cde5bd6e9ab97eb60
Patrick Linnane <patrick@linnane.io>
2022-08-26 13:56:09 -0700

    Update quarto from 1.1.132 to 1.1.141

--- Casks/quarto.rb
+++ Casks/quarto.rb
@@ -1,6 +1,6 @@
 cask "quarto" do
-  version "1.1.132"
-  sha256 "08dd43a1e768fae4ab23cea207dad15050302de20924d6ce609054dcdf48992d"
+  version "1.1.141"
+  sha256 "10738e05519a58b57a5812e9196933172139ccf67c67d148b0b94d305eba6f2e"
 
   url "https://github.com/quarto-dev/quarto-cli/releases/download/v#{version}/quarto-#{version}-macos.pkg",
       verified: "github.com/quarto-dev/quarto-cli/"
