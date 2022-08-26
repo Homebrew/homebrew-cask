568e006e50721f7390fb05e5a25f26a5a640b9af
krehel <39449589+krehel@users.noreply.github.com>
2022-08-26 17:43:02 -0400

    Update sabaki.rb
    Update Sabaki to include Apple Silicon build

--- Casks/sabaki.rb
+++ Casks/sabaki.rb
@@ -1,8 +1,11 @@
 cask "sabaki" do
-  version "0.52.1"
-  sha256 "231dcfcf635daecd17d78df71f606e32f0521d51125219f2a498b08fb13d9e6f"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://github.com/SabakiHQ/Sabaki/releases/download/v#{version}/sabaki-v#{version}-mac-x64.7z",
+  version "0.52.1"
+  sha256 arm:   "93e791a2278c542581846ae2331766415a630e86b8dabda36cf5d9e0823087d2",
+         intel: "231dcfcf635daecd17d78df71f606e32f0521d51125219f2a498b08fb13d9e6f"
+
+  url "https://github.com/SabakiHQ/Sabaki/releases/download/v#{version}/sabaki-v#{version}-mac-#{arch}.7z",
       verified: "github.com/SabakiHQ/Sabaki/"
   name "Sabaki"
   desc "Go board and SGF editor"
