59164b17f073f68ebe33e645c028de5da7437449
Patrick Linnane <patrick@linnane.io>
2022-08-26 09:06:21 -0700

    quarto 1.1.132
    Update quarto from 1.0.38 to 1.1.132
    
    Closes #130483.
    
    Signed-off-by: Markus Reiter <me@reitermark.us>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/quarto.rb
+++ Casks/quarto.rb
@@ -1,6 +1,6 @@
 cask "quarto" do
-  version "1.0.38"
-  sha256 "e7b8f13a999c58371eae27d586d707de66b797e24f0d44cbefb84aa2111a1759"
+  version "1.1.132"
+  sha256 "08dd43a1e768fae4ab23cea207dad15050302de20924d6ce609054dcdf48992d"
 
   url "https://github.com/quarto-dev/quarto-cli/releases/download/v#{version}/quarto-#{version}-macos.pkg",
       verified: "github.com/quarto-dev/quarto-cli/"
