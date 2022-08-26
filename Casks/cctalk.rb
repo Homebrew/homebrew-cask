04305d200256c041b5e2cb2b4e86c9e8786040e5
Patrick Linnane <patrick@linnane.io>
2022-08-25 20:51:54 -0700

    cctalk 7.9.7.6
    Update cctalk from 7.9.5.6 to 7.9.7.6
    
    Closes #130456.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/cctalk.rb
+++ Casks/cctalk.rb
@@ -1,6 +1,6 @@
 cask "cctalk" do
-  version "7.9.5.6"
-  sha256 "6f9ec965957a8db69632df89fdfdc8bdb90e3971ae3bea67ab69f2fdc30729aa"
+  version "7.9.7.6"
+  sha256 "2d5ed97ac2433dcb4bc93729091859ec99cf32994770880445adff99378692ba"
 
   url "https://cc.hjfile.cn/cc/#{version}/8/1/103/#{version}.dmg",
       verified: "cc.hjfile.cn/"
