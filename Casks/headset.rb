96427d7c1ce31d448ee427033c986367360df856
Felipe Castillo <fcastillo.ec@gmail.com>
2022-08-25 20:40:32 -0700

    headset 4.2.0
    Update headset from 4.2.0 to 4.2.0
    
    Closes #130457.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/headset.rb
+++ Casks/headset.rb
@@ -1,9 +1,9 @@
 cask "headset" do
   arch arm: "arm64", intel: "x64"
 
-  version "4.1.2"
-  sha256 arm:   "fa58608c4d3f4ffa2dedd07225661fe8a9f7b059391c7d2cab09c94c5016cf1b",
-         intel: "5252c69b2c3bc02692b013a41d6f4c5638fe01e7dcfcacc6b55908441b1176e7"
+  version "4.2.0"
+  sha256 arm:   "28a9d96d8bf7beebc6a93e77ac166fe16283c72e07171dd0ceb33df6d78bc2a2",
+         intel: "532dcb484b119b74bdc30c72856d072df03c9b64546d3912a7dd3d371733dff6"
 
   url "https://github.com/headsetapp/headset-electron/releases/download/v#{version}/Headset-#{version}-#{arch}.dmg",
       verified: "github.com/headsetapp/headset-electron/"
