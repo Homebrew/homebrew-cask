6556948118137c4b2a4efabaadb6e872d44881cf
Patrick Linnane <patrick@linnane.io>
2022-08-26 09:22:38 -0700

    Update temurin from 18.0.2 to 18.0.2.1

--- Casks/temurin.rb
+++ Casks/temurin.rb
@@ -1,9 +1,9 @@
 cask "temurin" do
   arch arm: "aarch64", intel: "x64"
 
-  version "18.0.2,9"
-  sha256 arm:   "006ee54d7537758036fa0020d92856c8305e1c3e24f63ff89236827abf2431b9",
-         intel: "aa78eba86b8bda6e1b38240707fc27792065046ff9e13f1b6a52eefa1dfa854c"
+  version "18.0.2.1,1"
+  sha256 arm:   "21797abd8a53efcc89da3b4613697a0b2737f3c3cbb7f6ce0e5128478d5ce262",
+         intel: "ca4a486dfe344cf9476fb7174df66740826f679c9356caf25f14246ea79e25b9"
 
   url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.csv.first}%2B#{version.csv.second}/OpenJDK#{version.major}U-jdk_#{arch}_mac_hotspot_#{version.csv.first}_#{version.csv.second.major}.pkg",
       verified: "github.com/adoptium/"
