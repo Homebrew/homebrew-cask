9411ca69d92151d13de800adba2de23fe6e62a81
ivaquero <46377665+ivaquero@users.noreply.github.com>
2022-08-26 08:06:05 +0800

    Create qt-designer.rb

---
+++ Casks/qt-designer.rb
@@ -0,0 +1,18 @@
+cask "qt-designer" do
+  version :latest
+  sha256 :no_check
+
+  url "https://build-system.fman.io/static/public/files/Qt%20Designer.dmg"
+  name "Qt Designer"
+  desc "A tool for quickly building graphical user interfaces with widgets from the Qt GUI framework."
+  homepage "https://build-system.fman.io/qt-designer-download/"
+
+  depends_on macos: ">= :sierra"
+
+  app "Qt Designer.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.qtproject.Designer.plist",
+    "~/Library/Saved Application State/org.qt-project.Designer.savedState",
+  ]
+end
