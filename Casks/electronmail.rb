c0183f9fa28023897d935f5adec04c60e34da945
Patrick Linnane <patrick@linnane.io>
2022-08-27 08:45:00 -0700

    electronmail 5.1.0
    Add arm64, bump to 5.1.0
    
    Closes #130524.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/electronmail.rb
+++ Casks/electronmail.rb
@@ -1,11 +1,16 @@
 cask "electronmail" do
-  version "5.0.2"
-  sha256 "72365ccfe7e80d412f7fcb4a702699f2a0cba4a6d6239b9ec18116ec4db4f7ce"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://github.com/vladimiry/ElectronMail/releases/download/v#{version}/electron-mail-#{version}-mac-x64.dmg"
+  version "5.1.0"
+  sha256 arm:   "983e8d7ef24cc6efc32cc0606217522e23db399a8493f4bcd4dc3f2423a476a5",
+         intel: "ae227cbc3716c5afea3c3a5d450f382120f5ad796502c91bfd131483a45796a2"
+
+  url "https://github.com/vladimiry/ElectronMail/releases/download/v#{version}/electron-mail-#{version}-mac-#{arch}.dmg"
   name "ElectronMail"
   desc "Unofficial ProtonMail Desktop App"
   homepage "https://github.com/vladimiry/ElectronMail"
+
+  depends_on macos: ">= :high_sierra"
 
   app "ElectronMail.app"
 
