426de391db224767bdfb761fef204f34adb124d7
Patrick Linnane <patrick@linnane.io>
2022-08-27 09:45:19 -0700

    Update sonic3air from 22.05.15.0 to 22.08.27.0

--- Casks/sonic3air.rb
+++ Casks/sonic3air.rb
@@ -1,6 +1,6 @@
 cask "sonic3air" do
-  version "22.05.15.0"
-  sha256 "849cacb3ffccaa7d030ee9e36677e4c055a66481657fb950bdfbc727ded6815d"
+  version "22.08.27.0"
+  sha256 "0c764d2acc3107b7822482258eaac6c9dcae786b5355ae60371b25a66e020325"
 
   url "https://projects.sappharad.com/s3air_mac/sonic3air_mac_#{version.major_minor_patch.no_dots}.dmg",
       verified: "projects.sappharad.com/s3air_mac/"
