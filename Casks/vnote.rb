17f948d2e40b93dbd96e2c7c4bf69c3336888d94
Patrick Linnane <patrick@linnane.io>
2022-08-27 10:01:27 -0700

    Update vnote from 3.14.0 to 3.15.0

--- Casks/vnote.rb
+++ Casks/vnote.rb
@@ -1,8 +1,8 @@
 cask "vnote" do
-  version "3.14.0"
-  sha256 "23c1865b38939e92006510a3d1d83d0cb7dc80878389a842a529a99755df3afd"
+  version "3.15.0"
+  sha256 "fa44d0328dc6a430d46bb272e1d59509838d6d7439ce6a88dad5df6d3b21a89e"
 
-  url "https://github.com/vnotex/vnote/releases/download/v#{version}/vnote-mac-x64_v#{version}.zip",
+  url "https://github.com/vnotex/vnote/releases/download/v#{version}/vnote-mac-x64-qt5.15.2_v#{version}.zip",
       verified: "github.com/vnotex/vnote/"
   name "VNote"
   desc "Note-taking platform"
