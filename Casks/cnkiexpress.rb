371f6a1ea25c002e24df998673bbf29d59544a63
Patrick Linnane <patrick@linnane.io>
2022-08-27 08:10:58 -0700

    cnkiexpress 0.2.24
    Update cnkiexpress from 0.2.23 to 0.2.24
    
    Closes #130522.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/cnkiexpress.rb
+++ Casks/cnkiexpress.rb
@@ -1,6 +1,6 @@
 cask "cnkiexpress" do
-  version "0.2.23"
-  sha256 "3afa75fdf14d90dd9cac8057f7c68c38ec523f79dbad831d612b5bc1c019957c"
+  version "0.2.24"
+  sha256 "6ce9d8ec494a0c69fd052a72289941030c1c5a14d786df8eacdf1dd2e0a33804"
 
   url "https://download.cnki.net/cnkiexpress/\%E5\%85\%A8\%E7\%90\%83\%E5\%AD\%A6\%E6\%9C\%AF\%E5\%BF\%AB\%E6\%8A\%A5-#{version}.dmg"
   name "CNKIExpress"
