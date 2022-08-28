2fae08bcd56935b87ec62862077a380d57bb34b8
R3dSt0ne <62245695+R3dst0ne@users.noreply.github.com>
2022-08-29 01:39:34 +0200

    Update Logisim-evolution.app GitHub URL (#130565)
    * Update Logisim-evolution.app GitHub URL
    
    * Update logisim-evolution.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/logisim-evolution.rb
+++ Casks/logisim-evolution.rb
@@ -2,15 +2,10 @@ cask "logisim-evolution" do
   version "3.7.2"
   sha256 "e94ba07fa606c927e6ec68c6d276cd6decb5988f00639f36a6507e847a7c3342"
 
-  url "https://github.com/reds-heig/logisim-evolution/releases/download/v#{version}/Logisim-evolution-#{version}.dmg"
+  url "https://github.com/logisim-evolution/logisim-evolution/releases/download/v#{version}/Logisim-evolution-#{version}.dmg"
   name "Logisim Evolution"
   desc "Digital logic designer and simulator"
-  homepage "https://github.com/reds-heig/logisim-evolution"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
+  homepage "https://github.com/logisim-evolution/logisim-evolution"
 
   app "Logisim-evolution.app"
 
