fab295c791c67a8689e0815917446e9a22ad9b29
Patrick Linnane <patrick@linnane.io>
2022-08-28 10:45:06 -0700

    Update contexts from 3.8.1 to 3.9.0

--- Casks/contexts.rb
+++ Casks/contexts.rb
@@ -3,8 +3,8 @@ cask "contexts" do
     version "3.7.1,371"
     sha256 "de5e4a660cc30276155606b539d1ae58684115a3983d69598f1505fcad499a87"
   else
-    version "3.8.1,381"
-    sha256 "f4ca05c6abb14f6193b158645d74ca9d2672aabba7e07cc1c6cc3af18aa44907"
+    version "3.9.0,390"
+    sha256 "c86a214d5ee7718453d0fb6442b3ef9a7fc02c5c1582b6d28ef0c2e621a6b6aa"
   end
 
   url "https://contexts.co/releases/Contexts-#{version.csv.first}.dmg"
@@ -14,8 +14,15 @@ cask "contexts" do
 
   livecheck do
     url "https://contexts.co/appcasts/stable.xml"
-    strategy :sparkle
+    strategy :sparkle do |item|
+      match = item.url.match(/Contexts-(\d+(?:\.\d+)+)\.dmg/i)
+      next if match.blank?
+
+      "#{match[1]},#{item.version}"
+    end
   end
+
+  depends_on macos: ">= :catalina"
 
   app "Contexts.app"
 
