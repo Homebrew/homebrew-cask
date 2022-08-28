5593c9b8850bceb9cb27c407fb175c8b99730f23
Patrick Linnane <patrick@linnane.io>
2022-08-28 16:35:39 -0700

    Update hepta from 0.190.0 to 0.193.0 (#130568)

--- Casks/hepta.rb
+++ Casks/hepta.rb
@@ -1,9 +1,9 @@
 cask "hepta" do
   arch arm: "arm64-mac", intel: "mac"
 
-  version "0.190.0"
-  sha256 arm:   "045f193d230c04c316185c9e506099a955ca7d91a11a6a3303b808e86f371fe3",
-         intel: "0762214c9f376c0d1ba6f44cc05ee58712f44f7179552d37ce64ea6ee08c1707"
+  version "0.193.0"
+  sha256 arm:   "c63cfa2f9006b21c4e2b9bf0b868cec7572cd47bef11c3f736484c04ac981b3c",
+         intel: "8142f1b982f29a763298d06cc571ca68fae0ca8f47af041b536936c4e669de55"
 
   url "https://github.com/heptameta/project-meta/releases/download/v#{version}/Hepta-#{version}-#{arch}.zip",
       verified: "github.com/heptameta/project-meta/"
