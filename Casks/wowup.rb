91f5eed61b5f6a8f1af9690d12f52e0d8687038d
Markus Reiter <me@reitermark.us>
2022-08-28 04:44:28 +0200

    Update wowup.rb

--- Casks/wowup.rb
+++ Casks/wowup.rb
@@ -5,7 +5,7 @@ cask "wowup" do
   sha256 arm:   "5aca79b30fa90307c6d78b45207b1b7f277798596b7f94fd9464dd8ed0c9a6d5",
          intel: "3e6baf5aafacbbe69b484af26fa6c426ec326a527243807db66d24f6ec354dae"
 
-  url "https://github.com/WowUp/WowUp/releases/download/v#{version}/WowUp-#{version}-#{arch}.dmg",
+  url "https://github.com/WowUp/WowUp/releases/download/v#{version}/WowUp-#{version}-#{arch}.zip",
       verified: "github.com/WowUp/WowUp/"
   name "WowUp"
   desc "World of Warcraft addon manager"
