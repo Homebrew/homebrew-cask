0c81d9b7a727d8cc013f239912a52ac8f47f2fdc
Patrick Linnane <patrick@linnane.io>
2022-08-28 09:52:19 -0700

    Update phpwebstudy from 1.0.25 to 1.0.26

--- Casks/phpwebstudy.rb
+++ Casks/phpwebstudy.rb
@@ -1,6 +1,6 @@
 cask "phpwebstudy" do
-  version "1.0.25"
-  sha256 "11da6e1aae41ecda56cf1247710eb9131a116372d474b5d28b48847b443a00dc"
+  version "1.0.26"
+  sha256 "ecc8adf8c4d75be6fcc4e47362d25fa4f645d2a52488f6eb0d95852111b3d5f6"
 
   url "https://github.com/xpf0000/PhpWebStudy/releases/download/v#{version}/PhpWebStudy-#{version}-universal-mac.zip",
       verified: "github.com/xpf0000/PhpWebStudy/"
