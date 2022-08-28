94aff3cda773da0479d857b46f07bcd17a4fee23
Markus Reiter <me@reitermark.us>
2022-08-28 20:07:19 +0200

    Update contexts.rb

--- Casks/contexts.rb
+++ Casks/contexts.rb
@@ -1,13 +1,13 @@
 cask "contexts" do
   if MacOS.version <= :mojave
-    version "3.7.1,371"
+    version "3.7.1"
     sha256 "de5e4a660cc30276155606b539d1ae58684115a3983d69598f1505fcad499a87"
   else
-    version "3.9.0,390"
+    version "3.9.0"
     sha256 "c86a214d5ee7718453d0fb6442b3ef9a7fc02c5c1582b6d28ef0c2e621a6b6aa"
   end
 
-  url "https://contexts.co/releases/Contexts-#{version.csv.first}.dmg"
+  url "https://contexts.co/releases/Contexts-#{version}.dmg"
   name "Contexts"
   desc "Allows switching between application windows"
   homepage "https://contexts.co/"
@@ -15,10 +15,7 @@ cask "contexts" do
   livecheck do
     url "https://contexts.co/appcasts/stable.xml"
     strategy :sparkle do |item|
-      match = item.url.match(/Contexts-(\d+(?:\.\d+)+)\.dmg/i)
-      next if match.blank?
-
-      "#{match[1]},#{item.version}"
+      item.version.split("").join(".")
     end
   end
 
