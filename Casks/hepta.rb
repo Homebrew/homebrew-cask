b36021cf880df827d3d0ebf8f521772eec61c08c
Patrick Linnane <patrick@linnane.io>
2022-08-27 22:11:47 -0700

    hepta 0.190.0
    Update hepta from 0.188.2 to 0.190.0
    
    Closes #130544.
    
    Signed-off-by: Markus Reiter <me@reitermark.us>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/hepta.rb
+++ Casks/hepta.rb
@@ -1,9 +1,9 @@
 cask "hepta" do
   arch arm: "arm64-mac", intel: "mac"
 
-  version "0.188.2"
-  sha256 arm:   "626181026d3e57002eedd8a7500ecdf19b1eab9ad422dd69ae39f761078575d2",
-         intel: "7f0da05f01a3da8b346ff161484b1c701a94ee8cf1813415734a81f66ec2d017"
+  version "0.190.0"
+  sha256 arm:   "045f193d230c04c316185c9e506099a955ca7d91a11a6a3303b808e86f371fe3",
+         intel: "0762214c9f376c0d1ba6f44cc05ee58712f44f7179552d37ce64ea6ee08c1707"
 
   url "https://github.com/heptameta/project-meta/releases/download/v#{version}/Hepta-#{version}-#{arch}.zip",
       verified: "github.com/heptameta/project-meta/"
