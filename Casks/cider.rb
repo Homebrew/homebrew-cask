5061c8d25a322aa8ba432de1df1ebcce42822b6b
cryptofyre <cryptofyre@cryptofyre.org>
2022-08-29 14:14:40 -0500

    cider 1.5.6-beta.9
    * Update cider from 1.5.6-beta.49 to 1.5.6-beta.9
    * cider: update livecheck
    
    Closes #130625.
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>
    Signed-off-by: Sam Ford <1584702+samford@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/cider.rb
+++ Casks/cider.rb
@@ -1,6 +1,6 @@
 cask "cider" do
-  version "1.5.6-beta.49"
-  sha256 "3f1779b61260b13280d60a73915f69d44880bde85e66c6935135ae0cf601e9e4"
+  version "1.5.6-beta.9"
+  sha256 "855e86be22b2b402fdbd16b41ee8e7dabfebcd446ca68404bb3f2aae4c9d7c9b"
 
   url "https://github.com/ciderapp/cider-releases/releases/download/v#{version}/Cider-#{version}-universal.dmg",
       verified: "github.com/ciderapp/cider-releases"
@@ -9,9 +9,9 @@ cask "cider" do
   homepage "https://cider.sh/"
 
   livecheck do
-    url "https://github.com/ciderapp/cider-releases/releases?q=prerelease%3Afalse&expanded=true"
+    url :url
     regex(/href=.*?Cider[._-]v?(\d+(?:\.\d+)+[^"' >]*?)(?:[._-]universal)?\.dmg/i)
-    strategy :page_match
+    strategy :github_latest
   end
 
   auto_updates true
