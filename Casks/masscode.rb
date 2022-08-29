f08b4883d23dfceb12ff751d7d1792bab9791ebc
Patrick Linnane <patrick@linnane.io>
2022-08-29 10:04:04 -0700

    masscode 3.4.1
    Update masscode from 3.4.0 to 3.4.1
    
    Closes #130599.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/masscode.rb
+++ Casks/masscode.rb
@@ -1,9 +1,9 @@
 cask "masscode" do
   arch arm: "-arm64"
 
-  version "3.4.0"
-  sha256 arm:   "4822c7842bb73174c8edc96e08968cd85e437ef828acba109bab3329d65dc014",
-         intel: "d91eb25fa095837ddc3b975e2dc0ef097d3a375bee9bccaf5a04384f7a90f92d"
+  version "3.4.1"
+  sha256 arm:   "b9508b32b6b8606bd0bbcab482b9a75ef87186db181b48f1242c8ef83b6ee288",
+         intel: "622ed42307902b194bf223f6b33a6c2c0cef11d23480986dfa30cd1be128edd9"
 
   url "https://github.com/massCodeIO/massCode/releases/download/v#{version}/massCode-#{version}#{arch}.dmg",
       verified: "https://github.com/massCodeIO/massCode/"
