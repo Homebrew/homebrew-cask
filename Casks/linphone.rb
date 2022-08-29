7f73195011d2e7bcbd0466fb449967ec40a29aa8
Patrick Linnane <patrick@linnane.io>
2022-08-29 10:02:36 -0700

    linphone 4.4.9
    Update linphone from 4.4.8 to 4.4.9
    
    Closes #130597.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/linphone.rb
+++ Casks/linphone.rb
@@ -1,6 +1,6 @@
 cask "linphone" do
-  version "4.4.8"
-  sha256 "6d1beb3b43c541fad7c46bff58c9e06c4bc873cb68ca3c698609382b0d6a3bbb"
+  version "4.4.9"
+  sha256 "c096862c5c2029f1c2d87dc9b1d641e8f8a50d1f19a2ebb4ba3c8666a7432193"
 
   url "https://download.linphone.org/releases/macosx/app/Linphone-#{version}-mac.dmg"
   name "Linphone"
