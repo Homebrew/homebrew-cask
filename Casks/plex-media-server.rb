642804c8932e152ae9e10bea1aaa953c9874e0af
Patrick Linnane <patrick@linnane.io>
2022-08-30 09:26:38 -0700

    Update plex-media-server from 1.28.2.6106 to 1.28.2.6151

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -1,6 +1,6 @@
 cask "plex-media-server" do
-  version "1.28.2.6106,44a5bbd28"
-  sha256 "5772ebd50ee5a77178830474ad55c91551bdc081f27b183da7d7a4b51d5f7d82"
+  version "1.28.2.6151,914ddd2b3"
+  sha256 "261a4c9326b576ba817e71143dbce4d6a1f44b80e818488326e331a4e33e4ff8"
 
   url "https://downloads.plex.tv/plex-media-server-new/#{version.csv.first}-#{version.csv.second}/macos/PlexMediaServer-#{version.csv.first}-#{version.csv.second}-universal.zip"
   name "Plex Media Server"
