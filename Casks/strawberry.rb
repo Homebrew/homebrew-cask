1da52ef01dd5954abfb51fc021032e634608d356
Patrick Linnane <patrick@linnane.io>
2022-08-29 21:03:04 -0700

    strawberry 1.0.8
    Update strawberry from 1.0.7 to 1.0.8
    
    Closes #130642.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/strawberry.rb
+++ Casks/strawberry.rb
@@ -1,6 +1,6 @@
 cask "strawberry" do
-  version "1.0.7"
-  sha256 "0b8d1bbf41747d7d7a26bcee9bb5a5b9d098b2e6be43e7833fdecb541fb74780"
+  version "1.0.8"
+  sha256 "41daea7651e3bfad7feffb578a1f32d625df72d6db132c78baafa90654d059b7"
 
   url "https://github.com/strawberrymusicplayer/strawberry/releases/download/#{version}/strawberry-#{version}-x86_64.dmg",
       verified: "github.com/strawberrymusicplayer/strawberry/"
