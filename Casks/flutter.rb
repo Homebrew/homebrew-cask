2e81ac7fb97d627fc662cc9cd8f2f511cc3f9e04
Patrick Linnane <patrick@linnane.io>
2022-08-30 11:20:53 -0700

    flutter 3.3.0
    * Update flutter from 3.0.5 to 3.3.0
    * Fix Intel sha256
    
    Closes #130702.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -1,9 +1,9 @@
 cask "flutter" do
   arch arm: "_arm64_", intel: "_"
 
-  version "3.0.5"
-  sha256 arm:   "d1d09e8d3647729338446965f63a59f3400b4a3f04efbcc9040628def81e6ecf",
-         intel: "e79a04dcfd1b583e5831433fc200800ba0d1e9fe4567cb661479bd2542d4c685"
+  version "3.3.0"
+  sha256 arm:   "ad3b620aef65d43f54c5432a7f3a72304de639c7cf393a8a8796aa1094476b2e",
+         intel: "f461df043b4a1b2dedcf31c11db5e5286b2b014804d2df445b85c6bb6ce0844b"
 
   url "https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos#{arch}#{version}-stable.zip",
       verified: "storage.googleapis.com/flutter_infra_release/releases/stable/macos/"
