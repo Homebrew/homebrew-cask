e5dae2209da5d9bf90def53daeb5e297dcdadc63
Matan Kushner <hello@matchai.dev>
2022-08-29 19:28:54 -0500

    Add GameMaker

---
+++ Casks/gamemaker.rb
@@ -0,0 +1,16 @@
+cask "gamemaker" do
+  version "2022.6.1.26"
+  sha256 "6d9414b73bbbd162d0277b07ef507ae3c78fe6307351f0883e04bfb2e586f036"
+
+  url "https://gms.yoyogames.com/GameMaker-#{version}.pkg"
+  name "GameMaker"
+  desc "A complete development tool for making 2D games, used by indie developers, professional studios, and educators worldwide"
+  homepage "https://gamemaker.io"
+
+  livecheck do
+    url "https://gms.yoyogames.com/update-mac.rss"
+    strategy :sparkle
+  end
+
+  pkg "GameMaker-#{version}.pkg"
+end
