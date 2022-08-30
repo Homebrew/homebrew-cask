51ffcacac6a0662e8aec7d25e45e232e9e04ea2c
yiNiPui <callmeliuweiqi@gmail.com>
2022-08-30 21:18:44 +0800

    Update listen1 from 2.24.0 to 2.25.0

--- Casks/listen1.rb
+++ Casks/listen1.rb
@@ -2,9 +2,9 @@ cask "listen1" do
   # NOTE: "1" is not a version number, but an intrinsic part of the product name
   arch arm: "arm64", intel: "x64"
 
-  version "2.24.0"
-  sha256 arm:   "8fd3e5c832dbdd02cbb9c68edcc902dd6749e67ea62d4d98ff155890c2aee954",
-         intel: "2327464903b24cdd620d1ecbad8e039cd1f6f200f4286b05d6d799e7cc125a29"
+  version "2.25.0"
+  sha256 arm:   "57bde2113ede517836817f8d5a8beee891206fd2556c9acbf03a6072467d2033",
+         intel: "69d40d11c5a050da83d8b66b84df22f7692436cf566cf19f61d953ac1e8de271"
 
   url "https://github.com/listen1/listen1_desktop/releases/download/v#{version}/Listen1_#{version}_mac_#{arch}.dmg",
       verified: "github.com/listen1/listen1_desktop/"
