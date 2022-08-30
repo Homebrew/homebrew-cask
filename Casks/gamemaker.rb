9ca9d264ec18c310ed3acd4e9e682a17c7f4977d
Matan Kushner <hello@matchai.dev>
2022-08-29 19:40:03 -0500

    Add missing uninstall stanza

--- Casks/gamemaker.rb
+++ Casks/gamemaker.rb
@@ -2,10 +2,11 @@ cask "gamemaker" do
   version "2022.6.1.26"
   sha256 "6d9414b73bbbd162d0277b07ef507ae3c78fe6307351f0883e04bfb2e586f036"
 
-  url "https://gms.yoyogames.com/GameMaker-#{version}.pkg"
+  url "https://gms.yoyogames.com/GameMaker-#{version}.pkg",
+      verified: "https://gms.yoyogames.com/"
   name "GameMaker"
-  desc "A complete development tool for making 2D games, used by indie developers, professional studios, and educators worldwide"
-  homepage "https://gamemaker.io"
+  desc "Complete development tool for making 2D games, used by indie developers, professional studios, and educators worldwide"
+  homepage "https://gamemaker.io/"
 
   livecheck do
     url "https://gms.yoyogames.com/update-mac.rss"
@@ -13,4 +14,6 @@ cask "gamemaker" do
   end
 
   pkg "GameMaker-#{version}.pkg"
+
+  uninstall pkgutil: "com.yoyogames.gms2"
 end
