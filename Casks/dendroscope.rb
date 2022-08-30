8881506fc4f56c46f5cae08020be4d5b4a71930e
Patrick Linnane <patrick@linnane.io>
2022-08-30 09:00:02 -0700

    Update dendroscope from 3.8.3 to 3.8.4

--- Casks/dendroscope.rb
+++ Casks/dendroscope.rb
@@ -1,6 +1,6 @@
 cask "dendroscope" do
-  version "3.8.3"
-  sha256 "680e8ff29accf91c5b154021956a22ff73ea6ba99400e929d8e2739459ca8377"
+  version "3.8.4"
+  sha256 "b07ec4c0c88f700414b6cd56d84254d6e93119c2154e3108c710fe84b69e0c00"
 
   url "https://software-ab.informatik.uni-tuebingen.de/download/dendroscope/Dendroscope_macos_#{version.dots_to_underscores}.dmg"
   name "Dendroscope"
