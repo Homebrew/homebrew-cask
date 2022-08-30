e8371f89951962e4e9f61ce2563cc2a9c4ae0f1e
jd calvelli <jdcalvelli@jds-macbook-air.lan>
2022-08-29 22:41:23 -0400

    update inform.rb cask to match style guide

--- Casks/inform.rb
+++ Casks/inform.rb
@@ -3,7 +3,7 @@ cask "inform" do
   sha256 "74cb35f6352821b94322cb990e9bc9129e7e4449f8e7322422d875df80657cd5"
 
   url "https://github.com/ganelson/inform/releases/download/v#{version}/Inform_#{version.dots_to_underscores}_macOS_1_82_1.dmg",
-       verified: "github.com/ganelson/inform"
+      verified: "github.com/ganelson/inform"
   name "Inform"
   desc "Writing system for interactive fiction based on natural language"
   homepage "https://ganelson.github.io/inform-website"
