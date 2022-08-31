15acfc6eb4d659912c716af200baaf27e5a68aeb
Patrick Linnane <patrick@linnane.io>
2022-08-30 19:29:01 -0700

    Update splashtop-business from 3.5.2.0 to 3.5.2.1

--- Casks/splashtop-business.rb
+++ Casks/splashtop-business.rb
@@ -1,6 +1,6 @@
 cask "splashtop-business" do
-  version "3.5.2.0"
-  sha256 "20340bc84de33144f21a9d433b0b1b3264376208987c7b6541a75dbd56f9bf97"
+  version "3.5.2.1"
+  sha256 "e406d230b8d174ac2f98a6fbfeb8a402c57c9bbbb0a92b46856e15ec20221fc0"
 
   url "https://d17kmd0va0f0mp.cloudfront.net/macclient/STB/Splashtop_Business_Mac_INSTALLER_v#{version}.dmg",
       verified: "d17kmd0va0f0mp.cloudfront.net/macclient/STB/"
