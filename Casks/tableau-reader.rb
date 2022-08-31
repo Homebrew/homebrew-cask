2a35f50938b2124d45fd379e89cda2d8d71712b0
Patrick Linnane <patrick@linnane.io>
2022-08-30 19:37:15 -0700

    tableau-reader 2022.2.1
    Update tableau-reader from 2022.2.0 to 2022.2.1
    
    Closes #130736.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/tableau-reader.rb
+++ Casks/tableau-reader.rb
@@ -1,6 +1,6 @@
 cask "tableau-reader" do
-  version "2022.2.0"
-  sha256 "fe09d1964fe4ac5dca05089873abbcc05bb1ef059e0e2fd3bd5d4bf34a12c712"
+  version "2022.2.1"
+  sha256 "d9b3923a216210d2db728c3fd812460aa1e1639cd497d6f50cc47032b5752644"
 
   url "https://downloads.tableau.com/tssoftware/TableauReader-#{version.dots_to_hyphens}.dmg"
   name "Tableau Reader"
