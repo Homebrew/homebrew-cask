925dfe8486f5d2848bc14fecc4f42a16390b00b2
Bevan Kay <email@bevankay.me>
2022-08-31 10:35:26 +1000

    Update logos from 9.15.0.0005 to 9.16.0.0006 (#130713)

--- Casks/logos.rb
+++ Casks/logos.rb
@@ -1,6 +1,6 @@
 cask "logos" do
-  version "9.15.0.0005"
-  sha256 "b1896eadd955e94bcdc028a48ad1e0a686cf911489d2b2f11d5de103021c2cd9"
+  version "9.16.0.0006"
+  sha256 "cb09f2a515c4d5194645de2f6af08435de5dc61dde3c92e6f92354fb2931271d"
 
   url "https://downloads.logoscdn.com/LBS#{version.major}/Installer/#{version}/LogosMac.dmg",
       verified: "downloads.logoscdn.com/"
