a1ac6493a8d04bc7f9d6bec74f2bd8a8e2628387
Aaron Jheng <wentworth@outlook.com>
2022-08-31 19:54:35 +0800

    wiznote 0.1.85
    Update wiznote from 0.1.85 to 0.1.85
    
    Closes #130742.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/wiznote.rb
+++ Casks/wiznote.rb
@@ -1,9 +1,9 @@
 cask "wiznote" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.1.84"
-  sha256 arm:   "30bb2e59f779393d5025993c3399ddd81ff7d877953ab922ec988858543e1e12",
-         intel: "1e9e867d6d49ba5b482d47e3ad9d610d9630ab2501626de65364141adb758dc7"
+  version "0.1.85"
+  sha256 arm:   "f152bb8e6d7814b89bbcf8f0a1ba5902c756e96ca8a35adf000561dea31ccc86",
+         intel: "6c5da2556ce74722822983916b696009be4f3f233d942417bdf25163f69bbf88"
 
   url "https://get.wiz.cn/x/wiznote-desktop-#{version}-mac-#{arch}.dmg"
   name "WizNote"
