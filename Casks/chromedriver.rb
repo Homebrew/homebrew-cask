1b725fbdb9af09b20c84a7d130dc6a6e457be3de
Patrick Linnane <patrick@linnane.io>
2022-08-31 09:07:00 -0700

    Update chromedriver from 104.0.5112.79 to 105.0.5195.52

--- Casks/chromedriver.rb
+++ Casks/chromedriver.rb
@@ -1,9 +1,9 @@
 cask "chromedriver" do
   arch arm: "mac64_m1", intel: "mac64"
 
-  version "104.0.5112.79"
-  sha256 arm:   "420d159af6b441f21a2c92c159cf817cdfc6a8069eb7fdc2822bfe625ac33287",
-         intel: "65766e1c5cecf0e560cfb602bdc62e181d89a35258cf2dfbbb2a8cad37d2f451"
+  version "105.0.5195.52"
+  sha256 arm:   "7e851af7aef841f5a4c231e089db29be700b1daca3ddc71368d51c2634c96593",
+         intel: "c0e2982c36f02216a4eaaaff2672b6aa702bbc2b904b00e4ca736c708b784867"
 
   url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_#{arch}.zip",
       verified: "chromedriver.storage.googleapis.com/"
