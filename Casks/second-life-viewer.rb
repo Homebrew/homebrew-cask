fd1596221b7a8697eff54cae7af34711bb30ecd0
Patrick Linnane <patrick@linnane.io>
2022-08-30 19:27:47 -0700

    second-life-viewer 6.6.3.574158
    Update second-life-viewer from 6.6.2.573358 to 6.6.3.574158
    
    Closes #130729.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/second-life-viewer.rb
+++ Casks/second-life-viewer.rb
@@ -1,6 +1,6 @@
 cask "second-life-viewer" do
-  version "6.6.2.573358"
-  sha256 "36f94de036ee0382d100f597d1527e231946bd67602aeba720a320fa8ef5a668"
+  version "6.6.3.574158"
+  sha256 "ab67b99275cc12a7192c2260425fec691affb8b8f508f87efac9cc544e0ae521"
 
   url "http://download.cloud.secondlife.com/Viewer_#{version.major}/Second_Life_#{version.dots_to_underscores}_x86_64.dmg"
   name "Linden Lab Second Life Viewer"
