e9b81272d140376fb0fff18f80fb0dc0bec7a366
Patrick Linnane <patrick@linnane.io>
2022-08-30 19:28:39 -0700

    sonic-pi 4.1.0
    Update sonic-pi from 4.0.3 to 4.1.0
    
    Closes #130730.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/sonic-pi.rb
+++ Casks/sonic-pi.rb
@@ -1,6 +1,6 @@
 cask "sonic-pi" do
-  version "4.0.3"
-  sha256 "68c31ce8679759aac565794d962970b86cdd36dce5b59c9d92c9a8810bec2119"
+  version "4.1.0"
+  sha256 "cf3cee887ddfe9e436b48167116e17e21a1b57fbce685697fc77b0736305d67a"
 
   url "https://sonic-pi.net/files/releases/v#{version}/Sonic-Pi-for-Mac-x64-v#{version.dots_to_hyphens}.dmg"
   name "Sonic Pi"
