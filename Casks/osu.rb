2fd6aedc2e0240a3797af7fc1c4bcdd7ef271dda
Zhenfu Shi <szf1234@me.com>
2022-09-01 18:39:48 -0400

    Update osu from 2022.901.0 to 2022.902.1 (#130835)

--- Casks/osu.rb
+++ Casks/osu.rb
@@ -1,9 +1,9 @@
 cask "osu" do
   arch arm: "Apple.Silicon", intel: "Intel"
 
-  version "2022.901.0"
-  sha256 arm:   "a356da040add25291f88d38444643f2b84c415331a848c29c0d72005909a556b",
-         intel: "33bd765bfef61f5bbe78bdf1b9578d096b90bb4d55b14f0ffcd0b3651dd6f0d1"
+  version "2022.902.1"
+  sha256 arm:   "c174aa4fec6df105eac76faf376d7e1788b154880e85bc4ca0ddd9e09fe32a85",
+         intel: "2853c498876c3a30e853f725e4423426f418e2ed7e892a2d157628e0e3ee5268"
 
   url "https://github.com/ppy/osu/releases/download/#{version}/osu.app.#{arch}.zip"
   name "osu!"
