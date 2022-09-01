c454a61cd392ac4fd776299cc9deba73ccfa40cf
Vit Kabele <vit@kabele.me>
2022-09-01 16:53:43 +0200

    Basecamp: Add Apple Silicon version
    Signed-off-by: Vit Kabele <vit@kabele.me>

--- Casks/basecamp.rb
+++ Casks/basecamp.rb
@@ -2,14 +2,21 @@ cask "basecamp" do
   version "3,2.3.5"
   sha256 "7553020b9406cc59fa0d76b009a1721983e4c0c305f402c0b865d19a772d87b8"
 
-  url "https://bc#{version.major}-desktop.s3.amazonaws.com/mac/basecamp#{version.major}-#{version.csv.second}.zip",
+  arch_suffix = ""
+
+  on_arm do
+    arch_suffix = "_arm64"
+    sha256 "adfdb4d99790d4cab594da29305b753b81711757c94cb5e23aed12dbaa65fbfa"
+  end
+
+  url "https://bc#{version.major}-desktop.s3.amazonaws.com/mac#{arch_suffix}/basecamp#{version.major}-#{version.csv.second}.zip",
       verified: "bc3-desktop.s3.amazonaws.com/"
   name "Basecamp"
   desc "All-In-One Toolkit for Working Remotely"
   homepage "https://basecamp.com/help/#{version}/guides/apps/mac"
 
   livecheck do
-    url "https://bc#{version.major}-desktop.s3.amazonaws.com/mac/updates.json"
+    url "https://bc#{version.major}-desktop.s3.amazonaws.com/mac#{arch_suffix}/updates.json"
     strategy :page_match do |page|
       minor_version = JSON.parse(page)["version"]
       major_version = page.match(/basecamp(\d)/i)
