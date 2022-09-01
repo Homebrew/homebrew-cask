c84ac860c1cec26cbb9719f134e34757f108ea15
owine <owine@users.noreply.github.com>
2022-09-01 18:58:26 -0500

    Update microsoft-edge from 104.0.1293.70 to 105.0.1343.25

--- Casks/microsoft-edge.rb
+++ Casks/microsoft-edge.rb
@@ -3,9 +3,9 @@ cask "microsoft-edge" do
                                intel: "C1297A47-86C4-4C1F-97FA-950631F94777"
   linkid = on_arch_conditional arm: "2093504", intel: "2069148"
 
-  version "104.0.1293.70"
-  sha256 arm:   "90d5b8d8a345ab69c9fd353ff7a1b28986f19189593f264a25276151b96f6e78",
-         intel: "18e1861cd4c51d7b6877f5d9f492feb5c34fcfad1f5165773024109cf483e166"
+  version "105.0.1343.25"
+  sha256 arm:   "d602bec28b926cf0748b018cb9465a52bb39d22414407853acb9fe51fb104d82",
+         intel: "634d845da1ad04a71fc1dcb43b3bd06a4619504b32fc09998dd1abb034283fef"
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/#{folder}/MacAutoupdate/MicrosoftEdge-#{version}.pkg",
       verified: "officecdn-microsoft-com.akamaized.net/"
