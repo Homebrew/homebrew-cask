60a9f1757cef58f94bc044b409dde75e676a15f2
Bevan Kay <email@bevankay.me>
2022-09-03 01:00:05 +1000

    Update burp-suite-professional from 2022.8.2 to 2022.8.3

--- Casks/burp-suite-professional.rb
+++ Casks/burp-suite-professional.rb
@@ -1,9 +1,9 @@
 cask "burp-suite-professional" do
   arch arm: "MacOsArm64", intel: "MacOsx"
 
-  version "2022.8.2"
-  sha256 arm:   "e5c429dc2afb111e66b60a8d337eb70eecac40ff1f01771e278daef7f4ecc99e",
-         intel: "04b2cd9ae11820e648c9eda99be39f3fd8d7843c96c2001e5b5d2741cb45fcc3"
+  version "2022.8.3"
+  sha256 arm:   "ee92d2d4796021b355afb6092171185c8b1302e3ce0faf7a17499c7cccdc124a",
+         intel: "bde745b662f9435351af25529f939f629ff4b511271771a3e4e0f19c61ac31f8"
 
   url "https://portswigger.net/burp/releases/download?product=pro&version=#{version}&type=#{arch}"
   name "Burp Suite Professional"
