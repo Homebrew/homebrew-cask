ef5d988c57bef94c5ecac1497cd89f51ef9111db
Patrick Linnane <patrick@linnane.io>
2022-09-01 22:07:54 -0700

    opera-gx 90.0.4480.78
    Update opera-gx from 90.0.4480.64 to 90.0.4480.78
    
    Closes #130892.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/opera-gx.rb
+++ Casks/opera-gx.rb
@@ -1,6 +1,6 @@
 cask "opera-gx" do
-  version "90.0.4480.64"
-  sha256 "2440860d7711f0d0a274df21e4404151eb8f4679a1b40d3660ce18b0f60bb955"
+  version "90.0.4480.78"
+  sha256 "c5e4c37bc82fbe39ed65b74486c2a3033f064efd7613f538654c84e3ebd9ee4a"
 
   url "https://get.geo.opera.com/pub/opera_gx/#{version}/mac/Opera_GX_#{version}_Setup.dmg"
   name "Opera GX"
