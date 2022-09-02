9629353b977065f721526ac132ae13bd6dc5df3c
Patrick Linnane <patrick@linnane.io>
2022-09-02 11:08:51 -0700

    Update superproductivity from 7.11.6 to 7.12.0

--- Casks/superproductivity.rb
+++ Casks/superproductivity.rb
@@ -1,9 +1,9 @@
 cask "superproductivity" do
   arch arm: "-arm64"
 
-  version "7.11.6"
-  sha256 arm:   "edb1f77a3642c37970637f0059336dd35234fd191d49399fdc9ac7169bbe4e83",
-         intel: "359836501f40ba8b7a02af1fec5a9dacddf60780116d63a8fb1bb9e3f27a1f94"
+  version "7.12.0"
+  sha256 arm:   "a4583ddbfc595286e9076b0463c7e2bf36b6711e02609f6f6894129b8c0538ac",
+         intel: "d7509cf6d42a4b12902529246497d82f3eda1bddf8bf6de053bd37edc872d036"
 
   url "https://github.com/johannesjo/super-productivity/releases/download/v#{version}/superProductivity-#{version}#{arch}.dmg",
       verified: "github.com/johannesjo/super-productivity/"
