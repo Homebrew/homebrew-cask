619ab1874f7a95c8eb9d3ef0c933ae65f28a0bc0
Patrick Linnane <patrick@linnane.io>
2022-09-01 21:37:13 -0700

    dcp-o-matic-disk-writer 2.16.22
    Update dcp-o-matic-disk-writer from 2.16.18 to 2.16.22
    
    Closes #130870.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/dcp-o-matic-disk-writer.rb
+++ Casks/dcp-o-matic-disk-writer.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-disk-writer" do
-  version "2.16.18"
-  sha256 "9c8b6d172c16db5d4397e42cebd63d082fdf1353697c06071082a912da4d519f"
+  version "2.16.22"
+  sha256 "dcce7786284c9857689dbeb349ec9e968e2732ae93df8fcfeef1a07e5dd4b860"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-disk&version=#{version}"
   name "DCP-o-matic Disk Writer"
