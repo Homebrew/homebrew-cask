8aa448a5d7c70d7a2d1868508958e32d9b7c6379
Patrick Linnane <patrick@linnane.io>
2022-09-01 21:35:30 -0700

    dcp-o-matic-batch-converter 2.16.22
    Update dcp-o-matic-batch-converter from 2.16.18 to 2.16.22
    
    Closes #130868.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/dcp-o-matic-batch-converter.rb
+++ Casks/dcp-o-matic-batch-converter.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-batch-converter" do
-  version "2.16.18"
-  sha256 "a4b57b1159778f8d29512aede22cbb99a59a8888f05d41baffd41d1af057b191"
+  version "2.16.22"
+  sha256 "56e273680765d631fb047524efc4aca55c5a486cc42e055d5f671cce4004cc69"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-batch&version=#{version}"
   name "DCP-o-matic Batch converter"
