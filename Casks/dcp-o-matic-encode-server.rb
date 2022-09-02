cb8033922df055bd4574d7d0bab483de4d1e22b9
Patrick Linnane <patrick@linnane.io>
2022-09-01 21:37:58 -0700

    dcp-o-matic-encode-server 2.16.22
    Update dcp-o-matic-encode-server from 2.16.18 to 2.16.22
    
    Closes #130871.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/dcp-o-matic-encode-server.rb
+++ Casks/dcp-o-matic-encode-server.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-encode-server" do
-  version "2.16.18"
-  sha256 "1393b180ab80a5d29c6794c3030c836a4b179c1e298d4166f66b6ec79217da74"
+  version "2.16.22"
+  sha256 "531c3323931cd3364e5c1332182b87ff1937dea0ebe76bdedc5861e38d47ab0f"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-server&version=#{version}"
   name "DCP-o-matic Encode Server"
