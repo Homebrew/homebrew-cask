6184bc78a6520ada560956b798e7a5da4b5da2d1
Patrick Linnane <patrick@linnane.io>
2022-09-01 21:32:52 -0700

    dcp-o-matic 2.16.22
    Update dcp-o-matic from 2.16.18 to 2.16.22
    
    Closes #130866.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/dcp-o-matic.rb
+++ Casks/dcp-o-matic.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic" do
-  version "2.16.18"
-  sha256 "da1816a7290e7e17bbc784beca49a4b4ad964fc747f6585c2a4999621a4ec30d"
+  version "2.16.22"
+  sha256 "f6167c12bfc29eed6b44a44b1408d023fad90e26052d0df58e99cbce93b5ac9f"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-main&version=#{version}"
   name "DCP-o-matic"
