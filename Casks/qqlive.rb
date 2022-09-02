0af64ef94910afc295f5c5a58d321eae9c238954
Patrick Linnane <patrick@linnane.io>
2022-09-02 11:03:36 -0700

    qqlive 2.53.0.53435
    Update qqlive from 2.52.2.53416 to 2.53.0.53435
    
    Closes #130954.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/qqlive.rb
+++ Casks/qqlive.rb
@@ -1,6 +1,6 @@
 cask "qqlive" do
-  version "2.52.2.53416"
-  sha256 "36d56978ac4073f1edfae4ec91f2ad6336366b34d6b3d38b913e0b57e429c2dd"
+  version "2.53.0.53435"
+  sha256 "77601f21c07e11ecb9c7dd93af0cb5cacb1d2f73f209278fdf60ecfc1479517e"
 
   url "https://dldir1.qq.com/qqtv/mac/QQLive_distribute_universal_#{version}.dmg"
   name "QQLive"
