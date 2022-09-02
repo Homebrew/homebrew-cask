6c9645975a8ddbd1baf9b54231dd68cdb6e51120
Patrick Linnane <patrick@linnane.io>
2022-09-01 22:17:44 -0700

    sigmaos 0.26.0.2
    Update sigmaos from 0.26.0.1 to 0.26.0.2
    
    Closes #130904.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/sigmaos.rb
+++ Casks/sigmaos.rb
@@ -1,6 +1,6 @@
 cask "sigmaos" do
-  version "0.26.0.1"
-  sha256 "4813db61b2b5b59848febe0f1b12cdd73188665fbeefce6af972a7b00c768c98"
+  version "0.26.0.2"
+  sha256 "b0dbe34b62f8903fc5e2882dc988004d2135f6dff323393b773fccd9aadd52b1"
 
   url "https://releases.sigmaos.com/SigmaOS-#{version}.dmg"
   name "sigmaos"
