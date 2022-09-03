97dce763f2b6689d305127cb74c7c97ebd744233
Patrick Linnane <patrick@linnane.io>
2022-09-03 09:14:16 -0700

    Update igv from 2.14.0 to 2.14.1

--- Casks/igv.rb
+++ Casks/igv.rb
@@ -1,6 +1,6 @@
 cask "igv" do
-  version "2.14.0"
-  sha256 "a6d9220699563621068e8487faad67a3034ea0fbf94d83045bf92fba6ee8cf52"
+  version "2.14.1"
+  sha256 "7e1f29e120656501d3b21890f9265398147a2636579050b8fab83dd06bc83ea8"
 
   url "https://data.broadinstitute.org/igv/projects/downloads/#{version.major_minor}/IGV_MacApp_#{version}.zip"
   name "Integrative Genomics Viewer (IGV)"
