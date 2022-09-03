941190c795a846182fc384ce757a17fad87ccf18
yuusharo <36213375+yuusharo@users.noreply.github.com>
2022-09-03 02:42:00 -0700

    Update fxfactory from 7.2.8,6999 to 8.0.3,7195 (#130981)
    * Update fxfactory from 7.2.8,6199 to 8.0.3,7195
    
    Updated url to new syntax
    
    * Update fxfactory from 7.2.8,6999 to 8.0.3,7195
    
    fixed url format
    
    * Update fxfactory from 7.2.8,6999 to 8.0.3,7195
    
    fixed livecheck regex
    
    * Update fxfactory from 7.2.8,6199 to 8.0.3,7195
    
    fixed livecheck regex hopefully
    
    * Update fxfactory from 7.2.8,6999 to 8.0.3,7195
    
    livecheck regex fix #2 (use hard hyphens instead of classes)
    
    * Update fxfactory.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/fxfactory.rb
+++ Casks/fxfactory.rb
@@ -1,8 +1,8 @@
 cask "fxfactory" do
-  version "7.2.8,6999"
-  sha256 "4f0ec77c2fa9cb3483db3e66f75b256d7f82ac5f292d1825f6ccaf37db9a304b"
+  version "8.0.3,7195"
+  sha256 "f42e82a8e40cf3add57375f1b47f65bb1e5cda7858e2853a82b6da2e6a060b29"
 
-  url "https://fxfactory.s3.amazonaws.com/noiseindustries/fxfactory/FxFactory_#{version.csv.first.no_dots}_#{version.csv.second}.zip",
+  url "https://fxfactory.s3.amazonaws.com/noiseindustries/fxfactory/FxFactory-#{version.csv.first}-#{version.csv.second}.zip",
       verified: "fxfactory.s3.amazonaws.com/noiseindustries/"
   name "FxFactory"
   desc "Browse, install and purchase effects and plugins from a huge catalog"
@@ -10,12 +10,12 @@ cask "fxfactory" do
 
   livecheck do
     url "https://fxfactory.com/download/"
-    regex(/FxFactory_\d+_(\d+)\.zip.*FxFactory\s(\d+(?:\.\d+)+)/i)
+    regex(/FxFactory[._-]v?(\d+(?:\.\d+)+)[._-](\d+).zip/i)
     strategy :page_match do |page, regex|
       match = page.match(regex)
       next if match.blank?
 
-      "#{match[2]},#{match[1]}"
+      "#{match[1]},#{match[2]}"
     end
   end
 
