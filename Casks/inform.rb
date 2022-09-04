8b39557539c2e07d3b6ec0f64563b2398c77aaf7
Patrick Linnane <patrick@linnane.io>
2022-09-03 22:05:47 -0700

    Add livecheck

--- Casks/inform.rb
+++ Casks/inform.rb
@@ -1,12 +1,24 @@
 cask "inform" do
-  version "10.1.1"
-  sha256 "74cb35f6352821b94322cb990e9bc9129e7e4449f8e7322422d875df80657cd5"
+  version "10.1.2,1_82_2"
+  sha256 "aacb91f097850da3138e040f5c2fbfa467efc22e49db9038be64761648b81648"
 
-  url "https://github.com/ganelson/inform/releases/download/v#{version}/Inform_#{version.dots_to_underscores}_macOS_1_82_1.dmg",
+  url "https://github.com/ganelson/inform/releases/download/v#{version.csv.first}/Inform_#{version.csv.first.dots_to_underscores}_macOS_#{version.csv.second}.dmg",
       verified: "github.com/ganelson/inform"
   name "Inform"
   desc "Writing system for interactive fiction based on natural language"
   homepage "https://ganelson.github.io/inform-website"
+
+  livecheck do
+    url "https://github.com/ganelson/inform/releases/"
+    strategy :page_match do |page|
+      match = page.match(%r{href=.*?/v?(\d+(?:\.\d+)+)/.*?macOS[._-](\d+(?:_\d+)+)\.dmg}i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
+  end
+
+  depends_on macos: ">= :mojave"
 
   app "Inform.app"
 end
