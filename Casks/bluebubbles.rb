76c67a1844a20ed279ebcb601e7134daaed35a8b
Josh Hagins <hagins.josh@gmail.com>
2022-09-03 21:28:11 -0400

    Add BlueBubbles 1.3.0 (#130976)
    * Add BlueBubbles 1.2.3
    
    * Add verified param
    
    * Add zap stanza
    
    * Bump to 1.3.0
    
    * Add uninstall stanza
    
    * Fix syntax and style

---
+++ Casks/bluebubbles.rb
@@ -0,0 +1,32 @@
+cask "bluebubbles" do
+  version "1.3.0"
+  sha256 "8c75146268bc92fea080c7eb8cca4b6e30b3c7e44d1fbdfc6fac2315de41039e"
+
+  url "https://github.com/BlueBubblesApp/bluebubbles-server/releases/download/v#{version}/BlueBubbles-#{version}.dmg",
+      verified: "github.com/BlueBubblesApp/bluebubbles-server"
+  name "BlueBubbles"
+  desc "Server for forwarding iMessages to clients within the BlueBubbles App ecosystem"
+  homepage "https://bluebubbles.app/"
+
+  app "BlueBubbles.app"
+
+  uninstall launchctl:  "com.BlueBubbles.BlueBubbles-Server.ShipIt",
+            login_item: "BlueBubbles",
+            quit:       [
+              "com.BlueBubbles.BlueBubbles-Server",
+              "com.BlueBubbles.BlueBubbles-Server.helper",
+              "com.BlueBubbles.BlueBubbles-Server.helper.GPU",
+              "com.BlueBubbles.BlueBubbles-Server.helper.Plugin",
+              "com.BlueBubbles.BlueBubbles-Server.helper.Renderer",
+              "com.bluebubbles.messaging",
+            ]
+
+  zap trash: [
+    "~/Library/Application Support/@bluebubbles",
+    "~/Library/Application Support/bluebubbles-server",
+    "~/Library/Logs/@bluebubbles",
+    "~/Library/Logs/bluebubbles-server",
+    "~/Library/Preferences/com.BlueBubbles.BlueBubbles-Server.plist",
+    "~/Library/Saved Application State/com.BlueBubbles.BlueBubbles-Server.savedState",
+  ]
+end
