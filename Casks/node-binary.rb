ff4057e6851a35bd927c74e703ee2d01f6326654
jylin <jlin@justinlin.net>
2022-09-04 12:05:47 -0400

    Add node-binary v18.8.0
    Add cask for NodeJS binaries for users who may not wish to compile from source. This includes node, npm, npx, corepack.

---
+++ Casks/node-binary.rb
@@ -0,0 +1,22 @@
+cask "node-binary" do
+  arch arm: "arm64", intel: "x64"
+
+  version "18.8.0"
+  sha256 arm:   "a03cb97533447a5005accef871b899df0e9da33d8a805675ac53715a534b3dcb",
+         intel: "f8527a1820f50a5f4c835d933e5c5318c4f93f7382294db5875791e2cb0cc4fa"
+
+  url "https://nodejs.org/dist/v#{version}/node-v#{version}-darwin-#{arch}.tar.xz"
+  name "Node.js"
+  desc "Platform built on V8 to build network applications"
+  homepage "https://nodejs.org/"
+
+  livecheck do
+    url "https://nodejs.org/en/download/current"
+    regex(/node-v(\d+(?:\.\d+)+)\.pkg/i)
+  end
+
+  binary "node-v#{version}-darwin-#{arch}/bin/node", target: "node-binary"
+  binary "node-v#{version}-darwin-#{arch}/bin/npm", target: "npm-binary"
+  binary "node-v#{version}-darwin-#{arch}/bin/npx", target: "npx-binary"
+  binary "node-v#{version}-darwin-#{arch}/bin/corepack", target: "corepack-binary"
+end
