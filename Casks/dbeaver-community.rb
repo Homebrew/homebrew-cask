d7ba75bde9fead2e69813bbe1083457a4bf2b2dc
Patrick Linnane <patrick@linnane.io>
2022-09-05 10:26:00 -0700

    Fix hashes

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -2,8 +2,8 @@ cask "dbeaver-community" do
   arch arm: "aarch64", intel: "x86_64"
 
   version "22.2.0"
-  sha256 arm:   "e32bf138b83973917c1c6ef66c8bd374565a24621edf56f8b19eac3eca71f176",
-         intel: "3851ca21e4569357dbc3b7063fd69536dd02dad4e620514288505f87498d8859"
+  sha256 arm:   "166bcf6186790f098395e14a4713063df4d7de2a1d359543fe6040240b7c346c",
+         intel: "5d972b98dee5fc2a9e9310bcab599b4773bb1348f18f4620b988fb10256e6d69"
 
   url "https://dbeaver.io/files/#{version}/dbeaver-ce-#{version}-macos-#{arch}.dmg"
   name "DBeaver Community Edition"
