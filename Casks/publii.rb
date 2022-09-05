dbd64f8f0b6c4baa2d29c7f60cc73c584180c6aa
Patrick Linnane <patrick@linnane.io>
2022-09-05 11:28:02 -0700

    Update publii from 0.40.2 to 0.40.3

--- Casks/publii.rb
+++ Casks/publii.rb
@@ -1,9 +1,9 @@
 cask "publii" do
   arch arm: "apple-silicon", intel: "intel"
 
-  version "0.40.2"
-  sha256 arm:   "eeb32bfd07375bbc5227e6c0e04268683411eaf19583bba75c0a2114fddc04f2",
-         intel: "c5826a1e570ca626df913d72b70a55b5df745a6801991fa2f3707fb68a43cf80"
+  version "0.40.3"
+  sha256 arm:   "4000898d0050ceff42c962ca033debb67f9e822d136d084b675cef6035d74713",
+         intel: "5575a04f5f0831303099e48f9de963018e543393a3dc9f297e3ae0cf69c32fe1"
 
   url "https://cdn.getpublii.com/Publii-#{version}-#{arch}.dmg"
   name "Publii"
