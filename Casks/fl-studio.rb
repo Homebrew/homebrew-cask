d020d504c088259e5a992cd281585700515316f6
Patrick Linnane <patrick@linnane.io>
2022-09-05 10:30:40 -0700

    fl-studio 20.9.2.2670
    Update fl-studio from 20.9.2.2459 to 20.9.2.2670
    
    Closes #131082.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/fl-studio.rb
+++ Casks/fl-studio.rb
@@ -1,6 +1,6 @@
 cask "fl-studio" do
-  version "20.9.2.2459"
-  sha256 "e59c4b4b1912fa14e6d6f8cdf8a57c6542bb7bbcd371b337b89a02a306dc4ffc"
+  version "20.9.2.2670"
+  sha256 "894dbe599046fb4c32fea8bf163569866c0d50d3e6d710685f1c62fe8732c4cc"
 
   url "https://demodownload.image-line.com/flstudio/flstudio_mac_#{version}.dmg"
   name "FL Studio"
