1deb1c34a14a9a09425f5620205ea69ac1609228
Scott Robinson <scott@quadhome.com>
2022-09-05 21:18:28 +1000

    Update unblah.rb

--- Casks/unblah.rb
+++ Casks/unblah.rb
@@ -1,8 +1,8 @@
 cask "unblah" do
-  version ".18"
+  version "1.8,.18"
   sha256 "4656c9cf8b6a41b7bb2ed3b5f9882892f30264066a2985bce51fca794831b39a"
 
-  url "https://github.com/akaalias/getunblah/releases/download/v#{version}/Unblah.dmg",
+  url "https://github.com/akaalias/getunblah/releases/download/v#{version.csv.second}/Unblah.dmg",
       verified: "github.com/akaalias/getunblah/"
   name "Unblah"
   desc "Keep track of how long you've been speaking"
