273b9ac0c14d93567b8cd264b7b8a68e861918d4
Patrick Linnane <patrick@linnane.io>
2022-09-05 20:41:38 -0700

    localxpose 22.9.2
    Update localxpose from 22.9.1 to 22.9.2
    
    Closes #131119.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/localxpose.rb
+++ Casks/localxpose.rb
@@ -1,7 +1,7 @@
 cask "localxpose" do
   arch arm: "arm64", intel: "amd64"
 
-  version "22.9.1"
+  version "22.9.2"
   sha256 :no_check
 
   url "https://api.localxpose.io/api/v2/downloads/loclx-darwin-#{arch}.zip"
