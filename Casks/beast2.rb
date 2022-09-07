81a684ca33ad2337313d6a85467624b86233b765
Miccal Matthews <miccal.matthews@gmail.com>
2022-09-07 18:51:52 +0800

    Update beast2.rb

--- Casks/beast2.rb
+++ Casks/beast2.rb
@@ -1,14 +1,13 @@
-# typed: false
-# frozen_string_literal: true
-
 cask "beast2" do
+  # note: "2" is not a version number, but an intrinsic part of the product name
   version "2.7.0"
   sha256 "656fee774e82ddbdf630b8787cf90912ab2bca8c1972bbe83ac1305fa3b608cd"
 
-  url "https://github.com/CompEvol/beast2/releases/download/v#{version}/BEAST.v#{version}.Mac.dmg", verified: "https://github.com/CompEvol/beast2"
+  url "https://github.com/CompEvol/beast2/releases/download/v#{version}/BEAST.v#{version}.Mac.dmg",
+      verified: "github.com/CompEvol/beast2/"
   name "BEAST2"
   desc "Bayesian evolutionary analysis by sampling trees"
   homepage "https://www.beast2.org/"
 
-  suite "BEAST 2.7.0"
+  suite "BEAST #{version}"
 end
