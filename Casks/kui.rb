f18907db9c12878caf58d6d616e64f6d923d146b
Nick Mitchell <starpit@users.noreply.github.com>
2022-09-07 20:44:38 -0400

    update Kui from 12.0.2 to 12.0.3 (#131173)
    sorry for the flurry here, a few bugs slipped through the cracks with 12.0.0

--- Casks/kui.rb
+++ Casks/kui.rb
@@ -1,9 +1,9 @@
 cask "kui" do
   arch arm: "arm64", intel: "x64"
 
-  version "12.0.2"
-  sha256 arm:   "a72e789b54c96bf569166f7af3473d6d35f8a37d5b7aa0a2eb6f7779ea02953a",
-         intel: "ea621bba53fe6184826380052aacb968af4a0904d12a01ae74d74e3e948bf239"
+  version "12.0.3"
+  sha256 arm:   "2b38f095b16abbfb9d44b4090c41c1b4946c26aaa236f6fd24c7055476bda4c1",
+         intel: "628cd982e2b9218f2b9a3a26059c25ca19000280ea861f933d7f15829569f167"
 
   url "https://github.com/kubernetes-sigs/kui/releases/download/v#{version}/Kui-darwin-#{arch}.tar.bz2"
   name "Kui"
