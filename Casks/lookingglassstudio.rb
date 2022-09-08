0998e309622e614c0b69de91c6f2b214bc334046
Miccal Matthews <miccal.matthews@gmail.com>
2022-09-08 16:44:18 +0800

    Update lookingglassstudio.rb

--- Casks/lookingglassstudio.rb
+++ Casks/lookingglassstudio.rb
@@ -2,7 +2,8 @@ cask "lookingglassstudio" do
   version "1.5.0"
   sha256 "6d6f4422b0872702ff170862f897da83ae18629b77e2f0ced49a4070822ccef4"
 
-  url "https://dhtk4bwj5r21z.cloudfront.net/HoloPlayStudio/PublicLinks/LookingGlassStudio-#{version}.dmg"
+  url "https://dhtk4bwj5r21z.cloudfront.net/HoloPlayStudio/PublicLinks/LookingGlassStudio-#{version}.dmg",
+      verified: "dhtk4bwj5r21z.cloudfront.net/HoloPlayStudio/PublicLinks/"
   name "Looking Glass Studio"
   desc "View and edit 3D image and video formats on the Looking Glass"
   homepage "https://look.glass/"
