0877f9649a786cdae73f5a0cd5c03cbdc6714855
Ethan Proia <47930611+ethanproia@users.noreply.github.com>
2022-09-08 02:25:09 -0500

    Add Looking Glass Studio v1.5.0

---
+++ Casks/looking-glass-studio.rb
@@ -0,0 +1,11 @@
+cask "looking-glass-studio" do
+  version "1.5.0"
+  sha256 :no_check
+
+  url "https://look.glass/hopstudio-mac"
+  name "looking-glass-studio"
+  desc "View and edit a variety of 3D image and video formats on the Looking Glass, and manage the Looking Glass Portrait's standalone mode"
+  homepage "https://look.glass/"
+
+  app "LookingGlassStudio.app"
+end
