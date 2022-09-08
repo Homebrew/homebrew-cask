5cec8fcf646625c960db719f37d8b2e87106b350
miccal <miccal.matthews@gmail.com>
2022-09-08 13:12:47 +0800

    Update rectangle-pro from 2.4.7 to 2.5

--- Casks/rectangle-pro.rb
+++ Casks/rectangle-pro.rb
@@ -1,6 +1,6 @@
 cask "rectangle-pro" do
-  version "2.4.7,118"
-  sha256 "6e6de589ca1e87552450f8c5ae731f35d813b2327bf4efef5aa41de10b44294d"
+  version "2.5,119"
+  sha256 "0f8260c0b4d0b3b938a02fe5ca2838af105080c3d29dcc667646d4dcced150f0"
 
   url "https://rectangleapp.com/pro/downloads/Rectangle%20Pro%20#{version.csv.first}.dmg"
   name "Rectangle Pro"
