c0eb75b992f846ce635b70c63cffec473fd1fec9
Patrick Linnane <patrick@linnane.io>
2022-09-09 10:37:45 -0700

    asciidocfx 1.7.8
    Update asciidocfx from 1.7.7 to 1.7.8
    
    Closes #131238.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/asciidocfx.rb
+++ Casks/asciidocfx.rb
@@ -1,9 +1,9 @@
 cask "asciidocfx" do
   arch arm: "_M1"
 
-  version "1.7.7"
-  sha256 arm:   "1f6779d46ff4a7452e53fbea2a5d7fbe72e5ad55a823307595ba936403cfd2b0",
-         intel: "84c2e92655551c2a02ed400815b6a004631ba6f2db9be6a0ab86e9be6b838f9f"
+  version "1.7.8"
+  sha256 arm:   "f7e2bb8c6e91a8afee18225d3032fbe3ac09cb48ffb59ce82829f0d7984c298d",
+         intel: "cb8ec4d80127e6521e3a01805d918e4bdf575517ccbc19f6e2283ab73a83275d"
 
   url "https://github.com/asciidocfx/AsciidocFX/releases/download/v#{version}/AsciidocFX_Mac#{arch}.dmg",
       verified: "github.com/asciidocfx/AsciidocFX/"
