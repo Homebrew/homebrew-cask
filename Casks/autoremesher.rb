e58527fd8d0deeb50bc63ed87590ec09ffded3be
Ethan Proia <47930611+ethanproia@users.noreply.github.com>
2022-09-09 14:32:44 -0500

    Update sha256 with suggestion
    Co-authored-by: Patrick Linnane <patrick@linnane.io>

--- Casks/autoremesher.rb
+++ Casks/autoremesher.rb
@@ -1,6 +1,6 @@
 cask "autoremesher" do
   version "1.0.0-beta.3"
-  sha256 :no_check
+  sha256 "be8450f69d8c52e1b7cd9d3e7ecacfe6bd64fa7420c6fa3944949509338a85a7"
 
   url "https://github.com/huxingyi/autoremesher/releases/download/#{version}/autoremesher-#{version}.dmg"
   name "AutoRemesher"
