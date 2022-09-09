7f9ce7c375efc8b12206ab0fdd04b0745ce60f6a
Kenneth Chew <kenneth.c0@protonmail.com>
2022-09-08 20:29:24 -0400

    Update polymc from 1.4.1 to 1.4.2

--- Casks/polymc.rb
+++ Casks/polymc.rb
@@ -1,6 +1,6 @@
 cask "polymc" do
-  version "1.4.1"
-  sha256 "881711b736193d6955d94bd7a00c0e465019fc60a0b3c36293c966f178e09f69"
+  version "1.4.2"
+  sha256 "c2b9faef97c6f01d49343428aea9a28412ba8290c428e8fe7e35a8799dc7766a"
 
   url "https://github.com/PolyMC/PolyMC/releases/download/#{version}/PolyMC-macOS-#{version}.tar.gz",
       verified: "github.com/PolyMC/PolyMC/"
