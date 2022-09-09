dd767ef9454beeca419b55d061e2621ba655f21e
Patrick Linnane <patrick@linnane.io>
2022-09-09 14:31:55 -0700

    Bump to 0.7.69

--- Casks/xemu.rb
+++ Casks/xemu.rb
@@ -1,6 +1,6 @@
 cask "xemu" do
-  version "0.7.68"
-  sha256 "be8fdbd878e8811eca4df3bc9351662ab6bd567771f0ae624c71c583f7e5a40e"
+  version "0.7.69"
+  sha256 "9e7f2f57e7097945f51f4c1cf05829c82e951b1e99814f1457333728a214670a"
 
   url "https://github.com/mborgerson/xemu/releases/download/v#{version}/xemu-macos-universal-release.zip",
       verified: "github.com/mborgerson/xemu/"
