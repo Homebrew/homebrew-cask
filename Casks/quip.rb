635f82ff462ecaea578ed848e397d66763ea871f
Patrick Linnane <patrick@linnane.io>
2022-09-09 13:48:59 -0700

    quip 7.68.0
    Update quip from 7.67.1 to 7.68.0
    
    Closes #131351.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/quip.rb
+++ Casks/quip.rb
@@ -1,6 +1,6 @@
 cask "quip" do
-  version "7.67.1"
-  sha256 "88239e6955cec6e2956141d25c1bac3f355d8080ff12ee8ee4b5beb472a4d0d4"
+  version "7.68.0"
+  sha256 "6b1128a4d0bc41e08888faf6831ad7c65064727f112211a2b897604b73d849ae"
 
   url "https://quip-clients.com/macosx_#{version}.dmg",
       verified: "quip-clients.com/"
