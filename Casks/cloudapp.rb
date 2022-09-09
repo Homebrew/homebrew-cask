07c8b62aca6a5ec4631517bfc3d264f6f1ffcc9c
Patrick Linnane <patrick@linnane.io>
2022-09-09 10:44:24 -0700

    cloudapp 6.6.8,2331
    Update cloudapp from 6.6.7 to 6.6.8
    
    Closes #131252.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/cloudapp.rb
+++ Casks/cloudapp.rb
@@ -1,6 +1,6 @@
 cask "cloudapp" do
-  version "6.6.7,2317"
-  sha256 "0fc85eed545eb04b1b5ba0040df6fb21ac70161cf89110a81a64c3d52cf77ae8"
+  version "6.6.8,2331"
+  sha256 "0acc45e8a2e5088614907d25f1f846df6f90facf58014f25be632955c1e1806b"
 
   url "https://downloads.getcloudapp.com/mac/CloudApp-#{version.csv.first}.#{version.csv.last}.zip"
   name "CloudApp"
