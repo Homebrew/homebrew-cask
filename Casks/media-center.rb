4c46848e1b5bd8c92f3de54c23fc35c4ec5b149d
Patrick Linnane <patrick@linnane.io>
2022-09-09 11:05:16 -0700

    Update media-center from 29.00.86 to 29.00.87

--- Casks/media-center.rb
+++ Casks/media-center.rb
@@ -1,6 +1,6 @@
 cask "media-center" do
-  version "29.00.86"
-  sha256 "1a3235859343232e4e5081bac57282e6fbe33cefc0a38f049ca2fb0a960a68dc"
+  version "29.00.87"
+  sha256 "838bde7f2545a332fa789605a9557a6ababa2a6bf93557cb335fabf3c3e82af9"
 
   url "https://files.jriver.com/mediacenter/channels/v#{version.major}/stable/MediaCenter#{version.no_dots}-Universal.dmg"
   name "JRiver Media Center"
