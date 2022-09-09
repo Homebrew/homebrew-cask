37b39be82c6948fd4ef4a06fc6ed2989f81ebcc9
Patrick Linnane <patrick@linnane.io>
2022-09-09 10:48:44 -0700

    Update drawio from 20.2.8 to 20.3.0

--- Casks/drawio.rb
+++ Casks/drawio.rb
@@ -1,9 +1,9 @@
 cask "drawio" do
   arch arm: "arm64", intel: "x64"
 
-  version "20.2.8"
-  sha256 arm:   "84012c9b4cc41da6b141d0ac5fd801f8b0cca2eeed202e0c356c7b7ab5f8fe60",
-         intel: "e0c676e10e20d76511b2d4bd877c40e1fa719de1faf893c0aef308e4ef1a9807"
+  version "20.3.0"
+  sha256 arm:   "ec62375a963cdc09fc854553c6cd12add73fdfbf72dd0fdbd1fea5d1ef72b9d3",
+         intel: "a7dee2427b341ed14a1d6f746ffec6e6388e5e31573289ce76124f2dbb64f1c4"
 
   url "https://github.com/jgraph/drawio-desktop/releases/download/v#{version}/draw.io-#{arch}-#{version}.dmg",
       verified: "github.com/jgraph/drawio-desktop/"
