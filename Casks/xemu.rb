15e0f6448c0224c783beb16ed881c44f7e4f6a80
Patrick Linnane <patrick@linnane.io>
2022-09-09 14:23:11 -0700

    xemu 0.7.69
    * Update xemu from 0.7.67 to 0.7.68
    * Bump to 0.7.69
    
    Closes #131358.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/xemu.rb
+++ Casks/xemu.rb
@@ -1,6 +1,6 @@
 cask "xemu" do
-  version "0.7.67"
-  sha256 "f8e3bceeff2ddd53e603a84c9d323f9b3b259098391283efaa046d77c34d9b38"
+  version "0.7.69"
+  sha256 "9e7f2f57e7097945f51f4c1cf05829c82e951b1e99814f1457333728a214670a"
 
   url "https://github.com/mborgerson/xemu/releases/download/v#{version}/xemu-macos-universal-release.zip",
       verified: "github.com/mborgerson/xemu/"
