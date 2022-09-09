1db3d58b9d42bb66fb06d786ae93dd97eef95727
Patrick Linnane <patrick@linnane.io>
2022-09-09 14:10:18 -0700

    Update remnote from 1.8.22 to 1.8.23

--- Casks/remnote.rb
+++ Casks/remnote.rb
@@ -1,9 +1,9 @@
 cask "remnote" do
   arch arm: "-arm64", intel: ""
 
-  version "1.8.22"
-  sha256 arm:   "d3f4a1f99710957e1d710d95897c56c93d5a033a00e45cfcd3c66de977a71391",
-         intel: "7984799dea8bc21d1cd848ef9c0eb76145ee84adb709c727c7bf274cc9e7bfcf"
+  version "1.8.23"
+  sha256 arm:   "8ea128de326465ecd457344ed8d5263656f2b042fbe8065b36544cfe4ad93cac",
+         intel: "7c1cb7977399cfd59943f4d24a40213b020647b57269d932e48438d4c62c6da9"
 
   url "https://download.remnote.io/RemNote-#{version}#{arch}.dmg",
       verified: "remnote.io"
