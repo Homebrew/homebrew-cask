9940dd5a16714ec1b6889764308709cfcc53ba60
Patrick Linnane <patrick@linnane.io>
2022-09-09 11:08:16 -0700

    Update nwjs from 0.67.1 to 0.68.0

--- Casks/nwjs.rb
+++ Casks/nwjs.rb
@@ -1,6 +1,6 @@
 cask "nwjs" do
-  version "0.67.1"
-  sha256 "5ea59bb00902884989525b8a26d22cb53dc98932ee993eae533a6f15c668d9ec"
+  version "0.68.0"
+  sha256 "efa87095b75529c862918a7b5cc5ce437166c289d5a048c9ba24131526c7440c"
 
   url "https://dl.nwjs.io/v#{version}/nwjs-sdk-v#{version}-osx-x64.zip"
   name "NW.js"
