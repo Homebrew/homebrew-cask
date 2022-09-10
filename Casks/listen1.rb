5bf18f64348aae9345affdd8311ecc95f7c68ec2
yiNiPui <callmeliuweiqi@gmail.com>
2022-09-10 22:51:26 +0800

    listen1 2.26.1
    Update listen1 from 2.26.0 to 2.26.1
    
    Closes #131376.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/listen1.rb
+++ Casks/listen1.rb
@@ -2,9 +2,9 @@ cask "listen1" do
   # NOTE: "1" is not a version number, but an intrinsic part of the product name
   arch arm: "arm64", intel: "x64"
 
-  version "2.26.0"
-  sha256 arm:   "d036af3aca9a29ab65a17b2daaebbf0f8690c3b683076babd91132d08bb562ef",
-         intel: "3a11ad65e60104a7f57d3e1696adb927d7e0b519286aeccc47420f0a8970354f"
+  version "2.26.1"
+  sha256 arm:   "47beb5b2a9f3b5a72ef02ecf73ea38a63d5e2086e50919b55f4c5993ccc8dfd2",
+         intel: "d5758372fb75cb3502372b2431ce3597144b0a77b07d5f258928a2ca0930e1d9"
 
   url "https://github.com/listen1/listen1_desktop/releases/download/v#{version}/Listen1_#{version}_mac_#{arch}.dmg",
       verified: "github.com/listen1/listen1_desktop/"
