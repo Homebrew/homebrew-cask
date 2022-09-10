34ca7790f67ae4467f897231e88e88f3ea679f11
Patrick Linnane <patrick@linnane.io>
2022-09-10 09:46:35 -0700

    Update ppsspp from 1.13.1 to 1.13.2

--- Casks/ppsspp.rb
+++ Casks/ppsspp.rb
@@ -1,6 +1,6 @@
 cask "ppsspp" do
-  version "1.13.1"
-  sha256 "1b4a99b03a661c68b293367916d840aac6fd442295ba78e8824c222d0e46ee37"
+  version "1.13.2"
+  sha256 "6d047749b4c9566ee579e11375c6b8483fe2da95657db9a2f5f8eaeb7bef2b32"
 
   url "https://github.com/hrydgard/ppsspp/releases/download/v#{version}/PPSSPPSDL-macOS-v#{version}.zip",
       verified: "github.com/hrydgard/ppsspp/"
