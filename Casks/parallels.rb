8a84040dcf8bff690df3755fc1e00b4e32f2dbc3
UltraHDR <108294295+UltraHDR@users.noreply.github.com>
2022-09-10 13:12:24 +0100

    Add parallels17 to conflicts with (#131302)
    parallels17 added via https://github.com/Homebrew/homebrew-cask-versions/pull/14655/files

--- Casks/parallels.rb
+++ Casks/parallels.rb
@@ -19,6 +19,7 @@ cask "parallels" do
     "homebrew/cask-versions/parallels14",
     "homebrew/cask-versions/parallels15",
     "homebrew/cask-versions/parallels16",
+    "homebrew/cask-versions/parallels17",
   ]
   depends_on macos: ">= :high_sierra"
 
