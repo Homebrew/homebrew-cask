a64ffa1a2d6ec00c2061bb43292da2dad8d9069b
Patrick Linnane <patrick@linnane.io>
2022-09-09 19:27:25 -0700

    Bump to 47.1.1-release.20220908.26

--- Casks/descript.rb
+++ Casks/descript.rb
@@ -1,9 +1,9 @@
 cask "descript" do
   arch arm: "-arm64"
 
-  version "47.1.0-release.20220830.6"
+  version "47.1.1-release.20220908.26"
   sha256 arm:   "260215ec6f601af65c1a5d1c303a1bcc114dc7ba9f769a7e6293b307ac258fa3",
-         intel: "69de0edb5d32b19a8f2864f88e6d4886349013bd0b306dd71576c7f033e7e5e3"
+         intel: "c1978c22820555d4e8f2287b3db697337da540254fee9540beba6cb77b68c6d1"
 
   url "https://electron.descript.com/Descript-#{version}#{arch}.dmg"
   name "Descript"
