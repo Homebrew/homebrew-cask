d255ce1a644514c0789d73bf5d682d90165c6c85
Ondrej Führer <ondrejfuhrer@users.noreply.github.com>
2022-09-11 21:23:06 +0200

    bunch 1.4.11,166
    Update bunch from 1.4.11,166 to 1.4.11,166
    
    Closes #131440.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/bunch.rb
+++ Casks/bunch.rb
@@ -1,6 +1,6 @@
 cask "bunch" do
-  version "1.4.10,163"
-  sha256 "905f79049ac1a1188222dd48e46f63e1ddd3033cd44cebcd25b48d85df0581f9"
+  version "1.4.11,166"
+  sha256 "240fe4dba00af30b7b875acfcf61130eddad92dd813f05c1766f95f8c2b5a22a"
 
   url "https://brettterpstra.com/updates/bunch/Bunch#{version.csv.first}#{version.csv.second}.dmg",
       verified: "brettterpstra.com/updates/bunch/"
