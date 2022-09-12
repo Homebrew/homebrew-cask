365eed7c98b2496f49865ab85d81e9cfa7631748
Mateusz Jabłoński <mateusz.piotr.jablonski@gmail.com>
2022-09-12 21:14:21 +0200

    Applied auto-fixes

--- Casks/devcleaner.rb
+++ Casks/devcleaner.rb
@@ -2,16 +2,16 @@ cask "devcleaner" do
   version "2.3.1-425"
   sha256 "d31a01b35e77e5bf6a7a87fde660712c46c69baefd83cfdf62403e1c221ff2cc"
 
-  url "https://github.com/vashpan/xcode-dev-cleaner/releases/download/#{version.sub(%r{\-\d+}, '')}/DevCleaner-#{version}.zip"
+  url "https://github.com/vashpan/xcode-dev-cleaner/releases/download/#{version.sub(/-\d+/, "")}/DevCleaner-#{version}.zip"
   name "DevCleaner"
   desc "Reclaim tens of gigabytes of your storage used for various Xcode caches"
   homepage "https://github.com/vashpan/xcode-dev-cleaner"
 
   livecheck do
     url "https://github.com/vashpan/xcode-dev-cleaner/releases/latest"
-    regex(/href=.*releases\/download\/.*\/DevCleaner\-(.*)\.zip/i)
+    regex(%r{href=.*releases/download/.*/DevCleaner-(.*)\.zip}i)
   end
-  
+
   app "DevCleaner.app"
 
   zap trash: [
