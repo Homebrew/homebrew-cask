9b2848317e774ab3113f98c4efbd928a1b6b7a04
Hristo Iliev <hristo@hiliev.eu>
2022-09-12 17:58:59 +0300

    Update moonlight from 4.2.0 to 4.2.1

--- Casks/moonlight.rb
+++ Casks/moonlight.rb
@@ -1,5 +1,5 @@
 cask "moonlight" do
-  version "4.2.0"
+  version "4.2.1"
   sha256 "25ee52eedc63f163ffc1dcd208c90ffdcfdc1c65f67812183f00830377866188"
 
   url "https://github.com/moonlight-stream/moonlight-qt/releases/download/v#{version}/Moonlight-#{version}.dmg",
