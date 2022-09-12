0ae6f0df44f2e6677bacda1db2fe67ed758cea5f
Mateusz Jabłoński <mateusz.piotr.jablonski@gmail.com>
2022-09-12 21:07:34 +0200

    Improved livecheck against build number in version

--- Casks/devcleaner.rb
+++ Casks/devcleaner.rb
@@ -1,12 +1,17 @@
 cask "devcleaner" do
-  version "2.3.1"
+  version "2.3.1-425"
   sha256 "d31a01b35e77e5bf6a7a87fde660712c46c69baefd83cfdf62403e1c221ff2cc"
 
-  url "https://github.com/vashpan/xcode-dev-cleaner/releases/download/#{version}/DevCleaner-#{version}-425.zip"
+  url "https://github.com/vashpan/xcode-dev-cleaner/releases/download/#{version.sub(%r{\-\d+}, '')}/DevCleaner-#{version}.zip"
   name "DevCleaner"
   desc "Reclaim tens of gigabytes of your storage used for various Xcode caches"
   homepage "https://github.com/vashpan/xcode-dev-cleaner"
 
+  livecheck do
+    url "https://github.com/vashpan/xcode-dev-cleaner/releases/latest"
+    regex(/href=.*releases\/download\/.*\/DevCleaner\-(.*)\.zip/i)
+  end
+  
   app "DevCleaner.app"
 
   zap trash: [
