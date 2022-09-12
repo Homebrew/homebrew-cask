d45d5daf9f3b089dd2db050495f4d4a90536a406
owine <owine@users.noreply.github.com>
2022-09-12 09:30:45 -0500

    wavebox 10.105.22.2
    Update wavebox from 10.105.8.2 to 10.105.22.2
    
    Closes #131460.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/wavebox.rb
+++ Casks/wavebox.rb
@@ -1,9 +1,9 @@
 cask "wavebox" do
   arch arm: "macarm64", intel: "mac"
 
-  version "10.105.8.2"
-  sha256 arm:   "6e3e0dba3831323635486e74a30ad149532068e0650067522b23661bd0923763",
-         intel: "82c16ae1208f11393245adac2efea214fffc48ca643cdf76f0938fdec285367d"
+  version "10.105.22.2"
+  sha256 arm:   "4cc4f44c0e2637a949392d0591a17f36854211d4245491067eae1ea09ea023a1",
+         intel: "e0f1d34ba6cdd2a73669ae1e220d7aaba450261f9b2695a358e0f31e72dd1c41"
 
   url "https://download.wavebox.app/stable/#{arch}/Install%20Wavebox%20#{version}.dmg",
       verified: "download.wavebox.app/"
