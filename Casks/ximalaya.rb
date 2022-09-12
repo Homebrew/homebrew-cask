13e9922ad087f71f9d7b03232a31863454d87fde
Patrick Linnane <patrick@linnane.io>
2022-09-11 19:02:42 -0700

    Change livecheck

--- Casks/ximalaya.rb
+++ Casks/ximalaya.rb
@@ -1,21 +1,21 @@
 cask "ximalaya" do
-  version "3.1.0,1661766058"
-  sha256 "bb64b084867b52b2b6544a154cdba137eaec5a4499ef39c62fb1683f4f5a6dc9"
+  version "3.1.0,GKwRIRwG4lU8ACAAAAGZWamd"
+  sha256 "23b1cceab392bc99ee96bd1ad3d9b03f0ff80ee167b900a6ae8f53f5d233b1f2"
 
-  url "https://s1.xmcdn.com/yx/ximalaya-pc-client/#{version.csv.first}/download/mac-x64/Ximalaya_#{version.csv.first}_#{version.csv.second}_x64.dmg",
-      verified: "s1.xmcdn.com/yx/ximalaya-pc-client/"
+  url "https://nativedl.pcdn.xmcdn.com/storages/3715-audiofreehighqps/FD/4F/#{version.csv.second}.dmg?ct=application/octet-stream&filename=Ximalaya_#{version.csv.first}_x64_c_99",
+      verified: "nativedl.pcdn.xmcdn.com/storages/3715-audiofreehighqps/FD/4F/"
   name "ximalaya"
   name "喜马拉雅"
   desc "Platform for podcasting and audio-sharing"
   homepage "https://www.ximalaya.com/"
 
   livecheck do
-    url "https://www.ximalaya.com/down/lite?client=mac"
+    url "https://www.ximalaya.com/down/lite/v2?client=mac&channelId=99"
     strategy :header_match do |headers|
-      match = headers["location"].match(%r{/Ximalaya[_-](\d+(?:\.\d+)+)[_-](\d+)_x64\.dmg}i)
+      match = headers["location"].match(%r{/4F/(.+)\.dmg.*?Ximalaya[._-](\d+(?:\.\d+)+)[._-]x64}i)
       next if match.blank?
 
-      "#{match[1]},#{match[2]}"
+      "#{match[2]},#{match[1]}"
     end
   end
 
