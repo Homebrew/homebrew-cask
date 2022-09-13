7c083baa8bdc8e7970d79d1db7acd687ddb291c5
Alex Jarvis <github@alexjarvis.net>
2022-09-13 17:16:33 -0500

    fmail2 1.1.6,22
    * Update fmail2 from 1.1.5 to 1.1.6
    * Update fmail2.rb Homepage
      Homepage has moved, updated URL.
    * Update fmail2.rb Homepage
      Add trailing slash
    
    Closes #131580.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/fmail2.rb
+++ Casks/fmail2.rb
@@ -1,13 +1,13 @@
 cask "fmail2" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "1.1.5,21"
-  sha256 "35fe60333ec1a0fa4884e8795b1c550be3ea216a79768670f80ff569d6f80847"
+  version "1.1.6,22"
+  sha256 "07843b9d0cc62fe2ed0e12ccf5217236a30782b536dd9103cc675b41bc5536c8"
 
   url "https://arievanboxel.fr/fmail2/sparkle/FMail2_#{version.csv.first.no_dots}.zip",
       verified: "arievanboxel.fr"
   name "fmail2"
   desc "Unofficial 'light' native application for Fastmail"
-  homepage "https://fmail-app.fr/tabs/main.html"
+  homepage "https://fmail-app.fr/"
 
   livecheck do
     url "https://arievanboxel.fr/fmail2/sparkle/appcast.xml"
