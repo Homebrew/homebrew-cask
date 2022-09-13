8f54310f7633dbbbb7ad15d2bb82eafd4c6544c6
erenakgunduz <coderenius@gmail.com>
2022-09-13 16:13:45 -0400

    Update vscodium from 1.71.0.22245 to 1.71.1.22256

--- Casks/vscodium.rb
+++ Casks/vscodium.rb
@@ -1,11 +1,11 @@
 cask "vscodium" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.71.0.22245"
+  version "1.71.1.22256"
   sha256 arm:   "12be281ab31b18a97af7d77a99dfadb356917f08cb282dfa00151bb4743fd720",
          intel: "27bb0d4baee3b1e06eb4e5f08e0c349b9ea0b2deb5802a3207b74278ce578d61"
 
-  url "https://github.com/VSCodium/vscodium/releases/download/#{version}/VSCodium.#{arch}.#{version}.dmg"
+  url "https://github.com/VSCodium/vscodium/releases/download/1.71.1.22256/VSCodium.x64.1.71.1.22256.dmg"
   name "VSCodium"
   desc "Binary releases of VS Code without MS branding/telemetry/licensing"
   homepage "https://github.com/VSCodium/vscodium"
