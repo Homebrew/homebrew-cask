7ea01b10f828df70797da20b05c850418cec371a
Patrick Linnane <patrick@linnane.io>
2022-09-12 19:26:59 -0700

    Update hashbackup from 3039 to 3050

--- Casks/hashbackup.rb
+++ Casks/hashbackup.rb
@@ -1,6 +1,6 @@
 cask "hashbackup" do
-  version "3039"
-  sha256 "1f72c9ab204a81e5a84d05b081e34298a4fbbf84f3ef11d28d6ca1ee026f343f"
+  version "3050"
+  sha256 "0bac266f4109a51f2568d3f8a9a9557ea146c071626572ae30784db62f20070a"
 
   url "http://upgrade.hashbackup.com/#{version}/hb.r#{version}.Darwin.x86_64.bz2"
   name "HashBackup"
