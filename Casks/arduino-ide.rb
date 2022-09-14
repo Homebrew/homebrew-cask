e795d55bef757d2dfde66e7b03180e3c9b1d0e9d
Miccal Matthews <miccal.matthews@gmail.com>
2022-09-14 17:47:28 +0800

    Update arduino-ide.rb

--- Casks/arduino-ide.rb
+++ Casks/arduino-ide.rb
@@ -2,7 +2,7 @@ cask "arduino-ide" do
   version "2.0.0"
   sha256 "fdab408c9227d25664ffd7fdc120520f0ca3fba0d2604f3bb86354e2dd9b7590"
 
-  url "https://downloads.arduino.cc/arduino-ide/arduino-ide_#{version.csv.first}_macOS_64bit.dmg"
+  url "https://downloads.arduino.cc/arduino-ide/arduino-ide_#{version}_macOS_64bit.dmg"
   name "Arduino IDE"
   desc "Electronics prototyping platform"
   homepage "https://www.arduino.cc/en/software"
@@ -10,9 +10,6 @@ cask "arduino-ide" do
   livecheck do
     url "https://www.arduino.cc/en/software/"
     regex(/href=.*?arduino[._-]ide[._-]?(\d+(?:\.\d+)+)[._-]macos[._-]64bit\.dmg/i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| (match[0]).to_s }
-    end
   end
 
   conflicts_with cask: "arduino-ide-nightly"
@@ -20,7 +17,7 @@ cask "arduino-ide" do
   app "Arduino IDE.app"
 
   zap trash: [
-    "~/Library/Application Support/arduino-ide",
     "~/.arduinoIDE",
+    "~/Library/Application Support/arduino-ide",
   ]
 end
