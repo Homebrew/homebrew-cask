a678a249ddd286e00e16104cd38ccffd0d5c7bd4
Lucas.Xu <tsuiyuenhong@gmail.com>
2022-09-14 10:39:30 +0800

    Update appflowy from 0.0.4 to 0.0.5 (#131536)
    * Update appflowy from 0.0.4 to 0.0.5
    
    * Update appflowy.rb
    
    * Update appflowy.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/appflowy.rb
+++ Casks/appflowy.rb
@@ -1,16 +1,16 @@
 cask "appflowy" do
-  version "0.0.4"
-  sha256 "2da964f659f6c0fcf260d76b8d8a3368aff9bf746fd5c3df2e8e8c28207d6bc5"
+  version "0.0.5"
+  sha256 "56f7946b1ec7eef63bc10dcbd41b0dc2cc597221f24489fc63f5095899738345"
 
   url "https://github.com/AppFlowy-IO/AppFlowy/releases/download/#{version}/Appflowy-macos-x86_64.zip",
       verified: "github.com/AppFlowy-IO/AppFlowy/"
-  name "appflowy"
+  name "AppFlowy"
   desc "Open-source project and knowledge management tool"
   homepage "https://www.appflowy.io/"
 
   livecheck do
-    url "https://www.appflowy.io/whatsnew"
-    regex(%r{href=.*?tag/v?(\d+(?:\.\d+)+)})
+    url :url
+    strategy :github_latest
   end
 
   app "AppFlowy.app"
