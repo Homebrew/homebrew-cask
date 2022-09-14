d7023e339bb3c9cc83933bc1ee2775b9df1707ec
Patrick Linnane <patrick@linnane.io>
2022-09-14 12:25:25 -0700

    eclipse-dsl 4.25.0,2022-09
    Update eclipse-dsl from 4.24.0 to 4.25.0
    
    Closes #131645.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/eclipse-dsl.rb
+++ Casks/eclipse-dsl.rb
@@ -1,9 +1,9 @@
 cask "eclipse-dsl" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "4.24.0,2022-06"
-  sha256 arm:   "ca3cb132a996f0dc63851d6444d4276337294a10c8a33e0ab9bdc2eb0e52122e",
-         intel: "68bb8274be67812bbb15b002a94f914a232f9af24a27a758459ffe350d366268"
+  version "4.25.0,2022-09"
+  sha256 arm:   "a6a1fec117bc920bc7451503bf7db64636b044cfabb6f2d70ac354251dadf5f0",
+         intel: "0ad39bc1b065d03210adebaba05e726c1432baeba5d675f018d2d33594a59710"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-dsl-#{version.csv.second}-R-macosx-cocoa-#{arch}.dmg&r=1"
   name "Eclipse IDE for Java and DSL Developers"
