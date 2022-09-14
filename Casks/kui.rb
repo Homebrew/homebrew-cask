eabfb14dc345e43511b2b58e7f3ffd421f7cf470
Nick Mitchell <nickm@us.ibm.com>
2022-09-14 13:30:33 -0400

    bump kui from 12.0.3 to 12.1.0

--- Casks/kui.rb
+++ Casks/kui.rb
@@ -1,9 +1,9 @@
 cask "kui" do
   arch arm: "arm64", intel: "x64"
 
-  version "12.0.3"
-  sha256 arm:   "2b38f095b16abbfb9d44b4090c41c1b4946c26aaa236f6fd24c7055476bda4c1",
-         intel: "628cd982e2b9218f2b9a3a26059c25ca19000280ea861f933d7f15829569f167"
+  version "12.1.0"
+  sha256 arm:   "3b3c654dc37842ee268147c97bec45fc1efc416289037df6109a538e10c57155",
+         intel: "42d45fbfaca87f17e06344c3f444b689c59dadd84e5e2e98d9c81d40a03e18df"
 
   url "https://github.com/kubernetes-sigs/kui/releases/download/v#{version}/Kui-darwin-#{arch}.tar.bz2"
   name "Kui"
