ecc1ecb9a2f52ebb80ec245dc85a3e284684ac55
Matt Coster <opensource@mtcoster.net>
2022-09-14 17:49:50 +0100

    Update idafree from 8.0 to 8.0

--- Casks/idafree.rb
+++ Casks/idafree.rb
@@ -2,8 +2,8 @@ cask "idafree" do
   arch arm: "arm_idafree", intel: "idafree"
 
   version "8.0"
-  sha256 arm:   "73354d6e4f2fae739943ac2b10b7d98e871ad8004c40e779a2f8b9ab0e40f194",
-         intel: "51017c0047b5b06d6cd46c93a65583d4fbf086502de884ebcb8d72033b65e5a0"
+  sha256 arm:   "afc2093e26521af320d7f4515bd618fb5c9132965981558e874fc375e0554f48",
+         intel: "70137e02202e218034d2eb6e6e23aa41728b022186a71b987c3754aa20219357"
 
   url "https://out7.hex-rays.com/files/#{arch}#{version.no_dots}_mac.app.zip"
   name "IDA Free"
