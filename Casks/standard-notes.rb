0c9ab8cab42d717e0381c4925ca5bf29a55e48eb
thk-cheng <kenneth.cheng.tsun.him@gmail.com>
2022-09-14 16:34:24 +0100

    Update standard-notes from 3.23.96 to 3.23.119

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -1,9 +1,9 @@
 cask "standard-notes" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.23.96"
-  sha256 arm:   "657a939227337cb7f6f1639311bd56ccdfafe1a55ed3b11b293b2ab7273a28eb",
-         intel: "fbf6503249747c2f4829399367032c1d02b7e36408206d44e7f854228582c790"
+  version "3.23.119"
+  sha256 arm:   "d08fbce748a90ca106a617c34836e0747ff4e2aa003d95d39edbf985c08c4376",
+         intel: "1e72a990fc815d766bf5804cb657ce76c3667c9ba9a82d6e408656323a7c3e6f"
 
   url "https://github.com/standardnotes/app/releases/download/%40standardnotes%2Fdesktop%40#{version}/standard-notes-#{version}-mac-#{arch}.zip",
       verified: "github.com/standardnotes/app/"
