24e8998e69adefa6ae5de703675e5f7dcf8d2ac1
Patrick Linnane <patrick@linnane.io>
2022-09-14 12:39:00 -0700

    Update eclipse-java from 4.24.0 to 4.25.0

--- Casks/eclipse-java.rb
+++ Casks/eclipse-java.rb
@@ -1,9 +1,9 @@
 cask "eclipse-java" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "4.24.0,2022-06"
-  sha256 arm:   "387983833dd7f8296e4bcdba063258a6c9194dba2cb0ed6cddf4b4157369b445",
-         intel: "46636f6d9e82394412cc6660358c84200499fa79c89dda52c77c39e81a9592f7"
+  version "4.25.0,2022-09"
+  sha256 arm:   "7c051602004d41a2f847d7cf93b1f3b37fe17f3af397df48199a76947ebcb519",
+         intel: "8ef98a458433e34f0853bf84f843e00a585eaaa004592d090ef0525cd4690072"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-java-#{version.csv.second}-R-macosx-cocoa-#{arch}.dmg&r=1"
   name "Eclipse IDE for Java Developers"
