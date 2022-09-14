e69a81377da8a627aaa1d7cefb245a7bbd0e49c9
Patrick Linnane <patrick@linnane.io>
2022-09-14 12:54:40 -0700

    Update fiji from 20220414-1745 to 20220913-1702

--- Casks/fiji.rb
+++ Casks/fiji.rb
@@ -1,6 +1,6 @@
 cask "fiji" do
-  version "20220414-1745"
-  sha256 "99b90c323d9840615d2c3f34295a28beff5f3fa9be4a2b283f3481645c332c83"
+  version "20220913-1702"
+  sha256 "b1da701bb5ffcb72e8e8a814676739b5439d7246abd74269e06349ce5a911250"
 
   url "https://downloads.imagej.net/fiji/archive/#{version}/fiji-macosx.zip",
       verified: "downloads.imagej.net/fiji/archive/"
