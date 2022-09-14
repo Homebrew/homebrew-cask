b2406001377db25e10f217f0af853e8683b50202
MoonFruit <dkmoonfruit@gmail.com>
2022-09-14 14:35:30 +0800

    Update rider from 2022.2.2 to 2022.2.3

--- Casks/rider.rb
+++ Casks/rider.rb
@@ -1,9 +1,9 @@
 cask "rider" do
   arch arm: "-aarch64"
 
-  version "2022.2.2,222.3962.23"
-  sha256 arm:   "449f219fd48b367ee51ac4627c86a6a6dd77846b6186cebc71ca0567e2adba05",
-         intel: "0a86dfb256154001ee2da50a0fa2126eceb9b85407cbd2d41976f29230367fdb"
+  version "2022.2.3,222.4167.23"
+  sha256 arm:   "5dd892ed16dd1bc819a97ffb62cdfbb3b60c6019581ba18358afc5c0a39585f5",
+         intel: "aa02c2c621d356486a0b698a45d773f5830ff4ef431940059f82e8d3c17a2335"
 
   url "https://download.jetbrains.com/rider/JetBrains.Rider-#{version.csv.first}#{arch}.dmg"
   name "JetBrains Rider"
