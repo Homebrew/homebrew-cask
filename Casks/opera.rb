595f661ea9a582cf6d7667e365692f522dae05fc
Lachezar Balabanov <lachezar.balabanov@sap.com>
2022-09-14 18:21:25 +0300

    opera 91.0.4516.16
    Update opera from 90.0.4480.107 to 91.0.4516.16
    
    Closes #131603.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask "opera" do
-  version "90.0.4480.107"
-  sha256 "ca9f451bbed5f05c73e6230d9e58197c08473e761c72848ccb440b661f9fc543"
+  version "91.0.4516.16"
+  sha256 "1d07ac2196510fba6aa43ac4f59d7b7fb283234b8274656b1e013efdc871d7d5"
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   name "Opera"
