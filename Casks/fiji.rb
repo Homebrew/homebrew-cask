5f4341653b8a1d8a72b1bb4f030cdbc09e438891
Patrick Linnane <patrick@linnane.io>
2022-09-14 12:54:40 -0700

    fiji 20220914-1417
    * Update fiji from 20220414-1745 to 20220913-1702
    * Bump to 20220914-1417
    
    Closes #131659.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/fiji.rb
+++ Casks/fiji.rb
@@ -1,6 +1,6 @@
 cask "fiji" do
-  version "20220414-1745"
-  sha256 "99b90c323d9840615d2c3f34295a28beff5f3fa9be4a2b283f3481645c332c83"
+  version "20220914-1417"
+  sha256 "11d3764f5f750e9948bab3834d6ad022b85b528d56af517c69c470b2dd5f2dbd"
 
   url "https://downloads.imagej.net/fiji/archive/#{version}/fiji-macosx.zip",
       verified: "downloads.imagej.net/fiji/archive/"
