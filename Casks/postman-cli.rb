139d28a06699bcf2c35c573b57aac6a045a27408
Patrick Linnane <patrick@linnane.io>
2022-09-14 13:30:53 -0700

    postman-cli 0.0.17
    * Update postman-cli from 0.0.12 to 0.0.15
    * Bump to 0.0.17
    
    Closes #131683.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/postman-cli.rb
+++ Casks/postman-cli.rb
@@ -1,9 +1,9 @@
 cask "postman-cli" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.0.12"
-  sha256 arm:   "d43dc6793fd6b3acd452a3c8c55ed56d7ad45ab3465aa0c58ffb750a648d18aa",
-         intel: "d43dc6793fd6b3acd452a3c8c55ed56d7ad45ab3465aa0c58ffb750a648d18aa"
+  version "0.0.17"
+  sha256 arm:   "78a05c6f8911f64502804fad45b06ad4c055966c2980692de04b722cdd296092",
+         intel: "78a05c6f8911f64502804fad45b06ad4c055966c2980692de04b722cdd296092"
 
   url "https://dl-cli.pstmn.io/download/postman-cli-cli-#{version}-macos-#{arch}.zip",
       verified: "dl-cli.pstmn.io/download/"
