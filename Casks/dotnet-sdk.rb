bb231f09311da2253a7ec39cc1bc38b5ddcd18c4
Patrick Linnane <patrick@linnane.io>
2022-09-14 12:34:33 -0700

    Bump Intel

--- Casks/dotnet-sdk.rb
+++ Casks/dotnet-sdk.rb
@@ -2,8 +2,8 @@ cask "dotnet-sdk" do
   arch arm: "arm64", intel: "x64"
 
   on_intel do
-    version "6.0.400,2382b0d3-f865-4818-b3aa-47a94ccedba8,cd1eb0d061a1e1d8d0373603c4e82a97"
-    sha256 "c31cde7a2a0f4f63e91229d3122ae5bb7f758b0cf3093b486b0e683f6d2a62a3"
+    version "6.0.401,c98d3ccf-561c-4f9b-a1d7-5debb0880031,fd1ebfbd783788649ee139c229cd48fa"
+    sha256 "667920bf202c420d57091f24b8051bad8ed174e131c4c15f45f79b2a785c3744"
   end
   on_arm do
     version "6.0.401,1011a115-ca23-4bad-8632-f9a96e47d0f0,164a08cde051b61a1669d2242770ef25"
