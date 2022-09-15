81e714489131a1bf719a5be51b2800cb797ca814
Bevan Kay <email@bevankay.me>
2022-09-15 12:27:20 +1000

    tidal: update url, add shasum, update livecheck (#131723)

--- Casks/tidal.rb
+++ Casks/tidal.rb
@@ -1,23 +1,20 @@
 cask "tidal" do
-  arch arm: ".arm64"
+  arch arm: "arm64", intel: "x64"
 
-  on_intel do
-    version "2.30.0"
-  end
-  on_arm do
-    version "2.33.2"
-  end
+  version "2.33.2"
+  sha256 arm:   "8d9e854dbe47a6587c4e9b8854c38a35c4bc1e8801629d265a7f7b102f101e76",
+         intel: "0855257b2bc4e1d341503eefd87c3190abc5e0294d11bf9cd5fa9171e64bbf9e"
 
-  sha256 :no_check
-
-  url "https://download.tidal.com/desktop/TIDAL#{arch}.dmg"
+  url "https://download.tidal.com/desktop/mac/TIDAL.#{arch}.#{version}.zip"
   name "TIDAL"
   desc "Music streaming service with high fidelity sound and hi-def video quality"
   homepage "https://tidal.com/"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url "https://download.tidal.com/desktop/mac/update-#{arch}.json"
+    strategy :page_match do |page|
+      JSON.parse(page)["currentRelease"]
+    end
   end
 
   auto_updates true
