1d310ffa5bf7b432406045aba3b76271e2e73efc
runner <runner@Mac-1663363158484.local>
2022-09-16 21:28:27 +0000

    Update warp from 0.2022.09.07.14.56.stable_00 to 0.2022.09.13.08.15.stable_03

--- Casks/warp.rb
+++ Casks/warp.rb
@@ -1,6 +1,6 @@
 cask "warp" do
-  version "0.2022.09.07.14.56.stable_00"
-  sha256 "6e0ac5d97267fbdc99c8797558146ede37e7ee1ef2e8697016657a7c26e06eec"
+  version "0.2022.09.13.08.15.stable_03"
+  sha256 "d15add93dbb60c095ae0626f89d591a74992ebb0f9b59b2cc5212390e6fc2d05"
 
   url "https://app.warp.dev/download/brew?version=v#{version}"
   name "Warp"
