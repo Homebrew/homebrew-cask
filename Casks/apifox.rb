dee2166369989e405a5d396cac8419439f4a085f
ZhengJin Fang <fangzhengjin@gmail.com>
2022-09-16 10:20:51 +0800

    Update apifox.rb

--- Casks/apifox.rb
+++ Casks/apifox.rb
@@ -11,7 +11,7 @@ cask "apifox" do
   homepage "https://www.apifox.cn/"
 
   livecheck do
-    url "https://cdn.apifox.cn/download/mac/latest-mac.yml"
+    url "https://cdn.apifox.cn/download/mac/latest-mac.yml?noCache=" + (Time.new.to_f * 1000).to_i
     strategy :electron_builder
   end
 
