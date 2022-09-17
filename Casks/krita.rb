38442d5aa803c8164eba1df91beaee86999ad755
Miccal Matthews <miccal.matthews@gmail.com>
2022-09-17 09:40:27 +0800

    Update krita.rb

--- Casks/krita.rb
+++ Casks/krita.rb
@@ -2,15 +2,15 @@ cask "krita" do
   version "5.1.1"
   sha256 "9c9825ce611dcbdade0e8c8f8570ffd79f5eba8e35cefed7a9dec43f039cbfc1"
 
-  url "https://download.kde.org/stable/krita/#{version.major_minor_patch}/krita-#{version}.dmg",
+  url "https://download.kde.org/stable/krita/#{version}/krita-#{version}.dmg",
       verified: "download.kde.org/stable/krita/"
   name "Krita"
   desc "Free and open-source painting and sketching program"
   homepage "https://krita.org/"
 
   livecheck do
-    url "https://krita.org/en/download/krita-desktop/"
-    regex(/href=.*?krita[._-](\d+(?:\.\d+)+)\.dmg/i)
+    url "https://download.kde.org/stable/krita/"
+    regex(%r{href="(\d+(?:\.\d+)+)/"}i)
   end
 
   depends_on macos: ">= :sierra"
