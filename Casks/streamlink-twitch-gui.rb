f6e343d9331e1a1250fa02d138598d9f6a00f8b1
Sebastian Meyer <mail@bastimeyer.de>
2022-09-17 03:21:57 +0200

    Update streamlink-twitch-gui to 2.2.0 (#131840)

--- Casks/streamlink-twitch-gui.rb
+++ Casks/streamlink-twitch-gui.rb
@@ -1,6 +1,6 @@
 cask "streamlink-twitch-gui" do
-  version "2.1.0"
-  sha256 "a093923ff3926338f3a6aa6041fd1c7ad32c864adeb657d197b5235e97acd020"
+  version "2.2.0"
+  sha256 "1101b5dd4076277dce55be0d86741f4e6a28af472c5498d26e6477c2c608502b"
 
   url "https://github.com/streamlink/streamlink-twitch-gui/releases/download/v#{version}/streamlink-twitch-gui-v#{version}-macOS.tar.gz"
   name "Streamlink Twitch GUI"
