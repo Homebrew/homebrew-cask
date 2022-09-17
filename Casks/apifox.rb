ff94baa9151223c32d6044656dc734721c0a7cbb
ZhengJin Fang <fangzhengjin@gmail.com>
2022-09-17 23:50:24 +0800

    Update Apifox from 2.1.34 to 2.1.35 (#131423)
    * Update Apifox from 2.1.34 to 2.1.35
    
    * Update apifox.rb
    
    * Update apifox.rb
    
    * Update Casks/apifox.rb
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/apifox.rb
+++ Casks/apifox.rb
@@ -1,9 +1,9 @@
 cask "apifox" do
   arch arm: "-macOS-arm64"
 
-  version "2.1.34"
-  sha256 arm:   "c8a5741801c15dbf078a5d99f29e3884aa9c03d249f54bed6dd5136a11d33532",
-         intel: "4b6ebce6da81ab38c84413dea010ef7e4b9432e82b9bba7d89c1ba351f069d0a"
+  version "2.1.35"
+  sha256 arm:   "2a8b286c22b7ecbab71ee91801187b0b3caf798597b8c70ca7af6a68197b5b9c",
+         intel: "a7ebff4967490dd17ca768319684dfb2efdeef09249a396ea0873a7d3eb8b224"
 
   url "https://cdn.apifox.cn/download/#{version}/Apifox#{arch}-#{version}.dmg"
   name "Apifox"
@@ -11,7 +11,7 @@ cask "apifox" do
   homepage "https://www.apifox.cn/"
 
   livecheck do
-    url "https://cdn.apifox.cn/download/mac/latest-mac.yml"
+    url "https://cdn.apifox.cn/download/mac/latest-mac.yml?noCache=#{Time.new.to_i * 2}"
     strategy :electron_builder
   end
 
