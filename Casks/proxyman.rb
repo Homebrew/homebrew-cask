deae8f45942592123dabf5e9250d33fd1f41e0f6
Nghia Tran <vinhnghiatran@gmail.com>
2022-09-18 20:46:32 +0700

    Update Proxyman 3.10.0

--- Casks/proxyman.rb
+++ Casks/proxyman.rb
@@ -1,6 +1,6 @@
 cask "proxyman" do
-  version "3.9.0,30900"
-  sha256 "59e9b5ea3f8feef8b9e28daea9711e90030ca42cfb1664cd25a47c2fc0c1de19"
+  version "3.10.0,31000"
+  sha256 "b711d8b8e9554baea9f1b6f053668892f0ab6ee32f3337aea974a9ba5aeed6fd"
 
   url "https://download.proxyman.io/#{version.csv.second}/Proxyman_#{version.csv.first}.dmg"
   name "Proxyman"
