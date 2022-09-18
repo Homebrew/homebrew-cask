f40e218f293518f3c0980d42a47944c6f1794c89
腹黒い茶 <haraguroicha@tipsy.coffee>
2022-09-18 17:44:04 +0800

    Update oxwu for arch aarm64

--- Casks/oxwu.rb
+++ Casks/oxwu.rb
@@ -1,18 +1,20 @@
 cask "oxwu" do
-  version "3.0.0"
-  sha256 "f901d96c4856fb045edf26ce339dfac4c302d36a4e202d8365fef35e3c70b467"
+  arch arm: "arm", intel: "intel"
 
-  url "http://eew.earthquake.tw/releases/autoupdates/OXWU-osx64-#{version}.zip"
+  on_intel do
+    version "3.0.0"
+    sha256 "f901d96c4856fb045edf26ce339dfac4c302d36a4e202d8365fef35e3c70b467"
+    url "http://eew.earthquake.tw/releases/autoupdates/OXWU-osx64-#{version}.zip"
+  end
+  on_arm do
+    version :latest
+    sha256 :no_check
+    url "https://eew.earthquake.tw/releases/mac/arm64/oxwu-mac-arm64.dmg"
+  end
+
   name "OX Wake Up"
   desc "Taiwan Earthquake app"
   homepage "https://eew.earthquake.tw/"
-
-  livecheck do
-    url "https://eew.earthquake.tw/releases/autoupdates/package.json"
-    strategy :page_match do |page|
-      JSON.parse(page)["version"]
-    end
-  end
 
   auto_updates true
   depends_on macos: ">= :high_sierra"
