51099338cec0f1d930d26f8393209e6332e05ccf
Martijn van der Ven <martijn@vanderven.se>
2022-09-19 09:40:55 +0200

    Add arm url to mytesync.app cask

--- Casks/mutesync.rb
+++ Casks/mutesync.rb
@@ -1,9 +1,17 @@
 cask "mutesync" do
   version "5.4.1"
-  sha256 "d9f4e90b9a9de3f779f1a3fbffc6d62b3c63e1d69f1be8a232cba19a5fffd633"
+  sha256 arm:   "037980f23abcb95dc879c4caec31985e6129a426a068603447573d51d04c9c45",
+         intel: "d9f4e90b9a9de3f779f1a3fbffc6d62b3c63e1d69f1be8a232cba19a5fffd633"
 
-  url "https://mutesync.s3-us-west-2.amazonaws.com/mutesync-#{version}.dmg",
-      verified: "mutesync.s3-us-west-2.amazonaws.com/"
+  on_arm do
+    url "https://mutesync.s3.us-west-2.amazonaws.com/mutesync-#{version}-arm64.dmg",
+      verified: "mutesync.s3.us-west-2.amazonaws.com/"
+  end
+  on_intel do
+    url "https://mutesync.s3.us-west-2.amazonaws.com/mutesync-#{version}.dmg",
+      verified: "mutesync.s3.us-west-2.amazonaws.com/"
+  end
+
   name "mütesync"
   desc "Companion app to the mütesync physical button"
   homepage "https://mutesync.com/"
