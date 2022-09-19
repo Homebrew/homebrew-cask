bdf229669f614001f5297659636c674bc375b8fc
2gn <101851090+2gn@users.noreply.github.com>
2022-09-19 10:01:31 +0900

    style fixed

--- Casks/plugdata.rb
+++ Casks/plugdata.rb
@@ -4,25 +4,25 @@
 cask "plugdata" do
   version "0.6.1"
   sha256 "b39f902d375d52b58714f3f8c5c07abef688668bf2608a03f5372dc27532b1d7"
+
   url "https://github.com/timothyschoen/PlugData/releases/download/v#{version}/PlugData-MacOS-Universal.zip"
   name "PlugData"
   desc "Pure Data as a plugin, with a new GUI"
   homepage "https://github.com/timothyschoen/PlugData"
-  
+
   livecheck do
     url :url
     strategy :github_latest
   end
-  
+
   auto_updates true
 
   pkg "PlugData-MacOS-Universal.pkg"
-  
+
   uninstall pkgutil: [
     "com.Octagon.app.pkg.PlugData",
     "com.Octagon.lv2.pkg.PlugData",
     "com.Octagon.au.pkg.PlugData",
-    "com.Octagon.vst3.pkg.PlugData"    
+    "com.Octagon.vst3.pkg.PlugData",
   ]
-  
 end
