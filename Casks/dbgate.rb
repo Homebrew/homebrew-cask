845632f2fb82def952cf98ebe2ef6904038ba139
Patrick Linnane <patrick@linnane.io>
2022-09-19 16:38:20 -0700

    dbgate 5.1.2
    Update dbgate from 5.1.1 to 5.1.2
    
    Closes #131961.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/dbgate.rb
+++ Casks/dbgate.rb
@@ -1,6 +1,6 @@
 cask "dbgate" do
-  version "5.1.1"
-  sha256 "4eb1c71ba598c3214a80d604d952bc7e1d2e082ab7d8a0b193b31b1b903a4feb"
+  version "5.1.2"
+  sha256 "f5cb4306bb484e3a06e7066e3870e2a1457c4f8563ba51401f783b9ef0a1d011"
 
   url "https://github.com/dbgate/dbgate/releases/download/v#{version}/dbgate-#{version}-mac_universal.dmg",
       verified: "github.com/dbgate/dbgate/"
