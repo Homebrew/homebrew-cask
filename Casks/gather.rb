e469baee9912a26f68f1f9c050e179818c03883f
Patrick Linnane <patrick@linnane.io>
2022-09-19 16:55:50 -0700

    Update gather from 0.3.8 to 0.3.9

--- Casks/gather.rb
+++ Casks/gather.rb
@@ -1,9 +1,9 @@
 cask "gather" do
   arch arm: "-arm64"
 
-  version "0.3.8"
-  sha256 arm:   "16a58158aa2916e1a1db0bf375cb4b49acb0c102616b1b002965994c8464815e",
-         intel: "d540dcc8e57afb7b55a3e35994a95e5791fdf11752cd1fee4715e8523db217d3"
+  version "0.3.9"
+  sha256 arm:   "66d7507146fd2272eaa249a51206b12887ca27d5b163e20aeebfa324a877b997",
+         intel: "ef196e8f48326a7d92313875c4c8947c57a3984b36aa2079d0d724ff796115b2"
 
   url "https://github.com/gathertown/gather-town-desktop-releases/releases/download/v#{version}/Gather-#{version}#{arch}-mac.zip",
       verified: "github.com/gathertown/gather-town-desktop-releases"
