6095392a94a7987229368687c40574045f9cff1f
Martijn van der Ven <martijn@vanderven.se>
2022-09-19 10:11:39 +0200

    Use arch variable in mutesync.app cask

--- Casks/mutesync.rb
+++ Casks/mutesync.rb
@@ -1,17 +1,12 @@
 cask "mutesync" do
+  arch arm: "-arm64", intel: ""
+
   version "5.4.1"
   sha256 arm:   "037980f23abcb95dc879c4caec31985e6129a426a068603447573d51d04c9c45",
          intel: "d9f4e90b9a9de3f779f1a3fbffc6d62b3c63e1d69f1be8a232cba19a5fffd633"
 
-  on_arm do
-    url "https://mutesync.s3.us-west-2.amazonaws.com/mutesync-#{version}-arm64.dmg",
-        verified: "mutesync.s3.us-west-2.amazonaws.com/"
-  end
-  on_intel do
-    url "https://mutesync.s3.us-west-2.amazonaws.com/mutesync-#{version}.dmg",
-        verified: "mutesync.s3.us-west-2.amazonaws.com/"
-  end
-
+  url "https://mutesync.s3.us-west-2.amazonaws.com/mutesync-#{version}#{arch}.dmg",
+      verified: "mutesync.s3.us-west-2.amazonaws.com/"
   name "mütesync"
   desc "Companion app to the mütesync physical button"
   homepage "https://mutesync.com/"
