6b8434c51f6ade498c4779627c933f5100009ee9
Ben Taylor <benty@amazon.com>
2022-09-20 11:20:29 -0700

    Update corretto from 18.0.2.9.1 to 19.0.0.36.1

--- Casks/corretto.rb
+++ Casks/corretto.rb
@@ -1,9 +1,9 @@
 cask "corretto" do
   arch arm: "aarch64", intel: "x64"
 
-  version "18.0.2.9.1"
-  sha256 arm:   "19f29b4973897d929011a5540db03b859b81ae5a0fc03dc487597b8cf15c8b26",
-         intel: "cc5703bc4984e73dd66b18c3623fa1d576e53fda9c33de335f42b3596de01786"
+  version "19.0.0.36.1"
+  sha256 arm:   "0f9241b93c4006df2b1dd0e6da1d428b2ba4523e4142e58ecbdbda75ed6c6b9a",
+         intel: "c78428ce793f1a609637345ca438d56885cade971a04bed5caf4ff3fcfe09098"
 
   url "https://corretto.aws/downloads/resources/#{version.sub(/-\d+/, "")}/amazon-corretto-#{version}-macosx-#{arch}.pkg"
   name "AWS Corretto JDK"
