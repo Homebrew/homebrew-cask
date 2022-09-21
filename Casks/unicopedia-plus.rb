d094ba909ed59f0e87c9768c7676dce05e1bdb3c
Patrick Linnane <patrick@linnane.io>
2022-09-20 21:13:21 -0700

    unicopedia-plus 12.0.0
    Update unicopedia-plus from 11.6.0 to 12.0.0
    
    Closes #132039.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/unicopedia-plus.rb
+++ Casks/unicopedia-plus.rb
@@ -1,6 +1,6 @@
 cask "unicopedia-plus" do
-  version "11.6.0"
-  sha256 "de355c6bc5e8dd51850f514e8fb8b326667eb1ac1d191f6303221b9d726c4936"
+  version "12.0.0"
+  sha256 "aa945abb9e326f2324465689b584d663a6e834fcec517f4abdce19552bf144ff"
 
   url "https://github.com/tonton-pixel/unicopedia-plus/releases/download/v#{version}/unicopedia-plus-mac-universal-#{version}.dmg"
   name "Unicopedia Plus"
