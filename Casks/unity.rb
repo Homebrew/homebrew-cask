85aa77fc4d54b1b3df625e2e23f3688cc605c807
Patrick Linnane <patrick@linnane.io>
2022-09-21 14:07:31 -0700

    unity 2022.1.17f1,2d349551e475
    Update unity from 2022.1.16f1 to 2022.1.17f1
    
    Closes #132095.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -1,9 +1,9 @@
 cask "unity" do
   arch arm: "Arm64"
 
-  version "2022.1.16f1,7321c9670bc2"
-  sha256 arm:   "89ff542616928d033086a2707f694ab1fd4036e7e15aea038286f769dbf9e3f4",
-         intel: "5a4b0feacc84fea7a4ffbb763702426fb57cf0db2626f09bd078b3ab0ef7916b"
+  version "2022.1.17f1,2d349551e475"
+  sha256 arm:   "6f837d01c8bd30d278f545d699897ce586618760418f33e8e08780bc52d876b1",
+         intel: "b9fea1888490bd17aa7ba220cb9af40999da4052e8df9f9e46ea5d0ae4bd81a6"
 
   url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorInstaller#{arch}/Unity-#{version.csv.first}.pkg",
       verified: "download.unity3d.com/download_unity/"
