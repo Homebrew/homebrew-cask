3952277945459e5265af1d31c120b52791265b5a
Patrick Linnane <patrick@linnane.io>
2022-09-20 21:11:44 -0700

    thunderbird 102.3.0
    Update thunderbird from 102.2.2 to 102.3.0
    
    Closes #132037.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/thunderbird.rb
+++ Casks/thunderbird.rb
@@ -1,68 +1,68 @@
 cask "thunderbird" do
-  version "102.2.2"
+  version "102.3.0"
 
   language "cs" do
-    sha256 "8155236cc94926e57792edb944d58146149d04e8360a637265648298d2010d39"
+    sha256 "7b07238dd5f4d444e4e8d774d94d1f8af3fc5c80ba5214d54f35ec622be165d8"
     "cs"
   end
   language "de" do
-    sha256 "b1e084a7c09aceb7535044b07fb07cf2aed9a1c3d5e087c99687db27fc28c0ea"
+    sha256 "0610f57d2780cc97dda4b0c333145fbfe87db5b9b2c9cf181a1b2fd8f0ba4207"
     "de"
   end
   language "en-GB" do
-    sha256 "4bcded94173085b7b8a1bb16283081c6bc22c7497109ac7845eb5ce6bbd3c18d"
+    sha256 "8ca4a9199e3c8c26ed2e3faf6a5e0ed0a13ce13f8a51ed73cc60b5d1377a44e2"
     "en-GB"
   end
   language "en", default: true do
-    sha256 "57826efdb4e93e8c9e04fee33ca43cc0c0dae457af9468e0f15f518fa5aecafd"
+    sha256 "3edc5b8128d4afb15a0b0db863ba7336879a0a0a56b655571575ae066bff7b87"
     "en-US"
   end
   language "fr" do
-    sha256 "7afdca17dd7d7e9526f17658d923fb1f7c0fbe1775c3bda18023c59534f9f9b1"
+    sha256 "09cd1bd9ffa89d4f441c40c57f91d569a4fb5b49f7067363c123df94e3e78c47"
     "fr"
   end
   language "gl" do
-    sha256 "78723931265e0d73dfa671d4981f52524b9963b0e4d3fc1d058c1d6f7e089cd1"
+    sha256 "28b8ccfcef1583de96c39b3c3d5690f705e972290d83899d8ffd6b4956f34e79"
     "gl"
   end
   language "it" do
-    sha256 "a2d3023e8bc8a6b1f3e24bf9e0f871e8ad8dfade1d247f3baccc1dd07560afb2"
+    sha256 "5334eab1e26a7f174cf91ec8ff7ca9a4fbdb72c48ddd1c432bd9ca4730665410"
     "it"
   end
   language "ja" do
-    sha256 "2648c09abf8bc00f9d10b85154a60693db3b2c5f74b96d2439127a88b70fddbe"
+    sha256 "7964a1a999d81c9be2eb0e0fba9a426f2d26fcb2acea61df1f432f64ec006f07"
     "ja-JP-mac"
   end
   language "nl" do
-    sha256 "db89a54615eae0e19fb2abbf3d01968f3ec73c651445843a64600cc019b63603"
+    sha256 "d9c74ab8e5375118d8e1b8904d3237773787a65678783ffe47a226ba18092eef"
     "nl"
   end
   language "pl" do
-    sha256 "6c7b37e9daa59092ec7de1aaa931e680b54f130cd108d18ef4251e6e860c7a66"
+    sha256 "c6e33a6c9225c9bc10507b985b35482b95d2fb2d6dd59da0cd144de729ec6426"
     "pl"
   end
   language "pt" do
-    sha256 "7ec82cbd445f24c911a0d6913602db5bb906d2e86d3c96028e7c544f6af62dd6"
+    sha256 "2ef01c9d7709aa48c9cad49e814920efa1c3e89ebb590014b694de1bc5582a96"
     "pt-PT"
   end
   language "pt-BR" do
-    sha256 "71528a67e0e02ec767657a3b158295dcde84ce6f5f169e60c61440c48f0a062d"
+    sha256 "0a2efdd3395f112828210c154cc69fb3da984a9d456bed4d6e2914641467f22a"
     "pt-BR"
   end
   language "ru" do
-    sha256 "b3287d1a5cc35aab4768e350f143b0cf393a3df42c11ac239ff6c06ff94606a9"
+    sha256 "8e4fb3e9aebd2284fb744622786a5e6892957b14b56eca72b34e901830dc6685"
     "ru"
   end
   language "uk" do
-    sha256 "7b7bd29b5e327f8f90eae5f5860291cf838f2fcf5749441411d3ec280639912d"
+    sha256 "a78c4e14d665969b129867ffb22e724613462792f7459dc85671dc58d7009ddd"
     "uk"
   end
   language "zh-TW" do
-    sha256 "210cb1780b2fa2a981db9493ebe84d82790d4645f01ec48b055634282b3ea437"
+    sha256 "871f32eb98fded4d221d8da9c390e0ee870d909d8a911a53a6eefe5a02a5cbca"
     "zh-TW"
   end
   language "zh" do
-    sha256 "b568dc3239fb8f65c7239b76f633e8438d66984af09d4fa5ec8fbe2f3986d15a"
+    sha256 "58c1b18abc963626077102864aa9f1737817e9aabdad86920cb1846ace99c377"
     "zh-CN"
   end
 
