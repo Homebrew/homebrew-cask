b62a22616b052465a5ebdd7f4f41eff7ca446d0d
Patrick Linnane <patrick@linnane.io>
2022-09-20 21:05:20 -0700

    Update superhuman from 13.0.4 to 13.0.5

--- Casks/superhuman.rb
+++ Casks/superhuman.rb
@@ -1,9 +1,9 @@
 cask "superhuman" do
   arch arm: "-arm64"
 
-  version "13.0.4"
-  sha256 arm:   "dd34c5520b579609c69bba798a8c1c6341458af2f4adf2a57d818b9924e79085",
-         intel: "852977d831bb690a289cbea1eea22147b49b9afce8eeb2f7379731cbbf33f4bf"
+  version "13.0.5"
+  sha256 arm:   "398e8dccea0a38495fe49e071589f6a91f39c6e55459b22a84fb485dff8f7d5c",
+         intel: "ffc6f2e98403a03a617f93d495322ee8a5160d92d2616ff6c69576aa8deaac21"
 
   url "https://storage.googleapis.com/download.superhuman.com/supertron-update/Superhuman-#{version}#{arch}-latest-mac.zip",
       verified: "storage.googleapis.com/download.superhuman.com/"
