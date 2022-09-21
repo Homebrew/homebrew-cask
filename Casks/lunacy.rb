5fbd11ec4fdfaa64fa44e3a16f9361b9a39e18c1
Alexey Vasilev <alex.vasilev@icons8.com>
2022-09-21 06:47:33 +0300

    Add Lunacy.app v8.6.0
    Add new cask Icons8 Lunacy app

---
+++ Casks/lunacy.rb
@@ -0,0 +1,11 @@
+cask "lunacy" do
+  version "8.6.2"
+  sha256 :no_check
+
+  url "https://lcdn.icons8.com/setup/Lunacy.dmg"
+  name "lunacy"
+  desc "Cross-Platform Graphic Design Software with Collaboration"
+  homepage "https://icons8.com/lunacy"
+
+  app "Lunacy.app"
+end
