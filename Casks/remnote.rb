a23ca51d4eb3832326f663fdf2dd2e4676e7d56a
Patrick Linnane <patrick@linnane.io>
2022-09-20 20:57:25 -0700

    remnote 1.8.30
    Update remnote from 1.8.29 to 1.8.30
    
    Closes #132031.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/remnote.rb
+++ Casks/remnote.rb
@@ -1,9 +1,9 @@
 cask "remnote" do
   arch arm: "-arm64", intel: ""
 
-  version "1.8.29"
-  sha256 arm:   "b3e1cce4f5e54beab56d0e19f28d08c3aa1635509eac5e22771a01414e14d528",
-         intel: "9a1352dd2abeecb4a4ab08f8002bc667b2c52e8d94fede3bcd5662712dbacca2"
+  version "1.8.30"
+  sha256 arm:   "1724f60e36204d4ee0910931a1c828fd8313a8082baaabde606396a8074e6461",
+         intel: "f9140737a405399d3c6e0a135d5c4c877aa4a9d11a09818133e0f6e03e675ef0"
 
   url "https://download.remnote.io/RemNote-#{version}#{arch}.dmg",
       verified: "remnote.io"
