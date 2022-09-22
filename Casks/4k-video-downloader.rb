8211458493d0db4cd73f1f297de4dde431247e81
Patrick Linnane <patrick@linnane.io>
2022-09-22 09:07:31 -0700

    4k-video-downloader 4.21.5
    Update 4k-video-downloader from 4.21.4 to 4.21.5
    
    Closes #132126.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/4k-video-downloader.rb
+++ Casks/4k-video-downloader.rb
@@ -1,6 +1,6 @@
 cask "4k-video-downloader" do
-  version "4.21.4"
-  sha256 "ab3b46fbf84361cce0eee7f085929fc89c2a368e3da770bcb372d7bee95b18ec"
+  version "4.21.5"
+  sha256 "9755a517a9274349e58f13288054e89c168416d76de0f3a0d19e3b0c96484e9b"
 
   url "https://dl.4kdownload.com/app/4kvideodownloader_#{version.major_minor_patch}.dmg"
   name "4K Video Downloader"
