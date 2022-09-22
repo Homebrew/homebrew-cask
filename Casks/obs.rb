24f2364317d7563f354f2fa2674fcf10306c08e2
i0ntempest <szf1234@me.com>
2022-09-22 15:11:06 -0400

    Update obs from 28.0.1 to 28.0.2

--- Casks/obs.rb
+++ Casks/obs.rb
@@ -1,9 +1,9 @@
 cask "obs" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "28.0.1"
-  sha256 arm:   "57b3777bd815d40d80bce0e8bc41e7ab6765318f024f44e5ccc6e82ac44e1694",
-         intel: "246e1b174af3f61513e4e004e948a325149091b5165b7005391570f6d334ee3e"
+  version "28.0.2"
+  sha256 arm:   "84a1eade1510814eb78f5cff31a3a256361eae36f33defdb70a71b7e7ba6b7a9",
+         intel: "bb8b67957389dc52db50c3222a2ce25239eb50cb58e7b5d7ca563810cff3e063"
 
   url "https://cdn-fastly.obsproject.com/downloads/obs-studio-#{version}-macos-#{arch}.dmg"
   name "OBS"
