3583549bffcfd61355aa22e374c8f67b478ec9a5
胡尊杰 <feixiang_hu@qq.com>
2022-09-22 15:40:02 +0800

    Add New tuitui.rb

---
+++ Casks/tuitui.rb
@@ -0,0 +1,16 @@
+cask "tuitui" do
+  version "5.10.0"
+  sha256 :no_check
+
+  url "https://down.360safe.com/tuitui/tuitui_#{version}.dmg",
+      verified: "down.360safe.com/tuitui/"
+  name "TuiTui IM"
+  desc "Office communication software"
+  homepage "https://im.qihoo.net/"
+
+  app "360家.app"
+
+  caveats do
+    discontinued
+  end
+end
