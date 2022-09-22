ddc40c6058b995a9042282edd3bc74b138e1c4df
Miccal Matthews <miccal.matthews@gmail.com>
2022-09-22 09:41:04 +0800

    Update lunacy.rb

--- Casks/lunacy.rb
+++ Casks/lunacy.rb
@@ -1,11 +1,16 @@
 cask "lunacy" do
   version "8.6.2"
-  sha256 :no_check
+  sha256 "6aba89b4cfce4ea3a680b16a7fb6a2bd7d92b3a33ba3fb78498f8be632803e53"
 
-  url "https://lcdn.icons8.com/setup/Lunacy.dmg"
-  name "lunacy"
-  desc "Cross-Platform Graphic Design Software with Collaboration"
+  url "https://lcdn.icons8.com/setup/Lunacy_#{version}.dmg"
+  name "Lunacy"
+  desc "Graphic design software"
   homepage "https://icons8.com/lunacy"
+
+  livecheck do
+    url "https://docs.icons8.com/release-notes/"
+    regex(/Lunacy[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+  end
 
   app "Lunacy.app"
 end
