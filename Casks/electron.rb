b941bf8e952c615a9c454519ff7b74043e463168
Patrick Linnane <patrick@linnane.io>
2022-09-21 19:47:33 -0700

    Update electron from 20.1.4 to 20.2.0

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,9 +1,9 @@
 cask "electron" do
   arch arm: "arm64", intel: "x64"
 
-  version "20.1.4"
-  sha256 arm:   "dff42627d50cc5ca56a780da33d574f33232c4cba7c63f7bd77372eab8c2158d",
-         intel: "3cd5ae73b7667556a7ffbd9ffcee32eafb9ba8f6cc65df224dea31591a6162b0"
+  version "20.2.0"
+  sha256 arm:   "0d4ea1a4f530b9bee78bd0a78430778a7e8982f78cf1d5ba8daac334a1312ce1",
+         intel: "8cc8ec2823f561493e3ab22b9f73ea01af2f8eea4904af25d74ee91ee4cbfe2f"
 
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-#{arch}.zip",
       verified: "github.com/electron/electron/"
