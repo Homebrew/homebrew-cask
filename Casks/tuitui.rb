2a2ea6c38845fb0e4de5be9be7e1dbc7d5752bba
胡尊杰 <huzunjie@pyzy.net>
2022-09-22 14:50:00 +0800

    Create tuitui.rb
    Add TuiTui IM APP

---
+++ Casks/tuitui.rb
@@ -0,0 +1,15 @@
+cask "tuitui" do
+  version "5.10.0"
+  sha256 :no_check
+
+  url "https://down.360safe.com/tuitui/tuitui_#{version}.dmg"
+  name "TuiTui IM"
+  desc "Office communication software"
+  homepage "https://im.qihoo.net/"
+
+  app "推推.app"
+
+  caveats do
+    discontinued
+  end
+end
