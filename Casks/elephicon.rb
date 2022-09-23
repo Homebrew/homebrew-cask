2f32273c6c1324f8f6e4df1634f865477b9494e5
Patrick Linnane <patrick@linnane.io>
2022-09-22 18:41:53 -0700

    Update elephicon from 2.0.0 to 2.0.1

--- Casks/elephicon.rb
+++ Casks/elephicon.rb
@@ -1,9 +1,9 @@
 cask "elephicon" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.0.0"
-  sha256 arm:   "aed6e00d6f2c9b2c5af5632feb6b1da6f942a3b8e6de7ff3a1226d198fa4bb45",
-         intel: "d470bcf3b547bd6f011283fef77eb3dafd5cf03a2d7e850cf938c541b4f1e052"
+  version "2.0.1"
+  sha256 arm:   "73c12ddf05235586c211b657f62720103723ec9492cbd1360f8f54ae7b723b50",
+         intel: "e1690e847575eda251774b9ef26b5737dfe702793f3b200a09788e300e8cc181"
 
   url "https://github.com/sprout2000/elephicon/releases/download/v#{version}/Elephicon-#{version}-darwin-#{arch}.dmg"
   name "Elephicon"
