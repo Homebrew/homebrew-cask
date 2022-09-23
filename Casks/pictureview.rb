fb3da9d637c7068de5168b1123d4a792d8ae63df
Ryan <25192197+singularitti@users.noreply.github.com>
2022-09-23 02:46:11 -0400

    Add cask "pictureview" (#132179)
    * Add cask "pictureview"
    
    * Update pictureview.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/pictureview.rb
@@ -0,0 +1,25 @@
+cask "pictureview" do
+  version "2.3.4"
+  sha256 "3975d6e10f8e04e4339eadeafd0af511611fd9ba852d4c3e724dbbb3d811abce"
+
+  url "https://wl879.github.io/apps/picview/PictureView_#{version}.dmg"
+  name "Picture View"
+  desc "Image viewer"
+  homepage "https://wl879.github.io/apps/picview/index.html"
+
+  livecheck do
+    url "https://raw.githubusercontent.com/wl879/wl879.github.io/master/apps/picview/appcase.xml"
+    strategy :sparkle do |item|
+      item.url[%r{/PictureView[._-]v?(\d+(?:\.\d+)+)\.dmg}i, 1]
+    end
+  end
+
+  depends_on macos: ">= :mojave"
+
+  app "PictureView.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.zouke.PictureView.plist",
+    "~/Library/Saved Application State/com.zouke.PictureView.savedState",
+  ]
+end
