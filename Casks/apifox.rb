a94478a19834973937248bb14dcad67427d09a98
Patrick Linnane <patrick@linnane.io>
2022-09-23 15:52:38 -0700

    Update apifox from 2.1.36 to 2.1.37

--- Casks/apifox.rb
+++ Casks/apifox.rb
@@ -1,9 +1,9 @@
 cask "apifox" do
   arch arm: "-macOS-arm64"
 
-  version "2.1.36"
-  sha256 arm:   "02a209bbc4a82b0885ba1d3ce69b17f9e7b25f4d520a9aad28978eb239a0764a",
-         intel: "4c58a7aa0302725591346f0fe789de61a916608b86176f28488114f13048b3e2"
+  version "2.1.37"
+  sha256 arm:   "9ec05b21288836d2821bdac898407d0a52473de81a5868e5fab79a10e503832d",
+         intel: "b2aa4f8c0c55737b8240650fe8a09397c4698793765ea22851d27415cb138281"
 
   url "https://cdn.apifox.cn/download/#{version}/Apifox#{arch}-#{version}.dmg"
   name "Apifox"
