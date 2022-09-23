b40a9dae6e2750f85d202082012d091d883be556
Ryan <25192197+singularitti@users.noreply.github.com>
2022-09-23 17:30:26 -0400

    Update quit-all from 1.2.2 to 1.2.3 (#132194)

--- Casks/quit-all.rb
+++ Casks/quit-all.rb
@@ -1,6 +1,6 @@
 cask "quit-all" do
-  version "1.2.2"
-  sha256 "0c9a4cc5b2bcaf1ccaffcbb63b5f87ebc6781cfa28d7d1e277fabefb4272d96c"
+  version "1.2.3"
+  sha256 "a2e2748ee7145fed3d427f3128ec0e3b5695f252a4510150a912c80b16ae7e10"
 
   url "https://amicoapps.com/wp-content/updates/quitall-#{version}.zip"
   name "QuitAll"
