5febdb614cb220d53352ad2fa16acc4f8d1119d9
Patrick Linnane <patrick@linnane.io>
2022-09-24 19:19:38 -0700

    Update brewtarget from 2.3.0 to 3.0.0

--- Casks/brewtarget.rb
+++ Casks/brewtarget.rb
@@ -1,18 +1,11 @@
 cask "brewtarget" do
-  version "2.3.0"
-  sha256 "f1b3b33029f7d7cd873a1cdbf0741da22085f53dcefa2943dcfa8d35eda89fdd"
+  version "3.0.0"
+  sha256 "35667724ebec5f11e7022f27a29e8eab4cf830ead6f94d139224c8a3e5c2f704"
 
-  url "https://launchpad.net/brewtarget/trunk/#{version}/+download/Brewtarget-#{version}.dmg",
-      verified: "launchpad.net/brewtarget/"
+  url "https://github.com/Brewtarget/brewtarget/releases/download/v#{version}/brewtarget_#{version}_x86_64.dmg"
   name "brewtarget"
-  homepage "http://www.brewtarget.org/"
-
-  # We need to check all releases since not all releases are for macOS.
-  livecheck do
-    url "https://github.com/Brewtarget/brewtarget/releases"
-    strategy :page_match
-    regex(%r{href=.*?/Brewtarget[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
-  end
+  desc "Beer recipe creation tool"
+  homepage "https://github.com/Brewtarget/brewtarget/"
 
   app "brewtarget.app"
 end
