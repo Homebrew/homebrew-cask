510f1056fda73db92f987b397f7c06e2afd25fff
Roman Tataurov <1154997+webhive@users.noreply.github.com>
2022-09-27 00:39:42 +0300

    Update superslicer from 2.4.58.4 to 2.4.58.5 (#132215)
    * Update superslicer from 2.4.58.4 to 2.4.58.5
    
    * Better livecheck
    
    * superslicer: update livecheck
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/superslicer.rb
+++ Casks/superslicer.rb
@@ -1,23 +1,17 @@
 cask "superslicer" do
-  arch arm: "arm", intel: "intel"
+  arch arm: "_arm", intel: ""
 
-  version "2.4.58.4,220811"
-  sha256 arm:   "ddd5f3822c69faefc31124fad194c536d6c4f0ea661e7e5427559c4ba9c4c1f4",
-         intel: "594f10459f4d3b2081ae89ef170bc9aa9ad13ebe62bbf2acac3311c58c114026"
+  version "2.4.58.5,220924"
+  sha256 arm:   "c65d21e51ac57eb75c915ae172a642f48d63584f6bdd8a280aa2443319a66010",
+         intel: "95652e91bef57c73f38c0c78f2fcc9dfce429516f5de516b5e8ac5db20d20132"
 
-  url "https://github.com/supermerill/SuperSlicer/releases/download/#{version.csv.first}/SuperSlicer_#{version.csv.first}_macos_#{arch}_#{version.csv.second}.dmg"
+  url "https://github.com/supermerill/SuperSlicer/releases/download/#{version.csv.first}/SuperSlicer_#{version.csv.first}_macos#{arch}_#{version.csv.second}.dmg"
   name "SuperSlicer"
   desc "Convert 3D models into G-code instructions or PNG layers"
   homepage "https://github.com/supermerill/SuperSlicer"
 
   livecheck do
-    url "https://github.com/supermerill/SuperSlicer/releases/latest"
-    strategy :page_match do |page|
-      match = page.match(%r{href=.*?/SuperSlicer_(\d+(?:\.\d+)+)_macos_#{arch}_(\d+)\.dmg}i)
-      next if match.blank?
-
-      "#{match[1]},#{match[2]}"
-    end
+    skip "Requires checking separate GitHub release asset list HTML"
   end
 
   depends_on formula: "zstd"
