ae2b2eb5a95da29f80bc15aa050b6c2a0a7a72bf
EricFromCanada <enk3@outlook.com>
2022-09-26 11:00:49 -0400

    Update 0-ad from 0.0.25b-alpha to 0.0.26-alpha

--- Casks/0-ad.rb
+++ Casks/0-ad.rb
@@ -1,8 +1,11 @@
 cask "0-ad" do
-  version "0.0.26-alpha"
-  sha256 "f8f0f9237d33f3b2acabc1d5b50ee6da32768231d5610a2ff52d1e65df76bf2c"
+  arch arm: "osx-aarch64", intel: "osx64"
 
-  url "https://releases.wildfiregames.com/0ad-#{version}-osx64.dmg",
+  version "0.0.26-alpha"
+  sha256 arm:   "3ef9a974ffa6f32577ba54f73b34a9d81a3798781fd8e30ea836626e3fdd3ac5",
+         intel: "f8f0f9237d33f3b2acabc1d5b50ee6da32768231d5610a2ff52d1e65df76bf2c"
+
+  url "https://releases.wildfiregames.com/0ad-#{version}-#{arch}.dmg",
       verified: "releases.wildfiregames.com/"
   name "0 A.D."
   desc "Real-time strategy game"
