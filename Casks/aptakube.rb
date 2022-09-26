3403bcf210e98c7eb38e143facf10d5025398342
goenning <me@goenning.net>
2022-09-26 07:22:28 +0100

    Add Aptakube.app 0.5.1

---
+++ Casks/aptakube.rb
@@ -0,0 +1,24 @@
+cask "aptakube" do
+  version "0.5.1"
+  sha256 "6ce8d5308bf2078c06dac12d3a99fd0102f31b5f818e8a11198d66679b580b12"
+
+  url "https://releases.aptakube.com/Aptakube_#{version}_universal.dmg"
+  name "Aptakube"
+  desc "Modern, lightweight and multi-cluster Kubernetes desktop client"
+  homepage "https://aptakube.com/"
+
+  livecheck do
+    url "https://releases.aptakube.com/latest.json"
+    regex(/"version": "(\d+(?:\.\d+)+)"/i)
+  end
+
+  auto_updates true
+
+  app "Aptakube.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.aptakube.Aptakube",
+    "~/Library/Caches/com.aptakube.Aptakube",
+    "~/Library/Logs/com.aptakube.Aptakube",
+  ]
+end
