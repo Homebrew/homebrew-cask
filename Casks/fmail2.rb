11fe1e0b3ed9a98d80bad95aff78b04f1f59389e
Alex Jarvis <github@alexjarvis.net>
2022-09-27 10:01:04 -0500

    Update fmail2 from 1.2.0,26 to 1.2.0,26

--- Casks/fmail2.rb
+++ Casks/fmail2.rb
@@ -1,7 +1,7 @@
 cask "fmail2" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "1.1.8,24"
-  sha256 "feb2e3cfd531df209d2cdf25825702e60fc032af61d9339a00f58880de1fd9c8"
+  version "1.2.0,26"
+  sha256 "f650f172b40f2367553b32179ecbf20ab043c1fa81bd16e55c3a7743b2fa43fd"
 
   url "https://arievanboxel.fr/fmail2/sparkle/FMail2_#{version.csv.first.no_dots}.zip",
       verified: "arievanboxel.fr"
