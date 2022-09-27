ac0bf3bf5dd486fc67095b90fcf05f9f0e20e9af
Patrick Linnane <patrick@linnane.io>
2022-09-27 09:20:54 -0700

    fastscripts 3.2.2,1723
    Update fastscripts from 3.1.2 to 3.2.2
    
    Closes #132331.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/fastscripts.rb
+++ Casks/fastscripts.rb
@@ -1,6 +1,6 @@
 cask "fastscripts" do
-  version "3.1.2,1661"
-  sha256 "6eeee622d1ba70dafe707247f8d7cd7dfbddc5f59918c2dd565acb1deafb2963"
+  version "3.2.2,1723"
+  sha256 "cf1522649ecd9103e61a654cbd4cdd61b2fa125f22f88f25e8e257659b4159d2"
 
   url "https://redsweater.com/fastscripts/FastScripts#{version.csv.first}.zip"
   name "FastScripts"
