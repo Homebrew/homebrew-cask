789ef4626a072a9b03c4051da4d4881652bf133f
Patrick Linnane <patrick@linnane.io>
2022-09-27 10:14:26 -0700

    radarr 4.2.4.6635
    Update radarr from 4.1.0.6175 to 4.2.4.6635
    
    Closes #132368.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/radarr.rb
+++ Casks/radarr.rb
@@ -1,9 +1,9 @@
 cask "radarr" do
   arch arm: "arm64", intel: "x64"
 
-  version "4.1.0.6175"
-  sha256 arm:   "cba7916c86a54d59b35506677620a4f9dc3de4aa45cea916d0b62d194244e93c",
-         intel: "85094096ef10c789e6b7ac59208f02cb3de0a02dd1acf358cb569e2ff9df984d"
+  version "4.2.4.6635"
+  sha256 arm:   "b8bbec0677c5e50a73d3a52b84f72d036d830bb5004659743f5327bf238b319f",
+         intel: "8b491adf6b72a20d20bf2a6e5e6b0e9f77f1ca236a4d5ccb9bb74d10a8e86319"
 
   url "https://github.com/Radarr/Radarr/releases/download/v#{version}/Radarr.master.#{version}.osx-app-core-#{arch}.zip",
       verified: "github.com/Radarr/Radarr/"
