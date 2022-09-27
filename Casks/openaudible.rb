c746d03d71f00bd2f94e5441f6fe08db27421b8d
Patrick Linnane <patrick@linnane.io>
2022-09-27 09:53:38 -0700

    openaudible 3.6
    Update openaudible from 3.5.9 to 3.6
    
    Closes #132354.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/openaudible.rb
+++ Casks/openaudible.rb
@@ -1,9 +1,9 @@
 cask "openaudible" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "3.5.9"
-  sha256 arm:   "2386378e1ed81a7a4c6a65b92b21794fe5675d9925a01534b6ab8f7523ddb281",
-         intel: "e5b9a122a249cebf0e1bd7adfcab24924274878f711e8a2b501f4e634687bb62"
+  version "3.6"
+  sha256 arm:   "734577d0cbfa922848da4ca7e296e020c5026b6d4f16c3b88daea59b4453c1d2",
+         intel: "7019710116465aebf2b620ec5d77c308a0f41258cd3ce9724676bace4f47f1a0"
 
   url "https://github.com/openaudible/openaudible/releases/download/v#{version}/OpenAudible_#{version}_#{arch}.dmg",
       verified: "github.com/openaudible/openaudible/"
