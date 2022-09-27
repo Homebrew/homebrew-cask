d147aca1ebce366da742d0566c524b143bd2e0a5
Zhenfu Shi <szf1234@me.com>
2022-09-27 10:49:39 -0400

    Update malus from 4.1.1 to 4.2.0 (#132273)

--- Casks/malus.rb
+++ Casks/malus.rb
@@ -1,6 +1,6 @@
 cask "malus" do
-  version "4.1.1"
-  sha256 "2637992d2cad9bcb3ac67194e3efb7905bf38f57f3b5ea2108964b03bec7eb2e"
+  version "4.2.0"
+  sha256 "088d1943c7a309c2157e119926e6311844112e5993ee49113c12148fd071a6ba"
 
   url "https://download.getmalus.com/uploads/malus_mac_#{version.dots_to_underscores}.dmg"
   name "Malus"
