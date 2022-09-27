a1498533bd05e72b449962bd86fac397974901b7
Patrick Linnane <patrick@linnane.io>
2022-09-27 11:10:35 -0700

    httpie 2022.14.0
    Update httpie from 2022.13.0 to 2022.14.0
    
    Closes #132399.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/httpie.rb
+++ Casks/httpie.rb
@@ -1,9 +1,9 @@
 cask "httpie" do
   arch arm: "-arm64"
 
-  version "2022.13.0"
-  sha256 arm:   "febb3fbab4021399a1968af0fb1f087598610e833221f629871335e12288ab5c",
-         intel: "b932117ee7dab5f9790cd48822df0c0f2ffdef46940c095873189a6469d55f25"
+  version "2022.14.0"
+  sha256 arm:   "69b2952bb661a9de3087f1e5dca5e93e784e8cb941a7b88a445d241848c6c524",
+         intel: "bf7b024890f9bc3a6bf7721e5e5cebf0f188b5c11d987c8e2de6e8c6ff6dbd7f"
 
   url "https://github.com/httpie/desktop/releases/download/v#{version}/HTTPie-#{version}#{arch}.dmg",
       verified: "github.com/httpie/desktop/"
