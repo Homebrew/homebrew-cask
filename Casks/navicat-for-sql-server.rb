c35456e7c7528471a7bcfe4ca7972e24958c8c1e
Patrick Linnane <patrick@linnane.io>
2022-09-28 12:00:46 -0700

    navicat-for-sql-server 16.1.3
    Update navicat-for-sql-server from 16.1.2 to 16.1.3
    
    Closes #132451.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/navicat-for-sql-server.rb
+++ Casks/navicat-for-sql-server.rb
@@ -1,5 +1,5 @@
 cask "navicat-for-sql-server" do
-  version "16.1.2"
+  version "16.1.3"
   sha256 :no_check
 
   url "https://download.navicat.com/download/navicat#{version.major_minor.no_dots}_sqlserver_en.dmg"
