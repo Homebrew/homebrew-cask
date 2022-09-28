d2a0b1876b33338aeb3dc93810cad811688f0594
Patrick Linnane <patrick@linnane.io>
2022-09-28 10:15:42 -0700

    Update godot-mono from 3.5 to 3.5.1

--- Casks/godot-mono.rb
+++ Casks/godot-mono.rb
@@ -1,6 +1,6 @@
 cask "godot-mono" do
-  version "3.5"
-  sha256 "341501d8eeb1715959dc1fb0fe6613d76e78be5a9375221ac46c2f5abfa1a55e"
+  version "3.5.1"
+  sha256 "7e1baf9693ee9ce5518e0a0f2db3e2acc36d99118b67dcda3715304fb8dfb0ca"
 
   url "https://downloads.tuxfamily.org/godotengine/#{version}/mono/Godot_v#{version}-stable_mono_osx.universal.zip",
       verified: "downloads.tuxfamily.org/godotengine/"
