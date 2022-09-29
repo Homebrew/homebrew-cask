83aab9538a42c8ecf53bd9b27678c9ba03b5aff5
Patrick Linnane <patrick@linnane.io>
2022-09-29 09:36:53 -0700

    Update unity from 2022.1.17f1 to 2022.1.18f1

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -1,9 +1,9 @@
 cask "unity" do
   arch arm: "Arm64"
 
-  version "2022.1.17f1,2d349551e475"
-  sha256 arm:   "6f837d01c8bd30d278f545d699897ce586618760418f33e8e08780bc52d876b1",
-         intel: "b9fea1888490bd17aa7ba220cb9af40999da4052e8df9f9e46ea5d0ae4bd81a6"
+  version "2022.1.18f1,8a091f9adba4"
+  sha256 arm:   "dc68de7f28c58ec0946fc93d665b0a798eed49b517af44571e95f06b75e95d39",
+         intel: "c0351e7b9a2bfbd7f88b78e6055154f88401fe029ed61d030c402422562e3c56"
 
   url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorInstaller#{arch}/Unity-#{version.csv.first}.pkg",
       verified: "download.unity3d.com/download_unity/"
