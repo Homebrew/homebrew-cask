e4983c6ad3df1dfb794ab0dde80c98a1507898b9
Patrick Linnane <patrick@linnane.io>
2022-09-29 08:17:58 -0700

    coccoc 105.0.5195.142,105.0.142
    Update coccoc from 104.0.5112.84 to 105.0.5195.142
    
    Closes #132514.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/coccoc.rb
+++ Casks/coccoc.rb
@@ -1,7 +1,7 @@
 cask "coccoc" do
   arch arm: "/arm64"
 
-  version "104.0.5112.84,104.0.84"
+  version "105.0.5195.142,105.0.142"
   sha256 :no_check
 
   url "https://files-cdn.coccoc.com/browser/mac#{arch}/coccoc.dmg"
