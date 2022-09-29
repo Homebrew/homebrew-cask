b39fed0896c2a6d9bc6973581c4c9a6bc24300db
dylan <331506+macdylan@users.noreply.github.com>
2022-09-29 14:48:24 +0800

    update snapmaker-luban to use github repo

--- Casks/snapmaker-luban.rb
+++ Casks/snapmaker-luban.rb
@@ -2,8 +2,9 @@ cask "snapmaker-luban" do
   version "4.4.0"
   sha256 "2d5f0414ee91a1dc7673ce5b4175e1baaad5d769e45a88a38240d131184c647f"
 
-  url "https://github.com/Snapmaker/Luban/releases/download/v#{version}/Snapmaker-Luban-#{version}-mac.zip",
-      verified: "github.com/Snapmaker/Luban/"
+  url "https://github.com/snapmaker/luban/releases/download/v#{version}/Snapmaker-Luban-#{version}-mac.zip",
+      verified: "github.com/snapmaker/luban/"
+  # appcast "https://github.com/snapmaker/luban/releases.atom"
   name "Snapmaker Luban"
   desc "3D printing software"
   homepage "https://luban.xyz/"
@@ -18,5 +19,6 @@ cask "snapmaker-luban" do
     "~/Library/Preferences/ByHost/com.snapmaker.luban.ShipIt.*.plist",
     "~/Library/Preferences/com.snapmaker.luban.helper.plist",
     "~/Library/Preferences/com.snapmaker.luban.plist",
+    "~/Library/Saved Application State/com.snapmaker.luban.savedState",
   ]
 end
