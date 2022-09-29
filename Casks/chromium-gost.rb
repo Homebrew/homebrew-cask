577eb0607845266a9d734e69c268c89d550dcb7f
Patrick Linnane <patrick@linnane.io>
2022-09-28 21:12:03 -0700

    chromium-gost 106.0.5249.62
    Update chromium-gost from 105.0.5195.125 to 106.0.5249.62
    
    Closes #132476.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/chromium-gost.rb
+++ Casks/chromium-gost.rb
@@ -1,6 +1,6 @@
 cask "chromium-gost" do
-  version "105.0.5195.125"
-  sha256 "ebcf104d5678fb7405f19001ae6f9d4b7e901754b8ddce87bf202bb143c0529f"
+  version "106.0.5249.62"
+  sha256 "0408b0d949677a3e947bcf3500a4c908cd844314b4d0852ed12c55edba7583f3"
 
   url "https://github.com/deemru/Chromium-Gost/releases/download/#{version}/chromium-gost-#{version}-macos-amd64.tar.bz2"
   name "Chromium-Gost"
