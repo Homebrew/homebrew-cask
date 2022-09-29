c22b5f8ea6223c8c0d5ba2635dfbf57c1b98f536
Patrick Linnane <patrick@linnane.io>
2022-09-29 09:52:11 -0700

    yuque 1.7.1
    Update yuque from 1.6.3 to 1.7.1
    
    Closes #132558.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/yuque.rb
+++ Casks/yuque.rb
@@ -1,9 +1,9 @@
 cask "yuque" do
   arch arm: "-arm64"
 
-  version "1.6.3"
-  sha256 arm:   "aa011dd6383f693b4e0db07b9e7261d1961d03f3b9f20e9f956f1917099a0638",
-         intel: "b72965de938e9399907bbd53c4e80bd6c6beae4c9eccf4b2aa335b87ee6ad1e9"
+  version "1.7.1"
+  sha256 arm:   "8be364ffa6e932e7f79978638a3021091548964b04a79d85193eafd2dc192f71",
+         intel: "ad9169ab1d4d618cf19892d0d661c116fdd3f8c0f6d103c4d3ec9f34893ef8f7"
 
   url "https://app.nlark.com/yuque-desktop/#{version}/Yuque-#{version}#{arch}.dmg",
       verified: "app.nlark.com/yuque-desktop/"
