73999c8d022b193cca69e83d5bb54ce0e15f7f14
dylan <331506+macdylan@users.noreply.github.com>
2022-09-29 12:18:26 +0800

    add snapmaker-luban 4.4.0

---
+++ Casks/snapmaker-luban.rb
@@ -0,0 +1,27 @@
+cask "snapmaker-luban" do
+  version "4.4.0"
+  sha256 "6d37bbb0d0cb62116b36cf7d55119eabe3f9697518957da7b84cf279d787af06"
+
+  url "https://snapmaker.oss-cn-beijing.aliyuncs.com/snapmaker.com/download/luban/Snapmaker-Luban-#{version}-mac.zip",
+      verified: "snapmaker.oss-cn-beijing.aliyuncs.com"
+  name "Snapmaker Luban"
+  desc "Intuitive and Powerful 3D Printing Software"
+  homepage "https://luban.xyz/"
+
+  livecheck do
+    url "https://snapmaker.oss-cn-beijing.aliyuncs.com/snapmaker.com/download/luban/latest-mac.yml"
+    regex(/version: (\d+(?:\.\d+)+)/i)
+  end
+
+  auto_updates true
+
+  app "Snapmaker Luban.app"
+
+  zap trash: [
+    "~/Library/Caches/com.snapmaker.luban",
+    "~/Library/Caches/com.snapmaker.luban.ShipIt",
+    "~/Library/Preferences/com.snapmaker.luban.plist",
+    "~/Library/Preferences/com.snapmaker.luban.helper.plist",
+    "~/Library/Preferences/ByHost/com.snapmaker.luban.ShipIt.*.plist",
+  ]
+end
