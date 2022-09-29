6799c171076691e90234771607d377684016f25c
Patrick Linnane <patrick@linnane.io>
2022-09-29 07:34:04 -0700

    4k-youtube-to-mp3 4.6.6
    Update 4k-youtube-to-mp3 from 4.6.5 to 4.6.6
    
    Closes #132509.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/4k-youtube-to-mp3.rb
+++ Casks/4k-youtube-to-mp3.rb
@@ -1,7 +1,7 @@
 cask "4k-youtube-to-mp3" do
   # NOTE: "3" is not a version number, but an intrinsic part of the product name
-  version "4.6.5"
-  sha256 "8073bef3bf453d8a64683dbebb9eec0c649e8de54633329eed5ceb1546e573aa"
+  version "4.6.6"
+  sha256 "9d1c53e5199eb6257729b748cb2094758f6326bc8bb725020838fc0478e1ee91"
 
   url "https://dl.4kdownload.com/app/4kyoutubetomp3_#{version}.dmg"
   name "4K YouTube to MP3"
