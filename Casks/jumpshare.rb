210c902f4a62df064b9d59e62a634c0f10d8e450
Patrick Linnane <patrick@linnane.io>
2022-09-28 21:29:08 -0700

    jumpshare 3.2.3,136
    Update jumpshare from 3.2.2 to 3.2.3
    
    Closes #132496.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/jumpshare.rb
+++ Casks/jumpshare.rb
@@ -1,6 +1,6 @@
 cask "jumpshare" do
-  version "3.2.2,133"
-  sha256 "0ebe041776a07ea308e15eadd93faf6652a3a7da751ba428d3713139de3ceb15"
+  version "3.2.3,136"
+  sha256 "0a5519561eb16a1e3030c8e9e3bcc5ece5d5ce05af219c4ba19577057f83ba28"
 
   url "https://d21hi1or3tbtjm.cloudfront.net/desktop/mac/updates/Jumpshare-#{version.csv.first}.tar.bz2",
       verified: "d21hi1or3tbtjm.cloudfront.net/"
