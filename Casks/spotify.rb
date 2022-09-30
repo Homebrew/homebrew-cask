c151cb368156861581f9652af9120838778d6268
thk-cheng <kenneth.cheng.tsun.him@gmail.com>
2022-09-30 09:24:39 +0100

    Update spotify from 1.1.94.872 to 1.1.95.893

--- Casks/spotify.rb
+++ Casks/spotify.rb
@@ -1,7 +1,7 @@
 cask "spotify" do
   arch arm: "ARM64"
 
-  version "1.1.94.872,7a9200fe,18"
+  version "1.1.95.893,6cf4d40c,39"
   sha256 :no_check
 
   url "https://download.scdn.co/Spotify#{arch}.dmg",
