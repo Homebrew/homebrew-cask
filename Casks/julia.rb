ffc8a015050c8ca28c785e2f86239f15a10e1dc6
Kyungdahm Yun <tomyun@gmail.com>
2022-09-29 22:49:37 -0700

    Update julia from 1.8.1 to 1.8.2

--- Casks/julia.rb
+++ Casks/julia.rb
@@ -1,9 +1,9 @@
 cask "julia" do
   arch arm: "aarch64", intel: "x64"
 
-  version "1.8.1"
-  sha256 arm:   "ef489c069304e9d8edbcddb8cecdcd4c52f1f74e818607a95cb268913e05f94f",
-         intel: "3274c3b3dbcb6d34e428797e143380b5c58db6688a5d44f0939f6319bcf3c18a"
+  version "1.8.2"
+  sha256 arm:   "06f2d3de4e514f519153b154f867efbc53f21ddfc897a3ebbaabb3f95b549aa5",
+         intel: "8e1fc578a153f8414f146de8f22acbecfd92d820813c34aa0c1f27e363629e15"
 
   url "https://julialang-s3.julialang.org/bin/mac/#{arch}/#{version.major_minor}/julia-#{version}-mac#{arch.delete_prefix("x")}.dmg"
   name "Julia"
