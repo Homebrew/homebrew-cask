e112ad5858a92c70aacbb69ffc3eb83207f3ef73
Patrick Linnane <patrick@linnane.io>
2022-09-30 20:32:46 -0700

    Update vapor from 3.6.0 to 3.7.0

--- Casks/vapor.rb
+++ Casks/vapor.rb
@@ -1,6 +1,6 @@
 cask "vapor" do
-  version "3.6.0"
-  sha256 "eb77d8c70ac1e03d4c3f79d362d1c6702d87d30ff23472b53773aaeda9dc2dff"
+  version "3.7.0"
+  sha256 "4d165628a8bd25cfe7c132e7d6018150e98f168ba003e0cf5873929c7d1b7b03"
 
   url "https://github.com/NCAR/VAPOR/releases/download/#{version}/VAPOR#{version.major}-#{version}-Darwin.dmg"
   name "VAPOR"
