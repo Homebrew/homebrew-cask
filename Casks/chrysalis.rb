abd97958dfb678d5466f1f6f0073c9f5fade0aad
Trevor Mosey <trevor.mosey@eatstreet.com>
2022-09-30 19:14:57 -0500

    Update chrysalis from 0.11.4 to 0.11.5 (#132565)
    * Update chrysalis from 0.11.4 to 0.11.5
    
    * chrysalis: update livecheck
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/chrysalis.rb
+++ Casks/chrysalis.rb
@@ -1,14 +1,18 @@
 cask "chrysalis" do
-  version "0.11.4"
-  sha256 "9f6f418727df0ce5f6f615a79493e6624492cf0c225e10acf38bcaa0d3c22c30"
+  version "0.11.5"
+  sha256 "576885c6e590d3850d26636e9ec2d9824f373570260177384cec7d475a0c343d"
 
   url "https://github.com/keyboardio/Chrysalis/releases/download/v#{version}/Chrysalis-#{version}.dmg"
   name "Chrysalis"
   desc "Graphical configurator for Kaleidoscope-powered keyboards"
   homepage "https://github.com/keyboardio/Chrysalis"
 
+  # This uses a regex that specifically matches versions from release tag links
+  # for this repository only. A description for a previous release linked to a
+  # tag in a different repository, which led to an incorrect version match.
   livecheck do
     url :url
+    regex(%r{href=.*?/Chrysalis/releases/tag/v?(\d+(?:\.\d+)+)["' >]}i)
     strategy :github_latest
   end
 
