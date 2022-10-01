516496319eaf9d12cd0c55964b9f2371b76386a0
Patrick Linnane <patrick@linnane.io>
2022-09-30 19:38:53 -0700

    deezer 5.30.340
    Update deezer from 5.30.330 to 5.30.340
    
    Closes #132628.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/deezer.rb
+++ Casks/deezer.rb
@@ -1,6 +1,6 @@
 cask "deezer" do
-  version "5.30.330"
-  sha256 "31a503c4603da5efd44aa6bb0cf5eb442acf00ee5fe0030940af6d30e8faee14"
+  version "5.30.340"
+  sha256 "6837b9881da45700cfc2f5fd4c43852d3394f5e489690b617b0939e679b0fa69"
 
   url "https://www.deezer.com/desktop/download/artifact/darwin/x64/#{version}"
   name "Deezer"
