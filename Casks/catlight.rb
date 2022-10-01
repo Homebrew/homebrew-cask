2c10e0c525b267a5f6db32923af7875c11e888c8
Patrick Linnane <patrick@linnane.io>
2022-10-01 09:12:39 -0700

    Fix URL & livecheck

--- Casks/catlight.rb
+++ Casks/catlight.rb
@@ -1,14 +1,14 @@
 cask "catlight" do
   version "2.38.18"
-  sha256 "5915f58d03bccb202ead3f43b41cc4c529e6830ed78c1b6748369ec39b2f5e35"
+  sha256 "dfc80aabb22336d53075f61ec25cb13ae4bcaf6e8848e4600c024314577fd958"
 
-  url "https://download.catlight.io/rel/mac/beta/CatLightSetup-#{version}.zip"
+  url "https://download.catlight.io/rel/mac/release/CatLightSetup-#{version}.zip"
   name "catlight"
   desc "Action center for developers"
   homepage "https://catlight.io/"
 
   livecheck do
-    url "https://catlight.io/downloads/mac/beta"
+    url "https://catlight.io/downloads/mac/release"
     strategy :header_match
   end
 
