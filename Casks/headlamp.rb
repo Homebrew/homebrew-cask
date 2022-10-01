c427f9f8764724b4e02ed78bcc8c5d0de579df4e
Patrick Linnane <patrick@linnane.io>
2022-09-30 19:46:06 -0700

    Update headlamp from 0.12.0 to 0.12.1

--- Casks/headlamp.rb
+++ Casks/headlamp.rb
@@ -1,9 +1,9 @@
 cask "headlamp" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.12.0"
-  sha256 arm:   "b4d11c4b2ed8fc9f273dfe487f4ceb51dd743fc2408e6c7150f7b43d8f692de1",
-         intel: "b94592a566b8bd297d8221bd34a57a6add83d1a6fa5f27fb2bfe2ff688803c3d"
+  version "0.12.1"
+  sha256 arm:   "181572d48843adc708d1e2b65860afd5c7909cecf3cdf50864407a48e50baaa2",
+         intel: "a3519a56b5554fbe9afdfef736c7c5b1655ea83e8b4e4b75e6028055fde45284"
 
   url "https://github.com/kinvolk/headlamp/releases/download/v#{version}/Headlamp-#{version}-mac-#{arch}.dmg",
       verified: "github.com/kinvolk/headlamp/"
