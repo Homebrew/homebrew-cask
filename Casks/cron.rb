5bd291f1cca85fad630a42a91fb9835b811a171c
Patrick Linnane <patrick@linnane.io>
2022-09-30 19:37:01 -0700

    Update cron from 1.109.0 to 1.110.0

--- Casks/cron.rb
+++ Casks/cron.rb
@@ -1,9 +1,9 @@
 cask "cron" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.109.0"
-  sha256 arm:   "d01ada02ea7232147ca2f053a7e000bf00e3bd4ab91800e3e58f438460b7180b",
-         intel: "24ec3d5a709313979ab8cd658e03cfee4ce4dec6201943c2fd8d467376328c43"
+  version "1.110.0"
+  sha256 arm:   "f36b7654dd41cf9c8bb8f1d9b6932fdda513d9d8d0f2592a5ce539dbab59ff6b",
+         intel: "66963577372b84a1ae48c87a92a9402bee3ade555d87f44b29bd14d5a9a42fa9"
 
   url "https://download.todesktop.com/210303leazlircz/Cron%20#{version}-#{arch}-mac.zip",
       verified: "download.todesktop.com/210303leazlircz/"
