cd0d4924797451c435d2b866b57f5541aa5c9885
Brian Gao <30551185+koihia@users.noreply.github.com>
2022-10-02 08:52:01 +0800

    Update mos from 3.3.2 to 3.4.0

--- Casks/mos.rb
+++ Casks/mos.rb
@@ -1,6 +1,6 @@
 cask "mos" do
-  version "3.3.2"
-  sha256 "5ff8bb9022c090457e1a85a8728e8d3cc9216e3d9122af4afe113c392e77cbd8"
+  version "3.4.0"
+  sha256 "01c338db847141b8fc21bba281cdb6138824ede85c98b406345eb8899457476f"
 
   url "https://github.com/Caldis/Mos/releases/download/#{version}/Mos.Versions.#{version}.dmg",
       verified: "github.com/Caldis/Mos/"
