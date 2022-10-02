5d102ab6cd5841834cbf0b5552ab454165d5e2ba
Fran Bolívar <francisco.bolivar.lupianez@gmail.com>
2022-10-02 18:46:11 +0200

    Add random-mouse-clicker

---
+++ Casks/random-mouse-clicker.rb
@@ -0,0 +1,13 @@
+cask "random-mouse-clicker" do
+  version "6.1"
+  sha256 :no_check
+
+  url "https://www.murgaa.com/mac-random-mouse-clicker/download/RandomClicker.zip"
+  name "Random Mouse Clicker"
+  desc "Automate left, right and middle mouse button clicks"
+  homepage "https://www.murgaa.com/"
+
+  auto_updates true
+
+  app "Random Mouse Clicker by MurGaa.com.app"
+end
