ded76deb7f9951992e15f8c489c6dde8747d3527
Patrick Linnane <patrick@linnane.io>
2022-10-03 11:27:15 -0700

    xiv-on-mac 4.3.1
    Update xiv-on-mac from 4.2.3 to 4.3.1
    
    Closes #132788.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/xiv-on-mac.rb
+++ Casks/xiv-on-mac.rb
@@ -1,6 +1,6 @@
 cask "xiv-on-mac" do
-  version "4.2.3"
-  sha256 "b50207cb488cd7962a3447f3c23572f1123eb2771a36fb32cf7c40c0fdfd6b3c"
+  version "4.3.1"
+  sha256 "786cdceb98803ca7511991df48db3f719b8fce9b8331f56a2b9af4c3be45aa25"
 
   url "https://softwareupdate.xivmac.com/sites/default/files/update_data/XIV%20on%20Mac#{version}.tar.xz"
   name "XIV on Mac"
