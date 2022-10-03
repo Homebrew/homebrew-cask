7016e131b0ffa0e34ac39b95e7d7ab538d3d9fec
Patrick Linnane <patrick@linnane.io>
2022-10-03 10:23:37 -0700

    logisim-evolution 3.8.0
    Update logisim-evolution from 3.7.2 to 3.8.0
    
    Closes #132763.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/logisim-evolution.rb
+++ Casks/logisim-evolution.rb
@@ -1,6 +1,6 @@
 cask "logisim-evolution" do
-  version "3.7.2"
-  sha256 "e94ba07fa606c927e6ec68c6d276cd6decb5988f00639f36a6507e847a7c3342"
+  version "3.8.0"
+  sha256 "6e898653e14e62adc724f613f00af6232f6b53e26999c8af099246b2c06ee803"
 
   url "https://github.com/logisim-evolution/logisim-evolution/releases/download/v#{version}/Logisim-evolution-#{version}.dmg"
   name "Logisim Evolution"
