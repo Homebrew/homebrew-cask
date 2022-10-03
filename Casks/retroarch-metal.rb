91ae303cf215ac24c6e869a971a55c0085aaf3f2
Patrick Linnane <patrick@linnane.io>
2022-10-03 11:21:05 -0700

    Update retroarch-metal from 1.10.3 to 1.11.0

--- Casks/retroarch-metal.rb
+++ Casks/retroarch-metal.rb
@@ -1,6 +1,6 @@
 cask "retroarch-metal" do
-  version "1.10.3"
-  sha256 "921eef01a5485dad8ebe98742e60b6cc56a137f0fba478f3c5f132a6c7e4cd67"
+  version "1.11.0"
+  sha256 "34ef4780162817675e1a6d6a54c16c8b5aa187db56f1287f978514d1120506bc"
 
   url "https://buildbot.libretro.com/stable/#{version}/apple/osx/universal/RetroArch_Metal.dmg"
   name "RetroArch"
