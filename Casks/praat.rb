158e4a164baf2521be69be3ec3c6f636f2781777
Patrick Linnane <patrick@linnane.io>
2022-10-03 11:01:00 -0700

    Update praat from 6.2.20 to 6.2.21

--- Casks/praat.rb
+++ Casks/praat.rb
@@ -1,6 +1,6 @@
 cask "praat" do
-  version "6.2.20"
-  sha256 "2654d5aadff277775f2d445c1f31f1363917b62039a7021b10f4dc51ff67c015"
+  version "6.2.21"
+  sha256 "bd6793176af0444ad90e0a5b0e766de6de6d255b208e8049b94d93ace734726b"
 
   url "https://github.com/praat/praat/releases/download/v#{version}/praat#{version.no_dots}_mac.dmg",
       verified: "github.com/praat/praat/"
