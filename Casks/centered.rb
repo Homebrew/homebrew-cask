1bde1141effbb0a861a3207be3b3cf7431fc5dcb
Patrick Linnane <patrick@linnane.io>
2022-10-03 09:34:32 -0700

    centered 5.1.0
    Update centered from 5.0.3 to 5.1.0
    
    Closes #132740.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/centered.rb
+++ Casks/centered.rb
@@ -1,6 +1,6 @@
 cask "centered" do
-  version "5.0.3"
-  sha256 "abcec46021e7368ae12731c2a3456ab39d28e1b319f37ba333f60c1ac20136e2"
+  version "5.1.0"
+  sha256 "5a191807d4485f6a35af55344277dcd567d3490a66cc5cd1ef563fc5bf263b38"
 
   url "https://d3ni5jxar6n4fz.cloudfront.net/Centered-#{version}-universal.dmg",
       verified: "d3ni5jxar6n4fz.cloudfront.net/"
