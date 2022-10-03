523891d00d08b7282f1e1e15fba10cf8b1c08534
Patrick Linnane <patrick@linnane.io>
2022-10-03 10:01:59 -0700

    hepta 0.201.0
    Update hepta from 0.199.0 to 0.201.0
    
    Closes #132755.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/hepta.rb
+++ Casks/hepta.rb
@@ -1,9 +1,9 @@
 cask "hepta" do
   arch arm: "arm64-mac", intel: "mac"
 
-  version "0.199.0"
-  sha256 arm:   "cfc699745c1e8cfd72246515eb82b3c602e1fa9410e7cc9e923c26c4c658bf39",
-         intel: "6a035c78fbd6a90e701ee668094bb891d38d21472b76f1bf62fc270c04673723"
+  version "0.201.0"
+  sha256 arm:   "45643b08de86a83da6465c72b58efbaaf872f8691575826ef8b86ea02c8bfb0f",
+         intel: "94966d3fe96dd54afbc54180d48863b823d45aef0054de4cf7bb145c7b1dc5ac"
 
   url "https://github.com/heptameta/project-meta/releases/download/v#{version}/Hepta-#{version}-#{arch}.zip",
       verified: "github.com/heptameta/project-meta/"
