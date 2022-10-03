440038d7edbe744a1e6dd80731a353f714efeb7a
Miccal Matthews <miccal.matthews@gmail.com>
2022-10-03 11:29:13 +0800

    Update random-mouse-clicker.rb

--- Casks/random-mouse-clicker.rb
+++ Casks/random-mouse-clicker.rb
@@ -1,11 +1,16 @@
 cask "random-mouse-clicker" do
-  version "6.1"
+  version "6.1,1"
   sha256 :no_check
 
   url "https://www.murgaa.com/mac-random-mouse-clicker/download/RandomClicker.zip"
   name "Random Mouse Clicker"
   desc "Automate left, right and middle mouse button clicks"
   homepage "https://www.murgaa.com/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   auto_updates true
 
