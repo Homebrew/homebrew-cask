c0756e02d86ed9b83ec5743e2dd9c712529b910b
Xav! <39637438+sq1000000@users.noreply.github.com>
2022-10-04 12:32:18 -0400

    samplebrain 0.18.3

---
+++ Casks/samplebrain.rb
@@ -0,0 +1,11 @@
+cask "samplebrain" do
+  version "0.18.3"
+  sha256 "addacbdb7f9fff41787c4dcf4a72b46691a567c8876939da7a8e1c67f91f369f"
+
+  url "https://static.thentrythis.org/samplebrain/samplebrain_#{version}_macintel.app.zip"
+  name "samplebrain"
+  desc "A custom sample mashing app designed by Aphex Twin"
+  homepage "https://thentrythis.org/projects/samplebrain/"
+
+  app "samplebrain.app"
+end
