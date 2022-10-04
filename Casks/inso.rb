2fd2830d3db5c4201909008502b98301de3bd00e
Patrick Linnane <patrick@linnane.io>
2022-10-03 18:24:29 -0700

    Update inso from 3.5.0 to 3.6.0 (#132805)
    * Update inso from 3.5.0 to 3.6.0
    
    * Fix livecheck
    
    * Remove redundant url
    
    * Fix regex format

--- Casks/inso.rb
+++ Casks/inso.rb
@@ -1,6 +1,6 @@
 cask "inso" do
-  version "3.5.0"
-  sha256 "1b3b2ec08eaa570ed418d97b708d3d704828eead474cd669efa222335956f767"
+  version "3.6.0"
+  sha256 "3a429d1920c78bbb42b12b262a9c54526f5a82d59f1fa904cff5369aff17af2a"
 
   url "https://github.com/Kong/insomnia/releases/download/lib%40#{version}/inso-macos-#{version}.zip",
       verified: "github.com/Kong/insomnia/"
@@ -9,9 +9,7 @@ cask "inso" do
   homepage "https://insomnia.rest/products/inso"
 
   livecheck do
-    url "https://github.com/Kong/insomnia/releases?q=prerelease%3Afalse+Inso+CLI"
-    strategy :page_match
-    regex(/href=.*?inso-macos-(?:latest-)*(\d+(?:\.\d+)+)\.zip/i)
+    regex(/lib@v?(\d+(?:\.\d+)+)/i)
   end
 
   conflicts_with cask: "homebrew/cask-versions/inso-beta"
