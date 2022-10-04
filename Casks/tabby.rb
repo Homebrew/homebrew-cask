c55403ebb4eae135b57052f22e5379fa8ac94304
Fran Bolívar <francisco.bolivar.lupianez@gmail.com>
2022-10-04 03:25:06 +0200

    Update tabby to 1.0.184 (#132706)
    * Update tabby to 1.0.184
    
    * Update livecheck strategy

--- Casks/tabby.rb
+++ Casks/tabby.rb
@@ -1,9 +1,9 @@
 cask "tabby" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.0.183"
-  sha256 arm:   "cfef3fd184b1bdf5070b5aec7dbeae3aa835f624a233e90e200170d51c7ef5a9",
-         intel: "43ba5fb3d776f2d2ed1128c377aaa9d3420d792579cc9389c32e75b1f9c87a6c"
+  version "1.0.184"
+  sha256 arm:   "aaded8ac525b6a3945f06a789e018368c95112874d6985a34ac8705d1f8eaa74",
+         intel: "c670121c84f19eab809e286f27c4d9922722262cfa4b9611d24cedd347f6808d"
 
   url "https://github.com/Eugeny/tabby/releases/download/v#{version}/tabby-#{version}-macos-#{arch}.zip",
       verified: "github.com/Eugeny/tabby/"
@@ -13,8 +13,8 @@ cask "tabby" do
   homepage "https://eugeny.github.io/tabby/"
 
   livecheck do
-    url :url
-    strategy :github_latest
+    url "https://github.com/Eugeny/tabby/releases/latest"
+    strategy :header_match
   end
 
   auto_updates true
