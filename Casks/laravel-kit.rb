6277768d34b3f8ccdc138d3655e61be1e68ebd3b
Tareque Md Hanif <tarequemd.hanif@yahoo.com>
2022-10-06 02:29:53 +0600

    Update laravel-kit to 2.0.7 (#132841)
    * Update laravel-kit to 2.0.7
    
    * Update laravel-kit.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/laravel-kit.rb
+++ Casks/laravel-kit.rb
@@ -1,14 +1,15 @@
 cask "laravel-kit" do
-  version "2.0.6"
-  sha256 "62c7ca0541b70009c842c672833a7ba269ae8abf4493b52fec9c18465d86ee8c"
+  arch arm: "-arm64", intel: ""
 
-  url "https://github.com/tmdh/laravel-kit/releases/download/v#{version}/laravel-kit-#{version}-mac.zip",
+  version "2.0.7"
+  sha256 arm:   "906209ab2ac81803caffa95227c414dabe7161f5dba075500507d7241b11828a",
+         intel: "fe0a3f8a210c646bfd6e449046a18523f4e4ca2d68d65ade14773b889e6b39d8"
+
+  url "https://github.com/tmdh/laravel-kit/releases/download/v#{version}/laravel-kit-#{version}#{arch}-mac.zip",
       verified: "github.com/tmdh/laravel-kit/"
   name "Laravel Kit"
   desc "Desktop Laravel admin panel app"
   homepage "https://tmdh.github.io/laravel-kit"
-
-  auto_updates true
 
   app "Laravel Kit.app"
 end
