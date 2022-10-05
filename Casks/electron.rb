8420c72d970d3bc5b1f301aba9a7338e864340af
Patrick Linnane <patrick@linnane.io>
2022-10-05 15:32:56 -0700

    Update electron from 21.0.1 to 21.1.0

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,9 +1,9 @@
 cask "electron" do
   arch arm: "arm64", intel: "x64"
 
-  version "21.0.1"
-  sha256 arm:   "0027d3ffe795e44a959e23f0e9e91452e742ea697fc1141eb93f31b840c3a26f",
-         intel: "f1ee563ac2b2defbf856e444c0f8fdbd7afae1a81dc0a29ebb190e2f68f48efd"
+  version "21.1.0"
+  sha256 arm:   "d1d8ba60361809a4f2c9fedcb62d8c7873939e1251629d6dcf9e91660873a75c",
+         intel: "5d987264c0a985d45819034891d7d03bc27989b6f4f84fd86fe1f832055471ee"
 
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-#{arch}.zip",
       verified: "github.com/electron/electron/"
