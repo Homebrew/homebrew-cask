55abda40734583e9f77ab8022b95afe5a329df42
Patrick Linnane <patrick@linnane.io>
2022-10-05 15:15:08 -0700

    confluent-cli 2.28.0
    Update confluent-cli from 2.27.0 to 2.28.0
    
    Closes #132869.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/confluent-cli.rb
+++ Casks/confluent-cli.rb
@@ -1,6 +1,6 @@
 cask "confluent-cli" do
-  version "2.27.0"
-  sha256 "430bf64d331e51c72ca51c4867cd8f18b68e88b5521b911f7c55fe9513f30d21"
+  version "2.28.0"
+  sha256 "83ee9cd27404095a91d1569f1fb27de54347c7083d10c0a2416c7255ad8a4cdf"
 
   url "https://s3-us-west-2.amazonaws.com/confluent.cloud/confluent-cli/archives/#{version}/confluent_v#{version}_darwin_amd64.tar.gz",
       verified: "s3-us-west-2.amazonaws.com/confluent.cloud/confluent-cli/archives/"
