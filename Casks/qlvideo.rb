57eac3e5430665ed7d9487a817dcb24a986b4d6f
Bevan Kay <email@bevankay.me>
2022-10-05 11:42:17 +1100

    qlvideo: update livecheck, add desc (#132814)
    * Update qlvideo from 1.94 to 1.94
    
    * Add desc

--- Casks/qlvideo.rb
+++ Casks/qlvideo.rb
@@ -4,12 +4,13 @@ cask "qlvideo" do
 
   url "https://github.com/Marginal/QLVideo/releases/download/rel-#{version.no_dots}/QLVideo_#{version.no_dots}.pkg"
   name "QuickLook Video"
+  desc "QuickLook generator for video files"
   homepage "https://github.com/Marginal/QLVideo"
 
   livecheck do
-    url "https://github.com/Marginal/QLVideo/releases/latest"
+    url "https://api.github.com/repos/Marginal/QLVideo/releases"
     strategy :page_match do |page|
-      match = page.match(%r{href=.*?/QLVideo_(\d+?)(\d+)\.pkg}i)
+      match = page.match(/QLVideo_(\d+?)(\d+)\.pkg/i)
       next if match.blank?
 
       "#{match[1]}.#{match[2]}"
