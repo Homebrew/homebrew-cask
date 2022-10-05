0540d919d3921ada838c21f2b8fcfa476c2af00c
Jamie Connolly <jamie@jamieconnolly.com>
2022-10-05 21:24:21 +0100

    Add readdle-spark (#132831)
    * Add spark-mail
    
    * Update spark-mail.rb
    
    * Rename spark-mail to readdle-spark
    
    * Update readdle-spark.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/readdle-spark.rb
@@ -0,0 +1,28 @@
+cask "readdle-spark" do
+  version "3.0.29.37433"
+  sha256 "7d12efe925ecdb4736fe6b4f1cae3e9e9b80057ef188b929fc7fc6f4ed116715"
+
+  url "https://downloads.sparkmailapp.com/Spark#{version.major}/mac/dist/#{version}/Spark.zip"
+  name "Spark"
+  desc "Email client"
+  homepage "https://sparkmailapp.com/"
+
+  livecheck do
+    url "https://downloads.sparkmailapp.com/Spark#{version.major}/mac/dist/appcast.xml"
+    strategy :sparkle
+  end
+
+  auto_updates true
+  depends_on macos: ">= :el_capitan"
+
+  app "Spark Desktop.app"
+
+  zap trash: [
+    "~/Library/Application Support/Spark Desktop",
+    "~/Library/Caches/com.readdle.SparkDesktop.helper",
+    "~/Library/Caches/Spark Desktop",
+    "~/Library/Logs/Spark Desktop",
+    "~/Library/Preferences/com.readdle.SparkDesktop.plist",
+    "~/Library/Saved Application State/com.readdle.SparkDesktop.savedState",
+  ]
+end
