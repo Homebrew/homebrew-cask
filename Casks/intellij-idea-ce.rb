164a632fdffd8db51a84ea5b45ecb176b979dd4d
Patrick Linnane <patrick@linnane.io>
2022-10-05 15:47:26 -0700

    intellij-idea-ce 2022.2.3,222.4345.14
    Update intellij-idea-ce from 2022.2.2 to 2022.2.3
    
    Closes #132892.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/intellij-idea-ce.rb
+++ Casks/intellij-idea-ce.rb
@@ -1,9 +1,9 @@
 cask "intellij-idea-ce" do
   arch arm: "-aarch64"
 
-  version "2022.2.2,222.4167.29"
-  sha256 arm:   "dd4b34ba9eab6a812966384d211c76dd7c6389e0b3e50f71ab7e45e8f8c1de7b",
-         intel: "46d103913c4d40b602fc83f0b65020df038dd9c815e169cd1fc79964e99fd616"
+  version "2022.2.3,222.4345.14"
+  sha256 arm:   "333c70caf452034ae332cdded4d24a71592049b4045725eb57826a0b997d1c7a",
+         intel: "6ec3721d9961918a14630eaf068765eeba97e71baecd95ec67510dc25c8bd1b1"
 
   url "https://download.jetbrains.com/idea/ideaIC-#{version.csv.first}#{arch}.dmg"
   name "IntelliJ IDEA Community Edition"
