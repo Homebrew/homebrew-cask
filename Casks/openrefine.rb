dc80effd528fe4ad983c2d1a064fb139c86f9b04
Patrick Linnane <patrick@linnane.io>
2022-10-05 16:07:32 -0700

    Update openrefine from 3.6.1 to 3.6.2

--- Casks/openrefine.rb
+++ Casks/openrefine.rb
@@ -1,6 +1,6 @@
 cask "openrefine" do
-  version "3.6.1"
-  sha256 "4afa441e9aad4b07b637b532a89630ee65345872b00de1c885204d0326ebfc34"
+  version "3.6.2"
+  sha256 "460e801446065d8ef25388d18c65aa0ecd3b889175ebe5eecb69179282be35b1"
 
   url "https://oss.sonatype.org/service/local/artifact/maven/content?r=releases&g=org.openrefine&a=openrefine&v=#{version}&c=mac&p=dmg",
       verified: "oss.sonatype.org/service/local/artifact/maven/"
