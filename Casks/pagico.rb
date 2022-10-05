fba9fa4722f9ad252e2f54f5982a432ddba1536c
Patrick Linnane <patrick@linnane.io>
2022-10-05 16:09:39 -0700

    pagico 10,20221007
    Update pagico from 10,20221002 to 10,20221007
    
    Closes #132908.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/pagico.rb
+++ Casks/pagico.rb
@@ -1,6 +1,6 @@
 cask "pagico" do
-  version "10,20221002"
-  sha256 "e71e1006c3f4c24767324cd448a67c6a8dabe930a7ef98f6b66db5b3ea8180fc"
+  version "10,20221007"
+  sha256 "bacb66afbe9b0a55a33dc8487ce555073502b8aed97ff66cb2831b383ca3343d"
 
   url "https://www.pagico.com/downloads/Pagico_macOS_r#{version.csv.second}.dmg"
   name "Pagico"
