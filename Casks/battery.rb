d90e294b2d091fe699c8bd0952fd1ae809ce0c6f
Miccal Matthews <miccal.matthews@gmail.com>
2022-10-06 05:03:08 +0800

    Update battery.rb

--- Casks/battery.rb
+++ Casks/battery.rb
@@ -3,10 +3,11 @@ cask "battery" do
   sha256 "71e699bcf634f481a0b1c54a2035a04e9e9fec4463c9dbfe95d0370a89aaaabc"
 
   url "https://github.com/actuallymentor/battery/releases/download/v#{version}/battery-#{version}-arm64.dmg"
-  name "battery"
-  desc "GUI & CLI for managing the battery charging status for M1 Macs"
+  name "Battery"
+  desc "CLI for managing the battery charging status"
   homepage "https://github.com/actuallymentor/battery/"
 
+  depends_on arch: :arm64
+
   app "Battery.app"
-  license "MIT"
 end
