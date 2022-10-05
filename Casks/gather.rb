5463958b7402294e087ab84320281424728b91f6
Patrick Linnane <patrick@linnane.io>
2022-10-05 15:39:25 -0700

    gather 0.3.14
    Update gather from 0.3.13 to 0.3.14
    
    Closes #132887.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/gather.rb
+++ Casks/gather.rb
@@ -1,9 +1,9 @@
 cask "gather" do
   arch arm: "-arm64"
 
-  version "0.3.13"
-  sha256 arm:   "0dd178e46cb51812d972a93e1e2e1f9ab9ee3b19a28da730a2fa94d85af53ba4",
-         intel: "4190a12f6611cd9246f1906f134d4fca554fad5928a8d5ab911e7f558b5b459c"
+  version "0.3.14"
+  sha256 arm:   "e5ef249e654826d6c3730ce9e6ca5374f08a35f73e540e2969918f523d6a3b85",
+         intel: "9ab9737856847356f3bd030106e02d520cea0a7d904f84546b8f63c71483d808"
 
   url "https://github.com/gathertown/gather-town-desktop-releases/releases/download/v#{version}/Gather-#{version}#{arch}-mac.zip",
       verified: "github.com/gathertown/gather-town-desktop-releases"
