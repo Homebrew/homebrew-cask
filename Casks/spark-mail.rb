ccd47ae07fd9c794b4cf5e1237a6426c467f887f
Bevan Kay <email@bevankay.me>
2022-10-05 23:17:33 +1100

    Update spark-mail.rb

--- Casks/spark-mail.rb
+++ Casks/spark-mail.rb
@@ -2,13 +2,13 @@ cask "spark-mail" do
   version "3.0.29.37433"
   sha256 "7d12efe925ecdb4736fe6b4f1cae3e9e9b80057ef188b929fc7fc6f4ed116715"
 
-  url "https://downloads.sparkmailapp.com/Spark3/mac/dist/#{version}/Spark.zip"
+  url "https://downloads.sparkmailapp.com/Spark#{version.major}/mac/dist/#{version}/Spark.zip"
   name "Spark Mail"
   desc "Spark helps you take your inbox under control"
   homepage "https://sparkmailapp.com/"
 
   livecheck do
-    url "https://downloads.sparkmailapp.com/Spark3/mac/dist/appcast.xml"
+    url "https://downloads.sparkmailapp.com/Spark#{version.major}/mac/dist/appcast.xml"
     strategy :sparkle
   end
 
