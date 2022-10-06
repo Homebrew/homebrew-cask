3efaf562f405d71ab3a6e8f5a7a634be8ab19f8c
Patrick Linnane <patrick@linnane.io>
2022-10-05 21:05:28 -0700

    bbedit 14.6
    * Convert to on_{macos_version} block
    * Adjust livecheck
    
    Closes #132951.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/bbedit.rb
+++ Casks/bbedit.rb
@@ -1,16 +1,30 @@
 cask "bbedit" do
-  if MacOS.version <= :el_capitan
+  version "14.6"
+  sha256 "9334031d3d43c599382f4b4eda22da12518f054b28c1565bb08a485f99ef7d64"
+
+  on_el_capitan :or_older do
     version "12.1.6"
     sha256 "23b9fc6ef5c03cbcab041566503c556d5baf56b2ec18f551e6f0e9e6b48dc690"
-  elsif MacOS.version <= :high_sierra
+
+    livecheck do
+      skip "Legacy version for El Capitan and earlier"
+    end
+  end
+  on_high_sierra :or_older do
     version "12.6.7"
     sha256 "d0647c864268b187343bd95bfcf490d6a2388579b1f8fce64a289c65341b1144"
-  elsif MacOS.version <= :mojave
+
+    livecheck do
+      skip "Legacy version for Sierra & High Sierra"
+    end
+  end
+  on_mojave do
     version "14.1.2"
     sha256 "d9ce7ef5976c0b8a5037599966ea3979695170b44b03987bb046d7f52af253c0"
-  else
-    version "14.6"
-    sha256 "9334031d3d43c599382f4b4eda22da12518f054b28c1565bb08a485f99ef7d64"
+
+    livecheck do
+      skip "Legacy version for Mojave"
+    end
   end
 
   url "https://s3.amazonaws.com/BBSW-download/BBEdit_#{version}.dmg",
