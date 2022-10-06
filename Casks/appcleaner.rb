2ad75a038d59bdee1414d0bb68f308256250c7c7
Patrick Linnane <patrick@linnane.io>
2022-10-05 20:32:27 -0700

    appcleaner 3.6.4,4270
    * Convert to on_{macos_version} block
    * Adjust livecheck
    
    Closes #132947.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/appcleaner.rb
+++ Casks/appcleaner.rb
@@ -1,10 +1,14 @@
 cask "appcleaner" do
-  if MacOS.version <= :sierra
+  version "3.6.4,4270"
+  sha256 "610c22fda19d558130e742f9c1d5a40587bc182967abdb2e47b4385fdaad1501"
+
+  on_sierra :or_older do
     version "3.4"
     sha256 "0c60d929478c1c91e0bad76d3c04795665c07a05e45e33321db845429c9aefa8"
-  else
-    version "3.6.4,4270"
-    sha256 "610c22fda19d558130e742f9c1d5a40587bc182967abdb2e47b4385fdaad1501"
+
+    livecheck do
+      skip "Legacy version for Sierra and earlier"
+    end
   end
 
   url "https://www.freemacsoft.net/downloads/AppCleaner_#{version.csv.first}.zip"
