e391a60a308555d92784898144c1d66568361219
Moulick Aggarwal <moulickaggarwal@gmail.com>
2022-10-07 00:03:10 +0530

    Update visual-studio-code from 1.71.2 to 1.72.0

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -1,9 +1,9 @@
 cask "visual-studio-code" do
   arch arm: "darwin-arm64", intel: "darwin"
 
-  version "1.71.2"
-  sha256 arm:   "7bc77a7cb5b0521bad5ac9ebab7b41b36694df7d60bb35c860657bda4ba2558c",
-         intel: "c1a2abf468c2f02aebdda433cfd8d1ba290c3215373b2738424dcc2b9eff774a"
+  version "1.72.0"
+  sha256 arm:   "3bb8ec6b0cee4bef7f897e6c0caf6dc12ebd20206b9aee6b43528e4efc704b5d",
+         intel: "4a145c1c06263afc031cd487320c00592ccdf81d5f814e9f2d114f3e92df4d96"
 
   url "https://update.code.visualstudio.com/#{version}/#{arch}/stable"
   name "Microsoft Visual Studio Code"
