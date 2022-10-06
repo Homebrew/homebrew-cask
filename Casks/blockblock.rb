c02ed9e774d8caf7b2e5bb369c808226837e4bb5
Patrick Linnane <patrick@linnane.io>
2022-10-05 21:14:47 -0700

    blockblock 2.1.4
    Convert to on_{macos_version} block
    
    Closes #132953.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/blockblock.rb
+++ Casks/blockblock.rb
@@ -1,5 +1,23 @@
 cask "blockblock" do
-  if MacOS.version <= :mojave
+  version "2.1.4"
+  sha256 "8c12d13c9327701245ccdfee6217296baf4086ae359e025c98c4980a7ce466ee"
+
+  url "https://github.com/objective-see/BlockBlock/releases/download/v#{version}/BlockBlock_#{version}.zip",
+      verified: "github.com/objective-see/BlockBlock/"
+
+  installer script: {
+    executable: "#{staged_path}/BlockBlock Installer.app/Contents/MacOS/BlockBlock Installer",
+    args:       ["-install"],
+    sudo:       true,
+  }
+
+  uninstall script: {
+    executable: "#{staged_path}/BlockBlock Installer.app/Contents/MacOS/BlockBlock Installer",
+    args:       ["-uninstall"],
+    sudo:       true,
+  }
+
+  on_mojave :or_older do
     version "0.9.9.4"
     sha256 "6ab3a8224e8bc77b9abe8d41492c161454c6b0266e60e61b06931fed4b431282"
 
@@ -14,24 +32,6 @@ cask "blockblock" do
 
     uninstall script: {
       executable: "#{staged_path}/BlockBlock Installer.app/Contents/MacOS/BlockBlock",
-      args:       ["-uninstall"],
-      sudo:       true,
-    }
-  else
-    version "2.1.4"
-    sha256 "8c12d13c9327701245ccdfee6217296baf4086ae359e025c98c4980a7ce466ee"
-
-    url "https://github.com/objective-see/BlockBlock/releases/download/v#{version}/BlockBlock_#{version}.zip",
-        verified: "github.com/objective-see/BlockBlock/"
-
-    installer script: {
-      executable: "#{staged_path}/BlockBlock Installer.app/Contents/MacOS/BlockBlock Installer",
-      args:       ["-install"],
-      sudo:       true,
-    }
-
-    uninstall script: {
-      executable: "#{staged_path}/BlockBlock Installer.app/Contents/MacOS/BlockBlock Installer",
       args:       ["-uninstall"],
       sudo:       true,
     }
