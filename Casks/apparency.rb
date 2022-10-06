f953eb27840ef05672a1e65293194a568542a099
Patrick Linnane <patrick@linnane.io>
2022-10-05 20:16:36 -0700

    apparency 1.4.1,218
    * Convert to on_{macos_version} block
    * Adjust livecheck
    
    Closes #132944.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/apparency.rb
+++ Casks/apparency.rb
@@ -1,12 +1,18 @@
 cask "apparency" do
-  if MacOS.version <= :mojave
+  version "1.4.1,218"
+  sha256 :no_check
+
+  url "https://mothersruin.com/software/downloads/Apparency.dmg"
+
+  on_mojave :or_older do
     version "1.3"
-    url "https://www.mothersruin.com/software/downloads/Apparency-#{version}.dmg"
     sha256 "31704bc2d9594bf185bd6dfa6541c986749d524ecdab11cff18c5a5c095e0157"
-  else
-    version "1.4.1,218"
-    url "https://mothersruin.com/software/downloads/Apparency.dmg"
-    sha256 :no_check
+
+    url "https://www.mothersruin.com/software/downloads/Apparency-#{version}.dmg"
+
+    livecheck do
+      skip "Legacy version for Mojave and earlier"
+    end
   end
 
   name "Apparency"
