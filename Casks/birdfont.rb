0143cefd4ebda2d0f589a49e383273904376123c
Patrick Linnane <patrick@linnane.io>
2022-10-05 21:10:30 -0700

    birdfont 4.31.4
    Convert to on_{macos_version} block
    
    Closes #132952.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/birdfont.rb
+++ Casks/birdfont.rb
@@ -1,5 +1,8 @@
 cask "birdfont" do
-  if MacOS.version <= :mojave
+  version "4.31.4"
+  sha256 "2eb96618ba468ff27173d9c327c3818b4c2147f7d483bfe5919d91abb39580aa"
+
+  on_mojave :or_older do
     version "4.17.0"
     sha256 "74c778e3b7598a66b9ddb11b189ba6a21d68263fb0499a965dcedd0ac987b051"
 
@@ -7,20 +10,17 @@ cask "birdfont" do
       url "https://birdfont.org/purchase.php"
       regex(%r{Mac\s*OS\s*10\.12.*?/birdfont[._-]v?(\d+(?:\.\d+)+)[._-]free\.dmg}i)
     end
-  else
-    version "4.31.4"
-    sha256 "2eb96618ba468ff27173d9c327c3818b4c2147f7d483bfe5919d91abb39580aa"
-
-    livecheck do
-      url "https://birdfont.org/purchase.php"
-      regex(%r{Mac\s*OS\s*10\.15.*?/birdfont[._-]v?(\d+(?:\.\d+)+)[._-]free\.dmg}i)
-    end
   end
 
   url "https://birdfont.org/download/birdfont-#{version}-free.dmg"
   name "BirdFont"
   desc "Font editor"
   homepage "https://birdfont.org/"
+
+  livecheck do
+    url "https://birdfont.org/purchase.php"
+    regex(%r{Mac\s*OS\s*10\.15.*?/birdfont[._-]v?(\d+(?:\.\d+)+)[._-]free\.dmg}i)
+  end
 
   depends_on macos: ">= :sierra"
 
