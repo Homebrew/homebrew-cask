9c02fa78fe9260ba7cf821350b93adc88b809d52
Misaka13514 <Misaka13514@gmail.com>
2022-10-07 16:50:52 +0800

    Update chromium-gost from 106.0.5249.91 to 106.0.5249.103

--- Casks/chromium-gost.rb
+++ Casks/chromium-gost.rb
@@ -1,6 +1,6 @@
 cask "chromium-gost" do
-  version "106.0.5249.91"
-  sha256 "b179cadeba9251f57df208a374f5819936f8cf41ede4a5c1a2b3e3198cc785a5"
+  version "106.0.5249.103"
+  sha256 "952a2e9ef390f06000ff8f286645a10fe1c2bf07efac465b3558690056bbbe05"
 
   url "https://github.com/deemru/Chromium-Gost/releases/download/#{version}/chromium-gost-#{version}-macos-amd64.tar.bz2"
   name "Chromium-Gost"
