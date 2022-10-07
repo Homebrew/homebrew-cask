25bfd00e656cec01de4e9081efcffdd5bf0c9808
Yusuf Kursad Kaya <ykursadkaya@gmail.com>
2022-10-07 21:56:35 +0300

    Update lens from 2022.9.280635 to 2022.10.70653

--- Casks/lens.rb
+++ Casks/lens.rb
@@ -1,9 +1,9 @@
 cask "lens" do
   arch arm: "-arm64"
 
-  version "2022.9.280635"
-  sha256 arm:   "1d9316b5c234f98814b82da8951f020b0659a955e84c2ef363430ead68ec414f",
-         intel: "0c4008c3e7e4fa7e929f742becdfe74e9f4e603382f1bbcef9718d66f40f1ba3"
+  version "2022.10.70653"
+  sha256 arm:   "8724ab1e08221db860fa32c7e6eeb828232ffc281025b43f525eb54089ef3c34",
+         intel: "166c53d5b0b2dfe251822decf08b4e0bd5f5302f607abd845791deca351f4ff3"
 
   url "https://api.k8slens.dev/binaries/Lens-#{version}-latest#{arch}.dmg"
   name "Lens"
