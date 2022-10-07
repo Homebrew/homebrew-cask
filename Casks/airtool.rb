7e5178ed2102ae852f7802aaffd5487a01e67045
i0ntempest <szf1234@me.com>
2022-10-07 15:38:25 -0400

    Update airtool from 2.4 to 2.4.1

--- Casks/airtool.rb
+++ Casks/airtool.rb
@@ -1,6 +1,6 @@
 cask "airtool" do
-  version "2.4,16"
-  sha256 "b04145743af00635e17c5159d84b0c2e388f0e75cfef6b271e3e5591b8ee1688"
+  version "2.4.1,17"
+  sha256 "49f118fad997cd4f14da7cf3a8867094fd6ce5894f49c81a7257205393acfa15"
 
   url "https://www.intuitibits.com/downloads/Airtool_#{version.csv.first}.pkg"
   name "Airtool"
