5b29309d0b8ad5cd32044cb3366d5bb8d82ad8ff
Patrick Linnane <patrick@linnane.io>
2022-10-07 09:41:59 -0700

    Update siyuan from 2.2.3 to 2.3.0

--- Casks/siyuan.rb
+++ Casks/siyuan.rb
@@ -1,6 +1,6 @@
 cask "siyuan" do
-  version "2.2.3"
-  sha256 "b08ffa8600c6616f5c0b703bdd6646793baa0364e2378d18d47492c398b1ae46"
+  version "2.3.0"
+  sha256 "d520c8a5464a9f44a556e3279f8359fba6017e939b30cf0fe4a7b44ba04d538b"
 
   url "https://github.com/siyuan-note/siyuan/releases/download/v#{version}/siyuan-#{version}-mac.dmg"
   name "SiYuan"
