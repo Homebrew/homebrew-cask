d76b67d699c78998e681cf3703deba732fab1e66
Patrick Linnane <patrick@linnane.io>
2022-10-07 10:34:55 -0700

    Fix homepage & try hash again

--- Casks/orange.rb
+++ Casks/orange.rb
@@ -1,12 +1,12 @@
 cask "orange" do
   version "3.33.0"
-  sha256 "8ec56a4f8823629ebc38bde82becbd788e2237558f458d2dfc6fd9e6bb370a49"
+  sha256 "3f6e7c91e1b490876193dae25d9460cd8337b88a943a5096e027669be4c34271"
 
   url "https://download.biolab.si/download/files/Orange#{version.major}-#{version}-Python3.8.8.dmg",
       verified: "download.biolab.si/download/"
   name "Orange"
   desc "Component-based data mining software"
-  homepage "hhttps://orangedatamining.com/"
+  homepage "https://orangedatamining.com/"
 
   livecheck do
     url "https://orangedatamining.com/download/#macos"
