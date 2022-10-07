80702c44338018819a384ca0cda71751cacc3a5c
Patrick Linnane <patrick@linnane.io>
2022-10-07 07:25:59 -0700

    bbc-iplayer-downloads 2.13.10
    Update bbc-iplayer-downloads from 2.13.9 to 2.13.10
    
    Closes #132998.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/bbc-iplayer-downloads.rb
+++ Casks/bbc-iplayer-downloads.rb
@@ -1,6 +1,6 @@
 cask "bbc-iplayer-downloads" do
-  version "2.13.9"
-  sha256 "556af7be50baf75456631bcc688565217c693ce2272f29115f3045b677d10233"
+  version "2.13.10"
+  sha256 "866e1aaec2d29fa2ab18afb1a95d1ab37d04e43bf978865cbf37df7e6452d7a4"
 
   url "https://static.files.bbci.co.uk/iplayer-pc-download-app/releases/darwin-x64/BBCiPlayerDownloads-#{version}.dmg"
   name "BBC iPlayer Downloads"
