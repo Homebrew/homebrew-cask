35a5ff6e465481bc5441110f46bbf7d0b4a13544
Patrick Linnane <patrick@linnane.io>
2022-10-07 07:42:13 -0700

    Update cycling74-max from 8.3.2_220913 to 8.3.3_221006

--- Casks/cycling74-max.rb
+++ Casks/cycling74-max.rb
@@ -1,6 +1,6 @@
 cask "cycling74-max" do
-  version "8.3.2_220913"
-  sha256 "7651accfae480ba376a250e6af34912cf8dd7592b90c79460e854df785f235ab"
+  version "8.3.3_221006"
+  sha256 "a23d8e76179161430ceef228d1d09dba977fdb766baa21a26ba75a7aa65defd8"
 
   url "https://akiaj5esl75o5wbdcv2a-maxmspjitter.s3.amazonaws.com/Max#{version.no_dots}.dmg",
       verified: "akiaj5esl75o5wbdcv2a-maxmspjitter.s3.amazonaws.com/"
