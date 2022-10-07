9a1849e840c586ddd465d8ae860123c7406c521e
Patrick Linnane <patrick@linnane.io>
2022-10-07 07:48:48 -0700

    elephicon 2.1.4
    Update elephicon from 2.1.3 to 2.1.4
    
    Closes #133007.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/elephicon.rb
+++ Casks/elephicon.rb
@@ -1,9 +1,9 @@
 cask "elephicon" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.1.3"
-  sha256 arm:   "683aa10a05e9fd8e87ed41d7a6d2679ad903e2af552458ebb5d7392b311f545a",
-         intel: "7617e366384566b560ff49eabd302ee8cb765509ddebb76e9e78593a4c2eec63"
+  version "2.1.4"
+  sha256 arm:   "5ede4a09de592fc59d68904891acc3e03743a14b59f2e23624ab9ca40656ca13",
+         intel: "9dde2d8b944a98336bd77b6f7b8c2d48c1bef5ffb0b79baf55dd5c4edd4f7742"
 
   url "https://github.com/sprout2000/elephicon/releases/download/v#{version}/Elephicon-#{version}-darwin-#{arch}.dmg"
   name "Elephicon"
