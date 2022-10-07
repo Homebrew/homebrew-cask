f02eb7bd51e37b3eabebdf213b5a8ebacb716e21
Patrick Linnane <patrick@linnane.io>
2022-10-07 08:00:30 -0700

    gridtracker 1.22.1006,1.22.1006
    Update gridtracker from 1.22.0903 to 1.22.1006
    
    Closes #133017.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/gridtracker.rb
+++ Casks/gridtracker.rb
@@ -1,6 +1,6 @@
 cask "gridtracker" do
-  version "1.22.0903,1.22.0903"
-  sha256 "c85d88bb128da6e7a83409d78f10d7b38ed4cd4f0c4960648e23916252bff5e2"
+  version "1.22.1006,1.22.1006"
+  sha256 "c6bc3df2245cd7d95e1f28ea2d4a3311a38ddd9d86372542e6a31812027214c1"
 
   url "https://storage.googleapis.com/gt_download/v#{version.csv.second}/GridTracker-#{version.csv.first}-mac-x64.zip",
       verified: "storage.googleapis.com/gt_download/"
