71b5ceee3279c2b1b192511bf89924ece24079b6
Patrick Linnane <patrick@linnane.io>
2022-10-07 20:22:30 -0700

    Fix livecheck

--- Casks/subsync.rb
+++ Casks/subsync.rb
@@ -9,7 +9,7 @@ cask "subsync" do
   homepage "https://subsync.online/"
 
   livecheck do
-    url "https://github.com/sc0ty/subsync/releases/"
+    url "https://subsync.online/en/download.html"
     strategy :page_match
     regex(%r{href=.*?/subsync[._-]v?(\d+(?:\.\d+)+)-mac-x86_64\.dmg}i)
   end
