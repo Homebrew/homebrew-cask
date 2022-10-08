8805b08578601c84a080ee2826d9b8a4eaa67292
frodera <219724+frodera@users.noreply.github.com>
2022-10-08 12:35:43 +1100

    Update eloston-chromium from 106.0.5249.91-1.1 to 106.0.5249.103-1.1 (#133067)

--- Casks/eloston-chromium.rb
+++ Casks/eloston-chromium.rb
@@ -2,12 +2,12 @@ cask "eloston-chromium" do
   arch arm: "arm64", intel: "x86-64"
 
   on_intel do
-    version "106.0.5249.91-1.1,1664706754"
-    sha256 "54d64006ec85caba6f92e061d72d74b190dadda3f8e1dd29ef455d7e1589acd4"
+    version "106.0.5249.103-1.1,1665122438"
+    sha256 "58725d236967bebe19af8bcf34fa6c6b5aeffd07b113d566bd78f9dbff84633e"
   end
   on_arm do
-    version "106.0.5249.91-1.1,1664733785"
-    sha256 "0ede52eaec1a181e2b68b7cefb956082cfff9ba1eb4331ef6337d94dfd644ebc"
+    version "106.0.5249.103-1.1,1665168352"
+    sha256 "08a9078a74d6a6fb16864e1e91f9f46c2b01392136dd353bc7e403b9c937f5d3"
   end
 
   url "https://github.com/kramred/ungoogled-chromium-macos/releases/download/#{version.csv.first}_#{arch}__#{version.csv.second}/ungoogled-chromium_#{version.csv.first}_#{arch}-macos.dmg",
