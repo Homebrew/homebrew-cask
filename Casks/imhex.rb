733781c30e4a878f698cec990cb62176b9318aff
Patrick Linnane <patrick@linnane.io>
2022-10-08 08:42:56 -0700

    imhex 1.24.0
    Update imhex from 1.23.2 to 1.24.0
    
    Closes #133105.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/imhex.rb
+++ Casks/imhex.rb
@@ -1,6 +1,6 @@
 cask "imhex" do
-  version "1.23.2"
-  sha256 "32c7f29e124c97e9e2f4e57a2d896e2e55a21da47206f9b2e14a549b33aced3b"
+  version "1.24.0"
+  sha256 "64a436bc503b6fd2b4f0a7db2c91660250c89f3632d768718b8a49f151b67186"
 
   url "https://github.com/WerWolv/ImHex/releases/download/v#{version}/imhex-#{version}-macOS.dmg",
       verified: "github.com/WerWolv/ImHex/"
