090c3265899ee8a1df14096bfee2daa2ae26caf8
Amory Meltzer <Amorymeltzer@gmail.com>
2022-10-08 08:55:32 -0400

    Update diffusionbee from 1.1.0 to 1.2.0

--- Casks/diffusionbee.rb
+++ Casks/diffusionbee.rb
@@ -1,6 +1,6 @@
 cask "diffusionbee" do
-  version "1.1.0"
-  sha256 "062a025c2a2b2328760e153eb0124ca87554bb2173a8899a5312f46b7d5689a5"
+  version "1.2.0"
+  sha256 "81e43f7d3416d6fd1ddd5fa8428d51383c91cd4810d3ae020bc636b568878e2f"
 
   url "https://github.com/divamgupta/diffusionbee-stable-diffusion-ui/releases/download/#{version}/DiffusionBee-#{version}-arm64.dmg",
       verified: "github.com/divamgupta/diffusionbee-stable-diffusion-ui"
