4e822680d989cfc5cb6c518227a1dd06bd034887
Nobody <acc.4code@gmail.com>
2022-10-09 02:05:37 +0800

    v2rayu: fix uninstall script

--- Casks/v2rayu.rb
+++ Casks/v2rayu.rb
@@ -14,4 +14,16 @@ cask "v2rayu" do
   depends_on macos: ">= :sierra"
 
   app "V2rayU.app"
+  
+  uninstall delete:    [
+              "~/Library/LaunchAgents/yanue.v2rayu.v2ray-core.plist",
+              "~/Library/Preferences/net.yanue.V2rayU.plist",
+              "~/Library/Logs/V2rayU.log",
+              "~/.V2rayU/",
+            ],
+            launchctl: [
+              "yanue.v2rayu.v2ray-core",
+              "yanue.v2rayu.http",
+            ]
+
 end
