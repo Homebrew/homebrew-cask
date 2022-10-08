b48c89990498cc640f880b9a7dc70dea49ad4258
Patrick Linnane <patrick@linnane.io>
2022-10-08 08:40:40 -0700

    Update gaphor from 2.12.0 to 2.12.1

--- Casks/gaphor.rb
+++ Casks/gaphor.rb
@@ -1,6 +1,6 @@
 cask "gaphor" do
-  version "2.12.0"
-  sha256 "574a1260a8b399547566ab053a80896409dfac8d85950475e3e1c112b72ae661"
+  version "2.12.1"
+  sha256 "dec6fbbd8d85b7ccc31453f21a6e0998f6d6b253236dd6464d9eed7b0b56302b"
 
   url "https://github.com/gaphor/gaphor/releases/download/#{version}/Gaphor-#{version}.dmg",
       verified: "github.com/gaphor/gaphor/"
