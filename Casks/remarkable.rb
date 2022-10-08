1ff18ceff0fd8e1172c89f00a113f3c9277e96b0
Patrick Linnane <patrick@linnane.io>
2022-10-08 11:34:44 -0700

    Update remarkable from 2.14.3.623 to 2.15.0.643

--- Casks/remarkable.rb
+++ Casks/remarkable.rb
@@ -1,8 +1,8 @@
 cask "remarkable" do
-  version "2.14.3.623,40828928"
-  sha256 "0bad462af1a1abdc4787e13b1c97a7eaa20aadb2465c733bde33dfcb0f03bfe3"
+  version "2.15.0.643,42139648"
+  sha256 "1829af522e566f4e848f391a4af18979b1e2d3109a4d1ffb717cda2bfc81851d"
 
-  url "https://updates-download.cloud.remarkable.engineering/sparkle/reMarkableMacOs/Prod/#{version.csv.second}/reMarkable-#{version.csv.first}.dmg",
+  url "https://updates-download.cloud.remarkable.engineering/sparkle/reMarkableMacOs/Dev/#{version.csv.second}/reMarkable-#{version.csv.first}.dmg",
       verified: "updates-download.cloud.remarkable.engineering/sparkle/reMarkableMacOs/"
   name "Remarkable"
   desc "View, Screen Share, organize, import, and download files to a reMarkable device"
