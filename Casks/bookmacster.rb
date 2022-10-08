e622f377627083bbc35a96c4f8e87015fac7bce1
Patrick Linnane <patrick@linnane.io>
2022-10-08 12:14:43 -0700

    bookmacster 3.0.5
    * Update bookmacster from 3.0.1 to 3.0.5
    * Version url, add shasum
    
    Closes #133141.
    
    Co-authored-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/bookmacster.rb
+++ Casks/bookmacster.rb
@@ -1,8 +1,8 @@
 cask "bookmacster" do
-  version "3.0.1"
-  sha256 :no_check
+  version "3.0.5"
+  sha256 "0480a2b08d3592f5e2621472ae677327f7c79d2a481ed0f085ecdd179b8de5f0"
 
-  url "https://sheepsystems.com/bookmacster/BookMacster.zip"
+  url "https://sheepsystems.com/bookmacster/BookMacster_#{version}.zip"
   name "BookMacster"
   desc "Bookmarks manager"
   homepage "https://sheepsystems.com/products/bookmacster.html"
