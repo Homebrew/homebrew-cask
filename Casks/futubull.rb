7cd384fee5cfadd48678599b2d1bd2566ca6b96d
Patrick Linnane <patrick@linnane.io>
2022-10-08 10:28:54 -0700

    futubull 12.10.2918
    Update futubull from 12.7.2608 to 12.10.2918
    
    Closes #133125.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/futubull.rb
+++ Casks/futubull.rb
@@ -1,6 +1,6 @@
 cask "futubull" do
-  version "12.7.2608"
-  sha256 "22bf6498713de8b0751a33490d6840764ccfaea872fcd8beceaa29bed1b3eeab"
+  version "12.10.2918"
+  sha256 "591204cbbac13a01b4da2f54f884091459b73cf883ce2b3fdbba6928d9296fed"
 
   url "https://softwarefile.futunn.com/FTNNForMac_#{version}_Website.dmg"
   name "Futubull"
@@ -10,7 +10,7 @@ cask "futubull" do
 
   livecheck do
     url "https://www.futunn.com/download/history?client=11"
-    regex(/FTNN[._-]v?(\d+(?:\.\d+)+)[._-]Website\.dmg/i)
+    regex(/FTNNForMac[._-]v?(\d+(?:\.\d+)+)[._-]Website\.dmg/i)
   end
 
   # Renamed for consistency: app name is different in the Finder and in a shell.
