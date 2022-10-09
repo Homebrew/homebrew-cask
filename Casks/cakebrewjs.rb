454393f676b2fb4e366766b5634b0cfc18e2bcfa
osx user <gili.shemesh@gmail.com>
2022-10-09 15:02:59 +0300

    cakebrewjs 1.2.1
    Update cakebrewjs from 1.1.1 to 1.2.1
    
    Closes #133151.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/cakebrewjs.rb
+++ Casks/cakebrewjs.rb
@@ -1,6 +1,6 @@
 cask "cakebrewjs" do
-  version "1.1.1"
-  sha256 "b7d12b6588c92715f5488e987e892530d39f88fa56c10a2d7196b933d0aee237"
+  version "1.2.1"
+  sha256 "cca2e657dcdea0b1cf146187563e5ab12533b62618ff6227bd2e2b5d5299c4c0"
 
   url "https://downloads.sourceforge.net/cakebrewjs/v#{version}/cakebrewjs-#{version}-mac.zip"
   name "cakebrewjs"
