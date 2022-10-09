b3381626f2509499f989e38bb6833f2cea8ac4dd
dandanio <responder@pacanka.com>
2022-10-09 11:25:37 -0400

    backuploupe 3.8,2452
    Update backuploupe.rb
    
    New version of BackupLoupe with new shasum.
    
    Closes #133152.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/backuploupe.rb
+++ Casks/backuploupe.rb
@@ -1,6 +1,6 @@
 cask "backuploupe" do
-  version "3.8,2448"
-  sha256 "a4363b250bbccf63fc71488bd8ea1741fa85b307f2c36577cd473d56eb732f9b"
+  version "3.8,2452"
+  sha256 "7bd9b80cd740b9008ad8d50c4ae56703c45374b090985faa98b47a7f16fcb1a1"
 
   url "https://www.soma-zone.com/download/files/BackupLoupe-#{version.csv.first}.tar.xz"
   name "BackupLoupe"
