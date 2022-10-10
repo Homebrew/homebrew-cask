c741a8c5a0726783c3b886fba6cc3c982096bbce
Patrick Linnane <patrick@linnane.io>
2022-10-10 08:47:10 -0700

    Update shortcat from 0.10.3 to 0.10.4

--- Casks/shortcat.rb
+++ Casks/shortcat.rb
@@ -1,6 +1,6 @@
 cask "shortcat" do
-  version "0.10.3,170"
-  sha256 "ee09f9d90ffd283a12f5922afe0e0c29baf10b90bba0a752cb781eb2126cf54f"
+  version "0.10.4,171"
+  sha256 "76ef691f935fb68e6e33319bec3ebebde882580604cd36d1c5f2478b0f558240"
 
   url "https://files.shortcat.app/releases/v#{version.csv.first}/Shortcat.zip"
   name "Sproutcube Shortcat"
