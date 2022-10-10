94c33f8348e6b9482e6677f7d8ae98fff179f769
Kai Winter <kaiwinter@gmx.de>
2022-10-10 13:20:55 +0200

    Update dbeaver-community from 22.2.1 to 22.2.2

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,9 +1,9 @@
 cask "dbeaver-community" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "22.2.1"
-  sha256 arm:   "5d4743eb00f966a418dec0870d17b9f1ad3631650aa0485b1457bec8d7f69551",
-         intel: "51e3de2b3d85e5afdd2d520985852888989d8f928804e26a26021643c5d141f7"
+  version "22.2.2"
+  sha256 arm:   "def1b9f89c82bb040bff8d477ae53af79e9e9650f81d0b423d0ef95f47574844",
+         intel: "ff0a782501b1272816ee621066edc2abc5377561db0497d7542e76a059caede3"
 
   url "https://dbeaver.io/files/#{version}/dbeaver-ce-#{version}-macos-#{arch}.dmg"
   name "DBeaver Community Edition"
