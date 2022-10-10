6235ad4b97fb87675627ae518251a4f9a35434ee
Patrick Linnane <patrick@linnane.io>
2022-10-10 08:43:21 -0700

    Update querious from 3.3.1 to 3.3.2

--- Casks/querious.rb
+++ Casks/querious.rb
@@ -1,6 +1,6 @@
 cask "querious" do
-  version "3.3.1"
-  sha256 "38fae26c271359ea7d40d7ac9ff16c2bbb13433c1d6ce3178acb1e8e3f998742"
+  version "3.3.2"
+  sha256 "74016295ef07865c04405a2ff7593377544f60d0a930f219e4689e21ae733041"
 
   url "https://www.araelium.com/querious/downloads/versions/Querious#{version}.zip"
   name "Querious #{version.major}"
