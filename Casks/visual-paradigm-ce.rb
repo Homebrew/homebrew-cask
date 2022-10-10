59e858efd187e37188fb63765fffe42e22ab076b
Patrick Linnane <patrick@linnane.io>
2022-10-10 09:05:55 -0700

    Update visual-paradigm-ce from 17.0,20220913 to 17.0,20221001

--- Casks/visual-paradigm-ce.rb
+++ Casks/visual-paradigm-ce.rb
@@ -1,9 +1,9 @@
 cask "visual-paradigm-ce" do
   arch arm: "AArch64", intel: "WithJRE"
 
-  version "17.0,20220913"
-  sha256 arm:   "775d43535931d596483ba2cf21935060497c8506ba7b70203256f510b10a4dd1",
-         intel: "d9b72513e2e6b57cbb15833fbe71ef335e0852ca489b04547a763ed3e9792801"
+  version "17.0,20221001"
+  sha256 arm:   "5c338022a3d44fbf7672579eafe71917e5d365d0a79126f15d5f6549603b650b",
+         intel: "c727c33b3d55418286882337b3354c0f055ef2e3f6ba61ddebc06422f0eac01f"
 
   url "https://www.visual-paradigm.com/downloads/vpce/Visual_Paradigm_CE_#{version.csv.first.dots_to_underscores}_#{version.csv.second}_OSX_#{arch}.dmg"
   name "Visual Paradigm Community Edition"
