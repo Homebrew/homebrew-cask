aaeeae3c86e037ea5e84b3687e47705dc8e52f87
singularitti <singularitti@outlook.com>
2022-10-10 07:25:26 -0400

    zoom 5.12.2.11434
    Update zoom from 5.12.0.11129 to 5.12.2.11434
    
    Closes #133169.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/zoom.rb
+++ Casks/zoom.rb
@@ -1,9 +1,9 @@
 cask "zoom" do
   arch arm: "arm64/"
 
-  version "5.12.0.11129"
-  sha256 arm:   "d2025d40c66fea528607e01c565ba4cccb7314f9bec451c8229a94d63640038b",
-         intel: "765da4b1d6d3e53489108650e3a84e9c51fde1550b6800c6de667ec2365c0a31"
+  version "5.12.2.11434"
+  sha256 arm:   "16682c7580ade4dc20cf6da02bb5db336470ea6be0470d04b78e2a8121b10be1",
+         intel: "dae93271f55a6c9524199d1bf682c98cdcb99ab47a4110595b145246df943bfc"
 
   url "https://cdn.zoom.us/prod/#{version}/#{arch}Zoom.pkg"
   name "Zoom.us"
