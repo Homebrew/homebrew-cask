5c436bb23e820f6185c3692ce9df54543413f9f6
eloyaldea <eloy.aldea@gmail.com>
2022-10-11 00:34:01 +0200

    streamlabs-obs 1.11.1
    Update streamlabs-obs from 1.11.0 to 1.11.1
    
    Closes #133224.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/streamlabs-obs.rb
+++ Casks/streamlabs-obs.rb
@@ -1,6 +1,6 @@
 cask "streamlabs-obs" do
-  version "1.11.0"
-  sha256 "84e02aee22afbb3fd25e456a02face15eddd28cd41fa1610b92508988c367290"
+  version "1.11.1"
+  sha256 "b0d651cdd4638a2d49bce72e1602b2226745bfd24951abcb0bd85899d6d7ab4b"
 
   url "https://slobs-cdn.streamlabs.com/Streamlabs+Desktop-#{version}.dmg"
   name "Streamlabs Desktop"
