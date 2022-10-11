21501e2759bb0066c452942192c707e92377bd97
Miccal Matthews <miccal.matthews@gmail.com>
2022-10-11 14:49:54 +0800

    Update monal.rb

--- Casks/monal.rb
+++ Casks/monal.rb
@@ -1,14 +1,14 @@
 cask "monal" do
-  version "821"
-  sha256 :no_check
+  version "822"
+  sha256 "663f837f25df1d5dc482822064aee2a44639b92560611a545f8d5b018bf07be6"
 
-  url "https://downloads.monal-im.org/monal-im/beta/macOS/Monal-#{version}.zip"
+  url "https://downloads.monal-im.org/monal-im/stable/macOS/Monal-#{version}.zip"
   name "Monal"
   desc "Tool to securely connect to chat servers"
   homepage "https://monal-im.org/"
 
   livecheck do
-    url "https://downloads.monal-im.org/monal-im/beta/macOS/latest.txt"
+    url "https://downloads.monal-im.org/monal-im/stable/macOS/latest.txt"
     regex(/^(\d+)$/i)
   end
 
