daffb0f8ec4de0b74ddeff60c5203f056b7ceeab
Patrick Linnane <patrick@linnane.io>
2022-10-10 20:37:45 -0700

    gitkraken 8.10.0
    Update gitkraken from 8.9.1 to 8.10.0
    
    Closes #133241.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/gitkraken.rb
+++ Casks/gitkraken.rb
@@ -1,9 +1,9 @@
 cask "gitkraken" do
   arch arm: "darwin-arm64", intel: "darwin"
 
-  version "8.9.1"
-  sha256 arm:   "a12d54a639d6fefa6b9b3d27a13610311da4af8b3f445d3e63066e4276fd4974",
-         intel: "1af1def7150d7d736ee5e94343789eccd860ea9c68e5b8fb042de1b7bfebe1d4"
+  version "8.10.0"
+  sha256 arm:   "67d13e59e9ae82e5369347042f3932c632bacbc213fcd44a27c7fa1dd235aee7",
+         intel: "41993a5c299a822ee779e49d97a5db2c53401d40218001a0e63d6112986a3f1b"
 
   url "https://release.axocdn.com/#{arch}/GitKraken-v#{version}.zip",
       verified: "release.axocdn.com/"
