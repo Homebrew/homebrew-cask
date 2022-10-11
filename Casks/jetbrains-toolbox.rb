feeb23564ad1cbb7f8758174e1364f7add7dd806
Moulick Aggarwal <moulickaggarwal@gmail.com>
2022-10-11 22:47:55 +0530

    Update jetbrains-toolbox from 1.26.1 to 1.26.2

--- Casks/jetbrains-toolbox.rb
+++ Casks/jetbrains-toolbox.rb
@@ -1,9 +1,9 @@
 cask "jetbrains-toolbox" do
   arch arm: "-arm64"
 
-  version "1.26.1,1.26.1.13138"
-  sha256 arm:   "52fb5ad0457e9eda36636f6538977866aae43e3c9b0e8b0009e4662231812d30",
-         intel: "e4f9d3a2d8d76b742d9159c3e388fef50c89ffcc03dd542ec67e6b163aebf332"
+  version "1.26.2,1.26.2.13244"
+  sha256 arm:   "5f5f27db1f9a3b53f98c25faaf5bef139d2ca2539e3dd3e562ce540c37913515",
+         intel: "4df76bec1cfb06d9c760afb33cb8a1ecf72dceaa18445fe553a273984cf4f783"
 
   url "https://download.jetbrains.com/toolbox/jetbrains-toolbox-#{version.csv.second}#{arch}.dmg"
   name "JetBrains Toolbox"
