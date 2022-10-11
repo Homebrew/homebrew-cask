eb1ab3099879b198a2793d907358e590b7ba5fe8
luca <luca@palli.ch>
2022-10-11 06:11:24 +0200

    Update silentknight from 1.21 to 2.0.0

--- Casks/silentknight.rb
+++ Casks/silentknight.rb
@@ -1,6 +1,6 @@
 cask "silentknight" do
-  version "1.21,2022.06"
-  sha256 "c1cbb734f620e073f1c08c473edaa036c2b5ccdca02baa99ca117f86c10ad505"
+  version "2.0.0,2022.10"
+  sha256 "a5fe4f2c758487b6a9d61bab39001771fa74b40b66c01977249bc9b5a2961939"
 
   url "https://eclecticlightdotcom.files.wordpress.com/#{version.csv.second.major}/#{version.csv.second.minor}/silentknight#{version.csv.first.no_dots}.zip",
       verified: "eclecticlightdotcom.files.wordpress.com/"
