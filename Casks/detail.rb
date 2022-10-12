a8c83d13e898d75b0328253e40535378b21af43f
Patrick Linnane <patrick@linnane.io>
2022-10-11 19:59:53 -0700

    Update detail from 3.3.2 to 3.4.0

--- Casks/detail.rb
+++ Casks/detail.rb
@@ -1,6 +1,6 @@
 cask "detail" do
-  version "3.3.2"
-  sha256 "7dbaa0536d081335a5eca08fc6e4ee4d74636a2a1ae497b49d5cde1d0af76d26"
+  version "3.4.0"
+  sha256 "82daf6a81f1bd94527620bc6e09a03ab7e618993a3dbad9fbac35327bef7bed0"
 
   url "https://detail-builds.s3.amazonaws.com/mac/#{version}/Detail.pkg",
       verified: "detail-builds.s3.amazonaws.com/mac/"
