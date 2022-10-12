7552ec3f304324575a88404a39cc51be31aec94f
Patrick Linnane <patrick@linnane.io>
2022-10-11 19:54:45 -0700

    Update cloudflare-warp from 2022.9.384.0 to 2022.9.582.0

--- Casks/cloudflare-warp.rb
+++ Casks/cloudflare-warp.rb
@@ -1,5 +1,5 @@
 cask "cloudflare-warp" do
-  version "2022.9.384.0,20220930.5"
+  version "2022.9.582.0,20221011.16"
   sha256 :no_check
 
   url "https://1111-releases.cloudflareclient.com/mac/Cloudflare_WARP.zip",
