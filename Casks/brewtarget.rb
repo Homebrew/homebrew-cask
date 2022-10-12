becd7f398fc20bb737685bfbd49b63036fbd3c33
Patrick Linnane <patrick@linnane.io>
2022-10-12 12:23:40 -0700

    brewtarget 3.0.1
    * Update brewtarget from 3.0.0 to 3.0.1
    * Fix artifact
    
    Closes #133362.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/brewtarget.rb
+++ Casks/brewtarget.rb
@@ -1,18 +1,11 @@
 cask "brewtarget" do
-  version "2.3.0"
-  sha256 "f1b3b33029f7d7cd873a1cdbf0741da22085f53dcefa2943dcfa8d35eda89fdd"
+  version "3.0.1"
+  sha256 "6d022a29698c46a70daecc5f5a3b4bbf5d44c760c92a9b62eaedcfb79539c585"
 
-  url "https://launchpad.net/brewtarget/trunk/#{version}/+download/Brewtarget-#{version}.dmg",
-      verified: "launchpad.net/brewtarget/"
+  url "https://github.com/Brewtarget/brewtarget/releases/download/v#{version}/brewtarget_#{version}_x86_64.dmg"
   name "brewtarget"
-  homepage "http://www.brewtarget.org/"
+  desc "Beer recipe creation tool"
+  homepage "https://github.com/Brewtarget/brewtarget/"
 
-  # We need to check all releases since not all releases are for macOS.
-  livecheck do
-    url "https://github.com/Brewtarget/brewtarget/releases"
-    strategy :page_match
-    regex(%r{href=.*?/Brewtarget[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
-  end
-
-  app "brewtarget.app"
+  app "brewtarget_#{version}.app"
 end
