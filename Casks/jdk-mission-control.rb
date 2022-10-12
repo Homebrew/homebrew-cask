136d96f8ffe02a593ef5af31a19ce1243fafe981
Miccal Matthews <miccal.matthews@gmail.com>
2022-10-12 19:33:57 +0800

    Update jdk-mission-control.rb

--- Casks/jdk-mission-control.rb
+++ Casks/jdk-mission-control.rb
@@ -2,8 +2,8 @@ cask "jdk-mission-control" do
   arch arm: "aarch64", intel: "x64"
 
   version "8.2.1,03"
-  sha256 intel: "0d320e08f5430ecd4389c66accaac2fdc4a33de236ddb26facf256f1bf93bca8",
-         arm:   "e974cdee33b762a5841af7df4b9bd7e837fe70c17ba0c4288b94d15c15303c0d"
+  sha256 arm:   "e974cdee33b762a5841af7df4b9bd7e837fe70c17ba0c4288b94d15c15303c0d",
+         intel: "0d320e08f5430ecd4389c66accaac2fdc4a33de236ddb26facf256f1bf93bca8"
 
   url "https://download.java.net/java/GA/jmc#{version.major}/#{version.csv.second}/binaries/jmc-#{version.csv.first}_osx-#{arch}.tar.gz"
   name "JDK Mission Control"
@@ -13,7 +13,7 @@ cask "jdk-mission-control" do
   livecheck do
     url :homepage
     strategy :page_match do |page|
-      match = page.match(%r{href=.*?/(\d+)/binaries/jmc-(\d+(?:\.\d+)*)_osx-x64.tar\.gz}i)
+      match = page.match(%r{href=.*?/(\d+)/binaries/jmc[._-]v?(\d+(?:\.\d+)*)[._-]osx[._-]#{arch}\.tar\.gz}i)
       next if match.blank?
 
       "#{match[2]},#{match[1]}"
