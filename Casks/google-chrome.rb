b0091711965d10d1315c26ccbd8578f77bcc843f
Branch Vincent <branchevincent@gmail.com>
2022-10-11 20:33:58 -0400

    Update google-chrome from 106.0.5249.103 to 106.0.5249.119

--- Casks/google-chrome.rb
+++ Casks/google-chrome.rb
@@ -1,5 +1,5 @@
 cask "google-chrome" do
-  version "106.0.5249.103"
+  version "106.0.5249.119"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
