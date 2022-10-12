fea0c40407e8774a45bded06b5c8bde96a9f7049
Patrick Linnane <patrick@linnane.io>
2022-10-12 13:14:08 -0700

    Update milanote from 3.3.9 to 3.3.10

--- Casks/milanote.rb
+++ Casks/milanote.rb
@@ -1,6 +1,6 @@
 cask "milanote" do
-  version "3.3.9"
-  sha256 "9606e6de8638e53d97d416ba80e03837c64016416258780a1d9d2c55147fb2bb"
+  version "3.3.10"
+  sha256 "ad6078454b4eb697f27c6405924d00ed0636dbc1343f7b21ab3e13ef3d9f387a"
 
   url "https://milanote-app-releases.s3.amazonaws.com/Milanote-#{version}.dmg",
       verified: "milanote-app-releases.s3.amazonaws.com/"
