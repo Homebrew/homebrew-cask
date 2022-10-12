d912db57423be75a097328aab471f7154a2ddb77
zsoltion <zsoltion@users.noreply.github.com>
2022-10-12 02:36:20 +0200

    Update adobe-acrobat-pro from 22.002.20212 to 22.003.20258 (#133292)
    * Update adobe-acrobat-pro from 22.002.20212 to 22.003.20258
    
    Update adobe-acrobat-pro from 22.002.20212 to 22.003.20258
    
    * Update adobe-acrobat-pro.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/adobe-acrobat-pro.rb
+++ Casks/adobe-acrobat-pro.rb
@@ -1,5 +1,5 @@
 cask "adobe-acrobat-pro" do
-  version "22.002.20212"
+  version "22.003.20258"
   sha256 :no_check
 
   url "https://trials.adobe.com/AdobeProducts/APRO/Acrobat_HelpX/osx10/Acrobat_DC_Web_WWMUI.dmg",
@@ -16,22 +16,22 @@ cask "adobe-acrobat-pro" do
 
   auto_updates true
 
-  pkg "Acrobat DC/Acrobat DC Installer.pkg"
+  pkg "Acrobat/Acrobat DC Installer.pkg"
 
   uninstall pkgutil:   [
               "com.adobe.acrobat.DC.*",
-              "com.adobe.PDApp.AdobeApplicationManager.installer.pkg",
               "com.adobe.AcroServicesUpdater",
               "com.adobe.armdc.app.pkg",
+              "com.adobe.PDApp.AdobeApplicationManager.installer.pkg",
             ],
             launchctl: [
               "Adobe_Genuine_Software_Integrity_Service",
               "com.adobe.AAM.Startup-1.0",
               "com.adobe.AAM.Updater-1.0",
+              "com.adobe.agsservice",
               "com.adobe.ARMDC.Communicator",
               "com.adobe.ARMDC.SMJobBlessHelper",
               "com.adobe.ARMDCHelper.cc24aef4a1b90ed56a725c38014c95072f92651fb65e1bf9c8e43c37a23d420d",
-              "com.adobe.agsservice",
             ],
             delete:    "/Applications/Adobe Acrobat DC/"
 
