23e0c6793562f5d9692d5552b65ccf6d19531ec9
zsoltion <zsoltion@users.noreply.github.com>
2022-10-12 02:36:05 +0200

    Update eset-cyber-security-pro from 6.10.460.1 to 6.11.100.0 (#133290)
    * Update eset-cyber-security-pro from 6.10.460.1 to 6.11.100.0
    
    Update eset-cyber-security-pro from 6.10.460.1 to 6.11.100.0
    
    * Update eset-cyber-security-pro.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/eset-cyber-security-pro.rb
+++ Casks/eset-cyber-security-pro.rb
@@ -1,11 +1,16 @@
 cask "eset-cyber-security-pro" do
-  version "6.10.460.1"
+  version "6.11.100.0"
   sha256 :no_check
 
   url "https://download.eset.com/com/eset/apps/home/ess/mac/latest/eset_cybersecurity_pro_en.dmg"
-  appcast "https://www.eset.com/int/home/cyber-security-pro/download/?type=13554&tx_esetdownloads_ajax%5Bproduct%5D=83&tx_esetdownloads_ajax%5Bplugin_id%5D=34398"
   name "ESET Cyber Security Pro"
+  desc "Security including personal firewall and parental control"
   homepage "https://www.eset.com/"
+
+  livecheck do
+    url "https://www.eset.com/int/home/cyber-security-pro/download/?type=13554&tx_esetdownloads_ajax%5Bproduct%5D=83&tx_esetdownloads_ajax%5Bplugin_id%5D=34398"
+    regex(/eset[._-]cybersecurity[._-]pro[._-]en\.dmg","full[._-]version":"v?(\d+(?:\.\d+)+)/i)
+  end
 
   pkg "Resources/Installer.pkg"
 
@@ -14,23 +19,25 @@ cask "eset-cyber-security-pro" do
               sudo:       true,
             },
             pkgutil:   [
-              "com.eset.esetCyberSecurityPro.com.eset.esets_gui.pkg",
-              "com.eset.esetCyberSecurityPro.pkgid2.pkg",
-              "com.eset.esetCyberSecurityPro.ESETCyberSecurityPro.pkg",
-              "com.eset.esetCyberSecurityPro.com.eset.esets_pidmapper.pkg",
               "com.eset.esetCyberSecurityPro.com.eset.esets_daemon.pkg",
+              "com.eset.esetCyberSecurityPro.com.eset.esets_gui.pkg",
+              "com.eset.esetCyberSecurityPro.com.eset.esets_pidmapper.pkg",
+              "com.eset.esetCyberSecurityPro.ESETCyberSecurityPro.pkg",
               "com.eset.esetCyberSecurityPro.GUI_startup.pkg",
+              "com.eset.esetCyberSecurityPro.pkgid2.pkg",
             ],
             launchctl: [
-              "com.eset.esets_pidmapper",
               "com.eset.esets_daemon",
+              "com.eset.esets_pidmapper",
             ],
-            delete:    "/Applications/ESET Cyber Security Pro.app"
+            delete:    [
+              "/Applications/ESET Cyber Security Pro.app",
+              "/Library/Application Support/ESET",
+            ]
 
   zap trash: [
+    "~/.esets",
     "~/Library/Caches/com.eset.ecsp.*",
     "~/Library/Preferences/com.eset.ecsp.*.plist",
-    "/Library/Application Support/ESET",
-    "~/.esets",
   ]
 end
