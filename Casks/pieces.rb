ab8204db9ec2e8de2a5a3363e292db9a41befa5f
Connor O'Leary <connoroleary@Connors-MacBook-Pro.local>
2022-10-12 11:24:35 -0400

    adding fixes for audit check

--- Casks/pieces.rb
+++ Casks/pieces.rb
@@ -1,20 +1,17 @@
-class Pieces < Formula
-  desc "A command line application for Pieces"
-  homepage "https://code.pieces.app/"
-  url "https://storage.googleapis.com/app-releases-59612ba/pieces-cli/release/pieces-1.2.1.tar.gz"
+cask "pieces" do
+  version "1.2.1"
   sha256 "a8248ab589b38a322b83bb77c03e752e32f2e6b83b4a9030affd0f5825c6da27"
-  license ""
 
-  on_linux do
-    depends_on "xsel"
+  url "https://storage.googleapis.com/app-releases-59612ba/pieces-cli/release/pieces-#{version}.tar.gz", verified: "storage.googleapis.com/app-releases-59612ba/pieces-cli/"
+  name "pieces"
+  desc "A command line tool for Pieces.app"
+  homepage "https://code.pieces.app"
+
+  livecheck do
+    url "https://code.pieces.app/cli-version"
+    strategy :page_match
+    regex(/pieces-cli-(\d+(?:\.\d+)+)/)
   end
 
-  def install
-	if OS.linux?
-		bin.install "pieces-linux" => "pieces"
-	else
-		bin.install "pieces-mac" => "pieces"
-	end
-  end
+  binary "pieces"
 end
-
