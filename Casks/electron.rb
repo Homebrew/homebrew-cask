8060aafd3e6a0108d88505e44b511241c255ea55
Patrick Linnane <patrick@linnane.io>
2022-10-12 19:40:42 -0700

    electron 21.1.1
    Update electron from 21.1.0 to 21.1.1
    
    Closes #133413.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,9 +1,9 @@
 cask "electron" do
   arch arm: "arm64", intel: "x64"
 
-  version "21.1.0"
-  sha256 arm:   "d1d8ba60361809a4f2c9fedcb62d8c7873939e1251629d6dcf9e91660873a75c",
-         intel: "5d987264c0a985d45819034891d7d03bc27989b6f4f84fd86fe1f832055471ee"
+  version "21.1.1"
+  sha256 arm:   "353b54f370b5f59e222db43a8cca9359426aa60a6d7ddde3e794cf641d497721",
+         intel: "babc6c8a48c2248a16d99cae1819f0803085d44c3e7e0c9234bbce98b1bb8c29"
 
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-#{arch}.zip",
       verified: "github.com/electron/electron/"
