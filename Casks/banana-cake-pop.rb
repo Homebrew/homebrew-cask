673eaa9c29352ea56351dfb5b2e59a5dc6dd15f5
Patrick Linnane <patrick@linnane.io>
2022-10-12 19:16:34 -0700

    banana-cake-pop 2.0.1
    Update banana-cake-pop from 2.0.0 to 2.0.1
    
    Closes #133399.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/banana-cake-pop.rb
+++ Casks/banana-cake-pop.rb
@@ -1,9 +1,9 @@
 cask "banana-cake-pop" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.0.0"
-  sha256 arm:   "1e3fc1e516a16c8376d436f67cccb896ab6cfa84e7ce826c22b624bcf0cb501e",
-         intel: "1c387640066c22b11e2d1b7496f5ea15f486f475227a3dcab4217dddfe5fdc30"
+  version "2.0.1"
+  sha256 arm:   "b0406aac93f117f18dfd8dafd2de5461165250e875d7062e97e3b1e0e63aee1d",
+         intel: "e60b80506ec8e832d996a7407f4c5ec8c69264ac11446baac2fc6879ca953240"
 
   url "https://download.chillicream.com/bananacakepop/BananaCakePop-#{version}-mac-#{arch}.dmg"
   name "Banana Cake Pop"
