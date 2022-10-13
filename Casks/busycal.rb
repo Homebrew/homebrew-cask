3c4c47dc13c5dbf8e9b95bd759b81879dea868e4
Patrick Linnane <patrick@linnane.io>
2022-10-12 19:18:09 -0700

    busycal 2022.4.1,2022-10-12-23-19
    Update busycal from 2022.4.1,2022-10-10-15-02 to 2022.4.1,2022-10-12-23-19
    
    Closes #133401.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/busycal.rb
+++ Casks/busycal.rb
@@ -1,6 +1,6 @@
 cask "busycal" do
-  version "2022.4.1,2022-10-10-15-02"
-  sha256 "9ef66b9c116aba5b66db8f09eb9515b51fe20165be361deeb36c20de63695708"
+  version "2022.4.1,2022-10-12-23-19"
+  sha256 "1f4f99fa84750826990b9552f79998047b8681210fb0e0413bd5919e94123208"
 
   url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bcl-#{version.csv.first}-#{version.csv.second}.zip",
       verified: "7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/"
