f9c307461d1eb5c68bc1709dfa59034bfe20ebdf
Patrick Linnane <patrick@linnane.io>
2022-10-12 21:47:01 -0700

    Fix livecheck

--- Casks/ghidra.rb
+++ Casks/ghidra.rb
@@ -9,10 +9,20 @@ cask "ghidra" do
   homepage "https://www.ghidra-sre.org/"
 
   livecheck do
-    url "https://github.com/NationalSecurityAgency/ghidra/releases"
-    strategy :page_match do |page|
-      page.scan(/href=.*?ghidra[._-]v?(\d+(?:\.\d+)+)[._-]PUBLIC[._-](\d+)\.zip/i)
-          .map { |matches| "#{matches[0]},#{matches[1]}" }
+    url "https://github.com/NationalSecurityAgency/ghidra/releases/latest"
+    regex(/href=.*?ghidra[._-]v?(\d+(?:\.\d+)+)[._-]PUBLIC[._-](\d+)\.zip/i)
+    strategy :header_match do |headers, regex|
+      next if headers["location"].blank?
+
+      # Identify the latest tag from the response's `location` header
+      latest_tag = File.basename(headers["location"])
+      next if latest_tag.blank?
+
+      # Fetch the assets list HTML for the latest tag and match within it
+      assets_page = Homebrew::Livecheck::Strategy.page_content(
+        @url.sub(%r{/releases/?.+}, "/releases/expanded_assets/#{latest_tag}"),
+      )
+      assets_page[:content]&.scan(regex)&.map { |match| "#{match[0]},#{match[1]}" }
     end
   end
 
