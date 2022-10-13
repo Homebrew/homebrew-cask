b2cb73c4aa1a61711a8729e610b7dc34b3e3e9fe
Guillaume C <me@gcqd.fr>
2022-10-13 22:23:53 +0200

    Add Arc v0.71.0
    Creation of a cask for Arc app, a Chromium based web browser by The Browser Company.

---
+++ Casks/arc.rb
@@ -0,0 +1,36 @@
+cask "arc" do
+  version "0.71.0,35084"
+  sha256 "3c5c2b22b81cdc336e0aa3b76253cfd60eb51f675cc936b5923a3c680ab5d0c7"
+
+  url "https://releases.arc.net/release/Arc-#{version.csv.first}-#{version.csv.second}.zip"
+  name "Arc"
+  desc "Chromium based browser"
+  homepage "https://arc.net/"
+
+  livecheck do
+    url "https://releases.arc.net/updates.xml"
+    regex(%r{/Arc-(\d+(?:\.\d+)+)-(\d+).zip}i)
+    strategy :sparkle do |item, regex|
+      match = item.url.match(regex)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
+  end
+
+  depends_on macos: ">= :catalina"
+
+  app "Arc.app"
+
+  uninstall quit: "company.thebrowser.Browser"
+
+  zap trash: [
+    "~/Library/Caches/company.thebrowser.Browser",
+    "~/Library/Caches/CloudKit/company.thebrowser.Browser",
+    "~/Library/HTTPStorages/company.thebrowser.Browser",
+    "~/Library/HTTPStorages/company.thebrowser.Browser.binarycookies",
+    "~/Library/Preferences/company.thebrowser.Browser.plist",
+    "~/Library/Saved Application State/company.thebrowser.Browser.savedState",
+    "~/Library/WebKit/company.thebrowser.Browser",
+  ]
+end
