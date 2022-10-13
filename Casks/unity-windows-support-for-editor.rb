279039ff9d2a225c885b3fe91e4980334eb4261f
Patrick Linnane <patrick@linnane.io>
2022-10-12 18:10:55 -0700

    Update unity-windows-support-for-editor from 2022.1.19f1 to 2022.1.20f1

--- Casks/unity-windows-support-for-editor.rb
+++ Casks/unity-windows-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask "unity-windows-support-for-editor" do
-  version "2022.1.19f1,2fd7b40534d1"
-  sha256 "31075943afe369760e4d5b61eb2fe2af0c70ddb595b7f07644c3199c09dd8c65"
+  version "2022.1.20f1,01d83b40d570"
+  sha256 "e2fe53566b0cfafe2572b140c1bdad30fd77cb32e24235c37933c008d70091ec"
 
   url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-#{version.csv.first}.pkg",
       verified: "download.unity3d.com/download_unity/"
