244b9daada6fbc8f6be8b3992e719d7306409de9
Miccal Matthews <miccal.matthews@gmail.com>
2022-10-14 07:07:03 +0800

    Update and rename chipmunk-log.rb to chipmunk.rb

--- Casks/chipmunk.rb
+++ Casks/chipmunk.rb
@@ -1,4 +1,4 @@
-cask "chipmunk-log" do
+cask "chipmunk" do
   version "2.26.3"
   sha256 "d50da01c071efb0b8be3eae9c39da3405ad79cb4c740430767b70b650f7cfd88"
 
@@ -7,16 +7,9 @@ cask "chipmunk-log" do
   desc "Log analysis tool"
   homepage "https://github.com/esrlabs/chipmunk/"
 
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
-
   auto_updates true
 
   app "chipmunk.app"
 
-  zap trash: [
-    "~/.chipmunk",
-  ]
+  zap trash: "~/.chipmunk"
 end
