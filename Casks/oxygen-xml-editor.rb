3725e6b3acb14a1b76bfeaa1ab82c944c00edd8e
Joe Wicentowski <joewiz@gmail.com>
2022-10-13 10:03:26 -0400

    Update oxygen-xml-editor from 24.1 to 25.0

--- Casks/oxygen-xml-editor.rb
+++ Casks/oxygen-xml-editor.rb
@@ -1,7 +1,7 @@
 cask "oxygen-xml-editor" do
   arch arm: "-aarch64"
 
-  version "24.1,2022092207"
+  version "25.0,2022101006"
   sha256 :no_check
 
   url "https://www.oxygenxml.com/InstData/Editor/MacOSX/VM/oxygen-openjdk#{arch}.dmg"
