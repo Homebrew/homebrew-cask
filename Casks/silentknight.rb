54adb22654069f263fc8fb09c0e5bcb90455f5af
Luca Palli <luca@palli.ch>
2022-10-14 01:17:50 +0200

    Update silentknight from 1.21 to 2.0.0 (#133248)
    * Update silentknight from 1.21 to 2.0.0
    
    * Update silentknight.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/silentknight.rb
+++ Casks/silentknight.rb
@@ -1,22 +1,31 @@
 cask "silentknight" do
-  version "1.21,2022.06"
-  sha256 "c1cbb734f620e073f1c08c473edaa036c2b5ccdca02baa99ca117f86c10ad505"
+  if MacOS.version <= :mojave
+    version "1.21,2022.06"
+    sha256 "c1cbb734f620e073f1c08c473edaa036c2b5ccdca02baa99ca117f86c10ad505"
+
+    livecheck do
+      skip "Legacy version for Mojave and earlier"
+    end
+  else
+    version "2.00,2022.10"
+    sha256 "a5fe4f2c758487b6a9d61bab39001771fa74b40b66c01977249bc9b5a2961939"
+
+    livecheck do
+      url "https://raw.githubusercontent.com/hoakleyelc/updates/master/eclecticapps.plist"
+      strategy :page_match do |page|
+        match = page.match(%r{(\d+)/(\d+)/silentknight([^1]\d+)\.zip}i)
+        next if match.blank?
+
+        "#{match[3].split("", 2).join(".")},#{match[1]}.#{match[2]}"
+      end
+    end
+  end
 
   url "https://eclecticlightdotcom.files.wordpress.com/#{version.csv.second.major}/#{version.csv.second.minor}/silentknight#{version.csv.first.no_dots}.zip",
       verified: "eclecticlightdotcom.files.wordpress.com/"
   name "SilentKnight"
   desc "Automatically checks computer's security"
   homepage "https://eclecticlight.co/lockrattler-systhist/"
-
-  livecheck do
-    url "https://raw.githubusercontent.com/hoakleyelc/updates/master/eclecticapps.plist"
-    strategy :page_match do |page|
-      match = page.match(%r{/(\d+)/(\d+)/silentknight(\d+)\.zip}i)
-      next if match.blank?
-
-      "#{match[3].split("", 2).join(".")},#{match[1]}.#{match[2]}"
-    end
-  end
 
   depends_on macos: ">= :el_capitan"
 
