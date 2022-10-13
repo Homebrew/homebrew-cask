947944dcff5377b2e92d228ce02b5772cd75d82e
Patrick Linnane <patrick@linnane.io>
2022-10-12 19:41:19 -0700

    fly 7.8.3
    Update fly from 7.8.2 to 7.8.3
    
    Closes #133414.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/fly.rb
+++ Casks/fly.rb
@@ -1,6 +1,6 @@
 cask "fly" do
-  version "7.8.2"
-  sha256 "f724d5700ebf607ce9b5ed554b3e088ee717a81bb413649d15b2b316b415d77a"
+  version "7.8.3"
+  sha256 "fe8c2eb0a1b4d9df1701916270b870cf203f07512055274470594569f625cde0"
 
   url "https://github.com/concourse/concourse/releases/download/v#{version}/fly-#{version}-darwin-amd64.tgz"
   name "fly"
