3cba68cc0a8c3ecf1177853e4895f4c849bc649e
Patrick Linnane <patrick@linnane.io>
2022-10-13 21:30:34 -0700

    Update missive from 10.32.0 to 10.33.0

--- Casks/missive.rb
+++ Casks/missive.rb
@@ -1,8 +1,8 @@
 cask "missive" do
-  version "10.32.0"
-  sha256 "54565c07401551ee2aad38af83346d65c017e7687b66cd5eec044e87095b0108"
+  version "10.33.0"
+  sha256 "1d7250b2d65a459bab0941cf39fb9a6011cd0628103b14c0e24b383a790d5414"
 
-  url "https://d1weoh55n7oehu.cloudfront.net/#{version}/Missive-#{version}.dmg",
+  url "https://d1weoh55n7oehu.cloudfront.net/stable/Missive-#{version}.dmg",
       verified: "d1weoh55n7oehu.cloudfront.net/"
   name "Missive"
   desc "Team inbox and chat tool"
