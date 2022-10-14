c79093da05261536e08adddfe362648256918b31
Connor Gray <code@connorgray.com>
2022-10-14 13:30:19 -0400

    Re-add updated wolfram-engine
    This commit re-adds the wolfram-engine.rb package script, which
    was removed in #131414 due to being out of date.
    
    We (Wolfram Research) have updated our internal release process
    to ensure that the URL we provide for Homebrew does not fall out
    of date again.
    
    The new script additionally includes a livecheck based on a
    dedicated `latest.txt` file that we will update at the same time
    the WolframEngine .dmg for Homebrew is published.
    
    This fixes an issue the previous livecheck would encounter when
    a new Wolfram Engine was released to our authenticated accounts
    download page (the livecheck would show a new version was available),
    but the dedicated Homebrew path in the CDN had not yet been updated.
    
    It also includes a quality-of-life change addition of a `binary`
    stanza to create a symlink to the wolframscript executable provided
    by Wolfram Engine.

---
+++ Casks/wolfram-engine.rb
@@ -0,0 +1,30 @@
+cask "wolfram-engine" do
+  version "13.1.0.0"
+  sha256 "f7595e229b9c9883d7d1a5cf27237794567ad89b7b884afee2857e9853f40ce9"
+
+  url "https://files.wolframcdn.com/packages/Homebrew/#{version}/WolframEngine_#{version.major_minor_patch}_MAC.dmg",
+      verified: "files.wolframcdn.com/packages/"
+  name "Wolfram Engine"
+  desc "Evaluator for the Wolfram Language"
+  homepage "https://www.wolfram.com/engine/"
+
+  livecheck do
+    url "https://files.wolframcdn.com/packages/Homebrew/latest.txt"
+    strategy :page_match
+    regex(/^(\d+\.\d+\.\d+\.\d+)$/)
+  end
+
+  depends_on macos: ">= :mojave"
+
+  app "Wolfram Engine.app"
+  binary "#{appdir}/Wolfram Engine.app/Contents/Resources/Wolfram Player.app/Contents/MacOS/wolframscript"
+
+  zap trash: [
+    "~/Library/WolframEngine",
+    "~/Library/Caches/Wolfram",
+  ]
+
+  caveats do
+    free_license "https://www.wolfram.com/engine/free-license/"
+  end
+end
