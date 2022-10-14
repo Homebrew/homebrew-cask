d7a2af3d3fba1659af2e01d84ee722a88f992a12
Lorelei Aurora <iam@l5i.me>
2022-10-14 14:50:15 +0100

    Add Topaz Video Enhance AI.app v2.6.4

---
+++ Casks/topaz-video-enhance-ai.rb
@@ -0,0 +1,11 @@
+cask "topaz-video-enhance-ai" do
+  version "2.6.4"
+  sha256 :no_check
+
+  url "https://topazlabs.com/d/veai/latest/mac/full"
+  name "topaz-video-enhance-ai"
+  desc "World-class video upscaler and quality enhancer on your desktop, powered by AI"
+  homepage "https://www.topazlabs.com/video-enhance-ai"
+
+  app "Topaz Video Enhance AI.app"
+end
