29a98a02d3bb4da23b4f2f126fb094dd8875851f
Connor O'Leary <connoroleary@Connors-MacBook-Pro.local>
2022-10-14 10:29:04 -0400

    changing path of cache file

--- Casks/pieces.rb
+++ Casks/pieces.rb
@@ -1,6 +1,6 @@
 cask "pieces" do
-  version "1.2.3"
-  sha256 "6c8c484cd796338d3fec91b247e75bcd26c1bef284c74950157bdb5d2ab5702e"
+  version "1.2.4"
+  sha256 "8c3f210254221b5dcef1a0f0798e0a1f3883758089b1017d4495e7181ef20781"
 
   url "https://storage.googleapis.com/app-releases-59612ba/pieces-cli/release/pieces-mac-#{version}.tar.gz", verified: "storage.googleapis.com/app-releases-59612ba/pieces-cli/"
   name "pieces"
@@ -14,4 +14,6 @@ cask "pieces" do
   end
 
   binary "pieces"
+
+  zap trash: "~/Library/.piecescli-cache"
 end
