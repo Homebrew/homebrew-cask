4ee875314dc40c3c96e0b0964b24552d8b5b86cc
Miccal Matthews <miccal.matthews@gmail.com>
2022-10-14 23:39:10 +0800

    Update topaz-video-enhance-ai.rb

--- Casks/topaz-video-enhance-ai.rb
+++ Casks/topaz-video-enhance-ai.rb
@@ -1,11 +1,16 @@
 cask "topaz-video-enhance-ai" do
   version "2.6.4"
-  sha256 :no_check
+  sha256 "e22c4c4abee9917d5b0daa283e91b7d2edfbaefb1e00f13d2904c6911d5d319c"
 
-  url "https://topazlabs.com/d/veai/latest/mac/full"
-  name "topaz-video-enhance-ai"
-  desc "World-class video upscaler and quality enhancer on your desktop, powered by AI"
+  url "https://downloads.topazlabs.com/deploy/TopazVideoEnhanceAI/#{version}/TopazVideoEnhanceAI-#{version}-univ.dmg"
+  name "Topaz Video Enhance AI"
+  desc "Video upscaler and quality enhancer"
   homepage "https://www.topazlabs.com/video-enhance-ai"
+
+  livecheck do
+    url "https://topazlabs.com/d/veai/latest/mac/full"
+    strategy :header_match
+  end
 
   app "Topaz Video Enhance AI.app"
 end
