82d55b5aa0394484ed031b576667164b56a8ea78
Patrick Linnane <patrick@linnane.io>
2022-10-13 21:50:02 -0700

    Update postgres-unofficial from 2.5.10 to 2.5.11

--- Casks/postgres-unofficial.rb
+++ Casks/postgres-unofficial.rb
@@ -1,16 +1,29 @@
 cask "postgres-unofficial" do
-  version "2.5.10"
-  sha256 "fd25322ddffc1ab45a2f4b5e57c306e129bfc5e7c3a00963dfcaf1d8a3731eea"
+  version "2.5.11,10-11-12-13-14-15"
+  sha256 "20020d167cfefaeead3194f9fb0b31f5475e52bd8a7228ea8b19d74d362fc8a6"
 
-  url "https://github.com/PostgresApp/PostgresApp/releases/download/v#{version}/Postgres-#{version}-10-11-12-13-14.dmg",
+  url "https://github.com/PostgresApp/PostgresApp/releases/download/v#{version.csv.first}/Postgres-#{version.csv.first}-#{version.csv.second}.dmg",
       verified: "github.com/PostgresApp/PostgresApp/"
   name "Postgres"
   desc "App wrapper for Postgres"
   homepage "https://postgresapp.com/"
 
   livecheck do
-    url :url
-    strategy :github_latest
+    url "https://github.com/PostgresApp/PostgresApp/releases/latest"
+    regex(/href=.*?Postgres[._-]v?(\d+(?:\.\d+)+)[._-](\d+(?:-\d+)+)\.dmg/i)
+    strategy :header_match do |headers, regex|
+      next if headers["location"].blank?
+
+      # Identify the latest tag from the response's `location` header
+      latest_tag = File.basename(headers["location"])
+      next if latest_tag.blank?
+
+      # Fetch the assets list HTML for the latest tag and match within it
+      assets_page = Homebrew::Livecheck::Strategy.page_content(
+        @url.sub(%r{/releases/?.+}, "/releases/expanded_assets/#{latest_tag}"),
+      )
+      assets_page[:content]&.scan(regex)&.map { |match| "#{match[0]},#{match[1]}" }
+    end
   end
 
   auto_updates true
