a6d7f88a8bdbd34c36e210ddd65ea536b15ca44c
Patrick Linnane <patrick@linnane.io>
2022-10-14 10:29:55 -0700

    Update decrediton from 1.7.3 to 1.7.5

--- Casks/decrediton.rb
+++ Casks/decrediton.rb
@@ -1,9 +1,9 @@
 cask "decrediton" do
   arch arm: "arm64", intel: "amd64"
 
-  version "1.7.3"
-  sha256 arm:   "b29286210fe17fe159b5ccb24f0be572da9235acc6b9093356091d52a218848d",
-         intel: "106a0d417163c9c247b5a658e7dda691290af8b9651dcecb99f83cfcda5683a8"
+  version "1.7.5"
+  sha256 arm:   "bb81ce2fcb2d87a152417d723092cd2b7f5401c236d21000da0afd36a34f9288",
+         intel: "aff85924f3c57d18983c9d00d6a3c9f65fb2b84da8ace162923dba6c6d3c348f"
 
   url "https://github.com/decred/decred-binaries/releases/download/v#{version}/decrediton-#{arch}-v#{version}.dmg"
   name "Decrediton"
