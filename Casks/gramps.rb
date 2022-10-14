a50c2c1b0a295c42018777befbfdaf80b8bbe9ee
Patrick Linnane <patrick@linnane.io>
2022-10-14 08:12:52 -0700

    gramps: Fix livecheck (#133518)
    Fix livecheck

--- Casks/gramps.rb
+++ Casks/gramps.rb
@@ -10,11 +10,19 @@ cask "gramps" do
 
   livecheck do
     url "https://github.com/gramps-project/gramps/releases/latest"
-    strategy :page_match do |page|
-      match = page.match(%r{href=.*?/Gramps[._-]Intel[._-]v?(\d+(?:.\d+)+)[._-](\d+)\.dmg}i)
-      next if match.blank?
+    regex(/Gramps[._-]Intel[._-]v?(\d+(?:.\d+)+)[._-](\d+)\.dmg/i)
+    strategy :header_match do |headers, regex|
+      next if headers["location"].blank?
 
-      "#{match[1]},#{match[2]}"
+      # Identify the latest tag from the response's `location` header
+      latest_tag = File.basename(headers["location"])
+      next if latest_tag.blank?
+
+      # Fetch the assets list HTML for the latest tag and match within it
+      assets_page = Homebrew::Livecheck::Strategy.page_content(
+        @url.sub(%r{/releases/?.+}, "/releases/expanded_assets/#{latest_tag}"),
+      )
+      assets_page[:content]&.scan(regex)&.map { |match| "#{match[0]},#{match[1]}" }
     end
   end
 
