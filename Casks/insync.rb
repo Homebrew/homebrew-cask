b7c587ec19403858de5f9870af7ceadab3be8879
Patrick Linnane <patrick@linnane.io>
2022-10-14 10:34:15 -0700

    Update insync from 3.7.12.50395 to 3.7.14.50440

--- Casks/insync.rb
+++ Casks/insync.rb
@@ -1,6 +1,6 @@
 cask "insync" do
-  version "3.7.12.50395"
-  sha256 "fcb88ba20c5c4ed3b100ee74ba1547dca25d61d6a42c611495ab7a0207b64c59"
+  version "3.7.14.50440"
+  sha256 "8d216c413ca2e5ff4ad90b4876bbeccc494e2035c7e035f7ccfa23eef7908091"
 
   url "http://cdn.insynchq.com/builds/mac/Insync-#{version}.dmg"
   name "Insync"
