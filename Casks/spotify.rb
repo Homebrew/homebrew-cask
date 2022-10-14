4b6243dcf72d048dce3cb473be35a4c72f4c683d
Branch Vincent <branchevincent@gmail.com>
2022-10-14 12:41:05 -0400

    Update spotify from 1.1.95.893 to 1.1.96.785 (#133536)
    * Update spotify from 1.1.95.893 to 1.1.96.785
    
    * Update spotify.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/spotify.rb
+++ Casks/spotify.rb
@@ -1,7 +1,7 @@
 cask "spotify" do
   arch arm: "ARM64"
 
-  version "1.1.95.893,6cf4d40c,39"
+  version "1.1.96.785,464c973a,1288"
   sha256 :no_check
 
   url "https://download.scdn.co/Spotify#{arch}.dmg",
