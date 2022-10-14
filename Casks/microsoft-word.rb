06950e6d7e067b84f1c6d83ee35ae842ebc05201
Patrick Linnane <patrick@linnane.io>
2022-10-14 10:54:29 -0700

    Update microsoft-word from 16.66.22100900 to 16.66.22101101

--- Casks/microsoft-word.rb
+++ Casks/microsoft-word.rb
@@ -12,8 +12,8 @@ cask "microsoft-word" do
     version "16.54.21101001"
     sha256 "7f3ed397b517aac3637d8b8f8b4233f9e7132941f0657eaca8ec423ac068616e"
   else
-    version "16.66.22100900"
-    sha256 "e7d4cb1780932c9cb1b4dbd63dc8e3d397e1c28db28f3d5cf1e8cca76d6e35e2"
+    version "16.66.22101101"
+    sha256 "5a6a75d9a5b46cceeff5a1b7925c0eab6e4976cba529149b7b291a0355e7a7c9"
   end
 
   url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Word_#{version}_Installer.pkg",
