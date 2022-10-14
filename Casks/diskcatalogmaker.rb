14a088dca44e3028d34a47a017eb3bbb4c8fdbc5
Patrick Linnane <patrick@linnane.io>
2022-10-14 10:31:44 -0700

    diskcatalogmaker 8.5.3
    Update diskcatalogmaker from 8.5.2 to 8.5.3
    
    Closes #133548.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/diskcatalogmaker.rb
+++ Casks/diskcatalogmaker.rb
@@ -1,6 +1,6 @@
 cask "diskcatalogmaker" do
-  version "8.5.2"
-  sha256 "dbb04d58e3e1d4854086396aa50d40ce994c1a25917d51f3eca2e044a7de2b8d"
+  version "8.5.3"
+  sha256 "3458b467429fe72db3990ea8e6fadc709e965b50a6e8e5c1b412560b48e1c780"
 
   url "https://download.diskcatalogmaker.com/download/zip/DiskCatalogMaker#{version.no_dots}.zip"
   name "DiskCatalogMaker"
