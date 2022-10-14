671c6b6df22d930c81f3fbd979729eebd50a9bf1
Patrick Linnane <patrick@linnane.io>
2022-10-14 10:45:27 -0700

    microsoft-powerpoint: rebuild
    Update microsoft-powerpoint from 16.66.22100900 to 16.66.22101101
    
    Closes #133558.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/microsoft-powerpoint.rb
+++ Casks/microsoft-powerpoint.rb
@@ -12,8 +12,8 @@ cask "microsoft-powerpoint" do
     version "16.54.21101001"
     sha256 "75a57c82b46d0e2558c454f19610576b7a48baf1ccc5cd1fa61b69cca5bf0bd1"
   else
-    version "16.66.22100900"
-    sha256 "5a7cc747c573cd83223d719aff22e4594e6bc60dffcbd53a1ce159e8eeeb324b"
+    version "16.66.22101101"
+    sha256 "bea8c4790445f726debd0f64d24fbdac59e3a9b51e95c092fb31da3913164540"
   end
 
   url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_PowerPoint_#{version}_Installer.pkg",
