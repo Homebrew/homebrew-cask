7194c48734e8d48da2e740694f74584fbbcd4ee8
NetSarangComputer <bigseungho@netsarang.com>
2022-10-14 18:26:42 +0900

    Update PortX version 2.1.4 to 2.1.5

--- Casks/portx.rb
+++ Casks/portx.rb
@@ -1,9 +1,9 @@
 cask "portx" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.1.4,10.31"
-  sha256 arm:   "CF5245E85E85803478827D8951BD75C48FC425BFBB3BA5A69C95EF96A5520E3F",
-         intel: "51699A4A9BAF10ED04683A348F6AB5C8527F3B02569CE5CE1A65EEAAB3718BBB"
+  version "2.1.5,10.33"
+  sha256 arm:   "B91A60A6498378CB2E8A33CE26ED5729DEAF7B681256A195FC559891FFCA4BA4",
+         intel: "1402FC53B25423DE5946C502AD1A945C1256EA487DADCBA0C1BF45E17D958CA2"
 
   url "https://cdn.netsarang.net/0ac7ea20/PortX-#{version.csv.first}-#{arch}.dmg",
       verified: "cdn.netsarang.net/"
