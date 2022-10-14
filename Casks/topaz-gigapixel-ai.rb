53549bcadd0e9ce401aaef4d50b97d69ac2412e9
Lorelei Aurora <iam@l5i.me>
2022-10-14 13:54:56 +0100

    Add Topaz Gigapixel AI.app v6.2.0

---
+++ Casks/topaz-gigapixel-ai.rb
@@ -0,0 +1,11 @@
+cask "topaz-gigapixel-ai" do
+  version "6.2.0"
+  sha256 :no_check
+
+  url "https://downloads.topazlabs.com/deploy/TopazGigapixelAI/latest/TopazGigapixelAI-Full-Installer.dmg"
+  name "topaz-gigapixel-ai"
+  desc "Exceptional AI image upscaler delivering enhanced detail and resolution by 600%"
+  homepage "https://www.topazlabs.com/gigapixel-ai"
+
+  app "Topaz Gigapixel AI.app"
+end
