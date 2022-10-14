297031a2e1e0a380d6d35bd4ddd438030f614369
Miccal Matthews <miccal.matthews@gmail.com>
2022-10-15 00:14:06 +0800

    Update topaz-gigapixel-ai.rb

--- Casks/topaz-gigapixel-ai.rb
+++ Casks/topaz-gigapixel-ai.rb
@@ -3,8 +3,8 @@ cask "topaz-gigapixel-ai" do
   sha256 :no_check
 
   url "https://downloads.topazlabs.com/deploy/TopazGigapixelAI/latest/TopazGigapixelAI-Full-Installer.dmg"
-  name "topaz-gigapixel-ai"
-  desc "Exceptional AI image upscaler delivering enhanced detail and resolution by 600%"
+  name "Topaz Gigapixel AI"
+  desc "AI image upscaler"
   homepage "https://www.topazlabs.com/gigapixel-ai"
 
   app "Topaz Gigapixel AI.app"
