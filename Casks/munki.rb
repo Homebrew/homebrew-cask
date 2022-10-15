1fb1261f4c4220f9928949eae0a32203382f9488
Patrick Linnane <patrick@linnane.io>
2022-10-14 20:40:49 -0700

    Fix livecheck

--- Casks/munki.rb
+++ Casks/munki.rb
@@ -9,9 +9,21 @@ cask "munki" do
   homepage "https://www.munki.org/munki/"
 
   livecheck do
-    url :url
-    strategy :github_latest
+    url "https://github.com/munki/munki/releases/latest"
     regex(%r{href=.*?/munkitools[._-]v?(\d+(?:\.\d+)+)\.pkg}i)
+    strategy :header_match do |headers, regex|
+      next if headers["location"].blank?
+
+      # Identify the latest tag from the response's `location` header
+      latest_tag = File.basename(headers["location"])
+      next if latest_tag.blank?
+
+      # Fetch the assets list HTML for the latest tag and match within it
+      assets_page = Homebrew::Livecheck::Strategy.page_content(
+        @url.sub(%r{/releases/?.+}, "/releases/expanded_assets/#{latest_tag}"),
+      )
+      assets_page[:content]&.scan(regex)&.map { |match| match[0] }
+    end
   end
 
   pkg "munkitools-#{version}.pkg"
