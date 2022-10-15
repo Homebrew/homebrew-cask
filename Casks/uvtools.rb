36e0c457e4c5dc5f192a202a1f9339766e31f75c
Tiago Conceição <Tiago_caza@hotmail.com>
2022-10-15 18:10:28 +0100

    Fix whitespaces

--- Casks/uvtools.rb
+++ Casks/uvtools.rb
@@ -1,6 +1,6 @@
 cask "uvtools" do
   arch arm: "arm64", intel: "x64"
-  
+
   version "3.7.0"
   sha256 arm:   "4daa81a5680ed115da825486f730daa10b4a2724d68801c33b01a0e3877c654f",
          intel: "a99c35b16c20413e5eac1aa90da97d0d2d11698e429ee0d1ac5eaaf35d4ace42"
