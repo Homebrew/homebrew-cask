7fe2f48b3988c94d64932ea0743e532f666c8090
singularitti <singularitti@outlook.com>
2022-10-15 12:01:31 -0400

    Update koodo-reader from 1.4.8 to 1.4.9

--- Casks/koodo-reader.rb
+++ Casks/koodo-reader.rb
@@ -1,9 +1,9 @@
 cask "koodo-reader" do
   arch arm: "-arm64"
 
-  version "1.4.8"
-  sha256 arm:   "88d9267e22850ff64d4de828c3ab9e60c31c4cf96efd27c0b48861567c99816a",
-         intel: "619d7a1c1aafce4a674e785245cb4540cea1d3f65f39d9c56d6aa9a77b2d6044"
+  version "1.4.9"
+  sha256 arm:   "2051f9cee01fa80e031670ba3cfa0fe740ba5dd6a514dada6ad74b8888734b21",
+         intel: "432f35c97d28af2deca2ac9ef819b426bee49a26e48c4db0c3efb1fff459960b"
 
   url "https://github.com/troyeguo/koodo-reader/releases/download/v#{version}/Koodo-Reader-#{version}#{arch}.dmg",
       verified: "github.com/troyeguo/koodo-reader/"
