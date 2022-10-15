4a1d1358c4fded40e8dab503aa98441df8ebf709
Patrick Linnane <patrick@linnane.io>
2022-10-14 20:43:26 -0700

    Fix livecheck

--- Casks/nextcloud.rb
+++ Casks/nextcloud.rb
@@ -5,6 +5,10 @@ cask "nextcloud" do
 
     url "https://github.com/nextcloud/desktop/releases/download/v#{version.major_minor_patch}/Nextcloud-#{version}.pkg",
         verified: "github.com/nextcloud/desktop/"
+
+    livecheck do
+      skip "Legacy version for El Capitan and earlier"
+    end
   else
     version "3.6.0"
     sha256 "bf4d6da4df3453658bd4b14bfcda450109a8ac85005f23785af8ccac54a5f3d3"
@@ -18,9 +22,21 @@ cask "nextcloud" do
   homepage "https://nextcloud.com/"
 
   livecheck do
-    url "https://github.com/nextcloud/desktop/releases/"
-    strategy :page_match
+    url "https://github.com/nextcloud/desktop/releases/latest"
     regex(/Nextcloud[._-]v?(\d+(?:\.\d+)+)\.pkg/i)
+    strategy :header_match do |headers, regex|
+      next if headers["location"].blank?
+
+      # Identify the latest tag from the response's `location` header
+      latest_tag = File.basename(headers["location"])
+      next if latest_tag.blank?
+
+      # Fetch the assets list HTML for the latest tag and match within it
+      assets_page = Homebrew::Livecheck::Strategy.page_content(
+        @url.sub(%r{/releases/?.+}, "/releases/expanded_assets/#{latest_tag}"),
+      )
+      assets_page[:content]&.scan(regex)&.map { |match| match[0] }
+    end
   end
 
   auto_updates true
