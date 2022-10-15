f5cae1fea830e3132f01a5c728ee0995780f7af4
zsoltion-cc <55063936+zsoltion-cc@users.noreply.github.com>
2022-10-16 01:36:16 +0200

    Update apifox from 2.1.41 to 2.2.0

--- Casks/apifox.rb
+++ Casks/apifox.rb
@@ -1,9 +1,9 @@
 cask "apifox" do
   arch arm: "-macOS-arm64"
 
-  version "2.1.41"
-  sha256 arm:   "986c7f5ff354cc2ea2af0e46d96437775beb611657e5bc3b724f3237afdf176d",
-         intel: "dfccf65b168dede9c5cac3436a8bf6f99e653dba0297c0b80314021aef37faf7"
+  version "2.2.0"
+  sha256 arm:   "9031cbbf44087e8c2ad859fa765139592da48fd2cd04b89036c255d6facf0a35",
+         intel: "4501af5c47e7a3788fbaaf6fed2b9e6ce2e9cf1b4b3b1a5a560159177ddd2fcd"
 
   url "https://cdn.apifox.cn/download/#{version}/Apifox#{arch}-#{version}.dmg"
   name "Apifox"
