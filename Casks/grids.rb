51c1b9c2f611e24fb0e6de0a64073b9aa5e98440
Patrick Linnane <patrick@linnane.io>
2022-10-16 09:16:48 -0700

    grids 8.2.1
    Update grids from 8.2 to 8.2.1
    
    Closes #133622.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/grids.rb
+++ Casks/grids.rb
@@ -1,6 +1,6 @@
 cask "grids" do
-  version "8.2"
-  sha256 "6ad716d29187ebc9a1623978893bc3bf819c8bb4ede53dac4131eecb17c7b441"
+  version "8.2.1"
+  sha256 "4edadb3ed56e7c6cdd45db815cb96227895a5059d9059e0954b1d5f52de45676"
 
   url "https://gridsapp.net/bin/Grids_#{version}.zip"
   name "Grids"
