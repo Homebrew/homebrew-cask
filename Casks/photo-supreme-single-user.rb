d59b5af3af8743371d3c46887c72bc1605020477
Patrick Linnane <patrick@linnane.io>
2022-10-17 08:53:31 -0700

    photo-supreme-single-user 7.4.1.4579
    Update photo-supreme-single-user from 7.4.1.4572 to 7.4.1.4579
    
    Closes #133682.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/photo-supreme-single-user.rb
+++ Casks/photo-supreme-single-user.rb
@@ -1,5 +1,5 @@
 cask "photo-supreme-single-user" do
-  version "7.4.1.4572"
+  version "7.4.1.4579"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://trial.idimager.com/PhotoSupreme_V#{version.major}.pkg"
