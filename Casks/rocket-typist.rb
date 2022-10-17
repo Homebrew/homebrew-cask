db6c6bd7d4311eba693f3c2ef7137114bd5023e4
Patrick Linnane <patrick@linnane.io>
2022-10-17 08:56:11 -0700

    rocket-typist 2.4,162b
    * Update rocket-typist from 2.3 to 2.4
    * Add desc
    
    Closes #133686.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/rocket-typist.rb
+++ Casks/rocket-typist.rb
@@ -1,9 +1,10 @@
 cask "rocket-typist" do
-  version "2.3,150b"
+  version "2.4,162b"
   sha256 :no_check
 
   url "https://witt-software.com/downloads/rockettypist/Rocket%20Typist.dmg"
   name "Rocket Typist"
+  desc "Text expander for common phrases"
   homepage "https://witt-software.com/rockettypist/"
 
   livecheck do
