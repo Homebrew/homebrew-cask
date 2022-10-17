dda53b8bf10f3620f9138949322296491b5431f7
Wen Zhou <wenzhou@redhat.com>
2022-10-17 14:56:20 +0200

    Update: temurin latest from 19+36 to 19.0.1+10

--- Casks/temurin.rb
+++ Casks/temurin.rb
@@ -1,9 +1,9 @@
 cask "temurin" do
   arch arm: "aarch64", intel: "x64"
 
-  version "19,36"
-  sha256 arm:   "7b087c71b8f8a288c51e291a45c888b606bc3be1eb146079348e28bdb4d0df0a",
-         intel: "6e43fb8c543ffa88f70f8a35fa1f1278697e13612d9577f92f6816d3fc82a400"
+  version "19.0.1,10"
+  sha256 arm:   "8a930fc3cf27088af88973389bad5968026b1da48e08675fd62f8ec555047c4f",
+         intel: "c1fecbbaaf09712bc6c9b2b03552808f57d18a9170bedf40dc78aff8df2a193c"
 
   url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.csv.first}%2B#{version.csv.second}/OpenJDK#{version.major}U-jdk_#{arch}_mac_hotspot_#{version.csv.first}_#{version.csv.second.major}.pkg",
       verified: "github.com/adoptium/"
