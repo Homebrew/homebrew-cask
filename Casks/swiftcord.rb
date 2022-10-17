1efa23cd9f0832f59c577312ea3ae5d15253f536
cryptoAlgorithm <cryptoalgro@gmail.com>
2022-10-17 21:36:15 +0800

    patch: remove unsuitable caveats stanza

--- Casks/swiftcord.rb
+++ Casks/swiftcord.rb
@@ -22,8 +22,4 @@ cask "swiftcord" do
     "~/Library/Containers/io.cryptoalgo.swiftcord",
     "~/Library/Saved Application State/io.cryptoalgo.swiftcord.savedState",
   ]
-
-  caveats "If you're updating from an older version of Swiftcord, you might get stuck on the loading screen " \
-          "due to a bug in the new account storage system. Clicking the logout button that appears after ~30s " \
-          "is a workaround which will allow you to log in again. You'll only have to do this once."
 end
