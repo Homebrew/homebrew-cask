1504d836305e45b124b378ad3bccbe7c0b26bff2
Patrick Linnane <patrick@linnane.io>
2022-10-16 22:16:32 -0700

    Update prowlarr from 0.4.6.1969 to 0.4.7.2016

--- Casks/prowlarr.rb
+++ Casks/prowlarr.rb
@@ -1,9 +1,9 @@
 cask "prowlarr" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.4.6.1969"
-  sha256 arm:   "2182d54d93cb444d99d34a621d4218b226fe7c7356f907a665bb60fd0b5e6395",
-         intel: "51309fac45c8862775eeb634efbed32d5fdd3223dcdc6436b7d08d36a3f1d6bb"
+  version "0.4.7.2016"
+  sha256 arm:   "9ed05eef26d93a8f35f0744604e3772d65c851ac818121ff403212fef1e0cba4",
+         intel: "3eaac28b12e82dd2f8715933981e95a190b2de9d558962d022f329d41f0069a2"
 
   url "https://github.com/Prowlarr/Prowlarr/releases/download/v#{version}/Prowlarr.develop.#{version}.osx-app-core-#{arch}.zip",
       verified: "github.com/Prowlarr/Prowlarr/"
