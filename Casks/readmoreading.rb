3e1877e3aac9a40fd9dc11f1fc9f5bd9b1edccb2
Miccal Matthews <miccal.matthews@gmail.com>
2022-10-17 09:39:02 +0800

    Update and rename readmo.rb to readmoreading.rb

--- Casks/readmoreading.rb
+++ Casks/readmoreading.rb
@@ -1,10 +1,10 @@
-cask "readmo" do
+cask "readmoreading" do
   version "0.11.8"
   sha256 "997da8763676b40846876679b1d9541df93ee84e91ecfd5c024659ff8608e2b2"
 
   url "https://cdn.readmoo.com/download/apps/desktop/osx64/el/Readmoo_Desktop-#{version}.dmg"
-  name "readmo"
-  desc "Readmoo讀墨電子書 ─ 台灣最大繁體中文EPUB電子書服務"
+  name "Readmo Reading"
+  desc "Traditional Chinese eBook service"
   homepage "https://readmoo.com/"
 
   livecheck do
