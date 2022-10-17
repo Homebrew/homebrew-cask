c9b7d24406d52246596ed55a86d04ef7e96ba9a0
ldbchan <ldb.chan@gmail.com>
2022-10-17 23:16:12 +0800

    fix: `messenger` download url and live check

--- Casks/messenger.rb
+++ Casks/messenger.rb
@@ -1,18 +1,15 @@
 cask "messenger" do
-  version "155.22.218,380205507"
-  sha256 "7315136efa45f2733eae29190608aef4513f0e304d0137efc68e846e80303a21"
+  version "164.0.0.8.109,406707321"
+  sha256 :no_check
 
-  url "https://www.facebook.com/messenger/desktop/update/#{version.csv.second}.zip",
-      verified: "facebook.com/messenger/desktop/"
+  url "https://www.messenger.com/messenger/desktop/downloadV2/?platform=mac"
   name "Facebook Messenger"
   desc "Native desktop app for Messenger (formerly Facebook Messenger)"
   homepage "https://www.messenger.com/desktop"
 
   livecheck do
-    url "https://www.facebook.com/messenger/desktop/update/latest-mac.yml"
-    strategy :electron_builder do |yml|
-      "#{yml["version"]},#{yml["path"][%r{/(\d+)\.zip}i, 1]}"
-    end
+    url "https://www.facebook.com/messenger/desktop/zeratul/update.xml?target=zeratul&platform=mac"
+    strategy :sparkle
   end
 
   auto_updates true
