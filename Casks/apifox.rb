d8479e7ef8890421765764235b4e710f7b19232c
Patrick Linnane <patrick@linnane.io>
2022-10-17 08:11:28 -0700

    Update apifox from 2.2.0 to 2.2.1

--- Casks/apifox.rb
+++ Casks/apifox.rb
@@ -1,9 +1,9 @@
 cask "apifox" do
   arch arm: "-macOS-arm64"
 
-  version "2.2.0"
-  sha256 arm:   "9031cbbf44087e8c2ad859fa765139592da48fd2cd04b89036c255d6facf0a35",
-         intel: "4501af5c47e7a3788fbaaf6fed2b9e6ce2e9cf1b4b3b1a5a560159177ddd2fcd"
+  version "2.2.1"
+  sha256 arm:   "dd107dbaea97506c2b96169c80ebeaa0d44016ca1909c33f02f4dc34311a0f45",
+         intel: "4db4f8f57f0c81e62a00b7a9566461ca146c2dd183fbe27f84cf4fb6c677ad82"
 
   url "https://cdn.apifox.cn/download/#{version}/Apifox#{arch}-#{version}.dmg"
   name "Apifox"
