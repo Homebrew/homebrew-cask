a7a5ef3a13f59b46047aed134643967176e3fdb7
EricFromCanada <enk3@outlook.com>
2022-10-18 17:22:23 -0400

    Update endless-sky from 0.9.16 to 0.9.16.1

--- Casks/endless-sky.rb
+++ Casks/endless-sky.rb
@@ -1,6 +1,6 @@
 cask "endless-sky" do
-  version "0.9.16"
-  sha256 "18f280dde67ed0776d8753bbdc8575a923e9dca3434c51fd79f93d3f2f347b4e"
+  version "0.9.16.1"
+  sha256 "0d0aa0f6c856263b5113eed33d532dc147631b8bb6e30f4dc6fdda2a0af297f6"
 
   url "https://github.com/endless-sky/endless-sky/releases/download/v#{version}/endless-sky-macos-v#{version}.dmg",
       verified: "github.com/endless-sky/endless-sky/"
