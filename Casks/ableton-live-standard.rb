fdc65fee1c9d290b98edb111bac956791fae78e4
Patrick Linnane <patrick@linnane.io>
2022-10-18 08:41:28 -0700

    Update ableton-live-standard from 11.2 to 11.2.5

--- Casks/ableton-live-standard.rb
+++ Casks/ableton-live-standard.rb
@@ -1,11 +1,11 @@
 cask "ableton-live-standard" do
   arch = "intel"
-  version "11.2"
+  version "11.2.5"
   sha256 "51bffe5e3ab58c943cabbd605964e74c26535e9b993a3cb76b7a7d230150ec75"
 
   on_mojave :or_newer do
     arch = "universal"
-    sha256 "71d98a6532ba838ed26e9a5bb2c067c1915dc8846060fac69e9fe9caf97d4bd0"
+    sha256 "55ad16da96f7487940ffea113575b426cea8217012395fd336a4b9ae4b8fb0b2"
   end
 
   url "https://cdn-downloads.ableton.com/channels/#{version}/ableton_live_standard_#{version}_#{arch}.dmg"
