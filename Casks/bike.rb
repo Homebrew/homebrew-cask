d7a9187eda44f362d64060f163e789b96f29ca28
Patrick Linnane <patrick@linnane.io>
2022-10-17 19:57:41 -0700

    bike 1.5,86
    Update bike from 1.4.2 to 1.5
    
    Closes #133709.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/bike.rb
+++ Casks/bike.rb
@@ -1,6 +1,6 @@
 cask "bike" do
-  version "1.4.2,85"
-  sha256 "13c5439c652abb9b59bcee19fe2247c85767a6193ae00cb44d694c4a8f8b0e94"
+  version "1.5,86"
+  sha256 "2b5f9ea56d06eae36aa614ed40fd2e0c1d1eac3e28c86844110391d438e002e3"
 
   url "https://www.hogbaysoftware.com/bike/releases/#{version.csv.second}.dmg"
   name "Bike"
