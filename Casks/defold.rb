c073d4f0cf3e082c1d76767a90d507497aac93d7
Patrick Linnane <patrick@linnane.io>
2022-10-18 09:12:18 -0700

    defold 1.3.7
    Update defold from 1.3.6 to 1.3.7
    
    Closes #133753.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/defold.rb
+++ Casks/defold.rb
@@ -1,6 +1,6 @@
 cask "defold" do
-  version "1.3.6"
-  sha256 "732882a2b8e32da97288f15591d1e45b36b0f45d843a5899746ad3ae1bf71185"
+  version "1.3.7"
+  sha256 "0d0983f4bc847ebfa7d914da979d9f974f062da5f627f716ce886af20d062c60"
 
   url "https://github.com/defold/defold/releases/download/#{version}/Defold-x86_64-macos.dmg",
       verified: "github.com/defold/defold/"
