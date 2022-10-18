1fa20dd4152e8402f8e7f6d770b7aee157465f16
eloyaldea <eloy.aldea@gmail.com>
2022-10-18 21:42:15 +0200

    Update microsoft-teams from 1.5.00.22362 to 1.5.00.26358

--- Casks/microsoft-teams.rb
+++ Casks/microsoft-teams.rb
@@ -1,6 +1,6 @@
 cask "microsoft-teams" do
-  version "1.5.00.22362"
-  sha256 "7dbc3a4fe93a47917243b5ce2b3c8d601bd79711f6c6926c067b11d4bfb7266c"
+  version "1.5.00.26358"
+  sha256 "92ea9480e852448cc8afada61192396f7236e1fc5bc053a890f5789553ce85bb"
 
   url "https://statics.teams.cdn.office.net/production-osx/#{version}/Teams_osx.pkg",
       verified: "statics.teams.cdn.office.net/production-osx/"
