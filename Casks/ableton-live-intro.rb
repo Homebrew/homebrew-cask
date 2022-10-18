14cd960b2ba82a34a2e841f3480796030808d9d2
Patrick Linnane <patrick@linnane.io>
2022-10-18 07:49:28 -0700

    Update ableton-live-intro from 11.2 to 11.2.5

--- Casks/ableton-live-intro.rb
+++ Casks/ableton-live-intro.rb
@@ -1,11 +1,11 @@
 cask "ableton-live-intro" do
   arch = "intel"
-  version "11.2"
+  version "11.2.5"
   sha256 "68ae4aa7c1a4286f77de340f5e2556442ea17a73976c3f90c9dce1c75dc8234a"
 
   on_mojave :or_newer do
     arch = "universal"
-    sha256 "68b8260f4b49cac7a3ef19f8efae8c95e0769867f1d25274b111bed51ddef86c"
+    sha256 "95b12615d28ef5d0b48a93e231da7736291be84fb0ed8f99e350b9fc703cf98e"
   end
 
   url "https://cdn-downloads.ableton.com/channels/#{version}/ableton_live_intro_#{version}_#{arch}.dmg"
