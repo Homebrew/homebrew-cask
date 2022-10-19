1eb095c3c05d14ceb7f0c4d118880f2b27dbbc6f
Patrick Linnane <patrick@linnane.io>
2022-10-19 11:46:28 -0700

    grammarly-desktop 1.13.2.0
    Update grammarly-desktop from 1.13.1.0 to 1.13.2.0
    
    Closes #133826.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/grammarly-desktop.rb
+++ Casks/grammarly-desktop.rb
@@ -1,6 +1,6 @@
 cask "grammarly-desktop" do
-  version "1.13.1.0"
-  sha256 "b1d8dbf557aff50573e18386ccf5e2d2ac2eb12a6930e0e468f5b6b6b89afa59"
+  version "1.13.2.0"
+  sha256 "9d0ce34d135c08e431a719548d68b523481af1a447975366c9b06a4a2635bbc8"
 
   url "https://download-mac.grammarly.com/versions/#{version}/Grammarly.dmg"
   name "Grammarly Desktop"
