2a1c7d9cd60614d4680b2833e2513d3cb2b0dc34
Patrick Linnane <patrick@linnane.io>
2022-10-19 12:20:22 -0700

    Update remote-desktop-manager-free from 2022.2.12.0 to 2022.2.15.0

--- Casks/remote-desktop-manager-free.rb
+++ Casks/remote-desktop-manager-free.rb
@@ -1,6 +1,6 @@
 cask "remote-desktop-manager-free" do
-  version "2022.2.12.0"
-  sha256 "3c541da2d4a182ed88da8e6685934f3728980e2cd1c2a1766590b73f54966220"
+  version "2022.2.15.0"
+  sha256 "60f640beddf4c060d0501cd0aa12026b119817cac6cd068f7989c07fc7f612b1"
 
   url "https://cdn.devolutions.net/download/Mac/Devolutions.RemoteDesktopManager.Free.Mac.#{version}.dmg",
       verified: "devolutions.net/"
