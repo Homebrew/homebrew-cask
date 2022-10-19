1d56e0b7247c7ca188fd8dca58130e4f1a055cf0
Patrick Linnane <patrick@linnane.io>
2022-10-19 12:44:54 -0700

    tiger-trade 7.20.2,9B36CF
    Update tiger-trade from 7.20.1 to 7.20.2
    
    Closes #133864.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/tiger-trade.rb
+++ Casks/tiger-trade.rb
@@ -1,6 +1,6 @@
 cask "tiger-trade" do
-  version "7.20.1,8DC719"
-  sha256 "33ec4cac0ef0b8f3602cb5b1d35bbbd19aea95dc8c25c17d60efe275dc19d4bf"
+  version "7.20.2,9B36CF"
+  sha256 "7626848e7e27542fbe5c9321b7ff72212d0ddacc49ff47882fecc4e5d48c41bb"
 
   url "https://s.tigerfintech.com/desktop/cdn/f/TigerTrade_#{version.tr(",", "_")}.dmg",
       verified: "s.tigerfintech.com/"
