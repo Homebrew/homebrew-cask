ea45ed2577a9ac2a73d3947c9c8224a2a5baaeb0
YellowForLight <94635645+YellowForLight@users.noreply.github.com>
2022-10-19 21:15:37 +0100

    Add PrismLauncher.app v5.0 (#133847)
    Co-authored-by: YellowForLight <yellowforlightmods@gmail.com>

---
+++ Casks/prism.rb
@@ -0,0 +1,26 @@
+cask "prism" do
+  version "5.0"
+
+  if MacOS.version < :catalina
+    sha256 "54816a644e0c9a0275837529a691c46481cf548ef140318bddb96f87a4b5152d"
+    url "https://github.com/PrismLauncher/PrismLauncher/releases/download/#{version}/PrismLauncher-macOS-Legacy-#{version}.tar.gz",
+        verified: "github.com/PrismLauncher/PrismLauncher"
+  else
+    sha256 "e6858fb3d484fa60df7ae7c415da1c23697149c1ede7e4a5e0f0cd1ca40dc4a6"
+    url "https://github.com/PrismLauncher/PrismLauncher/releases/download/#{version}/PrismLauncher-macOS-#{version}.tar.gz",
+        verified: "github.com/PrismLauncher/PrismLauncher"
+  end
+  name "Prism Launcher"
+  desc "Minecraft launcher"
+  homepage "https://prismlauncher.org/"
+
+  app "PrismLauncher.app"
+
+  zap trash: [
+    "~/Library/Application Support/PrismLauncher/metacache",
+    "~/Library/Application Support/PrismLauncher/PrismLauncher-*.log",
+    "~/Library/Application Support/PrismLauncher/prismlauncher.cfg",
+    "~/Library/Preferences/org.prismlauncher.PrismLauncher.plist",
+    "~/Library/Saved Application State/org.prismlauncher.PrismLauncher.savedState",
+  ]
+end
