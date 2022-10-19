dbb72906fc2d66dbae28c353ebd5a127758d1691
Patrick Linnane <patrick@linnane.io>
2022-10-19 12:00:11 -0700

    macx-youtube-downloader 5.2.2,20221019
    Update macx-youtube-downloader from 5.2.1 to 5.2.2
    
    Closes #133834.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/macx-youtube-downloader.rb
+++ Casks/macx-youtube-downloader.rb
@@ -1,5 +1,5 @@
 cask "macx-youtube-downloader" do
-  version "5.2.1,20211018"
+  version "5.2.2,20221019"
   sha256 :no_check
 
   url "https://www.macxdvd.com/download/macx-youtube-downloader-free.dmg"
