a80770272d9dfcceebc4daec9624c272962b2d69
Patrick Linnane <patrick@linnane.io>
2022-10-19 21:27:20 -0700

    sage: rebuild
    Fix livecheck
    
    Closes #133885.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/sage.rb
+++ Casks/sage.rb
@@ -12,7 +12,21 @@ cask "sage" do
   homepage "https://www.sagemath.org/"
 
   livecheck do
-    skip "Requires checking separate GitHub release asset list HTML"
+    url "https://github.com/3-manifolds/Sage_macOS/releases/latest"
+    regex(%r{href=.*?/v?(\d+(?:\.\d+)+)/SageMath[._-]v?(\d+(?:\.\d+)+)[._-].*?#{arch}\.dmg}i)
+    strategy :header_match do |headers, regex|
+      next if headers["location"].blank?
+
+      # Identify the latest tag from the response's `location` header
+      latest_tag = File.basename(headers["location"])
+      next if latest_tag.blank?
+
+      # Fetch the assets list HTML for the latest tag and match within it
+      assets_page = Homebrew::Livecheck::Strategy.page_content(
+        @url.sub(%r{/releases/?.+}, "/releases/expanded_assets/#{latest_tag}"),
+      )
+      assets_page[:content]&.scan(regex)&.map { |match| "#{match[1]},#{match[0]}" }
+    end
   end
 
   depends_on macos: ">= :high_sierra"
