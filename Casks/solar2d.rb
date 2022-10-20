ceb2d6db915fc3a8a080fb6edd20d1127d3bd59f
Patrick Linnane <patrick@linnane.io>
2022-10-19 21:41:14 -0700

    Update solar2d from 2022.3677 to 2022.3682

--- Casks/solar2d.rb
+++ Casks/solar2d.rb
@@ -1,6 +1,6 @@
 cask "solar2d" do
-  version "2022.3677"
-  sha256 "6458c9ba5735b918b397bdad609fb246e400be70cbcc9c56cca8722112cca7dc"
+  version "2022.3682"
+  sha256 "b16f4d5f9b2286c2d37e1321994cd4cf435c5a54cbc65a67e125ecc7d48ab518"
 
   url "https://github.com/coronalabs/corona/releases/download/#{version.minor}/Solar2D-macOS-#{version}.dmg",
       verified: "github.com/coronalabs/corona/"
@@ -9,9 +9,21 @@ cask "solar2d" do
   homepage "https://solar2d.com/"
 
   livecheck do
-    url :url
-    strategy :github_latest
+    url "https://github.com/coronalabs/corona/releases/latest"
     regex(%r{href=.*?/Solar2D[._-]macOS[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
+    strategy :header_match do |headers, regex|
+      next if headers["location"].blank?
+
+      # Identify the latest tag from the response's `location` header
+      latest_tag = File.basename(headers["location"])
+      next if latest_tag.blank?
+
+      # Fetch the assets list HTML for the latest tag and match within it
+      assets_page = Homebrew::Livecheck::Strategy.page_content(
+        @url.sub(%r{/releases/?.+}, "/releases/expanded_assets/#{latest_tag}"),
+      )
+      assets_page[:content]&.scan(regex)&.map { |match| match[0] }
+    end
   end
 
   suite "Corona-#{version.minor}"
