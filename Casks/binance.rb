2674bb2448cf83235423305e3ac87642cd7190b2
Patrick Linnane <patrick@linnane.io>
2022-10-20 08:06:28 -0700

    binance 1.40.0
    Update binance from 1.39.0 to 1.40.0
    
    Closes #133905.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/binance.rb
+++ Casks/binance.rb
@@ -1,6 +1,6 @@
 cask "binance" do
-  version "1.39.0"
-  sha256 "061f8ad050b20e852aa3b5a322eacb7b399e9b825f63c0a7e340a53b038d15a6"
+  version "1.40.0"
+  sha256 "778c1bd5505cce38f209cb2cfca0c87c11ac2f50a9560368b66b4841bca8f342"
 
   url "https://ftp.binance.com/electron-desktop/mac/production/binance-#{version}.dmg"
   name "Binance"
