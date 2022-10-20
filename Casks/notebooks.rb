39cc500cba9b8d73129f48d41169d788f4b81b7d
Patrick Linnane <patrick@linnane.io>
2022-10-20 09:09:29 -0700

    notebooks 3.0.2,225
    Update notebooks from 3.0.1 to 3.0.2
    
    Closes #133931.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/notebooks.rb
+++ Casks/notebooks.rb
@@ -1,6 +1,6 @@
 cask "notebooks" do
-  version "3.0.1,224"
-  sha256 "6bec6791c4efb2cc37ca3874d593c71ea07e4cdeeedda3294db7e8817018d790"
+  version "3.0.2,225"
+  sha256 "d3630fd9b8a2d9578373b58ec2cd762613a2fb8cdcf430d3eadec8e3a36a6d2f"
 
   url "https://www.notebooksapp.com/Download/macOS/v#{version.major}/Notebooks.dmg"
   name "Notebooks"
