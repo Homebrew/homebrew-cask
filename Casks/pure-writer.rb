e431883c40ceb40582c500fc3bd3453e0ef86a83
Patrick Linnane <patrick@linnane.io>
2022-10-19 20:55:10 -0700

    Fix livecheck

--- Casks/pure-writer.rb
+++ Casks/pure-writer.rb
@@ -9,10 +9,20 @@ cask "pure-writer" do
   homepage "https://writer.drakeet.com/desktop"
 
   livecheck do
-    url "https://github.com/PureWriter/desktop/releases"
+    url "https://github.com/PureWriter/desktop/releases/latest"
     regex(%r{href=.*?([^/]+)/PureWriter[._-]v?(\d+(?:\.\d+)+)-macOS\.dmg}i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| "#{match.second},#{match.first}" }
+    strategy :header_match do |headers, regex|
+      next if headers["location"].blank?
+
+      # Identify the latest tag from the response's `location` header
+      latest_tag = File.basename(headers["location"])
+      next if latest_tag.blank?
+
+      # Fetch the assets list HTML for the latest tag and match within it
+      assets_page = Homebrew::Livecheck::Strategy.page_content(
+        @url.sub(%r{/releases/?.+}, "/releases/expanded_assets/#{latest_tag}"),
+      )
+      assets_page[:content]&.scan(regex)&.map { |match| "#{match[0]},#{match[1]}" }
     end
   end
 
