cdb43b3bae3f3382a4e1b7dcd505c959a6993d1e
Patrick Linnane <patrick@linnane.io>
2022-10-20 08:20:44 -0700

    Update dcp-o-matic-disk-writer from 2.16.30 to 2.16.31

--- Casks/dcp-o-matic-disk-writer.rb
+++ Casks/dcp-o-matic-disk-writer.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-disk-writer" do
-  version "2.16.30"
-  sha256 "723ac63bed2ec5891b498527e4cf4b37547d5f6cf140d087d153749278744469"
+  version "2.16.31"
+  sha256 "22677cd1e8abf42fc6b43c5949c9a611b2642068c5fc293e5c4034a0b2ccfb18"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-disk&version=#{version}"
   name "DCP-o-matic Disk Writer"
