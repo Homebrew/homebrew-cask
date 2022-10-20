1bbb146ab3dda7bd27e7fe70e96c0d2182d74be9
Mark Gardner <mjg@phoenixtrap.com>
2022-10-20 17:32:11 -0500

    Undo ssh-config-editor beta version bumps
    This undoes #130409 and #131006, where `brew bump-cask-pr` was used
    to update ssh-config-editor to beta versions unlisted on the product
    home page. They should have gone to [homebrew-cask-versions][]
    
    [homebrew-cask-versions]: https://github.com/Homebrew/homebrew-cask-versions

--- Casks/ssh-config-editor.rb
+++ Casks/ssh-config-editor.rb
@@ -1,6 +1,6 @@
 cask "ssh-config-editor" do
-  version "2.6-b,98"
-  sha256 "57c604f95f7df5e89e132aad62e80fa320350641bf96ff777283cdf4b39b555d"
+  version "2.5.1,96"
+  sha256 "a91a0db47e5a858abe1788baea135d1e306f335bb295468066621cffd996a3bf"
 
   url "https://hejki.org/download/ssheditor/SSHConfigEditor-#{version.csv.second}.dmg"
   name "SSH Config Editor"
