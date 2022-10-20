a793e713550dfc288365450fa00d685f36ee3b5d
Patrick Linnane <patrick@linnane.io>
2022-10-20 08:21:57 -0700

    Update dcp-o-matic-editor from 2.16.30 to 2.16.31

--- Casks/dcp-o-matic-editor.rb
+++ Casks/dcp-o-matic-editor.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-editor" do
-  version "2.16.30"
-  sha256 "219d4fd25494805e4c176ecebd403eb59b1678d303358cc5cd699a435462a26b"
+  version "2.16.31"
+  sha256 "fe73b1d697dc8b15c44077eb41053c065d836f91749a3387e129b32969ea395b"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-editor&version=#{version}"
   name "DCP-o-matic Editor"
