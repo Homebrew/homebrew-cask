af677f9e63abbc781d7a463051e7e80b2f347825
Patrick Linnane <patrick@linnane.io>
2022-10-19 21:44:31 -0700

    sonic-robo-blast-2-kart 1.5
    Update sonic-robo-blast-2-kart from 1.3 to 1.5
    
    Closes #133887.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/sonic-robo-blast-2-kart.rb
+++ Casks/sonic-robo-blast-2-kart.rb
@@ -1,19 +1,13 @@
 cask "sonic-robo-blast-2-kart" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "1.3"
-  sha256 "0c598f0b8182391518d1d5777eb63b86465d7d36d3e3a6ab1db718f6b73c33c6"
+  version "1.5"
+  sha256 "6629e82d3d8970848a2c2ef2e7f73f70af40731ee6181b6eb21c6ba32fd30045"
 
-  url "https://github.com/STJr/Kart-Public/releases/download/v#{version}/SRB2Kart-#{version}-macOS.dmg",
+  url "https://github.com/STJr/Kart-Public/releases/download/v#{version}/SRB2Kart-#{version}-Darwin.dmg",
       verified: "github.com/STJr/Kart-Public/"
   name "Sonic Robo Blast 2 Kart"
   desc "Classic styled kart racer, complete with beautiful courses, and wacky items"
   homepage "https://mb.srb2.org/addons/srb2kart.2435/"
-
-  livecheck do
-    url "https://github.com/STJr/Kart-Public/releases/"
-    regex(%r{v?(\d+(?:\.\d+)+)/SRB2Kart.*?macOS\.dmg}i)
-    strategy :page_match
-  end
 
   app "Sonic Robo Blast 2 Kart.app"
 
