4237a32bcc341bc333a8302fa615c2a188205402
Lorelei Aurora <iam@l5i.me>
2022-10-20 01:20:21 +0100

    Add Topaz Video AI.app v3.0.0 (#133534)
    * Add Topaz Video Enhance AI.app v2.6.4
    
    * Update topaz-video-enhance-ai.rb
    
    * Rename & add zap
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Co-authored-by: Patrick Linnane <patrick@linnane.io>

---
+++ Casks/topaz-video-ai.rb
@@ -0,0 +1,21 @@
+cask "topaz-video-ai" do
+  version "3.0.0"
+  sha256 "86c9421b44ab10cf7f7319b0e64c64cfcc7da1ba560785b6b0eb7787e351fef0"
+
+  url "https://downloads.topazlabs.com/deploy/TopazVideoAI/#{version}/TopazVideoAI-#{version}.dmg"
+  name "Topaz Video AI"
+  desc "Video upscaler and quality enhancer"
+  homepage "https://www.topazlabs.com/topaz-video-ai"
+
+  livecheck do
+    url "https://topazlabs.com/d/veai/latest/mac/full"
+    strategy :header_match
+  end
+
+  app "Topaz Video AI.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.topazlabs.Topaz-Video-AI.plist",
+    "~/Library/Saved Application State/com.topazlabs.Topaz-Video-AI.savedState",
+  ]
+end
