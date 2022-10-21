22b835df156eba778d6ab11aa1856921a26097b8
Patrick Linnane <patrick@linnane.io>
2022-10-20 20:09:08 -0700

    apifox 2.2.2
    Update apifox from 2.2.1 to 2.2.2
    
    Closes #133958.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/apifox.rb
+++ Casks/apifox.rb
@@ -1,9 +1,9 @@
 cask "apifox" do
   arch arm: "-macOS-arm64"
 
-  version "2.2.1"
-  sha256 arm:   "dd107dbaea97506c2b96169c80ebeaa0d44016ca1909c33f02f4dc34311a0f45",
-         intel: "4db4f8f57f0c81e62a00b7a9566461ca146c2dd183fbe27f84cf4fb6c677ad82"
+  version "2.2.2"
+  sha256 arm:   "d0065345b589319971f6fb61134783bc3c14b87b697ef3c780fd8a6d3d16775d",
+         intel: "978de059885bc327ef56e891798edcb44ebb441fc3b009acfb8d4d8efcdea2f2"
 
   url "https://cdn.apifox.cn/download/#{version}/Apifox#{arch}-#{version}.dmg"
   name "Apifox"
