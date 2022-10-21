a6e04462fd94f4e9c10e04f5f8a03fd01858a8a1
Patrick Linnane <patrick@linnane.io>
2022-10-21 10:40:36 -0700

    Update onekey from 3.14.0 to 3.15.1

--- Casks/onekey.rb
+++ Casks/onekey.rb
@@ -1,9 +1,9 @@
 cask "onekey" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.14.0"
-  sha256 arm:   "c4071780c9f7915ea2bfabcae522a05688106f0e877eabfecf6ede09c7c6e36e",
-         intel: "9cf0bf33b2581111249f4a08dea9ab06ccd957f24088f4fdf9882188fddf90d2"
+  version "3.15.1"
+  sha256 arm:   "eed5bcf13d2c32d469a66d4eea5a4eab808c70c9d798941ce3125446164f4b19",
+         intel: "b6692f967702eaeb5b060fcaf1fd7fe972cbe1c286fe45943e5a3e143630e26a"
 
   url "https://github.com/OneKeyHQ/app-monorepo/releases/download/v#{version}/OneKey-Wallet-#{version}-mac-#{arch}.dmg",
       verified: "github.com/OneKeyHQ/app-monorepo/"
