18491a95336be8ea4ea86d218159fd9a02321354
Patrick Linnane <patrick@linnane.io>
2022-10-21 11:04:28 -0700

    propresenter 7.10.2,118096412
    Update propresenter from 7.10.1 to 7.10.2
    
    Closes #134018.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/propresenter.rb
+++ Casks/propresenter.rb
@@ -1,6 +1,6 @@
 cask "propresenter" do
-  version "7.10.1,118096129"
-  sha256 "46d99bb23f937727f299826771aa1e5bbdf5356934efd1436db6252caa79a207"
+  version "7.10.2,118096412"
+  sha256 "3cf804edd03e15c37b710f7dd0f818e5d92421d95a2ecb3d9f1e0dc35ff3f4dc"
 
   url "https://renewedvision.com/downloads/propresenter/mac/ProPresenter_#{version.csv.first}_#{version.csv.second}.zip"
   name "ProPresenter"
