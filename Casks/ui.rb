6c18ce73a9cf7215f8b0409501ce0d0d5d71ed8b
Nils Streedain <tannins_berets_0@icloud.com>
2022-10-21 12:47:03 -0700

    Switch back to versioned URL
    - Added versioned URL back
    - Switched only livecheck to unversioned URL
    - Updated current version

--- Casks/ui.rb
+++ Casks/ui.rb
@@ -1,14 +1,15 @@
 cask "ui" do
-  version "98a0-macOS-0.56.3-79aba98f601b49818f3dae0e3d34b754"
-  sha256 :no_check
+  version "b755-macOS-0.56.3-b2d6dcc1ca1b470db93a710da52de392"
+  sha256 "d3ce491d37b6aa184247359d1b4f1a5b2e48f6054f521a53d40f4de22b513884"
 
-  url "https://api-gw.uid.alpha.ui.com:443/location/api/v1/public/fw/download/latest/?app=UI-DESKTOP-MACOS"
+  url "https://fw-download.ubnt.com/data/uid-ui-desktop-app/#{version}.pkg",
+      verified: "fw-download.ubnt.com"
   name "UI Desktop"
   desc "Corporate Wi-Fi, VPN, SSO, and HR Application"
   homepage "https://www.ui.com/uid"
 
   livecheck do
-    url :url
+    url "https://api-gw.uid.alpha.ui.com:443/location/api/v1/public/fw/download/latest/?app=UI-DESKTOP-MACOS"
     strategy :header_match
     regex(/(\w+[._-]macOS[._-]\d+(?:\.\d+)+[._-]\w+)\.pkg/i)
   end
