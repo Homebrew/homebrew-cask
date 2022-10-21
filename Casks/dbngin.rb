85ce32a8af03e42b34c3d7579c3200552b30e974
Patrick Linnane <patrick@linnane.io>
2022-10-21 09:55:02 -0700

    dbngin 5.6,56
    Update dbngin from 5.2 to 5.6
    
    Closes #133993.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/dbngin.rb
+++ Casks/dbngin.rb
@@ -1,6 +1,6 @@
 cask "dbngin" do
-  version "5.2,54"
-  sha256 "2f57263c290a552092b99175a58da96f9ffd630aadc5fc9400a4cbecd90b0861"
+  version "5.6,56"
+  sha256 "d46d300a2e582d50543af30442e37c9de0aa83bee8cedbf0d4e8711ba9e3edfb"
 
   url "https://dbngin-osx-builds.s3.amazonaws.com/#{version.csv.second}/DBngin.dmg",
       verified: "dbngin-osx-builds.s3.amazonaws.com/"
