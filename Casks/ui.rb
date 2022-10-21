96231c1cbf12ed6d7eb45d96d91bd30bf40eee37
Patrick Linnane <patrick@linnane.io>
2022-10-21 14:06:35 -0700

    Fix livecheck & URL

--- Casks/ui.rb
+++ Casks/ui.rb
@@ -1,22 +1,27 @@
 cask "ui" do
-  version "b755-macOS-0.56.3-b2d6dcc1ca1b470db93a710da52de392"
+  version "0.56.3,b2d6dcc1ca1b470db93a710da52de392,b755"
   sha256 "d3ce491d37b6aa184247359d1b4f1a5b2e48f6054f521a53d40f4de22b513884"
 
-  url "https://fw-download.ubnt.com/data/uid-ui-desktop-app/#{version}.pkg",
-      verified: "fw-download.ubnt.com"
+  url "https://fw-download.ubnt.com/data/uid-ui-desktop-app/#{version.csv.third}-macOS-#{version.csv.first}-#{version.csv.second}.pkg",
+      verified: "fw-download.ubnt.com/data/uid-ui-desktop-app/"
   name "UI Desktop"
   desc "Corporate Wi-Fi, VPN, SSO, and HR Application"
   homepage "https://www.ui.com/uid"
 
   livecheck do
     url "https://api-gw.uid.alpha.ui.com:443/location/api/v1/public/fw/download/latest/?app=UI-DESKTOP-MACOS"
-    strategy :header_match
-    regex(/(\w+[._-]macOS[._-]\d+(?:\.\d+)+[._-]\w+)\.pkg/i)
+    regex(/(\w+)[._-]macOS[._-](\d+(?:\.\d+)+)[._-](\w+)/i)
+    strategy :header_match do |headers, regex|
+      match = headers["location"].match(regex)
+      next if match.blank?
+
+      "#{match[2]},#{match[3]},#{match[1]}"
+    end
   end
 
   depends_on macos: ">= :mojave"
 
-  pkg "#{version}.pkg"
+  pkg "#{version.csv.third}-macOS-#{version.csv.first}-#{version.csv.second}.pkg"
 
   uninstall pkgutil:   [
               "com.ui.uid.desktop",
