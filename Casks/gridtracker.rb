cc1d2908150c71b14ba1dd92a855d3a1fc800331
Patrick Linnane <patrick@linnane.io>
2022-10-21 10:19:20 -0700

    gridtracker 1.22.1016
    * Update gridtracker from 1.22.1010 to 1.22.1016
    * Adjust livecheck
    
    Closes #134001.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/gridtracker.rb
+++ Casks/gridtracker.rb
@@ -1,19 +1,15 @@
 cask "gridtracker" do
-  version "1.22.1010,1.22.1010"
-  sha256 "007f9e614e63f5c26ceb4249ec44c8c73e032022e018d80e4f040eac89f92e1c"
+  version "1.22.1016"
+  sha256 "57622bc9cd90fd9053081f187d50a700dccf5506ada6a86bb59f94d68bef7153"
 
-  url "https://storage.googleapis.com/gt_download/v#{version.csv.second}/GridTracker-#{version.csv.first}-mac-x64.zip",
-      verified: "storage.googleapis.com/gt_download/"
+  url "https://downloads.gridtracker.org/v#{version}-Harvest/GridTracker-#{version}-mac-x64.zip"
   name "GridTracker"
   desc "Warehouse of amateur radio information presented in an easy to use interface"
   homepage "https://gridtracker.org/grid-tracker/"
 
   livecheck do
     url "https://gridtracker.org/release.html"
-    regex(%r{href=.*/v?(\d+(?:[.-]\d+)+)/GridTracker[._-]v?(\d+(?:\.\d+)+)-mac-x64\.zip}i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| "#{match.second},#{match.first}" }
-    end
+    regex(%r{href=.*?/GridTracker[._-]v?(\d+(?:\.\d+)+)-mac-x64\.zip}i)
   end
 
   depends_on macos: ">= :mojave"
