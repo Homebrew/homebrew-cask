39c7bc0454e8aaa26c589a901cd1a70cb3c3eee3
Patrick Linnane <patrick@linnane.io>
2022-10-21 11:03:56 -0700

    Update postman-cli from 1.0.3 to 1.0.5

--- Casks/postman-cli.rb
+++ Casks/postman-cli.rb
@@ -1,6 +1,6 @@
 cask "postman-cli" do
-  version "1.0.3"
-  sha256 "2fce4aa48fed7437b8b8ec3ae008071df91737b8c7959217a8da16fbc9805f42"
+  version "1.0.5"
+  sha256 "277dca1f1f82b5124d6d76b37746b1fd63139d1a9dec3849252924aadf05e9c8"
 
   url "https://dl-cli.pstmn.io/download/postman-cli-cli-#{version}-macos-x64.zip",
       verified: "dl-cli.pstmn.io/download/"
