1987c226654aba15d4e0fcb9bfc8d99c77a38281
Lee Jongyoung <leejongyoung@icloud.com>
2022-10-22 20:15:01 +0900

    Update hancom-word-2022 from latest to latest

---
+++ Casks/hancom-word-2022.rb
@@ -0,0 +1,14 @@
+cask "hancom-word-2022" do
+  version :latest
+  sha256 :no_check
+
+  url "https://cdn.hancom.com/pds/hnc/DOWN/HancomDocs/HwpMac_HancomDocs.pkg",
+      referer: "https://hancom.com/mypage/myIndivPrdtList.do"
+  name "Hacom Word Processor 2022"
+  desc "Word processor"
+  homepage "https://office.hancom.com/"
+
+  pkg "HwpMac_HancomDocs.pkg"
+
+  uninstall quit: "com.hancom.office.hwp12.mac.general"
+end
