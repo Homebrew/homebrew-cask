8a073d4f9520c670656a6657d90a74619d033869
Patrick Linnane <patrick@linnane.io>
2022-10-22 09:07:29 -0700

    Update httpie from 2022.14.0 to 2022.15.0

--- Casks/httpie.rb
+++ Casks/httpie.rb
@@ -1,9 +1,9 @@
 cask "httpie" do
   arch arm: "-arm64"
 
-  version "2022.14.0"
-  sha256 arm:   "69b2952bb661a9de3087f1e5dca5e93e784e8cb941a7b88a445d241848c6c524",
-         intel: "bf7b024890f9bc3a6bf7721e5e5cebf0f188b5c11d987c8e2de6e8c6ff6dbd7f"
+  version "2022.15.0"
+  sha256 arm:   "9c020b589b285243e5c7b996bf6153803d2b3da7c318be2e76cfd5529e225a0a",
+         intel: "0b3901d559fa7dc4ca9bcbbd8bc73a061d8c595373e077c4e52b82663ad8feb9"
 
   url "https://github.com/httpie/desktop/releases/download/v#{version}/HTTPie-#{version}#{arch}.dmg",
       verified: "github.com/httpie/desktop/"
