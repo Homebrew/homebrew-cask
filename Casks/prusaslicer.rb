05dcd53f3b9738f96b8efd1b500ddcb1bb9a4460
Patrick Linnane <patrick@linnane.io>
2022-10-21 20:42:53 -0700

    Update URL & livecheck

--- Casks/prusaslicer.rb
+++ Casks/prusaslicer.rb
@@ -1,21 +1,15 @@
 cask "prusaslicer" do
-  version "2.5.0,202209060719"
-  sha256 "b790aabf043e67aadca2ff2384ad377be71a40b26e2abe87f8d96cc4c228a9b0"
+  version "2.5.0"
+  sha256 "73c30212f3eca4e494d60b0c14648c002bd9876f7ba39587f7315a27f6151296"
 
-  url "https://github.com/prusa3d/PrusaSlicer/releases/download/version_#{version.csv.first}/PrusaSlicer-#{version.csv.first}+MacOS-universal-#{version.csv.second}.dmg",
-      verified: "github.com/prusa3d/PrusaSlicer/"
+  url "https://cdn.prusa3d.com/downloads/drivers/prusa3d_mac_#{version.dots_to_underscores}.dmg"
   name "PrusaSlicer"
   desc "G-code generator for 3D printers (RepRap, Makerbot, Ultimaker etc.)"
   homepage "https://www.prusa3d.com/slic3r-prusa-edition/"
 
   livecheck do
-    url "https://github.com/prusa3d/PrusaSlicer/releases/"
-    strategy :page_match do |page|
-      match = page.match(%r{href=.*?/PrusaSlicer-(\d+(?:\.\d+)*)\+MacOS-universal-(\d+)\.dmg}i)
-      next if match.blank?
-
-      "#{match[1]},#{match[2]}"
-    end
+    url "https://files.prusa3d.com/?latest=slicer-stable"
+    regex(%r{<h2>VERSION\s(\d+(?:\.\d+)+).*</h2>}i)
   end
 
   app "PrusaSlicer.app"
