89adbcd56ca4b37d440f1d5f5c5f7feef0bf20f7
Patrick Linnane <patrick@linnane.io>
2022-10-22 21:58:55 -0700

    gcs 5.3.0
    Update gcs from 5.2.0 to 5.3.0
    
    Closes #134081.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/gcs.rb
+++ Casks/gcs.rb
@@ -1,9 +1,9 @@
 cask "gcs" do
   arch arm: "apple", intel: "intel"
 
-  version "5.2.0"
-  sha256 arm:   "78aa94c0d07a0d3b0bfcb8b4d9a4de9acfa4257f64a7ba786257f973b0cb4a52",
-         intel: "972e4d0a1b07e6ff4652d998e7e2ee0ee2742e3c13d1b6580aa56052996d2770"
+  version "5.3.0"
+  sha256 arm:   "61d036f86cb5bf68ddfaa34cbb3db602fa1820e0d19f9f63f1f1ac97c6a9f8c1",
+         intel: "0a18ec4877b773951d7a2639d5f5b1c37c3d5de529239210ed2f605fe2461303"
 
   url "https://github.com/richardwilkes/gcs/releases/download/v#{version}/gcs-#{version}-macos-#{arch}.dmg",
       verified: "github.com/richardwilkes/gcs/"
