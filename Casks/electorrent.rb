d6a79b68f6cc3b42bd146d0b524e9af5eb5b6057
Patrick Linnane <patrick@linnane.io>
2022-10-23 09:48:16 -0700

    Update electorrent from 2.8.1 to 2.8.2

--- Casks/electorrent.rb
+++ Casks/electorrent.rb
@@ -1,6 +1,6 @@
 cask "electorrent" do
-  version "2.8.1"
-  sha256 "c9cb9726a72e6ed6b7fab9d08d73ab44564bd262663139ce76dc4ca7406bac61"
+  version "2.8.2"
+  sha256 "8e871e5d88b97167422a53081e18f12b5d395e4f05d58e20ccbb32b35f6db54f"
 
   url "https://github.com/tympanix/Electorrent/releases/download/v#{version}/electorrent-#{version}.dmg"
   name "Electorrent"
