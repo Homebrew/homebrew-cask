b332528541bb351c91ee7cbf88448272be60686d
Jakob Yanagibashi <3247063+jeifour@users.noreply.github.com>
2022-10-23 18:21:54 +0200

    Update standard-notes from 3.23.221 to 3.23.238 (#134095)
    Update standard-notes from 3.23.238 to 3.23.238
    
    Co-authored-by: Jakob <iokybsukefsfwd@jy.invalid>

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -1,9 +1,9 @@
 cask "standard-notes" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.23.221"
-  sha256 arm:   "61fca67f17e5f7b5b466bffbe66a94f252b69262bcc5427100849c47e3e7f7d7",
-         intel: "e0c908f29a3d6c27f9df9f3126eb339b96d89debc9006aa2576dae5167e71056"
+  version "3.23.238"
+  sha256 arm:   "f583dffb2c816f93299385e88597f14ffa6ff5b05a78286c1ba0c3f6a02634cf",
+         intel: "2c5bcbbc6646b14df4565501c0eb19c7fbccbc513e8c23aae2567047527788b7"
 
   url "https://github.com/standardnotes/app/releases/download/%40standardnotes%2Fdesktop%40#{version}/standard-notes-#{version}-mac-#{arch}.zip",
       verified: "github.com/standardnotes/app/"
