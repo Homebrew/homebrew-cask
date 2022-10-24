10ad9a07c878986f86c9d45fc56c32a1cfee3639
Pavel Zwerschke <pavelzw@gmail.com>
2022-10-24 14:18:47 +0200

    linearmouse 0.7.4
    Update linearmouse from 0.7.3 to 0.7.4
    
    Closes #134178.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/linearmouse.rb
+++ Casks/linearmouse.rb
@@ -1,6 +1,6 @@
 cask "linearmouse" do
-  version "0.7.3"
-  sha256 "9dd93eec1f27dee5573ee6962a51c17b0be3b49869daa3dd690ce22c46c1adea"
+  version "0.7.4"
+  sha256 "7d0fc97e89078ab033aa00abfc6fd23549feb019d73533db3f32059855c2a5af"
 
   url "https://github.com/linearmouse/linearmouse/releases/download/v#{version}/LinearMouse.dmg",
       verified: "github.com/linearmouse/linearmouse/"
