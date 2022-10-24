cdb2bd9a834f43b782a58b3d36459e7b87ac625c
Patrick Linnane <patrick@linnane.io>
2022-10-24 08:37:20 -0700

    Update cnkiexpress from 0.2.28 to 0.2.29

--- Casks/cnkiexpress.rb
+++ Casks/cnkiexpress.rb
@@ -1,6 +1,6 @@
 cask "cnkiexpress" do
-  version "0.2.28"
-  sha256 "fa8a4eddd48970057a2fcda43b76ac6526f79418b5dc251a2450cf17b770f536"
+  version "0.2.29"
+  sha256 "eeff7f77f250420c77ae7abd28bbd8d0e7e6663ae9235ff9dce1c8387610e137"
 
   url "https://download.cnki.net/cnkiexpress/\%E5\%85\%A8\%E7\%90\%83\%E5\%AD\%A6\%E6\%9C\%AF\%E5\%BF\%AB\%E6\%8A\%A5-#{version}.dmg"
   name "CNKIExpress"
