553058fdfbc87addc2cbd940ad02d66c4f9ca78c
Curudel <andreas.florin.1988@outlook.com>
2022-10-24 18:49:33 +0200

    aerial 3.2.0
    Update aerial from 3.1.3 to 3.2.0
    
    Closes #134208.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/aerial.rb
+++ Casks/aerial.rb
@@ -1,6 +1,6 @@
 cask "aerial" do
-  version "3.1.3"
-  sha256 "73de2d73fd55d46e86e939999dcfb7861afbaf9610b1b2abf0cfbeaba7a77edb"
+  version "3.2.0"
+  sha256 "af269cf015ce3c809d15b78f5480a7eb748eda41fa796a2c5524ab4cd7b73b61"
 
   url "https://github.com/JohnCoates/Aerial/releases/download/v#{version}/Aerial.saver.zip"
   name "Aerial Screensaver"
