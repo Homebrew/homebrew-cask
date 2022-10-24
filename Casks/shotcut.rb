375aec6a18aa6aa067a1b0e595e5b2c3d7697d5a
Patrick Linnane <patrick@linnane.io>
2022-10-23 22:24:09 -0700

    shotcut 22.10.22
    Update shotcut from 22.09.23 to 22.10.22
    
    Closes #134166.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/shotcut.rb
+++ Casks/shotcut.rb
@@ -1,9 +1,9 @@
 cask "shotcut" do
   arch arm: "macos-ARM64", intel: "macos"
 
-  version "22.09.23"
-  sha256 arm:   "ab91a3a0a18ccb1f9486ad95a5d5f87899a6aa3e3f918eb1f0834ed361f457c2",
-         intel: "63d9a80c2a0afb5aaefdbf4d77b3301e4d9629f719b8b3bce13c68373b32b46b"
+  version "22.10.22"
+  sha256 arm:   "3006f44a233deb29745a9e58cae72e933dd6152cd3d01437cd90381c9ff0ddde",
+         intel: "7a50d75f56a891fac25bc7413f18a8798f06b799ff54222dbaf0ef4f80edac2c"
 
   url "https://github.com/mltframework/shotcut/releases/download/v#{version}/shotcut-#{arch}-#{version.no_dots}.dmg",
       verified: "github.com/mltframework/shotcut/"
