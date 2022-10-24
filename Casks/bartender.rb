529e2ae562658c8f24db2537b645ef55f9aa693f
Sri Mouli <srimouli.kathula@gmail.com>
2022-10-24 16:03:15 +0530

    Update bartender from 4.2.12,42012 to 4.2.12

--- Casks/bartender.rb
+++ Casks/bartender.rb
@@ -10,7 +10,7 @@ cask "bartender" do
     url "https://macbartender.com/B2/updates/#{version.csv.first.dots_to_hyphens}/Bartender%20#{version.major}.zip",
         referer: "https://www.macbartender.com"
   else
-    version "4.2.12,42012"
+    version "4.2.12"
     sha256 "29172e0546cea611d4c61ad710cece46550a3272c789ba512371af7a1090025f"
     url "https://macbartender.com/B2/updates/#{version.csv.first.major}-#{version.csv.first.minor}-#{version.csv.first.patch.rjust(1, "0")}/Bartender%20#{version.major}.dmg",
         referer: "https://www.macbartender.com"
