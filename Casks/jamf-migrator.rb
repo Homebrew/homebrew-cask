ffbaf53558dbc272a8bf54b3dde93eb5c5d35022
Patrick Linnane <patrick@linnane.io>
2022-10-24 19:02:50 -0700

    jamf-migrator 7.0.1
    Update jamf-migrator from 7.0.0 to 7.0.1
    
    Closes #134231.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/jamf-migrator.rb
+++ Casks/jamf-migrator.rb
@@ -1,19 +1,13 @@
 cask "jamf-migrator" do
-  version "7.0.0"
-  sha256 :no_check
+  version "7.0.1"
+  sha256 "ce0106e984cf84e7d5279b9a35aede3fba932396bbe9fa2164936d2befba1207"
 
-  url "https://github.com/jamf/JamfMigrator/releases/latest/download/jamf-migrator.zip"
+  url "https://github.com/jamf/JamfMigrator/releases/download/v#{version}/jamf-migrator.zip"
   name "JamfMigrator"
   desc "Tool to migrate data granularly between Jamf Pro servers"
   homepage "https://github.com/jamf/JamfMigrator"
 
-  livecheck do
-    url :url
-    strategy :github_latest
-    regex(/jamf-migrator\s+v?(\d+(?:\.\d+)+)/i)
-  end
-
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :high_sierra"
 
   app "jamf-migrator.app"
 end
