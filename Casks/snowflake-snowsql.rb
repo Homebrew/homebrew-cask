b46e6f298b5fd23ef698ccbb2bd50ca510ee823a
Mark Keller <mark.keller@snowflake.com>
2022-10-24 17:22:17 -0700

    Update snowflake-snowsql from 1.2.21 to 1.2.24 (#134219)
    * Update snowflake-snowsql from 1.2.21 to 1.2.24
    
    * update livecheck
    
    * update style
    
    * Adjust livecheck
    
    Co-authored-by: Patrick Linnane <patrick@linnane.io>

--- Casks/snowflake-snowsql.rb
+++ Casks/snowflake-snowsql.rb
@@ -1,6 +1,6 @@
 cask "snowflake-snowsql" do
-  version "1.2.21"
-  sha256 "ad11cf3b32a99ba6281536e375a71ba8c952371601a071481429810f0d266fb0"
+  version "1.2.24"
+  sha256 "28bd238991e00bf476d3fdb28c1e7d97bd6f0e315fa4c1f9dc78fad4f0e3b89c"
 
   url "https://sfc-repo.snowflakecomputing.com/snowsql/bootstrap/#{version.major_minor}/darwin_x86_64/snowsql-#{version}-darwin_x86_64.pkg",
       verified: "sfc-repo.snowflakecomputing.com/"
@@ -9,8 +9,8 @@ cask "snowflake-snowsql" do
   homepage "https://snowflake.com/"
 
   livecheck do
-    url "https://docs.snowflake.com/en/release-notes/client-change-log-snowsql.html"
-    regex(/>\s*SnowSQL\s*v?(\d+(?:\.\d+)+)\s*</i)
+    url "https://sfc-repo.snowflakecomputing.com/snowsql/bootstrap/#{version.major_minor}/darwin_x86_64/index.html"
+    regex(%r{">snowsql-(\d+(?:\.\d+)+)-darwin_x86_64.pkg</a>})
   end
 
   pkg "snowsql-#{version}-darwin_x86_64.pkg"
