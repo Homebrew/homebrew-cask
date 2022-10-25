982ec6539b3f28948bddbaae43c9df72b50ed505
Patrick Linnane <patrick@linnane.io>
2022-10-25 15:07:15 -0700

    Update understand from 6.2.1122 to 6.2.1123

--- Casks/understand.rb
+++ Casks/understand.rb
@@ -1,6 +1,6 @@
 cask "understand" do
-  version "6.2.1122"
-  sha256 "662066027d93aa1c6f6b00d4f2456036e1067127fa0afa479138491750d59857"
+  version "6.2.1123"
+  sha256 "4b87883876064ed3ab14903a4b88bcc22d01420876915d18b7524b838854ebd5"
 
   url "https://latest.scitools.com/Understand/Understand-#{version}-MacOSX-x86.dmg"
   name "SciTools Understand"
