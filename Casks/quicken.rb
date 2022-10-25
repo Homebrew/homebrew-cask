0401aa8bff1bc2c7e3740ffe71f1f09be03fc98f
Patrick Linnane <patrick@linnane.io>
2022-10-24 20:04:16 -0700

    quicken 6.10.2,610.46306.100
    Update quicken from 6.10.1 to 6.10.2
    
    Closes #134238.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/quicken.rb
+++ Casks/quicken.rb
@@ -1,6 +1,6 @@
 cask "quicken" do
-  version "6.10.1,610.46298.100"
-  sha256 "58aa6afc845290617eb19db22ae69540e982db2dd32a0f0ba6a060ba802c4d1a"
+  version "6.10.2,610.46306.100"
+  sha256 "d44fa843c2532671ff53e1ef6f7c5031323f74470e32cb76106131d81fc8f33e"
 
   url "https://download.quicken.com/mac/Quicken/001/Release/031A96D9-EFE6-4520-8B6A-7F465DDAA3E4/Quicken-#{version.csv.second}/Quicken-#{version.csv.second}.zip"
   name "Quicken"
