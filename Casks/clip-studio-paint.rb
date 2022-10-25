d6b123031379b4f369137ab0af052d5897d3c104
Patrick Linnane <patrick@linnane.io>
2022-10-24 18:29:49 -0700

    Update clip-studio-paint from 1.12.8 to 1.12.11

--- Casks/clip-studio-paint.rb
+++ Casks/clip-studio-paint.rb
@@ -1,6 +1,6 @@
 cask "clip-studio-paint" do
-  version "1.12.8"
-  sha256 "a6cc9bb571ac33e5a98920a54b91980f4d3b3877cb9f87b1a5954515ff0d75ea"
+  version "1.12.11"
+  sha256 "f2b861abcfe752c638c71d592a7f1bb2507aa48043a369d17abb20f2b52e8d19"
 
   url "https://vd.clipstudio.net/clipcontent/paint/app/#{version.no_dots}/CSP_#{version.no_dots}m_app.pkg"
   name "Clip Studio Paint"
