3d5ab4a7c6e96c3876d7f2319fdb2238196bd6d6
Patrick Linnane <patrick@linnane.io>
2022-10-25 13:44:43 -0700

    bankid: rebuild
    Fix livecheck
    
    Closes #134315.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/bankid.rb
+++ Casks/bankid.rb
@@ -8,7 +8,7 @@ cask "bankid" do
   homepage "https://install.bankid.com/"
 
   livecheck do
-    url "https://install.bankid.com/FileDownloader?fileId=Mac"
+    url "https://install.bankid.com/api/file-download/Mac"
     strategy :header_match
   end
 
