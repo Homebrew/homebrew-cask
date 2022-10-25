b0ee4a31e354fdfc2cb8a107342cbb20ffb45631
singularitti <singularitti@outlook.com>
2022-10-25 06:39:26 -0400

    Add more `zap` to Casks/eudic.rb

--- Casks/eudic.rb
+++ Casks/eudic.rb
@@ -29,8 +29,11 @@ cask "eudic" do
     "~/Library/Caches/com.eusoft.eudic.LightPeek",
     "~/Library/Containers/com.eusoft.eudic.QuickLook",
     "~/Library/Eudb_en",
+    "~/Library/HTTPStorages/com.eusoft.eudic",
+    "~/Library/HTTPStorages/com.eusoft.eudic.binarycookies",
     "~/Library/Preferences/com.eusoft.eudic.plist",
     "~/Library/Preferences/com.eusoft.eudic.LightPeek.plist",
     "~/Library/Preferences/group.com.eusoft.eudic.plist",
+    "~/Library/WebKit/com.eusoft.eudic",
   ]
 end
