8cfe7945692b52153cc4937af72582b9b269f753
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-10-26 02:58:09 +0800

    Update videofusion from 3.4.1 to 3.5.0 (#134266)
    * Update videofusion from 3.4.1 to 3.5.0
    
    * Update videofusion.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/videofusion.rb
+++ Casks/videofusion.rb
@@ -1,8 +1,8 @@
 cask "videofusion" do
-  version "3.4.1.7817.0"
-  sha256 "6e41155e72b6f36f7a84178742b266d21cd18e0633e61081327cd023a9cc2869"
+  version "3.5.0.7877"
+  sha256 "c972e6473d176ba774173f52f108eac3b5ace311fe37403e15706e2b46d33816"
 
-  url "https://lf3-package.vlabstatic.com/obj/faceu-packages/Jianying_#{version.dots_to_underscores}.dmg",
+  url "https://lf3-package.vlabstatic.com/obj/faceu-packages/Jianying_#{version.dots_to_underscores}_jianyingpro_0.dmg",
       verified: "lf3-package.vlabstatic.com/obj/faceu-packages/"
   name "VideoFusion"
   name "剪映专业版"
@@ -12,7 +12,7 @@ cask "videofusion" do
   livecheck do
     url "https://lf3-beecdn.bytetos.com/obj/ies-fe-bee/bee_prod/biz_80/bee_prod_80_bee_publish_3563.json"
     strategy :page_match do |page|
-      JSON.parse(page)["mac_download_pkg"]["channel_default"][/(\d+(?:_\d+)+)\.dmg/i, 1].tr("_", ".")
+      JSON.parse(page)["mac_download_pkg"]["channel_default"][/(\d+(?:_\d+)+).*\.dmg/i, 1].tr("_", ".")
     end
   end
 
