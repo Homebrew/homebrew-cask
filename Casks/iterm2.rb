442dfc072b39d8b420a8be4c4efbe8868cc3d9bf
Sean Molenaar <SMillerDev@users.noreply.github.com>
2022-10-26 21:48:35 +0200

    iterm2: depend on catalina (#134353)

--- Casks/iterm2.rb
+++ Casks/iterm2.rb
@@ -28,7 +28,7 @@ cask "iterm2" do
     "homebrew/cask-versions/iterm2-legacy",
     "homebrew/cask-versions/iterm2-nightly",
   ]
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :catalina"
 
   app "iTerm.app"
 
