63cf6933443e7abed3ad2b6015522ca1eb3945c0
Patrick Linnane <patrick@linnane.io>
2022-10-25 21:27:15 -0700

    Update for Ventura

---
+++ Casks/ventura-cache-cleaner.rb
@@ -0,0 +1,17 @@
+cask "ventura-cache-cleaner" do
+  version "18.0"
+  sha256 :no_check
+
+  url "https://www.northernsoftworks.com/downloads/nscc.dmg"
+  name "Monterey Cache Cleaner"
+  name "MCC"
+  desc "General purpose system maintenance tool"
+  homepage "https://www.northernsoftworks.com/venturacachecleaner.html"
+
+  livecheck do
+    url :homepage
+    regex(/Download\s*Ventura\s*Cache\s*Cleaner\s*v?(\d+(?:\.\d+)+)/i)
+  end
+
+  app "Ventura Cache Cleaner.app"
+end
