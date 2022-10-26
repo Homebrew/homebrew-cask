b334ffa870042a74beca8a7a0c233806288a90e2
Patrick Linnane <patrick@linnane.io>
2022-10-26 16:52:36 -0700

    Update livebook from 0.7.1 to 0.7.2

--- Casks/livebook.rb
+++ Casks/livebook.rb
@@ -1,6 +1,6 @@
 cask "livebook" do
-  version "0.7.1"
-  sha256 "5e55a1d99d9f7f830da811525ba275a29aadfea3b908d771606829153b1e48f8"
+  version "0.7.2"
+  sha256 "046871842b42199707150033dc773874cdb53c28e9587cfcda929a74e852c002"
 
   url "https://github.com/livebook-dev/livebook/releases/download/v#{version}/LivebookInstall-v#{version}-macos-universal.dmg",
       verified: "github.com/livebook-dev/livebook"
