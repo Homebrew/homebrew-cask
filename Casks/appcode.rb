2e45df1517488dcc37265774642bcf64929b063d
Patrick Linnane <patrick@linnane.io>
2022-10-26 15:28:05 -0700

    appcode 2022.2.4,222.4345.34
    Update appcode from 2022.2.3 to 2022.2.4
    
    Closes #134376.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/appcode.rb
+++ Casks/appcode.rb
@@ -1,9 +1,9 @@
 cask "appcode" do
   arch arm: "-aarch64"
 
-  version "2022.2.3,222.4345.23"
-  sha256 arm:   "f8056baa3e7edb0013cffe10da256d639244d6c2e9220dbcb0cf74f06b9ff1cb",
-         intel: "65f901c7d370132e167ba26a3c3bc2afa777b0fa471e3e51ae45082f72d99e84"
+  version "2022.2.4,222.4345.34"
+  sha256 arm:   "2a03afe701e69345bdd8476d9c2e863cbffb5702794b3a027cbae02ae547ca43",
+         intel: "3bce1e159b6c2b8e23a0e297c63bb06a12530940881c4f55cecc921632052ee7"
 
   url "https://download.jetbrains.com/objc/AppCode-#{version.csv.first}#{arch}.dmg"
   name "AppCode"
