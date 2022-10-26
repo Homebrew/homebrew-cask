07ca552812f46e2117aca808d57ccfcde57ff339
Jacob Ford <j@cobford.com>
2022-10-26 10:57:01 -0400

    Add basics

---
+++ Casks/picfindr.rb
@@ -0,0 +1,11 @@
+cask "picfindr" do
+  version "1.1.10"
+  sha256 "78a804b40b3d570aed0d64a402d48cda06f9a002c050c32845752df2c4a9b0e1"
+
+  url "https://ushining.softorino.com/shine_uploads/picfindr_#{version}.dmg"
+  name "picfindr"
+  desc "Search engine & manager for free stock images"
+  homepage "https://softorino.com/picfindr/"
+
+  app "PicFindr.app"
+end
