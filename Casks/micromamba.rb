3663cf51bd16431199ba4725ecb387c39fd48e9a
Pavel Zwerschke <pavelzw@gmail.com>
2022-10-27 20:57:07 +0200

    Update micromamba from 0.27.0 to 0.27.0 (#134447)
    * Update micromamba from 0.27.0 to 0.27.0
    
    * Adjust version, & livecheck
    
    Co-authored-by: Patrick Linnane <patrick@linnane.io>

--- Casks/micromamba.rb
+++ Casks/micromamba.rb
@@ -1,19 +1,22 @@
 cask "micromamba" do
   arch arm: "arm64", intel: "64"
 
-  version "0.27.0"
-  sha256 arm:   "4514ca08786692a7b4619fa7c215830e5b6cc6b7077b1c463f4a54b5c82b04e4",
-         intel: "3c1a5ebb151668ce683fb74073bce5e01740663d5917769adb6d2703441b4531"
+  version "0.27.0,2"
+  sha256 arm:   "cefcbab3c6e5b1cf2157211af64a9c054bbf842e632f48cfd14889b11c3f09c3",
+         intel: "32eb83ad09f9c97c230793ef5572ee8cbcf425bcc3683b0035303867c11025aa"
 
-  url "https://micro.mamba.pm/api/micromamba/osx-#{arch}/#{version}",
-      verified: "micro.mamba.pm/api/micromamba/"
+  url "https://api.anaconda.org/download/conda-forge/micromamba/#{version.csv.first}/osx-#{arch}/micromamba-#{version.csv.first}-#{version.csv.second}.tar.bz2",
+      verified: "api.anaconda.org/download/conda-forge/micromamba/"
   name "micromamba"
   desc "Tiny version of the Mamba cross-platform package manager"
   homepage "https://mamba.readthedocs.io/en/latest/installation.html#micromamba"
 
   livecheck do
     url "https://api.anaconda.org/release/conda-forge/micromamba/latest"
-    regex(%r{micromamba/(\d+(?:\.\d+)+)/osx-#{arch}/}i)
+    regex(%r{osx-#{arch}/micromamba-(\d+(?:\.\d+)+)-(\d)\.t}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
+    end
   end
 
   binary "bin/micromamba"
