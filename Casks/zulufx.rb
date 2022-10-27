06964d8210a479ddd925ec503459462d17afd179
UltraHDR <108294295+UltraHDR@users.noreply.github.com>
2022-10-27 16:55:02 +0100

    zulufx: rebuild
    Update zulufx arm sha256
    
    Closes #134448.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/zulufx.rb
+++ Casks/zulufx.rb
@@ -3,7 +3,7 @@ cask "zulufx" do
   choice = on_arch_conditional arm: "arm", intel: "x86"
 
   version "17.0.5,17.38.21-ca"
-  sha256 arm:   "c118d74f223a57d14b4607cd59ea5dcd94c443f2a5fbbf17746931350324a755",
+  sha256 arm:   "477aa9577c8c623dc3f93b484bf14350e36eef4685d121165d792e73a04acf95",
          intel: "da720e89bbb6a5881d80f9fccfea4232dbfb51789ca5df3c16ab7e385282e1a1"
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-fx-jdk#{version.csv.first}-macosx_#{arch}.dmg",
