e591b90339138032434e01d7006d6ae132ca28c1
Patrick Linnane <patrick@linnane.io>
2022-10-27 14:00:28 -0700

    vpn-tracker-365 22.3.0,220308
    Update vpn-tracker-365 from 22.3.0,220302 to 22.3.0,220308
    
    Closes #134529.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/vpn-tracker-365.rb
+++ Casks/vpn-tracker-365.rb
@@ -1,7 +1,7 @@
 cask "vpn-tracker-365" do
   # NOTE: "365" is not a version number, but an intrinsic part of the product name
-  version "22.3.0,220302"
-  sha256 "6c83109bdf66e5e8a7b5efc3563d71ef150dd942c896e333702abad867d2f642"
+  version "22.3.0,220308"
+  sha256 "b20ed7b811117ea85c348d9219ec820cec3257a89658ad490b3e844f5796ed13"
 
   url "https://download.equinux.com/files/other/VPN%20Tracker%20365%20-%20#{version.csv.first}%20#{version.csv.second}.zip",
       verified: "download.equinux.com/files/other/"
