c085cb3de4377f1d3fd4c35c06116f8d0c19fcc8
Patrick Linnane <patrick@linnane.io>
2022-10-27 12:33:03 -0700

    Update 4k-stogram from 4.4.1 to 4.4.2

--- Casks/4k-stogram.rb
+++ Casks/4k-stogram.rb
@@ -1,6 +1,6 @@
 cask "4k-stogram" do
-  version "4.4.1"
-  sha256 "06586ece97e09ee1df9024a60817d952bc9fc695bc34c0abadb3c232360f6e57"
+  version "4.4.2"
+  sha256 "2226895ef2e3b69b53c83dad26dca9df0761fc63e7cf700d5dab80c3d6f8d6dd"
 
   url "https://dl.4kdownload.com/app/4kstogram_#{version}.dmg"
   name "4K Stogram"
