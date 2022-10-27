0606ee044247b0069e2bff46fd156ae9d24a4c47
Kapeli <support@kapeli.com>
2022-10-27 15:12:09 +0300

    dash: rebuild
    Update dash from 6.4.0,1004 to 6.4.0,1006
    
    Closes #134441.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/dash.rb
+++ Casks/dash.rb
@@ -3,8 +3,8 @@ cask "dash" do
     version "4.6.7"
     sha256 "e2b5eb996645b25f12ccae15e24b1b0d8007bc5fed925e14ce7be45a2b693fb6"
   else
-    version "6.4.0,1004"
-    sha256 "3968afe0245c7c5c4c8eeca4f7f0babf9e1ff550bef65d18758cfde7743df76e"
+    version "6.4.0,1006"
+    sha256 "165b17939f1501bfe6fe6e363a7246fbc276b51d1769f80e41828551d1cbd8ed"
   end
 
   url "https://kapeli.com/downloads/v#{version.major}/Dash.zip"
