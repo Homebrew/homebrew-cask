615abbc28a034d34bbdf286681f61335e2d1459b
Amory Meltzer <Amorymeltzer@gmail.com>
2022-10-26 21:01:29 -0400

    bettertouchtool 3.931,2096
    Update bettertouchtool from 3.930 to 3.931
    
    Closes #134434.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -1,6 +1,6 @@
 cask "bettertouchtool" do
-  version "3.930,2094"
-  sha256 "6356fad6c7351133767bca0b9f2212bb80dd77e57282d57e37235005b464347d"
+  version "3.931,2096"
+  sha256 "59a34b084e4196a81965bc3c3952a7a6302a74f3ca5957f02a61879fd2d2d632"
 
   url "https://folivora.ai/releases/btt#{version.csv.first}-#{version.csv.second}.zip"
   name "BetterTouchTool"
