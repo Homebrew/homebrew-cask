e6a96bf4e6caa1e0d56637ac4824cbcf5f28c760
Patrick Linnane <patrick@linnane.io>
2022-10-28 13:26:29 -0700

    rotki 1.26.0
    Update rotki from 1.25.3 to 1.26.0
    
    Closes #134619.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/rotki.rb
+++ Casks/rotki.rb
@@ -1,6 +1,6 @@
 cask "rotki" do
-  version "1.25.3"
-  sha256 "5c2474a953fae41b4bd383dcc5a039b1fb4c229aaeb24bbbebeaf9ffccbf26f0"
+  version "1.26.0"
+  sha256 "e58145d33b099f3a44d22204f9e309cb7e9955bfe63700a56f9164a675bfc64c"
 
   url "https://github.com/rotki/rotki/releases/download/v#{version}/rotki-darwin_x64-v#{version}.dmg",
       verified: "github.com/rotki/rotki/"
