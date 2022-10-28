d8cdddb009d613b86e3540bba5a276a140f20673
Patrick Linnane <patrick@linnane.io>
2022-10-27 20:07:36 -0700

    moonlight 4.3.0
    Update moonlight from 4.2.1 to 4.3.0
    
    Closes #134550.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/moonlight.rb
+++ Casks/moonlight.rb
@@ -1,6 +1,6 @@
 cask "moonlight" do
-  version "4.2.1"
-  sha256 "85d77081470d36c58e1f09bf53615a1740fc363201ed92407a9e63296467fdd8"
+  version "4.3.0"
+  sha256 "4f3cd19edbb22296a202b6eb5b5c3d8944018197a36670b4b11e3e725b7e149e"
 
   url "https://github.com/moonlight-stream/moonlight-qt/releases/download/v#{version}/Moonlight-#{version}.dmg",
       verified: "github.com/moonlight-stream/moonlight-qt/"
