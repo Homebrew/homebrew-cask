56bf4238f0019033fd99bd6d70ddbe465b7af0a8
Patrick Linnane <patrick@linnane.io>
2022-10-27 19:20:30 -0700

    Update aural from 3.11.0 to 3.12.0

--- Casks/aural.rb
+++ Casks/aural.rb
@@ -1,6 +1,6 @@
 cask "aural" do
-  version "3.11.0"
-  sha256 "3e6464b3cb7e644ae2df2200b16f23ef99e6ca5ef0371436dbb7a1c321a67814"
+  version "3.12.0"
+  sha256 "5e79afebccb1989f74cb4d9853cb9568b6abf0e6f182291a26c32d63f97a432a"
 
   url "https://github.com/maculateConception/aural-player/releases/download/v#{version}/AuralPlayer-#{version}.dmg"
   name "Aural Player"
