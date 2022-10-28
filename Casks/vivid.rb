7c80cbd8320ec9d0cfbbba40ac9b7c8ad29ee5bb
Patrick Linnane <patrick@linnane.io>
2022-10-28 13:32:11 -0700

    vivid 2.2.1,32
    Update vivid from 2.1 to 2.2.1
    
    Closes #134621.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/vivid.rb
+++ Casks/vivid.rb
@@ -1,6 +1,6 @@
 cask "vivid" do
-  version "2.1,27"
-  sha256 "9fb281f015c36fdb927a157854f79cc9a32e974dce3672e3ff74970aa796db2e"
+  version "2.2.1,32"
+  sha256 "b358e8ad80a433ae227f4bb635d38f0730badfc99402d60f634f9e7aeb20dd14"
 
   url "https://lumen-digital.com/apps/vivid/releases/Vivid#{version.csv.first}.zip",
       verified: "https://lumen-digital.com/apps/vivid"
