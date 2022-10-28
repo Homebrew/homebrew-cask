28d432c30e0d1998cffbd8aab87ceacf1be03675
Patrick Linnane <patrick@linnane.io>
2022-10-27 21:35:30 -0700

    rotki: rebuild
    Fix livecheck
    
    Closes #134561.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/rotki.rb
+++ Casks/rotki.rb
@@ -8,6 +8,11 @@ cask "rotki" do
   desc "Portfolio tracking and accounting tool"
   homepage "https://rotki.com/"
 
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
   app "rotki.app"
 
   zap trash: [
