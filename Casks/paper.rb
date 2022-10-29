28b7057e2ad38e2dfa3c62c5fc9b49281ceec0a2
Patrick Linnane <patrick@linnane.io>
2022-10-29 14:38:39 -0700

    Add verified

--- Casks/paper.rb
+++ Casks/paper.rb
@@ -2,7 +2,8 @@ cask "paper" do
   version "5.1.1"
   sha256 "419a19535da009533c661ec4da35df9a806558783131d458aed9fe6b22c7313b"
 
-  url "https://s3.nsns.in/dl/paper-v#{version}.dmg"
+  url "https://s3.nsns.in/dl/paper-v#{version}.dmg",
+      verified: "s3.nsns.in/"
   name "pap.er"
   desc "4K 5K HD Wallpaper Application"
   homepage "https://paper.photos/"
