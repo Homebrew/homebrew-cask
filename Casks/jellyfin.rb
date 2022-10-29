567cf4f2de3e36d8033a5861ff52ca4edb5d4c3d
mstgrv <31542657+mstgrv@users.noreply.github.com>
2022-10-29 04:10:31 -0700

    Update jellyfin from 10.8.5 to 10.8.6 (#134642)
    Co-authored-by: mstgrv <mstgrv@gmail.com>

--- Casks/jellyfin.rb
+++ Casks/jellyfin.rb
@@ -1,6 +1,6 @@
 cask "jellyfin" do
-  version "10.8.5"
-  sha256 "2d486580d01773bf71192d47d14726ccb51a1da55029fd6a44ef9c7ef09b88fa"
+  version "10.8.6"
+  sha256 "e9cfc8240059658e7154884685139234e3194da70bc9694a5d68b73aa81e0aec"
 
   url "https://repo.jellyfin.org/releases/server/macos/stable/installer/jellyfin_#{version}.dmg"
   name "Jellyfin"
