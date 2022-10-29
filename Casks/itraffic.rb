72d3b5491c46cd0ff9f0b02364b0af1d5ff60212
foam <foamzou@gmail.com>
2022-10-29 21:38:48 +0800

    itraffic: checksum update
    update sha hash for itraffic 0.1.5
    
    Closes #134645.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/itraffic.rb
+++ Casks/itraffic.rb
@@ -1,6 +1,6 @@
 cask "itraffic" do
   version "0.1.5"
-  sha256 "45e49c0bc3278d446a37fbe844e0f13eba215b7904a2eb950aaf7aba8d7bbba2"
+  sha256 "923ed6170edf3f1b08d8f7a6aa2af1102305b3dd2f6eba86e34f9aa4347a8f1b"
 
   url "https://github.com/foamzou/ITraffic-monitor-for-mac/releases/download/v#{version}/ITraffic-v#{version}.zip"
   name "itraffic"
