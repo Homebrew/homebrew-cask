0ce88b4b2230eaa3f9f5f5f10c69fb02a42db6d8
Patrick Linnane <patrick@linnane.io>
2022-10-28 18:35:47 -0700

    Switch to .dmg

--- Casks/extraterm.rb
+++ Casks/extraterm.rb
@@ -1,8 +1,8 @@
 cask "extraterm" do
   version "0.67.0"
-  sha256 "3ada5d7281fdb3f60a64ed99b4323e52fcffb135eed1672823e153db6a6c12cc"
+  sha256 "6968aa2f656257ae0af9973abd3b5bbc5274596ab7b2f47e15af6efcd07a6101"
 
-  url "https://github.com/sedwards2009/extraterm/releases/download/v#{version}/extratermqt-#{version}-macos-x64.zip",
+  url "https://github.com/sedwards2009/extraterm/releases/download/v#{version}/ExtratermQt_#{version}.dmg",
       verified: "github.com/sedwards2009/extraterm/"
   name "extraterm"
   desc "Swiss army chainsaw of terminal emulators"
@@ -13,7 +13,7 @@ cask "extraterm" do
     strategy :github_latest
   end
 
-  app "extratermqt-#{version}-macos-x64/ExtratermQt.app"
+  app "ExtratermQt.app"
 
   zap trash: [
     "~/Library/Application Support/extraterm",
