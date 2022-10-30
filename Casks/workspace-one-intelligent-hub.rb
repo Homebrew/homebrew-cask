f1c9a05745ebe8590dcb96538a9e1d0435d6644d
Vikalp Rusia <53312141+VikalpRusia@users.noreply.github.com>
2022-10-31 01:18:41 +0530

    Added new cask: Workspace One Intelligent Hub (#134675)
    * Add workspace One Intelligent Hub as an application
    
    New application added
    
    * Added rb
    
    * added launchctl
    
    * Update workspace-one-intelligent-hub.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/workspace-one-intelligent-hub.rb
@@ -0,0 +1,42 @@
+cask "workspace-one-intelligent-hub" do
+  version "22.10.0.7"
+  sha256 :no_check
+
+  url "https://packages.vmware.com/wsone/VMwareWorkspaceONEIntelligentHub.pkg",
+      verified: "packages.vmware.com/wsone/"
+  name "Workspace ONE Intelligent Hub"
+  desc "VMware workspace"
+  homepage "https://www.getwsone.com/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
+
+  depends_on macos: ">= :catalina"
+
+  pkg "VMwareWorkspaceONEIntelligentHub.pkg"
+
+  uninstall pkgutil:   [
+              "com.air-watch.pkg.OSXAgent",
+              "com.airwatch.munki.admin",
+              "com.airwatch.munki.core",
+              "com.airwatch.munki.python",
+            ],
+            launchctl: [
+              "com.airwatch.awcmd",
+              "com.airwatch.mac.agent",
+              "com.vmware.hub.flagd",
+              "com.vmware.hub.hubupdatedaemon",
+              "com.vmware.hubupdateagent",
+              "com.vmware.uem.hubd",
+              "com.vmware.uem.hublogd",
+            ]
+
+  zap trash: [
+    "~/Library/Application Support/AirWatchs",
+    "~/Library/Caches/com.vmware.hub.mac",
+    "~/Library/Preferences/com.vmware.hub.mac.plist",
+    "~/Library/Saved Application State/com.vmware.hub.mac.savedState",
+  ]
+end
