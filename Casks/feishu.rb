2e44a9bf01b6e9246adf07a01851b0ab6067914a
Patrick Linnane <patrick@linnane.io>
2022-10-31 10:14:55 -0700

    feishu 5.23.7,547c7bd7
    Update feishu from 5.22.8 to 5.23.7
    
    Closes #134738.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/feishu.rb
+++ Casks/feishu.rb
@@ -2,12 +2,12 @@ cask "feishu" do
   arch arm: "arm64", intel: "x64"
 
   on_intel do
-    version "5.22.8,492f6e2e"
-    sha256 "1bc7e61ff621b4a0a97b1885cdd83ef89695cbbf5018298ae29ba93a8f355a50"
+    version "5.23.7,547c7bd7"
+    sha256 "0071f1b2a287ad4b9b0a53440125a1c61ed84b29e005ec87ab655766dc6f01e5"
   end
   on_arm do
-    version "5.22.8,e1af37f2"
-    sha256 "58aecc7935341ff641943e99c4fa06755d4b838c05da05f7221f553d94af4d3e"
+    version "5.23.7,0bf1707f"
+    sha256 "080ce67e8e44aa73e987ea9142adec2dcd64de9a8850c40bb76970b2ab7e34b9"
   end
 
   url "https://sf3-cn.feishucdn.com/obj/ee-appcenter/#{version.csv.second}/Feishu-darwin_#{arch}-#{version.csv.first}-signed.dmg",
