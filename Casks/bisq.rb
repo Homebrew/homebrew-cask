5d4ec738d569979e015093155133d5e9c6ffcffd
Patrick Linnane <patrick@linnane.io>
2022-10-31 09:49:00 -0700

    Update bisq from 1.9.5 to 1.9.6

--- Casks/bisq.rb
+++ Casks/bisq.rb
@@ -1,6 +1,6 @@
 cask "bisq" do
-  version "1.9.5"
-  sha256 "e961ca62129799df3cee319847afaa51a7f24d28a81c3640a9f100d66587d84b"
+  version "1.9.6"
+  sha256 "4a0b31b4c372cf4a4979ec44303936d754e5edfcff8bd92a12f5660ef70afa7c"
 
   url "https://github.com/bisq-network/bisq/releases/download/v#{version}/Bisq-#{version}.dmg",
       verified: "github.com/bisq-network/bisq/"
