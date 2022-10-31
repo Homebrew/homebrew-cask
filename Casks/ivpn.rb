280ed8092c0daf31075e735cba58f22f2313be9d
Patrick Linnane <patrick@linnane.io>
2022-10-31 10:23:01 -0700

    Update ivpn from 3.9.32 to 3.9.43

--- Casks/ivpn.rb
+++ Casks/ivpn.rb
@@ -1,9 +1,9 @@
 cask "ivpn" do
   arch arm: "-arm64"
 
-  version "3.9.32"
-  sha256 arm:   "74a640f30c22a5197b26de7b2fbc4f2b21cdfe848ed7869ec13d6d94253bee35",
-         intel: "f2c5ff37ec33c427694ff8680f4e3567ae207ee24038eb7fe41faf7e05e1a417"
+  version "3.9.43"
+  sha256 arm:   "ad0d9f1eae0a42759df1f1115f8edd1c9176f8af46516d6d2ada2958409e1a1e",
+         intel: "0e167ddc6418338b4f163008ee418eb0a3d5d6c8ff487b065fd7429b3d286b19"
 
   url "https://repo.ivpn.net/macos/bin/IVPN-#{version}#{arch}.dmg"
   name "IVPN"
