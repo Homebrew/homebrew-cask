b62816dd04df699cf6104363b63258ad4b404cc9
Amory Meltzer <Amorymeltzer@gmail.com>
2022-10-31 12:12:40 -0400

    stellarium 1.1
    * Update stellarium from 1.0 to 1.1
    * Bump legacy sha256
    
    Closes #134725.
    
    Co-authored-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/stellarium.rb
+++ Casks/stellarium.rb
@@ -1,13 +1,13 @@
 cask "stellarium" do
-  version "1.0"
+  version "1.1"
 
   if MacOS.version <= :catalina
-    sha256 "92649cdc75b2135a62faaf85f4b1617c8c8c850f160c8191f627cd32e1147ec2"
+    sha256 "f3b6b4cbcb7322c6c5d3ff861ed3fde4d261eb03e7c8a197714874ee570a8769"
 
     url "https://github.com/Stellarium/stellarium/releases/download/v#{version.major_minor_patch}/Stellarium-#{version}-x86_64.zip",
         verified: "github.com/Stellarium/stellarium/"
   else
-    sha256 "59c7ca0541322194085fcb6290d9a7637154b290b2c2ca4d504cc1bd3e2aeb72"
+    sha256 "3225237eeb762bb45eea356df0b2054f2bbf1c34b1235b86ce3777a74916a2f6"
 
     url "https://github.com/Stellarium/stellarium/releases/download/v#{version.major_minor_patch}/Stellarium-#{version}-macOS.zip",
         verified: "github.com/Stellarium/stellarium/"
