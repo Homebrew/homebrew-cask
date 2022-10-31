0eac38f0a966e35259d4b270dcea0aefb74b1d02
Patrick Linnane <patrick@linnane.io>
2022-10-30 22:11:36 -0700

    Update smultron from 13.3.2 to 13.3.3

--- Casks/smultron.rb
+++ Casks/smultron.rb
@@ -1,5 +1,5 @@
 cask "smultron" do
-  version "13.3.2,13320"
+  version "13.3.3,13330"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.peterborgapps.com/downloads/Smultron#{version.major}.zip"
