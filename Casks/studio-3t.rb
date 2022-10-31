eaad3e6e91641029b56d779861da1389178d7dd8
Patrick Linnane <patrick@linnane.io>
2022-10-31 10:58:28 -0700

    studio-3t 2022.9.0
    Update studio-3t from 2022.8.3 to 2022.9.0
    
    Closes #134759.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/studio-3t.rb
+++ Casks/studio-3t.rb
@@ -2,9 +2,9 @@ cask "studio-3t" do
   arch arm: "-aarch64"
   livecheckarch = on_arch_conditional arm: "_aarch64"
 
-  version "2022.8.3"
-  sha256 arm:   "b0b3d925f900a73592604bc203212bfc46f7027337745e6ec7110b11271c2acd",
-         intel: "4c1c70b32a90ed62bf193f679c3a29e2d960e1372378ac6d0988aa0c9302de58"
+  version "2022.9.0"
+  sha256 arm:   "eaaa64fc05614b43f69d4f5a5f25e66a1024b20f2c4e164dcfa6730e5bcac544",
+         intel: "ea116004bcf34aae04d124c7b368115f2b686033a6dfd52bf6a808897729b5a4"
 
   url "https://download.studio3t.com/studio-3t/mac#{arch}/#{version}/Studio-3T.dmg"
   name "Studio 3T"
