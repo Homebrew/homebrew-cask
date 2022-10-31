2d16253bf3c60187efd58bb25ebbb4ef103fe443
Patrick Linnane <patrick@linnane.io>
2022-10-30 22:00:02 -0700

    Update panoply from 5.2.1 to 5.2.2

--- Casks/panoply.rb
+++ Casks/panoply.rb
@@ -1,6 +1,6 @@
 cask "panoply" do
-  version "5.2.1"
-  sha256 "f0b221785a1f42be32346be50c511464f7f3345ffe3715d5e33da5deb240933c"
+  version "5.2.2"
+  sha256 "fc0dd5d34da91ab2533e24ab662224ae43d96913da28bb8e1a60c28ed0f7a561"
 
   url "https://www.giss.nasa.gov/tools/panoply/download/PanoplyMacOS-#{version}.dmg"
   name "Panoply netCDF, HDF and GRIB Data Viewer"
