11b8a213b8d35e9c0a58703d0dae4c2a9806993d
Bevan Kay <email@bevankay.me>
2022-11-01 17:39:41 +1100

    Update google-earth-pro.rb

--- Casks/google-earth-pro.rb
+++ Casks/google-earth-pro.rb
@@ -1,6 +1,6 @@
 cask "google-earth-pro" do
   version "7.3.6.9275"
-  sha256 :no_check
+  sha256 :no_check # required as upstream package is updated in-place
 
   url "https://dl.google.com/dl/earth/client/advanced/current/googleearthpromac-intel-#{version.major_minor_patch}.dmg"
   name "Google Earth Pro"
