f1f24f3bbe1f8ccfb7f693a6d9fd60e3738d32d5
xxyzz <gitpull@protonmail.com>
2022-11-01 09:11:04 +0800

    Fix stellarium 1.1.1 download link (#134770)
    * Fix stellarium 1.1.1 download link
    
    Version 1.1.1 requires macOS 11.0+: https://github.com/Stellarium/stellarium/releases/tag/v1.1
    
    * Add livecheck block to stellarium cask
    
    * Update stellarium.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/stellarium.rb
+++ Casks/stellarium.rb
@@ -1,23 +1,19 @@
 cask "stellarium" do
-  version "1.1"
+  version "1.1.1"
+  sha256 "961a5c9a0eefd06457ebc0ebf24c45dee9d9e1a51ab8fa4c6931584225381578"
 
-  if MacOS.version <= :catalina
-    sha256 "f3b6b4cbcb7322c6c5d3ff861ed3fde4d261eb03e7c8a197714874ee570a8769"
-
-    url "https://github.com/Stellarium/stellarium/releases/download/v#{version.major_minor_patch}/Stellarium-#{version}-x86_64.zip",
-        verified: "github.com/Stellarium/stellarium/"
-  else
-    sha256 "3225237eeb762bb45eea356df0b2054f2bbf1c34b1235b86ce3777a74916a2f6"
-
-    url "https://github.com/Stellarium/stellarium/releases/download/v#{version.major_minor_patch}/Stellarium-#{version}-macOS.zip",
-        verified: "github.com/Stellarium/stellarium/"
-  end
-
+  url "https://github.com/Stellarium/stellarium/releases/download/v#{version.major_minor}/Stellarium-#{version}-macOS.zip",
+      verified: "github.com/Stellarium/stellarium/"
   name "Stellarium"
   desc "Tool to render realistic skies in real time on the screen"
   homepage "https://stellarium.org/"
 
-  depends_on macos: ">= :sierra"
+  livecheck do
+    url :homepage
+    regex(%r{href=.*?/Stellarium[._-]v?(\d+(?:\.\d+)*)[._-]macOS\.zip}i)
+  end
+
+  depends_on macos: ">= :big_sur"
 
   app "Stellarium.app"
 
