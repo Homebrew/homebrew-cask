fadd620d9c300d36668821219ff8ce2132f71754
Patrick Linnane <patrick@linnane.io>
2022-11-01 15:28:32 -0700

    manyverse 0.2211.1-beta
    Update manyverse from 0.2210.31-beta to 0.2211.1-beta
    
    Closes #134839.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/manyverse.rb
+++ Casks/manyverse.rb
@@ -1,6 +1,6 @@
 cask "manyverse" do
-  version "0.2210.31-beta"
-  sha256 "e79383bb3ab15ed1979dd7f3652ab984764787b0aca62dbabc65e80be43de742"
+  version "0.2211.1-beta"
+  sha256 "b4aa67e007e87afffdfab69d08db62c3339f2ef6ab0c8b18555b6d61dc24d67e"
 
   url "https://github.com/staltz/manyverse/releases/download/v#{version}/Manyverse-#{version}.dmg",
       verified: "github.com/staltz/manyverse/"
