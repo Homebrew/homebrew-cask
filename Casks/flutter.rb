4de50390882fc8fad9af8dc7c46accaaec589c33
Patrick Linnane <patrick@linnane.io>
2022-11-02 12:01:30 -0700

    Update flutter from 3.3.6 to 3.3.7

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -1,9 +1,9 @@
 cask "flutter" do
   arch arm: "_arm64_", intel: "_"
 
-  version "3.3.6"
-  sha256 arm:   "4bb7cd85c31d90a40de6adb1a2bfab6b8f2e753805bd103968b5fb1b7625fcd6",
-         intel: "32845432ca48ca0fcf41857fdae54c681ff6b7e2ded4353046daf0e7649e168d"
+  version "3.3.7"
+  sha256 arm:   "95172586e28c8241a6c7a593573ec89fe0c48d70882421394923d477fac9f0a7",
+         intel: "1bc4a9c4a8a89005157ff1ae17d2691c511b49cc1f0cf8b465bc1d0284d04d55"
 
   url "https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos#{arch}#{version}-stable.zip",
       verified: "storage.googleapis.com/flutter_infra_release/releases/stable/macos/"
