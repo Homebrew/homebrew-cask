69928ac46e0b2a406640072a8252ae7be82f1598
Patrick Linnane <patrick@linnane.io>
2022-11-02 10:17:08 -0700

    Update openshift-client from 4.11.12 to 4.11.13

--- Casks/openshift-client.rb
+++ Casks/openshift-client.rb
@@ -1,9 +1,9 @@
 cask "openshift-client" do
   arch arm: "-arm64"
 
-  version "4.11.12"
-  sha256 arm:   "c831dda8967870212bb0ceef491d261bdab7fb44418a2c484071f3fa993a05aa",
-         intel: "85b64e3238e5676cba65abeb3ec7ed467a64bdb91c606ac2f64bb5d153d7f27e"
+  version "4.11.13"
+  sha256 arm:   "a754ac4ef929fee9aa4753c7307fb503a82daf742c04031d78d199a613547ff3",
+         intel: "02172f9f2f0e95ce912e57879e8ba8be5cb5619c446b7f6ca356ca4ae221723c"
 
   url "https://mirror.openshift.com/pub/openshift-v#{version.major}/clients/ocp/#{version}/openshift-client-mac#{arch}.tar.gz"
   name "Openshift Client"
