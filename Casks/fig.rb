5062d5db39c083eda22959d71dc99372655b8a65
Grant G <grant0417@users.noreply.github.com>
2022-11-02 15:10:29 -0700

    Update fig from 1.0.62 to 2.7.2 (#134966)
    * Update fig from 1.0.62 to 2.7.2
    
    * fix issues

--- Casks/fig.rb
+++ Casks/fig.rb
@@ -1,20 +1,21 @@
 cask "fig" do
-  version "1.0.62,542"
-  sha256 "0c70a157cf16785e8175fce7efc1bb6a2e056a099b9da5c673a7904f4b205e07"
+  version "2.7.2"
+  sha256 "1bbd96e832f47d387d236a47c65e0530f5916509466d4bb0ca17599351562728"
 
-  url "https://versions.withfig.com/fig%20#{version.csv.second}.dmg",
-      verified: "versions.withfig.com/"
+  url "https://repo.fig.io/generic/stable/asset/#{version}/universal/fig.dmg"
   name "fig"
   desc "Reimagine your terminal"
   homepage "https://fig.io/"
 
   livecheck do
-    url "https://versions.withfig.com/appcast.xml"
-    strategy :sparkle
+    url "https://repo.fig.io/generic/stable/index.json"
+    strategy :page_match do |page|
+      JSON.parse(page)["hints"]["livecheck"]
+    end
   end
 
   auto_updates true
-  depends_on macos: ">= :high_sierra"
+  depends_on macos: ">= :catalina"
 
   app "Fig.app"
   binary "#{appdir}/Fig.app/Contents/MacOS/fig-darwin-universal", target: "fig"
@@ -33,7 +34,7 @@ cask "fig" do
             quit:
                        [
                          "com.mschrage.fig",
-                         "io.fig.input-method.cursor",
+                         "io.fig.cursor",
                        ]
 
   zap trash: [
