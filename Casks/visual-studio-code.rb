6cedca63ba8cd3aea5cd11f6c9859defdd799039
Patrick Linnane <patrick@linnane.io>
2022-11-02 11:23:37 -0700

    visual-studio-code 1.73.0
    Update visual-studio-code from 1.72.2 to 1.73.0
    
    Closes #134947.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -1,9 +1,9 @@
 cask "visual-studio-code" do
   arch arm: "darwin-arm64", intel: "darwin"
 
-  version "1.72.2"
-  sha256 arm:   "1603596646ba4d9230b157b2a1ce25455f2c03d3e866137de682bfbed4cf9ce7",
-         intel: "03f8542c504f3348106698bd31ddbcf05374f4b721c2fe9b602fef2ce4593349"
+  version "1.73.0"
+  sha256 arm:   "2f2f0e268d1d3a72b942cf7cfe0b33be9a50624065ae464cbcb43e78f2d619a2",
+         intel: "080352103c9734ccd38d6836328f6ba56f8ab120a90ab138f0036df00b94fd7a"
 
   url "https://update.code.visualstudio.com/#{version}/#{arch}/stable"
   name "Microsoft Visual Studio Code"
