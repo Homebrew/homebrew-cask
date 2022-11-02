5d98d9f85f99d432d72ec62536d613fa4a300f19
Patrick Linnane <patrick@linnane.io>
2022-11-02 12:13:34 -0700

    Bump min OS

--- Casks/fig.rb
+++ Casks/fig.rb
@@ -14,7 +14,7 @@ cask "fig" do
   end
 
   auto_updates true
-  depends_on macos: ">= :high_sierra"
+  depends_on macos: ">= :catalina"
 
   app "Fig.app"
   binary "#{appdir}/Fig.app/Contents/MacOS/fig-darwin-universal", target: "fig"
