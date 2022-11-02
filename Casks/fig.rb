c84b240b59a65254635b4a711987a2e718f8882b
Patrick Linnane <patrick@linnane.io>
2022-11-02 11:57:54 -0700

    Update fig from 1.0.62 to 2.7.2

--- Casks/fig.rb
+++ Casks/fig.rb
@@ -1,8 +1,8 @@
 cask "fig" do
-  version "1.0.62,542"
-  sha256 "0c70a157cf16785e8175fce7efc1bb6a2e056a099b9da5c673a7904f4b205e07"
+  version "2.7.2,20221102.173314"
+  sha256 "1bbd96e832f47d387d236a47c65e0530f5916509466d4bb0ca17599351562728"
 
-  url "https://versions.withfig.com/fig%20#{version.csv.second}.dmg",
+  url "https://versions.withfig.com/fig%20#{version.csv.first}.dmg",
       verified: "versions.withfig.com/"
   name "fig"
   desc "Reimagine your terminal"
