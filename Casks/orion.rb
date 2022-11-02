94cc1cac1983851c531e1a850e913647dc80b29a
Sean Molenaar <SMillerDev@users.noreply.github.com>
2022-11-02 09:43:55 +0100

    orion: depend on specific macOS versions

--- Casks/orion.rb
+++ Casks/orion.rb
@@ -3,18 +3,23 @@ cask "orion" do
 
   if MacOS.version <= :mojave
     macos_version_string = "10_14"
+    macos_dependency = ":mojave"
     sha256 "05b90e0c714e67575c929f824b80e5e8e3bad2b8b5916e7e67b184409a9a742f"
   elsif MacOS.version <= :catalina
     macos_version_string = "10_15"
+    macos_dependency = ":catalina"
     sha256 "97af577dbc1f1177220e7d0a31d199397af725905a59e4c75b3cf4dedc19f337"
   elsif MacOS.version <= :big_sur
     macos_version_string = "11_0"
+    macos_dependency = ":big_sur"
     sha256 "4a97641e1abd4eb85bc9efb015878596c0a21f089393f7371925c818f42f683a"
   elsif MacOS.version <= :monterey
     macos_version_string = "12_0"
+    macos_dependency = ":monterey"
     sha256 "7ca512efb144ef46c31b68b5b91ee5c784ebbf17f25a3812ed5f6d870c881ed1"
   else
     macos_version_string = "13_0"
+    macos_dependency = ":ventura"
     sha256 "8072eca95d4556aa4d21a5d017f05ea7e68d7782f44fb9cec15f3dac79f667b4"
   end
   url "https://browser.kagi.com/updates/#{macos_version_string}/#{version.csv.second}.zip"
@@ -28,7 +33,7 @@ cask "orion" do
   end
 
   auto_updates true
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= #{macos_dependency}"
 
   app "Orion.app"
 
