af42b30a001a6c6a63b9828a133e2ecee87d35ac
Patrick Linnane <patrick@linnane.io>
2022-11-02 12:32:53 -0700

    plex-htpc 1.28.1.3340,2cc1dfd6
    Update plex-htpc from 1.27.1.3311 to 1.28.1.3340
    
    Closes #134956.
    
    Signed-off-by: Markus Reiter <me@reitermark.us>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/plex-htpc.rb
+++ Casks/plex-htpc.rb
@@ -1,6 +1,6 @@
 cask "plex-htpc" do
-  version "1.27.1.3311,94cb0aa3"
-  sha256 "c5fd73a4c4510bca04eb45a90a962496ff1f2d28d455a5e8b14109fcb06c37cd"
+  version "1.28.1.3340,2cc1dfd6"
+  sha256 "e0a518e515f4d9391d6feb5566a36c68affac1867e0afdfa0776c3ba6f16b71e"
 
   url "https://downloads.plex.tv/htpc/#{version.csv.first}-#{version.csv.second}/macos/PlexHTPC-#{version.csv.first}-#{version.csv.second}-x86_64.zip"
   name "Plex HTPC"
