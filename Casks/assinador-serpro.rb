0d52a259f25ca8aa370d6702f639ad5d505e73ea
Patrick Linnane <patrick@linnane.io>
2022-11-01 21:26:58 -0700

    assinador-serpro 4.0.0
    Update assinador-serpro from 3.0.2 to 4.0.0
    
    Closes #134887.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/assinador-serpro.rb
+++ Casks/assinador-serpro.rb
@@ -1,8 +1,8 @@
 cask "assinador-serpro" do
-  version "3.0.2"
-  sha256 "c043ef01b9bbbad0ac31b74f54ca29ff659e4340faaa0b996bb895bfcc4f0f88"
+  version "4.0.0"
+  sha256 "d343c10e2b5179fafae7cb32893a11dd3bb99b6b1bce2eff83b26fff2965cc74"
 
-  url "https://www.serpro.gov.br/links-fixos-superiores/assinador-digital/assinador-serpro/arquivos/assinador-serpro-#{version.dots_to_hyphens}-instalador.dmg"
+  url "https://assinadorserpro.estaleiro.serpro.gov.br/downloads/AssinadorSerpro-#{version}.pkg"
   name "Assinador Serpro"
   desc "Validate and sign documents using digital certificates"
   homepage "https://www.serpro.gov.br/links-fixos-superiores/assinador-digital/assinador-serpro"
@@ -14,7 +14,9 @@ cask "assinador-serpro" do
 
   depends_on macos: ">= :sierra"
 
-  app "Assinador Serpro.app"
+  pkg "AssinadorSerpro-#{version}.pkg"
+
+  uninstall pkgutil: "br.gov.serpro.desktop.assinador"
 
   zap trash: "~/Library/Preferences/org.demoiselle.signer.serpro.desktop.Main.plist"
 end
