caf76bd079b42e1d005be89bb2f45216b3867d0d
Patrick Linnane <patrick@linnane.io>
2022-11-01 20:02:08 -0700

    orion 0.99,121.1

--- Casks/orion.rb
+++ Casks/orion.rb
@@ -1,21 +1,21 @@
 cask "orion" do
-  version "0.99,121"
+  version "0.99,121.1"
 
   if MacOS.version <= :mojave
     macos_version_string = "10_14"
-    sha256 ""
+    sha256 "05b90e0c714e67575c929f824b80e5e8e3bad2b8b5916e7e67b184409a9a742f"
   elsif MacOS.version <= :catalina
     macos_version_string = "10_15"
-    sha256 ""
+    sha256 "97af577dbc1f1177220e7d0a31d199397af725905a59e4c75b3cf4dedc19f337"
   elsif MacOS.version <= :big_sur
     macos_version_string = "11_0"
-    sha256 "25c7a674b5842dcfd6a94c69d386c2a670c7219289facb26efaf8927cdef8c63"
+    sha256 "4a97641e1abd4eb85bc9efb015878596c0a21f089393f7371925c818f42f683a"
   elsif MacOS.version <= :monterey
     macos_version_string = "12_0"
-    sha256 "dd178dbc6a5e3ecc497b109b7227273ceb14e1fd26c72d0960c012fe5785a036"
+    sha256 "7ca512efb144ef46c31b68b5b91ee5c784ebbf17f25a3812ed5f6d870c881ed1"
   else
     macos_version_string = "13_0"
-    sha256 "c14c6ba313330d36e996f6e62ca21753f020da28b658c5a1afb4302d2ec62a84"
+    sha256 "8072eca95d4556aa4d21a5d017f05ea7e68d7782f44fb9cec15f3dac79f667b4"
   end
   url "https://browser.kagi.com/updates/#{macos_version_string}/#{version.csv.second}.zip"
   name "Orion Browser"
