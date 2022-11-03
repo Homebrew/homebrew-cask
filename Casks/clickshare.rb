435118d5f5f4836ba201f828a7b780cc77c24ee1
Patrick Linnane <patrick@linnane.io>
2022-11-03 16:20:27 -0700

    Adjust livecheck & URL

--- Casks/clickshare.rb
+++ Casks/clickshare.rb
@@ -2,15 +2,14 @@ cask "clickshare" do
   version "4.25.0.15"
   sha256 "bf93b500f4df2357e65b15e81d0867b98a8c19551b1f126e951150236dc76287"
 
-  url "https://www.barco.com/services/website/en/TdeFiles/Download?FileNumber=R3306192&TdeType=3&MajorVersion=#{format("%02d", version.major)}&MinorVersion=#{format("%02d", version.minor)}&PatchVersion=#{format("%02d", version.patch)}&BuildVersion=#{format("%03d", version.split(".").last)}&ShowDownloadPage=False"
+  url "https://www.barco.com/services/website/en/TdeFiles/Download?FileNumber=R3306192&TdeType=3&MajorVersion=#{format("%02d", version.major)}&MinorVersion=#{format("%02d", version.minor)}&PatchVersion=#{format("%02d", version.patch)}&BuildVersion=#{format("%03d", version.split(".").last)}"
   name "ClickShare"
   desc "Wireless teleconferencing client"
   homepage "https://www.barco.com/en/clickshare/app"
 
   livecheck do
     url "https://www.barco.com/en/support/software/R3306192"
-    strategy :page_match
-    regex(/ClickShare Desktop App Mac v(\d+.\d+.\d+.\d+)/)
+    regex(/td>v?(\d+(?:\.\d+)+)</i)
   end
 
   container nested:	"ClickShare_Setup.dmg"
