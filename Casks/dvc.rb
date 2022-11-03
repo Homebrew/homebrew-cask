4870a26a86ff65e9a85c3d232f26b4f85f2db03d
ivaquero <46377665+ivaquero@users.noreply.github.com>
2022-11-03 17:46:55 +0800

    update dvc

--- Casks/dvc.rb
+++ Casks/dvc.rb
@@ -1,24 +1,24 @@
 cask "dvc" do
-    version "2.31.1"
-    sha256 "16d24e2983c70480da37fea817c68455a8500ac86ead36395fb043994f778775"
+  version "2.33.2"
+  sha256 "ceebcb6be244f6fdb5c6329ccd0ceb2cc25c727c6a4929f7d690716a708b90af"
 
-    url "https://github.com/iterative/dvc/releases/download/#{version}/dvc-#{version}.pkg"
-    name "dvc"
-    desc "Open-source Version Control System for Machine Learning Projects"
-    homepage "https://dvc.org/"
+  # github.com/iterative/dvc/ was verified as official when first introduced to the cask
+  url "https://github.com/iterative/dvc/releases/download/#{version}/dvc-#{version}.pkg"
+  name "dvc"
+  desc "Open-source Version Control System for Machine Learning Projects"
+  homepage "https://dvc.org/"
 
-    livecheck do
-        url :url
-        strategy :github_latest
-    end
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
-    depends_on macos: ">= :mojave"
+  depends_on macos: ">= :mojave"
 
-    pkg "dvc-#{version}.pkg"
+  pkg "dvc-#{version}.pkg"
 
-    zap trash: [
-        "~/Library/Application Support/dvc",
-        "~/Library/Application Support/iterative"
-    ]
-
+  zap trash: [
+    "~/Library/Application Support/dvc",
+    "~/Library/Application Support/iterative",
+  ]
 end
