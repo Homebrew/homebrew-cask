869eea03de8e33af1cb5a190de3058b18ddf7de1
Patrick Linnane <patrick@linnane.io>
2022-11-02 18:47:05 -0700

    Update ishowu-instant from 1.4.11 to 1.4.12

--- Casks/ishowu-instant.rb
+++ Casks/ishowu-instant.rb
@@ -1,6 +1,6 @@
 cask "ishowu-instant" do
-  version "1.4.11,1409"
-  sha256 "84feb760d709c728dd9f2da7f75d4e9fb84d34190f09f866155cdddd0fb39215"
+  version "1.4.12,1422"
+  sha256 "27210dba96fdc912f994bd960d345aeab1e5fc8a58dfacdb5e9a236da7c1513a"
 
   url "https://www.shinywhitebox.com/downloads/instant/iShowU_Instant_#{version.csv.first}.dmg"
   name "iShowU Instant"
