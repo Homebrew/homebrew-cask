924c49ceed0c8340549c918c98d59dab60bad10c
Patrick Linnane <patrick@linnane.io>
2022-11-03 09:54:08 -0700

    Update slite from 1.2.14 to 1.2.15

--- Casks/slite.rb
+++ Casks/slite.rb
@@ -1,9 +1,9 @@
 cask "slite" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.2.14"
-  sha256 arm:   "a74ad9345c3dba1874ec682280996be21b2c517824879a1f9fc0ee9896c940df",
-         intel: "8103be5d53d349cf5070ec98e74df8d01ded0f93e8c84b9d18b18c19c6210b76"
+  version "1.2.15"
+  sha256 arm:   "ddd8e1055bf61e719e5727483ef2007d3a90e6e62bbda02be34210aa99301441",
+         intel: "f6093e28f1005e9f3e04ed5ae32ecb56f76f071d559fefb6df483d61a8a187f8"
 
   url "https://download.todesktop.com/20062929x31pwfi/Slite%20#{version}-#{arch}-mac.zip",
       verified: "download.todesktop.com/20062929x31pwfi/"
