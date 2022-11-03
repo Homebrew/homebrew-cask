09b6e0008fec3feb7db054e206be9fbbb87cf495
Patrick Linnane <patrick@linnane.io>
2022-11-02 20:20:12 -0700

    Update plugdata from 0.6.1 to 0.6.2 (#134979)
    * Update plugdata from 0.6.1 to 0.6.2
    
    * Add to prerelease allowlist

--- Casks/plugdata.rb
+++ Casks/plugdata.rb
@@ -1,6 +1,6 @@
 cask "plugdata" do
-  version "0.6.1"
-  sha256 "b39f902d375d52b58714f3f8c5c07abef688668bf2608a03f5372dc27532b1d7"
+  version "0.6.2"
+  sha256 "3b18da8fa9fcd155efeb78e1f192acc6f9b74a62040e901d19aa34d6dda8218f"
 
   url "https://github.com/timothyschoen/PlugData/releases/download/v#{version}/PlugData-MacOS-Universal.zip"
   name "PlugData"
