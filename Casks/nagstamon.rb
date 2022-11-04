9a9a8ca792c2ce84aacc21492c30d313e0c34657
Patrick Linnane <patrick@linnane.io>
2022-11-04 08:35:04 -0700

    Update nagstamon from 3.10.0 to 3.10.1

--- Casks/nagstamon.rb
+++ Casks/nagstamon.rb
@@ -1,6 +1,6 @@
 cask "nagstamon" do
-  version "3.10.0"
-  sha256 "c70ba59f57d03467fa52b54fa4da885ef05b16c660f72a8c1dc24de6b49cf71e"
+  version "3.10.1"
+  sha256 "ea8b51977a1bf257ad2533963e34c78403e62fbde3c29cab490b7f7898c2bec2"
 
   url "https://github.com/HenriWahl/Nagstamon/releases/download/v#{version}/Nagstamon.#{version}.dmg",
       verified: "github.com/HenriWahl/Nagstamon/"
