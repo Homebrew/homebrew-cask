956cacbe671708834de6e81ce38dc5b26891518b
Patrick Linnane <patrick@linnane.io>
2022-11-04 08:00:12 -0700

    Update clash-for-windows from 0.20.6 to 0.20.7

--- Casks/clash-for-windows.rb
+++ Casks/clash-for-windows.rb
@@ -1,9 +1,9 @@
 cask "clash-for-windows" do
   arch arm: "-arm64"
 
-  version "0.20.6"
-  sha256 arm:   "190c3680e0bc456d7e95c2f0c1620801878c7bff7eb08e73440e8da02db86a32",
-         intel: "ea0c9bd9fac5607ee2477301d2a12697a7fd6ed76f9f2afba6507eea2ae69fc0"
+  version "0.20.7"
+  sha256 arm:   "b001bda48037ca59e7385a7845173b86af3768a868ddbf36d494d691339d10fd",
+         intel: "781d792c96b70c622ece27178d53ae008bf46ffc54cd317ce38757b4737d22e8"
 
   url "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/#{version}/Clash.for.Windows-#{version}#{arch}.dmg"
   name "Clash for Windows"
