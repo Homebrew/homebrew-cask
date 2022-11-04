2eed84ae5f1b7ee976302bd8eba7809a4c006647
Xi <psi.xie@gmail.com>
2022-11-04 21:03:43 +0800

    Update pap.er from 5.1.1 to 5.1.2 (#135068)
    * Update pap.er from 5.1.1 to 5.1.2
    
    * Update paper.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/paper.rb
+++ Casks/paper.rb
@@ -1,11 +1,11 @@
 cask "paper" do
-  version "5.1.1"
-  sha256 "419a19535da009533c661ec4da35df9a806558783131d458aed9fe6b22c7313b"
+  version "5.1.2"
+  sha256 "dbdb7bf4abba1b08d66bb3f90786cc9e9bd1296d7b99dd3e34cfdbdc51189364"
 
   url "https://s3.nsns.in/dl/paper-v#{version}.dmg",
       verified: "s3.nsns.in/"
   name "pap.er"
-  desc "4K 5K HD Wallpaper Application"
+  desc "Pap.er, 4K 5K HD Wallpaper Application"
   homepage "https://paper.photos/"
 
   livecheck do
@@ -13,6 +13,7 @@ cask "paper" do
     regex(/href=.*?paper[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
+  auto_updates true
   depends_on macos: ">= :big_sur"
 
   app "pap.er.app"
