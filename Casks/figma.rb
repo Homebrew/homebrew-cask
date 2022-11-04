d438b9bb7abede207c3883a50541bb7f2eb27116
Patrick Linnane <patrick@linnane.io>
2022-11-04 08:08:56 -0700

    figma 116.5.15
    Update figma from 116.4.4 to 116.5.15
    
    Closes #135080.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/figma.rb
+++ Casks/figma.rb
@@ -1,9 +1,9 @@
 cask "figma" do
   arch arm: "mac-arm", intel: "mac"
 
-  version "116.4.4"
-  sha256 arm:   "9cf4bfa0bc01c0c34f2bace18c540e7b93ca9fbdb93b089c47b2318169e0dfa1",
-         intel: "b03e5d7ac57e87e60661e3804bef18a1b03c2a8096ae8d3be965402316b943cc"
+  version "116.5.15"
+  sha256 arm:   "4b97c019d6edbe9d609beac2c070e667b6e8d73552a41001de705e209d954eac",
+         intel: "6e657feb071257a781be86ebbb8be1e6eb5d5b12d7a053476f456753caa05cf1"
 
   url "https://desktop.figma.com/#{arch}/Figma-#{version}.zip"
   name "Figma"
