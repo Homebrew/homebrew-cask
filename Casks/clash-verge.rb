104f7db5f46556b1c2e0444ad254485615048fe8
ivaquero <46377665+ivaquero@users.noreply.github.com>
2022-11-04 11:56:39 +0800

    brew style --fix

--- Casks/clash-verge.rb
+++ Casks/clash-verge.rb
@@ -2,8 +2,8 @@ cask "clash-verge" do
   arch arm: "aarch64", intel: "x64"
 
   version "1.1.2"
-  sha256 arm: "83009f585572b76c7992c5a24180145ebd5aedf8793092de53b142306c39acf8",
-      intel: "0e62740c3e64ddb920166018fc89f8fdd546bab8a4615b32f3bd3f9514976e5d"
+  sha256 arm:   "83009f585572b76c7992c5a24180145ebd5aedf8793092de53b142306c39acf8",
+         intel: "0e62740c3e64ddb920166018fc89f8fdd546bab8a4615b32f3bd3f9514976e5d"
 
   url "https://github.com/zzzgydi/clash-verge/releases/download/v#{version}/Clash.Verge_#{version}_#{arch}.dmg"
   name "Clash for Windows"
