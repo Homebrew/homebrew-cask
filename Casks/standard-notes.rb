ce44240c7d5dce249ce78cc0bf4e2b66a5d4adec
Patrick Linnane <patrick@linnane.io>
2022-11-04 09:36:04 -0700

    standard-notes 3.23.273
    Update standard-notes from 3.23.258 to 3.23.273
    
    Closes #135106.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -1,9 +1,9 @@
 cask "standard-notes" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.23.258"
-  sha256 arm:   "5961a184572a93063dbdc82f95851dfa126d2deb036789e517acb689b14b5144",
-         intel: "e206eab78786378cdb49cbd52cf3ed508c112897d2b465e5208362653f5355e7"
+  version "3.23.273"
+  sha256 arm:   "e93160b86d166265738ea4a8f17be6075c79f8a1e3bc53fbbf6943e4f50a95af",
+         intel: "8203386574048d4930b0a1f5d1d6e37a2a904fe02d7fa6b011d1f690deb959a0"
 
   url "https://github.com/standardnotes/app/releases/download/%40standardnotes%2Fdesktop%40#{version}/standard-notes-#{version}-mac-#{arch}.zip",
       verified: "github.com/standardnotes/app/"
