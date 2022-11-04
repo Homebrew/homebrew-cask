09a8cc9264a547b53e0c202968386adcfc751143
YeungKC <I@YeungKC.com>
2022-11-04 16:40:37 +0800

    Add Mixin Messenger Desktop

---
+++ Casks/mixin-messenger-desktop.rb
@@ -0,0 +1,14 @@
+cask "mixin-messenger-desktop" do
+  version "0.37.8"
+  sha256 "bf264864ce40d306e1b05b027bec26a40da15762231061b2541ad3c194861f6e"
+
+  url "https://github.com/MixinNetwork/flutter-app/releases/download/v#{version}/mixin_#{version}.dmg",
+      verified: "github.com/MixinNetwork/flutter-app/releases/download/"
+  name "Mixin Messenger Desktop"
+  name "Mixin"
+  homepage "https://github.com/MixinNetwork/flutter-app"
+
+  auto_updates true
+
+  app "Mixin.app"
+end
