73614502fb4f3c5ce4195a447c596bbcb6a86b5d
Patrick Linnane <patrick@linnane.io>
2022-11-04 09:29:29 -0700

    sabnzbd 3.7.0
    Update sabnzbd from 3.6.1 to 3.7.0
    
    Closes #135102.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/sabnzbd.rb
+++ Casks/sabnzbd.rb
@@ -1,6 +1,6 @@
 cask "sabnzbd" do
-  version "3.6.1"
-  sha256 "a0a62cb7fb04390c12403bdc38186fc6e24f39b5a0456de582b6006f36b0e27e"
+  version "3.7.0"
+  sha256 "d663b4eeccbcf8c9d50bacb4f5cd9a8d475371848fe1567dc0c594ca88e0a8e3"
 
   url "https://github.com/sabnzbd/sabnzbd/releases/download/#{version}/SABnzbd-#{version}-osx.dmg",
       verified: "github.com/sabnzbd/sabnzbd/"
