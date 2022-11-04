9fc01fac2d929f549eb336375feadf4ec5ecc97d
Miccal Matthews <miccal.matthews@gmail.com>
2022-11-04 19:46:57 +0800

    Update and rename mixin-messenger-desktop.rb to mixin.rb

--- Casks/mixin.rb
+++ Casks/mixin.rb
@@ -1,12 +1,11 @@
-cask "mixin-messenger-desktop" do
+cask "mixin" do
   version "0.37.8"
   sha256 "bf264864ce40d306e1b05b027bec26a40da15762231061b2541ad3c194861f6e"
 
   url "https://github.com/MixinNetwork/flutter-app/releases/download/v#{version}/mixin_#{version}.dmg",
       verified: "github.com/MixinNetwork/flutter-app/releases/download/"
   name "Mixin Messenger Desktop"
-  name "Mixin"
-  desc "💻 Mixin Messenger desktop app for macOS, iPadOS, Linux, and Windows powered by Flutter/Dart."
+  desc "Cryptocurrency wallet"
   homepage "https://github.com/MixinNetwork/flutter-app"
 
   auto_updates true
