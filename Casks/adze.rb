8b056d2be60725358ca05d28b4b823893aa86fcc
Pierre-Luc Paour <github@paour.com>
2022-11-04 08:44:54 +0100

    Update adze

---
+++ Casks/adze.rb
@@ -0,0 +1,11 @@
+cask "adze" do
+  version "1.4.12"
+  sha256 :no_check
+
+  url "https://getadze.com/static/main/releases/Adze.zip"
+  name "adze"
+  desc "Edit GPX Data Like a Champ"
+  homepage "https://getadze.com/"
+
+  app "Adze.app"
+end
