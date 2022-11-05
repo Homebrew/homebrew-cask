1e6d774a41f76b98994efff75da25308315b3f3d
Jaroslav Holub <jaroslav@automato.io>
2022-11-05 14:19:05 +0100

    Update binocs from 0.7.3 to 0.7.4

--- Casks/binocs.rb
+++ Casks/binocs.rb
@@ -1,9 +1,9 @@
 cask "binocs" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.7.3"
-  sha256 arm:   "b9847ab788db65227d60bda2b7e08786519b7ed5d3fd952ed1e5e0b62b7edc04",
-         intel: "ae61a9793f852eef713f4084b2f246941ec03ba5c4ae8809992d45816bd1e3c0"
+  version "0.7.4"
+  sha256 arm:   "32736551ea212c726f408fa790c7b205b7c0dfeb64412a274d4f2c39384acd2c",
+         intel: "5795d10fe5e6247e6875ec2b064d963079116719709c4f03c5ccd4fd9b6f22b0"
 
   url "https://download.binocs.sh/binocs_#{version}_darwin_#{arch}.gz"
   name "Binocs"
