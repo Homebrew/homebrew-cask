45caadfa44b0415eaa99925f0e2fb98e9fcfe8ac
Patrick Linnane <patrick@linnane.io>
2022-11-06 09:02:44 -0800

    Update brewtarget from 3.0.2 to 3.0.3

--- Casks/brewtarget.rb
+++ Casks/brewtarget.rb
@@ -1,6 +1,6 @@
 cask "brewtarget" do
-  version "3.0.2"
-  sha256 "5e36976e594ea60f6bb7450efb5e6d7e253e1f1693d2d8a2da108831c0191b39"
+  version "3.0.3"
+  sha256 "f450f050801da57b58107e7ae955235c50ac4cc00380792b34e25a5dce4602a2"
 
   url "https://github.com/Brewtarget/brewtarget/releases/download/v#{version}/brewtarget_#{version}_x86_64.dmg"
   name "brewtarget"
