42735aa2b15fe1b9142b54048850903c1877eaf9
Patrick Linnane <patrick@linnane.io>
2022-11-05 20:23:09 -0700

    mcreator 2022.3.45616
    Update mcreator from 2022.2.34517 to 2022.3.45616
    
    Closes #135170.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/mcreator.rb
+++ Casks/mcreator.rb
@@ -1,6 +1,6 @@
 cask "mcreator" do
-  version "2022.2.34517"
-  sha256 "fb88ca5a85fff0724ad18bc6b1045c162237993d08576269d9281eddb3dd37ff"
+  version "2022.3.45616"
+  sha256 "3b70e9c417ac9f10e18b782e2a4c846417c37fe926bf831278de7e6d69e9fcd3"
 
   url "https://github.com/MCreator/MCreator/releases/download/#{version}/MCreator.#{version.major_minor}.Mac.64bit.dmg",
       verified: "github.com/MCreator/MCreator/"
