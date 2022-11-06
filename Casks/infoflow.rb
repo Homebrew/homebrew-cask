3bfc4e38ab05aec3c3990d8a55388cae583993ae
Patrick Linnane <patrick@linnane.io>
2022-11-05 20:17:04 -0700

    infoflow 2.3.21.4,20221104203943
    Update infoflow from 2.3.21.3 to 2.3.21.4
    
    Closes #135166.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/infoflow.rb
+++ Casks/infoflow.rb
@@ -1,5 +1,5 @@
 cask "infoflow" do
-  version "2.3.21.3,20221031180523"
+  version "2.3.21.4,20221104203943"
   sha256 :no_check
 
   url "https://hi-static.cdn.bcebos.com/hi-official/infoflow_setup.dmg",
