d7acedc89622920c1a53d6cacee646216525cb87
Patrick Linnane <patrick@linnane.io>
2022-11-05 21:41:02 -0700

    Update sonic-robo-blast-2-kart from 1.5 to 1.6

--- Casks/sonic-robo-blast-2-kart.rb
+++ Casks/sonic-robo-blast-2-kart.rb
@@ -1,9 +1,9 @@
 cask "sonic-robo-blast-2-kart" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "1.5"
-  sha256 "6629e82d3d8970848a2c2ef2e7f73f70af40731ee6181b6eb21c6ba32fd30045"
+  version "1.6"
+  sha256 "d433b8b8662068b7726f31bfd6b372bd9a991019d7c88be6ffc9db380cf3ba9d"
 
-  url "https://github.com/STJr/Kart-Public/releases/download/v#{version}/SRB2Kart-#{version}-Darwin.dmg",
+  url "https://github.com/STJr/Kart-Public/releases/download/v#{version}/SRB2Kart-#{version}-macOS.dmg",
       verified: "github.com/STJr/Kart-Public/"
   name "Sonic Robo Blast 2 Kart"
   desc "Classic styled kart racer, complete with beautiful courses, and wacky items"
