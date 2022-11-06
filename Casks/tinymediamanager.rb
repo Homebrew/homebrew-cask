781e84d068913c8cfa9d72349569e85aad6729fb
Patrick Linnane <patrick@linnane.io>
2022-11-06 10:48:35 -0800

    Update tinymediamanager from 4.3.4 to 4.3.5

--- Casks/tinymediamanager.rb
+++ Casks/tinymediamanager.rb
@@ -1,6 +1,6 @@
 cask "tinymediamanager" do
-  version "4.3.4"
-  sha256 "6659ee38b0a7a30ca3620b4d1bd73cf51ddbb909acd3369eb8eed6311ee11ef4"
+  version "4.3.5"
+  sha256 "ce944f6f0f7f18195d3a233c0149d3d5cb57a9c408a44617a0be353bdc6be902"
 
   url "https://release.tinymediamanager.org/v#{version.major}/dist/tmm_#{version}_macos-x86_64.zip"
   name "tinyMediaManager"
