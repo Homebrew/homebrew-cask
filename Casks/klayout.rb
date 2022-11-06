b634d77d6adb6f4717cac52d00e5f6d609f40626
Patrick Linnane <patrick@linnane.io>
2022-11-05 22:12:54 -0700

    Fix livecheck

--- Casks/klayout.rb
+++ Casks/klayout.rb
@@ -2,29 +2,27 @@ cask "klayout" do
   version "0.27.11"
 
   if MacOS.version <= :catalina
+    macos_version_string = "Catalina"
     sha256 "372c53e6ec7f79cdf86612fa8d3a8a719a7d75eedc940e61b71bc15d22a53b26"
 
-    url "https://www.klayout.org/downloads/MacOS/HW-klayout-#{version}-macOS-Catalina-1-qt5Brew-RsysPhb38.dmg",
-        verified: "klayout.org/downloads/MacOS/"
   elsif MacOS.version <= :big_sur
+    macos_version_string = "BigSur"
     sha256 "ca8939d4838f59412fc446209df249dda47e4b576c49856e9657318145bfc290"
 
-    url "https://www.klayout.org/downloads/MacOS/HW-klayout-#{version}-macOS-BigSur-1-qt5Brew-RsysPhb38.dmg",
-        verified: "klayout.org/downloads/MacOS/"
   else
+    macos_version_string = "Monterey"
     sha256 "b08e25589576bbf056fdcaa1562129583cabd3e72778da0a1a3cc25c394b8add"
 
-    url "https://www.klayout.org/downloads/MacOS/HW-klayout-#{version}-macOS-Monterey-1-qt5Brew-RsysPhb38.dmg",
-        verified: "klayout.org/downloads/MacOS/"
   end
-
+  url "https://www.klayout.org/downloads/MacOS/HW-klayout-#{version}-macOS-#{macos_version_string}-1-qt5Brew-RsysPhb38.dmg",
+      verified: "klayout.org/downloads/MacOS/"
   name "KLayout"
   desc "IC design layout viewer and editor"
   homepage "https://www.klayout.de/"
 
   livecheck do
-    url "https://www.klayout.de/development.html"
-    regex(%r{<b>Version\s+(\d+(?:\.\d+)+)</b>}i)
+    url "https://www.klayout.de/build.html"
+    regex(/href=.*?HW[._-]klayout[._-](\d+(?:\.\d+)+)[._-]macOS[._-]#{macos_version_string}.*?\.dmg/i)
   end
 
   depends_on macos: ">= :catalina"
