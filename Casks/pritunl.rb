da82222d202e402b61d18a9248a8fe9c09ad029b
Patrick Linnane <patrick@linnane.io>
2022-11-07 08:48:06 -0800

    Update pritunl from 1.3.3329.81 to 1.3.3343.50

--- Casks/pritunl.rb
+++ Casks/pritunl.rb
@@ -1,9 +1,9 @@
 cask "pritunl" do
   arch arm: ".arm64"
 
-  version "1.3.3329.81"
-  sha256 arm:   "aca2a30ac2e0432c0413361fdb50106830caa0872038c8f653d4a93be2f374d8",
-         intel: "5387c2060bc09ba4cafb3702c4056dfe38f76dd3c270dd69751971278602d51e"
+  version "1.3.3343.50"
+  sha256 arm:   "62f104e1b9ea7b0dfbfd4f22705b94c7285705ee61b92744932a28e9b108438a",
+         intel: "a18d51b914c22b2c4b1dcaacf6ca2f7770d334a3ee49282173bb143b4f0e772a"
 
   url "https://github.com/pritunl/pritunl-client-electron/releases/download/#{version}/Pritunl#{arch}.pkg.zip",
       verified: "github.com/pritunl/pritunl-client-electron/"
