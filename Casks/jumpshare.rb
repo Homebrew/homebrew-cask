53ebe565a0289534af2fe9c9b5687e5fc4ab3348
Patrick Linnane <patrick@linnane.io>
2022-11-07 08:27:25 -0800

    Update jumpshare from 3.2.4 to 3.2.5

--- Casks/jumpshare.rb
+++ Casks/jumpshare.rb
@@ -1,6 +1,6 @@
 cask "jumpshare" do
-  version "3.2.4,138"
-  sha256 "6770c83ca24f3e3d255b5b1762b6971d3a155a49e17be9599b6a2aab3952184b"
+  version "3.2.5,140"
+  sha256 "eea0a8df44d40db846703fb4d4e11277f61d5c195bf02c079f479334ad159c15"
 
   url "https://d21hi1or3tbtjm.cloudfront.net/desktop/mac/updates/Jumpshare-#{version.csv.first}.tar.bz2",
       verified: "d21hi1or3tbtjm.cloudfront.net/"
