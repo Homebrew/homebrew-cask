660c01759610ff02e64f35c713a513907deb1c8c
Patrick Linnane <patrick@linnane.io>
2022-11-07 08:05:19 -0800

    bankid 7.13.1
    Update bankid from 7.13.0 to 7.13.1
    
    Closes #135242.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/bankid.rb
+++ Casks/bankid.rb
@@ -1,6 +1,6 @@
 cask "bankid" do
-  version "7.13.0"
-  sha256 "11027607372cd1d7b663510770970542be722a9409693222feeb67663ce0bf81"
+  version "7.13.1"
+  sha256 "9e7f4385ade152ec82d89f56adfa7e48a4625e3ff8cd6a80ee38d8e1649b9da2"
 
   url "https://cdn.bankid.com/bisp/mac/BankID_installation_#{version.dots_to_underscores}.pkg"
   name "BankID"
