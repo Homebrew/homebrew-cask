1670445aad7cd80c6322eb75e881e8420b03a123
Patrick Linnane <patrick@linnane.io>
2022-11-07 09:05:22 -0800

    Update swifty from 0.6.8 to 0.6.9

--- Casks/swifty.rb
+++ Casks/swifty.rb
@@ -1,6 +1,6 @@
 cask "swifty" do
-  version "0.6.8"
-  sha256 "54674ca56117ddd8a343c1383c516e530c0511a8a662b823efd4073d1617d722"
+  version "0.6.9"
+  sha256 "a7a4a2059d8b703b2a33277fabb35a12c4b460c8b5f2679fd4013636be822ad2"
 
   url "https://github.com/swiftyapp/swifty/releases/download/v#{version}/Swifty-#{version}.dmg",
       verified: "github.com/swiftyapp/swifty/"
