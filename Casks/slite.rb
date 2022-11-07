b020ad34e9d208b8dad3acf8aaad994215889aa2
Patrick Linnane <patrick@linnane.io>
2022-11-07 09:04:45 -0800

    slite 1.2.16
    Update slite from 1.2.15 to 1.2.16
    
    Closes #135281.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/slite.rb
+++ Casks/slite.rb
@@ -1,9 +1,9 @@
 cask "slite" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.2.15"
-  sha256 arm:   "ddd8e1055bf61e719e5727483ef2007d3a90e6e62bbda02be34210aa99301441",
-         intel: "f6093e28f1005e9f3e04ed5ae32ecb56f76f071d559fefb6df483d61a8a187f8"
+  version "1.2.16"
+  sha256 arm:   "47bb631c260b8e1a0ea99a4db9cb6e2ef4c5d25aec2b90b4759bcf06c5648a6b",
+         intel: "7de3fe3355bd657ba9513169f08526a4e8bb6306686f468d91de4ad4a2d0da62"
 
   url "https://download.todesktop.com/20062929x31pwfi/Slite%20#{version}-#{arch}-mac.zip",
       verified: "download.todesktop.com/20062929x31pwfi/"
