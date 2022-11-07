c7165d156752567274353eea4e2ddf9c00d813a6
Patrick Linnane <patrick@linnane.io>
2022-11-07 08:46:45 -0800

    Update portfolioperformance from 0.59.2 to 0.59.3

--- Casks/portfolioperformance.rb
+++ Casks/portfolioperformance.rb
@@ -1,9 +1,9 @@
 cask "portfolioperformance" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.59.2"
-  sha256 arm:   "188e28f33e501c73706e656b88e7412d2a8998504b260eb893915d46d26d80a7",
-         intel: "c678b50860be1bec1feb16d3aed6362141da184060f642266a35d83ff2e5fa96"
+  version "0.59.3"
+  sha256 arm:   "1047aee9c15fe5a49c19071a7d8c93751ac9ce8ea49f77fc1bd48c37c5dae45f",
+         intel: "7de8906e71e8548222bb8bce7f7585ed3ad89c90462a37aa91101a2b0e94c96f"
 
   url "https://github.com/buchen/portfolio/releases/download/#{version}/PortfolioPerformance-#{version}-#{arch}.dmg",
       verified: "github.com/buchen/portfolio/"
