3511720fb12b98c3d6e9c3c43daf2f1394cde988
eloyaldea <35566945+eloyaldea@users.noreply.github.com>
2022-11-07 12:19:06 +0100

    Update blobsaver from 3.2.1 to 3.3.0 (#135231)

--- Casks/blobsaver.rb
+++ Casks/blobsaver.rb
@@ -1,9 +1,9 @@
 cask "blobsaver" do
   arch arm: "arm", intel: "intel"
 
-  version "3.2.1"
-  sha256 arm:   "0e510dd7315af465a9f1f333e7f303e16871fa9964ae9c1632145d38b6e32e04",
-         intel: "341dad87e5a36f7f6c1f14050a4f44128afa2da5651abea761af59054d5029c8"
+  version "3.3.0"
+  sha256 arm:   "4ab23b9dee4f1a742d36f5a6c5d53ee1e04178d177240304363593a7e83e2998",
+         intel: "0a26862b1153ad81de80f4ba36157bb4c6202c8f31a2daf08c0038a4af2d2f41"
 
   url "https://github.com/airsquared/blobsaver/releases/download/v#{version}/blobsaver-#{arch}-#{version}.dmg"
   name "blobsaver"
