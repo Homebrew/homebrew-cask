10d9101ab6f4f4679a9b8c7767014c1a411affed
Patrick Linnane <patrick@linnane.io>
2022-11-07 16:49:20 -0800

    Update simply-fortran from 3.25.3757 to 3.26.3806 (#135125)
    * Update simply-fortran from 3.25.3757 to 3.26.3806
    
    * Split legacy version
    
    * Restructure by OS

--- Casks/simply-fortran.rb
+++ Casks/simply-fortran.rb
@@ -1,28 +1,31 @@
 cask "simply-fortran" do
   arch arm: "-arm64", intel: "-x86_64"
 
-  version "3.25.3757"
-  sha256 arm:   "de1c523a1c4a2d2be2623eaa89b117ddca4bbd7edacdda6830e930d2463a3208",
-         intel: "cd177191c7a96d59c0e9c3bb271808a2b471d1001fca17b85cb420aaf55ee05a"
+  if MacOS.version <= :big_sur
+    version "3.25.3757"
+    sha256 "0975462a1593bdc976dc87d6a9da88b97d8268a1a97caebeefd359e56ef73195"
 
-  url "https://download.simplyfortran.com/#{version.major_minor}/macos/simplyfortran-#{version}#{arch}.dmg"
+    url "https://download.simplyfortran.com/#{version.major_minor}/macos/simplyfortran-#{version}-x86_64.legacy.dmg"
 
-  on_intel do
-    on_big_sur :or_older do
-      sha256 "0975462a1593bdc976dc87d6a9da88b97d8268a1a97caebeefd359e56ef73195"
+    livecheck do
+      skip "Legacy version"
+    end
+  else
+    version "3.26.3806"
+    sha256 arm:   "886dd1432bf070be8c4ba87e1585962ff3ca480b93797674c7886bf557c8867b",
+           intel: "95f4f0fdd2926e7cc5f0b81538ceb35c31f01cd65f72d5705ebcb0e8ef1f2bbd"
 
-      url "https://download.simplyfortran.com/#{version.major_minor}/macos/simplyfortran-#{version}-x86_64.legacy.dmg"
+    url "https://download.simplyfortran.com/#{version.major_minor}/macos/simplyfortran-#{version}#{arch}.dmg"
+
+    livecheck do
+      url "https://simplyfortran.com/download/?platform=macos"
+      regex(/href=.*?simplyfortran[._-]v?(\d+(?:\.\d+)+)#{arch}\.dmg/i)
     end
   end
 
   name "Simply Fortran"
   desc "Fortran development environment"
   homepage "https://simplyfortran.com/"
-
-  livecheck do
-    url "https://simplyfortran.com/download/?platform=macos"
-    regex(/href=.*?simplyfortran[._-]v?(\d+(?:\.\d+)+)#{arch}\.dmg/i)
-  end
 
   app "Simply Fortran.app"
 end
