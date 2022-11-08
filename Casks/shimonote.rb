55ec692858fd2e7604e083bcb0d44064c30a5127
Patrick Linnane <patrick@linnane.io>
2022-11-07 21:22:13 -0800

    shimonote 3.1.1,6d78b4a
    Update shimonote from 3.0.0 to 3.1.1
    
    Closes #135334.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/shimonote.rb
+++ Casks/shimonote.rb
@@ -1,9 +1,9 @@
 cask "shimonote" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.0.0,3fb3ddc0"
-  sha256 arm:   "7b74ece090b0344b52935d920c9276fc8f58078f8992cf4041c4aaf838c0c3b3",
-         intel: "54349b88722dce605cc8e6f254dc4b97822d8d21dce05b27458ea427a30ba769"
+  version "3.1.1,6d78b4a"
+  sha256 arm:   "ec741ed9d38b2df97925c4ea001c4d71a00dc5a5fd58b60cc1fd256fc36fa6d8",
+         intel: "13686d576d2fffc6438a803949f8f954613ef5723f0d198308b99b8ac4bdf9ac"
 
   url "https://as.smvm.cn/panther/shimo/release/darwin/#{arch}/\%e7\%9f\%b3\%e5\%a2\%a8\%e6\%96\%87\%e6\%a1\%a3_v#{version.csv.first}-release.#{version.csv.second}.shimo_darwin-#{arch}.zip",
       verified: "as.smvm.cn/panther/shimo/release/darwin/"
