343147918e479e58816b83f0e1ee7fee416e113b
Nils Streedain <tannins_berets_0@icloud.com>
2022-11-08 14:28:37 -0800

    Update balenaetcher.rb

--- Casks/balenaetcher.rb
+++ Casks/balenaetcher.rb
@@ -1,6 +1,6 @@
 cask "balenaetcher" do
-  version "1.8.14"
-  sha256 "4c3ca69f01e72c0c0e677cb1572bb887cc3cc146cfbe1aaf198cbab2266ed68d"
+  version "1.9.0"
+  sha256 "6cf590a8b76d81c0a34ead94134f06adce1c24e9f99e3091a521b6c4efc9e3b7"
 
   url "https://github.com/balena-io/etcher/releases/download/v#{version}/balenaEtcher-#{version}.dmg",
       verified: "github.com/balena-io/etcher/"
