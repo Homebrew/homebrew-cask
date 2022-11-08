e6677193193cec0a48806562927384a30f8bcf4f
Patrick Linnane <patrick@linnane.io>
2022-11-08 11:06:52 -0800

    compositor 1.20.4
    Update compositor from 1.20.3 to 1.20.4
    
    Closes #135362.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/compositor.rb
+++ Casks/compositor.rb
@@ -1,6 +1,6 @@
 cask "compositor" do
-  version "1.20.3"
-  sha256 "3f6ff8d2111445fcf255f83525a321b2d47613c41ca25e984d8f5c2a39a22ddc"
+  version "1.20.4"
+  sha256 "730fba8d2ed0cd32908a5ca201d3bcc0bb5a5289e65e522bff19d33fdef4c0b4"
 
   url "https://compositorapp.com/updates/Compositor_#{version}.zip"
   name "Compositor"
