acf0d116520ebe88e965ff4730083a75b398e42b
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-11-08 23:17:28 +0800

    Update cnkiexpress from 0.2.29 to 0.2.30

--- Casks/cnkiexpress.rb
+++ Casks/cnkiexpress.rb
@@ -1,6 +1,6 @@
 cask "cnkiexpress" do
-  version "0.2.29"
-  sha256 "eeff7f77f250420c77ae7abd28bbd8d0e7e6663ae9235ff9dce1c8387610e137"
+  version "0.2.30"
+  sha256 "e320cddf929df5b4fb059de99185f5517fc9c9cfe5f07ea8eb86b66b60b13cb0"
 
   url "https://download.cnki.net/cnkiexpress/\%E5\%85\%A8\%E7\%90\%83\%E5\%AD\%A6\%E6\%9C\%AF\%E5\%BF\%AB\%E6\%8A\%A5-#{version}.dmg"
   name "CNKIExpress"
