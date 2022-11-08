a754388ea32060428e7bd0ea8d742facee4d1164
Miccal Matthews <miccal.matthews@gmail.com>
2022-11-08 10:28:42 +0800

    duet: add version for Big Sur and earlier (#135306)
    Update duet.rb

--- Casks/duet.rb
+++ Casks/duet.rb
@@ -1,19 +1,33 @@
 cask "duet" do
-  version "3.0.1.0"
-  sha256 "d3aab0a50f4408d47aa2d3c7036965d45c18982f8c80cce5ef8d1ad86904238d"
+  if MacOS.version <= :big_sur
+    version "2.4.6.0"
+    sha256 "771bbb7fa08f38270eb0efacc553faf4bf10f558101bdf18cab3fd909cbd871b"
+    url "https://duet.nyc3.cdn.digitaloceanspaces.com/Mac/#{version.major_minor.dots_to_underscores}/duet-#{version.dots_to_hyphens}.zip",
+        verified: "duet.nyc3.cdn.digitaloceanspaces.com/Mac/"
 
-  url "https://duet.nyc3.cdn.digitaloceanspaces.com/Mac/#{version.major_minor}/duet-#{version.dots_to_hyphens}.zip",
-      verified: "duet.nyc3.cdn.digitaloceanspaces.com/Mac/"
-  name "Duet"
-  desc "Tool for using an iPad as a second display"
-  homepage "https://www.duetdisplay.com/"
+    livecheck do
+      url "https://updates.duetdisplay.com/latestMac"
+      strategy :header_match do |headers|
+        headers["location"][/duet[._-]v?(\d+(?:-\d+)+)\.zip/i, 1].tr("-", ".")
+      end
+    end
+  else
+    version "3.0.1.0"
+    sha256 "d3aab0a50f4408d47aa2d3c7036965d45c18982f8c80cce5ef8d1ad86904238d"
+    url "https://duet.nyc3.cdn.digitaloceanspaces.com/Mac/#{version.major_minor}/duet-#{version.dots_to_hyphens}.zip",
+        verified: "duet.nyc3.cdn.digitaloceanspaces.com/Mac/"
 
-  livecheck do
-    url "https://updates.duetdisplay.com/AppleSilicon"
-    strategy :header_match do |headers|
-      headers["location"][/duet[._-]v?(\d+(?:-\d+)+)\.zip/i, 1].tr("-", ".")
+    livecheck do
+      url "https://updates.duetdisplay.com/AppleSilicon"
+      strategy :header_match do |headers|
+        headers["location"][/duet[._-]v?(\d+(?:-\d+)+)\.zip/i, 1].tr("-", ".")
+      end
     end
   end
+
+  name "Duet"
+  desc "Remote desktop and second display tool"
+  homepage "https://www.duetdisplay.com/"
 
   auto_updates true
 
