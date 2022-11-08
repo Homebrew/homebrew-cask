11282d8a5e0988a0ae09303344a5edfcfc505c0b
Tin H Kyaw <tinhkyaw@users.noreply.github.com>
2022-11-08 02:55:45 -0800

    Update funter from 5.4.3 to 6.0.1 (#135337)
    * Update funter from 5.4.3 to 6.0.1
    
    * Update funter.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/funter.rb
+++ Casks/funter.rb
@@ -1,22 +1,18 @@
 cask "funter" do
-  if MacOS.version <= :sierra
-    version "4.1b64"
-    sha256 "b059000717f7f8af67ee027d81aea6c2d9f343e0eb72e439a9f286ab265b7b93"
-    url "https://nektony.com/download/funter/funter_10.12.dmg"
-  else
-    version "5.4.3,218"
-    sha256 :no_check
-    url "https://nektony.com/download/funter/Funter.dmg"
-  end
+  version "6.0.1,323"
+  sha256 :no_check
 
+  url "https://download.nektony.com/download/funter/Funter.dmg"
   name "Funter"
   desc "Shows hidden files and folders and switches their visibility in Finder"
   homepage "https://nektony.com/products/funter"
 
   livecheck do
-    url "https://nektony.com/pro-support/funter/update/update.xml"
+    url "https://download.nektony.com/pro-support/v3/funter/update/update.xml"
     strategy :sparkle
   end
+
+  depends_on macos: ">= :el_capitan"
 
   app "Funter #{version.major}.app"
 end
