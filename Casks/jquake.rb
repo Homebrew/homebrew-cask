d3fc19c18f5d8ded3c87c1daec41b2374921dee9
Patrick Linnane <patrick@linnane.io>
2022-11-07 20:38:14 -0800

    jquake 1.8.2
    Update jquake from 1.8.1 to 1.8.2
    
    Closes #135318.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/jquake.rb
+++ Casks/jquake.rb
@@ -1,9 +1,9 @@
 cask "jquake" do
   arch arm: "arm64", intel: "intel"
 
-  version "1.8.1"
-  sha256 arm:   "52a044d7110794cfec518444ad550b56a0328786e0af09629f319c52e3088c31",
-         intel: "d981c632467d4cddcff386643557e86972d6d577d38eaaee9b2f219037614ab6"
+  version "1.8.2"
+  sha256 arm:   "366f855034508c7aab6d7f95acb27eb08fbe57c1839913cb7099d39f50a7e3c6",
+         intel: "a36be6dddd8cc4ffa9154bdd6a2ef2d964d0edeec80ca90f0e093e3c0ca18498"
 
   url "https://fleneindre.github.io/downloads/JQuake_#{version}_mac_#{arch}.dmg",
       verified: "fleneindre.github.io/downloads/"
