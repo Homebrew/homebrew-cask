86cc8e49f10f8277e51a6c399c64985ba216dba8
Miccal Matthews <miccal.matthews@gmail.com>
2022-11-08 09:52:02 +0800

    duet 3.0.1.0 (#135302)
    * Update duet.rb
    
    * Update duet.rb

--- Casks/duet.rb
+++ Casks/duet.rb
@@ -1,15 +1,15 @@
 cask "duet" do
-  version "2.4.6.0"
-  sha256 "771bbb7fa08f38270eb0efacc553faf4bf10f558101bdf18cab3fd909cbd871b"
+  version "3.0.1.0"
+  sha256 "d3aab0a50f4408d47aa2d3c7036965d45c18982f8c80cce5ef8d1ad86904238d"
 
-  url "https://duet.nyc3.cdn.digitaloceanspaces.com/Mac/#{version.major_minor.dots_to_underscores}/duet-#{version.dots_to_hyphens}.zip",
+  url "https://duet.nyc3.cdn.digitaloceanspaces.com/Mac/#{version.major_minor}/duet-#{version.dots_to_hyphens}.zip",
       verified: "duet.nyc3.cdn.digitaloceanspaces.com/Mac/"
   name "Duet"
   desc "Tool for using an iPad as a second display"
   homepage "https://www.duetdisplay.com/"
 
   livecheck do
-    url "https://updates.duetdisplay.com/latestMac"
+    url "https://updates.duetdisplay.com/AppleSilicon"
     strategy :header_match do |headers|
       headers["location"][/duet[._-]v?(\d+(?:-\d+)+)\.zip/i, 1].tr("-", ".")
     end
