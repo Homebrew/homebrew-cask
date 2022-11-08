a02647f2b8aa4fba80452848b0664eb2735af886
Patrick Linnane <patrick@linnane.io>
2022-11-08 11:58:06 -0800

    lunar 5.8.0
    Update lunar from 5.7.9 to 5.8.0
    
    Closes #135380.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/lunar.rb
+++ Casks/lunar.rb
@@ -1,6 +1,6 @@
 cask "lunar" do
-  version "5.7.9"
-  sha256 "c69523c92b97ac785be523240facfa3b2208a375db5bd4f7ff82e60eb48b7c82"
+  version "5.8.0"
+  sha256 "81d8d8a09a71b1cd51fdb307c0c89f137b7893cf3359d0bb4a6d1fda71ea765e"
 
   url "https://static.lunar.fyi/releases/Lunar-#{version}.dmg"
   name "Lunar"
