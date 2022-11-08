525678e92752559aaed826c69f0e56f3c306b806
Patrick Linnane <patrick@linnane.io>
2022-11-08 11:02:15 -0800

    Update browserstacklocal from 3.5.4 to 3.5.5

--- Casks/browserstacklocal.rb
+++ Casks/browserstacklocal.rb
@@ -1,5 +1,5 @@
 cask "browserstacklocal" do
-  version "3.5.4"
+  version "3.5.5"
   sha256 :no_check
 
   url "https://www.browserstack.com/local-testing/downloads/native-app/BrowserStackLocal.dmg"
