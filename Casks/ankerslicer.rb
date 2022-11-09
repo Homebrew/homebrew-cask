d1d2e710cf17f360523d87f89115bbfd77db52a0
monkey-debugger <64543651+monkey-debugger@users.noreply.github.com>
2022-11-09 16:58:28 +0100

    replace conditional statements with variable
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>

--- Casks/ankerslicer.rb
+++ Casks/ankerslicer.rb
@@ -1,15 +1,10 @@
 cask "ankerslicer" do
+  arch intel: "Intel", arm: "M1"
   version "2.4.3"
   sha256 :no_check
 
-  on_intel do
-    url "https://public-make-moat-us.s3.us-east-2.amazonaws.com/ankermake/slicer/AnkerMake_Installer_Mac_Intel.dmg",
+  url "https://public-make-moat-us.s3.us-east-2.amazonaws.com/ankermake/slicer/AnkerMake_Installer_Mac_#{arch}.dmg",
         verified: "public-make-moat-us.s3.us-east-2.amazonaws.com/ankermake/slicer/"
-  end
-  on_arm do
-    url "https://public-make-moat-us.s3.us-east-2.amazonaws.com/ankermake/slicer/AnkerMake_Installer_Mac_M1.dmg",
-        verified: "public-make-moat-us.s3.us-east-2.amazonaws.com/ankermake/slicer"
-  end
 
   name "ankerslicer"
   desc "Slicer for AnkerMake 3D printers"
