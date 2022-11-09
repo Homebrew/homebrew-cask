fcef386e5d99e780cc661ceb8d7f0467bffce0ac
Patrick Linnane <patrick@linnane.io>
2022-11-09 07:47:39 -0800

    badlion-client 3.12.2
    Update badlion-client from 3.12.1 to 3.12.2
    
    Closes #135435.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/badlion-client.rb
+++ Casks/badlion-client.rb
@@ -1,6 +1,6 @@
 cask "badlion-client" do
-  version "3.12.1"
-  sha256 "5d606ca7378eadcdd607b8a6ef83aa83b6ba2c3482e331814d396cab04e15611"
+  version "3.12.2"
+  sha256 "2e8cb4fb78ec1f72e192600c354ad9bc3be2e2a98f86b0389289f7752c695f79"
 
   url "https://client-updates.badlion.net/Badlion%20Client-#{version}.dmg"
   name "Badlion Client"
