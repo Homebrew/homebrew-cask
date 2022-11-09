13e9f56094f617c0e267e382e9a175f649d9c122
monkey-debugger <64543651+monkey-debugger@users.noreply.github.com>
2022-11-09 17:11:43 +0100

    Corrected AnkerSlicer version to v0.7.5

--- Casks/ankerslicer.rb
+++ Casks/ankerslicer.rb
@@ -1,6 +1,6 @@
 cask "ankerslicer" do
   arch intel: "Intel", arm: "M1"
-  version "2.4.3"
+  version "0.7.5"
   sha256 :no_check
 
   url "https://public-make-moat-us.s3.us-east-2.amazonaws.com/ankermake/slicer/AnkerMake_Installer_Mac_#{arch}.dmg",
