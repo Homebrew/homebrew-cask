c1fc4a03cabc1347b5336600182fd393ba3bc088
Patrick Linnane <patrick@linnane.io>
2022-11-09 07:53:42 -0800

    daedalus-mainnet 5.1.1,22358
    Update daedalus-mainnet from 5.1.0 to 5.1.1
    
    Closes #135443.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/daedalus-mainnet.rb
+++ Casks/daedalus-mainnet.rb
@@ -1,6 +1,6 @@
 cask "daedalus-mainnet" do
-  version "5.1.0,22239"
-  sha256 "9c61eb7149106bf9ccc314ec6bb08cace557aeb347a0e76af9406b34f13a00b0"
+  version "5.1.1,22358"
+  sha256 "2f2ade6cb1f944751e1637782e38888cee13fb67ef76b134dcb453aa1cfb8a0d"
 
   url "https://update-cardano-mainnet.iohk.io/daedalus-#{version.csv.first}-mainnet-#{version.csv.second}-x86_64-darwin.pkg",
       verified: "update-cardano-mainnet.iohk.io/"
