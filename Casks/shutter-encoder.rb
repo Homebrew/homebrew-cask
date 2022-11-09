06319e25f668177c64b38e1cbfc2f2ef7733faef
Patrick Linnane <patrick@linnane.io>
2022-11-09 08:15:14 -0800

    shutter-encoder 16.5
    Update shutter-encoder from 16.4 to 16.5
    
    Closes #135465.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/shutter-encoder.rb
+++ Casks/shutter-encoder.rb
@@ -1,9 +1,9 @@
 cask "shutter-encoder" do
   arch arm: "Apple Silicon", intel: "Mac 64bits"
 
-  version "16.4"
-  sha256 arm:   "76aae44ad3da31a2a333f4d4fe6f0e0bf3c6ad59dcf04595498524230b5479b7",
-         intel: "f62a4ebde07dd6495037c41d96c6b75f0eb31c91b5e48e1e06c945cd7f33f974"
+  version "16.5"
+  sha256 arm:   "9790a3dc1101a93bd83762919dc6f0f792c7d621d0f2798bc3529ed8a8d19a6e",
+         intel: "097679733bbba128101f57c91dcfe9286910a8bf3559a98aec4c6d445d0470fd"
 
   url "https://www.shutterencoder.com/Shutter%20Encoder%20#{version}%20#{arch.gsub(" ", "%20")}.pkg"
   name "Shutter Encoder"
