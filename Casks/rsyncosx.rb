8499d9e078654bcfa68b5aa418609bfeabdd506e
Patrick Linnane <patrick@linnane.io>
2022-11-10 10:06:44 -0800

    rsyncosx: rebuild
    Add livecheck
    
    Closes #135563.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/rsyncosx.rb
+++ Casks/rsyncosx.rb
@@ -7,6 +7,11 @@ cask "rsyncosx" do
   desc "GUI for rsync"
   homepage "https://github.com/rsyncOSX/RsyncOSX"
 
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
   depends_on macos: ">= :big_sur"
 
   app "RsyncOSX.app"
