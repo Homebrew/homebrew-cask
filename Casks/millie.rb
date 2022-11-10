341d067204a942887628171a9a3d88b6bbffe942
Patrick Linnane <patrick@linnane.io>
2022-11-10 09:39:23 -0800

    Update millie from 2.44.2 to 2.45.2

--- Casks/millie.rb
+++ Casks/millie.rb
@@ -1,6 +1,6 @@
 cask "millie" do
-  version "2.44.2"
-  sha256 "2ef15394fd1826ed75ac937873707ced50ef1cdeabedda53ff80b60e4911a65c"
+  version "2.45.2"
+  sha256 "6e75fcde485543143ccd636f6e9c6f1669ad142e828f36c22519bdd5ca98bcb2"
 
   url "https://apis.millie.co.kr/v1/download/installer/mac/Millie-#{version}.dmg"
   name "millie"
