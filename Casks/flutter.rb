f7e64f51412e14f1df9577c99abb2658763264c1
Patrick Linnane <patrick@linnane.io>
2022-11-10 09:09:52 -0800

    flutter 3.3.8
    Update flutter from 3.3.7 to 3.3.8
    
    Closes #135534.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -1,9 +1,9 @@
 cask "flutter" do
   arch arm: "_arm64_", intel: "_"
 
-  version "3.3.7"
-  sha256 arm:   "95172586e28c8241a6c7a593573ec89fe0c48d70882421394923d477fac9f0a7",
-         intel: "1bc4a9c4a8a89005157ff1ae17d2691c511b49cc1f0cf8b465bc1d0284d04d55"
+  version "3.3.8"
+  sha256 arm:   "c49d66a68bd8e2dea1737cf58f72828e113f242ce4f28f268917baf30ea60ea9",
+         intel: "2d97b90933da42d332f343dab97a982ce5317e904ff0fce3ca1ebe12c7d19501"
 
   url "https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos#{arch}#{version}-stable.zip",
       verified: "storage.googleapis.com/flutter_infra_release/releases/stable/macos/"
