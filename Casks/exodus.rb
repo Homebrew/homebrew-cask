930dce636bf2aff9fcb4efe6b6b6489538f83be3
Patrick Linnane <patrick@linnane.io>
2022-11-10 09:06:16 -0800

    Update exodus from 22.11.4 to 22.11.9

--- Casks/exodus.rb
+++ Casks/exodus.rb
@@ -1,9 +1,9 @@
 cask "exodus" do
   arch arm: "macos-arm64", intel: "macos"
 
-  version "22.11.4"
-  sha256 arm:   "a89d39e9c5a49a5d90a7f1e1ec57b8c384e6cc290a84a9b4db099909dce03875",
-         intel: "bbde4e2f0820a5ab7454c179cbc35d50d99680e7ddcd5cf6df1dc3c8e72c9aa4"
+  version "22.11.9"
+  sha256 arm:   "4a8809947ba5e905ec4adff5aa0a2b4a7676d27af65d68e1c89820746f6cb3d3",
+         intel: "b46eeaf27f87c549c9ed1faf8a5781f848f32c269307e33fa08518e102eebf81"
 
   url "https://downloads.exodus.com/releases/exodus-#{arch}-#{version}.dmg"
   name "Exodus"
