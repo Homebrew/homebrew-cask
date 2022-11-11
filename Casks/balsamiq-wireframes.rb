88aefca1582b4e8e3244fcd608818700143f88ba
Patrick Linnane <patrick@linnane.io>
2022-11-11 14:56:12 -0800

    balsamiq-wireframes 4.6.3
    Update balsamiq-wireframes from 4.6.2 to 4.6.3
    
    Closes #135610.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/balsamiq-wireframes.rb
+++ Casks/balsamiq-wireframes.rb
@@ -1,6 +1,6 @@
 cask "balsamiq-wireframes" do
-  version "4.6.2"
-  sha256 "ecc4f6921efcd9e919c2f88aa0e6205ab698480a19279060d2c12536eedfd022"
+  version "4.6.3"
+  sha256 "2748f03e008c95252b93f08ea6bc179d0f824d1912cbabab0ea73aecb8f16896"
 
   url "https://builds.balsamiq.com/bwd/Balsamiq%20Wireframes%20#{version}.dmg"
   name "Balsamiq Wireframes"
