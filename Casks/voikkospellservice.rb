40fe721a057a4ce2daa9416a37c2f53cfab422b8
Marko Wallin <marko.wallin@iki.fi>
2022-11-11 14:23:39 +0200

    VoikkoSpellService 1.1.0b1

--- Casks/voikkospellservice.rb
+++ Casks/voikkospellservice.rb
@@ -1,15 +1,19 @@
 cask "voikkospellservice" do
-  version "1.0.6b1"
-  sha256 "b94df1e56bab0895831723d369a6e99c03202e99a6db5fe62625af9b4fa17973"
+  version "1.1.0b1"
+  sha256 "c1dec3cda95ed77de2e6246e5e16b4425f40c576a2409d272ea85fb2bc22f285"
 
-  url "https://verteksi.net/files/osxspell/VoikkoSpellService-#{version}.dmg"
+  url "https://github.com/walokra/osxspell/releases/download/#{version}/VoikkoSpellService-#{version}.dmg", verified: "github.com/walokra/osxspell/"
   name "VoikkoSpellService"
+  desc "Spell-checking service for Finnish"
   homepage "https://verteksi.net/lab/osxspell/"
 
   livecheck do
-    url :homepage
-    regex(%r{href=.*?/VoikkoSpellService-(\d+(?:\.\d+)*b\d+)\.dmg}i)
+    url :url
+    strategy :github_latest
+    regex(%r{href=.*?/tag/(\d+(?:\.\d+.\S{0,2}[^"])+)["' >]}i)
   end
+
+  depends_on macos: ">= :big_sur"
 
   service "VoikkoSpellService.app"
 
