c630ea3a652aa7e7550020361e0b5c02b684e7e4
Bevan Kay <email@bevankay.me>
2022-11-12 10:13:02 +1100

    sequel-ace 4.0.3,20042
    Update sequel-ace from 4.0.1 to 4.0.3
    
    Closes #135640.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/sequel-ace.rb
+++ Casks/sequel-ace.rb
@@ -1,6 +1,6 @@
 cask "sequel-ace" do
-  version "4.0.1,20039"
-  sha256 "550e3639c2eb59bf01b0555398dd80b7919f6b711ae3e6474424a2f8be8bcfbd"
+  version "4.0.3,20042"
+  sha256 "e58875dd6819c173c55fb6266317816498f50a87c35c47e80c26e5c1690f4142"
 
   url "https://github.com/Sequel-Ace/Sequel-Ace/releases/download/production/#{version.csv.first}-#{version.csv.second}/Sequel-Ace-#{version.csv.first}.zip"
   name "Sequel Ace"
