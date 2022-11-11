27842c9c035e04fce3bd0fa9b54ef3be1ee16580
Yusuf Kursad Kaya <ykursadkaya@gmail.com>
2022-11-11 08:43:52 +0300

    lens 2022.11.101953
    Update lens from 2022.11.92045 to 2022.11.101953
    
    Closes #135588.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/lens.rb
+++ Casks/lens.rb
@@ -1,9 +1,9 @@
 cask "lens" do
   arch arm: "-arm64"
 
-  version "2022.11.92045"
-  sha256 arm:   "a65c8c7cb76705d59223221527439cbb1761bbc88804d4694b32ffb9f8acfc1e",
-         intel: "9f3efc3b843b062b6c509d6ce70c84560ca6d159c4beafb221d4aad729229de2"
+  version "2022.11.101953"
+  sha256 arm:   "653854c3764c38baf6e12a470137e06d9583c3ad9cfa85e6b63e8528f7298b55",
+         intel: "f7007cc313f0c394d4943520c2d4848bfaa2a3ce4ee37a76196d9d95000fe97d"
 
   url "https://api.k8slens.dev/binaries/Lens-#{version}-latest#{arch}.dmg"
   name "Lens"
