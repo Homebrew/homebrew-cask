fdf104a1743af89514547c2ab8a1f0f5e55fc442
Kreeblah <kreeblah@gmail.com>
2022-11-11 14:31:52 -0800

    after-dark-classic: Update to 1.2 (#135592)
    * after-dark-classic: Update to 1.2
    
    * Update after-dark-classic.rb
    
    * Update after-dark-classic.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/after-dark-classic.rb
+++ Casks/after-dark-classic.rb
@@ -1,22 +1,31 @@
 cask "after-dark-classic" do
-  version "1.0"
-  sha256 :no_check
+  version "1.2"
+  sha256 "714762326b7887d8136af2e563d3a1d883f56a242db7ca5db62ea251601db89d"
 
-  url "https://www.infinisys.co.jp/archive/online/online_afterdarkclassicset_e.dmg.gz"
+  url "https://www.infinisys.co.jp/archive/online/online_afterdark#{version.no_dots}e.dmg"
   name "After Dark Classic Set"
+  desc "Classic After Dark screensaver set"
   homepage "https://en.infinisys.co.jp/product/afterdarkclassicset/index.shtml"
 
-  pkg "ClassicSet.pkg"
+  livecheck do
+    url :homepage
+    regex(/After\sDark\sClassic\sSet\s(\d+(?:\.\d+)+)/i)
+  end
+
+  pkg "AfterDark.pkg"
 
   uninstall pkgutil: [
-    "jp.co.infinisys.flyingToastersmowingmanAndBoris.Boris.pkg",
-    "jp.co.infinisys.flyingToastersmowingmanAndBoris.FlyingToasters.pkg",
-    "jp.co.infinisys.flyingToastersmowingmanAndBoris.MowingMan.pkg",
+    "jp.co.infinisys.afterdark.boris11.pkg",
+    "jp.co.infinisys.afterdark.flyingtoasters12.pkg",
+    "jp.co.infinisys.afterdark.mowingman11.pkg",
   ]
 
   zap trash: [
-    "~/Library/Preferences/ByHost/jp.co.infinisys.boris.*.plist",
-    "~/Library/Preferences/ByHost/jp.co.infinisys.flyingtoasters.*.plist",
-    "~/Library/Preferences/ByHost/jp.co.infinisys.mowingman.*.plist",
+    "~/Library/Containers/com.apple.ScreenSaver.Engine.legacyScreenSaver/" \
+    "Data/Library/Preferences/ByHost/jp.co.infinisys.boris11.*.plist",
+    "~/Library/Containers/com.apple.ScreenSaver.Engine.legacyScreenSaver/" \
+    "Data/Library/Preferences/ByHost/jp.co.infinisys.flyingscreensaver12.*.plist",
+    "~/Library/Containers/com.apple.ScreenSaver.Engine.legacyScreenSaver/" \
+    "Data/Library/Preferences/ByHost/jp.co.infinisys.mowingman11.*.plist",
   ]
 end
