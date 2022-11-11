f47085bd8187b7a22c30faba1d9921a354e8b69e
Patrick Linnane <patrick@linnane.io>
2022-11-11 15:17:25 -0800

    smartgit 22.1.0
    Update smartgit from 21.2.4 to 22.1.0
    
    Closes #135650.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/smartgit.rb
+++ Casks/smartgit.rb
@@ -1,9 +1,9 @@
 cask "smartgit" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "21.2.4"
-  sha256 arm:   "abbcaa70bf8c75815bb47d33091f2431380ffc0a15ebd701876f1656aff8c149",
-         intel: "eb89ce8c455553f1504c8be12bbbbdf8af2e6dcbceb73455a1cc967aad6f61c0"
+  version "22.1.0"
+  sha256 arm:   "9ea2c33fde1193ca45f15aee0e9dd342493ded5efb02421a4aaa432847de1d11",
+         intel: "a6bbaaf0b89a2723c68c601203d7a317e23a50a2d3f311d742fc5f32679c4af2"
 
   url "https://www.syntevo.com/downloads/smartgit/smartgit-#{arch}-#{version.dots_to_underscores}.dmg"
 
