e7cacf848bcc2aae2f632182414941dbd8055d46
Patrick Linnane <patrick@linnane.io>
2022-11-11 15:22:06 -0800

    weakauras-companion 4.1.0
    Update weakauras-companion from 4.0.9 to 4.1.0
    
    Closes #135659.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/weakauras-companion.rb
+++ Casks/weakauras-companion.rb
@@ -1,6 +1,6 @@
 cask "weakauras-companion" do
-  version "4.0.9"
-  sha256 "e241e3ab8bbd645ec9a69734a3ef64f4ac484d066c8e3be263b4ec9a8cde2739"
+  version "4.1.0"
+  sha256 "123dd57277e34f80decf7dab95b6df8fb4901b32edbdabd83f05cd3a59da7b2a"
 
   url "https://github.com/WeakAuras/WeakAuras-Companion/releases/download/v#{version}/WeakAuras-Companion-#{version}-mac-universal.dmg",
       verified: "github.com/WeakAuras/WeakAuras-Companion/"
