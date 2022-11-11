326d46fbc3595258aa17e30c3c4a00780cb99259
Branch Vincent <branchevincent@gmail.com>
2022-11-11 09:45:23 -0500

    openlens 6.1.19
    Update openlens from 6.1.18 to 6.1.19
    
    Closes #135600.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/openlens.rb
+++ Casks/openlens.rb
@@ -1,9 +1,9 @@
 cask "openlens" do
   arch arm: "-arm64"
 
-  version "6.1.18"
-  sha256 arm:   "4201023c98a295b55ad721974037ea5972a03ed1841b048685a081164751bbbf",
-         intel: "2e25893479e6910d06ef51808092d3a32a6025918cc3f7687ea2b2f3342eea08"
+  version "6.1.19"
+  sha256 arm:   "c2c0e376e058732bc538a9ddadfaf35087058b3c454ab0324c32fcdccc6a61f3",
+         intel: "61ed06b2fd2a8f7dcdc4cc7a883167d56cc1806bacaf305f61f1a607db6307d6"
 
   url "https://github.com/MuhammedKalkan/OpenLens/releases/download/v#{version}/OpenLens-#{version}#{arch}.dmg"
   name "OpenLens"
