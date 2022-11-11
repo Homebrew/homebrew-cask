047f8c3f33fc566ca719f547a9c9c375f27c087d
Patrick Linnane <patrick@linnane.io>
2022-11-11 08:47:58 -0800

    Switch livecheck strat

--- Casks/voikkospellservice.rb
+++ Casks/voikkospellservice.rb
@@ -2,16 +2,11 @@ cask "voikkospellservice" do
   version "1.1.0b1"
   sha256 "c1dec3cda95ed77de2e6246e5e16b4425f40c576a2409d272ea85fb2bc22f285"
 
-  url "https://github.com/walokra/osxspell/releases/download/#{version}/VoikkoSpellService-#{version}.dmg", verified: "github.com/walokra/osxspell/"
+  url "https://github.com/walokra/osxspell/releases/download/#{version}/VoikkoSpellService-#{version}.dmg",
+      verified: "github.com/walokra/osxspell/"
   name "VoikkoSpellService"
   desc "Spell-checking service for Finnish"
   homepage "https://verteksi.net/lab/osxspell/"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-    regex(%r{href=.*?/tag/(\d+(?:\.\d+.\S{0,2}[^"])+)["' >]}i)
-  end
 
   depends_on macos: ">= :big_sur"
 
