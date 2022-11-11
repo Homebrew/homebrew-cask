e94003fc7773c65c393ed6bf1e380fe9424440c1
Patrick Linnane <patrick@linnane.io>
2022-11-11 15:10:06 -0800

    mem 0.40.1
    Update mem from 0.40.0 to 0.40.1
    
    Closes #135633.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/mem.rb
+++ Casks/mem.rb
@@ -1,6 +1,6 @@
 cask "mem" do
-  version "0.40.0"
-  sha256 "9f490af04d8dab6e33249e3704eb69f4e2d341e5c8a707b08ec2b5efa14da885"
+  version "0.40.1"
+  sha256 "7ec0400c2a0dae76d6d3705dc5095f39d17b7cc7d17490874b94aa104e1a333b"
 
   url "https://storage.googleapis.com/mem-desktop-stable-releases/Mem-#{version}.dmg",
       verified: "storage.googleapis.com/mem-desktop-stable-releases/"
