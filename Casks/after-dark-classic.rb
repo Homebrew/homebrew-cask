2ee118d9d76a17538997cafb6a8de2d70f9d9070
Miccal Matthews <miccal.matthews@gmail.com>
2022-11-12 06:20:35 +0800

    Update after-dark-classic.rb

--- Casks/after-dark-classic.rb
+++ Casks/after-dark-classic.rb
@@ -1,11 +1,16 @@
 cask "after-dark-classic" do
   version "1.2"
-  sha256 :no_check
+  sha256 "714762326b7887d8136af2e563d3a1d883f56a242db7ca5db62ea251601db89d"
 
-  url "https://www.infinisys.co.jp/archive/online/online_afterdark12e.dmg"
+  url "https://www.infinisys.co.jp/archive/online/online_afterdark#{version.no_dots}e.dmg"
   name "After Dark Classic Set"
   desc "Classic After Dark screensaver set"
   homepage "https://en.infinisys.co.jp/product/afterdarkclassicset/index.shtml"
+
+  livecheck do
+    url :homepage
+    regex(/After\sDark\sClassic\sSet\s(\d+(?:\.\d+)+)/i)
+  end
 
   pkg "AfterDark.pkg"
 
