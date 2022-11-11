39b11422a357a847d297484a7d40ee1610758e3e
Patrick Linnane <patrick@linnane.io>
2022-11-11 15:20:04 -0800

    Update vrew from 0.12.10 to 0.12.11

--- Casks/vrew.rb
+++ Casks/vrew.rb
@@ -1,6 +1,6 @@
 cask "vrew" do
-  version "0.12.10"
-  sha256 "c141ee712821d2642bc77f3e8e1ddd99802071fe56544db7ba8e32ca2ed5c8c9"
+  version "0.12.11"
+  sha256 "4ef8a235a980c005cc546c352a31eb7c2899879a4ca22492a3803b06d0911827"
 
   url "https://vrew-files.voyagerx.com/Vrew-#{version}.dmg"
   name "vrew"
