77358b66aaec3d3210919bb43efa05436a07cfb1
Patrick Linnane <patrick@linnane.io>
2022-11-11 17:25:38 -0800

    Update assinador-serpro from 4.0.0 to 4.0.2

--- Casks/assinador-serpro.rb
+++ Casks/assinador-serpro.rb
@@ -1,8 +1,8 @@
 cask "assinador-serpro" do
-  version "4.0.0"
-  sha256 "d343c10e2b5179fafae7cb32893a11dd3bb99b6b1bce2eff83b26fff2965cc74"
+  version "4.0.2"
+  sha256 "4ed47e37da89b5fe80568d14222407de9711f87d8e6c8a9505b590cd8d69af8a"
 
-  url "https://assinadorserpro.estaleiro.serpro.gov.br/downloads/AssinadorSerpro-#{version}.pkg"
+  url "https://www.serpro.gov.br/links-fixos-superiores/assinador-digital/assinador-serpro/arquivos/AssinadorSerpro#{version}.mpkg.zip"
   name "Assinador Serpro"
   desc "Validate and sign documents using digital certificates"
   homepage "https://www.serpro.gov.br/links-fixos-superiores/assinador-digital/assinador-serpro"
@@ -14,7 +14,7 @@ cask "assinador-serpro" do
 
   depends_on macos: ">= :sierra"
 
-  pkg "AssinadorSerpro-#{version}.pkg"
+  pkg "AssinadorSerpro-#{version}.mpkg"
 
   uninstall pkgutil: "br.gov.serpro.desktop.assinador"
 
