339e63542911ace4e44a03233464c7a62952d451
Patrick Linnane <patrick@linnane.io>
2022-11-12 21:09:58 -0800

    Update plexamp from 4.5.2 to 4.5.3 (#135693)

--- Casks/plexamp.rb
+++ Casks/plexamp.rb
@@ -1,11 +1,11 @@
 cask "plexamp" do
-  arch arm: "-arm64"
+  arch arm: "arm64", intel: "x64"
 
-  version "4.5.2"
-  sha256 arm:   "2ab606c6ff4802e627ba9005d6e97777c6d196119f9e1284046af1be47cb5d0f",
-         intel: "de2612598ef282a78befaf8c4adb5961634fa279bb2db1d61c1513e0321e74cd"
+  version "4.5.3"
+  sha256 arm:   "36f1842ec760d554dbd7a3a6a94e1a00c5f9996df028860b0cea25c022e98949",
+         intel: "f2d49d1cc01aa20d19a15a5a6e5fa6709fa12ca1d9f7abb75bf9508714cbbec6"
 
-  url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}#{arch}.dmg",
+  url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}-#{arch}.dmg",
       verified: "plexamp.plex.tv/"
   name "Plexamp"
   desc "Music player focusing on visuals"
