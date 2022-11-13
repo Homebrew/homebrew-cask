9b6b6edfa5426f63b0bc06077214e505bb33b9fe
Miccal Matthews <miccal.matthews@gmail.com>
2022-11-14 06:54:33 +0800

    Update openmtp.rb

--- Casks/openmtp.rb
+++ Casks/openmtp.rb
@@ -5,7 +5,6 @@ cask "openmtp" do
   sha256 arm:   "7a6df0bca6c60dcf8653c5875372e1ff5f249cced2cc52ce6e04cee9f838d2cb",
          intel: "90c8412b9bf20b7f7bea08dea84e8c35ea8059847ca41d058d3131b75b81a831"
 
-
   url "https://github.com/ganeshrvel/openmtp/releases/download/v#{version}/openmtp-#{version}-mac-#{arch}.zip",
       verified: "github.com/ganeshrvel/openmtp/"
   name "OpenMTP"
