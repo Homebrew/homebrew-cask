99c08bc8fce8cdf3ed49413bfb5ceaaef9ce7324
Ganesh Rathinavel Medayil <ganeshrvel@outlook.com>
2022-11-14 04:34:12 +0530

    openmtp 3.2.0 added (#135719)
    * openmtp 3.2.0 added
    
    * Update openmtp.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/openmtp.rb
+++ Casks/openmtp.rb
@@ -1,8 +1,11 @@
 cask "openmtp" do
-  version "3.1.15"
-  sha256 "9e1002759ab0b8b0c82799c6eae951e53a0dcb5410c0276226fff7cd35d58ef7"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://github.com/ganeshrvel/openmtp/releases/download/v#{version}/OpenMTP-#{version}-mac.zip",
+  version "3.2.0"
+  sha256 arm:   "7a6df0bca6c60dcf8653c5875372e1ff5f249cced2cc52ce6e04cee9f838d2cb",
+         intel: "90c8412b9bf20b7f7bea08dea84e8c35ea8059847ca41d058d3131b75b81a831"
+
+  url "https://github.com/ganeshrvel/openmtp/releases/download/v#{version}/openmtp-#{version}-mac-#{arch}.zip",
       verified: "github.com/ganeshrvel/openmtp/"
   name "OpenMTP"
   desc "Android file transfer"
