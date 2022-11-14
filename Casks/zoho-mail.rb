897c1a6c8a93a12b97013e718eac8b76e1bf589b
Patrick Linnane <patrick@linnane.io>
2022-11-14 11:01:10 -0800

    Update zoho-mail from 1.5.0 to 1.5.1

--- Casks/zoho-mail.rb
+++ Casks/zoho-mail.rb
@@ -1,9 +1,9 @@
 cask "zoho-mail" do
   arch arm: "arm64-"
 
-  version "1.5.0"
-  sha256 arm:   "4daf19419ce8a7d00b02be6c08aefb9733b06e3f50750b30303250b957bbf57e",
-         intel: "9a4628e8d38b637dd80723f7f389a2ca0b2cc1dc2b454f6f35077c67bcb9851b"
+  version "1.5.1"
+  sha256 arm:   "fc76c823dc458101b0a30e29a5d175a4f1ce59cb50b3f0d06934c8765621f538",
+         intel: "ae3b75c4beca8ea2e201052ada529b2be62bc8e9f1b84796411561f5f6628adf"
 
   url "https://downloads.zohocdn.com/zmail-desktop/mac/zoho-mail-desktop-installer-#{arch}v#{version}.dmg",
       verified: "downloads.zohocdn.com/zmail-desktop/mac/"
