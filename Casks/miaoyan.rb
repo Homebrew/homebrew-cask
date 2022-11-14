701f6e08d175413f11bd4ee082514d3df3059b44
Patrick Linnane <patrick@linnane.io>
2022-11-13 20:47:40 -0800

    miaoyan 1.8.0
    Update miaoyan from 1.7.1 to 1.8.0
    
    Closes #135729.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/miaoyan.rb
+++ Casks/miaoyan.rb
@@ -1,6 +1,6 @@
 cask "miaoyan" do
-  version "1.7.1"
-  sha256 "afc7059096ef233faca36e1111574235300e09517770417a02f4fdbca14431b8"
+  version "1.8.0"
+  sha256 "a910c631b77d97c4bd43945f886d55cb30b0414f2aa22df471d09d12441b5201"
 
   url "https://github.com/tw93/MiaoYan/releases/download/V#{version}/MiaoYan.dmg",
       verified: "github.com/tw93/MiaoYan"
