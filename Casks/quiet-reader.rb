89881f2fa362e272b26914b28ba35b9aafd2be96
Marcin Mitura <mitura07@wp.pl>
2022-11-14 18:24:15 +0100

    Add `quiet-reader` to the Casks

---
+++ Casks/quiet-reader.rb
@@ -0,0 +1,11 @@
+cask "quiet-reader" do
+  version "0.41.0"
+  sha256 :no_check
+
+  url "https://quietreader.app/Quiet%20Reader.dmg"
+  name "Quiet Reader"
+  desc "Free app that let's you read articles or text highlights distraction free"
+  homepage "https://quietreader.app/"
+
+  app "Quiet Reader.app"
+end
