6174b0d75b6948dfe4ce71ef2897805417ff5261
Branch Vincent <branchevincent@gmail.com>
2022-11-14 18:50:29 -0500

    Update beekeeper-studio from 3.6.2 to 3.7.4

--- Casks/beekeeper-studio.rb
+++ Casks/beekeeper-studio.rb
@@ -1,9 +1,9 @@
 cask "beekeeper-studio" do
   arch arm: "-arm64"
 
-  version "3.6.2"
-  sha256 arm:   "f64c60f2f01647e489442e75b90e29058e548b9620316761ba9f78d5c7d233fc",
-         intel: "cdbeb8c32e11cd9c76360e17e0692d95d548464fbb414cfc0a1a84d802624a6f"
+  version "3.7.4"
+  sha256 arm:   "9013fbc92fe885f9977fc501e262d9c8db4d79f1f43f7818481fa852485dbfec",
+         intel: "aa676d65bd9c622c6229c60070164260a68b35909bc07cbdafb16c06950e8cbe"
 
   url "https://github.com/beekeeper-studio/beekeeper-studio/releases/download/v#{version}/Beekeeper-Studio-#{version}#{arch}.dmg",
       verified: "github.com/beekeeper-studio/beekeeper-studio/"
