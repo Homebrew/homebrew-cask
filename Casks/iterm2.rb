c4a317ed921ff4ad2256ffa50096d0d6fa80f551
Miccal Matthews <miccal.matthews@gmail.com>
2022-11-16 05:06:30 +0800

    Update iterm2.rb

--- Casks/iterm2.rb
+++ Casks/iterm2.rb
@@ -28,7 +28,7 @@ cask "iterm2" do
     "homebrew/cask-versions/iterm2-legacy",
     "homebrew/cask-versions/iterm2-nightly",
   ]
-  depends_on macos: ">= :catalina"
+  depends_on macos: ">= :mojave"
 
   app "iTerm.app"
 
