9cc1529b5503e29079eaa45ed9cb9c5f5b225945
Patrick Linnane <patrick@linnane.io>
2022-11-15 14:37:47 -0800

    archy 2.15.2
    Update archy from 2.15.1 to 2.15.2
    
    Closes #135813.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/archy.rb
+++ Casks/archy.rb
@@ -1,6 +1,6 @@
 cask "archy" do
-  version "2.15.1"
-  sha256 "bee1c65d9e4d083f94229620f44c9e4953a1d1d45e2287482cb20bf631c438e0"
+  version "2.15.2"
+  sha256 "d47e01d3fa698a09350bdf3ed43435b34190835917bfd04779a13fb305ff1b18"
 
   url "https://sdk-cdn.mypurecloud.com/archy/#{version}/archy-macos.zip",
       verified: "sdk-cdn.mypurecloud.com/archy/"
