d856c78dae75e9cffeeb407c82c979b3f18e5254
Patrick Linnane <patrick@linnane.io>
2022-11-15 14:53:20 -0800

    Update duckduckgo from 0.30.6 to 0.30.7

--- Casks/duckduckgo.rb
+++ Casks/duckduckgo.rb
@@ -1,6 +1,6 @@
 cask "duckduckgo" do
-  version "0.30.6"
-  sha256 "d1da3556d5af8f885ab929e64bdd78b50807cc443337463c5da8f66fd414058e"
+  version "0.30.7"
+  sha256 "e948f9ff674572334f599e7483e163bfc255b98d65fbd0dddf392875d6fac90f"
 
   url "https://staticcdn.duckduckgo.com/macos-desktop-browser/duckduckgo-#{version}.dmg"
   name "DuckDuckGo"
