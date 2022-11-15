7fb0e08edd252799a2d4558943307228fa797053
Patrick Linnane <patrick@linnane.io>
2022-11-15 15:32:46 -0800

    inkdrop 5.5.2, add desc

--- Casks/inkdrop.rb
+++ Casks/inkdrop.rb
@@ -1,13 +1,14 @@
 cask "inkdrop" do
   arch arm: "arm64", intel: "x64"
 
-  version "5.5.1"
-  sha256 arm:   "83f386ccc721378ba100435938fc53e8105ba572e1b39e5b9615b532f9ab3bb4",
-         intel: "c81e7f26d2b3984058309d65aaad2451d710838fd3f9c9ebb50eb465e70b8133"
+  version "5.5.2"
+  sha256 arm:   "913f6c181a1164d79068f57297d39a5564a27808ba464e7fb0ae1b3035fff611",
+         intel: "c4aa3592c153ad86bf674e706a1848e643307a8263c53ae831088d178a842232"
 
   url "https://d3ip0rje8grhnl.cloudfront.net/v#{version}/Inkdrop-#{version}-#{arch}-Mac.zip",
       verified: "d3ip0rje8grhnl.cloudfront.net/"
   name "Inkdrop"
+  desc "Markdown editor"
   homepage "https://www.inkdrop.info/"
 
   livecheck do
