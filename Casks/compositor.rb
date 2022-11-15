8e804feaf5416537c83035a652af90c92f464aa4
Patrick Linnane <patrick@linnane.io>
2022-11-15 14:43:56 -0800

    Update compositor from 1.20.4 to 1.20.6

--- Casks/compositor.rb
+++ Casks/compositor.rb
@@ -1,6 +1,6 @@
 cask "compositor" do
-  version "1.20.4"
-  sha256 "730fba8d2ed0cd32908a5ca201d3bcc0bb5a5289e65e522bff19d33fdef4c0b4"
+  version "1.20.6"
+  sha256 "0a67fde9cd2e643e9c32129a70834b394aa1ca4573d0c6dccd7a1a001c0f5624"
 
   url "https://compositorapp.com/updates/Compositor_#{version}.zip"
   name "Compositor"
