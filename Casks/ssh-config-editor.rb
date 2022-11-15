91f8111218ab8b9ccc0949fe92021f67f982d6fa
Patrick Linnane <patrick@linnane.io>
2022-11-15 15:48:13 -0800

    ssh-config-editor 2.6,99
    ssh-config-editor to 2.6, add min OS
    
    Closes #135868.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/ssh-config-editor.rb
+++ Casks/ssh-config-editor.rb
@@ -1,6 +1,6 @@
 cask "ssh-config-editor" do
-  version "2.5.1,96"
-  sha256 "a91a0db47e5a858abe1788baea135d1e306f335bb295468066621cffd996a3bf"
+  version "2.6,99"
+  sha256 "c2e9eb1b2f3f76dc38ce1446c5db0348dadd5dc82ad5e3363fa49e7c201d19cd"
 
   url "https://hejki.org/download/ssheditor/SSHConfigEditor-#{version.csv.second}.dmg"
   name "SSH Config Editor"
@@ -15,7 +15,7 @@ cask "ssh-config-editor" do
   end
 
   auto_updates true
-  depends_on macos: ">= :catalina"
+  depends_on macos: ">= :big_sur"
 
   app "SSH Config Editor.app"
 
