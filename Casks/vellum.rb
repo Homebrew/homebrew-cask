0dce19440a2e30f1358bb10763aff7eccc5c898e
Patrick Linnane <patrick@linnane.io>
2022-11-16 12:28:55 -0800

    vellum 3.4,34000
    Update vellum from 3.3.7 to 3.4
    
    Closes #135954.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/vellum.rb
+++ Casks/vellum.rb
@@ -1,6 +1,6 @@
 cask "vellum" do
-  version "3.3.7,33700"
-  sha256 "eb3617cd2dbb5ab5a6da164e4d892a1fe58ef60937a027e017640a16c5a65aa4"
+  version "3.4,34000"
+  sha256 "0edd0e547e16e58099470732abd38dab7baee17343f048c5fd8ad1c7ac898e5b"
 
   url "https://180g.s3.amazonaws.com/downloads/Vellum-#{version.csv.second}.zip",
       verified: "180g.s3.amazonaws.com/downloads/"
