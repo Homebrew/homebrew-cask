7346e9ef7bf54ca22ee5d8c05daff29c927bacfe
Patrick Linnane <patrick@linnane.io>
2022-11-16 12:30:54 -0800

    webull 6.5.0
    Update webull from 6.4.2 to 6.5.0
    
    Closes #135957.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/webull.rb
+++ Casks/webull.rb
@@ -2,9 +2,9 @@ cask "webull" do
   arch arm: "global_arm64", intel: "global_x64"
   livecheck_arch = on_arch_conditional arm: "qt_m1_global", intel: "qt_mac_global"
 
-  version "6.4.2"
-  sha256 arm:   "a822a1ebbb2d2f0e3515fcc0818796fb70b0bdadef40a00f96af32afef9f4c2a",
-         intel: "83638b5f2a2606e9c6a26a716b1d8cc8fd47f419ff72b4ee015fea67a3580a85"
+  version "6.5.0"
+  sha256 arm:   "4cd9156cd703aa0ba2ea75a008064535e8c6167382154d3056f51bf57d8e01f9",
+         intel: "48fe4e726b77fa82f1fb64201734b4181a126189cc878ba6952111215d1f36b4"
 
   url "https://u1sweb.webullfintech.com/us/desktop/Webull%20Desktop_#{version}_#{arch}signed.dmg",
       verified: "u1sweb.webullfintech.com/us/desktop/"
