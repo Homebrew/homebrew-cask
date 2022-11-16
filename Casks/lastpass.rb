5b5bd691943f83445381ddea250c2ebf595ce517
Patrick Linnane <patrick@linnane.io>
2022-11-16 11:56:54 -0800

    lastpass 4.103.0,1284
    lastpass 4.103.0, bump min OS
    
    Closes #135928.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/lastpass.rb
+++ Casks/lastpass.rb
@@ -1,5 +1,5 @@
 cask "lastpass" do
-  version "4.102.0,1281"
+  version "4.103.0,1284"
   sha256 :no_check
 
   url "https://download.cloud.lastpass.com/mac/LastPass.dmg"
@@ -13,7 +13,7 @@ cask "lastpass" do
   end
 
   auto_updates true
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :high_sierra"
 
   app "LastPass.app"
 
