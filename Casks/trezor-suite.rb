c68e2cef375536707870278d4716b88f3984abf5
owine <owine@users.noreply.github.com>
2022-11-16 08:52:20 -0600

    trezor-suite 22.11.1
    Update trezor-suite from 22.10.3 to 22.11.1
    
    Closes #135904.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/trezor-suite.rb
+++ Casks/trezor-suite.rb
@@ -1,9 +1,9 @@
 cask "trezor-suite" do
   arch arm: "arm64", intel: "x64"
 
-  version "22.10.3"
-  sha256 arm:   "1f966b5881444b771ba171187e6b9f5065d6b2061d99aff33538dbe96e2f2ef4",
-         intel: "d422a2f0720d8f900ac954b60008d88cc8603788549845e89ac92da11a69a064"
+  version "22.11.1"
+  sha256 arm:   "1201bd9764e95a808badeb7786e392b233cbabf38d707723cd595b4bb942d576",
+         intel: "644aa463f5bbe505bc618e37d042675504b4c9ec1ef97f489bfe08a6e9a6f13e"
 
   url "https://suite.trezor.io/web/static/desktop/Trezor-Suite-#{version}-mac-#{arch}.dmg"
   name "TREZOR Suite"
