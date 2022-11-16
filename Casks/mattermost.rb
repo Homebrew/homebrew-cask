6eb52337d0108736cd3487788b5fe27ed8720bdc
Balu <tw@b-a-l-u.de>
2022-11-16 21:23:03 +0100

    Update mattermost from 5.2.0 to 5.2.1 (#135909)

--- Casks/mattermost.rb
+++ Casks/mattermost.rb
@@ -1,9 +1,9 @@
 cask "mattermost" do
   arch arm: "m1", intel: "x64"
 
-  version "5.2.0"
-  sha256 arm:   "721946c3750dc28203af10a9696a22b5487a7d3c8178d79795fa906ec89644c2",
-         intel: "3bf5cbaeef54dc1ae41736c79bfd39bae82b2755fa51cefb183275807e9ce085"
+  version "5.2.1"
+  sha256 arm:   "40c380926d177685f32e45bf33ac1745198fb64d72fe1093528e3a74713ce750",
+         intel: "0c00d5096cb22afc501c001144edbf1654e504d09546c8767d7d5e8e669a2e35"
 
   url "https://releases.mattermost.com/desktop/#{version}/mattermost-desktop-#{version}-mac-#{arch}.zip"
   name "Mattermost"
