d78bea1e4cb46d9b95b139e5ca260e6b477fcf77
Patrick Linnane <patrick@linnane.io>
2022-11-16 11:35:38 -0800

    anydo 5.0.11
    Update anydo from 5.0.10 to 5.0.11
    
    Closes #135914.
    
    Signed-off-by: Markus Reiter <me@reitermark.us>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/anydo.rb
+++ Casks/anydo.rb
@@ -1,6 +1,6 @@
 cask "anydo" do
-  version "5.0.10"
-  sha256 "86b2dda20d9d05532b75ba10100121b5eb885a509efb4f7983a5f0d59da23207"
+  version "5.0.11"
+  sha256 "ce2f8ed0fa4a87508e73bff78c66b8c563b3a3ff8adecbaed57ef1bad8280616"
 
   url "https://electron-app.any.do/Anydo-#{version}.dmg"
   name "Any.do"
