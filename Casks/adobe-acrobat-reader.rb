92c2a21336d30de9ce7548683a972770f530c059
eloyaldea <eloy.aldea@gmail.com>
2022-11-17 13:00:15 +0100

    Update adobe-acrobat-reader from 22.003.20258 to 22.003.20281

--- Casks/adobe-acrobat-reader.rb
+++ Casks/adobe-acrobat-reader.rb
@@ -1,6 +1,6 @@
 cask "adobe-acrobat-reader" do
-  version "22.003.20258"
-  sha256 "f23d3f6a6c10e2df540c2345d8b1c90e28b9ef6de7b9d63739ed9b715b2b7ecc"
+  version "22.003.20281"
+  sha256 "43ccafc2cd8321119720b21c5b2821ac9d7e71c7eeb4eb85f09687ebceb0eefe"
 
   url "https://ardownload2.adobe.com/pub/adobe/reader/mac/AcrobatDC/#{version.no_dots}/AcroRdrDC_#{version.no_dots}_MUI.dmg"
   name "Adobe Acrobat Reader"
