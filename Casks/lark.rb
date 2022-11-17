5381dee4762ff27d68b5053ef96e215d5906ce32
Patrick Linnane <patrick@linnane.io>
2022-11-16 20:40:16 -0800

    lark 5.23.14,e42953
    Update lark from 5.20.10 to 5.23.14
    
    Closes #135982.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/lark.rb
+++ Casks/lark.rb
@@ -2,12 +2,12 @@ cask "lark" do
   arch arm: "arm64", intel: "x64"
 
   on_intel do
-    version "5.20.10,23ea9d"
-    sha256 "caba67db9b14184f20b373c6b06d88341a7830f676fd79f5e710bb6575cc1010"
+    version "5.23.14,e42953"
+    sha256 "68082315db66e25db66b50f992364e77127c1947019a1699c6e469d758cf35d0"
   end
   on_arm do
-    version "5.20.10,fa353d"
-    sha256 "0c8e521f6a535cc4bd5da9bc8e6842dabb8f72febc5533137681dc4bf3ce6f02"
+    version "5.23.14,e4870a"
+    sha256 "119d335e788e8a0a395de0efdac6c2bd085f0e52e1e2e6433bffc6aa670898ed"
   end
 
   url "https://sf16-va.larksuitecdn.com/obj/lark-artifact-storage/#{version.csv.second}/Lark-darwin_#{arch}-#{version.csv.first}-signed.dmg",
