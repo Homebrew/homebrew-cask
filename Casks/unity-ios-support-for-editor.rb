8f36e4f32d06e149f016f6b6437aa50189667d9b
Patrick Linnane <patrick@linnane.io>
2022-11-16 16:37:58 -0800

    unity-ios-support-for-editor 2022.1.23f1,9636b062134a
    Update unity-ios-support-for-editor from 2022.1.22f1 to 2022.1.23f1
    
    Closes #135974.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/unity-ios-support-for-editor.rb
+++ Casks/unity-ios-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask "unity-ios-support-for-editor" do
-  version "2022.1.22f1,6b6e9fc2adda"
-  sha256 "e0cb633733ae07d19f2ac733ebcf48c6c8349895e010b8cf5e21f58ec171b76e"
+  version "2022.1.23f1,9636b062134a"
+  sha256 "2a1d13144f4b02fdc1ff72e3fc9f5c3258ab32ab19cb67d8513e5438a1f4d2ca"
 
   url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-#{version.csv.first}.pkg",
       verified: "download.unity3d.com/download_unity/"
