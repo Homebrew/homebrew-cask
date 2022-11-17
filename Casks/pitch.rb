84052a76f873b06fbb74ab452b1b1ec2e9cb99e7
Patrick Linnane <patrick@linnane.io>
2022-11-17 14:17:41 -0800

    pitch 1.101.0,stable.3,4896382
    Update pitch from 1.101.0,stable.2,4890382 to 1.101.0,stable.3,4896382
    
    Closes #136035.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/pitch.rb
+++ Casks/pitch.rb
@@ -1,6 +1,6 @@
 cask "pitch" do
-  version "1.101.0,stable.2,4890382"
-  sha256 "38c2c763439412db89ad3549aa4107600275eefb71b4f60e2cedacfbe5c7567a"
+  version "1.101.0,stable.3,4896382"
+  sha256 "a6e1231e6bf3f85c73052e4d77348931325882823a96e5929503521e9b667d45"
 
   url "https://desktop-app-builds.pitch.com/Pitch-#{version.csv.first}-#{version.csv.second}-ci#{version.csv.third}.dmg"
   name "Pitch"
