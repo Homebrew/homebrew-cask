2d876ccad15d076ffbc5794ac2c6c7740c269bb0
Patrick Linnane <patrick@linnane.io>
2022-11-17 14:27:09 -0800

    Update tableau-reader from 2022.3.0 to 2022.3.1

--- Casks/tableau-reader.rb
+++ Casks/tableau-reader.rb
@@ -1,6 +1,6 @@
 cask "tableau-reader" do
-  version "2022.3.0"
-  sha256 "cf9ad29102c4d25c0b82861b54ea56112a2f5de98433a3347d2f89d76534e644"
+  version "2022.3.1"
+  sha256 "d91bcfc1095cb671ddd090825d00440c7333e01acba51b55d338794b97a70062"
 
   url "https://downloads.tableau.com/tssoftware/TableauReader-#{version.dots_to_hyphens}.dmg"
   name "Tableau Reader"
