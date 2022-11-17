9d72ea99bf8de8e595fbaef4ebfc7f0a0b449ef3
Patrick Linnane <patrick@linnane.io>
2022-11-17 14:10:24 -0800

    Update mcreator from 2022.3.46416 to 2022.3.47418

--- Casks/mcreator.rb
+++ Casks/mcreator.rb
@@ -1,6 +1,6 @@
 cask "mcreator" do
-  version "2022.3.46416"
-  sha256 "69724506d3f8146f0c89456828e36987b4c1091638319f8ae245574dbe4f8e02"
+  version "2022.3.47418"
+  sha256 "84989983216ee35aa9f8296b547c20b91bf0f52ff6805e53b5363f7d4d853338"
 
   url "https://github.com/MCreator/MCreator/releases/download/#{version}/MCreator.#{version.major_minor}.Mac.64bit.dmg",
       verified: "github.com/MCreator/MCreator/"
