1739f2dd2d1d01c0afa199d3a4eddf8c6eb4a934
Patrick Linnane <patrick@linnane.io>
2022-11-17 14:23:50 -0800

    Update slack-cli from 1.15.0 to 1.16.4

--- Casks/slack-cli.rb
+++ Casks/slack-cli.rb
@@ -1,6 +1,6 @@
 cask "slack-cli" do
-  version "1.15.0"
-  sha256 "db09e378a3f5ea07256b2d7bab6782e6d2fde31b111ded6b563825abd864400c"
+  version "1.16.4"
+  sha256 "f52e297f3c9b6378aabf42c460c978637ccfce05420b0cdf4cb2aaa3134ab78a"
 
   url "https://downloads.slack-edge.com/slack-cli/slack_cli_#{version}_macOS_64-bit.tar.gz",
       verified: "downloads.slack-edge.com"
