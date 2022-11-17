90295d7787d2a7a80334cbd5eabdee19a5985751
Patrick Linnane <patrick@linnane.io>
2022-11-16 20:59:10 -0800

    Update visual-paradigm from 17.0,20221107 to 17.0,20221123

--- Casks/visual-paradigm.rb
+++ Casks/visual-paradigm.rb
@@ -1,9 +1,9 @@
 cask "visual-paradigm" do
   arch arm: "AArch64", intel: "WithJRE"
 
-  version "17.0,20221107"
-  sha256 arm:   "06ab2b45cdd589d27e6e806744da565fba7359b0109408225bc49599882c41b4",
-         intel: "699d61b9692cce4eae80bf7bd4d9aa1cc360e4558b4a1faee217da856c2c2378"
+  version "17.0,20221123"
+  sha256 arm:   "23e3773bc8183a6873975fd51c08e3742e43abd00eb5b84e01fa4d40b999b1dd",
+         intel: "83a919e67909104540324dfcbab7b7839106e25b388838aa370d28f80f7cef78"
 
   url "https://www.visual-paradigm.com/downloads/vp#{version.csv.first}/#{version.csv.second}/Visual_Paradigm_#{version.csv.first.dots_to_underscores}_#{version.csv.second}_OSX_#{arch}.dmg"
   name "Visual Paradigm"
