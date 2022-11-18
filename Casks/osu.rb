8c5d4bb4f74f4e7bcc08be1119ba6908a417528c
miccal <miccal.matthews@gmail.com>
2022-11-18 11:55:15 +0800

    Update osu from 2022.1101.0 to 2022.1117.0

--- Casks/osu.rb
+++ Casks/osu.rb
@@ -1,9 +1,9 @@
 cask "osu" do
   arch arm: "Apple.Silicon", intel: "Intel"
 
-  version "2022.1101.0"
-  sha256 arm:   "186be9d324fed096317c927a2f1a1429ac6f0957fc0ed30567a9ef9eff1e36cc",
-         intel: "dc9b363409594fd54b32700f090248fa2a00b0ac03904c6b4da4a6538017af02"
+  version "2022.1117.0"
+  sha256 arm:   "3966b7a0c7f81cb4dde61959917c81aa0ab8b744e645d0072ce0cde506546b9d",
+         intel: "a44d2383af5182d2cdc132e4c007bfcb76b1739eb72bb79e8158093dcbf242da"
 
   url "https://github.com/ppy/osu/releases/download/#{version}/osu.app.#{arch}.zip"
   name "osu!"
