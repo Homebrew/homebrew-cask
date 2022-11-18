d88821d81dda93ca9e7294d2364efa1481db1a53
enclave-alistair <alistair.evans@enclave.io>
2022-11-18 18:05:16 +0000

    enclave 2022.11.18
    Update enclave from 2022.09.27 to 2022.11.18
    
    Closes #136080.
    
    Signed-off-by: Markus Reiter <me@reitermark.us>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/enclave.rb
+++ Casks/enclave.rb
@@ -1,6 +1,6 @@
 cask "enclave" do
-  version "2022.09.27"
-  sha256 "c21cbd43050b3b4dd76e19e9c68959bfc9a9e53112ec8c8a40692f974375a5b2"
+  version "2022.11.18"
+  sha256 "19055a1cb343f86a7872a786d2b0a21b8f57ce61f45f003afb2657154b340307"
 
   url "https://release.enclave.io/enclave_osx-installer-x64-stable-#{version}.pkg"
   name "Enclave"
