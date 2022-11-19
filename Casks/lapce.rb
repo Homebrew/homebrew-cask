57eccba2b4bd69089f74b329fc558d0d688b12cc
Patrick Linnane <patrick@linnane.io>
2022-11-19 08:43:41 -0800

    lapce 0.2.4
    Update lapce from 0.2.3 to 0.2.4
    
    Closes #136117.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/lapce.rb
+++ Casks/lapce.rb
@@ -1,6 +1,6 @@
 cask "lapce" do
-  version "0.2.3"
-  sha256 "8dae216fdeff3f9967d93c13847e9b012df69c6f77985f9f01b67d00b684000e"
+  version "0.2.4"
+  sha256 "fd4cb9578e3c07788ff9839c7f111a6ce2c1058d99eb51bab67c77d4a012c390"
 
   url "https://github.com/lapce/lapce/releases/download/v#{version}/Lapce-macos.dmg",
       verified: "github.com/lapce/lapce/"
