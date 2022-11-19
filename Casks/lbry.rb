6a594cd6535f1bad9ea8101aa14a513d7d16a6d0
Patrick Linnane <patrick@linnane.io>
2022-11-19 08:44:33 -0800

    Update lbry from 0.53.7 to 0.53.8

--- Casks/lbry.rb
+++ Casks/lbry.rb
@@ -1,6 +1,6 @@
 cask "lbry" do
-  version "0.53.7"
-  sha256 "d7bf5c57ec6bc657b0537bb0e9d223f89c4b4f9353411e48f9348c5ae9e50cbe"
+  version "0.53.8"
+  sha256 "68e1aa1a5189a822ec160dc0a3134146148da3ffe62ff69862ce48a92cb1e079"
 
   url "https://github.com/lbryio/lbry-desktop/releases/download/v#{version}/LBRY_#{version}.dmg",
       verified: "github.com/lbryio/lbry-desktop/"
