da65b34bdc9093dddfbe88dcd1ad0cae85eef4e9
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2022-11-20 04:15:01 +0500

    fix(browserstacklocal): latest package url and hash

--- Casks/browserstacklocal.rb
+++ Casks/browserstacklocal.rb
@@ -1,8 +1,8 @@
 cask "browserstacklocal" do
   version "3.5.5"
-  sha256 :no_check
+  sha256 "e0586e12c47d1fbc79806b58487f337a1e276cfc19ba7d138a8ba5fb51aa418e"
 
-  url "https://www.browserstack.com/local-testing/downloads/native-app/BrowserStackLocal.dmg"
+  url "https://www.browserstack.com/local-testing/downloads/native-app/BrowserStackLocal.tar.gz"
   name "BrowserStack Local Testing"
   desc "Test localhost and staging websites"
   homepage "https://www.browserstack.com/"
