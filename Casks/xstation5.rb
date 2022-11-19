e71c453848fd647372a5aa7930c09b95d59ed490
Patrick Linnane <patrick@linnane.io>
2022-11-19 08:55:41 -0800

    xstation5 2.40.0-Build.3
    Update xstation5 from 2.39.1-Build.3 to 2.40.0-Build.3
    
    Closes #136139.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/xstation5.rb
+++ Casks/xstation5.rb
@@ -1,7 +1,7 @@
 cask "xstation5" do
   # NOTE: "5" is not a version number, but an intrinsic part of the product name
-  version "2.39.1-Build.3"
-  sha256 "647671ccab3d11e98dd7ae7b23c8b9f69b547b801e394ec5dfbd1f62ebb689fd"
+  version "2.40.0-Build.3"
+  sha256 "de6a0aa18bb8ddc3b346b997fcd982b680cd448f8d17a9dab449aa156b8d52d3"
 
   url "https://desktopxstation5.xtb.com/prod/update/mac/xStation5-#{version}-mac.zip"
   name "xStation5"
