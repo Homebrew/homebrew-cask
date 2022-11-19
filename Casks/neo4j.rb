71cd8ad5a6726185512398a13e8cd529baac6950
Patrick Linnane <patrick@linnane.io>
2022-11-19 08:46:32 -0800

    neo4j 1.5.4
    Update neo4j from 1.5.2 to 1.5.4
    
    Closes #136123.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/neo4j.rb
+++ Casks/neo4j.rb
@@ -1,7 +1,7 @@
 cask "neo4j" do
   # NOTE: "4" is not a version number, but an intrinsic part of the product name
-  version "1.5.2"
-  sha256 "52b73a7ef11a6f93a17f8dd8bda43d6ab13f37a40487b2c6aac814d885c075b8"
+  version "1.5.4"
+  sha256 "1fc564524f44c4e2e828fc7674d3ff9b0c1233c1dce364b859c1fe8272345592"
 
   url "https://dist.neo4j.org/neo4j-desktop/mac/Neo4j%20Desktop-#{version}.dmg",
       verified: "dist.neo4j.org/neo4j-desktop/mac/"
