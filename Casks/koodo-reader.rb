3cc8a0d4a0b79acd277b7fdfac35a1d2cb71f664
Patrick Linnane <patrick@linnane.io>
2022-11-19 08:43:21 -0800

    koodo-reader 1.5.0
    Update koodo-reader from 1.4.9 to 1.5.0
    
    Closes #136116.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/koodo-reader.rb
+++ Casks/koodo-reader.rb
@@ -1,9 +1,9 @@
 cask "koodo-reader" do
   arch arm: "-arm64"
 
-  version "1.4.9"
-  sha256 arm:   "2051f9cee01fa80e031670ba3cfa0fe740ba5dd6a514dada6ad74b8888734b21",
-         intel: "432f35c97d28af2deca2ac9ef819b426bee49a26e48c4db0c3efb1fff459960b"
+  version "1.5.0"
+  sha256 arm:   "ba0d762a88a292b4056bfa970cac6e7c75c9efa6ee87cd20e1861d7a63c7d90d",
+         intel: "f4b049c266e97451bdb78814e587000361d15bc3f285d55cd24f5fb7f9d7313c"
 
   url "https://github.com/troyeguo/koodo-reader/releases/download/v#{version}/Koodo-Reader-#{version}#{arch}.dmg",
       verified: "github.com/troyeguo/koodo-reader/"
