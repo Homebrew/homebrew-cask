4822aef01c979fd45c11b7e26af20b0d5e029cd1
Alexander Regueiro <alexreg@gmail.com>
2022-11-19 13:56:41 +0000

    gdat: rebuild
    * gdat: update website
    * Add verified
    
    Closes #136093.
    
    Co-authored-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/gdat.rb
+++ Casks/gdat.rb
@@ -2,10 +2,11 @@ cask "gdat" do
   version "2022r08"
   sha256 :no_check
 
-  url "https://drive.google.com/uc?export=download&id=1lnA5hFiCV9qzreQE7a5YzdeValaicTHD"
+  url "https://drive.google.com/uc?export=download&id=1lnA5hFiCV9qzreQE7a5YzdeValaicTHD",
+      verified: "drive.google.com/uc?export=download&id=1lnA5hFiCV9qzreQE7a5YzdeValaicTHD"
   name "Genealogical DNA Analysis Tool"
   desc "App that utilizes autosomal DNA to aid in the research of family trees"
-  homepage "https://sites.google.com/view/genealogical-dna-analysis-tool"
+  homepage "https://www.getgdat.com/"
 
   livecheck do
     url "https://sites.google.com/view/genealogical-dna-analysis-tool/home/download"
