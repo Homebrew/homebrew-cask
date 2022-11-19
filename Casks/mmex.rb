720ece4571fc4e36558c6ef55cd1fdad2f800479
Patrick Linnane <patrick@linnane.io>
2022-11-19 08:46:05 -0800

    Update mmex from 1.6.0 to 1.6.1

--- Casks/mmex.rb
+++ Casks/mmex.rb
@@ -1,6 +1,6 @@
 cask "mmex" do
-  version "1.6.0"
-  sha256 "c1315f09656ccfec92cb4f9cffe5a39d661472175f682750a4bfbe174638bcbf"
+  version "1.6.1"
+  sha256 "c6d7e2b371790974b35b44f2951bfb972fcdae5a8c90a5b1dfa05f40bcd5f8fe"
 
   url "https://github.com/moneymanagerex/moneymanagerex/releases/download/v#{version}/mmex-#{version}-Darwin.dmg",
       verified: "github.com/moneymanagerex/moneymanagerex/"
