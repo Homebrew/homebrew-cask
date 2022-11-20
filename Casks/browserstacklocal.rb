ddb22e5f4f6bacd2be5140633b1aa1fe7a9a6f85
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2022-11-20 06:53:51 +0500

    fix(browserstacklocal): latest package url (#136145)
    * fix(browserstacklocal): latest package url and hash
    
    * fix(browserstack): remove sha256

--- Casks/browserstacklocal.rb
+++ Casks/browserstacklocal.rb
@@ -2,7 +2,7 @@ cask "browserstacklocal" do
   version "3.5.5"
   sha256 :no_check
 
-  url "https://www.browserstack.com/local-testing/downloads/native-app/BrowserStackLocal.dmg"
+  url "https://www.browserstack.com/local-testing/downloads/native-app/BrowserStackLocal.tar.gz"
   name "BrowserStack Local Testing"
   desc "Test localhost and staging websites"
   homepage "https://www.browserstack.com/"
