90fa2151e2473cf0b33ad1be1ff56e69fd185cf3
Patrick Linnane <patrick@linnane.io>
2022-11-20 08:08:53 -0800

    Update blockbench from 4.5.1 to 4.5.2

--- Casks/blockbench.rb
+++ Casks/blockbench.rb
@@ -1,9 +1,9 @@
 cask "blockbench" do
   arch arm: "arm64", intel: "x64"
 
-  version "4.5.1"
-  sha256 arm:   "865600f0a6562de82650b5e884cc132a217da5f98c5996b79582bcb721dd9bce",
-         intel: "7eab16f6ebe19f45665e4a74528131ca5f503a82469355b20f656a9b08b91b69"
+  version "4.5.2"
+  sha256 arm:   "93d76408fe896895a94c8f9ee03c619f026a448f5079da05d9e951cd1905e5ff",
+         intel: "67e297a176bfd8b90d83e8819b539b4b7288635680a89255913e7092e0eb3054"
 
   url "https://github.com/JannisX11/blockbench/releases/download/v#{version}/Blockbench_#{arch}_#{version}.dmg",
       verified: "github.com/JannisX11/blockbench/"
