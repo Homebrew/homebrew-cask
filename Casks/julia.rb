c77007d3ec71d2be2d1d45e8fee28468ec57d281
Ryan <25192197+singularitti@users.noreply.github.com>
2022-11-20 04:35:00 -0500

    julia: update zap (#136147)
    * Update julia from 1.8.3 to 1.8.3
    
    * Alphabetize zap stanza
    
    Co-authored-by: Patrick Linnane <patrick@linnane.io>

--- Casks/julia.rb
+++ Casks/julia.rb
@@ -20,6 +20,9 @@ cask "julia" do
 
   zap trash: [
     "~/.julia",
+    "~/Library/Logs/Julia",
+    "~/Library/Preferences/com.github.Julia.plist",
     "~/Library/Preferences/julia.plist",
+    "~/Library/Saved Application State/com.github.Julia.savedState",
   ]
 end
