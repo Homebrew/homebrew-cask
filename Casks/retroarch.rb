b6f943bbbf91d5c8a498933028378084194056f4
Patrick Linnane <patrick@linnane.io>
2022-11-20 08:21:34 -0800

    retroarch 1.13.0
    Update retroarch from 1.12.0 to 1.13.0
    
    Closes #136174.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/retroarch.rb
+++ Casks/retroarch.rb
@@ -1,6 +1,6 @@
 cask "retroarch" do
-  version "1.12.0"
-  sha256 "b8d37681957e0b4806b426c8a5bb38a138a86977c38fb7dc00c3b21d43f290d3"
+  version "1.13.0"
+  sha256 "6d34f053d1acfbdb3717b5d5f75943c0b707de6e043171d48010bab8a77a2168"
 
   url "https://buildbot.libretro.com/stable/#{version}/apple/osx/x86_64/RetroArch.dmg"
   name "RetroArch"
