4565ce80ca8b4195f3faa7c43bd7fe9bcb2dfb32
Patrick Linnane <patrick@linnane.io>
2022-11-20 08:09:51 -0800

    Update browserosaurus from 19.2.0 to 19.3.0

--- Casks/browserosaurus.rb
+++ Casks/browserosaurus.rb
@@ -1,9 +1,9 @@
 cask "browserosaurus" do
   arch arm: "arm64", intel: "x64"
 
-  version "19.2.0"
-  sha256 arm:   "f4da98259668f4a21f89b57a585b464095d0f80d0dd71e83258fbcf1102320a3",
-         intel: "fa39c58646e3ba9dec6c57a0f35dba4785415d5a6295ad1578d0fa2d7b361bb4"
+  version "19.3.0"
+  sha256 arm:   "fcbee7a05ee6fb5c64181d112de23b1a2fbd27f6fb1ffe1b65f98cf15a84359a",
+         intel: "e82de6399fb2e2447b3527782b67fb1fcef2a34b826a486ee288e755176dfef5"
 
   url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-darwin-#{arch}-#{version}.zip"
   name "Browserosaurus"
