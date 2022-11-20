f334349261edd2d64bb96837f2bcde177de39d5d
Andreas Eitel <github-aneitel@online.de>
2022-11-20 22:03:02 +0100

    portfolioperformance 0.59.4
    update portfolioperformance from 0.59.3 to 0.59.4
    
    Closes #136185.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/portfolioperformance.rb
+++ Casks/portfolioperformance.rb
@@ -1,9 +1,9 @@
 cask "portfolioperformance" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.59.3"
-  sha256 arm:   "1047aee9c15fe5a49c19071a7d8c93751ac9ce8ea49f77fc1bd48c37c5dae45f",
-         intel: "7de8906e71e8548222bb8bce7f7585ed3ad89c90462a37aa91101a2b0e94c96f"
+  version "0.59.4"
+  sha256 arm:   "31a13f0c9b9acb53a92e6c802a08d249208944ed9ec16c153667a192caa1cb4d",
+         intel: "6843f29ebf2c141c6063955014d6759ad17b366844352de19e345b60d59ccd9f"
 
   url "https://github.com/buchen/portfolio/releases/download/#{version}/PortfolioPerformance-#{version}-#{arch}.dmg",
       verified: "github.com/buchen/portfolio/"
