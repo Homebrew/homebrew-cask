f1a0656d164e75d19bed24ed6b851324d6bb6a0f
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2022-11-20 07:04:32 +0500

    fix(Folx): fix `url` and `livecheck` url (#136146)
    * fix(Folx): fix `url` and `livecheck` url
    
    * fix(folx): fix url verified
    
    * Update folx.rb
    
    * Update folx.rb
    
    * Update folx.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/folx.rb
+++ Casks/folx.rb
@@ -2,17 +2,19 @@ cask "folx" do
   version "5.27,13991"
   sha256 :no_check
 
-  url "https://cdn.eltima.com/download/downloader_mac.dmg"
+  url "https://cdn.electronic.us/products/folx/mac/download/downloader_mac.dmg",
+      verified: "https://cdn.electronic.us/products/folx/"
   name "Folx"
   desc "Download manager with a torrent client"
   homepage "https://mac.eltima.com/download-manager.html"
 
   livecheck do
-    url "https://cdn.eltima.com/download/folx-updater/folx.xml"
+    url "https://cdn.electronic.us/products/folx/mac/update/settings.xml"
     strategy :sparkle
   end
 
   auto_updates true
+  depends_on macos: ">= :sierra"
 
   app "Folx.app"
 
