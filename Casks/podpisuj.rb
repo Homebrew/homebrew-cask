3d19ee4bb1730146bd22fd62e6b10953c5b10d95
Patrick Linnane <patrick@linnane.io>
2022-11-21 12:27:41 -0800

    Update podpisuj from 5.6.46 to 5.6.50

--- Casks/podpisuj.rb
+++ Casks/podpisuj.rb
@@ -1,6 +1,6 @@
 cask "podpisuj" do
-  version "5.6.46"
-  sha256 "58a2f97171bd9c278a6906b6e5b8c04b5546cc0e8269f55aaf1e3e7523c3090e"
+  version "5.6.50"
+  sha256 "83e9a39432e6263fa93591d11b5fe3ef4242d042566cc706e1b163d524c94e51"
 
   url "https://www.podpisuj.sk/staticweb/install/podpisuj-#{version}.dmg"
   name "Podpisuj"
