3e592c27e52d60129b2556271390ca4af77b2562
Patrick Linnane <patrick@linnane.io>
2022-11-21 12:33:59 -0800

    yggdrasil 0.4.7
    Update yggdrasil from 0.4.6 to 0.4.7
    
    Closes #136263.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/yggdrasil.rb
+++ Casks/yggdrasil.rb
@@ -1,9 +1,9 @@
 cask "yggdrasil" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.4.6"
-  sha256 arm:   "7504f9f76bbfbd0cd66d218b965a0e1a5a4c48152fdf5dbb69d1928cb85a76cf",
-         intel: "657ccda1671b106ed03e501906f91bf232d33d18abf335c2429946ddba8f296f"
+  version "0.4.7"
+  sha256 arm:   "81f97a18908c43e706384ed747ab685efdb0f1df2bb820e588946260f22b5bf4",
+         intel: "73b9e60bfd720057b3dee386db67a0d23038b3623f84f0d77ad81bbc79c74610"
 
   url "https://github.com/yggdrasil-network/yggdrasil-go/releases/download/v#{version}/yggdrasil-#{version}-macos-#{arch}.pkg"
   name "Yggdrasil"
