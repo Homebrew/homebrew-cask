de9da5caa89441aa0400df4ac74978632473103e
Patrick Linnane <patrick@linnane.io>
2022-11-21 11:49:55 -0800

    Update chronycontrol from 1.4.6 to 1.4.7

--- Casks/chronycontrol.rb
+++ Casks/chronycontrol.rb
@@ -1,6 +1,6 @@
 cask "chronycontrol" do
-  version "1.4.6,281"
-  sha256 "8a3b340c92d6899b9799f2e212e5fe73f1a4f44bd0828057cebe060f3396b2a5"
+  version "1.4.7,282"
+  sha256 "3e26f7dbafd1a8d572ea2be37212f0ca4be0121847ec56981cb7323b26da93d5"
 
   url "https://www.whatroute.net/software/chronycontrol-#{version.csv.first}.zip"
   name "ChronyControl"
