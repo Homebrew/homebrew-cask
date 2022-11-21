f3aaaf0350d26df4f16a6d342e421bbf21a1d0bd
Patrick Linnane <patrick@linnane.io>
2022-11-21 12:18:40 -0800

    mongodb-compass-isolated-edition 1.34.0
    Update mongodb-compass-isolated-edition from 1.33.1 to 1.34.0
    
    Closes #136237.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/mongodb-compass-isolated-edition.rb
+++ Casks/mongodb-compass-isolated-edition.rb
@@ -1,9 +1,9 @@
 cask "mongodb-compass-isolated-edition" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.33.1"
-  sha256 arm:   "e7665f693998a4ea9fe824c869fadd27286d71ebbefa64007e554e467ccea34c",
-         intel: "e83d4762c2dbb5e25c89282de89574bd420486c309a900cbf9483c9b6754b7c7"
+  version "1.34.0"
+  sha256 arm:   "06d74daf95bea8d53958606edabe0501d4225552ddedfcec29e6eddc72a4f03f",
+         intel: "38691688c7722f5ae7c335427fa279c3999a38f337a1ce1bd8d2f5cd3bec7305"
 
   url "https://downloads.mongodb.com/compass/mongodb-compass-isolated-#{version}-darwin-#{arch}.dmg"
   name "MongoDB Compass Isolated"
