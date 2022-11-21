1bab3c1dae1eea2ae296a9f9636db7f1761c4907
Patrick Linnane <patrick@linnane.io>
2022-11-21 11:46:12 -0800

    Update altair-graphql-client from 5.0.4 to 5.0.5

--- Casks/altair-graphql-client.rb
+++ Casks/altair-graphql-client.rb
@@ -1,9 +1,9 @@
 cask "altair-graphql-client" do
   arch arm: "arm64", intel: "x64"
 
-  version "5.0.4"
-  sha256 arm:   "81e6a2eb92f1cb5aa241e49559dbad860ddde935e481b7bd2a893105c07bbe9a",
-         intel: "cf13cc9c063aaae6eb0e131fd051cea31ee1e79c086e06371527eb60a883ddda"
+  version "5.0.5"
+  sha256 arm:   "1b9eb653c837a0f0aa60b1953d22734c38d4906bbcd5b8db3138d9d356452984",
+         intel: "8e64831f43fd02acfe746a2a20abf77a5b80c7be837a2ce2a4f35f0669e536e0"
 
   url "https://github.com/imolorhe/altair/releases/download/v#{version}/altair_#{version}_#{arch}_mac.zip",
       verified: "github.com/imolorhe/altair/"
