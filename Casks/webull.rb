2650b679de7bba9dfaac75e32434ab1e5fff41c5
Patrick Linnane <patrick@linnane.io>
2022-11-21 12:33:28 -0800

    Update webull from 6.5.0 to 6.5.1

--- Casks/webull.rb
+++ Casks/webull.rb
@@ -2,9 +2,9 @@ cask "webull" do
   arch arm: "global_arm64", intel: "global_x64"
   livecheck_arch = on_arch_conditional arm: "qt_m1_global", intel: "qt_mac_global"
 
-  version "6.5.0"
-  sha256 arm:   "4cd9156cd703aa0ba2ea75a008064535e8c6167382154d3056f51bf57d8e01f9",
-         intel: "48fe4e726b77fa82f1fb64201734b4181a126189cc878ba6952111215d1f36b4"
+  version "6.5.1"
+  sha256 arm:   "5c4bbbeef1ba0d90c637ce95f0ae1dbd899efb218a8752f07b6a494faed1de43",
+         intel: "b7e07db6000ce876b0fd73e9e7c54395cd98f9001abdf8ba0c006926be946240"
 
   url "https://u1sweb.webullfintech.com/us/desktop/Webull%20Desktop_#{version}_#{arch}signed.dmg",
       verified: "u1sweb.webullfintech.com/us/desktop/"
