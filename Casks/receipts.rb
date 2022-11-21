1fa922bf509bdad762d03017b17f57c88e69a072
Patrick Linnane <patrick@linnane.io>
2022-11-21 12:29:11 -0800

    receipts 1.9.17-562
    Update receipts from 1.9.16-544 to 1.9.17-562
    
    Closes #136256.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/receipts.rb
+++ Casks/receipts.rb
@@ -1,6 +1,6 @@
 cask "receipts" do
-  version "1.9.16-544"
-  sha256 "a67c66bcca520ef71a88e652bf3058c016b9741e5d8e844e575decd27e14ee5c"
+  version "1.9.17-562"
+  sha256 "6689159629ab2e6e2391da0e0992edd6e82d63e37f40b682c5f41cb467016bec"
 
   url "https://www.receipts-app.com/update/download/Receipts-#{version}.zip"
   name "Receipts"
