8ad1c89f9623e163a439f0ba3d82867a0f05f86d
Jef LeCompte <jeffreylec@gmail.com>
2022-11-22 09:22:02 -0800

    chore(openlens): fix sha256

--- Casks/openlens.rb
+++ Casks/openlens.rb
@@ -3,7 +3,7 @@ cask "openlens" do
 
   version "6.2.1"
   sha256 arm:   "5aad8a7a5e7626921836f434dbdb0678c39f0fb41ea54c94e55417a639cb0e8c",
-         intel: "924d8aece336705adc60b95f4c7ce197126abf69a36992decf193d7e0f7f00c8"
+         intel: "379b3e0ca085a104e48c8168e5f894863105937010fe83b71ecc59b396de4ea1"
 
   url "https://github.com/MuhammedKalkan/OpenLens/releases/download/v#{version}/OpenLens-#{version}#{arch}.dmg"
   name "OpenLens"
