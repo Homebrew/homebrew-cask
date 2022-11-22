be8eddef9033b516ae1a5a82564abb2f65ab03dd
neutric <5984479+neutric@users.noreply.github.com>
2022-11-23 00:23:01 +0100

    Update tencent-docs from 2.3.25 to 2.4.0 (#136285)

--- Casks/tencent-docs.rb
+++ Casks/tencent-docs.rb
@@ -1,7 +1,7 @@
 cask "tencent-docs" do
   arch arm: "Arm64", intel: "X64"
 
-  version "2.3.25"
+  version "2.4.0"
   sha256 :no_check
 
   url "https://down.qq.com/qqweb/mac_docs/MacTencentDocs#{arch}.dmg"
