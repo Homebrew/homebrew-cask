18c0b863012ed53a6de8dbacb20f6875cc10b33d
Patrick Linnane <patrick@linnane.io>
2022-11-22 21:40:58 -0800

    Update trainerroad from 2022.41.0.212 to 2022.42.0.213

--- Casks/trainerroad.rb
+++ Casks/trainerroad.rb
@@ -1,6 +1,6 @@
 cask "trainerroad" do
-  version "2022.41.0.212"
-  sha256 "3f14a4491493cfdbfdb978e005c88c529804087167cf0f8595a28d0d3135dbc7"
+  version "2022.42.0.213"
+  sha256 "6298269184c165c73f64f5b9cb8934f517c4dfadbc9584ab502478b6b7d4369c"
 
   url "https://trainrdtrcmn01un1softw01.blob.core.windows.net/installers/mac/v001/Production/TrainerRoad-#{version}.dmg",
       verified: "trainrdtrcmn01un1softw01.blob.core.windows.net/"
