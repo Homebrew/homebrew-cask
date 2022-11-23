811c981dbd6f9e8d0e4ceea15ab070bc69bd4f26
Patrick Linnane <patrick@linnane.io>
2022-11-22 21:21:49 -0800

    Update mi from 3.7.1 to 3.7.2

--- Casks/mi.rb
+++ Casks/mi.rb
@@ -1,6 +1,6 @@
 cask "mi" do
-  version "3.7.1,1200"
-  sha256 "f467174263a4ac5e15a2354bf9539e87f91f71baa82be824a14de44c561b2b87"
+  version "3.7.2,1205"
+  sha256 "05e4ed319b5b1f6a5bdaa57b94901b9c60fb88c8be8dbb6d053fa377248d1be1"
 
   url "https://www.mimikaki.net/download/mi#{version.csv.first}.dmg"
   name "mi"
