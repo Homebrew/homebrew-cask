0e2402dab9ad3751532d1d0dd1205439e189e0df
Dan Berger <dberger@oubliette.org>
2022-11-23 08:57:39 +0000

    make changes recommended during PR review

--- Casks/cursr.rb
+++ Casks/cursr.rb
@@ -1,11 +1,11 @@
 cask "cursr" do
-  arch arm: "mac-arm64", intel: "mac-x64"
+  arch arm: "arm64", intel: "x64"
 
   version "1.4.11"
   sha256 arm:   "4552ea31b58f2b370c8891aae4bbf164095ddef1cffcc2507440fe0c331dcaed",
          intel: "5ed9db1eeee7697741c061333b344006190d83264e5bb2dd6096b35438c913ee"
 
-  url "https://github.com/bitgapp/Cursr/releases/download/v#{version}/Cursr-#{arch}.dmg",
+  url "https://github.com/bitgapp/Cursr/releases/download/v#{version}/Cursr-mac-#{arch}.dmg",
       verified: "github.com/bitgapp/Cursr/"
   name "Cursr.app"
   desc "Customize mouse movements between multiple displays"
