b0dde4bc368b619e33bc9e78460156737927943b
Andreas Jonsson <ajdev8@gmail.com>
2022-11-23 18:08:45 +0100

    duplicacy-web-edition: rebuild
    Add arm64 architecture for duplicacy-web-edition
    
    Closes #136367.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/duplicacy-web-edition.rb
+++ Casks/duplicacy-web-edition.rb
@@ -1,8 +1,11 @@
 cask "duplicacy-web-edition" do
-  version "1.6.3"
-  sha256 "39e96df1c43782632d1d600ed52e04de05226ff21e266d3d266de1be743679f3"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://acrosync.com/duplicacy-web/duplicacy_web_osx_x64_#{version}.dmg",
+  version "1.6.3"
+  sha256 arm:   "bf9d10dfc2faa7e43606281ec853e231aa009ae2c0b790ab85eadee8a1919708",
+         intel: "39e96df1c43782632d1d600ed52e04de05226ff21e266d3d266de1be743679f3"
+
+  url "https://acrosync.com/duplicacy-web/duplicacy_web_osx_#{arch}_#{version}.dmg",
       verified: "acrosync.com/duplicacy-web/"
   name "Duplicacy Web Edition"
   desc "Cloud backup tool"
@@ -10,7 +13,7 @@ cask "duplicacy-web-edition" do
 
   livecheck do
     url "https://duplicacy.com/download.html"
-    regex(%r{href=.*?/duplicacy[._-]web[._-]osx[._-]x64[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
+    regex(%r{href=.*?/duplicacy[._-]web[._-]osx[._-]#{arch}[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
   end
 
   app "Duplicacy Web Edition.app"
