31e18f17b649f754a49c8b7bae34d0b235753d4e
Patrick Linnane <patrick@linnane.io>
2022-11-22 19:21:24 -0800

    Update datovka from 4.21.0 to 4.21.1

--- Casks/datovka.rb
+++ Casks/datovka.rb
@@ -1,6 +1,6 @@
 cask "datovka" do
-  version "4.21.0"
-  sha256 "25eddc45e1257a0cafbfff8b89a69dcf8f2da6193ca2a5e2f44612d4699d475f"
+  version "4.21.1"
+  sha256 "7a2aa1aaca8d8d4daff0cfbb011a172df8e244207e9b6f6253d08ef8d275371f"
 
   url "https://secure.nic.cz/files/datove_schranky/#{version}/datovka-#{version}-64bit-macos10.12.dmg",
       verified: "secure.nic.cz/files/datove_schranky/"
