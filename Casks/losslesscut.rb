b8d8f259e03b517cbfaf025b68dead0d6fe658ae
redgarb <github@redgarb.de>
2022-11-24 16:13:52 +0100

    losslesscut: rebuild
    * Changes to cask losslesscut
      - adds possibility to install for arm64 and x86 architecture
    * New formatting from brew style --fix
    * Update Casks/losslesscut.rb
      Formatting arch stanza
    * Formatting arch stanza again
    * Update losslesscut.rb
    
    Closes #136434.
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Co-authored-by: redgarb <90768700+redgarb@users.noreply.github.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/losslesscut.rb
+++ Casks/losslesscut.rb
@@ -1,8 +1,11 @@
 cask "losslesscut" do
-  version "3.48.2"
-  sha256 "3f38c4d2824eb8f1b44be980ee980d53731181a1368c03f158437d2783dfa8c4"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://github.com/mifi/lossless-cut/releases/download/v#{version}/LosslessCut-mac-x64.dmg"
+  version "3.48.2"
+  sha256 arm:   "f76ec249cce014971b0499b67185ee705462764180b5ca30a0b3259b619369f7",
+         intel: "3f38c4d2824eb8f1b44be980ee980d53731181a1368c03f158437d2783dfa8c4"
+
+  url "https://github.com/mifi/lossless-cut/releases/download/v#{version}/LosslessCut-mac-#{arch}.dmg"
   name "LosslessCut"
   desc "Trims video and audio files losslessly"
   homepage "https://github.com/mifi/lossless-cut"
