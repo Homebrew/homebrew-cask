d817b25733ec539c3f9d08bc3a94232f76fae3ba
Patrick Linnane <patrick@linnane.io>
2022-11-23 20:51:18 -0800

    Update losslesscut from 3.47.1 to 3.48.2

--- Casks/losslesscut.rb
+++ Casks/losslesscut.rb
@@ -1,6 +1,6 @@
 cask "losslesscut" do
-  version "3.47.1"
-  sha256 "fbe826ba822fd16b973ad78e699ac613b818e61b3a23cd5da05af238be2e6e36"
+  version "3.48.2"
+  sha256 "3f38c4d2824eb8f1b44be980ee980d53731181a1368c03f158437d2783dfa8c4"
 
   url "https://github.com/mifi/lossless-cut/releases/download/v#{version}/LosslessCut-mac-x64.dmg"
   name "LosslessCut"
