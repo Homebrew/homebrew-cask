381c716e986f059fa81067ce799e8bf985c47064
Benjamin Reich <dev@benjaminreich.de>
2022-11-24 11:43:58 +0100

    spotify 1.1.99.878,1e4ccc6e,2664
    * Update spotify from 1.1.98.691 to 1.1.99.878
    * Update spotify.rb
    
    Closes #136431.
    
    Co-authored-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/spotify.rb
+++ Casks/spotify.rb
@@ -4,10 +4,10 @@ cask "spotify" do
   sha256 :no_check
 
   on_intel do
-    version "1.1.98.691,f759311c,1417"
+    version "1.1.99.878,1e4ccc6e,2664"
   end
   on_arm do
-    version "1.1.98.691,f759311c,1412"
+    version "1.1.99.878,1e4ccc6e,2660"
   end
 
   url "https://download.scdn.co/Spotify#{arch}.dmg",
