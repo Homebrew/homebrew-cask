4f324833deebaf2d3b7235c619f40040c1c6aabc
Patrick Linnane <patrick@linnane.io>
2022-11-25 09:31:16 -0800

    Update burp-suite from 2022.9.6 to 2022.11.2

--- Casks/burp-suite.rb
+++ Casks/burp-suite.rb
@@ -1,9 +1,9 @@
 cask "burp-suite" do
   arch arm: "MacOsArm64", intel: "MacOsx"
 
-  version "2022.9.6"
-  sha256 arm:   "ed893b4b6c0231c15e8d9b80ce124d9446e4b6465fc2d8534cadf6275ececd0b",
-         intel: "72c743f1c8a4f812209ff75d81a9bb1a6ecae24ef9e1a14ceafb13ec9dc88f2a"
+  version "2022.11.2"
+  sha256 arm:   "ea18adfa0ce90e6c432f7961c45954e324b1be883d713dc55ea171e51c290ef1",
+         intel: "efe5b8aae4565107f0a3d5c1746fea1cb33cec963426975dde39416b96f5ea12"
 
   url "https://portswigger.net/burp/releases/download?product=community&version=#{version}&type=#{arch}"
   name "Burp Suite Community Edition"
