1eb21a82e9e6128d0c312254499616c7d9b55c38
yolossn <yolossn@users.noreply.github.com>
2022-11-25 06:03:04 +0000

    headlamp 0.14.0
    Update Headlamp version to 0.14.0
    
    Closes #136444.
    
    Signed-off-by: yolossn <yolossn@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/headlamp.rb
+++ Casks/headlamp.rb
@@ -1,9 +1,9 @@
 cask "headlamp" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.13.0"
-  sha256 arm:   "062068070eefe91aed577ce743943905f0524e32071b1a1e5f28de4ebe539961",
-         intel: "d4080130c5940a011bdc48798267c1416715e7e747e21bfd7d890d34aa1971f4"
+  version "0.14.0"
+  sha256 arm:   "3222ad49660dedb022ea270fb19157564d58769308dbe5120710543a17fef86c",
+         intel: "f7d362053186c6bb322cab5a8718c2fc41f2c3fd14783820dfb4ddd06aa4bb94"
 
   url "https://github.com/kinvolk/headlamp/releases/download/v#{version}/Headlamp-#{version}-mac-#{arch}.dmg",
       verified: "github.com/kinvolk/headlamp/"
