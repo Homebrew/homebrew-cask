7d0a0fb0d0cea9e90163b2a97ab7754c552d716f
Patrick Linnane <patrick@linnane.io>
2022-11-25 10:06:41 -0800

    tomatobar 3.4.2a
    Update tomatobar from 3.4.1 to 3.4.2a
    
    Closes #136509.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/tomatobar.rb
+++ Casks/tomatobar.rb
@@ -1,6 +1,6 @@
 cask "tomatobar" do
-  version "3.4.1"
-  sha256 "ec5f3a6dfe131734fb52da36e2c7dbcbc54bbd16ed412b774cafef06fb80a7c9"
+  version "3.4.2a"
+  sha256 "c7620fa9f8d8c9202a90e98bfc978956454a2f3793a664d9d315d642b11b80c0"
 
   url "https://github.com/ivoronin/TomatoBar/releases/download/v#{version}/TomatoBar-v#{version}.zip"
   name "TomatoBar"
