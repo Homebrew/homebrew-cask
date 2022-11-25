6a2b66fb5f80d63507c75b19313d3df12cc9f21f
Hannes Braun <hannesbraun@mail.de>
2022-11-25 15:52:44 +0100

    Update vcv-rack from 2.1.2 to 2.2.0

--- Casks/vcv-rack.rb
+++ Casks/vcv-rack.rb
@@ -1,8 +1,8 @@
 cask "vcv-rack" do
-  version "2.1.2"
-  sha256 "9be9208b9a9ba9aedc7a4ca7f47893f4cbd94178146a0308484559a218c71c27"
+  version "2.2.0"
+  sha256 "f2db0877d165be94f495291950a122f09d213e51f393cd69626ef21f264f7db7"
 
-  url "https://vcvrack.com/downloads/RackFree-#{version}-mac.pkg"
+  url "https://vcvrack.com/downloads/RackFree-#{version}-mac-x64.pkg"
   name "VCV Rack"
   desc "Open-source virtual modular synthesizer"
   homepage "https://vcvrack.com/"
@@ -12,7 +12,7 @@ cask "vcv-rack" do
     regex(/###\s(\d+(?:\.\d+)+)/i)
   end
 
-  pkg "RackFree-#{version}-mac.pkg"
+  pkg "RackFree-#{version}-mac-x64.pkg"
 
   uninstall pkgutil: "com.vcvrack.rack"
 end
