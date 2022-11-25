adf6c7252365583bb7082146a07ed3b5b2ef57a4
Patrick Linnane <patrick@linnane.io>
2022-11-25 10:07:06 -0800

    Update plasticscm-cloud-edition from 11.0.16.7626 to 11.0.16.7649

--- Casks/plasticscm-cloud-edition.rb
+++ Casks/plasticscm-cloud-edition.rb
@@ -1,6 +1,6 @@
 cask "plasticscm-cloud-edition" do
-  version "11.0.16.7626"
-  sha256 "a0986c6f5d8566d4e99e19c297b785f5b6cbcb3c0eb78769271b779c19f0f53c"
+  version "11.0.16.7649"
+  sha256 "d5543213f997ace4ccf846e43f8eacbc86958ab53c784e89533722c3fca924b0"
 
   url "https://s3.eu-west-2.amazonaws.com/plastic-releases/releases/#{version}/plasticscm/osx/plasticscm-cloud-#{version}.pkg.zip",
       verified: "s3.eu-west-2.amazonaws.com/plastic-releases/"
