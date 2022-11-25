b4d0dfd44f6631ef80bf77b5dbffda525f0a7cf3
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-11-25 23:40:38 +0800

    utools 3.2.0
    Update utools from 3.1.0 to 3.2.0
    
    Closes #136455.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/utools.rb
+++ Casks/utools.rb
@@ -1,9 +1,9 @@
 cask "utools" do
   arch arm: "-arm64"
 
-  version "3.1.0"
-  sha256 arm:   "f6a1d4f52ea1c7c1621c85c3fd89e5f1c5ba954073a24784e52ec5df6e8fac54",
-         intel: "2d73cfb1a5b06758beffa1ade9ced5d6d4feb9ea03916726927dd0f5e484cbfa"
+  version "3.2.0"
+  sha256 arm:   "dfd8eb843ef7a489b991fea6a6b6a63aa610fc12e05ef0b2200b432e524c454a",
+         intel: "67b10336ea178bf66801e2dc52b58ec570b6d6762d000d1b23b7ed2582b38f38"
 
   url "https://publish.u-tools.cn/version2/uTools-#{version}#{arch}.dmg",
       verified: "publish.u-tools.cn/"
