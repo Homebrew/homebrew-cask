465adf70c9987238a2cb90687bc8fc054aa1d92f
Patrick Linnane <patrick@linnane.io>
2022-11-25 09:43:00 -0800

    Update codeql from 2.11.3 to 2.11.4

--- Casks/codeql.rb
+++ Casks/codeql.rb
@@ -1,6 +1,6 @@
 cask "codeql" do
-  version "2.11.3"
-  sha256 "82f21d034e6c84b527b2af32e84cfacfc58296aed30d717657a6347cc24ce9df"
+  version "2.11.4"
+  sha256 "d2af01639c3823249b111696f79dc611e08a7b1799b034b3cb304fa21f09bed9"
 
   url "https://github.com/github/codeql-cli-binaries/releases/download/v#{version}/codeql-osx64.zip"
   name "CodeQL"
