9cd619f914c2ac1ea4846eca11d4c3f00dafd2e9
ivaquero <46377665+ivaquero@users.noreply.github.com>
2022-11-26 23:10:04 +0800

    brew style --fix

--- Casks/siyuan.rb
+++ Casks/siyuan.rb
@@ -3,7 +3,7 @@ cask "siyuan" do
 
   version "2.5.1"
   sha256 arm:   "3d9e3916a314c527c1dc54555014ed92af3dc098e2b8b851ad7a36d92e9e95eb",
-  intel: "55ef77700bbb8ff5e3d9a5d77351e4b3c2e6eb8cbb3e084723b3aaeb308b9b27"
+         intel: "55ef77700bbb8ff5e3d9a5d77351e4b3c2e6eb8cbb3e084723b3aaeb308b9b27"
 
   url "https://github.com/siyuan-note/siyuan/releases/download/v#{version}/siyuan-#{version}-mac#{arch}.dmg"
   name "SiYuan"
