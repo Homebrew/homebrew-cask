f8f9ad8e732d3d29dccd6df6f62743d7c659a22d
Martin Stemmle <hi@martn.st>
2022-11-26 18:36:24 +0100

    Add MaxSnap.app v1.74

---
+++ Casks/maxsnap.rb
@@ -0,0 +1,11 @@
+cask "maxsnap" do
+  version "1.74"
+  sha256 :no_check
+
+  name "MaxSnap"
+  url "https://noteifyapp.com/download/MaxSnap.dmg"
+  desc "Powerful Window Manager for Mac, Alternative to SizeUp, Divvy and Moom."
+  homepage "https://noteifyapp.com/maxsnap-mac-window-manager/"
+
+  app "MaxSnap.app"
+end
