a575b9760e8efee04b1677220b81d8b1f6aabb99
ivaquero <46377665+ivaquero@users.noreply.github.com>
2022-11-26 23:02:26 +0800

    siyuan: rebuild
    * siyuan: add arm64
    * brew style --fix
    
    Closes #136552.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/siyuan.rb
+++ Casks/siyuan.rb
@@ -1,8 +1,11 @@
 cask "siyuan" do
-  version "2.5.1"
-  sha256 "55ef77700bbb8ff5e3d9a5d77351e4b3c2e6eb8cbb3e084723b3aaeb308b9b27"
+  arch arm: "-arm64"
 
-  url "https://github.com/siyuan-note/siyuan/releases/download/v#{version}/siyuan-#{version}-mac.dmg"
+  version "2.5.1"
+  sha256 arm:   "3d9e3916a314c527c1dc54555014ed92af3dc098e2b8b851ad7a36d92e9e95eb",
+         intel: "55ef77700bbb8ff5e3d9a5d77351e4b3c2e6eb8cbb3e084723b3aaeb308b9b27"
+
+  url "https://github.com/siyuan-note/siyuan/releases/download/v#{version}/siyuan-#{version}-mac#{arch}.dmg"
   name "SiYuan"
   desc "Local-first personal knowledge management system"
   homepage "https://github.com/siyuan-note/siyuan"
