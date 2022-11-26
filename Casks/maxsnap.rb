50b69c90e64d76f30ae1a488177618e88f93dff0
Martin Stemmle <hi@martn.st>
2022-11-26 18:53:40 +0100

    Fix style of Cask for MaxSnap.app

--- Casks/maxsnap.rb
+++ Casks/maxsnap.rb
@@ -2,9 +2,9 @@ cask "maxsnap" do
   version "1.74"
   sha256 :no_check
 
-  name "MaxSnap"
   url "https://noteifyapp.com/download/MaxSnap.dmg"
-  desc "Powerful Window Manager for Mac, Alternative to SizeUp, Divvy and Moom."
+  name "MaxSnap"
+  desc "Powerful Window Manager, Alternative to SizeUp, Divvy and Moom"
   homepage "https://noteifyapp.com/maxsnap-mac-window-manager/"
 
   app "MaxSnap.app"
