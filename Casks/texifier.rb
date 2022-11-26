d13593811c27478e433b07eaf5922cb14db2149b
Patrick Linnane <patrick@linnane.io>
2022-11-25 17:27:06 -0800

    Update texifier from 1.9.12 to 1.9.16 (#136508)
    * texifier 1.9.16, add min OS
    
    * Update texifier.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/texifier.rb
+++ Casks/texifier.rb
@@ -1,6 +1,6 @@
 cask "texifier" do
-  version "1.9.12,700,bb0511f"
-  sha256 "68e54874158c56dcc9e722ae62e540041a960c5f15e00793800c5b6c70a13823"
+  version "1.9.16,726,f98abbd"
+  sha256 "a7d14c989fda7b6ba4147f5dab2aefaea7ba3e418e64e29cd74706d3ab12e869"
 
   url "https://download.texifier.com/apps/osx/updates/Texifier_#{version.csv.first.dots_to_underscores}__#{version.csv.second}__#{version.csv.third}.dmg"
   name "Texifier"
@@ -8,13 +8,14 @@ cask "texifier" do
   homepage "https://www.texifier.com/mac"
 
   livecheck do
-    url "https://www.texifier.com/apps/updates/texifier/stable-appcast.xml"
+    url "https://www.texifier.com/apps/updates/texifier/appcast-stable.xml"
     strategy :sparkle do |item|
       "#{item.short_version},#{item.version},#{item.url[/_([^_]+)\.dmg/i, 1]}"
     end
   end
 
   auto_updates true
+  depends_on macos: ">= :big_sur"
 
   app "Texifier.app"
 
