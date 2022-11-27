f04d214c750fd6df76bfed66413071caca75d27b
Patrick Linnane <patrick@linnane.io>
2022-11-27 14:24:34 -0800

    Update dcp-o-matic-encode-server from 2.16.32 to 2.16.34

--- Casks/dcp-o-matic-encode-server.rb
+++ Casks/dcp-o-matic-encode-server.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-encode-server" do
-  version "2.16.32"
-  sha256 "b7277297d758574a4bea64a1b364083cd7ecf2c5f3f028698a957415ef59e88e"
+  version "2.16.34"
+  sha256 "740b6610e660515e890dd62af564c0bbdd1531360b4c35311fa53e19764105d2"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-server&version=#{version}"
   name "DCP-o-matic Encode Server"
