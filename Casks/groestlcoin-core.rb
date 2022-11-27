15381c55a9401e8b87ef246782b9e7a939e194a9
Patrick Linnane <patrick@linnane.io>
2022-11-27 14:34:55 -0800

    groestlcoin-core 24.0
    Update groestlcoin-core from 23.0 to 24.0
    
    Closes #136596.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/groestlcoin-core.rb
+++ Casks/groestlcoin-core.rb
@@ -1,9 +1,9 @@
 cask "groestlcoin-core" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "23.0"
-  sha256 arm:   "7df0f66ee01e0f2e5faade6bd900e4a96f970cc56043a1a428dd9bf34dc5777b",
-         intel: "82e6a1862974ca51bd65aeb79f3e5eb5327b5da1f687921e9fb573c83293051c"
+  version "24.0"
+  sha256 arm:   "4bbb9b3c73d383ed129f5662f8bb879f3da7f3ffc657afe86f75d29e6d20fee3",
+         intel: "0e7c264732b2d44d946b5267f36d8d3c3952821dcb8676c624b30aacdc687214"
 
   url "https://github.com/groestlcoin/groestlcoin/releases/download/v#{version}/groestlcoin-#{version}-#{arch}-apple-darwin.dmg",
       verified: "github.com/groestlcoin/groestlcoin/"
