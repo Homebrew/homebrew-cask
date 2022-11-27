b4890e9eb86c3b68ee64c50f227350167ddbe103
Patrick Linnane <patrick@linnane.io>
2022-11-27 11:54:18 -0800

    fix livechecks & names

--- Casks/rjmmusic-mastermind-gt-editor.rb
+++ Casks/rjmmusic-mastermind-gt-editor.rb
@@ -3,12 +3,12 @@ cask "rjmmusic-mastermind-gt-editor" do
   sha256 "10e37d4443b107eae1f735a8278fbb06d9776c13ec9626703d02757718086cee"
 
   url "https://www.rjmmusic.com/downloads/MMGT/MMGTEditor-#{version}.dmg"
-  name "rjmmusic-mastermind-gt-editor"
+  name "RJ Music Mastermind GT Editor"
   desc "Editor software to configure RJM Mastermind GT midi pedalboard"
   homepage "https://www.rjmmusic.com/"
 
   livecheck do
-    url "https://www.rjmmusic.com/downloads"
+    url "https://www.rjmmusic.com/wp-json/wp/v2/pages/6614"
     regex(/href=.*?MMGTEditor-(\d+(?:\.\d+)+)\.dmg/i)
   end
 
--- Casks/rjmmusic-mini-effect-gizmo-x-editor.rb
+++ Casks/rjmmusic-mini-effect-gizmo-x-editor.rb
@@ -3,12 +3,12 @@ cask "rjmmusic-mini-effect-gizmo-x-editor" do
   sha256 "a08d3f34b26dc8d1153319af64410c770c38abc4e29ed1ebd728bf7c14121c25"
 
   url "https://www.rjmmusic.com/downloads/MEGX/MEGXEditor-#{version}.dmg"
-  name "rjmmusic-mini-effect-gizmo-x-editor"
+  name "RJ Music Mini Effect Gizmo X Editor"
   desc "Editor software to configure RJM mini effect gizmo X MIDI audio loop switcher"
   homepage "https://www.rjmmusic.com/"
 
   livecheck do
-    url "https://www.rjmmusic.com/downloads"
+    url "https://www.rjmmusic.com/wp-json/wp/v2/pages/6614"
     regex(/href=.*?MEGXEditor-(\d+(?:\.\d+)+)\.dmg/i)
   end
 
