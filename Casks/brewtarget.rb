18b3bf548017cac70d464b96ff03b3bad8ccbc8f
Patrick Linnane <patrick@linnane.io>
2022-11-27 14:06:37 -0800

    Update brewtarget from 3.0.3 to 3.0.4

--- Casks/brewtarget.rb
+++ Casks/brewtarget.rb
@@ -1,11 +1,11 @@
 cask "brewtarget" do
-  version "3.0.3"
-  sha256 "f450f050801da57b58107e7ae955235c50ac4cc00380792b34e25a5dce4602a2"
+  version "3.0.4"
+  sha256 "1ae54e638c58ea24e69770b54dd3ee6e020716b2966d7e73f764bac7cd849e2c"
 
   url "https://github.com/Brewtarget/brewtarget/releases/download/v#{version}/brewtarget_#{version}_x86_64.dmg"
   name "brewtarget"
   desc "Beer recipe creation tool"
   homepage "https://github.com/Brewtarget/brewtarget/"
 
-  app "brewtarget_#{version}.app"
+  app "Brewtarget.app"
 end
