0e20dd4eacaa6e8cb0ec238b5852fde76b02c6c5
Patrick Linnane <patrick@linnane.io>
2022-11-27 14:32:23 -0800

    eloston-chromium 107.0.5304.122-1.1,1669477210
    Update eloston-chromium from 107.0.5304.110-1.1 to 107.0.5304.122-1.1
    
    Closes #136589.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/eloston-chromium.rb
+++ Casks/eloston-chromium.rb
@@ -2,12 +2,12 @@ cask "eloston-chromium" do
   arch arm: "arm64", intel: "x86-64"
 
   on_intel do
-    version "107.0.5304.110-1.1,1668258667"
-    sha256 "d0cf5f9d653199832702a1a004a8495d1238eef1c5fd2fa833fcd15f771f3d1e"
+    version "107.0.5304.122-1.1,1669477210"
+    sha256 "c9a9c151eb9b8af842b0a595af635b62f2d393993a1b0eebe8a36c675f95ad66"
   end
   on_arm do
-    version "107.0.5304.110-1.1,1668310949"
-    sha256 "182a92a86eedc5cc21275a1ae05b159e2aa2d00a22f6e1d0cabd3b761eb0896f"
+    version "107.0.5304.122-1.1,1669524672"
+    sha256 "107c2d79e80c5d1e01d514e85352963ceac7ccb70ce13d6e86adcf2dd4ef92f9"
   end
 
   url "https://github.com/kramred/ungoogled-chromium-macos/releases/download/#{version.csv.first}_#{arch}__#{version.csv.second}/ungoogled-chromium_#{version.csv.first}_#{arch}-macos.dmg",
