0e9a9c8f83d247870a3714ccb47ce4f2fc4ed77b
Miccal Matthews <miccal.matthews@gmail.com>
2022-11-28 09:18:06 +0800

    Update and rename rjmmusic-mini-effect-gizmo-x-editor.rb to megxeditor.rb

--- Casks/megxeditor.rb
+++ Casks/megxeditor.rb
@@ -1,4 +1,4 @@
-cask "rjmmusic-mini-effect-gizmo-x-editor" do
+cask "megxeditor" do
   version "1.1.1"
   sha256 "a08d3f34b26dc8d1153319af64410c770c38abc4e29ed1ebd728bf7c14121c25"
 
@@ -9,7 +9,7 @@ cask "rjmmusic-mini-effect-gizmo-x-editor" do
 
   livecheck do
     url "https://www.rjmmusic.com/wp-json/wp/v2/pages/6614"
-    regex(/href=.*?MEGXEditor-(\d+(?:\.\d+)+)\.dmg/i)
+    regex(/href=.*?MEGXEditor[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
   app "MEGXEditor.app"
