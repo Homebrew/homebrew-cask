9bb659334b1af50b1117f4abfe2aff8934e5ac5d
ransome <11188741+ransome1@users.noreply.github.com>
2022-11-28 09:15:02 +0100

    Update sleek to 1.3.0

--- Casks/sleek.rb
+++ Casks/sleek.rb
@@ -1,6 +1,6 @@
 cask "sleek" do
-  version "1.2.9"
-  sha256 "09eaf11fbd7440ba32e127e08b2fbce75d7313af4ac43735053622856967f1d1"
+  version "1.3.0"
+  sha256 "d436cc7d75285f11ae56fcc6b3c73b38178927096d880856e43663e6e7867ebc"
 
   url "https://github.com/ransome1/sleek/releases/download/v#{version}/sleek-#{version}-mac.dmg"
   name "sleek"
