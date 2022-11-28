2143817f128ad5c6c74d95d8667afb7282437895
Miccal Matthews <miccal.matthews@gmail.com>
2022-11-28 09:17:23 +0800

    Update and rename rjmmusic-mastermind-gt-editor.rb to mmgteditor.rb

--- Casks/mmgteditor.rb
+++ Casks/mmgteditor.rb
@@ -1,4 +1,4 @@
-cask "rjmmusic-mastermind-gt-editor" do
+cask "mmgteditor" do
   version "4.9.1"
   sha256 "10e37d4443b107eae1f735a8278fbb06d9776c13ec9626703d02757718086cee"
 
@@ -9,7 +9,7 @@ cask "rjmmusic-mastermind-gt-editor" do
 
   livecheck do
     url "https://www.rjmmusic.com/wp-json/wp/v2/pages/6614"
-    regex(/href=.*?MMGTEditor-(\d+(?:\.\d+)+)\.dmg/i)
+    regex(/href=.*?MMGTEditor[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
   app "MMGTEditor.app"
