1df025ccddc3651993093ace586997711f8752c7
Bevan Kay <email@bevankay.me>
2022-11-29 10:59:03 +1100

    Update reaper.rb

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -2,7 +2,7 @@ cask "reaper" do
   version "6.71"
 
   if MacOS.version <= :mojave
-    sha256 "7dd5b5619a3183d522007248d68d8d5020002341c45bed6fd130c2bfe0e78e49"
+    sha256 "1d2cd9bd0012cec3120f6711bb97336f3b1eff059fa0bf42742eb3730834ab99"
     url "https://dlcf.reaper.fm/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64.dmg"
   else
     sha256 "b6f1effd0bcee4ee56df7d239eadcf3251b54e3760699fedfc7856669f6e0ce5"
