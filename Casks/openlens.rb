21b8a68d970de38607006b4350f977699ebfc2ce
Adriaan Knapen <hi@aknapen.nl>
2022-11-28 15:17:46 +0100

    openlens: checksum update
    Update openlens from 6.2.3 to 6.2.3
    
    Closes #136629.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/openlens.rb
+++ Casks/openlens.rb
@@ -2,8 +2,8 @@ cask "openlens" do
   arch arm: "-arm64"
 
   version "6.2.3"
-  sha256 arm:   "e6206bf31d5588e647f088e82c88d7210c7dcb1705c4046afae3b0128acd47f4",
-         intel: "3f16ba5def6d239be21b9ff587847ad64f2f845924cc97de395fd033fe6d7542"
+  sha256 arm:   "a77cb765844295a81c0e5000f10c1271d07f3506cd577bf5ed94671c61075dac",
+         intel: "c95def0c10336ae0371670b5f6b14ecdfaa78df04104338b9dcd7c5dfa2cbd2c"
 
   url "https://github.com/MuhammedKalkan/OpenLens/releases/download/v#{version}/OpenLens-#{version}#{arch}.dmg"
   name "OpenLens"
