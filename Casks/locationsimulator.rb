a099d72300a47a4128a3ac8a51f412aeeb1a60b4
Patrick Linnane <patrick@linnane.io>
2022-11-28 20:38:12 -0800

    Update locationsimulator from 0.1.9.5 to 0.1.9.6

--- Casks/locationsimulator.rb
+++ Casks/locationsimulator.rb
@@ -1,6 +1,6 @@
 cask "locationsimulator" do
-  version "0.1.9.5"
-  sha256 "7e1d490664e17c099d5f3f6a600b9211d013d511eef482b931c40812cf274617"
+  version "0.1.9.6"
+  sha256 "6380425b63188ead9240ad36a9b3a7d75ca2b5d159fdd2b77210c36ade0cd43a"
 
   url "https://github.com/Schlaubischlump/LocationSimulator/releases/download/v#{version}/LocationSimulator.app.zip"
   name "LocationSimulator"
