3735d7e23598fcf6eaea2e477076e6af79199a3e
Johannes Klein <j.klein@meddevo.com>
2022-11-29 17:32:57 +0100

    bitwarden 2022.11.0
    Update bitwarden from 2022.10.1 to 2022.11.0
    
    Closes #136699.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/bitwarden.rb
+++ Casks/bitwarden.rb
@@ -1,6 +1,6 @@
 cask "bitwarden" do
-  version "2022.10.1"
-  sha256 "e8ef3168605a1b877a97d411b46339c23f4e08c172f3b747f0dd4c7c40418dc9"
+  version "2022.11.0"
+  sha256 "e6392b45d1870f9a01463e14059562ff8fa7d927865dee77e779e6636abdc019"
 
   url "https://github.com/bitwarden/clients/releases/download/desktop-v#{version}/Bitwarden-#{version}-universal.dmg",
       verified: "github.com/bitwarden/clients/"
