0a28ffbf01142eabcab202d7e26be404da9ea1a2
Tin Htet Kyaw <tin@lsvp.com>
2022-11-29 11:12:15 -0800

    waterfox-classic: checksum update
    Update waterfox-classic from 2022.11 to 2022.11
    
    Closes #136701.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/waterfox-classic.rb
+++ Casks/waterfox-classic.rb
@@ -1,6 +1,6 @@
 cask "waterfox-classic" do
   version "2022.11"
-  sha256 "ab9ea75eac79efa8da273c1a44798b77a71a9e7633bb70a69af3b7f50b44544a"
+  sha256 "507151508846e9ea09e695322b9d70ca48dc23d120ab9ac75b899596c1863ff2"
 
   url "https://github.com/WaterfoxCo/Waterfox-Classic/releases/download/#{version}-classic/Waterfox.Classic.#{version}.Setup.dmg",
       verified: "github.com/WaterfoxCo/Waterfox-Classic/"
