24102a74af0f38f3a644f9fb9d68c0ddfd05d58c
Patrick Linnane <patrick@linnane.io>
2022-11-30 12:31:51 -0800

    Update ledger-live from 2.49.2 to 2.50.0

--- Casks/ledger-live.rb
+++ Casks/ledger-live.rb
@@ -1,6 +1,6 @@
 cask "ledger-live" do
-  version "2.49.2"
-  sha256 "606a7ab4e32f16f971991a362e1b5948a88d9835424e24dd115a17d28e238ae0"
+  version "2.50.0"
+  sha256 "829c13635e8242a39ab4bea2ba363e7797ab322d46aec6b439851f7c802f04ed"
 
   url "https://download.live.ledger.com/ledger-live-desktop-#{version}-mac.dmg"
   name "Ledger Live"
