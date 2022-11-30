b4c02ba0a7dd54f652480fd3dd591a3818352362
Patrick Linnane <patrick@linnane.io>
2022-11-30 12:45:27 -0800

    openshift-client 4.11.18
    Update openshift-client from 4.11.17 to 4.11.18
    
    Closes #136812.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/openshift-client.rb
+++ Casks/openshift-client.rb
@@ -1,9 +1,9 @@
 cask "openshift-client" do
   arch arm: "-arm64"
 
-  version "4.11.17"
-  sha256 arm:   "48dba025a8c0bca9c358e89e4d5c4083a44a54400fbc4920c0ca7bba4c50f2e9",
-         intel: "17a1bcb309947f51aa4d57f2ca7526f9479d6790d4897e066bad096711820d5f"
+  version "4.11.18"
+  sha256 arm:   "7989fa72cd9935b2931519932dbbc7fc41e1fdda0346df358930ff1f4ca21de7",
+         intel: "a2d839d7ce7dcf74422d10f465bf5d46278d99ae79961bdec05cef1ffe3e7d6c"
 
   url "https://mirror.openshift.com/pub/openshift-v#{version.major}/clients/ocp/#{version}/openshift-client-mac#{arch}.tar.gz"
   name "Openshift Client"
