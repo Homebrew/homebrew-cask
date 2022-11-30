47d71795c60f82b16e3c46f81882d6544c4d4343
Patrick Linnane <patrick@linnane.io>
2022-11-30 11:38:34 -0800

    chromedriver 108.0.5359.71
    Update chromedriver from 107.0.5304.62 to 108.0.5359.71
    
    Closes #136790.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/chromedriver.rb
+++ Casks/chromedriver.rb
@@ -1,9 +1,9 @@
 cask "chromedriver" do
   arch arm: "mac_arm64", intel: "mac64"
 
-  version "107.0.5304.62"
-  sha256 arm:   "fc409b4a55fdb1c02b7c4c82236b87e35679c43993182d8dd59c798c4231d905",
-         intel: "3a1e597791ea186e831911b280620c49381bb59910969cda9a9432f27b2a8d2b"
+  version "108.0.5359.71"
+  sha256 arm:   "6c71bcea979a471a71e3647199963aa79c34cf3d0b4bb1b3457c7c6b995620bf",
+         intel: "b5f92f52bd993621a6e5983ffccda81176bcd1089ee9835e0841645ca53f6e30"
 
   url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_#{arch}.zip",
       verified: "chromedriver.storage.googleapis.com/"
