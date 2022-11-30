b3975b448e18786e5de7bddd443b42b699066f01
Patrick Linnane <patrick@linnane.io>
2022-11-29 17:22:36 -0800

    Update turbovnc-viewer from 3.0.1 to 3.0.2

--- Casks/turbovnc-viewer.rb
+++ Casks/turbovnc-viewer.rb
@@ -1,9 +1,9 @@
 cask "turbovnc-viewer" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "3.0.1"
-  sha256 arm:   "76f451817884185f5832dc794831fbfb62d50104a6b416d4c78c6d55ea4b0f12",
-         intel: "0b9f083d753dc65c0a023d8d9c6c95da997184645ea216b60a8e1db72f8baccb"
+  version "3.0.2"
+  sha256 arm:   "fbd3acc5a9f586dd372d8ce327340db7628f0f6c1e5358c140b1876b2539a7d8",
+         intel: "1603d7eb714a24269c91aec96b0c351969ebcabb8f397f0ca34b9a9974bf3692"
 
   url "https://downloads.sourceforge.net/turbovnc/#{version}/TurboVNC-#{version}-#{arch}.dmg",
       verified: "sourceforge.net/turbovnc/"
