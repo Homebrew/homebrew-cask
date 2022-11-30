af16119fed8bf6b8d0e2a3fc983f571613ca1c6f
Patrick Linnane <patrick@linnane.io>
2022-11-29 20:54:08 -0800

    geneious-prime 2023.0.1
    * Update geneious-prime from 2022.2.2 to 2023.0.1
    * fix livecheck
    
    Closes #136774.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/geneious-prime.rb
+++ Casks/geneious-prime.rb
@@ -1,6 +1,6 @@
 cask "geneious-prime" do
-  version "2022.2.2"
-  sha256 "59412a6b0bb5a7b240582e7185d5733cfeca6d84d90ebf6c484d6506e8dce44b"
+  version "2023.0.1"
+  sha256 "3ef8f65e123309d9803496ebcd5840bee522f45446c6136ef3d40f45d0a29a6b"
 
   url "https://assets.geneious.com/installers/geneious/release/Geneious_Prime_mac64_#{version.dots_to_underscores}_with_jre.dmg"
   name "Geneious Prime"
@@ -8,8 +8,8 @@ cask "geneious-prime" do
   homepage "https://www.geneious.com/"
 
   livecheck do
-    url "https://www.geneious.com/download/"
-    regex(/data-version="Prime\s*(\d+(?:\.\d+)+)/i)
+    url "https://assets.geneious.com/documentation/geneious/release_notes.html"
+    regex(/href="#v?(\d+(?:\.\d+)+)">/i)
   end
 
   app "Geneious Prime.app"
