225c82e87193dc06001007912c28903487710319
Patrick Linnane <patrick@linnane.io>
2022-11-30 22:41:52 -0800

    Update keeper-password-manager from 16.8.3 to 16.8.4

--- Casks/keeper-password-manager.rb
+++ Casks/keeper-password-manager.rb
@@ -1,5 +1,5 @@
 cask "keeper-password-manager" do
-  version "16.8.3,1669717336726"
+  version "16.8.4,1669839841729"
   sha256 :no_check
 
   url "https://keepersecurity.com/desktop_electron/Darwin/KeeperSetup.dmg"
