bed75c73ccba432a97f933e839a9043cf6d7370d
Nihaal Sangha <nihaal.git@gmail.com>
2022-12-02 18:52:25 +0000

    Add Superkey v0.10 (#136705)
    * Add Superkey v0.10
    
    * Update superkey.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/superkey.rb
@@ -0,0 +1,32 @@
+cask "superkey" do
+  version "0.10"
+  sha256 "069bcfcbedc2148cdb96c7e24bebc724d7d0d7fbc0f9c9084fb7cb6eb6cfdcd8"
+
+  url "https://superkey.app/downloads/Superkey#{version}.dmg"
+  name "Superkey"
+  desc "Search and click text anywhere on screen"
+  homepage "https://superkey.app/"
+
+  livecheck do
+    url "https://superkey.app/downloads/updates.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+  depends_on macos: ">= :monterey"
+
+  app "Superkey.app"
+
+  uninstall quit: "com.knollsoft.Superkey"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.knollsoft.SuperkeyLauncher",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.knollsoft.superkeylauncher.sfl2",
+    "~/Library/Application Support/Superkey",
+    "~/Library/Caches/com.knollsoft.Superkey",
+    "~/Library/Containers/com.knollsoft.SuperkeyLauncher",
+    "~/Library/HTTPStorages/com.knollsoft.Superkey",
+    "~/Library/HTTPStorages/com.knollsoft.Superkey.binarycookies",
+    "~/Library/Preferences/com.knollsoft.Superkey.plist",
+  ]
+end
