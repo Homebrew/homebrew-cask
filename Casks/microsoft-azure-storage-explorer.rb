0a23cf379c88f684f2c0cb72dea8df525e0e46f7
Patrick Linnane <patrick@linnane.io>
2022-12-02 12:11:02 -0800

    Update microsoft-azure-storage-explorer from 1.26.1 to 1.27.0

--- Casks/microsoft-azure-storage-explorer.rb
+++ Casks/microsoft-azure-storage-explorer.rb
@@ -1,6 +1,6 @@
 cask "microsoft-azure-storage-explorer" do
-  version "1.26.1"
-  sha256 "c2e456d7c781372ab9e4c5cb427e2bb8b7dd248abb5b4771b7963e69d6a52944"
+  version "1.27.0"
+  sha256 "5752efc8bc52fd2daf2cf3f4d408b7de7164c7676800d7709de3c2a34ddf5b4f"
 
   url "https://github.com/microsoft/AzureStorageExplorer/releases/download/v#{version}/Mac_StorageExplorer.zip",
       verified: "github.com/microsoft/AzureStorageExplorer/"
