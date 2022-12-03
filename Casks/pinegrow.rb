a253c7c42c0e76fa797235d780928f21fffbf047
Patrick Linnane <patrick@linnane.io>
2022-12-03 15:54:14 -0800

    Update pinegrow from 6.21 to 7.0.1

--- Casks/pinegrow.rb
+++ Casks/pinegrow.rb
@@ -1,15 +1,18 @@
 cask "pinegrow" do
-  version "6.21"
-  sha256 "d2c037f70a07a2ad4fc049a8e2bc6393593b12933a8c18ccb58a8c713899f9d5"
+  arch arm: "ARM64", intel: "X64"
 
-  url "https://download.pinegrow.com/PinegrowMac.#{version}.dmg"
+  version "7.0.1"
+  sha256 arm:   "fe38638599c24fcdc6cc3abec99e8e8a18e7d3c305321d0d78bdc010e280f2b1",
+         intel: "a22bb320b2802a4ed3104c41a96a59586910b88a49527f0108e1d53a81448d04"
+
+  url "https://download.pinegrow.com/PinegrowMac#{arch}.#{version}.dmg"
   name "Pinegrow"
   desc "Web editor"
   homepage "https://pinegrow.com/"
 
   livecheck do
-    url :homepage
-    regex(%r{href=.*?/PinegrowMac[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
+    url "https://docs.pinegrow.com/wp-json/wp/v2/pages/1698"
+    regex(%r{href=.*?/PinegrowMac#{arch}[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
   end
 
   app "Pinegrow.app"
