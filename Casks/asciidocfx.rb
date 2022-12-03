4bd31302dc0e7f5b8d77492e36bf6e39d8b11523
Patrick Linnane <patrick@linnane.io>
2022-12-03 14:38:27 -0800

    Update asciidocfx from 1.8.2 to 1.8.3

--- Casks/asciidocfx.rb
+++ Casks/asciidocfx.rb
@@ -1,9 +1,9 @@
 cask "asciidocfx" do
   arch arm: "_M1"
 
-  version "1.8.2"
-  sha256 arm:   "0f3dc9213813ff35db24767953628f57b7376f105ca83cf5fec15e93e3cf957f",
-         intel: "132d47283f1ce5574a82d28e5d4e7b2e15cdd347776c542faae7b434c3e2dca8"
+  version "1.8.3"
+  sha256 arm:   "07fed75cbab80c143783d6e0c092adfbd6ebea282f527c2ac7b036c5826e3a4d",
+         intel: "5a82fae23a172c47c2fd293a08e2ec4b3b6901b9d9ac63b06616aaac7721f745"
 
   url "https://github.com/asciidocfx/AsciidocFX/releases/download/v#{version}/AsciidocFX_Mac#{arch}.dmg",
       verified: "github.com/asciidocfx/AsciidocFX/"
