eed481ca33f48521e83c797a19a0cc2fcb9baf4b
Johannes Klein <j.klein@meddevo.com>
2022-12-03 17:19:00 +0100

    Update brave-browser from 1.46.133.0 to 1.46.134.0

--- Casks/brave-browser.rb
+++ Casks/brave-browser.rb
@@ -2,9 +2,9 @@ cask "brave-browser" do
   arch arm: "arm64", intel: "x64"
   folder = on_arch_conditional arm: "stable-arm64", intel: "stable"
 
-  version "1.46.133.0,146.133"
-  sha256 arm:   "6ceccd964e1264f2ff13192dec082ec37fb1eda1c5f4e5462dfa57d79f06a6db",
-         intel: "98c4278e022f7ebf143429b08f5fc0cc946a8c65409a1dca742a3226bcde134e"
+  version "1.46.134.0,146.134"
+  sha256 arm:   "c16ced42cbc068829354c10e5d30f8e93bd4f5914ef09433cd1ec69f2a859e17",
+         intel: "d84d1e1779a02884fcb15585670e53513c0cc8db1d795dbdc050f54d4dd53cce"
 
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.csv.second}/Brave-Browser-#{arch}.dmg",
       verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
