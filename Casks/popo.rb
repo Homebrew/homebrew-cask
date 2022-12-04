590eaa6da7f2ccdf3cd56c1321685367af076462
Patrick Linnane <patrick@linnane.io>
2022-12-03 16:01:24 -0800

    popo 3.61.0,1669961152461
    Update popo from 3.60.0 to 3.61.0
    
    Closes #137015.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/popo.rb
+++ Casks/popo.rb
@@ -1,15 +1,20 @@
 cask "popo" do
-  version "3.60.0"
-  sha256 "86df37f964fed1bb7aa470bef962026e11a4889e6fc4635423e1913ae8c980a3"
+  version "3.61.0,1669961152461"
+  sha256 "7dd3643c33997a91d5a936226924f4dbd61b370b4ee050bae8f0f41d857f9631"
 
-  url "https://popo.netease.com/file/popomac/POPO-setup_#{version.dots_to_underscores}.dmg"
+  url "https://popo.netease.com/file/popomac/POPO-setup_prod_#{version.csv.second}.dmg"
   name "NetEase POPO"
   desc "Instant messaging platform"
   homepage "https://popo.netease.com/"
 
   livecheck do
     url "https://popo.netease.com/api/open/jsonp/check_version?device=4&callback="
-    regex(/"version"\s*:\s*"(\d+(?:\.\d+)+)"/i)
+    strategy :page_match do |page|
+      match = page.match(/"version"\s*:\s*"(\d+(?:\.\d+)+)".*?(\d+)\.dmg/i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
   end
 
   app "popo_mac.app"
