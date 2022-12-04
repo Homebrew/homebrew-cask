31b998f0cbbbe338ef37623f159cd2721633b69b
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-12-04 14:44:42 +0800

    Update tencent-meeting from 3.13.3 to 3.13.4 (#137021)

--- Casks/tencent-meeting.rb
+++ Casks/tencent-meeting.rb
@@ -2,12 +2,12 @@ cask "tencent-meeting" do
   arch arm: "arm64", intel: "x86_64"
 
   on_intel do
-    version "3.13.3.431,4eb2e52332a2e1e840b34c28735b83b5"
-    sha256 "eebbf06173efe9c32094b5f83a19b842b79d0eb77087649010edf52c212b0434"
+    version "3.13.4.426,a9b2640f51c6659580cffec83520d591"
+    sha256 "28bf0de4840fa1d3853ad073d67a5f525246dc456c5ad74fa2a9ee26e8e29c28"
   end
   on_arm do
-    version "3.13.3.431,6658a2e92ca986ce0f5e1c77dca6324f"
-    sha256 "fd7d9334224a5672db5750657d57d2c88557a4b63a5a498f4977d1eacba8e451"
+    version "3.13.4.426,2c72e3dca5c9cb81ff5854a3b8e26ee5"
+    sha256 "618bbdda49693aee4589fb1d06ab7330a977f90bcb5f0b09373af5463184fc51"
   end
 
   url "https://updatecdn.meeting.qq.com/cos/#{version.csv.second}/TencentMeeting_0300000000_#{version.csv.first}.publish.#{arch}.dmg",
