736869ef2d6e64d797b9bea168cde3bb0b61481e
Patrick Linnane <patrick@linnane.io>
2022-12-05 11:11:57 -0800

    tidal 2.34.2
    Update tidal from 2.33.2 to 2.34.2
    
    Closes #137116.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/tidal.rb
+++ Casks/tidal.rb
@@ -1,9 +1,9 @@
 cask "tidal" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.33.2"
-  sha256 arm:   "8d9e854dbe47a6587c4e9b8854c38a35c4bc1e8801629d265a7f7b102f101e76",
-         intel: "0855257b2bc4e1d341503eefd87c3190abc5e0294d11bf9cd5fa9171e64bbf9e"
+  version "2.34.2"
+  sha256 arm:   "ba0ef379601811638378a0b012b010a175247cb5f0834eda012e19301f0d5627",
+         intel: "0cc14a71dd6cd2593ce93c566cf755ac9ce5535aea8e43f65836ced7c159bb98"
 
   url "https://download.tidal.com/desktop/mac/TIDAL.#{arch}.#{version}.zip"
   name "TIDAL"
