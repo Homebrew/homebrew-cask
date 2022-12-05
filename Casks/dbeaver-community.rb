dfaf0b711de22c7a78aab77ae1d839b60d939623
Patrick Linnane <patrick@linnane.io>
2022-12-04 20:54:47 -0800

    dbeaver-community 22.3.0
    Update dbeaver-community from 22.2.5 to 22.3.0
    
    Closes #137041.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,9 +1,9 @@
 cask "dbeaver-community" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "22.2.5"
-  sha256 arm:   "78f055daa897cfa95ba007a4e44e861f091d65ff6dbc3dd1f0bc21afd6591cdd",
-         intel: "e265d1d22f065d5cdb79d35b5c97cc2670727a6aa5acce39cd03035f25dc40c5"
+  version "22.3.0"
+  sha256 arm:   "8e100fbb111a2f0480794b38e420845cf1b1e3852414613fffd0143b91399830",
+         intel: "84fcd58e0bfcba22e3b1e7c3d78190cd1e56b5783e61bccde2e662fd42415d99"
 
   url "https://dbeaver.io/files/#{version}/dbeaver-ce-#{version}-macos-#{arch}.dmg"
   name "DBeaver Community Edition"
