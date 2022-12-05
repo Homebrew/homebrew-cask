f761ea7de1584052e7a95be7d0f8d70b47c748bb
Patrick Linnane <patrick@linnane.io>
2022-12-05 11:17:52 -0800

    Update xiv-on-mac from 4.4.2 to 4.4.3

--- Casks/xiv-on-mac.rb
+++ Casks/xiv-on-mac.rb
@@ -1,6 +1,6 @@
 cask "xiv-on-mac" do
-  version "4.4.2"
-  sha256 "ddedd6ec5786b921bf40ce4144c89577909f380aedb45f286516436dd0bf23f5"
+  version "4.4.3"
+  sha256 "e22792e069e6b19afbbfdde8566f3c0195ddf5653cd447dfc0f16a67c6bcd22b"
 
   url "https://softwareupdate.xivmac.com/sites/default/files/update_data/XIV%20on%20Mac#{version}.tar.xz"
   name "XIV on Mac"
