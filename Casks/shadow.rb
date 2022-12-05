e857a20a9b18c994d566cb798a9d2784a0619900
Bevan Kay <email@bevankay.me>
2022-12-05 23:45:13 +1100

    Update shadow from 8.0.10018.10014 to 8.0.10018

--- Casks/shadow.rb
+++ Casks/shadow.rb
@@ -1,17 +1,18 @@
 cask "shadow" do
   arch arm: "arm64", intel: "x64"
 
-  version "8.0.10018.10014"
-  sha256 :no_check
+  version "8.0.10018"
+  sha256 arm:   "8f78739a8c045a31e85dc167aa8c82e001eda995e6f913d7fed49547c8f10c84",
+         intel: "e05e6fbf9708e6a313eb951cf9804ed7afb4d47dd65b5ecc556be3e87fdfd6dd"
 
-  url "https://update.shadow.tech/launcher/prod/mac/#{arch}/Shadow.dmg"
+  url "https://update.shadow.tech/launcher/prod/mac/#{arch}/Shadow-#{version}.dmg"
   name "Shadow"
-  desc "Your gaming PC powered by Cloud technology"
+  desc "Online virtualized computer"
   homepage "https://shadow.tech/"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url "https://update.shadow.tech/launcher/prod/mac/#{arch}/latest-mac.yml"
+    strategy :electron_builder
   end
 
   app "Shadow.app"
