a27bc1b95d6f1ccd0f55d3e69323b94d50b75334
Patrick Linnane <patrick@linnane.io>
2022-12-04 21:30:58 -0800

    Update unite from 4.2.4.1 to 4.2.5

--- Casks/unite.rb
+++ Casks/unite.rb
@@ -1,6 +1,6 @@
 cask "unite" do
-  version "4.2.4.1"
-  sha256 "cf21de1eeaaa90f39fb5da99cc8c9eb3ef5a83511904b1d22f50e62cf1f3ad32"
+  version "4.2.5"
+  sha256 "167be8405981cca2c4a38c78e3369ff3d83c98da3b3141bf9ffbc00c848e4428"
 
   url "https://bzgdownloads.s3.amazonaws.com/Unite/Unite+#{version}.zip",
       verified: "bzgdownloads.s3.amazonaws.com/Unite/"
