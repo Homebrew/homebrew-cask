c02bd2b607230effa0c30a85f38a3ccc941814bb
Patrick Linnane <patrick@linnane.io>
2022-12-05 11:10:18 -0800

    thedesk 23.0.2
    Update thedesk from 23.0.1 to 23.0.2
    
    Closes #137115.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/thedesk.rb
+++ Casks/thedesk.rb
@@ -1,9 +1,9 @@
 cask "thedesk" do
   arch arm: "arm64", intel: "x64"
 
-  version "23.0.1"
-  sha256 arm:   "a41189097769a589a83f521c1ef7d73e80733e082bd407d2a15d75998328826d",
-         intel: "287586d97f2318cc83c32d1f85cbf8f8db9777763197d2f11dbc70bfc66c9497"
+  version "23.0.2"
+  sha256 arm:   "f80489d3e50237bcab4b010da55f549643b94789804c22a82cf7437046b6d888",
+         intel: "917d5b1eddd33ac2a09d21df0f8e14ca8aabf9f70353539c51d7418212a8f283"
 
   url "https://github.com/cutls/TheDesk/releases/download/v#{version}/TheDesk-#{version}-#{arch}.dmg",
       verified: "github.com/cutls/TheDesk/"
