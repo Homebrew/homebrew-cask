3fbe945c0989b58bcc2aa55aee577b316e0adcea
Patrick Linnane <patrick@linnane.io>
2022-12-07 09:27:53 -0800

    eclipse-installer 4.26.0,2022-12
    Update eclipse-installer from 4.25.0 to 4.26.0
    
    Closes #137245.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/eclipse-installer.rb
+++ Casks/eclipse-installer.rb
@@ -1,9 +1,9 @@
 cask "eclipse-installer" do
   arch arm: "mac-aarch64", intel: "mac64"
 
-  version "4.25.0,2022-09"
-  sha256 arm:   "a7f3bc7bc48ca1c313e548c7cbf5ec63ec33d88d8b2c6184d07e8f99bffdb20d",
-         intel: "ae7caff2e72323d506ca1a9a7ff773a1a8ef9b3f60738d52a19b8ff43244d3ac"
+  version "4.26.0,2022-12"
+  sha256 arm:   "f6d2908fc427041a40d0ceb7452d2edda00c8c58edb72c92a3d66123d07155e3",
+         intel: "902a2c408dd41025a2b67edb0a237dd637ed5af07cbd3d44c277e95d88d3c5d6"
 
   url "https://www.eclipse.org/downloads/download.php?file=/oomph/epp/#{version.csv.second}/R/eclipse-inst-jre-#{arch}.dmg&r=1"
   name "Eclipse Installer"
