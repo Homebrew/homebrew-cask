ce8739e9940aa698d971c993fa6f032d16e76034
Patrick Linnane <patrick@linnane.io>
2022-12-06 21:23:56 -0800

    remix-ide 1.3.6
    Update remix-ide from 1.3.5 to 1.3.6
    
    Closes #137196.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/remix-ide.rb
+++ Casks/remix-ide.rb
@@ -1,6 +1,6 @@
 cask "remix-ide" do
-  version "1.3.5"
-  sha256 "2af7fbbbaab22e23d71ca5c555236cc1aa065626cc47feba8c2a9f7a28f0ad80"
+  version "1.3.6"
+  sha256 "a4a0d81e06f99f5721864afbf587b273a61d19b2d6e690a015dff2bf681039a4"
 
   url "https://github.com/ethereum/remix-desktop/releases/download/v#{version}/Remix-IDE-#{version}.dmg",
       verified: "github.com/ethereum/remix-desktop/"
