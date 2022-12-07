5abd01598fbc380c486404b01d94ab1db9129de5
Patrick Linnane <patrick@linnane.io>
2022-12-06 20:00:04 -0800

    Update busycal from 2022.4.6,2022-12-05-17-55 to 2022.4.6,2022-12-06-17-50

--- Casks/busycal.rb
+++ Casks/busycal.rb
@@ -1,6 +1,6 @@
 cask "busycal" do
-  version "2022.4.6,2022-12-05-17-55"
-  sha256 "2ff3e3b28a5a29a34852b8bc5123f5dad188c66438d73e62d5c810955bd8760d"
+  version "2022.4.6,2022-12-06-17-50"
+  sha256 "c230fe47f7517e321b32bfbda27f7fd78420d3a5e0df2c6634ee65960a030e47"
 
   url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bcl-#{version.csv.first}-#{version.csv.second}.zip",
       verified: "7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/"
