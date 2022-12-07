4cfe14fcad8a7746f83c3d5cc3fca68a5bb8a00d
Branch Vincent <branchevincent@gmail.com>
2022-12-07 10:06:08 -0800

    openlens 6.2.5
    Update openlens from 6.2.4 to 6.2.5
    
    Closes #137253.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/openlens.rb
+++ Casks/openlens.rb
@@ -1,9 +1,9 @@
 cask "openlens" do
   arch arm: "-arm64"
 
-  version "6.2.4"
-  sha256 arm:   "a2f7b54fb35338db9e44f13975ea9f4499dfa568eb321c4993da207578f5b80e",
-         intel: "1b8c6287b077342925cebc978e7b005660925bcab75f0befebaeec60ea1c040a"
+  version "6.2.5"
+  sha256 arm:   "12354d266cda1957625c62c5d6b37f944e87c0257d594fe59f9ede202f949a37",
+         intel: "afdcdbe427ac730808e3105bda46264981be77d9e7061e0c76c9a822f00ed9c8"
 
   url "https://github.com/MuhammedKalkan/OpenLens/releases/download/v#{version}/OpenLens-#{version}#{arch}.dmg"
   name "OpenLens"
