a1cefc48f8c1b9ab1f84663952b505d544711c99
Patrick Linnane <patrick@linnane.io>
2022-12-06 20:14:56 -0800

    fl-studio 21.0.0.2855
    Update fl-studio from 20.9.2.2670 to 21.0.0.2855
    
    Closes #137172.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/fl-studio.rb
+++ Casks/fl-studio.rb
@@ -1,6 +1,6 @@
 cask "fl-studio" do
-  version "20.9.2.2670"
-  sha256 "894dbe599046fb4c32fea8bf163569866c0d50d3e6d710685f1c62fe8732c4cc"
+  version "21.0.0.2855"
+  sha256 "87745d751b10d6277c155dfb0d47c650d53e2c424fa18a24686b4f4f1918e076"
 
   url "https://demodownload.image-line.com/flstudio/flstudio_mac_#{version}.dmg"
   name "FL Studio"
@@ -14,7 +14,7 @@ cask "fl-studio" do
 
   pkg "Install FL Studio.pkg"
 
-  uninstall pkgutil: "com.Image-Line.pkg.FLStudio64"
+  uninstall pkgutil: "com.Image-Line.pkg.FLStudio21"
 
   zap trash: [
     "~/Library/Caches/com.image-line.flstudio",
