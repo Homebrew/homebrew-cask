adc91ed0fac1db523e4306f93c739d72378b32fb
owine <owine@users.noreply.github.com>
2022-12-07 16:12:36 -0600

    Update brave-browser from 1.46.134.0 to 1.46.138.0

--- Casks/brave-browser.rb
+++ Casks/brave-browser.rb
@@ -2,9 +2,9 @@ cask "brave-browser" do
   arch arm: "arm64", intel: "x64"
   folder = on_arch_conditional arm: "stable-arm64", intel: "stable"
 
-  version "1.46.134.0,146.134"
-  sha256 arm:   "c16ced42cbc068829354c10e5d30f8e93bd4f5914ef09433cd1ec69f2a859e17",
-         intel: "d84d1e1779a02884fcb15585670e53513c0cc8db1d795dbdc050f54d4dd53cce"
+  version "1.46.138.0,146.138"
+  sha256 arm:   "0dd9afc3a74ccf79cd17902bbbc34b7a1544f973d7f797dd73712e1d1d0a9d6c",
+         intel: "80ff69b49d20a2f6a2068e1ddde8f14381d431d631a4db4d21fb109837dcf314"
 
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.csv.second}/Brave-Browser-#{arch}.dmg",
       verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
