60bb484d8353fefa5b518fa54fd81bfca8eb9b3c
solzard <29563481+solzard@users.noreply.github.com>
2022-12-08 03:24:28 +0900

    launchcontrol: rebuild
    * Update launchcontrol from 2.0.3,2304 to 2.0.3,2310
    * Add min OS
    
    Closes #137257.
    
    Co-authored-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/launchcontrol.rb
+++ Casks/launchcontrol.rb
@@ -3,8 +3,8 @@ cask "launchcontrol" do
     version "1.52.7,1959"
     sha256 "16c3d89e41a99cbf43e6996681358e8e7a4bc63fa770b9f8c0bc72c5356a0b8a"
   else
-    version "2.0.3,2304"
-    sha256 "9b963e4df75ac36888c90ce607960d14c59368c3aa028bc86736e1a0105ecede"
+    version "2.0.3,2310"
+    sha256 "bd73e6836ba5b27df04b296d8a47df019e22e2bcc67619b11645a919c0f3dd8f"
   end
 
   url "https://www.soma-zone.com/download/files/LaunchControl-#{version.csv.first}.tar.xz"
@@ -18,6 +18,7 @@ cask "launchcontrol" do
   end
 
   auto_updates true
+  depends_on macos: ">= :big_sur"
 
   app "LaunchControl.app"
 
