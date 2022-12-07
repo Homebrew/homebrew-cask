2163b51a5b97ae2069944fdb44dece0fd807a746
Yurii Kolesnykov <root@yurikoles.com>
2022-12-07 22:19:35 +0200

    update tor-browser from 11.5.8 to 12.0
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/tor-browser.rb
+++ Casks/tor-browser.rb
@@ -1,155 +1,15 @@
 cask "tor-browser" do
-  version "11.5.8"
+  version "12.0"
+  sha256 "11c8360187356e6c0837612a320f1a117303fc449602c9fd73f4faf9f9bbcfc9"
 
-  language "ar" do
-    sha256 "dce7e9704be0038f54fe4c141aa2da86bdaa7b6a5e4276d78926f113cd119222"
-    "ar"
-  end
-  language "ca" do
-    sha256 "77626fd023652b6f0f38d0f1d95166b5f4cf95dfbedeb5dccbdb177a3a413377"
-    "ca"
-  end
-  language "cs" do
-    sha256 "318cd4060e47d265ccdc600f83246fd7bb415aabbbffd272a0ff8864cb5ada36"
-    "cs"
-  end
-  language "da" do
-    sha256 "cc067dd14ec1ca6d71345f44bfb3f4eeb26f216622f537ae90cfb3f3fadc2a44"
-    "da"
-  end
-  language "de" do
-    sha256 "f5e1ea87fd414f38a2a734d64e2967ce1332a123906409a9ed32698ce7b5eabf"
-    "de"
-  end
-  language "el" do
-    sha256 "fdd1bc91acc33fc0506912c73bf275d66396c00ed1d513ac9d3e3912244818db"
-    "el"
-  end
-  language "en", default: true do
-    sha256 "051e5a92ff493826e569eb5487adb4f767ed4936edf7ca8f5c12a8b31e1fad16"
-    "en-US"
-  end
-  language "es-AR" do
-    sha256 "9a3c98d805687e95ab828f1f89d54c4897c94c1289f672b9c02a63920afd79aa"
-    "es-AR"
-  end
-  language "es-ES" do
-    sha256 "8786383c9236d979e2bd8628fd3e337078a0fffb28169f9e9004e89e959238bd"
-    "es-ES"
-  end
-  language "fa" do
-    sha256 "7cbf112e69804a240f4adc84569b82f173c1be12a00f425a5ec7dfd6e16d7c74"
-    "fa"
-  end
-  language "fr" do
-    sha256 "f14b54ab201ac65fc699584b31894e9035ae7b0dd238d35fe14a761d8cecabce"
-    "fr"
-  end
-  language "ga" do
-    sha256 "c5fd75d17432016a1170c5bc4be6525df6a159c8863f3f708c730540bd1cfdc8"
-    "ga-IE"
-  end
-  language "he" do
-    sha256 "2136176636d2bb91009f3013f9f7e6a1e41115f9adad8eba4af6c924c9400e02"
-    "he"
-  end
-  language "hu" do
-    sha256 "fed6494f603403e7fa69365ecf8c6a95a291b7209850e5bb9b87a7cec38a935f"
-    "hu"
-  end
-  language "id" do
-    sha256 "23e30313a2d623b0a8ced5739102a85b1495a19d088d42e52fd843dd236ee195"
-    "id"
-  end
-  language "is" do
-    sha256 "c90b9c77f0f4e39c2635c8386bd75d8e48a27f43e442dff1cb2e3222073641b9"
-    "is"
-  end
-  language "it" do
-    sha256 "6bf4332bf1e25be5fb884bf7a25cf8dd5a10f6a18d344769c86eaedc68e41979"
-    "it"
-  end
-  language "ja" do
-    sha256 "75f4cc4efb80823ee188ba21b7a94b7acf447dfe65ae6c5e44aafe1f9dd40aa5"
-    "ja"
-  end
-  language "ka" do
-    sha256 "ee7f0b00dbfe27c019261778a52acfc215bf4a7392918136a58229c7d01f1e2c"
-    "ka"
-  end
-  language "ko" do
-    sha256 "aea5a64b3e8664927fb688bf144274005ce4db8cb01755c8cb16dfb6151da5ec"
-    "ko"
-  end
-  language "lt" do
-    sha256 "a4691ffb6a594eb4608428096c0647e0a711b18c74181ee937f13aeecbf561ea"
-    "lt"
-  end
-  language "mk" do
-    sha256 "828f71300094a4654342c341d8178d644ccaf25e5bdd67e4ae8966c61e68c277"
-    "mk"
-  end
-  language "ms" do
-    sha256 "bea900fbcaee848a41a0c651fa2b9a6ae55adf7ecd333509b56b98e25a0051c2"
-    "ms"
-  end
-  language "nb" do
-    sha256 "c475a8b72984c8e6ad28011e8b5d64789c8239d201e94b59c3baf2ff54b795bc"
-    "nb-NO"
-  end
-  language "nl" do
-    sha256 "50d50c8ba24dc67abc4c837456d5c48c44a271fd3e541fbccf6300cc116ef6e8"
-    "nl"
-  end
-  language "pl" do
-    sha256 "bd51edcca64eab236cc096f44e9d130d19317af055116ba614a5470c2d63ea97"
-    "pl"
-  end
-  language "pt" do
-    sha256 "8ed70a43059b29afcb7c59bf64de2611c19bf982af3d538517a52fc50909f86a"
-    "pt-BR"
-  end
-  language "ro" do
-    sha256 "cfd1462151ac05083919eb67b155f6f248c2e7da3e3ba2f720ba62c6ac34086f"
-    "ro"
-  end
-  language "ru" do
-    sha256 "5f920f83ce0d2003fcf6154320f53bc21e71781acd0f8b2596c5b45aa646f0fb"
-    "ru"
-  end
-  language "sv" do
-    sha256 "f0a632df5a6e0cb8bea4af5aaf2045564025f4422738affd41128fbdeff52230"
-    "sv-SE"
-  end
-  language "th" do
-    sha256 "aeffa13b2408f1e85ea7f151aa113c43035bd1434c36ec88871159855389479a"
-    "th"
-  end
-  language "tr" do
-    sha256 "3548ba0c562fc106bebb9a62f197dbc2f82f9fe3b666ac586cf6ac870af4bb20"
-    "tr"
-  end
-  language "vi" do
-    sha256 "2efd6058a997f28d9c1bd87a449a33439147c7147b14add6d0bffc25aa425b03"
-    "vi"
-  end
-  language "zh-CN" do
-    sha256 "b3591b89f7d72d74dec2814b594cce22f60e205e44de675b1766b898cd94d212"
-    "zh-CN"
-  end
-  language "zh-TW" do
-    sha256 "eec6d185619d18a431a99cb3576d3ae769cced438b7c7586805b30c910150e22"
-    "zh-TW"
-  end
-
-  url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_#{language}.dmg"
+  url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-macos_ALL.dmg"
   name "Tor Browser"
   desc "Web browser focusing on security"
   homepage "https://www.torproject.org/"
 
   livecheck do
     url "https://www.torproject.org/download/"
-    regex(%r{href=.*?/TorBrowser[._-]v?(\d+(?:\.\d+)+)[._-]osx64[._-].*\.dmg}i)
+    regex(%r{href=.*?/TorBrowser[._-]v?(\d+(?:\.\d+)+)[._-]macos[._-].*\.dmg}i)
   end
 
   auto_updates true
