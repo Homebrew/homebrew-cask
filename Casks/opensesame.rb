33fd72686f6f03d2e0785e88b8f062b428fdf17f
Patrick Linnane <patrick@linnane.io>
2022-12-07 10:58:37 -0800

    opensesame 3.3.14
    Update opensesame from 3.3.13 to 3.3.14
    
    Closes #137271.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/opensesame.rb
+++ Casks/opensesame.rb
@@ -1,6 +1,6 @@
 cask "opensesame" do
-  version "3.3.13"
-  sha256 "bffa12bb4782934516ed455ed4074d2aea1fb7d1e3f15cbc6213870b5d577b8d"
+  version "3.3.14"
+  sha256 "36f7dfc092a8a6531ca4b94f979a581ffbeb83bc007cb8cf0d9dd3aa280a6ff3"
 
   url "https://github.com/open-cogsci/OpenSesame/releases/download/release%2F#{version}/opensesame_#{version}-py37-macos-x64-1.dmg",
       verified: "github.com/open-cogsci/OpenSesame/"
