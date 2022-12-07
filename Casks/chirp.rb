1942686c1b51c5ea3c496a975bbc22b50008acc6
Patrick Linnane <patrick@linnane.io>
2022-12-07 08:49:07 -0800

    Update chirp from 20221205 to 20221207

--- Casks/chirp.rb
+++ Casks/chirp.rb
@@ -1,6 +1,6 @@
 cask "chirp" do
-  version "20221205"
-  sha256 "e5fdc4aee0db289b1462a48093865a7f5c426f188d41dbf618d09b086fb00c99"
+  version "20221207"
+  sha256 "f4d820033c5380c503955b17ea66841a75e96ce5dd8d98e6e24670fa5394b678"
 
   url "https://trac.chirp.danplanet.com/chirp_daily/daily-#{version}/chirp-unified-daily-#{version}.app.zip"
   name "CHIRP"
