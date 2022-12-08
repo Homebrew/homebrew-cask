d28e8c26ace216448d319f0fa6380e557c864d3c
Bevan Kay <bevanjkay@gmail.com>
2022-12-08 23:29:59 +1100

    Remove Catalina conditional

--- Casks/launchcontrol.rb
+++ Casks/launchcontrol.rb
@@ -1,30 +1,19 @@
 cask "launchcontrol" do
-  if MacOS.version <= :catalina
-    version "1.52.7,1959"
-    sha256 "760edc3f3238ecbbc9f0c14b17ced9ac2a46c46a4ed8feec6bfb532fced37b7e"
 
-    livecheck do
-      skip "Legacy version"
-    end
-
-    depends_on macos: "<= :catalina"
-  else
-    version "2.0.3,2310"
-    sha256 "bd73e6836ba5b27df04b296d8a47df019e22e2bcc67619b11645a919c0f3dd8f"
-
-    livecheck do
-      url "https://www.soma-zone.com/LaunchControl/a/appcast-update-2.xml"
-      strategy :sparkle
-    end
-
-    depends_on macos: ">= :big_sur"
-  end
+  version "2.0.3,2310"
+  sha256 "bd73e6836ba5b27df04b296d8a47df019e22e2bcc67619b11645a919c0f3dd8f"
 
   url "https://www.soma-zone.com/download/files/LaunchControl-#{version.csv.first}.tar.xz"
   name "LaunchControl"
   desc "Create, manage and debug system- and user services"
   homepage "https://www.soma-zone.com/LaunchControl/"
 
+  livecheck do
+    url "https://www.soma-zone.com/LaunchControl/a/appcast-update-2.xml"
+    strategy :sparkle
+  end
+
+  depends_on macos: ">= :big_sur"
   auto_updates true
 
   app "LaunchControl.app"
