96ae3e21a1b8e7a31bd7aacb0e8cf9c1ac0ec020
MoonFruit <dkmoonfruit@gmail.com>
2022-12-08 15:17:03 +0800

    Update httpie from 2022.15.1 to 2022.16.0 (#137300)

--- Casks/httpie.rb
+++ Casks/httpie.rb
@@ -1,9 +1,9 @@
 cask "httpie" do
   arch arm: "-arm64"
 
-  version "2022.15.1"
-  sha256 arm:   "3011a109491316451a206608ff6ab45595d47caaf3afa675a855481412c1be3b",
-         intel: "3634a7afdca927b7105f2572873b74b7ac6f785bdd9d45fd3021f5d516449f84"
+  version "2022.16.0"
+  sha256 arm:   "a557901161e35a0a9f6373b383b87043f6fa541009bf0ae5297314cc01b0961a",
+         intel: "2af6bc5f8b2895ce03dd63f159541d121d88ab8b9696c1a48c783e48e98999fd"
 
   url "https://github.com/httpie/desktop/releases/download/v#{version}/HTTPie-#{version}#{arch}.dmg",
       verified: "github.com/httpie/desktop/"
