20c4719f3734f4b62cfc82288af91de3ca26335c
Peter Rood <prood@peterrood.com>
2022-12-08 08:46:38 -0800

    Add Farcaster.app v0.5.2

---
+++ Casks/farcaster.rb
@@ -0,0 +1,12 @@
+cask "farcaster" do
+  version "0.5.2"
+  sha256 :no_check
+
+  url "https://downloads.farcaster.xyz/Farcaster-latest.dmg"
+  name "Farcaster"
+  desc "App for Farcaster which is a sufficiently decentralized social network"
+  homepage "https://www.farcaster.xyz/"
+
+  app "Farcaster.app"
+end
+
