2b2debe2b8935a48f435a773db25cdcbb2a30bb3
Owen Voke <development@voke.dev>
2022-12-08 12:12:27 +0000

    Update tinkerwell from 3.13.0 to 3.14.0

--- Casks/tinkerwell.rb
+++ Casks/tinkerwell.rb
@@ -1,9 +1,9 @@
 cask "tinkerwell" do
   arch arm: "-arm64"
 
-  version "3.13.0"
-  sha256 arm:   "96fd1ffe5026e79dd2f6d9963a7331bd311ed48c2d24dc0ef6403cf9d3c4ec2c",
-         intel: "f5788b600b8d8dc5621455455b3f83c6d97aac1b90263a79584754804c394f84"
+  version "3.14.0"
+  sha256 arm:   "5282e83d38e2e965d589aca5909d57690e08fb0f722769e763326bc8b356ee4c",
+         intel: "2879bfd4d64afc6a0838a94fd39a2f0d0027ac199c4605c9ca1db16a55ec9ffd"
 
   url "https://download.tinkerwell.app/tinkerwell/Tinkerwell-#{version}#{arch}.dmg"
   name "Tinkerwell"
