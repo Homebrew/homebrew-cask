189f6c6ad279db056e9828f144ae597f4e826dbd
Bruno Coelho <4brunu@users.noreply.github.com>
2022-12-08 16:49:56 +0000

    Update db-browser-for-sqlite.rb

--- Casks/db-browser-for-sqlite.rb
+++ Casks/db-browser-for-sqlite.rb
@@ -2,7 +2,7 @@ cask "db-browser-for-sqlite" do
   arch arm: "arm64", intel: "x64"
 
   version "3.12.2"
-  
+
   on_intel do
     sha256 "546d57b6c88c2be7517759c016c0bf0313dfcc14adfcb43967f3c5d24657f366"
 
@@ -15,7 +15,7 @@ cask "db-browser-for-sqlite" do
     url "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v#{version}/DB.Browser.for.SQLite-arm64-#{version}.dmg",
         verified: "github.com/sqlitebrowser/sqlitebrowser/"
   end
-  
+
   name "DB Browser for SQLite"
   desc "Browser for SQLite databases"
   homepage "https://sqlitebrowser.org/"
