0108659e65682041a9adb54ea3d89d216c2c4be9
Howard Rappaport <howie@rewardsmith.com>
2022-12-08 16:08:10 -0500

    Update messenger from 171.0.0.28.105 to 171.0.0.29.105

--- Casks/messenger.rb
+++ Casks/messenger.rb
@@ -1,5 +1,5 @@
 cask "messenger" do
-  version "171.0.0.28.105,426237024"
+  version "171.0.0.29.105"
   sha256 :no_check
 
   url "https://www.messenger.com/messenger/desktop/downloadV2/?platform=mac"
