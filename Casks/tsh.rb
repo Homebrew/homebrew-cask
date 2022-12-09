291fdf84bf968d433b49ba0fad78ac51ac30505c
Miccal Matthews <miccal.matthews@gmail.com>
2022-12-10 07:07:25 +0800

    Update and rename teleport-tsh.rb to tsh.rb

--- Casks/tsh.rb
+++ Casks/tsh.rb
@@ -1,19 +1,18 @@
-cask "teleport-tsh" do
+cask "tsh" do
   version "11.1.2"
   sha256 "3a9efbc794fc4a64b17d7a6f95fd777f5fac457f9456ac5faaace7aa9e966fc2"
 
   url "https://get.gravitational.com/tsh-#{version}.pkg"
   name "Teleport TSH"
-  desc "Modern SSH server for teams managing distributed infrastructure (tsh client)"
+  desc "SSH server for teams managing distributed infrastructure"
   homepage "https://gravitational.com/teleport"
 
   livecheck do
-    formula "teleport"
+    url "https://goteleport.com/download/"
+    regex(/tsh[._-]v?(\d+(?:\.\d+)+)\.pkg/i)
   end
 
-  conflicts_with formula: %w[
-    teleport
-  ]
+  conflicts_with formula: "teleport"
 
   pkg "tsh-#{version}.pkg"
 
