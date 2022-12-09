53e4b3d7e11e8d0331e96c4065e2c0c9112f79c1
owine <owine@users.noreply.github.com>
2022-12-09 09:36:10 -0600

    Update fantastical from 3.7.4 to 3.7.5

--- Casks/fantastical.rb
+++ Casks/fantastical.rb
@@ -1,6 +1,6 @@
 cask "fantastical" do
-  version "3.7.4,1540"
-  sha256 "cb9e3e1f37c7cd8322e3a0e2e69650e3f770522e17db9b7d3c9e3ace9cca5698"
+  version "3.7.5,1544"
+  sha256 "9d862a8698968b277093f3416fe8f25c1f7dc6674360c483f0bc187978d390b1"
 
   url "https://cdn.flexibits.com/Fantastical_#{version.csv.first}.zip"
   name "Fantastical"
