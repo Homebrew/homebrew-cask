53bcbb96b3cd532475406f216c0e3c70d4c764db
Loïc PÉRON <loic.peron@gmail.com>
2022-12-09 16:18:32 +0100

    Update appcleaner from 3.6.6 to 3.6.7

--- Casks/appcleaner.rb
+++ Casks/appcleaner.rb
@@ -3,8 +3,8 @@ cask "appcleaner" do
     version "3.4"
     sha256 "0c60d929478c1c91e0bad76d3c04795665c07a05e45e33321db845429c9aefa8"
   else
-    version "3.6.6,4299"
-    sha256 "85ae92384cdfcc1ceea6c6af620408687cb3bad5291f02d010ac0313c2e13bad"
+    version "3.6.7,4301"
+    sha256 "24c5c2e38b2cfd29573824e81af968b510d116f8e229ff333501937fec6c8470"
   end
 
   url "https://www.freemacsoft.net/downloads/AppCleaner_#{version.csv.first}.zip"
