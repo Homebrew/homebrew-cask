9ee8c48c132b01f90d55264dc66aa2f6d1e7ad67
Miccal Matthews <miccal.matthews@gmail.com>
2022-12-10 16:36:42 +0800

    Update overt.rb

--- Casks/overt.rb
+++ Casks/overt.rb
@@ -6,7 +6,7 @@ cask "overt" do
          intel: "2218c3e3ba4d322d48fa773bb9c1e83d280ced147fc9bd3aadc5c5bd2c7dd308"
 
   url "https://github.com/GetOvert/Overt/releases/download/v#{version}/Overt-darwin-#{arch}-#{version}.zip",
-      verified: "https://github.com/GetOvert/Overt"
+      verified: "github.com/GetOvert/Overt/"
   name "Overt"
   desc "Open app store"
   homepage "https://getovert.app/"
@@ -19,14 +19,14 @@ cask "overt" do
   app "Overt.app"
 
   zap trash: [
-    "~/Library/Application Support/Overt",
     "~/Library/Application Support/OpenStore",
-    "~/Library/Caches/Overt_v5",
-    "~/Library/Caches/Overt_v4",
-    "~/Library/Caches/OpenStore_v3",
-    "~/Library/Caches/OpenStore_v2",
+    "~/Library/Application Support/Overt",
     "~/Library/Caches/OpenStore_v1",
-    "~/Library/Preferences/app.getovert.Overt.plist",
+    "~/Library/Caches/OpenStore_v2",
+    "~/Library/Caches/OpenStore_v3",
+    "~/Library/Caches/Overt_v4",
+    "~/Library/Caches/Overt_v5",
     "~/Library/Preferences/app.getopenstore.OpenStore.plist",
+    "~/Library/Preferences/app.getovert.Overt.plist",
   ]
 end
