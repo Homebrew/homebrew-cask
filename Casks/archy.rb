516dbb25c02362d2c4bd53670e8cebe423586694
Patrick Linnane <patrick@linnane.io>
2022-12-09 19:50:15 -0800

    archy 2.15.3
    Update archy from 2.15.2 to 2.15.3
    
    Closes #137364.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/archy.rb
+++ Casks/archy.rb
@@ -1,6 +1,6 @@
 cask "archy" do
-  version "2.15.2"
-  sha256 "d47e01d3fa698a09350bdf3ed43435b34190835917bfd04779a13fb305ff1b18"
+  version "2.15.3"
+  sha256 "aef13e0efa619bae0ba2140369c855f6d3c5f851bc3437ff45e800f6b3d3abfd"
 
   url "https://sdk-cdn.mypurecloud.com/archy/#{version}/archy-macos.zip",
       verified: "sdk-cdn.mypurecloud.com/archy/"
