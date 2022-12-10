f5c54df9f3fdd8a09d8e880b38a406f5dde5a49c
Patrick Linnane <patrick@linnane.io>
2022-12-09 20:26:19 -0800

    Update linear-linear from 1.12.4 to 1.13.0

--- Casks/linear-linear.rb
+++ Casks/linear-linear.rb
@@ -1,7 +1,7 @@
 cask "linear-linear" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.12.4,221114yb4h0nuj3"
+  version "1.13.0,221209g3pffvinb"
   sha256 :no_check
 
   url "https://desktop.linear.app/mac/dmg/#{arch}"
