35f212be229860e2983160cd285b00714e4d1d17
Alexander Goldstein <alexg@alexland.org>
2022-12-10 03:01:26 -0600

    new cask for Elephas (#137380)
    * new cask for Elephas
    
    * elephas cask incorporating style feedback and taking latest version
    
    * Update elephas.rb
    
    * Update elephas.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/elephas.rb
@@ -0,0 +1,26 @@
+cask "elephas" do
+  version "4.4"
+  sha256 :no_check
+
+  url "https://elephas.app/downloads/Elephas_latest.dmg"
+  name "Elephas"
+  desc "Personal AI Writing Assistant"
+  homepage "https://elephas.app/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
+
+  conflicts_with cask: "homebrew/cask-versions/elephas-beta"
+  depends_on macos: ">= :catalina"
+
+  app "Elephas.app"
+
+  zap trash: [
+    "~/Library/Application Support/Elephas",
+    "~/Library/Caches/com.kamban.elephas",
+    "~/Library/HTTPStorages/com.kamban.elephas",
+    "~/Library/Preferences/com.kamban.elephas.plist",
+  ]
+end
