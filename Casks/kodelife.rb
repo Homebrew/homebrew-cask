2e9070711b403ce48b2ae263da18c8fc01d2be26
Patrick Linnane <patrick@linnane.io>
2022-12-09 20:25:56 -0800

    Update kodelife from 1.0.7 to 1.0.8

--- Casks/kodelife.rb
+++ Casks/kodelife.rb
@@ -1,6 +1,6 @@
 cask "kodelife" do
-  version "1.0.7,167"
-  sha256 "d14c4cda6e091589671effe28042c5158f9b3d883b84b0afc25e8199d70d508f"
+  version "1.0.8,170"
+  sha256 "d73bbbdfd9b7870ecc0cfbebee0f8dbfe07e6694b928ffcf6bc3ab25d463913e"
 
   url "https://hexler.net/pub/kodelife/kodelife-#{version.csv.first}.#{version.csv.second}-macos.dmg"
   name "KodeLife"
