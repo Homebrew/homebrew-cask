05ea75e81b1e53d48ce31923d83b4435b28b114e
Amory Meltzer <Amorymeltzer@gmail.com>
2022-12-10 07:31:16 -0500

    Update runjs from 2.7.1 to 2.7.4

--- Casks/runjs.rb
+++ Casks/runjs.rb
@@ -1,6 +1,6 @@
 cask "runjs" do
-  version "2.7.1"
-  sha256 "ce91d5b79eac088d7148373b6bb8f91100a4276a3e2160c122ef2cc510dcac6f"
+  version "2.7.4"
+  sha256 "70039b2278507a7a111db316d22cc73e1c9bbaf045a3d2ecd1d5512cd4f0e498"
 
   url "https://github.com/lukehaas/RunJS/releases/download/v#{version}/RunJS-#{version}-universal.dmg",
       verified: "github.com/lukehaas/RunJS/"
