27d0b272eb064afde649fce226523a3b802d2fb8
Patrick Linnane <patrick@linnane.io>
2022-12-10 09:22:33 -0800

    Update linphone from 4.4.11 to 5.0.0

--- Casks/linphone.rb
+++ Casks/linphone.rb
@@ -1,6 +1,6 @@
 cask "linphone" do
-  version "4.4.11"
-  sha256 "593f837e7e948ddf1e90ff3c1bddeb71827adcbf79d9cda14f07793095d196a7"
+  version "5.0.0"
+  sha256 "4d8507be145997a00605039591e4dc1179c2c5b6be7e451c2517fef6eea83540"
 
   url "https://download.linphone.org/releases/macosx/app/Linphone-#{version}-mac.dmg"
   name "Linphone"
@@ -8,8 +8,8 @@ cask "linphone" do
   homepage "https://www.linphone.org/"
 
   livecheck do
-    url "https://www.linphone.org/releases/macosx/RELEASE"
-    regex(%r{/Linphone-(\d+(?:\.\d+)+)-mac\.dmg}i)
+    url "https://download.linphone.org/releases/macosx/app/"
+    regex(/Linphone-(\d+(?:\.\d+)+)-mac\.dmg/i)
   end
 
   app "Linphone.app"
