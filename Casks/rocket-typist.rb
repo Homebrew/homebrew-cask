81186b5e8b28c1e2482ec2c05daeb79c7327e01a
Patrick Linnane <patrick@linnane.io>
2022-12-09 20:52:47 -0800

    rocket-typist 2.4.2,165b
    Update rocket-typist from 2.4.1 to 2.4.2
    
    Closes #137427.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/rocket-typist.rb
+++ Casks/rocket-typist.rb
@@ -1,5 +1,5 @@
 cask "rocket-typist" do
-  version "2.4.1,163b"
+  version "2.4.2,165b"
   sha256 :no_check
 
   url "https://witt-software.com/downloads/rockettypist/Rocket%20Typist.dmg"
@@ -11,6 +11,8 @@ cask "rocket-typist" do
     url "https://witt-software.com/downloads/rockettypist/appcast.xml"
     strategy :sparkle
   end
+
+  depends_on macos: ">= :monterey"
 
   app "Rocket Typist.app"
 end
