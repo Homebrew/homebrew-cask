0533e90b31e83db15730ce88a226669113e9d81d
Thomas Walter <tw@b-a-l-u.de>
2022-12-11 14:17:04 +0100

    jitsi-meet 2022.12.0
    Update jitsi-meet from 2022.11.0 to 2022.12.0
    
    Closes #137495.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/jitsi-meet.rb
+++ Casks/jitsi-meet.rb
@@ -1,6 +1,6 @@
 cask "jitsi-meet" do
-  version "2022.11.0"
-  sha256 "c3b4823f5da1ddc497dcf67a3dc3ec3427c584deaf580406b748fede8fc5d14b"
+  version "2022.12.0"
+  sha256 "5e641f5e6e25cf1f5cb29a2e9b9b9b2752d829779fc7e4b505c4920782118d60"
 
   url "https://github.com/jitsi/jitsi-meet-electron/releases/download/v#{version}/jitsi-meet.dmg"
   name "Jitsi Meet"
