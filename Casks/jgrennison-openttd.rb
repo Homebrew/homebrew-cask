9c4665182e1a855a8090b004b2df060066e73a03
Patrick Linnane <patrick@linnane.io>
2022-12-11 09:19:45 -0800

    jgrennison-openttd 0.50.0
    Update jgrennison-openttd from 0.49.2 to 0.50.0
    
    Closes #137503.
    
    Signed-off-by: Markus Reiter <me@reitermark.us>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/jgrennison-openttd.rb
+++ Casks/jgrennison-openttd.rb
@@ -1,6 +1,6 @@
 cask "jgrennison-openttd" do
-  version "0.49.2"
-  sha256 "8385248aad9015b154ee6d3ad403d90afa2323829ea82bde70db9e8a4cc1aa7a"
+  version "0.50.0"
+  sha256 "7fec71067e9bd0d25a217cc0e6405b08e2e0b0399c30bd92de4710347f53f491"
 
   url "https://github.com/JGRennison/OpenTTD-patches/releases/download/jgrpp-#{version}/openttd-jgrpp-#{version}-macos-universal.dmg"
   name "JGR's OpenTTD Patchpack"
