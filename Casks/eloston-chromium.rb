ec4ae635388d15deb2caff0d560673ad58b214cf
Patrick Linnane <patrick@linnane.io>
2022-12-11 09:19:00 -0800

    eloston-chromium 108.0.5359.99-1.1,1670691018
    Update eloston-chromium from 108.0.5359.95-1.1 to 108.0.5359.99-1.1
    
    Closes #137501.
    
    Signed-off-by: Markus Reiter <me@reitermark.us>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/eloston-chromium.rb
+++ Casks/eloston-chromium.rb
@@ -2,12 +2,12 @@ cask "eloston-chromium" do
   arch arm: "arm64", intel: "x86-64"
 
   on_intel do
-    version "108.0.5359.95-1.1,1670455322"
-    sha256 "ce83d6bc613de564c926af21ecaae4359459f28c5785e313bdec331ecd49ebc1"
+    version "108.0.5359.99-1.1,1670691018"
+    sha256 "950e0c1e4943b9c178cadde22d3e178c07f6aff7f4a54f81a39a0941eff2c099"
   end
   on_arm do
-    version "108.0.5359.95-1.1,1670513186"
-    sha256 "b101010cf448618d708a7d53eafde50ff4cd3fba4b1737cd52d67eafa2a8ba49"
+    version "108.0.5359.99-1.1,1670751721"
+    sha256 "c491ad69fc7572b798d28b15cafd2697d0dfa4366152a682f84444baba361248"
   end
 
   url "https://github.com/kramred/ungoogled-chromium-macos/releases/download/#{version.csv.first}_#{arch}__#{version.csv.second}/ungoogled-chromium_#{version.csv.first}_#{arch}-macos.dmg",
