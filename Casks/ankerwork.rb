18560fde5a800be9ee74399246685025797306ab
Patrick Linnane <patrick@linnane.io>
2022-12-12 08:48:35 -0800

    add livecheck, update desc

--- Casks/ankerwork.rb
+++ Casks/ankerwork.rb
@@ -5,8 +5,13 @@ cask "ankerwork" do
   url "https://ankerwork.s3.us-west-2.amazonaws.com/prod/AnkerWork_Official.dmg",
       verified: "ankerwork.s3.us-west-2.amazonaws.com"
   name "ankerwork"
-  desc "Designed to optimize and personalize your  products"
+  desc "Webcam & audio device software"
   homepage "https://us.ankerwork.com/pages/download-software"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   app "AnkerWork.app"
 end
