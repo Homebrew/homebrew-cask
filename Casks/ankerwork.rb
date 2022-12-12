010f5a50e562756d8ff35f60a39b7d8aefec5ee4
Eduardo Borges <euduardoborges@gmail.com>
2022-12-12 12:24:03 -0300

    feat: add ankerwork cask

---
+++ Casks/ankerwork.rb
@@ -0,0 +1,12 @@
+cask "ankerwork" do
+  version "2.4.5"
+  sha256 :no_check
+
+  url "https://ankerwork.s3.us-west-2.amazonaws.com/prod/AnkerWork_Official.dmg",
+    verified: "ankerwork.s3.us-west-2.amazonaws.com"
+  name "ankerwork"
+  desc "Designed to optimize and personalize your AnkerWork products."
+  homepage "https://us.ankerwork.com/pages/download-software"
+
+  app "AnkerWork.app"
+end
