7f9d60d8d54d088f800c597c50365186044cf982
Bevan Kay <email@bevankay.me>
2022-12-13 22:56:10 +1100

    cockatrice: convert to `on_system` blocks

--- Casks/cockatrice.rb
+++ Casks/cockatrice.rb
@@ -1,17 +1,19 @@
 cask "cockatrice" do
   version "2.8.0,2021-01-26,Prismatic.Bridge"
 
-  if MacOS.version <= :mojave
+  on_mojave :or_older do
     sha256 "53a4db3e3b97196b42c20959da701de5713f0811907d07ba187192f53fccef1d"
 
     url "https://github.com/Cockatrice/Cockatrice/releases/download/#{version.csv.second}-Release-#{version.csv.first}/Cockatrice-#{version.csv.third}-#{version.csv.first}-macOS-10.14_Mojave.dmg",
         verified: "github.com/Cockatrice/Cockatrice/"
-  elsif MacOS.version <= :catalina
+  end
+  on_catalina do
     sha256 "b4953e77a01c639563d6e05acb40c9ca266616e15d941dea592d10904082470f"
 
     url "https://github.com/Cockatrice/Cockatrice/releases/download/#{version.csv.second}-Release-#{version.csv.first}/Cockatrice-#{version.csv.third}-#{version.csv.first}-macOS-10.15_Catalina.dmg",
         verified: "github.com/Cockatrice/Cockatrice/"
-  else
+  end
+  on_big_sur :or_newer do
     sha256 "a200400b24d24c8e3e32f849e23d6714548a203d68a4456cee219901507bcd49"
 
     url "https://github.com/Cockatrice/Cockatrice/releases/download/#{version.csv.second}-Release-#{version.csv.first}/Cockatrice-#{version.csv.third}-#{version.csv.first}-macOS-11.0_Big_Sur.dmg",
