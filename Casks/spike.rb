5174824a3d9f7a5038488682a860457ccc095e05
Bevan Kay <email@bevankay.me>
2022-12-13 23:05:10 +1100

    Update Casks/spike.rb

--- Casks/spike.rb
+++ Casks/spike.rb
@@ -2,7 +2,7 @@ cask "spike" do
   version "3.2.0"
   sha256 "9a4aca2d7a8963eb4279ca5c215a828f64bb93c3983497dd18b77ae76b88a677"
 
-  url "https://education.lego.com/_/downloads/SPIKE_APP_3_macOS__#{version}_Global.dmg"
+  url "https://education.lego.com/_/downloads/SPIKE_APP_#{version.major}_macOS__#{version}_Global.dmg"
   name "Lego SPIKE"
   desc "Develop with Scratch and Python for your LEGO Spike set"
   homepage "https://education.lego.com/"
