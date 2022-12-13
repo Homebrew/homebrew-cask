27e1011f8a40cc6865870af029cfa31dcc72e5fe
UltraHDR <108294295+UltraHDR@users.noreply.github.com>
2022-12-13 20:34:07 +0000

    Update Retroarch & retroarch-metal to 1.14.0 (#137609)
    * Update to 1.14.0
    
    * Update to 1.14.0

--- Casks/retroarch-metal.rb
+++ Casks/retroarch-metal.rb
@@ -1,6 +1,6 @@
 cask "retroarch-metal" do
-  version "1.13.0"
-  sha256 "29b05a2f2f49074cbbb1e9a591b1353b4d273da89b5c4fe670057041849d02c2"
+  version "1.14.0"
+  sha256 "89162d663b45cdde938cdaf042abe2a23af40dae5fc3ac33f448b3e711c0d4a3"
 
   url "https://buildbot.libretro.com/stable/#{version}/apple/osx/universal/RetroArch_Metal.dmg"
   name "RetroArch"
