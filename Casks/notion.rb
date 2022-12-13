a09617c361c4269d944b6cefd3ee4a543c23551e
Bevan Kay <email@bevankay.me>
2022-12-14 09:30:01 +1100

    notion 2.1.11
    Update notion from 2.1.9 to 2.1.11
    
    Closes #137652.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/notion.rb
+++ Casks/notion.rb
@@ -3,12 +3,12 @@ cask "notion" do
   livecheck_folder = on_arch_conditional arm: "apple-silicon", intel: "mac"
 
   on_intel do
-    version "2.1.9"
-    sha256 "8d8819babc41d0ab554735b34f6880b6673203a4791503665824549bf57cee73"
+    version "2.1.11"
+    sha256 "c9eab7cca6d63b2869010010659e37d6cafa13f07c1c4fe21e859c15accbb546"
   end
   on_arm do
-    version "2.1.9"
-    sha256 "03c839398db0436fd70168ca8827d127eeaadee45a7b4f004822d8c0680a0e37"
+    version "2.1.11"
+    sha256 "c2d2cb58e85d9de250fd0dad75491ba1e1eb2d9c65a469121a09b9f1b285507c"
   end
 
   url "https://desktop-release.notion-static.com/Notion-#{version}#{arch}.dmg",
