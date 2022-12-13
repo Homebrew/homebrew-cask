cc40c4c467f390b8da69809641bbb486a297c5bc
Caesarovich <cesar@caesarovich.xyz>
2022-12-13 12:10:28 +0100

    Fixed styling

--- Casks/lego-spike.rb
+++ Casks/lego-spike.rb
@@ -1,17 +1,17 @@
+# typed: false
+# frozen_string_literal: true
+
 cask "lego-spike" do
   version "3.2.0"
   sha256 "9a4aca2d7a8963eb4279ca5c215a828f64bb93c3983497dd18b77ae76b88a677"
 
   url "https://education.lego.com/_/downloads/SPIKE_APP_3_macOS__#{version}_Global.dmg"
   name "Lego SPIKE"
-  desc "The intuitive SPIKE™ App lets your students develop coding capabilities with Scratch and Python, regardless of their skill level."
+  desc "Develop with Scratch and Python for your LEGO Spike set"
   homepage "https://education.lego.com/"
 
   depends_on macos: ">= :catalina"
-  depends_on arch: :x86_64 
-
+  depends_on arch: :x86_64
 
   app "Spike.app"
 end
-
-
