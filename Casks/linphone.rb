463f53a07e0aaed5cdea11ac6c719462f11dd18c
tiiiecherle <tiiiecherle@gmail.com>
2022-12-13 08:48:17 +0100

    linphone 5.0.2
    Update linphone from 5.0.0 to 5.0.2
    
    Closes #137619.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/linphone.rb
+++ Casks/linphone.rb
@@ -1,6 +1,6 @@
 cask "linphone" do
-  version "5.0.0"
-  sha256 "4d8507be145997a00605039591e4dc1179c2c5b6be7e451c2517fef6eea83540"
+  version "5.0.2"
+  sha256 "58b50d9ab6557066265080e033384b3d0845d765db42ea98e5c643b7b454c2b0"
 
   url "https://download.linphone.org/releases/macosx/app/Linphone-#{version}-mac.dmg"
   name "Linphone"
