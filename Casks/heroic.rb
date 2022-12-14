f8e5ddb31a169e5af3a1577ba19c59a52f65bca7
Marcin Mitura <mitura07@wp.pl>
2022-12-14 12:16:13 +0100

    Update Casks/heroic.rb
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>

--- Casks/heroic.rb
+++ Casks/heroic.rb
@@ -2,14 +2,9 @@ cask "heroic" do
   version "2.5.2"
   arch arm: "arm64", intel: "x64"
 
-  on_intel do
-    url "https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v#{version}/Heroic-#{version}-macOS-x64.dmg"
-    sha256 "39f48a8aa39ee7bf17767e9afca2abb974426f5856fa8794e2364cf7b0fcd9bc"
-  end
-  on_arm do
-    url "https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v#{version}/Heroic-#{version}-macOS-arm64.dmg"
-    sha256 "a82788df9b8e5603911280b9f7ef403c84ee9688e44fc4c90747a0b265646486"
-  end
+  url "https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v#{version}/Heroic-#{version}-macOS-#{arch}.dmg"
+  sha256 intel: "39f48a8aa39ee7bf17767e9afca2abb974426f5856fa8794e2364cf7b0fcd9bc",
+                arm:"a82788df9b8e5603911280b9f7ef403c84ee9688e44fc4c90747a0b265646486"
 
   name "Heroic Games Launcher"
   desc "Open Source Game Launcher"
