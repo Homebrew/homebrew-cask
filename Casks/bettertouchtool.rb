c2a0023e8f81cc2fcd61a85362ff7090112b5389
Amory Meltzer <Amorymeltzer@gmail.com>
2022-12-13 20:04:06 -0500

    Update bettertouchtool from 3.962 to 3.963

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -1,6 +1,6 @@
 cask "bettertouchtool" do
-  version "3.962,2127"
-  sha256 "2bb309288bfbdde821707df6987587cabd841f3c4305b8d5ad684c93adfad324"
+  version "3.963,2128"
+  sha256 "160efd13131572e5560b5dcf1d23e030b4217e8e12e94e9e4539edabcb2a1bff"
 
   url "https://folivora.ai/releases/btt#{version.csv.first}-#{version.csv.second}.zip"
   name "BetterTouchTool"
