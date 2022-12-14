a1c47fe5f4f5eed38c0bc7f85f0537c8eb66b384
Patrick Linnane <patrick@linnane.io>
2022-12-13 20:52:58 -0800

    Update cloudapp from 6.6.9 to 6.6.10

--- Casks/cloudapp.rb
+++ Casks/cloudapp.rb
@@ -1,8 +1,8 @@
 cask "cloudapp" do
-  version "6.6.9,2341"
-  sha256 "95cab68ff040e312862ea739992cc4f5e5b5a2329f3b161acc1715188a549a65"
+  version "6.6.10,2355"
+  sha256 "ec14314e336c562e66e106dde8271b8290e3f3ceff9ce34990a9548facac0597"
 
-  url "https://downloads.getcloudapp.com/mac/CloudApp-#{version.csv.first}.#{version.csv.last}.zip"
+  url "http://downloads.getcloudapp.com/mac/CloudApp-#{version.csv.first}.#{version.csv.last}.zip"
   name "CloudApp"
   desc "Visual communication platform"
   homepage "https://www.getcloudapp.com/"
