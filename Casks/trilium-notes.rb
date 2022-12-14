f35e56a2a637b2c80568ca078b23abb8f7428aec
Patrick Linnane <patrick@linnane.io>
2022-12-13 19:59:08 -0800

    Update trilium-notes from 0.57.4 to 0.57.5

--- Casks/trilium-notes.rb
+++ Casks/trilium-notes.rb
@@ -1,6 +1,6 @@
 cask "trilium-notes" do
-  version "0.57.4"
-  sha256 "b7753546e5d5b94e9a8034a5cdc60c29f606310ad0d41961215d1e12c3a4a68c"
+  version "0.57.5"
+  sha256 "fde732bb60f7582763d64c7d25fef034084a862a2a656e3097edef6e4ae0fff7"
 
   url "https://github.com/zadam/trilium/releases/download/v#{version}/trilium-mac-x64-#{version}.zip"
   name "Trilium Notes"
