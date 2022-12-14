8c06bc1c35588e17c778064a75d73ac45eada16e
Marcin Mitura <mitura07@wp.pl>
2022-12-14 12:29:17 +0100

    Forgotten `brew style --fix` after changes.

--- Casks/heroic.rb
+++ Casks/heroic.rb
@@ -1,11 +1,11 @@
 cask "heroic" do
-  version "2.5.2"
   arch arm: "arm64", intel: "x64"
 
-  url "https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v#{version}/Heroic-#{version}-macOS-#{arch}.dmg"
+  version "2.5.2"
   sha256 intel: "39f48a8aa39ee7bf17767e9afca2abb974426f5856fa8794e2364cf7b0fcd9bc",
-                arm:"a82788df9b8e5603911280b9f7ef403c84ee9688e44fc4c90747a0b265646486"
+         arm:   "a82788df9b8e5603911280b9f7ef403c84ee9688e44fc4c90747a0b265646486"
 
+  url "https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v#{version}/Heroic-#{version}-macOS-#{arch}.dmg"
   name "Heroic Games Launcher"
   desc "Open Source Game Launcher"
   homepage "https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher"
