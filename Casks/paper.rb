ccaaeac3a3ae5ae802012db4151f61b9bcab439b
Xi <psi.xie@gmail.com>
2022-12-14 13:19:00 +0800

    Update pap.er from 5.2.5 to 5.2.6 (#137694)
    * Update pap.er from 5.2.5 to 5.2.6
    
    * Update paper.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/paper.rb
+++ Casks/paper.rb
@@ -1,16 +1,16 @@
 cask "paper" do
-  version "5.2.5"
-  sha256 "ef1f6f8848ca225fea03814b5431c1ae4177538c8ae3b0c7eeb80c7bc104b74c"
+  version "5.2.6"
+  sha256 "54c5318629932a06580b323406cacef97802e2bad997a03d5bacfbb37feac584"
 
   url "https://s3.nsns.in/dl/paper-v#{version}.dmg",
       verified: "s3.nsns.in/"
   name "pap.er"
   desc "Pap.er, 4K 5K HD Wallpaper Application"
-  homepage "https://paper.photos/"
+  homepage "https://paper.meiyuan.in/"
 
   livecheck do
     url :homepage
-    regex(/href=.*?paper[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+    regex(/paper[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
   auto_updates true
