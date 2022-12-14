a5d661ecd4618ccd44203040bafa258983cdf5be
Bevan Kay <email@bevankay.me>
2022-12-14 12:57:47 +1100

    reaper: convert to `on_system` blocks

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,10 +1,11 @@
 cask "reaper" do
   version "6.71"
 
-  if MacOS.version <= :mojave
+  on_mojave :or_older do
     sha256 "1d2cd9bd0012cec3120f6711bb97336f3b1eff059fa0bf42742eb3730834ab99"
     url "https://dlcf.reaper.fm/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64.dmg"
-  else
+  end
+  on_catalina :or_newer do
     sha256 "b6f1effd0bcee4ee56df7d239eadcf3251b54e3760699fedfc7856669f6e0ce5"
     url "https://dlcf.reaper.fm/#{version.major}.x/reaper#{version.major_minor.no_dots}_universal.dmg"
   end
