e69dd09b091c680d591581a70cc2d27168d28211
Patrick Linnane <patrick@linnane.io>
2022-12-13 19:19:20 -0800

    busycontacts 2022.4.4,2022-12-13-20-12
    Update busycontacts from 2022.4.4,2022-12-13-04-48 to 2022.4.4,2022-12-13-20-12
    
    Closes #137671.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/busycontacts.rb
+++ Casks/busycontacts.rb
@@ -1,6 +1,6 @@
 cask "busycontacts" do
-  version "2022.4.4,2022-12-13-04-48"
-  sha256 "efaf11e4054cbf814bb6c5c4e0b9749f00eaf4a7c025884ed907a558f1a1194a"
+  version "2022.4.4,2022-12-13-20-12"
+  sha256 "3863bd0ac2563864cb74ab27fb671772eb8244e237f2441b5183ba62364e7c6f"
 
   url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bct-#{version.csv.first}-#{version.csv.second}.zip",
       verified: "7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/"
