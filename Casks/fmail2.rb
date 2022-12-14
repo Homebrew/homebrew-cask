23bbf350838cf66f785778650a86445ab5549f7b
Patrick Linnane <patrick@linnane.io>
2022-12-13 19:37:02 -0800

    Update fmail2 from 1.4.5 to 1.4.6

--- Casks/fmail2.rb
+++ Casks/fmail2.rb
@@ -1,7 +1,7 @@
 cask "fmail2" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "1.4.5,40"
-  sha256 "8da6be33b64460bed87f8db77fe8d7f7aa13b23264cb60baedad70dfabb1da02"
+  version "1.4.6,41"
+  sha256 "a53f7a7147fc85bbaeb63797ee58d71851b323d4ac3ff7abe6f21cd2bc219872"
 
   url "https://arievanboxel.fr/fmail2/sparkle/FMail2_#{version.csv.first.no_dots}.zip",
       verified: "arievanboxel.fr"
