5c4e12cde7a44b76f9c05e63cc3ecb949ae0fbcc
Patrick Linnane <patrick@linnane.io>
2022-12-13 19:58:41 -0800

    trainerroad 2022.45.0.218
    Update trainerroad from 2022.44.1.217 to 2022.45.0.218
    
    Closes #137723.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/trainerroad.rb
+++ Casks/trainerroad.rb
@@ -1,6 +1,6 @@
 cask "trainerroad" do
-  version "2022.44.1.217"
-  sha256 "b9e3d2e093ff2796f9ccc4a5676061fe1ee483abd4d5d08ac8cd00140c16413d"
+  version "2022.45.0.218"
+  sha256 "52ec8ec43239b96e75e46a814b8dad74da0b1cfcae5dd03f0114dbc80223f93b"
 
   url "https://trainrdtrcmn01un1softw01.blob.core.windows.net/installers/mac/v001/Production/TrainerRoad-#{version}.dmg",
       verified: "trainrdtrcmn01un1softw01.blob.core.windows.net/"
