1c8fb7eddbd3739505e40f824c9645894baaaff5
Marcin Mitura <mitura07@wp.pl>
2022-12-14 12:09:27 +0100

    heroic 2.5.2
    * Update heroic.rb
    * Update Casks/heroic.rb
    * Update Casks/heroic.rb
    * Forgotten `brew style --fix` after changes.
    
    Closes #137734.
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/heroic.rb
+++ Casks/heroic.rb
@@ -1,8 +1,11 @@
 cask "heroic" do
-  version "2.5.1"
-  sha256 "2131c753b37caf889f4e47f8780d1670ce8fec14c225f13af6419f2e93a2700f"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v#{version}/Heroic-#{version}.dmg"
+  version "2.5.2"
+  sha256 intel: "39f48a8aa39ee7bf17767e9afca2abb974426f5856fa8794e2364cf7b0fcd9bc",
+         arm:   "a82788df9b8e5603911280b9f7ef403c84ee9688e44fc4c90747a0b265646486"
+
+  url "https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v#{version}/Heroic-#{version}-macOS-#{arch}.dmg"
   name "Heroic Games Launcher"
   desc "Open Source Game Launcher"
   homepage "https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher"
