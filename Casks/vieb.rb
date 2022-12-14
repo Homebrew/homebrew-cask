0e04c318579f3f6a5b4e04210c3c428383391fab
Patrick Linnane <patrick@linnane.io>
2022-12-14 12:11:06 -0800

    Update vieb from 9.4.0 to 9.5.0

--- Casks/vieb.rb
+++ Casks/vieb.rb
@@ -1,9 +1,9 @@
 cask "vieb" do
   arch arm: "arm64-"
 
-  version "9.4.0"
-  sha256 arm:   "08529b36f105deff695014529108dd2fb6005829246d51c0b0d9b2702b10619f",
-         intel: "9356da853b16223a3aa930684d9fb020fc18e0afcac4002ac6ffd26419840d0b"
+  version "9.5.0"
+  sha256 arm:   "721dd6627d1e7a8c7210378a8c3427d50f69e423ff31a58075308089b63bdd49",
+         intel: "881a30184898346b1ccb34e0b04826a28f57b6b133af9543c8b37b0f211d04b7"
 
   url "https://github.com/Jelmerro/Vieb/releases/download/#{version}/Vieb-#{version}-#{arch}mac.zip",
       verified: "github.com/Jelmerro/Vieb/"
