a7cfd1ab6a0b9808c656671d6addf285f4b262b7
vonProteus <>
2022-12-14 15:23:55 +0100

    Update retroarch from 1.14.0 to 1.14.0

--- Casks/retroarch.rb
+++ Casks/retroarch.rb
@@ -1,6 +1,6 @@
 cask "retroarch" do
   version "1.14.0"
-  sha256 "6c10454d35d2e98ab3d06f0de97d2e320623f9bd4f6565d63675b81fc944f258"
+  sha256 "543740daefdbc28e2378ca32551a4bbd3f5c9f0d99108192c557875f5cfaf195"
 
   url "https://buildbot.libretro.com/stable/#{version}/apple/osx/x86_64/RetroArch.dmg"
   name "RetroArch"
