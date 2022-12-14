83c382b40ff67a9d07dcf58f1247cb220da7ec72
Anton Möller <anton@esportal.com>
2022-12-14 23:48:40 +0100

    Update tableau-reader from 2022.3.1 to 2022.4.0

--- Casks/tableau-reader.rb
+++ Casks/tableau-reader.rb
@@ -1,6 +1,6 @@
 cask "tableau-reader" do
-  version "2022.3.1"
-  sha256 "d91bcfc1095cb671ddd090825d00440c7333e01acba51b55d338794b97a70062"
+  version "2022.4.0"
+  sha256 "e53eee00a70376579b61ca77e94d7717c1893dc37947bae924a159d7799fb4b8"
 
   url "https://downloads.tableau.com/tssoftware/TableauReader-#{version.dots_to_hyphens}.dmg"
   name "Tableau Reader"
