2714fea7d1082537863d8d29c485f719b03f7c7c
Hayden Duffy <45819703+duffpop@users.noreply.github.com>
2022-12-14 21:44:18 +0000

    Add Cask: Bambu Studio v01.03.01.01 (#137642)
    * Add Cask: Bambu Studio
    
    * Update Casks/bambu-studio.rb
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>
    
    * Update Casks/bambu-studio.rb
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>
    
    * Update Casks/bambu-studio.rb
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

---
+++ Casks/bambu-studio.rb
@@ -0,0 +1,26 @@
+cask "bambu-studio" do
+  version "01.03.01.01"
+  sha256 "5b7f92514f3b7b3770edd2491fd2106edaf9659c0319fd5017e02314aafa9f62"
+
+  url "https://public-cdn.bambulab.com/upgrade/studio/software/#{version}/Bambu_Studio_mac-v#{version}.dmg"
+  name "Bambu Studio"
+  desc "3D model slicing software for 3D printers, maintained by Bambu Lab"
+  homepage "https://bambulab.com/en/download/studio"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?Bambu[._-]Studio[._-]mac[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+  end
+
+  app "BambuStudio.app"
+
+  zap trash: [
+    "/Library/Logs/DiagnosticsReports/BambuStudio*",
+    "~/Library/Application Support/BambuStudio",
+    "~/Library/Caches/com.bambulab.bambu-studio",
+    "~/Library/HTTPStorages/com.bambulab.bambu-studio.binarycookies",
+    "~/Library/Preferences/com.bambulab.bambu-studio.plist",
+    "~/Library/Saved Application State/com.bambulab.bambu-studio.savedState",
+    "~/Library/WebKit/com.bambulab.bambu-studio",
+  ]
+end
