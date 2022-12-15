ebd4b3c679f6ab1840bdf7cc3f957ad4dc17fd49
Bevan Kay <email@bevankay.me>
2022-12-15 19:14:36 +1100

    couchbase-server-enterprise: convert to `on_system` blocks

--- Casks/couchbase-server-enterprise.rb
+++ Casks/couchbase-server-enterprise.rb
@@ -1,12 +1,13 @@
 cask "couchbase-server-enterprise" do
-  if MacOS.version <= :el_capitan
+  on_el_capitan :or_older do
     version "4.5.1"
     sha256 "de014c7c134eb97ff00be6b2e6f5d0da84295ce05bbb7bb3a4d3c747a365cd22"
 
     url "https://packages.couchbase.com/releases/#{version}/couchbase-server-enterprise_#{version}-macos_x86_64.zip"
 
     app "couchbase-server-enterprise_#{version}/Couchbase Server.app"
-  else
+  end
+  on_sierra :or_newer do
     version "7.1.3"
     sha256 "2acf5f3d32482b9595686b1d236be1d477dfa6fe06fd8ee81e2f0b77f824c589"
 
