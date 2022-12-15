33cb7b6dad1bede5f7aec184f343f87ce5516d71
Patrick Linnane <patrick@linnane.io>
2022-12-15 12:25:37 -0800

    Update zoho-mail from 1.5.3 to 1.5.4

--- Casks/zoho-mail.rb
+++ Casks/zoho-mail.rb
@@ -1,9 +1,9 @@
 cask "zoho-mail" do
   arch arm: "arm64-"
 
-  version "1.5.3"
-  sha256 arm:   "e612ef18523dcff2fc35cc33559e166b3d15f685c3d9252810ee409e2ee2efa7",
-         intel: "42fa824b1a1bdafc7115e621fea5a3b5f468b869fe39f43801cfff5e63a2f879"
+  version "1.5.4"
+  sha256 arm:   "1306a455fb5711cf49f65c7623c3c7a0e58d5d429a1ea2d31b1f759fe3e60c96",
+         intel: "a599c992e72b3ac61d2c13327a1a7d64020e803a55c4f79c29a6ebec121e755e"
 
   url "https://downloads.zohocdn.com/zmail-desktop/mac/zoho-mail-desktop-installer-#{arch}v#{version}.dmg",
       verified: "downloads.zohocdn.com/zmail-desktop/mac/"
