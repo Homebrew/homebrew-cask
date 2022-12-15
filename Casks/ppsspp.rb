c55f6e4ff5f857fda51473aedf03257065f4a816
Aaron Jheng <wentworth@outlook.com>
2022-12-15 23:59:50 +0800

    ppsspp 1.14
    Update ppsspp from 1.13.2 to 1.14
    
    Closes #137826.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/ppsspp.rb
+++ Casks/ppsspp.rb
@@ -1,6 +1,6 @@
 cask "ppsspp" do
-  version "1.13.2"
-  sha256 "6d047749b4c9566ee579e11375c6b8483fe2da95657db9a2f5f8eaeb7bef2b32"
+  version "1.14"
+  sha256 "4ebce22aea17977cbc5cd307169e0103c2e5cc41bebd6dfa410ff4d4c2f3c1c8"
 
   url "https://github.com/hrydgard/ppsspp/releases/download/v#{version}/PPSSPPSDL-macOS-v#{version}.zip",
       verified: "github.com/hrydgard/ppsspp/"
