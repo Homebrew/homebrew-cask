5cf6592caf60d7e2ff3efe21e8e72eacc597ca92
Bevan Kay <email@bevankay.me>
2022-12-15 19:17:24 +1100

    datweatherdoe: convert to `on_system` blocks

--- Casks/datweatherdoe.rb
+++ Casks/datweatherdoe.rb
@@ -1,27 +1,26 @@
 cask "datweatherdoe" do
-  if MacOS.version <= :catalina
+  on_catalina :or_older do
     version "2.2.0"
     sha256 "0e7c7a9a770f3f7e10a17610e4e8670f3c7050a872b1cb2947bfbbbfda94174f"
-  else
+
+    livecheck do
+      skip "Legacy version for Catalina and earlier"
+    end
+  end
+  on_big_sur :or_newer do
     version "3.1.0"
     sha256 "19eac800249b286f7543991ab297c808aa9f96ee1412a0f133bd20278b129ede"
+
+    livecheck do
+      url :url
+      strategy :github_latest
+    end
   end
 
   url "https://github.com/inderdhir/DatWeatherDoe/releases/download/#{version}/DatWeatherDoe-#{version}.dmg"
   name "DatWeatherDoe"
   desc "Menu bar weather app"
   homepage "https://github.com/inderdhir/DatWeatherDoe"
-
-  if MacOS.version <= :catalina
-    livecheck do
-      skip "Legacy version for Catalina and earlier"
-    end
-  else
-    livecheck do
-      url :url
-      strategy :github_latest
-    end
-  end
 
   app "DatWeatherDoe.app"
 
