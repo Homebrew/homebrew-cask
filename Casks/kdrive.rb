16a306deee19716213273cf0a23238f3fe843f76
Patrick Linnane <patrick@linnane.io>
2022-12-15 11:45:06 -0800

    kdrive 3.3.7.20221110
    Update kdrive from 3.3.6.20221025 to 3.3.7.20221110
    
    Closes #137858.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/kdrive.rb
+++ Casks/kdrive.rb
@@ -1,6 +1,6 @@
 cask "kdrive" do
-  version "3.3.6.20221025"
-  sha256 "78725c34dcda001a763996ace20c1e94ac9268c86c6cce1134d183538b73fe4b"
+  version "3.3.7.20221110"
+  sha256 "0abd708fce895f998df07e581b279cb67b7d640605cac52252df2254a26a4df1"
 
   url "https://download.storage.infomaniak.com/drive/desktopclient/kDrive-#{version}.pkg"
   name "kDrive"
