f14c795f75c46bff139aae870148e7a5d5beda77
Ross Goldberg <484615+rgoldberg@users.noreply.github.com>
2022-12-15 12:48:10 -0500

    Update bettertouchtool from 3.965 to 3.967

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -1,6 +1,6 @@
 cask "bettertouchtool" do
-  version "3.965,2130"
-  sha256 "59c682a340a2a755957266c0f7ad82043f9f121320cedec0b662cdff456f2f9b"
+  version "3.967,2132"
+  sha256 "a944d4a28bfcd54127972616720720c3f68a538720dd9e703284ded58ded6a56"
 
   url "https://folivora.ai/releases/btt#{version.csv.first}-#{version.csv.second}.zip"
   name "BetterTouchTool"
