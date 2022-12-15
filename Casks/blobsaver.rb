d19710d9420fc174c514fbfeed8a808374a71127
Patrick Linnane <patrick@linnane.io>
2022-12-15 10:19:41 -0800

    Update blobsaver from 3.4.0 to 3.4.1

--- Casks/blobsaver.rb
+++ Casks/blobsaver.rb
@@ -1,9 +1,9 @@
 cask "blobsaver" do
   arch arm: "arm", intel: "intel"
 
-  version "3.4.0"
-  sha256 arm:   "9705418a6f4eb714acae69624a80cd461773ca95a70222c93b25ede14871049a",
-         intel: "8625a1274ae2357a8b7236977b366b1aa11a7b900a848da764500c079e37b478"
+  version "3.4.1"
+  sha256 arm:   "60e7ab55d450eb3d41f29a5156a92af8fb7e5dfbe278da2a623f8bd16b2eb4ea",
+         intel: "4cfb291ec952616c1cb63a4e075a4e4327701ba6028ff3880ce4a5d131296609"
 
   url "https://github.com/airsquared/blobsaver/releases/download/v#{version}/blobsaver-#{arch}-#{version}.dmg"
   name "blobsaver"
