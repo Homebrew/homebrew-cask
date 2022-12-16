86b1a05850ef64562e56a4fdfe1523be992c83a3
Patrick Linnane <patrick@linnane.io>
2022-12-15 18:40:09 -0800

    chemdoodle 11.12.0
    Update chemdoodle from 11.11.0 to 11.12.0
    
    Closes #137916.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/chemdoodle.rb
+++ Casks/chemdoodle.rb
@@ -1,8 +1,11 @@
 cask "chemdoodle" do
-  version "11.11.0"
-  sha256 "b66cbbd7b61efd4fd194d16e5b3b160d44f27641ec53474225ee5f7079827133"
+  arch arm: "aarch64", intel: "x64"
 
-  url "https://www.ichemlabs.com/downloads/ChemDoodle-macos-#{version}.dmg"
+  version "11.12.0"
+  sha256 arm:   "e04115f90e3f6d6cdf2ba4e73134ee9f780a9d0f9be46f940ff3ce92a23e5f46",
+         intel: "75803a20ea2c7dbd338dc82dbf452444ed3ba1fb641cb795370d86a73435f43b"
+
+  url "https://www.ichemlabs.com/downloads/ChemDoodle-macos-#{arch}-#{version}.dmg"
   name "ChemDoodle"
   name "ChemDoodle 2D"
   desc "2D chemical drawing, publishing and informatics"
@@ -10,7 +13,7 @@ cask "chemdoodle" do
 
   livecheck do
     url "https://www.ichemlabs.com/download#chemdoodle/osx-installation-instructions/"
-    regex(%r{href=.*?/ChemDoodle[._-]macos[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
+    regex(%r{href=.*?/ChemDoodle[._-]macos[._-]#{arch}[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
   end
 
   depends_on macos: ">= :el_capitan"
