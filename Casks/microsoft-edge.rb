2cfccec1403bc9e565c851e5c3b96014dc610620
Patrick Linnane <patrick@linnane.io>
2022-12-15 18:11:05 -0800

    Update microsoft-edge from 108.0.1462.46 to 108.0.1462.54

--- Casks/microsoft-edge.rb
+++ Casks/microsoft-edge.rb
@@ -3,9 +3,9 @@ cask "microsoft-edge" do
                                intel: "C1297A47-86C4-4C1F-97FA-950631F94777"
   linkid = on_arch_conditional arm: "2093504", intel: "2069148"
 
-  version "108.0.1462.46"
-  sha256 arm:   "859878b9a7147d19edd65b1b6596b8c981aab2de8810840e1bd91494fe634b5a",
-         intel: "c936e9800b0c5fc72818d6fe6f0dec4a9df2a17d418fc0a8d7d0228098d2e2ee"
+  version "108.0.1462.54"
+  sha256 arm:   "a72520fd25eefb3bf4c2a15ac5a0cb9af86505871ab590527a4d128410ed1d67",
+         intel: "9b3609dc68ba91605282950b7290b961af779aec8d8e52db747802e472ebb6af"
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/#{folder}/MacAutoupdate/MicrosoftEdge-#{version}.pkg",
       verified: "officecdn-microsoft-com.akamaized.net/"
