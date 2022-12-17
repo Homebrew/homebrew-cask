d9c11686a13bb8009a2eadd979865640838a3445
Patrick Linnane <patrick@linnane.io>
2022-12-16 18:04:13 -0800

    Update segger-embedded-studio-for-arm from 7.10 to 7.10a

--- Casks/segger-embedded-studio-for-arm.rb
+++ Casks/segger-embedded-studio-for-arm.rb
@@ -1,9 +1,9 @@
 cask "segger-embedded-studio-for-arm" do
   arch arm: "arm64", intel: "x64"
 
-  version "7.10"
-  sha256 arm:   "e9d3ee36529dac0873394574b17170afc3a7cba5170cd210e3a83d68b3151879",
-         intel: "869060ad15cfe64b4fb8e66efded215fbfe38d2d1906f12589325b8e7cf28f87"
+  version "7.10a"
+  sha256 arm:   "617b5b413be463a857d54fbc9609ee83b99eb77c3fa4ce615124c3e86ca11e55",
+         intel: "96dfc8f291b2a75a85cb1f2cc0d92cf4179d56f9c176083916d7d5f9e53d5c9b"
 
   url "https://www.segger.com/downloads/embedded-studio/Setup_EmbeddedStudio_ARM_v#{version.no_dots}_macos_#{arch}.dmg"
   name "SEGGER Embedded Studio for ARM"
