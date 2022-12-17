0c237e1e557b1582f48994103ab0fb1fee856b1b
Jeffrey Bailey <Jeffrey.Bailey2@nike.com>
2022-12-17 12:02:26 -0800

    add vero

---
+++ Casks/vero.rb
@@ -0,0 +1,22 @@
+cask "vero" do
+  version "0.5.4"
+  sha256 :no_check
+
+  url "https://vero.co/download/desktop/beta/macos"
+  name "vero"
+  desc "Ad-free, Algorithm-free Social."
+  homepage "https://vero.co/"
+
+  livecheck do
+    url "https://vero.co/download/desktop/beta/macos"
+    strategy :header_match
+  end
+
+  app "VERO.app"
+
+  zap trash: [
+    "~/Library/Application Support/VERO",
+    "~/Library/Preferences/co.vero.desktop.plist",
+    "~/Library/Saved Application State/co.vero.desktop.savedState",
+  ]
+end
