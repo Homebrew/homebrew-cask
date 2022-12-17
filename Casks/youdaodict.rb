e5f0e9be8bc5bde048a3e63dd900ec14de729bf6
Zhang Maiyun <me@myzhangll.xyz>
2022-12-17 11:39:52 -0800

    Update the zap stanza of youdaodict (#138007)
    * Update the zap stanza of youdaodict
    
    Signed-off-by: Zhang Maiyun <me@myzhangll.xyz>
    
    * Update youdaodict.rb
    
    Signed-off-by: Zhang Maiyun <me@myzhangll.xyz>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/youdaodict.rb
+++ Casks/youdaodict.rb
@@ -18,9 +18,12 @@ cask "youdaodict" do
   app "网易有道词典.app"
 
   zap trash: [
+    "~/Library/Application Scripts/com.youdao.YoudaoDict",
     "~/Library/Application Support/com.youdao.YoudaoDict",
+    "~/Library/com.youdao.YoudaoDict",
+    "~/Library/Containers/com.youdao.YoudaoDict",
     "~/Library/Cookies/com.youdao.YoudaoDict.binarycookies",
     "~/Library/Preferences/com.youdao.YoudaoDict.plist",
-    "~/Library/com.youdao.YoudaoDict",
+    "~/Library/Saved Application State/com.youdao.YoudaoDict.savedState",
   ]
 end
