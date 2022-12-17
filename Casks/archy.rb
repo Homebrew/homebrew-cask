071ea22f1358597caf383fe794daa5ac3e198090
Patrick Linnane <patrick@linnane.io>
2022-12-16 17:04:26 -0800

    Update archy from 2.15.3 to 2.16.0

--- Casks/archy.rb
+++ Casks/archy.rb
@@ -1,6 +1,6 @@
 cask "archy" do
-  version "2.15.3"
-  sha256 "aef13e0efa619bae0ba2140369c855f6d3c5f851bc3437ff45e800f6b3d3abfd"
+  version "2.16.0"
+  sha256 "6b614eec3cbcbbbbdff8ba97769eb56d66c1c1f9b3b370de118741f82bf0a6c5"
 
   url "https://sdk-cdn.mypurecloud.com/archy/#{version}/archy-macos.zip",
       verified: "sdk-cdn.mypurecloud.com/archy/"
