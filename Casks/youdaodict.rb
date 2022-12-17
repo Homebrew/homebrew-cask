025deb862ce819ed1992eed88f90afe234a1a27c
Miccal Matthews <miccal.matthews@gmail.com>
2022-12-18 03:29:38 +0800

    Update youdaodict.rb

--- Casks/youdaodict.rb
+++ Casks/youdaodict.rb
@@ -20,10 +20,10 @@ cask "youdaodict" do
   zap trash: [
     "~/Library/Application Scripts/com.youdao.YoudaoDict",
     "~/Library/Application Support/com.youdao.YoudaoDict",
+    "~/Library/com.youdao.YoudaoDict",
     "~/Library/Containers/com.youdao.YoudaoDict",
     "~/Library/Cookies/com.youdao.YoudaoDict.binarycookies",
     "~/Library/Preferences/com.youdao.YoudaoDict.plist",
     "~/Library/Saved Application State/com.youdao.YoudaoDict.savedState",
-    "~/Library/com.youdao.YoudaoDict",
   ]
 end
