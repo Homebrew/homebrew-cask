ed42bca8e0472ce3a0b1ed576d75d3e5d9d77485
Patrick Linnane <patrick@linnane.io>
2022-12-17 15:07:22 -0800

    metasploit 6.2.32,20221217113004
    Update metasploit from 6.2.31 to 6.2.32
    
    Closes #138017.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/metasploit.rb
+++ Casks/metasploit.rb
@@ -1,6 +1,6 @@
 cask "metasploit" do
-  version "6.2.31,20221215113026"
-  sha256 "bdf4e40d1f6e1f75c5ca6759bed5475e8c060d9420693d9fd9c25efdae0fbff4"
+  version "6.2.32,20221217113004"
+  sha256 "2b3c93f2c30e9b1fbbc6d46c1f820a7da52c615b01d0fc32c14f4ae088e955ce"
 
   url "https://osx.metasploit.com/metasploit-framework-#{version.csv.first}%2B#{version.csv.second}-1rapid7-1.x86_64.pkg"
   name "Metasploit Framework"
