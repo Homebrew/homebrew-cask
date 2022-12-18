35f6943ac66ed7bd0ecb5b2bb1fbc78f32921df0
EricFromCanada <enk3@outlook.com>
2022-12-18 15:19:36 -0500

    proxifier: convert to on_system blocks

--- Casks/proxifier.rb
+++ Casks/proxifier.rb
@@ -1,16 +1,19 @@
 cask "proxifier" do
-  if MacOS.version <= :catalina
+  on_catalina :or_older do
     version "2.26"
-    sha256 :no_check
-    url "https://www.proxifier.com/download/ProxifierMac.dmg"
+    sha256 "671c9e8bfb8619b8c39574bad38d3add7b460e491e99a6dfbace6dd3f7535e69"
+
+    url "https://www.proxifier.com/download/legacy/ProxifierMac#{version.no_dots}.dmg"
 
     livecheck do
       url "https://www.proxifier.com/changelog/mac#{version.major}.html"
       regex(/Version\s*(\d+(?:\.\d+)+)/i)
     end
-  else
+  end
+  on_big_sur :or_newer do
     version "3.7"
     sha256 "ed96cb643aa52292410109850997165d4c80f2782d596e018f53b33e45415ed3"
+
     url "https://www.proxifier.com/download/ProxifierMac#{version.major}.dmg"
 
     livecheck do
