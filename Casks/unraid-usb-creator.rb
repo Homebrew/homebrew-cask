70c863c6d02cade21f86d3ef1c18df760a1d7f12
Casey Korver <casey@korver.dev>
2022-12-17 20:01:20 -0600

    Address offences

--- Casks/unraid-usb-creator.rb
+++ Casks/unraid-usb-creator.rb
@@ -1,18 +1,18 @@
 cask "unraid-usb-creator" do
-    version "2.1"
-    sha256 "befa042b7fd611f106e182a3128e197173a15072e9f36f979dc89776e698da84"
-  
-    url "https://craftassets.unraid.net/uploads/downloads/Unraid.USB.Creator.macOS-#{version}.dmg"
-    name "Unraid USB Creator"
-    desc "Utility for installing Unraid on a USB drive"
-    homepage "https://unraid.net"
+  version "2.1"
+  sha256 "befa042b7fd611f106e182a3128e197173a15072e9f36f979dc89776e698da84"
 
-    livecheck do
-        url "https://unraid.net/download"
-        strategy :page_match
-        regex(/href=.*?Unraid[._-]USB[._-]Creator[._-]macOS[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
-      end      
+  url "https://craftassets.unraid.net/uploads/downloads/Unraid.USB.Creator.macOS-#{version}.dmg"
+  name "Unraid USB Creator"
+  desc "Utility for installing Unraid on a USB drive"
+  homepage "https://unraid.net/"
 
-    app "Unraid USB Creator.app"
+  livecheck do
+    url "https://unraid.net/download"
+    strategy :page_match
+    regex(/href=.*?Unraid[._-]USB[._-]Creator[._-]macOS[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+  end      
+
+  app "Unraid USB Creator.app"
 end
   
