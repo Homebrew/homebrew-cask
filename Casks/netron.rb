5524200673c242c9ed641c556c5597761b71f04b
Lutz <lutzroeder@users.noreply.github.com>
2022-12-18 15:40:53 +0000

    netron 6.3.1
    Update Netron to 6.3.1
    
    Closes #138029.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/netron.rb
+++ Casks/netron.rb
@@ -1,6 +1,6 @@
 cask "netron" do
-  version "6.2.9"
-  sha256 "202e27cd5ad3fa302eaae29820db9fe37931fc56bfa63b4a0a8bcf6f90ef37bb"
+  version "6.3.1"
+  sha256 "e596279fc1533682311a5a2175c043a8e0dda74f8608867a7918b83c8873ebca"
 
   url "https://github.com/lutzroeder/netron/releases/download/v#{version}/Netron-#{version}-mac.zip"
   name "Netron"
