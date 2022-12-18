eb43f68b67ea3f4dd06bb3bd9957fa68b499e09a
Lutz <lutzroeder@users.noreply.github.com>
2022-12-18 04:49:28 +0000

    netron 6.2.8
    Update Netron to 6.2.8
    
    Closes #138023.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/netron.rb
+++ Casks/netron.rb
@@ -1,6 +1,6 @@
 cask "netron" do
-  version "6.2.7"
-  sha256 "89f92dbaf6b6e9cb7bdfe8da8d932db1138eb6435365a85794d2c8db78480adb"
+  version "6.2.8"
+  sha256 "159a25612eddee8062c4e2369983f36fd8b3c002549f4d026335d3446feed631"
 
   url "https://github.com/lutzroeder/netron/releases/download/v#{version}/Netron-#{version}-mac.zip"
   name "Netron"
