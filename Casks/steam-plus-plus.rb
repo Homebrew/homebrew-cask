927e7fecc34ee856ec674a972e84c0f5b5a46343
Patrick Linnane <patrick@linnane.io>
2022-12-19 11:39:52 -0800

    Update steam-plus-plus from 2.8.5 to 2.8.6

--- Casks/steam-plus-plus.rb
+++ Casks/steam-plus-plus.rb
@@ -1,9 +1,9 @@
 cask "steam-plus-plus" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.8.5"
-  sha256 arm:   "1989de947fd6608314a0c53dcc6c0d58d98167a5bda0806745d3c12760fc8f5c",
-         intel: "bb506447eaf8649ff55d559f9fdbc950c6f5c66d7c0095863ec8e9b1220f0f92"
+  version "2.8.6"
+  sha256 arm:   "772cfb10035f5dc290aa6a1e6f373826db4f45e8bd65e4d06083e4973b576dad",
+         intel: "6c03b9358532254c2ae3329fcdbdad58c165f08343f759064f3a5c20bc59212e"
 
   url "https://github.com/BeyondDimension/SteamTools/releases/download/#{version}/Steam++_macos_#{arch}_v#{version}.dmg",
       verified: "github.com/BeyondDimension/SteamTools/"
