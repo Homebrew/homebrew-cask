dbe8fe55dc4732213746381b059e543f074816b2
Patrick Linnane <patrick@linnane.io>
2022-12-19 11:36:11 -0800

    reaper 6.72
    Update reaper from 6.71 to 6.72
    
    Closes #138099.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,12 +1,12 @@
 cask "reaper" do
-  version "6.71"
+  version "6.72"
 
   on_mojave :or_older do
     sha256 "1d2cd9bd0012cec3120f6711bb97336f3b1eff059fa0bf42742eb3730834ab99"
     url "https://dlcf.reaper.fm/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64.dmg"
   end
   on_catalina :or_newer do
-    sha256 "b6f1effd0bcee4ee56df7d239eadcf3251b54e3760699fedfc7856669f6e0ce5"
+    sha256 "d119e133f677b8a1a60a3b3474109024c5e30877675e5a9d2d906531b6cd2a3b"
     url "https://dlcf.reaper.fm/#{version.major}.x/reaper#{version.major_minor.no_dots}_universal.dmg"
   end
 
