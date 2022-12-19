de888fc8cef9cde0e1fe1861c0c75b84a4e79aab
Patrick Linnane <patrick@linnane.io>
2022-12-19 11:27:23 -0800

    Update millie from 5.1.0 to 5.1.1

--- Casks/millie.rb
+++ Casks/millie.rb
@@ -1,6 +1,6 @@
 cask "millie" do
-  version "5.1.0"
-  sha256 "385d117d6c6640915004a4a4d85f3eec96fad65f812eebd236d137073eb47eba"
+  version "5.1.1"
+  sha256 "8fffd1691353a2ed65e60c76a6f9c8eab7d32b4d00fad582e60b810f0caa0bf3"
 
   url "https://apis.millie.co.kr/v1/download/installer/mac/Millie-#{version}.dmg"
   name "millie"
