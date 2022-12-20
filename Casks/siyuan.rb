ebc4c973be957c76edf461464c17ec564ebb20ec
Patrick Linnane <patrick@linnane.io>
2022-12-20 12:11:48 -0800

    Update siyuan from 2.5.4 to 2.5.5

--- Casks/siyuan.rb
+++ Casks/siyuan.rb
@@ -1,9 +1,9 @@
 cask "siyuan" do
   arch arm: "-arm64"
 
-  version "2.5.4"
-  sha256 arm:   "31f3fe817cf4fc726969e94a10fa008c9521a0ebf1f18b52d372e6ae1dacab90",
-         intel: "a87998197aefbc53f836e7fe97fee262bc55dd7bd273adab2c1f94a46aa0b7e0"
+  version "2.5.5"
+  sha256 arm:   "3e17ac328913af3c867d9a3c009ea44605b40995c122b3453a762cd593f7cdf8",
+         intel: "55543e60e6e64cc3b08fd9ffc22de01639a94777bf10d6bbc2dfb52cb2181d2c"
 
   url "https://github.com/siyuan-note/siyuan/releases/download/v#{version}/siyuan-#{version}-mac#{arch}.dmg"
   name "SiYuan"
