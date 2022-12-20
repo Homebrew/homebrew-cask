30a154a4c894a20528acd521162122fffce45982
Patrick Linnane <patrick@linnane.io>
2022-12-20 11:51:19 -0800

    Update jetbrains-gateway from 223.7571.203 to 223.8214.51

--- Casks/jetbrains-gateway.rb
+++ Casks/jetbrains-gateway.rb
@@ -1,9 +1,9 @@
 cask "jetbrains-gateway" do
   arch arm: "-aarch64"
 
-  version "223.7571.203"
-  sha256 arm:   "e54364fa07fa03d1160243f9e1c05a89f4dc0545944371a77b94cd065719fda1",
-         intel: "3f49df87bf30d3a0a92b0d6cabbd920d43b1cfb8aa4f292edfd87f01ec683e38"
+  version "223.8214.51"
+  sha256 arm:   "555ca346ec41de06223d3a4b5e9247809e07c8339bff0d139b624634c812c8e5",
+         intel: "4b86b523b02f2df5150bc965bcef7e1a0bf7a7e6d2233a3a2603529a8577dd43"
 
   url "https://download.jetbrains.com/idea/gateway/JetBrainsGateway-#{version}#{arch}.dmg"
   name "jetbrains-gateway"
