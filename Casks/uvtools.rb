0b8dddf28987eb1636181b35bd0c3bf06b0099f4
Tin Htet Kyaw <tin@lsvp.com>
2022-12-20 09:55:07 +0630

    Update uvtools from 3.9.3 to 3.9.3

--- Casks/uvtools.rb
+++ Casks/uvtools.rb
@@ -2,7 +2,7 @@ cask "uvtools" do
   arch arm: "arm64", intel: "x64"
 
   version "3.9.3"
-  sha256 arm:   "02ec719445a3a41057cad32e72511ee3ab3be4e36421527593c26c5dd29c15c2",
+  sha256 arm:   "48ea27cd559328f89292648844aba3f90ffb26c9e0fdd69da47da84b377cde66",
          intel: "775aeb95a278784d9da367d97d206147f97f21d6989514aad3472c87d7f7961b"
 
   url "https://github.com/sn4k3/UVtools/releases/download/v#{version}/UVtools_osx-#{arch}_v#{version}.zip"
