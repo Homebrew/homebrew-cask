27441b5847cf697bbff515f73af746e169801472
Patrick Linnane <patrick@linnane.io>
2022-12-20 12:05:12 -0800

    Update pomatez from 1.1.0 to 1.2.1

--- Casks/pomatez.rb
+++ Casks/pomatez.rb
@@ -1,6 +1,6 @@
 cask "pomatez" do
-  version "1.1.0"
-  sha256 "64b5e09d9ee44328e89cc0f75675380210b202dc92ecf2b618e7a8f5d58a7bda"
+  version "1.2.1"
+  sha256 "f36450c031967059554a2ff7457b9b94b5914f9b82c2289f547056e5723ead3e"
 
   url "https://github.com/roldanjr/pomatez/releases/download/v#{version}/Pomatez-v#{version}-mac.dmg",
       verified: "github.com/roldanjr/pomatez/"
