7002b299b818b444f2b59adb2ee9e3f8804c9db5
Patrick Linnane <patrick@linnane.io>
2022-12-20 12:00:40 -0800

    Update opera from 94.0.4606.26 to 94.0.4606.38

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask "opera" do
-  version "94.0.4606.26"
-  sha256 "12b9b28afa6373217a68c7295065c045a2c6cbba5535fd095dea0dd7f8a1628c"
+  version "94.0.4606.38"
+  sha256 "c53d0100d01ab81343ffeb1a20731ea293cc211068f327c70844fcbd93a5fd57"
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   name "Opera"
