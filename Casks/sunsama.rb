bae7c6a8aaf8b6289a01857a34a9a59b6e075ecb
Masato Sugiyama <public@smasato.net>
2022-12-20 15:16:02 +0900

    Update Sunsama from 1.0.0 to 2.0.1 (#138126)
    * sunsama 2.0.1
    
    * brew style --fix sunsama
    
    * update zap
    
    * update desc
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/sunsama.rb
+++ Casks/sunsama.rb
@@ -1,15 +1,29 @@
 cask "sunsama" do
-  version "1.0.0"
-  sha256 "b110481cc0aa30db62cdcf1f3eb445a1da5bf2080b51f73190829665ae6511dd"
+  arch arm: "arm64", intel: "x64"
 
-  url "http://download.sunsama.com/desktop/Sunsama-#{version}.dmg"
+  version "2.0.1"
+  sha256 arm:   "f49c1786cf25ce350b077a2ea03e58e8a9ae02ebd092eceee0e6ff74f53d4ed6",
+         intel: "3a2fca66fe42971e7d817cd5781e6d657a48e8b12044b355b350278b7d379d4b"
+
+  url "https://download.todesktop.com/2003096gmmnl0g1/Sunsama%20#{version}-#{arch}.dmg",
+      verified: "download.todesktop.com/2003096gmmnl0g1/"
   name "Sunsama"
-  homepage "https://app.sunsama.com/desktop"
+  desc "Daily planner and calendar"
+  homepage "https://www.sunsama.com/desktop"
 
   livecheck do
-    url "https://s3-us-west-2.amazonaws.com/download.sunsama.com/desktop/latest-mac.yml"
+    url "https://download.todesktop.com/2003096gmmnl0g1/latest-mac.yml"
     strategy :electron_builder
   end
 
+  auto_updates true
+
   app "Sunsama.app"
+
+  zap trash: [
+    "~/Library/Application Support/Sunsama",
+    "~/Library/Logs/Sunsama",
+    "~/Library/Preferences/com.sunsama.native-app.plist",
+    "~/Library/Saved Application State/com.sunsama.native-app.savedState",
+  ]
 end
