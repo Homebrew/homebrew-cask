96535fcb2064059777e8ec4a76bcd5bae1e5539f
Patrick Linnane <patrick@linnane.io>
2022-12-20 12:36:28 -0800

    Update workflowy from 4.0.2212071134 to 4.0.2212200944

--- Casks/workflowy.rb
+++ Casks/workflowy.rb
@@ -1,6 +1,6 @@
 cask "workflowy" do
-  version "4.0.2212071134"
-  sha256 "9a8f53bb56e9de610046ed1e47c1c09d1c6cd368fc1bcfef689690d605e929b5"
+  version "4.0.2212200944"
+  sha256 "cb5ba37125af96ffea65dc2d844c96c074bf7b73973cbb0b541c5f4f62a15f3a"
 
   url "https://github.com/workflowy/desktop/releases/download/v#{version}/WorkFlowy.zip",
       verified: "github.com/workflowy/desktop/"
