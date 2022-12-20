94cbdae2697c77ff7a792e3b5f616be582ed3ab8
Patrick Linnane <patrick@linnane.io>
2022-12-20 11:50:26 -0800

    Update jedit-omega from 2.48 to 2.49

--- Casks/jedit-omega.rb
+++ Casks/jedit-omega.rb
@@ -3,8 +3,8 @@ cask "jedit-omega" do
     version "1.32"
     sha256 "251697fe6b76419b2cef41a89a2cb7d2e3f8caab0bc1ae82aed6c14a366fdad9"
   else
-    version "2.48"
-    sha256 "03790fa4e034e9a6201e765315f1f3432a44a1896916115bbade769ad7606fbe"
+    version "2.49"
+    sha256 "9c583f2861c6e3d2bcfb855cbfdcec35551db20d20fa1c348b57e60a9601d33c"
   end
 
   url "http://www.artman21.org/download/JeditOmega#{version.no_dots}.dmg",
