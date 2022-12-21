402d28af1224da46f99d4eb9496f7f48ddffe0b5
Patrick Linnane <patrick@linnane.io>
2022-12-21 08:53:18 -0800

    Update zoc from 8.04.7 to 8.04.8

--- Casks/zoc.rb
+++ Casks/zoc.rb
@@ -1,6 +1,6 @@
 cask "zoc" do
-  version "8.04.7"
-  sha256 "d5b16679c0b6be3a47cf8c7e4011711836e6efd612cde768aa61350ed57ba770"
+  version "8.04.8"
+  sha256 "d069246afa477574243b43e18352d72706274ccd40cec62ee32150d80a684e92"
 
   url "https://www.emtec.com/downloads/zoc/zoc#{version.no_dots}.dmg"
   name "ZOC"
