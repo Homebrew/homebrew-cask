554da473593c5c0377ba9aa5b3d6193d3a43cff8
Bevan Kay <email@bevankay.me>
2022-12-22 21:04:41 +1100

    f-bar: convert to `on_system` blocks
    Closes #138310.
    
    Signed-off-by: George Adams <george.adams@microsoft.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/f-bar.rb
+++ Casks/f-bar.rb
@@ -1,10 +1,20 @@
 cask "f-bar" do
-  if MacOS.version <= :el_capitan
+  on_el_capitan :or_older do
     version "3.1.1"
     sha256 "dd2cbcc8bfb9244cf952d29354f6cd7f442472ca4ed8a11063c006c9f83b07af"
-  else
+
+    livecheck do
+      skip "Legacy version"
+    end
+  end
+  on_sierra :or_newer do
     version "5.0.5"
     sha256 "2fb891afa0086310fa180d3b40585e1c9763b392f06ad5237995ea7d02aaf8af"
+
+    livecheck do
+      url "https://eastwest.se/apps/fbar/download"
+      strategy :header_match
+    end
   end
 
   url "https://apps.eastwest.se/fbar/updates/F-Bar_#{version}.zip",
@@ -12,11 +22,6 @@ cask "f-bar" do
   name "F-Bar"
   desc "Manage Laravel Forge servers from the menubar"
   homepage "https://laravel-forge-menubar.com/"
-
-  livecheck do
-    url "https://eastwest.se/apps/fbar/download"
-    strategy :header_match
-  end
 
   auto_updates true
 
