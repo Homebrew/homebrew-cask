cfaa7bf7b7b3c47b68087f6a4671bce89adc7c6c
Edson Samuel Jr <samuedson@gmail.com>
2022-12-22 08:36:18 -0300

    vscodium 1.74.2.22355
    Update vscodium from 1.74.1.22349 to 1.74.2.22355
    
    Closes #138316.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/vscodium.rb
+++ Casks/vscodium.rb
@@ -1,9 +1,9 @@
 cask "vscodium" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.74.1.22349"
-  sha256 arm:   "34c50f3cea3e7ec6113734123a4217633ea27053aaed279e4af9f3bce83609b7",
-         intel: "6f4732931b0834f5e6d1107be87685f0e66dcf72361ce72757c2da820f6c509a"
+  version "1.74.2.22355"
+  sha256 arm:   "43c3be3e546ca560da9836264ac69b5f5b98a5705244574e848548df8c3f9640",
+         intel: "1032858e6c07f1bc22dc6c80577133f2b84bb7f163f4a7954af25c0136c7a341"
 
   url "https://github.com/VSCodium/vscodium/releases/download/#{version}/VSCodium.#{arch}.#{version}.dmg"
   name "VSCodium"
