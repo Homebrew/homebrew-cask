b6ca0caabc6e1322c3cc85594d870a48de75c943
Ian Gregory <justcheesy@outlook.com>
2022-12-22 16:23:42 -0500

    Update overt from 0.5.3 to 0.5.4 (#138333)
    * Update zap stanza for overt
    
    * Update overt from 0.5.3 to 0.5.4

--- Casks/overt.rb
+++ Casks/overt.rb
@@ -1,9 +1,9 @@
 cask "overt" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.5.3"
-  sha256 arm:   "ddd6b67695b15bbca1cb1a0d2d281d1c9809cd0c49dfc8724a06108c5fea51ec",
-         intel: "42696c2eab8458580b0be2ca94a05ef53e0487c6c4774c9b5789f9daba34a377"
+  version "0.5.4"
+  sha256 arm:   "439b3dc73805ad0c550db8352ef5ae96cf103e253fc85f7bedf79fc98c734503",
+         intel: "25e078c72cf8f241e0d626ba377acd132a160517d444468a72fc038198b052dd"
 
   url "https://github.com/GetOvert/Overt/releases/download/v#{version}/Overt-darwin-#{arch}-#{version}.zip",
       verified: "github.com/GetOvert/Overt/"
@@ -26,6 +26,7 @@ cask "overt" do
     "~/Library/Caches/OpenStore_v3",
     "~/Library/Caches/Overt_v4",
     "~/Library/Caches/Overt_v5",
+    "~/Library/Caches/Overt_v6",
     "~/Library/Preferences/app.getopenstore.OpenStore.plist",
     "~/Library/Preferences/app.getovert.Overt.plist",
   ]
