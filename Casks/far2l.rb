aa6122a89d1d25f5b4eec21c79a87f8644ce3bce
Bevan Kay <email@bevankay.me>
2022-12-22 21:06:41 +1100

    far2l: convert to `on_system` blocks

--- Casks/far2l.rb
+++ Casks/far2l.rb
@@ -2,10 +2,11 @@ cask "far2l" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
   version "2.4.1"
 
-  if MacOS.version <= :mojave
+  on_mojave :or_older do
     url "https://github.com/elfmz/far2l/releases/download/v_#{version}/far2l-#{version}-beta-MacOS-10.11.dmg"
     sha256 "f2c1740d1ce950aa7643c44184339ec13056d5a41169c536cb58616014229d3e"
-  else
+  end
+  on_catalina :or_newer do
     url "https://github.com/elfmz/far2l/releases/download/v_#{version}/far2l-#{version}-beta-MacOS-10.15.dmg"
     sha256 "7bcce47fda76eeac00e24b36d8b80f80d30241f0f1d21b22034f93f788bba66d"
   end
