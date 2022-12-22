fafc1ba8b112910fdb53f37fa464687280334213
Adam Kłos <adam@klos.link>
2022-12-22 14:10:17 +0100

    azure-data-studio: rebuild
    * Add Azure Data Studio arm version
    * Fix formatting
    
    Closes #138318.
    
    Signed-off-by: George Adams <george.adams@microsoft.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/azure-data-studio.rb
+++ Casks/azure-data-studio.rb
@@ -1,15 +1,18 @@
 cask "azure-data-studio" do
-  version "1.40.1"
-  sha256 "f922da3606e7f62ab971cf4949c9ddcb3727692ca6d2efbce2657d48095c4aef"
+  arch arm: "darwin-arm64", intel: "darwin"
 
-  url "https://azuredatastudio-update.azurewebsites.net/#{version}/darwin/stable",
+  version "1.40.1"
+  sha256 arm:   "57accd53fac7063c9c5e5543fd0fd82488be1077ec8df0bd46c10552502f027e",
+         intel: "f922da3606e7f62ab971cf4949c9ddcb3727692ca6d2efbce2657d48095c4aef"
+
+  url "https://azuredatastudio-update.azurewebsites.net/#{version}/#{arch}/stable",
       verified: "azuredatastudio-update.azurewebsites.net/"
   name "Azure Data Studio"
   desc "Data management tool that enables working with SQL Server"
   homepage "https://docs.microsoft.com/en-us/sql/azure-data-studio/"
 
   livecheck do
-    url "https://azuredatastudio-update.azurewebsites.net/api/update/darwin/stable/VERSION"
+    url "https://azuredatastudio-update.azurewebsites.net/api/update/#{arch}/stable/VERSION"
     regex(/"productVersion"\s*:\s*"(\d+(:?\.\d+)+)"/i)
   end
 
