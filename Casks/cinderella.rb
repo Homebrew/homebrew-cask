2da1fd4772b869a1479f494841f8700d7c17c9b6
Ulrich Kortenkamp <kortenkamp@cinderella.de>
2022-12-23 11:12:55 +0100

    cinderella 3.0b.2065
    Update cinderella from 3.0b.2062 to 3.0b.2065
    
    Closes #138407.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/cinderella.rb
+++ Casks/cinderella.rb
@@ -1,6 +1,6 @@
 cask "cinderella" do
-  version "3.0b.2062"
-  sha256 "a37cfd3344ad27a75615284532a65bd0b06efe16a961a9a9b8d491ebe044e880"
+  version "3.0b.2065"
+  sha256 "e0db769f9c4b0415fa0eb762d41163f5f5e15f2c3ec3fb03a98f855f2d0cead5"
 
   url "https://beta.cinderella.de/Cinderella-#{version}.dmg"
   name "Cinderella"
