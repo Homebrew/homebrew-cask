04b142121d57158863ea92793837c9831ed37cfe
Patrick Linnane <patrick@linnane.io>
2022-12-22 19:40:02 -0800

    Update hstracker from 2.3.4 to 2.3.5

--- Casks/hstracker.rb
+++ Casks/hstracker.rb
@@ -1,6 +1,6 @@
 cask "hstracker" do
-  version "2.3.4"
-  sha256 "de75a92d385951a2412d68b8e6d97d52acc365ddbab343e866e897327cff9ada"
+  version "2.3.5"
+  sha256 "5b157dbe25e49ca9d89a27c486aa47644c2dde22da03beb799660b0d7fb9d25a"
 
   url "https://github.com/HearthSim/HSTracker/releases/download/#{version}/HSTracker.app.zip",
       verified: "github.com/HearthSim/HSTracker/"
