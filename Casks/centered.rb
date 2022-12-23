fe5c9d16d3426269de73ea7377e3d1fe39512b62
Patrick Linnane <patrick@linnane.io>
2022-12-22 19:14:19 -0800

    centered 5.7.1
    Update centered from 5.7.0 to 5.7.1
    
    Closes #138353.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/centered.rb
+++ Casks/centered.rb
@@ -1,6 +1,6 @@
 cask "centered" do
-  version "5.7.0"
-  sha256 "a8dffb08a038a8809e641b4a4cbd3c8731eeec9be2464501e21a08b2737d7c4f"
+  version "5.7.1"
+  sha256 "12cca8a453000a2e8ebe077daa11c6c31480efab9b71cd036402d65587924480"
 
   url "https://d3ni5jxar6n4fz.cloudfront.net/Centered-#{version}-universal.dmg",
       verified: "d3ni5jxar6n4fz.cloudfront.net/"
