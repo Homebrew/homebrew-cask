7566ba955b6e28d09c4c3c7cd3cfc430a894b9f9
Patrick Linnane <patrick@linnane.io>
2022-12-22 19:21:18 -0800

    dcp-o-matic-kdm-creator 2.16.36
    Update dcp-o-matic-kdm-creator from 2.16.35 to 2.16.36
    
    Closes #138359.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/dcp-o-matic-kdm-creator.rb
+++ Casks/dcp-o-matic-kdm-creator.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-kdm-creator" do
-  version "2.16.35"
-  sha256 "9c8250e394d51869ad11ee8b41723b83f9df75b727fd2522ad80dba4d85f806b"
+  version "2.16.36"
+  sha256 "ff4da4519bb3573bd38dcbddb27d599bff7c4b6417c6e7f0c821c2ffdd0942c2"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-kdm&version=#{version}"
   name "DCP-o-matic KDM Creator"
