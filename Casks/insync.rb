8c7c526842426360aa0287461780c32d192b4e65
Patrick Linnane <patrick@linnane.io>
2022-12-22 19:40:23 -0800

    Update insync from 3.8.3.50473 to 3.8.4.50481

--- Casks/insync.rb
+++ Casks/insync.rb
@@ -1,6 +1,6 @@
 cask "insync" do
-  version "3.8.3.50473"
-  sha256 "61d4f088f103bdad709ca671adbb578dba6ea1abb4b8ac441d2c272714d9ca74"
+  version "3.8.4.50481"
+  sha256 "766ea065cce79c54ab83f8df2f52090eef03bb97f2f559136ad31e4d69881cb1"
 
   url "http://cdn.insynchq.com/builds/mac/Insync-#{version}.dmg"
   name "Insync"
