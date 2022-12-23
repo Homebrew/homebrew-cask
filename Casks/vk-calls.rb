348d390db5c5f61e03d508379f3179d09abe67eb
Yaroslav Yadryshnikov <me@yay.qa>
2022-12-23 03:16:47 +0300

    Fix style

--- Casks/vk-calls.rb
+++ Casks/vk-calls.rb
@@ -1,23 +1,22 @@
 cask "vk-calls" do
-    version "1.1.10048"
-    sha256 :no_check
+  version "1.1.10048"
+  sha256 :no_check
+
+  url "https://vkcalls-native-ac.vk-apps.com/latest/vk-calls.dmg",
+      verified: "vkcalls-native-ac.vk-apps.com"
+  name "VK Calls"
+  desc "Platform for video calls of any purpose"
+  homepage "https://calls.vk.com/"
   
-    url "https://vkcalls-native-ac.vk-apps.com/latest/vk-calls.dmg",
-        verified: "vkcalls-native-ac.vk-apps.com"
-    name "VK Calls"
-    desc "VK Calls is a platform for video calls of any purpose, such as meetings, webinars and business presentations"
-    homepage "https://calls.vk.com/"
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
   
-    auto_updates true
+  app "VK Calls.app"
   
-    app "VK Calls.app"
-  
-    depends_on macos: ">= :high_sierra"
-  
-    zap trash: [
-      "~/Library/Application Support/VK Calls",
-      "~/Library/Preferences/com.vk.calls.native.1.plist",
-      "~/Library/Saved Application State/com.vk.calls.native.1.savedState",
-    ]
-  end
+  zap trash: [
+    "~/Library/Application Support/VK Calls",
+    "~/Library/Preferences/com.vk.calls.native.1.plist",
+    "~/Library/Saved Application State/com.vk.calls.native.1.savedState",
+  ]
+end
   
