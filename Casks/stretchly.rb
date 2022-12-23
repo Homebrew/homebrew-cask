e9a078595c9cd4cc18f260f7031980a62a3cb285
Patrick Linnane <patrick@linnane.io>
2022-12-23 09:58:32 -0800

    Update stretchly from 1.12.0 to 1.13.1

--- Casks/stretchly.rb
+++ Casks/stretchly.rb
@@ -1,9 +1,9 @@
 cask "stretchly" do
   arch arm: "-arm64"
 
-  version "1.12.0"
-  sha256 arm:   "9a2d2603c03b7337caef3291e285485405331a0e80304b70c6d170766cbdfb37",
-         intel: "58c2d3c77ee05de52ff6c04596220a5050ccca14140d25984dcc39052c11e032"
+  version "1.13.1"
+  sha256 arm:   "eca5d39db3061127a2b984ab1283a2d6619c08dd9644eca72f5291509f1dda21",
+         intel: "2f22b63ce5f8bd05b0248218b9af5555535901d2a287fd896a5ae281196976e5"
 
   url "https://github.com/hovancik/stretchly/releases/download/v#{version}/stretchly-#{version}#{arch}.dmg",
       verified: "github.com/hovancik/stretchly/"
