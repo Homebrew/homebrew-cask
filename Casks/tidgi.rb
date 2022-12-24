e7469f7031ad223e221bf44714ac8fd940301393
Patrick Linnane <patrick@linnane.io>
2022-12-24 10:17:37 -0800

    Update tidgi from 0.7.11 to 0.7.13

--- Casks/tidgi.rb
+++ Casks/tidgi.rb
@@ -1,11 +1,32 @@
 cask "tidgi" do
-  version "0.7.11"
-  sha256 "441ae2a5a45ef42b7e7530a5cb6e5b16aec42b0bfbeb87cb4b277aa358f1a19d"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://github.com/tiddly-gittly/TidGi-Desktop/releases/download/v#{version}/TidGi-darwin-x64-#{version}.zip"
+  version "0.7.13,0.7.13-15"
+  sha256 arm:   "0993d2f6fa895b7dafaeb79c0cb00aec6d3172d361757ed649a6778b82893bff",
+         intel: "2482e92fbc03e4f5588bcd110a48deee1350f3a1c87a476bb6fce53d36c33e19"
+
+  url "https://github.com/tiddly-gittly/TidGi-Desktop/releases/download/v#{version.csv.second}/TidGi-darwin-#{arch}-#{version.csv.first}.zip"
   name "TidGi"
   desc "Personal knowledge-base app"
   homepage "https://github.com/tiddly-gittly/TidGi-Desktop"
+
+  livecheck do
+    url "https://github.com/tiddly-gittly/TidGi-Desktop/releases/latest"
+    regex(%r{href=.*?download/v?(\d+(?:\.\d+)*-\d+)/TidGi[._-]darwin[._-]#{arch}[._-](\d+(?:\.\d+)+).zip}i)
+    strategy :header_match do |headers, regex|
+      next if headers["location"].blank?
+
+      # Identify the latest tag from the response's `location` header
+      latest_tag = File.basename(headers["location"])
+      next if latest_tag.blank?
+
+      # Fetch the assets list HTML for the latest tag and match within it
+      assets_page = Homebrew::Livecheck::Strategy.page_content(
+        @url.sub(%r{/releases/?.+}, "/releases/expanded_assets/#{latest_tag}"),
+      )
+      assets_page[:content]&.scan(regex)&.map { |match| "#{match[1]},#{match[0]}" }
+    end
+  end
 
   app "TidGi.app"
 
