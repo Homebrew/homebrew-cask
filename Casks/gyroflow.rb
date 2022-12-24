da4b11e1c9156201f5e515ba810f4e1ab5de80d6
Patrick Linnane <patrick@linnane.io>
2022-12-24 09:11:27 -0800

    Update gyroflow from 1.4.0 to 1.4.2

--- Casks/gyroflow.rb
+++ Casks/gyroflow.rb
@@ -1,6 +1,6 @@
 cask "gyroflow" do
-  version "1.4.0"
-  sha256 "fb65d4060148f31a1b63961f2df0574f9caddf6f80c3380aeb18e6230174b5fc"
+  version "1.4.2"
+  sha256 "37c225403898599aa0e01da8b8f4f19a44f5614d0d462f49522e4569cef0ab83"
 
   url "https://github.com/gyroflow/gyroflow/releases/download/v#{version}/Gyroflow-mac-universal.dmg",
       verified: "github.com/gyroflow/gyroflow/"
