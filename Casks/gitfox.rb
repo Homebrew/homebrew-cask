4dcbbfff3a4370d117f1772cf7374c1695f07b8c
singularitti <singularitti@outlook.com>
2022-12-24 23:38:23 -0500

    Update gitfox from 3.3.0,7799 to 3.3.0,7799

--- Casks/gitfox.rb
+++ Casks/gitfox.rb
@@ -20,15 +20,18 @@ cask "gitfox" do
   binary "#{appdir}/Gitfox.app/Contents/SharedSupport/bin/gitfox-cli", target: "gitfox"
 
   zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.bytieful.gitfox-retail.sfl2",
     "~/Library/Application Support/com.bytieful.Gitfox",
     "~/Library/Application Support/com.bytieful.Gitfox-retail",
     "~/Library/Application Support/Gitfox",
     "~/Library/Caches/com.bytieful.Gitfox",
     "~/Library/Caches/com.bytieful.Gitfox-retail",
     "~/Library/Caches/com.crashlytics.data/com.bytieful.Gitfox",
+    "~/Library/HTTPStorages/com.bytieful.Gitfox-retail",
     "~/Library/Preferences/com.bytieful.Gitfox.*",
     "~/Library/Preferences/com.bytieful.Gitfox-retail.plist",
     "~/Library/Saved Application State/com.bytieful.Gitfox-retail.savedState",
     "~/Library/WebKit/com.bytieful.Gitfox",
+    "~/Library/WebKit/com.bytieful.Gitfox-retail",
   ]
 end
