f1c8558415a36883ffc0dc4826fbfda6780db2f4
Patrick Linnane <patrick@linnane.io>
2022-12-25 09:10:30 -0800

    attachecase: add livecheck

--- Casks/attachecase.rb
+++ Casks/attachecase.rb
@@ -1,6 +1,3 @@
-# typed: false
-# frozen_string_literal: true
-
 cask "attachecase" do
   version "4.1.9"
   sha256 "fdcd7949b6b2f97ae3f9111097f39c8b35cf76746908d6771565a3b9a37630ef"
@@ -9,6 +6,16 @@ cask "attachecase" do
   name "AttacheCase"
   desc "Simple utility for encrypting/decrypting files and directories"
   homepage "https://hibara.org/software/attachecase/"
+
+  livecheck do
+    url "https://hibara.org/software/attachecase/json/attachecase_mac_history_en.json"
+    regex(/AtcMacv?(\d)(\d)(\d)\.zip/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map do |match|
+        "#{match[0]}.#{match[1]}.#{match[2]}"
+      end
+    end
+  end
 
   depends_on macos: ">= :mojave"
 
