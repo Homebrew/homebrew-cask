263f9087c881fbe196b43ef145c621075f973346
Teko012 <112829523+Teko012@users.noreply.github.com>
2022-12-26 18:11:43 +0100

    navicat-data-modeler: fix URL, add SSL
    Closes #138502.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/navicat-data-modeler.rb
+++ Casks/navicat-data-modeler.rb
@@ -2,7 +2,7 @@ cask "navicat-data-modeler" do
   version "3.1.4"
   sha256 :no_check
 
-  url "http://download.navicat.com/download/modeler0#{version.major_minor.no_dots}_en.dmg"
+  url "https://download3.navicat.com/download/modeler0#{version.major_minor.no_dots}_en.dmg"
   name "Navicat Data Modeler"
   desc "Database design tool"
   homepage "https://www.navicat.com/products/navicat-data-modeler"
