d19195beb64e7a08797e9fbc5bb4eafa03d0b8fb
Teko012 <112829523+Teko012@users.noreply.github.com>
2022-12-26 20:00:32 +0100

    kugoumusic: add SSL to URL
    Closes #138533.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/kugoumusic.rb
+++ Casks/kugoumusic.rb
@@ -2,7 +2,7 @@ cask "kugoumusic" do
   version "3.0.8"
   sha256 "6e2a87e8bb3e3c102c4fbaf31ba97cbfb290f4fd5e23fb813a7e1c3ed35bc7a2"
 
-  url "http://downmini.kugou.com/mac/Kugou_V#{version}.dmg"
+  url "https://downmini.kugou.com/mac/Kugou_V#{version}.dmg"
   name "Kugou Music"
   desc "Digital music service"
   homepage "https://www.kugou.com/"
