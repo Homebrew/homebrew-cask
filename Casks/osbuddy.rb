a96746be68b792b4ef4a1c345173b7925f15e879
Teko012 <112829523+Teko012@users.noreply.github.com>
2022-12-26 13:00:56 +0100

    osbuddy: add ssl to url
    Closes #138496.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/osbuddy.rb
+++ Casks/osbuddy.rb
@@ -2,7 +2,7 @@ cask "osbuddy" do
   version "1.0"
   sha256 :no_check
 
-  url "http://cdn.rsbuddy.com/OSBuddy.dmg"
+  url "https://cdn.rsbuddy.com/OSBuddy.dmg"
   name "OSBuddy"
   desc "RuneScape toolkit"
   homepage "https://rsbuddy.com/osbuddy"
