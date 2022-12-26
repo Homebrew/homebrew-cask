a7475f67c35e37acb9d09f68cd4f76fabe9377fa
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-12-26 09:25:55 +0800

    Add iqiyi with version 13.12.0

---
+++ Casks/iqiyi.rb
@@ -0,0 +1,28 @@
+cask "iqiyi" do
+  version "13.12.0,20221208165801"
+  sha256 :no_check
+
+  url "https://static-d.iqiyi.com/ext/common/iQIYIMedia_271.dmg"
+  name "iQIYI"
+  name "爱奇艺"
+  desc "Online video platform"
+  homepage "https://app.iqiyi.com/mac/player/index.html"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
+
+  depends_on macos: ">= :catalina"
+
+  app "爱奇艺.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.iqiyi.player",
+    "~/Library/Application Scripts/com.iqiyi.player.QYWidget",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.iqiyi.player.sfl*",
+    "~/Library/Containers/com.iqiyi.player",
+    "~/Library/Containers/com.iqiyi.player.QYWidget",
+    "~/Library/Group Containers/group.com.qiyi",
+  ]
+end
