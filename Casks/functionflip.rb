da1c7e8eac45dcff937c47b8d779c81d6c69541b
Teko012 <112829523+Teko012@users.noreply.github.com>
2022-12-26 19:34:56 +0100

    functionflip: add SSL to URL

--- Casks/functionflip.rb
+++ Casks/functionflip.rb
@@ -2,13 +2,13 @@ cask "functionflip" do
   version "2.2.4"
   sha256 "429a07deef5767534dee2d99ab147242c41ef860c48d0dbf0c68f2687fbe9db4"
 
-  url "http://kevingessner.com/public/downloads/FunctionFlip/#{version}/FunctionFlip.prefPane.zip"
+  url "https://kevingessner.com/public/downloads/FunctionFlip/#{version}/FunctionFlip.prefPane.zip"
   name "FunctionFlip"
   desc "Function key control"
-  homepage "http://kevingessner.com/software/functionflip/"
+  homepage "https://kevingessner.com/software/functionflip/"
 
   livecheck do
-    url "http://kevingessner.com/update/functionflip/"
+    url "https://kevingessner.com/update/functionflip/"
     regex(/v?(\d+(?:\.\d+)+)/i)
   end
 
