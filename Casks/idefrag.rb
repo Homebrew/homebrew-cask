c61aa14ff84b74dbfcfe5d1500afa484b6bb933b
Rylan Polster <rslpolster@gmail.com>
2022-12-27 16:23:48 -0500

    idefrag: convert to `on_system` blocks

--- Casks/idefrag.rb
+++ Casks/idefrag.rb
@@ -1,8 +1,9 @@
 cask "idefrag" do
-  if MacOS.version <= :el_capitan
+  on_el_capitan :or_older do
     version "5.1.3"
     sha256 "4b695c04f491b8f9f60a1fb43836164960f7d95f82aaa38d1e3b7dd4eacd7d5c"
-  else
+  end
+  on_sierra :or_newer do
     version "5.3.1"
     sha256 "b13be0fa76805e6ae363c7a8be370ed2c1865b0ac675ac77f3619d0c1dfe5cc3"
   end
