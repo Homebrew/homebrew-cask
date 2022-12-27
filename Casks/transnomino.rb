4f41f8cfe0d53cdbd5646229d3c1df9f837ca81c
Bastiaan <bastiaanverreijt@gmail.com>
2022-12-27 01:47:50 +0100

    transnomino: rebuild
    transnomino update of url and homepage
    
    Closes #138556.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/transnomino.rb
+++ Casks/transnomino.rb
@@ -2,10 +2,10 @@ cask "transnomino" do
   version "7.0"
   sha256 "b67a2415f0725cd666101aa2eeca471e0c3f792194ff1e802cac4ba32f0384b4"
 
-  url "https://transnomino.bastiaanverreijt.com/download/Transnomino-#{version}.dmg"
+  url "https://www.transnomino.com/download/Transnomino-#{version}.dmg"
   name "Transnomino"
   desc "Batch rename utility"
-  homepage "https://transnomino.bastiaanverreijt.com/"
+  homepage "https://www.transnomino.com/"
 
   livecheck do
     url :homepage
