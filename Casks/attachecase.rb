0b4f439b29461f8a4d77bc8341c35d62a1af7b62
Miccal Matthews <miccal.matthews@gmail.com>
2022-12-27 12:29:14 +0800

    attachecase: rebuild
    Update attachecase.rb
    
    Closes #138558.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/attachecase.rb
+++ Casks/attachecase.rb
@@ -2,9 +2,10 @@ cask "attachecase" do
   version "4.1.9"
   sha256 "fdcd7949b6b2f97ae3f9111097f39c8b35cf76746908d6771565a3b9a37630ef"
 
-  url "https://hibara.org/software/attachecase/download/?f=AtcMac#{version.no_dots}.zip", user_agent: :fake
+  url "https://hibara.org/software/attachecase/download/?f=AtcMac#{version.no_dots}.zip",
+      user_agent: :fake
   name "AttacheCase"
-  desc "Simple utility for encrypting/decrypting files and directories"
+  desc "Utility for encrypting/decrypting files and directories"
   homepage "https://hibara.org/software/attachecase/"
 
   livecheck do
