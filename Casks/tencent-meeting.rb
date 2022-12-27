a8f281112e95eb3d7a45f5d202d1283bd3136597
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2022-12-27 13:53:21 +0800

    tencent-meeting 3.13.5.435,c57d6fb5198a489fce92a064dd0a09dd
    Update tencent-meeting from 3.13.4 to 3.13.5
    
    Closes #138562.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/tencent-meeting.rb
+++ Casks/tencent-meeting.rb
@@ -2,12 +2,12 @@ cask "tencent-meeting" do
   arch arm: "arm64", intel: "x86_64"
 
   on_intel do
-    version "3.13.4.426,a9b2640f51c6659580cffec83520d591"
-    sha256 "28bf0de4840fa1d3853ad073d67a5f525246dc456c5ad74fa2a9ee26e8e29c28"
+    version "3.13.5.435,c57d6fb5198a489fce92a064dd0a09dd"
+    sha256 "9b9a67f3fddafd8e2190a086f2853b4d33cb6facfddbab4e1c8bc8f856c3a4e1"
   end
   on_arm do
-    version "3.13.4.426,2c72e3dca5c9cb81ff5854a3b8e26ee5"
-    sha256 "618bbdda49693aee4589fb1d06ab7330a977f90bcb5f0b09373af5463184fc51"
+    version "3.13.5.435,4b1ce1367f3b044ddfaf37f5671859af"
+    sha256 "287aceb3e2a3f5c5379ec991ee6a5e66bb958270024e7b14e6403b8b0a48f52a"
   end
 
   url "https://updatecdn.meeting.qq.com/cos/#{version.csv.second}/TencentMeeting_0300000000_#{version.csv.first}.publish.#{arch}.dmg",
