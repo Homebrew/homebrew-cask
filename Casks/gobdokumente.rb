fc9ddf68b840573137c7904a67e8ce8e7ff05ea1
Rylan Polster <rslpolster@gmail.com>
2022-12-27 16:20:37 -0500

    gobdokumente: convert to `on_system` blocks

--- Casks/gobdokumente.rb
+++ Casks/gobdokumente.rb
@@ -1,31 +1,20 @@
 cask "gobdokumente" do
-  if MacOS.version <= :catalina
-    version "1.6.8"
-    sha256 :no_check
-    url "https://download.moapp.software/GoBDokumente.zip",
-        verified: "download.moapp.software/"
+  version "2.1"
+  sha256 :no_check
 
-    livecheck do
-      skip "Legacy version for catalina and earlier"
-    end
-  else
-    version "2.1"
-    sha256 :no_check
-    url "https://download.moapp.software/GoBDokumente_BS.zip",
-        verified: "download.moapp.software/"
-
-    livecheck do
-      url "https://sparkle.moapp.software/gobdokumente.xml"
-      strategy :sparkle
-    end
-  end
-
+  url "https://download.moapp.software/GoBDokumente_BS.zip",
+      verified: "download.moapp.software/"
   name "GoBDokumente"
   name "GoBDocuments"
   desc "Document management system"
   homepage "https://gobdokumente.de/"
 
-  depends_on macos: ">= :el_capitan"
+  livecheck do
+    url "https://sparkle.moapp.software/gobdokumente.xml"
+    strategy :sparkle
+  end
+
+  depends_on macos: ">= :big_sur"
 
   app "GoBDokumente.app"
 end
