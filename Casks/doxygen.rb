d2eb2b833e961f2b22a2ee7ab206fe5fba9e4e07
Patrick Linnane <patrick@linnane.io>
2022-12-27 19:39:15 -0800

    doxygen 1.9.6
    Update doxygen from 1.9.5 to 1.9.6
    
    Closes #138629.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/doxygen.rb
+++ Casks/doxygen.rb
@@ -1,6 +1,6 @@
 cask "doxygen" do
-  version "1.9.5"
-  sha256 "9c35b154b831d9e63874d0fe0d557522a7d85556dc1d4e10da1cde729f3e51c5"
+  version "1.9.6"
+  sha256 "25ad21fa767bd71208947b848d482b46c6bc523a97ba1aacf803c71c4d043726"
 
   url "https://doxygen.nl/files/Doxygen-#{version}.dmg"
   name "Doxygen"
