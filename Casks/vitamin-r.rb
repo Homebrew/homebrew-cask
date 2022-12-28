66f360e531422bc1ec09c0fa06551d1d090e894e
Bevan Kay <email@bevankay.me>
2022-12-28 15:00:08 +1100

    vitamin-r: convert to `on_system` blocks

--- Casks/vitamin-r.rb
+++ Casks/vitamin-r.rb
@@ -1,8 +1,9 @@
 cask "vitamin-r" do
-  if MacOS.version <= :el_capitan
+  on_el_capitan :or_older do
     version "2.58"
     sha256 "c6c631430b44359aa022d9ca5ca6e98dbdf7258f2ceae0353f344a035682661e"
-  else
+  end
+  on_sierra :or_newer do
     version "4.15"
     sha256 "67186cf1af8895b405960fabe9cf3ba2dd6267adabaa22fac8cbeffbdb494f83"
   end
