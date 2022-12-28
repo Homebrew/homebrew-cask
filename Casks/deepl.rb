e45320fe41efee9f40262612268c782676df13a8
Miccal Matthews <miccal.matthews@gmail.com>
2022-12-29 06:54:52 +0800

    Update deepl.rb

--- Casks/deepl.rb
+++ Casks/deepl.rb
@@ -1,21 +1,21 @@
 cask "deepl" do
-  on_big_sur :or_newer do
-    version "4.0.317085"
-    sha256 "a43836ea319e0b22fc48761e992e531447cb6e28cf94d348c3210121e53352f0"
-
-    livecheck do
-      url "https://appdownload.deepl.com/macos/bigsur/update.json"
-      strategy :page_match do |page|
-        JSON.parse(page)["currentRelease"]
-      end
-    end
-  end
   on_catalina :or_older do
     version "3.7.292629"
     sha256 "efcac4988a606d9793a3bdb8e7e73dce8e3d06ed2249a4434eb54c1624b40b87"
 
     livecheck do
       url "https://appdownload.deepl.com/macos/update.json"
+      strategy :page_match do |page|
+        JSON.parse(page)["currentRelease"]
+      end
+    end
+  end
+  on_big_sur :or_newer do
+    version "4.0.317085"
+    sha256 "a43836ea319e0b22fc48761e992e531447cb6e28cf94d348c3210121e53352f0"
+
+    livecheck do
+      url "https://appdownload.deepl.com/macos/bigsur/update.json"
       strategy :page_match do |page|
         JSON.parse(page)["currentRelease"]
       end
