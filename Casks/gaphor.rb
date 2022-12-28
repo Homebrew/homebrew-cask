596c822482f88929771c9290377e8a83d83aa065
Patrick Linnane <patrick@linnane.io>
2022-12-27 19:42:09 -0800

    gaphor 2.14.0
    Update gaphor from 2.13.0 to 2.14.0
    
    Closes #138636.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/gaphor.rb
+++ Casks/gaphor.rb
@@ -1,6 +1,6 @@
 cask "gaphor" do
-  version "2.13.0"
-  sha256 "a681ec15d28fff33d3f107eb5265c819018080755822e848725a05dd319cf947"
+  version "2.14.0"
+  sha256 "a434a82da8e22f1636304b9ff573f561cc30790abf9474323ad7aace15363ca4"
 
   url "https://github.com/gaphor/gaphor/releases/download/#{version}/Gaphor-#{version}.dmg",
       verified: "github.com/gaphor/gaphor/"
