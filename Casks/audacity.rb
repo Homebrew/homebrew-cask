c42bcc5ed68b4b4326cc29d7cf68b74722d55f80
Patrick Linnane <patrick@linnane.io>
2022-12-27 19:36:08 -0800

    audacity 3.2.3
    Update audacity from 3.2.2 to 3.2.3
    
    Closes #138623.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/audacity.rb
+++ Casks/audacity.rb
@@ -1,9 +1,9 @@
 cask "audacity" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "3.2.2"
-  sha256 arm:   "7b4be3d7f91429abb6931dd610520a7d14fcc186d6230fc5546cb2e5bb6d0edf",
-         intel: "968256f34b740e329eebee9f007400d08544da5d19a90a82f0e384d126ce55df"
+  version "3.2.3"
+  sha256 arm:   "426cf06082926b060796051e12367d7cb304ee007f6861d8c35e758ff5f362ad",
+         intel: "719ba4b7c848131a624786f74795fca369197370db3455efaaa30136ed13e85f"
 
   url "https://github.com/audacity/audacity/releases/download/Audacity-#{version}/audacity-macOS-#{version}-#{arch}.dmg",
       verified: "github.com/audacity/audacity/"
