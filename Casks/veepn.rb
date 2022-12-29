cdbed65f3d21699d2499f67bc52cf651dd867f10
Bevan Kay <email@bevankay.me>
2022-12-29 15:40:06 +1100

    veepn 2.2.1,918494
    * Update veepn from 2.2.0 to 2.2.1
    * Update veepn.rb
    
    Closes #138742.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/veepn.rb
+++ Casks/veepn.rb
@@ -1,6 +1,6 @@
 cask "veepn" do
-  version "2.2.0,918493"
-  sha256 "6d55af3b8150821cd7de5f62a3d986e0dcc29e1415d2d4a947f6c40748ac63b0"
+  version "2.2.1,918494"
+  sha256 "ce238e009c1ae196c8413fcf86dc7afe5404a40b7c5410a112d90c09f9264b28"
 
   url "https://download.veepn.com/mac/VeePN_v#{version.csv.first}.dmg"
   name "VeePN"
@@ -11,6 +11,8 @@ cask "veepn" do
     url "https://download.veepn.com/mac/appcast.xml"
     strategy :sparkle
   end
+
+  depends_on macos: ">= :mojave"
 
   app "VeePN.app"
 
