f99fe844b3734044199679b636f38c0a60121c30
Alexander Regueiro <alexreg@gmail.com>
2022-12-29 00:10:36 +0000

    prowlarr: add livecheck

--- Casks/prowlarr.rb
+++ Casks/prowlarr.rb
@@ -11,6 +11,11 @@ cask "prowlarr" do
   desc "Indexer manager/proxy for various PVR apps"
   homepage "https://prowlarr.com/"
 
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
   depends_on macos: ">= :high_sierra"
 
   app "Prowlarr.app"
