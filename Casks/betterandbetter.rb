4ecaa0f2077c9a0aec5e9d1d2c193b45d68f7dc3
Patrick Linnane <patrick@linnane.io>
2022-12-29 10:44:30 -0800

    Update betterandbetter from 2.5.4 to 2.5.5

--- Casks/betterandbetter.rb
+++ Casks/betterandbetter.rb
@@ -1,6 +1,6 @@
 cask "betterandbetter" do
-  version "2.5.4"
-  sha256 "21451d409759bbf078a5478a824ad26824d8bf4b94b7d4a01d824c5191a9b857"
+  version "2.5.5"
+  sha256 "88b55f723e756406794cedc90bb3aa193712815ba465da46d3844e791040d707"
 
   url "https://cdn.better365.cn/BetterAndBetter/BetterAndBetter#{version}Beta.dmg"
   name "Better And Better"
