d8199af59f31e531c04621ea7a2f2bb87bba9338
Bevan Kay <email@bevankay.me>
2022-12-29 13:12:08 +1100

    Update cloudflare-warp from 2022.10.107.0 to 2022.12.475.0

--- Casks/cloudflare-warp.rb
+++ Casks/cloudflare-warp.rb
@@ -1,5 +1,5 @@
 cask "cloudflare-warp" do
-  version "2022.10.107.0,20221116.68"
+  version "2022.12.475.0,20221229.2"
   sha256 :no_check
 
   url "https://1111-releases.cloudflareclient.com/mac/Cloudflare_WARP.zip",
