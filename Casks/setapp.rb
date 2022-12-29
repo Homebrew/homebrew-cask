0bb26cbff60ba96113832ba3ba6750611f5c09aa
Amory Meltzer <Amorymeltzer@gmail.com>
2022-12-29 09:27:32 -0500

    Update setapp from 3.17.0 to 3.17.1

--- Casks/setapp.rb
+++ Casks/setapp.rb
@@ -1,6 +1,6 @@
 cask "setapp" do
-  version "3.17.0,19,1671655444"
-  sha256 "4a6c80886ac0d39bb7e794437a00db5b9780341bbb2e4560752633af52937b60"
+  version "3.17.1,20,1672244114"
+  sha256 "16c8cf3ab70d1813b877a61928142774f2e4241c019b14b7845ec8c0aa7b3a7e"
 
   url "https://dl.devmate.com/com.setapp.DesktopClient/#{version.csv.second}/#{version.csv.third}/Setapp-#{version.csv.second}.zip",
       verified: "devmate.com/com.setapp.DesktopClient/"
