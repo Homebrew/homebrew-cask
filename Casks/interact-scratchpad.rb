e21af194815a57e842757c88dbf4a491d3f64dd6
Bevan Kay <email@bevankay.me>
2022-12-29 18:38:40 +1100

    Add livecheck

--- Casks/interact-scratchpad.rb
+++ Casks/interact-scratchpad.rb
@@ -2,7 +2,7 @@ cask "interact-scratchpad" do
   on_big_sur :or_older do
     version "2.0.4"
     sha256 :no_check
-    url "https://s3.us-west-2.amazonaws.com/downloads.agiletortoise.com/Interact+Scratchpad-2.0.app.zip",
+    url "https://s3.us-west-2.amazonaws.com/downloads.agiletortoise.com/Interact+Scratchpad-#{version.major_minor}.app.zip",
         verified: "s3.us-west-2.amazonaws.com/downloads.agiletortoise.com/"
   end
   on_monterey :or_newer do
@@ -12,9 +12,14 @@ cask "interact-scratchpad" do
         verified: "s3-us-west-2.amazonaws.com/downloads.agiletortoise.com/"
   end
 
-  name "interact-scratchpad"
+  name "Interact Scratchpad"
   desc "Menu bar utility to ease the creation of contacts from snippets of text"
   homepage "https://docs.getdrafts.com/docs/misc/interact-scratchpad"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   depends_on macos: ">= :catalina"
 
