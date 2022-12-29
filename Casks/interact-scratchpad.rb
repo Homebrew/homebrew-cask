6c238817e2d365edeb0540d9f9ea1adecde46afd
HuaDeity <tidy.year0352@fastmail.com>
2022-12-29 14:43:26 +0800

    Fix MacOS version

--- Casks/interact-scratchpad.rb
+++ Casks/interact-scratchpad.rb
@@ -1,15 +1,17 @@
 cask "interact-scratchpad" do
-  if MacOS.version <= :big_sur
+  on_big_sur :or_older do
     version "2.0.4"
     sha256 :no_check
     url "https://s3.us-west-2.amazonaws.com/downloads.agiletortoise.com/Interact+Scratchpad-2.0.app.zip",
         verified: "s3.us-west-2.amazonaws.com/downloads.agiletortoise.com/"
-  else
+  end
+  on_monterey :or_newer do
     version "2.1.2"
     sha256 :no_check
     url "https://s3-us-west-2.amazonaws.com/downloads.agiletortoise.com/Interact+Scratchpad.app.zip",
         verified: "s3-us-west-2.amazonaws.com/downloads.agiletortoise.com/"
   end
+
   name "interact-scratchpad"
   desc "Menu bar utility to ease the creation of contacts from snippets of text"
   homepage "https://docs.getdrafts.com/docs/misc/interact-scratchpad"
