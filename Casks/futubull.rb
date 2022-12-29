b837a3a3a92b940eead5c16c9d965f7d712164ed
Patrick Linnane <patrick@linnane.io>
2022-12-28 22:16:04 -0800

    futubull 12.15.3438
    Update futubull from 12.15.3428 to 12.15.3438
    
    Closes #138750.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/futubull.rb
+++ Casks/futubull.rb
@@ -1,6 +1,6 @@
 cask "futubull" do
-  version "12.15.3428"
-  sha256 "2b6629c3005182ae614d50e5a56c9c9652c1613fdfd8a8bc858e28d614b75047"
+  version "12.15.3438"
+  sha256 "4273d177cecaf77ccc138123197ead786064e02199668bab3eac7d9ca0284c8e"
 
   url "https://softwarefile.futunn.com/FTNNForMac_#{version}_Website.dmg"
   name "Futubull"
