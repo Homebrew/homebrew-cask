4ee67e80c85b414cc62f6c107d6b24db14912755
Alexander Regueiro <alexreg@gmail.com>
2022-12-29 00:08:18 +0000

    sonarr: rebuild
    * sonarr: update zap stanza
    * sonarr: remove unnecessary preflight stanza
    
    Closes #138698.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/sonarr.rb
+++ Casks/sonarr.rb
@@ -16,13 +16,7 @@ cask "sonarr" do
 
   app "Sonarr.app"
 
-  preflight do
-    set_permissions "#{staged_path}/Sonarr.app", "0755"
-  end
-
   zap trash: [
-    "~/Library/Application Support/Sonarr",
-    "~/Library/Preferences/tv.sonarr.Sonarr.plist",
-    "~/.config/NzbDrone",
+    "~/.config/Sonarr",
   ]
 end
