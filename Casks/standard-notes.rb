d720fc680a3a8036b1da0ad65067c71ea0a31e3c
Bevan Kay <email@bevankay.me>
2022-12-29 15:23:23 +1100

    Update standard-notes from 3.121.1 to 3.129.0

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -1,9 +1,9 @@
 cask "standard-notes" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.121.1"
-  sha256 arm:   "4165a070b5c185ef1c72d64e1a21bff590c299305512e00adb5eda5dda3c475e",
-         intel: "02cb72e48390ce04ba2611b7529905196d004101352a6da0b273c7464a93dc64"
+  version "3.129.0"
+  sha256 arm:   "9f08a6cc386c46efd0a3cdce6b622bc3248027275ea2034205a5bfcd6786c120",
+         intel: "8c0812c504292bf05a2704af235365efdebafe3334bbdf6b24725e0a3172f41e"
 
   url "https://github.com/standardnotes/app/releases/download/%40standardnotes%2Fdesktop%40#{version}/standard-notes-#{version}-mac-#{arch}.zip",
       verified: "github.com/standardnotes/app/"
