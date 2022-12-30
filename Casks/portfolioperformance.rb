3114605b36ac8faf8b080b3abe74f00276dea051
Felix Honer <felix.honer@novatec-gmbh.de>
2022-12-30 03:08:33 +0100

    Update portfolioperformance from 0.60.0 to 0.60.1 (#138786)

--- Casks/portfolioperformance.rb
+++ Casks/portfolioperformance.rb
@@ -1,9 +1,9 @@
 cask "portfolioperformance" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.60.0"
-  sha256 arm:   "56488fbe94d8a0573a82529af5f1742a5051ff3989b441ae9e0fb061a7dc0f35",
-         intel: "7e214107c6ffa28a4522bfdcee35f2376f72d45f947da0a2a7a1ab361c9e5933"
+  version "0.60.1"
+  sha256 arm:   "413b26a12f424770d5f97d751fafc924467183c483104f5c0b7dadc7f13ed1c1",
+         intel: "be6a9ad2076799402b6811e9abdd4b1002ea6fc5e6f0575e6394a5118005d234"
 
   url "https://github.com/buchen/portfolio/releases/download/#{version}/PortfolioPerformance-#{version}-#{arch}.dmg",
       verified: "github.com/buchen/portfolio/"
