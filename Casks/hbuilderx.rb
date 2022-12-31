3eb71f761e7133a08f39dd1899846dbaf76fd4bc
Miccal Matthews <miccal.matthews@gmail.com>
2022-12-31 12:38:22 +0800

    Update hbuilderx.rb

--- Casks/hbuilderx.rb
+++ Casks/hbuilderx.rb
@@ -3,7 +3,7 @@ cask "hbuilderx" do
   sha256 "0fb824a3a722839c1cc90e76a7ee7a7b5b981e9a40e8e121a08a3d69bf587870"
 
   url "https://download1.dcloud.net.cn/download/HBuilderX.#{version}.dmg",
-      verified: "download1.dcloud.net.cn/"
+      verified: "download1.dcloud.net.cn/download/"
   name "HBuilderX"
   desc "HTML editor"
   homepage "https://www.dcloud.io/hbuilderx.html"
@@ -17,7 +17,5 @@ cask "hbuilderx" do
 
   app "HBuilderX.app"
 
-  zap trash: [
-    "~/Library/Application Support/HBuilder X",
-  ]
+  zap trash: "~/Library/Application Support/HBuilder X"
 end
