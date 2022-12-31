7d925aa882f28b594b330a2983ce3ab0c51b37f2
Patrick Linnane <patrick@linnane.io>
2022-12-30 19:54:47 -0800

    Update retro-virtual-machine from 2.0 to 2.1.0

--- Casks/retro-virtual-machine.rb
+++ Casks/retro-virtual-machine.rb
@@ -1,27 +1,22 @@
 cask "retro-virtual-machine" do
-  version "2.0,1.r7"
-  sha256 "75e94f2df589ead3fb1eab529713312a17dc16e6b2ba547594cd9d5975def566"
+  version "2.1.0"
+  sha256 "d4a65c0ab19cb3f39048b5bb6c795b2165af18c172da8a99a217e3b2fb9d1ced"
 
-  url "https://static.retrovm.org/release/beta#{version.csv.second.major}/macos/RetroVirtualMachine.#{version.csv.first}.beta-#{version.csv.second}.macos.dmg",
+  url "https://static.retrovm.org/release/#{version}/RetroVirtualMachine.#{version}.dmg",
       verified: "static.retrovm.org/release/"
   name "Retro Virtual Machine"
   desc "ZX Spectrum and Amstrad CPC emulator"
   homepage "https://www.retrovirtualmachine.org/en/"
 
   livecheck do
-    url "https://www.retrovirtualmachine.org/en/downloads"
-    strategy :page_match do |page|
-      match = page.match(/RetroVirtualMachine\.(\d+(?:\.\d+)*)\.beta-(\d+(?:\..\d+)*)\.macos\.dmg/i)
-      next if match.blank?
-
-      "#{match[1]},#{match[2]}"
-    end
+    url "https://www.retrovirtualmachine.org/download"
+    regex(/RetroVirtualMachine[._-](\d+(?:\.\d+)+)\.dmg/i)
   end
 
-  app "Retro Virtual Machine #{version.major}.app"
+  app "Retro Virtual Machine #{version.major_minor}.app"
 
   zap trash: [
-    "~/Library/Application Support/Retro Virtual Machine v#{version.major}.x",
-    "~/Library/Preferences/com.madeinalacant.RetroVirtualMachine#{version.major}.plist",
+    "~/Library/Application Support/Retro Virtual Machine v#{version.major_minor}.x",
+    "~/Library/Preferences/com.madeinalacant.RetroVirtualMachine#{version.major_minor}.plist",
   ]
 end
