9d23352b677a0ff28e704668ee88f5a6ac48597e
Patrick Linnane <patrick@linnane.io>
2022-12-30 19:30:04 -0800

    Update douyin from 1.7.1 to 1.7.201

--- Casks/douyin.rb
+++ Casks/douyin.rb
@@ -1,8 +1,8 @@
 cask "douyin" do
-  version "1.7.1,7044145585217083655,9692407"
-  sha256 "fc9bf121908dd4c2ae23dff401013960b8b2deceee83ca8b2ee8cede93e4a2e5"
+  version "1.7.201,7044145585217083655,9757194"
+  sha256 "3d09da21b4f89708ba5bd2fd3a08a627131d1febc819eb369820ecb6db735fe4"
 
-  url "https://www.douyin.com/download/pc/obj/douyin-pc-client/#{version.csv.second}/releases/#{version.csv.third}/#{version.csv.first}/darwin-universal/douyin-v#{version.csv.first}-darwin-universal.dmg"
+  url "https://www.douyin.com/download/pc/obj/douyin-pc-client/#{version.csv.second}/experiment/alpha-#{version.csv.first}/#{version.csv.third}/#{version.csv.first}/darwin-universal/douyin-v#{version.csv.first}-darwin-universal.dmg"
   name "Douyin"
   name "抖音"
   desc "Social software for creating music short videos"
@@ -11,10 +11,10 @@ cask "douyin" do
   livecheck do
     url "https://www.douyin.com/downloadpage"
     strategy :page_match do |page|
-      match = page.match(%r{douyin[._-]pc[._-]client/(\d+)/releases/(\d+)/(\d+(?:\.\d+)+)/darwin[._-]universal}i)
+      match = page.match(%r{client/(\d+)/experiment/alpha-(\d+(?:\.\d+)+)/(\d+).*?darwin[._-]universal\.dmg}i)
       next if match.blank?
 
-      "#{match[3]},#{match[1]},#{match[2]}"
+      "#{match[2]},#{match[1]},#{match[3]}"
     end
   end
 
