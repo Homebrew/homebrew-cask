839192183450f718158e9e67153d45faec0f0c4f
Miccal Matthews <miccal.matthews@gmail.com>
2022-12-31 08:05:27 +0800

    arq: rebuild
    * Update arq.rb
    * Update arq.rb
    
    Closes #138799.
    
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/arq.rb
+++ Casks/arq.rb
@@ -9,7 +9,7 @@ cask "arq" do
 
   livecheck do
     url "https://www.arqbackup.com/download/arqbackup/arq#{version.major}_release_notes.html"
-    regex(/Version\s*(\d+(?:\.\d+)+)/i)
+    regex(/Version\s*v?(\d+(?:\.\d+)+)/i)
   end
 
   auto_updates true
@@ -18,20 +18,18 @@ cask "arq" do
 
   uninstall pkgutil:   "com.haystacksoftware.Arq",
             quit:      "com.haystacksoftware.Arq",
-            delete:    [
-              "/Applications/Arq.app",
-            ],
+            delete:    "/Applications/Arq.app",
             launchctl: [
-              "com.haystacksoftware.ArqMonitor",
               "com.haystacksoftware.arqagent",
+              "com.haystacksoftware.ArqMonitor",
             ]
 
   zap trash: [
-    "~/Library/Arq *",
-    "~/Library/Application Support/Arq *",
-    "~/Library/Preferences/com.haystacksoftware.Arq.plist",
-    "~/Library/Preferences/com.haystacksoftware.ArqMonitor.plist",
     "/Library/Application Support/ArqAgent",
     "/Library/Application Support/ArqAgentAPFS",
+    "~/Library/Application Support/Arq *",
+    "~/Library/Arq *",
+    "~/Library/Preferences/com.haystacksoftware.Arq.plist",
+    "~/Library/Preferences/com.haystacksoftware.ArqMonitor.plist",
   ]
 end
