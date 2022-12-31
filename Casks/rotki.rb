cb45f28a62b36964959a98ef0c27d296b34a7667
Patrick Linnane <patrick@linnane.io>
2022-12-30 19:15:48 -0800

    Update rotki from 1.26.2 to 1.26.3

--- Casks/rotki.rb
+++ Casks/rotki.rb
@@ -1,6 +1,6 @@
 cask "rotki" do
-  version "1.26.2"
-  sha256 "2b64a9b30657420043ac7ddf5caf3371db8086e9871067370b482e5eb77d4ec5"
+  version "1.26.3"
+  sha256 "a41c6de935b62b04658e29bc8843ba1b2b8a5ef0d0a1ca246dfe05469691b9bd"
 
   url "https://github.com/rotki/rotki/releases/download/v#{version}/rotki-darwin_x64-v#{version}.dmg",
       verified: "github.com/rotki/rotki/"
