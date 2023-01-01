1ced161cbc19bf133c33280f1c9520e8e31ecb67
Miccal Matthews <miccal.matthews@gmail.com>
2023-01-02 06:33:50 +0800

    Update catoclient.rb

--- Casks/catoclient.rb
+++ Casks/catoclient.rb
@@ -1,26 +1,29 @@
 cask "catoclient" do
-  version :latest
-  sha256 :no_check
+  version "5.2.0.205"
+  sha256 "342a0da1c3a939679cdb7b72e826eecbd99bb6dd9443102fe8fd99181410dd71"
 
-  url "https://clientdownload.catonetworks.com/public/clients/CatoClient.pkg"
+  url "https://clients.catonetworks.com/macos/5.2.0.205/CatoClient.pkg"
   name "Cato Client"
   desc "VPN client"
   homepage "https://www.catonetworks.com/"
 
+  livecheck do
+    url "https://clientdownload.catonetworks.com/public/clients/CatoClient.pkg"
+    strategy :header_match
+  end
+
   pkg "CatoClient.pkg"
 
-  uninstall pkgutil: "com.catonetworks.mac.CatoClient"
+  uninstall pkgutil: "com.catonetworks.pkg.CatoClient"
 
-  # Re-installing after zapping still results in the same configuration; below
-  # at least clears out ~/Library/
   zap trash: [
-    "~/Library/Saved Application State/com.catonetworks.mac.CatoClient.savedState/",
-    "~/Library/Application Scripts/com.catonetworks.mac.CatoClient/",
-    "~/Library/Application Scripts/com.catonetworks.mac.CatoClient.CatoClientExtension/",
     "~/Library/Application Scripts/CKGSB8CH43.com.catonetworks.mac.CatoClient/",
+    "~/Library/Application Scripts/com.catonetworks.mac.CatoClient.CatoClientExtension/",
+    "~/Library/Application Scripts/com.catonetworks.mac.CatoClient/",
+    "~/Library/Containers/com.catonetworks.mac.CatoClient.CatoClientExtension/",
+    "~/Library/Containers/com.catonetworks.mac.CatoClient/",
     "~/Library/Group Containers/CKGSB8B8CH43.com.catonetworks.mac.CatoClient/",
     "~/Library/Group Containers/CKGSB8B8CH43.group/",
-    "~/Library/Containers/com.catonetworks.mac.CatoClient/",
-    "~/Library/Containers/com.catonetworks.mac.CatoClient.CatoClientExtension/",
+    "~/Library/Saved Application State/com.catonetworks.mac.CatoClient.savedState/",
   ]
 end
