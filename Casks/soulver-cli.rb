9529a2879c1a3a3e5f043657507909a84920c5c5
Zac Cohan <zac@acqualia.com>
2023-01-02 14:02:20 +0300

    Add soulver-cli 1.0

---
+++ Casks/soulver-cli.rb
@@ -0,0 +1,14 @@
+cask "soulver-cli" do
+  version "1.0.0"
+  sha256 "21d5463151181ee4b23c5bdc603c4bb52cf215bf0f29ca7b228ce812e2f2b80c"
+
+  url "https://github.com/soulverteam/Soulver-CLI/releases/download/#{version}/soulver.zip"
+  name "Soulver CLI"
+  desc "Standalone cli for the Soulver calculation engine"
+  homepage "https://github.com/soulverteam/Soulver-CLI"
+
+  depends_on macos: ">= :monterey"
+
+  binary "soulver"
+  zap trash: ["/opt/homebrew/bin/SoulverCore_SoulverCore.bundle"]
+end
