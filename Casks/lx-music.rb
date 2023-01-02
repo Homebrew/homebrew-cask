fcb9e8ceec3a74ceb452ba292cb6ca7818b95151
Patrick Linnane <patrick@linnane.io>
2023-01-01 19:27:40 -0800

    Update lx-music from 1.22.3 to 2.0.0

--- Casks/lx-music.rb
+++ Casks/lx-music.rb
@@ -1,9 +1,9 @@
 cask "lx-music" do
   arch arm: "-arm64"
 
-  version "1.22.3"
-  sha256 arm:   "acbb45635987def11ba8e28e0acde50a66834d08a756bbd02683c5b5bb782a4b",
-         intel: "720dee814471dcb3e360d3d2fdd5e3a2747551030500a7725fca7c1422f98a32"
+  version "2.0.0"
+  sha256 arm:   "b9a7d925851ba9865f9fd84ef775a27cafbd1756a9f342b8a229fc4e343c3ec3",
+         intel: "1d88e5e072d61e2167e5d7a7f3823fb3c34f4d273a68451b8c521cb430dbd716"
 
   url "https://github.com/lyswhut/lx-music-desktop/releases/download/v#{version}/lx-music-desktop-#{version}#{arch}.dmg"
   name "LX Music Assistant Desktop Edition"
