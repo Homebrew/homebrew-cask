c77a8c7f13f4a2c3e62060783c044602031ad7d6
Patrick Linnane <patrick@linnane.io>
2023-01-01 19:06:30 -0800

    Update cursr from 1.4.12 to 1.4.13

--- Casks/cursr.rb
+++ Casks/cursr.rb
@@ -1,9 +1,9 @@
 cask "cursr" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.4.12"
-  sha256 arm:   "2baa6b72c8b0747dec266d82c9f335b0f7511b230d27bfef334cfc62de38e358",
-         intel: "5751b18bfb8f24a8b55d3d6b2123272890191a0ec82035e159f6355fe9861191"
+  version "1.4.13"
+  sha256 arm:   "f9ff32c6048829b7e4103eeb82d62825ba5da8b890efe654f51206e91665c343",
+         intel: "fd893a4c0be96168d2f5a10edb93f9447a31d4f31a3a77747df6c4934e976038"
 
   url "https://github.com/bitgapp/Cursr/releases/download/v#{version}/Cursr-mac-#{arch}.dmg",
       verified: "github.com/bitgapp/Cursr/"
