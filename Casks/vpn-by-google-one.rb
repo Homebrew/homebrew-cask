08f709befdd40919383a0b30c0e6fec772340a39
Bevan Kay <email@bevankay.me>
2023-01-03 15:56:46 +1100

    add livecheck, add version

--- Casks/vpn-by-google-one.rb
+++ Casks/vpn-by-google-one.rb
@@ -1,11 +1,16 @@
 cask "vpn-by-google-one" do
-  version :latest
+  version "1.0.0.3"
   sha256 :no_check
 
   url "https://dl.google.com/google-one/VpnByGoogleOne.dmg"
   name "VPN by Google One"
   desc "VPN provided by Google One"
   homepage "https://one.google.com/about/vpn"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   depends_on macos: ">= :big_sur"
 
