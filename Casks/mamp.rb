436c959d71e7ecb16c47c1bf800f1c45b11a044b
Bevan Kay <email@bevankay.me>
2023-01-03 14:09:12 +1100

    Update mamp from 6.6 to 6.7

--- Casks/mamp.rb
+++ Casks/mamp.rb
@@ -1,9 +1,9 @@
 cask "mamp" do
   arch arm: "M1-arm", intel: "Intel-x86"
 
-  version "6.6"
-  sha256 arm:   "4eb2a5146c0f0ccacfdaf4ea31e5735317a9e2aab18c0b9b591ab96c17808f1d",
-         intel: "306b101a84251655b8e1d50ef1c4d59901d300f85d0e03910701d8d418d4a4d4"
+  version "6.7"
+  sha256 arm:   "8243e3cdde96e657c68f9555ac3334bf153e23656c9cc6a858de3935873b7610",
+         intel: "672f0fcfcb34d2b2bbc027abe859c4a0e31a4eaae5b0856aeac6b05c585b2508"
 
   url "https://downloads.mamp.info/MAMP-PRO/releases/#{version}/MAMP_MAMP_PRO_#{version}-#{arch}.pkg"
   name "MAMP"
