2ccf2b9b3037d9027915fad4b85f4845f4262551
Yobi <ordinary.yobi@gmail.com>
2023-01-03 12:18:48 +0900

    openlens: checksum update
    * update sha256 for arm
      according to https://github.com/MuhammedKalkan/OpenLens/releases/download/v6.3.0/OpenLens-6.3.0-arm64.dmg.sha256
    * update openlens sha256 for intel
      https://github.com/MuhammedKalkan/OpenLens/releases/download/v6.3.0/OpenLens-6.3.0.dmg.sha256
    
    Closes #138950.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/openlens.rb
+++ Casks/openlens.rb
@@ -2,8 +2,8 @@ cask "openlens" do
   arch arm: "-arm64"
 
   version "6.3.0"
-  sha256 arm:   "71c8a86161d17143251e6a9bf202d7d014573a6c6906c06f5d9ece33c2889f06",
-         intel: "a3dcac335e75f267bfd3f98f6911fd831ac8688de60dbe68399cb0746087e2cf"
+  sha256 arm:   "bd356557c1518f7e2d95166de4d0abfff6e4c49141c060fe7e3eef2a0303a1f5",
+         intel: "2eb1848bb189041c4475054631a5b33a8ccd00c6eedbb74776b3dd1f54db7898"
 
   url "https://github.com/MuhammedKalkan/OpenLens/releases/download/v#{version}/OpenLens-#{version}#{arch}.dmg"
   name "OpenLens"
