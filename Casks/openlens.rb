e8df2a2a510df7d2748964ae9d75dfca051e7234
Yobi <ordinary.yobi@gmail.com>
2023-01-03 12:51:59 +0900

    update openlens sha256 for intel
    https://github.com/MuhammedKalkan/OpenLens/releases/download/v6.3.0/OpenLens-6.3.0.dmg.sha256

--- Casks/openlens.rb
+++ Casks/openlens.rb
@@ -3,7 +3,7 @@ cask "openlens" do
 
   version "6.3.0"
   sha256 arm:   "bd356557c1518f7e2d95166de4d0abfff6e4c49141c060fe7e3eef2a0303a1f5",
-         intel: "a3dcac335e75f267bfd3f98f6911fd831ac8688de60dbe68399cb0746087e2cf"
+         intel: "2eb1848bb189041c4475054631a5b33a8ccd00c6eedbb74776b3dd1f54db7898"
 
   url "https://github.com/MuhammedKalkan/OpenLens/releases/download/v#{version}/OpenLens-#{version}#{arch}.dmg"
   name "OpenLens"
