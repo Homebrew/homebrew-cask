4e16d66bc4d276b92451bc90a52ee616b4faea23
Patrick Linnane <patrick@linnane.io>
2023-01-03 19:45:37 -0800

    fmail2 1.5.2,47
    Update fmail2 from 1.5.1 to 1.5.2
    
    Closes #138986.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/fmail2.rb
+++ Casks/fmail2.rb
@@ -1,7 +1,7 @@
 cask "fmail2" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "1.5.1,46"
-  sha256 "530797c564eea5543654d1940659e84ad816d37baedfbdfc076e61cfd508388d"
+  version "1.5.2,47"
+  sha256 "66d8df7018be3d278a63cd7c19af991340f52f9bcb675d1dc2620be5bebe762f"
 
   url "https://arievanboxel.fr/fmail2/sparkle/FMail2_#{version.csv.first.no_dots}.zip",
       verified: "arievanboxel.fr"
