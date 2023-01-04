4d058b8d28e4beee0628868bbf3c2ef896a25bce
Patrick Linnane <patrick@linnane.io>
2023-01-04 12:43:34 -0800

    gridtracker 1.22.1226
    Update gridtracker from 1.22.1204 to 1.22.1226
    
    Closes #139068.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/gridtracker.rb
+++ Casks/gridtracker.rb
@@ -1,15 +1,15 @@
 cask "gridtracker" do
-  version "1.22.1204"
-  sha256 "8fea8db6839e725d5a8e728a3dab214603ee7562e797c7f7267ca8f451ba8e18"
+  version "1.22.1226"
+  sha256 "fde1ba94c7dc33d19965047b08054ac2c24df636cad10e315f123fff0a4ae8e4"
 
   url "https://downloads.gridtracker.org/v#{version}/GridTracker-#{version}-mac-x64.zip"
   name "GridTracker"
   desc "Warehouse of amateur radio information presented in an easy to use interface"
-  homepage "https://gridtracker.org/grid-tracker/"
+  homepage "https://gridtracker.org/"
 
   livecheck do
-    url "https://gridtracker.org/release.html"
-    regex(%r{href=.*?/GridTracker[._-]v?(\d+(?:\.\d+)+)-mac-x64\.zip}i)
+    url "https://gridtracker.org/index.php/documentation/change-log"
+    regex(/v(\d+(?:\.\d+)+)/i)
   end
 
   depends_on macos: ">= :mojave"
