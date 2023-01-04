287c2e4f6e10ba7df4ab335d2ddf3d5efbaa8c01
Patrick Linnane <patrick@linnane.io>
2023-01-04 09:54:13 -0800

    Update imhex from 1.25.0 to 1.26.0

--- Casks/imhex.rb
+++ Casks/imhex.rb
@@ -1,6 +1,6 @@
 cask "imhex" do
-  version "1.25.0"
-  sha256 "17d3749d3d1a7a5bfdfd0c377b4234868f683c3756da1b5488fbe5408848bcca"
+  version "1.26.0"
+  sha256 "c4f3cf722e42aa4705df52f1b7334deb7a77f1895a257017b9b24d589494d660"
 
   url "https://github.com/WerWolv/ImHex/releases/download/v#{version}/imhex-#{version}-macOS.dmg",
       verified: "github.com/WerWolv/ImHex/"
