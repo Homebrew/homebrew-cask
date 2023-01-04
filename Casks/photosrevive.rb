edbf3445386c428d9632cfb0fa6798703715b6ef
Patrick Linnane <patrick@linnane.io>
2023-01-04 11:25:22 -0800

    Update photosrevive from 2.0.9 to 2.0.10

--- Casks/photosrevive.rb
+++ Casks/photosrevive.rb
@@ -1,6 +1,6 @@
 cask "photosrevive" do
-  version "2.0.9,acaa4ba7-bf87-4995-b090-20cf079263e9"
-  sha256 "293ab35b81dbef4c6cd30ff85f086161b08cebac39abe9e948c8ba6a6c63370f"
+  version "2.0.10,7a1415b0-7db0-4965-b7a9-0c20e462a417"
+  sha256 "33bb944d72d2e688635d291e3f63c8dc29d29b62cc63f7850fcafec6be9b08c7"
 
   url "https://neededapps.nyc3.digitaloceanspaces.com/media/public/#{version.csv.second}.zip",
       verified: "neededapps.nyc3.digitaloceanspaces.com"
