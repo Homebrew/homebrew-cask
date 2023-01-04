b8c6eaa1d53c02f4d2299756fc74b7746ed02ef1
Patrick Linnane <patrick@linnane.io>
2023-01-04 11:08:52 -0800

    Update pitch from 1.105.0,stable.4,5021857 to 1.105.0,stable.5,5039096

--- Casks/pitch.rb
+++ Casks/pitch.rb
@@ -1,6 +1,6 @@
 cask "pitch" do
-  version "1.105.0,stable.4,5021857"
-  sha256 "a67befea7ccc51bd50d762c0582268380f271031ed0854d44da69486e37a3e5c"
+  version "1.105.0,stable.5,5039096"
+  sha256 "684844b41f2554f972509a51c2e5e377a68b7ece319ba41119745df81c317814"
 
   url "https://desktop-app-builds.pitch.com/Pitch-#{version.csv.first}-#{version.csv.second}-ci#{version.csv.third}.dmg"
   name "Pitch"
