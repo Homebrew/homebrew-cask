313f736c6fc2a4c24fc0dfb5a43865bb5049e9f0
Patrick Linnane <patrick@linnane.io>
2023-01-03 20:22:57 -0800

    Update uvtools from 3.9.5 to 3.10.0

--- Casks/uvtools.rb
+++ Casks/uvtools.rb
@@ -1,9 +1,9 @@
 cask "uvtools" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.9.5"
-  sha256 arm:   "6648fb0f989a40445a2ebf3e24eafb3a348fde446f2aa410a67ea24960055cd2",
-         intel: "82b040cebd2506bbeff677dc6562020a6f09bc1daffe0caec3f7d5ab55eb6efa"
+  version "3.10.0"
+  sha256 arm:   "a0d047bbe3171e6093d19d0b174c1ddb1020e8f70f44b96d8015277910de80f5",
+         intel: "a72c2ecf18becd895d6e61abaaf1764c563c8b6702ef45dae4e0857a352c12ee"
 
   url "https://github.com/sn4k3/UVtools/releases/download/v#{version}/UVtools_osx-#{arch}_v#{version}.zip"
   name "UVtools"
