a914648b780c0268fa9cf4a81d495cf3349cbb0d
Miccal Matthews <miccal.matthews@gmail.com>
2023-01-05 10:07:02 +0800

    Update and rename mmhmm.rb to mmhmm-studio.rb

--- Casks/mmhmm-studio.rb
+++ Casks/mmhmm-studio.rb
@@ -1,11 +1,11 @@
-cask "mmhmm" do
-  version "2.3.3,1661402000"
-  sha256 "81b4cf7d1de85172ea0bfd9be41ed4990ca98c5de713c5e73b99bb4bd7fabf77"
+cask "mmhmm-studio" do
+  version "2.4.0,1666031000"
+  sha256 "ff09794c189e0f1964c5f3cb49dd44a41f60dc722f1ab2868bf8040fe86028ee"
 
   url "https://updates.mmhmm.app/mac/production/mmhmm_#{version.csv.first}.zip"
-  name "mmhmm"
+  name "mmhmm Studio"
   desc "Virtual video presentation software"
-  homepage "https://www.mmhmm.app/"
+  homepage "https://www.mmhmm.app/product"
 
   livecheck do
     url "https://updates.mmhmm.app/mac/production/sparkle.xml"
@@ -13,9 +13,9 @@ cask "mmhmm" do
   end
 
   auto_updates true
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= :big_sur"
 
-  app "mmhmm.app"
+  app "mmhmm Studio.app"
 
   uninstall pkgutil:   "app.mmhmm.app",
             quit:      "app.mmhmm.app",
