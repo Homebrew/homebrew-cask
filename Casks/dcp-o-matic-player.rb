13f461dbc414daa0dd67a6b54a56a2a31ab22d63
Patrick Linnane <patrick@linnane.io>
2023-01-05 07:28:49 -0800

    dcp-o-matic-player 2.16.39
    Update dcp-o-matic-player from 2.16.38 to 2.16.39
    
    Closes #139098.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/dcp-o-matic-player.rb
+++ Casks/dcp-o-matic-player.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-player" do
-  version "2.16.38"
-  sha256 "2b3d8dc04bc6b07581ad14442cf1914a6d657a0a1b407ac2d33f61f91f186fb0"
+  version "2.16.39"
+  sha256 "235a62da17b4966bc4fd7d2181c5e8d5549e56c174c9c04bcd2fbc1296abfb1a"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-player&version=#{version}"
   name "DCP-o-matic Player"
