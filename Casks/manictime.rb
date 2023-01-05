63d34d6f601ebd4eabae6ff83c9d083157e14f7c
Patrick Linnane <patrick@linnane.io>
2023-01-05 07:51:47 -0800

    Update manictime from 2.3.1 to 2.3.2

--- Casks/manictime.rb
+++ Casks/manictime.rb
@@ -1,9 +1,9 @@
 cask "manictime" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.3.1"
-  sha256 arm:   "960b18de5880cfc1c9091a1e7470a53d8052181a459d7ce078f9613cb90d9eb2",
-         intel: "9c3bc318f52e08d7373058b230a3e9e89bd175688c186ce79619569d499a79b1"
+  version "2.3.2"
+  sha256 arm:   "0d649ca734a3ec74f6805a5600d088d17070ef46c43cfebed80d25b2d6aef202",
+         intel: "5324700989f984538ca85f530ac0c7256fdc83ca8ba8201c7d97c73bc9c0527e"
 
   url "https://cdn.manictime.com/setup/mac/v#{version.dots_to_underscores}/manictime-#{version}-osx-#{arch}.dmg"
   name "ManicTime"
