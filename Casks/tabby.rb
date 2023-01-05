70d8e29bfb41618a9afe389dcb492f6a63bb85ec
owine <owine@users.noreply.github.com>
2023-01-05 09:06:08 -0600

    tabby 1.0.188
    Update tabby from 1.0.187 to 1.0.188
    
    Closes #139087.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/tabby.rb
+++ Casks/tabby.rb
@@ -1,9 +1,9 @@
 cask "tabby" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.0.187"
-  sha256 arm:   "1b381980d11d0d50a4b34ffadc8b4ab703701076fbe00a0e480f6a95624dcc7b",
-         intel: "723eb5c6749bf7dd00f6cab89b17b9cee866e623053a632ec392fedef99c2ac6"
+  version "1.0.188"
+  sha256 arm:   "457dd9f4fc4e5dfa3aa1ac857430d9c8c33713a3c454cd72d9e3f1afecb4fa3b",
+         intel: "ea8adc451d19beafd9de3540cc882467b5d1efa00696367572bb7c9d64496ca5"
 
   url "https://github.com/Eugeny/tabby/releases/download/v#{version}/tabby-#{version}-macos-#{arch}.zip",
       verified: "github.com/Eugeny/tabby/"
