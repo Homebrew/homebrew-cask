0efa2b70b73e5e6e81e2d9781d0e6fc6ff78ee45
Patrick Linnane <patrick@linnane.io>
2023-01-05 07:37:47 -0800

    dingtalk 7.0.0.10
    Update dingtalk from 7.0.2.4 to 7.0.2.7
    
    Closes #139104.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/dingtalk.rb
+++ Casks/dingtalk.rb
@@ -2,13 +2,13 @@ cask "dingtalk" do
   arch arm: "qd=2022mac-m1"
 
   on_intel do
-    version "7.0.0.6"
-    sha256 "abee2a1ad99aad2834a13332df81ca99618260941f6c732a7cdaed886e216957"
+    version "7.0.0.10"
+    sha256 "78cb31cca396a41b77beb2267ecfefa96ec3cd1e742114a591d729fc12c75ed4"
     url "https://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/DingTalk_v#{version}.dmg"
   end
   on_arm do
-    version "7.0.2.4"
-    sha256 "85a1152eb2a86688295d61b89c0e37489e34b9724283b2a12d8e8fcbc92cda5f"
+    version "7.0.2.7"
+    sha256 "65143445f6ab77dac6859bf3389bdd3cd47b6e22117f2a68638819a612ff7dc2"
     url "https://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/M1-Beta/DingTalk-ARM64-#{version}.dmg"
   end
 
