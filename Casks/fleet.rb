4ba992659362d363d5a339e2ff9b24dce4d19102
Patrick Linnane <patrick@linnane.io>
2023-01-05 07:41:12 -0800

    Update fleet from 1.12.110 to 1.13.92

--- Casks/fleet.rb
+++ Casks/fleet.rb
@@ -2,9 +2,9 @@ cask "fleet" do
   arch arm: "aarch64", intel: "x64"
   arch_suffix = on_arch_conditional arm: "-aarch64"
 
-  version "1.12.110"
-  sha256 arm:   "49f2e9f6acd0c752919ff8cb2e6f3a7756e7612233bcbd8d4151974e15dd3870",
-         intel: "b4180a872d5fc87a209f0ec2dc7bafdeddb5520ab0ad79d0cecd98056bee2ffa"
+  version "1.13.92"
+  sha256 arm:   "01935294039d80c4cf17bce9456dc2dd461e88e5b4677c52075237eae6ba6274",
+         intel: "55af791a81a6556b3438a5384368054b2c1bf3c698e6ce969b93107997ddf970"
 
   url "https://download.jetbrains.com/fleet/installers/macos_#{arch}/Fleet-#{version}#{arch_suffix}.dmg"
   name "Fleet"
