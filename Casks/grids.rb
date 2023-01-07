7739b8129cb3123693827c6dc3e78488b311b749
Patrick Linnane <patrick@linnane.io>
2023-01-06 20:42:35 -0800

    Update grids from 8.2.4 to 8.3

--- Casks/grids.rb
+++ Casks/grids.rb
@@ -1,6 +1,6 @@
 cask "grids" do
-  version "8.2.4"
-  sha256 "87ef34e75984594d422f8f5ea0701e2130735442f8ba5721e37261be72322082"
+  version "8.3"
+  sha256 "0e375416f0e249b2af6b0ecd82e133b434ae7ab5bc7ba788a559b11b5dff55e5"
 
   url "https://gridsapp.net/bin/Grids_#{version}.zip"
   name "Grids"
