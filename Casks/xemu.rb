f9da69d69c7061e3889c250ba9392952260ed072
Patrick Linnane <patrick@linnane.io>
2023-01-08 09:21:04 -0800

    xemu 0.7.72
    Update xemu from 0.7.71 to 0.7.72
    
    Closes #139258.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/xemu.rb
+++ Casks/xemu.rb
@@ -1,12 +1,17 @@
 cask "xemu" do
-  version "0.7.71"
-  sha256 "19c9fd3e801eef6d968148f02a16731de94acf0ec800a3dc5c430ff1b24bb8c4"
+  version "0.7.72"
+  sha256 "fd8d81062cf93ef82076e4d3d63c7df404607ba719b72d839178da8b3f1f7bfe"
 
-  url "https://github.com/mborgerson/xemu/releases/download/v#{version}/xemu-macos-universal-release.zip",
-      verified: "github.com/mborgerson/xemu/"
+  url "https://github.com/xemu-project/xemu/releases/download/v#{version}/xemu-macos-universal-release.zip",
+      verified: "github.com/xemu-project/xemu/"
   name "Xemu"
   desc "Original Xbox Emulator"
   homepage "https://xemu.app/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "Xemu.app"
 
