b55259f9bc7958c1ce56b3c61be0e96cec6c0060
nanvenomous <mrgarelli@gmail.com>
2023-01-07 23:50:48 -0600

    devsquadron 0.0.3
    upgrade devsquadron to v0.0.3
    
    Closes #139210.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/devsquadron.rb
+++ Casks/devsquadron.rb
@@ -1,9 +1,9 @@
 cask "devsquadron" do
   arch arm: "arm", intel: "intel"
 
-  version "0.0.2"
-  sha256 arm:   "fe118c69dfd050929c10f2a0db1e92e76c04a2ae2591f0300f6af2fd2a7e1118",
-         intel: "e61d04fba788e29e5fb46887814afb61cdb2fa2f79956ebfeaf5bb942cd7c45c"
+  version "0.0.3"
+  sha256 arm:   "e71d4708967e2d0cb95ee9d20a55cacc537fa7c45a46e3dfdb4a3ee5218c04d0",
+         intel: "3db905f07d2d9a9e9450bb6e793d54fc391e41add4b919f135355e692a05cab2"
 
   url "https://developersquadron.com/downloads/devsquadron/v#{version}/macos-#{arch}.tar.xz"
   name "devsquadron"
