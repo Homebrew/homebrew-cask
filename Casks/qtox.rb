4607e7b94f5ea533dbe484409bfd6460ca7a9c67
Patrick Linnane <patrick@linnane.io>
2023-01-09 13:17:46 -0800

    qtox: fix style

--- Casks/qtox.rb
+++ Casks/qtox.rb
@@ -9,7 +9,7 @@ cask "qtox" do
   homepage "https://qtox.github.io/"
 
   app "qTox.app"
-  
+
   zap trash: [
     "~/Library/Preferences/chat.tox.qtox.plist",
     "~/Library/Saved Application State/chat.tox.qtox.savedState",
