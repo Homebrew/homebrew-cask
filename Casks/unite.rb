c0a354cbbd7165ad1af87a95f298171c3469faaa
Patrick Linnane <patrick@linnane.io>
2023-01-08 21:14:03 -0800

    Update unite from 4.2.5.0.1 to 4.3

--- Casks/unite.rb
+++ Casks/unite.rb
@@ -1,6 +1,6 @@
 cask "unite" do
-  version "4.2.5.0.1"
-  sha256 "83b8b3bb24f333cc7fd7af6678da2abdca03e84301a64449f2835a6fc603e4ec"
+  version "4.3"
+  sha256 "e0ca490b0fd2b0c78a89473f8c3d3f6ac8672cae96d831af0a5a71e2f58f7cbd"
 
   url "https://bzgdownloads.s3.amazonaws.com/Unite/Unite+#{version}.zip",
       verified: "bzgdownloads.s3.amazonaws.com/Unite/"
