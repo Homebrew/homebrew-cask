80a254eea9c4d84e2a135b2155efd3a5d21509d7
Patrick Linnane <patrick@linnane.io>
2023-01-08 20:56:09 -0800

    devsquadron 0.0.4
    Update devsquadron from 0.0.3 to 0.0.4
    
    Closes #139275.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/devsquadron.rb
+++ Casks/devsquadron.rb
@@ -1,9 +1,9 @@
 cask "devsquadron" do
   arch arm: "arm", intel: "intel"
 
-  version "0.0.3"
-  sha256 arm:   "e71d4708967e2d0cb95ee9d20a55cacc537fa7c45a46e3dfdb4a3ee5218c04d0",
-         intel: "3db905f07d2d9a9e9450bb6e793d54fc391e41add4b919f135355e692a05cab2"
+  version "0.0.4"
+  sha256 arm:   "a0c8ec835e9026c22603b140cc09ed93a040e599537c344562db6bab6e551fbd",
+         intel: "d47639513edda140cfe8d2333ec8a3434ddb60dc6710c5d8f5398af0b1879dbb"
 
   url "https://developersquadron.com/downloads/devsquadron/v#{version}/macos-#{arch}.tar.xz"
   name "devsquadron"
