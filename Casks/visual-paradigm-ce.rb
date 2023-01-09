2b59159150dae250a5a8619a35debec6f0f7af70
Patrick Linnane <patrick@linnane.io>
2023-01-08 21:19:24 -0800

    visual-paradigm-ce 17.0,20230101
    Update visual-paradigm-ce from 17.0,20221123 to 17.0,20230101
    
    Closes #139283.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/visual-paradigm-ce.rb
+++ Casks/visual-paradigm-ce.rb
@@ -1,9 +1,9 @@
 cask "visual-paradigm-ce" do
   arch arm: "AArch64", intel: "WithJRE"
 
-  version "17.0,20221123"
-  sha256 arm:   "1d64fdeccd521dd8059978ad5e6c708a1cdb221c14001251bf77dc42d1eb4c98",
-         intel: "ed1217adcd2a2a3bb3e1e64f9499f08066f22c701d0d44c0f73ff225e2b53ae5"
+  version "17.0,20230101"
+  sha256 arm:   "f8aa5893abb002466001edf3769676839f934ad475a8793ddeae8d94d7aa4cbf",
+         intel: "56c7adf82d4b4593cb4d0f4f71f32edb5bef74637ba67962c16d99f13d50cc3f"
 
   url "https://www.visual-paradigm.com/downloads/vpce/Visual_Paradigm_CE_#{version.csv.first.dots_to_underscores}_#{version.csv.second}_OSX_#{arch}.dmg"
   name "Visual Paradigm Community Edition"
