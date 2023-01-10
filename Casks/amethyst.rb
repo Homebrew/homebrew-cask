c015b67da0c0576a364a02e34c5dceb335d6496b
Patrick Linnane <patrick@linnane.io>
2023-01-10 12:15:15 -0800

    Update amethyst from 0.16.0 to 0.16.1

--- Casks/amethyst.rb
+++ Casks/amethyst.rb
@@ -9,7 +9,7 @@ cask "amethyst" do
     end
   end
   on_sierra :or_newer do
-    version "0.16.0"
+    version "0.16.1"
     sha256 "a60857ef84f426c01693101a01a439348858aa3f3d7e8d19a91b713d384967f3"
     url "https://github.com/ianyh/Amethyst/releases/download/v#{version}/Amethyst.zip",
         verified: "github.com/ianyh/Amethyst/"
