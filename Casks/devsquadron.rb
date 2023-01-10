b6db690251f0ae552d02dc340f8a93aacfd8bdf2
Patrick Linnane <patrick@linnane.io>
2023-01-10 12:28:47 -0800

    Update devsquadron from 0.0.4 to 0.0.5

--- Casks/devsquadron.rb
+++ Casks/devsquadron.rb
@@ -1,9 +1,9 @@
 cask "devsquadron" do
   arch arm: "arm", intel: "intel"
 
-  version "0.0.4"
-  sha256 arm:   "a0c8ec835e9026c22603b140cc09ed93a040e599537c344562db6bab6e551fbd",
-         intel: "d47639513edda140cfe8d2333ec8a3434ddb60dc6710c5d8f5398af0b1879dbb"
+  version "0.0.5"
+  sha256 arm:   "5a7ca7f2e3fd1ea7a4bc35e5564d5d1d95594fec1415454295f7c55ab0ee9610",
+         intel: "a5ca536c26ad93d465d7192039cca1133845bd78400bf129fbb770516232e015"
 
   url "https://developersquadron.com/downloads/devsquadron/v#{version}/macos-#{arch}.tar.xz"
   name "devsquadron"
