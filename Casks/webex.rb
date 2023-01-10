12bcaf45922d3767ad5db0eb115a98ba10b3b7d3
Patrick Linnane <patrick@linnane.io>
2023-01-10 13:27:24 -0800

    Update webex from 42.12.0.24485 to 43.1.0.24716

--- Casks/webex.rb
+++ Casks/webex.rb
@@ -1,7 +1,7 @@
 cask "webex" do
   arch arm: "Desktop-MACOS-Apple-Silicon", intel: "TeamsDesktop-MACOS"
 
-  version "42.12.0.24485"
+  version "43.1.0.24716"
   sha256 :no_check
 
   url "https://binaries.webex.com/Webex#{arch}-Gold/Webex.dmg"
