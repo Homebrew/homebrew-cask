a3c4050c57350cdc5792c232f3c6b1a2bcb855a6
Patrick Linnane <patrick@linnane.io>
2023-01-11 10:09:04 -0800

    shadowsocksx-ng 1.10.0
    * Update shadowsocksx-ng from 1.9.4 to 1.10.0
    * shadowsocksx-ng: add desc
    
    Closes #139450.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/shadowsocksx-ng.rb
+++ Casks/shadowsocksx-ng.rb
@@ -1,9 +1,10 @@
 cask "shadowsocksx-ng" do
-  version "1.9.4"
-  sha256 "dc06a995b63f8e32be9b86c265fd2979a6d73d4742d0ff16e1b2bb8f538d77a3"
+  version "1.10.0"
+  sha256 "1443c94dc0f69c97538015a3c180289aa6ef672c61cc3d41e9159dbb3a6737a5"
 
-  url "https://github.com/shadowsocks/ShadowsocksX-NG/releases/download/v#{version}/ShadowsocksX-NG.#{version}.zip"
+  url "https://github.com/shadowsocks/ShadowsocksX-NG/releases/download/v#{version}/ShadowsocksX-NG.dmg"
   name "ShadowsocksX-NG"
+  desc "Tunneling proxy"
   homepage "https://github.com/shadowsocks/ShadowsocksX-NG/"
 
   conflicts_with cask: "shadowsocksx"
