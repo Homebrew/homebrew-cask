33edb52aceb3a26d7dc3d8054d7559bf783deebe
Yusuf Kursad Kaya <ykursadkaya@gmail.com>
2023-01-11 15:17:20 +0300

    Update postman from 10.6.0 to 10.7.0

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,9 +1,9 @@
 cask "postman" do
   arch arm: "osx_arm64", intel: "osx64"
 
-  version "10.6.0"
-  sha256 arm:   "a0bcfd3b08aad66453dae9ece3e246e1c9d775e9515a4a64f261cf86ecf156c6",
-         intel: "66673ae6cb02a02a0b740cd5f9f5939ff941567051686c48f9d4aed3620ce102"
+  version "10.7.0"
+  sha256 arm:   "684146c3826401c2ad65dd7a4b7f5f6b91c0a5ff5893d7f889092ed1f653ede6",
+         intel: "372c9681349f5f24c66bdf47420e47e5a678b904f3c17d37973019652d205490"
 
   url "https://dl.pstmn.io/download/version/#{version}/#{arch}",
       verified: "dl.pstmn.io/download/version/"
