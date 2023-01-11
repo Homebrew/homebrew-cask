ce3b0485d9c38b9e5411de6a645862793cd117dc
Patrick Linnane <patrick@linnane.io>
2023-01-11 10:21:27 -0800

    Update topaz-video-ai from 3.0.11 to 3.0.12

--- Casks/topaz-video-ai.rb
+++ Casks/topaz-video-ai.rb
@@ -1,6 +1,6 @@
 cask "topaz-video-ai" do
-  version "3.0.11"
-  sha256 "a80552f674e1b7d9a8429666fbd0cbc9043342bb8ecc3d29c81e387ae43cb0ec"
+  version "3.0.12"
+  sha256 "71103764bd8f4e5544d871f49a3f5350d5fe5bf6dab7d76300c83338d58b8e10"
 
   url "https://downloads.topazlabs.com/deploy/TopazVideoAI/#{version}/TopazVideoAI-#{version}.dmg"
   name "Topaz Video AI"
