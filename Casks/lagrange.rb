8756861d32195665c59a0f97d51c648c0d9190c2
Patrick Linnane <patrick@linnane.io>
2023-01-11 09:51:41 -0800

    Update lagrange from 1.14.1 to 1.14.2

--- Casks/lagrange.rb
+++ Casks/lagrange.rb
@@ -1,9 +1,9 @@
 cask "lagrange" do
   arch arm: "macos11.0-arm64", intel: "macos10.13-x86_64"
 
-  version "1.14.1"
-  sha256 arm:   "beb940c7aee5fd1129ccfd84622c07865166bcc7e2502f736fdb885546b2bfb9",
-         intel: "0fd2e51132ce3565dfcffe9a6b738d61006eae990b3af2a62967b00172e1aaae"
+  version "1.14.2"
+  sha256 arm:   "4ba0f3bb861f13ecaf4b8a533a2652f983e7ba928268d3758bcde43fae62fd39",
+         intel: "eb5a400a4e89da8979bcecd2b0c2d5295b834d4cbb85237af5a9e74b9b6b01cb"
 
   url "https://github.com/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_#{arch}.tbz",
       verified: "github.com/skyjake/lagrange/"
