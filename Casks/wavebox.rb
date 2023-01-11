bb7a476a00a64e7a182f723cb83cab4017f3c024
Patrick Linnane <patrick@linnane.io>
2023-01-11 10:23:51 -0800

    wavebox 10.109.8.2
    Update wavebox from 10.108.19.2 to 10.109.8.2
    
    Closes #139460.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/wavebox.rb
+++ Casks/wavebox.rb
@@ -1,9 +1,9 @@
 cask "wavebox" do
   arch arm: "macarm64", intel: "mac"
 
-  version "10.108.19.2"
-  sha256 arm:   "6a9e4f2fd433de5fec12b59202a76b18b4a1f7cb11cdaae08984cd74fd3a5fce",
-         intel: "cd38238c7f6eeeceaa91b65cc0ab4362c2a68f8f03350e1f36d9b734cdd5836e"
+  version "10.109.8.2"
+  sha256 arm:   "b522ad81d116e68f25c1edd57ef6f6551737b142db7ff95cbf0bb46c411611c0",
+         intel: "885e37fb14c128c873b5497ef286446f951d9d2a186983b778236e833b41a6f4"
 
   url "https://download.wavebox.app/stable/#{arch}/Install%20Wavebox%20#{version}.dmg",
       verified: "download.wavebox.app/"
