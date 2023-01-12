9ad7e4871c98c331643f5eee809d3c561ceeb0fd
Alex Perathoner <alex.perathoner@gmail.com>
2023-01-12 23:52:51 +0100

    slimhud 1.4.2
    * slimhud: fix min os and zap trash
    * Update slimhud.rb
    
    Closes #139526.
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/slimhud.rb
+++ Casks/slimhud.rb
@@ -1,15 +1,15 @@
 cask "slimhud" do
-  version "1.4.1"
-  sha256 "a9650cc1cfaed88804ca8814b766ff4e66033218b84c1c3e5e3e69ef070d2fd8"
+  version "1.4.2"
+  sha256 "23867d84760630debe22728ba6d088580c40e41c182a9e576ff7c615a9b239de"
 
   url "https://github.com/AlexPerathoner/SlimHUD/releases/download/v#{version}/SlimHUD.zip"
   name "SlimHUD"
   desc "Replacement for the volume, brightness and keyboard backlight HUDs"
   homepage "https://github.com/AlexPerathoner/SlimHUD/"
 
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= :high_sierra"
 
   app "SlimHUD.app"
 
-  zap trash: "~/Library/Preferences/AlexP.SlimHUD.plist"
+  zap trash: "~/Library/Preferences/com.alexpera.SlimHUD.plist"
 end
