8c1dec4133cc2e88550ceac716bbd3ec8fe2483e
Patrick Linnane <patrick@linnane.io>
2023-01-11 22:05:46 -0800

    standard-notes 3.137.1
    Update standard-notes from 3.131.0 to 3.137.1
    
    Closes #139492.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -1,9 +1,9 @@
 cask "standard-notes" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.131.0"
-  sha256 arm:   "b6c1f4527b5e707d6b17b67f06a2f72c8c42144abb64645203da3d487de248eb",
-         intel: "63848a8ffab2207d47216e52f8be6ec0c2433479ce950cc3f2b1655bd1524211"
+  version "3.137.1"
+  sha256 arm:   "f36ef563f86990320514255582b3c19296545d36b8811f0fde3e064f404e378c",
+         intel: "1f182800c21194bbcc4842659a06738c8c92ec339f359b6351b661ae9b61e59e"
 
   url "https://github.com/standardnotes/app/releases/download/%40standardnotes%2Fdesktop%40#{version}/standard-notes-#{version}-mac-#{arch}.zip",
       verified: "github.com/standardnotes/app/"
