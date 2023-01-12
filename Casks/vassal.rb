b5aaec96434a0e0e4c375baf911c548f5678c6c6
Patrick Linnane <patrick@linnane.io>
2023-01-11 22:20:19 -0800

    Update vassal from 3.6.9 to 3.6.10

--- Casks/vassal.rb
+++ Casks/vassal.rb
@@ -1,9 +1,9 @@
 cask "vassal" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "3.6.9"
-  sha256 arm:   "b9a5f98dea10b63916a0d2b6596cafe88046939f3b26a68ec2b9ab850b034e4e",
-         intel: "eabe625037db6a1a5f715500fddbb94ea381f474877ca7205bba820c9c07a30c"
+  version "3.6.10"
+  sha256 arm:   "1779feaf63db14ae091bfdcd5516a34d09d709f557ef23d15bcafe1297a5be5f",
+         intel: "602bac1009e292f36b515cae28bbffd319198c7c906dba40f2afb23d6a651f42"
 
   url "https://github.com/vassalengine/vassal/releases/download/#{version}/VASSAL-#{version}-macos-#{arch}.dmg",
       verified: "github.com/vassalengine/vassal/"
