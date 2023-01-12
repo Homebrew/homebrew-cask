850063dc4d450c37fd32e33be382efcd6bf2453c
Patrick Linnane <patrick@linnane.io>
2023-01-11 22:16:17 -0800

    thedesk 23.0.5
    Update thedesk from 23.0.3 to 23.0.5
    
    Closes #139494.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/thedesk.rb
+++ Casks/thedesk.rb
@@ -1,11 +1,8 @@
 cask "thedesk" do
-  arch arm: "arm64", intel: "x64"
+  version "23.0.5"
+  sha256 "88e089902e377c02a44998e4fb13687b6ce60e44918eed31afd2f94b49daa104"
 
-  version "23.0.3"
-  sha256 arm:   "55475f5574dff5c3ca73a561c15951527551da47ba977f4c3f2a6d6540389601",
-         intel: "a69be8db2f3f9ea84a1b5ad31ea91730008aa193728ae15731784ee4ced8378c"
-
-  url "https://github.com/cutls/TheDesk/releases/download/v#{version}/TheDesk-#{version}-#{arch}.dmg",
+  url "https://github.com/cutls/TheDesk/releases/download/v#{version}/TheDesk-#{version}-universal.dmg",
       verified: "github.com/cutls/TheDesk/"
   name "TheDesk"
   desc "Mastodon/Misskey Client for PC"
