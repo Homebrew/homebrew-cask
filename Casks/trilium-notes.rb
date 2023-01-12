de67e71bb39737545f7dd0d868fc90f30ecdaf27
Patrick Linnane <patrick@linnane.io>
2023-01-11 22:19:29 -0800

    trilium-notes 0.58.4
    Update trilium-notes from 0.57.5 to 0.58.4
    
    Closes #139496.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/trilium-notes.rb
+++ Casks/trilium-notes.rb
@@ -1,6 +1,6 @@
 cask "trilium-notes" do
-  version "0.57.5"
-  sha256 "fde732bb60f7582763d64c7d25fef034084a862a2a656e3097edef6e4ae0fff7"
+  version "0.58.4"
+  sha256 "ac2998d0db959292c7d30f209da6dda353780f5c9855e51d45f48a805207bfbf"
 
   url "https://github.com/zadam/trilium/releases/download/v#{version}/trilium-mac-x64-#{version}.zip"
   name "Trilium Notes"
