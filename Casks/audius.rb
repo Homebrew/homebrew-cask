2ad0b624df3296c49a7066739f375ede99b521cf
Patrick Linnane <patrick@linnane.io>
2023-01-11 20:24:29 -0800

    audius 1.5.5
    Update audius from 1.5.4 to 1.5.5
    
    Closes #139468.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/audius.rb
+++ Casks/audius.rb
@@ -1,6 +1,6 @@
 cask "audius" do
-  version "1.5.4"
-  sha256 "6592e7c1d7d76f2b664ffa5640e88da526e3b0203185c89e2321463181d0bc61"
+  version "1.5.5"
+  sha256 "ce28d3f5d3fdd19a53f6d04d290d269449fc34afe29c5d9f0e39fd4a9a106f1e"
 
   url "https://download.audius.co/Audius-#{version}.dmg"
   name "Audius"
