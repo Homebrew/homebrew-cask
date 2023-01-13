73b72da73685c64a4b38766a6bc9078dbdab6e38
EINDEX <eindex.me@outlook.com>
2023-01-13 01:21:22 +0000

    Update burp-suite from 2022.12.5 to 2022.12.6

--- Casks/burp-suite.rb
+++ Casks/burp-suite.rb
@@ -1,9 +1,9 @@
 cask "burp-suite" do
   arch arm: "MacOsArm64", intel: "MacOsx"
 
-  version "2022.12.5"
-  sha256 arm:   "40fa861491019eeaf64da14a4a4261cf5a06115f4a69c46dd9ae2cc256243be8",
-         intel: "8a29fefa00190f86efc22f4660aceb7d116b5e3b3d2b80a113320c7d526caff7"
+  version "2022.12.6"
+  sha256 arm:   "d87a7a2e74e70bd891b90bd61663bc7bbdf0acb8edb40a6b0e8256ee8802ad57",
+         intel: "6513c9588fb1c672f5a98d28a5f2278ae8ee988894234907d4013374d3205d5c"
 
   url "https://portswigger.net/burp/releases/download?product=community&version=#{version}&type=#{arch}"
   name "Burp Suite Community Edition"
