19c4a6f5d1471e52c54d04fda257c4b845d2204c
Yusuf Kursad Kaya <ykursadkaya@gmail.com>
2023-01-13 10:44:49 +0300

    confluent-cli 2.38.0
    Update confluent-cli from 2.37.0 to 2.38.0
    
    Closes #139535.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/confluent-cli.rb
+++ Casks/confluent-cli.rb
@@ -1,6 +1,6 @@
 cask "confluent-cli" do
-  version "2.37.0"
-  sha256 "ae54ee1cf9893d8110562554a8b7de61c76985d8e53169134a2db0cf5a12a099"
+  version "2.38.0"
+  sha256 "9b5519761b74659b1b7e5a4e37ad0e7428909a5a91908fd9f1a94a83e9da761c"
 
   url "https://s3-us-west-2.amazonaws.com/confluent.cloud/confluent-cli/archives/#{version}/confluent_v#{version}_darwin_amd64.tar.gz",
       verified: "s3-us-west-2.amazonaws.com/confluent.cloud/confluent-cli/archives/"
