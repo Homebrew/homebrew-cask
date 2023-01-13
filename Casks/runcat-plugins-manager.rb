4b6852ed0b3595b606288b609ec10e3e83fa0db1
Patrick Linnane <patrick@linnane.io>
2023-01-13 08:06:46 -0800

    runcat-plugins-manager: add livecheck

--- Casks/runcat-plugins-manager.rb
+++ Casks/runcat-plugins-manager.rb
@@ -1,11 +1,16 @@
 cask "runcat-plugins-manager" do
-  version "1.3"
+  version "1.3.0"
   sha256 :no_check
 
   url "https://kyome.io/resources/runcat_plugins_manager.dmg"
-  name "runcat-plugins-manager"
+  name "RunCat Plugins Manager"
   desc "Enable RunCat to show the CPU temperature"
   homepage "https://kyome.io/runcat/index.html?lang=en#plugins"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   depends_on macos: ">= :catalina"
 
