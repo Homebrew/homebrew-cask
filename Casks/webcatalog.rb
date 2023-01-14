233249458a7e2e67a29f37feee153eee17950778
Patrick Linnane <patrick@linnane.io>
2023-01-13 21:23:52 -0800

    webcatalog 44.5.0
    Update webcatalog from 44.4.0 to 44.5.0
    
    Closes #139623.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/webcatalog.rb
+++ Casks/webcatalog.rb
@@ -1,6 +1,6 @@
 cask "webcatalog" do
-  version "44.4.0"
-  sha256 "4086a8a5c462b1bea2e1bbd9155a58021d9346a0542de43d6c294fae2a970561"
+  version "44.5.0"
+  sha256 "3a0659f247d9683ab8d6ac733c3eb286b353d9dc5dfa6ec4de7b01495c0618b8"
 
   url "https://cdn-2.webcatalog.io/webcatalog/WebCatalog-#{version}-universal.dmg"
   name "WebCatalog"
