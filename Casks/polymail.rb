e3656c50b8ede4cc8f427525029f663bdf5c9dd4
Patrick Linnane <patrick@linnane.io>
2023-01-13 21:05:31 -0800

    polymail 2.2.8
    Update polymail from 2.2.6 to 2.2.8
    
    Closes #139601.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/polymail.rb
+++ Casks/polymail.rb
@@ -1,9 +1,9 @@
 cask "polymail" do
   arch arm: "-arm64"
 
-  version "2.2.6"
-  sha256 arm:   "e5d7ddf964e817ca604612d85ffe26381eb9d3f73962b40af32b5ef071833a2b",
-         intel: "7f0545b2a99ab9fa515b6f78437a53226e6e1f788a8ed411e6bbd3fff5c09249"
+  version "2.2.8"
+  sha256 arm:   "82cc62cc3435b75e1e2161a4b120cbd2fbeacf71e9673725f928a17ad9440082",
+         intel: "2e6a9f4fb78a16fad49496621279005ff7732123467051b260c8db353467f8b8"
 
   url "https://sparkle-updater.polymail.io/macos#{arch}/builds/Polymail-v#{version}.zip"
   name "Polymail"
