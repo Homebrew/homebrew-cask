cafa84ef1f928bb7e04451ac11c1420fc8d7f633
Miccal Matthews <miccal.matthews@gmail.com>
2023-01-15 01:42:35 +0800

    Update lyx.rb

--- Casks/lyx.rb
+++ Casks/lyx.rb
@@ -1,29 +1,19 @@
 cask "lyx" do
   version "2.3.7"
+  sha256 "4a0e5d9ad2d08f2b379892816934b64b99d815eaeede14157c3219f80fe039d2"
 
-  on_monterey :or_older do
-    sha256 "aaaaa005c5ec4bf574534de31cbd93ab4908dfa655da7535044185874a285c52"
-    url "https://ftp.lip6.fr/pub/lyx/bin/#{version.major_minor_patch}/LyX-#{version}+qt5-x86_64-cocoa.dmg",
-        verified: "ftp.lip6.fr/pub/lyx/"
-  end
-  on_ventura :or_newer do
-    sha256 "4a0e5d9ad2d08f2b379892816934b64b99d815eaeede14157c3219f80fe039d2"
-    url "https://ftp.lip6.fr/pub/lyx/bin/#{version.major_minor_patch}/LyX-#{version}+qt5-x86_64-arm64-cocoa.dmg",
-        verified: "ftp.lip6.fr/pub/lyx/"
-  end
-
+  url "https://ftp.lip6.fr/pub/lyx/bin/#{version.major_minor_patch}/LyX-#{version}+qt5-x86_64-arm64-cocoa.dmg",
+      verified: "ftp.lip6.fr/pub/lyx/bin/"
   name "LyX"
-  desc "Open source, GUI document processor based on the LaTeX typesetting system"
+  desc "GUI document processor based on the LaTeX typesetting system"
   homepage "https://www.lyx.org/"
 
   livecheck do
     url "https://www.lyx.org/Download"
-    regex(/LyX-(\d+(?:\.\d+)*)\+qt5/i)
+    regex(/LyX[._-]v?(\d+(?:\.\d+)+)\+qt5/i)
   end
 
-  on_monterey :or_newer do
-    depends_on formula: "python"
-  end
+  depends_on macos: ">= :mojave"
 
   app "LyX.app"
   binary "#{appdir}/LyX.app/Contents/MacOS/inkscape", target: "lyx-inkscape"
@@ -36,8 +26,8 @@ cask "lyx" do
 
   zap trash: [
     "~/Library/Application Support/LyX-#{version.major_minor}",
-    "~/Library/Preferences/org.lyx.LyX-#{version.major_minor}.plist",
     "~/Library/Caches/com.apple.python/Applications/LyX.app",
+    "~/Library/Preferences/org.lyx.LyX-#{version.major_minor}.plist",
     "~/Library/Preferences/org.lyx.lyx.plist",
     "~/Library/Saved Application State/org.lyx.lyx.savedState",
   ]
