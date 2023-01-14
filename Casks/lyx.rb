cfa50c0a3b42435056a5cf3b0f6030e886bceccd
kojiOnHill <95485564+kojiOnHill@users.noreply.github.com>
2023-01-15 02:59:56 +0900

    Reflects changes in download files in lyx-2.3.7 (#139334)
    * Update lyx.rb
    
    Current url points to a binary for older versions of MacOS, which is a limited version such that SyncTeX does not work. Modified so that newer MacOS  points to a full version.
    
    * Depends on python
    
    MacOS does not include python since Monterey on which LyX depends.
    See https://wiki.lyx.org/Mac/Mac#toc2
    
    * Modified styles
    
    Modified styles so that "brew style --fix lyx" does not complain.
    
    * Update lyx.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/lyx.rb
+++ Casks/lyx.rb
@@ -1,17 +1,19 @@
 cask "lyx" do
   version "2.3.7"
-  sha256 "aaaaa005c5ec4bf574534de31cbd93ab4908dfa655da7535044185874a285c52"
+  sha256 "4a0e5d9ad2d08f2b379892816934b64b99d815eaeede14157c3219f80fe039d2"
 
-  url "https://ftp.lip6.fr/pub/lyx/bin/#{version.major_minor_patch}/LyX-#{version}+qt5-x86_64-cocoa.dmg",
-      verified: "ftp.lip6.fr/pub/lyx/"
+  url "https://ftp.lip6.fr/pub/lyx/bin/#{version.major_minor_patch}/LyX-#{version}+qt5-x86_64-arm64-cocoa.dmg",
+      verified: "ftp.lip6.fr/pub/lyx/bin/"
   name "LyX"
-  desc "Open source, GUI document processor based on the LaTeX typesetting system"
+  desc "GUI document processor based on the LaTeX typesetting system"
   homepage "https://www.lyx.org/"
 
   livecheck do
     url "https://www.lyx.org/Download"
-    regex(/LyX-(\d+(?:\.\d+)*)\+qt5/i)
+    regex(/LyX[._-]v?(\d+(?:\.\d+)+)\+qt5/i)
   end
+
+  depends_on macos: ">= :mojave"
 
   app "LyX.app"
   binary "#{appdir}/LyX.app/Contents/MacOS/inkscape", target: "lyx-inkscape"
@@ -24,8 +26,8 @@ cask "lyx" do
 
   zap trash: [
     "~/Library/Application Support/LyX-#{version.major_minor}",
-    "~/Library/Preferences/org.lyx.LyX-#{version.major_minor}.plist",
     "~/Library/Caches/com.apple.python/Applications/LyX.app",
+    "~/Library/Preferences/org.lyx.LyX-#{version.major_minor}.plist",
     "~/Library/Preferences/org.lyx.lyx.plist",
     "~/Library/Saved Application State/org.lyx.lyx.savedState",
   ]
