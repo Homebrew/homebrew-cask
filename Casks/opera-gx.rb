9828b8730d9ed2b77ea965b58aeca034cddd3096
Patrick Linnane <patrick@linnane.io>
2023-01-13 20:57:30 -0800

    Update opera-gx from 93.0.4585.84 to 94.0.4606.64

--- Casks/opera-gx.rb
+++ Casks/opera-gx.rb
@@ -1,6 +1,6 @@
 cask "opera-gx" do
-  version "93.0.4585.84"
-  sha256 "f143a6a5bec52df8f8006f1f000b4c906100fb12d253fc21b6ab49c0aebfa675"
+  version "94.0.4606.64"
+  sha256 "8bae3c0df115b4d4fbb6b49d786f2957852947ba21de054fd6b1c1209ddc8598"
 
   url "https://get.geo.opera.com/pub/opera_gx/#{version}/mac/Opera_GX_#{version}_Setup.dmg"
   name "Opera GX"
