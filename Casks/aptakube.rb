926e21791aa4c68cd656c5cfe4fa0540a524637b
goenning <me@goenning.net>
2023-01-15 12:12:31 +0000

    aptakube 1.0.1
    Update aptakube from 1.0.0 to 1.0.1
    
    Closes #139659.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/aptakube.rb
+++ Casks/aptakube.rb
@@ -1,6 +1,6 @@
 cask "aptakube" do
-  version "1.0.0"
-  sha256 "a92152be99b42128ec8a212334b47f74dfd11054e1a700d89420c029e45cb90f"
+  version "1.0.1"
+  sha256 "7e288e03df5e05b7bc3e7298fab2c9b6bad089348e8a3ebf38e16bdb0d5926ff"
 
   url "https://releases.aptakube.com/Aptakube_#{version}_universal.dmg"
   name "Aptakube"
