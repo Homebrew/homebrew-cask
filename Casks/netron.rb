fd3a1e7ebf6ae94893ee6b071671a4b76a991c6e
Lutz <lutzroeder@users.noreply.github.com>
2023-01-16 05:01:30 +0000

    Update Netron to 6.4.4

--- Casks/netron.rb
+++ Casks/netron.rb
@@ -1,6 +1,6 @@
 cask "netron" do
-  version "6.4.2"
-  sha256 "3b8ed60e88f980f96ea0852bb8c0e5c30639919b53af56180ce3059450c8fb21"
+  version "6.4.4"
+  sha256 "0de7d71f1dae03bd16ad3e478f9c68fb00052f604bf49658afe4124d5bac621d"
 
   url "https://github.com/lutzroeder/netron/releases/download/v#{version}/Netron-#{version}-mac.zip"
   name "Netron"
