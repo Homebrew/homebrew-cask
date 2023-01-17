564f403b26bb065c2f27b0c54e56ed060edb05ad
Bevan Kay <email@bevankay.me>
2023-01-17 22:32:52 +1100

    Update expressvpn from 11.16.1.71768 to 11.17.0.71984

--- Casks/expressvpn.rb
+++ Casks/expressvpn.rb
@@ -1,6 +1,6 @@
 cask "expressvpn" do
-  version "11.16.1.71768"
-  sha256 "fec0b0d8a15f1c9f496f64707801a1066be53c174ccee62660cf1e0a1c153684"
+  version "11.17.0.71984"
+  sha256 "a22c95c6f66acfd0c65e556453d8021e925bb3602a4405335ac1f4c57763c49b"
 
   url "https://www.expressvpn.works/clients/mac/expressvpn_mac_#{version}_release.pkg"
   name "ExpressVPN"
