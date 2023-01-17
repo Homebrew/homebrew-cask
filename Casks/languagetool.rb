3125104c9f743489e1adc6231350ff87c516429a
Jeremy Wang <30622992+wjlmelody@users.noreply.github.com>
2023-01-17 09:25:03 +0800

    Update languagetool with version 1.1.3 (#139688)

--- Casks/languagetool.rb
+++ Casks/languagetool.rb
@@ -1,6 +1,6 @@
 cask "languagetool" do
-  version "1.1.2,39"
-  sha256 "97093c0d17d99f1cb9429abf378abc342fce08721e1f5639070ab6634ee8a8f2"
+  version "1.1.3,40"
+  sha256 "56124cf77c77ddbcf21a8005be7a26694123b16d4a5e3d3d1c57b181f95ba2b1"
 
   url "https://languagetool.org/download/mac-app/LanguageToolDesktop-#{version.csv.first}.dmg"
   name "LanguageTool for Desktop"
