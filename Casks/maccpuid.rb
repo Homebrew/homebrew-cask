f5da70d377f9740ff1029bab468ed4e97454db17
Sean Molenaar <SMillerDev@users.noreply.github.com>
2023-01-17 10:47:54 +0100

    maccpuid: use livecheck

--- Casks/maccpuid.rb
+++ Casks/maccpuid.rb
@@ -3,10 +3,14 @@ cask "maccpuid" do
   sha256 :no_check
 
   url "https://software.intel.com/content/dam/develop/public/us/en/documents/maccpuid.zip"
-  appcast "https://software.intel.com/content/www/us/en/develop/download/download-maccpuid.html"
   name "MacCPUID"
   desc "Displays information via the CPUID instruction"
   homepage "https://software.intel.com/content/www/us/en/develop/download/download-maccpuid"
+
+  livecheck do
+    url "https://software.intel.com/content/www/us/en/develop/download/download-maccpuid.html"
+    strategy :sparkle
+  end
 
   app "MacCPUID.app"
 
