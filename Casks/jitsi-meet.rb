b9717559864a9cd95880a70967ffc13bb95441db
Thomas Walter <tw@b-a-l-u.de>
2023-01-17 22:12:46 +0100

    jitsi-meet 2023.1.0
    Update jitsi-meet from 2022.12.0 to 2023.1.0
    
    Closes #139713.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/jitsi-meet.rb
+++ Casks/jitsi-meet.rb
@@ -1,6 +1,6 @@
 cask "jitsi-meet" do
-  version "2022.12.0"
-  sha256 "5e641f5e6e25cf1f5cb29a2e9b9b9b2752d829779fc7e4b505c4920782118d60"
+  version "2023.1.0"
+  sha256 "e6ce7f719faf145e69034c8ce64032487ce0c81ae1df1b118aa056a9c7311476"
 
   url "https://github.com/jitsi/jitsi-meet-electron/releases/download/v#{version}/jitsi-meet.dmg"
   name "Jitsi Meet"
