10219e6970df201029c3527612051215358ab8ac
Miccal Matthews <miccal.matthews@gmail.com>
2023-01-18 22:02:16 +0800

    Update codux.rb

--- Casks/codux.rb
+++ Casks/codux.rb
@@ -1,13 +1,13 @@
 cask "codux" do
-  arch arm: "arm64-arm", intel: "x64-dmg"
+  arch arm: "arm64-dmg", intel: "x64-dmg"
 
   on_arm do
-    version "14.1.0,b478ab_d61c0ea5192e4c558ebcc75d5403dbec"
-    sha256 "1113d39324d6e921c64455c5d4060da455f78f3dff5553a7eb97a8f902a7f937"
+    version "14.2.2,e855c7_0f725c10eb7d4b50848510cbd2c31a5d"
+    sha256 "91412362931ef5cc52241918cab60543226d7da68cf8045fce06cc3a48ede67f"
   end
   on_intel do
-    version "14.1.0,b478ab_4886668678ca41e2bcafc6403e4f7f6d"
-    sha256 "b76a4e3516bb8af9b4bec20056be74b98780786071a5ef1a835c88753a6b57f5"
+    version "14.2.2,e855c7_7a9afde8182f43f9a3cc7c7263434849"
+    sha256 "488f5f4b5ed403e5e210308b7db4e4c35f3f68c40467c31f1c79c22bc8413371"
   end
 
   url "https://95610582-f2db-4cc9-be23-c529650d1ad9.filesusr.com/archives/#{version.csv.second}.zip?dn=Codux-#{version.csv.first}.#{arch}.zip",
@@ -18,7 +18,7 @@ cask "codux" do
 
   livecheck do
     url "https://www.codux.com/download/"
-    regex(%r{archives/(.+)\.zip\?dn=Codux[._-]v?(\d+(?:\.\d+)+).#{arch}\.zip}i)
+    regex(%r{archives/(.+)\.zip\?dn=Codux[._-]v?(\d+(?:\.\d+)+)\.#{arch}\.zip}i)
     strategy :page_match do |page, regex|
       page.scan(regex).map { |match| "#{match[1]},#{match[0]}" }
     end
