95a53f174abdfcc906dd3ad878c3f85abbeb61f3
Miccal Matthews <miccal.matthews@gmail.com>
2023-01-19 05:41:41 +0800

    Update mimestream.rb

--- Casks/mimestream.rb
+++ Casks/mimestream.rb
@@ -1,6 +1,6 @@
 cask "mimestream" do
-  version "0.41.0"
-  sha256 "92ccfb463079e3304acdc83791aa96109d2be0dbbbad8950af2067f1c38ef88d"
+  version "0.41.1"
+  sha256 "a6fd07a312014932f3714abc01151fa91146fd0cf53ff22c54575b42c8477b9a"
 
   url "https://download.mimestream.com/Mimestream_#{version}.dmg"
   name "Mimestream"
@@ -8,8 +8,8 @@ cask "mimestream" do
   homepage "https://mimestream.com/"
 
   livecheck do
-    url "https://mimestream.com/appcast.xml"
-    strategy :sparkle, &:short_version
+    url "https://mimestream.com/releases"
+    regex(/Version\sv?(\d+(?:\.\d+)+)/i)
   end
 
   auto_updates true
@@ -18,11 +18,9 @@ cask "mimestream" do
   app "Mimestream.app"
 
   zap trash: [
-    "~/Library/Application Scripts/com.mimestream.Mimestream",
-    "~/Library/Application Scripts/com.mimestream.Mimestream.ShareExtension",
+    "~/Library/Application Scripts/com.mimestream.Mimestream*",
     "~/Library/Caches/com.mimestream.Mimestream",
-    "~/Library/Containers/com.mimestream.Mimestream",
-    "~/Library/Containers/com.mimestream.Mimestream.ShareExtension",
+    "~/Library/Containers/com.mimestream.Mimestream*",
     "~/Library/Preferences/com.mimestream.Mimestream.plist",
     "~/Library/Saved Application State/com.mimestream.Mimestream.savedState",
   ]
