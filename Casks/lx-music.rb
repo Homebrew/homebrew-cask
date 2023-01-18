901cbbd06322508f21953d35798535ba2e2cbf1b
SvenFE <shensven2016@gmail.com>
2023-01-18 12:55:13 +0800

    Update lx-music from 2.0.3 to 2.0.4

--- Casks/lx-music.rb
+++ Casks/lx-music.rb
@@ -1,9 +1,9 @@
 cask "lx-music" do
   arch arm: "-arm64"
 
-  version "2.0.3"
-  sha256 arm:   "52095a1a256ebc89cc4f7cc642971206bbd603b525a8d9357ffbe96bb1713b74",
-         intel: "d3a88801acb9a91efb1069bbfe185a14d559d600c3891afef0b14c8f3b34dfef"
+  version "2.0.4"
+  sha256 arm:   "56a6d1a935b0d51a27afeef2087506c6a990b51f0f9c1141cd28049b022021d3",
+         intel: "d16943f3419fff3d3f70e428be6bd59435a7037202611c2fba472a7768bf3465"
 
   url "https://github.com/lyswhut/lx-music-desktop/releases/download/v#{version}/lx-music-desktop-#{version}#{arch}.dmg"
   name "LX Music Assistant Desktop Edition"
