410f5b4f166a404503ad1bef4a735401046d3a3d
zuisong <19145952+zuisong@users.noreply.github.com>
2023-01-19 16:43:06 +0800

    Update dingtalk from 7.0.2.7 to 7.0.2.9 (#139756)
    * Update dingtalk from 7.0.2.7 to 7.0.2.9
    
    * dingtalk: bump Intel to 7.0.0.12
    
    Co-authored-by: Patrick Linnane <patrick@linnane.io>

--- Casks/dingtalk.rb
+++ Casks/dingtalk.rb
@@ -2,13 +2,13 @@ cask "dingtalk" do
   arch arm: "qd=2022mac-m1"
 
   on_intel do
-    version "7.0.0.10"
-    sha256 "78cb31cca396a41b77beb2267ecfefa96ec3cd1e742114a591d729fc12c75ed4"
+    version "7.0.0.12"
+    sha256 "6b279dab2560013fa4a5ea8a11ba408ef8fa377247c5ff63b4ead439bd5f6544"
     url "https://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/DingTalk_v#{version}.dmg"
   end
   on_arm do
-    version "7.0.2.7"
-    sha256 "65143445f6ab77dac6859bf3389bdd3cd47b6e22117f2a68638819a612ff7dc2"
+    version "7.0.2.9"
+    sha256 "736caa9e7590151a2b432c5cf84afc05811039510173e8fb0888b158ed519112"
     url "https://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/M1-Beta/DingTalk-ARM64-#{version}.dmg"
   end
 
