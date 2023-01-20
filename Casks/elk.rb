c089c1ca34f0330e3abb6c6ff4ef97d7455acb38
Miccal Matthews <miccal.matthews@gmail.com>
2023-01-21 00:48:02 +0800

    Update elk.rb

--- Casks/elk.rb
+++ Casks/elk.rb
@@ -1,18 +1,18 @@
 cask "elk" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.2.0"
-  sha256 arm:   "0d3124dfc666157e99206c28c569ceb47d48ca11f4465fe3edda8cc25a5143ca",
-         intel: "6e109fc83f7754250c9968d72a14420adaef9f2e8b46e18aa54ddd5f44534d9e"
+  version "0.2.1"
+  sha256 arm:   "55aeefa3706495344b98757c470a88f067f71d3ba3da5791f02e495a87936634",
+         intel: "6ead714773b0bf0575b9822d31c18692a43ac9dd5b074c996e617b1e0a1da7fe"
 
   url "https://github.com/elk-zone/elk-native/releases/download/elk-native-v#{version}/Elk_#{version}_macos_#{arch}.dmg"
   name "Elk Native"
-  desc "Native version of Elk, a nimble Mastodon web client"
+  desc "Mastodon web client"
   homepage "https://github.com/elk-zone/elk-native"
 
   livecheck do
-    url "https://github.com/elk-zone/elk-native/releases/"
-    regex(/elk-native-v(\d+(?:\.\d+)+)/i)
+    url :url
+    strategy :github_latest
   end
 
   app "Elk.app"
