88b0e74947b563ba1c652deeb1d6135a10c3647b
3xp0rt <61662492+3xp0rt@users.noreply.github.com>
2023-01-20 22:11:37 +0200

    Update forkgram-telegram.rb

--- Casks/forkgram-telegram.rb
+++ Casks/forkgram-telegram.rb
@@ -1,7 +1,7 @@
 cask "forkgram-telegram" do
   arch arm: "arm64", intel: "x86"
 
-  version "4.5.5"
+  version "4.5.6"
   sha256 arm:   "83308558e8204a65e02128f2ad822a821c096cc0b7c4794f6b285aa04c2e6100",
          intel: "cc6c75936c4db4bc3dc53654747ec4d295eb18c500fb259d656a20bb1afe9d5a"
 
