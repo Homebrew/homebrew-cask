46f449d133d2484a92558af0f753b93c935c2a60
miccal <miccal.matthews@gmail.com>
2023-01-23 06:11:36 +0800

    forkgram-telegram 4.5.6
    * Update forkgram-telegram from 4.5.5 to 4.5.6
    * forkgram-telegram: add livecheck
    
    Closes #139856.
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/forkgram-telegram.rb
+++ Casks/forkgram-telegram.rb
@@ -1,14 +1,40 @@
 cask "forkgram-telegram" do
   arch arm: "arm64", intel: "x86"
 
-  version "4.5.5"
-  sha256 arm:   "83308558e8204a65e02128f2ad822a821c096cc0b7c4794f6b285aa04c2e6100",
-         intel: "cc6c75936c4db4bc3dc53654747ec4d295eb18c500fb259d656a20bb1afe9d5a"
+  version "4.5.6"
+  sha256 arm:   "2e65ea7551014da618a48c4cbd58c0879f3a2022716cb3b90e1fcc13174bd517",
+         intel: "97133f09723ccf3eb158e6d85ccd0cb317b8eefa0b27ef5a5100290e08d48bfa"
 
   url "https://github.com/Forkgram/tdesktop/releases/download/v#{version}/Forkgram.macOS.no.auto-update_#{arch}.zip"
   name "Forkgram"
   desc "Fork of Telegram Desktop"
   homepage "https://github.com/Forkgram/"
+
+  # NOTE: This approach involves multiple requests and should be avoided
+  # whenever possible. If upstream starts reliably providing the macOS zip
+  # files in every release, we should switch to `url :url` with
+  # `strategy :github_latest`.
+  livecheck do
+    url "https://github.com/Forkgram/tdesktop/releases?q=prerelease%3Afalse"
+    regex(%r{/v?(\d+(?:\.\d+)+)/Forkgram[._-]macOS[._-][^"' >]*?#{arch}\.zip}i)
+    strategy :page_match do |page, regex|
+      # Collect the release tags on the page
+      tags = page.scan(%r{href=["']?[^"' >]*?/releases/tag/([^"' >]*?)["' >]}i)&.flatten&.uniq
+
+      max_reqs = 4
+      tags.each_with_index do |tag, i|
+        break if i >= max_reqs
+
+        # Fetch the assets list HTML for the tag and match within it
+        assets_page = Homebrew::Livecheck::Strategy.page_content(
+          @url.sub(%r{/releases/?.+}, "/releases/expanded_assets/#{tag}"),
+        )
+        matches = assets_page[:content]&.scan(regex)&.map { |match| match[0] }
+
+        break matches if matches.present?
+      end
+    end
+  end
 
   # Renamed to avoid conflict with telegram
   app "Telegram.app", target: "Forkgram.app"
