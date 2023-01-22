ac45951a1417fd96e5621888436d9ba43a77a17d
HuaDeity <tidy.year0352@fastmail.com>
2023-01-22 23:35:43 +0800

    Fix style

--- Casks/interact-scratchpad.rb
+++ Casks/interact-scratchpad.rb
@@ -3,8 +3,7 @@ cask "interact-scratchpad" do
   sha256 :no_check
 
   url "https://s3-us-west-2.amazonaws.com/downloads.agiletortoise.com/Interact+Scratchpad.app.zip",
-    verified: "s3-us-west-2.amazonaws.com/downloads.agiletortoise.com/"
-
+      verified: "s3-us-west-2.amazonaws.com/downloads.agiletortoise.com/"
   name "Interact Scratchpad"
   desc "Menu bar utility to create contacts from snippets of text"
   homepage "https://docs.getdrafts.com/docs/misc/interact-scratchpad"
