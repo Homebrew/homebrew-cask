1ec011d1eccff168218c8f6f94911ead526024ca
Dareth NHANG <dnhang@beapi.fr  # Switch to personnal account: contact@dsigned.me>
2023-01-23 00:28:41 +0100

    Update rectangle from 0.65,71 to 0.65

--- Casks/rectangle.rb
+++ Casks/rectangle.rb
@@ -1,5 +1,5 @@
 cask "rectangle" do
-  version "0.65,71"
+  version "0.65"
   sha256 "2b2ac56512a54c18f1f55067944a901c35513c616c27db828bdd6122d2885575"
 
   url "https://github.com/rxhanson/Rectangle/releases/download/v#{version.csv.first}/Rectangle#{version.csv.first}.dmg",
