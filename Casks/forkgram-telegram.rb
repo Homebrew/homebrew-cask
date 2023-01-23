7b51b67fa867cf36b317bb8c3804b07ed25a94f7
Sam Ford <1584702+samford@users.noreply.github.com>
2023-01-22 23:53:50 -0500

    forkgram-telegram: add livecheck

--- Casks/forkgram-telegram.rb
+++ Casks/forkgram-telegram.rb
@@ -10,6 +10,32 @@ cask "forkgram-telegram" do
   desc "Fork of Telegram Desktop"
   homepage "https://github.com/Forkgram/"
 
+  # NOTE: This approach involves multiple requests and should be avoided
+  # whenever possible. If upstream starts reliably providing the macOS zip
+  # files in every release, we should switch to `url :url` with
+  # `strategy :github_latest`.
+  livecheck do
+    url "https://github.com/Forkgram/tdesktop/releases?q=prerelease%3Afalse"
+    regex(%r{/v?(\d+(?:\.\d+)+)/Forkgram[._-]macOS[._-][^"' >]*?#{arch}\.zip}i)
+    strategy :page_match do |page, regex|
+      # Collect the release tags on the page
+      tags = page.scan(%r{href=["']?[^"' >]*?/releases/tag/([^"' >]*?)["' >]}i)&.flatten&.uniq
+
+      max_reqs = 4
+      tags.each_with_index do |tag, i|
+        break if i >= max_reqs
+
+        # Fetch the assets list HTML for the tag and match within it
+        assets_page = Homebrew::Livecheck::Strategy.page_content(
+          @url.sub(%r{/releases/?.+}, "/releases/expanded_assets/#{tag}"),
+        )
+        matches = assets_page[:content]&.scan(regex)&.map { |match| match[0] }
+
+        break matches if matches.present?
+      end
+    end
+  end
+
   # Renamed to avoid conflict with telegram
   app "Telegram.app", target: "Forkgram.app"
 
