41b612e2b62d45fb723875b95b68a523675d74af
Patrick Linnane <patrick@linnane.io>
2023-01-23 10:04:12 -0800

    electron: bump Intel

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -3,7 +3,7 @@ cask "electron" do
 
   version "22.0.3"
   sha256 arm:   "7ce05be9889d7103aae9f68fdf42b3c4ee5993f6517f1b3223c073e4225a7b26",
-         intel: "b42c036e099fd402b8f907aef229fb66bd4040a1b4d38a249a60bbd250b444a9"
+         intel: "859f8d3767560f51d1724e3d36efea1f89dee6438535110f9af902f0532d9aec"
 
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-#{arch}.zip",
       verified: "github.com/electron/electron/"
