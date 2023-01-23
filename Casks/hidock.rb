5d86f9b13b14363a3cd98bc6be5f2b748921e26e
Miccal Matthews <miccal.matthews@gmail.com>
2023-01-23 15:37:11 +0800

    Update hidock.rb

--- Casks/hidock.rb
+++ Casks/hidock.rb
@@ -2,7 +2,7 @@ cask "hidock" do
   version "1.0.1,629"
   sha256 "874095f3eb1281a1ed4822d4875723daa5cfbb5a248e2c84102bfc58f2b4453e"
 
-  url "https://hidock.app/HiDock_#{version.major_minor_patch}.zip"
+  url "https://hidock.app/HiDock_#{version.csv.first}.zip"
   name "HiDock"
   desc "Set custom Dock settings for when on different displays"
   homepage "https://hidock.app/"
