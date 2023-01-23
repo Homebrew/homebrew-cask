ceb4233d58cb9bd006c6dfa1c571ecffd2ca3722
hyuraku <32809703+hyuraku@users.noreply.github.com>
2023-01-23 20:45:40 +0900

    electron 22.0.3
    * Update electron from 22.0.2 to 22.0.3
    * electron: bump Intel
    
    Closes #139864.
    
    Co-authored-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,9 +1,9 @@
 cask "electron" do
   arch arm: "arm64", intel: "x64"
 
-  version "22.0.2"
-  sha256 arm:   "2aace0a6f4797b6fa6c3f84932ebcd5601168cd2945b2191e3fea48a29aa9f46",
-         intel: "b42c036e099fd402b8f907aef229fb66bd4040a1b4d38a249a60bbd250b444a9"
+  version "22.0.3"
+  sha256 arm:   "7ce05be9889d7103aae9f68fdf42b3c4ee5993f6517f1b3223c073e4225a7b26",
+         intel: "859f8d3767560f51d1724e3d36efea1f89dee6438535110f9af902f0532d9aec"
 
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-#{arch}.zip",
       verified: "github.com/electron/electron/"
