d2801714592e94c597b7bde9cd034c757b0ae324
Patrick Linnane <patrick@linnane.io>
2023-01-24 14:47:07 -0800

    linear-linear 1.15.0,23011361pf52q49
    Update linear-linear from 1.14.0 to 1.15.0
    
    Closes #140024.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/linear-linear.rb
+++ Casks/linear-linear.rb
@@ -1,7 +1,7 @@
 cask "linear-linear" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.14.0,230106wdceyvxl1"
+  version "1.15.0,23011361pf52q49"
   sha256 :no_check
 
   url "https://desktop.linear.app/mac/dmg/#{arch}"
