62e3c6c7ff1eea6a184b5fa6b99eb8cb1870bd36
hybras <24651269+hybras@users.noreply.github.com>
2023-01-24 12:12:01 -0500

    Add arm builds of museeks

--- Casks/museeks.rb
+++ Casks/museeks.rb
@@ -1,8 +1,11 @@
 cask "museeks" do
-  version "0.13.1"
-  sha256 "79130a16985aab46e2e0c1c270e3339f96886cc60d2f02b531533dd0307b0d48"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://github.com/martpie/museeks/releases/download/#{version}/museeks-x64.dmg",
+  version "0.13.1"
+  sha256 arm:   "97428b9d02dec50a80df8232d166e9460330acfda38b99320424350c33e8e061",
+         intel: "79130a16985aab46e2e0c1c270e3339f96886cc60d2f02b531533dd0307b0d48"
+
+  url "https://github.com/martpie/museeks/releases/download/#{version}/museeks-#{arch}.dmg",
       verified: "github.com/martpie/museeks/"
   name "Museeks"
   desc "Music player"
