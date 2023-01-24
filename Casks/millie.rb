288cfd59636f2e502bae9a43fada67c960a21323
Patrick Linnane <patrick@linnane.io>
2023-01-24 14:58:33 -0800

    millie 5.2.0
    Update millie from 5.1.1 to 5.2.0
    
    Closes #140039.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/millie.rb
+++ Casks/millie.rb
@@ -1,6 +1,6 @@
 cask "millie" do
-  version "5.1.1"
-  sha256 "8fffd1691353a2ed65e60c76a6f9c8eab7d32b4d00fad582e60b810f0caa0bf3"
+  version "5.2.0"
+  sha256 "3c415262d15f7730d3db85a4bf3835dbff0baef1a14b1f63c86b24d8fc669449"
 
   url "https://apis.millie.co.kr/v1/download/installer/mac/Millie-#{version}.dmg"
   name "millie"
