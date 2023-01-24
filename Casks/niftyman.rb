d2733cbf99fedac6735f5080a3755631b2b38383
Patrick Linnane <patrick@linnane.io>
2023-01-24 15:05:07 -0800

    niftyman 1.2.0
    Update niftyman from 1.1.2 to 1.2.0
    
    Closes #140049.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/niftyman.rb
+++ Casks/niftyman.rb
@@ -1,6 +1,6 @@
 cask "niftyman" do
-  version "1.1.2"
-  sha256 "b9fe90eff7427331360e0c2b603b443a383b3bbe936de5625644e70541bb6f1b"
+  version "1.2.0"
+  sha256 "a841228e1234791e04dc67b8d0c82441e6136d8844f99b2acbbb9a7958f0a4d0"
 
   url "https://notion-menubar-updates.s3-us-west-2.amazonaws.com/Niftyman-#{version}.dmg",
       verified: "notion-menubar-updates.s3-us-west-2.amazonaws.com/"
