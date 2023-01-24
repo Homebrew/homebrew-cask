7bf30682690c8423eb262855f3b2e67b75822445
Patrick Linnane <patrick@linnane.io>
2023-01-24 15:49:50 -0800

    thedesk 24.0.3
    Update thedesk from 23.0.5 to 24.0.3
    
    Closes #140105.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/thedesk.rb
+++ Casks/thedesk.rb
@@ -1,6 +1,6 @@
 cask "thedesk" do
-  version "23.0.5"
-  sha256 "88e089902e377c02a44998e4fb13687b6ce60e44918eed31afd2f94b49daa104"
+  version "24.0.3"
+  sha256 "8886cf187b64ef75257094b625f0d94927a8b3dff0b7b5b24f72a398c6bd14bd"
 
   url "https://github.com/cutls/TheDesk/releases/download/v#{version}/TheDesk-#{version}-universal.dmg",
       verified: "github.com/cutls/TheDesk/"
