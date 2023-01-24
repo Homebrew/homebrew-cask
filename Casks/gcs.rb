967e29c4b77701e1c1c1ef8023eab7b42e907fc0
Patrick Linnane <patrick@linnane.io>
2023-01-24 14:26:19 -0800

    Update gcs from 5.6.0 to 5.7.1

--- Casks/gcs.rb
+++ Casks/gcs.rb
@@ -1,9 +1,9 @@
 cask "gcs" do
   arch arm: "apple", intel: "intel"
 
-  version "5.6.0"
-  sha256 arm:   "564bfaf96e3eb0c1f1c1b485d95fb30f787467e51f27f9508d80ac9544a762ed",
-         intel: "f957f1f7dd12fe91d1ae7d3e59a81ad96801cabd66c26c8b8e7ddd3863c18a81"
+  version "5.7.1"
+  sha256 arm:   "eb429cc2c2982850efaa7f635f2a3b43b7b240765b2dfed92b9d6c068e3ef134",
+         intel: "bf06320de41043e9d7a85dacb9fba29a7da82493d3dc1f40982d33500a6c5729"
 
   url "https://github.com/richardwilkes/gcs/releases/download/v#{version}/gcs-#{version}-macos-#{arch}.dmg",
       verified: "github.com/richardwilkes/gcs/"
