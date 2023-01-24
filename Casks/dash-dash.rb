a37a0d739834cc8963e2adc79fdfd91bfea195ab
Patrick Linnane <patrick@linnane.io>
2023-01-24 13:39:42 -0800

    Update dash-dash from 18.2.0 to 18.2.1

--- Casks/dash-dash.rb
+++ Casks/dash-dash.rb
@@ -1,6 +1,6 @@
 cask "dash-dash" do
-  version "18.2.0"
-  sha256 "97e0eb5b4155eb63f544d6a35af0febe68c3c67192fbc5847467689ad3339880"
+  version "18.2.1"
+  sha256 "b1c8f75aab3a748c21fb946019e21a27dc40370fccf0e0eca5ee39e4ab2622b9"
 
   url "https://github.com/dashpay/dash/releases/download/v#{version}/dashcore-#{version}-osx.dmg",
       verified: "github.com/dashpay/dash/"
