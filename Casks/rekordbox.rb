e97df50d4f7bddf6618bcd35a86fe681ed09b861
Ignacio Lago <ignacio@ignaciolago.com>
2023-01-24 19:58:46 +0100

    rekordbox 6.6.9,20230111110322
    * Update rekordbox from 6.6.8 to 6.6.9
    * rekordbox: fix url
    
    Closes #139912.
    
    Co-authored-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/rekordbox.rb
+++ Casks/rekordbox.rb
@@ -1,6 +1,6 @@
 cask "rekordbox" do
-  version "6.6.8,20221125124421"
-  sha256 "d61bb3b7ccedbe17459ae417480b52c81659d3cc7f4f89d33cb42fe92442db86"
+  version "6.6.9,20230111110322"
+  sha256 "9b15cfb832896a6c3997f9dd19d57ce041527105c98bd81fce1e55264b1e893c"
 
   url "https://cdn.rekordbox.com/files/#{version.csv.second}/Install_rekordbox_#{version.csv.first.dots_to_underscores}.pkg_.zip"
   name "rekordbox"
