6a6b22ebde5c0dbee4755622d6c85376127d2931
Patrick Linnane <patrick@linnane.io>
2023-01-24 13:01:13 -0800

    alfaview 8.61.1
    Update alfaview from 8.61.0 to 8.61.1
    
    Closes #139928.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/alfaview.rb
+++ Casks/alfaview.rb
@@ -1,6 +1,6 @@
 cask "alfaview" do
-  version "8.61.0"
-  sha256 "ec9eb2ab451fd1d3109ad1bc0801795313d69046b3778d5d4a38993a13c92723"
+  version "8.61.1"
+  sha256 "9efe80a9ae2c25a9614a4252bb8bdd5401d7b2e0d5ea5f44fff4be777cbbe77f"
 
   url "https://assets.alfaview.com/stable/mac/alfaview-mac-production-#{version}.dmg"
   name "Alfaview"
