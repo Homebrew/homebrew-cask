5eef62873ebdf3335f08b1338db692b4b74361a6
Patrick Linnane <patrick@linnane.io>
2023-01-24 14:47:49 -0800

    livebook 0.8.1
    Update livebook from 0.8.0 to 0.8.1
    
    Closes #140025.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/livebook.rb
+++ Casks/livebook.rb
@@ -1,6 +1,6 @@
 cask "livebook" do
-  version "0.8.0"
-  sha256 "ad873cef7212f0ede82c19a10a0c6daae7361a403b3d0f5c2b2b2b40e13d126b"
+  version "0.8.1"
+  sha256 "00181027216346b3e4f7ef89668de719dcf1c3a55cabf8ca1b6a0e0987298f5b"
 
   url "https://github.com/livebook-dev/livebook/releases/download/v#{version}/LivebookInstall-v#{version}-macos-universal.dmg",
       verified: "github.com/livebook-dev/livebook"
