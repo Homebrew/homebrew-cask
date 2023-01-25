441420751d8ec150861c81d42fa4028a4ec29398
Patrick Linnane <patrick@linnane.io>
2023-01-24 19:43:06 -0800

    dcp-o-matic-playlist-editor 2.16.41
    Update dcp-o-matic-playlist-editor from 2.16.40 to 2.16.41
    
    Closes #140171.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/dcp-o-matic-playlist-editor.rb
+++ Casks/dcp-o-matic-playlist-editor.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-playlist-editor" do
-  version "2.16.40"
-  sha256 "1f1aac51186148bd22575b7f44bd6efed2108d3a91e2683cdc675cbfbad9ab92"
+  version "2.16.41"
+  sha256 "10a529087e3168561f27bb87c295a8262fbdced6f63bafc930715c6bc6f8f4c7"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-playlist&version=#{version}"
   name "DCP-o-matic Playlist Editor"
