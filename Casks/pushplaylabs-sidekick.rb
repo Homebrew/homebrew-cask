8c6b78776311dd87ccd17ebece10db0b90e04a68
Patrick Linnane <patrick@linnane.io>
2023-01-24 20:56:19 -0800

    Update pushplaylabs-sidekick from 1.0.75 to 108.34.2.29216

--- Casks/pushplaylabs-sidekick.rb
+++ Casks/pushplaylabs-sidekick.rb
@@ -1,23 +1,32 @@
 cask "pushplaylabs-sidekick" do
-  version "1.0.75"
-  sha256 "2a8e6c420ac6d16ad7b1bedeaffd779a7bd637218980d9798f3535677fa26dda"
+  arch arm: "arm64", intel: "x64"
+  livecheck_folder = on_arch_conditional arm: "macm1", intel: "mac"
 
-  url "https://cdn.meetsidekick.com/browser-builds/sidekick-mac-installer-#{version}.pkg"
+  on_intel do
+    version "108.35.1.29386,ec54390"
+    sha256 "b139771ffd47ffd4d2befd4ad83118bd616cd6635cbd777963750f9beeebc6bf"
+  end
+  on_arm do
+    version "108.34.2.29216,f02c915"
+    sha256 "018fcc2f8593cb6dca52547a9df6fcb3d371008f82d6882a600676dd8b121c86"
+  end
+
+  url "https://cdn.meetsidekick.com/browser-builds/sidekick-mac-release-#{arch}-#{version.csv.first}-#{version.csv.second}-df.dmg"
   name "Sidekick"
   desc "Browser designed for modern work"
   homepage "https://www.meetsidekick.com/"
 
   livecheck do
-    url "https://api.meetsidekick.com/downloads/df/mac"
-    regex(/(\d+(?:\.\d+)+)\.pkg/i)
-    strategy :header_match
+    url "https://api.meetsidekick.com/downloads/df/#{livecheck_folder}"
+    strategy :header_match do |headers|
+      match = headers["location"].match(/[_-](\d+(?:\.\d+)+)[_-](.+)[._-](?:default|df)\.dmg/i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
   end
 
-  pkg "sidekick-mac-installer-#{version}.pkg"
-
-  uninstall delete:  "/Applications/Sidekick.app",
-            pkgutil: "org.Sidekick.#{version}",
-            quit:    "com.pushplaylabs.sidekick"
+  app "Sidekick.app"
 
   zap trash: [
     "~/Library/Application Support/Sidekick",
