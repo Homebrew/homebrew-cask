5039b0940dac34a8dbab60658ebcbaf8e289677f
Patrick Linnane <patrick@linnane.io>
2023-01-24 19:21:38 -0800

    Update cctalk from 7.10.0.1 to 7.10.1-1331

--- Casks/cctalk.rb
+++ Casks/cctalk.rb
@@ -1,8 +1,8 @@
 cask "cctalk" do
-  version "7.10.0.1"
-  sha256 "3764ffeb9e63b9de570064fec29b69dcfe5c0dd9d9613e23cbfc3764b061ea12"
+  version "7.10.1-1331"
+  sha256 "d37a12dbbe06cbe1a7dbd13a3c074a6f82789b56abc1c82933d92601cf0a0634"
 
-  url "https://cc.hjfile.cn/cc/#{version}/8/1/103/#{version}.dmg",
+  url "https://cc.hjfile.cn/cc/CCtalk.#{version}/8/1/103/CCtalk.#{version}.dmg",
       verified: "cc.hjfile.cn/"
   name "CCtalk"
   desc "Real-time interactive education platform"
@@ -10,6 +10,7 @@ cask "cctalk" do
 
   livecheck do
     url "https://www.cctalk.com/webapi/basic/v1.1/version/down?apptype=1&terminalType=8&versionType=103"
+    regex(/CCtalk[._-]v?(\d+(?:\.\d+)+-\d+)/i)
     strategy :header_match
   end
 
