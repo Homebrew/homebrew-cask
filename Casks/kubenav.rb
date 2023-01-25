eaf40809c507d3d6c8609370587ca8415f15f907
Patrick Linnane <patrick@linnane.io>
2023-01-24 20:01:26 -0800

    Update kubenav from 3.9.0 to 4.0.0

--- Casks/kubenav.rb
+++ Casks/kubenav.rb
@@ -1,11 +1,8 @@
 cask "kubenav" do
-  arch arm: "arm64", intel: "amd64"
+  version "4.0.0"
+  sha256 "be1c5a65de00ed7cea60348dd5efd46b1e6836b90969e15dc84e76f72c2b5a2d"
 
-  version "3.9.0"
-  sha256 arm:   "ad3d0778d85cad9271ba5942f9bc6de0130edcb28d1ecc9cfdb0ee3e4c040222",
-         intel: "81dfd86497a1a5081cec1c4a544eb7959218db0107ddc3758b0b3e5367db950f"
-
-  url "https://github.com/kubenav/kubenav/releases/download/#{version}/kubenav-darwin-#{arch}.zip",
+  url "https://github.com/kubenav/kubenav/releases/download/v#{version}/kubenav-macos-universal.zip",
       verified: "github.com/kubenav/kubenav/"
   name "kubenav"
   desc "Navigator for your Kubernetes clusters right in your pocket"
