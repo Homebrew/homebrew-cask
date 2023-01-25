ad63fabb895f6c79da373055b4cad71a69de4244
Patrick Linnane <patrick@linnane.io>
2023-01-24 19:42:19 -0800

    Update dcp-o-matic from 2.16.40 to 2.16.41

--- Casks/dcp-o-matic.rb
+++ Casks/dcp-o-matic.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic" do
-  version "2.16.40"
-  sha256 "2906622b98c1a3fff570bdbea77aa3eec93b297eb0d8cd9e0012780dcfb7923d"
+  version "2.16.41"
+  sha256 "19473a0fb6492e552827ca2f800004c87020f89cd2d948cda178e91470536ed3"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-main&version=#{version}"
   name "DCP-o-matic"
