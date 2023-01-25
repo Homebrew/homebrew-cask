48137266a210f4e5bcd9d1fc6b5c291fcb5afb91
Patrick Linnane <patrick@linnane.io>
2023-01-24 20:26:31 -0800

    Update pd-l2ork from 2.17.0 to 2.18.1

--- Casks/pd-l2ork.rb
+++ Casks/pd-l2ork.rb
@@ -1,8 +1,8 @@
 cask "pd-l2ork" do
-  version "2.17.0,20210417,ca592f6f"
-  sha256 "c890f169663d9cdf947baaa2ce9cf0d48fcd07cb71368b7eab29547b237dab39"
+  version "2.18.1,20230117,e1770339"
+  sha256 "f20eb92635c25f118fed5a2f47b8fe1a4529e5b8ce798c7382a8bdb0ab2c46c1"
 
-  url "https://github.com/agraef/purr-data/releases/download/#{version.csv.first}/Pd-l2ork-OSX10.11-#{version.csv.first}-#{version.csv.second}-rev.#{version.csv.third}-x86_64.dmg",
+  url "https://github.com/agraef/purr-data/releases/download/#{version.csv.first}/Pd-l2ork-#{version.csv.first}-#{version.csv.second}-rev.#{version.csv.third}-x86_64.dmg",
       verified: "github.com/agraef/purr-data/"
   name "Pd-l2ork"
   name "Purr Data"
@@ -10,12 +10,20 @@ cask "pd-l2ork" do
   homepage "https://agraef.github.io/purr-data/"
 
   livecheck do
-    url :url
-    strategy :github_latest do |page|
-      match = page.match(%r{href=.*?/v?(\d+(?:\.\d+)+)/Pd-l2ork-OSX10\.11-\1-(\d+)-rev\.(\h+)-x86_64\.dmg}i)
-      next if match.blank?
+    url "https://github.com/agraef/purr-data/releases/latest"
+    regex(%r{href=.*?/v?(\d+(?:\.\d+)+)/Pd-l2ork-\1-(\d+)-rev\.(\h+)-x86_64\.dmg}i)
+    strategy :header_match do |headers, regex|
+      next if headers["location"].blank?
 
-      "#{match[1]},#{match[2]},#{match[3]}"
+      # Identify the latest tag from the response's `location` header
+      latest_tag = File.basename(headers["location"])
+      next if latest_tag.blank?
+
+      # Fetch the assets list HTML for the latest tag and match within it
+      assets_page = Homebrew::Livecheck::Strategy.page_content(
+        @url.sub(%r{/releases/?.+}, "/releases/expanded_assets/#{latest_tag}"),
+      )
+      assets_page[:content]&.scan(regex)&.map { |match| "#{match[0]},#{match[1]},#{match[2]}" }
     end
   end
 
