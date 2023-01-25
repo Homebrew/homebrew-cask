9887e553267c8f9bb51c32cc94fcfd50b4252727
Patrick Linnane <patrick@linnane.io>
2023-01-24 19:09:12 -0800

    anydo 5.0.19
    Update anydo from 5.0.18 to 5.0.19
    
    Closes #140157.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/anydo.rb
+++ Casks/anydo.rb
@@ -1,8 +1,8 @@
 cask "anydo" do
-  version "5.0.18"
-  sha256 "41dde3f73e41942ab1f551406dbb29af6068ec6895882abdce61aa21e8ef2b48"
+  version "5.0.19"
+  sha256 "d317a09940c6c52880a20e07ba64ef6c807b98d4cac787edf684c293fe11d460"
 
-  url "https://electron-app.any.do/Anydo-#{version}.dmg"
+  url "https://electron-app.any.do/Anydo-#{version}-universal.dmg"
   name "Any.do"
   desc "Reminder, planner & calendar"
   homepage "https://www.any.do/"
