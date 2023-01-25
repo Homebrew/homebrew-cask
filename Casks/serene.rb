2e2f389b3843688726c0a4bddf3a8824ff0380e4
Max <kanielrkirby@gmail.com>
2023-01-24 23:23:05 -0600

    Add Serene.app v3.0.1

---
+++ Casks/serene.rb
@@ -0,0 +1,11 @@
+cask "serene" do
+  version "3.0.1"
+  sha256 :no_check
+
+  url "https://app.sereneapp.com/download/"
+  name "Serene"
+  desc "Productivity app for focus and planning"
+  homepage "https://sereneapp.com/"
+
+  app "Serene.app"
+end
