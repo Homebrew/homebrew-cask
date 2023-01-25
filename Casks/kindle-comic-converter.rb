9fe9e73b5d71ad6cc0b85dd1d0c5216fe7205d45
Patrick Linnane <patrick@linnane.io>
2023-01-24 19:52:01 -0800

    Update kindle-comic-converter from 5.5.2 to 5.6.1

--- Casks/kindle-comic-converter.rb
+++ Casks/kindle-comic-converter.rb
@@ -1,17 +1,13 @@
 cask "kindle-comic-converter" do
-  version "5.5.2"
-  sha256 "73d3f7ab9fc3e1e3dee62a83848fb52bba62f45fef56063466a0ac9c9f57513a"
+  version "5.6.1"
+  sha256 "e16051d13146f0763712776db24318666760571fe6420017bf502f7a153f06fd"
 
-  url "https://kcc.iosphe.re/OSX/KindleComicConverter_osx_#{version}.dmg"
+  url "https://github.com/ciromattia/kcc/releases/download/v#{version}/KindleComicConverter_osx_#{version}.dmg",
+      verified: "github.com/ciromattia/kcc"
   name "Kindle Comic Converter"
   name "KCC"
   desc "Comic and manga converter for ebook readers"
   homepage "https://kcc.iosphe.re/"
-
-  livecheck do
-    url "https://github.com/ciromattia/kcc"
-    strategy :git
-  end
 
   depends_on macos: ">= :mojave"
 
