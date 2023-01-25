10eeef0e7e5e1a0d590224b3d3824e1c52bacf37
Patrick Linnane <patrick@linnane.io>
2023-01-24 16:05:46 -0800

    Update universal-media-server from 13.0.1 to 13.1.0

--- Casks/universal-media-server.rb
+++ Casks/universal-media-server.rb
@@ -1,9 +1,9 @@
 cask "universal-media-server" do
   arch arm: "-arm"
 
-  version "13.0.1"
-  sha256 arm:   "f848f356ee84568d9613d764dc6cf495cbd04e89e7689935e4d98fab24c78c91",
-         intel: "dae721f7ede2e4c62efa4d5b548f5b0720d029e385f716636d6a3408ab894ad1"
+  version "13.1.0"
+  sha256 arm:   "9e120e34a0a6bdeaa073a2bfb0acca2356d5672e7c7584010615ccab0693a909",
+         intel: "e1d6db4ebf33deaee9013711c178e93241bde7492a9b47b477fa69f397b1e486"
 
   url "https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/#{version}/UMS-#{version}#{arch}.dmg",
       verified: "github.com/UniversalMediaServer/UniversalMediaServer/"
