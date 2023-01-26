aa318efabf685f1114afed21396a0e8d7b972acb
Patrick Linnane <patrick@linnane.io>
2023-01-25 16:02:39 -0800

    tagspaces 5.2.2
    Update tagspaces from 5.1.4 to 5.2.2
    
    Closes #140232.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/tagspaces.rb
+++ Casks/tagspaces.rb
@@ -1,9 +1,9 @@
 cask "tagspaces" do
   arch arm: "arm64", intel: "x64"
 
-  version "5.1.4"
-  sha256 arm:   "766e6a9ee438cabb8ead63425f4a67460bdd05521c30a851487e10bfbb8cb628",
-         intel: "c8ed56e0a98aa9ba8eb91b05748d96d90dfd1f44a3388f8d32a7171a3616baf3"
+  version "5.2.2"
+  sha256 arm:   "c3c2236f03cac7751ffbb07d9611cf6b79861b8a9ac793cf2ee182b4cb20365b",
+         intel: "7e42e9dbeb00ca8fa3667d22c870a807cac4d6312eb9c53e6b52d33a147e1f7b"
 
   url "https://github.com/tagspaces/tagspaces/releases/download/v#{version}/tagspaces-mac-#{arch}-#{version}.dmg",
       verified: "github.com/tagspaces/tagspaces/"
