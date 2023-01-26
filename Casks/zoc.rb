8b60f5a7016a57bc8eab0381c79d7a4598f49696
Patrick Linnane <patrick@linnane.io>
2023-01-25 16:06:22 -0800

    zoc 8.05.0
    Update zoc from 8.04.8 to 8.05.0
    
    Closes #140236.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/zoc.rb
+++ Casks/zoc.rb
@@ -1,6 +1,6 @@
 cask "zoc" do
-  version "8.04.8"
-  sha256 "d069246afa477574243b43e18352d72706274ccd40cec62ee32150d80a684e92"
+  version "8.05.0"
+  sha256 "737adec851ba0d4fd5e6a44d3918dee85b13ffa301da0ae30496ba4e8ca2ea44"
 
   url "https://www.emtec.com/downloads/zoc/zoc#{version.no_dots}.dmg"
   name "ZOC"
