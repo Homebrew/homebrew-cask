fa6025c1bc069ea5d8bcb3d8f565578918efc5e6
Patrick Linnane <patrick@linnane.io>
2023-01-26 11:55:59 -0800

    rive: adjust url & version

--- Casks/rive.rb
+++ Casks/rive.rb
@@ -1,8 +1,8 @@
 cask "rive" do
-  version "0.8.69"
+  version "0.8.69,56"
   sha256 "3119f98a43afe8ea1d697b8cc018fcc82859e54a94b58a10c17961879ec7ba89"
 
-  url "https://releases.rive.app/macos/#{version}/files/Rive.dmg"
+  url "https://releases.rive.app/macos/#{version.csv.first}/files/Rive.dmg"
   name "Rive"
   desc "Design tool that creates functional graphics"
   homepage "https://rive.app/"
