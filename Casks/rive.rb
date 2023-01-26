7832a31c4a22b1f472e105efe0af0a91d6f10f19
Bevan Kay <email@bevankay.me>
2023-01-27 00:04:11 +1100

    Update rive.rb

--- Casks/rive.rb
+++ Casks/rive.rb
@@ -1,11 +1,16 @@
 cask "rive" do
-  version :latest
-  sha256 :no_check
+  version "0.8.69"
+  sha256 "3119f98a43afe8ea1d697b8cc018fcc82859e54a94b58a10c17961879ec7ba89"
 
-  url "https://releases.rive.app/macos/#{version}/Rive.dmg"
+  url "https://releases.rive.app/macos/#{version}/files/Rive.dmg"
   name "Rive"
   desc "Design tool that creates functional graphics"
   homepage "https://rive.app/"
+
+  livecheck do
+    url "https://releases.rive.app/release.xml"
+    strategy :sparkle
+  end
 
   app "Rive.app"
 end
