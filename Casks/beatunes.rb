7a3da044f39e0c2042d4c78c1bad5dd943f4424a
Patrick Linnane <patrick@linnane.io>
2023-01-27 10:35:41 -0800

    beatunes 5.2.31
    Update beatunes from 5.2.30 to 5.2.31
    
    Closes #140290.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/beatunes.rb
+++ Casks/beatunes.rb
@@ -1,6 +1,6 @@
 cask "beatunes" do
-  version "5.2.30"
-  sha256 "cc81ba7f42ddf1c50ffa39c2c4c1bfa180f43837b7ebc950ae3cd33c32bf67d9"
+  version "5.2.31"
+  sha256 "6523b2788b79ecd5cdb684d8b3c8bea575863a0591e71d736f6bad37c8623559"
 
   url "https://coxy.beatunes.com/download/beaTunes-#{version.dots_to_hyphens}.dmg"
   name "beaTunes"
