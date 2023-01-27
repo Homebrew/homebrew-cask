1df17ebdc45d3b68061ff6377e15d8f5b0f6f772
Patrick Linnane <patrick@linnane.io>
2023-01-27 11:52:42 -0800

    tidelift 1.12.22
    Update tidelift from 1.12.20 to 1.12.22
    
    Closes #140363.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/tidelift.rb
+++ Casks/tidelift.rb
@@ -1,6 +1,6 @@
 cask "tidelift" do
-  version "1.12.20"
-  sha256 "c8e99d49f7f1522c6d479a44677ef8c63b14bb2a2508975021f882bb4674535c"
+  version "1.12.22"
+  sha256 "3534d0694fb932c365267cc2c63b3796fce184e3954f5e101a54362e8aa300cd"
 
   url "https://download.tidelift.com/cli/#{version}/darwin_homebrew/tidelift"
   name "Tidelift CLI"
