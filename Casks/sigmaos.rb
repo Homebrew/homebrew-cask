e5eb7d598f013a95036a8a4012dc6ce7c28bca2e
Patrick Linnane <patrick@linnane.io>
2023-01-27 11:48:07 -0800

    Update sigmaos from 1.2.1.1 to 1.2.1.4

--- Casks/sigmaos.rb
+++ Casks/sigmaos.rb
@@ -1,6 +1,6 @@
 cask "sigmaos" do
-  version "1.2.1.1"
-  sha256 "b4a51fc611adaecb8e5cfcb31a22f55cd700e949e53e5c2653ce85c186844bd9"
+  version "1.2.1.4"
+  sha256 "f22216f80c7909dbb97de59140ce795347dea3bb2b44efaa6522499333849155"
 
   url "https://releases.sigmaos.com/SigmaOS-#{version}.dmg"
   name "sigmaos"
