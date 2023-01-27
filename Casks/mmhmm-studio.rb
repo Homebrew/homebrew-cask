481a34fab0e3ac5418872a3a478db07fe72344d9
Patrick Linnane <patrick@linnane.io>
2023-01-27 12:15:20 -0800

    mmhmm-studio 2.4.1,1674533000
    Update mmhmm-studio from 2.4.0 to 2.4.1
    
    Closes #140375.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/mmhmm-studio.rb
+++ Casks/mmhmm-studio.rb
@@ -1,8 +1,8 @@
 cask "mmhmm-studio" do
-  version "2.4.0,1666031000"
-  sha256 "ff09794c189e0f1964c5f3cb49dd44a41f60dc722f1ab2868bf8040fe86028ee"
+  version "2.4.1,1674533000"
+  sha256 "489fd4681a96705eb17790f93bd75311e65f39891c6c5971639532ce37cdb3ef"
 
-  url "https://updates.mmhmm.app/mac/production/mmhmm_#{version.csv.first}.zip"
+  url "https://updates.mmhmm.app/mac/production/mmhmmStudio_#{version.csv.first}.zip"
   name "mmhmm Studio"
   desc "Virtual video presentation software"
   homepage "https://www.mmhmm.app/product"
