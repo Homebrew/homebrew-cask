1bb3802d0d134f63bce1c0107202510b8908458c
Patrick Linnane <patrick@linnane.io>
2023-01-27 11:05:49 -0800

    httpie 2023.1.2
    Update httpie from 2023.1.1 to 2023.1.2
    
    Closes #140321.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/httpie.rb
+++ Casks/httpie.rb
@@ -1,9 +1,9 @@
 cask "httpie" do
   arch arm: "-arm64"
 
-  version "2023.1.1"
-  sha256 arm:   "7ac19569025c69a21eec143dcb41a89d4e45d1822ff9b45621b9ea7c017d14bc",
-         intel: "1b8720345d14cc96accd1bf4c5a324855d3907267a6ea64ec84b212d6c80f302"
+  version "2023.1.2"
+  sha256 arm:   "1d9924a0c256fbbccff2e1f1ec0e0b826c4af5acc8184bd2fffb4e05f72a4f40",
+         intel: "23fcc2aff253aaf442b4083992db8be7d818d329be78db5c59fd19b9cf1c70f2"
 
   url "https://github.com/httpie/desktop/releases/download/v#{version}/HTTPie-#{version}#{arch}.dmg",
       verified: "github.com/httpie/desktop/"
