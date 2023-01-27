85ce28ca48f29e419125a9eb0a28dfc69d4e9721
Patrick Linnane <patrick@linnane.io>
2023-01-27 11:25:27 -0800

    Update pcoipclient from 23.01.0 to 23.01.1

--- Casks/pcoipclient.rb
+++ Casks/pcoipclient.rb
@@ -1,6 +1,6 @@
 cask "pcoipclient" do
-  version "23.01.0"
-  sha256 "1eb170c8dbd90d709eefe55c8a693c303664e46381548faa00ed67950219dc02"
+  version "23.01.1"
+  sha256 "60783be8de9ca022e442cc2a3112fa35754759e1c3a0883a28a8baf04397c329"
 
   url "https://dl.teradici.com/DeAdBCiUYInHcSTy/pcoip-client/raw/names/pcoip-client-dmg/versions/#{version}/pcoip-client_#{version}.dmg"
   name "Teradici PCoIP Software Client for macOS"
