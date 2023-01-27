dc292a0c5d9d88fd5e19b6aac86e32e5a43f4415
Patrick Linnane <patrick@linnane.io>
2023-01-27 11:02:11 -0800

    Update geph from 4.6.3 to 4.7.3

--- Casks/geph.rb
+++ Casks/geph.rb
@@ -1,6 +1,6 @@
 cask "geph" do
-  version "4.6.3"
-  sha256 "ff085b22525456ea09a726f3744dc71da6b41b414a5982a1d2b1c4fc8c26268b"
+  version "4.7.3"
+  sha256 "f79477804c4750296b6c2e0834e2aab4dcd3a52c5b5fae9a8c7dc7ba69ae2049"
 
   url "https://sos-ch-dk-2.exo.io/utopia/geph-releases/macos-stable/#{version}/geph-macos.dmg",
       verified: "sos-ch-dk-2.exo.io/utopia/geph-releases/"
