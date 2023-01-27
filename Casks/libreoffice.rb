cdbedb94acc1e251b7a4b85042dfaa26be8893ea
eekleefeld <87045020+eekleefeld@users.noreply.github.com>
2023-01-26 21:21:02 -0500

    Update libreoffice from 7.4.4 to 7.4.5 (#140245)

--- Casks/libreoffice.rb
+++ Casks/libreoffice.rb
@@ -2,9 +2,9 @@ cask "libreoffice" do
   arch arm: "aarch64", intel: "x86-64"
   folder = on_arch_conditional arm: "aarch64", intel: "x86_64"
 
-  version "7.4.4"
-  sha256 arm:   "a5f289664f54184c290ed64dd24f788bf9661cefd0c0607aad60bd5a3ccbbbb3",
-         intel: "edc6946cbf84c0a8e8f5f43f16f516ad9b1eeb14b71718ba4d5745b1c716f6fb"
+  version "7.4.5"
+  sha256 arm:   "4bc396e74ed4408b0eb69583f87a59121f69d4748ab671fcffb4f9c4b763b75f",
+         intel: "d16986c9891eec65785c828415160a186afa02f2ce8c4032752553a4a95e9f71"
 
   url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/#{folder}/LibreOffice_#{version}_MacOS_#{arch}.dmg",
       verified: "download.documentfoundation.org/libreoffice/stable/"
