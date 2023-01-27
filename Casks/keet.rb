813ab7e3f9ff5c4417022b5746640633e37a05cf
Patrick Linnane <patrick@linnane.io>
2023-01-27 11:13:18 -0800

    Update keet from 1.2.1 to 1.2.2

--- Casks/keet.rb
+++ Casks/keet.rb
@@ -1,7 +1,7 @@
 cask "keet" do
   arch arm: "Apple-Silicon", intel: "Intel"
 
-  version "1.2.1"
+  version "1.2.2"
   sha256  arm:   "1137a41339a483c2135ab2af37736635744d5ae78ddfc6e6d7a7b3f5cf086527",
           intel: "727f590dc1b768632dc5528ae7e5012f94388b6e2167a89417156c89bd3637d6"
 
