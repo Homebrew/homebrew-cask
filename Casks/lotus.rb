8a6880961bf90b8205e108fd0f2a3cdc0c71f6ac
Glenn 'devalias' Grant <glenn@devalias.net>
2023-01-28 16:28:04 +1100

    add Lotus 1.3.2

---
+++ Casks/lotus.rb
@@ -0,0 +1,11 @@
+cask "lotus" do
+  version "1.3.2"
+  sha256 :no_check
+
+  url "https://getlotus.app/download"
+  name "Lotus"
+  desc "Keep up with GitHub notifications without stress"
+  homepage "https://getlotus.app/"
+
+  app "Lotus.app"
+end
