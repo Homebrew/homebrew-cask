5f59480a79e591eaf00c1ca56a00281e7a645662
Miccal Matthews <miccal.matthews@gmail.com>
2023-01-28 17:01:27 +0800

    Update lotus.rb

--- Casks/lotus.rb
+++ Casks/lotus.rb
@@ -2,10 +2,18 @@ cask "lotus" do
   version "1.3.2"
   sha256 :no_check
 
-  url "https://getlotus.app/download"
+  url "https://app-updates.vadimdemedes.com/lotus/download/osx",
+      verified: "app-updates.vadimdemedes.com/lotus/download/osx"
   name "Lotus"
-  desc "Keep up with GitHub notifications without stress"
+  desc "Keep up with GitHub notifications"
   homepage "https://getlotus.app/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
+
+  depends_on macos: ">= :el_capitan"
 
   app "Lotus.app"
 end
