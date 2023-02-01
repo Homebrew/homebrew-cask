aae63dd9c5ac8432c912bd5c9279f61ea3a502b1
Piotr Myszka <pmyszka@users.noreply.github.com>
2023-02-01 11:26:57 +0100

    Update scidvsmac from 4.23 to 4.23.x64 (#140479)
    * Update scidvsmac from 4.23 to 4.23.x64
    
    * Update scidvsmac.rb
    
    ---------
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/scidvsmac.rb
+++ Casks/scidvsmac.rb
@@ -1,15 +1,15 @@
 cask "scidvsmac" do
   version "4.23"
-  sha256 "dd50dc3923375a61eebeb0b425aa16ef100bb7d49b5233504961ba8839681df3"
+  sha256 "01cc4aacd544bfaad7cd75519c5e7041909c2f3a06cd1bc272fe59b0dfafd530"
 
-  url "https://downloads.sourceforge.net/scidvspc/ScidvsMac-#{version}.dmg"
+  url "https://downloads.sourceforge.net/scidvspc/ScidvsMac-#{version}.x64.dmg"
   name "Scid vs. Mac"
   desc "Chess toolkit"
   homepage "https://scidvspc.sourceforge.net/"
 
   livecheck do
     url :homepage
-    regex(/ScidvsMac-(\d+(?:\.\d+)*)\.dmg/i)
+    regex(/ScidvsMac-(\d+(?:\.\d+)*)\.x64\.dmg/i)
   end
 
   app "ScidvsMac.app"
