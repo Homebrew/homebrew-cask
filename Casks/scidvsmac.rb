c250756eddf310cca09159f6c00a8064790c0533
Piotr Myszka <pmyszka@users.noreply.github.com>
2023-02-01 10:07:32 +0100

    Update scidvsmac from 4.23 to 4.23.x64

--- Casks/scidvsmac.rb
+++ Casks/scidvsmac.rb
@@ -1,6 +1,6 @@
 cask "scidvsmac" do
-  version "4.23"
-  sha256 "dd50dc3923375a61eebeb0b425aa16ef100bb7d49b5233504961ba8839681df3"
+  version "4.23.x64"
+  sha256 "01cc4aacd544bfaad7cd75519c5e7041909c2f3a06cd1bc272fe59b0dfafd530"
 
   url "https://downloads.sourceforge.net/scidvspc/ScidvsMac-#{version}.dmg"
   name "Scid vs. Mac"
