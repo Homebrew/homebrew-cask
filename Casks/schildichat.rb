06c6a4a3c3503cf61f3e691fe5b2895c831104e2
Jackson Chen <jackson@jacksonchen666.com>
2023-02-01 23:59:30 +0100

    Update schildichat from 1.11.13-sc.1 to 1.11.22-sc.1

--- Casks/schildichat.rb
+++ Casks/schildichat.rb
@@ -1,6 +1,6 @@
 cask "schildichat" do
-  version "1.11.13-sc.1"
-  sha256 "b256358c4f9c98764e694f7fc020e65d45a19ce2394b6b7903093bf8a8e1d417"
+  version "1.11.22-sc.1"
+  sha256 "09d8cbadde2ca5be0e9d06b80b4e7c3975c8bb2125db180eceb02f5efe99d692"
 
   url "https://github.com/SchildiChat/schildichat-desktop/releases/download/v#{version}/SchildiChat-#{version}-universal_by_nyantec.dmg",
       verified: "github.com/SchildiChat/schildichat-desktop/"
