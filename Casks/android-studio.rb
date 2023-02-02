bed4520b9080a6e52d8fb04f04d81c4d1fa94ae8
MoonFruit <dkmoonfruit@gmail.com>
2023-02-02 16:23:39 +0800

    Update android-studio from 2022.1.1.19 to 2022.1.1.20

--- Casks/android-studio.rb
+++ Casks/android-studio.rb
@@ -1,9 +1,9 @@
 cask "android-studio" do
   arch arm: "mac_arm", intel: "mac"
 
-  version "2022.1.1.19"
-  sha256 arm:   "78c450022fc1a85af90ae64fced4eafbc1ca4149e60bb14085f4f1503ff1cb36",
-         intel: "3e965f50d7312859b4ea11cf530ca1a6ba0bc476a285fe614a2b2f3cde1fc2d0"
+  version "2022.1.1.20"
+  sha256 arm:   "ebb8e842eda3d500adf1aece9eb5b9d9b5963016d0233b42ed12107b2cc952a9",
+         intel: "b7ee174891a5d72bf02e026455d992228446b80566dae7cafe1cdc9437a67e0e"
 
   url "https://redirector.gvt1.com/edgedl/android/studio/install/#{version}/android-studio-#{version}-#{arch}.dmg",
       verified: "redirector.gvt1.com/edgedl/android/studio/"
