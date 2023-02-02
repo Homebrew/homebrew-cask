cc05495e4c5151e0da2f5a88c4bf697923bf99c7
Joaquim Rocha <joaquim.rocha@microsoft.com>
2023-02-02 16:49:21 +0000

    Upgrade Headlamp version to 0.15.0 (#140522)
    Update Headlamp version to 0.15.0
    
    Signed-off-by: joaquimrocha <joaquimrocha@users.noreply.github.com>
    Co-authored-by: joaquimrocha <joaquimrocha@users.noreply.github.com>

--- Casks/headlamp.rb
+++ Casks/headlamp.rb
@@ -1,9 +1,9 @@
 cask "headlamp" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.14.1"
-  sha256 arm:   "2def838c3995e0df21dce245d880f325d3e4d9c11b0c82ea0753253f730dc665",
-         intel: "1923beef3b381ebe8fa77285fd63f41c26ea1a99f0286b579f9877d67444ab31"
+  version "0.15.0"
+  sha256 arm:   "83aaed8f87a4c6aba49a10c83888e8eff2b8eee9c0142a211978a9a2c5baf78b",
+         intel: "0478bad98890926c0face47ba7cbacf5422505998816fe787de9d9cc515dd6c5"
 
   url "https://github.com/kinvolk/headlamp/releases/download/v#{version}/Headlamp-#{version}-mac-#{arch}.dmg",
       verified: "github.com/kinvolk/headlamp/"
