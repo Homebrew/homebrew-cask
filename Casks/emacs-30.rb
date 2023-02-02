9ff3c142e5d4f4de03c10f709a7de3f34971db6e
Shripad Sinari <shripad.sinari@gmail.com>
2023-02-01 19:19:06 -0700

    Cask of emacs-30 from nightly build.
    Built from nightly build:
    "https://emacsformacosx.com/emacs-builds/
    Emacs-2023-01-13_00-09-27-
    f1310859a0d84ceb725c74bf37a2336c7d21153f-universal.dmg"

---
+++ Casks/emacs-30.rb
@@ -0,0 +1,25 @@
+cask "emacs-30" do
+  version "30.0.50"
+  sha256 :no_check
+
+  url "https://emacsformacosx.com/emacs-builds/Emacs-2023-01-13_00-09-27-f1310859a0d84ceb725c74bf37a2336c7d21153f-universal.dmg"
+  name "Emacs"
+  desc "GNU Emacs Lisp Machine"
+  homepage "https://emacsformacosx.com/"
+
+  app "Emacs.app"
+  binary "#{appdir}/Emacs.app/Contents/MacOS/Emacs", target: "emacs"
+  binary "#{appdir}/Emacs.app/Contents/MacOS/bin/ebrowse"
+  binary "#{appdir}/Emacs.app/Contents/MacOS/bin/emacsclient"
+  binary "#{appdir}/Emacs.app/Contents/MacOS/bin/etags"
+  manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/ebrowse.1.gz"
+  manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/emacs.1.gz"
+  manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/emacsclient.1.gz"
+  manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/etags.1.gz"
+
+  zap trash: [
+    "~/Library/Caches/org.gnu.Emacs",
+    "~/Library/Preferences/org.gnu.Emacs.plist",
+    "~/Library/Saved Application State/org.gnu.Emacs.savedState",
+  ]
+end
