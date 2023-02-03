84651aff0f15c9c5f824613fc57d1a7f1fd57b7b
Ian Hough <142520+ihough@users.noreply.github.com>
2023-02-03 13:40:47 +0100

    panoply 5.2.3: add arm

--- Casks/panoply.rb
+++ Casks/panoply.rb
@@ -1,15 +1,18 @@
 cask "panoply" do
-  version "5.2.3"
-  sha256 "b8cfdf1098104ccfef5e0980aefbb8ca3d080845cf602fc11cf44ac837cbc16a"
+  arch arm: "arm64-"
 
-  url "https://www.giss.nasa.gov/tools/panoply/download/PanoplyMacOS-#{version}.dmg"
+  version "5.2.3"
+  sha256 arm:   "3d461299692dae72b7be5f31de31b1a8854dc66bcb3774b0010be0c8b00fcae4",
+         intel: "b8cfdf1098104ccfef5e0980aefbb8ca3d080845cf602fc11cf44ac837cbc16a"
+
+  url "https://www.giss.nasa.gov/tools/panoply/download/PanoplyMacOS-#{arch}#{version}.dmg"
   name "Panoply netCDF, HDF and GRIB Data Viewer"
   desc "Plot geo-referenced data from netCDF, HDF, and GRIB"
   homepage "https://www.giss.nasa.gov/tools/panoply/"
 
   livecheck do
     url "https://www.giss.nasa.gov/tools/panoply/download/"
-    regex(/href=.*?PanoplyMacOS[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+    regex(/href=.*?PanoplyMacOS[._-]#{arch}v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
   app "Panoply.app"
