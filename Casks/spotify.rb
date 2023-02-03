87ed8c61e5875284036a6336a5ce159469781eff
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-03 16:25:08 +0800

    Update spotify.rb

--- Casks/spotify.rb
+++ Casks/spotify.rb
@@ -1,14 +1,8 @@
 cask "spotify" do
   arch arm: "ARM64"
 
+  version "1.2.4.912,949d5fd0,267"
   sha256 :no_check
-
-  on_intel do
-    version "1.2.4.912,949d5fd0,267"
-  end
-  on_arm do
-    version "1.2.4.912,949d5fd0,267"
-  end
 
   url "https://download.scdn.co/Spotify#{arch}.dmg",
       verified: "download.scdn.co/"
