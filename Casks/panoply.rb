7daa085bba9982c1347c4b8b22206e3cca30afd2
Ian Hough <142520+ihough@users.noreply.github.com>
2023-02-03 13:27:16 +0100

    panoply 5.2.3: add arm

--- Casks/panoply.rb
+++ Casks/panoply.rb
@@ -1,6 +1,9 @@
 cask "panoply" do
+  arch arm: "arm64-"
+
   version "5.2.3"
-  sha256 "b8cfdf1098104ccfef5e0980aefbb8ca3d080845cf602fc11cf44ac837cbc16a"
+  sha256 arm:   "3d461299692dae72b7be5f31de31b1a8854dc66bcb3774b0010be0c8b00fcae4",
+         intel: "b8cfdf1098104ccfef5e0980aefbb8ca3d080845cf602fc11cf44ac837cbc16a"
 
   url "https://www.giss.nasa.gov/tools/panoply/download/PanoplyMacOS-#{arch}#{version}.dmg"
   name "Panoply netCDF, HDF and GRIB Data Viewer"
