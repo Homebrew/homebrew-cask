c1ded4a2d3ebb977735309e6a4af31e652289064
Curudel <andreas.florin.1988@outlook.com>
2023-02-03 09:15:45 +0100

    Update spotify from 1.2.3.1115 to 1.2.4.912

--- Casks/spotify.rb
+++ Casks/spotify.rb
@@ -4,10 +4,10 @@ cask "spotify" do
   sha256 :no_check
 
   on_intel do
-    version "1.2.3.1115,d61a8f5c,384"
+    version "1.2.4.912,949d5fd0,267"
   end
   on_arm do
-    version "1.2.3.1115,d61a8f5c,384"
+    version "1.2.4.912,949d5fd0,267"
   end
 
   url "https://download.scdn.co/Spotify#{arch}.dmg",
