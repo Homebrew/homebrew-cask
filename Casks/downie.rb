77cb58db83aa80c32e0f55528817de56661d2975
riverliu <30144128+origamirabbit@users.noreply.github.com>
2023-02-04 12:34:43 +0800

    Update downie from 4.6.7,4515 to 4.6.7,4516 (#140574)
    Update downie from 4.6.7,4516 to 4.6.7,4516
    
    Co-authored-by: riverliu <>

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,6 +1,6 @@
 cask "downie" do
-  version "4.6.7,4515"
-  sha256 "4f0b6f2dc97b689086dd83b1b74a5035d2bc1a6e49eea1b57a34b1cf2af1e97f"
+  version "4.6.7,4516"
+  sha256 "e426b8ce51280bdd39ed764e45bc12d5f4a992fa693a9beca333494b7ef3cbcd"
 
   url "https://software.charliemonroe.net/trial/downie/v#{version.major}/Downie_#{version.major}_#{version.csv.second}.dmg"
   name "Downie"
