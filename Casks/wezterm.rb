1b47cbb8ed63031a7e737d2174e56e81b8054542
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-05 07:44:27 +0800

    Update wezterm.rb

--- Casks/wezterm.rb
+++ Casks/wezterm.rb
@@ -16,6 +16,8 @@ cask "wezterm" do
     end
   end
 
+  conflicts_with cask: "homebrew/cask-versions/wezterm-nightly"
+
   app "WezTerm-macos-#{version.csv.first}-#{version.csv.second}/WezTerm.app"
 
   %w[
@@ -25,6 +27,15 @@ cask "wezterm" do
     strip-ansi-escapes
   ].each do |tool|
     binary "#{appdir}/WezTerm.app/Contents/MacOS/#{tool}"
+  end
+
+  preflight do
+    # Move "WezTerm-macos-#{version}/WezTerm.app" out of the subfolder
+    staged_subfolder = staged_path.glob(["WezTerm-*", "wezterm-*"]).first
+    if staged_subfolder
+      FileUtils.mv(staged_subfolder/"WezTerm.app", staged_path)
+      FileUtils.rm_rf(staged_subfolder)
+    end
   end
 
   zap trash: [
