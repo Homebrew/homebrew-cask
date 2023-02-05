779746c155fb1a9bb9d88f7bb2bfc7a94be46582
Mentor Palokaj <mentor@palokaj.co>
2023-02-05 15:47:57 +0100

    battery 1.0.6
    * Update battery.rb
      GUI update, plus new discharge feature.
    
      CI will fail due to app being `arm64`.
    * Update battery.rb
      Hotfix hash change
    
    Closes #140618.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/battery.rb
+++ Casks/battery.rb
@@ -1,6 +1,6 @@
 cask "battery" do
-  version "1.0.5"
-  sha256 "875369e590ce9ec169f30f7edf3db509223e701af65db8e1a50cb43efae76632"
+  version "1.0.6"
+  sha256 "e8df0969017f17f26381b41b47e9af9d5d04f20b197744fa7112c60a9bd7a143"
 
   url "https://github.com/actuallymentor/battery/releases/download/v#{version}/battery-#{version}-arm64.dmg"
   name "Battery"
