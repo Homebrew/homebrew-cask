ea2865d79109f4c08de353507c7eff0624515149
logwet <logwet@users.noreply.github.com>
2023-02-05 21:48:28 +1100

    Revert to on_system blocks

--- Casks/jitouch.rb
+++ Casks/jitouch.rb
@@ -5,23 +5,27 @@ cask "jitouch" do
   
   depends_on macos: ">= :el_capitan"
   
-  if MacOS.version <= :mojave
-    if MacOS.version <= :el_capitan
+  on_big_sur :or_older do
+    on_el_capitan do
       version "2.63"
       sha256 :no_check
       url "https://www.jitouch.com/jitouch_el_capitan.zip"
-    elsif MacOS.version <= :sierra
+    end
+    on_high_sierra :or_older do
       version "2.71"
       sha256 :no_check
       url "https://www.jitouch.com/jitouch_sierra.zip"
-    else
+    end
+    on_big_sur :or_older do
       version "2.74-beta"
       sha256 :no_check
       url "https://www.jitouch.com/jitouch_mojave.zip"
     end
     
     prefpane "jitouch/Jitouch.prefPane"
-  else
+  end
+  
+  on_monterey :or_newer do
     version "2.82.1"
     sha256 "3f5194a4da6fe19d17c843fa8a876131f7878905dcbb2e1d740d34d286d740c4"
     url "https://github.com/JitouchApp/Jitouch/releases/download/v#{version}/Install-Jitouch.pkg", verified: "github.com/JitouchApp/Jitouch/"
