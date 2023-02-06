9a5b429e4463f1c5897326749327322e75446087
Ching Chow <ching@getbento.com>
2023-02-06 10:36:55 -0500

    update version to 1.26.1

--- Casks/tastyworks.rb
+++ Casks/tastyworks.rb
@@ -1,6 +1,6 @@
 cask "tastyworks" do
-  version "1.22.0"
-  sha256 "e7e56d8aed91594c6259f47fffb0326c3ca7bda2eb0ad2a10c487d819b11ee26"
+  version "1.26.1"
+  sha256 "6860c20aadb5a3c203260c5a1616cfec76849100b68fa6f26dd94e5edc24484b"
 
   url "https://download.tastyworks.com/desktop-#{version.major}.x.x/#{version}/tastyworks-#{version}.dmg"
   name "tastyworks"
