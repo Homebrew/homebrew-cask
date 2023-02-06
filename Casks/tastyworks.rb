630b3900d28fb7441679c01fa6460a03cca87363
Ching Chow <ching@getbento.com>
2023-02-06 10:37:52 -0500

    add apple silicon arch

--- Casks/tastyworks.rb
+++ Casks/tastyworks.rb
@@ -1,8 +1,11 @@
 cask "tastyworks" do
-  version "1.26.1"
-  sha256 "6860c20aadb5a3c203260c5a1616cfec76849100b68fa6f26dd94e5edc24484b"
+  arch arm: "-aarch64", intel: ""
 
-  url "https://download.tastyworks.com/desktop-#{version.major}.x.x/#{version}/tastyworks-#{version}.dmg"
+  version "1.26.1"
+  sha256 arm:   "2b5944cbfa2d4f6e52ffdf06c1c46bb208b4504fb69afe2007e8aaa1cc3a2d0e",
+         intel: "6860c20aadb5a3c203260c5a1616cfec76849100b68fa6f26dd94e5edc24484b"
+
+  url "https://download.tastyworks.com/desktop-#{version.major}.x.x/#{version}/tastyworks-#{version}#{arch}.dmg"
   name "tastyworks"
   desc "Desktop trading platform for the tastyworks brokerage"
   homepage "https://tastyworks.com/"
