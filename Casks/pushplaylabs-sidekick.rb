44b768da1dbfa5cd82eb413fed4e0c6bbf922ee1
Bevan Kay <email@bevankay.me>
2023-02-06 23:54:47 +1100

    Update pushplaylabs-sidekick from 108.35.1.29385 to 108.36.1.29778 (#140644)
    * Update pushplaylabs-sidekick from 108.35.1.29385 to 108.36.1.29778
    
    * pushplaylabs-sidekick: bump Intel
    
    ---------
    
    Co-authored-by: Patrick Linnane <patrick@linnane.io>

--- Casks/pushplaylabs-sidekick.rb
+++ Casks/pushplaylabs-sidekick.rb
@@ -3,12 +3,12 @@ cask "pushplaylabs-sidekick" do
   livecheck_folder = on_arch_conditional arm: "macm1", intel: "mac"
 
   on_intel do
-    version "108.35.1.29386,ec54390"
-    sha256 "b139771ffd47ffd4d2befd4ad83118bd616cd6635cbd777963750f9beeebc6bf"
+    version "108.36.1.29779,5a26b25"
+    sha256 "c0f8f2d736019d563dd3ad454cb0b534837995fb645578fddc1977741256e658"
   end
   on_arm do
-    version "108.35.1.29385,517f21b"
-    sha256 "1f4c738576741c9aad21468c1731f28434d2484f6cb8050d9c36564d36d75e02"
+    version "108.36.1.29778,33cdb26"
+    sha256 "a3f791faafa54f307097aa4381f3566a2682e341a9a53107c85699ad6b66e31a"
   end
 
   url "https://cdn.meetsidekick.com/browser-builds/sidekick-mac-release-#{arch}-#{version.csv.first}-#{version.csv.second}-df.dmg"
