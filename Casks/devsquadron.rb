4232a8bdfdee89756c91f850bb84a81864584ab6
Matthew Garelli <mrgarelli@gmail.com>
2023-02-06 22:45:38 -0800

    move devsquadron static to it's own server & bump to v0.0.6 (#140686)
    move devsquadron static to it's own server
    bump to version 0.0.6

--- Casks/devsquadron.rb
+++ Casks/devsquadron.rb
@@ -1,17 +1,17 @@
 cask "devsquadron" do
   arch arm: "arm", intel: "intel"
 
-  version "0.0.5"
-  sha256 arm:   "5a7ca7f2e3fd1ea7a4bc35e5564d5d1d95594fec1415454295f7c55ab0ee9610",
-         intel: "a5ca536c26ad93d465d7192039cca1133845bd78400bf129fbb770516232e015"
+  version "0.0.6"
+  sha256 arm:   "dac7c726bf8970fd164d08f6723b8b477a7f03feaa5bb804c1e4ae6e552a3bbf",
+         intel: "38583faa0d40420ed50b4f9b880c79e2e4ba57837c6f1f13334618a70eeb87c4"
 
-  url "https://developersquadron.com/downloads/devsquadron/v#{version}/macos-#{arch}.tar.xz"
+  url "https://developersquadron.serveftp.com/downloads/devsquadron/v#{version}/macos-#{arch}.tar.xz"
   name "devsquadron"
   desc "Project management for power users and users"
-  homepage "https://developersquadron.com/"
+  homepage "https://developersquadron.serveftp.com/"
 
   livecheck do
-    url "https://developersquadron.com/downloads/devsquadron/"
+    url "https://developersquadron.serveftp.com/downloads/devsquadron/"
     regex(/v?(\d+(?:\.\d+)+)/i)
   end
 
