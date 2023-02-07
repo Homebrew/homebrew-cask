c253e69d436ee860166ffc657509f979a1be134d
zhangyulai <zyl@zhangyl.com>
2023-02-07 10:15:37 +0800

    Update the config of input-source-pro
    update the format of the version

--- Casks/input-source-pro.rb
+++ Casks/input-source-pro.rb
@@ -1,8 +1,8 @@
 cask "input-source-pro" do
-  version "2.1.15-beta,380"
+  version "2.1.15-beta"
   sha256 "a080357ee16f2b7289bd5079bf65a50093698e8832f917901cc56485696e0da7"
 
-  url "https://inputsource.pro/beta/Input%20Source%20Pro%20#{version.csv.first}.dmg"
+  url "https://inputsource.pro/beta/Input%20Source%20Pro%20#{version}.dmg"
   name "Input Source Pro"
   desc "Tool for multi-language users"
   homepage "https://inputsource.pro/"
