80b7cb350e14069251cf571733236d4045df7423
velayudhann <61645946+velayudhann@users.noreply.github.com>
2023-02-08 21:09:40 +0530

    Update chromedriver.rb
    Updating chromedriver to latest version 110.0.5481.77 from 109.0.5414.74

--- Casks/chromedriver.rb
+++ Casks/chromedriver.rb
@@ -1,7 +1,7 @@
 cask "chromedriver" do
   arch arm: "mac_arm64", intel: "mac64"
 
-  version "109.0.5414.74"
+  version "110.0.5481.77"
   sha256 arm:   "604df6f926218a267b3f42a8f9b945d510df6a5821e39772a48c6046da14ab2a",
          intel: "a0dad15fab5c00f8b09d8a2b04eddb8915b3457b5c5aa77177399e5a40eb8670"
 
