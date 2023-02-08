e0fd3d67a21262509b81cef47a4d20926242c21a
Zaher Ghaibeh <zaher@zaher.dev>
2023-02-08 20:24:55 +0300

    fix style

--- Casks/coherence.rb
+++ Casks/coherence.rb
@@ -1,6 +1,6 @@
 cask "coherence" do
-  version "4.1"
-  sha256 "e809c115d7ebc3da9c57162934a271984f9dbb3e4dd77d3ee6e30c5219a1d469"
+  version "4.3"
+  sha256 "7e0f6cab169afc54fe701e2a4301024d5c4cece90aeacd50bf954694af65d0fc"
 
   url "https://bzgdownloads.s3.amazonaws.com/Coherence/Coherence+X+#{version}.zip",
       verified: "bzgdownloads.s3.amazonaws.com/Coherence/"
@@ -19,8 +19,8 @@ cask "coherence" do
   app "Coherence X.app"
 
   zap trash: [
-        "~/Library/Application Support/Coherence X",
-        "~/Library/Application Support/ccom.BZG.CoherenceX4",
-        "~/Library/Preferences/com.BZG.CoherenceX4.*",
-      ]
+    "~/Library/Application Support/Coherence X",
+    "~/Library/Application Support/ccom.BZG.CoherenceX4",
+    "~/Library/Preferences/com.BZG.CoherenceX4.*",
+  ]
 end
