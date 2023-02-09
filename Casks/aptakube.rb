2f73e5e364397de9d8edd837c1665cec3c5f7ee5
goenning <me@goenning.net>
2023-02-09 16:40:08 +0000

    Update aptakube from 1.1.0 to 1.2.0

--- Casks/aptakube.rb
+++ Casks/aptakube.rb
@@ -1,6 +1,6 @@
 cask "aptakube" do
-  version "1.1.0"
-  sha256 "cfdf7a8e12d50cc9e4872b57c8a32083324fcbeb67f6be384a710af3c79aff44"
+  version "1.2.0"
+  sha256 "e8cd9559be687035f6d270231f5d232c7fb0abea6eb98fe3296f0b8d31d734ca"
 
   url "https://releases.aptakube.com/Aptakube_#{version}_universal.dmg"
   name "Aptakube"
