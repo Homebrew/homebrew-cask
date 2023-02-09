fd1c5bf1039b300719831fd80e9d0fb23817c7ac
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-09 10:10:38 +0800

    Update website-watchman.rb

--- Casks/website-watchman.rb
+++ Casks/website-watchman.rb
@@ -4,11 +4,12 @@ cask "website-watchman" do
 
   url "https://peacockmedia.software/mac/watchman/watchman.dmg"
   name "Website Watchman"
+  desc "Monitor a whole website, part of a website or a single page"
   homepage "https://peacockmedia.software/mac/watchman/"
 
   livecheck do
     url :homepage
-    regex(/Version\s+(\d+(?:\.\d+)*)\s*released/i)
+    regex(/Version\s+(\d+(?:\.\d+)+)\s*released/i)
   end
 
   app "Website Watchman.app"
