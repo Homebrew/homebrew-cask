318e3af308a1b5d5bc8cf98184c6288b129cc33b
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-09 10:08:48 +0800

    Update securityspy.rb

--- Casks/securityspy.rb
+++ Casks/securityspy.rb
@@ -4,11 +4,12 @@ cask "securityspy" do
 
   url "https://www.bensoftware.com/securityspy/SecuritySpy.dmg"
   name "SecuritySpy"
+  desc "Multi-camera CCTV software"
   homepage "https://www.bensoftware.com/securityspy/"
 
   livecheck do
     url "https://www.bensoftware.com/securityspy/versionhistory.html"
-    regex(/Version\s+(\d+(?:\.\d+)*)/i)
+    regex(/Version\s+(\d+(?:\.\d+)+)/i)
   end
 
   depends_on macos: ">= :high_sierra"
