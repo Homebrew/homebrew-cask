e189b454ef21332e8365da64db79700d74b6cfa2
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-09 10:06:02 +0800

    Update pdfelement-express.rb

--- Casks/pdfelement-express.rb
+++ Casks/pdfelement-express.rb
@@ -4,11 +4,12 @@ cask "pdfelement-express" do
 
   url "https://download.wondershare.com/cbs_down/mac-pdfelement-express_full4133.dmg"
   name "PDFelement Express"
+  desc "PDF editor"
   homepage "https://pdf.wondershare.com/pdfelement-express-mac.html"
 
   livecheck do
     url "https://cbs.wondershare.com/go.php?m=upgrade_info&pid=4133"
-    regex(/<Version>(\d+(?:\.\d+)*)</)
+    regex(/<Version>(\d+(?:\.\d+)+)</)
   end
 
   depends_on macos: ">= :sierra"
