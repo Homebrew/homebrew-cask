77057529d581d0a0163bee5f97c01bf6f1052c52
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-09 10:01:32 +0800

    Update mathtype.rb

--- Casks/mathtype.rb
+++ Casks/mathtype.rb
@@ -4,11 +4,12 @@ cask "mathtype" do
 
   url "https://store.wiris.com/en/products/downloads/mathtype/installer/mac/en"
   name "MathType"
+  desc "Write math equations everywhere"
   homepage "https://www.wiris.com/en/mathtype"
 
   livecheck do
     url "https://docs.wiris.com/en/mathtype/release_notes/start"
-    regex(/MathType\s+editor\s+v(\d+(?:\.\d+)*)/i)
+    regex(/MathType\s+editor\s+v(\d+(?:\.\d+)+)/i)
   end
 
   installer manual: "MathType-mac-en.pkg"
