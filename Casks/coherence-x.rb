e7272be3446f44401f29b9ba9447962fea6c8567
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-09 08:47:33 +0800

    Update and rename coherence.rb to coherence-x.rb

--- Casks/coherence-x.rb
+++ Casks/coherence-x.rb
@@ -1,10 +1,10 @@
-cask "coherence" do
+cask "coherence-x" do
   version "4.3"
   sha256 "7e0f6cab169afc54fe701e2a4301024d5c4cece90aeacd50bf954694af65d0fc"
 
   url "https://bzgdownloads.s3.amazonaws.com/Coherence/Coherence+X+#{version}.zip",
       verified: "bzgdownloads.s3.amazonaws.com/Coherence/"
-  name "Coherence"
+  name "Coherence X"
   desc "Turn websites into apps"
   homepage "https://bzgapps.com/coherence"
 
@@ -13,14 +13,13 @@ cask "coherence" do
     strategy :sparkle
   end
 
-  auto_updates false
   depends_on macos: ">= :high_sierra"
 
   app "Coherence X.app"
 
   zap trash: [
-    "~/Library/Application Support/Coherence X",
     "~/Library/Application Support/ccom.BZG.CoherenceX*",
+    "~/Library/Application Support/Coherence X",
     "~/Library/Preferences/com.BZG.CoherenceX*",
   ]
 end
