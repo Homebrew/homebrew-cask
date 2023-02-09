536288a3eeb74454f55dcb409a054447265182db
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-09 10:08:18 +0800

    Update red-eye.rb

--- Casks/red-eye.rb
+++ Casks/red-eye.rb
@@ -4,11 +4,12 @@ cask "red-eye" do
 
   url "https://www.hexedbits.com/downloads/redeye.zip"
   name "Red Eye"
+  desc "Tool to prevent the system from going into sleep mode""
   homepage "https://www.hexedbits.com/redeye/"
 
   livecheck do
     url :homepage
-    regex(/Version\s+(\d+(?:\.\d+)*)/i)
+    regex(/Version\s+(\d+(?:\.\d+)+)/i)
   end
 
   depends_on macos: ">= :mojave"
