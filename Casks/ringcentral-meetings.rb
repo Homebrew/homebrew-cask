cd76b4e35128f0007c1f1fde60abb27156d1e1a4
Markus Reiter <me@reitermark.us>
2023-02-10 22:18:18 +0100

    Update ringcentral-meetings from latest to 21.1.53852.0426 (#140854)
    * Update ringcentral-meetings from latest to 21.1.53852.0426
    
    * Update ringcentral-meetings.rb

--- Casks/ringcentral-meetings.rb
+++ Casks/ringcentral-meetings.rb
@@ -1,11 +1,16 @@
 cask "ringcentral-meetings" do
-  version :latest
+  version "21.1.53852.0426"
   sha256 :no_check
 
   url "https://dn.ringcentral.com/data/web/download/RCMeetings/1210/RCMeetingsClientSetup.pkg"
   name "RingCentral Meetings"
   desc "Video conferencing, screen sharing, and team messaging platform"
   homepage "https://www.ringcentral.com/online-meetings/overview.html"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   pkg "RCMeetingsClientSetup.pkg"
 
