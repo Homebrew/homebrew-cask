afb194438aebd69b78293de0e4c029e11617544b
Markus Reiter <me@reitermark.us>
2023-02-10 02:28:14 +0100

    Update flickr-uploadr from 1.1.2 to 1.2 (#140819)
    * Update flickr-uploadr from 1.1.2 to 1.2
    
    * Update flickr-uploadr.rb
    
    * Update flickr-uploadr.rb

--- Casks/flickr-uploadr.rb
+++ Casks/flickr-uploadr.rb
@@ -1,10 +1,16 @@
 cask "flickr-uploadr" do
-  version "1.1.2,2121"
+  version "1.2,2142"
   sha256 :no_check
 
   url "https://downloads.flickr.com/uploadr/FlickrUploadr.dmg"
   name "Flickr Uploadr"
+  desc "Photo upload tool"
   homepage "https://www.flickr.com/tools/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   app "Flickr Uploadr.app"
 end
