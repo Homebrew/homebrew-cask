64ab3f16dedf7c8e3de95147c48eba9dac9bf8e7
Markus Reiter <me@reitermark.us>
2023-02-10 15:30:17 +0100

    Update encryptr.rb

--- Casks/encryptr.rb
+++ Casks/encryptr.rb
@@ -2,7 +2,8 @@ cask "encryptr" do
   version "2.1.0"
   sha256 :no_check
 
-  url "https://spideroak.com/release/Encryptr/osx"
+  url "https://spideroak.com/release/Encryptr/osx",
+      verified: "https://spideroak.com/"
   name "SpiderOak Encryptr"
   desc "Zero-knowledge cloud-based password manager"
   homepage "https://spideroak.support/hc/en-us/categories/115000424503-Encryptr-Password-Manager"
