a2279318c0859c55405dcc4d5cf2696d9317fe6d
Markus Reiter <me@reitermark.us>
2023-02-10 15:03:27 +0100

    Update foldit.rb

--- Casks/foldit.rb
+++ Casks/foldit.rb
@@ -4,7 +4,13 @@ cask "foldit" do
 
   url "https://fold.it/portal/files/app/Foldit-macos_x64.dmg"
   name "Foldit"
+  desc "Crowdsourcing computer game"
   homepage "https://fold.it/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   depends_on macos: ">= :sierra"
 
