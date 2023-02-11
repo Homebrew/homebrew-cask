b3e23bb6e68106ceb25326281ac9479acb73ddbd
Patrick Linnane <patrick@linnane.io>
2023-02-12 00:33:39 +0100

    Update eqmac from 1.7.2 to 1.7.5 (#140990)

--- Casks/eqmac.rb
+++ Casks/eqmac.rb
@@ -1,6 +1,6 @@
 cask "eqmac" do
-  version "1.7.2"
-  sha256 "14eb1ba62b3ff32a5ce7abbaf393a59b643bd1db608244668bbc355df50a86da"
+  version "1.7.5"
+  sha256 "ae53a382129837d5d51931312d81a40847e857b53040ec90ae177fe0ee717e45"
 
   url "https://github.com/bitgapp/eqMac/releases/download/v#{version}/eqMac.dmg",
       verified: "github.com/bitgapp/eqMac/"
