d7e3ec282a18b9f2be81a430cb7d9a2ba1503a56
zuisong <19145952+zuisong@users.noreply.github.com>
2023-02-11 22:20:56 +0800

    Update dbgate from 5.2.1 to 5.2.2 (#140887)

--- Casks/dbgate.rb
+++ Casks/dbgate.rb
@@ -1,6 +1,6 @@
 cask "dbgate" do
-  version "5.2.1"
-  sha256 "f059bbdc52ff540f26495de85245272013ed046d9288cb61a99f6f3816897f36"
+  version "5.2.2"
+  sha256 "131c4c73ae9599ea543042e5e72d6aacabcb1da833ab1c4d4d5a2991a1854e5b"
 
   url "https://github.com/dbgate/dbgate/releases/download/v#{version}/dbgate-#{version}-mac_universal.dmg",
       verified: "github.com/dbgate/dbgate/"
