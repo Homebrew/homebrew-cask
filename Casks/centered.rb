c10fbada4ea7d6b66f1215e6876b47e82f77a534
Patrick Linnane <patrick@linnane.io>
2023-02-12 00:35:45 +0100

    Update centered from 5.8.0 to 5.9.0 (#140947)

--- Casks/centered.rb
+++ Casks/centered.rb
@@ -1,6 +1,6 @@
 cask "centered" do
-  version "5.8.0"
-  sha256 "e2c05ce12ce9671d174e773843937ae135a7a159a47f51878f2de0a230a23359"
+  version "5.9.0"
+  sha256 "2be7976d35fd27b8938982abe0a3711db0e440fba7488ba6184e8f1753cac1d4"
 
   url "https://d3ni5jxar6n4fz.cloudfront.net/Centered-#{version}-universal.dmg",
       verified: "d3ni5jxar6n4fz.cloudfront.net/"
