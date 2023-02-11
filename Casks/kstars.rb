a6d98270909b3ec77dca659ef1ffca0110370d64
Patrick Linnane <patrick@linnane.io>
2023-02-12 00:31:04 +0100

    Update kstars from 3.6.2 to 3.6.3 (#141044)

--- Casks/kstars.rb
+++ Casks/kstars.rb
@@ -1,6 +1,6 @@
 cask "kstars" do
-  version "3.6.2"
-  sha256 "70c52ac585bd43c5967cb072150c3638110421acab72e5826c8fb63a9dca5ffc"
+  version "3.6.3"
+  sha256 "f4d66bbdb09ebb673c9ffbba4940bbd82ee0eb299c86b5cfbd49049406591ace"
 
   url "https://www.indilib.org/jdownloads/kstars/kstars-#{version}.dmg",
       verified: "indilib.org/jdownloads/kstars/"
