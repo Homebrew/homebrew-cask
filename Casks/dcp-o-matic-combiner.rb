7405df14442282d5f7e023dad71365bb3979ab5a
Patrick Linnane <patrick@linnane.io>
2023-02-12 00:34:57 +0100

    Update dcp-o-matic-combiner from 2.16.41 to 2.16.42 (#140964)

--- Casks/dcp-o-matic-combiner.rb
+++ Casks/dcp-o-matic-combiner.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-combiner" do
-  version "2.16.41"
-  sha256 "bbc087c265d4ddaa85b78b7ba2fc0853bde3a8770deb1622e5a0a8ef2c9cc50f"
+  version "2.16.42"
+  sha256 "38eace96f89c2e3b4a7302365ad6bb271639817d69fafd52295084a6729dc17d"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-combiner&version=#{version}"
   name "dcp-o-matic-combiner"
