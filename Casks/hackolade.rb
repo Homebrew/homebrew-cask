4704a408a6795e9f06ef8e547f89e77363020076
Patrick Linnane <patrick@linnane.io>
2023-02-11 11:03:33 -0800

    Update hackolade from 6.8.5 to 6.9.0

--- Casks/hackolade.rb
+++ Casks/hackolade.rb
@@ -1,7 +1,7 @@
 cask "hackolade" do
   arch arm: "ARM64"
 
-  version "6.8.5"
+  version "6.9.0"
   sha256 :no_check
 
   url "https://s3-eu-west-1.amazonaws.com/hackolade/current/Hackolade-mac#{arch}-setup-signed.pkg",
