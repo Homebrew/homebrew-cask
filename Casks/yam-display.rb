e935266065e56fcefded7439481e6aa7e9519f82
Patrick Linnane <patrick@linnane.io>
2023-02-12 11:35:59 -0800

    Update yam-display from 2.4 to 2.4.3

--- Casks/yam-display.rb
+++ Casks/yam-display.rb
@@ -1,6 +1,6 @@
 cask "yam-display" do
-  version "2.4"
-  sha256 "a8a316bca01e47efeadae15309237c519427a110f5cba49d16b28432b4878af1"
+  version "2.4.3"
+  sha256 "d65d2711ab57e01866f19fc89b4a6a448a30bec2bf309d6b66138778ccf1fea0"
 
   url "https://www.yamdisplay.com/app/YamDisplay#{version}.zip"
   name "Yam Display"
