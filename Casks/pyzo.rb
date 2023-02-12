08ce4e38e28fe4806ca1ea4509c8d1415a74e91a
Patrick Linnane <patrick@linnane.io>
2023-02-11 17:47:41 -0800

    Update pyzo from 4.12.5 to 4.12.7

--- Casks/pyzo.rb
+++ Casks/pyzo.rb
@@ -1,6 +1,6 @@
 cask "pyzo" do
-  version "4.12.5"
-  sha256 "cfb07ed9c6fc9fcc3dad70dc29b33297cd78508c46987e444c5a82f31d74c5b2"
+  version "4.12.7"
+  sha256 "2bf0c360bda66e542a808a8d2e046d1b149099bd3577d096d9437ece177f84c8"
 
   url "https://github.com/pyzo/pyzo/releases/download/v#{version}/pyzo-#{version}-macos_x86_64.dmg",
       verified: "github.com/pyzo/pyzo/"
