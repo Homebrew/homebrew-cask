134c94e7e4e6f4ebb31c4eceed5918b29f8f67ba
Patrick Linnane <patrick@linnane.io>
2023-02-11 17:15:34 -0800

    Update cloudapp from 6.6.10 to 6.6.13

--- Casks/cloudapp.rb
+++ Casks/cloudapp.rb
@@ -1,6 +1,6 @@
 cask "cloudapp" do
-  version "6.6.10,2355"
-  sha256 "ec14314e336c562e66e106dde8271b8290e3f3ceff9ce34990a9548facac0597"
+  version "6.6.13,2389"
+  sha256 "70cfa9f62518cff63bcf8adba49073f175710ab34255d57b8000df74e891c669"
 
   url "https://downloads.getcloudapp.com/mac/CloudApp-#{version.csv.first}.#{version.csv.last}.zip"
   name "CloudApp"
@@ -9,7 +9,12 @@ cask "cloudapp" do
 
   livecheck do
     url "https://d2plwz9jdz9z5d.cloudfront.net/mac/latest/appcast.xml"
-    strategy :sparkle
+    strategy :sparkle do |item|
+      match = item.url.match(%r{/CloudApp[._-](\d+(?:\.\d+)+)\.(\d+)\.zip}i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
   end
 
   auto_updates true
