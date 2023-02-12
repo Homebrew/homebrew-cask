a01d479a3e8c613e7645fb1173c3a8a127ccbf85
Patrick Linnane <patrick@linnane.io>
2023-02-12 14:24:14 -0800

    Update timeular from 5.7.6 to 5.7.7 (#141230)

--- Casks/timeular.rb
+++ Casks/timeular.rb
@@ -1,6 +1,6 @@
 cask "timeular" do
-  version "5.7.6"
-  sha256 "0fef0a5b3d0c9a540cac0514a3dfaf94515c96fb12a8f1bf110bb77fc41d1894"
+  version "5.7.7"
+  sha256 "767b4b6a53c53a7c2e9168699e3bee4f4f59b6000a9105486d7f5c27f16cca1b"
 
   url "https://timeular-desktop-packages.s3.amazonaws.com/mac/production/Timeular-#{version}.dmg",
       verified: "timeular-desktop-packages.s3.amazonaws.com/"
