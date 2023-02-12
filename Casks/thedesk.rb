e6a35dd14f4ca21470cd16c8b6e8f51ae0e5256f
Patrick Linnane <patrick@linnane.io>
2023-02-12 14:24:23 -0800

    Update thedesk from 24.0.7 to 24.0.8 (#141227)

--- Casks/thedesk.rb
+++ Casks/thedesk.rb
@@ -1,6 +1,6 @@
 cask "thedesk" do
-  version "24.0.7"
-  sha256 "f8f4edbe6e0e5fbf57ab4d7371ef334d717110dc3a96c5bb71fbd75cec9878d4"
+  version "24.0.8"
+  sha256 "be9544a4e39aa41241976c97c56a6a9d9ee8afb89ee02c98016b2861c5773d50"
 
   url "https://github.com/cutls/TheDesk/releases/download/v#{version}/TheDesk-#{version}-universal.dmg",
       verified: "github.com/cutls/TheDesk/"
