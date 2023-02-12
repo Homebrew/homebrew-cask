92b032399231ff90b1a7df6a27f87253ea7fb7c4
Patrick Linnane <patrick@linnane.io>
2023-02-12 10:34:33 -0800

    Update tinkerwell from 3.17.0 to 3.18.0

--- Casks/tinkerwell.rb
+++ Casks/tinkerwell.rb
@@ -1,9 +1,9 @@
 cask "tinkerwell" do
   arch arm: "-arm64"
 
-  version "3.17.0"
-  sha256 arm:   "4c1559a41972190b128808b249c068665c445d3e91c392c8f625d41b00fc69aa",
-         intel: "9e276448577e6164c936c75f4b956d08826e5cf0ea1a370c450427c381b6e13d"
+  version "3.18.0"
+  sha256 arm:   "8adfb23eed966035c57e5063f6a8cd46dd676c028663c5fcbb5ce799d1d16465",
+         intel: "4a0963d27e963a1368a54332c8f9c16fc519762fe81d523a625f2f767c0454bd"
 
   url "https://download.tinkerwell.app/tinkerwell/Tinkerwell-#{version}#{arch}.dmg"
   name "Tinkerwell"
