21f0637ad8d0c886575fed2fb5ba94c6fcdb73df
zhangyulai <zyl@zhangyl.com>
2023-02-12 20:25:03 +0800

    Update menubarx.rb
    update the format of the version

--- Casks/menubarx.rb
+++ Casks/menubarx.rb
@@ -1,8 +1,8 @@
 cask "menubarx" do
-  version "1.5.7,163"
+  version "1.5.7"
   sha256 "5442f6bbc450cd2aa5d5921c7f7e4d7f07354f7e332a85405473c38c3b15601d"
 
-  url "https://menubarx-1251679148.file.myqcloud.com/download/MenubarX-#{version.csv.first}.dmg",
+  url "https://menubarx-1251679148.file.myqcloud.com/download/MenubarX-#{version}.dmg",
       verified: "menubarx-1251679148.file.myqcloud.com/download/"
   name "MenubarX"
   desc "Menu bar browser"
@@ -10,7 +10,7 @@ cask "menubarx" do
 
   livecheck do
     url "https://menubarx.app/appcast.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   auto_updates true
