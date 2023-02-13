695f0be00342ae7e17612b05de01d99038dcad50
Kilian Valkhof <kilian@kilianvalkhof.com>
2023-02-13 16:28:46 +0100

    Update polypane from 13.0.1 to 13.0.2

--- Casks/polypane.rb
+++ Casks/polypane.rb
@@ -1,9 +1,9 @@
 cask "polypane" do
   arch arm: "-arm64"
 
-  version "13.0.1"
-  sha256 arm:   "4d9f662741fa5c8cf194d0777f50c008793c3fb8c0f1f881a90b5339d633e1d6",
-         intel: "ac1a5459021c220ed3fd9529bb94a583ebdae1c8de279e799d1012490b9dd543"
+  version "13.0.2"
+  sha256 arm:   "ed037682617ced62d164ad738f17e4d00efd360171cf68ac0df746e0efcdc78c",
+         intel: "5aca950060f79a865229fe865eacd8c4f4d6853ef831a8058877d64d6f512606"
 
   url "https://github.com/firstversionist/polypane/releases/download/v#{version}/Polypane-#{version}#{arch}.dmg",
       verified: "github.com/firstversionist/polypane/"
