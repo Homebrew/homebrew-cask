57dd5d292696131cbddac3525d5de8a51b63f538
Olle Bröms <olle.broms@ewebbyran.se>
2023-02-13 14:56:47 +0100

    fix: gsub to tr and container nested

--- Casks/ytmdesktop-youtube-music.rb
+++ Casks/ytmdesktop-youtube-music.rb
@@ -2,7 +2,7 @@ cask "ytmdesktop-youtube-music" do
   version "1.14.2"
   sha256 "d7fb0b2dbe54469b39fc1c2daf0f17e65b0d84d4a2c171998c41c7691a378f0d"
 
-  url "https://github.com/ytmdesktop/ytmdesktop/releases/download/#{version}/ytm-desktop_macos-#{version.gsub(".", "_")}.zip",
+  url "https://github.com/ytmdesktop/ytmdesktop/releases/download/#{version}/ytm-desktop_macos-#{version.tr(".", "_")}.zip",
       verified: "github.com/ytmdesktop/"
   name "YouTube Music Desktop App"
   desc "YouTube music client"
@@ -12,6 +12,8 @@ cask "ytmdesktop-youtube-music" do
     url :url
     strategy :github_latest
   end
+
+  container nested: "YouTube Music Desktop App-#{version}.dmg"
 
   app "YouTube Music Desktop App.app"
 
