ef48abfef9dde2ca23f078df532220322138749e
vitahlin <linw1225@gmail.com>
2023-02-13 21:27:22 +0800

    Update picgo from 2.3.1 to 2.4.0-beta.0

--- Casks/picgo.rb
+++ Casks/picgo.rb
@@ -1,9 +1,9 @@
 cask "picgo" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.3.1"
-  sha256 arm:   "a2216b8572565a099d8d66b3d425a94e64de3b8b3ecfb7dc2d84f6b2814aa3bb",
-         intel: "6522bad65f0abce9e5ce0f5208244ce73660c51a299189aafd754431c03960e3"
+  version "2.4.0-beta.0"
+  sha256 arm:   "0f13da1021a66078490858a89946d7ea499967dd14ac7d281d75a81c1b4740b7",
+         intel: "fff73e1eb9d2bec6b8ff7435cfa8aa024126301bb6a566b58680c819bbef4ed8"
 
   url "https://github.com/Molunerfinn/PicGo/releases/download/v#{version}/PicGo-#{version}-#{arch}.dmg"
   name "PicGo"
