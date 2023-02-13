f1958a08f13fd4a51a24a62c6efe3c26475f6d1f
Patrick Linnane <patrick@linnane.io>
2023-02-13 12:15:41 -0800

    Update youdaonote from 7.2.1 to 7.2.3

--- Casks/youdaonote.rb
+++ Casks/youdaonote.rb
@@ -1,6 +1,6 @@
 cask "youdaonote" do
-  version "7.2.1"
-  sha256 "ca10a4efa806c3c06d03e0ada76f198447c74ab34d41183a3ce075d16ce0a723"
+  version "7.2.3"
+  sha256 "baa9de1426c1648fe8bf232851198c2004fae409e0eb9b750355ffbc25bc97b7"
 
   url "https://artifact.lx.netease.com/download/ynote-electron/%E6%9C%89%E9%81%93%E4%BA%91%E7%AC%94%E8%AE%B0-#{version}.zip",
       user_agent: :fake,
