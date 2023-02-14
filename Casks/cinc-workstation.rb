086b38367e22b23987a7f28031ae445bb5a50003
Sean Smith <SeanSith@users.noreply.github.com>
2023-02-14 09:59:22 -0500

    Update Cinc Workstation to 23.2.1028
    Additionally added ARM64 compatible builds.

--- Casks/cinc-workstation.rb
+++ Casks/cinc-workstation.rb
@@ -1,20 +1,23 @@
 cask "cinc-workstation" do
-  version "22.12.1024"
-  sha256 "c297e32e79dd30bddd643f39af532a5a3148a62e52193c261a3ea289786004e9"
+  arch arm: "arm64", intel: "x86_64"
 
-  url "http://downloads.cinc.sh/files/stable/cinc-workstation/#{version}/mac_os_x/10.15/cinc-workstation-#{version}-1.x86_64.dmg"
+  version "23.2.1028"
+  sha256 arm:   "87d9840067b90c9afa142057327355322f1ddf2e9505b2c61e723227cd2487f1",
+         intel: "fe51ad407032ca694397e6b9ffd7f5178b4158fd0c1818bfde69c17627f3578f"
+
+  url "http://downloads.cinc.sh/files/stable/cinc-workstation/#{version}/mac_os_x/11/cinc-workstation-#{version}-1.#{arch}.dmg"
   name "Cinc Workstation"
   desc "Free-as-in-Beer all-in-one installer for Chef infrastructure management tools"
   homepage "https://cinc.sh/start/workstation/"
 
   livecheck do
-    url "https://omnitruck.cinc.sh/stable/cinc-workstation/metadata?p=mac_os_x&pv=10.15&m=x86_64&v=latest"
+    url "https://omnitruck.cinc.sh/stable/cinc-workstation/metadata?p=mac_os_x&pv=11&m=#{arch}&v=latest"
     regex(/version\s*(\d+(?:\.\d+)+)/i)
   end
 
   depends_on macos: ">= :mojave"
 
-  pkg "cinc-workstation-#{version}-1.x86_64.pkg"
+  pkg "cinc-workstation-#{version}-1.#{arch}.pkg"
 
   uninstall pkgutil:   "com.cinc-project.pkg.cinc-workstation",
             launchctl: "io.chef.chef-workstation.app",
