eeb5a8adbfa8d62e3e568f00b9d1f959225248ad
Jameel Al-Aziz <me@jalaziz.io>
2023-02-15 00:45:11 -0800

    Update leapp from 0.17.2 to 0.17.3

--- Casks/leapp.rb
+++ Casks/leapp.rb
@@ -1,9 +1,9 @@
 cask "leapp" do
   arch arm: "-arm64"
 
-  version "0.17.2"
-  sha256 arm:   "9b5b09396ac207ff4dafaf65e569d593e4335ac78b939ba1e191c1039b23b1bf",
-         intel: "1c4e80bb00d91042cc99b3be5fac6ee0456d509e93a0a977c4dbb2cf71f871b6"
+  version "0.17.3"
+  sha256 arm:   "0d6ff055897122cfb1a0b6bdc77e69e99371d7686805f26b4c708ebc1e546b23",
+         intel: "aaeec65b581dcdc20aee46bbcfc6ab9b066d3a77f1a1a4d37cdd0923fb2167de"
 
   url "https://asset.noovolari.com/#{version}/Leapp-#{version}#{arch}.dmg",
       verified: "asset.noovolari.com/"
