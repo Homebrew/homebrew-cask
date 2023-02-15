440a7bbb7b6fa3d790d2a179715f8bca765edc7d
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-15 08:51:47 +0800

    microsoft-azure-storage-explorer: add arm (#141374)
    Update microsoft-azure-storage-explorer.rb

--- Casks/microsoft-azure-storage-explorer.rb
+++ Casks/microsoft-azure-storage-explorer.rb
@@ -1,8 +1,11 @@
 cask "microsoft-azure-storage-explorer" do
-  version "1.28.0"
-  sha256 "76094a5722243b8c594dbc23c1cbbfdc20fb27deec151f4deb18d4c7a10df8a7"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://github.com/microsoft/AzureStorageExplorer/releases/download/v#{version}/Mac_StorageExplorer.zip",
+  version "1.28.0"
+  sha256 arm:   "c53d9fb68579d807943776e9750bf11e97e1ff48866510daf222dbae583003ce",
+         intel: "76094a5722243b8c594dbc23c1cbbfdc20fb27deec151f4deb18d4c7a10df8a7"
+
+  url "https://github.com/microsoft/AzureStorageExplorer/releases/download/v#{version}/StorageExplorer-darwin-#{arch}.zip",
       verified: "github.com/microsoft/AzureStorageExplorer/"
   name "Microsoft Azure Storage Explorer"
   desc "Explorer for Azure Storage"
