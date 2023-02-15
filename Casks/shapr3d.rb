1716f031a4930b4ddf709072d0ebf5625598b8b2
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-02-15 16:25:35 +0800

    Update shapr3d from 5.301.0.4719 to 5.310.0.4771 (#141392)

--- Casks/shapr3d.rb
+++ Casks/shapr3d.rb
@@ -1,6 +1,6 @@
 cask "shapr3d" do
-  version "5.301.0.4719"
-  sha256 "5d96b84d275404fe651eebef6bfcdda288603c7549275a16fbfc9225b7c1ab88"
+  version "5.310.0.4771"
+  sha256 "32f180b4c14cae3920a5d3a452bfc80d7d6d87dbab9033a7e842a615617cfe55"
 
   url "https://download.shapr3d.com/mac/Shapr3D-#{version}.dmg"
   name "Shapr3D"
