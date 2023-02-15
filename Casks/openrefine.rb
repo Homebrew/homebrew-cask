c2b46f9abe7e88ec4fe55fbb43202764dcb0eb18
Patrick Linnane <patrick@linnane.io>
2023-02-15 07:46:59 -0800

    openrefine: adjust url

--- Casks/openrefine.rb
+++ Casks/openrefine.rb
@@ -2,8 +2,8 @@ cask "openrefine" do
   version "3.7.0"
   sha256 "bd602034ff26aa65a2e416ce349051515ecf6f17597cb4a45e79a7f0f99d6618"
 
-  url "https://oss.sonatype.org/service/local/artifact/maven/content?r=releases&g=org.openrefine&a=openrefine&v=#{version}&c=mac&p=dmg",
-      verified: "oss.sonatype.org/service/local/artifact/maven/"
+  url "https://github.com/OpenRefine/OpenRefine/releases/download/#{version}/openrefine-mac-#{version}.dmg",
+      verified: "github.com/OpenRefine/OpenRefine/"
   name "OpenRefine"
   desc "Tool for working with messy data (previously Google Refine)"
   homepage "https://openrefine.org/"
