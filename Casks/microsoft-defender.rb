e6c71085d142edab90fe775cad00c33b9ea9622e
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-15 14:57:50 +0800

    Create microsoft-defender.rb

---
+++ Casks/microsoft-defender.rb
@@ -0,0 +1,37 @@
+cask "microsoft-defender" do
+  version "101.96.85"
+  sha256 :no_check
+
+  url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft%20Defender.pkg"
+  name "Microsoft Defender"
+  desc "Antivirus software"
+  homepage "https://www.microsoft.com/security/"
+
+  livecheck do
+    url "https://aka.ms/MacDefender"
+    strategy :extract_plist
+  end
+
+  auto_updates true
+  depends_on cask: "microsoft-auto-update"
+  depends_on macos: ">= :big_sur"
+
+  pkg "Microsoft Defender.pkg",
+      choices: [
+        {
+          "choiceIdentifier" => "com.microsoft.autoupdate", # Office16_all_autoupdate.pkg
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 0,
+        },
+      ]
+
+  uninstall script: {
+    executable: "/Library/Application Support/Microsoft/Defender/uninstall/uninstall",
+    sudo:       true,
+  }
+
+  zap trash: [
+    "~/Library/Group Containers/*.com.microsoft.wdav/MicrosoftDefender.sqlite*",
+    "~/Library/Logs/Microsoft/Defender",
+  ]
+end
