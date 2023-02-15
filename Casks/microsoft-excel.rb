deabc537de1c29eb8b797f936f5dd290d0f91246
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-15 13:29:59 +0800

    Update microsoft-excel.rb

--- Casks/microsoft-excel.rb
+++ Casks/microsoft-excel.rb
@@ -36,7 +36,10 @@ cask "microsoft-excel" do
   end
 
   auto_updates true
-  conflicts_with cask: "microsoft-office"
+  conflicts_with cask: %w[
+    microsoft-office
+    microsoft-office-businesspro
+  ]
   depends_on cask: "microsoft-auto-update"
 
   pkg "Microsoft_Excel_#{version}_Installer.pkg",
