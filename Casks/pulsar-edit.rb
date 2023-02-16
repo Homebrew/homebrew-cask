852c64d3c4cf6fc8efa77e97ca95d69396b2024a
YellowForLight <94635645+YellowForLight@users.noreply.github.com>
2023-02-16 11:20:41 +0000

    Update Casks/pulsar-edit.rb
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>

--- Casks/pulsar-edit.rb
+++ Casks/pulsar-edit.rb
@@ -1,16 +1,10 @@
 cask "pulsar-edit" do
   version "1.102.0"
-
-  on_intel do
-    url "https://github.com/pulsar-edit/pulsar/releases/download/v#{version}/Intel.Mac.#{version}.zip",
+  arch intel: "Intel", arm: "Silicon"
+  url "https://github.com/pulsar-edit/pulsar/releases/download/v#{version}/#{arch}.Mac.#{version}.zip",
         verified: "github.com/pulsar-edit/pulsar"
-    sha256 "b3350445ea50139849312ad03dc535b4ede20ea47c24fa1564814cbc9a186d0e"
-  end
-  on_arm do
-    url "https://github.com/pulsar-edit/pulsar/releases/download/v#{version}/Silicon.Mac.#{version}.zip",
-        verified: "github.com/pulsar-edit/pulsar"
-    sha256 "07fb1e91ae99bfbc40833ec19339d05de708e6a41ec4a68c697f2e028490f68d"
-  end
+  sha256 intel: "b3350445ea50139849312ad03dc535b4ede20ea47c24fa1564814cbc9a186d0e",
+                arch: "07fb1e91ae99bfbc40833ec19339d05de708e6a41ec4a68c697f2e028490f68d"
 
   name "Pulsar"
   desc "Text editor"
