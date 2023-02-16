01f038066762d32c6a11b68294935d2e54f4274d
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-16 19:50:26 +0800

    Update and rename pulsar-edit.rb to pulsar.rb

--- Casks/pulsar.rb
+++ Casks/pulsar.rb
@@ -1,12 +1,12 @@
-cask "pulsar-edit" do
+cask "pulsar" do
   arch intel: "Intel", arm: "Silicon"
 
   version "1.102.0"
-  sha256 intel: "b3350445ea50139849312ad03dc535b4ede20ea47c24fa1564814cbc9a186d0e",
-         arm:   "07fb1e91ae99bfbc40833ec19339d05de708e6a41ec4a68c697f2e028490f68d"
+  sha256 arm:   "07fb1e91ae99bfbc40833ec19339d05de708e6a41ec4a68c697f2e028490f68d",
+         intel: "b3350445ea50139849312ad03dc535b4ede20ea47c24fa1564814cbc9a186d0e"
 
   url "https://github.com/pulsar-edit/pulsar/releases/download/v#{version}/#{arch}.Mac.#{version}.zip",
-      verified: "github.com/pulsar-edit/pulsar"
+      verified: "github.com/pulsar-edit/pulsar/"
   name "Pulsar"
   desc "Text editor"
   homepage "https://pulsar-edit.dev/"
@@ -16,9 +16,9 @@ cask "pulsar-edit" do
   binary "#{appdir}/Pulsar.app/Contents/Resources/pulsar.sh", target: "pulsar"
 
   zap trash: [
-    "~/.pular",
-    "~/Library/Application Support/Pulsar",
+    "~/.pulsar",
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/dev.pulsar-edit.pulsar.sfl*",
+    "~/Library/Application Support/Pulsar",
     "~/Library/Preferences/div.pulsar-edit.pulsar.plist",
     "~/Library/Saved Application State/dev.pulsar-edit.pulsar.savedState",
   ]
