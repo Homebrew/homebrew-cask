3aae837c6b0617bf05b061686a8393fbab77d4f7
Adon <dev@adon.dev>
2023-02-15 19:46:11 -0500

    Update opera-gx from 94.0.4606.101 to 94.0.4606.107 (#141416)

--- Casks/opera-gx.rb
+++ Casks/opera-gx.rb
@@ -1,6 +1,6 @@
 cask "opera-gx" do
-  version "94.0.4606.101"
-  sha256 "67e457f968a81d73824ab3a6848a4ed4f418aea10a69ed5b5c484e3e48d85c5e"
+  version "94.0.4606.107"
+  sha256 "e8bf62f3155ce1510ab31921e00a1916d416ca8f9e96e2dc00688d23210ed312"
 
   url "https://get.geo.opera.com/pub/opera_gx/#{version}/mac/Opera_GX_#{version}_Setup.dmg"
   name "Opera GX"
