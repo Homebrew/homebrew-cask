f4fcc9e56faadff741e8f9a727ba66beaeeee034
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-17 21:58:28 +0800

    Update rectangle-pro.rb

--- Casks/rectangle-pro.rb
+++ Casks/rectangle-pro.rb
@@ -1,15 +1,15 @@
 cask "rectangle-pro" do
-  version "2.7.7,133"
+  version "2.7.7"
   sha256 "dc1aaa8dbf8c6f5821832d7cffc16708a9a21c4ebb0151a5c43fd34320ef853f"
 
-  url "https://rectangleapp.com/pro/downloads/Rectangle%20Pro%20#{version.csv.first}.dmg"
+  url "https://rectangleapp.com/pro/downloads/Rectangle%20Pro%20#{version}.dmg"
   name "Rectangle Pro"
   desc "Window snapping tool"
   homepage "https://rectangleapp.com/pro"
 
   livecheck do
     url "https://rectangleapp.com/pro/downloads/updates.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   auto_updates true
