197ff498d37858d4b692a486e35f0778f1cb4bbf
ShredderWalnut <34428332+ShredderWalnut@users.noreply.github.com>
2023-02-17 10:05:45 +0000

    Update carla.rb

--- Casks/carla.rb
+++ Casks/carla.rb
@@ -3,7 +3,7 @@ cask "carla" do
   sha256 "e69145e1b3b9232d8ec31da9a23cf96163563e63"
 
   url "https://github.com/falkTX/Carla-Releases/releases/download/v#{version}/Carla-#{version}-macos-universal.dmg",
-    verified: "github.com/falkTX/Carla-releases/"
+      verified: "github.com/falkTX/Carla-releases/"
   name "Carla"
   desc "Audio plugin host supporting LADSPA, LV2, VST2/3, SF2 and more"
   homepage "https://kxstudio.linuxaudio.org/Applications:Carla"
@@ -14,5 +14,4 @@ cask "carla" do
   end
 
   app "Carla.app"
-
 end
