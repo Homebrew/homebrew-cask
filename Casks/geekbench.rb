4d033e4be1b574f11b4e811bd4498e9e739659d8
5ouma <101255979+5ouma@users.noreply.github.com>
2023-02-17 20:45:46 +0900

    Update geekbench from 5.5.1 to 6.0.0

--- Casks/geekbench.rb
+++ Casks/geekbench.rb
@@ -3,9 +3,13 @@ cask "geekbench" do
     version "4.4.2"
     sha256 "3c46e630a28a0752afd702fc1cd379edd2420001be22302c932e61751284c0cc"
   end
-  on_high_sierra :or_newer do
+  on_catalina :or_older do
     version "5.5.1,503987"
     sha256 "04b06cb642e51230a3dfd07ce2d3a4ea696cb349583737622749174dc8747313"
+  end
+  on_big_sur :or_newer do
+    version "6.0.0,602754"
+    sha256 "42c443ac203c49dce0646f847042b207560c0eb092673a47224cca5c4233f40b"
   end
 
   url "https://cdn.geekbench.com/Geekbench-#{version.csv.first}-Mac.zip"
