577439d8f2d798e451133aca8a237cc6dc25d200
Souma <101255979+5ouma@users.noreply.github.com>
2023-02-17 21:44:38 +0900

    Update geekbench from 5.5.1 to 6.0.0 (#141475)
    * Update geekbench from 5.5.1 to 6.0.0
    
    * Fix macOS version error.
    
    * Update geekbench.rb
    
    ---------
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/geekbench.rb
+++ Casks/geekbench.rb
@@ -1,11 +1,23 @@
 cask "geekbench" do
   on_sierra :or_older do
-    version "4.4.2"
-    sha256 "3c46e630a28a0752afd702fc1cd379edd2420001be22302c932e61751284c0cc"
+    version "4.4.4,401571"
+    sha256 "1fc2b87742cd27deaa184a753a57bfc5a8c81de34524e9fd318d6875be6ac5c9"
   end
-  on_high_sierra :or_newer do
+  on_high_sierra do
     version "5.5.1,503987"
     sha256 "04b06cb642e51230a3dfd07ce2d3a4ea696cb349583737622749174dc8747313"
+  end
+  on_mojave do
+    version "5.5.1,503987"
+    sha256 "04b06cb642e51230a3dfd07ce2d3a4ea696cb349583737622749174dc8747313"
+  end
+  on_catalina do
+    version "5.5.1,503987"
+    sha256 "04b06cb642e51230a3dfd07ce2d3a4ea696cb349583737622749174dc8747313"
+  end
+  on_big_sur :or_newer do
+    version "6.0.0,602754"
+    sha256 "42c443ac203c49dce0646f847042b207560c0eb092673a47224cca5c4233f40b"
   end
 
   url "https://cdn.geekbench.com/Geekbench-#{version.csv.first}-Mac.zip"
