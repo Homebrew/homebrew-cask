7e23b51c4a1b0f00bdeeab27adf85d0ef36b8782
leaves chen <leaves615@gmail.com>
2023-02-19 01:12:14 +0800

    Update keyboardholder from 1.10.4 to 1.10.5

--- Casks/keyboardholder.rb
+++ Casks/keyboardholder.rb
@@ -1,6 +1,6 @@
 cask "keyboardholder" do
-  version "1.10.4"
-  sha256 "3714c7ecf9b04111f553f8ebc14c216560fac44bba4539d9ffd930e0d67c98e0"
+  version "1.10.5"
+  sha256 "396e8daf80c009e39f6e5e73f883b37045d2d708ff150cb91d86762cf627a656"
 
   url "https://github.com/leaves615/KeyboardHolder/releases/download/v#{version}/KeyboardHolder-#{version}.zip",
       verified: "github.com/leaves615/KeyboardHolder/"
