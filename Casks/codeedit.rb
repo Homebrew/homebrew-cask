7145896397622c0fefd7e139f5025bb41b6fcb66
Patrick Linnane <patrick@linnane.io>
2023-02-19 08:08:08 -0800

    codeedit: add `livecheck`, adjust url & version

--- Casks/codeedit.rb
+++ Casks/codeedit.rb
@@ -1,11 +1,30 @@
 cask "codeedit" do
-  version "0.0.1-alpha.25"
+  version "0.0.1-alpha.25,8e438ba"
   sha256 "b4c05d73faacc6f70d4db1e06205653e855692455b016e4955ba27355611ba7f"
 
-  url "https://github.com/CodeEditApp/CodeEdit/releases/download/#{version}/CodeEdit-8e438ba.dmg"
+  url "https://github.com/CodeEditApp/CodeEdit/releases/download/#{version.csv.first}/CodeEdit-#{version.csv.second}.dmg",
+      verified: "github.com/CodeEditApp/CodeEdit/"
   name "codeedit"
   desc "Code editor"
   homepage "https://www.codeedit.app/"
+
+  livecheck do
+    url "https://github.com/CodeEditApp/CodeEdit/releases/latest"
+    regex(%r{href=.*?(\d+(?:\.\d+)+-alpha.\d+)/CodeEdit-([a-f0-9]+)\.dmg}i)
+    strategy :header_match do |headers, regex|
+      next if headers["location"].blank?
+
+      # Identify the latest tag from the response's `location` header
+      latest_tag = File.basename(headers["location"])
+      next if latest_tag.blank?
+
+      # Fetch the assets list HTML for the latest tag and match within it
+      assets_page = Homebrew::Livecheck::Strategy.page_content(
+        @url.sub(%r{/releases/?.+}, "/releases/expanded_assets/#{latest_tag}"),
+      )
+      assets_page[:content]&.scan(regex)&.map { |match| "#{match[0]},#{match[1]}" }
+    end
+  end
 
   app "CodeEdit.app"
 end
