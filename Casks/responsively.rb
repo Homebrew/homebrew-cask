87778df6afc9e240d6ab5e699362e9e2972e2bdb
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-20 07:28:11 +0800

    Update responsively.rb

--- Casks/responsively.rb
+++ Casks/responsively.rb
@@ -2,8 +2,8 @@ cask "responsively" do
   arch arm: "-arm64"
 
   version "1.0.2"
-  sha256 intel: "9438c0649c008f7872f8d9784a63de083f1d04e1ddc3528d10c7e8c2fc82e2c1",
-         arm:   "9031fdc12f1d0d7e5002e714dfdfabf345c46dc5711657cdf7570b69a22c3a54"
+  sha256 arm:   "9031fdc12f1d0d7e5002e714dfdfabf345c46dc5711657cdf7570b69a22c3a54",
+         intel: "9438c0649c008f7872f8d9784a63de083f1d04e1ddc3528d10c7e8c2fc82e2c1"
 
   url "https://github.com/responsively-org/responsively-app-releases/releases/download/v#{version}/ResponsivelyApp-#{version}#{arch}.dmg",
       verified: "github.com/responsively-org/responsively-app-releases/"
