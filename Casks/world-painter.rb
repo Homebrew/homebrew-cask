118cf3347e63dc8d9898b1a7627aa69a4969030b
Joseph Shanak <josephshanak@protonmail.com>
2023-02-18 18:16:09 -0600

    world-painter 2.5.18

---
+++ Casks/world-painter.rb
@@ -0,0 +1,17 @@
+cask "world-painter" do
+  version "2.15.18"
+  sha256 "0b86e4d8d623acd4c27f3b1f5b3c4d1502713a24599dde8704d5b98da24d0892"
+
+  url "https://www.worldpainter.net/files/worldpainter_#{version}.tgz"
+  name "WorldPainter"
+  desc "Interactive map generator for Minecraft"
+  homepage "https://www.worldpainter.net/"
+
+  livecheck do
+    url "https://www.worldpainter.net/"
+    strategy :page_match
+    regex(%r{href=.*?/files/worldpainter_(\d+(?:\.\d+)*)\.tgz}i)
+  end
+
+  app "WorldPainter.app"
+end
