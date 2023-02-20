bfec5d15dd8cb3d0aeec64455bb94c9d072041ff
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-20 08:09:33 +0800

    Update purei-play.rb

--- Casks/purei-play.rb
+++ Casks/purei-play.rb
@@ -3,7 +3,7 @@ cask "purei-play" do
   sha256 "2b173322edb37f999af82875127e1f8a609da6be4b97f759a48fabdd76169a93"
 
   url "https://s3.us-east-2.amazonaws.com/playbuilds/#{version.csv.second}/Play.dmg",
-      verified: "https://s3.us-east-2.amazonaws.com/playbuilds/"
+      verified: "s3.us-east-2.amazonaws.com/playbuilds/"
   name "Play!"
   desc "PlayStation 2 emulator"
   homepage "https://purei.org/"
