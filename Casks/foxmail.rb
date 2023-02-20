20151867dc0ba0a16df55f01cea67505dcfe721d
zuisong <19145952+zuisong@users.noreply.github.com>
2023-02-20 14:15:17 +0800

    Update foxmail from 1.5.6.94576 to 1.5.8.94580 (#141564)
    * Update foxmail from 1.5.6.94576 to 1.5.8.94580
    
    * Update foxmail.rb
    
    ---------
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/foxmail.rb
+++ Casks/foxmail.rb
@@ -1,6 +1,6 @@
 cask "foxmail" do
-  version "1.5.6.94576"
-  sha256 "c7a4e6b0892f06b432e1f079ebe80f33740eb9e2ca3cf1a4a83149e8015f0bbd"
+  version "1.5.8.94580"
+  sha256 "8443e9bd5cfab983725daae69d4b4065db22cd30aba252ce19691a95932a67cd"
 
   url "https://dldir1.qq.com/foxmail/MacFoxmail/Foxmail_for_Mac_#{version}.dmg",
       verified: "dldir1.qq.com/foxmail/MacFoxmail/"
@@ -16,4 +16,9 @@ cask "foxmail" do
   depends_on macos: ">= :el_capitan"
 
   app "Foxmail.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.tencent.Foxmail*",
+    "~/Library/Containers/com.tencent.Foxmail*",
+  ]
 end
