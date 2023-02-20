26c51fdcd344f1b08b56aed88dafdac660236df8
Kai Winter <kaiwinter@gmx.de>
2023-02-20 07:00:01 +0100

    Update dbeaver-community from 22.3.4 to 22.3.5 (#141565)

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,9 +1,9 @@
 cask "dbeaver-community" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "22.3.4"
-  sha256 arm:   "ff8cbbfa29e33520485774d2c7a159811f8ff81dfc324e8032c054e0dc81cb07",
-         intel: "8e01af13b32acf53377f32e940ae0cdf00f6967c723e46fed70adb225cc11d0b"
+  version "22.3.5"
+  sha256 arm:   "56a9f5d50bcb381797d6ab7512f154ca5d6a580d59230a2314d1e727f536f3f9",
+         intel: "5fd2d811bed5251d235df8547d59cfa27bfa835620f67daa24d0a69be776031d"
 
   url "https://dbeaver.io/files/#{version}/dbeaver-ce-#{version}-macos-#{arch}.dmg"
   name "DBeaver Community Edition"
