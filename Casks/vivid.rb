113119032d7f3816d3f1dcec43903c26c009614e
miccal <miccal.matthews@gmail.com>
2023-02-20 14:12:28 +0800

    Update vivid from 2.5,37 to 2.5

--- Casks/vivid.rb
+++ Casks/vivid.rb
@@ -1,5 +1,5 @@
 cask "vivid" do
-  version "2.5,37"
+  version "2.5"
   sha256 "9ec5a14c727028941ae0389101994be98d45ae00058195e8a6ed3c05521e0504"
 
   url "https://lumen-digital.com/apps/vivid/releases/Vivid#{version.csv.first}.zip",
