1a7c90a998e57f216a06ad0f55e5375088a922eb
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-21 16:07:17 +0800

    Update notebooks.rb

--- Casks/notebooks.rb
+++ Casks/notebooks.rb
@@ -1,5 +1,5 @@
 cask "notebooks" do
-  version "3.1,233"
+  version "3.1"
   sha256 "34bb68a4b0395340e4d4f0275b494b2798db5ac86bebc310d7408295ba8e746b"
 
   url "https://www.notebooksapp.com/Download/macOS/v#{version.major}/Notebooks.dmg"
@@ -9,7 +9,7 @@ cask "notebooks" do
 
   livecheck do
     url "https://notebooksapp.com/Download/macOS/v#{version.major}/Notebooks#{version.major}Appcast.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   app "Notebooks.app"
