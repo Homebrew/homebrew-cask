3f3f25570a9266fe8d4e2bf9e68cee1540bad777
rqel <rqel@pm.me>
2023-02-21 10:57:25 -0800

    Update iina-plus from 0.6.23 to 0.6.24

--- Casks/iina-plus.rb
+++ Casks/iina-plus.rb
@@ -1,6 +1,6 @@
 cask "iina-plus" do
-  version "0.6.23"
-  sha256 "a0cf693ef5c8ff5f255698dd491a6e33670d2315c396575f2f218d32855cd4a5"
+  version "0.6.24"
+  sha256 "1e586e9bf90a5625e430ae64c22ca7b75ca82a97ed98061497b117242ab30eeb"
 
   url "https://github.com/xjbeta/iina-plus/releases/download/#{version}/IINA+.#{version}.dmg"
   name "IINA+"
