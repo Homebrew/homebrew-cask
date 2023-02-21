5514b6116a1574968005bbf1bc3b3ed037f53d8b
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-21 08:46:07 +0800

    Update mist.rb

--- Casks/mist.rb
+++ Casks/mist.rb
@@ -4,29 +4,29 @@ cask "mist" do
 
   url "https://github.com/ninxsoft/Mist/releases/download/v#{version}/Mist.#{version}.pkg"
   name "Mist"
-  desc "Utility that automatically downloads macOS Firmwares / Installers"
+  desc "Utility that automatically downloads firmwares and installers"
   homepage "https://github.com/ninxsoft/Mist"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
 
   auto_updates true
   depends_on macos: ">= :monterey"
 
   pkg "Mist.#{version}.pkg"
 
-  uninstall quit:      ["com.ninxsoft.mist.helper", "com.ninxsoft.mist"],
+  uninstall quit:      [
+              "com.ninxsoft.mist.helper",
+              "com.ninxsoft.mist",
+            ],
             launchctl: "com.ninxsoft.mist.helper",
-            pkgutil:   "com.ninxsoft.pkg.mist"
+            pkgutil:   "com.ninxsoft.pkg.mist",
+            delete:    [
+              "/Library/LaunchDaemons/com.ninxsoft.mist.helper.plist",
+              "/Library/PrivilegedHelperTools/com.ninxsoft.mist.helper",
+            ]
 
   zap trash: [
-    "/Users/Shared/Mist", # Mist.app creates this directory for cache files
+    "/Users/Shared/Mist",
     "~/Library/Caches/com.ninxsoft.mist",
     "~/Library/Preferences/com.ninxsoft.mist.plist",
-    "~/Library/Saved Application State/com.ninxsoft.mist.savedState",
-    "/Library/LaunchDaemons/com.ninxsoft.mist.helper.plist",
-    "/Library/PrivilegedHelperTools/com.ninxsoft.mist.helper",
+    "~/Library/Saved Application State/com.ninxsoft.mist.savedState", 
   ]
 end
