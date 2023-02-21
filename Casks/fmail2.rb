2d89fb3cfb782d78311b9b3653ac66c79ec1372b
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-21 15:54:00 +0800

    Update fmail2.rb

--- Casks/fmail2.rb
+++ Casks/fmail2.rb
@@ -1,17 +1,17 @@
 cask "fmail2" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "1.7.0,58"
+  version "1.7.0"
   sha256 "5023b58664be6a446d1dd35c288914ca5dba7b0409b139885fde995598e17173"
 
-  url "https://arievanboxel.fr/fmail2/sparkle/FMail2_#{version.csv.first.no_dots}.zip",
-      verified: "arievanboxel.fr"
+  url "https://arievanboxel.fr/fmail2/sparkle/FMail2_#{version.no_dots}.zip",
+      verified: "arievanboxel.fr/fmail2/sparkle/"
   name "fmail2"
   desc "Unofficial 'light' native application for Fastmail"
   homepage "https://fmail-app.fr/"
 
   livecheck do
     url "https://arievanboxel.fr/fmail2/sparkle/appcast.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   auto_updates true
