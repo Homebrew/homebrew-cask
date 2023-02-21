2110421e4e53b3802f2da5ed08dbc18c37b771c3
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-21 08:48:09 +0800

    Update elk.rb

--- Casks/elk.rb
+++ Casks/elk.rb
@@ -1,9 +1,9 @@
 cask "elk" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.3.0"
-  sha256 arm:   "623889c29fc7c708d121db73eb8475389daa115e262dbb38c5f58902bd424097",
-         intel: "7088f83531ce3de7a3173499b05e9368661fa5f8d405fc888cea278b557e2b04"
+  version "0.4.0"
+  sha256 arm:   "bfa8cf1854302eeb4f8f4ac28240b32ca674e2066bb29f518226b4fe93ecc4f0",
+         intel: "e358c071ba8b310bdc29441f18fe56d6e31d96a5975c6f82be1088b10f1fbad9"
 
   url "https://github.com/elk-zone/elk-native/releases/download/elk-native-v#{version}/Elk_#{version}_macos_#{arch}.dmg"
   name "Elk Native"
