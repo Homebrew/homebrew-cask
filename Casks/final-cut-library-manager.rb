c01f0b82459f7656e9d320d5bf52b5a0e0cf7aab
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-22 06:06:29 +0800

    Update final-cut-library-manager.rb

--- Casks/final-cut-library-manager.rb
+++ Casks/final-cut-library-manager.rb
@@ -1,8 +1,8 @@
 cask "final-cut-library-manager" do
-  version "3.95.00"
+  version "3.95"
   sha256 "11c16759a2a5078c2da4c79d9939b2645c75d571925c6856f686a13faaea9cc4"
 
-  url "http://cdn.arcticwhiteness.com/finalcutlibrarymanager/download/zips/FinalCutLibraryManager_#{version.major_minor}.zip"
+  url "http://cdn.arcticwhiteness.com/finalcutlibrarymanager/download/zips/FinalCutLibraryManager_#{version}.zip"
   name "Arctic Whiteness Final Cut Library Manager"
   name "FCPX Library Manager"
   desc "Displays Final Cut Pro X libraries"
