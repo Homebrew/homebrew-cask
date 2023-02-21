019c7e7c7d1f8f9208dc75d3e3df72b6898f32d9
lethefrost <47198693+lethefrost@users.noreply.github.com>
2023-02-21 09:08:24 -0500

    Update ogdesign-eagle.rb (#141643)
    * Update ogdesign-eagle.rb
    
    Update to Ver 3.0 Build 33
    
    * Update ogdesign-eagle.rb
    
    ---------
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/ogdesign-eagle.rb
+++ Casks/ogdesign-eagle.rb
@@ -1,14 +1,15 @@
 cask "ogdesign-eagle" do
   arch arm: "M1-"
 
-  version "3.0,32"
-  sha256 arm:   "7f8569b1898f919d16b9f8dc34e6921225074f2318a07c8f1e96c2078f88855a",
-         intel: "ee2d8f8f8c75e737013c66874c805b95781d1ce507a1cc075be030fc05e261f1"
+  version "3.0,33"
+  sha256 arm:   "55c4a6bb7544c13e1a405aed2e0ef2d8e09da80e2d1515ecb68555e65b47b13b",
+         intel: "957f7ab37099e20321962509626787f9255cf8b7466d3643f027f7d432a89953"
 
-  url "https://r2-app.eagle.cool/releases/Eagle-#{version.csv.first}-#{arch}build#{version.csv.second}.dmg"
+  url "https://eaglefile.oss-cn-shenzhen.aliyuncs.com/releases/Eagle-#{version.csv.first}-#{arch}build#{version.csv.second}.dmg",
+      verified: "eaglefile.oss-cn-shenzhen.aliyuncs.com/releases/"
   name "Eagle"
   desc "Organize all your reference images in one place"
-  homepage "https://eagle.cool/macOS"
+  homepage "https://eagle.cool/"
 
   livecheck do
     url "https://eagle.cool/check-for-update"
