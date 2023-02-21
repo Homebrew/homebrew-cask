c71c1fd13a6cbb3107ac8727fa8feee42b9a827e
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-21 17:45:51 +0800

    Update sidenotes from 1.4.8 to 1.4.9 (#141630)

--- Casks/sidenotes.rb
+++ Casks/sidenotes.rb
@@ -1,15 +1,15 @@
 cask "sidenotes" do
-  version "1.4.8,211"
-  sha256 "ce8ba6faa655deb3030c3efdc0388a1ff47c7e154e066fc9a9fd93c135b56c81"
+  version "1.4.9"
+  sha256 "3d897bb794bc7b1fa46a20aef46709797f158d2a41b057ccd0a97c48f03d9bf4"
 
-  url "https://www.apptorium.com/public/products/sidenotes/releases/SideNotes-#{version.csv.first}.zip"
+  url "https://www.apptorium.com/public/products/sidenotes/releases/SideNotes-#{version}.zip"
   name "SideNotes"
   desc "Note-taking application"
   homepage "https://www.apptorium.com/sidenotes"
 
   livecheck do
     url "https://www.apptorium.com/updates/sidenotes"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   depends_on macos: ">= :catalina"
