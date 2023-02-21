0f9dfeaf258b1b5224f2011b06ad02cfa1e8b545
miccal <miccal.matthews@gmail.com>
2023-02-21 16:12:33 +0800

    Update principle from 6.28 to 6.29

--- Casks/principle.rb
+++ Casks/principle.rb
@@ -1,6 +1,6 @@
 cask "principle" do
-  version "6.28,6033"
-  sha256 "32927b42daa6a93d2ba050e54b7216e2ce0728981aaf659bd00b7320acb571c3"
+  version "6.29,6036"
+  sha256 "db4a1101231e720fcb5eaca17c6110d3b004b493d0461ddfa8929b755414d6bd"
 
   url "https://principleformac.com/download/Principle_#{version.csv.first.dots_to_underscores}.zip"
   name "Principle"
