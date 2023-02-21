5d89fcc6a6bdbe8c59f91baf575afa4cae90ab1b
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-21 16:05:50 +0800

    Update ishowu-instant.rb

--- Casks/ishowu-instant.rb
+++ Casks/ishowu-instant.rb
@@ -1,15 +1,15 @@
 cask "ishowu-instant" do
-  version "1.4.14,1433"
+  version "1.4.14"
   sha256 "a3d1bf98d395168c636f2fef8da4fded84386a11e7de9443fa36572064448948"
 
-  url "https://www.shinywhitebox.com/downloads/instant/iShowU_Instant_#{version.csv.first}.dmg"
+  url "https://www.shinywhitebox.com/downloads/instant/iShowU_Instant_#{version}.dmg"
   name "iShowU Instant"
   desc "Realtime screen recording"
   homepage "https://www.shinywhitebox.com/ishowu-instant"
 
   livecheck do
     url "https://www.shinywhitebox.com/store/appcast.php?p=14"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   depends_on macos: ">= :mojave"
