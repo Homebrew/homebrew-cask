477275ac8d935c0929b67ce95747bf56b147f6f9
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-21 21:49:23 +0800

    Update ogdesign-eagle.rb

--- Casks/ogdesign-eagle.rb
+++ Casks/ogdesign-eagle.rb
@@ -5,10 +5,11 @@ cask "ogdesign-eagle" do
   sha256 arm:   "55c4a6bb7544c13e1a405aed2e0ef2d8e09da80e2d1515ecb68555e65b47b13b",
          intel: "957f7ab37099e20321962509626787f9255cf8b7466d3643f027f7d432a89953"
 
-  url "https://r2-app.eagle.cool/releases/Eagle-#{version.csv.first}-#{arch}build#{version.csv.second}.dmg"
+  url "https://eaglefile.oss-cn-shenzhen.aliyuncs.com/releases/Eagle-#{version.csv.first}-#{arch}build#{version.csv.second}.dmg",
+      verified: "eaglefile.oss-cn-shenzhen.aliyuncs.com/releases/"
   name "Eagle"
   desc "Organize all your reference images in one place"
-  homepage "https://eagle.cool/macOS"
+  homepage "https://eagle.cool/"
 
   livecheck do
     url "https://eagle.cool/check-for-update"
