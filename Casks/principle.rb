5b2f1aa479d39b3cc95d34ed8d022dc3c85297a2
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-21 16:13:22 +0800

    Update principle.rb

--- Casks/principle.rb
+++ Casks/principle.rb
@@ -1,15 +1,15 @@
 cask "principle" do
-  version "6.29,6036"
+  version "6.29"
   sha256 "db4a1101231e720fcb5eaca17c6110d3b004b493d0461ddfa8929b755414d6bd"
 
-  url "https://principleformac.com/download/Principle_#{version.csv.first.dots_to_underscores}.zip"
+  url "https://principleformac.com/download/Principle_#{version.dots_to_underscores}.zip"
   name "Principle"
   desc "Design animated and interactive user interfaces"
   homepage "https://principleformac.com/"
 
   livecheck do
     url "https://principleformac.com/update2.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   app "Principle.app"
