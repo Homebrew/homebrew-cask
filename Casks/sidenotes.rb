02e4fba23797876e5ed33deaf7470a805deedd8b
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-21 16:31:26 +0800

    Update sidenotes.rb

--- Casks/sidenotes.rb
+++ Casks/sidenotes.rb
@@ -1,15 +1,15 @@
 cask "sidenotes" do
-  version "1.4.9,216"
+  version "1.4.9"
   sha256 "3d897bb794bc7b1fa46a20aef46709797f158d2a41b057ccd0a97c48f03d9bf4"
 
-  url "https://www.apptorium.com/public/products/sidenotes/releases/SideNotes-#{version.csv.first}.zip"
+  url "https://www.apptorium.com/public/products/sidenotes/releases/SideNotes-#{version}.zip"
   name "SideNotes"
   desc "Note-taking application"
   homepage "https://www.apptorium.com/sidenotes"
 
   livecheck do
     url "https://www.apptorium.com/updates/sidenotes"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   depends_on macos: ">= :catalina"
