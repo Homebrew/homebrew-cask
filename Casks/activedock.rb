9e1bc70c655c999a144cf04aedbfeb5eb7798d07
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-21 17:37:41 +0800

    Update activedock from 2.223 to 2.400 (#141605)

--- Casks/activedock.rb
+++ Casks/activedock.rb
@@ -1,19 +1,19 @@
 cask "activedock" do
-  version "2.223,2223"
+  version "2.400"
   sha256 :no_check
 
   url "https://macplus-software.com/downloads/ActiveDock.zip",
-      verified: "macplus-software.com/"
+      verified: "macplus-software.com/downloads/"
   name "ActiveDock"
   desc "Customizable dock, application launcher, dock replacement"
   homepage "https://www.noteifyapp.com/activedock/"
 
   livecheck do
     url "https://macplus-software.com/downloads/ActiveDock.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   depends_on macos: ">= :high_sierra"
 
-  app "ActiveDock #{version.csv.first.major}.app"
+  app "ActiveDock #{version.major}.app"
 end
