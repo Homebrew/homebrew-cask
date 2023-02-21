8745b63f6e42207da7b1eabeef9f583f9477f903
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-21 15:55:46 +0800

    Update activedock.rb

--- Casks/activedock.rb
+++ Casks/activedock.rb
@@ -3,7 +3,7 @@ cask "activedock" do
   sha256 :no_check
 
   url "https://macplus-software.com/downloads/ActiveDock.zip",
-      verified: "macplus-software.com/"
+      verified: "macplus-software.com/downloads/"
   name "ActiveDock"
   desc "Customizable dock, application launcher, dock replacement"
   homepage "https://www.noteifyapp.com/activedock/"
