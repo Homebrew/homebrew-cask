fd9b569049ce94b58810365e5d8e8aca7e44144f
Andreas Eitel <35565269+aneitel@users.noreply.github.com>
2023-02-24 00:13:02 +0100

    Update portfolioperformance from 0.61.0 to 0.61.1 (#141753)
    * Update portfolioperformance from 0.61.0 to 0.61.1
    
    * Update portfolioperformance.rb
    
    ---------
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/portfolioperformance.rb
+++ Casks/portfolioperformance.rb
@@ -1,9 +1,9 @@
 cask "portfolioperformance" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.61.0"
-  sha256 arm:   "c9ce8466a250c9c3eed4e4ff9f64799ab08b40e1cd6615ceeede41ca654882bf",
-         intel: "d75dd22b25a9d2e9bed851527be4e4cb9f90419b8b26a9f3983e9b370f9eb099"
+  version "0.61.1"
+  sha256 arm:   "91b2acce550cafca27d8d370f89a8d587403219b1af7b2aab3427c2129bad1b6",
+         intel: "bfa8b2770d6c76452e850d4fae0e4598c582b7ba10d3e0a896dd1bc2d4f0583d"
 
   url "https://github.com/buchen/portfolio/releases/download/#{version}/PortfolioPerformance-#{version}-#{arch}.dmg",
       verified: "github.com/buchen/portfolio/"
@@ -13,7 +13,7 @@ cask "portfolioperformance" do
 
   livecheck do
     url :url
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    strategy :github_latest
   end
 
   auto_updates true
