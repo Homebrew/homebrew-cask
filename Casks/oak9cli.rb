fcf7473630fbc8c943cd4b5cef6618f2849f0b41
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-23 08:21:49 +0800

    Update oak9cli.rb

--- Casks/oak9cli.rb
+++ Casks/oak9cli.rb
@@ -3,18 +3,13 @@ cask "oak9cli" do
   sha256 "cef527114243a2d48390f8ed454e56cf83f20135511f4f0eabf3dbed660c8ee2"
 
   url "https://github.com/oak9io/oak9.cli-public/blob/master/binaries/iac-mac_os-#{version}.zip"
-  name "oak9cli"
-  desc "This is the cli component of oak9's security compliance SaaS product. Which allows you to scan your IaC for security vulnerabilities and dynamically secure your infrastructure. https://oak9.io"
+  name "oak9 CLI"
+  desc "Component of oak9's security compliance SaaS product"
   homepage "https://github.com/oak9io/oak9.cli-public/"
 
-  app "oak9cli"
+  auto_updates true
 
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
+  binary "oak9"
 
-   auto_updates true
-   container nested: "oak9"
-   zap trash: "./oak9"
+  zap trash: "./oak9"
 end
