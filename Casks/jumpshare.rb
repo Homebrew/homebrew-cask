7404261f76f571053babe412449d78e8e35c528d
miccal <miccal.matthews@gmail.com>
2023-02-23 22:14:59 +0800

    Update jumpshare from 3.2.7 to 3.2.8

--- Casks/jumpshare.rb
+++ Casks/jumpshare.rb
@@ -1,6 +1,6 @@
 cask "jumpshare" do
-  version "3.2.7,148"
-  sha256 "ddf247efeb95d6b203f70c8580e8acd22474f405513c668447be49a1fab9ae42"
+  version "3.2.8,149"
+  sha256 "f98ffadbd3837eaad87a32eda86d536ee32cc3bebb2852c0452388dc73b5b234"
 
   url "https://d21hi1or3tbtjm.cloudfront.net/desktop/mac/updates/Jumpshare-#{version.csv.first}.tar.bz2",
       verified: "d21hi1or3tbtjm.cloudfront.net/"
