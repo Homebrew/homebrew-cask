b287fc25bac1f322371da5c6aeec810e4202aaf5
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-23 22:23:19 +0800

    Update prezi-next.rb

--- Casks/prezi-next.rb
+++ Casks/prezi-next.rb
@@ -1,15 +1,15 @@
 cask "prezi-next" do
-  version "1.61.0,27448"
+  version "1.61.0"
   sha256 "1afc3c50dfd2a0a0bb5b35f9e5071e0df42342fd5c1215ca60656544ffeec51e"
 
-  url "https://desktopassets.prezi.com/mac/pitch/releases/Prezi_Next_#{version.csv.first}.dmg"
+  url "https://desktopassets.prezi.com/mac/pitch/releases/Prezi_Next_#{version}.dmg"
   name "Prezi Next"
   desc "Presentation software"
   homepage "https://prezi.com/"
 
   livecheck do
     url "https://prezidesktop.s3.amazonaws.com/assets/mac/pitch/updates/prezi-business.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   depends_on macos: ">= :el_capitan"
