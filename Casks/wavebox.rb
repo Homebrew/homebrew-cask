8f92ac8a74f6db7938ece9c15ad1c20f9133e847
owine <owine@users.noreply.github.com>
2023-02-23 08:26:39 -0600

    Update wavebox from 10.110.18.2 to 10.110.26.2 (#141686)

--- Casks/wavebox.rb
+++ Casks/wavebox.rb
@@ -1,9 +1,9 @@
 cask "wavebox" do
   arch arm: "macarm64", intel: "mac"
 
-  version "10.110.18.2"
-  sha256 arm:   "bcba59322252eb4bd91def48de1e8443d2315b51214fa7f20df5ade3a43c5c4d",
-         intel: "fbc467e8634861acef775aa7e806260cca0649aee0c7d7001f2bdbc2ef064c35"
+  version "10.110.26.2"
+  sha256 arm:   "6a2585937ded9ff8a8287f353b6af2de599c2d0ba156c20f0f2436c493fe7c04",
+         intel: "6d100239c953ed820b8973887fc6b8150eda91dedfd77b1291a6016425b9dabc"
 
   url "https://download.wavebox.app/stable/#{arch}/Install%20Wavebox%20#{version}.dmg",
       verified: "download.wavebox.app/"
