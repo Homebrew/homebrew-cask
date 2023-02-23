7f63754597e67f2fd1c1dd6c709ec014063a192c
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-23 23:03:04 +0800

    Update jumpshare from 3.2.7 to 3.2.8 (#141725)
    * Update jumpshare from 3.2.7 to 3.2.8
    
    * Update jumpshare.rb

--- Casks/jumpshare.rb
+++ Casks/jumpshare.rb
@@ -1,16 +1,16 @@
 cask "jumpshare" do
-  version "3.2.7,148"
-  sha256 "ddf247efeb95d6b203f70c8580e8acd22474f405513c668447be49a1fab9ae42"
+  version "3.2.8"
+  sha256 "f98ffadbd3837eaad87a32eda86d536ee32cc3bebb2852c0452388dc73b5b234"
 
-  url "https://d21hi1or3tbtjm.cloudfront.net/desktop/mac/updates/Jumpshare-#{version.csv.first}.tar.bz2",
-      verified: "d21hi1or3tbtjm.cloudfront.net/"
+  url "https://d21hi1or3tbtjm.cloudfront.net/desktop/mac/updates/Jumpshare-#{version}.tar.bz2",
+      verified: "d21hi1or3tbtjm.cloudfront.net/desktop/mac/updates/"
   name "Jumpshare"
   desc "File sharing, screen recording, and screenshot capture app"
   homepage "https://jumpshare.com/"
 
   livecheck do
     url "https://apps.jumpshare.com/desktop/mac/updates/appcast.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   depends_on macos: ">= :mojave"
