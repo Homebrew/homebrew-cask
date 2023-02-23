645a29388878a32096ba50ccfd78fc8a1ec44a12
Christian Kraemer <christian.kraemer@gmail.com>
2023-02-23 14:15:55 +0100

    Update google-chrome from 110.0.5481.100 to 110.0.5481.177 (#141696)

--- Casks/google-chrome.rb
+++ Casks/google-chrome.rb
@@ -1,5 +1,5 @@
 cask "google-chrome" do
-  version "110.0.5481.100"
+  version "110.0.5481.177"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
