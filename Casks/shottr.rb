1c2c323a583d76cb97ef620ef9dd986f793f0edd
János Orcsik <orcsikj@gmail.com>
2023-02-23 16:30:21 +0100

    Update shottr from 1.6.1 to 1.6.2 (#141720)
    * Update shottr from 1.6.1 to 1.6.2
    
    * shottr: update livecheck
    
    ---------
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/shottr.rb
+++ Casks/shottr.rb
@@ -1,16 +1,22 @@
 cask "shottr" do
-  version "1.6.1"
-  sha256 "dd022de05108a282c83a9d12f7faecaab90d1b4b4646e8b9cadcd5411f1d28b0"
+  version "1.6.2"
+  sha256 "56e747c35f9d2f1eed7d612b8551a6123cb2075c45bd46d493b822ee5f0d1ca6"
 
   url "https://shottr.cc/dl/Shottr-#{version}.dmg"
   name "Shottr"
   desc "Screenshot measurement and annotation tool"
   homepage "https://shottr.cc/"
 
+  # `version.json` provides a `latestVersion` field but it can reference an
+  # older version while the `package` field points to a file with a newer
+  # version. With this in mind, we extract the version from the `package` URL.
+  # Note: This regex intentionally omits the file extension, as the cask uses a
+  # dmg file but `version.json` references a pkg file.
   livecheck do
     url "https://shottr.cc/api/version.json"
-    strategy :page_match do |page|
-      JSON.parse(page)["latestVersion"]
+    regex(/Shottr[._-]v?(\d+(?:\.\d+)+)/i)
+    strategy :page_match do |page, regex|
+      JSON.parse(page)["package"][regex, 1]
     end
   end
 
