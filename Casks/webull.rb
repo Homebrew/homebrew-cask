cddea71c23444cb5ae8cc90541ae55727173be05
Patrick Linnane <patrick@linnane.io>
2023-02-23 12:57:15 -0800

    Update webull from 6.8.0 to 6.9.0 (#141746)

--- Casks/webull.rb
+++ Casks/webull.rb
@@ -1,19 +1,15 @@
 cask "webull" do
-  arch arm: "global_arm64", intel: "global_x64"
-  livecheck_arch = on_arch_conditional arm: "qt_m1_global", intel: "qt_mac_global"
+  version "6.9.0"
+  sha256 "788907cffc24106430e8fd83eeb227dba8e649919e5bd95e2795ded1be068ff7"
 
-  version "6.8.0"
-  sha256 arm:   "41219d312b3d71c634e87dcac0acb768159fcc9fd820bbdff8bd15921cf40c95",
-         intel: "30d405f4282b0981064f685dfaa38bb8cc8990771b0c8d9d024709f4824e116c"
-
-  url "https://u1sweb.webullfintech.com/us/Webull%20Desktop_#{version}_#{arch}signed.dmg",
+  url "https://u1sweb.webullfintech.com/us/Webull%20Desktop_#{version}_global_universalsigned.dmg",
       verified: "u1sweb.webullfintech.com/us/"
   name "Webull"
   desc "Desktop client for Webull Financial LLC"
   homepage "https://webull.com/"
 
   livecheck do
-    url "https://infoapi.webullfintech.com/api/operation/appver/last?platform=#{livecheck_arch}&osv=10.15"
+    url "https://infoapi.webullfintech.com/api/operation/appver/last?platform=qt_mac_global&osv=10.15"
     strategy :page_match
     regex(/Webull%20Desktop[._-](\d+(?:\.\d+)+).*?.dmg/i)
   end
