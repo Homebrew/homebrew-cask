896385e71a47c3b22c7c12750732be411d19efac
miccal <miccal.matthews@gmail.com>
2023-02-23 22:11:20 +0800

    Update freedom from 2.13.2 to 2.14.1

--- Casks/freedom.rb
+++ Casks/freedom.rb
@@ -1,6 +1,6 @@
 cask "freedom" do
-  version "2.13.2,2106"
-  sha256 "41a4be4cc0d9ff765ab4ab9d39908187ae889fb323c4aa273a365bf84e76bede"
+  version "2.14.1,2127"
+  sha256 "e3bebf3da6a6f17af3940fdf2383b4c5ccfdce1028ff89080dbd89b463bbf0f8"
 
   url "https://cdn.freedom.to/installers/updates/mac/#{version.csv.first}/Freedom.zip"
   name "Freedom"
