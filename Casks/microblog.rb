c8286365f3b559808ba69379de043fd9b7a1ecea
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-24 07:06:22 +0800

    Update microblog.rb

--- Casks/microblog.rb
+++ Casks/microblog.rb
@@ -1,16 +1,16 @@
 cask "microblog" do
-  version "2.8.1,128"
+  version "2.8.1"
   sha256 "7f5a47fee0028f70a7793e4c6d3f63b78334404bee5c21d1e82f15fd35285974"
 
-  url "https://s3.amazonaws.com/micro.blog/mac/Micro.blog_#{version.csv.first}.zip",
-      verified: "s3.amazonaws.com/micro.blog/"
+  url "https://s3.amazonaws.com/micro.blog/mac/Micro.blog_#{version}.zip",
+      verified: "s3.amazonaws.com/micro.blog/mac/"
   name "Micro.blog"
   desc "Microblogging and social networking service"
   homepage "https://help.micro.blog/t/micro-blog-for-mac/45"
 
   livecheck do
     url "https://s3.amazonaws.com/micro.blog/mac/appcast.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   auto_updates true
