db2d64a676a9259fa38425d3233ab9bb2a56c33d
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-23 22:15:45 +0800

    Update jumpshare.rb

--- Casks/jumpshare.rb
+++ Casks/jumpshare.rb
@@ -2,15 +2,15 @@ cask "jumpshare" do
   version "3.2.8,149"
   sha256 "f98ffadbd3837eaad87a32eda86d536ee32cc3bebb2852c0452388dc73b5b234"
 
-  url "https://d21hi1or3tbtjm.cloudfront.net/desktop/mac/updates/Jumpshare-#{version.csv.first}.tar.bz2",
-      verified: "d21hi1or3tbtjm.cloudfront.net/"
+  url "https://d21hi1or3tbtjm.cloudfront.net/desktop/mac/updates/Jumpshare-#{version}.tar.bz2",
+      verified: "d21hi1or3tbtjm.cloudfront.net/desktop/mac/updates/"
   name "Jumpshare"
   desc "File sharing, screen recording, and screenshot capture app"
   homepage "https://jumpshare.com/"
 
   livecheck do
     url "https://apps.jumpshare.com/desktop/mac/updates/appcast.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   depends_on macos: ">= :mojave"
