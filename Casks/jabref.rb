e238189ce19a1f3e0e1c82a7d145e44af55112d4
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-23 08:17:13 +0800

    Update jabref.rb

--- Casks/jabref.rb
+++ Casks/jabref.rb
@@ -1,9 +1,9 @@
 cask "jabref" do
-  arch arm: "._arm64", intel: ""
+  arch arm: "._arm64"
 
   version "5.9"
-  sha256 intel: "53db743600453ecca840547ddd43323c16d34bdd7ce69ce50b8f58bd1c27f3ad",
-         arm:   "b99ccab858b46ac094237070718bbc408957f4dd0a011949806045841029d0b1"
+  sha256 arm:   "b99ccab858b46ac094237070718bbc408957f4dd0a011949806045841029d0b1",
+         intel: "53db743600453ecca840547ddd43323c16d34bdd7ce69ce50b8f58bd1c27f3ad"
 
   url "https://github.com/JabRef/jabref/releases/download/v#{version}/JabRef-#{version}#{arch}.dmg",
       verified: "github.com/JabRef/jabref/"
