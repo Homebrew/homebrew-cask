a7ae56b75b06eab9d11b956b1b997fe0c46ecd5b
Daniel Sattler <daniel.sattler@freenet.de>
2023-02-23 14:01:43 +0100

    Update darktable from 4.2.0 to 4.2.1 (#141711)
    Co-authored-by: Daniel Sattler <ds@Mini-von-Daniel.fritz.box>

--- Casks/darktable.rb
+++ Casks/darktable.rb
@@ -1,9 +1,9 @@
 cask "darktable" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "4.2.0"
-  sha256 arm:   "7cbc03cc0e9a4a1769e4321815ed99384de8c063990eb5d746ffbd7599e32193",
-         intel: "89308cb7222ec0d0b539771dd90284e98b35bae4b53c8f3aa1b2feb861536b82"
+  version "4.2.1"
+  sha256 arm:   "d037a23e8b37f6971a1f2b7c4cf3e03647b168ad2fb43080761d7a307b43048d",
+         intel: "993a29685397c6e1a429d84be578da9271eefc06d2c75c10818ffc00b7d04a00"
 
   url "https://github.com/darktable-org/darktable/releases/download/release-#{version.major_minor_patch}/darktable-#{version}_#{arch}.dmg",
       verified: "github.com/darktable-org/darktable/"
