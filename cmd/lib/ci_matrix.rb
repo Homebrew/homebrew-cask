c891fd7c23d86a0502a32c4d31a7fc3d7f4838c1
Bevan Kay <email@bevankay.me>
2023-02-24 23:41:05 +1100

    ci_matrix: fix repository audit skipping

--- cmd/lib/ci_matrix.rb
+++ cmd/lib/ci_matrix.rb
@@ -113,7 +113,10 @@ module CiMatrix
       audit_args << "--new-cask" if changed_files[:added_files].include?(path)
 
       audit_exceptions = []
-      audit_exceptions << "repository" if labels.include?("ci-skip-repository")
+      if labels.include?("ci-skip-repository")
+        audit_exceptions << ["github_repository", "gitlab_repository",
+                             "bitbucket_repository"]
+      end
 
       # TODO: Replace with `except`.
       audit_args << if labels.include?("ci-skip-appcast")
