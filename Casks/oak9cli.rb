40a5969900b083f291313493a794d14e1aee4301
awright <awright@oak9.io>
2023-02-23 18:03:47 -0600

    Update URL from blob to raw

--- Casks/oak9cli.rb
+++ Casks/oak9cli.rb
@@ -2,7 +2,7 @@ cask "oak9cli" do
   version "0.11.16"
   sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://github.com/oak9io/oak9.cli-public/blob/master/binaries/iac-mac_os-#{version}.zip",
+  url "https://github.com/oak9io/oak9.cli-public/raw/master/binaries/iac-mac_os-#{version}.zip",
       verified: "github.com/oak9io/oak9.cli-public/"
   name "oak9 CLI"
   desc "Component of oak9's security compliance SaaS product"
