c476407a24eeef9d912a83a7310b2faf818cdded
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-25 06:19:36 +0800

    Update fx-cast-bridge.rb

--- Casks/fx-cast-bridge.rb
+++ Casks/fx-cast-bridge.rb
@@ -2,11 +2,11 @@ cask "fx-cast-bridge" do
   arch arm: "arm64", intel: "x64"
 
   version "0.3.0"
-  sha256 intel: "f24e347fd240d5e74b488c83aee8c939b5abf7498dd047b4d3cdb5912a71e8aa",
-         arm:   "2e8dd783be80c21b5b25615284046df7ea7322dba80e96f09c93a329f3f6b6fb"
+  sha256 arm:   "2e8dd783be80c21b5b25615284046df7ea7322dba80e96f09c93a329f3f6b6fb",
+         intel: "f24e347fd240d5e74b488c83aee8c939b5abf7498dd047b4d3cdb5912a71e8aa"
 
   url "https://github.com/hensm/fx_cast/releases/download/v#{version}/fx_cast_bridge-#{version}-#{arch}.pkg",
-      verified: "https://github.com/hensm/fx_cast"
+      verified: "github.com/hensm/fx_cast/"
   name "fx_cast Bridge"
   desc "Bridge helper for fx_cast Firefox extension to enable Chromecast support"
   homepage "https://hensm.github.io/fx_cast/"
