91fd95f0ce504b3c06503d233cdb4d91e1ee4185
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-24 15:52:20 +0800

    Update dingtalk.rb

--- Casks/dingtalk.rb
+++ Casks/dingtalk.rb
@@ -1,27 +1,27 @@
 cask "dingtalk" do
   arch arm: "arm64", intel: "x86"
-  choice = on_arch_conditional arm: "qd=2022mac-m1", intel: ""
+  folder = on_arch_conditional arm: "M1-Beta/"
+  linkid = on_arch_conditional arm: "qd=2022mac-m1"
 
   on_intel do
     version "7.0.10.5_28436509"
     sha256 "8311a41e392f170489a8c58aac3cd0cc555de8912f58108bc1911419ce5f7fe6"
-    url "https://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/DingTalk_v#{version}_#{arch}.dmg"
   end
   on_arm do
     version "7.0.12.5_28488321"
     sha256 "11acb4fc794ee7349d2715681e719ee0f43e9d95658f35b7198ccb90dd59442c"
-    url "https://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/M1-Beta/DingTalk_v#{version}_#{arch}.dmg"
   end
 
+  url "https://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/#{folder}DingTalk_v#{version}_#{arch}.dmg"
   name "DingTalk"
   name "钉钉"
   desc "Teamwork app by Alibaba Group"
   homepage "https://www.dingtalk.com/"
 
   livecheck do
-    url "https://www.dingtalk.com/mac/d/#{choice}"
+    url "https://www.dingtalk.com/mac/d/#{linkid}"
     strategy :header_match
-    regex(/DingTalk_v([\d._]+)_#{arch}\.dmg/i)
+    regex(/DingTalk[._-]v?(\d+(?:.\d+)+)[._-]#{arch}\.dmg/i)
   end
 
   auto_updates true
