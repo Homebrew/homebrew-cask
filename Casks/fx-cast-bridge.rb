6cf655707079d6a30779a9c9d34b3082ef8c7318
Colin Dean <colin.dean@target.com>
2023-02-24 14:32:41 -0500

    fx-cast-bridge: Run style

--- Casks/fx-cast-bridge.rb
+++ Casks/fx-cast-bridge.rb
@@ -1,14 +1,14 @@
 cask "fx-cast-bridge" do
-  version "0.3.0"
   arch arm: "arm64", intel: "x64"
+
+  version "0.3.0"
   sha256 intel: "f24e347fd240d5e74b488c83aee8c939b5abf7498dd047b4d3cdb5912a71e8aa",
-         arm: "2e8dd783be80c21b5b25615284046df7ea7322dba80e96f09c93a329f3f6b6fb"
+         arm:   "2e8dd783be80c21b5b25615284046df7ea7322dba80e96f09c93a329f3f6b6fb"
 
   url "https://github.com/hensm/fx_cast/releases/download/v#{version}/fx_cast_bridge-#{version}-#{arch}.pkg",
-    verified: "https://github.com/hensm/fx_cast"
-
+      verified: "https://github.com/hensm/fx_cast"
   name "fx_cast Bridge"
-  desc "The bridge program that helps the fx_cast Firefox extension enable Chromecast support"
+  desc "Bridge helper for fx_cast Firefox extension to enable Chromecast support"
   homepage "https://hensm.github.io/fx_cast/"
 
   livecheck do
@@ -17,8 +17,8 @@ cask "fx-cast-bridge" do
       # look for the latest bridge release since some releases are bridge-only
       # or extension-only, falling back to version if none is found
       found = page.scan(/href=.*?fx_cast_bridge[._-]?(\d{4}.\d{4}.\d{4})[._-]\w{3,5}\.pkg/i)
-        .map { |match| match&.first }
-      !found.empty? ? found : version
+                  .map { |match| match&.first }
+      found.empty? ? version : found
     end
   end
 
