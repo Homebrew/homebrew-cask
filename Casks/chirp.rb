cbb891a20ce7443ec03d0fdfeb339a3dfe6b8304
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-24 19:35:00 +0800

    Update chirp.rb

--- Casks/chirp.rb
+++ Casks/chirp.rb
@@ -8,8 +8,8 @@ cask "chirp" do
   homepage "https://chirp.danplanet.com/projects/chirp/wiki/Home"
 
   livecheck do
-    url "https://trac.chirp.danplanet.com/chirp_next/next-20230224/SHA1SUM"
-    regex(/chirp[._-]next[._-]v?(\d+(?:\.\d+)*)\.app\.zip/i)
+    url "https://trac.chirp.danplanet.com/chirp_next/"
+    regex(/next[._-]v?(\d+(?:\.\d+)*)/i)
   end
 
   app "CHIRP.app"
