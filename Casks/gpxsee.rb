3fd5cf7367d203aa77b51218a652a0abc369489b
Patrick Linnane <patrick@linnane.io>
2023-02-25 10:43:53 -0800

    Update gpxsee from 12.0 to 12.1

--- Casks/gpxsee.rb
+++ Casks/gpxsee.rb
@@ -1,6 +1,6 @@
 cask "gpxsee" do
-  version "12.0"
-  sha256 "dc58a05820ad8214249859b8926ab5d9bf86318f386bf11f5226e456088f98c3"
+  version "12.1"
+  sha256 "02d10f3672507007fee97a0aea54ba9d0f278aaa7726cd292e870215dcad6352"
 
   url "https://downloads.sourceforge.net/gpxsee/Mac%20OS%20X/GPXSee-#{version}.dmg",
       verified: "sourceforge.net/gpxsee/Mac%20OS%20X/"
