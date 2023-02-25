d008c0757c637bd37b11d4253a5f51ea6bddaf5a
Patrick Linnane <patrick@linnane.io>
2023-02-24 22:50:14 -0800

    Update buckets from 0.68.0 to 0.69.0 (#141832)

--- Casks/buckets.rb
+++ Casks/buckets.rb
@@ -1,6 +1,6 @@
 cask "buckets" do
-  version "0.68.0"
-  sha256 "54d86f8a8661ae7b1bbeb077af89643f39f2764710401fd60bd9857455237feb"
+  version "0.69.0"
+  sha256 "bd783ebd3f76d5e517886c802e2f0eaa103744704e35978a91d364c25ed00900"
 
   url "https://github.com/buckets/application/releases/download/v#{version}/Buckets-#{version}.dmg",
       verified: "github.com/buckets/application/"
