6568cc269ba67e7ac43d8877571b3241cb707409
Patrick Linnane <patrick@linnane.io>
2023-02-24 22:48:55 -0800

    Update datadog-agent from 7.42.1-1 to 7.43.0-1 (#141853)

--- Casks/datadog-agent.rb
+++ Casks/datadog-agent.rb
@@ -1,6 +1,6 @@
 cask "datadog-agent" do
-  version "7.42.1-1"
-  sha256 "7f46c7d427e6756ebef8fb6823bc439b7828909260261e3048f1047bf4b4782f"
+  version "7.43.0-1"
+  sha256 "03810da365ce2a6f13cdb7bf2ec66e29cb9bd0d90ca2bf44f2a70caa90fa045e"
 
   url "https://s3.amazonaws.com/dd-agent/datadog-agent-#{version}.dmg",
       verified: "s3.amazonaws.com/dd-agent/"
