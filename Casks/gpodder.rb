a71cd5a61942caa256559083f5c0de77873076fe
Patrick Linnane <patrick@linnane.io>
2023-02-25 10:25:20 -0800

    Update gpodder from 3.11.0 to 3.11.1

--- Casks/gpodder.rb
+++ Casks/gpodder.rb
@@ -1,6 +1,6 @@
 cask "gpodder" do
-  version "3.11.0"
-  sha256 "e0d8bb410f6ba1c6fd526acfd8461cc9c798061e9c62cf65ffc2c88115224175"
+  version "3.11.1"
+  sha256 "3b8eb0b27a8fa3bcd27e6a57f2a6c7a8e264e50175da5e0d95c518f565bb9c05"
 
   url "https://github.com/gpodder/gpodder/releases/download/#{version}/macOS-gPodder-#{version}.zip",
       verified: "github.com/gpodder/gpodder/"
