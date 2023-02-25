d184df97b1f3593661e48370add842471d95fac6
Patrick Linnane <patrick@linnane.io>
2023-02-25 10:09:22 -0800

    Update flycast from 2.0 to 2.1

--- Casks/flycast.rb
+++ Casks/flycast.rb
@@ -1,8 +1,8 @@
 cask "flycast" do
-  version "2.0"
-  sha256 "7e2d651de3f3a27c4705cbab093ae7013f0164b8bad67961d1bfdf2d01c464f0"
+  version "2.1"
+  sha256 "e7462614af75d9fa36c254ffd73dc66dab2ea09b34193b6066e92579d16da8c3"
 
-  url "https://github.com/flyinghead/flycast/releases/download/v#{version}/flycast-macOS-#{version}.zip"
+  url "https://github.com/flyinghead/flycast/releases/download/V#{version}/flycast-macOS-#{version}.zip"
   name "Flycast"
   desc "Dreamcast, Naomi and Atomiswave emulator"
   homepage "https://github.com/flyinghead/flycast"
