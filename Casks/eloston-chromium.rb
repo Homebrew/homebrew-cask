36f6fd95e1b2049f0fcb11c909738c63a76ff35b
Patrick Linnane <patrick@linnane.io>
2023-02-24 23:12:46 -0800

    Update eloston-chromium from 110.0.5481.77-1.1 to 110.0.5481.100-1.1 (#141875)
    * Update eloston-chromium from 110.0.5481.77-1.1 to 110.0.5481.100-1.1
    
    * Update eloston-chromium.rb
    
    ---------
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/eloston-chromium.rb
+++ Casks/eloston-chromium.rb
@@ -2,12 +2,12 @@ cask "eloston-chromium" do
   arch arm: "arm64", intel: "x86-64"
 
   on_intel do
-    version "110.0.5481.77-1.1,1675982498"
-    sha256 "8f2e469052381d155792b70d328d56ed637f767daa6ce3cd2e0a47a1a5bc9332"
+    version "110.0.5481.100-1.1,1676834273"
+    sha256 "973ca448be3f74a2c726cc78c349279f556e8b732c5c0683685b19115f3c27a7"
   end
   on_arm do
-    version "110.0.5481.77-1.1,1676295113"
-    sha256 "38f1f924c67f2fc07108b1993742d4794fe53758784a836a9fbf76e52855006f"
+    version "110.0.5481.100-1.1,1676884754"
+    sha256 "d4e8258a9e91618d2da82968285498f035f31e4912646479c225b1a4ae43675c"
   end
 
   url "https://github.com/kramred/ungoogled-chromium-macos/releases/download/#{version.csv.first}_#{arch}__#{version.csv.second}/ungoogled-chromium_#{version.csv.first}_#{arch}-macos.dmg",
