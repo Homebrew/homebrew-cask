41c5a44fd013cd47c12f41b4ec0500a64960863a
Patrick Linnane <patrick@linnane.io>
2023-02-25 10:57:19 -0800

    Update mochi-diffusion from 2.4 to 2.4.1

--- Casks/mochi-diffusion.rb
+++ Casks/mochi-diffusion.rb
@@ -1,6 +1,6 @@
 cask "mochi-diffusion" do
-  version "2.4"
-  sha256 "6d6e0cbfe2b2c8e3ee91cb306a3debda7fe4e63cb67d93c64cc51f6a3c964ea7"
+  version "2.4.1"
+  sha256 "c1e9a5646cde7a942fae72ef683d949d51f79b2304b19b126384a83a10b0ad67"
 
   url "https://github.com/godly-devotion/MochiDiffusion/releases/download/v#{version}/MochiDiffusion_v#{version}.dmg"
   name "Mochi Diffusion"
