2cc142cac073b87e7b822d2446fcf7788a93955c
Patrick Linnane <patrick@linnane.io>
2023-02-25 14:56:01 -0800

    Update laravel-kit from 2.0.7 to 2.0.8 (#141930)

--- Casks/laravel-kit.rb
+++ Casks/laravel-kit.rb
@@ -1,11 +1,11 @@
 cask "laravel-kit" do
   arch arm: "-arm64", intel: ""
 
-  version "2.0.7"
-  sha256 arm:   "f4e91f92e5e85609567c173812a7dcfdae6c1b1103dca71a9b56890fa27e1198",
-         intel: "7f3073655b32191360c378897a819fe4560ffad1e784469bad7a9089d0b31ca4"
+  version "2.0.8"
+  sha256 arm:   "f5cc20ce507ca6bef4e6de99d7e00aee12f55d9ef01dab85be7f1a6074b8a045",
+         intel: "fb748da7a99911a7b182ac7958862dfa1a9e036ec82529a2fb65e4b768c72b1a"
 
-  url "https://github.com/tmdh/laravel-kit/releases/download/v#{version}/laravel-kit-#{version}#{arch}.dmg",
+  url "https://github.com/tmdh/laravel-kit/releases/download/v#{version}/Laravel-Kit-#{version}#{arch}-mac.zip",
       verified: "github.com/tmdh/laravel-kit/"
   name "Laravel Kit"
   desc "Desktop Laravel admin panel app"
