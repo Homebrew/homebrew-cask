0e56a961eb4ea96ff8f8760afe17e70b9d386d71
Patrick Linnane <patrick@linnane.io>
2023-02-24 22:51:17 -0800

    Update apifox from 2.2.23 to 2.2.24 (#141813)

--- Casks/apifox.rb
+++ Casks/apifox.rb
@@ -1,9 +1,9 @@
 cask "apifox" do
   arch arm: "-macOS-arm64"
 
-  version "2.2.23"
-  sha256 arm:   "991e7719fc250896e1edc4cf62641314f05b509d9e521ddce61a10abf77b3e32",
-         intel: "28223dc1dbad173a6cc685d08e910a156f17d0ae36573ac3bf944e1402bc9010"
+  version "2.2.24"
+  sha256 arm:   "7508c8987c391e89c8b3f61e730aefd06057673317b75231d2b61c5b39b49bc8",
+         intel: "9dd35d20b1f764f176b18d498879f3be2b6c4c6d442b026a4d7ed9a089a041b0"
 
   url "https://cdn.apifox.cn/download/#{version}/Apifox#{arch}-#{version}.dmg"
   name "Apifox"
