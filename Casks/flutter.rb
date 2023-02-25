958d6d15834843d4824d5c7b36321c05dbca57ef
Patrick Linnane <patrick@linnane.io>
2023-02-25 10:14:00 -0800

    Update flutter from 3.7.3 to 3.7.5

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -1,9 +1,9 @@
 cask "flutter" do
   arch arm: "_arm64_", intel: "_"
 
-  version "3.7.3"
-  sha256 arm:   "6b4eda9371094ca2cdaae944f91a0a95d1f71803dcae0d01f899edf8aa4f58a1",
-         intel: "41032963628ef651a81833af202ca6b636b49411c3710030898ed4277d636595"
+  version "3.7.5"
+  sha256 arm:   "aec66d79633dafa37d5fd00bcec366993921e802050728c5b2e0c3478fd2076f",
+         intel: "653961c303d34eed4ca976b814ee3f4a15078fa743e454c77624a5fe3299c5bd"
 
   url "https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos#{arch}#{version}-stable.zip",
       verified: "storage.googleapis.com/flutter_infra_release/releases/stable/macos/"
