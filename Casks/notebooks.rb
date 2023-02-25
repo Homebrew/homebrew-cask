ccac95ea934ab0216856cc978256dbee090fcc3a
Patrick Linnane <patrick@linnane.io>
2023-02-25 14:53:56 -0800

    Update notebooks from 3.1 to 3.1.1 (#141964)

--- Casks/notebooks.rb
+++ Casks/notebooks.rb
@@ -1,6 +1,6 @@
 cask "notebooks" do
-  version "3.1"
-  sha256 "34bb68a4b0395340e4d4f0275b494b2798db5ac86bebc310d7408295ba8e746b"
+  version "3.1.1"
+  sha256 "7679d76760432fd5d3efddd36d20716a1cf0d4a5659d0b8ec94a5606ee70d4b3"
 
   url "https://www.notebooksapp.com/Download/macOS/v#{version.major}/Notebooks.dmg"
   name "Notebooks"
