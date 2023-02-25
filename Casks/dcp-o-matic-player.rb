fa57d47ab5400fff7832a4be26c00aca7701329a
Patrick Linnane <patrick@linnane.io>
2023-02-24 20:50:03 -0800

    Update dcp-o-matic-player from 2.16.42 to 2.16.44

--- Casks/dcp-o-matic-player.rb
+++ Casks/dcp-o-matic-player.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-player" do
-  version "2.16.42"
-  sha256 "69b4e730b31478358c27aad7db94afa0012135f7a25d53cc03244036a7f70ef2"
+  version "2.16.44"
+  sha256 "a8fb5e4b0e208e0e0e1ccca4fc38f77bac5252bd90ac648381c1eca5fb38ee2f"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-player&version=#{version}"
   name "DCP-o-matic Player"
