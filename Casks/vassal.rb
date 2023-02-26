6899497a202bb6dc0ffcddc27109fa86a7176f91
Amory Meltzer ☀ <Amorymeltzer@gmail.com>
2023-02-26 00:49:04 -0500

    Update vassal from 3.6.11 to 3.6.13 (#141990)

--- Casks/vassal.rb
+++ Casks/vassal.rb
@@ -1,9 +1,9 @@
 cask "vassal" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "3.6.11"
-  sha256 arm:   "b7f419d922ea3a99b6c3188bcaf0a553923d085db73a481393a19eaf7fd4fd82",
-         intel: "35cd3e3e936ff59330e956ac8041b8096a5d1fba8436b0bcb434ca0adfa769bb"
+  version "3.6.13"
+  sha256 arm:   "b18e5f2afc6e1232c9a994fae400e5504c45532cd3c0711ed51aa7c11f645e16",
+         intel: "4b4659f98a9f87168261a4226fac2781814787c62bb501cb5dd6420028e4a6c2"
 
   url "https://github.com/vassalengine/vassal/releases/download/#{version}/VASSAL-#{version}-macos-#{arch}.dmg",
       verified: "github.com/vassalengine/vassal/"
