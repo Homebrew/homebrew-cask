6571ec448c99b1ccc039edfd8fefe51a307d0b55
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-26 19:35:31 +0800

    Update libreoffice.rb

--- Casks/libreoffice.rb
+++ Casks/libreoffice.rb
@@ -18,7 +18,7 @@ cask "libreoffice" do
   end
 
   conflicts_with cask: "homebrew/cask-versions/libreoffice-still"
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :mojave"
 
   app "LibreOffice.app"
   binary "#{appdir}/LibreOffice.app/Contents/MacOS/gengal"
