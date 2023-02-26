6839e11e59b570eb97617573991b0d4ee618375c
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-02-26 17:17:45 -0500

    Update browserosaurus from 20.0.2 to 20.1.0

--- Casks/browserosaurus.rb
+++ Casks/browserosaurus.rb
@@ -1,9 +1,9 @@
 cask "browserosaurus" do
   arch arm: "arm64", intel: "x64"
 
-  version "20.0.2"
-  sha256 arm:   "d3af96f45bfa039b9b376ba36a4cd72c214908010b6616a57aded43cf682bce3",
-         intel: "56ec3510f7ef48d1b21f8766d59bebb4291d5d427ec2c8ce5ce9e43231beac8f"
+  version "20.1.0"
+  sha256 arm:   "f49b8b732a297c7254c93b1fa967b5f0028570bd875d037d343c67fc942cb0d9",
+         intel: "fd4d3a94de59833d08ddbe282967962f45040d04b4426be2cfc24d672bdc7e31"
 
   url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-darwin-#{arch}-#{version}.zip"
   name "Browserosaurus"
