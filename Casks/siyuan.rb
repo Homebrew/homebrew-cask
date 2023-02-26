357c9f01c6a49f46007ae65ab9bacf8baf054f28
Amory Meltzer <Amorymeltzer@gmail.com>
2023-02-26 18:57:29 -0500

    Update siyuan from 2.7.4 to 2.7.6

--- Casks/siyuan.rb
+++ Casks/siyuan.rb
@@ -1,9 +1,9 @@
 cask "siyuan" do
   arch arm: "-arm64"
 
-  version "2.7.4"
-  sha256 arm:   "6cbcc7b3a878bf7a93485e30e878d30c1a6e888d4fdb38c532331352aaa03a73",
-         intel: "224296c09a505ebe86094cdf3e733dd706e36856b1efe5e9515129e2f73b395b"
+  version "2.7.6"
+  sha256 arm:   "ace9a3e179391669118be7885d044c6b8abb26ea7b2199f00566fc2f1277fa25",
+         intel: "dc086c1a30ee457a4a2371c34804a14a010627c4636f9a32e5e8cc2610ad1757"
 
   url "https://github.com/siyuan-note/siyuan/releases/download/v#{version}/siyuan-#{version}-mac#{arch}.dmg"
   name "SiYuan"
