7051440467e10a904057fd18da43eccf4856a5c2
logwet <logwet@users.noreply.github.com>
2023-02-26 23:28:33 +1100

    Updated Swift Quit Cask to use Github Releases versioning (#142000)
    * Updated Swift Quit Cask
    
    * Update swift-quit.rb
    
    ---------
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/swift-quit.rb
+++ Casks/swift-quit.rb
@@ -1,20 +1,18 @@
 cask "swift-quit" do
   version "1.3"
-  sha256 :no_check
+  sha256 "4809143dc46da3b4f968eb58f354250a821afff4611e8c807717bd039c8013e7"
 
-  url "https://swiftquit.com/downloads/Swift%20Quit.zip"
+  url "https://github.com/onebadidea/swiftquit/releases/download/v#{version}/Swift.Quit.zip",
+      verified: "github.com/onebadidea/swiftquit/"
   name "Swift Quit"
-  desc "Quit apps when closing the last window"
+  desc "Enable Windows-like program quitting when all windows are closed"
   homepage "https://swiftquit.com/"
-
-  livecheck do
-    url :url
-    strategy :extract_plist
-  end
 
   depends_on macos: ">= :big_sur"
 
   app "Swift Quit.app"
+
+  uninstall quit: "onebadidea.Swift-Quit"
 
   zap trash: "~/Library/Preferences/onebadidea.Swift-Quit.plist"
 end
