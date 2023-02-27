ddc1d49bc7239f8c6f28fe05029a7413e1461b89
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-28 05:49:04 +0800

    Update tableflip.rb

--- Casks/tableflip.rb
+++ Casks/tableflip.rb
@@ -1,8 +1,8 @@
 cask "tableflip" do
-  version "1.4.1,71"
+  version "1.4.1"
   sha256 "aa9e26c15dbaf840f9bdb75b47e59e22276a7ec3143491f6988f09cb82a6a716"
 
-  url "https://update.christiantietze.de/tableflip/v#{version.major}/TableFlip-v#{version.csv.first}.zip",
+  url "https://update.christiantietze.de/tableflip/v#{version.major}/TableFlip-v#{version}.zip",
       verified: "update.christiantietze.de/tableflip/"
   name "TableFlip"
   desc "App to edit markdown files in place"
@@ -10,7 +10,7 @@ cask "tableflip" do
 
   livecheck do
     url "https://update.christiantietze.de/tableflip/v#{version.major}/release.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   auto_updates true
