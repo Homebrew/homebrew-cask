0cfabad0eabddca7ad4d2fe752746dc1352c847a
miccal <miccal.matthews@gmail.com>
2023-02-28 05:46:58 +0800

    Update screens from 4.12.7 to 4.12.8

--- Casks/screens.rb
+++ Casks/screens.rb
@@ -1,6 +1,6 @@
 cask "screens" do
-  version "4.12.7,1674762178"
-  sha256 "c709efd79d44d75e61ecfee9f48b2d7c2cb5b6f756d9aebe03654306159300eb"
+  version "4.12.8,1677528436"
+  sha256 "c5efda4c9d94a61f57118bb5e237fc795516c316f156bfefb624ed74fcb8dbd4"
 
   url "https://updates.edovia.com/com.edovia.screens#{version.major}.mac/Screens_#{version.csv.first}b#{version.csv.second}.zip"
   name "Screens"
