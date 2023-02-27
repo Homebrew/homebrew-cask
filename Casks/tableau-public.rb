8d5fe7531b731f16976f39a21e6b58e62ae86f9c
Anton Möller <anton@esportal.com>
2023-02-27 18:32:46 +0100

    Update tableau-public from 2022.4.1 to 2022.4.1

--- Casks/tableau-public.rb
+++ Casks/tableau-public.rb
@@ -1,6 +1,6 @@
 cask "tableau-public" do
-  version "2022.4.0"
-  sha256 "5f1a960cb8cd8ecc404fae3e0e6d3857ad22e554bbe9fabc75148b27041fc588"
+  version "2022.4.1"
+  sha256 "d359bcafe4e172b7ab2c60c2ee4a0651865423201b8c3f3bc826b1f9b54dc5a9"
 
   url "https://downloads.tableau.com/public/TableauPublic-#{version.dots_to_hyphens}.dmg"
   name "Tableau Public"
