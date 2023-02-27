8a112022bb1c70c29d48761ae5f7d8ad0495fcdc
miccal <miccal.matthews@gmail.com>
2023-02-28 05:35:42 +0800

    Update bike from 1.9,103 to 1.9,106

--- Casks/bike.rb
+++ Casks/bike.rb
@@ -1,6 +1,6 @@
 cask "bike" do
-  version "1.9,103"
-  sha256 "7f1988f0ac8f726729585ed8ba2428055a23c5ac64bc6c7ec2306fccad6c6846"
+  version "1.9,106"
+  sha256 "f4205e9177e8325d52100c46ca2a332996f859bdfd085a49009c4b2402730b0a"
 
   url "https://www.hogbaysoftware.com/bike/releases/#{version.csv.second}.dmg"
   name "Bike"
