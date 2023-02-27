14ea10d28dbb3c8916de69e3bceffad5398a8fda
Amory Meltzer <Amorymeltzer@gmail.com>
2023-02-26 19:17:50 -0500

    xit: Bump to 1.0b17
    Now depends on Monterey

--- Casks/xit.rb
+++ Casks/xit.rb
@@ -1,8 +1,11 @@
 cask "xit" do
-  version "1.0b16"
-  sha256 "0e9897d16a5a613a4da9861c907e1f0922df446cc88628e13271793a6c94b229"
+  arch arm: "-arm"
 
-  url "https://github.com/Uncommon/Xit/releases/download/#{version}/Xit.zip"
+  version "1.0b17"
+  sha256 arm:   "6b44a102747811e0373737e24104902dc1d8aaf8801fc83c0e77622bab1b12fa",
+         intel: "7b25b255a1af84261321b40ac08c31311bbff73a13be2c523862863be0bba620"
+
+  url "https://github.com/Uncommon/Xit/releases/download/#{version}/Xit#{arch}.#{version}.zip"
   name "Xit"
   desc "GUI for the git version control system"
   homepage "https://github.com/Uncommon/Xit"
@@ -12,7 +15,7 @@ cask "xit" do
     regex(/^v?(\d+(?:\.\d+)+(?:b\d+)?)$/i)
   end
 
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= :monterey"
 
   app "Xit.app"
 
