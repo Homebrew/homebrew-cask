f1ea9e13b545439f82106e0c429a019cad44922e
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-28 17:52:28 +0800

    Update ximalaya.rb

--- Casks/ximalaya.rb
+++ Casks/ximalaya.rb
@@ -4,7 +4,7 @@ cask "ximalaya" do
 
   url "https://nativedl.pcdn.xmcdn.com/storages/#{version.csv.third}-audiofreehighqps/#{version.csv.fourth}/#{version.csv.fifth}/#{version.csv.second}.dmg?ct=application/octet-stream&filename=Ximalaya_#{version.csv.first}_x64_c_99",
       verified: "nativedl.pcdn.xmcdn.com/storages/"
-  name "ximalaya"
+  name "Ximalaya"
   name "喜马拉雅"
   desc "Platform for podcasting and audio-sharing"
   homepage "https://www.ximalaya.com/"
@@ -12,7 +12,7 @@ cask "ximalaya" do
   livecheck do
     url "https://www.ximalaya.com/down/lite/v2?client=mac&channelId=99"
     strategy :header_match do |headers|
-      match = headers["location"].match(%r{storages/(.+)-aud.*?/(.+)/(.+)/(.+)\.dmg.*?Ximalaya[._-](\d+(?:\.\d+)+)}i)
+      match = headers["location"].match(%r{storages/(.+)[._-]aud.*?/(.+)/(.+)/(.+)\.dmg.*?Ximalaya[._-](\d+(?:\.\d+)+)}i)
       next if match.blank?
 
       "#{match[5]},#{match[4]},#{match[1]},#{match[2]},#{match[3]}"
@@ -26,6 +26,6 @@ cask "ximalaya" do
     "~/Library/Application Support/Ximalaya SetUp",
     "~/Library/Logs/喜马拉雅",
     "~/Library/Preferences/com.gemd.iting.plist",
-    "~/Library/Saved Application State/com.gemd.iting.savedState"
+    "~/Library/Saved Application State/com.gemd.iting.savedState",
   ]
 end
