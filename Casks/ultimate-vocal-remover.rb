f0d2dfd5ee830ae7ad933f419fe8b63ea3b689cd
Tamar <hamlynt@gmail.com>
2023-02-28 17:56:55 +1100

    Simplified ultimate vocal remover desc stanza

--- Casks/ultimate-vocal-remover.rb
+++ Casks/ultimate-vocal-remover.rb
@@ -7,7 +7,7 @@ cask "ultimate-vocal-remover" do
 
   url "https://github.com/Anjok07/ultimatevocalremovergui/releases/download/v#{version}/Ultimate_Vocal_Remover_v#{version.major_minor.dots_to_underscores}_MacOS_#{arch}.dmg"
   name "Ultimate Vocal Remover"
-  desc "Uses state-of-the-art source separation models to remove vocals from audio files"
+  desc "Removes vocals from audio files"
   homepage "https://github.com/Anjok07/ultimatevocalremovergui/"
 
   depends_on macos: ">= :catalina"
