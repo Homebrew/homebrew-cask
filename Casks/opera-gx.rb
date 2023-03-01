ddd68907f05b8b75764c6f34ecd181309c9c02d1
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-01 08:06:24 -0500

    Update opera-gx from 95.0.4635.54 to 95.0.4635.71

--- Casks/opera-gx.rb
+++ Casks/opera-gx.rb
@@ -1,6 +1,6 @@
 cask "opera-gx" do
-  version "95.0.4635.54"
-  sha256 "7f4e21008bbeb591544a0046c9bd680c8cc63cfee0c8a8e4cb23c725bf6f558a"
+  version "95.0.4635.71"
+  sha256 "b60c21b7eee9d64df63ad20efb3f8e215f0cac7900e23e8acf77bda1739d9a92"
 
   url "https://get.geo.opera.com/pub/opera_gx/#{version}/mac/Opera_GX_#{version}_Setup.dmg"
   name "Opera GX"
