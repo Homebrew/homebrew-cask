48f199a47a4f0993921fc37870339bdbd439fde2
John Benninghoff <jbenninghoff@mac.com>
2023-03-01 07:43:43 -0600

    Update webex-meetings from 2302.1604.4303.2 to 2302.2206.4303.3

--- Casks/webex-meetings.rb
+++ Casks/webex-meetings.rb
@@ -1,6 +1,6 @@
 cask "webex-meetings" do
   on_intel do
-    version "2302.1604.4303.2"
+    version "2302.2206.4303.3"
     sha256 :no_check
 
     url "https://akamaicdn.webex.com/client/webexapp.dmg"
