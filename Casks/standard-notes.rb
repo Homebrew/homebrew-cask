297c58336332e6d3b286fc6d593aa81e63bdbadb
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2023-03-01 23:13:26 +0800

    Update standard-notes from 3.150.0 to 3.150.7

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -1,9 +1,9 @@
 cask "standard-notes" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.150.0"
-  sha256 arm:   "e2814dba4d6952fda7be337b3a8abbf78de776ebe4aebeefefd307b4fd5b69fd",
-         intel: "fa19d663a0c1b6dbb38d21219f002c25e7c7be19cd1c473abfb5a12f0a6fb1ec"
+  version "3.150.7"
+  sha256 arm:   "c5d760c8f9e8128fc7d19b82077b820617f8667a537dc2c5ee7ad8a235afda02",
+         intel: "28765d13c0d13b19c184f57d5bbaf46b2a6547d2171686ab4d350bce3f71d9f0"
 
   url "https://github.com/standardnotes/app/releases/download/%40standardnotes%2Fdesktop%40#{version}/standard-notes-#{version}-mac-#{arch}.zip",
       verified: "github.com/standardnotes/app/"
