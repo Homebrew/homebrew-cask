be1e033c8fda34e8901bf5d0364c99567b28408f
Bevan Kay <email@bevankay.me>
2023-03-02 15:24:03 +1100

    Update reaper from 6.76 to 6.77

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,12 +1,12 @@
 cask "reaper" do
-  version "6.76"
+  version "6.77"
 
   on_mojave :or_older do
-    sha256 "3e78b98ab78a07fa5c1995b4d1b96e636e002023c3cec94c8ffe0e13510c5ccf"
+    sha256 "bc1c5ef863345345007c8b21d3972647ad458a753d22d1decf521d7efdb5ca68"
     url "https://dlcf.reaper.fm/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64.dmg"
   end
   on_catalina :or_newer do
-    sha256 "b60ee5a039d4c00b1bc753f2ecece56ba84cc638dd8458064a0e1ef80672e0ae"
+    sha256 "6117dabcd61c0546dae05173ae02ce12249f69a7ecd5b3e9ed741ddd87d1b447"
     url "https://dlcf.reaper.fm/#{version.major}.x/reaper#{version.major_minor.no_dots}_universal.dmg"
   end
 
