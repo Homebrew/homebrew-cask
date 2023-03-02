58ad9dc3d1c348301d725b22165778c3c7af108e
Bevan Kay <email@bevankay.me>
2023-03-03 10:22:05 +1100

    Update figma from 116.6.3 to 116.7.6 (#142219)

--- Casks/figma.rb
+++ Casks/figma.rb
@@ -1,9 +1,9 @@
 cask "figma" do
   arch arm: "mac-arm", intel: "mac"
 
-  version "116.6.3"
-  sha256 arm:   "4ebf3e324a5c87aa6464b499cdd35f5cb1281ddb09a03e92f3c65a8836585918",
-         intel: "a1b03c1924084feaa715b022c2323ff4c86a6cb094284baf6f65d4bffd96aa89"
+  version "116.7.6"
+  sha256 arm:   "0422dc5fd01bf3e2e23980d013a23fd53487b0319d2b592857e08ad227c417ba",
+         intel: "792a620de02b925a08dfc964445e40bf04bf3e1ee173cf479ce09b71b2ce69cd"
 
   url "https://desktop.figma.com/#{arch}/Figma-#{version}.zip"
   name "Figma"
