37341648fbe3b26c2ac3509e10e3c3b600163769
Yizun Wang <64831499+HuaDeity@users.noreply.github.com>
2023-03-02 10:54:58 +0800

    Update menubarx from 1.5.9 to 1.6.0 (#142180)

--- Casks/menubarx.rb
+++ Casks/menubarx.rb
@@ -1,6 +1,6 @@
 cask "menubarx" do
-  version "1.5.9"
-  sha256 "a716541b088cf4a80e958c8c400f81e3367424c85a01220ec28ec2176198dd86"
+  version "1.6.0"
+  sha256 "c4065617b5fe0f50b538bc24cb5900aa69d4eb47888882c4a8051fb7fd43b84f"
 
   url "https://menubarx-1251679148.file.myqcloud.com/download/MenubarX-#{version}.dmg",
       verified: "menubarx-1251679148.file.myqcloud.com/download/"
