8b42873a3212634f021f0f0515b295838f46742e
Giovanni Merlino <giovanni.merlino@unime.it>
2023-03-02 02:53:19 +0100

    Update cryptomator from 1.6.17 to 1.7.0 (#142178)
    * Update cryptomator from 1.6.17 to 1.7.0
    
    * Update cryptomator.rb
    
    ---------
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/cryptomator.rb
+++ Casks/cryptomator.rb
@@ -1,21 +1,15 @@
 cask "cryptomator" do
-  arch arm:   "-arm64",
-       intel: "-x64"
+  arch arm: "-arm64", intel: "-x64"
 
-  version "1.6.17"
-  sha256 arm:   "9b98791303bc310cbbc78b69dc4cffa1b3d72a860ccf84bf2d7d396d391d4cce",
-         intel: "9ca0e230650b75b7ae424ffad8aaf98ef131facf1ca524b709f497c768e0727d"
+  version "1.7.0"
+  sha256 arm:   "c1e6620f64ed2605b8747c07aecca6928fca71485c351f9be30b3fc4b816dd36",
+         intel: "efc74c80a8623d7489e3a80807b17e3cfa3d918709a7b3432e621ef654276136"
 
   url "https://github.com/cryptomator/cryptomator/releases/download/#{version}/Cryptomator-#{version}#{arch}.dmg",
       verified: "github.com/cryptomator/cryptomator/"
   name "Cryptomator"
   desc "Multi-platform client-side cloud file encryption tool"
   homepage "https://cryptomator.org/"
-
-  livecheck do
-    url "https://cryptomator.org/downloads/mac/thanks/"
-    regex(%r{href=.*?/Cryptomator[._-]v?(\d+(?:\.\d+)+)-x64\.dmg}i)
-  end
 
   depends_on macos: ">= :high_sierra"
 
