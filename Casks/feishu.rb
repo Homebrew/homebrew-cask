46a0e46da39abfb795cba65723f47e97fde9a71a
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-03-03 19:02:48 +0800

    Update feishu from 5.31.6 to 5.32.6

--- Casks/feishu.rb
+++ Casks/feishu.rb
@@ -6,8 +6,8 @@ cask "feishu" do
     sha256 "7a693a8ceb5b3013c464bb615ecb962a1d621f26fa6e4630534ae19b001c0215"
   end
   on_arm do
-    version "5.31.6,caa46348"
-    sha256 "23614a62b2b2ac6a5294152b177d3fe66b154eb5f336e86c548ca27129a0276f"
+    version "5.32.6,746ebb24"
+    sha256 "7957d18a566b18b1a625bfe0558048696bff6928e13c3404b2e9e3b7d607009e"
   end
 
   url "https://sf3-cn.feishucdn.com/obj/ee-appcenter/#{version.csv.second}/Feishu-darwin_#{arch}-#{version.csv.first}-signed.dmg",
