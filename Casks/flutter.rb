a100f4bb1a72d60b2f09589acc67fd4a03377863
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-03 11:51:13 +0800

    Update flutter.rb

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -1,11 +1,11 @@
 cask "flutter" do
-  arch arm: "_arm64_", intel: "_"
+  arch arm: "_arm64"
 
   version "3.7.6"
   sha256 arm:   "0823d126788058af49d2e212e13e783517d332339bbc0538f94eb13ee3f194fa",
          intel: "ef50057523ec5220921c8594db6e05e92077783a8232246f9c297d7d5ca95fb4"
 
-  url "https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos#{arch}#{version}-stable.zip",
+  url "https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos#{arch}_#{version}-stable.zip",
       verified: "storage.googleapis.com/flutter_infra_release/releases/stable/macos/"
   name "Flutter SDK"
   desc "UI toolkit for building applications for mobile, web and desktop"
