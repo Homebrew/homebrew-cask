8143e677d5e12351b620594809f2ccfd7d020aa7
Christian Kraemer <christian.kraemer@gmail.com>
2023-03-03 06:53:16 +0100

    Update microsoft-edge from 110.0.1587.57 to 110.0.1587.63

--- Casks/microsoft-edge.rb
+++ Casks/microsoft-edge.rb
@@ -3,9 +3,9 @@ cask "microsoft-edge" do
                                intel: "C1297A47-86C4-4C1F-97FA-950631F94777"
   linkid = on_arch_conditional arm: "2093504", intel: "2069148"
 
-  version "110.0.1587.57"
-  sha256 arm:   "29cb7bc9747fc9ad02a16e18ce87ffd3c44b9a5c688d544c74d17ece45866815",
-         intel: "23d09535d666ba0ee1e1b02cdc360e98a1a005ae72efe0accd56aeb492155fbf"
+  version "110.0.1587.63"
+  sha256 arm:   "c61a6ec10dee0d4ab0a6c208fbb650cf784c87ce84dd81a0ffad0717cf33aad3",
+         intel: "7f491e427e18003521649604382a2fb8e0781be68f495560c69fc027f6334a3d"
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/#{folder}/MacAutoupdate/MicrosoftEdge-#{version}.pkg",
       verified: "officecdn-microsoft-com.akamaized.net/"
