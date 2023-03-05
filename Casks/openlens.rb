13398d9d6a67cb47b126d1cb68d657ec588cbbc3
Amory Meltzer ☀ <Amorymeltzer@gmail.com>
2023-03-05 15:06:46 -0500

    Update openlens from 6.4.0 to 6.4.1 (#142335)

--- Casks/openlens.rb
+++ Casks/openlens.rb
@@ -1,9 +1,9 @@
 cask "openlens" do
   arch arm: "-arm64"
 
-  version "6.4.0"
-  sha256 arm:   "023316448832da811865bd383ada6e2fb60ee274b3f40df55adad3307793abf3",
-         intel: "b4befe2751ccf9565788ee00463907965171b577d8f010ea7f522c2390bcc066"
+  version "6.4.1"
+  sha256 arm:   "0b879cfec55756fbecfb22abdddc834dae10c28e1e77a04a82119ef99b690cb1",
+         intel: "30729dd7b6ae396fb6e9a8f71cc36120e176af6dab3916cc8732ac7ca548c8f0"
 
   url "https://github.com/MuhammedKalkan/OpenLens/releases/download/v#{version}/OpenLens-#{version}#{arch}.dmg"
   name "OpenLens"
