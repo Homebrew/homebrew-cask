1f2948777a535ff2429d143ae7ba4f381466f89f
Jackson Chen <jackson@jacksonchen666.com>
2023-03-05 15:01:55 +0100

    Update osu from 2023.301.0 to 2023.305.0

--- Casks/osu.rb
+++ Casks/osu.rb
@@ -1,9 +1,9 @@
 cask "osu" do
   arch arm: "Apple.Silicon", intel: "Intel"
 
-  version "2023.301.0"
-  sha256 arm:   "b5e9068e6504d4943a372235ddaf2be3cec4c182f9051358f13b76d80c0f702b",
-         intel: "714d23d6a0c8420d4453fa344f12e691b041789e78dad6afbafe6d4c0114b9a2"
+  version "2023.305.0"
+  sha256 arm:   "bdf0c4114f889c3e6e21cd5029c78cd923751a5595f770f24a7de293531261fe",
+         intel: "74afaaab0073b8f5c7a9093c5374e9590056133fbc4bd1874a15704d3ab60414"
 
   url "https://github.com/ppy/osu/releases/download/#{version}/osu.app.#{arch}.zip"
   name "osu!"
