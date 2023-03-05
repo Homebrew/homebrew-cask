951a57f569323c99f2cce748248f1b5f1c58758b
stevenlele <stevenlele@outlook.com>
2023-03-05 22:14:09 +0800

    Add MFiles v2.4.1

---
+++ Casks/mfiles.rb
@@ -0,0 +1,25 @@
+cask "mfiles" do
+  version "2.4.1,20221127"
+  sha256 "41db127cfba3278215e6f22380d0f8dccb593f98c613c527d433a39721e9a336"
+
+  url "https://mfiles.maokebing.com/mfiles-helper-#{version.csv.first}-macos-#{version.csv.second}.dmg"
+  name "MFiles"
+  name "iTrunSo"
+  name "爱传送"
+  desc "Transfer files over local network"
+  homepage "https://mfiles.maokebing.com/"
+
+  livecheck do
+    url :homepage
+    strategy :page_match do |page|
+      match = page.match(/mfiles-helper-([.\d]+)-macos-(\d{8})\.dmg/)
+      "#{match[1]},#{match[2]}"
+    end
+  end
+
+  app "爱传送.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.windtune.itransfer.plist",
+  ]
+end
