2a0acc30c8f5b3025755219d7576c177afafa701
Issy Long <me@issyl0.co.uk>
2023-03-05 17:28:47 +0000

    youdaonote: Remove protocol from verified stanza
    - According to https://github.com/Homebrew/brew/issues/14717 it's wrong
      to have the protocol in the "verified" part of a Cask URL. So there's
      now an autocorrecting RuboCop for it at
      https://github.com/Homebrew/brew/pull/14886. This fixes the singular
      offense.

--- Casks/youdaonote.rb
+++ Casks/youdaonote.rb
@@ -4,7 +4,7 @@ cask "youdaonote" do
 
   url "https://artifact.lx.netease.com/download/ynote-electron/%E6%9C%89%E9%81%93%E4%BA%91%E7%AC%94%E8%AE%B0-#{version}.zip",
       user_agent: :fake,
-      verified:   "https://artifact.lx.netease.com/download/ynote-electron/"
+      verified:   "artifact.lx.netease.com/download/ynote-electron/"
   name "youdaonote"
   name "有道云笔记"
   desc "Multi-platform note application"
