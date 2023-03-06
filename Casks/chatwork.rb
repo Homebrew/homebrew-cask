29df40560ac98e0688397bb9dbe98c074ecd808b
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-06 06:32:32 -0500

    Update chatwork from 2.6.37.5342 to 2.6.38.5441 (#142388)

--- Casks/chatwork.rb
+++ Casks/chatwork.rb
@@ -1,5 +1,5 @@
 cask "chatwork" do
-  version "2.6.37.5342"
+  version "2.6.38.5441"
   sha256 :no_check
 
   url "https://desktop-app.chatwork.com/installer/ChatWork.dmg"
@@ -13,4 +13,12 @@ cask "chatwork" do
   end
 
   app "ChatWork.app"
+
+  zap trash: [
+    "~/Library/Application Support/Chatwork",
+    "~/Library/Caches/com.electron.chatwork",
+    "~/Library/Caches/com.electron.chatwork.ShipIt",
+    "~/Library/HTTPStorages/com.electron.chatwork",
+    "~/Library/Preferences/com.electron.chatwork.plist",
+  ]
 end
