ff1c4ce831eec6cf8c872fa944106184c1cdf226
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-06 16:21:42 -0500

    Update fantastical from 3.7.7 to 3.7.8 (#142417)
    * Update fantastical from 3.7.7 to 3.7.8
    
    * add short_version to sparkle

--- Casks/fantastical.rb
+++ Casks/fantastical.rb
@@ -1,6 +1,6 @@
 cask "fantastical" do
-  version "3.7.7,1558"
-  sha256 "da9b9d4d98ff9da712ff003310751d687d794448167d25d219d35fffd9349d56"
+  version "3.7.8"
+  sha256 "99fb77ece3f79e4970766f312b7a4ec30fca78aa3ac1e4ec160e05c5a3872e53"
 
   url "https://cdn.flexibits.com/Fantastical_#{version.csv.first}.zip"
   name "Fantastical"
@@ -9,7 +9,7 @@ cask "fantastical" do
 
   livecheck do
     url "https://flexibits.com/fantastical/appcast2.php"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   auto_updates true
