3f4d2d4965111e15c2f322a7a4a4c09bd54998ec
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-06 15:09:46 -0500

    add short_version to sparkle

--- Casks/fantastical.rb
+++ Casks/fantastical.rb
@@ -1,5 +1,5 @@
 cask "fantastical" do
-  version "3.7.8,1566"
+  version "3.7.8"
   sha256 "99fb77ece3f79e4970766f312b7a4ec30fca78aa3ac1e4ec160e05c5a3872e53"
 
   url "https://cdn.flexibits.com/Fantastical_#{version.csv.first}.zip"
@@ -9,7 +9,7 @@ cask "fantastical" do
 
   livecheck do
     url "https://flexibits.com/fantastical/appcast2.php"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   auto_updates true
