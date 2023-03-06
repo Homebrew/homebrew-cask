ee23fd6d90467da8440e0c1a54b36b768948effd
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-06 17:51:06 +0800

    tencent-meeting: update `on_#{arch}` block order (#142343)
    Update tencent-meeting.rb

--- Casks/tencent-meeting.rb
+++ Casks/tencent-meeting.rb
@@ -1,13 +1,13 @@
 cask "tencent-meeting" do
   arch arm: "arm64", intel: "x86_64"
 
-  on_intel do
-    version "3.14.7.407,e12d770b7dba1aa37791964b4c631431"
-    sha256 "5c67f180a22beebc44ee933d6a4538829778b038eacf7daf5cf94c6912b30779"
-  end
   on_arm do
     version "3.14.7.407,99a99009360d9c219608b568bf6450da"
     sha256 "b4f49783501c31bae07a396471ec48955222622893a44f7ce56ed2bb8c09fc5d"
+  end
+  on_intel do
+    version "3.14.7.407,e12d770b7dba1aa37791964b4c631431"
+    sha256 "5c67f180a22beebc44ee933d6a4538829778b038eacf7daf5cf94c6912b30779"
   end
 
   url "https://updatecdn.meeting.qq.com/cos/#{version.csv.second}/TencentMeeting_0300000000_#{version.csv.first}.publish.#{arch}.dmg",
