902f362ebefd0adba2a47658ad08df675f567bb0
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-06 06:08:00 -0500

    Update avast-secure-browser from 110.0.3425.104 to 110.0.3475.178 (#142378)

--- Casks/avast-secure-browser.rb
+++ Casks/avast-secure-browser.rb
@@ -1,5 +1,5 @@
 cask "avast-secure-browser" do
-  version "110.0.3425.104"
+  version "110.0.3475.178"
   sha256 :no_check
 
   url "https://cdn-download.avastbrowser.com/AvastSecureBrowserSetup.pkg",
