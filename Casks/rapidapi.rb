006b1f398657e8b7d7cd77c088db8b8f8630fd5a
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-06 18:27:45 +0800

    Update rapidapi from 4.1.4 to 4.1.5 (#142367)
    * Update rapidapi from 4.1.4 to 4.1.5
    
    * Update rapidapi.rb

--- Casks/rapidapi.rb
+++ Casks/rapidapi.rb
@@ -1,15 +1,15 @@
 cask "rapidapi" do
-  version "4.1.4,4001004001"
-  sha256 "7d211203de1fa4857d458af1ffd3b6d282ac8bed05fe8766088f0a9dc7955b26"
+  version "4.1.5"
+  sha256 "5a710901e96e9eda91197125c53b1b511e2899261737808b602bf667ea32d61f"
 
-  url "https://cdn-builds.paw.cloud/paw/RapidAPI-#{version.csv.first}.zip"
+  url "https://cdn-builds.paw.cloud/paw/RapidAPI-#{version}.zip"
   name "RapidAPI"
   desc "HTTP client that helps testing and describing APIs"
   homepage "https://paw.cloud/"
 
   livecheck do
     url "https://paw.cloud/api/v2/updates/appcast"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   auto_updates true
