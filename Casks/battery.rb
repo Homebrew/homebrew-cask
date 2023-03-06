ed80a995969deb40eb9e304c38424e16c3425998
Kevin Manca <72861758+kevinm6@users.noreply.github.com>
2023-03-06 19:53:35 +0100

    Update battery.rb with correct url

--- Casks/battery.rb
+++ Casks/battery.rb
@@ -2,7 +2,7 @@ cask "battery" do
   version "1.0.8"
   sha256 "b3101d9aca85d41835cd5895d8c911ab6e07ed9279546a31bd7723f62bd00547"
 
-  url "https://github.com/actuallymentor/battery/releases/download/v#{version}/battery-#{version}-arm64.dmg"
+  url "https://github.com/actuallymentor/battery/releases/download/v#{version}/battery-#{version}-mac-arm64.dmg"
   name "Battery"
   desc "CLI for managing the battery charging status"
   homepage "https://github.com/actuallymentor/battery/"
