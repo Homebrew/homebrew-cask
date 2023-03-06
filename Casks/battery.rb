94768a3f5cd1147b477d1454dc19fd531f2b3e11
Kevin Manca <72861758+kevinm6@users.noreply.github.com>
2023-03-06 20:37:00 +0100

    Update battery.rb with correct url

--- Casks/battery.rb
+++ Casks/battery.rb
@@ -1,5 +1,5 @@
 cask "battery" do
-  version "1.0.8"
+  version "1.1.1"
   sha256 "b3101d9aca85d41835cd5895d8c911ab6e07ed9279546a31bd7723f62bd00547"
 
   url "https://github.com/actuallymentor/battery/releases/download/v#{version}/battery-#{version}-mac-arm64.dmg"
