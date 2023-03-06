71a80b7fba9d0c3df29629ab26993c93779dfdc9
Justin Lecher <justin.lecher@astrazeneca.com>
2023-03-06 10:54:46 +0000

    fix: Add hard dependency

--- Casks/deeper.rb
+++ Casks/deeper.rb
@@ -4,34 +4,50 @@ cask "deeper" do
   on_el_capitan :or_older do
     version "2.1.4"
     url "https://www.titanium-software.fr/download/1011/Deeper.dmg"
+
+    depends_on macos: :el_captain
   end
   on_sierra do
     version "2.2.3"
     url "https://www.titanium-software.fr/download/1012/Deeper.dmg"
+
+    depends_on macos: :sierra
   end
   on_high_sierra do
     version "2.3.3"
     url "https://www.titanium-software.fr/download/1013/Deeper.dmg"
+
+    depends_on macos: :high_sierra
   end
   on_mojave do
     version "2.4.8"
     url "https://www.titanium-software.fr/download/1014/Deeper.dmg"
+
+    depends_on macos: :mojave
   end
   on_catalina do
     version "2.6.0"
     url "https://www.titanium-software.fr/download/1015/Deeper.dmg"
+
+    depends_on macos: :catalina
   end
   on_big_sur do
     version "2.7.1"
     url "https://www.titanium-software.fr/download/11/Deeper.dmg"
+
+    depends_on macos: :big_sur
   end
   on_monterey do
     version "2.8.0"
     url "https://www.titanium-software.fr/download/12/Deeper.dmg"
+
+    depends_on macos: :monterey
   end
   on_ventura :or_newer do
     version "2.8.8"
     url "https://www.titanium-software.fr/download/13/Deeper.dmg"
+
+    depends_on macos: :ventura
   end
 
   name "Deeper"
@@ -42,9 +58,6 @@ cask "deeper" do
     url :homepage
     regex(/>\s*Deeper\s+v?(\d+(?:\.\d+)+)\s+for\s+[\w\s.-]*\s+#{MacOS.version}\s*</i)
   end
-
-  # This software has releases for specific versions of macOS. Running a version for a different OS is dangerous.
-  depends_on macos: "<= :ventura"
 
   app "Deeper.app"
 
