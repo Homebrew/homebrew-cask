7f9c88e893e05a747614db437291d9d2dbfb9a89
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-06 18:14:19 +0800

    Update rapidapi.rb

--- Casks/rapidapi.rb
+++ Casks/rapidapi.rb
@@ -1,15 +1,15 @@
 cask "rapidapi" do
-  version "4.1.5,4001005001"
+  version "4.1.5"
   sha256 "5a710901e96e9eda91197125c53b1b511e2899261737808b602bf667ea32d61f"
 
-  url "https://cdn-builds.paw.cloud/paw/RapidAPI-#{version.csv.first}.zip"
+  url "https://cdn-builds.paw.cloud/paw/RapidAPI-#{version}.zip"
   name "RapidAPI"
   desc "HTTP client that helps testing and describing APIs"
   homepage "https://paw.cloud/"
 
   livecheck do
     url "https://paw.cloud/api/v2/updates/appcast"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   auto_updates true
