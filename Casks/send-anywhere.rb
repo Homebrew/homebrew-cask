d45b062ab2040893a18ae3122d5df8c6989c0d45
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-07 06:29:10 -0500

    Update send-anywhere from 23.2.13 to 23.3.7 (#142461)
    * Update send-anywhere from 23.2.13 to 23.3.7
    
    * add zap stanza

--- Casks/send-anywhere.rb
+++ Casks/send-anywhere.rb
@@ -1,5 +1,5 @@
 cask "send-anywhere" do
-  version "23.2.13"
+  version "23.3.7"
   sha256 :no_check
 
   url "https://update.send-anywhere.com/osx_downloads/Send%20Anywhere.dmg"
@@ -13,4 +13,10 @@ cask "send-anywhere" do
   end
 
   app "Send Anywhere.app"
+
+  zap trash: [
+    "~/Library/Application Support/Send Anywhere",
+    "~/Library/Logs/Send Anywhere",
+    "~/Library/Preferences/com.estmob.osx.sendanywhere.plist",
+  ]
 end
