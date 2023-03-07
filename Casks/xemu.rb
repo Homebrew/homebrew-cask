489f7a06d92c171d7dd8ba2f8ca4eff95eae66cf
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-07 06:34:50 -0500

    Update xemu from 0.7.84 to 0.7.85 (#142492)

--- Casks/xemu.rb
+++ Casks/xemu.rb
@@ -1,6 +1,6 @@
 cask "xemu" do
-  version "0.7.84"
-  sha256 "3683570f9b70740c2c15e07c01acc368363cf2c4732e90870c533ca1033ab752"
+  version "0.7.85"
+  sha256 "0570dc858ab70eca250382e68b0f8b09c3dd772d92c9bed5501cb8d59b250ca6"
 
   url "https://github.com/xemu-project/xemu/releases/download/v#{version}/xemu-macos-universal-release.zip",
       verified: "github.com/xemu-project/xemu/"
