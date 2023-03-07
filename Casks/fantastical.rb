392fc6a239d58d6b4d145fed644c4989b5298953
Bevan Kay <email@bevankay.me>
2023-03-07 22:36:33 +1100

    Update fantastical.rb

--- Casks/fantastical.rb
+++ Casks/fantastical.rb
@@ -17,16 +17,16 @@ cask "fantastical" do
 
   app "Fantastical.app"
 
-  uninstall launchctl: "com.flexibits.fantastical2.mac.launcher",
+  uninstall launchctl: "com.flexibits.fantastical*.mac.launcher",
             quit:      [
-              "85C27NK92C.com.flexibits.fantastical2.mac.helper",
-              "com.flexibits.fantastical2.mac",
+              "*.com.flexibits.fantastical*.mac.helper",
+              "com.flexibits.fantastical*.mac",
             ]
 
   zap trash: [
-    "~/Library/Application Scripts/com.flexibits.fantastical2.*",
+    "~/Library/Application Scripts/com.flexibits.fantastical*",
     "~/Library/Application Scripts/com.flexibits.fbcaldav.*",
-    "~/Library/Containers/com.flexibits.fantastical2.*",
+    "~/Library/Containers/com.flexibits.fantastical*",
     "~/Library/Containers/com.flexibits.fbcaldav.*",
     "~/Library/Preferences/com.flexibits.fantastical.plist",
   ]
