c8f36691ae387b05078de9c4e37a570d0401607a
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-06 19:11:20 -0500

    Update notesnook from 2.4.3 to 2.4.4

--- Casks/notesnook.rb
+++ Casks/notesnook.rb
@@ -1,9 +1,9 @@
 cask "notesnook" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.4.3"
-  sha256 arm:   "56953cbe6cc13e2935feee727a33b3a92d4834c846b3bf173b55e8d85e16ebb4",
-         intel: "dc3f4b934e0b7941e42bbbe4e46a99f5377dbd7e785ceb3c1d7912d57758872b"
+  version "2.4.4"
+  sha256 arm:   "4a91d4a7dc1c1e117e922e3a86a6a27bb12092915a23d561a831b625126622d7",
+         intel: "9e46638cc93f8fc938cbde4878dd82552bdebcf844761ed57e1d22c04f0c5f15"
 
   url "https://github.com/streetwriters/notesnook/releases/download/v#{version}/notesnook_mac_#{arch}.dmg",
       verified: "github.com/streetwriters/notesnook/"
