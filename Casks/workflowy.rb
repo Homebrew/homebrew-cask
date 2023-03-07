43c8a542a3f145a4c4de52ff0539aa6fd7ccdf61
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-07 06:33:55 -0500

    Update workflowy from 4.0.2302081114 to 4.0.2303021329 (#142489)

--- Casks/workflowy.rb
+++ Casks/workflowy.rb
@@ -1,6 +1,6 @@
 cask "workflowy" do
-  version "4.0.2302081114"
-  sha256 "7679e4bc64c7a4efc6576c0e458b535f8c6f68a3af0e113d6c618f23a976f920"
+  version "4.0.2303021329"
+  sha256 "4d19f6621c57f570261ac5a205c95815697c70b5d91a7abe36e3412352734bde"
 
   url "https://github.com/workflowy/desktop/releases/download/v#{version}/WorkFlowy.zip",
       verified: "github.com/workflowy/desktop/"
