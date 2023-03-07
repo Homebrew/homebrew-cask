e936c52fdf648ca582a92d27b24b8784666d17c7
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-08 03:40:15 +0800

    Update get-backup-pro.rb

--- Casks/get-backup-pro.rb
+++ Casks/get-backup-pro.rb
@@ -1,5 +1,5 @@
 cask "get-backup-pro" do
-  version "3.7.1,1765"
+  version "3.7.1"
   sha256 "53adcbb18f4d7f4fbd06f97f05ce7719a622512a137595c6498bc2fe106ca9a4"
 
   url "https://belightsoft.s3.amazonaws.com/updates/Get+Backup+Pro+#{version.major}.zip",
@@ -10,7 +10,7 @@ cask "get-backup-pro" do
 
   livecheck do
     url "https://www.belightsoft.com/download/updates/appcast_getbackup_pro#{version.major}.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   auto_updates true
