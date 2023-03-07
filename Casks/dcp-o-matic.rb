3bab22b3a1b2f35bc98ce0abafebc895881bb9ae
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-07 05:33:40 -0500

    update dcp-o-matic

--- Casks/dcp-o-matic.rb
+++ Casks/dcp-o-matic.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic" do
-  version "2.16.44"
-  sha256 "98e6b321036cbdf8af5371d32f11d9961f5692c38d211e5386a1b0e0d6c8bb36"
+  version "2.16.46"
+  sha256 "80bc96e344b731b746775a1cbe9ae96863e0d820f2bd6dd69ebd1624ed7b1104"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-main&version=#{version}"
   name "DCP-o-matic"
