4304b11f32cc080234e305be32700d2d460e5a73
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-07 06:43:36 -0500

    Update tiger-trade from 8.4.1 to 8.5.1 (#142481)
    * Update tiger-trade from 8.4.1 to 8.5.1
    
    * add zap stanza
    
    * Update tiger-trade.rb
    
    ---------
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/tiger-trade.rb
+++ Casks/tiger-trade.rb
@@ -1,6 +1,6 @@
 cask "tiger-trade" do
-  version "8.4.1,11BB37"
-  sha256 "1846390a649d222fbe9705a16e23f33b65247ea712e3381ebba53a3f2c1ff0de"
+  version "8.5.1,21BD95"
+  sha256 "3ac89eb719997bb7262e6023a2d72187d11a1ef02e570aa0223d5fddd668882c"
 
   url "https://s.tigerfintech.com/desktop/cdn/f/TigerTrade_#{version.tr(",", "_")}.dmg",
       verified: "s.tigerfintech.com/"
@@ -22,4 +22,9 @@ cask "tiger-trade" do
   depends_on macos: ">= :sierra"
 
   app "Tiger Trade.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.itiger.TigerTrade-Mac",
+    "~/Library/Containers/com.itiger.TigerTrade-Mac",
+  ]
 end
