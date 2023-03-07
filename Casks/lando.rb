5bd69c646aff4201c1379ce2639da29ab3df6255
Erik Hanson <erik.hanson@gmail.com>
2023-03-07 13:25:32 -0600

    Bump Lando to v3.14.0
    Updated Lando hashes to version 3.14.0

--- Casks/lando.rb
+++ Casks/lando.rb
@@ -1,9 +1,9 @@
 cask "lando" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.11.0"
-  sha256 arm:   "8709ec42863a16afecbf9afe3103e55176ae3e26b68199ff1096ca72c14b8cb9",
-         intel: "c813fd1d30d901c0ed22c0c2c60a0d3e94670dbc420ae7c7c99eb32be1fe5cb7"
+  version "3.14.0"
+  sha256 arm:   "617e47c1e8e11e6cd22cfa7aaacec5b7a26fb8f74598f2e042118e1e91c26d87",
+         intel: "3870855fff35a186ed478eca37c3c095b1a196267cd52a1e34f22a8a79084a7d"
 
   url "https://github.com/lando/lando/releases/download/v#{version}/lando-#{arch}-v#{version}.dmg",
       verified: "github.com/lando/lando/"
