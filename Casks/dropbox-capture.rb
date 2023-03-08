b10798814bf38c2b1fa60ad8a1435c5833021761
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-08 05:18:21 -0500

    Update dropbox-capture from 94.0.3 to 95.0.11

--- Casks/dropbox-capture.rb
+++ Casks/dropbox-capture.rb
@@ -1,9 +1,9 @@
 cask "dropbox-capture" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "94.0.3"
-  sha256 arm:   "4ef23def8b981757aa1da3a810f72284ad19b9e3818a02583e2a26fb4529d39d",
-         intel: "1d7e9a3e0797a373350a87285b317962fa7216d7b4473e2e0e6a26065f38b48d"
+  version "95.0.11"
+  sha256 arm:   "49ac5afc9da44def9420a562585590a111b19fe8c0d209eb41e1693bb9090a0a",
+         intel: "6351ac1dc885304fa9641b8bce24f7c2536620b347bec286c4d9cebbee81f793"
 
   url "https://edge.dropboxstatic.com/dbx-releng/products/dropbox-capture/#{version}/mac.#{arch}/Dropbox_Capture.dmg",
       verified: "edge.dropboxstatic.com/dbx-releng/products/dropbox-capture/"
