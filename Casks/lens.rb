74a6bce6fd7b5f30aa8e4e25d5ad4960f7aff376
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-08 16:29:59 -0500

    Update lens from 2023.1.110749 to 2023.3.71735 (#142555)
    * Update lens from 2023.1.110749 to 2023.3.71735
    
    fix livecheck
    
    * Update lens.rb
    
    * Update lens.rb
    
    ---------
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/lens.rb
+++ Casks/lens.rb
@@ -1,9 +1,9 @@
 cask "lens" do
   arch arm: "-arm64"
 
-  version "2023.1.110749"
-  sha256 arm:   "210782fbe953d7ed71797d2d2357e89677a1fa4c762a054ae79c9e2230e1cf35",
-         intel: "b8631d29ffdea2b0b0f3e398a7e31aa2801d49bb03c4c830d290f0b90704dc99"
+  version "2023.3.71735"
+  sha256 arm:   "cafea2ea300d38f75fd8a42db25c46c42563fc4d2162ae4efaa4035bf135726a",
+         intel: "42e0de39402ca8ee22dd5de21b3a4b5ed730f544b9907d64cd6a0ee78215477e"
 
   url "https://api.k8slens.dev/binaries/Lens-#{version}-latest#{arch}.dmg"
   name "Lens"
@@ -11,9 +11,10 @@ cask "lens" do
   homepage "https://k8slens.dev/"
 
   livecheck do
-    url "https://lens-binaries.s3.amazonaws.com/ide/latest-mac.yml"
-    strategy :electron_builder do |data|
-      data["version"].sub("-latest", "")
+    url "https://api.k8slens.dev/binaries/latest-mac.json/"
+    regex(/(\d+(?:\.\d+)+)/i)
+    strategy :json do |json|
+      json["version"].sub("-latest", "")
     end
   end
 
