79d0a924f9e5e80a9957557505acb2d87455e011
Yurii Kolesnykov <root@yurikoles.com>
2023-03-08 23:52:01 +0200

    Update cmake from 3.25.2 to 3.25.3 (#142563)
    * Update cmake from 3.25.2 to 3.25.3
    
    * Update cmake.rb
    
    ---------
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/cmake.rb
+++ Casks/cmake.rb
@@ -1,6 +1,6 @@
 cask "cmake" do
-  version "3.25.2"
-  sha256 "644f104ae744df582b54ae47a8188bedacadd67dd996d70caa8159a47feb8749"
+  version "3.25.3"
+  sha256 "6ae16393534e06bcd5c36288095e266d40fa2936b9d592b94f8fb95b97589cca"
 
   url "https://cmake.org/files/LatestRelease/cmake-#{version}-macos-universal.dmg"
   name "CMake"
@@ -9,15 +9,15 @@ cask "cmake" do
 
   livecheck do
     url "https://cmake.org/files/LatestRelease/"
-    regex(/href=.*?cmake[._-]v?(\d+(?:\.\d+)+)-macos-universal\.dmg/i)
+    regex(/href=.*?cmake[._-]v?(\d+(?:\.\d+)+)[._-]macos[._-]universal\.dmg/i)
   end
 
   conflicts_with formula: "cmake"
 
   app "CMake.app"
   binary "#{appdir}/CMake.app/Contents/bin/ccmake"
-  binary "#{appdir}/CMake.app/Contents/bin/cmake-gui"
   binary "#{appdir}/CMake.app/Contents/bin/cmake"
+  binary "#{appdir}/CMake.app/Contents/bin/cmake-gui"
   binary "#{appdir}/CMake.app/Contents/bin/cpack"
   binary "#{appdir}/CMake.app/Contents/bin/ctest"
   manpage "#{appdir}/CMake.app/Contents/man/man1/ccmake.1"
