ad050a620e1683a7996bce9eaf3e4b2abd04fd3c
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-08 10:50:35 +0800

    Update Casks/alex313031-thorium.rb
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/alex313031-thorium.rb
+++ Casks/alex313031-thorium.rb
@@ -11,15 +11,27 @@ cask "alex313031-thorium" do
   desc "Thorium - The fastest browser on Earth"
   homepage "https://thorium.rocks/"
 
+  # NOTE: This approach involves multiple requests and should be avoided
+  # whenever possible. If upstream starts reliably providing dmg files in every
+  # release, we should switch to `url :url` with `strategy :github_latest`.
   livecheck do
-    url "https://api.github.com/repos/Alex313031/Thorium-Special/releases"
+    url "https://github.com/Alex313031/Thorium-Special/releases?q=prerelease%3Afalse"
     regex(%r{/([^/]+?)/Thorium[._-]MacOS[._-]#{arch}\.dmg}i)
-    strategy :json do |json, regex|
-      json.map do |release|
-        curr_asset = release["assets"]&.find { |asset| asset["browser_download_url"]&.match(regex) }
-        next if curr_asset.blank?
+    strategy :page_match do |page, regex|
+      # Collect the release tags on the page
+      tags = page.scan(%r{href=["']?[^"' >]*?/releases/tag/([^"' >]*?)["' >]}i)&.flatten&.uniq
 
-        curr_asset["browser_download_url"][regex, 1]
+      max_reqs = 4
+      tags.each_with_index do |tag, i|
+        break if i >= max_reqs
+
+        # Fetch the assets list HTML for the tag and match within it
+        assets_page = Homebrew::Livecheck::Strategy.page_content(
+          @url.sub(%r{/releases/?.+}, "/releases/expanded_assets/#{tag}"),
+        )
+        matches = assets_page[:content]&.scan(regex)&.map { |match| match[0] }
+
+        break matches if matches.present?
       end
     end
   end
