f43d8a54e02dd0258460f6dca8f00348b3181308
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-09 13:40:25 -0500

    Update protege from 5.6.0 to 5.6.1 (#142627)

--- Casks/protege.rb
+++ Casks/protege.rb
@@ -1,6 +1,6 @@
 cask "protege" do
-  version "5.6.0"
-  sha256 "19408c35153994576f72be4ce7e11b2ec745dcc6764d01790440de233bd5e354"
+  version "5.6.1"
+  sha256 "1cb7af245c514c5b7587ca64b3d6d6ea02254e2c056f54a1d2939fef3d860126"
 
   url "https://github.com/protegeproject/protege-distribution/releases/download/protege-#{version}/Protege-#{version}-os-x.zip",
       verified: "github.com/protegeproject/protege-distribution/"
@@ -9,4 +9,10 @@ cask "protege" do
   homepage "https://protege.stanford.edu/"
 
   app "Protege-#{version}/Protégé.app"
+
+  zap trash: [
+    "~/.Protege",
+    "~/Library/Preferences/protege_preferences.*",
+    "~/Library/Saved Application State/edu.stanford.protege.savedState",
+  ]
 end
