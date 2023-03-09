8d0673c9367bed5e17bdac75ec9beb794858e6c0
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-09 13:35:21 -0500

    Update coin-wallet from 5.9.4 to 5.10.0 (#142642)

--- Casks/coin-wallet.rb
+++ Casks/coin-wallet.rb
@@ -1,6 +1,6 @@
 cask "coin-wallet" do
-  version "5.9.4"
-  sha256 "74ce411f20ab3d636d478ed6d3c166bc0a9ae7bdb821e21958679c5bbf56bfa5"
+  version "5.10.0"
+  sha256 "2ed06cc133101112751cbfcbed9798fee7cbfaec752814f20ecd3a246428b46f"
 
   url "https://github.com/CoinSpace/CoinSpace/releases/download/v#{version}/Coin.Wallet.dmg",
       verified: "github.com/CoinSpace/CoinSpace/"
