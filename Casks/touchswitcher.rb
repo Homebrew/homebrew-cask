9e69b8ca6c9e2f7d0dc2dbb84399e13299ec84c0
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-09 12:56:36 +0800

    Update touchswitcher.rb

--- Casks/touchswitcher.rb
+++ Casks/touchswitcher.rb
@@ -1,15 +1,15 @@
 cask "touchswitcher" do
-  version "1.4.5,157"
+  version "1.4.5"
   sha256 :no_check
 
   url "https://hazeover.com/touchswitcher/TouchSwitcher.zip"
   name "TouchSwitcher"
-  desc "Use Touch Bar to switch apps"
+  desc "Use the Touch Bar to switch apps"
   homepage "https://hazeover.com/touchswitcher.html"
 
   livecheck do
     url "https://hazeover.com/touchswitcher/updates.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   depends_on macos: ">= :mojave"
