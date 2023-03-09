b8a07b7ce78483343c77e22b9efd45b9afd170e6
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-09 13:35:07 -0500

    Update coscreen from 4.5.26 to 4.5.28 (#142643)

--- Casks/coscreen.rb
+++ Casks/coscreen.rb
@@ -1,9 +1,9 @@
 cask "coscreen" do
   arch arm: "arm64", intel: "x64"
 
-  version "4.5.26"
-  sha256 arm:   "20a2e17a8149c80fc6e327ae495a5bdc8806c2b11d901dc839a676a7d6e3cad3",
-         intel: "752bf26f9089f14d5494b4d56335e364418eb592c32f713d448450f368f7b580"
+  version "4.5.28"
+  sha256 arm:   "dc8d31111f9d2417ebc013b0dad6731a36caec83c8d38c4a3cbdd5eda5649666",
+         intel: "929919a8cd262cdcaff65e636eb8fa55b5d4e288cae776984d75fe3ca7c5758f"
 
   url "https://update.coscreen.org/CoScreen-#{version}-stable-#{arch}.dmg",
       verified: "update.coscreen.org/"
