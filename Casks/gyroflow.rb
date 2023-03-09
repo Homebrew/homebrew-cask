4a1c79d868bb6cbb2a9a447cfbbf60db4eefe90b
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-08 21:12:37 -0500

    Update gyroflow from 1.4.2 to 1.5.0

--- Casks/gyroflow.rb
+++ Casks/gyroflow.rb
@@ -1,6 +1,6 @@
 cask "gyroflow" do
-  version "1.4.2"
-  sha256 "37c225403898599aa0e01da8b8f4f19a44f5614d0d462f49522e4569cef0ab83"
+  version "1.5.0"
+  sha256 "4b5901f9b200f59a93270d6d17bb0fd714e7501b4bc0c615cba70238483f4c3c"
 
   url "https://github.com/gyroflow/gyroflow/releases/download/v#{version}/Gyroflow-mac-universal.dmg",
       verified: "github.com/gyroflow/gyroflow/"
