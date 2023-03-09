35d018e795f530ca9df3ce837244c67386426c34
Andreas Scherer <andreas_github@freenet.de>
2023-03-09 17:05:19 +0100

    Revert "Retry 'livecheck'."
    This reverts commit 165cf2a2f43156d7e837edd9e4bde59c1aa59a32.

--- Casks/rawtherapee.rb
+++ Casks/rawtherapee.rb
@@ -1,15 +1,11 @@
 cask "rawtherapee" do
   version "5.9"
-  sha256 "d0812e94e8297449ca6bb4136d8a96951540dc14e8e5d98bdac152f7f082d47f"
+  sha256 :no_check
 
   url "https://www.rawtherapee.com/shared/builds/mac/RawTherapee_macOS_11.7_Universal_5.9.zip"
   name "RawTherapee"
   desc "RAW photo processer"
   homepage "https://rawtherapee.com/"
-
-  livecheck do
-    url "https://www.rawtherapee.com/shared/builds/mac/RawTherapee_macOS_11.7_Universal_5.9.zip.sha256"
-  end
 
   container nested: "RawTherapee_macOS_11.7_Universal_5.9_folder/RawTherapee_macOS_11.7_Universal_5.9.dmg"
 
