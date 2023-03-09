14dc447a1459088bbda2f68306b339e229307f1c
Alistair Evans <alistair.evans@enclave.io>
2023-03-09 18:59:56 +0000

    Update enclave from 2023.03.09 to 2023.03.09.hotfix1 (#142649)

--- Casks/enclave.rb
+++ Casks/enclave.rb
@@ -1,6 +1,6 @@
 cask "enclave" do
-  version "2023.03.09"
-  sha256 "12f2441ebdb36d977b5bd37eae23a259e41a74d7ccc32037379e641daa180dad"
+  version "2023.03.09.hotfix1"
+  sha256 "68aff7b844b4201347ba9e35c34b59d39601aa9dcdb46c4b66a797eac205cc3f"
 
   url "https://release.enclave.io/enclave_osx-installer-x64-stable-#{version}.pkg"
   name "Enclave"
@@ -9,7 +9,7 @@ cask "enclave" do
 
   livecheck do
     url "https://install.enclave.io/latest/osx-homebrew-version.txt"
-    regex(/(\d+(?:\.\d+)*)/i)
+    regex(/(\d+(?:\.\d+)+(?:\.hotfix\d+)?)/i)
   end
 
   pkg "enclave_osx-installer-x64-stable-#{version}.pkg"
