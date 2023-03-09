a6d3b9c915da1fcbd79a13dfd95696db48983d3b
Michael Tibben <michael.tibben@99designs.com>
2023-03-10 07:51:27 +1100

    Update aws-vault from 7.0.1 to 7.0.2

--- Casks/aws-vault.rb
+++ Casks/aws-vault.rb
@@ -1,9 +1,9 @@
 cask "aws-vault" do
   arch arm: "arm64", intel: "amd64"
 
-  version "7.0.1"
-  sha256 arm:   "ea0944db21bbc2bfd05b07fb5fd02ad8e6a2fb7e699e959b39cc14a5680d06c3",
-         intel: "6c2b2ff18f6b04378793b0c3100330067677bbee3a65dae7ab4b7ec956ddf36a"
+  version "7.0.2"
+  sha256 arm:   "9314b0fc3aac18acc56430c0ded5626e53e921a48a29b50b7ca75d91d2ce927d",
+         intel: "9284fe20c24e19562be4c3ff7987c2df48757123e83a4420f1b049c7ee57ce22"
 
   url "https://github.com/99designs/aws-vault/releases/download/v#{version}/aws-vault-darwin-#{arch}.dmg"
   name "aws-vault"
