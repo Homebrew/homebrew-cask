7fa658647030ffa172d23d12efea800e30a248d4
tiiiecherle <tiiiecherle@gmail.com>
2023-03-09 09:52:33 +0100

    Update signal from 6.8.0 to 6.9.0 (#142605)

--- Casks/signal.rb
+++ Casks/signal.rb
@@ -1,9 +1,9 @@
 cask "signal" do
   arch arm: "arm64", intel: "x64"
 
-  version "6.8.0"
-  sha256 arm:   "406bbee39584157b2c976fd9eac5c25b1a16ecad5ca6960a58d21ea9d49fc91e",
-         intel: "b2cd0daffcf1e8e91fc3286dc46f8df27762cd6bbb93f59a78bd30959bf09fdc"
+  version "6.9.0"
+  sha256 arm:   "4b4cdc4acb37ba9f9a8b5110ee1ea998e4012cec80545ab304cdef7f1dfd1f20",
+         intel: "b4a9755c66592337c766b61a0c41c9724812289fb8532361a63c2ab03814acf0"
 
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{arch}-#{version}.dmg"
   name "Signal"
