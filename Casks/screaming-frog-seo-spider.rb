21faaee2ed28a50ab3054eb10a3be9c140eb1f90
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-08 21:32:42 -0500

    Update screaming-frog-seo-spider from 18.2 to 18.3

--- Casks/screaming-frog-seo-spider.rb
+++ Casks/screaming-frog-seo-spider.rb
@@ -1,9 +1,9 @@
 cask "screaming-frog-seo-spider" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "18.2"
-  sha256 arm:   "4fed8fc4cc7661adafcaeb80e69f8e1df9a5f29ae6fc323e8b17f99856fd150f",
-         intel: "65b08d61386c4606c1d3efdf74656bce2d7138bc4f41251acd0301f515aeb804"
+  version "18.3"
+  sha256 arm:   "87ff05b914c8f0f63180f8e8f353e97008250f014acf91fd49ab36ecaea89b8e",
+         intel: "55f8b6052c953c1049312152be6495e79423cb1845b0851a53ca402e7baadfc0"
 
   url "https://download.screamingfrog.co.uk/products/seo-spider/ScreamingFrogSEOSpider-#{version}-#{arch}.dmg"
   name "Screaming Frog SEO Spider"
