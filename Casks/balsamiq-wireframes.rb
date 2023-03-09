edad987eaa65dfd99e0a66d792b5f77d0a5a219c
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-08 21:02:09 -0500

    Update balsamiq-wireframes from 4.6.6 to 4.7.1

--- Casks/balsamiq-wireframes.rb
+++ Casks/balsamiq-wireframes.rb
@@ -1,6 +1,6 @@
 cask "balsamiq-wireframes" do
-  version "4.6.6"
-  sha256 "8812eb41fe115c95b965f563524e0cef5525730e64dc5c72d49164186e6640b4"
+  version "4.7.1"
+  sha256 "ed3708810971032cd664869c13ab9944c1ae54821f37aae51e1f98a0dbc2659f"
 
   url "https://builds.balsamiq.com/bwd/Balsamiq%20Wireframes%20#{version}.dmg"
   name "Balsamiq Wireframes"
