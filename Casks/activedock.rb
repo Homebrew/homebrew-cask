59fd226decc598a30df74fe7693578f4f7b8ceb9
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-09 18:36:57 -0500

    Update activedock from 2.400 to 2.401 (#142659)
    * Update activedock from 2.400 to 2.401
    
    * modify zap stanza

--- Casks/activedock.rb
+++ Casks/activedock.rb
@@ -1,5 +1,5 @@
 cask "activedock" do
-  version "2.400"
+  version "2.401"
   sha256 :no_check
 
   url "https://macplus-software.com/downloads/ActiveDock.zip",
@@ -16,4 +16,12 @@ cask "activedock" do
   depends_on macos: ">= :high_sierra"
 
   app "ActiveDock #{version.major}.app"
+
+  zap trash: [
+    "/Users/razvanazamfirei/Library/Application Support/ActiveDock #{version.major}",
+    "/Users/razvanazamfirei/Library/Application Support/com.sergey-gerasimenko.ActiveDock-#{version.major}",
+    "/Users/razvanazamfirei/Library/Caches/com.sergey-gerasimenko.ActiveDock-#{version.major}",
+    "/Users/razvanazamfirei/Library/HTTPStorages/com.sergey-gerasimenko.ActiveDock-#{version.major}",
+    "/Users/razvanazamfirei/Library/Preferences/com.sergey-gerasimenko.ActiveDock-#{version.major}.plist",
+  ]
 end
