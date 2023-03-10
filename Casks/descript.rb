2262ec78f418098fb767646e20092b444757f143
Patrick Linnane <patrick@linnane.io>
2023-03-10 13:54:13 -0800

    Update descript from 59.1.1-release.20230307.10 to 59.1.2-release.20230309.18 (#142747)

--- Casks/descript.rb
+++ Casks/descript.rb
@@ -1,9 +1,9 @@
 cask "descript" do
   arch arm: "-arm64"
 
-  version "59.1.1-release.20230307.10"
-  sha256 arm:   "c32622dad11f957350f8145ea32a2e1a7d97fdd82b5a05cfff3cef3b1d054de2",
-         intel: "87d26555807c6a1671421d3cf705e0c117d5679dd354201ac3d9d44c718464ab"
+  version "59.1.2-release.20230309.18"
+  sha256 arm:   "d7d1865026049f86a9cfa39f56f4aaf127da46969d7317fe05bb097bb759a11e",
+         intel: "dd4c6dba7e23209e497ac270b915fb13ede124c8c57ec5906a3781845423aa61"
 
   url "https://electron.descript.com/Descript-#{version}#{arch}.dmg"
   name "Descript"
