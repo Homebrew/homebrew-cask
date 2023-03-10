5f90d0a4cac0c022f4909a1aeb4660de6f11de11
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-10 10:33:02 -0500

    Update oak9 from 0.11.16 to 0.11.17 (#142707)

--- Casks/oak9.rb
+++ Casks/oak9.rb
@@ -1,7 +1,7 @@
 cask "oak9" do
   # NOTE: "9" is not a version number, but an intrinsic part of the product name
-  version "0.11.16"
-  sha256 "cef527114243a2d48390f8ed454e56cf83f20135511f4f0eabf3dbed660c8ee2"
+  version "0.11.17"
+  sha256 "fe5a81d28d816fa059a28afbc492ccff74401c5cec1d1dd5be71b18a99eec48f"
 
   url "https://github.com/oak9io/oak9.cli-public/raw/master/binaries/iac-mac_os-#{version}.zip",
       verified: "github.com/oak9io/oak9.cli-public/"
