5599a396c652166ae222a8f54851d683c419939a
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-11 05:33:25 +0800

    Update ammonite.rb

--- Casks/ammonite.rb
+++ Casks/ammonite.rb
@@ -1,15 +1,15 @@
 cask "ammonite" do
-  version "1.24,604"
+  version "1.24"
   sha256 "b2fd26878ba90379b71705b1bc8dacaf107c0e61533e4c093475c943898dd745"
 
-  url "https://www.soma-zone.com/download/files/Ammonite-#{version.csv.first}.tar.xz"
+  url "https://www.soma-zone.com/download/files/Ammonite-#{version}.tar.xz"
   name "Ammonite"
   desc "Tag visualizer and search utility"
   homepage "https://www.soma-zone.com/Ammonite/"
 
   livecheck do
     url "https://www.soma-zone.com/Ammonite/a/appcast.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   app "Ammonite.app"
