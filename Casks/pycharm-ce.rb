09e12e190ad65b0372a8ebe7f19cf642e2b6ab73
Moulick Aggarwal <Moulick@users.noreply.github.com>
2023-03-10 21:11:05 +0530

    Update pycharm-ce from 2022.3.2 to 2022.3.3 (#142681)
    * Update pycharm-ce from 2022.3.2 to 2022.3.3
    
    * Update pycharm-ce.rb
    
    ---------
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/pycharm-ce.rb
+++ Casks/pycharm-ce.rb
@@ -1,9 +1,9 @@
 cask "pycharm-ce" do
   arch arm: "-aarch64"
 
-  version "2022.3.2,223.8617.48"
-  sha256 arm:   "5a0fcb9fdc94896cd5651d9d60fa708596aebe374bc35944b3ff6133f4eb5aae",
-         intel: "0a5a396b71533ab7ec77b2f10e08a20a970ac5712cfeb3378728020ec84be416"
+  version "2022.3.3,223.8836.34"
+  sha256 arm:   "8fca3f1be009285ce2ea6352ad989e12ca8cacc318d95f7261eaad88566c0cbc",
+         intel: "c913f4bbf661fd5ad3a70ca662420b6cf850a8a03fc90f06f73487c8c1811a11"
 
   url "https://download.jetbrains.com/python/pycharm-community-#{version.csv.first}#{arch}.dmg"
   name "Jetbrains PyCharm Community Edition"
@@ -35,8 +35,8 @@ cask "pycharm-ce" do
   end
 
   zap trash: [
-    "~/Library/Application Support/PyCharm#{version.major_minor}",
     "~/Library/Application Support/JetBrains/PyCharmCE#{version.major_minor}",
+    "~/Library/Application Support/PyCharm#{version.major_minor}",
     "~/Library/Caches/com.apple.python/Applications/PyCharm CE.app",
     "~/Library/Caches/JetBrains/PyCharmCE#{version.major_minor}",
     "~/Library/Caches/PyCharm#{version.major_minor}",
