8e7a25b0a27d6cd846135c3ed997b2f4dd7a77c2
Lorenzoantonelli <56717474+Lorenzoantonelli@users.noreply.github.com>
2023-03-12 00:15:44 +0100

    Update aldente from 1.20 to 1.21 (#142909)

--- Casks/aldente.rb
+++ Casks/aldente.rb
@@ -1,6 +1,6 @@
 cask "aldente" do
-  version "1.20"
-  sha256 "46e56281e61d1e3a817a208a9e7b0bafec8f18e14f6be7557f9632b4a3235622"
+  version "1.21"
+  sha256 "b25a01e2db791964cb361ffd63f65fb4d521de956a02ffa4aab89c4655609ec5"
 
   url "https://github.com/davidwernhart/AlDente/releases/download/#{version}/AlDente.dmg"
   name "AlDente"
