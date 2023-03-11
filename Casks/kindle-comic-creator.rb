7914f87ae11d55e5befb48acd77fbee473f1c2e5
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-11 12:00:15 +0800

    Update kindle-comic-creator.rb

--- Casks/kindle-comic-creator.rb
+++ Casks/kindle-comic-creator.rb
@@ -5,6 +5,7 @@ cask "kindle-comic-creator" do
   url "https://kc2.s3.amazonaws.com/KindleComicCreatorInstall.dmg",
       verified: "kc2.s3.amazonaws.com/"
   name "Kindle Comic Creator"
+  desc "Turns comics, graphic novels and manga into Kindle books"
   homepage "https://www.amazon.com/gp/feature.html?ie=UTF8&docId=1001103761"
 
   livecheck do
@@ -16,7 +17,6 @@ cask "kindle-comic-creator" do
 
   uninstall pkgutil:   [
               "com.amazon.kc2.kindleComicCreator*",
-              "com.amazon.kc2.KindleComicCreator*",
               "com.amazon.kc2.KindlePreviewer.pkg",
             ],
             launchctl: "com.amazon.KindlePreviewerUpdater"
