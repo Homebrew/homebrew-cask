e659d56b690b898036e54f08353d7295d72f5c95
miccal <miccal.matthews@gmail.com>
2023-03-13 06:44:25 +0800

    Update frhelper from 4.2.9 to 4.3.0

--- Casks/frhelper.rb
+++ Casks/frhelper.rb
@@ -1,6 +1,6 @@
 cask "frhelper" do
-  version "4.2.9,1081"
-  sha256 "5eae043981f58139f734caa86404bb524cc57ff621b35d5ab15375d7ee6a06dc"
+  version "4.3.0,1082"
+  sha256 "fc5b9336231c41a44197389dcff32c9ebcffc1a5f6f902b711f79dda173c42c9"
 
   url "https://static.frdic.com/pkg/fhmac.dmg?v=#{version.csv.second}",
       verified:   "static.frdic.com/",
