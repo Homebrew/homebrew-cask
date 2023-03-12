2b026dde2652c0fa1f715279db231ab5b7afa2ec
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-12 11:38:00 +0800

    Update keysmith.rb

--- Casks/keysmith.rb
+++ Casks/keysmith.rb
@@ -1,15 +1,15 @@
 cask "keysmith" do
-  version "1.18.4,97"
+  version "1.18.4"
   sha256 "6f27e3aca201ddc451f92a15ca31dc5193a8c94d5b3e03e7e0d5df62ac2b53ad"
 
-  url "https://keysmith.app/versions/Keysmith-#{version.csv.first}.dmg"
+  url "https://keysmith.app/versions/Keysmith-#{version}.dmg"
   name "Keysmith"
   desc "Create custom keyboard shortcuts for anything"
   homepage "https://www.keysmith.app/"
 
   livecheck do
     url "https://www.keysmith.app/versions/appcast.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   depends_on macos: ">= :catalina"
