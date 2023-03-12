e30f9350ea06913d41d20e4a26806e777dc2e5e9
Leo Pla <leopoldoplasempere@gmail.com>
2023-03-12 18:08:44 +0100

    Update aldente from 1.21 to 1.21.1 (#142927)

--- Casks/aldente.rb
+++ Casks/aldente.rb
@@ -1,6 +1,6 @@
 cask "aldente" do
-  version "1.21"
-  sha256 "b25a01e2db791964cb361ffd63f65fb4d521de956a02ffa4aab89c4655609ec5"
+  version "1.21.1"
+  sha256 "1b97ed2a4ae2c77151345e14bbdc935c556994be7763fb75654d738083f596c3"
 
   url "https://github.com/davidwernhart/AlDente/releases/download/#{version}/AlDente.dmg"
   name "AlDente"
