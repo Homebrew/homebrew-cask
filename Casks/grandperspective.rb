2fc176707f8ba0d331b824bb00584451e569f98c
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-13 06:47:40 -0400

    Update grandperspective from 3.0.1 to 3.1.0

--- Casks/grandperspective.rb
+++ Casks/grandperspective.rb
@@ -1,6 +1,6 @@
 cask "grandperspective" do
-  version "3.0.1"
-  sha256 "64faab94df5ac39abbeb9e869a6c429d3441c3796ef67f79dab232ba7f0cb222"
+  version "3.1.0"
+  sha256 "237b7045fe703801a551d4e7e08d31cff3001bc07ee55f2be8e3dd538c05e8d0"
 
   url "https://downloads.sourceforge.net/grandperspectiv/grandperspective/#{version}/GrandPerspective-#{version.dots_to_underscores}.dmg",
       verified: "downloads.sourceforge.net/grandperspectiv/"
