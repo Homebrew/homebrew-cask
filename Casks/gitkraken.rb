5c46d8be11ca66281694e3aca10682b185798d43
Amory Meltzer <Amorymeltzer@gmail.com>
2023-03-13 18:45:15 -0400

    Update gitkraken from 9.2.0 to 9.2.1

--- Casks/gitkraken.rb
+++ Casks/gitkraken.rb
@@ -1,9 +1,9 @@
 cask "gitkraken" do
   arch arm: "darwin-arm64", intel: "darwin"
 
-  version "9.2.0"
-  sha256 arm:   "9a4c9fed96548a924f81127282f7f201e10ee2a818ef2e546a94c445e62935ae",
-         intel: "a022185b7a9d4d34ab49ade625864e730c92e26334bf35cf49f665eab1b08a14"
+  version "9.2.1"
+  sha256 arm:   "8a3e1e60a757bce62a87fc9dcd810392878450b9d07e01120a0015d5f1eaac68",
+         intel: "e9039e53b1f3be42b3f29e3942d69db64d2dc47abe19f1d2e04c1904bb41a0c4"
 
   url "https://release.axocdn.com/#{arch}/GitKraken-v#{version}.zip",
       verified: "release.axocdn.com/"
