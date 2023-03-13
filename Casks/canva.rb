4c982ecdda22c062b327fd4ce7eea8399a1641f1
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-13 16:53:56 -0400

    Update canva from 1.61.1 to 1.62.0

--- Casks/canva.rb
+++ Casks/canva.rb
@@ -1,9 +1,9 @@
 cask "canva" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.61.1"
-  sha256 arm:   "a9ba98ad93c7bb48b9cc969f7393618bf99d088bc7d2d9e096daf621bde68aab",
-         intel: "b5789f8f263dd121dab25c935b33e143500b0e8890776744e56099e6cc166fa0"
+  version "1.62.0"
+  sha256 arm:   "7494aadd9a1e748dbcf078fc83a8bf103d2122f43d7127a6dfa47fa68b260be7",
+         intel: "fb37bd899a66496bf2b5b500845df6b837a9a29a22d5360a49fde3df73d3f4c5"
 
   url "https://desktop-release.canva-deploy.com/Canva-#{version}-#{arch}.dmg",
       verified: "desktop-release.canva-deploy.com/"
