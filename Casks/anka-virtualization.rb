f2e0d9685ceff8576af67247a3f5bfb3553fdec6
Nathan Pierce <NorseGaud@users.noreply.github.com>
2023-03-14 11:42:07 -0400

    Update anka-virtualization from 3.2.1.157 to 3.2.2.158 (#143019)
    * Update anka-virtualization from 3.2.1.157 to 3.2.2.158
    
    * Update anka-virtualization.rb
    
    ---------
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/anka-virtualization.rb
+++ Casks/anka-virtualization.rb
@@ -2,13 +2,13 @@ cask "anka-virtualization" do
   arch arm: "-arm", intel: "-intel"
   livecheck_folder = on_arch_conditional arm: "arm", intel: "intel"
 
+  on_arm do
+    version "3.2.2.158"
+    sha256 "19edf4e1802ef9925d6f4df5335f9963b911a8b1c99b1f387255062b3f21f292"
+  end
   on_intel do
     version "3.2.1.155"
     sha256 "5d78b31d318238d5289a887922ebdb65bdda20ea25fcb2dfab82a24cce4aabc6"
-  end
-  on_arm do
-    version "3.2.1.157"
-    sha256 "22d4e83a0c848028d01ae6a626ceb11b6ff19cbb36f4aafd0c146533a4639065"
   end
 
   url "https://downloads.veertu.com/anka/Anka-#{version}#{arch}.pkg"
