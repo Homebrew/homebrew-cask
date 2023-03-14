7ca5112acdf6b7fc54b62e2004be2ff625a163f3
miccal <miccal.matthews@gmail.com>
2023-03-14 12:04:32 +0800

    Update cardhop from 2.2.5 to 2.2.6

--- Casks/cardhop.rb
+++ Casks/cardhop.rb
@@ -1,6 +1,6 @@
 cask "cardhop" do
-  version "2.2.5,1126"
-  sha256 "06c3a7fee58716121f2a8dd812c4f0187c82356a95bda8d8496969830e508bf4"
+  version "2.2.6,1132"
+  sha256 "d15c083d131925516ce4aa120b7d175d8c8f18342977b96b3d91a8c04a4bd942"
 
   url "https://cdn.flexibits.com/Cardhop_#{version.csv.first}.zip"
   name "Cardhop"
