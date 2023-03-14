5999d1c34b619b6f4f466a96672901eafd1d12b1
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-14 12:32:25 +0800

    gdat: update `livecheck`

--- Casks/gdat.rb
+++ Casks/gdat.rb
@@ -2,14 +2,14 @@ cask "gdat" do
   version "2023r01"
   sha256 :no_check
 
-  url "https://drive.google.com/uc?export=download&id=1lnA5hFiCV9qzreQE7a5YzdeValaicTHD",
-      verified: "drive.google.com/uc?export=download&id=1lnA5hFiCV9qzreQE7a5YzdeValaicTHD"
+  url "https://drive.google.com/file/d/1lnA5hFiCV9qzreQE7a5YzdeValaicTHD/view?usp=sharing",
+      verified: "drive.google.com/file/d/1lnA5hFiCV9qzreQE7a5YzdeValaicTHD/"
   name "Genealogical DNA Analysis Tool"
   desc "App that utilizes autosomal DNA to aid in the research of family trees"
   homepage "https://www.getgdat.com/"
 
   livecheck do
-    url "https://sites.google.com/view/genealogical-dna-analysis-tool/home_1/download"
+    url "https://www.getgdat.com/home/download"
     regex(/Genealogical DNA Analysis Tool (\d+(?:.\d+)+)/i)
   end
 
