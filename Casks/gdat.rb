74b939b8d207bc651d6f085ef0b4c257dc9dda0c
Markus Reiter <me@reitermark.us>
2023-03-14 05:49:28 +0100

    Update gdat.rb

--- Casks/gdat.rb
+++ Casks/gdat.rb
@@ -2,8 +2,8 @@ cask "gdat" do
   version "2023r01"
   sha256 :no_check
 
-  url "https://drive.google.com/file/d/1lnA5hFiCV9qzreQE7a5YzdeValaicTHD/view?usp=sharing",
-      verified: "drive.google.com/file/d/1lnA5hFiCV9qzreQE7a5YzdeValaicTHD/"
+  url "https://drive.google.com/uc?export=download&id=1lnA5hFiCV9qzreQE7a5YzdeValaicTHD",
+      verified: "drive.google.com/uc?export=download&id=1lnA5hFiCV9qzreQE7a5YzdeValaicTHD"
   name "Genealogical DNA Analysis Tool"
   desc "App that utilizes autosomal DNA to aid in the research of family trees"
   homepage "https://www.getgdat.com/"
