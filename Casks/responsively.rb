c60a7c37af246f2c3cad8450682432cb43f16584
Bevan Kay <email@bevankay.me>
2023-03-15 10:24:33 +1100

    Update responsively from 1.0.3 to 1.1.0 (#143069)
    \n

--- Casks/responsively.rb
+++ Casks/responsively.rb
@@ -1,9 +1,9 @@
 cask "responsively" do
   arch arm: "-arm64"
 
-  version "1.0.3"
-  sha256 arm:   "279f8b8b8d2d9faa31a9a1dfb9c31fcbf43fd6103805db5190e710fdf9b14785",
-         intel: "387b1a11f44dc163a28b3106114299aec238bceac8e4c4f2719248efcb865c0b"
+  version "1.1.0"
+  sha256 arm:   "18c9e27010fececf587448b7c5473161940f818b48144427bd803282930fe5f3",
+         intel: "a11d0e577e8c8cb17d6e616d16a786b25426ebb478cb465c2ca9db447c476a17"
 
   url "https://github.com/responsively-org/responsively-app-releases/releases/download/v#{version}/ResponsivelyApp-#{version}#{arch}.dmg",
       verified: "github.com/responsively-org/responsively-app-releases/"
