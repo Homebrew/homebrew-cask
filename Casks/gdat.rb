e49a80c7320e6c7b900e778fcf5ec1ec8aee168e
Markus Reiter <me@reitermark.us>
2023-03-14 06:04:18 +0100

    Update gdat.rb

--- Casks/gdat.rb
+++ Casks/gdat.rb
@@ -1,16 +1,22 @@
 cask "gdat" do
-  version "2023r01"
-  sha256 :no_check
+  version "2023r01,1lnA5hFiCV9qzreQE7a5YzdeValaicTHD"
+  sha256 "cda240039696b18972b0fb9d7ebccab107baa41a2a87342515c73a4e29e6d4d7"
 
-  url "https://drive.google.com/uc?export=download&id=1lnA5hFiCV9qzreQE7a5YzdeValaicTHD",
-      verified: "drive.google.com/uc?export=download&id=1lnA5hFiCV9qzreQE7a5YzdeValaicTHD"
+  url "https://drive.google.com/uc?export=download&id=#{version.csv.second}",
+      verified: "drive.google.com/uc?export=download&id=#{version.csv.second}"
   name "Genealogical DNA Analysis Tool"
   desc "App that utilizes autosomal DNA to aid in the research of family trees"
   homepage "https://www.getgdat.com/"
 
   livecheck do
     url "https://www.getgdat.com/home/download"
-    regex(/Genealogical DNA Analysis Tool (\d+(?:.\d+)+)/i)
+    strategy :page_match do |page|
+      match = page.match(
+        %r{Genealogical DNA Analysis Tool (\d+(?:.\d+)+).*drive.google.com/file/d/([^/]+).*?Mac 64 Bit}i,
+      )
+
+      "#{match[1]},#{match[2]}" if match
+    end
   end
 
   app "OS X 64 bit/Genealogical DNA Analysis Tool.app"
