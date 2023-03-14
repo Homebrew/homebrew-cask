6274e46cbd4e2062e2b37ab98cbfe057e4d0d536
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-14 14:20:38 -0400

    Update geogebra from 6.0.760.0 to 6.0.764.0

--- Casks/geogebra.rb
+++ Casks/geogebra.rb
@@ -1,6 +1,6 @@
 cask "geogebra" do
-  version "6.0.760.0"
-  sha256 "44135ce15a4801f827781c92d857c853af6a8af742f6900a84e87880c9660e01"
+  version "6.0.764.0"
+  sha256 "cbc2b2fd4d6d9189ec79841e5b46e3fbc8398dbed7f6da4a993a29e5c1c217b1"
 
   url "https://download.geogebra.org/installers/#{version.major_minor}/GeoGebra-Classic-#{version.major}-MacOS-Portable-#{version.dots_to_hyphens}.zip"
   name "GeoGebra"
