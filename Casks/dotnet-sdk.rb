eaf8eafef2530c0e85e3e8c61cc0a3f5bb4e8e73
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-15 08:09:13 +0800

    Update dotnet-sdk.rb

--- Casks/dotnet-sdk.rb
+++ Casks/dotnet-sdk.rb
@@ -6,8 +6,8 @@ cask "dotnet-sdk" do
     sha256 "48a45898dc3e3a92f91f9e10c7d17a657e55399669e4cdf1917f1649c2338444"
   end
   on_intel do
-    version "7.0.201,225948f7-3889-4bdf-9cf4-184e81216700,be35fbca24493ffbe0062c2f798e4461"
-    sha256 "146f3ec4b682f0e3b1c9d1a0bc989d7998a6a26ec62e617d636669c59cd2f81e"
+    version "7.0.202,a87236b2-9ddd-4f48-ac81-d8d07a7cdac2,91a91d1eac4d598a6eaf5faf148f3afd"
+    sha256 "31e48e3bddddd7e30f839b949608c80994dd6172e4fd025f2789c31a48d93f5b"
   end
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv.second}/#{version.csv.third}/dotnet-sdk-#{version.csv.first}-osx-#{arch}.pkg"
