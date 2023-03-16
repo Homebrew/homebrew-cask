4298ee91a0f2a75fa6dfb2c2f705529593349f1e
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-16 10:20:49 +0800

    Update ghdl.rb

--- Casks/ghdl.rb
+++ Casks/ghdl.rb
@@ -1,8 +1,11 @@
 cask "ghdl" do
-  version "3.0.0"
-  sha256 "4fabe56ecb5affa39ebc5a7de5de43eb020b1cc3c0ab3ff1bc4354505cd4ee8e"
+  arch arm: "llvm", intel: "mcode"
 
-  url "https://github.com/ghdl/ghdl/releases/download/v#{version}/ghdl-macos-11-mcode.tgz"
+  version "3.0.0"
+  sha256 arm:   "cb8c6ad3e6763e7c346ccee111daf761416c7d54ac0704f8a2560641fc3c2d85",
+         intel: "4fabe56ecb5affa39ebc5a7de5de43eb020b1cc3c0ab3ff1bc4354505cd4ee8e"
+
+  url "https://github.com/ghdl/ghdl/releases/download/v#{version}/ghdl-macos-11-#{arch}.tgz"
   name "ghdl"
   desc "VHDL 2008/93/87 simulator"
   homepage "https://github.com/ghdl/ghdl/"
