71cf6e0fbf9919324725cc301f8b275b47a2ed29
doeber <47430879+doeber@users.noreply.github.com>
2023-03-17 01:58:01 +0100

    Update signal from 6.10.0 to 6.10.1 (#143192)

--- Casks/signal.rb
+++ Casks/signal.rb
@@ -1,9 +1,9 @@
 cask "signal" do
   arch arm: "arm64", intel: "x64"
 
-  version "6.10.0"
-  sha256 arm:   "32a56b3e016ef2f46c6e1ba5ca4453abc9a743fb50a0d2b8ab2977995c8d29e2",
-         intel: "b08f83e2fefcb97443bdc32deacff501fd859d3dc172c13600340072071e9f04"
+  version "6.10.1"
+  sha256 arm:   "405f1da035e23780843a6c95794cdb8cf3512360e7f9c6a7f298df419e8e41f1",
+         intel: "b8b1dcaab8f4e32505da9e65b4cfba75c4757755ce22982df18c846f51f5dfc0"
 
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{arch}-#{version}.dmg"
   name "Signal"
