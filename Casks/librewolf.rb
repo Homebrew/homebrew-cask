c27a99f44a367c9d3305b07806ee72d5fbb0a94b
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-17 23:00:53 +0800

    Update librewolf.rb

--- Casks/librewolf.rb
+++ Casks/librewolf.rb
@@ -1,17 +1,17 @@
 cask "librewolf" do
   arch arm: "aarch64", intel: "x86_64"
 
-  on_intel do
-    version "111.0,2,3a4c6f6cc727f4e805166bfaf6439323"
-    sha256 "b14910a74eadecf53aa672795fc7e0a97d9b5b6f77f9b44985bcf0f787e36a4f"
-  end
   on_arm do
     version "111.0,2,0240b8c0b64aa90c69ba14ecb44b4af0"
     sha256 "e5a5854914da6ec486105d07c45007d41a1ce0add491765b71837802c2c4adce"
   end
+  on_intel do
+    version "111.0,2,3a4c6f6cc727f4e805166bfaf6439323"
+    sha256 "b14910a74eadecf53aa672795fc7e0a97d9b5b6f77f9b44985bcf0f787e36a4f"
+  end
 
   url "https://gitlab.com/librewolf-community/browser/macos/uploads/#{version.csv.third}/librewolf-#{version.csv.first}-#{version.csv.second}.en-US.mac.#{arch}.dmg",
-      verified: "gitlab.com/librewolf-community/browser/macos/"
+      verified: "gitlab.com/librewolf-community/browser/macos/uploads/"
   name "LibreWolf"
   desc "Web browser"
   homepage "https://librewolf.net/"
