9b0951e07769000208367809577ef69e3c7734f7
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-17 17:01:55 -0400

    Update shift from 8.4.2.740 to 8.4.3.757 (#143254)

--- Casks/shift.rb
+++ Casks/shift.rb
@@ -1,9 +1,9 @@
 cask "shift" do
   arch arm: "arm64", intel: "x64"
 
-  version "8.4.2.740"
-  sha256 arm:   "661f367b8e70befb78f169f9a5d1df6b2209d2e83150d122425341977dd4a837",
-         intel: "1ced7716b9401908b10106d8e52ff5c462d7234956a93af15dcae0fb4d02a0d6"
+  version "8.4.3.757"
+  sha256 arm:   "1a1941385c3495131bb0ee98b25733317db750cde9f02bfefa7467266bba924c",
+         intel: "706022a2fad09744434123d3403c7c73e56452ebfc109fd178fdcd40a81d86d8"
 
   url "https://updates.tryshift.com/v#{version.major_minor_patch}/stable/shift-v#{version}-stable-#{arch}.dmg"
   name "Shift"
