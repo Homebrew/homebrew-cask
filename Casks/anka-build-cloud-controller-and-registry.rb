8951e3dcadbae6266d4042fc4407c8a4de8a6b79
Nathan Pierce <NorseGaud@users.noreply.github.com>
2023-03-16 20:58:27 -0400

    Update anka-build-cloud-controller-and-registry from 1.32.0-c375584c to 1.33.0-5465bb03 (#143191)

--- Casks/anka-build-cloud-controller-and-registry.rb
+++ Casks/anka-build-cloud-controller-and-registry.rb
@@ -1,6 +1,6 @@
 cask "anka-build-cloud-controller-and-registry" do
-  version "1.32.0-c375584c"
-  sha256 "c2b1b5f43e53a4c0e78642d0c8c3074f5f0d8eff88440c0abd2827d570e45094"
+  version "1.33.0-5465bb03"
+  sha256 "b41c8f944314eea9b891c928b45c239a234f3844678f67d756ccacabd8069c01"
 
   url "https://downloads.veertu.com/anka/AnkaControllerRegistry-#{version}.pkg"
   name "Anka Build Cloud Controller & Registry"
