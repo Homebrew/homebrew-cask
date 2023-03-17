c962d578155811f1874c60917aab1aa08c71f29c
Bevan Kay <email@bevankay.me>
2023-03-17 23:47:10 +1100

    Update webcatalog from 45.1.0 to 46.0.1

--- Casks/webcatalog.rb
+++ Casks/webcatalog.rb
@@ -1,6 +1,6 @@
 cask "webcatalog" do
-  version "45.1.0"
-  sha256 "ea9976cf14786247d1d765e80553d3a2134d242aeedbbcb9333e62311e978361"
+  version "46.0.1"
+  sha256 "6fbbaef43b445ceaeda85e6ceac3645b822be47810d69454400087565bfce223"
 
   url "https://cdn-2.webcatalog.io/webcatalog/WebCatalog-#{version}-universal.dmg"
   name "WebCatalog"
