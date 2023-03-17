6212127b71522d79a94c4f1ad21f8240d4de6a2b
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-17 19:40:42 -0400

    Update linphone from 5.0.13 to 5.0.14 (#143244)
    * Update linphone from 5.0.13 to 5.0.14
    
    * add zap stanza

--- Casks/linphone.rb
+++ Casks/linphone.rb
@@ -1,6 +1,6 @@
 cask "linphone" do
-  version "5.0.13"
-  sha256 "eb9550e88949b7e3d402658f4fbb9193e9db71a38933e121cc9e816b133c2ac9"
+  version "5.0.14"
+  sha256 "1d88f75661669f1305f83b3e11e6d1362713365374d3c3ab39b247d3f6cebe9a"
 
   url "https://download.linphone.org/releases/macosx/app/Linphone-#{version}-mac.dmg"
   name "Linphone"
@@ -13,4 +13,13 @@ cask "linphone" do
   end
 
   app "Linphone.app"
+
+  zap trash: [
+    "~/Documents/linphone",
+    "~/Library/Application Support/linphone",
+    "~/Library/Preferences/com.belledonnecommunications.linphone.plist",
+    "~/Library/Preferences/com.linphone.linphone.plist",
+    "~/Library/Preferences/linphone",
+    "~/Library/Saved Application State/com.belledonnecommunications.linphone.savedState",
+  ]
 end
