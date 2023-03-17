b2aadb8657d62b1fa72690550d0825bacf284389
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-16 20:59:54 -0400

    Update wrike from 4.1.0 to 4.1.1 (#143187)

--- Casks/wrike.rb
+++ Casks/wrike.rb
@@ -1,6 +1,6 @@
 cask "wrike" do
-  version "4.1.0"
-  sha256 "b26950c4a9584c905a6e954030120408839198d874d21edd50b28959914083ca"
+  version "4.1.1"
+  sha256 "ee41dff2dcadfb8bc670ee05fa22acef189b7bf38a5dc8abd5e80c3f1b0bd532"
 
   url "https://dl.wrike.com/download/WrikeDesktopApp.v#{version}.dmg"
   name "Wrike"
