0d8c0a7810834f693acd1f1ac111c750d3819a33
Issy Long <me@issyl0.co.uk>
2023-03-18 00:24:10 +0000

    jamulus: Fix `on_arch` and `on_os` block ordering

--- Casks/jamulus.rb
+++ Casks/jamulus.rb
@@ -2,9 +2,6 @@ cask "jamulus" do
   version "3.9.1"
   sha256 "724f15fd564737fe06c717aa678a8c3817a006f7105bb13b71d5b650ed8c7cc5"
 
-  url "https://downloads.sourceforge.net/llcon/jamulus_#{version}_mac.dmg",
-      verified: "downloads.sourceforge.net/llcon/"
-
   on_mojave :or_older do
     sha256 "6c645ba373205f774a09df59565e39808868b7ef3fb296a56f616ef90e74aefe"
 
@@ -12,6 +9,8 @@ cask "jamulus" do
         verified: "downloads.sourceforge.net/llcon/"
   end
 
+  url "https://downloads.sourceforge.net/llcon/jamulus_#{version}_mac.dmg",
+      verified: "downloads.sourceforge.net/llcon/"
   name "Jamulus"
   desc "Play music online with friends"
   homepage "https://jamulus.io/"
