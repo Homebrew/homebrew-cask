7f827654e84bfe3e70f93b06e9f94b00140aa54a
Issy Long <me@issyl0.co.uk>
2023-03-18 00:24:00 +0000

    anki: Fix `on_arch` and `on_os` block ordering

--- Casks/anki.rb
+++ Casks/anki.rb
@@ -5,9 +5,6 @@ cask "anki" do
   sha256 arm:   "cbaee7c65b32293d4fb81abc28c87c35ab692d588295a37a04830ca2b6a529d4",
          intel: "0d066c2a427c48c005a59dbf6954408a099bb1d9d6efd8d6ee8b4f39df18feff"
 
-  url "https://github.com/ankitects/anki/releases/download/#{version}/anki-#{version}-mac-#{arch}-qt6.dmg",
-      verified: "github.com/ankitects/anki/"
-
   on_high_sierra :or_older do
     sha256 "9e9fe5ba51a3f6c43a568d15aaa4acb74992f013af17a0211b0447e0742588ec"
 
@@ -15,6 +12,8 @@ cask "anki" do
         verified: "github.com/ankitects/anki/"
   end
 
+  url "https://github.com/ankitects/anki/releases/download/#{version}/anki-#{version}-mac-#{arch}-qt6.dmg",
+      verified: "github.com/ankitects/anki/"
   name "Anki"
   desc "Memory training application"
   homepage "https://apps.ankiweb.net/"
