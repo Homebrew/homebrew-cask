a83d7599b7c7e2bad2fa64b63c24e257f0e7b20c
Issy Long <me@issyl0.co.uk>
2023-03-18 00:24:04 +0000

    devbook: Fix `on_arch` and `on_os` block ordering

--- Casks/devbook.rb
+++ Casks/devbook.rb
@@ -1,16 +1,16 @@
 cask "devbook" do
   version "0.1.18"
 
-  on_intel do
-    sha256 "1294edfd9ecd586ffb0d1d2cf0247dde8b274b128e7c22c773c8645c8cd8e233"
-
-    url "https://download.todesktop.com/2102273jsy18baz/Devbook%20#{version}.dmg",
-        verified: "download.todesktop.com/"
-  end
   on_arm do
     sha256 "a1e7a0f821bf30dc9bd3b6dd07da8c28af51b415faafb80cc96f4eaed06d37e5"
 
     url "https://download.todesktop.com/2102273jsy18baz/Devbook%20#{version}-arm64.dmg",
+        verified: "download.todesktop.com/"
+  end
+  on_intel do
+    sha256 "1294edfd9ecd586ffb0d1d2cf0247dde8b274b128e7c22c773c8645c8cd8e233"
+
+    url "https://download.todesktop.com/2102273jsy18baz/Devbook%20#{version}.dmg",
         verified: "download.todesktop.com/"
   end
 
