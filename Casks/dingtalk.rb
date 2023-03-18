558a578908242ebd1821304c7f21b6673b79b872
Issy Long <me@issyl0.co.uk>
2023-03-18 00:24:05 +0000

    dingtalk: Fix `on_arch` and `on_os` block ordering

--- Casks/dingtalk.rb
+++ Casks/dingtalk.rb
@@ -3,13 +3,13 @@ cask "dingtalk" do
   folder = on_arch_conditional arm: "M1-Beta/"
   linkid = on_arch_conditional arm: "qd=2022mac-m1"
 
-  on_intel do
-    version "7.0.10.5_28436509"
-    sha256 "8311a41e392f170489a8c58aac3cd0cc555de8912f58108bc1911419ce5f7fe6"
-  end
   on_arm do
     version "7.0.12.5_28488321"
     sha256 "11acb4fc794ee7349d2715681e719ee0f43e9d95658f35b7198ccb90dd59442c"
+  end
+  on_intel do
+    version "7.0.10.5_28436509"
+    sha256 "8311a41e392f170489a8c58aac3cd0cc555de8912f58108bc1911419ce5f7fe6"
   end
 
   url "https://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/#{folder}DingTalk_v#{version}_#{arch}.dmg"
