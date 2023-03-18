14b0cff083fdf75606993d06b613542064ccb463
Issy Long <me@issyl0.co.uk>
2023-03-18 00:24:13 +0000

    librewolf: Fix `on_arch` and `on_os` block ordering

--- Casks/librewolf.rb
+++ Casks/librewolf.rb
@@ -1,13 +1,13 @@
 cask "librewolf" do
   arch arm: "aarch64", intel: "x86_64"
 
-  on_intel do
-    version "110.0,1,888ba593f6db715c053f5d3247c1b10e"
-    sha256 "93f41c758e5f1918a4061170f689022dfe4bce43420fe402958372892332639e"
-  end
   on_arm do
     version "110.0,1,fc1f7e860eb340beddce51556642a82b"
     sha256 "b9bfee951d72669db40f70f4585774acd884c52c60feb391cdd4072a06d92268"
+  end
+  on_intel do
+    version "110.0,1,888ba593f6db715c053f5d3247c1b10e"
+    sha256 "93f41c758e5f1918a4061170f689022dfe4bce43420fe402958372892332639e"
   end
 
   url "https://gitlab.com/librewolf-community/browser/macos/uploads/#{version.csv.third}/librewolf-#{version.csv.first}-#{version.csv.second}.en-US.mac.#{arch}.dmg",
