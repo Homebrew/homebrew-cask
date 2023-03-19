c4241f47a89c0d9e8c6060c1be6fe0d07123bda3
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-19 18:25:01 -0400

    Update rhino from 7.27.23032.13002 to 7.28.23058.03002

--- Casks/rhino.rb
+++ Casks/rhino.rb
@@ -1,6 +1,6 @@
 cask "rhino" do
-  version "7.27.23032.13002"
-  sha256 "d82da5bc5c40ff65251bc33e709310210fdbccce94fd36d177004fb42237b9a3"
+  version "7.28.23058.03002"
+  sha256 "50ae365346c2c8a14feeb957065f30942e08a66fb2d74cbbdea5c6186b3b4ac0"
 
   url "https://files.mcneel.com/rhino/#{version.major}/mac/releases/rhino_#{version}.dmg",
       verified: "mcneel.com/"
