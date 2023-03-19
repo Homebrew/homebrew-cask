26f61e00ef419be9ce9a57cbcada500fa07ba509
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-19 07:18:34 -0400

    Update mongodb-compass-readonly from 1.36.0 to 1.36.1

--- Casks/mongodb-compass-readonly.rb
+++ Casks/mongodb-compass-readonly.rb
@@ -1,9 +1,9 @@
 cask "mongodb-compass-readonly" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.36.0"
-  sha256 arm:   "a7336bafa8847e869caf2426b74193da6602638a7f739337a47942e11f19f6e9",
-         intel: "2516f80c0eeee1bddada664456cb27c4656d78fab4083c9122c5810fe1fb7ab5"
+  version "1.36.1"
+  sha256 arm:   "bc835735bf019a2fd524cd50dba5371b65b27ebe11b9b10259de8db087469eb6",
+         intel: "bb08bcee6396d308574d9b47be28972efc7b1bac53be4c04989b24489f2e1fd1"
 
   url "https://downloads.mongodb.com/compass/mongodb-compass-readonly-#{version}-darwin-#{arch}.dmg"
   name "MongoDB Compass Readonly"
