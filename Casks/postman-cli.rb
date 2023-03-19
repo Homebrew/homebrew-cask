2ff131b4f489bc3f3d75d51754e0ae07bb75ede7
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-19 06:33:47 -0400

    Update postman-cli from 1.1.0 to 1.1.1

--- Casks/postman-cli.rb
+++ Casks/postman-cli.rb
@@ -1,9 +1,9 @@
 cask "postman-cli" do
   arch arm: "osx_arm64", intel: "osx64"
 
-  version "1.1.0"
-  sha256 arm:   "00d29ea911a53cb034af26f0ee70d06e94d3b12ac02e0f98717a3cdfdeed30c3",
-         intel: "45871dedd18474ea13494f3493d5ef76ace4136d0eb673698f322157b529d864"
+  version "1.1.1"
+  sha256 arm:   "840f97bc7939b6d7f373cf20498b73bdc024c1bfc699f93c8b166e2f3584f043",
+         intel: "1339db7423900dcb94416825599e8be58438e31a1bae90544a1aaf503b80b5a4"
 
   url "https://dl-cli.pstmn.io/download/version/#{version}/#{arch}",
       verified: "dl-cli.pstmn.io/download/"
