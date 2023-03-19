d572a099ca7af021ced0e15a2822878c5801b209
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-19 18:21:38 -0400

    Update qownnotes from 23.3.3 to 23.3.6

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,6 +1,6 @@
 cask "qownnotes" do
-  version "23.3.3"
-  sha256 "c003e264c17030e050ce90913a577571d11b1734090e84d8039e6eddb18bbd90"
+  version "23.3.6"
+  sha256 "2d18ba5e52683547a704132cd1632e6cbc64b54bc24ac4b285c18f10ebb8d534"
 
   url "https://github.com/pbek/QOwnNotes/releases/download/v#{version}/QOwnNotes.dmg",
       verified: "github.com/pbek/QOwnNotes/"
