fb8836ba967dbe82f79bcaa2162a8f64eff1069e
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-19 06:34:36 -0400

    Update openaudible and fix download link

--- Casks/openaudible.rb
+++ Casks/openaudible.rb
@@ -1,11 +1,8 @@
 cask "openaudible" do
-  arch arm: "aarch64", intel: "x86_64"
+  version "3.7"
+  sha256 "5e6da9a2714ee33b6e776e16bc611e7e1ab60ce7e83bb7c90c1a9d7a1aae1a96"
 
-  version "3.6.3"
-  sha256 arm:   "fbd286aa141852081f438fa06c9ed1053453dce2d773cb9aaa3ee984cf7a2618",
-         intel: "455b03a543b030bfc5de5daf80e71e7f1bb7bf2a6c6c4c883817007b35750284"
-
-  url "https://github.com/openaudible/openaudible/releases/download/v#{version}/OpenAudible_#{version}_#{arch}.dmg",
+  url "https://github.com/openaudible/openaudible/releases/download/v#{version}/OpenAudible_#{version}.dmg",
       verified: "github.com/openaudible/openaudible/"
   name "OpenAudible"
   desc "Audiobook manager for Audible users"
