e732237a96ca42746d57a75cdd01e6a7fabcbac8
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-19 06:36:59 -0400

    Update tabby from 1.0.191 to 1.0.192

--- Casks/tabby.rb
+++ Casks/tabby.rb
@@ -1,9 +1,9 @@
 cask "tabby" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.0.191"
-  sha256 arm:   "a38f895a8a244fb41d585eea752d6f7b6c61a055a3fc22320c98d2bef4618077",
-         intel: "d5e51dcf99d28b21a10f3bd5f1917b217c921e76ff4f07fca8c3091f925c5e92"
+  version "1.0.192"
+  sha256 arm:   "16b17688c3af20b82d40831edf736817e013951ca2b7342aca6f4c057cf0dd88",
+         intel: "ec68d925515533aa878044ed38d88d32bdd4789ad1b36f9d71b6fa406748329e"
 
   url "https://github.com/Eugeny/tabby/releases/download/v#{version}/tabby-#{version}-macos-#{arch}.zip",
       verified: "github.com/Eugeny/tabby/"
