5b567414c497e80a14c97413775f3c0bc7626972
Amory Meltzer <Amorymeltzer@gmail.com>
2023-03-20 19:57:23 -0400

    Update jgrennison-openttd from 0.51.1 to 0.52.0

--- Casks/jgrennison-openttd.rb
+++ Casks/jgrennison-openttd.rb
@@ -1,6 +1,6 @@
 cask "jgrennison-openttd" do
-  version "0.51.1"
-  sha256 "e4bdbea338505c9903a9c892174a57441d17b83c14ad0e55b984544bd34b55b6"
+  version "0.52.0"
+  sha256 "eb6a03e53c183f176be947906ac2cf92d8ec420a9b8597ec6be918addccedc02"
 
   url "https://github.com/JGRennison/OpenTTD-patches/releases/download/jgrpp-#{version}/openttd-jgrpp-#{version}-macos-universal.dmg"
   name "JGR's OpenTTD Patchpack"
