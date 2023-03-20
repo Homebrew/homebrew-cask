b8c598586aea3776621ba2ed17d4903ddaac2cac
Amory Meltzer <Amorymeltzer@gmail.com>
2023-03-20 19:55:53 -0400

    Update iconset from 2.4.0 to 2.5.0

--- Casks/iconset.rb
+++ Casks/iconset.rb
@@ -1,9 +1,9 @@
 cask "iconset" do
   arch arm: "arm64-"
 
-  version "2.4.0"
-  sha256 arm:   "00eb6b91ccce50b39d35d44db423fcc62fc95a83a23479ecefcecaa4af31dc2e",
-         intel: "6a7a19b97ca5a0f0588f8e47307cccb881c72a2a9fa5ad8798dd8374b99c7f67"
+  version "2.5.0"
+  sha256 arm:   "ed567c877bb0b7ba6889edc857f7cc83deb1f4e8c6bfc692ae7eb97e9ef9452c",
+         intel: "20ec982a39fc513ade4526a1f87f30067931b70072b6cc2395cf5c17d4a28809"
 
   url "https://github.com/IconsetApp/iconset/releases/download/v#{version}/Iconset-#{version}-#{arch}mac.zip",
       verified: "github.com/IconsetApp/iconset/"
