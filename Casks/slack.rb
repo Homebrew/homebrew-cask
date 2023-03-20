11799d891b856430c91470a1b78ab5c3bb57eaf7
Bevan Kay <email@bevankay.me>
2023-03-21 09:06:52 +1100

    Update slack from 4.29.149 to 4.31.152

--- Casks/slack.rb
+++ Casks/slack.rb
@@ -1,9 +1,9 @@
 cask "slack" do
   arch arm: "arm64", intel: "x64"
 
-  version "4.29.149"
-  sha256 arm:   "367f9d143dc7fc1cbe6813cb0f19cf9de357b8597eb4774b87125779804c3918",
-         intel: "13462738f9da5857b5ee00a9172c31bb9b87b35a4492d035b54bb842abca858c"
+  version "4.31.152"
+  sha256 arm:   "ec227b3181732c554b1e2652f05573875a950256d30218344ccd9445a91ad0d6",
+         intel: "62e0c4d266de7f460769bdbafce1726718061fb72826bd8d7cd1f3470ab38132"
 
   url "https://downloads.slack-edge.com/releases/macos/#{version}/prod/#{arch}/Slack-#{version}-macOS.dmg",
       verified: "downloads.slack-edge.com/releases/macos/"
