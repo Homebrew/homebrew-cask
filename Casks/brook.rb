37988f7633c038cb1af985bcebc784b39fb04170
Amory Meltzer <Amorymeltzer@gmail.com>
2023-03-20 19:49:23 -0400

    Update brook from 20230122 to 20230401

--- Casks/brook.rb
+++ Casks/brook.rb
@@ -1,6 +1,6 @@
 cask "brook" do
-  version "20230122"
-  sha256 "4f9d14b46868bdec9086d19abb1ff1aa0cba4fc2a5ef6c63796e81b3320b1e74"
+  version "20230401"
+  sha256 "ac4fabcf5d907907be6c051feb6245eadad361d5375aa6ed15fabd01477bc3f2"
 
   url "https://github.com/txthinking/brook/releases/download/v#{version}/Brook.dmg"
   name "Brook"
