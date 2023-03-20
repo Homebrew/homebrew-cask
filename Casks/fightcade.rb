f8afc2abaa5165dbee354627a4a674b7158e4506
ninjawarrior1337 <me@treelar.xyz>
2023-03-19 17:02:44 -0700

    Add Fightcade2.app v2.1.38

---
+++ Casks/fightcade.rb
@@ -0,0 +1,20 @@
+cask "fightcade" do
+  version "2.1.38"
+  sha256 "30b0e8ef341dfb1ae935fa29e42bf7e54e4e9478440cda0e0430c8d0c38498ce"
+
+  url "https://web.fightcade.com/download/Fightcade-osx-v#{version}.tar.gz"
+  name "Fightcade"
+  desc "Matchmaking platform for retro gaming"
+  homepage "https://www.fightcade.com/"
+
+  livecheck do
+    url "https://web.fightcade.com/download/update.json"
+    strategy :json do |json|
+      json["mac"]["version"]
+    end
+  end
+
+  depends_on macos: ">= :high_sierra"
+
+  app "Fightcade2.app"
+end
