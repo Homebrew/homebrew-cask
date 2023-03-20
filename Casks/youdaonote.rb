6ced491426daf98aa468fe480813f8945f8e91a6
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-19 20:57:38 -0400

    Update youdaonote from 7.2.4 to 7.2.5 (#143385)

--- Casks/youdaonote.rb
+++ Casks/youdaonote.rb
@@ -1,6 +1,6 @@
 cask "youdaonote" do
-  version "7.2.4"
-  sha256 "ed5dd35d090760ce9b220db6e4d3d7c8b0ef5e877ca8492afef6ecd2a306c7d1"
+  version "7.2.5"
+  sha256 "a2f096489bcb4b55113430b23ee02640bd05902123752282e548b448344d170b"
 
   url "https://artifact.lx.netease.com/download/ynote-electron/%E6%9C%89%E9%81%93%E4%BA%91%E7%AC%94%E8%AE%B0-#{version}.zip",
       user_agent: :fake,
