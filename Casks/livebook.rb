d13856126030229166805630c8fa8fbc416e6821
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-21 08:22:40 +0800

    Update livebook.rb

--- Casks/livebook.rb
+++ Casks/livebook.rb
@@ -3,9 +3,9 @@ cask "livebook" do
   sha256 "f4adf55692a033c99293d0cfd25e7765ebed1ca5bf6b589048b3bcee27c419de"
 
   url "https://github.com/livebook-dev/livebook/releases/download/v#{version}/LivebookInstall-v#{version}-macos-universal.dmg",
-      verified: "github.com/livebook-dev/livebook"
+      verified: "github.com/livebook-dev/livebook/"
   name "Livebook"
-  desc "Code notebooks for Elixir Developers"
+  desc "Code notebooks for Elixir developers"
   homepage "https://livebook.dev/"
 
   app "Livebook.app"
