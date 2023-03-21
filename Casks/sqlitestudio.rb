1f14dcb082dc61efc52448932bd0034fba23ea50
LenaWil <lena.wildervanck@gmail.com>
2023-03-21 23:31:04 +0100

    Disable auto update in sqlite studio and add more paths to the trash (#143484)
    * Disable auto update in sqlite studio and add more paths to the trash
    
    While it does have update notifications, those just give you a link, you are still required to manually download and install the program itself, which makes brew the easier option.
    
    * Update sqlitestudio.rb
    
    ---------
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/sqlitestudio.rb
+++ Casks/sqlitestudio.rb
@@ -8,12 +8,13 @@ cask "sqlitestudio" do
   desc "Create, edit, browse SQLite databases"
   homepage "https://sqlitestudio.pl/"
 
-  auto_updates true
-
   app "SQLiteStudio.app"
 
   zap trash: [
     "~/.config/sqlitestudio",
+    "~/Library/Preferences/pl.com.salsoft.SQLiteStudio.plist",
+    "~/Library/Preferences/SalSoft/SQLiteStudio/",
     "~/Library/Saved Application State/com.yourcompany.SQLiteStudio.savedState",
+    "~/Library/Saved Application State/pl.com.salsoft.SQLiteStudio.savedState",
   ]
 end
