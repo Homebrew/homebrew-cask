c8cb449cf568a0f2664f20b3f20a9c5236d503f8
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-20 21:10:06 -0400

    Update dashcam-viewer from 3.8.9 to 3.9.1

--- Casks/dashcam-viewer.rb
+++ Casks/dashcam-viewer.rb
@@ -1,6 +1,6 @@
 cask "dashcam-viewer" do
-  version "3.8.9"
-  sha256 "b36a8db8a1f8a0c2be4baa83ac5c52e4ac847922ef3c81b39372e62f3fa5865a"
+  version "3.9.1"
+  sha256 "fadb44fa5e504b28542466b8d32ca38ce4927a3013139bd64a5e3472e286520c"
 
   url "https://filedn.com/l2s8TAtm4VASBX72ds0zYD8/dcv/Dashcam_Viewer_v#{version}.dmg",
       verified: "filedn.com/l2s8TAtm4VASBX72ds0zYD8/dcv/"
