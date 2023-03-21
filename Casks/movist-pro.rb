98dd625918a1300d91553b754b68f5b3de1b93be
Jake <46098567+jakefy@users.noreply.github.com>
2023-03-21 18:29:09 +0800

    Update movist-pro from 2.8.4 to 2.10.0

--- Casks/movist-pro.rb
+++ Casks/movist-pro.rb
@@ -1,6 +1,6 @@
 cask "movist-pro" do
-  version "2.8.4,191"
-  sha256 "4ece5bb4f028a09b73f89fadcdff44e538a862a33412f06122fcd2b8793ae8b9"
+  version "2.10.0,201"
+  sha256 "6bb088aa2a9de997c2f84e36287ddf383543654932cae90ce70d373e3706cd9c"
 
   url "https://s3.ap-northeast-2.amazonaws.com/update.cocoable.com/MovistPro_#{version.csv.first}.dmg",
       verified: "s3.ap-northeast-2.amazonaws.com/update.cocoable.com/"
