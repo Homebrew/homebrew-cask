8c6ab8add56b5baaa1c1bb78be4dc2378adf11a3
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-21 19:14:22 +0800

    Update movist-pro.rb

--- Casks/movist-pro.rb
+++ Casks/movist-pro.rb
@@ -1,8 +1,8 @@
 cask "movist-pro" do
-  version "2.10.0,201"
+  version "2.10.0"
   sha256 "6bb088aa2a9de997c2f84e36287ddf383543654932cae90ce70d373e3706cd9c"
 
-  url "https://s3.ap-northeast-2.amazonaws.com/update.cocoable.com/MovistPro_#{version.csv.first}.dmg",
+  url "https://s3.ap-northeast-2.amazonaws.com/update.cocoable.com/MovistPro_#{version}.dmg",
       verified: "s3.ap-northeast-2.amazonaws.com/update.cocoable.com/"
   name "Movist Pro"
   desc "Media player"
@@ -10,15 +10,17 @@ cask "movist-pro" do
 
   livecheck do
     url "https://s3.ap-northeast-2.amazonaws.com/update.cocoable.com/appcast.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
+
+  depends_on macos: ">= :high_sierra"
 
   app "Movist Pro.app"
 
   zap trash: [
     "~/Library/Application Scripts/com.movist.MovistPro.MovistSafariExtension",
-    "~/Library/Application Support/Movist Pro",
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.movist.movistpro.sfl2",
+    "~/Library/Application Support/Movist Pro",
     "~/Library/Caches/com.movist.MovistPro",
     "~/Library/Containers/com.movist.MovistPro.MovistSafariExtension",
     "~/Library/HTTPStorages/com.movist.MovistPro",
