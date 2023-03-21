306cf0600993e3fa4892cbb91546f5805c5c1d0b
JD Calvelli <58123790+jdcalvelli@users.noreply.github.com>
2023-03-21 19:54:23 -0400

    update godot mono cask to 4.0.1 (#143514)

--- Casks/godot-mono.rb
+++ Casks/godot-mono.rb
@@ -1,6 +1,6 @@
 cask "godot-mono" do
-  version "4.0"
-  sha256 "5ec01af38bd5f4096bc4c7b1999ccb7fbc7ebfd3f565ef6325739844b86c849c"
+  version "4.0.1"
+  sha256 "0746903e0e39304485a547b98bca8fbc19e93787d804771063c8b0c6ad8b41e2"
 
   url "https://downloads.tuxfamily.org/godotengine/#{version}/mono/Godot_v#{version}-stable_mono_macos.universal.zip",
       verified: "downloads.tuxfamily.org/godotengine/"
