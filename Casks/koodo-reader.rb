dd55cde57bb212db86c41e5a52e67be784d1b0eb
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-20 21:45:20 -0400

    Update koodo-reader from 1.5.2 to 1.5.3 (#143453)
    * Update koodo-reader from 1.5.2 to 1.5.3
    
    * add zap stanza

--- Casks/koodo-reader.rb
+++ Casks/koodo-reader.rb
@@ -1,9 +1,9 @@
 cask "koodo-reader" do
   arch arm: "-arm64", intel: "-x64"
 
-  version "1.5.2"
-  sha256 arm:   "95e2b0b6d6b0e0d1745b2820a8727e9af9143f868fd7582e9bd3e9a721e3bfbd",
-         intel: "1df8ba0f631aac086ffba6cbc5d3f1b29995f596635bf0f6db03feacb2ba8a64"
+  version "1.5.3"
+  sha256 arm:   "8f85f14aaad305d4bd506c23e1664c4e1bd66fdb9144935945244d86ff1eaa49",
+         intel: "c0e944d2ec3bdad5d90b5f88218b48f536cd1ae791e2395c29a781d7335b3130"
 
   url "https://github.com/troyeguo/koodo-reader/releases/download/v#{version}/Koodo-Reader-#{version}#{arch}.dmg",
       verified: "github.com/troyeguo/koodo-reader/"
@@ -12,4 +12,10 @@ cask "koodo-reader" do
   homepage "https://koodo.960960.xyz/"
 
   app "Koodo Reader.app"
+
+  zap trash: [
+    "~/Library/Application Support/koodo-reader",
+    "~/Library/Preferences/xyz.960960.koodo.plist",
+    "~/Library/Saved Application State/xyz.960960.koodo.savedState",
+  ]
 end
