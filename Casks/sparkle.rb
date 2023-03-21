9f34e26793ff3a0fafc73cfa2042cb3162f09669
Amory Meltzer ☀ <Amorymeltzer@gmail.com>
2023-03-20 20:55:47 -0400

    Update sparkle from 2.3.2 to 2.4.0 (#143444)
    * Update sparkle from 2.3.2 to 2.4.0
    
    * sparkle: Add zap

--- Casks/sparkle.rb
+++ Casks/sparkle.rb
@@ -1,6 +1,6 @@
 cask "sparkle" do
-  version "2.3.2"
-  sha256 "2b3fe6918ca20a83729aad34f8f693a678b714a17d33b5f13ca2d25edfa7eed3"
+  version "2.4.0"
+  sha256 "7bd2605a3a668c4965d671c9736ad2d179a093928fd450f8b11a0f15a0970b7c"
 
   url "https://github.com/sparkle-project/Sparkle/releases/download/#{version}/Sparkle-#{version}.tar.xz",
       verified: "github.com/sparkle-project/Sparkle/"
@@ -15,4 +15,12 @@ cask "sparkle" do
 
   app "Sparkle Test App.app"
   binary "sparkle.app/Contents/MacOS/sparkle"
+
+  zap trash: [
+    "~/Library/Application Scripts/org.sparkle-project.Downloader",
+    "~/Library/Application Scripts/org.sparkle-project.SparkleTestApp",
+    "~/Library/Containers/org.sparkle-project.Downloader",
+    "~/Library/Containers/org.sparkle-project.SparkleTestApp",
+    "~/Library/Preferences/org.sparkle-project.SparkleTestApp.plist",
+  ]
 end
