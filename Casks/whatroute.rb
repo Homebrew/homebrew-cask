d747abc0daef40d0966e19737a574c6a48f5b9bc
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-21 10:55:40 +0800

    Update whatroute.rb

--- Casks/whatroute.rb
+++ Casks/whatroute.rb
@@ -1,15 +1,15 @@
 cask "whatroute" do
-  version "2.6.1,11813"
+  version "2.6.1"
   sha256 "541937fe5a57425742d6d34e98b4195c82f9512453c7ea47a03eb779cedb0b04"
 
-  url "https://downloads.whatroute.net/software/whatroute-#{version.csv.first}.zip"
+  url "https://downloads.whatroute.net/software/whatroute-#{version}.zip"
   name "WhatRoute"
   desc "Network diagnostic utility"
   homepage "https://www.whatroute.net/"
 
   livecheck do
     url "https://www.whatroute.net/whatroute#{version.major}appcast.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   depends_on macos: ">= :high_sierra"
