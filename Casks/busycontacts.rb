739f279e631c16353308f7bdf5d79a8fa5be3e82
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-22 15:59:37 -0400

    bump version again

--- Casks/busycontacts.rb
+++ Casks/busycontacts.rb
@@ -1,6 +1,6 @@
 cask "busycontacts" do
-  version "2023.1.1,2023-03-22-10-22"
-  sha256 "d6e3f0b903862bbf362e81c5e0ca0782a6270e78db378b64847e6f25be4a389c"
+  version "2023.1.1,2023-03-22-21-30"
+  sha256 "dc4e5dfb6e6028b1a823ec0e2a2dfec4e5d9c72f7f0a0e44f602f0b377b43972"
 
   url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bct-#{version.csv.first}-#{version.csv.second}.zip",
       verified: "7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/"
