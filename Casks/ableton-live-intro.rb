0aabdaf93af43fed795986fae3619400b635b99f
Issy Long <me@issyl0.co.uk>
2023-03-22 23:58:45 +0000

    ableton-live-intro: Fix new RuboCop `Cask/NoOverrides` offenses

--- Casks/ableton-live-intro.rb
+++ Casks/ableton-live-intro.rb
@@ -1,10 +1,12 @@
 cask "ableton-live-intro" do
-  arch = "intel"
-  version "11.2.11"
-  sha256 "68ae4aa7c1a4286f77de340f5e2556442ea17a73976c3f90c9dce1c75dc8234a"
+  arch arm: "universal", intel: "intel"
 
+  version "11.2.11"
+
+  on_high_sierra :or_older do
+    sha256 "68ae4aa7c1a4286f77de340f5e2556442ea17a73976c3f90c9dce1c75dc8234a"
+  end
   on_mojave :or_newer do
-    arch = "universal"
     sha256 "70fb6f33c7e03f7c6290c7f5d5a6bee4454df272527c5fdd5dfd045b7d76f1eb"
   end
 
