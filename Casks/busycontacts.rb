3dab0c3deb7047a3c5a29cce899dcdfa09aece68
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-22 16:36:56 -0400

    Update busycontacts from 2022.4.5 to 2023.1.1 (#143549)
    * Update busycontacts from 2022.4.5 to 2023.1.1
    
    * add zap stanza
    
    * update zap stanza
    
    * bump version again

--- Casks/busycontacts.rb
+++ Casks/busycontacts.rb
@@ -1,6 +1,6 @@
 cask "busycontacts" do
-  version "2022.4.5,2023-02-13-23-25"
-  sha256 "1d4277cb675f62ec990e82ad747454af2646bf8aa4035ae92ead7169deaf40e6"
+  version "2023.1.1,2023-03-22-21-30"
+  sha256 "dc4e5dfb6e6028b1a823ec0e2a2dfec4e5d9c72f7f0a0e44f602f0b377b43972"
 
   url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bct-#{version.csv.first}-#{version.csv.second}.zip",
       verified: "7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/"
@@ -25,4 +25,14 @@ cask "busycontacts" do
   uninstall pkgutil: "com.busymac.busycontacts.pkg",
             quit:    "com.busymac.busycontacts",
             signal:  ["KILL", "com.busymac.busycontacts"]
+
+  zap trash: [
+    "~/Library/Application Scripts/com.busymac.busycontacts",
+    "~/Library/Application Scripts/N4RA379GBW.com.busymac.busycontacts",
+    "~/Library/Application Scripts/N4RA379GBW.com.busymac.contacts/",
+    "~/Library/Application Support/Mail/BusyContacts/",
+    "~/Library/Containers/com.busymac.busycontacts",
+    "~/Library/Group Containers/N4RA379GBW.com.busymac.busycontacts",
+    "~/Library/Group Containers/N4RA379GBW.com.busymac.contacts",
+  ]
 end
