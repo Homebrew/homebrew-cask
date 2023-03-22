369afcd87c90cdc70008228481a909e1578b78e5
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-22 12:56:02 -0400

    Update downie from 4.6.11 to 4.6.12

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,6 +1,6 @@
 cask "downie" do
-  version "4.6.11,4533"
-  sha256 "752d17a5185d8770289a3548dc77b0d3b0f4c8e6afbd6108871c542e04a75933"
+  version "4.6.12,4538"
+  sha256 "b07023328ac506664ea509c3ed0f87bd23ae6788cf1755afab9b2399bf3b822b"
 
   url "https://software.charliemonroe.net/trial/downie/v#{version.major}/Downie_#{version.major}_#{version.csv.second}.dmg"
   name "Downie"
