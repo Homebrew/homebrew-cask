3216c11e3aca11d098c29423ef05ae3eece8d380
Amory Meltzer <Amorymeltzer@gmail.com>
2023-03-22 19:21:28 -0400

    Update openaudible from 3.7.1 to 3.7.2

--- Casks/openaudible.rb
+++ Casks/openaudible.rb
@@ -1,6 +1,6 @@
 cask "openaudible" do
-  version "3.7.1"
-  sha256 "44daf82b5759976a344a30024ba17414b97bc3663082f1d086c3eb7835b36b53"
+  version "3.7.2"
+  sha256 "145ce7a5d32310254724f8edc3885149dd00447fb73a44e6db6d1cad85173c13"
 
   url "https://github.com/openaudible/openaudible/releases/download/v#{version}/OpenAudible_#{version}.dmg",
       verified: "github.com/openaudible/openaudible/"
