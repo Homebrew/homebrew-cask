765d4fecf5851d282d9f15154dd10b3185ac7ca1
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-22 13:06:16 -0400

    Update quicken from 6.12.4 to 6.12.5

--- Casks/quicken.rb
+++ Casks/quicken.rb
@@ -1,6 +1,6 @@
 cask "quicken" do
-  version "6.12.4,612.47934.100"
-  sha256 "638fe5876b61542e32104243de3a65f70454b2b3154234d7eb127099a2741d96"
+  version "6.12.5,612.47953.100"
+  sha256 "c3eab60eb92f7fc2f2db1322dc6b31a92f1e7e23b7f22f3d6dfef8770336d5f0"
 
   url "https://download.quicken.com/mac/Quicken/001/Release/031A96D9-EFE6-4520-8B6A-7F465DDAA3E4/Quicken-#{version.csv.second}/Quicken-#{version.csv.second}.zip"
   name "Quicken"
