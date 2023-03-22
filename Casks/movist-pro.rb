7f563ca70c6d32d3b8a895481e07d07a4622bab3
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-22 13:03:08 -0400

    Update movist-pro from 2.10.1 to 2.10.2

--- Casks/movist-pro.rb
+++ Casks/movist-pro.rb
@@ -1,6 +1,6 @@
 cask "movist-pro" do
-  version "2.10.1"
-  sha256 "fb6e289a3a5fd9c66961dcf821945ab31c4a449f38dfd7e9554238f5234d6ca5"
+  version "2.10.2"
+  sha256 "c3ef07b465f66ae4a5fc138c54928861497f14e106c322b32aa0178fd653a3fb"
 
   url "https://s3.ap-northeast-2.amazonaws.com/update.cocoable.com/MovistPro_#{version}.dmg",
       verified: "s3.ap-northeast-2.amazonaws.com/update.cocoable.com/"
