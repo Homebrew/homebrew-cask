73e19aa7540cb6838befd379af309f34be81df50
miccal <miccal.matthews@gmail.com>
2023-03-22 08:42:32 +0800

    Various: Fix arch ordering

--- Casks/feishu.rb
+++ Casks/feishu.rb
@@ -1,18 +1,18 @@
 cask "feishu" do
   arch arm: "arm64", intel: "x64"
 
-  on_intel do
-    version "6.0.5,8dde8b15"
-    sha256 "41f5678ec9c60888ba7ffe729fed326170318d40192271f75bd83d6c439488d9"
-  end
   on_arm do
     version "6.0.5,6bd3688a"
     sha256 "62156ddc37e1f8e701fe15ab31645855cb523933bdbc809e5aea45ca7e0b5419"
   end
+  on_intel do
+    version "6.0.5,8dde8b15"
+    sha256 "41f5678ec9c60888ba7ffe729fed326170318d40192271f75bd83d6c439488d9"
+  end
 
   url "https://sf3-cn.feishucdn.com/obj/ee-appcenter/#{version.csv.second}/Feishu-darwin_#{arch}-#{version.csv.first}-signed.dmg",
-      verified: "sf3-cn.feishucdn.com/"
-  name "feishu"
+      verified: "sf3-cn.feishucdn.com/obj/ee-appcenter/"
+  name "Feishu"
   desc "Project management software"
   homepage "https://www.feishu.cn/"
 
