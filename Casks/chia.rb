256628a1c8bedc17e3cb6b602aa01e2fa2076870
Jeremy Chabernaud <djerfy@gmail.com>
2023-03-22 21:34:59 +0100

    Update chia from 1.7.0 to 1.7.1 (#143563)

--- Casks/chia.rb
+++ Casks/chia.rb
@@ -1,9 +1,9 @@
 cask "chia" do
   arch arm: "-arm64"
 
-  version "1.7.0"
-  sha256 arm:   "fa5711369ff7185d5f45abfdc938a4fd93e29729713309ec658cae5b16f1bf68",
-         intel: "29489cdcef2099ee62e5c075cb67c84736d957ee23c16d875ef79d5e69e05c2e"
+  version "1.7.1"
+  sha256 arm:   "276a8b22a3f904c7c93f6e69231fac15b2a80550dad3c9a7a490767773888215",
+         intel: "1e0a8ee0d459ab47d1997691a49dd52c4d54c26f9f13fd105c0de8aebfadd539"
 
   url "https://github.com/Chia-Network/chia-blockchain/releases/download/#{version}/Chia-#{version}#{arch}.dmg",
       verified: "github.com/Chia-Network/chia-blockchain/"
