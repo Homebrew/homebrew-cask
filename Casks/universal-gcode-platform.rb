b90c313b743ed3644ff261c340b1ed06ad685d2e
Amory Meltzer <Amorymeltzer@gmail.com>
2023-03-22 19:32:31 -0400

    Update universal-gcode-platform from 2.0.14 to 2.0.15

--- Casks/universal-gcode-platform.rb
+++ Casks/universal-gcode-platform.rb
@@ -1,6 +1,6 @@
 cask "universal-gcode-platform" do
-  version "2.0.14"
-  sha256 "0937e56fb98ca205d9c3ca8a9029b074c23b4c5c33813154089c02ef114d6901"
+  version "2.0.15"
+  sha256 "8120458b2688b1c7637af8275eb2952661e896d037730ebc3f3f3e93bb421420"
 
   url "https://github.com/winder/Universal-G-Code-Sender/releases/download/v#{version}/ios-ugs-platform-app-#{version}.dmg",
       verified: "github.com/winder/Universal-G-Code-Sender/"
