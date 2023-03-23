d9147acaea5117c5acd7877925280e8aa54bb1f3
Amory Meltzer ☀ <Amorymeltzer@gmail.com>
2023-03-22 20:30:30 -0400

    Update appium-inspector from 2023.2.1 to 2023.3.1 (#143569)

--- Casks/appium-inspector.rb
+++ Casks/appium-inspector.rb
@@ -1,6 +1,6 @@
 cask "appium-inspector" do
-  version "2023.2.1"
-  sha256 "995ec315ff3db907b308bb5afe99f9e412947b2308cf03a3dc1a906da177aa1a"
+  version "2023.3.1"
+  sha256 "4c737e70451debdcad8aa825027c556388065e9102a8bb3f4223485c1821e42c"
 
   url "https://github.com/appium/appium-inspector/releases/download/v#{version}/Appium-Inspector-#{version}-universal-mac.zip"
   name "Appium Inspector GUI"
