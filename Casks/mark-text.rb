16fcdd27049b738c3dffd47e71288d348bfe86cb
thtliife <9210782+thtliife@users.noreply.github.com>
2023-03-23 22:22:18 +1000

    feat: add arm version to cask
    also added caveat explaining the arm version is unsigned, causing
    the "damaged package" error if quarnatine attr is set

--- Casks/mark-text.rb
+++ Casks/mark-text.rb
@@ -1,8 +1,11 @@
 cask "mark-text" do
-  version "0.17.1"
-  sha256 "83320faad3b217079f6638b1f4169ffc37465f255a93e262d1646f2a5f53f263"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://github.com/marktext/marktext/releases/download/v#{version}/marktext-x64.dmg"
+  version "0.17.1"
+  sha256 arm:   "703e5411b80514c867b4e9ce26dde5c16c416158ef45c6479256b6818aea5acf",
+         intel: "daba6e7358dc3488e29ffd786d5bf49ba41da66a53df91b11065d68c68846ee5"
+
+  url "https://github.com/marktext/marktext/releases/download/v#{version}/marktext-#{arch}-mac.zip"
   name "MarkText"
   desc "Simple and elegant markdown editor"
   homepage "https://github.com/marktext/marktext"
@@ -15,4 +18,14 @@ cask "mark-text" do
     "~/Library/Saved Application State/com.github.marktext.marktext.savedState",
     "~/Library/Logs/marktext",
   ]
+
+  caveats do
+    "The apple silicon (arm) version of #{token} is not signed, and may
+display an error stating it is damaged and can't be openedif it is not
+installed installed using the --no-quarantine option.
+
+You can also use the command:
+    xattr -dr com.apple.quarantine /Applications/MarkText.app
+to allow it to run if installed without the --no-quarantine option."
+  end
 end
