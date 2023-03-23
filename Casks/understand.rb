a2b5ce1fe5a31d5522e2c52d0958a258a6725e81
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-23 13:34:51 -0400

    Update understand from 6.3.1135 to 6.3.1136

--- Casks/understand.rb
+++ Casks/understand.rb
@@ -1,6 +1,6 @@
 cask "understand" do
-  version "6.3.1135"
-  sha256 "072ce8e16da132dda931c0ebf70b1d28d399413700f11d4d883029f08ec1275a"
+  version "6.3.1136"
+  sha256 "bd228615f9c837b8450a131c9ed61eb5c383c9cdfc202dd046642e283d6d307f"
 
   url "https://latest.scitools.com/Understand/Understand-#{version}-macOS-Universal.dmg"
   name "SciTools Understand"
