a50195dcf77b223b97cc3bf1c14948f7be4a816d
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-24 00:01:57 +0800

    Update mark-text.rb

--- Casks/mark-text.rb
+++ Casks/mark-text.rb
@@ -2,25 +2,25 @@ cask "mark-text" do
   arch arm: "arm64", intel: "x64"
 
   version "0.17.1"
-  sha256 arm:   "703e5411b80514c867b4e9ce26dde5c16c416158ef45c6479256b6818aea5acf",
-         intel: "daba6e7358dc3488e29ffd786d5bf49ba41da66a53df91b11065d68c68846ee5"
+  sha256 arm:   "c7fb2f9917b0626999d4fef9a1827ccc515afb1c32f7453977af5c1cbcd9de4f",
+         intel: "83320faad3b217079f6638b1f4169ffc37465f255a93e262d1646f2a5f53f263"
 
-  url "https://github.com/marktext/marktext/releases/download/v#{version}/marktext-#{arch}-mac.zip"
+  url "https://github.com/marktext/marktext/releases/download/v#{version}/marktext-#{arch}.dmg"
   name "MarkText"
-  desc "Simple and elegant markdown editor"
+  desc "Markdown editor"
   homepage "https://github.com/marktext/marktext"
 
   app "MarkText.app"
 
   zap trash: [
     "~/Library/Application Support/marktext",
+    "~/Library/Logs/marktext",
     "~/Library/Preferences/com.github.marktext.marktext.plist",
     "~/Library/Saved Application State/com.github.marktext.marktext.savedState",
-    "~/Library/Logs/marktext",
   ]
 
-  caveats do
-    "The apple silicon (arm) version of #{token} is not signed, and will
-display an error stating it is damaged and can't be opened.
-  end
+  caveats <<~EOS
+    The apple silicon (arm) version of #{token} is not signed, and 
+    will display an error stating it is damaged and can't be opened.
+  EOS
 end
