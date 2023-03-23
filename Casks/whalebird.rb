a2f504b97d27074c70a43c7e6a434f367acc55d3
AkiraFukushima <h3.poteto@gmail.com>
2023-03-23 20:13:57 +0900

    Specify arm64 and intel arch dmg for whalebird (#143543)

--- Casks/whalebird.rb
+++ Casks/whalebird.rb
@@ -1,11 +1,14 @@
 cask "whalebird" do
-  version "5.0.2"
-  sha256 "d7cd43fbeea83f13bba954fafe3aa1b01ed023bdaab29fbd8b1efc2ca6a43794"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://github.com/h3poteto/whalebird-desktop/releases/download/v#{version}/Whalebird-#{version}-darwin-universal.dmg",
+  version "5.0.2"
+  sha256 arm:   "b35f47bb5719b77eabfcba9d1e0af72813a6536cc48fe88b6b533a412c0d0646",
+         intel: "72e0f2a1f9ea7a9e5759237c1b3fe0dd836918187e08054f376da2379bc8957c"
+
+  url "https://github.com/h3poteto/whalebird-desktop/releases/download/v#{version}/Whalebird-#{version}-darwin-#{arch}.dmg",
       verified: "github.com/h3poteto/whalebird-desktop/"
   name "Whalebird"
-  desc "Mastodon, Pleroma and Misskey client"
+  desc "Mastodon, Pleroma, and Misskey client"
   homepage "https://whalebird.social/"
 
   app "Whalebird.app"
@@ -13,7 +16,7 @@ cask "whalebird" do
   zap trash: [
     "~/Library/Application Support/Whalebird",
     "~/Library/Logs/Whalebird",
-    "~/Library/Preferences/org.whalebird.desktop.plist",
-    "~/Library/Saved Application State/org.whalebird.desktop.savedState",
+    "~/Library/Preferences/social.whalebird.app.plist",
+    "~/Library/Saved Application State/social.whalebird.app.savedState",
   ]
 end
