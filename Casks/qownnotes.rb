8f37729864d2c49886e15b2c173947ee54593ad9
Amory Meltzer ☀ <Amorymeltzer@gmail.com>
2023-03-22 20:29:13 -0400

    Update qownnotes from 23.3.7 to 23.3.8 (#143587)

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,6 +1,6 @@
 cask "qownnotes" do
-  version "23.3.7"
-  sha256 "7592d88a48fb0e0d89dd752b2bc8f5388ad07da8897d445efa67af6bf4eea0b8"
+  version "23.3.8"
+  sha256 "b3b7e985a668c234bc28aaa8ac429c3a3a817d5ee2cb8b822225de0cd6d821eb"
 
   url "https://github.com/pbek/QOwnNotes/releases/download/v#{version}/QOwnNotes.dmg",
       verified: "github.com/pbek/QOwnNotes/"
