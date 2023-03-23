e29ddef1ead3cf9f39207d81296142b67ed9f26f
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-23 13:29:43 -0400

    Update sunsama from 2.0.6 to 2.0.7

--- Casks/sunsama.rb
+++ Casks/sunsama.rb
@@ -1,9 +1,9 @@
 cask "sunsama" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.0.6"
-  sha256 arm:   "37ba553284bc770ff3320389d4178e751129692d2f410ef6ce24f5fd4ce15749",
-         intel: "04f178d45f142f2208de289bda024e996ad3979490e0a4a51281e80d788160fd"
+  version "2.0.7"
+  sha256 arm:   "1871a114eece0afd50e16e1202ed083b9a1becfbcdbc6e920902479c722e5d82",
+         intel: "6652d075f92bc528a86c541e3b0bcdbbb37ae59f902056007f2f6efd8543ddc3"
 
   url "https://download.todesktop.com/2003096gmmnl0g1/Sunsama%20#{version}-#{arch}.dmg",
       verified: "download.todesktop.com/2003096gmmnl0g1/"
