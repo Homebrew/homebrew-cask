dd66daf4b6002fd4686afda39bc769e81725afe1
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-23 04:23:51 -0400

    Fix pulsar download links (#143630)
    fix pulsar download

--- Casks/pulsar.rb
+++ Casks/pulsar.rb
@@ -1,12 +1,19 @@
 cask "pulsar" do
-  arch arm: "Silicon", intel: "Intel"
+  arch arm: "-arm64"
 
   version "1.103.0"
   sha256 arm:   "87d540ee1df71e8763e3c615747cd0ef8810861ed46c93b1605edbae63ec9818",
          intel: "32285e0622f10ded87b98b2df77cd6f0d20465fbaf5b569b703d0d083370e929"
 
-  url "https://github.com/pulsar-edit/pulsar/releases/download/v#{version}/#{arch}.Mac.Pulsar-#{version}-mac.zip",
-      verified: "github.com/pulsar-edit/pulsar/"
+  on_arm do
+    url "https://github.com/pulsar-edit/pulsar/releases/download/v#{version}/Silicon.Mac.Pulsar-#{version}#{arch}-mac.zip",
+        verified: "github.com/pulsar-edit/pulsar/"
+  end
+  on_intel do
+    url "https://github.com/pulsar-edit/pulsar/releases/download/v#{version}/Intel.Mac.Pulsar-#{version}#{arch}-mac.zip",
+        verified: "github.com/pulsar-edit/pulsar/"
+  end
+
   name "Pulsar"
   desc "Text editor"
   homepage "https://pulsar-edit.dev/"
