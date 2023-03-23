aff34945a168913269744238725b743290a81117
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-22 20:32:04 -0400

    Update dcp-o-matic-playlist-editor from 2.16.47 to 2.16.48

--- Casks/dcp-o-matic-playlist-editor.rb
+++ Casks/dcp-o-matic-playlist-editor.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-playlist-editor" do
-  version "2.16.47"
-  sha256 "829e48e1e841fac38d7c4ddf9e6ba89b81cb990fe502e78c2c7f380040faf458"
+  version "2.16.48"
+  sha256 "5c793493714a0682caaf9054aa6c0634530f148376dd6f8c9230981e3d97c0e2"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-playlist&version=#{version}"
   name "DCP-o-matic Playlist Editor"
