c60ac635a4abe7f5a67f78f825d8e120dca71cc8
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-22 20:34:50 -0400

    Update dcp-o-matic-kdm-creator from 2.16.47 to 2.16.48 (#143623)

--- Casks/dcp-o-matic-kdm-creator.rb
+++ Casks/dcp-o-matic-kdm-creator.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-kdm-creator" do
-  version "2.16.47"
-  sha256 "04bdedcd904c3929eea2ebb8890740b87bc698c6c0b5eadaf00715b5dfe1d9c8"
+  version "2.16.48"
+  sha256 "561fda41858c3e70a3cff136779b059b3eb374527fa2dcd96c0f61e330ecef67"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-kdm&version=#{version}"
   name "DCP-o-matic KDM Creator"
