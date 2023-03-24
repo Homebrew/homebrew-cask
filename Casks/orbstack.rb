f9a0880811ebe8fb3a5b589c68ace131ee9f28fe
Timothy Clark <git@tdhc.uk>
2023-03-24 20:05:57 +0000

    Fixes from brew audit

--- Casks/orbstack.rb
+++ Casks/orbstack.rb
@@ -2,6 +2,7 @@ cask "orbstack" do
   arch arm:   "arm64",
        intel: "amd64"
 
+  version :latest
   sha256 :no_check
 
   url "https://orbstack.dev/download/beta/latest/#{arch}"
@@ -9,13 +10,11 @@ cask "orbstack" do
   desc "Run Docker and Linux with this drop-in replacement for Docker Desktop"
   homepage "https://orbstack.dev/"
 
-  auto_updates true
-
-  app "OrbStack.app"
-
   livecheck do
     skip "No version information available"
   end
+
+  app "OrbStack.app"
 
   zap trash: "~/.orbstack"
 end
