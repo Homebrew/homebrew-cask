d58aa996ab8c452396b6cda189ab7f4e1e221610
Ondřej Führer <ondrejfuhrer@users.noreply.github.com>
2023-03-24 13:47:10 +0100

    Update electron from 23.1.4 to 23.2.0 (#143697)

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,9 +1,9 @@
 cask "electron" do
   arch arm: "arm64", intel: "x64"
 
-  version "23.1.4"
-  sha256 arm:   "80233ff1b5e5bbae9cbb2e0d2e15af26e4ea9b5571d77f547951d043f6aaaaf3",
-         intel: "442947513094387e0457d56beea37849e33ac8ec736b3c3d9e3350ad25c2d30c"
+  version "23.2.0"
+  sha256 arm:   "cbdfac23b6a0500d1ebd8baec7463112ce8728d49a48d8398c0dbcc6da125aa4",
+         intel: "1e1020540929dc5f8b7516e4260ddd937486fe9080c69b3a0d13c9e07e1436c0"
 
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-#{arch}.zip",
       verified: "github.com/electron/electron/"
