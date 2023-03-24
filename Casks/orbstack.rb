9f58ebad76de6d3d4c82a548124dd3db24cd3b8f
Jacob Ford <j@cobford.com>
2023-03-24 18:03:28 -0400

    orbstack: fix url

--- Casks/orbstack.rb
+++ Casks/orbstack.rb
@@ -5,7 +5,7 @@ cask "orbstack" do
   sha256 arm:   "beb2bc4570b402056d9d6f4c73b78d06bfba4bc2e5e9f8aa68d16a9e9c06018c",
          intel: "c581b59f0037cd4d4d14ef432f1ec6b4ca5683e5fe19aca665f7050c851ede2a"
 
-  url "https://cdn-updates.orbstack.dev/amd64/OrbStack_v#{version}_#{arch}.dmg"
+  url "https://cdn-updates.orbstack.dev/#{arch}/OrbStack_v#{version}_#{arch}.dmg"
   name "OrbStack"
   desc "Replacement for Docker Desktop"
   homepage "https://orbstack.dev/"
