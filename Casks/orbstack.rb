5e5473bcd3b587172b019e929cde4e4ff68245dc
Timothy Clark <git@tdhc.uk>
2023-03-24 19:58:42 +0000

    Fix livecheck

--- Casks/orbstack.rb
+++ Casks/orbstack.rb
@@ -1,11 +1,10 @@
 cask "orbstack" do
-  arch arm: "arm64", intel: "amd64"
+  arch arm:   "arm64",
+       intel: "amd64"
 
-  version "v0.5.0_924"
-  sha256 arm:   "beb2bc4570b402056d9d6f4c73b78d06bfba4bc2e5e9f8aa68d16a9e9c06018c",
-         intel: "c581b59f0037cd4d4d14ef432f1ec6b4ca5683e5fe19aca665f7050c851ede2a"
+  sha256 :no_check
 
-  url "https://cdn-updates.orbstack.dev/#{arch}/OrbStack_#{version}_#{arch}.dmg"
+  url "https://orbstack.dev/download/beta/latest/#{arch}"
   name "orbstack"
   desc "Run Docker and Linux with this drop-in replacement for Docker Desktop"
   homepage "https://orbstack.dev/"
@@ -13,6 +12,10 @@ cask "orbstack" do
   auto_updates true
 
   app "OrbStack.app"
+
+  livecheck do
+    skip "No version information available"
+  end
 
   zap trash: "~/.orbstack"
 end
