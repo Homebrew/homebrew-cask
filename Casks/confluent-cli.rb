49703fd8d57ef45eb1974f7aaa5df77bd214211f
Yusuf Kursad Kaya <ykursadkaya@gmail.com>
2023-03-24 14:03:56 +0300

    Update confluent-cli from 3.5.2 to 3.6.0

--- Casks/confluent-cli.rb
+++ Casks/confluent-cli.rb
@@ -1,9 +1,9 @@
 cask "confluent-cli" do
   arch arm: "arm64", intel: "amd64"
 
-  version "3.5.2"
-  sha256 arm:   "ca1fc247779365b9e1c0950f3ad9fcb4f964bd4002435e1d56468c8ed6784a55",
-         intel: "2824d1d2952e280ddb260fa5ff248ac096a5fb5d7f693800c1cefd53750fbd69"
+  version "3.6.0"
+  sha256 arm:   "78a1a5a604f3f3903a6d7e477948e0d0c3ed527d2aa9631136f589d06b21b89a",
+         intel: "a3a0627a09763d7925c46e176502715bb5f431249f85aa006b16ce8321bb1409"
 
   url "https://s3-us-west-2.amazonaws.com/confluent.cloud/confluent-cli/archives/#{version}/confluent_#{version}_darwin_#{arch}.tar.gz",
       verified: "s3-us-west-2.amazonaws.com/confluent.cloud/confluent-cli/archives/"
