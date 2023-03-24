bbbd35595837a62f07f2fc3fe375e80739c17b2a
Timothy Clark <git@tdhc.uk>
2023-03-24 19:27:11 +0000

    brew style fixes

--- Casks/orbstack.rb
+++ Casks/orbstack.rb
@@ -3,14 +3,16 @@ cask "orbstack" do
 
   version "v0.5.0_924"
   sha256 arm:   "beb2bc4570b402056d9d6f4c73b78d06bfba4bc2e5e9f8aa68d16a9e9c06018c",
-  intel: "c581b59f0037cd4d4d14ef432f1ec6b4ca5683e5fe19aca665f7050c851ede2a"
+         intel: "c581b59f0037cd4d4d14ef432f1ec6b4ca5683e5fe19aca665f7050c851ede2a"
 
   url "https://cdn-updates.orbstack.dev/#{arch}/OrbStack_#{version}_#{arch}.dmg"
   name "orbstack"
-  desc "OrbStack is a fast, light, and simple way to run Docker containers and Linux machines on macOS. You can think of it as a supercharged WSL and Docker Desktop for macOS, all in one easy-to-use app."
-  homepage "https://orbstack.dev"
+  desc "Run Docker and Linux with this drop-in replacement for Docker Desktop"
+  homepage "https://orbstack.dev/"
 
   auto_updates true
 
   app "OrbStack.app"
+
+  zap trash: "~/.orbstack"
 end
