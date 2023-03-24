bb020c7f340ac8e1c74f1ab57eb5c722167fa2ef
miccal <miccal.matthews@gmail.com>
2023-03-24 18:08:21 +0800

    Various: Use short_version in the livecheck

--- Casks/disk-diet.rb
+++ Casks/disk-diet.rb
@@ -1,15 +1,15 @@
 cask "disk-diet" do
-  version "5.5.1,1595"
+  version "5.5.1"
   sha256 "e4ab5d2e9d5935fdc85af9421ce805fa336291d4c83b748b62928699d852a3a7"
 
-  url "https://www.tunabellysoftware.com/resources/Disk%20Diet%20#{version.csv.first}.dmg"
+  url "https://www.tunabellysoftware.com/resources/Disk%20Diet%20#{version}.dmg"
   name "Disk Diet"
   desc "Free up disk space"
   homepage "https://www.tunabellysoftware.com/disk_diet/"
 
   livecheck do
     url "https://www.tunabellysoftware.com/resources/sparkle/diskdiet.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   app "Disk Diet.app"
