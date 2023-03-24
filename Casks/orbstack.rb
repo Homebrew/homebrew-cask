055e208b03c8be9c61e55c1bfbdb10ce786d84bd
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-25 04:34:33 +0800

    Update orbstack.rb

--- Casks/orbstack.rb
+++ Casks/orbstack.rb
@@ -1,18 +1,22 @@
 cask "orbstack" do
-  arch arm:   "arm64",
-       intel: "amd64"
+  arch arm: "arm64", intel: "amd64"
 
-  version :latest
-  sha256 :no_check
+  version "0.5.0_924"
+  sha256 arm:   "beb2bc4570b402056d9d6f4c73b78d06bfba4bc2e5e9f8aa68d16a9e9c06018c",
+         intel: "c581b59f0037cd4d4d14ef432f1ec6b4ca5683e5fe19aca665f7050c851ede2a"
 
-  url "https://orbstack.dev/download/beta/latest/#{arch}"
-  name "orbstack"
-  desc "Run Docker and Linux with this drop-in replacement for Docker Desktop"
+  url "https://cdn-updates.orbstack.dev/amd64/OrbStack_v#{version}_#{arch}.dmg",
+  name "OrbStack"
+  desc "Replacement for Docker Desktop"
   homepage "https://orbstack.dev/"
 
   livecheck do
-    skip "No version information available"
+    url "https://orbstack.dev/download/beta/latest/#{arch}"
+    regex(/OrbStack[._-]v?(\d+(?:[._-]\d+)+)[._-]#{arch}\.dmg/i)
+    strategy :header_match
   end
+
+  depends_on macos: ">= :monterey"
 
   app "OrbStack.app"
 
