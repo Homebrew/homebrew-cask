af3409f9f8ae67db314356061f1396fa38e30953
Markus Reiter <me@reitermark.us>
2023-03-25 11:54:03 +0100

    Fix ableton casks. (#143739)

--- Casks/ableton-live-intro.rb
+++ Casks/ableton-live-intro.rb
@@ -1,17 +1,8 @@
 cask "ableton-live-intro" do
-  arch arm: "universal", intel: "intel"
-
   version "11.2.11"
+  sha256 "70fb6f33c7e03f7c6290c7f5d5a6bee4454df272527c5fdd5dfd045b7d76f1eb"
 
-  on_high_sierra :or_older do
-    sha256 "68ae4aa7c1a4286f77de340f5e2556442ea17a73976c3f90c9dce1c75dc8234a"
-  end
-  on_mojave :or_newer do
-    sha256 arm:   "70fb6f33c7e03f7c6290c7f5d5a6bee4454df272527c5fdd5dfd045b7d76f1eb",
-           intel: "ad24ed3cecd392464efd8b6bd3a0b65f8f7b624fdd07b0315e266050d97f77e2"
-  end
-
-  url "https://cdn-downloads.ableton.com/channels/#{version}/ableton_live_intro_#{version}_#{arch}.dmg"
+  url "https://cdn-downloads.ableton.com/channels/#{version}/ableton_live_intro_#{version}_universal.dmg"
   name "Ableton Live Intro"
   desc "Sound and music editor"
   homepage "https://www.ableton.com/en/live/"
