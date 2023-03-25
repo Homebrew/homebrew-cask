acbf7e73c2f330f00a1e77a2e5b9ba275829f8ef
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-26 06:49:48 +0800

    Update tg-pro from 2.78 to 2.79 (#143768)
    \n

--- Casks/tg-pro.rb
+++ Casks/tg-pro.rb
@@ -1,19 +1,19 @@
 cask "tg-pro" do
-  version "2.78,13920"
-  sha256 "c486caf2068e2756b2f753afef4bddea4bf40d7abe456851d27cb12abe6cc528"
+  version "2.79"
+  sha256 "485e4ed589f97fd5075219324fb16e9818c56f1102d01bbe84de22bdfa31a323"
 
-  url "https://www.tunabellysoftware.com/resources/TG%20Pro%20#{version.csv.first}.dmg"
+  url "https://www.tunabellysoftware.com/resources/TG%20Pro%20#{version}.dmg"
   name "TG Pro"
   desc "Temperature monitoring, fan control and diagnostics"
   homepage "https://www.tunabellysoftware.com/tgpro/"
 
   livecheck do
     url "https://www.tunabellysoftware.com/resources/sparkle/tgpro.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   auto_updates true
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :high_sierra"
 
   app "TG Pro.app"
 
