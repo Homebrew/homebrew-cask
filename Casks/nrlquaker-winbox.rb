6c7a52f68b487b32661980cf22c6485eef11d1ab
FAN VINGA <fanalcest@gmail.com>
2023-03-26 02:09:17 +0800

    Update nrlquaker-winbox to 3.37.1

--- Casks/nrlquaker-winbox.rb
+++ Casks/nrlquaker-winbox.rb
@@ -1,6 +1,6 @@
 cask "nrlquaker-winbox" do
-  version "3.37.0"
-  sha256 "02294303a181663a79070c60d2c56e7e5b9aa36fa68c7abddcd7bf05078a7ae8"
+  version "3.37.1"
+  sha256 "c54136e4287cb6a79ca04ef72b52035f077a3c68060885ae0283939defaf4630"
 
   url "https://github.com/nrlquaker/winbox-mac/releases/download/v#{version}/Winbox-mac-#{version}.zip"
   name "Winbox-mac"
