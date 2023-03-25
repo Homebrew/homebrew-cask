c3bdeef2c7cb025df3755efb35c1a400b40f2db9
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-25 18:28:22 -0400

    Update grids from 8.5 to 8.5.1 (#143755)

--- Casks/grids.rb
+++ Casks/grids.rb
@@ -1,6 +1,6 @@
 cask "grids" do
-  version "8.5"
-  sha256 "87145186dfe28dd6d1d9bb533badcac9d692a40f55f079ae5a5bb623a2aea450"
+  version "8.5.1"
+  sha256 "708c69edb3db0e8b495d13e8cb7dae124a767cea6f614d55e29379070742a869"
 
   url "https://gridsapp.net/bin/Grids_#{version}.zip"
   name "Grids"
