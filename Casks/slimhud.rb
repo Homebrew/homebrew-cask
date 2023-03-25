5bd25aab47557bbfdcdef70ab624558302e6560f
Alex Perathoner <alex.perathoner@gmail.com>
2023-03-25 19:32:40 +0100

    Update slimhud from 1.5.0 to 1.5.1

--- Casks/slimhud.rb
+++ Casks/slimhud.rb
@@ -1,6 +1,6 @@
 cask "slimhud" do
-  version "1.5.0"
-  sha256 "901fd7ccd78dbe3fc68d41bb17c241a126552f6a0e2b0105ccdd8a984c73d007"
+  version "1.5.1"
+  sha256 "aecd074d2b2a98645a3136cb870e3e9f57fca903e36fc9e9b406049258ffe362"
 
   url "https://github.com/AlexPerathoner/SlimHUD/releases/download/v#{version}/SlimHUD.zip"
   name "SlimHUD"
