98083f7338fc6785df576784c7e0f6ee044af46d
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-25 13:32:02 +0800

    Update pika.rb

--- Casks/pika.rb
+++ Casks/pika.rb
@@ -3,7 +3,7 @@ cask "pika" do
   sha256 "79780c4da605799e56a43c3e3ca65481b0e168d2f94d35c82d7a7359adbb3a02"
 
   url "https://github.com/superhighfives/pika/releases/download/#{version}/Pika-#{version}.dmg",
-      verified: "github.com/superhighfives/pika"
+      verified: "github.com/superhighfives/pika/"
   name "Pika"
   desc "Colour picker for colours onscreen"
   homepage "https://superhighfives.com/pika"
