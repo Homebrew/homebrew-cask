c987db8655d70ebbc5507ccb4c7400a40aaf4a2a
Issy Long <me@issyl0.co.uk>
2023-03-25 01:16:59 +0000

    ableton-live-intro: Separate SHAs for different architecture downloads

--- Casks/ableton-live-intro.rb
+++ Casks/ableton-live-intro.rb
@@ -7,7 +7,8 @@ cask "ableton-live-intro" do
     sha256 "68ae4aa7c1a4286f77de340f5e2556442ea17a73976c3f90c9dce1c75dc8234a"
   end
   on_mojave :or_newer do
-    sha256 "70fb6f33c7e03f7c6290c7f5d5a6bee4454df272527c5fdd5dfd045b7d76f1eb"
+    sha256 arm:   "70fb6f33c7e03f7c6290c7f5d5a6bee4454df272527c5fdd5dfd045b7d76f1eb",
+           intel: "ad24ed3cecd392464efd8b6bd3a0b65f8f7b624fdd07b0315e266050d97f77e2"
   end
 
   url "https://cdn-downloads.ableton.com/channels/#{version}/ableton_live_intro_#{version}_#{arch}.dmg"
