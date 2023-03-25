4e4701f54abde355f7e430485367b5ba98840f20
Markus Reiter <me@reitermark.us>
2023-03-25 07:40:52 +0100

    Update ableton-live-standard.rb

--- Casks/ableton-live-standard.rb
+++ Casks/ableton-live-standard.rb
@@ -2,7 +2,7 @@ cask "ableton-live-standard" do
   version "11.2.11"
 
   on_high_sierra :or_older do
-    sha256 "51bffe5e3ab58c943cabbd605964e74c26535e9b993a3cb76b7a7d230150ec75"
+    sha256 "d72ce86fc89a4857071335d53fa22a355df33690acdac8d0aebed35d6c70b5b0"
 
     url "https://cdn-downloads.ableton.com/channels/#{version}/ableton_live_standard_#{version}_intel.dmg"
   end
