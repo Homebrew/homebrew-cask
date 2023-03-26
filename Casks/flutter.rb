577526c4f6e3b4ba66b68e73ea24a114c2064179
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-03-27 00:01:33 +0800

    flutter 3.7.8

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -1,9 +1,9 @@
 cask "flutter" do
   arch arm: "_arm64"
 
-  version "3.7.7"
-  sha256 arm:   "0511b9f164a62f467b063e6c83d9c683bb3fb056ee556b2f45e987a2c2f4a260",
-         intel: "78957b52f023a0d811add27eddc59b1a59d27d2ada5df123f39d0315708fb2d5"
+  version "3.7.8"
+  sha256 arm:   "36aefeab0c5460a538c42149542514d89d3799310ad22e903741aca2e2ca3006",
+         intel: "487c3dd6cd03aac0edf319c010cbb787b91903eff2fa5098cb948d122279b80a"
 
   url "https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos#{arch}_#{version}-stable.zip",
       verified: "storage.googleapis.com/flutter_infra_release/releases/stable/macos/"
