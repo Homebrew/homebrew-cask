98f8e4b24a909c1198a1a655a7b4ea384bad332e
Thomas Walter <tw@b-a-l-u.de>
2023-03-26 23:44:44 +0200

    Update fsnotes from 6.2.0 to 6.2.1

--- Casks/fsnotes.rb
+++ Casks/fsnotes.rb
@@ -1,6 +1,6 @@
 cask "fsnotes" do
-  version "6.2.0"
-  sha256 "cf7875de438a091d61d257eab264fb11fa1fb506b66159a6e166214a3822a9ca"
+  version "6.2.1"
+  sha256 "b32dc357adda0138f697236a21ce91bcd967d395579713a27b344f2851534270"
 
   url "https://github.com/glushchenko/fsnotes/releases/download/#{version}/FSNotes_#{version}.zip",
       verified: "github.com/glushchenko/fsnotes/"
