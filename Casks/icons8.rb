c77a1df0302e767e243d975f5693cb0de61b8ef6
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-26 19:47:02 +0800

    Update icons8.rb

--- Casks/icons8.rb
+++ Casks/icons8.rb
@@ -16,10 +16,10 @@ cask "icons8" do
   app "Icons8.app"
 
   zap trash: [
+    "~/Library/Application Scripts/com.visualpharm.StandaloneLoginHelper",
+    "~/Library/Application Support/com.visualpharm.Icons8",
     "~/Library/Caches/com.visualpharm.Icons8",
     "~/Library/Containers/com.visualpharm.StandaloneLoginHelper",
     "~/Library/Preferences/com.visualpharm.Icons8.plist",
-    "~/Library/Application Scripts/com.visualpharm.StandaloneLoginHelper",
-    "~/Library/Application Support/com.visualpharm.Icons8",
   ]
 end
