f3bfc7f5d4d4c18ee37f97209023f57263c0cb70
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-26 07:30:35 -0400

    Update hookmark from 5.0 to 5.0.2

--- Casks/hookmark.rb
+++ Casks/hookmark.rb
@@ -1,8 +1,8 @@
 cask "hookmark" do
-  version "5.0,2023.01"
-  sha256 "ccd8e7db0ab1647125717595e1ac3a177c0149cd0aac9217dba7afa5507f6732"
+  version "5.0.2,2023.03.b5331"
+  sha256 "363a9a3eed1bc0983ce1d74ce43041a4ded0c42a05e0bde84db0e230d9205b66"
 
-  url "https://hookproductivity.com/wp-content/uploads/#{version.csv.second.major}/#{version.csv.second.minor}/Hookmark-app-#{version.csv.first}.dmg_.zip",
+  url "https://hookproductivity.com/wp-content/uploads/#{version.csv.second.major}/#{version.csv.second.minor}/Hookmark-app-#{version.csv.first}-#{version.csv.second.patch}.dmg_.zip",
       user_agent: :fake
   name "Hook"
   desc "Link and retrieve key information"
@@ -11,10 +11,10 @@ cask "hookmark" do
   livecheck do
     url "https://hookproductivity.com/download"
     strategy :page_match do |page|
-      match = page.match(%r{href=.*?/(\d+)/(\d+)/Hookmark[._-]app[._-](\d+(?:\.\d+)*(?:-\d+)*)\.dmg}i)
+      match = page.match(%r{href=.*?/(\d+)/(\d+)/Hookmark[._-]app[._-](\d+(?:\.\d+)*)-(\S?\d+(?:\.\d+)*)\.dmg}i)
       next if match.blank?
 
-      "#{match[3]},#{match[1]}.#{match[2]}"
+      "#{match[3]},#{match[1]}.#{match[2]}.#{match[4]}"
     end
   end
 
