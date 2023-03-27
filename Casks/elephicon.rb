c736bee0e0cf2ae240a0c3cfe326b96545905e75
Amory Meltzer <Amorymeltzer@gmail.com>
2023-03-27 10:24:19 -0400

    Update elephicon from 2.3.5 to 2.3.6

--- Casks/elephicon.rb
+++ Casks/elephicon.rb
@@ -1,9 +1,9 @@
 cask "elephicon" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.3.5"
-  sha256 arm:   "fef3229a400b81b993834baf22aace9286dd012737c356e36228035039004f08",
-         intel: "41aec280866c38dadc26398ea965fbeeb03d3c33639221caf1a2362105e036d4"
+  version "2.3.6"
+  sha256 arm:   "c96b0e194cef3930e295f0bf03f0e89a76a206ded5e4c4a01494afc6281fbe5b",
+         intel: "ddb62454bd4d0cc2a02e283f3ee4518341d34b852f66f71413dfc6b3642b841a"
 
   url "https://github.com/sprout2000/elephicon/releases/download/v#{version}/Elephicon-#{version}-darwin-#{arch}.dmg"
   name "Elephicon"
