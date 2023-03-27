fcf3842cadd8e7488b4a3afc01e62010fc96478f
Mustafa Berkay Mutlu <berkay.mutlu@trendyol.com>
2023-03-27 11:14:21 +0200

    Separate stanza groups

--- Casks/gephi.rb
+++ Casks/gephi.rb
@@ -1,5 +1,6 @@
 cask "gephi" do
   arch arm: "aarch64", intel: "x64"
+
   version "0.10.1"
   sha256 arm:   "cbb852d2f33cddf793290a64aee564cd46ad80ef6c51766ad05dfeb801ac7227",
          intel: "abc7ca1ba7f2a91e51af716ad0a1a785a798f7bb7100841dfdb775895919f508"
