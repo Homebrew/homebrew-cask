7cddb707cb7cc79fe4f5f39ae2b369f7235e7626
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-27 16:53:16 -0400

    Update remote-buddy from 2.4 to 2.4.1 (#143861)
    * Update remote-buddy from 2.4 to 2.4.1
    
    * add desc and zap stanza

--- Casks/remote-buddy.rb
+++ Casks/remote-buddy.rb
@@ -1,9 +1,10 @@
 cask "remote-buddy" do
-  version "2.4"
+  version "2.4.1"
   sha256 :no_check
 
   url "https://www.iospirit.com/products/remotebuddy/download/RemoteBuddy.zip"
   name "Remote Buddy"
+  desc "Control apps and web videos from your phone"
   homepage "https://www.iospirit.com/products/remotebuddy/"
 
   livecheck do
@@ -12,4 +13,12 @@ cask "remote-buddy" do
   end
 
   app "Remote Buddy.app"
+
+  zap trash: [
+    "~/Library/Application Support/Remote Buddy",
+    "~/Library/Caches/com.apple.helpd/Generated/com.iospirit.RemoteBuddy.help*",
+    "~/Library/Preferences/ByHost/com.iospirit.RemoteBuddy.*",
+    "~/Library/Preferences/com.iospirit.RemoteBuddy.plist",
+    "~/Library/Saved Application State/com.iospirit.RemoteBuddy.savedState",
+  ]
 end
