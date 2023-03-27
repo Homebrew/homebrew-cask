788012641ac40df3cf3f359f67672e1558736826
Amory Meltzer <Amorymeltzer@gmail.com>
2023-03-27 10:41:37 -0400

    Update sol from 2.0.7 to 2.0.9

--- Casks/sol.rb
+++ Casks/sol.rb
@@ -1,6 +1,6 @@
 cask "sol" do
-  version "2.0.7"
-  sha256 "6cd0d9c8b7de53ce1c81c0019023acce005f6e08938cbd7c83effdfb28542297"
+  version "2.0.9"
+  sha256 "8c2201e3377f2a8192ef6ecf15447a25755d0ab963c5c8220f26b1a4cdf005bf"
 
   url "https://raw.githubusercontent.com/ospfranco/sol/main/releases/#{version}.zip",
       verified: "raw.githubusercontent.com/ospfranco/sol/"
