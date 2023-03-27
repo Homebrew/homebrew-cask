e92d732c8e616965779f6ee39ff2c3949fcd5ecd
Amory Meltzer <Amorymeltzer@gmail.com>
2023-03-27 10:44:48 -0400

    Update uvtools from 3.12.1 to 3.12.2

--- Casks/uvtools.rb
+++ Casks/uvtools.rb
@@ -1,9 +1,9 @@
 cask "uvtools" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.12.1"
-  sha256 arm:   "ccf85dcbfd6123640470009e496d16220e46413147b808b12185c26bf53c1950",
-         intel: "148aa672493c703ca0ca0f0906584527f205f8dab097a840df31559b80d477c9"
+  version "3.12.2"
+  sha256 arm:   "e2751a92e9dfd1a5d91b738a6011baac440336c3f1af6fb8fa5b06c48e6587b4",
+         intel: "e42a44e6410d6088a6166c6d5dba672bb03d2ee18d2254c7e589f1d5eae4b234"
 
   url "https://github.com/sn4k3/UVtools/releases/download/v#{version}/UVtools_osx-#{arch}_v#{version}.zip"
   name "UVtools"
