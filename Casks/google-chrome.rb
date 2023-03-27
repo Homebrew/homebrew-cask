f757f60ffec39130a76d2042a360484d934e5f96
Hanlin He <hanlhe@cisco.com>
2023-03-27 12:19:04 -0700

    Update google-chrome from 111.0.5563.110 to 111.0.5563.146

--- Casks/google-chrome.rb
+++ Casks/google-chrome.rb
@@ -1,5 +1,5 @@
 cask "google-chrome" do
-  version "111.0.5563.110"
+  version "111.0.5563.146"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
