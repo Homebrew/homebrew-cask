2d2548574e0b0e928ba6ccd39338e1a56300b147
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-03-29 05:51:19 +0800

    Update chirp from 20230322 to 20230328 (#143944)

--- Casks/chirp.rb
+++ Casks/chirp.rb
@@ -1,6 +1,6 @@
 cask "chirp" do
-  version "20230322"
-  sha256 "398adcbba07c72ff6a663adcf805e7a54da79ccbf633aacc0339c3271e74633e"
+  version "20230328"
+  sha256 "be994d3aab4f5ffa3e753b4af9b2200fcd58104192eccb025e42fb980bd109c5"
 
   url "https://trac.chirp.danplanet.com/chirp_next/next-#{version}/chirp-next-#{version}.app.zip"
   name "CHIRP"
