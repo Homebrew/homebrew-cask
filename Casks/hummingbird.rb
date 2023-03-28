114e2fe64020837243b00a593e5b1574776710a8
Markus Reiter <me@reitermark.us>
2023-03-29 00:03:56 +0200

    Update hummingbird.rb

--- Casks/hummingbird.rb
+++ Casks/hummingbird.rb
@@ -7,7 +7,7 @@ cask "hummingbird" do
 
   url "https://eddie.website/repository/hummingbird/#{version}/hummingbird-macos-#{arch}-notarized-#{version}.zip",
       verified: "eddie.website/repository/hummingbird/"
-  name "hummingbird"
+  name "Hummingbird"
   desc "OpenVPN 3 client"
   homepage "https://airvpn.org/hummingbird"
 
