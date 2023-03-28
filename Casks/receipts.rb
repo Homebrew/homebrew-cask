fba389c000bcb399947cfe493c3c472379abcc24
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-28 17:40:31 -0400

    Update receipts from 1.9.18-584 to 1.9.19-59 (#143914)

--- Casks/receipts.rb
+++ Casks/receipts.rb
@@ -1,6 +1,6 @@
 cask "receipts" do
-  version "1.9.18-584"
-  sha256 "9c04f704fc40743491c09d3b4f3e94abf9dcf9235c71b918c31b35024ac04f97"
+  version "1.9.19-594"
+  sha256 "0b6acaf38e455141e482f26ee19e76b0c73570ccd74a5954adada176e46f74f7"
 
   url "https://www.receipts-app.com/update/download/Receipts-#{version}.zip"
   name "Receipts"
@@ -15,4 +15,17 @@ cask "receipts" do
   depends_on macos: ">= :el_capitan"
 
   app "Receipts.app"
+
+  zap trash: [
+    "~/Documents/Receipts.receipts",
+    "~/Library/Application Scripts/com.apperdeck.mac.Receipts.ReceiptsShareExtension",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.apperdeck.mac.receipts.sfl2",
+    "~/Library/Caches/com.apperdeck.mac.Receipts",
+    "~/Library/Containers/com.apperdeck.mac.Receipts.ReceiptsShareExtension",
+    "~/Library/HTTPStorages/com.apperdeck.mac.Receipts",
+    "~/Library/Logs/Receipts",
+    "~/Library/Preferences/com.apperdeck.mac.Receipts.plist",
+    "~/Library/Saved Application State/com.apperdeck.mac.Receipts.savedState",
+    "~/Library/WebKit/com.apperdeck.mac.Receipts",
+  ]
 end
