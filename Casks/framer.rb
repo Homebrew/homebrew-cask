cdb4aac40b307ed48550502d6d8346f578a007ee
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-03-29 06:49:33 +0800

    Update framer from 2023.11.2 to 2023.13.1

--- Casks/framer.rb
+++ Casks/framer.rb
@@ -1,7 +1,7 @@
 cask "framer" do
   arch arm: "arm64", intel: "x64"
 
-  version "2023.11.2"
+  version "2023.13.1"
   sha256 :no_check
 
   url "https://updates.framer.com/electron/darwin/#{arch}/Framer.zip"
