15743533378c953dbcd59c7e61806a3a1e3b98ea
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-28 17:35:54 -0400

    Update openaudible from 3.7.2 to 3.7.3 (#143910)

--- Casks/openaudible.rb
+++ Casks/openaudible.rb
@@ -1,6 +1,6 @@
 cask "openaudible" do
-  version "3.7.2"
-  sha256 "145ce7a5d32310254724f8edc3885149dd00447fb73a44e6db6d1cad85173c13"
+  version "3.7.3"
+  sha256 "6acd1987a0219efa0a519785d2b28d339f1af3b527bba5c3d5d075e9192a9d03"
 
   url "https://github.com/openaudible/openaudible/releases/download/v#{version}/OpenAudible_#{version}.dmg",
       verified: "github.com/openaudible/openaudible/"
