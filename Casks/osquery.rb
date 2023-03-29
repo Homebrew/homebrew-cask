a947e6a620ad1775a98c09d4c66113c6a0c95b0b
Patrick Linnane <patrick@linnane.io>
2023-03-29 15:45:21 -0700

    Update osquery from 5.8.1 to 5.8.2

--- Casks/osquery.rb
+++ Casks/osquery.rb
@@ -1,6 +1,6 @@
 cask "osquery" do
-  version "5.8.1"
-  sha256 "a09c0afc29c8150cbbd8a3603398e756b86b5eb68978ee6a6ba0b425b611d22c"
+  version "5.8.2"
+  sha256 "1fea8ac9b603851d2e76c5fc73138a468a3075a3002c8cb1fd7fff53b889c4dd"
 
   url "https://pkg.osquery.io/darwin/osquery-#{version}.pkg"
   name "osquery"
