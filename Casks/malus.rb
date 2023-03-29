ab8d3e03d434722f554700774055ba9a33b710fa
Steve Zheng <36894554+stevezhengshiqi@users.noreply.github.com>
2023-03-29 16:35:12 -0400

    Update malus.rb

--- Casks/malus.rb
+++ Casks/malus.rb
@@ -2,7 +2,7 @@ cask "malus" do
   version "5.0.0"
   sha256 "0667f2c2d32a96c751b4b9859b9286329d9f9067db29a4a201b829b45af9edcd"
 
-  url "https://getmalus.com/download-start?mac=1&dlu=/r/mac"
+  url "https://download.getmalus.com/uploads/malus#{version.dots_to_underscores}.dmg"
   name "Malus"
   desc "Proxy to help accessing various online media resources/services"
   homepage "https://getmalus.com/"
