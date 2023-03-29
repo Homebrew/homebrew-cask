4ed9f201fab2f49f788df429caeaf8c882f10697
Patrick Linnane <patrick@linnane.io>
2023-03-29 16:00:04 -0700

    Update remotion from 3.072.1 to 3.087.1

--- Casks/remotion.rb
+++ Casks/remotion.rb
@@ -1,6 +1,6 @@
 cask "remotion" do
-  version "3.072.1"
-  sha256 "d2251873579ab949b65a2148add5b2a61680fa886d1d70eef60986b5bc9ad0b2"
+  version "3.087.1"
+  sha256 "55000e19e800d8429c0e57e47947d9204599ecc00ba44e7061e34d6081f199d5"
 
   url "https://updates.remotion.com/installers/Remotion%20#{version}.dmg"
   name "remotion"
