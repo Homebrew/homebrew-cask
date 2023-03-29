0700907fc324513a94c28e763ec4d6bd27044699
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-29 15:18:51 +0800

    Update i1profiler from 3.6.2.16933 to 3.6.3.16999 (#143978)
    \n

--- Casks/i1profiler.rb
+++ Casks/i1profiler.rb
@@ -1,6 +1,6 @@
 cask "i1profiler" do
-  version "3.6.2.16933"
-  sha256 "22db78faff816ebe81b7ee9b5986d63250fbf9311a2e69117ee2ba759a9fa7cd"
+  version "3.6.3.16999"
+  sha256 "38af8e61d9c2b6101d78c1c1c9d5c99d6c5ca00cc781a51ccda6b042b6e9ca3c"
 
   url "https://downloads.xrite.com/downloads/software/i1Profiler/#{version.major_minor_patch}/Mac/i1Profiler.zip"
   name "i1Profiler"
@@ -17,9 +17,9 @@ cask "i1profiler" do
   pkg "i1Profiler.pkg"
 
   uninstall pkgutil:   [
+              "com.xrite.hasp.installer.*",
               "com.xrite.i1profiler.*",
               "com.xrite.xritedeviceservices.*",
-              "com.xrite.hasp.installer.*",
             ],
             launchctl: [
               "com.aladdin.aksusbd",
