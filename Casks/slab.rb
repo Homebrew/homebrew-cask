55b1419b790a6c4625bfd0891853d31ca0db0b11
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-03-30 07:21:14 +0800

    Update slab from 1.4.1 to 1.5.0 (#144121)

--- Casks/slab.rb
+++ Casks/slab.rb
@@ -1,9 +1,9 @@
 cask "slab" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.4.1"
-  sha256 arm:   "f466524ec22f7a74ab9cb5e79f3be261972d1cda3bdb301f650ac0ead230068a",
-         intel: "a40646a72adf8d2ec8c6134d395ea69467e4b362eb63603d78d1c0631d887071"
+  version "1.5.0"
+  sha256 arm:   "aa11f368ea25463ce153ce8a22077b25852edcaf592473a71817e52a45ec7bf6",
+         intel: "10ab8b790b915bd28bd4b04c32bac7a973f82b0b16c52c3682e544a93cba8f63"
 
   url "https://github.com/slab/desktop-releases/releases/download/v#{version}/Slab-#{version}-darwin-#{arch}.dmg",
       verified: "github.com/slab/desktop-releases/"
