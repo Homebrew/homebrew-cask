f39f69e63064eb42f00ed7bafa8b489a8a666eae
Patrick Linnane <patrick@linnane.io>
2023-03-29 15:28:54 -0700

    Update jprofiler from 13.0.5 to 13.0.6

--- Casks/jprofiler.rb
+++ Casks/jprofiler.rb
@@ -1,6 +1,6 @@
 cask "jprofiler" do
-  version "13.0.5"
-  sha256 "e19f40a118f3848d1e86f3f9184a2952915c119017b9730b2c6aed5d4f6c4c9e"
+  version "13.0.6"
+  sha256 "388e8d48fa98c2ce51bf6e54e6320fce4c89b41f0b174e2a1c1dcd3d1f570568"
 
   url "https://download-gcdn.ej-technologies.com/jprofiler/jprofiler_macos_#{version.dots_to_underscores}.dmg"
   name "JProfiler"
