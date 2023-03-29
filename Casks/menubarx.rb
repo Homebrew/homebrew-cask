8124539d026d395b42f9aac5c45430ef9a92fc50
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-03-30 00:10:13 +0800

    Update menubarx from 1.6.0 to 1.6.1

--- Casks/menubarx.rb
+++ Casks/menubarx.rb
@@ -1,6 +1,6 @@
 cask "menubarx" do
-  version "1.6.0"
-  sha256 "c4065617b5fe0f50b538bc24cb5900aa69d4eb47888882c4a8051fb7fd43b84f"
+  version "1.6.1"
+  sha256 "c7354cf02fd1dcfd33aed915abb035f1f2e481c22f60c6c162d62f18d93f9c0e"
 
   url "https://menubarx-1251679148.file.myqcloud.com/download/MenubarX-#{version}.dmg",
       verified: "menubarx-1251679148.file.myqcloud.com/download/"
