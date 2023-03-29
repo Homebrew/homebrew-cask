5ce307a0611bc0155c30a5a0f8ffcc2a5e9891a1
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-29 17:55:33 -0400

    Update opera-gx from 96.0.4693.117 to 96.0.4693.127 (#144014)

--- Casks/opera-gx.rb
+++ Casks/opera-gx.rb
@@ -1,6 +1,6 @@
 cask "opera-gx" do
-  version "96.0.4693.117"
-  sha256 "4c9bec07507619ebeb027c90b5f6f0cb06a787298c1538303588f4cff8244b4f"
+  version "96.0.4693.127"
+  sha256 "ebb0d6ddb5905529b10ab639f84ed3dc8bdf188e162774e3b5b83b6bcfb9f90d"
 
   url "https://get.geo.opera.com/pub/opera_gx/#{version}/mac/Opera_GX_#{version}_Setup.dmg"
   name "Opera GX"
