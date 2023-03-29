dbb2b3af065ac179c788eea22d4bbf30f71641e2
Nanda H Krishna <me@nandahkrishna.com>
2023-03-28 20:45:21 -0400

    Update ngrok from 3.2.1 to 3.2.2 (#143967)
    \n

--- Casks/ngrok.rb
+++ Casks/ngrok.rb
@@ -6,8 +6,8 @@ cask "ngrok" do
     sha256 "896f37cff976229b39392916f23403349c350f871cb63677cb6b4fbafb97faee"
   end
   on_intel do
-    version "3.2.1,9jMiBksxbSZ,a"
-    sha256 "0687f6eb27be27defe78156e37dc22aa253fe19997a2dd024376a3bed4bbf281"
+    version "3.2.2,hoFbK2Q2jz3,a"
+    sha256 "707bea6a294413e6d39edcb3f131846bccdb1a00d4fb2ed2a8f2cb5aa94da231"
   end
 
   url "https://bin.equinox.io/#{version.csv.third}/#{version.csv.second}/ngrok-v#{version.major}-#{version.csv.first}-stable-darwin-#{arch}.zip",
