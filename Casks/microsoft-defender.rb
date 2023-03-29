2743be74131af1954655bd5438187d9bee8b65d0
Patrick Linnane <patrick@linnane.io>
2023-03-29 15:34:31 -0700

    Update microsoft-defender from 101.97.94 to 101.98.30

--- Casks/microsoft-defender.rb
+++ Casks/microsoft-defender.rb
@@ -1,5 +1,5 @@
 cask "microsoft-defender" do
-  version "101.97.94"
+  version "101.98.30"
   sha256 :no_check
 
   url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft%20Defender.pkg"
