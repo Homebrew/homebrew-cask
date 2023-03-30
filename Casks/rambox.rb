7a2698aeb7971fff65b8cb35503930b7fdd9b3b4
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-03-30 22:45:08 +0800

    Update rambox from 2.1.1 to 2.1.2

--- Casks/rambox.rb
+++ Casks/rambox.rb
@@ -1,6 +1,6 @@
 cask "rambox" do
-  version "2.1.1"
-  sha256 "77af67acac0209bc8c78c203908d97a37224ec363dde1fc2276a97861f28a3d8"
+  version "2.1.2"
+  sha256 "be85da1256668b915932b0f0e9430599aef4d388d6612f7eb998be8fdcd1e2bb"
 
   url "https://github.com/ramboxapp/download/releases/download/v#{version}/Rambox-#{version}-mac.zip",
       verified: "github.com/ramboxapp/download/"
