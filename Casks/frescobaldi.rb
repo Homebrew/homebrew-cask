41e7db1ce5ec16ad0344619dd4bf0f6914a9361a
Patrick Linnane <patrick@linnane.io>
2023-03-30 00:42:58 -0700

    Update frescobaldi from 3.1.3 to 3.3.0 (#144135)
    \n

--- Casks/frescobaldi.rb
+++ Casks/frescobaldi.rb
@@ -1,6 +1,6 @@
 cask "frescobaldi" do
-  version "3.1.3"
-  sha256 "a7c53b67015c8515ae31102a53e16a9eca6b0e5c98ced6e859bb834e3e12f29c"
+  version "3.3.0"
+  sha256 "82f4377c3c9e787a079cfe1cf269c7c3ba41bd933fba4fc4a60347aeb1ed32f0"
 
   url "https://github.com/frescobaldi/frescobaldi/releases/download/v#{version}/Frescobaldi-#{version}-x86_64.dmg",
       verified: "github.com/frescobaldi/frescobaldi/"
