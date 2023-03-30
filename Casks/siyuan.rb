92c9f9a283b48a3fd165020ace788b525967307f
Patrick Linnane <patrick@linnane.io>
2023-03-30 00:40:51 -0700

    Update siyuan from 2.8.0 to 2.8.1 (#144146)
    \n

--- Casks/siyuan.rb
+++ Casks/siyuan.rb
@@ -1,9 +1,9 @@
 cask "siyuan" do
   arch arm: "-arm64"
 
-  version "2.8.0"
-  sha256 arm:   "2d8e59f186df86ddc881d83377da62bf296a7110de60f297edf13e8a62896386",
-         intel: "962e7f9c5d45259bf570035b33d974429b2d238b7ef4dd179d350be1430dd9e8"
+  version "2.8.1"
+  sha256 arm:   "ad36b2d9f4f755a4d7b4dbbe1d0feafd4a2942cea06b42388378d6b97305ee0d",
+         intel: "3c485f329c39c32eab842197176a4c9b2ac58df21e25b877cc7e417bfd83ab2b"
 
   url "https://github.com/siyuan-note/siyuan/releases/download/v#{version}/siyuan-#{version}-mac#{arch}.dmg"
   name "SiYuan"
