66ccbc95dcc02655338cee68e300e379b39b0a19
Jihai Xu <jxu@rippling.com>
2023-03-30 02:44:22 -0700

    Add SHA for both Intel and AppleSilicon

--- Casks/openai-translator.rb
+++ Casks/openai-translator.rb
@@ -1,22 +1,18 @@
 cask "openai-translator" do
-  version "0.0.36"
-  sha256 :no_check # Since we have different binaries for different CPU types
+  arch intel: "x64", arm: "aarch64"
 
-  url do
-    on_intel do
-      "https://github.com/yetone/openai-translator/releases/download/v#{version}/OpenAI.Translator_#{version}_x64.dmg"
-    end
-    on_arm do
-      "https://github.com/yetone/openai-translator/releases/download/v#{version}/OpenAI.Translator_#{version}_aarch64.dmg"
-    end
-  end
+  version "0.0.36"
+  sha256 intel: "55cff09700d3dd4f634142e1fd360b8c2b5289e79f8a1557d06a278d6ad1c6c8",
+         arm:   "0edfecd0984f36d5828f45c60e985b570862f2d9fefa1851f92799ec6cec630a"
+
+  url "https://github.com/yetone/openai-translator/releases/download/v#{version}/OpenAI.Translator_#{version}_#{arch}.dmg"
   name "OpenAI Translator"
   desc "OpenAI based translation tool"
   homepage "https://github.com/yetone/openai-translator"
 
   app "OpenAI Translator.app"
 
-zap trash: [
-  "~/Library/Application Support/xyz.yetone.apps.openai-translator",
-]
+  zap trash: [
+    "~/Library/Application Support/xyz.yetone.apps.openai-translator",
+  ]
 end
