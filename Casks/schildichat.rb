f360d514b86fc592450093ea014737c74b21e4cf
Patrick Linnane <patrick@linnane.io>
2023-03-30 03:44:42 -0700

    Update schildichat from 1.11.22-sc.1 to 1.11.26-sc.1 (#144145)

--- Casks/schildichat.rb
+++ Casks/schildichat.rb
@@ -1,6 +1,6 @@
 cask "schildichat" do
-  version "1.11.22-sc.1"
-  sha256 "39b55b81099a58dc3fc04e6a3da2be2e559ded2b5fff880b3351cc48473ed969"
+  version "1.11.26-sc.1"
+  sha256 "e5556b18d0aeec6594ec4c442836966249fcb77ad0ddfc069b605424ae81646e"
 
   url "https://github.com/SchildiChat/schildichat-desktop/releases/download/v#{version}/SchildiChat-#{version}-universal_by_nyantec.dmg",
       verified: "github.com/SchildiChat/schildichat-desktop/"
