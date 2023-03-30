8349b5ef1554d1640082067f36fbcbe93cf87286
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-30 06:43:21 -0400

    simplify version and change livecheck

--- Casks/spotify.rb
+++ Casks/spotify.rb
@@ -1,14 +1,8 @@
 cask "spotify" do
   arch arm: "ARM64"
 
+  version "1.2.8.923"
   sha256 :no_check
-
-  on_arm do
-    version "1.2.8.923,4f94bf0d,1547"
-  end
-  on_intel do
-    version "1.2.7.1277,2b3ce637,220"
-  end
 
   url "https://download.scdn.co/Spotify#{arch}.dmg",
       verified: "download.scdn.co/"
@@ -22,7 +16,7 @@ cask "spotify" do
       match = items["com.spotify.client"].version.match(/^(\d+(?:\.\d+)+)[._-]g(\h+)[._-](\d+)$/i)
       next if match.blank?
 
-      "#{match[1]},#{match[2]},#{match[3]}"
+      (match[1]).to_s
     end
   end
 
