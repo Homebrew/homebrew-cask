7c515e89a258f285875ea43854df716595527f69
Jimmy Liang <57280094+JimmyLiang-lzm@users.noreply.github.com>
2023-03-31 00:13:54 +0800

    Update bili-downloader from 1.7.20230325 to 1.7.2 (#144211)
    * Update bili-downloader.rb
    
    * bili-downloader: add livecheck
    
    ---------
    
    Co-authored-by: Patrick Linnane <patrick@linnane.io>

--- Casks/bili-downloader.rb
+++ Casks/bili-downloader.rb
@@ -1,11 +1,16 @@
 cask "bili-downloader" do
-  version "1.7.20230325"
-  sha256 "f081685bef1b1877202a6e182cb78eb8c0049bede1996dc169efc9eae067f8f6"
+  version "1.7.2"
+  sha256 "aacc58eb79d64437b15882502190be17e68656999af6efcc81fa831973115bfa"
 
   url "https://github.com/JimmyLiang-lzm/biliDownloader_GUI/releases/download/V#{version}/BiliDownloader_for_MacOS_X.dmg"
   name "BiliDownloader"
   desc "BiliBili media downloader"
   homepage "https://github.com/JimmyLiang-lzm/biliDownloader_GUI"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "biliDownloader_GUI.app"
 
