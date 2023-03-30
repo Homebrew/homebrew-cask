83ebae11367dcc5bc12e414c736fae78f76e7227
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-31 04:39:29 +0800

    postico 1.5.22,9592 (#144223)

--- Casks/postico.rb
+++ Casks/postico.rb
@@ -1,16 +1,15 @@
 cask "postico" do
-  version "1.5.21"
-  sha256 "dfb11623e6539c2a5127a0dc7639baf06d62c774b195117a49b3e0eaf6f70ed6"
+  version "1.5.22,9592"
+  sha256 "8adcbe10a1a077739e56d5c2795b763d9e1c546b773c65fcfd9eb03e0edad8d7"
 
-  url "https://eggerapps-downloads.s3.amazonaws.com/postico-#{version}.zip",
-      verified: "eggerapps-downloads.s3.amazonaws.com/"
+  url "https://downloads.eggerapps.at/postico/postico-#{version.csv.second}.zip"
   name "Postico"
   desc "GUI client for PostgreSQL databases"
   homepage "https://eggerapps.at/postico/"
 
   livecheck do
     url "https://eggerapps.at/postico/download/"
-    strategy :header_match
+    strategy :extract_plist
   end
 
   app "Postico.app"
