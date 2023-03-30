005cf0e4db298c20e844bf54cf19985a5dcd252a
Jihai Xu <jxu@rippling.com>
2023-03-30 01:41:22 -0700

    Add new cask: OpenAI Translator

---
+++ Casks/openai-translator.rb
@@ -0,0 +1,18 @@
+cask "openai-translator" do
+  version "0.0.36"
+  sha256 :no_check # Since we have different binaries for different CPU types
+
+  url do
+    on_intel do
+      "https://github.com/yetone/openai-translator/releases/download/v#{version}/OpenAI.Translator_#{version}_x64.dmg"
+    end
+    on_arm do
+      "https://github.com/yetone/openai-translator/releases/download/v#{version}/OpenAI.Translator_#{version}_aarch64.dmg"
+    end
+  end
+  name "OpenAI Translator"
+  desc "OpenAI based translation tool"
+  homepage "https://github.com/yetone/openai-translator"
+
+  app "OpenAI Translator.app"
+end
