3fb7e1bbae482dfd9ec0bdc7c36e63a855f72c37
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-30 15:11:54 +0800

    Update oversight.rb

--- Casks/oversight.rb
+++ Casks/oversight.rb
@@ -3,7 +3,7 @@ cask "oversight" do
   sha256 "a2911647a02d57bcedf6d71297a3ed281a601ad262fa0acec5e847c4ae1e609e"
 
   url "https://github.com/objective-see/OverSight/releases/download/v#{version}/OverSight_#{version}.zip",
-      verified: "github.com/objective-see/OverSight"
+      verified: "github.com/objective-see/OverSight/"
   name "OverSight"
   desc "Monitors computer mic and webcam"
   homepage "https://objective-see.com/products/oversight.html"
