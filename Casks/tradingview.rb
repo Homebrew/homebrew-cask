69a925a5f15da68c787bef3775a9a1bc79f826e3
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-30 17:58:01 -0400

    Update tradingview from 2.3.1 to 2.3.2

--- Casks/tradingview.rb
+++ Casks/tradingview.rb
@@ -1,6 +1,6 @@
 cask "tradingview" do
-  version "2.3.1"
-  sha256 "cfa3be718af32d64d8291e09eda9e40a859895079f7c655f86534aa131f971f1"
+  version "2.3.2"
+  sha256 "fe92b51a3ffd4b06d614412f792e0c689e8b3f738495dfd5880d55554c5222b2"
 
   url "https://tvd-packages.tradingview.com/stable/#{version}/darwin/TradingView.dmg"
   name "TradingView Desktop"
