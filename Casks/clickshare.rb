79fbf91399d5b8f728e763af6c25d98326e30bc4
Markus Reiter <me@reitermark.us>
2023-03-30 04:41:56 +0200

    Update clickshare.rb

--- Casks/clickshare.rb
+++ Casks/clickshare.rb
@@ -1,8 +1,8 @@
 cask "clickshare" do
-  version "4.28.0.11"
+  version "4.28.0.11,45"
   sha256 "9b14dddad05c87a85c7d5fa9f004dc029fcec49abd14f8b8196d675f7ebf2f9e"
 
-  url "https://www.barco.com/services/website/en/TdeFiles/Download?FileNumber=R3306192&TdeType=3&MajorVersion=#{format("%02d", version.major)}&MinorVersion=#{format("%02d", version.minor)}&PatchVersion=#{format("%02d", version.patch)}&BuildVersion=#{format("%03d", version.split(".").last)}"
+  url "https://www.barco.com/services/website/en/TdeFiles/Download?FileNumber=R3306192&TdeType=3&MajorVersion=#{format("%02d", version.csv.first.major)}&MinorVersion=#{format("%02d", version.csv.first.minor)}&PatchVersion=#{format("%02d", version.csv.first.patch)}&BuildVersion=#{format("%03d", version.csv.first.split(".").last)}"
   name "ClickShare"
   desc "Wireless teleconferencing client"
   homepage "https://www.barco.com/en/clickshare/app"
@@ -10,9 +10,13 @@ cask "clickshare" do
   livecheck do
     url "https://www.barco.com/en/support/software/R3306192"
     regex(/td>v?(\d+(?:\.\d+)+)</i)
+    strategy :page_match do |page, regex|
+      versions = page.scan(regex).flatten.uniq
+      "#{versions.first},#{versions.count + 1}"
+    end
   end
 
-  container nested:	"R3306192_45_ApplicationSw/ClickShare_Setup.dmg"
+  container nested:	"R3306192_#{version.csv.second}_ApplicationSw/ClickShare_Setup.dmg"
 
   app "ClickShare.app"
 
