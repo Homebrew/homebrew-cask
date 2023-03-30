78563d6b3c79616b27f35923eac4a645a914822b
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-30 07:11:08 -0400

    fix typo

--- Casks/spotify.rb
+++ Casks/spotify.rb
@@ -7,7 +7,7 @@ cask "spotify" do
     version "1.2.8.923,4f94bf0d,1547"
   end
   on_intel do
-    version "1.2.8.923,g4f94bf0d,1545"
+    version "1.2.8.923,4f94bf0d,1545"
   end
 
   url "https://download.scdn.co/Spotify#{arch}.dmg",
