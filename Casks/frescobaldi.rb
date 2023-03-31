ae309c36b9e9acc966b749c1ab18898e2035ce53
fanninpm <fanninpm@miamioh.edu>
2023-03-30 22:53:02 -0400

    Add ARM to Frescobaldi (#144263)
    Co-authored-by: Razvan Azamfirei <azamfireirazvan@gmail.com>

--- Casks/frescobaldi.rb
+++ Casks/frescobaldi.rb
@@ -1,17 +1,20 @@
 cask "frescobaldi" do
-  version "3.3.0"
-  sha256 "82f4377c3c9e787a079cfe1cf269c7c3ba41bd933fba4fc4a60347aeb1ed32f0"
+  arch arm: "arm64", intel: "x86_64"
 
-  url "https://github.com/frescobaldi/frescobaldi/releases/download/v#{version}/Frescobaldi-#{version}-x86_64.dmg",
+  version "3.3.0"
+  sha256 arm:   "53de57e3aef74170c617ce96e24b63343d4f4c11cdfcd98cc39e7233c6b168b7",
+         intel: "82f4377c3c9e787a079cfe1cf269c7c3ba41bd933fba4fc4a60347aeb1ed32f0"
+
+  url "https://github.com/frescobaldi/frescobaldi/releases/download/v#{version}/Frescobaldi-#{version}-#{arch}.dmg",
       verified: "github.com/frescobaldi/frescobaldi/"
   name "Frescobaldi"
   desc "LilyPond editor"
   homepage "https://frescobaldi.org/"
 
+  # Some GitHub tags do not follow standard versioning pattern
   livecheck do
-    url "https://github.com/frescobaldi/frescobaldi/releases/"
-    regex(/Frescobaldi[._-]v?(\d+(?:\.\d+)+)[._-]x86[._-]64\.dmg/i)
-    strategy :page_match
+    url :url
+    strategy :github_latest
   end
 
   app "Frescobaldi.app"
