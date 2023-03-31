b9807b77f7a93882c0604ec6c500d79e78cf0749
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-31 18:16:52 -0400

    pyzo 4.12.8

--- Casks/pyzo.rb
+++ Casks/pyzo.rb
@@ -1,6 +1,6 @@
 cask "pyzo" do
-  version "4.12.7"
-  sha256 "2bf0c360bda66e542a808a8d2e046d1b149099bd3577d096d9437ece177f84c8"
+  version "4.12.8"
+  sha256 "f807a5079ddb9cd7c04ad8915bd6df98e302185469886f6521aea818f884b018"
 
   url "https://github.com/pyzo/pyzo/releases/download/v#{version}/pyzo-#{version}-macos_x86_64.dmg",
       verified: "github.com/pyzo/pyzo/"
