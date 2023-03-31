838c20ab3cd5763f2eade2c98dd7b416903753a3
eekleefeld <87045020+eekleefeld@users.noreply.github.com>
2023-03-31 11:41:18 -0400

    Update simple-comic.rb (#144289)
    * Update simple-comic.rb
    
    The filename structure for the downloadable zip changed slightly, in a way that has to be done manually here rather than just the usual method of bump-cask-pr --version.
    
    * Update simple-comic.rb
    
    ---------
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/simple-comic.rb
+++ Casks/simple-comic.rb
@@ -1,8 +1,8 @@
 cask "simple-comic" do
-  version "1.9.5"
-  sha256 "ff9bce3fa518e568cad3eba2112e54a6b831cc9b7c017cb203c8b07fcf95bdab"
+  version "1.9.6"
+  sha256 "daf936d4eac078bce73c4b544a2f9f921e019e496aa6b35c75d201a411e2e5a2"
 
-  url "https://github.com/MaddTheSane/Simple-Comic/releases/download/App-Store-#{version}/Simple.Comic.#{version}.zip"
+  url "https://github.com/MaddTheSane/Simple-Comic/releases/download/App-Store-#{version}/Simple.Comic.app.zip"
   name "Simple Comic"
   desc "Comic viewer/reader"
   homepage "https://github.com/MaddTheSane/Simple-Comic"
