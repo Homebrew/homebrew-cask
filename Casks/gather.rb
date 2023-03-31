bb9b5420370f6936273404daea55bd8cd8118ba2
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-31 18:49:54 -0400

    gather 0.5.22 (#144313)
    \n

--- Casks/gather.rb
+++ Casks/gather.rb
@@ -1,9 +1,9 @@
 cask "gather" do
   arch arm: "-arm64"
 
-  version "0.5.21"
-  sha256 arm:   "ab9dbf5af61e91a71f01dd61c33057abd3f998ebb481a79cf0c5ee278437feaf",
-         intel: "b3073575dc595f2ceba6457dac6dca88a532b2c2df5806cbc3d593df5fac4899"
+  version "0.5.22"
+  sha256 arm:   "b4eb1fc89d9fc43a945a277fbd0b38ad0a0be6561e0dd3de5b3b9ba97d181eb8",
+         intel: "ac96a59dfc93f9ca460e62e8e1be62060e5bdd6c655ddeb590a6255483e40a2f"
 
   url "https://github.com/gathertown/gather-town-desktop-releases/releases/download/v#{version}/Gather-#{version}#{arch}-mac.zip",
       verified: "github.com/gathertown/gather-town-desktop-releases/"
