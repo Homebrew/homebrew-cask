2beb93d7659ddf0cebe56c881490729426c28c88
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-03-31 10:43:46 +0800

    Update strawberry from 1.0.16 to 1.0.17 (#144262)

--- Casks/strawberry.rb
+++ Casks/strawberry.rb
@@ -1,6 +1,6 @@
 cask "strawberry" do
-  version "1.0.16"
-  sha256 "bd6f1b82fab14da44fecf7e22a90a21db48ed134a7a41d193f3c2954ea36f190"
+  version "1.0.17"
+  sha256 "654410e49fb51c752b3fb982e7e0dc5af9ac361ac70e18e529e432432c024d00"
 
   url "https://github.com/strawberrymusicplayer/strawberry/releases/download/#{version}/strawberry-#{version}-x86_64.dmg",
       verified: "github.com/strawberrymusicplayer/strawberry/"
