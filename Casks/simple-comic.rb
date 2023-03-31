dd72cdfd95449390efec7dc1ca6d9228cbefda1d
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-31 23:34:17 +0800

    Update simple-comic.rb

--- Casks/simple-comic.rb
+++ Casks/simple-comic.rb
@@ -3,7 +3,6 @@ cask "simple-comic" do
   sha256 "daf936d4eac078bce73c4b544a2f9f921e019e496aa6b35c75d201a411e2e5a2"
 
   url "https://github.com/MaddTheSane/Simple-Comic/releases/download/App-Store-#{version}/Simple.Comic.app.zip"
-  
   name "Simple Comic"
   desc "Comic viewer/reader"
   homepage "https://github.com/MaddTheSane/Simple-Comic"
