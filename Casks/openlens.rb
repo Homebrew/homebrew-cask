f221ed12df1b2dd48e4378bd1c7690f27c7102c7
Branch Vincent <branchevincent@gmail.com>
2023-03-31 19:29:55 -0700

    openlens 6.4.13

--- Casks/openlens.rb
+++ Casks/openlens.rb
@@ -1,9 +1,9 @@
 cask "openlens" do
   arch arm: "-arm64"
 
-  version "6.4.11"
-  sha256 arm:   "5201bf99200fd7f1298d838f78b02f081cb53b869da6890d3411c142a0221e0d",
-         intel: "47c91b79d2d52f66a410f37fd0a49b68845abdbad67140f2f4c2f4b214d26baf"
+  version "6.4.13"
+  sha256 arm:   "b4e0b54d908e796920424daeee64cb9d060188a9a1dfca96a69b95ef19e3a60e",
+         intel: "2738c8dad79bfec900c048992815e8ad3b5c8d9dc036134b573d49521172628e"
 
   url "https://github.com/MuhammedKalkan/OpenLens/releases/download/v#{version}/OpenLens-#{version}#{arch}.dmg"
   name "OpenLens"
