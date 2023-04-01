46367b48f86d73a39231a9bf27cfa7218339555c
miccal <miccal.matthews@gmail.com>
2023-04-01 11:09:17 +0800

    phew: various updates

--- Casks/phew.rb
+++ Casks/phew.rb
@@ -1,14 +1,15 @@
 cask "phew" do
-  version "1.0,9"
+  version "1.0"
   sha256 "f4d6c78f98926b5e84bdbba68e06f5646545af15623807df26a7715e3397203d"
 
-  url "https://sveinbjorn.org/files/software/phew/Phew-#{version.csv.first}.zip"
+  url "https://sveinbjorn.org/files/software/phew/Phew-#{version}.zip"
   name "Phew"
+  desc "FLIF image viewer and QuickLook plugin"
   homepage "https://sveinbjorn.org/phew"
 
   livecheck do
     url "https://sveinbjorn.org/files/appcasts/PhewAppcast.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   auto_updates true
