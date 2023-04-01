9409b3fb9cc45ef09623bd85ad0196a1b87fa61b
Amory Meltzer ☀ <Amorymeltzer@gmail.com>
2023-04-01 11:14:37 -0400

    hepta 0.290.0 (#144336)

--- Casks/hepta.rb
+++ Casks/hepta.rb
@@ -1,9 +1,9 @@
 cask "hepta" do
   arch arm: "arm64-mac", intel: "mac"
 
-  version "0.289.0"
-  sha256 arm:   "c184ebc86aedc109ffb44abe4a8dc1f3637814c715992200674140f4e3ac93b6",
-         intel: "2370cbc2234fffd47faa2d0532426aed19286d969ce7f7a7bda83e318d6486f3"
+  version "0.290.0"
+  sha256 arm:   "5f710fd5ccd03722417612c095a241d4b9c14a281c4b9a154721bc7d531dd094",
+         intel: "c486e6d99eeabdd3af303edce37e6a3af23c60221c8f827d1b99cbee3928bfb1"
 
   url "https://github.com/heptameta/project-meta/releases/download/v#{version}/Hepta-#{version}-#{arch}.zip",
       verified: "github.com/heptameta/project-meta/"
