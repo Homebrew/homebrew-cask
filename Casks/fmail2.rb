b74627c138203a241abf212040c5dc830b7faad0
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-04-01 21:26:08 +0800

    fmail2 1.7.6 (#144329)

--- Casks/fmail2.rb
+++ Casks/fmail2.rb
@@ -1,7 +1,7 @@
 cask "fmail2" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "1.7.5"
-  sha256 "0bb395ac2a480d71d83cf972b40234c52794f7e47199434e1029260b38774d26"
+  version "1.7.6"
+  sha256 "c012fd591e3f8202e9e3b9ad39bd12156a56f86c9a5968531345e0481ea1dd4f"
 
   url "https://arievanboxel.fr/fmail2/sparkle/FMail2_#{version.no_dots}.zip",
       verified: "arievanboxel.fr/fmail2/sparkle/"
