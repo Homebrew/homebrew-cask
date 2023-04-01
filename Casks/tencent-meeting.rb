749fb29ccfed0d1cef56b78a6fb9185a6a60715b
stevenlele <stevenlele@outlook.com>
2023-04-01 17:25:31 +0800

    tencent-meeting 3.15.5.405

--- Casks/tencent-meeting.rb
+++ Casks/tencent-meeting.rb
@@ -2,8 +2,8 @@ cask "tencent-meeting" do
   arch arm: "arm64", intel: "x86_64"
 
   on_arm do
-    version "3.14.7.407,99a99009360d9c219608b568bf6450da"
-    sha256 "b4f49783501c31bae07a396471ec48955222622893a44f7ce56ed2bb8c09fc5d"
+    version "3.15.5.405,bdb371582c2b80b959b440e5b065fbd7"
+    sha256 "8ffec113333eafdde92cde50e570bc970285e7ed51b66940f04ac76d02993474"
   end
   on_intel do
     version "3.14.7.407,e12d770b7dba1aa37791964b4c631431"
@@ -18,16 +18,32 @@ cask "tencent-meeting" do
   homepage "https://meeting.tencent.com/"
 
   livecheck do
-    skip "No version information available"
+    url %Q(https://meeting.tencent.com/web-service/query-download-info?q=[{"package-type":"app","channel":"0300000000","platform":"mac","arch":"#{arch}"}]&nonce=0000000000000000)
+    regex(%r{/cos/([0-9a-z]{32})/})
+    strategy :json do |json, regex|
+      info = json["info-list"][0]
+      "#{info["version"]},#{info["url"][regex, 1]}"
+    end
   end
 
   auto_updates true
+  depends_on macos: ">= :el_capitan"
 
   app "TencentMeeting.app"
 
+  uninstall quit: "com.tencent.meeting"
+
   zap trash: [
+    "~/Library/Application Scripts/FN2V63AD2J.com.tencent.meeting",
+    "~/Library/Application Scripts/com.tencent.meeting",
     "~/Library/Caches/com.tencent.meeting",
     "~/Library/Containers/com.tencent.meeting",
+    "~/Library/Containers/com.tencent.wemeet.FileDelta",
+    "~/Library/Group Containers/FN2V63AD2J.com.tencent.meeting",
+    "~/Library/HTTPStorages/com.tencent.meeting",
+    "~/Library/HTTPStorages/com.tencent.meeting.binarycookies",
     "~/Library/Preferences/com.tencent.meeting.plist",
+    "~/Library/Saved Application State/com.tencent.meeting.savedState",
+    "~/Library/WebKit/com.tencent.meeting",
   ]
 end
