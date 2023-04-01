b6d9873038480453dba02d8817650ff54cc212b3
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-01 19:35:00 -0400

    klayout 0.28.6

--- Casks/klayout.rb
+++ Casks/klayout.rb
@@ -1,5 +1,5 @@
 cask "klayout" do
-  version "0.28.5"
+  version "0.28.6"
 
   on_catalina :or_older do
     sha256 "53aa5c32af9ec0180f7caad7fa8924f0c151e610606d659c442e7a1ed36cd9dc"
@@ -35,7 +35,7 @@ cask "klayout" do
     end
   end
   on_ventura :or_newer do
-    sha256 "8442764b209808bda676be20add000c50b815f12a5150611be9251f045ba54aa"
+    sha256 "02fbba906feab1e3e0b021a9646374eabe901e59e886a01d96b3886496e9b0c6"
 
     url "https://www.klayout.org/downloads/MacOS/HW-klayout-#{version}-macOS-Ventura-1-qt5Brew-RsysPhb39.dmg",
         verified: "klayout.org/downloads/MacOS/"
