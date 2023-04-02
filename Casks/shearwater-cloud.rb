8039fecd4751faf2e8dd5f1cc0c4648f4855b771
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-02 18:32:28 -0400

    shearwater-cloud 2.9.8

--- Casks/shearwater-cloud.rb
+++ Casks/shearwater-cloud.rb
@@ -1,6 +1,6 @@
 cask "shearwater-cloud" do
-  version "2.9.7"
-  sha256 "5dc32099da534e35a401d6898129c69189554bacb8ba11fbdbd55b06a398e2f8"
+  version "2.9.8"
+  sha256 "4731a2e8a6bd54c261edce85b301ce71f07209ec4ba82f15d26a8cf28c7770ea"
 
   url "https://www.shearwater.com/wp-content/downloads/ShearwaterCloudInstaller_#{version}.dmg"
   name "Shearwater Cloud"
