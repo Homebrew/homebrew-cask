75720039b81d759add8bb14a7c411239cc52873e
Junyin Chen <vulpesvulpes825@gmail.com>
2023-04-02 11:23:57 -0700

    Update eloston-chromium from 110.0.5481.177-1.1 to 111.0.5563.146-1.1 (#144357)
    * Update eloston-chromium from 110.0.5481.177-1.1 to 111.0.5563.146-1.1
    
    * Update eloston-chromium from 110.0.5481.177-1.1 to 111.0.5563.146-1.1
    * Update eloston-chromium.rb to use "ungoogled-software/ungoogled-chromium-macos" repo
    
    * style: Fix Final newline missing

--- Casks/eloston-chromium.rb
+++ Casks/eloston-chromium.rb
@@ -2,22 +2,22 @@ cask "eloston-chromium" do
   arch arm: "arm64", intel: "x86-64"
 
   on_arm do
-    version "110.0.5481.100-1.1,1676884754"
-    sha256 "d4e8258a9e91618d2da82968285498f035f31e4912646479c225b1a4ae43675c"
+    version "111.0.5563.146-1.1,1680284468"
+    sha256 "3a72d63c954cc26ae42b189ad54b803546dfc8aea4c8fc180d7eced2f30c4493"
   end
   on_intel do
-    version "110.0.5481.177-1.1,1677414408"
-    sha256 "c2a956c0e6c1ff3afbb831032cac7eca2573e9521dce4c0e61809b657f88562d"
+    version "111.0.5563.146-1.1,1680221358"
+    sha256 "a3cf1a38f214493d66d1aa23331ca1268b5b9e6b4ef1422a97bda957b251837d"
   end
 
-  url "https://github.com/kramred/ungoogled-chromium-macos/releases/download/#{version.csv.first}_#{arch}__#{version.csv.second}/ungoogled-chromium_#{version.csv.first}_#{arch}-macos.dmg",
-      verified: "github.com/kramred/ungoogled-chromium-macos/"
+  url "https://github.com/ungoogled-software/ungoogled-chromium-macos/releases/download/#{version.csv.first}_#{arch}__#{version.csv.second}/ungoogled-chromium_#{version.csv.first}_#{arch}-macos.dmg",
+      verified: "github.com/ungoogled-software/ungoogled-chromium-macos/"
   name "Ungoogled Chromium"
   desc "Google Chromium, sans integration with Google"
   homepage "https://ungoogled-software.github.io/ungoogled-chromium-binaries/"
 
   livecheck do
-    url "https://github.com/kramred/ungoogled-chromium-macos/releases?q=prerelease%3Afalse"
+    url "https://github.com/ungoogled-software/ungoogled-chromium-macos/releases?q=prerelease%3Afalse"
     regex(%r{href=["']?[^"' >]*?/tree/v?(\d+(?:[.-]\d+)+)(?:[._-]#{arch})?(?:[._-]+?(\d+(?:\.\d+)*))?["' >]}i)
     strategy :page_match do |page, regex|
       page.scan(regex).map do |match|
