f97a7a3d5071766428943dea0571fcd46f16e2d4
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-02 12:37:16 -0400

    portfolioperformance 0.62.0 (#144352)
    \n

--- Casks/portfolioperformance.rb
+++ Casks/portfolioperformance.rb
@@ -1,9 +1,9 @@
 cask "portfolioperformance" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.61.4"
-  sha256 arm:   "83efb52535cdc65d4c2576938b77b9a07fd0dd6faad521d57b3fb5dc6404bc22",
-         intel: "2fe43aee1d7d3a8afd93e9ca07d08f313a721342844c6bf1cd274f7581088ae8"
+  version "0.62.0"
+  sha256 arm:   "b28a54c5a5c9fa137cb31cd16f7e8fa82960e9ccbc15d0fc500bf140355d2e4d",
+         intel: "45730f2b1de71b98827a6a80adc76d71cc31d254c028aa5f670af0a10aa45781"
 
   url "https://github.com/buchen/portfolio/releases/download/#{version}/PortfolioPerformance-#{version}-#{arch}.dmg",
       verified: "github.com/buchen/portfolio/"
