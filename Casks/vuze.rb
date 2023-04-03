8a6b13a96a378dd61c765f3fe1fbf4e9ff0bfe67
Markus Reiter <me@reitermark.us>
2023-04-03 04:57:33 +0200

    Add livechecks for unversioned casks.

--- Casks/vuze.rb
+++ Casks/vuze.rb
@@ -1,11 +1,15 @@
 cask "vuze" do
-  version :latest
+  version "5.7.7.0"
   sha256 :no_check
 
   url "https://cf1.vuze.com/files/J7/VuzeBittorrentClientInstaller.dmg"
   name "Vuze"
   desc "Bit torrent client"
   homepage "https://www.vuze.com/"
+
+  livecheck do
+    skip "version is contained in the mounted DMG volume name"
+  end
 
   installer script: {
     executable: "Vuze Installer.app/Contents/MacOS/JavaApplicationStub",
