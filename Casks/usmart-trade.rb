dbc2d53b63932ef2aa6a5c43931113daa3530b6b
Charles Siu <chunheisiu@gmail.com>
2023-04-03 08:20:40 +0800

    Apply suggestions from code review
    Co-authored-by: Razvan Azamfirei <azamfireirazvan@gmail.com>

--- Casks/usmart-trade.rb
+++ Casks/usmart-trade.rb
@@ -3,18 +3,14 @@ cask "usmart-trade" do
   sha256 "a48955eb6df082ccc6e1612cf213c7dd60c50ece4d2f1eeff8f6cc06bcf02888"
 
   url "https://jy-common-prd-1257884527.cos.ap-guangzhou.myqcloud.com/admin/app-version-file/#{version.csv.third}/#{version.csv.second}/uSMART%20Trade-#{version.csv.first}.dmg",
-      verified: "jy-common-prd-1257884527.cos.ap-guangzhou.myqcloud.com/admin/app-version-file/"
+      verified: "jy-common-prd-1257884527.cos.ap-guangzhou.myqcloud.com/"
   name "uSMART Trade"
   desc "Trade Stocks & Options"
   homepage "https://www.usmartglobal.com/"
 
   livecheck do
     url "https://www.usmart.hk/en/download"
-    regex(%r{
-      /(\d{4}\b[._-]\d{2}\b[._-]\d{2}\b)
-      /([0-9a-f]{8}\b-[0-9a-f]{4}\b-[0-9a-f]{4}\b-[0-9a-f]{4}\b-[0-9a-f]{12})
-      /uSMART\sTrade[._-](\d+(?:\.\d+)+)\.dmg
-    }ix)
+    regex(%r{href=['"].*?file/(\d+[-_]\d+[-_]\d+)/((?:\w+-)+(?:\w+))/.*?Trade[-_]?(\d+(?:\.\d+)+).dmg}i)
     strategy :page_match do |page|
       page.scan(regex)
           .map { |match| "#{match[2]},#{match[1]},#{match[0]}" }
