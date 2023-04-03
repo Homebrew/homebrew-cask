210442f415444a1e80a98a343f1068495cb14d11
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-02 20:04:19 -0400

    altair-graphql-client 5.0.20 (#144369)
    \n

--- Casks/altair-graphql-client.rb
+++ Casks/altair-graphql-client.rb
@@ -1,9 +1,9 @@
 cask "altair-graphql-client" do
   arch arm: "arm64", intel: "x64"
 
-  version "5.0.19"
-  sha256 arm:   "e12aa639322d33867f31398c743927720cbb05a1f22c4da10829392616a75805",
-         intel: "1d9f7a048fab7381d461766e06c6f3b34ccc890a0e7a84f60b75fc4143e825f7"
+  version "5.0.20"
+  sha256 arm:   "5d552bfa39f76a5a898e16d92d61def317be0db44bae3f6a9ad70281d6517866",
+         intel: "29c4c3363a4a99e28b56108504887709d4bf547368ecbe1baefd1a0632b0e8dd"
 
   url "https://github.com/imolorhe/altair/releases/download/v#{version}/altair_#{version}_#{arch}_mac.zip",
       verified: "github.com/imolorhe/altair/"
