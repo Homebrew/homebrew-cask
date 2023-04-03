ecd538dfe23724fefd4cd5d1345bef1d2b64b314
Ian Hu <ianhu92@gmail.com>
2023-04-03 11:03:25 +0800

    tencent-docs 2.4.16 (#144397)
    Co-authored-by: ianhu92 <ianhu@tencent.com>

--- Casks/tencent-docs.rb
+++ Casks/tencent-docs.rb
@@ -1,7 +1,7 @@
 cask "tencent-docs" do
   arch arm: "Arm64", intel: "X64"
 
-  version "2.4.14"
+  version "2.4.16"
   sha256 :no_check
 
   url "https://down.qq.com/qqweb/mac_docs/MacTencentDocs#{arch}.dmg"
