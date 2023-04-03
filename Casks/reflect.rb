10b4a62f4816992524672bee96791d818c805c1e
Patrick Linnane <patrick@linnane.io>
2023-04-02 21:32:49 -0700

    reflect 2.0.6

--- Casks/reflect.rb
+++ Casks/reflect.rb
@@ -1,9 +1,9 @@
 cask "reflect" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.0.5"
-  sha256 arm:   "a49882fa00cddb1e8d5cf02a922885e4ac786ee32ab2da263cee7fe05372ba3c",
-         intel: "5b2fa051ad0343d5a1082951194fbfe4a568ae6ef9edb17aa0601a2cb3d360be"
+  version "2.0.6"
+  sha256 arm:   "b473b033cf810c659533f8f9fa70435b22922eaaca20dc00a8a5053606dee763",
+         intel: "ed2292fc93ff16a195309765b5da4f116b506a6cc0d4056050891d9159e46440"
 
   url "https://github.com/team-reflect/reflect-electron-updates/releases/download/v#{version}/Reflect-darwin-#{arch}-#{version}.zip",
       verified: "github.com/team-reflect/reflect-electron-updates/"
