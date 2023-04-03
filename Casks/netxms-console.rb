ec7c5a79d873ed7585834252007da4d9d129cb3e
Patrick Linnane <patrick@linnane.io>
2023-04-02 21:27:08 -0700

    netxms-console 4.3.3

--- Casks/netxms-console.rb
+++ Casks/netxms-console.rb
@@ -1,9 +1,9 @@
 cask "netxms-console" do
   arch arm: "-aarch64"
 
-  version "4.3.2"
-  sha256 arm:   "49b0fe4df758b197e9ecc2275a583ab37c3b43fe50e38ed43ee133604d0ea5e7",
-         intel: "4d25b0cf6506be0924967bf83f491879707ee0771c6409b1942b2fa54aa49b82"
+  version "4.3.3"
+  sha256 arm:   "e73ca93d4d029f0596636c08a726e00cd5c69ddb7f652c171dcac67f71acafa5",
+         intel: "73f17ff70fd4fca4816a96abe7df6b1922c76ff33d28b9ff4a084d3ea0ecd8f2"
 
   url "https://netxms.org/download/releases/#{version.major_minor}/nxmc-#{version}#{arch}.dmg"
   name "NetXMS Management Console"
