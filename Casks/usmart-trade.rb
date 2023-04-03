41dcd962f1c3dfca6d83fdb053ac6f485d4482c4
Charles Siu <chunheisiu@gmail.com>
2023-04-03 09:18:31 +0800

    Add uSMART Trade v3.20.0 (#144363)
    Co-authored-by: Razvan Azamfirei <azamfireirazvan@gmail.com>

---
+++ Casks/usmart-trade.rb
@@ -0,0 +1,27 @@
+cask "usmart-trade" do
+  version "3.20.0,1a879021-a3eb-4059-b141-5760ac163737,2023-03-24"
+  sha256 "a48955eb6df082ccc6e1612cf213c7dd60c50ece4d2f1eeff8f6cc06bcf02888"
+
+  url "https://jy-common-prd-1257884527.cos.ap-guangzhou.myqcloud.com/admin/app-version-file/#{version.csv.third}/#{version.csv.second}/uSMART%20Trade-#{version.csv.first}.dmg",
+      verified: "jy-common-prd-1257884527.cos.ap-guangzhou.myqcloud.com/"
+  name "uSMART Trade"
+  desc "Trade Stocks & Options"
+  homepage "https://www.usmartglobal.com/"
+
+  livecheck do
+    url "https://www.usmart.hk/en/download"
+    regex(%r{href=['"].*?file/(\d+[-_]\d+[-_]\d+)/((?:\w+-)+(?:\w+))/.*?Trade[-_]?(\d+(?:\.\d+)+).dmg}i)
+    strategy :page_match do |page|
+      page.scan(regex)
+          .map { |match| "#{match[2]},#{match[1]},#{match[0]}" }
+    end
+  end
+
+  app "uSMART Trade.app"
+
+  zap trash: [
+    "~/Library/Application Support/uSmart",
+    "~/Library/Preferences/com.usmart.mac.test.plist",
+    "~/Library/Saved Application State/com.usmart.mac.test.savedState",
+  ]
+end
