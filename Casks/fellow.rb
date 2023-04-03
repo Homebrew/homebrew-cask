6e8b6a0760d649fb45cbacec0ebe249fd18c1638
miccal <miccal.matthews@gmail.com>
2023-04-03 08:25:54 +0800

    fellow 1.4.16

--- Casks/fellow.rb
+++ Casks/fellow.rb
@@ -1,6 +1,6 @@
 cask "fellow" do
-  version "1.4.7"
-  sha256 "bc62e204413b151e1f6875e09a3ddf3019be0e7e49dc373d2b7eb6338af980c3"
+  version "1.4.16"
+  sha256 "bbf19a2113a5f9b68472dd61e136c0f783066ffae7c22a55d6ae5da058c2feec"
 
   url "https://cdn.fellow.app/desktop/#{version}/darwin/stable/universal/Fellow-#{version}-universal.dmg"
   name "Fellow"
