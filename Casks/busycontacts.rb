c7b3ceefb9419d5353f6479f8b1c84e5e71aa722
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-04-05 06:56:43 +0800

    busycontacts 2023.1.1,2023-04-04-23-38

--- Casks/busycontacts.rb
+++ Casks/busycontacts.rb
@@ -1,6 +1,6 @@
 cask "busycontacts" do
-  version "2023.1.1,2023-03-22-21-30"
-  sha256 "dc4e5dfb6e6028b1a823ec0e2a2dfec4e5d9c72f7f0a0e44f602f0b377b43972"
+  version "2023.1.1,2023-04-04-23-38"
+  sha256 "440c41d4cefc6d09970434d6ea15d8a909217f92b73bcc0444e21527d1877cdc"
 
   url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bct-#{version.csv.first}-#{version.csv.second}.zip",
       verified: "7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/"
