31a67e274070b01773dda9195d00fac824e120eb
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-04-05 01:06:46 +0800

    crossover 22.1.1.35679 (#144501)
    * crossover 22.1.1.35679
    
    * Update crossover.rb
    
    ---------
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/crossover.rb
+++ Casks/crossover.rb
@@ -1,15 +1,15 @@
 cask "crossover" do
-  version "22.1.0.35672"
-  sha256 "4c799041079abeabdad987135d3e1a281f670376e575c098e2bfbff6c3dfe4b0"
+  version "22.1.1"
+  sha256 "ffcaa3366d3d78fd2c3ba4101b94eb7b1646449336b92a143663ba52ab3147ae"
 
-  url "https://media.codeweavers.com/pub/crossover/cxmac/demo/crossover-#{version.major_minor_patch}.zip"
+  url "https://media.codeweavers.com/pub/crossover/cxmac/demo/crossover-#{version}.zip"
   name "CrossOver"
   desc "Tool to run Windows software"
   homepage "https://www.codeweavers.com/products/crossover-mac/"
 
   livecheck do
     url "https://www.codeweavers.com/xml/versions/cxmac.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   auto_updates true
