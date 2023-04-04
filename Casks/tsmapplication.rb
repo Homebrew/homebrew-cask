4534acb358c2403670dc8f2d1520e934e36e6faf
Rahul Mohandas <rahul0705@gmail.com>
2023-04-04 13:20:01 -0400

    feat(TSMApplication) added cask
    Added the TSMApplication to be managed through homebrew

---
+++ Casks/tsmapplication.rb
@@ -0,0 +1,11 @@
+cask "tsmapplication" do
+  version "306"
+  sha256 :no_check
+
+  url "https://www.tradeskillmaster.com/download/TSMApplication.dmg"
+  name "tsmapplication"
+  desc "Bridge between the TradeSkillMaster website and addons"
+  homepage "https://www.tradeskillmaster.com/"
+
+  app "TSMApplication.app"
+end
