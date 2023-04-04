5aecb32bf7e1161ab0d51b43d97aa1aab9e6f2e7
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-04-04 23:27:31 +0800

    imhex 1.28.0

--- Casks/imhex.rb
+++ Casks/imhex.rb
@@ -1,6 +1,6 @@
 cask "imhex" do
-  version "1.27.1"
-  sha256 "68f3102a93123ed6d4f7226bbbfd7cf239bb3eab2b000475110dd1aad6ca8237"
+  version "1.28.0"
+  sha256 "ca5b33700f233d615de700a86fec48144ce1f40372ffda6254cd3c64a68e77b5"
 
   url "https://github.com/WerWolv/ImHex/releases/download/v#{version}/imhex-#{version}-macOS-x86_64.dmg",
       verified: "github.com/WerWolv/ImHex/"
