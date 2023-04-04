8d949895a8a6920c0536a439a327382cc2e28169
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-04-05 07:18:50 +0800

    mi 3.7.5 (#144528)
    * mi 3.7.5
    
    * update livecheck
    
    * update version
    
    * new livecheck block
    
    ---------
    
    Co-authored-by: Razvan Azamfirei <azamfireirazvan@gmail.com>

--- Casks/mi.rb
+++ Casks/mi.rb
@@ -1,8 +1,8 @@
 cask "mi" do
-  version "3.7.4,1235"
-  sha256 "2bd7eb7d9976fb5a3b4420dd60e64c0dbbe8ba9c600881f0dbcfec57c43c8544"
+  version "3.7.5"
+  sha256 "12085323b38e9facffad676a0080344831cee58f88c602468e5a15ea647ec54b"
 
-  url "https://www.mimikaki.net/download/mi#{version.csv.first}.dmg"
+  url "https://www.mimikaki.net/download/mi#{version}.dmg"
   name "mi"
   desc "Text editor"
   homepage "https://www.mimikaki.net/"
@@ -10,6 +10,13 @@ cask "mi" do
   livecheck do
     url "https://www.mimikaki.net/download/appcast.xml"
     strategy :sparkle
+    regex(/(\d+(?:\.\d+)*)/i)
+    strategy :sparkle do |item, regex|
+      match = item.short_version.match(regex)
+      next if match.blank?
+
+      match.to_s
+    end
   end
 
   conflicts_with cask: "homebrew/cask-versions/mi-beta"
