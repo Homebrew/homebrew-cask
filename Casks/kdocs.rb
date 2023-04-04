e3612691a02c1bb66c626076d0fb24fed0e8c439
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-04-04 17:55:24 +0800

    kdocs 3.8.2

--- Casks/kdocs.rb
+++ Casks/kdocs.rb
@@ -1,6 +1,6 @@
 cask "kdocs" do
-  version "3.8.1,1001"
-  sha256 "987ff5cf36c899648e9575c93dcb88a15aaec8173ffca5556cc6485fae0bd707"
+  version "3.8.2,1001"
+  sha256 "1cc22cfa226f5c6b62ed19b740c51a91a595d9b59ea431ec8e17a8ebcda4ddff"
 
   url "https://qn.cache.wpscdn.cn/kdocs/apk/kdesktopmac/KDocs_#{version.csv.second}_v#{version.csv.first}.dmg",
       verified: "qn.cache.wpscdn.cn/kdocs/apk/kdesktopmac/"
