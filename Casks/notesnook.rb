12c1f2272f098e2323d50bfc7899bc98b058d213
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-04-05 07:14:43 +0800

    notesnook 2.4.7

--- Casks/notesnook.rb
+++ Casks/notesnook.rb
@@ -1,9 +1,9 @@
 cask "notesnook" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.4.6"
-  sha256 arm:   "a11540b4be49bf23e5a23fcec80c192b656cefdd99a663ce981d333d7c1257a1",
-         intel: "d9d5a042f7da7f5b1edc6c45c5d4267db8ed1c88529b96eefab79de3ab406cdb"
+  version "2.4.7"
+  sha256 arm:   "62f963ca7290362e7678a4d8e02b8267512c75a968a49b20864d8da7f0ec69f0",
+         intel: "d3b3af58ad9c7bb380b2aa06aa2af185b1176c8b385437143398728ed340cb50"
 
   url "https://github.com/streetwriters/notesnook/releases/download/v#{version}/notesnook_mac_#{arch}.dmg",
       verified: "github.com/streetwriters/notesnook/"
