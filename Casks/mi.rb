81a33605b3ae613dd4a3cf393f2b49981aefde9d
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-04 19:04:58 -0400

    new livecheck block

--- Casks/mi.rb
+++ Casks/mi.rb
@@ -2,14 +2,21 @@ cask "mi" do
   version "3.7.5"
   sha256 "12085323b38e9facffad676a0080344831cee58f88c602468e5a15ea647ec54b"
 
-  url "https://www.mimikaki.net/download/mi#{version.csv.first}.dmg"
+  url "https://www.mimikaki.net/download/mi#{version}.dmg"
   name "mi"
   desc "Text editor"
   homepage "https://www.mimikaki.net/"
 
   livecheck do
     url "https://www.mimikaki.net/download/appcast.xml"
-    strategy :sparkle, &:short_version
+    strategy :sparkle
+    regex(/(\d+(?:\.\d+)*)/i)
+    strategy :sparkle do |item, regex|
+      match = item.short_version.match(regex)
+      next if match.blank?
+
+      match.to_s
+    end
   end
 
   conflicts_with cask: "homebrew/cask-versions/mi-beta"
