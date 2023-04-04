dd3f730a2d2275a3f985879135ee855409496a34
Miccal Matthews <miccal.matthews@gmail.com>
2023-04-05 00:59:17 +0800

    Update crossover.rb

--- Casks/crossover.rb
+++ Casks/crossover.rb
@@ -1,15 +1,15 @@
 cask "crossover" do
-  version "22.1.1.35679"
+  version "22.1.1"
   sha256 "ffcaa3366d3d78fd2c3ba4101b94eb7b1646449336b92a143663ba52ab3147ae"
 
-  url "https://media.codeweavers.com/pub/crossover/cxmac/demo/crossover-#{version.major_minor_patch}.zip"
+  url "https://media.codeweavers.com/pub/crossover/cxmac/demo/crossover-#{version}.zip"
   name "CrossOver"
   desc "Tool to run Windows software"
   homepage "https://www.codeweavers.com/products/crossover-mac/"
 
   livecheck do
     url "https://www.codeweavers.com/xml/versions/cxmac.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   auto_updates true
