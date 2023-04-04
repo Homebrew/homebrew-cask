54df254ef08bf901fccf988a314ca28132e997ce
jrobinfo <6076421+jrobinfo@users.noreply.github.com>
2023-04-04 13:19:25 -0400

    sparrow 1.7.4 (#144516)

--- Casks/sparrow.rb
+++ Casks/sparrow.rb
@@ -1,9 +1,9 @@
 cask "sparrow" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.7.3"
-  sha256 arm:   "a679507771867edc51f608eacb606a32e63034937c22dd445a1caa634178ddda",
-         intel: "e84dfe28938a5e35390407a1006010a4d92393a6f5103939e1765d39407a27c8"
+  version "1.7.4"
+  sha256 arm:   "810e60c8761e25ba78449a7c41a8a3af19e1d84dfc80566f715b69a800eacec1",
+         intel: "980f7fb4fecf3f261e65c23ffe11bf66221f745a7b3ebd91f3a7934e8db2f59c"
 
   url "https://github.com/sparrowwallet/sparrow/releases/download/#{version}/Sparrow-#{version}-#{arch}.dmg",
       verified: "github.com/sparrowwallet/sparrow/"
