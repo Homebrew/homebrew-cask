a8571e42fee6567c3255ee606d18230a6ea6cef4
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-04-05 06:42:26 +0800

    warp 0.2023.03.28.08.03.stable_02

--- Casks/warp.rb
+++ Casks/warp.rb
@@ -1,6 +1,6 @@
 cask "warp" do
-  version "0.2023.03.28.08.03.stable_01"
-  sha256 "c61b43c3416351d692ac96cbf3b997476e9070798ed1b4ff8903a6efe6209190"
+  version "0.2023.03.28.08.03.stable_02"
+  sha256 "e7703d9419988501130c33266285d0c87d06d404912594de6c65d6d86b7efd58"
 
   url "https://app.warp.dev/download/brew?version=v#{version}"
   name "Warp"
