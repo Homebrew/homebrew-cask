a1e17172aeab3ae74d2800b4d17b2e6181ce9373
Markus Reiter <me@reitermark.us>
2023-04-05 14:39:08 +0200

    google-cloud-sdk 425.0.0 (#144555)
    * google-cloud-sdk 425.0.0
    
    * Don't break existing `google-cloud-sdk` installs as much.
    
    * Use snake-case for `zsh` path.
    
    * Uninstall `latest` symlink.

--- Casks/google-cloud-sdk.rb
+++ Casks/google-cloud-sdk.rb
@@ -1,47 +1,72 @@
 cask "google-cloud-sdk" do
-  version :latest # Must remain unversioned, else all installed gcloud components would be lost on upgrade
-  sha256 :no_check
+  arch arm: "arm", intel: "x86_64"
 
-  url "https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.tar.gz"
+  version "425.0.0"
+  sha256 arm:   "0871269d6f7314b2c8f32afcddcc1054d62a108cb89c80a0cdee9f4542d0cece",
+         intel: "b1f0731482243a08ffc2c362092871ab0fa2bd0db1bc68f12ae0c6bf284a0b1c"
+
+  url "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-#{version}-darwin-#{arch}.tar.gz"
   name "Google Cloud SDK"
   desc "Set of tools to manage resources and applications hosted on Google Cloud"
   homepage "https://cloud.google.com/sdk/"
 
+  livecheck do
+    url "https://cloud.google.com/sdk/docs/install-sdk"
+    regex(/google-cloud-cli-(\d+(?:\.\d+)+)/i)
+  end
+
   depends_on formula: "python"
 
+  google_cloud_sdk_root = "#{HOMEBREW_PREFIX}/share/google-cloud-sdk"
+
   installer script: {
-    executable: "#{token}/install.sh",
+    executable: "google-cloud-sdk/install.sh",
     args:       [
-      "--usage-reporting", "false", "--bash-completion", "false", "--path-update", "false",
-      "--rc-path", "false", "--quiet",
+      "--quiet",
+      "--usage-reporting", "false",
+      "--bash-completion", "false",
+      "--path-update", "false",
+      "--rc-path", "false",
       "--install-python", "false"
     ],
   }
-  binary "#{token}/bin/anthoscli"
-  binary "#{token}/bin/bq"
-  binary "#{token}/bin/docker-credential-gcloud"
-  binary "#{token}/bin/gcloud"
-  binary "#{token}/bin/git-credential-gcloud.sh", target: "git-credential-gcloud"
-  binary "#{token}/bin/gsutil"
-  binary "#{token}/completion.bash.inc",
-         target: "#{HOMEBREW_PREFIX}/etc/bash_completion.d/#{token}"
-  binary "#{token}/completion.zsh.inc",
-         target: "#{HOMEBREW_PREFIX}/share/zsh/site-functions/_#{token}"
+  binary "google-cloud-sdk/bin/anthoscli"
+  binary "google-cloud-sdk/bin/bq"
+  binary "google-cloud-sdk/bin/docker-credential-gcloud"
+  binary "google-cloud-sdk/bin/gcloud"
+  binary "google-cloud-sdk/bin/git-credential-gcloud.sh", target: "git-credential-gcloud"
+  binary "google-cloud-sdk/bin/gsutil"
+  binary "google-cloud-sdk/completion.bash.inc",
+         target: "#{HOMEBREW_PREFIX}/etc/bash_completion.d/google-cloud-sdk"
+  binary "google-cloud-sdk/completion.zsh.inc",
+         target: "#{HOMEBREW_PREFIX}/share/zsh/site-functions/_google_cloud_sdk"
 
-  # Not actually necessary, since it would be deleted anyway.
-  # It is present to make clear an uninstall was not forgotten and that for this cask it is indeed this simple.
-  uninstall delete: "#{staged_path}/#{token}"
+  preflight do
+    # HACK: Allow existing shell profiles to work by linking the current version to the `latest` directory.
+    FileUtils.ln_s staged_path, (staged_path.dirname/"latest"), force: true
+
+    FileUtils.cp_r staged_path/"google-cloud-sdk/.", google_cloud_sdk_root, remove_destination: true
+    (staged_path/"google-cloud-sdk").rmtree
+    FileUtils.ln_s google_cloud_sdk_root, (staged_path/"google-cloud-sdk")
+  end
+
+  uninstall delete: staged_path.dirname/"latest"
+
+  zap trash: [
+    google_cloud_sdk_root,
+    "#{google_cloud_sdk_root}.staging",
+  ]
 
   caveats <<~EOS
     To add gcloud components to your PATH, add this to your profile:
 
       for bash users
-        source "#{staged_path}/#{token}/path.bash.inc"
+        source "#{google_cloud_sdk_root}/path.bash.inc"
 
       for zsh users
-        source "#{staged_path}/#{token}/path.zsh.inc"
+        source "#{google_cloud_sdk_root}/path.zsh.inc"
 
       for fish users
-        source "#{staged_path}/#{token}/path.fish.inc"
+        source "#{google_cloud_sdk_root}/path.fish.inc"
   EOS
 end
