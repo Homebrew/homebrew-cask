490048aedaeb60a89eb6994ca3dc69c8ce93653e
Alex Ati <alejandro@atienzaramos.com>
2023-04-05 15:41:19 +0200

    Update chromedriver from 111.0.5563.64 to 112.0.5615.49

--- Casks/chromedriver.rb
+++ Casks/chromedriver.rb
@@ -1,9 +1,9 @@
 cask "chromedriver" do
   arch arm: "mac_arm64", intel: "mac64"
 
-  version "111.0.5563.64"
-  sha256 arm:   "ec65b47e99b474a74a9a12a1986c00ba614019523a698012cbdc0cb9e23af67b",
-         intel: "0edd1b537f52ac19c32c0e63a7e6b2c7d3d6de509671f42b2c2d72d2ccb8ed47"
+  version "112.0.5615.49"
+  sha256 arm:   "78f9e001d7850a81dfe8978cd084c28627fa5a06ea9db4788b2b48a5b1dd1fbc",
+         intel: "f9746ab188d454ec59dc772d0ccbc138c70e270d73e3ad35b1efbdf24a99c761"
 
   url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_#{arch}.zip",
       verified: "chromedriver.storage.googleapis.com/"
