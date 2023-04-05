095d82291a8cac4b4a7e736a5bd5baa62f60a058
eloyaldea <35566945+eloyaldea@users.noreply.github.com>
2023-04-05 19:19:47 +0200

    macupdater 3.0.3 (#144582)

--- Casks/macupdater.rb
+++ Casks/macupdater.rb
@@ -8,7 +8,7 @@ cask "macupdater" do
     depends_on macos: ">= :mojave"
   end
   on_ventura :or_newer do
-    version "3.0.2,15600"
+    version "3.0.3,15686"
     sha256 :no_check
 
     url "https://www.corecode.io/downloads/macupdater_latest.dmg"
