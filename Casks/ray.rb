d828726c85d3f9f0b0651e3670dc545b796aaac1
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-04-05 20:49:03 +0800

    ray 2.4.0 (#144567)

--- Casks/ray.rb
+++ Casks/ray.rb
@@ -2,9 +2,9 @@ cask "ray" do
   arch arm: "-arm64"
   folder = on_arch_conditional arm: "arm64/"
 
-  version "2.3.0"
-  sha256 arm:   "eb9648cc2b36b5829d408c947485d15e70396666ef95c812ba9c4fb754ba60c5",
-         intel: "4fac3d4425e8f3e656267ebe41a265353ec1bf3d6c15467b7f1a70e72d919955"
+  version "2.4.0"
+  sha256 arm:   "bb6d8973add10cfecd9f3bf6f2e3c45a54caa80fa1cbdd57d4c9a4759f46c33e",
+         intel: "304ab98cb746cb0a79898a505a233ff7cca1d984c7d016c2a6b339d9c003ed17"
 
   url "https://ray-app.s3.eu-west-1.amazonaws.com/#{folder}Ray-#{version}#{arch}.dmg",
       verified: "ray-app.s3.eu-west-1.amazonaws.com/"
