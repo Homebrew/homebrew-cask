90ce424de29753b468a929297462a898b46d2d02
Duane Moody <duane_moody@yahoo.com>
2023-04-05 09:13:25 -0700

    Update webarchiveextractor.rb
    Project is now maintained on github, sourceforge version out of date

--- Casks/webarchiveextractor.rb
+++ Casks/webarchiveextractor.rb
@@ -1,9 +1,9 @@
 cask "webarchiveextractor" do
-  version "0.2"
-  sha256 "ff9d0debc9c718b58752c1c8163fea627e461238b7f315bdbe85329d427bd7f4"
+  version "1.0"
+  sha256 "fbaf4cea8473966145202eda1cbb9fdcfc32a7ba291a247af1d2858663517f61"
 
-  url "https://downloads.sourceforge.net/webarchivext/WebArchiveExtractor.#{version}.zip",
-      verified: "downloads.sourceforge.net/webarchivext/"
+  url "https://github.com/robrohan/WebArchiveExtractor/releases/download/v#{version}/WebArchiveExtractor_#{version}.zip",
+      verified: "github.com/robrohan/WebArchiveExtractor/"
   name "Web Archive Extractor"
   desc "Extracts content from webarchive files to a directory"
   homepage "https://webarchivext.sourceforge.io/"
