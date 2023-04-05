293bf99d65b204ff781033d6194ec92e5d03d1ea
Amory Meltzer ☀ <Amorymeltzer@gmail.com>
2023-04-04 20:38:47 -0400

    foxglove-studio 1.48.0 (#144543)

--- Casks/foxglove-studio.rb
+++ Casks/foxglove-studio.rb
@@ -1,6 +1,6 @@
 cask "foxglove-studio" do
-  version "1.47.0"
-  sha256 "5f4c5715aa3310b66904d06d0306904de15200faf5ad3836c5e9e68d140e20db"
+  version "1.48.0"
+  sha256 "037aee8959878102b50d2e80c365d1f23724c95734e2bf3b6d4f08bec652e85e"
 
   url "https://github.com/foxglove/studio/releases/download/v#{version}/foxglove-studio-#{version}-mac-universal.dmg",
       verified: "github.com/foxglove/studio/"
