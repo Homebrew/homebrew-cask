dd306dead03716b109ebac856ddd5120dc1377d1
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-04-05 18:20:18 +0800

    remotion 3.094.0 (#144559)

--- Casks/remotion.rb
+++ Casks/remotion.rb
@@ -1,6 +1,6 @@
 cask "remotion" do
-  version "3.088.1"
-  sha256 "841715978912288783b4e90e18cb7ffb17f3db6e1e595096a715a9527522f9dc"
+  version "3.094.0"
+  sha256 "bcc892f08e3f7a40ffe3fb4f8bceee5fbe50f911cdd7da3967ea8ca48db40279"
 
   url "https://updates.remotion.com/installers/Remotion%20#{version}.dmg"
   name "remotion"
