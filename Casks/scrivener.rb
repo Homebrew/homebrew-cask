32f1bc94a3925a5484cb33f061866d7067b26347
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-04-07 03:55:09 +0800

    scrivener 3.3.1,15588

--- Casks/scrivener.rb
+++ Casks/scrivener.rb
@@ -1,6 +1,6 @@
 cask "scrivener" do
-  version "3.3.1,15586"
-  sha256 "80f857b563fb61278e80ef7b82eeb35c3f73c8df551ebe04bdf96405dfa6eec3"
+  version "3.3.1,15588"
+  sha256 "ac1665c7b72a2b88eb6678dd21c17b8ebb20e10cbde420ba5b24be38ad8492c8"
 
   url "https://scrivener.s3.amazonaws.com/mac_updates/Scrivener_1012_#{version.csv.second}.zip",
       verified: "scrivener.s3.amazonaws.com/"
