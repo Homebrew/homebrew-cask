2fef42eb1820e5526e12f526d68837c4a6e08056
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-06 10:50:24 -0400

    nozbe 2023.14 (new cask) (#144623)

---
+++ Casks/nozbe.rb
@@ -0,0 +1,27 @@
+cask "nozbe" do
+  version "2023.14"
+  sha256 "92cd7aa7855567ac3a563c481caf1bf0f795cd9efc96f20516ea77186448c990"
+
+  url "https://builds.nozbe.com/mac/#{version}/Nozbe.app.zip"
+  name "Nozbe"
+  desc "Project management app"
+  homepage "https://nozbe.com/"
+
+  livecheck do
+    url "https://nozbe.help/general/release-notes"
+    regex(/id=["']newest["'][^>]*?>\s*version\s*(\d+(?:\.\d+)+)/i)
+  end
+
+  depends_on macos: ">= :big_sur"
+
+  app "Nozbe.app"
+
+  zap trash: [
+    "~/Library/Application Support/Nozbe4",
+    "~/Library/Caches/com.nozbe4mac",
+    "~/Library/Caches/com.nozbe4mac.ShipIt",
+    "~/Library/HTTPStorages/com.nozbe4mac",
+    "~/Library/Preferences/com.nozbe4mac.plist",
+    "~/Library/Saved Application State/com.nozbe4mac.savedState",
+  ]
+end
