ae066b844157631be502c719de4c58fa3daf938a
theGreatWhiteShark <princess.trudildis@posteo.de>
2023-04-07 22:51:49 +0200

    Casks/hydrogen: update to 1.2.0 (#144714)
    Co-authored-by: Razvan Azamfirei <azamfireirazvan@gmail.com>

--- Casks/hydrogen.rb
+++ Casks/hydrogen.rb
@@ -1,12 +1,17 @@
 cask "hydrogen" do
-  version "1.1.1"
-  sha256 "755008edb085d1a5b7655e592bd6d983735a3f3a1af2b39e386c336bed002f87"
+  version "1.2.0"
+  sha256 "ac8be6d94128a3737604c670ec21c37956f3d297d12fb930a615f5f9cf35f767"
 
-  url "https://github.com/hydrogen-music/hydrogen/releases/download/#{version}/Hydrogen-#{version}-x86_64.dmg",
+  url "https://github.com/hydrogen-music/hydrogen/releases/download/#{version}/Hydrogen-#{version}.dmg",
       verified: "github.com/hydrogen-music/hydrogen/"
   name "Hydrogen"
   desc "Drum machine and sequencer"
   homepage "http://www.hydrogen-music.org/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   depends_on macos: ">= :mojave"
 
