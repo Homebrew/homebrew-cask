cfb7ff6a812a82feb3bbfc06bf72c0cad2125136
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-04-08 07:33:55 +0800

    kstars 3.6.4 (#144728)

--- Casks/kstars.rb
+++ Casks/kstars.rb
@@ -1,6 +1,6 @@
 cask "kstars" do
-  version "3.6.3"
-  sha256 "4b796cd0aa5465422d6973da64892fbff9ed89fd64fa91ee3c498f8651056805"
+  version "3.6.4"
+  sha256 "b872b28a4d91f2a786e70038fc6f75572d311c7cc46ca139b6d90bb1221e9c58"
 
   url "https://www.indilib.org/jdownloads/kstars/kstars-#{version}.dmg",
       verified: "indilib.org/jdownloads/kstars/"
