1c9faddd8abb7fb4ac000e33ae551577e88e4da5
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-04-07 18:02:29 +0800

    superhuman 21.0.24

--- Casks/superhuman.rb
+++ Casks/superhuman.rb
@@ -1,9 +1,9 @@
 cask "superhuman" do
   arch arm: "-arm64"
 
-  version "21.0.22"
-  sha256 arm:   "1fcbd13b074f1f4f8a308675b86a93898e27a8b75d1b637447f220824f062830",
-         intel: "9674a0129dd4d0596897cb32cc619b04315d7cf26e55422c5664e115d12c1ebf"
+  version "21.0.24"
+  sha256 arm:   "e35ec905514c948462e3f8260f16259e422801c9382f26121f0d29c419739034",
+         intel: "fb377562c2cf84ab9644b2fa048802f352790ea8b13666a22318816b23627350"
 
   url "https://storage.googleapis.com/download.superhuman.com/supertron-update/Superhuman-#{version}#{arch}-latest-mac.zip",
       verified: "storage.googleapis.com/download.superhuman.com/"
