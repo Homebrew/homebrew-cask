17fb82137d4eaac707e8ca6571f715b591099141
Ciarán Ainsworth <ciaranainsworth@posteo.net>
2023-04-07 16:07:35 +0200

    lagrange 1.15.8 (#144701)

--- Casks/lagrange.rb
+++ Casks/lagrange.rb
@@ -1,9 +1,9 @@
 cask "lagrange" do
   arch arm: "macos11.0-arm64", intel: "macos10.13-x86_64"
 
-  version "1.15.7"
-  sha256 arm:   "9ed912dcb8c1b02c465f7b7a846c2050d2249eda350919db5701484f51fb3778",
-         intel: "47d1d660ca2df60a0af8d2aaaffbaa465465bb2992aa023e831061acf6e4914e"
+  version "1.15.8"
+  sha256 arm:   "b746d61780336963c794dab94f8aff49b3504f894e1fc72283e7940d52d73a3e",
+         intel: "eac8cd58c9fbf8c7e8b54defa981aac3de0f502d2135185cf7e7d461c571603e"
 
   url "https://github.com/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_#{arch}.tbz",
       verified: "github.com/skyjake/lagrange/"
