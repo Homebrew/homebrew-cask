95a15813d140946021a65b67e07776da8bb0f51a
Christian Kraemer <christian.kraemer@gmail.com>
2023-04-07 11:30:41 +0200

    calibre 6.15.0 (#144685)

--- Casks/calibre.rb
+++ Casks/calibre.rb
@@ -24,8 +24,8 @@ cask "calibre" do
     end
   end
   on_big_sur :or_newer do
-    version "6.14.1"
-    sha256 "dfe117447866b49bb7d159071b248219ac8f80daae45e774dd07db2fa0988b35"
+    version "6.15.0"
+    sha256 "cd9e1c115ae32f5830c374ee5dd98fd2198065fa0b3f42d200621ce5af852167"
 
     livecheck do
       url "https://github.com/kovidgoyal/calibre"
