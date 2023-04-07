8c5ce5474394e952aec320b87bbca5eb4c1837bc
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-07 16:43:05 -0400

    fix sha and update livecheck to github latest

--- Casks/hydrogen.rb
+++ Casks/hydrogen.rb
@@ -1,12 +1,17 @@
 cask "hydrogen" do
   version "1.2.0"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "ac8be6d94128a3737604c670ec21c37956f3d297d12fb930a615f5f9cf35f767"
 
   url "https://github.com/hydrogen-music/hydrogen/releases/download/#{version}/Hydrogen-#{version}.dmg",
       verified: "github.com/hydrogen-music/hydrogen/"
   name "Hydrogen"
   desc "Drum machine and sequencer"
   homepage "http://www.hydrogen-music.org/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   depends_on macos: ">= :mojave"
 
