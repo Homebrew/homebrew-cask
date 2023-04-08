73d8fb3222d246690a10b5009c2bb78f6de89e62
rich2014 <rich2014.git@outlook.com>
2023-04-08 18:14:01 +0800

    Update Double-Commander to 1.1.0

--- Casks/double-commander.rb
+++ Casks/double-commander.rb
@@ -1,16 +1,20 @@
 cask "double-commander" do
-  version "0.9.10-9640"
-  sha256 "1859504e33816db8b19e494dedafa97e3cc90accdb15d9b9ee0c1ad2ba6064d1"
+  arch arm: "aarch64", intel: "x86_64"
 
-  url "https://downloads.sourceforge.net/doublecmd/doublecmd-#{version}.qt.x86_64.dmg",
+  version "1.1.0,10668"
+  sha256 arm:   "4727e9a4a75cb6b455d010cbd01bf8b82f1a5557a996774a98ababae3c902d34",
+         intel: "22196b11a5943ad833d5adc6df34deb3a98ea1d1d80138be34bed2ce5bda2300"
+
+  url "https://downloads.sourceforge.net/doublecmd/doublecmd-#{version.csv.first}-#{version.csv.second}.cocoa.#{arch}.dmg",
       verified: "downloads.sourceforge.net/doublecmd/"
   name "Double Commander"
-  desc "File manager"
+  desc "File Manager with double panels"
   homepage "https://doublecmd.sourceforge.io/"
 
   app "Double Commander.app"
 
-  caveats do
-    discontinued
+  preflight do
+    system_command "xattr",
+                   args: ["-d", "com.apple.quarantine", "#{staged_path}/Double Commander.app"]
   end
 end
