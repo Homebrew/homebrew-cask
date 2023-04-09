c0852f7fff01f6106a655435db1be8be0f37494b
Patrick Linnane <patrick@linnane.io>
2023-04-09 14:52:25 -0700

    atlauncher 3.4.28.0 (#144760)
    \n

--- Casks/atlauncher.rb
+++ Casks/atlauncher.rb
@@ -1,6 +1,6 @@
 cask "atlauncher" do
-  version "3.4.27.0"
-  sha256 "8c8024ef613ad3e5de4769bb1220fd7c1e4a859156a2d0b7a28777f5f0d0478b"
+  version "3.4.28.0"
+  sha256 "ef8711386e40652ba30c2190d15874789940330b419d4b53e583dcaf23d8750e"
 
   url "https://github.com/ATLauncher/ATLauncher/releases/download/v#{version}/ATLauncher-#{version}.zip",
       verified: "github.com/ATLauncher/ATLauncher/"
