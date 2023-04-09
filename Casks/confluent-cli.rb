f5ba8301d522f3f828dd193340e5343decf8d122
Patrick Linnane <patrick@linnane.io>
2023-04-09 15:11:28 -0700

    confluent-cli 3.9.0 (#144763)

--- Casks/confluent-cli.rb
+++ Casks/confluent-cli.rb
@@ -1,9 +1,9 @@
 cask "confluent-cli" do
   arch arm: "arm64", intel: "amd64"
 
-  version "3.8.0"
-  sha256 arm:   "a25c132cffc0942a2031adb165ecd43f0ca34af7458f3d0bb56022fa7aa04b1c",
-         intel: "412ebff606e9ef447ee2bce044ff67d82238595f53ca5289c99a04e17888497d"
+  version "3.9.0"
+  sha256 arm:   "634190edb841d6bb3c60d5d560410e5af0aea77ec66ff02b9d60ca68b9ed8934",
+         intel: "e8a1454c799463fcef5cc790a14b78a3264f283dda0cce70b83674f04df4cdfb"
 
   url "https://s3-us-west-2.amazonaws.com/confluent.cloud/confluent-cli/archives/#{version}/confluent_#{version}_darwin_#{arch}.tar.gz",
       verified: "s3-us-west-2.amazonaws.com/confluent.cloud/confluent-cli/archives/"
