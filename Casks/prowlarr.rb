af4ee7d17a753a3d8021f9cf2de5af8749a411d9
Patrick Linnane <patrick@linnane.io>
2023-04-09 14:35:55 -0700

    prowlarr 1.3.2.3006

--- Casks/prowlarr.rb
+++ Casks/prowlarr.rb
@@ -1,9 +1,9 @@
 cask "prowlarr" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.2.2.2699"
-  sha256 arm:   "8e5537913785e90c087d3a383ca38867bdf4358e3a20561aad8792babe6b12f0",
-         intel: "b0156fb969c24f4c846eab65e16795955215a7608f5129283c111d43312211c3"
+  version "1.3.2.3006"
+  sha256 arm:   "2a7b184b2e8937d37a5bd8f46b0a50ff72e1e4610ea20d7e7c0e80cb8de2f9a7",
+         intel: "643cadcdedc370f849dcb3750c7dd64ec10eb19f601e841c57b24b16d933d84d"
 
   url "https://github.com/Prowlarr/Prowlarr/releases/download/v#{version}/Prowlarr.master.#{version}.osx-app-core-#{arch}.zip",
       verified: "github.com/Prowlarr/Prowlarr/"
