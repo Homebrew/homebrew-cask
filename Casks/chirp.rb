54fd628b89492b92c0e4f1dd55170fd7e706515f
Patrick Linnane <patrick@linnane.io>
2023-04-09 15:10:24 -0700

    chirp 20230409 (#144762)

--- Casks/chirp.rb
+++ Casks/chirp.rb
@@ -1,6 +1,6 @@
 cask "chirp" do
-  version "20230404"
-  sha256 "031f7c6e4ad8977d1b63c8a04ea7d14d424088f1c8a83547211fbede4d1200d7"
+  version "20230409"
+  sha256 "5ee0b504fe27477aa82d8e5487716dfb0a53eb796c206c98975dc1e57cec4c1c"
 
   url "https://trac.chirp.danplanet.com/chirp_next/next-#{version}/chirp-next-#{version}.app.zip"
   name "CHIRP"
