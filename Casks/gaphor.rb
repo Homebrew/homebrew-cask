d78ac284f8b11301b2db1e6d0554317688b9bd8f
Patrick Linnane <patrick@linnane.io>
2023-04-09 15:13:42 -0700

    gaphor 2.18.0 (#144772)

--- Casks/gaphor.rb
+++ Casks/gaphor.rb
@@ -1,6 +1,6 @@
 cask "gaphor" do
-  version "2.17.0"
-  sha256 "190dc8cf931fdf3f2c990536ce1f98b8f001bb157a3f208ec24455e5ba3d9d3f"
+  version "2.18.0"
+  sha256 "422a981eb816aff4bf1dc6ebd7b9f25ba87fb71df7ecc8e5d8e89da6e7137124"
 
   url "https://github.com/gaphor/gaphor/releases/download/#{version}/Gaphor-#{version}.dmg",
       verified: "github.com/gaphor/gaphor/"
