f11f24b75149827816ce6bdb888f192417cb9096
Patrick Linnane <patrick@linnane.io>
2023-04-09 14:01:11 -0700

    anydo 5.0.28

--- Casks/anydo.rb
+++ Casks/anydo.rb
@@ -1,6 +1,6 @@
 cask "anydo" do
-  version "5.0.27"
-  sha256 "1bb0263a0e18b09a3a8081ceddb2bee2f402d0971e4e65b06f7e35982b0e119a"
+  version "5.0.28"
+  sha256 "f9e232125945ffc86ce75e93daa1dd21a54beddd15eab45340b021e0924d9723"
 
   url "https://electron-app.any.do/Anydo-#{version}-universal.dmg"
   name "Any.do"
