d7271593dc190cac17b274825e4f69d893ea84d1
Bevan Kay <email@bevankay.me>
2023-04-10 22:38:47 +1000

    filebot 5.0.2

--- Casks/filebot.rb
+++ Casks/filebot.rb
@@ -1,9 +1,9 @@
 cask "filebot" do
   arch arm: "arm64", intel: "x64"
 
-  version "5.0.1"
-  sha256 arm:   "99904222cebdef0fbdb1e4e304fccae19d7d3440f65def3daaa5a035de8e5c70",
-         intel: "4fc4ee2dd263a911c9ecf52ffcca76e8cd320b7cef4cee4084938cae1dfe8b9f"
+  version "5.0.2"
+  sha256 arm:   "4d1be1956f212437fca88452dfb128e49cb8cf22f4bb77b89c4d75fdd9e644c8",
+         intel: "45335cbb88ccaba675ba5666d3011382c9696e34e59b8c5e906265925c80663a"
 
   url "https://get.filebot.net/filebot/FileBot_#{version}/FileBot_#{version}_#{arch}.app.tar.xz"
   name "FileBot"
