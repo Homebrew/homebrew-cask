00aca8914ff2fa6992b863f85802b28c5e8ad974
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-04-10 19:54:34 +0800

    proxyman 4.6.0

--- Casks/proxyman.rb
+++ Casks/proxyman.rb
@@ -1,6 +1,6 @@
 cask "proxyman" do
-  version "4.5.0,45000"
-  sha256 "6ac686a97863ff60114a64ffac4389abe0f91b3a71c7f17b4f43180202ae42ed"
+  version "4.6.0,46000"
+  sha256 "d9fb2b51ed5eebb701dabca0ac42bdac077191d7f31082dc483a3c904b629c16"
 
   url "https://download.proxyman.io/#{version.csv.second}/Proxyman_#{version.csv.first}.dmg"
   name "Proxyman"
