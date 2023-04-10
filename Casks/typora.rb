a0aacd71e5b42383c1583a1b7f35ce45eb4d1389
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-10 19:56:37 -0400

    typora 1.5.12 (#144857)
    \n

--- Casks/typora.rb
+++ Casks/typora.rb
@@ -1,6 +1,6 @@
 cask "typora" do
-  version "1.5.11"
-  sha256 "9eeb8e934853598c390b7972eb99d904d0ba8d1b3d8058bb847dbb1d47df1c66"
+  version "1.5.12"
+  sha256 "126eced04988e30227fbbbba424ec8c7002df7a8f03db277e0e4bf19c81e7e38"
 
   url "https://download.typora.io/mac/Typora-#{version}.dmg"
   name "Typora"
