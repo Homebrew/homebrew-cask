91cac2e15c0efa47f37b77503e00c2b03b617ad7
Nihaal Sangha <nihaal.git@gmail.com>
2023-04-10 12:08:49 +0100

    Update replacicon to 1.5 (#144808)

--- Casks/replacicon.rb
+++ Casks/replacicon.rb
@@ -1,6 +1,6 @@
 cask "replacicon" do
-  version "1.4,22"
-  sha256 "5fee470ab27499c9c9581d1ca97c5a85e24a5f132ef7073c17f3c37ae571dabd"
+  version "1.5,23"
+  sha256 "ab2dc6215cd80e557e92a6489d442eafda317ee3e0b087c8dd38c1a5b27e0b0e"
 
   url "https://replacicon.app/builds/Replacicon_#{version.csv.second}.zip"
   name "Replacicon"
@@ -11,6 +11,8 @@ cask "replacicon" do
     url "https://replacicon.app/updates.xml"
     strategy :sparkle
   end
+
+  auto_updates true
 
   app "Replacicon.app"
 
