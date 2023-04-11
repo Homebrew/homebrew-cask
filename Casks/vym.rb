3102470961c59a07c7012ed19e4f670e02939abd
Sam Ford <1584702+samford@users.noreply.github.com>
2023-04-11 13:58:45 -0400

    vym 2.9.0 (#144890)

--- Casks/vym.rb
+++ Casks/vym.rb
@@ -1,16 +1,15 @@
 cask "vym" do
-  version "2.8.8"
-  sha256 "17587f3ad5550795f6224d09dd1bff659cb525ba77f6482b2d35138a16da1e80"
+  version "2.9.0"
+  sha256 "2243e0d2c89dc9eef53ffac8d4da084650247f025011a522aeba0c19e7857be6"
 
-  url "https://downloads.sourceforge.net/vym/osx11-vym-#{version}-by%20shemeshg.zip"
+  url "https://downloads.sourceforge.net/vym/vym-#{version}.dmg"
   name "VYM (View Your Mind)"
   desc "Generate and manipulate maps which show your thoughts"
   homepage "https://sourceforge.net/projects/vym/"
 
   livecheck do
-    url "https://sourceforge.net/projects/vym/rss"
-    regex(/.*?osx11[._-]vym[._-]v?(\d+(?:\.\d+)+).*?\.zip/i)
-    strategy :page_match
+    url :url
+    regex(%r{url=.*?/vym[._-]v?(\d+(?:\.\d+)+)\.(?:dmg|pkg)}i)
   end
 
   app "vym.app"
