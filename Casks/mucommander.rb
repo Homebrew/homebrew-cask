ac47b069e1883039fadf4004f81fab84e4acda28
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-12 14:54:41 -0400

    Update version and download url

--- Casks/mucommander.rb
+++ Casks/mucommander.rb
@@ -1,18 +1,12 @@
 cask "mucommander" do
-  version "1.2.0-1"
+  version "1.2.0-2"
   sha256 "d5e386d62b01ed7614b15a3fb807b21f8c3e650ddbd2b5b8d08c6949ffcd779d"
 
-  url "https://github.com/mucommander/mucommander/releases/download/#{version}/muCommander-1.2.0-2.dmg",
+  url "https://github.com/mucommander/mucommander/releases/download/#{version}/muCommander-#{version}.dmg",
       verified: "github.com/mucommander/mucommander/"
   name "muCommander"
   desc "File manager with a dual-pane interface"
   homepage "https://www.mucommander.com/"
-
-  livecheck do
-    url :url
-    regex(%r{href=.*?/tag/v?(\d+(?:[.-]\d+)+)["' >]}i)
-    strategy :github_latest
-  end
 
   app "muCommander.app"
 
