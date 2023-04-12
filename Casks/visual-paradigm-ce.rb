6a414ff852aba5af2fef775f1c4883e15a9cb4c8
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-12 05:34:58 -0400

    visual-paradigm-ce 17.0,20230401 (zap) (#144932)

--- Casks/visual-paradigm-ce.rb
+++ Casks/visual-paradigm-ce.rb
@@ -22,4 +22,10 @@ cask "visual-paradigm-ce" do
 
   # Renamed to avoid conflict with visual-paradigm.
   app "Visual Paradigm.app", target: "Visual Paradigm CE.app"
+
+  zap trash: [
+    "~/Library/Application Support/Visual Paradigm",
+    "~/Library/Application Support/VisualParadigm",
+    "~/Library/Saved Application State/com.install4j.1106-5897-7327-6550.5.savedState",
+  ]
 end
