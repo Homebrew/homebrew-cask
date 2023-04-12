b8acf0996d7e8a146a6a375d6e0846b8a30a8239
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-04-12 08:12:30 +0800

    reflect 2.0.7

--- Casks/reflect.rb
+++ Casks/reflect.rb
@@ -1,9 +1,9 @@
 cask "reflect" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.0.6"
-  sha256 arm:   "b473b033cf810c659533f8f9fa70435b22922eaaca20dc00a8a5053606dee763",
-         intel: "ed2292fc93ff16a195309765b5da4f116b506a6cc0d4056050891d9159e46440"
+  version "2.0.7"
+  sha256 arm:   "83e97aab7f48c12f54cc5861e449dc6b04c5715d36d79dfb60fe80a59c1b3f69",
+         intel: "801cbaf058aa7f9921f595f865fa697bb2ff1ebcdad122a5dbe7dec4cf8c08b3"
 
   url "https://github.com/team-reflect/reflect-electron-updates/releases/download/v#{version}/Reflect-darwin-#{arch}-#{version}.zip",
       verified: "github.com/team-reflect/reflect-electron-updates/"
