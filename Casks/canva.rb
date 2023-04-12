1514589ca5bc3d36601352ec1fa513632bce6dd8
Bevan Kay <email@bevankay.me>
2023-04-12 15:00:29 +1000

    canva 1.63.0 (#144935)
    \n

--- Casks/canva.rb
+++ Casks/canva.rb
@@ -1,9 +1,9 @@
 cask "canva" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.62.0"
-  sha256 arm:   "7494aadd9a1e748dbcf078fc83a8bf103d2122f43d7127a6dfa47fa68b260be7",
-         intel: "fb37bd899a66496bf2b5b500845df6b837a9a29a22d5360a49fde3df73d3f4c5"
+  version "1.63.0"
+  sha256 arm:   "03958f8a9a288c00faea7e08d9c7e30fbf8a5c1d50dafdba9a627c81bbdcdc57",
+         intel: "225e539c6d800738c5b094025cfa281a1466d1ec8404a0700ced6a8bbcd4dc36"
 
   url "https://desktop-release.canva-deploy.com/Canva-#{version}-#{arch}.dmg",
       verified: "desktop-release.canva-deploy.com/"
