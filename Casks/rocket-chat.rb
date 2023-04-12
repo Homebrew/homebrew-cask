096f88d250c17feb42b2fee7eefc63eb7b4ee710
Damien Leroy <damien.leroy@synoptik-labs.com>
2023-04-12 14:05:37 +0200

    rocket-chat 3.9.2

--- Casks/rocket-chat.rb
+++ Casks/rocket-chat.rb
@@ -1,6 +1,6 @@
 cask "rocket-chat" do
-  version "3.9.1"
-  sha256 "91e9864e57020f665172b5de2841ef76adf9cca8598f2791cec15a7fd15041bb"
+  version "3.9.2"
+  sha256 "16f299db0cf2fd6e556277614438dc787966a311a47ffba9906cf066d0edb768"
 
   url "https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/#{version}/rocketchat-#{version}-mac.dmg",
       verified: "github.com/RocketChat/Rocket.Chat.Electron/"
