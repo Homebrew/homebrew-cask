497f348b9d0e15a233a0aeb71ccd39981e680d94
DattatreyaReddy Panta <58727124+DattatreyaReddy@users.noreply.github.com>
2023-04-13 03:11:32 +0530

    Add Tachidesk Sorayomi v0.5.1 (#144958)
    Co-authored-by: Razvan Azamfirei <azamfireirazvan@gmail.com>

---
+++ Casks/tachidesk-sorayomi.rb
@@ -0,0 +1,18 @@
+cask "tachidesk-sorayomi" do
+  version "0.5.1"
+  sha256 "09dcaddd040b41ac5321d19b1099769511d2497c4553be283826efcbc87ee858"
+
+  url "https://github.com/Suwayomi/Tachidesk-Sorayomi/releases/download/#{version}/tachidesk-sorayomi-#{version}-macos-x64.zip"
+  name "Tachidesk Sorayomi"
+  desc "Manga reader"
+  homepage "https://github.com/Suwayomi/Tachidesk-Sorayomi/"
+
+  depends_on macos: ">= :mojave"
+
+  app "Tachidesk Sorayomi.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.suwayomi.tachideskSorayomi",
+    "~/Library/Containers/com.suwayomi.tachideskSorayomi",
+  ]
+end
