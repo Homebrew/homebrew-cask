5ed0d245fb690e78d04d312cede3b8dd355d6ab4
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-04-13 05:18:33 +0800

    vscodium 1.77.3.23102

--- Casks/vscodium.rb
+++ Casks/vscodium.rb
@@ -1,9 +1,9 @@
 cask "vscodium" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.77.2.23101"
-  sha256 arm:   "b8ee632556b3d5b30201ebffc213dd71c53df80759f7950e506270c3c45821a3",
-         intel: "2bcd6efb6de0626a53cf21f1b276782490cf8057c818007b23e10978df1ced97"
+  version "1.77.3.23102"
+  sha256 arm:   "326f91340f0505a698636719d5b009508bf4e17f8721e693c046c2c848ca5c13",
+         intel: "34a7cd4f310beba4b112730f6d6898f697dbae913a7cfe2b30a39ea870a8c828"
 
   url "https://github.com/VSCodium/vscodium/releases/download/#{version}/VSCodium.#{arch}.#{version}.dmg"
   name "VSCodium"
