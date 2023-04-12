1ef15817588eef1874c1f8ced8bded4ec432c3c7
Issy Long <me@issyl0.co.uk>
2023-04-12 17:55:05 +0100

    flrig: Autofix `Cask/StanzaGrouping` offenses inside `on_*` blocks

--- Casks/flrig.rb
+++ Casks/flrig.rb
@@ -3,10 +3,12 @@ cask "flrig" do
 
   on_sierra :or_older do
     sha256 "03cfdcefe90784f5f6fc643817a22d9590d7508ba03cd5cde0c5168723921f30"
+
     url "https://downloads.sourceforge.net/fldigi/fldigi/flrig-#{version}_LI.dmg"
   end
   on_high_sierra :or_newer do
     sha256 "6ee34eeb589ac00a82d74ac2146fdfbf2295eb24f0b1c9bff96ed1630e086c73"
+
     url "https://downloads.sourceforge.net/fldigi/fldigi/flrig-#{version}_U.dmg"
   end
 
