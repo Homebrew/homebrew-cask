2e53058caecc5399bc74f1567fde4061ac2d0320
Laine Taffin Altman <alexanderaltman@me.com>
2023-04-13 12:36:36 -0700

    farrago 2.0.0 (#145064)
    Co-authored-by: Razvan Azamfirei <azamfireirazvan@gmail.com>

--- Casks/farrago.rb
+++ Casks/farrago.rb
@@ -1,5 +1,5 @@
 cask "farrago" do
-  version "1.6.8"
+  version "2.0.0"
   sha256 :no_check
 
   url "https://rogueamoeba.com/farrago/download/Farrago.zip"
@@ -16,4 +16,13 @@ cask "farrago" do
   depends_on macos: ">= :catalina"
 
   app "Farrago.app"
+
+  zap trash: [
+    "~/Library/Application Support/Farrago #{version.major}",
+    "~/Library/Caches/com.rogueamoeba.farrago",
+    "~/Library/HTTPStorages/com.rogueamoeba.farrago",
+    "~/Library/Preferences/com.rogueamoeba.farrago.plist",
+    "~/Library/Saved Application State/com.rogueamoeba.farrago.savedState",
+    "~/Library/WebKit/com.rogueamoeba.farrago",
+  ]
 end
