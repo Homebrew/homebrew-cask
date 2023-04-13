def44ad06aec4afb017113e3409319f457236567
Amory Meltzer ☀ <Amorymeltzer@gmail.com>
2023-04-13 11:07:20 -0400

    bunch 1.4.12 (#145047)

--- Casks/bunch.rb
+++ Casks/bunch.rb
@@ -1,6 +1,6 @@
 cask "bunch" do
-  version "1.4.11,166"
-  sha256 "240fe4dba00af30b7b875acfcf61130eddad92dd813f05c1766f95f8c2b5a22a"
+  version "1.4.12,172"
+  sha256 "9adedbfa94eab494e6851409a2c6265a36038ecbd15b1e57cfc095941b924a87"
 
   url "https://brettterpstra.com/updates/bunch/Bunch#{version.csv.first}#{version.csv.second}.dmg",
       verified: "brettterpstra.com/updates/bunch/"
