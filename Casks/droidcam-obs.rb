b48f625693b8ccb71cd0e122f494cade3805be7f
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-13 16:22:31 -0400

    migrate droidcam-obs

---
+++ Casks/droidcam-obs.rb
@@ -0,0 +1,18 @@
+cask "droidcam-obs" do
+  version "2.0.1"
+  sha256 "945d04f10a1d5ef5aaeb144904c36c726bed73ad3d4c37e0e3d3fa431d5bc0c0"
+
+  url "https://github.com/dev47apps/droidcam-obs-plugin/releases/download/#{version}/DroidCamOBS_#{version}_macos.pkg",
+      verified: "github.com/dev47apps/droidcam-obs-plugin/"
+  name "DroidCam OBS"
+  desc "Use your phone as a camera directly in OBS Studio"
+  homepage "https://www.dev47apps.com/obs/"
+
+  depends_on cask: "obs"
+
+  pkg "DroidCamOBS_#{version}_macos.pkg"
+
+  uninstall pkgutil: "com.dev47apps.droidcamobs",
+            delete:  "/Library/Application Support/obs-studio/plugins/droidcam-obs",
+            rmdir:   "/Library/Application Support/obs-studio/plugins"
+end
