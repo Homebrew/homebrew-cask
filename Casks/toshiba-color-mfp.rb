1c2ac3f0e69d53ade017bd39766e4457b2cff458
Bevan Kay <email@bevankay.me>
2023-04-13 10:20:20 +1000

    cask-drivers: migrate casks to homebrew/cask

---
+++ Casks/toshiba-color-mfp.rb
@@ -0,0 +1,27 @@
+cask "toshiba-color-mfp" do
+  version "7.115.0.0,19993"
+  sha256 "126a0d1b4987aab22833f719c79acc09123c7ed5238bf79beb2aea691d139c5e"
+
+  url "https://business.toshiba.com/downloads/KB/f1Ulds/#{version.csv.second}/TOSHIBA_ColorMFP.dmg.gz"
+  name "Toshiba ColorMFP Drivers"
+  desc "Drivers for Toshiba ColorMFP devices"
+  homepage "https://business.toshiba.com/support"
+
+  livecheck do
+    url "http://business.toshiba.com/support/downloads/GetDownloads.jsp?model=5015"
+    strategy :page_match do |page|
+      match = page.match(/"MacDC",.*?"id":"(\d+)",.*?"versionName":"(\d+(?:\.\d+)+)",/)
+      next if match.blank?
+
+      "#{match[2]},#{match[1]}"
+    end
+  end
+
+  pkg "TOSHIBA ColorMFP.pkg"
+
+  uninstall pkgutil: "com.toshiba.pde.x7.colormfp",
+            delete:  [
+              "/Library/Printers/PPDs/Contents/Resources/TOSHIBA_ColorMFP*.gz",
+              "/Library/Printers/toshiba",
+            ]
+end
