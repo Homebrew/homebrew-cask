ed7acae075d2b80c7e5f7c0a9fac49a5af92073c
Bevan Kay <email@bevankay.me>
2023-04-13 12:16:05 +1000

    cask-drivers: migrate casks to homebrew/cask (#145008)

---
+++ Casks/elgato-wave-link.rb
@@ -0,0 +1,27 @@
+cask "elgato-wave-link" do
+  version "1.6.1.3278"
+  sha256 "d4814196c8efaee886bf3f484d6b0e1d1a362533534c04a2c404b3e1276ebeb0"
+
+  url "https://edge.elgato.com/egc/macos/wavelink/#{version.major_minor_patch}/WaveLink_#{version}.pkg"
+  name "Elgato Wave Link"
+  desc "Software custom-built for content creation"
+  homepage "https://www.elgato.com/en/wave-1"
+
+  livecheck do
+    url "https://www.elgato.com/sites/default/files/downloads.json"
+    regex(%r{"downloadURL"\s*:\s*"[^"]*?/WaveLink[._-]v?(\d+(?:[._]\d+)+)\.pkg"}i)
+  end
+
+  depends_on macos: ">= :catalina"
+
+  pkg "WaveLink_#{version}.pkg"
+
+  uninstall launchctl: "com.elgato.WaveLink",
+            quit:      "com.elgato.WaveLink",
+            pkgutil:   "com.elgato.pkg.WaveLink"
+
+  zap trash: [
+    "~/Library/Logs/WaveLink",
+    "~/Library/Preferences/com.elgato.WaveLink.plist",
+  ]
+end
