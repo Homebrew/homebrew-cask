fcb92aed7980b2a2bc96929ca497237b107e4ce5
Amory Meltzer <Amorymeltzer@gmail.com>
2023-04-13 12:03:02 -0400

    logseq 0.9.2

--- Casks/logseq.rb
+++ Casks/logseq.rb
@@ -1,9 +1,9 @@
 cask "logseq" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.9.1"
-  sha256 arm:   "80082517aa3d0e537c1d372041875377d98875cde3150bef8985c981aa71639f",
-         intel: "d030dd93ce4233d635898b025e1e08f770f25c3581ce2e8b618d064e2b09a264"
+  version "0.9.2"
+  sha256 arm:   "a28a7d34eb068667cc77c2fc789de153537775c0d58a1b7bac9bd72b85b072d6",
+         intel: "27fd77b60c81c1958fb48e448ab95badb86c97c03cc449437eaffbd2d7f76f4d"
 
   url "https://github.com/logseq/logseq/releases/download/#{version}/logseq-darwin-#{arch}-#{version}.dmg"
   name "Logseq"
