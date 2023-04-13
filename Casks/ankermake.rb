4b3ed41b606a8e232429bbddc0c2db5b7580c5f8
Koen van Zuijlen <8818390+kvanzuijlen@users.noreply.github.com>
2023-04-13 02:26:58 +0200

    Update and rename ankerslicer.rb to ankermake.rb

--- Casks/ankermake.rb
+++ Casks/ankermake.rb
@@ -1,12 +1,12 @@
-cask "ankerslicer" do
+cask "ankermake" do
   arch arm: "M1", intel: "Intel"
 
-  version "2.0.0"
+  version "0.9.22_3"
   sha256 :no_check
 
   url "https://public-make-moat-us.s3.us-east-2.amazonaws.com/ankermake/slicer/AnkerMake_Installer_Mac_#{arch}.dmg",
       verified: "public-make-moat-us.s3.us-east-2.amazonaws.com/ankermake/slicer/"
-  name "ankerslicer"
+  name "AnkerMake"
   desc "Slicer for AnkerMake 3D printers"
   homepage "https://www.ankermake.com/software"
 
@@ -24,7 +24,6 @@ cask "ankerslicer" do
     "~/Library/Application Support/AnkerSlicer",
     "~/Library/Caches/AnkerMake",
     "~/Library/Caches/AnkerSlicer",
-    "~/Library/Preferences/com.anker.pcankerwork.plist",
     "~/Library/Preferences/com.ankermake.AnkerMake_64bit_fp.plist",
     "~/Library/Preferences/com.ankerslicer.AnkerMake_64bit_fp.plist",
   ]
