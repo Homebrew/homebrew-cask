3de51ac219d44ed479865d3c9989efa84347d227
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-13 15:28:09 -0400

    add zap

--- Casks/farrago.rb
+++ Casks/farrago.rb
@@ -16,4 +16,13 @@ cask "farrago" do
   depends_on macos: ">= :catalina"
 
   app "Farrago.app"
+
+  zap trash: [
+    "~/Library/Application Support/Farrago #{version.major}",
+    "~/Library/Caches/com.rogueamoeba.farrago",
+    "~/Library/HTTPStorages/com.rogueamoeba.farrago",
+    "~/Library/Preferences/com.rogueamoeba.farrago.plist",
+    "~/Library/Saved Application State/com.rogueamoeba.farrago.savedState",
+    "~/Library/WebKit/com.rogueamoeba.farrago",
+  ]
 end
