e39e2c4fe08a373b41f41fdd6fce20dba5da6d9f
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-04-14 03:23:02 +0800

    logitech-g-hub 2023.3.394992

--- Casks/logitech-g-hub.rb
+++ Casks/logitech-g-hub.rb
@@ -1,5 +1,5 @@
 cask "logitech-g-hub" do
-  version "2023.2.373424"
+  version "2023.3.394992"
   sha256 :no_check
 
   url "https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.zip",
