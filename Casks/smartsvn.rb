87afde357841e9f98313dea33b2f39dbb134b8fd
Loïc PÉRON <44115467+nicerloop@users.noreply.github.com>
2023-04-13 13:12:01 +0200

    smartsvn 14.3.0 (#145019)
    Co-authored-by: Razvan Azamfirei <azamfireirazvan@gmail.com>

--- Casks/smartsvn.rb
+++ Casks/smartsvn.rb
@@ -1,9 +1,9 @@
 cask "smartsvn" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "14.2.2"
-  sha256 arm:   "7eae47423b65df4a3dbefde3f34175bd123340d537f3694b58e4026fcadf03e7",
-         intel: "43d8ec95ce3e8b4a40a08a0ceff76c9d06f282ab3d211e2e6e37a7bb61ec8a3a"
+  version "14.3.0"
+  sha256 arm:   "d4826dfb5ba1546a864460cd14d5a70c7172757a1fb7f824d859391ae41f744d",
+         intel: "df26df963925eca412b9699b333825d57b6b9f005bdb4a5ec3ade1d91e084acd"
 
   url "https://www.smartsvn.com/downloads/smartsvn/smartsvn-#{arch}-#{version.dots_to_underscores}.dmg"
   name "SmartSVN"
@@ -11,8 +11,11 @@ cask "smartsvn" do
   homepage "https://www.smartsvn.com/"
 
   livecheck do
-    url "https://www.smartsvn.com/documents/smartsvn/changelog.txt"
-    regex(/SmartSVN\s+v?(\d+(?:\.\d+)+)/i)
+    url "https://www.smartsvn.com/download/"
+    strategy :page_match do |page|
+      page.scan(/smartsvn[._-]#{arch}[._-](\d+(?:_\d+)+).dmg/i)
+          .map { |match| match&.first&.tr("_", ".") }
+    end
   end
 
   depends_on macos: ">= :high_sierra"
