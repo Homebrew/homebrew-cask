d040ea64515a914187455d54f97006c1d195149f
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-04-13 22:26:39 +0800

    usmart-trade 3.20.2 (#145029)
    Co-authored-by: Razvan Azamfirei <azamfireirazvan@gmail.com>

--- Casks/usmart-trade.rb
+++ Casks/usmart-trade.rb
@@ -1,11 +1,11 @@
 cask "usmart-trade" do
-  version "3.20.1,aec612d8-44f2-465b-a8ca-9618eca890c5,2023-04-03"
-  sha256 "ca7334bd9a68ee17e0b1cbcf5cd3bde8dd1adbeaccd8ae65933f0aca2a8b1416"
+  version "3.20.2,42922e52-94e2-45f0-baa0-c6a993692a11,2023-04-13"
+  sha256 "bbd9515f44fd997117ed1cafd869b6d5a71aa9295c35d3884f94dbd19d0237c1"
 
   url "https://jy-common-prd-1257884527.cos.ap-guangzhou.myqcloud.com/admin/app-version-file/#{version.csv.third}/#{version.csv.second}/uSMART%20Trade-#{version.csv.first}.dmg",
       verified: "jy-common-prd-1257884527.cos.ap-guangzhou.myqcloud.com/"
   name "uSMART Trade"
-  desc "Trade Stocks & Options"
+  desc "Stock and options trading platform"
   homepage "https://www.usmartglobal.com/"
 
   livecheck do
