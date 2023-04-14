a330b70a793e4458055f1c6415c9b702d0b6c81c
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-13 22:42:28 -0400

    softube-central 1.9.0 (#145028)
    \n

--- Casks/softube-central.rb
+++ Casks/softube-central.rb
@@ -1,8 +1,8 @@
 cask "softube-central" do
-  version "1.6.1"
-  sha256 "dc908a2036294ec1c2a126a9f5f19b4ff807fbdf41c97a30032111247505d422"
+  version "1.9.0"
+  sha256 "ae3c05350332c638ae566216624f672ab86ad366e88334cba079890e920d8c3f"
 
-  url "https://softubestorage.b-cdn.net/softubecentral/Softube%20Central-#{version}.pkg",
+  url "https://softubestorage.b-cdn.net/softubecentral/Softube%20Central-#{version}-universal.pkg",
       verified: "softubestorage.b-cdn.net/"
   name "Softube Central"
   desc "Installer for installation and license activation of Softube products"
@@ -15,15 +15,22 @@ cask "softube-central" do
   auto_updates true
   depends_on cask: "ilok-license-manager"
 
-  pkg "Softube Central-#{version}.pkg"
+  pkg "Softube Central-#{version}-universal.pkg"
 
-  uninstall quit:    [
-              "org.softube.com.softubecentral",
+  uninstall quit:      [
               "com.softube.Console1OSD_Release",
+              "org.softube.com.softubecentral",
             ],
-            pkgutil: [
+            pkgutil:   [
+              "com.paceap.pkg.eden.activationexperience",
+              "com.paceap.pkg.eden.iLokLicenseManager",
+              "com.paceap.pkg.eden.licensed",
               "com.softube.installerdaemon.helper",
               "org.softube.com.softubecentral",
+            ],
+            launchctl: [
+              "com.paceap.eden.licensed",
+              "com.paceap.eden.licensed.agent",
             ]
 
   zap trash: [
