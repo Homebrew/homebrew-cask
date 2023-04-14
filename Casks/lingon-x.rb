411a96abcb70f0d81750787733c3318bc76674fb
Steve Huff <shuff@vecna.org>
2023-04-14 18:08:45 -0400

    lingon-x 9.2.1 (#145131)

--- Casks/lingon-x.rb
+++ Casks/lingon-x.rb
@@ -16,8 +16,8 @@ cask "lingon-x" do
     sha256 "c1c839e8dc13bd295f2080980c5bea22299c33f3333b7c6981161b46d6f021d8"
   end
   on_monterey :or_newer do
-    version "9.2"
-    sha256 "269f285c10d67d0b32502cf4e649bbed97006366246151c1ffba146e85679667"
+    version "9.2.1"
+    sha256 "9eef547962f3f2f35004cede130ffe619e11e76fb93c946551738ab0cd5b990e"
   end
 
   url "https://www.peterborgapps.com/downloads/LingonX#{version.major}.zip"
