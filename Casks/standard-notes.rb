ec4a7029ae70f5cfad1e289f8ce2c46b1f08eff4
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-13 21:54:55 -0400

    standard-notes 3.150.52

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -1,9 +1,9 @@
 cask "standard-notes" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.150.45"
-  sha256 arm:   "ea899b979179baa016e9701d79611d84daa3822c66836d45ba41c268fb66374b",
-         intel: "18949b0656f0567dd7b6109d7e0cc65bd66719b0f5a1439ec06f737e6dd2f8ec"
+  version "3.150.52"
+  sha256 arm:   "7393e4af72970271b3a17096511830afb81984046241cc3f1b6c2db0133c9342",
+         intel: "5d95b84fd38f84cb7f26b391cc2bf58b2d84dd4314f92382c5e148ac3a4f6615"
 
   url "https://github.com/standardnotes/app/releases/download/%40standardnotes%2Fdesktop%40#{version}/standard-notes-#{version}-mac-#{arch}.zip",
       verified: "github.com/standardnotes/app/"
@@ -12,8 +12,9 @@ cask "standard-notes" do
   homepage "https://standardnotes.org/"
 
   livecheck do
-    url "https://github.com/standardnotes/app/releases/latest"
-    strategy :header_match
+    url "https://github.com/standardnotes/app/releases"
+    strategy :page_match
+    regex(/Desktop\s+(\d+(?:\.\d+)+)/i)
   end
 
   auto_updates true
