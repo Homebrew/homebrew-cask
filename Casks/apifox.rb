09d6f08e5371e553257503294f95b12fc7e334a3
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-04-14 21:41:25 +0800

    apifox 2.2.32 (#145115)

--- Casks/apifox.rb
+++ Casks/apifox.rb
@@ -1,9 +1,9 @@
 cask "apifox" do
   arch arm: "-macOS-arm64"
 
-  version "2.2.31"
-  sha256 arm:   "c32abe49d5ba031747d54eeee71f018d829d8bc84668095bf640f01dc29d7abc",
-         intel: "489f68d0ec61c73e211c005fdcacefaba608d543032d2c3fdc389fbe63251608"
+  version "2.2.32"
+  sha256 arm:   "dc0efd9bf72bda17ba840f020cbc7188a1d890d2040c1510291cdc1220dc589b",
+         intel: "b830aad58188bb361562c25be3ed10370f712a8aaeba1352f3eb948173450286"
 
   url "https://cdn.apifox.cn/download/#{version}/Apifox#{arch}-#{version}.dmg"
   name "Apifox"
