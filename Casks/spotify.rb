2954fea88f4c91b35f15af19c4536bae58f9e40a
tiiiecherle <tiiiecherle@gmail.com>
2023-04-14 13:43:59 +0200

    spotify 1.2.9.743 (#145105)
    Co-authored-by: Razvan Azamfirei <azamfireirazvan@gmail.com>

--- Casks/spotify.rb
+++ Casks/spotify.rb
@@ -4,10 +4,10 @@ cask "spotify" do
   sha256 :no_check
 
   on_arm do
-    version "1.2.8.923,4f94bf0d,1547"
+    version "1.2.9.743,85d9593d,2480"
   end
   on_intel do
-    version "1.2.8.923,4f94bf0d,1545"
+    version "1.2.9.743,85d9593d,2477"
   end
 
   url "https://download.scdn.co/Spotify#{arch}.dmg",
