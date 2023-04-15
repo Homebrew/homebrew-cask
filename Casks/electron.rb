1592529114452aa8f777945a3f9a32f9f419fc5a
Patrick Linnane <patrick@linnane.io>
2023-04-14 21:01:38 -0700

    electron 24.1.2

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,9 +1,9 @@
 cask "electron" do
   arch arm: "arm64", intel: "x64"
 
-  version "24.1.1"
-  sha256 arm:   "6e508ec2b744ffa65b695fede3fa4a1b219450197de572b8438473602cabdc6e",
-         intel: "4942638d63dcad945aa2e320a39547da36f696d4ba787225201648af4616c4c8"
+  version "24.1.2"
+  sha256 arm:   "3644b29ab2dde6cac0ac455fb5e965f739c4faaaad7e678bd4379c72723ea40a",
+         intel: "ffe6a7c23d360b31bbc4dc05e05eb15b4261e72f1aac7d98ca7fbbd29bbba5e7"
 
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-#{arch}.zip",
       verified: "github.com/electron/electron/"
