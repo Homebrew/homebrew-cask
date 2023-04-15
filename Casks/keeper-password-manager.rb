0e5ccc5cabf847c0982a9e885993d7589beedf09
Patrick Linnane <patrick@linnane.io>
2023-04-15 03:18:41 -0700

    keeper-password-manager 16.9.1 (#145168)
    \n

--- Casks/keeper-password-manager.rb
+++ Casks/keeper-password-manager.rb
@@ -1,5 +1,5 @@
 cask "keeper-password-manager" do
-  version "16.9.0,1680719943838"
+  version "16.9.1,1681325899845"
   sha256 :no_check
 
   url "https://keepersecurity.com/desktop_electron/Darwin/KeeperSetup.dmg"
