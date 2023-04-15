ca6e7415881db8d1b26450e28f301fd39b75da1c
Patrick Linnane <patrick@linnane.io>
2023-04-14 20:58:12 -0700

    devsquadron 0.0.12

--- Casks/devsquadron.rb
+++ Casks/devsquadron.rb
@@ -1,9 +1,9 @@
 cask "devsquadron" do
   arch arm: "arm", intel: "intel"
 
-  version "0.0.11"
-  sha256 arm:   "10d346c17b72ed71c8092f6d37d34a261c91d7e05ef2516ce1f4bf9bc16a7909",
-         intel: "992b7b13441d6db99263eed4688cc6fc7af2d318c53edacad7a1236f4e0aad1e"
+  version "0.0.12"
+  sha256 arm:   "b1289fd39ac11ccf94c708663a6854c27e8c547207f4a291d2d4eb2b3eb6bd7d",
+         intel: "1d721d51ac7e89215ec1564be5751bac693085ddba62f244a6cad490f17f39f1"
 
   url "https://developersquadron.serveftp.com/downloads/devsquadron/v#{version}/macos-#{arch}.tar.xz"
   name "devsquadron"
