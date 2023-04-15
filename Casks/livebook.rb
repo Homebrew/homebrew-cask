c5401eb3b738117730b3073cc0da2a332701f1d3
Patrick Linnane <patrick@linnane.io>
2023-04-14 21:21:32 -0700

    livebook 0.9.2

--- Casks/livebook.rb
+++ Casks/livebook.rb
@@ -1,6 +1,6 @@
 cask "livebook" do
-  version "0.9.1"
-  sha256 "e314e463fc70e5510a675ef1ba91d3e62e51506a4ef24938f155a7f6870c2153"
+  version "0.9.2"
+  sha256 "f9ecfcdf9f2a4dec4f11f7f299d3b3e1d6ab8131cb0f173ea530343aa97608e4"
 
   url "https://github.com/livebook-dev/livebook/releases/download/v#{version}/LivebookInstall-v#{version}-macos-universal.dmg",
       verified: "github.com/livebook-dev/livebook/"
