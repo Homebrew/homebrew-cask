22ca0038c2c685cf34ae8dc4bca6f48e2253ea97
Patrick Linnane <patrick@linnane.io>
2023-04-14 22:21:36 -0700

    sparrow 1.7.6

--- Casks/sparrow.rb
+++ Casks/sparrow.rb
@@ -1,9 +1,9 @@
 cask "sparrow" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.7.4"
-  sha256 arm:   "810e60c8761e25ba78449a7c41a8a3af19e1d84dfc80566f715b69a800eacec1",
-         intel: "980f7fb4fecf3f261e65c23ffe11bf66221f745a7b3ebd91f3a7934e8db2f59c"
+  version "1.7.6"
+  sha256 arm:   "50118a6bb6e21a60aef0c7d5dfea3fa1b8944d18bbd0a39d66e79a1f49856560",
+         intel: "2cc49ee72eb99074bfeff754c65885be37dd8081c027c0fd524ca3d3a7b2d353"
 
   url "https://github.com/sparrowwallet/sparrow/releases/download/#{version}/Sparrow-#{version}-#{arch}.dmg",
       verified: "github.com/sparrowwallet/sparrow/"
