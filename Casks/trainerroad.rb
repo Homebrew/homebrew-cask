3c7f870263be27f06d7fd985e81861adc2eb5404
Patrick Linnane <patrick@linnane.io>
2023-04-15 03:15:27 -0700

    trainerroad 2023.13.0.237 (#145203)
    \n

--- Casks/trainerroad.rb
+++ Casks/trainerroad.rb
@@ -1,6 +1,6 @@
 cask "trainerroad" do
-  version "2023.12.2.236"
-  sha256 "776a05ecb0e85d09e63536e16dd87770fcc07590b7cb1b67b35f678027adbe46"
+  version "2023.13.0.237"
+  sha256 "35b534c21c47d16163abd676c74de9606979d73ab3ab18348ae78ec2db6beec1"
 
   url "https://trainrdtrcmn01un1softw01.blob.core.windows.net/installers/mac/v001/Production/TrainerRoad-#{version}.dmg",
       verified: "trainrdtrcmn01un1softw01.blob.core.windows.net/"
