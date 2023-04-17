b0fb4a4d23cb60e35c51438a81d54801635f3548
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-17 17:03:10 -0400

    fix version

--- Casks/cloudflare-warp.rb
+++ Casks/cloudflare-warp.rb
@@ -1,5 +1,5 @@
 cask "cloudflare-warp" do
-  version "2023.3.415.0"
+  version "2023.3.415.0,20230417.6"
   sha256 :no_check
 
   url "https://1111-releases.cloudflareclient.com/mac/Cloudflare_WARP.zip",
