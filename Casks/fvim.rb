51a510dae782b73a9975425075a9530575375f01
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-17 07:25:53 -0400

    fvim 0.3.543

--- Casks/fvim.rb
+++ Casks/fvim.rb
@@ -1,8 +1,8 @@
 cask "fvim" do
-  version "0.3.531,g119a455"
-  sha256 "505f3f3db3cb2841bb8fa3ff19c1c2796ddf4d911118d7737bc064696d6d711b"
+  version "0.3.543,g18454bc"
+  sha256 "4e98f186d0a72b7c6b247a599231756709e282b93e0a7aced1e3d2729142fa0f"
 
-  url "https://github.com/yatli/fvim/releases/download/v#{version.csv.first}%2B#{version.csv.second}/fvim-osx-v#{version.csv.first}+#{version.csv.second}.zip"
+  url "https://github.com/yatli/fvim/releases/download/v#{version.csv.first}%2B#{version.csv.second}/fvim-osx-x64-v#{version.csv.first}+#{version.csv.second}.zip"
   name "FVim"
   desc "GUI for the Neovim text editor"
   homepage "https://github.com/yatli/fvim"
