ce4b92dde016ebf6d34e2938fc9ea0b51f023cba
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-04-18 12:31:42 +0800

    gather 0.6.4

--- Casks/gather.rb
+++ Casks/gather.rb
@@ -1,9 +1,9 @@
 cask "gather" do
   arch arm: "-arm64"
 
-  version "0.6.3"
-  sha256 arm:   "5748efe87fec33b144fed6cc0f492b927823c1df75814799d5d24d0c2f5b30cc",
-         intel: "e86b31e0737f50601c09a7c73c384facc9a7ab128308528721b11e4d16c0e65d"
+  version "0.6.4"
+  sha256 arm:   "7d6ffd265a03ff9874719fae7204906670d3f4b7fcb3fed324b03e699b2d5a01",
+         intel: "e3e76e878fbd29a408c3c98a4c847fd6d706e17432ad3d088364b196d7d0f163"
 
   url "https://github.com/gathertown/gather-town-desktop-releases/releases/download/v#{version}/Gather-#{version}#{arch}-mac.zip",
       verified: "github.com/gathertown/gather-town-desktop-releases/"
