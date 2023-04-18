6cf271360939167268db332f23e56685bad3419b
miccal <miccal.matthews@gmail.com>
2023-04-19 07:08:26 +0800

    google-chrome 112.0.5615.137

--- Casks/google-chrome.rb
+++ Casks/google-chrome.rb
@@ -1,5 +1,5 @@
 cask "google-chrome" do
-  version "112.0.5615.121"
+  version "112.0.5615.137"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
