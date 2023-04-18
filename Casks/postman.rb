e379c9ceb35192e2a21c4924b72dfa4d3e84a754
stevenlele <stevenlele@outlook.com>
2023-04-18 17:30:12 +0800

    postman 10.13.0

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,9 +1,9 @@
 cask "postman" do
   arch arm: "osx_arm64", intel: "osx64"
 
-  version "10.12.13"
-  sha256 arm:   "589817c086766fb8ab8fa7e3b8bdf5b245bc61c999edb5cfcb305184d322ec5c",
-         intel: "24080ba5cd3cefc8911e19de435f54f9fdbda5ef428c4170ae938738101e78d8"
+  version "10.13.0"
+  sha256 arm:   "cda9232a6e8193f1e5f32688ac601066b16d78156d05d90033180ba98e9eba62",
+         intel: "adcba72ba5e3640dbd3996005eee54a64e216aa2f0515daa5a16e91bf8dec871"
 
   url "https://dl.pstmn.io/download/version/#{version}/#{arch}",
       verified: "dl.pstmn.io/download/version/"
