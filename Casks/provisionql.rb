cce97edddea16ccd552ac2d346f922a19ba3e718
Evgeny Aleksandrov <evgeny@aleksandrov.ws>
2023-04-18 19:47:35 +0300

    provisionql 1.6.3

--- Casks/provisionql.rb
+++ Casks/provisionql.rb
@@ -1,6 +1,6 @@
 cask "provisionql" do
-  version "1.6.2"
-  sha256 "762075acffde7c2aad376bbd946c4533ca81f214ef0e88eaab85d94551cc497a"
+  version "1.6.3"
+  sha256 "2f6fad63edb7c9064a5b859ef71b1265eee71b9e4bd29776207f6576a22c72b3"
 
   url "https://github.com/ealeksandrov/ProvisionQL/releases/download/#{version}/ProvisionQL.zip"
   name "ProvisionQL"
