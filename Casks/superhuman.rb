3b7288209ae212dc5f0392513ff4efe8f70bca47
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-18 19:11:25 -0400

    superhuman 21.0.26 (#145365)
    \n

--- Casks/superhuman.rb
+++ Casks/superhuman.rb
@@ -1,9 +1,9 @@
 cask "superhuman" do
   arch arm: "-arm64"
 
-  version "21.0.25"
-  sha256 arm:   "df440c5e521fd7fdeebf52354d5614236cecce73664774d4a84d5831d9eab9d6",
-         intel: "9cad350275a70450c8ae8f3f661f34170d9a9b06089dfcdfd4dd1a16ada201f4"
+  version "21.0.26"
+  sha256 arm:   "22fb6b81e2f24a172e6e742f61d5064517ab29d5bc0104b8c833dfc777f50928",
+         intel: "d716e3e3bdd332f00ee2e996e53265a58418630d289e402d669584954c1c10ff"
 
   url "https://storage.googleapis.com/download.superhuman.com/supertron-update/Superhuman-#{version}#{arch}-latest-mac.zip",
       verified: "storage.googleapis.com/download.superhuman.com/"
