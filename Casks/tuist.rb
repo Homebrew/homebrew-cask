b46dc9056b63c1e6155e672d7b45b82fbad8c87b
danibachar <danibachar89@gmail.com>
2023-04-19 15:00:34 -0600

    Add Tuist CLI version 3.18.0 (#145456)
    * Add tuist CLI version 3.18.0
    
    * style fixes
    
    * Update tuist.rb
    
    ---------
    
    Co-authored-by: Daniel Bachar <danielbac@zillowgroup.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/tuist.rb
@@ -0,0 +1,14 @@
+cask "tuist" do
+  version "3.18.0"
+  sha256 "afec90f79ff620821d56c3bcc361c25b7fb9bb876cceb77c0c38acc4921ff959"
+
+  url "https://github.com/tuist/tuist/releases/download/#{version}/tuist.zip",
+      verified: "github.com/tuist/tuist/"
+  name "Tuist"
+  desc "Create, maintain, and interact with Xcode projects at scale"
+  homepage "https://tuist.io/"
+
+  binary "tuist"
+
+  # No zap stanza required
+end
