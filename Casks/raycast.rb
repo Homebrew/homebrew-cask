9d2d8d7dbb4463e558dd4d733df31aff0e9a105b
Grzegorz Krukowski <grkrukowski@gmail.com>
2023-04-19 19:01:45 +0200

    raycast 1.49.3 (update url) (#145439)
    Co-authored-by: Razvan Azamfirei <azamfireirazvan@gmail.com>

--- Casks/raycast.rb
+++ Casks/raycast.rb
@@ -1,11 +1,11 @@
 cask "raycast" do
   version "1.49.3"
-  sha256 :no_check
+  sha256 "22b9fdf7fe3d7d1420ef7717f1a299ef60f5a3e9833e0e38435a5700c757adbd"
 
-  url "https://api.raycast.app/v2/download"
+  url "https://releases.raycast.com/releases/#{version}/download?build=universal"
   name "Raycast"
   desc "Control your tools with a few keystrokes"
-  homepage "https://raycast.app/"
+  homepage "https://raycast.com/"
 
   livecheck do
     url :url
