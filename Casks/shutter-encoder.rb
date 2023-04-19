e66981533fedeb43b050598c9f9d4c1d3d2309b5
Tin Htet Kyaw <tin@lsvp.com>
2023-04-19 15:19:21 -0700

    updated intel sha256 sum for shutter-encoder

--- Casks/shutter-encoder.rb
+++ Casks/shutter-encoder.rb
@@ -3,7 +3,7 @@ cask "shutter-encoder" do
 
   version "17.0"
   sha256 arm:   "6da7423e39dc6a7b189499799d98be31259e11380a3573f7bb8e0ea3ef1ee737",
-         intel: "b13d91345f7545c39c6fe30fdb3abee8a19cc6bfa08f885940015e22dd150355"
+         intel: "3332dc46aa7d4d67fcd90d73f0ba5e1bf87d658c29591ae378368f2b6afc998c",
 
   url "https://www.shutterencoder.com/Shutter%20Encoder%20#{version}%20#{arch.gsub(" ", "%20")}.pkg"
   name "Shutter Encoder"
