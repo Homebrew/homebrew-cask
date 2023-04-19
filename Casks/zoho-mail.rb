7487139ff1df1a3921ef98968c2880c29a0d5605
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-19 00:05:01 -0400

    revert zoho-mail

--- Casks/zoho-mail.rb
+++ Casks/zoho-mail.rb
@@ -17,11 +17,4 @@ cask "zoho-mail" do
   end
 
   app "Zoho Mail - Desktop.app"
-
-  zap trash: [
-    "~/Library/Application Support/Zoho Mail - Desktop",
-    "~/Library/Logs/Zoho Mail - Desktop",
-    "~/Library/Preferences/com.zoho.mail.desktop.plist",
-    "~/Library/Saved Application State/com.zoho.mail.desktop.savedState",
-  ]
 end
