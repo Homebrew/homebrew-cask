721b71516383e7da95ecf893438a8d0ff1d47da8
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-18 23:27:41 -0400

    opera-gx 97.0.4719.84 (#145415)
    \n

--- Casks/opera-gx.rb
+++ Casks/opera-gx.rb
@@ -1,6 +1,6 @@
 cask "opera-gx" do
-  version "97.0.4719.79"
-  sha256 "bcfd829ede7db387ae9eef31532ced8f3dea529dc6ede013f43da6b81a9d95af"
+  version "97.0.4719.84"
+  sha256 "4d135cfc5aa42f10ff719352533267c36965c67c5054d8e37beb16cf818c816b"
 
   url "https://get.geo.opera.com/pub/opera_gx/#{version}/mac/Opera_GX_#{version}_Setup.dmg"
   name "Opera GX"
