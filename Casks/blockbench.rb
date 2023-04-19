d9233b245a048bf93eaf556ab276cace4367f2bd
Chuanchan <williamtsai1111@gmail.com>
2023-04-20 01:04:08 +0800

    blockbench 4.7.0

--- Casks/blockbench.rb
+++ Casks/blockbench.rb
@@ -1,9 +1,9 @@
 cask "blockbench" do
   arch arm: "arm64", intel: "x64"
 
-  version "4.6.5"
-  sha256 arm:   "cbc11c9ddc71461ee78701b36f5868cfb74561da660967dbe8a709022bdba9e3",
-         intel: "5ba148e26ffda14c1582998254b29a8c1b2cc0873520e6f70656d17ecd163207"
+  version "4.7.0"
+  sha256 arm:   "0a86576a2eb23caf4c242b010a8d3fd2fc0497b54e4dd71a33907f692ce7a1b4",
+         intel: "2bce82bda56948c43acb610268f906115ab699acfa6b446fa01ae29655a5e8ef"
 
   url "https://github.com/JannisX11/blockbench/releases/download/v#{version}/Blockbench_#{arch}_#{version}.dmg",
       verified: "github.com/JannisX11/blockbench/"
