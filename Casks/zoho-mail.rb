9a06ee525beb6c0392bab49b3997d55bec25786f
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-19 00:06:56 -0400

    zoho-mail 1.5.5

--- Casks/zoho-mail.rb
+++ Casks/zoho-mail.rb
@@ -1,11 +1,11 @@
 cask "zoho-mail" do
   arch arm: "arm64-"
 
-  version "1.5.4"
-  sha256 arm:   "1306a455fb5711cf49f65c7623c3c7a0e58d5d429a1ea2d31b1f759fe3e60c96",
-         intel: "a599c992e72b3ac61d2c13327a1a7d64020e803a55c4f79c29a6ebec121e755e"
+  version "1.5.5"
+  sha256 arm:   "f5fefa81ec13096bcd677110eaf8f659f790709a9e90ac294a337f0d270da9eb",
+         intel: "918bafb5d69a4d9c1623da0a58546672033f43085b42f4751b39c4f308dee321"
 
-  url "https://downloads.zohocdn.com/zmail-desktop/mac/zoho-mail-desktop-installer-#{arch}v#{version}.dmg",
+  url "https://downloads.zohocdn.com/zmail-desktop/mac/zoho-mail-desktop-lite-installer-#{arch}v#{version}.dmg",
       verified: "downloads.zohocdn.com/zmail-desktop/mac/"
   name "Zoho Mail"
   desc "Email client"
@@ -13,8 +13,15 @@ cask "zoho-mail" do
 
   livecheck do
     url "https://downloads.zohocdn.com/zmail-desktop/artifacts.json"
-    regex(/zoho[._-]mail[._-]desktop[._-]installer[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+    regex(/zoho[._-]mail[._-]desktop[._-]lite[._-]installer[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
   app "Zoho Mail - Desktop.app"
+
+  zap trash: [
+    "~/Library/Application Support/Zoho Mail - Desktop",
+    "~/Library/Logs/Zoho Mail - Desktop",
+    "~/Library/Preferences/com.zoho.mail.desktop.plist",
+    "~/Library/Saved Application State/com.zoho.mail.desktop.savedState",
+  ]
 end
