2849a5fbf9c54a85a5b374ac895a8c5f341a3147
Amory Meltzer <Amorymeltzer@gmail.com>
2023-04-20 19:52:05 -0400

    chromium-gost 112.0.5615.165 (#145525)

--- Casks/chromium-gost.rb
+++ Casks/chromium-gost.rb
@@ -1,6 +1,6 @@
 cask "chromium-gost" do
-  version "112.0.5615.121"
-  sha256 "5050835eb2b578084abbd1a0746aa7ed0cc4b00155f9bd04e9f0724b3463e3a0"
+  version "112.0.5615.165"
+  sha256 "95e17ef3ddc6f8704c37ca89a24454cac5851d6d8cf22098b425e1084da299e0"
 
   url "https://github.com/deemru/Chromium-Gost/releases/download/#{version}/chromium-gost-#{version}-macos-amd64.tar.bz2"
   name "Chromium-Gost"
