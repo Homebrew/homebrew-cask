66f3cd23754fef878be3e7a08f067bb946568156
Arthur Brugière <16764085+RoiArthurB@users.noreply.github.com>
2023-04-20 11:09:28 +0700

    Fix hash & path for gama-jdk 1.9.1

--- Casks/gama-jdk.rb
+++ Casks/gama-jdk.rb
@@ -2,10 +2,10 @@ cask "gama-jdk" do
   arch arm: "Apple_Silicon", intel: "Intel"
 
   version "1.9.1"
-  sha256 arm:   "360e06ab4a864897b00e82e0603869ca52e4ee5e713ffddd41713765c3ed3946",
-         intel: "19d967383e0138253e392e005eb6e1eeb248a6783a11aae4ea291cfef405fff5"
+  sha256 arm:   "bad8a2da2ebe6a0276fef3539416625dce75118cef5ffc5fcd3e84b94d5f6b60",
+         intel: "98ccfa0fd0393cf0e4283ccf6c9ba16d2980464bf40012574882a879f013c358"
 
-  url "https://github.com/gama-platform/gama/releases/download/#{version}/GAMA_#{version}_macOS_#{arch}.dmg",
+  url "https://github.com/gama-platform/gama/releases/download/#{version}/GAMA_#{version}_macOS_#{arch}_with_JDK.dmg",
       verified: "github.com/gama-platform/gama/"
   name "GAMA Platform with embedded JDK"
   desc "IDE for building spatially explicit agent-based simulations"
