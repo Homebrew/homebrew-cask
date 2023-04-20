7a00754c5b2be31e5828ef1962cdbf03bce3da95
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-20 04:44:59 -0400

    various changes

--- Casks/active-trader-pro.rb
+++ Casks/active-trader-pro.rb
@@ -1,30 +1,26 @@
 cask "active-trader-pro" do
   version "1.0.50"
-  sha256 :no_check
+  sha256 "8f1266d172afdd9dd90c2ec93ebcf9fa304fdaaabe1f4ca8bbd59bc9ed769b30"
 
-  url "https://www.fidelity.com/webcontent/CodeweaverInstaller/ActiveTraderPro.pkg"
+  url "https://www.fidelity.com/webcontent/Codeweaver/activetrader-#{version}.zip"
   name "Active Trader Pro"
   desc "Fidelity's customizable trading platform"
   homepage "https://www.fidelity.com/trading/advanced-trading-tools/active-trader-pro/overview"
 
-  pkg "ActiveTraderPro.pkg"
-
-  postflight do
-    # The postinstall script automatically opens the app by making a call to
-    # `open -b com.fmr.activetrader`. Therefore, we must suppress this behavior
-    # to make the cask installation non-interactive.
-    retries ||= 3
-    ohai "The Active Trader Pro package postinstall script launches the app" unless retries < 3
-    ohai "Attempting to close Active Trader Pro to avoid unwanted user intervention" unless retries < 3
-    return unless system_command "/usr/bin/pkill", args: ["-f", "/Applications/Active Trader Pro.app"]
+  livecheck do
+    url "https://www.fidelity.com/webcontent/CodeweaverUpgradeInfo/activetrader-mac.xml"
+    strategy :sparkle, &:short_version
   end
 
-  uninstall quit:    "com.fmr.activetrader",
-            pkgutil: "com.fmr.activetrader",
-            delete:  "/Applications/Active Trader Pro.app"
+  depends_on macos: ">= :sierra"
+
+  app "Active Trader Pro.app"
 
   zap trash: [
-    "~/Library/Application Support/Active Trader Pro/",
+    "~/Library/Application Support/Active Trader Pro",
+    "~/Library/HTTPStorages/com.fmr.activetrader",
+    "~/Library/HTTPStorages/com.fmr.activetrader.binarycookies",
     "~/Library/Preferences/com.fmr.activetrader.plist",
+    "~/Library/Saved Application State/com.fmr.activetrader.savedState",
   ]
 end
