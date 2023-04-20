736dafba0d731c98073c7585128f24e29f23952f
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-04-20 08:04:26 +0800

    brave-browser 1.50.121.0

--- Casks/brave-browser.rb
+++ Casks/brave-browser.rb
@@ -2,9 +2,9 @@ cask "brave-browser" do
   arch arm: "arm64", intel: "x64"
   folder = on_arch_conditional arm: "stable-arm64", intel: "stable"
 
-  version "1.50.119.0,150.119"
-  sha256 arm:   "9eaabd92f757cecbcd3978fb16dd6b1685ec79f601add4b3a4859c1c03b375e5",
-         intel: "4570b67ecaedd5f9ba5c42aa41fee5d3f3bdeb9ab8dd8bf5cab304a03d328ec0"
+  version "1.50.121.0,150.121"
+  sha256 arm:   "9a4efaaf6c6c4c40d33936ab0db25ca8d1100bcd32d39e0d6a756fc4c7e6c606",
+         intel: "fb6a1a7d1c9a596825a296c24d926c2c19f6c1767126f567c355cfe07cc45738"
 
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.csv.second}/Brave-Browser-#{arch}.dmg",
       verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
