7713ba19637507f55c42285023bdafe27f3306a1
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-20 22:29:12 -0400

    pitch 1.120.2 (#145546)
    \n

--- Casks/pitch.rb
+++ Casks/pitch.rb
@@ -1,6 +1,6 @@
 cask "pitch" do
-  version "1.120.1,stable.1,5402597"
-  sha256 "92d9c0228ab101a99497e76ad7dd2ec79f43d83ffbfb5fb854c8620f51944b99"
+  version "1.120.2,stable.1,5403556"
+  sha256 "2b630a670fdb28389c6df03750368400013e5418476cb5a0830fc97125aaf150"
 
   url "https://desktop-app-builds.pitch.com/Pitch-#{version.csv.first}-#{version.csv.second}-ci#{version.csv.third}.dmg"
   name "Pitch"
