b82703f89550f4009ea0640c5e2f9f164434503e
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-21 12:58:26 -0400

    standard-notes 3.151.8

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -1,9 +1,9 @@
 cask "standard-notes" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.151.5"
-  sha256 arm:   "44f8073a95c0ac5ee0be8e26e02a5f8a9d5f1540301684124eebc866633b5f88",
-         intel: "bcb5ea8df43c1ac113b8b6e922e9226afd8bd788732e25446c80363e48a4d4b6"
+  version "3.151.8"
+  sha256 arm:   "41c8edb98a03549ed869faaccbce12332741c1a4d4f659369fab5a5119fd64b8",
+         intel: "ca1492e97188bc6847d0e84f9b1d475d7cad4922bb946cb7aa8203ed7f65e0db"
 
   url "https://github.com/standardnotes/app/releases/download/%40standardnotes%2Fdesktop%40#{version}/standard-notes-#{version}-mac-#{arch}.zip",
       verified: "github.com/standardnotes/app/"
