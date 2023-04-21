11da661bf74639e53f2b2a85399e0b4cf8583d5b
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-04-21 09:58:13 -0400

    Update archipelago for arm and include zap

--- Casks/archipelago.rb
+++ Casks/archipelago.rb
@@ -1,11 +1,23 @@
 cask "archipelago" do
-  version "4.3.0"
-  sha256 "2ad8d09afc04aec3d6f9011cd029eb6958c058b598085619ec5c2ce8b6db88ab"
+  arch arm: "-arm64"
 
-  url "https://github.com/npezza93/archipelago/releases/download/v#{version}/Archipelago-#{version}.dmg"
+  version "4.3.0"
+  sha256 arm:   "92ce07cd5984f63966a66219a3cdea3df88ea84c53c750928f1077daf839b43c",
+         intel: "2ad8d09afc04aec3d6f9011cd029eb6958c058b598085619ec5c2ce8b6db88ab"
+
+  url "https://github.com/npezza93/archipelago/releases/download/v#{version}/Archipelago-#{version}#{arch}.dmg"
   name "Archipelago"
   desc "Terminal emulator built on web technology"
   homepage "https://github.com/npezza93/archipelago"
 
   app "Archipelago.app"
+
+  zap trash: [
+    "~/Library/Application Support/Archipelago",
+    "~/Library/Caches/dev.archipelago",
+    "~/Library/Caches/dev.archipelago.ShipIt",
+    "~/Library/HTTPStorages/dev.archipelago",
+    "~/Library/Preferences/dev.archipelago.plist",
+    "~/Library/Saved Application State/dev.archipelago.savedState",
+  ]
 end
