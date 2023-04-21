2c6e055d90946cdeb7ab35184e1c4877abca20db
kortenkamp <kortenkamp@cinderella.de>
2023-04-21 12:12:13 +0200

    cinderella 3.0b.2073 (#145584)

--- Casks/cinderella.rb
+++ Casks/cinderella.rb
@@ -1,6 +1,6 @@
 cask "cinderella" do
-  version "3.0b.2072"
-  sha256 "7c1b90dc460cbb82a56606afc45c5d1db2505caace0aa38994ea79fcfa0c7dd6"
+  version "3.0b.2073"
+  sha256 "9c5e6662d1709c5610d043d524394faa3798b986d4228a77f7d1f4a41c7d8400"
 
   url "https://beta.cinderella.de/Cinderella-#{version}.dmg"
   name "Cinderella"
