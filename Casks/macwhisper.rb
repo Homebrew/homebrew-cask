2d72d3451fbbd08355402c769abc8b19be21621d
Bevan Kay <email@bevankay.me>
2023-04-23 00:23:21 +1000

    macwhisper: fix name (#145640)
    \n

--- Casks/macwhisper.rb
+++ Casks/macwhisper.rb
@@ -4,7 +4,7 @@ cask "macwhisper" do
 
   url "https://macwhisper-site.vercel.app/releases/MacWhisper-#{version.csv.second}.zip",
       verified: "macwhisper-site.vercel.app/"
-  name "macwhisper"
+  name "MacWhisper"
   desc "Speech recognition tool"
   homepage "https://goodsnooze.gumroad.com/l/macwhisper"
 
