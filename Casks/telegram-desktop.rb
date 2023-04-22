7408bceef8875e1f965673eb1cec890965ef249f
Valentin Zwerschke <vallezw@gmail.com>
2023-04-22 16:49:28 +0200

    telegram-desktop 4.8.0

--- Casks/telegram-desktop.rb
+++ Casks/telegram-desktop.rb
@@ -1,6 +1,6 @@
 cask "telegram-desktop" do
-  version "4.7.0"
-  sha256 "058c76ca6544d3bf28d1464598dfd67842bcbf4f82d127067076efd18cfdde6d"
+  version "4.8.0"
+  sha256 "caf501757f93f7767462c55868c26141762e2c343316ba437e371dae65ac8d53"
 
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg",
       verified: "updates.tdesktop.com/tmac/"
