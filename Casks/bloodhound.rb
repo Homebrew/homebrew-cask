b5b352f6d30e26fc90268a0414aa2bc95b8d27bb
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-04-22 00:20:41 -0400

    bloodhound: update version and add zap

--- Casks/bloodhound.rb
+++ Casks/bloodhound.rb
@@ -1,16 +1,22 @@
 cask "bloodhound" do
   arch arm: "arm64", intel: "x64"
 
-  version "4.2.0"
-  sha256 arm:   "74fd341390aaee0b7dac4f892df392eadb934932bf7e6d6047ffcc8532df1f3d",
-         intel: "121b6cc756d066cd5581f54215db3faf5edb5ff02697421cd38b6f93d6d5d65b"
+  version "4.3.0"
+  sha256 arm:   "1d86c9e3eb7fe0980381f071b0416ba1f53f665c18b418b66c0ce2344d98a135",
+         intel: "ebd36ece57ad0fefe730705480742620c4d189aa6a3d3a00d46504d1bd9741c9"
 
-  url "https://github.com/BloodHoundAD/BloodHound/releases/download/#{version}/BloodHound-darwin-#{arch}.zip"
+  url "https://github.com/BloodHoundAD/BloodHound/releases/download/v#{version}/BloodHound-darwin-#{arch}.zip"
   name "bloodhound"
   desc "Six Degrees of Domain Admin"
   homepage "https://github.com/BloodHoundAD/BloodHound"
 
   app "BloodHound-darwin-#{arch}/BloodHound.app"
+
+  zap trash: [
+    "~/Library/Application Support/bloodhound",
+    "~/Library/Preferences/com.electron.bloodhound.plist",
+    "~/Library/Saved Application State/com.electron.bloodhound.savedState",
+  ]
 
   caveats <<~EOS
     According to https://github.com/BloodHoundAD/BloodHound/issues/504
