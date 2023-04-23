a3b605593e68e00b7d61eda3b2922d0340b6fef7
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-04-23 16:55:20 -0400

    sfdx 7.197.8: add arch and sha256 (#145693)

--- Casks/sfdx.rb
+++ Casks/sfdx.rb
@@ -1,8 +1,11 @@
 cask "sfdx" do
-  version "7.196.6"
-  sha256 :no_check
+  arch arm: "arm64", intel: "x64"
 
-  url "https://developer.salesforce.com/media/salesforce-cli/sfdx/channels/stable/sfdx.pkg"
+  version "7.197.8"
+  sha256 arm:   "149d44c2b8063eaebc7e054f72384e7384d966978fb2dc09ddfdc6712eff4673",
+         intel: "f0c4770b8b1763e9ed2bbed99a438a73f986e286c6fad81541c804b985e7ba3e"
+
+  url "https://developer.salesforce.com/media/salesforce-cli/sfdx/channels/stable/sfdx-#{arch}.pkg"
   name "Salesforce DX CLI"
   desc "SalesForce CLI tools"
   homepage "https://developer.salesforce.com/tools/sfdxcli"
@@ -13,7 +16,7 @@ cask "sfdx" do
     strategy :page_match
   end
 
-  pkg "sfdx.pkg"
+  pkg "sfdx-#{arch}.pkg"
 
   uninstall pkgutil: [
               "com.salesforce.cli",
