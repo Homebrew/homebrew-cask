9e32c0a0d5473e004a2110ff47815f7b0ce83fc1
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-23 00:50:27 -0400

    r 4.3.0 (#145651)
    * r 4.3.0
    
    * rework cask

--- Casks/r.rb
+++ Casks/r.rb
@@ -1,33 +1,79 @@
 cask "r" do
-  arch arm: "-arm64"
-  folder = on_arch_conditional arm: "big-sur-arm64/base/", intel: "base/"
+  arch arm: "arm64", intel: "x86_64"
+  folder = on_arch_conditional arm: "big-sur-arm64/"
+  arch_legacy = on_arch_conditional arm: "-arm64"
 
   on_sierra :or_older do
     version "3.6.3.nn"
     sha256 "f2b771e94915af0fe0a6f042bc7a04ebc84fb80cb01aad5b7b0341c4636336dd"
 
     url "https://cloud.r-project.org/bin/macosx/R-#{version}.pkg"
+
+    livecheck do
+      skip "Legacy version"
+    end
+
+    pkg "R-#{version}.pkg"
   end
-  on_high_sierra :or_newer do
+  on_high_sierra do
     version "4.2.3"
     sha256 arm:   "e61f25b529940e229b69c19e01428505d7f59cc1e1209ed41dca39452b56fb98",
            intel: "dd96e8dcae20cf3c9cde429dd29f252b87af69028a6a403ec867eb92bb8eb659"
 
-    url "https://cloud.r-project.org/bin/macosx/#{folder}R-#{version}#{arch}.pkg"
+    url "https://cloud.r-project.org/bin/macosx/#{folder}base/R-#{version}#{arch_legacy}.pkg"
+
+    livecheck do
+      skip "Legacy version"
+    end
+
+    pkg "R-#{version}#{arch_legacy}.pkg"
+  end
+  on_mojave do
+    version "4.2.3"
+    sha256 arm:   "e61f25b529940e229b69c19e01428505d7f59cc1e1209ed41dca39452b56fb98",
+           intel: "dd96e8dcae20cf3c9cde429dd29f252b87af69028a6a403ec867eb92bb8eb659"
+
+    url "https://cloud.r-project.org/bin/macosx/#{folder}base/R-#{version}#{arch_legacy}.pkg"
+
+    livecheck do
+      skip "Legacy version"
+    end
+
+    pkg "R-#{version}#{arch_legacy}.pkg"
+  end
+  on_catalina do
+    version "4.2.3"
+    sha256 arm:   "e61f25b529940e229b69c19e01428505d7f59cc1e1209ed41dca39452b56fb98",
+           intel: "dd96e8dcae20cf3c9cde429dd29f252b87af69028a6a403ec867eb92bb8eb659"
+
+    url "https://cloud.r-project.org/bin/macosx/#{folder}base/R-#{version}#{arch_legacy}.pkg"
+
+    livecheck do
+      skip "Legacy version"
+    end
+
+    pkg "R-#{version}#{arch_legacy}.pkg"
+  end
+  on_big_sur :or_newer do
+    version "4.3.0"
+    sha256 arm:   "1bdddb68172d99ad31e85f1e59cdcdf38352f673b8b74a93b32bcc7b7e400050",
+           intel: "91dcf184da1365a386e7595622f86781993aac7013d497f011895f718fcc2852"
+
+    url "https://cloud.r-project.org/bin/macosx/big-sur-#{arch}/base/R-#{version}-#{arch}.pkg"
+
+    livecheck do
+      url "https://cloud.r-project.org/bin/macosx"
+      regex(/href=.*?R[._-]v?(\d+(?:\.\d+)*)([._-]#{arch})?\.pkg/i)
+    end
+
+    pkg "R-#{version}-#{arch}.pkg"
   end
 
   name "R"
   desc "Environment for statistical computing and graphics"
   homepage "https://www.r-project.org/"
 
-  livecheck do
-    url "https://cloud.r-project.org/bin/macosx"
-    regex(/href=.*?R[._-]v?(\d+(?:\.\d+)*)\.pkg/i)
-  end
-
   depends_on macos: ">= :el_capitan"
-
-  pkg "R-#{version}#{arch}.pkg"
 
   uninstall pkgutil: [
               "org.r-project*",
