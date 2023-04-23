e74545dd005c2eee8e841b656d67aa6a5ca8c794
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-04-23 22:10:48 +0800

    p4v 2023.1,2431464 (#145671)

--- Casks/p4v.rb
+++ Casks/p4v.rb
@@ -1,6 +1,6 @@
 cask "p4v" do
-  version "2023.1,2419860"
-  sha256 "79c85979c35ede86d6fd17b0f055655b08377484f32c9b45acaf7c98d161277f"
+  version "2023.1,2431464"
+  sha256 "edeec7b60addf7488a491e3d074bbc53c253b46aa8a294d3981428b411f3f531"
 
   url "https://www.perforce.com/downloads/perforce/r#{version.major[-2..]}.#{version.minor}/bin.macosx12u/P4V.dmg"
   name "Perforce Helix Visual Client"
