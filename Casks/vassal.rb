1ad7d9161737554fc577a14a69c3b8cfe2efa2bf
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-23 22:21:46 -0400

    vassal 3.6.17

--- Casks/vassal.rb
+++ Casks/vassal.rb
@@ -1,9 +1,9 @@
 cask "vassal" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "3.6.16"
-  sha256 arm:   "282ce69f399f71a8d9bfb1c8660e703b9864a47aab58dd796c6e344b6f3f7642",
-         intel: "6ac6ec7a5818e980970d0b577bc54d6ce40352079a88fcbb592cb48255599afa"
+  version "3.6.17"
+  sha256 arm:   "1850a37ef3e46401e2cce13ae7653a22c49eee1c285f581dc92020bb181c5cca",
+         intel: "a6abc0854b72283920e29807de227847fe5c961253cda869c1997cc61f59bda9"
 
   url "https://github.com/vassalengine/vassal/releases/download/#{version}/VASSAL-#{version}-macos-#{arch}.dmg",
       verified: "github.com/vassalengine/vassal/"
