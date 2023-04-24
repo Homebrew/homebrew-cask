6ddcc73790edf2deeb40cb469bd7c72ba272e3ad
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-23 22:41:23 -0400

    add zap

--- Casks/sound-siphon.rb
+++ Casks/sound-siphon.rb
@@ -15,4 +15,20 @@ cask "sound-siphon" do
   depends_on macos: ">= :mojave"
 
   app "Sound Siphon.app"
+
+  uninstall script: {
+    executable: "#{appdir}/Sound Siphon.app/Contents/Resources/uninstall_soundsiphon",
+    sudo:       true,
+  }
+
+  zap trash: [
+    "~/Library/Application Support/Sound Siphon",
+    "~/Library/Caches/com.staticz.SoundSiphon3",
+    "~/Library/HTTPStorages/com.staticz.SoundSiphon3",
+    "~/Library/HTTPStorages/com.staticz.SoundSiphon3.binarycookies",
+    "~/Library/Preferences/com.staticz.soundsiphon.playeragent.plist",
+    "~/Library/Preferences/com.staticz.SoundSiphon3.plist",
+    "~/Library/Saved Application State/com.staticz.SoundSiphon3.savedState",
+    "~/Music/Sound Siphon",
+  ]
 end
