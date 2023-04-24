fc73460953b6b51ed2bd1936325d91093bcf1dd9
Lex.Chen <ctyytc@gmail.com>
2023-04-24 15:14:41 +0800

    Update dbeaver-community.rb
    Fix sha256 sum code

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -2,8 +2,8 @@ cask "dbeaver-community" do
   arch arm: "aarch64", intel: "x86_64"
 
   version "23.0.3"
-  sha256 arm:   "d5592b0bff8bf0a2dce307121a1ca8c474e7f315a65e9e290f57692f66fe1c97",
-         intel: "03c7106df3ea7b50104115e44895072c1b3260280570bd4c089818a06dd689e6"
+  sha256 arm:   "ecce3eb5e84ff773675e92a67db0f799cc290bd9bbd744df1008dad5fd1232ea",
+         intel: "d41e3f2cc44b30fc74a0870e9afd819e6f674887d024ca8f3bf8bce64ae0866f"
 
   url "https://dbeaver.io/files/#{version}/dbeaver-ce-#{version}-macos-#{arch}.dmg"
   name "DBeaver Community Edition"
