44cb4b7627ce93aeaa08c7c363664a0e882bbf4d
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-04-25 12:16:27 -0400

    poker-copilot 7.38 (#145807)

--- Casks/poker-copilot.rb
+++ Casks/poker-copilot.rb
@@ -1,6 +1,6 @@
 cask "poker-copilot" do
-  version "7.37,9055"
-  sha256 "74d92838c5f232c76e4cb994b42eca1effb2e9d8a852da9277d9b9bc8e1ca22f"
+  version "7.38,9074"
+  sha256 "b4ffe892a2de093fca29e97b76aa8f0535a0c1956605ca558e1cb2ecc9ddb817"
 
   url "https://static.pokercopilot.com/pokercopilot_macos_#{version.csv.first.dots_to_underscores}_build_#{version.csv.second}.dmg"
   name "Poker Copilot"
