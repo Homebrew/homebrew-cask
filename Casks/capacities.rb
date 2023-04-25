1ae70b9eaa657e0fe35d49bc42d4c7d60ab327c4
Alejandro García Rodríguez <alejgarciarodriguez@gmail.com>
2023-04-25 23:35:19 +0200

    Add Capacities.app v1.21.10

---
+++ Casks/capacities.rb
@@ -0,0 +1,29 @@
+cask "capacities" do
+  arch arm: "-arm64"
+  on_arch_conditional arm: "apple-silicon", intel: "mac"
+
+  version "1.21.10"
+  sha256 arm:   "8f83514639af65237a095cdeb9f5e9c0d5782aff7c21a4537b05005a4e00d9c2",
+         intel: "acc61baed9329c14cae1cf765d39407ad709937b604f584c81be3b9afa3697ff"
+
+  url "https://capacities-frontend-app.s3.eu-central-1.amazonaws.com/Capacities-#{version}#{arch}.dmg",
+      verified: "capacities-frontend-app.s3.eu-central-1.amazonaws.com/"
+  name "Capacities"
+  desc "Is a place for all your information"
+  homepage "https://capacities.io/"
+
+  livecheck do
+    skip "No version information available"
+  end
+
+  auto_updates false
+
+  app "Capacities.app"
+
+  zap trash: [
+    "~/Library/Application Support/Capacities",
+    "~/Library/Preferences/io.capacities.app.plist",
+    "~/Library/Logs/Capacities/",
+    "~/Library/Saved Application State/io.capacities.app.savedState",
+  ]
+end
