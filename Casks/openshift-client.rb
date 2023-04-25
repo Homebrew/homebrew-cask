1865602d9b4666a3096c43e14f61ee050f10b0ec
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-04-25 11:45:21 -0400

    openshift-client 4.12.14 (#145803)

--- Casks/openshift-client.rb
+++ Casks/openshift-client.rb
@@ -1,9 +1,9 @@
 cask "openshift-client" do
   arch arm: "-arm64"
 
-  version "4.12.13"
-  sha256 arm:   "f8f7740b504eeb9fcf792b3787d9faf87757802f10fbefb917a0bdb2d988f786",
-         intel: "0577506c00bbc13aab96f1fd5bcc0a0bc391f88e27b37268f2990294d8f8e4d0"
+  version "4.12.14"
+  sha256 arm:   "7e3a398d4798784f76a7a90441939e54ad1b46f49c08a3c5d4af0a3443ea5c53",
+         intel: "e1e41a72d30348d98495d49f94eff0607534d02e2c4de29c38339747f4e72b9c"
 
   url "https://mirror.openshift.com/pub/openshift-v#{version.major}/clients/ocp/#{version}/openshift-client-mac#{arch}.tar.gz"
   name "Openshift Client"
