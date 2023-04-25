ebba9b07d5d3c31939da4dcd397f0b238542be8d
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-25 18:35:49 -0400

    missive 10.47.0

--- Casks/missive.rb
+++ Casks/missive.rb
@@ -1,9 +1,8 @@
 cask "missive" do
-  version "10.44.0"
-  sha256 "6568046ef0bf902d55e6a726c49d92e38a47b59734d8cb6111099ab2bcbffd1f"
+  version "10.47.0"
+  sha256 "547c002fc8433d62ee25951b2feea97406182a947fa56b84d62552da20fe880a"
 
-  url "https://d1weoh55n7oehu.cloudfront.net/stable/Missive-#{version}.dmg",
-      verified: "d1weoh55n7oehu.cloudfront.net/"
+  url "https://downloads.missiveapp.com/#{version}/Missive-#{version}.dmg"
   name "Missive"
   desc "Team inbox and chat tool"
   homepage "https://missiveapp.com/"
