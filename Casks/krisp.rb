5cee07bd85c0dc8b92b932fe17a80c64adcf5d91
Gabe <gabe@GMBA.local>
2023-04-25 09:43:40 -0400

    krisp 1.45.1

--- Casks/krisp.rb
+++ Casks/krisp.rb
@@ -1,10 +1,13 @@
 cask "krisp" do
-  version "1.44.1"
-  sha256 "88e861c282ffb504becd8108b55947b9f6b005106c898f7ce2ceef0d6792e0a6"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://cdn.krisp.ai/mac/release/v#{version.major}.#{version.minor}/krisp_#{version}.pkg"
+  version "2.18.6"
+  sha256 arm:   "e85e9cda73bd4b8b5d0b1eba4767b603d28fcc07f92e00d0aa793ac2d2812476",
+         intel: "264e0640ca73bc6874069c51c2d780f1d68aae3033ae5efa4a7a6f9755802e4a"
+
+  url "https://cdn.krisp.ai/mp/#{version}/mac/#{arch}/krisp_#{version}_#{arch}.pkg"
   name "Krisp"
-  desc "Sound clear in online meetings"
+  desc "Noise cancelling application"
   homepage "https://krisp.ai/"
 
   livecheck do
@@ -33,7 +36,7 @@ cask "krisp" do
     "~/Library/Application Support/ai.krisp.krispMac",
     "~/Library/Caches/ai.krisp.krispMac",
     "~/Library/Containers/ai.krisp.krispMac.LaunchHelper",
-    "~/Library/HTTPStorages/ai.krisp.krispMac.uninstaller",
+    "~/Library/HTTPStorages/ai.krisp.krispMac.*",
     "~/Library/LaunchAgents/ai.krisp.krispMac.cameraAssistant.plist",
     "~/Library/Preferences/ai.krisp.krispMac.plist",
   ]
