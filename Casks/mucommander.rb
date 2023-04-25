e236de03fced033d27e74c8033f690ed121978aa
Italo Cunha <cunha@dcc.ufmg.br>
2023-04-24 22:24:26 -0300

    Update muCommander Mac version number

--- Casks/mucommander.rb
+++ Casks/mucommander.rb
@@ -1,8 +1,8 @@
 cask "mucommander" do
-  version "1.2.0-1"
-  sha256 "c77c3d0105de57dcb2e2641a23266aef576b9874a61096935d961427eece636c"
+  version "1.2.0-2"
+  sha256 "d5e386d62b01ed7614b15a3fb807b21f8c3e650ddbd2b5b8d08c6949ffcd779d"
 
-  url "https://github.com/mucommander/mucommander/releases/download/#{version}/muCommander-#{version}.dmg",
+  url "https://github.com/mucommander/mucommander/releases/download/1.2.0-1/muCommander-#{version}.dmg",
       verified: "github.com/mucommander/mucommander/"
   name "muCommander"
   desc "File manager with a dual-pane interface"
