ed66982b25aa3d7bfdfe0a429e1df6919c9bbced
Baocang Nie <baocang.nie@icloud.com>
2023-04-26 02:51:48 +0800

    hbuilderx 3.7.10.20230425

--- Casks/hbuilderx.rb
+++ Casks/hbuilderx.rb
@@ -1,6 +1,6 @@
 cask "hbuilderx" do
-  version "3.7.9.20230324"
-  sha256 "075827c8b81b82662b1cea54bcf10186c4ea69a44be24db87513d8a2a3bb5527"
+  version "3.7.10.20230425"
+  sha256 "de8e465f35efd24f5183fcba8e1bfaf6c18bcea4aa3f1dfe5be9ce299991dc1d"
 
   url "https://download1.dcloud.net.cn/download/HBuilderX.#{version}.dmg",
       verified: "download1.dcloud.net.cn/download/"
