18b119f1ba838fce5b5fe4bb77c962977eeb3a65
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-04-26 09:51:10 -0400

    razorsql 10.3.3 (#145903)

--- Casks/razorsql.rb
+++ Casks/razorsql.rb
@@ -1,9 +1,9 @@
 cask "razorsql" do
   arch arm: "_aarch64"
 
-  version "10.3.2"
-  sha256 arm:   "f081d5f5caa4ca1079ba1f1d378102bdc3eee2188a081e9a2de063fb8048151f",
-         intel: "a2f7f6704272e86883f58cd629a9312dfe2d756cab853af3a38a2ed745c3088d"
+  version "10.3.3"
+  sha256 arm:   "e422c37b40e540d036ca4df131f0bd0df50cf90b7c574d49102394e6c2494f91",
+         intel: "a530449b5e21844604849a1b5bf61b8c73222996af0980094ab06aaab2cf8c80"
 
   url "https://s3.dualstack.us-east-1.amazonaws.com/downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}#{arch}.dmg",
       verified: "s3.dualstack.us-east-1.amazonaws.com/"
