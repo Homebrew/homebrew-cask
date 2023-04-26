9cd474ce37d299b150c521c160d3c06e6d03ece7
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-04-26 10:48:52 -0400

    bitwarden 2023.4.0

--- Casks/bitwarden.rb
+++ Casks/bitwarden.rb
@@ -1,6 +1,6 @@
 cask "bitwarden" do
-  version "2023.3.2"
-  sha256 "0e4e854e0621c464ba734aeaf37648e1a799482f01b2fcf8435ee7e28c1953a4"
+  version "2023.4.0"
+  sha256 "0425d08e24d3e45f34f4f7f0989c952c73329295d9204d54b3114b294d78b2b0"
 
   url "https://github.com/bitwarden/clients/releases/download/desktop-v#{version}/Bitwarden-#{version}-universal.dmg",
       verified: "github.com/bitwarden/clients/"
