d1dd052769d430236f0688cfd80f1ea5494a69a3
Mikl Wolfe <wolfemikl@gmail.com>
2023-04-26 08:13:53 -0600

    grammarly-desktop 1.25.0.3

--- Casks/grammarly-desktop.rb
+++ Casks/grammarly-desktop.rb
@@ -1,6 +1,6 @@
 cask "grammarly-desktop" do
-  version "1.24.6.0"
-  sha256 "03f6095216a68ed9e15573b940b33242ac279674266674d59ae864c6bea4122c"
+  version "1.25.0.3"
+  sha256 "a58809c9cba4aa9c84fd476239a10cdcb6cf644b82fa788d3ad6498dc8e50011"
 
   url "https://download-mac.grammarly.com/versions/#{version}/Grammarly.dmg"
   name "Grammarly Desktop"
