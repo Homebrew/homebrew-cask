9fc02aabd2b622082910176bfde6d433f1512595
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-04-27 08:07:38 -0400

    capacities 1.23.30

--- Casks/capacities.rb
+++ Casks/capacities.rb
@@ -1,9 +1,9 @@
 cask "capacities" do
   arch arm: "-arm64"
 
-  version "1.21.10"
-  sha256 arm:   "8f83514639af65237a095cdeb9f5e9c0d5782aff7c21a4537b05005a4e00d9c2",
-         intel: "acc61baed9329c14cae1cf765d39407ad709937b604f584c81be3b9afa3697ff"
+  version "1.23.30"
+  sha256 arm:   "5fd1420f999aaa884f870fd0b373e1299dfc87f7a64446cc317c1d09640d6195",
+         intel: "c8a7adf3912d36f5231ab1296b98611d4b88cb1dcb55e643228a31216435cc31"
 
   url "https://capacities-frontend-app.s3.eu-central-1.amazonaws.com/Capacities-#{version}#{arch}.dmg",
       verified: "capacities-frontend-app.s3.eu-central-1.amazonaws.com/"
