70f4d1f8643ff2ad6266920f6b222a6b0e562896
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-04-26 22:00:38 -0400

    second-life-viewer 6.6.11.579629

--- Casks/second-life-viewer.rb
+++ Casks/second-life-viewer.rb
@@ -1,6 +1,6 @@
 cask "second-life-viewer" do
-  version "6.6.10.579060"
-  sha256 "867c8979d0cd6ddfb1b4537b203f2eb991aa8ace460306ad64c20e1d520fc7dd"
+  version "6.6.11.579629"
+  sha256 "a2d75d57c3bcb392b4268da1e26895dcf046e89c2593c8fe4a90d990e44e10c9"
 
   url "http://download.cloud.secondlife.com/Viewer_#{version.major}/Second_Life_#{version.dots_to_underscores}_x86_64.dmg"
   name "Linden Lab Second Life Viewer"
