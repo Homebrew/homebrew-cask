9708ed594e06fce73d76a7d242fcfc0f3658da78
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-04-27 17:45:54 -0400

    sfdx (fix sha256 no_check, update zap) (#146035)
    sfdx (fixes)
    
    * Revert back to sha256 no_check due to unversioned URL
    
    * Add path to zap

--- Casks/sfdx.rb
+++ Casks/sfdx.rb
@@ -2,8 +2,7 @@ cask "sfdx" do
   arch arm: "arm64", intel: "x64"
 
   version "7.197.8"
-  sha256 arm:   "149d44c2b8063eaebc7e054f72384e7384d966978fb2dc09ddfdc6712eff4673",
-         intel: "f0c4770b8b1763e9ed2bbed99a438a73f986e286c6fad81541c804b985e7ba3e"
+  sha256 :no_check
 
   url "https://developer.salesforce.com/media/salesforce-cli/sfdx/channels/stable/sfdx-#{arch}.pkg"
   name "Salesforce DX CLI"
@@ -28,5 +27,6 @@ cask "sfdx" do
     "~/.cache/sfdx",
     "~/.config/sfdx",
     "~/.local/share/sfdx",
+    "~/.sf",
   ]
 end
