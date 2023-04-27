efe65ee8c3f0796d4cab4d5fa046acfd4cf2c1d7
Bevan Kay <email@bevankay.me>
2023-04-28 09:24:24 +1000

    karafun: various updates

--- Casks/karafun.rb
+++ Casks/karafun.rb
@@ -1,13 +1,25 @@
 cask "karafun" do
-  version "2.2.0.86"
-  sha256 :no_check
+  version "2.2.0,86"
+  sha256 "ca26e741588827b17b8ae4d72d3f713ba8aa029cbddea08eb8403a9bc8151b42"
 
-  url "https://www.karafun.com/download/mac.html"
+  url "https://c20.recis.io/sl/75651288c7/63683b44547cfdbafd795b67c7101da5/KaraFun_#{version.csv.first}.#{version.csv.second}.dmg",
+      verified: "c20.recis.io/sl/75651288c7/63683b44547cfdbafd795b67c7101da5/"
   name "KaraFun"
   desc "Karaoke player software"
   homepage "https://www.karafun.com/"
 
+  livecheck do
+    url "https://www.karafun.fr/osx/appcast.xml"
+    strategy :sparkle
+  end
+
+  auto_updates true
+  depends_on macos: ">= :monterey"
+
   app "KaraFun.app"
 
-  # No zap stanza required
+  zap trash: [
+    "~/Library/Application Support/com.recisio.kfiphone",
+    "~/Library/Containers/com.recisio.kfiphone",
+  ]
 end
