13c2d5167934ef722e2b74fba25c2fb02e6e3d64
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-04-28 09:15:02 -0400

    binance 1.45.2 (#146083)

--- Casks/binance.rb
+++ Casks/binance.rb
@@ -1,9 +1,9 @@
 cask "binance" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.44.0"
-  sha256  arm:   "e1fa7263a2fe1318ea19834b43486075cde33946a5573dde755fcc7b3385dbfc",
-          intel: "42778d4623087371a9ff4ab2ea63a24127b8169291593bb9c6f7c6470f3555ca"
+  version "1.45.2"
+  sha256  arm:   "98c6f67a3a591e37fe7f65e5b488afdee8c53e1dda97f557c67028e31b81bceb",
+          intel: "17737b3fc67489acde817aff207acc0c226475d5a9a16e4ec0c1529a47456148"
 
   url "https://ftp.binance.com/electron-desktop/mac/production/binance-#{version}-#{arch}.dmg"
   name "Binance"
