37b2b5188b815730af3d012d8e42abd2509d0023
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-04-29 09:55:31 -0400

    webull (update zap) (#146167)

--- Casks/webull.rb
+++ Casks/webull.rb
@@ -17,5 +17,10 @@ cask "webull" do
 
   app "Webull Desktop.app"
 
-  zap trash: "~/Library/Application Support/Webull Desktop"
+  zap trash: [
+    "~/.config/Webull Desktop",
+    "~/Library/Application Support/Webull Desktop",
+    "~/Library/Preferences/com.webulldesktop.*",
+    "~/Library/Saved Application State/com.webull.desktop.v1.savedState",
+  ]
 end
