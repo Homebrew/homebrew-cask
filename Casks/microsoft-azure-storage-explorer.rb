751bf4f1ddc7a5e81e1a6a660e88cb878e7b080f
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-29 20:35:33 -0400

    microsoft-azure-storage-explorer 1.29.0 (#146173)
    \n

--- Casks/microsoft-azure-storage-explorer.rb
+++ Casks/microsoft-azure-storage-explorer.rb
@@ -1,9 +1,9 @@
 cask "microsoft-azure-storage-explorer" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.28.1"
-  sha256 arm:   "d9a20d28a38cfd605d2a84a833bccaf9edb4cde950406769cf739e57dbf99a80",
-         intel: "6c4528319b8aac3954a5355ff60ecae19045852680daa2dc003f7fabebfa42ad"
+  version "1.29.0"
+  sha256 arm:   "ca29393b210e9a1e4ceeb66b39c9c03428585be88563a6024a17c101e8bad033",
+         intel: "c6856634962c42a709096cfdfa353ebeb7dd2157b425a146b23d47d95ad93902"
 
   url "https://github.com/microsoft/AzureStorageExplorer/releases/download/v#{version}/StorageExplorer-darwin-#{arch}.zip",
       verified: "github.com/microsoft/AzureStorageExplorer/"
