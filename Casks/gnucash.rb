6952d9b6c32b20897abde7ba2ddcdfbe51e21c09
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-30 18:52:09 -0400

    gnucash 5.1-1 (#146219)
    \n

--- Casks/gnucash.rb
+++ Casks/gnucash.rb
@@ -1,6 +1,6 @@
 cask "gnucash" do
-  version "5.0-1"
-  sha256 "c8ea60b2ccbeab5f6997a927939a0fad715fbbe494644e586c6c386bfec6857a"
+  version "5.1-1"
+  sha256 "48c38097fe744a7da7bbd454d0e58e3619194d8f41e6542137c0bebac440e386"
 
   url "https://github.com/Gnucash/gnucash/releases/download/#{version.hyphens_to_dots.major_minor}/Gnucash-Intel-#{version}.dmg",
       verified: "github.com/Gnucash/gnucash/"
