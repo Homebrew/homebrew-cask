a5f6416f2237b7f6e93499d72d32ef18eaa7e21c
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-30 18:52:56 -0400

    blockbench 4.7.2 (#146209)

--- Casks/blockbench.rb
+++ Casks/blockbench.rb
@@ -1,9 +1,9 @@
 cask "blockbench" do
   arch arm: "arm64", intel: "x64"
 
-  version "4.7.1"
-  sha256 arm:   "3d7b9a2643f164901f6dc6491da1fcc8d5c4b792a5ab47f1b7c9628cd0a6b9ea",
-         intel: "84f6d7ce908e6e464a2d82d05c10b7762453460b63d48b86072423a835b59678"
+  version "4.7.2"
+  sha256 arm:   "4c36f73168a180b9d6da7d5be83dcf28e53540cbe3c70993747c5116897113e1",
+         intel: "234d7249f95421e2811abce5f840c8d4b1af16b96385ef8b21e629e29307c00d"
 
   url "https://github.com/JannisX11/blockbench/releases/download/v#{version}/Blockbench_#{arch}_#{version}.dmg",
       verified: "github.com/JannisX11/blockbench/"
