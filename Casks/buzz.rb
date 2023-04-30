ab27c6e7f794fb70f49df90a0a30d177328fe3ba
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-30 18:52:45 -0400

    buzz 0.8.0 (#146212)

--- Casks/buzz.rb
+++ Casks/buzz.rb
@@ -1,6 +1,6 @@
 cask "buzz" do
-  version "0.7.2"
-  sha256 "e187ba271c8491a2d34bdede96e7bbd663c5a248332dd656e3c1a6140e99208f"
+  version "0.8.0"
+  sha256 "80823cc30a6befbb572ed388699c591882c1fe15e13cac6dbd58fb452e6277f4"
 
   url "https://github.com/chidiwilliams/buzz/releases/download/v#{version}/Buzz-#{version}-mac.dmg"
   name "Buzz"
