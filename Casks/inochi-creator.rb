fbc3d9b0321aa3d90eb1406fe39906d67ea70fc9
Caleb Jasik <calebjasik@jasik.xyz>
2023-04-30 16:04:19 -0500

    Fix version and description

--- Casks/inochi-creator.rb
+++ Casks/inochi-creator.rb
@@ -1,11 +1,11 @@
 cask "inochi-creator" do
-  version "0.7.4"
-  sha256 "fad0830b40ff22c371bdb1d84708a211ec400c48157c82ab563887f7919c6fa2"
+  version "0.7.4.1"
+  sha256 "cb59ac8d58c6874a13f9e178ff0bc31e9630f8fd1a36c24403f82c65e62c6dac"
 
   url "https://github.com/Inochi2D/inochi-creator/releases/download/v#{version}/Install_Inochi_Creator.dmg",
       verified: "github.com/Inochi2D/inochi-creator/"
   name "Inochi Creator"
-  desc "An open source editor for the Inochi2D puppet format."
+  desc "Open source editor for the Inochi2D puppet format"
   homepage "https://inochi2d.com/"
 
   livecheck do
