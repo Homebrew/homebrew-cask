9381d09798e5ffc582259e086cb3b2be38cf9eeb
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-05-02 00:18:27 +0800

    browserosaurus 20.2.2

--- Casks/browserosaurus.rb
+++ Casks/browserosaurus.rb
@@ -1,9 +1,9 @@
 cask "browserosaurus" do
   arch arm: "arm64", intel: "x64"
 
-  version "20.2.1"
-  sha256 arm:   "4510a1eff2a156ea0bcfacbabeb6b52e996879fd003d1f6f5d593a525e729b00",
-         intel: "ae846a151c00b66a869646f9687a79e027cd1c86939ba33b4020db5e38fa8f0f"
+  version "20.2.2"
+  sha256 arm:   "4735b26fa13d896d82dfe8915563196b02955d9a31d271954772be9d6f6f7a53",
+         intel: "e772cb4803f6868c7d58b85c5e4b5666afb8205367e4fcb099936da3583a599f"
 
   url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-darwin-#{arch}-#{version}.zip"
   name "Browserosaurus"
