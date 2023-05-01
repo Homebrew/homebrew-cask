306571b848c424a285cce28cad0ff9100c9d9ed3
urwrstkn8mare <42769125+urwrstkn8mare@users.noreply.github.com>
2023-05-01 15:12:15 +0800

    fix desc

--- Casks/bloop.rb
+++ Casks/bloop.rb
@@ -8,7 +8,7 @@ cask "bloop" do
   url "https://github.com/BloopAI/bloop/releases/download/v#{version}/bloop_#{version}_#{arch}.dmg",
       verified: "github.com/BloopAI/bloop/"
   name "bloop"
-  desc "Is a code-search engine that uses GPT-4 to answer questions about your code. Search both your local and remote repositories with natural language, regex and filtered queries"
+  desc "bloop is a fast code search engine written in Rust."
   homepage "https://bloop.ai/"
 
   livecheck do
