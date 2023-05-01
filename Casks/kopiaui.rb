a40dfe2b86148c2a746f179cd2f116fb02b6b840
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-05-02 00:24:19 +0800

    kopiaui 0.13.0

--- Casks/kopiaui.rb
+++ Casks/kopiaui.rb
@@ -1,9 +1,9 @@
 cask "kopiaui" do
   arch arm: "-arm64"
 
-  version "0.12.1"
-  sha256 arm:   "e1cb1f902a9fb2224d978c9a4e595ec082b674d45137caa816f71afe16d3f7c8",
-         intel: "61c4d20bc477f7e88188de7433c7dc8380db981301bbefc6f2624b05c84ebb47"
+  version "0.13.0"
+  sha256 arm:   "47981cdb5ea1bef7b6b587cfc8225965d3283abc398ac8c70899fa59937d50ba",
+         intel: "cb07d568196bae64dd2dc7c1a9d940308c4d802793cc0a937c893f985d96e287"
 
   url "https://github.com/kopia/kopia/releases/download/v#{version}/KopiaUI-#{version}#{arch}.dmg",
       verified: "github.com/kopia/kopia/"
