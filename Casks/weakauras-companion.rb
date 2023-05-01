531499ee04e36cc174f790696ca27f87b4680759
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-30 21:56:39 -0400

    Update weakauras-companion.rb

--- Casks/weakauras-companion.rb
+++ Casks/weakauras-companion.rb
@@ -1,6 +1,6 @@
 cask "weakauras-companion" do
-  version "4.1.5"
-  sha256 "3afe8259b0ecc9cd13a4921b315b551cda7097b835ee03057d4aba62695e34f6"
+  version "5.0.0"
+  sha256 "2acd02c97e4bfef118f1cb8dee33806329224fc99e1e6a5c0003fb6605b6e78d"
 
   url "https://github.com/WeakAuras/WeakAuras-Companion/releases/download/v#{version}/WeakAuras-Companion-#{version}-mac-universal.dmg",
       verified: "github.com/WeakAuras/WeakAuras-Companion/"
