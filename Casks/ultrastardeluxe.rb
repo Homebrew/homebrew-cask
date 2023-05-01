59aa4721730c8e4f4039062e32b45e5bd4e46363
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-30 21:38:59 -0400

    ultrastardeluxe 2023.4.0

--- Casks/ultrastardeluxe.rb
+++ Casks/ultrastardeluxe.rb
@@ -1,8 +1,8 @@
 cask "ultrastardeluxe" do
-  version "2023.3.0"
-  sha256 "124ec65edad0f935a5c4b89ee224049a0e0bf1870c89d11b2e6cf3fac08acb92"
+  version "2023.4.0"
+  sha256 "7fde820d3194e1846bd626438294b03ae8ba5482c15ff949db20d859484ee692"
 
-  url "https://github.com/UltraStar-Deluxe/USDX/releases/download/v#{version}/UltraStarDeluxe-v#{version}-0.dmg",
+  url "https://github.com/UltraStar-Deluxe/USDX/releases/download/v#{version}/UltraStar.Deluxe_v#{version}.stable.dmg",
       verified: "github.com/UltraStar-Deluxe/USDX/"
   name "UltraStar Deluxe"
   desc "Karaoke game"
