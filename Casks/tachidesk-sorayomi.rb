9fe42db29c01e92cc431cb2487d20e85bf336626
DattatreyaReddy Panta <58727124+DattatreyaReddy@users.noreply.github.com>
2023-05-02 10:44:12 +0530

    tachidesk-sorayomi 0.5.5 (#146309)

--- Casks/tachidesk-sorayomi.rb
+++ Casks/tachidesk-sorayomi.rb
@@ -1,6 +1,6 @@
 cask "tachidesk-sorayomi" do
-  version "0.5.4"
-  sha256 "e603c3a23deca26fa743fb2748f7cb43d4f146363e900a3dc2df84cf727720dd"
+  version "0.5.5"
+  sha256 "533fe4ff9a15ffd2ab838824d3b606e1396d5660afaf84adbe20ab5a1d2c1ede"
 
   url "https://github.com/Suwayomi/Tachidesk-Sorayomi/releases/download/#{version}/tachidesk-sorayomi-#{version}-macos-x64.zip"
   name "Tachidesk Sorayomi"
