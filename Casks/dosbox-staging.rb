83daa2546c2c764aff0b64ca5c902c9c6a381741
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-02 17:19:03 -0400

    cask changes

--- Casks/dosbox-staging.rb
+++ Casks/dosbox-staging.rb
@@ -1,21 +1,13 @@
 cask "dosbox-staging" do
   version "0.80.1"
-  sha256 "46a256645255e8345981ea357f1416b8ce4bc60a2aba9a86b5122d5075aa7fa"
+  sha256 "46a256645255e8345981ea357f1416b8ce4bc60a2aba9a86b5122d5075aa7fa9"
 
-  url "https://github.com/dosbox-staging/dosbox-staging/releases/download/v#{version}/dosbox-staging-macOS-v#{version}.dmg",
-      verified: "github.com/dosbox-staging/dosbox-staging/"
+  url "https://github.com/dosbox-staging/dosbox-staging/releases/download/v#{version}/dosbox-staging-macOS-v#{version}.dmg"
   name "DOSBox Staging"
-  desc "Modern continuation of DOSBox to run DOS games on current operating systems"
-  homepage "https://dosbox-staging.github.io/"
-
-  livecheck do
-    url "https://dosbox-staging.github.io/downloads/macos/"
-    regex(%r{href=.*/dosbox-staging-macOS-v(\d+\.\d+\.\d+)\.dmg}i)
-  end
+  desc "DOS game emulator"
+  homepage "https://github.com/dosbox-staging/dosbox-staging/"
 
   app "DOSBox Staging.app"
 
-  zap trash: [
-    "~/Library/Preferences/DOSBox",
-  ]
+  zap trash: "~/Library/Preferences/DOSBox"
 end
