cb7d85430bac9e521855b285451ad91a8101ba9a
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-02 15:11:28 -0400

    010-editor 13.0.2

--- Casks/010-editor.rb
+++ Casks/010-editor.rb
@@ -1,6 +1,6 @@
 cask "010-editor" do
-  version "13.0.1"
-  sha256 "8cf8c6b38a0d08b4308ebdc4cc7fabb4884c41b321a92becc4be0b87e7c3fafe"
+  version "13.0.2"
+  sha256 "8ad3e6ecbab3815ffcf914ac38ea207cfc0b81b76e4bbf04b075ccceb5433d65"
 
   url "https://download.sweetscape.com/010EditorMac64Installer#{version}.dmg"
   name "010 Editor"
