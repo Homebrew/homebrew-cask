eb6256c6e2279bd0733e3faf376e7b792a8a7cf7
DattatreyaReddy Panta <58727124+DattatreyaReddy@users.noreply.github.com>
2023-05-02 08:30:53 +0530

    Revert "tachidesk-sorayomi 0.5.4 (#146289)"
    This reverts commit 78109e05cc439b67eac79dc09cd345081fc8726f.

--- Casks/tachidesk-sorayomi.rb
+++ Casks/tachidesk-sorayomi.rb
@@ -1,6 +1,6 @@
 cask "tachidesk-sorayomi" do
-  version "0.5.4"
-  sha256 "e603c3a23deca26fa743fb2748f7cb43d4f146363e900a3dc2df84cf727720dd"
+  version "0.5.3"
+  sha256 "bec297de606e68cdb69661b38e140d9141e206410e06e9a5b7b1f01ffaec83bf"
 
   url "https://github.com/Suwayomi/Tachidesk-Sorayomi/releases/download/#{version}/tachidesk-sorayomi-#{version}-macos-x64.zip"
   name "Tachidesk Sorayomi"
