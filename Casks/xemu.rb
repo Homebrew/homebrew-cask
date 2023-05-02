70a69d2175d2924bcaaea7402c9fa18f4ff5f235
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-01 20:27:11 -0400

    xemu 0.7.87

--- Casks/xemu.rb
+++ Casks/xemu.rb
@@ -1,6 +1,6 @@
 cask "xemu" do
-  version "0.7.86"
-  sha256 "7d3c3e8f36bbefff0eadc205feef71afad9fa5c0504af318a54e374783ec605a"
+  version "0.7.87"
+  sha256 "6310974f4dfd8dc02706e2c13be479b5026f2a353399bf4f4922d0bfbe3ae159"
 
   url "https://github.com/xemu-project/xemu/releases/download/v#{version}/xemu-macos-universal-release.zip",
       verified: "github.com/xemu-project/xemu/"
