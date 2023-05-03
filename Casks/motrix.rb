cdf354103332f85a0eea64d84feb4918a32cf0dd
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-03 11:09:31 -0400

    motrix 1.8.19 (#146410)

--- Casks/motrix.rb
+++ Casks/motrix.rb
@@ -1,9 +1,9 @@
 cask "motrix" do
   arch arm: "-arm64"
 
-  version "1.6.11"
-  sha256 arm:   "1705d6ef4781c17ebf19007dbb2ac52f6227592c783e91ad5b81a1432cf6b668",
-         intel: "70fa245e44b8e0ec62f50ba5731bc0a876535a51dc0ea318010736d2a6be6dd9"
+  version "1.8.19"
+  sha256 arm:   "d73f4d69f3597ad8f98b821aa0fb58ba964bf85061b4a13f00edcb3618001c0e",
+         intel: "b644cc83aa98224147ef2942fd49ecfc8cdcebfce9616550fa35caa6850c4178"
 
   url "https://github.com/agalwood/Motrix/releases/download/v#{version}/Motrix-#{version}#{arch}.dmg",
       verified: "github.com/agalwood/Motrix/"
