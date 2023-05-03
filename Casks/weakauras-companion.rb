c2d27020895c47dd84884c3c6ed2d1597eab3527
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-05-03 23:07:15 +0800

    weakauras-companion 5.0.1 (#146405)

--- Casks/weakauras-companion.rb
+++ Casks/weakauras-companion.rb
@@ -1,6 +1,6 @@
 cask "weakauras-companion" do
-  version "5.0.0"
-  sha256 "2acd02c97e4bfef118f1cb8dee33806329224fc99e1e6a5c0003fb6605b6e78d"
+  version "5.0.1"
+  sha256 "9f6f56c7c75c15dd1d164b739d55da5c47384b1b35f6029aa47e26c0a6bc5a98"
 
   url "https://github.com/WeakAuras/WeakAuras-Companion/releases/download/v#{version}/WeakAuras-Companion-#{version}-mac-universal.dmg",
       verified: "github.com/WeakAuras/WeakAuras-Companion/"
