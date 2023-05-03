9c29cf5c02a9405d531ac7f3d5541c1d1269bdab
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-05-03 22:51:35 +0800

    zoc 8.05.3

--- Casks/zoc.rb
+++ Casks/zoc.rb
@@ -1,6 +1,6 @@
 cask "zoc" do
-  version "8.05.2"
-  sha256 "6b76ba88bee3956962fd19815281ce8817c36a077b552eb2b13762b31e7013f2"
+  version "8.05.3"
+  sha256 "d22122520164b7a440f012eed1e30ea8650bdc4c3d4ac2a34668ad9e9ebbfe51"
 
   url "https://www.emtec.com/downloads/zoc/zoc#{version.no_dots}.dmg"
   name "ZOC"
