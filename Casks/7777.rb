cba9369d09c283dd76f307d6337826c8dc684ce9
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-03 14:06:22 -0400

    7777 1.1.6 (#146414)
    * 7777 1.1.6
    
    * 7777 (mark no zap)
    
    * Add no zap required

--- Casks/7777.rb
+++ Casks/7777.rb
@@ -1,6 +1,6 @@
 cask "7777" do
-  version "1.1.4"
-  sha256 "e4e2a518e35aadb65e0e0b14eb2f343d1a56e63c7307bfe57b7956e453a5bfbc"
+  version "1.1.6"
+  sha256 "80005a7f70b95ac148fa3539dc00709edbb37af36f2d6ec03da6b6c8cee3a56d"
 
   url "https://releases.port7777.com/#{version}/macos/7777"
   name "7777"
@@ -13,4 +13,6 @@ cask "7777" do
   end
 
   binary "7777"
+
+  # No zap stanza required
 end
