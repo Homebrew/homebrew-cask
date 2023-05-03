d76164c9148e80fc15fafed3a409838fdcec0824
Yoon_JW <77672508+grabic1060@users.noreply.github.com>
2023-05-04 04:17:44 +0900

    entry: new cask (#146367)
    Co-authored-by: Razvan Azamfirei <azamfireirazvan@gmail.com>

---
+++ Casks/entry.rb
@@ -0,0 +1,25 @@
+cask "entry" do
+  version "2.1.3"
+  sha256 "7193585548b619d882d1d7ceeff48e4ff0e1e20f2625aa6ba86084197a8a5e47"
+
+  url "https://playentry.org/uploads/data/installers/Entry-#{version}.pkg"
+  name "entry"
+  desc "Block-based coding platform"
+  homepage "https://playentry.org/"
+
+  livecheck do
+    url "https://github.com/entrylabs/entry-offline/"
+    strategy :github_latest
+  end
+
+  pkg "Entry-#{version}.pkg"
+
+  uninstall pkgutil: "org.playentry.entry"
+
+  zap trash: [
+    "~/Library/Application Support/Entry",
+    "~/Library/Application Support/entry-hw",
+    "~/Library/Preferences/org.playentry.entry.plist",
+    "~/Library/Saved Application State/org.playentry.entry.savedState",
+  ]
+end
