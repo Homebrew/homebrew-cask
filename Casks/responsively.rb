f8fc001804257f77ace4b604dc00512fd2bf4c54
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-05-03 23:03:10 +0800

    responsively 1.2.0

--- Casks/responsively.rb
+++ Casks/responsively.rb
@@ -1,9 +1,9 @@
 cask "responsively" do
   arch arm: "-arm64"
 
-  version "1.1.0"
-  sha256 arm:   "18c9e27010fececf587448b7c5473161940f818b48144427bd803282930fe5f3",
-         intel: "a11d0e577e8c8cb17d6e616d16a786b25426ebb478cb465c2ca9db447c476a17"
+  version "1.2.0"
+  sha256 arm:   "2ad06d4383e5188b2e45283cbc8ae676fd8cb7cc57e4b8cdd260da52442e0be0",
+         intel: "32362dcada974de73f3b9f22d717b26a418426d61475f5f53c765053bd3d2f8f"
 
   url "https://github.com/responsively-org/responsively-app-releases/releases/download/v#{version}/ResponsivelyApp-#{version}#{arch}.dmg",
       verified: "github.com/responsively-org/responsively-app-releases/"
