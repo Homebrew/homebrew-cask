9f04e016a121769f9727c94b4f28bff76def68e9
vvvvv <vvvvv@vvvvv.io>
2023-05-03 11:53:02 +0200

    feat: added a way to upgrade wezterm@nightly
    - added livecheck which is used to determine
      whether an upgrade is executed or not.
    - this livecheck uses the last-modified http header
      which is set to the date/time the file
      was uploaded to see if there is a new version

--- Casks/w/wezterm@nightly.rb
+++ Casks/w/wezterm@nightly.rb
@@ -8,6 +8,27 @@ cask "wezterm@nightly" do
   desc "GPU-accelerated cross-platform terminal emulator and multiplexer"
   homepage "https://wezfurlong.org/wezterm/"
 
+  livecheck do
+    url :url
+    strategy :header_match do |headers|
+      # matches last-modified header syntax
+      regex = /(\w{3}, )?(\d{1,2}) (\w{3}) (\d{4}) (\d{2}):(\d{2}):(\d{2}) GMT/
+      match = headers["last-modified"].match(regex)
+      match = headers["x-ms-creation-time"].match(regex) if match.nil?
+
+      year = match[4]
+      month_idx = %w[jan feb mar apr may jun jul aug sep oct nov dec].index do |month|
+        month == match[3].downcase
+      end
+      month = (month_idx ? month_idx + 1 : 0).to_s.rjust(2, "0")
+      day = match[2].rjust(2, "0")
+      hour = match[5].rjust(2, "0")
+      minute = match[6].rjust(2, "0")
+      second = match[7].rjust(2, "0")
+      "#{year}#{month}#{day}#{hour}#{minute}#{second}"
+    end
+  end
+
   conflicts_with cask: "wezterm"
 
   app "WezTerm.app"
