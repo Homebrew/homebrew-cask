9b1d5f7ad7ec7266e772bdc686325b1bf5d9babc
Aaron Jheng <wentworth@outlook.com>
2023-05-03 22:23:54 +0800

    ppsspp 1.15.1 (#146375)

--- Casks/ppsspp.rb
+++ Casks/ppsspp.rb
@@ -1,6 +1,6 @@
 cask "ppsspp" do
-  version "1.15"
-  sha256 "e52bc9ed93f75ee758baca08dde55cb9d46a3f8b2debe95c55fe6418eb7c7fed"
+  version "1.15.1"
+  sha256 "f197277bca9b408d955dd7def7f47f1a45fb3b59e0aa55145545b518dc6c0e10"
 
   url "https://github.com/hrydgard/ppsspp/releases/download/v#{version}/PPSSPPSDL-macOS-v#{version}.zip",
       verified: "github.com/hrydgard/ppsspp/"
