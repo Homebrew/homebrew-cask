3307c3a91efb5a45473e608950960792343efcc1
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-03 18:50:20 -0400

    hiarcs-chess-explorer (rework)
    * Adjust livecheck url to support optional [a-z] at version end
    
    * Add zap stanza
    
    * Add legacy versions, remove depends_on macOS version

--- Casks/hiarcs-chess-explorer.rb
+++ Casks/hiarcs-chess-explorer.rb
@@ -1,21 +1,40 @@
 cask "hiarcs-chess-explorer" do
-  version "1.12.2"
-  sha256 "a77ed22e80ef8b85c65e7f74e72876b12b42525b1ae45bcdce656e3f40b1418f"
+  on_sierra :or_older do
+    version "1.9.4"
+    sha256 "eaf7627801ca4cc2351fef58bb313353eca2a51d894e28df2b627dd011856a7f"
+
+    livecheck do
+      skip "Legacy version"
+    end
+  end
+  on_high_sierra do
+    version "1.11.1"
+    sha256 "6f188f9c9041ed5667f0398e7b2a9b00d998bd6c77e9391163895bbd746f49ee"
+
+    livecheck do
+      skip "Legacy version"
+    end
+  end
+  on_mojave :or_newer do
+    version "1.12.2"
+    sha256 "a77ed22e80ef8b85c65e7f74e72876b12b42525b1ae45bcdce656e3f40b1418f"
+
+    livecheck do
+      url "https://www.hiarcs.com/mac-chess-explorer-download.htm"
+      regex(%r{href=.*?/HIARCS-Chess-Explorer-Installer[._-]v?(\d+(?:\.\d+)+)[a-z]?\.pkg}i)
+    end
+  end
 
   url "https://www.hiarcs.com/hce/HIARCS-Chess-Explorer-Installer-v#{version}.pkg"
   name "(Deep) HIARCS Chess Explorer"
   desc "Chess database, analysis and game playing program"
   homepage "https://www.hiarcs.com/mac-chess-explorer.htm"
 
-  livecheck do
-    url "https://www.hiarcs.com/mac-chess-explorer-download.htm"
-    regex(%r{href=.*?/HIARCS-Chess-Explorer-Installer[._-]v?(\d+(?:\.\d+)+)\.pkg}i)
-  end
-
-  depends_on macos: ">= :high_sierra"
-
   pkg "HIARCS-Chess-Explorer-Installer-v#{version}.pkg"
 
   uninstall signal:  ["TERM", "com.hiarcs.chessexplorer"],
             pkgutil: "com.hiarcs.*"
+
+  zap trash: "~/Library/Preferences/com.hiarcs.Chess Explorer.plist",
+      rmdir: "~/Documents/HIARCS Chess"
 end
