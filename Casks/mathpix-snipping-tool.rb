85a49db89c090f765085c84bc34adb543e924717
Miccal Matthews <miccal.matthews@gmail.com>
2023-05-05 07:55:24 +0800

    mathpix-snipping-tool 3.4.3.4 (#146504)

--- Casks/mathpix-snipping-tool.rb
+++ Casks/mathpix-snipping-tool.rb
@@ -1,15 +1,17 @@
 cask "mathpix-snipping-tool" do
-  version "3.4.2,342.4"
-  sha256 "dfbd8aef66c5eb9e06f7f6cb6935d90a907d00876e98cd39d9e344e03a0f5879"
+  version "3.4.3.4"
+  sha256 "f72e0e8fa1cf824a1365ac1b9b62c7ca31e45c64cb6136d13cb64d69fae0d78e"
 
-  url "https://mathpix.com/dmg/SnippingTool-v#{version.csv.first}.#{version.csv.second}.dmg"
+  url "https://mathpix.com/dmg/SnippingTool-v#{version}.dmg"
   name "Mathpix Snipping Tool"
   desc "Scanner app for math and science"
   homepage "https://mathpix.com/"
 
   livecheck do
     url "https://mathpix.com/appcast.xml"
-    strategy :sparkle
+    strategy :sparkle do |item|
+      item.url[/SnippingTool[._-]v?(\d+(?:\.\d+)+)\.dmg/i, 1]
+    end
   end
 
   auto_updates true
