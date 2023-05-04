c841873830c4515aada54bf48a148d01a924f58f
Zhenfu Shi <szf1234@me.com>
2023-05-04 14:43:03 -0400

    browserosaurus 20.3.0 (#146496)

--- Casks/browserosaurus.rb
+++ Casks/browserosaurus.rb
@@ -1,9 +1,9 @@
 cask "browserosaurus" do
   arch arm: "arm64", intel: "x64"
 
-  version "20.2.2"
-  sha256 arm:   "4735b26fa13d896d82dfe8915563196b02955d9a31d271954772be9d6f6f7a53",
-         intel: "e772cb4803f6868c7d58b85c5e4b5666afb8205367e4fcb099936da3583a599f"
+  version "20.3.0"
+  sha256 arm:   "8995cd6f888fd44c7771b6121b5dd6b887f09c85076aab6a1c67197716d3134d",
+         intel: "4281ae5e8a40f6422a7773d542ef945eae4dfa75990143f8f341c1509fbe724e"
 
   url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-darwin-#{arch}-#{version}.zip"
   name "Browserosaurus"
