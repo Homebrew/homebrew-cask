2ac4ecf748751231eb978f3974fbacc22a74e3e2
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-04 13:35:53 -0400

    pgadmin4 7.1

--- Casks/pgadmin4.rb
+++ Casks/pgadmin4.rb
@@ -1,6 +1,6 @@
 cask "pgadmin4" do
-  version "7.0"
-  sha256 "a3526a759dbb3687b679833736ee6abbeea9743d028db1268d13ee40359a2a2b"
+  version "7.1"
+  sha256 "e4bd958d9bc739a5082c87c34dec0e7f1e63d555bc11f76a034ec51dcb40fb09"
 
   url "https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v#{version}/macos/pgadmin4-#{version}.dmg",
       verified: "ftp.postgresql.org/pub/pgadmin/pgadmin4/"
