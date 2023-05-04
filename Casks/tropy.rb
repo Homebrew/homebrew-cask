6c66dbfaf1eb77f8db733eb65b82a34eb75c21b7
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-04 09:34:44 -0400

    tropy 1.13.1

--- Casks/tropy.rb
+++ Casks/tropy.rb
@@ -1,9 +1,9 @@
 cask "tropy" do
   arch arm: "-arm64"
 
-  version "1.13.0"
-  sha256 arm:   "ba589ff69c43d80eea4615e8a23f0a4ad4b2da22a820bffa9a95acf9866e02d9",
-         intel: "8c94ae5e2dd93065cc7ce8af61a5f58a36b65251fe69e51ec10b2f114d2d5a42"
+  version "1.13.1"
+  sha256 arm:   "5a189520ce2c51de5d7832073238201521bf6c439ab885447adb247bc8e4eedb",
+         intel: "bfa695f3c2b8b99977952433b2dfb16dce8cc751f756a4c3dd8d12d23bc8f05e"
 
   url "https://github.com/tropy/tropy/releases/download/v#{version}/tropy-#{version}#{arch}.dmg",
       verified: "github.com/tropy/tropy/"
