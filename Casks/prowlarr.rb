cc7fb467d2490fd30ce5f9878fa97b7edfdee194
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-04 12:44:29 -0400

    prowlarr 1.4.1.3258 (#146493)

--- Casks/prowlarr.rb
+++ Casks/prowlarr.rb
@@ -1,9 +1,9 @@
 cask "prowlarr" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.3.2.3006"
-  sha256 arm:   "2a7b184b2e8937d37a5bd8f46b0a50ff72e1e4610ea20d7e7c0e80cb8de2f9a7",
-         intel: "643cadcdedc370f849dcb3750c7dd64ec10eb19f601e841c57b24b16d933d84d"
+  version "1.4.1.3258"
+  sha256 arm:   "cbb2d03c8eeb1e9d0fd3b4777d39e57643637eeb502cbb466ca9f7365f27c2c2",
+         intel: "fa81739389548618cd7fd61b852270f1dc79da3fdc39e018ba17e9c004b59b1c"
 
   url "https://github.com/Prowlarr/Prowlarr/releases/download/v#{version}/Prowlarr.master.#{version}.osx-app-core-#{arch}.zip",
       verified: "github.com/Prowlarr/Prowlarr/"
