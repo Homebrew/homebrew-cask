fb317df82b0953fa0bb82c5ba77501d81c0e815a
Bevan Kay <email@bevankay.me>
2023-05-04 11:11:10 +1000

    Add arch

--- Casks/volta.rb
+++ Casks/volta.rb
@@ -1,16 +1,19 @@
 cask "volta" do
-  version "1.15.0"
-  sha256 "75b85ccd92474dfd5f15f3b83e1148b891551d0eccddae073e84e290d2b1edaf"
+  arch arm: "-arm64"
 
-  url "https://volta-build.s3.fr-par.scw.cloud/Volta-#{version}.dmg",
+  version "1.15.0"
+  sha256 intel: "75b85ccd92474dfd5f15f3b83e1148b891551d0eccddae073e84e290d2b1edaf",
+         arm:   "7c73cd19c9622db1039b8357c0bd04d11c1062f8cf85013bc7641a70da4cfaff"
+
+  url "https://volta-build.s3.fr-par.scw.cloud/Volta#{arch}-#{version}.dmg",
       verified: "volta-build.s3.fr-par.scw.cloud/"
   name "Volta"
   desc "GitHub issues and notifications"
   homepage "https://volta.net/"
 
   livecheck do
-    url "https://volta.net/changelog"
-    regex(%r{/changelog/v?(\d+(?:\.\d+)+)})
+    url "https://s3.fr-par.scw.cloud/volta-build/latest-mac.yml"
+    strategy :electron_builder
   end
 
   app "Volta.app"
