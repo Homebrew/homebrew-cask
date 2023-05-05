5cae9ba7ade51956704f9126b2735b1f03e6bcf2
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-05-05 19:04:42 +0800

    piclist 1.8.2

--- Casks/piclist.rb
+++ Casks/piclist.rb
@@ -1,9 +1,9 @@
 cask "piclist" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.8.1"
-  sha256 arm:   "9d851018500a48a5d11436f921a616eb3725523986633cfcd7183042ee20d6ac",
-         intel: "c26a20b2033f05b26c6998a520685e22ed8ec47351a5c3a22e3f3047b69abfbf"
+  version "1.8.2"
+  sha256 arm:   "965b667954a16bef0befcb7120b5155d9f4322cdf626812d4ce069698beadbe5",
+         intel: "342e071659d186ff0150bef92ea047c7ed2e10802cade5f8a169990e3b0597b4"
 
   url "https://release.piclist.cn/latest/PicList-#{version}-#{arch}.dmg"
   name "PicList"
