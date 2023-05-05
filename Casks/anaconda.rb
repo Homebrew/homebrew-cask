9a89f698211798618977ad3f0e8bc9ee13615a5e
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-05 10:17:10 -0400

    anaconda: rework cask

--- Casks/anaconda.rb
+++ Casks/anaconda.rb
@@ -2,10 +2,10 @@ cask "anaconda" do
   arch arm: "arm64", intel: "x86_64"
 
   version "2023.03-1"
-  sha256 arm:   "85152324c423fedbeed2e7491cb32e597eaeb1b86ae7a61ff7597b401fd053ce",
-         intel: "3593921c8a5516db82f0d7dd1c691f7ee7794236852e7da614e9ad6e93eeb342"
+  sha256 arm:   "d22ab7a22ab4ba3c02d6fe4e9c2a9c673ff34b80442922e4e49663287f6ace3f",
+         intel: "561ea77b7172e15568d21b854c4de4178789ca59caca16af9a6449653bfd9a21"
 
-  url "https://repo.anaconda.com/archive/Anaconda3-#{version}-MacOSX-#{arch}.sh"
+  url "https://repo.anaconda.com/archive/Anaconda3-#{version}-MacOSX-#{arch}.pkg"
   name "Continuum Analytics Anaconda"
   desc "Distribution of the Python and R programming languages for scientific computing"
   homepage "https://www.anaconda.com/"
@@ -16,22 +16,18 @@ cask "anaconda" do
   end
 
   auto_updates true
-  container type: :naked
 
-  installer script: {
-    executable: "Anaconda3-#{version}-MacOSX-#{arch}.sh",
-    args:       ["-b", "-p", "#{HOMEBREW_PREFIX}/anaconda3"],
-    sudo:       true,
-  }
+  pkg "Anaconda3-#{version}-MacOSX-#{arch}.pkg"
 
-  postflight do
-    set_ownership "#{HOMEBREW_PREFIX}/anaconda3"
-  end
-
-  uninstall delete: [
-    "#{HOMEBREW_PREFIX}/anaconda3",
-    "/Applications/Anaconda-Navigator.app",
+  uninstall pkgutil: [
+    "com.anaconda.io",
+    "com.anaconda.AnacondaCloudPlugin",
   ]
+
+  # uninstall delete: [
+  #   "#{HOMEBREW_PREFIX}/anaconda3",
+  #   "/Applications/Anaconda-Navigator.app",
+  # ]
 
   zap trash: [
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.continuum.io.sfl*",
