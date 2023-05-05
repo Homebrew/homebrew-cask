23570257255427e6cc62d4be700182f39ff5052f
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-05-05 19:07:29 +0800

    pitch 1.122.0,stable.4,5459451

--- Casks/pitch.rb
+++ Casks/pitch.rb
@@ -1,6 +1,6 @@
 cask "pitch" do
-  version "1.122.0,stable.3,5454710"
-  sha256 "bdde0535077992a3710097dd632ce43a567bc0fdf39a16c9d2a5112d5a004048"
+  version "1.122.0,stable.4,5459451"
+  sha256 "d6db91c91ea3be4e054d9798df075f948e6183f5f4fb5f7417c1c26225daaaf9"
 
   url "https://desktop-app-builds.pitch.com/Pitch-#{version.csv.first}-#{version.csv.second}-ci#{version.csv.third}.dmg"
   name "Pitch"
