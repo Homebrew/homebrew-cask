abaff1757fbbe085e9b8d912add06b88bfab08a5
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-05 06:59:07 -0400

    update hashes

--- Casks/orion.rb
+++ Casks/orion.rb
@@ -2,7 +2,7 @@ cask "orion" do
   version "0.99,124"
 
   on_mojave :or_older do
-    sha256 "7164c435e517f7c354c4bbe37d7524e924fdfda51c6368838c01b45edcd955b7"
+    sha256 "5a83ddbbfcb3ebaabd67ee4daa1b875ceeed15bdb47c3775946f6ee62e81557a"
 
     url "https://browser.kagi.com/updates/10_14/#{version.csv.second}.zip"
 
@@ -14,7 +14,7 @@ cask "orion" do
     depends_on macos: ">= :mojave"
   end
   on_catalina do
-    sha256 "2279a4bfddc6b5e4a5c3d96e45d4bc7da878a3363601ca5a662d4f87941de132"
+    sha256 "766fd81fb5dc60624e2bbd3fcfc95710eab1bf4e42f6e59858d3901afa7a201d"
 
     url "https://browser.kagi.com/updates/10_15/#{version.csv.second}.zip"
 
@@ -26,7 +26,7 @@ cask "orion" do
     depends_on macos: :catalina
   end
   on_big_sur do
-    sha256 "c15aff1f0f419ca02440983c242292a73ec61a316b2fa5490c8fa81a402723bd"
+    sha256 "7bb3ff7d36c97fa512f4858ca78bf25cd8ab6cd177f33be5e9d38f6c34cb3f13"
 
     url "https://browser.kagi.com/updates/11_0/#{version.csv.second}.zip"
 
@@ -38,7 +38,7 @@ cask "orion" do
     depends_on macos: :big_sur
   end
   on_monterey do
-    sha256 "e6f6056025309e54b7b2f96129b023d4d9b26065e24f4db451c6b866986018a5"
+    sha256 "ebb15c1cf88cc72c12817ac66ea85df3304c982cf599206c6d3ce63fcbf7b953"
 
     url "https://browser.kagi.com/updates/12_0/#{version.csv.second}.zip"
 
