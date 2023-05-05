8da92a285f1817025dfa55071f89f07d17cab109
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-05-05 21:09:00 +0800

    bruno 0.12.2 (#146561)

--- Casks/bruno.rb
+++ Casks/bruno.rb
@@ -1,9 +1,9 @@
 cask "bruno" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.12.1"
-  sha256 arm:   "6c6703880f342faf90c9e1771ce9c98a2c2c27e6a1ba55906edb13134bb2dea8",
-         intel: "1b52f1c6454d6a5939565956cb2e074f0a2ec2acc72508f9fb86c62ac3913866"
+  version "0.12.2"
+  sha256 arm:   "8f25066224e2397a14d2b43215abc36de77adba30faa127a06e3b6bb00b44184",
+         intel: "ab69eb1fd78062549b1744e8b1a3668d66646bcc4146c46e881cfb0c61c7061e"
 
   url "https://github.com/usebruno/bruno/releases/download/v#{version}/bruno_#{version}_#{arch}_mac.dmg",
       verified: "github.com/usebruno/bruno/"
