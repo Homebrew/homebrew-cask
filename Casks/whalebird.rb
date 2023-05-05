111b943d12713774f751ed98bdf0f376558aaac2
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-04 20:45:33 -0400

    whalebird 5.0.5

--- Casks/whalebird.rb
+++ Casks/whalebird.rb
@@ -1,11 +1,11 @@
 cask "whalebird" do
   arch arm: "arm64", intel: "x64"
 
-  version "5.0.4"
-  sha256 arm:   "8a2b174392e63d9244f8fa6f32d2033e8f91ed4c24510dbe8df44578e09cc31f",
-         intel: "c8fd297d9e217c26743a482870ff3168ea6ddf30770d70205ad2a1ee14107c01"
+  version "5.0.5"
+  sha256 arm:   "72e7d9bfffe5636b9d80ae8ad7f72d553494a4b1814af0b32943dc031dca27ea",
+         intel: "54009176ff7853faf69649b84079b31d1971667de988b33dbb24598c33560b76"
 
-  url "https://github.com/h3poteto/whalebird-desktop/releases/download/v#{version}/Whalebird-#{version}-darwin-#{arch}.dmg",
+  url "https://github.com/h3poteto/whalebird-desktop/releases/download/v#{version}/Whalebird-#{version}-mac-#{arch}.dmg",
       verified: "github.com/h3poteto/whalebird-desktop/"
   name "Whalebird"
   desc "Mastodon, Pleroma, and Misskey client"
