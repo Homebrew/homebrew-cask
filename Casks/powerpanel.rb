686436f9c41178445cd1a31de35ae1ca7df4a483
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-05 20:13:28 -0400

    powerpanel: 2.4.8 (#146586)

--- Casks/powerpanel.rb
+++ Casks/powerpanel.rb
@@ -1,8 +1,8 @@
 cask "powerpanel" do
-  version "2.4.6"
-  sha256 "85e4bcdaf178e3f2919aff1ad638bc9e80dcdbfa3a868486613aafb0d775c8a8"
+  version "2.4.8"
+  sha256 "b3517556c2fb9b6dfc0b912ec60b10f12ce54d7075f43d2d727a48d3e264f1b0"
 
-  url "https://dl4jz3rbrsfum.cloudfront.net/software/PPP_Mac_v#{version}.dmg",
+  url "https://dl4jz3rbrsfum.cloudfront.net/software/ppp_macos_#{version.dots_to_underscores}.dmg",
       verified: "dl4jz3rbrsfum.cloudfront.net/"
   name "CyberPower PowerPanel Personal"
   desc "Manage and control UPS systems"
@@ -10,7 +10,10 @@ cask "powerpanel" do
 
   livecheck do
     url "https://www.cyberpowersystems.com/product/software/power-panel-personal/powerpanel-personal-mac/"
-    regex(%r{href=.*?/PPP_Mac[._-]v?(\d+(?:.\d+)*)\.dmg}i)
+    regex(%r{href=.*?/ppp_macos[._-]v?(\d+(?:_\d+)*)\.dmg}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| match[0].tr("_", ".").to_s }
+    end
   end
 
   depends_on macos: ">= :mojave"
@@ -20,4 +23,13 @@ cask "powerpanel" do
   uninstall pkgutil:   "com.cpsww.ppupsd",
             launchctl: "com.cpsww.ppupsd",
             delete:    "/Applications/CyberPower PowerPanel Personal"
+
+  zap trash:  [
+        "~/Library/Preferences/PowerPanel Personal.plist",
+        "~/Library/Saved Application State/PowerPanel Personal.savedState",
+      ],
+      delete: [
+        "/Library/LaunchAgents/com.cyberpower.powerpanel-personal.client.plist",
+        "/Library/LaunchDaemons/com.cyberpower.powerpanel-personal.daemon.plist",
+      ]
 end
