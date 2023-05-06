1c7fd69c9523e732946a39a3532fc7ac54764c11
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-06 18:17:03 -0400

    gpxsee 13.1 (#146636)
    \n

--- Casks/gpxsee.rb
+++ Casks/gpxsee.rb
@@ -1,6 +1,6 @@
 cask "gpxsee" do
-  version "12.4"
-  sha256 "56df808deb49feaa53960b16cc099117c91dd15596cc9a68ec7525c54985c504"
+  version "13.1"
+  sha256 "1cc330473ecc04e884e39fc119c7c6078c3443ba188ff2139589b04551e5fd28"
 
   url "https://downloads.sourceforge.net/gpxsee/Mac%20OS%20X/GPXSee-#{version}.dmg",
       verified: "sourceforge.net/gpxsee/Mac%20OS%20X/"
