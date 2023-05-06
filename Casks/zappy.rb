2cb627761d2a416636ce4a578428665f7d57c1b7
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-06 12:03:12 -0400

    zappy 3.3.1 (#146625)

--- Casks/zappy.rb
+++ Casks/zappy.rb
@@ -1,6 +1,6 @@
 cask "zappy" do
-  version "3.3.0"
-  sha256 "81d9268658b234b4e103af368448b313e05a6d8354d26a99831df4cb161cafc0"
+  version "3.3.1"
+  sha256 "bbf1e22c53525336fa5589240ec0745e71736a421d6753ea5023c5af1b9bda90"
 
   url "https://zappy.zapier.com/releases/zappy-#{version}.dmg"
   name "Zappy"
