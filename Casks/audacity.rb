041752ce10d7afe6fc934d7470bb4a08e36ebb6b
Hannes Braun <hannes@hannesbraun.net>
2023-05-06 01:12:51 +0100

    audacity 3.3.2 (#146597)

--- Casks/audacity.rb
+++ Casks/audacity.rb
@@ -1,9 +1,9 @@
 cask "audacity" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "3.3.1"
-  sha256 arm:   "b06126b34a8011c47c36c738587ef100c40013fabb3c974f2d8cb5c5eca88e74",
-         intel: "2ecd1f8be1d976eac4bc2fa3fd893fea8f40ec856cd7e8d913c35ba33f62396d"
+  version "3.3.2"
+  sha256 arm:   "1f85c9b476ff52f9c30ae5bb1a74ed8e98b9c6d28714080af8bd09fb6b9a50bf",
+         intel: "3b876e40eebe6430de9659a4c4ce1b18c5941d0fb1d4e9bc491792e37f73914e"
 
   url "https://github.com/audacity/audacity/releases/download/Audacity-#{version}/audacity-macOS-#{version}-#{arch}.dmg",
       verified: "github.com/audacity/audacity/"
