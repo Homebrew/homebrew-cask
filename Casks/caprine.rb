de5bdfab9e613cca5be04046a5a800d102869731
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-05-07 02:30:56 +0800

    caprine 2.57.4

--- Casks/caprine.rb
+++ Casks/caprine.rb
@@ -1,6 +1,6 @@
 cask "caprine" do
-  version "2.57.3"
-  sha256 "5a9d6fd78da0860d3ddb5a692edd4c28b2408a6055f6f7a34146941131c487a6"
+  version "2.57.4"
+  sha256 "9a7f80442bf3c07cd2fb702f91473918c9abb2e4c3b5c909b278d5bf3b4f8cfc"
 
   url "https://github.com/sindresorhus/caprine/releases/download/v#{version}/Caprine-#{version}.dmg"
   name "Caprine"
