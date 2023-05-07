bff7149c8beffbeb79234739bc8a8a7866f87209
Moulick Aggarwal <Moulick@users.noreply.github.com>
2023-05-07 12:18:39 +0200

    stats 2.8.20 (#146653)

--- Casks/stats.rb
+++ Casks/stats.rb
@@ -1,6 +1,6 @@
 cask "stats" do
-  version "2.8.19"
-  sha256 "85760def85acd09f07b3a58fc54cffa6d6f97266b6a3d5adfa6b0e9659507944"
+  version "2.8.20"
+  sha256 "9b1c8b4321287d59c756d0da798fc5d504cbc1d1f3050ed1e9d33199727e774c"
 
   url "https://github.com/exelban/stats/releases/download/v#{version}/Stats.dmg"
   name "Stats"
