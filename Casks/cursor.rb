2007e9c2f13f591d13742e7c87699e05bccc5c94
Edward <73746306+WangEdward@users.noreply.github.com>
2023-05-07 11:46:33 +0800

    Update Cursor to `0.2.10`
    - Update Cursor to version `0.2.10`
    - Include sha256 hashes
    - Change download URL and use electron_builder strategy for livecheck
    - Set `auto_updates` to `true`
    - Remove installer script and use app instead

--- Casks/cursor.rb
+++ Casks/cursor.rb
@@ -1,26 +1,25 @@
 cask "cursor" do
-  version "0.1.11"
-  sha256 :no_check
+  arch arm: "arm64", intel: "x64"
 
-  url "https://dl.todesktop.com/230313mzl4w4u92/mac/installer/universal",
-      verified: "dl.todesktop.com/230313mzl4w4u92/mac/installer/"
+  version "0.2.10"
+  sha256 arm:   "a383df69c4eb19ad130a38c41d04e6cde2017b2d49ea85020b2496eb31698455",
+         intel: "580ce4889a94cf359a61308437be05dc288cc8b8b735614cb7707dc94b270a1a"
+
+  url "https://download.todesktop.com/230313mzl4w4u92/Cursor%20#{version}-#{arch}-mac.zip",
+      verified: "download.todesktop.com/230313mzl4w4u92"
   name "Cursor"
   desc "Write, edit, and chat about your code with AI"
   homepage "https://www.cursor.so/"
 
   livecheck do
-    url "https://raw.githubusercontent.com/getcursor/cursor/main/package.json"
-    strategy :json do |json|
-      json["version"]
-    end
+    url "https://download.todesktop.com/230313mzl4w4u92/latest-mac.yml"
+    strategy :electron_builder
   end
 
+  auto_updates true
   depends_on macos: ">= :catalina"
 
-  installer script: "Install Cursor.app/Contents/MacOS/ToDesktop Installer"
-
-  uninstall quit:   "com.todesktop.230313mzl4w4u92",
-            delete: "/Applications/Cursor.app"
+  app "Cursor.app"
 
   zap trash: [
     "~/cursor-tutor",
