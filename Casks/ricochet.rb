bd273abb9e12b1be660e92542c4ec808742d7335
Sam Ford <1584702+samford@users.noreply.github.com>
2023-05-08 16:21:35 -0400

    ricochet: update homepage

--- Casks/ricochet.rb
+++ Casks/ricochet.rb
@@ -2,11 +2,10 @@ cask "ricochet" do
   version "1.1.4.1"
   sha256 "e5fbcbebe51fa52d9443fd2a07714d0e6e087c314a9c3eecf73ed4da8ca9e8aa"
 
-  url "https://github.com/ricochet-im/ricochet/releases/download/v#{version.major_minor_patch}/ricochet-#{version}-macos.dmg",
-      verified: "github.com/ricochet-im/"
+  url "https://github.com/ricochet-im/ricochet/releases/download/v#{version.major_minor_patch}/ricochet-#{version}-macos.dmg"
   name "Ricochet"
   desc "Anonymous peer-to-peer instant messaging"
-  homepage "https://ricochet.im/"
+  homepage "https://github.com/ricochet-im/ricochet"
 
   # Upstream sometimes replaces a release asset and bumps the version, so it
   # doesn't correspond to the tagged version anymore. For example, they may
