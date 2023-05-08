e11abbc7358dc625043871ab4223334f09129709
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-08 16:17:57 -0400

    responsively 1.2.2

--- Casks/responsively.rb
+++ Casks/responsively.rb
@@ -1,9 +1,9 @@
 cask "responsively" do
   arch arm: "-arm64"
 
-  version "1.2.1"
-  sha256 arm:   "cf1b9ccc7cb3d7cbe48894c263fb13b8facaed6e9deb734f08aa3991597e99a6",
-         intel: "b0880075207f6409bb0de56538dd8d0816fb042dfa64540971812a5d57f1e514"
+  version "1.2.2"
+  sha256 arm:   "6e93c83902774181c0bfd7a575c70695314cd782fd14b3d01ee58f0b38f579c8",
+         intel: "7b1484835b05fe4e25c93738f7cbe269845fd4e3870792a1e74c9bb644c6c5a1"
 
   url "https://github.com/responsively-org/responsively-app-releases/releases/download/v#{version}/ResponsivelyApp-#{version}#{arch}.dmg",
       verified: "github.com/responsively-org/responsively-app-releases/"
