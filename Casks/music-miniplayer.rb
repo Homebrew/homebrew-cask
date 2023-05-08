5cfbbd03155676975340ab048214ab7c32fc4599
Danny Dorazio <danny@dorazio.me>
2023-05-08 17:27:54 -0400

    Modify formatting of zap array

--- Casks/music-miniplayer.rb
+++ Casks/music-miniplayer.rb
@@ -11,5 +11,9 @@ cask "music-miniplayer" do
 
   app "Music MiniPlayer.app"
 
-  zap trash: ["~/Library/Preferences/com.marioaguzman.Music-MiniPlayer.plist", "~/Library/Saved Application State/com.marioaguzman.Music-MiniPlayer.savedState", "~/Library/Caches/com.marioaguzman.Music-MiniPlayer"]
+  zap trash: [
+    "~/Library/Preferences/com.marioaguzman.Music-MiniPlayer.plist",
+    "~/Library/Saved Application State/com.marioaguzman.Music-MiniPlayer.savedState",
+    "~/Library/Caches/com.marioaguzman.Music-MiniPlayer",
+  ]
 end
