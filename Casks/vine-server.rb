3aad82664f5662a9fe2490ece052fcb968fd22f8
Sean Molenaar <smillerdev@me.com>
2023-05-08 18:12:55 +0200

    vine-server: fix livecheck for github API

--- Casks/vine-server.rb
+++ Casks/vine-server.rb
@@ -9,7 +9,7 @@ cask "vine-server" do
 
   livecheck do
     url :url
-    regex(%r{href=["']?[^"' >]*?/tag/[^"' >]*?(\d+(?:[._-]\d+)+[a-z]?)["' >]}i)
+    regex(/(\d+(?:[._-]\d+)+[a-z]?)/i)
     strategy :github_latest do |page, regex|
       page.scan(regex).map { |match| match[0].tr("_", ".") }
     end
