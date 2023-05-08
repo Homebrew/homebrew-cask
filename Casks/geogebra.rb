435b95969a42f3afa3efdb5e9271f8dfafe4dfde
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-05-09 05:07:36 +0800

    geogebra 6.0.776.0 (#146751)

--- Casks/geogebra.rb
+++ Casks/geogebra.rb
@@ -1,6 +1,6 @@
 cask "geogebra" do
-  version "6.0.775.0"
-  sha256 "782f9079fb298ea2de7c501db97da8946675dac974711d3508fa3f114dd37ad1"
+  version "6.0.776.0"
+  sha256 "3f359a0839303dab7f18d0ca8d43efae8eb764f7fdb75ca8d857b678c50fc731"
 
   url "https://download.geogebra.org/installers/#{version.major_minor}/GeoGebra-Classic-#{version.major}-MacOS-Portable-#{version.dots_to_hyphens}.zip"
   name "GeoGebra"
