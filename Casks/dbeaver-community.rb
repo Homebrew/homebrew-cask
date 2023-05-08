fdbcfd0213705933a664fca5d2694d7c200aab23
Kai Winter <kaiwinter@gmx.de>
2023-05-08 09:59:57 +0200

    dbeaver-community 23.0.4

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,9 +1,9 @@
 cask "dbeaver-community" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "23.0.3"
-  sha256 arm:   "ecce3eb5e84ff773675e92a67db0f799cc290bd9bbd744df1008dad5fd1232ea",
-         intel: "d41e3f2cc44b30fc74a0870e9afd819e6f674887d024ca8f3bf8bce64ae0866f"
+  version "23.0.4"
+  sha256 arm:   "76d8fceedb87197c1d4d9f6cd3fb652626ad9f0af5a3e58728d4cc6065ab68c9",
+         intel: "9a250b7489b51a15a0f320db2807f61bf87c3ec36f911cbcd20f93cce6456459"
 
   url "https://dbeaver.io/files/#{version}/dbeaver-ce-#{version}-macos-#{arch}.dmg"
   name "DBeaver Community Edition"
