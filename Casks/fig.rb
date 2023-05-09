f8980c5bf9caf5452fe9eb54077377ab9b31b49e
runner <runner@Mac-1683665736106.local>
2023-05-09 21:49:23 +0000

    fig 2.16.0

--- Casks/fig.rb
+++ Casks/fig.rb
@@ -1,6 +1,6 @@
 cask "fig" do
-  version "2.15.0"
-  sha256 "d32a5848fb68f93d402f660fa3182f940b2df2719fa3f111a72fcc2fe5874bcc"
+  version "2.16.0"
+  sha256 "61fe404ab063974d794c7c6f7879ccf7e91adc4f139211361967fe58582b0623"
 
   url "https://repo.fig.io/generic/stable/asset/#{version}/universal/fig.dmg"
   name "fig"
