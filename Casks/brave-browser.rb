983beab56a309c3904d0f6f101b2c8032e1dedb7
Bevan Kay <email@bevankay.me>
2023-05-10 09:36:42 +1000

    brave-browser 1.51.114.0

--- Casks/brave-browser.rb
+++ Casks/brave-browser.rb
@@ -2,9 +2,9 @@ cask "brave-browser" do
   arch arm: "arm64", intel: "x64"
   folder = on_arch_conditional arm: "stable-arm64", intel: "stable"
 
-  version "1.51.110.0,151.110"
-  sha256 arm:   "53644ebce5f21197f561228210a5af1e2af5d15fdbefa0005731604d0e665097",
-         intel: "66bfee5d6b7d3709065192ae094eed85e2263251c607a19505daa71033bc98a3"
+  version "1.51.114.0,151.114"
+  sha256 arm:   "53c022fd4dba6b7ab3ae9019c78b05ccd761a9b5d9ab4c88c98da81f9dd94250",
+         intel: "2709ca93e0002d86ee558622775a9ea49108c9682f964c050ac0dd555cee5028"
 
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.csv.second}/Brave-Browser-#{arch}.dmg",
       verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
