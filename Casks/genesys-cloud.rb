c33cc443d60bc8aed4a72b6016605f8eb183fed2
Alex Rudoy <alex.rudoy@icloud.com>
2023-05-09 15:25:57 +0200

    adding Genesys Cloud for MacOS cask

---
+++ Casks/genesys-cloud.rb
@@ -0,0 +1,18 @@
+cask "genesys-cloud" do
+  version "2.22.557-56"
+  sha256 "a743ef082b2dca01056b73f55e3e91c4d4093d994560a02df99f5ec71c7a5b4f"
+
+  url "https://app.mypurecloud.com/directory-mac/build-assets/#{version}/genesys-cloud-mac-#{version[0...-3]}.dmg"
+  name "Genesys Cloud for macOS"
+  desc "Run Genesys Cloud as a stand-alone program, and keep it separate from your browser windows and tabs"
+  homepage "https://apps.mypurecloud.com/directory-mac/"
+
+  livecheck do
+    url :homepage
+    regex(%r{href=.*?/build-assets/(\d+(?:\.\d+)+-\d+).*\.dmg}i)
+  end
+
+  auto_updates true
+
+  app "Genesys Cloud.app"
+end
