41a015fa25133ec505e1c4e8ce8fc928242440e1
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-09 11:18:38 -0400

    version changes

--- Casks/genesys-cloud.rb
+++ Casks/genesys-cloud.rb
@@ -1,15 +1,20 @@
 cask "genesys-cloud" do
-  version "2.22.557-56"
+  version "2.22.557,56"
   sha256 "a743ef082b2dca01056b73f55e3e91c4d4093d994560a02df99f5ec71c7a5b4f"
 
-  url "https://app.mypurecloud.com/directory-mac/build-assets/#{version}/genesys-cloud-mac-#{version[0...-3]}.dmg"
+  url "https://app.mypurecloud.com/directory-mac/build-assets/#{version.csv.first}-#{version.csv.second}/genesys-cloud-mac-#{version.csv.first}.dmg"
   name "Genesys Cloud for macOS"
   desc "Run Genesys Cloud as a stand-alone program, keeping it separate from web browser"
   homepage "https://apps.mypurecloud.com/directory-mac/"
 
   livecheck do
     url :homepage
-    regex(%r{href=.*?/build-assets/(\d+(?:\.\d+)+-\d+).*\.dmg}i)
+    regex(%r{href=.*?/build-assets/(\d+(?:\.\d+)+)[._-](\d+).*?\.dmg}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map do |match|
+        "#{match[0]},#{match[1]}"
+      end
+    end
   end
 
   auto_updates true
