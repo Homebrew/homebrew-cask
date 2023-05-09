65951d3bacb3d6744822bad7decb312d25c72615
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-09 19:56:57 -0400

    foxglove-studio 1.53.1

--- Casks/foxglove-studio.rb
+++ Casks/foxglove-studio.rb
@@ -1,6 +1,6 @@
 cask "foxglove-studio" do
-  version "1.53.0"
-  sha256 "6af17ac5be0a136a65d4c5d7571b36d4113af057e239eaa2e9056b586f37f8c4"
+  version "1.53.1"
+  sha256 "d6af7b9dad865509d3f99d8605ca83fb803cd081f7f7b48fab79ba597a261180"
 
   url "https://github.com/foxglove/studio/releases/download/v#{version}/foxglove-studio-#{version}-mac-universal.dmg",
       verified: "github.com/foxglove/studio/"
