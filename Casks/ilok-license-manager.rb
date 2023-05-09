30356031945bfc80f6a37182bee44e43850356d9
miccal <miccal.matthews@gmail.com>
2023-05-09 15:48:50 +0800

    ilok-license-manager: update livecheck

--- Casks/ilok-license-manager.rb
+++ Casks/ilok-license-manager.rb
@@ -1,5 +1,5 @@
 cask "ilok-license-manager" do
-  version "5.7.1,4844"
+  version "5.7.1"
   sha256 :no_check
 
   url "https://installers.ilok.com/iloklicensemanager/LicenseSupportInstallerMac.zip"
@@ -10,7 +10,7 @@ cask "ilok-license-manager" do
   livecheck do
     url "https://updates.ilok.com/iloklicensemanager/LicenseSupportInstallerMacAppcast.xml"
     strategy :sparkle do |item|
-      "#{item.short_version.split.first},#{item.version}"
+      item.short_version.split.first.to_s
     end
   end
 
