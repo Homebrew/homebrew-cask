16bc190e780fc5c7d913dfa5e069fc4d15138ee0
miccal <miccal.matthews@gmail.com>
2023-05-09 15:23:01 +0800

    darwindumper: various updates

--- Casks/darwindumper.rb
+++ Casks/darwindumper.rb
@@ -1,15 +1,16 @@
 cask "darwindumper" do
-  version "3.1.1,311"
+  version "3.1.1"
   sha256 "b840162363ec8d1b505a158f195c45f7969a99545150825e8d2b9e72f1cf43f5"
 
-  url "https://bitbucket.org/blackosx/darwindumper/downloads/DarwinDumper_v#{version.csv.first}.zip"
+  url "https://bitbucket.org/blackosx/darwindumper/downloads/DarwinDumper_v#{version}.zip"
   name "DarwinDumper"
+  desc "App to dump system information to aid troubleshooting"
   homepage "https://bitbucket.org/blackosx/darwindumper"
 
   livecheck do
     url "https://bitbucket.org/blackosx/darwindumper/wiki/DD_AppCast.xml"
     strategy :sparkle do |item|
-      "#{item.short_version.delete_prefix("r")},#{item.version}"
+      "#{item.short_version.delete_prefix("r")}"
     end
   end
 
