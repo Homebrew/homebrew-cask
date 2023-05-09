01ede19ff45e533657bc0a3aa273ab7e77473e04
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-09 17:23:44 -0400

    siyuan 2.8.7

--- Casks/siyuan.rb
+++ Casks/siyuan.rb
@@ -1,9 +1,9 @@
 cask "siyuan" do
   arch arm: "-arm64"
 
-  version "2.8.6"
-  sha256 arm:   "408b8ff724b4e128ab5204ad3ccf210b35b577cfde984d5108dbed6ec3177524",
-         intel: "0e09076355329aec1e39e732783be0959c0194eb47d651a957bd6e1db7946d4a"
+  version "2.8.7"
+  sha256 arm:   "4f87165a4958fc4a9665e1c1837a88fff49fc09f507ab95a91a90423e3fb795b",
+         intel: "fa47adcacf59965e43275e7c8aa4e3a2c2920f5ffcfa3702ef4780efd3460e8e"
 
   url "https://github.com/siyuan-note/siyuan/releases/download/v#{version}/siyuan-#{version}-mac#{arch}.dmg"
   name "SiYuan"
