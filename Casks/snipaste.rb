119c6042dab80967912b1cae178b2ac1449c93eb
MoonFruit <dkmoonfruit@gmail.com>
2023-05-10 10:51:53 +0800

    snipaste 2.8.5-Beta

--- Casks/snipaste.rb
+++ Casks/snipaste.rb
@@ -1,6 +1,6 @@
 cask "snipaste" do
-  version "2.8.3-Beta2"
-  sha256 "34828092963044681664e7178599e9668298a3bec8f34187b2fcb20642310820"
+  version "2.8.5-Beta"
+  sha256 "46ecdaf721aefeafa97681c82c0404bc2b67056bfcab1f03f922b501d017897a"
 
   url "https://bitbucket.org/liule/snipaste/downloads/Snipaste-#{version}.dmg",
       verified: "bitbucket.org/liule/snipaste/"
