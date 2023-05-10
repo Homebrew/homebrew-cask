1fb6a18d65fb602708374301cd9192d115a0e760
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-05-11 05:08:02 +0800

    dbvisualizer 23.2.1 (#146919)

--- Casks/dbvisualizer.rb
+++ Casks/dbvisualizer.rb
@@ -1,9 +1,9 @@
 cask "dbvisualizer" do
   arch arm: "aarch64", intel: "x64"
 
-  version "23.2"
-  sha256 arm:   "2b1b866935ad8003b45643d59ddcbd81f8b3d9fe188094bcda95fa8609a27595",
-         intel: "6544561779e45a3b4e4ac94dabc7e632f51d573d5755dc0ca3980e45bf433e14"
+  version "23.2.1"
+  sha256 arm:   "90d0310618dbcfcbe5a07b65a6bab178c3987b7349e23064c8b2506d268a71fd",
+         intel: "88c8ed7a39b4be5fe65d4b2353e5471472c1b7a2d380b1968a78bf8943002043"
 
   url "https://www.dbvis.com/product_download/dbvis-#{version}/media/dbvis_macos-#{arch}_#{version.dots_to_underscores}.dmg"
   name "DbVisualizer"
