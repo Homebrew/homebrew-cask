52c1c33c2d1bf0843d90c3e430606dec3735b839
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-10 07:54:57 -0400

    dbvisualizer: fix sha256 (#146883)

--- Casks/dbvisualizer.rb
+++ Casks/dbvisualizer.rb
@@ -2,8 +2,8 @@ cask "dbvisualizer" do
   arch arm: "aarch64", intel: "x64"
 
   version "23.2"
-  sha256 arm:   "75c6e43d5743b75fd92cb279332429622db8ae2fac3a47d8d2bc61f5c788d8f8",
-         intel: "4401f9c756c1501909cded0759365f3e6c1bfd16f93a50427be87b9528f4d832"
+  sha256 arm:   "2b1b866935ad8003b45643d59ddcbd81f8b3d9fe188094bcda95fa8609a27595",
+         intel: "6544561779e45a3b4e4ac94dabc7e632f51d573d5755dc0ca3980e45bf433e14"
 
   url "https://www.dbvis.com/product_download/dbvis-#{version}/media/dbvis_macos-#{arch}_#{version.dots_to_underscores}.dmg"
   name "DbVisualizer"
