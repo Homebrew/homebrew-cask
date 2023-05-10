99071a3fde457bc110946e2555966d1694d7ed55
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-05-11 06:49:41 +0800

    opentoonz 1.7.1 (#146933)

--- Casks/opentoonz.rb
+++ Casks/opentoonz.rb
@@ -1,6 +1,6 @@
 cask "opentoonz" do
-  version "1.7.0"
-  sha256 "292a1b51964099eb9814d5df03f0386db5e069131215fc4965dc3b3501008829"
+  version "1.7.1"
+  sha256 "b2849f4359499164ee610c17e1a79019f5a3474fb37620243e4bd8c8724fc136"
 
   url "https://github.com/opentoonz/opentoonz/releases/download/v#{version}/OpenToonz.pkg",
       verified: "github.com/opentoonz/opentoonz/"
