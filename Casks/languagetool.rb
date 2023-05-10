f191ce2fab982193a4e396b504f2636d7ea5d4b5
Yurii Kolesnykov <root@yurikoles.com>
2023-05-10 14:32:56 +0300

    languagetool 1.3.3

--- Casks/languagetool.rb
+++ Casks/languagetool.rb
@@ -1,6 +1,6 @@
 cask "languagetool" do
-  version "1.3.2"
-  sha256 "52d641b1272ce2766ba8510fe7858d83d865822de9f51db9bcea07367660bdc8"
+  version "1.3.3"
+  sha256 "94e90faf1d3e096d65ab3b28f468507843586d771c7e03138fb3932b9e0480cf"
 
   url "https://languagetool.org/download/mac-app/LanguageToolDesktop-#{version}.dmg"
   name "LanguageTool for Desktop"
