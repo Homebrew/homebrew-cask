7d8b2b01b0ad1a7e5c6b9a4b72be0517ea5c5381
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-10 17:25:12 -0400

    bump version and change description

--- Casks/hdhomerun.rb
+++ Casks/hdhomerun.rb
@@ -1,10 +1,10 @@
 cask "hdhomerun" do
-  version "20230503"
-  sha256 "46db4a8dfa95c9290eddd5513c639b9bdef92cd8dc1ea64f8376d09fc38e4c74"
+  version "20230505"
+  sha256 "a02b78291d87c03ba7c07285dd5e631b0f50d53992435c93c81e64d309fa240b"
 
   url "https://download.silicondust.com/hdhomerun/hdhomerun_mac_#{version}.dmg"
   name "HDHomeRun"
-  desc "Watch, schedule and record terrestrial HDTV"
+  desc "Client for HDHomeRun streamer"
   homepage "https://www.silicondust.com/support/downloads/"
 
   livecheck do
