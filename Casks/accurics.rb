3b3863a509f2879327ba1fa258a8581b4203b303
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-11 10:48:22 -0400

    accurics (update homepage, add verified)

--- Casks/accurics.rb
+++ Casks/accurics.rb
@@ -3,10 +3,11 @@ cask "accurics" do
   sha256 "42faba775fa215ce0a55a297f08cd36d8a051e81484bed5e2d8c1e20ea36c195"
 
   url "https://downloads.accurics.com/cli/#{version}/accurics-cli.dmg",
-      user_agent: :fake
+      user_agent: :fake,
+      verified:   "downloads.accurics.com/"
   name "Accurics CLI"
   desc "Security and compliance for Infrastructure as Code"
-  homepage "https://www.accurics.com/"
+  homepage "https://www.tenable.com/accurics"
 
   livecheck do
     url "https://downloads.accurics.com/cli/latest/accurics-cli.yml"
