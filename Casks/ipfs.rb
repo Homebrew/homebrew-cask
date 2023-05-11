99bf8c5363447074e2c1b990e4d0ec08842dd394
Luflosi <luflosi@luflosi.de>
2023-05-11 11:31:00 +0200

    ipfs 0.28.0

--- Casks/ipfs.rb
+++ Casks/ipfs.rb
@@ -1,6 +1,6 @@
 cask "ipfs" do
-  version "0.27.3"
-  sha256 "5ce8132b37d0a8460661b7b6575bf7d1900a22883d4af248f1f1de2d013f5300"
+  version "0.28.0"
+  sha256 "dd0e33cab02a3cabc269d72ba7a607b434d4e2a77e8d5956da068e98ef9a73eb"
 
   url "https://github.com/ipfs/ipfs-desktop/releases/download/v#{version}/ipfs-desktop-#{version}-mac.dmg"
   name "IPFS Desktop"
