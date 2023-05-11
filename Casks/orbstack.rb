61615317b66a6c5c419e9bef81335f039053048e
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-11 10:41:57 -0400

    orbstack 0.10.1_1427 (#146986)

--- Casks/orbstack.rb
+++ Casks/orbstack.rb
@@ -1,9 +1,9 @@
 cask "orbstack" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.10.0_1407"
-  sha256 arm:   "a47165b2629a9bdbbff2b6f93fa7d590f488f6a1d02b1bd8b2f5e9a9a27f4c1f",
-         intel: "c93c8b65c7f08cb4bb95355b4418e2b17fc7a744a288f2e765fd183cf6fc6051"
+  version "0.10.1_1427"
+  sha256 arm:   "d1e6a0ccb537a9d6ebf59a1c4832159defa0792d97064e6663965522d542ef8d",
+         intel: "59de17c7af07cff666819603029e17d5ad99a2b109ac6b6fc97dcf6ae3eb841b"
 
   url "https://cdn-updates.orbstack.dev/#{arch}/OrbStack_v#{version}_#{arch}.dmg"
   name "OrbStack"
