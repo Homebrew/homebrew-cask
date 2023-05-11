1a2b048d627549fc1fdfa24cae2d17ab3903a5fc
ridicolos <ridicolos@posteo.de>
2023-05-12 00:55:58 +0200

    dbeaverlite 23.0.0 (#147028)

--- Casks/dbeaverlite.rb
+++ Casks/dbeaverlite.rb
@@ -2,8 +2,8 @@ cask "dbeaverlite" do
   arch arm: "aarch64", intel: "x86_64"
 
   version "23.0.0"
-  sha256 arm:   "b6de6d7124084bda4ac832ef69043392057bb0f1ddb1d06865e64b9a1569adff",
-         intel: "9cc3ae68590aa7f0c7043a412f6797427a6ea2699babb5efc367d3d41ac4775d"
+  sha256 arm:   "e90befb2d9eb754ec0c498ece247e2f7342a4daa181eb395b82a4f6c25d5fe22",
+         intel: "4e596fad5017b4bcc35a7d065c7d5a04b2498e915d6f48e125c15494c7bdace4"
 
   url "https://dbeaver.com/downloads-lite/#{version}/dbeaver-le-#{version}-macos-#{arch}.dmg"
   name "DBeaver Lite Edition"
