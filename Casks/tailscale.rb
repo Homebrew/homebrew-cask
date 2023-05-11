84582e13ecdf3d4e1feb54b8e6d665915b886e26
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-11 10:42:04 -0400

    tailscale 1.40.1 (#146984)

--- Casks/tailscale.rb
+++ Casks/tailscale.rb
@@ -1,6 +1,6 @@
 cask "tailscale" do
-  version "1.40.0"
-  sha256 "7ec44ba42823127b044bbbbf5bf6583d0272058b77c2e66d2d348043a1e8f489"
+  version "1.40.1"
+  sha256 "0d7bb5566c31f84d7d30a227ab7cef6953aabe80fdaa45bf6349761b76cf432c"
 
   url "https://pkgs.tailscale.com/stable/Tailscale-#{version}-macos.zip"
   name "Tailscale"
