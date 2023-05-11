934d1f916de2c60aec14b0c0c25c7fd7d6bf3c3a
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-05-11 14:07:25 +0800

    quicken 7.0.3 (#146956)

--- Casks/quicken.rb
+++ Casks/quicken.rb
@@ -1,6 +1,6 @@
 cask "quicken" do
-  version "7.0.2,700.48777.100"
-  sha256 "faf94ea46215a79f750d2bf7071b17f0dba93ef2fe5c67ffe9717096f8241f31"
+  version "7.0.3,700.48782.100"
+  sha256 "82256af556552304c4bc1183f679b5304e86b67c700e8263eafdf58b38db3c88"
 
   url "https://download.quicken.com/mac/Quicken/001/Release/031A96D9-EFE6-4520-8B6A-7F465DDAA3E4/Quicken-#{version.csv.second}/Quicken-#{version.csv.second}.zip"
   name "Quicken"
