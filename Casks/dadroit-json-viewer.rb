d2995ebf26f7fc228e17c55a05fe8e6900081780
Justine Jose <justinejose123@gmail.com>
2023-05-12 15:00:43 +0100

    add dadroit-json-viewer

---
+++ Casks/dadroit-json-viewer.rb
@@ -0,0 +1,16 @@
+cask "dadroit-json-viewer" do
+  version "2.0.1"
+  sha256 :no_check
+
+  url "https://dadroit.com/releases/mac/DadroitJSONViewer.zip"
+  name "Dadroit JSON Viewer"
+  desc "Rapid JSON Viewer to open large data files in a blink"
+  homepage "https://dadroit.com/"
+
+  livecheck do
+    url "https://dadroit.com/download/"
+    regex(/>Ver.*?(\d+(?:\.\d+)+)/i)
+  end
+
+  app "Dadroit JSON Viewer.app"
+end
