a8d3f02ce57b9a95a9e49262bd7153a61c929182
Markus Reiter <me@reitermark.us>
2023-05-12 17:14:27 +0200

    Update nightowl.rb

--- Casks/nightowl.rb
+++ Casks/nightowl.rb
@@ -18,7 +18,8 @@ cask "nightowl" do
   pkg "nightowl-#{version.csv.first}(release).pkg"
 
   uninstall pkgutil:   "com.mygreatcompany.pkg.NightOwl",
-            launchctl: "org.nightowl.autoupdater.com"
+            launchctl: "org.nightowl.autoupdater.com",
+                       "NightOwlUpdater"
 
   zap trash: [
     "~/Library/Caches/com.fuekiin.NightOwl",
