7fe008dc260eb82274530352cba1b227265b9b5b
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-13 11:47:32 -0400

    sfdx 7.200.7 (#147128)

--- Casks/sfdx.rb
+++ Casks/sfdx.rb
@@ -1,7 +1,7 @@
 cask "sfdx" do
   arch arm: "arm64", intel: "x64"
 
-  version "7.199.7"
+  version "7.200.7"
   sha256 :no_check
 
   url "https://developer.salesforce.com/media/salesforce-cli/sfdx/channels/stable/sfdx-#{arch}.pkg"
