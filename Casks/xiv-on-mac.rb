274ad8250802299dde81dca606d56dcdb4ca1118
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-12 20:23:54 -0400

    xiv-on-mac 4.9

--- Casks/xiv-on-mac.rb
+++ Casks/xiv-on-mac.rb
@@ -1,6 +1,6 @@
 cask "xiv-on-mac" do
-  version "4.8"
-  sha256 "019e36c1bd5d5873126f1c04a1adfd3adc4ccdaf7dd51d6426019fee52b458d0"
+  version "4.9"
+  sha256 "dfcb98761c89e2955318e369d95e15851ee15c2d486b9d007bd4c81b01e64f1d"
 
   url "https://softwareupdate.xivmac.com/sites/default/files/update_data/XIV%20on%20Mac#{version}.tar.xz"
   name "XIV on Mac"
