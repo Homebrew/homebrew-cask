7ba9bfa3a6014177189c29929f65b92b11be5510
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-12 20:18:16 -0400

    quarto 1.3.353

--- Casks/quarto.rb
+++ Casks/quarto.rb
@@ -1,6 +1,6 @@
 cask "quarto" do
-  version "1.3.340"
-  sha256 "2b2d09e2b1361ef222cd555f35076042711b9011f373836bc544aa3880c777e8"
+  version "1.3.353"
+  sha256 "c64ecb1a3e9f13b156281c6c13740fbb7043fa7078c31c4be56568d96803dac5"
 
   url "https://github.com/quarto-dev/quarto-cli/releases/download/v#{version}/quarto-#{version}-macos.pkg",
       verified: "github.com/quarto-dev/quarto-cli/"
