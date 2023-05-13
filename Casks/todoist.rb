cd54462501d5e2d772e9597ee59f884305aea8cf
Miccal Matthews <miccal.matthews@gmail.com>
2023-05-14 05:01:31 +0800

    Update todoist.rb

--- Casks/todoist.rb
+++ Casks/todoist.rb
@@ -1,8 +1,8 @@
 cask "todoist" do
-  version "8.2.0"
-  sha256 "c982c1238438393fec4978a9b91f7eeb8094fcdb39db5a36efeaa544c95862fc"
+  version "8.1.2"
+  sha256 "ec4a407708850b6105dca21d4dd87a9722ed472d07bf25ab0060d36fba299402"
 
-  url "https://electron-dl.todoist.com/mac/Todoist-#{version}-universal.zip"
+  url "https://electron-dl.todoist.com/mac/Todoist-#{version}.dmg"
   name "Todoist"
   desc "To-do list"
   homepage "https://todoist.com/home"
@@ -13,6 +13,7 @@ cask "todoist" do
   end
 
   auto_updates true
+  depends_on macos: ">= :el_capitan"
 
   app "Todoist.app"
 
