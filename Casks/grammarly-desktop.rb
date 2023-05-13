f45b784446c7c9ba3e071e95cc70c9b28e8b15a1
Luca Palli <luca@palli.ch>
2023-05-13 13:32:04 +0200

    grammarly-desktop 1.26.0.1 (#147113)

--- Casks/grammarly-desktop.rb
+++ Casks/grammarly-desktop.rb
@@ -1,6 +1,6 @@
 cask "grammarly-desktop" do
-  version "1.25.1.1"
-  sha256 "a014f52bd32a28625c3fed40eade65cc2f57836714164ef4dd351214aeaf0935"
+  version "1.26.0.1"
+  sha256 "a25ba778b6c098fad3b6ed3a296d2f8429b9ac1f7ca75f57c198ccd37ab1001e"
 
   url "https://download-mac.grammarly.com/versions/#{version}/Grammarly.dmg"
   name "Grammarly Desktop"
