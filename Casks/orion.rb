befbcfc0156d9386901daa686bb049587ab499b3
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-14 10:55:41 -0400

    orion 0.99,124.1 (#147155)
    * orion 0.99,124.1
    
    * orion (sha256 updates)
    
    * Update all sha256 for version

--- Casks/orion.rb
+++ Casks/orion.rb
@@ -1,8 +1,8 @@
 cask "orion" do
-  version "0.99,124"
+  version "0.99,124.1"
 
   on_mojave :or_older do
-    sha256 "5a83ddbbfcb3ebaabd67ee4daa1b875ceeed15bdb47c3775946f6ee62e81557a"
+    sha256 "9ebd528959ae66b1afd0b2a68bf32b3c99dc124ba174178556d33ee32e8a7bcc"
 
     url "https://browser.kagi.com/updates/10_14/#{version.csv.second}.zip"
 
@@ -14,7 +14,7 @@ cask "orion" do
     depends_on macos: ">= :mojave"
   end
   on_catalina do
-    sha256 "766fd81fb5dc60624e2bbd3fcfc95710eab1bf4e42f6e59858d3901afa7a201d"
+    sha256 "c3ac204fb7430a057322dd5d90a396bac69f8beaed6efb09930333dd8418fa47"
 
     url "https://browser.kagi.com/updates/10_15/#{version.csv.second}.zip"
 
@@ -26,7 +26,7 @@ cask "orion" do
     depends_on macos: :catalina
   end
   on_big_sur do
-    sha256 "7bb3ff7d36c97fa512f4858ca78bf25cd8ab6cd177f33be5e9d38f6c34cb3f13"
+    sha256 "9c1c1378e0a756ab11ac252c8cbe39061d390bc9e3beeeba98ee1d3f08ec19ec"
 
     url "https://browser.kagi.com/updates/11_0/#{version.csv.second}.zip"
 
@@ -38,7 +38,7 @@ cask "orion" do
     depends_on macos: :big_sur
   end
   on_monterey do
-    sha256 "ebb15c1cf88cc72c12817ac66ea85df3304c982cf599206c6d3ce63fcbf7b953"
+    sha256 "c8b12d38dc44c71b13f2392d7e7acde26360af507b39715757379c1701cece83"
 
     url "https://browser.kagi.com/updates/12_0/#{version.csv.second}.zip"
 
@@ -50,7 +50,7 @@ cask "orion" do
     depends_on macos: :monterey
   end
   on_ventura do
-    sha256 "d0a84eef87bab99644120848994527c24a29b10709338a2c1ae044fafa05b052"
+    sha256 "8df6997ee7fcd09cb2c0ce7761430abd1dfbdee038f7bf434ae9b6633b9948cc"
 
     url "https://browser.kagi.com/updates/13_0/#{version.csv.second}.zip"
 
