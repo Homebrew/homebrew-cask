890557db0732c4d9b005aed823c7bf3df2a30755
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-05-15 02:56:49 +0800

    busycal 2023.2.1

--- Casks/busycal.rb
+++ Casks/busycal.rb
@@ -1,6 +1,6 @@
 cask "busycal" do
-  version "2023.1.5,2023-03-24-13-51"
-  sha256 "3dd58abbf7c16d38d1e3fe418bfe8705387484767c1d20c00c80293400178fba"
+  version "2023.2.1,2023-05-13-21-56"
+  sha256 "abd276379542a813cee9bdc16821915681173926c1563f710baa577e07f98854"
 
   url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bcl-#{version.csv.first}-#{version.csv.second}.zip",
       verified: "7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/"
