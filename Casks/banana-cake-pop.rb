a3fd0c4f046776b7173c9c3f732f94e9781e1225
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-15 09:00:10 -0400

    banana-cake-pop 5.0.6 (#147181)

--- Casks/banana-cake-pop.rb
+++ Casks/banana-cake-pop.rb
@@ -1,9 +1,9 @@
 cask "banana-cake-pop" do
   arch arm: "arm64", intel: "x64"
 
-  version "5.0.5"
-  sha256 arm:   "e6e216e827e9079aa54030751e9cfefa0cbf05534ac4a4877539d49adacfee27",
-         intel: "7b484adbc59a4c4a6be93ab97a4ba908a2533a1de217222d73af233b5780ff2c"
+  version "5.0.6"
+  sha256 arm:   "129942c30b55a1c0375cef92c7b2ca99bdba90b1dea6ae83865b27c1585a4b6f",
+         intel: "9c36916a95b89dcd80f9ecdbbebe0a0671bcbc7c88d2c62b5325034e013b1a27"
 
   url "https://download.chillicream.com/bananacakepop/BananaCakePop-#{version}-mac-#{arch}.dmg"
   name "Banana Cake Pop"
