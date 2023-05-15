1740f727a5ee7651d6584502068b7e960b3acaaa
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-15 09:30:46 -0400

    worldpainter 2.17.1

--- Casks/worldpainter.rb
+++ Casks/worldpainter.rb
@@ -1,6 +1,6 @@
 cask "worldpainter" do
-  version "2.17.0"
-  sha256 "feabe80e00c5ee4119852a844b7545edaffe35492a94bcf4bc68e7c3d1425471"
+  version "2.17.1"
+  sha256 "68b491746545b1cff16d76d7203a5b3e696debc729aeeef3a4c7d484a3217994"
 
   url "https://www.worldpainter.net/files/worldpainter_#{version}.tgz"
   name "WorldPainter"
