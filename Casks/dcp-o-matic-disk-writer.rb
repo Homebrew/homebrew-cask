9eedd8a99c9de92bd729e9d5b5e41d978ec87883
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-15 10:29:01 -0400

    dcp-o-matic-disk-writer 2.16.56 (#147190)

--- Casks/dcp-o-matic-disk-writer.rb
+++ Casks/dcp-o-matic-disk-writer.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-disk-writer" do
-  version "2.16.55"
-  sha256 "af24a60adac56b70657f65fe6bace8ef3594cfb2dc48478c331c1b65bec54a9f"
+  version "2.16.56"
+  sha256 "bfae87502966aa23faaa677cc913735bb3feefadd27be2b4df613108eea5dba9"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-disk&version=#{version}"
   name "DCP-o-matic Disk Writer"
