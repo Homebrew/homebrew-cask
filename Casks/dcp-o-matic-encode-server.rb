845756cf7945b2ab9f1d3b6dbb5a1dc349597c13
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-15 09:01:41 -0400

    dcp-o-matic-encode-server 2.16.56

--- Casks/dcp-o-matic-encode-server.rb
+++ Casks/dcp-o-matic-encode-server.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-encode-server" do
-  version "2.16.55"
-  sha256 "16e9219ee6a41bbe245d84ca5f78822298eed8f50b98abef4a4c00026522bb4d"
+  version "2.16.56"
+  sha256 "191da6938ea03795618923bde3b063c72acb682281ce2cde61b35c0ce35ebcaa"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-server&version=#{version}"
   name "DCP-o-matic Encode Server"
