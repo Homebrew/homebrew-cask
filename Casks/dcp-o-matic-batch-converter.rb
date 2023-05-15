afedcda7258776d3fa91152a22808ec069240d98
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-15 09:01:42 -0400

    dcp-o-matic-batch-converter 2.16.56 (#147187)

--- Casks/dcp-o-matic-batch-converter.rb
+++ Casks/dcp-o-matic-batch-converter.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-batch-converter" do
-  version "2.16.55"
-  sha256 "dba0d29d45964730f271bd2c63aa18a087b012399e55f64941fd5b85c9751a61"
+  version "2.16.56"
+  sha256 "ac835c7dc73c5425e3759ea5af1c44cb0b74045c676c782bcf9d8a735ec88c7e"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-batch&version=#{version}"
   name "DCP-o-matic Batch converter"
