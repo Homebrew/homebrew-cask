abf2392249f8b45a425b888d9113b0ff61e0331c
Phill Tran <philltran@users.noreply.github.com>
2023-05-15 15:37:52 -0400

    lando 3.17.1 (#147226)

--- Casks/lando.rb
+++ Casks/lando.rb
@@ -1,9 +1,9 @@
 cask "lando" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.17.0"
-  sha256 arm:   "12ab2c6df437b7cffdca132bf87f4cab533f55075e2492d29c4b7e7d2a03f762",
-         intel: "fd45f14122f3845c1a1f7728e5a511cb8e5bd6a2448f03258ae393f36116f26e"
+  version "3.17.1"
+  sha256 arm:   "2f8395d34c0a04e0d4e3a5f03dc8f63796e3cb054116fe147fef506e976dc87d",
+         intel: "c2b592faa1703457c3b390235ded66a74778c2990c5c99e7bd52db65a4765d5a"
 
   url "https://github.com/lando/lando/releases/download/v#{version}/lando-#{arch}-v#{version}.dmg",
       verified: "github.com/lando/lando/"
