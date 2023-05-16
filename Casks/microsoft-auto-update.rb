f090d316eb5d73c6ebbf8ddba50f861ff22c2b1c
owine <owine@users.noreply.github.com>
2023-05-16 17:28:40 -0500

    microsoft-auto-update 4.59.23051401

--- Casks/microsoft-auto-update.rb
+++ Casks/microsoft-auto-update.rb
@@ -4,8 +4,8 @@ cask "microsoft-auto-update" do
     sha256 "f638f7e0da9ee659c323f2ede0f176804bfe9a615a8f8b6320bd2e69d91ef2b2"
   end
   on_sierra :or_newer do
-    version "4.58.23040900"
-    sha256 "a3cb0b46dc7ba819f1a0e86dfe8b3faa13aa6b8fca8660090d0ce8d21f333971"
+    version "4.59.23051401"
+    sha256 "ccaaaba23e77026bb3c8f7c504698fb3e9490956feb299f5d4e7f678fd2f12cf"
   end
 
   url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_AutoUpdate_#{version}_Updater.pkg"
