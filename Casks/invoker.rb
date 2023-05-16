21b706d54da78e590e2f69497f07ffa0255eaaa7
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-05-17 01:50:04 +0800

    invoker 2.13.0

--- Casks/invoker.rb
+++ Casks/invoker.rb
@@ -1,9 +1,9 @@
 cask "invoker" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.12.2"
-  sha256  arm:   "b5af36c38fb664ab5f5634edca6d528e2c214e21dc98042565dac8f9ea1bb18a",
-          intel: "cb10e6ecccb0364fbcf1a4184d61cb7cf1e08609dfe3f84889f7c2c0f21c34be"
+  version "2.13.0"
+  sha256  arm:   "a13eef9fe4fb4d3bcf6ce0a29b2ab513d80a8bf5921468c4fea7a5b7c66ba84c",
+          intel: "cf8dcfa1827a5f0b79bdccf2348a114e7920813440ae2ae5df9486239a1df4db"
 
   url "https://invokerdev.fra1.digitaloceanspaces.com/invoker/Invoker-#{version}-#{arch}.dmg",
       verified: "invokerdev.fra1.digitaloceanspaces.com/invoker/"
