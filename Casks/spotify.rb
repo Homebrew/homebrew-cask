f28b73f09324293fbde448958f765c1c722c2788
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-16 11:26:34 -0400

    add spotify version

--- Casks/spotify.rb
+++ Casks/spotify.rb
@@ -4,7 +4,7 @@ cask "spotify" do
   sha256 :no_check
 
   on_arm do
-    version "1.2.10.760,52970952,1110"
+    version "1.2.11.916,eb595a67,2521"
   end
   on_intel do
     version "1.2.11.916,eb595a67,2517"
