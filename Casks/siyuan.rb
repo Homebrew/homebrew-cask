3550a0e48efbbf3fe920f83a221764e440dfa04a
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-05-17 05:27:38 +0800

    siyuan 2.8.8 (#147304)

--- Casks/siyuan.rb
+++ Casks/siyuan.rb
@@ -1,9 +1,9 @@
 cask "siyuan" do
   arch arm: "-arm64"
 
-  version "2.8.7"
-  sha256 arm:   "4f87165a4958fc4a9665e1c1837a88fff49fc09f507ab95a91a90423e3fb795b",
-         intel: "fa47adcacf59965e43275e7c8aa4e3a2c2920f5ffcfa3702ef4780efd3460e8e"
+  version "2.8.8"
+  sha256 arm:   "9006a4ea3b8d28fb9aaa21e9b0a831dc509ac939f4ca380c7d0aefee7d0e659e",
+         intel: "76e96abad66d74daaa46d8e6179d279c10a0668ff75151fa6b9904c4cf025893"
 
   url "https://github.com/siyuan-note/siyuan/releases/download/v#{version}/siyuan-#{version}-mac#{arch}.dmg"
   name "SiYuan"
