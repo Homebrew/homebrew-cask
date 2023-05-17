5dcd4982690f4e3ae2f5049ad782cd61998eb5fb
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-16 20:43:36 -0400

    sigmaos 1.7.0.5 (#147332)

--- Casks/sigmaos.rb
+++ Casks/sigmaos.rb
@@ -1,6 +1,6 @@
 cask "sigmaos" do
-  version "1.7.0.2"
-  sha256 "9b1dce044455660d3c619b5afd396738a6ce467ea6a4e91b6c4c7f3e2fa05041"
+  version "1.7.0.5"
+  sha256 "d76d009196c1b2be6d70a1ec8a5ea15164e7340e47f36adbd1f7a0148c65494d"
 
   url "https://releases.sigmaos.com/SigmaOS-#{version}.dmg"
   name "SigmaOS"
