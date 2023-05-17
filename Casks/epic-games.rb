f232184503194b56da5afb310bf7faa55963a633
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-16 20:12:11 -0400

    epic-games 15.2.1

--- Casks/epic-games.rb
+++ Casks/epic-games.rb
@@ -1,6 +1,6 @@
 cask "epic-games" do
-  version "15.1.1"
-  sha256 "cdc4e337cb894b9c540109d9bcf66b3a82636a73e869b78b93cb65fc196ef8d8"
+  version "15.2.1"
+  sha256 "eb78d186860d7b4405bdf6eeaba0274752bdcca1855b87827f3ad818161ea4de"
 
   url "https://epicgames-download1.akamaized.net/Builds/UnrealEngineLauncher/Installers/Mac/EpicInstaller-#{version}.dmg",
       verified: "epicgames-download1.akamaized.net/"
