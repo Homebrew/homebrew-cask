f0da0c94780f4ac42751485070a94e578c677e0e
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-16 20:43:18 -0400

    dotnet-sdk 7.0.302 (#147325)
    * dotnet-sdk 7.0.302
    
    * dotnet-sdk 7.0.302
    
    * Update x64 version and sha256

--- Casks/dotnet-sdk.rb
+++ Casks/dotnet-sdk.rb
@@ -2,12 +2,12 @@ cask "dotnet-sdk" do
   arch arm: "arm64", intel: "x64"
 
   on_arm do
-    version "7.0.203,a5070179-d53b-4a84-98d6-37a9d3ef458b,f8bba83817d23e3b7726746c59de4e0c"
-    sha256 "c80e0eabfa3681fa1db33c149ccbbb7de9155702aa5baaa11ea63f998151f326"
+    version "7.0.302,d23d8494-56f8-4fb9-8b17-633f62d3e30d,f7564881f12ec53cdd82ffa721667b15"
+    sha256 "bab6c2f6bd6c53de04ef8eb23c9534ce881f7f3e20c9ccf8908ace4513a7bf1b"
   end
   on_intel do
-    version "7.0.203,08b3b509-05e6-4df1-84e1-a76c8855d899,d9b9a2d8ef9788f345d97304ceb67b07"
-    sha256 "82e6bfd3301d1b718e136fdaa1dd23d6abb03adbca05fcbc268b0da7e0d65b46"
+    version "7.0.302,8cf422e2-ae69-4285-944f-b685058be7b1,811763e4723370528b3097c5c1c8ad92"
+    sha256 "24f88a37ccbd64878dba3c89f4eada1b3709be9abf33faedb63f1d1b9542a28c"
   end
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv.second}/#{version.csv.third}/dotnet-sdk-#{version.csv.first}-osx-#{arch}.pkg"
