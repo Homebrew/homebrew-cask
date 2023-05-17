e211aed206daa6e95130f4ac5d63e7d749ec1693
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-16 20:10:12 -0400

    descript 64.1.0-release.20230516.17239

--- Casks/descript.rb
+++ Casks/descript.rb
@@ -1,9 +1,9 @@
 cask "descript" do
   arch arm: "-arm64"
 
-  version "63.1.7-release.20230516.17203"
-  sha256 arm:   "5c0d856e55805af116450bc068dec9fdadfdf4fa3c0cae79839d4005c93d1fa6",
-         intel: "70b987eb8f3bb597eb431055b96fedddb5b6b83466dd92fb0148cf14f7da8e82"
+  version "64.1.0-release.20230516.17239"
+  sha256 arm:   "b6248cc28cdc2e44f2890f21dcfbb6040b03b89f13ba8489fb892e1e4366c3a7",
+         intel: "d51d5b51b1791a12826c20aaac411e0001d63a2a9031c4d94792cbb0710fc3cc"
 
   url "https://electron.descript.com/Descript-#{version}#{arch}.dmg"
   name "Descript"
