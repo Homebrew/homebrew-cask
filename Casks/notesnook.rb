6a3b8893684c3a0340d8a4f812927c12b2f679f5
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-17 09:40:57 -0400

    notesnook 2.4.11 (#147361)

--- Casks/notesnook.rb
+++ Casks/notesnook.rb
@@ -1,9 +1,9 @@
 cask "notesnook" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.4.10"
-  sha256 arm:   "1eb98e21f274fd327f1b6ce5acc352482396eae408676aa5a1dac2652257ebc3",
-         intel: "5ec7ac060971b76d3fc9bac51a0b90a76584a25f972980d2ee78339862a405bd"
+  version "2.4.11"
+  sha256 arm:   "ad912e0578ca01130af15031561cc708f2649af83c47c3d58d70ade76c0e3f65",
+         intel: "c16b86dc3fa6cd808a50b9e3923917af2d0d917f8af2a5b063ac400fce3b1f17"
 
   url "https://github.com/streetwriters/notesnook/releases/download/v#{version}/notesnook_mac_#{arch}.dmg",
       verified: "github.com/streetwriters/notesnook/"
