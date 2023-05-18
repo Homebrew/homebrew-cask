725fb7abe24264e09478a14aee7baf727788518e
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-18 11:03:18 -0400

    readyapi 3.46.0 (#147447)

--- Casks/readyapi.rb
+++ Casks/readyapi.rb
@@ -1,9 +1,9 @@
 cask "readyapi" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.45.0"
-  sha256 arm:   "0bacba271319e30e9093a163bf5aa84d719944cfc3e6c2b1bb20493e9ea85b68",
-         intel: "c106d25c5cd26704997af2e351ac7688ad552020115ea5437ad7bc56f9548da1"
+  version "3.46.0"
+  sha256 arm:   "14081de8e3b6d8f2c78b43f062b59faff0075c8c3c5666ae6e8356d64e14ecbe",
+         intel: "b638ff75118647fb365d7a3fe9e695e4ea62b3d98e2c436e1f040db1f4bb0a2a"
 
   url "https://dl.eviware.com/ready-api/#{version}/ReadyAPI-#{arch}-#{version}.dmg",
       verified: "dl.eviware.com/ready-api/"
