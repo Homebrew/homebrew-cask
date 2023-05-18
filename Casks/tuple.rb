ff35424b1d01251611ba077b0cd15a539e2762aa
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-17 23:01:04 -0400

    tuple 0.103.0 (#147422)

--- Casks/tuple.rb
+++ Casks/tuple.rb
@@ -1,6 +1,6 @@
 cask "tuple" do
-  version "0.102.0,2023-04-27,eefb3078d"
-  sha256 "626e92b310aa68f846058712507a3331711e24e257ec5081de6bb932312b4334"
+  version "0.103.0,2023-05-17,215eea2e2"
+  sha256 "27773035822c6840d9220faa5ba5f8871184e0ffecb0507326dd9e7b8a4196da"
 
   url "https://d32ifkf9k9ezcg.cloudfront.net/production/sparkle/tuple-#{version.tr(",", "-")}.zip",
       verified: "d32ifkf9k9ezcg.cloudfront.net/"
