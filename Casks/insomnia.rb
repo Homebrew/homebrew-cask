2f70f3ac427f3bb8478eb2fe4d2873bc35cfa0ab
insomnia-infra <88453862+insomnia-infra@users.noreply.github.com>
2023-05-18 00:10:34 +0000

    insomnia 2023.2.2

--- Casks/insomnia.rb
+++ Casks/insomnia.rb
@@ -1,6 +1,6 @@
 cask "insomnia" do
-  version "2023.2.1"
-  sha256 "38c38cb2acf1c31bbdc0935b54c2cf11ff0bf4f4762ceb1a070da9d306b0a3dd"
+  version "2023.2.2"
+  sha256 "eed26b9b3487d6afef7ee3c2bc4d7374e0714a6ec874fd9769eee08bab47e9d8"
 
   url "https://github.com/Kong/insomnia/releases/download/core%40#{version}/Insomnia.Core-#{version}.dmg",
       verified: "github.com/Kong/insomnia/"
