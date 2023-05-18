4997bb3e1449a5bc19f44aef4f65eeffa39334a8
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-17 23:02:21 -0400

    quicken 7.0.4 (#147415)

--- Casks/quicken.rb
+++ Casks/quicken.rb
@@ -1,6 +1,6 @@
 cask "quicken" do
-  version "7.0.3,700.48782.100"
-  sha256 "82256af556552304c4bc1183f679b5304e86b67c700e8263eafdf58b38db3c88"
+  version "7.0.4,700.48787.100"
+  sha256 "f4abff450ec2819612271fc98a129ff9bbed02f76289320cbca31dbf3f10b92b"
 
   url "https://download.quicken.com/mac/Quicken/001/Release/031A96D9-EFE6-4520-8B6A-7F465DDAA3E4/Quicken-#{version.csv.second}/Quicken-#{version.csv.second}.zip"
   name "Quicken"
