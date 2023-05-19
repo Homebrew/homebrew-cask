0726eed32c15c35c5c6c92e0acddde11d7222858
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-18 23:38:48 -0400

    syncovery 10.5.5 (#147517)
    \n

--- Casks/syncovery.rb
+++ Casks/syncovery.rb
@@ -1,9 +1,9 @@
 cask "syncovery" do
   arch arm: "-Apple"
 
-  version "10.5.3"
-  sha256 arm:   "c34723b35ceae358c9338c087837a39993442564bc9b340d0c0bd77cd86c6053",
-         intel: "10bb35191481ba180cba2c6b410dabc4be39999ef642da84d65eb6073445467f"
+  version "10.5.5"
+  sha256 arm:   "c2879a8bae1b7a9d05a7dc3a20c7d997456268b61560287887422ed3872a422c",
+         intel: "d45752df8bc7e5a6c7cc429859944cfd01bf9f3d171b4035399603b88649b6e9"
 
   url "https://www.syncovery.com/release/SyncoveryMac#{version}#{arch}.dmg"
   name "Syncovery"
