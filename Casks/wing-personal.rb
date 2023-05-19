ff810af435b35172357541ad98137029f92bc8a9
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-18 23:52:42 -0400

    wing-personal 9.1.1.1

--- Casks/wing-personal.rb
+++ Casks/wing-personal.rb
@@ -1,6 +1,6 @@
 cask "wing-personal" do
-  version "9.1.1.0"
-  sha256 "bcc1515925c5d1e1f16130cd0004182c7b568757364963deab03e640a6ec624d"
+  version "9.1.1.1"
+  sha256 "7d418b17fa789080f92dfbfb9745ef93cf0023c747f63dd69e7e03facc44db6f"
 
   url "https://wingware.com/pub/wing-personal/#{version}/wing-personal-#{version}.dmg"
   name "Wing Personal"
