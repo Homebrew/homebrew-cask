6ce81a4e59bb9b8d078a4da3118784071f306cd3
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-19 14:43:20 -0400

    Update local.rb

--- Casks/local.rb
+++ Casks/local.rb
@@ -1,19 +1,20 @@
 cask "local" do
-  arch arm: "mac-arm64", intel: "mac"
+  arch arm: "-arm64"
 
   version "6.7.2,6375"
   sha256  arm:   "577e8c0da79dfc33150497d085cc15b8b905b48fb83d7553a1b160cb3587268d",
           intel: "74e23f5db5a7a05e660695c34081833517b72ec83e6513a3b7bd6fc3c50f09ad"
 
-  url "https://cdn.localwp.com/releases-stable/#{version.csv.first}+#{version.csv.second}/local-#{version.csv.first}-#{arch}.dmg"
+  url "https://cdn.localwp.com/releases-stable/#{version.csv.first}+#{version.csv.second}/local-#{version.csv.first}-mac#{arch}.dmg"
   name "Local"
   desc "WordPress local development tool by Flywheel"
   homepage "https://localwp.com/"
 
   livecheck do
-    url "https://cdn.localwp.com/stable/latest/#{arch}"
+    url "https://cdn.localwp.com/stable/latest/mac#{arch}"
+    regex(%r{/(\d+(?:\.\d+)+)\+(\d+)/})
     strategy :header_match do |headers|
-      match = headers["location"].match(%r{/(\d+(?:\.\d+)+)\+(\d+)/})
+      match = headers["location"].match(regex)
       next if match.blank?
 
       "#{match[1]},#{match[2]}"
