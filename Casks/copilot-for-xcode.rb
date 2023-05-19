25b826404c7bd38fd49ab6e97ad8fcae0f6593ba
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-18 22:32:17 -0400

    copilot-for-xcode 0.16.0

--- Casks/copilot-for-xcode.rb
+++ Casks/copilot-for-xcode.rb
@@ -1,6 +1,6 @@
 cask "copilot-for-xcode" do
-  version "0.15.4"
-  sha256 "59584d935fe1a1e463cda4742742d7b4c043b082dcd78e3ef2e82f2939898a71"
+  version "0.16.0"
+  sha256 "4e893c0ba3fa9c686322e397025c172f9b2af9c5a1103553c10330fb6491c24c"
 
   url "https://github.com/intitni/CopilotForXcode/releases/download/#{version}/Copilot.for.Xcode.app.zip"
   name "Copilot for Xcode"
