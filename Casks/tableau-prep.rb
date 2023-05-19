b45e00b57b261fdd3c3457c555880ff3e5d05b0e
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-19 06:45:36 -0400

    add zap

--- Casks/tableau-prep.rb
+++ Casks/tableau-prep.rb
@@ -31,4 +31,19 @@ cask "tableau-prep" do
     "com.tableausoftware.telemetry",
     "simba.sparkodbc",
   ]
+
+  zap trash:  [
+        "~/Documents/My Tableau Prep Repository",
+        "~/Library/Application Support/Tableau Prep Builder #{version}",
+        "~/Library/Caches/com.tableau.caching",
+        "~/Library/Preferences/com.tableau.Tableau-Prep-tableau-#{version.major}-#{version.minor}.plist",
+        "~/Library/Preferences/com.tableausoftware.tableauprep.plist",
+        "~/Library/Preferences/com.tableausoftware.tabminerva.plist",
+        "~/Library/Saved Application State/com.tableausoftware.tableauprep.savedState",
+        "~/Library/Tableau",
+      ],
+      delete: [
+        "/Library/Application Support/Tableau Prep Builder",
+        "/Library/Preferences/FLEXnet Publisher",
+      ]
 end
