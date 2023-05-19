b36ebabcc420727652414a258674d5049b181001
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-19 07:22:15 -0400

    tableau-prep 2023.1.2 (#147519)
    \n

--- Casks/tableau-prep.rb
+++ Casks/tableau-prep.rb
@@ -1,6 +1,6 @@
 cask "tableau-prep" do
-  version "2023.1.0"
-  sha256 "a8580322734b822e71fed353e9f24005fb79b152ec8d4b1126a9c3b2d7bf95e3"
+  version "2023.1.2"
+  sha256 "98b51ed5853d663df3756415c842f861d8e05b20ed17ada9473dc8a7c8d4267a"
 
   url "https://downloads.tableau.com/esdalt/tableau_prep/#{version}/TableauPrep-#{version.dots_to_hyphens}.dmg"
   name "Tableau Prep"
@@ -31,4 +31,19 @@ cask "tableau-prep" do
     "com.tableausoftware.telemetry",
     "simba.sparkodbc",
   ]
+
+  zap trash:  [
+        "~/Documents/My Tableau Prep Repository",
+        "~/Library/Application Support/Tableau Prep Builder #{version}",
+        "~/Library/Caches/com.tableau.caching",
+        "~/Library/Preferences/com.tableau.Tableau-Prep-tableau-#{version.major}-#{version.minor}.plist",
+        "~/Library/Preferences/com.tableausoftware.tableauprep.plist",
+        "~/Library/Preferences/com.tableausoftware.tabminerva.plist",
+        "~/Library/Saved Application State/com.tableausoftware.tableauprep.savedState",
+        "~/Library/Tableau",
+      ],
+      delete: [
+        "/Library/Application Support/Tableau Prep Builder",
+        "/Library/Preferences/FLEXnet Publisher",
+      ]
 end
