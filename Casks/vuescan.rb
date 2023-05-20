31e156ed2c46337dc25d81b7870b241a244d2d98
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-20 06:48:47 -0400

    vuescan 9.8.01 (#147576)

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,5 +1,5 @@
 cask "vuescan" do
-  version "9.7.99"
+  version "9.8.01"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.hamrick.com/files/vuea64#{version.major_minor.no_dots}.dmg"
@@ -13,4 +13,13 @@ cask "vuescan" do
   end
 
   app "VueScan.app"
+
+  zap trash: [
+        "~/.vuescanrc",
+        "~/Library/Preferences/com.hamrick.vuescan.plist",
+        "~/Library/Saved Application State/com.hamrick.vuescan.savedState",
+        "~/Pictures/VueScan/vuescan.ini",
+        "~/Pictures/VueScan/vuescan.log",
+      ],
+      rmdir: "~/Pictures/VueScan"
 end
