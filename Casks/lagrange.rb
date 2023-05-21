16e2579c5dfa5648651e78bb0f6b4d734310ddec
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-21 06:31:08 -0400

    lagrange 1.16.0

--- Casks/lagrange.rb
+++ Casks/lagrange.rb
@@ -1,9 +1,9 @@
 cask "lagrange" do
   arch arm: "macos11.0-arm64", intel: "macos10.13-x86_64"
 
-  version "1.15.9"
-  sha256 arm:   "43053ff8de20d0b16339d4762c9abdba8c9b5479900f53714fa0d6ec10924a03",
-         intel: "10208368e95d84bc4fccf32dd3bcddabb2400f017afc21cb07b4c42181419756"
+  version "1.16.0"
+  sha256 arm:   "d38eb0f244b26f6151999868e2ad2adc061d5f2bc0450c9cc494a794d404b0dc",
+         intel: "0fb618e028b4aa9c8d7b7fb695efe8b8d971a8518da5b21e53092145c0b5406f"
 
   url "https://github.com/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_#{arch}.tbz",
       verified: "github.com/skyjake/lagrange/"
