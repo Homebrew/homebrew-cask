a66ce3a098406090e84993bd2633a0e5639dff52
Bevan Kay <email@bevankay.me>
2023-05-21 22:11:38 +1000

    castr 2.6.0 (#147618)
    \n

--- Casks/castr.rb
+++ Casks/castr.rb
@@ -1,11 +1,11 @@
 cask "castr" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.6.0"
-  sha256 arm:   "e61107f6f60b58c9507a9ce4889d5ef3f2409891f0868d32995b91cf2179bf6e",
-         intel: "982363c40112102a24c990ff333446a113b7980cfe4c8e161d3b7d6692447e79"
+  version "2.6.0"
+  sha256 arm:   "fc4381771318927d9531d23632bbf3720c432c44795f03e3b719dc41d0b47a7c",
+         intel: "47bd863483f5360f6b08f1ad0dd593ce23eb1798c271393c021393878eab2771"
 
-  url "https://download.todesktop.com/210610elr9v3cm6/Castr%20#{version}-#{arch}.dmg",
+  url "https://download.todesktop.com/210610elr9v3cm6/Castr%20#{version}-#{arch}-mac.zip",
       verified: "download.todesktop.com/210610elr9v3cm6/"
   name "castr"
   desc "Desktop application for controlling Castr streaming platform"
