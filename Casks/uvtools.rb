e46c53145494de751ed7f52500a348a01856980d
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-21 07:53:40 -0400

    uvtools 3.13.4 (#147608)

--- Casks/uvtools.rb
+++ Casks/uvtools.rb
@@ -1,9 +1,9 @@
 cask "uvtools" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.13.3"
-  sha256 arm:   "19ff74eedd374c9cf8bd0e162b420bae4cf9ce729cba437e2ffaa267fbdca2f7",
-         intel: "73de6608da8edf3a90112888998ac924c8f8935914f62296bc8fd5f93fc137f1"
+  version "3.13.4"
+  sha256 arm:   "2d202574308ed64a3f5fdc7fca41379588ffc97bf03ec2af707607f3c5a277f0",
+         intel: "439f7c8076eb0dd5e7e59044d85a460317538ec5c10aaba61bac94f9a8ad49ad"
 
   url "https://github.com/sn4k3/UVtools/releases/download/v#{version}/UVtools_osx-#{arch}_v#{version}.zip"
   name "UVtools"
