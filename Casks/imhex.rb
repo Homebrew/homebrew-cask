938e9a7bdaffec5c7ae87e6cf1964bddde21cab6
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-21 06:31:31 -0400

    imhex 1.29.0

--- Casks/imhex.rb
+++ Casks/imhex.rb
@@ -1,6 +1,6 @@
 cask "imhex" do
-  version "1.28.0"
-  sha256 "ca5b33700f233d615de700a86fec48144ce1f40372ffda6254cd3c64a68e77b5"
+  version "1.29.0"
+  sha256 "15ea85abd9db68d3b55161cdd385cc9765647db9c8a0dc2f91653ac82b5fbe84"
 
   url "https://github.com/WerWolv/ImHex/releases/download/v#{version}/imhex-#{version}-macOS-x86_64.dmg",
       verified: "github.com/WerWolv/ImHex/"
