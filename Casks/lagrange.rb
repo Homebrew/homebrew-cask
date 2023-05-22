da4a9e748793a4b59b1b31aa12538514d0504dcf
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-22 06:19:47 -0400

    Update lagrange.rb

--- Casks/lagrange.rb
+++ Casks/lagrange.rb
@@ -1,11 +1,11 @@
 cask "lagrange" do
-  arch arm: "macos11.0-arm64", intel: "macos10.13-x86_64"
+  arch arm: "11.0-arm64", intel: "10.13-x86_64"
 
   version "1.16.1"
   sha256 arm:   "fa9379718a214e1efcd0db9c67ae0a2f372984ea39a026c9e6c6bff5647e7386",
          intel: "2c5209caac3e0e3c44a8259a59ef816ee55cfb7564c3623933a39c3aab4a45d9"
 
-  url "https://github.com/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_#{arch}.tbz",
+  url "https://github.com/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_macos#{arch}.tbz",
       verified: "github.com/skyjake/lagrange/"
   name "Lagrange"
   desc "Desktop GUI client for browsing Geminispace"
