ce607c8ead2ec7daa2d561780fb04cc5674c0dc7
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-22 08:53:17 -0400

    visual-paradigm 17.1

--- Casks/visual-paradigm.rb
+++ Casks/visual-paradigm.rb
@@ -1,9 +1,9 @@
 cask "visual-paradigm" do
   arch arm: "AArch64", intel: "WithJRE"
 
-  version "17.0,20230401"
-  sha256 arm:   "5213cfaf52fb3cd4bcceccd48c7d7b604282cf94f8e6971970a6f026c664acda",
-         intel: "a6ebfba1b196f24b32e15bf104a0f3788ea3f73910672999b1d102d81b290a49"
+  version "17.1,20230520"
+  sha256 arm:   "5167d37ee5508b74db01c60f73c7eaa82069a1e18eaa5e08e0ff5a6bbafbc097",
+         intel: "cd07a2f2f8b1fb594f403e38cb7a597a4989aa5193f2a548392c6e141497bf52"
 
   url "https://www.visual-paradigm.com/downloads/vp#{version.csv.first}/#{version.csv.second}/Visual_Paradigm_#{version.csv.first.dots_to_underscores}_#{version.csv.second}_OSX_#{arch}.dmg"
   name "Visual Paradigm"
