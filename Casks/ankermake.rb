d20db62c94103ee5a1e12d70d322d4f1aae8a600
Patrick Linnane <patrick@linnane.io>
2023-05-22 12:10:24 -0700

    ankermake: fix version

--- Casks/ankermake.rb
+++ Casks/ankermake.rb
@@ -1,7 +1,7 @@
 cask "ankermake" do
   arch arm: "M1", intel: "Intel"
 
-  version "0.9.22_3"
+  version "2.0.0"
   sha256 :no_check
 
   url "https://public-make-moat-us.s3.us-east-2.amazonaws.com/ankermake/slicer/AnkerMake_Installer_Mac_#{arch}.dmg",
