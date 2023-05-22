c9c32b545a2a37933098f9e43bc2466215989035
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-22 05:05:41 +0000

    grandtotal 8.0.9.14

--- Casks/grandtotal.rb
+++ Casks/grandtotal.rb
@@ -1,6 +1,6 @@
 cask "grandtotal" do
-  version "8.0.9.13"
-  sha256 "96d68331bd184a4c72c298585e431790ecb4ddcf065a235cbcf1657b23a3a413"
+  version "8.0.9.14"
+  sha256 "08ebb293043e9db695cf888d25d51baaef1f4ddb155eb97fffe6485d2da271e4"
 
   url "https://www.mediaatelier.com/GrandTotal#{version.major}/GrandTotal_#{version}.dmg"
   name "GrandTotal"
