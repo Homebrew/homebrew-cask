f2482e80ccfc3091f4523eda616c8b5e777f6c3d
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-22 08:27:27 -0400

    splashtop-streamer 3.5.8.0

--- Casks/splashtop-streamer.rb
+++ Casks/splashtop-streamer.rb
@@ -1,6 +1,6 @@
 cask "splashtop-streamer" do
-  version "3.5.6.0"
-  sha256 "1b4863eceb89e2d5fa3e58c160cf30d8bd531f8aeda962d7f4c18f63b9210345"
+  version "3.5.8.0"
+  sha256 "1221148e778ab0e0088b5b88cd87056f462ef5c5ea206bc42e8070c05626c673"
 
   url "https://d17kmd0va0f0mp.cloudfront.net/mac/Splashtop_Streamer_Mac_INSTALLER_v#{version}.dmg",
       verified: "d17kmd0va0f0mp.cloudfront.net/"
