1fc2a33bcf363bad1ab7be345a30e75bacd383c6
Bevan Kay <email@bevankay.me>
2023-05-23 22:09:50 +1000

    midiview: update livecheck, version url, add shasum (#147678)
    \n

--- Casks/midiview.rb
+++ Casks/midiview.rb
@@ -1,15 +1,18 @@
 cask "midiview" do
-  version "1.2.2"
-  sha256 :no_check
+  version "1.2.0,Ke6OYmrQ"
+  sha256 "25ea4e903c7eba3c3768c7de5d2dea16aeccf43812ce1e596d4959de4cdf769f"
 
-  url "https://hautetechnique.com/apps/backend/v1/apps/midiview/latest/macOS"
+  url "https://hautetechnique.com/apps/backend/v1/installers/#{version.csv.second}"
   name "Midi View"
   desc "Monitor MIDI inputs and outputs"
   homepage "https://hautetechnique.com/midi/midiview/"
 
   livecheck do
-    url :url
-    strategy :header_match
+    url "https://hautetechnique.com/apps/backend/v1/apps/midiview/cast"
+    regex(%r{/installers/(.+)$}i)
+    strategy :sparkle do |item|
+      "#{item.short_version},#{item.url[regex, 1]}"
+    end
   end
 
   auto_updates true
