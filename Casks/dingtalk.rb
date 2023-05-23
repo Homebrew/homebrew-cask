2d3b6980356cf380c5a323e88be3dec91eadf49e
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-23 11:46:59 -0400

    dingtalk 7.0.32.5_29965244 (#147709)
    * dingtalk 7.0.32.5_29965244
    
    * dingtalk (update intel)

--- Casks/dingtalk.rb
+++ Casks/dingtalk.rb
@@ -4,12 +4,12 @@ cask "dingtalk" do
   linkid = on_arch_conditional arm: "qd=2022mac-m1"
 
   on_arm do
-    version "7.0.22.6_29200526"
-    sha256 "ef9280d4676e41fbae99ea821ef71a4d76a76a4afdc360b3bd83e0106745c672"
+    version "7.0.32.5_29965244"
+    sha256 "48670f0cfca37a0de4d3c41ab48df05bfa8c4d659a0bec063c4465e62dbca14f"
   end
   on_intel do
-    version "7.0.20.13_29200617"
-    sha256 "90c33c4afa88f459fbe9070e5b8b4338a4c07addf65c51b5e63b1498eb89b508"
+    version "7.0.30.12_29965215"
+    sha256 "f1efc5d16fbd75a7ffa9442d8d8e5b5a6b23956c2aca1343d8d95ad485893511"
   end
 
   url "https://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/#{folder}DingTalk_v#{version}_#{arch}.dmg"
