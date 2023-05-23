4001c9fbe7ab985a095c721e4ea8aa29122570d8
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-23 19:20:15 -0400

    microsoft-outlook 16.73.23052000 (#147774)

--- Casks/microsoft-outlook.rb
+++ Casks/microsoft-outlook.rb
@@ -20,8 +20,8 @@ cask "microsoft-outlook" do
     sha256 "bddede85956713be21fdb5ab72be07ecefd05552752e8e60c649e6a15fd0a2c2"
   end
   on_big_sur :or_newer do
-    version "16.73.23051401"
-    sha256 "2228e229033a19a03619dd4065649f851f2d81a60b0d49d6cde40cba172d636f"
+    version "16.73.23052000"
+    sha256 "64be32aa0e8324f270951b56dffc59e5711ef6084e285945eb455ba90bef8f1c"
   end
 
   url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Outlook_#{version}_Installer.pkg",
