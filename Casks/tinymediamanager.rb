573b152fb0d57bd7b001649a334aa1c3642aa279
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-23 19:20:45 -0400

    tinymediamanager 4.3.11.1 (#147783)

--- Casks/tinymediamanager.rb
+++ Casks/tinymediamanager.rb
@@ -1,6 +1,6 @@
 cask "tinymediamanager" do
-  version "4.3.11"
-  sha256 "f338cf45d77d9a2ef5b66092af7178c9e99cc6526f5d8a566b8f731a6e446ac3"
+  version "4.3.11.1"
+  sha256 "46a6317a4cd0ae96271d7dc8a38a3eca1310b6753270488349f1c80eebab971f"
 
   url "https://release.tinymediamanager.org/v#{version.major}/dist/tmm_#{version}_macos-x86_64.zip"
   name "tinyMediaManager"
