83853ec79ee6a48cff80693596242b35ac5e07cc
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-23 10:18:05 -0400

    trainerroad 2023.19.0.247

--- Casks/trainerroad.rb
+++ Casks/trainerroad.rb
@@ -1,6 +1,6 @@
 cask "trainerroad" do
-  version "2023.18.0.246"
-  sha256 "5a6e851427c796d834903ad3e27783bc787cc36ac2ce083409cd50e1097f2dea"
+  version "2023.19.0.247"
+  sha256 "74e920dfc03697f5489269588c2d169393e89df7db959802f56cc48684c64d69"
 
   url "https://trainrdtrcmn01un1softw01.blob.core.windows.net/installers/mac/v001/Production/TrainerRoad-#{version}.dmg",
       verified: "trainrdtrcmn01un1softw01.blob.core.windows.net/"
