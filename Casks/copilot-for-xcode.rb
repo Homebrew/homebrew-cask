0186edb14b58652e4b8c2eb3805d8d427e3ee582
BrewTestBot <ops@brew.sh>
2023-05-23 06:12:06 +0100

    copilot-for-xcode 0.16.1 (#147679)
    \n

--- Casks/copilot-for-xcode.rb
+++ Casks/copilot-for-xcode.rb
@@ -1,6 +1,6 @@
 cask "copilot-for-xcode" do
-  version "0.16.0"
-  sha256 "4e893c0ba3fa9c686322e397025c172f9b2af9c5a1103553c10330fb6491c24c"
+  version "0.16.1"
+  sha256 "7869b307219213dcb17e94b5247f001cf3e31aaefbb776a791cb7bd685b56020"
 
   url "https://github.com/intitni/CopilotForXcode/releases/download/#{version}/Copilot.for.Xcode.app.zip"
   name "Copilot for Xcode"
