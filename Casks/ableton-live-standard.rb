403b6c12fdc9a3ef93133996d941394dfa33722f
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-23 11:47:25 -0400

    ableton-live-standard 11.3.2 (#147697)

--- Casks/ableton-live-standard.rb
+++ Casks/ableton-live-standard.rb
@@ -1,6 +1,6 @@
 cask "ableton-live-standard" do
-  version "11.2.11"
-  sha256 "0516a702d0b3c48e8c8a2578717fc74fd411dd77b9f36564f7b50227843d9063"
+  version "11.3.2"
+  sha256 "cca9d19b29b8c9a9ac7e07f03db926bb9b7b0992b954f8e82d06d20a734caf66"
 
   url "https://cdn-downloads.ableton.com/channels/#{version}/ableton_live_standard_#{version}_universal.dmg"
   name "Ableton Live Standard"
