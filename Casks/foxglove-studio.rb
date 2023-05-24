a9a3aa80402d623ec9200d622459ce1fb4763bde
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-05-25 06:05:24 +0800

    foxglove-studio 1.54.1 (#147831)

--- Casks/foxglove-studio.rb
+++ Casks/foxglove-studio.rb
@@ -1,6 +1,6 @@
 cask "foxglove-studio" do
-  version "1.54.0"
-  sha256 "30e27382826830c50293c399c6875c0300c2e967a62ca6c0ce9fe9406d58e22e"
+  version "1.54.1"
+  sha256 "b33e1844b08960e3d9ec355718806cf4530f4ec62e05491d88829816cdce41d9"
 
   url "https://github.com/foxglove/studio/releases/download/v#{version}/foxglove-studio-#{version}-mac-universal.dmg",
       verified: "github.com/foxglove/studio/"
