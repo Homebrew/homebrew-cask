ac93d7ea7ed54e7db9dd1a270114fbb44e0459ce
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-24 08:56:39 -0400

    dropshare 5.32 (#147795)

--- Casks/dropshare.rb
+++ Casks/dropshare.rb
@@ -1,6 +1,6 @@
 cask "dropshare" do
-  version "5.31,5416"
-  sha256 "133e98b8fbb4878760b4aa2b95922f190490008811518d956dd8a0052038082e"
+  version "5.32,5421"
+  sha256 "5d9fbc732ef1a43a1992f4445476ec5415cdfdc970dd7f63a64fc3bb4ea5d955"
 
   url "https://d2wvuuix8c9e48.cloudfront.net/Dropshare#{version.major}-#{version.csv.second}.app.zip",
       verified: "d2wvuuix8c9e48.cloudfront.net/"
