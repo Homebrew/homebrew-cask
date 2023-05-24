7ef7dfeda8c445ab47c5e1c1976ed339bb8489d0
Bowen <17046239+MoienBowen@users.noreply.github.com>
2023-05-24 13:24:37 +0200

    Update and rename skiff.rb to skiff-desktop.rb

--- Casks/skiff-desktop.rb
+++ Casks/skiff-desktop.rb
@@ -1,8 +1,8 @@
-cask "skiff" do
+cask "skiff-desktop" do
   version "0.3.0"
   sha256 "fce28da19b2f0f0cd84b2ba232fe8f80c83bd76005c4dae77c68ebe29e04b345"
 
-  url "https://raw.githubusercontent.com/skiff-org/skiff-org.github.io/main/macos/Skiff Desktop #{version}.dmg",
+  url "https://raw.githubusercontent.com/skiff-org/skiff-org.github.io/main/macos/Skiff%20Desktop%20#{version}.dmg",
       verified: "raw.githubusercontent.com/skiff-org/skiff-org.github.io/main/macos/"
   name "Skiff Desktop"
   desc "End-to-end encrypted email, calendar, documents, and files support"
