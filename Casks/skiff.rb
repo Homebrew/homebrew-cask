6d50f4d6e2ab50cac5b757066a6db4219dc4495f
Bowen <17046239+MoienBowen@users.noreply.github.com>
2023-05-24 12:01:39 +0200

    New cask: Skiff

---
+++ Casks/skiff.rb
@@ -0,0 +1,23 @@
+cask "skiff" do
+  version "0.3.0"
+  sha256 "fce28da19b2f0f0cd84b2ba232fe8f80c83bd76005c4dae77c68ebe29e04b345"
+
+  url "https://raw.githubusercontent.com/skiff-org/skiff-org.github.io/main/macos/Skiff%20Desktop%20#{version}.dmg"
+  name "Skiff"
+  desc "End-to-end encrypted email, calendar, documents, and files that give you the power to communicate freely."
+  homepage "https://skiff.com"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+
+  app "Skiff Desktop.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/org.reactjs.native.Skiff-Desktop",
+    "~/Library/Containers/org.reactjs.native.Skiff-Desktop",
+  ]
+end
