e801ee67cee79328b924553215c9561b752e9b31
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-25 08:29:25 -0400

    rework cask

--- Casks/raycast.rb
+++ Casks/raycast.rb
@@ -1,25 +1,26 @@
 cask "raycast" do
-  
-  if MacOS.version <= :big_sur
+  on_big_sur :or_older do
     version "1.50.0"
     sha256 "F8BBD00D06636E3FE9F154FF69FF57C0A48A90A77AE58CDCC2B285F61A170A88"
-    depends_on macos: ">= :big_sur"
-  else
+    livcheck do
+      skip "Legacy version"
+    end
+  end
+  on_monterey :or_newer do
     version "1.52.0"
     sha256 "5E0DE474E0C1CB43D2677DC2FD87BD4AB6E7BAAE875496D216075DBA70100F52"
-    depends_on macos: ">= :monterey"
+    livecheck do
+      url "https://releases.raycast.com/releases"
+      strategy :json do |json|
+        json.map { |release| release["version"] }
+      end
+    end
   end
 
   url "https://releases.raycast.com/releases/#{version}/download?build=universal"
   name "Raycast"
   desc "Control your tools with a few keystrokes"
   homepage "https://raycast.com/"
-
-  livecheck do
-    url :url
-    regex(/Raycast[._-]v?(\d+(?:\.\d+)+)(?:[._-](\h+))[._-]universal\.dmg/i)
-    strategy :header_match
-  end
 
   auto_updates true
 
