11a6fc52cdec32ee325ca406ee15cd8a91e468d5
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-25 11:46:34 -0400

    mongodb-compass 1.37.0 (#147878)

--- Casks/mongodb-compass.rb
+++ Casks/mongodb-compass.rb
@@ -1,9 +1,9 @@
 cask "mongodb-compass" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.36.4"
-  sha256 arm:   "2d5d89cf646338e874db6e02697c8a404e8864506e659052736e1f772f1bc203",
-         intel: "55cbac6dcd88f36e18a870e846e87f7c353a1147127b861ed5b2a20b96ffd5fb"
+  version "1.37.0"
+  sha256 arm:   "6720a2640f6a263fd2098a982cc5733a3115b7effef6a6d97022959f171b41e8",
+         intel: "2a87a507e80458283fd12a77c4240ed4f6dfeb32470078875196ee2a79015370"
 
   url "https://downloads.mongodb.com/compass/mongodb-compass-#{version}-darwin-#{arch}.dmg"
   name "MongoDB Compass"
