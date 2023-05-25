7ae804d97f9da998e219856445ddab9443d07d0e
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-25 08:45:28 -0400

    final-cut-library-manager (add zap)

--- Casks/final-cut-library-manager.rb
+++ Casks/final-cut-library-manager.rb
@@ -16,4 +16,11 @@ cask "final-cut-library-manager" do
   depends_on macos: ">= :sierra"
 
   app "Final Cut Library Manager.app"
+
+  zap trash: [
+    "~/Library/Application Support/Final Cut Library Manager",
+    "~/Library/HTTPStorages/com.arcticwhiteness.finalcutlibrarymanager",
+    "~/Library/Preferences/com.arcticwhiteness.finalcutlibrarymanager.plist",
+    "~/Library/Saved Application State/com.arcticwhiteness.finalcutlibrarymanager.savedState",
+  ]
 end
