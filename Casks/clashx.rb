d659d74a7c40e279447e6afeb4dbd8e2546040be
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-25 08:50:17 -0400

    clashx 1.116.0 (#147859)
    \n

--- Casks/clashx.rb
+++ Casks/clashx.rb
@@ -1,6 +1,6 @@
 cask "clashx" do
-  version "1.115.1"
-  sha256 "ba313548e989bc75b48eddf83ede53179f9ec4f52f3f28dafb047e76abee5763"
+  version "1.116.0"
+  sha256 "09bb3cc8c7360b80058b74e8bdeef99aa18ef14a1bfe3aa434e11db824f2c990"
 
   url "https://github.com/yichengchen/clashX/releases/download/#{version}/ClashX.dmg"
   name "ClashX"
