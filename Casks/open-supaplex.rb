d3021a2b6abc108d90526a53d55d51ecef61e509
Mateusz Jabłoński <mateusz.piotr.jablonski@gmail.com>
2023-05-25 18:06:39 +0200

    Add OpenSupaplex.app v7.1.2

---
+++ Casks/open-supaplex.rb
@@ -0,0 +1,16 @@
+cask "open-supaplex" do
+  version "7.1.2"
+  sha256 "fad3e2916a09cb5765b19e37d1db3b4ba68b189246dbe20789016cbed168502e"
+
+  url "https://github.com/sergiou87/open-supaplex/releases/download/v#{version}/OpenSupaplex-macOS-v#{version}.zip"
+  name "OpenSupaplex"
+  desc "Reimplementation of the original Supaplex game in C and SDL"
+  homepage "https://github.com/sergiou87/open-supaplex"
+
+  livecheck do
+    url "https://github.com/sergiou87/open-supaplex/releases"
+    strategy :github_releases
+  end
+
+  app "OpenSupaplex.app"
+end
