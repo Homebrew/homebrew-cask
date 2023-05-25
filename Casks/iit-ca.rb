d5c1518ab9ce3f27ab6a27e054801f0bf4e9e185
Roman Ternavskyi <rternavskyi@gmail.com>
2023-05-25 23:29:57 +0300

    Update iit-ca.rb

--- Casks/iit-ca.rb
+++ Casks/iit-ca.rb
@@ -1,10 +1,15 @@
 cask "iit-ca" do
-  version "1.3"
+  version "1.3.1"
   sha256 :no_check
 
   url "https://iit.com.ua/download/productfiles/EUInstall.pkg"
   name "IIT Certification"
-  desc "Користувач центру сертифікації ключів"
+  desc "The program is designed to be used on the computer equipment of the clients of the Qualified EDS Provider of the State Tax Service of Ukraine and performs the following functions
+        - application of a qualified electronic signature or seal to any information in electronic form (text, video, audio files, database files, etc.), as well as for cryptographic protection of information by means of its directed encryption
+        - generation of keys of applicants of the Qualified EDS Provider of the State Tax Service of Ukraine, backup of the personal key from one key information carrier to another, destruction of the personal key;
+        - verification of the user certificate;
+        - forming and submitting a request to block/cancel the user certificate to the Qualified EDS Provider of the State Tax Service of Ukraine;
+        - access to certificates of the Qualified EDS Provider of the State Tax Service of Ukraine, servers of the Qualified EDS Provider of the State Tax Service of Ukraine, certificates of other users and the list of revoked certificates for the purpose of viewing, search for user certificates in the file storage, determine the status of user certificates, check the integrity of certificates."
   homepage "https://iit.com.ua/"
 
   pkg "EUInstall.pkg"
