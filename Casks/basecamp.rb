7341fe1080b367a3dc8e8c2093f0c9f4773bc9df
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-25 08:22:00 -0400

    basecamp 3,2.3.9

--- Casks/basecamp.rb
+++ Casks/basecamp.rb
@@ -1,9 +1,9 @@
 cask "basecamp" do
   arch arm: "_arm64"
 
-  version "3,2.3.8"
-  sha256 arm:   "c15bf207ee8cf4525631cad322700df02690ef8a95cf64c242674afe72608fdb",
-         intel: "3b7bdb06c7ec63da72fafe20bef0acd10c8e71f548e356a6a3537dbaba524088"
+  version "3,2.3.9"
+  sha256 arm:   "b9d5ae54c1b497352ce4fbade58b8a6d46c283204f72075b765ec7fedbf2649c",
+         intel: "ee692f0462998ac16ea14c812fee83b3598fa548d19c3500ad8eeba03f8273d7"
 
   url "https://bc#{version.major}-desktop.s3.amazonaws.com/mac#{arch}/basecamp#{version.major}-#{version.csv.second}.zip",
       verified: "bc3-desktop.s3.amazonaws.com/"
