7febe477e63571c882bb6248de990f1eb6d1ec94
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-25 19:27:53 -0400

    raycast: rework (#147901)
    * raycast: rework
    
    Co-authored-by: Upamanyu Das <upamanyudas@hotmail.com>
    
    * revert livecheck
    
    * Update raycast.rb
    
    ---------
    
    Co-authored-by: Upamanyu Das <upamanyudas@hotmail.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/raycast.rb
+++ Casks/raycast.rb
@@ -1,20 +1,29 @@
 cask "raycast" do
-  version "1.52.1"
-  sha256 "05a659c736da0c9ac2fe1ac12106f3ff0ea4671067e6115ded48448e89662a03"
+  on_big_sur :or_older do
+    version "1.50.0"
+    sha256 "f8bbd00d06636e3fe9f154ff69ff57c0a48a90a77ae58cdcc2b285f61a170a88"
+
+    livecheck do
+      skip "Legacy version"
+    end
+  end
+  on_monterey :or_newer do
+    version "1.52.1"
+    sha256 "05a659c736da0c9ac2fe1ac12106f3ff0ea4671067e6115ded48448e89662a03"
+
+    livecheck do
+      url :url
+      regex(/Raycast[._-]v?(\d+(?:\.\d+)+)(?:[._-](\h+))[._-]universal\.dmg/i)
+      strategy :header_match
+    end
+  end
 
   url "https://releases.raycast.com/releases/#{version}/download?build=universal"
   name "Raycast"
   desc "Control your tools with a few keystrokes"
   homepage "https://raycast.com/"
 
-  livecheck do
-    url :url
-    regex(/Raycast[._-]v?(\d+(?:\.\d+)+)(?:[._-](\h+))[._-]universal\.dmg/i)
-    strategy :header_match
-  end
-
   auto_updates true
-  depends_on macos: ">= :big_sur"
 
   app "Raycast.app"
 
