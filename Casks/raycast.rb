32a36f9d96ffa37fac267122b5d4c164fd29490f
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-25 08:47:50 -0400

    Update raycast.rb

--- Casks/raycast.rb
+++ Casks/raycast.rb
@@ -2,7 +2,7 @@ cask "raycast" do
   on_big_sur :or_older do
     version "1.50.0"
     sha256 "F8BBD00D06636E3FE9F154FF69FF57C0A48A90A77AE58CDCC2B285F61A170A88"
-    livcheck do
+    livecheck do
       skip "Legacy version"
     end
   end
