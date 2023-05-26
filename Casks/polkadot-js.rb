81ac5af6783da573dc328e51747d1896d3b5ceef
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-26 09:17:12 -0400

    polkadot-js 0.131.4

--- Casks/polkadot-js.rb
+++ Casks/polkadot-js.rb
@@ -1,6 +1,6 @@
 cask "polkadot-js" do
-  version "0.130.1"
-  sha256 "b5269824b61d523f6897cbadf2d4956a230ec35658935fd7d1fdf50319d4bdff"
+  version "0.131.4"
+  sha256 "97147b8090a1ff52c49e1c7b86759bf43d902c803ea385ce7a01e1d5ca4ebd02"
 
   url "https://github.com/polkadot-js/apps/releases/download/v#{version}/Polkadot-JS-Apps-mac-#{version}.dmg",
       verified: "github.com/polkadot-js/apps/"
