82a92707a8b9e15781b58191ac3988bb5cec87c2
Miccal Matthews <miccal.matthews@gmail.com>
2023-05-26 10:49:07 +0800

    Update firefly-shimmer.rb

--- Casks/firefly-shimmer.rb
+++ Casks/firefly-shimmer.rb
@@ -1,18 +1,12 @@
 cask "firefly-shimmer" do
-  version "2.1.0"
-  sha256 "73ebfa0a52d803156af492c4ec4c6a87331ff4fa27ea86ceafca13e1359de0b8"
+  version "2.1.3"
+  sha256 "1f5398215f3095b1db5df76f6bd2cb6290a22aa85cb765c37985170995d403aa"
 
   url "https://github.com/iotaledger/firefly/releases/download/desktop-#{version}/firefly-desktop-#{version}.dmg",
       verified: "github.com/iotaledger/firefly/"
   name "Firefly Shimmer"
   desc "Official wallet for IOTA"
   homepage "https://firefly.iota.org/"
-
-  livecheck do
-    url :url
-    regex(/firefly[._-]desktop[._-]v?(\d+(?:\.\d+)+)/i)
-    strategy :github_latest
-  end
 
   auto_updates true
   depends_on macos: ">= :high_sierra"
