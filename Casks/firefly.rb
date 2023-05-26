ca37ab52082ec2328d88750d0efd2fc3ebe72085
Miccal Matthews <miccal.matthews@gmail.com>
2023-05-26 10:37:19 +0800

    Update firefly.rb

--- Casks/firefly.rb
+++ Casks/firefly.rb
@@ -2,21 +2,21 @@ cask "firefly" do
   version "1.7.2"
   sha256 "c62de21639fb0081150fd97bc2ca511197646a3826baef66b37a2be28a976424"
 
-  url "https://github.com/iotaledger/firefly/releases/download/desktop-#{version}/firefly-desktop-#{version}.dmg",
-      verified: "github.com/iotaledger/firefly/"
+  url "https://dl.firefly.iota.org/firefly-desktop-#{version}.dmg"
   name "Firefly"
   desc "Official wallet for IOTA"
   homepage "https://firefly.iota.org/"
 
   livecheck do
     url "https://dl.firefly.iota.org/latest-mac.yml"
-    regex(/desktop[._-]v?(\d+(?:\.\d+)+)/i)
+    regex(/firefly[._-]desktop[._-]v?(\d+(?:\.\d+)+)/i)
     strategy :yaml do |yaml|
       yaml["version"]
     end
   end
 
   auto_updates true
+  depends_on macos: ">= :el_capitan"
 
   app "Firefly.app"
 
