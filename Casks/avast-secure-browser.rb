95242a495574433366d72a86787ba7678daafc2e
Patrick Linnane <patrick@linnane.io>
2023-05-27 13:41:11 -0700

    avast-secure-browser 113.0.3821.127 (#147994)
    \n

--- Casks/avast-secure-browser.rb
+++ Casks/avast-secure-browser.rb
@@ -1,5 +1,5 @@
 cask "avast-secure-browser" do
-  version "113.0.3796.93"
+  version "113.0.3821.127"
   sha256 :no_check
 
   url "https://cdn-download.avastbrowser.com/AvastSecureBrowserSetup.pkg",
