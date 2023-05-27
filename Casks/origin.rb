203cb104c4453cb5ffb078b0fdcf3301e570c5bb
Patrick Linnane <patrick@linnane.io>
2023-05-27 09:28:49 -0700

    origin 10.5.123.53403

--- Casks/origin.rb
+++ Casks/origin.rb
@@ -1,5 +1,5 @@
 cask "origin" do
-  version "10.5.123.0000"
+  version "10.5.123.53403"
   sha256 :no_check
 
   url "https://origin-a.akamaihd.net/Origin-Client-Download/origin/mac/live/Origin.dmg",
