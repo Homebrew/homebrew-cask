b68b278325fe71b4d17199596e08ca80d0690066
Patrick Linnane <patrick@linnane.io>
2023-05-27 09:36:59 -0700

    sfdx 7.202.0

--- Casks/sfdx.rb
+++ Casks/sfdx.rb
@@ -1,7 +1,7 @@
 cask "sfdx" do
   arch arm: "arm64", intel: "x64"
 
-  version "7.201.6"
+  version "7.202.0"
   sha256 :no_check
 
   url "https://developer.salesforce.com/media/salesforce-cli/sfdx/channels/stable/sfdx-#{arch}.pkg"
