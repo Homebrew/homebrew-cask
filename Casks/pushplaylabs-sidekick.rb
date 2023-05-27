f73c2f4f9c2ad19ad6cf89594002548cf601876f
Patrick Linnane <patrick@linnane.io>
2023-05-27 13:46:41 -0700

    pushplaylabs-sidekick 112.45.1.33306 (#148021)
    \n

--- Casks/pushplaylabs-sidekick.rb
+++ Casks/pushplaylabs-sidekick.rb
@@ -3,12 +3,12 @@ cask "pushplaylabs-sidekick" do
   livecheck_folder = on_arch_conditional arm: "macm1", intel: "mac"
 
   on_arm do
-    version "112.44.2.32803,271ffa0"
-    sha256 "265d31759c4bc9281470337e1298ce00a3a20235d8e52380ca44154239459954"
+    version "112.45.1.33306,a499eb8"
+    sha256 "a42e9b9f754728fae3bbca3c2ace931d7f3c9c5cfa05bee7b75157ddf7f5e9e1"
   end
   on_intel do
-    version "112.44.2.32804,fbb6601"
-    sha256 "37806fc2bcbcc26f57ad94810c5780a53bf1aad8257ee99fa5e36d0e3ac8799c"
+    version "112.45.1.33307,29b1ece"
+    sha256 "d7fc0361f9331d1635e8ad9e7cf665adcc61eafbfc5aa4cc6c68f00b7223dd91"
   end
 
   url "https://cdn.meetsidekick.com/browser-builds/sidekick-mac-release-#{arch}-#{version.csv.first}-#{version.csv.second}-df.dmg"
