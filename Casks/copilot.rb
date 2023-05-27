f19018cca4428a8211ec590d5d7de2aa55558256
0az <30874884+0az@users.noreply.github.com>
2023-05-27 15:50:18 -0700

    copilot: new cask (#147988)
    Co-authored-by: Razvan Azamfirei <azamfireirazvan@gmail.com>

---
+++ Casks/copilot.rb
@@ -0,0 +1,25 @@
+cask "copilot" do
+  version "3.1.9,90-c7a9d755"
+  sha256 "c7a9d755adaf51e4f343e2db6b82b1f8aa113cb3f0d058d31ef2f928ad3f1a4f"
+
+  url "https://storage.googleapis.com/copilot-mac-releases/images/Copilot-#{version.csv.first}-#{version.csv.second}.dmg",
+      verified: "storage.googleapis.com/copilot-mac-releases/"
+  name "Copilot"
+  desc "Track and budget money"
+  homepage "https://copilot.money/"
+
+  livecheck do
+    url "https://releases.copilot.money/latest"
+    regex(/copilot[._-](\d+(?:\.\d+)+)-(\d+)-(\w+)/i)
+    strategy :header_match do |headers, regex|
+      headers["location"].scan(regex).map { |match| "#{match[0]},#{match[1]}-#{match[2]}" }
+    end
+  end
+
+  app "Copilot.app"
+
+  zap trash: [
+    "~/Library/Containers/com.copilot.production",
+    "~/Library/Group Containers/group.com.copilot.production",
+  ]
+end
