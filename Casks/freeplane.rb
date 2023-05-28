cdf113d8756e339993445416045072dc3f1e9f2a
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-28 19:25:13 -0400

    freeplane 1.11.4

--- Casks/freeplane.rb
+++ Casks/freeplane.rb
@@ -1,9 +1,9 @@
 cask "freeplane" do
   arch arm: "apple", intel: "intel"
 
-  version "1.11.3"
-  sha256 arm:   "aabc423bf3e8654a7fd872d12860f19bb45458a3426ff9d71280776efa84395b",
-         intel: "88312253aca6451bcff932051c6d9921be5f09ab964cf0cec845f790c3959889"
+  version "1.11.4"
+  sha256 arm:   "6dc5d30090ac279b9ab0405d48dc12867b32ab1935d42099d887a4e067a2aeed",
+         intel: "b50f599e5eba56e6ad52a3aed6fb5a1abc572b32fb05303b3ec8b3040402c215"
 
   url "https://downloads.sourceforge.net/freeplane/Freeplane-#{version}-#{arch}.dmg",
       verified: "downloads.sourceforge.net/freeplane/"
