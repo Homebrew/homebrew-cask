c1b0c09b93ee6d0fa07ea9f72033257e6f60e5d8
Vulxan <32930147+Vulxan@users.noreply.github.com>
2023-05-29 06:47:10 +0800

    fsnotes 6.3.0 (#148056)

--- Casks/fsnotes.rb
+++ Casks/fsnotes.rb
@@ -1,6 +1,6 @@
 cask "fsnotes" do
-  version "6.2.6"
-  sha256 "abb9d32ed553e16dd6b5ba091fd2164ad136814ebacaffb677c9917756f32556"
+  version "6.3.0"
+  sha256 "b68e30e5ee82f5658e2590e49a074d64b06be5da98435b948ae9f4ad0d065709"
 
   url "https://github.com/glushchenko/fsnotes/releases/download/#{version}/FSNotes_#{version}.zip",
       verified: "github.com/glushchenko/fsnotes/"
