2ba4d94a447cab7b9d3848f34d09aeaadcc85720
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-27 21:52:36 -0400

    blockbench 4.7.4 (#148036)

--- Casks/blockbench.rb
+++ Casks/blockbench.rb
@@ -1,9 +1,9 @@
 cask "blockbench" do
   arch arm: "arm64", intel: "x64"
 
-  version "4.7.3"
-  sha256 arm:   "abcfcd4c93c84c2e6e2bc3972746a86b2d82de56898c6fbdbb77f61f82b9d0c1",
-         intel: "5dbe17aef4b4c4fe331f6a6d1fe73ce326b5983313e1d43fde8ccba2fe292764"
+  version "4.7.4"
+  sha256 arm:   "431d721bda374113a90a73b449d36a991c2fa92d7cc9129684487a4377467a37",
+         intel: "a41523df12301c07f53329d48a66621543b63b563da1db9601e659c1799706a0"
 
   url "https://github.com/JannisX11/blockbench/releases/download/v#{version}/Blockbench_#{arch}_#{version}.dmg",
       verified: "github.com/JannisX11/blockbench/"
