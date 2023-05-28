0d91a1ea70c9c347c667c41e2e381fe9be116f71
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-27 21:48:09 -0400

    tea 0.2.20 (#148046)

--- Casks/tea.rb
+++ Casks/tea.rb
@@ -1,9 +1,9 @@
 cask "tea" do
   arch arm: "-arm64"
 
-  version "0.2.16"
-  sha256 arm:   "dcd65ced5f042b208178e6975b6c64d7792bc7875c6a4c26c2a16d1a364c76f6",
-         intel: "9138d325f68b7400cb60c98aacd908b573379d9cf98981e10f9ace195ddfbd5c"
+  version "0.2.20"
+  sha256 arm:   "eb3d8adc8c69470ed2f9c32318d5ae76f627d0637ad5c49d8ba0f2238f09754f",
+         intel: "9a49bd3fffd4e2e5cf157e4315a858174d65322f1077c610d6885a04b662c468"
 
   url "https://gui.tea.xyz/release/tea-#{version}#{arch}-mac.zip"
   name "tea"
