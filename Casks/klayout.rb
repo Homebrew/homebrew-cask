c578300e734aa8b912ea9ce0a0ad347f2ef7b991
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-27 21:52:17 -0400

    klayout 0.28.8 (#148043)
    * klayout 0.28.8
    
    * kaylout (add sha256 for other macOS versions)
    
    * Added sha256 for all macOS versions

--- Casks/klayout.rb
+++ Casks/klayout.rb
@@ -1,8 +1,8 @@
 cask "klayout" do
-  version "0.28.7"
+  version "0.28.8"
 
   on_catalina :or_older do
-    sha256 "bf84df179018a065cd0c28bac5f363daa14dc606818d9fdb66ac4d6772d99922"
+    sha256 "5dc26c623e70e105293440799705b6cd00e35549e7845a2980563f1068a26b2a"
 
     url "https://www.klayout.org/downloads/MacOS/HW-klayout-#{version}-macOS-Catalina-1-qt5Brew-RsysPhb39.dmg",
         verified: "klayout.org/downloads/MacOS/"
@@ -13,7 +13,7 @@ cask "klayout" do
     end
   end
   on_big_sur do
-    sha256 "4c0e8766ce1a19a4aaa410d57acc801b9a39c811758619cb262f0f8588154b86"
+    sha256 "f54b207992d6f45924758ae985abb53e132bc909e3548231c4fc98dbc348eaad"
 
     url "https://www.klayout.org/downloads/MacOS/HW-klayout-#{version}-macOS-BigSur-1-qt5Brew-RsysPhb39.dmg",
         verified: "klayout.org/downloads/MacOS/"
@@ -24,7 +24,7 @@ cask "klayout" do
     end
   end
   on_monterey do
-    sha256 "91c16fe64c7e44bb8c7e4dc4fd0f55212646aea27e0b76a3e53347b1a3379f1b"
+    sha256 "c9b62c03ef284b0585c6f6b06d0c8092b2372bea321f5b89e58065f5286903b0"
 
     url "https://www.klayout.org/downloads/MacOS/HW-klayout-#{version}-macOS-Monterey-1-qt5Brew-RsysPhb39.dmg",
         verified: "klayout.org/downloads/MacOS/"
@@ -35,7 +35,7 @@ cask "klayout" do
     end
   end
   on_ventura :or_newer do
-    sha256 "77b2df36ad2445bdb9c42c941d6bcc37ff8c1ad974a7958f49fbfbe1f6c9e35c"
+    sha256 "b4597e9094e45d545523cabfed5e9b9b493b005ada821a94ef569af537bcf9ad"
 
     url "https://www.klayout.org/downloads/MacOS/HW-klayout-#{version}-macOS-Ventura-1-qt5Brew-RsysPhb39.dmg",
         verified: "klayout.org/downloads/MacOS/"
