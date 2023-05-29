6e4332bf2442b93639f437d046c9b90ffde695b8
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-29 08:51:40 -0400

    anki: update min_os

--- Casks/anki.rb
+++ Casks/anki.rb
@@ -3,13 +3,13 @@ cask "anki" do
 
   version "2.1.64"
 
-  on_high_sierra :or_older do
-    sha256 "9e9fe5ba51a3f6c43a568d15aaa4acb74992f013af17a0211b0447e0742588ec"
+  on_catalina :or_older do
+    sha256 "d39cb359228ed4e0166f26b64656003aa6c41bc86f009d2e6ea49bab13e1cdee"
 
     url "https://github.com/ankitects/anki/releases/download/#{version}/anki-#{version}-mac-#{arch}-qt5.dmg",
         verified: "github.com/ankitects/anki/"
   end
-  on_mojave :or_newer do
+  on_big_sur :or_newer do
     sha256 arm:   "489591008d578bdda921c394696f1ce2c2c00094d45ad78be28a979bae8e5afb",
            intel: "e6fbdee9e887fce942a9ad8720d41fbb09860e98dcd6e8a254949cea2179ae5a"
 
