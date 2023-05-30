0b7a1a8db2bd728d63f08e58ab184ea2e2add768
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-30 07:46:22 -0400

    anydo 5.0.37

--- Casks/anydo.rb
+++ Casks/anydo.rb
@@ -1,6 +1,6 @@
 cask "anydo" do
-  version "5.0.36"
-  sha256 "a1536f62247be29e664254647a69996bcbeaab94b3e7e4d12c1db6d1eef701bd"
+  version "5.0.37"
+  sha256 "cfece1b05410f506361e2127e2045310926eb4a6e270d1b1cdc9a41943e37222"
 
   url "https://electron-app.any.do/Anydo-#{version}-universal.dmg"
   name "Any.do"
