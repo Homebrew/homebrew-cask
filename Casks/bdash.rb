15696ddf36ac6089531ed63248b9ea765e78ef7a
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-05-30 07:48:27 -0400

    bdash 1.16.0

--- Casks/bdash.rb
+++ Casks/bdash.rb
@@ -1,6 +1,6 @@
 cask "bdash" do
-  version "1.15.3"
-  sha256 "7575147610340cdd6e4171aedafd303e2d24240d4fe5165736b7619251f4e5eb"
+  version "1.16.0"
+  sha256 "178f895b9615fc01f7fa8cf3dd3d3a5c2fb15bcca27bd65d7e616fa19a5ef983"
 
   url "https://github.com/bdash-app/bdash/releases/download/v#{version}/Bdash-#{version}-mac.zip"
   name "Bdash"
