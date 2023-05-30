cbf07b0f81a7642889d7b4565ce9951a4fdfeae3
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-29 20:27:27 -0400

    rework cask

--- Casks/engine-dj.rb
+++ Casks/engine-dj.rb
@@ -5,24 +5,29 @@ cask "engine-dj" do
   url "https://imb-cicd-public.s3.amazonaws.com/Engine/#{version.csv.first}/Release/EDJ/mac/5e1011cdeb/Engine_DJ_Release_#{version.csv.first}_#{version.csv.second}.dmg",
       verified: "imb-cicd-public.s3.amazonaws.com/Engine/"
   name "Engine DJ Desktop"
-  desc "World's premier DJ software suite"
+  desc "DJ software suite"
   homepage "https://enginedj.com/"
 
   livecheck do
     url "https://enginedj.com/downloads"
-    strategy :page_match do |page|
-      match = page.match(
-        %r{href=.*?/Engine/[.\d]*?/Release/EDJ/mac/[\da-f]*?/Engine_DJ_Release_(\d+\.\d+\.\d+)_([\da-f]*?)\.dmg}i,
-      )
-      next if match.blank?
-
-      "#{match[1]}#{"," + match[2] if match[2]}"
+    regex(%r{href=.*?/Release/EDJ/mac/.*?/Engine[._-]DJ_Release[._-](\d+(?:\.\d+)+)[._-](\w*?)\.dmg}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
     end
   end
 
   pkg "Engine DJ_#{version.csv.first}_Setup.pkg"
 
-  uninstall pkgutil: "com.airmusictechnology.enginedj.application"
+  uninstall pkgutil: [
+    "com.airmusictechnology.enginedj.application",
+    "com.airmusictechnology.engineprime.application",
+  ]
 
-  zap trash: "~/Music/Engine Library"
+  zap trash: [
+    "~/Library/Application Support/AIR Music Technology",
+    "~/Library/Caches/AIR Music Technology",
+    "~/Library/Preferences/com.air-music-technology.EnginePrime.plist",
+    "~/Library/Saved Application State/com.air-music-technology.EnginePrime.savedState",
+    "~/Music/Engine Library",
+  ]
 end
