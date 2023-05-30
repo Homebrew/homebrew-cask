f7cf50e30d202f3e1d793fabdf136c0981f343b5
Jan-Otto Kröpke <github@jkroepke.de>
2023-05-30 09:54:12 +0200

    Remove sha256 checksum check from OpenLens cask

--- Casks/openlens.rb
+++ Casks/openlens.rb
@@ -2,8 +2,7 @@ cask "openlens" do
   arch arm: "-arm64"
 
   version "6.5.1"
-  sha256 arm:   "595c2d76f19c98f3d109e6cb89816d3f6510a7abc905c8c7128378483c06854f",
-         intel: "4c2132cb4ee50c145dc4e090ada2e01f57187a879dcbdbd9483d9563413ada41"
+  sha256 :no_check
 
   url "https://github.com/MuhammedKalkan/OpenLens/releases/download/v#{version}/OpenLens-#{version}#{arch}.dmg"
   name "OpenLens"
