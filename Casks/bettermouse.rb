9a7ad0375c3052078d51994aedec683aa08385a5
Mikl Wolfe <wolfemikl@gmail.com>
2023-05-30 15:24:31 -0600

    bettermouse 1.5.3673

--- Casks/bettermouse.rb
+++ Casks/bettermouse.rb
@@ -1,6 +1,6 @@
 cask "bettermouse" do
-  version "1.5.3669"
-  sha256 "ef26dd615be2a022b68f0959acaf38849c4c459ffcef9ae1cd7bf58d97efea58"
+  version "1.5.3673"
+  sha256 "e04c539ffd86837f6c5e7175a05a79998b883cb053defc02bb905ff29ea6e1e9"
 
   url "https://better-mouse.com/wp-content/uploads/BetterMouse.#{version}.zip"
   name "BetterMouse"
