bb7136ee077c2cdb35a2e93983e72ea14a09ec6a
Bevan Kay <email@bevankay.me>
2023-06-01 11:28:16 +1000

    update livecheck

--- Casks/shottr.rb
+++ Casks/shottr.rb
@@ -7,17 +7,9 @@ cask "shottr" do
   desc "Screenshot measurement and annotation tool"
   homepage "https://shottr.cc/"
 
-  # `version.json` provides a `latestVersion` field but it can reference an
-  # older version while the `package` field points to a file with a newer
-  # version. With this in mind, we extract the version from the `package` URL.
-  # Note: This regex intentionally omits the file extension, as the cask uses a
-  # dmg file but `version.json` references a pkg file.
   livecheck do
-    url "https://shottr.cc/api/version.json"
-    regex(/Shottr[._-]v?(\d+(?:\.\d+)+)/i)
-    strategy :json do |json, regex|
-      json["package"][regex, 1]
-    end
+    url "https://shottr.cc/newversion.html"
+    regex(/href=.*?Shottr[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
   auto_updates true
