cd46a6f98862301d40e92c75ad9216ed724913fd
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-01 16:56:48 -0400

    castr 2.7.0

--- Casks/castr.rb
+++ Casks/castr.rb
@@ -1,9 +1,9 @@
 cask "castr" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.6.0"
-  sha256 arm:   "fc4381771318927d9531d23632bbf3720c432c44795f03e3b719dc41d0b47a7c",
-         intel: "47bd863483f5360f6b08f1ad0dd593ce23eb1798c271393c021393878eab2771"
+  version "2.7.0"
+  sha256 arm:   "814c5c080818554b23400e85f53258a52c102b16def62ed72b87d332cc009107",
+         intel: "3eb46e778d9cf476ac0cc61dbb37c45f9b50125ac0f21f13fb05415e9f5078d1"
 
   url "https://download.todesktop.com/210610elr9v3cm6/Castr%20#{version}-#{arch}-mac.zip",
       verified: "download.todesktop.com/210610elr9v3cm6/"
