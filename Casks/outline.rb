3699cbdd62de9e912fc647e7729dcc457878c951
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-01 08:11:50 -0400

    outline 3.2305.0

--- Casks/outline.rb
+++ Casks/outline.rb
@@ -1,6 +1,6 @@
 cask "outline" do
-  version "3.2302.0"
-  sha256 "f552a44970c7698c970e3c6f421ba4e2a69cc8359b64d444ec17f91236f1f9e7"
+  version "3.2305.0"
+  sha256 "74b2be3df41928aa714b1d2193d21df18643a8a69f6a02fcb5f252f1b0a0b7ef"
 
   url "https://static.outline.ws/versions/Outline_#{version}.zip"
   name "Outline"
