7a9876ea260281fc6025f1ed30e02c0a2e651249
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-01 08:52:52 -0400

    xtool-creative-space (add intel version)

--- Casks/xtool-creative-space.rb
+++ Casks/xtool-creative-space.rb
@@ -6,8 +6,8 @@ cask "xtool-creative-space" do
     sha256  "cf0da60fee213cc0ad8854ab1996389a49fce3864b1ad654025b2eb976abc5f2"
   end
   on_intel do
-    version "16.234.1678158837754,1.2.14-2023-03-06-13-36-37"
-    sha256 "2720a0dfd87cdf73f71831a5335722b5898401173bdb0cf1f406fbb3e92c3164"
+    version "16.264.1685527600463,1.3.21-2023-05-30-15-17-14"
+    sha256 "d4b4604047e459f2c09f8d22902c31a41158f7e9f3b2011cef0bf0156ab24aa1"
   end
 
   url "https://res-us.makeblock.com/ms/updater/production/packages/#{version.csv.first.tr(".", "/")}/xTool%20Creative%20Space-#{version.csv.second}-#{arch}.dmg",
