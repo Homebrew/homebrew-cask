f07ba1445fa5cd3fe06cd584175941db60e388c5
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-06-01 19:54:35 -0400

    cctalk 7.10.7.2

--- Casks/cctalk.rb
+++ Casks/cctalk.rb
@@ -1,16 +1,15 @@
 cask "cctalk" do
-  version "7.10.6.5-1341"
-  sha256 "582ee1502d4a5065482bb41b48dc7bb256ae2c43a3ce6720d6fa71914d383149"
+  version "7.10.7.2"
+  sha256 :no_check
 
-  url "https://cc.hjfile.cn/cc/CCtalk#{version}/8/1/103/CCtalk#{version}.dmg",
-      verified: "cc.hjfile.cn/cc/"
+  url "https://www.cctalk.com/webapi/basic/v1.1/version/down?apptype=1&terminalType=8&versionType=103"
   name "CCtalk"
   desc "Real-time interactive education platform"
   homepage "https://www.cctalk.com/download/"
 
   livecheck do
-    url "https://www.cctalk.com/webapi/basic/v1.1/version/down?apptype=1&terminalType=8&versionType=103"
-    regex(/CCtalkv?(\d+(?:\.\d+)+-\d+)/i)
+    url :url
+    regex(/(\d+(?:\.\d+)+-*\d*)\.dmg/i)
     strategy :header_match
   end
 
