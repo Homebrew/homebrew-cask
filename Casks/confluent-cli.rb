3ac3a05938aff3c182c0cd882bec53e6492abf8b
Yusuf Kursad Kaya <ykursadkaya@gmail.com>
2023-06-02 13:33:58 +0300

    confluent-cli 3.15.1

--- Casks/confluent-cli.rb
+++ Casks/confluent-cli.rb
@@ -1,9 +1,9 @@
 cask "confluent-cli" do
   arch arm: "arm64", intel: "amd64"
 
-  version "3.15.0"
-  sha256 arm:   "85a63f016e053090870eb65ab3eef5fd98f520db85aaab289493e39749ccfc9d",
-         intel: "7f5a48bdc6628ba83e004b8d4a2a130a9966806890f4bc89a4d6c9eb45cbbfe3"
+  version "3.15.1"
+  sha256 arm:   "ecf3699c9d68a7b2afde02eb313a531c02ac4904efc56f06e80bb4287ea78566",
+         intel: "835f4e226d164da90cc9bc9f5fdda8fd3aec0e3ab50ccb59699a4a0eb4623de3"
 
   url "https://s3-us-west-2.amazonaws.com/confluent.cloud/confluent-cli/archives/#{version}/confluent_#{version}_darwin_#{arch}.tar.gz",
       verified: "s3-us-west-2.amazonaws.com/confluent.cloud/confluent-cli/archives/"
