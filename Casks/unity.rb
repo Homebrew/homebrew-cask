15a1cd879991956d6da01dec290b0f8daf74f406
Takdanai <jozaza07@gmail.com>
2023-06-03 02:37:36 +0700

    unity 2022.3.0f1

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -1,9 +1,9 @@
 cask "unity" do
   arch arm: "Arm64"
 
-  version "2022.1.23f1,9636b062134a"
-  sha256 arm:   "5f4e8d5b8b2df25c3a84e5a875502e62df63759c20925e3b1e8a9d1bcfac9343",
-         intel: "be6f32f4e5e8388471f7ed0be8e050762f6b12e492f2b1b91d310f5b6144b590"
+  version "2022.3.0f1,fb119bb0b476"
+  sha256 arm:   "45bbd7e921cdad96ddfe53830765d86b343a37523ed5832d136e71106534c2d7",
+         intel: "b4b3ad2845b2c997b0d7c2d36e9de9bbc469c73036b6c93349a39e958adfba02"
 
   url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorInstaller#{arch}/Unity-#{version.csv.first}.pkg",
       verified: "download.unity3d.com/download_unity/"
