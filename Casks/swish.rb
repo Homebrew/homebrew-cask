4c78c482740d1e3334614463294c49b8f846f210
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-04 11:07:21 -0400

    swish 1.10.2

--- Casks/swish.rb
+++ Casks/swish.rb
@@ -1,6 +1,6 @@
 cask "swish" do
-  version "1.10.1"
-  sha256 "eef4c5bc8e006685c55bfc7f2b4eddbb1f12a33f2a7821d0dde3c7778fa368f7"
+  version "1.10.2"
+  sha256 "4032e4a6afe429f4e745328400950eb01e0f998291a07055cc9df9d1c4a6bce7"
 
   url "https://github.com/chrenn/swish-dl/releases/download/#{version}/Swish.dmg",
       verified: "github.com/chrenn/swish-dl/"
