ef7daccff56e9092a9e175173a8b230204d0a729
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-05 17:45:21 -0400

    hackintool 3.9.8

--- Casks/hackintool.rb
+++ Casks/hackintool.rb
@@ -1,6 +1,6 @@
 cask "hackintool" do
-  version "3.9.7"
-  sha256 "0a2703cecadb036404b3aa7f1c68906342a07e4c8596de962ebba8ab9f264d01"
+  version "3.9.8"
+  sha256 "c34e366f131e367a9b888eeb64e763c417cdb119f25a0d22891e5e7218f97a74"
 
   url "https://github.com/headkaze/Hackintool/releases/download/#{version}/Hackintool.zip"
   name "Hackintool"
