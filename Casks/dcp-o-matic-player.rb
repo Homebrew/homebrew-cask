4572672d8a88711e2068806380e38170181a4c76
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-05 17:42:20 -0400

    dcp-o-matic-player 2.16.58

--- Casks/dcp-o-matic-player.rb
+++ Casks/dcp-o-matic-player.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-player" do
-  version "2.16.57"
-  sha256 "29a6a1846112a439d3b8202291a06343f8b56cdcf9176c8f48f0ef7372664966"
+  version "2.16.58"
+  sha256 "b44de3bf7d214ede8d552c15a5c472b7a8ededba2e574711679f2359ed3942ff"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-player&version=#{version}"
   name "DCP-o-matic Player"
