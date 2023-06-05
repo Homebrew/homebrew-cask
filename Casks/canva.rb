cd1ad946d78d09dc7c229a337c7bf9d4200b678b
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-06-05 07:42:33 -0400

    Update canva.rb

--- Casks/canva.rb
+++ Casks/canva.rb
@@ -1,7 +1,6 @@
 cask "canva" do
   version "1.67.0"
-  sha256 arm:   "430d8e86b3e254a7dae95e793212792cc68854a73ea07a21fa455e1e0f86371d",
-         intel: "430d8e86b3e254a7dae95e793212792cc68854a73ea07a21fa455e1e0f86371d"
+  sha256 "430d8e86b3e254a7dae95e793212792cc68854a73ea07a21fa455e1e0f86371d"
 
   url "https://desktop-release.canva-deploy.com/Canva-#{version}-universal.dmg",
       verified: "desktop-release.canva-deploy.com/"
