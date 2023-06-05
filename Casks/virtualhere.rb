ae2726c516cf0d087bc355186284e546e6a81410
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-06-05 13:53:29 -0400

    Update virtualhere.rb

--- Casks/virtualhere.rb
+++ Casks/virtualhere.rb
@@ -2,7 +2,7 @@ cask "virtualhere" do
   version "5.5.2"
   sha256 :no_check
 
-  url "https://www.virtualhere.com/sites/default/files/usbclient/VirtualHere.dmg"
+  url "https://www.virtualhere.com/sites/default/files/usbclient/VirtualHereUniversal.dmg"
   name "VirtualHere"
   desc "Use USB devices remotely over a network"
   homepage "https://www.virtualhere.com/usb_client_software"
