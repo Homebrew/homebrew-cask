095f1e8333420dff8947ed708b63cf6abae75922
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-06 08:21:29 -0400

    iriunwebcam 2.8.4

--- Casks/iriunwebcam.rb
+++ Casks/iriunwebcam.rb
@@ -1,6 +1,6 @@
 cask "iriunwebcam" do
-  version "2.8.3"
-  sha256 "28924066d79e4ee3888cf4ce4edcd731275a588275ec1ffc175ece9278bc8bb9"
+  version "2.8.4"
+  sha256 "76ebd1b1ac52a3486119ae5864ab7b92b477dd572c46b6ea081b95b2f61598e2"
 
   url "https://1758658189.rsc.cdn77.org/IriunWebcam-#{version}.pkg",
       verified: "1758658189.rsc.cdn77.org/"
