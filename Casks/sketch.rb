0aa00b44d4d17c3c34b627ddcd05b06d9420c0ef
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-06 09:00:55 -0400

    sketch (add on_system blocks)
    * Add on_monterey block for latest version
    
    * Add on_big_sur block to retain legacy version

--- Casks/sketch.rb
+++ Casks/sketch.rb
@@ -1,19 +1,33 @@
 cask "sketch" do
-  version "97,173164"
-  sha256 "c77adaa7fa146edb3ce144e66e199186589d04797a374d7ee1d26c10c3db4349"
+  on_big_sur :or_older do
+    version "96.3,167315"
+    sha256 "3ba2c147aab6b2fcb4b5cc3e3ee7fce3f63551e6ef743a7afe459bd0a87bb4a6"
 
-  url "https://download.sketch.com/sketch-#{version.csv.first}-#{version.csv.second}.zip"
+    url "https://download.sketch.com/sketch-#{version.csv.first}-#{version.csv.second}.zip"
+
+    livecheck do
+      skip "Legacy version"
+    end
+  end
+  on_monterey :or_newer do
+    version "97,173164"
+    sha256 "c77adaa7fa146edb3ce144e66e199186589d04797a374d7ee1d26c10c3db4349"
+
+    url "https://download.sketch.com/sketch-#{version.csv.first}-#{version.csv.second}.zip"
+
+    livecheck do
+      url "https://download.sketch.com/sketch-versions.xml"
+      strategy :sparkle
+    end
+
+    depends_on macos: ">= :monterey"
+  end
+
   name "Sketch"
   desc "Digital design and prototyping platform"
   homepage "https://www.sketch.com/"
 
-  livecheck do
-    url "https://download.sketch.com/sketch-versions.xml"
-    strategy :sparkle
-  end
-
   auto_updates true
-  depends_on macos: ">= :big_sur"
 
   app "Sketch.app"
 
