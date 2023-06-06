5d2fccf7bfd042e9ba25b8d9e549098f09d91f5e
Christian Kraemer <christian.kraemer@gmail.com>
2023-06-07 00:22:26 +0200

    brave-browser 1.52.122.0

--- Casks/brave-browser.rb
+++ Casks/brave-browser.rb
@@ -2,9 +2,9 @@ cask "brave-browser" do
   arch arm: "arm64", intel: "x64"
   folder = on_arch_conditional arm: "stable-arm64", intel: "stable"
 
-  version "1.52.117.0,152.117"
-  sha256 arm:   "43dfcd6a4869a510a792b6f6cdfbb94b0c9d30b735bf0f53bb31e6f82590d78e",
-         intel: "16e73562d7db40a6a8a8c0e1c83683213ac9e2932a03ef39ecbdab801b57f4ed"
+  version "1.52.122.0,152.122"
+  sha256 arm:   "cde8469c9f184cf096a685bd614eb41db2006a6322843df848e507f47f998f73",
+         intel: "58e3ea7a51cb491d2216000faeea6d7b3746c08eea3d1923efffe813f141ee29"
 
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.csv.second}/Brave-Browser-#{arch}.dmg",
       verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
