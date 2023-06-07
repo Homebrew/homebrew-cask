174feb3319c0a1d3180bdc993d3fdcbf0d8c4d25
Bevan Kay <email@bevankay.me>
2023-06-07 21:02:59 +1000

    update livecheck and url

--- Casks/karafun.rb
+++ Casks/karafun.rb
@@ -1,15 +1,23 @@
 cask "karafun" do
-  version "2.3.0,91"
-  sha256 :no_check
+  version "2.3.0.91,8b1dd03e90009107ae99a212322cc03b,fcfd34139c"
+  sha256 "9342909a313edb999a16885963538314f477438bcfb1fb7fd79c36c6b1cbc126"
 
-  url "https://www.karafun.com/download/mac.html"
+  url "https://c20.recis.io/sl/#{version.csv.third}/#{version.csv.second}/KaraFun_#{version.csv.first}.dmg",
+      verified: "c20.recis.io/sl/"
   name "KaraFun"
   desc "Karaoke player software"
   homepage "https://www.karafun.com/"
 
   livecheck do
     url "https://www.karafun.fr/osx/appcast.xml"
-    strategy :sparkle
+    regex(%r{sl/([^/]+)/([^/]+)/v?KaraFun[._-]v?(\d+(?:\.\d+)+)})
+    strategy :sparkle do |item|
+      puts item.url
+      match = item.url.match(regex)
+      next if match.blank?
+
+      "#{match[3]},#{match[2]},#{match[1]}"
+    end
   end
 
   auto_updates true
