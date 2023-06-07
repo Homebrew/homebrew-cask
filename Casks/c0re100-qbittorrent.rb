03f1ec484f727df391f957508358e7f96034e591
IwfWcf <iwfwcf@gmail.com>
2023-06-07 23:59:52 +0800

    c0re100-qbittorrent 4.5.3.10

--- Casks/c0re100-qbittorrent.rb
+++ Casks/c0re100-qbittorrent.rb
@@ -1,6 +1,6 @@
 cask "c0re100-qbittorrent" do
   version "4.5.3.10"
-  sha256 "ab18cbe8d8462a8a2776792110323a1f1f7e1c07cc467a278549ce4c9d725293"
+  sha256 "d6b95f2f63f48b64f0ba5f97916edd2cbe4aa65f9f0ca8c9723ebd6557b7ffdd"
 
   url "https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-#{version}/qBittorrent-Enhanced-Edition-release-#{version}-macOS-universal.dmg"
   name "qBittorrent Enhanced Edition"
