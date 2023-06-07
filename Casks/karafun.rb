b6b8ab64610d112beb2ae91e062feadcb540b9a3
Edward <73746306+WangEdward@users.noreply.github.com>
2023-06-07 19:28:26 +0800

    change to main website

--- Casks/karafun.rb
+++ Casks/karafun.rb
@@ -1,15 +1,15 @@
 cask "karafun" do
-  version "2.3.0,91,8b1dd03e90009107ae99a212322cc03b,fcfd34139c"
+  version "2.3.0,91,8b1dd03e90009107ae99a212322cc03b,e8179eab96"
   sha256 "9342909a313edb999a16885963538314f477438bcfb1fb7fd79c36c6b1cbc126"
 
-  url "https://c20.recis.io/sl/#{version.csv.fourth}/#{version.csv.third}/KaraFun_#{version.csv.first}.#{version.csv.second}.dmg",
-      verified: "c20.recis.io/sl/"
+  url "https://c17.recis.io/sl/#{version.csv.fourth}/#{version.csv.third}/KaraFun_#{version.csv.first}.#{version.csv.second}.dmg",
+      verified: "c17.recis.io/sl/"
   name "KaraFun"
   desc "Karaoke player software"
   homepage "https://www.karafun.com/"
 
   livecheck do
-    url "https://www.karafun.fr/osx/appcast.xml"
+    url "https://www.karafun.com/osx/appcast.xml"
     regex(%r{sl/([^/]+)/([^/]+)/v?KaraFun[._-]v?(\d+(?:\.\d+)+)})
     strategy :sparkle do |item|
       puts item.url
