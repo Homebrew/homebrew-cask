182b01d61a7d81faa170836206be08d1c03c5665
Tin Kyaw <tin@lsvp.com>
2023-06-07 21:18:23 -0700

    minizincide 2.7.5

--- Casks/minizincide.rb
+++ Casks/minizincide.rb
@@ -1,6 +1,6 @@
 cask "minizincide" do
   version "2.7.5"
-  sha256 "d1cdb0005a37c37d8da7bde4b60baa493baa71a2a69e652b133c827e2f2d57ba"
+  sha256 "1d9593797f8f8d2944d54b6620b2c0386071a67895ff5bd67fd2c1736762a4cd"
 
   url "https://github.com/MiniZinc/MiniZincIDE/releases/download/#{version}/MiniZincIDE-#{version}-bundled.dmg",
       verified: "github.com/MiniZinc/MiniZincIDE/"
