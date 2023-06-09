0ae791c0668fd09842023a8a335e4afe309d6f29
benwaco <127471645+benwaco@users.noreply.github.com>
2023-06-08 23:19:28 -0700

    Change the URL to Fix

--- Casks/whisky.rb
+++ Casks/whisky.rb
@@ -1,11 +1,11 @@
 cask "whisky" do
-    version "pre-0.2.1"
-    sha256 :no_check
-  
-    url "https://github.com/IsaacMarovitz/Whisky/releases/download/v#{version}/Whisky.zip"
-    name "Whisky"
-    desc "A modern Wine wrapper for macOS built with SwiftUI"
-    homepage "https://github.com/IsaacMarovitz/Whisky"
-  
-    app "Whisky.app"
-  end
+  version "pre-0.2.1"
+  sha256 "64f1dc3b594b6f570232c2daea17c8828ffc7f721839d4179c977ce238eb903b"
+
+  url "https://github.com/IsaacMarovitz/Whisky/releases/download/#{version}/Whisky.zip"
+  name "whisky"
+  desc "Modern Wine wrapper for macOS built with SwiftUI"
+  homepage "https://github.com/IsaacMarovitz/Whisky"
+
+  app "Whisky.app"
+end
