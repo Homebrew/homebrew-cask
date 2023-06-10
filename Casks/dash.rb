842581c2987350897141ec01077a0319a04f661b
Sergey Pogorelov <pogorelov.s.y@gmail.com>
2023-06-10 19:44:19 +0200

    Upgrade Dash.app to 6.4.1

--- Casks/dash.rb
+++ Casks/dash.rb
@@ -4,8 +4,8 @@ cask "dash" do
     sha256 "e2b5eb996645b25f12ccae15e24b1b0d8007bc5fed925e14ce7be45a2b693fb6"
   end
   on_mojave :or_newer do
-    version "6.4.0"
-    sha256 "165b17939f1501bfe6fe6e363a7246fbc276b51d1769f80e41828551d1cbd8ed"
+    version "6.4.1"
+    sha256 "c0d9919bd180972aaaba2c86e0e8351e69a11bb5149ec951c373f8ce976c2ce5"
   end
 
   url "https://kapeli.com/downloads/v#{version.major}/Dash.zip"
