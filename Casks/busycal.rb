5f64b281da0b67fd594bad31d131512f68109d50
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-11 21:06:09 -0400

    busycal 2023.2.3,2023-06-11-17-26

--- Casks/busycal.rb
+++ Casks/busycal.rb
@@ -1,6 +1,6 @@
 cask "busycal" do
-  version "2023.2.3,2023-06-10-23-34"
-  sha256 "82b32b264445daab3f0e774ff70ec0774d33b9b826150e10fd7bdd274e68e9e2"
+  version "2023.2.3,2023-06-11-17-26"
+  sha256 "f0257517a68137223091fa881518c8ffbea1286ec5cbaa5fb44566f54498c012"
 
   url "https://7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/bcl-#{version.csv.first}-#{version.csv.second}.zip",
       verified: "7e968b6ce8a839f034d9-23cfb9eddcb7b94cb43ba95f95a76900.ssl.cf1.rackcdn.com/"
