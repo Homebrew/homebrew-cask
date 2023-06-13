022157550a763f29a4d5df26564730989272204c
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-13 16:45:07 -0400

    easydict 1.3.4

--- Casks/easydict.rb
+++ Casks/easydict.rb
@@ -1,6 +1,6 @@
 cask "easydict" do
-  version "1.3.2"
-  sha256 "42ca4d8ab849481ca8ed4c2188ece6330b3820a6c180b908d9857140df7bef45"
+  version "1.3.4"
+  sha256 "559bec79ab75b7e367b4b218803aae0e7c923660d4cc27c171cf9d2c79b63ca3"
 
   url "https://github.com/tisfeng/Easydict/releases/download/#{version}/Easydict.dmg"
   name "Easydict"
