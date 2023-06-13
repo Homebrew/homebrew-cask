c0aaf23fce473921336cc6c7c32ca3ef1847a5b4
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-13 08:56:20 -0400

    dbeaver-enterprise 23.1.0

--- Casks/dbeaver-enterprise.rb
+++ Casks/dbeaver-enterprise.rb
@@ -1,9 +1,9 @@
 cask "dbeaver-enterprise" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "23.0.0"
-  sha256 arm:   "75ce4e6784dfe76c82f2bf9b6bcd0700189861d455d4e7142e09e5781235898d",
-         intel: "a642f60bb88c95c6d7709145457b27160605edae764e3c42d87e5e4f63a8516d"
+  version "23.1.0"
+  sha256 arm:   "02cb3568f79afa436781283ee4c86644f0460a32304988ac5aea9643648a5503",
+         intel: "8694a8148694c4759e13a5bd400fd9b1933eceb624a56fd4ccd27ec5f5ea236c"
 
   url "https://dbeaver.com/files/#{version}/dbeaver-ee-#{version}-macos-#{arch}.dmg"
   name "DBeaver Enterprise Edition"
