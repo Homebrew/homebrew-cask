9cbf150439fd84297c1596d667ee0da19cdaf59b
Bevan Kay <email@bevankay.me>
2023-06-15 09:31:33 +1000

    drawio 21.4.0

--- Casks/drawio.rb
+++ Casks/drawio.rb
@@ -1,9 +1,9 @@
 cask "drawio" do
   arch arm: "arm64", intel: "x64"
 
-  version "21.3.7"
-  sha256 arm:   "e79255c63582d89ed90c0b6c1149a23d1eb2a6eb705eeda3b15789d38fa58230",
-         intel: "49e24ceabd6b2d203b976c598938de92043433164c9b43add38035d9983aba42"
+  version "21.4.0"
+  sha256 arm:   "96c33fd796037d53861702bba0973ec4b68f783957b820af0155fa083919d0c9",
+         intel: "9bb4cb1c093c79a475912a8fb296069bb2549e4b9ad0d8b41447c9d1435b7db1"
 
   url "https://github.com/jgraph/drawio-desktop/releases/download/v#{version}/draw.io-#{arch}-#{version}.dmg",
       verified: "github.com/jgraph/drawio-desktop/"
