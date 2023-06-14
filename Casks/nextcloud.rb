a640a8bd8d6574864082712846644852b617973d
Alexander Fischer <fischer.alexander@web.de>
2023-06-14 10:14:02 +0200

    corrected download-urls

--- Casks/nextcloud.rb
+++ Casks/nextcloud.rb
@@ -10,8 +10,8 @@ cask "nextcloud" do
     version "3.9.0"
     sha256 "2e1f8386462f296061f44ff8f1e705a39789661c3f73aa6093e110ebdff53354"
 
-    url "https://github.com/nextcloud/desktop/releases/download/v#{version}/Nextcloud-#{version}.pkg",
-        verified: "github.com/nextcloud/desktop/"
+    url "https://github.com/nextcloud-releases/desktop/releases/download/v#{version}/Nextcloud-#{version}.pkg",
+        verified: "github.com/nextcloud-releases/desktop/"
   end
 
   name "Nextcloud"
@@ -19,7 +19,7 @@ cask "nextcloud" do
   homepage "https://nextcloud.com/"
 
   livecheck do
-    url "https://github.com/nextcloud/desktop/releases/latest"
+    url "https://github.com/nextcloud-releases/desktop/releases/latest"
     regex(/Nextcloud[._-]v?(\d+(?:\.\d+)+)\.pkg/i)
     strategy :header_match do |headers, regex|
       next if headers["location"].blank?
