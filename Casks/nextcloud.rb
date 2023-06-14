23015e1f79366c7219d784e0782bb2fc45c76d86
Alexander Fischer <fischer.alexander@web.de>
2023-06-14 08:12:56 +0200

    Update nextcloud.rb for version 3.9.0

--- Casks/nextcloud.rb
+++ Casks/nextcloud.rb
@@ -7,8 +7,8 @@ cask "nextcloud" do
         verified: "github.com/nextcloud/desktop/"
   end
   on_sierra :or_newer do
-    version "3.8.2"
-    sha256 "3f52186bc3fc54d5592cb3a202a13ef3da95109cd3fb125761243bdc96f1cc75"
+    version "3.9.0"
+    sha256 "2e1f8386462f296061f44ff8f1e705a39789661c3f73aa6093e110ebdff53354"
 
     url "https://github.com/nextcloud/desktop/releases/download/v#{version}/Nextcloud-#{version}.pkg",
         verified: "github.com/nextcloud/desktop/"
