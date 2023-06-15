5b4dcd4c6e027f8059d11b8a984c5f4dc4952548
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-15 10:19:40 -0400

    whalebird 5.0.7

--- Casks/whalebird.rb
+++ Casks/whalebird.rb
@@ -1,9 +1,9 @@
 cask "whalebird" do
   arch arm: "arm64", intel: "x64"
 
-  version "5.0.6"
-  sha256 arm:   "e3b5c9285efc9b2f12a74dcc79e76c0d19a95a481ef57172172a04bf27a3656a",
-         intel: "b595e1ccef6af020568f4664079e7449717c9388cc3ba550a3042026cc1fd921"
+  version "5.0.7"
+  sha256 arm:   "667a67f6134975e56b791a0b9c37650385fb665bc16dc77956541ac8e979c759",
+         intel: "cdd1832332c0ab9ce3175df71cb8327c6f07c69408eeeea644258567bd424090"
 
   url "https://github.com/h3poteto/whalebird-desktop/releases/download/v#{version}/Whalebird-#{version}-mac-#{arch}.dmg",
       verified: "github.com/h3poteto/whalebird-desktop/"
