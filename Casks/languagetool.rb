94cedbb712e5f4a11b8cead561ac1468c5ff0aa1
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-15 10:33:30 -0400

    languagetool 1.4

--- Casks/languagetool.rb
+++ Casks/languagetool.rb
@@ -1,6 +1,6 @@
 cask "languagetool" do
-  version "1.3.3"
-  sha256 "94e90faf1d3e096d65ab3b28f468507843586d771c7e03138fb3932b9e0480cf"
+  version "1.4"
+  sha256 "4c3e9684488b475140948d66eb9a4eae7960efd7d54d054e6d7b448dae8b6fb4"
 
   url "https://languagetool.org/download/mac-app/LanguageToolDesktop-#{version}.dmg"
   name "LanguageTool for Desktop"
