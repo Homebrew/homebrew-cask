9a4844edecc17a233cde2eaa989f26399ee7e97e
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-15 16:03:10 -0400

    opencpn 5.8.4
    * Update to version 5.8.4
    
    * Update url to latest format
    
    * Update livecheck to parse latest release artifact format

--- Casks/opencpn.rb
+++ Casks/opencpn.rb
@@ -1,19 +1,32 @@
 cask "opencpn" do
-  version "5.6.2"
-  sha256 "dd093babd8b565cced08c0fd6467c70e6af4da707caf5ddc47e64b601944467a"
+  version "5.8.4,0+637.1637c28"
+  sha256 "2c3dc08908c002dcfe61a67b8e594a14d4de7ec486f6e44fc4ddc18219b65b44"
 
-  url "https://github.com/OpenCPN/OpenCPN/releases/download/Release_#{version}/OpenCPN_#{version}.pkg",
+  url "https://github.com/OpenCPN/OpenCPN/releases/download/Release_#{version.csv.first}/OpenCPN_#{version.csv.first}-#{version.csv.second}.pkg",
       verified: "github.com/OpenCPN/OpenCPN/"
   name "OpenCPN"
   desc "Full-featured and concise ChartPlotter/Navigator"
   homepage "https://www.opencpn.org/"
 
   livecheck do
-    url :url
-    regex(/v?(\d+(?:\.\d+)+)$/i)
+    url "https://github.com/OpenCPN/OpenCPN/releases/latest"
+    regex(%r{href=.*?/OpenCPN[._-]?v?(\d+(?:\.+\d+)+)[._-]?([0-9]?[+-]?\d*\.[0-9a-z]*)\.pkg}i)
+    strategy :header_match do |headers, regex|
+      next if headers["location"].blank?
+
+      # Identify the latest tag from the response's `location` header
+      latest_tag = File.basename(headers["location"])
+      next if latest_tag.blank?
+
+      # Fetch the assets list HTML for the latest tag and match within it
+      assets_page = Homebrew::Livecheck::Strategy.page_content(
+        @url.sub(%r{/releases/?.+}, "/releases/expanded_assets/#{latest_tag}"),
+      )
+      assets_page[:content]&.scan(regex)&.map { |match| "#{match[0]},#{match[1]}" }
+    end
   end
 
-  pkg "OpenCPN_#{version}.pkg"
+  pkg "OpenCPN_#{version.csv.first}-#{version.csv.second}.pkg"
 
   uninstall pkgutil: [
     "org.opencpn.pkg.OpenCPN",
