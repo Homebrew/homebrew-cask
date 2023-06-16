eb228e90f515a6323e2b2f3102095575cda5bf53
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-16 08:02:47 -0400

    biscuit 1.2.28

--- Casks/biscuit.rb
+++ Casks/biscuit.rb
@@ -1,9 +1,9 @@
 cask "biscuit" do
   arch arm: "-arm64"
 
-  version "1.2.27"
-  sha256 arm:   "b2bd9101f9eec59430db3586387cf5bcd077b06115f244ecdff9de1f4f329ac4",
-         intel: "4eff2c48e3ce24af3ffeb814ed46f5a21406516b0c094763f5896e73bc82bd9b"
+  version "1.2.28"
+  sha256 arm:   "0f4a6b23d818cefc2a79b96ea769811d263f186e9f301486096610bd5dd908d1",
+         intel: "35939b0cb19b803ce636a8e886631892afd51cbe952dfbe26286624342be4524"
 
   url "https://github.com/agata/dl.biscuit/releases/download/v#{version}/Biscuit-#{version}#{arch}.dmg",
       verified: "github.com/agata/dl.biscuit/"
