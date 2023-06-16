ba8f204e12af8513fdc2ddc3a50da4351e968ebf
Adam Brousseau <adam.brousseau@ca.ibm.com>
2023-06-16 15:11:11 -0400

    Semeru: Add jdk20 initial release
    20.0.1.0 based on jdk-20.0.1+9
    with OpenJ9 0.39.0
    
    Signed-off-by: Adam Brousseau <adam.brousseau@ca.ibm.com>

--- Casks/semeru-jdk-open.rb
+++ Casks/semeru-jdk-open.rb
@@ -1,9 +1,9 @@
 cask "semeru-jdk-open" do
   arch arm: "aarch64", intel: "x64"
 
-  version "18.0.2+9,openj9-0.33.1"
-  sha256 arm:   "b6dd0bca76fb3d606fdc2ad1dc75612354c4c515262e8450c326944a41be11d6",
-         intel: "9cc0bd4f66b68631ff43fed90fd05867bf3c69ab3d78daa6de3f77f6d6f99645"
+  version "20.0.1+9,openj9-0.39.0"
+  sha256 arm:   "d312ae562c363875d44a2d5d5af677e7e90bc26e94f286bfb20a60c4a6697925",
+         intel: "a775c711ff62153cb6965ca63e69da282ef23116973672965495428b00d2d554"
 
   url "https://github.com/ibmruntimes/semeru#{version.major}-binaries/releases/download/jdk-#{version.csv.first}_#{version.csv.second}/ibm-semeru-open-jdk_#{arch}_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg",
       verified: "github.com/ibmruntimes/semeru#{version.major}-binaries/"
@@ -11,11 +11,17 @@ cask "semeru-jdk-open" do
   desc "Production-ready JDK with the OpenJDK class libraries and the Eclipse OpenJ9 JVM"
   homepage "https://developer.ibm.com/languages/java/semeru-runtimes"
 
+   livecheck do
+    url "https://github.com/ibmruntimes/semeru#{version.major}-binaries/releases"
+    strategy :github_latest do |page|
+      match = page.match(%r{href=.*?/tag/jdk[._-](\d+(?:[._+]\d+)+)[._-]([^&]+)&quot;}i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
+  end
+
   pkg "ibm-semeru-open-jdk_#{arch}_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg"
 
   uninstall pkgutil: "net.ibm-semeru-open.#{version.major}.jdk"
-
-  caveats do
-    discontinued
-  end
 end
