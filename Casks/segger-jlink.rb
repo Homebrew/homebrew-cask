411e7f1316306836b1a1220da9fe9268f9d0be88
Ernest Guillem <ernest@Ernests-Laptop.local>
2023-06-16 11:32:51 +0200

    segger-jlink 7.88h

--- Casks/segger-jlink.rb
+++ Casks/segger-jlink.rb
@@ -1,6 +1,6 @@
 cask "segger-jlink" do
-  version "7.88g"
-  sha256 "08efcdb0c3ccbbdbbc6365760e29b1d05c24e9624f726fb986c5a9f399b5715d"
+  version "7.88h"
+  sha256 "abe6a42664167df98172c86ad9c814a19d5c9296f80dd6b8ce1ef4f1cf200c35"
 
   url "https://www.segger.com/downloads/jlink/JLink_MacOSX_V#{version.no_dots}_universal.pkg",
       using: :post,
