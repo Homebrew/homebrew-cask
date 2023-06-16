97ecbfa4c970b0ccc64887922508b0cab2bcc559
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-16 07:58:03 -0400

    eclipse-php 4.28.0

--- Casks/eclipse-php.rb
+++ Casks/eclipse-php.rb
@@ -1,9 +1,9 @@
 cask "eclipse-php" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "4.27.0,2023-03"
-  sha256 arm:   "5d590bfcddd95e3e554fa2f7a09f84d52a83179ed3254ab3e689d4ddc5f47371",
-         intel: "637052f351c14fd3bac3c6e7f0799d4e49d75512e8cfde393d94165b8fc7cc86"
+  version "4.28.0,2023-06"
+  sha256 arm:   "01eb81f259b2c9de2813c8046d477a28781ed356b1cee3071f59e6be1684325f",
+         intel: "6653bbc65879f62fac7e24527fc72cd4508aabed1fef966b19996d8151d0bd60"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-php-#{version.csv.second}-R-macosx-cocoa-#{arch}.dmg&r=1"
   name "Eclipse IDE for PHP Developers"
