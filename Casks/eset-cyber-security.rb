26950e8a645e2f24562f6ddf7b767550b61a9ad4
Teko012 <112829523+Teko012@users.noreply.github.com>
2023-06-17 21:03:47 +0200

    eset-cyber-security 7.3.3700.0

---
+++ Casks/eset-cyber-security.rb
@@ -0,0 +1,21 @@
+cask "eset-cyber-security" do
+  version "7.3.3700.0"
+  sha256 :no_check
+
+  url "https://download.eset.com/com/eset/apps/home/eav/mac/latest/eset_cybersecurity_en.dmg"
+  name "ESET Cyber Security"
+  desc "Security including web and email protection"
+  homepage "https://www.eset.com/"
+
+  livecheck do
+    url "https://help.eset.com/latestVersions/"
+    regex(%r{ESET\sCyber\sSecurity</p>\n.*/span>\sv?(\d+(?:\.\d+)+)}i)
+  end
+
+  pkg "Resources/Installer.pkg"
+
+  uninstall script: {
+    executable: "/Applications/ESET Cyber Security.app/Contents/Helpers/Uninstaller.app/Contents/Scripts/uninstall.sh",
+    sudo:       true,
+  }
+end
