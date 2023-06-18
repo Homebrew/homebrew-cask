5623491d611a336d5f4d7490c98fd517d6ad532b
Edward <73746306+WangEdward@users.noreply.github.com>
2023-06-18 09:40:55 +0800

    prismlauncher 7.1

--- Casks/prismlauncher.rb
+++ Casks/prismlauncher.rb
@@ -1,5 +1,5 @@
 cask "prismlauncher" do
-  version "7.0"
+  version "7.1"
 
   on_mojave :or_older do
     sha256 "b81ee66ce31eccab7887c372140838ef3437ccc256e514f9c69c6255f389b485"
@@ -8,7 +8,7 @@ cask "prismlauncher" do
         verified: "github.com/PrismLauncher/PrismLauncher/"
   end
   on_catalina :or_newer do
-    sha256 "93460deafa31fced022f29547aad17c01560bbc2233b36b3a6421fd73c26512b"
+    sha256 "676d0fa10a9a07df152f904bc8515e04c20e4b9d3efd419eed2c92afdd0f9b2c"
 
     url "https://github.com/PrismLauncher/PrismLauncher/releases/download/#{version}/PrismLauncher-macOS-#{version}.tar.gz",
         verified: "github.com/PrismLauncher/PrismLauncher/"
