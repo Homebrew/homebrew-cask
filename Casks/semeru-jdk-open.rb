9ac4ddc2d0461b3c8ce81e9609a545a6fb86cb05
Miccal Matthews <miccal.matthews@gmail.com>
2023-06-19 23:54:57 +0800

    Update semeru-jdk-open.rb

--- Casks/semeru-jdk-open.rb
+++ Casks/semeru-jdk-open.rb
@@ -6,12 +6,12 @@ cask "semeru-jdk-open" do
          intel: "a775c711ff62153cb6965ca63e69da282ef23116973672965495428b00d2d554"
 
   url "https://github.com/ibmruntimes/semeru#{version.major}-binaries/releases/download/jdk-#{version.csv.first}_#{version.csv.second}/ibm-semeru-open-jdk_#{arch}_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg",
-      verified: "github.com/ibmruntimes/semeru#{version.major}-binaries/"
+      verified: "github.com/ibmruntimes/"
   name "IBM Semeru Runtime (JDK) Open Edition"
   desc "Production-ready JDK with the OpenJDK class libraries and the Eclipse OpenJ9 JVM"
   homepage "https://developer.ibm.com/languages/java/semeru-runtimes"
 
-   livecheck do
+  livecheck do
     url "https://github.com/ibmruntimes/semeru#{version.major}-binaries/releases"
     strategy :github_latest do |page|
       match = page.match(%r{href=.*?/tag/jdk[._-](\d+(?:[._+]\d+)+)[._-]([^&]+)&quot;}i)
@@ -24,4 +24,6 @@ cask "semeru-jdk-open" do
   pkg "ibm-semeru-open-jdk_#{arch}_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg"
 
   uninstall pkgutil: "net.ibm-semeru-open.#{version.major}.jdk"
+
+  # No zap stanza required
 end
