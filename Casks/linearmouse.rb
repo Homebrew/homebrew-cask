08be12c02c73f0232f8564ca79dcbfb149363caf
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-06-18 21:42:17 -0400

    linearmouse 0.9.2

--- Casks/linearmouse.rb
+++ Casks/linearmouse.rb
@@ -1,6 +1,6 @@
 cask "linearmouse" do
-  version "0.9.1"
-  sha256 "131b5b57fa493799e890f4c95e3a9f14df2b976cd9644462d773141b89011ab8"
+  version "0.9.2"
+  sha256 "8f4a8b7986db393d6ae4ea12e60f02822025854a03cd004983c7eb4ef3583ccd"
 
   url "https://github.com/linearmouse/linearmouse/releases/download/v#{version}/LinearMouse.dmg",
       verified: "github.com/linearmouse/linearmouse/"
