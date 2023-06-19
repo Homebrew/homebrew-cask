ccbd7abed7a12e1b2fc42b20609c61097bdba8ed
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-06-19 06:37:47 -0400

    cask changes

--- Casks/frappe-books.rb
+++ Casks/frappe-books.rb
@@ -5,12 +5,18 @@ cask "frappe-books" do
   url "https://github.com/frappe/books/releases/download/v#{version}/Frappe-Books-#{version}.dmg",
       verified: "github.com/frappe/books/"
   name "Frappe Books"
-  desc "Free Desktop book-keeping software for small businesses and freelancers"
+  desc "Book-keeping software for small businesses and freelancers"
   homepage "https://frappebooks.com/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "Frappe Books.app"
 
   zap trash: [
+    "~/Library/Application Support/frappe-books",
     "~/Library/Preferences/io.frappe.books.plist",
     "~/Library/Saved Application State/io.frappe.books.savedState",
   ]
