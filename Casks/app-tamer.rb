5a4f7e062a19475fe58ffe3fdae0afd14bb5d44d
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-19 09:02:22 -0400

    app-tamer 2.7.7

--- Casks/app-tamer.rb
+++ Casks/app-tamer.rb
@@ -1,6 +1,6 @@
 cask "app-tamer" do
-  version "2.7.6"
-  sha256 "3d55fb809a7b098e1904c7ecb968c842df8478dfc31a13959ac81fd2d6cc89c3"
+  version "2.7.7"
+  sha256 "89f59743dc832872d98df03de8c6f0d1ca3ba74c356c6119ccdb261799d8a416"
 
   url "https://www.stclairsoft.com/download/AppTamer-#{version}.dmg"
   name "AppTamer"
