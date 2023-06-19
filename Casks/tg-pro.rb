dd638570b0151eda2ca2263c378c6b65661cc16f
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-19 09:15:26 -0400

    tg-pro 2.82

--- Casks/tg-pro.rb
+++ Casks/tg-pro.rb
@@ -1,6 +1,6 @@
 cask "tg-pro" do
-  version "2.81"
-  sha256 "8bc7c3945e97e2278498966a49f3be7af865767b8c27b2584602ce526d3c9a7b"
+  version "2.82"
+  sha256 "81ce529497aac2cf9b86d3de103fe0a5812cc25902fb3248b7a3c8a01e9c5f3c"
 
   url "https://www.tunabellysoftware.com/resources/TG%20Pro%20#{version}.dmg"
   name "TG Pro"
