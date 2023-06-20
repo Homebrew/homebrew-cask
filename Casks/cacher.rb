a9c1e04ad2f62fe7261a7ea4932391b23f312403
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-20 19:06:28 -0400

    cacher 2.47.0

--- Casks/cacher.rb
+++ Casks/cacher.rb
@@ -1,6 +1,6 @@
 cask "cacher" do
-  version "2.46.0"
-  sha256 "97e482b223134316aa39facf7fac4db97e89f464d8852744917dc19de6cc9490"
+  version "2.47.0"
+  sha256 "387202089334afac091e8f4f484ca0862efc0abe8f889fa4be1f0632de27c715"
 
   url "https://cacher-download.nyc3.digitaloceanspaces.com/Cacher-#{version}-universal-mac.zip",
       verified: "cacher-download.nyc3.digitaloceanspaces.com/"
