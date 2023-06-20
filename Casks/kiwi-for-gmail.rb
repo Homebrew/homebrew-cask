0848618410c397133b4ea7e3008c805a06b26a5d
LanikSJ <laniksj@gmail.com>
2023-06-20 16:48:48 -0700

    kiwi-for-gmail 4.0.1

--- Casks/kiwi-for-gmail.rb
+++ Casks/kiwi-for-gmail.rb
@@ -1,5 +1,5 @@
 cask "kiwi-for-gmail" do
-  version "3.6.2,2518"
+  version "4.0.1,2540"
   sha256 :no_check
 
   url "https://downloads.kiwiforgmail.com/kiwi/release/consumer/Kiwi+for+Gmail+Setup.dmg"
