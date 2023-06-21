73348716f30db11afafd6c4909f83409f69642d9
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-21 07:59:40 -0400

    gcs 5.13.0

--- Casks/gcs.rb
+++ Casks/gcs.rb
@@ -1,9 +1,9 @@
 cask "gcs" do
   arch arm: "apple", intel: "intel"
 
-  version "5.12.0"
-  sha256 arm:   "b68f7a431a75cb4a43dc19729eb4138e32dc700f00daecf1dc126bc8a36d270e",
-         intel: "235fdff5432141047b4e08c9f213a31f50a0927d682bd0a1dccbc4d5511cbe7c"
+  version "5.13.0"
+  sha256 arm:   "13f9e997789a654c6320c6b96656d8a98881b30800853794094be430f9b96f27",
+         intel: "092d983f25c31174fcb7d5ebc9f97321da5519d641dc06fe23c81ab7ec669a24"
 
   url "https://github.com/richardwilkes/gcs/releases/download/v#{version}/gcs-#{version}-macos-#{arch}.dmg",
       verified: "github.com/richardwilkes/gcs/"
