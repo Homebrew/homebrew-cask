864329f788df55a8b8890e0dcebb916c3e76fa76
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-21 08:05:15 -0400

    prepros 7.8.0

--- Casks/prepros.rb
+++ Casks/prepros.rb
@@ -1,6 +1,6 @@
 cask "prepros" do
-  version "7.7.0"
-  sha256 "e7d7a23d70ccc1fa9c4a2fe1ae75cedda6b7215b62e08c771fa23fa4f40c8c22"
+  version "7.8.0"
+  sha256 "ef153f08aed1171382bf5947d15be688fca24a93353dc385a627c6f60c8d921e"
 
   url "https://downloads.prepros.io/v#{version.major}/#{version}/Prepros-#{version}.zip"
   name "Prepros"
