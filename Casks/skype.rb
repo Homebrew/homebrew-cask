7567a4c907d0392e04ab5326cc34515e9d87d117
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-21 22:35:10 -0400

    skype 8.99.0.202

--- Casks/skype.rb
+++ Casks/skype.rb
@@ -1,6 +1,6 @@
 cask "skype" do
-  version "8.98.0.407"
-  sha256 "1164c1dc9bfeb632dab6a4d39118f2eb0c6579fd0e48ed28e1b260eef17bee08"
+  version "8.99.0.202"
+  sha256 "2a7ee07d49fa210120e990e4943a1686fed2b1ab6a10137c066a21624d667477"
 
   url "https://endpoint920510.azureedge.net/s4l/s4l/download/mac/Skype-#{version}.dmg",
       verified: "endpoint920510.azureedge.net/s4l/s4l/download/mac/"
