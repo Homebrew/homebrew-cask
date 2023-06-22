8851d617bc0d46fc45137dd4a5c10fe0000db6e9
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-22 16:45:27 -0400

    dataspell 2023.1.3

--- Casks/dataspell.rb
+++ Casks/dataspell.rb
@@ -1,9 +1,9 @@
 cask "dataspell" do
   arch arm: "-aarch64"
 
-  version "2023.1.2,231.9011.36"
-  sha256 arm:   "f9ef32141d044c371de6f410f8d53f8b45bc7339aea45c29bfc345d1c54d9198",
-         intel: "5f4375f653ce28b45e17c972ca71b8b34866e76e9a324a6cc3bd482ad4421b04"
+  version "2023.1.3,231.9161.44"
+  sha256 arm:   "9dde75ec9fbccc9bfe7c390fd1f46a81fb153f226da7d3ca96bdeef5e60c51e1",
+         intel: "5d57d35ea08bc3ef9519b9669771a6d36a22424298e05dc82b18df787b52c79c"
 
   url "https://download.jetbrains.com/python/dataspell-#{version.csv.first}#{arch}.dmg"
   name "DataSpell"
