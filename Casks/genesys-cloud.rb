1fe87aa99cae52384148b490f28e245474fe1f3e
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-21 22:47:16 -0400

    genesys-cloud 2.24.562

--- Casks/genesys-cloud.rb
+++ Casks/genesys-cloud.rb
@@ -1,6 +1,6 @@
 cask "genesys-cloud" do
-  version "2.23.561,60"
-  sha256 "c110d90223baec7253eec3a8281007fe31fe75c11df5bacb8f20edf266adeefa"
+  version "2.24.562,61"
+  sha256 "280881f584695e5d9dd5211f9ade51337e68f7f1c852431e0eea54765c2c5f39"
 
   url "https://app.mypurecloud.com/directory-mac/build-assets/#{version.csv.first}-#{version.csv.second}/genesys-cloud-mac-#{version.csv.first}.dmg"
   name "Genesys Cloud for macOS"
