c0351713064bdeff06ee90b391d35ee4c2d8c942
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-22 16:46:43 -0400

    notion 2.1.17

--- Casks/notion.rb
+++ Casks/notion.rb
@@ -2,9 +2,9 @@ cask "notion" do
   arch arm: "-arm64"
   livecheck_folder = on_arch_conditional arm: "arm64", intel: "latest"
 
-  version "2.1.15"
-  sha256 arm:   "1a53c2963b97f3b1d530f99433fa5d63a9d4cfc41a67ee710e2d485529db1cea",
-         intel: "cd04c42533342a1ba2c1bfb345ce1ab3106842a5aa250bf04eeae56068c5b755"
+  version "2.1.17"
+  sha256 arm:   "0d012abc261b5f6c756ab4e08f146ef1064ceb85db77c8ec95a0ec75c4a015b2",
+         intel: "cd482fd1efd349a9f40ea1d56807833ad6481c5ec8d0bc82dd704e3a5a245667"
 
   url "https://desktop-release.notion-static.com/Notion-#{version}#{arch}.dmg",
       verified: "desktop-release.notion-static.com/"
