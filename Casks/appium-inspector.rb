abfc6cb272a001ebd172023ddb52b7085e6f7009
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-23 09:06:24 -0400

    appium-inspector 2023.6.2

--- Casks/appium-inspector.rb
+++ Casks/appium-inspector.rb
@@ -1,6 +1,6 @@
 cask "appium-inspector" do
-  version "2023.6.1"
-  sha256 "d76e4afa450b54d4f8f57b931ce96becfda5ff51f809153c1080c893f828d031"
+  version "2023.6.2"
+  sha256 "b42ac0987dbc41eb59325449730a69a96e5bdfeef0f434b452cf4e31034c6a5f"
 
   url "https://github.com/appium/appium-inspector/releases/download/v#{version}/Appium-Inspector-#{version}-universal-mac.zip"
   name "Appium Inspector GUI"
