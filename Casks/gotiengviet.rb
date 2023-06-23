d062fe91c3cc54d06837dff88b8279861ef30bdd
Phuc Nguyen Minh <minhphuc429@gmail.com>
2023-06-23 16:12:07 +0700

    Update gotiengviet.rb

--- Casks/gotiengviet.rb
+++ Casks/gotiengviet.rb
@@ -1,6 +1,6 @@
 cask "gotiengviet" do
-  version "2.3"
-  sha256 :no_check
+  version "2.4"
+  sha256 "c3be5eb6ad582674489fee63b45bfd97b34bd5501f4c766616a818a002138c18"
 
   url "https://www.trankynam.com/gotv/downloads/GoTiengViet.dmg"
   name "GoTiengViet"
