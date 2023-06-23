1239c0c17668c204a75e1c14920735d1a5fb8192
Tin Kyaw <tin@lsvp.com>
2023-06-23 09:24:00 -0700

    flacon 11.2.0

--- Casks/flacon.rb
+++ Casks/flacon.rb
@@ -1,6 +1,6 @@
 cask "flacon" do
-  version "11.1.0"
-  sha256 "fec17303d77771dd635590af469aeaf4f926534f5344664c9e1650fae55618cc"
+  version "11.2.0"
+  sha256 "469e426c48271fc9bbe12f355e5c89300ef6231c2d95f21b1e9fc4eaccea6e8c"
 
   url "https://github.com/flacon/flacon/releases/download/v#{version}/Flacon_#{version}.dmg",
       verified: "github.com/flacon/flacon/"
