cc0f0f72c632033a01c9d68089ae9d0a50843023
Russell Centanni <russell.centanni@gmail.com>
2023-06-23 20:47:13 -0400

    devspace: migrate to cask

---
+++ Casks/devspace.rb
@@ -0,0 +1,26 @@
+cask "devspace" do
+  arch arm: "arm64", intel: "amd64"
+
+  version "6.3.2"
+  sha256 arm:   "de09322bd4186192433a0bba3ac2f6284d1e2819b638871011365fbff788b382",
+         intel: "ddb2af8cc6e2b184e643df721e20a47b025aa70861f452f3a6c444de8a23ca89"
+
+  url "https://github.com/devspace-sh/devspace/releases/download/v#{version}/devspace-darwin-#{arch}",
+      verified: "github.com/devspace-sh/devspace/"
+  name "DevSpace"
+  desc "Develop and deploy cloud-native software with Docker and Kubernetes faster"
+  homepage "https://devspace.sh/"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on formula: "kubernetes-cli"
+  container type: :naked
+
+  binary "devspace-darwin-#{arch}", target: "devspace"
+
+  zap trash: "~/.devspace"
+end
