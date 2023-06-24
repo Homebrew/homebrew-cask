d6dcbb16ac719478bbb991f2ddd89ded904e7c68
Edward <73746306+WangEdward@users.noreply.github.com>
2023-06-24 17:25:45 +0800

    koodo-reader 1.5.6

--- Casks/koodo-reader.rb
+++ Casks/koodo-reader.rb
@@ -1,9 +1,9 @@
 cask "koodo-reader" do
   arch arm: "-arm64", intel: "-x64"
 
-  version "1.5.5"
-  sha256 arm:   "f19c27c49b37cf1236491341c295063639c7ce51628235a30fcdf391f3b72f93",
-         intel: "3e45542d990e2ac243321d361332339f2298d3e944194d9fcc3b0171714b68fa"
+  version "1.5.6"
+  sha256 arm:   "53000472714b3bf85bc7699f098b73eaf601e9c18d71b87957427cd3e8c7d715",
+         intel: "c90489f0c8e43dc4e07b4324e6020f309e0af043f3ae7a26b4c74f10026fd1b8"
 
   url "https://github.com/troyeguo/koodo-reader/releases/download/v#{version}/Koodo-Reader-#{version}#{arch}.dmg",
       verified: "github.com/troyeguo/koodo-reader/"
