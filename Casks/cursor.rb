65bfa0e5364a2489dce02b4f919a7dfee4d82690
Edward <73746306+WangEdward@users.noreply.github.com>
2023-06-25 14:06:01 +0800

    cursor 0.2.35

--- Casks/cursor.rb
+++ Casks/cursor.rb
@@ -1,9 +1,9 @@
 cask "cursor" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.2.33"
-  sha256 arm:   "1788be87386fc70db7e98141a1f8f8597d3a1b54bfd7f2c9c655ce9ea3a43205",
-         intel: "1d454cd6a869590a761230af2d6f931370a005802c7876fcfac69a3982cae8b7"
+  version "0.2.35"
+  sha256 arm:   "03f1e400aa20f465f373ce8a8cd2d81e9083c0a5d2ef7dc4d5ed819747ee78db",
+         intel: "f97ce01c715e273820b8a6229db72af66a8b1530ffd09adf7f250df129b36f2f"
 
   url "https://download.todesktop.com/230313mzl4w4u92/Cursor%20#{version}-#{arch}-mac.zip",
       verified: "download.todesktop.com/230313mzl4w4u92"
