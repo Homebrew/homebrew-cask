5909ddff9ac68b05e27c0bf6e27caff58595a1f7
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-26 07:29:16 -0400

    opera-gx 99.0.4788.86

--- Casks/opera-gx.rb
+++ Casks/opera-gx.rb
@@ -1,6 +1,6 @@
 cask "opera-gx" do
-  version "99.0.4788.75"
-  sha256 "8c447f77dc07c9ae5ee0812997c87c3718564778cfa342cc2d75fcdc16cb7cb6"
+  version "99.0.4788.86"
+  sha256 "4b9a8bc66a61410dc746e628a79538ab5a1f54cfc4ad173ef060ce4ded46fd23"
 
   url "https://get.geo.opera.com/pub/opera_gx/#{version}/mac/Opera_GX_#{version}_Setup.dmg"
   name "Opera GX"
