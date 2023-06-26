b05453e6681643ad40cca067a315f81992525443
Percy Ma <kecrily@gmail.com>
2023-06-27 02:44:01 +0800

    anytype 0.32.3

--- Casks/anytype.rb
+++ Casks/anytype.rb
@@ -1,18 +1,18 @@
 cask "anytype" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.31.9"
-  sha256 arm:   "a82bb95f077a921416b97e86534023d51858140491f32b44e4583b3d0c513882",
-         intel: "e8b98f7145f7d262a8a8edf79de958c71af9a28169e90fae4e2eb5a41f6f76d7"
+  version "0.32.3"
+  sha256 arm:   "0b44ed28eb6df6ec85cd59f1cc0d5cfb9cfbc8626b64966bae7bd2cfdf201ec7",
+         intel: "8309223a10ce26c6709b4650bc3e84f6f2fb5ed5d3f37ec07474062a63eb8016"
 
-  url "https://at9412003.fra1.cdn.digitaloceanspaces.com/Anytype-#{version}-mac-#{arch}.dmg",
-      verified: "at9412003.fra1.cdn.digitaloceanspaces.com/"
+  url "https://anytype-release.fra1.cdn.digitaloceanspaces.com/Anytype-#{version}-mac-#{arch}.dmg",
+      verified: "anytype-release.fra1.cdn.digitaloceanspaces.com/"
   name "Anytype"
   desc "Local-first and end-to-end encrypted notes app"
   homepage "https://anytype.io/"
 
   livecheck do
-    url "https://at9412003.fra1.digitaloceanspaces.com/latest-mac.yml"
+    url "https://anytype-release.fra1.cdn.digitaloceanspaces.com/latest-mac.yml"
     strategy :electron_builder
   end
 
