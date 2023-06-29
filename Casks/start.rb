ff5f18694fabf04c5e04ac049f384746e3574a00
Edward <73746306+WangEdward@users.noreply.github.com>
2023-06-29 19:54:51 +0800

    style start.rb: change version

--- Casks/start.rb
+++ Casks/start.rb
@@ -1,16 +1,14 @@
+# typed: true
+# frozen_string_literal: true
+
 cask "start" do
   arch arm: "arm", intel: "x86"
   folder_arch = on_arch_conditional arm: "m1/"
   livecheck_arch = on_arch_conditional arm: "-arm"
 
-  on_arm do
-    version "0.301.14084-45"
-    sha256 "5a84b3bd28a9654ca70b1a9375e7e7cd5fe1be87a9dfe20ec991daafa5434b2f"
-  end
-  on_intel do
-    version "0.301.14084-45"
-    sha256 "f125b978900c4a2098ef0aa20c5070f1c4b3f768b956d005fde3e16d2c5cb0bd"
-  end
+  version "0.301.14084-45"
+  sha256 arm:   "5a84b3bd28a9654ca70b1a9375e7e7cd5fe1be87a9dfe20ec991daafa5434b2f",
+         intel: "f125b978900c4a2098ef0aa20c5070f1c4b3f768b956d005fde3e16d2c5cb0bd"
 
   url "https://imgcdn.start.qq.com/cdn/mac.client/installer/#{folder_arch}START-Installer-#{arch}-#{version}.dmg"
   name "START"
