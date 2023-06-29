e6a42befb04921dff4575dc851804b7f4e00084b
MoonFruit <dkmoonfruit@gmail.com>
2023-06-29 15:30:11 +0800

    tencent-meeting 3.17.6.405

--- Casks/tencent-meeting.rb
+++ Casks/tencent-meeting.rb
@@ -2,12 +2,12 @@ cask "tencent-meeting" do
   arch arm: "arm64", intel: "x86_64"
 
   on_arm do
-    version "3.17.5.443,c08e8d5995859a5435ec276f08414d54"
-    sha256 "84606d3b230c60cc31e69c89cb7b79aa37520fddd6504c970f23a854515fccc9"
+    version "3.17.6.405,ff2aa83d934a1ed8f8880cdb032580b7"
+    sha256 "ceff0fc67225aff0c66f42f3c2e397df573f770a073f8a70199dadc33eb09950"
   end
   on_intel do
-    version "3.17.5.443,2f5c905fc027f936eb04a3a8527cf00f"
-    sha256 "5acc6ca9f555921d12786a5ac22385e5e48c46e536b9ddfbd972e16daae6c2f7"
+    version "3.17.6.405,3e4a752d49d19ec15581e27f80ed6651"
+    sha256 "2e911937d8d27e5922ee803744aeb31eefcd43fdeb18bc0ac2a454b0158662dd"
   end
 
   url "https://updatecdn.meeting.qq.com/cos/#{version.csv.second}/TencentMeeting_0300000000_#{version.csv.first}.publish.#{arch}.officialwebsite.dmg",
