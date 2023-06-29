a1174e158a9e5c40fbc28648d0b12a3140c1de31
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-06-29 16:34:34 -0400

    tiddly 0.0.17

--- Casks/tiddly.rb
+++ Casks/tiddly.rb
@@ -1,6 +1,6 @@
 cask "tiddly" do
-  version "0.0.16"
-  sha256 "5453bb8ccb8e1fcb7b97a2afc287a194dd1b0ac28d675951c4710322b98f3836"
+  version "0.0.17"
+  sha256 "991899b0a7fa6ebc8a971aea952e29ac49f3df48e88d3a5c0a9f6781f0d6e688"
 
   url "https://github.com/Jermolene/TiddlyDesktop/releases/download/v#{version}/tiddlydesktop-mac64-v#{version}.zip"
   name "TiddlyWiki"
