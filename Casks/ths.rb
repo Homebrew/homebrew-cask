f136c94db007e73fff9013d60d92146455b62f55
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-06-30 07:13:09 -0400

    cask updates

--- Casks/ths.rb
+++ Casks/ths.rb
@@ -1,22 +1,25 @@
 cask "ths" do
-  version "4.0.3,7.0.30"
-  sha256 :no_check
+  version "4.0.3"
+  sha256 "757c65787375723eda5b35005cc0d37dd848c49d3e5d79b5a8bc963fa2257617"
 
-  url "https://download.10jqka.com.cn/index/download/id/113/"
-  name "ths"
+  url "https://resource.thsi.cn/soft/ThsMac#{version}.dmg",
+      verified: "resource.thsi.cn/soft/"
+  name "同花顺"
+  name "Straight Flush"
   desc "Stock trading software"
   homepage "https://download.10jqka.com.cn/free/mac"
 
   livecheck do
-    strategy :extract_plist
+    url "https://download.10jqka.com.cn/index/download/id/113/"
+    strategy :header_match
   end
 
   app "同花顺.app"
 
   zap trash: [
-    "~/Library/Containers/cn.com.10jqka.macstock",
-    "~/Library/Containers/cn.com.10jqka.macstock.widget",
     "~/Library/Application Scripts/cn.com.10jqka.macstock",
     "~/Library/Application Scripts/cn.com.10jqka.macstock.widget",
+    "~/Library/Containers/cn.com.10jqka.macstock",
+    "~/Library/Containers/cn.com.10jqka.macstock.widget",
   ]
 end
