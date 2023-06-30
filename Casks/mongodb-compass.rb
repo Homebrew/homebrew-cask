00dffd13d16308791b4936c0651c9d260985e23b
MoonFruit <dkmoonfruit@gmail.com>
2023-06-30 16:40:57 +0800

    mongodb-compass 1.38.1

--- Casks/mongodb-compass.rb
+++ Casks/mongodb-compass.rb
@@ -1,9 +1,9 @@
 cask "mongodb-compass" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.38.0"
-  sha256 arm:   "c353d803cdfd01467a975415d671a033d30ec43b28486f4ee4e36ed33e61a366",
-         intel: "4ceac65881f070b1172d36870b6c5cd093cf9db59bbc53542b48d9ffac6ec974"
+  version "1.38.1"
+  sha256 arm:   "3822a6a76fa01af6bb36a7477df0b5bf02ffb860986c5469dba7c026f42948d2",
+         intel: "227fa271efd502c4458bf16891ffa459d1676e64d4deb5a4cf8d76f4efade126"
 
   url "https://downloads.mongodb.com/compass/mongodb-compass-#{version}-darwin-#{arch}.dmg"
   name "MongoDB Compass"
