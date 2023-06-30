ff628a0d9640c5887e98f62358c20e74cadac756
Edward <73746306+WangEdward@users.noreply.github.com>
2023-06-30 08:50:09 +0800

    burp-suite-professional 2023.6.2

--- Casks/burp-suite-professional.rb
+++ Casks/burp-suite-professional.rb
@@ -1,9 +1,9 @@
 cask "burp-suite-professional" do
   arch arm: "MacOsArm64", intel: "MacOsx"
 
-  version "2023.5.4"
-  sha256 arm:   "ec73180d68675600de48730553bbe3a162494e52da41cf17c08d10dff06e4bab",
-         intel: "b452c5811d1cd66658cc5933ad3dfbd35c91c2f624fedc044fe89133b58a1d90"
+  version "2023.6.2"
+  sha256 arm:   "852c8abd12908d5f864670cc02a1cf513bcf7c153855a43453a44e7583c0e562",
+         intel: "ec067503e757ec479a6e65f58503e1c31721e264ec23f995276d014cbaaf166b"
 
   url "https://portswigger.net/burp/releases/download?product=pro&version=#{version}&type=#{arch}"
   name "Burp Suite Professional"
