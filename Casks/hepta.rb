79c940c8847c2480b81bc82f73805f2a3a4e6a2d
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-01 09:14:30 -0400

    hepta 0.330.1

--- Casks/hepta.rb
+++ Casks/hepta.rb
@@ -1,9 +1,9 @@
 cask "hepta" do
   arch arm: "arm64-mac", intel: "mac"
 
-  version "0.329.0"
-  sha256 arm:   "662991a4efabd0dd6133a0227c9a7c77ecc85b24282353bd1bfed2cbbb813b10",
-         intel: "a2299c34344e635f6442a1cc6fc73fa3da7124cab044b17dac99b099c8dd2959"
+  version "0.330.1"
+  sha256 arm:   "837ae48b44b8afcd3aff4da7686ef3a4c4b34f9e4e1fb94aa98250c6f1e77e7d",
+         intel: "9e122256dd0971a818acde2eb27fa04ccd920b4d33e2c64c9dcd182e6be43b90"
 
   url "https://github.com/heptameta/project-meta/releases/download/v#{version}/Hepta-#{version}-#{arch}.zip",
       verified: "github.com/heptameta/project-meta/"
