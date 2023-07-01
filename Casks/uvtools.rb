26014f6072a81eb61d03bd4239624925302eeb68
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-01 05:12:39 +0000

    uvtools 3.15.1

--- Casks/uvtools.rb
+++ Casks/uvtools.rb
@@ -1,9 +1,9 @@
 cask "uvtools" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.15.0"
-  sha256 arm:   "0b6b5da69768ed8f0e22ac7ca54f5ca18719323dd085ce7daf32b0fbd0c3da5b",
-         intel: "c14e356cd813ba8ea0ab4ebf521fbe7fa115121d8807ae4ec347826baa4f3237"
+  version "3.15.1"
+  sha256 arm:   "28d71721d1b1628d263ccd31387ff2914bb5798a075a0dd07c9c2179c97acee0",
+         intel: "c7d029aa9bc88d7611510370852bb3ba3b22ac7910e1aab0597909a643d0f97c"
 
   url "https://github.com/sn4k3/UVtools/releases/download/v#{version}/UVtools_osx-#{arch}_v#{version}.zip"
   name "UVtools"
