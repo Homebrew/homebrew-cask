152abc3534d8f1858aa1975bd62353c3feac34a4
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-01 09:14:01 -0400

    netxms-console 4.4.0

--- Casks/netxms-console.rb
+++ Casks/netxms-console.rb
@@ -1,9 +1,9 @@
 cask "netxms-console" do
   arch arm: "-aarch64"
 
-  version "4.3.7"
-  sha256 arm:   "658d70cc8b662fea1c2a10bcc3e60920a5ddbe10b5dcd2a1a314fa19b7842d27",
-         intel: "1d7752f07b501bbc2e13324dfcbd06e124434adf7ee5d593f1deee1311c9db17"
+  version "4.4.0"
+  sha256 arm:   "38f5624ed9e8aa8b46548b9839773f58f5356fc41147f3bcb19f0b9b836c4c07",
+         intel: "2dfc3afb9b7bdfd1ae82b1fb8d11bc14b52b54967481b27eab8635e8460fe430"
 
   url "https://netxms.org/download/releases/#{version.major_minor}/nxmc-#{version}#{arch}.dmg"
   name "NetXMS Management Console"
