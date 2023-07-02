cd409031177763a7f245d2cf4beec3c078d0fdf8
Isaac Marovitz <42140194+IsaacMarovitz@users.noreply.github.com>
2023-07-02 17:25:53 -0400

    Update whisky.rb

--- Casks/whisky.rb
+++ Casks/whisky.rb
@@ -1,9 +1,8 @@
 cask "whisky" do
-  # Pre-release is currently the only release available
-  version "0.3.1"
-  sha256 "323d1693752f96eaa971c6df2ca1f04ae456c361f4192ee332683066eac1097d"
+  version "1.0.0"
+  sha256 "c483277cdc5dc6a679fb48652eaba0fba0e3f11b1d16fee7767b2c3238842a63"
 
-  url "https://github.com/IsaacMarovitz/Whisky/releases/download/pre-#{version}/Whisky.zip"
+  url "https://github.com/IsaacMarovitz/Whisky/releases/download/v#{version}/Whisky.zip"
   name "whisky"
   desc "Wine wrapper built with SwiftUI"
   homepage "https://github.com/IsaacMarovitz/Whisky"
@@ -19,10 +18,10 @@ cask "whisky" do
   app "Whisky.app"
 
   zap trash: [
-    "~/Library/Application Support/Whisky",
+    "~/Library/Application Support/com.isaacmarovitz.Whisky",
     "~/Library/Containers/com.isaacmarovitz.Whisky",
     "~/Library/HTTPStorages/com.isaacmarovitz.Whisky",
-    "~/Library/Logs/Whisky",
+    "~/Library/Logs/com.isaacmarovitz.Whisky",
     "~/Library/Preferences/com.isaacmarovitz.Whisky.plist",
     "~/Library/Saved Application State/com.isaacmarovitz.Whisky.savedState",
   ]
