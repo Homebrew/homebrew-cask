b7da3d1f4c90c7423a02c76e622d26a4c33178cb
Miccal Matthews <miccal.matthews@gmail.com>
2023-07-03 17:14:49 +0000

    Update pronterface.rb

--- Casks/pronterface.rb
+++ Casks/pronterface.rb
@@ -2,7 +2,7 @@ cask "pronterface" do
   version "2.0.1,3.10"
 
   on_big_sur :or_older do
-    sha256 "34467fb64dba31f43a15a503f2e1285b3e367a55d9a414a5bd33675d8e81719c"
+    sha256 "5c66655efd1f5e8906169467d9843e55ec5a7f9b72fc0d827e59b850ebf944fb"
 
     url "https://github.com/kliment/Printrun/releases/download/printrun-#{version.csv.first}/printrun-#{version.csv.first}_macos-11_x64_py#{version.csv.second}.zip"
   end
