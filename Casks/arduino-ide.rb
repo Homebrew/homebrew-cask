4b133b2f2c2262f34ea97a91771209ba6825a8e4
i0ntempest <szf1234@me.com>
2023-07-03 19:05:30 -0400

    arduino-ide 2.1.1

--- Casks/arduino-ide.rb
+++ Casks/arduino-ide.rb
@@ -1,19 +1,15 @@
 cask "arduino-ide" do
-  arch arm: "arm64", intel: "64bit"
+  arch arm: "ARM64", intel: "64bit"
 
-  version "2.1.0"
-  sha256 arm:   "507a0519591de9664b2b53d8744da424cfbcade5172cdf009d06756bb412fa59",
-         intel: "dc5e9b58421f6244d068ed7b4e85f209d313fa74e5e3080ece2b6e56c6e72cb4"
+  version "2.1.1"
+  sha256 arm:   "786d18416e80105c42105a1c2126c1a985d4ed6d3b34965fa85396f8466764c4",
+         intel: "8b50e791ea6b6b9dca2501d6fbcb98d0e30ace934c8b278cfca53b43f90f1bcc"
 
-  url "https://downloads.arduino.cc/arduino-ide/arduino-ide_#{version}_macOS_#{arch}.dmg"
+  url "https://github.com/arduino/arduino-ide/releases/download/#{version}/arduino-ide_#{version}_macOS_#{arch}.dmg",
+      verified: "github.com/arduino/arduino-ide/"
   name "Arduino IDE"
   desc "Electronics prototyping platform"
   homepage "https://www.arduino.cc/en/software"
-
-  livecheck do
-    url "https://www.arduino.cc/en/software/"
-    regex(/href=.*?arduino[._-]ide[._-]?(\d+(?:\.\d+)+)[._-]macos[._-]#{arch}\.dmg/i)
-  end
 
   conflicts_with cask: "arduino-ide-nightly"
 
