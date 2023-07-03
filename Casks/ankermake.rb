710430fabff973a707150606a85144b2b1d477ad
Miccal Matthews <miccal.matthews@gmail.com>
2023-07-03 06:15:10 +0000

    Update ankermake.rb

--- Casks/ankermake.rb
+++ Casks/ankermake.rb
@@ -1,20 +1,22 @@
 cask "ankermake" do
-  arch arm: "M1", intel: "Intel"
+  version "0.9.34_1,PtaR1wXoSj1GXEjC"
+  sha256 "6b5bc78e29387b6f1f5f6429e7b380aa53f9978e235154fe7f3eb2444fb8f4e9"
 
-  version "2.0.0"
-  sha256 :no_check
-
-  url "https://public-make-moat-us.s3.us-east-2.amazonaws.com/ankermake/slicer/AnkerMake_Installer_Mac_#{arch}.dmg",
-      verified: "public-make-moat-us.s3.us-east-2.amazonaws.com/ankermake/slicer/"
+  url "https://public-make-moat-us.s3.amazonaws.com/security/#{version.csv.second}.AnkerMake_V#{version.csv.first}.dmg",
+      verified: "public-make-moat-us.s3.amazonaws.com/security/"
   name "AnkerMake"
   desc "Slicer for AnkerMake 3D printers"
   homepage "https://www.ankermake.com/software"
 
   livecheck do
     url :url
-    strategy :extract_plist
-  end
+    strategy :page_match do |page|
+      match = page.match(%r{security/(.+)\.AnkerMake[._-]v?(\d+(?:.\d+)+)\.dmg}i)
+      next if match.blank?
 
+      "#{match[2]},#{match[1]}"
+    end
+  end
   depends_on macos: ">= :big_sur"
 
   app "AnkerMake.app"
