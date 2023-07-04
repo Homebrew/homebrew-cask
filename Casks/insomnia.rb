66f816f9446b6d72a24ee9d82a035b7438caef40
Bevan Kay <email@bevankay.me>
2023-07-05 09:18:15 +1000

    insomnia 2023.4.0

--- Casks/insomnia.rb
+++ Casks/insomnia.rb
@@ -1,6 +1,6 @@
 cask "insomnia" do
-  version "2023.3.0"
-  sha256 "e1d4e524fc8779b73e86323f66e1b5b838d0f9c1bc542e6c718db83223cc315f"
+  version "2023.4.0"
+  sha256 "6900f2595e98f2df6a938d85af4c2d463ea91e17b288a44bbfeaa0172455d285"
 
   url "https://github.com/Kong/insomnia/releases/download/core%40#{version}/Insomnia.Core-#{version}.dmg",
       verified: "github.com/Kong/insomnia/"
