6d316c5a9c71245cc031d9dccd0bb34cbb845b62
Miccal Matthews <miccal.matthews@gmail.com>
2023-07-04 21:09:50 +0800

    Update mpc.rb

--- Casks/mpc.rb
+++ Casks/mpc.rb
@@ -4,14 +4,16 @@ cask "mpc" do
 
   url "https://cdn.inmusicbrands.com/akai/MPC2/#{version.major_minor_patch.no_dots}_sufbchdj/Install-MPC-v#{version.major_minor}-#{version}-release-Mac.zip",
       verified: "cdn.inmusicbrands.com/akai/MPC2/"
-  name "mpc"
+  name "MPC"
   desc "Composition software (DAW) from Akai"
   homepage "https://www.akaipro.com/mpc-software-2"
 
   livecheck do
-    url "https://www.akaipro.com/mpc-software-2"
+    url :homepage
     regex(/href=.*?Install[._-]MPC[._-]v?[\d.]+[_-](\d+(?:\.\d+)+)[_-]release[_-]Mac\.zip"/i)
   end
+
+  depends_on macos: ">= : catalina"
 
   pkg "MPC-Installer-#{version}/Install MPC #{version}.app/Contents/Resources/InstallApp.pkg"
 
@@ -25,9 +27,9 @@ cask "mpc" do
   ]
 
   zap delete:    [
-        "/usr/local/bin/iloktool",
         "/Library/Preferences/com.paceap.eden.clientdb.*.sdb",
         "/Library/Preferences/com.paceap.eden.floating.*.prefs",
+        "/usr/local/bin/iloktool",
       ],
       launchctl: [
         "com.paceap.eden.licensed",
@@ -40,8 +42,8 @@ cask "mpc" do
       ],
       trash:     [
         "~/Library/Caches/com.paceap.iLokLicenseManager",
-        "~/Library/Logs/Eden",
         "~/Library/HTTPStorages/com.paceap.eden.iLokLicenseManager",
+        "~/Library/Logs/Eden",
         "~/Library/Preferences/com.paceap.iLokLicenseManager.plist",
         "~/Library/Saved Application State/com.paceap.eden.iLokLicenseManager.savedState",
       ]
