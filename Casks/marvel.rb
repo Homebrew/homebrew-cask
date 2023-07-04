8ed552e78b30dd4276bcdbd60a54069c6cdd4305
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-04 10:01:25 -0400

    marvel 11.1.0

--- Casks/marvel.rb
+++ Casks/marvel.rb
@@ -1,6 +1,6 @@
 cask "marvel" do
-  version "11.0.0"
-  sha256 "495fa8784e8574060ad063c02367cf8e6977271941ea60924b25de4465bebe55"
+  version "11.1.0"
+  sha256 "28f549114b9898453f3da71ede11439811cc3d4260eb426d356eb33e87259700"
 
   url "https://storage.googleapis.com/sketch-plugin/#{version}/Marvel.zip",
       verified: "storage.googleapis.com/sketch-plugin/"
