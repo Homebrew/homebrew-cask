e07b8b4e1f73336a856eab548c8031b81c7e7dc0
Miccal Matthews <miccal.matthews@gmail.com>
2023-07-06 02:09:46 +0800

    Update flexoptix.rb

--- Casks/flexoptix.rb
+++ Casks/flexoptix.rb
@@ -3,15 +3,17 @@ cask "flexoptix" do
   sha256 "bc1b9712c6a030a2b028fa259aba0673bc75463413d52096595c79ee48f6fd27"
 
   url "https://flexbox.reconfigure.me/download/electron/mac/x64/FLEXOPTIX%20App-#{version}.dmg",
-      verified: "flexbox.reconfigure.me/"
-  name "flexoptix"
-  desc "Program your FLEXOPTIX Trancievers using a FLEXBOX"
+      verified: "flexbox.reconfigure.me/download/electron/mac/x64/"
+  name "FLEXOPTIX App"
+  desc "Connect to your FLEXBOX without cables and configure transceivers"
   homepage "https://www.flexoptix.net/en/flexoptix-app/#"
 
   livecheck do
-    url "https://flexbox.reconfigure.me/download/electron/mac/x64/current"
-    strategy :header_match
+    url "https://flexbox.reconfigure.me/download/electron/mac/x64/latest-mac.yml"
+    strategy :electron_builder
   end
+
+  depends_on macos: ">= :el_capitan"
 
   app "FLEXOPTIX App.app"
 
@@ -20,4 +22,8 @@ cask "flexoptix" do
     "~/Library/Preferences/net.flexoptix.flexoptix.app.plist",
     "~/Library/Saved Application State/net.flexoptix.flexoptix.app.savedState",
   ]
+
+  caveats do
+    requires_rosetta
+  end
 end
