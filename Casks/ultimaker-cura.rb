521388314a0eec25efddd6bdd25080c86624663b
miccal <miccal.matthews@gmail.com>
2023-07-06 06:41:04 +0800

    ultimaker-cura 5.4.0

--- Casks/ultimaker-cura.rb
+++ Casks/ultimaker-cura.rb
@@ -1,6 +1,6 @@
 cask "ultimaker-cura" do
   version "5.4.0"
-  sha256 "3d6533dc417a4d8094e934cbc20f7f4f6374be8c8604e67086b5ec76bb5fc4f2"
+  sha256 "d5883826fda5b8125d60b1434161e8c7ba8805fa116b8c14c27bbbc14717bfe1"
 
   url "https://github.com/Ultimaker/Cura/releases/download/#{version}/Ultimaker-Cura-#{version}-mac.dmg",
       verified: "github.com/Ultimaker/Cura/"
