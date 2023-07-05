567dbcbdffa94d8279c505bb49738a5d1fc1239b
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-07-05 18:34:43 -0400

    4k-stogram 4.6.0

--- Casks/4k-stogram.rb
+++ Casks/4k-stogram.rb
@@ -1,6 +1,6 @@
 cask "4k-stogram" do
-  version "4.5.0"
-  sha256 "665143b57ca9e8bd489aca461fc4f011e62d6579ee9c16b1480d9c2ed2be7105"
+  version "4.6.0"
+  sha256 "53fda83c1f6e12730b12991eb942f3d167422dd7253fffe38b0d8ff043d44e8d"
 
   url "https://dl.4kdownload.com/app/4kstogram_#{version}.dmg"
   name "4K Stogram"
