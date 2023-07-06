6d73989749eab2304b33f723efc623462274390b
Loïc PÉRON <loic.peron@gmail.com>
2023-07-06 09:29:45 +0200

    logi-options-plus 1.46.425296

--- Casks/logi-options-plus.rb
+++ Casks/logi-options-plus.rb
@@ -1,5 +1,5 @@
 cask "logi-options-plus" do
-  version "1.44.415782"
+  version "1.46.425296"
   sha256 :no_check
 
   url "https://download01.logi.com/web/ftp/pub/techsupport/optionsplus/logioptionsplus_installer.zip",
