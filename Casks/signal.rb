ec1e2232c3eeef47e7f7f261c6440e7389bdd4df
Patrick Linnane <patrick@linnane.io>
2023-07-05 20:08:51 -0700

    signal 6.24.0

--- Casks/signal.rb
+++ Casks/signal.rb
@@ -1,9 +1,9 @@
 cask "signal" do
   arch arm: "arm64", intel: "x64"
 
-  version "6.23.0"
-  sha256 arm:   "04c7b42d8098a762dd320458cf7cfede772e2f732f66de5d5cfcb17144cc9df0",
-         intel: "356a773aab5978d5a78c6fd049b763626421f57b3691e33f7b880dcfc81e3367"
+  version "6.24.0"
+  sha256 arm:   "0864c24a0a5117138779dd1552fbb30fac9d3faa332be6b8e4ed54597962f0d7",
+         intel: "33bf618ece625aec6a3dce87c60981efc3e3b09b74b57048e1a42ae3f229cf8d"
 
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{arch}-#{version}.dmg"
   name "Signal"
