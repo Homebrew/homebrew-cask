d0efe0a6e3ccda49263a6232437ef98a253b3a43
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-07 09:31:11 -0400

    knime 4.7.5

--- Casks/knime.rb
+++ Casks/knime.rb
@@ -1,9 +1,9 @@
 cask "knime" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "4.7.4"
-  sha256 arm:   "4a49ccf7f64c7c015b4a1354a1c14ad6098b88e8e4d6d74327a4bcb11aae3b12",
-         intel: "9f04edbe699b073c0bfb563d66ba545a1536612078b478dd6f72cd3da52daf00"
+  version "4.7.5"
+  sha256 arm:   "dd576330f355456e003b3ef03d2e16620c139117d5431a308b3c2c6a44dc2e68",
+         intel: "0a3be0534c1056e9d5b1a1442574565fc99b91cc1fd5d23a3ac08644be7f89c9"
 
   url "https://download.knime.org/analytics-platform/macosx/knime_#{version}.app.macosx.cocoa.#{arch}.dmg",
       verified: "download.knime.org/analytics-platform/macosx/"
