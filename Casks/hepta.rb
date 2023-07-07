244c4942576838f432499f3164298c1ddc587c7f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-07 05:09:41 +0000

    hepta 0.336.0

--- Casks/hepta.rb
+++ Casks/hepta.rb
@@ -1,9 +1,9 @@
 cask "hepta" do
   arch arm: "arm64-mac", intel: "mac"
 
-  version "0.333.1"
-  sha256 arm:   "be850a938861b73408c5a99c5611c02f9d09bfec5ff59be24261f0d52e511728",
-         intel: "4404e32b53c818de581d8bc3437dacfcdbf5dd5d5034edf3d982aadb2e2d2953"
+  version "0.336.0"
+  sha256 arm:   "4656cc0c226337f91b6da2fc43b8de500cdfd7112553ef30a998e763663741f9",
+         intel: "ef52858bd07a42b06f2e69e9e1b452f88c995c5e1f6a215fd57924337f5a2c8e"
 
   url "https://github.com/heptameta/project-meta/releases/download/v#{version}/Hepta-#{version}-#{arch}.zip",
       verified: "github.com/heptameta/project-meta/"
