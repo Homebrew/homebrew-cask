b3ee70b3d292434e2972a0cbede4816a6b35be7d
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-08 19:21:17 -0400

    oak9 0.11.23

--- Casks/oak9.rb
+++ Casks/oak9.rb
@@ -1,7 +1,7 @@
 cask "oak9" do
   # NOTE: "9" is not a version number, but an intrinsic part of the product name
-  version "0.11.19"
-  sha256 "c96ad641f18af3728b33eaacb892ef3dda3a135c47d5691e431395bcd8878fd8"
+  version "0.11.23"
+  sha256 "8cb02c145f8c4cc5122bb416d50a0d4437006930a33576eee2f8fdeb0cde5df7"
 
   url "https://github.com/oak9io/oak9.cli-public/raw/master/binaries/iac-mac_os-#{version}.zip",
       verified: "github.com/oak9io/oak9.cli-public/"
