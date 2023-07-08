73b0f4cbc0135ffd007c63f6098b93535ed2f8bb
James Cuzella <james.cuzella@lyraphase.com>
2023-07-08 17:16:29 -0500

    Migrate elektron-overbridge ( Copy direct as per: Homebrew/homebrew-cask-drivers#3414 )

---
+++ Casks/elektron-overbridge.rb
@@ -0,0 +1,27 @@
+cask "elektron-overbridge" do
+  version "2.1.1.2,7fc48133-4bbc-5e3b-b646-c180686f9c2d"
+  sha256 "b2a7b13a0dd4e971562d7668a9c0783c073c2be20756b762242c5480786c9af9"
+
+  url "https://se-elektron-devops.s3.amazonaws.com/release/#{version.csv.second}/Elektron_Overbridge_#{version.csv.first}.dmg",
+      verified: "se-elektron-devops.s3.amazonaws.com/release/"
+  name "Overbridge"
+  desc "Integrate Elektron hardware into music software"
+  homepage "https://www.elektron.se/overbridge/"
+
+  livecheck do
+    url "https://www.elektron.se/support/?connection=overbridge"
+    regex(%r{href=.*?/(\h+(?:-\h+)*)/Elektron[._-]?Overbridge[._-]?v?(\d+(?:\.\d+)+)\.dmg}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[1]},#{match[0]}" }
+    end
+  end
+
+  depends_on macos: ">= :sierra"
+
+  pkg "Elektron Overbridge Installer #{version.before_comma}.pkg"
+
+  uninstall quit:      "se.elektron.OverbridgeEngine",
+            pkgutil:   "se.elektron.overbridge.*",
+            launchctl: "se.elektron.overbridge.engine",
+            delete:    "/Applications/Elektron"
+end
