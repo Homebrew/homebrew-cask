1b41c4d54a7ba6fe8d67ccda053bc4e4094a6246
James Cuzella <james.cuzella@lyraphase.com>
2023-07-08 18:42:30 -0500

    elektron-overbridge: Fix outdated livecheck RegExp, URL

--- Casks/elektron-overbridge.rb
+++ Casks/elektron-overbridge.rb
@@ -2,17 +2,16 @@ cask "elektron-overbridge" do
   version "2.1.1.2,7fc48133-4bbc-5e3b-b646-c180686f9c2d"
   sha256 "b2a7b13a0dd4e971562d7668a9c0783c073c2be20756b762242c5480786c9af9"
 
-  url "https://se-elektron-devops.s3.amazonaws.com/release/#{version.csv.second}/Elektron_Overbridge_#{version.csv.first}.dmg",
-      verified: "se-elektron-devops.s3.amazonaws.com/release/"
+  url "https://cdn.www.elektron.se/media/downloads/overbridge/Elektron_Overbridge_#{version.csv.first}.dmg"
   name "Overbridge"
   desc "Integrate Elektron hardware into music software"
   homepage "https://www.elektron.se/overbridge/"
 
   livecheck do
-    url "https://www.elektron.se/support/?connection=overbridge"
-    regex(%r{href=.*?/(\h+(?:-\h+)*)/Elektron[._-]?Overbridge[._-]?v?(\d+(?:\.\d+)+)\.dmg}i)
+    url "https://www.elektron.se/us/download-support-overbridge-new"
+    regex(%r{https?:\/\/(cdn\.)?(www\.)?elektron\.se\/(media\/)?(downloads\/)?(overbridge\/)?Elektron[._-]?Overbridge[._-]?v?(\d+(?:\.\d+)+)\.dmg}i)
     strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| "#{match[1]},#{match[0]}" }
+      page.scan(regex).map { |match| "#{match[5]}," }
     end
   end
 
