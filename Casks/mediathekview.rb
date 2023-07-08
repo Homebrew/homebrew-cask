6b3f394eeced09d550642d4344e98e94951ab9c3
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-08 19:22:58 -0400

    mediathekview 14.0.0

--- Casks/mediathekview.rb
+++ Casks/mediathekview.rb
@@ -1,9 +1,9 @@
 cask "mediathekview" do
   arch arm: "mac-as", intel: "mac"
 
-  version "13.9.1"
-  sha256 arm:   "4eb0e2de15f1852b66ac16b84b6fdba1c3dfc18e19a646763c4e636a7c7932b0",
-         intel: "4529bbed37b3736623b5083e99088866bfec98973383df2553c27175738072a9"
+  version "14.0.0"
+  sha256 arm:   "6a50961dfd889c3cc6e3ad91e5bf6b575d44fe9440996339b432e158f347a746",
+         intel: "76c2066dcf36ceabcd8235194fbef7a878a0628092aa38d0d40ee6e4bddce63f"
 
   url "https://download.mediathekview.de/stabil/MediathekView-#{version}-#{arch}.dmg"
   name "MediathekView"
