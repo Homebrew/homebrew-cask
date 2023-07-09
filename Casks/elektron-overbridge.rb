78c73373a877d8017f72a728e60f31ecaeac8fb3
James Cuzella <james.cuzella@lyraphase.com>
2023-07-08 19:00:53 -0500

    elektron-overbridge: Fix RuboCop nitpicks

--- Casks/elektron-overbridge.rb
+++ Casks/elektron-overbridge.rb
@@ -1,6 +1,6 @@
 cask "elektron-overbridge" do
-  version "2.1.1.2,7fc48133-4bbc-5e3b-b646-c180686f9c2d"
-  sha256 "b2a7b13a0dd4e971562d7668a9c0783c073c2be20756b762242c5480786c9af9"
+  version "2.5.1"
+  sha256 ""
 
   url "https://cdn.www.elektron.se/media/downloads/overbridge/Elektron_Overbridge_#{version.csv.first}.dmg"
   name "Overbridge"
@@ -9,7 +9,13 @@ cask "elektron-overbridge" do
 
   livecheck do
     url "https://www.elektron.se/us/download-support-overbridge-new"
-    regex(%r{https?:\/\/(cdn\.)?(www\.)?elektron\.se\/(media\/)?(downloads\/)?(overbridge\/)?Elektron[._-]?Overbridge[._-]?v?(\d+(?:\.\d+)+)\.dmg}i)
+    regex(
+      %r{
+        https?://(cdn\.)?(www\.)?elektron\.se/
+          (media/)?(downloads/)?(overbridge/)?
+            Elektron[._-]?Overbridge[._-]?v?(\d+(?:\.\d+)+)\.dmg
+      }ix,
+    )
     strategy :page_match do |page, regex|
       page.scan(regex).map { |match| "#{match[5]}," }
     end
