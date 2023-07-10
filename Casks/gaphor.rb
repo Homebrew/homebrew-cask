8ed5a64bd4fd7814738543da127131687ba7fde9
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-10 09:11:42 -0400

    gaphor 2.19.2

--- Casks/gaphor.rb
+++ Casks/gaphor.rb
@@ -1,6 +1,6 @@
 cask "gaphor" do
-  version "2.19.1"
-  sha256 "a5e4eb84c785d101098961d66343d09f4dc669fc11495e007c8e76ef6fd49e4d"
+  version "2.19.2"
+  sha256 "5468a922054f9457ccf74f46011e83dc18dd5b199004917c5a45e54c8330cf8b"
 
   url "https://github.com/gaphor/gaphor/releases/download/#{version}/Gaphor-#{version}.dmg",
       verified: "github.com/gaphor/gaphor/"
