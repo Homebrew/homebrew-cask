1d0d6ee962634adde6491271326fa166af156444
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-07-11 16:17:21 -0400

    miro 0.7.37

--- Casks/miro.rb
+++ Casks/miro.rb
@@ -1,7 +1,7 @@
 cask "miro" do
   arch arm: "darwin-arm64", intel: "darwin"
 
-  version "0.7.36"
+  version "0.7.37"
   sha256 :no_check
 
   url "https://desktop.miro.com/platforms/#{arch}/Miro.dmg"
