27cee934ce7dc1bf0d52bab9350d72f2d038d3ec
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-11 08:40:44 -0400

    virtual-ii: add desc, depends_on, and zap

--- Casks/virtual-ii.rb
+++ Casks/virtual-ii.rb
@@ -4,6 +4,7 @@ cask "virtual-ii" do
 
   url "https://virtualii.com/VirtualII.dmg"
   name "Virtual ]["
+  desc "Apple II Emulator"
   homepage "https://virtualii.com/"
 
   livecheck do
@@ -11,12 +12,21 @@ cask "virtual-ii" do
     regex(/version\s+(\d+(?:\.\d+)+)/i)
   end
 
+  depends_on macos: ">= :high_sierra"
+
   suite "Virtual ]["
 
   preflight do
     # There is no sub-folder in the DMG; the root *is* the folder
     FileUtils.mv(staged_path.children, staged_path.join("Virtual ][").tap(&:mkpath))
   end
+
+  zap trash: [
+    "~/Library/Application Support/Virtual ][",
+    "~/Library/Caches/SentryCrash/Virtual ][",
+    "~/Library/Preferences/nl.xs4all.gp.virtualii.plist",
+    "~/Library/Saved Application State/nl.xs4all.gp.virtualii.savedState",
+  ]
 
   caveats <<~EOS
     This app requires a ROM image, which must be downloaded and installed
