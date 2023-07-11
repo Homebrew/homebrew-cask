6b6b7b362ebcd0b534851933e8450379e9770389
Mikl Wolfe <wolfemikl@gmail.com>
2023-07-11 16:36:28 -0600

    microsoft-outlook 16.75.23070901

--- Casks/microsoft-outlook.rb
+++ Casks/microsoft-outlook.rb
@@ -20,8 +20,8 @@ cask "microsoft-outlook" do
     sha256 "bddede85956713be21fdb5ab72be07ecefd05552752e8e60c649e6a15fd0a2c2"
   end
   on_big_sur :or_newer do
-    version "16.74.23062500"
-    sha256 "140daf0f9f98a382a0a1a2e6cd0b17a9db0c30210d7fdab65ad9e31426e466b3"
+    version "16.75.23070901"
+    sha256 "d9ae98fd3d0bd6c4abf6d7f5ff19ce853391a0f8f5c22afe5abfe2092e558f86"
   end
 
   url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Outlook_#{version}_Installer.pkg",
