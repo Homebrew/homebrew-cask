f814286663549eaa8634d15ba7f3809af06656eb
Rahul Mohandas <rahul0705@gmail.com>
2023-07-12 19:15:12 -0400

    feat(warcraft logs) added cask

--- Casks/warcraft-logs-uploader.rb
+++ Casks/warcraft-logs-uploader.rb
@@ -2,7 +2,7 @@ cask "warcraft-logs-uploader" do
   version "6.0.2"
   sha256 "4f9f08b9eb88f63542e43e254df16672ceadb30e2e4112d19307fd920cd5aae9"
 
-  url "https://github.com/RPGLogs/Uploaders-warcraftlogs/releases/download/v#{version}/Warcraft-Logs-Uploader-6.0.2-universal.dmg",
+  url "https://github.com/RPGLogs/Uploaders-warcraftlogs/releases/download/v#{version}/Warcraft-Logs-Uploader-#{version}-universal.dmg",
       verified: "github.com/RPGLogs/Uploaders-warcraftlogs/"
   name "warcraft-logs-uploader"
   desc "Client to upload logs to Warcraft Logs"
