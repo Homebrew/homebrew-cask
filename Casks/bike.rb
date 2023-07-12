80b19ffda13f296991e0b97c08049992993e75d1
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-12 13:56:53 -0400

    bike 1.13,139

--- Casks/bike.rb
+++ Casks/bike.rb
@@ -1,6 +1,6 @@
 cask "bike" do
-  version "1.13,138"
-  sha256 "0b4a01da44820ad2bb01794aae20530a27dad32c259bc4172a38ade44b6b9abe"
+  version "1.13,139"
+  sha256 "0621634a44b1d5e30ba2f069ac3aed5d082177a39190c7d2adee7844e51fd36a"
 
   url "https://www.hogbaysoftware.com/bike/releases/#{version.csv.second}.dmg"
   name "Bike"
