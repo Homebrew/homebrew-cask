65a423f3d7f03eef8694d7dfca3a13888abc2f92
UKMeng <ukmengse@gmail.com>
2023-07-12 11:25:34 +0800

    Add MAA.app v4.19.3

---
+++ Casks/maa.rb
@@ -0,0 +1,24 @@
+cask "maa" do
+  version "4.19.3"
+  sha256 "536ec0b8a6238e07c62811cc45e5591a5a0830a495dea3260640e9c5e033522e"
+
+  url "https://github.com/MaaAssistantArknights/MaaAssistantArknights/releases/download/v#{version}/MAA-v#{version}-macos-universal.dmg"
+  name "MAA"
+  desc "One-click tool for the daily tasks of Arknights"
+  homepage "https://github.com/MaaAssistantArknights/MaaAssistantArknights"
+
+  livecheck do
+    url :url
+    regex(/v?(\d+(?:\.\d+)+[\w.]+)/i)
+    strategy :github_latest
+  end
+
+  auto_updates true
+
+  app "MAA.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.hguandl.MeoAsstMac",
+    "~/Library/Containers/com.hguandl.MeoAsstMac",
+  ]
+end
