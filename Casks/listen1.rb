4f1117c48d9d799602dc857ef1384434da614b0b
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-13 10:05:37 -0400

    listen1 2.29.0

--- Casks/listen1.rb
+++ Casks/listen1.rb
@@ -2,9 +2,9 @@ cask "listen1" do
   # NOTE: "1" is not a version number, but an intrinsic part of the product name
   arch arm: "arm64", intel: "x64"
 
-  version "2.28.0"
-  sha256 arm:   "a71abe781fe85ee47e5225bb51b4a245a7113c2bc4b4e347c0020c08b64bdfdd",
-         intel: "0f836da96ea85d84e8bcb8e8c3df90f4199c9476f947875d7857f3424c7d7f33"
+  version "2.29.0"
+  sha256 arm:   "012739979ab391840307450a02cd8f8a42555f810aea7a2b80dd907da2c43162",
+         intel: "43dcb3f476a95df0aece1d452dc97d8071ffc0c44d562b853634d85b6adf4637"
 
   url "https://github.com/listen1/listen1_desktop/releases/download/v#{version}/Listen1_#{version}_mac_#{arch}.dmg",
       verified: "github.com/listen1/listen1_desktop/"
