22b7290644caa52dc91ce6bf141b0a97f425bc8b
Massimiliano Pasquini <massimiliano.pasquini@outlook.com>
2023-07-13 10:23:02 +0200

    Updated raspberry-pi-imager.rb
    - Changed download url from raspberrypi site to github page.
    - Added universal binary for arm.

--- Casks/raspberry-pi-imager.rb
+++ Casks/raspberry-pi-imager.rb
@@ -1,16 +1,15 @@
 cask "raspberry-pi-imager" do
-  version "1.7.5"
-  sha256 "bd690d70253db356965e065a6098170874dfc48f864b0c157c6dda28ac2479f7"
+  arch arm: "UNIVERSAL.BUILD.", intel: ""
 
-  url "https://downloads.raspberrypi.org/imager/imager_#{version}.dmg"
+  version "1.7.5"
+  sha256 arm:   "03f73b1402acd52718e27496e8c2a3778f5fc1d6845cd19b04318ffde24d6c4b",
+         intel: "bd690d70253db356965e065a6098170874dfc48f864b0c157c6dda28ac2479f7"
+
+  url "https://github.com/raspberrypi/rpi-imager/releases/download/v#{version}/Raspberry.Pi.Imager.#{version}.#{arch}dmg",
+      verified: "github.com/raspberrypi/rpi-imager/"
   name "Raspberry Pi Imager"
   desc "Imaging utility to install operating systems to a microSD card"
   homepage "https://www.raspberrypi.org/downloads/"
-
-  livecheck do
-    url "https://downloads.raspberrypi.org/imager/imager_latest.dmg"
-    strategy :header_match
-  end
 
   app "Raspberry Pi Imager.app"
 
