488df1b626a9eda275d0fe2a859fb9410b2d7972
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-15 07:59:56 -0400

    portfolioperformance 0.64.3

--- Casks/portfolioperformance.rb
+++ Casks/portfolioperformance.rb
@@ -1,9 +1,9 @@
 cask "portfolioperformance" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.64.2"
-  sha256 arm:   "b2a230b07c85b1f1a805b234ae5bedd13d95b3855e5c0ffd610eb89e184005d1",
-         intel: "c68f77a4b962aeacf11668d43e955ecdc07c02e20c6e70cd5e4e4c181411a5b2"
+  version "0.64.3"
+  sha256 arm:   "a4ef230334db44813640b55839bf5232d1d90d566ec9d3832b6ed357203c910a",
+         intel: "e7171ada783c6467da5872b24db5d8baec5c9df79b32e87154223217f2448b80"
 
   url "https://github.com/buchen/portfolio/releases/download/#{version}/PortfolioPerformance-#{version}-#{arch}.dmg",
       verified: "github.com/buchen/portfolio/"
