89b30cbd9222c50e8a4c3d2868a8ef6ebc5c2dd4
Miccal Matthews <miccal.matthews@gmail.com>
2023-07-15 22:34:30 +0800

    Update gpgfrontend.rb

--- Casks/gpgfrontend.rb
+++ Casks/gpgfrontend.rb
@@ -2,13 +2,13 @@ cask "gpgfrontend" do
   version "2.1.1"
 
   on_big_sur do
-    sha256 :no_check
+    sha256 "365ffc4a42efbb695b52c03fc98f861e388c4ca38b3921f0597ccc49a08b44ae"
 
     url "https://github.com/saturneric/GpgFrontend/releases/download/v#{version}/GpgFrontend-#{version}-macos-11.dmg",
         verified: "github.com/saturneric/GpgFrontend/"
   end
   on_monterey :or_newer do
-    sha256 :no_check
+    sha256 "480b2b603da29fcff0ff22f42bff853580b637d17c61ed401bd9a3cd4c5d4c10"
 
     url "https://github.com/saturneric/GpgFrontend/releases/download/v#{version}/GpgFrontend-#{version}-macos-12.dmg",
         verified: "github.com/saturneric/GpgFrontend/"
