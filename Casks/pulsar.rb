b0df17419fbdf65ac72bd9f8665d50991c892a42
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-16 11:26:40 -0400

    pulsar 1.107.1

--- Casks/pulsar.rb
+++ Casks/pulsar.rb
@@ -2,9 +2,9 @@ cask "pulsar" do
   arch arm: "Silicon", intel: "Intel"
   arch_suffix = on_arch_conditional arm: "-arm64"
 
-  version "1.107.0"
-  sha256 arm:   "fc7f6d72d81abe033e0b21bc1972c97e75ed8469ffdf7173d21acf623647a987",
-         intel: "f98abb9a8dc310058c98fa5e12348e8ac45dff8cd795cf28d241a9acf5b7783a"
+  version "1.107.1"
+  sha256 arm:   "703be78f2ea3dcd3f9c048c22cd82e483be4820513584e1daa298f1828a9d89e",
+         intel: "4f838e8d32dbedb355356e79f7dc330932d69567c36d3542bad1154523d4803a"
 
   url "https://github.com/pulsar-edit/pulsar/releases/download/v#{version}/#{arch}.Mac.Pulsar-#{version}#{arch_suffix}-mac.zip",
       verified: "github.com/pulsar-edit/pulsar/"
