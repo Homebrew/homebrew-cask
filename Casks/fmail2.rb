0836d8437e2236a03fe842c61d224b4db735cbf9
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-17 08:57:58 -0400

    fmail2 2.3.7

--- Casks/fmail2.rb
+++ Casks/fmail2.rb
@@ -1,7 +1,7 @@
 cask "fmail2" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "2.3.6"
-  sha256 "9d58e68e81d9074bc8ec1f58d41d6df434889494c2f0e88fc5b164ea94e63e66"
+  version "2.3.7"
+  sha256 "8445f990921a5064985a8216bb13416ab4f38989357e9848d78927616887658f"
 
   url "https://arievanboxel.fr/fmail2/sparkle/FMail2_#{version.no_dots}.zip",
       verified: "arievanboxel.fr/fmail2/sparkle/"
