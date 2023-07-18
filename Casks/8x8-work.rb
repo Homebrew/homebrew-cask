aa998167b838484a1224a094b368b5e8d16ec7d8
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-18 09:22:58 -0400

    8x8-work 8.4.6-1

--- Casks/8x8-work.rb
+++ Casks/8x8-work.rb
@@ -1,6 +1,6 @@
 cask "8x8-work" do
-  version "8.4.5-4"
-  sha256 "97e85a7c23ffd8ffb85240707ba36f040008615e93c0698046f08b5dd9defb5a"
+  version "8.4.6-1"
+  sha256 "5d9e869a31f825032018e86be72a744eb583d4c4dce673fdeb976ac41bef349b"
 
   url "https://vod-updates.8x8.com/ga/work-dmg-v#{version}.dmg"
   name "8x8_work"
