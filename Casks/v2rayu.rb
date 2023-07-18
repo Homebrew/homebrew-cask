a68945b5c1871672a05960b28e973a3d771f6ab4
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-18 09:08:58 -0400

    v2rayu 3.8.0

--- Casks/v2rayu.rb
+++ Casks/v2rayu.rb
@@ -1,9 +1,9 @@
 cask "v2rayu" do
   arch arm: "arm64", intel: "64"
 
-  version "3.7.0"
-  sha256 arm:   "d0967aede4b1562af28eba55c99c49f19fc7ece55462312625ebc582c7be86d1",
-         intel: "cdc914bf19f24cec53880f1f2be916eae3c004fcd0d21a0babdc9d7adbb2f270"
+  version "3.8.0"
+  sha256 arm:   "2215b7c29780ecd395277b43611eb67b1fc0da8adf3925320e666b49e1ff12a9",
+         intel: "c36f0140842c24f9613eec7534e7591f1c1497b0d74437a0eb297d1a5f7e264a"
 
   url "https://github.com/yanue/V2rayU/releases/download/v#{version}/V2rayU-#{arch}.dmg"
   name "V2rayU"
