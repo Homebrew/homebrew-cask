f7cf557087c752b71de58e956f07dc22914201a0
runner <runner@Mac-1689725648185.local>
2023-07-19 00:33:05 +0000

    finch 0.7.0

--- Casks/finch.rb
+++ Casks/finch.rb
@@ -1,9 +1,9 @@
 cask "finch" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.6.2"
-  sha256 arm:   "6253074c932171a8c9246b5b29be960ed37e4e54fa5b493a09100f110e8d44ff",
-         intel: "69cdc8af920d07f50e011dc7ac1e41d93d9e217aa0709adb412e59f462437a35"
+  version "0.7.0"
+  sha256 arm:   "7e74db60b13196fd459ea9d4f016b373fad59bc1901c17ac13cc2b89a97f49c0",
+         intel: "bbd19619c4175d487107b01f908c3f8f90be4bbb5bfbad5bb061ac41227155d8"
 
   url "https://github.com/runfinch/finch/releases/download/v#{version}/Finch-v#{version}-#{arch}.pkg"
   name "Finch"
