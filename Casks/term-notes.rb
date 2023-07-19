7d7669dcbc1e8e7681452087662c39c8655db89a
Felipe Alfonso González <felipe.alfonso.glz@gmail.com>
2023-07-19 03:12:06 -0400

    Update term-notes.rb

--- Casks/term-notes.rb
+++ Casks/term-notes.rb
@@ -1,36 +1,28 @@
-class TermNotes < Formula
+cask "term-notes" do
+  version "1.0.2"
+  sha256 "e1ae65286b64c3466d5749524c79df1063ec9db35b265f0359d24cc76397d88c"
+
+  url "https://github.com/felipealfonsog/TermNotes/archive/refs/tags/v#{version}.tar.gz"
+  name "Term Notes"
   desc "Terminal-based note-taking application"
   homepage "https://github.com/felipealfonsog/TermNotes"
-  url "https://github.com/felipealfonsog/TermNotes/archive/refs/tags/v.1.0.2.tar.gz"
-  sha256 "e1ae65286b64c3466d5749524c79df1063ec9db35b265f0359d24cc76397d88c"
-  license "MIT"
 
-  depends_on "gcc"
-  depends_on "vim"
-  depends_on "nano"
+  depends_on formula: "gcc"
+  depends_on formula: "vim"
+  depends_on formula: "nano"
 
   def install
-    if OS.mac?
-      bin.install "term_notes" => "term-notes"
-    else
-      bin.install "term_notes"
-    end
+    bin.install "term_notes"
   end
 
-  def caveats
-    <<~EOS
-      'Term Notes' has been installed!
+  caveats <<~EOS
+    'Term Notes' has been installed!
 
-      On Mac OS, you can run the program using 'term-notes'.
+    To run the program, use the command:
+      term-notes
 
-      On Linux, you may need to create a symbolic link to make it accessible globally.
-      Run the following command:
-
-        sudo ln -s #{opt_bin}/term_notes /usr/local/bin/term-notes
-
-      Enjoy taking notes with 'Term Notes'!
-    EOS
-  end
+    Enjoy taking notes with 'Term Notes'!
+  EOS
 
   test do
     # Add your test logic here
