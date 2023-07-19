1912420e0ff8542d8aca467a82488830c88a69d3
Franck Brun <fbrun@moxadventu.com>
2023-07-19 13:19:34 +0200

    add ShowMeYourHotKeys

---
+++ Casks/showmeyourhotkeys.rb
@@ -0,0 +1,18 @@
+cask "showmeyourhotkeys" do
+  version "1.7"
+  sha256 :no_check
+
+  url "https://showmeyourhotkeys.moxadventu.com/app/ShowMeYourHotKeys.zip"
+  name "showmeyourhotkeys"
+  desc "Show applications menu items hotkeys"
+  homepage "https://showmeyourhotkeys.moxadventu.com/"
+
+  livecheck do
+    url "https://showmeyourhotkeys.moxadventu.com/update/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  depends_on macos: ">= :big_sur"
+
+  app "ShowMeYourHotKeys.app"
+end
