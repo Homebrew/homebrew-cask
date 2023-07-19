eab810cf2e35a1f02552b6a1951b0feac5280024
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-18 22:07:45 -0400

    camtasia 2023.1.1

--- Casks/camtasia.rb
+++ Casks/camtasia.rb
@@ -1,6 +1,6 @@
 cask "camtasia" do
-  version "2023.1.0"
-  sha256 "f1c7f6d5e21701b61db08b3d64fb81e3df969db37c283c84c021822c9f14ec07"
+  version "2023.1.1"
+  sha256 "a63e6dac2e6a5b41f1dc48378be078652642d52e598268b4799959ffd74f5891"
 
   url "https://download.techsmith.com/camtasiamac/releases/#{version.major[-2..]}#{version.minor_patch.no_dots}/Camtasia.dmg"
   name "Camtasia"
