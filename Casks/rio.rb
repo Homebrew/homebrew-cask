d218e7432837d053e400f889509c47682fe820b4
Raphael Amorim <rapha850@gmail.com>
2023-07-20 23:17:46 +0200

    rio 0.0.12

--- Casks/rio.rb
+++ Casks/rio.rb
@@ -1,6 +1,6 @@
 cask "rio" do
-  version "0.0.11"
-  sha256 "d03f548ac9a4fd8e53cf5d2cff1e107a312b8bd5e340216db359c29c9b2e4474"
+  version "0.0.12"
+  sha256 "c085bd25282221c5ec44d0346f65e1408d7d9b65d70aa3ad328c407bf569a3d4"
 
   url "https://github.com/raphamorim/rio/releases/download/v#{version}/Rio-v#{version}.dmg"
   name "Rio"
