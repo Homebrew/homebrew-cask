2e7798a1a5b8341882289faecab47525b9683a4b
Bevan Kay <email@bevankay.me>
2023-07-20 15:41:31 +1000

    Fix url

--- Casks/showmeyourhotkeys.rb
+++ Casks/showmeyourhotkeys.rb
@@ -2,7 +2,7 @@ cask "showmeyourhotkeys" do
   version "1.7,20"
   sha256 "4cca26821cd0d715f1b90ea34e886b2814361b932ac23a502b31e418a6d55c4e"
 
-  url "https://showmeyourhotkeys.moxadventu.com/update/ShowMeYourHotKeys-prod-stable-#{version.csv.first}-(#{version.csv.second).zip"
+  url "https://showmeyourhotkeys.moxadventu.com/update/ShowMeYourHotKeys-prod-stable-#{version.csv.first}-(#{version.csv.second}).zip"
   name "ShowMeYourHotKeys"
   desc "Show applications menu items hotkeys"
   homepage "https://showmeyourhotkeys.moxadventu.com/"
