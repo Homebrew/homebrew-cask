5727b56c36341c92ce965a52a0707a2f2e3a9ef3
fwcd <fwcdmail@gmail.com>
2023-07-20 16:57:43 +0200

    Add pulseview sha256

--- Casks/pulseview.rb
+++ Casks/pulseview.rb
@@ -1,5 +1,6 @@
 cask "pulseview" do
   version "0.4.2"
+  sha256 "5a0f61a590d0866f7696d23d2da801d12ee33c97b24dc71f669dfb1250f341c5"
 
   url "https://sigrok.org/download/binary/pulseview/PulseView-#{version}.dmg"
   name "PulseView"
