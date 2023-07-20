f623368877d15a5e5b5f78ead9ec428e1a0a8195
Franck Brun <franck.brun.luc@gmail.com>
2023-07-20 07:39:22 +0200

    Update Casks/showmeyourhotkeys.rb
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>

--- Casks/showmeyourhotkeys.rb
+++ Casks/showmeyourhotkeys.rb
@@ -3,7 +3,7 @@ cask "showmeyourhotkeys" do
   sha256 "4cca26821cd0d715f1b90ea34e886b2814361b932ac23a502b31e418a6d55c4e"
 
   url "https://showmeyourhotkeys.moxadventu.com/update/ShowMeYourHotKeys-prod-stable-#{version.csv.first}-(#{version.csv.second).zip"
-  name "showmeyourhotkeys"
+  name "ShowMeYourHotKeys"
   desc "Show applications menu items hotkeys"
   homepage "https://showmeyourhotkeys.moxadventu.com/"
 
