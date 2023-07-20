ead07e1f4e9f6e196727a542cb6c339a5efa41bb
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-20 09:43:18 -0400

    screens 4.12.10

--- Casks/screens.rb
+++ Casks/screens.rb
@@ -1,6 +1,6 @@
 cask "screens" do
-  version "4.12.8,1677528436"
-  sha256 "c5efda4c9d94a61f57118bb5e237fc795516c316f156bfefb624ed74fcb8dbd4"
+  version "4.12.10,1689789098"
+  sha256 "edf188f25384d7c39857fec9ce491aedf7176bd6b52ea5b41c5e433285cdfe8c"
 
   url "https://updates.edovia.com/com.edovia.screens#{version.major}.mac/Screens_#{version.csv.first}b#{version.csv.second}.zip"
   name "Screens"
