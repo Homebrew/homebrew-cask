7c7ae72f981c608b14a18e6329ead5ba34ddb64f
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-20 11:01:52 -0400

    lingon-x 9.3.3

--- Casks/lingon-x.rb
+++ Casks/lingon-x.rb
@@ -16,8 +16,8 @@ cask "lingon-x" do
     sha256 "c1c839e8dc13bd295f2080980c5bea22299c33f3333b7c6981161b46d6f021d8"
   end
   on_monterey :or_newer do
-    version "9.3.2"
-    sha256 "1abf1bfa6f5291f7b714857ba4f1455f2b431bc01eb6af4b6c776bbde605f117"
+    version "9.3.3"
+    sha256 "4878f575114247fdc29bb239dc8914da240867e7e4a0cb13d349dd021ca1df3b"
   end
 
   url "https://www.peterborgapps.com/downloads/LingonX#{version.major}.zip"
