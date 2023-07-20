2eb5035aefe6843957e27868261028ac6d1ef13c
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-20 09:43:44 -0400

    appflowy 0.2.7

--- Casks/appflowy.rb
+++ Casks/appflowy.rb
@@ -1,6 +1,6 @@
 cask "appflowy" do
-  version "0.2.6"
-  sha256 "17e0f58c67540f98570a27b3b85a7a66f482acc4ca7b0fba8a6124134538db5d"
+  version "0.2.7"
+  sha256 "d5b7079d53f549a823ef824961ad7456fc42c2534de554c3c5a531a799ee47e0"
 
   url "https://github.com/AppFlowy-IO/AppFlowy/releases/download/#{version}/Appflowy_#{version}_macos-x86_64.zip",
       verified: "github.com/AppFlowy-IO/AppFlowy/"
