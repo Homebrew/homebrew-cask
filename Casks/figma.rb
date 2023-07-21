3b369641e9c2f7440293ee7dccf483dc1f4ce8fd
Mikl Wolfe <wolfemikl@gmail.com>
2023-07-20 19:20:29 -0600

    figma 116.11.1

--- Casks/figma.rb
+++ Casks/figma.rb
@@ -1,9 +1,9 @@
 cask "figma" do
   arch arm: "mac-arm", intel: "mac"
 
-  version "116.10.9"
-  sha256 arm:   "72728ad0d49eec758a94a2b8eb1a50093fdf0c0bb1571e2fe983198ec6434f05",
-         intel: "7bb3cc0298c4b543f006ff920aac93abf61e8fcdf27ab97b1486a7207f37fa45"
+  version "116.11.1"
+  sha256 arm:   "97668dcdee05d910526f249bafeaf203e12bf82ef13e99566678b52b80ffbcf7",
+         intel: "0d8dcc6d2e9aa43ecb7cd9c0e051bded25251bd7e25e1e4a498e623a92de8f2a"
 
   url "https://desktop.figma.com/#{arch}/Figma-#{version}.zip"
   name "Figma"
