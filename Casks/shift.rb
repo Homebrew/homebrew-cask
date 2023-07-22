85b048d2a8cf3ddffde276c8a262633d478dc2a4
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-22 11:21:38 -0400

    shift 9.0.0.942

--- Casks/shift.rb
+++ Casks/shift.rb
@@ -1,9 +1,9 @@
 cask "shift" do
   arch arm: "arm64", intel: "x64"
 
-  version "8.5.11.920"
-  sha256 arm:   "22d5f0d3ee726d46a925e952c279d71d4cee16dc5931d5be4c71c2ba58ee7f54",
-         intel: "854755aab16de088245488440038f0217ec0c714ab6b8c39557e1ade1ed7fae9"
+  version "9.0.0.942"
+  sha256 arm:   "38ee184bad1c13882233b4aea56f50da223f7bc0156344e49f36fd7949ddd044",
+         intel: "1b02ca39219bd870780fdac7322bd740451c64d9acd65c211a3a47ac81217a59"
 
   url "https://updates.tryshift.com/v#{version.major_minor_patch}/stable/shift-v#{version}-stable-#{arch}.dmg"
   name "Shift"
