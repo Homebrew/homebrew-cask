78cfc04ecd00cfeb96f692321583e02f9184d283
Bevan Kay <email@bevankay.me>
2023-07-23 22:01:26 +1000

    Update lm-studio.rb

--- Casks/lm-studio.rb
+++ Casks/lm-studio.rb
@@ -2,24 +2,24 @@ cask "lm-studio" do
   version "0.1.8"
   sha256 "7006e37d08f019a7c1160436e6d55ee470637cd01aca1fecaa32b42073e6214a"
 
-  url "https://s3.amazonaws.com/releases.lmstudio.ai/#{version}/LM+Studio-#{version}-arm64.dmg",
+  url "https://s3.amazonaws.com/releases.lmstudio.ai/#{version}/LM+Studio-darwin-arm64-#{version}.zip",
       verified: "s3.amazonaws.com/releases.lmstudio.ai/"
-  name "lm-studio"
+  name "LM Studio"
   desc "Discover, download, and run local LLMs"
   homepage "https://lmstudio.ai/"
 
   livecheck do
-    url :homepage
-    strategy :page_match
-    regex(/LM\+Studio-(\d+(?:\.\d+)*?)-arm64\.dmg/i)
+    url "https://s3.amazonaws.com/releases.lmstudio.ai/update.json"
+    regex(/LM\+Studio[._-]darwin[._-]arm64[._-]v?(\d+(?:\.\d+)+)\.zip/i)
   end
+
+  depends_on arch: :arm64
 
   app "LM Studio.app"
 
   uninstall quit: [
     "ai.elementlabs.lmstudio",
     "ai.elementlabs.lmstudio.helper",
-    "com.electron.reactive",
   ]
 
   zap trash: [
