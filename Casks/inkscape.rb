1f09993c28751cc4a418696a80f271f6614bf53f
Bevan Kay <email@bevankay.me>
2023-07-24 22:32:49 +1000

    Update livecheck

--- Casks/inkscape.rb
+++ Casks/inkscape.rb
@@ -1,18 +1,18 @@
 cask "inkscape" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.3"
+  version "1.3.0"
   sha256 arm:   "e37b5f8b8995a0ecc41ca7fcae90d79bcd652b7a25d2f6e52c4e2e79aef7fec1",
          intel: "e97de6804d8811dd2f1bc45d709d87fb6fe45963aae710c24a4ed655ecd8eb8a"
 
-  url "https://media.inkscape.org/dl/resources/file/Inkscape-#{version}.0_#{arch}.dmg"
+  url "https://media.inkscape.org/dl/resources/file/Inkscape-#{version}_#{arch}.dmg"
   name "Inkscape"
   desc "Vector graphics editor"
   homepage "https://inkscape.org/"
 
   livecheck do
-    url "https://inkscape.org/release"
-    strategy :header_match
+    url "https://inkscape.org/release/all/mac-os-x/"
+    regex(/href=.*?Inkscape[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}\.dmg/i)
   end
 
   app "Inkscape.app"
