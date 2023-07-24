7e50b3aa5f57f0faa7928cb062b29b226fc327a9
singularitti <singularitti@outlook.com>
2023-07-23 22:25:54 -0400

    metaimage 2.5.7

--- Casks/metaimage.rb
+++ Casks/metaimage.rb
@@ -1,6 +1,6 @@
 cask "metaimage" do
-  version "2.5.6,236c5a19-7254-4655-88cb-0ab4dfc56704"
-  sha256 "22bccdc001532a45ca138f3b78d8146f4ecedd176634b72545dffd802d108aef"
+  version "2.5.7,4a9c8df7-9329-41b0-abb2-3c9a473de587"
+  sha256 "33739b848fc313f6bc0b8ff26b4b32ddf7d4465c891d900b04d1220fb5912fe7"
 
   url "https://neededapps.nyc3.digitaloceanspaces.com/media/public/#{version.csv.second}.zip",
       verified: "neededapps.nyc3.digitaloceanspaces.com/media/public/"
