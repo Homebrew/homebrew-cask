f95c88dbade99fc45876ef5ee63a6012e6d4cb03
Howard Rappaport <howie@rewardsmith.com>
2023-07-24 07:00:53 -0400

    cloudflare-warp 2023.7.159.0

--- Casks/cloudflare-warp.rb
+++ Casks/cloudflare-warp.rb
@@ -1,5 +1,5 @@
 cask "cloudflare-warp" do
-  version "2023.7.8.0,20230707.17"
+  version "2023.7.159.0"
   sha256 :no_check
 
   url "https://1111-releases.cloudflareclient.com/mac/Cloudflare_WARP.zip",
