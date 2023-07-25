144360ffb666ca7ba78087814ea740d838aef1ac
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-25 07:51:50 -0400

    chatall 1.37.55

--- Casks/chatall.rb
+++ Casks/chatall.rb
@@ -1,9 +1,9 @@
 cask "chatall" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.37.54"
-  sha256 arm:   "1b1d72032346b4378e263e259ca0b24a9b93a86dfe1e42b4278e9b30c1e42de1",
-         intel: "125a8f067232ea55743bcf48f863bb0e0477f9c5d9f1913d7e7c910b772e7819"
+  version "1.37.55"
+  sha256 arm:   "585851cc3881ac3fd0a78dabb4b59487414c1c350f4e5c61fae639f66bf281a8",
+         intel: "49d96bc6ec2e665bf14841d222d076d74e6a048426d7935e78459a25842ed890"
 
   url "https://github.com/sunner/ChatALL/releases/download/v#{version}/ChatALL-#{version}-mac-#{arch}.dmg"
   name "ChatALL"
