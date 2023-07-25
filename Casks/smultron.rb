95e313905e3c16e3732f24d8ed5951be1ff81937
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-25 08:27:39 -0400

    smultron 13.3.8

--- Casks/smultron.rb
+++ Casks/smultron.rb
@@ -1,5 +1,5 @@
 cask "smultron" do
-  version "13.3.7,13370"
+  version "13.3.8,13380"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.peterborgapps.com/downloads/Smultron#{version.major}.zip"
