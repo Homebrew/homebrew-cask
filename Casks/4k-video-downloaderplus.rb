edade0fed419c6acf6e68c9e7dc69fbcb866a61f
Miccal Matthews <miccal.matthews@gmail.com>
2023-07-25 09:00:30 +0800

    4k-video-downloaderplus 1.0.1

---
+++ Casks/4k-video-downloaderplus.rb
@@ -0,0 +1,24 @@
+cask "4k-video-downloaderplus" do
+  version "1.0.1"
+  sha256 "1cfef986b588cc856bac7930f91b6ea425313d9cf66c6b0a75f47a2f73ac609d"
+
+  url "https://dl.4kdownload.com/app/4kvideodownloaderplus_#{version}.dmg"
+  name "4K Video Downloader Plus"
+  desc "Free video downloader"
+  homepage "https://www.4kdownload.com/products/product-videodownloader"
+
+  livecheck do
+    url "https://www.4kdownload.com/download"
+    regex(%r{href=.*?/4kvideodownloaderplus[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
+  end
+
+  depends_on macos: ">= :high_sierra"
+
+  app "4K Video Downloader+.app"
+
+  zap trash: [
+    "~/Library/Application Support/4kdownload.com/4K Video Downloader",
+    "~/Library/Preferences/com.4kdownload.4K Video Downloader.plist",
+    "~/Library/Preferences/com.4kdownload.ApplicationDirectories.plist",
+  ]
+end
