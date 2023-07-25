3d3ae8113925538974806c37dcc7f3ae01bdfd27
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-07-25 06:42:34 -0400

    wondershare-uniconverter 15.0.0

--- Casks/wondershare-uniconverter.rb
+++ Casks/wondershare-uniconverter.rb
@@ -1,9 +1,9 @@
 cask "wondershare-uniconverter" do
   arch arm: "arm_"
 
-  version "14.2.17"
-  sha256 arm:   "06f9622316a1e18b00635a354d2bf8bc50370985795cf7ecb877d5669c4232ed",
-         intel: "62a18d09a30e027daa2e4f4feca9cb5b1097d2f3df82bdfab7a37a58ffaae298"
+  version "15.0.0"
+  sha256 arm:   "0a342f5e0ffceec203da5e7639a6eb9a75d66881d626408dd92738ada7d3341d",
+         intel: "a5d0322ac8be2b3d0be67a1c3ae3dabf2a3872d19ac9f355473be02bb726fbce"
 
   url "https://download.wondershare.com/cbs_down/uniconverter#{version.major}-mac_#{arch}#{version}_full14207.zip"
   name "UniConverter"
