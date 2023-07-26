9506af1025fb6639c8b847895671d970f31bd4bc
世界 <i@sekai.icu>
2023-07-26 11:19:16 +0800

    Add sing-box

---
+++ Casks/sing-box.rb
@@ -0,0 +1,20 @@
+cask "sing-box" do
+  version "1.3.4"
+  sha256 "751456e146155f959568403ffd186524e6770e5e19f0780fc97ed664bcfc7122"
+
+  url "https://github.com/SagerNet/sing-box/releases/download/v#{version}/SFM-#{version}-universal.zip",
+      verified: "github.com/SagerNet/sing-box/"
+  name "SFM"
+  desc "Standalone client for sing-box, the universal proxy platform"
+  homepage "https://sing-box.sagernet.org/"
+
+  depends_on macos: ">= :ventura"
+
+  app "SFM.app"
+
+  uninstall quit: "io.nekohasekai.sfa.independent", login_item: "SFM"
+
+  zap trash: [
+    "~/Library/Group Containers/group.io.nekohasekai.sfa",
+  ]
+end
