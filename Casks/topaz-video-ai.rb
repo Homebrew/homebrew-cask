2c7da26d731e919eb1df105ec9591df15c7b4a85
Cody Schrank <cody.schrank@gmail.com>
2023-07-25 21:38:16 -0700

    topaz-video-ai: 3.3.6 - update livecheck url

--- Casks/topaz-video-ai.rb
+++ Casks/topaz-video-ai.rb
@@ -1,6 +1,6 @@
 cask "topaz-video-ai" do
-  version "3.1.5"
-  sha256 "aa70b1cc890528463e993bcc3fefdc6a0b611bc732b0e8c3893c0929b3e832a2"
+  version "3.3.6"
+  sha256 "6f5dac4319c3e6b797e425b0906325f08a44f176f8bc7669d68a2c8a8d0c333a"
 
   url "https://downloads.topazlabs.com/deploy/TopazVideoAI/#{version}/TopazVideoAI-#{version}.dmg"
   name "Topaz Video AI"
@@ -8,7 +8,7 @@ cask "topaz-video-ai" do
   homepage "https://www.topazlabs.com/topaz-video-ai"
 
   livecheck do
-    url "https://topazlabs.com/d/veai/latest/mac/full"
+    url "https://topazlabs.com/d/tvai/latest/mac/full"
     strategy :header_match
   end
 
