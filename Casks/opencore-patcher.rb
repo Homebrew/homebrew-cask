ebd126a6170d12b80dd1d8393e098d483e7f316a
Rayenimo <michal.hudek@gmail.com>
2023-07-26 21:03:46 +0200

    opencore-patcher 0.6.8

--- Casks/opencore-patcher.rb
+++ Casks/opencore-patcher.rb
@@ -1,6 +1,6 @@
 cask "opencore-patcher" do
-  version "0.6.7"
-  sha256 "2b69b86ea8698491d279b4cd5e12f77b8b41166db3f8d522bb8449e03c818247"
+  version "0.6.8"
+  sha256 "996cbf77d08a5ac6a13403ada89858ce9f5184a99e94788178e321d8fd49caab"
 
   url "https://github.com/dortania/OpenCore-Legacy-Patcher/releases/download/#{version}/OpenCore-Patcher-GUI.app.zip",
       verified: "github.com/dortania/OpenCore-Legacy-Patcher/"
