76129b1d054c17f7103c73caa8c5f6f5dac1404a
Marcocanc <marco.cancellieri@traderepublic.com>
2023-07-27 14:33:30 +0200

    Add Cilicon.app v2.1.0

---
+++ Casks/cilicon.rb
@@ -0,0 +1,24 @@
+cask "cilicon" do
+  version "2.1.0"
+  sha256 "af39cff356afb5c22f006349a72f0bf1db02ad55bb2401eeb6bd360308fd9c01"
+
+  url "https://github.com/traderepublic/Cilicon/releases/download/v#{version}/Cilicon.zip"
+  name "cilicon"
+  desc "Self-Hosted ephemeral CI on Apple Silicon"
+  homepage "https://github.com/traderepublic/Cilicon"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: :ventura
+  depends_on arch: :arm64
+
+  app "Cilicon.app"
+
+  zap trash: [
+    "~/cilicon.yml",
+    "~/.cilicon.yml",
+  ]
+end
