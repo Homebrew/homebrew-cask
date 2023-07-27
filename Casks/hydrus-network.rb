5c9d5951cdc86e68f310b525f36c65e43f040621
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-26 21:31:53 -0400

    hydrus-network 536

--- Casks/hydrus-network.rb
+++ Casks/hydrus-network.rb
@@ -1,6 +1,6 @@
 cask "hydrus-network" do
-  version "535"
-  sha256 "278846176f1285bbc211b07455151383b8c381f1614fe0c52cff714b6fb4ad22"
+  version "536"
+  sha256 "51047a35cf9b8a8661bc5b798ec47bd9a6d52ad02a283ae788a125940f42a681"
 
   url "https://github.com/hydrusnetwork/hydrus/releases/download/v#{version}/Hydrus.Network.#{version}.-.macOS.-.App.dmg",
       verified: "github.com/hydrusnetwork/hydrus/"
