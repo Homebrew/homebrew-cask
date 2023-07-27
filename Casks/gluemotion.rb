a057b9a0fa215adb386c3db7a97352652bf256ee
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-27 19:15:20 -0400

    gluemotion 2.1.3

--- Casks/gluemotion.rb
+++ Casks/gluemotion.rb
@@ -1,6 +1,6 @@
 cask "gluemotion" do
-  version "2.1.2,be3b54ca-775b-479f-9aac-ea69ad9c5ffa"
-  sha256 "6c99cfe87ec87f880e3fcb12bee7366c40ca0cc2865fba49024dcefdd8deefc8"
+  version "2.1.3,ce9761bb-80d7-4b04-a4d4-78a7b9b0eea2"
+  sha256 "a9030faae46db077e28984012adb2773ff58401b21e849de1053b9444f1a0240"
 
   url "https://neededapps.nyc3.digitaloceanspaces.com/media/public/#{version.csv.second}.zip",
       verified: "neededapps.nyc3.digitaloceanspaces.com/media/public/"
