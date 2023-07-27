cd410e607aefe7fb3e50e65d993283d84af109b0
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-26 21:31:02 -0400

    electron 25.3.2

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,9 +1,9 @@
 cask "electron" do
   arch arm: "arm64", intel: "x64"
 
-  version "25.3.1"
-  sha256 arm:   "1cc5e9b6451757ada1c07130b9454164d4206cf92595708fb6fc9ebff030f860",
-         intel: "66c86c8651c4699b069fde53d5fcbf8887a2573c27e3eefc655462b27c047d07"
+  version "25.3.2"
+  sha256 arm:   "14dcf303e2e9c4a50d50df36e0a44dcb29d44d153a9536752c1fcb99e384a686",
+         intel: "dbf7718fffca0093041968b9e5797c37a4f691d192394616a5905db7a481abe3"
 
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-#{arch}.zip",
       verified: "github.com/electron/electron/"
