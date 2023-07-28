59aa2107fd0ec1fbcc861efe0496613521851c3e
Benedikt Werner <1benediktwerner@gmail.com>
2023-07-29 01:09:43 +0200

    ilspy: add arm version

--- Casks/ilspy.rb
+++ Casks/ilspy.rb
@@ -1,8 +1,11 @@
 cask "ilspy" do
-  version "7.2-rc"
-  sha256 "480f0fe2b5dc6ad4d0df3d4c9ecd44429b97e0c8b5bac8bc99c2ac2a8e3025ff"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://github.com/icsharpcode/AvaloniaILSpy/releases/download/v#{version}/macOS.x64.Release.zip"
+  version "7.2-rc"
+  sha256 arm:   "613ac0dc004dfef71fc805178694623a0c0f8ce7807118947b9208494f23fed1",
+         intel: "480f0fe2b5dc6ad4d0df3d4c9ecd44429b97e0c8b5bac8bc99c2ac2a8e3025ff"
+
+  url "https://github.com/icsharpcode/AvaloniaILSpy/releases/download/v#{version}/macOS.#{arch}.Release.zip"
   name "ILSpy"
   desc "Avalonia-based .NET decompiler"
   homepage "https://github.com/icsharpcode/AvaloniaILSpy"
@@ -14,7 +17,7 @@ cask "ilspy" do
     url :url
   end
 
-  app "artifacts/osx-x64/ILSpy.app"
+  app "artifacts/osx-#{arch}/ILSpy.app"
 
   zap trash: [
     "~/Library/Preferences/ilspy.plist",
