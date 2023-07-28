827db77f98f4b41b7ea03e090ef348f38d4e47d7
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-07-28 10:32:29 -0400

    openmsx 19_0

--- Casks/openmsx.rb
+++ Casks/openmsx.rb
@@ -1,6 +1,6 @@
 cask "openmsx" do
-  version "18_0"
-  sha256 "51e0887a8515fa50a880485a5b343783cf8928ea3d7909733956b90df4b0ded4"
+  version "19_0"
+  sha256 "f247b515a0dd9864906044fac564d04cbcf6345167cd0275117bbf859fbd36ab"
 
   url "https://github.com/openMSX/openMSX/releases/download/RELEASE_#{version}/openmsx-#{version.underscores_to_dots}-mac-x86_64-bin.dmg",
       verified: "github.com/openMSX/openMSX/"
