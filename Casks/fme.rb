2f883a3365d5a56c4a27efda997cdf6759658625
Pat Lougheed <pat@plaid.is>
2023-07-28 20:06:23 -0700

    Upgrade FME to 2023.0
    - Bump version
    - Reflect name change from FME Desktop to FME Form where necessary
    - Add Intel and Mac Silicon versions along with appropriate URLs

--- Casks/fme.rb
+++ Casks/fme.rb
@@ -1,27 +1,37 @@
 cask "fme" do
-  version "2022.2.6,22800"
-  sha256 "f28c0235423afa9d19c308c8d35cc0908e0ebf7579c3e566f0b2ac245df33310"
+  arch arm:   "aarch64",
+       intel: "x64"
 
-  url "https://downloads.safe.com/fme/#{version.major}/fme-desktop-#{version.csv.first}-b#{version.csv.second}-macosx-x64.pkg"
-  name "FME Desktop"
+  version "2023.0.1,23332"
+  sha256 arm:   "6bcb39d4287f751984ede9845dad3c33f3a5c7c8161e966b16ae8eea3a7da0f8",
+         intel: "f9bac5931173696d27a65db3e311ecf362444089b198f313fa4536771615c7ee"
+
+  on_arm do
+    url "https://downloads.safe.com/fme/#{version.major}/macos-#{arch}/fme-form-#{version.csv.first}-b#{version.csv.second}-macosx-#{arch}.pkg"
+  end
+  on_intel do
+    url "https://downloads.safe.com/fme/#{version.major}/macos/fme-form-#{version.csv.first}-b#{version.csv.second}-macosx-#{arch}.pkg"
+  end
+
+  name "FME Form"
   desc "Platform for integrating spatial data"
   homepage "https://www.safe.com/"
 
   livecheck do
     url "https://engage.safe.com/support/downloads/"
     strategy :page_match do |page|
-      match = page.match(/fme-desktop-(\d+(?:\.\d+)+)-b(\d+)-macosx-x64\.pkg/i)
+      match = page.match(/fme-form-(\d+(?:\.\d+)+)-b(\d+)-macosx-#{arch}\.pkg/i)
       next if match.blank?
 
       "#{match[1]},#{match[2]}"
     end
   end
 
-  pkg "fme-desktop-#{version.csv.first}-b#{version.csv.second}-macosx-x64.pkg"
+  pkg "fme-form-#{version.csv.first}-b#{version.csv.second}-macosx-#{arch}.pkg"
 
   uninstall pkgutil: [
-              "com.safesoftware.pkg.engine.fme-desktop-#{version.major_minor}-b#{version.csv.second}-macosx-x64",
-              "com.safesoftware.pkg.apps.fme-desktop-#{version.major_minor}-b#{version.csv.second}-macosx-x64",
+              "com.safesoftware.pkg.engine.fme-form-#{version.major_minor}-b#{version.csv.second}-macosx-#{arch}",
+              "com.safesoftware.pkg.apps.fme-form-#{version.major_minor}-b#{version.csv.second}-macosx-#{arch}",
             ],
             delete:  [
               "/Applications/FME #{version.major_minor}",
