25f119244a14577815ed5782e7c41373cf21491f
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-07-31 15:53:54 -0400

    dictionaries 2.1

--- Casks/dictionaries.rb
+++ Casks/dictionaries.rb
@@ -1,6 +1,6 @@
 cask "dictionaries" do
-  version "2.0"
-  sha256 "bdddac7d12b9a30a1bea5d7150a2fa41b83461fbfcc8d3b6ae2fb99d4fe2b20e"
+  version "2.1"
+  sha256 "34248976ae7409e4334a31553cffd6ada4bebd345bcb289d6e6a4452b9e5d3b6"
 
   url "https://download.dictionaries.io/mac/Dictionaries-#{version}.zip"
   name "Dictionaries"
