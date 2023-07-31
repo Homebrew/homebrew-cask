0aecbcb0a73516e98fcf096edb22050413921f6f
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-07-30 22:56:55 -0400

    cask updates

--- Casks/zspace.rb
+++ Casks/zspace.rb
@@ -1,22 +1,15 @@
 cask "zspace" do
-  arch arm: "MACARM", intel: "MAC"
+  arch arm: "ARM"
 
-  on_arm do
-    version "2.4.2023071305"
-    sha256 "96d7abedabc469962c8c51fbc560121b53fccfb0a0b34bbab9cd1a225a51bc86"
+  version "2.4.2023071305"
+  sha256 arm:   "96d7abedabc469962c8c51fbc560121b53fccfb0a0b34bbab9cd1a225a51bc86",
+         intel: "d58c7a5fbe52afe40431fd389b40714a994282307b0a39fe5580b425f1c5b615"
 
-    url "https://update.zenithspace.net/app/APP_ZSPACE_DESKTOP_#{arch}/V#{version}/zspace/1689577756/zspace_V#{version}.dmg", verified: "update.zenithspace.net/app/"
-  end
-  on_intel do
-    version "2.4.2023071305"
-    sha256 "d58c7a5fbe52afe40431fd389b40714a994282307b0a39fe5580b425f1c5b615"
-
-    url "https://update.zenithspace.net/app/APP_ZSPACE_DESKTOP_#{arch}/V#{version}/zspace/1689566844/zspace_V#{version}.dmg", verified: "update.zenithspace.net/app/"
-  end
-
+  url "https://update.zenithspace.net/app/APP_ZSPACE_DESKTOP_MAC#{arch}/V#{version}/zspace/1689577756/zspace_V#{version}.dmg",
+      verified: "update.zenithspace.net/app/"
   name "zspace"
   desc "极空间苹果MAC客户端"
-  homepage "https://www.zspace.cn/"
+  homepage "https://www.zspace.cn/net"
 
   livecheck do
     skip "No version information available"
