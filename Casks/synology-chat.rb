ce6784357af5f4374d493eca4d39d86816be607f
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-31 10:18:18 -0400

    synology-chat 1.2.2-0222

--- Casks/synology-chat.rb
+++ Casks/synology-chat.rb
@@ -1,6 +1,6 @@
 cask "synology-chat" do
-  version "1.2.1-0207"
-  sha256 "782cb4c67d5beb3d2157566ab066ca69ad877c343d5ba9892303a7930cc4383e"
+  version "1.2.2-0222"
+  sha256 "9c3ad4a3e11f6646f9c91317ef9bfa4e4352ef68d025c5ad782771250e2f1015"
 
   url "https://global.download.synology.com/download/Utility/ChatClient/#{version}/Mac/Installer/Synology%20Chat%20Client-#{version}.dmg"
   name "Synology Chat"
