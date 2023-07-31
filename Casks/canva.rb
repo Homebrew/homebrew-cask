628c2d3238dfec6436504d6b4e058a2dcd17c554
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-31 05:06:47 +0000

    canva 1.71.0

--- Casks/canva.rb
+++ Casks/canva.rb
@@ -1,6 +1,6 @@
 cask "canva" do
-  version "1.70.0"
-  sha256 "86e0ee46499d93336768a403859849cb6320349f5aa06a439d3abef4fa89d6be"
+  version "1.71.0"
+  sha256 "3fd814c58bb5d230cb9d6f465c8dd976a3f16ed02bc8ca304f09c0c74c3a4879"
 
   url "https://desktop-release.canva-deploy.com/Canva-#{version}-universal.dmg",
       verified: "desktop-release.canva-deploy.com/"
