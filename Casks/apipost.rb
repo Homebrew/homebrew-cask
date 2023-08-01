2afbcfe8d9a5b25026c527ae4539fa75f948221e
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-07-31 21:30:54 -0400

    apipost 7.1.9

--- Casks/apipost.rb
+++ Casks/apipost.rb
@@ -1,9 +1,9 @@
 cask "apipost" do
   arch arm: "arm64", intel: "x64"
 
-  version "7.1.8"
-  sha256 arm:   "cf8c0f117d61e69f5c6eb9e787974f0d04497d408e2b360c33291957668fb4bf",
-         intel: "1ced6ae6ecd39f993dba16afa2dbeabaa768a3786dabc6f2a56278f28052a763"
+  version "7.1.9"
+  sha256 arm:   "2563c656854d561a2fc7b9f27a3d036285d85b4e87594ffbe8c5d8dfc7c09ba4",
+         intel: "3eab9c11adfa90e8a796be6f038da0267641b7ad6033f3a768ac4f80c73a72af"
 
   url "https://www.apipost.cn/dl.php?client=Mac&arch=#{arch}&version=#{version}"
   name "Apipost"
