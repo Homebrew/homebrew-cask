3206b9c82d78c9c2ffaeb072d5638a982811eba3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-01 05:09:02 +0000

    purei-play 2023-07-31

--- Casks/purei-play.rb
+++ Casks/purei-play.rb
@@ -1,6 +1,6 @@
 cask "purei-play" do
-  version "2023-07-26,0433e963"
-  sha256 "aae6ebfcc4efbe1c36620de38ee91b8105882db43f283e92dc3602e45b1d4c41"
+  version "2023-07-31,97d971cb"
+  sha256 "d9e0b3ddb8a8df1f18d3e96e48b0518d39f1bb4f22ee685ed5e5edf743ea2cd3"
 
   url "https://s3.us-east-2.amazonaws.com/playbuilds/#{version.csv.second}/Play.dmg",
       verified: "s3.us-east-2.amazonaws.com/playbuilds/"
