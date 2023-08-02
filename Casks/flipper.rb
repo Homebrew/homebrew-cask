740b0bb47e0fbba529af4e19bd64249eab231101
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-02 19:39:36 -0400

    flipper 0.210.0

--- Casks/flipper.rb
+++ Casks/flipper.rb
@@ -1,6 +1,6 @@
 cask "flipper" do
-  version "0.209.0"
-  sha256 "7b31910d1404ea2171569df079af478e1524c5e919f99e4a58288e0331db4e00"
+  version "0.210.0"
+  sha256 "77f1ce0d1885287561f4b534b030de3cd5d0f3fc2b8d882a75c1dc6acb3d7a2b"
 
   url "https://github.com/facebook/flipper/releases/download/v#{version}/Flipper-mac.dmg",
       verified: "github.com/facebook/flipper/"
