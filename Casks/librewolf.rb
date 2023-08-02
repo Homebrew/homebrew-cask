dcec79576217fc8d324fbcc80cc8da42cfb9a539
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-02 09:56:28 -0400

    librewolf 116.0

--- Casks/librewolf.rb
+++ Casks/librewolf.rb
@@ -2,12 +2,12 @@ cask "librewolf" do
   arch arm: "arm64", intel: "x86_64"
 
   on_arm do
-    version "115.0.2,2"
-    sha256 "2c90600f1bf92085acf5b4c35d8e82bc314300d7a179ff496251de50784bd629"
+    version "116.0,1"
+    sha256 "0f2c39f7171e2a0d8e2c1d6d622cc65d93066c53877465eeccebed05534387de"
   end
   on_intel do
-    version "115.0.2,2"
-    sha256 "71d14d4123a8768a694cb778b595c54d504446474df415d7c3507dd9b0d0c024"
+    version "116.0,1"
+    sha256 "7bb3c7dd8c87b3eaef2208fb92f32facb81ac048178c4f23a3165d2a165b8e50"
   end
 
   url "https://gitlab.com/api/v4/projects/44042130/packages/generic/librewolf/#{version.csv.first}-#{version.csv.second}/librewolf-#{version.csv.first}-#{version.csv.second}-macos-#{arch}-package.dmg",
