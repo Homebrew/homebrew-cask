14f4e0f5b8e7a3486e4f494375f2767cb03f510c
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-02 19:44:03 -0400

    logos 27.1.0.0020

--- Casks/logos.rb
+++ Casks/logos.rb
@@ -1,9 +1,9 @@
 cask "logos" do
   arch arm: "-arm"
 
-  version "27.0.0.0014"
-  sha256 arm:   "2fd8d69c11377f3216c25bc15c79d806a414948a99d92fb0c60f8f564a8f8135",
-         intel: "de144b55b320b70b2784f80216418003bf2b44c4c209c6257ddfd02ef5a44c4b"
+  version "27.1.0.0020"
+  sha256 arm:   "e531d453237a887d1e98e5965f41b72d82f719a20759b4b4a7a3a65f7812aa43",
+         intel: "8ea3eed4e6ee0d593ebeed524f1d5d0a27f55b9379f2f30dbc13dd3ad414f4dd"
 
   url "https://downloads.logoscdn.com/LBS10/Installer/#{version}/LogosMac#{arch}.dmg",
       verified: "downloads.logoscdn.com/"
