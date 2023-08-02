5f207024a2f2af9327dc3c9b07b93acb85f598b8
Anze <dkanze@gmail.com>
2023-08-02 21:26:20 +0200

    new version include livecheck update zap

--- Casks/keyclu.rb
+++ Casks/keyclu.rb
@@ -1,6 +1,6 @@
 cask "keyclu" do
-  version "0.8.1"
-  sha256 "2c83da08083872fec15adca19a5ddf3953873475a66d8118361a93a9079c098b"
+  version "0.9"
+  sha256 "e4d0b872a3c88c258d23ccc2fc6d25987d43d76b2d0556ae2c57e93fa5101167"
 
   url "https://github.com/Anze/KeyCluCask/releases/download/v#{version}/KeyClu.zip",
       verified: "github.com/Anze/KeyCluCask/"
@@ -12,5 +12,17 @@ cask "keyclu" do
 
   app "KeyClu.app"
 
-  zap trash: "~/Library/Containers/com.0804Team.KeyClu"
+  livecheck do
+    url "https://sergii.tatarenkov.name/keyclu/appcast.xml"
+    strategy :sparkle
+  end
+
+  auto_updates true
+
+  uninstall quit: "com.0804Team.KeyClu"
+
+  zap trash: [
+    "~/Library/Containers/com.0804Team.KeyClu",
+    "~/Library/Group Containers/group.com.0804Team.KeyClu"
+  ]
 end
