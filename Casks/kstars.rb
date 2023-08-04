3816babf189be2e1b470a73096841e7a9ba9d99a
hyuraku <32809703+hyuraku@users.noreply.github.com>
2023-08-04 21:42:59 +0900

    kstars 3.6.6

--- Casks/kstars.rb
+++ Casks/kstars.rb
@@ -1,6 +1,6 @@
 cask "kstars" do
-  version "3.6.5"
-  sha256 "b72163c0b4af69257bede297e3b601d9a7591f82966f74bcc950aa133d2c2c0b"
+  version "3.6.6"
+  sha256 "9a2da9c8f9eb6da847ccfaa7ca1270d15a7ab14b1becf805f98600811d928a2c"
 
   url "https://www.indilib.org/jdownloads/kstars/kstars-#{version}.dmg",
       verified: "indilib.org/jdownloads/kstars/"
