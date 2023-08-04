15314421ac68775842f3966bf44e10e4c38dc9dc
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-03 20:59:20 -0400

    unity-ios-support-for-editor 2023.1.7f1

--- Casks/unity-ios-support-for-editor.rb
+++ Casks/unity-ios-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask "unity-ios-support-for-editor" do
-  version "2023.1.6f1,964b2488c462"
-  sha256 "5c5edb91d6fd500c9a1fa70192f7036a468aac194ed6991e7eca114281a474da"
+  version "2023.1.7f1,4f4e359ec3fc"
+  sha256 "0e9f1a6fc479e8faae9a00ff30a37be3f4e9279830460bd687cbac2b13649868"
 
   url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-#{version.csv.first}.pkg",
       verified: "download.unity3d.com/download_unity/"
