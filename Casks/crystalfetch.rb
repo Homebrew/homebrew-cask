9c968d9b571a7a0d02c5f37223b60991820fc7ed
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-08-04 10:36:55 -0400

    add zap and livecheck

--- Casks/crystalfetch.rb
+++ Casks/crystalfetch.rb
@@ -3,9 +3,19 @@ cask "crystalfetch" do
   sha256 "24091b4395267fda58331185ec4b31a6c491ff75b40dfa26074ccc4c8e409f83"
 
   url "https://github.com/TuringSoftware/CrystalFetch/releases/download/v#{version}/CrystalFetch.dmg"
-  name "crystalfetch"
+  name "Crystalfetch"
   desc "UI for creating Windows installer ISO from UUPDump"
   homepage "https://github.com/TuringSoftware/CrystalFetch"
 
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
   app "CrystalFetch.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/llc.turing.CrystalFetch",
+    "~/Library/Containers/llc.turing.CrystalFetch",
+  ]
 end
