1362b0cc1c918fe2b68a0eb2cb4fb00bb3d6d033
Alexey Tarutin <alexeytarutin@gmail.com>
2023-08-06 05:21:29 +0700

    Update Hovrly.app v2.4.5

--- Casks/hovrly.rb
+++ Casks/hovrly.rb
@@ -2,12 +2,17 @@ cask "hovrly" do
   version "2.4.5"
   sha256 "2cd4d3fc696a7a59446fea3c0a1d9f77b23cfed8bcd53c6992ffb5dc78711b88"
 
-  url "https://github.com/tarutin/hovrly/releases/download/v#{version}/Hovrly-#{version}-universal.dmg", verified: "github.com/tarutin/hovrly/"
+  url "https://github.com/tarutin/hovrly/releases/download/v#{version}/Hovrly-#{version}-universal.dmg",
+    verified: "github.com/tarutin/hovrly/"
   name "Hovrly"
-  desc "Best partner for disctributed teams"
+  desc "Hovrly is the best partner for distributed work teams, display and convert timezones time in different cities"
   homepage "https://hovrly.com/"
 
   app "Hovrly.app"
 
-  # No zap stanza required
+  zap trash: [
+    "~/Library/Application Support/Hovrly",
+    "~/Library/Preferences/com.treasy.hovrly.plist",
+    "~/Library/Saved Application State/com.treasy.hovrly.savedState"
+  ]
 end
