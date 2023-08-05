faa3ec7bf97f885b3a7dc6195e7c46fb10ca3080
Alexey Tarutin <alexeytarutin@gmail.com>
2023-08-06 05:42:19 +0700

    Update hovrly.rb

--- Casks/hovrly.rb
+++ Casks/hovrly.rb
@@ -3,7 +3,7 @@ cask "hovrly" do
   sha256 "2cd4d3fc696a7a59446fea3c0a1d9f77b23cfed8bcd53c6992ffb5dc78711b88"
 
   url "https://github.com/tarutin/hovrly/releases/download/v#{version}/Hovrly-#{version}-universal.dmg",
-    verified: "github.com/tarutin/hovrly/"
+      verified: "github.com/tarutin/hovrly/"
   name "Hovrly"
   desc "Display and convert timezones time in different cities"
   homepage "https://hovrly.com/"
