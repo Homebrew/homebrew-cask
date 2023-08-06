553190d4d8d68bded09658885520987d457c196a
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-06 10:45:22 -0400

    bloop 0.4.11

--- Casks/bloop.rb
+++ Casks/bloop.rb
@@ -1,9 +1,9 @@
 cask "bloop" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.4.10"
-  sha256 arm:   "bda1f92ba292f155c90387c01138cf4664b5f1d4be6763322198253c1b310324",
-         intel: "8eadf51bb3cc4b15de37ad20cf07742596d97aba646c8da7561c9d9a71ed37cd"
+  version "0.4.11"
+  sha256 arm:   "ce2086f3ae3b01d773af513ebb110470b15a56e75bce82a700d3fde4bd81c6bb",
+         intel: "4b451bf82e5b4734a64137c2a400135607c45aae92c9c310166ef400a3b3c462"
 
   url "https://github.com/BloopAI/bloop/releases/download/v#{version}/bloop_#{version}_#{arch}.dmg",
       verified: "github.com/BloopAI/bloop/"
