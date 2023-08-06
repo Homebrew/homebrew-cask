10612785315073793efc84d182ba82203accec6b
Miccal Matthews <miccal.matthews@gmail.com>
2023-08-06 18:37:56 +0800

    Update goldencheetah.rb

--- Casks/goldencheetah.rb
+++ Casks/goldencheetah.rb
@@ -2,7 +2,7 @@ cask "goldencheetah" do
   version "3.6"
   sha256 "3cc4540d0490c5b1026c5f523cea34dede17c63d4eb582403d710ccb2cba1156"
 
-  url "https://github.com/GoldenCheetah/GoldenCheetah/releases/download/V#{version}/GoldenCheetah_v#{version}_x64.dmg",
+  url "https://github.com/GoldenCheetah/GoldenCheetah/releases/download/v#{version}/GoldenCheetah_v#{version}_x64.dmg",
       verified: "github.com/GoldenCheetah/GoldenCheetah/"
   name "GoldenCheetah"
   desc "Performance software for cyclists, runners and triathletes"
