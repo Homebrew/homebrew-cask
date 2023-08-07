5c44f605439546b8990625e70ee2f390918a930f
Bevan Kay <email@bevankay.me>
2023-08-07 10:15:06 +1000

    zulu 20.0.2,20.32.11_1-ca

--- Casks/zulu.rb
+++ Casks/zulu.rb
@@ -2,9 +2,9 @@ cask "zulu" do
   arch arm: "aarch64", intel: "x64"
   choice = on_arch_conditional arm: "arm", intel: "x86"
 
-  version "20.0.2,20.32.11-ca"
-  sha256 arm:   "6db524f701c3a64ade65e83d21d34a832e39a8342650d4c1515f0a50cdc63ef0",
-         intel: "78dc577888711f954d690eb5d41c5408dd02a45ae25782d36efc170df02a0ee9"
+  version "20.0.2,20.32.11_1-ca"
+  sha256 arm:   "c5c196a0a25eca25a22edee325ff8ee2cd96aa9bfdee0ca3554dfbfd31dba8df",
+         intel: "6f02981801832230edd0a0091b8de10873d3271cd7cc4d7a9ed439b046fa4b1f"
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}.dmg",
       referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
@@ -15,7 +15,7 @@ cask "zulu" do
   livecheck do
     url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&bundle_type=jdk&javafx=false&ext=dmg&os=macos&arch=#{choice}"
     strategy :page_match do |page|
-      match = page.match(/zulu(\d+(?:\.\d+)*-.*?)-jdk(\d+(?:\.\d+)*)-macosx_#{arch}\.dmg/i)
+      match = page.match(/zulu(\d+(?:[._]\d+)*-.*?)-jdk(\d+(?:\.\d+)*)-macosx_#{arch}\.dmg/i)
       next if match.blank?
 
       "#{match[2]},#{match[1]}"
