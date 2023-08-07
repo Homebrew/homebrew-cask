5df9b04f1ca274e276c39dfd45da8db35b26a773
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-07 19:19:44 -0400

    cron 1.114.0

--- Casks/cron.rb
+++ Casks/cron.rb
@@ -1,9 +1,9 @@
 cask "cron" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.113.0"
-  sha256 arm:   "fceb1825d5bc0a734b6f46371ed730050241bac0050f36d5fb7092b5093a8775",
-         intel: "46a93ec8c570a360ef379ead2ac1b7c212577c2e104d48ddfd8c425438c3a751"
+  version "1.114.0"
+  sha256 arm:   "d726eeea65a6260a3d709a2d2e2ac6ecd5d39afb7216cc322375d5426c87202f",
+         intel: "26928c3a405a5384cf21f156113be51de99ea4a22118899c1c5295d5d42ad6ec"
 
   url "https://download.todesktop.com/210303leazlircz/Cron%20#{version}-#{arch}-mac.zip",
       verified: "download.todesktop.com/210303leazlircz/"
