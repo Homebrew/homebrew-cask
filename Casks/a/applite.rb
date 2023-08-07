84eabe80390f8038be48e48dacb5a5ca859ad5d3
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-08-07 15:05:27 -0400

    update livecheck, zap and auto_updates

--- Casks/a/applite.rb
+++ Casks/a/applite.rb
@@ -4,15 +4,16 @@ cask "applite" do
 
   url "https://github.com/milanvarady/Applite/releases/download/v#{version}/Applite.dmg",
       verified: "github.com/milanvarady/Applite/"
-  name "applite"
+  name "Applite"
   desc "User-friendly GUI app for Homebrew"
   homepage "https://aerolite.dev/applite"
 
   livecheck do
-    url "https://milanvarady.github.io/Applite/appcast.xml"
-    strategy :sparkle, &:short_version
+    url :url
+    strategy :github_latest
   end
 
+  auto_updates true
   depends_on macos: ">= :ventura"
 
   app "Applite.app"
@@ -20,9 +21,11 @@ cask "applite" do
   zap trash: [
     "~/Library/Application Support/Applite",
     "~/Library/Application Support/dev.aerolite.Applite",
-    "~/Library/Containers/dev.aerolite.Applite",
     "~/Library/Caches/Applite",
     "~/Library/Caches/dev.aerolite.Applite",
+    "~/Library/Containers/dev.aerolite.Applite",
+    "~/Library/HTTPStorages/dev.aerolite.Applite",
+    "~/Library/Preferences/dev.aerolite.Applite.plist",
     "~/Library/Saved Application State/dev.aerolite.Applite.savedState",
   ]
 end
