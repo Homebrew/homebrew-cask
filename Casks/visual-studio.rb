663114b7dfd1578d9160e563beb69cae37975149
owine <owine@users.noreply.github.com>
2023-08-08 15:46:54 -0500

    visual-studio 17.6.3.421

--- Casks/visual-studio.rb
+++ Casks/visual-studio.rb
@@ -2,12 +2,12 @@ cask "visual-studio" do
   arch arm: "arm64", intel: "x64"
 
   on_arm do
-    version "17.6.2.405,a8ffff35-74be-47f6-a375-4fad1c1f18a2,e5175f2ae40baf21bd275ba53b432cd6"
-    sha256 "0b48a8e1adf6bae9c469ac7b08c9397ba3ed217aff64a67d435660dcde4a6956"
+    version "17.6.3.421,91026eef-01fe-4118-a2d7-25859b4d95dc,8f15c87d4f5541e8f893e4dbbb94c2e4"
+    sha256 "9909c94659b640a51afba3e4bd41446adb4944bd5721ba3b0f6740aa9e10b11f"
   end
   on_intel do
-    version "17.6.2.405,a8ffff35-74be-47f6-a375-4fad1c1f18a2,a0b9b13bf42db7ac6386d88a2e3b5ab2"
-    sha256 "ec04616d6add81e14605d09aa295918bad4b0892bd36fe64d6b84cfe729c61c5"
+    version "17.6.3.421,91026eef-01fe-4118-a2d7-25859b4d95dc,4430f81b1e1ae325e1f4ea849b54d54d"
+    sha256 "9f02d73b576819b94fe890a98fd63b0cd513f334d53da22e4fd6c5dc4edbad40"
   end
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv.second}/#{version.csv.third}/visualstudioformac-#{version.csv.first}-#{arch}.dmg"
