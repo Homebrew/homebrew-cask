ea0906b70754615cb270088dd7fb7259f3cc2556
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-08 18:01:01 -0400

    dangerzone 0.4.2

--- Casks/dangerzone.rb
+++ Casks/dangerzone.rb
@@ -1,9 +1,9 @@
 cask "dangerzone" do
   arch arm: "arm64", intel: "i686"
 
-  version "0.4.1"
-  sha256 arm:   "ecaa65f030cdf2ecdd422c134b4c5951249dbd85d8a7bcce33ec45838f7fff49",
-         intel: "7fc652c81efd45318e6b07372c6c075c345a55730e0b8b4b30cd8ace8a5fe0a1"
+  version "0.4.2"
+  sha256 arm:   "7db12734438cbf32701a374a6bd3106b798ceda509ec9da9279ea666b7a2005d",
+         intel: "04e82b127658e776fe2349fab20609b413e9659133f364d81000d7c822f4842c"
 
   url "https://github.com/freedomofpress/dangerzone/releases/download/v#{version}/Dangerzone-#{version}-#{arch}.dmg",
       verified: "github.com/freedomofpress/dangerzone/"
