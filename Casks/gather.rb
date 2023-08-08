f6eb8cdfb3f567ea1cd0010bcf246b6a2a646c4e
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-08 07:15:52 -0400

    gather 0.11.1

--- Casks/gather.rb
+++ Casks/gather.rb
@@ -1,9 +1,9 @@
 cask "gather" do
   arch arm: "-arm64"
 
-  version "0.11.0"
-  sha256 arm:   "bd692382c752e9021b0f70f0ad0739f710a297610ce4f71525968150f53f7bbb",
-         intel: "d1fb2476e320f7c41c69839f934767b72052db42da6e43c9722c2e4565e327db"
+  version "0.11.1"
+  sha256 arm:   "e47c331918f335574a3b550fb6fdb934bded2343c6f8f7027bbc075ec67c8291",
+         intel: "5a4a0f68146463199e9d40889b1c4a905800da028fbeea39a9e9d512d5d9597c"
 
   url "https://github.com/gathertown/gather-town-desktop-releases/releases/download/v#{version}/Gather-#{version}#{arch}-mac.zip",
       verified: "github.com/gathertown/gather-town-desktop-releases/"
