9f292ffc86b556cf87ff661dad8d9b342a19b4f5
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-07 21:51:07 -0400

    saleae-logic: rework
    * Bump to version 2.4.9
    
    * Add arch support
    
    * Update download url
    
    * Rework livecheck

--- Casks/saleae-logic.rb
+++ Casks/saleae-logic.rb
@@ -1,15 +1,24 @@
 cask "saleae-logic" do
-  version "2.4.7"
-  sha256 "e6b1a6ada05c81f209dd31bcc55da759689e8f409abdbce9726c36c2c73f377f"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://downloads.saleae.com/logic#{version.major}/Logic-macOS-#{version}-master.zip"
+  version "2.4.9"
+  sha256 arm:   "72c2fc5d2738333ec76445bfbb87438057297a24369cf9f76ba50c51cdc44099",
+         intel: "609a3031f021067ca760184fad745ec239ebc6530de617ca84c45c195c71f0f5"
+
+  url "https://downloads.saleae.com/logic#{version.major}/Logic-#{version}-macos-#{arch}.zip"
   name "Saleae Logic2"
   desc "Signal analysis for Saleae's devices"
   homepage "https://www.saleae.com/"
 
   livecheck do
-    url "https://logic#{version.major}api.saleae.com/download?os=osx"
-    strategy :header_match
+    url "https://logic#{version.major}api.saleae.com/download?os=osx&arch=#{arch}"
+    regex(/Logic[._-]?(\d+(?:\.\d+)+)[._-]?macos[._-]?#{arch}\.zip/i)
+    strategy :header_match do |headers, regex|
+      match = headers["location"].match(regex)
+      next if match.blank?
+
+      "#{match[1]}"
+    end
   end
 
   depends_on macos: ">= :mojave"
