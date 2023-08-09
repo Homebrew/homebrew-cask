1cdf99a9b13a22276f9c93121065d2c60801851c
Eric Park <me@ericswpark.com>
2023-08-09 11:50:53 +0900

    cryptomator 1.9.3

--- Casks/cryptomator.rb
+++ Casks/cryptomator.rb
@@ -1,9 +1,9 @@
 cask "cryptomator" do
   arch arm: "-arm64", intel: "-x64"
 
-  version "1.9.1"
-  sha256 arm:   "aa3908ec156ba4ede7d2c9239d81a2b21fcfee60389872df1a68b3edb24175f7",
-         intel: "09754948a2837ea4546a6753a3b6556937fbeb557a621f23d2646e9e08976400"
+  version "1.9.3"
+  sha256 arm:   "73db86c16438477823efc0bb6e594fcd0254aa0e4b3fbe3cbf1a0f6f74b7a6cd",
+         intel: "399bbbe914e66b745b839d19da6523c74310ac76cc22096b90f44deb85c2f644"
 
   url "https://github.com/cryptomator/cryptomator/releases/download/#{version}/Cryptomator-#{version}#{arch}.dmg",
       verified: "github.com/cryptomator/cryptomator/"
