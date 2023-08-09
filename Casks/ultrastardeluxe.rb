0b899aa2f17e24411cdf192a5f6eb8dc25786ce3
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-08 20:43:41 -0400

    ultrastardeluxe 2023.6.0

--- Casks/ultrastardeluxe.rb
+++ Casks/ultrastardeluxe.rb
@@ -1,8 +1,8 @@
 cask "ultrastardeluxe" do
-  version "2023.5.0"
-  sha256 "7f646ca8db28be8d4c938ef7d8a934b6c1e276db09855caa2c8ee2b2be84455b"
+  version "2023.6.0"
+  sha256 "a0d47271ae836fd2fccf6962ca127e1ad0313a6ea3f455ad336510e13a85ded0"
 
-  url "https://github.com/UltraStar-Deluxe/USDX/releases/download/v#{version}/UltraStarDeluxe_v#{version}-0.dmg",
+  url "https://github.com/UltraStar-Deluxe/USDX/releases/download/v#{version}/UltraStarDeluxe-v#{version}-0.dmg",
       verified: "github.com/UltraStar-Deluxe/USDX/"
   name "UltraStar Deluxe"
   desc "Karaoke game"
