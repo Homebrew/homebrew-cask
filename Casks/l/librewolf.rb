b12fc3a49caccecbb9661599b265d124870e7c49
Patrick Linnane <patrick@linnane.io>
2023-08-09 15:30:21 +0000

    librewolf 116.0.2

--- Casks/l/librewolf.rb
+++ Casks/l/librewolf.rb
@@ -2,12 +2,12 @@ cask "librewolf" do
   arch arm: "arm64", intel: "x86_64"
 
   on_arm do
-    version "116.0,1"
-    sha256 "0f2c39f7171e2a0d8e2c1d6d622cc65d93066c53877465eeccebed05534387de"
+    version "116.0.2,1"
+    sha256 "353e833ccaeb9f0dbe3454dfcb56c960b3390d84c27b39e0ddb288bd4e3dbf3d"
   end
   on_intel do
-    version "116.0,1"
-    sha256 "7bb3c7dd8c87b3eaef2208fb92f32facb81ac048178c4f23a3165d2a165b8e50"
+    version "116.0.2,1"
+    sha256 "71677ce783650c89c067838f6f21130c3a4c0e4d6112c7cdc294604a6298038a"
   end
 
   url "https://gitlab.com/api/v4/projects/44042130/packages/generic/librewolf/#{version.csv.first}-#{version.csv.second}/librewolf-#{version.csv.first}-#{version.csv.second}-macos-#{arch}-package.dmg",
