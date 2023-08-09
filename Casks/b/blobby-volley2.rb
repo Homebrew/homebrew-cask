29e2e4050d3b3790953296b7bd7736a9d377e6c3
Patrick Linnane <patrick@linnane.io>
2023-08-09 15:16:39 -0700

    blobby-volley2 1.1.1

--- Casks/b/blobby-volley2.rb
+++ Casks/b/blobby-volley2.rb
@@ -1,7 +1,7 @@
 cask "blobby-volley2" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "1.0"
-  sha256 "6e2b1381e32607e1f452920b1fbfcb5f2393bc13e48fcc58cbcd26b8e9e70117"
+  version "1.1.1"
+  sha256 "a2e2a67a3c77a0ace49b4db3b4ee2eaeba8f9c5b02a15b3eec5b033bba36a879"
 
   url "https://downloads.sourceforge.net/blobby/blobby2-macosx-#{version}.dmg",
       verified: "downloads.sourceforge.net/blobby/"
@@ -9,5 +9,5 @@ cask "blobby-volley2" do
   desc "Head-to-head multiplayer ball game"
   homepage "https://blobby.sourceforge.io/"
 
-  app "Blobby Volley 2.app"
+  app "blobby.app"
 end
