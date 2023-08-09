bceabc7c47d44b0cac735084730414beb1f56efa
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-09 12:30:52 -0400

    fly 7.10.0

--- Casks/f/fly.rb
+++ Casks/f/fly.rb
@@ -1,6 +1,6 @@
 cask "fly" do
-  version "7.9.1"
-  sha256 "51c2d8eed5b83725b7854974e9466d6d750ef51e3b57c2551419eb2324f9b0d4"
+  version "7.10.0"
+  sha256 "97f88286bff09e4cbb550983ccd8b84d37d3511862b4373adea5fa8b1d8f0b27"
 
   url "https://github.com/concourse/concourse/releases/download/v#{version}/fly-#{version}-darwin-amd64.tgz"
   name "fly"
