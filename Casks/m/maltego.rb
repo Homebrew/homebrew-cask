5a14ecc03fb22e494c46773a1ca52bfb95e694da
Bevan Kay <email@bevankay.me>
2023-08-10 21:38:22 +1000

    Update maltego.rb

--- Casks/m/maltego.rb
+++ Casks/m/maltego.rb
@@ -2,13 +2,13 @@ cask "maltego" do
   version "4.5.0"
   sha256 "d2ad23677000da380e9ef7b3ee71d3ca8f42d436fb9f37c3cee2bb30f67c434d"
 
-  url "https://downloads.maltego.com/maltego-v4/mac/Maltego.v#{version}.dmg"
+  url "https://downloads.maltego.com/maltego-v#{version.major}/mac/Maltego.v#{version}.dmg"
   name "Maltego"
   desc "Open source intelligence and graphical link analysis tool"
   homepage "https://www.maltego.com/pricing-plans/"
 
   livecheck do
-    url "https://downloads.maltego.com/maltego-v4/info.json"
+    url "https://downloads.maltego.com/maltego-v#{version.major}/info.json"
     regex(/Maltego[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
