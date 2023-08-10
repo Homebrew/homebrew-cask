1f831ad0050594af2e00e499430621ae11764c7c
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-10 10:18:32 -0400

    burp-suite 2023.9.1

--- Casks/b/burp-suite.rb
+++ Casks/b/burp-suite.rb
@@ -1,9 +1,9 @@
 cask "burp-suite" do
   arch arm: "MacOsArm64", intel: "MacOsx"
 
-  version "2023.9"
-  sha256 arm:   "d53d523f1e81d4ca19adf5272f904864554a315222adcf1dda962081cd3d8751",
-         intel: "e2fd3f4b8a4ccb6d4f4685f26011a95b1bd2e9fda0b014a0094f4638698b5095"
+  version "2023.9.1"
+  sha256 arm:   "2b574d5c8fd547db32a72b9a3bfa0f8ea9c5988cf9009b532c019d0c48271907",
+         intel: "9bd4160e6d5b0f2214bc3b9e3174e92cdd92283dc2e68ec3bcbf9b0471c0b880"
 
   url "https://portswigger.net/burp/releases/download?product=community&version=#{version}&type=#{arch}"
   name "Burp Suite Community Edition"
