109fbf4cb87b68e46697b347778444bf83485d8c
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-08-10 10:27:43 -0400

    cask changes

--- Casks/h/helidon.rb
+++ Casks/h/helidon.rb
@@ -3,19 +3,17 @@ cask "helidon" do
   sha256 "485a58eb5e82fe27ef8600329e3b1f79714d9a07c5993b6eba2bf37988179db8"
 
   url "https://github.com/helidon-io/helidon-build-tools/releases/download/#{version}/helidon-cli.zip",
-      verified: "github.com/"
+      verified: "github.com/helidon-io/helidon-build-tools/"
   name "helidon"
   desc "Command-line tool for Helidon application development"
   homepage "https://helidon.io/"
 
   livecheck do
-    url "https://github.com/helidon-io/helidon-build-tools/releases"
+    url :url
     strategy :github_latest
   end
 
   binary "helidon-#{version}/bin/helidon"
 
-  zap trash: [
-    "~/.helidon",
-  ]
+  zap trash: "~/.helidon"
 end
