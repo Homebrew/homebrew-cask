edc84c082c58b6ff06446c27760b851fd60ab79f
Amory Meltzer <Amorymeltzer@gmail.com>
2023-08-10 16:58:58 -0400

    trailer 1.8.6

--- Casks/t/trailer.rb
+++ Casks/t/trailer.rb
@@ -1,6 +1,6 @@
 cask "trailer" do
-  version "1.8.5"
-  sha256 "509a097b20cfbefb36693bb304c3347250d706c4a3bfb1d31e2010f96d421e8e"
+  version "1.8.6"
+  sha256 "5e602f5942bda4273d3b34f6970d86e98c958678813e835d3461249db2bab108"
 
   url "https://github.com/ptsochantaris/trailer/releases/download/#{version}/Trailer-#{version.no_dots}.zip",
       verified: "github.com/ptsochantaris/trailer/"
