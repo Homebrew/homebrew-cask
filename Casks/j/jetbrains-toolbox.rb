83a6d15b0a6cf71f1955475daaf822aad17b7929
Nikolas Evers <vintagesucks@users.noreply.github.com>
2023-08-12 00:00:41 +0200

    jetbrains-toolbox 2.0.2

--- Casks/j/jetbrains-toolbox.rb
+++ Casks/j/jetbrains-toolbox.rb
@@ -1,9 +1,9 @@
 cask "jetbrains-toolbox" do
   arch arm: "-arm64"
 
-  version "2.0.1,2.0.1.16621"
-  sha256 arm:   "ad8cc0b8e6cd6909a76f1f27758f6e02f4bd46e78b943165eafcf7fe0574efea",
-         intel: "ee6ebbd99fa2003cb2ee650ce5cdc7645c11c940ddb8b5bd7500bf50183480f0"
+  version "2.0.2,2.0.2.16660"
+  sha256 arm:   "f9a647d5cfde799c5a68944f6a109a790620198b326b3893b6d1f4f47130c1c5",
+         intel: "e8abff4a952fd2d34cc10c8e3de9187a2c005138464e9d98a9e3e0827346b294"
 
   url "https://download.jetbrains.com/toolbox/jetbrains-toolbox-#{version.csv.second}#{arch}.dmg"
   name "JetBrains Toolbox"
