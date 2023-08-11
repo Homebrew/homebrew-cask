f37dff3949796eeb5d06a36ebe2dace2b3016977
Comix Inc <56793799+tqoo@users.noreply.github.com>
2023-08-11 17:25:54 +0100

    Fixed URL, version number, and checksum

--- Casks/f/furnace.rb
+++ Casks/f/furnace.rb
@@ -1,11 +1,11 @@
 cask "furnace" do
-  version "0.6pre7"
-  sha256 "8863c7f7becea62c2a65e4121e2cfe4d843d23e50e483eb68516df3301c9bbbc"
+  version :latest
+  sha256 :no_check
 
-  url "https://github.com/tildearrow/furnace/releases/download/v#{version}/furnace-#{version}-mac-arm64.dmg"
+  url "https://github.com/tildearrow/furnace/releases/download/v0.6pre7/furnace-0.6pre7-mac-arm64.dmg"
   name "Furnace"
-  desc "A multi-system chiptune tracker"
-
+  desc "Multi-system chiptune tracker"
   homepage "https://github.com/tildearrow/furnace"
+
   app "Furnace.app"
 end
