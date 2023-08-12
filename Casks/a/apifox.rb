54d27e73770954201bbbb76f019850f6e5a17f7c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-12 05:05:42 +0000

    apifox 2.3.10

--- Casks/a/apifox.rb
+++ Casks/a/apifox.rb
@@ -1,9 +1,9 @@
 cask "apifox" do
   arch arm: "-macOS-arm64"
 
-  version "2.3.9"
-  sha256 arm:   "01491d5a0fc91c1462f0afedfa58cafec0d82347ce8eb9001ba8b1f8afd7bd73",
-         intel: "0b70bbd01156b1f86f07ae83f6c239a06b0a4dfe56e052ee55cc021cce0c53bf"
+  version "2.3.10"
+  sha256 arm:   "4634e62e5d72e245c204291589711b37f07ba0ef0b49904719dc86a2c6517e27",
+         intel: "5587dcfae202291bbdfd54532b1b045e48eef18fb0af26dd24c7d81cf0532c4c"
 
   url "https://cdn.apifox.cn/download/#{version}/Apifox#{arch}-#{version}.dmg"
   name "Apifox"
