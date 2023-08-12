e45d0b8234d915c1685d2dc82013270e70de4538
Pavel Kulyov <kulyov.pavel@gmail.com>
2023-08-12 19:23:44 +0300

    defold: update SHA-256

--- Casks/d/defold.rb
+++ Casks/d/defold.rb
@@ -1,6 +1,6 @@
 cask "defold" do
   version "1.4.8"
-  sha256 "0ffc57e4ef74f8ae0b7d262457c06ff42502c9540e46f41b5ff784ea69854716"
+  sha256 "9973009e97323c11a3be49d4d0774c1c2db2e4a7f001c2c9b986b07259ed45ea"
 
   url "https://github.com/defold/defold/releases/download/#{version}/Defold-x86_64-macos.dmg",
       verified: "github.com/defold/defold/"
