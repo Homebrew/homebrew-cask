6a598d85a12f8aa09f75c5d1e3d4f716d3226826
Miccal Matthews <miccal.matthews@gmail.com>
2023-08-13 17:51:02 +0800

    Update stash.rb

--- Casks/s/stash.rb
+++ Casks/s/stash.rb
@@ -4,7 +4,7 @@ cask "stash" do
 
   url "https://mac-release-static.stash.ws/Stash-build-#{version.csv.second}.zip"
   name "Stash"
-  desc "Simple & Powerful Network Tool based on Clash"
+  desc "Network tool based on Clash"
   homepage "https://stash.ws/"
 
   livecheck do
@@ -17,22 +17,24 @@ cask "stash" do
 
   app "Stash.app"
 
-  uninstall quit: "ws.stash.app.mac"
+  uninstall quit: "ws.stash.app.mac",
+            delete: [
+              "/Library/LaunchDaemons/ws.stash.app.mac.daemon.helper.plist",
+              "/Library/PrivilegedHelperTools/ws.stash.app.mac.daemon.helper",
+            ]
 
   zap trash: [
-    "~/Library/HTTPStorages/ws.stash.app.mac.binarycookies",
-    "~/Library/HTTPStorages/ws.stash.app.mac",
-    "~/Library/Preferences/ws.stash.app.mac.plist",
-    "~/Library/Group Containers/group.ws.stash.app",
-    "~/Library/Containers/ws.stash.app.mac-LaunchAtLoginHelper",
-    "~/Library/Application Support/Stash",
-    "~/Library/Application Support/ws.stash.app.mac",
     "~/Library/Application Scripts/group.ws.stash.app",
     "~/Library/Application Scripts/ws.stash.app",
     "~/Library/Application Scripts/ws.stash.app.mac-LaunchAtLoginHelper",
+    "~/Library/Application Support/Stash",
+    "~/Library/Application Support/ws.stash.app.mac",
     "~/Library/Caches/ws.stash.app.mac",
     "~/Library/Caches/ws.stash.app.mac.dashboard",
-    "/Library/PrivilegedHelperTools/ws.stash.app.mac.daemon.helper",
-    "/Library/LaunchDaemons/ws.stash.app.mac.daemon.helper.plist",
+    "~/Library/Containers/ws.stash.app.mac-LaunchAtLoginHelper",
+    "~/Library/Group Containers/group.ws.stash.app",
+    "~/Library/HTTPStorages/ws.stash.app.mac",
+    "~/Library/HTTPStorages/ws.stash.app.mac.binarycookies",
+    "~/Library/Preferences/ws.stash.app.mac.plist",
   ]
 end
