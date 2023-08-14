0562e12a0efe378bc6403c95a520c4547c079bd2
Miccal Matthews <miccal.matthews@gmail.com>
2023-08-15 05:35:25 +0800

    Update ipepresenter.rb

--- Casks/i/ipepresenter.rb
+++ Casks/i/ipepresenter.rb
@@ -1,8 +1,11 @@
 cask "ipepresenter" do
-  version "7.2.28"
-  sha256 "08068414c6455e85a85b9467c7773849a4987761cfab7dd0594b7183992348e8"
+  arch arm: "arm", intel: "intel"
 
-  url "https://github.com/otfried/ipe/releases/download/v#{version}/ipepresenter-#{version}-mac.dmg",
+  version "7.2.28"
+  sha256 arm:   "e2a699e65248595a6e31abea0766cb74c9b58ddd8ccb3a9bc890e000b48fb699",
+         intel: "2de5402c14cc8bbfcc3f6ea45a3712c5cfac928aeabf506d0673aecbb6884ff7"
+
+  url "https://github.com/otfried/ipe/releases/download/v#{version}/ipepresenter-#{version}-mac-#{arch}.dmg",
       verified: "github.com/otfried/ipe/"
   name "IpePresenter"
   desc "Make presentations from PDFs"
@@ -10,12 +13,8 @@ cask "ipepresenter" do
 
   livecheck do
     url :homepage
-    regex(/href=.*?ipepresenter[._-](\d+(?:\.\d+)+)[._-]mac\.dmg/i)
+    regex(/href=.*?ipepresenter[._-](\d+(?:\.\d+)+)[._-]mac[._-]#{arch}\.dmg/i)
   end
 
   app "IpePresenter.app"
-
-  caveats do
-    requires_rosetta
-  end
 end
