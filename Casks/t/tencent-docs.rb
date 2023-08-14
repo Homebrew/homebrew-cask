46340900e9ba06d398c0856fbeed85e15aaa7cad
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-14 10:41:19 -0400

    tencent-docs 3.1.5

--- Casks/t/tencent-docs.rb
+++ Casks/t/tencent-docs.rb
@@ -1,7 +1,7 @@
 cask "tencent-docs" do
   arch arm: "Arm64", intel: "X64"
 
-  version "3.1.4,356"
+  version "3.1.5,364"
   sha256 :no_check
 
   url "https://down.qq.com/qqweb/mac_docs/MacTencentDocs#{arch}.dmg"
