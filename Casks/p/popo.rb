1a3bf64027fa27fdf594276a8c5e0d532bb12d33
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-14 09:51:49 -0400

    popo 3.78.0

--- Casks/p/popo.rb
+++ Casks/p/popo.rb
@@ -1,6 +1,6 @@
 cask "popo" do
-  version "3.77.1,1691031079697"
-  sha256 "8e4a2c423502687dd95d94c4d9978be6122943730e7c560ec8be7ee7876605c3"
+  version "3.78.0,1691635666703"
+  sha256 "fd9801f90ee623c8dff33d4db05f00673c4f91f1bb58a26790f99a1ca1cdb560"
 
   url "https://popo.netease.com/file/popomac/POPO-setup_prod_#{version.csv.second}.dmg"
   name "NetEase POPO"
