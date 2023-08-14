8785d5e56d72ff3fc2be954044fa5ff3db30e174
Jan Linxweiler <j.linxweiler@tu-braunschweig.de>
2023-08-14 14:31:52 +0200

    Add Energiza.app v1.3.0
    Add a Cask to install Energiza.app using homebrew.

---
+++ Casks/energiza.rb
@@ -0,0 +1,11 @@
+cask "energiza" do
+  version "1.3.0"
+  sha256 :no_check
+
+  url "https://appgineers.de/energiza/files/Energiza.zip"
+  name "Energiza Pro"
+  desc "Nifty charging manager for your MacBooks"
+  homepage "https://appgineers.de/energiza/"
+
+  app "Energiza.app"
+end
