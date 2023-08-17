b88be544925c7beec8bbba9a9fd18454342938d3
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-17 11:01:16 -0400

    readdle-spark 3.7.0.54485

--- Casks/r/readdle-spark.rb
+++ Casks/r/readdle-spark.rb
@@ -1,6 +1,6 @@
 cask "readdle-spark" do
-  version "3.6.8.53256"
-  sha256 "73d6e152e228839cb291dc5eead72738053c8538db56e7aa25dc2d7c40d6cbf1"
+  version "3.7.0.54485"
+  sha256 "ba7c516b90aa75944f24dc34f35b3e73aef9979792c9648de5dfcbf23f98035a"
 
   url "https://downloads.sparkmailapp.com/Spark#{version.major}/mac/dist/#{version}/Spark.zip"
   name "Spark"
