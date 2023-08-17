9796a334dcf4f3e60b1f12f4f2f57cf5ce42ca21
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-08-17 12:54:48 -0400

    ppduck 3.10.18

--- Casks/p/ppduck.rb
+++ Casks/p/ppduck.rb
@@ -1,6 +1,6 @@
 cask "ppduck" do
-  version "3.10.16"
-  sha256 "e03a162603cc606a77cc8cf95b59ad097cf627113bc4b39ba72acf35204c3b15"
+  version "3.10.18"
+  sha256 "0787ea079fb3ccfcf792c3c552b1c17bb96eb8ba3f9e9d43d7241a67c6b23c43"
 
   url "https://download.ppduck.com/PPDuck#{version.major}_#{version}.dmg"
   name "PPDuck"
