dd889ebd9607dea655b186e44bc5ecc9aea62870
Nathan Courtney <nathan@pieces.app>
2023-08-17 11:08:38 -0400

    pieces: new cask

---
+++ Casks/p/pieces.rb
@@ -0,0 +1,38 @@
+cask "pieces" do
+  arch arm: "-arm64"
+  livecheck_arch = on_arch_conditional arm: "_arm64"
+
+  version "2.3.1"
+  sha256 :no_check
+
+  url "https://builds.pieces.app/stages/production/pieces_for_x/macos#{arch}/download"
+  name "pieces"
+  desc "Code Snippets, Screenshots, Workflow Context. A powerful Copilot for Developers"
+  homepage "https://pieces.app/"
+
+  livecheck do
+    url "https://builds.pieces.app/stages/production/pieces_for_x/sparkle#{livecheck_arch}"
+    strategy :sparkle
+  end
+
+  auto_updates true
+  depends_on cask: "pieces-os"
+  depends_on macos: ">= :big_sur"
+
+  app "Pieces.app"
+
+  uninstall quit: "com.pieces.x"
+
+  zap trash: [
+    "~/Library/com.pieces.pfd",
+    "~/Library/Preferences/com.pieces.x.plist",
+    "~/Library/Application Support/com.pieces.x",
+  ]
+
+  caveats <<~EOS
+    To Get Started With Pieces, First Launch Pieces OS:
+      open -a "Pieces OS.app"
+    Then Launch Pieces.app:
+      open -a "Pieces.app"
+  EOS
+end
