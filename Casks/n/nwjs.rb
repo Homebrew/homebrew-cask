108a446e7e51b53888d772f268c306e16bb4ab32
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-18 09:01:13 -0400

    nwjs 0.79.0

--- Casks/n/nwjs.rb
+++ Casks/n/nwjs.rb
@@ -1,6 +1,6 @@
 cask "nwjs" do
-  version "0.78.1"
-  sha256 "e197b0017e0214716f6447a27fe55513445e3ce54d6bf2991d62ba62007f8210"
+  version "0.79.0"
+  sha256 "bee025bc62cb4a2f25ae25dde3cb8bde1672ea7c558d752e3fe61e0a12fc2642"
 
   url "https://dl.nwjs.io/v#{version}/nwjs-sdk-v#{version}-osx-x64.zip"
   name "NW.js"
