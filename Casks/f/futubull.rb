7eca48e4e68e5a005bec4c6e670b79008e4c590f
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-08-18 12:13:59 -0400

    futubull 13.12.4808

--- Casks/f/futubull.rb
+++ Casks/f/futubull.rb
@@ -1,6 +1,6 @@
 cask "futubull" do
-  version "13.11.4708"
-  sha256 "00afa603e13a72410d7cf70e872b4a5b26394a204c10f1d66ebc60de66308b82"
+  version "13.12.4808"
+  sha256 "4997dba787c78614afb47d44bce9665689f00ecea618981c9592436f01524b60"
 
   url "https://softwarefile.futunn.com/FTNNForMac_#{version}_Website.dmg"
   name "Futubull"
