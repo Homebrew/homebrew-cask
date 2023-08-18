1263288c6f4867ec8dbecd3dd492c476cf4d634b
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-18 09:25:00 -0400

    redcine-x-pro 61.1.2

--- Casks/r/redcine-x-pro.rb
+++ Casks/r/redcine-x-pro.rb
@@ -1,6 +1,6 @@
 cask "redcine-x-pro" do
-  version "60.3.11"
-  sha256 "9112ac715a1490a03fd250eb59cc87b33659cb25e71c985a143a2917a08d4002"
+  version "61.1.2"
+  sha256 "dafc8e78e00f3fa75a93c7bb896cdc1acaa2ce8b2a1678159cbbb355c089ba1b"
 
   url "https://downloads.red.com/software/rcx/mac/release/#{version}/REDCINE-X_PRO_Build_#{version}.pkg"
   name "REDCINE-X PRO"
