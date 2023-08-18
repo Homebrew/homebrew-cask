22c1adf12d47c07fd0fc67df24762a3c96e648fe
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-18 09:50:04 -0400

    swiftformat-for-xcode 0.52.1

--- Casks/s/swiftformat-for-xcode.rb
+++ Casks/s/swiftformat-for-xcode.rb
@@ -1,6 +1,6 @@
 cask "swiftformat-for-xcode" do
-  version "0.52.0"
-  sha256 "e46e6e2b5751e895521408441e276c3858b0befc113162d81456f44c1da5f668"
+  version "0.52.1"
+  sha256 "2a69cd4323f858f155a10256a8e021544068092e5d5ce8c5418dec2f3d3a87aa"
 
   url "https://github.com/nicklockwood/SwiftFormat/releases/download/#{version}/SwiftFormat.for.Xcode.app.zip"
   name "SwiftFormat for Xcode"
