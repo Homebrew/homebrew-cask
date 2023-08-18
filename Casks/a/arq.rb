409dafa008ad53da8262499d9b4c3b5bf7eb94c7
owine <owine@users.noreply.github.com>
2023-08-18 15:40:23 -0500

    arq 7.23

--- Casks/a/arq.rb
+++ Casks/a/arq.rb
@@ -1,6 +1,6 @@
 cask "arq" do
-  version "7.22"
-  sha256 "138910b32dfbf26b7dc3a616929b1460acdd786a21e0a47796aebab7b0585c7d"
+  version "7.23"
+  sha256 "1ad9478309be3997eb896d7347499411edd8ab5b686d42c25070ba55f45b5f91"
 
   url "https://www.arqbackup.com/download/arqbackup/Arq#{version}.pkg"
   name "Arq"
