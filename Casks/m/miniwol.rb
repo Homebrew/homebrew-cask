6a91734355d07dee6cb37a0fb297a3daaee64cd8
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-08-18 12:07:30 -0400

    update checksum

--- Casks/m/miniwol.rb
+++ Casks/m/miniwol.rb
@@ -1,6 +1,6 @@
 cask "miniwol" do
   version "2.1.4"
-  sha256 "ab9cf31a15339fa548edf35754edaa4f1d5b824fdd92d56a1dead20c0087b8aa"
+  sha256 "2b4f146a8632e3313604707bb8f4606c8c3a528839a7f9dc77939c05e85569ce"
 
   url "https://www.tweaking4all.com/downloads/network/miniWOL-v#{version}-64bit.dmg"
   name "miniWOL"
