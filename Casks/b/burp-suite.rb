dac3edd50ed5a01751179e675aea196b1a8d9a2a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-18 05:07:05 +0000

    burp-suite 2023.9.2

--- Casks/b/burp-suite.rb
+++ Casks/b/burp-suite.rb
@@ -1,9 +1,9 @@
 cask "burp-suite" do
   arch arm: "MacOsArm64", intel: "MacOsx"
 
-  version "2023.9.1"
-  sha256 arm:   "2b574d5c8fd547db32a72b9a3bfa0f8ea9c5988cf9009b532c019d0c48271907",
-         intel: "9bd4160e6d5b0f2214bc3b9e3174e92cdd92283dc2e68ec3bcbf9b0471c0b880"
+  version "2023.9.2"
+  sha256 arm:   "7b9ef920c5e021e0f55fb4b37c8b304747722213a192a0b307e64d40732896c8",
+         intel: "12896f2707f39e7efbde172fda762ae811b4eb5ac6b320bd2acae2338fd5ae18"
 
   url "https://portswigger.net/burp/releases/download?product=community&version=#{version}&type=#{arch}"
   name "Burp Suite Community Edition"
