6884ca559557ecaa96c6a04fdb646d56f57ba0b7
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-19 13:30:43 -0400

    antconc: add ARM arch + version

--- Casks/a/antconc.rb
+++ Casks/a/antconc.rb
@@ -1,8 +1,11 @@
 cask "antconc" do
-  version "4.2.1"
-  sha256 "265ef0ec0ea894d6e6369f253cc39e54596fcf3e4ab0caab9aa9a45e3cd7c453"
+  arch arm: "silicon", intel: "intel"
 
-  url "https://www.laurenceanthony.net/software/antconc/releases/AntConc#{version.no_dots}/AntConc.dmg"
+  version "4.2.1"
+  sha256 arm:   "4cf5564cfecc408fff8ada0ca74b5014b881f3915fa35afb3eea9f8240e58913",
+         intel: "391f73e85c66db508d0c36bfd425ceb47761a92ecfd8b395b774890c597a19ae"
+
+  url "https://www.laurenceanthony.net/software/antconc/releases/AntConc#{version.no_dots}/apple-#{arch}/AntConc.dmg"
   name "AntConc"
   desc "Corpus analysis toolkit for concordancing and text analysis"
   homepage "https://www.laurenceanthony.net/software/antconc/"
