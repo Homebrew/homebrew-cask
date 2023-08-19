ffbef2ed3ba56b937f412885d3e2c544350e32f8
Leo <leo-van@hotmail.com>
2023-08-19 19:23:26 +0800

    scihubeva 6.2.0

--- Casks/s/scihubeva.rb
+++ Casks/s/scihubeva.rb
@@ -1,19 +1,21 @@
 cask "scihubeva" do
-  version "6.1.0"
-  sha256 "324ff6867fc26d9d163e18244637a871f896c8a12dca69a194f2ee0d0852a169"
+  arch arm: "arm64", intel: "x86_64"
 
-  url "https://github.com/leovan/SciHubEVA/releases/download/v#{version}/SciHubEVA-v#{version}.dmg"
-  name "SciHubEVA"
-  desc "Sci-Hub GUI app"
+  version "6.2.0"
+  sha256 arm:   "fa885251ee123db16829cb6869041016009139197fec2152b9e0bf622a9bdca9",
+         intel: "157f48cf7ebac34bef0d92aae36c893beb493396869b179627c01ce1fdde0304"
+
+  url "https://github.com/leovan/SciHubEVA/releases/download/v#{version}/SciHubEVA-#{arch}-v#{version}.dmg"
+  name "Sci-Hub EVA"
+  desc "A cross-platform Sci-Hub GUI application powered by Python and Qt."
   homepage "https://github.com/leovan/SciHubEVA"
 
-  app "SciHubEVA.app"
+  app "Sci-Hub EVA.app"
 
   zap trash: [
-    "~/Library/Caches/LeoVan",
-    "~/Library/Logs/LeoVan",
-    "~/Library/Preferences/me.leovan.SciHubEVA.plist",
-    "~/Library/Preferences/SciHubEVA.plist",
-    "~/Library/Saved Application State/SciHubEVA.savedState",
+    "~/Library/Caches/tech.leovan.SciHubEVA",
+    "~/Library/Logs/leovan.tech/SciHubEVA",
+    "~/Library/Preferences/tech.leovan.SciHubEVA.plist",
+    "~/Library/Saved Application State/tech.leovan.SciHubEVA.savedState",
   ]
 end
