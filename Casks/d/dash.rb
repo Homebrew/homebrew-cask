51a8fb8069c5d27ceeb2a58c691f833d62331238
singularitti <singularitti@outlook.com>
2023-08-19 16:45:28 -0400

    dash 0.5.12

--- Casks/d/dash.rb
+++ Casks/d/dash.rb
@@ -4,8 +4,8 @@ cask "dash" do
     sha256 "e2b5eb996645b25f12ccae15e24b1b0d8007bc5fed925e14ce7be45a2b693fb6"
   end
   on_mojave :or_newer do
-    version "7.0.0"
-    sha256 "4eb3095a247bc676779e6fd4e08a06f8fcf91edac31f651e80f439acf6701930"
+    version "0.5.12"
+    sha256 "20965967ba5cb8039cedd6c3bad8935d08f26e4e530e84d04c6c40dd7c7c76b5"
   end
 
   url "https://kapeli.com/downloads/v#{version.major}/Dash.zip"
@@ -28,6 +28,8 @@ cask "dash" do
     "~/Library/Application Support/Dash",
     "~/Library/Caches/com.kapeli.dashdoc",
     "~/Library/Cookies/com.kapeli.dashdoc.binarycookies",
+    "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.kapeli.dashdoc",
+    "~/Library/HTTPStorages/com.kapeli.dashdoc",
     "~/Library/HTTPStorages/com.kapeli.dashdoc.binarycookies",
     "~/Library/Logs/Dash",
     "~/Library/Preferences/com.kapeli.dashdoc.plist",
