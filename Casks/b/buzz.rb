431130d197055857fe21d57d55c746bfa38dc2a7
runner <runner@Mac-1692529417474.local>
2023-08-20 11:23:47 +0000

    buzz 0.8.4

--- Casks/b/buzz.rb
+++ Casks/b/buzz.rb
@@ -1,6 +1,6 @@
 cask "buzz" do
-  version "0.8.3"
-  sha256 "fcc7eb06df2749b5757b748e77f206e130b3751181f67ec24ae0624b63c06438"
+  version "0.8.4"
+  sha256 "272c9010e23b7b1c47e6bf7db376238e7d2ab897efc1cb2ccfa4ea5ecd733feb"
 
   url "https://github.com/chidiwilliams/buzz/releases/download/v#{version}/Buzz-#{version}-mac.dmg"
   name "Buzz"
