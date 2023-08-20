b111462d1528a990b1245a07dee7cc043e3d80fa
Loïc PÉRON <loic.peron@gmail.com>
2023-08-20 23:05:22 +0200

    dbeaver-community 23.1.5

--- Casks/d/dbeaver-community.rb
+++ Casks/d/dbeaver-community.rb
@@ -1,9 +1,9 @@
 cask "dbeaver-community" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "23.1.4"
-  sha256 arm:   "28b71c19fdd1244ee9cab284f4dae52ac4f233e110e1309cd94c9845ac332237",
-         intel: "4c10ee65f96688084ca0ae0302948b209879d8edade6a273b31d9e28f16c59fd"
+  version "23.1.5"
+  sha256 arm:   "72a48359673109a95e739f170423493d092482bebe157c483d52eca0ecb83699",
+         intel: "c84d1fea5ec7532874ccd3899b2c46ebcf5b84f337ec5f2aeebe08858021e09c"
 
   url "https://dbeaver.io/files/#{version}/dbeaver-ce-#{version}-macos-#{arch}.dmg"
   name "DBeaver Community Edition"
