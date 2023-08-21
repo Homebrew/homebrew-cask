2edaabf4eb01a6274755089c8283db8f62e1a108
airsquared <36649395+airsquared@users.noreply.github.com>
2023-08-20 19:21:50 -0700

    blobsaver 3.5.2

--- Casks/b/blobsaver.rb
+++ Casks/b/blobsaver.rb
@@ -1,9 +1,9 @@
 cask "blobsaver" do
   arch arm: "arm", intel: "intel"
 
-  version "3.5.1"
-  sha256 arm:   "5c267bde6930bdea4007a601b55310c989226303cc7811f0d1f8ec23df542edc",
-         intel: "8ceb2536b73d0c36af84b0a1eb61131a5263c142517566a651e69b0abbea1071"
+  version "3.5.2"
+  sha256 arm:   "85a25c4148bb43c213b6cc1be31beeb748e94f37334ff705f846b205da0b06ce",
+         intel: "06bf93418d68da77cfb77ef87381f1cfc175c3dfe3338fe16819b6cf8e90903a"
 
   url "https://github.com/airsquared/blobsaver/releases/download/v#{version}/blobsaver-#{arch}-#{version}.dmg"
   name "blobsaver"
