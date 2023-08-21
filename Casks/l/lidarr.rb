c43f2cf3252228b357a6ec111f24111a391fc0e7
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-20 21:58:33 -0400

    lidarr 1.3.5.3530

--- Casks/l/lidarr.rb
+++ Casks/l/lidarr.rb
@@ -1,6 +1,6 @@
 cask "lidarr" do
-  version "1.2.6.3313"
-  sha256 "d608f733a98e5400ba7bebe2b945316a44751cb1e79fed39f2e39be646495a6b"
+  version "1.3.5.3530"
+  sha256 "8844c295364f02f60c3c01c7f6983f7768b4a19d53ec520e6fa4386c658892b9"
 
   url "https://github.com/lidarr/Lidarr/releases/download/v#{version}/Lidarr.master.#{version}.osx-app-core-x64.zip",
       verified: "github.com/lidarr/Lidarr/"
