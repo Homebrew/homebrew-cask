bb615a487f699ba8c23b1dfd61e5a841b25b1f1c
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-08-22 11:09:09 -0400

    ableton-live-intro 11.3.10

--- Casks/a/ableton-live-intro.rb
+++ Casks/a/ableton-live-intro.rb
@@ -1,6 +1,6 @@
 cask "ableton-live-intro" do
-  version "11.3.4"
-  sha256 "fb506644438d25aa14aa2d392f7dd5846dcf48f529ebfd361d1e50019af8aebe"
+  version "11.3.10"
+  sha256 "75a145140dd836b121749750676a6e88624728980122fbe692b7ae64a75eb9c3"
 
   url "https://cdn-downloads.ableton.com/channels/#{version}/ableton_live_intro_#{version}_universal.dmg"
   name "Ableton Live Intro"
