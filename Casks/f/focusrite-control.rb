49a418abd29d9d253246f85019143d2fb5289757
Kevin Ji <1146876+kevinji@users.noreply.github.com>
2023-08-21 17:39:33 -0700

    focusrite-control 3.14.2
    The URL format has changed slightly, so livecheck has also been updated.

--- Casks/f/focusrite-control.rb
+++ Casks/f/focusrite-control.rb
@@ -1,15 +1,15 @@
 cask "focusrite-control" do
-  version "3.11.0.1983"
-  sha256 "6563ce66aaec2cb24a31bc8bc91f0d819ecaafc515559f6a692c3c0d766c210c"
+  version "3.14.2"
+  sha256 "90d065ac23fbd26de57d581ad43ab1fd455364fdfaeba406e3d4b098df8adad4"
 
-  url "https://fael-downloads-prod.focusrite.com/customer/prod/downloads/Focusrite%20Control%20-%20#{version}.dmg"
+  url "https://fael-downloads-prod.focusrite.com/customer/prod/downloads/focusrite_control_#{version}.dmg"
   name "Focusrite Control"
   desc "Focusrite interface controller"
   homepage "https://focusrite.com/en"
 
   livecheck do
     url "https://downloads.focusrite.com/focusrite/scarlett-3rd-gen/scarlett-solo-3rd-gen"
-    regex(%r{href=.*?/Focusrite%20Control%20-%20(\d+(?:\.\d+)+)\.dmg}i)
+    regex(%r{href=.*?/focusrite_control_(\d+(?:\.\d+)+)\.dmg}i)
   end
 
   pkg "Focusrite Control.pkg"
