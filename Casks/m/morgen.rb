e9d4d2871a11d611ad51250aea34278cb6a11ddd
Alessandro Bellanda <alessandro.bellanda@weroad.it>
2023-08-22 14:41:03 +0200

    morgen 3.0.0

--- Casks/m/morgen.rb
+++ Casks/m/morgen.rb
@@ -1,9 +1,9 @@
 cask "morgen" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.7.5"
-  sha256 arm:   "abb777f01bffc2758616cda96086e6fd904d10e8ce6ed0a46953aebe10e864ac",
-         intel: "505e7b2c1d6d4802640b4ddedee1160a8e198a5343bbb56de3c006886b9920f2"
+  version "3.0.0"
+  sha256 arm:   "173283212079ff3c85993777095719a16bc75101227574645ab468b3a6dba6ad",
+         intel: "8ef19989345971fd64980ca52019e47db01477287d670e030e7cf77c3154c987"
 
   url "https://download.todesktop.com/210203cqcj00tw1/Morgen%20#{version}-#{arch}.dmg",
       verified: "download.todesktop.com/210203cqcj00tw1/"
