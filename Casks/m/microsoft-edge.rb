4c867be416bca6d67e3bd10862692b7f524bb22d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-22 05:07:49 +0000

    microsoft-edge 116.0.1938.54

--- Casks/m/microsoft-edge.rb
+++ Casks/m/microsoft-edge.rb
@@ -2,12 +2,12 @@ cask "microsoft-edge" do
   linkid = on_arch_conditional arm: "2093504", intel: "2069148"
 
   on_arm do
-    version "115.0.1901.203,65a73219-02a3-48d1-a9d5-69cfbaf9e74a"
-    sha256 "b55e96ef8f84782bdec44f13e33073357112441f04cd633dababe18f36579904"
+    version "116.0.1938.54,8580ca57-ee78-481d-8575-c9b38485815a"
+    sha256 "d6c9c9291d3616da8e1f09f8b1caddb8fcbd0189ebdd383a944a1389c1c882ce"
   end
   on_intel do
-    version "115.0.1901.203,17b8fa3b-a260-4364-9d56-a271c2890a65"
-    sha256 "20d6f3eebb88048eb1036dc220333e5d3cd854dbbda0e413032194f4ff9824b5"
+    version "116.0.1938.54,e145e569-40ba-46a4-8d12-84da83edf495"
+    sha256 "f1a568fc895e86f9ca0eefaa089ef4568f11c84df0eee019c2042a4b9591f744"
   end
 
   url "https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/#{version.csv.second}/MicrosoftEdge-#{version.csv.first}.pkg"
