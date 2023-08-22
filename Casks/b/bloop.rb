6b1687c6cdd6d4877d4e43e38baf1afc384b0471
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-08-22 10:57:06 -0400

    bloop 0.4.14

--- Casks/b/bloop.rb
+++ Casks/b/bloop.rb
@@ -1,9 +1,9 @@
 cask "bloop" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.4.13"
-  sha256 arm:   "29b015815b8e755bc77426fdef878ce03cb8b6364245662e84b2d5b341b08afc",
-         intel: "1d626d08b01689042fe28a936882708da92c9c487f36bfc063acadd164869b41"
+  version "0.4.14"
+  sha256 arm:   "f4332114566bd551e70e25de88032f6f8bb62677f4b27ea7ac7f1d220aa4c5a5",
+         intel: "1795a5a95d92b40e12907d7d2a87d91c81e2454362961ead6f3f960b93556046"
 
   url "https://github.com/BloopAI/bloop/releases/download/v#{version}/bloop_#{version}_#{arch}.dmg",
       verified: "github.com/BloopAI/bloop/"
