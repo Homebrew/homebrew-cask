e3a94dd7aba2be8b5ea4296e3cb99a40f9c10606
owine <owine@users.noreply.github.com>
2023-08-22 08:04:37 -0500

    hdhomerun 20230816

--- Casks/h/hdhomerun.rb
+++ Casks/h/hdhomerun.rb
@@ -1,6 +1,6 @@
 cask "hdhomerun" do
-  version "20230726"
-  sha256 "2d2beedd112ca155316e150a66c9e2ab01bb14d2a1223d14d4f4259f0c32187d"
+  version "20230816"
+  sha256 "395349e514a93e610488c92b268c9fce644bfc8cb244e67a5c23e4c4f8cf3b04"
 
   url "https://download.silicondust.com/hdhomerun/hdhomerun_mac_#{version}.dmg"
   name "HDHomeRun"
