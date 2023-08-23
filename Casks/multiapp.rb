b7be09da4dbf2b54a2d6a4f718dba5ebad6564b6
Reed Harston <reed@remotion.com>
2023-08-23 12:01:42 -0600

    create multi app cask

---
+++ Casks/multiapp.rb
@@ -0,0 +1,17 @@
+cask "multiapp" do
+  version "0.241.1"
+  sha256 "dc47acd93c08d5bfa3338faff036ad90af83c1d693eed009b5f2fc6e6263985b"
+
+  url "https://updates.multi.app/installers/Multi%20#{version}.dmg"
+  name "Multi"
+  desc "Multiplayer Collaboration for macOS"
+  homepage "https://www.multi.app/"
+
+  app "Multi.app"
+
+  zap trash: [
+    "~/Library/Application Support/Multi",
+    "~/Library/Caches/app.multi.multi",
+    "~/Library/Preferences/app.multi.multi.plist",
+  ]
+end
