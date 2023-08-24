c7cde69c2de5c5330b9ade88366f7e7d755d5607
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-24 11:38:12 -0400

    textexpander 7.6.1

--- Casks/t/textexpander.rb
+++ Casks/t/textexpander.rb
@@ -1,6 +1,6 @@
 cask "textexpander" do
-  version "7.6,760.6"
-  sha256 "c928629e81de60e2fe56882dc9712572a75eb097a85f58e7bcce12ad6775be59"
+  version "7.6.1,761.3"
+  sha256 "0b3cd66cbf174db2546866fb60494a77ab2364c1e2897cc4f7a78952967c7ab8"
 
   url "https://cdn.textexpander.com/mac/#{version.csv.second}/TextExpander_#{version.csv.first}.dmg",
       verified: "cdn.textexpander.com/mac/"
