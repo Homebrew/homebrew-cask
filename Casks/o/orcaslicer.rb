183c402a30e9a09f11d0e74bf5686d05f4fe30af
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-24 11:05:23 -0400

    orcaslicer 1.6.6

--- Casks/o/orcaslicer.rb
+++ Casks/o/orcaslicer.rb
@@ -1,9 +1,9 @@
 cask "orcaslicer" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.6.4"
-  sha256 arm:   "4e4bf76ae1f22eb8bf5c11bc3478c04cd1a9cf5a5aeaab18643b74eb07bf9fb2",
-         intel: "21ed4869a5f628aacfce48403cca00b028498292c8679d356f963e23a04aac00"
+  version "1.6.6"
+  sha256 arm:   "453049f845a6f495f9ca3eeda578331df15e22d17e7017b324a113717bfc0a83",
+         intel: "74eed76b2297a84257437cc053f06cba5d22bda04041ccead04335a2a6a7c977"
 
   url "https://github.com/SoftFever/OrcaSlicer/releases/download/v#{version}/OrcaSlicer_Mac_#{arch}_V#{version}.dmg"
   name "Orca Slicer"
