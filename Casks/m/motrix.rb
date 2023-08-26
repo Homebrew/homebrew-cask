e5f123cb6e8103bbec42e71be83525bb6c90c509
Changren Wang <wangchangren@yahoo.com>
2023-08-26 09:59:26 +0800

    feat: speed up motrix download by proxy

--- Casks/m/motrix.rb
+++ Casks/m/motrix.rb
@@ -5,8 +5,8 @@ cask "motrix" do
   sha256 arm:   "d73f4d69f3597ad8f98b821aa0fb58ba964bf85061b4a13f00edcb3618001c0e",
          intel: "b644cc83aa98224147ef2942fd49ecfc8cdcebfce9616550fa35caa6850c4178"
 
-  url "https://github.com/agalwood/Motrix/releases/download/v#{version}/Motrix-#{version}#{arch}.dmg",
-      verified: "github.com/agalwood/Motrix/"
+  url "https://dl.moapp.me/https://github.com/agalwood/Motrix/releases/download/v#{version}/Motrix-#{version}#{arch}.dmg",
+      verified: "dl.moapp.me/"
   name "Motrix"
   desc "Open-source download manager"
   homepage "https://motrix.app/"
@@ -14,7 +14,7 @@ cask "motrix" do
   # A tag using the stable version format is sometimes marked as "Pre-release"
   # on the GitHub releases page, so we have to use the `GithubLatest` strategy.
   livecheck do
-    url :url
+    url "https://github.com/agalwood/Motrix/releases"
     strategy :github_latest
   end
 
