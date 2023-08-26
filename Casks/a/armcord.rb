47b314a4c2efa0668e7d9c51211dc4218d76625d
georgemoodier <84156418+georgemoodier@users.noreply.github.com>
2023-08-27 00:13:37 +0330

    Update Armored to v3.2.3

--- Casks/a/armcord.rb
+++ Casks/a/armcord.rb
@@ -1,12 +1,18 @@
 cask "armcord" do
-  version "3.2.3"
-  sha256 "47137f6a47f6ef5823c9192f56577c36aa4f2f395fcf969712345655657cb1c8"
+  version "3.2.4"
+  sha256 "a246a1559bcdc4c4243c7544fec7968caf6f6415e0eb442a9a2348332de8a04c"
 
   url "https://github.com/ArmCord/ArmCord/releases/download/v#{version}/ArmCord-#{version}.dmg",
       verified: "github.com/ArmCord/ArmCord/"
   name "armcord"
   desc "Custom lightweight Discord client designed to enhance your experience"
   homepage "https://armcord.app/"
+
+  zap trash: [
+  "~/Library/Application Support/ArmCord",
+  "~/Library/Preferences/com.smartfridge.armcord.plist",
+  "~/Library/Saved Application State/com.smartfridge.armcord.savedState",
+]
 
   app "armcord.app"
 end
