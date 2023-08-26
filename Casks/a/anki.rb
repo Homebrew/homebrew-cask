cf4f90f8d1ae64ac2c1ffab20c9f2ac3d188a68e
iPumpkin <ipumpkin.lv17@gmail.com>
2023-08-26 22:48:11 +0800

    anki 2.1.66

--- Casks/a/anki.rb
+++ Casks/a/anki.rb
@@ -1,17 +1,17 @@
 cask "anki" do
   arch arm: "apple", intel: "intel"
 
-  version "2.1.65"
+  version "2.1.66"
 
   on_catalina :or_older do
-    sha256 "dc69a97b4acacc9890c727695cc466a85dc3d6df7479ebb8e4aa1d99718e4954"
+    sha256 "b7afd01d6f65ab5669215c3135c80c34f4aa7e1cc0e300c0092a132b651787f7"
 
     url "https://github.com/ankitects/anki/releases/download/#{version}/anki-#{version}-mac-#{arch}-qt5.dmg",
         verified: "github.com/ankitects/anki/"
   end
   on_big_sur :or_newer do
-    sha256 arm:   "d68c1b78bdbc7673980fb9d1f6e66df441ef14aef91851917ba4bda15f39128a",
-           intel: "ce6088efbc8b768266ff39d4832fb167c25defb17708f973ab771e0737240385"
+    sha256 arm:   "d31b474f4aeaed09c7aa12324a904ab4efa4300bbcf10e675a65af312c55b41a",
+           intel: "70562ec3e65241de51c3ec7b7882840f357d532beec57de267c76b9ad40d67bf"
 
     url "https://github.com/ankitects/anki/releases/download/#{version}/anki-#{version}-mac-#{arch}-qt6.dmg",
         verified: "github.com/ankitects/anki/"
