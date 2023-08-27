665b2a778393a997d57508a90eaf3ed4051467c1
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-27 19:35:44 -0400

    tabby 1.0.198

--- Casks/t/tabby.rb
+++ Casks/t/tabby.rb
@@ -1,9 +1,9 @@
 cask "tabby" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.0.197"
-  sha256 arm:   "eb24189e037d1436ab1947b1e8edf10d7af791f43cfda5983b3994ff76b2f373",
-         intel: "6e472a0e4788d4c79b95ae57d30d1afd4f72a6dab9e15bf3fd9d4399507e626c"
+  version "1.0.198"
+  sha256 arm:   "2bcedbb9666e88ecab2816d409a5c29a583d85aa8d1a1e44c9491649d9f2802a",
+         intel: "3201f07a812eb038ce65f44ed84c4053f20211bc6a4270af47cf88c7a8c3a892"
 
   url "https://github.com/Eugeny/tabby/releases/download/v#{version}/tabby-#{version}-macos-#{arch}.zip",
       verified: "github.com/Eugeny/tabby/"
