517a984cee7253b9409b7834eea17ed110fd8669
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-27 18:36:19 -0400

    devpod 0.3.5

--- Casks/d/devpod.rb
+++ Casks/d/devpod.rb
@@ -1,9 +1,9 @@
 cask "devpod" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.3.4"
-  sha256 arm:   "ed76446a47279d21c8e4b091f495d46c26dde56c27b58b514ef6804f28f781de",
-         intel: "c3a048db8c5cb3ec0183d936bcc63830410f8821bbd7405739a1a198d1ff9b1a"
+  version "0.3.5"
+  sha256 arm:   "44e39b1f8f7538680216fa55f8375015a82e526bd7e059928ecf95f77b7d56c1",
+         intel: "6aa0a5366642612520e6a34d0b4707f731730105ee1702a7a2c06a4ed8a46bc2"
 
   url "https://github.com/loft-sh/devpod/releases/download/v#{version}/DevPod_macos_#{arch}.dmg",
       verified: "github.com/loft-sh/devpod/"
