6b778f964325fa47c556dcc81f046672c1e5d67d
Bevan Kay <email@bevankay.me>
2023-08-28 14:03:05 +1000

    alex313031-thorium M115.0.5790.172

--- Casks/a/alex313031-thorium.rb
+++ Casks/a/alex313031-thorium.rb
@@ -1,39 +1,19 @@
 cask "alex313031-thorium" do
-  arch arm: "arm64", intel: "x64"
+  arch arm: "ARM", intel: "X64"
 
-  version "M114.0.5735.134-2"
-  sha256 arm:   "fa8a3fdfd0ae4ed09a39f94114cae2af56a21cd52d7c2315523cecd8f478b7ce",
-         intel: "ab48e8974012142e8c8e7f9b8dc5631eac89fbf376225f88274964dcc8732828"
+  version "M115.0.5790.172"
+  sha256 arm:   "052a4d9ee2cbfab78671aae2ca9972b3b481fd0d47641d7668c519aa26b8f800",
+         intel: "9ed769e69efe14decd37aa192f97a7b5a20fd038074cb217be2bc0d1b949fd4a"
 
-  url "https://github.com/Alex313031/Thorium-Special/releases/download/#{version}/Thorium_MacOS_#{arch}.dmg",
-      verified: "github.com/Alex313031/Thorium-Special/"
+  url "https://github.com/Alex313031/Thorium-MacOS/releases/download/#{version}/Thorium_MacOS_#{arch}.dmg",
+      verified: "github.com/Alex313031/Thorium-MacOS/"
   name "Thorium"
   desc "Web browser"
   homepage "https://thorium.rocks/"
 
-  # NOTE: This approach involves multiple requests and should be avoided
-  # whenever possible. If upstream starts reliably providing dmg files in every
-  # release, we should switch to `url :url` with `strategy :github_latest`.
   livecheck do
-    url "https://github.com/Alex313031/Thorium-Special/releases?q=prerelease%3Afalse"
-    regex(%r{/([^/]+?)/Thorium[._-]MacOS[._-]#{arch}\.dmg}i)
-    strategy :page_match do |page, regex|
-      # Collect the release tags on the page
-      tags = page.scan(%r{href=["']?[^"' >]*?/releases/tag/([^"' >]*?)["' >]}i)&.flatten&.uniq
-
-      max_reqs = 4
-      tags.each_with_index do |tag, i|
-        break if i >= max_reqs
-
-        # Fetch the assets list HTML for the tag and match within it
-        assets_page = Homebrew::Livecheck::Strategy.page_content(
-          @url.sub(%r{/releases/?.+}, "/releases/expanded_assets/#{tag}"),
-        )
-        matches = assets_page[:content]&.scan(regex)&.map { |match| match[0] }
-
-        break matches if matches.present?
-      end
-    end
+    strategy :git
+    regex(/^(M\d+(?:\.\d+)*)/i)
   end
 
   conflicts_with cask: "thorium"
