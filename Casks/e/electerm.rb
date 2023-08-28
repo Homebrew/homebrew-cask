a9d036cad5962915746749adf58318b7929859e6
zuisong <com.me@foxmail.com>
2023-08-28 09:48:02 +0800

    electerm 1.29.2

--- Casks/e/electerm.rb
+++ Casks/e/electerm.rb
@@ -1,9 +1,9 @@
 cask "electerm" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.28.4"
-  sha256 arm:   "43cc3219ad82027b94ba2f97062f62798d5780914cde837e3d49a67573b3188e",
-         intel: "b3280b172eb3b56ec0128bd73bd74947030a146206fada26ae4eadd89fa945b2"
+  version "1.29.2"
+  sha256 arm:   "b42171dd7a2870c7789d169048da5fab22c77a64ad9a9261ed33dc61f639c8ad",
+         intel: "226e68219e44c161b73d681fbfe565efdd21c194e5784d22989bbb7c391eced0"
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac-#{arch}.dmg"
   name "electerm"
