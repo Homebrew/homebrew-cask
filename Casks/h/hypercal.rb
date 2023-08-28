ec99941d59ca2b80042e59bac17048c384a1edd1
Hugo Ferreira <hugocf@users.noreply.github.com>
2023-08-28 18:42:58 +0100

    hypercal 0.0.96 (new cask)

---
+++ Casks/h/hypercal.rb
@@ -0,0 +1,11 @@
+cask "hypercal" do
+  version "0.0.96"
+  sha256 :no_check
+
+  url "https://updater-api.hypercal.app/download"
+  name "Hypercal"
+  desc "Privacy first calendar that connects your schedules and tasks into a single view"
+  homepage "https://hypercal.app/"
+
+  app "Hypercal.app"
+end
