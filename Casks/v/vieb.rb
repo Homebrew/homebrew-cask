fb8d3d4104c67b3dd9b7b5c242e086581be0add9
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-28 10:22:35 -0400

    vieb 10.3.0

--- Casks/v/vieb.rb
+++ Casks/v/vieb.rb
@@ -1,9 +1,9 @@
 cask "vieb" do
   arch arm: "arm64-"
 
-  version "10.2.0"
-  sha256 arm:   "bf9cf85ad9a0c787f2e42cad8399ee5bfe2ed47e668b75e27eedae41511776d9",
-         intel: "33794c87bda8dc55ce7d9764dac1f87bbb6951d8f465297a224992f8f61535e9"
+  version "10.3.0"
+  sha256 arm:   "ebf96151f3f406f88484aa532fad9124da53dca7d0f4c93a1f8fe83142d7b2de",
+         intel: "3782ed0b6eb22849504e862d6eeee040f2b8c5cb7a948365b607c1e6b6572808"
 
   url "https://github.com/Jelmerro/Vieb/releases/download/#{version}/Vieb-#{version}-#{arch}mac.zip",
       verified: "github.com/Jelmerro/Vieb/"
