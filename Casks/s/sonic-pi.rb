1780f70208d06330328cb253cbd24d52dbb1b0d9
Bevan Kay <email@bevankay.me>
2023-08-29 14:08:49 +1000

    sonic-pi: update to `on_system` blocks

--- Casks/s/sonic-pi.rb
+++ Casks/s/sonic-pi.rb
@@ -25,7 +25,7 @@ cask "sonic-pi" do
     version "4.3.0"
     sha256 "c5646b221d61ba55c8e1025a646718d1244333bd57e2a7bccc8eb71c5a7be585"
 
-    url "https://sonic-pi.net/files/releases/v#{version}/Sonic-Pi-for-Mac-x64-v#{version.dots_to_hyphens}.dmg"
+    url "https://sonic-pi.net/files/releases/v#{version}/Sonic-Pi-for-Intel-Mac-x64-v#{version.dots_to_hyphens}.dmg"
 
     livecheck do
       skip "Legacy version"
