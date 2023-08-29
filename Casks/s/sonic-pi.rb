bbab58f41ce2bc1732f8499f469bf5c325c6b5f2
Bevan Kay <email@bevankay.me>
2023-08-29 14:07:35 +1000

    sonic-pi: update to `on_system` blocks

--- Casks/s/sonic-pi.rb
+++ Casks/s/sonic-pi.rb
@@ -6,24 +6,40 @@ cask "sonic-pi" do
     sha256 "0bfd12f930311e8ef1c7306dc9c012cfcc1f8e50710fd26a8c18ba003573a506"
 
     url "https://sonic-pi.net/files/releases/v#{version}/Sonic-Pi-for-Mac-x64-v#{version.dots_to_hyphens}.dmg"
+
+    livecheck do
+      skip "Legacy version"
+    end
   end
   on_catalina do
     version "4.3.0"
     sha256 "c5646b221d61ba55c8e1025a646718d1244333bd57e2a7bccc8eb71c5a7be585"
 
     url "https://sonic-pi.net/files/releases/v#{version}/Sonic-Pi-for-Mac-x64-v#{version.dots_to_hyphens}.dmg"
+
+    livecheck do
+      skip "Legacy version"
+    end
   end
   on_big_sur do
     version "4.3.0"
     sha256 "c5646b221d61ba55c8e1025a646718d1244333bd57e2a7bccc8eb71c5a7be585"
 
     url "https://sonic-pi.net/files/releases/v#{version}/Sonic-Pi-for-Mac-x64-v#{version.dots_to_hyphens}.dmg"
+
+    livecheck do
+      skip "Legacy version"
+    end
   end
   on_monterey do
     version "4.4.0"
     sha256 "7ed0e94cf92fdf2e8d51ee42c5f7c3478fbcef9630eb12dccf203f5e72bb517f"
 
     url "https://sonic-pi.net/files/releases/v#{version}/Sonic-Pi-for-Mac-x64-v#{version.dots_to_hyphens}.dmg"
+
+    livecheck do
+      skip "Legacy version"
+    end
   end
   on_ventura :or_newer do
     arch_bonus = on_arch_conditional intel: "-Intel"
