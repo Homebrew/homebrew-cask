b153ee80e62cef2da3f559d3e819a17b1b3a55ca
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-29 09:13:43 -0400

    kiwi-for-gmail 4.0.3

--- Casks/k/kiwi-for-gmail.rb
+++ Casks/k/kiwi-for-gmail.rb
@@ -1,5 +1,5 @@
 cask "kiwi-for-gmail" do
-  version "4.0.2,2560"
+  version "4.0.3,2569"
   sha256 :no_check
 
   url "https://downloads.kiwiforgmail.com/kiwi/release/consumer/Kiwi+for+Gmail+Setup.dmg"
