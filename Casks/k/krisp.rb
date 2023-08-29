cce46ac7d0f49e7cd700c8205d3e1439c95198cc
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-29 09:14:09 -0400

    krisp 2.24.5

--- Casks/k/krisp.rb
+++ Casks/k/krisp.rb
@@ -1,9 +1,9 @@
 cask "krisp" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.23.4"
-  sha256 arm:   "2f6efc75737f39c1c7082bf9ce26e3a68972748321e5a7b507447a4cf931174b",
-         intel: "373e62e7588e2bfa4fda59144f7936fe5c908337d222c441da84de9617ddf8d4"
+  version "2.24.5"
+  sha256 arm:   "a5a346d842f1caf35f3265651d7c55c166c7a6826f9ab7901db571a2f7e727a7",
+         intel: "5ea275c1861b2893737d25a41626ce2569d372e7faedac363ea76a779f55ee48"
 
   url "https://cdn.krisp.ai/mp/#{version}/mac/krisp_#{version}_#{arch}.pkg"
   name "Krisp"
