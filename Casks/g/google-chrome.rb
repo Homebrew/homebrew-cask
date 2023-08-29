815c89963eba8acac84282a90fb11ea126d6f028
Mikl Wolfe <wolfemikl@gmail.com>
2023-08-29 12:44:49 -0600

    google-chrome 116.0.5845.140

--- Casks/g/google-chrome.rb
+++ Casks/g/google-chrome.rb
@@ -1,5 +1,5 @@
 cask "google-chrome" do
-  version "116.0.5845.110"
+  version "116.0.5845.140"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
