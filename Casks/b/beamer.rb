5a145dcd9e2f03ac32016dfececedf96782a6128
Jacob Ford <j@cobford.com>
2023-08-29 14:04:11 -0400

    beamer: acquired by Softorino
    https://www.youtube.com/watch\?v\=wkolPg6cwUg

--- Casks/b/beamer.rb
+++ Casks/b/beamer.rb
@@ -1,15 +1,15 @@
 cask "beamer" do
-  version "3.5"
-  sha256 "8f8d72ab99fd47c7b9df1acd51c6fcc8b6ebb8c574587e8e1fc6cdc5868ba21c"
+  version "4.1.17"
+  sha256 "3f1b9fec947cbc91ad8b46344e57fb39db8d06147e077f178db7f939703dc763"
 
-  url "https://beamer-app.com/resources/downloads/Beamer-#{version}.zip"
+  url "https://ushining.softorino.com/shine_uploads/beamermac_#{version}.dmg"
   name "Beamer"
   desc "Desktop casting/streaming app for Apple TV and Chromecast"
-  homepage "https://beamer-app.com/"
+  homepage "https://softorino.com/beamer/"
 
   livecheck do
-    url "https://beamer-app.com/beamer#{version.major}-appcast.xml"
-    strategy :sparkle, &:short_version
+    url "https://ushining.softorino.com/appcast.php?abbr=bmrm"
+    strategy :sparkle
   end
 
   app "Beamer.app"
