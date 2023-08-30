37c5c0ebacdd8747c68da273af02048f662f2b53
Miccal Matthews <miccal.matthews@gmail.com>
2023-08-31 06:57:48 +0800

    Update sparkplate.rb

--- Casks/s/sparkplate.rb
+++ Casks/s/sparkplate.rb
@@ -3,9 +3,11 @@ cask "sparkplate" do
   sha256 "f9bff9256c4c95bf1a53b9c074e6a3f44d35e75043a404f306c1bd09121e93e7"
 
   url "https://github.com/GreenfireInc/homebrew-sparkplate/releases/download/v#{version}/Sparkplate.zip"
-  name "sparkplate"
+  name "Sparkplate"
   desc "Features a test page for resolving human readable domains to crypto addresses"
   homepage "https://github.com/GreenfireInc/Sparkplate.Vue"
+
+  depends_on macos: ">= :high_sierra"
 
   app "Sparkplate.app"
 
