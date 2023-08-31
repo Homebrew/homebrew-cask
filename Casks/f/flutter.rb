bcbf3d50320506ca5665feaffed58f78aa964328
c1yde3 <c1yde3@outlook.com>
2023-08-31 13:02:52 +0800

    flutter 3.13.2

--- Casks/f/flutter.rb
+++ Casks/f/flutter.rb
@@ -1,9 +1,9 @@
 cask "flutter" do
   arch arm: "_arm64"
 
-  version "3.13.1"
-  sha256 arm:   "c47f3f2846cc62bb8da568e47d2afff02c947e1b07c83154debc96cc35de6474",
-         intel: "2d8da51a64a83c01c0817a45b957f97ef58e7942713e3253646abbfc25a55db0"
+  version "3.13.2"
+  sha256 arm:   "cbe731697658ccebc4cbbd7d32da5f18d9e51afe193cf0279d0cf6720b1ab09f",
+         intel: "64a8cf741b492adddbf06763c84ec2e72a70ae908806916b50e3fc9f8ee8e3d7"
 
   url "https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos#{arch}_#{version}-stable.zip",
       verified: "storage.googleapis.com/flutter_infra_release/releases/stable/macos/"
