fc5c834b6db8e31829ef2310b6db4471c3ab0d92
ShredderWalnut <34428332+ShredderWalnut@users.noreply.github.com>
2023-08-31 09:01:50 +0000

    Update podolski.rb

--- Casks/p/podolski.rb
+++ Casks/p/podolski.rb
@@ -2,7 +2,7 @@ cask "podolski" do
   version "1.2.3,12092"
   sha256 "1051d7d3af520ff8131ceae85f39ae61f90a187b9dafe7afe8d2cf28cdec868a"
 
-  url "https://dl.u-he.com/releases/Podolski_#{version.csv.first.no_dots}_#{version.csv.second}_Mac.zip",
+  url "https://dl.u-he.com/releases/Podolski_#{version.csv.first.no_dots}_#{version.csv.second}_Mac.zip"
   name "Podolski"
   desc "Virtual analogue synthesizer"
   homepage "https://u-he.com/products/podolski/"
