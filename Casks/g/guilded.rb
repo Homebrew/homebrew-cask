bef41bed5f42975322ad379aad4864477c5f1ebe
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-31 11:24:14 -0400

    guilded 1.0.9218125

--- Casks/g/guilded.rb
+++ Casks/g/guilded.rb
@@ -1,6 +1,6 @@
 cask "guilded" do
-  version "1.0.9199744"
-  sha256 "20e96f4992edda79ec8b47a7e3053133394b4218f5d2c4e6f0f92633783d5392"
+  version "1.0.9218125"
+  sha256 "74c0f45e69182f4de61adacd6282502c97aeab4320034f06388eb2cafbc2445e"
 
   url "https://s3-us-west-2.amazonaws.com/www.guilded.gg/AppBuilds/mac/Guilded-#{version}-release.dmg",
       verified: "s3-us-west-2.amazonaws.com/www.guilded.gg/AppBuilds/mac/"
