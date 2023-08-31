9d5cea6026354bec946b615ae2c3500a829397cb
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-08-31 14:26:37 -0400

    saoimageds9: drop legacy support
    * Mojave download no longer exists, therefore removed
    
    * Catalina no longer receiving version updates

--- Casks/s/saoimageds9.rb
+++ Casks/s/saoimageds9.rb
@@ -4,26 +4,8 @@ cask "saoimageds9" do
   # NOTE: "9" is not a version number, but an intrinsic part of the product name
   version "8.5"
 
-  on_mojave :or_older do
-    sha256 "bfe57aa62f1c36ec6ba00e6423bf84b2ce1b34d1af60dffc3adc141caa67d878"
-
-    url "https://ds9.si.edu/download/macosmojave/SAOImageDS9%20#{version}.dmg",
-        verified: "ds9.si.edu/download/",
-        using:    :homebrew_curl
-
-    depends_on macos: :mojave
-  end
-  on_catalina do
-    sha256 "a076aa60f206297ea8f926ba7b267b368ff9e1f0b50f1c7543aa021fc9bfad17"
-
-    url "https://ds9.si.edu/download/macoscatalina/SAOImageDS9%20#{version}.dmg",
-        verified: "ds9.si.edu/download/",
-        using:    :homebrew_curl
-
-    depends_on macos: :catalina
-  end
   on_big_sur do
-    sha256 "b48565cdaadca1c1363173fac685cd75a68351c914792b2f43c6cb66b3316eed"
+    sha256 "46917bdab7fd22cb4cfd85145e9813c59e03dc1468f961172ad5ca396616fbb4"
 
     url "https://ds9.si.edu/download/macosbigsurx86/SAOImageDS9%20#{version}.dmg",
         verified: "ds9.si.edu/download/",
@@ -32,8 +14,8 @@ cask "saoimageds9" do
     depends_on macos: :big_sur
   end
   on_monterey do
-    sha256 arm:   "bbee664d4b4c3abab017c7971b92c13071161aa30fff95d2dfa9b1c1f4691a67",
-           intel: "32ad574b38e589417cfec7ca34aba52a126d71efd512bcb5145c268aaaa2e4b6"
+    sha256 arm:   "309367b8cfc45eeb481e1cddbb03c3bb615d17eca83fb6c215755bef1e2a9205",
+           intel: "473bf1c8421f1120bfa39543a469b91f95ae7509807f911ee44f5e69868424ec"
 
     url "https://ds9.si.edu/download/macosmonterey#{arch}/SAOImageDS9%20#{version}.dmg",
         verified: "ds9.si.edu/download/"
