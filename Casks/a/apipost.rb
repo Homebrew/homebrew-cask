0f954b49685cc6a8ca9ef21ffe2d62ec5ce27716
zyl <zyl@zhangyl.com>
2023-09-01 20:41:00 +0800

    apipost 7.2.1

--- Casks/a/apipost.rb
+++ Casks/a/apipost.rb
@@ -1,9 +1,9 @@
 cask "apipost" do
   arch arm: "arm64", intel: "x64"
 
-  version "7.1.9"
-  sha256 arm:   "2563c656854d561a2fc7b9f27a3d036285d85b4e87594ffbe8c5d8dfc7c09ba4",
-         intel: "3eab9c11adfa90e8a796be6f038da0267641b7ad6033f3a768ac4f80c73a72af"
+  version "7.2.1"
+  sha256 arm:   "e2563694bea3fbd6ea847ecdfa1e5de26931c0b9ed1409acf38282e55c760512",
+         intel: "5c77ba8c9673cc2b59f945a756b614e1171fca9eb4d2e739c486493c31424e5e"
 
   url "https://www.apipost.cn/dl.php?client=Mac&arch=#{arch}&version=#{version}"
   name "Apipost"
