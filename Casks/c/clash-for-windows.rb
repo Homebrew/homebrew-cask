603ce9e431a37713eb1b4c23e55ca052d8565cea
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-01 08:32:15 -0400

    clash-for-windows 0.20.33

--- Casks/c/clash-for-windows.rb
+++ Casks/c/clash-for-windows.rb
@@ -1,9 +1,9 @@
 cask "clash-for-windows" do
   arch arm: "-arm64"
 
-  version "0.20.32"
-  sha256 arm:   "5606ea55131220606ec428d429b4f53162866f6b93afa4efdc99c6c8d839fe7a",
-         intel: "25aa70191306528dc41efecd8f977b7560fa27adbea884073dbf68db3b8d50d7"
+  version "0.20.33"
+  sha256 arm:   "2b914d29f689c2ca9bacff4f1a92aa74bd9b1dec70da3ab6132b0e9674f596cc",
+         intel: "22fe7349c389e3895aa2f707eb1827a9c7dc8f77f817c1a8e307c42777c81326"
 
   url "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/#{version}/Clash.for.Windows-#{version}#{arch}.dmg"
   name "Clash for Windows"
