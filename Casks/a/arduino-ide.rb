50f4e437790dc77f38d15de014026f8e8ea1386f
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-01 08:30:30 -0400

    arduino-ide 2.2.1

--- Casks/a/arduino-ide.rb
+++ Casks/a/arduino-ide.rb
@@ -1,9 +1,9 @@
 cask "arduino-ide" do
   arch arm: "ARM64", intel: "64bit"
 
-  version "2.2.0"
-  sha256 arm:   "c5b556d94630c65d68ce6c7f6bc9d51a6ae9cb338b93360ce8aed9a58dec7b84",
-         intel: "1b37489ac9408006a7617b49906a4ad0ee4a19ce8770bb72312ff1b148a6f2f5"
+  version "2.2.1"
+  sha256 arm:   "ddab9126d8fda9dd29b241c37acfa32c13b8d3c4c02eb7b5cb2b25e6cdd65a5e",
+         intel: "cc7ae3709f8fcc921b7e90bb70139dd56b340741edea743c4e5b9d8ce838d521"
 
   url "https://github.com/arduino/arduino-ide/releases/download/#{version}/arduino-ide_#{version}_macOS_#{arch}.dmg",
       verified: "github.com/arduino/arduino-ide/"
