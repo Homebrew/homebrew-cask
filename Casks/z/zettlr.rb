470c566b48daec58843a59e5c7ac01f4a2e8b157
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-01 09:51:07 -0400

    zettlr 3.0.0

--- Casks/z/zettlr.rb
+++ Casks/z/zettlr.rb
@@ -1,9 +1,9 @@
 cask "zettlr" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.3.0"
-  sha256 arm:   "0236d873d0d1477e480334fe8e68b795a8a7eb61cdbbf72d300143773e98955d",
-         intel: "73ee8fbcf481b1cf68a3456f3c215be8a7d8835342220cc44fba6fa0440f37fc"
+  version "3.0.0"
+  sha256 arm:   "dfd83093d395a0d6a4f4efe096b08b5ac26c76cea9195776b833474ad3f34839",
+         intel: "cc1fcdd35846023ae2e2baca85bf5c67e1bb57e35347e12268e4cfd382a79047"
 
   url "https://github.com/Zettlr/Zettlr/releases/download/v#{version}/Zettlr-#{version}-#{arch}.dmg"
   name "Zettlr"
