1d346fee641ab2d372c4e441df4474cd7dc9541c
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-01 09:23:47 -0400

    redisinsight 2.32

--- Casks/r/redisinsight.rb
+++ Casks/r/redisinsight.rb
@@ -1,7 +1,7 @@
 cask "redisinsight" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.30.0"
+  version "2.32"
   sha256 :no_check
 
   url "https://download.redisinsight.redis.com/latest/RedisInsight-v#{version.major}-mac-#{arch}.dmg"
