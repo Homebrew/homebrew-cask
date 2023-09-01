da1de06185adc452c8714b7be576959fa2be931f
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-01 09:08:39 -0400

    oscar 1.5.0

--- Casks/o/oscar.rb
+++ Casks/o/oscar.rb
@@ -1,6 +1,6 @@
 cask "oscar" do
-  version "1.4.0"
-  sha256 "edee3693bcf0804f1ddb912031dc7a2940c48abc01909a6d5a1beef94f329ea1"
+  version "1.5.0"
+  sha256 "3ec1ba2f8ab0234964106bf02a7ba11a97de9f09fe3ec2cca9c1eeeac30af846"
 
   url "https://www.apneaboard.com/OSCAR/OSCAR-#{version}.dmg",
       verified: "apneaboard.com/OSCAR/"
