948150075eea398614431582445c9932423fc13f
John Pitchko <john.pitchko@icloud.com>
2023-09-01 09:12:31 -0600

    Remove comment

--- Casks/r/rippling.rb
+++ Casks/r/rippling.rb
@@ -1,6 +1,5 @@
 cask "rippling" do
   version :latest
-  # sha256 "36e900f896ec932fa1782867341fd12bf46914f09dda4282a545ff9feef0adfe"
   sha256 :no_check
 
   url "https://hardware-cdn.rippling.com/device_release/electron-installer/3.6.2/macos/Rippling.dmg"
