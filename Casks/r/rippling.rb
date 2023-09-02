2c7f18d3b2c23cb3025228bca71660490ac5d463
Bevan Kay <email@bevankay.me>
2023-09-02 10:14:43 +1000

    add livecheck

--- Casks/r/rippling.rb
+++ Casks/r/rippling.rb
@@ -1,11 +1,16 @@
 cask "rippling" do
-  version :latest
-  sha256 :no_check
+  version "3.6.3"
+  sha256 "a6277f7c3a5c7c683f3cb836531f14117d322f2027a4af6e74b734b880f430c9"
 
-  url "https://hardware-cdn.rippling.com/device_release/electron-installer/3.6.2/macos/Rippling.dmg"
-  name "rippling"
+  url "https://hardware-cdn.rippling.com/device_release/electron-installer/#{version}/macos/Rippling.dmg"
+  name "Rippling"
   desc "MDM for Rippling"
-  homepage "https://www.rippling.com/"
+  homepage "https://www.rippling.com/device-management"
+
+  livecheck do
+    url "https://app.rippling.com/api/hardware/general_installer/?os=mac"
+    strategy :header_match
+  end
 
   app "Rippling.app"
 end
