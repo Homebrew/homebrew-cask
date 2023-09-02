36813fe8f5386e5fcef00bfe4a4d073bc0bc195d
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-02 19:56:49 -0400

    dcp-o-matic-editor 2.16.63

--- Casks/d/dcp-o-matic-editor.rb
+++ Casks/d/dcp-o-matic-editor.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-editor" do
-  version "2.16.62"
-  sha256 "ce73d66014c359c1985602a697d589fb09b47eeb43aeffe008b39537fd4ea85c"
+  version "2.16.63"
+  sha256 "f184661041ad7c75ee10e95e924bc16df2eb63c6cd3ae2a3721580b92b07afda"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-editor&version=#{version}"
   name "DCP-o-matic Editor"
