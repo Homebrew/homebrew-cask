5f5e201cf322b630af98856ddc5e08f882c0722d
Rui Chen <rui@chenrui.dev>
2023-09-03 16:45:52 -0400

    utools 4.0.1

--- Casks/u/utools.rb
+++ Casks/u/utools.rb
@@ -1,9 +1,9 @@
 cask "utools" do
   arch arm: "-arm64"
 
-  version "4.0.0"
-  sha256 arm:   "4f4265eafd43eef07c45d6cceb65cafdfe42babee71f51ab95426724875e2983",
-         intel: "103acc3e44483ac40c4ab13171c17cf1627cd244184cd40ca49f5bb86df9fa4d"
+  version "4.0.1"
+  sha256 arm:   "f4f2db0f7a065937a374c603a1f29ea9c7b06f5c222a664aceac73442ea278fc",
+         intel: "21878a7b2011df37d4a07ba4c73a946f8873f68c4fbf28e040b9c3c2f111d080"
 
   url "https://publish.u-tools.cn/version2/uTools-#{version}#{arch}.dmg",
       verified: "publish.u-tools.cn/"
