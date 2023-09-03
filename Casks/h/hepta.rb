24bec11b79c2fff775638302e0ada64e8718168f
Rui Chen <rui@chenrui.dev>
2023-09-03 19:30:14 -0400

    hepta 1.0.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/h/hepta.rb
+++ Casks/h/hepta.rb
@@ -1,11 +1,11 @@
 cask "hepta" do
-  arch arm: "arm64-mac", intel: "mac"
+  arch arm: "-arm64"
 
-  version "0.371.0"
-  sha256 arm:   "c12e4c68c8f0a60b14eac0cbe482965058f45890efdd2fb05380f88f3aba0566",
-         intel: "dca2310654419d021320d0575f99ff79c3a2986059c0d2da5b5e6066d7ce668b"
+  version "1.0.0"
+  sha256 arm:   "a46d04472dc6084ba2fb337960bdcd877d0a0902ca34a793c3204463d472a5f1",
+         intel: "323e6b6516723e9046267c247fc6c33f8beb0233bc2c58e355ecd03f03b7a2f7"
 
-  url "https://github.com/heptameta/project-meta/releases/download/v#{version}/Hepta-#{version}-#{arch}.zip",
+  url "https://github.com/heptameta/project-meta/releases/download/v#{version}/Hepta-#{version}#{arch}.zip",
       verified: "github.com/heptameta/project-meta/"
   name "Hepta"
   desc "Note-taking tool for visual learning"
