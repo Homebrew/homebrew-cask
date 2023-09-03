0ef9951d83985d8663454cfd1d3c31901433e508
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-09-03 18:19:37 -0400

    devpod 0.3.7

--- Casks/d/devpod.rb
+++ Casks/d/devpod.rb
@@ -1,9 +1,9 @@
 cask "devpod" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.3.6"
-  sha256 arm:   "9745e271702987c44f12d3e0de8f06b4640e4e1ce3519ed9884fd4293339df71",
-         intel: "ee0dab07e09bce65516c3e108b9dea1d1022cf600130e63e7b23e6e489945f13"
+  version "0.3.7"
+  sha256 arm:   "c8e03f04e2d1a0f5a42118989d69f33183e694a6bf071e1fe3defbb484cfebed",
+         intel: "9a0cbbe9e7b6e75760d4a739b5855d31eaeb4444e21d13cb8f5155393a04ac32"
 
   url "https://github.com/loft-sh/devpod/releases/download/v#{version}/DevPod_macos_#{arch}.dmg",
       verified: "github.com/loft-sh/devpod/"
