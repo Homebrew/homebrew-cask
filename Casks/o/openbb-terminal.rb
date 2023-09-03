bd01887cc54d443ba2d2cb5362a6b2c072426761
Rui Chen <rui@chenrui.dev>
2023-09-03 19:23:42 -0400

    openbb-terminal 3.2.2
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    openbb-terminal: update arch name for x86.64
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/o/openbb-terminal.rb
+++ Casks/o/openbb-terminal.rb
@@ -1,9 +1,10 @@
 cask "openbb-terminal" do
-  arch arm: "ARM64", intel: "x86.64"
+  # raised an issue about the x86.64 typo, https://github.com/OpenBB-finance/OpenBBTerminal/issues/5405
+  arch arm: "ARM64", intel: "x86.84"
 
-  version "3.2.1"
-  sha256 arm:   "5299fcc49fde86e581baceeb008499257fdadf84274c7502ac0e7ddb9063b8fc",
-         intel: "8ff7fa349dd87db6cdcc9d012a959d84aa16e9049dbdca83555d3ad044351055"
+  version "3.2.2"
+  sha256 arm:   "63706c56c1d264dd8b98957cf62d3d1689a8edb684d26c4f7a0bf1b1eb1335fc",
+         intel: "6fe94ec142155f6abf8dfcb77dda834454720594d85a3292b0772b8b27472247"
 
   url "https://github.com/OpenBB-finance/OpenBBTerminal/releases/download/v#{version}/#{arch}.MacOS.OpenBB.Terminal.v#{version}.pkg",
       verified: "github.com/OpenBB-finance/OpenBBTerminal/"
