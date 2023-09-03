77a0e29a0d95a3ec08042388e81d29651da43449
Rui Chen <rui@chenrui.dev>
2023-09-03 18:56:53 -0400

    portfolioperformance 0.65.2

--- Casks/p/portfolioperformance.rb
+++ Casks/p/portfolioperformance.rb
@@ -1,9 +1,9 @@
 cask "portfolioperformance" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.65.1"
-  sha256 arm:   "08643cfbb3f06037ce15b3a0d445b2d0a87adcb820876b98d6446d6ca33e994d",
-         intel: "e59a8bf18a0685a986ccbb2e53a3b47aab40f1bf89e631bf7af35b70a7ee0419"
+  version "0.65.2"
+  sha256 arm:   "c09714f43cd8d3bf6f96246fd5e275caed470e5c16f9c9a887c4380648043eab",
+         intel: "c6b89cf58a4918bc22a3e36f48d02fc41238c5d52dbf0bfa30406bc32f1401b5"
 
   url "https://github.com/buchen/portfolio/releases/download/#{version}/PortfolioPerformance-#{version}-#{arch}.dmg",
       verified: "github.com/buchen/portfolio/"
