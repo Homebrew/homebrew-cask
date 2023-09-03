3f699551dbd26f626f9f694565616cf323c611a6
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-02 20:21:04 -0400

    openmsx 19_1

--- Casks/o/openmsx.rb
+++ Casks/o/openmsx.rb
@@ -1,6 +1,6 @@
 cask "openmsx" do
-  version "19_0"
-  sha256 "f247b515a0dd9864906044fac564d04cbcf6345167cd0275117bbf859fbd36ab"
+  version "19_1"
+  sha256 "e81a31cd19b1fdf029b08b55879fef6793cb0bbcbdc79e6e4cc8892f64d82d8b"
 
   url "https://github.com/openMSX/openMSX/releases/download/RELEASE_#{version}/openmsx-#{version.underscores_to_dots}-mac-x86_64-bin.dmg",
       verified: "github.com/openMSX/openMSX/"
