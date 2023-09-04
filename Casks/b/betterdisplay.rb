13d8b928c83a1bab88d7369274e745c2bc43e60b
BingoKingo <24961267+BingoKingo@users.noreply.github.com>
2023-09-04 21:37:24 +0800

    Update betterdisplay.rb

--- Casks/b/betterdisplay.rb
+++ Casks/b/betterdisplay.rb
@@ -1,6 +1,6 @@
 cask "betterdisplay" do
   on_big_sur :or_older do
-    version "1.4.1"
+    version "1.4.15"
     sha256 "26a75c3a4e95b076dcb7468e6ce9f9493675e4a9676fd267e5b32459db900077"
 
     livecheck do
