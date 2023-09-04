f7e952f7b5786e77af4aea7130387368cc2402bd
Rui Chen <rui@chenrui.dev>
2023-09-04 11:12:02 -0400

    mysteriumvpn 10.16.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/m/mysteriumvpn.rb
+++ Casks/m/mysteriumvpn.rb
@@ -1,8 +1,8 @@
 cask "mysteriumvpn" do
-  version "10.14.4"
-  sha256 "d82c2657c4ba550a93858a3b4658eb439b09ea9802da99e37b7367d5bf42e15f"
+  version "10.16.0"
+  sha256 "f82135d0d86ac8f567783063a42fb87c1c98bfb76bb38e92d0929cd65b4fe4eb"
 
-  url "https://github.com/mysteriumnetwork/mysterium-vpn-desktop/releases/download/#{version}/MysteriumVPN-#{version}-universal.dmg",
+  url "https://github.com/mysteriumnetwork/mysterium-vpn-desktop/releases/download/#{version}/MysteriumDark-#{version}-universal.dmg",
       verified: "github.com/mysteriumnetwork/mysterium-vpn-desktop/"
   name "Mysterium VPN"
   desc "VPN client"
