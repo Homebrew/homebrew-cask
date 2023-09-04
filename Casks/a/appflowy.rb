28f1a50cfc262ed25c819c42b60c72a522cb5359
Steven Reina <stevenreina@proton.me>
2023-09-04 16:08:48 +0200

    appflowy 0.3.1

--- Casks/a/appflowy.rb
+++ Casks/a/appflowy.rb
@@ -1,6 +1,6 @@
 cask "appflowy" do
-  version "0.3.0"
-  sha256 "af4777470b35f6cc2a09edf72573bad8be8efb71f03058445f65038b62e389be"
+  version "0.3.1"
+  sha256 "4a5f67cf971bed23dad3c6fab878e31d4ccf49de8c27fc10bfe636d54f8fe157"
 
   url "https://github.com/AppFlowy-IO/AppFlowy/releases/download/#{version}/Appflowy_#{version}_macos-universal.zip",
       verified: "github.com/AppFlowy-IO/AppFlowy/"
