d61a66d404cf46e9e4f91a48648e7e25100d3949
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-09-03 21:14:28 -0400

    milkytracker 1.04.00

--- Casks/m/milkytracker.rb
+++ Casks/m/milkytracker.rb
@@ -1,12 +1,22 @@
 cask "milkytracker" do
-  version "1.03.00"
-  sha256 "21c7261805e800602843a575b55993d87530aa075cc4ee4bd3978e30e3a2bc29"
+  arch arm: "arm64", intel: "x86_64"
 
-  url "https://github.com/milkytracker/MilkyTracker/releases/download/v#{version}/milkytracker-#{version}-macos.dmg",
+  version "1.04.00"
+  sha256 arm:   "e3a1fcead7c6149151042ee75985219ec1893be62f2e789887608c08c2b65c3d",
+         intel: "078b341100147d28661c9cfb61ca496e2890e8affc4df2434cb2c5aa5fc26bf3"
+
+  url "https://github.com/milkytracker/MilkyTracker/releases/download/v#{version}/milkytracker-#{version}-Darwin-#{arch}.dmg",
       verified: "github.com/milkytracker/MilkyTracker/"
   name "MilkyTracker"
   desc "Music tracker compatible with FT2"
   homepage "https://milkytracker.titandemo.org/"
 
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
   app "MilkyTracker.app"
+
+  zap trash: "~/Library/Preferences/com.Titan.MilkyTracker.plist"
 end
