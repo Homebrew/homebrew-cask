6c02831e57ad57990d2e22ff7b99a53fce3335e6
Miccal Matthews <miccal.matthews@gmail.com>
2023-09-05 13:30:28 +0800

    Update cord.rb

--- Casks/c/cord.rb
+++ Casks/c/cord.rb
@@ -2,8 +2,7 @@ cask "cord" do
   version "0.5.7"
   sha256 "8f505b12b94167100b3b8b44ed3cee32ffcc94b73dc44fe0ecc896151f114100"
 
-  url "https://downloads.sourceforge.net/cord/cord/#{version.csv.first}/CoRD_#{version}.zip",
-      verified: "downloads.sourceforge.net/cord/"
+  url "https://downloads.sourceforge.net/cord/cord/#{version.csv.first}/CoRD_#{version}.zip"
   name "CoRD"
   desc "Remote desktop client"
   homepage "https://cord.sourceforge.net/"
