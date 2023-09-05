d7511b2e90d662a9ba230420b1fa532b24bd48d7
Miccal Matthews <miccal.matthews@gmail.com>
2023-09-05 13:17:34 +0800

    Update brain-workshop.rb

--- Casks/b/brain-workshop.rb
+++ Casks/b/brain-workshop.rb
@@ -2,8 +2,7 @@ cask "brain-workshop" do
   version "4.8.4"
   sha256 "b795220306862f55e79e3de9c8cf8b4e8667e7bb1b0e3f59604b4d5f29f3f623"
 
-  url "https://downloads.sourceforge.net/brainworkshop/brainworkshop/Brain%20Workshop%20#{version.major_minor}/brainworkshop-#{version}-MacOSX.zip",
-      verified: "downloads.sourceforge.net/brainworkshop/"
+  url "https://downloads.sourceforge.net/brainworkshop/brainworkshop/Brain%20Workshop%20#{version.major_minor}/brainworkshop-#{version}-MacOSX.zip"
   name "Brain Workshop"
   homepage "https://brainworkshop.sourceforge.net//"
 
