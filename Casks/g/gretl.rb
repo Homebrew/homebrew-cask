7bf716dbbb805c119e989eb7a1d603c2b5da5adb
Miccal Matthews <miccal.matthews@gmail.com>
2023-09-05 13:21:08 +0800

    Update gretl.rb

--- Casks/g/gretl.rb
+++ Casks/g/gretl.rb
@@ -2,11 +2,10 @@ cask "gretl" do
   version "2023b"
   sha256 "307c0d6ae748c80f9a64f12c827523b6fd7ac32265cb94594afc8a5f45d71cda"
 
-  url "https://downloads.sourceforge.net/gretl/gretl-#{version}-macos-intel.pkg",
-      verified: "downloads.sourceforge.net/gretl/"
+  url "https://downloads.sourceforge.net/gretl/gretl-#{version}-macos-intel.pkg"
   name "gretl"
   desc "Software package for econometric analysis"
-  homepage "https://gretl.sourceforge.net//"
+  homepage "https://gretl.sourceforge.net/"
 
   livecheck do
     url "https://gretl.sourceforge.net/osx.html"
