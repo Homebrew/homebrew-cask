b1bb5f1a120530893d05f2dd01f818f66d32b452
Miccal Matthews <miccal.matthews@gmail.com>
2023-09-05 13:18:57 +0800

    Update camed.rb

--- Casks/c/camed.rb
+++ Casks/c/camed.rb
@@ -2,11 +2,10 @@ cask "camed" do
   version "3.2.2"
   sha256 "407e101a3a47566395606b03114388f94f48b488d3fa27041aecb937eb43bd12"
 
-  url "https://downloads.sourceforge.net/camprocessor/CAMEd-#{version}-macosx-cocoa-x86_64.tar.gz",
-      verified: "downloads.sourceforge.net/camprocessor/"
+  url "https://downloads.sourceforge.net/camprocessor/CAMEd-#{version}-macosx-cocoa-x86_64.tar.gz"
   name "CAM Editor"
   desc "XML editor"
-  homepage "https://sourceforge.net/projects/camprocessor//"
+  homepage "https://sourceforge.net/projects/camprocessor/"
 
   app "CAMEd-#{version}/CAMed.app"
 end
