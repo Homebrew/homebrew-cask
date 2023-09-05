27798a4e26c485fb3e0d8423d6dd970341c11d47
ZhengJin <fangzhengjin@gmail.com>
2023-09-05 14:44:54 +0800

    Update wpsoffice-cn.rb

--- Casks/w/wpsoffice-cn.rb
+++ Casks/w/wpsoffice-cn.rb
@@ -1,8 +1,11 @@
 cask "wpsoffice-cn" do
-  version "5.5.1,7991"
-  sha256 "d655522da7a24363b390192d007f37317ba28c6b0200b2479441fefe7e35ab8d"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://package.mac.wpscdn.cn/mac_wps_pkg/#{version.csv.first}/WPS_Office_#{version.csv.first}(#{version.csv.second})_universal.dmg",
+  version "6.1.0,8274"
+  sha256 arm:   "f03712628787299d3b1304add7f6588b5844cafdfb28da3685b8120c14f1c348",
+         intel: "14598438295838fc20518323303b61a75a15edc09321c095bd900f169e3d59d5"
+
+  url "https://package.mac.wpscdn.cn/mac_wps_pkg/#{version.csv.first}/WPS_Office_#{version.csv.first}(#{version.csv.second})_#{arch}.dmg",
       verified: "package.mac.wpscdn.cn/mac_wps_pkg/"
   name "WPS Office"
   desc "All-in-one office service platform in Chinese"
@@ -10,7 +13,7 @@ cask "wpsoffice-cn" do
 
   livecheck do
     url :homepage
-    regex(/href=.*?WPS_Office[._-]v?(\d+(?:\.\d+)+)[_(](\d+)[_)]*universal\.dmg/i)
+    regex(%r{>\s+(\d+(?:\.\d+)+)[_(](\d+)[_)]/[\d.]+\s+<}i)
     strategy :page_match do |page|
       page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
     end
@@ -25,10 +28,12 @@ cask "wpsoffice-cn" do
     "~/Library/Application Scripts/com.kingsoft.wpsoffice.mac",
     "~/Library/Application Scripts/com.kingsoft.wpsoffice.mac.FinderSync",
     "~/Library/Application Scripts/com.kingsoft.wpsoffice.mac.recentfiles",
+    "~/Library/Application Scripts/com.kingsoft.wpsoffice.mac.WPSFinderMenu",
     "~/Library/Application Scripts/YK4WKE5WAM.wpsoffice",
     "~/Library/Containers/com.kingsoft.wpsoffice.mac",
     "~/Library/Containers/com.kingsoft.wpsoffice.mac.FinderSync",
     "~/Library/Containers/com.kingsoft.wpsoffice.mac.recentfiles",
+    "~/Library/Containers/com.kingsoft.wpsoffice.mac.WPSFinderMenu",
     "~/Library/Containers/YK4WKE5WAM.wpsoffice",
   ]
 end
