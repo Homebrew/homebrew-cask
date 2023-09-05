2c66473cfbb2b6a3b9d2692cef6c771361912dca
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-05 17:02:32 -0400

    simple-web-server 1.2.9

--- Casks/s/simple-web-server.rb
+++ Casks/s/simple-web-server.rb
@@ -1,9 +1,9 @@
 cask "simple-web-server" do
   arch arm: "-arm64"
 
-  version "1.2.8"
-  sha256 arm:   "d97b37141ba26a5cd29627dbbc0919df79072d42c421511db9687a4fd93e8c51",
-         intel: "f183ffa747d20df49c3608352de6b4d6ca91e8ca748358db36df70380c6ca2f1"
+  version "1.2.9"
+  sha256 arm:   "268bdf33bc10e1e47262f9104879cc46238fce0a670f4365f2c11a1fc244f402",
+         intel: "fb2800f3375d3581e875557269047daf74733561735ed7bf95ab3e66c33adcfa"
 
   url "https://github.com/terreng/simple-web-server/releases/download/v#{version}/Simple-Web-Server-#{version}#{arch}.dmg",
       verified: "github.com/terreng/simple-web-server/"
