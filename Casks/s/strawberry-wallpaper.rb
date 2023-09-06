bff60dacd0df5d1f922d4d41baee1a572ef35601
Rui Chen <rui@chenrui.dev>
2023-09-05 21:35:06 -0400

    strawberry-wallpaper: use github source
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/s/strawberry-wallpaper.rb
+++ Casks/s/strawberry-wallpaper.rb
@@ -2,14 +2,14 @@ cask "strawberry-wallpaper" do
   version "1.4.2"
   sha256 :no_check
 
-  url "http://sw.taoacat.com/Strawberry%20Wallpaper-mac.dmg",
-      verified: "sw.taoacat.com/"
+  url "https://github.com/aitexiaoy/Strawberry-Wallpaper/releases/download/#{version}/Strawberry.Wallpaper-mac.dmg",
+      verified: "github.com/aitexiaoy/Strawberry-Wallpaper/"
   name "Strawberry Wallpaper"
   homepage "https://aitexiaoy.github.io/Strawberry-Wallpaper/"
 
   livecheck do
-    url "http://sw.taoacat.com/version/latest-mac.yml"
-    strategy :electron_builder
+    url :url
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
   app "Strawberry Wallpaper.app"
