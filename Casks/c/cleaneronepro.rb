86431cdadc13e0bfe0b81ae2424c4cf419cc49ec
Rui Chen <rui@chenrui.dev>
2023-09-06 15:31:08 -0400

    cleaneronepro 6.7.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/c/cleaneronepro.rb
+++ Casks/c/cleaneronepro.rb
@@ -1,8 +1,8 @@
 cask "cleaneronepro" do
-  version "6.6.9"
-  sha256 "c6ac927fa2908165175534b9dc3df53e8329d3be0b1724fdde5592aaa4a95c86"
+  version "6.7.0"
+  sha256 "f162c03c91da23058c7fff63896ca107ff7d35c07c4849e2c81ea5cf5943536a"
 
-  url "https://files.trendmicro.com/products/CleanerOnePro/COPG0001/5137/CleanerOnePro_#{version.dots_to_underscores}.zip"
+  url "https://files.trendmicro.com/products/CleanerOnePro/COPG0001/5191/CleanerOnePro_#{version.dots_to_underscores}.zip"
   name "CleanerOnePro"
   desc "All-in-one Cleaner App"
   homepage "https://cleanerone.trendmicro.com/"
