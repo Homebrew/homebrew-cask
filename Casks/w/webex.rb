f76e9dbea6731a3bf8a18ddb0ce24d996b06bfe9
Rui Chen <rui@chenrui.dev>
2023-09-05 20:54:31 -0400

    webex 43.9.0.27194

--- Casks/w/webex.rb
+++ Casks/w/webex.rb
@@ -1,7 +1,7 @@
 cask "webex" do
   arch arm: "Desktop-MACOS-Apple-Silicon", intel: "TeamsDesktop-MACOS"
 
-  version "43.8.0.26955"
+  version "43.9.0.27194"
   sha256 :no_check
 
   url "https://binaries.webex.com/Webex#{arch}-Gold/Webex.dmg"
