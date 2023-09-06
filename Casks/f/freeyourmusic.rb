33ecf09cb3184d0bb11d5cb75b4ebd235ec83e8d
Rui Chen <rui@chenrui.dev>
2023-09-06 18:06:29 -0400

    freeyourmusic 8.0.1

--- Casks/f/freeyourmusic.rb
+++ Casks/f/freeyourmusic.rb
@@ -1,6 +1,6 @@
 cask "freeyourmusic" do
-  version "8.0.0"
-  sha256 "5a86ceb19426be26cdeece0b30230898bf7ae5199da965525f0de600b9960121"
+  version "8.0.1"
+  sha256 "6c0b16b2ff3417dfffb310a58f408ccaa087b54b3627a3758f3ed6759d5fa5bd"
 
   url "https://fym-app-production.s3.nl-ams.scw.cloud/FreeYourMusic-#{version}.dmg",
       verified: "fym-app-production.s3.nl-ams.scw.cloud/"
