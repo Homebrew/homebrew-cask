5bf7d4912a21efd7e19fedaa22c9c949e4981440
Rui Chen <rui@chenrui.dev>
2023-09-05 20:11:38 -0400

    amazon-chime 5.22.22088

--- Casks/a/amazon-chime.rb
+++ Casks/a/amazon-chime.rb
@@ -1,6 +1,6 @@
 cask "amazon-chime" do
-  version "5.22.22085"
-  sha256 "c588258665b01d0948e0fe641b177b6c9ec2999b704320c23c809910c1ffef38"
+  version "5.22.22088"
+  sha256 "f56300a367a3142cc12a64b98c4c50a5d5e9d50f0f0c897855983f944750c26a"
 
   url "https://clients.chime.aws/mac-nme/AmazonChime-#{version}.dmg"
   name "Amazon Chime"
