33bb42da99df1884ba2f5fa67d63e66cb889c91e
Bevan Kay <email@bevankay.me>
2023-09-06 10:29:43 +1000

    Update geomap.rb

--- Casks/g/geomap.rb
+++ Casks/g/geomap.rb
@@ -5,7 +5,7 @@ cask "geomap" do
   sha256 arm:   "9b76e98cd954779658b5d41b9486e30315a6106ee9d44e429d38d1dd1c35b3e8",
          intel: "07c69b0282bc6828e769a73e00fd6b87a4ec82669e80727fc5a35fd2b61b462f"
 
-  url "https://www.geomapapp.org/MapApp/GeoMapApp-#{version}-#{arch}.dmg"
+  url "https://app.geomapapp.org/MapApp/GeoMapApp-#{version}-#{arch}.dmg"
   name "GeoMapApp"
   desc "Browse, visualize and analyze geoscience data sets"
   homepage "https://www.geomapapp.org/"
@@ -19,6 +19,3 @@ cask "geomap" do
 
   zap trash: "~/.GMA"
 end
-
-# https://www.geomapapp.org/MapApp/GeoMapApp-3.7.1-Intel.dmg
-# https://www.geomapapp.org/MapApp/GeoMapApp-3.7.1-Silicon.dmg
