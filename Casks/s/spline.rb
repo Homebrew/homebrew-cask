b63d94bbcec477d82f90ae0622820cc1433734ae
Rui Chen <rui@chenrui.dev>
2023-09-05 21:54:58 -0400

    spline: update artifact url
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/s/spline.rb
+++ Casks/s/spline.rb
@@ -5,7 +5,7 @@ cask "spline" do
   sha256 arm:   "c2a78bb2f46ed0e5ab36280df65509f90bb3ee327610b483a7b6ec5e27871834",
          intel: "316bc704267d1eca58a6e62e23981f415d3e70e121f94192c64f3de88730a2a7"
 
-  url "https://cdn.spline.design/_assets/Spline-#{version}-#{arch}.osx_update.zip"
+  url "https://cdn.spline.design/_assets/Spline-#{version}-#{arch}.mac.zip"
   name "Spline"
   desc "Design and collaborate in 3D"
   homepage "https://spline.design/"
