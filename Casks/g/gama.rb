666c6f8d9ce42205ac7f0d5c99bd1d78baeb823f
Miccal Matthews <miccal.matthews@gmail.com>
2023-09-06 12:09:05 +0800

    Update gama.rb

--- Casks/g/gama.rb
+++ Casks/g/gama.rb
@@ -5,7 +5,7 @@ cask "gama" do
   sha256 arm:   "dfc74a0004a32302f0eee9303b5c45f83a9e66307e3d4fe30999de1eeb1a00e6",
          intel: "cad4827fcf2e8eb06459b6e8d7468537831fdcccaf2ee896171c6168cb03de74"
 
-  url "https://github.com/gama-platform/gama/releases/download/#{version}/GAMA_#{version}_macOS_#{arch}.dmg",
+  url "https://github.com/gama-platform/gama/releases/download/#{version}/GAMA_#{version}_MacOS.dmg",
       verified: "github.com/gama-platform/gama/"
   name "GAMA Platform"
   desc "IDE for building spatially explicit agent-based simulations"
