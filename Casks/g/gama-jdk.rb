df001fc641e4a7b1a6f5183324881fda5e77d305
Miccal Matthews <miccal.matthews@gmail.com>
2023-09-06 12:08:43 +0800

    Update gama-jdk.rb

--- Casks/g/gama-jdk.rb
+++ Casks/g/gama-jdk.rb
@@ -1,11 +1,11 @@
 cask "gama-jdk" do
-  arch arm: "Apple_Silicon", intel: "Intel"
+  arch arm: "_M1"
 
   version "1.9.2"
   sha256 arm:   "c9b1307df181137e58a39966b5d332bc8e145a823ba189f93e489cf75a214234",
          intel: "1d428e832dbe1778b9b90209052418a941a0549f7653b7e3977ac5980738a1fc"
 
-  url "https://github.com/gama-platform/gama/releases/download/#{version}/GAMA_#{version}_macOS_#{arch}_with_JDK.dmg",
+  url "https://github.com/gama-platform/gama/releases/download/#{version}/GAMA_#{version}_MacOS#{arch}_with_JDK.dmg",
       verified: "github.com/gama-platform/gama/"
   name "GAMA Platform with embedded JDK"
   desc "IDE for building spatially explicit agent-based simulations"
