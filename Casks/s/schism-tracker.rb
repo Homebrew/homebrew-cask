492740e4eca7569d980188c775fe888509a72aad
Rui Chen <rui@chenrui.dev>
2023-09-06 21:38:47 -0400

    schism-tracker 20230906

--- Casks/s/schism-tracker.rb
+++ Casks/s/schism-tracker.rb
@@ -1,6 +1,6 @@
 cask "schism-tracker" do
-  version "20221201"
-  sha256 "02e9761f8b385b81b3ccd4945ca70e995705866babcd87c90f06fb824973b8ef"
+  version "20230906"
+  sha256 "3424f99611484b4859ad86d2cb8ed344cce26e4f1623d56ad12e06cddb09ee19"
 
   url "https://github.com/schismtracker/schismtracker/releases/download/#{version}/schismtracker-#{version}-macos.zip"
   name "Schism Tracker"
