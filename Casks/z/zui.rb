ec4f021d2b4ae6a25e1cc6af918f251db1c119d6
graelo <graelo@grael.cc>
2023-09-07 20:49:59 +0200

    zui: new cask

---
+++ Casks/z/zui.rb
@@ -0,0 +1,12 @@
+cask "zui" do
+  version "1.2.0"
+  sha256 "8a45c01f4e80441c59a052dc641f777ef199f00a92f5c85cb3efc91ff3f8b020"
+
+  url "https://github.com/brimdata/zui/releases/download/v#{version}/Zui-#{version}.dmg",
+      verified: "github.com/brimdata/zui/"
+  name "Zui"
+  desc "Graphical user interface for exploring data in Zed lakes"
+  homepage "https://zui.brimdata.io/docs"
+
+  app "Zui.app"
+end
