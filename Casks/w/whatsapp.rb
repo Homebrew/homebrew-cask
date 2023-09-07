d72b499f9e0555299dc890f52afc3d1e4c07bb91
Rui Chen <rui@chenrui.dev>
2023-09-07 12:48:03 -0400

    whatsapp 2.23.17.82

--- Casks/w/whatsapp.rb
+++ Casks/w/whatsapp.rb
@@ -1,6 +1,6 @@
 cask "whatsapp" do
-  version "2.23.17.81"
-  sha256 "67d1ca4e0ff2ff78f701a7f7fc37b3eec99e560ce519fe53ac7ded3307077939"
+  version "2.23.17.82"
+  sha256 "57efe4ea2ebb2e8cbc919e3d4e243c678f80a3f9c4ee4d48b3ac69b987ec8d10"
 
   url "https://web.whatsapp.com/desktop/mac_native/release/?version=#{version}&extension=zip&configuration=Release&branch=relbranch"
   name "WhatsApp"
