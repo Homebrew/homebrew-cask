1dc228336654e72f464135cd1fa5ee14ed17c31d
Bevan Kay <email@bevankay.me>
2023-09-07 21:33:04 +1000

    Update 1password-cli.rb

--- Casks/1/1password-cli.rb
+++ Casks/1/1password-cli.rb
@@ -5,7 +5,7 @@ cask "1password-cli" do
   sha256 arm:   "1de120081d7a7d278fdc6c619a4ce15d691bc535c2d2aa494d975bc3463c6a33",
          intel: "6e81ac0d9f7f2dbc4dc1e3ed71c02790a7b874ceb36e79432a03ea084b291820"
 
-  url "https://cache.agilebits.com/dist/1P/op2/pkg/v#{version}/op_darwin_#{arch}_v#{version}.zip"
+  url "https://cache.agilebits.com/dist/1P/op2/pkg/v#{version}/op_darwin_#{arch}_v#{version}.zip",
       verified: "cache.agilebits.com/dist/1P/op2/pkg/"
   name "1Password CLI"
   desc "Command-line interface for 1Password"
