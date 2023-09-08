305a08fef36c7776396516d920c8e61b1f33ca72
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-09-08 16:12:39 -0400

    fission 2.8.4

--- Casks/f/fission.rb
+++ Casks/f/fission.rb
@@ -1,5 +1,5 @@
 cask "fission" do
-  version "2.8.3"
+  version "2.8.4"
   sha256 :no_check
 
   url "https://rogueamoeba.com/fission/download/Fission.zip"
@@ -12,7 +12,7 @@ cask "fission" do
     strategy :sparkle
   end
 
-  depends_on macos: ">= :catalina"
+  depends_on macos: ">= :big_sur"
 
   app "Fission.app"
 end
