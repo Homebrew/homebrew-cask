8226279f7f453cef29ac9b4c78c0faf5f2a7f1d5
Rui Chen <rui@chenrui.dev>
2023-09-08 16:23:19 -0400

    guilded 1.0.9219633

--- Casks/g/guilded.rb
+++ Casks/g/guilded.rb
@@ -1,6 +1,6 @@
 cask "guilded" do
-  version "1.0.9218125"
-  sha256 "74c0f45e69182f4de61adacd6282502c97aeab4320034f06388eb2cafbc2445e"
+  version "1.0.9219633"
+  sha256 "c20527d99c1f9eddb046daee27f8e35a28f358aa95da980c091589dddc013481"
 
   url "https://s3-us-west-2.amazonaws.com/www.guilded.gg/AppBuilds/mac/Guilded-#{version}-release.dmg",
       verified: "s3-us-west-2.amazonaws.com/www.guilded.gg/AppBuilds/mac/"
