052ca424dd2961fe511634b963430f176aa5b712
Rui Chen <rui@chenrui.dev>
2023-09-08 10:22:34 -0400

    unity 2023.1.12f1

--- Casks/u/unity.rb
+++ Casks/u/unity.rb
@@ -1,9 +1,9 @@
 cask "unity" do
   arch arm: "Arm64"
 
-  version "2023.1.11f1,fbf46ad88f3d"
-  sha256 arm:   "d4b27b4153bf199d39be242512c235ac10357340fb289a8fa4026d86f8f631e9",
-         intel: "46d550be1be79cd4bb01284a1949f2d8fcc12c751c31d0ef8b988971d84107b8"
+  version "2023.1.12f1,38370f5b2257"
+  sha256 arm:   "c189be03d3fe2bad7069c839384f59ae3eb54e7c01892235e9c7b2f4b2153ad0",
+         intel: "4eace936729ae09023488593e9d2089cbba60b139842acf05669505b914c9c18"
 
   url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorInstaller#{arch}/Unity-#{version.csv.first}.pkg",
       verified: "download.unity3d.com/download_unity/"
