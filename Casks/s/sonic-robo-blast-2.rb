a482d31116426fe4420e2572ff0bb03b31f845a9
Rui Chen <rui@chenrui.dev>
2023-09-08 00:02:32 -0400

    sonic-robo-blast-2 2.2.12
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/s/sonic-robo-blast-2.rb
+++ Casks/s/sonic-robo-blast-2.rb
@@ -1,9 +1,9 @@
 cask "sonic-robo-blast-2" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "2.2.11"
-  sha256 "e072a030cd9680058b73e02be87d44a643480a06224795e2eb8d3d6204809ea0"
+  version "2.2.12"
+  sha256 "ebe271488e80514c4a3fe1bd7c69e57548ff948c271bf77fcfc0bb893d84d6b0"
 
-  url "https://github.com/STJr/SRB2/releases/download/SRB2_release_#{version}/SRB2-#{version}-Darwin.dmg",
+  url "https://github.com/STJr/SRB2/releases/download/SRB2_release_#{version}/SRB2-#{version}-macOS-Installer.dmg",
       verified: "github.com/STJr/SRB2/"
   name "Sonic Robo Blast 2"
   desc "3D open-source Sonic the Hedgehog fangame built using a Doom Legacy port of Doom"
