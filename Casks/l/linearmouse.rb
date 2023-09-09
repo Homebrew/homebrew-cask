a0576aed39e703a65e8343656d39412e5f9ee8fe
Rui Chen <rui@chenrui.dev>
2023-09-09 13:09:31 -0400

    linearmouse 0.9.5

--- Casks/l/linearmouse.rb
+++ Casks/l/linearmouse.rb
@@ -1,6 +1,6 @@
 cask "linearmouse" do
-  version "0.9.4"
-  sha256 "72463565ef0b38ea9c4a2ce44101f9f882ce45e084c29283881046064d476500"
+  version "0.9.5"
+  sha256 "4f6e47d8298306ddc9e73682f20cbcecfc8938d3ec0d9a45ed2e5e92c47382b0"
 
   url "https://github.com/linearmouse/linearmouse/releases/download/v#{version}/LinearMouse.dmg",
       verified: "github.com/linearmouse/linearmouse/"
