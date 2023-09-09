c90f84a6582408ba59456615a9524eca8aaf84e6
Rui Chen <rui@chenrui.dev>
2023-09-09 15:20:37 -0400

    caprine 2.58.1

--- Casks/c/caprine.rb
+++ Casks/c/caprine.rb
@@ -1,6 +1,6 @@
 cask "caprine" do
-  version "2.58.0"
-  sha256 "46a2be7c9240b7d5be9bbce0e99608e8f10039add5d54c6ca6d1291b5aa389b8"
+  version "2.58.1"
+  sha256 "6afa13a8333673b52870a9a9a58a8c5e6788873f232b770b5630c86340604bb6"
 
   url "https://github.com/sindresorhus/caprine/releases/download/v#{version}/Caprine-#{version}.dmg"
   name "Caprine"
