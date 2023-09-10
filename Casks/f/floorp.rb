286ff05c9a3c77130017b8418ba62f4328c88cdb
AraigumaG <1692419-araigumaG@users.noreply.gitlab.com>
2023-09-10 12:52:47 +0900

    fix style about formula for Floorp

--- Casks/f/floorp.rb
+++ Casks/f/floorp.rb
@@ -3,10 +3,11 @@ cask "floorp" do
   sha256 "ceb10603888b93672cc1ac18e2a1b98625f56ec9bb2f2d82f94e34f2a7f512b1"
 
   url "https://github.com/Floorp-Projects/Floorp/releases/download/v#{version}/floorp-macOS-universal.dmg",
-  verified: "github.com/Floorp-Projects"
+      verified: "github.com/Floorp-Projects/"
   name "Floorp browser"
   desc "Firefox based browser with excellent privacy"
-  homepage "https://floorp.app"
+  homepage "https://floorp.app/"
+
   auto_updates true
 
   app "Floorp.app"
