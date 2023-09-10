796be60eedfe32a3b43b8a2a1e181322220c97b7
Rui Chen <rui@chenrui.dev>
2023-09-10 09:38:45 -0400

    qownnotes 23.9.2

--- Casks/q/qownnotes.rb
+++ Casks/q/qownnotes.rb
@@ -1,6 +1,6 @@
 cask "qownnotes" do
-  version "23.9.1"
-  sha256 "b0d08236d2fb5b0b758314dbf7f4311ffe7575bae53021b4f4bbed9f0e19d9ff"
+  version "23.9.2"
+  sha256 "e0435c7c5cc6a76a14d9587e1de7295b9b3dffd850cef001b103e9331de58fc9"
 
   url "https://github.com/pbek/QOwnNotes/releases/download/v#{version}/QOwnNotes.dmg",
       verified: "github.com/pbek/QOwnNotes/"
