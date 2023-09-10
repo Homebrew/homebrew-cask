8c683349e8d9a6947703c21343d14a4cdd5ebbbc
Miccal Matthews <miccal.matthews@gmail.com>
2023-09-11 06:03:08 +0800

    Update koodo-reader.rb

--- Casks/k/koodo-reader.rb
+++ Casks/k/koodo-reader.rb
@@ -5,7 +5,7 @@ cask "koodo-reader" do
   sha256 arm:   "29939046e324835e2054d251e83f252992c51b5a45ae47bedeb2dd8d7cd82fa8",
          intel: "6f06f60bc5ef34e947be07874914641cddd6379fe676b11bcdbb79493c9a4d79"
 
-  url "https://github.com/troyeguo/koodo-reader/releases/download/v#{version}/Koodo-Reader-#{version}.dmg",
+  url "https://github.com/troyeguo/koodo-reader/releases/download/v#{version}/Koodo-Reader-#{version}#{arch}.dmg",
       verified: "github.com/troyeguo/koodo-reader/"
   name "Koodo Reader"
   desc "Open-source epub reader"
