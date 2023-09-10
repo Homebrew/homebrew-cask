ae37642d6da3e9c1eecf5fc4c6480d162c3ee078
Bevan Kay <email@bevankay.me>
2023-09-11 09:42:19 +1000

    Add livecheck, change version, update name

--- Casks/w/wiso-steuer-2023.rb
+++ Casks/w/wiso-steuer-2023.rb
@@ -1,11 +1,16 @@
 cask "wiso-steuer-2023" do
-  version "2023"
+  version "30.09.3800"
   sha256 :no_check
 
   url "https://download.buhl.de/ESD/Steuer/2023/WISOSteuer2023.dmg"
-  name "wiso-steuer-2023"
+  name "WISO Steuer 2023"
   desc "Tax declaration for the fiscal year 2022"
   homepage "https://www.buhl.de/download/wiso-steuer-2023/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   app "SteuerMac 2023.app"
 
