6d9dd71f756dba8c4b486dbd22a8ec66c7e89bd6
Rui Chen <rui@chenrui.dev>
2023-09-11 19:19:00 -0400

    privileges 1.5.4

--- Casks/p/privileges.rb
+++ Casks/p/privileges.rb
@@ -1,6 +1,6 @@
 cask "privileges" do
-  version "1.5.3"
-  sha256 "d8d269b8721e0844821c0a0dc6e428ecca6f53403e9993caed55c80bfb7fb52e"
+  version "1.5.4"
+  sha256 "6f16af136a928a9e0c233ef7d36db4458588224e676b079ede1b626c8ddba346"
 
   url "https://github.com/SAP/macOS-enterprise-privileges/releases/download/#{version}/Privileges.zip"
   name "Privileges"
