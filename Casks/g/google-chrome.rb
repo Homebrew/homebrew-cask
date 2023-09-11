a5d29b47aeaf30ff1d1464ea8e11d529074526d3
Patrick Linnane <patrick@linnane.io>
2023-09-11 12:53:36 -0700

    google-chrome 116.0.5845.187

--- Casks/g/google-chrome.rb
+++ Casks/g/google-chrome.rb
@@ -1,5 +1,5 @@
 cask "google-chrome" do
-  version "116.0.5845.179"
+  version "116.0.5845.187"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
