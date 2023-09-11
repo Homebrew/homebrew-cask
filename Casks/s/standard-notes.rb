f852585d23b923bb9e511da9525b717733a455fd
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-11 12:24:18 -0400

    standard-notes 3.172.15

--- Casks/s/standard-notes.rb
+++ Casks/s/standard-notes.rb
@@ -1,9 +1,9 @@
 cask "standard-notes" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.172.1"
-  sha256 arm:   "c7fa1017447f452fe10b296a3cdbe923b6e8ad48af3d798549a274d2def86d5b",
-         intel: "b228821803464f223c58f4206b050da8927db6526ac5b5363b2d14b45a1cb98c"
+  version "3.172.15"
+  sha256 arm:   "b32a70e6f2f5228d26a8b541ed576fce89cef2fe37303e1f2ce1e933a25596c7",
+         intel: "cc84dfd592785c8caa630478533c22579ac4b8afd3c6aa148273d381d29c129f"
 
   url "https://github.com/standardnotes/app/releases/download/%40standardnotes%2Fdesktop%40#{version}/standard-notes-#{version}-mac-#{arch}.zip",
       verified: "github.com/standardnotes/app/"
