cc9df4815a4f91878c9446292d43fd38704089f6
Rui Chen <rui@chenrui.dev>
2023-09-11 18:26:57 -0400

    openrefine 3.7.5

--- Casks/o/openrefine.rb
+++ Casks/o/openrefine.rb
@@ -1,6 +1,6 @@
 cask "openrefine" do
-  version "3.7.4"
-  sha256 "262c5cbd4da5d9156d0c24422474511ee3111ffea2e9c1481e0340b127e10701"
+  version "3.7.5"
+  sha256 "87e5bfeddb2032a5e6ef18f2f06e39c8c3068c9630e134f2b9c8e70a4e16ce7c"
 
   url "https://github.com/OpenRefine/OpenRefine/releases/download/#{version}/openrefine-mac-#{version}.dmg",
       verified: "github.com/OpenRefine/OpenRefine/"
