d8189df5a4602da90bb04ba0244ba23f9921f15b
Rui Chen <rui@chenrui.dev>
2023-09-12 12:22:04 -0400

    recipeui 0.8.0

--- Casks/r/recipeui.rb
+++ Casks/r/recipeui.rb
@@ -1,9 +1,9 @@
 cask "recipeui" do
   arch arm: "aarch64", intel: "universal"
 
-  version "0.7.7"
-  sha256 arm:   "b443a1124480b149a17bb52fafd04b25d35ccfa59ef5aef0f03c3709a1856efe",
-         intel: "d0e217c6fd9121afcfea8302fa0a8f46bfe81b5c8b82b0922fa2631bd922e37d"
+  version "0.8.0"
+  sha256 arm:   "f961d802c5b83bd47baee5f0da7bd9607885a428eec18db7ea96163266b86df0",
+         intel: "6299d0ef268e7c68a22f1ec12872d41f62ae052620d3cd707e4702c371ca7f86"
 
   url "https://github.com/RecipeUI/RecipeUI/releases/download/app-v#{version}/RecipeUI_#{arch}.app.tar.gz",
       verified: "github.com/RecipeUI/RecipeUI/"
