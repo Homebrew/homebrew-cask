6114fbac45bab086be4b1a3d1945aba88aa2bf25
BingoKingo <24961267+BingoKingo@users.noreply.github.com>
2023-09-13 00:54:46 +0800

    wetype: new cask

---
+++ Casks/w/wetype.rb
@@ -0,0 +1,27 @@
+cask "wetype" do
+  version "0.9.8,198"
+  sha256 "d27c39beeb2b4d1ce8da2af48417465539d321f25f4249ad4034d62f48bb6836"
+
+  url "https://wetype.wxqcloud.qq.com/app/mac/#{version.csv.first}/WeTypeInstaller_#{version.csv.first}_#{version.csv.second}.zip"
+  name "WeType"
+  desc "Input method by Weixin"
+  homepage "https://z.weixin.qq.com/"
+
+  livecheck do
+    strategy :sparkle
+  end
+
+  installer manual: "WeTypeInstaller_#{version.csv.first}_#{version.csv.second}.app"
+
+  uninstall delete: [
+    "/Library/Input Methods/WeType.app",
+  ]
+
+  zap trash: [
+    "~/Library/Application Support/WeType",
+    "~/Library/Caches/WeType",
+    "~/Library/Caches/com.tencent.inputmethod.wetype",
+    "~/Library/HTTPStorages/com.tencent.inputmethod.wetype",
+    "~/Library/Preferences/com.tencent.inputmethod.wetype.plist",
+  ]
+end
