4be7ed28fe548b09c1dee14f541819eb63a9990d
Ross Goldberg <484615+rgoldberg@users.noreply.github.com>
2023-09-12 08:39:35 -0400

    macupdater 2.3.9

--- Casks/m/macupdater.rb
+++ Casks/m/macupdater.rb
@@ -1,7 +1,7 @@
 cask "macupdater" do
   on_monterey :or_older do
-    version "2.3.8"
-    sha256 "bbf8f310ee2e0857e7e34b6b1456e5e03484848616c2ebe7799e4f840536139b"
+    version "2.3.9"
+    sha256 "786144fee33e8654e7d3e21763801aa008a492e75ea761f1b00dfdb1d4b5ee72"
 
     depends_on macos: ">= :mojave"
   end
