a022bc45afc9ad59a606b4d2b916e000942cd1fc
Miccal Matthews <miccal.matthews@gmail.com>
2023-09-12 08:36:29 +0800

    Revert "ubiquiti-unifi-controller 7.5.174"

--- Casks/u/ubiquiti-unifi-controller.rb
+++ Casks/u/ubiquiti-unifi-controller.rb
@@ -1,19 +1,34 @@
 cask "ubiquiti-unifi-controller" do
-  version "7.5.174"
-  sha256 "c086c23f3da18d286d859ca202d705a51733c625bdbaae301a53aecfe4a71103"
+  version "7.4.162"
+  sha256 "444409c1c8fb162a1f32e5c31ec86951737318a8aad5378705f69d616005b0eb"
 
-  url "https://fw-download.ubnt.com/data/unifi-controller/5c14-macos-#{version}-05bb1ef6-1356-4888-af43-620e8c19643a.dmg",
-      verified: "fw-download.ubnt.com/"
+  url "https://dl.ubnt.com/unifi/#{version}/UniFi.pkg",
+      verified: "dl.ubnt.com/"
   name "Ubiquiti UniFi Network Controller"
   desc "Set up, configure, manage and analyze your UniFi network"
-  homepage "https://ui.com/consoles"
+  homepage "https://unifi-sdn.ui.com/"
 
   livecheck do
     url "https://fw-update.ubnt.com/api/firmware-latest?filter=eq~~product~~unifi-controller&filter=eq~~channel~~release&filter=eq~~platform~~macos"
     regex(/"version"\s*:\s*"v?(\d+(?:\.\d+)+)/i)
   end
 
-  app "UniFi.app"
+  pkg "UniFi.pkg"
+
+  postflight do
+    set_ownership "~/Library/Application Support/UniFi"
+  end
+
+  uninstall quit:    [
+              "com.oracle.java.*.jre",
+              "com.ubnt.UniFi-Discover",
+            ],
+            signal:  ["TERM", "com.ubnt.UniFi"],
+            pkgutil: "com.ubnt.UniFi",
+            delete:  [
+              "/Applications/UniFi-Discover.app",
+              "/Applications/UniFi.app",
+            ]
 
   zap trash: [
     "~/Library/Application Support/UniFi",
@@ -24,6 +39,5 @@ cask "ubiquiti-unifi-controller" do
   caveats do
     depends_on_java "8"
     license "https://www.ui.com/eula/"
-    requires_rosetta
   end
 end
