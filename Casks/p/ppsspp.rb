d28390112159553373d8cef95fa96e8f6a6f9025
Daniel Sattler <ds@Mini-von-Daniel.fritz.box>
2023-09-12 20:51:43 +0200

    ppsspp 1.16.1

--- Casks/p/ppsspp.rb
+++ Casks/p/ppsspp.rb
@@ -1,6 +1,6 @@
 cask "ppsspp" do
-  version "1.16"
-  sha256 "dbf09947066088525eaa5bb26e2a6da4dc3f076ad84a23b20fa8d1fd93e7ade1"
+  version "1.16.1"
+  sha256 "72376913a32370ec7eb358040e1e7f950557f7ddb9f98cd9e3705e778a3807bc"
 
   url "https://github.com/hrydgard/ppsspp/releases/download/v#{version}/PPSSPPSDL-macOS-v#{version}.zip",
       verified: "github.com/hrydgard/ppsspp/"
