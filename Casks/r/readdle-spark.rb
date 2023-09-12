8b72b17aceadd0677a295950fa5ee63bb8f3a927
Rui Chen <rui@chenrui.dev>
2023-09-12 15:56:14 -0400

    readdle-spark 3.8.3.56322

--- Casks/r/readdle-spark.rb
+++ Casks/r/readdle-spark.rb
@@ -1,6 +1,6 @@
 cask "readdle-spark" do
-  version "3.8.2.56243"
-  sha256 "cf5fd1b6b1bea11bd61294107e68b75f7d8b92c9af599068343a1a70b96e2523"
+  version "3.8.3.56322"
+  sha256 "2b2d87515cdb1cd3d99a63a63c448db1835edc2f6b5adda02b4f92c989c3b57f"
 
   url "https://downloads.sparkmailapp.com/Spark#{version.major}/mac/dist/#{version}/Spark.zip"
   name "Spark"
