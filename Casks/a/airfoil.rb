99e432a052695bf584b891e7f12754d706ca52fd
Rui Chen <rui@chenrui.dev>
2023-09-12 10:44:34 -0400

    airfoil: update osx requirement
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/a/airfoil.rb
+++ Casks/a/airfoil.rb
@@ -13,7 +13,7 @@ cask "airfoil" do
   end
 
   auto_updates true
-  depends_on macos: ">= :catalina"
+  depends_on macos: ">= :big_sur"
 
   app "Airfoil/Airfoil Satellite.app"
   app "Airfoil/Airfoil.app"
