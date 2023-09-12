3188dc3f2516a6a1ae3ce1caa2a6612586e8c75f
Rui Chen <rui@chenrui.dev>
2023-09-12 09:58:53 -0400

    workflowy 4.0.2309111622

--- Casks/w/workflowy.rb
+++ Casks/w/workflowy.rb
@@ -1,6 +1,6 @@
 cask "workflowy" do
-  version "4.0.2309051849"
-  sha256 "b47ad3848d322f332ba4e87419b7ff2b3564bd55e747c29c0671833dd497f7cd"
+  version "4.0.2309111622"
+  sha256 "1c3ac99477d9ea580ac0693566fbda83fd0e98574a010a5bc336fa9c94c52bc9"
 
   url "https://github.com/workflowy/desktop/releases/download/v#{version}/WorkFlowy.zip",
       verified: "github.com/workflowy/desktop/"
