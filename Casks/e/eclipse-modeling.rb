66563291adac7fb6b24ce0f3cb8ae954687e28b8
Rui Chen <rui@chenrui.dev>
2023-09-13 15:00:55 -0400

    eclipse-modeling 4.28.0,2023-09

--- Casks/e/eclipse-modeling.rb
+++ Casks/e/eclipse-modeling.rb
@@ -1,9 +1,9 @@
 cask "eclipse-modeling" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "4.28.0,2023-06"
-  sha256 arm:   "b01b2b21bbfa62554e3486467353713e747245b6492daf60eb769b8919f08c71",
-         intel: "ca244f3dff376858e8a5ca01cd4532973c2e8d195d78e7a2ab666294193eed6f"
+  version "4.28.0,2023-09"
+  sha256 arm:   "5ad2dd4243a05facd56bee42aeac7332c184ecf306c33e72b83d57776b5b1d06",
+         intel: "4897c5c1f971341165e001016dbb96733899ed2f23a086cdb2a08115939b4fa8"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-modeling-#{version.csv.second}-R-macosx-cocoa-#{arch}.dmg&r=1"
   name "Eclipse Modeling Tools"
