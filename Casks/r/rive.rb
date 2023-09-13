437aa931c5aea6eff12db973fde80caf1797329a
Rui Chen <rui@chenrui.dev>
2023-09-13 17:28:41 -0400

    rive 0.8.756

--- Casks/r/rive.rb
+++ Casks/r/rive.rb
@@ -1,6 +1,6 @@
 cask "rive" do
-  version "0.8.734"
-  sha256 "437e8f09d9ded5841511bc911230564dbf520cd7cebec849d206d3297620d7d6"
+  version "0.8.756"
+  sha256 "da9fbb9ab25be570ce7fc4655c68b9f0532e0217c58947d19c813ecbb36220ea"
 
   url "https://releases.rive.app/macos/#{version}/Rive.dmg"
   name "Rive"
