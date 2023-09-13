f1aba21b56542eff50150c10104b918e66f63325
Rui Chen <rui@chenrui.dev>
2023-09-13 11:44:58 -0400

    eclipse-ide 4.28.0,2023-09

--- Casks/e/eclipse-ide.rb
+++ Casks/e/eclipse-ide.rb
@@ -1,9 +1,9 @@
 cask "eclipse-ide" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "4.28.0,2023-06"
-  sha256 arm:   "1b5ddbafc1bfb1dda7af6cbf62a8c5305b26feba4ae1c2543cb3275af5484f1f",
-         intel: "a1d5b32052ffd115c94280dc360295b19b4cbb0466cf72c1b5d7214a111080a4"
+  version "4.28.0,2023-09"
+  sha256 arm:   "463f89c7e25196c0935235cc025bcc3621a6a16d1378d98ea1c2847bb5e14cdb",
+         intel: "1661cc5d8128b392d892aa315819d9bcaa7a59fb0d4e468a48eade8f5ea4b66a"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-committers-#{version.csv.second}-R-macosx-cocoa-#{arch}.dmg&r=1"
   name "Eclipse IDE for Eclipse Committers"
