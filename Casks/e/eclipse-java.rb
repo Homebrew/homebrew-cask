a1d64e37106fc3171f1e28a8f5cb5a5d50bf4602
Rui Chen <rui@chenrui.dev>
2023-09-13 11:49:02 -0400

    eclipse-java 4.28.0,2023-09

--- Casks/e/eclipse-java.rb
+++ Casks/e/eclipse-java.rb
@@ -1,9 +1,9 @@
 cask "eclipse-java" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "4.28.0,2023-06"
-  sha256 arm:   "3d67e20a135aa52e1c6d24abd9af66a2a8f9d9b58c4d8b9a354c98c04502ccfb",
-         intel: "07df02358304ef0c8241a86e4f514836d3c71f0b1db3dec4c5a2c7564ef5bfd6"
+  version "4.28.0,2023-09"
+  sha256 arm:   "4462ede59651f0af4111c4e61f0d83b4b0c9e50a31baf9108525db92fe0a8c8b",
+         intel: "cbcea2a755e12ab80a58cd71b4418e53a5765022423a6730ad3b31541ddee4be"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-java-#{version.csv.second}-R-macosx-cocoa-#{arch}.dmg&r=1"
   name "Eclipse IDE for Java Developers"
