737fea7b03ffa21dff65d0dcd42291b213782c7a
Rui Chen <rui@chenrui.dev>
2023-09-12 20:10:26 -0400

    betterdisplay: rename from betterdummy
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/b/betterdisplay.rb
+++ Casks/b/betterdisplay.rb
@@ -1,40 +1,33 @@
 cask "betterdisplay" do
-  on_big_sur :or_older do
-    version "1.4.15"
-    sha256 "26a75c3a4e95b076dcb7468e6ce9f9493675e4a9676fd267e5b32459db900077"
+  version "2.0.9"
+  sha256 "e12d9b43585388f7c7bef3e30c0fc93c9361e6cf3b404c3ac0c1b7ebd3c7c978"
 
-    livecheck do
-      skip "Legacy version"
-    end
-
-    depends_on macos: ">= :mojave"
-  end
-  on_monterey :or_newer do
-    version "2.0.9"
-    sha256 "e12d9b43585388f7c7bef3e30c0fc93c9361e6cf3b404c3ac0c1b7ebd3c7c978"
-
-    livecheck do
-      url :url
-      strategy :github_latest
-    end
-  end
-
-  url "https://github.com/waydabber/BetterDisplay/releases/download/v#{version}/BetterDisplay-v#{version}.dmg",
-      verified: "github.com/waydabber/BetterDisplay/"
+  url "https://github.com/waydabber/BetterDisplay/releases/download/v#{version}/BetterDisplay-v#{version}.dmg"
   name "BetterDisplay"
-  desc "Display management tool"
-  homepage "https://betterdisplay.pro/"
+  desc "Dummy Display for Apple Silicon Macs to achieve custom resolutions"
+  homepage "https://github.com/waydabber/BetterDisplay"
 
   auto_updates true
+  depends_on macos: ">= :big_sur"
 
   app "BetterDisplay.app"
 
-  uninstall quit: "me.waydabber.BetterDummy"
-
   zap trash: [
-    "~/Library/Application Support/BetterDummy",
-    "~/Library/Caches/me.waydabber.BetterDummy",
-    "~/Library/HTTPStorages/me.waydabber.BetterDummy",
-    "~/Library/Preferences/me.waydabber.BetterDummy.plist",
+    "~/Library/Application Scripts/me.waydabber.BetterDisplayHelper",
+    "~/Library/Application Scripts/me.waydabber.BetterDisplay",
+    "~/Library/Containers/me.waydabber.BetterDisplayHelper",
+    "~/Library/Containers/me.waydabber.BetterDisplay",
+    "~/Library/Preferences/me.waydabber.BetterDisplay.plist",
   ]
+
+  caveats do
+    discontinued
+
+    <<~EOS
+      #{token} has been renamed 'betterdisplay',
+      install betterdisplay to continue receiving updates;
+
+        brew install --cask betterdisplay
+    EOS
+  end
 end
