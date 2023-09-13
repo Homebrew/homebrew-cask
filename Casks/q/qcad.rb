15bb9ce1257c9d98ff8ef2deed3ef1db80b62dd2
Rui Chen <rui@chenrui.dev>
2023-09-13 11:59:09 -0400

    qcad 3.28.2.4

--- Casks/q/qcad.rb
+++ Casks/q/qcad.rb
@@ -1,7 +1,7 @@
 cask "qcad" do
   arch arm: "11-13-qt6-arm64", intel: "10.14-13"
 
-  version "3.28.2"
+  version "3.28.2.4"
 
   on_high_sierra :or_older do
     sha256 "e7b9ca380f0477c0252875eeacacf526806f4e303ea02cf6b200ece9e33512dc"
@@ -9,7 +9,7 @@ cask "qcad" do
     url "https://www.qcad.org/archives/qcad/qcad-#{version}-trial-macos-10.10-10.13.dmg"
   end
   on_mojave :or_newer do
-    sha256 arm:   "2e36b66092963a6a36604111001d3276ac5905f66a9608ca46cc50952cc772e8",
+    sha256 arm:   "863efc7a245efaea6d63aa8f9433b7e87bff5211d771508304d4a672531917e7",
            intel: "02c44a3bffaf4224f603933786c502c2cf7a1107e97ac6d90c5e095581b8bdea"
 
     url "https://www.qcad.org/archives/qcad/qcad-#{version}-trial-macos-#{arch}.dmg"
