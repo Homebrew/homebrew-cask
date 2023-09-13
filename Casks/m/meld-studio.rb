d2d124646b6121a64b99d5ee7f0207ad22399acc
Matt Polito <matt.polito@gmail.com>
2023-09-13 09:59:27 -0400

    Add meld-studio

---
+++ Casks/m/meld-studio.rb
@@ -0,0 +1,24 @@
+cask "meld-studio" do
+  version "0.3.0.2"
+  sha256 :no_check
+
+  url "https://packages.streamwithmeld.com/MeldStudioInstaller.dmg",
+      verified: "streamwithmeld.com/"
+  name "Meld Studio"
+  desc "Live streaming and recording software"
+  homepage "https://www.meldstudio.co/"
+
+  auto_updates true
+  depends_on macos: ">= :big_sur"
+
+  app "Meld Studio.app"
+
+  zap trash: [
+    "~/Library/Application Support/MeldStudio",
+    "~/Library/HTTPStorages/co.meldstudio.MeldStudioUpdater",
+    "~/Library/LaunchAgents/co.meldstudio.MeldStudioUpdater.wake.plist",
+    "~/Library/LaunchAgents/co.meldstudio.keystone.agent.plist",
+    "~/Library/LaunchAgents/co.meldstudio.keystone.xpcservice.plist",
+    "~/Library/MeldStudio",
+  ]
+end
