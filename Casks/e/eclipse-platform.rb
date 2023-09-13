40894d28fe3e526b3466988cb36e84f9841aed08
Rui Chen <rui@chenrui.dev>
2023-09-13 10:16:01 -0400

    eclipse-platform 4.29

--- Casks/e/eclipse-platform.rb
+++ Casks/e/eclipse-platform.rb
@@ -1,9 +1,9 @@
 cask "eclipse-platform" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "4.28,202306050440"
-  sha256 arm:   "b7ca39d276548e339222b5d0d6761c7b730f3314ce26f986a6e5f966bf00ee9f",
-         intel: "98e25190d983a4749edc3762ce87d6a3361f550bc0144866a667965c2c0f5d6c"
+  version "4.29,202309031000"
+  sha256 arm:   "558a73cf699408d1c81810543929878766c9ac722b11036d7ca99f693a838df9",
+         intel: "8d78e825332bd99a1a5ff88c2f595949d0a62e33f7e6485c0e475ab07121d611"
 
   url "https://www.eclipse.org/downloads/download.php?file=/eclipse/downloads/drops#{version.major}/R-#{version.csv.first}-#{version.csv.second}/eclipse-SDK-#{version.csv.first}-macosx-cocoa-#{arch}.dmg&r=1"
   name "Eclipse SDK"
