e8a5e01c7849803b6e8c4e313a56e875d9530e3e
Patrick Linnane <patrick@linnane.io>
2023-09-13 13:27:46 -0700

    tor-browser 12.5.4

--- Casks/t/tor-browser.rb
+++ Casks/t/tor-browser.rb
@@ -1,6 +1,6 @@
 cask "tor-browser" do
-  version "12.5.3"
-  sha256 "51c0a47823b5ae6a786e97cca54a79134d69ba2362d6ea0e3fc302c91d416ca1"
+  version "12.5.4"
+  sha256 "d223b3dd2d20a891236f66be5957c0f6e49b13fe5f1eb51890f658c141564afc"
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-macos_ALL.dmg"
   name "Tor Browser"
