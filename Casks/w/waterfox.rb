869337b202179ff37f129237cbcbde6498595c1c
Rui Chen <rui@chenrui.dev>
2023-09-13 09:25:44 -0400

    waterfox 5.1.13

--- Casks/w/waterfox.rb
+++ Casks/w/waterfox.rb
@@ -1,6 +1,6 @@
 cask "waterfox" do
-  version "5.1.12"
-  sha256 "e9784e8d4606b121799ab5fc58e35c8eb18ef151beb9bb172145ce608547d18c"
+  version "5.1.13"
+  sha256 "1301fdc598320252442c01cf9772a8d757e95f69607d822610890cb5368e8258"
 
   url "https://cdn1.waterfox.net/waterfox/releases/G#{version}/Darwin_x86_64-aarch64/Waterfox%20G#{version}.dmg"
   name "Waterfox"
