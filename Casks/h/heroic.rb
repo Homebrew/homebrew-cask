78a03f46960b063a73698c709cbc3d15ba648aae
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-13 10:38:13 -0400

    heroic 2.9.2

--- Casks/h/heroic.rb
+++ Casks/h/heroic.rb
@@ -1,9 +1,9 @@
 cask "heroic" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.9.1"
-  sha256 arm:   "c9e9283a198bd8e30d4c833c49a57f965b1ce6741b1692e05897121482e805af",
-         intel: "352e182d1ed24ff0dba6faa524a8a357f1e922470b65084ce02df8d856480c64"
+  version "2.9.2"
+  sha256 arm:   "e91120656b65205882eded98a9bd655d336a76d0b8b84b6fedd8d4928094309c",
+         intel: "b6d86624cb840a562962a930e5a033da103abe15e19885f351e3ac5f81fad902"
 
   url "https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v#{version}/Heroic-#{version}-macOS-#{arch}.dmg"
   name "Heroic Games Launcher"
