44b4a6f7e9760c40978e5c252cc4642ff189f518
Rui Chen <rui@chenrui.dev>
2023-09-14 17:56:16 -0400

    dcp-o-matic-playlist-editor 2.16.64

--- Casks/d/dcp-o-matic-playlist-editor.rb
+++ Casks/d/dcp-o-matic-playlist-editor.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-playlist-editor" do
-  version "2.16.63"
-  sha256 "a0cc5298624d103445a424f8c855ed2be342847f9d72523ed99305793796e128"
+  version "2.16.64"
+  sha256 "1ddedf083ea02a2387eb7b7aafa0da093da2b7a0d416aadd00d6ca0be1b8f162"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-playlist&version=#{version}"
   name "DCP-o-matic Playlist Editor"
