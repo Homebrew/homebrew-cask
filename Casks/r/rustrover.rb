d80b49c34d06a4df8a5316308017ae9ced39879d
Can Berk Güder <cbg@cbg.me>
2023-09-14 11:45:41 -0700

    Add RustRover 2023.2 EAP

---
+++ Casks/r/rustrover.rb
@@ -0,0 +1,35 @@
+cask "rustrover" do
+  arch arm: "-aarch64"
+
+  version "2023.2,232.9921.46"
+  sha256 arm:   "7f01fef11d89c6c6c870a79007607babde40f7a958b7103d1028aa760ed713b7",
+         intel: "4c7193acf07f44b91512d8b4c04c88068b8599e76150a81dfd728046910a0929"
+
+  url "https://download.jetbrains.com/rustrover/RustRover-#{version.csv.second}#{arch}.dmg"
+  name "RustRover"
+  name "RustRover EAP"
+  desc "Rust IDE"
+  homepage "https://www.jetbrains.com/rust/"
+
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=RR&latest=true&type=eap"
+    strategy :json do |json|
+      json["RR"].map do |release|
+        "#{release["version"]},#{release["build"]}"
+      end
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
+  app "RustRover #{version.before_comma} EAP.app"
+
+  zap trash: [
+    "~/Library/Application Support/JetBrains/RustRover#{version.major_minor}",
+    "~/Library/Caches/JetBrains/RustRover#{version.major_minor}",
+    "~/Library/Logs/JetBrains/RustRover#{version.major_minor}",
+    "~/Library/Preferences/com.jetbrains.rustrover-EAP.plist",
+    "~/Library/Saved Application State/com.jetbrains.rustrover-EAP.SavedState",
+  ]
+end
