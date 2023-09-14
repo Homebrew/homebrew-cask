263f087b938be238a619727e904b01bf492e104b
Anders Kaseorg <andersk@mit.edu>
2023-09-14 00:44:01 -0700

    zulip 5.10.1

--- Casks/z/zulip.rb
+++ Casks/z/zulip.rb
@@ -1,9 +1,9 @@
 cask "zulip" do
   arch arm: "arm64", intel: "x64"
 
-  version "5.10.0"
-  sha256 arm:   "a885afad9a1d52b5b5b229a1ed87b8e4b5d5a7e1e2fc393c3c0efc95b13b2963",
-         intel: "be60e2e7f4f6b0a6f518ed777ae1503bcbc16b04c164ae96c0a51e30f3bcf0c5"
+  version "5.10.1"
+  sha256 arm:   "8bc9c5403cc5bd178d344689043afa64063dfdff87ea1687fd799e049dc40762",
+         intel: "ed25eb99b0d831b8f581f43fffb322fd3b08c2774edaf60e9b30cc3810ad0e4e"
 
   url "https://github.com/zulip/zulip-desktop/releases/download/v#{version}/Zulip-#{version}-#{arch}.dmg",
       verified: "github.com/zulip/zulip-desktop/"
