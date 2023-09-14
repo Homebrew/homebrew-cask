68031cc702e2f0ba8efa2b1cfddd704a369bb343
Rui Chen <rui@chenrui.dev>
2023-09-14 17:55:45 -0400

    dcp-o-matic-disk-writer 2.16.64

--- Casks/d/dcp-o-matic-disk-writer.rb
+++ Casks/d/dcp-o-matic-disk-writer.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-disk-writer" do
-  version "2.16.63"
-  sha256 "0d23c2087deb32461d9a95f3d48e1b87d51953088edab12fbafc289d36529d55"
+  version "2.16.64"
+  sha256 "ec2dfa803c07b1e655cd4f879994267ba9d3c17e34bec711c844f91250a3ae49"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-disk&version=#{version}"
   name "DCP-o-matic Disk Writer"
