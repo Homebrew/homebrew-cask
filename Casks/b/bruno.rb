ba9f4bb421250b15d61f42b74103180e25255a4b
Rui Chen <rui@chenrui.dev>
2023-09-13 20:02:59 -0400

    bruno 0.14.0

--- Casks/b/bruno.rb
+++ Casks/b/bruno.rb
@@ -1,9 +1,9 @@
 cask "bruno" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.13.2"
-  sha256 arm:   "9b792caec145611ad72c5d0066f0e4d3e8a72ef9bff8cb736aad0fbad50319a3",
-         intel: "3585b8e0d4cc3afc865e152be913e582e5ae23d14016d4b6fcb50646e0959a42"
+  version "0.14.0"
+  sha256 arm:   "dd4003555df9f96220c38b562ce7085991b0b766a874e7618e31e326a9b7c4fe",
+         intel: "a9e38f3b77b4146c4b379fbc54ce08a2e0b963fcc0d884465909f26fb04dda36"
 
   url "https://github.com/usebruno/bruno/releases/download/v#{version}/bruno_#{version}_#{arch}_mac.dmg",
       verified: "github.com/usebruno/bruno/"
