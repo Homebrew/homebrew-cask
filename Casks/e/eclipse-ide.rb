cdd3e4ba6dd4bdee296ca4f2e0507159cc6d60cc
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-09-14 08:07:51 -0400

    eclipse-ide 4.29

--- Casks/e/eclipse-ide.rb
+++ Casks/e/eclipse-ide.rb
@@ -1,7 +1,7 @@
 cask "eclipse-ide" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "4.28.0,2023-09"
+  version "4.29,2023-09"
   sha256 arm:   "463f89c7e25196c0935235cc025bcc3621a6a16d1378d98ea1c2847bb5e14cdb",
          intel: "1661cc5d8128b392d892aa315819d9bcaa7a59fb0d4e468a48eade8f5ea4b66a"
 
@@ -12,13 +12,9 @@ cask "eclipse-ide" do
 
   livecheck do
     url "https://www.eclipse.org/downloads/packages/"
-    regex(/Eclipse IDE (\d+-\d+) R Packages/i)
+    regex(/release.*?>eclipse\s+(\d+-\d+)\s\((\d+(?:\.\d+)+)\)/i)
     strategy :page_match do |page, regex|
-      page.scan(regex).map do |release|
-        version_page = Homebrew::Livecheck::Strategy.page_content("https://projects.eclipse.org/releases/#{release[0]}")[:content]
-        version = version_page.scan(%r{href="/projects/eclipse/releases/(\d+(?:\.\d+)*)"}i)
-        "#{version[0][0]},#{release[0]}"
-      end
+      page.scan(regex).map { |match| "#{match[1]},#{match[0]}" }
     end
   end
 
