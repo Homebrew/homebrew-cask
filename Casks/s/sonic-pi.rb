023afad4ea361383d5176fea06e850c29d66df34
Miccal Matthews <miccal.matthews@gmail.com>
2023-09-15 08:45:53 +0800

    Update sonic-pi.rb

--- Casks/s/sonic-pi.rb
+++ Casks/s/sonic-pi.rb
@@ -1,5 +1,5 @@
 cask "sonic-pi" do
-  arch arm: "arm64", intel: "x64"
+  arch arm: "Mac-arm64", intel: "Intel-Mac-x64"
 
   on_mojave :or_older do
     version "3.3.1"
@@ -42,16 +42,15 @@ cask "sonic-pi" do
     end
   end
   on_ventura :or_newer do
-    arch_bonus = on_arch_conditional intel: "-Intel"
     version "4.4.0"
     sha256 arm:   "fa4020ac4c3259bbed6d456c4b0e13126fe404c1b474654bffd583335f5d0ab5",
            intel: "7ed0e94cf92fdf2e8d51ee42c5f7c3478fbcef9630eb12dccf203f5e72bb517f"
 
-    url "https://sonic-pi.net/files/releases/v#{version}/Sonic-Pi-for#{arch_bonus}-Mac-#{arch}-v#{version.dots_to_hyphens}.dmg"
+    url "https://sonic-pi.net/files/releases/v#{version}/Sonic-Pi-for-#{arch}-v#{version.dots_to_hyphens}.dmg"
 
     livecheck do
       url :homepage
-      regex(/href=.*?Sonic-Pi[^"' >]*?[._-]v?(\d+(?:[.-]\d+)+)\.dmg/i)
+      regex(/href=.*?Sonic[._-]Pi[._-]for[._-]#{arch}[._-]v?(\d+(?:[.-]\d+)+)\.dmg/i)
       strategy :page_match do |page, regex|
         page.scan(regex).map { |match| match[0].tr("-", ".") }
       end
