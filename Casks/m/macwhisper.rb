5c70492d88a0c4d3eaad856a4f233ba6e3491eda
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-15 10:29:35 -0400

    macwhisper 4.6

--- Casks/m/macwhisper.rb
+++ Casks/m/macwhisper.rb
@@ -1,6 +1,6 @@
 cask "macwhisper" do
-  version "4.4,340"
-  sha256 "9b3ec6c473c9cc71527526aa17beabe2d3c8977f6d8250e15664239e2cb81de1"
+  version "4.6,361"
+  sha256 "4166ed01c7dbc6511a41536fd86dca82375fa14b191e15fdc4ef9a2c5de01984"
 
   url "https://macwhisper-site.vercel.app/releases/MacWhisper-#{version.csv.second}.zip",
       verified: "macwhisper-site.vercel.app/releases/"
