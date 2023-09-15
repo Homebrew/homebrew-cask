199aa9bd9b3228e3e4631fa3421b6663ff03a342
Rui Chen <rui@chenrui.dev>
2023-09-15 00:03:59 -0400

    lm-studio 0.2.6

--- Casks/l/lm-studio.rb
+++ Casks/l/lm-studio.rb
@@ -1,6 +1,6 @@
 cask "lm-studio" do
-  version "0.2.5"
-  sha256 "521642fc3693d60f54d01af47e226d4da5b816ae47d7f12ebe84b63265ecac26"
+  version "0.2.6"
+  sha256 "39bb25ea2f003f6f87c6d8aa9653e04e2b6813f38470b64329497bcf875b0922"
 
   url "https://s3.amazonaws.com/releases.lmstudio.ai/#{version}/LM+Studio-darwin-arm64-#{version}.zip",
       verified: "s3.amazonaws.com/releases.lmstudio.ai/"
