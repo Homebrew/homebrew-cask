bfd3142d76b5d0234698304c2ff2757941a62221
Rui Chen <rui@chenrui.dev>
2023-09-16 14:51:57 -0400

    antconc 4.2.3

--- Casks/a/antconc.rb
+++ Casks/a/antconc.rb
@@ -1,9 +1,9 @@
 cask "antconc" do
   arch arm: "silicon", intel: "intel"
 
-  version "4.2.2"
-  sha256 arm:   "d5b9faa095f4da346e07e3e552850357983fddb87a4fbfa8cf8ab2bcb1b26908",
-         intel: "ad347d0189997500929f947c415fe9d3b1972d24e3551ba3bf7a6a95d46d2c63"
+  version "4.2.3"
+  sha256 arm:   "9de860192c1f31ba500574f07ac5af73f2e0bc3c4ffc2c2c7a7d0b1e7f86e73c",
+         intel: "e974003aedc51253fdbb1edaf6cf74f851d41b8d4b35fe21fc9d80ff158342c0"
 
   url "https://www.laurenceanthony.net/software/antconc/releases/AntConc#{version.no_dots}/apple-#{arch}/AntConc.dmg"
   name "AntConc"
