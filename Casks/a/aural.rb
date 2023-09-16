ead4670ecdf34d3af7f7efe1b0c9639492298385
Rui Chen <rui@chenrui.dev>
2023-09-16 18:59:22 -0400

    aural 3.20.0

--- Casks/a/aural.rb
+++ Casks/a/aural.rb
@@ -1,6 +1,6 @@
 cask "aural" do
-  version "3.19.0"
-  sha256 "855b6cc42fabc75f947a4b1f61975876bbc768955660accf88be5f72ea9cc23e"
+  version "3.20.0"
+  sha256 "8d6d7909cae7ec975dfb8f7dc02928c016b4e7322b3fe23cfea42f32147ace06"
 
   url "https://github.com/maculateConception/aural-player/releases/download/v#{version}/AuralPlayer-#{version}.dmg"
   name "Aural Player"
