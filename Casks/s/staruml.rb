a36c8f693ffa6aa024d1530b63956d52fce0e8d0
Rui Chen <rui@chenrui.dev>
2023-09-18 01:00:10 -0400

    staruml 6.0.1

--- Casks/s/staruml.rb
+++ Casks/s/staruml.rb
@@ -1,9 +1,9 @@
 cask "staruml" do
   arch arm: "-arm64"
 
-  version "5.1.0"
-  sha256 arm:   "9dbdb2a205cdb1a11e296e9aec328f925d002a6037a0fae610fc40c72a36d6ff",
-         intel: "c21ecc5517ea3ab96c8a5a10158f77904f76cff4054349c2dea94e27fb8fe82a"
+  version "6.0.1"
+  sha256 arm:   "b29dbfec7232a653baf0a0d796160c93b4d908321f403298722ac15772d9c3aa",
+         intel: "b29dbfec7232a653baf0a0d796160c93b4d908321f403298722ac15772d9c3aa"
 
   url "https://staruml.io/download/releases-v#{version.major}/StarUML-#{version}#{arch}.dmg"
   name "StarUML"
