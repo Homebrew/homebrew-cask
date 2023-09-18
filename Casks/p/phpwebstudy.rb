d2dc2b62500a0d2c3f0a37170c9a90de9222b4cf
Rui Chen <rui@chenrui.dev>
2023-09-17 23:41:33 -0400

    phpwebstudy 1.0.51

--- Casks/p/phpwebstudy.rb
+++ Casks/p/phpwebstudy.rb
@@ -1,9 +1,9 @@
 cask "phpwebstudy" do
   arch arm: "-arm64"
 
-  version "1.0.50"
-  sha256 arm:   "bd0ea25c0a434f2a290475062fb32e0fec4a5f85d608eaa4118740d0ae3f4c2a",
-         intel: "712d41b33a7624bfa1b2f2b2d35900cec879d773b332539653c1317db3b9909b"
+  version "1.0.51"
+  sha256 arm:   "7bb40726f157b0778be7efb227fe292f5f1f338d4eebb13a9c7ed8a8a2949ec5",
+         intel: "ddea25aac553d2f2e51e20ab84e80b641aa3114a43b8c35a85cff34fc92a5e86"
 
   url "https://github.com/xpf0000/PhpWebStudy/releases/download/v#{version}/PhpWebStudy-#{version}#{arch}-mac.zip",
       verified: "github.com/xpf0000/PhpWebStudy/"
