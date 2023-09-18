63a1f843cd5017dc45a87f06458387faa19394da
Rui Chen <rui@chenrui.dev>
2023-09-17 21:14:09 -0400

    forkgram-telegram 4.9.8

--- Casks/f/forkgram-telegram.rb
+++ Casks/f/forkgram-telegram.rb
@@ -1,9 +1,9 @@
 cask "forkgram-telegram" do
   arch arm: "arm64", intel: "x86"
 
-  version "4.9.7"
-  sha256 arm:   "89e44cde8d8cb3b547288548492405ab4959c895009211807f75f44e160a92b2",
-         intel: "29fe3082945971ad31257816955ee187e5a0277ae655134fc351bace11702a4b"
+  version "4.9.8"
+  sha256 arm:   "dcd05f43d4e65f6952d24f8072338d5056424c804e960512b2dc61be78a018ff",
+         intel: "92cf685f139405eb518276f46a01a69ca3cb590767777bb9454f0db8db4fa5d0"
 
   url "https://github.com/Forkgram/tdesktop/releases/download/v#{version}/Forkgram.macOS.no.auto-update_#{arch}.zip"
   name "Forkgram"
