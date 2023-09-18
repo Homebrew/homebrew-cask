8bca9636a10e45af102a89f582d7cf5f826cf05c
Rui Chen <rui@chenrui.dev>
2023-09-18 00:35:08 -0400

    fishing-funds 8.1.0-legacy

--- Casks/f/fishing-funds.rb
+++ Casks/f/fishing-funds.rb
@@ -1,9 +1,9 @@
 cask "fishing-funds" do
   arch arm: "-arm64"
 
-  version "8.1.0"
-  sha256 arm:   "bd0395ef7ca7f327d43f4eabba199acdabdd2c1bea69f68a71d52a1622dbc4c8",
-         intel: "58b98c46a3001522b4f56ce687345b62ec56667ae4df7f3b480078425f33279c"
+  version "8.1.0-legacy"
+  sha256 arm:   "a6296b20cd518f2afdd3e21ca68fe6c8ca258659f556ce80b4a6e406e7dd5a92",
+         intel: "7d122a2bfa4a3fff5fbfd36a09d14b183e7233427b288b8d25d161e808dbb4c3"
 
   url "https://github.com/1zilc/fishing-funds/releases/download/v#{version}/Fishing-Funds-#{version}#{arch}.dmg",
       verified: "github.com/1zilc/fishing-funds/"
