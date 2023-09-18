480bcf633d9d50de0ec4b50727d15caeda0a9f8c
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-09-18 10:58:18 -0400

    update download links and checksum

--- Casks/s/staruml.rb
+++ Casks/s/staruml.rb
@@ -2,10 +2,10 @@ cask "staruml" do
   arch arm: "-arm64"
 
   version "6.0.1"
-  sha256 arm:   "b29dbfec7232a653baf0a0d796160c93b4d908321f403298722ac15772d9c3aa",
-         intel: "b29dbfec7232a653baf0a0d796160c93b4d908321f403298722ac15772d9c3aa"
+  sha256 arm:   "c897479c91dfeddc3efbf8b67a68e93cdcd4881d54c27b21def5768e8279446e",
+         intel: "43095e53aff0b327a3710b872aa20413196bca6a5e473ccc1ff3bbbf7559670b"
 
-  url "https://staruml.io/download/releases-v#{version.major}/StarUML-#{version}#{arch}.dmg"
+  url "https://files.staruml.io/releases-v#{version.major}/StarUML-#{version}#{arch}.dmg"
   name "StarUML"
   desc "Software modeler"
   homepage "https://staruml.io/"
