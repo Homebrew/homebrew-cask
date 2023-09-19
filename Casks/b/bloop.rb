24beb9336eacd3958b929f675218fb0a75bf479e
Rui Chen <rui@chenrui.dev>
2023-09-19 10:54:54 -0400

    bloop 0.5.2

--- Casks/b/bloop.rb
+++ Casks/b/bloop.rb
@@ -1,9 +1,9 @@
 cask "bloop" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.5.0"
-  sha256 arm:   "ad9d71c56f754e451740887e528817265e0a693b2767572269f2a7a68026f442",
-         intel: "6e1cb6fb9fec10f83d42eff95e9895fd256727fadfb81805c04addbb214473c9"
+  version "0.5.2"
+  sha256 arm:   "a813798ab39bf4685325c6283526b758ed7cc51fcff3a3a424faa5821fdb5def",
+         intel: "698a9e743aa7e74edd2088c6c103af223ef11c3c5e7ce4b6110a19ada5e2d531"
 
   url "https://github.com/BloopAI/bloop/releases/download/v#{version}/bloop_#{version}_#{arch}.dmg",
       verified: "github.com/BloopAI/bloop/"
