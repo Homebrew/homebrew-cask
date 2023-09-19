f48fd591e23f2b44c588160967baef34857bf8dd
Rui Chen <rui@chenrui.dev>
2023-09-19 15:08:22 -0400

    microsoft-word 16.77.23091703

--- Casks/m/microsoft-word.rb
+++ Casks/m/microsoft-word.rb
@@ -20,8 +20,8 @@ cask "microsoft-word" do
     sha256 "5a6a75d9a5b46cceeff5a1b7925c0eab6e4976cba529149b7b291a0355e7a7c9"
   end
   on_big_sur :or_newer do
-    version "16.77.23091003"
-    sha256 "73f7aa0e250b10345016ecd363d0a8d42adbb5e01b26e394d9e223dbcfce5bf1"
+    version "16.77.23091703"
+    sha256 "10c8db978206275a557faf3650763a656b1f7170c9b2a65fa6fdce220bd23066"
   end
 
   url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Word_#{version}_Installer.pkg",
