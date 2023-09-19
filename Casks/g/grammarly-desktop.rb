a2ea3883f03a3c2b49a0198dd0b5fe363d827379
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-09-19 07:25:54 -0400

    grammarly-desktop 1.36.2.0

--- Casks/g/grammarly-desktop.rb
+++ Casks/g/grammarly-desktop.rb
@@ -1,6 +1,6 @@
 cask "grammarly-desktop" do
-  version "1.35.1.0"
-  sha256 "07bb58452f202ab4812357a39c864b0717bd3c8b9597a96331c8f297e23e699b"
+  version "1.36.2.0"
+  sha256 "94c60e8e5ee93f7dfe617ccf96159f4f2c7e9d000e7919106d93ed9fccdeea62"
 
   url "https://download-mac.grammarly.com/versions/#{version}/Grammarly.dmg"
   name "Grammarly Desktop"
