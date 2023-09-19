61ec125e7c8b5df3add81c7cd06b2ecd50b1569c
Philipp Jetschina <kontakt@jetschina.de>
2023-09-19 09:17:11 +0200

    anytype 0.35.0

--- Casks/a/anytype.rb
+++ Casks/a/anytype.rb
@@ -1,9 +1,9 @@
 cask "anytype" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.34.3"
-  sha256 arm:   "e97c36d4a0949f3c6d0771c98e1f7d03a48b4742f026255cf433f9d2f4df1557",
-         intel: "d8db4cabf14bbba3f0141ebb508713a47d28433c0a5ef80143b0ff293c480c00"
+  version "0.35.0"
+  sha256 arm:   "e16087417b724dfc5138bb1cdf57fee39c76f73d39c3a03e90194869472a8337",
+         intel: "2062218393f462f485a0523eedbe3c0f34eadc9a2d9d1f56f33213fe39c674b4"
 
   url "https://anytype-release.fra1.cdn.digitaloceanspaces.com/Anytype-#{version}-mac-#{arch}.dmg",
       verified: "anytype-release.fra1.cdn.digitaloceanspaces.com/"
