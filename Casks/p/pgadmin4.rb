b1ff843e6969cf6cfed92fd44336489fce73f789
Dmitry Chirkin <dmytro.chyrkin@nixs.com>
2023-09-21 16:17:31 +0300

    fix: added arm64 version for pgadmin4

--- Casks/p/pgadmin4.rb
+++ Casks/p/pgadmin4.rb
@@ -1,8 +1,11 @@
 cask "pgadmin4" do
-  version "7.7"
-  sha256 "9bf48155f8803d3151ea0b649dee739ebe7d7f6e3b994e5f753f3617bc64abac"
+  arch arm: "arm64", intel: "x86_64"
 
-  url "https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v#{version}/macos/pgadmin4-#{version}-x86_64.dmg",
+  version "7.7"
+  sha256 arm:   "186ac6976da49e0dcf35539357c2ec231e656524eb4b05c6e8bf36ac961ad790",
+         intel: "9bf48155f8803d3151ea0b649dee739ebe7d7f6e3b994e5f753f3617bc64abac"
+
+  url "https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v#{version}/macos/pgadmin4-#{version}-#{arch}.dmg",
       verified: "ftp.postgresql.org/pub/pgadmin/pgadmin4/"
   name "pgAdmin4"
   desc "Administration and development platform for PostgreSQL"
