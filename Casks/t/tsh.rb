7673a9eea0a288e40731872eee43962f7a933c58
Rui Chen <rui@chenrui.dev>
2023-09-20 23:12:17 -0400

    tsh 14.0.0

--- Casks/t/tsh.rb
+++ Casks/t/tsh.rb
@@ -1,6 +1,6 @@
 cask "tsh" do
-  version "13.3.8"
-  sha256 "c4678ce55cf030388363d7e12780e6b79e54b89e2e9d0b0635959a41c3ab7931"
+  version "14.0.0"
+  sha256 "5222d60d691e2830bf7b89fcdf0eb6fc70122719509c4a2d011b34f64dd055c2"
 
   url "https://cdn.teleport.dev/tsh-#{version}.pkg",
       verified: "cdn.teleport.dev/"
