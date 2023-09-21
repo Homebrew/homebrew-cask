30455c09fb2972db1fd3095b5e365f9a08ee2f55
Rui Chen <rui@chenrui.dev>
2023-09-21 17:21:09 -0400

    dcp-o-matic-combiner 2.16.65

--- Casks/d/dcp-o-matic-combiner.rb
+++ Casks/d/dcp-o-matic-combiner.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-combiner" do
-  version "2.16.64"
-  sha256 "8a583406365c073ed9529fc58e7656ab8a3b81d3149d1484a738a2c20ef11284"
+  version "2.16.65"
+  sha256 "9fa415ee224c80e9936d0c1b056df250f0a6feb4aaefa60c62bbfc7f8e2ffa90"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-combiner&version=#{version}"
   name "dcp-o-matic-combiner"
