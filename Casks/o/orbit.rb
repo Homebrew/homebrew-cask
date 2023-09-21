0fa2628b87c76cc2c5e7222db6b45353ae7d6a0a
Gabriel Donadel <donadeldev@gmail.com>
2023-09-21 18:20:30 -0300

    orbit 0.1.3

---
+++ Casks/o/orbit.rb
@@ -0,0 +1,19 @@
+cask "orbit" do
+  version "0.1.3"
+  sha256 "c49a59a3a9651ea65b929b72f3b69b86f31950b79aa8dd70c3d8a7df343230fb"
+
+  url "https://github.com/expo/orbit/releases/download/expo-orbit-v#{version}/expo-orbit.v#{version}.zip"
+  name "Orbit"
+  desc "Launch builds and start simulators from your menu bar"
+  homepage "https://github.com/expo/orbit/"
+
+  app "Expo Orbit.app"
+
+  zap trash: [
+    "~/Library/Application Support/dev.expo.orbit",
+    "~/Library/Caches/dev.expo.orbit",
+    "~/Library/HTTPStorages/dev.expo.orbit",
+    "~/Library/Preferences/dev.expo.orbit.plist",
+    "~/Library/WebKit/dev.expo.orbit",
+  ]
+end
