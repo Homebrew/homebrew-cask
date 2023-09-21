db0b8ecc4714ff2dab718519d367c5550b91a716
Tim Weston <tim@tim-weston.com>
2023-09-21 18:51:50 -0400

    Update banktivity.rb
    Add verified URL

--- Casks/b/banktivity.rb
+++ Casks/b/banktivity.rb
@@ -2,7 +2,8 @@ cask "banktivity" do
   version "9.1.1-247"
   sha256 "e8e6c93f0514b614c213ca1bef9eea907a09e21dde2c0ae0ce2fa1e2a6a997a1"
 
-  url "https://www.iggsoft.com/banktivity/Banktivity9.1.1-247.dmg"
+  url "https://www.iggsoft.com/banktivity/Banktivity#{version}.dmg",
+      verified: "iggsoft.com/banktivity/"
   name "Banktivity"
   desc "App to manage bank accounts in one place"
   homepage "https://www.iggsoftware.com/banktivity/"
