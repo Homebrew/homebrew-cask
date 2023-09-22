6ffcf31017bb8376a980850dd05091984c301085
Miccal Matthews <miccal.matthews@gmail.com>
2023-09-22 08:38:56 +0800

    Update banktivity.rb

--- Casks/b/banktivity.rb
+++ Casks/b/banktivity.rb
@@ -1,16 +1,16 @@
 cask "banktivity" do
-  version "9.1.1-247"
+  version "9.1.1,247"
   sha256 "e8e6c93f0514b614c213ca1bef9eea907a09e21dde2c0ae0ce2fa1e2a6a997a1"
 
-  url "https://www.iggsoft.com/banktivity/Banktivity#{version}.dmg",
+  url "https://www.iggsoft.com/banktivity/Banktivity#{version.csv.first}-#{version.csv.second}.dmg",
       verified: "iggsoft.com/banktivity/"
   name "Banktivity"
   desc "App to manage bank accounts in one place"
   homepage "https://www.iggsoftware.com/banktivity/"
 
   livecheck do
-    url "https://www.iggsoft.com/banktivity/banktivity-versions-feed.json"
-    regex(/Banktivityv?(\d+(?:[._-]\d+)+)\.dmg/i)
+    url "https://www.banktivity.com/download.php"
+    strategy :extract_plist
   end
 
   depends_on macos: ">= :catalina"
