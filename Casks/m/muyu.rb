73c26794440bd114c200f8c12a54578c9daa45e4
wuj13 <wuj13@xiaopeng.com>
2023-09-23 10:37:28 +0800

    updated: update formula

--- Casks/m/muyu.rb
+++ Casks/m/muyu.rb
@@ -1,8 +1,8 @@
 cask "muyu" do
-  version "1.4.2_27"
-  sha256 "6d2b6cb80c8c392eaf56090e16b82a1fc3b69f9b1e7cf0f6b19a8cb286fe788a"
+  version "1.4.2"
+  sha256 :no_check
 
-  url "https://breakit.thriller.fun/packages/Muyu.v#{version}.zip"
+  url "https://breakit.thriller.fun/packages/Muyu.zip"
   name "Muyu"
   desc "木鱼：集成番茄工作法与喝水提示功能"
   homepage "https://breakit.thriller.fun/"
@@ -13,19 +13,20 @@ cask "muyu" do
   end
 
   auto_updates true
+  depends_on macos: ">= :monterey"
 
   app "Muyu.app"
 
-  uninstall quit: "com.thriller.breakit"
+  uninstall quit: "fun.thriller.BreakIt"
 
   zap trash: [
     "~/Library/Containers/fun.thriller.BreakIt",
     "~/Library/Containers/fun.thriller.BreakItLoginItem",
     "~/Library/Containers/org.sparkle-project.Downloader/Data/Library/Caches/fun.thriller.BreakIt",
-    "~/Library/Containers/fun.thriller.BreakIt/Data/Library/Saved\ Application\ State/fun.thriller.BreakIt.savedState",
+    "~/Library/Containers/fun.thriller.BreakIt/Data/Library/Saved Application State/fun.thriller.BreakIt.savedState",
     "~/Library/Preferences/fun.thriller.BreakIt.plist",
-    "~/Library/Application\ Scripts/fun.thriller.BreakIt",
-    "~/Library/Application\ Scripts/fun.thriller.BreakItLoginItem",
-    "~/Library/Caches/fun.thriller.BreakIt"
+    "~/Library/Application Scripts/fun.thriller.BreakIt",
+    "~/Library/Application Scripts/fun.thriller.BreakItLoginItem",
+    "~/Library/Caches/fun.thriller.BreakIt",
   ]
 end
