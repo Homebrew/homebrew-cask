458aeee51813a8d64c5c0dc7fb919520d0489cb7
Rui Chen <rui@chenrui.dev>
2023-09-24 10:50:14 -0400

    responsively 1.8.0

--- Casks/r/responsively.rb
+++ Casks/r/responsively.rb
@@ -1,9 +1,9 @@
 cask "responsively" do
   arch arm: "-arm64"
 
-  version "1.7.1"
-  sha256 arm:   "c4b76812fcf43a534adfcedc8b2ad1a4441b778f5a1942df5822de8d6437fabd",
-         intel: "7372a1f14f881c69b8c799f5b76c84d6984f9c814886dfbb466d8edb9300a33a"
+  version "1.8.0"
+  sha256 arm:   "801e868f5f47ffa9903318515c090592735dabd97dab79d4f0797ae004d6c20c",
+         intel: "9aef2af6ec3a4f07c9f46b41e2d518af644228ddbc58f6a61f423c7cfd349c4c"
 
   url "https://github.com/responsively-org/responsively-app-releases/releases/download/v#{version}/ResponsivelyApp-#{version}#{arch}.dmg",
       verified: "github.com/responsively-org/responsively-app-releases/"
