0b8a9679f58ec4ca575c4e779286f5e2f429bb74
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-25 16:18:30 -0400

    omnigraffle 7.22

--- Casks/o/omnigraffle.rb
+++ Casks/o/omnigraffle.rb
@@ -50,8 +50,8 @@ cask "omnigraffle" do
     end
   end
   on_big_sur :or_newer do
-    version "7.21.6"
-    sha256 "d7253dbacb5fe6ba099ded488c1cb99bb4e292b8b8961770a1db8dc1e07e2983"
+    version "7.22"
+    sha256 "299350fd99c9221df580e264bf4abb102a34f2ff01c553d002b96f0e13f10da8"
 
     url "https://downloads.omnigroup.com/software/macOS/11/OmniGraffle-#{version}.dmg"
 
