9190518d71c6cf3d0186fc9aea68a8df3221459a
Rui Chen <rui@chenrui.dev>
2023-09-25 21:55:40 +0800

    defold 1.6.0

--- Casks/d/defold.rb
+++ Casks/d/defold.rb
@@ -1,6 +1,6 @@
 cask "defold" do
-  version "1.5.0"
-  sha256 "42fe4b6adc081cbda4cc28a0253ea54afb26c0322706cb43fe8a53d8466b6c9b"
+  version "1.6.0"
+  sha256 "71af7002692cc68b8f2a4e152bfbd0b9828e2e377fb26a57c642206ce4a5ebc6"
 
   url "https://github.com/defold/defold/releases/download/#{version}/Defold-x86_64-macos.dmg",
       verified: "github.com/defold/defold/"
