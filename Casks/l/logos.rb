a62066ef095efde1d9c5b149c8f1d8c6b0861e22
Rui Chen <rui@chenrui.dev>
2023-09-26 02:17:41 +0800

    logos 28.1.0.0042

--- Casks/l/logos.rb
+++ Casks/l/logos.rb
@@ -1,9 +1,9 @@
 cask "logos" do
   arch arm: "-arm"
 
-  version "28.0.0.0034"
-  sha256 arm:   "d8b30fb45ee66c69a1ff2ec04545ae6e1558ba536c30e75c8ac10e32dcb1cf89",
-         intel: "4eb3c66fa2b44d04afbfcaefab5c051d2deda87500dafa6ec18991d504bf7e22"
+  version "28.1.0.0042"
+  sha256 arm:   "a9f57e6204352c32a8ab2bb2fcad4a81a6d731d8e8ec949c9d8c7f3cf5412ffc",
+         intel: "056c3b1691aa70ab99618ab287367fab0d4a5bef92eb1db19f58e92d2e5a729b"
 
   url "https://downloads.logoscdn.com/LBS10/Installer/#{version}/LogosMac#{arch}.dmg",
       verified: "downloads.logoscdn.com/"
