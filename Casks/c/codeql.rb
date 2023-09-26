3be440c72a7689b6e529fd3c47e55c23d7a68def
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-26 09:43:21 -0400

    codeql 2.14.6

--- Casks/c/codeql.rb
+++ Casks/c/codeql.rb
@@ -1,6 +1,6 @@
 cask "codeql" do
-  version "2.14.5"
-  sha256 "0712e3edb3992cac7455911d78ebad74cea4eecc0a7781b4709a9ec95edf7276"
+  version "2.14.6"
+  sha256 "81f895f08db87e00f345446918436006c596a91348da87b0c6330c0ae40d215d"
 
   url "https://github.com/github/codeql-cli-binaries/releases/download/v#{version}/codeql-osx64.zip"
   name "CodeQL"
