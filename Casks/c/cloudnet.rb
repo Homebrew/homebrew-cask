165b84785bad67e71283643edc4ab17019330664
Jocker <yutaoo1230@gmail.com>
2023-09-26 11:04:21 +0800

    update: CloudNet 1.36.2.3

--- Casks/c/cloudnet.rb
+++ Casks/c/cloudnet.rb
@@ -1,15 +1,17 @@
 cask "cloudnet" do
-  version "1.36.2.2"
-  sha256 "952d6d7ec0c83fa74d1e9c15f283bbd90a3118a1949ec3fa0f180ec6f453fbfc"
+  version "1.36.2.3"
+  sha256 "a4f4d15a68beb292ef7ea7754fde5108f6561f2b84426b33dd7dd40fe5c75a56"
 
   url "https://pkgs.cloudnet.world/stable/macos/CloudNet_v#{version}.dmg"
   name "CloudNet for Mac client"
-  desc "Enterprise-level meshVPN cloud service"
+  desc "Professional and easy-to-use enterprise-level meshVPN product"
   homepage "https://cloudnet.world/"
 
+  # This appcast is slower to update than the submissions we get. See:
+  #   https://github.com/Homebrew/homebrew-cask/pull/90907#issuecomment-710107547
   livecheck do
     url "https://pkgs.cloudnet.world/stable/appcast.xml"
-    strategy :sparkle
+    strategy :sparkle, &:version
   end
 
   auto_updates true
@@ -29,14 +31,12 @@ cask "cloudnet" do
               args:       ["uninstall"],
               sudo:       true,
             },
-            delete:    [
-              "/Applications/CloudNet.app",
-              "/Library/LaunchDaemons/world.cloudnet.client.cloudnetd.plist",
-            ]
+            delete:    "/Applications/CloudNet.app"
 
   zap trash: [
+    "/Library/LaunchDaemons/world.cloudnet.client.cloudnetd.plist",
+    "~/Library/Preferences/world.cloudnet.client.cloudnetd.plist",
     "~/Library/Containers/world.cloudnet.client",
     "~/Library/Group Containers/$(TeamIdentifierPrefix)world.cloudnet.client",
-    "~/Library/Preferences/world.cloudnet.client.cloudnetd.plist",
   ]
 end
