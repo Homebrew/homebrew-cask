7e3e5f8603e1840193ec38992ca349ba2500c331
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-26 09:42:08 -0400

    drawio 21.8.2

--- Casks/d/drawio.rb
+++ Casks/d/drawio.rb
@@ -1,9 +1,9 @@
 cask "drawio" do
   arch arm: "arm64", intel: "x64"
 
-  version "21.7.5"
-  sha256 arm:   "7cf171b78db388dfc899a297c53441862561345b84ccb0fae3922cd2b4e8d641",
-         intel: "05ef6bda2bb24046e7fbe293d2dc9384f165727fac15d001afdc94706c058e81"
+  version "21.8.2"
+  sha256 arm:   "55a1e3a30ad41e681745a7a5933cd857360cb925715044bc6f7215dae25a8db0",
+         intel: "730ea0a6850699a27c72ed07808915bf6294b46e1a798b6c792f37cedb9a44bc"
 
   url "https://github.com/jgraph/drawio-desktop/releases/download/v#{version}/draw.io-#{arch}-#{version}.dmg",
       verified: "github.com/jgraph/drawio-desktop/"
