1a461eee984a8fee1bca414afbe6723a57af44f0
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-26 09:39:41 -0400

    mongodb-compass-isolated-edition 1.40.0

--- Casks/m/mongodb-compass-isolated-edition.rb
+++ Casks/m/mongodb-compass-isolated-edition.rb
@@ -1,9 +1,9 @@
 cask "mongodb-compass-isolated-edition" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.39.4"
-  sha256 arm:   "3f7b705a241a76cc175c336fb998167415bb29a69641dc14b10955933a5782f2",
-         intel: "5a89de2df284d4470c50cc401e353d3b35e3595849c358f088a03576bab99f23"
+  version "1.40.0"
+  sha256 arm:   "d45bd2f7bf972a304cde007a3bfb68f4e0b699f961241701ba8aac2f8a9e4516",
+         intel: "30bc7280f86478e738df8856712b6f3eecb042e0d2e3b699d193547b6708954b"
 
   url "https://downloads.mongodb.com/compass/mongodb-compass-isolated-#{version}-darwin-#{arch}.dmg"
   name "MongoDB Compass Isolated"
