9ee9ce28586da6db6800477d72291139511008cc
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-26 09:33:37 -0400

    ultimate-vocal-remover 5.6

--- Casks/u/ultimate-vocal-remover.rb
+++ Casks/u/ultimate-vocal-remover.rb
@@ -1,9 +1,9 @@
 cask "ultimate-vocal-remover" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "5.5.0"
-  sha256 arm:   "fecc265d04f885f7aaf572a88003311155a0bd9c4c811e17e9ec78aa99b0da67",
-         intel: "2804769a3a5976bdf1e9d3c32e8c9c056e17f83f17623275f4996f20623e9cb4"
+  version "5.6"
+  sha256 arm:   "276b1bbcf0e5de3b25df53b0855f4ae0a37af2ecaf0c10f59389ec53ca3b4513",
+         intel: "99f4528c6c98e82c441045b34d796ee1a36d6bc5ffe32ff6547da0a8514d9e0c"
 
   url "https://github.com/Anjok07/ultimatevocalremovergui/releases/download/v#{version}/Ultimate_Vocal_Remover_v#{version.major_minor.dots_to_underscores}_MacOS_#{arch}.dmg"
   name "Ultimate Vocal Remover"
