51e61b3842ee2659697fb301d98c13bdbe35975a
Sebastjan Rechberger <sebastjan.rechberger@agilcon.com>
2023-09-27 13:33:19 +0200

    Add Salesforce CLI Cask it is diferent than sfdx

---
+++ Casks/s/salesforce-cli.rb
@@ -0,0 +1,32 @@
+cask "salesforce-cli" do
+    arch arm: "arm64", intel: "x64"
+
+    version "2.9.8"
+    sha256 :no_check
+  
+    url "https://developer.salesforce.com/media/salesforce-cli/sf/channels/stable/sf-#{arch}.pkg"
+    name "Salesforce CLI"
+    desc "SalesForce CLI tools"
+    homepage "https://developer.salesforce.com/tools/salesforcecli"
+
+    livecheck do
+        url "https://github.com/forcedotcom/cli/blob/main/releasenotes/README.md"
+        regex(/(\d+(?:\.\d+)+).*?[stable]\]/i)
+        strategy :page_match
+    end
+  
+    pkg "sf-#{arch}.pkg"
+  
+    uninstall pkgutil: [
+                    "com.salesforce.cli",
+                    "com.salesforce.developer.cli",
+              ],
+              delete:  "/usr/local/bin/sf"
+
+    zap trash: [
+        "~/.cache/sf",
+        "~/.config/sf",
+        "~/.local/share/sf",
+        "~/.sf",
+    ]
+  end
