e04b41318db76dba75b261f30a96ed36e59a8617
Miccal Matthews <miccal.matthews@gmail.com>
2023-09-27 09:59:05 +0800

    Update affinity-designer.rb

--- Casks/a/affinity-designer.rb
+++ Casks/a/affinity-designer.rb
@@ -8,8 +8,8 @@ cask "affinity-designer" do
   homepage "https://affinity.serif.com/en-us/designer/"
 
   livecheck do
-    url "https://forum.affinity.serif.com/index.php?/topic/170203-latest-affinity-v2-releases-on-each-platform-by-store/"
-    regex(/Affinity\s*Designer\s*\d+\s*\|\s*Affinity\s*Store\s*\|\s*macOS[^|]*\|\s*(\d+(?:\.\d+)+)/i)
+    url :url
+    strategy :header_match
   end
 
   auto_updates true
