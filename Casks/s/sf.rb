b5af107c32d21ff92cbdd84607de91cfa5d93ad0
Bevan Kay <email@bevankay.me>
2023-09-28 09:06:51 +1000

    sf: version bump, versioned url, livecheck update

--- Casks/s/sf.rb
+++ Casks/s/sf.rb
@@ -1,20 +1,23 @@
 cask "sf" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.9.8"
-  sha256 :no_check
+  version "2.10.2,1c35561"
+  sha256 intel: "0b54d605dee853abe4c575f549271141ee1647532d2345b51cd1f2045fff3a87",
+         arm: "020d85b97be4834441b477efb3f801705eda7d44eed9ab4ef1a9ac43ae64f699"
 
-  url "https://developer.salesforce.com/media/salesforce-cli/sf/channels/stable/sf-#{arch}.pkg"
+  url "https://developer.salesforce.com/media/salesforce-cli/sf/versions/#{version.csv.first}/#{version.csv.second}/sf-v#{version.csv.first}-#{version.csv.second}-#{arch}.pkg"
   name "Salesforce CLI"
   desc "Salesforce CLI tools"
   homepage "https://developer.salesforce.com/tools/salesforcecli"
 
   livecheck do
-    url "https://raw.githubusercontent.com/forcedotcom/cli/main/releasenotes/README.md"
-    regex(/(\d+(?:\.\d+)+)\s+\(.*?\)\s+\[stable\]/i)
+    url "https://developer.salesforce.com/media/salesforce-cli/sf/channels/stable/sf-darwin-#{arch}-buildmanifest"
+    strategy :json do |json|
+      "#{json["version"]},#{json["sha"]}"
+    end
   end
 
-  pkg "sf-#{arch}.pkg"
+  pkg "sf-v#{version.csv.first}-#{version.csv.second}-#{arch}.pkg"
 
   uninstall pkgutil: "com.salesforce.cli",
             delete:  [
