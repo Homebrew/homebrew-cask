86c00dfddf62b3310081c8976f74c93a3575cab1
Sebastjan Rechberger <sebastjan.rechberger@agilcon.com>
2023-09-27 15:00:17 +0200

    changed version

--- Casks/s/salesforce-cli.rb
+++ Casks/s/salesforce-cli.rb
@@ -1,7 +1,7 @@
 cask "salesforce-cli" do
   arch arm: "arm64", intel: "x64"
 
-  version "5.0"
+  version "5.428578"
   sha256 arm:   "e31290dd504c535a99469b8bc1650fdba74e5c6580b679b3f740bb3b1fb01d4f",
          intel: "d2a2a46c0771e6c3bbd0d111ecb22fb684a53a28835207d7fe1c4a40154bdec6"
 
