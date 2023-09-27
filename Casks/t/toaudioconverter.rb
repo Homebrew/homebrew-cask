26fc5962a5653423aa13a0db393025e39e1c655d
SoCuul <63339559+SoCuul@users.noreply.github.com>
2023-09-27 03:32:24 -0700

    Add "To Audio Converter" by Amvidia

---
+++ Casks/t/toaudioconverter.rb
@@ -0,0 +1,22 @@
+ask "toaudioconverter" do
+  version "1.0.18,1173"
+  sha256 :no_check
+
+  url "https://amvidia.com/download-to-audio-converter-mac.dmg.zip"
+  name "To Audio Converter"
+  desc "Convert over 200 audio formats with a simplified interface"
+  homepage "https://amvidia.com/to-audio-converter"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
+
+  app "To Audio Converter.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.amvidia.To-Audio-Converter-FS",
+    "~/Library/Containers/To Audio Converter",
+    "~/Library/Saved Application State/com.amvidia.To-Audio-Converter-FS.savedState",
+  ]
+end
