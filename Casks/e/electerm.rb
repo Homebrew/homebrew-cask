63c17e1708b1b766038059dd2efbf2e77d238797
Rui Chen <rui@chenrui.dev>
2023-09-27 08:40:55 +0800

    electerm 1.33.0

--- Casks/e/electerm.rb
+++ Casks/e/electerm.rb
@@ -1,9 +1,9 @@
 cask "electerm" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.32.46"
-  sha256 arm:   "cc89ee7a664e8aaf76c4aa5074277eb58be257e1e9bb00c52fa6d92358ade801",
-         intel: "dfeda1ab6b3d3040a94eb8f24fd76f735206cc0e700a685381784f795ab28835"
+  version "1.33.0"
+  sha256 arm:   "0ceaf45c335b283f4e507411489a52cc5b8b0296d5b21fd8adf6376e310ac66f",
+         intel: "9de98479c0753c19d6a423d9d96d556033e1da02ee463c9f64a642d492ddfb15"
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac-#{arch}.dmg"
   name "electerm"
