6c01588509359d1d31a9a1c777c31e9961765ef7
miccal <miccal.matthews@gmail.com>
2023-09-27 12:45:48 +0800

    rectangle-pro 3.0.12

--- Casks/r/rectangle-pro.rb
+++ Casks/r/rectangle-pro.rb
@@ -1,6 +1,6 @@
 cask "rectangle-pro" do
-  version "3.0.11"
-  sha256 "1ecd9e44ee43a58a18d2b2df7261994478e683ec1d773ffe2c4d2ee04f600939"
+  version "3.0.12"
+  sha256 "6f88668a9854f2a0f2d311005d812e58a16d4a4b6f0035bd82dae23cd4a2ca06"
 
   url "https://rectangleapp.com/pro/downloads/Rectangle%20Pro%20#{version}.dmg"
   name "Rectangle Pro"
