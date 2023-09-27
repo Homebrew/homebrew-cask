2d3fbaafba85e7b8536335ce2962cdaf785556ba
Bevan Kay <email@bevankay.me>
2023-09-28 09:16:47 +1000

    fix style

--- Casks/s/sf.rb
+++ Casks/s/sf.rb
@@ -3,7 +3,7 @@ cask "sf" do
 
   version "2.10.2,1c35561"
   sha256 intel: "0b54d605dee853abe4c575f549271141ee1647532d2345b51cd1f2045fff3a87",
-         arm: "020d85b97be4834441b477efb3f801705eda7d44eed9ab4ef1a9ac43ae64f699"
+         arm:   "020d85b97be4834441b477efb3f801705eda7d44eed9ab4ef1a9ac43ae64f699"
 
   url "https://developer.salesforce.com/media/salesforce-cli/sf/versions/#{version.csv.first}/#{version.csv.second}/sf-v#{version.csv.first}-#{version.csv.second}-#{arch}.pkg"
   name "Salesforce CLI"
