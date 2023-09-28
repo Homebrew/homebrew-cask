eaccd35ff2c2ab09f5482336f4d7b0226ace32a6
Pascal Ochel <pascal.ochel@posteo.de>
2023-09-28 05:29:11 +0200

    Update AlDente sources and improve uninstall routine
    The developers do not use the davidwernhart identifiers any longer. The new source includes not only the free version, now the premium features can be unlocked.

--- Casks/a/aldente.rb
+++ Casks/a/aldente.rb
@@ -1,14 +1,14 @@
 cask "aldente" do
   version "1.22.2"
-  sha256 "6d1108d114b8c6f11c732c37c9e779683f281b9c260f98721bcdaa5b6b6a9440"
+  sha256 :no_check
 
-  url "https://github.com/davidwernhart/AlDente/releases/download/#{version}/AlDente.dmg"
+  url "https://apphousekitchen.com/aldente/AlDente-Pro.dmg"
   name "AlDente"
   desc "Menu bar tool to limit maximum charging percentage"
-  homepage "https://github.com/davidwernhart/AlDente"
+  homepage "https://apphousekitchen.com/"
 
   livecheck do
-    url :url
+    url "https://github.com/AppHouseKitchen/AlDente-Charge-Limiter"
     strategy :github_latest
   end
 
@@ -17,15 +17,16 @@ cask "aldente" do
 
   app "AlDente.app"
 
-  uninstall quit:   "com.davidwernhart.Helper",
-            delete: [
-              "/Library/LaunchDaemons/com.apphousekitchen.aldente-pro.helper.plist",
-              "/Library/PrivilegedHelperTools/com.apphousekitchen.aldente-pro.helper",
-            ]
+  uninstall quit:       "com.apphousekitchen.aldente-pro",
+            launchctl:  "com.apphousekitchen.aldente-pro.helper",
+            login_item: "AlDente",
+            delete:     "/Library/PrivilegedHelperTools/com.apphousekitchen.aldente-pro.helper"
 
   zap trash: [
     "~/Library/Application Support/AlDente",
     "~/Library/Caches/com.apphousekitchen.aldente-pro",
     "~/Library/HTTPStorages/com.apphousekitchen.aldente-pro.binarycookies",
+    "~/Library/Preferences/com.apphousekitchen.aldente-pro_backup.plist",
+    "~/Library/Preferences/com.apphousekitchen.aldente-pro.plist",
   ]
 end
