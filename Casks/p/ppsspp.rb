3dbaa98b10d8bfc035a0388065b3ffd2f5ca0ed4
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-28 09:08:09 -0400

    ppsspp 1.16.5

--- Casks/p/ppsspp.rb
+++ Casks/p/ppsspp.rb
@@ -1,6 +1,6 @@
 cask "ppsspp" do
-  version "1.16.4"
-  sha256 "936d49f44225fb6880b3f52a549e3f355fe35ac4bcb305e796aaf76121c4e47c"
+  version "1.16.5"
+  sha256 "b8b02e3f26e56561578647cac4cda9d9fcddcb03e7c6284bf6cbb00763315533"
 
   url "https://github.com/hrydgard/ppsspp/releases/download/v#{version}/PPSSPPSDL-macOS-v#{version}.zip",
       verified: "github.com/hrydgard/ppsspp/"
