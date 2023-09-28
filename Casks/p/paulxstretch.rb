2c05206b8474ff77f054ee2d2890473d12e6b3ae
albevier <13409891+albevier@users.noreply.github.com>
2023-09-28 12:20:51 -0700

    Changed url to github

--- Casks/p/paulxstretch.rb
+++ Casks/p/paulxstretch.rb
@@ -2,7 +2,7 @@ cask "paulxstretch" do
   version "1.6.0"
   sha256 "c5bcba5215076f629c3442902eaba4825a7f71ac33fc2d793c7cb729bdeb17ad"
 
-  url "https://sonosaurus.com/paulxstretch/releases/paulxstretch-#{version}-mac.dmg"
+  url "https://github.com/essej/paulxstretch/releases/download/#{version}/paulxstretch-#{version}-mac.dmg"
   name "paulxstretch"
   desc "Extreme time stretching plugin for audio files"
   homepage "https://github.com/essej/paulxstretch"
