085ea28fbc78304077bfc9d3a41c1851e2a32f56
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-28 11:26:12 -0400

    bruno 0.16.2

--- Casks/b/bruno.rb
+++ Casks/b/bruno.rb
@@ -1,9 +1,9 @@
 cask "bruno" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.16.0"
-  sha256 arm:   "35a7b3d5ff77e3dd13da315da060724f428a7e69547742a1fbc3be755d7337d4",
-         intel: "555eddba7e12951434ad0775416aaabc851a801534caa198eae917a3887d0e3c"
+  version "0.16.2"
+  sha256 arm:   "4d6fd2d3c4b213e855f6555948791567c847d71ce66235c4d3f7511af40583fd",
+         intel: "cd6b431f6aff5e929dd30d4f1a82a6a400a57354dafc6b678c21bc275acd1c5f"
 
   url "https://github.com/usebruno/bruno/releases/download/v#{version}/bruno_#{version}_#{arch}_mac.dmg",
       verified: "github.com/usebruno/bruno/"
