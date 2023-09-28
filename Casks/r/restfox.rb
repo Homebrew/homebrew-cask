8c235a4009400bd100a1a9ac9d7c10d9425fc2c6
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-28 14:07:11 -0400

    restfox 0.1.0

--- Casks/r/restfox.rb
+++ Casks/r/restfox.rb
@@ -1,6 +1,6 @@
 cask "restfox" do
-  version "0.0.9"
-  sha256 "405b880cca5beea996bc94be6caeafff467ca5c72d135c844db72d0f87f3ea5f"
+  version "0.1.0"
+  sha256 "6ebf72f80c4403f2d5f33112ee4b0e3ede4e273801e73ed5e2e8cc13d2ffa6bc"
 
   url "https://github.com/flawiddsouza/Restfox/releases/download/v#{version}/Restfox-darwin-x64-#{version}.zip",
       verified: "github.com/flawiddsouza/Restfox/releases/download/"
