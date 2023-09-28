aa5ddde84143ad7a06dc6c49ae98e198158a9b81
albevier <13409891+albevier@users.noreply.github.com>
2023-09-28 12:41:35 -0700

    Fixed livecheck issue

--- Casks/p/paulxstretch.rb
+++ Casks/p/paulxstretch.rb
@@ -2,14 +2,16 @@ cask "paulxstretch" do
   version "1.6.0"
   sha256 "c5bcba5215076f629c3442902eaba4825a7f71ac33fc2d793c7cb729bdeb17ad"
 
-  url "https://github.com/essej/paulxstretch/releases/download/#{version}/paulxstretch-#{version}-mac.dmg"
+  url "https://github.com/essej/paulxstretch/releases/download/v#{version}/paulxstretch-#{version}-mac.dmg"
   name "paulxstretch"
   desc "Extreme time stretching plugin for audio files"
   homepage "https://github.com/essej/paulxstretch"
 
+  livecheck do
+    strategy :github_latest
+  end
+
   pkg "PaulXStretch Installer.pkg"
+
   uninstall pkgutil: "com.sonosaurus.paulxstretch.pkg.app"
-  uninstall pkgutil: "com.sonosaurus.paulxstretch.pkg.au"
-  uninstall pkgutil: "com.sonosaurus.paulxstretch.pkg.vst3"
-  uninstall pkgutil: "com.sonosaurus.paulxstretch.pkg.axx"
 end
