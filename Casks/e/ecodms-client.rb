9e67a1d1258392fcdc9986c9ec7a5a64af5c3da3
Miccal Matthews <miccal.matthews@gmail.com>
2023-09-30 04:34:48 +0800

    Update and rename ecodms.rb to ecodms-client.rb

--- Casks/e/ecodms-client.rb
+++ Casks/e/ecodms-client.rb
@@ -1,18 +1,17 @@
-cask "ecodms" do
+cask "ecodms-client" do
   version "23.01"
-  sha256 "1bc94d3bc28a3b17fa525d8f9c5151450606a166bbb3752f3b2c19cdc9ed9226"
+  sha256 :no_check
 
   url "https://www.ecodms.de/index.php/de/component/jdownloads/?task=download.send&id=413&catid=162&m=0&Itemid=661"
-  name "ecoDMS"
+  name "ecoDMS Client"
   desc "Document Management System"
   homepage "https://www.ecodms.de/"
 
   livecheck do
-    url "https://www.ecodms.de/index.php/en/download/ecodms-archiv/ecodms-burns"
-    # TODO: define regex pattern and strategy
+    url :url
+    strategy :header_match
   end
 
-  auto_updates false
   depends_on macos: ">= :ventura"
 
   pkg "ecoDMS Clients_#{version}.pkg"
@@ -30,7 +29,5 @@ cask "ecodms" do
     "~/Library/Preferences/com.ecodms-gmbh.ecodmsclientarchivesettings.plist",
     "~/Library/Preferences/de.applord.ecoDMS-Client.plist",
     "~/Library/Saved Application State/de.applord.ecoDMS-Client.savedState",
-    "/private/var/folders/j3/zpp4m98n2pl8c12vbyrgcyhw0000gn/C/de.applord.ecoDMS-Client",
-    "/private/var/folders/j3/zpp4m98n2pl8c12vbyrgcyhw0000gn/C/de.applord.ecoDMS-Connection-Manager",
   ]
 end
