2d55547bf83de4ec52933dd3792140e220daa87a
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-29 09:21:49 -0400

    sfm 1.5.0

--- Casks/s/sfm.rb
+++ Casks/s/sfm.rb
@@ -1,6 +1,6 @@
 cask "sfm" do
-  version "1.4.6"
-  sha256 "1dc0c76ddfd2153f73f080fe46134e7e5dfaca477af7962c77ac38bb20c09e6f"
+  version "1.5.0"
+  sha256 "ffe2928f0e29803afb5a7ac7d91e9bf75493e8e1d72253e678cdd77eafb40f99"
 
   url "https://github.com/SagerNet/sing-box/releases/download/v#{version}/SFM-#{version}-universal.zip",
       verified: "github.com/SagerNet/sing-box/"
