534cf51d1e769679c6ec0f910a3ea955bfc0bc30
Rui Chen <rui@chenrui.dev>
2023-09-29 08:08:28 +0800

    gzdoom 4.11.0

--- Casks/g/gzdoom.rb
+++ Casks/g/gzdoom.rb
@@ -1,6 +1,6 @@
 cask "gzdoom" do
-  version "4.10.0"
-  sha256 "12eca4a3582eb9400b5a738e22fa80aa32a2bf50fb85dc28d41371a53b3df103"
+  version "4.11.0"
+  sha256 "62981787fea16f0a620af6bc93556a8931c03fe06f78fdccd9001357b3461f10"
 
   url "https://github.com/coelckers/gzdoom/releases/download/g#{version}/gzdoom-#{version.dots_to_hyphens}-macOS.zip",
       verified: "github.com/coelckers/gzdoom/"
