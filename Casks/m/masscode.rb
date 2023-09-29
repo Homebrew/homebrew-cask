7e28073d9c92f3ad541addd7853b1c9e38396484
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-29 09:05:56 -0400

    masscode 3.8.0

--- Casks/m/masscode.rb
+++ Casks/m/masscode.rb
@@ -1,9 +1,9 @@
 cask "masscode" do
   arch arm: "-arm64"
 
-  version "3.7.0"
-  sha256 arm:   "99e1bb681af61b190db98012eaa179d441c5eeeb28c3a7811cd2fb1be69f897c",
-         intel: "9cb708bddb096c7b16eff76574ae0cf23bc41e02fbf5c3667fd18a94ad655123"
+  version "3.8.0"
+  sha256 arm:   "4e6d1e87d393aca5293730def40761daf46a11948804aef6d0be80ed26849b24",
+         intel: "c1c2446c84b52b55c0ba1a0108267f2c0638dcfe5716cc35d2dcc4b53c22fded"
 
   url "https://github.com/massCodeIO/massCode/releases/download/v#{version}/massCode-#{version}#{arch}.dmg",
       verified: "github.com/massCodeIO/massCode/"
