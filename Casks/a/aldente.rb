61c93a5cc634d399437c4aa23c1441f91e25f7bf
Miccal Matthews <miccal.matthews@gmail.com>
2023-09-29 09:20:55 +0800

    Update aldente.rb

--- Casks/a/aldente.rb
+++ Casks/a/aldente.rb
@@ -1,15 +1,15 @@
 cask "aldente" do
-  version "1.22.2"
+  version "1.22.2,42"
   sha256 "6d1108d114b8c6f11c732c37c9e779683f281b9c260f98721bcdaa5b6b6a9440"
 
-  url "https://apphousekitchen.com/aldente/AlDente#{version}.dmg"
+  url "https://apphousekitchen.com/aldente/AlDente#{version.csv.first}.dmg"
   name "AlDente"
   desc "Menu bar tool to limit maximum charging percentage"
   homepage "https://apphousekitchen.com/"
 
   livecheck do
-    url "https://apphousekitchen.com/aldente/aldenteproappcast.xml"
-    strategy :sparkle, &:short_version
+    url "https://apphousekitchen.com/aldente/AlDente-Pro.dmg"
+    strategy :extract_plist
   end
 
   auto_updates true
