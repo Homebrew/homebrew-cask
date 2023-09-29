3a819a0a2722cdced0356c3dc11ec3b8f6e84d7c
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2023-09-29 10:39:11 +0700

    signal 6.32.0

--- Casks/s/signal.rb
+++ Casks/s/signal.rb
@@ -1,9 +1,9 @@
 cask "signal" do
   arch arm: "arm64", intel: "x64"
 
-  version "6.31.0"
-  sha256 arm:   "bd5c4f05c53a6a289f6512537b2abe026083408196cf530733e6b371161cfcfd",
-         intel: "7d275ee32f0c7653392dac006d011a3e0b769fd69334dde94d3bac6d13005bff"
+  version "6.32.0"
+  sha256 arm:   "957127a2004754ef25eb86dd06dd4a09469084e0adbcbaa5a612eb23feaa1813",
+         intel: "0947fd291a0c932b35af2690fc6672eb0731ef883a7bf50598d65a7b97fb2230"
 
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{arch}-#{version}.dmg"
   name "Signal"
