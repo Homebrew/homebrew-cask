93a925d9f22e60028af49907e34040e0c0cbf2d5
Rui Chen <rui@chenrui.dev>
2023-09-29 11:46:29 +0800

    chatall 1.48.72

--- Casks/c/chatall.rb
+++ Casks/c/chatall.rb
@@ -1,9 +1,9 @@
 cask "chatall" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.48.71"
-  sha256 arm:   "0e8cce2b4020263527f7695923c9dea90eda4ddf867c3479f9cd9da689efded0",
-         intel: "275d58a764e3c0a442c575148a529ecff4bd40e2c5d7c4e71c1ac77fa6f8491c"
+  version "1.48.72"
+  sha256 arm:   "e35ef307a1150caad4812fa74732e2a012d5aff14aeb4b33f59a9c445b43ec05",
+         intel: "821fd544ab82019d17b3f8dc63c5b2c84c0c5efbf525c4d5773a3611c531c113"
 
   url "https://github.com/sunner/ChatALL/releases/download/v#{version}/ChatALL-#{version}-mac-#{arch}.dmg"
   name "ChatALL"
