44d169e8fcb81a34af7a3840db4482e84a3b951b
Rui Chen <rui@chenrui.dev>
2023-09-29 11:01:17 +0800

    multiviewer-for-f1 1.26.10

--- Casks/m/multiviewer-for-f1.rb
+++ Casks/m/multiviewer-for-f1.rb
@@ -2,12 +2,12 @@ cask "multiviewer-for-f1" do
   arch arm: "arm64", intel: "x64"
 
   on_arm do
-    version "1.26.9,125346790"
-    sha256 "c27bb6ef2173b45313cd5dd6b176c1551bba1db9e5db3fc7b10e88d9d380009e"
+    version "1.26.10,128048240"
+    sha256 "7aa6a31e79d446b3d3cc3f9bcf4b4d15e33cdc8cff6cc67aa390bf8f43e7c2dc"
   end
   on_intel do
-    version "1.26.9,125347169"
-    sha256 "447f3f5c16cebfdb1ac07e8b05e612dceebb46c2aa7920cc40c65f7c7cc0b5d8"
+    version "1.26.10,128048860"
+    sha256 "5a0987c1294da61632ad2af8fcd9409353078667e371ac3012045ccd89954488"
   end
 
   url "https://releases.multiviewer.app/download/#{version.csv.second}/MultiViewer.for.F1-#{version.csv.first}-#{arch}.dmg"
