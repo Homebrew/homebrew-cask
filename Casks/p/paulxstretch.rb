10d8d4c6b0a23c9cff499808b5e07c96c875f8f9
Miccal Matthews <miccal.matthews@gmail.com>
2023-09-29 09:07:31 +0800

    Update paulxstretch.rb

--- Casks/p/paulxstretch.rb
+++ Casks/p/paulxstretch.rb
@@ -3,13 +3,9 @@ cask "paulxstretch" do
   sha256 "c5bcba5215076f629c3442902eaba4825a7f71ac33fc2d793c7cb729bdeb17ad"
 
   url "https://github.com/essej/paulxstretch/releases/download/v#{version}/paulxstretch-#{version}-mac.dmg"
-  name "paulxstretch"
+  name "PaulXStretch"
   desc "Extreme time stretching plugin for audio files"
   homepage "https://github.com/essej/paulxstretch"
-
-  livecheck do
-    strategy :github_latest
-  end
 
   pkg "PaulXStretch Installer.pkg"
 
