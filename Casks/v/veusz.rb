fc8b5ceaecb270f44b3c3b3152bfa503f653fbb5
Sam Ford <1584702+samford@users.noreply.github.com>
2023-09-30 01:12:03 -0400

    veusz 3.6.2

--- Casks/v/veusz.rb
+++ Casks/v/veusz.rb
@@ -1,28 +1,23 @@
 cask "veusz" do
-  version "3.6"
-  sha256 "c23d7593724f5398747c6858bc99fdc950633631dfc8d9224730eed682ada8f6"
+  version "3.6.2"
+  sha256 "cc09d7491c64259b5b4e06004719c350f7e9a5a472173cf293c8ad067056c002"
 
-  url "https://github.com/veusz/veusz/releases/download/veusz-#{version.major_minor}/veusz-#{version}-AppleOSX.dmg",
+  url "https://github.com/veusz/veusz/releases/download/veusz-#{version}/veusz-#{version}-AppleOSX.dmg",
       verified: "github.com/veusz/veusz/"
   name "Veusz"
   desc "Scientific plotting application"
   homepage "https://veusz.github.io/"
 
   livecheck do
-    url "https://github.com/veusz/veusz/releases/latest"
-    regex(/href=.*?veusz[._-]v?(\d+(?:\.\d+)+)(?:[._-][^"' >]+)?\.dmg/i)
-    strategy :header_match do |headers, regex|
-      next if headers["location"].blank?
+    url :url
+    regex(/^veusz[._-]v?(\d+(?:\.\d+)+)(?:[._-].+)?\.dmg$/i)
+    strategy :github_latest do |json, regex|
+      json["assets"]&.map do |asset|
+        match = asset["name"]&.match(regex)
+        next if match.blank?
 
-      # Identify the latest tag from the response's `location` header
-      latest_tag = File.basename(headers["location"])
-      next if latest_tag.blank?
-
-      # Fetch the assets list HTML for the latest tag and match within it
-      assets_page = Homebrew::Livecheck::Strategy.page_content(
-        @url.sub(%r{/releases/?.+}, "/releases/expanded_assets/#{latest_tag}"),
-      )
-      assets_page[:content]&.scan(regex)&.map { |match| match[0] }
+        match[1]
+      end
     end
   end
 
