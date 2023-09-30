3e00a868678faa7449b1f483b7f4edd416a2d61c
Sam Ford <1584702+samford@users.noreply.github.com>
2023-09-29 23:57:45 -0400

    p/pure-writer: update livecheck

--- Casks/p/pure-writer.rb
+++ Casks/p/pure-writer.rb
@@ -9,20 +9,15 @@ cask "pure-writer" do
   homepage "https://writer.drakeet.com/desktop"
 
   livecheck do
-    url "https://github.com/PureWriter/desktop/releases/latest"
-    regex(%r{href=.*?([^/]+)/PureWriter[._-]v?(\d+(?:\.\d+)+)-macOS\.dmg}i)
-    strategy :header_match do |headers, regex|
-      next if headers["location"].blank?
+    url :url
+    regex(%r{/v?(\d+(?:\.\d+)+)/PureWriter[._-]v?(\d+(?:\.\d+)+)-macOS\.dmg$}i)
+    strategy :github_latest do |json, regex|
+      json["assets"]&.map do |asset|
+        match = asset["browser_download_url"]&.match(regex)
+        next if match.blank?
 
-      # Identify the latest tag from the response's `location` header
-      latest_tag = File.basename(headers["location"])
-      next if latest_tag.blank?
-
-      # Fetch the assets list HTML for the latest tag and match within it
-      assets_page = Homebrew::Livecheck::Strategy.page_content(
-        @url.sub(%r{/releases/?.+}, "/releases/expanded_assets/#{latest_tag}"),
-      )
-      assets_page[:content]&.scan(regex)&.map { |match| "#{match[0]},#{match[1]}" }
+        "#{match[1]},#{match[2]}"
+      end
     end
   end
 
