750d08fe05e84f30e6a8c030ba55de73e34e4b23
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-10-02 09:04:39 -0400

    wavebox 10.117.21.2

--- Casks/w/wavebox.rb
+++ Casks/w/wavebox.rb
@@ -1,9 +1,9 @@
 cask "wavebox" do
   arch arm: "macarm64", intel: "mac"
 
-  version "10.117.18.2"
-  sha256 arm:   "4364bac6b85c6b9547448d284c66f2c1a1c0f001c7af731f17fa6fb0b61b4241",
-         intel: "c33c24fd64726d7f5615936cfa4e07c4bdb5c238bfc051b452466f18f5959e4c"
+  version "10.117.21.2"
+  sha256 arm:   "f6773c3b0db839a610338ec72691d5626d27e3333baf7dcea86c06981238023a",
+         intel: "52e7160824bd295c7447e65e66364a86ec1fba93f6b27de61cc61aaa2476f1ab"
 
   url "https://download.wavebox.app/stable/#{arch}/Install%20Wavebox%20#{version}.dmg",
       verified: "download.wavebox.app/"
