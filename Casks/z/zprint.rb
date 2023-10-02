c4e91dd0b260bf71ef74d2d3cef3da1639fcfa02
Bevan Kay <email@bevankay.me>
2023-10-02 17:15:26 +1000

    Update zprint.rb

--- Casks/z/zprint.rb
+++ Casks/z/zprint.rb
@@ -1,17 +1,16 @@
 cask "zprint" do
-  arch intel: "zprintm",
-       arm:   "zprintma"
+  arch arm:   "a"
 
   version "1.2.8"
   sha256 intel: "03c146a2d411325114f38d211c3d4a9f2ab837b9287c46a7387114319253b2bd",
          arm:   "11246e16db97cb03649bf67938455c7cb25a3326a7e99e45d019b0ae405d8c3d"
 
-  url "https://github.com/kkinnear/zprint/releases/download/#{version}/#{arch}-#{version}"
+  url "https://github.com/kkinnear/zprint/releases/download/#{version}/zprintm#{arch}-#{version}"
   name "zprint"
   desc "Library to reformat Clojure and Clojurescript source code and s-expressions"
   homepage "https://github.com/kkinnear/zprint"
 
-  binary "#{arch}-#{version}", target: "zprint"
+  binary "zprintm#{arch}-#{version}", target: "zprint"
 
   # No zap stanza required
 end
