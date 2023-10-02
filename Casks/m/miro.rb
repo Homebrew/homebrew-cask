1eb0f49c7784529532e503c525a3489c17be4d49
Rui Chen <rui@chenrui.dev>
2023-10-03 07:34:25 +0800

    miro 0.7.47

--- Casks/m/miro.rb
+++ Casks/m/miro.rb
@@ -1,7 +1,7 @@
 cask "miro" do
   arch arm: "darwin-arm64", intel: "darwin"
 
-  version "0.7.45"
+  version "0.7.47"
   sha256 :no_check
 
   url "https://desktop.miro.com/platforms/#{arch}/Miro.dmg"
