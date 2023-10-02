db9142e803d58a00ab159955bdcd052de122b8c1
Rui Chen <rui@chenrui.dev>
2023-10-03 06:35:59 +0800

    opera-gx 102.0.4880.85

--- Casks/o/opera-gx.rb
+++ Casks/o/opera-gx.rb
@@ -1,6 +1,6 @@
 cask "opera-gx" do
-  version "102.0.4880.82"
-  sha256 "ac5078743c76fe4391c9ba59ccf889d47505382812b6b7881740cebfff1d592b"
+  version "102.0.4880.85"
+  sha256 "27af60facf04289a39d1af5a443153ef0525fa18e02a97458afb7969af5f7961"
 
   url "https://get.geo.opera.com/pub/opera_gx/#{version}/mac/Opera_GX_#{version}_Setup.dmg"
   name "Opera GX"
