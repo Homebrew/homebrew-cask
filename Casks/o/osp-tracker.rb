53e58c8ef0922acfd0f040cbc763db4f702e3731
Rui Chen <rui@chenrui.dev>
2023-10-03 06:40:18 +0800

    osp-tracker 6.1.4

--- Casks/o/osp-tracker.rb
+++ Casks/o/osp-tracker.rb
@@ -1,6 +1,6 @@
 cask "osp-tracker" do
-  version "6.1.3"
-  sha256 "cb4d0579d7d2dbbd3134fc8052d295d412c60e60cd02823223f099bb00b3509b"
+  version "6.1.4"
+  sha256 "9942d542ea7fde4f6b64b35c1ce982efb7c6fe80ec16de541b5a05b6e6812214"
 
   url "https://physlets.org/tracker/installers/download.php?file=Tracker-#{version}-osx-installer.dmg"
   name "Tracker"
