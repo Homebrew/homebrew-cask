c924ee1fda3ae84bba33cecd8c2ba71b3c2bf6e3
David Duthie <dave.a.duthie@gmail.com>
2023-10-02 08:30:46 +0200

    Install separate binaries for intel, arm

--- Casks/z/zprint.rb
+++ Casks/z/zprint.rb
@@ -1,13 +1,17 @@
 cask "zprint" do
-  version "1.2.8"
-  sha256 "03c146a2d411325114f38d211c3d4a9f2ab837b9287c46a7387114319253b2bd"
+  arch intel: "zprintm",
+       arm:   "zprintma"
 
-  url "https://github.com/kkinnear/zprint/releases/download/#{version}/zprintm-#{version}"
+  version "1.2.8"
+  sha256 intel: "03c146a2d411325114f38d211c3d4a9f2ab837b9287c46a7387114319253b2bd",
+         arm:   "11246e16db97cb03649bf67938455c7cb25a3326a7e99e45d019b0ae405d8c3d"
+
+  url "https://github.com/kkinnear/zprint/releases/download/#{version}/#{arch}-#{version}"
   name "zprint"
   desc "Library to reformat Clojure and Clojurescript source code and s-expressions"
   homepage "https://github.com/kkinnear/zprint"
 
-  binary "zprintm-#{version}", target: "zprint"
+  binary "#{arch}-#{version}", target: "zprint"
 
   # No zap stanza required
 end
