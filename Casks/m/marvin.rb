69870b732c6080c1048a603eb33b6acbf94315f5
Nihaal Sangha <nihaal.git@gmail.com>
2023-10-02 12:25:33 +0100

    Use zip instead of dmg for `marvin`

--- Casks/m/marvin.rb
+++ Casks/m/marvin.rb
@@ -3,10 +3,10 @@ cask "marvin" do
   hostname_arch = on_arch_conditional arm: "amarm", intel: "amazingmarvin"
 
   version "1.63.0"
-  sha256 arm:   "0e0bc18c1274f3f6a2d45dd45ddd6448f0a917d8de5cd68a3a69eaabd0595903",
-         intel: "8f6c4ca13956c41731086e2112fe79418e00248544e61d713b3ea9ff8d89c77a"
+  sha256 arm:   "46bd20dbef2ca137e9452fec120ac454b4eeca038954ea2af054eb4d830785d6",
+         intel: "3a3c44968e66274785c928545db34833d3c9369dd2fee17288de7054ce78ae66"
 
-  url "https://#{hostname_arch}.s3.amazonaws.com/Marvin-#{version}#{arch}.dmg",
+  url "https://#{hostname_arch}.s3.amazonaws.com/Marvin-#{version}#{arch}-mac.zip",
       verified: "#{hostname_arch}.s3.amazonaws.com/"
   name "Amazing Marvin"
   desc "Personal productivity app"
