f840116d42822d9f5428e586e9d033ba870ceb3f
Miccal Matthews <miccal.matthews@gmail.com>
2023-10-02 16:59:59 +0800

    Update and rename figmaagent.rb to figma-agent.rb

--- Casks/f/figma-agent.rb
+++ Casks/f/figma-agent.rb
@@ -1,16 +1,14 @@
-cask "figmaagent" do
+cask "figma-agent" do
   version "116.13.3"
   sha256 :no_check
 
   url "https://desktop.figma.com/agent/mac/InstallFigmaAgent.dmg"
-  name "Figma Font Installers"
+  name "Figma Agent"
+  name "Font installers for Figma.app"
   homepage "https://www.figma.com/"
 
   livecheck do
-    url "https://desktop.figma.com/mac/RELEASE.json"
-    strategy :json do |json|
-      json["version"]
-    end
+    cask "figma"
   end
 
   installer manual: "Install Figma Agent.app"
