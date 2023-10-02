a7b61bd53c13765747aab217d409999c8f9099ae
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-10-02 09:04:09 -0400

    waterfox 6.0.3

--- Casks/w/waterfox.rb
+++ Casks/w/waterfox.rb
@@ -1,6 +1,6 @@
 cask "waterfox" do
-  version "6.0.2"
-  sha256 "d5b4488f72112f76ac58a60bd3bf5e4208b8ca7a1cbf47a95643d7fc3327195f"
+  version "6.0.3"
+  sha256 "e22905d54490235b4f851625b067a9a60464f3cc1965d004862d2b222986581f"
 
   url "https://cdn1.waterfox.net/waterfox/releases/G#{version}/Darwin_x86_64-aarch64/Waterfox%20G#{version}.dmg"
   name "Waterfox"
