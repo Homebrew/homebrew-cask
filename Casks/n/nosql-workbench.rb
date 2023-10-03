bdb486c936d466b5b1c7ee334b4e23a9b108b27b
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-10-03 13:15:33 -0400

    nosql-workbench 3.8.2

--- Casks/n/nosql-workbench.rb
+++ Casks/n/nosql-workbench.rb
@@ -1,6 +1,6 @@
 cask "nosql-workbench" do
-  version "3.8.1"
-  sha256 "62651ea937298a82cd910f630b554b5517b71e19de5df23f5adda3783dfc67ac"
+  version "3.8.2"
+  sha256 "7991066ff8a0a145023c44e58fbc39f3314275df446ce196019dfaf2d2f6b7d0"
 
   url "https://s3.amazonaws.com/nosql-workbench/NoSQL%20Workbench-mac-#{version}.dmg",
       verified: "s3.amazonaws.com/nosql-workbench/"
