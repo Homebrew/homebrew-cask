df122c9708be0038cfe947d4d01f0dc247442ef0
Miccal Matthews <miccal.matthews@gmail.com>
2023-10-04 19:15:26 +0800

    Update fedora-media-writer.rb

--- Casks/f/fedora-media-writer.rb
+++ Casks/f/fedora-media-writer.rb
@@ -1,16 +1,16 @@
 cask "fedora-media-writer" do
-  version "5.0.6"
-  sha256 "7fa0a444b926523fb6284b643c3b650bfca77adb74145a13d8eb37d1be43cf56"
+  version "5.0.8"
+  sha256 "21d3fbe8cdac4be64fbf1a04e508d695ac116a9b1168c28e2569da85075e248c"
 
-  url "https://github.com/FedoraQt/MediaWriter/releases/download/#{version}/FedoraMediaWriter-osx-#{version}.notarized.dmg",
+  url "https://github.com/FedoraQt/MediaWriter/releases/download/#{version}/FedoraMediaWriter-osx-#{version}.unnotarized.dmg",
       verified: "github.com/FedoraQt/MediaWriter/"
   name "Fedora Media Writer"
   desc "Tool to write Fedora images to portable media files"
   homepage "https://docs.fedoraproject.org/en-US/quick-docs/creating-and-using-a-live-installation-image/"
 
+  depends_on macos: ">= :mojave"
+
   app "Fedora Media Writer.app"
 
-  zap trash: [
-    "~/Library/Saved Application State/org.fedoraproject.MediaWriter.savedState",
-  ]
+  zap trash: "~/Library/Saved Application State/org.fedoraproject.MediaWriter.savedState"
 end
