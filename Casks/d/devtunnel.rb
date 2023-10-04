f253d5af5cfe0d236cf555860280693732c3e32e
Miccal Matthews <miccal.matthews@gmail.com>
2023-10-05 05:37:25 +0800

    Update devtunnel.rb

--- Casks/d/devtunnel.rb
+++ Casks/d/devtunnel.rb
@@ -2,7 +2,7 @@ cask "devtunnel" do
   arch arm: "arm64", intel: "x64"
 
   version "1.0.922+fa34179138"
-  sha256 arm:   "3d81eaa2bd1d5be99489ac378c2925f2076f01d1a505044b0e51902ce052a868",
+  sha256 arm:   "065e86312e3513aa2a5bd6ea2f0822ca3a50ad6981e497043ded6b1630044737",
          intel: "d9875469d96c563302cb68d1bd6834bde6ba949ace57714a86ecd0108073da1e"
 
   url "https://tunnelsassetsprod.blob.core.windows.net/cli/#{version}/osx-#{arch}-devtunnel-zip",
