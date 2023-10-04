90cbf241c8cb7be24d20f1b139a5a48241bbedaf
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-10-03 20:24:35 -0400

    topaz-video-ai 3.5.2

--- Casks/t/topaz-video-ai.rb
+++ Casks/t/topaz-video-ai.rb
@@ -1,6 +1,6 @@
 cask "topaz-video-ai" do
-  version "3.5.1"
-  sha256 "b2d6d40570decc483194eb81e8e6381de1b519d25496bec7e5e183956a0c4364"
+  version "3.5.2"
+  sha256 "1ba3967006fb9ccd64cfb22acd06d1884000e9f6aef7db0d22a2da14cb7c1cde"
 
   url "https://downloads.topazlabs.com/deploy/TopazVideoAI/#{version}/TopazVideoAI-#{version}.dmg"
   name "Topaz Video AI"
