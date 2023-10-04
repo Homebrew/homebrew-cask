9625fba853776354c77599e5c7ef678d6d929872
Selen Taskin <setaskin@Selens-Mac-mini.local>
2023-10-04 08:23:24 -0700

    fix sha

--- Casks/d/devtunnel.rb
+++ Casks/d/devtunnel.rb
@@ -3,7 +3,7 @@ cask "devtunnel" do
 
   version "1.0.922+fa34179138"
   sha256 arm:   "3d81eaa2bd1d5be99489ac378c2925f2076f01d1a505044b0e51902ce052a868",
-         intel: "d9875469d96c563302cb68d1bd6834bde6ba949ace57714a86ecd0108073da1e"
+         intel: "c48f72ec265cf152929143672ccc090ff9739b925bbf56e54621ea55657e8f54"
   
   url "https://tunnelsassetsprod.blob.core.windows.net/cli/#{version}/osx-#{arch}-devtunnel-zip",
     verified: "tunnelsassetsprod.blob.core.windows.net/cli/"
