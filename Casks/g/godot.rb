603e5b28015afc43d73cbb9d6cd310444ca16bd0
junminhong <junminhong1110@gmail.com>
2023-10-04 18:32:54 +0800

    bump-godot-4.1.2

--- Casks/g/godot.rb
+++ Casks/g/godot.rb
@@ -1,6 +1,6 @@
 cask "godot" do
-  version "4.1.1"
-  sha256 "bbfe748f094fb655c32e0655d9c187b7c76031220d55a9f67a8e360756170c40"
+  version "4.1.2"
+  sha256 "8ddff74a6fb77b2cfda02cc43a637a01268dd32dd0e81e64570c7703a9bfd412"
 
   url "https://downloads.tuxfamily.org/godotengine/#{version}/Godot_v#{version}-stable_macos.universal.zip",
       verified: "downloads.tuxfamily.org/godotengine/"
