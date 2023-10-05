62abf60d523911fa9354b6d88b611687cc0c55a0
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-10-05 08:14:31 -0400

    accordance 14.0.8

--- Casks/a/accordance.rb
+++ Casks/a/accordance.rb
@@ -1,6 +1,6 @@
 cask "accordance" do
-  version "14.0.7"
-  sha256 "457713505eded617c7938b91998ca5c1fe0b813d52d82dd683b5d1a629090f7c"
+  version "14.0.8"
+  sha256 "c5634124ac26bd7a1028c4edacb5ebd7d3e3f341f211459104d00a2791cce944"
 
   url "https://d366ra67x2bs56.cloudfront.net/mac/Acc_#{version}.zip",
       verified: "d366ra67x2bs56.cloudfront.net/"
