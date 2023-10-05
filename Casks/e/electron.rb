1914c02e91b6b92785c2a9008a6a25e6e442c68c
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2023-10-05 09:24:48 +0700

    electron 26.3.0

--- Casks/e/electron.rb
+++ Casks/e/electron.rb
@@ -1,9 +1,9 @@
 cask "electron" do
   arch arm: "arm64", intel: "x64"
 
-  version "26.2.4"
-  sha256 arm:   "dda224e19ff2d2c99624e1da7d20fa24b92a34b49fac8dcef15542e183bc89c6",
-         intel: "a1e33c66a13913306e80812a9051ce7e5632d7cc13ff76910cc8daa791580589"
+  version "26.3.0"
+  sha256 arm:   "97cb2d00d06f331b4c028fa96373abdd7b5a71c2aa31b56cdf67d391f889f384",
+         intel: "ea9434ad717f12771f8c508b664ed8d18179b397910ce81f4b6e21efce90b754"
 
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-#{arch}.zip",
       verified: "github.com/electron/electron/"
