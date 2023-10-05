8bf615de45415cd0da41bebc60c3850f44c841e0
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-10-05 08:17:18 -0400

    chromedriver 117.0.5938.149

--- Casks/c/chromedriver.rb
+++ Casks/c/chromedriver.rb
@@ -1,9 +1,9 @@
 cask "chromedriver" do
   arch arm: "arm64", intel: "x64"
 
-  version "117.0.5938.92"
-  sha256 arm:   "17d44cd652110a9b3814552d313f70ffbe3831930bb73c24ff823fb11549d5b3",
-         intel: "67d0e2fff494d5b180ac84d35b5484493ea9bb176218edc228c0826c6ee1158d"
+  version "117.0.5938.149"
+  sha256 arm:   "7fcd2c535b583a4a128f3cd881fd9657fcb56a2cbc3ff916cffa371ab1bab31d",
+         intel: "016f95b8ed5e037ed08f572be7d6ca6c488a4fc2997d511220b548eaaeaad01b"
 
   url "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/#{version}/mac-#{arch}/chromedriver-mac-#{arch}.zip",
       verified: "edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/"
