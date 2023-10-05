f69e26ef738dbd936aa0a4d7dfed4a7cbcd153e6
Miccal Matthews <miccal.matthews@gmail.com>
2023-10-05 10:36:24 +0800

    Update devtunnel.rb

--- Casks/d/devtunnel.rb
+++ Casks/d/devtunnel.rb
@@ -1,11 +1,10 @@
 cask "devtunnel" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.0.922+fa34179138"
-  sha256 arm:   "065e86312e3513aa2a5bd6ea2f0822ca3a50ad6981e497043ded6b1630044737",
-         intel: "d9875469d96c563302cb68d1bd6834bde6ba949ace57714a86ecd0108073da1e"
+  version :latest
+  sha256 :no_check
 
-  url "https://tunnelsassetsprod.blob.core.windows.net/cli/#{version}/osx-#{arch}-devtunnel-zip",
+  url "https://tunnelsassetsprod.blob.core.windows.net/cli/osx-#{arch}-devtunnel-zip",
       verified: "tunnelsassetsprod.blob.core.windows.net/cli/"
   name "Microsoft Dev Tunnels"
   desc "Provides developers secure tunnels to share local services"
