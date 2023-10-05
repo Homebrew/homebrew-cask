9181962c5125e2fd8b7ca720bf8ac772c32d2228
Rui Chen <rui@chenrui.dev>
2023-10-05 16:52:06 +0800

    rar 6.24

--- Casks/r/rar.rb
+++ Casks/r/rar.rb
@@ -1,9 +1,9 @@
 cask "rar" do
   arch arm: "arm", intel: "x64"
 
-  version "6.23"
-  sha256 arm:   "574410f00a90483a585872c8b484d379f4aeefe6e2f4256966c5d98cbf6c43f0",
-         intel: "afebcd94a9d3457aa14d883a8ff7c3b5356cf0fd105f2d53abfa33ac3d382986"
+  version "6.24"
+  sha256 arm:   "dabe335834fb5f0fc236f03ba0d12c19fad7243cc56b980db61201ff94d84795",
+         intel: "e2f10d3cd7cca50b2db26f7ef3e8251cf2bd7c09439a71d66c21d6f87530497e"
 
   url "https://www.rarlab.com/rar/rarmacos-#{arch}-#{version.no_dots}.tar.gz"
   name "RAR Archiver"
