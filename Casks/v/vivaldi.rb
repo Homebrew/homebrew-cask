c63080c19ed976eb5c1c4b7bf1895f88a4081e0b
ik-fib <ik11235@gmail.com>
2023-10-06 20:38:13 +0900

    vivaldi 6.2.3105.58

--- Casks/v/vivaldi.rb
+++ Casks/v/vivaldi.rb
@@ -1,6 +1,6 @@
 cask "vivaldi" do
-  version "6.2.3105.56"
-  sha256 "ee72458d51fb5ad9cfc2ee80abeecc95b6aa642e06cdc03792740c16ed74efb0"
+  version "6.2.3105.58"
+  sha256 "32ea170f9d43be45c4b62bb79dbc861456981293c4a907476a701e4f409750e1"
 
   url "https://downloads.vivaldi.com/stable-auto/Vivaldi.#{version}.universal.tar.xz"
   name "Vivaldi"
@@ -13,7 +13,7 @@ cask "vivaldi" do
   end
 
   auto_updates true
-  depends_on macos: ">= :high_sierra"
+  depends_on macos: ">= :catalina"
 
   app "Vivaldi.app"
 
