7adf14d1ff9dd9bf07041e56d31e0d223eeda6a5
synkathairo <20174932+synkathairo@users.noreply.github.com>
2023-10-05 21:52:30 -0400

    revise style

--- Casks/p/palemoon.rb
+++ Casks/p/palemoon.rb
@@ -1,6 +1,7 @@
 cask "palemoon" do
   arch arm: "arm", intel: "intel"
-  version '32.4.1'
+
+  version "32.4.1"
   sha256 :no_check
 
   url "https://www.palemoon.org/download.php?mirror=us&bits=64&type=mac#{arch}"
@@ -10,7 +11,7 @@ cask "palemoon" do
 
   livecheck do
     url "https://repo.palemoon.org/MoonchildProductions/Pale-Moon/releases.rss"
-    regex(/(?<=<title>Pale Moon )(.*?)(?=<\/title>)/)
+    regex(%r{(?<=<title>Pale Moon )(.*?)(?=</title>)})
     strategy :page_match
   end
 
