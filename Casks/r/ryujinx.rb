87f3ac225d4d4fff3cb409a4b140a88b0de4e300
gerrydoro <gerrydoro97@gmail.com>
2023-10-06 23:14:00 +0200

    Added new "ryujinx" cask

---
+++ Casks/r/ryujinx.rb
@@ -0,0 +1,27 @@
+cask "ryujinx" do
+    version "1.1.1045"
+    sha256 "3ef5918bf73e4c68041802a394ead65b9a9c72be9951f41e43cce830e181e8b3"
+
+    url "https://github.com/Ryujinx/release-channel-master/releases/download/#{version}/test-ava-ryujinx-#{version}-macos_universal.app.tar.gz",
+        verified: "github.com/Ryujinx/release-channel-master/"
+    name "Ryujinx"
+    desc "Experimental Nintendo Switch Emulator written in C#"
+    homepage "https://ryujinx.org/"
+  
+    livecheck do
+      url :url
+      strategy :github_latest
+    end
+  
+    auto_updates true
+    depends_on macos: ">= :big_sur"
+  
+    app "Ryujinx.app"
+  
+    zap trash: [
+      "~/Library/Application Support/Ryujinx",
+      "~/Library/Application Support/CrashReporter/Ryujinx_*.plist",
+      "~/Library/Preferences/org.ryujinx.Ryujinx.plist",
+      "~/Library/Saved Application State/org.ryujinx.Ryujinx.savedState",
+    ]
+  end
