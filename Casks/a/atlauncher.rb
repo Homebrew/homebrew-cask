dad99afab28a4764c992621c6692f13d5f83ed26
Rui Chen <rui@chenrui.dev>
2023-10-06 19:52:38 +0800

    atlauncher 3.4.34.2

--- Casks/a/atlauncher.rb
+++ Casks/a/atlauncher.rb
@@ -1,6 +1,6 @@
 cask "atlauncher" do
-  version "3.4.34.1"
-  sha256 "a5caf0d5aa899e4be09c2d67d0059d937e6ac4d2188ca295d3b71338c073aada"
+  version "3.4.34.2"
+  sha256 "ddaf83ab3014217568cb08289eb683079293fc184e5e2f2fac1050ab91bf814b"
 
   url "https://github.com/ATLauncher/ATLauncher/releases/download/v#{version}/ATLauncher-#{version}.zip",
       verified: "github.com/ATLauncher/ATLauncher/"
