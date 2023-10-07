044c12674fe507463f782ca41b4ef997fcc766a1
Edward <73746306+WangEdward@users.noreply.github.com>
2023-10-07 19:36:11 +0800

    devkinsta 2.11.0

--- Casks/d/devkinsta.rb
+++ Casks/d/devkinsta.rb
@@ -1,16 +1,19 @@
 cask "devkinsta" do
-  version "2.7.0.4846"
-  sha256 :no_check
+  arch arm: "arm64", intel: "x64"
 
-  url "https://devkinsta-updates.s3.amazonaws.com/DevKinsta.dmg",
+  version "2.11.0"
+  sha256 arm:   "a7ee5ba5f42cb3c864140b216097403a200fb21b0ade230fe317784e816fa334",
+         intel: "e929d4fbce20a27e54dd5b970376190e55c75529cf3a5b6f64a53cb904d07152"
+
+  url "https://devkinsta-updates.s3.amazonaws.com/DevKinsta-#{version}-#{arch}.zip",
       verified: "devkinsta-updates.s3.amazonaws.com/"
   name "DevKinsta"
   desc "Local WordPress Development Suite by Kinsta"
   homepage "https://devkinsta.com/"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url "https://devkinsta-updates.s3.amazonaws.com/latest-mac.yml"
+    strategy :electron_builder
   end
 
   app "DevKinsta.app"
