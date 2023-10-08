4144431ad4b464914a0af6134857c53c71b1aac6
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-10-08 12:59:14 -0400

    update version and livecheck

--- Casks/s/scrivener.rb
+++ Casks/s/scrivener.rb
@@ -1,8 +1,8 @@
 cask "scrivener" do
-  version "3.3.3,15931"
-  sha256 :no_check
+  version "3.3.3,15931,1013"
+  sha256 "c01fbfd30dc99717af75dfac36b21cb397c7bcc034b12368187054b82ec89081"
 
-  url "https://scrivener.s3.amazonaws.com/Scrivener.dmg",
+  url "https://scrivener.s3.amazonaws.com/mac_updates/Scrivener_#{version.csv.third}_#{version.csv.second}.zip",
       verified: "scrivener.s3.amazonaws.com/"
   name "Scrivener"
   desc "Word processing software with a typewriter style"
@@ -10,7 +10,10 @@ cask "scrivener" do
 
   livecheck do
     url "https://www.literatureandlatte.com/downloads/scrivener-#{version.major}.xml"
-    strategy :sparkle
+    regex(/scrivener[._-](\d*?)[._-].*\.zip/i)
+    strategy :sparkle do |item, regex|
+      "#{item.short_version},#{item.version},#{item.url[regex, 1]}"
+    end
   end
 
   auto_updates true
