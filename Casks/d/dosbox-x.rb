fe7f26786e9cf79e7365d287eee400f24d360c09
Rui Chen <rui@chenrui.dev>
2023-10-09 06:27:46 +0800

    dosbox-x 2023.10.06

--- Casks/d/dosbox-x.rb
+++ Casks/d/dosbox-x.rb
@@ -2,12 +2,12 @@ cask "dosbox-x" do
   arch arm: "arm64", intel: "x86_64"
 
   on_arm do
-    version "2023.09.01,20230901150441"
-    sha256 "6f150e6d17254d3b1130a5b88162f00196380dd02bddabf9a8bc758b56490a3b"
+    version "2023.10.06,20231006231203"
+    sha256 "d77c3bc3b26e63adb87ad6dac740a9338e62e13ba5d5137c229c2b71b8566c3b"
   end
   on_intel do
-    version "2023.09.01,20230901155520"
-    sha256 "169e4da416de3dc43c8817043ebfa7b9807c6cd092be6f2ce9d9715baaa03bfe"
+    version "2023.10.06,20231006231157"
+    sha256 "22fd4908b464c1c8d9742f63d054ec52f7a747a981e5d1e86c126d3984f9f590"
   end
 
   url "https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v#{version.csv.first}/dosbox-x-macosx-#{arch}-#{version.csv.second}.zip",
