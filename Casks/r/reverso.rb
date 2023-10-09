ca9914f6130fe2dec20ae3d95e6ae43d584a9bde
Rui Chen <rui@chenrui.dev>
2023-10-09 21:31:01 +0800

    reverso 2.11.0

--- Casks/r/reverso.rb
+++ Casks/r/reverso.rb
@@ -1,6 +1,6 @@
 cask "reverso" do
-  version "2.10.0,481"
-  sha256 "30bdeb4b30965e215759391d6eadbc531d6e34520458370a565004064bc27123"
+  version "2.11.0,496"
+  sha256 "e3780cc6a2efddf40b90c114a090c3d01f5da75893112a4606ae790da2357750"
 
   url "https://cdn.reverso.net/download/reverso/desktop/macos/distrib/Reverso_#{version.csv.first}.#{version.csv.second}.zip"
   name "Reverso"
