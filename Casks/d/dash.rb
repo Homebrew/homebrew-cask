e86776e215971604f478db73f7d0b3df9ee58a94
Rui Chen <rui@chenrui.dev>
2023-10-10 00:58:14 +0800

    dash 7.1.3

--- Casks/d/dash.rb
+++ Casks/d/dash.rb
@@ -4,8 +4,8 @@ cask "dash" do
     sha256 "e2b5eb996645b25f12ccae15e24b1b0d8007bc5fed925e14ce7be45a2b693fb6"
   end
   on_mojave :or_newer do
-    version "7.1.2"
-    sha256 "24fd0c3e75ca4834e87cd6b92a9f076406e4d21f2a0af23612ed697292a5179c"
+    version "7.1.3"
+    sha256 "4e1df170483e444a1ba0b61d4307ba944c653505c8b136de8295e5978b20eea4"
   end
 
   url "https://kapeli.com/downloads/v#{version.major}/Dash.zip"
