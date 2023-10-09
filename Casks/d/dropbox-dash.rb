6c982dfd87a7d23b3f86c42e2b194da854fc5432
Rui Chen <rui@chenrui.dev>
2023-10-10 02:17:00 +0800

    dropbox-dash 2.83.2

--- Casks/d/dropbox-dash.rb
+++ Casks/d/dropbox-dash.rb
@@ -1,6 +1,6 @@
 cask "dropbox-dash" do
-  version "2.77.1"
-  sha256 "e148468fbae575d0934f7fab29bfa5abde3f1f37990dc1816c65e122b2fb0c88"
+  version "2.83.2"
+  sha256 "088902a48256bc9b27102f028c7ee0af91dae47ef9922148f888c5fbe661e01d"
 
   url "https://dash-releases.s3.amazonaws.com/Dropbox%20Dash-#{version}.dmg",
       verified: "dash-releases.s3.amazonaws.com/"
