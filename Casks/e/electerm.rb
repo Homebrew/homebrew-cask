5dd238b467814662bba474856d03f494c1f5e8ca
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-10-09 10:02:16 -0400

    electerm 1.34.8

--- Casks/e/electerm.rb
+++ Casks/e/electerm.rb
@@ -1,9 +1,9 @@
 cask "electerm" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.34.6"
-  sha256 arm:   "2ee72dcdea4fa873b613b623711b6c7fc876db5486c17cd4053527da8c553e42",
-         intel: "271ed1b8dff9b7269535ce0905e2b21bfe294115b3fc2d36c98b4841c288c05c"
+  version "1.34.8"
+  sha256 arm:   "239a617b4abd83579f27094bb553f64207375fab9fba4d63608ebae1f07e07e5",
+         intel: "8c9106d72085b2155c8c90e2900200295a386e62ba4156a3784222424952a6a8"
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac-#{arch}.dmg"
   name "electerm"
