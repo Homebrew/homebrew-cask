c185fbf9f88978fc92a6775bbfe4c6debdcdcb34
Jonathan de Jong <jonathandejong02@gmail.com>
2023-10-09 11:28:35 +0200

    bump xpra and add arm64 versioning

--- Casks/x/xpra.rb
+++ Casks/x/xpra.rb
@@ -1,15 +1,23 @@
 cask "xpra" do
-  version "5.0.2,3"
-  sha256 "4fce517e0547dc5cd6b26c08df9f42c4ad30bdfe7eebc9e0d0f4f4c6f280de9a"
+  arch arm: "arm64", intel: "x86_64"
 
-  url "https://www.xpra.org/dists/osx/x86_64/Xpra-x86_64-#{version.csv.first}-r#{version.csv.second}.pkg"
+  on_arm do
+    version "5.0.3,0"
+    sha256 "34a6b8c831b5a31a87dbd93898c3afb415ab2096faccea0c4530ada4ccbef9f1"
+  end
+  on_intel do
+    version "5.0.2,3"
+    sha256 "4fce517e0547dc5cd6b26c08df9f42c4ad30bdfe7eebc9e0d0f4f4c6f280de9a"
+  end
+
+  url "https://www.xpra.org/dists/osx/#{arch}/Xpra-#{arch}-#{version.csv.first}-r#{version.csv.second}.pkg"
   name "Xpra"
   desc "Screen and application forwarding system"
   homepage "https://www.xpra.org/"
 
   livecheck do
-    url "https://www.xpra.org/dists/osx/x86_64/Xpra-x86_64.pkg.sha1"
-    regex(/x86_64[._-]v?(\d+(?:\.\d+)+)[._-]r(\d+)\.pkg/i)
+    url "https://www.xpra.org/dists/osx/#{arch}/Xpra-#{arch}.pkg.sha1"
+    regex(/#{arch}[._-]v?(\d+(?:\.\d+)+)[._-]r(\d+)\.pkg/i)
     strategy :page_match do |page, regex|
       page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
     end
@@ -17,7 +25,7 @@ cask "xpra" do
 
   depends_on macos: ">= :sierra"
 
-  pkg "Xpra-x86_64-#{version.csv.first}-r#{version.csv.second}.pkg"
+  pkg "Xpra-#{arch}-#{version.csv.first}-r#{version.csv.second}.pkg"
 
   uninstall pkgutil: "org.xpra.pkg",
             delete:  [
