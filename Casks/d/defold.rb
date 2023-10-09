a157432cbc7a99e2f47455a46ad7e2312d6830eb
Miccal Matthews <miccal.matthews@gmail.com>
2023-10-09 22:10:49 +0800

    Update defold.rb

--- Casks/d/defold.rb
+++ Casks/d/defold.rb
@@ -2,8 +2,7 @@ cask "defold" do
   arch arm: "arm64", intel: "x86_64"
 
   version "1.6.0"
-  sha256 arm:   "0314a6deb42d1a332f95a5f15876283cceea3b6fd8badd76b9537aa349c23f50",
-         intel: "91ab893541ded87c612c141cd39bad62f527b1dc4f863ed0be35745d7acb5394"
+  sha256 :no_check # required as upstream package is updated in-place
 
   url "https://github.com/defold/defold/releases/download/#{version}/Defold-#{arch}-macos.dmg",
       verified: "github.com/defold/defold/"
