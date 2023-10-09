658bfa35faf4916cb234bd9a987e908c20bfa868
Bevan Kay <email@bevankay.me>
2023-10-09 15:20:25 +1100

    epic 117.0.5938.92

--- Casks/e/epic.rb
+++ Casks/e/epic.rb
@@ -1,17 +1,17 @@
 cask "epic" do
-  arch arm: "m1"
+  arch arm: "arm", intel: "intel"
 
-  version "103.0.5060.53"
-  sha256 arm:   "445d5344376c5f586350fb53ed04f7535dd48c48029e0f4a18a5b9be45995b8f",
-         intel: "d3e352ae9cd7aa342ff8d9b89f87be9b94e7887c38ac5fb9b92e9e2089a721ba"
+  version "117.0.5938.92"
+  sha256 arm:   "bac7bdf1da335ff1acb0f5136e9e9e871944acfd6a49f4d4e520abb7e5ee08d5",
+         intel: "996a1a80c86183c97af33f1ea19ee594dda7586a4db3b94a08e066776863f663"
 
-  url "https://cdn.epicbrowser.com/v100/#{arch}/epic_#{version}.dmg"
+  url "https://cdn.epicbrowser.com/mac#{version.major}#{arch}/epic_#{version}.dmg"
   name "Epic Privacy Browser"
   desc "Private, secure web browser"
-  homepage "https://www.epicbrowser.com/"
+  homepage "https://epicbrowser.com/"
 
   livecheck do
-    url "https://epicbrowser.com/thank_you.php"
+    url "https://epicbrowser.com/thank-you"
     regex(%r{href=.*?/epic[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
   end
 
