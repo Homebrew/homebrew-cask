23678dba0c5f1657319440eff49234aed85723dc
Rui Chen <rui@chenrui.dev>
2023-10-10 17:47:46 -0400

    snowflake-snowsql 1.2.29

--- Casks/s/snowflake-snowsql.rb
+++ Casks/s/snowflake-snowsql.rb
@@ -1,6 +1,6 @@
 cask "snowflake-snowsql" do
-  version "1.2.28"
-  sha256 "24644650811c720847bc6f8e04fc5dea080265a893dc0dc32f2ab044dbf5f206"
+  version "1.2.29"
+  sha256 "cd60a137c7918febaacf03407150dc325d4e2f1d87b3476052e385b3fe120203"
 
   url "https://sfc-repo.snowflakecomputing.com/snowsql/bootstrap/#{version.major_minor}/darwin_x86_64/snowsql-#{version}-darwin_x86_64.pkg",
       verified: "sfc-repo.snowflakecomputing.com/"
