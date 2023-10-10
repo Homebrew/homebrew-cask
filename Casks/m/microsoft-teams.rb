48ab86a1d16410e46b3a4534dcfcf742cac5d2ab
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2023-10-10 12:52:47 +0200

    microsoft-teams 23247.720.2421.8365

--- Casks/m/microsoft-teams.rb
+++ Casks/m/microsoft-teams.rb
@@ -1,12 +1,12 @@
 cask "microsoft-teams" do
-  version "1.6.00.27656"
-  sha256 "cd4cc25e0b4d24037e414226e201176e5be5d350d4d35be73fc531f11bc7ad2a"
+  version "23247.720.2421.8365"
+  sha256 "d27227e722499b3f15e38812b01f48d39bb99caddb5756643ba32201e7aa55e0"
 
-  url "https://statics.teams.cdn.office.net/production-osx/#{version}/Teams_osx.pkg",
+  url "https://statics.teams.cdn.office.net/production-osx/#{version}/MicrosoftTeams.pkg",
       verified: "statics.teams.cdn.office.net/production-osx/"
   name "Microsoft Teams"
   desc "Meet, chat, call, and collaborate in just one place"
-  homepage "https://teams.microsoft.com/downloads"
+  homepage "https://www.microsoft.com/en-us/microsoft-teams/group-chat-software"
 
   # Microsoft releases multiple versions and builds of Teams, as listed here:
   #   https://raw.githubusercontent.com/ItzLevvie/MicrosoftTeams-msinternal/master/defconfig
@@ -19,22 +19,33 @@ cask "microsoft-teams" do
   # Any pull request that updates this Cask to a version that
   # differs from the `livecheck` version will be closed.
   livecheck do
-    url "https://aka.ms/teamsmac"
-    strategy :header_match
+    url "https://statics.teams.cdn.office.net/production-osx/enterprise/webview2/lkg/MicrosoftTeams.pkg"
+    strategy :extract_plist
   end
 
   auto_updates true
   conflicts_with cask: "microsoft-office-businesspro"
+  depends_on cask: "microsoft-auto-update"
+  depends_on macos: ">= :catalina"
 
-  pkg "Teams_osx.pkg"
+  pkg "MicrosoftTeams.pkg",
+      choices: [
+        {
+          "choiceIdentifier" => "com.microsoft.autoupdate",
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 0,
+        },
+      ]
 
   uninstall pkgutil:   [
               "com.microsoft.MSTeamsAudioDevice",
-              "com.microsoft.teams",
+              "com.microsoft.package.Microsoft_AutoUpdate.app",
+              "com.microsoft.teams2",
             ],
             launchctl: "com.microsoft.teams.TeamsUpdaterDaemon",
+            quit:      "com.microsoft.autoupdate2",
             delete:    [
-              "/Applications/Microsoft Teams.app",
+              "/Applications/Microsoft Teams (work or school).app",
               "/Library/Logs/Microsoft/Teams",
               "/Library/Preferences/com.microsoft.teams.plist",
             ]
