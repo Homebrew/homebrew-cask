75a88548f94a296377b42eb08a8fc6631d3341ec
Rui Chen <rui@chenrui.dev>
2023-10-10 07:17:38 -0400

    electron 27.0.0

--- Casks/e/electron.rb
+++ Casks/e/electron.rb
@@ -1,9 +1,9 @@
 cask "electron" do
   arch arm: "arm64", intel: "x64"
 
-  version "26.3.0"
-  sha256 arm:   "97cb2d00d06f331b4c028fa96373abdd7b5a71c2aa31b56cdf67d391f889f384",
-         intel: "ea9434ad717f12771f8c508b664ed8d18179b397910ce81f4b6e21efce90b754"
+  version "27.0.0"
+  sha256 arm:   "2fc319c53f6dc61e2e424d46712caead7022b5124c9674f3b15b45c556dd0623",
+         intel: "8c2b944f3949265526410704ecd925c85ebb20d61f5c739081336bd1d29bd083"
 
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-#{arch}.zip",
       verified: "github.com/electron/electron/"
