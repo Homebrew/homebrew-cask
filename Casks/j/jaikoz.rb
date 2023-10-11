d98babf1435d5b271ae64bb9eb71906ba78ddf96
Jeff Byrnes <thejeffbyrnes@gmail.com>
2023-10-11 16:26:11 -0400

    Jaikoz: enable install for Apple Silicon

--- Casks/j/jaikoz.rb
+++ Casks/j/jaikoz.rb
@@ -1,8 +1,10 @@
 cask "jaikoz" do
+  arch arm: "-m1"
+
   version "11.7,1.0"
   sha256 :no_check
 
-  url "https://www.jthink.net/jaikoz/jsp/manualdownload/jaikoz-osx.dmg"
+  url "https://www.jthink.net/jaikoz/jsp/manualdownload/jaikoz-osx#{arch}.dmg"
   name "Jaikoz"
   desc "Audio tag editor"
   homepage "https://www.jthink.net/jaikoz/"
