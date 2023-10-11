f7f2a25fd686e393b965bcd76bb2ea7e5c7044e0
Rui Chen <rui@chenrui.dev>
2023-10-10 22:04:18 -0400

    quicken 7.3.2

--- Casks/q/quicken.rb
+++ Casks/q/quicken.rb
@@ -1,6 +1,6 @@
 cask "quicken" do
-  version "7.3.1,703.50428.100"
-  sha256 "62ef2d78074975a3423555509909f35db5eaef9a93c2dc32afd25c975481e965"
+  version "7.3.2,703.50456.100"
+  sha256 "a11ff716c3e911ca54258cf339247774175e03c68bd2bee054c945b488f2b6d8"
 
   url "https://download.quicken.com/mac/Quicken/001/Release/031A96D9-EFE6-4520-8B6A-7F465DDAA3E4/Quicken-#{version.csv.second}/Quicken-#{version.csv.second}.zip"
   name "Quicken"
