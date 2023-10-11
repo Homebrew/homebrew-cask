3e5b387a0148d8a86155746bb761f60c46a974cd
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-10-11 10:14:11 -0400

    vassal 3.7.2
    * Update for version 3.7.2
    
    * Update to universal dmg

--- Casks/v/vassal.rb
+++ Casks/v/vassal.rb
@@ -1,11 +1,8 @@
 cask "vassal" do
-  arch arm: "aarch64", intel: "x86_64"
+  version "3.7.2"
+  sha256 "4d33fe62b848d5d3bfa0e4fb32693141022146949a1c78e5740e398deebfcd25"
 
-  version "3.7.1"
-  sha256 arm:   "d02cfd0b2b77b50aee851ee18e60af63ef63d0a2ef3fde6cd186f02b478f6128",
-         intel: "b2328641728af6fc751aaeedf0c9686baff62d2408e03cfcac5e01482621d4fb"
-
-  url "https://github.com/vassalengine/vassal/releases/download/#{version}/VASSAL-#{version}-macos-#{arch}.dmg",
+  url "https://github.com/vassalengine/vassal/releases/download/#{version}/VASSAL-#{version}-macos-universal.dmg",
       verified: "github.com/vassalengine/vassal/"
   name "VASSAL"
   desc "Board game engine"
