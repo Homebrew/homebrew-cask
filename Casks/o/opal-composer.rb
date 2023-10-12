eaf4b786aac40c5d4fdb44d892eb45c411443c49
Bevan Kay <email@bevankay.me>
2023-10-12 20:16:10 +1000

    Update opal-composer.rb

--- Casks/o/opal-composer.rb
+++ Casks/o/opal-composer.rb
@@ -2,7 +2,8 @@ cask "opal-composer" do
   version "1.1.1,5"
   sha256 "82ddfe5c8375caf3523b7409fa32fc34612b5d8e755fe9a1d4b08502112c5bda"
 
-  url "https://updates.opal.camera/release/Opal_Composer_#{version.csv.first}_#{version.csv.second}.dmg"
+  url "https://updates.opal.camera/release/Opal_Composer_#{version.csv.first}_#{version.csv.second}.dmg",
+      verified: "updates.opal.camera/release/"
   name "Opal Composer"
   desc "Professional webcam software for the Opal C1"
   homepage "https://opalcamera.com/opal-composer"
