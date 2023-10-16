a8e724fd1223b444f552b6d870b803722f59443e
Rui Chen <rui@chenrui.dev>
2023-10-16 09:30:50 -0400

    wetype 0.9.9,217

--- Casks/w/wetype.rb
+++ Casks/w/wetype.rb
@@ -1,6 +1,6 @@
 cask "wetype" do
-  version "0.9.9,214"
-  sha256 "d67d45a5deb39c460b35e311eb8f66c45347773e2619e58616b8de251b707cb7"
+  version "0.9.9,217"
+  sha256 "445c02aa9f6da4b3885ecb2995f9b1f02097c7149eeaf9f2aa1ed3d9e6b1d96c"
 
   url "https://wetype.wxqcloud.qq.com/app/mac/#{version.csv.first}/WeTypeInstaller_#{version.csv.first}_#{version.csv.second}.zip"
   name "WeType"
