1e2affbe439889ba4a28f1f9e26f62e995717391
Rui Chen <rui@chenrui.dev>
2023-10-16 19:39:33 -0400

    descript 75.1.0-release.20231016.27303

--- Casks/d/descript.rb
+++ Casks/d/descript.rb
@@ -1,9 +1,9 @@
 cask "descript" do
   arch arm: "-arm64"
 
-  version "74.1.3-release.20231012.27005"
-  sha256 arm:   "2acaa9da5960530603dc8538d7bd72b989ca916ffdb32d3c3281a0a290e3b550",
-         intel: "f9d57832b6e7ca1f41b44cccebe38e883476d4fa245276ef513429d28c663f9e"
+  version "75.1.0-release.20231016.27303"
+  sha256 arm:   "42c37c133fe46ce8089c595f871051ce53358479d49ac0a6871f13631af3a73a",
+         intel: "d31f87ad8cdf46df3ab8bf00cea03ac9deaf764f2582047653b9d1456875930e"
 
   url "https://electron.descript.com/Descript-#{version}#{arch}.dmg"
   name "Descript"
