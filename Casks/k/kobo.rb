881937f06e8be836e8b722b6ed50af344ae4faca
Eugene <e@egeek.me>
2023-10-16 07:13:38 +0300

    Update the download URL for "Kobo"
    With the previous URL, `brew outdated --cask --greedy` would always fail
    with this message:
    
    ```
    Error: Download failed on Cask 'kobo' with message: Download failed: https://kbdownload1-a.akamaihd.net/desktop/kobodesktop/kobosetup.dmg
    ```
    
    The new URL is taken from the homepage. This should fix the `brew
    outdated` error.

--- Casks/k/kobo.rb
+++ Casks/k/kobo.rb
@@ -2,8 +2,8 @@ cask "kobo" do
   version :latest
   sha256 :no_check
 
-  url "https://kbdownload1-a.akamaihd.net/desktop/kobodesktop/kobosetup.dmg",
-      verified: "kbdownload1-a.akamaihd.net/"
+  url "https://cdn.kobo.com/downloads/desktop/kobodesktop/kobosetup.dmg",
+      verified: "cdn.kobo.com"
   name "Kobo"
   homepage "https://www.kobo.com/desktop"
 
