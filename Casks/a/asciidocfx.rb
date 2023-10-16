0beed615ebcab484effe672641326c4746ee5be4
Tin Kyaw <tin@lsvp.com>
2023-10-16 10:42:20 -0700

    asciidocfx 1.8.6

--- Casks/a/asciidocfx.rb
+++ Casks/a/asciidocfx.rb
@@ -2,8 +2,8 @@ cask "asciidocfx" do
   arch arm: "_M1"
 
   version "1.8.6"
-  sha256 arm:   "12b6eaad44c25243d4c3ddd3594fc8e76a8ead2ae14fc45e011ba3fe70dfa036",
-         intel: "cb8c1d71d77b7a5516e81ea775c6b7596d5d253016e3d7590a9f160653822a4f"
+  sha256 arm:   "cc3912340f5da0f3b39eda6b384e4e94985fdcef9f9af9ba978f6904029ff632",
+         intel: "2dcceeca0232652ef60e79ba3e9ed730e086050c8c598c810acded4c8c989956"
 
   url "https://github.com/asciidocfx/AsciidocFX/releases/download/v#{version}/AsciidocFX_Mac#{arch}.dmg",
       verified: "github.com/asciidocfx/AsciidocFX/"
