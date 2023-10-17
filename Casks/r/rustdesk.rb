40a3d3d7e81e70e0337312dcb23ed0f9ccfb277a
nthumann <me@n-thumann.de>
2023-10-17 22:59:54 +0200

    rustdesk: Fix shasum

--- Casks/r/rustdesk.rb
+++ Casks/r/rustdesk.rb
@@ -3,7 +3,7 @@ cask "rustdesk" do
 
   version "1.2.3"
   sha256 arm:   "647bc014238086d7a73a3d8f1543a1a559b5240f1796e1311a2142be0b57152e",
-         intel: "dafdbbaa0d8d60e5928df0b7523ac9a6c7d156dbe20c72068b2f46fbbbdba162"
+         intel: "63f1f5bbe2a308495be039604886adc83937bc36e97b09d4fd9b675ee108ee09"
 
   url "https://github.com/rustdesk/rustdesk/releases/download/#{version}/rustdesk-#{version}-#{arch}.dmg",
       verified: "github.com/rustdesk/rustdesk/"
