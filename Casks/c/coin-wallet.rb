ad2ed64b1a72176cdb3b5765dde32fbb1f3935fd
Patrick Linnane <patrick@linnane.io>
2023-10-17 10:15:52 -0700

    coin-wallet 6.0.0

--- Casks/c/coin-wallet.rb
+++ Casks/c/coin-wallet.rb
@@ -1,8 +1,8 @@
 cask "coin-wallet" do
-  version "5.10.5"
-  sha256 "67d6e9c26cc0e00da3bfdf08e5b8709983180bdfff531b8a57961a4d9c05857c"
+  version "6.0.0"
+  sha256 "074e65578db779aab2170ed0be8c9393fa08007b1060c9c08807944253cd5151"
 
-  url "https://github.com/CoinSpace/CoinSpace/releases/download/v#{version}/Coin.Wallet.dmg",
+  url "https://github.com/CoinSpace/CoinSpace/releases/download/v#{version}/Coin-Wallet.dmg",
       verified: "github.com/CoinSpace/CoinSpace/"
   name "Coin Wallet"
   desc "Digital currency wallet"
