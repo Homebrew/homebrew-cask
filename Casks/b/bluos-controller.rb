93f56603db9859ca01f6bf57e85f07764b0708b5
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-10-17 16:33:48 -0400

    bluos-controller 4.0.0

--- Casks/b/bluos-controller.rb
+++ Casks/b/bluos-controller.rb
@@ -1,8 +1,8 @@
 cask "bluos-controller" do
-  version "3.20.6,2023,08"
-  sha256 "f991bb0f881aaec0e8ab52c34fb7c9e1638920be2f85436df6e6ba97c798a785"
+  version "4.0.0,2023,10"
+  sha256 "3576b437b9285cde2205277105f7669560b29c19516ce2cdb7390043989bff14"
 
-  url "https://content-bluesound-com.s3.amazonaws.com/uploads/#{version.csv.second}/#{version.csv.third}/BluOS-Controller-#{version.csv.first}.dmg",
+  url "https://content-bluesound-com.s3.amazonaws.com/uploads/#{version.csv.second}/#{version.csv.third}/BluOS-Controller-#{version.csv.first}-MacOS.zip",
       verified: "content-bluesound-com.s3.amazonaws.com/uploads/"
   name "BluOS Controller"
   desc "Manage audio systems"
@@ -10,7 +10,7 @@ cask "bluos-controller" do
 
   livecheck do
     url "https://www.bluesound.com/downloads"
-    regex(%r{uploads/(\d+)/(\d+)/BluOS[._-]Controller[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
+    regex(%r{uploads/(\d+)/(\d+)/BluOS[._-]Controller[._-]v?(\d+(?:\.\d+)+)[._-]MacOS\.zip}i)
     strategy :page_match do |page, regex|
       page.scan(regex).map { |match| "#{match[2]},#{match[0]},#{match[1]}" }
     end
