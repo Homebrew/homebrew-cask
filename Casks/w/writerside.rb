aaff3cae5ddfedeb474b491c2cfb98cde9dfbbf1
Hong Minhee <hong.minhee@gmail.com>
2023-10-17 13:38:15 +0900

    writerside 2023.2,232.10165 (new cask)

---
+++ Casks/w/writerside.rb
@@ -0,0 +1,43 @@
+cask "writerside" do
+  arch arm: "-aarch64"
+
+  version "2023.2,232.10165"
+  sha256 arm:   "c43ff0a06e9f057bb3d0a9f979439e14e25aa9eeb83e5aa47e65e79a81b6156e",
+         intel: "78cf63c8ae5d129766e6e146a5e943078103f2990e70b20406d2a5c76ebfaf06"
+
+  url "https://download.jetbrains.com/writerside/writerside-#{version.csv.second}#{arch}.dmg"
+  name "Writerside"
+  desc "Technical writing environment"
+  homepage "https://www.jetbrains.com/writerside/"
+
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=WRS&latest=true&type=eap"
+    strategy :json do |json|
+      json["WRS"].map do |release|
+        "#{release["version"]},#{release["build"]}"
+      end
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
+  app "Writerside #{version.before_comma} EAP.app", target: "Writerside.app"
+
+  uninstall_postflight do
+    ENV["PATH"].split(File::PATH_SEPARATOR).map { |path| File.join(path, "writerside") }.each do |path|
+      if File.readable?(path) &&
+         File.readlines(path).grep(/# see com.intellij.idea.SocketLock for the server side of this interface/).any?
+        File.delete(path)
+      end
+    end
+  end
+
+  zap trash: [
+    "~/Library/Application Support/JetBrains/Writerside#{version.major_minor}",
+    "~/Library/Caches/JetBrains/Writerside#{version.major_minor}",
+    "~/Library/Logs/JetBrains/Writerside#{version.major_minor}",
+    "~/Library/Preferences/com.jetbrains.writerside-EAP.plist",
+    "~/Library/Saved Application State/com.jetbrains.writerside-EAP.SavedState",
+  ]
+end
