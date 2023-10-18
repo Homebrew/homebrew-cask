4d3244a6652a55c5b30f6b3e3270bbdb29c6d920
Patrick Linnane <patrick@linnane.io>
2023-10-18 12:35:36 -0700

    geotag-photos-pro: add livecheck

--- Casks/g/geotag-photos-pro.rb
+++ Casks/g/geotag-photos-pro.rb
@@ -8,5 +8,10 @@ cask "geotag-photos-pro" do
   desc "Geotagging software"
   homepage "https://www.geotagphotos.net/"
 
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
   app "Geotag Photos Pro 2.app"
 end
