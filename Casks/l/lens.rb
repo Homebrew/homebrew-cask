836c2f72823f3b75725169589d20551e127a4627
Rui Chen <rui@chenrui.dev>
2023-10-18 11:34:44 -0400

    lens 2023.10.181418

--- Casks/l/lens.rb
+++ Casks/l/lens.rb
@@ -1,9 +1,9 @@
 cask "lens" do
   arch arm: "-arm64"
 
-  version "2023.10.122002"
-  sha256 arm:   "3754cdfba8f4155d263b6841f80246cce5f71011991c62fd184808050db852fd",
-         intel: "e46a3c8c3bc62c7b3cd97a9434139192303b3f6713a4dd5d4310e9541fe4ca8b"
+  version "2023.10.181418"
+  sha256 arm:   "81e038527c800ed5fca82635f72152afd8b1ded561e420b3318804ee855743e7",
+         intel: "ab7af80ceae7dca18ffff54c2908ffa1215361977cf94014fa83e7c2aa8448db"
 
   url "https://api.k8slens.dev/binaries/Lens-#{version}-latest#{arch}.dmg"
   name "Lens"
