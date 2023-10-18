f54ca07ff924e49deb2f273de0702569112e686a
Patrick Linnane <patrick@linnane.io>
2023-10-18 11:36:11 -0700

    chipmunk 3.9.20

--- Casks/c/chipmunk.rb
+++ Casks/c/chipmunk.rb
@@ -1,6 +1,6 @@
 cask "chipmunk" do
-  version "3.9.19"
-  sha256 "0e5f0a50bee8bf38b1b57a49964aae2d379df991ecda455fc381a51be95ac45c"
+  version "3.9.20"
+  sha256 "27837ff257c37dbd8787c7c80451182f168af2e5e194d7724dfd7cbe5d961ab3"
 
   url "https://github.com/esrlabs/chipmunk/releases/download/#{version}/chipmunk@#{version}-darwin-portable.tgz"
   name "Chipmunk Log Analyzer & Viewer"
