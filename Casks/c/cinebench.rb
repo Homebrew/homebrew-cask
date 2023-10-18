8c24517878d0c17db0a934c6915be81e69842100
Yurii Kolesnykov <root@yurikoles.com>
2023-10-18 16:25:53 +0300

    Update cinebench from 23 to 2024
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/c/cinebench.rb
+++ Casks/c/cinebench.rb
@@ -1,18 +1,18 @@
 cask "cinebench" do
-  version "23"
-  sha256 "c8826647af0c7c8cc9bfa2533b254a37deb9852caeea1a47cbacec5e577cdda9"
+  version "2024"
+  sha256 "dcc840852c85c2f9ea39f55e78b23efbb59a27960555c0fb319d5bce7f51fdb6"
 
-  url "https://http.maxon.net/pub/cinebench/CinebenchR#{version}.dmg"
+  url "https://mx-app-blob-prod.maxon.net/mx-package-production/website/macos/maxon/cinebench/Cinebench#{version}_macOS.dmg"
   name "Cinebench"
   desc "Hardware benchmarking utility"
   homepage "https://www.maxon.net/products/cinebench/"
 
   livecheck do
-    url "https://www.maxon.net/_nuxt/static/1661539108/en/downloads/payload.js"
-    regex(/href=.*?cinebench-r(\d+(?:\.\d+)*)-downloads/i)
+    url "https://www.maxon.net/_nuxt/static/1697633267/en/downloads/cinebench-2024-downloads/payload.js"
+    regex(/Cinebench(\d+(?:\.\d+)*)(?:_macOS\.dmg)/i)
   end
 
-  depends_on macos: ">= :el_capitan"
+  depends_on macos: ">= :big_sur"
 
   app "Cinebench.app"
 
