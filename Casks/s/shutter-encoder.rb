ed306e54d0fb11de5c0ca865b799c445246f4dcb
Lex.Chen <ctyytc@gmail.com>
2023-10-18 18:09:56 +0800

    Fix sha256 code for arm
    Fix sha256 code for arm

--- Casks/s/shutter-encoder.rb
+++ Casks/s/shutter-encoder.rb
@@ -2,7 +2,7 @@ cask "shutter-encoder" do
   arch arm: "Apple Silicon", intel: "Mac 64bits"
 
   version "17.6"
-  sha256 arm:   "89d93aa68bec81821e6bac6aad579a21c08934431a2eb89285eca8ba68943639",
+  sha256 arm:   "dd6cec3bed36f1a9a8a31241216007025e58b6c874d3822fa104602db0303021",
          intel: "17e327e9305e335c8bdd45f20892ef7c9df23cc6b75171943d121766f3cf9b7b"
 
   url "https://www.shutterencoder.com/Shutter%20Encoder%20#{version}%20#{arch.gsub(" ", "%20")}.pkg"
