a1aa98a8d37c8c919d6e1fb338002a2e48c5ede1
Walter Wahlstedt <wahlstedtw@gmail.com>
2023-10-19 11:08:22 -0400

    brickstore 2023.8.1

--- Casks/b/brickstore.rb
+++ Casks/b/brickstore.rb
@@ -2,10 +2,11 @@ cask "brickstore" do
   version "2023.8.1"
   sha256 "5cf7391dc5d8fa3d61a821bf14d8a14f9bb6394463ac82ad1816f292f3a5ff39"
 
-  url "https://github.com/rgriebl/brickstore/releases/download/v#{version}/macOS-BrickStore-#{version}.dmg"
+  url "https://github.com/rgriebl/brickstore/releases/download/v#{version}/macOS-BrickStore-#{version}.dmg",
+      verified: "github.com/rgriebl/brickstore/"
   name "BrickStore"
-  desc "Desktop client for BrickStore"
-  homepage "https://github.com/rgriebl/brickstore"
+  desc "BrickLink offline management tool"
+  homepage "https://www.brickstore.dev/"
 
   app "BrickStore.app"
 
