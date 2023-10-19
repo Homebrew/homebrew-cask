68c3daacc2f2f6002d76a6e8df0d3892adecb33b
Miccal Matthews <miccal.matthews@gmail.com>
2023-10-19 14:20:50 +0800

    Update actual-odbc-pack.rb

--- Casks/a/actual-odbc-pack.rb
+++ Casks/a/actual-odbc-pack.rb
@@ -23,9 +23,9 @@ cask "actual-odbc-pack" do
   ]
 
   zap trash: [
-    "~/Library/Application Support/Actual ODBC",
-    "~/Library/Logs/Actual_iODBC_Installer.log",
-    "~/Library/Saved Application State/com.actualtechnologies.odbcmanager.savedState",
-  ],
+        "~/Library/Application Support/Actual ODBC",
+        "~/Library/Logs/Actual_iODBC_Installer.log",
+        "~/Library/Saved Application State/com.actualtechnologies.odbcmanager.savedState",
+      ],
       rmdir: "~/Documents/ODBC Documents"
 end
