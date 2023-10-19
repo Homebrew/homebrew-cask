b786bc5afe0234b8007f9cbf60e982a3f7f15254
Miccal Matthews <miccal.matthews@gmail.com>
2023-10-20 07:09:40 +0800

    Update blink1control.rb

--- Casks/b/blink1control.rb
+++ Casks/b/blink1control.rb
@@ -5,7 +5,7 @@ cask "blink1control" do
   version "2.2.9"
   sha256 arm:   "5201cc77aa1b51b927d90e59f6221ff55f147f5910f6e75b6acd0966b3f4c099",
          intel: "fa4a8457f905b6e7ef288c621fed646305ac31408932a9cfa7181fde41499ec2"
-         
+
   url "https://github.com/todbot/Blink1Control2/releases/download/v#{version}/Blink1Control#{version.major}-#{version}-mac-#{arch}.dmg",
       verified: "github.com/todbot/Blink1Control2/"
   name "Blink1Control"
