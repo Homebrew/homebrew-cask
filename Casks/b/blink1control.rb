c5831bd8513b602dc3255e46ceb331a3eb2e828f
Miccal Matthews <miccal.matthews@gmail.com>
2023-10-20 07:03:35 +0800

    Update blink1control.rb

--- Casks/b/blink1control.rb
+++ Casks/b/blink1control.rb
@@ -1,10 +1,10 @@
 cask "blink1control" do
+  # NOTE: "1" is not a version number, but an intrinsic part of the product name
+  arch arm: "arm64", intel: "x64"
+
   version "2.2.9"
-  arch arm: "arm64"
-  arch intel: "x64"
-  sha512
-   sha512 arm:   "FJsSMLEhw15CauZ1gM/ALsJDqDMQxZZCeOjSIuhuiswR+pi63+NrVlsp2C5MBESEEFnr0vD4rek8k7Ufd9SW0Q=="
-   sha512 intel: "MxBa22/EdYaptnj/0pRc+x6gMdRtE7MApvU+zOK7AAIHy0TpLBnnDpCHzkuZpTWoInzojSuaD+2/XxCLiy72eg=="
+  sha256 arm:   "5201cc77aa1b51b927d90e59f6221ff55f147f5910f6e75b6acd0966b3f4c099",
+         intel: "fa4a8457f905b6e7ef288c621fed646305ac31408932a9cfa7181fde41499ec2"
          
   url "https://github.com/todbot/Blink1Control2/releases/download/v#{version}/Blink1Control#{version.major}-#{version}-mac-#{arch}.dmg",
       verified: "github.com/todbot/Blink1Control2/"
@@ -12,10 +12,7 @@ cask "blink1control" do
   desc "Utility to control blink(1) USB RGB LED devices"
   homepage "https://blink1.thingm.com/"
 
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
+  depends_on macos: ">= :el_capitan"
 
   app "Blink1Control#{version.major}.app"
 end
