c7db9288c292d4518cbc1933b56eb60cfc23a61f
Walter Wahlstedt <wahlstedtw@gmail.com>
2023-10-18 21:45:59 -0400

    brickstore 2023.8.1

---
+++ Casks/b/brickstore.rb
@@ -0,0 +1,17 @@
+cask "brickstore" do
+  version "2023.8.1"
+  sha256 "5cf7391dc5d8fa3d61a821bf14d8a14f9bb6394463ac82ad1816f292f3a5ff39"
+
+  url "https://github.com/rgriebl/brickstore/releases/download/v#{version}/macOS-BrickStore-#{version}.dmg"
+  name "BrickStore"
+  desc "Desktop client for BrickStore"
+  homepage "https://github.com/rgriebl/brickstore"
+
+  app "BrickStore.app"
+
+  zap trash: [
+    "~/Library/Preferences/de.brickforge.brickstore.plist",
+    "~/Library/Preferences/org.brickstore.BrickStore.plist",
+    "~/Library/Saved Application State/de.brickforge.brickstore.savedState",
+  ]
+end
