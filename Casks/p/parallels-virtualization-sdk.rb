fb13f6bbb968a0a25be91ef2c78ff89571037365
Rui Chen <rui@chenrui.dev>
2023-10-19 14:48:35 -0400

    parallels-virtualization-sdk 19.1.0-54729

--- Casks/p/parallels-virtualization-sdk.rb
+++ Casks/p/parallels-virtualization-sdk.rb
@@ -1,6 +1,6 @@
 cask "parallels-virtualization-sdk" do
-  version "19.0.0-54570"
-  sha256 "c1fa27a7126dfa06f77f5cde5be7f7616716ac07621bd23adc44c15f6d1d6f3a"
+  version "19.1.0-54729"
+  sha256 "ecdd28b7d2f3f169a093244b660fd9f98fa849593dcf8cc9bdbecc20ace2d64d"
 
   url "https://download.parallels.com/desktop/v#{version.major}/#{version}/ParallelsVirtualizationSDK-#{version}-mac.dmg"
   name "Parallels Virtualization SDK"
