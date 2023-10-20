7b3b36b5572a6d55e6d90bd8266cc7c3acfae8ca
Rui Chen <rui@chenrui.dev>
2023-10-20 18:26:30 -0400

    tuist 3.30.0

--- Casks/t/tuist.rb
+++ Casks/t/tuist.rb
@@ -1,6 +1,6 @@
 cask "tuist" do
-  version "3.29.0"
-  sha256 "1243bb691c6b4131fe3a1ad8107a0fb24f354cf1633ca99df6043c5686497bde"
+  version "3.30.0"
+  sha256 "eaa63e2d63d6185b11cf28e4c1bd2ea5d8de4fda43f328babab2305f2ebda217"
 
   url "https://github.com/tuist/tuist/releases/download/#{version}/tuist.zip",
       verified: "github.com/tuist/tuist/"
