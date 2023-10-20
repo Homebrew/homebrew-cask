9400f6865a3f65ce3a89d44ed4e6168192fdc76a
Rui Chen <rui@chenrui.dev>
2023-10-20 09:41:55 -0400

    sigmaos 1.12.0.1

--- Casks/s/sigmaos.rb
+++ Casks/s/sigmaos.rb
@@ -1,6 +1,6 @@
 cask "sigmaos" do
-  version "1.11.4.1"
-  sha256 "53687aa0a5ae51c4ca28b6ff039164ebbf8c02fcd77d7e507b002bd778982094"
+  version "1.12.0.1"
+  sha256 "20b42db16c0023ab8682c8b2a39cca9c8973e258c9516fe2ac663e88df6c1fc3"
 
   url "https://releases.sigmaos.com/SigmaOS-#{version}.dmg"
   name "SigmaOS"
