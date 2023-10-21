710f5c7cdfe29d2dbe9759dd1bf352a0797a4c05
Jesper Almstrom <jesperalmstrom@gmail.com>
2023-10-21 12:10:21 +0000

    syntax cookbook - order of stanza - line length

--- Casks/u/usbimager.rb
+++ Casks/u/usbimager.rb
@@ -1,7 +1,9 @@
 cask "usbimager" do
   arch arm: "arm", intel: "intel"
-  sha256 arm: "54fb7b909009acca97f8ae42939d53e5a70c1030c001a024a0531173001e908d", intel: "85081cde8626b3714b77244de85d38830e89f0d1af7341514ad93077c7ba826a"
+
   version "1.0.10"
+  sha256 arm: "54fb7b909009acca97f8ae42939d53e5a70c1030c001a024a0531173001e908d", 
+         intel: "85081cde8626b3714b77244de85d38830e89f0d1af7341514ad93077c7ba826a"
 
   url "https://gitlab.com/bztsrc/usbimager/raw/binaries/usbimager_#{version}-#{arch}-macosx-cocoa.zip",
       verified: "gitlab.com/bztsrc/usbimager/"
