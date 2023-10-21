c7fea871d0f3ffe3203c1154593600a9ac2a88ef
Eric Knibbe <enk3@outlook.com>
2023-10-21 17:43:58 -0400

    endless-sky 0.10.4

--- Casks/e/endless-sky.rb
+++ Casks/e/endless-sky.rb
@@ -1,6 +1,6 @@
 cask "endless-sky" do
-  version "0.10.2"
-  sha256 "47e417ac3dbda9cc7eab0202e85301f84971451a6ce793c70b53ec25e5df4890"
+  version "0.10.4"
+  sha256 "d450d75788bf07573a0812f383af12a90ec5e0101e989d4c9bf0e9e736e7f8d5"
 
   url "https://github.com/endless-sky/endless-sky/releases/download/v#{version}/Endless-Sky-v#{version}.dmg",
       verified: "github.com/endless-sky/endless-sky/"
@@ -15,5 +15,9 @@ cask "endless-sky" do
 
   app "Endless Sky.app"
 
-  zap trash: "~/Library/Application Support/endless-sky"
+  zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/endless-sky.sfl",
+    "~/Library/Application Support/endless-sky",
+    "~/Library/Saved Application State/Endless-Sky.savedState",
+  ]
 end
