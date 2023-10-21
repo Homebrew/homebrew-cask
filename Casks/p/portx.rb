5285d906a3e1275ac01faca8116e53f89daaf4fe
bigseungho <bigseungho@netsarang.com>
2023-10-21 09:28:30 +0900

    Updated PortX Form 2.2.2 to 2.2.3

--- Casks/p/portx.rb
+++ Casks/p/portx.rb
@@ -1,9 +1,9 @@
 cask "portx" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.2.2,10.49"
-  sha256 arm:   "848F25E81AAF45929DB8F7A2C0AC80AAA7EFFA3D5844391C5E6ED645655DE4E8",
-         intel: "986E9669A89C0BF07EEC1854DDF626F57C8360F3E6263FD8517C3C442E7A0877"
+  version "2.2.3,10.50"
+  sha256 arm:   "238186E09045C8D6E002E5B60CBDD09B739D7948E8408CB4B897F233A42122BE",
+         intel: "02CB1DCC775C68C84CF711C4643C82BF90DF56828E80C79A52406B63C08E77DD"
 
   url "https://cdn.netsarang.net/0ac7ea20/PortX-#{version.csv.first}-#{arch}.dmg",
       verified: "cdn.netsarang.net/"
