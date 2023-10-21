3aaa2980872c58c73e5a5e2176951705c56503e5
Rui Chen <rui@chenrui.dev>
2023-10-20 21:09:36 -0400

    vassal 3.7.4

--- Casks/v/vassal.rb
+++ Casks/v/vassal.rb
@@ -1,6 +1,6 @@
 cask "vassal" do
-  version "3.7.3"
-  sha256 "08ab0a857bf3484e78939d55951d9584f7f9afc95d3ecb92f7a9f2347d9f2e49"
+  version "3.7.4"
+  sha256 "2432826251fac85fdc555d5fe2e450743ebd2e70805db7903bc3755d72639f62"
 
   url "https://github.com/vassalengine/vassal/releases/download/#{version}/VASSAL-#{version}-macos-universal.dmg",
       verified: "github.com/vassalengine/vassal/"
