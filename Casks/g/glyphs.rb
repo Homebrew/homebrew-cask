0f83a8922caa3326e61baf235f8650b343eacbfe
Rui Chen <rui@chenrui.dev>
2023-10-21 19:01:57 -0400

    glyphs 3.2,3223

--- Casks/g/glyphs.rb
+++ Casks/g/glyphs.rb
@@ -1,6 +1,6 @@
 cask "glyphs" do
-  version "3.2,3222"
-  sha256 "9b518c3d0a93cda84da7fef38cfa8e9c79f279b1c70b2d2cde36ca8a5205829d"
+  version "3.2,3223"
+  sha256 "e4e10946481cbf29a8dacf1f5e123025ae2cacfc0bb991c6c6c474dffeed3b78"
 
   url "https://updates.glyphsapp.com/Glyphs#{version.csv.first}-#{version.csv.second}.zip"
   name "Glyphs"
