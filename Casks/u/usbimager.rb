aeeb138755e835d2961a184d58cba1023adba436
Jesper Almstrom <jesperalmstrom@gmail.com>
2023-10-21 11:38:07 +0000

    syntax: try to add more single empty lines

--- Casks/u/usbimager.rb
+++ Casks/u/usbimager.rb
@@ -1,10 +1,13 @@
 cask "usbimager" do
+
   arch arm: "arm", intel: "intel"
-  
+
   version "1.0.10"
+
   on_arm do
     sha256 "54fb7b909009acca97f8ae42939d53e5a70c1030c001a024a0531173001e908d"
   end
+
   on_intel do
     sha256 "85081cde8626b3714b77244de85d38830e89f0d1af7341514ad93077c7ba826a"
   end
