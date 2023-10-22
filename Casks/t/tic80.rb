3d51e18606731e7128a8dd8ac6930461b8bdd36a
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-10-22 09:37:38 -0400

    tic80 1.1.2837

--- Casks/t/tic80.rb
+++ Casks/t/tic80.rb
@@ -1,7 +1,7 @@
 cask "tic80" do
   # NOTE: "80" is not a version number, but an intrinsic part of the product name
-  version "1.1.2736"
-  sha256 "747a819bd7fddbbac7f3a03e579be5d4ee7af6c4d1b9e34601738cb3eda04043"
+  version "1.1.2837"
+  sha256 "324e91d08fb5dcfaf2f41dc846b89465074b5db3b5aa63befc3fdb664493c1b9"
 
   url "https://github.com/nesbox/TIC-80/releases/download/v#{version}/tic80-v#{version.major_minor}-mac.dmg",
       verified: "github.com/nesbox/TIC-80/"
