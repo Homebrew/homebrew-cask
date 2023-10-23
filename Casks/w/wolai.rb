1516a1de0636fde9096c16240067b24d594116df
Rui Chen <rui@chenrui.dev>
2023-10-23 01:33:15 -0400

    wolai 1.2.9

--- Casks/w/wolai.rb
+++ Casks/w/wolai.rb
@@ -1,9 +1,9 @@
 cask "wolai" do
   arch arm: "-arm64"
 
-  version "1.2.8"
-  sha256 arm:   "c013fbe063bd9b59b560562519f602a6d61c6116adc8178829c8f0401f389221",
-         intel: "d4878217d2c588933448c3cad34d033af74c1605786820dbbc1d2e7b341c4bbb"
+  version "1.2.9"
+  sha256 arm:   "d25706ac7b915a0cdb8bbced0db8e670e7a65c2b75c6fe8777afd4840469781a",
+         intel: "61c6d25285164e2a5864988b24fc45d912de675320af9c81705493fb0b041635"
 
   url "https://cdn.wostatic.cn/dist/installers/wolai-#{version}#{arch}.dmg",
       verified: "cdn.wostatic.cn/dist/installers/"
