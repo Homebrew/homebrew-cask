2165abf2c5dc0db083e4014f4cb4aaedd6f00a92
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-10-23 09:24:04 -0400

    chronos 6.1.0

--- Casks/c/chronos.rb
+++ Casks/c/chronos.rb
@@ -1,6 +1,6 @@
 cask "chronos" do
-  version "6.0.9"
-  sha256 "33fa8831337892c54b3048049da24918c5c3c60cbb284400b48cf5cb1d71a3da"
+  version "6.1.0"
+  sha256 "100375094187ea1f6cc97ce90ec54a807c6c80dcb6ab1f2a7012a181d1bae7f1"
 
   url "https://github.com/web-pal/chronos-timetracker/releases/download/v#{version}/Chronos-#{version}-mac.zip"
   name "Chronos Timetracker"
