d113ff9317110f73d9728470f5c96d40642381fd
Rui Chen <rui@chenrui.dev>
2023-10-24 13:38:32 -0400

    idagio 1.9.8

--- Casks/i/idagio.rb
+++ Casks/i/idagio.rb
@@ -1,6 +1,6 @@
 cask "idagio" do
-  version "1.9.7"
-  sha256 "242616e458d6fd972e83046fa233e53eb30419949a797b50bae09eb7722dd47f"
+  version "1.9.8"
+  sha256 "ae7e57172a1fda8e6c8349c27bee98cf32dfc02d69524685bd4f4dd8a433e2a8"
 
   url "https://dl.idagio.com/IDAGIO-#{version}.dmg"
   name "IDAGIO"
