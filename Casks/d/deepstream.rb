e25bc82d4b3a982ed062cb38a27334fc7a853da0
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-10-24 09:33:29 -0400

    deepstream 7.0.8

--- Casks/d/deepstream.rb
+++ Casks/d/deepstream.rb
@@ -1,6 +1,6 @@
 cask "deepstream" do
-  version "7.0.7"
-  sha256 "1ca15f94f062a2b7b076d47d9854e0530e43034a098e1a7f9244ed147e9e4fbc"
+  version "7.0.8"
+  sha256 "69661ce2391e91358bb9d4845dbc857150135c5d9c3d86627f422f5cacf14f55"
 
   url "https://github.com/deepstreamIO/deepstream.io/releases/download/v#{version}/deepstream.io-mac-#{version}.pkg",
       verified: "github.com/deepstreamIO/deepstream.io/"
