782b47a7bf70db82aacd1d9ae379bd07e48491ee
Carson Full <carsonfull@gmail.com>
2023-10-25 09:01:44 -0500

    Fix arm sha256 hash for sf.rb

--- Casks/s/sf.rb
+++ Casks/s/sf.rb
@@ -2,7 +2,7 @@ cask "sf" do
   arch arm: "arm64", intel: "x64"
 
   version "2.13.9,364e02e"
-  sha256 arm:   "22b910708b769813acd59511c79a7d874fcf8a1f4d0c755b51cedf127ae13f2d",
+  sha256 arm:   "086b1a88ec5e665a30baa717fa24f1580f7f43a40a937093eb5faebd0c67e95d",
          intel: "bf179bdb30184d58cf083da1b6a241c1a5ac2a0855bb53bb815a25087b260e07"
 
   url "https://developer.salesforce.com/media/salesforce-cli/sf/versions/#{version.csv.first}/#{version.csv.second}/sf-v#{version.csv.first}-#{version.csv.second}-#{arch}.pkg"
