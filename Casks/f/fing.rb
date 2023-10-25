cc1f2eba1b84257b07532e2c2d1f50b520f194c5
Rui Chen <rui@chenrui.dev>
2023-10-25 13:32:11 -0400

    fing 3.5.0

--- Casks/f/fing.rb
+++ Casks/f/fing.rb
@@ -1,6 +1,6 @@
 cask "fing" do
-  version "3.4.1"
-  sha256 "a5fffb5aba82e622c71395dcb168bf86d9c96c809b860dfe9e376bf7724a4dcf"
+  version "3.5.0"
+  sha256 "fd4edd8b2faa400fbf038e0b290061c1a26a476e5160c80ac93868aa12657d6f"
 
   url "https://get.fing.com/fing-desktop-releases/mac/Fing-#{version}.dmg"
   name "Fing Desktop"
