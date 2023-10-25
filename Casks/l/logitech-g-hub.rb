c2f793404ce1b3eb12020bd608eaeaa60df37295
Patrick Linnane <patrick@linnane.io>
2023-10-25 11:41:12 -0700

    logitech-g-hub 2023.9.473951

--- Casks/l/logitech-g-hub.rb
+++ Casks/l/logitech-g-hub.rb
@@ -1,5 +1,5 @@
 cask "logitech-g-hub" do
-  version "2023.8.460923"
+  version "2023.9.473951"
   sha256 :no_check
 
   url "https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.zip",
