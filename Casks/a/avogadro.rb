f91714646346e4772df5fc94cabab506b606f79a
Geoff Hutchison <geoff.hutchison@gmail.com>
2023-10-26 17:31:18 -0400

    Fix style

--- Casks/a/avogadro.rb
+++ Casks/a/avogadro.rb
@@ -1,8 +1,8 @@
 cask "avogadro" do
-  version "1.98.0"
   arch arm: "-arm64", intel: ""
 
-  sha256 arm:   "ae0911223b408b22652b81a99a71b7f08b0eb7a0026bfdd3e2d7765f2408f5e7", 
+  version "1.98.0"
+  sha256 arm:   "ae0911223b408b22652b81a99a71b7f08b0eb7a0026bfdd3e2d7765f2408f5e7",
          intel: "424766cf79b79b12f588a90feca3b16a59943af8d63272eacbf27c0ae6293a50"
 
   url "https://github.com/OpenChemistry/avogadrolibs/releases/download/#{version}/Avogadro2-#{version}-Darwin#{arch}.dmg",
