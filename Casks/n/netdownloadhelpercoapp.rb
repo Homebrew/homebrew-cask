5627359518358cd2eb8f45fd254915c0dbac3fce
Rui Chen <rui@chenrui.dev>
2023-10-26 01:46:59 -0400

    netdownloadhelpercoapp 2.0.5

--- Casks/n/netdownloadhelpercoapp.rb
+++ Casks/n/netdownloadhelpercoapp.rb
@@ -1,9 +1,9 @@
 cask "netdownloadhelpercoapp" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "2.0.4"
-  sha256 arm:   "009039b6a63eb33e66649cade8e1a3c0ee7fa6f38fb20269f1904fc74258f7bb",
-         intel: "8d18855fe0cce690844e822d7852657bd69623affbf4369e90309547fb5876d2"
+  version "2.0.5"
+  sha256 arm:   "2a03ed394934ce69b09051f6fda5373c4c0e46eef4cfccf789708ba92f2a7545",
+         intel: "11cc6d33d6d50483be0f3b0c10ea4a37090c01745d7f6670e30b493dcabbf2ad"
 
   url "https://github.com/mi-g/vdhcoapp/releases/download/v#{version}/vdhcoapp-#{version}-mac-#{arch}-installer.pkg",
       verified: "github.com/mi-g/vdhcoapp/"
