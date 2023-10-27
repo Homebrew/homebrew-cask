da4e8b038f04ff8079fdad66a39640bd00bef299
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-10-27 08:44:36 -0400

    banking-4 8.3.0

--- Casks/b/banking-4.rb
+++ Casks/b/banking-4.rb
@@ -1,6 +1,6 @@
 cask "banking-4" do
   # NOTE: "4" is not a version number, but an intrinsic part of the product name
-  version "8.2.1,8693"
+  version "8.3.0,8700"
   sha256 :no_check
 
   url "https://subsembly.com/download/MacBanking4.pkg"
