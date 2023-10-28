7d8cedd978df68342b4d85807f9055c2ef63385a
Nathan <subtype.kitsch_0w@icloud.com>
2023-10-28 00:24:06 -0700

    opentabletdriver 0.6.3.0

---
+++ Casks/o/opentabletdriver.rb
@@ -0,0 +1,11 @@
+cask "opentabletdriver" do
+  version "0.6.3.0"
+  sha256 "8b26edadcab6da44a518e956714f636ba9adcac678644a7c73a418bd25b6b43b"
+
+  url "https://github.com/OpenTabletDriver/OpenTabletDriver/releases/download/v#{version}/OpenTabletDriver.osx-x64.tar.gz"
+  name "OpenTabletDriver"
+  desc "Open source, cross platform, user mode tablet driver."
+  homepage "https://github.com/OpenTabletDriver/OpenTabletDriver"
+
+  app "OpenTabletDriver.app"
+end
