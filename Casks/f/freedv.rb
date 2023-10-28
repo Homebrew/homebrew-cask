b21951b0b515060d1b31b1cb571ebdbbf938e3f9
luzpaz <luzpaz@users.noreply.github.com>
2023-10-28 19:58:32 -0400

    Add FreeDV 1.9.4 as cask

---
+++ Casks/f/freedv.rb
@@ -0,0 +1,11 @@
+cask "freedv" do
+  version "1.9.4"
+  sha256 "515e6c3bf7bee0da5c55e84fcad1d81473aeb1709e93adbc5d7c86bb1a80acce"
+
+  url "https://github.com/drowe67/freedv-gui/releases/download/v1.9.4/FreeDV.dmg"
+  name "FreeDV"
+  desc "Digital voice for HF radio"
+  homepage "http://freedv.org/"
+
+  app "FreeDV.app"
+end
