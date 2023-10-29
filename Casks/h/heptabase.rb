6408f42602be54dc0cb00489e138ea3b174c9316
Amory Meltzer <Amorymeltzer@gmail.com>
2023-10-29 11:27:52 -0400

    heptabase 1.13.0

--- Casks/h/heptabase.rb
+++ Casks/h/heptabase.rb
@@ -1,9 +1,9 @@
 cask "heptabase" do
   arch arm: "-arm64"
 
-  version "1.12.2"
-  sha256 arm:   "a46919e97abe4aa5b05fcb10506f088fe2cc9ff902f5f5f8344f936c824a0bd3",
-         intel: "9a6d118fd5023894051ad671f81877c9dd3f0dbdda26a8eb55e40d442241b35e"
+  version "1.13.0"
+  sha256 arm:   "4f02bd3ceebc59d18fb959b314979debdf149f63ec5959630992d0eeea5581d3",
+         intel: "541d6567b9d855a166a3a687307fe62cb8a934b63a5b79e5ddcbf9c9a48713d8"
 
   url "https://github.com/heptameta/project-meta/releases/download/v#{version}/Heptabase-#{version}#{arch}-mac.zip",
       verified: "github.com/heptameta/project-meta/"
