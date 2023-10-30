e238d366946ed7607748c8bf8849fab808a66c17
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-10-30 10:40:04 -0400

    pitch 1.147.1

--- Casks/p/pitch.rb
+++ Casks/p/pitch.rb
@@ -1,6 +1,6 @@
 cask "pitch" do
-  version "1.147.0,stable.4,6080628"
-  sha256 "6f13e2c6631a600a736ea940a1d8e25f5ec5ea93610e93ce4bf1ea1e567ff5fe"
+  version "1.147.1,stable.1,6087497"
+  sha256 "5075ed1340c948f5767a620436eface4e39ac630a43a53d92839371085803d2e"
 
   url "https://desktop-app-builds.pitch.com/Pitch-#{version.csv.first}-#{version.csv.second}-ci#{version.csv.third}.dmg"
   name "Pitch"
