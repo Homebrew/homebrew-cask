fe036a86bcb7aa0d5253ae01cc20de23ee03ebed
Rui Chen <rui@chenrui.dev>
2023-10-30 17:18:17 -0400

    sleek 2.0.0

--- Casks/s/sleek.rb
+++ Casks/s/sleek.rb
@@ -1,6 +1,6 @@
 cask "sleek" do
-  version "1.3.1"
-  sha256 "38bfccfbeb13b42d7b4831aed175158fcc046a66b834ba0b0949700251c46487"
+  version "2.0.0"
+  sha256 "fc3437a202b624271c455b5e1fe4ebab3d363a513d569eb52e060875fa0ed042"
 
   url "https://github.com/ransome1/sleek/releases/download/v#{version}/sleek-#{version}-mac.dmg"
   name "sleek"
