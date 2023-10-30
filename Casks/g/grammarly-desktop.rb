754de93a524d2848cd1d641e3a948d7a5c340851
Rui Chen <rui@chenrui.dev>
2023-10-30 10:16:03 -0400

    grammarly-desktop 1.42.3.0

--- Casks/g/grammarly-desktop.rb
+++ Casks/g/grammarly-desktop.rb
@@ -1,6 +1,6 @@
 cask "grammarly-desktop" do
-  version "1.41.2.0"
-  sha256 "474138e6df1657620e9e0f091f8b5932f4d274aa72272491dd58ab81a0573db5"
+  version "1.42.3.0"
+  sha256 "a52c2507de7e01f31d8ee702240c85c618d37a960a63a462c6b90e7b74d1a61b"
 
   url "https://download-mac.grammarly.com/versions/#{version}/Grammarly.dmg"
   name "Grammarly Desktop"
