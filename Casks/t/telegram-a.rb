17fa4539e6ed605414999e39e70be99dbc2e8d90
Rui Chen <rui@chenrui.dev>
2023-10-31 12:29:16 -0400

    telegram-a 10.0.16

--- Casks/t/telegram-a.rb
+++ Casks/t/telegram-a.rb
@@ -1,9 +1,9 @@
 cask "telegram-a" do
   arch arm: "arm64", intel: "x64"
 
-  version "10.0.15"
-  sha256 arm:   "11daddc23d22f96e24e838bce3a60c83544eda35fc9a21dfaf0252fa80bad11d",
-         intel: "78cd8acab2109ffc8261532108720b74521dff520f0e1bf88cff5b25284f0c72"
+  version "10.0.16"
+  sha256 arm:   "ce02791bc0f5dc91c13a17fbe3bf334ff3c0b5b5c549152ff3d6e6ce0631115d",
+         intel: "b117c3ad4b97b7e492367d55a388521a76439012de2a276fb44ecc2d5c64e5df"
 
   url "https://github.com/Ajaxy/telegram-tt/releases/download/v#{version}/Telegram-A-#{arch}.dmg",
       verified: "github.com/Ajaxy/telegram-tt/"
