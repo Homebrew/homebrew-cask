f40ea57d0bb56d8368726003520fd94decb50c4f
Miccal Matthews <miccal.matthews@gmail.com>
2023-10-31 11:30:29 +0800

    Update orion.rb

--- Casks/o/orion.rb
+++ Casks/o/orion.rb
@@ -62,7 +62,7 @@ cask "orion" do
     depends_on macos: :ventura
   end
   on_sonoma :or_newer do
-    sha256 "21a1b6a43a318b3b351e87b47ee794af9b3f6803016b0a3f3e8ce619975d520c"
+    sha256 "042cc5432ad4a134da669183830059d7d7ce2f286739e4b18f7a725fb58146eb"
 
     url "https://browser.kagi.com/updates/14_0/#{version.csv.second}.zip"
 
