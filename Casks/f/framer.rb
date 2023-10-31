092f789af9e50cfc21e5c3526ced13aa1d12001d
Rui Chen <rui@chenrui.dev>
2023-10-31 08:08:39 -0400

    framer 2023.43.0

--- Casks/f/framer.rb
+++ Casks/f/framer.rb
@@ -1,7 +1,7 @@
 cask "framer" do
   arch arm: "arm64", intel: "x64"
 
-  version "2023.15.1"
+  version "2023.43.0"
   sha256 :no_check
 
   url "https://updates.framer.com/electron/darwin/#{arch}/Framer.zip"
