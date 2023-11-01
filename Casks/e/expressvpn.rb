e9351607907eb6cb4ebe9e138d9bfccf6448641e
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-11-01 09:42:20 -0400

    expressvpn 11.44.1.78733

--- Casks/e/expressvpn.rb
+++ Casks/e/expressvpn.rb
@@ -1,6 +1,6 @@
 cask "expressvpn" do
-  version "11.44.0.78629"
-  sha256 "5091b91d3ee95b8cda9c1358cf5695aa5e7d3a00c804b5c85392b992e3699ab9"
+  version "11.44.1.78733"
+  sha256 "1c749e48d7efcc888a53e2217ac6efaf4e041c3c194f7fdc834ae51e82904f13"
 
   url "https://www.expressvpn.works/clients/mac/expressvpn_mac_#{version}_release.pkg"
   name "ExpressVPN"
