7b8552773def7726ed54cafa77b917436ae813b0
Bevan Kay <email@bevankay.me>
2023-11-02 12:01:49 +1100

    cron 1.116.0

--- Casks/c/cron.rb
+++ Casks/c/cron.rb
@@ -1,11 +1,11 @@
 cask "cron" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.115.0"
-  sha256 arm:   "91c96dd1de41b0fb30a5aecc0e688adce1ba108dc7bab825a6f1ac62ce51ee6d",
-         intel: "6d624c79c1a48743e383dbeb953c9856477b93c67bd4c77ae6949d7487e62b13"
+  version "1.116.0,2311017rcjbjbtx"
+  sha256 arm:   "66457edbbf2f5c736e885dea559629fb710717b6a8df8b777d13a2cb562ae278",
+         intel: "2cc49e5b9e1b75b7c00e198025c55a601ca6876822fe8745a570a7554ed06b17"
 
-  url "https://download.todesktop.com/210303leazlircz/Cron%20#{version}-#{arch}-mac.zip",
+  url "https://download.todesktop.com/210303leazlircz/Cron%20#{version.csv.first}%20-%20Build%20#{version.csv.second}-#{arch}-mac.zip",
       verified: "download.todesktop.com/210303leazlircz/"
   name "Cron Calendar"
   desc "Calendar for professionals and teams"
@@ -13,7 +13,13 @@ cask "cron" do
 
   livecheck do
     url "https://download.todesktop.com/210303leazlircz/latest-mac.yml"
-    strategy :electron_builder
+    regex(/Build[ ._-]([^-]+)[._-]/i)
+    strategy :electron_builder do |item, regex|
+      build = item["files"].first["url"][regex, 1]
+      next if build.blank?
+
+      "#{item["version"]},#{build}"
+    end
   end
 
   auto_updates true
