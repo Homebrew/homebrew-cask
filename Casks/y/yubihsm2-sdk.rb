36cea35fae89c78a075dba3a6036a20689c1bb37
Rui Chen <rui@chenrui.dev>
2023-11-02 15:57:16 -0400

    yubihsm2-sdk 2023-11

--- Casks/y/yubihsm2-sdk.rb
+++ Casks/y/yubihsm2-sdk.rb
@@ -1,9 +1,9 @@
 cask "yubihsm2-sdk" do
   arch arm: "arm64", intel: "amd64"
 
-  version "2023-08"
-  sha256 arm:   "13667895f6e83b7c98279a1422ca8af23da60fb4250ce576ee7144da7e25ccd2",
-         intel: "6f755b2d19b55e69191bfc8799afced986d223e300e3d5f317aed1fe3a4ea39b"
+  version "2023-11"
+  sha256 arm:   "0eb0a878ce3c35644b7a7a264f4ba5f3309af04e499ab258744b3a5f11bfe5ed",
+         intel: "b27a5dfa6e3a6c3deb282668220eda93a09c05ba80b25276d35ee95b35326ad2"
 
   url "https://developers.yubico.com/YubiHSM2/Releases/yubihsm2-sdk-#{version}-darwin-#{arch}.pkg"
   name "YubiHSM 2 SDK"
