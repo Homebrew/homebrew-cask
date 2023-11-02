812c9c7ed13ff0a010055f38c9f5bb8923a6a2e9
Rui Chen <rui@chenrui.dev>
2023-11-02 12:39:08 -0400

    kuaitie 3.6.0

--- Casks/k/kuaitie.rb
+++ Casks/k/kuaitie.rb
@@ -1,6 +1,6 @@
 cask "kuaitie" do
-  version "3.5.2,1698230703"
-  sha256 "d16ef31abb43983b4ceede4ec388313453b93ccdde1a7c752eb9653a1eb15c43"
+  version "3.6.0,1698925843"
+  sha256 "18fddd56be6f287cd2f8e5c5996666d97680ac7d5262757bba4e40f7d647e678"
 
   url "https://clipweb.oss-cn-qingdao.aliyuncs.com/release/macos/packages/kuaitie-#{version.csv.first.no_dots}_#{version.csv.second}.zip",
       verified: "clipweb.oss-cn-qingdao.aliyuncs.com/release/macos/packages/"
