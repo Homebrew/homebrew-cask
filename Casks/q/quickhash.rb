887b151d605d12b690e49df717257a271b227569
Bevan Kay <email@bevankay.me>
2023-11-02 13:28:26 +1100

    quickhash: update version, add zap

--- Casks/q/quickhash.rb
+++ Casks/q/quickhash.rb
@@ -1,5 +1,5 @@
 cask "quickhash" do
-  version "3.3.4,2510"
+  version "3.3.4,2510,01,11,2023"
   sha256 "d5b73524b0d62a7f100f7a6f976eaad98968f03f20f6d6b6c28040f9c255a884"
 
   url "https://www.quickhash-gui.org/download/quickhash-gui-v#{version.csv.first.dots_to_hyphens}-apple-osx/?wpdmdl=#{version.csv.second}"
@@ -18,4 +18,9 @@ cask "quickhash" do
   container nested: "Quickhash-GUI_#{version.csv.third}#{version.csv.fourth}#{version.csv.fifth}.dmg"
 
   app "Quickhash-GUI.app"
+
+  zap trash: [
+    "~/.Quickhash-GUI",
+    "~/Library/Saved Application State/www.Quickhash-GUI.org.savedState",
+  ]
 end
