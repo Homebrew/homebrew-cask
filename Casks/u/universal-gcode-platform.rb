34212fc2713faa6e454a510d8392f45b515ca1ab
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-11-02 09:06:19 -0400

    universal-gcode-platform 2.1.0
    * Update to version 2.1.0
    
    * Add ARM support

--- Casks/u/universal-gcode-platform.rb
+++ Casks/u/universal-gcode-platform.rb
@@ -1,8 +1,11 @@
 cask "universal-gcode-platform" do
-  version "2.0.21"
-  sha256 "1f9e876d29b941ad285a5c0c9eb83fae5a3c34b2ffb500a9331905591d25cc18"
+  arch arm: "aarch64", intel: "x64"
 
-  url "https://github.com/winder/Universal-G-Code-Sender/releases/download/v#{version}/ios-ugs-platform-app-#{version}.dmg",
+  version "2.1.0"
+  sha256 arm:   "d2c3402ac5ae8b714544523ec276cdd9c24e52cca36a2af68eae971d079b1426",
+         intel: "d22de8705803fa4800be3cf8ade7b3416b0f5c69b9fd11b43312a600c81ff536"
+
+  url "https://github.com/winder/Universal-G-Code-Sender/releases/download/v#{version}/macosx-#{arch}-ugs-platform-app-#{version}.dmg",
       verified: "github.com/winder/Universal-G-Code-Sender/"
   name "Universal G-code Sender (Platform version)"
   desc "G-code sender for CNC (compatible with GRBL, TinyG, g2core and Smoothieware)"
