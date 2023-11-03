85327190c59341b1a0d55b605244b0eac8284545
Bevan Kay <email@bevankay.me>
2023-11-04 10:04:27 +1100

    Update pushplaylabs-sidekick.rb

--- Casks/p/pushplaylabs-sidekick.rb
+++ Casks/p/pushplaylabs-sidekick.rb
@@ -7,8 +7,8 @@ cask "pushplaylabs-sidekick" do
     sha256 "5791b0fc4c3a900a3913f39ea2db58a33e157a093470ba94a4cd342c5e8d74a5"
   end
   on_intel do
-    version "114.52.2.36022,d2f5eac"
-    sha256 "f664c6012677dc66f97a36f2c00d31fa29a12f04064e42139bc37a772293a398"
+    version "116.53.1.36303,52cf202"
+    sha256 "4276c244e7a54e110a135d16bcad7b8afb1130074294a676b37149bf3c8a9351"
   end
 
   url "https://cdn.meetsidekick.com/browser-builds/sidekick-mac-release-#{arch}-#{version.csv.first}-#{version.csv.second}-df.dmg"
