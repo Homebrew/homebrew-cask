138deff749096e9ae649c07d9cef19426ded79d4
Miccal Matthews <miccal.matthews@gmail.com>
2023-11-04 06:16:17 +0800

    Update remote-desktop-manager-free.rb

--- Casks/r/remote-desktop-manager-free.rb
+++ Casks/r/remote-desktop-manager-free.rb
@@ -1,17 +1,12 @@
 cask "remote-desktop-manager-free" do
-  version "2023.3.7.0"
-  sha256 "0159f5721c9612347c616629e4ad5190e3449cd378ff1beabb7019761fc53cce"
+  version "2022.2.16.0"
+  sha256 "26545b52627f780fc3618f62649ff58589adfd1409d72f5b84f50be11b218879"
 
-  url "https://cdn.devolutions.net/download/Mac/Devolutions.RemoteDesktopManager.Mac.#{version}.dmg",
+  url "https://cdn.devolutions.net/download/Mac/Devolutions.RemoteDesktopManager.Free.Mac.#{version}.dmg",
       verified: "devolutions.net/"
   name "Remote Desktop Manager Free"
   desc "Centralizes all remote connections on a single platform"
   homepage "https://mac.remotedesktopmanager.com/"
-
-  livecheck do
-    url "https://devolutions.net/products.htm"
-    regex(/RDMMacFreeWeb\.Version=(\d+(?:\.\d+)+)/i)
-  end
 
   depends_on macos: ">= :sierra"
 
@@ -21,4 +16,15 @@ cask "remote-desktop-manager-free" do
     "~/Library/Application Support/com.devolutions.remotedesktopmanager.free",
     "~/Library/Preferences/com.devolutions.remotedesktopmanager.free",
   ]
+
+  caveats do
+    discontinued
+    <<~EOS
+      TO install the free version run:
+
+        brew install --cask remote-desktop-manager
+
+      and when launched you can choose to use the free version.
+    EOS
+  end
 end
