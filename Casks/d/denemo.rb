cd2f4c3f7d9c77c678ed098cec39e9bfbea118bb
Sam Ford <1584702+samford@users.noreply.github.com>
2023-11-04 15:18:01 -0400

    denemo: update urls

--- Casks/d/denemo.rb
+++ Casks/d/denemo.rb
@@ -2,13 +2,13 @@ cask "denemo" do
   version "2.2"
   sha256 "6ec66279be560e6e997b1108ae9ed61e6c37c36a350fbd99cd3dded8e2cfbe5f"
 
-  url "http://denemo.org/downloads/denemo-#{version}-darwin-x64.tar.bz2"
+  url "https://denemo.org/downloads/denemo-#{version}-darwin-x64.tar.bz2"
   name "Denemo"
   desc "Music notation program"
-  homepage "http://denemo.org/"
+  homepage "https://denemo.org/"
 
   livecheck do
-    url "http://denemo.org/downloads/"
+    url "https://denemo.org/downloads/"
     regex(/href=.*?denemo[._-]v?(\d+(?:\.\d+)+)[._-]darwin[._-]x64.tar\.bz2/i)
   end
 
