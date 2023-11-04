5404ae6edf21af1cd095fbb810065839f022ea0d
Sam Ford <1584702+samford@users.noreply.github.com>
2023-11-04 12:30:17 -0400

    thinlinc-client: update homepage, livecheck

--- Casks/t/thinlinc-client.rb
+++ Casks/t/thinlinc-client.rb
@@ -5,10 +5,10 @@ cask "thinlinc-client" do
   url "https://www.cendio.com/downloads/clients/tl-#{version}-client-macos.iso"
   name "ThinLinc"
   desc "Linux remote desktop server"
-  homepage "https://www.cendio.com/thinlinc"
+  homepage "https://www.cendio.com/thinlinc/what-is-thinlinc/"
 
   livecheck do
-    url "https://www.cendio.com/thinlinc/download"
+    url "https://www.cendio.com/thinlinc/download/"
     regex(/tl[._-]v?(\d+(?:[._]\d+)+)[._-]client[._-]macos\.iso/i)
   end
 
