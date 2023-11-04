51a6599421b6bfe16947265f37315d164b9d750c
Sam Ford <1584702+samford@users.noreply.github.com>
2023-11-04 15:19:21 -0400

    rocks-n-diamonds: update url, livecheck

--- Casks/r/rocks-n-diamonds.rb
+++ Casks/r/rocks-n-diamonds.rb
@@ -2,13 +2,13 @@ cask "rocks-n-diamonds" do
   version "4.3.2.1"
   sha256 "6499fe6382eec6506cc625b0f372a29918640cfb6ebdca3ee3e6b97d83ff458c"
 
-  url "https://www.artsoft.org/RELEASES/macosx/rocksndiamonds/rocksndiamonds-#{version}.dmg"
+  url "https://www.artsoft.org/RELEASES/mac/rocksndiamonds/rocksndiamonds-#{version}.dmg"
   name "Rocks'n'Diamonds"
   desc "Arcade-style game"
   homepage "https://www.artsoft.org/rocksndiamonds/"
 
   livecheck do
-    url "https://www.artsoft.org/RELEASES/macosx/rocksndiamonds/"
+    url "https://www.artsoft.org/RELEASES/mac/rocksndiamonds/"
     regex(/href=.*?rocksndiamonds[._-]?v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
