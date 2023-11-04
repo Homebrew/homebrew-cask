c326d84f661f9c6f2c83bf71fbc79c72a8f65564
Rui Chen <rui@chenrui.dev>
2023-11-04 19:09:31 -0400

    pritunl 1.3.3705.92

--- Casks/p/pritunl.rb
+++ Casks/p/pritunl.rb
@@ -1,9 +1,9 @@
 cask "pritunl" do
   arch arm: ".arm64"
 
-  version "1.3.3704.98"
-  sha256 arm:   "328eeeb5000db382e8f513af48d9194c2c710178dd5c770badd7741895e4db71",
-         intel: "4dd3c4b3cedf88c1abc6cd422cd99f5b0297db1423854df3d9e111180d7bc166"
+  version "1.3.3705.92"
+  sha256 arm:   "a3d56b3617172f72409a4a0d5e146eb2ca01dfe77d8bf67e895d43007974cc36",
+         intel: "3630d79d4c3dbb5074f88757f54f97026886045f9456bae1888afbd9be43a3ae"
 
   url "https://github.com/pritunl/pritunl-client-electron/releases/download/#{version}/Pritunl#{arch}.pkg.zip",
       verified: "github.com/pritunl/pritunl-client-electron/"
