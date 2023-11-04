e318fa5a4b527c19625097d302751aabf4793c74
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-11-04 13:20:08 -0400

    buttercup 2.21.0

--- Casks/b/buttercup.rb
+++ Casks/b/buttercup.rb
@@ -1,6 +1,6 @@
 cask "buttercup" do
-  version "2.20.3"
-  sha256 "281becfa2405b0c533c364b0572f497a95d0986fd3a3e18f778ea1e8c5a6d24d"
+  version "2.21.0"
+  sha256 "35456afa1afb9df9273a120dc24cadbe2ff53a2613e5984b3cbc9a0a7c99c101"
 
   url "https://github.com/buttercup/buttercup-desktop/releases/download/v#{version}/Buttercup-mac-x64-#{version}.dmg",
       verified: "github.com/buttercup/buttercup-desktop/"
