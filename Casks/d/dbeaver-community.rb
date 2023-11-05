36dc04313ec52fc40b399c2f472baf1240079529
junminhong <junminhong1110@gmail.com>
2023-11-06 01:57:25 +0800

    dbeaver-community 23.2.4

--- Casks/d/dbeaver-community.rb
+++ Casks/d/dbeaver-community.rb
@@ -1,9 +1,9 @@
 cask "dbeaver-community" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "23.2.3"
-  sha256 arm:   "b5b3e781e28f6d205566ac717f6cf2a0ee2c0409150c9f29a29516bc48e22fce",
-         intel: "7eea3b60f42949032d4b2f9c5098e7ad63f9519657d3f76b5b66218c426f19d9"
+  version "23.2.4"
+  sha256 arm:   "31ae05b25d967f6b2f39b8f45c5a36e712f21a3a6cf50a7122e1bacf49b97c47",
+         intel: "a51ae7765d336021534a4c44b39365a5bcae89b1d8f3d3ba6c0b47628a70aa19"
 
   url "https://dbeaver.io/files/#{version}/dbeaver-ce-#{version}-macos-#{arch}.dmg"
   name "DBeaver Community Edition"
