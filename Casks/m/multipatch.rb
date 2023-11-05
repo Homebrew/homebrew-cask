8ae3155ef582ee7c2c73fe6b9baa5dd0be2b6fb9
Sam Ford <1584702+samford@users.noreply.github.com>
2023-11-05 16:42:00 -0500

    multipatch: update url, livecheck

--- Casks/m/multipatch.rb
+++ Casks/m/multipatch.rb
@@ -2,14 +2,15 @@ cask "multipatch" do
   version "2.0"
   sha256 "92d4077bc10802c7b4395d6716afc5c23bbdb34788be4a672fd5fef807a2072b"
 
-  url "https://projects.sappharad.com/multipatch/multipatch#{version.no_dots}.zip"
+  url "https://github.com/Sappharad/MultiPatch/releases/download/#{version}/multipatch#{version.no_dots}.zip",
+      verified: "github.com/Sappharad/MultiPatch/"
   name "MultiPatch"
   desc "File patching utility"
   homepage "https://projects.sappharad.com/multipatch/"
 
   livecheck do
-    url "https://github.com/Sappharad/MultiPatch"
-    strategy :git
+    url :url
+    strategy :github_latest
   end
 
   auto_updates true
