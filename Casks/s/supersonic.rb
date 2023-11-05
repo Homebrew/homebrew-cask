cdf1c2543eefeb2de28401e4b1b4c796a6578924
Drew Weymouth <dweymouth@gmail.com>
2023-11-05 10:35:46 -0800

    Remove arm support because of notarization requirement

--- Casks/s/supersonic.rb
+++ Casks/s/supersonic.rb
@@ -1,9 +1,8 @@
 cask "supersonic" do
-  arch arm: "arm64", intel: "x64"
+  arch intel: "x64"
 
   version "0.6.0"
-  sha256 arm:   "82356d7d63776063c28c8ab78d2d6fb48df9083b27fbfefff50273e66dbc393b",
-         intel: "49a63286b6eee27f996b0edfce0a780aef1803c1a539b70507a3eccb2bfc423e"
+  sha256 intel: "49a63286b6eee27f996b0edfce0a780aef1803c1a539b70507a3eccb2bfc423e"
 
   url "https://github.com/dweymouth/supersonic/releases/download/v#{version}/Supersonic-#{version}-mac-#{arch}.zip"
   name "Supersonic"
