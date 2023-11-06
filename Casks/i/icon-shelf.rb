e370691676df96ed7417841c5a99199758e2c01c
Rui Chen <rui@chenrui.dev>
2023-11-06 10:15:26 -0500

    icon-shelf 0.1.25

--- Casks/i/icon-shelf.rb
+++ Casks/i/icon-shelf.rb
@@ -1,9 +1,9 @@
 cask "icon-shelf" do
   arch arm: "-arm64"
 
-  version "0.1.24"
-  sha256 arm:   "5004379dfb9f2658fc85c871a5cfe6a5a55f0e81774e293a7d538e0d0e2515fe",
-         intel: "8081c28e6b03a0302ad56bd59100c486db73e3b14fd98accdb4121f87100112d"
+  version "0.1.25"
+  sha256 arm:   "de1245dd6835dbc83ca30b244f79ba636e71c2d8e03b373d8463ebd94ceed7b3",
+         intel: "d1d7cd6d39090f766950a27f2123c72965f5ef287ad1fa33c71c5fb1f70db49a"
 
   url "https://github.com/Icon-Shelf/icon-shelf/releases/download/v#{version}/Icon-Shelf-#{version}#{arch}.dmg",
       verified: "github.com/Icon-Shelf/icon-shelf/"
