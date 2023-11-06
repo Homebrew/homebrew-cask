70405d3287f10e7dc9a56d2f7cc9f65facf79ab1
Rui Chen <rui@chenrui.dev>
2023-11-06 10:43:41 -0500

    nomachine 8.10.1_1

--- Casks/n/nomachine.rb
+++ Casks/n/nomachine.rb
@@ -1,6 +1,6 @@
 cask "nomachine" do
-  version "8.9.1_1"
-  sha256 "0e73cef028bb8b33aad135172f490adf931e5d5ac05a3212211100bd9dbe2876"
+  version "8.10.1_1"
+  sha256 "2a5c394e98f1e801fde029f87cff3b8094bcfaec4130a1b37da8623bb490c6fd"
 
   url "https://download.nomachine.com/download/#{version.major_minor}/MacOSX/nomachine_#{version}.dmg"
   name "NoMachine"
