09604c84caf919ecf57f842ccb8b61469f82fff5
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-11-06 11:56:09 -0500

    macupdater: update `sha256` for `3.2.3`

--- Casks/m/macupdater.rb
+++ Casks/m/macupdater.rb
@@ -7,7 +7,7 @@ cask "macupdater" do
   end
   on_ventura :or_newer do
     version "3.2.3"
-    sha256 "1b14dd1965e2bb3cfa3986ad30ebfe5085b86bd0afe9a2862687f197c5784907"
+    sha256 "26623de0f05691b8f87df5207b70fdadf3e26cc0ece356a2286b503a4745cce5"
 
     depends_on macos: ">= :ventura"
   end
