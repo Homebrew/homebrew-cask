55a0f7a65a4f82de4ca605599d53241a80f5ceae
Miccal Matthews <miccal.matthews@gmail.com>
2023-11-07 22:19:01 +0800

    Update clibor.rb

--- Casks/c/clibor.rb
+++ Casks/c/clibor.rb
@@ -1,23 +1,21 @@
 cask "clibor" do
   version "1.3"
+  sha256 :no_check
 
   language "en", default: true do
-    sha256 :no_check
     url "https://chigusa-web.com/clibor-for-mac-en/dl/clibor-for-mac/"
   end
   language "ja" do
-    sha256 :no_check
     url "https://chigusa-web.com/clibor-for-mac/dl/clibor-for-mac/"
   end
 
   name "Clibor for Mac"
-  desc "Clipboard manager that enables copy and paste like FIFO / LIFO"
+  desc "Clipboard manager"
   homepage "https://chigusa-web.com/clibor-for-mac-en/"
 
   livecheck do
-    url "https://chigusa-web.com/clibor-for-mac-en/download/"
-    regex(/Clibor\.dmg – v(\d+\.\d+) – \d+\.\d+ MB/i)
-    strategy :page_match
+    url :homepage
+    regex(/Clibor\.dmg\s–\sv?(\d+(?:\.\d+)+)/i)
   end
 
   auto_updates true
