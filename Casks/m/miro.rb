8504e3524cb282d22088ec326162c8daf842ee1a
Rui Chen <rui@chenrui.dev>
2023-11-07 15:25:36 -0500

    miro 0.8.12

--- Casks/m/miro.rb
+++ Casks/m/miro.rb
@@ -1,7 +1,7 @@
 cask "miro" do
   arch arm: "darwin-arm64", intel: "darwin"
 
-  version "0.8.4"
+  version "0.8.12"
   sha256 :no_check
 
   url "https://desktop.miro.com/platforms/#{arch}/Miro.dmg"
