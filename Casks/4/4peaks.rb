4d912d3e9c741f14b1d32cc4d72edd55fce200b6
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-11-07 15:04:22 -0500

    4peaks: add zap

--- Casks/4/4peaks.rb
+++ Casks/4/4peaks.rb
@@ -13,4 +13,13 @@ cask "4peaks" do
   end
 
   app "4Peaks.app"
+
+  zap trash: [
+    "~/Library/Application Support/4Peaks",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.mekentosj.4peaks.*",
+    "~/Library/Caches/com.apple.helpd/Generated/4Peaks Help*",
+    "~/Library/HTTPStorages/com.mekentosj.4peaks",
+    "~/Library/Preferences/com.mekentosj.4peaks.plist",
+    "~/Library/Saved Application State/com.mekentosj.4peaks.savedState",
+  ]
 end
