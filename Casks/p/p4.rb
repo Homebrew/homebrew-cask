0d2571e7c47bf9a77fa3b1e48a184bbbbdb12715
Rui Chen <rui@chenrui.dev>
2023-11-08 06:12:07 -0500

    p4 2023.1,2513900

--- Casks/p/p4.rb
+++ Casks/p/p4.rb
@@ -1,6 +1,6 @@
 cask "p4" do
-  version "2023.1,2468153"
-  sha256 "1892bdbad11f948e8056fb64bdf0c801ab49966419ca5c2dea1d2680d70947b6"
+  version "2023.1,2513900"
+  sha256 "9f4652b4396578e2b98303439b6fa96b02797defee3c81d641293e2e374ac727"
 
   url "https://filehost.perforce.com/perforce/r#{version.major[-2..]}.#{version.minor}/bin.macosx1015x86_64/p4"
   name "Perforce Helix Command-Line Client (P4)"
