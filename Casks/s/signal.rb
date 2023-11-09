41cdeb699f6ebee01b93208c0a153613c65630b7
Rui Chen <rui@chenrui.dev>
2023-11-08 21:47:34 -0500

    signal 6.38.0

--- Casks/s/signal.rb
+++ Casks/s/signal.rb
@@ -1,9 +1,9 @@
 cask "signal" do
   arch arm: "arm64", intel: "x64"
 
-  version "6.37.0"
-  sha256 arm:   "150f239294308360267fe76434746ef131a4c81abe75ac6da46c16ae662b5e3b",
-         intel: "ae14f5eef08cc0fdfafad3633557e1d8b0f576fdc2693e5b2f11c6bbe3bad97e"
+  version "6.38.0"
+  sha256 arm:   "5c8213ed9cc2ad3b12ba949c9b2224d7d8d3dd833f61e1842e2ec092675b5288",
+         intel: "4d27e79aca917fa3225329ee2670b30e56f80439e2eb672d37be0e13c598c9ca"
 
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{arch}-#{version}.dmg"
   name "Signal"
