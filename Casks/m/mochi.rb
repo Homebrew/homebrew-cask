6c9c10fda3f820eac00e769a243a4559b739680b
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-11-09 09:03:28 -0500

    mochi 1.16.4

--- Casks/m/mochi.rb
+++ Casks/m/mochi.rb
@@ -1,9 +1,9 @@
 cask "mochi" do
   arch arm: "-arm64"
 
-  version "1.16.3"
-  sha256 arm:   "378eadad836665c6ed30ac24a4a383bf3b41d2d7f64ff5909a6a0182d84eedeb",
-         intel: "fb906ec964a72d2a4e9fafdb617be31cb92452efa05a5c836b68b7c7ed2a7429"
+  version "1.16.4"
+  sha256 arm:   "3399cd206f4bca4dbc4c51c6214db7b9c37cd3ecd900bc58838633f16c8aedb9",
+         intel: "f1372ee4979fb19ca18773c708db05f707f88f6dfa0eff5f82c334ccc8346411"
 
   url "https://mochi.cards/releases/Mochi-#{version}#{arch}.dmg"
   name "Mochi"
