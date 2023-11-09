49afae7098261334a998e6c270c241bb533f6281
Rui Chen <rui@chenrui.dev>
2023-11-08 22:56:16 -0500

    melodics 3.1.1773

--- Casks/m/melodics.rb
+++ Casks/m/melodics.rb
@@ -1,6 +1,6 @@
 cask "melodics" do
-  version "3.1.1626,CE4BE5FE-AB7C-4EBC-AB26-6C675844CF71"
-  sha256 "0abf1018e9b3bbd9dba11fda1f06926daf268fa93a384b79b982a29e1d789a5b"
+  version "3.1.1773,F2EEC977-E404-4C46-BA87-E9056B2C12F5"
+  sha256 "c0d1e1829bc1ab7f12ae5697e0e204dd1a808c4212e077bb2c71ccb471da373b"
 
   url "https://web-cdn.melodics.com/download/#{version.csv.second}.zip"
   name "Melodics"
