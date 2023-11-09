df6fa3b50cf3b6e257ff5806bd8d6d90a68858a8
Rui Chen <rui@chenrui.dev>
2023-11-09 00:03:51 -0500

    electron-fiddle 0.34.5

--- Casks/e/electron-fiddle.rb
+++ Casks/e/electron-fiddle.rb
@@ -1,9 +1,9 @@
 cask "electron-fiddle" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.34.2"
-  sha256 arm:   "1b428d2f8cd3af954f5955d72b23515d5cb02a0edcad551422e6958fdc8988c2",
-         intel: "c65a21c94f8fb3913ab65b1a848fcbbf57371e3e691c52d42d461b80c7f2c910"
+  version "0.34.5"
+  sha256 arm:   "4072c6c620a3b03c9997c0cb2380192c7b1b60ac3c583c328bdccf235a454416",
+         intel: "a632d1ed1dca9a3b8ee0ed1ba8f0c4ef7e75b2de39d33cfa3af458cf879d3c83"
 
   url "https://github.com/electron/fiddle/releases/download/v#{version}/Electron.Fiddle-darwin-#{arch}-#{version}.zip",
       verified: "github.com/electron/fiddle/"
