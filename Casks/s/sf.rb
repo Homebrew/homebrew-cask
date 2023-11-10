667d1c6378a3b78b22907d73326d69377d5c256f
Rui Chen <rui@chenrui.dev>
2023-11-10 14:06:48 -0500

    sf 2.16.8

--- Casks/s/sf.rb
+++ Casks/s/sf.rb
@@ -1,9 +1,9 @@
 cask "sf" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.16.7,ee67deb"
-  sha256 arm:   "6cbd506670b23040cb7b4f3125c7075bb8f9f2f8c561dd41ed68d04bc95bcf67",
-         intel: "73788038c0c4f5e769164c1b48e0cb74aee11f73621e19be32c8177624c8abe4"
+  version "2.16.8,91e68c4"
+  sha256 arm:   "ceca5add66cd30b9c26320fe480aad39e0d11c532125e1671a870109011a56e2",
+         intel: "d6af5c3f7fe3daf7032a28bcf2ab4399d25e37b42f9ed2f6737c1bf0166d9cc5"
 
   url "https://developer.salesforce.com/media/salesforce-cli/sf/versions/#{version.csv.first}/#{version.csv.second}/sf-v#{version.csv.first}-#{version.csv.second}-#{arch}.pkg"
   name "Salesforce CLI"
