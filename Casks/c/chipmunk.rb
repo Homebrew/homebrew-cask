b3bd508c6654e8eebc535590291dd9a4e15303ff
Rui Chen <rui@chenrui.dev>
2023-11-10 15:25:30 -0500

    chipmunk 3.10.3

--- Casks/c/chipmunk.rb
+++ Casks/c/chipmunk.rb
@@ -1,6 +1,6 @@
 cask "chipmunk" do
-  version "3.10.2"
-  sha256 "098c775fefdf095fb74e8cd5015071f15b4ba8f8a3930b43af6978ee7cfffddc"
+  version "3.10.3"
+  sha256 "136cd6cc31189015140f2b59b01bd8bd9a0197798caa767f2efcb0f08b9a395e"
 
   url "https://github.com/esrlabs/chipmunk/releases/download/#{version}/chipmunk@#{version}-darwin-portable.tgz"
   name "Chipmunk Log Analyzer & Viewer"
