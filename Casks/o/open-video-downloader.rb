b9723ed31407379d8b0134ae53b790222aa6348a
Chris Brown <code@drbyte.dev>
2023-11-11 00:01:13 -0500

    Fix url

--- Casks/o/open-video-downloader.rb
+++ Casks/o/open-video-downloader.rb
@@ -2,7 +2,7 @@ cask "open-video-downloader" do
   version "2.5.4"
   sha256 "a1c10401771c2526b47777d075fecf0401068ed40f3d3a524d52ec7e2405e769"
 
-  url "https://github.com/StefanLobbenmeier/youtube-dl-gui/releases/download/v#{version}/Open-Video-Downloader-#{version}-universal.dmg
+  url "https://github.com/StefanLobbenmeier/youtube-dl-gui/releases/download/v#{version}/Open-Video-Downloader-#{version}-universal.dmg"
   name "Open Video Downloader"
   desc "Cross-platform GUI for youtube-dl made in Electron and node.js"
   homepage "https://github.com/StefanLobbenmeier/youtube-dl-gui"
