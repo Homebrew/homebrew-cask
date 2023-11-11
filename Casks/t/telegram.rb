725fca45a051bb7670534a829fad3769bb2e8ca8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-11 12:44:43 +0000

    telegram 10.2.4,255298

--- Casks/t/telegram.rb
+++ Casks/t/telegram.rb
@@ -1,6 +1,6 @@
 cask "telegram" do
-  version "10.2.4,255294"
-  sha256 "ef5e381cdfef998bee477658c47c41f7fae8b25c8418375b4dcd4c2972fe97cc"
+  version "10.2.4,255298"
+  sha256 "ddc8cb0e7393157df7c1bc0a01709ee6accea16e14e18e708a73d4d7a3b15110"
 
   url "https://osx.telegram.org/updates/Telegram-#{version.csv.first}.#{version.csv.second}.app.zip"
   name "Telegram for macOS"
