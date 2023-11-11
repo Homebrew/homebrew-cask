8067f80c55007898afec917d6693beca9955d29b
Rui Chen <rui@chenrui.dev>
2023-11-11 01:07:06 -0500

    elephicon 2.7.5

--- Casks/e/elephicon.rb
+++ Casks/e/elephicon.rb
@@ -1,9 +1,9 @@
 cask "elephicon" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.7.4"
-  sha256 arm:   "2252d515315b7ff8442312c1bd79a34782423f24220e33711dba6f8696bc9937",
-         intel: "460da17a67daad261c0a24e5b89483e4560353871374f2ef8e9e4fa95aa2a8d7"
+  version "2.7.5"
+  sha256 arm:   "ffed4e120623b09a0e27bc3eddcd8bfd7f0aff147cbb877442af345c4090376d",
+         intel: "b38cd1599ce04115c5df9ddc55fd8c7c39dbd202d710ee54d89ebd75335ec10c"
 
   url "https://github.com/sprout2000/elephicon/releases/download/v#{version}/Elephicon-#{version}-darwin-#{arch}.dmg"
   name "Elephicon"
