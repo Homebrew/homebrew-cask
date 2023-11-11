55e1f0d1dcdc360c18e004627e0fde21249ba027
Rui Chen <rui@chenrui.dev>
2023-11-11 11:02:22 -0500

    stretchly 1.15.0

--- Casks/s/stretchly.rb
+++ Casks/s/stretchly.rb
@@ -1,9 +1,9 @@
 cask "stretchly" do
   arch arm: "-arm64"
 
-  version "1.14.1"
-  sha256 arm:   "512a92316439b4dabbb12e05618d19a6323088ed9f325db2c41a1e3faa391136",
-         intel: "092bb8c315cb8cba4c04b7867c94d826d789fe400143c616cfec1d1a19031f12"
+  version "1.15.0"
+  sha256 arm:   "47429cf3a149bd45a884e7ae9e1fc50b9f31afb315ce1e1dcc6f919ddc7bf8c8",
+         intel: "6065509aa6667939b464d912455b777ce64b6869cb47721562c786eea1e0c773"
 
   url "https://github.com/hovancik/stretchly/releases/download/v#{version}/stretchly-#{version}#{arch}.dmg",
       verified: "github.com/hovancik/stretchly/"
