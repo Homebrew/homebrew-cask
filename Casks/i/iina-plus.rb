c7f5ef8d93eaf687c1e705093c7de294610b3436
Rui Chen <rui@chenrui.dev>
2023-11-11 10:10:52 -0500

    iina-plus 0.7.15

--- Casks/i/iina-plus.rb
+++ Casks/i/iina-plus.rb
@@ -1,6 +1,6 @@
 cask "iina-plus" do
-  version "0.7.14"
-  sha256 "07cd1155e5630fd4bd10bac1d437497acfc37ec1dcc839bdee3df47e0b3f2610"
+  version "0.7.15"
+  sha256 "afedf5d2415d828b3ae9984338b69f7cebae2da318297b0ee801d30d7c954375"
 
   url "https://github.com/xjbeta/iina-plus/releases/download/#{version}/IINA+.#{version}.dmg"
   name "IINA+"
