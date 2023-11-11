f3c1d496c22e514ac031835a0351c120a09190dc
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-11-11 10:58:35 -0500

    open-video-downloader: livecheck updates, auto_update, depends_on

--- Casks/o/open-video-downloader.rb
+++ Casks/o/open-video-downloader.rb
@@ -7,9 +7,18 @@ cask "open-video-downloader" do
   desc "Cross-platform GUI for youtube-dl made in Electron and node.js"
   homepage "https://github.com/StefanLobbenmeier/youtube-dl-gui"
 
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
   app "Open Video Downloader.app"
 
   zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.jelleglebbeek.youtube-dl-gui.*",
     "~/Library/Preferences/com.jelleglebbeek.youtube-dl-gui.plist",
     "~/Library/Saved Application State/com.jelleglebbeek.youtube-dl-gui.savedState",
   ]
