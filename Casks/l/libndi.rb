463337264ebb10476f0a9179f2af529641f5e603
Bevan Kay <email@bevankay.me>
2023-11-13 10:39:25 +1100

    libndi: add cask

---
+++ Casks/l/libndi.rb
@@ -0,0 +1,14 @@
+cask "libndi" do
+  version :latest
+  sha256 :no_check
+
+  url "https://downloads.ndi.tv/SDK/NDI_SDK_Mac/libNDI_for_Mac.pkg",
+      verified: "downloads.ndi.tv/SDK/NDI_SDK_Mac/"
+  name "libNDI"
+  desc "NDI SDK"
+  homepage "https://ndi.video/"
+
+  pkg "libNDI_for_Mac.pkg"
+
+  uninstall pkgutil: "com.newtek.libndi"
+end
