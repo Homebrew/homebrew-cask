27108702ec4a8755e9f2ad35f1ae879f82c60f13
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-11-12 12:44:49 -0500

    reaper 7.04

--- Casks/r/reaper.rb
+++ Casks/r/reaper.rb
@@ -1,5 +1,5 @@
 cask "reaper" do
-  version "7.03"
+  version "7.04"
 
   on_mojave :or_older do
     sha256 "48840f0653df14e606bbec469a84a110a9410c1fda831388523669a7a7b46591"
@@ -7,7 +7,7 @@ cask "reaper" do
     url "https://dlcf.reaper.fm/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64.dmg"
   end
   on_catalina :or_newer do
-    sha256 "8092450e83d50b3dcb1f35f5139b2824225fafb1b90762128bd81307d70f4df4"
+    sha256 "4783c0cfbd0449a0cda825ca854e12bfb702ec8076232ae0f7b21fee8b3638bf"
 
     url "https://dlcf.reaper.fm/#{version.major}.x/reaper#{version.major_minor.no_dots}_universal.dmg"
   end
