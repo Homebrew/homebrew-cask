4ac2be38ef4023a468f2d9d71d815d2b85be29de
Bevan Kay <email@bevankay.me>
2023-11-13 14:27:05 +1100

    dingtalk 7.1.10.10_33121551, use universal build, update livecheck

--- Casks/d/dingtalk.rb
+++ Casks/d/dingtalk.rb
@@ -1,27 +1,19 @@
 cask "dingtalk" do
-  arch arm: "arm64", intel: "x86"
-  folder = on_arch_conditional arm: "M1-Beta/"
-  linkid = on_arch_conditional arm: "qd=2022mac-m1"
+  version "7.1.10.10_33121551"
+  sha256 "d1e243d713aa2fc01e9e3476ab79902c558aaab820f9d34fb1be17e6945ed81e"
 
-  on_arm do
-    version "7.1.2.22_32750040"
-    sha256 "1716e9fce77fa3913b6b760f17fe4de9e7a37851b0ee70407f28a532b1b0bee6"
-  end
-  on_intel do
-    version "7.1.0.21_32750009"
-    sha256 "6d2a68f29b9c44ef8c3e1b65cad3008791955c0a27f56e75d5d2b30dd6f6dad5"
-  end
-
-  url "https://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/#{folder}DingTalk_v#{version}_#{arch}.dmg"
+  url "https://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/M1-Beta/DingTalk_v#{version}_universal.dmg"
   name "DingTalk"
   name "钉钉"
   desc "Teamwork app by Alibaba Group"
   homepage "https://www.dingtalk.com/"
 
   livecheck do
-    url "https://www.dingtalk.com/mac/d/#{linkid}"
-    regex(/DingTalk[._-]v?(\d+(?:.\d+)+)[._-]#{arch}\.dmg/i)
-    strategy :header_match
+    url "https://im.dingtalk.com/manifest/appcast_gray_release.xml"
+    regex(/DingTalk[._-]v?(\d+(?:[._]\d+)+)[._-]universal\.dmg/i)
+    strategy :sparkle do |item|
+      item.url[regex, 1]
+    end
   end
 
   auto_updates true
