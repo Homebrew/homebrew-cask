d73a880cf124fcc0db8c68d8c3332b5c643bc75a
Bevan Kay <email@bevankay.me>
2023-11-13 14:58:11 +1100

    douyin-chat: use appcast for livecheck

--- Casks/d/douyin-chat.rb
+++ Casks/d/douyin-chat.rb
@@ -1,8 +1,8 @@
 cask "douyin-chat" do
-  version "1.1.5,7094550955558967563,11213796"
+  version "1.1.5,11213796"
   sha256 "70e8d195579fd878f369e955d1ef92a3d59ce5183cbb326ffbdbe64cfd9a3dea"
 
-  url "https://lf-impc.douyinstatic.com/obj/tos-aweme-im-pc/#{version.csv.second}/releases/#{version.csv.third}/#{version.csv.first}/darwin-x64/DouyinChat.dmg",
+  url "https://lf-impc.douyinstatic.com/obj/tos-aweme-im-pc/7094550955558967563/releases/#{version.csv.second}/#{version.csv.first}/darwin-x64/DouyinChat.dmg",
       verified: "lf-impc.douyinstatic.com/"
   name "Douyin Chat"
   name "抖音聊天"
@@ -10,10 +10,13 @@ cask "douyin-chat" do
   homepage "https://www.douyin.com/downloadpage/chat"
 
   livecheck do
-    url "https://www.douyin.com/downloadpage/chat"
-    regex(%r{tos[._-]aweme[._-]im[._-]pc/(\d+)/releases/(\d+)/(\d+(?:\.\d+)+)/darwin[._-]x64}i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| "#{match[2]},#{match[0]},#{match[1]}" }
+    url "https://tron.jiyunhudong.com/api/sdk/check_update?pid=7094550955558967563&uid=2386374454157337&branch=release&buildId=0"
+    strategy :json do |json|
+      version = json.dig("data", "manifest", "darwin", "version")
+      build = json.dig("data", "buildId")
+      next if version.blank? || build.blank?
+
+      "#{version},#{build}"
     end
   end
 
