b724d0551498c018a14aa5192ec36bc55deff396
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-11-13 08:26:45 -0500

    sf: update sha256

--- Casks/s/sf.rb
+++ Casks/s/sf.rb
@@ -3,7 +3,7 @@ cask "sf" do
 
   version "2.16.10,38f3e4f"
   sha256 arm:   "f6845bb2d329bdae9ed835f53390b8ad60e335f0cc15cd368cef63803e09b93a",
-         intel: "2fa5fc2af2cd2b525fb0822fbf870b2f8e038717d0d4bae54486a947d27524f8"
+         intel: "18d09197e7cd31519589d7f8117a369b1b0ffb26cc921ce53a9e17ac1165b454"
 
   url "https://developer.salesforce.com/media/salesforce-cli/sf/versions/#{version.csv.first}/#{version.csv.second}/sf-v#{version.csv.first}-#{version.csv.second}-#{arch}.pkg"
   name "Salesforce CLI"
