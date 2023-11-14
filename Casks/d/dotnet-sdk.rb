00eabfed000bc1992af6334235f95bb872aaa47b
asafcyclops <asaf@cyclops.security>
2023-11-14 21:48:16 +0200

    dotnet-sdk 7.0.404

--- Casks/d/dotnet-sdk.rb
+++ Casks/d/dotnet-sdk.rb
@@ -2,12 +2,12 @@ cask "dotnet-sdk" do
   arch arm: "arm64", intel: "x64"
 
   on_arm do
-    version "7.0.403,70b57ca7-7fcb-432d-bad9-809dbcfc35a7,8fca3fbc3f6e638edb29a908621cf9ba"
-    sha256 "a4b38ad38a0f40bf6e8d473628c0231b511124cfd6701dc488f2f51da6db5fa5"
+    version "7.0.404,bc914058-c08a-44a2-a581-5c9807143b55,ddb7f2330a4250f28b15e1329bc11fa9"
+    sha256 "8cd0c91195fc90ce83ade8b3fd6f8aabae77ee35deeb8820d1b94b802942a45b"
   end
   on_intel do
-    version "7.0.403,4243dbe5-7173-4422-a073-b13395231cfa,808d2c3639437ef67630dc0083a0fbd6"
-    sha256 "7b4d9fd4d003b681240eb9e79d757c30aa7437774a49a2a15dd7a09947476635"
+    version "7.0.404,81b6b736-1218-4bb0-9100-89945de897be,a2be9ad05a16c0b39c02f2328ccdc6b4"
+    sha256 "e374bc81d680193d9ce37a79e544325d5ad5944d6dff51037148e4437e8362f5"
   end
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv.second}/#{version.csv.third}/dotnet-sdk-#{version.csv.first}-osx-#{arch}.pkg"
