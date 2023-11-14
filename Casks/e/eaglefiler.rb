618db14aaadd9f70640c35c3df86b4260a01e779
Miccal Matthews <miccal.matthews@gmail.com>
2023-11-14 10:18:39 +0800

    Update eaglefiler.rb

--- Casks/e/eaglefiler.rb
+++ Casks/e/eaglefiler.rb
@@ -12,12 +12,14 @@ cask "eaglefiler" do
     regex(%r{href=.*?/EagleFiler[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
   end
 
+  depends_on macos: ">= :high_sierra"
+
   app "EagleFiler.app"
 
   zap trash: [
     "~/Library/Application Scripts/com.c-command.EagleFiler.EagleFilerShare",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.c-command.eaglefiler.sfl*",
     "~/Library/Application Support/EagleFiler",
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.c-command.eaglefiler.sfl2",
     "~/Library/Caches/com.c-command.EagleFiler",
     "~/Library/Containers/com.c-command.EagleFiler.EagleFilerShare",
     "~/Library/HTTPStorages/com.c-command.EagleFiler",
