2d1a210db3ca05ad73b3f1bf41c5c96f5e9f3781
Mikl Wolfe <wolfemikl@gmail.com>
2023-11-14 10:43:33 -0700

    uhk-agent 3.2.0

--- Casks/u/uhk-agent.rb
+++ Casks/u/uhk-agent.rb
@@ -1,6 +1,6 @@
 cask "uhk-agent" do
-  version "3.1.0"
-  sha256 "c3897aa5a449049bde33b3da955f5c72bc6194bd38aaf7b90e1941057ba01350"
+  version "3.2.0"
+  sha256 "765bb3153beb8699152a190544f2eb6c5a3a35bce0f92e726d27458370f1b602"
 
   url "https://github.com/UltimateHackingKeyboard/agent/releases/download/v#{version}/UHK.Agent-#{version}-mac.dmg"
   name "Ultimate Hacking Keyboard Agent"
