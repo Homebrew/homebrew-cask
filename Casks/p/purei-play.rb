b1aab3172ff7247b61955393e9352a78d6e7b4cb
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-11-15 14:14:42 -0500

    purei-play 2023-11-15

--- Casks/p/purei-play.rb
+++ Casks/p/purei-play.rb
@@ -1,6 +1,6 @@
 cask "purei-play" do
-  version "2023-11-09,bf72162c"
-  sha256 "475463edbe88d694025bb17612551214baf9913778efb53f8737cca44d5ba52c"
+  version "2023-11-15,932a3816"
+  sha256 "c15cb901d698305c8842dd7ea9094a1876c360944c55db773e9f7dc64436321a"
 
   url "https://s3.us-east-2.amazonaws.com/playbuilds/#{version.csv.second}/Play.dmg",
       verified: "s3.us-east-2.amazonaws.com/playbuilds/"
