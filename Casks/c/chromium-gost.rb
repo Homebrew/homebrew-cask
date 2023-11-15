d7037de54571ed6b60fe27223447af57915f26f3
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-11-15 09:33:38 -0500

    chromium-gost 119.0.6045.159

--- Casks/c/chromium-gost.rb
+++ Casks/c/chromium-gost.rb
@@ -1,9 +1,9 @@
 cask "chromium-gost" do
   arch arm: "arm64", intel: "amd64"
 
-  version "119.0.6045.123"
-  sha256 arm:   "28626794cf92101c3549a1a7eeed3bbf164edf9154e9973eeeaa98a348e1aabb",
-         intel: "666954cd3740d89edc8daaed74fe72101ba44412ced6f2af14dbd7bda42efdf0"
+  version "119.0.6045.159"
+  sha256 arm:   "3cfdca430f1247865b054bac36ee5bc07d1440c07ba5d9eaad41211b98cd3e67",
+         intel: "919c55210292866d4d2e152884c754b189932d33bf680c3fc0f95cd9690501e0"
 
   url "https://github.com/deemru/Chromium-Gost/releases/download/#{version}/chromium-gost-#{version}-macos-#{arch}.tar.bz2"
   name "Chromium-Gost"
