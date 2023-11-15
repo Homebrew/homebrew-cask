b2fe3d51cd714375aa7512cdd01a7951301250b4
Ladislas de Toldi <ladislas@detoldi.me>
2023-11-15 12:25:36 +0100

    tuist 3.32.1

--- Casks/t/tuist.rb
+++ Casks/t/tuist.rb
@@ -1,6 +1,6 @@
 cask "tuist" do
-  version "3.32.0"
-  sha256 "81c129e8bda17822fd4f3d43917a0543bd06d3b10e44c6bd033de2256c368672"
+  version "3.32.1"
+  sha256 "34a695e31828c2158ec8f3aba60ef0195e0a729bc5b02ac5a40eefd2b8c52e1a"
 
   url "https://github.com/tuist/tuist/releases/download/#{version}/tuist.zip",
       verified: "github.com/tuist/tuist/"
