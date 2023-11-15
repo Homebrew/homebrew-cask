c2ef706f1b3d6dbbd6312dc455a6c0cf7923dcb5
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-11-15 14:00:42 -0500

    steelseries-gg 51.0.0

--- Casks/s/steelseries-gg.rb
+++ Casks/s/steelseries-gg.rb
@@ -1,6 +1,6 @@
 cask "steelseries-gg" do
-  version "50.0.0"
-  sha256 "95d005a943223261ac4c093bdd9f787b4ca5a06bbc2d839236f946fb4a72189c"
+  version "51.0.0"
+  sha256 "b9b2399a61470af1ee3af6382030a9655c3700ef508fac96ee49b3a3e93fdb01"
 
   url "https://engine.steelseriescdn.com/SteelSeriesGG#{version}.pkg",
       verified: "engine.steelseriescdn.com/"
