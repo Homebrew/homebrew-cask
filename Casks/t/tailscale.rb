df20a0694551e1f02df6b0873956b7166885fe8e
ShredderWalnut <34428332+ShredderWalnut@users.noreply.github.com>
2023-11-16 11:19:10 +0800

    tailscale 1.54.0

--- Casks/t/tailscale.rb
+++ Casks/t/tailscale.rb
@@ -1,6 +1,6 @@
 cask "tailscale" do
-  version "1.52.1"
-  sha256 "8e9c5011c828175f6c92abc76b804fabca629b8286ebca52174ca4e5885974d7"
+  version "1.54.0"
+  sha256 "61290c0fcd3eec52fa8a70a57f680d8cfb735d96934fd93d70d22f8d710aa73f"
 
   url "https://pkgs.tailscale.com/stable/Tailscale-#{version}-macos.zip"
   name "Tailscale"
