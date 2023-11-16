db53c88e46a9f5fff5a3326419a4f0f23e21c4a5
LanikSJ <laniksj@gmail.com>
2023-11-16 15:34:07 -0800

    dropbox-dash 2.111.0

--- Casks/d/dropbox-dash.rb
+++ Casks/d/dropbox-dash.rb
@@ -1,8 +1,8 @@
 cask "dropbox-dash" do
-  version "2.103.0"
-  sha256 "c049ac46b6f37d8182dd316910fc338bce26b0816cd713f1c2a7a881e53f7f04"
+  version "2.111.0"
+  sha256 "fb8f4c49a1a47be51a0867f2ec6f40f501a24c6f199ddff2a2fb9941624c2c82"
 
-  url "https://dash-releases.s3.amazonaws.com/Dropbox%20Dash-#{version}.dmg",
+  url "https://edge.dropboxstatic.com/dbx-releng/products/dash-tesla/#{version}/mac.x86_64/Dropbox%20Dash-#{version}.dmg",
       verified: "dash-releases.s3.amazonaws.com/"
   name "Dropbox Dash"
   desc "Universal search tool"
