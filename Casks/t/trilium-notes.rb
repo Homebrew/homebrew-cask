76f5132e4e26e19bb96714eba14007d3b219a5e1
perfectra1n <jonfuller2012@gmail.com>
2023-11-15 16:10:04 -0800

    trilium-notes 0.61.14

--- Casks/t/trilium-notes.rb
+++ Casks/t/trilium-notes.rb
@@ -1,6 +1,6 @@
 cask "trilium-notes" do
-  version "0.61.13"
-  sha256 "383682d454200da33bd4221daedfb114651c044ab57ad965661d86308dafdcb1"
+  version "0.61.14"
+  sha256 "2b868567ca2568ea39bbac1aa3ac10b17d0a7a9debd4ab07e720595066f07edf"
 
   url "https://github.com/zadam/trilium/releases/download/v#{version}/trilium-mac-x64-#{version}.zip"
   name "Trilium Notes"
