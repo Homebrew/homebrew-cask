7b102922c61b3884b84966ed8bd10d0cab898f24
Ben Soares <b.soares@dundee.ac.uk>
2023-11-16 17:50:42 +0000

    Update Jalview to 2.11.3.0

--- Casks/j/jalview.rb
+++ Casks/j/jalview.rb
@@ -1,8 +1,11 @@
 cask "jalview" do
-  version "2.11.2.0"
-  sha256 "2f7bd183e346b521a40a3e52dcb5a785e78061b38faff4a3923ac8d0da96afad"
+  version "2.11.3.0"
+  arch  arm: "aarch64",
+        intel: "x64"
+  sha256 arm: "c67afbdc207db642f96454449e8594bb80aef4e0996faa2e7ddacd891638955c",
+         intel: "4faa926bc6d925bf6332eae2fc59c29d4eec61cb502509735b57a4adb5f0d5fd"
 
-  url "https://www.jalview.org/getdown/release/install4j/1.8/Jalview-#{version.dots_to_underscores}-macos-java_8.dmg"
+  url "https://www.jalview.org/downloads/installers/release/Jalview-#{version.dots_to_underscores}-macos-#{arch}-java_8.dmg"
   name "Jalview"
   desc "Multiple sequence alignment editor, visualisor, analysis and figure generator"
   homepage "https://www.jalview.org/"
