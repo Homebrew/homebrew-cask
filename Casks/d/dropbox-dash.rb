76af9ffa0ea0b6c5ad471a358c4cef2c60965d39
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-11-16 18:59:49 -0500

    dropbox-dash: update verified and livecheck
    dropbox-dash: update livecheck

--- Casks/d/dropbox-dash.rb
+++ Casks/d/dropbox-dash.rb
@@ -3,13 +3,13 @@ cask "dropbox-dash" do
   sha256 "fb8f4c49a1a47be51a0867f2ec6f40f501a24c6f199ddff2a2fb9941624c2c82"
 
   url "https://edge.dropboxstatic.com/dbx-releng/products/dash-tesla/#{version}/mac.x86_64/Dropbox%20Dash-#{version}.dmg",
-      verified: "dash-releases.s3.amazonaws.com/"
+      verified: "edge.dropboxstatic.com/dbx-releng/products/dash-tesla/"
   name "Dropbox Dash"
   desc "Universal search tool"
   homepage "https://www.dropbox.com/dash"
 
   livecheck do
-    url "https://dash-releases.s3.amazonaws.com/latest-mac.yml"
+    url "https://client.dropbox.com/electron_builder/dash-tesla/update_check/stable-mac.yml?arch=x64&version=0"
     strategy :electron_builder
   end
 
