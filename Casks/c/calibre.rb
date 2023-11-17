07a441625225258ca70be297539fec9c6275a8b0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-17 06:33:20 +0000

    calibre 7.0.0

--- Casks/c/calibre.rb
+++ Casks/c/calibre.rb
@@ -24,8 +24,8 @@ cask "calibre" do
     end
   end
   on_big_sur :or_newer do
-    version "6.29.0"
-    sha256 "2f76428ae19617875c5725cd892751a80eb2acdda76e06cd19c2f21a63966998"
+    version "7.0.0"
+    sha256 "884061bce0df80085b4a9ce60da1f63530040f615804eb8cf499718a2e314a8e"
 
     livecheck do
       url "https://github.com/kovidgoyal/calibre"
