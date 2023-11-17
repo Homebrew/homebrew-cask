dbb754e8f3eac08e97630f95d3966dbfbe1d2e94
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-11-16 19:15:55 -0500

    openzfs: update versions for all os and arch

--- Casks/o/openzfs.rb
+++ Casks/o/openzfs.rb
@@ -5,37 +5,42 @@ cask "openzfs" do
       version "2.2.0,496"
       sha256 "2fb66a62d21319a6d818035e0c744e5991e485792ed5f2b9b32f11fb1d846f2a"
     end
-    on_ventura :or_newer do
+    on_ventura do
       arch arm: "Ventura-13-arm64"
       version "2.2.0,494"
       sha256 "0bd8871abf1658d18b0b5a38349f34f8ace4698676028ca3957c515836b1e9a5"
+    end
+    on_sonoma :or_newer do
+      arch arm: "Sonoma-14-arm64"
+      version "2.2.0,492"
+      sha256 "2bcd47a3af097e16a532d973222cc5ac9559b67aa385b479cf47ab6d2e70e85b"
     end
   end
   on_intel do
     on_el_capitan :or_older do
       arch intel: "EL.CAPITAN-10.11"
-      version "2.1.6,427"
-      sha256 "8cccc2db29724d32de0db836d8649c0fb90fa7480624d66f354fd58d8d9199f4"
+      version "2.2.0,487"
+      sha256 "e8f2785128f3659a3bffc0f619926621f4451a2e0b5ee7be2d3be6072b0321bb"
     end
     on_sierra do
       arch intel: "Sierra-10.12"
-      version "2.1.6,428"
-      sha256 "5285dc7f54f107023c129e22ec48e9c794e953e03537325d33a29254d237e726"
+      version "2.2.0,486"
+      sha256 "7957a22e6db39351a2f84b06212cc807c673261e49988be14964c78efc97edff"
     end
     on_high_sierra do
       arch intel: "High.Sierra-10.13"
-      version "2.1.6,429"
-      sha256 "0b7ac146858fe0fb70b223d4ee1d9ef3fe2ef280d84d17d49e13205e0f5df7ea"
+      version "2.2.0,488"
+      sha256 "318e9866c87d7baa7b5262bd609c51f192cbcc2f05c0e8614f30d7bdaaa3ad73"
     end
     on_mojave do
       arch intel: "Mojave-10.14"
-      version "2.1.6,430"
-      sha256 "74d54451e1ffbabb982888e81b50e4310622bac65e7846ecf92df74e304bc98f"
+      version "2.2.0,489"
+      sha256 "92d7a06e2ae5afc2de4d217775ca3d44a1684024e74588ebe28d82bae48b3679"
     end
     on_catalina do
       arch intel: "Catalina-10.15"
-      version "2.1.6,424"
-      sha256 "b37bf6c86f103fd7d4a9b219e3d7a8c041acef2ae87a807ef97f976c731b089d"
+      version "2.2.0,490"
+      sha256 "f877469bad532ef1178d9b927eeaefc5d894fec4ca210c148af10a2521735ccc"
     end
     on_big_sur do
       arch intel: "Big.Sur-11"
@@ -47,10 +52,15 @@ cask "openzfs" do
       version "2.2.0,493"
       sha256 "3549fcd07820ec75c6f21674988c7ad41dfd834976d9d8ca54e353ee46af3eaf"
     end
-    on_ventura :or_newer do
+    on_ventura do
       arch intel: "Ventura-13"
-      version "2.2.0,495"
-      sha256 "8818bb186c610e9d51b8e48ff4ad545794c835b0df246ff22b3e0dce37d74aba"
+      version "2.2.0,494"
+      sha256 "0bd8871abf1658d18b0b5a38349f34f8ace4698676028ca3957c515836b1e9a5"
+    end
+    on_sonoma :or_newer do
+      arch intel: "Sonoma-14"
+      version "2.2.0,497"
+      sha256 "c030556ac295c787a95c78cbd76b4a83db69bc119bec7fc2feb018938921c395"
     end
   end
 
