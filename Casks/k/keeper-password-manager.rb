de39b68ab144bc16e2e1de1cae4b88196d2a2ff2
Mikl Wolfe <wolfemikl@gmail.com>
2023-11-17 08:07:18 -0700

    keeper-password-manager 16.10.10

--- Casks/k/keeper-password-manager.rb
+++ Casks/k/keeper-password-manager.rb
@@ -1,5 +1,5 @@
 cask "keeper-password-manager" do
-  version "16.10.9,16959317881016"
+  version "16.10.10,17001555381131"
   sha256 :no_check
 
   url "https://keepersecurity.com/desktop_electron/Darwin/KeeperSetup.dmg"
