3b37e9683d901d46f526880ca9d4c93c228d4e99
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-11-17 08:19:26 -0500

    cursor 0.16.0

--- Casks/c/cursor.rb
+++ Casks/c/cursor.rb
@@ -1,9 +1,9 @@
 cask "cursor" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.15.5,231115a5mv63u9f"
-  sha256 arm:   "e6443c55264891cfd3b180a0134b152ac48ec80b41e38ae92aee5b746821c10c",
-         intel: "28355274ba262289ee489888efc071e97b9118ea3d230fde062ad11a1d258345"
+  version "0.16.0,231116rek2xuq6a"
+  sha256 arm:   "01dae7ccbe4abc0a6abf6d1093350815b090587b2e67693ac05e367effe22fe4",
+         intel: "1a4bf0a3bc72afc9d3e94b8c40acdf4789a886837566f9714020087515f4e850"
 
   url "https://download.todesktop.com/230313mzl4w4u92/Cursor%20#{version.csv.first}%20-%20Build%20#{version.csv.second}-#{arch}-mac.zip",
       verified: "download.todesktop.com/230313mzl4w4u92/"
