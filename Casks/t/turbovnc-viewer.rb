ac6f0fbe6d48e920b23fdd7e8dfb4ced86b40d3f
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-11-18 10:33:51 -0500

    turbovnc-viewer 3.1

--- Casks/t/turbovnc-viewer.rb
+++ Casks/t/turbovnc-viewer.rb
@@ -1,9 +1,9 @@
 cask "turbovnc-viewer" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "3.0.91"
-  sha256 arm:   "77ea491139eff7cf897e59e6fab52442cd3b560f804f9c2b1bd6c5a4cb5abf69",
-         intel: "b045ceeef338415de7dba04ce5b90e0aca656152c1f6d760fbc45028138885c5"
+  version "3.1"
+  sha256 arm:   "b0d9071d517d5e510605228d47d862ba94874e25e3d508116f2195913999890c",
+         intel: "7d302dd611ff7b1ab92d2215ca550361dbf081b6bbf3916f3640730483a314d4"
 
   url "https://downloads.sourceforge.net/turbovnc/#{version}/TurboVNC-#{version}-#{arch}.dmg",
       verified: "sourceforge.net/turbovnc/"
