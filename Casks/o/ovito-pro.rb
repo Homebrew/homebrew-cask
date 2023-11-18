4e9845ab909bac55f392c917a403739ab28bbef9
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-11-18 14:25:19 -0500

    ovito-pro: update sha256, name, and livecheck

--- Casks/o/ovito-pro.rb
+++ Casks/o/ovito-pro.rb
@@ -3,16 +3,16 @@ cask "ovito-pro" do
 
   version "3.9.4"
   sha256 arm:   "43014be524a9fa538ee2679e5e65d4a81dfefbbfb919368d527cdc5c03a83caa",
-         intel: "43014be524a9fa538ee2679e5e65d4a81dfefbbfb919368d527cdc5c03a83caa"
+         intel: "d63e0bd9e4e7b192c03481b5671cacde90f4024d63bfac61e057e36457a74a6e"
 
   url "https://www.ovito.org/download/master/ovito-pro-#{version}-macos-#{arch}.dmg"
-  name "OVITO"
+  name "OVITO Pro"
   desc "Scientific data visualization and analysis software"
   homepage "https://www.ovito.org/"
 
   livecheck do
     url "https://www.ovito.org/os-downloads/"
-    regex(/Version (\d+(?:\.\d+)*)/i)
+    regex(/version\s(\d+(?:\.\d+)*)/i)
     strategy :page_match
   end
 
