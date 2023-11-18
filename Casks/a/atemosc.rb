1ec746adabf51206e549d8e8ff59ee88c1638050
Bevan Kay <email@bevankay.me>
2023-11-18 13:37:44 +1100

    atemosc: fix shasum

--- Casks/a/atemosc.rb
+++ Casks/a/atemosc.rb
@@ -2,7 +2,8 @@ cask "atemosc" do
   arch arm: "-arm64"
 
   version "5.0.0"
-  sha256 "a7a2b7f39c8a19e66739fd51427ff4b8c90c7f9357a9c7a7c47da71c99f90c4d"
+  sha256 arm:   "a7a2b7f39c8a19e66739fd51427ff4b8c90c7f9357a9c7a7c47da71c99f90c4d",
+         intel: "3cd9ed1ccbcdabebe06bcce5b7ef3bcc059168cb7b2bebe5b472ebec9026c2d9"
 
   url "https://atemosc.com/download/atemOSC-#{version}#{arch}.dmg"
   name "atemOSC"
