877a84d1e00997546bb678a1504682122780c02d
Jocker <yutaoo1230@gmail.com>
2023-11-19 14:15:14 +0800

    update: CloudNet 1.36.2.12

--- Casks/c/cloudnet.rb
+++ Casks/c/cloudnet.rb
@@ -1,6 +1,6 @@
 cask "cloudnet" do
-  version "1.36.2.11"
-  sha256 "7be000cd1106dd7b76d11175102f6b8d76ca949955c2741f173eeb473ef4ed01"
+  version "1.36.2.12"
+  sha256 "b97a73bce260af35dc7674a225549d24d290d76a1d1c4b450263d063ee3259fa"
 
   url "https://pkgs.cloudnet.world/stable/macos/CloudNet_v#{version}.dmg"
   name "CloudNet for Mac client"
