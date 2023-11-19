90b6fdcb6d1ac10cbfc802518c2f387c8e8754ec
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-19 18:32:10 +0000

    dbeaver-community 23.2.5

--- Casks/d/dbeaver-community.rb
+++ Casks/d/dbeaver-community.rb
@@ -1,9 +1,9 @@
 cask "dbeaver-community" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "23.2.4"
-  sha256 arm:   "31ae05b25d967f6b2f39b8f45c5a36e712f21a3a6cf50a7122e1bacf49b97c47",
-         intel: "a51ae7765d336021534a4c44b39365a5bcae89b1d8f3d3ba6c0b47628a70aa19"
+  version "23.2.5"
+  sha256 arm:   "68e283cfe202dbbc3acc6e18cfdcbba201a8230bbe427cf08fe01821d70e1802",
+         intel: "1cca1241c8e569e1296c237352672846d51fd3a8e2d0a27a5f06a20b11178137"
 
   url "https://dbeaver.io/files/#{version}/dbeaver-ce-#{version}-macos-#{arch}.dmg"
   name "DBeaver Community Edition"
