21ba93cfb1a2d2bf0ecdcfc55a97d6f4499b24b5
Sam Ford <1584702+samford@users.noreply.github.com>
2023-11-20 16:43:58 -0500

    quassel-client: update url, livecheck

--- Casks/q/quassel-client.rb
+++ Casks/q/quassel-client.rb
@@ -2,15 +2,18 @@ cask "quassel-client" do
   version "0.14.0"
   sha256 "73b1b65f0e75c88d1dd23aa91c1916a6a3c231472a042eca0907689ab0981b60"
 
-  url "https://quassel-irc.org/pub/QuasselClient-macOS-#{version}.dmg"
+  url "https://github.com/quassel/quassel/releases/download/#{version}/QuasselClient-macOS-#{version}.dmg",
+      verified: "github.com/quassel/quassel/"
   name "Quassel IRC"
-  desc "Quassel IRC: Chat comfortably.  Everywhere"
+  desc "Quassel IRC: Chat comfortably. Everywhere"
   homepage "https://quassel-irc.org/"
 
   livecheck do
-    url "https://github.com/quassel/quassel"
+    url :url
     strategy :github_latest
   end
+
+  depends_on macos: ">= :high_sierra"
 
   app "Quassel Client.app"
 end
