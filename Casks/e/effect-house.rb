848ad2e62c4d6f69b230f522a50fb8fb9577d443
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-11-19 19:12:30 -0500

    Update effect-house.rb

--- Casks/e/effect-house.rb
+++ Casks/e/effect-house.rb
@@ -1,21 +1,24 @@
 cask "effect-house" do
+  arch arm: "_M1_101", intel: "_Intel_101"
+
   on_arm do
-    version "3.4.1.424,M1_101,341_ExternalRelease_1107"
+    version "3.4.1,424,1107"
+    sha256 "cef69652a1e4572aeec7eb72b147bd072340f866a93cb28886adfe82452217eb"
   end
   on_intel do
-    version "3.4.1.468,Intel_101,341_ExternalRelease_1107"
+    version "3.4.1,468,1107"
+    sha256 "ecf9cfadf8ee587f4845fe771297a904ffe347f9e1c70b66fd5312202ae78e18"
   end
-  sha256 :no_check
+
+  url "https://sf16-va.tiktokcdn.com/obj/eden-va2/olaa_ajlmml_zlp/ljhwZthlaukjlkulzlp/#{version.csv.first.no_dots}_ExternalRelease_#{version.csv.third}/Effect_House_v#{version.csv.first}.#{version.csv.second}#{arch}.dmg",
+      verified: "sf16-va.tiktokcdn.com/"
+  name "TikTok Effect House"
+  desc "Create vibrant AR effects for TikTok"
+  homepage "https://effecthouse.tiktok.com/"
 
   livecheck do
     skip "No version information available"
   end
-
-  url "https://sf16-va.tiktokcdn.com/obj/eden-va2/olaa_ajlmml_zlp/ljhwZthlaukjlkulzlp/#{version.csv.third}/Effect_House_v#{version.csv.first}_#{version.csv.second}.dmg",
-      verified: "sf16-va.tiktokcdn.com/"
-  name "Effect House"
-  desc "Create vibrant AR effects for TikTok"
-  homepage "https://effecthouse.tiktok.com/"
 
   depends_on macos: ">= :sierra"
 
