323f618d82dc5bf06bdab4c7682bb696ab6e57d4
Sam Ford <1584702+samford@users.noreply.github.com>
2023-11-20 16:49:42 -0500

    trezor-suite: update url, livecheck

--- Casks/t/trezor-suite.rb
+++ Casks/t/trezor-suite.rb
@@ -5,17 +5,19 @@ cask "trezor-suite" do
   sha256 arm:   "886e5a47acbe09447bebe8a43628b3b859d55e3c5ee734ed3024eeb877e9ca99",
          intel: "55a16ee64c1f3d111443efc638dffb1788775fe6fd5e1fb9869a301bb860600b"
 
-  url "https://suite.trezor.io/web/static/desktop/Trezor-Suite-#{version}-mac-#{arch}.dmg"
+  url "https://github.com/trezor/trezor-suite/releases/download/v#{version}/Trezor-Suite-#{version}-mac-#{arch}.dmg",
+      verified: "github.com/trezor/trezor-suite/"
   name "TREZOR Suite"
   desc "Companion app for the Trezor hardware wallet"
   homepage "https://suite.trezor.io/"
 
   livecheck do
-    url "https://github.com/trezor/trezor-suite"
+    url :url
     strategy :github_latest
   end
 
   auto_updates true
+  depends_on macos: ">= :catalina"
 
   app "Trezor Suite.app"
 
