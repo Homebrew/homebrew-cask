620241df981726f2ef2b041d9b48ef81b2a43fa6
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-11-20 19:08:14 -0500

    bunch 1.4.16

--- Casks/b/bunch.rb
+++ Casks/b/bunch.rb
@@ -1,6 +1,6 @@
 cask "bunch" do
-  version "1.4.13,173"
-  sha256 "3e29b3161a8be58c9f89fa92927d492a0919090dfa6e468ea1e15c517ccc8b04"
+  version "1.4.16,177"
+  sha256 "efabe6b190b0c9e3484384887ea26efd8a37b7747b0c450ab493ff2e71f0ac4f"
 
   url "https://brettterpstra.com/updates/bunch/Bunch#{version.csv.first}#{version.csv.second}.dmg",
       verified: "brettterpstra.com/updates/bunch/"
