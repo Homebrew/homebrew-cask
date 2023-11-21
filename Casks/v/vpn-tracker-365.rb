a7f7108c2c8c55f6e92bd12d6429366bb459e15c
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-11-21 08:00:12 -0500

    vpn-tracker-365 23.6.0

--- Casks/v/vpn-tracker-365.rb
+++ Casks/v/vpn-tracker-365.rb
@@ -1,7 +1,7 @@
 cask "vpn-tracker-365" do
   # NOTE: "365" is not a version number, but an intrinsic part of the product name
-  version "23.5.3,230530"
-  sha256 "e5aa506ae5b3f396e1b7d061709cf047d25357e132ac4334a3a3a802789810cf"
+  version "23.6.0,230604"
+  sha256 "a73e1c9a9317b8ef732b9c1abf47596f8455e30c08459f74bb8bf1faf708123c"
 
   url "https://download.equinux.com/files/other/VPN%20Tracker%20365%20-%20#{version.csv.first}%20#{version.csv.second}.zip",
       verified: "download.equinux.com/files/other/"
