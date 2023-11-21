3f7ecbf84b7a32885dd3a6154092fd94568af482
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-11-21 07:59:11 -0500

    hey 1.2.14

--- Casks/h/hey.rb
+++ Casks/h/hey.rb
@@ -1,9 +1,9 @@
 cask "hey" do
   arch arm: "-arm64"
 
-  version "1.2.13"
-  sha256 arm:   "7cbad39de3d591f67cdf754b00fdb3adb971482e3d4081c0d197cc71282902ea",
-         intel: "04e66b402baea4bc7e4c0e826030f915f9d8e28b2f9996f315f87ab27b7ae1c2"
+  version "1.2.14"
+  sha256 arm:   "ffe8da4959aa02c654704b150078b5dfbdfd0bc04778aa813c3c5fd072298d6e",
+         intel: "ec7be5edda258f044c6bd6d1862fb6e0dd317e3ee18d5bc3d0b3c649a3d8607d"
 
   url "https://hey-desktop.s3.amazonaws.com/HEY-#{version}#{arch}-mac.zip",
       verified: "hey-desktop.s3.amazonaws.com/"
