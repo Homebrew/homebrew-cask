826edcf5e273209865f5ab8c5b325223bb6292ea
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-11-21 08:00:15 -0500

    pivy 0.11.2

--- Casks/p/pivy.rb
+++ Casks/p/pivy.rb
@@ -1,6 +1,6 @@
 cask "pivy" do
-  version "0.11.1"
-  sha256 "df5857714b0a5a5710a6a36c7197b3c3c453de846b92a21ba81d569f35670549"
+  version "0.11.2"
+  sha256 "a8249869cc6a922eabe80ac64e5895b503a4f2d23007b21497aa0aca11e2b20c"
 
   url "https://github.com/joyent/pivy/releases/download/v#{version}/pivy-#{version}-macos12.pkg"
   name "pivy"
