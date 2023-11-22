2b2b498e16a11ced4dc632d18be32f2793bf881c
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-11-22 10:30:49 -0500

    minisim 0.8.2

--- Casks/m/minisim.rb
+++ Casks/m/minisim.rb
@@ -1,6 +1,6 @@
 cask "minisim" do
-  version "0.8.1"
-  sha256 "62156b4c4631495c0966636a14333c5853402301deaf472a6138f3b44e019027"
+  version "0.8.2"
+  sha256 "c136983c7d18ac67331b96989ce9b35ac274ed92d296033a54b58e501df18ca8"
 
   url "https://github.com/okwasniewski/MiniSim/releases/download/v#{version}/MiniSim.app.zip",
       verified: "github.com/okwasniewski/MiniSim/"
