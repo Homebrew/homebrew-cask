77e9a32328fc583a8bddea19f9bee9c474c0d97f
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-11-22 14:08:39 -0500

    volanta 1.7.1

--- Casks/v/volanta.rb
+++ Casks/v/volanta.rb
@@ -1,6 +1,6 @@
 cask "volanta" do
-  version "1.7.0,4ec9417b"
-  sha256 "3c5eba3da8fdcea08e1ab02c42fad7ad5df9d4fb08b5c222d2b0a8032a8d41e4"
+  version "1.7.1,500b797e"
+  sha256 "4e5da1f34560d7372f2f21f67c5faaac9281f280a485523167175f1467c98440"
 
   url "https://cdn.volanta.app/software/volanta-app/#{version.csv.first}-#{version.csv.second}/volanta-#{version.csv.first}.dmg"
   name "Volanta"
