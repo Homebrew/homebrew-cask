35ca606295aa302d1bbc9bc40e4390b92b700985
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-11-22 14:01:05 -0500

    sparrow 1.8.1

--- Casks/s/sparrow.rb
+++ Casks/s/sparrow.rb
@@ -1,9 +1,9 @@
 cask "sparrow" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.8.0"
-  sha256 arm:   "a144925d9b58462202dd87100f1e3bc7b135cedaa033f6dd28d2290e4a5c09c8",
-         intel: "72098d503b807268226832f2e883cf2ec862475d849265cad00dbbf3956777d9"
+  version "1.8.1"
+  sha256 arm:   "b2203fc939ae61068dbd084e256e581b58813019aea8b2c71beb70cfe33ebbe1",
+         intel: "2789804db778f553f3b743fe87eb2affa171f3a427185aba2d902e07a02a1588"
 
   url "https://github.com/sparrowwallet/sparrow/releases/download/#{version}/Sparrow-#{version}-#{arch}.dmg",
       verified: "github.com/sparrowwallet/sparrow/"
