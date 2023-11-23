bc5857fb5b0cbcea2cc3586c685e6d9c471003c2
langdon <getpacman.gq@gmail.com>
2023-11-23 12:56:56 -0500

    aded my new cask

---
+++ Casks/p/pac-man.rb
@@ -0,0 +1,22 @@
+cask "pac-man" do
+  version "10.31"
+  sha256 :no_check
+  url "http://www2.getpacman.gq/files/Pac-Man.zip"
+
+  name "Pac-Man"
+  desc "A Java-Based Pac-Man Game" 
+  homepage "https://www.getpacman.gq"
+
+  app "Pac-Man.app"
+end
+
+
+
+=begin
+
+
+,
+      verified: "http://www2.getpacman.gq"
+=end
+
+git add Casks/p/pac-man.rb
