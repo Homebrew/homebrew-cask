f6843524d63c05252bf2e0f5c73429c68ce30612
Thomas Walter <tw@b-a-l-u.de>
2023-11-23 19:37:38 +0100

    prusaslicer 2.7.0

--- Casks/p/prusaslicer.rb
+++ Casks/p/prusaslicer.rb
@@ -1,6 +1,6 @@
 cask "prusaslicer" do
-  version "2.6.1,202309060812"
-  sha256 "a58aa133d590757dfa550bbf8ba306ef1339e6c9cc662017f45df858949b920d"
+  version "2.7.0,202311231501"
+  sha256 "9a8b5d5b4ed771877d55d311fbf7ebebe35b5c84a4eb5ff310ec2570fd5f9155"
 
   url "https://github.com/prusa3d/PrusaSlicer/releases/download/version_#{version.csv.first}/PrusaSlicer-#{version.csv.first}+MacOS-universal-#{version.csv.second}.dmg",
       verified: "github.com/prusa3d/PrusaSlicer/"
