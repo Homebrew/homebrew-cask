fecad0ff240ad9250815d5a6841ebebde5b7f4d9
singularitti <singularitti@outlook.com>
2023-11-23 15:34:34 -0500

    Add cask `mindmac` v1.8

---
+++ Casks/m/mindmac.rb
@@ -0,0 +1,24 @@
+cask "mindmac" do
+  version "1.8.0"
+  sha256 "ef9b8078d59f0f8786682301b284f678699be7299784ccb56405a44a5f25d688"
+
+  url "https://github.com/MindMacApp/MindMac/releases/download/#{version}/MindMac_#{version}.dmg"
+  name "MindMac"
+  desc "ChatGPT client"
+  homepage "https://mindmac.app/index.html"
+
+  livecheck do
+    url "https://github.com/MindMacApp/MindMac"
+    strategy :github_latest
+  end
+
+  app "MindMac.app"
+
+  zap trash: [
+    "~/Library/Application Support/app.mindmac.macos",
+    "~/Library/Application Support/MindMac",
+    "~/Library/Caches/app.mindmac.macos",
+    "~/Library/Preferences/app.mindmac.macos.plist",
+    "~/Library/Saved Application State/app.mindmac.macos.savedState",
+  ]
+end
