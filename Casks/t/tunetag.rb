20750f084133cf1f68cec350ed29575cd79bf499
Philipp Jetschina <kontakt@jetschina.de>
2023-11-25 00:27:10 +0100

    fix url path

--- Casks/t/tunetag.rb
+++ Casks/t/tunetag.rb
@@ -2,7 +2,7 @@ cask "tunetag" do
   version "1.2"
   sha256 :no_check
 
-  url "https://sweetpproductions.com/products/tunetag/tunetag.dmg"
+  url "https://sweetpproductions.com/products/tunetag/TuneTag.dmg"
   name "TuneTag"
   desc "TuneTag is a lightning fast ID3 and meta data editor for mp3 and m4a audio files."
   homepage "https://tunetag.sweetpproductions.com/"
