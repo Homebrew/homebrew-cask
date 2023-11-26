f22ed2a6542e689411f1767f79f5ffe72f830313
Amory Meltzer <Amorymeltzer@gmail.com>
2023-11-25 21:47:51 -0500

    marathon2 20231125

--- Casks/m/marathon2.rb
+++ Casks/m/marathon2.rb
@@ -1,7 +1,7 @@
 cask "marathon2" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "20230529"
-  sha256 "3d85be6ae7b45c926fad71cb6b05237836c8a6bdf0af5299777a138a3aa6eb36"
+  version "20231125"
+  sha256 "709ab94e35a8cf83167afe6fcf14a862db945a9193deeae2ec98826f1812da52"
 
   url "https://github.com/Aleph-One-Marathon/alephone/releases/download/release-#{version}/Marathon2-#{version}-Mac.dmg",
       verified: "github.com/Aleph-One-Marathon/alephone/"
