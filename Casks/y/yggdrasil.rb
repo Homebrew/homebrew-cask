f204f77773318d83bad679c6d4390b506aee9b8a
Rui Chen <rui@chenrui.dev>
2023-11-27 13:29:45 -0500

    yggdrasil 0.5.4

--- Casks/y/yggdrasil.rb
+++ Casks/y/yggdrasil.rb
@@ -1,9 +1,9 @@
 cask "yggdrasil" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.5.3"
-  sha256 arm:   "fed16cc8f8e8b5746831fd3946453350ad900b2e460bb15ddb209d80c3e8794d",
-         intel: "aea9a510cd73ed7fe0c743608a89e5f297eff4f2829244764022b18dd4abac34"
+  version "0.5.4"
+  sha256 arm:   "eb9951b8af67a4c2797d6e671641362def0cc52a54bd71b8d1109b277bbda93e",
+         intel: "72d7b171fdb88ec75ac0602cf34f847f189a914aea87a34525d1b0ddf17fb87c"
 
   url "https://github.com/yggdrasil-network/yggdrasil-go/releases/download/v#{version}/yggdrasil-#{version}-macos-#{arch}.pkg"
   name "Yggdrasil"
