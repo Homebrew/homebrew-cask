ee708f0d168ab35cc342273eb13bc8ae87ae4279
Mikl Wolfe <wolfemikl@gmail.com>
2023-11-27 08:18:40 -0700

    heptabase 1.17.0

--- Casks/h/heptabase.rb
+++ Casks/h/heptabase.rb
@@ -1,9 +1,9 @@
 cask "heptabase" do
   arch arm: "-arm64"
 
-  version "1.16.2"
-  sha256 arm:   "9676167c38875d033d28a584f710aba2180672c70b241e1a185dc4a08baa5f9d",
-         intel: "fb6a9ddf524e171ecc8e2e379d3359685a1b52a6e5c983029ce35ed8cc458777"
+  version "1.17.0"
+  sha256 arm:   "48d86551f26221f5d1275f72d7f047aaf616a1a91e0facb6fa6740c1bd01d018",
+         intel: "2c4b663e3ca2c4a2cc14120a831ca7727e13c4ab97fc039b65a7de60e1c0c9ee"
 
   url "https://github.com/heptameta/project-meta/releases/download/v#{version}/Heptabase-#{version}#{arch}-mac.zip",
       verified: "github.com/heptameta/project-meta/"
