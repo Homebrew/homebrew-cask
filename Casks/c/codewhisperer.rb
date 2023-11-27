f19c6c9370453f88aa246dbef116f45c0f1d946a
Bevan Kay <email@bevankay.me>
2023-11-28 10:31:39 +1100

    codewhisperer: add livecheck, version url, add shasum

--- Casks/c/codewhisperer.rb
+++ Casks/c/codewhisperer.rb
@@ -1,11 +1,19 @@
 cask "codewhisperer" do
   version "0.12.0"
-  sha256 :no_check
+  sha256 "705ecf656209689184c4d66072cc147b965838bb3f57c3b10c8b5c442b1421b3"
 
-  url "https://desktop-release.codewhisperer.us-east-1.amazonaws.com/latest/CodeWhisperer.dmg", verified: "desktop-release.codewhisperer.us-east-1.amazonaws.com/"
+  url "https://desktop-release.codewhisperer.us-east-1.amazonaws.com/#{version}/CodeWhisperer.dmg",
+      verified: "desktop-release.codewhisperer.us-east-1.amazonaws.com/"
   name "CodeWhisperer for Command Line"
   desc "IDE-style autocomplete for 500+ CLIs. Translate natural language-to-bash"
   homepage "https://docs.aws.amazon.com/codewhisperer/latest/userguide/command-line-getting-started-installing.html"
+
+  livecheck do
+    url "https://desktop-release.codewhisperer.us-east-1.amazonaws.com/index.json"
+    strategy :json do |json|
+      json["versions"].map { |item| item["version"] }
+    end
+  end
 
   auto_updates true
   depends_on macos: ">= :high_sierra"
