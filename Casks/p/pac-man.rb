08f75cea3f7e05da50c7be725317c7c6a12306cb
langdon <getpacman.gq@gmail.com>
2023-11-27 13:10:43 -0500

    Fixed some minor formatting issues in pac-man.rb

--- Casks/p/pac-man.rb
+++ Casks/p/pac-man.rb
@@ -1,20 +1,17 @@
 cask "pac-man" do
+  
   version "10.31"
+
   sha256 :no_check
+
   url "http://www2.getpacman.gq/files/Pac-Man.zip"
 
   name "Pac-Man"
-  desc "A Java-Based Pac-Man Game" 
-  homepage "https://www.getpacman.gq"
+
+  desc "Java-Based Pac-Man Game"
+
+  homepage "https://www.getpacman.gq/"
 
   app "Pac-Man.app"
+
 end
-
-
-
-=begin
-
-
-,
-      verified: "http://www2.getpacman.gq"
-=end
