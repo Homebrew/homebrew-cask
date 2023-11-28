4f769d94dde80d3546c0d96193813ad675246cd4
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-11-28 11:09:18 -0500

    pyzo 4.14.2

--- Casks/p/pyzo.rb
+++ Casks/p/pyzo.rb
@@ -1,6 +1,6 @@
 cask "pyzo" do
-  version "4.13.3"
-  sha256 "70f4678e181b78d2bc5a8d9a2ec7046cc8b0548f6899a140aac0f1748280b455"
+  version "4.14.2"
+  sha256 "9577c6ceff764c3d3822a950cbce6eda2b38898d6daf0d78f9281212f40e7c9f"
 
   url "https://github.com/pyzo/pyzo/releases/download/v#{version}/pyzo-#{version}-macos_x86_64.dmg",
       verified: "github.com/pyzo/pyzo/"
