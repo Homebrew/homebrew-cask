bfce5ab3e73237b78869f1deccc424a3b884eda5
Rui Chen <rui@chenrui.dev>
2023-11-29 12:26:36 -0500

    milanote 3.4.34

--- Casks/m/milanote.rb
+++ Casks/m/milanote.rb
@@ -1,6 +1,6 @@
 cask "milanote" do
-  version "3.4.33"
-  sha256 "f1c111825d4592e9afe57af47918a08f357d75fd52eef0c269b3f48974e8ade7"
+  version "3.4.34"
+  sha256 "9828ad6810b25183e1bd65985bb55c1d86ee03c681694425107ca576e377978a"
 
   url "https://milanote-app-releases.s3.amazonaws.com/Milanote-#{version}.dmg",
       verified: "milanote-app-releases.s3.amazonaws.com/"
