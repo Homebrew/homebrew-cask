d129ce49601607bd8da1e032def808a134f7878e
Philipp Jetschina <kontakt@jetschina.de>
2023-11-30 15:54:27 +0100

    add wiso-steuer-2024

---
+++ Casks/w/wiso-steuer-2024.rb
@@ -0,0 +1,25 @@
+cask "wiso-steuer-2024" do
+  version "31.01.3340"
+  sha256 :no_check
+
+  url "https://download.buhl.de/ESD/Steuer/2024/WISOSteuer2024.dmg"
+  name "WISO Steuer 2024"
+  desc "Tax declaration for the fiscal year 2023"
+  homepage "https://www.buhl.de/download/wiso-steuer-2024/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
+
+  app "WISO Steuer 2024.app"
+
+  zap trash: [
+    "~/Library/Application Support/BuhlData.com/WISOsteuerMac2024",
+    "~/Library/Caches/com.BuhlData.WISOsteuerMac2024",
+    "~/Library/HTTPStorages/com.BuhlData.WISOsteuerMac2024",
+    "~/Library/Preferences/com.buhldata.WISOsteuerMac2024.plist",
+    "~/Library/Saved Application State/com.BuhlData.WISOsteuerMac2024.savedState",
+    "~/Library/WebKit/com.BuhlData.WISOsteuerMac2024",
+  ]
+end
