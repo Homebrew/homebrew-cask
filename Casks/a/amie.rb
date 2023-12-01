05fc47080ce4f7a9956f1f667eca86a04ae6abcb
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-12-01 11:09:15 -0500

    amie 231130.0.1

--- Casks/a/amie.rb
+++ Casks/a/amie.rb
@@ -1,9 +1,9 @@
 cask "amie" do
   arch arm: "-arm64"
 
-  version "231115.0.0"
-  sha256 arm:   "62fb2f9c30718b2299b0eff7432c1bfc2747e7c9c2dbf278f7a5266d4e0a954b",
-         intel: "b1a5be6eb5633a008a657bf1016e77c12a35f090a7b37bf1142dde657555927a"
+  version "231130.0.1"
+  sha256 arm:   "e699ff6e3d7e0a3354bd7f09c4cdb7e4f231a73a232ed7ca98f96ca8f0671c31",
+         intel: "87ad3af2bdacc685ed8c6594360f704c8a0a9d5cb8a5fd9295faa3dd7ed26c68"
 
   url "https://github.com/amieso/electron-releases/releases/download/v#{version}/Amie-#{version}#{arch}-mac.zip",
       verified: "github.com/amieso/electron-releases/"
