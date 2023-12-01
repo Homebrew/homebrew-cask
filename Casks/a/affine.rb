d2ad76b7615b04ff31b757fba448bdab788952ed
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-12-01 11:07:08 -0500

    affine 0.10.3

--- Casks/a/affine.rb
+++ Casks/a/affine.rb
@@ -1,9 +1,9 @@
 cask "affine" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.10.2"
-  sha256 arm:   "d693fdbc8100970db667359956f92e1bd03f40855fea80ea88783f04f8336470",
-         intel: "e46a65f7d59222dc858fe76d03e972ed382e98ee64dcdc01f83b789d76684cb1"
+  version "0.10.3"
+  sha256 arm:   "2c117b0cbc43b5fc972824fdcc1b8940cc458ed99a87df5fa5563df01010224c",
+         intel: "29d6efea39150648595850f6a91740f862049e7938e44c80c02767435a9e7093"
 
   url "https://github.com/toeverything/AFFiNE/releases/download/v#{version}/affine-stable-macos-#{arch}.zip",
       verified: "github.com/toeverything/AFFiNE/"
