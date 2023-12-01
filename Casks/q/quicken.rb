ab6a8f472ed901ce381e2e5138eb4eb2253953ea
Mikl Wolfe <wolfemikl@gmail.com>
2023-11-30 20:30:55 -0700

    quicken 7.4.0

--- Casks/q/quicken.rb
+++ Casks/q/quicken.rb
@@ -1,6 +1,6 @@
 cask "quicken" do
-  version "7.3.2,703.50456.100"
-  sha256 "a11ff716c3e911ca54258cf339247774175e03c68bd2bee054c945b488f2b6d8"
+  version "7.4.0,704.51545.100"
+  sha256 "56e57be5a3cc5b1840e66c340b535ad0700ba7b686eb6710bc1e2649cf4f34bc"
 
   url "https://download.quicken.com/mac/Quicken/001/Release/031A96D9-EFE6-4520-8B6A-7F465DDAA3E4/Quicken-#{version.csv.second}/Quicken-#{version.csv.second}.zip"
   name "Quicken"
