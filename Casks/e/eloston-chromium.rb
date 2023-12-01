22bf526764df41df80a93b89da7c76a420e28fe8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-01 12:46:55 +0000

    eloston-chromium 118.0.5993.117-1.1,1698927545

--- Casks/e/eloston-chromium.rb
+++ Casks/e/eloston-chromium.rb
@@ -6,8 +6,8 @@ cask "eloston-chromium" do
     sha256 "defbdb16ece48263ffba286d9337c2b1db96aa788129f37d469a0cd1e3b156c3"
   end
   on_intel do
-    version "118.0.5993.117-1.1,1698883432"
-    sha256 "8913b03fa10e8724ca1f8536d7570cbbfd743f2709930ec58d49d6f25d115d68"
+    version "119.0.6045.159-1.1,1701417454"
+    sha256 "273e36f8f0b86f9c2f59e1c34541f385848678a44a6a162c30148aa848c65dd1"
   end
 
   url "https://github.com/ungoogled-software/ungoogled-chromium-macos/releases/download/#{version.csv.first}_#{arch}__#{version.csv.second}/ungoogled-chromium_#{version.csv.first}_#{arch}-macos.dmg",
