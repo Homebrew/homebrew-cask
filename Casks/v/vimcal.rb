5a187c26b07c0103540c18cf573fe479278a8020
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-12-02 13:12:20 -0500

    vimcal: remove empty `arch` and comment

--- Casks/v/vimcal.rb
+++ Casks/v/vimcal.rb
@@ -1,12 +1,11 @@
 cask "vimcal" do
-  arch arm: "-arm64", intel: ""
+  arch arm: "-arm64"
   host_suffix = on_arch_conditional arm: "m1", intel: "production"
 
   version "1.0.25"
   sha256 arm:   "6cbc226dcd82089162c4032af83dd2764619254a9df90eab5212ad5f477ac16d",
          intel: "a07763ff4c6d3e1f2aa91674c425baf234d927402488df77030d30f4e6417642"
 
-  # linked from sidebar of https://calendar.vimcal.com
   url "https://vimcal-#{host_suffix}.s3.us-west-1.amazonaws.com/Vimcal-#{version}#{arch}.dmg",
       verified: "vimcal-#{host_suffix}.s3.us-west-1.amazonaws.com/"
   name "Vimcal"
