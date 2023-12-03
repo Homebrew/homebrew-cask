b359d00ad42ade41257fb953dcb03aba8acca8a9
Bevan Kay <email@bevankay.me>
2023-12-04 09:32:01 +1100

    Update cardo-update.rb

--- Casks/c/cardo-update.rb
+++ Casks/c/cardo-update.rb
@@ -1,8 +1,8 @@
 cask "cardo-update" do
   version "4.2.0"
-  sha256 :no_check
+  sha256 "4e8698968179412beae7caeb08b5c70190ba912e994d3ce42b5e1c18d67cbca3"
 
-  url "https://update.cardosystems.com/cardo-app/cardo_updater_macOS_latest.pkg"
+  url "https://update.cardosystems.com/cardo-app/Cardo%20Update-#{version}-mac.zip"
   name "Cardo Update"
   desc "Update Packtalk and Freecom motorcycle intercoms"
   homepage "https://www.cardosystems.com/download-cardo-updater/"
@@ -12,13 +12,12 @@ cask "cardo-update" do
     strategy :electron_builder
   end
 
-  pkg "cardo_updater_macOS_latest.pkg"
-
-  uninstall quit:    "com.cardo.fwupdater",
-            pkgutil: "com.cardo.fwupdater"
+  app "Cardo Update.app"
 
   zap trash: [
     "~/Library/Application Support/Cardo Update",
+    "~/Library/Logs/Cardo Update",
+    "~/Library/Preferences/com.cardo.fwupdater.plist",
   ]
 
   caveats do
