d1dbb56df9d8fec1e17940c7c68078dc7a80aab4
Mikl Wolfe <wolfemikl@gmail.com>
2023-12-03 08:12:57 -0700

    dcp-o-matic-disk-writer 2.16.70

--- Casks/d/dcp-o-matic-disk-writer.rb
+++ Casks/d/dcp-o-matic-disk-writer.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-disk-writer" do
-  version "2.16.69"
-  sha256 "fa313f4d7f708a23834a76da205fe541e4d43a1807b5614b287112ec39be2a0a"
+  version "2.16.70"
+  sha256 "28a6946ca4a31ec11be883c5dd3e62602e419353edb27f78f108f7f1461be2f7"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-disk&version=#{version}"
   name "DCP-o-matic Disk Writer"
