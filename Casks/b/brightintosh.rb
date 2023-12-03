1edce62381ba8b34ae8d76c765933ce9c0578435
Niklas Rousset <niklas.rousset@gmail.com>
2023-12-03 14:49:10 +0100

    Add BrightIntosh.app v1.4.2

---
+++ Casks/b/brightintosh.rb
@@ -0,0 +1,19 @@
+cask "brightintosh" do
+  version "1.4.2"
+  sha256 "9f3a18c3ef27078f69d094d89b815d2f0a039f5901db3fa98108c0720d67c813"
+
+  url "https://github.com/niklasr22/BrightIntosh/releases/download/v#{version}/BrightIntosh_v#{version}.dmg",
+      verified: "github.com/niklasr22/BrightIntosh/"
+  name "BrightIntosh"
+  desc "This tool enables the full brightness of your MacBook Pro M1 or newer"
+  homepage "https://www.brightintosh.de/"
+
+  livecheck do
+    url "https://c.brightintosh.de/updates/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  depends_on macos: "> :monterey"
+
+  app "BrightIntosh.app"
+end
