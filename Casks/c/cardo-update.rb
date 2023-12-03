ded76c32b47748e75ab4d9656e92196e94feacde
altf4arnold <arnold.dech@adct.be>
2023-12-03 02:40:59 +0100

    fixing CI-CD

--- Casks/c/cardo-update.rb
+++ Casks/c/cardo-update.rb
@@ -4,6 +4,7 @@ cask "cardo-update" do
 
   url "https://update.cardosystems.com/cardo-app/cardo_updater_macOS_latest.pkg"
   name "Cardo Update - firmware update tool for PACKTALK and FREECOM unit lines"
+  desc "Software to update Packtalk and Freecom motorcycle intercom's"
   homepage "https://www.cardosystems.com/download-cardo-updater/"
 
   pkg "cardo_updater_macOS_latest.pkg"
