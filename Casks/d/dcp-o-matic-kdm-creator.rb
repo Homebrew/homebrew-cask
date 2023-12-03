967e9f0be9fe12b9c39f9e081cdee0167ca3fd31
Mikl Wolfe <wolfemikl@gmail.com>
2023-12-03 08:15:46 -0700

    dcp-o-matic-kdm-creator 2.16.70

--- Casks/d/dcp-o-matic-kdm-creator.rb
+++ Casks/d/dcp-o-matic-kdm-creator.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-kdm-creator" do
-  version "2.16.69"
-  sha256 "f49a5c863f119f73ce21f37da5024d03d4fce7fc01349af35a25bbd721a2f5ce"
+  version "2.16.70"
+  sha256 "beeccf76a49b5c17fbcdee5a5c72894cbb40b71c144a2a05e7166a918609ac6e"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-kdm&version=#{version}"
   name "DCP-o-matic KDM Creator"
