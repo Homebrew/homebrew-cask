f7fc797e0ab0d3714e5858ca227ef6a84fd736f8
altf4arnold <arnold.dech@adct.be>
2023-12-03 02:26:57 +0100

    cardo-repair

--- Casks/c/cardo-update.rb
+++ Casks/c/cardo-update.rb
@@ -1,6 +1,6 @@
 cask 'cardo-update' do
   version :latest
-  sha256 : "261dcca0337d5a97230290267f8285a9a7b02b87f59ab7336c3b1a04fd6f1a1f"
+  sha256 "261dcca0337d5a97230290267f8285a9a7b02b87f59ab7336c3b1a04fd6f1a1f"
 
   url 'https://update.cardosystems.com/cardo-app/cardo_updater_macOS_latest.pkg'
   name 'Cardo Update - firmware update tool for PACKTALK and FREECOM unit lines'
