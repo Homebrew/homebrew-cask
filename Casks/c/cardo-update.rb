9eb771517c802123322ee79e1e7f42b83b5526fc
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-12-02 22:06:49 -0500

    cardo-update: fixes
    * Update name and desc
    
    * Fix `version`
    
    * Add `livecheck`
    
    * Add `requires_rosetta`

--- Casks/c/cardo-update.rb
+++ Casks/c/cardo-update.rb
@@ -1,18 +1,27 @@
 cask "cardo-update" do
-  version :latest
+  version "4.2.0.30204"
   sha256 :no_check
 
   url "https://update.cardosystems.com/cardo-app/cardo_updater_macOS_latest.pkg"
-  name "Cardo Update - firmware update tool for PACKTALK and FREECOM unit lines"
-  desc "Software to update Packtalk and Freecom motorcycle intercom's"
+  name "Firmware update tool for PACKTALK and FREECOM unit lines"
+  desc "Update Packtalk and Freecom motorcycle intercoms"
   homepage "https://www.cardosystems.com/download-cardo-updater/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   pkg "cardo_updater_macOS_latest.pkg"
 
   uninstall quit:    "com.cardo.fwupdater",
             pkgutil: "com.cardo.fwupdater"
 
-  zap rmdir: [
+  zap trash: [
     "~/Library/Application Support/Cardo Update",
   ]
+
+  caveats do
+    requires_rosetta
+  end
 end
