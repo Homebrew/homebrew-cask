cd46626cf1ef641b688a27cdc21899878ba4a5e9
Rui Chen <rui@chenrui.dev>
2023-12-04 12:34:08 -0500

    standard-notes 3.183.7

--- Casks/s/standard-notes.rb
+++ Casks/s/standard-notes.rb
@@ -1,9 +1,9 @@
 cask "standard-notes" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.181.32"
-  sha256 arm:   "a0b7cea234ccd23b05968f0815aaded69a793ede92e69d039cd682118a2f5121",
-         intel: "f993e5102a846182dd0629af6f7ab16a78091ac0d0b06ff1fa84bf0967fa3cd8"
+  version "3.183.7"
+  sha256 arm:   "32d674daa87f71d49fc983d440db9dd49dc6097f4bee25871f1aea6db989af60",
+         intel: "3d12baefe463352e5463e0765807a5bbf7381fb746ea4adfedcc489c982a3259"
 
   url "https://github.com/standardnotes/app/releases/download/%40standardnotes%2Fdesktop%40#{version}/standard-notes-#{version}-mac-#{arch}.zip",
       verified: "github.com/standardnotes/app/"
