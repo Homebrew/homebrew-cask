0f1c6122c481454415b5ab6d79feb96f3ff020fd
Miccal Matthews <miccal.matthews@gmail.com>
2023-12-04 12:51:27 +0800

    Update vimcal.rb

--- Casks/v/vimcal.rb
+++ Casks/v/vimcal.rb
@@ -23,9 +23,9 @@ cask "vimcal" do
   app "Vimcal.app"
 
   zap trash: [
-    "~/Library/Application Support/Vimcal",
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/" \
     "com.vimcal.app.sfl*",
+    "~/Library/Application Support/Vimcal",
     "~/Library/Preferences/com.vimcal.app.plist",
     "~/Library/Saved Application State/com.vimcal.app.savedState",
   ]
