ca6df176a39661b9622b456117180618e96c5641
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-12-04 14:58:39 -0500

    nx-studio: update `url` and `livecheck`

--- Casks/n/nx-studio.rb
+++ Casks/n/nx-studio.rb
@@ -2,15 +2,15 @@ cask "nx-studio" do
   version "1.5.0"
   sha256 "803e231464f589dcdcd958a57dd288f8824a06735e45b7e1abc18945d2e56890"
 
-  url "https://download.nikonimglib.com/archive6/sRoLC00mMTZQ06g0yqe26VXd2O80/S-NXSTDO-#{format("%<major>02d%<minor>02d%<patch>02d", major: version.major, minor: version.minor, patch: version.patch)}MF-ALLIN-ALL___.dmg",
+  url "https://download.nikonimglib.com/archive6/sRoLC00mMTZQ06g0yqe26VXd2O80/S-NXSTDO-0#{version.major}0#{version.minor}0#{version.patch}MF-ALLIN-ALL___.dmg",
       verified: "download.nikonimglib.com/"
   name "NX Studio"
   desc "Single software comprehensively covers image processing for Nikon"
   homepage "https://imaging.nikon.com/imaging/lineup/software/nx_studio/"
 
   livecheck do
-    url "https://downloadcenter.nikonimglib.com/en/products/564/NX_Studio.html"
-    regex(/Ver. (\d+\.\d+\.\d+)/i)
+    url "https://downloadcenter.nikonimglib.com/en/download/sw/247.html"
+    regex(/NX\s+Studio\s+Version\s+(\d+(?:\.\d+)+)/i)
   end
 
   auto_updates true
