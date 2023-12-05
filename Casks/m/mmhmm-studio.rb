c96443709a9328d03d901f1c7cebfbab09433b87
Rui Chen <rui@chenrui.dev>
2023-12-05 02:38:59 -0500

    mmhmm-studio 2.6.2

--- Casks/m/mmhmm-studio.rb
+++ Casks/m/mmhmm-studio.rb
@@ -10,8 +10,8 @@ cask "mmhmm-studio" do
     depends_on macos: ">= :big_sur"
   end
   on_ventura :or_newer do
-    version "2.6.1,1698814000"
-    sha256 "8faa3aff843170d94b10e7cead4e5404bcdf36e20fcf287baa80d24aafcb14a2"
+    version "2.6.2,1701716000"
+    sha256 "04de056a9ab2e7266110136f31c59514339b377850ea4914f3dbdc7aae896b1e"
 
     # This appcast sometimes uses a newer pubDate for an older version, so we
     # have to ignore the default `Sparkle` strategy sorting (which involves the
