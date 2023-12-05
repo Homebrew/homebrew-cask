ce3351ded4f79d370ffd6330784810d7ed1fe263
Patrick Linnane <patrick@linnane.io>
2023-12-05 11:52:39 -0800

    discord 0.0.286

--- Casks/d/discord.rb
+++ Casks/d/discord.rb
@@ -1,6 +1,6 @@
 cask "discord" do
-  version "0.0.285"
-  sha256 "f478117363c54fab8c82b17c02c4283ec631efaf144454bb2ea3a393805698e9"
+  version "0.0.286"
+  sha256 "4280dfba16870c5f5cc27824b343a3c5f21d9514b9165ce370321271e5ee175d"
 
   url "https://dl.discordapp.net/apps/osx/#{version}/Discord.dmg",
       verified: "dl.discordapp.net/"
