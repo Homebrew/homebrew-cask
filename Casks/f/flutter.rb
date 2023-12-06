34f6a731010c293822de8d3dde32ebaf30bdc5f8
Rui Chen <rui@chenrui.dev>
2023-12-06 13:22:08 -0500

    flutter 3.16.3

--- Casks/f/flutter.rb
+++ Casks/f/flutter.rb
@@ -1,9 +1,9 @@
 cask "flutter" do
   arch arm: "_arm64"
 
-  version "3.16.2"
-  sha256 arm:   "5c58fac2bf236ced8ade193b5883a431a861663949e9d5d1a4bf1d93b1f2076f",
-         intel: "99e3e8f80d03be212bb991b03c37dc598dffbf190784166ce9d27cb12ac7d06b"
+  version "3.16.3"
+  sha256 arm:   "ca0a61fc594adff123521cc72e84bfdec0dfdb58c49f3e7e0f82c3969da77446",
+         intel: "0230d67d13817b65e2006bcd3330d72e4161f5c10b558afd1c72a818cd7c578e"
 
   url "https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos#{arch}_#{version}-stable.zip",
       verified: "storage.googleapis.com/flutter_infra_release/releases/stable/macos/"
