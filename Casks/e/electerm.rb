b7e749c265f9b9b1db8d17342ca645cadbf31ad1
Mikl Wolfe <wolfemikl@gmail.com>
2023-12-06 08:13:17 -0700

    electerm 1.37.36

--- Casks/e/electerm.rb
+++ Casks/e/electerm.rb
@@ -1,9 +1,9 @@
 cask "electerm" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.37.20"
-  sha256 arm:   "bcb6f589e60743f533bda2a3934933842b83f26d347a4b1e8462a026e112b5f0",
-         intel: "b9a393ddfadd9ea98e0ae914d9188853ddd554c00fa0db595afde1320c5a40cc"
+  version "1.37.36"
+  sha256 arm:   "511d81b0fb1e8d63f182e375235b81d61832b2c5ea691f1add00f2f5a13cb2a9",
+         intel: "7acd751b7ed2f160314b1d9b3babd589dcdf243924e40dfb2ea01e36df237354"
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac-#{arch}.dmg"
   name "electerm"
