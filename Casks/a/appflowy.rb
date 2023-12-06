06eebd3c88e5e1c56b4b5a0cd7058adb562d86ba
Bevan Kay <email@bevankay.me>
2023-12-06 23:32:18 +1100

    appflowy 0.3.9

--- Casks/a/appflowy.rb
+++ Casks/a/appflowy.rb
@@ -1,6 +1,6 @@
 cask "appflowy" do
-  version "0.3.8"
-  sha256 "1e6d4aef22eb9fa286018f78a887ea8ed68ce53f8bb6970a35ce2ddfeeae5e0b"
+  version "0.3.9"
+  sha256 "f32404fc4476f63d136184feb74af701bdd9f71ffaa3c9841fa9d622841796e3"
 
   url "https://github.com/AppFlowy-IO/AppFlowy/releases/download/#{version}/Appflowy-#{version}-macos-universal.zip",
       verified: "github.com/AppFlowy-IO/AppFlowy/"
