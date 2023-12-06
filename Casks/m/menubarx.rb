457bcf4cc4921ec730525fa35797a33b0714ac17
Rui Chen <rui@chenrui.dev>
2023-12-06 13:16:36 -0500

    menubarx 1.6.7

--- Casks/m/menubarx.rb
+++ Casks/m/menubarx.rb
@@ -1,6 +1,6 @@
 cask "menubarx" do
-  version "1.6.6"
-  sha256 "3f4ea3e01363232aa5c34b2b61b4ff0be6fafbebf718adde9a8817d92e04f482"
+  version "1.6.7"
+  sha256 "475b993ab6d24ce1785735c55f86ef4c1de3c4bc30817c9bf29d2da8086e483c"
 
   url "https://menubarx-1251679148.file.myqcloud.com/download/MenubarX-#{version}.dmg",
       verified: "menubarx-1251679148.file.myqcloud.com/download/"
