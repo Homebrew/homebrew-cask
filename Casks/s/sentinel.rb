a3ee20d2ac2346d65c521b089d3af91ff7bb81bd
Mikl Wolfe <wolfemikl@gmail.com>
2023-12-07 07:59:01 -0700

    sentinel 0.24.0

--- Casks/s/sentinel.rb
+++ Casks/s/sentinel.rb
@@ -1,9 +1,9 @@
 cask "sentinel" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.23.1"
-  sha256 arm:   "44ad8edc19ff7fb4f09b073e8cb1790cf2ac0a76e21c33c88edc48f15da21ca6",
-         intel: "3b02b0ac37bdb0f135e08454f42a796f36b5ead9044abffd305c497c8ea4b396"
+  version "0.24.0"
+  sha256 arm:   "d79a1823c34e7867b49ccf4a224ec24dff21185466ebb3afa5636c98cc6cffc9",
+         intel: "108a9d0cb6531e8beac4a37f98c72a1248d491f0eb5ee09b9a1bf887dd9d871d"
 
   url "https://releases.hashicorp.com/sentinel/#{version}/sentinel_#{version}_darwin_#{arch}.zip"
   name "Sentinel"
