8082b23d187e2c698a5f15526dcf50ed3764860f
Mikl Wolfe <wolfemikl@gmail.com>
2023-12-06 17:57:21 -0700

    eclipse-dsl 4.30.0

--- Casks/e/eclipse-dsl.rb
+++ Casks/e/eclipse-dsl.rb
@@ -1,9 +1,9 @@
 cask "eclipse-dsl" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "4.29.0,2023-09"
-  sha256 arm:   "7e036cd496966d8b5176272df95a835e7b228fe5e9796df0ce4f33c5144daa69",
-         intel: "1d6b5f8bc7682ce66624d15d53008731fdc8b58651294b57379b61cb3b05b14f"
+  version "4.30.0,2023-12"
+  sha256 arm:   "e6cf131bd053fb4db8953d2dac50292742410b59345fb7f5085749751ea7c6cf",
+         intel: "7ac79e5ce76209dd7575003eeb159dcaa0f497b75818d1da12d7fbec02358b47"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-dsl-#{version.csv.second}-R-macosx-cocoa-#{arch}.dmg&r=1"
   name "Eclipse IDE for Java and DSL Developers"
