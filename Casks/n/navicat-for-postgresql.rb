5d7d07d0e1f87d830d40c06a965e4dfd3b67c6c8
Rui Chen <rui@chenrui.dev>
2023-12-06 23:48:15 -0500

    navicat-for-postgresql 16.3.3

--- Casks/n/navicat-for-postgresql.rb
+++ Casks/n/navicat-for-postgresql.rb
@@ -1,5 +1,5 @@
 cask "navicat-for-postgresql" do
-  version "16.3.2"
+  version "16.3.3"
   sha256 :no_check
 
   url "https://download.navicat.com/download/navicat#{version.major_minor.no_dots}_pgsql_en.dmg"
