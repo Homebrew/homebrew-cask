83f12c8cbd0dd1e7cc4f25edc448e535bd6e952a
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-12-07 22:32:29 -0500

    fedora-media-writer: fix broken checksum

--- Casks/f/fedora-media-writer.rb
+++ Casks/f/fedora-media-writer.rb
@@ -2,7 +2,7 @@ cask "fedora-media-writer" do
   arch arm: "-arm64"
 
   version "5.0.9"
-  sha256 arm:   "aec43572e16furl7dc49bd48bd319f25b644ea475b95389322ab6b6cd56044b6fde",
+  sha256 arm:   "aec43572e16f7dc49bd48bd319f25b644ea475b95389322ab6b6cd56044b6fde",
          intel: "888d23f9265fc39525ae4ba5626ecd540bbd0a2984ca3ebaae496512269fb33c"
 
   url "https://github.com/FedoraQt/MediaWriter/releases/download/#{version}/FedoraMediaWriter-osx#{arch}-#{version}.dmg",
