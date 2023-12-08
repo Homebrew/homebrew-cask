b3b1882366299e22b1795c031e0c570fe8f9ff8e
Bevan Kay <email@bevankay.me>
2023-12-09 08:59:48 +1100

    truhu: update livecheck

--- Casks/t/truhu.rb
+++ Casks/t/truhu.rb
@@ -4,46 +4,20 @@ cask "truhu" do
 
   url "https://truhu-mobile-builds.s3.us-west-2.amazonaws.com/TruHu+Desktop+#{version}.dmg"
   name "TruHu"
-  desc "Uses a Desktop and Mobile app together to measure your display’s colors and proceed with calibration"
+  desc "Display calibration utility"
   homepage "https://truhu.app/"
 
-  # NOTE: This method isn't stable since the build hash will change
-  # livecheck do
-  #   url "https://truhu.app/static/js/main.9202b0c2.js"
-  #   regex(/\{no:(\d+),date:"[^"]*"/i)
-  # end
-
-  # NOTE: This gets an error while trying to run it:
-  #   Error: truhu: private method `open' called for URI:Module
-  # Is there a better way to check/use content from 2 different URLs in a livecheck?
   livecheck do
     url "https://truhu.app/downloads/index.html"
-    regex(%r{/static/js/main\.[a-f0-9]+\.js})
-
+    regex(/\{no:(\d+)\s*,date/i)
     strategy :page_match do |page, regex|
-      # Find the script URL with the hash
-      script_path = page.match(regex)
-      next if script_path.blank?
+      js_file = page[%r{src=["']?/(static/js/main\.[a-f0-9]+\.js)}, 1]
+      next if js_file.blank?
 
-      # Form the complete URL for the script
-      script_full_url = "https://truhu.app#{script_path}"
-
-      # Fetch the script content
-      script_content = URI.open(script_full_url).read
-
-      # Extract version and date
-      script_content.scan(/\{no:(\d+),date:"([^"]+)"/i).map do |match|
-        version, date = match
-        # You can format the version and date as needed here
-        "#{version},#{date}"
-      end
+      version_page = Homebrew::Livecheck::Strategy.page_content("https://truhu.app/#{js_file}")
+      version_page[:content].scan(regex).flatten
     end
   end
-
-  # NOTE: This is a last resort
-  # livecheck do
-  #   skip "No version information available"
-  # end
 
   app "TruHu Mac Desktop.app"
 
