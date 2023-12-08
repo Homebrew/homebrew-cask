2c77615ce273b9753a95c640013850411e43b5c3
Bevan Kay <email@bevankay.me>
2023-12-09 09:41:20 +1100

    mqttx 1.9.7

--- Casks/m/mqttx.rb
+++ Casks/m/mqttx.rb
@@ -1,11 +1,11 @@
 cask "mqttx" do
-  arch arm: "arm64-"
+  arch arm: "-arm64"
 
-  version "1.9.6"
-  sha256 arm:   "b2b7723acb100c8ac51829aea7c498486bfc78cd6c4284f5280c82947a734436",
-         intel: "b08d7081e1efadec3bd7d0dc63d1cd8e2e0f06bafc9e5ec1bf210d7de555d71b"
+  version "1.9.7"
+  sha256 arm:   "f9136f189ba6e78e64ded7b9ef8a59e462f99b9d0f81df3d53e592a5f9c1e4bb",
+         intel: "31e6ba10e181c531d32545fe6b0409a234d7a86eb24e8e5192a183e69885494d"
 
-  url "https://github.com/emqx/MQTTX/releases/download/v#{version}/MQTTX-#{version}-#{arch}mac.zip",
+  url "https://github.com/emqx/MQTTX/releases/download/v#{version}/MQTTX-#{version}#{arch}.dmg",
       verified: "github.com/emqx/MQTTX/"
   name "MQTTX"
   desc "Cross-platform MQTT 5.0 Desktop Client"
