08b43ebe1850fc99dbaaf19cc2ccf17bbb966f40
Mikl Wolfe <wolfemikl@gmail.com>
2023-12-09 14:23:21 -0700

    metashape 2.0.4

--- Casks/m/metashape.rb
+++ Casks/m/metashape.rb
@@ -1,6 +1,6 @@
 cask "metashape" do
-  version "2.0.3"
-  sha256 "90dc0315a90b82e9501cd008796aec1f5d5b8f26cb60a5e84440d1fe6c342817"
+  version "2.0.4"
+  sha256 "37a3082fcc3f04121bb8736c8f82116d319a6de0d23aa6dae5e401b4aa8b0864"
 
   url "http://download.agisoft.com/metashape_#{version.dots_to_underscores}.dmg"
   name "Agisoft Metashape Standard Edition"
