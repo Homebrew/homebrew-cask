ce84b91746e20cf3c6a28dd213b0e42d7242aef8
Miccal Matthews <miccal.matthews@gmail.com>
2023-12-09 13:19:28 +0800

    Update marsedit.rb

--- Casks/m/marsedit.rb
+++ Casks/m/marsedit.rb
@@ -18,7 +18,7 @@ cask "marsedit" do
 
   zap trash: [
     "~/Library/Application Scripts/com.red-sweater.*",
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.red-sweater.marsedit5.sfl2",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.red-sweater.marsedit5.sfl*",
     "~/Library/Caches/com.apple.helpd/Generated/com.red-sweater.marsedit5.help*",
     "~/Library/Containers/com.red-sweater.marsedit5.*",
     "~/Library/Group Containers/493CVA9A35.com.red-sweater",
