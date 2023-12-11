54301320cc50e89abe1cce60520af0c51f5384f8
Rui Chen <rui@chenrui.dev>
2023-12-11 08:16:01 -0500

    geotag-photos-pro 1.9.7

--- Casks/g/geotag-photos-pro.rb
+++ Casks/g/geotag-photos-pro.rb
@@ -1,6 +1,6 @@
 cask "geotag-photos-pro" do
-  version "1.9.5"
-  sha256 "441ec244a567238abbf582026cbbed6932683bacf4239f01df5fcd239f6e5c6a"
+  version "1.9.7"
+  sha256 "4cf4414fad5c9c472f8c334b543868d8d6b9d0a3a90736285dd2ec0be3ee3238"
 
   url "https://github.com/tappytaps/geotag-desktop-app/releases/download/v#{version}/Geotag-Photos-Pro-2-#{version}.dmg",
       verified: "github.com/tappytaps/geotag-desktop-app/"
