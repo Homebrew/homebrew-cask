5d2e0d4986c9513eceb7ca49019a2f1b707be6aa
Bevan Kay <email@bevankay.me>
2023-12-11 14:06:07 +1100

    klayout 0.28.13

--- Casks/k/klayout.rb
+++ Casks/k/klayout.rb
@@ -1,10 +1,10 @@
 cask "klayout" do
-  version "0.28.12"
+  version "0.28.13"
 
   on_catalina :or_older do
     sha256 "b2bca1ad625f84be8d6eeba7cc4864c83dd09d01d4e413059c8cff130c825b3e"
 
-    url "https://www.klayout.org/downloads/MacOS/HW-klayout-#{version}-macOS-Catalina-1-qt5Brew-RsysPhb39.dmg",
+    url "https://www.klayout.org/downloads/MacOS/HW-klayout-#{version}-macOS-Catalina-1-qt5Brew-RsysPhb311.dmg",
         verified: "klayout.org/downloads/MacOS/"
 
     livecheck do
@@ -15,7 +15,7 @@ cask "klayout" do
   on_big_sur do
     sha256 "1c7b6e12ac722d718dfc1711dd4d05cfa0a7b97d60ad02f98cd15cbae22d7fb0"
 
-    url "https://www.klayout.org/downloads/MacOS/HW-klayout-#{version}-macOS-BigSur-1-qt5Brew-RsysPhb39.dmg",
+    url "https://www.klayout.org/downloads/MacOS/HW-klayout-#{version}-macOS-BigSur-1-qt5Brew-RsysPhb311.dmg",
         verified: "klayout.org/downloads/MacOS/"
 
     livecheck do
@@ -24,9 +24,9 @@ cask "klayout" do
     end
   end
   on_monterey do
-    sha256 "731abc251c398c70284bc75c1a197e1ac31e2ea2e5b5c9047d2a8c14754ea300"
+    sha256 "731abc251c3118c70284bc75c1a197e1ac31e2ea2e5b5c9047d2a8c14754ea300"
 
-    url "https://www.klayout.org/downloads/MacOS/HW-klayout-#{version}-macOS-Monterey-1-qt5Brew-RsysPhb39.dmg",
+    url "https://www.klayout.org/downloads/MacOS/HW-klayout-#{version}-macOS-Monterey-1-qt5Brew-RsysPhb311.dmg",
         verified: "klayout.org/downloads/MacOS/"
 
     livecheck do
@@ -34,15 +34,37 @@ cask "klayout" do
       regex(/href=.*?HW[._-]klayout[._-](\d+(?:\.\d+)+)[._-]macOS[._-]Monterey.*?\.dmg/i)
     end
   end
-  on_ventura :or_newer do
-    sha256 "aa408835d859276ecb10d40cada1639eda8d0f10c5833121e6688bb0c1a04608"
+  on_monterey do
+    sha256 "2dd03a4ab5e1a5f0c9f1f33e69be690a6e923e6d25c6c7083f808f53a46c3d7a"
 
-    url "https://www.klayout.org/downloads/MacOS/HW-klayout-#{version}-macOS-Ventura-1-qt5Brew-RsysPhb39.dmg",
+    url "https://www.klayout.org/downloads/MacOS/HW-klayout-#{version}-macOS-Ventura-1-qt5Brew-RsysPhb311.dmg",
+        verified: "klayout.org/downloads/MacOS/"
+
+    livecheck do
+      url "https://www.klayout.de/build.html"
+      regex(/href=.*?HW[._-]klayout[._-](\d+(?:\.\d+)+)[._-]macOS[._-]Monterey.*?\.dmg/i)
+    end
+  end
+  on_ventura do
+    sha256 "2dd03a4ab5e1a5f0c9f1f33e69be690a6e923e6d25c6c7083f808f53a46c3d7a"
+
+    url "https://www.klayout.org/downloads/MacOS/HW-klayout-#{version}-macOS-Ventura-1-qt5Brew-RsysPhb311.dmg",
         verified: "klayout.org/downloads/MacOS/"
 
     livecheck do
       url "https://www.klayout.de/build.html"
       regex(/href=.*?HW[._-]klayout[._-](\d+(?:\.\d+)+)[._-]macOS[._-]Ventura.*?\.dmg/i)
+    end
+  end
+  on_sonoma :or_newer do
+    sha256 "2dd03a4ab5e1a5f0c9f1f33e69be690a6e923e6d25c6c7083f808f53a46c3d7a"
+
+    url "https://www.klayout.org/downloads/MacOS/HW-klayout-#{version}-macOS-Ventura-1-qt5Brew-RsysPhb311.dmg",
+        verified: "klayout.org/downloads/MacOS/"
+
+    livecheck do
+      url "https://www.klayout.de/build.html"
+      regex(/href=.*?HW[._-]klayout[._-](\d+(?:\.\d+)+)[._-]macOS[._-]Sonoma.*?\.dmg/i)
     end
   end
 
