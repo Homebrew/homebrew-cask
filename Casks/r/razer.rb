be2dc7b7312da2903810e73ccf349db095a154b3
miccal <miccal.matthews@gmail.com>
2023-12-11 14:14:27 +0800

    1kc-razer: rename to razer

--- Casks/r/razer.rb
+++ Casks/r/razer.rb
@@ -1,16 +1,13 @@
-cask "1kc-razer" do
+cask "razer" do
   version "0.4.10"
   sha256 "9f57f928adb31ca843dbf2047a7d6ea308eceaccb50620792535d1fcd7ec73ca"
 
   url "https://github.com/1kc/razer-macos/releases/download/v#{version}/Razer.macOS-#{version}-universal.dmg"
   name "Razer macOS"
-  desc "Open source color effects manager for Razer devices"
-  homepage "https://github.com/1kc/razer-macos"
+  desc "Open source colour effects manager for Razer devices"
+  homepage "https://github.com/1kc/razer-macos/"
 
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
+  depends_on macos: ">= :high_sierra"
 
   app "Razer macOS.app"
 
