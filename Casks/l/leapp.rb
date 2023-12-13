3d164019c6619d28f723da009d7bc25d722e4742
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-12-13 10:07:10 -0500

    leapp 0.23.0

--- Casks/l/leapp.rb
+++ Casks/l/leapp.rb
@@ -1,9 +1,9 @@
 cask "leapp" do
   arch arm: "-arm64"
 
-  version "0.22.2"
-  sha256 arm:   "8b98d8a0fcfe238cb1273a016d9a98cc52ec1a2af11dc34f0b8db5e9ad836d8b",
-         intel: "ed9697179d7ce0f3337127296c4fd2488d1de653240cfa6b8c466d91f7ac86c5"
+  version "0.23.0"
+  sha256 arm:   "f39c2b5db16397df893b3e6ae021207a5a798a3e7e85779cb38544e19dd11c27",
+         intel: "bfaa3ec3f55454f36be199a7e2e263f120bc63464f2e0778d0dbaabd59a3412f"
 
   url "https://asset.noovolari.com/#{version}/Leapp-#{version}#{arch}.dmg",
       verified: "asset.noovolari.com/"
