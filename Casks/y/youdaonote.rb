69f3713bbd61f7f4a818547b27a8e9c9b5aefc60
Mikl Wolfe <wolfemikl@gmail.com>
2023-12-13 12:51:33 -0700

    youdaonote 7.2.160

--- Casks/y/youdaonote.rb
+++ Casks/y/youdaonote.rb
@@ -1,6 +1,6 @@
 cask "youdaonote" do
-  version "7.2.151"
-  sha256 "35bbac208294b12048abc003c74e44adf723441c6e129011672d6a5358dd1e19"
+  version "7.2.160"
+  sha256 "40b442570afa9d757b7fc0b62534a3624bdbc57a59418971d24e628cfc783e36"
 
   url "https://artifact.lx.netease.com/download/ynote-electron/%E6%9C%89%E9%81%93%E4%BA%91%E7%AC%94%E8%AE%B0-#{version}.zip",
       user_agent: :fake,
