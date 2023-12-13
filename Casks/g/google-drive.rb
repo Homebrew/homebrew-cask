382a2dbeec29bd4b246f76f833678cecf60df0e1
Bevan Kay <email@bevankay.me>
2023-12-14 10:39:34 +1100

    google-drive 85.0.23

--- Casks/g/google-drive.rb
+++ Casks/g/google-drive.rb
@@ -1,5 +1,5 @@
 cask "google-drive" do
-  version "85.0.22"
+  version "85.0.23"
   sha256 :no_check
 
   # "5-percent" is included in the url to ensure that `brew upgrade` does not update to an older version as the
