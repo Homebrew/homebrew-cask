e5d24666334f4c29d3c9a7327876c2a58a6a4435
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-12-13 09:56:08 -0500

    opensc 0.24.0

--- Casks/o/opensc.rb
+++ Casks/o/opensc.rb
@@ -1,6 +1,6 @@
 cask "opensc" do
-  version "0.23.0"
-  sha256 "9e81cf6dd93fa209c27b61714e9dfc7e63f6d79abf656a586e542ed01e0c3f07"
+  version "0.24.0"
+  sha256 "96eb53af88f91d25e6e127081bceb7687a8e53a5790914b5a47c6651783a0b44"
 
   url "https://github.com/OpenSC/OpenSC/releases/download/#{version}/OpenSC-#{version}.dmg"
   name "OpenSC"
