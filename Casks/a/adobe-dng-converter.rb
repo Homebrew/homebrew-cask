3415b41fab2c8f0a0330420a6cc133f464235212
Mikl Wolfe <wolfemikl@gmail.com>
2023-12-14 07:49:03 -0700

    adobe-dng-converter 16.1

--- Casks/a/adobe-dng-converter.rb
+++ Casks/a/adobe-dng-converter.rb
@@ -1,6 +1,6 @@
 cask "adobe-dng-converter" do
-  version "16.0.1"
-  sha256 "49fb3f6c5b771b33150abee9130272a7f54bf16f0b564f0c92288ece8c4b49bf"
+  version "16.1"
+  sha256 "d67247aec390e5dc60f547a5c46041c8e2252944ab0d023a43daed3b70c4aa5b"
 
   url "https://download.adobe.com/pub/adobe/dng/mac/DNGConverter_#{version.dots_to_underscores}.dmg"
   name "Adobe DNG Converter"
