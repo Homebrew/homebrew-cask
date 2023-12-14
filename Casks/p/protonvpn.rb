ab280a27d231c9e02aa6d7b77dae580114137a91
Rui Chen <rui@chenrui.dev>
2023-12-14 08:48:45 -0500

    protonvpn 4.1.1

--- Casks/p/protonvpn.rb
+++ Casks/p/protonvpn.rb
@@ -1,6 +1,6 @@
 cask "protonvpn" do
-  version "4.0.1"
-  sha256 "197b3c0bb5e0527c4a1fa0421f7ae1adac90682aaab23bff4282ee89aa9c1285"
+  version "4.1.1"
+  sha256 "00bdf9f7e1c6779beb059bf980e5ba20e94ae397370c457b5e5f52048f0b5b6d"
 
   url "https://protonvpn.com/download/ProtonVPN_mac_v#{version}.dmg"
   name "ProtonVPN"
