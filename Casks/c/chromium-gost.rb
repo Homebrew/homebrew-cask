0eb76fe16fc45319a8aacf8d1fa85e6abdff5d2d
Rui Chen <rui@chenrui.dev>
2023-12-14 08:59:22 -0500

    chromium-gost 120.0.6099.109

--- Casks/c/chromium-gost.rb
+++ Casks/c/chromium-gost.rb
@@ -1,9 +1,9 @@
 cask "chromium-gost" do
   arch arm: "arm64", intel: "amd64"
 
-  version "120.0.6099.62"
-  sha256 arm:   "cd81df2a56365dafcbdf7b70bfc09131ff7e80280532e445c560009ae086be81",
-         intel: "758f11a7a8f68d5ef779da898e749e632cab1f67163c2bd4ce57822af02b23ee"
+  version "120.0.6099.109"
+  sha256 arm:   "50847521842139718f046ae20207baf4e918fe9add666d7efa0aea19a634365f",
+         intel: "b28218786a2f6415e556f208ac95c9a67276cba402af05335132793e85ccf347"
 
   url "https://github.com/deemru/Chromium-Gost/releases/download/#{version}/chromium-gost-#{version}-macos-#{arch}.tar.bz2"
   name "Chromium-Gost"
