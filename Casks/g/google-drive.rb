69aa3f346a50817afe3ab1d46180e544ac988b48
Branch Vincent <branchevincent@gmail.com>
2023-12-14 09:16:38 -0800

    google-drive 85.0.24

--- Casks/g/google-drive.rb
+++ Casks/g/google-drive.rb
@@ -1,5 +1,5 @@
 cask "google-drive" do
-  version "85.0.23"
+  version "85.0.24"
   sha256 :no_check
 
   # "5-percent" is included in the url to ensure that `brew upgrade` does not update to an older version as the
