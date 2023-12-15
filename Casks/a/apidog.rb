54bf4337f247845901c3574c98f3dfb76f437336
Rui Chen <rui@chenrui.dev>
2023-12-15 12:23:06 -0500

    apidog 2.4.4

--- Casks/a/apidog.rb
+++ Casks/a/apidog.rb
@@ -2,9 +2,9 @@ cask "apidog" do
   arch arm: "-macOS-arm64"
   livecheck_folder = on_arch_conditional arm: "-arm64"
 
-  version "2.4.3"
-  sha256 arm:   "3bac351cfc7265acddd2c1b1bf5984cc2f36f1fe69839d408976243c7bb2b095",
-         intel: "d53fea895186545ced27f725f61cb7e7890fd344a8ef8cb320df86ba26b3115e"
+  version "2.4.4"
+  sha256 arm:   "b3d5c8ecc61e62933ca175ae89b9d948ff569ac2f052d693bd6c0329eafa52a1",
+         intel: "efe198d2b9b8258495ddc565fb322dabd910945eb7e4ad9388b48d74675e867f"
 
   url "https://assets.apidog.com/download/#{version}/Apidog#{arch}-#{version}.dmg"
   name "Apidog"
