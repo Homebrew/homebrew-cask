a36bfb4dee727c036421d48722c26dca18ac4fe5
Miccal Matthews <miccal.matthews@gmail.com>
2023-12-17 07:48:28 +0800

    Update capcut.rb

--- Casks/c/capcut.rb
+++ Casks/c/capcut.rb
@@ -2,24 +2,28 @@ cask "capcut" do
   version "0_1.2.4"
   sha256 "f42ec732aa94552def1452cbc2099dba46139a052fa97313bcba4475cac492f0"
 
-  url "https://lf16-capcut.faceulv.com/obj/capcutpc-packages-us/installer/capcut_capcutpc_#{version}_installer.dmg"
+  url "https://lf16-capcut.faceulv.com/obj/capcutpc-packages-us/installer/capcut_capcutpc_#{version}_installer.dmg",
+      verified: "lf16-capcut.faceulv.com/obj/capcutpc-packages-us/installer/"
   name "CapCut"
   desc "Creative platform powered by AI that enables video editing and image design"
   homepage "https://www.capcut.com/"
 
   livecheck do
     url :homepage
-    regex(/capcut[._-]capcutpc[._-]v?(\d+(?:.\d+)+)[._-]installer.dmg/i)
+    regex(/capcut[._-]capcutpc[._-]v?(\d+(?:.\d+)+)[._-]installer\.dmg/i)
   end
 
   installer script: {
     executable: "#{staged_path}/CapCut-Downloader.app/Contents/MacOS/CapCut-Downloader",
   }
 
-  uninstall delete: "/Applications/CapCut.app"
+  uninstall quit:   "com.lemon.lvoverseas",
+            delete: [
+              "/Applications/CapCut.app",
+              "/Library/Logs/DiagnosticReport/CapCut-Downloader_*.diag",
+            ]
 
   zap trash: [
-    "/Library/Logs/DiagnosticReport/CapCut-Downloader_*.diag",
     "~/Library/Application Scripts/com.lemon.lvoverseas",
     "~/Library/Containers/com.lemon.lvoverseas",
     "~/Library/Group Containers/22MMUN2RN5.lv",
