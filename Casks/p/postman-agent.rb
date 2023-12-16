dc706d138d2706add754fd46bfd273251ba1ca2f
Miccal Matthews <miccal.matthews@gmail.com>
2023-12-17 07:57:35 +0800

    Update postman-agent.rb

--- Casks/p/postman-agent.rb
+++ Casks/p/postman-agent.rb
@@ -2,16 +2,17 @@ cask "postman-agent" do
   arch arm: "osx_arm64", intel: "osx_64"
 
   version "0.4.18"
-  sha256 :no_check
+  sha256 arm:   "e49789c5807c98fce85273a6a429f8d5c120e1a1c7f04e9689ead71a90741eb5",
+         intel: "a0aabdd3115ac695400be9861d0bc537b477905f19655b6d7b4acc0afe10a2ad"
 
-  url "https://dl-agent.pstmn.io/download/latest/#{arch}",
-      verified: "dl-agent.pstmn.io/download/latest/"
+  url "https://dl-agent.pstmn.io/download/version/#{version}/#{arch}",
+      verified: "dl-agent.pstmn.io/download/version/"
   name "Postman Agent"
   desc "Desktop agent for Postman on the Web"
   homepage "https://www.postman.com/downloads/postman-agent/"
 
   livecheck do
-    url :url
+    url "https://dl-agent.pstmn.io/download/latest/#{arch}"
     strategy :header_match
   end
 
