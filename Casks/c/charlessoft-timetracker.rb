5362f38e8e4a6b1625d54d24bbb2d7d58c613113
Rylan Polster <rslpolster@gmail.com>
2023-12-17 17:45:49 -0500

    charlessoft-timetracker: use `deprecate!` instead of `discontinued`

--- Casks/c/charlessoft-timetracker.rb
+++ Casks/c/charlessoft-timetracker.rb
@@ -6,9 +6,7 @@ cask "charlessoft-timetracker" do
   name "TimeTracker"
   homepage "https://charlessoft.com/"
 
-  app "TimeTracker.app"
+  deprecate! date: "2023-12-17", because: :discontinued
 
-  caveats do
-    discontinued
-  end
+  app "TimeTracker.app"
 end
