8b6763dd5347a16bc54fdac50be6e668f93722bf
Rylan Polster <rslpolster@gmail.com>
2023-12-17 17:46:09 -0500

    x-mirage: use `deprecate!` instead of `discontinued`

--- Casks/x/x-mirage.rb
+++ Casks/x/x-mirage.rb
@@ -7,9 +7,7 @@ cask "x-mirage" do
   desc "AirPlay and Google Cast receiver"
   homepage "https://www.x-mirage.com/x-mirage/"
 
-  app "X-Mirage.app"
+  deprecate! date: "2023-12-17", because: :discontinued
 
-  caveats do
-    discontinued
-  end
+  app "X-Mirage.app"
 end
