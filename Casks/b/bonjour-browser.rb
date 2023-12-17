2400c33d0d1d2c9bb9b37f5792b95499bc06f15c
Rylan Polster <rslpolster@gmail.com>
2023-12-17 17:45:48 -0500

    bonjour-browser: use `deprecate!` instead of `discontinued`

--- Casks/b/bonjour-browser.rb
+++ Casks/b/bonjour-browser.rb
@@ -7,11 +7,9 @@ cask "bonjour-browser" do
   desc "Display all the bonjour services on your local network"
   homepage "https://www.tildesoft.com/"
 
+  deprecate! date: "2023-12-17", because: :discontinued
+
   depends_on macos: "<= :mojave"
 
   app "Bonjour Browser.app"
-
-  caveats do
-    discontinued
-  end
 end
