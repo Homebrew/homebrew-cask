dfb59c12b69611849ccf6ee24835641988408b04
Rylan Polster <rslpolster@gmail.com>
2023-12-17 17:45:53 -0500

    expo-xde: use `deprecate!` instead of `discontinued`

--- Casks/e/expo-xde.rb
+++ Casks/e/expo-xde.rb
@@ -7,9 +7,7 @@ cask "expo-xde" do
   name "Expo Development Environment (XDE)"
   homepage "https://expo.io/"
 
-  app "Expo XDE.app"
+  deprecate! date: "2023-12-17", because: :discontinued
 
-  caveats do
-    discontinued
-  end
+  app "Expo XDE.app"
 end
