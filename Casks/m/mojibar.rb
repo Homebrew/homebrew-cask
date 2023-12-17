0fb0d0f5a9f8c3fd48c787da80d38d1fc61a8cf1
Rylan Polster <rslpolster@gmail.com>
2023-12-17 17:46:00 -0500

    mojibar: use `deprecate!` instead of `discontinued`

--- Casks/m/mojibar.rb
+++ Casks/m/mojibar.rb
@@ -7,9 +7,7 @@ cask "mojibar" do
   desc "Emoji searcher as a menu bar app"
   homepage "https://github.com/muan/mojibar"
 
-  app "Mojibar-darwin-x64/Mojibar.app"
+  deprecate! date: "2023-12-17", because: :discontinued
 
-  caveats do
-    discontinued
-  end
+  app "Mojibar-darwin-x64/Mojibar.app"
 end
