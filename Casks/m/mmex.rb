cd048538b1bc62fbb2669525389a1ca69e31bb52
Mateusz Jabłoński <mateusz.piotr.jablonski@gmail.com>
2023-12-17 23:43:34 +0100

    Update zap and livecheck stanzas for mmex

--- Casks/m/mmex.rb
+++ Casks/m/mmex.rb
@@ -3,16 +3,23 @@ cask "mmex" do
   sha256 "51991878cd5bf2741743d499f15ed8fc1201d23f9dbe4a98e704ff66a710cf1f"
 
   url "https://github.com/moneymanagerex/moneymanagerex/releases/download/v#{version}/mmex-#{version}-Darwin.dmg",
-      verified: "github.com/moneymanagerex/moneymanagerex/"
+    verified: "github.com/moneymanagerex/moneymanagerex"
   name "Money Manager Ex"
-  desc "Money management application"
-  homepage "https://www.moneymanagerex.org/"
+  desc "Easy to use, money management application built with wxWidgets"
+  homepage "https://moneymanagerex.org"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "MMEX.app"
 
   zap trash: [
     "~/Library/Application Support/MoneyManagerEx",
-    "~/Library/Caches/com.yourcompany.MMEX",
-    "~/Library/Preferences/com.yourcompany.MMEX.plist",
+    "~/Library/Caches/org.moneymanagerex.mmex",
+    "~/Library/Preferences/org.moneymanagerex.mmex.plist",
+    "~/Library/Saved Application State/org.moneymanagerex.mmex.savedState",
+    "~/Library/WebKit/org.moneymanagerex.mmex",
   ]
 end
