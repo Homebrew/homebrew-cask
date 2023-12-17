a6e3580fd6f8e59f476833fb0becb1d9a317869b
Rylan Polster <rslpolster@gmail.com>
2023-12-17 17:45:56 -0500

    hotswitch: use `deprecate!` instead of `discontinued`

--- Casks/h/hotswitch.rb
+++ Casks/h/hotswitch.rb
@@ -7,11 +7,9 @@ cask "hotswitch" do
   desc "Fast window switcher using a 2-stroke hotkey"
   homepage "https://oniatsu.github.io/HotSwitch/"
 
+  deprecate! date: "2023-12-17", because: :discontinued
+
   app "HotSwitch.app"
 
   zap trash: "~/Library/Preferences/com.oniatsu.HotSwitch.plist"
-
-  caveats do
-    discontinued
-  end
 end
