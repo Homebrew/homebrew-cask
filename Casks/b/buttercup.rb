252da45fbbca3eb030c2cbc89ef908451ad646a1
Rui Chen <rui@chenrui.dev>
2023-12-17 00:41:31 -0500

    buttercup 2.24.4

--- Casks/b/buttercup.rb
+++ Casks/b/buttercup.rb
@@ -1,6 +1,6 @@
 cask "buttercup" do
-  version "2.24.3"
-  sha256 "83a61f557445cc8a8d24326a64bca47a703fd170752f6a217f1aa1ef1db2c0d4"
+  version "2.24.4"
+  sha256 "ad3f462603d1c16d958428fd7ce5627195b761a6329fe45e51e21e069e93c44c"
 
   url "https://github.com/buttercup/buttercup-desktop/releases/download/v#{version}/Buttercup-mac-x64-#{version}.dmg",
       verified: "github.com/buttercup/buttercup-desktop/"
