75670a65330d9c3c34bbf6b8258db6a4ade08a39
Rylan Polster <rslpolster@gmail.com>
2023-12-17 17:45:57 -0500

    jenkins-menu: use `deprecate!` instead of `discontinued`

--- Casks/j/jenkins-menu.rb
+++ Casks/j/jenkins-menu.rb
@@ -7,14 +7,12 @@ cask "jenkins-menu" do
   desc "Menu item which shows the status of a Jenkins CI server"
   homepage "https://github.com/qvacua/jenkins-menu/"
 
+  deprecate! date: "2023-12-17", because: :discontinued
+
   app "Jenkins Menu.app"
 
   zap trash: [
     "~/Library/Caches/com.qvacua.Jenkins-Menu",
     "~/Library/Preferences/com.qvacua.Jenkins-Menu.plist",
   ]
-
-  caveats do
-    discontinued
-  end
 end
