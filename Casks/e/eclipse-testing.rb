9f56ee06fcdadb01f37e75c72cc7727add016546
Rylan Polster <rslpolster@gmail.com>
2023-12-17 17:45:52 -0500

    eclipse-testing: use `deprecate!` instead of `discontinued`

--- Casks/e/eclipse-testing.rb
+++ Casks/e/eclipse-testing.rb
@@ -7,10 +7,8 @@ cask "eclipse-testing" do
   desc "Eclipse IDE for testing purposes"
   homepage "https://eclipse.org/"
 
+  deprecate! date: "2023-12-17", because: :discontinued
+
   # Renamed to avoid conflict with other Eclipse.
   app "Eclipse.app", target: "Eclipse Testing.app"
-
-  caveats do
-    discontinued
-  end
 end
