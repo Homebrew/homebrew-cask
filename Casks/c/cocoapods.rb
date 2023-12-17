8ce464b99cc1c113808425aa87de32a9563d76c4
Rylan Polster <rslpolster@gmail.com>
2023-12-17 17:45:50 -0500

    cocoapods: use `deprecate!` instead of `discontinued`

--- Casks/c/cocoapods.rb
+++ Casks/c/cocoapods.rb
@@ -8,6 +8,8 @@ cask "cocoapods" do
   desc "Dependency manager for Cocoa projects"
   homepage "https://cocoapods.org/"
 
+  deprecate! date: "2023-12-17", because: :discontinued
+
   conflicts_with formula: "cocoapods"
 
   app "CocoaPods.app"
@@ -21,8 +23,4 @@ cask "cocoapods" do
   end
 
   zap trash: "~/Library/Preferences/org.cocoapods.CocoaPods.plist"
-
-  caveats do
-    discontinued
-  end
 end
