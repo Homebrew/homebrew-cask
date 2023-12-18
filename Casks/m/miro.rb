2a50d17616dd421db6939d79b635c606616261dc
Rui Chen <rui@chenrui.dev>
2023-12-18 08:55:17 -0500

    miro 0.8.26

--- Casks/m/miro.rb
+++ Casks/m/miro.rb
@@ -1,7 +1,7 @@
 cask "miro" do
   arch arm: "darwin-arm64", intel: "darwin"
 
-  version "0.8.24"
+  version "0.8.26"
   sha256 :no_check
 
   url "https://desktop.miro.com/platforms/#{arch}/Miro.dmg"
