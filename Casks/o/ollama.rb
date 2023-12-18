30deaa9fa1febdfd6cd8e19d796b75cbb2697dac
ongyuxing <78018704+ongyuxing@users.noreply.github.com>
2023-12-18 16:17:47 +0800

    add ollama 0.1.16 (new cask)

---
+++ Casks/o/ollama.rb
@@ -0,0 +1,22 @@
+cask "ollama" do
+  version "0.1.16"
+  sha256 "c4c58fb3caed86f1fd5108fc4df8e4cbe6b25e86bb00bb24b8dde2fd0ed3ee1b"
+
+  url "https://github.com/jmorganca/ollama/releases/download/v#{version}/Ollama-darwin.zip",
+      verified: "github.com/jmorganca/ollama/"
+  name "Ollama"
+  desc "Get up and running with large language models locally"
+  homepage "https://ollama.ai/"
+
+  auto_updates true
+  conflicts_with formula: "ollama"
+  depends_on macos: ">= :high_sierra"
+
+  app "Ollama.app"
+  binary "#{appdir}/Ollama.app/Contents/Resources/ollama"
+
+  zap trash: [
+    "~/.ollama",
+    "~/Library/Application Support/Ollama",
+  ]
+end
