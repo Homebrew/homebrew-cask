912f764c45e5c3d1b2db4ee3110bc19818c912e4
Rui Chen <rui@chenrui.dev>
2023-12-18 18:52:22 -0500

    chromedriver 120.0.6099.109

--- Casks/c/chromedriver.rb
+++ Casks/c/chromedriver.rb
@@ -1,9 +1,9 @@
 cask "chromedriver" do
   arch arm: "arm64", intel: "x64"
 
-  version "120.0.6099.71"
-  sha256 arm:   "51504b0a8f0b91b9edf3cd8f7933273bc9716a6e369880e410ddbc3ecf04d1af",
-         intel: "4e1b0a38e2cd7261535272e2ad21de6bdc33c3e1720b7bfb23fe6085bcfc1808"
+  version "120.0.6099.109"
+  sha256 arm:   "eda235e3535db6e9f71e094d2a25b8f934d580e5404a7cfdf119f5b6b814829a",
+         intel: "db43a02d6aedc36430c9fa1e86853b5a3987dc8a0e1b8937740c9ae54e5ced07"
 
   url "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/#{version}/mac-#{arch}/chromedriver-mac-#{arch}.zip",
       verified: "edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/"
