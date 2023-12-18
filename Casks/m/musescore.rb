855e1455a7a402adc03db9473eab855a219fdcaa
Leopoldo Pla <leopoldoplasempere@gmail.com>
2023-12-18 17:02:02 +0100

    musescore 4.2.0.233521125

--- Casks/m/musescore.rb
+++ Casks/m/musescore.rb
@@ -1,6 +1,6 @@
 cask "musescore" do
-  version "4.1.1.232071203"
-  sha256 "31340ce562e40ba275d5e6be4593f4828231be61bc7ca1f22b5424dc2fba18fd"
+  version "4.2.0.233521125"
+  sha256 "383c589c1b5fa4768807a59f22898c5560d83989345aa19584f3e4452dfc0b26"
 
   url "https://github.com/musescore/MuseScore/releases/download/v#{version.major_minor_patch}/MuseScore-#{version}.dmg",
       verified: "github.com/musescore/MuseScore/"
