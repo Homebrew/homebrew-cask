fb76c788235dffff603247ec85f969051a8ec483
Bevan Kay <email@bevankay.me>
2023-12-18 12:19:25 +1100

    codux 15.16.2

--- Casks/c/codux.rb
+++ Casks/c/codux.rb
@@ -1,9 +1,9 @@
 cask "codux" do
   arch arm: "arm64", intel: "x64"
 
-  version "15.16.1"
-  sha256 arm:   "48138d58e8c357ff93e01a0853350e68a6207b191d5c505820baf660cbfd6acc",
-         intel: "0f36bb44f86366f1a5919758739f819d6227f96f13eada774d52d61f26172e5a"
+  version "15.16.2"
+  sha256 arm:   "550682d58171760da78d776383c9838bb592fc760ad8412890ae979f9702044f",
+         intel: "1fa88e283090e74c138d70bcb54f0d325b3513d09cf4da21e7bae4f57ce7c2a2"
 
   url "https://github.com/wixplosives/codux-versions/releases/download/#{version}/Codux-#{version}.#{arch}.dmg",
       verified: "github.com/wixplosives/codux-versions/"
