44ad3c181415e54c1f4c07d3e9c60e3141a2af7c
Rui Chen <rui@chenrui.dev>
2023-12-18 08:54:33 -0500

    mongodb-compass-isolated-edition 1.41.0

--- Casks/m/mongodb-compass-isolated-edition.rb
+++ Casks/m/mongodb-compass-isolated-edition.rb
@@ -1,9 +1,9 @@
 cask "mongodb-compass-isolated-edition" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.40.4"
-  sha256 arm:   "a245828c7041d01584a568c345b7b61b8df88f3f9aae31810324cb384b49df90",
-         intel: "246a1772e8e9998075a8caaf3f0123dc787a75ee713c242a36fc1582dc084421"
+  version "1.41.0"
+  sha256 arm:   "9bd1ca88d3b77843e1a500ccf1780c8cf0955b7d56f0c34ee29eb6a83fa2e7b7",
+         intel: "f3072cf294962aa5f548088cca0f2696cb66465d581b458f27ac181682b3c8a8"
 
   url "https://downloads.mongodb.com/compass/mongodb-compass-isolated-#{version}-darwin-#{arch}.dmg"
   name "MongoDB Compass Isolated"
