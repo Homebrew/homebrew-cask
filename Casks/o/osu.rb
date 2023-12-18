cfb7d5b0975522b1d97efe241450ada00fa1e036
Jackson <jackson@jacksonchen666.com>
2023-12-18 21:34:48 +0100

    osu 2023.1218.1

--- Casks/o/osu.rb
+++ Casks/o/osu.rb
@@ -1,9 +1,9 @@
 cask "osu" do
   arch arm: "Apple.Silicon", intel: "Intel"
 
-  version "2023.1218.0"
-  sha256 arm:   "93c02bbc0b652caedacd9b06ecb678fb504f2364cd99a1e3b546e938667bcb17",
-         intel: "8479d2fd09a4751668467280a552034e7d6c2a673daab226e36c560be5428746"
+  version "2023.1218.1"
+  sha256 arm:   "6a061a50c9f6530f6f03f24269ee5e50b81e043630f68a9bfe571bd20aab478f",
+         intel: "b9a70e29f4b679c44cab0f0e220a72b3032a7be6d1d9a2d31e3fc3ee3e69f0e8"
 
   url "https://github.com/ppy/osu/releases/download/#{version}/osu.app.#{arch}.zip"
   name "osu!"
