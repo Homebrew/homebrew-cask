bbed36ebaa52ab9855ba4df92d35c112fea5791e
Rui Chen <rui@chenrui.dev>
2023-12-18 18:56:07 -0500

    xiv-on-mac 4.13.4

--- Casks/x/xiv-on-mac.rb
+++ Casks/x/xiv-on-mac.rb
@@ -1,6 +1,6 @@
 cask "xiv-on-mac" do
-  version "4.13.3"
-  sha256 "dcf0cc2ec2c5e47e7c75d0ecb66a96413d55b370333c965453721c85bbc05079"
+  version "4.13.4"
+  sha256 "17b5dcaa8103d717377746cdf66a91a907eba2130f4eba194c54f868d43f061a"
 
   url "https://softwareupdate.xivmac.com/sites/default/files/update_data/XIV%20on%20Mac#{version}.tar.xz"
   name "XIV on Mac"
