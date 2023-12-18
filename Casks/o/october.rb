a6761db7c9c90af7504fe7118aa5a071ad37ccc5
Marcus Crane <marcus@utf9k.net>
2023-12-18 18:24:07 +1300

    october 1.8.0
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- Casks/o/october.rb
+++ Casks/o/october.rb
@@ -1,9 +1,9 @@
 cask "october" do
   arch arm: "arm64", intel: "amd64"
 
-  version "1.7.0"
-  sha256 arm:   "ad12fd61c789155e635f5ae1f5a8faf26405f4b6c19b12a01fbf4e653c274777",
-         intel: "f8a707aaec4c4c74e5d5dba722346ffe9ad48db59af74adeb7d6da7bbe9b7855"
+  version "1.8.0"
+  sha256 arm:   "a97993cf52e985c92c2a287a44702d87a0c692402b58934925d6e75d1b19d8aa",
+         intel: "4342c2adc3125a3a8522ad14f72c50d43377ba0b98391e68314dfbd08392ef54"
 
   url "https://github.com/marcus-crane/october/releases/download/v#{version}/october_#{version}_darwin_#{arch}.zip",
       verified: "github.com/marcus-crane/october/"
