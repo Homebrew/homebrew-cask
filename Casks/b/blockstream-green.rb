c7ff5d46103fad4918924108b9b9b437bdd2a92d
Miccal Matthews <miccal.matthews@gmail.com>
2023-12-19 05:56:15 +0800

    Update blockstream-green.rb

--- Casks/b/blockstream-green.rb
+++ Casks/b/blockstream-green.rb
@@ -1,6 +1,6 @@
 cask "blockstream-green" do
-  version "1.2.8"
-  sha256 "9bfee4300aefc6f518f181c83e4af21bbdab7ae8b3fd8e334ea3fbe2c4a3b5ee"
+  version "1.2.9"
+  sha256 "1b8589bc997016087ba27ef28f2026626834c91af525ca88bd3841bafcbe4c06"
 
   url "https://github.com/Blockstream/green_qt/releases/download/release_#{version}/BlockstreamGreen_MacOS_x86_64.zip",
       verified: "github.com/Blockstream/green_qt/"
