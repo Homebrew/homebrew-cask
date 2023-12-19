59319c02efe6ce258a24c76916bcf697804d0996
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-12-19 11:19:26 -0500

    4k-stogram 4.7.0

--- Casks/4/4k-stogram.rb
+++ Casks/4/4k-stogram.rb
@@ -1,6 +1,6 @@
 cask "4k-stogram" do
-  version "4.6.3"
-  sha256 "44b479d867b2949adedef85441c03472d25a9d74bcf648080e3e0a0007a0dede"
+  version "4.7.0"
+  sha256 "c213dab20ba1571937c304eea25d67f35b05948e6c5c93fe1c1de91a8b69d3cb"
 
   url "https://dl.4kdownload.com/app/4kstogram_#{version}.dmg"
   name "4K Stogram"
