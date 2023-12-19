c7e2a9ed359102fdfb1f07e10b237592527bd3f4
root <root@sdl63315.labs.teradata.com>
2023-12-19 13:55:29 -0800

    ttu-base-suite-2000 20.00.04.00

--- Casks/t/ttu-base-suite-2000.rb
+++ Casks/t/ttu-base-suite-2000.rb
@@ -1,7 +1,7 @@
 cask "ttu-base-suite-2000" do
   version "20.00.04.00"
-  sha256 "663e8bd8f66f11fa35face2860a38de5a77f38b2954db93f5dc2db0b0cfb86ae"
-  
+  sha256 "663e8bd8f66f11fa35face2860a38de5a77f38b2954db93f5dc2db0b0cfb86ae"  
+
   url "https://downloads.teradata.com/sites/default/files/2023-12/TeradataToolsAndUtilities-macosx-brew-#{version}.tar"
   name "TTU BASE Suite #{version}"
   desc "Teradata Tools and Utilities BASE Suite 20.00.04.00"
@@ -9,16 +9,22 @@ cask "ttu-base-suite-2000" do
 
   depends_on macos: ">= :catalina"
 
+  livecheck do
+    skip "No version information available"
+  end
+  
   installer script: {
     executable: "silent-install.sh",
-    args:       ["ttu-base-suite-2000"],
-    sudo:       true,
-    }
+    args:  ["ttu-base-suite-2000"],
+    sudo:  true,
+  }
 
   uninstall script: {
     executable: "silent-uninstall.sh",
-    args:       ["BASE"],
-    sudo:       true,
-    },
-    pkgutil: "com.Teradata.*2000.pkg.ttuuninstaller"
+    args:  ["BASE"],
+    sudo:  true,
+  },
+  pkgutil: "com.Teradata.*2000.pkg.ttuuninstaller"
+  
+  # No zap stanza required
 end
