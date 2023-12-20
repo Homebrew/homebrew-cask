ce22686931c7f8eafd0db6c0bef194a2c0474723
Mikl Wolfe <wolfemikl@gmail.com>
2023-12-20 12:45:43 -0700

    miro 0.8.28

--- Casks/m/miro.rb
+++ Casks/m/miro.rb
@@ -1,7 +1,7 @@
 cask "miro" do
   arch arm: "darwin-arm64", intel: "darwin"
 
-  version "0.8.26"
+  version "0.8.28"
   sha256 :no_check
 
   url "https://desktop.miro.com/platforms/#{arch}/Miro.dmg"
