bd163c2309a05d97abeea3c3cc819116f20c94a4
Amory Meltzer <Amorymeltzer@gmail.com>
2023-12-20 11:24:42 -0500

    runjs 2.10.1

--- Casks/r/runjs.rb
+++ Casks/r/runjs.rb
@@ -1,6 +1,6 @@
 cask "runjs" do
-  version "2.9.0"
-  sha256 "cc5821b98a9d846ce58ce7233836ed7d093a264ccaa9eedceffd528647bf0f7b"
+  version "2.10.1"
+  sha256 "a82a039febaf64ae44dc3eca35c7cfd817544a795fb53468a247e06d80e5ddc8"
 
   url "https://github.com/lukehaas/RunJS/releases/download/v#{version}/RunJS-#{version}-universal.dmg",
       verified: "github.com/lukehaas/RunJS/"
