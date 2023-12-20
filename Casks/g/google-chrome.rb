74c1360c858b8baa4fd541cdc68f6904884ecb85
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-20 18:36:17 +0000

    google-chrome 120.0.6099.129

--- Casks/g/google-chrome.rb
+++ Casks/g/google-chrome.rb
@@ -1,5 +1,5 @@
 cask "google-chrome" do
-  version "120.0.6099.109"
+  version "120.0.6099.129"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
