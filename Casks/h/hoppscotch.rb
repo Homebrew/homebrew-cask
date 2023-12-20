a204b9794214734d3aaf3b3ee8c711747f8fef25
Bevan Kay <email@bevankay.me>
2023-12-20 23:16:05 +1100

    hoppscotch 23.12.0-2

--- Casks/h/hoppscotch.rb
+++ Casks/h/hoppscotch.rb
@@ -1,9 +1,9 @@
 cask "hoppscotch" do
   arch arm: "aarch64", intel: "x64"
 
-  version "23.12.0-1"
-  sha256 arm:   "747282846f80456a023aad566ee0427b35321cf58a6a1a18be0c143b8d461eb3",
-         intel: "3a1156c328195de02119b8b65b1b7065ab000afb9fd994aabe716d94dcb6e8c3"
+  version "23.12.0-2"
+  sha256 arm:   "5b9fd6e1695730768075d9d82b209b3a89bfcc7471f6f521d44df79ca0133207",
+         intel: "6bd25829edb7131d04f51888b8731b645ed1f23edea6754dc7f0ab6a8d022aaf"
 
   url "https://github.com/hoppscotch/releases/releases/download/v#{version}/Hoppscotch_mac_#{arch}.dmg"
   name "Hoppscotch"
