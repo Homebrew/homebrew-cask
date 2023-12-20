bd85301811b87d4a01eb959062e08e7e3c54ddf2
Mikl Wolfe <wolfemikl@gmail.com>
2023-12-20 11:48:34 -0700

    focusrite-control 3.17.0

--- Casks/f/focusrite-control.rb
+++ Casks/f/focusrite-control.rb
@@ -1,6 +1,6 @@
 cask "focusrite-control" do
-  version "3.16.0"
-  sha256 "7d3b479eac54c66a945109a323b98c12327dee87040bc1c4cd3df592f8d89d63"
+  version "3.17.0"
+  sha256 "d3421b8cd90a54c69072b5c0e391adaaac58a4ad9c09290a5d5caea46838ecf6"
 
   url "https://fael-downloads-prod.focusrite.com/customer/prod/downloads/focusrite_control_#{version}.dmg"
   name "Focusrite Control"
