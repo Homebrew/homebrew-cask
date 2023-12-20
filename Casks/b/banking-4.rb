773688acee64905e6fb80f75ebedd2ba8d683ed9
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-12-20 10:00:20 -0500

    banking-4 8.3.5

--- Casks/b/banking-4.rb
+++ Casks/b/banking-4.rb
@@ -1,6 +1,6 @@
 cask "banking-4" do
   # NOTE: "4" is not a version number, but an intrinsic part of the product name
-  version "8.3.4,8747"
+  version "8.3.5,8753"
   sha256 :no_check
 
   url "https://subsembly.com/download/MacBanking4.pkg"
