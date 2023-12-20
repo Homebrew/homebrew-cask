d8bdce0d9f0bf70b8d71786e05dd5e4df1d9a37d
Mikl Wolfe <wolfemikl@gmail.com>
2023-12-20 13:52:54 -0700

    thunder 5.20.1.66132

--- Casks/t/thunder.rb
+++ Casks/t/thunder.rb
@@ -1,6 +1,6 @@
 cask "thunder" do
-  version "5.20.0.66074"
-  sha256 "d972352769e61f758c036b80991e994a86f39a00b69bf934993d7763cc5b53fc"
+  version "5.20.1.66132"
+  sha256 "719e6e9a9df1dc07368b46b2c072ff724cf170c885054194476cb751a66a81b7"
 
   url "https://down.sandai.net/mac/thunder_#{version}.dmg",
       verified: "down.sandai.net/mac/"
