5cb58434d5c7a43357f90ea2d941da839fdfbb36
Mikl Wolfe <wolfemikl@gmail.com>
2023-12-20 14:02:43 -0700

    universal-gcode-platform 2.1.4

--- Casks/u/universal-gcode-platform.rb
+++ Casks/u/universal-gcode-platform.rb
@@ -1,9 +1,9 @@
 cask "universal-gcode-platform" do
   arch arm: "aarch64", intel: "x64"
 
-  version "2.1.3"
-  sha256 arm:   "978557e8525a087a8f896e33f063b3cc46c573977a86b21b78292b0b71bca4fa",
-         intel: "5349fc74dfb8cfdfe257c76eb8e4f0faf3729cea3fb7300155f8d62b0ed91753"
+  version "2.1.4"
+  sha256 arm:   "bc4e6bfc43479a525fbadea49f89abf253668d0e328cea3c4f363304aed07087",
+         intel: "783a1ef92f4edd1e7d5b729fdc94f45a5b5dbd9dc2d6e9e4efed0703ef23bc73"
 
   url "https://github.com/winder/Universal-G-Code-Sender/releases/download/v#{version}/macosx-#{arch}-ugs-platform-app-#{version}.dmg",
       verified: "github.com/winder/Universal-G-Code-Sender/"
