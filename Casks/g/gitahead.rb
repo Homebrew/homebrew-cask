594bf2db95d4f12cd54d7b7e50c9a85d6d9b7e31
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-12-21 08:11:31 -0500

    gitahead: add `desc`, update `depends_on`, add `zap`

--- Casks/g/gitahead.rb
+++ Casks/g/gitahead.rb
@@ -4,9 +4,16 @@ cask "gitahead" do
 
   url "https://github.com/gitahead/gitahead/releases/download/v#{version}/GitAhead-#{version}.dmg"
   name "GitAhead"
+  desc "Git Client"
   homepage "https://github.com/gitahead/gitahead"
 
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :big_sur"
 
   app "GitAhead.app"
+
+  zap trash: [
+    "~/Library/Application Support/GitAhead",
+    "~/Library/Preferences/com.gitahead.GitAhead.plist",
+    "~/Library/Saved Application State/com.gitahead.GitAhead.savedState",
+  ]
 end
