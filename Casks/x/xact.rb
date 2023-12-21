cc00d08d33aee3fb085dbb4a515f60ef876f8f14
Tobias Tschinkowitz <tobias.tschinkowitz@icloud.com>
2023-12-21 09:06:26 +0100

    xACT 2.54 (new cask)

---
+++ Casks/x/xact.rb
@@ -0,0 +1,20 @@
+cask "xact" do
+  arch arm: "arm64", intel: "x64"
+  depends_on macos: ">= :high_sierra"
+
+  version "2.54"
+  sha256 "8e7f3af32202148139beefda41c4a75c59955df967d8545f2da62e41f32dc9f5"
+
+  url "http://xact.scottcbrown.org/xACT#{version}.zip"
+  name "xACT"
+  desc "X Audio Compression Toolkit"
+  homepage "http://xact.scottcbrown.org/"
+
+  livecheck do
+    url "http://xact.scottcbrown.org/"
+    regex(/href=.*?xACTv?(\d+(?:\.\d+)+)\.zip/i)
+  end
+
+  app "xACT#{version}/xACT.app"
+
+end
