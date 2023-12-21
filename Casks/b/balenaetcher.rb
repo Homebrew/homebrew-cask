ca9f705f34a67fbefc2dcead49231af334c53e3f
Balu <tw@b-a-l-u.de>
2023-12-21 19:55:28 +0100

    balenaetcher 1.19.0

--- Casks/b/balenaetcher.rb
+++ Casks/b/balenaetcher.rb
@@ -1,8 +1,11 @@
 cask "balenaetcher" do
-  version "1.18.11"
-  sha256 "251f403b79f53bbf7c558cb0b6ce085bd9d4f1fb7f70d96fdfc10ecbc493e70f"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://github.com/balena-io/etcher/releases/download/v#{version}/balenaEtcher-#{version}.dmg",
+  version "1.19.0"
+  sha256 arm:   "8b885afc72ae1cff988b399044b49cfeffa87f206b047385f09fcc68dcd5845f",
+         intel: "2c6da9bb0f69adead0b7b4d603d06794bc490ce3a55db1ce82b103555cf3eee1"
+
+  url "https://github.com/balena-io/etcher/releases/download/v#{version}/balenaEtcher-#{version}-#{arch}.dmg",
       verified: "github.com/balena-io/etcher/"
   name "Etcher"
   desc "Tool to flash OS images to SD cards & USB drives"
