f5ca62eeb2ba4a0e462b59ac037f7f410b3e19a8
singularitti <singularitti@outlook.com>
2023-12-22 17:38:35 -0500

    permute 3.11.3

--- Casks/p/permute.rb
+++ Casks/p/permute.rb
@@ -1,6 +1,6 @@
 cask "permute" do
-  version "3.11.2,2723"
-  sha256 "f004c43ed64c42e3b485d56a71043b15c10b1d54cd9f2dab4c1960800a313d2c"
+  version "3.11.3,2729"
+  sha256 "c53abca23d17910d84a2ea1196090f4f680fdcf562e27e85e10160b6339d461e"
 
   url "https://software.charliemonroe.net/trial/permute/v#{version.major}/Permute_#{version.major}_#{version.csv.second}.dmg"
   name "Permute"
