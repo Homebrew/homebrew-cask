17edefea9b854bbd8f388cb4d3b675daa14be3c5
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-12-21 21:28:30 -0500

    clop 2.5.2

--- Casks/c/clop.rb
+++ Casks/c/clop.rb
@@ -1,6 +1,6 @@
 cask "clop" do
-  version "2.5.1"
-  sha256 "1a435099e37dd31815be5555fa42f09e4e3ed1a292539a5559083a0e1329de93"
+  version "2.5.2"
+  sha256 "3318655cc307652050852a880012acfecdeb01c645f9c17efc9371f02c05b003"
 
   url "https://files.lowtechguys.com/releases/Clop-#{version}.dmg"
   name "Clop"
