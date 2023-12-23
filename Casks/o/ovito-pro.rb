4e8c927ad6bbcedeec2f1a8bbf3ac48b3c3ee22a
Mikl Wolfe <wolfemikl@gmail.com>
2023-12-23 10:25:36 -0700

    ovito-pro 3.10.0

--- Casks/o/ovito-pro.rb
+++ Casks/o/ovito-pro.rb
@@ -1,9 +1,9 @@
 cask "ovito-pro" do
   arch arm: "arm64", intel: "intel"
 
-  version "3.9.4"
-  sha256 arm:   "43014be524a9fa538ee2679e5e65d4a81dfefbbfb919368d527cdc5c03a83caa",
-         intel: "d63e0bd9e4e7b192c03481b5671cacde90f4024d63bfac61e057e36457a74a6e"
+  version "3.10.0"
+  sha256 arm:   "35f2ce8b50fddae1b68faf309d32f65fef02e3ff19fd35d6dfeef886d7a5b09f",
+         intel: "43357e198b5a24a935ddba5903a7ef2d8cc316d9e6465d6b5d320257c04f11da"
 
   url "https://www.ovito.org/download/master/ovito-pro-#{version}-macos-#{arch}.dmg"
   name "OVITO Pro"
