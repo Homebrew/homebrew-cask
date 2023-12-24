f8a692d7700cc1a40bd683dc47cf1ce426ac4594
singularitti <singularitti@outlook.com>
2023-12-24 17:23:08 -0500

    aural 3.25.1

--- Casks/a/aural.rb
+++ Casks/a/aural.rb
@@ -1,6 +1,6 @@
 cask "aural" do
-  version "3.25.0"
-  sha256 "e306803777b7735e3966a10ff2b90cda562c56aa6508cfcad3e7752a007eb845"
+  version "3.25.1"
+  sha256 "018378b99e532d57e1f466c4b23e0f912b768ab21ccc0d4cbdb90e038d4df708"
 
   url "https://github.com/maculateConception/aural-player/releases/download/v#{version}/AuralPlayer-#{version}.dmg"
   name "Aural Player"
