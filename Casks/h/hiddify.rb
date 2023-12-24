b7ba69bc3711148f3d9d8b2db956a44aa88e928f
Lex.Chen <ctyytc@gmail.com>
2023-12-24 09:23:28 +0800

    Update hiddify.rb
    Fix desc issue

--- Casks/h/hiddify.rb
+++ Casks/h/hiddify.rb
@@ -1,11 +1,10 @@
 cask "hiddify-next" do
-
   version "0.12.2"
   sha256 "a58a7ddd0be1b27195786e508290ea0e77fef3714b53a274931ba48f37be8f9d"
 
   url "https://github.com/hiddify/hiddify-next/releases/download/v#{version}/hiddify-macos-universal.zip"
   name "Hiddify-Next"
-  desc "Multi-platform auto-proxy client, supporting Sing-box, X-ray, TUIC, Hysteria, Reality, Trojan, SSH etc. It’s an open-source, secure and ad-free."
+  desc "Multi-platform proxy client, support Sing-box, X-ray, Hysteria, Trojan, SS etc"
   homepage "https://hiddify.com/"
 
   livecheck do
