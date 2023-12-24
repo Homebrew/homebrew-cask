f4706d0bfff2f0aead735f13d08fc43006f81cd8
Connor Adams <connorads@users.noreply.github.com>
2023-12-24 13:41:47 +0000

    Update url and livecheck

--- Casks/c/creality-print.rb
+++ Casks/c/creality-print.rb
@@ -1,11 +1,18 @@
 cask "creality-print" do
-  version "v4.3.8.6984"
+  version "4.3.8.6984"
   sha256 "d2febf68dce011ffa317837e5e63d6729cdd254894ba34129a57118869b5cf91"
 
-  url "https://file2-cdn.creality.com/file/8dcd085c64cc389dacd21cd851593d42/Creality_Print-v4.3.8.6984-macx-Release.dmg"
+  url "https://github.com/CrealityOfficial/CrealityPrint/releases/download/v#{version.split('.')[0..2].join('.')}/Creality_Print-v#{version}-macx-Release.dmg"
   name "Creality Print"
   desc "Slicer and cloud services for some Creality FDM 3D printers"
-  homepage "https://www.creality.com/download/"
+  homepage "https://github.com/CrealityOfficial"
+
+  livecheck do
+    url "https://api.github.com/repos/CrealityOfficial/CrealityPrint/releases"
+    strategy :page_match do |page|
+      page.scan(/Creality_Print-v?(\d+(?:\.\d+)+)-macx-Release\.dmg/i).map { |match| match.first }
+    end
+  end
 
   app "Creality Print.app"
 
