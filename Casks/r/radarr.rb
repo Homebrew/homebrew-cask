5e4969ec56966f055360788f9cd9c635a1f1185e
Rui Chen <rui@chenrui.dev>
2023-12-25 10:44:09 -0500

    radarr 5.2.6.8376

--- Casks/r/radarr.rb
+++ Casks/r/radarr.rb
@@ -1,9 +1,9 @@
 cask "radarr" do
   arch arm: "arm64", intel: "x64"
 
-  version "5.1.3.8246"
-  sha256 arm:   "cc6a103f2128f5e588b8f7ef248f8326226a2d6f70deaa5a64b5c1b30c2b28d2",
-         intel: "0ce43a4792c27a4dd76bdf1538100832d6a027b93acd14c08de9827ef879b094"
+  version "5.2.6.8376"
+  sha256 arm:   "f3fb5837df392d20e672fb79ff8c5f7004a0ff868be23e84bcf4d3e856f6f9c5",
+         intel: "8f1688ccad85eee915afd1572310f7848ef89f581295be334ef7d251263fea92"
 
   url "https://github.com/Radarr/Radarr/releases/download/v#{version}/Radarr.master.#{version}.osx-app-core-#{arch}.zip",
       verified: "github.com/Radarr/Radarr/"
