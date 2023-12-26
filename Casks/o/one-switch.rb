cd206b68a230d19e76eff09444f4a9e1f1356dcc
CyanWoods <i@cyanwoods.com>
2023-12-26 17:54:49 +0800

    Update one-switch to 1.33

--- Casks/o/one-switch.rb
+++ Casks/o/one-switch.rb
@@ -1,6 +1,6 @@
 cask "one-switch" do
-  version "1.31,392"
-  sha256 "ba090841303e008c499b4d2ec91854d42f01245becb6e4a1bbec23be7d353c1f"
+  version "1.33,406"
+  sha256 "94c4d7e54d6c7b378dbca8d5d31501d508cba1f3cfbf4387d1fe5bc514afb495"
 
   url "https://fireball.studio/media/uploads/files/OneSwitchOfficial-#{version.csv.second}.dmg"
   name "One Switch"
