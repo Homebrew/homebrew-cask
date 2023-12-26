72e642b31e90e8e5382c62d50dff264a96dc7100
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-12-25 21:43:20 -0500

    anytype 0.37.0

--- Casks/a/anytype.rb
+++ Casks/a/anytype.rb
@@ -1,9 +1,9 @@
 cask "anytype" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.36.0"
-  sha256 arm:   "1bda0de93d967da4b9806fbe2e439349d3be51965dc799f9352c1d5be3959788",
-         intel: "a68afafd9a17246287794ce7278cc35071119b74a921d87585176dc46782bc06"
+  version "0.37.0"
+  sha256 arm:   "7450cd2fd5f1ad7e6846888ac1aadbe6296d3e5075b46f08665d5dbd5b507935",
+         intel: "09cfcc44e88e230bb073e1316369f06081f2c1ace3c94060ca3df7aaedb4d6cc"
 
   url "https://anytype-release.fra1.cdn.digitaloceanspaces.com/Anytype-#{version}-mac-#{arch}.dmg",
       verified: "anytype-release.fra1.cdn.digitaloceanspaces.com/"
