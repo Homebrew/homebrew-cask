ab7169b1b8f614174ad3eaf29a7176580546ca7b
Rui Chen <rui@chenrui.dev>
2023-12-27 12:48:21 -0500

    redisinsight 2.40.0

--- Casks/r/redisinsight.rb
+++ Casks/r/redisinsight.rb
@@ -1,7 +1,7 @@
 cask "redisinsight" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.38.0"
+  version "2.40.0"
   sha256 :no_check
 
   url "https://download.redisinsight.redis.com/latest/RedisInsight-mac-#{arch}.dmg"
