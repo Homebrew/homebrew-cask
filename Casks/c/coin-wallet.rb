d7d6284107c0ebc8a412742374618713f5ea23d7
Mikl Wolfe <wolfemikl@gmail.com>
2023-12-27 11:07:45 -0700

    coin-wallet 6.0.2

--- Casks/c/coin-wallet.rb
+++ Casks/c/coin-wallet.rb
@@ -1,6 +1,6 @@
 cask "coin-wallet" do
-  version "6.0.1"
-  sha256 "c7c3b47ffb874466a09edf21c3216b2e601731ee76a6299c11ef85f51adee3a8"
+  version "6.0.2"
+  sha256 "56bc31e507f0aaaed0a3924ef7e003b02a619a79acf1a8231ce7600832d7b3fa"
 
   url "https://github.com/CoinSpace/CoinSpace/releases/download/v#{version}/Coin-Wallet.dmg",
       verified: "github.com/CoinSpace/CoinSpace/"
