6e4ed5b91449a138fdb46136d2f3c785a9a712cd
Sam Ford <1584702+samford@users.noreply.github.com>
2023-12-27 21:36:27 -0500

    ilya-birman-typography-layout 3.9

--- Casks/i/ilya-birman-typography-layout.rb
+++ Casks/i/ilya-birman-typography-layout.rb
@@ -1,19 +1,18 @@
 cask "ilya-birman-typography-layout" do
-  version "3.8"
-  sha256 "9c6fd58ef2f527a400e850358c38f4c36a5f264e66d03a464f1d70102c5ce9df"
+  version "3.9"
+  sha256 "64b4f7b1421cc4275864c25941646ddf2d3075212a77e9ba1c2d27b726afe123"
 
-  url "https://ilyabirman.ru/typography-layout/download/ilya-birman-typolayout-#{version}-mac.dmg"
+  url "https://ilyabirman.ru/typography-layout/download/ilya-birman-typolayout-#{version}-mac.zip"
   name "Ilya Birman Typography Layout"
   desc "Typography keyboard layout"
   homepage "https://ilyabirman.ru/typography-layout/"
 
   livecheck do
     url :homepage
-    regex(%r{href=.*?/ilya[._-]birman[._-]typolayout[._-]v?(\d+(?:\.\d+)+)[._-]mac\.dmg}i)
+    regex(/href=.*?ilya[._-]birman[._-]typolayout[._-]v?(\d+(?:\.\d+)+)[._-]mac\.(?:dmg|zip)/i)
   end
 
-  keyboard_layout \
-    "Install Ilya Birman Typography Layout.app/Contents/Resources/Layout/Ilya Birman Typography Layout.bundle"
+  keyboard_layout "Ilya Birman Typography Layout.bundle"
 
   # No zap stanza required
 
