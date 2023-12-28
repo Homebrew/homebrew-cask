0b5f68051fb6cd2dc811140a09dbf72a4e64968c
Rui Chen <rui@chenrui.dev>
2023-12-27 20:03:45 -0500

    packetsender 8.6.5

--- Casks/p/packetsender.rb
+++ Casks/p/packetsender.rb
@@ -1,6 +1,6 @@
 cask "packetsender" do
-  version "8.6.3"
-  sha256 "f778842269b185893e5ea20ab0453f2400a747560054e3cda92e2a2f687eca1b"
+  version "8.6.5"
+  sha256 "72acd6b351e67d3aa60237f8b3e2253ea1918d529c2a15cf910c3a14fa27e4e4"
 
   url "https://github.com/dannagle/PacketSender/releases/download/v#{version}/PacketSender_v#{version}.dmg",
       verified: "github.com/dannagle/PacketSender/"
