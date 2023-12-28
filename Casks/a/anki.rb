747eb418ee50e8687c452fe827b5ee6787d43cf0
Rui Chen <rui@chenrui.dev>
2023-12-28 10:52:13 -0500

    anki 23.12.1

--- Casks/a/anki.rb
+++ Casks/a/anki.rb
@@ -1,7 +1,7 @@
 cask "anki" do
   arch arm: "apple", intel: "intel"
 
-  version "23.12"
+  version "23.12.1"
 
   on_catalina :or_older do
     sha256 "c74c1c660e45f29fbd7b939de3cec6a8cbff2ea143ba3e27b0a807a48290ff37"
@@ -10,8 +10,8 @@ cask "anki" do
         verified: "github.com/ankitects/anki/"
   end
   on_big_sur :or_newer do
-    sha256 arm:   "4bcfe84739cecd3e8645a83466e566daae1f750c24db36a8bb681ca223c2b877",
-           intel: "568c2f302c7dbce3a668a62154f7b5a309d9c71614b764a8c2f6f67d85ba8e6f"
+    sha256 arm:   "6dd682a928e37bceb0995288159ab3b8568467b49642bec2012fc79b50a938c8",
+           intel: "cf8f11101d78a7badbe74b72f6eff6eb0c74b18e1065be298fac0e5ec48109d8"
 
     url "https://github.com/ankitects/anki/releases/download/#{version}/anki-#{version}-mac-#{arch}-qt6.dmg",
         verified: "github.com/ankitects/anki/"
