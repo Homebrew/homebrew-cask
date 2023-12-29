e33721dc4dd1acc5ed7557156ccc5f5d143e5cdb
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-12-29 08:36:34 -0500

    naver-whale 3.24.223.18

--- Casks/n/naver-whale.rb
+++ Casks/n/naver-whale.rb
@@ -1,7 +1,7 @@
 cask "naver-whale" do
   arch arm: "ARM64"
 
-  version "3.24.223.12"
+  version "3.24.223.18"
   sha256 :no_check
 
   url "http://update.whale.naver.net/downloads/installers/NaverWhale#{arch}.dmg",
