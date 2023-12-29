424aba895254f56bb041c4696d4abcb2143ff1cb
Mikl Wolfe <wolfemikl@gmail.com>
2023-12-29 11:20:39 -0700

    kicad 7.0.10

--- Casks/k/kicad.rb
+++ Casks/k/kicad.rb
@@ -1,6 +1,6 @@
 cask "kicad" do
-  version "7.0.9"
-  sha256 "0e626521850594596d30711967216203e85343e0ed59f552336975fef6caa7dc"
+  version "7.0.10"
+  sha256 "f3bcf25618cf56c06e774b8b63eed123d1b72fb608473a6f009ee2b28ccea804"
 
   url "https://github.com/KiCad/kicad-source-mirror/releases/download/#{version}/kicad-unified-universal-#{version}.dmg",
       verified: "github.com/KiCad/kicad-source-mirror/"
