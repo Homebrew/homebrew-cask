e592d3337cf9e8b6c0ad7a8807eb6e9909ee3ba0
Rui Chen <rui@chenrui.dev>
2023-12-29 16:11:40 -0500

    cncnet: add `desc`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/c/cncnet.rb
+++ Casks/c/cncnet.rb
@@ -5,6 +5,7 @@ cask "cncnet" do
   url "https://funkyfr3sh.cnc-comm.com/files/CnCNet.dmg",
       verified: "funkyfr3sh.cnc-comm.com/"
   name "CnCNet: Classic Command & Conquer"
+  desc "Multiplayer platform for classic Command & Conquer games"
   homepage "https://cncnet.org/"
 
   livecheck do
