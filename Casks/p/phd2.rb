ab831676fb3b20ba5e7edbea3141429efa18dfd3
Sam Ford <1584702+samford@users.noreply.github.com>
2023-12-29 20:45:41 -0500

    phd2 2.6.13

--- Casks/p/phd2.rb
+++ Casks/p/phd2.rb
@@ -1,16 +1,30 @@
 cask "phd2" do
-  version "2.6.12"
-  sha256 "3f30394349b963b3bc7164a7341743667c5d9a5c9e1da2d2b18ac25506a7c508"
+  version "2.6.13"
 
-  url "https://openphdguiding.org/PHD2-#{version}-OSX-64.zip"
+  on_ventura :or_older do
+    sha256 "3d098cc6ccaa7ddf5040e51ca9f423647fe167948d7d92bece456fa207491b7f"
+
+    url "https://openphdguiding.org/PHD2-#{version}-OSX-64.zip"
+
+    livecheck do
+      url "https://openphdguiding.org/downloads/"
+      regex(/href=.*?PHD2[._-]v?(\d+(?:\.\d+)+)-OSX-64\.zip/i)
+    end
+  end
+  on_sonoma :or_newer do
+    sha256 "c8ea80bfaa57d7092e7b8ce67f6b905a768bf0b9e810f3c1faa065072feace2d"
+
+    url "https://openphdguiding.org/PHD2-#{version}-OSX-64-sonoma+.zip"
+
+    livecheck do
+      url "https://openphdguiding.org/downloads/"
+      regex(/href=.*?PHD2[._-]v?(\d+(?:\.\d+)+)-OSX-64-sonoma\+\.zip/i)
+    end
+  end
+
   name "PHD2"
   desc "Telescope guiding software"
   homepage "https://openphdguiding.org/"
-
-  livecheck do
-    url "https://openphdguiding.org/changelog/"
-    regex(%r{href=.*?/PHD2-(\d+(?:\.\d+)*)-OSX-64\.zip}i)
-  end
 
   app "PHD2.app"
 end
