cbc00f9933f1a8db80a9433dc582b7e45654d78e
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-12-30 13:17:30 +0200

    openthesaurus-deutsch: modify download url and livecheck

--- Casks/o/openthesaurus-deutsch.rb
+++ Casks/o/openthesaurus-deutsch.rb
@@ -1,15 +1,16 @@
 cask "openthesaurus-deutsch" do
   version "2023.12.28"
-  sha256 :no_check
+  sha256 "bc28b0ad3c2d64c40ffd13f72061109dd5b01e26a1a76af5a412005f0b31eb11"
 
-  url "https://downloads.tekl.de/OpenThesaurus_Deutsch_dictionaryfile.zip"
+  url "https://github.com/Tekl/openthesaurus-deutsch/releases/download/v#{version}/OpenThesaurus_Deutsch.dmg",
+      verified: "github.com/Tekl/openthesaurus-deutsch/"
   name "OpenThesaurus Deutsch Dictionary plugin"
   desc "Extends Apples Dictionary App with a German thesaurus"
   homepage "https://tekl.de/lexikon-plug-ins/openthesaurus-deutsch-lexikon-plugin"
 
   livecheck do
-    url :homepage
-    regex(%r{<em>\s*v(\d\d\d\d.\d\d.\d\d)\s*</em>}i)
+    url :url
+    strategy :github_latest
   end
 
   depends_on macos: ">= :el_capitan"
