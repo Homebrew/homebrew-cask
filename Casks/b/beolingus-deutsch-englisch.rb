c9446e519328ecd441a8f834992489dc84291905
Wolfgang Kreutz <support@tekl.de>
2023-12-30 15:23:02 +0100

    Update beolingus-deutsch-englisch.rb

--- Casks/b/beolingus-deutsch-englisch.rb
+++ Casks/b/beolingus-deutsch-englisch.rb
@@ -1,15 +1,16 @@
 cask "beolingus-deutsch-englisch" do
   version "2023.12.28"
-  sha256 :no_check
+  sha256 "8075ef0f1316d3b2da36d3dc563b129eda4ae50c68117fe90b49a3aaa5e27fc9"
 
-  url "https://downloads.tekl.de/BeoLingus_Deutsch-Englisch_dictionaryfile.zip"
+  url "https://github.com/Tekl/beolingus-deutsch-englisch/releases/download/v#{version}/BeoLingus_Deutsch-Englisch_dictionaryfile.zip",
+      verified: "github.com/Tekl/beolingus-deutsch-englisch/"
   name "BeoLingus Deutsch-Englisch Dictionary plugin"
   desc "Extends Apples Dictionary App with a German-English dictionary"
   homepage "https://tekl.de/lexikon-plug-ins/beolingus-deutsch-englisch-lexikon-plugin"
 
   livecheck do
-    url :homepage
-    regex(%r{<em>\s*v(\d\d\d\d.\d\d.\d\d)\s*</em>}i)
+    url :url
+    strategy :github_latest
   end
 
   depends_on macos: ">= :el_capitan"
