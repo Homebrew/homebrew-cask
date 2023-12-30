9464826cf20cb47638ace11f2edea0f8d3932cea
Wolfgang Kreutz <support@tekl.de>
2023-12-30 12:36:13 +0100

    Update openthesaurus-deutsch.rb

--- Casks/o/openthesaurus-deutsch.rb
+++ Casks/o/openthesaurus-deutsch.rb
@@ -1,8 +1,8 @@
 cask "openthesaurus-deutsch" do
   version "2023.12.28"
-  sha256 "bc28b0ad3c2d64c40ffd13f72061109dd5b01e26a1a76af5a412005f0b31eb11"
+  sha256 "cb12ab497c71d0aa56cfa6ae2df4b5e5b9fb7eed424c35dd82400cc9ce66a9e5"
 
-  url "https://github.com/Tekl/openthesaurus-deutsch/releases/download/v#{version}/OpenThesaurus_Deutsch.dmg",
+  url "https://github.com/Tekl/openthesaurus-deutsch/releases/download/v#{version}/OpenThesaurus_Deutsch_dictionaryfile.zip",
       verified: "github.com/Tekl/openthesaurus-deutsch/"
   name "OpenThesaurus Deutsch Dictionary plugin"
   desc "Extends Apples Dictionary App with a German thesaurus"
