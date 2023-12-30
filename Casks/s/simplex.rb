9808ff66a339a393333d9350761310f7e781a916
Rui Chen <rui@chenrui.dev>
2023-12-30 09:05:37 -0500

    simplex 5.4.2

--- Casks/s/simplex.rb
+++ Casks/s/simplex.rb
@@ -1,9 +1,9 @@
 cask "simplex" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "5.4.1"
-  sha256 arm:   "d32443ce908968d018bb0ebb2c2e3a749150a9f5d19e40eac9242b4c77aa9ad7",
-         intel: "6b387fe27bd289efe20400efad3b662e7957da81c32540180d09faf0327fee5b"
+  version "5.4.2"
+  sha256 arm:   "52f857387808834e4dbc8d16ef911072d1e6a829a285fdd3e5567e86e7b536d0",
+         intel: "b43675b36be05ce638d6366d7341b4ef3aa2213468a57eadfc74d50742065510"
 
   url "https://github.com/simplex-chat/simplex-chat/releases/download/v#{version}/simplex-desktop-macos-#{arch}.dmg",
       verified: "github.com/simplex-chat/simplex-chat/"
