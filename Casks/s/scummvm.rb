31ba0e4427876ff19a909d2f9fcc6ff927583254
Mikl Wolfe <wolfemikl@gmail.com>
2023-12-30 16:49:10 -0700

    scummvm 2.8.0

--- Casks/s/scummvm.rb
+++ Casks/s/scummvm.rb
@@ -1,6 +1,6 @@
 cask "scummvm" do
-  version "2.7.1"
-  sha256 "d99d468b9606cfc331ac0db18a05501676356a0aa643c7417dd6b9b42df4cf60"
+  version "2.8.0"
+  sha256 "bd74ccdcf918347f430ea7a121b6cd78697eed8549c9d239452dc163e91c7ad0"
 
   url "https://downloads.scummvm.org/frs/scummvm/#{version}/scummvm-#{version}-macosx.dmg"
   name "ScummVM"
