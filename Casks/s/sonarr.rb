3f65bc8719c6a709f4d29db0e5d9e67cd2ae7e72
Alexander Regueiro <alexreg@gmail.com>
2023-12-31 01:33:02 +0000

    sonarr 4.0.0.741

--- Casks/s/sonarr.rb
+++ Casks/s/sonarr.rb
@@ -1,18 +1,22 @@
 cask "sonarr" do
-  version "3.0.10.1567"
-  sha256 "16566295630a50ba483b480a0dbe308f4fdfe55ad90487f5729d05860c1fcd7c"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://download.sonarr.tv/v#{version.major}/main/#{version}/Sonarr.main.#{version}.macos.zip"
+  version "4.0.0.741"
+  sha256 arm:   "f3fb5837df392d20e672fb79ff8c5f7004a0ff868be23e84bcf4d3e856f6f9c5",
+         intel: "8f1688ccad85eee915afd1572310f7848ef89f581295be334ef7d251263fea92"
+
+  url "https://download.sonarr.tv/v#{version.major}/main/#{version}/Sonarr.main.#{version}.osx-#{arch}-app.zip"
   name "Sonarr"
   desc "PVR for Usenet and BitTorrent users"
   homepage "https://sonarr.tv/"
 
   livecheck do
-    url "https://download.sonarr.tv/v3/main/"
+    url "https://download.sonarr.tv/v4/main/"
     regex(%r{href=["']?v?(\d+(?:\.\d+)+)/?["' >]}i)
   end
 
-  depends_on cask: "mono-mdk"
+  auto_updates true
+  depends_on macos: ">= :catalina"
 
   app "Sonarr.app"
 
