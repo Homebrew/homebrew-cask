a5aa5020b6bda026b42d2da44025335a5dafffbe
Rui Chen <rui@chenrui.dev>
2023-12-31 11:30:16 -0500

    fabfilter-micro: update download url
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/f/fabfilter-micro.rb
+++ Casks/f/fabfilter-micro.rb
@@ -2,7 +2,7 @@ cask "fabfilter-micro" do
   version "1.26"
   sha256 "595eff8f30ed0d81d27c4d2978b0a7b2614259c7e36af2edd9f1c47c982cc559"
 
-  url "https://download.fabfilter.com/ffmicro#{version.no_dots}.dmg"
+  url "https://www.fabfilter.com/downloads/ffmicro#{version.no_dots}.dmg"
   name "FabFilter Micro"
   desc "Filter plug-in"
   homepage "https://www.fabfilter.com/products/micro-mini-filter-plug-in"
