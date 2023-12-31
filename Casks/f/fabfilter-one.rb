5e37eaab39fda4527412d1f7623ec224b21fab2b
Rui Chen <rui@chenrui.dev>
2023-12-31 11:30:16 -0500

    fabfilter-one: update download url
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/f/fabfilter-one.rb
+++ Casks/f/fabfilter-one.rb
@@ -2,7 +2,7 @@ cask "fabfilter-one" do
   version "3.37"
   sha256 "4059594580e365237ded16a213d8d549cbb01c4b8bad80895c61f44bcff7eb68"
 
-  url "https://download.fabfilter.com/ffone#{version.no_dots}.dmg"
+  url "https://www.fabfilter.com/downloads/ffone#{version.no_dots}.dmg"
   name "FabFilter One"
   desc "Synthesizer plug-in"
   homepage "https://www.fabfilter.com/products/volcano-2-powerful-filter-plug-in"
