85c1bb07d8cf6a01419fa795af7c85625cb08bc0
Rui Chen <rui@chenrui.dev>
2023-12-31 11:30:17 -0500

    fabfilter-pro-ds: update download url
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/f/fabfilter-pro-ds.rb
+++ Casks/f/fabfilter-pro-ds.rb
@@ -2,7 +2,7 @@ cask "fabfilter-pro-ds" do
   version "1.21"
   sha256 "6a2b1fd70457e5d54f9575ba28a6593509594406e37a052bcad5e0d85ad57ba1"
 
-  url "https://download.fabfilter.com/ffprods#{version.no_dots}.dmg"
+  url "https://www.fabfilter.com/downloads/ffprods#{version.no_dots}.dmg"
   name "FabFilter Pro-DS"
   desc "De-esser plug-in"
   homepage "https://www.fabfilter.com/products/pro-ds-de-esser-plug-in"
