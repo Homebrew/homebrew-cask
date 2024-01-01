d6c76e7d40d678fc74a68b8a02d3e426144a41bb
Rui Chen <rui@chenrui.dev>
2024-01-01 10:01:48 -0500

    dcp-o-matic-editor 2.16.71

--- Casks/d/dcp-o-matic-editor.rb
+++ Casks/d/dcp-o-matic-editor.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-editor" do
-  version "2.16.70"
-  sha256 "538b1b69d497907811f6fe05d0d6ce82e191c975a24389f50694522ff1ea8691"
+  version "2.16.71"
+  sha256 "5bf632b2c75cbdd3453780f4807ace1ed7e934c8338eb4926cccb2f877eaa37a"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-editor&version=#{version}"
   name "DCP-o-matic Editor"
