8b18109c7a469497b6a68a7a26dc13249d7f93be
Rui Chen <rui@chenrui.dev>
2024-01-01 10:01:18 -0500

    dcp-o-matic-player 2.16.71

--- Casks/d/dcp-o-matic-player.rb
+++ Casks/d/dcp-o-matic-player.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-player" do
-  version "2.16.70"
-  sha256 "192843d850bc8aadc36332bb55d892662e87522fa28349b5437afd2bbe9a5c1d"
+  version "2.16.71"
+  sha256 "a3f8b42d98ea2097200d35f655be22a6895ee50d5633a6526b61cc042ffe932b"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-player&version=#{version}"
   name "DCP-o-matic Player"
