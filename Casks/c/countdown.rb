fbdf82061c387445a61066ff3fe79eef0ab9164d
Rui Chen <rui@chenrui.dev>
2024-01-01 15:06:54 -0500

    countdown: `disable` as it stopped working
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/c/countdown.rb
+++ Casks/c/countdown.rb
@@ -6,5 +6,11 @@ cask "countdown" do
   name "Countdown Screensaver"
   homepage "https://github.com/soffes/Countdown"
 
+  # no releases in the past 8 years, and no commits in the past 6 years
+  # users have reported that it stopped working
+  # https://github.com/soffes/Countdown/issues/8
+  # https://github.com/soffes/Countdown/issues/11
+  disable! date: "2024-01-01", because: :unmaintained
+
   screen_saver "Countdown.saver"
 end
