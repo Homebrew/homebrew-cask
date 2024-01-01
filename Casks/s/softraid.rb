a30a6304d54b29c4ea9dafd0398e498a69090012
Rui Chen <rui@chenrui.dev>
2024-01-01 17:17:03 -0500

    softraid: add `zap`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/s/softraid.rb
+++ Casks/s/softraid.rb
@@ -16,4 +16,15 @@ cask "softraid" do
   depends_on macos: ">= :catalina"
 
   app "SoftRAID #{version}/SoftRAID #{version}.app"
+
+  zap trash: [
+    "~/Library/Caches/com.apple.helpd/Generated/com.softraid.SoftRAID.help*#{version}/",
+    "~/Library/Caches/com.softraid.SoftRAID_AM_#{version.major}",
+    "~/Library/HTTPStorages/com.softraid.SoftRAIDMonitor/",
+    "~/Library/LaunchAgents/com.softraid.SoftRAIDMonitor.plist",
+    "~/Library/LaunchDaemons/com.OWC.HelperTool.RepairSRTool.plist",
+    "~/Library/LaunchDaemons/com.softraid.softtraidd.plist",
+    "~/Library/Preferences/com.softraid.SoftRAID.plist",
+    "~/Library/Preferences/com.softraid.SoftRAIDMonitor.plist",
+  ]
 end
