33efe71e78e702829db03c39d9042c71683fd5fe
Rui Chen <rui@chenrui.dev>
2024-01-01 16:50:26 -0500

    sqleditor: use http due to cert issue
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/s/sqleditor.rb
+++ Casks/s/sqleditor.rb
@@ -2,13 +2,13 @@ cask "sqleditor" do
   version "4.0.3"
   sha256 "c737c67ad57a2c9be0168a5c7466a35e71669ed0ec908cd09cb678bc41db3fea"
 
-  url "https://www.malcolmhardie.com/sqleditor/releases/#{version}/SQLEditor-#{version.dots_to_hyphens}.zip"
+  url "http://www.malcolmhardie.com/sqleditor/releases/#{version}/SQLEditor-#{version.dots_to_hyphens}.zip"
   name "SQLEditor"
   desc "SQL database design tool"
-  homepage "https://www.malcolmhardie.com/sqleditor/"
+  homepage "http://www.malcolmhardie.com/sqleditor/"
 
   livecheck do
-    url "https://www.malcolmhardie.com/sqleditor/appcast/sq2release.xml"
+    url "http://www.malcolmhardie.com/sqleditor/appcast/sq2release.xml"
     strategy :sparkle, &:short_version
   end
 
