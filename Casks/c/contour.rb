c12579884dc8164cf18942aadd8f4daf131b14f6
Rui Chen <rui@chenrui.dev>
2024-01-01 10:19:16 -0500

    contour: app name `Contour.app` -> `contour.app`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/c/contour.rb
+++ Casks/c/contour.rb
@@ -3,17 +3,17 @@ cask "contour" do
   sha256 "c168263e9e50ece9b9f4e4168143f66bbc7c6b0a8b7392020a192c54dcf24a8d"
 
   url "https://github.com/contour-terminal/contour/releases/download/v#{version}/contour-#{version}-osx.dmg"
-  name "Contour"
+  name "contour"
   desc "Terminal emulator"
   homepage "https://github.com/contour-terminal/contour/"
 
-  app "Contour.app"
-  binary "Contour.app/Contents/MacOS/contour"
-  binary "Contour.app/Contents/Resources/shell-integration/shell-integration.zsh",
+  app "contour.app"
+  binary "contour.app/Contents/MacOS/contour"
+  binary "contour.app/Contents/Resources/shell-integration/shell-integration.zsh",
          target: "#{HOMEBREW_PREFIX}/share/zsh/site-functions/_contour"
-  binary "Contour.app/Contents/Resources/terminfo/63/contour",
+  binary "contour.app/Contents/Resources/terminfo/63/contour",
          target: "#{ENV.fetch("TERMINFO", "~/.terminfo")}/63/contour"
-  binary "Contour.app/Contents/Resources/terminfo/63/contour-latest",
+  binary "contour.app/Contents/Resources/terminfo/63/contour-latest",
          target: "#{ENV.fetch("TERMINFO", "~/.terminfo")}/63/contour-latest"
 
   zap trash: "~/.config/contour"
