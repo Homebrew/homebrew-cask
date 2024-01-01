7da93859998f67d51a59c782f123e279ccdc3bf9
Rui Chen <rui@chenrui.dev>
2024-01-01 13:37:02 -0500

    restream-chat: update artifact url and livecheck
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/r/restream-chat.rb
+++ Casks/r/restream-chat.rb
@@ -2,15 +2,14 @@ cask "restream-chat" do
   version "2.5.4-beta"
   sha256 "32b4dae6cdce145b1f6a43b5046636bfa2188c924d792b21337c9604d3dc1809"
 
-  url "https://s3.eu-central-1.amazonaws.com/restream-chat-client/Restream+Chat-#{version}.dmg",
-      verified: "s3.eu-central-1.amazonaws.com/restream-chat-client/"
+  url "https://chat-client.restream.io/Restream+Chat-#{version}.dmg"
   name "Restream Chat"
   desc "Keep your streaming chats in one place"
   homepage "https://restream.io/chat/"
 
   livecheck do
-    url "https://s3.eu-central-1.amazonaws.com/restream-chat-client/beta-mac.yml"
-    strategy :electron_builder
+    url "https://website-backend.restream.io/v2/public/chat/download/mac"
+    strategy :header_match
   end
 
   app "Restream Chat.app"
