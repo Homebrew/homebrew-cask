ea3b0f8a1058cb54da2b60d13bcfcbdb6d73e5bd
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-01-02 14:32:32 -0500

    ishowu-instant: add `auto_update` and `zap`

--- Casks/i/ishowu-instant.rb
+++ Casks/i/ishowu-instant.rb
@@ -12,7 +12,19 @@ cask "ishowu-instant" do
     strategy :sparkle, &:short_version
   end
 
+  auto_updates true
   depends_on macos: ">= :mojave"
 
   app "iShowU Instant.app"
+
+  zap trash: [
+    "~/Library/Application Support/iShowU Instant",
+    "~/Library/Application Support/iShowU Studio",
+    "~/Library/Caches/com.shinywhitebox.iShowU-Instant",
+    "~/Library/HTTPStorages/com.shinywhitebox.iShowU-Instant",
+    "~/Library/HTTPStorages/com.shinywhitebox.iShowU-Instant.binarycookies",
+    "~/Library/Preferences/com.shinywhitebox.iShowU-Instant.plist",
+    "~/Library/Saved Application State/com.shinywhitebox.iShowU-Instant.savedState",
+    "~/Library/WebKit/com.shinywhitebox.iShowU-Instant",
+  ]
 end
