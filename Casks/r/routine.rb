55e4bdf59d302ac26f3cdfc1a76ae247918bcd66
Mikl Wolfe <wolfemikl@gmail.com>
2024-01-02 12:51:52 -0700

    routine 0.17.4

--- Casks/r/routine.rb
+++ Casks/r/routine.rb
@@ -1,9 +1,9 @@
 cask "routine" do
   arch arm: "-arm64"
 
-  version "0.17.3"
-  sha256 arm:   "c77454ebc2f1115156a5cc488ded96fe34a464e67c7d4ee9d88de611aa5d2bf4",
-         intel: "2daa06c281185e7d38d74311303f12486d4b82ba6d1cc1fbf30f6bb3a3876398"
+  version "0.17.4"
+  sha256 arm:   "b1c16f7ca153ce5324d9cc0455dd859d61dae1c451a9aad9bafd113130b02819",
+         intel: "e81652d0e87b729772e436dfc24fdbfbcba8b2e013720a3ef62a7006a18537b4"
 
   url "https://releases.routine.co/routine/osx#{arch}/Routine-#{version}.zip"
   name "Routine"
