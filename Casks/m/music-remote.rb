09c6056dea3152e96c318f6e833d1c6596ce7c82
Rui Chen <rui@chenrui.dev>
2024-01-01 19:00:37 -0500

    music-remote: update url
    ```
    ==> Downloading https://marioaguzman.github.io/musicremote/updater/Music%20Remote.zip
    curl: (22) The requested URL returned error: 404
    Error: Download failed on Cask 'music-remote' with message: Download failed: https://marioaguzman.github.io/musicremote/updater/Music%20Remote.zip
    ```
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/m/music-remote.rb
+++ Casks/m/music-remote.rb
@@ -2,7 +2,7 @@ cask "music-remote" do
   version "1.2"
   sha256 :no_check
 
-  url "https://marioaguzman.github.io/musicremote/updater/Music%20Remote.zip"
+  url "https://marioaguzman.github.io/musicremote/updater/Music%20Remote.dmg"
   name "Music Remote"
   desc "Remote application for Music.app"
   homepage "https://marioaguzman.github.io/musicremote/"
