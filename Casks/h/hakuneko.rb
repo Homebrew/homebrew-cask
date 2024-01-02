5c865207ad4b16788ec9829d80dc17e9b2c06891
Rui Chen <rui@chenrui.dev>
2024-01-02 10:47:29 -0500

    hakuneko: add `verified` for homepage change
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/h/hakuneko.rb
+++ Casks/h/hakuneko.rb
@@ -2,7 +2,8 @@ cask "hakuneko" do
   version "6.1.7"
   sha256 "5ee455ce2f9bcebdf598182dccb46dbf66637d78920fce6267b30e7db170f987"
 
-  url "https://github.com/manga-download/hakuneko/releases/download/v#{version}/hakuneko-desktop_#{version}_macos_amd64.dmg"
+  url "https://github.com/manga-download/hakuneko/releases/download/v#{version}/hakuneko-desktop_#{version}_macos_amd64.dmg",
+      verified: "github.com/manga-download/hakuneko/"
   name "HakuNeko"
   desc "Manga and anime downloader and reader"
   homepage "https://hakuneko.download/"
