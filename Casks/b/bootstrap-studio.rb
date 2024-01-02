7ae0dd04e7980fce3179ecbcdb67c8ed89957516
Miccal Matthews <miccal.matthews@gmail.com>
2024-01-03 06:02:01 +0800

    Update bootstrap-studio.rb

--- Casks/b/bootstrap-studio.rb
+++ Casks/b/bootstrap-studio.rb
@@ -12,7 +12,7 @@ cask "bootstrap-studio" do
 
   livecheck do
     url "https://bootstrapstudio.io/pages/releases/"
-    regex(/Version\s*(\d+(?:\.\d+)+)/i)
+    regex(/Version\s*v?(\d+(?:\.\d+)+)/i)
   end
 
   auto_updates true
@@ -22,13 +22,13 @@ cask "bootstrap-studio" do
 
   zap trash: [
     "~/Library/Application Support/bstudio",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/bg.zine.bootstrapstudio.sfl*",
     "~/Library/Application Support/CrashReporter/Bootstrap Studio*",
     "~/Library/Caches/bg.zine.bootstrapstudio",
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/bg.zine.bootstrapstudio.sfl2",
     "~/Library/Caches/bg.zine.bootstrapstudio.ShipIt",
-    "~/Library/Preferences/ByHost/bg.zine.bootstrapstudio.ShipIt.*",
     "~/Library/Preferences/bg.zine.bootstrapstudio.helper.plist",
     "~/Library/Preferences/bg.zine.bootstrapstudio.plist",
+    "~/Library/Preferences/ByHost/bg.zine.bootstrapstudio.ShipIt.*",
     "~/Library/Saved Application State/bg.zine.bootstrapstudio.savedState",
   ]
 end
