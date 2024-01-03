ed391bc6e995069624f2e150b9574ea3422769c4
owine <owine@users.noreply.github.com>
2024-01-03 14:28:43 -0600

    google-chrome 120.0.6099.199

--- Casks/g/google-chrome.rb
+++ Casks/g/google-chrome.rb
@@ -1,5 +1,5 @@
 cask "google-chrome" do
-  version "120.0.6099.129"
+  version "120.0.6099.199"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
