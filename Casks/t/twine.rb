50780d1bfbb1306ec754a91d75414cb2987101a1
Rui Chen <rui@chenrui.dev>
2024-01-04 00:15:45 -0500

    twine 2.8.1

--- Casks/t/twine.rb
+++ Casks/t/twine.rb
@@ -1,6 +1,6 @@
 cask "twine" do
-  version "2.8.0"
-  sha256 "dbe533f806607300c80414bd45494239797d658f3af288be361c58b5de9cd4f2"
+  version "2.8.1"
+  sha256 "c1f78f6d74e3ed121d586687b8f489cb4db0cca8b63847006e9b0d002064eff7"
 
   url "https://github.com/klembot/twinejs/releases/download/#{version}/Twine-#{version}-macos.dmg",
       verified: "github.com/klembot/twinejs/"
