e013bb403abc945a411e02900881a019418b5194
Tomy Hsieh <git@tomy.me>
2024-01-05 04:02:56 +0800

    Add WakaTime.app 5.0.0

---
+++ Casks/w/wakatime.rb
@@ -0,0 +1,26 @@
+cask "wakatime" do
+  version "5.0.0"
+  sha256 "8c59f4c9d5f7c4ca1797b3e4fabc4c03c78e8c7283603de80cfd3941cdd12a37"
+
+  url "https://github.com/wakatime/macos-wakatime/releases/download/v#{version}/macos-wakatime.zip",
+      verified: "github.com/wakatime/macos-wakatime/"
+  name "Wakatime"
+  desc "System tray app for automatic time tracking"
+  homepage "https://wakatime.com/mac"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :catalina"
+
+  app "WakaTime.app"
+
+  zap trash: [
+    "~/Library/Application Support/macos-wakatime.WakaTime",
+    "~/Library/Caches/macos-wakatime.WakaTime",
+    "~/Library/HTTPStorages/macos-wakatime.WakaTime",
+    "~/Library/Preferences/macos-wakatime.WakaTime.plist",
+  ]
+end
