d41b7dd26c49d34622bf3a20fce7ae6b585eb69d
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-01-04 14:51:00 -0500

    tmpdisk 2.0.7

--- Casks/t/tmpdisk.rb
+++ Casks/t/tmpdisk.rb
@@ -1,6 +1,6 @@
 cask "tmpdisk" do
-  version "2.0.6"
-  sha256 "11f279ac2d142be90f4042a92f4f7da4b437bed80ac6963ecf68fc83524dd0b5"
+  version "2.0.7"
+  sha256 "91f8389fa88192e9edda8880643d4b9994ab7bc6b1ad99981a52ea7535325eea"
 
   url "https://github.com/imothee/tmpdisk/releases/download/v#{version}/TmpDisk.dmg"
   name "TmpDisk"
