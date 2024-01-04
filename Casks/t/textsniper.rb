71545dee2c407bb12a141812a385eace7ec0b195
Aaron Rumpler <80610725+Aaron-Rumpler@users.noreply.github.com>
2024-01-05 08:25:36 +1300

    textsniper: change to Gumroad version

--- Casks/t/textsniper.rb
+++ Casks/t/textsniper.rb
@@ -1,8 +1,8 @@
 cask "textsniper" do
   version "1.10.1"
-  sha256 "a553dd9fdf54b8a12950db71519cf3aee6aa7b18d590d32813b71c2e1bc6a010"
+  sha256 "3542eedfa1a1d15b8d5351d7fee0ae28451d0b3d01d474995f214bf675eb76d2"
 
-  url "https://s3.amazonaws.com/textsniper.app/TextSniper#{version}.dmg",
+  url "https://s3.amazonaws.com/textsniper.app/Gumroad/TextSniper#{version}.dmg",
       verified: "s3.amazonaws.com/textsniper.app/"
   name "TextSniper"
   desc "Extract text from images and other digital documents in seconds"
@@ -18,15 +18,18 @@ cask "textsniper" do
 
   app "TextSniper.app"
 
-  uninstall  launchctl: "com.valerijs.boguckis.TextSniper-LaunchAtLoginHelper",
-             quit:      "com.valerijs.boguckis.TextSniper",
+  uninstall  launchctl: "com.valerijs.boguckis.gumroad.TextSniper-LaunchAtLoginHelper",
+             quit:      "com.valerijs.boguckis.gumroad.TextSniper",
              delete:    "/Applications/TextSniper.app"
 
   zap trash: [
-    "~/Library/Application Scripts/com.valerijs.boguckis.TextSniper-LaunchAtLoginHelper",
-    "~/Library/Application Support/com.valerijs.boguckis.TextSniper",
-    "~/Library/Caches/com.valerijs.boguckis.TextSniper",
-    "~/Library/Containers/com.valerijs.boguckis.TextSniper-LaunchAtLoginHelper",
-    "~/Library/Preferences/com.valerijs.boguckis.TextSniper.plist",
+    "~/Library/Application Scripts/com.valerijs.boguckis.gumroad.TextSniper-LaunchAtLoginHelper",
+    "~/Library/Application Support/com.valerijs.boguckis.gumroad.TextSniper",
+    "~/Library/Application Support/TextSniper",
+    "~/Library/Caches/com.valerijs.boguckis.gumroad.TextSniper",
+    "~/Library/Containers/com.valerijs.boguckis.gumroad.TextSniper-LaunchAtLoginHelper",
+    "~/Library/Preferences/com.valerijs.boguckis.gumroad.TextSniper.plist",
   ]
+
+  caveats "Older licence keys (those issued through Paddle) won't work with this version of #{token}. Download #{token} from #{homepage}/download instead."
 end
