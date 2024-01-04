247a13f1b72f9ca99a48c5d20fc53f991e35ded8
Rui Chen <rui@chenrui.dev>
2024-01-04 00:15:40 -0500

    screens-connect 5.0.6

--- Casks/s/screens-connect.rb
+++ Casks/s/screens-connect.rb
@@ -1,6 +1,6 @@
 cask "screens-connect" do
-  version "5.0.5,21743"
-  sha256 "657e66fed48bfcad2ff43b58e79e3171c061152169be2d4f8c6584b747825aa3"
+  version "5.0.6,21745"
+  sha256 "7473b6758230df6926326ec756b8bbe7397786e8c556db931c0138a94382d72b"
 
   url "https://updates.edovia.com/com.edovia.screens.connect.mac/ScreensConnect_#{version.csv.first}b#{version.csv.second}.zip"
   name "Screens Connect"
