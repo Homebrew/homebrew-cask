6f96d2814e269f59eff459be6d35e1c6f209c815
Rui Chen <rui@chenrui.dev>
2024-01-04 00:36:09 -0500

    lark 7.8.8

--- Casks/l/lark.rb
+++ Casks/l/lark.rb
@@ -2,12 +2,12 @@ cask "lark" do
   arch arm: "arm64", intel: "x64"
 
   on_arm do
-    version "7.7.8,6fca309d"
-    sha256 "61f73c06708e0cd0e397305a858691755eab99841c7ed5f23829c36a9a021bfd"
+    version "7.8.8,c6c14a01"
+    sha256 "e64585e00158100e6a6239016d360fbb69ebe41d10b34dba3c8f9e54298cf7e3"
   end
   on_intel do
-    version "7.7.8,3fb4233a"
-    sha256 "6ae1461f1209081acab45c00ed5483a27a3228f22d807b3a90a1b14aa1ffa4ac"
+    version "7.8.8,e3b6ab38"
+    sha256 "c28228469623593069873adbe9acdc0f268c615561e2e034035c7152ed512ac6"
   end
 
   url "https://sf16-va.larksuitecdn.com/obj/lark-artifact-storage/#{version.csv.second}/Lark-darwin_#{arch}-#{version.csv.first}-signed.dmg",
