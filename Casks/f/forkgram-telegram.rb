c5524c8fdffbdc82ec702f3516a14ba2e195ece9
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-01-04 14:57:13 -0500

    forkgram-telegram 4.14.3

--- Casks/f/forkgram-telegram.rb
+++ Casks/f/forkgram-telegram.rb
@@ -1,9 +1,9 @@
 cask "forkgram-telegram" do
   arch arm: "arm64", intel: "x86"
 
-  version "4.14.2"
-  sha256 arm:   "46d4c7fc095ce09b59b45f67c48157a4b58c669ca43aaf82b9c8287a2fd51875",
-         intel: "37b524f1b4fe44fc59eff11c8f6a5c84239505ec94a70d5854122b6b3b3059b3"
+  version "4.14.3"
+  sha256 arm:   "030dafbd1612257051667b9d2ac1bc7fe2fc9129ffe0d81a7f1a7faf6bad0dbe",
+         intel: "1251316434976727e64df2f1632f483f29dfabd04f6072f663970d0674092546"
 
   url "https://github.com/Forkgram/tdesktop/releases/download/v#{version}/Forkgram.macOS.no.auto-update_#{arch}.zip"
   name "Forkgram"
