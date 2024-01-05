77f1f347a05451178cc925195ab2bea378fc5aa7
Rui Chen <rui@chenrui.dev>
2024-01-05 09:54:32 -0500

    clickhouse 23.12.2.59-stable

--- Casks/c/clickhouse.rb
+++ Casks/c/clickhouse.rb
@@ -1,9 +1,9 @@
 cask "clickhouse" do
   arch arm: "-aarch64"
 
-  version "23.12.1.1368-stable"
-  sha256 arm:   "98fca434c2408ddce278750ab48ca7d6ebaaa9ebeef77b0843d24659ac4fee1e",
-         intel: "f2ffff7729e20a84c5833d82ef7b058a1a999f2772562682813296b5282cbb13"
+  version "23.12.2.59-stable"
+  sha256 arm:   "30bcae2490fb5d9cdfd52ba9b25c4ad12efacc8882a6676a6a994e651c5aedee",
+         intel: "52c6f05a051a1c4b6fed28c3c8a54c44113c5681f18de4675c2368e086f8bcad"
 
   url "https://github.com/ClickHouse/ClickHouse/releases/download/v#{version}/clickhouse-macos#{arch}",
       verified: "github.com/ClickHouse/ClickHouse/"
