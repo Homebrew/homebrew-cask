ee07bb7fc670cf50bd5ba88b05bde42846aa0bd1
Nikolas Evers <vintagesucks@users.noreply.github.com>
2024-01-05 21:08:30 +0100

    simple-comic 1.9.8

--- Casks/s/simple-comic.rb
+++ Casks/s/simple-comic.rb
@@ -1,8 +1,8 @@
 cask "simple-comic" do
-  version "1.9.7"
-  sha256 "77957b74391ca924b2e4631b8407bdf20d44a97401606dee4cba84d0d5552572"
+  version "1.9.8"
+  sha256 "4346ad5fe036ce7f37831d9a7e1e4ee4a1cd3f2fae64b92ca9a0cd028f95d545"
 
-  url "https://github.com/MaddTheSane/Simple-Comic/releases/download/App-Store-#{version}/Simple.Comic.zip"
+  url "https://github.com/MaddTheSane/Simple-Comic/releases/download/App-Store-#{version}/Simple.Comic.#{version}.zip"
   name "Simple Comic"
   desc "Comic viewer/reader"
   homepage "https://github.com/MaddTheSane/Simple-Comic"
