f4a309ae02047a9d114f3d7cf1d7a6fb44126f01
andre4ik3 <62390580+andre4ik3@users.noreply.github.com>
2024-01-06 16:17:48 +0400

    Add iA Presenter

---
+++ Casks/i/ia-presenter.rb
@@ -0,0 +1,24 @@
+cask "ia-presenter" do
+  version "1.1.2-11002"
+  sha256 "ce026bfc2155104e68e5eeb9eaa29d0a279e7c5de7771ea55e19ff5a4ea2740e"
+
+  url "https://files.ia.net/presenter/release/iA-Presenter-#{version}.zip"
+  name "ia-presenter"
+  desc "Create presentation slides from a Markdown document"
+  homepage "https://ia.net/presenter"
+
+  livecheck do
+    url "https://iapresenter.s3.amazonaws.com/release/updates.xml"
+    strategy :sparkle do |item|
+      "#{item.short_version}-#{item.version}"
+    end
+  end
+
+  depends_on macos: ">= :monterey"
+  app "iA Presenter.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/net.ia.presenter",
+    "~/Library/Containers/net.ia.presenter",
+  ]
+end
