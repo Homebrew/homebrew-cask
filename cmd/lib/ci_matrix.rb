bb1e02337bce482c9e5e8a560d11d4f7e86f4043
Markus Reiter <me@reitermark.us>
2024-01-06 12:00:12 +0100

    Fix code style.

--- cmd/lib/ci_matrix.rb
+++ cmd/lib/ci_matrix.rb
@@ -144,9 +144,7 @@ module CiMatrix
 
       audit_exceptions = []
 
-      if labels.include?("ci-skip-homepage")
-        audit_exceptions << %w[homepage_https_availability]
-      end
+      audit_exceptions << %w[homepage_https_availability] if labels.include?("ci-skip-homepage")
 
       if labels.include?("ci-skip-livecheck")
         audit_exceptions << %w[hosting_with_livecheck livecheck_https_availability
