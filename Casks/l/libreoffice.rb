6ace13a091a136cc81d478c481d141ef0bfebb8d
NamelessGO <66227691+NameLessGO@users.noreply.github.com>
2024-01-06 20:43:27 -0300

    "cask name shouldn't be the first word in desc" fixed

--- Casks/l/libreoffice.rb
+++ Casks/l/libreoffice.rb
@@ -9,7 +9,7 @@ cask "libreoffice" do
   url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/#{folder}/LibreOffice_#{version}_MacOS_#{arch}.dmg",
       verified: "download.documentfoundation.org/libreoffice/stable/"
   name "LibreOffice"
-  desc "LibreOffice is the successor to OpenOffice, a free and open-source office suite"
+  desc "Successor to OpenOffice, LibreOffice is a free and open-source office suite"
   homepage "https://www.libreoffice.org/"
 
   livecheck do
