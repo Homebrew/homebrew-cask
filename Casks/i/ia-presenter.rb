8440038fdbf9faee1fb0711649ecf59a1589fb80
andre4ik3 <62390580+andre4ik3@users.noreply.github.com>
2024-01-07 21:18:11 +0400

    Make version comma separated

--- Casks/i/ia-presenter.rb
+++ Casks/i/ia-presenter.rb
@@ -1,5 +1,5 @@
 cask "ia-presenter" do
-  version "1.1.2-11002"
+  version "1.1.2,11002"
   sha256 "ce026bfc2155104e68e5eeb9eaa29d0a279e7c5de7771ea55e19ff5a4ea2740e"
 
   url "https://files.ia.net/presenter/release/iA-Presenter-#{version.csv.first}-#{version.csv.second}.zip"
