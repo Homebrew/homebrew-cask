a98c6ca2f3587a320ecb62b47da461ca7e068b75
andre4ik3 <62390580+andre4ik3@users.noreply.github.com>
2024-01-07 21:15:01 +0400

    Make requested changes

--- Casks/i/ia-presenter.rb
+++ Casks/i/ia-presenter.rb
@@ -2,16 +2,14 @@ cask "ia-presenter" do
   version "1.1.2-11002"
   sha256 "ce026bfc2155104e68e5eeb9eaa29d0a279e7c5de7771ea55e19ff5a4ea2740e"
 
-  url "https://files.ia.net/presenter/release/iA-Presenter-#{version}.zip"
-  name "ia-presenter"
+  url "https://files.ia.net/presenter/release/iA-Presenter-#{version.csv.first}-#{version.csv.second}.zip"
+  name "iA Presenter"
   desc "Create presentation slides from a Markdown document"
   homepage "https://ia.net/presenter"
 
   livecheck do
     url "https://iapresenter.s3.amazonaws.com/release/updates.xml"
-    strategy :sparkle do |item|
-      "#{item.short_version}-#{item.version}"
-    end
+    strategy :sparkle
   end
 
   auto_updates true
