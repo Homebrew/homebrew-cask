1c0e1dba7a02db9df36badac1df36c74a112e5c8
JaminYe <JaminYe2018@outlook.com>
2024-01-07 23:35:34 +0800

    add another arch name,update livecheck use page_match to get the latest

--- Casks/p/prettyclean.rb
+++ Casks/p/prettyclean.rb
@@ -1,11 +1,12 @@
 cask "prettyclean" do
   arch arm: "aarch64", intel: "x86_64"
+  framework = on_arch_conditional arm: "aarch64", intel: "x64"
 
   version "0.1.38"
   sha256 arm:   "1832d6ffdbe0fcbffe35c3b2f6ac4031e7e8ce93a7c564124914888ebfb43885",
          intel: "bc7512ff7544d8522ebd1ac0c13e94da8661dd6a5772bda33d68a9145be73436"
 
-  url "https://downloads.jmotor.org/prettyclean/v#{version}/darwin-#{arch}/PrettyClean_#{version}_#{arch.gsub("86_", "")}.dmg",
+  url "https://downloads.jmotor.org/prettyclean/v#{version}/darwin-#{arch}/PrettyClean_#{version}_#{framework}.dmg",
       verified: "downloads.jmotor.org/"
   name "PrettyClean"
   desc "Easy to use Disk Cleanup Tools"
@@ -13,7 +14,13 @@ cask "prettyclean" do
 
   livecheck do
     url "https://www.prettyclean.cc/en/download"
-    regex(/href=.*?PrettyClean[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}\.dmg/i)
+    regex(/href=.*?PrettyClean[._-]v?(\d+(?:\.\d+)+)[._-]#{framework}\.dmg/i)
+    strategy :page_match do |page, regex|
+      match = page.match(regex)
+      next if match.blank?
+
+      match[1]
+    end
   end
 
   depends_on macos: ">= :high_sierra"
