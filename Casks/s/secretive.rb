58d51c19ae14c80132a72ea92732d15518faf8ed
Rui Chen <rui@chenrui.dev>
2024-01-07 22:31:04 -0500

    secretive 2.4.0

--- Casks/s/secretive.rb
+++ Casks/s/secretive.rb
@@ -8,8 +8,8 @@ cask "secretive" do
     end
   end
   on_big_sur :or_newer do
-    version "2.3.1"
-    sha256 "493a72362898b4480baa70f115d9515b41b2af4a503caf00277e2bc3824b0bbd"
+    version "2.4.0"
+    sha256 "6441b6be03e778c23143120fae7d0c0fa002bf711483842f47d2f87bd5e18e10"
 
     livecheck do
       url :url
