4ec5b7e464fda75cedfec70a9189782c6fabda7f
Patrick Linnane <patrick@linnane.io>
2024-01-08 10:21:37 -0800

    unite: alphabetize `zap` stanza
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/u/unite.rb
+++ Casks/u/unite.rb
@@ -19,14 +19,14 @@ cask "unite" do
   app "Unite.app"
 
   zap trash: [
-        "~/Library/Application Support/Unite",
-        "~/Library/Application Support/com.BZG.unite.*",
-        "~/Library/Preferences/com.BZG.unite.*",
         "~/Library/Application Scripts/group.BZG.unite.sharedData",
+        "~/Library/Application Support/com.BZG.unite.*",
+        "~/Library/Application Support/Unite",
         "~/Library/Caches/com.BZG.unite",
         "~/Library/Cookies/com.BZG.unite.3.binarycookies",
         "~/Library/Group Containers/group.BZG.unite.sharedData",
         "~/Library/HTTPStorages/com.BZG.unite*",
+        "~/Library/Preferences/com.BZG.unite.*",
         "~/Library/Saved Application State/com.BZG.unite*.savedState",
       ],
       rmdir: "/Users/Shared/Unite"
