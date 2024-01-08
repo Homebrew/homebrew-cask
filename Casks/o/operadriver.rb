0307110decbd893d8bcc278c3f1f08c4aebcd6c8
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-01-08 05:40:32 -0500

    operadriver 120.0.6099.200

--- Casks/o/operadriver.rb
+++ Casks/o/operadriver.rb
@@ -1,6 +1,6 @@
 cask "operadriver" do
-  version "119.0.6045.124"
-  sha256 "ab7f0782a1c9240ab469c7e498cac8eeb324c9320115d937c2e57fdc270452d2"
+  version "120.0.6099.200"
+  sha256 "998b99dcab3b476749fc970e86f47a8571329479937726179b3f0bfac4c94f06"
 
   url "https://github.com/operasoftware/operachromiumdriver/releases/download/v.#{version}/operadriver_mac64.zip"
   name "OperaChromiumDriver"
