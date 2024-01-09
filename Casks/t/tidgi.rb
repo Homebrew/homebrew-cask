a1cf89c5d3be297196222a8480c1275a4d5325f0
Rui Chen <rui@chenrui.dev>
2024-01-09 09:46:16 -0500

    tidgi 0.9.0

--- Casks/t/tidgi.rb
+++ Casks/t/tidgi.rb
@@ -1,9 +1,9 @@
 cask "tidgi" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.8.0"
-  sha256 arm:   "7b70a14879636522623e1e8a27ae81800bab0480ff1e2510ec679faba9a953e7",
-         intel: "c37a0d6a269386c8d7f2404250480131155bf91e7b6c7ef30013362b76dd51e5"
+  version "0.9.0"
+  sha256 arm:   "387babbdb122d4557f30f618af56431f848ce615a3d183f0cc82786747fb46be",
+         intel: "53a7c428f67cebef44f93e7ffb98193b57bf6d60ba370eb5d8518f7b8089b75a"
 
   url "https://github.com/tiddly-gittly/TidGi-Desktop/releases/download/v#{version}/TidGi-darwin-#{arch}-#{version}.zip"
   name "TidGi"
