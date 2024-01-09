c751aba34e8bc52348b8dbb06a9bff295f4ff8a1
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-01-09 17:22:34 -0500

    ovito 3.10.1

--- Casks/o/ovito.rb
+++ Casks/o/ovito.rb
@@ -1,9 +1,9 @@
 cask "ovito" do
   arch arm: "arm64", intel: "intel"
 
-  version "3.10.0"
-  sha256 arm:   "513c1e464671fa23691674eb9217a23312001f6105618d6dafdcde1ce0833b92",
-         intel: "7a89d0c4e998f841c07420a896f61b10ab236f7ae2a66e8ab95938f84d6508ba"
+  version "3.10.1"
+  sha256 arm:   "fed42dc1c176071a4d50af3806ff13bd8890607b260deb546bfd71561c7bc17b",
+         intel: "45b196cb615d05f67f66cf496147f715f0524a6ec79531e0284db2e0fe2ee263"
 
   url "https://www.ovito.org/download/master/ovito-basic-#{version}-macos-#{arch}.dmg"
   name "OVITO"
