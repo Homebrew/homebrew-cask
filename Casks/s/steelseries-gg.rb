fbe460eadb4c8394b73ba854a95a370a2fb3847e
Bevan Kay <email@bevankay.me>
2024-01-10 11:18:08 +1100

    steelseries-gg: update uninstall

--- Casks/s/steelseries-gg.rb
+++ Casks/s/steelseries-gg.rb
@@ -25,11 +25,8 @@ cask "steelseries-gg" do
               "com.steelseries.gg.uninstaller",
               "com.steelseries.SteelSeries-GG",
             ],
-            kext:      [
-              "com.steelseries.ssenext.driver",
-              "com.highpoint-tech.kext.HighPointIOP",
-              "com.highpoint-tech.kext.HighPointRR",
-            ],
+            kext:
+                       "com.steelseries.ssenext.driver",
             script:    {
               executable: "/Applications/SteelSeries GG/SteelSeries GG Uninstaller.app/Contents/Resources/Uninstall.sh",
               sudo:       true,
