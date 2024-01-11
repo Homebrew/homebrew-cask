6e3823ba1fe9df2ca88f8260447a61eb22a67e1c
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-01-11 12:03:57 -0500

    epilogue-playback 1.1.0

--- Casks/e/epilogue-playback.rb
+++ Casks/e/epilogue-playback.rb
@@ -1,11 +1,8 @@
 cask "epilogue-playback" do
-  arch arm: "arm64", intel: "amd64"
+  version "1.1.0"
+  sha256 "8f91e0f5311915e02e3cced3a62d4b0f536703172ed23507222b8943b9b1cba9"
 
-  version "1.0.3"
-  sha256 arm:   "75697c8c3ea9719c41101f62f5787bfb1465fad7e407835dc191280fecae8556",
-         intel: "d1f76509bfdccdf8a68fd8ef71eab20000c7bb5a8c3299ec71f9fba6df562a5b"
-
-  url "https://epilogue.nyc3.digitaloceanspaces.com/releases/software/Playback/version/#{version}/release/mac/Playback.#{arch}.dmg",
+  url "https://epilogue.nyc3.digitaloceanspaces.com/releases/software/Playback/version/#{version}/release/mac/Playback.dmg",
       verified: "epilogue.nyc3.digitaloceanspaces.com/releases/software/Playback/version/"
   name "Epilogue Playback"
   desc "Play and manage Game Boy cartridges on your computer"
