eaf43929a07f2f0cafb8a3721aebb8739039a1fa
Rui Chen <rui@chenrui.dev>
2024-01-10 23:48:25 -0500

    openaudible 4.0

--- Casks/o/openaudible.rb
+++ Casks/o/openaudible.rb
@@ -1,6 +1,6 @@
 cask "openaudible" do
-  version "3.9.7.1"
-  sha256 "0de3dc367a4046aea3e3f58e99e8e38188d353a1764055e03be7a0a9ec626dc9"
+  version "4.0"
+  sha256 "33c6491644f3d3e6d6b5caa82cdf65887795debc8742800f0912abbc19bf4554"
 
   url "https://github.com/openaudible/openaudible/releases/download/v#{version}/OpenAudible_#{version}.dmg",
       verified: "github.com/openaudible/openaudible/"
