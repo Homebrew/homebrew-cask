b971002834c1856a0f3758bffec6d01dd6e2ae5e
Rui Chen <rui@chenrui.dev>
2024-01-12 09:13:16 -0500

    postman-cli 1.7.1

--- Casks/p/postman-cli.rb
+++ Casks/p/postman-cli.rb
@@ -1,9 +1,9 @@
 cask "postman-cli" do
   arch arm: "osx_arm64", intel: "osx64"
 
-  version "1.7.0"
-  sha256 arm:   "ae7392f4e6d12b6d2e0e406db29ca6ea7dd0d9538ebe39434e9c12995799d228",
-         intel: "9e4cf63bba46fd4156c67c343211d876d5fda30792674fe4e054b7f0fa616ff6"
+  version "1.7.1"
+  sha256 arm:   "b55151baafd04085d7d1c56814916f76404c182cb4e88060e1414d85a2d924dd",
+         intel: "4f414d29588345b55ff88df992d82412af8e6e68800f0a667bc3e404c8b6e59c"
 
   url "https://dl-cli.pstmn.io/download/version/#{version}/#{arch}",
       verified: "dl-cli.pstmn.io/download/"
