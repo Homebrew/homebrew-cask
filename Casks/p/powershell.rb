5d03ce3401e2e5c606d1c7f499ddbc1799455aea
Oliver Jehle <ojehle@yahoo.com>
2024-01-12 06:05:33 +0100

    powershell 7.4.1

--- Casks/p/powershell.rb
+++ Casks/p/powershell.rb
@@ -1,9 +1,9 @@
 cask "powershell" do
   arch arm: "arm64", intel: "x64"
 
-  version "7.4.0"
-  sha256 arm:   "aa7f452a96aaf5a1260e50ed5888bd564bb2a5b1b0fda730542ec7831a730612",
-         intel: "08abedd85e9f3ed3a8c7acf43be6048ddc761d2c75c19f7bfdb7be7539e9f8c4"
+  version "7.4.1"
+  sha256 arm:   "fa6c4499fb36a851f72f6136ce2d6804dc5c50e5bdedbe0207b6fc60c68712f4",
+         intel: "6279b3e81406fd7b6124ff5afae211de1e6cce46568749874e4ff2bb64b78edc"
 
   url "https://github.com/PowerShell/PowerShell/releases/download/v#{version}/powershell-#{version}-osx-#{arch}.pkg"
   name "PowerShell"
