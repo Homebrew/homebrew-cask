fb10a188318fc5cf31eaac21211d25e86c84e54a
Philipp Klaushardt <klaushardt@users.noreply.github.com>
2024-01-12 22:39:11 +0100

    Updated sha256 for Shutter Encoder 17.8 Apple Silicon
    philipp@nix ~/$ wget https://www.shutterencoder.com/Shutter%20Encoder%2017.8%20Apple%20Silicon.pkg
    philipp@nix ~/$ shasum -a 256 Shutter\ Encoder\ 17.8\ Apple\ Silicon.pkg
    ac6dd034a423ac84aeee5cfe600336483313bd55e5ba0d94cfc465264a1c41b4  Shutter Encoder 17.8 Apple Silicon.pkg
    philipp@nix ~/ $ date
    Fr 12 Jan 2024 22:38:19 CET

--- Casks/s/shutter-encoder.rb
+++ Casks/s/shutter-encoder.rb
@@ -2,7 +2,7 @@ cask "shutter-encoder" do
   arch arm: "Apple Silicon", intel: "Mac 64bits"
 
   version "17.8"
-  sha256 arm:   "4cfa9d2b0a4b3c83e9bcd481dba9a609db9130df89fc653a6ba3c8f1ef1f03e6",
+  sha256 arm:   "ac6dd034a423ac84aeee5cfe600336483313bd55e5ba0d94cfc465264a1c41b4",
          intel: "8512788704f63fdcfd62f7985293c6aad732a2a7ffae7077b7a4d012516c02c6"
 
   url "https://www.shutterencoder.com/Shutter%20Encoder%20#{version}%20#{arch.gsub(" ", "%20")}.pkg"
