e280cadb2970661f9a88ce0c0e4b2db11d93474c
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-01-13 23:15:28 -0500

    znote: fix cask structure

--- Casks/z/znote.rb
+++ Casks/z/znote.rb
@@ -1,21 +1,14 @@
 cask "znote" do
+  arch arm: "-arm64"
+
   version "2.3.11"
+  sha256 arm:   "10413386d5b2e5f46ca7a1bb93a6e41d31be9a0b891f6024d1e5467dad621cf9",
+         intel: "34a92ed5fd66fcc53789681be80b5586356daaca053d96dc615b91067607fe1d"
 
-  on_arm do
-    sha256 "10413386d5b2e5f46ca7a1bb93a6e41d31be9a0b891f6024d1e5467dad621cf9"
-
-    url "https://github.com/alagrede/znote-app/releases/download/v#{version}/znote-#{version}-arm64.dmg",
-        verified: "github.com/alagrede/znote-app/"
-  end
-  on_intel do
-    sha256 "34a92ed5fd66fcc53789681be80b5586356daaca053d96dc615b91067607fe1d"
-
-    url "https://github.com/alagrede/znote-app/releases/download/v#{version}/znote-#{version}.dmg",
-        verified: "github.com/alagrede/znote-app/"
-  end
-
+  url "https://github.com/alagrede/znote-app/releases/download/v#{version}/znote-#{version}#{arch}.dmg",
+      verified: "github.com/alagrede/znote-app/"
   name "znote"
-  desc "Turn your notes into micro apps"
+  desc "Notes-taking app"
   homepage "https://znote.io/"
 
   livecheck do
@@ -26,6 +19,7 @@ cask "znote" do
   app "znote.app"
 
   zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.tony.znote.sfl3",
     "~/Library/Application Support/znote",
     "~/Library/Preferences/com.tony.znote.plist",
     "~/Library/Saved Application State/com.tony.znote.savedState",
