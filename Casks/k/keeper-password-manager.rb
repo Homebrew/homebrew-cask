c7ff082d65ec91e6c22b17fbeba4ad07c9c64708
Rui Chen <rui@chenrui.dev>
2024-01-15 16:31:38 -0500

    keeper-password-manager 16.10.12

--- Casks/k/keeper-password-manager.rb
+++ Casks/k/keeper-password-manager.rb
@@ -1,5 +1,5 @@
 cask "keeper-password-manager" do
-  version "16.10.11,17026842741143"
+  version "16.10.12,17048331511155"
   sha256 :no_check
 
   url "https://keepersecurity.com/desktop_electron/Darwin/KeeperSetup.dmg"
