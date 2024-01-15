8d49eaefc43bd78ac00a5c163e99beea41d40d39
Rui Chen <rui@chenrui.dev>
2024-01-15 09:50:19 -0500

    avast-secure-browser 120.0.4788.216

--- Casks/a/avast-secure-browser.rb
+++ Casks/a/avast-secure-browser.rb
@@ -1,5 +1,5 @@
 cask "avast-secure-browser" do
-  version "120.0.4766.199"
+  version "120.0.4788.216"
   sha256 :no_check
 
   url "https://cdn-download.avastbrowser.com/AvastSecureBrowserSetup.pkg",
