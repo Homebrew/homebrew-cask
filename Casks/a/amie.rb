7fbca57efdd2c10d7dff47d33e7466234e7d00ea
Rui Chen <rui@chenrui.dev>
2024-01-15 16:17:08 -0500

    amie 240115.0.0

--- Casks/a/amie.rb
+++ Casks/a/amie.rb
@@ -1,9 +1,9 @@
 cask "amie" do
   arch arm: "-arm64"
 
-  version "240111.0.0"
-  sha256 arm:   "f229e88e7fac7b74095eaf72df26f615dbf3d7e5be765f560f537d0e375ed7dd",
-         intel: "028226fe246d34e206715fcaa6121578e2d62c5838e9e9e95a018aa238d9aab0"
+  version "240115.0.0"
+  sha256 arm:   "a05092f496f4313840d2211423ea09365bb0a2575cb2885847b44399e42e357b",
+         intel: "c8be0a507bb4835691b358d58f7b81b8460914a7b84a973661be3310ee8ff315"
 
   url "https://github.com/amieso/electron-releases/releases/download/v#{version}/Amie-#{version}#{arch}-mac.zip",
       verified: "github.com/amieso/electron-releases/"
