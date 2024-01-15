a9c58fb1d06cc6191d822086a2947d002a6bc3f1
Rui Chen <rui@chenrui.dev>
2024-01-15 17:31:00 -0500

    hp-prime: update homepage
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/h/hp-prime.rb
+++ Casks/h/hp-prime.rb
@@ -5,7 +5,7 @@ cask "hp-prime" do
   url "https://ftp.hp.com/pub/calculators/Prime/HP_Prime_Virtual_Calculator_#{version.hyphens_to_underscores}.dmg"
   name "HP Prime"
   desc "Graphing calculator emulator"
-  homepage "https://www.hp.com/us-en/campaigns/prime-graphing-calculator/overview.html"
+  homepage "https://www.hp.com/us-en/calculators.html"
 
   livecheck do
     url "https://www.hpcalc.org/details/7799"
