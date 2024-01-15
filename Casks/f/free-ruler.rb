fcaa26560c113293c05be71a008b32e331371d04
Rui Chen <rui@chenrui.dev>
2024-01-15 17:20:11 -0500

    free-ruler: update homepage
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/f/free-ruler.rb
+++ Casks/f/free-ruler.rb
@@ -6,7 +6,7 @@ cask "free-ruler" do
       verified: "github.com/pascalpp/FreeRuler/"
   name "Free Ruler"
   desc "Horizontal and vertical rulers"
-  homepage "https://www.pascal.com/software/freeruler/"
+  homepage "https://www.pascal.com/freeruler"
 
   app "Free Ruler.app"
 
