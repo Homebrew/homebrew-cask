e78fafdbdae198cd1b9c3d845b484b06619b1294
Bevan Kay <email@bevankay.me>
2024-01-16 23:37:29 +1100

    plex 1.85.0.4071

--- Casks/p/plex.rb
+++ Casks/p/plex.rb
@@ -1,6 +1,6 @@
 cask "plex" do
-  version "1.84.1.4069,ff740b6d"
-  sha256 "734994e9d82d6d9a8262250b17b68cf3c978f4bae2b0c1b0c3baaeeaa5e54008"
+  version "1.85.0.4071,21128b56"
+  sha256 "1bfb1526e1577fe6a5da2c3bcb8d574d560b855c4e0eabe2233d6c0ef571bc90"
 
   url "https://downloads.plex.tv/plex-desktop/#{version.csv.first}-#{version.csv.second}/macos/Plex-#{version.csv.first}-#{version.csv.second}-universal.zip"
   name "Plex"
