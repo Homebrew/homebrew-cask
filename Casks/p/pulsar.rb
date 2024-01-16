4621b46b448bc16f5fe95f52a101a7b8e33f3f0e
Rui Chen <rui@chenrui.dev>
2024-01-16 11:15:32 -0500

    pulsar 1.113.0

--- Casks/p/pulsar.rb
+++ Casks/p/pulsar.rb
@@ -2,9 +2,9 @@ cask "pulsar" do
   arch arm: "Silicon", intel: "Intel"
   arch_suffix = on_arch_conditional arm: "-arm64"
 
-  version "1.112.1"
-  sha256 arm:   "15db385fade7f64661980fc5505a8eff28689b386e92b6e60ec8b93ab53a1d8d",
-         intel: "32c954aafde3614c56b2c5a05e630b6d2acd63058bcd77297fe2d1937e4c036b"
+  version "1.113.0"
+  sha256 arm:   "747395a9e45846de82781d4fc6cdaeeb97b2d9ab5405cfed3bab2c7947acd09f",
+         intel: "7d908edc8dec2fb27a923824af56aa5e5f8c761a81b819adc15664057214cdff"
 
   url "https://github.com/pulsar-edit/pulsar/releases/download/v#{version}/#{arch}.Mac.Pulsar-#{version}#{arch_suffix}-mac.zip",
       verified: "github.com/pulsar-edit/pulsar/"
