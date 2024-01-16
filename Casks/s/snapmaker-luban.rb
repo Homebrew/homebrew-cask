2a47dc7aca3c424ce75440b6986a098642570fe3
Rui Chen <rui@chenrui.dev>
2024-01-16 10:57:48 -0500

    snapmaker-luban 4.10.2

--- Casks/s/snapmaker-luban.rb
+++ Casks/s/snapmaker-luban.rb
@@ -1,9 +1,9 @@
 cask "snapmaker-luban" do
   arch arm: "-arm64", intel: "-x64"
 
-  version "4.10.1"
-  sha256 arm:   "4a401b17f5f67a5c16f567b32b880ebe4e3a9e2679396d48e5fbefaa08728c17",
-         intel: "ee6dab1e393d65e950250ea53868a5cd6c786fad518eaf9e61d0fcf454dc4a56"
+  version "4.10.2"
+  sha256 arm:   "c8e21cde3bbb4b75412c4256dc257bb4f13d1f131c7dd98b8592100255e650a2",
+         intel: "a82f50e12fd27b4f39154d8e12d52f141ccb4d04569a44c1495d7a09e28232b1"
 
   url "https://github.com/snapmaker/luban/releases/download/v#{version}/Snapmaker-Luban-#{version}-mac#{arch}.dmg",
       verified: "github.com/snapmaker/luban/"
