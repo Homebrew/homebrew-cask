833aeadc7036c783628014528b7280ad1ab52614
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-01-17 08:27:42 -0500

    trezor-suite 24.1.2

--- Casks/t/trezor-suite.rb
+++ Casks/t/trezor-suite.rb
@@ -1,9 +1,9 @@
 cask "trezor-suite" do
   arch arm: "arm64", intel: "x64"
 
-  version "23.12.3"
-  sha256 arm:   "1a7dc47e010f0c0f46e4fb5bd93643fd89e2331c45c3f074859981b5a740979f",
-         intel: "fc9251ef3b96acd736934a31ca47add144016f580239e5e2e02ad8520210370a"
+  version "24.1.2"
+  sha256 arm:   "a1779a43b9b1ff759e6edd0a311696eb607c5f945ab4f0198455e680f93e288a",
+         intel: "c6f313d0832740b2533ef35d559b23ddc8418cbb50d9525717fb4e7ac08d4daa"
 
   url "https://github.com/trezor/trezor-suite/releases/download/v#{version}/Trezor-Suite-#{version}-mac-#{arch}.dmg",
       verified: "github.com/trezor/trezor-suite/"
