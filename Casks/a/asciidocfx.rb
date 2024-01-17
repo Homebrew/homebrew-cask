6197b11386c2ef9c935a2b57f111a75d06e95f38
Rui Chen <rui@chenrui.dev>
2024-01-16 23:52:34 -0500

    asciidocfx 1.8.7

--- Casks/a/asciidocfx.rb
+++ Casks/a/asciidocfx.rb
@@ -1,9 +1,9 @@
 cask "asciidocfx" do
   arch arm: "_M1"
 
-  version "1.8.6"
-  sha256 arm:   "cc3912340f5da0f3b39eda6b384e4e94985fdcef9f9af9ba978f6904029ff632",
-         intel: "2dcceeca0232652ef60e79ba3e9ed730e086050c8c598c810acded4c8c989956"
+  version "1.8.7"
+  sha256 arm:   "f6aac9a46d46461cc9b99fbc0e4e82cdabcd188dda87f8e3aea33439aadce433",
+         intel: "b629a15a41df756416847cacb03b06b3068a5fdc3cba511622c332e43d524392"
 
   url "https://github.com/asciidocfx/AsciidocFX/releases/download/v#{version}/AsciidocFX_Mac#{arch}.dmg",
       verified: "github.com/asciidocfx/AsciidocFX/"
