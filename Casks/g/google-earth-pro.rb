3ed9789ea84d4508721b1cda84a5093345e81736
Rui Chen <rui@chenrui.dev>
2024-01-17 18:13:06 -0500

    google-earth-pro 7.3.6.9750

--- Casks/g/google-earth-pro.rb
+++ Casks/g/google-earth-pro.rb
@@ -1,5 +1,5 @@
 cask "google-earth-pro" do
-  version "7.3.6.9345"
+  version "7.3.6.9750"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://dl.google.com/dl/earth/client/advanced/current/googleearthpromac-intel-#{version.major_minor_patch}.dmg"
