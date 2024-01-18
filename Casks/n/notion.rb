8b727a44734cf035c35c10351500d631227d4e22
Bevan Kay <email@bevankay.me>
2024-01-18 11:00:15 +1100

    notion 3.1.1

--- Casks/n/notion.rb
+++ Casks/n/notion.rb
@@ -2,9 +2,9 @@ cask "notion" do
   arch arm: "-arm64"
   livecheck_folder = on_arch_conditional arm: "arm64", intel: "latest"
 
-  version "3.1.0"
-  sha256 arm:   "2e80ba81eca7b90e14469a1a85aa1541e2c3d1b656a94ec1411aa246b992e13b",
-         intel: "326ce260aca93cef71e0332e4b5186a707da415181be827dac17743a2a920417"
+  version "3.1.1"
+  sha256 arm:   "8355c50ba835f1c06ac3ebb9cb0909e15e0a574d4a9e4f3f13dea565ac00533c",
+         intel: "caaedb39343ebdd5edcc8090fff8fb18fbc07beb78ae9a1c2dc08b7acefcb676"
 
   url "https://desktop-release.notion-static.com/Notion-#{version}#{arch}.dmg",
       verified: "desktop-release.notion-static.com/"
