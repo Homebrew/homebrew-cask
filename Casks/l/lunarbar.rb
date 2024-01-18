8ef44f7ec96cb80ee3f1e2010ae00068fae2e749
Gnohz Gniy <0x00eeee@gmail.com>
2024-01-18 13:21:34 +0800

    Add new cask: lunarbar

---
+++ Casks/l/lunarbar.rb
@@ -0,0 +1,25 @@
+cask "lunarbar" do
+  version "1.1.0"
+  sha256 "ac6097aaa993bc8724be082853fa87f127cc7245b00b665cc1e0742fc033c898"
+
+  url "https://github.com/LunarBar-app/LunarBar/releases/download/v#{version}/LunarBar-#{version}.dmg"
+  name "LunarBar"
+  desc "Lunar calendar for menu bar"
+  homepage "https://github.com/LunarBar-app/LunarBar"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :ventura"
+
+  app "LunarBar.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/app.cyan.lunarbar",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/app.cyan.lunarbar.sfl3",
+    "~/Library/Containers/app.cyan.lunarbar",
+    "~/Library/Saved Application State/app.cyan.lunarbar.savedState",
+  ]
+end
