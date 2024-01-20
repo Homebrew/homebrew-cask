d268fc734a615c2d274b27ba600fcd8e6364e107
Issy Long <me@issyl0.co.uk>
2024-01-20 14:46:43 +0000

    katana: Fix RuboCop `Cask/ArrayAlphabetization` offenses

--- Casks/k/katana.rb
+++ Casks/k/katana.rb
@@ -10,9 +10,9 @@ cask "katana" do
   app "Katana.app"
 
   zap trash: [
+    "~/.katana",
     "~/Library/Application Support/Katana",
     "~/Library/Logs/Katana",
     "~/Library/Preferences/com.electron.katana.plist",
-    "~/.katana",
   ]
 end
