d7ad6367b4489be30d8e666ee938b1bb15e76942
Rui Chen <rui@chenrui.dev>
2024-01-20 12:23:03 -0500

    pritunl 1.3.3782.35

--- Casks/p/pritunl.rb
+++ Casks/p/pritunl.rb
@@ -1,6 +1,6 @@
 cask "pritunl" do
-  version "1.3.3781.28"
-  sha256 "2b8d71ebffbbfb9bb580ef8f4496aa0a1ac60f868f3d6db29680a8e17b3fc95f"
+  version "1.3.3782.35"
+  sha256 "c9ed6a548d32e38b8bdf086e4b385b2bc2a42c07b9ae30ca236930530c89774d"
 
   url "https://github.com/pritunl/pritunl-client-electron/releases/download/#{version}/Pritunl.pkg.zip",
       verified: "github.com/pritunl/pritunl-client-electron/"
