8ab0e9891682a2ccbb07e194fa4177db7a5223f8
Rui Chen <rui@chenrui.dev>
2024-01-20 12:19:27 -0500

    brickstore 2024.1.3

--- Casks/b/brickstore.rb
+++ Casks/b/brickstore.rb
@@ -1,6 +1,6 @@
 cask "brickstore" do
-  version "2024.1.2"
-  sha256 "13206fc451a34918b4930090e6337ff1b7bd621140a0e152880828ca14c1b7c9"
+  version "2024.1.3"
+  sha256 "74739687edf75751c0887fcf6c013765a69e277f4b703441c9ea1319004adfdc"
 
   url "https://github.com/rgriebl/brickstore/releases/download/v#{version}/macOS-BrickStore-#{version}.dmg",
       verified: "github.com/rgriebl/brickstore/"
