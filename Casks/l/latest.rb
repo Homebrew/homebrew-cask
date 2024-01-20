2a3b47357907eb7428d684cf4f5d7ad529a9196d
Edward <73746306+WangEdward@users.noreply.github.com>
2024-01-20 17:26:18 +0800

    latest 0.10.3

--- Casks/l/latest.rb
+++ Casks/l/latest.rb
@@ -1,15 +1,19 @@
 cask "latest" do
-  version "0.10.2"
-  sha256 "4382a2e2c923334cefa5fc7f749021e4b7e0dc158011ad9ee509efc4c532c8ca"
+  version "0.10.3"
+  sha256 "26e72a8a1555f1f352f4c09189d2feb7c8073f1a4a798fe4beb0e8fa86a7c649"
 
   url "https://max.codes/latest/#{version}.zip"
   name "Latest"
   desc "Utility that shows the latest app updates"
   homepage "https://max.codes/latest"
 
+  # Older versions may have a more recent `pubDate` than newer versions, so we
+  # have to check all the items in the appcast.
   livecheck do
     url "https://max.codes/latest/update.xml"
-    strategy :sparkle, &:short_version
+    strategy :sparkle do |items|
+      items.map(&:short_version)
+    end
   end
 
   auto_updates true
