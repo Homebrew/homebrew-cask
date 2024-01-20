456db08c3b171673f63ab3abd65f38904f99b136
Issy Long <me@issyl0.co.uk>
2024-01-20 14:46:39 +0000

    jandi: Fix RuboCop `Cask/ArrayAlphabetization` offenses

--- Casks/j/jandi.rb
+++ Casks/j/jandi.rb
@@ -18,8 +18,8 @@ cask "jandi" do
 
   zap trash: [
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.jandi.osx.jandi.sfl*",
-    "~/Library/Application Support/JANDI",
     "~/Library/Application Support/com.jandi.osx.JANDI.ShipIt",
+    "~/Library/Application Support/JANDI",
     "~/Library/Caches/com.jandi.osx.JANDI",
     "~/Library/Preferences/com.jandi.osx.JANDI.plist",
     "~/Library/Saved Application State/com.jandi.osx.JANDI",
