9157b4c0ca9d7bc09987fad8eb94e682b0f11a02
Issy Long <me@issyl0.co.uk>
2024-01-20 15:44:34 +0000

    pine: Fix RuboCop `Cask/ArrayAlphabetization` offenses

--- Casks/p/pine.rb
+++ Casks/p/pine.rb
@@ -12,8 +12,8 @@ cask "pine" do
   app "Pine.app"
 
   zap trash: [
-    "~/Library/Application Support/Pine",
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/io.github.lukakerr.pine.sfl2",
+    "~/Library/Application Support/Pine",
     "~/Library/Caches/io.github.lukakerr.Pine",
     "~/Library/Preferences/io.github.lukakerr.Pine.plist",
     "~/Library/Saved Application State/io.github.lukakerr.Pine.savedState",
