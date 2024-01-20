7c65dd0758b5b3792f9278b4ed18b6feac9ddfdd
Issy Long <me@issyl0.co.uk>
2024-01-20 15:44:46 +0000

    printopia: Fix RuboCop `Cask/ArrayAlphabetization` offenses

--- Casks/p/printopia.rb
+++ Casks/p/printopia.rb
@@ -21,13 +21,13 @@ cask "printopia" do
             ]
 
   zap trash: [
-    "~/Library/Application Support/com.decisivetactics.printopia",
-    "~/Library/Caches/com.decisivetactics.printopia",
-    "~/Library/Preferences/com.decisivetactics.printopia.plist",
-    "~/Library/Saved Application State/com.decisivetactics.printopia.savedState",
     "/Library/Application Support/com.decisivetactics.printopia",
     "/Library/Logs/Printopia",
     "/Users/Shared/._com.decisivetactics.printopia",
     "/Users/Shared/Printopia",
+    "~/Library/Application Support/com.decisivetactics.printopia",
+    "~/Library/Caches/com.decisivetactics.printopia",
+    "~/Library/Preferences/com.decisivetactics.printopia.plist",
+    "~/Library/Saved Application State/com.decisivetactics.printopia.savedState",
   ]
 end
