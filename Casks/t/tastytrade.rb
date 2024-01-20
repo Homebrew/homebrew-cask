014942e21d9dc55e2219f2fff2fefec7ff1ec0a3
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-01-20 13:10:27 -0500

    tastytrade: style fixes

--- Casks/t/tastytrade.rb
+++ Casks/t/tastytrade.rb
@@ -4,7 +4,7 @@ cask "tastytrade" do
   version "2.8.0"
   sha256 arm:   "62532bc7b23a439e501c0eff1c3bab216adcbc5c64c05539d06fcdf5d1705088",
          intel: "98ace9292f65022780328fb3a61c98c6542e62046390770d16b8942a9b9e6be8"
-  
+
   url "https://download.tastytrade.com/desktop-#{version.major}.x.x/#{version}/tastytrade-#{version}#{arch}.dmg"
   name "tastytrade"
   desc "Desktop trading platform"
