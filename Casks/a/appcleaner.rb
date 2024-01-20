bba6730fec80bc692a182d4e4b10d88654171824
Issy Long <me@issyl0.co.uk>
2024-01-20 14:44:32 +0000

    appcleaner: Fix RuboCop `Cask/ArrayAlphabetization` offenses

--- Casks/a/appcleaner.rb
+++ Casks/a/appcleaner.rb
@@ -40,8 +40,8 @@ cask "appcleaner" do
 
   zap trash: [
     "~/Library/Caches/net.freemacsoft.AppCleaner",
-    "~/Library/Preferences/net.freemacsoft.AppCleaner.plist",
     "~/Library/Preferences/net.freemacsoft.AppCleaner-SmartDelete.plist",
+    "~/Library/Preferences/net.freemacsoft.AppCleaner.plist",
     "~/Library/Saved Application State/net.freemacsoft.AppCleaner.savedState",
   ]
 end
