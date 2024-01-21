b7962d2e995685963b7525d98a12be5002b27b26
Rui Chen <rui@chenrui.dev>
2024-01-21 12:16:35 -0500

    red-eye: deprecate as download link is not available
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/r/red-eye.rb
+++ Casks/r/red-eye.rb
@@ -2,15 +2,12 @@ cask "red-eye" do
   version "1.2.0"
   sha256 :no_check
 
-  url "https://www.hexedbits.com/downloads/redeye.zip"
+  url "https://web.archive.org/web/20200814031954/https://www.hexedbits.com/downloads/redeye.zip"
   name "Red Eye"
   desc "Tool to prevent the system from going into sleep mode"
   homepage "https://www.hexedbits.com/redeye/"
 
-  livecheck do
-    url :homepage
-    regex(/Version\s+(\d+(?:\.\d+)+)/i)
-  end
+  deprecate! date: "2024-01-21", because: "download artifact not available"
 
   depends_on macos: ">= :mojave"
 
