83551f38672358c38db7397375165e05fd0e79b4
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-01-22 09:27:31 -0500

    openaudible 4.0.3

--- Casks/o/openaudible.rb
+++ Casks/o/openaudible.rb
@@ -1,6 +1,6 @@
 cask "openaudible" do
-  version "4.0.2"
-  sha256 "1ec6e5f2bd9e26968b9421add2ffc7d642acbf199e1edb1256ea3332d1e6da53"
+  version "4.0.3"
+  sha256 "f0b22500a9b4036457f9e3a85aba250753f6066d43feec228c3687a8775ede0d"
 
   url "https://github.com/openaudible/openaudible/releases/download/v#{version}/OpenAudible_#{version}.dmg",
       verified: "github.com/openaudible/openaudible/"
