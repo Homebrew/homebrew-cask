7f3cb4269a9b21fb383eab2151096d89d87aa874
runner <runner@Mac-1705964441246.local>
2024-01-22 23:11:07 +0000

    devtunnel 1.0.1160+d54aaf0b07

--- Casks/d/devtunnel.rb
+++ Casks/d/devtunnel.rb
@@ -1,9 +1,9 @@
 cask "devtunnel" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.0.1110+47c982ae3e"
-  sha256 arm:   "86e19416fbe5e46dd7a8ad61317e8c839c9035c00012ced694efac7edaba1836",
-         intel: "e605f365b892d36910e2c0c1dfab6b4e5d13d6746118eff9eda163a46105c8e6"
+  version "1.0.1160+d54aaf0b07"
+  sha256 arm:   "7ebd20481ed0a2a127ddaacad5c17b420a4aea31b768349e51e80c67d8ff4d96",
+         intel: "fe19afd1c54b5f248aba13b816efff962bf00739cecc9ac0f798db8b49f8195d"
 
   url "https://tunnelsassetsprod.blob.core.windows.net/cli/#{version}/osx-#{arch}-devtunnel-zip",
       verified: "tunnelsassetsprod.blob.core.windows.net/cli/"
