8e27b6131b321390978ba671d51c45c0fb622349
Jose Storopoli <jose@storopoli.io>
2024-01-22 16:24:17 -0300

    feat: add spotube
    Adds [`spotube`](https://github.com/KRTirtho/spotube)

---
+++ Casks/s/spotube.rb
@@ -0,0 +1,22 @@
+cask "spotube" do
+  version "3.4.1"
+  sha256 "7627f679cdfb40bf4554f1a0aaabed076cd15fa3f7342b61e45167a40491fc0b"
+
+  url "https://github.com/KRTirtho/spotube/releases/download/v#{version}/Spotube-macos-universal.dmg",
+      verified: "github.com/KRTirtho/spotube/"
+  name "spotube"
+  desc "Open source Spotify client that doesn't require Premium nor uses Electron"
+  homepage "https://spotube.netlify.app/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "spotube.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/oss.krtirtho.spotube",
+    "~/Library/Containers/oss.krtirtho.spotube",
+  ]
+end
