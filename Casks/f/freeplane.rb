891da4e0f3b8dea48b8add1aa5991737653b1df3
Rui Chen <rui@chenrui.dev>
2024-01-22 18:17:20 -0500

    freeplane 1.11.10

--- Casks/f/freeplane.rb
+++ Casks/f/freeplane.rb
@@ -1,9 +1,9 @@
 cask "freeplane" do
   arch arm: "apple", intel: "intel"
 
-  version "1.11.8"
-  sha256 arm:   "6aae1dece672fc9fae974fc45931dc5d64148fecd957fb2f3a623c4b5160a90a",
-         intel: "1802c0c881bc5829f1438d865561f4fa49b6b8e1f30b434ae8e0e4c00cbbfc93"
+  version "1.11.10"
+  sha256 arm:   "59ab21169a7422097a4f9b347c8bdb69e36237d279b2732f21482422f623a9ba",
+         intel: "d9d638827adffbb8a2863934ab7953e675dbef34061f7e3ac3f78ba154d3369a"
 
   url "https://downloads.sourceforge.net/freeplane/Freeplane-#{version}-#{arch}.dmg",
       verified: "downloads.sourceforge.net/freeplane/"
