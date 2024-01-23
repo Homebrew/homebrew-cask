c99627e3472de32ee5153d67a17b03b70faa71fa
Bevan Kay <email@bevankay.me>
2024-01-24 10:01:02 +1100

    rode-connect 1.3.33

--- Casks/r/rode-connect.rb
+++ Casks/r/rode-connect.rb
@@ -1,5 +1,5 @@
 cask "rode-connect" do
-  version "1.3.31"
+  version "1.3.33"
   sha256 :no_check
 
   url "https://update.rode.com/connect/RODE_Connect_MACOS.zip"
@@ -7,11 +7,12 @@ cask "rode-connect" do
   desc "Podcasting software"
   homepage "https://rode.com/en-us/software/rodeconnect"
 
+  # Upstream's update manifest is out of date but may be able to be used in future
+  # https://update.rode.com/rode-devices-manifest.json
+  # json.dig("rode-connect-manifest", "macos", "main-version", "update-version")
   livecheck do
-    url "https://update.rode.com/rode-devices-manifest.json"
-    strategy :json do |json|
-      json["rode-connect-manifest"]["macos"]["main-version"]["update-version"]
-    end
+    url :url
+    strategy :extract_plist
   end
 
   pkg "RØDE Connect (#{version}).pkg"
