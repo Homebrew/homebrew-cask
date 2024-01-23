ecab6476e1c3285c8d316c0cc20b60ecc0129488
Chef Expeditor <expeditor@chef.io>
2024-01-23 11:37:03 +0530

    chef-workstation 24.1.1056

--- Casks/c/chef-workstation.rb
+++ Casks/c/chef-workstation.rb
@@ -3,17 +3,17 @@ cask "chef-workstation" do
 
   macos_version = "11"
 
-  version "23.12.1055"
-  sha256 arm:   "9f785db7db24a115e8c0e6fa0dc847438aa60ee794565823099bd0c130bbddf0",
-         intel: "cc09b1b1f7fbc25b28176e19839191ed031d23466c9c5155c96de9d5d9ed003c"
+  version "24.1.1056"
+  sha256 arm:   "9d51eeefc36b9b05fee27e9b35cf45fe062d9539e52b48f5246e275d70681434",
+         intel: "72c8e59fb2f64ded9e38dae578e39653e46b21663079d80cb2db88deb517ca1d"
 
-  url "https://packages.chef.io/files/stable/chef-workstation/#{version}/mac_os_x/#{macos_version}/chef-workstation-#{version}-1.#{arch}.dmg"
+  url "https://packages.chef.io/files/current/chef-workstation/#{version}/mac_os_x/#{macos_version}/chef-workstation-#{version}-1.#{arch}.dmg"
   name "Chef Workstation"
   desc "All-in-one installer for the tools you need to manage your Chef infrastructure"
   homepage "https://docs.chef.io/workstation/"
 
   livecheck do
-    url "https://omnitruck.chef.io/stable/chef-workstation/metadata?p=mac_os_x&pv=#{macos_version}&m=#{arch}&v=latest"
+    url "https://omnitruck.chef.io/current/chef-workstation/metadata?p=mac_os_x&pv=#{macos_version}&m=#{arch}&v=latest"
     regex(/version\s*(\d+(?:\.\d+)+)/i)
   end
 
