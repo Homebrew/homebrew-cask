f1fcaa07746fdc467a8104093c20f5b23b3b3d55
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-01-23 12:24:02 -0500

    datovka 4.23.4

--- Casks/d/datovka.rb
+++ Casks/d/datovka.rb
@@ -1,6 +1,6 @@
 cask "datovka" do
-  version "4.23.3"
-  sha256 "6260d7c2a1efbd7548af0cf0422b01a3f5418a7faacf796cac8c2ffc1e0544f5"
+  version "4.23.4"
+  sha256 "5feb4a4b23d4b298df83330e9bdadad9a42f7c9d43ccdbdcb1e997c53804badb"
 
   url "https://secure.nic.cz/files/datove_schranky/#{version}/datovka-#{version}-64bit-macos10.12.dmg",
       verified: "secure.nic.cz/files/datove_schranky/"
