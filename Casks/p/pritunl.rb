b18de6e5bf9e1fdf5edb9776077cd3bc63819e65
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-01-23 21:10:46 -0500

    pritunl 1.3.3785.81

--- Casks/p/pritunl.rb
+++ Casks/p/pritunl.rb
@@ -1,6 +1,6 @@
 cask "pritunl" do
-  version "1.3.3782.35"
-  sha256 "c9ed6a548d32e38b8bdf086e4b385b2bc2a42c07b9ae30ca236930530c89774d"
+  version "1.3.3785.81"
+  sha256 "3d86375735c8e14842e01dfff638511f3c953354cdfc46cf8f726fa79be16f41"
 
   url "https://github.com/pritunl/pritunl-client-electron/releases/download/#{version}/Pritunl.pkg.zip",
       verified: "github.com/pritunl/pritunl-client-electron/"
