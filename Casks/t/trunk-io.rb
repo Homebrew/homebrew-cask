47f9ba8708f6847a6f4588ee9e7a98484f6b82d5
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-01-23 20:52:02 -0500

    trunk-io 1.3.0

--- Casks/t/trunk-io.rb
+++ Casks/t/trunk-io.rb
@@ -1,6 +1,6 @@
 cask "trunk-io" do
-  version "1.2.7"
-  sha256 "8b69bee98ea0be41d8c3311226fd0390b707917ab3fa0e1ee374d5eaed544e3f"
+  version "1.3.0"
+  sha256 "bfd5896fda08b399366485fcd0bf37751b446ab4e256c5c1b5706d6b4b0ffbe0"
 
   url "https://trunk.io/releases/launcher/#{version}/trunk"
   name "Trunk Launcher"
