a97be88222c5e8f1896206eba163357e3bbe038f
Rui Chen <rui@chenrui.dev>
2024-01-24 16:15:18 -0500

    marvin 1.64.1

--- Casks/m/marvin.rb
+++ Casks/m/marvin.rb
@@ -2,9 +2,9 @@ cask "marvin" do
   arch arm: "-arm64"
   hostname_arch = on_arch_conditional arm: "amarm", intel: "amazingmarvin"
 
-  version "1.63.0"
-  sha256 arm:   "46bd20dbef2ca137e9452fec120ac454b4eeca038954ea2af054eb4d830785d6",
-         intel: "3a3c44968e66274785c928545db34833d3c9369dd2fee17288de7054ce78ae66"
+  version "1.64.1"
+  sha256 arm:   "39f4b39080dc6188a32978adedee40e6c537bdcb9b2060abd83be837d934e2dd",
+         intel: "9f973b578f6e5bded54dc305154c7e95ade2447e3a68bc40924a82c231b780ba"
 
   url "https://#{hostname_arch}.s3.amazonaws.com/Marvin-#{version}#{arch}-mac.zip",
       verified: "#{hostname_arch}.s3.amazonaws.com/"
