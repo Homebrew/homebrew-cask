7f4583f3680d54f2ef35468960e9cd1a2c44fbc9
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-01-23 21:10:00 -0500

    dcp-o-matic-playlist-editor 2.16.72

--- Casks/d/dcp-o-matic-playlist-editor.rb
+++ Casks/d/dcp-o-matic-playlist-editor.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-playlist-editor" do
-  version "2.16.71"
-  sha256 "01bb517fdb2aeb78a383293c581cb5e52a3620fb3fc22b2a6b83a223720412bb"
+  version "2.16.72"
+  sha256 "4b9e23f799e5b3259a615da750aa1bf316d9bf48788935ba3bf52eb8b83a48c2"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-playlist&version=#{version}"
   name "DCP-o-matic Playlist Editor"
