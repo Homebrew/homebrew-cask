18b0beeeadf25a7f32a1d449bd4475458ca4d0a9
Rui Chen <rui@chenrui.dev>
2024-01-24 17:57:26 -0500

    fme 2023.2.1,23774
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/f/fme.rb
+++ Casks/f/fme.rb
@@ -2,9 +2,9 @@ cask "fme" do
   arch arm: "aarch64", intel: "x64"
   folder = on_arch_conditional arm: "-aarch64"
 
-  version "2023.2,23764"
-  sha256 arm:   "4b65e94a5defb29626f2ef3a97ce9b4d8f74459c125f3159849c3868e75633d1",
-         intel: "819baf6ccc55d96e3867b09f9db24b6e13c29e4bbbf8828e2f08b8020e159f3e"
+  version "2023.2.1,23774"
+  sha256 arm:   "cbf701cd244faebc42de7418d47d4bf8c0fbddb329842f72535df4682f49183c",
+         intel: "d9e8f25d812219747f9b4d63e0f14fc85f7367e7aabb8970bc0c738989977fb0"
 
   url "https://downloads.safe.com/fme/#{version.major}/macos#{folder}/fme-form-#{version.csv.first}-b#{version.csv.second}-macosx-#{arch}.pkg"
   name "FME Form"
