a57a88a518fd1a01578210d88682945e13808ec4
Rui Chen <rui@chenrui.dev>
2024-01-24 18:03:20 -0500

    moefe-google-translate: deprecate
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/m/moefe-google-translate.rb
+++ Casks/m/moefe-google-translate.rb
@@ -4,7 +4,12 @@ cask "moefe-google-translate" do
 
   url "https://github.com/MoeFE/GoogleTranslate/releases/download/v#{version}/google-translate-#{version}-mac.zip"
   name "MoeFE Google Translate"
+  desc "Google Translate client"
   homepage "https://github.com/MoeFE/GoogleTranslate"
+
+  # no new release since 2018-08-27
+  # current build has google client integration issue, https://github.com/MoeFE/GoogleTranslate/issues/468
+  deprecate! date: "2024-01-24", because: :unmaintained
 
   app "mac/Google 翻译.app"
 end
