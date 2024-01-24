1eb9408b5d1228b16114a32e39542ed0587aa72f
Rui Chen <rui@chenrui.dev>
2024-01-24 18:00:17 -0500

    mysql-shell 8.3.0

--- Casks/m/mysql-shell.rb
+++ Casks/m/mysql-shell.rb
@@ -1,23 +1,23 @@
 cask "mysql-shell" do
   arch arm: "arm64", intel: "x86-64bit"
 
-  version "8.2.1"
-  sha256 arm:   "099bb8194994f775190f9ef30a9c41192205c2ca04ea3566bb01e130edd96565",
-         intel: "117fcb004136227e2d6f74acc4a6bd42e1128fed6f8f4cc4180c278819435f4a"
+  version "8.3.0"
+  sha256 arm:   "9b772d1908c1413cafe1cd76eec4ae81014ab70ae820c87977396d584371b392",
+         intel: "a2121ba87bce1b65eb7f756107ea90b8d99e4c63d629987a8d4436ca28065fa7"
 
-  url "https://dev.mysql.com/get/Downloads/MySQL-Shell/mysql-shell-#{version}-macos13-#{arch}.dmg"
+  url "https://dev.mysql.com/get/Downloads/MySQL-Shell/mysql-shell-#{version}-macos14-#{arch}.dmg"
   name "MySQL Shell"
   desc "Interactive JavaScript, Python or SQL interface"
   homepage "https://dev.mysql.com/downloads/shell/"
 
   livecheck do
     url "https://dev.mysql.com/downloads/shell/?tpl=platform&os=33"
-    regex(/mysql[._-]shell[._-]v?(\d+(?:\.\d+)+)[._-]macos13[._-]#{arch}\.dmg/i)
+    regex(/mysql[._-]shell[._-]v?(\d+(?:\.\d+)+)[._-]macos14[._-]#{arch}\.dmg/i)
   end
 
   depends_on macos: ">= :monterey"
 
-  pkg "mysql-shell-#{version}-macos13-#{arch}.pkg"
+  pkg "mysql-shell-#{version}-macos14-#{arch}.pkg"
 
   uninstall pkgutil: "com.mysql.shell"
 
