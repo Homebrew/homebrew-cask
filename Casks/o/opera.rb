709ab1ac9664ff2a6fd2045268d27d65845bfb07
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-01-25 10:03:44 -0500

    opera 106.0.4998.66

--- Casks/o/opera.rb
+++ Casks/o/opera.rb
@@ -1,6 +1,6 @@
 cask "opera" do
-  version "106.0.4998.52"
-  sha256 "0b38daa63b85ce5fd2c77957c46be882940608ef1bb4b7ad0e5c67bbb2de4333"
+  version "106.0.4998.66"
+  sha256 "5c3014670b166a02ce2480ab355e05e4a59c1f9f49fa6b24e8c5eea4a85aba94"
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   name "Opera"
