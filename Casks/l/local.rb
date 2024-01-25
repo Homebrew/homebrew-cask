9a9499f7f7746823120d413c25df1fb875fde8a8
Andy Fragen <andy@thefragens.com>
2024-01-25 08:59:28 -0800

    update Local 8.2.1
    update version

--- Casks/l/local.rb
+++ Casks/l/local.rb
@@ -1,9 +1,9 @@
 cask "local" do
   arch arm: "-arm64"
 
-  version "8.2.0,6554"
-  sha256  arm:   "1d9ff40c50582157408904eece0ce4e44f68dcd7d25fa8a9bddf8298be5a49c3",
-          intel: "a45223f40f3f8cd275cabc4c8759a32005daab88147ca2ccbf0e29f355b8e046"
+  version "8.2.1,6583"
+  sha256  arm:   "b2fc22407fdc650fe5771719eed3b767597317eef2f4b478b035bdac86a7ccbf",
+          intel: "dde7f702f95364d9f685d221e34557acf1937e72ef8b2a91d77948b6f95aae39"
 
   url "https://cdn.localwp.com/releases-stable/#{version.csv.first}+#{version.csv.second}/local-#{version.csv.first}-mac#{arch}.dmg"
   name "Local"
