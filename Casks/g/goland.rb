b4d9ab854d0fa19ff072fd92af865d26471697a7
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-01-26 10:26:59 -0500

    goland 2023.3.3

--- Casks/g/goland.rb
+++ Casks/g/goland.rb
@@ -1,9 +1,9 @@
 cask "goland" do
   arch arm: "-aarch64"
 
-  version "2023.3.2,233.13135.104"
-  sha256 arm:   "28669ecf701dd4b60f86218e9f96de0839536b1623dfb42186fd5bb54541b69c",
-         intel: "36c18551deb5e249896bd56b405e1fa4a29e48b6b203eecbe7875f0f83468121"
+  version "2023.3.3,233.14015.113"
+  sha256 arm:   "4b192b5a59d86ca8f20377d7905e8a91aa1f53e99bb868ff74d9d0959b54d9c4",
+         intel: "2e459e390988e2d196add580c8cbfce8132ef0a4d55709d7495cb65a195ed4f9"
 
   url "https://download.jetbrains.com/go/goland-#{version.csv.first}#{arch}.dmg"
   name "Goland"
