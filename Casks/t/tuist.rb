11bff7bb45ea0925873e6aa302607934a758ad7a
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-01-26 10:26:40 -0500

    tuist 3.42.1

--- Casks/t/tuist.rb
+++ Casks/t/tuist.rb
@@ -1,6 +1,6 @@
 cask "tuist" do
-  version "3.42.0"
-  sha256 "106928d6cca15d58c9b32fb88c2bec13bd531a8e7a3e70b050e872f855df8cae"
+  version "3.42.1"
+  sha256 "4ea94560c70714e7b801fb7b2eab482ac5c830239ad62479d9ae1004e2649225"
 
   url "https://github.com/tuist/tuist/releases/download/#{version}/tuist.zip",
       verified: "github.com/tuist/tuist/"
