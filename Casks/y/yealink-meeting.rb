c5b5533c8d96393938e71b78c371112afcd34a21
Rui Chen <rui@chenrui.dev>
2024-01-26 19:42:38 -0500

    yealink-meeting 4.6.4

--- Casks/y/yealink-meeting.rb
+++ Casks/y/yealink-meeting.rb
@@ -1,6 +1,6 @@
 cask "yealink-meeting" do
-  version "4.5.18,34c31fa5e2985182aaef5331da7c60ca"
-  sha256 "3170ef52a77ba5a648ffe212bee2839982d076730a9185ee73fa9083f0b3ae28"
+  version "4.6.4,1074a39a74ee96ffa624c702ba30caad"
+  sha256 "5f4ef4c080dfe080cf53901efa685c1c1aa35235c48b19f780c25b90a2187757"
 
   url "https://download.ylyun.com/yealink-meeting/macos/latest/Yealink-Meeting.dmg?v=#{version.csv.second}"
   name "Yealink Meeting"
