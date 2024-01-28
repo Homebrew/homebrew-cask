1d3f16056666eadcdd434a5be60a1e93e6f1362a
HuaDeity <hello@huadeity.com>
2024-01-29 02:39:45 +0800

    Add Cask:cleanupbuddy

---
+++ Casks/c/cleanupbuddy.rb
@@ -0,0 +1,25 @@
+cask "cleanupbuddy" do
+  version "1.0"
+  sha256 :no_check
+
+  url "https://sparkle.cleanupbuddy.app/CleanupBuddy-latest.dmg"
+  name "CleanupBuddy"
+  desc "Clean Mac's keyboard and trackpad"
+  homepage "https://cleanupbuddy.app/"
+
+  livecheck do
+    url "https://sparkle.cleanupbuddy.app/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+  depends_on macos: ">= :ventura"
+
+  app "CleanupBuddy.app"
+
+  zap trash: [
+    "~/Library/HTTPStorages/codes.rambo.CleanupBuddy",
+    "~/Library/Preferences/codes.rambo.CleanupBuddy.plist",
+    "~/Library/Saved Application State/codes.rambo.CleanupBuddy.savedState",
+  ]
+end
