094dd7364e59d5da8aed0eb08abf6e8b828c4275
Rui Chen <rui@chenrui.dev>
2024-01-29 09:56:08 -0500

    backuploupe 3.12.2

--- Casks/b/backuploupe.rb
+++ Casks/b/backuploupe.rb
@@ -1,6 +1,6 @@
 cask "backuploupe" do
-  version "3.12.1"
-  sha256 "828fb480c3dd7b20cb0346faa2c431103fd00a2a0c7fa6c7e4956d89fb757e1f"
+  version "3.12.2"
+  sha256 "74920ed5e15777eb441a0a2a6d425f086447e4ec7438a4cb588929091c57b798"
 
   url "https://www.soma-zone.com/download/files/BackupLoupe-#{version}.tar.xz"
   name "BackupLoupe"
