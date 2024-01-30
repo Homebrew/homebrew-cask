f58cc5a061ca90c19131074d70cb9cb5a6589d76
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-01-30 12:36:59 +0000

    chirp 20240130

--- Casks/c/chirp.rb
+++ Casks/c/chirp.rb
@@ -1,6 +1,6 @@
 cask "chirp" do
-  version "20240122"
-  sha256 "01266c71cbfd39dc3a27e8fe005d70b53710f769cc71028d0f0df9562754e85e"
+  version "20240130"
+  sha256 "869fef0900bf99b8dbc5b1f052384880f0a09bcff5a6ee430cd25ff87a779930"
 
   url "https://trac.chirp.danplanet.com/chirp_next/next-#{version}/chirp-next-#{version}.app.zip"
   name "CHIRP"
