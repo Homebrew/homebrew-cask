8a54d8a03af7f10c43120d0fc94179812beb3fb1
Patrick Linnane <patrick@linnane.io>
2024-01-30 10:27:30 -0800

    guilded 1.0.9236953
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/g/guilded.rb
+++ Casks/g/guilded.rb
@@ -1,15 +1,14 @@
 cask "guilded" do
-  version "1.0.9235285"
-  sha256 "ab4471d11be89d33bab43307709b154b83b1342d0c595cf48dd165fd60e94a50"
+  version "1.0.9236953"
+  sha256 "8cf047c746fdd5c3fe41d8289ae7940a1e03d38b98bf7c49536c71ec6f39b459"
 
-  url "https://s3-us-west-2.amazonaws.com/www.guilded.gg/AppBuilds/mac/Guilded-#{version}-release.dmg",
-      verified: "s3-us-west-2.amazonaws.com/www.guilded.gg/AppBuilds/mac/"
+  url "https://www.guilded.gg/AppBuilds/mac/Guilded-#{version}-release.dmg"
   name "Guilded"
   desc "Group chat platform"
   homepage "https://www.guilded.gg/"
 
   livecheck do
-    url "https://s3-us-west-2.amazonaws.com/www.guilded.gg/AppBuilds/mac/release-mac.yml"
+    url "https://www.guilded.gg/AppBuilds/mac/release-mac.yml"
     strategy :electron_builder do |data|
       data["version"].tr("-release", "")
     end
@@ -23,4 +22,8 @@ cask "guilded" do
     "~/Library/Preferences/com.electron.guilded.plist",
     "~/Library/Saved Application State/com.electron.guilded.savedState",
   ]
+
+  caveats do
+    requires_rosetta
+  end
 end
