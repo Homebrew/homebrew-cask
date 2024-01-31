7fbcd7fab5f220795538f705bfd82c0ef90dd8e2
Rui Chen <rui@chenrui.dev>
2024-01-31 09:49:38 -0500

    wavebox 10.121.6.2

--- Casks/w/wavebox.rb
+++ Casks/w/wavebox.rb
@@ -1,9 +1,9 @@
 cask "wavebox" do
   arch arm: "macarm64", intel: "mac"
 
-  version "10.121.4.2"
-  sha256 arm:   "55d050a926b06a05330f8b75d3041aec9b442b1a8de1c510b924cca792d618ef",
-         intel: "245d79b84fad03d4c04bce0c699f88e34651268be55b03a615bbcb7141dd8bcb"
+  version "10.121.6.2"
+  sha256 arm:   "4fba40298af3250d1781072477f5b20ed61a8f4f8eedcf557adf953a48573a53",
+         intel: "8525af844ae7048be1a09af74ed83c2374bf50e79d2c4d56482be78d01d03dee"
 
   url "https://download.wavebox.app/stable/#{arch}/Install%20Wavebox%20#{version}.dmg",
       verified: "download.wavebox.app/"
