5a1d2bba7679baac34c27f2bf0c2c58c818a1cf2
Erich Saturn <erich@MacBook-Air-von-Erich.local>
2024-02-02 00:23:00 +0800

    gpgfrontend 2.1.2

--- Casks/g/gpgfrontend.rb
+++ Casks/g/gpgfrontend.rb
@@ -1,14 +1,14 @@
 cask "gpgfrontend" do
-  version "2.1.1"
+  version "2.1.2"
 
   on_big_sur do
-    sha256 "365ffc4a42efbb695b52c03fc98f861e388c4ca38b3921f0597ccc49a08b44ae"
+    sha256 "46b285779f9674438df79a660c76a8baafa9aa25c788f20f6200d652cb1e78ff"
 
     url "https://github.com/saturneric/GpgFrontend/releases/download/v#{version}/GpgFrontend-#{version}-macos-11.dmg",
         verified: "github.com/saturneric/GpgFrontend/"
   end
   on_monterey :or_newer do
-    sha256 "480b2b603da29fcff0ff22f42bff853580b637d17c61ed401bd9a3cd4c5d4c10"
+    sha256 "d9ad042487a56800e44aff7111f03a0757ef1580c771ed99627cab988196cb67"
 
     url "https://github.com/saturneric/GpgFrontend/releases/download/v#{version}/GpgFrontend-#{version}-macos-12.dmg",
         verified: "github.com/saturneric/GpgFrontend/"
