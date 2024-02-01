90cf9c96f5293d9a961c12ebce04f7c506b4020f
Rui Chen <rui@chenrui.dev>
2024-02-01 14:55:35 -0500

    keysafe 1.3.1

--- Casks/k/keysafe.rb
+++ Casks/k/keysafe.rb
@@ -1,6 +1,6 @@
 cask "keysafe" do
-  version "1.3.0"
-  sha256 "0294b76878086e412bcae4c683271328f2df41abe7912d320e79d6483da7bda7"
+  version "1.3.1"
+  sha256 "429eff0350d61f8502b95aea40c62b43fa31e40cc14d13960e0dc66f3858ce24"
 
   url "https://miln.eu/keysafe/miln-keysafe-v#{version}-darwin-universal.zip"
   name "Keysafe"
