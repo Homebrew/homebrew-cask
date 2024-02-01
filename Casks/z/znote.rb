46115b94732d83eea3f3e5c5176d4deaa1a7fb04
Rui Chen <rui@chenrui.dev>
2024-02-01 14:35:59 -0500

    znote 2.4.1

--- Casks/z/znote.rb
+++ Casks/z/znote.rb
@@ -1,9 +1,9 @@
 cask "znote" do
   arch arm: "-arm64"
 
-  version "2.4.0"
-  sha256 arm:   "49c4674ad10d6c87233fbd0ec88c6e238aabdc412a6a79e6c44694733b0ebc88",
-         intel: "558943f9066ad4f8c864635ca56b1ba5b17535b4b5b671e7d0c96893b4e5947b"
+  version "2.4.1"
+  sha256 arm:   "67a4517374bc6834c64625807c6094abe3aa73f1c39ae8c036617e070ad69813",
+         intel: "130221d2dcddc7dfe007baa875454193b8443bf3a7c1a6d796b38e3295a055cd"
 
   url "https://github.com/alagrede/znote-app/releases/download/v#{version}/znote-#{version}#{arch}.dmg",
       verified: "github.com/alagrede/znote-app/"
