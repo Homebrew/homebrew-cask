de161b1fb56c8b0b20eca4126472b3abede6dcce
Rui Chen <rui@chenrui.dev>
2024-02-01 14:59:17 -0500

    plasticscm-cloud-edition 11.0.16.8411

--- Casks/p/plasticscm-cloud-edition.rb
+++ Casks/p/plasticscm-cloud-edition.rb
@@ -1,6 +1,6 @@
 cask "plasticscm-cloud-edition" do
-  version "11.0.16.8395"
-  sha256 "d91fdd4566c8fdf6ffbf49ef07f82fe4cef0d38446efd5e0dabd10d1ed9482fb"
+  version "11.0.16.8411"
+  sha256 "ce87aa9083b5f09515463d0f6ea5a5be070519bf0bc7ca84797e12bc6da5f62b"
 
   url "https://d26z97tczqnlef.cloudfront.net/releases/#{version}/plasticscm/osx/unity-vcs-#{version}-mac.pkg.zip",
       verified: "d26z97tczqnlef.cloudfront.net/"
