77a1cbad145a426a660a9542c6a7fa67436dc685
Rui Chen <rui@chenrui.dev>
2024-01-31 23:49:45 -0500

    runelite 2.6.13

--- Casks/r/runelite.rb
+++ Casks/r/runelite.rb
@@ -1,9 +1,9 @@
 cask "runelite" do
   arch arm: "aarch64", intel: "x64"
 
-  version "2.6.11"
-  sha256 arm:   "48c7baf3f5eedd8156d1fbf9bcd477de6157e6e7383b9e6182ca2eec2dfdab42",
-         intel: "a9fb994d2a46a06fc225b5e534c0c9533343664145d89fb77513ee27af11698b"
+  version "2.6.13"
+  sha256 arm:   "480cbe9dc08a23c81088d44e53f08092892727d8a2b5e4f7c308891edc7912c8",
+         intel: "609ddcd5cd951c43b8a4ea9db59b2589b093c41acc8da0191d1c985af437c1c9"
 
   url "https://github.com/runelite/launcher/releases/download/#{version}/RuneLite-#{arch}.dmg",
       verified: "github.com/runelite/launcher/"
