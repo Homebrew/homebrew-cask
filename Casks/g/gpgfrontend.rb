63844ccf9a36c8be628a455dae6caff759ebaeb0
Rui Chen <rui@chenrui.dev>
2024-02-01 14:34:09 -0500

    gpgfrontend 2.1.2

--- Casks/g/gpgfrontend.rb
+++ Casks/g/gpgfrontend.rb
@@ -1,5 +1,5 @@
 cask "gpgfrontend" do
-  version "2.1.1"
+  version "2.1.2"
 
   on_big_sur do
     sha256 "365ffc4a42efbb695b52c03fc98f861e388c4ca38b3921f0597ccc49a08b44ae"
@@ -8,7 +8,7 @@ cask "gpgfrontend" do
         verified: "github.com/saturneric/GpgFrontend/"
   end
   on_monterey :or_newer do
-    sha256 "480b2b603da29fcff0ff22f42bff853580b637d17c61ed401bd9a3cd4c5d4c10"
+    sha256 "d9ad042487a56800e44aff7111f03a0757ef1580c771ed99627cab988196cb67"
 
     url "https://github.com/saturneric/GpgFrontend/releases/download/v#{version}/GpgFrontend-#{version}-macos-12.dmg",
         verified: "github.com/saturneric/GpgFrontend/"
