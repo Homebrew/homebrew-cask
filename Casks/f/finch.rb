01cf06ca532763d827fb4846908d9b4ac692d216
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-02-02 00:27:33 +0100

    finch 1.1.1

--- Casks/f/finch.rb
+++ Casks/f/finch.rb
@@ -1,9 +1,9 @@
 cask "finch" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.1.0"
-  sha256 arm:   "ab39c412578eed5771a6385ba6877e494aa2c8c628ece282834883767437800d",
-         intel: "de876721cb62c98ccffe07a63d52f845152c629d940e7cd34738999e5dda21b4"
+  version "1.1.1"
+  sha256 arm:   "7110f407fddca2dd0c14b9d3432bdda324cb690e352bb79ed184a7b1a6135c82",
+         intel: "21e4594799d2a6a4175eebcbb0c1d26e9cb830120a0d8e36e44695543f63ce79"
 
   url "https://github.com/runfinch/finch/releases/download/v#{version}/Finch-v#{version}-#{arch}.pkg"
   name "Finch"
