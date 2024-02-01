0c6d32d4061a894710847a84b1948e9ddf98e59f
ik-fib <ik11235@gmail.com>
2024-02-02 07:23:39 +0900

    warp 0.2024.01.30.16.52.stable_00

--- Casks/w/warp.rb
+++ Casks/w/warp.rb
@@ -1,6 +1,6 @@
 cask "warp" do
-  version "0.2024.01.16.16.31.stable_01"
-  sha256 "861227acd19342c9747abc87c0cd766644527dc7762ade8a656a1d3699a0e627"
+  version "0.2024.01.30.16.52.stable_00"
+  sha256 "6f9715b6f6dac4ea5c80cf7594945aafa7aac9e6058a1b71c7002aa075933294"
 
   url "https://app.warp.dev/download/brew?version=v#{version}"
   name "Warp"
