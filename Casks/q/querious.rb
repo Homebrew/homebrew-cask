9e81894d77cb04e42b54f7dfe31e6c6a4bccf894
Rui Chen <rui@chenrui.dev>
2024-02-02 12:58:19 +0100

    querious 4.0.11

--- Casks/q/querious.rb
+++ Casks/q/querious.rb
@@ -1,6 +1,6 @@
 cask "querious" do
-  version "4.0.10"
-  sha256 "395eda8a9f867ec3c72dcd0c7142c93b29d17d8f08076d1c4fcecea89bc9c5ce"
+  version "4.0.11"
+  sha256 "f6ace8c20ebbe691a98ab8ad07b0c0b976844713ccb1a1696c56838a970d5132"
 
   url "https://www.araelium.com/querious/downloads/versions/Querious#{version}.zip"
   name "Querious #{version.major}"
