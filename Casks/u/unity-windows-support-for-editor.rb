e824e92667a77b67052d249d1e6c77d1fc1392b5
Rui Chen <rui@chenrui.dev>
2024-02-02 11:05:10 +0100

    unity-windows-support-for-editor 2023.2.8f1

--- Casks/u/unity-windows-support-for-editor.rb
+++ Casks/u/unity-windows-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask "unity-windows-support-for-editor" do
-  version "2023.2.7f1,0a9195b3d453"
-  sha256 "ec492087e1fd803df3d4c019dc504f80d719496c264aaed76eab91d266190320"
+  version "2023.2.8f1,a3bb09f8c8c4"
+  sha256 "8a994256ec4cf2ee49fbaedf81b41a2a7e07ba10fbe1b7b90140730a7896a910"
 
   url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-#{version.csv.first}.pkg",
       verified: "download.unity3d.com/download_unity/"
