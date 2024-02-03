87e3f739dd55541c5a5ce732f12c02b90abdc5e2
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-02-03 14:54:29 +0100

    ollama 0.1.23

--- Casks/o/ollama.rb
+++ Casks/o/ollama.rb
@@ -1,6 +1,6 @@
 cask "ollama" do
-  version "0.1.22"
-  sha256 "0d94b718cae2729a0ef207eeb86092597f869e6873c842d26aafd1da7013c8a7"
+  version "0.1.23"
+  sha256 "b1f3343f657dab21410090c8dd5f41e301cf2dd8a9ead3e5d7dfeb0654f69fdd"
 
   url "https://github.com/jmorganca/ollama/releases/download/v#{version}/Ollama-darwin.zip",
       verified: "github.com/jmorganca/ollama/"
