c0172a422ae528dedd8396754ec645a3ddd711fa
Aaron Sun <aaronzs@users.noreply.github.com>
2024-02-05 10:48:24 +0800

    fix typo

--- Casks/i/itk-snap.rb
+++ Casks/i/itk-snap.rb
@@ -6,12 +6,12 @@ cask "itk-snap" do
     sha256 "aba01235a44b9080fae2c5cf0b35485f96b335f1a50442ee4fb941d4c4846501"
   end
   on_intel do
-    version "4.2.0,13697"
+    version "4.0.2,13697"
     sha256 "333071f6703e6d3a8554fcc33f2a632cf3106a0bdc230893807a4c288e9e13da"
   end
 
-  url "https://downloads.sourceforge.net/project/itk-snap/itk-snap/#{version.csv.first}/itksnap-#{version.csv.first}-Darwin-#{arch}.dmg",
-      verified: "downloads.sourceforge.net/project/itk-snap/"
+  url "https://downloads.sourceforge.net/itk-snap/itksnap-#{version.csv.first}-Darwin-#{arch}.dmg",
+      verified: "downloads.sourceforge.net/itk-snap/"
   name "ITK-SNAP"
   desc "Segment structures in 3D medical images"
   homepage "http://www.itksnap.org/pmwiki/pmwiki.php"
