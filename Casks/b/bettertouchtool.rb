51b2ce5f097126caf76686967f5d6fff70947c23
tiiiecherle <tiiiecherle@gmail.com>
2024-02-05 08:49:57 +0100

    bettertouchtool 4.386

--- Casks/b/bettertouchtool.rb
+++ Casks/b/bettertouchtool.rb
@@ -1,6 +1,6 @@
 cask "bettertouchtool" do
-  version "4.385,43850"
-  sha256 "b7c094d9b1fbf3c571c186b11c1a01cb7ccec9d233c43e6e55390763002d9742"
+  version "4.386,43860"
+  sha256 "b419a1b23ef7a0c0a898d98f393de27674d374c66a0d0c6cbb6e8cb9df8fa819"
 
   url "https://folivora.ai/releases/btt#{version.csv.first}-#{version.csv.second}.zip"
   name "BetterTouchTool"
