1107df15a73fcc03b534729b4ebcd532ac0a2ab6
Rui Chen <rui@chenrui.dev>
2024-02-05 06:21:25 +0100

    dcp-o-matic-encode-server 2.16.74

--- Casks/d/dcp-o-matic-encode-server.rb
+++ Casks/d/dcp-o-matic-encode-server.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-encode-server" do
-  version "2.16.73"
-  sha256 "077a8d80ff4adf67d396211069d092f28c8101a40585f2f09d7e3e7595226175"
+  version "2.16.74"
+  sha256 "b77f2441412f10969b27fba555fd5fc4f407b0fb614aba370d42b45c9d2b6bb6"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-server&version=#{version}"
   name "DCP-o-matic Encode Server"
