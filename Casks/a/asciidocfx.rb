c3a2b5fd6c332ea2936243f47aa2e1817b2543c2
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-02-06 22:37:33 +0100

    asciidocfx 1.8.8

--- Casks/a/asciidocfx.rb
+++ Casks/a/asciidocfx.rb
@@ -1,9 +1,9 @@
 cask "asciidocfx" do
   arch arm: "_M1"
 
-  version "1.8.7"
-  sha256 arm:   "f6aac9a46d46461cc9b99fbc0e4e82cdabcd188dda87f8e3aea33439aadce433",
-         intel: "b629a15a41df756416847cacb03b06b3068a5fdc3cba511622c332e43d524392"
+  version "1.8.8"
+  sha256 arm:   "05f71fea8d821b1245c8bc8c33efcdca71809b21356926032379bfa9ddc594fb",
+         intel: "89b93a3c59b1275629cc0bb9fa872e304a0d1e8faa7dd3e9943e52d8a43cc6cf"
 
   url "https://github.com/asciidocfx/AsciidocFX/releases/download/v#{version}/AsciidocFX_Mac#{arch}.dmg",
       verified: "github.com/asciidocfx/AsciidocFX/"
