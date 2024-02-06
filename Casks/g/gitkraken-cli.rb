7bcffef42755e184e0b9dd93408f329d77efae5d
Sergio <86774052+sergiolms@users.noreply.github.com>
2024-02-06 18:28:05 +0000

    gitkraken-cli 1.5.1

--- Casks/g/gitkraken-cli.rb
+++ Casks/g/gitkraken-cli.rb
@@ -1,9 +1,9 @@
 cask "gitkraken-cli" do
   arch arm: "macOS_arm64", intel: "macOS_x86_64"
 
-  version "1.5.0"
-  sha256 arm:   "bfed7b2b73f29c4565781baa0b1bb45d803d61dc7d485aae67c0ad42dc7e4a41",
-         intel: "64e87ec6615d2d5a495a4cc7a8a1b27236c6ebb325576ac98fca48e817a2f9fc"
+  version "1.5.1"
+  sha256 arm:   "0d2191829d02bd435a48d16ef4e28408457aed1b56d10b6bffd88d79ed7a02d6",
+         intel: "af59717e612fd120918b602f13f2c96cea4345c254a81c3e98dc0d7c0f5a9d77"
 
   url "https://github.com/gitkraken/gk-cli/releases/download/v#{version}/gk_#{version}_#{arch}.zip"
   name "GitKraken CLI"
