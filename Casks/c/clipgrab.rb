14e95b5e3a3a7fc181e7d8e724012c92c47c9361
Bevan Kay <email@bevankay.me>
2024-02-07 19:07:02 +0000

    clipgrab 3.9.10

--- Casks/c/clipgrab.rb
+++ Casks/c/clipgrab.rb
@@ -1,18 +1,15 @@
 cask "clipgrab" do
-  version "3.9.7,1010"
-  sha256 "514fc0e8ec9c37aface3eb070710de7dae329794dda63aac5d1e40d629220399"
+  version "3.9.10"
+  sha256 "13bf212540cd2ed89299f40f70505c9d1209752500e63ded1967aedd266f32f4"
 
-  url "https://download.clipgrab.org/ClipGrab-#{version.csv.first}-cpython-#{version.csv.second}.dmg"
+  url "https://download.clipgrab.org/ClipGrab-#{version}.dmg"
   name "ClipGrab"
   desc "Downloads videos and audio from websites"
   homepage "https://clipgrab.org/"
 
   livecheck do
     url :homepage
-    regex(%r{href=.*?/ClipGrab[._-]v?(\d+(?:\.\d+)+)[._-]cpython[._-](\d+)\.dmg}i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
-    end
+    regex(/href=.*?ClipGrab[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
   depends_on macos: ">= :sierra"
