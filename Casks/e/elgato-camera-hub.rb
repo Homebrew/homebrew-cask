be93b4d3f2cafe98fac099e58ebd6b46094393de
Bevan Kay <email@bevankay.me>
2024-02-07 20:18:02 +0000

    elgato-camera-hub: update livecheck

--- Casks/e/elgato-camera-hub.rb
+++ Casks/e/elgato-camera-hub.rb
@@ -8,8 +8,10 @@ cask "elgato-camera-hub" do
   homepage "https://www.elgato.com/ww/en/s/downloads"
 
   livecheck do
-    url "https://www.elgato.com/graphql?query=query%20contentJson(%24identifier%3A%5BString%5D%24contentType%3AString%24options%3AContentJsonOptionsInput)%7BcontentJson(identifiers%3A%24identifier%20contentType%3A%24contentType%20options%3A%24options)%7Bidentifier%20entries%7D%7D&operationName=contentJson&variables=%7B%22contentType%22%3A%22downloads%22%2C%22identifier%22%3A%5B%22downloads%22%5D%2C%22options%22%3A%7B%22level%22%3A1%7D%7D&locale=en-US"
-    regex(/Camera[._-]Hub[._-]v?(\d+(?:\.\d+)+)\.pkg/i)
+    url "https://gc-updates.elgato.com/mac/echm-update/final/app-version-check.json.php"
+    strategy :json do |json|
+      json.dig("Manual", "Version")
+    end
   end
 
   depends_on macos: ">= :big_sur"
