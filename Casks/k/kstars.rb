7910779253ab731b9043ed1a1170b9a149ce5085
Rui Chen <rui@chenrui.dev>
2024-02-07 15:05:03 +0100

    kstars 3.6.9

--- Casks/k/kstars.rb
+++ Casks/k/kstars.rb
@@ -1,6 +1,6 @@
 cask "kstars" do
-  version "3.6.8"
-  sha256 "6b24d03e4edc70d34cc99255c4bd21099081c6f098adfc89d3985f3cbb493981"
+  version "3.6.9"
+  sha256 "afb69e848195e12d437057596ccaf9bd46175427ade7ae7d98ca00c61a4a5655"
 
   url "https://www.indilib.org/jdownloads/kstars/kstars-#{version}.dmg",
       verified: "indilib.org/jdownloads/kstars/"
