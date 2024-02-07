a03ee6ac42d745ec9681497cb978c7d39d612f05
Rui Chen <rui@chenrui.dev>
2024-02-07 14:42:58 +0100

    zettlr 3.0.5

--- Casks/z/zettlr.rb
+++ Casks/z/zettlr.rb
@@ -1,9 +1,9 @@
 cask "zettlr" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.0.3"
-  sha256 arm:   "0f4e45815309223623d3a5b798b12014dd71d77144a978f13b3f2ea7fcf63aa0",
-         intel: "e7ec0ecb50917c155308cf67606f22b46e39c08c25a115d487626d310af08114"
+  version "3.0.5"
+  sha256 arm:   "b94090ac66458b8dd08ed0805c1d14e7d9bdf9fd337be2a86ce8ad793cfb00cd",
+         intel: "7f29702fcf0f0c458e0c6bcf00e88c712d9c5f85d2b9883dd9c7f5aed29c651b"
 
   url "https://github.com/Zettlr/Zettlr/releases/download/v#{version}/Zettlr-#{version}-#{arch}.dmg"
   name "Zettlr"
