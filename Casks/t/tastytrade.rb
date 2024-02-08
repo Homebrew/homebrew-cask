9ec8ff5649ca4f4b5d45e5bd3688a8431f677505
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-02-08 18:13:48 +0100

    tastytrade 2.9.0

--- Casks/t/tastytrade.rb
+++ Casks/t/tastytrade.rb
@@ -1,9 +1,9 @@
 cask "tastytrade" do
   arch arm: "-aarch64"
 
-  version "2.8.0"
-  sha256 arm:   "62532bc7b23a439e501c0eff1c3bab216adcbc5c64c05539d06fcdf5d1705088",
-         intel: "98ace9292f65022780328fb3a61c98c6542e62046390770d16b8942a9b9e6be8"
+  version "2.9.0"
+  sha256 arm:   "3b0dcaac8da21804cf1db10a7cff0c097d377682f631aa1bf4645bd5ef402306",
+         intel: "cb2962dcfce96601a3b915556d6d5322268239d4110606749e8b2b55a19828f2"
 
   url "https://download.tastytrade.com/desktop-#{version.major}.x.x/#{version}/tastytrade-#{version}#{arch}.dmg"
   name "tastytrade"
