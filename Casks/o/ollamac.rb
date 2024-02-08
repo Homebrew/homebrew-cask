df79890be1b5896c057fa9b0526c2ff1e654a2cd
Dakota Chambers <dakotachambers@gmail.com>
2024-02-08 15:19:56 -0600

    Add ollamac 1.1.1 (new cask)

---
+++ Casks/o/ollamac.rb
@@ -0,0 +1,16 @@
+cask "ollamac" do
+  version "1.1.1"
+  sha256 "345bf77419a81c16c1b43fcc26d8d92f039fc79852c5c3f2462d9c4582d67610"
+
+  url "https://github.com/kevinhermawan/Ollamac/releases/download/v#{version}/Ollamac-#{version}.dmg",
+    verified: "https://github.com/kevinhermawan/Ollamac/"
+  name "Ollamac"
+  desc "Interact with Ollama models"
+  homepage "https://github.com/kevinhermawan/Ollamac"
+
+  auto_updates true
+  depends_on macos: ">= :sonoma"
+
+  app "Ollamac.app"
+
+end
