f54659fc1ce7a1cea240b903b095e01fdcd5c82c
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-02-09 07:09:57 -0500

    atlauncher 3.4.35.7

--- Casks/a/atlauncher.rb
+++ Casks/a/atlauncher.rb
@@ -1,6 +1,6 @@
 cask "atlauncher" do
-  version "3.4.35.6"
-  sha256 "6ae83102cf61aa3d2d7ae48dd57ba732e4b03d12d6711a63fdfac8adde278c91"
+  version "3.4.35.7"
+  sha256 "b607443bbae7beb32f5c0cb18215f6a6fab9086e8fe4241c7e0e32ff681556bb"
 
   url "https://github.com/ATLauncher/ATLauncher/releases/download/v#{version}/ATLauncher-#{version}.zip",
       verified: "github.com/ATLauncher/ATLauncher/"
