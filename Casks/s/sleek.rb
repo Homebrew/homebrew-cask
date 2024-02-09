1a1bdbe4fa651f8faf2ffba856c4fad214f082ec
ransome <11188741+ransome1@users.noreply.github.com>
2024-02-09 23:42:41 +0100

    Update sleek.rb

--- Casks/s/sleek.rb
+++ Casks/s/sleek.rb
@@ -1,4 +1,5 @@
 cask "sleek" do
+  arch arm: "arm64", intel: "x64"
   version "2.0.9"
   sha256  arm:   "ad58b9fafa600488ea3027bbad0354ec2c7c46afc8759a6bb6912debc553329a",
           intel: "b9241e89449d5e40fb68e7770829577161de9919d83155d5e88e3adba49b92e6"
