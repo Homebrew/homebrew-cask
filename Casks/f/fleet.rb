19288780153440f9edd6633610b18cb80b1ff444
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-02-09 19:51:00 +0100

    fleet 1.30.83

--- Casks/f/fleet.rb
+++ Casks/f/fleet.rb
@@ -2,9 +2,9 @@ cask "fleet" do
   arch arm: "aarch64", intel: "x64"
   arch_suffix = on_arch_conditional arm: "-aarch64"
 
-  version "1.29.216"
-  sha256 arm:   "fbc116cb0f9e456692e8753174505955734dae6beb657d7cb98e082f9afd4c4e",
-         intel: "f938eb2a4de0501e3ba69893fa11e43ee1ed4de54ae1e1a42f5efcef6c5b0a97"
+  version "1.30.83"
+  sha256 arm:   "90074311b627987dbb6db6e53111135e5a570159fe3c656654d7458c1b91c6ae",
+         intel: "c5b7bd49939789c0d9e0c2709090f5fc5dab565f39f19f1218269d300d2cebbc"
 
   url "https://download.jetbrains.com/fleet/installers/macos_#{arch}/Fleet-#{version}#{arch_suffix}.dmg"
   name "Fleet"
