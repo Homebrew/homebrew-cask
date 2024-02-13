beda6f76d297054a5c08778a0613894d90ed503a
Miccal Matthews <miccal.matthews@gmail.com>
2024-02-14 07:16:52 +0800

    Update intellij-idea-ce.rb

--- Casks/i/intellij-idea-ce.rb
+++ Casks/i/intellij-idea-ce.rb
@@ -21,7 +21,7 @@ cask "intellij-idea-ce" do
   end
 
   auto_updates true
-  conflicts_with cask: "homebrew/cask-versions/intellij-idea-ce19"
+  conflicts_with cask: "intellij-idea-ce19"
   depends_on macos: ">= :high_sierra"
 
   app "IntelliJ IDEA CE.app"
