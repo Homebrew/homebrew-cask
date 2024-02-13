1b2e0606e6f2cd2edd88229a4f995ef8aebc6ab5
Rui Chen <rui@chenrui.dev>
2024-02-13 10:19:34 +0100

    siyuan 2.12.8

--- Casks/s/siyuan.rb
+++ Casks/s/siyuan.rb
@@ -1,9 +1,9 @@
 cask "siyuan" do
   arch arm: "-arm64"
 
-  version "2.12.7"
-  sha256 arm:   "80379602bd1f454e709e13bca5cba4dd1038dbf25736e3cbaf17bf0c9ebb5738",
-         intel: "66670ebc9a4946991685f7c72d6c19e307f0e165f1c207ac9d5a1803f85d41f3"
+  version "2.12.8"
+  sha256 arm:   "51f75c7a97ed74148b1083a2d85aa5f8f286d654dbf614d1041c3aef6652ecd8",
+         intel: "fd0516e1b3fe7a65035e6d492e8db5e4c178b7e014950df7782cb43118c4550a"
 
   url "https://github.com/siyuan-note/siyuan/releases/download/v#{version}/siyuan-#{version}-mac#{arch}.dmg"
   name "SiYuan"
