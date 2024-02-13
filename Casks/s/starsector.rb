d6dda2af6bfe91265942b83f658ce5ef807f69bd
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-02-12 19:50:17 -0500

    starsector 0.97a-RC9

--- Casks/s/starsector.rb
+++ Casks/s/starsector.rb
@@ -1,6 +1,6 @@
 cask "starsector" do
-  version "0.97a-RC8"
-  sha256 "814901c90b9a7575b1c369346afdd6088c169ef6a3cf03688831c52b87f8e9ea"
+  version "0.97a-RC9"
+  sha256 "e77e87e191e284d9f2e1880e6295f2f1479826b42ed254c148cb05d5791ec11b"
 
   url "https://f005.backblazeb2.com/file/fractalsoftworks/release/starsector_mac-#{version}.zip",
       verified: "f005.backblazeb2.com/file/fractalsoftworks/"
