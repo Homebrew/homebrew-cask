3d361aa6a04110a5fcb5dd10ed04016c9b4baea5
Michael Stahnke <stahnke@floxdev.com>
2024-02-13 23:12:00 -0600

    Add Flox as new Cask
    See https://flox.dev for more information.

---
+++ Casks/f/flox.rb
@@ -0,0 +1,54 @@
+cask "flox" do
+  arch arm: "aarch64", intel: "x86_64"
+
+  version "1.0.3"
+  sha256 arm:   "08c5c45ba72809c208405002f0200246fc90fc44cf09ac7298bc2c3258ef3b4f",
+         intel: "e03da4432d564fa9099ac256f472885eb8b6ab074b6d6a70378396b2b4641464"
+
+  url "https://downloads.flox.dev/by-env/stable/osx/flox-#{version}.#{arch}-darwin.pkg"
+  name "flox"
+  desc "Manages environments across the software lifecycle"
+  homepage "https://flox.dev/"
+
+  livecheck do
+    url "https://downloads.flox.dev/by-env/stable/LATEST_VERSION"
+    strategy :page_match do |page|
+      match = page.match(/(\d+(?:\.\d+)+)/)
+      match ? match[0] : nil
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
+  pkg "flox-#{version}.#{arch}-darwin.pkg"
+
+  uninstall early_script: {
+              executable: "/usr/bin/killall",
+              args:       ["-9", "pkgdb"],
+              sudo:       true,
+            },
+            launchctl:    [
+              "org.nixos.darwin-store",
+              "org.nixos.nix-daemon",
+            ],
+            quit:         [
+              "org.nixos.darwin-store",
+              "org.nixos.nix-daemon",
+            ],
+            script:       {
+              executable: "/usr/local/share/flox/scripts/uninstall",
+              sudo:       true,
+            },
+            pkgutil:      "com.floxdev.flox",
+            delete:       [
+              "/opt/homebrew/Caskroom/flox",
+              "~/.cache/flox",
+            ]
+
+  zap trash: [
+    "/opt/homebrew/Caskroom/flox",
+    "~/.cache/flox",
+    "~/.config/flox",
+  ]
+end
