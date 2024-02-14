c002995daf70f991c41beb1d8f264c1a3df20c41
Rui Chen <rui@chenrui.dev>
2024-02-14 11:18:42 +0100

    workspace-one-intelligent-hub 24.01.0.10

--- Casks/w/workspace-one-intelligent-hub.rb
+++ Casks/w/workspace-one-intelligent-hub.rb
@@ -1,5 +1,5 @@
 cask "workspace-one-intelligent-hub" do
-  version "23.10.0.16"
+  version "24.01.0.10"
   sha256 :no_check
 
   url "https://packages.vmware.com/wsone/VMwareWorkspaceONEIntelligentHub.pkg",
