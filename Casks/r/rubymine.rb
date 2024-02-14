a8e0c543c9bd4cd1ff2e7aadecabc162a6d70e91
Rui Chen <rui@chenrui.dev>
2024-02-14 10:49:02 +0100

    rubymine 2023.3.4

--- Casks/r/rubymine.rb
+++ Casks/r/rubymine.rb
@@ -1,9 +1,9 @@
 cask "rubymine" do
   arch arm: "-aarch64"
 
-  version "2023.3.3,233.14015.117"
-  sha256 arm:   "1e9b62b495709c5d40af0aa9e5bdca21658dc23339659bcc3d5666ef45686281",
-         intel: "5fcba619de2c19b969aa6ce200c859a196de6597476cd9c31b8ffef415486b8b"
+  version "2023.3.4,233.14475.27"
+  sha256 arm:   "bbb442f4099567bbd632e9997385fde7ea844927458c9731a0e21aeb403092e8",
+         intel: "4527e537bf953d3c33de9d0cb8d2d566ef65fbe4234c7737bb82c1955c90e923"
 
   url "https://download.jetbrains.com/ruby/RubyMine-#{version.csv.first}#{arch}.dmg"
   name "RubyMine"
