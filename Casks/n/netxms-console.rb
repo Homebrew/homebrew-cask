a4ca57a2b98eef9705c634883308f74c0d9e2829
Rui Chen <rui@chenrui.dev>
2024-02-15 07:25:08 +0100

    netxms-console: update url
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/n/netxms-console.rb
+++ Casks/n/netxms-console.rb
@@ -5,10 +5,10 @@ cask "netxms-console" do
   sha256 arm:   "5edbb4166bc6b7c855be1cec17aa59e3bf2f3fdada948824bcef123fd75ecfdc",
          intel: "929721b8225392b6926cb35a2f4a44ca4e5a8a408101290d0d8e18ca32983b4f"
 
-  url "https://netxms.org/download/releases/#{version.major_minor}/nxmc-#{version}#{arch}.dmg"
+  url "https://netxms.com/download/releases/#{version.major_minor}/nxmc-#{version}#{arch}.dmg"
   name "NetXMS Management Console"
   desc "Network and infrastructure monitoring and management system"
-  homepage "https://netxms.org/"
+  homepage "https://netxms.com/"
 
   livecheck do
     url "https://netxms.com/downloads/"
