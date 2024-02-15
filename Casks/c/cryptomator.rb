7c62d9125724d4b69bdef7ffbd5f1c3eeb222dbc
Patrick Linnane <patrick@linnane.io>
2024-02-15 15:26:07 -0800

    cryptomator: add livecheck
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/c/cryptomator.rb
+++ Casks/c/cryptomator.rb
@@ -11,6 +11,25 @@ cask "cryptomator" do
   desc "Multi-platform client-side cloud file encryption tool"
   homepage "https://cryptomator.org/"
 
+  # Not every GitHub release provides a file for macOS, so we check multiple
+  # recent releases instead of only the "latest" release.
+  livecheck do
+    url :url
+    regex(/^Cryptomator[._-]v?(\d+(?:\.\d+)+).*?\.(?:dmg|pkg)$/i)
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        next if release["draft"] || release["prerelease"]
+
+        release["assets"]&.map do |asset|
+          match = asset["name"]&.match(regex)
+          next if match.blank?
+
+          match[1]
+        end
+      end.flatten
+    end
+  end
+
   depends_on macos: ">= :high_sierra"
 
   app "Cryptomator.app"
