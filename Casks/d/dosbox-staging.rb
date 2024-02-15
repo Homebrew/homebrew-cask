2e9cb5bd02c7f1683b2767f5b82e66006d3481bc
Burrito78 <61995128+Burrito78@users.noreply.github.com>
2024-02-15 20:20:46 +0100

    Forgot to change version number to current one.

--- Casks/d/dosbox-staging.rb
+++ Casks/d/dosbox-staging.rb
@@ -1,5 +1,5 @@
 cask "dosbox-staging" do
-  version "0.80.1"
+  version "0.81.0"
   sha256 "5d3cec60552b2a34caa70702b35bdf9ce591cf0b03143802351725a7c729d9fe"
 
   url "https://github.com/dosbox-staging/dosbox-staging/releases/download/v#{version}/dosbox-staging-macOS-v#{version}.dmg"
