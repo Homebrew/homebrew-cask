32e92e7d96111353bdd9d7a58c9d172509485c65
Dakota Chambers <dakotachambers@gmail.com>
2024-02-15 12:58:22 -0600

    Update github url in ollama.rb
    The github owner for Ollama changed. Currently github redirected from the old URL to the new project home (ollama/ollama). This makes it explicit in the Homebrew cask.

--- Casks/o/ollama.rb
+++ Casks/o/ollama.rb
@@ -2,8 +2,8 @@ cask "ollama" do
   version "0.1.25"
   sha256 "89ade906f6c69c6b4451d69dcd33b36817375acdcc61137a3010de2ebeb9151e"
 
-  url "https://github.com/jmorganca/ollama/releases/download/v#{version}/Ollama-darwin.zip",
-      verified: "github.com/jmorganca/ollama/"
+  url "https://github.com/ollama/ollama/releases/download/v#{version}/Ollama-darwin.zip",
+      verified: "github.com/ollama/ollama/"
   name "Ollama"
   desc "Get up and running with large language models locally"
   homepage "https://ollama.ai/"
