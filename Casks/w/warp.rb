f03b94ac3372aff10abede500c88724c60637b3d
Aloke Desai <aloke.desai@gmail.com>
2024-02-16 16:12:19 -0500

    Update warp version to the correct stable build

--- Casks/w/warp.rb
+++ Casks/w/warp.rb
@@ -1,16 +1,11 @@
 cask "warp" do
-  version "0.2024.02.16.17.24.stable_00"
-  sha256 "403bc06f399f5e735d58f2050ea023c4ccc7564207b7bac17eac9450f8c32272"
+  version "0.2024.02.13.08.02.stable_00"
+  sha256 "1c4d6bc23d1956fae36512c364839706d0ae467d30a96f251efec7e55c283073"
 
   url "https://app.warp.dev/download/brew?version=v#{version}"
   name "Warp"
   desc "Rust-based terminal"
   homepage "https://www.warp.dev/"
-
-  livecheck do
-    url "https://storage.googleapis.com/warp-releases/channel_versions.json"
-    regex(/v(\d+(?:\.\d+)+\.stable_\d+)/i)
-  end
 
   auto_updates true
 
