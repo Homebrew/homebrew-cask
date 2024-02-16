988727ae78dbad04ff76b9022b5fded37893a2d9
Patrick Linnane <patrick@linnane.io>
2024-02-16 00:02:37 +0000

    fossa 3.9.5

--- Casks/f/fossa.rb
+++ Casks/f/fossa.rb
@@ -1,6 +1,6 @@
 cask "fossa" do
-  version "3.9.4"
-  sha256 "cc60870d099018f2b7a3ec44656e73031463e20e3a6c6e82b97b71dc576dedbb"
+  version "3.9.5"
+  sha256 "24afb6df57d648f235008445455549439ba646f207a7fe85135b048bdbb9ee9c"
 
   url "https://github.com/fossas/fossa-cli/releases/download/v#{version}/fossa_#{version}_darwin_amd64.zip",
       verified: "github.com/fossas/fossa-cli/"
