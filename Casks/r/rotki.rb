1fe5362068eecfa7ddccc4ceab3ed7782227aa0c
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-02-16 09:37:51 -0500

    rotki 1.32.0

--- Casks/r/rotki.rb
+++ Casks/r/rotki.rb
@@ -1,6 +1,6 @@
 cask "rotki" do
-  version "1.31.3"
-  sha256 "84ed46dd3daaf70ba9bd8daa562218db1237d79fefe95bca74daec95845dd6f5"
+  version "1.32.0"
+  sha256 "eade0ef64a43b369b6c06174c1eeb8b75cabb9585b02f67c4a48813e767797a8"
 
   url "https://github.com/rotki/rotki/releases/download/v#{version}/rotki-darwin_x64-v#{version}.dmg",
       verified: "github.com/rotki/rotki/"
