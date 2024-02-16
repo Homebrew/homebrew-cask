19060d725821d75d72d646dc7d004f3782d7962e
miccal <miccal.matthews@gmail.com>
2024-02-16 09:31:06 +0800

    microsoft-onenote 16.82.24021116

--- Casks/m/microsoft-onenote.rb
+++ Casks/m/microsoft-onenote.rb
@@ -1,6 +1,6 @@
 cask "microsoft-onenote" do
-  version "16.81.24011420"
-  sha256 "194fa90eb4cec54d09395aaf9ab8275629eb2c6260d76ead345a26ee8c6901ee"
+  version "16.82.24021116"
+  sha256 "77898172322656167c2e959b755d9a0078a8a2be4f869f71f418e8e7b0713ac4"
 
   url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_OneNote_#{version}_Updater.pkg"
   name "Microsoft OneNote"
