057d16bdc970ed856c4db0cd85e30be53875d3f0
gnought <1684105+gnought@users.noreply.github.com>
2024-02-18 13:00:26 +0800

    revert arch changes

--- Casks/v/visual-studio-code.rb
+++ Casks/v/visual-studio-code.rb
@@ -1,15 +1,18 @@
 cask "visual-studio-code" do
-  version "1.86.2"
-  sha256 "b9fe35872597f54afa0391bbea7470ae847074051067c09daabbfb4b1388b5ff"
+  arch arm: "darwin-arm64", intel: "darwin"
 
-  url "https://update.code.visualstudio.com/#{version}/darwin-universal/stable"
+  version "1.86.2"
+  sha256 arm:   "b82dcb4bc6b2316ef4fdc5a0ab924152758a1a0e437831bd63d501d717e36b6f",
+         intel: "0ac71d38cf0a50c8757153a8db238e60d33d50c1559d26998b5c002af69f6b8b"
+
+  url "https://update.code.visualstudio.com/#{version}/#{arch}/stable"
   name "Microsoft Visual Studio Code"
   name "VS Code"
   desc "Open-source code editor"
   homepage "https://code.visualstudio.com/"
 
   livecheck do
-    url "https://update.code.visualstudio.com/api/update/darwin-universal/stable/latest"
+    url "https://update.code.visualstudio.com/api/update/#{arch}/stable/latest"
     strategy :json do |json|
       json["productVersion"]
     end
