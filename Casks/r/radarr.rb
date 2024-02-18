80c6fb1aae5f416b5c86383f0097ff67566b591a
Bevan Kay <email@bevankay.me>
2024-02-19 09:21:31 +1100

    radarr 5.3.6.8612

--- Casks/r/radarr.rb
+++ Casks/r/radarr.rb
@@ -1,9 +1,9 @@
 cask "radarr" do
   arch arm: "arm64", intel: "x64"
 
-  version "5.2.6.8376"
-  sha256 arm:   "f3fb5837df392d20e672fb79ff8c5f7004a0ff868be23e84bcf4d3e856f6f9c5",
-         intel: "8f1688ccad85eee915afd1572310f7848ef89f581295be334ef7d251263fea92"
+  version "5.3.6.8612"
+  sha256 arm:   "1a96af7f7399262fd4f5ca2c65a6cd2ac0c342728426c6632df97ad8306de1b7",
+         intel: "f202d66936bff8e57bc6712e38ec48117683a8d9d732cb9d68d92080175c5f89"
 
   url "https://github.com/Radarr/Radarr/releases/download/v#{version}/Radarr.master.#{version}.osx-app-core-#{arch}.zip",
       verified: "github.com/Radarr/Radarr/"
