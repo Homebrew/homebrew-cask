1fb12a50f828da6a306c9665c6b633d9bf44dae5
Bevan Kay <email@bevankay.me>
2024-02-19 09:01:32 +1100

    subsurface: add `user_agent` argument to `url`

--- Casks/s/subsurface.rb
+++ Casks/s/subsurface.rb
@@ -2,7 +2,8 @@ cask "subsurface" do
   version "6.0.5094"
   sha256 "4b7f26abeb05c3b538c307fa6dec6724f691b9f1565d09dc38700045adb5c55c"
 
-  url "https://subsurface-divelog.org/downloads/Subsurface-#{version}-CICD-release.dmg"
+  url "https://subsurface-divelog.org/downloads/Subsurface-#{version}-CICD-release.dmg",
+      user_agent: :fake
   name "Subsurface"
   desc "Open source divelog program"
   homepage "https://subsurface-divelog.org/"
