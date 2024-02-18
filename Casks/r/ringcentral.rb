347988dd4e71d8a89fe8711f73d3147970e22d0b
Bevan Kay <email@bevankay.me>
2024-02-19 09:20:35 +1100

    ringcentral: use versioned url, rebuild

--- Casks/r/ringcentral.rb
+++ Casks/r/ringcentral.rb
@@ -2,9 +2,10 @@ cask "ringcentral" do
   arch arm: "-arm64"
 
   version "24.1.10"
-  sha256 :no_check
+  sha256 arm:   "279b79671fb5f939d1579f00de0d93e4f027dc973e4c31b267eef776737650e1",
+         intel: "3117a3fce245d1791fa2f1aa0493970c2f52733d946eb17e7fe27e0c385b7ebe"
 
-  url "https://app.ringcentral.com/download/RingCentral#{arch}.pkg"
+  url "https://app.ringcentral.com/download/RingCentral-#{version}#{arch}.zip"
   name "RingCentral"
   desc "Team messaging, video meetings, and business phone"
   homepage "https://www.ringcentral.com/download.html"
@@ -17,16 +18,15 @@ cask "ringcentral" do
   auto_updates true
   depends_on macos: ">= :high_sierra"
 
-  pkg "RingCentral#{arch}.pkg"
+  app "RingCentral.app"
 
-  uninstall quit:    "RingCentral",
-            pkgutil: "com.ringcentral.glip",
-            delete:  "/Applications/RingCentral.app"
+  uninstall quit: "RingCentral"
 
   zap trash: [
-    "~/Library/Application Support/RingCentral",
-    "~/Library/Logs/RingCentral",
-    "~/Library/Preferences/com.ringcentral.glip.plist",
-    "~/Library/Saved Application State/com.ringcentral.glip.savedState",
-  ]
+        "~/Library/Application Support/RingCentral",
+        "~/Library/Logs/RingCentral",
+        "~/Library/Preferences/com.ringcentral.glip.plist",
+        "~/Library/Saved Application State/com.ringcentral.glip.savedState",
+      ],
+      rmdir: "~/Documents/RingCentral"
 end
