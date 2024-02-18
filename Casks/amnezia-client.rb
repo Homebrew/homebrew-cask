fbcc4ed72167a622a8af0e0e4242af0cc5b4e3a6
Yaroslav Slepukhin <y.i.slepukhin@ringcentral.com>
2024-02-18 17:22:43 +0400

    Add Amnezia VPN client cask

---
+++ Casks/amnezia-client.rb
@@ -0,0 +1,20 @@
+cask "amnezia-client" do
+  version "4.1.0.1"
+  sha256 "c421f28697fd9200fd9dbcdd111fa2778fe8d01b45e8ae672886399181c780cb"
+
+  url "https://github.com/amnezia-vpn/amnezia-client/releases/download/#{version}/AmneziaVPN_#{version}.dmg",
+      verified: "github.com/amnezia-vpn/amnezia-client/"
+  name "Amnezia VPN"
+  desc "Amnezia is an open-source VPN client"
+  homepage "https://amnezia.org/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "AmneziaVPN.app"
+end
