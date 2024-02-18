a00a52152f3c7cb73c1a6dd5dd31cb4df7c85846
rich2014 <rich2014.git@outlook.com>
2024-02-18 15:54:38 +0800

    double-commander 1.1.10 (strategy removed)

--- Casks/d/double-commander.rb
+++ Casks/d/double-commander.rb
@@ -14,9 +14,6 @@ cask "double-commander" do
   livecheck do
     url "https://sourceforge.net/projects/doublecmd/rss?path=/macOS"
     regex(%r{url=["']?.*?/doublecmd[._-](\d+(?:\.\d+)+)[._-][^"']*?.dmg}i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| (match[0]).to_s }
-    end
   end
 
   app "Double Commander.app"
