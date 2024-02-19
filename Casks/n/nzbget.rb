384fef9a39df23eccbfbdb568fdd05c12a09fe0b
Sam Ford <1584702+samford@users.noreply.github.com>
2024-02-19 08:24:41 -0500

    nzbget: deprecate

--- Casks/n/nzbget.rb
+++ Casks/n/nzbget.rb
@@ -8,10 +8,7 @@ cask "nzbget" do
   desc "Usenet downloader focusing on efficiency"
   homepage "https://nzbget.net/"
 
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
+  deprecate! date: "2024-02-19", because: :discontinued
 
   app "NZBGet.app"
 
