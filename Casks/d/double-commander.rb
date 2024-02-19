61a6397754dfc57c178a1a32b0aed71b710e2c3b
rich2014 <rich2014.git@outlook.com>
2024-02-19 22:23:03 +0800

    double-commander 1.1.10 (url updated)
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/d/double-commander.rb
+++ Casks/d/double-commander.rb
@@ -5,7 +5,7 @@ cask "double-commander" do
   sha256 arm:   "4f6fe3b2023c0cc123a5caba6fce27be63f3023b3e9aca91ea2f668659cbd691",
          intel: "3a827eb57095071de16b85a1054e053cceec2ea448c0c1430f43396a0089c9be"
 
-  url "https://downloads.sourceforge.net/doublecmd/doublecmd-#{version}.cocoa.#{arch}.dmg",
+  url "https://downloads.sourceforge.net/doublecmd/doublecmd-#{version.tr(",", "-")}.cocoa.#{arch}.dmg",
       verified: "downloads.sourceforge.net/doublecmd/"
   name "Double Commander"
   desc "File manager with two panels"
