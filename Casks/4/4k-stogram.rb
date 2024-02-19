47cd8f84da4ee595c840bb236c726f1cb5bcf146
Bevan Kay <email@bevankay.me>
2024-02-19 13:58:42 +1100

    4k-stogram 4.8.0

--- Casks/4/4k-stogram.rb
+++ Casks/4/4k-stogram.rb
@@ -1,15 +1,15 @@
 cask "4k-stogram" do
-  version "4.7.0"
-  sha256 "c213dab20ba1571937c304eea25d67f35b05948e6c5c93fe1c1de91a8b69d3cb"
+  version "4.8.0"
+  sha256 "d70edf81120d46a82b8bd0687355dd2929caac8b93c02250dd7646a2cb899817"
 
-  url "https://dl.4kdownload.com/app/4kstogram_#{version}.dmg"
+  url "https://dl.4kdownload.com/app/4kstogram_#{version}_x64.dmg"
   name "4K Stogram"
   desc "Download Instagram photos, accounts, hashtags and locations"
   homepage "https://www.4kdownload.com/products/product-stogram"
 
   livecheck do
     url "https://www.4kdownload.com/downloads"
-    regex(%r{href=.*?/4kstogram[._-]?v?(\d+(?:\.\d+)+)\.dmg}i)
+    regex(%r{href=.*?/4kstogram[._-]?v?(\d+(?:\.\d+)+)(?:[._-]x64)?\.dmg}i)
   end
 
   auto_updates true
