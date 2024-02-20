0464b432944c940d45accfd4a2b72f01e1aad7ce
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-02-20 10:06:03 -0500

    dnsmonitor: updates

--- Casks/d/dnsmonitor.rb
+++ Casks/d/dnsmonitor.rb
@@ -2,11 +2,10 @@ cask "dnsmonitor" do
   version "1.0.1"
   sha256 "ba0a54877cea0e3731271621ee09f4a6877548b5bdcbf785ba38862600bbabaf"
 
-  url "https://github.com/objective-see/DNSMonitor/releases/download/v#{version}/DNSMonitor_#{version}.zip",
-      verified: "github.com/objective-see/DNSMonitor/"
+  url "https://github.com/objective-see/DNSMonitor/releases/download/v#{version}/DNSMonitor_#{version}.zip"
   name "dnsmonitor"
   desc "Monitor DNS activity"
-  homepage ""
+  homepage "https://github.com/objective-see/DNSMonitor/"
 
   livecheck do
     url :url
@@ -16,5 +15,6 @@ cask "dnsmonitor" do
   depends_on macos: ">= :catalina"
 
   app "DNSMonitor.app"
-  binary "#{appdir}/DNSMonitor.app/Contents/MacOS/DNSMonitor", target: "dnsmonitor"
+
+  # No zap stanza required
 end
