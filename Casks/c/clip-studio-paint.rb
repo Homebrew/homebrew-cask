f4558fd30d2583eb31a6b45a239e1e375a25e591
iivusly <subtype.kitsch_0w@icloud.com>
2024-02-20 21:53:04 +0000

    clip-studio-paint 2.3.4

--- Casks/c/clip-studio-paint.rb
+++ Casks/c/clip-studio-paint.rb
@@ -1,6 +1,6 @@
 cask "clip-studio-paint" do
-  version "1.13.2"
-  sha256 "ce0b38fca420b8bf3073385c8e06d66294e86b4cff96f7c844d1a4e10a3c098a"
+  version "2.3.4"
+  sha256 "52303ea331dbdced3b8ae73cec0e9c3f1d5ae7b842bebba8988215262baef6a1"
 
   url "https://vd.clipstudio.net/clipcontent/paint/app/#{version.no_dots}/CSP_#{version.no_dots}m_app.pkg"
   name "Clip Studio Paint"
@@ -8,7 +8,7 @@ cask "clip-studio-paint" do
   homepage "https://www.clipstudio.net/en"
 
   livecheck do
-    url "https://www.clipstudio.net/en/dl/release_note/"
+    url "https://www.clipstudio.net/en/dl/release_note/v2"
     regex(/Clip\s+Studio\s+Paint\s+(?:v|Ver\.?|Version)?\s*(\d+(?:\.\d+)+)/i)
   end
 
