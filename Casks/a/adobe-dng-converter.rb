d3317da56d95cc45cf8c40c7b2373204352af1c8
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-02-20 10:33:46 -0500

    adobe-dng-converter 16.2

--- Casks/a/adobe-dng-converter.rb
+++ Casks/a/adobe-dng-converter.rb
@@ -1,6 +1,6 @@
 cask "adobe-dng-converter" do
-  version "16.1"
-  sha256 "d67247aec390e5dc60f547a5c46041c8e2252944ab0d023a43daed3b70c4aa5b"
+  version "16.2"
+  sha256 "ab55e587e73391bc1321d92ba0737844cc06eaac897d049048eb1c308f3522c1"
 
   url "https://download.adobe.com/pub/adobe/dng/mac/DNGConverter_#{version.dots_to_underscores}.dmg"
   name "Adobe DNG Converter"
