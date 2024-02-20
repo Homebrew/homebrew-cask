d7547342464f53189e43a96486cebe91fa78115a
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-02-20 10:47:52 -0500

    keyboard-cowboy 3.22.2

--- Casks/k/keyboard-cowboy.rb
+++ Casks/k/keyboard-cowboy.rb
@@ -1,6 +1,6 @@
 cask "keyboard-cowboy" do
-  version "3.22.1"
-  sha256 "20f7cbef7ddd54267d749816e6271b42f4cc01352da1e6802f60738f929ad42b"
+  version "3.22.2"
+  sha256 "d31982a0e183f651e053c5b2a9f576c00d81165c94a869ff720689a9aad7f0ca"
 
   url "https://github.com/zenangst/KeyboardCowboy/releases/download/#{version}/Keyboard.Cowboy.#{version}.dmg"
   name "Keyboard Cowboy"
