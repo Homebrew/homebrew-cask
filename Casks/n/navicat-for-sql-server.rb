8d7ac6fb6184ed6bd05bd85011e6e0abb8f9ea80
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-02-21 11:13:21 -0500

    navicat-for-sql-server 16.3.6

--- Casks/n/navicat-for-sql-server.rb
+++ Casks/n/navicat-for-sql-server.rb
@@ -1,5 +1,5 @@
 cask "navicat-for-sql-server" do
-  version "16.3.4"
+  version "16.3.6"
   sha256 :no_check
 
   url "https://dn.navicat.com/download/navicat#{version.major_minor.no_dots}_sqlserver_en.dmg"
