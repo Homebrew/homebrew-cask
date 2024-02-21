8e9ecf231ad66c8c395c3653b8a4379e5982a374
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-02-21 11:13:20 -0500

    navicat-for-sqlite 16.3.6

--- Casks/n/navicat-for-sqlite.rb
+++ Casks/n/navicat-for-sqlite.rb
@@ -1,5 +1,5 @@
 cask "navicat-for-sqlite" do
-  version "16.3.4"
+  version "16.3.6"
   sha256 :no_check
 
   url "https://dn.navicat.com/download/navicat#{version.major_minor.no_dots}_sqlite_en.dmg"
