684ff0b654926ccdcfa29b863093341668bd0ced
Rui Chen <rui@chenrui.dev>
2024-02-22 11:20:08 -0500

    phpwebstudy 2.0.1

--- Casks/p/phpwebstudy.rb
+++ Casks/p/phpwebstudy.rb
@@ -1,9 +1,9 @@
 cask "phpwebstudy" do
   arch arm: "-arm64"
 
-  version "2.0.0"
-  sha256 arm:   "6c18db6122d6f0f03113bdf6a5964ed6af3e9dad54578b11248c4a25405ff1e8",
-         intel: "1324d7472710d85d0a930d0cea7930269c54d1332acedef3dd2f466f40da8aa7"
+  version "2.0.1"
+  sha256 arm:   "f89be1159d2a1a8017df56bde58a42ef8e637cb96c522b60eaa8335191278bd7",
+         intel: "a80da58693a30e8d6027c1290aa2b9913034c431069ef6a61a76372395f9699a"
 
   url "https://github.com/xpf0000/PhpWebStudy/releases/download/v#{version}/PhpWebStudy-#{version}#{arch}-mac.zip",
       verified: "github.com/xpf0000/PhpWebStudy/"
