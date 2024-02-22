b8aecf7272e87490fa5eddde567b94e75b74bb78
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-02-22 09:11:14 -0500

    elephicon 2.9.1

--- Casks/e/elephicon.rb
+++ Casks/e/elephicon.rb
@@ -1,9 +1,9 @@
 cask "elephicon" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.9.0"
-  sha256 arm:   "34e789c4f8214cd2c8f1d35ede73e03497b6ae87ff4b610c316f8525bafa94c4",
-         intel: "1eb6770eaf8d69f74ebeecc040b5907c08b0b7dbac4d9dec55bb679e22bda070"
+  version "2.9.1"
+  sha256 arm:   "2ea2e09792c799d6caef39ae7f3b00a6fb7fcaca0820ccec46bf04754f1c5932",
+         intel: "eb486e8f4f0a2658ca1d4f076ccbebf771e901cedda9dd22a09d887e265d11db"
 
   url "https://github.com/sprout2000/elephicon/releases/download/v#{version}/Elephicon-#{version}-darwin-#{arch}.dmg"
   name "Elephicon"
