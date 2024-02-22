44192fb6f8b4a25c7af7e81ded20950724cfdcdf
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-02-22 09:01:10 -0500

    flirc 3.27.12

--- Casks/f/flirc.rb
+++ Casks/f/flirc.rb
@@ -1,6 +1,6 @@
 cask "flirc" do
-  version "3.27.11"
-  sha256 "d20aeede60e8cd42c05c130bc35fcde5cddbdaf423a8054ccb0e126254ea327f"
+  version "3.27.12"
+  sha256 "a8193d568ace8d8ccc291fc5d3341ecddd51abec1170b0b6d5e58f3b742d0b07"
 
   url "https://flirc.com/software/flirc-usb/GUI/release/mac/Flirc-#{version}.dmg"
   name "Flirc"
