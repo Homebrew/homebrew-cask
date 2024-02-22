0aff0dcedac0f397faf50a2bf8f05a5c1e3ae6c0
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-02-22 16:58:31 -0500

    slack-cli 2.18.0

--- Casks/s/slack-cli.rb
+++ Casks/s/slack-cli.rb
@@ -1,6 +1,6 @@
 cask "slack-cli" do
-  version "2.17.1"
-  sha256 "b8c4b87667ecfc72734bc7ae0f19b78555000de16a80072dce5829eb1969a8ef"
+  version "2.18.0"
+  sha256 "12ba88987613f5b415061702e8a92fb79a13f23b0c6575a826c174334d79bba0"
 
   url "https://downloads.slack-edge.com/slack-cli/slack_cli_#{version}_macOS_64-bit.tar.gz",
       verified: "downloads.slack-edge.com/slack-cli/"
