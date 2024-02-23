b8f01d5c994edd0e4d2bf0463f33d8fe305eacd0
Rui Chen <rui@chenrui.dev>
2024-02-23 10:28:58 -0500

    aifun 0.8.0

--- Casks/a/aifun.rb
+++ Casks/a/aifun.rb
@@ -1,6 +1,6 @@
 cask "aifun" do
-  version "0.7.6"
-  sha256 "f319784cfb89ab6caad6ecc03ac6c7c175b951c3b80007cd01f8c834474dcde1"
+  version "0.8.0"
+  sha256 "91d9097071b891292b4934d563488b044d168260cd12e3f0fed7494ac0a5005e"
 
   url "https://static.aifun.kapokcloud.com/v#{version}/AiFun_#{version}_macos.dmg",
       verified: "static.aifun.kapokcloud.com/"
