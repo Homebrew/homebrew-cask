68959f01e65d4e875af761a724462ab3e3131126
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-02-23 09:09:07 -0500

    preform 3.34.2

--- Casks/p/preform.rb
+++ Casks/p/preform.rb
@@ -1,6 +1,6 @@
 cask "preform" do
-  version "3.34.0,308_54766"
-  sha256 "f85f2797412c981954ce7b264a6bb0ae1ea1bef82b8ca835d5e05f8136def67e"
+  version "3.34.2,312_56125"
+  sha256 "2f641810ec8a93e043a7c321a7312900491522d252b2cd4d1bcc427e742d8b27"
 
   url "https://downloads.formlabs.com/PreForm/Release/#{version.csv.first}/PreForm_mac_#{version.csv.first}_release_releaser_#{version.csv.second}.dmg"
   name "PreForm"
