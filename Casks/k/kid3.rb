8a71f0517258c47eb66eb13cfd059a73d0d0e606
Rui Chen <rui@chenrui.dev>
2024-02-24 10:17:46 -0500

    kid3 3.9.5

--- Casks/k/kid3.rb
+++ Casks/k/kid3.rb
@@ -1,9 +1,9 @@
 cask "kid3" do
   # NOTE: "3" is not a version number, but an intrinsic part of the product name (ID3 tags)
-  version "3.9.4"
+  version "3.9.5"
 
   on_high_sierra :or_older do
-    sha256 "b8668c493982bcbeade9b17244d440158b24f17149ac7f5988e086f65063207d"
+    sha256 "1154c252cbc1b70dccebf6e1f48e00e16eb87e7cc8302d700db2236f1de5ed8e"
 
     url "https://downloads.sourceforge.net/kid3/kid3-#{version}-Darwin-Qt5.dmg",
         verified: "downloads.sourceforge.net/kid3/"
@@ -11,7 +11,7 @@ cask "kid3" do
     depends_on macos: "<= :high_sierra"
   end
   on_mojave :or_newer do
-    sha256 "b8668c493982bcbeade9b17244d440158b24f17149ac7f5988e086f65063207d"
+    sha256 "1154c252cbc1b70dccebf6e1f48e00e16eb87e7cc8302d700db2236f1de5ed8e"
 
     url "https://downloads.sourceforge.net/kid3/kid3-#{version}-Darwin-amd64.dmg",
         verified: "downloads.sourceforge.net/kid3/"
