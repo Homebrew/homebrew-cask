3bd258827db67c848f89f7bc297c60d80832dbde
Muneeb Ahmed <32603485+muneebmahmed@users.noreply.github.com>
2024-02-23 23:02:29 -0800

    webex: Update zap

--- Casks/w/webex.rb
+++ Casks/w/webex.rb
@@ -21,8 +21,31 @@ cask "webex" do
   uninstall signal: ["TERM", "Cisco-Systems.Spark"]
 
   zap trash: [
+    "~/Library/Application Scripts/com.cisco.webex.Cisco-WebEx-Start.CWSSafariExtension",
+    "~/Library/Application Scripts/group.com.cisco.webex.meetings",
+    "~/Library/Application Support/Cisco Spark",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.cisco.webex.*",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/webex-pt.webexapplauncher.sfl*",
+    "~/Library/Application Support/WebEx Folder",
+    "~/Library/Application Support/Webex Meetings",
     "~/Library/Caches/Cisco-Systems.Spark",
+    "~/Library/Caches/com.cisco.webex.*",
+    "~/Library/Caches/com.webex.meetingmanager",
+    "~/Library/Containers/com.cisco.webex.*",
+    "~/Library/Group Containers/group.com.cisco.webex.meetings",
+    "~/Library/HTTPStorages/com.cisco.webex.*",
+    "~/Library/HTTPStorages/com.webex.*",
+    "~/Library/HTTPStorages/WebEx-PT.webexAppLauncher",
     "~/Library/Logs/SparkMacDesktop",
+    "~/Library/Logs/Webex Meetings",
+    "~/Library/Logs/webexmta",
     "~/Library/Preferences/Cisco-Systems.Spark.plist",
+    "~/Library/Preferences/com.cisco.meetings.shortcut.plist",
+    "~/Library/Preferences/com.cisco.webex.*",
+    "~/Library/Preferences/com.webex.*",
+    "~/Library/Saved Application State/Cisco-Systems.Spark.savedState",
+    "~/Library/Saved Application State/com.webex.meetingmanager.savedState",
+    "~/Library/WebKit/Cisco-Systems.Spark",
+    "~/Library/WebKit/com.webex.meetingmanager",
   ]
 end
