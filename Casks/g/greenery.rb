05edff64975dee91a823ec3c0e0966ddbf1ef5f7
Timothy Reynolds <tmrdev@gmail.com>
2024-02-25 17:03:36 -0500

    greenery: update checksum

--- Casks/g/greenery.rb
+++ Casks/g/greenery.rb
@@ -1,6 +1,6 @@
 cask "greenery" do
   version "0.9.7"
-  sha256 "c2c0d298585d443eb6dde267d744d7f92283ff586709ef7582f0cfa4812722a0"
+  sha256 "5f062694ea82a4e97ed248b896cb7c565437a8c36221aeb85df28f97d2e78a12"
 
   url "https://github.com/GreenfireInc/Releases.Greenery/releases/download/v#{version}/Greenery.#{version}.zip",
       verified: "github.com/GreenfireInc/Releases.Greenery/"
