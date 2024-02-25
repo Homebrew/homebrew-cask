3c9c8326591519b8bb2c1b9d599cfdb832c39da4
Bevan Kay <email@bevankay.me>
2024-02-25 19:46:52 +1100

    motion: style updates

--- Casks/m/motion.rb
+++ Casks/m/motion.rb
@@ -1,23 +1,21 @@
 cask "motion" do
   arch arm: "aarch64", intel: "amd64"
 
-  version "0.61.0"
+  version "0.65.0"
+  sha256 arm:   "68615c3edbf4f46a11480f82cc74986947761497031696f4330a57e11e4a87ae",
+         intel: "ddda53c8751849c5116ba9660e61f934e057042007adb4e3be188978a6a40cb8"
 
   on_arm do
-    sha256 "38b7153b6c41148f6fa13c70a4ea0d21ebde62006245751c9bffa89ce33e8512"
-
     depends_on macos: ">= :big_sur"
   end
   on_intel do
-    sha256 "d2a4968b4a2e561243adbab2b1e083663284486e3dfe18210df7786cc687b983"
-
     depends_on macos: ">= :catalina"
   end
 
   url "https://github.com/usemotion/desktopapp/releases/download/#{version}/motion-#{version}-mac-#{arch}.zip",
       verified: "github.com/usemotion/desktopapp/"
   name "Motion"
-  desc "To-do list and project management app which autoschedules tasks"
+  desc "To-do list and project management app"
   homepage "https://www.usemotion.com/"
 
   livecheck do
