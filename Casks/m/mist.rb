1eb3863974a834fe6bfe7188284748297c875e7f
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-02-25 11:54:36 -0500

    mist 0.10

--- Casks/m/mist.rb
+++ Casks/m/mist.rb
@@ -1,6 +1,6 @@
 cask "mist" do
-  version "0.9.1"
-  sha256 "f88c80cbf4f3feb54dbf5fb0d783d6d18e8e039b59b2f87881103fa84677eb8f"
+  version "0.10"
+  sha256 "9d7f2b1df18fc4f4ce118dabed212ce30f1ff990824535694503ae44c9102319"
 
   url "https://github.com/ninxsoft/Mist/releases/download/v#{version}/Mist.#{version}.pkg"
   name "Mist"
