9f53fa982f13d5d7cc4ddafd1340cbaeaa334178
Rui Chen <rui@chenrui.dev>
2024-02-26 09:18:53 -0500

    onlyoffice 8.0.1

--- Casks/o/onlyoffice.rb
+++ Casks/o/onlyoffice.rb
@@ -1,9 +1,9 @@
 cask "onlyoffice" do
   arch arm: "arm", intel: "x86_64"
 
-  version "8.0.0"
-  sha256 arm:   "1421036044168c1891cb8411f5d13fc0663a2cce95d95ae3525055ccc284da2e",
-         intel: "759b4a7ead43a878ad78ff33afbcbd6b213d815641f3d6f5d9525273ab2bc72f"
+  version "8.0.1"
+  sha256 arm:   "a8b94cbbd8c5140fb4ad54aefe128985bdad1eb791917acaa32fbb3c9036e168",
+         intel: "b8cbb67ac65d63c017a8551af41c1dc77f892b0b9f2356a0f461fb5adee04c54"
 
   url "https://github.com/ONLYOFFICE/DesktopEditors/releases/download/v#{version}/ONLYOFFICE-#{arch}.dmg",
       verified: "github.com/ONLYOFFICE/DesktopEditors/"
