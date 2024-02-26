bb792fcb311b0559df531a1c9c36323d1b7e5feb
Bevan Kay <email@bevankay.me>
2024-02-26 15:17:45 +1100

    opencat 2.4.0

--- Casks/o/opencat.rb
+++ Casks/o/opencat.rb
@@ -1,6 +1,6 @@
 cask "opencat" do
-  version "2.1.2,424"
-  sha256 "ed46d98c7921c29c067761b3d33fd836ed8781564c5af5f0ab0e8670241127de"
+  version "2.4.0,431"
+  sha256 "4464b17705127d5ba39c56c189848603b23a22cb4281f9585b67c1076d18ebb2"
 
   url "https://opencat.app/releases/OpenCat-#{version.csv.first}.#{version.csv.second}.dmg"
   name "OpenCat"
@@ -9,7 +9,10 @@ cask "opencat" do
 
   livecheck do
     url "https://opencat.app/releases/versions.xml"
-    strategy :sparkle
+    strategy :sparkle do |item|
+      short_version = (item.short_version.split(".").length < 3) ? "#{item.short_version}.0" : item.short_version.to_s
+      "#{short_version},#{item.version}"
+    end
   end
 
   depends_on macos: ">= :ventura"
