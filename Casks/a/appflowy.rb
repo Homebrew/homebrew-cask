28a6b04ee29f614224c2c9fe2103d5250acdca22
Rui Chen <rui@chenrui.dev>
2024-02-26 09:15:43 -0500

    appflowy 0.5.0

--- Casks/a/appflowy.rb
+++ Casks/a/appflowy.rb
@@ -1,6 +1,6 @@
 cask "appflowy" do
-  version "0.4.9"
-  sha256 "88d1d93179894746e7a9cc3d30d5e7e499558466fc042c3438b0efae71c554d4"
+  version "0.5.0"
+  sha256 "af75325e686432d3915de7d61398b51097b404b09939644c75d6176a807abb7d"
 
   url "https://github.com/AppFlowy-IO/AppFlowy/releases/download/#{version}/Appflowy-#{version}-macos-universal.zip",
       verified: "github.com/AppFlowy-IO/AppFlowy/"
