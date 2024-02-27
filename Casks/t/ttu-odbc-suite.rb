d68a1adb469a12fa520f77209202d7bfcfa591c3
Bevan Kay <email@bevankay.me>
2024-02-27 13:08:01 +1100

    ttu-odbc-suite: fix style

--- Casks/t/ttu-odbc-suite.rb
+++ Casks/t/ttu-odbc-suite.rb
@@ -22,7 +22,7 @@ cask "ttu-odbc-suite" do
   uninstall pkgutil: [
     "com.Teradata.teradataToolsAndUtilities2000.pkg.odbc",
     "com.Teradata.teradataToolsAndUtilities2000.pkg.ttulistproducts",
-    "com.Teradata.teradataToolsAndUtilities2000.pkg.ttuuninstaller"
+    "com.Teradata.teradataToolsAndUtilities2000.pkg.ttuuninstaller",
   ]
 
   zap trash: "~/Library/Saved Application State/com.teradata.TTUListProducts.savedState"
