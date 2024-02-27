e5b50ae40af8d9de681a8d389b1a1419d61f25d1
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-02-27 10:15:44 -0500

    xca 2.6.0

--- Casks/x/xca.rb
+++ Casks/x/xca.rb
@@ -1,6 +1,6 @@
 cask "xca" do
-  version "2.5.0"
-  sha256 "0042758b99d09aa254d721e1e94fd97adfc186727891093e9eeec4d18e9d734f"
+  version "2.6.0"
+  sha256 "34caf717016921c19707719db35c967c2b18fe59efca76473a9008f7d462ba9b"
 
   url "https://github.com/chris2511/xca/releases/download/RELEASE.#{version}/xca-#{version}-Darwin.dmg",
       verified: "github.com/chris2511/xca/"
