2bde5dd56fffefaa52c374a638de5418736b4cb9
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-02-27 10:06:59 -0500

    sunsama 2.3.1

--- Casks/s/sunsama.rb
+++ Casks/s/sunsama.rb
@@ -1,9 +1,9 @@
 cask "sunsama" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.3.0,240124tpxpvhuyf"
-  sha256 arm:   "5953aee70d42d77b9ef8d64b3c456986cab4762dbd25ec2ce7456fe881c480e0",
-         intel: "635fb982f4053d71bff9c8fc214ddf774fc9076d8d4a3d1f77aaccb0e3637c6e"
+  version "2.3.1,240227k9s7v6ynn"
+  sha256 arm:   "1712abe95e6d7feeebd33f56b8c9f3d9ad84bab7585d57f1a3eb3b913fd35050",
+         intel: "930678b166cc27baff85abf7250c1665bc74fcb1b7c8d59c56ca5b0782c78009"
 
   url "https://download.todesktop.com/2003096gmmnl0g1/Sunsama%20#{version.csv.first}%20-%20Build%20#{version.csv.second}-#{arch}-mac.zip",
       verified: "download.todesktop.com/2003096gmmnl0g1/"
