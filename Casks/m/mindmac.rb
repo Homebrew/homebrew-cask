d12b72e9777ad3ba7405081035ff76e6faa1f968
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-02-27 10:24:18 -0500

    mindmac: add `livecheck`

--- Casks/m/mindmac.rb
+++ Casks/m/mindmac.rb
@@ -8,6 +8,11 @@ cask "mindmac" do
   desc "ChatGPT client"
   homepage "https://mindmac.app/"
 
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
   depends_on macos: ">= :ventura"
 
   app "MindMac.app"
