af11da0be835635d9c9ffe644c8bb86ed68b2385
miccal <miccal.matthews@gmail.com>
2024-02-27 18:47:27 +0800

    phoenix-slides 1.5.7

--- Casks/p/phoenix-slides.rb
+++ Casks/p/phoenix-slides.rb
@@ -1,6 +1,6 @@
 cask "phoenix-slides" do
-  version "1.5.6"
-  sha256 "665680822f8cb6ca45e56336aadde29c77693868e568740c10952ab069ed16aa"
+  version "1.5.7"
+  sha256 "d761d9c34b44d1a5d7d9731ef27b83a2085d8293211d9b28ac7774e8bf6ef833"
 
   url "https://github.com/gobbledegook/creevey/releases/download/v#{version}/phoenix-slides-#{version.no_dots}.dmg",
       verified: "github.com/gobbledegook/creevey/"
