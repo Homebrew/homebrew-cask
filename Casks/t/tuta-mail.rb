f85de59617cd466d8102eae888991e563a9b7fb4
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-02-28 10:41:53 -0500

    tutanota: rename and update to 218.240227.0

---
+++ Casks/t/tuta-mail.rb
@@ -0,0 +1,27 @@
+cask "tuta-mail" do
+  version "218.240227.0"
+  sha256 :no_check
+
+  url "https://app.tuta.com/desktop/tutanota-desktop-mac.dmg"
+  name "Tuta Mail"
+  desc "Email client"
+  homepage "https://tuta.com/"
+
+  livecheck do
+    url "https://app.tuta.com/desktop/latest-mac.yml"
+    strategy :electron_builder
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
+  app "Tuta Mail.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/de.tutao.tutanota.sfl*",
+    "~/Library/Application Support/tutanota-desktop",
+    "~/Library/Caches/de.tutao.tutanota",
+    "~/Library/Caches/de.tutao.tutanota.ShipIt",
+    "~/Library/Preferences/de.tutao.tutanota.plist",
+  ]
+end
