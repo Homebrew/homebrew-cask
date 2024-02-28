add46e652577308d6b502b87b3e561bc3f2f0639
Rui Chen <rui@chenrui.dev>
2024-02-27 23:06:27 -0500

    chrome-remote-desktop-host 122.0.6261.29

--- Casks/c/chrome-remote-desktop-host.rb
+++ Casks/c/chrome-remote-desktop-host.rb
@@ -1,5 +1,5 @@
 cask "chrome-remote-desktop-host" do
-  version "121.0.6167.9"
+  version "122.0.6261.29"
   sha256 :no_check
 
   url "https://dl.google.com/chrome-remote-desktop/chromeremotedesktop.dmg"
