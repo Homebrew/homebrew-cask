c8d8e6c06800d64f4f9af9f6b3c4777e340ab282
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-02-28 10:30:08 -0500

    notesollama 0.2.5

--- Casks/n/notesollama.rb
+++ Casks/n/notesollama.rb
@@ -1,8 +1,8 @@
 cask "notesollama" do
-  version "0.2.4"
-  sha256 "5eea5a5f373339d408498bc8fc489462f65b3a2acfa7789cc408d9f472543c72"
+  version "0.2.5"
+  sha256 "0570b3d95caf00a991d7ce225cbc9fa0f3b396d85bc1e61a35cbbecc18da5064"
 
-  url "https://smallest.app/notesollama/NotesOllama-#{version}.zip"
+  url "https://smallest.app/notesollama/dist/NotesOllama-#{version}.zip"
   name "NotesOllama"
   desc "LLM support for Apple Notes through Ollama"
   homepage "https://smallest.app/notesollama/"
