f47b3a3bfdfd17aa222d017df32863e31823eb06
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-02-28 10:54:32 -0500

    lazarus 3.2

--- Casks/l/lazarus.rb
+++ Casks/l/lazarus.rb
@@ -1,6 +1,6 @@
 cask "lazarus" do
-  version "3.0"
-  sha256 "71d77f4c6491265181078adf0e410dcb6176e8237e958c3aea9217bb5499f661"
+  version "3.2"
+  sha256 "c84a2ee58d791613a03cb3de119fb92c2dd14a2686b44cf342580376b950a5b1"
 
   url "https://downloads.sourceforge.net/lazarus/Lazarus%20macOS%20x86-64/Lazarus%20#{version}/Lazarus-#{version}-macosx-x86_64.pkg",
       verified: "sourceforge.net/lazarus/"
