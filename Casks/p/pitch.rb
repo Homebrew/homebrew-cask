368aec452f205777ccef959faa880b10145bd9ef
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-28 15:36:56 +0000

    pitch 2.12.0,stable.2,6441856

--- Casks/p/pitch.rb
+++ Casks/p/pitch.rb
@@ -1,6 +1,6 @@
 cask "pitch" do
-  version "2.12.0,stable.1,6438937"
-  sha256 "274c5030c329624051080c73852b42f7a7c6fa00a8b5393cb080344bf14f1f03"
+  version "2.12.0,stable.2,6441856"
+  sha256 "b9751ee4fe8f89bf5af4259935e4ebd4f1b013bbc8cb4bd71735acc78aa4522b"
 
   url "https://desktop-app-builds.pitch.com/Pitch-#{version.csv.first}-#{version.csv.second}-ci#{version.csv.third}.dmg"
   name "Pitch"
