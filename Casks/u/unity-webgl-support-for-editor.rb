19a390cd6910b7627e2408db32c75351999f99c9
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-03-02 10:35:09 -0500

    unity-webgl-support-for-editor 2023.2.12f1

--- Casks/u/unity-webgl-support-for-editor.rb
+++ Casks/u/unity-webgl-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask "unity-webgl-support-for-editor" do
-  version "2023.2.11f1,6a1e21c61430"
-  sha256 "9b6c8a7f3ac542e6b05be945a8e847f74938fcebd53e3b221bb984e127e5a4c4"
+  version "2023.2.12f1,19eeb3b320af"
+  sha256 "7c04a8cd46e1d6cdf750d5cef5ba19a008bf349809dc2316351788966489d9aa"
 
   url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorTargetInstaller/UnitySetup-WebGL-Support-for-Editor-#{version.csv.first}.pkg",
       verified: "download.unity3d.com/download_unity/"
