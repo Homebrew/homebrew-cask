8617e7ea8ac64574bc6703a839fe38d4197510c4
Rui Chen <rui@chenrui.dev>
2024-03-03 20:51:24 -0500

    puzzles: deprecate
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/p/puzzles.rb
+++ Casks/p/puzzles.rb
@@ -7,16 +7,8 @@ cask "puzzles" do
   desc "Collection of small computer programmes which implement one-player puzzle games"
   homepage "https://www.chiark.greenend.org.uk/~sgtatham/puzzles/"
 
-  livecheck do
-    url "https://www.chiark.greenend.org.uk/~sgtatham/puzzles/devel/"
-    regex(/collection,\s+version\s+(\d+)\.\w+/i)
-    strategy :page_match do |page, regex|
-      # Throttle updates to one every 2 days.
-      next version if Date.parse(version) + 2 > Date.today
-
-      page[regex, 1]
-    end
-  end
+  # see discussions in https://github.com/Homebrew/homebrew-core/pull/164944#issuecomment-1975508329
+  deprecate date: "2024-03-03", because: "in favor of core tap formula"
 
   app "Puzzles.app"
 
@@ -24,4 +16,12 @@ cask "puzzles" do
     "~/Library/Caches/com.apple.helpd/Generated/Puzzles Help*",
     "~/Library/Saved Application State/uk.org.greenend.chiark.sgtatham.puzzles.savedState",
   ]
+
+  caveats do
+    <<~EOS
+      Please use core tap formula instead of this cask:
+
+        brew install puzzles
+    EOS
+  end
 end
