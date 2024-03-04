7418539bb4e20157d9451de1b9a636195a99858d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-04 15:41:45 +0000

    tuist 4.5.0

--- Casks/t/tuist.rb
+++ Casks/t/tuist.rb
@@ -1,6 +1,6 @@
 cask "tuist" do
-  version "4.4.0"
-  sha256 "7f8365e86b9a8e47a6fbd10de6cc6f83d1ea1285cff80ad76505e21532ad3430"
+  version "4.5.0"
+  sha256 "991630e8c406c9630fe7fcfc571c5402ccee03d389c3a53ab9544db049d9a2b1"
 
   url "https://github.com/tuist/tuist/releases/download/#{version}/tuist.zip",
       verified: "github.com/tuist/tuist/"
