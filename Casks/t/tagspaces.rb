f6339c7838498864dffd13ca9591a80753a4a0ce
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-03-04 14:46:43 -0500

    tagspaces 5.7.8

--- Casks/t/tagspaces.rb
+++ Casks/t/tagspaces.rb
@@ -1,9 +1,9 @@
 cask "tagspaces" do
   arch arm: "arm64", intel: "x64"
 
-  version "5.7.4"
-  sha256 arm:   "3b027710b8e54009752ae278d6189767a382042577d30e516acae4e1a8cc5da9",
-         intel: "1e58ffeb03971bb36abaf3263d3166a11676232e6fae11273175d4e2787cc61f"
+  version "5.7.8"
+  sha256 arm:   "45460449a2ea44984171143a15756f6ad5105fd393930f02c7420469bed20dae",
+         intel: "c5f21e865506f9085abdb2116d19dc7f412a2073bc007f794074a594c0a6c1dd"
 
   url "https://github.com/tagspaces/tagspaces/releases/download/v#{version}/tagspaces-mac-#{arch}-#{version}.dmg",
       verified: "github.com/tagspaces/tagspaces/"
