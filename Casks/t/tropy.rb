f1722c7ea3c883cfc1c71205379dc54ab0b8cbf6
Rui Chen <rui@chenrui.dev>
2024-03-05 09:36:20 -0500

    tropy 1.16.2

--- Casks/t/tropy.rb
+++ Casks/t/tropy.rb
@@ -1,9 +1,9 @@
 cask "tropy" do
   arch arm: "-arm64"
 
-  version "1.16.1"
-  sha256 arm:   "5f8718d78e1baf74ae379a2c90bc93a95baf2e4490f666ae23052f2528c095f5",
-         intel: "5799380b2ea9da6182b2c616ca68b3e32dafa711bf451340da60af72beb05171"
+  version "1.16.2"
+  sha256 arm:   "3dc286d185f1fead018d628421bdf29a470a587e6ec64245495a35dafd79a3be",
+         intel: "595d4da00f680767f5b9b48a16510216833cbbeaf50b8b8b2a03b89985765e8e"
 
   url "https://github.com/tropy/tropy/releases/download/v#{version}/tropy-#{version}#{arch}.dmg",
       verified: "github.com/tropy/tropy/"
