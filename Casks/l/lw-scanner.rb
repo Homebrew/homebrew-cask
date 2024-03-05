81df247a2fa7e6ee32ba055839b1be8207628cca
Rui Chen <rui@chenrui.dev>
2024-03-05 17:51:12 -0500

    lw-scanner 0.24.0

--- Casks/l/lw-scanner.rb
+++ Casks/l/lw-scanner.rb
@@ -1,6 +1,6 @@
 cask "lw-scanner" do
-  version "0.23.2"
-  sha256 "11f1617e9eb033e9ccdfcdfefcb4fc2fdc48944fd7d013da2915bb72e1ef1e83"
+  version "0.24.0"
+  sha256 "a2bbd9b615143ca1f63824fa335d49da9dd2744a58776bff1c9a85e6bcd3c34e"
 
   url "https://github.com/lacework/lacework-vulnerability-scanner/releases/download/v#{version}/lw-scanner-darwin-amd64",
       verified: "github.com/lacework/lacework-vulnerability-scanner/"
