4ec4f5de9c2e8226dc1234a32f742c383fc1cd7c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-06 12:45:56 +0000

    roam 99.0.2-beta001

--- Casks/r/roam.rb
+++ Casks/r/roam.rb
@@ -1,9 +1,9 @@
 cask "roam" do
   arch arm: "arm64", intel: "x64"
 
-  version "99.0.1-beta001"
-  sha256 arm:   "43b68f623b85849816260cc0e75c58ac0c1efe3da1d1eede7099256c759b069b",
-         intel: "a7ee3c8ed75a957d759479be3892ae8a3d99d4e66c3c4efc01d11654e9d9f9c2"
+  version "99.0.2-beta001"
+  sha256 arm:   "884a05e4032b4cbf71a70f596afe723c9d8b5e09b0959db0140711c93786a7a3",
+         intel: "3bb84649bb51487df8c6611bbda45031a5ba9a19989d6507d17ffe5050470d64"
 
   url "https://download.ro.am/Roam/8a86d88cfc9da3551063102e9a4e2a83/latest/darwin/#{arch}/Roam.dmg"
   name "Roam"
