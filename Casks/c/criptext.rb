a1d3f42576bb93ce7df15e25df1723422ebc583a
Rui Chen <rui@chenrui.dev>
2024-03-06 10:21:56 -0500

    criptext: switch to use http due to cert issue
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/c/criptext.rb
+++ Casks/c/criptext.rb
@@ -2,10 +2,10 @@ cask "criptext" do
   version "0.31.0,2.0.82"
   sha256 :no_check
 
-  url "https://cdn.criptext.com/Criptext-Email-Desktop/mac/Criptext-latest.dmg"
+  url "http://cdn.criptext.com/Criptext-Email-Desktop/mac/Criptext-latest.dmg"
   name "Criptext"
   desc "Email service that's built around privacy"
-  homepage "https://criptext.com/"
+  homepage "http://criptext.com/"
 
   livecheck do
     url :url
