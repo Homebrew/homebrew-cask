8a1239de2562ba31fdc003b9f37f1186873ce07b
adithyasunil26 <adithyasunil26@gmail.com>
2024-03-07 02:51:03 +0530

    marvin 1.64.4

--- Casks/m/marvin.rb
+++ Casks/m/marvin.rb
@@ -2,9 +2,9 @@ cask "marvin" do
   arch arm: "-arm64"
   hostname_arch = on_arch_conditional arm: "amarm", intel: "amazingmarvin"
 
-  version "1.64.3"
-  sha256 arm:   "11090975330e3f45172f55e36b0fb8b39456a5b76b46eb856fa0c66d82f7832a",
-         intel: "350604cb95a5d9de1405002f1459fc4f62799c02f863a2c551a1a750606683b1"
+  version "1.64.4"
+  sha256 arm:   "213f87692d55cbcd5997308203f35b8718ac2c18648f0109354a2132b36a675b",
+         intel: "68edd8325fc946305834d304e78dd39ab40336f187a78de90b80e6376047d34c"
 
   url "https://#{hostname_arch}.s3.amazonaws.com/Marvin-#{version}#{arch}-mac.zip",
       verified: "#{hostname_arch}.s3.amazonaws.com/"
