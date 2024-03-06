8bdb420cc3a0f6d6a92958e20719926305e5f2db
WangEdward <73746306+WangEdward@users.noreply.github.com>
2024-03-06 20:00:48 +0800

    yesplaymusic 0.4.8-2

--- Casks/y/yesplaymusic.rb
+++ Casks/y/yesplaymusic.rb
@@ -1,8 +1,11 @@
 cask "yesplaymusic" do
-  version "0.4.7"
-  sha256 "d41dee5dd151673b62115dd391e89d50567072f044b3a7bfc07bd584d835263a"
+  arch arm: "arm64", intel: "universal"
 
-  url "https://github.com/qier222/YesPlayMusic/releases/download/v#{version}/YesPlayMusic-mac-#{version}-universal.dmg"
+  version "0.4.8-2"
+  sha256 arm:   "15a7ae98d9a43d0623f407b65f0fde294b9ecd1e1b11d9e2f258be35153d8b59",
+         intel: "2f182bbbc6f654d59e0bd77a8b5801ff0502b1c2e43f38bd3b96fa5dc29818c7"
+
+  url "https://github.com/qier222/YesPlayMusic/releases/download/v#{version}/YesPlayMusic-mac-#{version.hyphens_to_dots.major_minor_patch}-#{arch}.dmg"
   name "YesPlayMusic"
   desc "Third-party NetEase cloud player"
   homepage "https://github.com/qier222/YesPlayMusic"
@@ -10,6 +13,7 @@ cask "yesplaymusic" do
   livecheck do
     url :url
     strategy :github_latest
+    regex(/v?(\d+(?:[.-]\d+)+)/i)
   end
 
   app "YesPlayMusic.app"
