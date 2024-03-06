3e5f40fd7d94231b02ac1021c1d9c8cd0c8fc448
adithyasunil26 <adithyasunil26@gmail.com>
2024-03-07 02:07:01 +0530

    framer 2024.9.1

--- Casks/f/framer.rb
+++ Casks/f/framer.rb
@@ -1,7 +1,7 @@
 cask "framer" do
   arch arm: "arm64", intel: "x64"
 
-  version "2023.47.2"
+  version "2024.9.1"
   sha256 :no_check
 
   url "https://updates.framer.com/electron/darwin/#{arch}/Framer.zip"
