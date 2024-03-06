11659e7b006a11f501b69c3d1402643c06ae942f
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-03-06 18:56:36 -0500

    phpwebstudy 2.2.5

--- Casks/p/phpwebstudy.rb
+++ Casks/p/phpwebstudy.rb
@@ -1,9 +1,9 @@
 cask "phpwebstudy" do
   arch arm: "-arm64"
 
-  version "2.2.4"
-  sha256 arm:   "f8de3f841de9d154df42af05bb585df43f2a3461afbeccf9cc107ffd64c94b21",
-         intel: "2941aa3934efee6f4608e9aa2a5dbe8f8aa4eff2b4fddf30019722c7e9bb5ad1"
+  version "2.2.5"
+  sha256 arm:   "fc20f7652e3c4b672097034612014d25cc74d53be568077eb978a3fff8abf763",
+         intel: "a560789620207b190c968bd92b0b5b7d3794aa1a992033817e9fbfd8993dfaea"
 
   url "https://github.com/xpf0000/PhpWebStudy/releases/download/v#{version}/PhpWebStudy-#{version}#{arch}-mac.zip",
       verified: "github.com/xpf0000/PhpWebStudy/"
