b10ab2f31b9d0b9e1a64582edd905220f9d2ea18
Rui Chen <rui@chenrui.dev>
2024-03-06 09:18:42 -0500

    sparrow 1.8.3

--- Casks/s/sparrow.rb
+++ Casks/s/sparrow.rb
@@ -1,9 +1,9 @@
 cask "sparrow" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.8.2"
-  sha256 arm:   "541b82e83ec928e7b54490211fa5e9d3b315394d895aebeab9a9696ce24c378d",
-         intel: "508a5ba212c2393a9d4cd122c2a8aa8b16e593c640bc3e4ac111ee574b2fd82d"
+  version "1.8.3"
+  sha256 arm:   "83af936cc1e25f0c3e2b3cc7c41cb566604a1c7019cf91d944c4419414502a40",
+         intel: "5f1eaca64fcacf068af46fc2098ae34e8fcb26a4095965c8014df915c62dafe0"
 
   url "https://github.com/sparrowwallet/sparrow/releases/download/#{version}/Sparrow-#{version}-#{arch}.dmg",
       verified: "github.com/sparrowwallet/sparrow/"
