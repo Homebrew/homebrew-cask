b73aa9cab28c7d9c77ff11d1dc6c171254263290
Rui Chen <rui@chenrui.dev>
2024-03-06 13:36:30 -0500

    ddpm 1.3.0.0026
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/d/ddpm.rb
+++ Casks/d/ddpm.rb
@@ -1,19 +1,19 @@
 cask "ddpm" do
-  version "1.2.0.0052"
-  sha256 "53faf376738a35a8f1af6f4ba48ea18986d885f032ca89911172f6e6e1b4c58b"
+  version "1.3.0.0026"
+  sha256 "fad4c289349f0d6e6b55af535f7494dcba1762fffddaa089d75065f9dd4ccdd3"
 
-  url "https://dl.dell.com/FOLDER10656664M/1/DDPMv#{version}.zip"
+  url "https://dl.dell.com/FOLDER11285771M/1/DDPMv#{version}.zip"
   name "DDPM"
   name "Dell Display and Peripheral Manager"
   desc "Monitors and peripherals manager"
   homepage "https://dell.com/"
 
   livecheck do
-    url "https://www.dell.com/support/home/en-us/drivers/driversdetails?driverid=3v8tw"
+    url "https://www.dell.com/support/home/en-us/drivers/driversdetails?driverid=ND3C5"
     regex(/ddpmv(\d+(?:\.\d+)+)\.zip/i)
   end
 
-  pkg "DDPMv#{version}/DDPM_Installer.pkg"
+  pkg "DDPM_Installer.pkg"
   binary "/Applications/DDPM/DDPM.app/Contents/MacOS/DDPM"
 
   uninstall pkgutil: "Qisda.pkg.DDPM",
