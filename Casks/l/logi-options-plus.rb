5f489025797300dc70f941c767735b6a6fec4f4d
Rui Chen <rui@chenrui.dev>
2024-03-06 09:44:13 -0500

    logi-options-plus 1.66.531337

--- Casks/l/logi-options-plus.rb
+++ Casks/l/logi-options-plus.rb
@@ -1,5 +1,5 @@
 cask "logi-options-plus" do
-  version "1.64.527952"
+  version "1.66.531337"
   sha256 :no_check
 
   url "https://download01.logi.com/web/ftp/pub/techsupport/optionsplus/logioptionsplus_installer.zip",
