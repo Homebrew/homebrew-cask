1f92c7c79119e96825e0d198549077ff6e4aa702
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-06 00:47:20 +0000

    plex-htpc 1.57.1.100

--- Casks/p/plex-htpc.rb
+++ Casks/p/plex-htpc.rb
@@ -1,6 +1,6 @@
 cask "plex-htpc" do
-  version "1.56.1.94,f294e1e6"
-  sha256 "2875c23f9fcddaef391545c0a49b7ffc867c0ebe8ad4f8d3ade874e4ab5c582c"
+  version "1.57.1.100,26625b61"
+  sha256 "47cf219ec01743f7c7d3780aac69ac2e300f85e19330d4592a0662b3fc3a383a"
 
   url "https://downloads.plex.tv/htpc/#{version.csv.first}-#{version.csv.second}/macos/PlexHTPC-#{version.csv.first}-#{version.csv.second}-universal.zip"
   name "Plex HTPC"
