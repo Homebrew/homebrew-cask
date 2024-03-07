05607c85976e85b0ca79942af95d535dc7314998
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-03-07 11:16:28 -0500

    ibkr-desktop: add uninstall step

--- Casks/i/ibkr.rb
+++ Casks/i/ibkr.rb
@@ -19,7 +19,11 @@ cask "ibkr" do
     args:       ["-q"],
   }
 
-  uninstall quit:   "com.install4j.5557-0173-2810-0000",
+  uninstall quit:   [
+              "com.install4j.5557-0173-2810-0000",
+              "com.install4j.5557-0173-2810-0000.22",
+              "IBKR Desktop.app",
+            ],
             script: {
               executable: "/Applications/IBKR Desktop/IBKR Desktop Uninstaller.app/Contents/MacOS/JavaApplicationStub",
               args:       ["-q"],
