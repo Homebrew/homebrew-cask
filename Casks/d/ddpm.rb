8101a2f27e29b7f6472a442627923fba9d20c970
Bevan Kay <email@bevankay.me>
2024-03-07 15:42:16 +1100

    ddpm: update url & livecheck

--- Casks/d/ddpm.rb
+++ Casks/d/ddpm.rb
@@ -2,15 +2,17 @@ cask "ddpm" do
   version "1.3.0.0026"
   sha256 "fad4c289349f0d6e6b55af535f7494dcba1762fffddaa089d75065f9dd4ccdd3"
 
-  url "https://dl.dell.com/FOLDER11285771M/1/DDPMv#{version}.zip"
+  url "https://www.delldisplaymanagermac.com/DDPM/DDPMv#{version}.zip"
   name "DDPM"
   name "Dell Display and Peripheral Manager"
   desc "Monitors and peripherals manager"
   homepage "https://dell.com/"
 
   livecheck do
-    url "https://www.dell.com/support/home/en-us/drivers/driversdetails?driverid=ND3C5"
-    regex(/ddpmv(\d+(?:\.\d+)+)\.zip/i)
+    url "https://www.delldisplaymanagermac.com/DDPM/ddpm.json"
+    strategy :json do |json|
+      json.dig("versionTable", "*", "originVersion")
+    end
   end
 
   pkg "DDPM_Installer.pkg"
