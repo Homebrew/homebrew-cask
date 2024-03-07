0128255e03e51376ed5f5c882be92169bdb55458
Sam Ford <1584702+samford@users.noreply.github.com>
2024-03-06 20:00:39 -0500

    coolterm: update version, url, livecheck

--- Casks/c/coolterm.rb
+++ Casks/c/coolterm.rb
@@ -1,15 +1,15 @@
 cask "coolterm" do
-  version "2.1.1"
+  version "2.1.1.3.0.1288"
   sha256 "24a2ca96132258a5080412de44e8e7e309dbb4e61a1ada85dbe6e079e7a2c046"
 
-  url "https://freeware.the-meiers.org/previous/CoolTermMac#{version.no_dots}.dmg"
+  url "https://freeware.the-meiers.org/previous/CoolTermMac#{version.major_minor_patch.no_dots}.dmg"
   name "CoolTerm"
   desc "Serial port terminal"
   homepage "https://freeware.the-meiers.org/"
 
   livecheck do
-    url "https://freeware.the-meiers.org/previous"
-    regex(/href=.*?CoolTermMac(\d+(?:\.\d+)*)\.dmg/i)
+    url "https://freeware.the-meiers.org/version/CoolTerm.ver"
+    regex(/^\s*v?(\d+(?:\.\d+)+)\s*$/i)
   end
 
   depends_on macos: ">= :mojave"
