2edf51d8e1d88a7f893b7719084d06197b5381b0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-08 09:52:34 +0000

    unity 2023.2.13f1

--- Casks/u/unity.rb
+++ Casks/u/unity.rb
@@ -1,9 +1,9 @@
 cask "unity" do
   arch arm: "Arm64"
 
-  version "2023.2.12f1,19eeb3b320af"
-  sha256 arm:   "7881b734c10883f247d51e69a919284765813e921700220bedfbce47ce1e9f03",
-         intel: "1c83d7c7805b7891b4458fcc9c123a4af8e95de2dc0408497479c06ead23e362"
+  version "2023.2.13f1,70197a359f36"
+  sha256 arm:   "fa7a9e4128a76d9241ac1d8ba8214455bccc704d4a49f3bdb7a9c521d115b7e1",
+         intel: "99280d247a0d770dddf29ba2b9dc4201237c0e14f16b17c0ae4e0337f88a0e1e"
 
   url "https://download.unity3d.com/download_unity/#{version.csv.second}/MacEditorInstaller#{arch}/Unity-#{version.csv.first}.pkg",
       verified: "download.unity3d.com/download_unity/"
