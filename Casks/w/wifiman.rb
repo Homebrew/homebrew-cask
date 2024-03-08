1aa2f8ecc9423a9324a3749d9e3e7a0dea5d2bad
Miccal Matthews <miccal.matthews@gmail.com>
2024-03-08 11:11:23 +0800

    Update wifiman.rb

--- Casks/w/wifiman.rb
+++ Casks/w/wifiman.rb
@@ -2,8 +2,8 @@ cask "wifiman" do
   arch arm: "arm64", intel: "x64"
 
   version "0.3.0"
-  sha256 intel: "be1e7bb2123a94ecb624ebc26ad3b83d075193aa0d70b06b05bd0f0fb2f70da9",
-         arm:   "47c1794e20d8a728f790beb26e8bdbd5a30a251fd421419d2589fb40ca3faf7a"
+  sha256 arm:   "47c1794e20d8a728f790beb26e8bdbd5a30a251fd421419d2589fb40ca3faf7a",
+         intel: "be1e7bb2123a94ecb624ebc26ad3b83d075193aa0d70b06b05bd0f0fb2f70da9"
 
   url "https://desktop.wifiman.com/wifiman-desktop-#{version}-mac-#{arch}.pkg"
   name "WiFiman Desktop"
