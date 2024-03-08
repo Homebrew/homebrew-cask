ac49f531ced4231508d9002b75a0952d1d121901
LanikSJ <laniksj@gmail.com>
2024-03-08 03:48:47 -0800

    joplin 2.14.19

--- Casks/j/joplin.rb
+++ Casks/j/joplin.rb
@@ -1,9 +1,9 @@
 cask "joplin" do
   arch arm: "-arm64"
 
-  version "2.14.17"
-  sha256 arm:   "398a6c1cf23eeeb88c54d029d89a4196674574951236a36f0667981c3c3acb36",
-         intel: "2a3370027259197fc3bc70cfc35e6f1a549b278eece984f9f446a5011b759b1e"
+  version "2.14.19"
+  sha256 arm:   "8986835294fccf947e653cf3f84f6f1d8e37f0540d576cd6917834a86147813b",
+         intel: "476eab50c33e1bf5311228260b3762ad74c7aa2caeadd5ac35aa2b73188554e1"
 
   url "https://github.com/laurent22/joplin/releases/download/v#{version}/Joplin-#{version}#{arch}.DMG",
       verified: "github.com/laurent22/joplin/"
