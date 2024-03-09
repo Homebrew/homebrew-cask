9cb07156e0648cfabec01f6f3474092fe1e8e34f
Lutz <lutzroeder@users.noreply.github.com>
2024-03-09 03:23:09 +0000

    netron 7.5.4

--- Casks/n/netron.rb
+++ Casks/n/netron.rb
@@ -1,6 +1,6 @@
 cask "netron" do
-  version "7.5.3"
-  sha256 "a1eaa33ba907c1fab3d837b3fd08126196950558f9d1d45d45e1d2a88ec62d2c"
+  version "7.5.4"
+  sha256 "cdae9140e4894d7bcd687657be5157dd6b0e8f5a83bc131615b860b82c0dcd43"
 
   url "https://github.com/lutzroeder/netron/releases/download/v#{version}/Netron-#{version}-mac.zip"
   name "Netron"
