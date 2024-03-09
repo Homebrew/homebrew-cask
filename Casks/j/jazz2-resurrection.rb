7dd8ce0ab41cc9bca25e725147f2d2c98373695c
Rui Chen <rui@chenrui.dev>
2024-03-09 10:09:01 -0500

    jazz2-resurrection 2.6.0

--- Casks/j/jazz2-resurrection.rb
+++ Casks/j/jazz2-resurrection.rb
@@ -2,8 +2,8 @@ cask "jazz2-resurrection" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
   arch arm: "ARM64", intel: "x64"
 
-  version "2.5.0"
-  sha256 "e724d0650032f9272bfc25c512b342fd2c0ed728a3230e0b5bc59f4e6b9bd6f7"
+  version "2.6.0"
+  sha256 "eb2a805b4594e8745f396b07508e363718ea0cee45af916215a1df4559394268"
 
   url "https://github.com/deathkiller/jazz2/releases/download/#{version}/Jazz2_#{version}_MacOS.zip",
       verified: "github.com/deathkiller/jazz2/"
