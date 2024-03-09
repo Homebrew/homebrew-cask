d0633e46537431258a92a9991f8a85e3683fbe7b
Rui Chen <rui@chenrui.dev>
2024-03-09 10:13:04 -0500

    vpn-tracker-365 24.0.0

--- Casks/v/vpn-tracker-365.rb
+++ Casks/v/vpn-tracker-365.rb
@@ -1,7 +1,7 @@
 cask "vpn-tracker-365" do
   # NOTE: "365" is not a version number, but an intrinsic part of the product name
-  version "23.6.3,230634"
-  sha256 "24af65dfcf4d23cfb3dbc57cb7e05d3dd1e053cf4289437efa783d2d6ed39af0"
+  version "24.0.0,240005"
+  sha256 "64350cf731c8c0494004085872e199fb4043eece1cbe2e7306b5ceb0ddf8144e"
 
   url "https://download.equinux.com/files/other/VPN%20Tracker%20365%20-%20#{version.csv.first}%20#{version.csv.second}.zip",
       verified: "download.equinux.com/files/other/"
