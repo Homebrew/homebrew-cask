03f52b31675e4d939592c2aaf10eaabc941f48a0
James Raikes <james.raikes@pm.me>
2024-03-09 00:22:32 +0000

    sonarr: update version, url and livecheck

--- Casks/s/sonarr.rb
+++ Casks/s/sonarr.rb
@@ -1,18 +1,19 @@
 cask "sonarr" do
   arch arm: "arm64", intel: "x64"
 
-  version "4.0.0.748"
-  sha256 arm:   "4f6cfa4a0451079df914d20ed695fbf7f5ba2a5014788f8833ee16ce57d1b54a",
-         intel: "c5e1c185edcd0bd3cc410ceb390c6f3ea68601124f2cdc45b2fc6b3e1f0b266f"
+  version "4.0.2.1183"
+  sha256 arm:   "9808f3fac1c15739161d755d7cc5c4bc9a41f2cd6d47519db30aa5a9f3100d82",
+         intel: "0e83249a17616f809fd651a1f1ae1106a568625a4cc52f3bf82f82ef13d0553a"
 
-  url "https://download.sonarr.tv/v#{version.major}/main/#{version}/Sonarr.main.#{version}.osx-#{arch}-app.zip"
+  url "https://github.com/Sonarr/Sonarr/releases/download/v#{version}/Sonarr.main.#{version}.osx-#{arch}-app.zip",
+      verified: "github.com/Sonarr/Sonarr/"
   name "Sonarr"
   desc "PVR for Usenet and BitTorrent users"
   homepage "https://sonarr.tv/"
 
   livecheck do
-    url "https://download.sonarr.tv/v4/main/"
-    regex(%r{href=["']?v?(\d+(?:\.\d+)+)/?["' >]}i)
+    url :url
+    strategy :github_latest
   end
 
   auto_updates true
