8b8cd099123dd1d2287824e617a8929271e0ac93
adithyasunil26 <adithyasunil26@gmail.com>
2024-03-10 02:09:27 +0530

    deltachat 1.44.1

--- Casks/d/deltachat.rb
+++ Casks/d/deltachat.rb
@@ -1,6 +1,6 @@
 cask "deltachat" do
-  version "1.42.2"
-  sha256 "cc78c69a613384e1cdd30481598479c24b5e82af5d55e5b7e818607d729c8942"
+  version "1.44.1"
+  sha256 "4cedf6f267a3a0565990149402562f4b356f6be8f4d18ede0e00b4409aa22cfd"
 
   url "https://download.delta.chat/desktop/v#{version}/DeltaChat-#{version}.dmg"
   name "DeltaChat"
