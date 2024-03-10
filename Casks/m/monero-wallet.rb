32ddb2cc10df9c186aaf7661e388ccc18cfac5f4
selsta <selsta@sent.at>
2024-03-10 22:01:15 +0100

    monero-wallet 0.18.3.2

--- Casks/m/monero-wallet.rb
+++ Casks/m/monero-wallet.rb
@@ -2,9 +2,9 @@ cask "monero-wallet" do
   arch arm: "armv8", intel: "x64"
   livecheck_folder = on_arch_conditional arm: "arm8", intel: "64"
 
-  version "0.18.3.1"
-  sha256 arm:   "b0c8d07f8d8ade49d08419b196ddb9f691717ef05cae066e220db707e4dfedc4",
-         intel: "8ae53f0908f9bc03452f23d5092bf1eb1d2ad9f1224580486b486cf0a2020401"
+  version "0.18.3.2"
+  sha256 arm:   "3c48b77e0b5258350a40d8cf23c2f6fda56a7ba0193fc368473ce1e0bf59342f",
+         intel: "8f18d3a63f0f52c6ae61de1881e420c6c8c2bf3296084c3d30b529430cdd9896"
 
   url "https://downloads.getmonero.org/gui/monero-gui-mac-#{arch}-v#{version}.dmg"
   name "Monero Wallet"
