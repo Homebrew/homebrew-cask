5442ee6c7a8905796a60d63446b5e236f7fb7577
William Kaiser <wkaisertexas@gmail.com>
2024-03-11 16:17:31 -0400

    Add TimeLapze.app v1.0
    Adds wkaisertexas/screentimelapse to homebrew

---
+++ Casks/t/timelapze.rb
@@ -0,0 +1,22 @@
+cask "timelapze" do
+  version "1.0.0"
+  sha256 :no_check
+
+  url "https://github.com/wkaisertexas/ScreenTimeLapse/releases/download/v1.0.0/TimeLapze.zip"
+  name "TimeLapze"
+  desc "Record screen time lapses with ease in a simple, intuitive interface"
+  homepage "https://github.com/wkaisertexas/ScreenTimeLapse"
+
+  auto_updates true
+  depends_on macos: ">= :ventura"
+
+  app "TimeLapze.app"
+
+  uninstall quit: "com.smartservices.TimeLapze"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.smartservices.TimeLapze",
+    "~/Library/Containers/com.smartservices.TimeLapze",
+    "~/Library/Preferences/com.smartservices.TimeLapze.plist",
+  ]
+end
