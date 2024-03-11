40f8caa45fcf7999d88912c2565c9be853ab0e8c
Karim Ibrahim <karimibr@amazon.com>
2024-03-11 13:07:24 -0700

    Fixing the SHA signature for love2d 11.5.

--- Casks/l/love.rb
+++ Casks/l/love.rb
@@ -1,6 +1,6 @@
 cask "love" do
   version "11.5"
-  sha256 "c8ff4b57274b87772a91af4efa532721848b935eef1e0b72ac0464fb177631a5"
+  sha256 "6795bb3a1656af6a2fdfe741e150787b481886d3a280327a261a3fdded586913"
 
   url "https://github.com/love2d/love/releases/download/#{version}/love-#{version}-macos.zip",
       verified: "github.com/love2d/love/"
