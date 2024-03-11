62a23455baefc5386f3bf17ca9a74f8b45c15a4a
Bevan Kay <email@bevankay.me>
2024-03-11 22:35:34 +1100

    dbeaverlite 24.0.0

--- Casks/d/dbeaverlite.rb
+++ Casks/d/dbeaverlite.rb
@@ -1,9 +1,9 @@
 cask "dbeaverlite" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "23.3.0"
-  sha256 arm:   "bd905460bf42db0a9ab1929c72c66480764402321882f532ee41a46cf194a1e0",
-         intel: "78774a9e50c93720c51829069435c972cbd3819531857e78177bf0d83e9c0835"
+  version "24.0.0"
+  sha256 arm:   "efecb11bcedc7d3b171ce751d58a9d8340b27fa41c542cf5c3b22ab55819b141",
+         intel: "902e77e4b36b117956bd9c930e3d7f2f1d50a2e4f34641d00f152e9080e6bf66"
 
   url "https://dbeaver.com/downloads-lite/#{version}/dbeaver-le-#{version}-macos-#{arch}.dmg"
   name "DBeaver Lite Edition"
