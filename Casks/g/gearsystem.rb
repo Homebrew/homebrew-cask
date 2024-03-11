b39b18570e32b9c9dd231eacbf66ac9a58e60a0d
Bevan Kay <email@bevankay.me>
2024-03-11 23:11:28 +1100

    gearsystem 3.5.0

--- Casks/g/gearsystem.rb
+++ Casks/g/gearsystem.rb
@@ -1,10 +1,16 @@
 cask "gearsystem" do
-  version "3.4.1"
-  sha256 "b5a9a2dc62d721448d315f7116e08433257ee5f849362f570ebe99eb7f425336"
+  arch arm: "arm", intel: "intel"
 
-  url "https://github.com/drhelius/Gearsystem/releases/download/gearsystem-#{version}/Gearsystem-#{version}-macOS.zip"
+  version "3.5.0"
+  sha256 arm:   "9ec8565059b7804eabea720ed96187d8493b7f1818543b84667fe7dc1a648c81",
+         intel: "3c3c8da07289a7db03a3662891b1abbe7a7a862fdd61ba08df51546830ba889d"
+
+  url "https://github.com/drhelius/Gearsystem/releases/download/#{version}/Gearsystem-#{version}-macos-#{arch}.zip"
   name "Gearsystem"
+  desc "Sega Master System, Game Gear and SG-1000 emulator"
   homepage "https://github.com/drhelius/Gearsystem"
 
-  app "Gearsystem-#{version}-macOS/Gearsystem.app"
+  app "Gearsystem.app"
+
+  zap trash: "~/Library/Saved Application State/me.ignaciosanchez.Gearsystem.savedState"
 end
