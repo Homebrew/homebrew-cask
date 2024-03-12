7985ae2ff821a460e6b936a42590cbc87f0cf049
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-03-11 20:35:23 -0400

    dbvisualizer 24.1.3

--- Casks/d/dbvisualizer.rb
+++ Casks/d/dbvisualizer.rb
@@ -1,9 +1,9 @@
 cask "dbvisualizer" do
   arch arm: "aarch64", intel: "x64"
 
-  version "24.1.2"
-  sha256 arm:   "5864b6f2dbdb054f74b3d9b3247bbb16fbcc7615b7d5105e4dfc4ee92396c713",
-         intel: "208cd5201f3388a8fb08acd02a36e590902dbe1b85091584402a91560747e834"
+  version "24.1.3"
+  sha256 arm:   "cb41958f2c720b904a106c5421a373b5bc79cb3316ebbe8ef4050397dc3ca870",
+         intel: "24159cdad6edd9fdaf24aa4a9f1e06368f52858bc31e6c0721f90d3c5abb1439"
 
   url "https://www.dbvis.com/product_download/dbvis-#{version}/media/dbvis_macos-#{arch}_#{version.dots_to_underscores}.dmg"
   name "DbVisualizer"
