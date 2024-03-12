3f94552c8aaa8608962572b62efe53a0471fe87c
Rui Chen <rui@chenrui.dev>
2024-03-12 15:50:02 -0400

    cahier 0.4.3 (new cask)

---
+++ Casks/c/cahier.rb
@@ -0,0 +1,31 @@
+cask "cahier" do
+  version "0.4.3"
+  sha256 "4d221e1c0e91e79496e48dd717f4de2229e396ab1923b83fc9345973d0234a47"
+
+  url "https://getcahier.com/releases-macos/cahier-#{version}.dmg"
+  name "Cahier"
+  desc "Knowledge base with native support for research"
+  homepage "https://getcahier.com/"
+
+  livecheck do
+    url "https://getcahier.com/releases-macos/appcast.xml"
+    strategy :sparkle
+  end
+
+  auto_updates true
+  depends_on macos: ">= :big_sur"
+
+  app "Cahier.app"
+
+  zap trash: [
+        "~/Library/HTTPStorages/org.Cahier-App.Cahier",
+        "~/Library/Preferences/com.cahier-app.Cahier.plist",
+        "~/Library/Preferences/org.Cahier-App.Cahier.plist",
+        "~/Library/Saved Application State/org.Cahier-App.Cahier.savedState",
+      ],
+      rmdir: "~/Documents/Cahier"
+
+  caveats do
+    requires_rosetta
+  end
+end
