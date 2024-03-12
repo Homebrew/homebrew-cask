6280274a32abd8dc6c191e70e4350569d3c969d4
Anatoli Nicolae <an@thundersquared.com>
2024-03-12 19:22:32 +0100

    herd 1.5.0

--- Casks/h/herd.rb
+++ Casks/h/herd.rb
@@ -1,6 +1,6 @@
 cask "herd" do
-  version "1.4.2"
-  sha256 "7089bf2c7d5bb69d08694242071ba052f5a3a388a259193446417e0e1215a5a3"
+  version "1.5.0"
+  sha256 "3a31161f7b3c6cc757d96e02ae521ba51e45811294cf29d62dedb0c81e545406"
 
   url "https://download.herdphp.com/app_versions/Herd_#{version}.dmg",
       verified: "download.herdphp.com/app_versions/"
