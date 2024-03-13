1553e7d94d176ec21d828d18f8445df195cb7356
Amory Meltzer <Amorymeltzer@gmail.com>
2024-03-13 11:58:11 -0400

    pdf-reader-pro 3.3.1.0

--- Casks/p/pdf-reader-pro.rb
+++ Casks/p/pdf-reader-pro.rb
@@ -1,6 +1,6 @@
 cask "pdf-reader-pro" do
-  version "3.3.0.0"
-  sha256 "d85f3e56d64e0a9e38906f71b9f82e18853a0005ae01a67205ede2831d39cd43"
+  version "3.3.1.0"
+  sha256 "e0cf3e6f232a5984d1e5df0ab15ec170c9d87fa0d57af1f8c0337ccce3776b87"
 
   url "https://www.pdfreaderpro.com/downloads/PDFReaderPro_v#{version.major_minor_patch}.dmg"
   name "PDF Reader Pro"
