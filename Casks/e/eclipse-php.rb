86da1b5c70884ca4811fa48dbd4d26a9a6fd8b0f
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-03-13 13:36:36 -0400

    eclipse-php 4.31.0

--- Casks/e/eclipse-php.rb
+++ Casks/e/eclipse-php.rb
@@ -1,9 +1,9 @@
 cask "eclipse-php" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "4.30.0,2023-12"
-  sha256 arm:   "a0bc97c935bbe89307054a53868532df617b7ece0f5ed5504f6437c4b0760cf3",
-         intel: "e3a6bbef82f194984df58f95f464b5af078603ae8746d51a414a75ddec12321e"
+  version "4.31.0,2024-03"
+  sha256 arm:   "7c14b762b6ee44d7cc29a1cb5565e30a63b41cf3649b81a0086ec656f8abe25c",
+         intel: "6fbaf48541c5b25d892855c02b239c3512882009e4f6cd0c6200941945bd2f63"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-php-#{version.csv.second}-R-macosx-cocoa-#{arch}.dmg&r=1"
   name "Eclipse IDE for PHP Developers"
