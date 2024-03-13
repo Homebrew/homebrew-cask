693832d7c59effb02aba2f9e0b8883a573722e50
kiblik <kiblik@gjh.sk>
2024-03-13 00:25:11 +0000

    Update Casks/i/instantview.rb
    Co-authored-by: Razvan Azamfirei <azamfireirazvan@gmail.com>

--- Casks/i/instantview.rb
+++ Casks/i/instantview.rb
@@ -2,7 +2,7 @@ cask "instantview" do
   version "V3.19R0002"
   sha256 "b9c258ccca92281a0c942139f7e7053a655015c1221245c4a83b119e65cf0080"
 
-  url "https://www.siliconmotion.com/downloads/macOS_InstantView_#{version}.dmg"
+  url "https://www.siliconmotion.com/downloads/macOS_InstantView_V#{version.csv.first}#{version.csv.second}.dmg"
   name "InstantView"
   desc "SiliconMotion: SM76x Driver with UI"
   homepage "https://www.siliconmotion.com/"
