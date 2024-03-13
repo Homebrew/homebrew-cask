3c665d538f004c0048dc6790405cea043babdfc0
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-03-13 13:45:42 -0400

    epilogue-playback 1.2.0

--- Casks/e/epilogue-playback.rb
+++ Casks/e/epilogue-playback.rb
@@ -1,6 +1,6 @@
 cask "epilogue-playback" do
-  version "1.1.0"
-  sha256 "8f91e0f5311915e02e3cced3a62d4b0f536703172ed23507222b8943b9b1cba9"
+  version "1.2.0"
+  sha256 "a6be692c75010f410dbc4d7d23023531cdf916474c19ff93c3f6cf434803ab48"
 
   url "https://epilogue.nyc3.digitaloceanspaces.com/releases/software/Playback/version/#{version}/release/mac/Playback.dmg",
       verified: "epilogue.nyc3.digitaloceanspaces.com/releases/software/Playback/version/"
