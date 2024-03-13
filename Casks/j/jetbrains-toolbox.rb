40e753a2ca4a588f9c6b29c0d462841a4c39f807
ik-fib <ik11235@gmail.com>
2024-03-14 03:41:41 +0900

    jetbrains-toolbox 2.2.3

--- Casks/j/jetbrains-toolbox.rb
+++ Casks/j/jetbrains-toolbox.rb
@@ -1,9 +1,9 @@
 cask "jetbrains-toolbox" do
   arch arm: "-arm64"
 
-  version "2.2.2,2.2.2.20062"
-  sha256 arm:   "bf8683378c845c959a5ba948cec67d0c4c45b2b04ace3fff01444f0aa2c6ae82",
-         intel: "8f990816de6dbd7c62e8cfee09ce4f809a178ad1ecba8199fecafdf7e19054fd"
+  version "2.2.3,2.2.3.20090"
+  sha256 arm:   "25f00c9022402e16c4a93c30abcd832bde60085d7bd37103c3156ef25e2727ca",
+         intel: "542fc6b1feb2dd071486f05792c15d18ab0241e2c22769f75c4af138cbd25b98"
 
   url "https://download.jetbrains.com/toolbox/jetbrains-toolbox-#{version.csv.second}#{arch}.dmg"
   name "JetBrains Toolbox"
