5675cc9ace69647ef51d3a0c5b97c13c8d103080
William Kaiser <wkaisertexas@gmail.com>
2024-03-12 23:58:42 -0400

    Update Casks/t/timelapze.rb
    Co-authored-by: Razvan Azamfirei <azamfireirazvan@gmail.com>

--- Casks/t/timelapze.rb
+++ Casks/t/timelapze.rb
@@ -2,7 +2,7 @@ cask "timelapze" do
   version "1.0.0"
   sha256 :no_check
 
-  url "https://github.com/wkaisertexas/ScreenTimeLapse/releases/download/v1.0.0/TimeLapze.zip"
+  url "https://github.com/wkaisertexas/ScreenTimeLapse/releases/download/v#{version}/TimeLapze.zip"
   name "TimeLapze"
   desc "Record screen time lapses with ease in a simple, intuitive interface"
   homepage "https://github.com/wkaisertexas/ScreenTimeLapse"
