6dc54e116d98b06417825e0ec6bc1264db28fb69
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-03-13 12:49:11 -0400

    anytype 0.39.0

--- Casks/a/anytype.rb
+++ Casks/a/anytype.rb
@@ -1,9 +1,9 @@
 cask "anytype" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.38.0"
-  sha256 arm:   "1358fd6cd3215edc00567b9594b594537230dbb1830231877b8b36c4595a6f98",
-         intel: "f3f09d7723ce63392d876ac5f714bdabd275de5fe47af197adb0dd4851725e9f"
+  version "0.39.0"
+  sha256 arm:   "97381c492913deaa2c9b846c4735dc1f35a061a742aabc7d7374091b452bd518",
+         intel: "0dbbef40c7265b98bd9379eba2d9f17649fce6d798828de2078b6ddcc182f8cc"
 
   url "https://anytype-release.fra1.cdn.digitaloceanspaces.com/Anytype-#{version}-mac-#{arch}.dmg",
       verified: "anytype-release.fra1.cdn.digitaloceanspaces.com/"
