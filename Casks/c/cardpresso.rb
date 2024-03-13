f619c456444942e785a9740c95b69ec7d8dc2d15
Bevan Kay <email@bevankay.me>
2024-03-13 12:46:48 +1100

    cardpresso 1.7.70

--- Casks/c/cardpresso.rb
+++ Casks/c/cardpresso.rb
@@ -1,6 +1,6 @@
 cask "cardpresso" do
-  version "1.7.65.1"
-  sha256 "ed3aebaad75fdfd735ff01e936f44198597f8f0f9dbcf71a57711ce64c5a991f"
+  version "1.7.70"
+  sha256 "0b97dda95c18f9bd21089b73af7ebfe009d5295d116d5dc26cc2d6965d5b1dac"
 
   url "https://www.cardpresso.com/downloads/cardpresso_releases/for_mac_osx/cardPresso#{version}.dmg"
   name "cardpresso"
