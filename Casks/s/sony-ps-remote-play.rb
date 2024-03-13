7860bdc621097beb4582f4898c9091468cbecf5f
owine <owine@users.noreply.github.com>
2024-03-13 08:45:25 -0500

    sony-ps-remote-play 7.0.0

--- Casks/s/sony-ps-remote-play.rb
+++ Casks/s/sony-ps-remote-play.rb
@@ -1,5 +1,5 @@
 cask "sony-ps-remote-play" do
-  version "6.5.5"
+  version "7.0.0"
   sha256 :no_check
 
   url "https://remoteplay.dl.playstation.net/remoteplay/module/mac/RemotePlayInstaller.pkg"
