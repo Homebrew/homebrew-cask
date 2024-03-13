bbe100199f348ca8b5cdd6afdd4c47b935234eef
Bevan Kay <email@bevankay.me>
2024-03-14 09:49:30 +1100

    cardpresso: update livecheck, add `auto_updates` true

--- Casks/c/cardpresso.rb
+++ Casks/c/cardpresso.rb
@@ -8,9 +8,11 @@ cask "cardpresso" do
   homepage "https://www.cardpresso.com/"
 
   livecheck do
-    skip "No version information available"
+    url "https://www.cardpresso.com/cardpresso-software/latest-release/"
+    regex(/href=.*?cardPresso[._-]?v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
+  auto_updates true
   depends_on macos: ">= :high_sierra"
 
   app "cardPresso.app"
