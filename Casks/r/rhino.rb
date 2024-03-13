b0140608b66a74b42558b6df21221b0c71d5e59c
Klaus Hipp <khipp@users.noreply.github.com>
2024-03-13 01:23:11 +0100

    rhino 8.5.24072.13002

--- Casks/r/rhino.rb
+++ Casks/r/rhino.rb
@@ -1,6 +1,6 @@
 cask "rhino" do
-  version "8.4.24044.15002"
-  sha256 "63cadde2bf0f5a287f44f67a59e629e42bff1c0b39a36f67fd603c574a5708c2"
+  version "8.5.24072.13002"
+  sha256 "adc1fe566840dfb72f0fe159d30a7adafa3566d5f16a38337da8ee444cf9b314"
 
   url "https://files.mcneel.com/rhino/#{version.major}/mac/releases/rhino_#{version}.dmg",
       verified: "mcneel.com/rhino/"
