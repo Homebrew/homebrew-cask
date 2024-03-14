1e68d79a36e34ee486d62c63612a7d2cc1db2cdc
Miccal Matthews <miccal.matthews@gmail.com>
2024-03-14 09:21:22 +0800

    Update Casks/n/nperf.rb
    Co-authored-by: Daeho Ro <lamanus@outlook.kr>

--- Casks/n/nperf.rb
+++ Casks/n/nperf.rb
@@ -1,27 +1,28 @@
 cask "nperf" do
-  version "1.12.3"
-  sha256 :no_check
+  arch arm: "arm64", intel: "x86_64"
 
-  url "https://app.nperf.com/getMacApp?arch=arm64"
+  version "1.12.3"
+  sha256 arm:   "4b2d260a02a1d5567badc2ea15de06c03be80deeb8ef74bb155c5b222b8d4d37",
+         intel: "4aed59b0718a88037a2ab3413e22b10605face4b114010bb3acfb69e83b1f47a"
+
+  url "https://repo.nperf.com/macos/nperf/nPerf-#{arch}-#{version}.pkg"
   name "nPerf"
   desc "Test the quality of your Internet connection up to 10 Gb/s"
-  homepage "https://www.nperf.com/en/nperf-application-pc-mac"
+  homepage "https://www.nperf.com/"
 
   livecheck do
-    url :url
-    strategy :header_match
+    url "https://app.nperf.com/getMacApp?arch=#{arch}"
+    strategy :header_match do |headers|
+      headers["location"][/(\d+\.\d+\.\d+)\.pkg/i, 1]
+    end
   end
 
-  depends_on arch: :arm64
-
-  pkg "nPerf-arm64-#{version}.pkg"
+  pkg "nPerf-#{arch}-#{version}.pkg"
 
   uninstall pkgutil: "com.nperf.nPerf"
 
   zap trash: [
     "~/Library/Application Support/nPerf",
     "~/Library/Saved Application State/com.nperf.nPerf.savedState",
-    "/var/db/receipts/com.nperf.nPerf.bom",
-    "/var/db/receipts/com.nperf.nPerf.plist",
   ]
 end
