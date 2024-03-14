790e0b424668a4912e2649a57beedc47495530c3
G-eos <1248414+G-eos@users.noreply.github.com>
2024-03-14 22:40:01 +0100

    Update tuxguitar to 1.6.1 and change source repository
    A new team take the lead to maintain tuxguitar which was orphan since a long time.

--- Casks/t/tuxguitar.rb
+++ Casks/t/tuxguitar.rb
@@ -1,11 +1,11 @@
 cask "tuxguitar" do
-  version "1.5.6"
-  sha256 "fa53ebd78fa507bbb2a654e17efb6913957cf5eb8cceeb6da3423c82ff6e18ff"
+  version "1.6.1"
+  sha256 "d83fa6298f3c32bbe52636402f71ddc73a6d7d7ef809459d278529c0177de6a6"
 
-  url "https://downloads.sourceforge.net/tuxguitar/tuxguitar-#{version}-macosx-cocoa-64.app.tar.gz"
+  url "https://github.com/helge17/tuxguitar/releases/download/#{version}/tuxguitar-#{version}-macosx-swt-cocoa-x86_64.app.tar.gz"
   name "TuxGuitar"
   desc "Multitrack guitar tablature editor and player"
-  homepage "https://sourceforge.net/projects/tuxguitar/"
+  homepage "https://www.tuxguitar.app"
 
   app "tuxguitar-#{version}-macosx-cocoa-64.app"
 end
