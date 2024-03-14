70e67b7e340a6b20cdcbe6bc58d21bcdeaf16c92
Gnohz Gniy <0x00eeee@gmail.com>
2024-03-14 10:49:23 +0800

    Fix lunarbar 1.2.1

--- Casks/l/lunarbar.rb
+++ Casks/l/lunarbar.rb
@@ -1,6 +1,6 @@
 cask "lunarbar" do
   version "1.2.1"
-  sha256 "33a0046b10d293b1db324d105b05d4165fc7aa2ceb333f90e2ac84a130683e92"
+  sha256 "47af325b18fb3350cc9092432583cc3be72677ef41828b9ffbced5d0a4f4b897"
 
   url "https://github.com/LunarBar-app/LunarBar/releases/download/v#{version}/LunarBar-#{version}.dmg"
   name "LunarBar"
