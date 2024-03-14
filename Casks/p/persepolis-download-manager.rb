d9aa0896ed3682b65078cd2e6689d52eae14d502
Kia Hamedi <kia.arta9793@gmail.com>
2024-03-14 11:42:26 +0330

    Update to 4.0.1

--- Casks/p/persepolis-download-manager.rb
+++ Casks/p/persepolis-download-manager.rb
@@ -1,8 +1,8 @@
 cask "persepolis-download-manager" do
-  version "3.2.0"
-  sha256 "39514a158201483a713c918d744b823f63ae39142aa2024713c21174385400a3"
+  version "4.0.1"
+  sha256 "563a591c92c1fd3a6ea11761ba29614eef2b1a0e78eb7f0827bde452c1b5b610"
 
-  url "https://github.com/persepolisdm/persepolis/releases/download/#{version}/persepolis_#{version}_mac.zip",
+  url "https://github.com/persepolisdm/persepolis/releases/download/#{version}/Persepolis.Download.Manager.Intel.Chips.dmg",
       verified: "github.com/persepolisdm/persepolis/"
   name "Persepolis"
   desc "GUI for aria2"
