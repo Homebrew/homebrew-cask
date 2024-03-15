8ea0496e519e869629d708d9067a2ab93da93011
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-03-14 21:05:44 -0400

    openbb-terminal 3.2.5

--- Casks/o/openbb-terminal.rb
+++ Casks/o/openbb-terminal.rb
@@ -2,9 +2,9 @@ cask "openbb-terminal" do
   # raised an issue about the x86.64 typo, https://github.com/OpenBB-finance/OpenBBTerminal/issues/5405
   arch arm: "ARM64", intel: "x86.64"
 
-  version "3.2.4"
-  sha256 arm:   "658a195afc1656a372733f6b9d2860fd431644652f5cb4af17d4de532c06a1db",
-         intel: "95c3b32a98ed3614614c10684dba25356dbe7eec91706cc11ae5cc304ccddaef"
+  version "3.2.5"
+  sha256 arm:   "80c12368b62b135bb74736a411c9adf277dbed46969b0b49be46cbd768afb618",
+         intel: "d5a386b10ba076a69a7e642034a55b1c5e53ef32c8fc6383985199709b06572e"
 
   url "https://github.com/OpenBB-finance/OpenBBTerminal/releases/download/v#{version}/#{arch}.MacOS.OpenBB.Terminal.v#{version}.pkg",
       verified: "github.com/OpenBB-finance/OpenBBTerminal/"
