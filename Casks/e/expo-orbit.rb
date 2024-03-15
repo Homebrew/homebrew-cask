1d86700fadf477bfcc0f3fd58ee99e6bd9f02b97
Gabriel Donadel <donadeldev@gmail.com>
2024-03-15 00:44:29 -0300

    expo-orbit 1.1.1

--- Casks/e/expo-orbit.rb
+++ Casks/e/expo-orbit.rb
@@ -1,6 +1,6 @@
 cask "expo-orbit" do
-  version "1.1.0"
-  sha256 "437e6906f99883fe2bbcb732fe86046d33f4ad95dc2a76d83019a701847e72bc"
+  version "1.1.1"
+  sha256 "19d92c8906253fed5472b98f640fc35f7246f1acd2b936ce8a39e778bba24f95"
 
   url "https://github.com/expo/orbit/releases/download/expo-orbit-v#{version}/expo-orbit.v#{version}.zip"
   name "Expo Orbit"
