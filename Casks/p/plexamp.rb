7c504d8b98825cc942b4072a02b7ddab3c825136
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-03-16 16:52:00 -0400

    plexamp 4.10.0

--- Casks/p/plexamp.rb
+++ Casks/p/plexamp.rb
@@ -1,9 +1,9 @@
 cask "plexamp" do
   arch arm: "-arm64"
 
-  version "4.9.5"
-  sha256 arm:   "4598557aa06fe685422a6013158735da74b7f4e2ad4bbd1e598abf456b965252",
-         intel: "c184450c07e2613b4edf0e174060c74f2947c6c5d663279c6a4586aa819911cc"
+  version "4.10.0"
+  sha256 arm:   "dadfeae05a1ec037cdc414108de8b97cc9c21e96f335ab980b90b8c6cec04ad6",
+         intel: "32754d6a7d6a65dc57e2d3c3feab72ffe37fe0d2d859b2ec9a85e2c92a51649f"
 
   url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}#{arch}.dmg",
       verified: "plexamp.plex.tv/"
