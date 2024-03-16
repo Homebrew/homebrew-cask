f9f532e64db4bc1b87d9f7eaf2785d776810cd56
Rui Chen <rui@chenrui.dev>
2024-03-16 13:45:48 -0400

    flycast 2.3

--- Casks/f/flycast.rb
+++ Casks/f/flycast.rb
@@ -1,6 +1,6 @@
 cask "flycast" do
-  version "2.2"
-  sha256 "436702143ae8d8bbdd165784245e50249b9e27e56baaaa26823dfcecda56e2c2"
+  version "2.3"
+  sha256 "abf46c3ec2cf7059f765d23fe8bd7b9febac652f4b943188048f0c327aa06f0c"
 
   url "https://github.com/flyinghead/flycast/releases/download/v#{version}/flycast-macOS-#{version}.zip"
   name "Flycast"
