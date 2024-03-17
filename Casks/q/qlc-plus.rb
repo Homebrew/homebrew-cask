76fe94c24ccc4a265eb65183962e734264446253
adithyasunil26 <adithyasunil26@gmail.com>
2024-03-18 00:32:00 +0530

    qlc-plus 4.13.0

--- Casks/q/qlc-plus.rb
+++ Casks/q/qlc-plus.rb
@@ -1,6 +1,6 @@
 cask "qlc-plus" do
-  version "4.12.7"
-  sha256 "3fb75d4e267e3743c2659cc32301d2158a0abe0910a2dc14808780fb9aed28f8"
+  version "4.13.0"
+  sha256 "2d64913a088eea6afe6210e781a092ed743dcb02fbe769cccf490e893665402e"
 
   url "https://qlcplus.org/downloads/#{version}/QLC+_#{version}.dmg"
   name "Q Light Controller+"
