8fad96090baf67492f3829f4efad8b30ac371fe1
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-03-16 20:43:03 -0400

    Update persepolis-download-manager.rb

--- Casks/p/persepolis-download-manager.rb
+++ Casks/p/persepolis-download-manager.rb
@@ -2,7 +2,7 @@ cask "persepolis-download-manager" do
   version "4.0.1"
   sha256 "ef4d6ee3d17788408cb4cca1ab34b1b00126cce3dfdccf4c2d926f151f957277"
 
-  url "https://github.com/persepolisdm/persepolis/releases/download/#{version}/persepolis_#{version}.1_macos.dmg ",
+  url "https://github.com/persepolisdm/persepolis/releases/download/#{version}/persepolis_#{version}.1_macos.dmg",
       verified: "github.com/persepolisdm/persepolis/"
   name "Persepolis"
   desc "GUI for aria2"
