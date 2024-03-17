5f853db25f54fc7df2921e9b891d1c79267c2f54
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-03-17 15:53:08 -0400

    wire: update livecheck
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/w/wire.rb
+++ Casks/w/wire.rb
@@ -8,9 +8,21 @@ cask "wire" do
   desc "Collaboration platform focusing on security"
   homepage "https://wire.com/"
 
+  # Not every GitHub release provides a file for macOS, so we check multiple
+  # recent releases instead of only the "latest" release.
   livecheck do
     url :url
     regex(%r{^macos[/._-]v?(\d+(?:\.\d+)+)$}i)
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        next if release["draft"] || release["prerelease"]
+
+        match = release["tag_name"]&.match(regex)
+        next if match.blank?
+
+        match[1]
+      end.flatten
+    end
   end
 
   pkg "Wire.pkg"
