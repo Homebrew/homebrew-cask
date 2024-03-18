3257d4a7b9314b878a091a5db26b390b8ce9cfdc
Bevan Kay <email@bevankay.me>
2024-03-18 11:00:37 +1100

    4k-youtube-to-mp3 5.2.0

--- Casks/4/4k-youtube-to-mp3.rb
+++ Casks/4/4k-youtube-to-mp3.rb
@@ -1,16 +1,18 @@
 cask "4k-youtube-to-mp3" do
-  # NOTE: "3" is not a version number, but an intrinsic part of the product name
-  version "5.1.1"
-  sha256 "3b4dd8668b6ae04db206c184f60403cea055931d1464d289dc8a4b99bf04208f"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://dl.4kdownload.com/app/4kyoutubetomp3_#{version}.dmg"
+  # NOTE: "3" is not a version number, but an intrinsic part of the product name
+  version "5.2.0"
+  sha256 "34452eb2d36ead216b967e092fd9255bb96deca734f613a9835732530e585514"
+
+  url "https://dl.4kdownload.com/app/4kyoutubetomp3_#{version}_#{arch}.dmg"
   name "4K YouTube to MP3"
   desc "Turn YouTube links into MP3 files"
   homepage "https://www.4kdownload.com/products/youtubetomp3/1"
 
   livecheck do
     url "https://www.4kdownload.com/download"
-    regex(%r{href=.*?/4kyoutubetomp3[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
+    regex(%r{href=.*?/4kyoutubetomp3[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}\.dmg}i)
   end
 
   depends_on macos: ">= :high_sierra"
