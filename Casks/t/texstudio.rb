7eefae7543c06e89f5980a7f7b2b61b418f7b754
Sean Molenaar <SMillerDev@users.noreply.github.com>
2024-03-18 09:24:55 +0100

    texstudio: use a simple conditional for the app name

--- Casks/t/texstudio.rb
+++ Casks/t/texstudio.rb
@@ -1,17 +1,11 @@
 cask "texstudio" do
   arch arm: "-m1"
   ext = on_arch_conditional arm: "zip", intel: "dmg"
+  app_suffix = on_arch_conditional arm: "-#{version}-osx-m1", intel: ""
 
   version "4.7.3"
   sha256 arm:   "314da45d6071c4e18643282b25f6df7114bd42964d85b63526ffbfa048d993e7",
          intel: "ce1cd5ab82d907165e232dfc5ccf8329f086e375f4936c6b9da9b0091ddd0674"
-
-  on_arm do
-    app "texstudio-#{version}-osx-m1.app"
-  end
-  on_intel do
-    app "texstudio.app"
-  end
 
   url "https://github.com/texstudio-org/texstudio/releases/download/#{version}/texstudio-#{version}-osx#{arch}.#{ext}",
       verified: "github.com/texstudio-org/texstudio/"
@@ -20,6 +14,8 @@ cask "texstudio" do
   homepage "https://texstudio.org/"
 
   depends_on macos: ">= :big_sur"
+
+  app "texstudio#{app_suffix}.app"
 
   zap trash: [
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/texstudio.sfl*",
