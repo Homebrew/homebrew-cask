068b7fbc624213c24f60ba281d9ffaa9a2b74798
Anton Engelhardt <antoncengelhardt@icloud.com>
2024-03-20 16:04:33 +0100

    jamie 4.0.4
    Signed-off-by: Anton Engelhardt <antoncengelhardt@icloud.com>

--- Casks/j/jamie.rb
+++ Casks/j/jamie.rb
@@ -1,6 +1,6 @@
 cask "jamie" do
-  version "4.0.3"
-  sha256 "fc7330450cdbe10464012cc091434fe4701049810dfc027e802b7cdf93af0e90"
+  version "4.0.4"
+  sha256 "2971c57d9948ba548ed02ae15eff2958e92592dbc92d53ad921e4c0211d46696"
 
   url "https://github.com/louismorgner/jamie-release/releases/download/v#{version}/jamie-#{version}.dmg",
       verified: "github.com/louismorgner/jamie-release/"
