ab48de1149bdc6701d8104544a3ae3579ff5038e
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-03-20 09:30:26 -0400

    trezor-suite 24.3.2

--- Casks/t/trezor-suite.rb
+++ Casks/t/trezor-suite.rb
@@ -1,9 +1,9 @@
 cask "trezor-suite" do
   arch arm: "arm64", intel: "x64"
 
-  version "24.2.4"
-  sha256 arm:   "78ad5c31db8034129a2664ab4012441906f7cabd2624247e57e90c48d518eff2",
-         intel: "7ccb75097496d72f736e1d73e46a1ff4621300868ce273f7acaac2faeb7e2988"
+  version "24.3.2"
+  sha256 arm:   "19b25a604309b43d7d258f9939972f901526e6b6bba895c7915df6fa0aa56769",
+         intel: "a658bbb217d34804513434b29305d579882add0d0dd4c026175fd3e1ca1b1ceb"
 
   url "https://github.com/trezor/trezor-suite/releases/download/v#{version}/Trezor-Suite-#{version}-mac-#{arch}.dmg",
       verified: "github.com/trezor/trezor-suite/"
