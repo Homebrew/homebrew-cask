93ffa6898b29fb2eed03ef8e2ca1b18fc2a72582
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-03-20 09:01:03 -0400

    fixkey 1.3.2

--- Casks/f/fixkey.rb
+++ Casks/f/fixkey.rb
@@ -1,6 +1,6 @@
 cask "fixkey" do
-  version "1.2.2"
-  sha256 "1221114291c09bdf16ce286c56201db0adebc40eeb285aefd26dc8a88f2611de"
+  version "1.3.2"
+  sha256 "828d6d87ad8e1df78c9060e99ba13e8ea108711ec083a7d34af90552bc5271cc"
 
   url "https://fixkey.download/fixkey%20#{version}.dmg",
       verified: "fixkey.download/"
