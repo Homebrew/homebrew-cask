33ac1b1a7d747495293dd957811acbbdfe9b74d6
Bevan Kay <email@bevankay.me>
2024-03-20 13:43:05 +1100

    recovery-disk-assistant: deprecate, update url

--- Casks/r/recovery-disk-assistant.rb
+++ Casks/r/recovery-disk-assistant.rb
@@ -2,14 +2,11 @@ cask "recovery-disk-assistant" do
   version "1.0,18"
   sha256 :no_check
 
-  url "https://support.apple.com/downloads/DL1433/en_US/RecoveryDiskAssistant.dmg"
+  url "https://download.info.apple.com/Mac_OS_X/041-2001.20110808.QFC1d/RecoveryDiskAssistant.dmg"
   name "Recovery Disk Assistant"
   homepage "https://support.apple.com/kb/DL1433"
 
-  livecheck do
-    url :url
-    strategy :extract_plist
-  end
+  deprecate! date: "2024-03-20", because: :unmaintained
 
   app "Recovery Disk Assistant.app"
 end
