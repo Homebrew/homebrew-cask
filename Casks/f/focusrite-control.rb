2b3f6515644c1b3d0eaf875f3ab6f69e12bc1f3a
adithyasunil26 <adithyasunil26@gmail.com>
2024-03-22 01:58:14 +0530

    Updated focusrite-control.rb

--- Casks/f/focusrite-control.rb
+++ Casks/f/focusrite-control.rb
@@ -1,8 +1,8 @@
 cask "focusrite-control" do
-  version "3.17.0"
-  sha256 "d3421b8cd90a54c69072b5c0e391adaaac58a4ad9c09290a5d5caea46838ecf6"
+  version "3.18"
+  sha256 "75e946eb0e1700df42a4220ac2477db5522bdacb739a6ab85669baa98248b4f2"
 
-  url "https://fael-downloads-prod.focusrite.com/customer/prod/downloads/focusrite_control_#{version}.dmg"
+  url "https://fael-downloads-prod.focusrite.com/customer/prod/downloads/focusrite-control-#{version}.dmg"
   name "Focusrite Control"
   desc "Focusrite interface controller"
   homepage "https://focusrite.com/en"
