f53d9a41e2b057a198e5f8718ef0d14393f0228c
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-03-21 15:05:53 -0400

    simply-fortran: fix versions
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/s/simply-fortran.rb
+++ Casks/s/simply-fortran.rb
@@ -2,8 +2,8 @@ cask "simply-fortran" do
   arch arm: "-arm64", intel: "-x86_64"
 
   on_big_sur :or_older do
-    version "3.25.3757"
-    sha256 "0975462a1593bdc976dc87d6a9da88b97d8268a1a97caebeefd359e56ef73195"
+    version "3.34.4154"
+    sha256 "676f096f8ee509252023bcd3eafd99897bf079e87ebd3ffd2f41dfad6b45f273"
 
     url "https://download.simplyfortran.com/#{version.major_minor}/macos/simplyfortran-#{version}-x86_64.legacy.dmg"
 
@@ -13,8 +13,8 @@ cask "simply-fortran" do
   end
   on_monterey :or_newer do
     on_arm do
-      version "3.34.4154"
-      sha256 "b0560847015583306090fd6a8ab5df83f73396fec0c6dd5af4eb29b9de9d2306"
+      version "3.32.4016"
+      sha256 "11d513fc26c367869e678e3a46f5ec80bae8e55747c1331c46c022deb388937e"
     end
     on_intel do
       version "3.34.4154"
