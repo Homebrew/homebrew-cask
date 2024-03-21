34483a698b97ee96c30876b08071d4044bf3a30e
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-03-21 11:09:32 -0400

    p4v 2024.1

--- Casks/p/p4v.rb
+++ Casks/p/p4v.rb
@@ -1,6 +1,6 @@
 cask "p4v" do
-  version "2023.4,2558838"
-  sha256 "f9f599f85b9c824df03166dbd239a51c6a71c123f4ab4caf387480cad0034004"
+  version "2024.1,2573667"
+  sha256 "fd89300cdab59db1380ce61ece18fa8224e24c4de811b5d126d7b84d0656ddf9"
 
   url "https://filehost.perforce.com/perforce/r#{version.major[-2..]}.#{version.minor}/bin.macosx12u/P4V.dmg"
   name "Perforce Helix Visual Client"
