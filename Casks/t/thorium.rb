38185740154f351f5a761f16fbfe69cb6cd60f87
adithyasunil26 <adithyasunil26@gmail.com>
2024-03-21 18:27:04 +0530

    thorium 2.4.0

--- Casks/t/thorium.rb
+++ Casks/t/thorium.rb
@@ -1,6 +1,6 @@
 cask "thorium" do
-  version "2.3.0"
-  sha256 "87680fb4bdf63f2dcaed9507f9af10a8e8f8f20412fcdc455f611eefc99458f6"
+  version "2.4.0"
+  sha256 "353ea171a32953398d9fff85aa6e4d8b8b21e1552aef5b10528d160af6bf335b"
 
   url "https://github.com/edrlab/thorium-reader/releases/download/v#{version}/Thorium-#{version}.dmg",
       verified: "github.com/edrlab/thorium-reader/"
