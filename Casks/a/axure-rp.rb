5063ca55ae1b519d17de8186ef3aa0acc198b218
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-21 18:30:16 +0000

    axure-rp 10.0.0.3917

--- Casks/a/axure-rp.rb
+++ Casks/a/axure-rp.rb
@@ -1,6 +1,6 @@
 cask "axure-rp" do
-  version "10.0.0.3916"
-  sha256 "8b838246953009f355bbfd16160b1455ef64ce466f0ea11df3a3c2efa0014475"
+  version "10.0.0.3917"
+  sha256 "a954799fe781580f73596ede88e2ab6e9847a0bf594221d2bbace3d1ae4f1c8d"
 
   url "https://axure.cachefly.net/versions/#{version.csv.first.major_minor.dots_to_hyphens}/AxureRP-Setup-#{version.split(".")[3]}.dmg",
       verified: "axure.cachefly.net/"
