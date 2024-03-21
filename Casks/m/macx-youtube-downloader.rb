d6a07f002385f4b114076d9a465d8a4db59ad2df
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-03-21 15:28:45 -0400

    macx-youtube-downloader 5.2.9

--- Casks/m/macx-youtube-downloader.rb
+++ Casks/m/macx-youtube-downloader.rb
@@ -1,5 +1,5 @@
 cask "macx-youtube-downloader" do
-  version "5.2.8,2024011901"
+  version "5.2.9,2024032001"
   sha256 :no_check
 
   url "https://www.macxdvd.com/download/macx-youtube-downloader-free.dmg"
