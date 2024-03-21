acfe122cabba65fe9f53aee069295613537170ef
adithyasunil26 <adithyasunil26@gmail.com>
2024-03-22 00:26:01 +0530

    simply-fortran 3.34.4154

--- Casks/s/simply-fortran.rb
+++ Casks/s/simply-fortran.rb
@@ -13,12 +13,12 @@ cask "simply-fortran" do
   end
   on_monterey :or_newer do
     on_arm do
-      version "3.32.4016"
-      sha256 "11d513fc26c367869e678e3a46f5ec80bae8e55747c1331c46c022deb388937e"
+      version "3.34.4154"
+      sha256 "b0560847015583306090fd6a8ab5df83f73396fec0c6dd5af4eb29b9de9d2306"
     end
     on_intel do
-      version "3.34.4153"
-      sha256 "e51694b4311331824066f4e047d711e1a8e78d0270a726975253ca7ec941e52b"
+      version "3.34.4154"
+      sha256 "ee48deb9308b68df1ff65d86d6f1df37d5c46bdd40f8ccd1ebe27677bc9b2a27"
     end
 
     url "https://download.simplyfortran.com/#{version.major_minor}/macos/simplyfortran-#{version}#{arch}.dmg"
