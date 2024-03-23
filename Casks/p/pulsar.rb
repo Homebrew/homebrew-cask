11e206b82f18946a7d2ba16ef246be4e9589ff07
runner <runner@Mac-1711184898034.local>
2024-03-23 09:25:21 +0000

    pulsar 1.115.0

--- Casks/p/pulsar.rb
+++ Casks/p/pulsar.rb
@@ -2,9 +2,9 @@ cask "pulsar" do
   arch arm: "Silicon", intel: "Intel"
   arch_suffix = on_arch_conditional arm: "-arm64"
 
-  version "1.114.0"
-  sha256 arm:   "d34f8c78df573db23c07ffa6cb716b24eb06bdc8616cb618ffd7b531272d47d0",
-         intel: "d346bb086c826f8618318f74fc5b1479238da8afdbd3b743111c6a1d41391193"
+  version "1.115.0"
+  sha256 arm:   "4f6a7880d478b52d7442dec81542c9f66c433a5bbd981a4283f3f9aa2487e2ff",
+         intel: "1df1bfa39284a66ac2d7e79f098b11592febabaf1a01b69adfd4a73fd57af087"
 
   url "https://github.com/pulsar-edit/pulsar/releases/download/v#{version}/#{arch}.Mac.Pulsar-#{version}#{arch_suffix}-mac.zip",
       verified: "github.com/pulsar-edit/pulsar/"
