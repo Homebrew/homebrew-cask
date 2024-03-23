267d4178acc1a8f48208f1e4dc87be5294809f78
adithyasunil26 <adithyasunil26@gmail.com>
2024-03-23 21:33:25 +0530

    imhex 1.33.2

--- Casks/i/imhex.rb
+++ Casks/i/imhex.rb
@@ -1,9 +1,9 @@
 cask "imhex" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.33.1"
-  sha256 arm:   "0f476ecc50309102bdd4adbe5124843a45232bd67639928d01fd7a6d53ec5249",
-         intel: "41596ccba6288ab97ae12b460335e8c8ea48ac6f0dac6884ca1a50c8fa4f0a8c"
+  version "1.33.2"
+  sha256 arm:   "7b61556864d8156801364213e33b463e50373d49d77565cca9e68687bef5e0e2",
+         intel: "97e59c85cb8ebc8bcd5b413ec09c6e462ee4e72a191474df53174e10b35be831"
 
   url "https://github.com/WerWolv/ImHex/releases/download/v#{version}/imhex-#{version}-macOS-#{arch}.dmg",
       verified: "github.com/WerWolv/ImHex/"
