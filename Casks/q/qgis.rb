fe1c19d5d8c979ec3948b2da78c0c536aa3b21ea
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-03-23 14:20:32 -0400

    qgis 3.36.1

--- Casks/q/qgis.rb
+++ Casks/q/qgis.rb
@@ -1,6 +1,6 @@
 cask "qgis" do
-  version "3.36.0,20240223_123737"
-  sha256 "d26e0ca3105c2a68aaeba3dd9f7654c487ea1540d24999519263c1eae3e3406c"
+  version "3.36.1,20240322_122530"
+  sha256 "0f089160d276380db6f0f7a7e880ed1a688833fd5787ea77f1f4bf7dadebdb1f"
 
   url "https://qgis.org/downloads/macos/pr/qgis_pr_final-#{version.csv.first.dots_to_underscores}_#{version.csv.second}.dmg"
   name "QGIS"
