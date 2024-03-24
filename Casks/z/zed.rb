a40a537038a79a43d42b8a52f9bcd2151d8aeafd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-24 18:42:40 +0000

    zed 0.127.4

--- Casks/z/zed.rb
+++ Casks/z/zed.rb
@@ -1,6 +1,6 @@
 cask "zed" do
-  version "0.127.3"
-  sha256 "7d3f20d3618d7dcaa35465f468eef9d03cd7204c5d5ab1303a5d6f1d6e943b54"
+  version "0.127.4"
+  sha256 "ee221ec0b766e17b497539bd545fc92e20770ca6a1bd90b09b9de4859ede590e"
 
   url "https://zed.dev/api/releases/stable/#{version}/Zed.dmg"
   name "Zed"
