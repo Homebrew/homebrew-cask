cd577427e8a6ba3b882545d03efcc7a837a1c766
Dark-detsixE <c2572388311@gmail.com>
2024-03-25 18:04:50 +0800

    idafree 8.4

--- Casks/i/idafree.rb
+++ Casks/i/idafree.rb
@@ -1,9 +1,9 @@
 cask "idafree" do
   arch arm: "arm_"
 
-  version "8.3"
-  sha256 arm:   "52fd0726640f3f5697e657d074f363c147a97b46781955339fb63fef547c8992",
-         intel: "e1c679dbf6518c4b25d86d84572aa215c998281d4f61b99affa65a9583c0ee11"
+  version "8.4"
+  sha256 arm:   "75b806df3a3be1268fa079fb3b1e0bfbaf3340ee2712f9a2eb33fca0e2c19c83",
+         intel: "d5fb5cc4443c85a692503c59654b46910d92d5dcbd68c893c12c1ba8c0ea2729"
 
   url "https://out7.hex-rays.com/files/#{arch}idafree#{version.no_dots}_mac.app.zip"
   name "IDA Free"
