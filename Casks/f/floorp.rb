5cb221b1cc28e4bf1181181af2a9dd8781fc1ce4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-25 09:32:52 +0000

    floorp 11.11.2

--- Casks/f/floorp.rb
+++ Casks/f/floorp.rb
@@ -1,6 +1,6 @@
 cask "floorp" do
-  version "11.11.1"
-  sha256 "88d669b8b6187b7be28738f0b39382322450d8a88143ef9c0fac0a7a2737e009"
+  version "11.11.2"
+  sha256 "24f058e1cc2cf2b27aa6ac675bb4422969645be1415804742544fd72afc65416"
 
   url "https://github.com/Floorp-Projects/Floorp/releases/download/v#{version}/floorp-macOS-universal.dmg",
       verified: "github.com/Floorp-Projects/Floorp/"
