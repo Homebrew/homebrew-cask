e5a34276de12c005ce3b47efa732c5e17b3155f6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-25 18:33:06 +0000

    fork 2.41.2

--- Casks/f/fork.rb
+++ Casks/f/fork.rb
@@ -1,6 +1,6 @@
 cask "fork" do
-  version "2.41.1"
-  sha256 "b544e88fc24ea226fad9d11e63170600c4677085ba4465fffacc6fda85d93214"
+  version "2.41.2"
+  sha256 "316932411a0e96262f0093ee732a271313820c43a0dd03978a34d67c7bc4741b"
 
   url "https://cdn.fork.dev/mac/Fork-#{version}.dmg"
   name "Fork"
