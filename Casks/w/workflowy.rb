14da402570c537cdba4d4576d8dfd76507ee2f22
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-03-26 15:22:38 +0000

    workflowy 4.0.2403261208

--- Casks/w/workflowy.rb
+++ Casks/w/workflowy.rb
@@ -1,6 +1,6 @@
 cask "workflowy" do
-  version "4.0.2403190958"
-  sha256 "6f55783c44d2eb074a26c92678d3ef857eb7803aef6ffb2058df96d1b3a585f9"
+  version "4.0.2403261208"
+  sha256 "67194e38df7cc740b7df5a48c24ca3bf32ecc6b011e6d3f8ae7c7d0b47586847"
 
   url "https://github.com/workflowy/desktop/releases/download/v#{version}/WorkFlowy.zip",
       verified: "github.com/workflowy/desktop/"
