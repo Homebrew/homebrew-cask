fa6d6a601a8795a29ed4deb06792874d793522fe
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-03-26 10:35:40 -0400

    kopiaui 0.16.1

--- Casks/k/kopiaui.rb
+++ Casks/k/kopiaui.rb
@@ -1,9 +1,9 @@
 cask "kopiaui" do
   arch arm: "-arm64"
 
-  version "0.16.0"
-  sha256 arm:   "3a638d034f063a6a162d1e217efdbbee4309b950c5d1de090ecef6be5a10f850",
-         intel: "4bd3e663a7b9db0635770b8019718efee4d66ffdb334e756be6daff238cc84d5"
+  version "0.16.1"
+  sha256 arm:   "109c3a2e1bebdc43e2425a742f3bd795f4a64b7adce3a2ff60a5354e1dc4e69a",
+         intel: "479416fe5b720cf05b919959b5032bfdb2df86cfcbd3c6a9c38e664dedc19699"
 
   url "https://github.com/kopia/kopia/releases/download/v#{version}/KopiaUI-#{version}#{arch}.dmg",
       verified: "github.com/kopia/kopia/"
