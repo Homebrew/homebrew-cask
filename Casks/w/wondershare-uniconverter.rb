043e80c18464b49fb3a005183ea1f1e29d8a42a0
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-03-27 17:05:12 -0400

    wondershare-uniconverter 15.5.4

--- Casks/w/wondershare-uniconverter.rb
+++ Casks/w/wondershare-uniconverter.rb
@@ -1,9 +1,9 @@
 cask "wondershare-uniconverter" do
   arch arm: "arm_"
 
-  version "15.5.3"
-  sha256 arm:   "20d7eb3debb41d7b1a227020efeaf49e5d3f9a5c40c98df294d76bfcaa306dbb",
-         intel: "f95d82257d37e8d98fcca5bcc2100a0e502aa68dae728b8cb42265c3cee15b46"
+  version "15.5.4"
+  sha256 arm:   "e8ffff33fdcbd3215e8454e3438d51e6a1181f51e29a399fcde0335966fac5b2",
+         intel: "85f86890d0c76f1e676a37c3526d28386dca9f7803e31ddc5248cbe6d09d4a1d"
 
   url "https://download.wondershare.com/cbs_down/uniconverter#{version.major}-mac_#{arch}#{version}_full14207.zip"
   name "UniConverter"
