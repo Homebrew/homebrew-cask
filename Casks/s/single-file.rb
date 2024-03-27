5562a8aaa79fda2e53169ce23d30cc2e1991290f
AngelaDMerkel <63363428+AngelaDMerkel@users.noreply.github.com>
2024-03-27 19:14:34 +0100

    Included Intel Compatibility
    Updated to include support for x86

--- Casks/s/single-file.rb
+++ Casks/s/single-file.rb
@@ -1,11 +1,12 @@
 cask "single-file" do
+  arch arm: "aarch64-apple-darwin", intel: "x86_64-apple-darwin"
   version "2.0.14"
   sha256 :no_check
 
-  url "https://github.com/gildas-lormeau/single-file-cli/releases/download/v2.0.14/single-file-aarch64-apple-darwin"
+  url "https://github.com/gildas-lormeau/single-file-cli/releases/download/v#{version}/single-file-#{arch}"
   name "single-file"
   desc "CLI tool for saving a faithful copy of a complete web page in a single HTML file"
   homepage "https://github.com/gildas-lormeau/single-file-cli/blob/master/README.MD"
 
-  binary "single-file-aarch64-apple-darwin"
+  binary "single-file-#{arch}"
 end
