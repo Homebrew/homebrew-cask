5f50d0271ad20c217040a563082c84046231478b
Bevan Kay <email@bevankay.me>
2024-03-28 09:24:34 +1100

    shutter-encoder: remove checksum

--- Casks/s/shutter-encoder.rb
+++ Casks/s/shutter-encoder.rb
@@ -2,8 +2,7 @@ cask "shutter-encoder" do
   arch arm: "Apple Silicon", intel: "Mac 64bits"
 
   version "18.0"
-  sha256 arm:   "4d69db00f2ab55d6f8b550af6847dd77a0717084631a238488256c8ccd3dc130",
-         intel: "b9513d56511e6de192053aca52b35a831e64a394b4f33c60437e897581bbd787"
+  sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.shutterencoder.com/Shutter%20Encoder%20#{version}%20#{arch.gsub(" ", "%20")}.pkg"
   name "Shutter Encoder"
