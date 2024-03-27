4f869456d3c4e9b8ee6ec3a7178308bdef56f9bf
joaquimrocha <joaquimrocha@users.noreply.github.com>
2024-03-27 13:31:10 +0000

    Update Headlamp version to 0.23.1
    Signed-off-by: joaquimrocha <joaquimrocha@users.noreply.github.com>

--- Casks/h/headlamp.rb
+++ Casks/h/headlamp.rb
@@ -1,9 +1,9 @@
 cask "headlamp" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.23.0"
-  sha256 arm:   "d42f31ccb98d6c4e3db630b54fe79a595f8dc2c7fff7268b7cee0ec8b0e5824f",
-         intel: "a53a27f822dc525b06b14d0d3e029189adcefe0c0fc3c711120cccbb25efbf4c"
+  version "0.23.1"
+  sha256 arm:   "d488c8d0e07110f56df1a16f84a3cfac97219a4ce7167525bada2682c4b30d4a",
+         intel: "042fded279cd2c7d7fd90c4007327d62c8e6d7ff693c9998cd352c0c6a706973"
 
   url "https://github.com/kinvolk/headlamp/releases/download/v#{version}/Headlamp-#{version}-mac-#{arch}.dmg",
       verified: "github.com/kinvolk/headlamp/"
