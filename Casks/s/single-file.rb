611845198ba5d409420ffe45c3f7461c116075d7
AngelaDMerkel <63363428+AngelaDMerkel@users.noreply.github.com>
2024-03-27 18:33:59 +0100

    Added single-file-cli

--- Casks/s/single-file.rb
+++ Casks/s/single-file.rb
@@ -1,8 +1,8 @@
 cask "single-file" do
-  version "2.0.10"
-  sha256 "48f4c81b3bf76ab4a2ec1672056fefba73ddceed34fd436c77b6a45ba50de96b"
+  version "2.0.14"
+  sha256 :no_check
 
-  url "https://github.com/gildas-lormeau/single-file-cli/releases/download/v#{version}/single-file-aarch64-apple-darwin"
+  url "https://github.com/gildas-lormeau/single-file-cli/releases/download/v2.0.14/single-file-aarch64-apple-darwin"
   name "single-file"
   desc "CLI tool for saving a faithful copy of a complete web page in a single HTML file"
   homepage "https://github.com/gildas-lormeau/single-file-cli/blob/master/README.MD"
@@ -12,5 +12,5 @@ cask "single-file" do
     strategy :github_latest
   end
 
-  app "single-file"
+  binary "single-file"
 end
