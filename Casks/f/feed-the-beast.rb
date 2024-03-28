6ef5696f4a59d081d9bdc64de05fb2c101973c3b
Bevan Kay <email@bevankay.me>
2024-03-28 11:24:07 +1100

    feed-the-beast: various updates (wip)

--- Casks/f/feed-the-beast.rb
+++ Casks/f/feed-the-beast.rb
@@ -1,19 +1,18 @@
 cask "feed-the-beast" do
-  version "202401041638,9dc7936164"
-  sha256 "7cd9db09f32a90c48029f698e60100e18d19bde8f16b7b26649f17e9c2828f98"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://apps.modpacks.ch/FTBApp/release/#{version.csv.first}-#{version.csv.second}/FTBA_macos_#{version.csv.first}-#{version.csv.second}.dmg",
-      verified: "apps.modpacks.ch/FTBApp/"
+  version "1.25.6"
+  sha256 arm:   "7cd9db09f32a90c48029f698e60100e18d19bde8f16b7b26649f17e9c2828f98",
+         intel: "7cd9db09f32a90c48029f698e60100e18d19bde8f16b7b26649f17e9c2828f99"
+
+  url "https://feed-the-beast.com/app/ftb-app-#{version}-#{arch}.dmg"
   name "Feed the Beast"
   desc "Minecraft mod downloader and manager"
   homepage "https://www.feed-the-beast.com/"
 
   livecheck do
     url "https://meta.feed-the-beast.com/v1/app/versions"
-    regex(/FTBA[._-]macos[._-](\d+)[._-](\h+)\.dmg/i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
-    end
+    regex(/ftb[._-]app[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}\.dmg/i)
   end
 
   app "FTBApp.app"
