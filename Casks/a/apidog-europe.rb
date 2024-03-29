1e4b0e66d54de7f0bad07a71c497bf1322eb8937
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-03-29 11:47:46 -0400

    apidog-europe 2.5.15

--- Casks/a/apidog-europe.rb
+++ Casks/a/apidog-europe.rb
@@ -2,9 +2,9 @@ cask "apidog-europe" do
   arch arm: "-macOS-arm64"
   livecheck_folder = on_arch_conditional arm: "-arm64"
 
-  version "2.5.13"
-  sha256 arm:   "4171b20dfbe2f648520b7a0198ebcb491c3de7640c88145cf3a85964285a2670",
-         intel: "fadf10c2479305788e5b55f1263e799ec46a0578452f5cfcdf5ca5c121c84125"
+  version "2.5.15"
+  sha256 arm:   "588c20bcc08a8a39e60316ca857fc2528411e4a973ddedbb9ee6321912c1fc18",
+         intel: "7b4222a14fb868f693b90613bc55e25121b0ad016348faecc5660ed52e8e458a"
 
   url "https://assets.eu.apidog.com/download/#{version}/Apidog%20Europe#{arch}-#{version}.dmg"
   name "Apidog Europe"
