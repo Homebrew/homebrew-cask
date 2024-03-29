02b5151ac3971444616420ed62b93792499377ab
Danyal Aytekin <danyal@alienpaper.com>
2024-03-29 04:35:30 +0000

    treesheets `8473991279`, with source switched to github

--- Casks/t/treesheets.rb
+++ Casks/t/treesheets.rb
@@ -1,16 +1,25 @@
 cask "treesheets" do
-  version "2022.08.02,20220802020630"
-  sha256 :no_check
+  version "8473991279"
+  sha256 "34c56edb4d2eff97662fa3f4bb097f9e740e0360cacf15269cfd14726893f18f"
 
-  url "https://strlen.com/treesheets/treesheets_osx.zip"
+  url "https://github.com/aardappel/treesheets/releases/download/#{version}/mac_treesheets.zip",
+      verified: "github.com/aardappel/treesheets/"
   name "TreeSheets"
   desc "Hierarchical spreadsheet and outline application"
   homepage "https://strlen.com/treesheets/"
 
   livecheck do
     url :url
-    strategy :extract_plist
+    strategy :github_latest
   end
 
-  app "TreeSheets.app"
+  app "build/Build/Products/Release/TreeSheets.app"
+
+  uninstall quit: "dot3labs.TreeSheets"
+
+  zap trash: [
+    "~/Library/Preferences/dot3labs.TreeSheets.plist",
+    "~/Library/Preferences/TreeSheets Preferences",
+    "~/Library/Saved Application State/dot3labs.TreeSheets.savedState",
+  ]
 end
