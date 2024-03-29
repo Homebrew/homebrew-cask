73fdbb2a2b7f521272e1af958cecb3009ecadf25
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-03-29 11:15:42 -0400

    affine 0.13.4

--- Casks/a/affine.rb
+++ Casks/a/affine.rb
@@ -1,9 +1,9 @@
 cask "affine" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.13.3"
-  sha256 arm:   "c05084b8595bcadd8ae47da844d39d4e93ee4456a617e2ddba81d66f0dff0070",
-         intel: "60dabbcf82eef4d6fdf3962fa74956a0a4dca05302539fa6ca4a6d715abbb541"
+  version "0.13.4"
+  sha256 arm:   "1eb646b5dab662809fcda53ae11e4f3f331e3fd6ed78ee4a789a3ea751420214",
+         intel: "ff36883da1ab37410d46274dce4f70db8b98c90ae760c992c660eb26faae3616"
 
   url "https://github.com/toeverything/AFFiNE/releases/download/v#{version}/affine-#{version}-stable-macos-#{arch}.zip",
       verified: "github.com/toeverything/AFFiNE/"
