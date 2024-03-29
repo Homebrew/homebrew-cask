64658d719ed425ad04293eeb2fc5b0ac8f2e2dc4
Roman Ternavskyi <rternavskyi@gmail.com>
2024-03-30 01:50:40 +0200

    Add ClearVPN cask

---
+++ Casks/c/clearvpn.rb
@@ -0,0 +1,29 @@
+cask "clearvpn" do
+  version "3.0.0"
+  sha256 "10b009d6f8d0ff79edec6545a751716000bb42a6c4084c03287d063b1e82d810"
+
+  url "https://dl.devmate.com/com.macpaw.clearvpn.macos-site-ver/ClearVPN.dmg",
+      verified: "dl.devmate.com/"
+  name "ClearVPN"
+  desc "ClearVPN is a hassle-free VPN app for secure online journeys"
+  homepage "https://clearvpn.com"
+
+  livecheck do
+    url "https://updates.devmate.com/com.macpaw.clearvpn.macos-site-ver.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "ClearVPN.app"
+
+  zap trash: [
+    "~/Library/Caches/com.macpaw.clearvpn.macos-site-ver",
+    "~/Library/Group Containers/S8EX82NJP6.com.macpaw.clearvpn.macos-site-ver",
+    "~/Library/HTTPStorages/com.macpaw.clearvpn.macos-site-ver",
+    "~/Library/HTTPStorages/com.macpaw.clearvpn.macos-site-ver.binarycookies",
+    "~/Library/Preferences/com.macpaw.clearvpn.macos-site-ver.plist",
+    "~/Library/WebKit/com.macpaw.clearvpn.macos-site-ver",
+  ]
+end
