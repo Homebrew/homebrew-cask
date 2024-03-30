34a333b84520ff30fcc98c5326996397e842af0c
Daeho Ro <lamanus@outlook.kr>
2024-03-30 20:35:36 +0900

    spacedrive 0.2.10

--- Casks/s/spacedrive.rb
+++ Casks/s/spacedrive.rb
@@ -1,9 +1,9 @@
 cask "spacedrive" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.2.9"
-  sha256 arm:   "d45752b71eff607cd7ab7f1fdb78c473de7e7de8b66e2c4970d822a862fe5832",
-         intel: "3b83debe5e073cdb0c75cbf6da4723dd8bd00fe1623a2ae23d231c11bcaf91bb"
+  version "0.2.10"
+  sha256 arm:   "fd4744ea59637717f77cb85a0ba09539e81b99225367dee3f6dd8c4d7a7f0de1",
+         intel: "b72fc6d912a7c8f9754294e9dbae60df4ad76b23b0ab48a76c2f7aeb17e6f979"
 
   url "https://github.com/spacedriveapp/spacedrive/releases/download/#{version}/Spacedrive-darwin-#{arch}.dmg"
   name "Spacedrive"
