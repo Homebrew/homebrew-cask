5b2d7be7cf607ec6d58c092e4bf57c57a16e11e2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-01 21:30:07 +0000

    copilot 4.0.7

--- Casks/c/copilot.rb
+++ Casks/c/copilot.rb
@@ -1,6 +1,6 @@
 cask "copilot" do
-  version "4.0.6,197-28d830ea"
-  sha256 "28d830eadea5821362ff5c8dc8c273cc86d361b875a3dea6acfb12d61ae9e9e8"
+  version "4.0.7,198-75dff20b"
+  sha256 "75dff20bfc215bcaeb0ec3efa6f3a5a162e4a333a392659811e98714d7a6314a"
 
   url "https://storage.googleapis.com/copilot-mac-releases/images/Copilot-#{version.csv.first}-#{version.csv.second}.dmg",
       verified: "storage.googleapis.com/copilot-mac-releases/"
