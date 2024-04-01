11837eec5b09f2ae8b09f8fcfe2a2964a90e6785
Yusuf Kursad Kaya <ykursadkaya@gmail.com>
2024-04-01 19:19:21 +0300

    cloudflare-warp 2024.3.407.0

--- Casks/c/cloudflare-warp.rb
+++ Casks/c/cloudflare-warp.rb
@@ -1,5 +1,5 @@
 cask "cloudflare-warp" do
-  version "2024.2.68.0,20240214.9"
+  version "2024.3.407.0,20240329.17"
   sha256 :no_check
 
   url "https://1111-releases.cloudflareclient.com/mac/Cloudflare_WARP.zip",
