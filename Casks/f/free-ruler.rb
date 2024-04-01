07de95bbe630973c3db93e827ba6ce8bf03eabc4
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-04-01 10:57:49 -0400

    free-ruler 2.0.7

--- Casks/f/free-ruler.rb
+++ Casks/f/free-ruler.rb
@@ -1,6 +1,6 @@
 cask "free-ruler" do
-  version "2.0.6"
-  sha256 "ee762183261e58b8b121a09cd282bc63f2b8664f0d1e31d24be2c623c99d01a8"
+  version "2.0.7"
+  sha256 "cd8890d2c59da41acd45a5cf16c318a21419eb604fa049fc0a55fb151fe26420"
 
   url "https://github.com/pascalpp/FreeRuler/releases/download/v#{version}/free-ruler-#{version}.zip",
       verified: "github.com/pascalpp/FreeRuler/"
