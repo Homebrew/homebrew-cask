34494d4473b267e2506024413e635d3804a67df8
Eric Knibbe <enk3@outlook.com>
2024-04-02 18:24:01 -0400

    openrct2 0.4.10

--- Casks/o/openrct2.rb
+++ Casks/o/openrct2.rb
@@ -15,8 +15,8 @@ cask "openrct2" do
         verified: "github.com/OpenRCT2/OpenRCT2/"
   end
   on_mojave :or_newer do
-    version "0.4.9"
-    sha256 "32ae6aaa69cd4ccff9ea97b0e728df40e6a8690114d0884f1ef1a899372927dd"
+    version "0.4.10"
+    sha256 "3678347ec3b46d91424c4339358e71ce01594a9ebf1f976d323566f9e55ffa20"
 
     url "https://github.com/OpenRCT2/OpenRCT2/releases/download/v#{version}/OpenRCT2-#{version}-macos-universal.zip",
         verified: "github.com/OpenRCT2/OpenRCT2/"
