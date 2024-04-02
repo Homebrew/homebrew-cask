60eeb32fe9bc181e1bbde82365c82bb12cc6b498
Alan Kwan <akwan@users.noreply.github.com>
2024-04-02 16:47:23 -0700

    p4 update checksum 2023.2,2563409

--- Casks/p/p4.rb
+++ Casks/p/p4.rb
@@ -2,9 +2,11 @@ cask "p4" do
   # NOTE: "4" is not a version number, but an intrinsic part of the product name
   arch arm: "12arm64", intel: "1015x86_64"
 
-  version "2023.2,2563409"
-  sha256 arm:   "c825bb42d24312184b7a5f937d78e9b549392b10ecf0248375a2bc7aa5486e3b",
-         intel: "818d345df408b6a01693c0a501d80cba4473d90719e6910ccaa08380b6c13dd6"
+  version "2023.2,2578891"
+  # arm:   https://filehost.perforce.com/perforce/r23.2/bin.macosx12arm64/SHA256SUMS
+  # intel: https://filehost.perforce.com/perforce/r23.2/bin.macosx1015x86_64/SHA256SUMS
+  sha256 arm:   "120fcd7e2756a5ef31e3ae0eb69f88ac5e451f73e36429376466cf3e86d1b33c",
+         intel: "3d994f626cef4a599e98da4383f37b06d205d5420b90188a307fe0de189b0ae8"
 
   url "https://filehost.perforce.com/perforce/r#{version.major[-2..]}.#{version.minor}/bin.macosx#{arch}/p4"
   name "Perforce Helix Command-Line Client (P4)"
