503ec85d84839039c8ff3944b524dcd29367ff9b
Miccal Matthews <miccal.matthews@gmail.com>
2024-04-02 20:55:31 +0800

    Update firefox.rb

--- Casks/f/firefox.rb
+++ Casks/f/firefox.rb
@@ -219,9 +219,9 @@ cask "firefox" do
 
   auto_updates true
   conflicts_with cask: [
-    "homebrew/cask-versions/firefox-beta",
-    "homebrew/cask-versions/firefox-cn",
-    "homebrew/cask-versions/firefox-esr",
+    "firefox-beta",
+    "firefox-cn",
+    "firefox-esr",
   ]
   depends_on macos: ">= :catalina"
 
