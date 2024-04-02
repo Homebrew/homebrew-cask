48bd69b8df5d970dc4774fb55d3f8aed5afe1298
Bevan Kay <email@bevankay.me>
2024-04-02 22:09:51 +1100

    binary-ninja 4.0.4958

--- Casks/b/binary-ninja.rb
+++ Casks/b/binary-ninja.rb
@@ -1,15 +1,19 @@
 cask "binary-ninja" do
-  version "3.5.4542"
+  version "4.0.4958"
   sha256 :no_check
 
-  url "https://cdn.binary.ninja/installers/BinaryNinja-demo.dmg"
+  url "https://cdn.binary.ninja/installers/binaryninja_free_macosx.dmg"
   name "Binary Ninja"
   desc "Reverse engineering platform"
   homepage "https://binary.ninja/"
 
   livecheck do
     url :url
-    strategy :extract_plist
+    regex(/(\d+(?:\.\d+)+)/)
+    strategy :extract_plist do |items|
+      puts items["com.vector35.binaryninja"].short_version
+      items["com.vector35.binaryninja"].short_version[regex, 1]
+    end
   end
 
   app "Binary Ninja.app"
