9cf544c22654087b846555b80a61f87787c3b668
Alan Kwan <akwan@users.noreply.github.com>
2024-04-03 09:48:56 -0700

    Disabling sha256 check on p4

--- Casks/p/p4.rb
+++ Casks/p/p4.rb
@@ -3,10 +3,7 @@ cask "p4" do
   arch arm: "12arm64", intel: "1015x86_64"
 
   version "2023.2,2578891"
-  # arm:   https://filehost.perforce.com/perforce/r23.2/bin.macosx12arm64/SHA256SUMS
-  # intel: https://filehost.perforce.com/perforce/r23.2/bin.macosx1015x86_64/SHA256SUMS
-  sha256 arm:   "120fcd7e2756a5ef31e3ae0eb69f88ac5e451f73e36429376466cf3e86d1b33c",
-         intel: "3d994f626cef4a599e98da4383f37b06d205d5420b90188a307fe0de189b0ae8"
+  sha256 :no_check
 
   url "https://filehost.perforce.com/perforce/r#{version.major[-2..]}.#{version.minor}/bin.macosx#{arch}/p4"
   name "Perforce Helix Command-Line Client (P4)"
