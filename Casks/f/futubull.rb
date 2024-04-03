b0c8bcb04aed184050eb6f0d6d608430f776fea0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-03 06:37:21 +0000

    futubull 14.6.6418

--- Casks/f/futubull.rb
+++ Casks/f/futubull.rb
@@ -1,6 +1,6 @@
 cask "futubull" do
-  version "14.6.6408"
-  sha256 "288a0aa3a08f38adfb0ae23b4ddd72535852fe08752e00af272ae2163eca7fdb"
+  version "14.6.6418"
+  sha256 "24bfa49a36b03a0cec1a1d3813ee12a46aafbb5b7398c1a58b38cf73450611c6"
 
   url "https://softwarefile.futunn.com/FTNNForMac_#{version}_Website.dmg"
   name "Futubull"
