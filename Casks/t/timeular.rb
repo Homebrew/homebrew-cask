85964f9b8a2058ad1d1a3bb2b0b9c1d39f425db8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-04 15:41:57 +0000

    timeular 6.7.6

--- Casks/t/timeular.rb
+++ Casks/t/timeular.rb
@@ -1,6 +1,6 @@
 cask "timeular" do
-  version "6.7.5"
-  sha256 "59d2fac07debe0fb87a1c55732f2614acc807353bc8a04cdd27ca354695f35ce"
+  version "6.7.6"
+  sha256 "107785e3ec608edc401d12fd5f2a21f8601b2f2b4bf907e65351e869fe1f1a98"
 
   url "https://timeular-desktop-packages.s3.amazonaws.com/mac/production/Timeular-#{version}.dmg",
       verified: "timeular-desktop-packages.s3.amazonaws.com/"
