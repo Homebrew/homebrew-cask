3662104fa451618a5535db8b08a72cb21257b405
adithyasunil26 <adithyasunil26@gmail.com>
2024-04-04 20:40:30 +0530

    simply-fortran 3.34.4166

--- Casks/s/simply-fortran.rb
+++ Casks/s/simply-fortran.rb
@@ -2,7 +2,7 @@ cask "simply-fortran" do
   arch arm: "-arm64", intel: "-x86_64"
 
   on_big_sur :or_older do
-    version "3.34.4154"
+    version "3.34.4166"
     sha256 "676f096f8ee509252023bcd3eafd99897bf079e87ebd3ffd2f41dfad6b45f273"
 
     url "https://download.simplyfortran.com/#{version.major_minor}/macos/simplyfortran-#{version}-x86_64.legacy.dmg"
@@ -13,12 +13,12 @@ cask "simply-fortran" do
   end
   on_monterey :or_newer do
     on_arm do
-      version "3.32.4016"
-      sha256 "11d513fc26c367869e678e3a46f5ec80bae8e55747c1331c46c022deb388937e"
+      version "3.34.4166"
+      sha256 "3a94937f608ccac9c63ee600952f1f821309a895d3f85e391f404d52a734b37c"
     end
     on_intel do
-      version "3.34.4154"
-      sha256 "ee48deb9308b68df1ff65d86d6f1df37d5c46bdd40f8ccd1ebe27677bc9b2a27"
+      version "3.34.4166"
+      sha256 "5f51dc383b9c22f5564e31f119eed40ef2005a2490173a9f5895528c1c3a20a4"
     end
 
     url "https://download.simplyfortran.com/#{version.major_minor}/macos/simplyfortran-#{version}#{arch}.dmg"
