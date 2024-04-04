5a32dd8f001b16511616a105c6f2d0612b49ee5e
adithyasunil26 <adithyasunil26@gmail.com>
2024-04-04 20:47:35 +0530

    Fix version and sha mismatches

--- Casks/s/simply-fortran.rb
+++ Casks/s/simply-fortran.rb
@@ -3,7 +3,7 @@ cask "simply-fortran" do
 
   on_big_sur :or_older do
     version "3.34.4166"
-    sha256 "676f096f8ee509252023bcd3eafd99897bf079e87ebd3ffd2f41dfad6b45f273"
+    sha256 "d623e96c544c173978a28fa60702a2ee0f42ea3344932139e4528df03f0bc7d7"
 
     url "https://download.simplyfortran.com/#{version.major_minor}/macos/simplyfortran-#{version}-x86_64.legacy.dmg"
 
@@ -13,8 +13,8 @@ cask "simply-fortran" do
   end
   on_monterey :or_newer do
     on_arm do
-      version "3.34.4166"
-      sha256 "3a94937f608ccac9c63ee600952f1f821309a895d3f85e391f404d52a734b37c"
+      version "3.32.4016"
+      sha256 "11d513fc26c367869e678e3a46f5ec80bae8e55747c1331c46c022deb388937e"
     end
     on_intel do
       version "3.34.4166"
