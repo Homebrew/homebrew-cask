155c0438522722c9645d563f3eee8166a4b72b52
Mikaela Nöteberg <mikaela.noteberg@nordnet.se>
2024-04-04 09:48:41 +0200

    corretto 22.0.0.36.2

--- Casks/c/corretto.rb
+++ Casks/c/corretto.rb
@@ -1,9 +1,9 @@
 cask "corretto" do
   arch arm: "aarch64", intel: "x64"
 
-  version "21.0.2.13.1"
-  sha256 arm:   "7767e44e1d2d387bd94bc5e2053ab32f0c4e340cb651ed923ff97b737ce31e01",
-         intel: "d4848f84ce21a8db91aea58681ad34346fd7cac7fc06eefab22c374bed7c8c2e"
+  version "22.0.0.36.2"
+  sha256 arm:   "7310e5c440f555cd3ff589fcfff0b1a1cebd6cf58b7694de77b44e3314a99fc6",
+         intel: "73a2d3271ee04a69e383322f92a642f23e52a5eadeb7b029bd1299f326a2fcde"
 
   url "https://corretto.aws/downloads/resources/#{version.sub(/-\d+/, "")}/amazon-corretto-#{version}-macosx-#{arch}.pkg"
   name "AWS Corretto JDK"
