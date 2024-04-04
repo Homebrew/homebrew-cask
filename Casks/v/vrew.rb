73e8922fa743500f7f2ec368d6ab999e63ad780a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-04 06:43:25 +0000

    vrew 1.17.0

--- Casks/v/vrew.rb
+++ Casks/v/vrew.rb
@@ -1,6 +1,6 @@
 cask "vrew" do
-  version "1.16.1"
-  sha256 "0fa942c856b996988e02a424f45a7072f10f105f051b0c4d337f75e5ee34cff2"
+  version "1.17.0"
+  sha256 "a220bc1bda1012841c5264dee5c629a515c6693c527ad11b8e5bc3edd7a47e0d"
 
   url "https://vrew-files.voyagerx.com/Vrew-#{version}.dmg"
   name "Vrew"
