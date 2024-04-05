37f6e17ea68879a7a97a346ff571c0e30ec4be7f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-05 03:30:46 +0000

    confluent-cli 3.56.0

--- Casks/c/confluent-cli.rb
+++ Casks/c/confluent-cli.rb
@@ -1,9 +1,9 @@
 cask "confluent-cli" do
   arch arm: "arm64", intel: "amd64"
 
-  version "3.55.0"
-  sha256 arm:   "9c033d3c10d3c1578235a06c55ab72f01557ce8c55a7d20c7997688f3343c7e6",
-         intel: "2cbaa9843af71fc1587f593633b71fd95cd42443af843b9ad4668c10bf9dc59d"
+  version "3.56.0"
+  sha256 arm:   "63bb9b7525fd7ba7315461a5176a06caae09586f91f20ddc92b7221c3a14a520",
+         intel: "37426dd3b0c19c62199a6767a21d02e7e8a370d9adda532c7db2748dacc782f2"
 
   url "https://s3-us-west-2.amazonaws.com/confluent.cloud/confluent-cli/archives/#{version}/confluent_#{version}_darwin_#{arch}.tar.gz",
       verified: "s3-us-west-2.amazonaws.com/confluent.cloud/confluent-cli/archives/"
