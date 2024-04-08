204a9f4364c4bcb710072ee79b2d940e1f0f4ffc
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-04-08 09:42:27 -0400

    box-tools 4.25.0.1046

--- Casks/b/box-tools.rb
+++ Casks/b/box-tools.rb
@@ -1,5 +1,5 @@
 cask "box-tools" do
-  version "4.24.0.1029"
+  version "4.25.0.1046"
   sha256 :no_check
 
   url "https://e3.boxcdn.net/box-installers/boxedit/mac/currentrelease/BoxToolsInstaller.dmg",
