58b1b85d9a1724dbeafb30c2e042d34a82781f80
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-04-07 21:30:51 -0400

    go-shiori 1.6.2

--- Casks/g/go-shiori.rb
+++ Casks/g/go-shiori.rb
@@ -1,9 +1,9 @@
 cask "go-shiori" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.6.1"
-  sha256 arm:   "0700771e6ded0c80302acf6ce04e9deb2cf9c04c4ae85419989cab9eaabf911a",
-         intel: "a1eea9b2d1fbd04618529c790e23fe81a5852ed7d704a029033ea43d63ac2ad1"
+  version "1.6.2"
+  sha256 arm:   "2932f36954c0977a557d0d60c09604a543166c8f4ae9e9f897fd1d23fa662326",
+         intel: "d0acaefd09231427d436b77ae9a8be2008c909989d9199c649c1159f91026567"
 
   url "https://github.com/go-shiori/shiori/releases/download/v#{version}/shiori_Darwin_#{arch}.tar.gz"
   name "Shiori"
