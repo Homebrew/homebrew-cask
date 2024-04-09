c76f7ef178df4830419c26bc172895a2362c9656
Daeho Ro <lamanus@outlook.kr>
2024-04-10 07:32:56 +0900

    android-studio 2023.2.1.25

--- Casks/a/android-studio.rb
+++ Casks/a/android-studio.rb
@@ -1,9 +1,9 @@
 cask "android-studio" do
   arch arm: "mac_arm", intel: "mac"
 
-  version "2023.2.1.24"
-  sha256 arm:   "985f39d146b6534f256694dec90a158a412a39b0da53f58bc135559703f7a830",
-         intel: "1b7e1d5da8d88dadff7276d775372fa25e97a2bda2682fff7494f7ef46a93390"
+  version "2023.2.1.25"
+  sha256 arm:   "bc55ca35f72e6dcdccff617e6794e9fa4cebc28feff9fa612430d3fd01f95a3a",
+         intel: "f16ef3483b341ef387c60d6d5a58618e190543256ddde4526c0dc730034a07df"
 
   url "https://redirector.gvt1.com/edgedl/android/studio/install/#{version}/android-studio-#{version}-#{arch}.dmg",
       verified: "redirector.gvt1.com/edgedl/android/studio/"
