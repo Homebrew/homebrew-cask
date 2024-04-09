9598e1d45d46825f994a664cfb896db098bb076c
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-04-09 19:52:25 -0400

    lyn 2.3.7
    * Update to version 2.3.7
    
    * Update livecheck to return current version, as Sparkle was not working correctly
    
    * Update `depends_on`
    
    * Update `zap`
    
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/l/lyn.rb
+++ Casks/l/lyn.rb
@@ -1,6 +1,6 @@
 cask "lyn" do
-  version "2.1.1"
-  sha256 "62f70642e55e6c1ee7ea68c89fbe71114331861bdecfca2a5c95d30342886153"
+  version "2.3.7"
+  sha256 "8fcf3fb201ed5fbe4f01e0a12744efcda08267b398c43efc1142882af5d1d172"
 
   url "https://www.lynapp.com/downloads/Lyn-#{version}.dmg"
   name "Lyn"
@@ -8,17 +8,21 @@ cask "lyn" do
   homepage "https://www.lynapp.com/"
 
   livecheck do
-    url "https://www.lynapp.com/lyn/update#{version.major}x.xml"
-    strategy :sparkle
+    url :homepage
+    regex(%r{href=.*?/Lyn[-_.]?v?(\d+(?:\.\d+)+)\.dmg}i)
   end
 
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :high_sierra"
 
   app "Lyn.app"
 
   zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.lynapp.lyn.sfl*",
     "~/Library/Application Support/Lyn",
     "~/Library/Caches/com.lynapp.lyn",
+    "~/Library/HTTPStorages/com.lynapp.lyn",
     "~/Library/Preferences/com.lynapp.lyn.plist",
+    "~/Library/Saved Application State/com.lynapp.lyn.savedState",
+    "~/Library/WebKit/com.lynapp.lyn",
   ]
 end
