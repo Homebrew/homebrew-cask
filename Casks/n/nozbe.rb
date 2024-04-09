72be390cef59253d48d15bd6a6f6491f1fdb3855
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-09 15:41:18 +0000

    nozbe 2024.08

--- Casks/n/nozbe.rb
+++ Casks/n/nozbe.rb
@@ -1,6 +1,6 @@
 cask "nozbe" do
-  version "2024.07"
-  sha256 "26ab828d9fc1abd41f91718e9e93c2574833f6d50d447f6d1368be86d3857a62"
+  version "2024.08"
+  sha256 "5ca762fe9ffd980be5f8cafdc7f56b6f9f8ae1280a929a296f46f017e7465239"
 
   url "https://builds.nozbe.com/mac/#{version}/Nozbe.app.zip"
   name "Nozbe"
