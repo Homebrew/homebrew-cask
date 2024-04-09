10de54baab9566c1b2384ef1e8a21d3f75ba1aab
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-04-09 10:07:40 -0400

    sentinel 0.25.0

--- Casks/s/sentinel.rb
+++ Casks/s/sentinel.rb
@@ -1,9 +1,9 @@
 cask "sentinel" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.24.4"
-  sha256 arm:   "d5b46b32f03d6d7e95911502233d0e2721e876e4564f5e912c8c1f93eb510083",
-         intel: "880c8b7a21aec46715980fe5ee4c47283412696b7e1a6c99fcc91cebb8057e78"
+  version "0.25.0"
+  sha256 arm:   "a8d787cd53a32aeb59e70f7a165f239304f1a18b7426fdf34a9fa9cc5f2ed362",
+         intel: "e43b3ce562e060565ee92dc8445a932728022a261aebe16d3af2399845048409"
 
   url "https://releases.hashicorp.com/sentinel/#{version}/sentinel_#{version}_darwin_#{arch}.zip"
   name "Sentinel"
