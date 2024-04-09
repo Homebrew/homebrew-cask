9ac725ed08227f2132d2c2dbff5de3b777910e45
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-09 18:35:01 +0000

    logos 33.0.0.0363

--- Casks/l/logos.rb
+++ Casks/l/logos.rb
@@ -1,9 +1,9 @@
 cask "logos" do
   arch arm: "-arm"
 
-  version "32.1.0.0031"
-  sha256 arm:   "fcbd23087345a8568ad7dd95fb75ef4f430d1157d8c3ab740cb1fd9b2803e208",
-         intel: "86a13f627050361ff1784c6dd3fd084f7d0b43a3484f3eb4de9c31b7c19894dc"
+  version "33.0.0.0363"
+  sha256 arm:   "f91260276f73733817bd1e26d976eda0354afdab32bdcd5c5bf09113042b0bc9",
+         intel: "d133a895e87aa73d946ece3075a73c3d492830fbbbdc797345ae2efe00ebd832"
 
   url "https://downloads.logoscdn.com/LBS10/Installer/#{version}/LogosMac#{arch}.dmg",
       verified: "downloads.logoscdn.com/"
