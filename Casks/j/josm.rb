1c52d62a803b40f60286cf8dc6a3100e306915f3
Daeho Ro <lamanus@outlook.kr>
2024-04-11 00:06:44 +0900

    josm 19039

--- Casks/j/josm.rb
+++ Casks/j/josm.rb
@@ -1,6 +1,6 @@
 cask "josm" do
-  version "19017"
-  sha256 "418bc00be5bb8070b9ba7c1f71b434b33e54d5590cc082149035902bdbc37b60"
+  version "19039"
+  sha256 "57cf9452ee257f713e0d1e47e911e6e877b4306f4232f9ee629fa6794dfe014a"
 
   url "https://github.com/JOSM/josm/releases/download/#{version}-tested/JOSM-macOS-java17-#{version}.zip",
       verified: "github.com/JOSM/josm/"
