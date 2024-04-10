85a9b70cdc3ed3cd3d2e1e544decff0d3eaf3f66
Daeho Ro <lamanus@outlook.kr>
2024-04-11 00:32:54 +0900

    reqable 2.11.1

--- Casks/r/reqable.rb
+++ Casks/r/reqable.rb
@@ -1,9 +1,9 @@
 cask "reqable" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "2.10.0"
-  sha256 arm:   "82e65c826091e947d1a041fad3d2c9982b9048f4ccbb17a1814f918a0cae010d",
-         intel: "1e354eb1f08605c3199dd06d692e1071137cc2fd6704c956e8cfcdcf7fc3fbeb"
+  version "2.11.1"
+  sha256 arm:   "298f4fc8db2c6029f8ea71923da757e62e70d334ba672ffe85b7d42187c19c6f",
+         intel: "be663d0aa0138b5378856ce6faff5b2fd92eb418b98a09ccc8208a59d0fb3a52"
 
   url "https://github.com/reqable/reqable-app/releases/download/#{version}/reqable-app-macos-#{arch}.dmg",
       verified: "github.com/reqable/reqable-app/"
