4c31e91460199ec9b7cde838b68b83a9e710e73c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-11 18:31:15 +0000

    brave-browser 1.64.122.0

--- Casks/b/brave-browser.rb
+++ Casks/b/brave-browser.rb
@@ -2,9 +2,9 @@ cask "brave-browser" do
   arch arm: "arm64", intel: "x64"
   folder = on_arch_conditional arm: "stable-arm64", intel: "stable"
 
-  version "1.64.116.0"
-  sha256 arm:   "905476e48fa2501d98f2aff624e36c0baa97396cd7562b52220b31edd67d3402",
-         intel: "b7d24c13f63e203b89c19e6403eff885bb945ae29ae84981956d0d984d19a05d"
+  version "1.64.122.0"
+  sha256 arm:   "e5f3bd78c440d4faa6b20f317b42589b82d484d2ea127919959c5a520a290e2f",
+         intel: "cb71a70e02e1d5f158d4e094cce54b1e1ae7e7d8d8edbc7d2796054e2b77377d"
 
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.major_minor_patch.sub(".", "")}/Brave-Browser-#{arch}.dmg",
       verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
