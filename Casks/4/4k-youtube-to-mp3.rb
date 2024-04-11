cb87fc5a9529848c38dc2a5f13eade1f33464155
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-04-11 10:50:38 -0400

    4k-youtube-to-mp3 5.2.2

--- Casks/4/4k-youtube-to-mp3.rb
+++ Casks/4/4k-youtube-to-mp3.rb
@@ -2,9 +2,9 @@ cask "4k-youtube-to-mp3" do
   arch arm: "arm64", intel: "x64"
 
   # NOTE: "3" is not a version number, but an intrinsic part of the product name
-  version "5.2.1"
-  sha256 arm:   "869f10c7e88f5878dd2ca4a3e0c2a6a27078b55bd57fb956899f2f774b1a1c5e",
-         intel: "d42bdd74dc9cd90feb45651747707f4023e03b1cac1041c12036c916d3050206"
+  version "5.2.2"
+  sha256 arm:   "ebc9ba40d907e21e7588062aa850558ef83d5790e50eeadb2d4814c28022a1cd",
+         intel: "7cc8ee01dc4b23dc0a994459b3a2dccc97677a0185086a946aeadb21ed0efcae"
 
   url "https://dl.4kdownload.com/app/4kyoutubetomp3_#{version}_#{arch}.dmg"
   name "4K YouTube to MP3"
