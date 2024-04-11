38197224f241fb600c073a218de89c1538f60a03
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-04-11 10:39:09 -0400

    submariner 3.1.1

--- Casks/s/submariner.rb
+++ Casks/s/submariner.rb
@@ -1,6 +1,6 @@
 cask "submariner" do
-  version "3.1"
-  sha256 "5d44638cca45f6d17c64af875bbf7c3ea224e442b556c7ed4d9f181a731d41c2"
+  version "3.1.1"
+  sha256 "1354aba4ccf2912f44b5402b3f2295631358e44777614dc416749d2464a1e03b"
 
   url "https://github.com/NattyNarwhal/Submariner/releases/download/v#{version}/Submariner-#{version}.zip",
       verified: "github.com/NattyNarwhal/Submariner/"
