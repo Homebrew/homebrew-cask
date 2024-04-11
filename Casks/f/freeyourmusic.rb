2b83637e7581e94708460f950cf61a56cc791524
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-04-11 15:35:45 -0400

    freeyourmusic: updates
    * Update for ARM and Intel versions
    
    * Add `auto_updates true`
    
    * Add minimum macOS of Catalina
    
    * Update `zap`
    
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/f/freeyourmusic.rb
+++ Casks/f/freeyourmusic.rb
@@ -1,8 +1,11 @@
 cask "freeyourmusic" do
-  version "8.7.0"
-  sha256 "e2e9bc7064cc5c25462325f03d9e16dc0a8ad1a2889bc1021308e885c15b1312"
+  arch intel: "-x64"
 
-  url "https://fym-app-production.s3.nl-ams.scw.cloud/FreeYourMusic-#{version}.dmg",
+  version "8.7.0"
+  sha256 arm:   "e2e9bc7064cc5c25462325f03d9e16dc0a8ad1a2889bc1021308e885c15b1312",
+         intel: "03f4168f274387d58519eee84251de4d823074b0c395b0411e75c4fe64cfbb11"
+
+  url "https://fym-app-production.s3.nl-ams.scw.cloud/FreeYourMusic-#{version}#{arch}.dmg",
       verified: "fym-app-production.s3.nl-ams.scw.cloud/"
   name "FreeYourMusic"
   desc "Move playlists, tracks, and albums between music platforms"
@@ -13,11 +16,19 @@ cask "freeyourmusic" do
     strategy :electron_builder
   end
 
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
   app "FreeYourMusic.app"
 
   zap trash: [
+    "~/Library/Application Support/Caches/freeyourmusic-updater",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.freeyourmusic.app.sfl*",
     "~/Library/Application Support/FreeYourMusic",
+    "~/Library/Caches/com.freeyourmusic.app",
+    "~/Library/HTTPStorages/com.freeyourmusic.app",
     "~/Library/Logs/FreeYourMusic",
+    "~/Library/Preferences/ByHost/com.freeyourmusic.app.ShipIt.*.plist",
     "~/Library/Preferences/com.freeyourmusic.app.plist",
     "~/Library/Saved Application State/com.freeyourmusic.app.savedState",
   ]
