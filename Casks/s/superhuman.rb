b26f2dc4c2a860de1ade707ae6375d149bcd69bb
Miccal Matthews <miccal.matthews@gmail.com>
2024-04-12 09:02:13 +0800

    Update superhuman.rb

--- Casks/s/superhuman.rb
+++ Casks/s/superhuman.rb
@@ -6,7 +6,7 @@ cask "superhuman" do
          intel: "c46e25971988c06c72602f5d60c834720bcc5ea4efc1295dd9231edf3c9f780d"
 
   url "https://storage.googleapis.com/download.superhuman.com/supertron-update/Superhuman-#{version}#{arch}-latest-mac.zip",
-      verified: "storage.googleapis.com/download.superhuman.com/"
+      verified: "storage.googleapis.com/download.superhuman.com/supertron-update/"
   name "Superhuman"
   desc "Email client"
   homepage "https://superhuman.com/"
@@ -22,13 +22,10 @@ cask "superhuman" do
 
   zap trash: [
     "~/Library/Application Support/Superhuman",
-    "~/Library/Caches/com.superhuman.electron",
-    "~/Library/Caches/com.superhuman.electron.helper",
-    "~/Library/Caches/com.superhuman.electron.ShipIt",
+    "~/Library/Caches/com.superhuman.electron*",
     "~/Library/Caches/Superhuman",
     "~/Library/Logs/Superhuman",
-    "~/Library/Preferences/com.superhuman.electron.helper.plist",
-    "~/Library/Preferences/com.superhuman.electron.plist",
+    "~/Library/Preferences/com.superhuman.electron.*",
     "~/Library/Preferences/com.superhuman.mail.plist",
     "~/Library/Saved Application State/com.superhuman.electron.savedState",
   ]
