a0fb0f12d929349694b0e416f02b37ed82992f5f
Daeho Ro <lamanus@outlook.kr>
2024-04-12 10:13:03 +0900

    josm: add arm

--- Casks/j/josm.rb
+++ Casks/j/josm.rb
@@ -1,8 +1,12 @@
 cask "josm" do
-  version "19039"
-  sha256 "c80f907fbe8c6e8ba22dd1fda35462f29b5b1bbd18668235de9040f4a8bad7fd"
+  arch arm: "aarch64", intel: "x64"
+  app_suffix = on_arch_conditional arm: "arm64", intel: "x86_64"
 
-  url "https://github.com/JOSM/josm/releases/download/#{version}-tested/JOSM-macOS-java21-#{version}.zip",
+  version "19039"
+  sha256 arm:   "92ff3d6635c50bca62aa2628647a2c8415f5686ee433609b2d8385559aa14384",
+         intel: "6d8bdf05291fad96293e5c0f1675eef273db54aa3184a50e8365bf23fab63215"
+
+  url "https://github.com/JOSM/josm/releases/download/#{version}-tested/JOSM-macOS-java21-#{version}-#{arch}.zip",
       verified: "github.com/JOSM/josm/"
   name "JOSM"
   desc "Extensible editor for OpenStreetMap"
@@ -14,7 +18,7 @@ cask "josm" do
     strategy :github_latest
   end
 
-  app "JOSM.app"
+  app "JOSM_21_#{app_suffix}.app"
 
   zap trash: [
     "~/Library/Caches/JOSM",
