07d5fe9bfbbb5314ee1242fb7a3c3e04dc718581
Lorenzo Antonelli <56717474+Lorenzoantonelli@users.noreply.github.com>
2024-04-13 17:04:22 +0200

    anki 24.04.1

--- Casks/a/anki.rb
+++ Casks/a/anki.rb
@@ -1,7 +1,7 @@
 cask "anki" do
   arch arm: "apple", intel: "intel"
 
-  version "24.04"
+  version "24.04.1"
 
   on_catalina :or_older do
     sha256 "24de21187d667d2831e4d09fe906e8c7ed97ed77d7d1db11f813a8abe7827966"
@@ -10,8 +10,8 @@ cask "anki" do
         verified: "github.com/ankitects/anki/"
   end
   on_big_sur :or_newer do
-    sha256 arm:   "6d1a4f54e5e90d1abe117c16d725a2020ce471d2e12e732b1e8fedf49833b61d",
-           intel: "69bf1c73e40cb77649a75359980c33d95359c1640ad0304f2a7cf67c6982ec5b"
+    sha256 arm:   "94c51f22f5786e268e6f40014d972204fe7d5d8d4b9f7b64c29862da8691bb87",
+           intel: "2e51db67c630d7b626d752f596a3226c0695f17581f2888c1464311353ef0292"
 
     url "https://github.com/ankitects/anki/releases/download/#{version}/anki-#{version}-mac-#{arch}-qt6.dmg",
         verified: "github.com/ankitects/anki/"
