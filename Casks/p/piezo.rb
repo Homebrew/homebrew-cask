ae6a0d62a22bd6eaf8cac51ea8ff9b95415dad5f
Sam Ford <1584702+samford@users.noreply.github.com>
2024-04-13 13:29:35 -0400

    piezo 1.9.1
    This updates `piezo` to the latest version, 1.9.1.
    
    This also updates the `livecheck` block to check the Support Page
    instead of the Sparkle feed. The Sparkle feed only returns the latest
    version when an appropriate `system` value is provided but we're
    using an old version that 1.9.1 doesn't support. Omitting the `system`
    value returns 1.2.2 instead of 1.9.1, so we can't do that. If we use
    a hardcoded `1441` value (for macOS 14.4.1), we will eventually end
    up in a situation again where livecheck will always return an older
    version until we update the `system` value, so that isn't a
    sustainable approach.
    
    Our options seem to be to check either the Support Center
    (https://rogueamoeba.com/support/knowledgebase/?showCategory=Piezo)
    or the Release Notes
    (https://rogueamoeba.com/support/knowledgebase/releasenotes/?product=Piezo)
    page. The Release Notes are more complete but the download size will
    grow with each new release, so I've opted for the Support Center page.
    This isn't ideal but it works and isn't tied to a specific macOS
    version.

--- Casks/p/piezo.rb
+++ Casks/p/piezo.rb
@@ -1,5 +1,5 @@
 cask "piezo" do
-  version "1.8.2"
+  version "1.9.1"
   sha256 :no_check
 
   url "https://rogueamoeba.com/piezo/download/Piezo.zip"
@@ -7,9 +7,15 @@ cask "piezo" do
   desc "Audio recording application"
   homepage "https://rogueamoeba.com/piezo/"
 
+  # The Sparkle feed contents varies based on the provided [macOS] `system`
+  # value (e.g., 1441 for 14.4.1) and we only receive an older version if we
+  # omit it. If we use a hardcoded `system` value, eventually the `livecheck`
+  # block can get stuck on an older version until the `system` value is updated
+  # to a newer macOS version. To avoid this, we simply check the versions listed
+  # on the related Support Center page.
   livecheck do
-    url "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&system=1231&bundleid=com.rogueamoeba.Piezo&platform=osx&version=#{version.no_dots}8000"
-    strategy :sparkle
+    url "https://rogueamoeba.com/support/knowledgebase/?showCategory=Piezo"
+    regex(/Piezo\s+v?(\d+(?:\.\d+)+)/im)
   end
 
   auto_updates true
