0d24c44038c241a000ca39a1dd80f4e92127e36e
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-04-15 10:32:55 -0400

    herd 1.6.0

--- Casks/h/herd.rb
+++ Casks/h/herd.rb
@@ -1,6 +1,6 @@
 cask "herd" do
-  version "1.5.0"
-  sha256 "3a31161f7b3c6cc757d96e02ae521ba51e45811294cf29d62dedb0c81e545406"
+  version "1.6.0"
+  sha256 "9f69a846445bd6c26b49e4833e9059df913db5094cb452a741d065206ce30996"
 
   url "https://download.herdphp.com/app_versions/Herd_#{version}.dmg",
       verified: "download.herdphp.com/app_versions/"
