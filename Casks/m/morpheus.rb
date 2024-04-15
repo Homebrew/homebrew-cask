10d6b8825d3f69ccec096dc2f825824c982a2c04
Diego Jahn <jahndiego@mailbox.org>
2024-04-16 00:55:39 +0200

    morpheus: change cask's deprecate disable reason

--- Casks/m/morpheus.rb
+++ Casks/m/morpheus.rb
@@ -8,7 +8,7 @@ cask "morpheus" do
   desc "Modelling environment for multi-cellular systems biology"
   homepage "https://morpheus.gitlab.io/"
 
-  deprecate! date: "2024-04-15", because: :no_longer_available
+  deprecate! date: "2024-04-15", because: :discontinued
 
   conflicts_with cask: "homebrew/cask-versions/morpheus-beta"
 
