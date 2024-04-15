16453aa3725eeb25ec444ec126f15455bfae6c54
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-04-15 10:34:36 -0400

    jan 0.4.11

--- Casks/j/jan.rb
+++ Casks/j/jan.rb
@@ -1,9 +1,9 @@
 cask "jan" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.4.10"
-  sha256 arm:   "0d5901d559d1d4cc6db03b162ae5c14e3f051c82e9a81af541320459615c8921",
-         intel: "89fbf913f36d45cd1348aba7600954e6550b3a9a52fd6fa96ba94f389b888467"
+  version "0.4.11"
+  sha256 arm:   "5b1b638a2f33ca95020d44da9bf8c4f46e0a205356282040c66d5e47bcfe5e04",
+         intel: "27b5a12cd452273cea8e3677c2a7bfe0561436385e8b6b2ba8746c0033b4c03a"
 
   url "https://github.com/janhq/jan/releases/download/v#{version}/jan-mac-#{arch}-#{version}.dmg",
       verified: "github.com/janhq/jan/"
