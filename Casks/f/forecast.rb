846c2a63434543eec4047d6422ca7e2dc2acef7c
adithyasunil26 <adithyasunil26@gmail.com>
2024-04-16 14:12:38 +0530

    forecast 0.9.5

--- Casks/f/forecast.rb
+++ Casks/f/forecast.rb
@@ -1,6 +1,6 @@
 cask "forecast" do
-  version "0.9.4,137"
-  sha256 "ed3dd19cd69199b7e5c7e29e8e4c1fbbe0b901e1b28f3d2b4a0c0cf2568baf7f"
+  version "0.9.5,138"
+  sha256 "a3c129a261aa9d54c41f3fe4f4910cda23e19e4e912afa1c08121c0d8dcf817c"
 
   url "https://d2uzvmey2c90kn.cloudfront.net/appcast_download/Forecast_#{version.csv.first}_#{version.csv.second}.zip",
       verified: "d2uzvmey2c90kn.cloudfront.net/appcast_download/"
