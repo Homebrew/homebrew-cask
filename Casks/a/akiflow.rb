85fda1132a202bfc7cecdb5cdb66d53a61399d33
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-04-16 10:16:15 -0400

    akiflow: update livecheck, url, version
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/a/akiflow.rb
+++ Casks/a/akiflow.rb
@@ -1,18 +1,24 @@
 cask "akiflow" do
-  version "2.41.107-9bd4a3ff"
+  version "2.41.107,9bd4a3ff"
   sha256 "f41815589336cb8ae97d25c27962df011c1c431817651beec459bc92cb4b3be2"
 
-  url "https://download.akiflow.com/builds/Akiflow-#{version}-universal.dmg"
+  url "https://download.akiflow.com/builds/Akiflow-#{version.csv.first}-#{version.csv.second}-universal.dmg"
   name "Akiflow"
-  desc "Time blocking platform to save 2 hours every day"
+  desc "Time blocking and productivity platform"
   homepage "https://akiflow.com/"
 
   livecheck do
-    url "https://download.akiflow.com/builds/download"
-    strategy :header_match 
+    url "https://akiflow.com/download/latest"
+    regex(/Akiflow[._-](\d+(?:\.\d+)+)[._-](\h+)[._-]universal\.dmg/i)
+    strategy :header_match do |headers, regex|
+      match = headers["location"]&.match(regex)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
   end
 
-  depends_on macos: ">= :high_sierra"
+  depends_on macos: ">= :catalina"
 
   app "Akiflow.app"
 
