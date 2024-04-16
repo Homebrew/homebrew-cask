b0c8847509f990b4081eebbe4556b3d43e83fec5
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-04-16 11:22:36 -0400

    popsql: updates
    * Add ARM version
    
    * Update URL to use versioned download
    
    * Add `auto_updates true`
    
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/p/popsql.rb
+++ Casks/p/popsql.rb
@@ -1,8 +1,12 @@
 cask "popsql" do
-  version "1.0.128"
-  sha256 :no_check
+  arch arm: "-arm64"
 
-  url "https://get.popsql.com/download/mac"
+  version "1.0.128"
+  sha256 arm:   "31dafb58a7f5ec8d21a713aaf346fc6d5f483e59f560065bdbc013cf8168e81c",
+         intel: "e897fdeca98f23b2b9478f56892c842a0c181bdac745822fe78c13568ab12bf9"
+
+  url "https://popsql-releases.s3.amazonaws.com/mac/PopSQL-#{version}#{arch}.dmg",
+      verified: "popsql-releases.s3.amazonaws.com/"
   name "PopSQL"
   desc "Collaborative SQL editor"
   homepage "https://popsql.com/"
@@ -12,10 +16,14 @@ cask "popsql" do
     strategy :electron_builder
   end
 
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
   app "PopSQL.app"
 
   zap trash: [
     "~/.popsql.json",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.electron.popsql.sfl*",
     "~/Library/Application Support/PopSQL",
     "~/Library/Preferences/org.electron.popsql.plist",
     "~/Library/Saved Application State/org.electron.popsql.savedState",
