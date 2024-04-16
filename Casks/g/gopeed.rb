ad4b5bd23b84064f67d17629244a86ae908ab8e1
liwei <liwei-8466@qq.com>
2024-04-16 14:51:04 +0800

    gopeed 1.5.6

---
+++ Casks/g/gopeed.rb
@@ -0,0 +1,24 @@
+cask "gopeed" do
+  version "1.5.6"
+  sha256 "58a22f680a84c72c1a9ba9728c2e17dd27dae171c24b5ceb94bc8dd20475232e"
+
+  url "https://github.com/GopeedLab/gopeed/releases/download/v#{version}/Gopeed-v#{version}-macos.dmg",
+  verified: "github.com/GopeedLab/gopeed/"
+  name "Gopeed"
+  desc "A modern download manager that supports all platforms. Built with Golang and Flutter."
+  homepage "https://gopeed.com/"
+
+  livecheck do
+    url origin.to_s
+    strategy :github_latest
+  end
+
+  auto_updates true
+
+  app "Gopeed.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.gopeed",
+    "~/Library/Containers/com.gopeed",
+  ]
+end
