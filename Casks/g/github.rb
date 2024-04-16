b6f3c22be023281bd0bf5fce1efa5e340a122aee
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-16 12:40:54 +0000

    github 3.3.14-b6c1402e

--- Casks/g/github.rb
+++ Casks/g/github.rb
@@ -2,9 +2,9 @@ cask "github" do
   arch arm: "arm64", intel: "x64"
   platform = on_arch_conditional arm: "darwin-arm64", intel: "darwin"
 
-  version "3.3.13-1b0804db"
-  sha256 arm:   "6ef292556a2e8db99ca62d703b49fad3308a6a3eef0df7fd63fa139fcb315891",
-         intel: "df85436557e7b3d709cc702b751f180f48655a3241cce6a864e55cf5161d9a7a"
+  version "3.3.14-b6c1402e"
+  sha256 arm:   "1195878e7d27b18d63706e4a8cf76ea3d471278142578b27b3680ab9eece81f2",
+         intel: "79dd7a600c1598cbf32603570aed72a57a3f52495bff30f795e401256bf4352b"
 
   url "https://desktop.githubusercontent.com/github-desktop/releases/#{version}/GitHubDesktop-#{arch}.zip",
       verified: "desktop.githubusercontent.com/github-desktop/"
