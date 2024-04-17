fa101031c4879e27d41f5b984ee1fd24f4d8e8af
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-17 15:34:48 +0000

    gitbutler 0.11.2

--- Casks/g/gitbutler.rb
+++ Casks/g/gitbutler.rb
@@ -2,9 +2,9 @@ cask "gitbutler" do
   arch arm: "aarch64", intel: "x86_64"
   arch_suffix = on_arch_conditional arm: "aarch64", intel: "x64"
 
-  version "0.11.1,814"
-  sha256 arm:   "135ea732dee0e0b7d7bbb20ce8b2ff1ee720fddf9f85a3ad872ae9960272f564",
-         intel: "1e3f015b3153cf8ff48f2bfccf28e595f78e2022ca99dba0c1ed510a9c26b4e8"
+  version "0.11.2,831"
+  sha256 arm:   "eeb50543f2beb0715df6da670933e090f3bc8338a9c7edc61384a4b5bbe0136f",
+         intel: "49a2a929a632d348733584c95c1e66c4c63e92db7c6db3d83799db3e52e5840b"
 
   url "https://releases.gitbutler.com/releases/release/#{version.csv.first}-#{version.csv.second}/macos/#{arch}/GitButler_#{version.csv.first}_#{arch_suffix}.dmg"
   name "GitButler"
