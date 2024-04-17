b553102e354e90eea1d238defb31e19fd42fc535
Bevan Kay <email@bevankay.me>
2024-04-18 09:21:10 +1000

    streammusic: fix style

--- Casks/s/streammusic.rb
+++ Casks/s/streammusic.rb
@@ -1,6 +1,7 @@
 cask "streammusic" do
   version "1.2.8"
   sha256 "318d33d842ae0db2fa7f4ef46a273085357d59e8f6bc17170db5671c819d3cd5"
+
   url "https://oss.aqzscn.cn/stream-music/versions/#{version}/%E9%9F%B3%E6%B5%81v#{version}.dmg"
   name "StreamMusic"
   desc "Music client compatible with self-hosted music services"
