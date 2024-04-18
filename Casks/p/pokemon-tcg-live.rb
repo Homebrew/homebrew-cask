679c2f93fc788c960bfa0cdf1b464d8adfb9e663
Bevan Kay <email@bevankay.me>
2024-04-18 15:48:08 +1000

    pokemon-tcg-live 1.13.0

--- Casks/p/pokemon-tcg-live.rb
+++ Casks/p/pokemon-tcg-live.rb
@@ -1,5 +1,5 @@
 cask "pokemon-tcg-live" do
-  version "1.12.0,293233"
+  version "1.13.0,303435"
   sha256 :no_check
 
   url "https://installer.studio-prod.pokemon.com/installer/PokemonTCGLiveInstaller_Mac.dmg"
