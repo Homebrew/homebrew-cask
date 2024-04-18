dd7b527e294022675d9c907cd127193306b00112
Bevan Kay <email@bevankay.me>
2024-04-18 15:46:15 +1000

    vellum 3.7

--- Casks/v/vellum.rb
+++ Casks/v/vellum.rb
@@ -1,6 +1,6 @@
 cask "vellum" do
-  version "3.6.4,36400"
-  sha256 "294b2787f07715d8cc44d4a851b9bd194a66fd0b0ac41721b3b07fcca8947d05"
+  version "3.7,37010"
+  sha256 "10be7763283f574f5396396ba60d6792d520793cdca0ee4b5c68f91619dc927b"
 
   url "https://180g.s3.amazonaws.com/downloads/Vellum-#{version.csv.second}.zip",
       verified: "180g.s3.amazonaws.com/downloads/"
