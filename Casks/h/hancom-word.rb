4941134c4548b8f71d03b7c5dfd38f45aaa307af
Lee Jongyoung <leejongyoung@icloud.com>
2024-04-19 15:54:38 +0900

    hancom-word 12.30.0.5688

--- Casks/h/hancom-word.rb
+++ Casks/h/hancom-word.rb
@@ -1,19 +1,19 @@
 cask "hancom-word" do
-  version "12.30.0,5688"
-  sha256 :no_check
+  version "12.30.0.5688"
+  sha256 "8caf71c07ae0ed3876b2ca1fd092ecaae0370d7b6b0826dcb7b1bba518567082"
 
-  url "https://cdn.hancom.com/pds/hnc/DOWN/HancomDocs/HwpMac_HancomDocs.pkg",
+  url "https://cdn.hancom.com/pds/hnc/DOWN/HancomOfficeHWPGeneral.pkg",
       referer: "https://hancom.com/mypage/myIndivPrdtList.do"
-  name "Hacom Word Processor 2022"
+  name "Hacom Word Processor 2024"
   desc "Word processor"
   homepage "https://office.hancom.com/"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url "https://cdn.hancom.com/upd/HwpMac2022/HancomOfficeHWPDocs/hupdate_mac.ini"
+    regex(/Hwp_mac\s*=\s*(\d+(?:\.\d+)+)/i)
   end
 
-  pkg "HwpMac_HancomDocs.pkg"
+  pkg "HancomOfficeHWPGeneral.pkg"
 
   uninstall quit:    "com.hancom.office.hwp#{version.major}.mac.general",
             pkgutil: "com.hancom.office.hwp#{version.major}.mac.general"
