1aa338585ed26202f5575fc843e1c58190ae4d7e
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-04-18 20:18:14 -0400

    banktivity: update livecheck
    * Removes `:extract_plist` strategy and replaces with `:json`
    
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/b/banktivity.rb
+++ Casks/b/banktivity.rb
@@ -9,8 +9,12 @@ cask "banktivity" do
   homepage "https://www.iggsoftware.com/banktivity/"
 
   livecheck do
-    url "https://www.banktivity.com/download.php"
-    strategy :extract_plist
+    url "https://www.iggsoft.com/banktivity/banktivity#{version.major}-versions-feed.json"
+    strategy :json do |json|
+      json["Banktivity"].map do |release|
+        "#{release["version"]},#{release["build"]}"
+      end
+    end
   end
 
   depends_on macos: ">= :catalina"
@@ -19,6 +23,7 @@ cask "banktivity" do
 
   zap trash: [
     "~/Library/Application Scripts/com.iggsoftware.banktivity",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.iggsoftware.banktivity.sfl*",
     "~/Library/Containers/com.iggsoftware.banktivity",
   ]
 end
