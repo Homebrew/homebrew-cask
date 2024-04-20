71b412f8f2898cb211d1f9d0e675ff2635cb3c36
Rui Chen <rui@chenrui.dev>
2024-04-20 09:34:55 -0400

    qgis 3.36.2
    autobump: add qgis
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/q/qgis.rb
+++ Casks/q/qgis.rb
@@ -1,6 +1,6 @@
 cask "qgis" do
-  version "3.36.1,20240322_122530"
-  sha256 "0f089160d276380db6f0f7a7e880ed1a688833fd5787ea77f1f4bf7dadebdb1f"
+  version "3.36.2,20240419_122719"
+  sha256 "df8ba38fc66b64f6bbce42be8e2122dd69ffd635faa8cc1ef5f1f87b7ef8958e"
 
   url "https://qgis.org/downloads/macos/pr/qgis_pr_final-#{version.csv.first.dots_to_underscores}_#{version.csv.second}.dmg"
   name "QGIS"
