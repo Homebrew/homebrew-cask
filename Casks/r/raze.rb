60ecb76c472696513480e7cb6f847fd5ad0e8cc3
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-04-21 21:20:09 -0400

    raze 1.10.0

--- Casks/r/raze.rb
+++ Casks/r/raze.rb
@@ -1,6 +1,6 @@
 cask "raze" do
-  version "1.9.1"
-  sha256 "d871f0c580c2b314792d5bb812b64fa4669fe6e16167d34da5ff2f1535c21a9c"
+  version "1.10.0"
+  sha256 "31125be24872c694b605a5ed7267a31a24d8e86eae377b7dce9faf922312d27b"
 
   url "https://github.com/coelckers/Raze/releases/download/#{version}/raze-macos-#{version}.zip"
   name "Raze"
