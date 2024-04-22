cf1c92c9bda811dc639f8245165683833c6c1b4e
Bevan Kay <email@bevankay.me>
2024-04-22 12:41:15 +1000

    apidog-europe 2.5.21

--- Casks/a/apidog-europe.rb
+++ Casks/a/apidog-europe.rb
@@ -2,11 +2,11 @@ cask "apidog-europe" do
   arch arm: "-macOS-arm64"
   livecheck_folder = on_arch_conditional arm: "-arm64"
 
-  version "2.5.20"
-  sha256 arm:   "6cf0f338a909a062ab9706f82408fa771f5fb87d5bf0eb23511a4cfa18364355",
-         intel: "1ad6e7aa8396100b9f0d6d23a63113b3444ae2a98f61f31e92558c3ca663abb0"
+  version "2.5.21"
+  sha256 arm:   "b00d3b65b84bfb457686b7ca3c4a5e3e8047ed7b0f836a0c706581754561b024",
+         intel: "2b266abbb5376013f915be9712ee3665ad3ea0e604d6854939e4446b573c2a88"
 
-  url "https://assets.eu.apidog.com/download/#{version}/Apidog%20Europe#{arch}-#{version}.dmg"
+  url "https://file-assets.eu.apidog.com/download/#{version}/Apidog%20Europe#{arch}-#{version}.dmg"
   name "Apidog Europe"
   desc "API development platform hosted in Europe"
   homepage "https://apidog.com/"
