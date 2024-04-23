71e8151931799f02901878dee2f04f445cfe4c91
Miccal Matthews <miccal.matthews@gmail.com>
2024-04-23 17:55:48 +0800

    Update navicat-for-postgresql.rb

--- Casks/n/navicat-for-postgresql.rb
+++ Casks/n/navicat-for-postgresql.rb
@@ -17,7 +17,7 @@ cask "navicat-for-postgresql" do
 
   zap trash: [
     "~/Library/Application Support/PremiumSoft CyberTech/Navicat CC/Navicat for PostgreSQL",
-    "~/Library/Caches/com.apple.helpd/Generated/Navicat Help*#{version}",
+    "~/Library/Caches/com.apple.helpd/Generated/Navicat Help*",
     "~/Library/Preferences/com.navicat.NavicatForPostgreSQL.plist",
     "~/Library/Saved Application State/com.navicat.NavicatForPostgreSQL.savedState",
   ]
