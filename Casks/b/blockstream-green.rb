a48b769cdf57e094b027d51da02635990ff02ffd
adithyasunil26 <adithyasunil26@gmail.com>
2024-04-23 18:56:28 +0530

    blockstream-green 2.0.4

--- Casks/b/blockstream-green.rb
+++ Casks/b/blockstream-green.rb
@@ -1,9 +1,9 @@
 cask "blockstream-green" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "2.0.3"
-  sha256 arm:   "1531938cad0a5f458eaa0e3dc04860622876eb2bbf2e521312735a8a1374e061",
-         intel: "12acba25e9a3607e72b3849bef59e6cc15fa2bae8468630daf454dcd139c91e8"
+  version "2.0.4"
+  sha256 arm:   "290e829121f6fdb355eb4c9841af782e12bb4e670516ef9463fe3242dc1e647c",
+         intel: "71c12fae521f6346c9817f902cc1872490cdfe180e4a085b4ca34e44129f75fe"
 
   url "https://github.com/Blockstream/green_qt/releases/download/release_#{version}/BlockstreamGreen-#{arch}.dmg",
       verified: "github.com/Blockstream/green_qt/"
