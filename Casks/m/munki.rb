c16d5c7700b0f1103653fda5e63b4c7e11792ee5
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-04-23 15:09:54 -0400

    munki 6.5.0.4659
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/m/munki.rb
+++ Casks/m/munki.rb
@@ -1,6 +1,6 @@
 cask "munki" do
-  version "6.4.2.4634"
-  sha256 "ac6648dc479c8eaa82e6aa66be90d5847cadab74c7e0644c4371a839ee1a3c3f"
+  version "6.5.0.4659"
+  sha256 "853988f3542821b4bfb1d171a123950b916f1c0bb09c5b949e578f26a7556d1d"
 
   url "https://github.com/munki/munki/releases/download/v#{version.major_minor_patch}/munkitools-#{version}.pkg",
       verified: "github.com/munki/munki/"
@@ -26,6 +26,7 @@ cask "munki" do
   uninstall launchctl: [
               "com.googlecode.munki.app_usage_monitor",
               "com.googlecode.munki.appusaged",
+              "com.googlecode.munki.authrestartd",
               "com.googlecode.munki.logouthelper",
               "com.googlecode.munki.ManagedSoftwareCenter",
               "com.googlecode.munki.managedsoftwareupdate-check",
