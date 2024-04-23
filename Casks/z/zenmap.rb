c68dd1a2ddf6303c47b83c8792090980f4c73f9d
adithyasunil26 <adithyasunil26@gmail.com>
2024-04-23 15:03:30 +0530

    zenmap 7.95

--- Casks/z/zenmap.rb
+++ Casks/z/zenmap.rb
@@ -1,6 +1,6 @@
 cask "zenmap" do
-  version "7.94"
-  sha256 "6f84fe22af583c4ff7822e23a5bf8da7cbdfb3fc529867db671aefc44dd23114"
+  version "7.95"
+  sha256 "fc069b88f6b64cbb56b02a644075d78df603d9417d3947ff32acc0566439de3c"
 
   url "https://nmap.org/dist/nmap-#{version}.dmg"
   name "Zenmap"
