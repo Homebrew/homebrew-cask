a070466aae1118d508d9fd4b9d1e763ba6378dfc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-23 18:32:22 +0000

    camtasia 2023.3.14

--- Casks/c/camtasia.rb
+++ Casks/c/camtasia.rb
@@ -1,6 +1,6 @@
 cask "camtasia" do
-  version "2023.3.13"
-  sha256 "263e2444d081492ed5393401189eb70f5a5085b00fcccf232e9994e42e37fabf"
+  version "2023.3.14"
+  sha256 "48a4a2e9e090ee1ba1e34076bfd30f7ff6179a82af53ba1a6617b736ce6b74ec"
 
   url "https://download.techsmith.com/camtasiamac/releases/#{version.major[-2..]}#{version.minor_patch.no_dots}/Camtasia.dmg"
   name "Camtasia"
