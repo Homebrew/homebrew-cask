bd095481345dbe218922cfaa7527634b109bfe0b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-24 12:44:01 +0000

    inso 9.0.0

--- Casks/i/inso.rb
+++ Casks/i/inso.rb
@@ -1,6 +1,6 @@
 cask "inso" do
-  version "8.6.1"
-  sha256 "ab76b4402fd04a205045f7c2b391a86739176f843ef28f81d95906a1a02a3063"
+  version "9.0.0"
+  sha256 "d3a82af18d33b09570fcb40a34415fd8af151d8d123c622a7b5057b9fa27abc0"
 
   url "https://github.com/Kong/insomnia/releases/download/lib%40#{version}/inso-macos-#{version}.zip",
       verified: "github.com/Kong/insomnia/"
