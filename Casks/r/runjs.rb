cb08f954f82864adf18cc8ba74ad366ee9f73120
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-04-24 20:02:23 -0400

    runjs 2.12.0

--- Casks/r/runjs.rb
+++ Casks/r/runjs.rb
@@ -1,6 +1,6 @@
 cask "runjs" do
-  version "2.11.0"
-  sha256 "a787f9a42a451584ffb7df39f993128d159debd3d48f163e9d3ffc42f98b3829"
+  version "2.12.0"
+  sha256 "9323eb6444460ceccd7fc249bef44a973143957bf4751bcf2d48a254e5f4ffeb"
 
   url "https://github.com/lukehaas/RunJS/releases/download/v#{version}/RunJS-#{version}-universal.dmg",
       verified: "github.com/lukehaas/RunJS/"
