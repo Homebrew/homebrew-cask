4c1eefc18bb7bf841340e88af238ff8f2c0c7931
adithyasunil26 <adithyasunil26@gmail.com>
2024-04-25 20:04:21 +0530

    wolai 1.2.10

--- Casks/w/wolai.rb
+++ Casks/w/wolai.rb
@@ -1,9 +1,9 @@
 cask "wolai" do
   arch arm: "-arm64"
 
-  version "1.2.9"
-  sha256 arm:   "d25706ac7b915a0cdb8bbced0db8e670e7a65c2b75c6fe8777afd4840469781a",
-         intel: "61c6d25285164e2a5864988b24fc45d912de675320af9c81705493fb0b041635"
+  version "1.2.10"
+  sha256 arm:   "0f6cf997a7bd82197ef58245f2099a6c3de3d5d695f51af306d6f1512f27a94e",
+         intel: "a946f416991fff1c502fb84d0a07fcaa7fb8ccc6e2879af3ab7187210619be6d"
 
   url "https://cdn.wostatic.cn/dist/installers/wolai-#{version}#{arch}.dmg",
       verified: "cdn.wostatic.cn/dist/installers/"
