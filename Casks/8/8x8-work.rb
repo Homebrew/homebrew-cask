93e68b02787f1aa65266df005d8c2f6d433f1e5e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-25 15:29:23 +0000

    8x8-work 8.12.2-3

--- Casks/8/8x8-work.rb
+++ Casks/8/8x8-work.rb
@@ -1,6 +1,6 @@
 cask "8x8-work" do
-  version "8.11.3-2"
-  sha256 "df10505ce462833073e5ec5cf1b15f1df26ee47b1ad86d5af263ab83606ce74d"
+  version "8.12.2-3"
+  sha256 "8c098515b319e0d283868fe7e096896215617252c1441569fa43327ea76c9429"
 
   url "https://vod-updates.8x8.com/ga/work-dmg-v#{version}.dmg"
   name "8x8_work"
