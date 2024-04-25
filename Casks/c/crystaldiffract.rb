0b0054481ca579ab919ddc06b46ddab677e8dacc
Qi Zhang <25192197+singularitti@users.noreply.github.com>
2024-04-25 19:44:50 -0400

    `crystaldiffract`: 7.0.1 (new cask)

---
+++ Casks/c/crystaldiffract.rb
@@ -0,0 +1,28 @@
+cask "crystaldiffract" do
+  version "7.0.1"
+  sha256 :no_check
+
+  url "https://crystalmaker.com/downloads/crystaldiffract#{version.major}_mac.zip"
+  name "CrystalDiffract"
+  desc "Powder diffraction software including phase ID & rietveld refinement"
+  homepage "https://crystalmaker.com/crystaldiffract/index.html"
+
+  livecheck do
+    url "https://crystalmaker.com/support/updates/VersionData-CDM.xml"
+    regex(/<version[^>]+?number=["']?v?(\d+(?:\.\d+)+)["']?[^>]*?>/i)
+  end
+
+  depends_on macos: ">= :mojave"
+
+  app "CrystalDiffract.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.crystalmaker.crystaldiffract.v#{version.major}",
+    "~/Library/Application Scripts/com.crystalmaker.crystaldiffract.v#{version.major}.QuickLookPlugin",
+    "~/Library/Application Scripts/com.crystalmaker.crystaldiffract.v#{version.major}.ThumbnailExtension",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.crystalmaker.crystaldiffract.v#{version.major}.sfl*",
+    "~/Library/Containers/com.crystalmaker.crystaldiffract.v#{version.major}",
+    "~/Library/Containers/com.crystalmaker.crystaldiffract.v#{version.major}.QuickLookPlugin",
+    "~/Library/Containers/com.crystalmaker.crystaldiffract.v#{version.major}.ThumbnailExtension",
+  ]
+end
