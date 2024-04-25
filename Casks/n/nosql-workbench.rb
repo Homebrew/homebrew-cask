8ed810b640de456b17b7630e0ab0413619071d1e
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-04-24 20:40:33 -0400

    nosql-workbench 3.13.0

--- Casks/n/nosql-workbench.rb
+++ Casks/n/nosql-workbench.rb
@@ -1,9 +1,9 @@
 cask "nosql-workbench" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.12.1"
-  sha256 arm:   "cebc1e19e2e1c97f2a60d66c2eb43d9a441c0e9d4365a9356a47a1f9ec81fcb0",
-         intel: "82015cdce14eedbc7131a39643e38eb5b71a8eb769b8f476c31386b86813549e"
+  version "3.13.0"
+  sha256 arm:   "403d05ea89cfdc684c448ccd89fc7f4599313c84bf18cb67178ceba39ca0b9c8",
+         intel: "0e87f51752d30f8efcc21ee34d1e73c0599baefc8239654efc2d505d379b8b77"
 
   url "https://s3.amazonaws.com/nosql-workbench/NoSQL%20Workbench-mac-#{arch}-#{version}.dmg",
       verified: "s3.amazonaws.com/nosql-workbench/"
