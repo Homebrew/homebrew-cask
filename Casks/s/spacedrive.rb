a0b8fc588dc1e9c24875cb5bd88c1cf881df52f3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-26 06:41:03 +0000

    spacedrive 0.2.13

--- Casks/s/spacedrive.rb
+++ Casks/s/spacedrive.rb
@@ -1,9 +1,9 @@
 cask "spacedrive" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.2.12"
-  sha256 arm:   "9f65186a04a40faf00e4ac343bcaa6c55fa83200dde0aff8b69490080f3019f2",
-         intel: "8df0b5a223e2cfc2a370c6a9e31756d9d0652e095ac0e35d3ce806fdf6889ea5"
+  version "0.2.13"
+  sha256 arm:   "3e1f7e26f7b5a8fd4a05b291375236972947472fd2591240c7b9ce17d97703f1",
+         intel: "f851f126b7bea2e3b12af8431beb83283a7b652c7c35647169ce26e3216a82b1"
 
   url "https://github.com/spacedriveapp/spacedrive/releases/download/#{version}/Spacedrive-darwin-#{arch}.dmg"
   name "Spacedrive"
