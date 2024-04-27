b36db538a329b2d26e576c46e9876822a84ce8b6
adithyasunil26 <adithyasunil26@gmail.com>
2024-04-27 16:03:42 +0530

    update aware.rb

--- Casks/a/aware.rb
+++ Casks/a/aware.rb
@@ -1,8 +1,8 @@
 cask "aware" do
-  version "1.1.1"
-  sha256 "d436d21ca4915feaa5b34426dc82c9c3a90d78cce8792506be4d3c9c13ff1ada"
+  version "1.2.0"
+  sha256 "f39d1fabc3a80df649bbb7aa6c596d0fc5082d8718d442ba857ec49bf7fe5eab"
 
-  url "https://github.com/josh/Aware/releases/download/v#{version}/Aware.zip",
+  url "https://github.com/josh/Aware/releases/download/v#{version}/Aware.app.zip",
       verified: "github.com/josh/Aware/"
   name "Aware"
   desc "Menubar app to track active computer use"
