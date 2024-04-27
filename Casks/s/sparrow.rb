f4e5c2f4f5def07b455e8f16b26b6c69061d92be
adithyasunil26 <adithyasunil26@gmail.com>
2024-04-27 15:38:17 +0530

    sparrow 1.9.0

--- Casks/s/sparrow.rb
+++ Casks/s/sparrow.rb
@@ -1,9 +1,9 @@
 cask "sparrow" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.8.6"
-  sha256 arm:   "775d1f6cd77deba0f9ea67b495fcd26bc9d666147c4abd93dd71c1df3caf270f",
-         intel: "aa6d64dd3ce5d6de280f557b52d32aa6bbdf575ff046593f64e217c8c61cbac9"
+  version "1.9.0"
+  sha256 arm:   "fc33ff80de0fed12fb76b2f9d8ea9f10420e7edfdf65e589f95bc7fb274c8181",
+         intel: "b6a00ba6e09ada16702303e2354069da7cf5c1810afa818f997e904ee4441d9e"
 
   url "https://github.com/sparrowwallet/sparrow/releases/download/#{version}/Sparrow-#{version}-#{arch}.dmg",
       verified: "github.com/sparrowwallet/sparrow/"
