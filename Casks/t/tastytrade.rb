df78c114478592f7466f714fead3ce6c05c091da
adithyasunil26 <adithyasunil26@gmail.com>
2024-04-27 15:23:22 +0530

    tastytrade 2.11.0

--- Casks/t/tastytrade.rb
+++ Casks/t/tastytrade.rb
@@ -1,9 +1,9 @@
 cask "tastytrade" do
   arch arm: "-aarch64"
 
-  version "2.10.0"
-  sha256 arm:   "1f1e3384cdba6db4df6b95eeabfd846b86c6f162b8baa0d111c85343f7eae6c3",
-         intel: "45d943b68cc80fd38fc9f4dd204de5f02bd82d131e3e61b82301c9716d1a1575"
+  version "2.11.0"
+  sha256 arm:   "7f0d64639549ea11134f4dda8a0680e017c6fa9a37a2e3efb4f039a59a909000",
+         intel: "244c509640e9518e49cf9fe00ae0f1984fa290f2c3a4c45f9d94e5e9f2b4c00a"
 
   url "https://download.tastytrade.com/desktop-#{version.major}.x.x/#{version}/tastytrade-#{version}#{arch}.dmg"
   name "tastytrade"
