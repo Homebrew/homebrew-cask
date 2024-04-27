6292b84dd487b64561465b91f07c2c19182e663e
Michael Cho <michael@michaelcho.dev>
2024-04-27 16:23:26 -0400

    brave-browser@*: migrate from `homebrew/cask-versions`
    Signed-off-by: Michael Cho <michael@michaelcho.dev>

---
+++ Casks/b/brave-browser@nightly.rb
@@ -0,0 +1,30 @@
+cask "brave-browser@nightly" do
+  arch arm: "arm64", intel: "x64"
+  folder = on_arch_conditional arm: "nightly-arm64", intel: "nightly"
+
+  version "1.67.42.0"
+  sha256 arm:   "dd5b01c106b393ea94ffb3a59ab2151c2baa68d6abe14bf12ace39327bd9a935",
+         intel: "bc6dfbeefe757301ac7052a953402e27ffc62add6cd78d47cae7346d08cb5d68"
+
+  url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.major_minor_patch.sub(".", "")}/Brave-Browser-Nightly-#{arch}.dmg",
+      verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
+  name "Brave Nightly"
+  desc "Web browser focusing on privacy"
+  homepage "https://brave.com/download-nightly/"
+
+  livecheck do
+    url "https://updates.bravesoftware.com/sparkle/Brave-Browser/#{folder}/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
+  app "Brave Browser Nightly.app"
+
+  zap trash: [
+    "~/Library/Application Support/brave",
+    "~/Library/Preferences/com.electron.brave.plist",
+    "~/Library/Saved Application State/com.electron.brave.savedState",
+  ]
+end
