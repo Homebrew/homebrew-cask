a837ebce5a6e51884363f45e6e826eb5b0310697
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-27 00:47:33 +0000

    openshift-client 4.15.11

--- Casks/o/openshift-client.rb
+++ Casks/o/openshift-client.rb
@@ -1,9 +1,9 @@
 cask "openshift-client" do
   arch arm: "-arm64"
 
-  version "4.15.10"
-  sha256 arm:   "78bd0e658c2aa16017619f10976a600418c159830805e4d597cb1d66558b6844",
-         intel: "6efe45f6de41cfb7bfa7dce007ddce2a3654cfbf3b01dd3f0edd60265cf9301d"
+  version "4.15.11"
+  sha256 arm:   "d73354f2cf0c9c2635d8dd0afc5463e54f93fffc3a1f505e2a3ac5eea0a6b380",
+         intel: "b6c0097038d56bdb01a6d6ec4d653126c49aaf1266efcecde7761f881bbecd54"
 
   url "https://mirror.openshift.com/pub/openshift-v#{version.major}/clients/ocp/#{version}/openshift-client-mac#{arch}.tar.gz"
   name "Openshift Client"
