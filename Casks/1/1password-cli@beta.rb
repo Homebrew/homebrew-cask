39803053c079608d8341c655e0f4577ff9f1ffe9
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-27 19:50:48 -0400

    migrate 1password-cli@beta

---
+++ Casks/1/1password-cli@beta.rb
@@ -0,0 +1,21 @@
+cask "1password-cli@beta" do
+  version "2.23.0-beta.01"
+  sha256 "79f6b4fb2b58733c6b69a1ac9ccfb4f7255eec20a089ed29322ca93c76710856"
+
+  url "https://cache.agilebits.com/dist/1P/op2/pkg/v#{version}/op_apple_universal_v#{version}.pkg",
+      verified: "cache.agilebits.com/dist/1P/op2/pkg/"
+  name "1Password CLI"
+  desc "Command-line helper for the 1Password password manager"
+  homepage "https://developer.1password.com/docs/cli"
+
+  livecheck do
+    url "https://app-updates.agilebits.com/product_history/CLI2"
+    regex(%r{href=.*?/op_apple_universal[._-]v?(\d+(?:\.\d+)+-beta\.\d+)\.pkg}i)
+  end
+
+  pkg "op_apple_universal_v#{version}.pkg"
+
+  uninstall pkgutil: "com.1password.op"
+
+  zap trash: "~/.config/op"
+end
