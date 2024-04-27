ba94a28bd320c027bbe9174121e01beba39dc093
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-27 19:50:04 -0400

    migrate 1password@7

---
+++ Casks/1/1password@7.rb
@@ -0,0 +1,27 @@
+cask "1password@7" do
+  version "7.9.11"
+  sha256 "11b14910a2cf0e544e317e49f4a62491e5190545ed684236a295f51d722f30db"
+
+  url "https://c.1password.com/dist/1P/mac#{version.major}/1Password-#{version}.zip"
+  name "1Password"
+  desc "Password manager that keeps all passwords secure behind one password"
+  homepage "https://1password.com/"
+
+  livecheck do
+    url "https://app-updates.agilebits.com/product_history/OPM#{version.major}"
+    regex(%r{href=.*?/1Password-(\d+(?:\.\d+)+)\.pkg}i)
+  end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "1Password #{version.major}.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/*.agilebits.onepassword*",
+    "~/Library/Containers/*.agilebits.onepassword*",
+    "~/Library/Group Containers/2BUA8C4S2C.com.agilebits",
+    "~/Library/Logs/1Password",
+    "~/Library/Preferences/com.agilebits.onepassword*",
+  ]
+end
