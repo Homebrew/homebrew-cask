149846560974c548c8ce91f0d85118c4ad4e538d
Michael Cho <michael@michaelcho.dev>
2024-04-28 13:13:30 -0400

    temurin@8: migrate from `homebrew/cask-versions`

---
+++ Casks/t/temurin@8.rb
@@ -0,0 +1,29 @@
+cask "temurin@8" do
+  version "8,412,08"
+  sha256 "d9815891463bdbc8e7489309ee3845a4f91415a8e0e5369f449474adca8b66d9"
+
+  url "https://github.com/adoptium/temurin8-binaries/releases/download/jdk#{version.csv.first}u#{version.csv.second}-b#{version.csv.third}/OpenJDK#{version.csv.first}U-jdk_x64_mac_hotspot_#{version.csv.first}u#{version.csv.second}b#{version.csv.third}.pkg",
+      verified: "github.com/adoptium/temurin8-binaries/"
+  name "Eclipse Temurin 8"
+  desc "JDK from the Eclipse Foundation (Adoptium)"
+  homepage "https://adoptium.net/"
+
+  livecheck do
+    url "https://api.adoptium.net/v3/assets/feature_releases/8/ga?architecture=x64&image_type=jdk&jvm_impl=hotspot&os=mac&page=0&page_size=1&project=jdk&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse"
+    regex(/^jdk(\d+)u(\d+)-b(\d+)$/i)
+    strategy :json do |json, regex|
+      json.map do |release|
+        match = release["release_name"]&.match(regex)
+        next if match.blank?
+
+        "#{match[1]},#{match[2]},#{match[3]}"
+      end
+    end
+  end
+
+  pkg "OpenJDK#{version.csv.first}U-jdk_x64_mac_hotspot_#{version.csv.first}u#{version.csv.second}b#{version.csv.third}.pkg"
+
+  uninstall pkgutil: "net.temurin.#{version.csv.first}.jdk"
+
+  # No zap stanza required
+end
