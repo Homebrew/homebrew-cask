122dc9cf7262fc651b5c84c9040f51f6a2528037
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-04-28 17:01:22 -0400

    aerial 3.3.8

--- Casks/a/aerial.rb
+++ Casks/a/aerial.rb
@@ -1,6 +1,6 @@
 cask "aerial" do
-  version "3.3.7"
-  sha256 "9445903df76aeb9c50937a32af3981acd65e079f83f403af558df83a940a8352"
+  version "3.3.8"
+  sha256 "457804fd7ba8959d353908aeae27fc998d5379033970b59a2afe60434623c44d"
 
   url "https://github.com/JohnCoates/Aerial/releases/download/v#{version}/Aerial.saver.zip",
       verified: "github.com/JohnCoates/Aerial/"
