9ad08980576a8c76b4e0b643926ed8ecd5a730fb
Michael Cho <michael@michaelcho.dev>
2024-04-28 13:13:27 -0400

    temurin@21: migrate from `homebrew/cask-versions`

---
+++ Casks/t/temurin@21.rb
@@ -0,0 +1,32 @@
+cask "temurin@21" do
+  arch arm: "aarch64", intel: "x64"
+
+  version "21.0.3,9"
+  sha256 arm:   "f0a8cfcc9612caba26f6362ec63c5c67936d9b0a1be973a6e3b629fee9507214",
+         intel: "91f0ce47ac9189901d4dd5c6e9fb59d4aeaa365ab72c8cedbe6b5d4e9f1d6f49"
+
+  url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.csv.first}%2B#{version.csv.second}/OpenJDK#{version.major}U-jdk_#{arch}_mac_hotspot_#{version.csv.first}_#{version.csv.second.major}.pkg",
+      verified: "github.com/adoptium/"
+  name "Eclipse Temurin 21"
+  desc "JDK from the Eclipse Foundation (Adoptium)"
+  homepage "https://adoptium.net/"
+
+  livecheck do
+    url "https://api.adoptium.net/v3/assets/feature_releases/#{version.major}/ga?architecture=#{arch}&image_type=jdk&jvm_impl=hotspot&os=mac&page=0&page_size=1&project=jdk&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse"
+    regex(/^jdk-(\d+(?:\.\d+)*?)\+(\d+(?:\.\d+)*)(?:-LTS)?$/i)
+    strategy :json do |json, regex|
+      json.map do |release|
+        match = release["release_name"]&.match(regex)
+        next if match.blank?
+
+        "#{match[1]},#{match[2]}"
+      end
+    end
+  end
+
+  pkg "OpenJDK#{version.major}U-jdk_#{arch}_mac_hotspot_#{version.csv.first}_#{version.csv.second.major}.pkg"
+
+  uninstall pkgutil: "net.temurin.#{version.major}.jdk"
+
+  # No zap stanza required
+end
