e1e6bf31ae71f2904d3a89331e7bd7b42f82610e
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-29 08:54:04 -0400

    affinity-publisher@1: migrate

---
+++ Casks/a/affinity-publisher@1.rb
@@ -0,0 +1,24 @@
+cask "affinity-publisher@1" do
+  version "1.10.8"
+  sha256 :no_check
+
+  url "https://store.serif.com/download/7ef252/"
+  name "Affinity Publisher"
+  desc "Professional desktop publishing software"
+  homepage "https://affinity.serif.com/en-us/publisher/"
+
+  livecheck do
+    url :url
+    strategy :header_match
+  end
+
+  auto_updates true
+
+  app "Affinity Publisher.app"
+
+  zap trash: [
+    "~/Library/Application Support/Affinity Publisher",
+    "~/Library/Caches/com.seriflabs.affinitypublisher",
+    "~/Library/Saved Application State/com.seriflabs.affinitypublisher.savedState",
+  ]
+end
