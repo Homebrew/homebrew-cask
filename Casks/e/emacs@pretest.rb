1c4ad46ce4c6c57929cf30206b6b77b104028e35
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-29 09:27:48 -0400

    emacs@pretest: migrate

---
+++ Casks/e/emacs@pretest.rb
@@ -0,0 +1,40 @@
+cask "emacs@pretest" do
+  arch arm: "arm64-11", intel: "x86_64-10_11"
+
+  version "29.1.90"
+  sha256 "e3a8fa0e6289052003ef8c729abe7bee5fbd6bd8de60a638940c5d3971934bc9"
+
+  url "https://emacsformacosx.com/emacs-builds/Emacs-pretest-#{version}-universal.dmg"
+  name "Emacs"
+  desc "Text editor"
+  homepage "https://emacsformacosx.com/"
+
+  livecheck do
+    url "https://emacsformacosx.com/atom/pretest"
+    regex(/Emacs[._-]pretest[._-]v?(\d+(?:\.\d+)+)[._-]universal.dmg/i)
+  end
+
+  conflicts_with cask:    [
+                   "emacs",
+                   "emacs-nightly",
+                 ],
+                 formula: "emacs"
+
+  app "Emacs.app"
+  binary "#{appdir}/Emacs.app/Contents/MacOS/Emacs", target: "emacs"
+  binary "#{appdir}/Emacs.app/Contents/MacOS/bin-#{arch}/ctags"
+  binary "#{appdir}/Emacs.app/Contents/MacOS/bin-#{arch}/ebrowse"
+  binary "#{appdir}/Emacs.app/Contents/MacOS/bin-#{arch}/emacsclient"
+  binary "#{appdir}/Emacs.app/Contents/MacOS/bin-#{arch}/etags"
+  manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/ctags.1.gz"
+  manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/ebrowse.1.gz"
+  manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/emacs.1.gz"
+  manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/emacsclient.1.gz"
+  manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/etags.1.gz"
+
+  zap trash: [
+    "~/Library/Caches/org.gnu.Emacs",
+    "~/Library/Preferences/org.gnu.Emacs.plist",
+    "~/Library/Saved Application State/org.gnu.Emacs.savedState",
+  ]
+end
