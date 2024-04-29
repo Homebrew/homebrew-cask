e97b9ddb83bc8880c0487f5a560743fa03d15b3d
Howard Rappaport <howie@rewardsmith.com>
2024-04-28 21:30:54 -0400

    pdf-reader-pro 4.0.0.0

--- Casks/p/pdf-reader-pro.rb
+++ Casks/p/pdf-reader-pro.rb
@@ -1,6 +1,6 @@
 cask "pdf-reader-pro" do
-  version "3.3.1.0"
-  sha256 "e0cf3e6f232a5984d1e5df0ab15ec170c9d87fa0d57af1f8c0337ccce3776b87"
+  version "4.0.0.0"
+  sha256 "613a92d992b043e2cc83a8876465e72544904331991c410356a637c255b4b3e4"
 
   url "https://www.pdfreaderpro.com/downloads/PDFReaderPro_v#{version.major_minor_patch}.dmg"
   name "PDF Reader Pro"
