aa4799b3f5141e92236d67197451f160615be820
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-29 17:06:04 -0400

    julia@lts: migrate

---
+++ Casks/j/julia@lts.rb
@@ -0,0 +1,19 @@
+cask "julia@lts" do
+  version "1.6.7"
+  sha256 "e7b3ad41ec6dd1dd7e6d811ff71682680e056e0f82fc6b17710546630092915a"
+
+  url "https://julialang-s3.julialang.org/bin/mac/x64/#{version.major_minor}/julia-#{version}-mac64.dmg"
+  name "Julia"
+  desc "Programming language for technical computing"
+  homepage "https://julialang.org/"
+
+  livecheck do
+    url "https://julialang.org/downloads/"
+    regex(/\(LTS\)\s+release:\s+v?(\d+(?:\.\d+)+)/i)
+  end
+
+  app "Julia-#{version.major_minor}.app"
+  binary "#{appdir}/Julia-#{version.major_minor}.app/Contents/Resources/julia/bin/julia", target: "julia-lts"
+
+  zap trash: "~/.julia"
+end
