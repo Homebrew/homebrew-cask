f35a44ddde998ef630e9addfc38dab058db3d277
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-29 16:49:30 -0400

    insomnia@alpha: migrate

---
+++ Casks/i/insomnia@alpha.rb
@@ -0,0 +1,42 @@
+cask "insomnia@alpha" do
+  version "9.0.0-beta.5"
+  sha256 "c2ceb27a13f960c6f37bc51b87e8a46e33f170f5b0ad220f175d052591ebda49"
+
+  url "https://github.com/Kong/insomnia/releases/download/core%40#{version}/Insomnia.Core-#{version}.dmg",
+      verified: "github.com/Kong/insomnia/"
+  name "Insomnia"
+  desc "HTTP and GraphQL Client"
+  homepage "https://insomnia.rest/"
+
+  livecheck do
+    url :url
+    regex(/^core@v?(\d+(?:\.\d+)+[._-](?:alpha|beta|rc)[._-]?\d*)$/i)
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        next if release["draft"]
+
+        match = release["tag_name"]&.match(regex)
+        next if match.blank?
+
+        match[1]
+      end
+    end
+  end
+
+  auto_updates true
+  conflicts_with cask: "insomnia"
+  depends_on macos: ">= :catalina"
+
+  app "Insomnia.app"
+
+  zap trash: [
+    "~/Library/Application Support/Insomnia",
+    "~/Library/Caches/com.insomnia.app",
+    "~/Library/Caches/com.insomnia.app.ShipIt",
+    "~/Library/Cookies/com.insomnia.app.binarycookies",
+    "~/Library/Preferences/ByHost/com.insomnia.app.ShipIt.*.plist",
+    "~/Library/Preferences/com.insomnia.app.helper.plist",
+    "~/Library/Preferences/com.insomnia.app.plist",
+    "~/Library/Saved Application State/com.insomnia.app.savedState",
+  ]
+end
