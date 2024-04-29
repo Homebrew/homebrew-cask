6c6ce0c0894443327eba497a1d6758a1b88cf434
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-29 08:27:42 -0400

    ableton-live-intro@11: migrate cask

---
+++ Casks/a/ableton-live-intro@11.rb
@@ -0,0 +1,30 @@
+cask "ableton-live-intro@11" do
+  version "11.3.22"
+  sha256 "544bb47609c5e34bbbe834a049c2dfeb0f7bb72d3334db8e15812460d19765f0"
+
+  url "https://cdn-downloads.ableton.com/channels/#{version}/ableton_live_intro_#{version}_universal.dmg"
+  name "Ableton Live Intro"
+  desc "Sound and music editor"
+  homepage "https://www.ableton.com/en/live/"
+
+  livecheck do
+    cask "ableton-live-suite@11"
+  end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "Ableton Live #{version.major} Intro.app"
+
+  uninstall quit: "com.ableton.live"
+
+  zap trash: [
+    "~/Library/Application Support/Ableton",
+    "~/Library/Application Support/CrashReporter/Ableton *_*.plist",
+    "~/Library/Application Support/CrashReporter/Live_*.plist",
+    "~/Library/Caches/Ableton",
+    "~/Library/Preferences/Ableton",
+    "~/Library/Preferences/com.ableton.live.plist*",
+    "~/Music/Ableton",
+  ]
+end
