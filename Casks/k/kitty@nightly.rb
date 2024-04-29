a57c02279c386ed0685cb862966455d16f0ce818
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-29 17:09:59 -0400

    kitty@nightly: migrate

---
+++ Casks/k/kitty@nightly.rb
@@ -0,0 +1,32 @@
+cask "kitty@nightly" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/kovidgoyal/kitty/releases/download/nightly/kitty-nightly.dmg"
+  name "kitty-nightly"
+  desc "GPU-based terminal emulator"
+  homepage "https://github.com/kovidgoyal/kitty"
+
+  conflicts_with cask: "kitty"
+  depends_on macos: ">= :sierra"
+
+  app "kitty.app"
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/kitty.wrapper.sh"
+  binary shimscript, target: "kitty"
+
+  preflight do
+    File.write shimscript, <<~EOS
+      #!/bin/sh
+      exec '#{appdir}/kitty.app/Contents/MacOS/kitty' "$@"
+    EOS
+  end
+
+  zap trash: [
+    "~/.config/kitty",
+    "~/Library/Caches/kitty",
+    "~/Library/Preferences/kitty",
+    "~/Library/Preferences/net.kovidgoyal.kitty.plist",
+    "~/Library/Saved Application State/net.kovidgoyal.kitty.savedState",
+  ]
+end
