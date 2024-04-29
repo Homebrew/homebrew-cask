a77bf1f9e74fd38c31055be1e89e9cc720a1eab3
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-29 16:49:30 -0400

    inso@beta: migrate

---
+++ Casks/i/inso@beta.rb
@@ -0,0 +1,22 @@
+cask "inso@beta" do
+  version "9.0.0-beta.5"
+  sha256 "ef022cfb82a756bbdf0f7cd10b5ed5567205a19bf4bb802adda2db1402f6c0bf"
+
+  url "https://github.com/Kong/insomnia/releases/download/lib%40#{version}/inso-macos-#{version}.zip",
+      verified: "github.com/Kong/insomnia/"
+  name "inso"
+  desc "CLI HTTP and GraphQL Client"
+  homepage "https://insomnia.rest/products/inso"
+
+  livecheck do
+    url "https://github.com/Kong/insomnia/releases?q=prerelease%3Atrue+Inso+CLI"
+    regex(%r{href=["']?[^"' >]*?/tag/lib%40([^"' >]+?)["' >]}i)
+    strategy :page_match
+  end
+
+  conflicts_with cask: "inso"
+
+  binary "inso"
+
+  # No zap stanza required
+end
