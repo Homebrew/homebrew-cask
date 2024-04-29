2fab1bf5941575561b18bb04ea4897a67751e391
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-29 16:49:30 -0400

    iterm2@beta: migrate

---
+++ Casks/i/iterm2@beta.rb
@@ -0,0 +1,34 @@
+cask "iterm2@beta" do
+  # NOTE: "2" is not a version number, but an intrinsic part of the product name
+  version "3.5.0beta24"
+  sha256 "c596ad3b9719308cb0eb574628ff1305af7662a999f743277d71db6e4813fc9d"
+
+  url "https://iterm2.com/downloads/beta/iTerm2-#{version.dots_to_underscores}.zip"
+  name "iTerm2"
+  desc "Terminal emulator as alternative to Apple's Terminal app"
+  homepage "https://www.iterm2.com/"
+
+  livecheck do
+    url "https://iterm2.com/appcasts/testing_modern.xml"
+    strategy :sparkle, &:version
+  end
+
+  auto_updates true
+  conflicts_with cask: [
+    "iterm2",
+    "iterm2-legacy",
+    "iterm2-nightly",
+  ]
+  depends_on macos: ">= :catalina"
+
+  app "iTerm.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.googlecode.iterm2.sfl*",
+    "~/Library/Application Support/iTerm",
+    "~/Library/Application Support/iTerm2",
+    "~/Library/Caches/com.googlecode.iterm2",
+    "~/Library/Preferences/com.googlecode.iterm2.plist",
+    "~/Library/Saved Application State/com.googlecode.iterm2.savedState",
+  ]
+end
