157ba6b2f101b0bcae5778eee3ee40c77697fb41
Bevan Kay <email@bevankay.me>
2024-04-29 11:22:22 +1000

    aviatrix-vpn-client: fix broken links

--- Casks/a/aviatrix-vpn-client.rb
+++ Casks/a/aviatrix-vpn-client.rb
@@ -6,10 +6,10 @@ cask "aviatrix-vpn-client" do
       verified: "aviatrix-download.s3.amazonaws.com/"
   name "Aviatrix VPN Client"
   desc "VPN client that provides SAML authentication"
-  homepage "https://docs.aviatrix.com/Downloads/samlclient.html"
+  homepage "https://docs.aviatrix.com/previous/documentation/latest/aviatrix-openvpn/download-vpn-client.html"
 
   livecheck do
-    url "https://docs.aviatrix.com/previous/documentation/latest/release-notes/vpn-client/vpn-release-notes.html"
+    url "https://docs.aviatrix.com/documentation/latest/release-notes/vpn-client/vpn-release-notes.html"
     regex(/href=.*?aviatrix[._-]vpn[._-]client[._-]v?(\d+(?:[.-]\d+)+)[ "<]/i)
     strategy :page_match do |page, regex|
       page.scan(regex).map { |match| match&.first&.tr("-", ".") }
