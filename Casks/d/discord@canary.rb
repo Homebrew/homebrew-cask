cadd63f8af2f83568e7f240637e24388c739c95f
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-29 09:20:11 -0400

    discord@development: migrate

---
+++ Casks/d/discord@canary.rb
@@ -0,0 +1,30 @@
+cask "discord@canary" do
+  version "0.0.487"
+  sha256 "8a6cda6825a3958949a81da84b2b4cfd1560e7b1fa961bcd9c8fdb7e2d93e3ff"
+
+  url "https://dl-canary.discordapp.net/apps/osx/#{version}/DiscordCanary.dmg",
+      verified: "dl-canary.discordapp.net/"
+  name "Discord Canary"
+  desc "Voice and text chat software"
+  homepage "https://canary.discord.com/"
+
+  livecheck do
+    url "https://discord.com/api/download/canary?platform=osx"
+    strategy :header_match
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
+  app "Discord Canary.app"
+
+  zap trash: [
+    "~/Library/Application Support/discordcanary",
+    "~/Library/Caches/com.hnc.DiscordCanary",
+    "~/Library/Caches/com.hnc.DiscordCanary.ShipIt",
+    "~/Library/Cookies/com.hnc.DiscordCanary.binarycookies",
+    "~/Library/Preferences/com.hnc.DiscordCanary.helper.plist",
+    "~/Library/Preferences/com.hnc.DiscordCanary.plist",
+    "~/Library/Saved Application State/com.hnc.DiscordCanary.savedState",
+  ]
+end
---
+++ Casks/d/discord@development.rb
@@ -0,0 +1,30 @@
+cask "discord@development" do
+  version "0.0.39"
+  sha256 "9d957d2cadde1a95caff6c10289067dcad9477ab8193c69a9a691e134d2b5b1d"
+
+  url "https://dl-development.discordapp.net/apps/osx/#{version}/DiscordDevelopment.dmg",
+      verified: "dl-development.discordapp.net/"
+  name "Discord Development"
+  desc "Voice and text chat software"
+  homepage "https://discord.com/"
+
+  livecheck do
+    url "https://discord.com/api/download/development?platform=osx"
+    strategy :header_match
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
+  app "Discord Development.app"
+
+  zap trash: [
+    "~/Library/Application Support/discorddevelopment",
+    "~/Library/Caches/com.hnc.DiscordDevelopment",
+    "~/Library/Caches/com.hnc.DiscordDevelopment.ShipIt",
+    "~/Library/Cookies/com.hnc.DiscordDevelopment.binarycookies",
+    "~/Library/Preferences/com.hnc.DiscordDevelopment.helper.plist",
+    "~/Library/Preferences/com.hnc.DiscordDevelopment.plist",
+    "~/Library/Saved Application State/com.hnc.DiscordDevelopment.savedState",
+  ]
+end
