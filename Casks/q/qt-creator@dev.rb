9a66ccff0689e18f9c940a8dcb7867b8c4b44e39
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-30 09:28:51 -0400

    qt-creator@dev: migrate

---
+++ Casks/q/qt-creator@dev.rb
@@ -0,0 +1,35 @@
+cask "qt-creator@dev" do
+  version "13.0.0-rc1"
+  sha256 "9c2056aadf552a0d118bfd0afa9bcee5f3ed2fda588f55e22c8d504246678ea0"
+
+  url "https://download.qt.io/development_releases/qtcreator/#{version.major_minor}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
+  name "Qt Creator Dev"
+  desc "IDE for application development"
+  homepage "https://www1.qt.io/developers/"
+
+  livecheck do
+    url "https://download.qt.io/development_releases/qtcreator/"
+    regex(%r{href=["']?v?(\d+(?:\.\d+)+[._-](?:alpha|beta|rc)\d*)/?["' >]}i)
+    strategy :page_match do |page, regex|
+      versions = page.scan(%r{href=["']?v?(\d+(?:\.\d+)+)/?["' >]}i).flatten.uniq.sort_by { |v| Version.new(v) }
+      newest_major_minor = versions.last
+      next if newest_major_minor.blank?
+
+      # Fetch the directory listing page for the newest version
+      version_page = Homebrew::Livecheck::Strategy.page_content(URI.join(@url, "#{newest_major_minor}/").to_s)
+      next if version_page[:content].blank?
+
+      version_page[:content].scan(regex).map(&:first)
+    end
+  end
+
+  depends_on macos: ">= :big_sur"
+
+  app "Qt Creator.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.qtproject.QtCreator.plist",
+    "~/Library/Preferences/org.qt-project.qtcreator.plist",
+    "~/Library/Saved Application State/org.qt-project.qtcreator.savedState",
+  ]
+end
