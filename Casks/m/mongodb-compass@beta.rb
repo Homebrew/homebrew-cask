9e6c9776cbce0c2b5e051e4ff02ba176141ac9c8
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-30 08:38:15 -0400

    mongodb-compass@beta: migrate

---
+++ Casks/m/mongodb-compass@beta.rb
@@ -0,0 +1,26 @@
+cask "mongodb-compass@beta" do
+  version "1.42.6-beta.4"
+  sha256 "e0a04054fd925cb3516e7d93d25d8eefd9e4ad6e2d0090e7447ff5c939b117b9"
+
+  url "https://downloads.mongodb.com/compass/beta/mongodb-compass-#{version}-darwin-x64.dmg"
+  name "MongoDB Compass"
+  desc "GUI for MongoDB"
+  homepage "https://www.mongodb.com/try/download/compass"
+
+  livecheck do
+    url "https://github.com/mongodb-js/compass/releases?q=prerelease%3Atrue&expanded=true"
+    regex(%r{href=["']?[^"' >]*?/tag/\D*?(\d+(?:\.\d+)+-beta\.\d)[^"' >]*?["' >]}i)
+    strategy :page_match
+  end
+
+  depends_on macos: ">= :catalina"
+
+  app "MongoDB Compass Beta.app"
+
+  zap trash: [
+    "~/.mongodb",
+    "~/Library/Application Support/MongoDB Compass Beta",
+    "~/Library/Preferences/com.mongodb.compass.beta.plist",
+    "~/Library/Saved Application State/com.mongodb.compass.beta.savedState",
+  ]
+end
