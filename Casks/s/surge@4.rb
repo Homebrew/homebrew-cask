fcbb344ac61bdd1981b2c133e2173ce79d916c7b
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-30 09:36:30 -0400

    surge@4: migrate

---
+++ Casks/s/surge@4.rb
@@ -0,0 +1,36 @@
+cask "surge@4" do
+  version "4.11.2,2016,b4c9dad3472594b01ef3d8ac9b05c78e"
+  sha256 "144ba12a3ea884f264ebe8fb5fc2c77003f51057fd9efe8662824acaf3805104"
+
+  url "https://dl.nssurge.com/mac/v#{version.major}/Surge-#{version.tr(",", "-")}.zip"
+  name "Surge"
+  desc "Network toolbox"
+  homepage "https://nssurge.com/"
+
+  livecheck do
+    url "https://www.nssurge.com/mac/v#{version.major}/appcast-signed.xml"
+    regex(/[._-](\d+(?:\.\d+)+)[._-](\d+)[._-](\h+)\.zip/i)
+    strategy :sparkle do |item, regex|
+      item.url.scan(regex).map do |match|
+        "#{match[0]},#{match[1]},#{match[2]}"
+      end
+    end
+  end
+
+  auto_updates true
+  conflicts_with cask: "surge"
+  depends_on macos: ">= :high_sierra"
+
+  app "Surge.app"
+
+  uninstall launchctl: "com.nssurge.surge-mac.helper",
+            delete:    "/Library/PrivilegedHelperTools/com.nssurge.surge-mac.helper"
+
+  zap delete: [
+    "~/Library/Application Support/com.nssurge.surge-mac",
+    "~/Library/Caches/com.nssurge.surge-mac",
+    "~/Library/Caches/com.nssurge.surge-mac.plist",
+    "~/Library/Logs/Surge",
+    "~/Library/Preferences/com.nssurge.surge-mac.plist",
+  ]
+end
