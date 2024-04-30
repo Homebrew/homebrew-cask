f4555a68100a812bb930d6435a647b8bb1f6ad36
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-30 08:39:17 -0400

    netnewswire@beta: migrate

---
+++ Casks/n/netnewswire@beta.rb
@@ -0,0 +1,31 @@
+cask "netnewswire@beta" do
+  version "6.1.5b1"
+  sha256 "17a458daca9c3a9a5d88f05e94ee87cd34ab9fde7853dfb2e3f667476c556d5b"
+
+  url "https://github.com/brentsimmons/NetNewsWire/releases/download/mac-#{version}/NetNewsWire#{version}.zip",
+      verified: "github.com/brentsimmons/NetNewsWire/"
+  name "NetNewsWire"
+  desc "Free and open-source RSS reader"
+  homepage "https://ranchero.com/netnewswire/"
+
+  livecheck do
+    url :url
+    regex(/^mac-(\d+(?:\.\d+)*b\d+)$/i)
+  end
+
+  auto_updates true
+  conflicts_with cask: "netnewswire"
+  depends_on macos: ">= :big_sur"
+
+  app "NetNewsWire.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.ranchero.NetNewsWire-Evergreen.Subscribe-to-Feed",
+    "~/Library/Application Support/NetNewsWire",
+    "~/Library/Caches/com.ranchero.NetNewsWire-Evergreen",
+    "~/Library/Containers/com.ranchero.NetNewsWire-Evergreen.Subscribe-to-Feed",
+    "~/Library/Preferences/com.ranchero.NetNewsWire-Evergreen.plist",
+    "~/Library/Saved Application State/com.ranchero.NetNewsWire-Evergreen.savedState",
+    "~/Library/WebKit/com.ranchero.NetNewsWire-Evergreen",
+  ]
+end
