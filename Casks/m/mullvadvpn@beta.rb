f9205796f46d5b6a0f35c9b76c8ee12b75428512
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-30 08:38:15 -0400

    mullvadvpn@beta: migrate

---
+++ Casks/m/mullvadvpn@beta.rb
@@ -0,0 +1,41 @@
+cask "mullvadvpn@beta" do
+  version "2024.2-beta1"
+  sha256 "4770625008315c85a5c231ff38ec005e4133329bda0cb25544337f7887656f2b"
+
+  url "https://github.com/mullvad/mullvadvpn-app/releases/download/#{version}/MullvadVPN-#{version}.pkg",
+      verified: "github.com/mullvad/mullvadvpn-app/"
+  name "Mullvad VPN"
+  desc "VPN client"
+  homepage "https://mullvad.net/"
+
+  livecheck do
+    url :url
+    regex(/^v?(\d+(?:\.\d+)+[._-]beta\d*)$/i)
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        next if release["draft"]
+
+        match = release["tag_name"]&.match(regex)
+        next if match.blank?
+
+        match[1]
+      end
+    end
+  end
+
+  conflicts_with cask: "mullvadvpn"
+
+  pkg "MullvadVPN-#{version}.pkg"
+
+  uninstall launchctl: "net.mullvad.daemon",
+            quit:      "net.mullvad.vpn",
+            pkgutil:   "net.mullvad.vpn"
+
+  zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/net.mullvad.vpn.sfl*",
+    "~/Library/Application Support/Mullvad VPN",
+    "~/Library/Logs/Mullvad VPN",
+    "~/Library/Preferences/net.mullvad.vpn.helper.plist",
+    "~/Library/Preferences/net.mullvad.vpn.plist",
+  ]
+end
