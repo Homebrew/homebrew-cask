df24f0a2838089a0f9e5d01a5306a837e7595fe3
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-30 11:34:39 -0400

    qgis@ltr: migrate

---
+++ Casks/q/qgis@ltr.rb
@@ -0,0 +1,29 @@
+cask "qgis@ltr" do
+  version "3.34.6,20240419_184338"
+  sha256 "a694b3999d88638484463ff33f3f33f7c53ce3b587c80383bed4a8bb17556a63"
+
+  url "https://qgis.org/downloads/macos/ltr/qgis_ltr_final-#{version.csv.first.dots_to_underscores}_#{version.csv.second}.dmg"
+  name "QGIS LTR"
+  desc "Geographic Information System"
+  homepage "https://www.qgis.org/"
+
+  livecheck do
+    url "https://qgis.org/downloads/macos/qgis-macos-ltr.sha256sum"
+    regex(/qgis_ltr_final[._-]v?(\d+(?:_\d+)+)[._-](\d+_\d+)\.dmg/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[0].tr("_", ".")},#{match[1]}" }
+    end
+  end
+
+  deprecate! date: "2025-05-01", because: :unsigned
+
+  depends_on macos: ">= :high_sierra"
+
+  app "QGIS-LTR.app"
+
+  zap trash: [
+    "~/Library/Application Support/QGIS",
+    "~/Library/Caches/QGIS",
+    "~/Library/Saved Application State/org.qgis.qgis*.savedState",
+  ]
+end
