f34986b6f0f43d50fe441d95dca5fdaa5bcf1d08
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-30 09:27:32 -0400

    processing@3: migrate

---
+++ Casks/p/processing@3.rb
@@ -0,0 +1,33 @@
+cask "processing@3" do
+  version "3.5.4,0270"
+  sha256 "4d64fe42a6c5c0863cc82e93a036e73731999ee9448be45bc322f91b0010bb6b"
+
+  url "https://github.com/processing/processing/releases/download/processing-#{version.csv.second}-#{version.csv.first}/processing-#{version.csv.first}-macosx.zip",
+      verified: "github.com/processing/processing/"
+  name "Processing"
+  desc "Flexible software sketchbook and a language for learning how to code"
+  homepage "https://processing.org/"
+
+  livecheck do
+    url :url
+    regex(/^processing[._-](\d+(?:\.\d+)*)[@_-](3(?:\.\d+)+)$/i)
+    strategy :github_latest do |json, regex|
+      json["tag_name"]&.scan(regex)&.map { |match| "#{match[1]},#{match[0]}" }
+    end
+  end
+
+  conflicts_with cask: [
+    "processing",
+    "processing2",
+  ]
+
+  app "Processing.app"
+
+  uninstall quit: "org.processing.app"
+
+  zap trash: [
+    "~/Library/Processing",
+    "~/Preferences/org.processing.app.plist",
+    "~/Preferences/processing.app.tools.plist",
+  ]
+end
