322ba82f31deb18abe1400ad1b8777dd58cfc24d
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-30 09:39:21 -0400

    telegram-desktop@beta: migrate

---
+++ Casks/t/telegram-desktop@beta.rb
@@ -0,0 +1,29 @@
+cask "telegram-desktop@beta" do
+  version "4.16.10"
+  sha256 "e6a929bd029fa1b3f0afa049b7def7475996979f2b2e69d88dcde2562e7e5eb4"
+
+  url "https://updates.tdesktop.com/tmac/tsetup.#{version}.beta.dmg",
+      verified: "updates.tdesktop.com/tmac/"
+  name "Telegram Desktop"
+  desc "Desktop client for Telegram messenger"
+  homepage "https://desktop.telegram.org/"
+
+  livecheck do
+    url "https://telegram.org/dl/desktop/mac?beta=1"
+    regex(/tsetup[._-]v?(\d+(?:\.\d+)+)[._-]beta\.dmg/i)
+    strategy :header_match
+  end
+
+  auto_updates true
+  conflicts_with cask: "telegram-desktop"
+  depends_on macos: ">= :high_sierra"
+
+  # Renamed to avoid conflict with telegram
+  app "Telegram.app", target: "Telegram Desktop.app"
+
+  zap trash: [
+    "~/Library/Application Support/Telegram Desktop",
+    "~/Library/Preferences/com.tdesktop.Telegram.plist",
+    "~/Library/Saved Application State/com.tdesktop.Telegram.savedState",
+  ]
+end
