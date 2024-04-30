7ef32da6ca9fb5b6f3192c969ab4a1da8a2dc66e
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-30 11:03:12 -0400

    utm@beta: migrate

---
+++ Casks/u/utm@beta.rb
@@ -0,0 +1,31 @@
+cask "utm@beta" do
+  version "4.5.2"
+  sha256 "a41466b475e7c3d06815e6820a8924e61ba50a1744165f5d10d6cbf33cd7333b"
+
+  url "https://github.com/utmapp/UTM/releases/download/v#{version}/UTM.dmg",
+      verified: "github.com/utmapp/UTM/"
+  name "UTM"
+  desc "Virtual machines UI using QEMU"
+  homepage "https://mac.getutm.app/"
+
+  # Use the default livecheck strategy to return the "latest" release
+  # regardless of how it is tagged. https://github.com/Homebrew/homebrew-cask-versions/pull/18839#issuecomment-1874765632
+
+  conflicts_with cask: "utm"
+  depends_on macos: ">= :big_sur"
+
+  app "UTM.app"
+  binary "#{appdir}/UTM.app/Contents/MacOS/utmctl"
+
+  uninstall quit: "com.utmapp.UTM"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.utmapp.QEMUHelper",
+    "~/Library/Application Scripts/com.utmapp.UTM",
+    "~/Library/Containers/com.utmapp.QEMUHelper",
+    "~/Library/Containers/com.utmapp.UTM",
+    "~/Library/Group Containers/*.com.utmapp.UTM",
+    "~/Library/Preferences/com.utmapp.UTM.plist",
+    "~/Library/Saved Application State/com.utmapp.UTM.savedState",
+  ]
+end
