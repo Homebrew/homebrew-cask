19c7ed8193aa42a739c23b03bb2ddab0504b4ebb
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-30 11:03:13 -0400

    visual-studio-code@insiders: migrate

---
+++ Casks/v/visual-studio-code@insiders.rb
@@ -0,0 +1,31 @@
+cask "visual-studio-code@insiders" do
+  arch arm: "-arm64"
+
+  version :latest
+  sha256 :no_check
+
+  url "https://code.visualstudio.com/sha/download?build=insider&os=darwin#{arch}"
+  name "Microsoft Visual Studio Code"
+  name "Visual Studio Code Insiders"
+  desc "Code editor"
+  homepage "https://code.visualstudio.com/insiders"
+
+  depends_on macos: ">= :catalina"
+
+  app "Visual Studio Code - Insiders.app"
+  binary "#{appdir}/Visual Studio Code - Insiders.app/Contents/Resources/app/bin/code", target: "code-insiders"
+
+  zap trash: [
+    "~/.vscode-insiders",
+    "~/Library/Application Support/Code - Insiders",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.microsoft.vscodeinsiders.sfl*",
+    "~/Library/Caches/Code - Insiders",
+    "~/Library/Caches/com.microsoft.VSCodeInsiders",
+    "~/Library/Caches/com.microsoft.VSCodeInsiders.ShipIt",
+    "~/Library/HTTPStorages/com.microsoft.VSCodeInsiders",
+    "~/Library/Preferences/ByHost/com.microsoft.VSCodeInsiders.ShipIt.*.plist",
+    "~/Library/Preferences/com.microsoft.VSCodeInsiders.helper.plist",
+    "~/Library/Preferences/com.microsoft.VSCodeInsiders.plist",
+    "~/Library/Saved Application State/com.microsoft.VSCodeInsiders.savedState",
+  ]
+end
