b852f204af88d04fe6b597b23fb5e3749dbd42ac
Miccal Matthews <miccal.matthews@gmail.com>
2024-05-01 05:04:04 +0800

    Update deckset.rb

--- Casks/d/deckset.rb
+++ Casks/d/deckset.rb
@@ -23,8 +23,7 @@ cask "deckset" do
     "~/Library/Caches/com.apple.helpd/Generated/com.unsignedinteger.Deckset.Helpbook*",
     "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.unsignedinteger.Deckset-Paddle",
     "~/Library/Caches/com.unsignedinteger.Deckset-Paddle",
-    "~/Library/HTTPStorages/com.unsignedinteger.Deckset-Paddle",
-    "~/Library/HTTPStorages/com.unsignedinteger.Deckset-Paddle.binarycookies",
+    "~/Library/HTTPStorages/com.unsignedinteger.Deckset-Paddle*",
     "~/Library/Preferences/com.unsignedinteger.Deckset-Paddle.plist",
     "~/Library/Saved Application State/com.unsignedinteger.Deckset-Paddle.savedState",
   ]
