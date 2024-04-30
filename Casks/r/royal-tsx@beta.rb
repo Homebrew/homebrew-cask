0c43d63c76a495f266bfb791a572499749f2df61
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-30 09:29:57 -0400

    royal-tsx@beta: migrate

---
+++ Casks/r/royal-tsx@beta.rb
@@ -0,0 +1,27 @@
+cask "royal-tsx@beta" do
+  version "6.0.3.6"
+  sha256 "ba94dd967acea12d18270d3789ef1cca7e141a576013a2badbf7f7696a237f7a"
+
+  url "https://royaltsx-v#{version.major}.royalapps.com/updates/royaltsx_#{version}.dmg"
+  name "Royal TSX"
+  desc "Remote management solution"
+  homepage "https://www.royalapps.com/ts/mac/features"
+
+  livecheck do
+    url "https://royaltsx-v#{version.major}.royalapps.com/updates_beta.php"
+    strategy :sparkle
+  end
+
+  auto_updates true
+  conflicts_with cask: "royal-tsx"
+  depends_on macos: ">= :big_sur"
+
+  app "Royal TSX.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.lemonmojo.RoyalTSX.App",
+    "~/Library/Application Support/Royal TSX",
+    "~/Library/Caches/com.lemonmojo.RoyalTSX.App",
+    "~/Library/Preferences/com.lemonmojo.RoyalTSX.App.plist",
+  ]
+end
