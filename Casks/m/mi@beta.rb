2c038a5ef24ce782056bceb077e3dafce0f1d402
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-30 08:38:14 -0400

    mi@beta: migrate

---
+++ Casks/m/mi@beta.rb
@@ -0,0 +1,24 @@
+cask "mi@beta" do
+  version "3.7.9"
+  sha256 "483d204dee40348490c8264b5c370ca6788d9e4250e4571fbdd47ded573421bd"
+
+  url "https://www.mimikaki.net/download/mi#{version}.dmg"
+  name "mi"
+  desc "Text editor"
+  homepage "https://www.mimikaki.net/download/beta.html"
+
+  livecheck do
+    url "https://www.mimikaki.net/download/appcast_beta.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  conflicts_with cask: "mi"
+  depends_on macos: ">= :high_sierra"
+
+  app "mi.app"
+
+  zap trash: [
+    "~/Library/Caches/net.mimikaki.mi",
+    "~/Library/Preferences/net.mimikaki.mi.plist",
+  ]
+end
