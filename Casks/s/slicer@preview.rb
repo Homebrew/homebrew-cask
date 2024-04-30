878c49a46617bbb14c95eb14a773ddbdc006946e
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-30 11:34:39 -0400

    slicer@preview: migrate

---
+++ Casks/s/slicer@preview.rb
@@ -0,0 +1,27 @@
+cask "slicer@preview" do
+  version :latest
+  sha256 :no_check
+
+  url "https://download.slicer.org/find?os=macosx&stability=nightly" do |page|
+    require "json"
+    file_path = JSON.parse(page)["download_url"]
+    URI.join(page.url, file_path)
+  end
+  name "3D Slicer"
+  desc "Medical image processing and visualization system"
+  homepage "https://www.slicer.org/"
+
+  deprecate! date: "2025-05-01", because: :unsigned
+
+  conflicts_with cask: "slicer"
+
+  app "Slicer.app"
+
+  zap trash: [
+    "~/.config/www.na-mic.org",
+    "~/Library/Application Support/NA-MIC",
+    "~/Library/Preferences/org.slicer.slicer.plist",
+    "~/Library/Preferences/Slicer.plist",
+    "~/Library/Saved Application State/org.slicer.slicer.savedState",
+  ]
+end
