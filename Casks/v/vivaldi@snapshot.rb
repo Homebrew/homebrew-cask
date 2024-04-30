b05786516417ad4b3ae9c17b92ce1a353dcd8475
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-30 11:03:13 -0400

    vivaldi@snapshot: migrate

---
+++ Casks/v/vivaldi@snapshot.rb
@@ -0,0 +1,30 @@
+cask "vivaldi@snapshot" do
+  version "6.7.3329.19"
+  sha256 "d4899580576236af4dc3408e8700fad0e030ca42c9e23876f4b5ee52ca235f68"
+
+  url "https://downloads.vivaldi.com/snapshot-auto/Vivaldi.#{version}.universal.tar.xz"
+  name "Vivaldi"
+  desc "Web browser with built-in email client focusing on customization and control"
+  homepage "https://vivaldi.com/"
+
+  livecheck do
+    url "https://update.vivaldi.com/update/1.0/snapshot/mac/appcast.xml"
+    strategy :sparkle
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
+  app "Vivaldi Snapshot.app"
+
+  uninstall quit: "com.vivaldi.Vivaldi.snapshot"
+
+  zap trash: [
+    "~/Library/Application Support/Vivaldi Snapshot",
+    "~/Library/Caches/com.vivaldi.Vivaldi.snapshot",
+    "~/Library/Caches/Vivaldi Snapshot",
+    "~/Library/HTTPStorages/com.vivaldi.Vivaldi.snapshot",
+    "~/Library/Preferences/com.vivaldi.Vivaldi.snapshot.plist",
+    "~/Library/Saved Application State/com.vivaldi.Vivaldi.snapshot.savedState",
+  ]
+end
