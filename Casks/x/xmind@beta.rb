6cf165071dcc3b724613239baa9a53371d7e2321
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-30 11:03:14 -0400

    xmind@beta: migrate

---
+++ Casks/x/xmind@beta.rb
@@ -0,0 +1,29 @@
+cask "xmind@beta" do
+  version "24.07.03011,202403312339"
+  sha256 "abd4e869d377087cd7ba273e764740bfdb48ee55dc0bab38f089abdcd59efeab"
+
+  url "https://dl3.xmind.net/Xmind-for-macOS-#{version.csv.first}-beta-#{version.csv.second}.dmg"
+  name "XMind"
+  desc "Mind mapping and brainstorming tool"
+  homepage "https://www.xmind.net/"
+
+  livecheck do
+    url "https://xmind.app/desktop-beta/download/mac/"
+    regex(/Xmind[._-]for[._-]macOS[._-](\d+(?:\.\d+)+)-beta-(\d*)\.dmg/i)
+    strategy :header_match do |headers, regex|
+      headers["location"].scan(regex).map { |match| "#{match[0]},#{match[1]}" }
+    end
+  end
+
+  conflicts_with cask: [
+    "xmind",
+    "xmind8",
+  ]
+
+  app "Xmind.app"
+
+  zap trash: [
+    "~/Library/Saved Application State/org.xmind.cathy.application.savedState",
+    "~/Library/XMind",
+  ]
+end
