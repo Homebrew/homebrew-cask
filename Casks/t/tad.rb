0a209a59af75aafc6d906af67dd2ee1ffeadf183
Miccal Matthews <miccal.matthews@gmail.com>
2024-05-02 04:51:25 +0800

    Update tad.rb

--- Casks/t/tad.rb
+++ Casks/t/tad.rb
@@ -1,12 +1,11 @@
 cask "tad" do
-  arch arm: "aarch64", intel: "x86-64"
-  suffix = on_arch_conditional arm: "-arm64", intel: ""
+  arch arm: "-arm64"
 
   version "0.13.0"
-  sha256  intel: "4c71f6f6a0fadf65891663d1a0462dd8d3576a4c62bdd8721012cbdd61ee1fee",
-          arm:   "9c129a65fdf94b4b32b6e26d8836a56d4284bff9a6e1df657cc23d19e5c802b1"
+  sha256  arm:   "9c129a65fdf94b4b32b6e26d8836a56d4284bff9a6e1df657cc23d19e5c802b1",
+          intel: "4c71f6f6a0fadf65891663d1a0462dd8d3576a4c62bdd8721012cbdd61ee1fee"
 
-  url "https://github.com/antonycourtney/tad/releases/download/v#{version}/Tad-#{version}#{suffix}.dmg",
+  url "https://github.com/antonycourtney/tad/releases/download/v#{version}/Tad-#{version}#{arch}.dmg",
       verified: "github.com/antonycourtney/tad/"
   name "Tad"
   desc "Desktop application for viewing and analyzing tabular data"
