dfa5cd45a90b38136cfd8bf45e5c1998c4c48813
ezrayellin <62797392+ezrayellin@users.noreply.github.com>
2024-04-30 21:02:16 -0400

    Add logi-options-plus-minimal
    Logitech Options+ without the bloatware

---
+++ Casks/l/logi-options-plus-minimal.rb
@@ -0,0 +1,61 @@
+cask "logi-options-plus-minimal" do
+  version "1.70.551909"
+  sha256 :no_check
+
+  url "https://download01.logi.com/web/ftp/pub/techsupport/optionsplus/logioptionsplus_installer_offline.zip",
+      verified: "download01.logi.com/web/ftp/pub/techsupport/optionsplus/"
+  name "Logitech Options+ (Minimal)"
+  desc "Software for Logitech devices, most extra features disabled"
+  homepage "https://www.logitech.com/en-us/software/logi-options-plus.html"
+
+  livecheck do
+    url "https://support.logi.com/hc/en-gb/articles/1500005516462"
+    regex(/version\D*?(\d+(?:\.\d+)+)/i)
+  end
+
+  auto_updates false
+  conflicts_with cask: "logi-options-plus"
+  depends_on macos: ">= :catalina"
+
+  # see https://prosupport.logi.com/hc/en-us/articles/6046882446359
+  installer script: {
+    executable: "logioptionsplus_installer_offline.app/Contents/MacOS/logioptionsplus_installer",
+    args:       ["--quiet"],
+    sudo:       true,
+  }
+
+  uninstall launchctl: [
+              "com.logi.cp-dev-mgr",
+              "com.logi.optionsplus",
+              "com.logi.optionsplus.updater",
+            ],
+            quit:      [
+              "com.logi.cp-dev-mgr",
+              "com.logi.optionsplus",
+              "com.logi.optionsplus.driverhost",
+              "com.logi.optionsplus.updater",
+              "com.logitech.FirmwareUpdateTool",
+              "com.logitech.logiaipromptbuilder",
+            ],
+            delete:    [
+              "/Applications/logioptionsplus.app",
+              "/Applications/Utilities/Logi Options+ Driver Installer.bundle",
+              "/Library/Application Support/Logitech.localized/LogiOptionsPlus",
+            ],
+            rmdir:     "/Library/Application Support/Logitech.localized"
+
+  zap trash: [
+    "/Users/Shared/logi",
+    "/Users/Shared/LogiOptionsPlus",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.logi.optionsplus*.sfl*",
+    "~/Library/Application Support/LogiOptionsPlus",
+    "~/Library/Preferences/com.logi.cp-dev-mgr.plist",
+    "~/Library/Preferences/com.logi.optionsplus.driverhost.plist",
+    "~/Library/Preferences/com.logi.optionsplus.plist",
+    "~/Library/Saved Application State/com.logi.optionsplus.savedState",
+  ]
+
+  caveats do
+    reboot
+  end
+end
