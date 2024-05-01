310ea5dc05a2ac4516152d13974403cd4e43b3e1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-01 06:33:07 +0000

    chatall 1.78.103

--- Casks/c/chatall.rb
+++ Casks/c/chatall.rb
@@ -1,9 +1,9 @@
 cask "chatall" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.77.102"
-  sha256 arm:   "fecb88f685d242fc650cbcefa8ddd3c67f00a9d8d6c159e9c79d197ba2a65a1e",
-         intel: "63f6b7d6f50755123096d1813202e156b49c6e34f02b03d20c033bc61b95d032"
+  version "1.78.103"
+  sha256 arm:   "c9aaec64fc01410539dd315aa8742ba99edfc3afe36c5957d7303d3a3979393a",
+         intel: "54f166a46bc854f151934c019c489917dca571ef663d25a35ccc3c6584bda36a"
 
   url "https://github.com/sunner/ChatALL/releases/download/v#{version}/ChatALL-#{version}-mac-#{arch}.dmg"
   name "ChatALL"
