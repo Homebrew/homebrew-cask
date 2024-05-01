888db642743efbe1ca05dcd018be619d54cddbf1
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-05-01 18:30:03 -0400

    virtualbuddy: fix conflicts

--- Casks/v/virtualbuddy.rb
+++ Casks/v/virtualbuddy.rb
@@ -21,7 +21,7 @@ cask "virtualbuddy" do
   end
 
   auto_updates true
-  conflicts_with cask: "homebrew/cask-versions/virtualbuddy-beta"
+  conflicts_with cask: "virtualbuddy@beta"
   depends_on arch: :arm64
   depends_on macos: ">= :monterey"
 
