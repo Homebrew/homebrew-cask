b3995e931e787f772bc0d8775d98588b0ac4d71f
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-05-02 13:29:02 -0400

    semeru-jdk-open@17: migrate

---
+++ Casks/s/semeru-jdk-open@17.rb
@@ -0,0 +1,27 @@
+cask "semeru-jdk-open@17" do
+  arch arm: "aarch64", intel: "x64"
+
+  version "17.0.10+7,openj9-0.43.0"
+  sha256 arm:   "487806ddf6b36391d18040e3a5f83f33e2f1497d39bbe623458a18906770caf9",
+         intel: "96f6ae58eb3b76eb7fd0c3a9b63d94bbad5a895ebbc096ca1c2440b2177ce23c"
+
+  url "https://github.com/ibmruntimes/semeru#{version.major}-binaries/releases/download/jdk-#{version.csv.first}_#{version.csv.second}/ibm-semeru-open-jdk_#{arch}_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg",
+      verified: "github.com/ibmruntimes/semeru#{version.major}-binaries/"
+  name "IBM Semeru Runtime (JDK 17) Open Edition"
+  desc "Production-ready JDK with the OpenJDK class libraries and the Eclipse OpenJ9 JVM"
+  homepage "https://developer.ibm.com/languages/java/semeru-runtimes"
+
+  livecheck do
+    url :stable
+    regex(/^jdk[._-](\d+(?:[.+]\d+)*)[._-](.+?)$/i)
+    strategy :github_latest do |json, regex|
+      json["tag_name"]&.scan(regex)&.map { |match| "#{match[0]},#{match[1]}" }
+    end
+  end
+
+  pkg "ibm-semeru-open-jdk_#{arch}_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg"
+
+  uninstall pkgutil: "net.ibm-semeru-open.#{version.major}.jdk"
+
+  # No zap stanza required
+end
