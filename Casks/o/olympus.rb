91f4c8bf72442a263c24b9bc5598e367d96c1c7b
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-05-02 12:39:26 -0400

    olympus: migrate

---
+++ Casks/o/olympus.rb
@@ -0,0 +1,28 @@
+cask "olympus" do
+  version "4044"
+  sha256 "59c75c10dd251221bf6bcf98f87a86cdb52a91b6e54e9e3faf1a62f4c407ac1a"
+
+  url "https://dev.azure.com/EverestAPI/Olympus/_apis/build/builds/#{version}/artifacts?artifactName=macos.main&$format=zip",
+      verified: "dev.azure.com/EverestAPI/Olympus/_apis/build/builds/"
+  name "Olympus"
+  desc "Everest (Mod loader for video games Celeste) installer / manager"
+  homepage "https://everestapi.github.io/"
+
+  livecheck do
+    url "https://dev.azure.com/EverestAPI/Olympus/_apis/build/builds"
+    strategy :json do |json|
+      json["value"]&.map do |build|
+        build["id"].to_s if build["sourceBranch"] == "refs/heads/stable"
+      end
+    end
+  end
+
+  container nested: "macos.main/dist.zip"
+
+  app "Olympus.app"
+
+  zap trash: [
+    "~/Library/Application Support/Olympus",
+    "~/Library/Saved Application State/everest.olympus.savedState",
+  ]
+end
