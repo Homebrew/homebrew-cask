a5653427bc28abfad2424afa27e5c3dc00be9169
Daeho Ro <lamanus@outlook.kr>
2024-05-03 14:30:25 +0900

    cinc-workstation 24.4.1064

--- Casks/c/cinc-workstation.rb
+++ Casks/c/cinc-workstation.rb
@@ -1,9 +1,9 @@
 cask "cinc-workstation" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "24.2.1058"
-  sha256 arm:   "8be0c1b792302947adcbcf843a2710a331e0a52732455a7012fb99f10aee7569",
-         intel: "088dc9534e97cc191bd1eebca9185dbcb1d2b181ea39fdbbc18a2232ba080c46"
+  version "24.4.1064"
+  sha256 arm:   "4365418ae6f2387cc925fede5245bb40e8c7432629728a546bed625efe2f20bb",
+         intel: "85752c80079b9a9d2038bbdaba30b764f8dd2dfb23f62652e68592e9dea917ce"
 
   url "https://downloads.cinc.sh/files/stable/cinc-workstation/#{version}/mac_os_x/11/cinc-workstation-#{version}-1.#{arch}.dmg"
   name "Cinc Workstation"
