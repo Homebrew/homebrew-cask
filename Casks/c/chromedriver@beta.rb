3d38c3260fa237ffb92451666266a94198ade2cf
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-05-03 11:30:11 -0400

    chromedriver@beta 125.0.6422.26

--- Casks/c/chromedriver@beta.rb
+++ Casks/c/chromedriver@beta.rb
@@ -1,9 +1,9 @@
 cask "chromedriver@beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "125.0.6422.14"
-  sha256 arm:   "3fe6d38083e6f26e0b0f2d6c34111812512f2b8d1a87f8c023155ea99a423261",
-         intel: "2dcc0f090681addfa8562476b7781e445da67b157382409a24f127396cdb8fd3"
+  version "125.0.6422.26"
+  sha256 arm:   "d9f73cd1318eac9ed6e11d165931f16e18eee4eaadb523f95c2e9cacc35388fc",
+         intel: "643ea14b8bb3e295b9328aa174925c6907f2ae77fd15a7aea8d751bafbe6f14a"
 
   url "https://storage.googleapis.com/chrome-for-testing-public/#{version}/mac-#{arch}/chromedriver-mac-#{arch}.zip",
       verified: "storage.googleapis.com/chrome-for-testing-public/"
