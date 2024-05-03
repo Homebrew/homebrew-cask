cef699bd722a7b650ec98c21b76866f5a04d7a76
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-03 00:54:31 +0000

    wave 0.7.5

--- Casks/w/wave.rb
+++ Casks/w/wave.rb
@@ -1,6 +1,6 @@
 cask "wave" do
-  version "0.7.4"
-  sha256 "f7ce95cca1cfafa36560e4128aff9c29780ddc8e81f60f17525d608bb2ec7808"
+  version "0.7.5"
+  sha256 "0ab3c75455ee888e71d4cd291acafd6ca86872053bbe85f2ca4b73eeb2b09932"
 
   url "https://dl.waveterm.dev/releases/Wave-darwin-universal-#{version}.dmg"
   name "Wave Terminal"
