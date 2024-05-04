ae0ec21bab04965f423b7ee544073417ddf59bec
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-04 15:37:19 +0000

    restfox 0.14.0

--- Casks/r/restfox.rb
+++ Casks/r/restfox.rb
@@ -1,9 +1,9 @@
 cask "restfox" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.13.0"
-  sha256 arm:   "f480a9d44200490f8a0a11ceb58400c440ab0f5ed770452dbeb7293876b5dbdd",
-         intel: "e1d02515583e57f3f191b542d5eed5b21183a41c81ea092c06212b8e2042b517"
+  version "0.14.0"
+  sha256 arm:   "fd7d867d26d182b37d6cc8cc1c9fac1fd8859c58ed6c7a50c7e9809d72d0158a",
+         intel: "ecdece4485288c356a5b1aa8555171f0a64f708710463a8f1b83873d00700412"
 
   url "https://github.com/flawiddsouza/Restfox/releases/download/v#{version}/Restfox-darwin-#{arch}-#{version}.zip",
       verified: "github.com/flawiddsouza/Restfox/releases/download/"
