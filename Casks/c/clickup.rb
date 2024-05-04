6518c241fcea5d1733f823ee1fccde7a238a5213
Sam Ford <1584702+samford@users.noreply.github.com>
2024-05-04 09:21:33 -0400

    clickup 3.3.79
    The magic string in the URLs (e.g. `221003ra4tebclw`) seems to change
    over time and the `livecheck` block will consistently return an old
    version until the URL is updated. Hopefully this doesn't happen very
    often but it has made us miss updates for seven months at this point.

--- Casks/c/clickup.rb
+++ Casks/c/clickup.rb
@@ -1,22 +1,32 @@
 cask "clickup" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.3.57"
-  sha256 arm:   "b24b87c66d0e2bd60412c9cf4728ab7daf68c5b058576600cecdadf774d25935",
-         intel: "3dcbea893e1fb51b624c012a67271444a58d28822c136c6377feae0a66e16033"
+  version "3.3.79,240105hz1mbg8s0"
+  sha256 arm:   "fd0a88bc4396f35e10409a07f4e5576acbc3fd787ed5639e076c6517ff263e34",
+         intel: "3fad667921e5abf3e40ac70419a7e4565bd452b50c359c39817f970b37095ce7"
 
-  url "https://download.todesktop.com/210531zdwwjv8ke/ClickUp%20#{version}-#{arch}.dmg",
-      verified: "download.todesktop.com/210531zdwwjv8ke/"
+  url "https://download.todesktop.com/221003ra4tebclw/ClickUp%20#{version.csv.first}%20-%20Build%20#{version.csv.second}-#{arch}.dmg",
+      verified: "download.todesktop.com/221003ra4tebclw/"
   name "ClickUp"
   desc "Productivity platform for tasks, docs, goals, and chat"
   homepage "https://clickup.com/"
 
+  # NOTE: The magic string in the URL (e.g. `221003ra4tebclw`) may need to be
+  # updated over time, as the existing URL may only return an old version.
   livecheck do
-    url "https://download.todesktop.com/210531zdwwjv8ke/latest-mac.yml"
-    strategy :electron_builder
+    url "https://download.todesktop.com/221003ra4tebclw/latest-mac.yml"
+    regex(/ClickUp\s*v?(\d+(?:\.\d+)+).*?Build\s*([a-z0-9]+)[._-]#{arch}\.dmg/i)
+    strategy :electron_builder do |yaml|
+      yaml["files"]&.map do |item|
+        match = item["url"]&.match(regex)
+        next if match.blank?
+
+        "#{match[1]},#{match[2]}"
+      end
+    end
   end
 
-  depends_on macos: ">= :high_sierra"
+  depends_on macos: ">= :catalina"
 
   app "ClickUp.app"
 
