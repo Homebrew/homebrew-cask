d270356a2ecdc345948167865df2fea38176ea9b
Aaron Rumpler <80610725+Aaron-Rumpler@users.noreply.github.com>
2024-05-05 00:54:23 +1200

    msty 0.7.1 (new cask)

---
+++ Casks/m/msty.rb
@@ -0,0 +1,31 @@
+cask "msty" do
+  arch arm: "arm64", intel: "x64"
+
+  version "0.7.1"
+  sha256 :no_check
+
+  url "https://assets.msty.app/Msty_#{arch}.dmg"
+  name "Msty"
+  desc "Run LLMs locally"
+  homepage "https://msty.app/"
+
+  livecheck do
+    url "https://assets.msty.app/latest-mac.yml"
+    strategy :electron_builder
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
+  app "Msty.app"
+
+  uninstall quit: "app.msty.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/app.msty.app.sfl*",
+    "~/Library/Application Support/Msty",
+    "~/Library/Logs/Msty",
+    "~/Library/Preferences/app.msty.app.plist",
+    "~/Library/Saved Application State/app.msty.app.savedState",
+  ]
+end
