7280b6a54676813fbc651ebe8331259f2d18897d
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2024-05-04 11:34:29 +0200

    Update and rename vleer.rb to vleer@beta.rb

--- Casks/v/vleer@beta.rb
+++ Casks/v/vleer@beta.rb
@@ -1,5 +1,5 @@
-cask "vleer" do
-  version "1.0.0-pre.3"
+cask "vleer@beta" do
+  version "1.0.0-beta.3"
   sha256 "08adac24441a6ba9409209a2c4788ae8e8c9e25c04e5535d1184cf3024d7cf13"
 
   url "https://github.com/Vleerapp/Vleer/releases/download/v#{version}/Vleer_#{version}_aarch64.dmg"
