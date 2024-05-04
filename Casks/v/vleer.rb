cd1a04bb7b75447884fb35b6b925456f25d0f0c8
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2024-05-04 11:28:14 +0200

    Create vleer.rb

---
+++ Casks/v/vleer.rb
@@ -0,0 +1,11 @@
+cask "vleer" do
+  version "1.0.0-pre.3"
+  sha256 "08adac24441a6ba9409209a2c4788ae8e8c9e25c04e5535d1184cf3024d7cf13"
+
+  url "https://github.com/Vleerapp/Vleer/releases/download/v#{version}/Vleer_#{version}_aarch64.dmg"
+  name "Vleer"
+  desc "A music app from the future"
+  homepage "https://vleer.app"
+
+  app "Vleer.app"
+end
