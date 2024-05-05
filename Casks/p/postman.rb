e0290181d53819a48ac339ffe7dc496da29c6af8
WangEdward <73746306+WangEdward@users.noreply.github.com>
2024-05-05 21:09:19 +0800

    postman 11.0.4

--- Casks/p/postman.rb
+++ Casks/p/postman.rb
@@ -1,9 +1,9 @@
 cask "postman" do
   arch arm: "osx_arm64", intel: "osx64"
 
-  version "10.24.16"
-  sha256 arm:   "9a1292ca81e8401e8180ccd659871f0fd3a4dd601d1b000d902db90342bb68eb",
-         intel: "65f4cbaebcca8965d77b5591d59dbe7461d5da35f4912c97cacb71a521fde747"
+  version "11.0.4"
+  sha256 arm:   "fa10b48681ffa6ad64ccb4258074a81ee73afc8f5aba708a6705ed30e2b6d691",
+         intel: "d6389fff61b8b7ee170e0178a8d23ccf06a7f93628a30a759808b3bbb2e85125"
 
   url "https://dl.pstmn.io/download/version/#{version}/#{arch}",
       verified: "dl.pstmn.io/download/version/"
