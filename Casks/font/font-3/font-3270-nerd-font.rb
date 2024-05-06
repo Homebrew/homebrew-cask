b3a69dc2cd6b4f096c0a853f1795d728e37fb290
Patrick Linnane <patrick@linnane.io>
2024-05-06 10:39:06 -0700

    various casks (#): migrate
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

---
+++ Casks/font/font-3/font-3270-nerd-font.rb
@@ -0,0 +1,26 @@
+cask "font-3270-nerd-font" do
+  version "3.2.1"
+  sha256 "0e7cffe10575de3d80df920065ea6f320e86cd515ddd16beed817d284ba44afc"
+
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/3270.zip"
+  name "3270 Nerd Font (IBM 3270)"
+  desc "Developer targeted fonts with a high number of glyphs"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  font "3270NerdFont-Condensed.ttf"
+  font "3270NerdFont-Regular.ttf"
+  font "3270NerdFont-SemiCondensed.ttf"
+  font "3270NerdFontMono-Condensed.ttf"
+  font "3270NerdFontMono-Regular.ttf"
+  font "3270NerdFontMono-SemiCondensed.ttf"
+  font "3270NerdFontPropo-Condensed.ttf"
+  font "3270NerdFontPropo-Regular.ttf"
+  font "3270NerdFontPropo-SemiCondensed.ttf"
+
+  # No zap stanza required
+end
