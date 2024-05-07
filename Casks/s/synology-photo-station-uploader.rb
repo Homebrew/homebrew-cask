6a50bb5090bdf2e0e6425db55c90c8908fc0b146
Eason Lu <aelnosu@outlook.com>
2024-05-07 14:11:54 -0700

    synology-photo-station-uploader: disable Disable synology-photo-station-uploader because it is no longer aviable on the  official website.

--- Casks/s/synology-photo-station-uploader.rb
+++ Casks/s/synology-photo-station-uploader.rb
@@ -6,37 +6,5 @@ cask "synology-photo-station-uploader" do
   name "Synology Photo Station Uploader"
   desc "Bulk upload photos and videos to Synology Photo Station"
   homepage "https://www.synology.com/"
-
-  livecheck do
-    url "https://www.synology.com/en-us/releaseNote/PhotoStationUploader"
-    strategy :patch_match do |page|
-      match = page.match(/<h3>Version:\s(\d+(?:\.\d+)+)-(\d+)/i)
-
-      "#{match[1]},#{match[2]}"
-    end
-  end
-
-  pkg "SynologyPhotoStationUploader-#{version.after_comma}-Mac-Installer.pkg"
-
-  uninstall launchctl: [
-              "com.synology.PhotoUploaderFinderSync",
-              "com.synology.PhotoUploaderShellApp",
-              "com.synology.PhotoUploaderUninstaller",
-              "com.synology.SynoSIMBL_RefreshFinder",
-            ],
-            quit:      "com.synology.PhotoStationUploader",
-            pkgutil:   [
-              "com.synology.photostationuploader.installer",
-              "inc.synology.photostationuploader",
-            ]
-
-  zap trash: [
-        "~/Library/Application Scripts/com.synology.PhotoUploaderShellApp.PhotoUploaderFinderSync",
-        "~/Library/Application Scripts/group.com.synology.PhotoUploader",
-        "~/Library/Application Support/Synology/Photo Station Uploader",
-        "~/Library/Containers/com.synology.PhotoUploaderShellApp.PhotoUploaderFinderSync",
-        "~/Library/Group Containers/group.com.synology.PhotoUploader",
-        "~/Library/Saved Application State/com.synology.PhotoStationUploader.savedState",
-      ],
-      rmdir: "~/Library/Application Support/Synology"
+  disable! date: "2024-05-07", because: :no_longer_available
 end
