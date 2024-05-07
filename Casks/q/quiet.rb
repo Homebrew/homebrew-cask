57465900c2e4d48c7cf879d2ac6e98fc3c4c6674
Richard Torres <richardtorres314@gmail.com>
2024-05-07 15:23:50 -0400

    quiet 2.1.2 (new cask)
    This commit fixes problems with livecheck audit

--- Casks/q/quiet.rb
+++ Casks/q/quiet.rb
@@ -2,7 +2,7 @@ cask "quiet" do
   version "2.1.2"
   sha256 "f3fbe187e6710bdda02172777173d0340dc5a97a502d07c3c817b2f7a2897555"
 
-  url "https://github.com/TryQuiet/quiet/releases/download/%40quiet%2Fdesktop%40#{version}/Quiet-#{version}.dmg",
+  url "https://github.com/TryQuiet/quiet/releases/download/@quiet/desktop@#{version}/Quiet-#{version}.dmg",
       verified: "github.com/TryQuiet/quiet/"
   name "Quiet"
   desc "A private, p2p alternative to Slack and Discord built on Tor & IPFS"
@@ -13,7 +13,7 @@ cask "quiet" do
   # releases to match the latest stable desktop version.
   livecheck do
     url :url
-    regex(/^@quiet\/desktop@\d+(?:.\d+)+$/i)
+    regex(/^@quiet\/desktop@(\d+(?:\.\d+)+)$/i)
     strategy :github_releases do |json, regex|
       json.map do |release|
         next if release["draft"] || release["prerelease"]
