c2f4b61ed0de3c44d76ef931c6878db587e9e7cd
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-05-07 03:05:10 +0300

    Update todesk.rb

--- Casks/t/todesk.rb
+++ Casks/t/todesk.rb
@@ -1,6 +1,6 @@
 cask "todesk" do
   version "4.7.2.1"
-  sha256 :no_check # required as upstream package is updated in-place
+  sha256 "cba50ad3b10523f71175953d4606d3daf122d344dd1e5f130fd190a93ba509e8"
 
   url "https://dl.todesk.com/macos/ToDesk_#{version}.pkg"
   name "ToDesk"
