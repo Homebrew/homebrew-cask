a995b88a53ae8ca83994e73ee5911af99a74f6de
Richard Torres <richardtorres314@gmail.com>
2024-05-07 15:24:50 -0400

    quiet 2.1.2 (new cask)
    This commit fixes styling for cask formula

--- Casks/q/quiet.rb
+++ Casks/q/quiet.rb
@@ -5,7 +5,7 @@ cask "quiet" do
   url "https://github.com/TryQuiet/quiet/releases/download/@quiet/desktop@#{version}/Quiet-#{version}.dmg",
       verified: "github.com/TryQuiet/quiet/"
   name "Quiet"
-  desc "A private, p2p alternative to Slack and Discord built on Tor & IPFS"
+  desc "Private, p2p alternative to Slack and Discord built on Tor & IPFS"
   homepage "https://tryquiet.org/"
 
   # Upstream creates GitHub releases for both stable and alpha versions for
@@ -13,7 +13,7 @@ cask "quiet" do
   # releases to match the latest stable desktop version.
   livecheck do
     url :url
-    regex(/^@quiet\/desktop@(\d+(?:\.\d+)+)$/i)
+    regex(%r{^@quiet/desktop@(\d+(?:\.\d+)+)$}i)
     strategy :github_releases do |json, regex|
       json.map do |release|
         next if release["draft"] || release["prerelease"]
