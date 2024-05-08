32b03345e902268372333a3cdb4e98b5e0464f87
Sam Ford <1584702+samford@users.noreply.github.com>
2024-05-07 20:38:40 -0400

    shift: update homepage, livecheck

--- Casks/s/shift.rb
+++ Casks/s/shift.rb
@@ -5,13 +5,14 @@ cask "shift" do
   sha256 arm:   "ed9648e7b1c62d54a20b43ec1a4dfc19a7f5a5af8ef2b91e271d9aafaa555a63",
          intel: "b2ad6011b0981c7d840495687bf73e683570781c7f9c36f7ae09d76d703255e8"
 
-  url "https://updates.tryshift.com/v#{version.major_minor_patch}/stable/shift-v#{version}-stable-#{arch}.dmg"
+  url "https://updates.tryshift.com/v#{version.major_minor_patch}/stable/shift-v#{version}-stable-#{arch}.dmg",
+      verified: "updates.tryshift.com/"
   name "Shift"
   desc "Workstation to streamline your accounts, apps, and workflows"
-  homepage "https://tryshift.com/"
+  homepage "https://shift.com/"
 
   livecheck do
-    url "https://tryshift.com/download/?platform=mac"
+    url "https://shift.com/download/?platform=mac"
     regex(/href=.*?(\d+(?:[._-]\d+)+)[._-]stable.*?\.dmg/i)
   end
 
