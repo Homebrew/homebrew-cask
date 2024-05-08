e163f85c60db5fbd809ef1dc128e009c373aa9e7
Bevan Kay <email@bevankay.me>
2024-05-08 20:49:24 +1000

    decentr 1.6.0

--- Casks/d/decentr.rb
+++ Casks/d/decentr.rb
@@ -1,9 +1,9 @@
 cask "decentr" do
   arch arm: "arm", intel: "x86"
 
-  version "1.5.12"
-  sha256 arm:   "f234db7ae4664752ca5248ccc05b75b387a14e0a83859efd0ce4bea155cd37d3",
-         intel: "4985e9c8d5cb49b8d495a56ef8713afc8f972584b57ddc031b9c0a75f830ce81"
+  version "1.6.0"
+  sha256 arm:   "f109ed171afd6a7b4f24661af5d25d6116f9edc3969a55d5ceb0c3e9fb76bc39",
+         intel: "7e58ee967118e376eeb38ec1bc018143778bbf664a7a6cbc69a4c89224a97c03"
 
   url "https://decentr.net/MacOS_#{arch}_Decentr_#{version}.dmg"
   name "Decentr"
