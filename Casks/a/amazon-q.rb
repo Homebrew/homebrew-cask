d6f924d751aba4728ee7c2eaa6eb9a508e42364d
Bevan Kay <email@bevankay.me>
2024-05-08 19:56:04 +1000

    amazon-q: rename `codewhisperer` to `amazon-q`

---
+++ Casks/a/amazon-q.rb
@@ -0,0 +1,30 @@
+cask "amazon-q" do
+  version "1.0.2"
+  sha256 "e5aa6653d27a67fe0389d4347487dfd0e2e96ec4f441c1f4635219215d6395bd"
+
+  url "https://desktop-release.codewhisperer.us-east-1.amazonaws.com/#{version}/Amazon%20Q.dmg",
+      verified: "desktop-release.codewhisperer.us-east-1.amazonaws.com/"
+  name "Amazon Q"
+  desc "AI-powered productivity tool for the command-line"
+  homepage "https://aws.amazon.com/q/developer/"
+
+  livecheck do
+    url "https://desktop-release.codewhisperer.us-east-1.amazonaws.com/index.json"
+    strategy :json do |json|
+      json["versions"].map { |item| item["version"] }
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "Amazon Q.app"
+
+  zap trash: [
+    "~/Library/Application Support/codewhisperer",
+    "~/Library/Caches/com.amazon.codewhisperer",
+    "~/Library/LaunchAgents/com.amazon.codewhisperer.launcher.plist",
+    "~/Library/Preferences/com.amazon.codewhisperer.plist",
+    "~/Library/WebKit/com.amazon.codewhisperer",
+  ]
+end
