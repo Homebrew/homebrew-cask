98b84794a664de71780603a7f941bf350762885f
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-05-08 09:18:43 -0400

    yam-display 2.4.5

--- Casks/y/yam-display.rb
+++ Casks/y/yam-display.rb
@@ -1,6 +1,6 @@
 cask "yam-display" do
-  version "2.4.3"
-  sha256 "d65d2711ab57e01866f19fc89b4a6a448a30bec2bf309d6b66138778ccf1fea0"
+  version "2.4.5"
+  sha256 "eb74a1ccd76fb0f920a1a2e1096155da2b92f0cfebfcac76a97c8e17725fe63c"
 
   url "https://www.yamdisplay.com/app/YamDisplay#{version}.zip"
   name "Yam Display"
