d2aba7452ec8f313ab9045bc993fade04d38363e
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-05-08 09:19:36 -0400

    wowup 2.12.0

--- Casks/w/wowup.rb
+++ Casks/w/wowup.rb
@@ -1,9 +1,9 @@
 cask "wowup" do
   arch arm: "-arm64"
 
-  version "2.11.0"
-  sha256 arm:   "7ea8b3bbcf554c3abfbb38fc5db89c4147f3d3f12068c2b0dbc32452f2c33412",
-         intel: "90847e6dfe032d1a52e95f2397034fd119f4c0369bdbd90fe0a4c4b1ac9829ad"
+  version "2.12.0"
+  sha256 arm:   "689befa5cb69078f0d15e3998ac5ecefb985530b004f9d7c1a1086cff301d8b2",
+         intel: "ef7f1f12ebb01ed0e69a710b2beb5d0eed8470cf62fe28ecbfdd31f4ef62020c"
 
   url "https://github.com/WowUp/WowUp/releases/download/v#{version}/WowUp-#{version}#{arch}.dmg",
       verified: "github.com/WowUp/WowUp/"
