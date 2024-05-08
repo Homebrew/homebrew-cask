25eb7eec558d0e563e2878208296e34088cb0455
Bevan Kay <email@bevankay.me>
2024-05-08 21:05:51 +1000

    fujifilm-x-raw-studio: update `livecheck`, `name`

--- Casks/f/fujifilm-x-raw-studio.rb
+++ Casks/f/fujifilm-x-raw-studio.rb
@@ -3,12 +3,12 @@ cask "fujifilm-x-raw-studio" do
   sha256 "a6f72c8ca5bbe83c5a6d2bab0c64c611112c195e617518c075b2530ac065a638"
 
   url "https://dl.fujifilm-x.com/support/software/x-raw-studio-mac#{version.no_dots}-p2ep21rl/XRawStudio#{version.no_dots}.dmg"
-  name "fujifilm-x-raw-studio"
+  name "FUJIFILM X RAW STUDIO"
   desc "Convert RAW images captured with Fujifilm cameras"
   homepage "https://fujifilm-x.com/global/products/software/x-raw-studio/"
 
   livecheck do
-    url "https://fujifilm-x.com/en-us/support/download/software/x-raw-studio/"
+    url "https://fujifilm-x.com/global/support/download/software/x-raw-studio/"
     regex(/Mac\s*Version\s*:\s*v?(\d+(?:\.\d+)+)/i)
   end
 
