e6f061c1687fbd95f2538f84f223d86eb6bbe139
Bevan Kay <email@bevankay.me>
2024-05-08 22:11:33 +1000

    gitfox 3.9.0

--- Casks/g/gitfox.rb
+++ Casks/g/gitfox.rb
@@ -1,6 +1,6 @@
 cask "gitfox" do
-  version "3.5.0,8092"
-  sha256 "756af41e4b5f53ee63f5b9f44c52ae7b8b8fc27cd2f52af24e7367257b7bf9a6"
+  version "3.9.0,9402"
+  sha256 "0380671cb27d53849da017b24715077814b21264e3fef05b2befb2026a74c553"
 
   url "https://storage.googleapis.com/gitfox/builds/retail/#{version.csv.second}/Gitfox.#{version.csv.second}.zip",
       verified: "storage.googleapis.com/gitfox/"
