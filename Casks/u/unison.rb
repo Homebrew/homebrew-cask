f1aafcd80e50f63186faccf451c989adf4bd66b2
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-05-08 12:48:28 -0400

    unison 2.53.5
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>
    
    unison 2.53.5

--- Casks/u/unison.rb
+++ Casks/u/unison.rb
@@ -1,8 +1,11 @@
 cask "unison" do
-  version "2.53.4"
-  sha256 "1c3e18eefcd63ced1cd4bf43015cb6c0adac9824466de2c5cf7d598b3a8901fb"
+  arch arm: "arm64", intel: "x86_64"
 
-  url "https://github.com/bcpierce00/unison/releases/download/v#{version}/Unison-#{version}-macos.app.tar.gz",
+  version "2.53.5"
+  sha256 arm:   "545b3b0818e671b7f966cd2f8d61df71b79f07a6cf7f180b969ddf26d50a0931",
+         intel: "ff2ab52930f6642a8ca4c903a75ec6341ca8e886701819a1c39c678dfd492228"
+
+  url "https://github.com/bcpierce00/unison/releases/download/v#{version}/Unison-#{version}-macos-#{arch}.app.tar.gz",
       verified: "github.com/bcpierce00/unison/"
   name "Unison"
   desc "File synchroniser"
