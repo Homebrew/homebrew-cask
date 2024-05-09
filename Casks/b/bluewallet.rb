0a5f331c27762ab9e68a500a44f012523c31d428
Marcos Rodriguez Vélez <marcospr@pm.me>
2024-05-09 13:36:01 -0400

    Update bluewallet.rb

--- Casks/b/bluewallet.rb
+++ Casks/b/bluewallet.rb
@@ -1,6 +1,6 @@
 cask "bluewallet" do
-  version "6.6.3"
-  sha256 "9d1356e2c831689cf731dbf2c0c3873dd843c334442fced1d69745badc0d0d20"
+  version "6.6.5"
+  sha256 "0ec38faf32cdf32afe6c9750bfd87b404a4916ea4db51ec5413183ec544d6b3d"
 
   url "https://github.com/BlueWallet/BlueWallet/releases/download/v#{version}/BlueWallet.#{version}.dmg",
       verified: "github.com/BlueWallet/BlueWallet/"
