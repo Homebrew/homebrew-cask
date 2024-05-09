b11b3d1d44d9ad7e2aefa695832b9981b54652fa
Bevan Kay <email@bevankay.me>
2024-05-09 11:20:59 +1000

    rotki 1.33.0

--- Casks/r/rotki.rb
+++ Casks/r/rotki.rb
@@ -1,9 +1,9 @@
 cask "rotki" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.32.2"
-  sha256 arm:   "ef378cb6703ecde86bbfdd3f485e75aa396248fc806ef08214074098e28867ba",
-         intel: "c4c0ad9b2eda72586e21289dd3e2909255fe656b5c20042b011608ac06f8c654"
+  version "1.33.0"
+  sha256 arm:   "14c094ecd42ba7b48c9c5e133d27e81ea687bcacb1996b362bf24862d0f840e1",
+         intel: "88f414943b0c40eaeedc835afb186f9c1add13b629342b54480dafd0ee4802a7"
 
   url "https://github.com/rotki/rotki/releases/download/v#{version}/rotki-darwin_#{arch}-v#{version}.dmg",
       verified: "github.com/rotki/rotki/"
