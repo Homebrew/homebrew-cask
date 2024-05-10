c5e346d568cb723181d478fdb95204cd1525ae50
Klaus Hipp <khipp@users.noreply.github.com>
2024-05-10 14:03:55 +0200

    front 3.52.5

--- Casks/f/front.rb
+++ Casks/f/front.rb
@@ -1,9 +1,9 @@
 cask "front" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.51.5"
-  sha256 arm:   "ce0a58dd4e8f8cf0fe67680e69ebd8d9db8e73ccaf3c56565453a2bd8e46ea8f",
-         intel: "d07be8306761e916343c4f9dd30e75fb298ab01cd8f3c694614230512b72292d"
+  version "3.52.5"
+  sha256 arm:   "991f1b151372197b5c9f840bf1ff98924734e3f00ae6b5d3226630e2b40f64b6",
+         intel: "5f660da08ddccdc80bd91964ace84417cdcd97376e6496afebbb94dd0aa6b25e"
 
   url "https://dl.frontapp.com/desktop/builds/#{version}/Front-#{version}-#{arch}.zip",
       verified: "dl.frontapp.com/desktop/builds/"
