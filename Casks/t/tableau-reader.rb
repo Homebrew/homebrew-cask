ca778b82c4e2033600fdd01d24c275edb37e4613
Teko <112829523+Teko012@users.noreply.github.com>
2024-05-11 00:15:18 +0200

    tableau-reader 2024.1.2

--- Casks/t/tableau-reader.rb
+++ Casks/t/tableau-reader.rb
@@ -1,19 +1,17 @@
 cask "tableau-reader" do
-  version "2024.1.1"
-  sha256 "d4f57fe4005e93a6f080a3321628a0c5d454d15e7e0a46b32c6012effd983ae3"
+  version "2024.1.2"
+  sha256 "196298aa2a671319d852f399bcb65561639fc4ce296a6de5d4c1bc3ec3ac21eb"
 
-  url "https://downloads.tableau.com/tssoftware/TableauReader-#{version.dots_to_hyphens}.dmg"
+  url "https://downloads.tableau.com/tssoftware/TableauReader-#{version.dots_to_hyphens}.dmg",
+      user_agent: "curl/8.7.1"
   name "Tableau Reader"
   desc "Open and interact with data visualisations built in Tableau Desktop"
   homepage "https://www.tableau.com/products/reader"
 
   livecheck do
-    url "https://www.tableau.com/downloads/reader/mac"
-    regex(/-(\d+(?:-\d+)+)\.dmg/i)
-    strategy :header_match do |headers, regex|
-      headers["location"].scan(regex).map do |match|
-        match[0].tr("-", ".").to_s
-      end
+    url "https://downloads.tableau.com/TableauAutoUpdate.xml"
+    strategy :xml do |xml|
+      xml.get_elements("//version").map { |item| item.attributes["releaseNotesVersion"] }
     end
   end
 
