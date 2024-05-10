ce72f1f649d5e9d4c12dd73e9303417aa9983b3e
Henry Chu <henrytech@outlook.com>
2024-05-10 09:11:32 +0800

    swiftcord 0.7.1

--- Casks/s/swiftcord.rb
+++ Casks/s/swiftcord.rb
@@ -1,16 +1,11 @@
 cask "swiftcord" do
-  version "0.6.1"
-  sha256 "6da79f88930ce384a58c3fd902f75ad64ef0b9e015aa547db6a85af7d46b2032"
+  version "0.7.1"
+  sha256 "36d9f9fbed2ab3e124c69e7ef16b5f3f6979fa6c576e6ad005ff34c1738a2961"
 
-  url "https://github.com/SwiftcordApp/Swiftcord/releases/download/v#{version}/Swiftcord.#{version}.dmg"
+  url "https://github.com/SwiftcordApp/Swiftcord/releases/download/v#{version}/Swiftcord.zip"
   name "Swiftcord"
   desc "Native Discord client built in Swift"
   homepage "https://github.com/SwiftcordApp/Swiftcord"
-
-  livecheck do
-    url "https://raw.githubusercontent.com/SwiftcordApp/Swiftcord/main/appcast.xml"
-    strategy :sparkle, &:short_version
-  end
 
   depends_on macos: ">= :monterey"
 
