943ccd72473c17edb315312deea1154f18b335a8
Klaus Hipp <khipp@users.noreply.github.com>
2024-05-10 14:48:50 +0200

    vellum 3.7.2

--- Casks/v/vellum.rb
+++ Casks/v/vellum.rb
@@ -1,6 +1,6 @@
 cask "vellum" do
-  version "3.7.1,37100"
-  sha256 "23c1697607b1ce99c087f4c65c1af59edef73963a5d7bb650a0f5e3a8353b5b8"
+  version "3.7.2,37200"
+  sha256 "7f294d02f90172252dd4f250707e877fc1432530a8f7bf2f964f3200aab66d6a"
 
   url "https://180g.s3.amazonaws.com/downloads/Vellum-#{version.csv.second}.zip",
       verified: "180g.s3.amazonaws.com/downloads/"
