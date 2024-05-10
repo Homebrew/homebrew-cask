7cbb40dcd91eb49ae7fcbb1efd68bff40745f91b
hussainweb <hussainweb@gmail.com>
2024-05-10 11:04:54 -0400

    Fix issues with URL

--- Casks/t/tinymediamanager.rb
+++ Casks/t/tinymediamanager.rb
@@ -5,14 +5,14 @@ cask "tinymediamanager" do
   sha256 arm:   "8ed75f5381bf1978c28730ee8db9f6c504401f57a84c7c972eb58af45b3beeb4",
          intel: "fc690cb7e2ba33b6b93de18691330ca36f6fac4e1bae60bcbfff59e28fe9024f"
 
-  url "https://release.tinymediamanager.org/v#{version.major}/dist/tinyMediaManager_#{version}_macos-x86_64.dmg"
+  url "https://release.tinymediamanager.org/v#{version.major}/dist/tinyMediaManager-#{version}-macos-#{arch}.dmg"
   name "tinyMediaManager"
   desc "Media management tool"
   homepage "https://www.tinymediamanager.org/"
 
   livecheck do
     url "https://release.tinymediamanager.org/"
-    regex(%r{href=.*?/tinyMediaManager[._-]v?(\d+(?:\.\d+)+)[._-]macos[._-]x86[._-]64\.dmg}i)
+    regex(%r{href=.*?v?(\d+(?:\.\d+)+)[._-]macos[._-]x86[._-]64\.dmg}i)
   end
 
   auto_updates true
