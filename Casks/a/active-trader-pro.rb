fd311fdbed2e770814b1aa809606add5ffe4599d
Karim Benbourenane <karim@benbourenane.com>
2024-05-11 13:38:42 -0400

    Update active-trader-pro package resource location
    Fidelity has changed the URL for Active Trader Pro to be an unversioned
    link, and at the same time has switched from using a zip archive to
    using a pkg installer.

--- Casks/a/active-trader-pro.rb
+++ Casks/a/active-trader-pro.rb
@@ -1,8 +1,8 @@
 cask "active-trader-pro" do
   version "1.0.55"
-  sha256 "394689bce9630cd949a40ae9914533acaf028f8d08019833dbb7a49f3416866e"
+  sha256 :no_check
 
-  url "https://www.fidelity.com/webcontent/Codeweaver/activetrader-#{version}.zip"
+  url "https://www.fidelity.com/webcontent/CodeweaverInstaller/ActiveTraderPro.pkg"
   name "Active Trader Pro"
   desc "Trading platform"
   homepage "https://www.fidelity.com/trading/advanced-trading-tools/active-trader-pro/overview"
@@ -14,7 +14,7 @@ cask "active-trader-pro" do
 
   depends_on macos: ">= :sierra"
 
-  app "Active Trader Pro.app"
+  pkg "ActiveTraderPro.pkg"
 
   uninstall quit: "com.fmr.activetrader"
 
