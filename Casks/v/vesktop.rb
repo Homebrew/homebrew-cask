06a3af6e7a8fc62217d536eae6ac951f46911bcf
Lucien Thany-Eynard <lthany@Luciens-MacBook-Pro.local>
2024-05-11 19:25:53 +0200

    Corrected some style errors

--- Casks/v/vesktop.rb
+++ Casks/v/vesktop.rb
@@ -1,13 +1,12 @@
 cask "vesktop" do
   version "1.5.2"
-  sha256 on_arch_conditional arm: "37dab286677c3a07d3488631be6d3112159e5ab16641737a3dbbfb1b77eb286a",
-    intel: "feed86e6ff7cf850087e0ed432ae6f5059983b64e3e5988068badda9474e300a"
+  sha256 on_arch_conditional arm:   "37dab286677c3a07d3488631be6d3112159e5ab16641737a3dbbfb1b77eb286a",
+                             intel: "feed86e6ff7cf850087e0ed432ae6f5059983b64e3e5988068badda9474e300a"
 
-  url on_arch_conditional arm: "https://github.com/Vencord/Vesktop/releases/download/v#{version}/Vesktop-#{version}-arm64.dmg",
-    intel: "https://github.com/Vencord/Vesktop/releases/download/v#{version}/Vesktop-#{version}.dmg"
-
+  url on_arch_conditional arm:   "https://github.com/Vencord/Vesktop/releases/download/v#{version}/Vesktop-#{version}-arm64.dmg",
+                          intel: "https://github.com/Vencord/Vesktop/releases/download/v#{version}/Vesktop-#{version}.dmg"
   name "Vesktop"
-  desc "Vesktop gives you the performance of web Discord and the comfort of Discord Desktop"
+  desc "Gives you the performance of web Discord and the comfort of Discord Desktop"
   homepage "https://github.com/Vencord/Vesktop"
 
   livecheck do
@@ -23,11 +22,10 @@ cask "vesktop" do
   zap trash: [
     "/private/var/folders/2k/qp_mlc_17rl8lp1rljgkdbfm0000gn/C/dev.vencord.vesktop",
     "/private/var/folders/2k/qp_mlc_17rl8lp1rljgkdbfm0000gn/C/dev.vencord.vesktop.helper.GPU",
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/dev.vencord.vesktop.sfl3",
-    "~/Library/Logs/Deezer",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/dev.vencord.vesktop.sfl*",
     "~/Library/Application Support/vesktop",
+    "~/Library/Logs/Deezer",
     "~/Library/Preferences/dev.vencord.vesktop.plist",
     "~/Library/Saved Application State/dev.vencord.vesktop.savedState",
   ]
 end
-
