5aadaaf1d1df0f65c3f9d943497bf3b9610a73ce
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-13 09:29:54 +0000

    badlion-client 4.3.0

--- Casks/b/badlion-client.rb
+++ Casks/b/badlion-client.rb
@@ -1,9 +1,9 @@
 cask "badlion-client" do
   arch arm: "-arm64"
 
-  version "4.2.0"
-  sha256 arm:   "0224e12ac2cdf491334ea492ca5f2163441dcd02c301e213714cf590b1c56209",
-         intel: "e68263ae153ff1463e7f2b1a6fa2d92cc48cbef9d84abb7699249687201458df"
+  version "4.3.0"
+  sha256 arm:   "c7e1447b4cd98210f06d40bc6697903b1b3e497c88fc43fa5f189f1060392334",
+         intel: "66a7ac7c83e6ffe3040a3ca0be9d2d1ba13ca7abbd2700c66e2fbcff719a04b0"
 
   url "https://client-updates.badlion.net/Badlion%20Client-#{version}#{arch}.dmg"
   name "Badlion Client"
