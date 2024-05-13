b4ea39b895f4f8f93b40a2ccfa917337633745de
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-05-13 10:15:06 -0400

    bambu-studio 01.09.01.66

--- Casks/b/bambu-studio.rb
+++ Casks/b/bambu-studio.rb
@@ -1,6 +1,6 @@
 cask "bambu-studio" do
-  version "01.08.04.51,20240117164301"
-  sha256 "a0ac2c1742751b0b132748489a1c20ac7dc3241732a38c5d9342f051d231e8ac"
+  version "01.09.01.66,20240511203248"
+  sha256 "7d0ab21dd5ccdb8247551fc22bde81bbdff9673225bc417485c521c5509a9d8f"
 
   url "https://github.com/bambulab/BambuStudio/releases/download/v#{version.csv.first}/Bambu_Studio_mac-v#{version.csv.first}-#{version.csv.second}.dmg",
       verified: "github.com/bambulab/BambuStudio/"
