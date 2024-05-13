cf0e501b2447d01003ac297f90e5b2288c832e1f
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-05-13 10:15:00 -0400

    dropzone 4.80.8

--- Casks/d/dropzone.rb
+++ Casks/d/dropzone.rb
@@ -1,6 +1,6 @@
 cask "dropzone" do
-  version "4.80.7"
-  sha256 "6c90872c1ef3e8bbf493516e980618fec0f11a8261a6fc5bd64f77f13ab9b78c"
+  version "4.80.8"
+  sha256 "d8f39fec1e4d03e16f204eda25d4914fa6d1ca3b2739d56f42562b6a9201943b"
 
   url "https://aptonic.com/releases/Dropzone-#{version}.zip"
   name "Dropzone"
