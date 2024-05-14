c9d34d7542b53c6282e45ba842b8e2510e7a1dae
Aaron Ruan <aaron212cn@outlook.com>
2024-05-14 10:53:45 +0800

    add chatgpt.rb

---
+++ Casks/c/chatgpt.rb
@@ -0,0 +1,24 @@
+cask "chatgpt" do
+  version :latest
+  sha256 :no_check
+
+  url "https://persistent.oaistatic.com/sidekick/public/ChatGPT_Desktop_public_latest.dmg",
+      verified: "persistent.oaistatic.com/sidekick/public/"
+  name "ChatGPT"
+  desc "OpenAI official ChatGPT desktop app for GPT-4o / GPT-3.5"
+  homepage "https://chatgpt.com/"
+
+  depends_on macos: ">= :sonoma"
+
+  app "ChatGPT.app"
+
+  uninstall quit: "com.openai.chat"
+
+  zap trash: [
+    "~/Library/Application Support/com.openai.chat",
+    "~/Library/Caches/com.openai.chat",
+    "~/Library/HTTPStorages/com.openai.chat",
+    "~/Library/Preferences/com.openai.chat.plist",
+    "~/Library/Saved Application State/com.openai.chat.savedState",
+  ]
+end
