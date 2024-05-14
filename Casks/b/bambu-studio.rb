39d2a4fcc76899d2dd123f5fb289e50792906163
Sam Ford <1584702+samford@users.noreply.github.com>
2024-05-14 18:07:28 -0400

    bambu-studio: update version format, livecheck
    This reorganizes the `version` parts to place the optional part at
    the end, so the cask can handle a mismatch between the tag version
    and filename version without requiring manual changes.

--- Casks/b/bambu-studio.rb
+++ Casks/b/bambu-studio.rb
@@ -1,8 +1,8 @@
 cask "bambu-studio" do
-  version "01.09.01.66,01.09.01.67,20240514131542"
+  version "01.09.01.67,20240514131542,01.09.01.66"
   sha256 "b08d482ed87298cd473a6b5b4127f4c39b1363dffb1a781916219b12dab77144"
 
-  url "https://github.com/bambulab/BambuStudio/releases/download/v#{version.csv.first}/Bambu_Studio_mac-v#{version.csv.second}-#{version.csv.third}.dmg",
+  url "https://github.com/bambulab/BambuStudio/releases/download/v#{version.csv.third || version.csv.first}/Bambu_Studio_mac-v#{version.csv.first}-#{version.csv.second}.dmg",
       verified: "github.com/bambulab/BambuStudio/"
   name "Bambu Studio"
   desc "3D model slicing software for 3D printers, maintained by Bambu Lab"
@@ -12,7 +12,13 @@ cask "bambu-studio" do
     url :homepage
     regex(%r{href=.*/v?(\d+(?:\.\d+)+)/Bambu[._-]Studio[._-]mac[._-]v?(\d+(?:\.\d+)+)[._-](\d+)\.dmg}i)
     strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| "#{match.first},#{match.second},#{match.third}" }
+      page.scan(regex).map do |match|
+        if match[2] == match[0]
+          "#{match[1]},#{match[2]}"
+        else
+          "#{match[1]},#{match[2]},#{match[0]}"
+        end
+      end
     end
   end
 
