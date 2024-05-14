037a118606d559b109fc62daa75b8d5c264cd720
Bevan Kay <email@bevankay.me>
2024-05-14 17:59:47 +1000

    obs-advanced-scene-switcher 1.26.1

--- Casks/o/obs-advanced-scene-switcher.rb
+++ Casks/o/obs-advanced-scene-switcher.rb
@@ -1,6 +1,6 @@
 cask "obs-advanced-scene-switcher" do
-  version "1.26.0"
-  sha256 "9801efde7ae04afded03d548587121c50db718cff97bea48869fc7a4c04b5fcd"
+  version "1.26.1"
+  sha256 "07983db8489d431b6198339f586140175a9f04c5e30eb4c6570a8ce0de551779"
 
   url "https://github.com/WarmUpTill/SceneSwitcher/releases/download/#{version}/advanced-scene-switcher-#{version}-macos-universal.pkg",
       verified: "github.com/WarmUpTill/SceneSwitcher/"
