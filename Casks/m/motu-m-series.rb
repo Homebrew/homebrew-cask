a76b515ef462ef127a0f5632b1fb2943e2c8383c
Bevan Kay <email@bevankay.me>
2024-05-14 17:38:12 +1000

    motu-m-series 95541

--- Casks/m/motu-m-series.rb
+++ Casks/m/motu-m-series.rb
@@ -1,20 +1,20 @@
 cask "motu-m-series" do
-  version "93305"
-  sha256 "4936d5ecb335dad01c473d4d588420acb044a75434c5bf939c94ba6edd1fb7d8"
+  version "95541"
+  sha256 "a01b367553f9b02c64ab0737c40295c249e1767cb87fd585ff64b543bdd69622"
 
-  url "https://cdn-data.motu.com/downloads/audio/mseries/driver/RC/MOTU%20MSeries%20Installer%20(#{version}).pkg"
+  url "https://cdn-data.motu.com/downloads/audio/mseries/driver/RC/MOTU%20M%20Series%20Installer%20(#{version}).pkg"
   name "Motu M-Series"
   desc "Audio interface driver for Motu M-Series (M2, M4, M6) audio interfaces"
   homepage "https://motu.com/en-us/download/product/408/"
 
   livecheck do
     url :homepage
-    regex(/<h2[^>]*?>.*?motu\s?m-series\s?installer.*?\s*?<p[^>]*?>.*?version.*?(\d+(?:\.\d+)*).*?</i)
+    regex(/<span[^>]*?>Mac\s*v?(\d+(?:\.\d+)*)</i)
   end
 
   depends_on macos: ">= :mojave"
 
-  pkg "MOTU MSeries Installer (#{version}).pkg"
+  pkg "MOTU M Series Installer (#{version}).pkg"
 
   uninstall launchctl: "com.motu.coreuac.reenumerator",
             pkgutil:   [
