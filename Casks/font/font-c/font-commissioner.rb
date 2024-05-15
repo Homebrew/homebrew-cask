573c562388b683ae9422ff275a42d317a7370e3d
Bevan Kay <email@bevankay.me>
2024-05-15 23:12:54 +1000

    font-commissioner: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-c/font-commissioner.rb
@@ -0,0 +1,14 @@
+cask "font-commissioner" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/commissioner/Commissioner%5BFLAR%2CVOLM%2Cslnt%2Cwght%5D.ttf",
+      verified: "github.com/google/fonts/"
+  name "Commissioner"
+  desc "Low-contrast humanist sans-serif font with almost classical proportions"
+  homepage "https://fonts.google.com/specimen/Commissioner"
+
+  font "Commissioner[FLAR,VOLM,slnt,wght].ttf"
+
+  # No zap stanza required
+end
