a1fba93215c7a8626f66ca9464807427837ae2f2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-15 18:35:38 +0000

    datagrip 2024.1.3

--- Casks/d/datagrip.rb
+++ Casks/d/datagrip.rb
@@ -1,9 +1,9 @@
 cask "datagrip" do
   arch arm: "-aarch64"
 
-  version "2024.1.2,241.15989.49"
-  sha256 arm:   "583c32f39918681673028a20cb911a41fc4495cf44c151c0790594042ec9e160",
-         intel: "41a4dabdd2ad97387d806742c41b34e5a1544860c159f5d71c09b00a1e605356"
+  version "2024.1.3,241.17011.41"
+  sha256 arm:   "9fb1c0528209a1d32857b61b1e6910472b4f7316f8ec7ee0e3dfd2baeb164c06",
+         intel: "9b37f5943532c5413179043721290f292be23a9baaccb6d42c5b31ae70af8ca2"
 
   url "https://download.jetbrains.com/datagrip/datagrip-#{version.csv.first}#{arch}.dmg"
   name "DataGrip"
