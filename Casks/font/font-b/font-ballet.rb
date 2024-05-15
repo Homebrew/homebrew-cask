1f8277156cd2956fda8fd581cd70fda2d5e44be8
Bevan Kay <email@bevankay.me>
2024-05-15 23:07:49 +1000

    font-ballet: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-b/font-ballet.rb
@@ -0,0 +1,14 @@
+cask "font-ballet" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/ballet/Ballet%5Bopsz%5D.ttf",
+      verified: "github.com/google/fonts/"
+  name "Ballet"
+  desc "Designed by maximiliano sproviero and developed by eduardo tunni"
+  homepage "https://fonts.google.com/specimen/Ballet"
+
+  font "Ballet[opsz].ttf"
+
+  # No zap stanza required
+end
