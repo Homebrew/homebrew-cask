3a2251e519fb9bdbd60c040b522d56b7346a2493
Bevan Kay <email@bevankay.me>
2024-05-15 23:12:50 +1000

    font-codicon: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-c/font-codicon.rb
@@ -0,0 +1,13 @@
+cask "font-codicon" do
+  version "0.0.36"
+  sha256 "360527575395ad70f940eff1e43eb7c2ec64f4e69f89dc5861b73420d2a578f0"
+
+  url "https://github.com/microsoft/vscode-codicons/releases/download/#{version}/codicon.ttf"
+  name "Codicon"
+  desc "Icon font for Visual Studio Code"
+  homepage "https://github.com/microsoft/vscode-codicons"
+
+  font "codicon.ttf"
+
+  # No zap stanza required
+end
