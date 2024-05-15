36159cc72fe80aa97848b0dc6816dff72f6d6b83
Bevan Kay <email@bevankay.me>
2024-05-15 17:00:42 +1000

    font-african-serif: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-a/font-african-serif.rb
@@ -0,0 +1,15 @@
+cask "font-african-serif" do
+  version "9.380"
+  sha256 :no_check
+
+  url "https://www.languagegeek.com/font/AfricanSerif.zip"
+  name "African Serif"
+  homepage "https://www.languagegeek.com/font/fontdownload.html"
+
+  font "African Serif REGULAR 938.ttf"
+  font "African Serif BOLD 938.ttf"
+  font "African Serif BOLD ITALIC 938.ttf"
+  font "African Serif ITALIC 938.ttf"
+
+  # No zap stanza required
+end
