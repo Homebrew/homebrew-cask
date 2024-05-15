ad3d67763d2320c8a7ca06d8cda4b6b1e5336610
Bevan Kay <email@bevankay.me>
2024-05-15 23:13:20 +1000

    font-diphylleia: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-d/font-diphylleia.rb
@@ -0,0 +1,14 @@
+cask "font-diphylleia" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/diphylleia/Diphylleia-Regular.ttf",
+      verified: "github.com/google/fonts/"
+  name "Diphylleia"
+  desc "Mysterious flower that gets transparent when the water touches its petals"
+  homepage "https://fonts.google.com/specimen/Diphylleia"
+
+  font "Diphylleia-Regular.ttf"
+
+  # No zap stanza required
+end
