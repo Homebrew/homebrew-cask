b765f696c63c27c49c668f43235826cafff5accc
Bevan Kay <email@bevankay.me>
2024-05-15 23:12:40 +1000

    font-cascadia-mono-nf: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-c/font-cascadia-mono-nf.rb
@@ -0,0 +1,19 @@
+cask "font-cascadia-mono-nf" do
+  version "2404.23"
+  sha256 "a911410626c0e09d03fa3fdda827188fda96607df50fecc3c5fee5906e33251b"
+
+  url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/CascadiaCode-#{version}.zip"
+  name "Cascadia Mono NF"
+  desc "Version of Cascadia Code without ligatures and with embedded Nerd Fonts symbols"
+  homepage "https://github.com/microsoft/cascadia-code"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  font "ttf/CascadiaMonoNF.ttf"
+  font "ttf/CascadiaMonoNFItalic.ttf"
+
+  # No zap stanza required
+end
