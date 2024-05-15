4e181316baf8cff536e2550c23958d0ac09e0936
Bevan Kay <email@bevankay.me>
2024-05-15 23:12:35 +1000

    font-cairo-play: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-c/font-cairo-play.rb
@@ -0,0 +1,14 @@
+cask "font-cairo-play" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/cairoplay/CairoPlay%5Bslnt%2Cwght%5D.ttf",
+      verified: "github.com/google/fonts/"
+  name "Cairo Play"
+  desc "Color font version of cairo which features colored marks"
+  homepage "https://fonts.google.com/specimen/Cairo+Play"
+
+  font "CairoPlay[slnt,wght].ttf"
+
+  # No zap stanza required
+end
