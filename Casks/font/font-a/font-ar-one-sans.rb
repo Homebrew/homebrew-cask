d241012cca79c09a9010121b6c445f10bb2bfddb
Bevan Kay <email@bevankay.me>
2024-05-15 17:01:00 +1000

    font-ar-one-sans: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-a/font-ar-one-sans.rb
@@ -0,0 +1,14 @@
+cask "font-ar-one-sans" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/aronesans/AROneSans%5BARRR%2Cwght%5D.ttf",
+      verified: "github.com/google/fonts/"
+  name "AR One Sans"
+  desc "Type family is for use in augmented reality environments and user interfaces"
+  homepage "https://fonts.google.com/specimen/AR+One+Sans"
+
+  font "AROneSans[ARRR,wght].ttf"
+
+  # No zap stanza required
+end
