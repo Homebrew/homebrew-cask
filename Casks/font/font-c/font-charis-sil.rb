ad3b1f46dca49ed34088e2ee13e4a2420867257a
Bevan Kay <email@bevankay.me>
2024-05-15 23:12:43 +1000

    font-charis-sil: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-c/font-charis-sil.rb
@@ -0,0 +1,21 @@
+cask "font-charis-sil" do
+  version "6.200"
+  sha256 "4b09aa75760b8aa697b762c34afb995dde0754c8f09256cb912dbfc478c97ade"
+
+  url "https://software.sil.org/downloads/r/charis/CharisSIL-#{version}.zip"
+  name "Charis SIL"
+  desc "Unicode-based font family supporting languages using Latin and Cyrillic scripts"
+  homepage "https://software.sil.org/charis/"
+
+  livecheck do
+    url "https://software.sil.org/charis/download/"
+    regex(/href=.*CharisSIL[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+  end
+
+  font "CharisSIL-#{version}/CharisSIL-Bold.ttf"
+  font "CharisSIL-#{version}/CharisSIL-BoldItalic.ttf"
+  font "CharisSIL-#{version}/CharisSIL-Italic.ttf"
+  font "CharisSIL-#{version}/CharisSIL-Regular.ttf"
+
+  # No zap stanza required
+end
