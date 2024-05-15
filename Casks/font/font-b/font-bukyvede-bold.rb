31b9eb6889e918dc625892c339cfe7babf28fb9a
Bevan Kay <email@bevankay.me>
2024-05-15 23:08:09 +1000

    font-bukyvede-bold: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-b/font-bukyvede-bold.rb
@@ -0,0 +1,12 @@
+cask "font-bukyvede-bold" do
+  version :latest
+  sha256 :no_check
+
+  url "https://kodeks.uni-bamberg.de/aksl/media/BukyVede-Bold.ttf"
+  name "BukyVede Bold"
+  homepage "https://kodeks.uni-bamberg.de/aksl/Schrift/BukyVede.htm"
+
+  font "BukyVede-Bold.ttf"
+
+  # No zap stanza required
+end
