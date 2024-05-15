99315ab5ddd67708ac9a1de56b70636eeb310c1e
Bevan Kay <email@bevankay.me>
2024-05-15 23:12:38 +1000

    font-carlito: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-c/font-carlito.rb
@@ -0,0 +1,19 @@
+cask "font-carlito" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
+      branch:    "main",
+      only_path: "ofl/carlito"
+  name "Carlito"
+  desc "Metric-compatible with calibri"
+  homepage "https://fonts.google.com/specimen/Carlito"
+
+  font "Carlito-Bold.ttf"
+  font "Carlito-BoldItalic.ttf"
+  font "Carlito-Italic.ttf"
+  font "Carlito-Regular.ttf"
+
+  # No zap stanza required
+end
