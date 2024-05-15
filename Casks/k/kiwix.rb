6339dbe2e6220538617ee4ebc5506804aa7a6026
Ilia Maskov <genco.oil.corp@gmail.com>
2024-05-15 21:13:55 +0300

    kiwix: download url changed

--- Casks/k/kiwix.rb
+++ Casks/k/kiwix.rb
@@ -2,13 +2,13 @@ cask "kiwix" do
   version "3.3.0"
   sha256 "72e47a64afd57ee6182189dfa7969741d39ee3122da275b3308ed252fadd9a12"
 
-  url "https://download.kiwix.org/release/kiwix-desktop-macos/kiwix-desktop-macos_#{version}.dmg"
+  url "https://download.kiwix.org/release/kiwix-macos/kiwix-macos_#{version}.dmg"
   name "Kiwix"
   desc "App providing offline access to Wikipedia and many other web sites"
   homepage "https://www.kiwix.org/"
 
   livecheck do
-    url "https://download.kiwix.org/release/kiwix-desktop-macos/kiwix-desktop-macos.dmg"
+    url "https://download.kiwix.org/release/kiwix-macos/kiwix-macos.dmg"
     strategy :header_match
   end
 
