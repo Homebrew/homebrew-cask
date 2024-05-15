be6d591e6eabc8cc9329fccf51dc86e8a7966f61
Jakob Zudrell <jakob@zudrell.eu>
2024-05-15 15:03:12 +0200

    jellyfin-media-player 1.10.0

--- Casks/j/jellyfin-media-player.rb
+++ Casks/j/jellyfin-media-player.rb
@@ -1,9 +1,13 @@
 cask "jellyfin-media-player" do
-  version "1.9.1"
-  sha256 "6ed5f5c1489c177de45f46fdbeefee8cc9eee7faff75d03e3840f522849674d2"
+  version "1.10.0"
+  arch arm: "AppleSilicon", intel: "Intel"
 
-  url "https://github.com/jellyfin/jellyfin-media-player/releases/download/v#{version}/JellyfinMediaPlayer-#{version}-macos-notarized.dmg",
+  sha256 arm:   "1fd584132af2107d7aaeb46c66a5b05ba78a2f56150b6f54482abe6bb705acd0",
+         intel: "dda9b26971a8c25eea9e759ab89aa8bf5abd4e4b805dc37827d13bddc6da1ab5"
+  
+  url "https://github.com/jellyfin/jellyfin-media-player/releases/download/v#{version}/JellyfinMediaPlayer-#{arch}.dmg",
       verified: "github.com/jellyfin/jellyfin-media-player/"
+
   name "Jellyfin Media Player"
   desc "Jellyfin desktop client"
   homepage "https://jellyfin.org/"
