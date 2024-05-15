66035c3099009c006c0459c0584119360721d73b
Bevan Kay <email@bevankay.me>
2024-05-15 23:08:09 +1000

    font-bukyvede-regular: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-b/font-bukyvede-regular.rb
@@ -0,0 +1,12 @@
+cask "font-bukyvede-regular" do
+  version :latest
+  sha256 :no_check
+
+  url "https://kodeks.uni-bamberg.de/aksl/media/BukyVede-Regular.ttf"
+  name "BukyVede Regular"
+  homepage "https://kodeks.uni-bamberg.de/aksl/Schrift/BukyVede.htm"
+
+  font "BukyVede-Regular.ttf"
+
+  # No zap stanza required
+end
