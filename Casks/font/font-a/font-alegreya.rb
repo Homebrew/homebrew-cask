ff51ee1d34ce013e95aa94b5b420bdfffccf7837
Bevan Kay <email@bevankay.me>
2024-05-15 17:00:47 +1000

    font-alegreya: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-a/font-alegreya.rb
@@ -0,0 +1,31 @@
+cask "font-alegreya" do
+  version "2.008"
+  sha256 "8a10cae2b660b048c53e9be1612698b86ed123a284f9f9d17cc9a4a45388650a"
+
+  url "https://github.com/huertatipografica/Alegreya/archive/v#{version}.zip"
+  name "Alegreya"
+  homepage "https://github.com/huertatipografica/Alegreya"
+
+  font "Alegreya-#{version}/fonts/otf/Alegreya-Black.otf"
+  font "Alegreya-#{version}/fonts/otf/Alegreya-BlackItalic.otf"
+  font "Alegreya-#{version}/fonts/otf/Alegreya-Bold.otf"
+  font "Alegreya-#{version}/fonts/otf/Alegreya-BoldItalic.otf"
+  font "Alegreya-#{version}/fonts/otf/Alegreya-ExtraBold.otf"
+  font "Alegreya-#{version}/fonts/otf/Alegreya-ExtraBoldItalic.otf"
+  font "Alegreya-#{version}/fonts/otf/Alegreya-Italic.otf"
+  font "Alegreya-#{version}/fonts/otf/Alegreya-Medium.otf"
+  font "Alegreya-#{version}/fonts/otf/Alegreya-MediumItalic.otf"
+  font "Alegreya-#{version}/fonts/otf/Alegreya-Regular.otf"
+  font "Alegreya-#{version}/fonts/otf/AlegreyaSC-Black.otf"
+  font "Alegreya-#{version}/fonts/otf/AlegreyaSC-BlackItalic.otf"
+  font "Alegreya-#{version}/fonts/otf/AlegreyaSC-Bold.otf"
+  font "Alegreya-#{version}/fonts/otf/AlegreyaSC-BoldItalic.otf"
+  font "Alegreya-#{version}/fonts/otf/AlegreyaSC-ExtraBold.otf"
+  font "Alegreya-#{version}/fonts/otf/AlegreyaSC-ExtraBoldItalic.otf"
+  font "Alegreya-#{version}/fonts/otf/AlegreyaSC-Italic.otf"
+  font "Alegreya-#{version}/fonts/otf/AlegreyaSC-Medium.otf"
+  font "Alegreya-#{version}/fonts/otf/AlegreyaSC-MediumItalic.otf"
+  font "Alegreya-#{version}/fonts/otf/AlegreyaSC-Regular.otf"
+
+  # No zap stanza required
+end
