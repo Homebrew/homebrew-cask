a273e7ec00637a05cde4f5cfa0351fb6e7d9c61a
Bevan Kay <email@bevankay.me>
2024-05-15 17:00:41 +1000

    font-adinatha-tamil-brahmi: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-a/font-adinatha-tamil-brahmi.rb
@@ -0,0 +1,12 @@
+cask "font-adinatha-tamil-brahmi" do
+  version :latest
+  sha256 :no_check
+
+  url "http://www.virtualvinodh.com/download/Adinatha-Tamil-Brahmi.zip"
+  name "Adinatha Tamil Brahmi"
+  homepage "http://www.virtualvinodh.com/wp/tamil-brahmi-font/"
+
+  font "Adinatha-Tamil-Brahmi/Adinatha-Tamil-Brahmi.otf"
+
+  # No zap stanza required
+end
