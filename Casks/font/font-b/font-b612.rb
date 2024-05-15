e0d7e93c7722bf049bde470cef85351c0be9595e
Bevan Kay <email@bevankay.me>
2024-05-15 23:07:47 +1000

    font-b612: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-b/font-b612.rb
@@ -0,0 +1,17 @@
+cask "font-b612" do
+  version "1.008"
+  sha256 "727cb91e47d65ac49c2d97d7b1c36d9891b885d9ddf06e15ad3d23d22bdad9cf"
+
+  url "https://github.com/polarsys/b612/archive/refs/tags/#{version}.zip",
+      verified: "github.com/polarsys/b612/"
+  name "B612"
+  desc "Font designed and tested to be used on aircraft cockpit screens"
+  homepage "https://b612-font.com/"
+
+  font "b612-#{version}/fonts/ttf/B612-Bold.ttf"
+  font "b612-#{version}/fonts/ttf/B612-BoldItalic.ttf"
+  font "b612-#{version}/fonts/ttf/B612-Italic.ttf"
+  font "b612-#{version}/fonts/ttf/B612-Regular.ttf"
+
+  # No zap stanza required
+end
