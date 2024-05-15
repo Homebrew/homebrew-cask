60764f70243d4eacb102f6c718b814ea11fac8b9
Bevan Kay <email@bevankay.me>
2024-05-15 23:13:04 +1000

    font-cozette: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-c/font-cozette.rb
@@ -0,0 +1,18 @@
+cask "font-cozette" do
+  version "1.24.1"
+  sha256 "27176cc4f76259db3e65eecda8999b2167c305bca9e563c272216208d94044f7"
+
+  url "https://github.com/slavfox/Cozette/releases/download/v.#{version}/CozetteVector.dfont"
+  name "Cozette"
+  desc "Bitmap programming font"
+  homepage "https://github.com/slavfox/Cozette"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  font "CozetteVector.dfont"
+
+  # No zap stanza required
+end
