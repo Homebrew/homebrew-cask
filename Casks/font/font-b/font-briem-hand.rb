d2d69e48715cbeef851f3a076efd97f9cf9062d0
Bevan Kay <email@bevankay.me>
2024-05-15 23:08:07 +1000

    font-briem-hand: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-b/font-briem-hand.rb
@@ -0,0 +1,14 @@
+cask "font-briem-hand" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/briemhand/BriemHand%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
+  name "Briem Hand"
+  desc "Modern adaptation of sixteenth-century chancery cursive"
+  homepage "https://fonts.google.com/specimen/Briem+Hand"
+
+  font "BriemHand[wght].ttf"
+
+  # No zap stanza required
+end
