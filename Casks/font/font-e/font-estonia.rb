b30704d306a826af85fa5146142d515107794a28
Bevan Kay <email@bevankay.me>
2024-05-15 23:12:28 +1000

    font-estonia: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-e/font-estonia.rb
@@ -0,0 +1,14 @@
+cask "font-estonia" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/estonia/Estonia-Regular.ttf",
+      verified: "github.com/google/fonts/"
+  name "Estonia"
+  desc "Based on the calligraphic style found in the east european country of estonia"
+  homepage "https://fonts.google.com/specimen/Estonia"
+
+  font "Estonia-Regular.ttf"
+
+  # No zap stanza required
+end
