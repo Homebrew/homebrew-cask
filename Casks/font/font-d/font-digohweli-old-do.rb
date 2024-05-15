5c180689db7e423fb9c37770491f031015735bec
Bevan Kay <email@bevankay.me>
2024-05-15 23:13:19 +1000

    font-digohweli-old-do: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-d/font-digohweli-old-do.rb
@@ -0,0 +1,12 @@
+cask "font-digohweli-old-do" do
+  version :latest
+  sha256 :no_check
+
+  url "https://www.languagegeek.com/font/OldDODigohweli.zip"
+  name "Digohweli Old DO"
+  homepage "https://www.languagegeek.com/font/fontdownload.html"
+
+  font "OldDODigohweli.ttf"
+
+  # No zap stanza required
+end
