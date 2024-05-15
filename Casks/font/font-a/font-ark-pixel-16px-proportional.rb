7ba76e74dc9c2f716134838345f274631d3d72e4
Bevan Kay <email@bevankay.me>
2024-05-15 17:01:04 +1000

    font-ark-pixel-16px-proportional: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-a/font-ark-pixel-16px-proportional.rb
@@ -0,0 +1,19 @@
+cask "font-ark-pixel-16px-proportional" do
+  version "2024.05.12"
+  sha256 "c02c9ab969edd153600af854f3f271800ee07d5bd854c29f1596aeca96fabea9"
+
+  url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-16px-proportional-otf-v#{version}.zip"
+  name "Ark Pixel 16px Proportional"
+  desc "Open source Pan-CJK pixel font"
+  homepage "https://github.com/TakWolf/ark-pixel-font"
+
+  font "ark-pixel-16px-proportional-ja.otf"
+  font "ark-pixel-16px-proportional-ko.otf"
+  font "ark-pixel-16px-proportional-latin.otf"
+  font "ark-pixel-16px-proportional-zh_cn.otf"
+  font "ark-pixel-16px-proportional-zh_hk.otf"
+  font "ark-pixel-16px-proportional-zh_tr.otf"
+  font "ark-pixel-16px-proportional-zh_tw.otf"
+
+  # No zap stanza required
+end
