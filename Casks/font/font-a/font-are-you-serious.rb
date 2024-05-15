3043e63206d0ff00b40ec5179c93960bdd710397
Bevan Kay <email@bevankay.me>
2024-05-15 17:01:02 +1000

    font-are-you-serious: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-a/font-are-you-serious.rb
@@ -0,0 +1,14 @@
+cask "font-are-you-serious" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/areyouserious/AreYouSerious-Regular.ttf",
+      verified: "github.com/google/fonts/"
+  name "Are You Serious"
+  desc "Fun playful font with a very joyful spirit"
+  homepage "https://fonts.google.com/specimen/Are+You+Serious"
+
+  font "AreYouSerious-Regular.ttf"
+
+  # No zap stanza required
+end
