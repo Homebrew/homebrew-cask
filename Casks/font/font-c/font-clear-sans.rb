75e96fef97e528afd743c228e5fe36edae566f17
Bevan Kay <email@bevankay.me>
2024-05-15 23:12:49 +1000

    font-clear-sans: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-c/font-clear-sans.rb
@@ -0,0 +1,24 @@
+cask "font-clear-sans" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/intel/clear-sans.git",
+      branch:    "main",
+      only_path: "TTF"
+  name "Clear Sans"
+  desc "Sans-serif typeface"
+  homepage "https://github.com/intel/clear-sans"
+
+  deprecate! date: "2023-12-17", because: :discontinued
+
+  font "ClearSans-Bold.ttf"
+  font "ClearSans-BoldItalic.ttf"
+  font "ClearSans-Italic.ttf"
+  font "ClearSans-Light.ttf"
+  font "ClearSans-Medium.ttf"
+  font "ClearSans-MediumItalic.ttf"
+  font "ClearSans-Regular.ttf"
+  font "ClearSans-Thin.ttf"
+
+  # No zap stanza required
+end
