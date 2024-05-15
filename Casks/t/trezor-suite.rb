a4e1a45784e2c9187ccda1f9d90b4cab1da10a0a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-15 12:54:37 +0000

    trezor-suite 24.5.2

--- Casks/t/trezor-suite.rb
+++ Casks/t/trezor-suite.rb
@@ -1,9 +1,9 @@
 cask "trezor-suite" do
   arch arm: "arm64", intel: "x64"
 
-  version "24.4.3"
-  sha256 arm:   "fcf078646a6df69efaf350831de3a10be9b7a6fa7e96342b13947f3587c734a8",
-         intel: "27f01d52ce27d72007576c0ffe9d95e14d64b689dcd029541f8202dd7bfec04f"
+  version "24.5.2"
+  sha256 arm:   "c81d87e6c04a1ea5f35d9c25007ab3d5b85ef7ed25f709fc9139e78415c316da",
+         intel: "dd99e21c31da6f25e851edf33b527ad77e59440868ee0981515d40736f3017ac"
 
   url "https://github.com/trezor/trezor-suite/releases/download/v#{version}/Trezor-Suite-#{version}-mac-#{arch}.dmg",
       verified: "github.com/trezor/trezor-suite/"
