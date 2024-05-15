9234d8df1cc347d5c463d13771a20c208d4274df
Bevan Kay <email@bevankay.me>
2024-05-15 23:07:58 +1000

    font-big-shoulders-inline-display: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-b/font-big-shoulders-inline-display.rb
@@ -0,0 +1,14 @@
+cask "font-big-shoulders-inline-display" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/bigshouldersinlinedisplay/BigShouldersInlineDisplay%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
+  name "Big Shoulders Inline Display"
+  desc "Superfamily of condensed American Gothic typefaces"
+  homepage "https://fonts.google.com/specimen/Big+Shoulders+Inline+Display"
+
+  font "BigShouldersInlineDisplay[wght].ttf"
+
+  # No zap stanza required
+end
