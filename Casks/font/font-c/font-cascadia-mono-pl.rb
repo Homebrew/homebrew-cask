e0abb6d8b7d67d57fcccfce536477eeccdc7950a
Bevan Kay <email@bevankay.me>
2024-05-15 23:12:40 +1000

    font-cascadia-mono-pl: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-c/font-cascadia-mono-pl.rb
@@ -0,0 +1,19 @@
+cask "font-cascadia-mono-pl" do
+  version "2404.23"
+  sha256 "a911410626c0e09d03fa3fdda827188fda96607df50fecc3c5fee5906e33251b"
+
+  url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/CascadiaCode-#{version}.zip"
+  name "Cascadia Mono PL"
+  desc "Version of Cascadia Code without ligatures and with embedded Powerline symbols"
+  homepage "https://github.com/microsoft/cascadia-code"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  font "ttf/CascadiaMonoPL.ttf"
+  font "ttf/CascadiaMonoPLItalic.ttf"
+
+  # No zap stanza required
+end
