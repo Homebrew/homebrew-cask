09c1c684aa096bec03f4f8153a32edb4c526a2f7
Bevan Kay <email@bevankay.me>
2024-05-15 23:07:58 +1000

    font-big-shoulders-stencil-display: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-b/font-big-shoulders-stencil-display.rb
@@ -0,0 +1,14 @@
+cask "font-big-shoulders-stencil-display" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/bigshouldersstencildisplay/BigShouldersStencilDisplay%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
+  name "Big Shoulders Stencil Display"
+  desc "Superfamily of condensed American Gothic typefaces"
+  homepage "https://fonts.google.com/specimen/Big+Shoulders+Stencil+Display"
+
+  font "BigShouldersStencilDisplay[wght].ttf"
+
+  # No zap stanza required
+end
