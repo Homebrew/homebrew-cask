d6f7365ca7224c9429b7e5e5699f05f9ef75257a
Bevan Kay <email@bevankay.me>
2024-05-15 17:00:45 +1000

    font-albert-sans: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-a/font-albert-sans.rb
@@ -0,0 +1,17 @@
+cask "font-albert-sans" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
+      branch:    "main",
+      only_path: "ofl/albertsans"
+  name "Albert Sans"
+  desc "Modern geometric sans serif family"
+  homepage "https://fonts.google.com/specimen/Albert+Sans"
+
+  font "AlbertSans-Italic[wght].ttf"
+  font "AlbertSans[wght].ttf"
+
+  # No zap stanza required
+end
