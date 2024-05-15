d4c2ee0e1ad84a1e0863ea68ef43f605ab5cf413
Bevan Kay <email@bevankay.me>
2024-05-15 17:00:54 +1000

    font-andika: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-a/font-andika.rb
@@ -0,0 +1,21 @@
+cask "font-andika" do
+  version "6.200"
+  sha256 "cc9a7523fcdecf5d2e1858f8ed43bca8f1e1be1ad5f20ac81475d816a9dcf87c"
+
+  url "https://software.sil.org/downloads/r/andika/Andika-#{version}.zip"
+  name "Andika"
+  desc "Sans-serif font family designed and optimized for literacy use"
+  homepage "https://software.sil.org/andika/"
+
+  livecheck do
+    url "https://software.sil.org/andika/download/"
+    regex(/href=.*Andika[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+  end
+
+  font "Andika-#{version}/Andika-Bold.ttf"
+  font "Andika-#{version}/Andika-BoldItalic.ttf"
+  font "Andika-#{version}/Andika-Italic.ttf"
+  font "Andika-#{version}/Andika-Regular.ttf"
+
+  # No zap stanza required
+end
