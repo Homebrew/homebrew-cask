48d0b6ec35f7798d805425d64e4fc52e81f70e57
Michael Cho <michael@michaelcho.dev>
2024-05-15 11:40:52 -0400

    dotnet-sdk@preview 9.0.100-preview.3.24204.13

--- Casks/d/dotnet-sdk@preview.rb
+++ Casks/d/dotnet-sdk@preview.rb
@@ -2,18 +2,18 @@ cask "dotnet-sdk@preview" do
   arch arm: "arm64", intel: "x64"
 
   on_arm do
-    version "8.0.204,a1b04e7e-464d-45f6-ac93-f8901c46c247,7acd4fa3f321aaec4598a1492cc745c6"
-    sha256 "9c41150210de26549d520cb274cdc4b6db24ee7168a150b0465adf18828ee348"
+    version "9.0.100-preview.3.24204.13,788839e8-1e23-4ed2-b176-534d3c4d5899,d80c58a63108090e803c06d0b05a1b73"
+    sha256 "b00ab761376b421853ce40a35fff85497d9ba9850b751aec6b279b87dad0f96e"
   end
   on_intel do
-    version "8.0.204,7bcf1329-6691-45e9-8fdd-96c65bcabd1f,cb618d2620974fc029e4f758eb267452"
-    sha256 "cfc35a77cb675ebeb31570b723b58514f68c0034bb4194d7b40523cd2b819455"
+    version "9.0.100-preview.3.24204.13,9794b13e-14f8-4fd8-baa9-265adc2c7f31,605ec6e450a81e1acfeedc06444450f9"
+    sha256 "bf6b12af95d2f83a50e50762902e09318fd5172c965427292d6ba8f0aaefda72"
   end
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv.second}/#{version.csv.third}/dotnet-sdk-#{version.csv.first}-osx-#{arch}.pkg"
   name ".NET SDK"
   desc "Developer platform"
-  homepage "https://www.microsoft.com/net/core#macos"
+  homepage "https://dotnet.microsoft.com/en-us/"
 
   livecheck do
     url "https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/#{version.major_minor}/releases.json"
