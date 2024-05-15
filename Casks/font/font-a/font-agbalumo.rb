07064ab901920c6312c86c06a154d4b432a496a9
Bevan Kay <email@bevankay.me>
2024-05-15 17:00:43 +1000

    font-agbalumo: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-a/font-agbalumo.rb
@@ -0,0 +1,14 @@
+cask "font-agbalumo" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/agbalumo/Agbalumo-Regular.ttf",
+      verified: "github.com/google/fonts/"
+  name "Agbalumo"
+  desc "Single weight font"
+  homepage "https://fonts.google.com/specimen/Agbalumo"
+
+  font "Agbalumo-Regular.ttf"
+
+  # No zap stanza required
+end
