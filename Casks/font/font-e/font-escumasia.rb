b0c3c0007e6ce6250aa9c2ea2ff5910523573665
Bevan Kay <email@bevankay.me>
2024-05-15 23:12:27 +1000

    font-escumasia: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-e/font-escumasia.rb
@@ -0,0 +1,13 @@
+cask "font-escumasia" do
+  version :latest
+  sha256 :no_check
+
+  url "https://moji-waku.com/download/escumasia.zip"
+  name "Escumasia"
+  desc "Ancient biology styled font inspired by escumasia"
+  homepage "https://moji-waku.com/escumasia/index.html"
+
+  font "escumasia/Escumasia-Regular.otf"
+
+  # No zap stanza required
+end
