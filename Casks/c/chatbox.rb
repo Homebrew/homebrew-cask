fa64e47fa749d242a83719569157f75d42b0cc09
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-15 06:33:28 +0000

    chatbox 1.3.10

--- Casks/c/chatbox.rb
+++ Casks/c/chatbox.rb
@@ -1,9 +1,9 @@
 cask "chatbox" do
   arch arm: "-arm64"
 
-  version "1.3.9"
-  sha256 arm:   "88698941e8ad51b81f5f91bbebf4f693fd9d31c312d64ac848876c50df7b755f",
-         intel: "4898396d83f0783f1eb4569b56f400459abb8c8b29147f9996ad702128fcad9e"
+  version "1.3.10"
+  sha256 arm:   "deeb54bf37d7451c3796dbcac5cbb0f834fe54d5d0f652aa5b4f274abf6bf5c6",
+         intel: "8cf9a89e225d88dc18a1a5daebb7226503528ab3904a5912ee117d5d2ca1534e"
 
   url "https://github.com/Bin-Huang/chatbox/releases/download/v#{version}/Chatbox-#{version}#{arch}.dmg",
       verified: "github.com/Bin-Huang/chatbox/"
