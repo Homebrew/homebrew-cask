e8397ea1a95e8b1100cf073ef683bc2496581670
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-05-15 09:12:03 -0400

    devtunnel 1.0.1301+f410beb2af

--- Casks/d/devtunnel.rb
+++ Casks/d/devtunnel.rb
@@ -1,9 +1,9 @@
 cask "devtunnel" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.0.1299+92794a823b"
-  sha256 arm:   "8f72449d1a24b0c7da53583c06d691b22f20629e8788bc3a676fc777e9d1f7a0",
-         intel: "5be5be3f2dd6e0974da626e0d6d0ec2aed087f97646ecba6ba0cd5394e5f861f"
+  version "1.0.1301+f410beb2af"
+  sha256 arm:   "a2300a1435157eea27dab64ae3ff9fef87db407f9799283e8fbcb6dfc9f86fcb",
+         intel: "98d36fc7632dc0b1fdaeb02776cf035df085285172d5974de3fd943a3c3c70d0"
 
   url "https://tunnelsassetsprod.blob.core.windows.net/cli/#{version}/osx-#{arch}-devtunnel-zip",
       verified: "tunnelsassetsprod.blob.core.windows.net/cli/"
