878631883f4a428938c9b87aa7f2ef6a0b90d667
Bevan Kay <email@bevankay.me>
2024-05-15 23:12:24 +1000

    font-elstobd: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-e/font-elstobd.rb
@@ -0,0 +1,14 @@
+cask "font-elstobd" do
+  version "2.104"
+  sha256 "f20439bf6de7c4dd177b013fe5f262cad30bc8caedd07245ad5dfe2b8e9231cc"
+
+  url "https://github.com/psb1558/Elstob-font/releases/download/v#{version}/Elstob_#{version}.zip"
+  name "ElstobD"
+  desc "Variable font for medievalists"
+  homepage "https://github.com/psb1558/Elstob-font"
+
+  font "Elstob_font/variable/Elstob-Italic.ttf"
+  font "Elstob_font/variable/Elstob.ttf"
+
+  # No zap stanza required
+end
