c4035d73e3d6f3909c348009a235da97de6383fc
Bevan Kay <email@bevankay.me>
2024-05-15 23:08:00 +1000

    font-biorhyme: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-b/font-biorhyme.rb
@@ -0,0 +1,14 @@
+cask "font-biorhyme" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/biorhyme/BioRhyme%5Bwdth%2Cwght%5D.ttf",
+      verified: "github.com/google/fonts/"
+  name "BioRhyme"
+  desc "Latin typeface family"
+  homepage "https://fonts.google.com/specimen/BioRhyme"
+
+  font "BioRhyme[wdth,wght].ttf"
+
+  # No zap stanza required
+end
