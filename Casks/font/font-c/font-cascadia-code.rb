6b557fefcbb4f560275a51c8c333b53e74bc94c3
Bevan Kay <email@bevankay.me>
2024-05-15 23:12:40 +1000

    font-cascadia-code: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-c/font-cascadia-code.rb
@@ -0,0 +1,19 @@
+cask "font-cascadia-code" do
+  version "2404.23"
+  sha256 "a911410626c0e09d03fa3fdda827188fda96607df50fecc3c5fee5906e33251b"
+
+  url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/CascadiaCode-#{version}.zip"
+  name "Cascadia Code"
+  desc "Monospaced font that includes programming ligatures"
+  homepage "https://github.com/microsoft/cascadia-code"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  font "ttf/CascadiaCode.ttf"
+  font "ttf/CascadiaCodeItalic.ttf"
+
+  # No zap stanza required
+end
