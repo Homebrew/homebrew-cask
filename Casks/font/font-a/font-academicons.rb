5e39c6a0f45bf6b98d58f79393f52ff1148e93a8
Bevan Kay <email@bevankay.me>
2024-05-15 17:00:40 +1000

    font-academicons: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-a/font-academicons.rb
@@ -0,0 +1,13 @@
+cask "font-academicons" do
+  version "1.9.4"
+  sha256 "6f27234f174e9a8872296b1778083393127773ddcb19f0b3cc5ab3bfa42c37e9"
+
+  url "https://github.com/jpswalsh/academicons/archive/v#{version}.zip"
+  name "Academicons"
+  desc "Specialist icon font for academics"
+  homepage "https://github.com/jpswalsh/academicons"
+
+  font "academicons-#{version}/fonts/academicons.ttf"
+
+  # No zap stanza required
+end
