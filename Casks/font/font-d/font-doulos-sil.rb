7c8e36745ad78a74c9dfaeca2588f5618ec682b4
Bevan Kay <email@bevankay.me>
2024-05-15 23:13:24 +1000

    font-doulos-sil: migrate from `homebrew-cask-fonts`

---
+++ Casks/font/font-d/font-doulos-sil.rb
@@ -0,0 +1,18 @@
+cask "font-doulos-sil" do
+  version "6.200"
+  sha256 "a7ad76326c126b2748297b987a634a56f7e42cd45bc3ff2c90a7909cbb164223"
+
+  url "https://software.sil.org/downloads/r/doulos/DoulosSIL-#{version}.zip"
+  name "Doulos SIL"
+  desc "Unicode-based font family supporting languages using Latin and Cyrillic scripts"
+  homepage "https://software.sil.org/doulos/"
+
+  livecheck do
+    url "https://software.sil.org/doulos/download/"
+    regex(/href=.*DoulosSIL[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+  end
+
+  font "DoulosSIL-#{version}/DoulosSIL-Regular.ttf"
+
+  # No zap stanza required
+end
