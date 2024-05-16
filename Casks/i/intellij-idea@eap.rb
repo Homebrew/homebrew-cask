29d4bc2819b9c00f466ec67837df15e46b3846df
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-16 15:46:31 +0000

    intellij-idea@eap 2024.2

--- Casks/i/intellij-idea@eap.rb
+++ Casks/i/intellij-idea@eap.rb
@@ -1,9 +1,9 @@
 cask "intellij-idea@eap" do
   arch arm: "-aarch64"
 
-  version "2024.1.2,241.17011.2"
-  sha256 arm:   "4323118869222885519d42b60f2f49f6f9f5e34e893a27e97c5220faa062c9d0",
-         intel: "1e8d38612e3c01efcb8a69c9bc8544169a44f1a8ba585969f6147ce7074e141d"
+  version "2024.2,242.10180.25"
+  sha256 arm:   "e483dd332b6eb381b8dbbd88420bd9881351985c1fc237bed208102de17da1f4",
+         intel: "ab80170b2b765c22cd1de701f6d5603ca9a664691cc6d1ffe2748deafbbbb6c3"
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version.csv.second}#{arch}.dmg"
   name "IntelliJ IDEA EAP"
@@ -22,7 +22,7 @@ cask "intellij-idea@eap" do
   auto_updates true
   depends_on macos: ">= :high_sierra"
 
-  app "IntelliJ IDEA.app", target: "IntelliJ IDEA #{version.csv.first} EAP.app"
+  app "IntelliJ IDEA #{version.csv.first} EAP.app"
 
   uninstall_postflight do
     ENV["PATH"].split(File::PATH_SEPARATOR).map { |path| File.join(path, "idea") }.each do |path|
