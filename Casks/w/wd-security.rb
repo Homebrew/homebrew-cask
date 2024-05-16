471291c4a38ef63f33683d8df91767fc2b5f7e71
Jacob Ford <j@cobford.com>
2024-05-15 21:46:43 -0400

    wd-security (new cask)

---
+++ Casks/w/wd-security.rb
@@ -0,0 +1,34 @@
+cask "wd-security" do
+  version "2.1.2.144"
+  sha256 :no_check
+
+  url "https://downloads.wdc.com/wdapp/WD_Security_MACOS.zip",
+      verified: "downloads.wdc.com/wdapp/"
+  name "WD Security"
+  desc "Lock and unlock Western Digital external drives with hardware encryption"
+  homepage "https://support-en.wd.com/app/answers/detailweb/a_id/50696"
+
+  livecheck do
+    url "https://support-en.wd.com/app/answers/detailweb/a_id/29490"
+    regex(/Version:?\s*(\d+(?:\.\d+)+)/i)
+  end
+
+  container nested: "WD Security Installer.dmg"
+
+  installer script: {
+    # replicating #{staged_path}/installer.sh
+    executable: "#{staged_path}/exec/WD Security Installer.app/Contents/MacOS/WD Security Installer",
+    args:       ["-install", "-silent"],
+  }
+
+  uninstall script: {
+    # replicating #{staged_path}/installer.sh
+    executable: "#{staged_path}/exec/WD Security Installer.app/Contents/MacOS/WD Security Installer",
+    args:       ["-uninstall", "-silent"],
+  }
+
+  zap trash: [
+    "/Library/LaunchDaemons/com.wdc.WDPrivilegedHelper.plist",
+    "~/Library/Preferences/com.wdc.branded.security.plist",
+  ]
+end
