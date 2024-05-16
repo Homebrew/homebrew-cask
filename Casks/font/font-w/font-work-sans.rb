92c7f7a8dbc97b2a7ec1972d2c6d12109f6b050e
Aaron Ruan <aaron212cn@outlook.com>
2024-05-16 17:58:50 +0800

    update font-work-sans to use git repository
    also added variable fonts to the cask

--- Casks/font/font-w/font-work-sans.rb
+++ Casks/font/font-w/font-work-sans.rb
@@ -2,29 +2,33 @@ cask "font-work-sans" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/weiweihuanghuang/Work-Sans/archive/master.zip",
-      verified: "github.com/weiweihuanghuang/Work-Sans/"
+  url "https://github.com/weiweihuanghuang/Work-Sans.git",
+      verified:  "github.com/weiweihuanghuang/Work-Sans",
+      branch:    "master",
+      only_path: "fonts"
   name "Work Sans"
   homepage "https://weiweihuanghuang.github.io/Work-Sans/"
 
-  font "Work-Sans-master/fonts/ttf/WorkSans-Black.ttf"
-  font "Work-Sans-master/fonts/ttf/WorkSans-BlackItalic.ttf"
-  font "Work-Sans-master/fonts/ttf/WorkSans-Bold.ttf"
-  font "Work-Sans-master/fonts/ttf/WorkSans-BoldItalic.ttf"
-  font "Work-Sans-master/fonts/ttf/WorkSans-ExtraBold.ttf"
-  font "Work-Sans-master/fonts/ttf/WorkSans-ExtraBoldItalic.ttf"
-  font "Work-Sans-master/fonts/ttf/WorkSans-ExtraLight.ttf"
-  font "Work-Sans-master/fonts/ttf/WorkSans-ExtraLightItalic.ttf"
-  font "Work-Sans-master/fonts/ttf/WorkSans-Italic.ttf"
-  font "Work-Sans-master/fonts/ttf/WorkSans-Light.ttf"
-  font "Work-Sans-master/fonts/ttf/WorkSans-LightItalic.ttf"
-  font "Work-Sans-master/fonts/ttf/WorkSans-Medium.ttf"
-  font "Work-Sans-master/fonts/ttf/WorkSans-MediumItalic.ttf"
-  font "Work-Sans-master/fonts/ttf/WorkSans-Regular.ttf"
-  font "Work-Sans-master/fonts/ttf/WorkSans-SemiBold.ttf"
-  font "Work-Sans-master/fonts/ttf/WorkSans-SemiBoldItalic.ttf"
-  font "Work-Sans-master/fonts/ttf/WorkSans-Thin.ttf"
-  font "Work-Sans-master/fonts/ttf/WorkSans-ThinItalic.ttf"
+  font "ttf/WorkSans-Black.ttf"
+  font "ttf/WorkSans-BlackItalic.ttf"
+  font "ttf/WorkSans-Bold.ttf"
+  font "ttf/WorkSans-BoldItalic.ttf"
+  font "ttf/WorkSans-ExtraBold.ttf"
+  font "ttf/WorkSans-ExtraBoldItalic.ttf"
+  font "ttf/WorkSans-ExtraLight.ttf"
+  font "ttf/WorkSans-ExtraLightItalic.ttf"
+  font "ttf/WorkSans-Italic.ttf"
+  font "ttf/WorkSans-Light.ttf"
+  font "ttf/WorkSans-LightItalic.ttf"
+  font "ttf/WorkSans-Medium.ttf"
+  font "ttf/WorkSans-MediumItalic.ttf"
+  font "ttf/WorkSans-Regular.ttf"
+  font "ttf/WorkSans-SemiBold.ttf"
+  font "ttf/WorkSans-SemiBoldItalic.ttf"
+  font "ttf/WorkSans-Thin.ttf"
+  font "ttf/WorkSans-ThinItalic.ttf"
+  font "variable/WorkSans-Italic[wght].ttf"
+  font "variable/WorkSans[wght].ttf"
 
   # No zap stanza required
 end
