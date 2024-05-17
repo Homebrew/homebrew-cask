14b6b9710f6bde2cdf45f85c905378a59fa1b0eb
Jay Paul Morgan <jaypmorgan@users.noreply.github.com>
2024-05-17 09:26:56 +0000

    Fix style offenses

--- Casks/font/font-d/font-darwin.rb
+++ Casks/font/font-d/font-darwin.rb
@@ -1,3 +1,6 @@
+# typed: true
+# frozen_string_literal: true
+
 cask "font-darwin" do
   version :latest
   sha256 :no_check
@@ -5,8 +8,8 @@ cask "font-darwin" do
   url "https://github.com/topological-modular-forms/Darwin-Typeface/raw/main/output/DarwinSerif-Regular.ttf"
   name "Darwin"
   homepage "https://github.com/topological-modular-forms/Darwin-Typeface/tree/main"
+
   font "DarwinSerif-Regular.ttf"
 
-    # No zap stanza required
+  # No zap stanza required
 end
-  
