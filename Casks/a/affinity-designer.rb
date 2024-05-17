81e2c9e4f2ef5b994cb743436fdbb6ed14d50544
Sam Ford <1584702+samford@users.noreply.github.com>
2024-05-17 14:50:08 -0400

    affinity-designer: use versioned url, update livecheck

--- Casks/a/affinity-designer.rb
+++ Casks/a/affinity-designer.rb
@@ -1,28 +1,31 @@
 cask "affinity-designer" do
-  version "2.4.2"
-  sha256 :no_check
+  version "2.4.2,2371"
+  sha256 "94f19ba7c058978b8f9498b0cd5ba77926e3ed9fc25b22f155de24d43c2c2958"
 
-  url "https://store.serif.com/download/60f51f/"
-  name "Affinity Designer #{version.major}"
+  url "https://s3-eu-west-1.amazonaws.com/affinity-update/mac2/retail/Affinity%20Designer%20#{version.csv.first.major}%20Affinity%20Store%20#{version.csv.second}.zip",
+      verified: "s3-eu-west-1.amazonaws.com/"
+  name "Affinity Designer #{version.csv.first.major}"
   desc "Professional graphic design software"
   homepage "https://affinity.serif.com/en-us/designer/"
 
   livecheck do
-    url :url
-    strategy :header_match
+    url "https://go.seriflabs.com/affinity-update-mac-retail-designer#{version.csv.first.major}"
+    strategy :sparkle do |item|
+      "#{item.short_version},#{item.version}"
+    end
   end
 
   auto_updates true
   depends_on macos: ">= :catalina"
 
-  app "Affinity Designer #{version.major}.app"
+  app "Affinity Designer #{version.csv.first.major}.app"
 
   zap trash: [
-    "~/Library/Application Support/Affinity Designer 2",
-    "~/Library/Caches/com.seriflabs.affinitydesigner2",
-    "~/Library/HTTPStorages/com.seriflabs.affinitydesigner2",
-    "~/Library/Preferences/com.seriflabs.affinitydesigner2.plist",
-    "~/Library/Saved Application State/com.seriflabs.affinitydesigner2.savedState",
-    "~/Library/WebKit/com.seriflabs.affinitydesigner2",
+    "~/Library/Application Support/Affinity Designer #{version.csv.first.major}",
+    "~/Library/Caches/com.seriflabs.affinitydesigner#{version.csv.first.major}",
+    "~/Library/HTTPStorages/com.seriflabs.affinitydesigner#{version.csv.first.major}",
+    "~/Library/Preferences/com.seriflabs.affinitydesigner#{version.csv.first.major}.plist",
+    "~/Library/Saved Application State/com.seriflabs.affinitydesigner#{version.csv.first.major}.savedState",
+    "~/Library/WebKit/com.seriflabs.affinitydesigner#{version.csv.first.major}",
   ]
 end
