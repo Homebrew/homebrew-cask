7357be32ad6a442bc7e9c19979b49fcaa755fb73
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-05-17 10:42:34 -0400

    apache-couchdb 3.3.3-3

--- Casks/a/apache-couchdb.rb
+++ Casks/a/apache-couchdb.rb
@@ -1,7 +1,7 @@
 cask "apache-couchdb" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "3.3.3-2"
+  version "3.3.3-3"
   sha256 arm:   "ab2c24a8511fb9e8749727ec63510ba63d1a017090b72787a1ab9d8959498bb0",
          intel: "d863d74ab65df1f5cfd79fd2601e2f631201eeec0667a74bab251d8cffbe1070"
 
