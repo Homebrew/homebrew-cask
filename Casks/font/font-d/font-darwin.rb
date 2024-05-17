c0b1d3c398f5566f3c4054b0a87759a9472fa2d4
Jay Paul Morgan <jaypmorgan@users.noreply.github.com>
2024-05-17 09:18:35 +0000

    Add recipe for Darwin Font

---
+++ Casks/font/font-d/font-darwin.rb
@@ -0,0 +1,12 @@
+cask "font-darwin" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/topological-modular-forms/Darwin-Typeface/raw/main/output/DarwinSerif-Regular.ttf"
+  name "Darwin"
+  homepage "https://github.com/topological-modular-forms/Darwin-Typeface/tree/main"
+  font "DarwinSerif-Regular.ttf"
+
+    # No zap stanza required
+end
+  
