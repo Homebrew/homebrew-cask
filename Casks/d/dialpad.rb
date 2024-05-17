7883a18b96315099addf3c27c6a85fbf34dff524
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-05-16 21:17:16 -0400

    dialpad: rework
    * Add `arch` specific downloads
    
    * Add `sha256` checksums
    
    * Replace `extract_plist` strategy with `json`
    
    * Add `depends_on` minimum macOS version
    
    * Add `zao`
    
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/d/dialpad.rb
+++ Casks/d/dialpad.rb
@@ -1,17 +1,32 @@
 cask "dialpad" do
-  version "22.0.1"
-  sha256 :no_check
+  arch arm: "arm64", intel: "x64"
 
-  url "https://storage.googleapis.com/dialpad_native/osx/Dialpad.dmg",
-      verified: "storage.googleapis.com/dialpad_native/osx/"
+  version "2404.1.0"
+  sha256 arm:   "c99e2ebcb89818caaad785d366ec95dfe909f470e7da51291892a1ae237919cc",
+         intel: "c60f3e4d9cd1f952d6c3ed63e9773db3f569bda52d3140c4d379433d31a6fe31"
+
+  url "https://storage.googleapis.com/dialpad_native/osx/#{arch}/Dialpad.#{version}.zip",
+      verified: "storage.googleapis.com/dialpad_native/osx/#{arch}"
   name "Dialpad"
   desc "Cloud communication platform"
   homepage "https://dialpad.com/download"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url "https://dialpad.com/native/checkversion/darwin/#{arch}"
+    strategy :json do |json|
+      json["version"]
+    end
   end
 
+  depends_on macos: ">= :high_sierra"
+
   app "Dialpad.app"
+
+  zap trash: [
+    "~/Library/Application Support/Dialpad",
+    "~/Library/Caches/com.electron.dialpad*",
+    "~/Library/HTTPStorages/com.electron.dialpad",
+    "~/Library/Preferences/com.electron.dialpad.plist",
+    "~/Library/Saved Application State/com.electron.dialpad.savedState",
+  ]
 end
