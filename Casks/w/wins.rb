129d12fea611f9186394ce8f37078b1a389b6ec4
EggsBlue <1719411461@qq.com>
2024-05-17 17:42:05 +0800

    add wins app

---
+++ Casks/w/wins.rb
@@ -0,0 +1,23 @@
+
+cask "wins" do
+  version :latest
+  sha256 :no_check
+
+  url "https://f005.backblazeb2.com/file/winsWebsite/Wins-latest-1.6.6.dmg", verified: "backblazeb2.com/file/winsWebsite"
+  name "Wins"
+  desc "a brand new window manager for macOS"
+  homepage "https://wins.cool/"
+  depends_on macos: '>= :high_sierra'
+
+  app "Wins.app"
+
+  zap trash: [
+    "~/Library/Application Support/cools.wins.main",
+    "~/Library/Application Support/Wins",
+    "~/Library/Caches/cools.wins.main",
+    "~/Library/Caches/cools.wins.main",
+    "~/Library/Containers/WinsHelper",
+    "~/Library/HTTPStorages/cools.wins.main",
+    "~/Library/Preferences/com.tinynudge.pomello.plist",
+  ]
+end
