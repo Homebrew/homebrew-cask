3a6aa74055435258cbd423e390c41f7fe9d24417
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-17 09:36:00 +0000

    iterm2@nightly 3_5_20240517

--- Casks/i/iterm2@nightly.rb
+++ Casks/i/iterm2@nightly.rb
@@ -1,7 +1,7 @@
 cask "iterm2@nightly" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "3_5_20240516"
-  sha256 "0c431541c62f0318234c1a94b4eeb2cb79efd4ed90ab96dd86eba21bd2b1fec1"
+  version "3_5_20240517"
+  sha256 "00bd9ee3be32376da916207a415f907f70f7f869228b02f53a10963a553a0a74"
 
   url "https://www.iterm2.com/nightly/iTerm2-#{version}-nightly.zip"
   name "iTerm2"
