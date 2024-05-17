0603734fec1d67c095193f8c5d5204ac44321b9c
EggsBlue <1719411461@qq.com>
2024-05-17 21:48:00 +0800

    Update wins.rb

--- Casks/w/wins.rb
+++ Casks/w/wins.rb
@@ -1,17 +1,22 @@
 cask "wins" do
   version "1.6.6"
   sha256 "594fb383c26f3b36dfa132312ddbafb0e59f7566616b5f02294e769cecf55605"
+
   url "https://f005.backblazeb2.com/file/winsWebsite/Wins-latest-#{version}.dmg", verified: "backblazeb2.com/file/winsWebsite/"
   name "Wins"
   desc "Window manager"
   homepage "https://wins.cool/"
-  depends_on macos: ">= :high_sierra"
-  app "Wins.app"
+
   livecheck do
     url "https://wins.cool/"
     strategy :page_match
     regex(/href=.*?Wins-latest[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
+
+  depends_on macos: ">= :high_sierra"
+
+  app "Wins.app"
+
   zap trash: [
     "~/Library/Application Support/cools.wins.main",
     "~/Library/Application Support/Wins",
