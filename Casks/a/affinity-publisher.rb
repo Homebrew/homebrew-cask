15631bec2124f8e0b8acf8993d8b73ea23cf6cc0
Sam Ford <1584702+samford@users.noreply.github.com>
2024-05-17 14:50:44 -0400

    affinity-publisher: use versioned url, update livecheck

--- Casks/a/affinity-publisher.rb
+++ Casks/a/affinity-publisher.rb
@@ -1,28 +1,31 @@
 cask "affinity-publisher" do
-  version "2.4.2"
-  sha256 :no_check
+  version "2.4.2,2371"
+  sha256 "06b2f36dc4bfa772c06c8cfbd7e02bf739822e9303521a40e70ec1998f897bf9"
 
-  url "https://store.serif.com/download/668dfb/"
-  name "Affinity Publisher #{version.major}"
+  url "https://s3-eu-west-1.amazonaws.com/affinity-update/mac2/retail/Affinity%20Publisher%20#{version.csv.first.major}%20Affinity%20Store%20#{version.csv.second}.zip",
+      verified: "s3-eu-west-1.amazonaws.com/"
+  name "Affinity Publisher #{version.csv.first.major}"
   desc "Professional desktop publishing software"
   homepage "https://affinity.serif.com/en-us/publisher/"
 
   livecheck do
-    url :url
-    strategy :header_match
+    url "https://go.seriflabs.com/affinity-update-mac-retail-publisher#{version.csv.first.major}"
+    strategy :sparkle do |item|
+      "#{item.short_version},#{item.version}"
+    end
   end
 
   auto_updates true
   depends_on macos: ">= :catalina"
 
-  app "Affinity Publisher #{version.major}.app"
+  app "Affinity Publisher #{version.csv.first.major}.app"
 
   zap trash: [
-    "~/Library/Application Support/Affinity Publisher #{version.major}",
-    "~/Library/Caches/com.seriflabs.affinitypublisher#{version.major}",
-    "~/Library/HTTPStorages/com.seriflabs.affinitypublisher#{version.major}",
-    "~/Library/Preferences/com.seriflabs.affinitypublisher#{version.major}.plist",
-    "~/Library/Saved Application State/com.seriflabs.affinitypublisher#{version.major}.savedState",
-    "~/Library/WebKit/com.seriflabs.affinitypublisher#{version.major}",
+    "~/Library/Application Support/Affinity Publisher #{version.csv.first.major}",
+    "~/Library/Caches/com.seriflabs.affinitypublisher#{version.csv.first.major}",
+    "~/Library/HTTPStorages/com.seriflabs.affinitypublisher#{version.csv.first.major}",
+    "~/Library/Preferences/com.seriflabs.affinitypublisher#{version.csv.first.major}.plist",
+    "~/Library/Saved Application State/com.seriflabs.affinitypublisher#{version.csv.first.major}.savedState",
+    "~/Library/WebKit/com.seriflabs.affinitypublisher#{version.csv.first.major}",
   ]
 end
