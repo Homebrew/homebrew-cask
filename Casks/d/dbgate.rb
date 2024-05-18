32c6a7a6e15916d23233cbd459c58a2c8b3c3898
d7an <d7an@live.com>
2024-05-18 11:25:12 +0800

    dbgate 5.2.8

--- Casks/d/dbgate.rb
+++ Casks/d/dbgate.rb
@@ -1,6 +1,6 @@
 cask "dbgate" do
-  version "5.2.7"
-  sha256 "0f69344bbe00f77a40f1219c493a9c27c8c24f2ac658b481af2b55c7bb471e74"
+  version "5.2.8"
+  sha256 "841b08b9ddaf9a2a8fe1b46212796a2821960002729b35418736353e2fa46b62"
 
   url "https://github.com/dbgate/dbgate/releases/download/v#{version}/dbgate-#{version}-mac_universal.dmg",
       verified: "github.com/dbgate/dbgate/"
