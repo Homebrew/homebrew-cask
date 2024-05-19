d5ce9fd496c3f5421bf30b3ee25541e2ae1b2cea
Klaus Hipp <khipp@users.noreply.github.com>
2024-05-19 09:24:21 +0200

    font-inconsolata-lgc 1.8.0

--- Casks/font/font-i/font-inconsolata-lgc.rb
+++ Casks/font/font-i/font-inconsolata-lgc.rb
@@ -1,6 +1,6 @@
 cask "font-inconsolata-lgc" do
-  version "1.7.2"
-  sha256 "e771a9375994909434cf1d1d1adb8956ca994bc4ea0964f60c64cf7a0b9ecf1d"
+  version "1.8.0"
+  sha256 "d9135f9c40abc8d2673f0415575ded31d0fd89a7f36f778570faf5fe2952e4f1"
 
   url "https://github.com/MihailJP/Inconsolata-LGC/releases/download/LGC-#{version}/InconsolataLGC-#{version}.tar.xz"
   name "Inconsolata LGC"
