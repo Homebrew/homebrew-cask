a6122ec1bac1320d7b4744ca6e59471c50d309ef
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-05-20 09:29:52 -0400

    vbrokers 2.4.224

--- Casks/v/vbrokers.rb
+++ Casks/v/vbrokers.rb
@@ -1,6 +1,6 @@
 cask "vbrokers" do
-  version "2.4.106,600004,1714110189"
-  sha256 "1151bb4e9adbf2a8fc5f53eea25bb0487662bb0b62f1b6cad95b55043f9274a4"
+  version "2.4.224,600004,1716184464"
+  sha256 "023daaeeb3b27c341592a9763755f80d12a1b89fc0c2a060a82caa702bac1beb"
 
   url "https://r.hstong.com/public/app/Mac/HSTONG_Mac_#{version.csv.second}_#{version.csv.first}_#{version.csv.third}.dmg"
   name "VBrokers"
