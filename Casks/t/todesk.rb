901b37ffb0c72aa611d3ba68a12d4ad979ca099d
Bevan Kay <email@bevankay.me>
2024-05-20 19:16:13 +1000

    todesk: add `url` parameters

--- Casks/t/todesk.rb
+++ Casks/t/todesk.rb
@@ -2,7 +2,9 @@ cask "todesk" do
   version "4.7.2.1"
   sha256 "cba50ad3b10523f71175953d4606d3daf122d344dd1e5f130fd190a93ba509e8"
 
-  url "https://dl.todesk.com/macos/ToDesk_#{version}.pkg"
+  url "https://dl.todesk.com/macos/ToDesk_#{version}.pkg",
+      user_agent: :fake,
+      referer:    "https://www.todesk.com/"
   name "ToDesk"
   desc "Remote control software"
   homepage "https://www.todesk.com/"
