40462bc2c70322a11aa73bc03abe6f5ebc06a41d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-21 15:30:45 +0000

    appflowy 0.5.8

--- Casks/a/appflowy.rb
+++ Casks/a/appflowy.rb
@@ -1,6 +1,6 @@
 cask "appflowy" do
-  version "0.5.7"
-  sha256 "08b9202aa99d41ca531944280c421e65ee46f9fbe4d9475856f8bc832407c2ab"
+  version "0.5.8"
+  sha256 "64a1b5fa05a99dd19b8e6755ef07ceb1f4d13889d35a68f6c99e261ef1073ce0"
 
   url "https://github.com/AppFlowy-IO/AppFlowy/releases/download/#{version}/Appflowy-#{version}-macos-universal.zip",
       verified: "github.com/AppFlowy-IO/AppFlowy/"
