45795f7e9c84f2b2ec09351beb8f150e4b67e593
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-21 06:46:40 +0000

    vsee 4.21.2

--- Casks/v/vsee.rb
+++ Casks/v/vsee.rb
@@ -1,6 +1,6 @@
 cask "vsee" do
-  version "4.21.1,51254"
-  sha256 "2529791f8d97742769dce96457cda1afe999f0bf52148b4f3d3ba2bad5e01785"
+  version "4.21.2,51273"
+  sha256 "b19b5882805636a588456c180235db6090d60fb4dc4deacf1dfb8661eaa8a63d"
 
   url "https://d2q5hugz2rti4w.cloudfront.net/mac/#{version.csv.second}/vseemac.dmg",
       verified: "d2q5hugz2rti4w.cloudfront.net/mac/"
