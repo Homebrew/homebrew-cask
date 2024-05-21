8da30c61bbd1ea3e164c539346dd8e46bd3ff51b
Bevan Kay <email@bevankay.me>
2024-05-21 17:42:44 +1000

    philips-hue-sync: use versioned url, update livecheck

--- Casks/p/philips-hue-sync.rb
+++ Casks/p/philips-hue-sync.rb
@@ -1,24 +1,25 @@
 cask "philips-hue-sync" do
-  version "1.12.0.64"
-  sha256 :no_check
+  version "1.12.0.64,8b10db6f-3066-451c-b08a-d1b860935be1,64"
+  sha256 "1b18327ca982c48660ec267be7aff437f0d8bc299eba1c2791d56a787a4b4b1e"
 
-  url "https://firmware.meethue.com/v1/download?deviceTypeId=HueSyncMac",
-      verified: "firmware.meethue.com/v1/"
+  url "https://firmware.meethue.com/storage/huesyncmac/#{version.csv.third}/#{version.csv.second}/HueSyncInstaller_#{version.csv.first}.pkg",
+      verified: "firmware.meethue.com/storage/huesyncmac/"
   name "Philips Hue Sync"
   desc "Control your smart light system"
   homepage "https://www.philips-hue.com/en-us/explore-hue/propositions/entertainment/sync-with-pc"
 
   livecheck do
-    url :url
-    regex(/HueSyncInstaller[._-]v?(\d+(?:\.\d+)+)\.pkg/i)
-    strategy :header_match
+    url "https://firmware.meethue.com/v1/download?deviceTypeId=HueSyncMac"
+    regex(%r{/([^/]+)/([^/]+)/HueSyncInstaller[._-]v?(\d+(?:\.\d+)+)\.pkg}i)
+    strategy :header_match do |headers, regex|
+      match = headers["location"]&.match(regex)
+      next if match.blank?
+
+      "#{match[3]},#{match[2]},#{match[1]}"
+    end
   end
 
-  pkg "HueSyncInstaller.pkg"
-
-  preflight do
-    staged_path.glob("HueSyncInstaller_*.pkg").first.rename(staged_path/"HueSyncInstaller.pkg")
-  end
+  pkg "HueSyncInstaller_#{version.csv.first}.pkg"
 
   uninstall quit:    [
               "com.lighting.huesync",
