fd47cb631be3df1827063c66b58349bb8f729764
Sam Ford <1584702+samford@users.noreply.github.com>
2024-05-21 19:22:34 -0400

    paraview: update version/url format

--- Casks/p/paraview.rb
+++ Casks/p/paraview.rb
@@ -1,12 +1,16 @@
 cask "paraview" do
   arch arm: "arm64", intel: "x86_64"
-  min_macos_version = on_arch_conditional arm: "11.0", intel: "10.15"
 
-  version "5.12.0"
-  sha256 arm:   "6b50e2d90fd2d2caf1792a66358a854f59aa9c4825b3a8ec997339c893c94bb6",
-         intel: "1b9aca43835a58fb490137031434e4fcbdd4a07247038e3931624a0139917f4b"
+  on_arm do
+    version "5.12.0,MPI-OSX11.0-Python3.10"
+    sha256 "6b50e2d90fd2d2caf1792a66358a854f59aa9c4825b3a8ec997339c893c94bb6"
+  end
+  on_intel do
+    version "5.12.0,MPI-OSX10.15-Python3.10"
+    sha256 "1b9aca43835a58fb490137031434e4fcbdd4a07247038e3931624a0139917f4b"
+  end
 
-  url "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v#{version.major_minor}&type=binary&os=macOS&downloadFile=ParaView-#{version}-MPI-OSX#{min_macos_version}-Python3.10-#{arch}.dmg",
+  url "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v#{version.csv.first.major_minor}&type=binary&os=macOS&downloadFile=ParaView-#{version.csv.first}#{"-#{version.csv.second}" if version.csv.second}-#{arch}.dmg",
       user_agent: :fake
   name "ParaView"
   desc "Data analysis and visualization application"
@@ -14,13 +18,18 @@ cask "paraview" do
 
   livecheck do
     url "https://www.paraview.org/files/listing.txt"
-    regex(%r{/v?(?:\d+(?:\.\d+)+)/ParaView[._-]v?(\d+(?:\.\d+)+).*?[._-]#{arch}\.dmg}i)
+    regex(%r{/v?(?:\d+(?:\.\d+)+)/ParaView[._-]v?(\d+(?:[.-]\d+)+)(?:[._-](.*?))?[._-](?:#{arch}|universal)\.dmg}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map do |match|
+        match[1] ? "#{match[0]},#{match[1]}" : match[0]
+      end
+    end
   end
 
   depends_on macos: ">= :sierra"
 
-  app "ParaView-#{version}.app"
-  binary "#{appdir}/ParaView-#{version}.app/Contents/MacOS/paraview"
+  app "ParaView-#{version.csv.first}.app"
+  binary "#{appdir}/ParaView-#{version.csv.first}.app/Contents/MacOS/paraview"
 
   zap trash: [
     "~/.config/ParaView",
