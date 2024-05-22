28247fbaea6a45e27ddc3cfb85103b65fb32ed3b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-22 00:54:53 +0000

    reaper 7.16

--- Casks/r/reaper.rb
+++ Casks/r/reaper.rb
@@ -1,5 +1,5 @@
 cask "reaper" do
-  version "7.15"
+  version "7.16"
 
   on_mojave :or_older do
     sha256 "04b01b62c36f93826f29f35a9e7953c74171a1f6a7f9359738b3eea2b9c2bb1d"
@@ -7,7 +7,7 @@ cask "reaper" do
     url "https://dlcf.reaper.fm/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64.dmg"
   end
   on_catalina :or_newer do
-    sha256 "eed5a06c722401182c3e2d1bbaf6d06f8a93aa77723f0451aaeb751a3e163591"
+    sha256 "50c9620fdb64f150e9790e2d042df5a5e7a6bfb1c02875b4c6daa84a4cd81b86"
 
     url "https://dlcf.reaper.fm/#{version.major}.x/reaper#{version.major_minor.no_dots}_universal.dmg"
   end
