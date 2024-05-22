eda5fdfee55b9188b7ff6b3110f5a0cf22f97ac7
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-05-22 11:52:38 -0400

    iterm2@beta 3.5.1beta1

--- Casks/i/iterm2@beta.rb
+++ Casks/i/iterm2@beta.rb
@@ -1,12 +1,12 @@
 cask "iterm2@beta" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "3.5.0beta26"
-  sha256 "b6256a9b10ed3a63871b657c02fdc4b952e1815df21646c4e5c704b4250347a7"
+  version "3.5.1beta1"
+  sha256 "0c7dae1b88b3e5bbe729b436492991ccde1698750bc0ee09420585c67f0dd66a"
 
   url "https://iterm2.com/downloads/beta/iTerm2-#{version.dots_to_underscores}.zip"
   name "iTerm2"
   desc "Terminal emulator as alternative to Apple's Terminal app"
-  homepage "https://www.iterm2.com/"
+  homepage "https://iterm2.com/"
 
   livecheck do
     url "https://iterm2.com/appcasts/testing_modern.xml"
