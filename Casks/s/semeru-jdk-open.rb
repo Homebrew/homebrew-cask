a290b3623c9257983cd5921bc0efb2f5be56d036
j9build <j9build@ca.ibm.com>
2024-05-22 12:49:22 -0400

    semeru-jdk-open 22.0.1+8

--- Casks/s/semeru-jdk-open.rb
+++ Casks/s/semeru-jdk-open.rb
@@ -1,9 +1,9 @@
 cask "semeru-jdk-open" do
   arch arm: "aarch64", intel: "x64"
 
-  version "21.0.2+13,openj9-0.43.0"
-  sha256 arm:   "569ec5928e834240ab8b2655c7d80100335808cd364e343457489bbea124890a",
-         intel: "7540948635e1c275476cc5e0a2a72c7303852cea84419750733af63e76539f0a"
+  version "22.0.1+8,openj9-0.45.0"
+  sha256 arm:   "20d2d2c7d6058dfabe49dc714ec63e06ff739ad0188baf88ca807cff96eb0ce3",
+         intel: "24227d6ef5789b6fe067c3c44a9764c54cf084ddd997bbdb5c3735e108b1b11e"
 
   url "https://github.com/ibmruntimes/semeru#{version.major}-binaries/releases/download/jdk-#{version.csv.first}_#{version.csv.second}/ibm-semeru-open-jdk_#{arch}_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg",
       verified: "github.com/ibmruntimes/"
