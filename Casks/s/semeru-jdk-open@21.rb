cc99b66d0673fee86fa80c0a8d9181e22718aeef
Adam Brousseau <adam.brousseau@ca.ibm.com>
2024-05-22 12:45:53 -0400

    semeru-jdk-open 22.0.1+8
    Move 21 to a new rb file
    
    Signed-off-by: Adam Brousseau <adam.brousseau@ca.ibm.com>

--- Casks/s/semeru-jdk-open@21.rb
+++ Casks/s/semeru-jdk-open@21.rb
@@ -1,18 +1,18 @@
-cask "semeru-jdk-open" do
+cask "semeru-jdk-open@21" do
   arch arm: "aarch64", intel: "x64"
 
-  version "22.0.1+8,openj9-0.45.0"
-  sha256 arm:   "20d2d2c7d6058dfabe49dc714ec63e06ff739ad0188baf88ca807cff96eb0ce3",
-         intel: "24227d6ef5789b6fe067c3c44a9764c54cf084ddd997bbdb5c3735e108b1b11e"
+  version "21.0.2+13,openj9-0.43.0"
+  sha256 arm:   "569ec5928e834240ab8b2655c7d80100335808cd364e343457489bbea124890a",
+         intel: "7540948635e1c275476cc5e0a2a72c7303852cea84419750733af63e76539f0a"
 
   url "https://github.com/ibmruntimes/semeru#{version.major}-binaries/releases/download/jdk-#{version.csv.first}_#{version.csv.second}/ibm-semeru-open-jdk_#{arch}_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg",
-      verified: "github.com/ibmruntimes/"
-  name "IBM Semeru Runtime (JDK) Open Edition"
+      verified: "github.com/ibmruntimes/semeru#{version.major}-binaries/"
+  name "IBM Semeru Runtime (JDK 21) Open Edition"
   desc "Production-ready JDK with the OpenJDK class libraries and the Eclipse OpenJ9 JVM"
   homepage "https://developer.ibm.com/languages/java/semeru-runtimes"
 
   livecheck do
-    url :url
+    url :stable
     regex(/^jdk[._-](\d+(?:[.+]\d+)*)[._-](.+?)$/i)
     strategy :github_latest do |json, regex|
       json["tag_name"]&.scan(regex)&.map { |match| "#{match[0]},#{match[1]}" }
