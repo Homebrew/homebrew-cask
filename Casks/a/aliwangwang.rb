10b7c50d46762c60b719241f814a0a7591bb0f34
Loyalsoldier <10487845+Loyalsoldier@users.noreply.github.com>
2024-05-23 22:48:00 +0800

    aliwangwang 10.01.06M

--- Casks/a/aliwangwang.rb
+++ Casks/a/aliwangwang.rb
@@ -1,15 +1,16 @@
 cask "aliwangwang" do
-  version "10.01.02M"
-  sha256 "5a13d4890944a2a64b862cda41f9786e344b5e520e218dc37e9090ae282b0d8a"
+  version "10.01.06M"
+  sha256 "9b23e1a1cd067893fef407b3037ce6c4072182a9a8a3294d236e0a3a26d020a2"
 
   url "https://download.alicdn.com/wangwang/AliWangWang_(#{version}).dmg",
       verified: "download.alicdn.com/wangwang/"
   name "AliWangwang"
-  desc "Shopping communication tool"
+  name "阿里旺旺"
+  desc "Shopping communication tool for Taobao and Tmall users"
   homepage "https://pages.tmall.com/wow/qnww/act/index"
 
   livecheck do
-    url "https://g.alicdn.com/im/ww-home/0.0.5/detail.min.js"
+    url "https://g.alicdn.com/im/ww-home/0.0.10/detail.min.js"
     regex(/AliWangWang[._-]v?\((\d+(?:\.\d+)+\w+)\)\.dmg/i)
   end
 
@@ -18,12 +19,9 @@ cask "aliwangwang" do
 
   app "AliWangwang.app"
 
-  uninstall quit: "com.taobao.Aliwangwang"
-
   zap trash: [
     "~/Library/Application Support/AliWangwang",
     "~/Library/Caches/com.taobao.Aliwangwang",
-    "~/Library/Containers/com.taobao.Aliwangwang",
     "~/Library/HTTPStorages/com.taobao.Aliwangwang",
     "~/Library/HTTPStorages/com.taobao.Aliwangwang.binarycookies",
     "~/Library/Preferences/com.taobao.Aliwangwang.plist",
