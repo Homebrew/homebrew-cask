70ed9d935691dd5e4a5f1f9e14b5c420a1915fa8
marongkuan <marongkuan@hotmail.com>
2024-05-23 13:42:49 +0800

    update windterm url

--- Casks/w/windterm.rb
+++ Casks/w/windterm.rb
@@ -2,7 +2,7 @@ cask "windterm" do
   version "2.6.0"
   sha256 "f9343ba28070d5039a89600133e06e0e3e023d67cd9540aae437f055417ce019"
 
-  url "https://github.com/kingToolbox/WindTerm/releases/download/#{version}/WindTerm_#{version}_Mac_Portable_x86_64.dmg"
+  url "https://github.com/kingToolbox/WindTerm/releases/download/#{version}/WindTerm_2.6.1_Mac_Portable_x86_64.dmg"
   name "WindTerm"
   desc "SSH/SFTP/Shell/Telnet/Serial terminal"
   homepage "https://github.com/kingToolbox/WindTerm"
