da96c090f234759026cece9828ce9a80c8ecf58e
Avery Winters <git@averywinters.org>
2024-05-23 09:10:40 -0500

    core-tunnel 3.8.7 (new cask)

---
+++ Casks/c/core-tunnel.rb
@@ -0,0 +1,28 @@
+cask "core-tunnel" do
+  version "3.8.7"
+  sha256 "4d8c70ce2db948ed9d0aa219a4c84a1d65c45da732cf729e12ece4a926e06656"
+
+  url "https://codinn.com/update/tunnel/Core%20Tunnel-#{version}.zip"
+  name "Core Tunnel"
+  desc "SSH tunnel manager"
+  homepage "https://codinn.com/tunnel/"
+
+  livecheck do
+    url "https://codinn.com/update/tunnel/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+  depends_on macos: ">= :monterey"
+
+  app "Core Tunnel.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/E78WKS7W4U.io.coressh",
+    "~/Library/Application Scripts/E78WKS7W4U.io.coressh.ssh",
+    "~/Library/Application Scripts/io.coressh.tunnel-direct",
+    "~/Library/Containers/io.coressh.tunnel-direct",
+    "~/Library/Group Containers/E78WKS7W4U.io.coressh",
+    "~/Library/Group Containers/E78WKS7W4U.io.coressh.ssh",
+  ]
+end
