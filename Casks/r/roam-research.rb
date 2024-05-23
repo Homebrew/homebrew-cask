93afc21afb0e922262b53e57c33f971e98149f7c
adithyasunil26 <adithyasunil26@gmail.com>
2024-05-23 12:24:00 +0530

    roam-research 0.0.21

--- Casks/r/roam-research.rb
+++ Casks/r/roam-research.rb
@@ -1,9 +1,9 @@
 cask "roam-research" do
   arch arm: "-arm64"
 
-  version "0.0.19"
-  sha256 arm:   "89045a692534df7b7759559948ab9708f25b328369c0b9c31c1cf941446ee86c",
-         intel: "a481f8a7b7679b23a01b2aee489dfdc41f22278e70b717084267d47812e50deb"
+  version "0.0.21"
+  sha256 arm:   "efdfab00e9bc12519df33204e11660f0c2c12ee6a6466c1c8fea027bec58a4e7",
+         intel: "c2fa19c7f4d4e641696a9d572cbbe100eddd154678fa139d059537f4feb8b1a2"
 
   url "https://roam-electron-deploy.s3.us-east-2.amazonaws.com/Roam+Research-#{version}#{arch}.dmg",
       verified: "roam-electron-deploy.s3.us-east-2.amazonaws.com/"
