6765a352cd973663e49d7f9a9b993ec708b80d92
miccal <miccal.matthews@gmail.com>
2024-05-24 18:09:39 +0800

    microsoft-teams@classic 1.7.00.13452

--- Casks/m/microsoft-teams@classic.rb
+++ Casks/m/microsoft-teams@classic.rb
@@ -1,6 +1,6 @@
 cask "microsoft-teams@classic" do
-  version "1.7.00.11653"
-  sha256 "ae8ae2085fc05f57c04ac30b34ca8ecbdc806ae709523598cf69ba4f9a526397"
+  version "1.7.00.13452"
+  sha256 "89970cc80de644134a477447bfa4b01e5446083c469558baf51f7b32227671b3"
 
   url "https://statics.teams.cdn.office.net/production-osx/#{version}/Teams_osx.pkg",
       verified: "statics.teams.cdn.office.net/production-osx/"
