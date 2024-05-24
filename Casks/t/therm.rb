6ae35e932f7eb23e81f0a228ddc97105705aa30f
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-05-24 08:37:31 -0400

    therm 0.6.4

--- Casks/t/therm.rb
+++ Casks/t/therm.rb
@@ -1,6 +1,6 @@
 cask "therm" do
-  version "0.6.3"
-  sha256 "fc93a8e275449ddb9da76e5da27425dfddf0be3727ba14decebe56632f7eef3a"
+  version "0.6.4"
+  sha256 "96ea558143ae60de68eecbfcd9e70aaeab2901d852189ef364ed2e6fc269d055"
 
   url "https://github.com/trufae/Therm/releases/download/#{version}/Therm-#{version}.zip"
   name "Therm"
