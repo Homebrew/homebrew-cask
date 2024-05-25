049563847fe49eb60fcec88b514b03811a426042
Stephen <stephengroat@users.noreply.github.com>
2024-05-24 18:02:26 -0700

    ankerwork 3.0.3

--- Casks/a/ankerwork.rb
+++ Casks/a/ankerwork.rb
@@ -1,9 +1,12 @@
 cask "ankerwork" do
-  version "2.6.0"
-  sha256 :no_check
+  arch arm: "arm64", intel: "x64"
 
-  url "https://ankerwork.s3.us-west-2.amazonaws.com/prod/AnkerWork_Official.dmg",
-      verified: "ankerwork.s3.us-west-2.amazonaws.com/prod/"
+  version "3.0.3"
+  sha256 arm:   "4c229125ccc1374cc409462d15218e22e96ff784eade63a36b097bd4900c0f24",
+         intel: "61226e32c989a31bf7b3c6b53a32fd5a379d434d1fd05ac2cc2003bcf95f70b9"
+
+  url "https://ankerwork.s3.us-west-2.amazonaws.com/electron/AnkerWork-Setup-#{arch}.dmg",
+      verified: "ankerwork.s3.us-west-2.amazonaws.com/electron/"
   name "AnkerWork"
   desc "Webcam & audio device software"
   homepage "https://us.ankerwork.com/pages/download-software"
