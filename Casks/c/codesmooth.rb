49921b1281f24f418373ef138385e9a02b456b1b
Artur Kot <arturkot95@gmail.com>
2024-05-25 09:50:02 +0200

    codesmooth 1.0.0

---
+++ Casks/c/codesmooth.rb
@@ -0,0 +1,18 @@
+cask "codesmooth" do
+  sha256 "62a088560abe5ca71cf3900165a0178facc3b39960695d00084f2926f9e4acf0"
+
+  url "https://storage.googleapis.com/codesmooth-resources/public/cli/codesmooth-#{version}.tar.gz"
+  name "CodeSmooth CLI"
+  desc "Command-line interface for CodeSmooth tasks"
+  homepage "https://github.com/CodeSmooth-dev/codesmooth-cli"
+
+  binary "bin/codesmooth-macos", target: "codesmooth"
+
+  zap trash: [
+    "~/.config/codesmooth",
+  ]
+
+  caveats do
+    license "https://www.apache.org/licenses/LICENSE-2.0"
+  end
+end
