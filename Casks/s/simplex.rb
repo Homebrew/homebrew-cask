261c206c78354a978a0fd1754a4d3c600d99e19e
Klaus Hipp <khipp@users.noreply.github.com>
2024-05-26 14:12:20 +0200

    simplex 5.7.4

--- Casks/s/simplex.rb
+++ Casks/s/simplex.rb
@@ -1,9 +1,9 @@
 cask "simplex" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "5.7.3"
-  sha256 arm:   "94cdc132d6158eddded25c2e2d2ce7e02b276c8f8c889d3122fc919e22568c72",
-         intel: "9c6393aa70858b2d1c371e2b22a75c2aa1a7fd31ad7dafe5f76cb9004874ecb9"
+  version "5.7.4"
+  sha256 arm:   "dd6c131dff4934ea98d248701b4f2fe67f7735e5c959210d4964ce5ffa5d26fc",
+         intel: "0135156d72d524f5a3fb77a266fd2b1ee87da33815515274add6ddfe43721ba6"
 
   url "https://github.com/simplex-chat/simplex-chat/releases/download/v#{version}/simplex-desktop-macos-#{arch}.dmg",
       verified: "github.com/simplex-chat/simplex-chat/"
