ef8317aaa5dabed436d804bbc0c9191688ad3f83
Michael Cho <michael@michaelcho.dev>
2024-05-27 10:49:40 -0400

    flykey: update livecheck

--- Casks/f/flykey.rb
+++ Casks/f/flykey.rb
@@ -1,8 +1,8 @@
 cask "flykey" do
-  version "1.6.4,2023121902"
-  sha256 "1348e13fdbd52e5db3fe9f3d3cbb55321d39bdfc52e653c390e9a19d73f813f7"
+  version "1.6.5,2024052201"
+  sha256 "e275afe105bb1bd88013a7116f1783477c561b7522fa3ef9e04141ea6911b0ab"
 
-  url "https://cdn.better365.cn/FlyKey/FlyKey#{version.csv.second}.zip"
+  url "https://cdn.better365.cn/FlyKey/#{version.csv.second[0..3]}/FlyKey#{version.csv.first}_#{version.csv.second}.zip"
   name "FlyKey"
   desc "One-click display of shortcuts"
   homepage "https://www.better365.cn/FlyKey.html"
