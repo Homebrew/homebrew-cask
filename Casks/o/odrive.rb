e782f22c4dd29fd83a8e3007d02a00bfc0bb8391
Klaus Hipp <khipp@users.noreply.github.com>
2024-05-27 14:30:45 +0200

    odrive 7417

--- Casks/o/odrive.rb
+++ Casks/o/odrive.rb
@@ -1,6 +1,6 @@
 cask "odrive" do
-  version "7406"
-  sha256 "68a567e792f66ca90891299a94615af7e93e89519c6ae626bf7641fa7c314c86"
+  version "7417"
+  sha256 "8be052053ecf48b875acdd7a4591f3f2a12541986e99cc2d531a05c1c5a404dd"
 
   url "https://d3huse1s6vwzq6.cloudfront.net/odrivesync.#{version}.pkg",
       verified: "d3huse1s6vwzq6.cloudfront.net/"
