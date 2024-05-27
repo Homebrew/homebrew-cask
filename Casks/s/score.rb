8fc505481483548dfce3be1744f9a92aa7c8d695
Klaus Hipp <khipp@users.noreply.github.com>
2024-05-27 14:50:32 +0200

    score 3.2.0

--- Casks/s/score.rb
+++ Casks/s/score.rb
@@ -1,9 +1,9 @@
 cask "score" do
   arch arm: "AppleSilicon", intel: "Intel"
 
-  version "3.1.14"
-  sha256 arm:   "d13ffe554a76a0c4a80854e364138d60ea40b09322581001ba26c3a231f19ba4",
-         intel: "130160990470de3bc501c118e371a8623c230c52e6cbab86794289239cec36f9"
+  version "3.2.0"
+  sha256 arm:   "6dbc9a18d286988a0f357be32a89395f627ac5bec1534a17f8eaaa284238713e",
+         intel: "1ada9e1a840a2f2b9c8e74a871c40c10a5ed72d6724d016b2d915c6e9c60b428"
 
   url "https://github.com/ossia/score/releases/download/v#{version}/ossia.score-#{version}-macOS-#{arch}.dmg",
       verified: "github.com/ossia/score/"
