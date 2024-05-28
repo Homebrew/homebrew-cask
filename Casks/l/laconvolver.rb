7bb6f6afcd3beb51d11ebff6fae0e123e63143c1
Viktor Szakats <commit@vsz.me>
2024-05-28 22:49:02 +0200

    laconvolver: upgrade URLs to HTTPS

--- Casks/l/laconvolver.rb
+++ Casks/l/laconvolver.rb
@@ -2,10 +2,10 @@ cask "laconvolver" do
   version "0.4"
   sha256 "20c79ebe117936d1abb1bc700018b4253072e3e8567041805ea71cbdecf51242"
 
-  url "http://audio.lernvall.com/LAConvolver#{version}.zip"
+  url "https://audio.lernvall.com/LAConvolver#{version}.zip"
   name "LAConvolver"
   desc "AU plug-in for convolving the audio stream with an impulse response audio file"
-  homepage "http://audio.lernvall.com/"
+  homepage "https://audio.lernvall.com/"
 
   livecheck do
     url :homepage
