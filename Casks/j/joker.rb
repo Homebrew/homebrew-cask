96e28b889bbd01bbb848a3dda5683387e10f6f3d
Viktor Szakats <commit@vsz.me>
2024-05-28 22:49:02 +0200

    joker: upgrade URLs to HTTPS

--- Casks/j/joker.rb
+++ Casks/j/joker.rb
@@ -2,9 +2,9 @@ cask "joker" do
   version :latest
   sha256 :no_check
 
-  url "http://newosxbook.com/tools/joker.tar"
+  url "https://newosxbook.com/tools/joker.tar"
   name "Joker iOS kernelcache handling utility"
-  homepage "http://newosxbook.com/tools/joker.html"
+  homepage "https://newosxbook.com/tools/joker.html"
 
   deprecate! date: "2023-12-17", because: :discontinued
 
