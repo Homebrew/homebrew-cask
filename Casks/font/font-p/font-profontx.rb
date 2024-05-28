f884a90265e30f88297d8d1ce2c0a533f206b3d5
Viktor Szakats <commit@vsz.me>
2024-05-28 22:49:02 +0200

    font-profontx: upgrade URLs to HTTPS

--- Casks/font/font-p/font-profontx.rb
+++ Casks/font/font-p/font-profontx.rb
@@ -2,9 +2,9 @@ cask "font-profontx" do
   version :latest
   sha256 :no_check
 
-  url "http://faisal.com/software/profontx/ProFontX.zip"
+  url "https://www.faisal.com/software/profontx/ProFontX.zip"
   name "ProFontX"
-  homepage "http://faisal.com/software/profontx/"
+  homepage "https://www.faisal.com/software/profontx/"
 
   font "ProFontX/ProFontX"
 
