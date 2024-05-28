b8b14da61283d08521903ad326b45512c6dc776f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-28 12:50:29 +0000

    phpstorm 2024.1.2

--- Casks/p/phpstorm.rb
+++ Casks/p/phpstorm.rb
@@ -1,9 +1,9 @@
 cask "phpstorm" do
   arch arm: "-aarch64"
 
-  version "2024.1.1,241.15989.102"
-  sha256 arm:   "b6b2b6181c724c320a491cea013c620f662e3fbc3a2f62718354d9e88c9d210d",
-         intel: "0232f3e562698bbacf8c72446fc868d50d2b308ce17b6a0655fe351b46370fa4"
+  version "2024.1.2,241.17011.119"
+  sha256 arm:   "3618fb76a47ecb3b1b675516f330a455626d3006338da61ce1b4921448e379c4",
+         intel: "1b1318acb34358478deab7d8e64f8e37f31d5118939678f497bda9c33fe2d2d8"
 
   url "https://download.jetbrains.com/webide/PhpStorm-#{version.csv.first}#{arch}.dmg"
   name "JetBrains PhpStorm"
