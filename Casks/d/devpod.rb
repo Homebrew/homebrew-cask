b83fa2b7199ba85b42b2515f0949d22895023b3f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-28 15:33:14 +0000

    devpod 0.5.12

--- Casks/d/devpod.rb
+++ Casks/d/devpod.rb
@@ -1,9 +1,9 @@
 cask "devpod" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.5.11"
-  sha256 arm:   "8ed977dd12279ea3e7b16a4d8aa41c092f7a5dc04d8582c84f882ef8620dd410",
-         intel: "e945a13c1341408836daf89b628a4a548b5a89a8f6d6b19e066c8ff9a1554e5b"
+  version "0.5.12"
+  sha256 arm:   "2c315732eb53da5d4c67afafcbf73b72172efe7d82bb8001cd1c057a0c00f4e4",
+         intel: "4b0b2b56aecd4bca9bcc51adc7c27ea5673cd7c65cea475ce374facfd810480a"
 
   url "https://github.com/loft-sh/devpod/releases/download/v#{version}/DevPod_macos_#{arch}.dmg",
       verified: "github.com/loft-sh/devpod/"
