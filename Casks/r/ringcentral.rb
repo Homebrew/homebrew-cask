5adb77c9dd393eae4ab86c8c3f51ee076cbaafb9
Klaus Hipp <khipp@users.noreply.github.com>
2024-05-28 09:00:59 +0200

    ringcentral 24.2.24

--- Casks/r/ringcentral.rb
+++ Casks/r/ringcentral.rb
@@ -1,9 +1,9 @@
 cask "ringcentral" do
   arch arm: "-arm64"
 
-  version "24.2.17"
-  sha256 arm:   "9755f38bc969c8388917aedc8b00ca6f7b47e2ec5f7c66490a7ebd88350659b9",
-         intel: "2c9f398b5309eb16eea05ba9db5478ff894d23ca53afbfef860f39607dd7f983"
+  version "24.2.24"
+  sha256 arm:   "e96d8f3dffec8e16ea364ba28c24595318cad3908302820cbf4dadd29d757954",
+         intel: "0adbd4f09d1baf9b1404ae012c877a02f5dae8b489c8ba3fec91b2081bb5a2e4"
 
   url "https://app.ringcentral.com/download/RingCentral-#{version}#{arch}.zip"
   name "RingCentral"
