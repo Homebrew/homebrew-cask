ec5f60524c046ba20719f049f5b4b693ae27d2c5
Viktor Szakats <commit@vsz.me>
2024-05-28 22:49:02 +0200

    font-museo: upgrade homepage to HTTPS

--- Casks/font/font-m/font-museo.rb
+++ Casks/font/font-m/font-museo.rb
@@ -5,7 +5,7 @@ cask "font-museo" do
   url "https://abstractfonts-downloads.s3.amazonaws.com/zips/1/4/8/9/0/contents/museo-300.zip",
       verified: "abstractfonts-downloads.s3.amazonaws.com/"
   name "Museo"
-  homepage "http://www.abstractfonts.com/font/14890"
+  homepage "https://www.abstractfonts.com/font/14890"
 
   font "Museo300-Regular.otf"
   font "Museo700-Regular.otf"
