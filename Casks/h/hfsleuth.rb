436e74ab735bcda506a3b3058f6b6699f3217283
Viktor Szakats <commit@vsz.me>
2024-05-28 22:49:02 +0200

    hfsleuth: upgrade URLs to HTTPS

--- Casks/h/hfsleuth.rb
+++ Casks/h/hfsleuth.rb
@@ -2,10 +2,10 @@ cask "hfsleuth" do
   version :latest
   sha256 :no_check
 
-  url "http://newosxbook.com/files/hfsleuth.tar"
+  url "https://newosxbook.com/files/hfsleuth.tar"
   name "HFSleuth"
   desc "HFS+/HFSX file system inspection tool"
-  homepage "http://newosxbook.com/tools/hfsleuth.html"
+  homepage "https://newosxbook.com/tools/hfsleuth.html"
 
   livecheck do
     skip "unversioned command-line application"
