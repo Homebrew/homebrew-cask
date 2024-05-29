b41b0395af2c3c2ed9b6a9f08d4f97d57af36f31
Viktor Szakats <commit@vsz.me>
2024-05-29 12:42:38 +0200

    emailchemy: use prefix-style S3 URLs

--- Casks/e/emailchemy.rb
+++ Casks/e/emailchemy.rb
@@ -2,8 +2,8 @@ cask "emailchemy" do
   version "14.6"
   sha256 :no_check
 
-  url "https://s3.amazonaws.com/wksdownload/emailchemy/Emailchemy-Mac.dmg",
-      verified: "s3.amazonaws.com/wksdownload/emailchemy/"
+  url "https://wksdownload.s3.amazonaws.com/emailchemy/Emailchemy-Mac.dmg",
+      verified: "wksdownload.s3.amazonaws.com/emailchemy/"
   name "Emailchemy"
   desc "Email migration, conversion and archival software"
   homepage "https://weirdkid.com/emailchemy/"
