919e63283b07e6885f70fdafbb85ef524a5fd4e2
Viktor Szakats <commit@vsz.me>
2024-05-29 13:16:14 +0200

    data-integration: use region-agnostic S3 URLs

--- Casks/d/data-integration.rb
+++ Casks/d/data-integration.rb
@@ -2,8 +2,8 @@ cask "data-integration" do
   version "9.4.0.0-343"
   sha256 "e6804fae1a9aa66b92e781e9b2e835d72d56a6adc53dc03e429a847991a334e8"
 
-  url "https://privatefilesbucket-community-edition.s3.us-west-2.amazonaws.com/#{version}/ce/client-tools/pdi-ce-#{version}.zip",
-      verified: "privatefilesbucket-community-edition.s3.us-west-2.amazonaws.com/"
+  url "https://privatefilesbucket-community-edition.s3.amazonaws.com/#{version}/ce/client-tools/pdi-ce-#{version}.zip",
+      verified: "privatefilesbucket-community-edition.s3.amazonaws.com/"
   name "Pentaho Data Integration"
   desc "End to end data integration and analytics platform"
   homepage "https://www.hitachivantara.com/en-us/products/pentaho-platform/data-integration-analytics/pentaho-community-edition.html"
