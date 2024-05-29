83a6f0bc10df010b5c638c7aa71499f555874be8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-29 21:49:21 +0000

    treesheets 9293446795

--- Casks/t/treesheets.rb
+++ Casks/t/treesheets.rb
@@ -1,6 +1,6 @@
 cask "treesheets" do
-  version "9276163611"
-  sha256 "c521c3455995c4329369764757f34fd43b6badafc67b00de6f4f4354ceadfdf6"
+  version "9293446795"
+  sha256 "69ca9a2f35e9fb244da2374abf0ab4857a678e4065555b93539834c87991ed9c"
 
   url "https://github.com/aardappel/treesheets/releases/download/#{version}/mac_treesheets.zip",
       verified: "github.com/aardappel/treesheets/"
