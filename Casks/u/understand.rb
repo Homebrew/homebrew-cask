78e186bacd63948d8aa07b30b5ac0fcd09fa26fc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-29 21:50:32 +0000

    understand 6.5.1187

--- Casks/u/understand.rb
+++ Casks/u/understand.rb
@@ -1,6 +1,6 @@
 cask "understand" do
-  version "6.5.1186"
-  sha256 "1e0804c6df8d3d1d1ee282b21992ee1ec89433c4a2dd816d8aad6ff74129d18b"
+  version "6.5.1187"
+  sha256 "7d855f42836064dc8cd4e4f242f642b3c1db982e9d6d5f52780ce5907a1f9d43"
 
   url "https://latest.scitools.com/Understand/Understand-#{version}-macOS-Universal.dmg"
   name "SciTools Understand"
