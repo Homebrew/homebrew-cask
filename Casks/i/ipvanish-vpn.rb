3979f6d61b9c41180ed1609e54391b5018dac913
Viktor Szakats <commit@vsz.me>
2024-05-29 12:42:23 +0200

    ipvanish-vpn: use prefix-style S3 URLs

--- Casks/i/ipvanish-vpn.rb
+++ Casks/i/ipvanish-vpn.rb
@@ -2,8 +2,8 @@ cask "ipvanish-vpn" do
   version "4.0.5,95100"
   sha256 :no_check
 
-  url "https://s3.amazonaws.com/ipvanish-apps/software/osx/IPVanish.dmg",
-      verified: "s3.amazonaws.com/ipvanish-apps/software/osx/"
+  url "https://ipvanish-apps.s3.amazonaws.com/software/osx/IPVanish.dmg",
+      verified: "ipvanish-apps.s3.amazonaws.com/software/osx/"
   name "IPVanish"
   desc "VPN client"
   homepage "https://www.ipvanish.com/"
