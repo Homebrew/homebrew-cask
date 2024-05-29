cbc922ecea7189945a737643db264de367a32aad
Viktor Szakats <commit@vsz.me>
2024-05-29 13:04:32 +0200

    orka: use region-agnostic S3 URLs

--- Casks/o/orka.rb
+++ Casks/o/orka.rb
@@ -2,8 +2,8 @@ cask "orka" do
   version "2.4.0"
   sha256 "c7f6fbd9bfca29353618f22736d1d83234239fa1a00cb4493c62a853ebaa5abb"
 
-  url "https://cli-builds-public.s3-eu-west-1.amazonaws.com/official/#{version}/macos/orka.pkg",
-      verified: "cli-builds-public.s3-eu-west-1.amazonaws.com/official/"
+  url "https://cli-builds-public.s3.amazonaws.com/official/#{version}/macos/orka.pkg",
+      verified: "cli-builds-public.s3.amazonaws.com/official/"
   name "Orka CLI"
   desc "Orchestration with Kubernetes on Apple"
   homepage "https://orkadocs.macstadium.com/docs"
