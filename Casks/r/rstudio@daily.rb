2934380dedaabf3508cc1663d461250b07d101e7
Viktor Szakats <commit@vsz.me>
2024-05-29 12:40:09 +0200

    rstudio@daily: use prefix-style S3 URLs

--- Casks/r/rstudio@daily.rb
+++ Casks/r/rstudio@daily.rb
@@ -2,8 +2,8 @@ cask "rstudio@daily" do
   version "2024.07.0-daily-236"
   sha256 "33229755fbd0c532328765a18deb27a78bcd6f038aa16cf58596fea4b52adead"
 
-  url "https://s3.amazonaws.com/rstudio-ide-build/electron/macos/RStudio-#{version}.dmg",
-      verified: "s3.amazonaws.com/rstudio-ide-build/electron/macos/"
+  url "https://rstudio-ide-build.s3.amazonaws.com/electron/macos/RStudio-#{version}.dmg",
+      verified: "rstudio-ide-build.s3.amazonaws.com/electron/macos/"
   name "RStudio Daily"
   desc "Data science software focusing on R and Python"
   homepage "https://dailies.rstudio.com/"
