acc77f0e37cf0bfa829fa07542bbed4ba56a995c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-30 12:50:55 +0000

    mendeley-reference-manager 2.116.0

--- Casks/m/mendeley-reference-manager.rb
+++ Casks/m/mendeley-reference-manager.rb
@@ -1,6 +1,6 @@
 cask "mendeley-reference-manager" do
-  version "2.115.0"
-  sha256 "58700d0e69457e8dad5c993be63a0caea6bb7bb5f012393d6113bef667718e3b"
+  version "2.116.0"
+  sha256 "72d4e92221e7b980f162941f0883a42b3cd717a3441bf5f9a4eefa337949aabb"
 
   url "https://static.mendeley.com/bin/desktop/mendeley-reference-manager-#{version}-x64.dmg"
   name "Mendeley Reference Manager"
