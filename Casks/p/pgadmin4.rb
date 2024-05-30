d21d534e2c7e8781f41832e92e47be9e9b543aca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-30 09:46:39 +0000

    pgadmin4 8.7

--- Casks/p/pgadmin4.rb
+++ Casks/p/pgadmin4.rb
@@ -1,9 +1,9 @@
 cask "pgadmin4" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "8.6"
-  sha256 arm:   "6242ccdc2f351e8fd33dfc6d1c891a7a834f840e0a0b71d4d5bfdb6bd786221f",
-         intel: "0adc70f1f110eacba392e351d875fff8445f73d2310366e18922fbe181a34f28"
+  version "8.7"
+  sha256 arm:   "ab2f71135f855557f20e09c8602c4510d9d07553c643db3b81ea1cf3941fbe76",
+         intel: "c627aa57f1aad48b788879a37050d1b25931ad01ec101f737dd2c81e23d11057"
 
   url "https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v#{version}/macos/pgadmin4-#{version}-#{arch}.dmg",
       verified: "ftp.postgresql.org/pub/pgadmin/pgadmin4/"
