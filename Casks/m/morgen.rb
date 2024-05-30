71cbfb8bdba8f9f86d616a78169e1ba1524d2d01
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-30 12:52:23 +0000

    morgen 3.4.4

--- Casks/m/morgen.rb
+++ Casks/m/morgen.rb
@@ -1,9 +1,9 @@
 cask "morgen" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.4.3,240520t6c3vaj17"
-  sha256 arm:   "d8a95374571efbd41c01ed9670bf67fdc85b035f2a717df845f398aa9d27aa33",
-         intel: "32ec8a2aa394264efe71c0434f86499f6a55f4d97440a2f8e4b15a02eb81a062"
+  version "3.4.4,240529k23icdsjp"
+  sha256 arm:   "9d861969a594ba0048c854bf2b010e4a77175c33950903cc938364fc476e390d",
+         intel: "8d877e3204e3688b83d3b431d2d78914ec640f9382c4c61262c29737ee52a209"
 
   url "https://download.todesktop.com/210203cqcj00tw1/Morgen%20#{version.csv.first}%20-%20Build%20#{version.csv.second}-#{arch}.dmg",
       verified: "download.todesktop.com/210203cqcj00tw1/"
