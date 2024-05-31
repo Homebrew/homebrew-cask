1ebadca5653f791ea5454b4799ec81a842e5d65a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-31 13:01:53 +0000

    zotero@beta 7.0.0-beta.83

--- Casks/z/zotero@beta.rb
+++ Casks/z/zotero@beta.rb
@@ -1,6 +1,6 @@
 cask "zotero@beta" do
-  version "7.0.0-beta.82,79fa160e4"
-  sha256 "caeecc2d8cc9aa0ed968ea7c2b03aaac2140cd1d5d0c9dc23f5fba482b9115b0"
+  version "7.0.0-beta.83,066eda731"
+  sha256 "79341dd0bc7a6f7457d33d8e4733c15355c8a67adef20a921162c2d7f43ace24"
 
   url "https://download.zotero.org/client/beta/#{version.csv.first}%2B#{version.csv.second}/Zotero-#{version.csv.first}%2B#{version.csv.second}.dmg"
   name "Zotero Beta"
