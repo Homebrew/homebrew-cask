93f67ad3145520c1581b858ff3f7aacbc56b7a0b
Manav Rathi <manav@ente.io>
2024-05-31 12:33:35 +0530

    [ente] Skip pre-releases during livecheck
    Add a livecheck block to ignore pre-releases.

--- Casks/e/ente.rb
+++ Casks/e/ente.rb
@@ -8,6 +8,11 @@ cask "ente" do
   desc "Desktop client for Ente Photos"
   homepage "https://ente.io/"
 
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
   auto_updates true
 
   app "ente.app"
