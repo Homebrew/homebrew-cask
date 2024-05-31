32cf20198bf5e465d9131efa131d9b2336948271
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-05-31 11:03:26 -0400

    app-cleaner: updates
    * Add versioned URL
    
    * Add `sha256`
    
    * Add `auto_updates true`
    
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/a/app-cleaner.rb
+++ Casks/a/app-cleaner.rb
@@ -1,17 +1,18 @@
 cask "app-cleaner" do
-  version "8.2.7"
-  sha256 :no_check
+  version "8.2.7,1976"
+  sha256 "990d22472bc284455286c28f5f9ba4f9bb1047985b61089ad8d04d368387bac1"
 
-  url "https://nektony.com/download/app-cleaner-uninstaller/app-cleaner-uninstaller.dmg"
+  url "https://download.nektony.com/download/app-cleaner-uninstaller/app-cleaner-uninstaller.dmg?build=#{version.csv.second}"
   name "Nektony App Cleaner & Uninstaller"
   desc "Uninstaller and cleaning assistant"
   homepage "https://nektony.com/mac-app-cleaner"
 
   livecheck do
     url "https://download.nektony.com/pro-support/v3/app-cleaner/update/update.xml"
-    strategy :sparkle, &:short_version
+    strategy :sparkle
   end
 
+  auto_updates true
   depends_on macos: ">= :high_sierra"
 
   app "App Cleaner #{version.major}.app"
