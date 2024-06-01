80a22e6962988507bdff7ba0d582366cf494a65f
Aaron-212 <aaron212cn@outlook.com>
2024-06-02 00:36:14 +0800

    update font-migmix-1p to use osdn mirror

--- Casks/font/font-m/font-migmix-1p.rb
+++ Casks/font/font-m/font-migmix-1p.rb
@@ -2,7 +2,8 @@ cask "font-migmix-1p" do
   version "20150712"
   sha256 "d71aa59146c600bc2f22de87495fe0127741fbb692736be0e1fe454e128c9d76"
 
-  url "https://osdn.dl.osdn.jp/mix-mplus-ipa/63544/migmix-1p-#{version}.zip"
+  url "https://ftp.iij.ad.jp/pub/osdn.jp/mix-mplus-ipa/63544/migmix-1p-#{version}.zip",
+    verified: "ftp.iij.ad.jp/pub/osdn.jp/mix-mplus-ipa/"
   name "MigMix 1P"
   homepage "https://mix-mplus-ipa.osdn.jp/migmix/#migmix1p"
 
