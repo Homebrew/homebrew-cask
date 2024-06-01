410d2eef8a05c57aed74d7b577c2faf5739bba17
Aaron-212 <aaron212cn@outlook.com>
2024-06-02 00:38:55 +0800

    update font-migu-1p to use osdn mirror

--- Casks/font/font-m/font-migu-1p.rb
+++ Casks/font/font-m/font-migu-1p.rb
@@ -2,7 +2,8 @@ cask "font-migu-1p" do
   version "20150712"
   sha256 "9406399eeb94bb98f0844c2cd6bc94c390d994e6705af56e550d27f2a30b4bd5"
 
-  url "https://osdn.dl.osdn.jp/mix-mplus-ipa/63545/migu-1p-#{version}.zip"
+  url "https://ftp.iij.ad.jp/pub/osdn.jp/mix-mplus-ipa/63545/migu-1p-#{version}.zip",
+    verified: "ftp.iij.ad.jp/pub/osdn.jp/mix-mplus-ipa/"
   name "Migu 1P"
   homepage "https://mix-mplus-ipa.osdn.jp/migu/#migu1p"
 
