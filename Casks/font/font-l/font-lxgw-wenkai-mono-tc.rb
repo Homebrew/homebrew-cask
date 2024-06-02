4e27a5a424b4d81c8e08985d2b800873c77160d5
BrewTestBot <BrewTestBot@users.noreply.github.com>
2024-06-02 00:12:40 +0000

    Update Google Fonts

--- Casks/font/font-l/font-lxgw-wenkai-mono-tc.rb
+++ Casks/font/font-l/font-lxgw-wenkai-mono-tc.rb
@@ -3,10 +3,11 @@ cask "font-lxgw-wenkai-mono-tc" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/lxgwwenkaimonotc"
   name "LXGW WenKai Mono TC"
-  homepage "https://github.com/aaronbell/LxgwWenkaiTC"
+  homepage "https://fonts.google.com/specimen/LXGW+WenKai+Mono+TC"
 
   font "LXGWWenKaiMonoTC-Bold.ttf"
   font "LXGWWenKaiMonoTC-Light.ttf"
