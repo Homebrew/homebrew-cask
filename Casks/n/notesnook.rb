eac13ec2f3afbb4d34bc03bb9eebbe8098942f14
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-03 12:48:45 +0000

    notesnook 3.0.8

--- Casks/n/notesnook.rb
+++ Casks/n/notesnook.rb
@@ -1,9 +1,9 @@
 cask "notesnook" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.0.6"
-  sha256 arm:   "266eeb65612c32ca632ab30e0c63c11893f42397c5dda8fe59774136c9afe23c",
-         intel: "03445b9ba6e8608d81dcd7be15ccc0337aa6b4cd8ac0d583f82d8dd2c81646d8"
+  version "3.0.8"
+  sha256 arm:   "0f92885c78735d704e11ca0e9f640a2946d518c5a1456f8bedf8314712e95ffd",
+         intel: "b93e31a382d3ef48eaedb1e6898bb814bf0595da697366a2f32119cc633333a4"
 
   url "https://github.com/streetwriters/notesnook/releases/download/v#{version}/notesnook_mac_#{arch}.dmg",
       verified: "github.com/streetwriters/notesnook/"
