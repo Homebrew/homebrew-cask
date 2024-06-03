1a15a16ee355de464aef12bc209160bf659b00d1
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-06-03 13:54:52 +0000

    warcraft-logs-uploader 8.5.12

--- Casks/w/warcraft-logs-uploader.rb
+++ Casks/w/warcraft-logs-uploader.rb
@@ -1,6 +1,6 @@
 cask "warcraft-logs-uploader" do
-  version "8.5.10"
-  sha256 "30d3041710b6899455e1b1a81626b2b3c7c448d86e54a31dc1fd366b85966d95"
+  version "8.5.12"
+  sha256 "568bc2995d41e84f30f0246477726bee024e9157477a512727d1817789bed275"
 
   url "https://github.com/RPGLogs/Uploaders-warcraftlogs/releases/download/v#{version}/warcraftlogs-v#{version}.dmg",
       verified: "github.com/RPGLogs/Uploaders-warcraftlogs/"
