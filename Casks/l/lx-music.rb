97ee3ebcc84e8fc7ce2c09f7e5e7147170ca074c
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-06-03 09:46:21 -0400

    lx-music 2.8.0

--- Casks/l/lx-music.rb
+++ Casks/l/lx-music.rb
@@ -1,9 +1,9 @@
 cask "lx-music" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.7.0"
-  sha256 arm:   "0f1a6f6001f34ddabb436e5a34b69ebe9f4105f39d4c48e72e0b390e7aeb294b",
-         intel: "cfe8b1b456cb804afad287d2fd3cca3de2879f0473063445312771e556b07afb"
+  version "2.8.0"
+  sha256 arm:   "d60f678df1cab75130accfd25197113eb7ddb2b0a8558698e815a29a1feff333",
+         intel: "5c9ebfd3c3a19b962f9c6b1f68ca2abf67839a49b09db9e795f30d606156c97b"
 
   url "https://github.com/lyswhut/lx-music-desktop/releases/download/v#{version}/lx-music-desktop-#{version}-#{arch}.dmg"
   name "LX Music Assistant Desktop Edition"
