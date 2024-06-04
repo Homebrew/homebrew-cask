532b0a9fc190c7dabb96e39d1e36f4655c5c6428
adithyasunil26 <adithyasunil26@gmail.com>
2024-06-04 16:38:00 +0530

    sunsama 2.3.3

--- Casks/s/sunsama.rb
+++ Casks/s/sunsama.rb
@@ -1,9 +1,9 @@
 cask "sunsama" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.3.1,240227k9s7v6ynn"
-  sha256 arm:   "1712abe95e6d7feeebd33f56b8c9f3d9ad84bab7585d57f1a3eb3b913fd35050",
-         intel: "930678b166cc27baff85abf7250c1665bc74fcb1b7c8d59c56ca5b0782c78009"
+  version "2.3.3,240603i64u3o2ox"
+  sha256 arm:   "9a5122ec6ac9d0fa2d5a585f1bf1989852a958b6c0f480530e3458945c031f12",
+         intel: "21b64cbb17ff1bf30d7332890f8ae6fe5153caa6468919d138bf3499554c424a"
 
   url "https://download.todesktop.com/2003096gmmnl0g1/Sunsama%20#{version.csv.first}%20-%20Build%20#{version.csv.second}-#{arch}-mac.zip",
       verified: "download.todesktop.com/2003096gmmnl0g1/"
