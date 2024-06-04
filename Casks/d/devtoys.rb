2504319980d5a0f8c7b66f52359960f12beda3b3
Etienne Baudoux <ebaudoux@velersoftware.com>
2024-06-03 21:01:21 -0700

    Updated version and hash

--- Casks/d/devtoys.rb
+++ Casks/d/devtoys.rb
@@ -1,8 +1,8 @@
 cask "devtoys" do
   arch arm: "arm64", intel: "x64"
-  version "2.0.0.3"
-  sha256 arm:   "70385adb55941d47b145c2e7622c7efb972bd04d3d3013846f06f174ecf1b1b6",
-         intel: "66b5065c8a39e78bd4a0bad69f26faf892652cf8d95909b1ac2dfc5282301b40"
+  version "2.0.1.0"
+  sha256 arm:   "15b81d500ec20f608abf837acdc52c1fd6d22163de789c72683e5fc166808ec5",
+         intel: "f397473471b1b9cae40ae005912c096bd4cda8572325bd0d515a07fbf6a6b1a6"
 
   url "https://github.com/DevToys-app/DevToys/releases/download/v#{version}/devtoys_osx_#{arch}.zip"
   name "DevToys"
