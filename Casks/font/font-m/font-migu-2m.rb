a4f8853c5e8bc0b08fe15c39edddc9a619458829
Aaron-212 <aaron212cn@outlook.com>
2024-06-04 22:11:19 +0800

    update font-migu-2m to use github, bump to 2023.1123

--- Casks/font/font-m/font-migu-2m.rb
+++ Casks/font/font-m/font-migu-2m.rb
@@ -1,14 +1,18 @@
 cask "font-migu-2m" do
-  version "20150712"
-  sha256 "659a6a121dadb6eac78369b9d129e2ec77a09fa292ca20932e42a5c753874297"
+  version "2023.1123"
+  sha256 "e7845f148772c984064c325eba70ed4dfb9a27084c2011a3a1b6194be6e439e5"
 
-  url "https://ftp.iij.ad.jp/pub/osdn.jp/mix-mplus-ipa/63545/migu-2m-#{version}.zip",
-    verified: "ftp.iij.ad.jp/pub/osdn.jp/mix-mplus-ipa/"
+  url "https://github.com/itouhiro/mixfont-mplus-ipa/releases/download/v#{version}/migu-2m-#{version.no_dots}.zip",
+      verified: "github.com/itouhiro/mixfont-mplus-ipa/"
   name "Migu 2M"
-  homepage "https://mix-mplus-ipa.osdn.jp/migu/#migu2m"
+  homepage "https://itouhiro.github.io/mixfont-mplus-ipa/migu/"
 
   livecheck do
-    skip "No version information available"
+    url :homepage
+    strategy :page_match do |page|
+      page.scan(%r{href=.*?/migu-2m-(\d*)\.zip}i)
+          .map { |match| match[0].insert(4, ".") }
+    end
   end
 
   font "migu-2m-#{version}/migu-2m-bold.ttf"
