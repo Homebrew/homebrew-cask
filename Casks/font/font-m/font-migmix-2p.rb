73e8b98e0f005174d2ae6f98a9bc94c418737e0f
Aaron-212 <aaron212cn@outlook.com>
2024-06-04 22:10:25 +0800

    update font-migmix-2p to use github, bump to 2023.1123

--- Casks/font/font-m/font-migmix-2p.rb
+++ Casks/font/font-m/font-migmix-2p.rb
@@ -1,14 +1,18 @@
 cask "font-migmix-2p" do
-  version "20150712"
-  sha256 "b9289b61661ed2c158230651a963724618620607b060ae9701f1c5bbedfdee7f"
+  version "2023.1123"
+  sha256 "be93ac23840224586b58bdd7a468d22e28affa6e49f4e2812bb03961706ac278"
 
-  url "https://ftp.iij.ad.jp/pub/osdn.jp/mix-mplus-ipa/63544/migmix-2p-#{version}.zip",
-    verified: "ftp.iij.ad.jp/pub/osdn.jp/mix-mplus-ipa/"
+  url "https://github.com/itouhiro/mixfont-mplus-ipa/releases/download/v#{version}/migmix-2p-#{version.no_dots}.zip",
+      verified: "github.com/itouhiro/mixfont-mplus-ipa/"
   name "MigMix 2P"
-  homepage "https://mix-mplus-ipa.osdn.jp/migmix/#migmix2p"
+  homepage "https://itouhiro.github.io/mixfont-mplus-ipa/migmix/"
 
   livecheck do
-    skip "No version information available"
+    url :homepage
+    strategy :page_match do |page|
+      page.scan(%r{href=.*?/migmix-2p-(\d*)\.zip}i)
+          .map { |match| match[0].insert(4, ".") }
+    end
   end
 
   font "migmix-2p-#{version}/migmix-2p-bold.ttf"
