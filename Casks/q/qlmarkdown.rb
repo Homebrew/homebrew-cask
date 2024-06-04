4bb0e71dd42d1e86576456a0dc95d825b004521b
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-06-04 19:49:09 -0400

    qlmarkdown 1.0.18

--- Casks/q/qlmarkdown.rb
+++ Casks/q/qlmarkdown.rb
@@ -1,6 +1,6 @@
 cask "qlmarkdown" do
-  version "1.0.17"
-  sha256 "9f6265557db18fed69fd63a956557ccf0f4d980e323caa4a3957e247bb7a3926"
+  version "1.0.18"
+  sha256 "e2f42e96d938af05a3fb7ed4c7f18c84d8b12ecab8ab0862fdc418056b58aaa9"
 
   url "https://github.com/sbarex/QLMarkdown/releases/download/#{version}/QLMarkdown.zip"
   name "sbarex QLMarkdown"
