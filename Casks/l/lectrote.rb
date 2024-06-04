7c3179410e55f9af25b579a17c90d68eb0c4a5a2
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-06-04 08:26:12 -0400

    lectrote 1.4.6

--- Casks/l/lectrote.rb
+++ Casks/l/lectrote.rb
@@ -1,6 +1,6 @@
 cask "lectrote" do
-  version "1.4.5"
-  sha256 "e0aca6c4348e9e96a45a81c4f38a937686f59f492d391d626fcb9f8791a63b4f"
+  version "1.4.6"
+  sha256 "52d890a6685121d56dce401dcc76ef520885137c52d99f3a62777ac949182370"
 
   url "https://github.com/erkyrath/lectrote/releases/download/lectrote-#{version}/Lectrote-#{version}-macos-universal.dmg"
   name "lectrote"
