32347feedf4f93f53c514bc101a3adca77b4439f
Anton Engelhardt <106314688+antonengelhardt@users.noreply.github.com>
2024-06-05 16:58:31 +0200

    fix(sha): yanked version with new sha

--- Casks/j/jamie.rb
+++ Casks/j/jamie.rb
@@ -2,8 +2,8 @@ cask "jamie" do
   arch arm: "-arm64"
 
   version "4.0.10"
-  sha256 arm:   "9b74d981535d4cb9ed689d5bf5b99f364e65268fb90a54f254eb7ad13f750c30",
-         intel: "9fbb972d291dff6a0edfff9d2ac12bf33f5e2e9d343e13c707715f3afe97e372"
+  sha256 arm:   "7fc52d1ee9d514ed006648416e34b4db94612a85a95b39c51d43f943a508b19b",
+         intel: "815d4335bdeb22623618e769c0dcca7cc03698cad75351c0b28e8f04ef957d00"
 
   url "https://github.com/louismorgner/jamie-release/releases/download/v#{version}/jamie-#{version}#{arch}.dmg",
       verified: "github.com/louismorgner/jamie-release/"
