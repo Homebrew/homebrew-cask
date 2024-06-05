0518a713e2726b014e54a316a8ea71e97e2185b6
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-06-04 20:34:40 -0400

    simplex 5.8.0

--- Casks/s/simplex.rb
+++ Casks/s/simplex.rb
@@ -1,9 +1,9 @@
 cask "simplex" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "5.7.5"
-  sha256 arm:   "7eb3ce28c214666417cceafaec210a56b514fc7ef7ecefd353cea5a70340846f",
-         intel: "67b905d1a42d1d44e713e61a2022612ad729cb2df012cdded02444eeb384f284"
+  version "5.8.0"
+  sha256 arm:   "62737ec91ba421ca761b5c39d81d7d4cfe198d7331353edfa6ac219e7d8866f5",
+         intel: "721ffb4e97c3d1a1122b2d02184790fadb2333f452c1c96f76ec2eeeb8f5afb1"
 
   url "https://github.com/simplex-chat/simplex-chat/releases/download/v#{version}/simplex-desktop-macos-#{arch}.dmg",
       verified: "github.com/simplex-chat/simplex-chat/"
