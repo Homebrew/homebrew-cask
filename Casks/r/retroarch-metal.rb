4635c85586835f773a5a52f9f41d8a2d3c61c7ac
Ömercan Kömür <fpscan@gmail.com>
2024-06-05 17:54:52 +0000

    retroarch version update

--- Casks/r/retroarch-metal.rb
+++ Casks/r/retroarch-metal.rb
@@ -1,11 +1,11 @@
 cask "retroarch-metal" do
-  version "1.19.0"
+  version "1.19.1"
   sha256 :no_check # required as upstream package is often updated in place
 
   url "https://buildbot.libretro.com/stable/#{version}/apple/osx/universal/RetroArch_Metal.dmg"
   name "RetroArch"
-  desc "Emulator frontend (Metal graphics API version)"
-  homepage "https://www.libretro.com/"
+  desc "RetroArch is a frontend for emulators, game engines and media players. (Metal graphics API version)"
+  homepage "https://www.retroarch.com/"
 
   livecheck do
     cask "retroarch"
--- Casks/r/retroarch.rb
+++ Casks/r/retroarch.rb
@@ -1,11 +1,11 @@
 cask "retroarch" do
-  version "1.19.0"
+  version "1.19.1"
   sha256 :no_check # required as upstream package is often updated in place
 
   url "https://buildbot.libretro.com/stable/#{version}/apple/osx/x86_64/RetroArch.dmg"
   name "RetroArch"
-  desc "Emulator frontend (OpenGL graphics API version)"
-  homepage "https://www.libretro.com/"
+  desc "RetroArch is a frontend for emulators, game engines and media players. (OpenGL graphics API version)"
+  homepage "https://www.retroarch.com/"
 
   livecheck do
     url "https://buildbot.libretro.com/stable/"
