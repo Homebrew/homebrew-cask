5b7643b584b9bde6b71471485f8be1bbb6f52285
Kassio Borges <kassioborgesm@gmail.com>
2024-06-05 08:53:44 +0100

    font-0xproto 2.100
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>
    
    font-0xproto 2.100

--- Casks/font/font-0/font-0xproto.rb
+++ Casks/font/font-0/font-0xproto.rb
@@ -1,14 +1,14 @@
 cask "font-0xproto" do
-  version "2.001"
-  sha256 "fa35e66b4bc045717dcec67f6b6a83a8128b34b1bd024a7f6a689908f15d6cf2"
+  version "2.100"
+  sha256 "3c6fb670f65fde3d5f91301955680c282710d91f55580462152528f3e92849e8"
 
   url "https://github.com/0xType/0xProto/releases/download/#{version}/0xProto_#{version.dots_to_underscores}.zip"
   name "0xProto"
   desc "Programming font focused on source code legibility"
   homepage "https://github.com/0xType/0xProto"
 
-  font "0xProto-Regular.otf"
-  font "0xProto-Italic.otf"
+  font "fonts/0xProto-Regular.otf"
+  font "fonts/0xProto-Italic.otf"
 
   # No zap stanza required
 end
