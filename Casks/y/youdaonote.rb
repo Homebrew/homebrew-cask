aa7e266925e6e2b2b009f3445cf0d37af0adbe0e
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-06-04 20:41:59 -0400

    youdaonote 7.2.182

--- Casks/y/youdaonote.rb
+++ Casks/y/youdaonote.rb
@@ -1,6 +1,6 @@
 cask "youdaonote" do
-  version "7.2.181"
-  sha256 "4581992d0391c72f3b01341bc6ac606dfaab2a1aef5ec0ac085996f8029ca89a"
+  version "7.2.182"
+  sha256 "062ada2c13f6063a66192f6c57444f91c0d34ec722011274602dacde246e720e"
 
   url "https://artifact.lx.netease.com/download/ynote-electron/%E6%9C%89%E9%81%93%E4%BA%91%E7%AC%94%E8%AE%B0-#{version}.zip",
       user_agent: :fake,
