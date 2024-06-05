72020d54f79beff0c77251a9b7f78291320539b4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-05 15:45:09 +0000

    plasticscm-cloud-edition 11.0.16.8673

--- Casks/p/plasticscm-cloud-edition.rb
+++ Casks/p/plasticscm-cloud-edition.rb
@@ -1,6 +1,6 @@
 cask "plasticscm-cloud-edition" do
-  version "11.0.16.8664"
-  sha256 "c9955fc84b8a16d3b12675f28ea4f043df03b69680208310e146f1b9ceb56b1d"
+  version "11.0.16.8673"
+  sha256 "8aa59d6bb0ac737b686398d83bc3bcbe3e4edd9c092051f4db5aa4d2c69048f8"
 
   url "https://d26z97tczqnlef.cloudfront.net/releases/#{version}/plasticscm/osx/unity-vcs-#{version}-mac.pkg.zip",
       verified: "d26z97tczqnlef.cloudfront.net/"
