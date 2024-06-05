024ef5ccf3379a09ecafd1a7e5df8512b42dd69f
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-06-05 19:50:27 -0400

    mailplane 4.3.9

--- Casks/m/mailplane.rb
+++ Casks/m/mailplane.rb
@@ -1,6 +1,6 @@
 cask "mailplane" do
-  version "4.3.8,4942"
-  sha256 "2c66788feebd36e08f672f576169538f93a9a8f7b49c1853db78cf7cdbd74833"
+  version "4.3.9,4945"
+  sha256 "c586ced484bc9bf2f3880f8a85ed387fc4ced2c65682b3dd3ec05c5ba01a37b3"
 
   url "https://builds.mailplaneapp.com/Mailplane_#{version.major}_#{version.csv.second}.tbz"
   name "Mailplane"
