c040ca7fadb588b567e420d49d3a11cef8df9e7c
Valerio <36407154+Scartiloffista@users.noreply.github.com>
2024-06-06 15:18:42 +0200

    chore: bump jordanbaird-ice.rb to 0.9.0

--- Casks/j/jordanbaird-ice.rb
+++ Casks/j/jordanbaird-ice.rb
@@ -1,5 +1,5 @@
 cask "jordanbaird-ice" do
-  version "0.8.0"
+  version "0.9.0"
   sha256 "44e27ccea96dd311c1744b399f6e0b19799665b43aaab38bd3d8715ac84b7713"
 
   url "https://github.com/jordanbaird/Ice/releases/download/#{version}/Ice.zip"
