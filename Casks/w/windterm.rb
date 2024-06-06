b8e800d738ae5b156972cc9432cd098c0c3c7fdb
Miccal Matthews <miccal.matthews@gmail.com>
2024-06-06 12:29:52 +0800

    Update windterm.rb

--- Casks/w/windterm.rb
+++ Casks/w/windterm.rb
@@ -1,15 +1,21 @@
 cask "windterm" do
-  version "2.6.1"
+  version "2.6.1,2.6.0"
   sha256 "c1a1ff2263b3ded7816c8d2e7309e77f3d0901d488c8559281cfd4d2bb7b891b"
 
-  url "https://github.com/kingToolbox/WindTerm/releases/download/#{version}/WindTerm_#{version}_Mac_Portable_x86_64.dmg"
+  url "https://github.com/kingToolbox/WindTerm/releases/download/#{version.csv.second}/WindTerm_#{version.csv.first}_Mac_Portable_x86_64.dmg"
   name "WindTerm"
   desc "SSH/SFTP/Shell/Telnet/Serial terminal"
   homepage "https://github.com/kingToolbox/WindTerm"
 
   livecheck do
     url :url
-    strategy :github_latest
+    regex(/WindTerm\sv?(\d+(?:\.\d+)+)/i)
+    strategy :github_latest do |item|
+      version = item["name"][regex, 1]
+      next if version.blank?
+
+      "#{version},#{item["tag_name"]}"
+    end
   end
 
   auto_updates true
@@ -21,4 +27,8 @@ cask "windterm" do
     "~/Library/Preferences/KingToolbox.WindTerm.plist",
     "~/Library/Saved Application State/KingToolbox.WindTerm.savedState",
   ]
+
+  caveats do
+    requires_rosetta
+  end
 end
