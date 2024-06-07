22c0fbe403477c2ae8043830e9b5137ea319cf66
Eric Knibbe <enk3@outlook.com>
2024-06-07 19:37:15 -0400

    dash: remove legacy version

--- Casks/d/dash.rb
+++ Casks/d/dash.rb
@@ -1,12 +1,6 @@
 cask "dash" do
-  on_high_sierra :or_older do
-    version "4.6.7"
-    sha256 "e2b5eb996645b25f12ccae15e24b1b0d8007bc5fed925e14ce7be45a2b693fb6"
-  end
-  on_mojave :or_newer do
-    version "7.2.1"
-    sha256 "815a11f6efd7701830d321918274c36a768c643003f52079a6bb331dd41202f5"
-  end
+  version "7.2.1"
+  sha256 "815a11f6efd7701830d321918274c36a768c643003f52079a6bb331dd41202f5"
 
   url "https://kapeli.com/downloads/v#{version.major}/Dash.zip"
   name "Dash"
@@ -20,7 +14,7 @@ cask "dash" do
 
   auto_updates true
   conflicts_with cask: "dash@6"
-  depends_on macos: ">= :high_sierra"
+  depends_on macos: ">= :mojave"
 
   app "Dash.app"
 
