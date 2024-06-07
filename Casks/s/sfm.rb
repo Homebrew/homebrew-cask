c8ba2b4ad8dae2bd81c1c6703a1f6509b2a4ba0f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-07 18:50:26 +0000

    sfm 1.9.1

--- Casks/s/sfm.rb
+++ Casks/s/sfm.rb
@@ -1,6 +1,6 @@
 cask "sfm" do
-  version "1.9.0"
-  sha256 "04c986f50782e912786d48ba0bb7009ce0c5ded03ecd943d56139fa96be67f1c"
+  version "1.9.1"
+  sha256 "9be0d5c3bbc52ff9cc664e8b0b10000ac473230f4d95cfb0d355e101f8cf5244"
 
   url "https://github.com/SagerNet/sing-box/releases/download/v#{version}/SFM-#{version}-universal.zip",
       verified: "github.com/SagerNet/sing-box/"
