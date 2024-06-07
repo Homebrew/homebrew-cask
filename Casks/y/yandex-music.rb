2c8f6702775a855f456261e1d7106de4d0efb61e
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-06-07 13:19:10 -0400

    yandex-music 5.4.2

--- Casks/y/yandex-music.rb
+++ Casks/y/yandex-music.rb
@@ -1,6 +1,6 @@
 cask "yandex-music" do
-  version "5.3.2"
-  sha256 "03f329f4b4c752c574c2fc59a22aadaabf87697540f3d103d33bdd4c39c1cd0f"
+  version "5.4.2"
+  sha256 "d93b00e853d7ada558ab68533480a9bbc5bfc7fa472bb53a8619495f1fb82b42"
 
   url "https://music-desktop-application.s3.yandex.net/stable/Yandex_Music_universal_#{version}.dmg",
       verified: "music-desktop-application.s3.yandex.net/stable/"
