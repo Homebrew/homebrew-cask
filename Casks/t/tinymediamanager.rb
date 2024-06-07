e9528435db402a9c55bbd342e276d258defb0759
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-06-07 13:09:27 -0400

    tinymediamanager 5.0.6

--- Casks/t/tinymediamanager.rb
+++ Casks/t/tinymediamanager.rb
@@ -1,9 +1,9 @@
 cask "tinymediamanager" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "5.0.5"
-  sha256 arm:   "8ed75f5381bf1978c28730ee8db9f6c504401f57a84c7c972eb58af45b3beeb4",
-         intel: "fc690cb7e2ba33b6b93de18691330ca36f6fac4e1bae60bcbfff59e28fe9024f"
+  version "5.0.6"
+  sha256 arm:   "32d3ce907e01641340cc79837ff6935fb06dfbd61f77e71b29b1283022e988be",
+         intel: "fa78e2801bbb83e2df1ec48ee0e9954f6e85f99f5e4cc5e8f4eee6093e3ddb36"
 
   url "https://release.tinymediamanager.org/v#{version.major}/dist/tinyMediaManager-#{version}-macos-#{arch}.dmg"
   name "tinyMediaManager"
