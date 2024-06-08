57795ca08cdeddf87e96edaa94acec34b2b5c03e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-08 12:52:02 +0000

    wasabi-wallet 2.0.8.1

--- Casks/w/wasabi-wallet.rb
+++ Casks/w/wasabi-wallet.rb
@@ -1,9 +1,9 @@
 cask "wasabi-wallet" do
   arch arm: "-arm64"
 
-  version "2.0.8"
-  sha256 arm:   "078e6d15f61a5a3c3b99886506fac0fb7f830fa7c00981f7b7577d3d57ea9ce9",
-         intel: "c5ba98c35c4a68142053f67d37fabdf5f2c7bba17be59b55d23cc635f6da523c"
+  version "2.0.8.1"
+  sha256 arm:   "2740ea34925bfe917987ef2d649842d518d7083a0dbe9dae17ace4851a96ffb0",
+         intel: "68f9178f2ef32cc2fb371a04bea3250ee1ffcaad77d9224e2b7964ad5804de73"
 
   url "https://github.com/zkSNACKs/WalletWasabi/releases/download/v#{version}/Wasabi-#{version.chomp(".0")}#{arch}.dmg"
   name "Wasabi Wallet"
