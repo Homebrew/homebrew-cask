394abc28844b4018fba93e2ac3662bbb0f5e99b7
Kevin Hermawan <84965338+kevinhermawan@users.noreply.github.com>
2024-06-09 06:10:38 +0700

    ollamac 2.0.0

--- Casks/o/ollamac.rb
+++ Casks/o/ollamac.rb
@@ -1,6 +1,6 @@
 cask "ollamac" do
-  version "1.2.0"
-  sha256 "118d3567a7e26f2e44b74a62c6158876e91bd068864b7f1fabd74d114b7f1846"
+  version "2.0.0"
+  sha256 "615f52db607f16c5c9501bbadb3da80ce5e39c8aa32696912fc37bcb9c9fcb9f"
 
   url "https://github.com/kevinhermawan/Ollamac/releases/download/v#{version}/Ollamac-#{version}.dmg"
   name "Ollamac"
