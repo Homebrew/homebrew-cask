091d369c4cd81ea907f33653dd04386b8f666fdd
Tao Zuhong <taozuhong@users.noreply.github.com>
2024-06-09 21:45:46 +0800

    Update kangaroo-multiple.rb

--- Casks/k/kangaroo-multiple.rb
+++ Casks/k/kangaroo-multiple.rb
@@ -2,7 +2,7 @@ cask "kangaroo-multiple" do
   version "5.0.0.240609"
   sha256 "c11e6f3aa3fa7cea30ec52a2c30a1677a337d9e59b248ee04ca1c21b2a0a5890"
 
-  url "https://github.com/dbkangaroo/kangaroo/releases/download/v#{version}/kangaroo-multiple-#{version}-x86_64.dmg
+  url "https://github.com/dbkangaroo/kangaroo/releases/download/v#{version}/kangaroo-multiple-#{version}-x86_64.dmg"
   name "Kangaroo multiple"
   desc "Native GUI SQL client and admin tool for popular databases"
   homepage "https://www.datatable.online/"
