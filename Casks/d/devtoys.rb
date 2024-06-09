676d2974cc1de37644c1717771321d82e42f05fc
Etienne Baudoux <ebaudoux@velersoftware.com>
2024-06-09 11:11:00 -0700

    fixed style

--- Casks/d/devtoys.rb
+++ Casks/d/devtoys.rb
@@ -1,5 +1,6 @@
 cask "devtoys" do
   arch arm: "arm64", intel: "x64"
+
   version "2.0.1.0"
   sha256 arm:   "15b81d500ec20f608abf837acdc52c1fd6d22163de789c72683e5fc166808ec5",
          intel: "f397473471b1b9cae40ae005912c096bd4cda8572325bd0d515a07fbf6a6b1a6"
@@ -14,10 +15,10 @@ cask "devtoys" do
   app "DevToys.app"
 
   zap trash: [
+    "~/Library/Caches/com.devtoys.preview",
     "~/Library/com.devtoys.preview",
     "~/Library/Preferences/com.devtoys.plist",
     "~/Library/Preferences/com.yuki.DevToys.plist",
-    "~/Library/Caches/com.devtoys.preview",
     "~/Library/WebKit/come.devtoys.app",
   ]
 end
