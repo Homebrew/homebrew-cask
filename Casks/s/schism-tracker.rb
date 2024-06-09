fd864bd5e67ef26856d63aa230c21b8d0de1877a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-09 09:40:36 +0000

    schism-tracker 20240609

--- Casks/s/schism-tracker.rb
+++ Casks/s/schism-tracker.rb
@@ -1,6 +1,6 @@
 cask "schism-tracker" do
-  version "20240529"
-  sha256 "a2ffce5b2135249cae701f4fd3d58ecae7b2c482349cc7e6940e6e2d0fc6f307"
+  version "20240609"
+  sha256 "b1440fab5a2a0426366fed0abc69a05df8d5aa68d7d27c1a9ee683f688903f1b"
 
   url "https://github.com/schismtracker/schismtracker/releases/download/#{version}/schismtracker-#{version}-macos.zip"
   name "Schism Tracker"
