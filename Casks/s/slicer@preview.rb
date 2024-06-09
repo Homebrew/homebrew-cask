3b5ed02e27229c4a796e796cbaf350dd855ed13a
Bo Anderson <mail@boanderson.me>
2024-06-09 23:58:19 +0100

    slicer@preview: remove `url do`

--- Casks/s/slicer@preview.rb
+++ Casks/s/slicer@preview.rb
@@ -2,11 +2,7 @@ cask "slicer@preview" do
   version :latest
   sha256 :no_check
 
-  url "https://download.slicer.org/find?os=macosx&stability=nightly" do |page|
-    require "json"
-    file_path = JSON.parse(page)["download_url"]
-    URI.join(page.url, file_path)
-  end
+  url "https://download.slicer.org/download?os=macosx&stability=nightly"
   name "3D Slicer"
   desc "Medical image processing and visualization system"
   homepage "https://www.slicer.org/"
