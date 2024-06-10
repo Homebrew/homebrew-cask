65681df566c7b50057edc528ddf1434395cdb9f6
Eric Knibbe <enk3@outlook.com>
2024-06-10 08:36:59 -0400

    sitesucker-pro: update legacy versions

--- Casks/s/sitesucker-pro.rb
+++ Casks/s/sitesucker-pro.rb
@@ -1,5 +1,5 @@
 cask "sitesucker-pro" do
-  on_mojave :or_older do
+  on_catalina :or_older do
     version "3.2.7"
     sha256 "dd61a113ad86b580e0faf97b4aa86290e038bb3e098f2d19e67fc9e194ce1a3e"
 
@@ -7,7 +7,15 @@ cask "sitesucker-pro" do
       skip "Legacy version"
     end
   end
-  on_catalina :or_newer do
+  on_big_sur do
+    version "5.2"
+    sha256 "244fa73a0aa73b3786ee6eb4e5d6f6438942853d6d58c5de38b92f11e8d44428"
+
+    livecheck do
+      skip "Legacy version"
+    end
+  end
+  on_monterey :or_newer do
     version "5.3.5"
     sha256 "189f4e60238dbc81d87abd278bc286952aed83cc6b6d94906a5470decd90e94e"
 
