76f4972e6359bb2142b62aa2688278c9341d2040
Nihaal Sangha <nihaal.git@gmail.com>
2024-06-10 20:38:38 +0100

    Add VCam

---
+++ Casks/v/vcam.rb
@@ -0,0 +1,44 @@
+cask "vcam" do
+  version "2.0.133"
+  sha256 "e88aa8ae051bbb82deb112657bd1ece69fbc90c7f06dcfef7e26809103f6fe99"
+
+  url "https://downloads.vcam.ai/mac/VCam.ai_#{version}.pkg"
+  name "VCam"
+  desc "Webcam background tool"
+  homepage "https://vcam.ai/"
+
+  livecheck do
+    url "https://downloads.vcam.ai/mac/updates.txt"
+    regex(/ProductVersion = (\d+(?:\.\d+)+)/i)
+  end
+
+  depends_on macos: ">= :catalina"
+
+  pkg "VCam.ai_#{version}.pkg"
+
+  preflight do
+    retries ||= 3
+    ohai "Attempting to close VCam.app to avoid unwanted user intervention" if retries >= 3
+    return unless system_command "/usr/bin/pkill", args: ["-f", "/Applications/VCam.ai/VCam.app"]
+  rescue RuntimeError
+    sleep 1
+    retry unless (retries -= 1).zero?
+    opoo "Unable to forcibly close VCam.app"
+  end
+
+  uninstall quit:    "ai.vcam.desktop",
+            pkgutil: [
+              "camera-helper",
+              "electron-app",
+              "vcam.ai-uninstall",
+            ],
+            delete:  "/Applications/VCam.ai"
+
+  zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/ai.vcam.desktop.sfl*",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.splitmedialabs.camerahelper.sfl*",
+    "~/Library/Application Support/VCam.ai",
+    "~/Library/Preferences/ai.vcam.desktop.plist",
+    "~/Library/Saved Application State/ai.vcam.desktop.savedState",
+  ]
+end
