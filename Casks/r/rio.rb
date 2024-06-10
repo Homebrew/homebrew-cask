feb4a041733b214d4b1bb4df100f0238ecbf692f
Arjun P <ap2045202@gmail.com>
2024-06-10 16:20:43 -0400

    Bump rio to 0.1.0

--- Casks/r/rio.rb
+++ Casks/r/rio.rb
@@ -1,6 +1,6 @@
 cask "rio" do
-  version "0.0.39"
-  sha256 "73b163a68a87c023a9fd1075f9bf498292cf730a9c91abe89334abb70d042e75"
+  version "0.1.0"
+  sha256 "77a7d198824252e117d2ab091938ed678ecaef81e62871ec2c63699ba23014fa"
 
   url "https://github.com/raphamorim/rio/releases/download/v#{version}/Rio-v#{version}.dmg"
   name "Rio"
