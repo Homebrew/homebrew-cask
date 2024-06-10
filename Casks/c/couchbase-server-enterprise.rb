7181ce6ef5d02f0ba57e235c5c342d54fab6d4e0
Eric Knibbe <enk3@outlook.com>
2024-06-10 09:40:34 -0400

    couchbase-server-enterprise: update requirements

--- Casks/c/couchbase-server-enterprise.rb
+++ Casks/c/couchbase-server-enterprise.rb
@@ -1,36 +1,21 @@
 cask "couchbase-server-enterprise" do
-  on_el_capitan :or_older do
-    version "4.5.1"
-    sha256 "de014c7c134eb97ff00be6b2e6f5d0da84295ce05bbb7bb3a4d3c747a365cd22"
+  version "7.6.1"
+  sha256 "a1fed8b57ba82b655ae7da9d670a4bbf8d1903d9eb38b930a2830d9cb826edc7"
 
-    url "https://packages.couchbase.com/releases/#{version}/couchbase-server-enterprise_#{version}-macos_x86_64.zip"
-
-    livecheck do
-      skip "Legacy version"
-    end
-
-    app "couchbase-server-enterprise_#{version}/Couchbase Server.app"
-  end
-  on_sierra :or_newer do
-    version "7.6.1"
-    sha256 "a1fed8b57ba82b655ae7da9d670a4bbf8d1903d9eb38b930a2830d9cb826edc7"
-
-    url "https://packages.couchbase.com/releases/#{version}/couchbase-server-enterprise_#{version}-macos_x86_64.dmg"
-
-    livecheck do
-      url "https://www.couchbase.com/downloads/"
-      regex(/couchbase[._-]server[._-]enterprise[._-]v?(\d+(:?\.\d+)+)[._-]macos[._-]x86[._-]64\.dmg/i)
-    end
-
-    app "Couchbase Server.app"
-  end
-
+  url "https://packages.couchbase.com/releases/#{version}/couchbase-server-enterprise_#{version}-macos_x86_64.dmg"
   name "Couchbase Server"
   desc "Distributed NoSQL cloud database"
   homepage "https://www.couchbase.com/"
 
+  livecheck do
+    url "https://www.couchbase.com/downloads/"
+    regex(/couchbase[._-]server[._-]enterprise[._-]v?(\d+(:?\.\d+)+)[._-]macos[._-]x86[._-]64\.dmg/i)
+  end
+
   conflicts_with cask: "couchbase-server-community"
-  depends_on macos: ">= :catalina"
+  depends_on macos: ">= :monterey"
+
+  app "Couchbase Server.app"
 
   zap trash: [
     "~/Library/Application Support/Couchbase",
