32d5eb1dff49c19a6a9ef87de19b36e5391d1562
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-06-11 17:38:53 -0400

    klayout 0.29.2
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/k/klayout.rb
+++ Casks/k/klayout.rb
@@ -24,8 +24,8 @@ cask "klayout" do
     end
   end
   on_monterey do
-    version "0.29.1"
-    sha256 "48335aa0c34611aad45f1558b27a88d0021243ba0c72e13ee3b1cdf07d64f156"
+    version "0.29.2"
+    sha256 "1013a442c13f02653fdd160c1933e6dca3edae47d515135ac15511545d48fe6a"
 
     url "https://www.klayout.org/downloads/MacOS/HW-klayout-#{version}-macOS-Monterey-1-qt5MP-RsysPhb311.dmg",
         verified: "klayout.org/downloads/MacOS/"
@@ -36,8 +36,8 @@ cask "klayout" do
     end
   end
   on_ventura do
-    version "0.29.1"
-    sha256 "c6f0b08a1410168bf854103991b59471b4c0ecf7667724706917132b50816d28"
+    version "0.29.2"
+    sha256 "bf226f6936adf9ce43e79c5cbdf0e32d289a11d92b36ef9c576b0d3cd2b364b4"
 
     url "https://www.klayout.org/downloads/MacOS/HW-klayout-#{version}-macOS-Ventura-1-qt5MP-RsysPhb311.dmg",
         verified: "klayout.org/downloads/MacOS/"
@@ -48,8 +48,8 @@ cask "klayout" do
     end
   end
   on_sonoma :or_newer do
-    version "0.29.1"
-    sha256 "5741aefac5ad02011d05500b71b5e9c4fbe86df2e5893538b7148a8efa95581b"
+    version "0.29.2"
+    sha256 "9215b0c4490019afd243b254a132acb7a84bd03e8278d8b01009d98e1db4d7ff"
 
     url "https://www.klayout.org/downloads/MacOS/HW-klayout-#{version}-macOS-Sonoma-1-qt5MP-RsysPhb311.dmg",
         verified: "klayout.org/downloads/MacOS/"
