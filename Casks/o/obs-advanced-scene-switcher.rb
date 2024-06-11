68f40c71fdb030876508d66db2b9b5954aa2c5ce
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-11 18:54:11 +0000

    obs-advanced-scene-switcher 1.26.4

--- Casks/o/obs-advanced-scene-switcher.rb
+++ Casks/o/obs-advanced-scene-switcher.rb
@@ -1,6 +1,6 @@
 cask "obs-advanced-scene-switcher" do
-  version "1.26.3"
-  sha256 "e076726806284cc93f7c8c94b56b6ad7b5a4f8ec1a1f866883adaf40e046dd69"
+  version "1.26.4"
+  sha256 "311f55b35f1b6ea11c5724d4ff574d47e96e325b89ba648f0fbebc01a588ffb7"
 
   url "https://github.com/WarmUpTill/SceneSwitcher/releases/download/#{version}/advanced-scene-switcher-#{version}-macos-universal.pkg",
       verified: "github.com/WarmUpTill/SceneSwitcher/"
