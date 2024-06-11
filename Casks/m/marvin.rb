3d69a301750cb3ecbd17e523f2978af123b3f8b9
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-06-11 17:34:53 -0400

    marvin 1.65.0

--- Casks/m/marvin.rb
+++ Casks/m/marvin.rb
@@ -2,9 +2,9 @@ cask "marvin" do
   arch arm: "-arm64"
   hostname_arch = on_arch_conditional arm: "amarm", intel: "amazingmarvin"
 
-  version "1.64.4"
-  sha256 arm:   "213f87692d55cbcd5997308203f35b8718ac2c18648f0109354a2132b36a675b",
-         intel: "68edd8325fc946305834d304e78dd39ab40336f187a78de90b80e6376047d34c"
+  version "1.65.0"
+  sha256 arm:   "749617692387e9f8738b6c59ada2cf02ff2b5eea93546c25765966d6c82cd68d",
+         intel: "d8b18a2324e4fe66b34b5dcfcd12db7c5353ba85c260bab306156d29f5ae36fa"
 
   url "https://#{hostname_arch}.s3.amazonaws.com/Marvin-#{version}#{arch}-mac.zip",
       verified: "#{hostname_arch}.s3.amazonaws.com/"
