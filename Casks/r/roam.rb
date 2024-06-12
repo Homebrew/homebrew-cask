d3fd407c1fb97179b2a068065d1c560012011a02
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-06-12 11:30:19 -0400

    roam 113.0.1-beta001

--- Casks/r/roam.rb
+++ Casks/r/roam.rb
@@ -1,9 +1,9 @@
 cask "roam" do
   arch arm: "arm64", intel: "x64"
 
-  version "112.0.0-beta001"
-  sha256 arm:   "4e97ce58f63354083049dc3c438d4bdd254e97994e3bfffbc420444f40dd6503",
-         intel: "0f23e056ab3b8ddd3b57c02aa78ccbd0b8805be6b43882920033a816c920a5b6"
+  version "113.0.1-beta001"
+  sha256 arm:   "3cedec3dd32a3f21123d4b26ecc9b850902361737ae6353e1db851532cd957ce",
+         intel: "d5e02b5708f3493f2294edf205b0bc31e64143ad0a61ef9089a90cedc88937d6"
 
   url "https://download.ro.am/Roam/8a86d88cfc9da3551063102e9a4e2a83/latest/darwin/#{arch}/Roam.dmg"
   name "Roam"
