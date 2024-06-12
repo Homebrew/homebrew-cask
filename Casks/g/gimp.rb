247d10f596e2ca339881b2e68a858840a8d4a3e3
Bevan Kay <email@bevankay.me>
2024-06-12 16:41:32 +1000

    gimp 2.10.38,1

--- Casks/g/gimp.rb
+++ Casks/g/gimp.rb
@@ -1,11 +1,11 @@
 cask "gimp" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "2.10.38"
-  sha256 arm:   "509d544470e61570a645940ebc1229145abdf995829a8ef0f5296c7408e9a2d6",
-         intel: "cf0c79201f54b0c8978ba43806199f0d85bb0ac8db3e77f9dc4f6f4393a9d836"
+  version "2.10.38,1"
+  sha256 arm:   "dc1aa78a40695f9f4580ce710960ff411eeef48af45b659b03b51e4cd6cdf4e8",
+         intel: "d2d3ac20c762fe12f0dd0ec8d7c6c2f1f3a43e046ecb4ed815a49afcbaa92b92"
 
-  url "https://download.gimp.org/gimp/v#{version.major_minor}/macos/gimp-#{version}-#{arch}.dmg"
+  url "https://download.gimp.org/gimp/v#{version.major_minor}/macos/gimp-#{version.csv.first}-#{arch}#{version.csv.second ? "-" + version.csv.second : ""}.dmg"
   name "GIMP"
   name "GNU Image Manipulation Program"
   desc "Free and open-source image editor"
@@ -13,7 +13,14 @@ cask "gimp" do
 
   livecheck do
     url "https://www.gimp.org/downloads/"
-    regex(%r{href=.*?/gimp[._-]v?(\d+(?:\.\d+)+(-\d)?)[._-]#{arch}\.dmg}i)
+    regex(%r{href=.*?/gimp[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}(?:-(\d+))?\.dmg}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map do |match|
+        next match[0] unless match[1]
+
+        "#{match[0]},#{match[1]}"
+      end
+    end
   end
 
   conflicts_with cask: "gimp@dev"
