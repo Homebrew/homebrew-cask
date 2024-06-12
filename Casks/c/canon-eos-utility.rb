f40f193fe3a0044ffc16c43be05d8bd93a81fd84
Bevan Kay <email@bevankay.me>
2024-06-12 15:11:23 +1000

    canon-eos-utility 3.18.0.1

--- Casks/c/canon-eos-utility.rb
+++ Casks/c/canon-eos-utility.rb
@@ -1,6 +1,6 @@
 cask "canon-eos-utility" do
-  version "3.17.2.1,0200006949,9"
-  sha256 "aa14d8d2681b6473fffcdde3a15944ddfb6bb5463af157c442816bef48027205"
+  version "3.18.0.1,0200007073,3"
+  sha256 "b046ffa5f127e6432907af49803df0680ae71a06d13bf6fc71448d1fd550f119"
 
   url "https://gdlp01.c-wss.com/gds/#{version.csv.third}/#{version.csv.second}/01/EU-Installset-M#{version.csv.first}.dmg.zip",
       verified: "gdlp01.c-wss.com/"
@@ -15,7 +15,7 @@ cask "canon-eos-utility" do
     url "https://gdlp01.c-wss.com/rmds/ic/autoupdate/common/tls_eu_updater_url.xml"
     regex(%r{http.*?/(\d+)/(\d+)/\d+/EU[._-]Installset[._-]v?M?(\d+(?:\.\d+)+)\.dmg\.zip}i)
     strategy :page_match do |page, regex|
-      match = page.match(/<Component\sID="[^"]+mac_11[^"]+".*\n?.*(https.*)\n/i)
+      match = page.match(/<Component\sID="[^"]+mac_14[^"]+".*\n?.*(https.*)\n/i)
       next if match.blank?
 
       url = match[1].strip
