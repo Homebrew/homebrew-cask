8d75fff6ad458d0d7014c0dac8a7cd2e190a8c1c
Miccal Matthews <6127163+miccal@users.noreply.github.com>
2024-06-12 21:03:05 +0000

    signal 7.12.0

--- Casks/s/signal.rb
+++ Casks/s/signal.rb
@@ -1,9 +1,9 @@
 cask "signal" do
   arch arm: "arm64", intel: "x64"
 
-  version "7.11.1"
-  sha256 arm:   "b4c99b62ea0923866a88f4d6e4364ba4a1405b9c07b7ec1636ed37fa14405021",
-         intel: "60175a596c88e8aa70b7441d9b831eff333329084bbb34232c1f3a94b055d156"
+  version "7.12.0"
+  sha256 arm:   "7916938a4aabb3017c0c66dbe75ea8f0f1e28460f5a4f6c68f4d20924a8c4317",
+         intel: "7541e8e74c41adac2ae3cbf4ec77761f2b39b2b68fa5655ebf0817dc8fb40017"
 
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{arch}-#{version}.dmg"
   name "Signal"
