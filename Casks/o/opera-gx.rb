c134b30b2ea371404f13f752f63846667833f503
Miccal Matthews <6127163+miccal@users.noreply.github.com>
2024-06-12 20:59:32 +0000

    opera-gx 109.0.5097.130

--- Casks/o/opera-gx.rb
+++ Casks/o/opera-gx.rb
@@ -1,6 +1,6 @@
 cask "opera-gx" do
-  version "109.0.5097.126"
-  sha256 "ff0684ad6f7c3de585034e1a73f5898d45f8f4069e319587242fe835a6ccae86"
+  version "109.0.5097.130"
+  sha256 "59d8676e053a7f80a0792c6924f03a3287e045ce07fd0fd96ce732d5d0d4db94"
 
   url "https://get.geo.opera.com/pub/opera_gx/#{version}/mac/Opera_GX_#{version}_Setup.dmg"
   name "Opera GX"
