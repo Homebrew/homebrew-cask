1508ba717f4d4775477ec322f4d4aedb651c7235
Bevan Kay <email@bevankay.me>
2024-06-13 22:59:15 +1000

    font-cubic-11 1.200

--- Casks/font/font-c/font-cubic-11.rb
+++ Casks/font/font-c/font-cubic-11.rb
@@ -1,14 +1,14 @@
 cask "font-cubic-11" do
-  version "1.100"
-  sha256 "354247bb7e48b2e3896758e7c3413f7ce4329d130b0c9a7ef124d476ca66f4ae"
+  version "1.200"
+  sha256 "903e9dc53652edd85273766f30329a83503a8fe147f536f63f853646cf04d676"
 
-  url "https://github.com/ACh-K/Cubic-11/releases/download/v#{version}/Cubic_11.zip"
+  url "https://github.com/ACh-K/Cubic-11/archive/refs/tags/v#{version}.tar.gz"
   name "Cubic 11"
   name "俐方體11號"
   desc "Open-source 11x11 Traditional Chinese bitmap font"
   homepage "https://github.com/ACh-K/Cubic-11"
 
-  font "fonts/ttf/Cubic_11_#{version}_R.ttf"
+  font "Cubic-11-#{version}/fonts/ttf/Cubic_11_#{version}_R.ttf"
 
   # No zap stanza required
 end
