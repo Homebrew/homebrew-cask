a37b54ebc7dc1395f31d726ac8f1585a1c7673ea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-13 15:41:06 +0000

    logi-options-plus 1.74.577900

--- Casks/l/logi-options-plus.rb
+++ Casks/l/logi-options-plus.rb
@@ -1,5 +1,5 @@
 cask "logi-options-plus" do
-  version "1.72.568951"
+  version "1.74.577900"
   sha256 :no_check
 
   url "https://download01.logi.com/web/ftp/pub/techsupport/optionsplus/logioptionsplus_installer.zip",
