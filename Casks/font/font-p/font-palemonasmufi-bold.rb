9308ad1fa3fbdd53f9ded56bf99429a4425f7106
jellis <jellis@iqvia.com>
2024-06-13 19:04:08 -0700

    font-palemonasmufi-bold 2.0

---
+++ Casks/font/font-p/font-palemonasmufi-bold.rb
@@ -0,0 +1,13 @@
+cask "font-palemonasmufi-bold" do
+  version "2.0"
+  sha256 "3b8338fe0333188f359ce06719b23af25ffbe21c33395d9332032717e8e9fcba"
+
+  url "https://mufi.info/q.php?o=mufifontfiledownload&name=PalemMUFI-bd.ttf"
+  name "PalemonasMUFI Italic"
+  desc "Unicode font with 1340 glyphs from MUFI v3.0"
+  homepage "https://mufi.info/q.php?p=mufi/fonts/fontfamily/6"
+
+  font "PalemMUFI-bd.ttf"
+
+  # No zap stanza required
+end
