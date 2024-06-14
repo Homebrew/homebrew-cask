4ab80551204fd27b57a101f937cf2c5828d820e1
jellis <jellis@iqvia.com>
2024-06-13 19:04:21 -0700

    font-palemonasmufi-italic 2.0

---
+++ Casks/font/font-p/font-palemonasmufi-italic.rb
@@ -0,0 +1,13 @@
+cask "font-palemonasmufi-italic" do
+  version "2.0"
+  sha256 "1be812a33d5fe6fb7a9b7be7118e2bc24adfe42bd30652541282d7edad6dd089"
+
+  url "https://mufi.info/q.php?o=mufifontfiledownload&name=PalemMUFI-it.ttf"
+  name "PalemonasMUFI Italic"
+  desc "Unicode font with 2217 glyphs from MUFI v3.0"
+  homepage "https://mufi.info/q.php?p=mufi/fonts/fontfamily/6"
+
+  font "PalemMUFI-it.ttf"
+
+  # No zap stanza required
+end
