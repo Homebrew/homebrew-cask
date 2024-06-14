f3afea6316e1cb6f5711cbf55fe4479f37e3e45d
jellis <jellis@iqvia.com>
2024-06-13 19:03:52 -0700

    font-palemonasmufi-regular 2.0

---
+++ Casks/font/font-p/font-palemonasmufi-regular.rb
@@ -0,0 +1,13 @@
+cask "font-palemonasmufi-regular" do
+  version "2.0"
+  sha256 "7cf9a368b5aa437ab99f817361ddcd9d363dad6b3e4d8e5b89a0aaa32e268d68"
+
+  url "https://mufi.info/q.php?o=mufifontfiledownload&name=PalemMUFI-reg.ttf"
+  name "PalemonasMUFI Regular"
+  desc "Unicode font with all glyphs from MUFI v3.0"
+  homepage "https://mufi.info/q.php?p=mufi/fonts/fontfamily/6"
+
+  font "PalemMUFI-reg.ttf"
+
+  # No zap stanza required
+end
