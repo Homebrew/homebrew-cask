287090cd4a4750b9cb2ead899b298494382f262e
Michelangelo Camaioni <michelangelocamaioni@protonmail.com>
2024-06-16 00:42:41 +0200

    ea 13.235.0.5748 (new cask)

---
+++ Casks/e/ea.rb
@@ -0,0 +1,35 @@
+cask "ea" do
+  version "13.235.0.5748"
+  sha256 :no_check
+
+  url "https://origin-a.akamaihd.net/EA-Desktop-Client-Download/installer-releases/EA%20app.dmg",
+      verified: "origin-a.akamaihd.net/EA-Desktop-Client-Download/"
+  name "EA App"
+  desc "Electronic Arts game launcher"
+  homepage "https://www.ea.com/ea-app"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
+  app "EA app.app"
+
+  zap delete: [
+        "/Library/Application Support/Electronic Arts",
+        "/Library/LaunchDaemons/com.ea.app.backgroundservice.plist",
+      ],
+      trash:  [
+        "~/Library/Application Support/Electronic Arts",
+        "~/Library/Caches/EA app",
+        "~/Library/Caches/EACrashReporter",
+        "~/Library/Caches/EALaunchHelper",
+        "~/Library/HTTPStorages/com.EA.EA-app-Migrator",
+        "~/Library/LaunchAgents/com.ea.app.backgroundAgent.plist",
+        "~/Library/Preferences/com.ea.EACefSubProcessRenderer.plist",
+        "~/Library/Saved Application State/com.ea.mac.eaapp.savedState",
+      ]
+end
