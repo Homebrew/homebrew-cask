3cda84053469f2c5d89f27746d0e629cea026848
Tom Moor <tom@getoutline.com>
2024-06-15 13:28:47 -0700

    Update outline.rb
    outline.app no longer exists

--- Casks/o/outline.rb
+++ Casks/o/outline.rb
@@ -1,28 +1,20 @@
 cask "outline" do
-  version "3.2305.0"
-  sha256 "74b2be3df41928aa714b1d2193d21df18643a8a69f6a02fcb5f252f1b0a0b7ef"
+  version "1.3.1"
+  sha256 :no_check
 
-  url "https://static.outline.ws/versions/Outline_#{version}.zip",
-      verified: "static.outline.ws/"
+  url "https://desktop.getoutline.com/mac/dmg"
   name "Outline"
-  desc "Note taking app"
-  homepage "https://outline.app/"
+  desc "Desktop app for Outline"
+  homepage "https://www.getoutline.com/"
 
   livecheck do
-    url "https://gorillized.s3.amazonaws.com/versions/update_channel.xml"
-    strategy :sparkle, &:short_version
+    url :url
+    strategy :header_match
   end
 
   app "Outline.app"
 
   zap trash: [
-    "~/Library/Application Scripts/6WN928JT76.com.gorillized",
-    "~/Library/Application Scripts/com.gorillized.outlinemac.standalone.extension",
-    "~/Library/Application Support/com.gorillized.outlinemac.standalone",
-    "~/Library/Caches/com.gorillized.outlinemac.standalone",
-    "~/Library/Containers/com.gorillized.outlinemac.standalone.extension",
-    "~/Library/Group Containers/6WN928JT76.com.gorillized",
-    "~/Library/Outline",
-    "~/Library/Preferences/com.gorillized.outlinemac.standalone.plist",
+    "~/Library/Application Support/Outline"
   ]
 end
