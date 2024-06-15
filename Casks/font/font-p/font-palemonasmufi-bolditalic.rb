3524c3981361c92fa11b8aadcaf8c828d1a90216
jellis <jellis@iqvia.com>
2024-06-15 12:33:12 -0700

    font-palemonasmufi-bolditalic 2.0

--- Casks/font/font-p/font-palemonasmufi-bolditalic.rb
+++ Casks/font/font-p/font-palemonasmufi-bolditalic.rb
@@ -4,7 +4,6 @@ cask "font-palemonasmufi-bolditalic" do
 
   url "https://mufi.info/q.php?o=mufifontfiledownload&name=PalemMUFI-bi.ttf"
   name "PalemonasMUFI Bold italic"
-  desc "Unicode font with 1327 glyphs from MUFI v3.0"
   homepage "https://mufi.info/q.php?p=mufi/fonts/fontfamily/6"
 
   font "PalemMUFI-bi.ttf"
