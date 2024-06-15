5b2b4dee318bf748a9fbab68252509259f6ac38d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-15 00:49:38 +0000

    jumpshare 3.3.8

--- Casks/j/jumpshare.rb
+++ Casks/j/jumpshare.rb
@@ -1,6 +1,6 @@
 cask "jumpshare" do
-  version "3.3.7"
-  sha256 "87a180045b7247c7a10b161033ac68d73bfa3b9e7049d90dec5ddb59b82b16b4"
+  version "3.3.8"
+  sha256 "3c259ef9f1a6472607d71721e3f0693649cbacf171493d566d1151b18a399634"
 
   url "https://d21hi1or3tbtjm.cloudfront.net/desktop/mac/updates/Jumpshare-#{version}.tar.bz2",
       verified: "d21hi1or3tbtjm.cloudfront.net/desktop/mac/updates/"
