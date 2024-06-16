701233adb8b32534f25edf5b54a06db14dced72e
Yurii Kolesnykov <root@yurikoles.com>
2024-06-16 22:14:47 +0100

    turbovnc-viewer 3.1.1 moved to GitHub
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/t/turbovnc-viewer.rb
+++ Casks/t/turbovnc-viewer.rb
@@ -1,12 +1,12 @@
 cask "turbovnc-viewer" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "3.1"
-  sha256 arm:   "b0d9071d517d5e510605228d47d862ba94874e25e3d508116f2195913999890c",
-         intel: "7d302dd611ff7b1ab92d2215ca550361dbf081b6bbf3916f3640730483a314d4"
+  version "3.1.1"
+  sha256 arm:   "401db71b4f4e2865e9b11fdbae4fbc40cc9161b6082eb3165a80ad30b3644b90",
+         intel: "534004265ce53cfec20082416f8e949db377079a6fe12aacfef1e6a85c384a9d"
 
-  url "https://downloads.sourceforge.net/turbovnc/#{version}/TurboVNC-#{version}-#{arch}.dmg",
-      verified: "sourceforge.net/turbovnc/"
+  url "https://github.com/TurboVNC/turbovnc/releases/download/#{version}/TurboVNC-#{version}-#{arch}.dmg",
+      verified: "github.com/TurboVNC/turbovnc/"
   name "TurboVNC"
   desc "Remote display system"
   homepage "https://www.turbovnc.org/"
