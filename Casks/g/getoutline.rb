2728009d1da05454694b057dd5a39bf24c11a34a
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-06-16 09:21:47 -0400

    getoutline: cask changes

--- Casks/g/getoutline.rb
+++ Casks/g/getoutline.rb
@@ -4,21 +4,25 @@ cask "getoutline" do
   version "1.3.1"
   sha256 :no_check
 
-    url "https://desktop.getoutline.com/mac/dmg/#{arch}"
+  url "https://desktop.getoutline.com/mac/dmg/#{arch}"
   name "Outline"
-  desc "Fast, collaborative, knowledge base for your team built using React and Node.js"
+  desc "Knowledge management tool"
   homepage "https://getoutline.com/"
+
+  livecheck do
+    url :url
+    regex(/outline\s?(\d+(?:\.\d+)*)/i)
+    strategy :header_match
+  end
 
   depends_on macos: ">= :catalina"
 
   app "Outline.app"
 
-  # locations to zap (uninstall)
   zap trash: [
-    "/Applications/Outline.app",
-    "/Volumes/Outline 1.3.1-arm64/",
-    "/Volumes/Outline 1.3.1/",
-    "~/Library/Application Support/Outline/",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.generaloutline.outline.sfl*",
+    "~/Library/Application Support/Outline",
+    "~/Library/Logs/Outline",
     "~/Library/Preferences/com.generaloutline.outline.plist",
     "~/Library/Saved Application State/com.generaloutline.outline.savedState",
   ]
