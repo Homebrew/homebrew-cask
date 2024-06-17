7a19e430411441105c81fe66c82e34bcf9e8964f
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-06-17 14:45:55 -0400

    getoutline: add checksums and update version

--- Casks/g/getoutline.rb
+++ Casks/g/getoutline.rb
@@ -1,8 +1,9 @@
 cask "getoutline" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.3.1"
-  sha256 :no_check
+  version "1.3.1,240227x80f4vacd"
+  sha256 arm:   "5178207301ee32ff0540dba762af9a8c64d61c42bbc64bdfbee4f058e9953ad4",
+         intel: "8409afc5d0433084d9aacbd6decb3bb387c343a75aa37acdf5ba29d145a229f6"
 
   url "https://download.todesktop.com/2211128hgkdcltv/Outline%20#{version.csv.first}%20-%20Build%20#{version.csv.second}-#{arch}.dmg",
       verified: "download.todesktop.com/2211128hgkdcltv/"
