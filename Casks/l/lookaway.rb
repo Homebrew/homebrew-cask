cf76d1c9df47bb88cc900b24db27cee2e27c9f72
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-17 21:40:42 +0000

    lookaway 1.5.6

--- Casks/l/lookaway.rb
+++ Casks/l/lookaway.rb
@@ -1,6 +1,6 @@
 cask "lookaway" do
-  version "1.5.5"
-  sha256 "073d8da25eb8c866cbea131166b8ced8cde309179f574840a8172e15727122b4"
+  version "1.5.6"
+  sha256 "34291dbebcfe7a311275f69b8a2716e4373d97fa069029637ba61b3d8526ec30"
 
   url "https://github.com/mysticalbits/lookaway-releases/releases/download/#{version}/LookAway.dmg",
       verified: "github.com/mysticalbits/lookaway-releases/"
