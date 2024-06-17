9da78b34a8ef6188fef4fcf09713829008832371
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-17 06:43:13 +0000

    mixxx@snapshot 2.6-alpha-19-g796d056a58

--- Casks/m/mixxx@snapshot.rb
+++ Casks/m/mixxx@snapshot.rb
@@ -1,9 +1,9 @@
 cask "mixxx@snapshot" do
   arch arm: "arm", intel: "intel"
 
-  version "2.6-alpha-17-g5056f56f50"
-  sha256 arm:   "4d2fd4d70d8dba84cafb3f535cc0638f21123400f62bce164f28357743bb4684",
-         intel: "3f59ab11df8a6b06e8f9cea89752f4b4897c7e1bcab12cb7b9b22dec14a1bcfe"
+  version "2.6-alpha-19-g796d056a58"
+  sha256 arm:   "3dab037f8f1caea8c42748296ce67e0644cf01ce9735e07aba6eeefb90147f13",
+         intel: "1ec896e60fc196384321c3470a9e0031ad900ed6f4eb448ccaff860d61ab11bc"
 
   on_arm do
     depends_on macos: ">= :big_sur"
