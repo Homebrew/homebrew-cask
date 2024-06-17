48eb6432c50c6aa09ffaef69446b35ae7dfb02f9
Charuka Karunanayake <charukakarunanayake@gmail.com>
2024-06-17 20:40:33 +0530

    Add suggested changes for choreo cli PR

--- Casks/c/choreo.rb
+++ Casks/c/choreo.rb
@@ -1,19 +1,15 @@
-# Documentation: https://docs.brew.sh/Cask-Cookbook
-#                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 cask "choreo" do
   arch arm: "arm64", intel: "amd64"
 
-  version "1.1.3"
-  sha256 arm:   "0985ebb872bae5946a351363a402c4d3fd4eb3d4e035b104c0e2a491afef71dc",
-         intel: "999919b615f5fad3972c3fc09404361b2d53604549f4ab662ab409da3e87542d"
+  version "1.1.4"
+  sha256 arm:   "89989762f522e95b2d20ca9c362856a0b48d0229c853e6e170170f0a9fe68dfa",
+         intel: "4b3573eb8cd86349a6c9231ebf4f586279cf33214c3c4fd01de3d7bd2763833f"
 
   url "https://github.com/wso2/choreo-cli/releases/download/v#{version}/choreo-cli-v#{version}-darwin-#{arch}.zip"
   name "choreo"
   desc "Command-line Interface tool for WSO2"
   homepage "https://github.com/wso2/choreo-cli/releases"
 
-  # Documentation: https://docs.brew.sh/Brew-Livecheck
   livecheck do
     url :url
     strategy :github_latest
@@ -25,68 +21,10 @@ cask "choreo" do
 
   # post-install message
   postflight do
-    user_home = Dir.home
-    choreo_home = "#{user_home}/.choreo"
-
-    detected_profile = ""
-    completion_script = ""
-
-    # Create the .choreo directory
-    FileUtils.mkdir_p choreo_home
-
-    if ENV["SHELL"].include? "bash"
-      if File.exist?("#{user_home}/.bashrc")
-        detected_profile = "#{user_home}/.bashrc"
-      elsif File.exist?("#{user_home}/.bash_profile")
-        detected_profile = "#{user_home}/.bash_profile"
-      end
-      # Genereate the completion script for the Choreo CLI
-      # save output to a variable
-      completion_script = `#{staged_path}/choreo completion bash`
-    elsif ENV["SHELL"].include? "zsh"
-      if File.exist?("#{user_home}/.zshrc")
-        detected_profile = "#{user_home}/.zshrc"
-      elsif File.exist?("#{user_home}/.zprofile")
-        detected_profile = "#{user_home}/.zprofile"
-      end
-      # Genereate the completion script for the Choreo CLI
-      # save output to a variable
-      completion_script = `#{staged_path}/choreo completion zsh`
-    end
-
-    unless completion_script.empty?
-      # Create the file to store the completion script
-      FileUtils.mkdir_p "#{choreo_home}/bin"
-
-      # write the completion script to a file
-      File.write("#{choreo_home}/bin/choreo-completion", completion_script)
-    end
-
-    if detected_profile.empty?
-      if File.exist?("#{user_home}/.profile")
-        detected_profile = "#{user_home}/.profile"
-      elsif File.exist?("#{user_home}/.bashrc")
-        detected_profile = "#{user_home}/.bashrc"
-      elsif File.exist?("#{user_home}/.bash_profile")
-        detected_profile = "#{user_home}/.bash_profile"
-      elsif File.exist?("#{user_home}/.zshrc")
-        detected_profile = "#{user_home}/.zshrc"
-      elsif File.exist?("#{user_home}/.zprofile")
-        detected_profile = "#{user_home}/.zprofile"
-      end
-    end
-
-    if File.exist? detected_profile
-      # Check if the profile file already contains the Choreo CLI completion script
-      if File.foreach(detected_profile).grep(/choreo-completion/).none?
-        File.open(detected_profile, "a") do |file|
-          file.write("\n# Choreo CLI\n")
-          file.write("[ -f $CHOREO_DIR/bin/choreo-completion ] && source $CHOREO_DIR/bin/choreo-completion")
-        end
-      end
-
-      # Source the profile file
-      system("source", detected_profile.to_s)
-    end
+    puts "\nFor generating completion scripts, run the following command:" <<
+    "\n\n    choreo completion <shell-name> >> ~/.choreo/bin/choreo-completion" <<
+    "\n\nSupported shell names: bash, zsh, fish\n\n" <<
+    "To load the completion scripts, add the following line to your shell configuration file:" <<
+    "\n\n   [ -f ~/.choreo/bin/choreo-completion ] && source ~/.choreo/bin/choreo-completion\n\n"
   end
 end
