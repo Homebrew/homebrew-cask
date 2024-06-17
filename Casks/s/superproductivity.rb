d360ef36471e9cd7e6d2fc42df819d4cfebfc7de
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-17 09:50:00 +0000

    superproductivity 8.0.10

--- Casks/s/superproductivity.rb
+++ Casks/s/superproductivity.rb
@@ -1,9 +1,9 @@
 cask "superproductivity" do
   arch arm: "-arm64"
 
-  version "8.0.9"
-  sha256 arm:   "47310897a42c4e5c47fde98500202fb9c79759a7f794d42e0099ed28c0488627",
-         intel: "72975b554bb6ed31abea6bd4edd7ac53e6d794d23bb3f0dfd10dd9557626d098"
+  version "8.0.10"
+  sha256 arm:   "ff2d9f06958ab0ad09318e3fe009f5ebae5264c76ee8f58947b7735ddeda43a4",
+         intel: "9e160d9a4d5d823406b03cceb7ed074865643b48c693eb7d80f3c8e2486d93b8"
 
   url "https://github.com/johannesjo/super-productivity/releases/download/v#{version}/superProductivity-#{version}#{arch}.dmg",
       verified: "github.com/johannesjo/super-productivity/"
