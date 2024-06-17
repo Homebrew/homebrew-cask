94c8392d94b8f612c52a84c79686e6df53f80da2
Sean Molenaar <smillerdev@me.com>
2024-06-17 20:08:31 +0200

    font-academicons: use tarbal

--- Casks/font/font-a/font-academicons.rb
+++ Casks/font/font-a/font-academicons.rb
@@ -1,8 +1,8 @@
 cask "font-academicons" do
   version "1.9.4"
-  sha256 "6f27234f174e9a8872296b1778083393127773ddcb19f0b3cc5ab3bfa42c37e9"
+  sha256 "5b6aeecaaf2a63d23b07b722f3e1be2ae1a12f433677704e73a6b8f564943784"
 
-  url "https://github.com/jpswalsh/academicons/archive/v#{version}.zip"
+  url "https://github.com/jpswalsh/academicons/archive/refs/tags/v#{version}.tar.gz"
   name "Academicons"
   homepage "https://github.com/jpswalsh/academicons"
 
