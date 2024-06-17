e103d0c365d66bb05ea78ea89fb3e1682f11527a
Eric Knibbe <enk3@outlook.com>
2024-06-17 11:17:28 -0400

    widelands: bump minimum macOS

--- Casks/w/widelands.rb
+++ Casks/w/widelands.rb
@@ -5,6 +5,13 @@ cask "widelands" do
   sha256 arm:   "7b19ba62c561daa7880a4fa5683591d916f43a4b846f1fc527e1c47e6e84d4e4",
          intel: "ca3a4db1c6ccf07d2ff246d6bd979f5e7e116a8529d3ae41fdfc8d66bf4d5f93"
 
+  on_arm do
+    depends_on macos: ">= :sonoma"
+  end
+  on_intel do
+    depends_on macos: ">= :monterey"
+  end
+
   url "https://github.com/widelands/widelands/releases/download/v#{version}/Widelands-#{version}-MacOS#{arch}.dmg",
       verified: "github.com/widelands/widelands/"
   name "Widelands"
@@ -15,8 +22,6 @@ cask "widelands" do
     url "https://www.widelands.org/wiki/Download/"
     regex(/href=.*?Widelands[._-]v?(\d+(?:\.\d+)+)[._-]MacOS#{arch}\.dmg/i)
   end
-
-  depends_on macos: ">= :big_sur"
 
   app "Widelands.app"
 
