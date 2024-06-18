7c4b81e730fed2af9de95b1f799f9301f4f2a99e
Klaus Hipp <khipp@users.noreply.github.com>
2024-06-18 13:48:44 +0200

    fabfilter-simplon 1.39

--- Casks/f/fabfilter-simplon.rb
+++ Casks/f/fabfilter-simplon.rb
@@ -1,6 +1,6 @@
 cask "fabfilter-simplon" do
-  version "1.38"
-  sha256 "cc88e8bf40c2232f01022e471b6064977d83dadd321c6c664fe4e2d1007f8520"
+  version "1.39"
+  sha256 "f63dddedb3102f9de3b08a2371b9ba585dab4654049863beeb54702c17a204e9"
 
   url "https://www.fabfilter.com/downloads/ffsimplon#{version.no_dots}.dmg"
   name "FabFilter Simplon"
