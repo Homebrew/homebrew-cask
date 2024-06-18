d856c272e0554341a1be6cbf70970a109919bc4d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-18 15:42:58 +0000

    plex 1.95.2.166

--- Casks/p/plex.rb
+++ Casks/p/plex.rb
@@ -1,6 +1,6 @@
 cask "plex" do
-  version "1.94.1.155,f1e7f457"
-  sha256 "244a928d54ee0c727fcad9a2d224e738cf04e2adc12e40aa0c3378cd7de6760b"
+  version "1.95.2.166,92c7babe"
+  sha256 "32059e761fafb018cd0528d0bbae264b7af2b396eb422f574748e33b4415578e"
 
   url "https://downloads.plex.tv/plex-desktop/#{version.csv.first}-#{version.csv.second}/macos/Plex-#{version.csv.first}-#{version.csv.second}-universal.zip"
   name "Plex"
