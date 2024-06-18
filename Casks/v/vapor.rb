f53ca9cdc7fb8d4a88efe05844fb4c39996e9517
Klaus Hipp <khipp@users.noreply.github.com>
2024-06-18 16:00:45 +0200

    vapor: fix url

--- Casks/v/vapor.rb
+++ Casks/v/vapor.rb
@@ -5,7 +5,7 @@ cask "vapor" do
   sha256 arm:   "130806331261298a53c07451ab11bc728e5fe0fc86483bcc8a8f49a5d8018751",
          intel: "00dbfcea7e6a7512d76fd6684af9471ba9bd0cc62dd324b6ee9e941e091f174d"
 
-  url "https://github.com/NCAR/VAPOR/releases/download/v#{version}/VAPOR#{version.major}-#{version}-Darwin#{arch}.dmg"
+  url "https://github.com/NCAR/VAPOR/releases/download/v#{version}/VAPOR#{version.major}-#{version}-MacOS-#{arch}.dmg"
   name "VAPOR"
   desc "Visualisation and analysis platform"
   homepage "https://github.com/NCAR/VAPOR"
