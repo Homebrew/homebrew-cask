18c1ca0fb4d92a99c83fabd33ce081db56935cf9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-18 15:33:18 +0000

    datovka 4.24.0

--- Casks/d/datovka.rb
+++ Casks/d/datovka.rb
@@ -1,9 +1,9 @@
 cask "datovka" do
   arch arm: "arm64-macos13.3", intel: "64bit-macos10.12"
 
-  version "4.23.8"
-  sha256 arm:   "63914fffd0eda78f0dac09dd24f3b50a6f1bb422b349d992be898cad7e183559",
-         intel: "d62e6745c0cf2156778a101c554bd08df2618ccbd869d97d3709e71fe1c9dcbd"
+  version "4.24.0"
+  sha256 arm:   "349ee08efe6d1937b3c7532b023ee411141e37b63070460474c33b34f0b0c6ed",
+         intel: "8b667dec37d9acc7697133ceb35dbaab0d0f4493529929ec8faf1c027e0368b6"
 
   url "https://secure.nic.cz/files/datove_schranky/#{version}/datovka-#{version}-#{arch}.dmg",
       verified: "secure.nic.cz/files/datove_schranky/"
