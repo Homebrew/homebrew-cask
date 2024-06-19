97a1c020dd160cd6e785a5169cd343da2b5cf415
i0ntempest <szf1234@me.com>
2024-06-19 15:39:04 -0400

    Loopback: move to new URL
    to actually download the new version

--- Casks/l/loopback.rb
+++ Casks/l/loopback.rb
@@ -1,21 +1,40 @@
 cask "loopback" do
-  version "2.4.0"
   sha256 :no_check
 
-  url "https://rogueamoeba.com/loopback/download/Loopback.zip"
+  on_ventura :or_older do
+    version "2.3.3"
+
+    url "https://rogueamoeba.com/loopback/download/Loopback.zip"
+
+    # NOTE: The `system` value will need to be kept up to date with the latest
+    # macOS Ventura version (e.g. 1366 for 13.6.6).
+    livecheck do
+      url "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&system=1366&bundleid=com.rogueamoeba.Loopback&platform=osx&version=#{version.no_dots}8000"
+      strategy :sparkle
+    end
+
+    depends_on macos: ">= :big_sur"
+  end
+  on_sonoma :or_newer do
+    version "2.4.0"
+
+    url "https://rogueamoeba.com/loopback/download/Loopback-ARK.zip"
+
+    # NOTE: The `system` value will need to be kept up to date with the latest
+    # macOS version (e.g. 1441 for 14.4.1).
+    livecheck do
+      url "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&system=145&bundleid=com.rogueamoeba.Loopback&platform=osx&version=#{version.no_dots}8000"
+      strategy :sparkle
+    end
+
+    depends_on macos: ">= :sonoma"
+  end
+
   name "Loopback"
   desc "Cable-free audio router"
   homepage "https://rogueamoeba.com/loopback/"
 
-  # NOTE: The `system` value will need to be kept up to date with the latest
-  # macOS version (e.g. 1441 for 14.4.1).
-  livecheck do
-    url "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&system=145&bundleid=com.rogueamoeba.Loopback&platform=osx&version=#{version.no_dots}8000"
-    strategy :sparkle
-  end
-
   auto_updates true
-  depends_on macos: ">= :big_sur"
 
   app "Loopback.app"
 
