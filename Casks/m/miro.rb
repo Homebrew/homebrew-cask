fe84c57d74f848e395bd1d62e6be1df878055aec
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-19 09:42:02 +0000

    miro 0.8.68

--- Casks/m/miro.rb
+++ Casks/m/miro.rb
@@ -1,7 +1,7 @@
 cask "miro" do
   arch arm: "darwin-arm64", intel: "darwin"
 
-  version "0.8.61"
+  version "0.8.68"
   sha256 :no_check
 
   url "https://desktop.miro.com/platforms/#{arch}/Install-Miro.dmg"
