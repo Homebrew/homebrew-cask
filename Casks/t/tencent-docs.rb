0847ef66c7136ff876033e992c95d499d7befe46
Bevan Kay <email@bevankay.me>
2024-06-20 23:41:25 +1000

    tencent-docs 3.5.4
    tencent-docs: update app name and zap
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/t/tencent-docs.rb
+++ Casks/t/tencent-docs.rb
@@ -1,28 +1,29 @@
 cask "tencent-docs" do
-  arch arm: "Arm64", intel: "X64"
+  arch arm: "arm64", intel: "x64"
 
-  version "3.1.8,25"
-  sha256 :no_check
+  version "3.5.4"
+  sha256 arm:   "596065c0a03a6eac64c6cc5cb029cd57456ec34377fe86dbce895804130b070e",
+         intel: "597be6ba46dd655a090befbeecb5b9b4184af8b79bcd679ebd8fb13ba1a13ef3"
 
-  url "https://down.qq.com/qqweb/mac_docs/MacTencentDocs#{arch}.dmg"
+  url "https://desktop.docs.qq.com/Installer/30001/#{version}/TencentDocs-#{arch}.dmg"
   name "Tencent Docs"
   name "腾讯文档"
   desc "Online editor for Word, Excel and PPT documents"
   homepage "https://docs.qq.com/"
 
   livecheck do
-    # curl \
-    #   -H 'content-type: application/json' \
-    #   --data-binary \
-    #   '{"pull_item":{"app_id":"936c3190-4a5a-4608-ac9e-d37fe33f40be","group":"DOWNLOAD.release-note-mac"}}' \
-    #   https://docs.qq.com/config.v2.ConfigService/PullConfigReq
-    url :url
-    strategy :extract_plist
+    url "https://docs.qq.com/api/package/update"
+    regex(/"version.*?(\d+(?:\.\d+)+)[\\ "]/i)
   end
 
   auto_updates true
 
-  app "腾讯文档.app"
+  app "TencentDocs.app"
 
-  zap trash: "~/Library/Preferences/com.tencent.mac.tdappdesktop.plist"
+  zap trash: [
+    "/Library/Logs/DiagnosticReports/TencentDocs_*",
+    "~/Library/Application Support/CrashReporter/TencentDocs_*",
+    "~/Library/Preferences/com.tencent.mac.tdappdesktop.plist",
+    "~/Library/Saved Application State/com.tencent.mac.tdappdesktop.savedState",
+  ]
 end
