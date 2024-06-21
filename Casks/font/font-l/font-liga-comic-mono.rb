eb756c930e9f747b67491bc6e1308a59549531ac
Sean Molenaar <smillerdev@me.com>
2024-06-21 12:07:13 +0200

    font-liga-comic-mono: style fixes

--- Casks/font/font-l/font-liga-comic-mono.rb
+++ Casks/font/font-l/font-liga-comic-mono.rb
@@ -2,7 +2,7 @@ cask "font-liga-comic-mono" do
   version :latest
   sha256 :no_check
 
-  url "https://codeload.github.com/wayou/comic-mono-font/zip/master"
+  url "https://github.com/wayou/comic-mono-font/archive/refs/heads/master.tar.gz"
   name "Liga Comic Mono"
   homepage "https://github.com/wayou/comic-mono-font"
 
