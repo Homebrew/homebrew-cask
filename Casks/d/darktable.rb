7c2f4d9256ece6ee70de0e4263b3ecc38c58cd52
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-06-21 17:07:07 -0400

    darktable 4.8.0

--- Casks/d/darktable.rb
+++ Casks/d/darktable.rb
@@ -1,9 +1,9 @@
 cask "darktable" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "4.6.1"
-  sha256 arm:   "145a11c3965b4c5cc2c53c9277f5896569fe55c05f2445f3185de94cd9667585",
-         intel: "a7676fb36f208a41e026d806a0408d2364251d843810fd7dc2003e251ae09773"
+  version "4.8.0"
+  sha256 arm:   "628442bf336e4f51fdf39f8b144372cbd855fe7b17812e359b5f8f3e6b6d99fb",
+         intel: "bded6986f72af8901f0fa28686d6d06e6b766d831c29abe8a0163b5a503a50e6"
 
   url "https://github.com/darktable-org/darktable/releases/download/release-#{version.major_minor_patch}/darktable-#{version}-#{arch}.dmg",
       verified: "github.com/darktable-org/darktable/"
