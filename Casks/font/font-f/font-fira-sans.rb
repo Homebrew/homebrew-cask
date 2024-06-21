1d1f655092709dc00b3395a1bc3ba401c6673d9d
Sean Molenaar <smillerdev@me.com>
2024-06-21 12:07:12 +0200

    font-fira-sans: style fixes

--- Casks/font/font-f/font-fira-sans.rb
+++ Casks/font/font-f/font-fira-sans.rb
@@ -2,7 +2,7 @@ cask "font-fira-sans" do
   version "4.202"
   sha256 "d86269657387f144d77ba12011124f30f423f70672e1576dc16f918bb16ddfe4"
 
-  url "https://github.com/mozilla/Fira/archive/#{version}.tar.gz",
+  url "https://github.com/mozilla/Fira/archive/refs/tags/#{version}.tar.gz",
       verified: "github.com/mozilla/Fira/"
   name "Fira Sans"
   homepage "https://mozilla.github.io/Fira/"
