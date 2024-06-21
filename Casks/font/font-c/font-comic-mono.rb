81909291897487b4fe9dccdfb139c0222fda0a28
Sean Molenaar <smillerdev@me.com>
2024-06-21 11:34:54 +0200

    font-comic-mono: update download

--- Casks/font/font-c/font-comic-mono.rb
+++ Casks/font/font-c/font-comic-mono.rb
@@ -2,8 +2,8 @@ cask "font-comic-mono" do
   version :latest
   sha256 :no_check
 
-  url "https://codeload.github.com/dtinth/comic-mono-font/zip/master",
-      verified: "codeload.github.com/dtinth/comic-mono-font/"
+  url "https://github.com/dtinth/comic-mono-font/archive/refs/heads/master.tar.gz",
+      verified: "github.com/dtinth/comic-mono-font/"
   name "Comic Mono"
   homepage "https://dtinth.github.io/comic-mono-font/"
 
