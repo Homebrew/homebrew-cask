e1a9064985109ef39c017b1a91265a9e4f98be72
Sean Molenaar <smillerdev@me.com>
2024-06-21 12:07:17 +0200

    ukrainian-unicode-layout: style fixes

--- Casks/u/ukrainian-unicode-layout.rb
+++ Casks/u/ukrainian-unicode-layout.rb
@@ -2,7 +2,7 @@ cask "ukrainian-unicode-layout" do
   version "1.2.2"
   sha256 "f5159ebc4f8aad0efd454d9080f7cd705a2132d57f64c10abb3d5e50ba772ab5"
 
-  url "https://github.com/korzhyk/macOS-Ukrainian-Unicode-Layout/archive/#{version}.tar.gz"
+  url "https://github.com/korzhyk/macOS-Ukrainian-Unicode-Layout/archive/refs/tags/#{version}.tar.gz"
   name "Ukrainian Unicode Layout"
   desc "Installer for Ukrainian Unicode layout"
   homepage "https://github.com/korzhyk/OSX-Ukrainian-Unicode-Layout"
