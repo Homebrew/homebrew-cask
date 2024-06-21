b5b5b43a8d943e67a59339f005fd668c5e6f04b0
Nick Moore <nick@pilotmoon.com>
2024-06-21 11:44:02 +0100

    fix trailing whitespace line 20

--- Casks/s/scroll-reverser.rb
+++ Casks/s/scroll-reverser.rb
@@ -17,7 +17,7 @@ cask "scroll-reverser" do
   app "Scroll Reverser.app"
 
   zap trash: [
-    "~/Library/Caches/com.pilotmoon.scroll-reverser",    
+    "~/Library/Caches/com.pilotmoon.scroll-reverser",
     "~/Library/Preferences/com.pilotmoon.scroll-reverser.plist",
   ]
 end
