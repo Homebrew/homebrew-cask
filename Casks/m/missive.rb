d75d3fba37ea8ff0a58b2a8e6da8bbd1e54b39d2
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-06-21 17:02:33 -0400

    missive 10.64.1

--- Casks/m/missive.rb
+++ Casks/m/missive.rb
@@ -1,6 +1,6 @@
 cask "missive" do
-  version "10.64.0"
-  sha256 "d2e519db25a04c5d9778068d994465381698da330059edcc2a77fd4d41a5da9c"
+  version "10.64.1"
+  sha256 "3e79609aba0464f2816759094fc9aa4b82d6698e4d346c4044ea0e5a01f18170"
 
   url "https://downloads.missiveapp.com/#{version}/Missive-#{version}.dmg"
   name "Missive"
