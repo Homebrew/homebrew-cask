74087184ebb21ed7634da17ed516d6009bdd7d4a
Bevan Kay <email@bevankay.me>
2024-06-22 12:58:53 +1000

    picoscope 7.1.29.19560

--- Casks/p/picoscope.rb
+++ Casks/p/picoscope.rb
@@ -1,21 +1,21 @@
 cask "picoscope" do
-  version "7.1.21.18179"
-  sha256 "aaab107b5b02ecab423e636bf5045f32c0931f1beddb6ff71a5f8bd1d851a47f"
+  version "7.1.29.19560"
+  sha256 "b4d1b08acc73b449e4ea377a3d44874846710e8d8d7249ba50cceff0558438b9"
 
-  url "https://www.picotech.com/download/software/sr/PicoScope_#{version}_TandM_Stable.pkg"
+  url "https://www.picotech.com/download/software/sr/PicoScope_#{version.major}_TandM_#{version}.pkg"
   name "PicoScope"
   desc "Test and measurement oscilloscope software for PicoScope oscilloscopes"
   homepage "https://www.picotech.com/"
 
   livecheck do
     url "https://www.picotech.com/downloads/_lightbox/picoscope-#{version.major}-stable-for-macos"
-    regex(%r{href=.*?/PicoScope[._-]v?(\d+(?:.\d+)+)[._-]T(?:and|n)M[._-]Stable\.pkg}i)
+    regex(%r{href=.*?/PicoScope[._-]#{version.major}[._-]T(?:and|n)M[._-]v?(\d+(?:.\d+)+)\.pkg}i)
   end
 
   conflicts_with cask: "picoscope@beta"
   depends_on macos: ">= :high_sierra"
 
-  pkg "PicoScope_#{version}_TandM_Stable.pkg"
+  pkg "PicoScope_#{version.major}_TandM_#{version}.pkg"
 
   uninstall pkgutil: "com.picotech.picoscope#{version.major}tnm"
 
