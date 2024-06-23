d5b07e379ef703976af00b26ee70498a4c34efb7
Klaus Hipp <khipp@users.noreply.github.com>
2024-06-23 17:06:14 +0200

    techsmith-capture: fix url

--- Casks/t/techsmith-capture.rb
+++ Casks/t/techsmith-capture.rb
@@ -2,7 +2,7 @@ cask "techsmith-capture" do
   version "1.3.31"
   sha256 :no_check
 
-  url "https://assets.techsmith.com/techsmithcapture/mac/TechSmithCapture.dmg"
+  url "https://cdn.cloud.techsmith.com/techsmithcapture/mac/TechSmithCapture.dmg"
   name "TechSmith Capture"
   desc "Screen capture software"
   homepage "https://www.techsmith.com/jing-tool.html"
@@ -13,7 +13,7 @@ cask "techsmith-capture" do
   end
 
   auto_updates true
-  depends_on macos: ">= :high_sierra"
+  depends_on macos: ">= :catalina"
 
   app "TechSmith Capture.app"
 end
