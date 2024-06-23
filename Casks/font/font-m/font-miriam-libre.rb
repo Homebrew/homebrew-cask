dc1b7555206bd115817c54af6ddc2336b63e097a
BrewTestBot <BrewTestBot@users.noreply.github.com>
2024-06-23 00:12:53 +0000

    Update Google Fonts

--- Casks/font/font-m/font-miriam-libre.rb
+++ Casks/font/font-m/font-miriam-libre.rb
@@ -2,15 +2,12 @@ cask "font-miriam-libre" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
-      branch:    "main",
-      only_path: "ofl/miriamlibre"
+  url "https://github.com/google/fonts/raw/main/ofl/miriamlibre/MiriamLibre%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Miriam Libre"
   homepage "https://fonts.google.com/specimen/Miriam+Libre"
 
-  font "MiriamLibre-Bold.ttf"
-  font "MiriamLibre-Regular.ttf"
+  font "MiriamLibre[wght].ttf"
 
   # No zap stanza required
 end
