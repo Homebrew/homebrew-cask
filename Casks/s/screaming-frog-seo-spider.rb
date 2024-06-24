c695ec63f6bbd1048d7e621f7cec3fee4651efb4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-24 10:05:18 +0000

    screaming-frog-seo-spider 20.2

--- Casks/s/screaming-frog-seo-spider.rb
+++ Casks/s/screaming-frog-seo-spider.rb
@@ -1,9 +1,9 @@
 cask "screaming-frog-seo-spider" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "20.1"
-  sha256 arm:   "a39d20ba77e1423840a60318bc91e8831c9b182b4c5fd4ca87ee7f5cf85d0611",
-         intel: "88d33be6bb195ec3a83cfc8c139263e11a3f8e33d39ea47a78c4ec4a0dc25003"
+  version "20.2"
+  sha256 arm:   "8518507cb8ced13ae7b13cda46668836e9eb3d34008bc3f2ddcaed3c4ad746b5",
+         intel: "885211b925a88b6fdd405f68a46aa9e4a978fc5d7b26969b5c6ee57b33989b9a"
 
   url "https://download.screamingfrog.co.uk/products/seo-spider/ScreamingFrogSEOSpider-#{version}-#{arch}.dmg"
   name "Screaming Frog SEO Spider"
