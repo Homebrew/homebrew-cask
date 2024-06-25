74482384257702d1995e6a4eca1557b46874947d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-25 15:51:27 +0000

    proton-mail 1.0.4

--- Casks/p/proton-mail.rb
+++ Casks/p/proton-mail.rb
@@ -1,9 +1,9 @@
 cask "proton-mail" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.0.3"
-  sha256 arm:   "c691cfec43b6876209e8cca8e35b89cb5d20805027341128bcaf35c07234540e",
-         intel: "b042e8f30820f7ef8bdbf7a74d7f35bf948234bd6b31d9fa533cc11d388b96aa"
+  version "1.0.4"
+  sha256 arm:   "72169fad0b27f2d72ab8ce0601ad3ae86ac3831cb1c21bb96fae72c7abb7214a",
+         intel: "a302e5e27722f6b18ae7bf1713b551a4d0c68b3e9cb3ef872cf54f297ad32f09"
 
   url "https://github.com/ProtonMail/inbox-desktop/releases/download/v#{version}/Proton.Mail-#{version}-#{arch}.dmg",
       verified: "github.com/ProtonMail/inbox-desktop/"
