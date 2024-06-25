76d19d28f8c1b4b1c92dddb6aea8dd680c3f61df
Sean Molenaar <smillerdev@me.com>
2024-06-25 10:32:58 +0200

    font-spectral: style fixes

--- Casks/font/font-s/font-spectral.rb
+++ Casks/font/font-s/font-spectral.rb
@@ -2,7 +2,7 @@ cask "font-spectral" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/productiontype/Spectral/archive/master.zip"
+  url "https://github.com/productiontype/Spectral/archive/refs/heads/master.tar.gz"
   name "Spectral"
   homepage "https://github.com/productiontype/Spectral"
 
