81c77e85f02934c2b64fc73f74e9e84b39784947
Klaus Hipp <khipp@users.noreply.github.com>
2024-06-25 14:13:09 +0200

    threema@beta 2.0-beta34

--- Casks/t/threema@beta.rb
+++ Casks/t/threema@beta.rb
@@ -1,9 +1,9 @@
 cask "threema@beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.0-beta33"
-  sha256 arm:   "89d3c013bf3589682ef40a1ce4fbaa1a1df4954fcfdd1c49af039eac34c9044e",
-         intel: "a1c0cc00106f74db099defc5e6cfb059c9526a88fe700b403534fd7331506125"
+  version "2.0-beta34"
+  sha256 arm:   "795bcf0a8d2cea72b5f0481d4aa7a0ab570fc15df713fabd8068715232d8db88",
+         intel: "64c0c1fa66d414b8c77d60796859a1a119a1b2f481519e9a7fa90f34769b677e"
 
   url "https://releases.threema.ch/desktop/#{version}/threema-desktop-v#{version}-macos-#{arch}.dmg"
   name "Threema"
