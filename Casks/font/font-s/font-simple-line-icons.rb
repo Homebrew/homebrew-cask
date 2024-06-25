7161acc18436a2765ae190f788e5726107630aad
Sean Molenaar <smillerdev@me.com>
2024-06-25 10:32:58 +0200

    font-simple-line-icons: style fixes

--- Casks/font/font-s/font-simple-line-icons.rb
+++ Casks/font/font-s/font-simple-line-icons.rb
@@ -2,7 +2,7 @@ cask "font-simple-line-icons" do
   version "2.5.4"
   sha256 "1b0b4f39c0ed88e5507548bfeac01b1177804941ec687195ad551a7ce690b24d"
 
-  url "https://github.com/thesabbir/simple-line-icons/archive/refs/tags/#{version}.zip",
+  url "https://github.com/thesabbir/simple-line-icons/archive/refs/tags/#{version}.tar.gz",
       verified: "github.com/thesabbir/simple-line-icons/"
   name "Simple Line Icons"
   homepage "https://simplelineicons.github.io/"
