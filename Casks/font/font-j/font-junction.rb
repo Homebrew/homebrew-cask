29abd39a6d9f9314f3f702b44d4c7c77fb14d256
Sean Molenaar <smillerdev@me.com>
2024-06-25 10:32:56 +0200

    font-junction: style fixes

--- Casks/font/font-j/font-junction.rb
+++ Casks/font/font-j/font-junction.rb
@@ -2,7 +2,7 @@ cask "font-junction" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/theleagueof/junction/archive/master.zip",
+  url "https://github.com/theleagueof/junction/archive/refs/heads/master.tar.gz",
       verified: "github.com/theleagueof/junction/"
   name "Junction"
   homepage "https://www.theleagueofmoveabletype.com/junction"
