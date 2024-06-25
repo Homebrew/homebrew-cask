14fd489a739f6143569d2450df2136479fb4c14b
Klaus Hipp <khipp@users.noreply.github.com>
2024-06-25 12:51:49 +0200

    deltachat 1.46.1

--- Casks/d/deltachat.rb
+++ Casks/d/deltachat.rb
@@ -1,9 +1,9 @@
 cask "deltachat" do
   arch arm: "arm64", intel: "universal"
 
-  version "1.46.0"
-  sha256 arm:   "d4bfb4fd8e24100ee807332248c5506fe204d4ce585ff5d0418fee9e53187af3",
-         intel: "b03ebf4b5eabbdfec6eb42c856308a896de24098819c0339c7ce98f40a3142c0"
+  version "1.46.1"
+  sha256 arm:   "4ef319b3608594e8477fde66ef5711fe3e658c67cc2f76ad17eb8cecca21df6b",
+         intel: "e07972fb2f8e4ce83ad77339b57b77e18aac954000f0e3387429600230821656"
 
   url "https://download.delta.chat/desktop/v#{version}/DeltaChat-#{version}-#{arch}.dmg"
   name "DeltaChat"
