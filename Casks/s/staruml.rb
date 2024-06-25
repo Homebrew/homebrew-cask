6ea10e6ff5495e5e1c37e0ec42bfa1981ddd5b39
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-06-25 14:48:12 -0400

    staruml 6.1.2

--- Casks/s/staruml.rb
+++ Casks/s/staruml.rb
@@ -1,9 +1,9 @@
 cask "staruml" do
   arch arm: "-arm64"
 
-  version "6.1.1"
-  sha256 arm:   "d0788ea168867c44cb0c08a7bbe11631c552502f366f11014c5afcf330cb97a4",
-         intel: "a2d770631cf59bf82658dc3fd971a27e34e9b29e343516e6c29d90160a5b4d6a"
+  version "6.1.2"
+  sha256 arm:   "b94d5ec9b7229228c5270d05e71bc1812ff71adf2075fe88ab29f2f029de04ef",
+         intel: "304fe708258035733cd8e6d9760d4dd847a2317d8b3c2ba7ab84dd77c5b6c82b"
 
   url "https://files.staruml.io/releases-v#{version.major}/StarUML-#{version}#{arch}.dmg"
   name "StarUML"
