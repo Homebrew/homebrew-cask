1f6d5c214268b0a3ff684150496fd9e953a8de29
Sean Molenaar <smillerdev@me.com>
2024-06-25 10:32:55 +0200

    font-icomoon: style fixes

--- Casks/font/font-i/font-icomoon.rb
+++ Casks/font/font-i/font-icomoon.rb
@@ -2,7 +2,7 @@ cask "font-icomoon" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/Keyamoon/IcoMoon-Free/archive/master.zip",
+  url "https://github.com/Keyamoon/IcoMoon-Free/archive/refs/heads/master.tar.gz",
       verified: "github.com/Keyamoon/IcoMoon-Free/"
   name "IcoMoon"
   homepage "https://icomoon.io/"
