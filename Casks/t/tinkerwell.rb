f6122c00649a4daebc92984fcc69668433911029
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-26 12:55:38 +0000

    tinkerwell 4.17.0

--- Casks/t/tinkerwell.rb
+++ Casks/t/tinkerwell.rb
@@ -1,9 +1,9 @@
 cask "tinkerwell" do
   arch arm: "-arm64"
 
-  version "4.16.0"
-  sha256 arm:   "2272be82a567412d8cc84819a66dfa1f89bbb6945692a75fb6c7d3c5f2919572",
-         intel: "4a00186fdcb2a18d6a7a2321380cc3896f1ede4eb93424af078cc1af169f2041"
+  version "4.17.0"
+  sha256 arm:   "56a927c8d44c73423d741b71d6fc7a2ade692a27169fe7ea936cf1b843505f01",
+         intel: "d3c8a7832b6ff98c74c4613fe0eb426406368298bdaa37f2b97b6bed088bc3f4"
 
   url "https://download.tinkerwell.app/tinkerwell/Tinkerwell-#{version}#{arch}.dmg"
   name "Tinkerwell"
