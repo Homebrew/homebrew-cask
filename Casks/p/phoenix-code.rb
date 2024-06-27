32d7c824f57de2a42f73eba7fd1c505e3c183f4a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-27 15:44:56 +0000

    phoenix-code 3.8.8

--- Casks/p/phoenix-code.rb
+++ Casks/p/phoenix-code.rb
@@ -1,9 +1,9 @@
 cask "phoenix-code" do
   arch arm: "aarch64", intel: "x64"
 
-  version "3.7.3"
-  sha256 arm:   "c8b4ae5b6b1d9cd6c648dbe4d6be59c3433c8b9b24e2d587c3a6b91361b91308",
-         intel: "0e6e94db88f6bd11e67bc05bfb0294303db9bc3973cddf42a2f6bc23c4514112"
+  version "3.8.8"
+  sha256 arm:   "5e22f890b090ff3313d58a47f0b7efc616855bcbf98d8d487f8cffa71db451f2",
+         intel: "0b22b912b44ba4cfd41dfef26839ce86111621107ea9c37ef87ec1bfb60f37eb"
 
   url "https://github.com/phcode-dev/phoenix-desktop/releases/download/prod-app-v#{version}/Phoenix.Code_#{version}_#{arch}.dmg",
       verified: "github.com/phcode-dev/phoenix-desktop/"
