868e9cff3d8c68b207f6f4a258752f3b8df92ac5
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-06-27 14:50:28 -0400

    jbrowse 2.12.3

--- Casks/j/jbrowse.rb
+++ Casks/j/jbrowse.rb
@@ -1,6 +1,6 @@
 cask "jbrowse" do
-  version "2.12.2"
-  sha256 "ccb8beaf985bd2fdc34bf3096f7ca1157026274f337e6f4e068cae617aa7f11c"
+  version "2.12.3"
+  sha256 "422d159985414b2bcdc5e1f1ff73715ee170bba4162ef56d07a58bc54674bb79"
 
   url "https://github.com/GMOD/jbrowse-components/releases/download/v#{version}/jbrowse-desktop-v#{version}-mac.dmg",
       verified: "github.com/GMOD/jbrowse-components/"
