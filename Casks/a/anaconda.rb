dbd673f0703cb53cfb32eac9564f038f6fc304cd
Klaus Hipp <khipp@users.noreply.github.com>
2024-06-27 10:08:05 +0200

    anaconda 2024.06-1

--- Casks/a/anaconda.rb
+++ Casks/a/anaconda.rb
@@ -1,9 +1,9 @@
 cask "anaconda" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "2024.02-1"
-  sha256 arm:   "14a1c80af18c2c2e743e63cdb41228cd554a3fdb250563b6978348c80b6860f6",
-         intel: "944aee9b90f7d8a2a997d0337cb37219757f22e76c10de38c7c68191c6b5b226"
+  version "2024.06-1"
+  sha256 arm:   "f9e0795431910ee4ac341c0cc2a0b62e6c497440789b4117c20736db45b34204",
+         intel: "9b2aa48bfc58e337176d8b5f3776692815807e1346ec12ebcc46a01801bb4ee0"
 
   url "https://repo.anaconda.com/archive/Anaconda3-#{version}-MacOSX-#{arch}.sh"
   name "Anaconda Distribution"
