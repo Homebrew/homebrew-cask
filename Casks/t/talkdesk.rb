c0825e0aa4b336ee03e88b5f5276fa3adf68bf04
Aleksy Rudy <DanzigerGeist@icloud.com>
2024-06-28 16:04:13 +0200

    renamed cask to just talkdesk

--- Casks/t/talkdesk.rb
+++ Casks/t/talkdesk.rb
@@ -1,5 +1,5 @@
 # https://support.talkdesk.com/hc/en-us/articles/13920616167963-Workspace-Desktop-App-for-Talkdesk-CX-Cloud
-cask "talkdesk-desktop" do
+cask "talkdesk" do
   version "2.0.1-alpha.1"
   sha256 "16eb1e4708ea74250231404caf18a472624dd67fc907c4f438d65fc0694c63c5"
 
