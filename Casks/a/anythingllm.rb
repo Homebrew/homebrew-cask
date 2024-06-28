e5e56d0df3f93efece47100c5620da17da5df502
mstgrv <mstgrv@gmail.com>
2024-06-28 14:43:54 -0700

    anythingllm 1.5.8

--- Casks/a/anythingllm.rb
+++ Casks/a/anythingllm.rb
@@ -1,9 +1,9 @@
 cask "anythingllm" do
   arch arm: "-Silicon"
 
-  version "1.5.7"
-  sha256 arm:   "66809b298c238e82405813d363988b4fd41bcfd6381d52844896d3dde5da22df",
-         intel: "a658341aece9db418f42a026476c6d3281e6f7181aa7c34fbd7a937c3c54cc1c"
+  version "1.5.8"
+  sha256 arm:   "816797207043cb1d8eeb8ce13f37a38252d774d84c773ce28b964d921deb533e",
+         intel: "3aa886db87116209630caac3d408d9903fc2dd3b2752baa65dbe27b94cdb2654"
 
   url "https://s3-us-west-1.amazonaws.com/public.useanything.com/latest/AnythingLLMDesktop#{arch}.dmg",
       verified: "s3-us-west-1.amazonaws.com/public.useanything.com/"
