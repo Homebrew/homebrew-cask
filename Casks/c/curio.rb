fca5cd3bd41aed8517798e16b95072f5228f9acd
Klaus Hipp <khipp@users.noreply.github.com>
2024-06-28 10:57:48 +0200

    curio 29.0

--- Casks/c/curio.rb
+++ Casks/c/curio.rb
@@ -1,8 +1,8 @@
 cask "curio" do
-  version "25.0.1,25101"
-  sha256 "4cbb833fc6ceeb7442fb5b0ca25b2cd8670da9fb0e9bded0c118d0db2117cce7"
+  version "29.0,29004"
+  sha256 "03b9b381fb272f10e8b5e0316a499dbed45450eebfcea5bb6deb66589498a4fb"
 
-  url "https://www.zengobi.com/downloads/Curio#{version.csv.second.no_dots}.zip"
+  url "https://www.zengobi.com/downloads/Curio#{version.csv.second.no_dots}.dmg"
   name "Curio"
   desc "Note-taking and organisation tool"
   homepage "https://zengobi.com/curio/"
@@ -12,7 +12,19 @@ cask "curio" do
     strategy :sparkle
   end
 
-  depends_on macos: ">= :big_sur"
+  depends_on macos: ">= :monterey"
 
   app "Curio.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.zengobi.curio.Curio-Quick-Look-*",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.zengobi.curio.sfl*",
+    "~/Library/Application Support/Curio",
+    "~/Library/Caches/com.zengobi.curio",
+    "~/Library/Containers/com.zengobi.curio.Curio-Quick-Look-*",
+    "~/Library/HTTPStorages/com.zengobi.curio",
+    "~/Library/Preferences/com.zengobi.curio.plist",
+    "~/Library/Saved Application State/com.zengobi.curio.savedState",
+    "~/Library/WebKit/com.zengobi.curio",
+  ]
 end
