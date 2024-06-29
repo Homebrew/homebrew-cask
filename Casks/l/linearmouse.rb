08023a85ab5b1179613b5cfb58a7e0b5e99044fc
Klaus Hipp <khipp@users.noreply.github.com>
2024-06-29 14:17:05 +0200

    linearmouse 0.10.0

--- Casks/l/linearmouse.rb
+++ Casks/l/linearmouse.rb
@@ -1,6 +1,6 @@
 cask "linearmouse" do
-  version "0.9.5"
-  sha256 "4f6e47d8298306ddc9e73682f20cbcecfc8938d3ec0d9a45ed2e5e92c47382b0"
+  version "0.10.0"
+  sha256 "21cd1d11010dd5eefc1ff3368ce8366c4114f99b59ea737aca4309f9d6fb69f1"
 
   url "https://github.com/linearmouse/linearmouse/releases/download/v#{version}/LinearMouse.dmg",
       verified: "github.com/linearmouse/linearmouse/"
