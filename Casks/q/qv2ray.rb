4cdb6782388857081aea90b0755fd210c87e828b
wickles <wickles137@gmail.com>
2024-06-30 12:00:09 -0700

    add rosetta caveat: qv2ray

--- Casks/q/qv2ray.rb
+++ Casks/q/qv2ray.rb
@@ -2,11 +2,10 @@ cask "qv2ray" do
   version "2.7.0"
   sha256 "457e1b70989515ddb9a1bd4c0635d4ea7d5eda17435bd63908151621b1235a97"
 
-  url "https://github.com/Qv2ray/Qv2ray/releases/download/v#{version}/Qv2ray-v#{version}-macOS-x64.dmg",
-      verified: "github.com/Qv2ray/Qv2ray/"
+  url "https://github.com/Qv2ray/Qv2ray/releases/download/v#{version}/Qv2ray-v#{version}-macOS-x64.dmg"
   name "Qv2ray"
   desc "V2Ray GUI client with extensive protocol support"
-  homepage "https://qv2ray.net/"
+  homepage "https://github.com/Qv2ray/Qv2ray"
 
   deprecate! date: "2023-12-17", because: :discontinued
 
@@ -20,4 +19,8 @@ cask "qv2ray" do
     "~/Library/Preferences/qv2ray",
     "~/Library/Saved Application State/com.github.qv2ray.savedState",
   ]
+
+  caveats do
+    requires_rosetta
+  end
 end
