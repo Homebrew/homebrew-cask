42e0db2f60b359125f95b4aeec62b771bad6379b
Klaus Hipp <khipp@users.noreply.github.com>
2024-06-30 16:53:50 +0200

    metashape: update url

--- Casks/m/metashape.rb
+++ Casks/m/metashape.rb
@@ -2,7 +2,8 @@ cask "metashape" do
   version "2.1.2"
   sha256 "bbc204e8bf03dca4863abc8f11309c1794c4c3ff8fa9f2848d726843baa485ca"
 
-  url "http://download.agisoft.com/metashape_#{version.dots_to_underscores}.dmg"
+  url "https://s3-eu-west-1.amazonaws.com/download.agisoft.com/metashape_#{version.dots_to_underscores}.dmg",
+      verified: "s3-eu-west-1.amazonaws.com/download.agisoft.com/"
   name "Agisoft Metashape Standard Edition"
   desc "Process digital images and generate 3D spatial data"
   homepage "https://www.agisoft.com/"
