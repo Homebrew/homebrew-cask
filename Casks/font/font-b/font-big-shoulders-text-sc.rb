5b4b2eaecbb19056f405c1d0449ed09675e94ad6
BrewTestBot <BrewTestBot@users.noreply.github.com>
2024-06-30 00:13:18 +0000

    Update Google Fonts

---
+++ Casks/font/font-b/font-big-shoulders-text-sc.rb
@@ -0,0 +1,12 @@
+cask "font-big-shoulders-text-sc" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/bigshoulderstextsc/BigShouldersTextSC%5Bwght%5D.ttf"
+  name "Big Shoulders Text SC"
+  homepage "https://github.com/xotypeco/big_shoulders"
+
+  font "BigShouldersTextSC[wght].ttf"
+
+  # No zap stanza required
+end
