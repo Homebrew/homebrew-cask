84ad81e2ea464b5ce1c333ba77fe09617549122a
huhu415 <yanwave@qq.com>
2024-07-01 21:42:00 +0800

    lazycat 1.0.96 (new cask)

--- Casks/l/lazycat.rb
+++ Casks/l/lazycat.rb
@@ -1,11 +1,11 @@
 cask "lazycat" do
   arch arm: "arm64", intel: "x64"
 
-  version "v1.0.96"
+  version "1.0.96"
   sha256  arm:   "8bfb3c6c64d62ee2fb91778e7db3cf8fb0ce90efa4f579aac10521c20ae9cb48",
           intel: "3f00f3bbb8d86c86424e4ec2b9d2d0194ec0c63eceae300ed5b3bce1e9349a71"
 
-  url "https://dl.lazycat.cloud/client/desktop/stable/lzc-client-desktop_#{version}_#{arch}.dmg"
+  url "https://dl.lazycat.cloud/client/desktop/stable/lzc-client-desktop_v#{version}_#{arch}.dmg"
   name "LazyCat"
   desc "懒猫微服客户端"
   homepage "https://lazycat.cloud/"
@@ -14,6 +14,7 @@ cask "lazycat" do
     url "https://dl.lazycat.cloud/client/desktop/lzc-client-desktop_#{arch}.dmg.metadata.json"
     strategy :json do |json|
       json["buildVersion"]
+      version.delete_prefix('v')
     end
   end
 
