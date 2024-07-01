01d2257b017951b783416533b00763ba4ce16c87
huhu415 <yanwave@qq.com>
2024-07-01 21:13:06 +0800

    lazycat 1.0.96 (new cask)

--- Casks/l/lazycat.rb
+++ Casks/l/lazycat.rb
@@ -2,8 +2,7 @@ cask "lazycat" do
   arch arm: "arm64", intel: "x64"
 
   version "v1.0.96"
-  sha256  arm: "8bfb3c6c64d62ee2fb91778e7db3cf8fb0ce90efa4f579aac10521c20ae9cb48",
-          intel: "3f00f3bbb8d86c86424e4ec2b9d2d0194ec0c63eceae300ed5b3bce1e9349a71"
+  sha256  arm: "8bfb3c6c64d62ee2fb91778e7db3cf8fb0ce90efa4f579aac10521c20ae9cb48", intel: "3f00f3bbb8d86c86424e4ec2b9d2d0194ec0c63eceae300ed5b3bce1e9349a71"
 
   url "https://dl.lazycat.cloud/client/desktop/stable/lzc-client-desktop_#{version}_#{arch}.dmg"
   name "LazyCat"
