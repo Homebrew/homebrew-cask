076f5292117b49fc3c1859c379954b7fd7e3334a
Bevan Kay <email@bevankay.me>
2024-07-01 10:28:36 +1000

    elektron-transfer 1.8.10

--- Casks/e/elektron-transfer.rb
+++ Casks/e/elektron-transfer.rb
@@ -1,21 +1,15 @@
 cask "elektron-transfer" do
-  version "1.8.6,fc5c8eff-954f-5651-9283-9a17a9ae9db9"
-  sha256 "05b590054659decd46c47431e47c0747330e000bc928f8a43e1e4c9c8f202355"
+  version "1.8.10"
+  sha256 "11002ad64daecd585a0f27219c1c97e2fa944fa79d2311f2897977f4577d1480"
 
-  url "https://se-elektron-devops.s3.amazonaws.com/release/#{version.csv.second}/Elektron_Transfer_#{version.csv.first}.dmg",
-      verified: "se-elektron-devops.s3.amazonaws.com/"
+  url "https://cdn.www.elektron.se/media/downloads/transfer/Elektron_Transfer_#{version.csv.first}.dmg"
   name "Elektron Transfer"
   desc "Transfer samples, presets, sounds, projects and firmware to Elektron devices"
   homepage "https://www.elektron.se/en/download-support-transfer"
 
   livecheck do
     url :homepage
-    regex(%r{([a-zA-Z0-9_-]+)/Elektron[._-]Transfer[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map do |match|
-        "#{match[1]},#{match[0]}"
-      end
-    end
+    regex(/Elektron[._-]Transfer[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
   depends_on macos: ">= :high_sierra"
