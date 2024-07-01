e7d6f84d5465165a264319c848ba1d365d67b78c
Philipp Jetschina <kontakt@jetschina.de>
2024-07-01 23:54:27 +0200

    bump-atomic-wallter-2.87.3

--- Casks/a/atomic-wallet.rb
+++ Casks/a/atomic-wallet.rb
@@ -1,14 +1,14 @@
 cask "atomic-wallet" do
-  version "2.76.4"
-  sha256 "909783befef17913e5d4b4500c35cd0a99beb2c9ed45970bee724c593bde0422"
+  version "2.87.3"
+  sha256 "80a5aa67609906e7aa5be15d65cb2d84b1c4ce3414eff0154d821fc451b1c112"
 
-  url "https://get.atomicwallet.io/download/atomicwallet-#{version}.dmg"
+  url "https://releases.atomicwallet.io/AtomicWallet-#{version}.dmg"
   name "Atomic Wallet"
   desc "Manage Bitcoin, Ethereum, XRP, Litecoin, XLM and over 300 other coins and tokens"
   homepage "https://atomicwallet.io/"
 
   livecheck do
-    url "https://get.atomicwallet.io/download/"
+    url "https://releases.atomicwallet.io/"
     regex(/href=.*?atomicwallet[._-](\d+(?:\.\d+)+[._-]\d+)\.dmg/i)
   end
 
