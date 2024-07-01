1ab85cf4506526385677a383536f3d044b5ed2e7
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-01 19:05:39 +0200

    fedora-media-writer 5.1.2

--- Casks/f/fedora-media-writer.rb
+++ Casks/f/fedora-media-writer.rb
@@ -1,9 +1,9 @@
 cask "fedora-media-writer" do
   arch arm: "-arm64"
 
-  version "5.1.1"
-  sha256 arm:   "192fc5de90d76e85183b97db45ece0715b6bad7b1902968611879f620ae35f03",
-         intel: "84f99e9abcb9dd782836baa046ec651e91afdcf2a0c4e9d521e3378082d8c862"
+  version "5.1.2"
+  sha256 arm:   "b008bccf7709ebab6c3e376d68913e2cfd437dd55e6408cd311a3fc8a31793fa",
+         intel: "f3f1cbe6a5f039b54f553caf2126f4b5c5b0852becf6f0d0f37e981788b3d259"
 
   url "https://github.com/FedoraQt/MediaWriter/releases/download/#{version}/FedoraMediaWriter-osx#{arch}-#{version}.dmg",
       verified: "github.com/FedoraQt/MediaWriter/"
