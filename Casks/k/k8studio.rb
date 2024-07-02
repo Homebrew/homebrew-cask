2ad4b63fc68336d9af5ca1b83de6e9bd55f4bcd0
gui <gquiman@gmail.com>
2024-07-02 12:52:10 +0200

    Remove End message, add version as variable and better handling or arch

--- Casks/k/k8studio.rb
+++ Casks/k/k8studio.rb
@@ -2,29 +2,15 @@
 # frozen_string_literal: true
 
 cask "k8studio" do
-  version "1.0.11"
+  arch arm: "-arm64", intel: ""
+  version "1.0.11-beta"
+  sha256 arm:   "77f5e0b4fb2d74070ef9b3a8d55e11fed326e9a4941ae855272a412983bd843b",
+         intel: "c78651df1b92abd9a55dfb4c89129d046b04d19cda634759af204c59b102373e"
 
-  on_arm do
-    sha256 "77f5e0b4fb2d74070ef9b3a8d55e11fed326e9a4941ae855272a412983bd843b"
-
-    url "https://github.com/guiqui/k8Studio/releases/download/v1.0.11-beta/K8Studio-1.0.11-beta-arm64.dmg"
-  end
-  on_intel do
-    sha256 "c78651df1b92abd9a55dfb4c89129d046b04d19cda634759af204c59b102373e"
-
-    url "https://github.com/guiqui/k8Studio/releases/download/v1.0.11-beta/K8Studio-1.0.11-beta.dmg"
-  end
-
+  url "https://github.com/guiqui/k8Studio/releases/download/v#{version}/K8Studio-#{version}#{arch}.dmg"
   name "K8studio"
   desc "Kubernetes GUI"
   homepage "https://k8studio.io/"
 
   app "K8Studio.app"
-
-  caveats do
-    <<~EOS
-      K8Studio has been installed. You can run it with:
-        open -a K8Studio
-    EOS
-  end
 end
