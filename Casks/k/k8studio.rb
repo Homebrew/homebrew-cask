d5cb28400fee24d631b9a48cf7655563c87d6cbb
gui <gquiman@gmail.com>
2024-07-02 10:22:10 +0200

    Adding file

---
+++ Casks/k/k8studio.rb
@@ -0,0 +1,26 @@
+cask "k8studio" do
+  version "1.0.11-beta"
+
+
+  if Hardware::CPU.intel?
+    url "https://github.com/guiqui/k8Studio/releases/download/v1.0.11-beta/K8Studio-1.0.11-beta.dmg"
+    sha256 "c78651df1b92abd9a55dfb4c89129d046b04d19cda634759af204c59b102373e"
+  else
+    url "https://github.com/guiqui/k8Studio/releases/download/v1.0.11-beta/K8Studio-1.0.11-beta-arm64.dmg"
+    sha256 "77f5e0b4fb2d74070ef9b3a8d55e11fed326e9a4941ae855272a412983bd843b"
+  end
+
+  name "K8studio"
+  desc "K8studio Kubernetes GUI"
+  homepage "https://k8studio.io"
+
+  app "K8Studio.app"
+
+  caveats do
+    <<~EOS
+      K8Studio has been installed. You can run it with:
+        open -a K8Studio
+    EOS
+  end
+end
+
