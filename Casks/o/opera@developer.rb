293fe1876da958163bfc35c6e9cb0c8c383a4fb3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-02 15:43:09 +0000

    opera@developer 113.0.5215.0

--- Casks/o/opera@developer.rb
+++ Casks/o/opera@developer.rb
@@ -1,6 +1,6 @@
 cask "opera@developer" do
-  version "113.0.5210.0"
-  sha256 "90ed06764e832b747eb8ef30fce0a0c3001ba69ecc3f218ecb21c4e8e1bfbf50"
+  version "113.0.5215.0"
+  sha256 "d682799bf89ac0445d3a2c8acff5ace142b76bc076756db2e9819e844f66fb94"
 
   url "https://get.geo.opera.com/pub/opera-developer/#{version}/mac/Opera_Developer_#{version}_Setup.dmg"
   name "Opera Developer"
