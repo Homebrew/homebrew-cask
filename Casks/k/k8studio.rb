8b76a1c92a5814f09d611ff98db8e92df7ac71c6
gui <gquiman@gmail.com>
2024-07-02 13:09:18 +0200

    Resolve homepage and download url discrepancy

--- Casks/k/k8studio.rb
+++ Casks/k/k8studio.rb
@@ -7,7 +7,7 @@ cask "k8studio" do
   sha256 arm:   "77f5e0b4fb2d74070ef9b3a8d55e11fed326e9a4941ae855272a412983bd843b",
          intel: "c78651df1b92abd9a55dfb4c89129d046b04d19cda634759af204c59b102373e"
 
-  url "https://github.com/guiqui/k8Studio/releases/download/v#{version}/K8Studio-#{version}#{arch}.dmg"
+  url "https://github.com/guiqui/k8Studio/releases/download/v#{version}/K8Studio-#{version}#{arch}.dmg",verified: "github.com/guiqui/k8Studio/"
   name "K8studio"
   desc "Kubernetes GUI"
   homepage "https://k8studio.io/"
