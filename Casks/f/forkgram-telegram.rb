3e6d6f64f1875c256fa1e9c1e7da6bda39482904
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-02 00:47:53 +0000

    forkgram-telegram 5.2.1

--- Casks/f/forkgram-telegram.rb
+++ Casks/f/forkgram-telegram.rb
@@ -1,9 +1,9 @@
 cask "forkgram-telegram" do
   arch arm: "arm64", intel: "x86"
 
-  version "5.2.0"
-  sha256 arm:   "9df0ea621367f180f7400a5337cac5386dd523b18aee520bd3e15510d6a45e33",
-         intel: "f25c2e383b339b59619c0a294639718c86549eeca5706d61f230d41b814363b6"
+  version "5.2.1"
+  sha256 arm:   "f47e689d60a419e69fc43d82c24ee9fb0b1e3ec7b0f0671b1800ceb19c935d81",
+         intel: "8dcb5cb6f8ee842c7d202115c27f4c7e091eefddf1aaa33d133edef531b7e981"
 
   url "https://github.com/Forkgram/tdesktop/releases/download/v#{version}/Forkgram.macOS.no.auto-update_#{arch}.zip"
   name "Forkgram"
