086e71d7cbd1df45b1f0c4edca174d9e121781c3
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-03 15:18:52 +0200

    miditrail: update homepage and source

--- Casks/m/miditrail.rb
+++ Casks/m/miditrail.rb
@@ -1,20 +1,19 @@
 cask "miditrail" do
-  version "2.0.2,77944"
+  version "2.0.2"
   sha256 "15204f7c61703b66c053fc96b6552f53a53b192c3c05c1a4c5a93bd2ce640281"
 
-  url "https://acc.dl.osdn.jp/miditrail/#{version.csv.second}/MIDITrail-Ver.#{version.csv.first}-macOS.zip",
-      verified: "acc.dl.osdn.jp/miditrail/"
+  url "https://github.com/wdmss/MIDITrail-macOS/releases/download/v#{version}/MIDITrail-Ver.#{version}-macOS.zip",
+      verified: "github.com/wdmss/MIDITrail-macOS/"
   name "MIDITrail"
   desc "MIDI player which provides 3D visualization of MIDI data sets"
-  homepage "https://osdn.net/projects/miditrail/"
+  homepage "https://www.yknk.org/miditrail/en/"
 
-  livecheck do
-    url "https://ja.osdn.net/projects/miditrail/releases/rss"
-    regex(%r{url=.+downloads/(\d+)/MIDITrail[._-]Ver[._-](\d+(?:\.\d+)+)[._-]macOS\.zip}i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| "#{match[1]},#{match[0]}" }
-    end
-  end
+  depends_on macos: ">= :sierra"
 
   app "MIDITrail/MIDITrail.app"
+
+  zap trash: [
+    "~/Library/Preferences/jp.sourceforge.users.yknk.MIDITrail.plist",
+    "~/Library/Saved Application State/jp.sourceforge.users.yknk.MIDITrail.savedState",
+  ]
 end
