15edad3e138d9924aafebf4b84af8f6495710abf
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-07-03 22:07:16 -0400

    catlight 3.7.3

--- Casks/c/catlight.rb
+++ Casks/c/catlight.rb
@@ -1,6 +1,6 @@
 cask "catlight" do
-  version "3.7.2"
-  sha256 "fbe067e9c995f2bfb99ee4909ef0dbe216329e730546b9c28a0a7e2b71bc5cd4"
+  version "3.7.3"
+  sha256 "d0e279f07136d0665b5188386d2a3a59a8ad9a456c968fca196a5d260569f098"
 
   url "https://download.catlight.io/rel/mac/release/CatLightSetup-#{version}.zip"
   name "catlight"
