6ff1152dda1a9cf48b7d3729f9979017730bcb37
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-04 19:24:04 +0200

    sixtyforce: update livecheck, use versioned url

--- Casks/s/sixtyforce.rb
+++ Casks/s/sixtyforce.rb
@@ -1,15 +1,15 @@
 cask "sixtyforce" do
-  version "2.0.2,87"
-  sha256 :no_check
+  version "2.0.2"
+  sha256 "c83a2b3a194e5c62647b2be503ea331cf9e4a29baa5d5626fd63f1f9c7a7eab4"
 
-  url "https://sixtyforce.com/download/sixtyforce.zip"
+  url "https://sixtyforce.com/download/sixtyforce_#{version.no_dots}.zip"
   name "sixtyforce"
   desc "N64 emulator"
   homepage "https://sixtyforce.com/"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url "https://sixtyforce.com/download/"
+    regex(/>\s*sixtyforce\s*v?(\d+(?:\.\d+)*)\.zip/i)
   end
 
   app "sixtyforce.app"
@@ -20,4 +20,8 @@ cask "sixtyforce" do
     "~/Library/Caches/com.Gerrit.sixtyforce",
     "~/Library/Preferences/com.Gerrit.sixtyforce.plist",
   ]
+
+  caveats do
+    requires_rosetta
+  end
 end
