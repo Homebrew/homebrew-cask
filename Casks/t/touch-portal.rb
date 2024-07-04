e34daf1695a2969acfaf6112da79a2bd61f1ecb7
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-04 22:58:20 +0200

    touch-portal 4.2.0

--- Casks/t/touch-portal.rb
+++ Casks/t/touch-portal.rb
@@ -1,8 +1,10 @@
 cask "touch-portal" do
-  version "3.1.11"
+  arch arm: "M1"
+
+  version "4.2.0"
   sha256 :no_check
 
-  url "https://www.touch-portal.com/downloads/TouchPortalMacRelease.dmg"
+  url "https://www.touch-portal.com/downloads/releases/mac/TouchPortalMacRelease#{arch}.dmg"
   name "Touch Portal"
   desc "Macro remote control"
   homepage "https://www.touch-portal.com/"
@@ -11,6 +13,8 @@ cask "touch-portal" do
     url :url
     strategy :extract_plist
   end
+
+  depends_on macos: ">= :el_capitan"
 
   app "TouchPortal.app"
 
