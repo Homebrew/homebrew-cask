01b2afc6937ce59a025024b177d5fae4621a7094
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-04 09:45:04 +0000

    navicat-for-sql-server 17.0.12

--- Casks/n/navicat-for-sql-server.rb
+++ Casks/n/navicat-for-sql-server.rb
@@ -1,5 +1,5 @@
 cask "navicat-for-sql-server" do
-  version "17.0.10"
+  version "17.0.12"
   sha256 :no_check
 
   url "https://dn.navicat.com/download/navicat#{version.major_minor.no_dots}_sqlserver_en.dmg"
