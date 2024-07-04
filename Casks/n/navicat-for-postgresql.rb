562567245776b2983c22f5c1717d726a6a9bcc05
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-04 09:44:24 +0000

    navicat-for-postgresql 17.0.12

--- Casks/n/navicat-for-postgresql.rb
+++ Casks/n/navicat-for-postgresql.rb
@@ -1,5 +1,5 @@
 cask "navicat-for-postgresql" do
-  version "17.0.10"
+  version "17.0.12"
   sha256 :no_check
 
   url "https://dn.navicat.com/download/navicat#{version.major_minor.no_dots}_pgsql_en.dmg"
