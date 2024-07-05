be3a695836cddfb39d85e4d21635af9d7fadf189
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-05 15:39:40 +0200

    idrive: update livecheck, use versioned url

--- Casks/i/idrive.rb
+++ Casks/i/idrive.rb
@@ -1,21 +1,31 @@
 cask "idrive" do
-  version "3.5.10.79"
-  sha256 :no_check
+  version "3.5.10.80,070624"
+  sha256 "231518ded3b6273e56ae68c98bfb068d90c6a7e836fa761f29ad8d402d243714"
 
-  url "https://www.idrive.com/downloads/IDrive.dmg"
+  url "https://static.idriveonlinebackup.com/downloads/#{version.csv.second}/IDrive.dmg",
+      verified: "static.idriveonlinebackup.com/downloads/"
   name "iDrive"
   desc "Cloud backup and storage solution"
   homepage "https://www.idrive.com/"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url "https://static.idriveonlinebackup.com/downloads/version_mac.js"
+    strategy :page_match do |page|
+      version = page.match(/Version:?\s*(\d+(?:\.\d+)*)/i)
+      next if version.blank?
+
+      download_id = page.match(%r{downloads/(\w+)/}i)
+      next if download_id.blank?
+
+      "#{version[1]},#{download_id[1]}"
+    end
   end
 
   pkg "IDrive.pkg"
 
   uninstall launchctl: [
               "com.iDrive.FinderPluginApp",
+              "com.prosoftnet.DaemonHelper",
               "IDriveDaemon",
               "IDSyncDaemon",
               "IDWifiManager",
