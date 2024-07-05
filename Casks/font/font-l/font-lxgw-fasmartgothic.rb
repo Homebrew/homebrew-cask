ea3182530c99c9eb99ebbfa6a40b0696a2ee5537
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-05 10:21:10 +0200

    font-lxgw-fasmartgothic 1.124

--- Casks/font/font-l/font-lxgw-fasmartgothic.rb
+++ Casks/font/font-l/font-lxgw-fasmartgothic.rb
@@ -1,6 +1,6 @@
 cask "font-lxgw-fasmartgothic" do
-  version "1.123.2"
-  sha256 "77210a70955a5cf216fb6513934943527fb904641270a87a47812708c2f19a03"
+  version "1.124"
+  sha256 "fdd76652cda628b3b7fe9e6dacde8a5c45ff0808712ebc512bc9287a56a22cc1"
 
   url "https://github.com/lxgw/LxgwNeoXiHei/releases/download/v#{version}/LXGWFasmartGothic.ttf"
   name "LXGW FasmartGothic"
