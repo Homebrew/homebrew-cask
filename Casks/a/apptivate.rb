cbab32753a2300a82e34663e69b26ba7d0a8dfb8
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-06 00:25:47 +0200

    apptivate: update livecheck, use versioned url

--- Casks/a/apptivate.rb
+++ Casks/a/apptivate.rb
@@ -1,15 +1,15 @@
 cask "apptivate" do
-  version "2.2.1,15"
-  sha256 :no_check
+  version "2.2.1"
+  sha256 "06515419c235aab0a17961d5288233d6fee64f9f75905f5473f78e882b3c1cb2"
 
-  url "http://www.apptivateapp.com/resources/Apptivate.app.zip"
+  url "http://www.apptivateapp.com/resources/Apptivate-#{version}.app.zip"
   name "Apptivate"
   desc "Create global hotkeys for your files and applications"
   homepage "http://www.apptivateapp.com/"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url "http://www.apptivateapp.com/changelog.txt"
+    regex(/Apptivate\s*v?(\d+(?:\.\d+)+)/i)
   end
 
   app "Apptivate.app"
@@ -19,4 +19,8 @@ cask "apptivate" do
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/se.cocoabeans.apptivate.sfl*",
     "~/Library/Preferences/se.cocoabeans.apptivate.plist",
   ]
+
+  caveats do
+    requires_rosetta
+  end
 end
