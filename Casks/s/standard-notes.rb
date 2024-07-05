afbadaedc2230ffe3010b80b55514cca1e396fc3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-05 06:49:10 +0000

    standard-notes 3.194.13

--- Casks/s/standard-notes.rb
+++ Casks/s/standard-notes.rb
@@ -1,9 +1,9 @@
 cask "standard-notes" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.194.0"
-  sha256 arm:   "2afbb7e6e98189dd8925737936b39e0d13c8d3726c090f9c9f89ea8a0f308174",
-         intel: "474a5cd5f8de08dc7ecca5acf38aac67433c5a1e1a0fce1b644612601cc810c8"
+  version "3.194.13"
+  sha256 arm:   "0fba480db845336b5257f25e4de8579645f0e1421bb09591b457b0385da4ab7e",
+         intel: "8ccc921a25c3056eec0811fc45f39b1422d9af0a796966b4cfe51850d00d7519"
 
   url "https://github.com/standardnotes/app/releases/download/%40standardnotes%2Fdesktop%40#{version}/standard-notes-#{version}-mac-#{arch}.zip",
       verified: "github.com/standardnotes/app/"
