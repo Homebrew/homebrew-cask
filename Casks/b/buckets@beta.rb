fb08284f38dc53a2576c671a9ab0047d06e17320
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-07-05 11:52:44 -0400

    buckets@beta 0.76.0

--- Casks/b/buckets@beta.rb
+++ Casks/b/buckets@beta.rb
@@ -1,9 +1,9 @@
 cask "buckets@beta" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.75.0"
-  sha256 arm:   "90b14145220c12c26f9590319365f6e0ca83c008712967afe12dedf7a4ba43cb",
-         intel: "55300ce9121527de68ca90933f6e91f129c5a689de70bb464c973801b0e58fda"
+  version "0.76.0"
+  sha256 arm:   "e86da65f59b8c1d167723b44e3b90a0e650f388c8dbc28c1812ff57e6526f692",
+         intel: "49ec517f2b3353a97cd4d71d80692d2c7282d40778de9cb466998f116379b28f"
 
   url "https://github.com/buckets/desktop-beta/releases/download/v#{version}/Buckets-Beta-#{arch}-#{version}.dmg",
       verified: "github.com/buckets/desktop-beta/"
