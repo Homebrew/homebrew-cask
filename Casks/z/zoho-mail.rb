5b3a8b3195b4dad4ea898b720985ff7254ea55bc
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-07-05 11:37:54 -0400

    zoho-mail 1.6.3

--- Casks/z/zoho-mail.rb
+++ Casks/z/zoho-mail.rb
@@ -1,9 +1,9 @@
 cask "zoho-mail" do
   arch arm: "arm64-"
 
-  version "1.6.2"
-  sha256 arm:   "012bc0159073ce340a3f56c6a69beaf79bb29ce21ab32896717b48a794f83d28",
-         intel: "cc529d14d4294c4c1ee87a356997e99e7ab7eeb621abc203b9f1813dea02f811"
+  version "1.6.3"
+  sha256 arm:   "b79c5f61ecb149dbd539f9446e85a37bb1e549693365c76789490b2054ddc9fc",
+         intel: "1986c21ec7816bd8f790b13086018aa9cc2a8ee3b58fa114b49338731bed95d2"
 
   url "https://downloads.zohocdn.com/zmail-desktop/mac/zoho-mail-desktop-lite-installer-#{arch}v#{version}.dmg",
       verified: "downloads.zohocdn.com/zmail-desktop/mac/"
