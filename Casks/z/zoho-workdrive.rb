7466174bd4853f5a79f0aec30a690d94759bd20d
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-06 17:31:05 +0200

    zoho-workdrive: update livecheck

--- Casks/z/zoho-workdrive.rb
+++ Casks/z/zoho-workdrive.rb
@@ -1,6 +1,6 @@
 cask "zoho-workdrive" do
   version "2.7.39"
-  sha256 :no_check
+  sha256 "59eb1d050fd5dea1e05c6cb422c1f8ab2fccc3ce68bd404362425759584f704e"
 
   url "https://files-accl.zohopublic.com/public/wdbin/download/46f971e4fc4a32b68ad5d7dade38a7d2",
       verified: "files-accl.zohopublic.com/"
@@ -9,8 +9,17 @@ cask "zoho-workdrive" do
   homepage "https://www.zoho.com/workdrive/desktop-sync.html"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url "https://teamsync.zoho.com/wd/checksum"
+    strategy :json do |json|
+      checksum_match = json["mac"]
+      next if checksum_match.blank?
+
+      cask = CaskLoader.load(__FILE__)
+      next cask.version if cask.sha256 == checksum_match
+
+      url = cask.url.to_s
+      Homebrew::Livecheck::Strategy::ExtractPlist.find_versions(cask:, url:)[:matches].values
+    end
   end
 
   depends_on macos: ">= :mojave"
