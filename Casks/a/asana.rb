3600ba9743c156ebb2418cdba6c27baa56688163
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-07 01:22:24 +0200

    asana: update livecheck
    asana: various updates

--- Casks/a/asana.rb
+++ Casks/a/asana.rb
@@ -1,19 +1,24 @@
 cask "asana" do
-  version "2.1.4"
-  sha256 :no_check
+  arch arm: "arm64", intel: "x64"
 
-  url "https://desktop-downloads.asana.com/darwin_universal/prod/latest/Asana.dmg"
+  version "2.1.4"
+  sha256 arm:   "82516089460c84c02e5b9466ef8539e26338166ad7b4827f67537c942ab88e94",
+         intel: "c3c4770f3bb575e0a5bcc19e84b1f5773fc529452c6d7ebf47f6d970d73c68c3"
+
+  url "https://desktop-downloads.asana.com/darwin_#{arch}/prod/v#{version}/Asana-darwin-#{arch}-#{version}.zip"
   name "Asana"
   desc "Manage team projects and tasks"
   homepage "https://asana.com/"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url "https://desktop-downloads.asana.com/darwin_#{arch}/prod/RELEASES.json"
+    strategy :json do |json|
+      json["currentRelease"]
+    end
   end
 
   auto_updates true
-  depends_on macos: ">= :el_capitan"
+  depends_on macos: ">= :catalina"
 
   app "Asana.app"
 
