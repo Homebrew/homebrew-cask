dedb937d46aa3857ac772331aa333576749eb77b
Kofi Gumbs <h.kofigumbs@gmail.com>
2024-07-06 21:55:20 -0400

    multi 3.0.1

--- Casks/m/multi.rb
+++ Casks/m/multi.rb
@@ -1,6 +1,6 @@
 cask "multi" do
-  version "3.0.0"
-  sha256 "f3f118ea3616638166095d28502a84fde3449d897d064e6c0a801f65c0f3f536"
+  version "3.0.1"
+  sha256 "6df8a9f8db0b69733d91428eba815928bb3b65ec526a7c714de9d8e95a03e2f8"
 
   url "https://github.com/hkgumbs/multi/releases/download/v#{version}/Multi.#{version}.dmg"
   name "Multi"
