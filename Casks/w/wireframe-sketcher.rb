9d1479bba51caf7c1a47ee8b57a3474aebbee5c1
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-07 16:47:15 +0200

    wireframe-sketcher: update livecheck, use versioned url

--- Casks/w/wireframe-sketcher.rb
+++ Casks/w/wireframe-sketcher.rb
@@ -1,15 +1,18 @@
 cask "wireframe-sketcher" do
-  version "7.1.1"
-  sha256 :no_check
+  arch arm: "arm64", intel: "x86_64"
 
-  url "https://wireframesketcher.com/downloads/WireframeSketcher-latest-macosx.zip"
+  version "7.1.1"
+  sha256 arm:   "9bf23a643558a5a0b2ed1e9cd265ab7227cd31b2ae1ffb7e5c26c5806b2995ec",
+         intel: "a951781e8cf1de233296a663d59151cfad7ffae1b333ea9e9891c4474039a24f"
+
+  url "https://wireframesketcher.com/downloads/studio/WireframeSketcher-#{version}-macosx.#{arch}.zip"
   name "WireframeSketcher"
   desc "Tool for creating wireframes, mockups and prototypes"
   homepage "https://wireframesketcher.com/"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url "https://wireframesketcher.com/updates/"
+    regex(/version.*?(\d+(?:\.\d+)+)/i)
   end
 
   app "WireframeSketcher.app"
