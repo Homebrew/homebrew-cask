bb177b7b7bfd325fd56ee9a504b5809c31a30cfa
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-07 10:08:32 +0200

    pokemon-trading-card-game-online: disable

--- Casks/p/pokemon-trading-card-game-online.rb
+++ Casks/p/pokemon-trading-card-game-online.rb
@@ -8,10 +8,7 @@ cask "pokemon-trading-card-game-online" do
   desc "Play the Pokemon TCG online"
   homepage "https://www.pokemon.com/us/pokemon-tcg/play-online/"
 
-  livecheck do
-    url :url
-    strategy :extract_plist
-  end
+  disable! date: "2024-07-07", because: :no_longer_available
 
   app "Pokemon Trading Card Game Online.app"
 
@@ -20,4 +17,8 @@ cask "pokemon-trading-card-game-online" do
     "~/Library/Caches/com.plausiblelabs.crashreporter.data/unity.The Pokémon Company International.Pokemon Trading Card Game Online",
     "~/Library/Preferences/unity.The Pokémon Company International.Pokemon Trading Card Game Online.plist",
   ]
+
+  caveats do
+    requires_rosetta
+  end
 end
