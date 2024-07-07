6b02c7746990dc35e77992816e9b49bac844159f
lucky <lucky@lucky-book.local>
2024-07-07 20:00:01 +0400

    versioned url, livecheck to electron builder

--- Casks/o/outline-manager.rb
+++ Casks/o/outline-manager.rb
@@ -1,17 +1,16 @@
 cask "outline-manager" do
   version "1.15.2"
-  sha256 :no_check
+  sha256 "9c5ccba90834056bcf18ee0ca84d3e036483f503576966da020450bc5bdd1a1d"
 
-  url "https://s3.amazonaws.com/outline-releases/manager/macos/stable/Outline-Manager.dmg",
+  url "https://s3.amazonaws.com/outline-releases/manager/macos/#{version}/1/Outline-Manager.dmg",
       verified: "s3.amazonaws.com/outline-releases/manager/macos/"
   name "Outline Manager"
   desc "Tool to create and manage Outline servers, powered by Shadowsocks"
   homepage "https://www.getoutline.org/"
 
   livecheck do
-    url "https://github.com/Jigsaw-Code/outline-apps/"
-    regex(%r{^(?:manager(?:[._-]macos)?[/._-])?v?(\d+(?:\.\d+)+)$}i)
-    strategy :github_tags
+    url "https://s3.amazonaws.com/outline-releases/manager/latest-mac.yml"
+    strategy :electron_builder
   end
 
   app "Outline Manager.app"
