6a403f9524bc913436e658b54eb202fa23a126ad
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-07 22:15:28 +0200

    spark: update livecheck

--- Casks/s/spark.rb
+++ Casks/s/spark.rb
@@ -1,14 +1,15 @@
 cask "spark" do
-  version "3.3.2,501"
+  version "3.3.2"
   sha256 :no_check
 
   url "https://www.shadowlab.org/softwares/Spark/Spark.zip"
   name "Spark"
+  desc "Shortcut manager"
   homepage "https://www.shadowlab.org/softwares/spark.php"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url :homepage
+    regex(/Version\s*v?(\d+(?:\.\d+)+)/i)
   end
 
   app "Spark.app"
