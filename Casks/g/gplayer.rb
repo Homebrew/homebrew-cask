5252ab51bd86d4cfbf5c0573d8cedca5eac99a96
Chi Zhang <chizhang9135@icloud.com>
2024-07-09 00:45:04 +0800

    Update gplayer.rb

--- Casks/g/gplayer.rb
+++ Casks/g/gplayer.rb
@@ -4,7 +4,17 @@ cask 'gplayer' do
 
   url 'https://ztgg-crm-us.oss-us-west-1.aliyuncs.com/software/g-player/prod/GPlayer.dmg'
   name 'GPlayer'
+  desc 'GPlayer is a media player application.'
   homepage 'https://ztgg-crm-us.oss-us-west-1.aliyuncs.com/software/g-player/prod/GPlayer.dmg'
 
+  depends_on macos: '>= :high_sierra'
+  depends_on arch: :x86_64
+
   app 'GPlayer.app'
+
+  caveats do
+    "GPlayer requires Rosetta 2 to run on Apple Silicon. " \
+    "You can install Rosetta 2 by running: " \
+    "`/usr/sbin/softwareupdate --install-rosetta`"
+  end
 end
