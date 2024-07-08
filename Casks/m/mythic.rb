3f2bb5b088b6eec507c88a0281579cc427d94a59
mattmaniak <72657016+mattmaniak@users.noreply.github.com>
2024-07-08 21:13:39 +0200

    mythic 0.4.2
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>

---
+++ Casks/m/mythic.rb
@@ -0,0 +1,27 @@
+cask "mythic" do
+  version "0.4.2"
+  sha256 "13f10cedb4d410ec6e9b939df6ab96726c6e4aa9bde777cc1bfa862bb080b775"
+
+  url "https://dl.getmythic.app/sparkle-temp/Mythic-#{version}.zip"
+  name "Mythic"
+  desc "Unique open-source game launcher with the ability to run Windows games"
+  homepage "https://getmythic.app/"
+
+  livecheck do
+    url "https://dl.getmythic.app/sparkle-temp/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  depends_on macos: ">= :sonoma"
+  depends_on arch: :arm64
+
+  app "Mythic.app"
+
+  zap trash: [
+    "~/Library/Application Support/Mythic",
+    "~/Library/Caches/xyz.blackxfiied.Mythic.plist",
+    "~/Library/HTTPStorages/xyz.blackxfiied.Mythic",
+    "~/Library/HTTPStorages/xyz.blackxfiied.Mythic.binarycookies",
+    "~/Library/Preferences/xyz.blackxfiied.Mythic.plist",
+  ]
+end
