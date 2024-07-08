dd5cd23a4b44d316a6e20cdfa8d0eddab665c338
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-08 20:11:52 +0200

    xnconvert: update livecheck, use versioned url

--- Casks/x/xnconvert.rb
+++ Casks/x/xnconvert.rb
@@ -1,15 +1,15 @@
 cask "xnconvert" do
   version "1.100.1"
-  sha256 :no_check
+  sha256 "605a7dbe9248db32f1f988fbf5fa577943a7d565d2c6e6fd0b66c4f0296adfd7"
 
-  url "https://download.xnview.com/XnConvert-mac.dmg"
+  url "https://download.xnview.com/old_versions/XnConvert/XnConvert-#{version}-mac.dmg"
   name "XnSoft XnConvert"
   desc "Image-converter and resiser tool"
   homepage "https://www.xnview.com/en/xnconvert/"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url "https://download.xnview.com/old_versions/XnConvert/"
+    regex(/href=.*XnConvert[._-]v?(\d+(?:\.\d+)+)[._-]mac\.dmg/i)
   end
 
   app "XnConvert.app"
