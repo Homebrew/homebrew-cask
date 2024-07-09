3e6d7222b688d1c1fe4d33b5b2d37d695a2fe0d5
Bevan Kay <email@bevankay.me>
2024-07-09 20:16:58 +1000

    genesys-cloud 2.37.598

--- Casks/g/genesys-cloud.rb
+++ Casks/g/genesys-cloud.rb
@@ -1,6 +1,6 @@
 cask "genesys-cloud" do
-  version "2.36.591,92"
-  sha256 "e0039718d5d288feb82d940ed30ef7d6c0aad2ea9e74ffba7aae7faa2f1a91b6"
+  version "2.37.598,98"
+  sha256 "73d377abb3672dc2cf9dba35f03e0c8e085ce1f6f28dfa6549cba35b3248ebe1"
 
   url "https://app.mypurecloud.com/directory-mac/build-assets/#{version.csv.first}-#{version.csv.second}/genesys-cloud-mac-#{version.csv.first}.dmg"
   name "Genesys Cloud for macOS"
@@ -27,4 +27,8 @@ cask "genesys-cloud" do
     "~/Library/Preferences/com.inin.purecloud.directory.plist",
     "~/Library/Saved Application State/com.inin.purecloud.directory.savedState",
   ]
+
+  caveats do
+    requires_rosetta
+  end
 end
