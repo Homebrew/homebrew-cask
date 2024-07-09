f66cc8319d493c7e0bd503a1a1e00e434109f24c
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-09 20:43:24 +0200

    mongodb-compass@beta 1.43.5-beta.0

--- Casks/m/mongodb-compass@beta.rb
+++ Casks/m/mongodb-compass@beta.rb
@@ -1,9 +1,9 @@
 cask "mongodb-compass@beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.43.4-beta.1"
-  sha256 arm:   "d03a7d9a385d5dd0dd7c84e79935c682147d9a89d71b178f04ca51b04746987a",
-         intel: "dc2215a705ce8bd3eec885dd6c203511414b220cf3f2fdd4d4ce883cb92b5813"
+  version "1.43.5-beta.0"
+  sha256 arm:   "b18b1c003884924a8e6d1d018bc4d5c1d6c9e3653cf2b9186d52e80ae6d36bb1",
+         intel: "aeb27d05d44e85a5677d3e8aa83b4be9c0a8f7626fde409d4157ad5f4cf57c6b"
 
   url "https://downloads.mongodb.com/compass/beta/mongodb-compass-#{version}-darwin-#{arch}.dmg"
   name "MongoDB Compass"
