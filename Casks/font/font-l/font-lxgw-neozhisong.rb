742b0d5a91f84f9c0956c80137461344ae3c6024
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-09 19:29:49 +0200

    font-lxgw-neozhisong 0.924.1

--- Casks/font/font-l/font-lxgw-neozhisong.rb
+++ Casks/font/font-l/font-lxgw-neozhisong.rb
@@ -1,6 +1,6 @@
 cask "font-lxgw-neozhisong" do
-  version "0.924"
-  sha256 "3416e329e60c12f576035ac5ac1ab3762a7eb67c214010e66e74b5b5ee9322a1"
+  version "0.924.1"
+  sha256 "2f294f881bb10f6714063dc5c6a9fc8b7aa3ccf48299db843b38a4ac691a919c"
 
   url "https://github.com/lxgw/LxgwNeoZhiSong/releases/download/v#{version}/LXGWNeoZhiSong.ttf"
   name "LXGW Neo ZhiSong"
