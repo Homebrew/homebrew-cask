ac7bb0a11cbc9763e3577019d951f5142c87b6e4
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-09 19:16:36 +0200

    embyserver: add arm arch

--- Casks/e/embyserver.rb
+++ Casks/e/embyserver.rb
@@ -1,8 +1,11 @@
 cask "embyserver" do
-  version "4.8.8.0"
-  sha256 "15d6c4a0797c805cf9b8edffc9c062abb33c01710faa2749755052c15fcc6a39"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://github.com/MediaBrowser/Emby.Releases/releases/download/#{version}/embyserver-osx-x64-#{version}.zip",
+  version "4.8.8.0"
+  sha256 arm:   "0f4cb2a08d7f8c9010ce9f4cba335d6cff1a6efe0cf4a03dc0d19c53fa8f93d3",
+         intel: "15d6c4a0797c805cf9b8edffc9c062abb33c01710faa2749755052c15fcc6a39"
+
+  url "https://github.com/MediaBrowser/Emby.Releases/releases/download/#{version}/embyserver-osx-#{arch}-#{version}.zip",
       verified: "github.com/MediaBrowser/Emby.Releases/"
   name "Emby Server"
   desc "Personal media server with apps on just about every device"
@@ -18,8 +21,4 @@ cask "embyserver" do
   app "EmbyServer.app"
 
   zap trash: "~/.config/emby-server"
-
-  caveats do
-    requires_rosetta
-  end
 end
