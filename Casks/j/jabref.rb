fe75fb8d78b3d5add543441b55f2746e4c1c0795
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-07-08 20:05:57 -0400

    jabref 5.14

--- Casks/j/jabref.rb
+++ Casks/j/jabref.rb
@@ -1,9 +1,9 @@
 cask "jabref" do
   arch arm: "-arm64"
 
-  version "5.13"
-  sha256 arm:   "4487b119781fcdc9559878280f54c8684062be746abbb9b95b54473a3fea3b1a",
-         intel: "65f5fab28437af86b6105a43324c73e4530affca7f21b0487667aa161c7e126d"
+  version "5.14"
+  sha256 arm:   "b13a33e95f20b4a6220df748243b6010a7191236bd50aa443becf7bc406e9f76",
+         intel: "a0dd346cf774f3c5d20332d11532661ae7d99d58ed51c9871a419cafb74f7d89"
 
   url "https://github.com/JabRef/jabref/releases/download/v#{version}/JabRef-#{version}#{arch}.dmg",
       verified: "github.com/JabRef/jabref/"
