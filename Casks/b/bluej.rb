ed311fa4de4029a09efa565119a3c75e1ed70c9a
Patrick Linnane <patrick@linnane.io>
2024-07-10 13:16:11 -0700

    bluej: update livecheck
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/b/bluej.rb
+++ Casks/b/bluej.rb
@@ -8,15 +8,7 @@ cask "bluej" do
   homepage "https://www.bluej.org/"
 
   livecheck do
-    url "https://www.bluej.org"
-    regex(%r{href=.*?/BlueJ-mac-(\d+)(\d+)(\d+)(a)?\.dmg}i)
-    strategy :page_match do |page|
-      match = page.match(regex)
-      next if match.blank?
-
-      "#{match[1]}.#{match[2]}.#{match[3]}" unless match[4]
-      "#{match[1]}.#{match[2]}.#{match[3]}#{match[4]}"
-    end
+    url "https://github.com/k-pet-group/BlueJ-Greenfoot"
   end
 
   depends_on macos: ">= :sierra"
