9161b9c78b206e285795a447f76b4be103894012
Sam Ford <1584702+samford@users.noreply.github.com>
2024-07-10 12:39:26 -0400

    ricoh-theta: use versioned url, update livecheck
    The app checks an electron-builder YAML file, which references the
    versioned .app.zip file.

--- Casks/r/ricoh-theta.rb
+++ Casks/r/ricoh-theta.rb
@@ -1,16 +1,16 @@
 cask "ricoh-theta" do
   version "3.20.0"
-  sha256 :no_check
+  sha256 "78c215e3f44fe4c9422ba018ed21dae5323f3d6907c7c275d349436f51109696"
 
-  url "https://theta360-statics.s3.amazonaws.com/app/viewer/RICOH%20THETA.dmg",
+  url "https://theta360-statics.s3.amazonaws.com/app/viewer/SphericalViewerUpdate/RICOH%20THETA.#{version}.app.zip",
       verified: "theta360-statics.s3.amazonaws.com/app/viewer/"
   name "RICOH THETA"
   desc "Companion software for 360 degree cameras"
   homepage "https://theta360.com/en/support/download/pcmac/"
 
   livecheck do
-    url :homepage
-    regex(/>\s*Version\s*v?(\d+(?:\.\d+)+)[^<]*</i)
+    url "https://theta360-statics.s3.amazonaws.com/app/viewer/SphericalViewerUpdate/latest-mac.yml"
+    strategy :electron_builder
   end
 
   app "RICOH THETA.app"
