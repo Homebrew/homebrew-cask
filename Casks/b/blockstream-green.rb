4d0ff9fb1af6f4b924d061619cdedcd0e0226aa4
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-11 00:09:09 +0200

    blockstream-green 2.0.8

--- Casks/b/blockstream-green.rb
+++ Casks/b/blockstream-green.rb
@@ -1,9 +1,9 @@
 cask "blockstream-green" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "2.0.7"
-  sha256 arm:   "42730aadef276499385b19e79cfcb5c3c8f7258a46171a557dc695d20c060352",
-         intel: "53e6a54355152a91e54ea4fb3ba458e7376206674176baed57a2d99e19a7f69d"
+  version "2.0.8"
+  sha256 arm:   "f04d278a91332834bee4d9c021b24b589961a706d9e6cc4f529bbf1a34c1617a",
+         intel: "37598de068d5849a887b6d3cf0402f0df5f44850190bda1b9853ae294e86c740"
 
   url "https://github.com/Blockstream/green_qt/releases/download/release_#{version}/BlockstreamGreen-#{arch}.dmg",
       verified: "github.com/Blockstream/green_qt/"
