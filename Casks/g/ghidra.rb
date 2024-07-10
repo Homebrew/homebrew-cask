ae1e3c29ec1b80eaf162c516df7ef8166f8fd94d
Marco Grassi <marco.gra@gmail.com>
2024-07-10 11:44:01 +0800

    ghidra 11.1.2

--- Casks/g/ghidra.rb
+++ Casks/g/ghidra.rb
@@ -1,6 +1,6 @@
 cask "ghidra" do
-  version "11.1.1,20240614"
-  sha256 "7fe8d9a6e7e5267f3cf487a0c046b21fb08d7a602facaa2e81ac2f09b5df2866"
+  version "11.1.2,20240709"
+  sha256 "219ec130b901645779948feeb7cc86f131dd2da6c36284cf538c3a7f3d44b588"
 
   url "https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_#{version.csv.first}_build/ghidra_#{version.csv.first}_PUBLIC_#{version.csv.second}.zip",
       verified: "github.com/NationalSecurityAgency/ghidra/"
