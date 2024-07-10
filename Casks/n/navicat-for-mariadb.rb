e9c5e83bc591c7a0ce9ddcfb1c4ffa5b7325c6df
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-10 03:43:40 +0000

    navicat-for-mariadb 17.0.13

--- Casks/n/navicat-for-mariadb.rb
+++ Casks/n/navicat-for-mariadb.rb
@@ -1,5 +1,5 @@
 cask "navicat-for-mariadb" do
-  version "17.0.12"
+  version "17.0.13"
   sha256 :no_check
 
   url "https://dn.navicat.com/download/navicat#{version.major_minor.no_dots}_mariadb_en.dmg"
