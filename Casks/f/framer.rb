bd130bc3dbdae27d96e5daa6b3822a7b1f5624c6
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-10 21:31:33 +0200

    framer: use versioned url

--- Casks/f/framer.rb
+++ Casks/f/framer.rb
@@ -2,9 +2,10 @@ cask "framer" do
   arch arm: "arm64", intel: "x64"
 
   version "2024.24.1"
-  sha256 :no_check
+  sha256 arm:   "b336363cc62845d408fa1e5d69c1448dd9a26f9425bbed58445245cc99aac04e",
+         intel: "ecbe6448fa6564a2b897cf1a4535be07221cce883b12827f61bff87e9be1d024"
 
-  url "https://updates.framer.com/electron/darwin/#{arch}/Framer.zip"
+  url "https://updates.framer.com/electron/darwin/#{arch}/Framer-#{version}.zip"
   name "Framer"
   desc "Tool that helps teams design every part of the product experience"
   homepage "https://www.framer.com/"
@@ -23,6 +24,8 @@ cask "framer" do
     "~/Library/Application Support/Framer",
     "~/Library/Caches/com.framer.electron",
     "~/Library/Caches/com.framer.electron.ShipIt",
+    "~/Library/HTTPStorages/com.framer.electron",
+    "~/Library/Preferences/ByHost/com.framer.electron.ShipIt.*.plist",
     "~/Library/Preferences/com.framer.electron.plist",
     "~/Library/Saved Application State/com.framer.electron.savedState",
   ]
