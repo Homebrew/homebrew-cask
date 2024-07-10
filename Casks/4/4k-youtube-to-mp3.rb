8bba8aa87ac2652bf44e770d0148d5dedd24b42d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-10 09:32:08 +0000

    4k-youtube-to-mp3 5.4.3

--- Casks/4/4k-youtube-to-mp3.rb
+++ Casks/4/4k-youtube-to-mp3.rb
@@ -2,9 +2,9 @@ cask "4k-youtube-to-mp3" do
   # NOTE: "3" is not a version number, but an intrinsic part of the product name
   arch arm: "arm64", intel: "x64"
 
-  version "5.4.2"
-  sha256 arm:   "b61420cdee33eddcb27a13e1fe7735811d1542580611d809c4ea276ba7242b8f",
-         intel: "147475cf6a1c1349a3724ff08a775c7e70dd484bdd9fa57e5083e9baa513892d"
+  version "5.4.3"
+  sha256 arm:   "c8d0f0d14a3edaee28b7d820b237d6381f713f73c3a0f77978428a350b59313d",
+         intel: "b3e14860d0c4a327921e587fc4303266c812f77546819c9f9e132c8161a64c69"
 
   url "https://dl.4kdownload.com/app/4kyoutubetomp3_#{version}_#{arch}.dmg"
   name "4K YouTube to MP3"
