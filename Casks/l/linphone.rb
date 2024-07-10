baad12e003ad7a3cf21610a025669bfbda8f37ca
adithyasunil26 <adithyasunil26@gmail.com>
2024-07-11 01:33:18 +0530

    linphone 5.2.5

--- Casks/l/linphone.rb
+++ Casks/l/linphone.rb
@@ -1,6 +1,6 @@
 cask "linphone" do
-  version "5.2.4"
-  sha256 "4e0938244a87742db8bd71a50ecbe0b2bc6291adc0fee631c30bb38b032cae2b"
+  version "5.2.5"
+  sha256 "148c8de586bcf06f6e482f71cfd841e584f79802cbdb72e19da19b86862891c9"
 
   url "https://download.linphone.org/releases/macosx/app/Linphone-#{version}-mac.dmg"
   name "Linphone"
@@ -22,4 +22,8 @@ cask "linphone" do
     "~/Library/Preferences/linphone",
     "~/Library/Saved Application State/com.belledonnecommunications.linphone.savedState",
   ]
+
+  caveats do
+    requires_rosetta
+  end
 end
