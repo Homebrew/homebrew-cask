dac95c175658610d637255a040f6dbd7832cb0eb
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-07-11 11:08:22 -0400

    graphql-playground: deprecate
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/g/graphql-playground.rb
+++ Casks/g/graphql-playground.rb
@@ -7,10 +7,7 @@ cask "graphql-playground" do
   desc "GraphQL IDE for better development workflows"
   homepage "https://github.com/prisma/graphql-playground"
 
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
+  deprecate! date: "2024-07-11", because: :unmaintained
 
   app "GraphQL Playground.app"
 
@@ -22,4 +19,8 @@ cask "graphql-playground" do
     "~/Library/Preferences/cool.graph.playground.plist",
     "~/Library/Saved Application State/cool.graph.playground.savedState",
   ]
+
+  caveats do
+    requires_rosetta
+  end
 end
