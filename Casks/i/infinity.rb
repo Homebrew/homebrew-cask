f288e95b84ff5660506528c6f8029cc4be89538b
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-11 22:38:58 +0200

    infinity: update livecheck, use versioned url

--- Casks/i/infinity.rb
+++ Casks/i/infinity.rb
@@ -1,16 +1,18 @@
 cask "infinity" do
-  version "1.0.0"
-  sha256 :no_check
+  version "1.2.2"
+  sha256 "49f43d5a3dbbeb0f51b627c9d654bafcbd928b30f794721f3643559b4c2c9a41"
 
-  url "https://startinfinity.com/downloads/Infinity_macOS.dmg"
+  url "https://downloads.startinfinity.com/desktop/production/#{version}/infinity_#{version}_darwin_universal.dmg"
   name "Infinity"
   desc "Customizable work management platform"
   homepage "https://startinfinity.com/"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url "https://startinfinity.com/desktop-apps/download?url=https%3A%2F%2Fs3.amazonaws.com%2Fdownloads.startinfinity.com%2Fdesktop%2Fproduction%2Finfinity_darwin_universal.dmg"
+    strategy :header_match
   end
+
+  depends_on macos: ">= :high_sierra"
 
   app "Infinity.app"
 
