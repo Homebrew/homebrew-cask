2b351ebb94eeed97a31479a14fe9567dd91af232
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-11 23:09:35 +0200

    jaikoz: update livecheck

--- Casks/j/jaikoz.rb
+++ Casks/j/jaikoz.rb
@@ -1,7 +1,7 @@
 cask "jaikoz" do
   arch arm: "-m1"
 
-  version "11.8,1.0"
+  version "11.8"
   sha256 :no_check
 
   url "https://www.jthink.net/jaikoz/jsp/manualdownload/jaikoz-osx#{arch}.dmg"
@@ -10,8 +10,8 @@ cask "jaikoz" do
   homepage "https://www.jthink.net/jaikoz/"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url "https://www.jthink.net/jaikoz/jsp/download/start.jsp"
+    regex(/Jaikoz\s+v?(\d+(?:\.\d+)+)/i)
   end
 
   depends_on macos: ">= :sierra"
