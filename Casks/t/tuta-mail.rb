da9170d4dd87ed290f3be95850c6dc43c76ba901
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-11 13:02:51 +0000

    tuta-mail 235.240709.0

--- Casks/t/tuta-mail.rb
+++ Casks/t/tuta-mail.rb
@@ -1,6 +1,6 @@
 cask "tuta-mail" do
-  version "234.240627.0"
-  sha256 "6e4ebd04475cafbc867683cba917f8660b907acd9bb3969dfc84e54d56c14f03"
+  version "235.240709.0"
+  sha256 "93cc3e8b14dd859ec71f0adb58705a15bb30c4a38616ea5d9e56bdd919ee7017"
 
   url "https://github.com/tutao/tutanota/releases/download/tutanota-desktop-release-#{version}/tutanota-desktop-mac.dmg",
       verified: "github.com/tutao/tutanota/"
