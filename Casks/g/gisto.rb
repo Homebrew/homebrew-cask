3025da510a9d867a9e3cfa51980593735259b655
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-07-11 11:21:38 -0400

    gisto: deprecate
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/g/gisto.rb
+++ Casks/g/gisto.rb
@@ -8,6 +8,8 @@ cask "gisto" do
   desc "Snippets management desktop application with (team) sharing options"
   homepage "https://www.gistoapp.com/"
 
+  deprecate! date: "2024-07-11", because: :unmaintained
+
   app "Gisto.app"
 
   zap trash: [
@@ -16,4 +18,8 @@ cask "gisto" do
     "~/Library/Preferences/com.gistoapp.gisto2.plist",
     "~/Library/Saved Application State/com.gistoapp.gisto2.savedState",
   ]
+
+  caveats do
+    requires_rosetta
+  end
 end
