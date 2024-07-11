cbe79c9740e1bd1f37ca19b284fd62fe1dae1481
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-11 09:44:10 +0000

    geogebra 6.0.848.0

--- Casks/g/geogebra.rb
+++ Casks/g/geogebra.rb
@@ -1,6 +1,6 @@
 cask "geogebra" do
-  version "6.0.846.0"
-  sha256 "c213820f37b99fbd0a4686c3781b3a550d7a74cda780c96d92823ba681b07b3c"
+  version "6.0.848.0"
+  sha256 "d70ded7aa7ed7c69a78a43844925f6f8c8fd3ce500b8b0606abe2b8277d5f7f8"
 
   url "https://download.geogebra.org/installers/#{version.major_minor}/GeoGebra-Classic-#{version.major}-MacOS-Portable-#{version.dots_to_hyphens}.zip"
   name "GeoGebra"
