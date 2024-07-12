fcc1adb788be07f3ff233b44c70cf308c303093f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-12 13:02:04 +0000

    yealink-meeting 4.6.26

--- Casks/y/yealink-meeting.rb
+++ Casks/y/yealink-meeting.rb
@@ -1,6 +1,6 @@
 cask "yealink-meeting" do
-  version "4.6.23,0851df859740efdab24f3066cf3dfc51"
-  sha256 "8cb9ea423dd6dac93d5fb5fdb1609725aadfd0010ec25703c39873125f6f916f"
+  version "4.6.26,a8e8023773218a38dbbc96a75392e767"
+  sha256 "6f61cec436108723cace15e59c302b730cb7dd1ca9b98556246c8a9463a2dd1f"
 
   url "https://download.ylyun.com/yealink-meeting/macos/latest/Yealink-Meeting.dmg?v=#{version.csv.second}"
   name "Yealink Meeting"
