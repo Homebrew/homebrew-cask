d82de0b455ba025a5a5e66580e69275def018234
Bevan Kay <email@bevankay.me>
2024-07-14 23:56:10 +1000

    desktoppr 0.5

--- Casks/d/desktoppr.rb
+++ Casks/d/desktoppr.rb
@@ -1,18 +1,30 @@
 cask "desktoppr" do
-  version "0.4"
-  sha256 "d4da2ee76007c89ba416930c674b30c77adefba7627b4fc643d3856c93d619ee"
+  version "0.5,218"
+  sha256 "1cfb67d7023bc6bc7b1e3c8ccf5c891a1bad22876df77f3fbc77d27877cc7b9d"
 
-  url "https://github.com/scriptingosx/desktoppr/releases/download/v#{version}/desktoppr-#{version}.pkg"
+  url "https://github.com/scriptingosx/desktoppr/releases/download/v#{version.csv.first}/desktoppr-#{version.tr(",", "-")}.pkg"
   name "desktoppr"
   desc "Command-line tool to set the desktop picture"
   homepage "https://github.com/scriptingosx/desktoppr"
 
   livecheck do
     url :url
-    strategy :github_latest
+    regex(/desktoppr[._-]v?(\d+(?:\.\d+)+)(?:[._-](\d+))?\.pkg/i)
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        next if release["draft"] || release["prerelease"]
+
+        release["assets"]&.map do |asset|
+          match = asset["name"]&.match(regex)
+          next if match.blank?
+
+          match[2].present? ? "#{match[1]},#{match[2]}" : match[1]
+        end
+      end.flatten
+    end
   end
 
-  pkg "desktoppr-#{version}.pkg"
+  pkg "desktoppr-#{version.tr(",", "-")}.pkg"
 
   uninstall pkgutil: "com.scriptingosx.desktoppr"
 end
