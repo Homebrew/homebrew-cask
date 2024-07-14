fed55bef2e8aedf9a1fc1fafbe27de9d3ba8c4a3
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-14 22:21:31 +0200

    nifty: add depends_on, update zap stanza

--- Casks/n/nifty.rb
+++ Casks/n/nifty.rb
@@ -13,11 +13,15 @@ cask "nifty" do
     strategy :extract_plist
   end
 
+  depends_on macos: ">= :catalina"
+
   app "Nifty.app"
 
   zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.creative360.niftypm.sfl*",
     "~/Library/Application Support/Nifty",
     "~/Library/Logs/Nifty",
     "~/Library/Preferences/com.creative360.niftypm.plist",
+    "~/Library/Saved Application State/com.creative360.niftypm.savedState",
   ]
 end
