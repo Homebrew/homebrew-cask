745ea3d2f244fd35384063f98e6fe44704dc7e59
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-15 00:47:22 +0000

    gcs 5.24.0

--- Casks/g/gcs.rb
+++ Casks/g/gcs.rb
@@ -1,9 +1,9 @@
 cask "gcs" do
   arch arm: "apple", intel: "intel"
 
-  version "5.23.0"
-  sha256 arm:   "781898945c724d9cf2c18b60b325a43b3ec2594f798d49b5a1ed59ca709d6f7f",
-         intel: "a16396ac570fdea74bdc9c54d65bf9b11ba02d81b9d337487339feb3168f7aef"
+  version "5.24.0"
+  sha256 arm:   "d250a69a611a60acb6666ac7df5a393234a34315113db0bea31f9d39fdb446bb",
+         intel: "9067cb58b8be3ef59ed4f46309a2aeb1dc857128db9cbbe0b8623f5575c42402"
 
   url "https://github.com/richardwilkes/gcs/releases/download/v#{version}/gcs-#{version}-macos-#{arch}.dmg",
       verified: "github.com/richardwilkes/gcs/"
