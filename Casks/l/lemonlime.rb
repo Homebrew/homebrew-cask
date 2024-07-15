b7b7fd421add8546c78f151827143c9aec75c84e
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-15 09:37:43 +0200

    lemonlime 0.3.5

--- Casks/l/lemonlime.rb
+++ Casks/l/lemonlime.rb
@@ -1,8 +1,11 @@
 cask "lemonlime" do
-  version "0.3.4.4"
-  sha256 "f5c129a4e0e9e22966c9afcba4ead477f58255a65149097b4305599cc54ec8e0"
+  arch arm: "arm64", intel: "x86_64"
 
-  url "https://github.com/Project-LemonLime/Project_LemonLime/releases/download/#{version}/lemon-Release.dmg"
+  version "0.3.5"
+  sha256 arm:   "ba44fd655b53a2827034e2ddefb60648adbe0cfe390a8163552140e74a2656b5",
+         intel: "500103bc88198b7ad875069d8030d30c5c677cd773196c237f2dc068cc6db038"
+
+  url "https://github.com/Project-LemonLime/Project_LemonLime/releases/download/#{version}/lemon-Qt6.7.2-Release-#{arch}.dmg"
   name "lemonlime"
   desc "Tiny judging environment for OI contest based on Lemon + LemonPlus"
   homepage "https://github.com/Project-LemonLime/Project_LemonLime"
