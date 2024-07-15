070fe545957bea48b7225434681d79a321604f4d
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-15 13:14:01 +0200

    tyme: update livecheck, add depends_on

--- Casks/t/tyme.rb
+++ Casks/t/tyme.rb
@@ -1,5 +1,5 @@
 cask "tyme" do
-  version "2024.13,5055"
+  version "2024.13"
   sha256 :no_check
 
   url "https://www.tyme-app.com/app/Tyme3.zip"
@@ -8,19 +8,24 @@ cask "tyme" do
   homepage "https://www.tyme-app.com/"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url "https://api.tyme-app.com/v1/updates?unique_id=&version=&version=#{version}"
+    strategy :sparkle, &:short_version
   end
+
+  depends_on macos: ">= :catalina"
 
   app "Tyme.app"
 
   zap trash: [
     "~/Library/Application Scripts/com.tyme-app.Tyme3-macOS",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.tyme-app.tyme3-menubarhelper.sfl*",
     "~/Library/Application Support/com.tyme-app.Tyme3-macOS",
     "~/Library/Caches/CloudKit/com.tyme-app.Tyme3-macOS",
     "~/Library/Caches/com.tyme-app.Tyme3-macOS",
     "~/Library/Containers/com.tyme-app.Tyme3-macOS",
+    "~/Library/Containers/com.tyme-app.Tyme3-MenubarHelper",
     "~/Library/Group Containers/*.group.com.tyme-app.Tyme3",
+    "~/Library/HTTPStorages/com.tyme-app.Tyme3-macOS",
     "~/Library/Preferences/3https:/api.tyme-app.com",
     "~/Library/Preferences/com.tyme-app.Tyme3-macOS.plist",
     "~/Library/Saved Application State/com.tyme-app.Tyme3-macOS.savedState",
