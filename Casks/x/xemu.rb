0bc513145a0f164730d24148c56cb6c7362f38d1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-16 22:06:11 +0000

    xemu 0.7.129

--- Casks/x/xemu.rb
+++ Casks/x/xemu.rb
@@ -1,6 +1,6 @@
 cask "xemu" do
-  version "0.7.128"
-  sha256 "b1d28f89591abfa81932545f66275b3e4bf98786ed2c41364a0ac27911994464"
+  version "0.7.129"
+  sha256 "7d1177c324fd3e9f0dfc313a1a9cb4feb6975aca1986765235c229346b20af22"
 
   url "https://github.com/xemu-project/xemu/releases/download/v#{version}/xemu-macos-universal-release.zip",
       verified: "github.com/xemu-project/xemu/"
