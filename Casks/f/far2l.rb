5b53fca6d74ceb7547175542e30e38a3135d63b1
Yurii Kolesnykov <root@yurikoles.com>
2024-07-16 10:10:38 +0100

    far2l 2.6.2

--- Casks/f/far2l.rb
+++ Casks/f/far2l.rb
@@ -1,6 +1,6 @@
 cask "far2l" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "2.6.1_hotfix1"
+  version "2.6.2"
 
   on_mojave :or_older do
     sha256 "d66f6468538f7f3648d0134e4b4bf39c264e84f84ce73795ca2edfadedb6cf21"
@@ -13,7 +13,7 @@ cask "far2l" do
     url "https://github.com/elfmz/far2l/releases/download/v_#{version}/far2l-#{version}-beta-MacOS-10.15-x64.dmg"
   end
   on_big_sur :or_newer do
-    sha256 "081341178f047e335d714ddffa9797441ccff7ae632137374cf8a059e1e14e01"
+    sha256 "5c9afe9e4dfbed416c1c8cf7d9e71589e5eed45efc46724a8bf638a40abd2083"
 
     url "https://github.com/elfmz/far2l/releases/download/v_#{version}/far2l-#{version}-beta-MacOS-11.2-universal.dmg"
   end
