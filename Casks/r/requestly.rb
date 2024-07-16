5395f3873d2f4eefa9cad44e90fb3b8798a9e603
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-16 19:21:42 +0200

    requestly 1.7.0

--- Casks/r/requestly.rb
+++ Casks/r/requestly.rb
@@ -1,9 +1,9 @@
 cask "requestly" do
   arch arm: "-arm64"
 
-  version "1.6.1"
-  sha256 arm:   "2574911b66301240ba4a424378ab42178ebdc92a1575854f90ce16642a4e2f31",
-         intel: "f1da77e8202d041e0f346a16cc6a91ba94f475006a7289e0af6ed43e078b48da"
+  version "1.7.0"
+  sha256 arm:   "d1168a91c900154f392c347fc3ac0b83417e5f1a1f6422e0910613fd7cf69f5b",
+         intel: "560f283a6bc425ee26be0dfbe538c62187ca6c21a424a8193f756e7fcd4ced98"
 
   url "https://github.com/requestly/requestly-desktop-app/releases/download/v#{version}/Requestly-#{version}#{arch}.dmg",
       verified: "github.com/requestly/requestly-desktop-app/"
