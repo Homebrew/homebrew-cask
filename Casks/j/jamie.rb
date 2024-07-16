49c7bd60fa5b0e5a88fbef72ea0d67c0e36b98ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-16 15:39:10 +0000

    jamie 4.1.6

--- Casks/j/jamie.rb
+++ Casks/j/jamie.rb
@@ -1,9 +1,9 @@
 cask "jamie" do
   arch arm: "-arm64"
 
-  version "4.1.5"
-  sha256 arm:   "4da56d1aaf9f62f8cdceb225b08531075160b5388ab6b14a883aa82fda707b5f",
-         intel: "79573e4b82e63b7db9854708570da0c546176f4eeba47bde82355d03b15a17c3"
+  version "4.1.6"
+  sha256 arm:   "38d680cc178a3b015295c0a0d4e3224723db05d9e7b0cc8d8c5c24e11e2fe333",
+         intel: "40d7d2d864d42a19ff714939535b15fbe60b7099198f0728e72b75ee3528fd9c"
 
   url "https://github.com/louismorgner/jamie-release/releases/download/v#{version}/jamie-#{version}#{arch}.dmg",
       verified: "github.com/louismorgner/jamie-release/"
