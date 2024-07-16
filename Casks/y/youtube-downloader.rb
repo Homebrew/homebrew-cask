09bd990ad9eea02761156ce6c633964abec1c9b9
Christian Kraemer <christian.kraemer@gmail.com>
2024-07-16 06:50:36 +0200

    youtube-downloader 0.13

--- Casks/y/youtube-downloader.rb
+++ Casks/y/youtube-downloader.rb
@@ -1,6 +1,6 @@
 cask "youtube-downloader" do
-  version "0.12"
-  sha256 "85f5ca5df1060ee32a0db6ccb9671fa8082b1e139ebb5793cc03f18c1ac24471"
+  version "0.13"
+  sha256 "a8b7d17d3bf858fe8c6415347d04cb14d7f4a37397c37dd06a848e47b6235984"
 
   url "https://github.com/DenBeke/YouTube-Downloader-for-macOS/releases/download/v#{version}/Youtube.Downloader.zip"
   name "YouTube Downloader"
