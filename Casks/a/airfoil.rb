aedfda3e1261f5630ed39dba091527a0b311c139
Jacob Ford <j@cobford.com>
2024-07-17 02:21:20 -0400

    airfoil: new urls, refactor livecheck

--- Casks/a/airfoil.rb
+++ Casks/a/airfoil.rb
@@ -4,34 +4,31 @@ cask "airfoil" do
   on_ventura :or_older do
     version "5.11.8"
 
-    url "https://rogueamoeba.com/airfoil/mac/download/Airfoil.zip"
+    url "https://rogueamoeba.com/airfoil/mac/download-ace.php"
 
-    # NOTE: The `system` value will need to be kept up to date with the latest
-    # macOS Ventura version (e.g. 1366 for 13.6.6).
-    livecheck do
-      url "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&system=1366&bundleid=com.rogueamoeba.airfoil&platform=osx&version=#{version.no_dots}8000"
-      strategy :sparkle
-    end
+    depends_on macos: ">= :big_sur"
   end
   on_sonoma :or_newer do
     version "5.12.1"
 
-    url "https://rogueamoeba.com/airfoil/mac/download/Airfoil-ARK.zip"
+    url "https://rogueamoeba.com/airfoil/mac/download-ark.php"
 
-    # NOTE: The `system` value will need to be kept up to date with the latest
-    # macOS version (e.g. 1441 for 14.4.1).
-    livecheck do
-      url "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&system=1441&bundleid=com.rogueamoeba.airfoil&platform=osx&version=#{version.no_dots}8000"
-      strategy :sparkle
-    end
+    depends_on macos: ">= :sonoma"
+
+    # NOTE: see https://rogueamoeba.com/airfoil/mac/
+    caveats "Airfoil #{version} requires macOS 14.4 or higher."
   end
 
   name "Airfoil"
   desc "Sends audio from computer to outputs"
   homepage "https://rogueamoeba.com/airfoil/mac/"
 
+  livecheck do
+    url "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&system=#{MacOS.full_version.to_s.delete(".")}&bundleid=com.rogueamoeba.airfoil&platform=osx&version=#{version.no_dots}8000"
+    strategy :sparkle
+  end
+
   auto_updates true
-  depends_on macos: ">= :big_sur"
 
   app "Airfoil/Airfoil Satellite.app"
   app "Airfoil/Airfoil.app"
