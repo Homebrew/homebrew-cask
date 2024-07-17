cfb57ade3c95fb6e4975d18041c6ae8be265efdf
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-07-17 11:54:23 -0400

    cisdem-data-recovery: update min os from :sierra to :high_sierra
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/c/cisdem-data-recovery.rb
+++ Casks/c/cisdem-data-recovery.rb
@@ -12,7 +12,7 @@ cask "cisdem-data-recovery" do
     regex(/(\d+(?:\.\d+)+)\s+\(\d+(?:-\d+)+\)/i)
   end
 
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :high_sierra"
 
   app "Cisdem Data Recovery.app"
 
