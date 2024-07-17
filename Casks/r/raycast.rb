751af91d3613cef15217e28c72d512d69c343164
Anka <runner@Mac-1721220569230.local>
2024-07-17 13:04:22 +0000

    raycast 1.79.0

--- Casks/r/raycast.rb
+++ Casks/r/raycast.rb
@@ -14,9 +14,9 @@ cask "raycast" do
 
     livecheck_arch = on_arch_conditional arm: "arm", intel: "x86"
 
-    version "1.78.1"
-    sha256 arm:   "883b45c4e855b4448a41effee6edddb2202973db33534550dd60ff943be93bb3",
-           intel: "1b1e297dceafaec5e0a78c7162078461522bab878f8b67f5a1b91def77c5c128"
+    version "1.79.0"
+    sha256 arm:   "39651a8e1389dbf27129155b263bcf4a67265bb77a5ce2d09861a3584670dbc6",
+           intel: "c1d2e67a96422df4c31b2bdb26522f72ca84ff5f5bbc0e46e8110d2285333741"
 
     url "https://releases.raycast.com/releases/#{version}/download?build=#{arch}"
 
