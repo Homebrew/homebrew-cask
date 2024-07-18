1e254b8e510a51e6a230217c710379685b580a7d
IamBatman <iambatmanvpn@proton.me>
2024-07-18 11:55:27 +0800

    update: FlashGo 1.4.0+10400

---
+++ Casks/f/flashgo.rb
@@ -0,0 +1,27 @@
+cask "flashgo" do
+  version "1.4.0+10400"
+  sha256 "5c3f333c518c8c9c762273c7a89d1ee6745be215abd04f9ed5296fb93fd704ea"
+
+  url "https://pkgs.flashgo.net/client/1.4.0/FlashGo_v#{version}.dmg"
+  name "FlashGo for Mac client"
+  desc "Modern high-performance network proxy tool"
+  homepage "https://flashgo.net/"
+
+  livecheck do
+    url "https://pkgs.flashgo.net/client/appcast.xml"
+    strategy :sparkle
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
+  app "FlashGo.app"
+
+  uninstall quit:   "app.flashgo.net",
+            delete: "/Applications/FlashGo.app"
+
+  zap trash: [
+    "~/Library/Containers/app.flashgo.net",
+    "~/Library/Group Containers/$(TeamIdentifierPrefix).app.flashgo.net",
+  ]
+end
