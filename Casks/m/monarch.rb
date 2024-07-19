79995d222f684d7181661bacddad553793e1d184
Michael Law <rmdashrfv@hey.com>
2024-07-19 18:48:49 -0400

    added monarch (spotlight search replacement)

---
+++ Casks/m/monarch.rb
@@ -0,0 +1,30 @@
+cask "monarch" do
+  arch arm: "arm64", intel: "x64"
+
+  version '0.6.70'
+  sha256 arm:   "e4b557c50f5ffa37ab0d2dc18dca43935bc6a5a6ce919531b2dd656faab79f14",
+         intel: "7db2c4940c483e3c96df64f0bb37ae6fcbbda90d45cb61075897031ac1765a8c"
+
+  url "https://monarchlauncher.s3.amazonaws.com/#{arch}/monarch-#{version}.dmg",
+      verified: "monarchlauncher.s3.amazonaws.com/"
+
+  name "Monarch"
+  desc "Spotlight Search with Superpowers"
+  homepage "https://monarchlauncher.com"
+
+  depends_on macos: ">= :monterey"
+
+  app "Monarch.app"
+
+  zap trash: [
+    "~/Library/Application Support/monarch",
+    "~/Library/Saved Application State/com.electron.monarch.savedState"
+  ]
+
+  livecheck do
+    url "https://monarchlauncher.s3.amazonaws.com/#{arch}/latest-mac.yml"
+    strategy :yaml do |yaml|
+      yaml["version"]
+    end
+  end
+end
