552058d3de8233197d95b978da4f3cab82e57a8e
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-20 00:02:29 +0200

    wiso-steuer-2023: update livecheck, use versioned url

--- Casks/w/wiso-steuer-2023.rb
+++ Casks/w/wiso-steuer-2023.rb
@@ -1,17 +1,21 @@
 cask "wiso-steuer-2023" do
   # NOTE: "2023" is not a version number, but an intrinsic part of the product name
   version "30.14.4340"
-  sha256 :no_check
+  sha256 "fc59b630ca312d6fae28804f21e0c1464c83b42991b3fb5eea8bfba564df1d6b"
 
-  url "https://download.buhl.de/ESD/Steuer/2023/WISOSteuer2023.dmg"
+  url "https://update.buhl-data.com/Updates/Steuer/2023/Mac/Files/#{version}-RC1/SteuerMac2023-#{version}.dmg",
+      verified: "update.buhl-data.com/Updates/Steuer/"
   name "WISO Steuer 2023"
   desc "Tax declaration for the fiscal year 2022"
   homepage "https://www.buhl.de/download/wiso-steuer-2023/"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url "https://update.buhl-data.com/Updates/Steuer/2023/Mac/Aktuell/appcast-steuer.xml"
+    strategy :sparkle
   end
+
+  auto_updates true
+  depends_on macos: ">= :mojave"
 
   app "SteuerMac 2023.app"
 
@@ -23,4 +27,8 @@ cask "wiso-steuer-2023" do
     "~/Library/Saved Application State/com.BuhlData.WISOsteuerMac2023.savedState",
     "~/Library/WebKit/com.BuhlData.WISOsteuerMac2023",
   ]
+
+  caveats do
+    requires_rosetta
+  end
 end
