24e372ac34c1535c0a159db29b165d114d7e4bae
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-21 03:31:30 +0000

    altair-graphql-client 7.3.0

--- Casks/a/altair-graphql-client.rb
+++ Casks/a/altair-graphql-client.rb
@@ -1,9 +1,9 @@
 cask "altair-graphql-client" do
   arch arm: "arm64", intel: "x64"
 
-  version "7.2.4"
-  sha256 arm:   "a6f234b2d979b35b4801b50e44778633640d8c7f84426000f3bf636d2b679f8c",
-         intel: "078a4279a6c148d8f3c94fb9c3167900b2b50c9b0722c8ee4f060b113e15df13"
+  version "7.3.0"
+  sha256 arm:   "d22d9e9b98badea2bd76e87d2d75fc20e8a8dd90cae2895d2c719601c36533c7",
+         intel: "21963fabeab2c67d2d4f7b8367e30f8d67d64d178f7bba7dc845be4ba5815351"
 
   url "https://github.com/imolorhe/altair/releases/download/v#{version}/altair_#{version}_#{arch}_mac.zip",
       verified: "github.com/imolorhe/altair/"
