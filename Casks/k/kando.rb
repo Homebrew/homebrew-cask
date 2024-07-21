c794ffbf36a1396ec8d4c508c386cd412d63755a
Akshay Kalose <akshay@kalose.net>
2024-07-21 20:50:37 +0530

    Brew Audit

--- Casks/k/kando.rb
+++ Casks/k/kando.rb
@@ -5,8 +5,7 @@ cask "kando" do
   sha256 arm:   "ee20889ab29bc05988e06363bcc9c77f26a1743c5db52b61ccab23b3b52ef534",
          intel: "b04b5d89d33887777711586e0aa61640ff8bfa98335871d993feb12c1957cb9a"
 
-  url "https://github.com/kando-menu/kando/releases/download/v#{version}/Kando-#{version}-#{arch}.dmg",
-      verified: "github.com/kando-menu/kando"
+  url "https://github.com/kando-menu/kando/releases/download/v#{version}/Kando-#{version}-#{arch}.dmg"
   name "Kando"
   desc "The Cross-Platform Pie Menu"
   homepage "https://github.com/kando-menu/kando"
