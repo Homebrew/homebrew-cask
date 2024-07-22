436fb19be0627be11920062829c64f25d96e3b4e
Akshay Kalose <AkshayProductions@gmail.com>
2024-07-22 18:49:03 +0530

    Apply suggestions from code review
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/k/kando.rb
+++ Casks/k/kando.rb
@@ -7,19 +7,15 @@ cask "kando" do
 
   url "https://github.com/kando-menu/kando/releases/download/v#{version}/Kando-#{version}-#{arch}.dmg"
   name "Kando"
-  desc "Cross-Platform Pie Menu"
+  desc "Pie Menu"
   homepage "https://github.com/kando-menu/kando"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
 
   depends_on macos: ">= :catalina"
 
   app "Kando.app"
 
   zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.electron.kando.sfl*",
     "~/Library/Application Support/Kando",
     "~/Library/Preferences/com.electron.kando.plist",
   ]
