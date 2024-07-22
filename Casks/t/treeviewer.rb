be974fdb2f184ef6fa3b80bac017265fd4500a2e
Yokohide0317 <yokohide0317@gmail.com>
2024-07-22 22:55:53 +0900

    treeviewer 2.2.0 (new formula)

--- Casks/t/treeviewer.rb
+++ Casks/t/treeviewer.rb
@@ -5,7 +5,7 @@ cask "treeviewer" do
   arch arm: "arm64", intel: "x64"
 
   version "2.2.0"
-  sha256 arm: "dd530a459a691df44463cf7f39ed8415657ec5847db1d7dededa96fb1063ef8f",
+  sha256 arm:   "dd530a459a691df44463cf7f39ed8415657ec5847db1d7dededa96fb1063ef8f",
          intel: "082cd5f0858511634c0e8a8e32a24137f54476b4314900c6c83cda9be7251d19"
 
   url "https://github.com/arklumpus/TreeViewer/releases/download/v#{version}/TreeViewer-Mac-#{arch}.dmg"
@@ -24,6 +24,6 @@ cask "treeviewer" do
   # Documentation: https://docs.brew.sh/Cask-Cookbook#stanza-zap
   zap trash: [
     "/private/var/db/receipts/io.github.arklumpus.TreeViewer.bom",
-    "/private/var/db/receipts/io.github.arklumpus.TreeViewer.plist"
+    "/private/var/db/receipts/io.github.arklumpus.TreeViewer.plist",
   ]
 end
