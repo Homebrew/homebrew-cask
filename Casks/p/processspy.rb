acaf0a1a1fa715fec008ac33a76f6363b1e85053
Pierre-Luc Paour <github@paour.com>
2024-07-24 15:44:07 +0200

    Apply suggestions from code review
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/p/processspy.rb
+++ Casks/p/processspy.rb
@@ -4,7 +4,7 @@ cask "processspy" do
 
   url "https://process-spy.app/archive/ProcessSpy_#{version}.dmg"
   name "ProcessSpy"
-  desc "Monitor processes with command-line arguments"
+  desc "Process monitor"
   homepage "https://process-spy.app/"
 
   livecheck do
@@ -12,6 +12,7 @@ cask "processspy" do
     strategy :sparkle, &:short_version
   end
 
+  auto_updates true
   depends_on macos: ">= :sonoma"
 
   app "ProcessSpy.app"
@@ -19,5 +20,6 @@ cask "processspy" do
   zap trash: [
     "~/Library/HTTPStorages/com.itone.ProcessSpy",
     "~/Library/Preferences/com.itone.ProcessSpy.plist",
+    "~/Library/Saved Application State/com.itone.ProcessSpy.savedState",
   ]
 end
