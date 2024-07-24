2c9c96c04f8dfaad5e8f7a8866dffdd875ea988d
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-24 14:29:34 +0200

    gb-studio 4.0.1

--- Casks/g/gb-studio.rb
+++ Casks/g/gb-studio.rb
@@ -1,9 +1,9 @@
 cask "gb-studio" do
   arch arm: "apple-silicon", intel: "intel"
 
-  version "4.0.0"
-  sha256 arm:   "c78df6f5ee1759364e6ca5c0c57f0f6fbdc4627655aadc560a75bc77f70f5074",
-         intel: "f289eda2a9cfa9ff5f39e8bc615d7e41be97935b6b8a414095adc4f014895dbb"
+  version "4.0.1"
+  sha256 arm:   "2972d44440ff4f90a652c6f0941c65218f5c42c8066bfe7b3bce156fece29b5f",
+         intel: "3feb11309fed3676efd8c2879ec18c6cd719623dc7826e0193a36bc1817f0f09"
 
   url "https://github.com/chrismaltby/gb-studio/releases/download/v#{version}/gb-studio-mac-#{arch}.zip",
       verified: "github.com/chrismaltby/gb-studio/"
