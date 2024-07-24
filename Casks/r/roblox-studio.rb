5e08ab996b3e72471237f02c1a0db3aedffc8e7b
lucky <lucky@lucky-book.local>
2024-07-25 00:21:56 +0400

    roblox studio (new cask)

---
+++ Casks/r/roblox-studio.rb
@@ -0,0 +1,37 @@
+cask "roblox-studio" do
+  arch arm: "arm64/"
+
+  version "0.634.0.6340420,f0cf2b7d1f064073"
+  sha256 arm:   "a1db3f2afe054b852405901bd5a104b6b5835f13707f7231808cf4365cd0e7f4",
+         intel: "ebb7bbb47df9e6c76ca9cde58f3edea352250bc9b87cd129bf447e75c9cbcb9c"
+
+  url "https://setup.rbxcdn.com/mac/#{arch}version-#{version.csv.second}-RobloxStudioApp.zip",
+      verified: "setup.rbxcdn.com/"
+  name "Roblox Studio"
+  desc "Roblox IDE to build your experiences"
+  homepage "https://create.roblox.com/"
+
+  livecheck do
+    url "https://clientsettingscdn.roblox.com/v1/client-version/MacStudio"
+    strategy :json do |json|
+      version = json["version"]
+      client_version = json["clientVersionUpload"]&.split("-")&.second
+      next if version.blank? || client_version.blank?
+
+      "#{version},#{client_version}"
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "RobloxStudio.app"
+
+  uninstall quit:   "com.roblox.RobloxStudio",
+            delete: "/Applications/RobloxStudio.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.roblox.RobloxStudio.plist",
+    "~/Library/Preferences/com.roblox.RobloxStudioChannel.plist",
+  ]
+end
