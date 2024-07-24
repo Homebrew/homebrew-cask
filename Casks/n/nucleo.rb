63169b9e6bbdf7f4abf7be3279b3266521cfd7d3
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-24 16:46:33 +0200

    nucleo 4.1.4

--- Casks/n/nucleo.rb
+++ Casks/n/nucleo.rb
@@ -1,8 +1,11 @@
 cask "nucleo" do
-  version "4.1.2"
-  sha256 "8108a8d6899e76b37d51a3d697c6b2e0fd2f9e7a0ef999beda120186873dbc24"
+  arch arm: "-silicon"
 
-  url "https://nucleo-app-releases.s3.amazonaws.com/mac/Nucleo_#{version}.zip",
+  version "4.1.4"
+  sha256 arm:   "624133d70caa43292b7dbb67f8d3a29ed3eb8d1db1921bd19bec5efd61ef7b07",
+         intel: "77e898fe09a14dbd350f248e8cf65e629d34279df5ba7850a64cd5251fdb8493"
+
+  url "https://nucleo-app-releases.s3.amazonaws.com/mac#{arch}/Nucleo_#{version}.zip",
       verified: "nucleo-app-releases.s3.amazonaws.com/"
   name "Nucleo"
   desc "Icon manager and library"
@@ -12,6 +15,8 @@ cask "nucleo" do
     url "https://nucleoapp.com/updates"
     regex(/href=.*?Nucleo[._-]v?(\d+(?:\.\d+)+)\.zip/i)
   end
+
+  depends_on macos: ">= :high_sierra"
 
   app "Nucleo.app"
 
