d8c2fbc3db1cf9e1557aed7885bc8906b7b29515
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-07-24 20:20:05 -0400

    openboardview 9.95.1

--- Casks/o/openboardview.rb
+++ Casks/o/openboardview.rb
@@ -1,6 +1,6 @@
 cask "openboardview" do
-  version "9.95.0"
-  sha256 "eeb3ac2a453215c38a21467cac9bb562895e3b283b0dc0140aed2ef31d08b253"
+  version "9.95.1"
+  sha256 "dcc5646053f2635ece1a88a8412c87aca738519d16becc38870520549b72db1a"
 
   url "https://github.com/OpenBoardView/OpenBoardView/releases/download/#{version}/OpenBoardView-#{version}-Darwin.dmg",
       verified: "github.com/OpenBoardView/OpenBoardView/"
