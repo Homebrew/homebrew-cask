3bb329e03e9ee8aaa4b61f18c6685c12238fc369
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-25 18:11:12 +0200

    roon: add zap stanza

--- Casks/r/roon.rb
+++ Casks/r/roon.rb
@@ -2,7 +2,8 @@ cask "roon" do
   version "1.0"
   sha256 :no_check
 
-  url "https://download.roonlabs.com/builds/Roon.dmg"
+  url "https://download.roonlabs.net/builds/Roon.dmg",
+      verified: "download.roonlabs.net/"
   name "Roon"
   desc "Music player"
   homepage "https://roonlabs.com/"
@@ -12,5 +13,17 @@ cask "roon" do
     strategy :extract_plist
   end
 
+  depends_on macos: ">= :catalina"
+
   app "Roon.app"
+
+  uninstall quit:       "RoonServer",
+            login_item: "RoonServer"
+
+  zap trash: [
+    "~/Library/Roon",
+    "~/Library/RoonGoer",
+    "~/Library/RoonServer",
+    "~/Library/Saved Application State/com.roon.Roon.savedState",
+  ]
 end
