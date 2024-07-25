e4f8326766363848599e4e5fc723283badd72c53
William Isted <WilliamIsted@users.noreply.github.com>
2024-07-25 15:14:07 +0100

    Create mamp@6.rb
    With MAMP Pro 7 being a paid for update, support should be kept for MAMP Pro 6.
    
    Ref: https://github.com/Homebrew/homebrew-cask/commit/7481fac97ed14ef2387379b5f7979ac9e3f6b42b

---
+++ Casks/m/mamp@6.rb
@@ -0,0 +1,48 @@
+cask "mamp" do
+  arch arm: "M1-arm", intel: "Intel-x86"
+
+  version "6.9"
+  sha256 arm:   "e66a67fad914a065c882ffa137c9c9dc5ae8c24de53320d560b6b5cc0f4a890a",
+         intel: "7a3ff488c9c8d3f8f6730edfb06822e2640303cfcd03d75d14bb977d85518570"
+
+  url "https://downloads.mamp.info/MAMP-PRO/releases/#{version}/MAMP_MAMP_PRO_#{version}-#{arch}.pkg"
+  name "MAMP"
+  desc "Web development solution with Apache, Nginx, PHP & MySQL"
+  homepage "https://www.mamp.info/"
+
+  livecheck do
+    url "https://www.mamp.info/en/downloads/"
+    regex(%r{href=.*?/MAMP[._-]MAMP[._-]PRO[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}\.pkg}i)
+  end
+
+  auto_updates true
+  depends_on macos: ">= :sierra"
+
+  pkg "MAMP_MAMP_PRO_#{version}-#{arch}.pkg"
+
+  postflight do
+    set_ownership ["/Applications/MAMP", "/Applications/MAMP PRO"]
+  end
+
+  uninstall pkgutil: "de.appsolute.installer.(mamp|mampacticon|mampendinstall|mamppro).pkg",
+            delete:  "/Applications/MAMP"
+
+  zap delete: [
+        "/Library/Application Support/appsolute",
+        "/Library/LaunchDaemons/de.appsolute.mampprohelper.plist",
+        "/Library/PrivilegedHelperTools/de.appsolute.mampprohelper",
+      ],
+      trash:  [
+        "~/Library/Application Support/appsolute",
+        "~/Library/Application Support/de.appsolute.MAMP",
+        "~/Library/Application Support/de.appsolute.mamppro",
+        "~/Library/Caches/de.appsolute.MAMP",
+        "~/Library/Caches/de.appsolute.mamppro",
+        "~/Library/HTTPStorages/de.appsolute.MAMP",
+        "~/Library/HTTPStorages/de.appsolute.mamppro",
+        "~/Library/Preferences/de.appsolute.MAMP.plist",
+        "~/Library/Preferences/de.appsolute.mamppro.plist",
+        "~/Library/Saved Application State/de.appsolute.MAMP.savedState",
+        "~/Library/Saved Application State/de.appsolute.mamppro.savedState",
+      ]
+end
