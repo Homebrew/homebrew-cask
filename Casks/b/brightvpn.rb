b39ab8009271e974f59328edccc3097eb6938e80
Dmitrii Shelonin <110693375+dmitriisbd@users.noreply.github.com>
2024-07-25 15:16:13 +0300

    brightvpn modified version
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>

--- Casks/b/brightvpn.rb
+++ Casks/b/brightvpn.rb
@@ -1,5 +1,5 @@
 cask "brightvpn" do
-  version "1.227.590"
+  version "1.1-1.227.590"
   sha256 "e2597911d0c0b7b6b0fba571990a875c7da8fe7d08728512b7f18017478ab881"
 
   url "https://brightdata.com/static/BrightVPN-1.1-#{version}.dmg",
