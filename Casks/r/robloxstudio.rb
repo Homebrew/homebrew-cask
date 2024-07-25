0a406843f648367843b1bf35ff923c060967655b
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-07-24 20:13:21 -0400

    robloxstudio 0.635.0.6350588

--- Casks/r/robloxstudio.rb
+++ Casks/r/robloxstudio.rb
@@ -1,9 +1,9 @@
 cask "robloxstudio" do
   arch arm: "arm64/"
 
-  version "0.634.0.6340420,f0cf2b7d1f064073"
-  sha256 arm:   "a1db3f2afe054b852405901bd5a104b6b5835f13707f7231808cf4365cd0e7f4",
-         intel: "ebb7bbb47df9e6c76ca9cde58f3edea352250bc9b87cd129bf447e75c9cbcb9c"
+  version "0.635.0.6350588,ea15dfacf9a7428a"
+  sha256 arm:   "0c7cf65baaca96638051329ad455a39ecd89288589b14c9f1e22b9fee647dbec",
+         intel: "97fa11011a45879ee2b60f8c1dd9a1149288a9c30e1613190e92fc0eda078f03"
 
   url "https://setup.rbxcdn.com/mac/#{arch}version-#{version.csv.second}-RobloxStudioApp.zip",
       verified: "setup.rbxcdn.com/"
