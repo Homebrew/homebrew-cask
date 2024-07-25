00b7ba659e298fc0c7e487bf65a3ee06a6d587f7
Qi Zhang <25192197+singularitti@users.noreply.github.com>
2024-07-25 12:46:03 -0600

    Update font-genyogothic.rb
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/font/font-g/font-genyogothic.rb
+++ Casks/font/font-g/font-genyogothic.rb
@@ -2,7 +2,7 @@ cask "font-genyogothic" do
   version "2.000"
   sha256 "e570102fd1c9abb67897b215e6b7f2a0666610f9bb1e2af5fbb3a2db8f67fa93"
 
-  url "https://github.com/ButTaiwan/genyog-font/releases/download/v#{version}/GenYoGothic#{version.major}.zip"
+  url "https://github.com/ButTaiwan/genyog-font/releases/download/v#{version}/GenYoGothic#{version.major}-ttc.zip"
   name "GenYoGothic"
   name "源樣黑體"
   desc "Open-source Chinese font derived from Source Han Sans"
