67d401985070a1ba3acb5fabd66f86ad86d67b11
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-07-25 21:44:47 -0400

    webex 44.7.0.30285

--- Casks/w/webex.rb
+++ Casks/w/webex.rb
@@ -1,7 +1,7 @@
 cask "webex" do
   arch arm: "Desktop-MACOS-Apple-Silicon", intel: "TeamsDesktop-MACOS"
 
-  version "44.7.0.30141"
+  version "44.7.0.30285"
   sha256 :no_check
 
   url "https://binaries.webex.com/Webex#{arch}-Gold/Webex.dmg"
