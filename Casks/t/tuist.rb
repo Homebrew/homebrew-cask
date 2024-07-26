d2660d6228a951ec048adbf1f241aa4f313dcef4
Pedro <pedro@pepicrft.me>
2024-07-26 18:02:53 +0200

    Disable the cask

--- Casks/t/tuist.rb
+++ Casks/t/tuist.rb
@@ -1,4 +1,5 @@
 cask "tuist" do
+  disable! date: "2024-07-26", because: "It's distributed through the tap https://github.com/tuist/homebrew-tuist"
   version "4.21.2"
   sha256 "7c00de9e0d45de9b1071b84d81a4cb95010c807733676e065a2926db45e4fc88"
 
