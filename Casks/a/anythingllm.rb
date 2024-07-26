7407257130f0ed9f041bc3f8a37cc9f4b6934591
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-07-26 18:16:17 -0400

    anythingllm 1.5.12

--- Casks/a/anythingllm.rb
+++ Casks/a/anythingllm.rb
@@ -1,9 +1,9 @@
 cask "anythingllm" do
   arch arm: "-Silicon"
 
-  version "1.5.11"
-  sha256 arm:   "54bcd78f3043efb33c1548992d2f3f9208b5f6636dda65322e4b7047b29bd46f",
-         intel: "9dc26996c726ca24403ac79945fb7ff71a58bd0ec9b90f2ec93a46045f20047b"
+  version "1.5.12"
+  sha256 arm:   "f972d0724a9ed223f02b090132ba99b97e920f798787f705fbf6b00c4e5a397e",
+         intel: "3ab1410b14284469d198c138427b90684580a611fb5b2c5201d6014ac4c7385c"
 
   url "https://s3-us-west-1.amazonaws.com/public.useanything.com/latest/AnythingLLMDesktop#{arch}.dmg",
       verified: "s3-us-west-1.amazonaws.com/public.useanything.com/"
