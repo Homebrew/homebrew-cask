19354ca630261f7b8e6ef304a0f53bafa50319d6
Sam Ford <1584702+samford@users.noreply.github.com>
2024-07-26 22:59:13 -0400

    fl-studio: update url, livecheck
    The cask URL for `fl-studio` doesn't work locally or on CI, so this
    updates it to the current URL from the redirecting macOS link on the
    download page. Even with this change, it was still necessary to use
    a `:browser` user agent and add a referer to get this to work.
    
    The existing `livecheck` block for `fl-studio` returns an `Unable to
    get versions` error, as the request is blocked by Cloudflare. As with
    the dmg URL, we have to set a `:browser` user agent and referer for
    this to work. We can't set the referer in livecheck yet, so we have
    to work around this for now.
    
    This addresses the issue by updating the `livecheck` block to parse
    the version from the URL that the download page uses to fetch
    version information. Unfortunately the server returns JavaScript, so
    we have to extract the JSON data from the content, unescape the
    string, and parse it. The data only contains FL Studio releases at
    the moment but I added some safeguards to help ensure we only return
    appropriate versions if the data changes in the future.

--- Casks/f/fl-studio.rb
+++ Casks/f/fl-studio.rb
@@ -2,14 +2,34 @@ cask "fl-studio" do
   version "24.1.1.3888"
   sha256 "5dea7153764f3f4a3c19099c79d7b790993335728600e05e890efac699a00ab5"
 
-  url "https://demodownload.image-line.com/flstudio/flstudio_mac_#{version}.dmg"
+  url "https://install.image-line.com/flstudio/flstudio_mac_#{version}.dmg",
+      referer:    "https://www.image-line.com/fl-studio-download/",
+      user_agent: :browser
   name "FL Studio"
   desc "Digital audio production application"
   homepage "https://www.image-line.com/flstudio/"
 
+  # The macOS link on the download page redirects to the latest dmg file but
+  # livecheck is blocked by Cloudflare unless we use a `:browser` user agent
+  # and set the download page as the referer. We can't set the referer in
+  # livecheck yet, so this works around the issue by parsing the version from
+  # the URL that the download page uses to fetch version information.
   livecheck do
-    url "https://support.image-line.com/redirect/flstudio20_mac_installer"
-    strategy :header_match
+    url "https://support.image-line.com/api.php?call=get_version_info&callback=il_get_version_info_cb"
+    strategy :page_match do |page|
+      # Extract the JSON text from the JavaScript
+      match = page.match(/il_get_version_info_cb\("(.+?)"\);/i)
+      next if match.blank?
+
+      # Unescape the JSON text and parse it
+      json = Homebrew::Livecheck::Strategy::Json.parse_json(match[1].gsub(/\\+"/, '"'))
+      json["prod"]&.filter_map do |_id, prod|
+        next unless (prod_mac = prod["mac"])
+        next unless prod_mac["name"]&.include?("FL Studio")
+
+        prod_mac["version"]
+      end
+    end
   end
 
   pkg "Install FL Studio.pkg"
