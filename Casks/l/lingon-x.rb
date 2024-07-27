606cd073297fca5b789f0ab5ad8592afd62f6c4e
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-27 14:30:34 +0200

    lingon-x 9.5.2

--- Casks/l/lingon-x.rb
+++ Casks/l/lingon-x.rb
@@ -16,8 +16,8 @@ cask "lingon-x" do
     sha256 "c1c839e8dc13bd295f2080980c5bea22299c33f3333b7c6981161b46d6f021d8"
   end
   on_monterey :or_newer do
-    version "9.5.1"
-    sha256 "462efbcf97a36795396bf5968fab2345e2c0d7f566e48b14734b58720315ed03"
+    version "9.5.2"
+    sha256 "cdc5215df413a99ae400eb4e6e8bc1a15f802b046d26b7812b09e7611e0676cb"
   end
 
   url "https://www.peterborgapps.com/downloads/LingonX#{version.major}.zip"
