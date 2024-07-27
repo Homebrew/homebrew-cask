a5be7c4afaddcf4b1ce2ee80718c9d5a542c4cc0
Greenstar <39073901+greenstar1151@users.noreply.github.com>
2024-07-27 18:07:01 +0900

    font-goorm-sans-code 1.0.1 (new cask)

---
+++ Casks/font/font-g/font-goorm-sans-code.rb
@@ -0,0 +1,17 @@
+cask "font-goorm-sans-code" do
+  version "1.0.1"
+  sha256 "ddf93e59558509f2b62959fb553461b0b03cd00a186e61d21f410428c5ee76ba"
+
+  url "https://statics.goorm.io/fonts/GoormSansCode/v#{version}/goorm-sans-code-#{version}.zip"
+  name "goorm Sans Code"
+  homepage "https://goorm-sans.goorm.io/"
+
+  livecheck do
+    url "https://goorm-sans.goorm.io"
+    strategy :page_match do |page|
+      page.scan(/href=.*?goorm[._-]sans[._-]code[._-](\d+(?:\.\d+)*)\.zip/i).map { |match| match[0] }
+    end
+  end
+
+  font "goorm sans code 2/Public/goorm_Sans_Code_400.ttf"
+end
