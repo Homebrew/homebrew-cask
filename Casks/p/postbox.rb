c78526e2176cca61ebcd87df02859e6139ad5aed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-29 06:47:49 +0000

    postbox 7.0.62

--- Casks/p/postbox.rb
+++ Casks/p/postbox.rb
@@ -1,6 +1,6 @@
 cask "postbox" do
-  version "7.0.61"
-  sha256 "0e0f5be4ba4c34bdaeb5220f408437ec0c03e6635854af4582e18286c600d6c3"
+  version "7.0.62"
+  sha256 "4ef6b1346800a0d41296f5daf896b4847c309ab508504e2b5df9a95effa09638"
 
   url "https://d3nx85trn0lqsg.cloudfront.net/mac/postbox-#{version}-mac64.dmg",
       verified: "d3nx85trn0lqsg.cloudfront.net/mac/"
