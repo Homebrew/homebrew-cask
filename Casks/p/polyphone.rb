9fe5ccff63b8c4241d482d41b263140dc1f8148c
Bevan Kay <email@bevankay.me>
2024-07-29 16:14:03 +1000

    polyphone: update livecheck

--- Casks/p/polyphone.rb
+++ Casks/p/polyphone.rb
@@ -1,22 +1,26 @@
 cask "polyphone" do
-  version "2.3.1,905"
+  version "2.3.1,84"
   sha256 "46d3abf42f88cc157d7f2d751619d7c599789ebef65b2d31f5873342f1184e9d"
 
-  url "https://www.polyphone-soundfonts.com/download/file/#{version.csv.second}-polyphone-#{version.csv.first.dots_to_hyphens}-dmg/latest/download"
+  url "https://www.polyphone.io/download/0/v#{version.csv.second}/polyphone_#{version.csv.first}.dmg"
   name "Polyphone"
   desc "Soundfont editor for quickly designing musical instruments"
-  homepage "https://polyphone-soundfonts.com/"
+  homepage "https://www.polyphone.io/en"
 
   livecheck do
-    url "https://www.polyphone-soundfonts.com/download"
-    regex(/(\d+)[._-]polyphone[._-]v?(\d+(?:-\d+)+)[._-]dmg/i)
+    url "https://www.polyphone.io/en/software"
+    regex(/Polyphone\s+(\d+(?:\.\d+)+).*download&file_id=(\d+).*Mac OS X/im)
     strategy :page_match do |page, regex|
       match = page.match(regex)
       next if match.blank?
 
-      "#{match[2].tr("-", ".")},#{match[1]}"
+      "#{match[1]},#{match[2]}"
     end
   end
 
   app "polyphone.app"
+
+  caveats do
+    requires_rosetta
+  end
 end
