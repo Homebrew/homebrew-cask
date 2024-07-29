8d5c405c2c065ba5dd0075348b36d3d9e5b0d1a9
wyhaya <hi@wyhaya.com>
2024-07-29 16:14:27 +0800

    Dataflare 1.7.0 (new cask)

---
+++ Casks/d/dataflare.rb
@@ -0,0 +1,35 @@
+cask "dataflare" do
+  arch arm: "aarch64", intel: "x86_64"
+
+  version "1.7.0"
+  sha256 arm:   "6fe5170a05dcdee38553c70f2e8c484bff40e6e1cc92606f80718bb026f9abb6",
+         intel: "1cd66bd8948b49fc224ea7f579517e3439aa76e1c0467670234fad91cfc4a50b"
+
+  url "https://assets.dataflare.app/release/darwin/#{arch}/Dataflare-#{version}.dmg"
+  name "Dataflare"
+  desc "Simple and elegant database manager"
+  homepage "https://dataflare.app/"
+
+  livecheck do
+    url "https://assets.dataflare.app/release/darwin/#{arch}/latest.json"
+    strategy :json do |json|
+      json["version"]
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "Dataflare.app"
+
+  zap trash: [
+    "~/Library/Application Support/app.dataflare.desktop",
+    "~/Library/Caches/app.dataflare.desktop",
+    "~/Library/Caches/Dataflare",
+    "~/Library/HTTPStorages/Dataflare.binarycookies",
+    "~/Library/Preferences/app.dataflare.desktop.plist",
+    "~/Library/Preferences/Dataflare.plist",
+    "~/Library/WebKit/Dataflare",
+    "~/Library/WebKit/app.dataflare.desktop"
+  ]
+end
