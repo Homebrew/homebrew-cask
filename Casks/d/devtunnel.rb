08052981e3462bf3db6b645d87040f27009472c3
Janis Büse <47151705+jbuese@users.noreply.github.com>
2024-07-29 10:36:07 +0200

    fix(devtunnel): incorrect sha256 checksum

--- Casks/d/devtunnel.rb
+++ Casks/d/devtunnel.rb
@@ -3,7 +3,7 @@ cask "devtunnel" do
 
   version "1.0.1338+932252c8a1"
   sha256 arm:   "a0db69bd6f16bbda78a941aee19b13b9af0415859d114bb23ec3240d49484773",
-         intel: "9b2a798c02c7bc68e644c809cc06f10d245c1ba6ea4d1f921f201cd262f75cf8"
+         intel: "648e13e2c4eebeb411a68f6912dc7b4cf222ef85b14700089011f88b7c13a8cb"
 
   url "https://tunnelsassetsprod.blob.core.windows.net/cli/#{version}/osx-#{arch}-devtunnel-zip",
       verified: "tunnelsassetsprod.blob.core.windows.net/cli/"
