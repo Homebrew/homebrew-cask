9f94e0973d52ca78fe4f458bdc2ed2acd0a77567
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-30 18:54:48 +0200

    sonos: update livecheck

--- Casks/s/sonos.rb
+++ Casks/s/sonos.rb
@@ -1,18 +1,22 @@
 cask "sonos" do
-  version "16.3,80.1.55014"
+  version "80.1-55014,xqkmivn9QB"
   sha256 "cac8a5f9e13c2959bd2dff239a8576c40c2fb84afa7f412ac393dabf7b120461"
 
-  url "https://update-software.sonos.com/software/xqkmivn9QB/Sonos_#{version.csv.second.sub(/(\d+)\.(\d+)\.(\d+)/, '\1.\2-\3')}.dmg"
+  url "https://update-software.sonos.com/software/#{version.csv.second}/Sonos_#{version.csv.first}.dmg"
   name "Sonos S2"
   desc "Control your Sonos system"
   homepage "https://www.sonos.com/"
 
   livecheck do
-    url "https://www.sonos.com/en/redir/controller_software_mac2"
-    strategy :extract_plist
+    url "https://www.sonos.com/redir/controller_software_mac2"
+    regex(%r{software/(\w+)/Sonos[._-]v?(\d+(?:.\d+)+)\.dmg}i)
+    strategy :header_match do |headers, regex|
+      headers["location"]&.scan(regex)&.map { |match| "#{match[1]},#{match[0]}" }
+    end
   end
 
   auto_updates true
+  depends_on macos: ">= :el_capitan"
 
   app "Sonos.app"
 
