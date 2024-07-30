90443486c4efb498a36c77ad7c306ff8f8b6bb47
Andrew Leonard <31470007+andrew-m-leonard@users.noreply.github.com>
2024-07-30 14:39:04 +0100

    Update Casks/t/temurin@8.rb
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/t/temurin@8.rb
+++ Casks/t/temurin@8.rb
@@ -17,7 +17,7 @@ cask "temurin@8" do
 
   livecheck do
     url "https://api.adoptium.net/v3/assets/feature_releases/8/ga?architecture=x64&image_type=jdk&jvm_impl=hotspot&os=mac&page=0&page_size=1&project=jdk&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse"
-    regex(/^jdk(\d+)u(\d+)-b(\d+)\.(\d+)$/i)
+    regex(/^jdk(\d+)u(\d+)-b(\d+(?:\.\d+)*)$/i)
     strategy :json do |json, regex|
       json.map do |release|
         match = release["release_name"]&.match(regex)
