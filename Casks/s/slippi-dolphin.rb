0f9c138d78d6f923e53f897439f961477612fa23
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-31 00:31:38 +0200

    slippi-dolphin 3.4.2

--- Casks/s/slippi-dolphin.rb
+++ Casks/s/slippi-dolphin.rb
@@ -1,6 +1,6 @@
 cask "slippi-dolphin" do
-  version "3.4.1"
-  sha256 "56ee2d69d4abd2dbbc6661c5de88cd635d49886df11a1709585c05f40944945a"
+  version "3.4.2"
+  sha256 "2e696cc442a25f01d9567433d31cda8f99bbafc4dddb1ee80fc160657fabe5a8"
 
   url "https://github.com/project-slippi/Ishiiruka/releases/download/v#{version}/FM-Slippi-#{version}-Mac.dmg",
       verified: "github.com/project-slippi/Ishiiruka/"
