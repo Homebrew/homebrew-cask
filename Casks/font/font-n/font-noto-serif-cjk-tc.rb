e5df2c267c582aede4e1d6af897591f0d46d4531
Yukai Chou <muzimuzhi@gmail.com>
2024-07-30 21:33:49 +0000

    font-noto-serif-cjk-tc 2.003

--- Casks/font/font-n/font-noto-serif-cjk-tc.rb
+++ Casks/font/font-n/font-noto-serif-cjk-tc.rb
@@ -1,6 +1,6 @@
 cask "font-noto-serif-cjk-tc" do
-  version "2.002"
-  sha256 "bd14b808431c5058c69f999c3c6d898b9f37d96962ec26f7212fb34c2d49ea5c"
+  version "2.003"
+  sha256 "b4aa07b217532c5859b3674d53588671e7e4f340054fc30e9bf417ee3b1aa4d4"
 
   url "https://github.com/notofonts/noto-cjk/releases/download/Serif#{version}/10_NotoSerifCJKtc.zip"
   name "Noto Serif CJK TC"
