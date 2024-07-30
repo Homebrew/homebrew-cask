84bceaef6700b4ad3848da84425ce303e5dc383e
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-30 20:11:38 +0200

    responsively 1.13.0

--- Casks/r/responsively.rb
+++ Casks/r/responsively.rb
@@ -1,9 +1,9 @@
 cask "responsively" do
   arch arm: "-arm64"
 
-  version "1.12.0"
-  sha256 arm:   "1409d81741674bdf9ff12706681bf613c5ff16504c50a1c60afaf2ee1e324a73",
-         intel: "2e33b9754b7fd1b614fba2a00d904a37fa322fa8711ccb108c401c22f13e3953"
+  version "1.13.0"
+  sha256 arm:   "d2359d562ae5bfba89c4d88e784bfd16e82cea7e14adc8ff00848e640033f6a5",
+         intel: "f9d8e5d3fd199687eed19de899cb007af9d3f47d5f844a61f241a83bf481df5e"
 
   url "https://github.com/responsively-org/responsively-app-releases/releases/download/v#{version}/ResponsivelyApp-#{version}#{arch}.dmg",
       verified: "github.com/responsively-org/responsively-app-releases/"
