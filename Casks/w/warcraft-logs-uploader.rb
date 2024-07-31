c31a1d3573282f9ccb576f4490c33ddb3ba08b8b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-31 13:01:59 +0000

    warcraft-logs-uploader 8.11.3

--- Casks/w/warcraft-logs-uploader.rb
+++ Casks/w/warcraft-logs-uploader.rb
@@ -1,6 +1,6 @@
 cask "warcraft-logs-uploader" do
-  version "8.11.0"
-  sha256 "1847bd71b26c0d210bca90ac2a6a58d397f620bfeedd6cc4a845957447975ba0"
+  version "8.11.3"
+  sha256 "ace71d039472557c0191a21c78fefd623f75cf58fcb1214a315cde5b3b330065"
 
   url "https://github.com/RPGLogs/Uploaders-warcraftlogs/releases/download/v#{version}/warcraftlogs-v#{version}.dmg",
       verified: "github.com/RPGLogs/Uploaders-warcraftlogs/"
