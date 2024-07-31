d1f1a9a00f5b593ec644ab83d7d620c9d66d8f0a
Joaquim Rocha <joaquim.rocha@microsoft.com>
2024-07-31 17:15:17 +0100

    Update Headlamp version to a fixed 0.25.0 build
    The previous build had symlinks in the app dependencies which make
    the notarization verification fail, so this new build is the same
    version as before but with those symlink replaced by what they
    pointed to.
    
    Signed-off-by: Joaquim Rocha <joaquim.rocha@microsoft.com>

--- Casks/h/headlamp.rb
+++ Casks/h/headlamp.rb
@@ -2,10 +2,10 @@ cask "headlamp" do
   arch arm: "arm64", intel: "x64"
 
   version "0.25.0"
-  sha256 arm:   "ea0ddeb12f7b3b32346f438586324c25583f9bc127128660f972fbc4823cdcba",
-         intel: "a091c9f833b030f1cd7cf99873d7e670581a772e9737bec7f78b77db5ddb2409"
+  sha256 arm:   "b65e0d0a795dac7deeb0bb4547e46a585d9df4f95bf6f2ef654428ab31237ad9",
+         intel: "b3a4586df03e5a45af7c2eeebb63fba2eeb08373374189c3adf51212210a2e62"
 
-  url "https://github.com/kinvolk/headlamp/releases/download/v#{version}/Headlamp-#{version}-mac-#{arch}.dmg",
+  url "https://github.com/kinvolk/headlamp/releases/download/v#{version}/Headlamp-#{version}-mac-#{arch}-1.dmg",
       verified: "github.com/kinvolk/headlamp/"
   name "Headlamp"
   desc "UI for Kubernetes"
