a9353bd44935bb254b9160683379d6864ddf837a
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-07-31 13:52:59 -0400

    vimcal 1.0.31

--- Casks/v/vimcal.rb
+++ Casks/v/vimcal.rb
@@ -2,9 +2,9 @@ cask "vimcal" do
   arch arm: "-arm64"
   host_suffix = on_arch_conditional arm: "m1", intel: "production"
 
-  version "1.0.30"
-  sha256 arm:   "dcc17bbbef2693b24d5e70e90cd0f34db4ad742f0cd2da79741ddd69742c6e3d",
-         intel: "55a489342db24a1839bf9728597ce3420352c96fb59c60b950e00c5bb4c64167"
+  version "1.0.31"
+  sha256 arm:   "8591360b7d8209454ea894af784d2f8e6491bcc56a53c4f99f72600cb9a2f560",
+         intel: "a3f764b1d0829f214caa04bb0f9aff803b109c02b28d74f66522f1d81994e3a9"
 
   url "https://vimcal-#{host_suffix}.s3.amazonaws.com/Vimcal-#{version}#{arch}.dmg",
       verified: "vimcal-#{host_suffix}.s3.amazonaws.com/"
