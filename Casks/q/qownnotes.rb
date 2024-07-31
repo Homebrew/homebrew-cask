9350f3a3e4050232dcdf94d475a501217338b3dd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-31 21:44:35 +0000

    qownnotes 24.8.0

--- Casks/q/qownnotes.rb
+++ Casks/q/qownnotes.rb
@@ -1,6 +1,6 @@
 cask "qownnotes" do
-  version "24.7.3"
-  sha256 "e1c10930c569feaa60f7acfe6360ceb5b1761491381a5f9ce72486a9e0e72ac3"
+  version "24.8.0"
+  sha256 "ed58f5d47daf63c3565e524be9b6e8fa4c76e59c46a03f573b983e901914cb68"
 
   url "https://github.com/pbek/QOwnNotes/releases/download/v#{version}/QOwnNotes.dmg",
       verified: "github.com/pbek/QOwnNotes/"
