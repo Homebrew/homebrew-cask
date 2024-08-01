5c249ef27362c98e77ff4f72b8a379578b09cb1b
René Dudfield <renesd@gmail.com>
2024-08-01 09:55:43 +0200

    headlamp: 0.25.0-1
    In the last fix to 0.25.0, we didn't bump the version, so current
    installations of that version are kept with the broken binaries.
    Having this new build version should force the update for those users.
    
    Signed-off-by: René Dudfield <renesd@gmail.com>

--- Casks/h/headlamp.rb
+++ Casks/h/headlamp.rb
@@ -1,15 +1,19 @@
 cask "headlamp" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.25.0"
+  version "0.25.0-1"
   sha256 arm:   "b65e0d0a795dac7deeb0bb4547e46a585d9df4f95bf6f2ef654428ab31237ad9",
          intel: "b3a4586df03e5a45af7c2eeebb63fba2eeb08373374189c3adf51212210a2e62"
 
-  url "https://github.com/kinvolk/headlamp/releases/download/v#{version}/Headlamp-#{version}-mac-#{arch}-1.dmg",
+  url "https://github.com/kinvolk/headlamp/releases/download/v0.25.0/Headlamp-0.25.0-1-mac-#{arch}.dmg",
       verified: "github.com/kinvolk/headlamp/"
   name "Headlamp"
   desc "UI for Kubernetes"
   homepage "https://kinvolk.github.io/headlamp"
+
+  livecheck do
+    skip "No version information available"
+  end
 
   app "Headlamp.app"
 
