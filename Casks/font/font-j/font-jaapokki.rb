0ef4597d0961fe1f57b3f039bcec9cc278dc5eae
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-01 07:20:22 +0200

    font-jaapokki: update homepage and url

--- Casks/font/font-j/font-jaapokki.rb
+++ Casks/font/font-j/font-jaapokki.rb
@@ -1,14 +1,14 @@
 cask "font-jaapokki" do
-  version "1.0"
-  sha256 "aa5f38cbb682df8732d406996e37349b83e47d3c14bae4af2125f2a7784430b7"
+  version "1.001"
+  sha256 :no_check
 
-  url "https://mikkonuuttila.com/jaapokki/Jaapokki-font-package-#{version}.zip"
+  url "https://type.mikkonuuttila.com/fonts/jaapokki.zip"
   name "Jaapokki"
-  homepage "https://mikkonuuttila.com/jaapokki/"
+  homepage "https://type.mikkonuuttila.com/jaapokki/"
 
-  font "Jaapokki-font-package-#{version}/Regular/Jaapokki-Regular.otf"
-  font "Jaapokki-font-package-#{version}/enchance/Jaapokkienchance-Regular.otf"
-  font "Jaapokki-font-package-#{version}/subtract/Jaapokkisubtract-Regular.otf"
+  font "Jaapokki-Regular.otf"
+  font "Jaapokkienchance-Regular.otf"
+  font "Jaapokkisubtract-Regular.otf"
 
   # No zap stanza required
 end
