eb8610b0ce24ed14bfec4518a3830c7890ec5bab
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-01 07:58:08 +0200

    ifunbox: update url

--- Casks/i/ifunbox.rb
+++ Casks/i/ifunbox.rb
@@ -2,7 +2,7 @@ cask "ifunbox" do
   version "1.8"
   sha256 "19bef7c6079cb3d13dc109478c473e420643e3164ed02b668f76220f60884a11"
 
-  url "http://dl.i-funbox.com/updates/ifunbox.mac/#{version}/ifunboxmac.dmg"
+  url "http://dl.i-funbox.com/mac/ifunboxmac.#{version}.dmg"
   name "iFunBox"
   desc "File management software for iPhone and other Apple products"
   homepage "https://www.i-funbox.com/"
@@ -18,4 +18,8 @@ cask "ifunbox" do
     "~/Library/Caches/com.iFunBoxDevTeam.iFunBox",
     "~/Library/Preferences/com.iFunBoxDevTeam.iFunBox.plist",
   ]
+
+  caveats do
+    requires_rosetta
+  end
 end
