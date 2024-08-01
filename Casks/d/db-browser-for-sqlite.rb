8650952ca946016ee66e4a3d0b4fa2e11523feae
SeongTae Jeong <seongtaejg@gmail.com>
2024-08-01 11:26:25 +0900

    Fix wrong URL syntax

--- Casks/d/db-browser-for-sqlite.rb
+++ Casks/d/db-browser-for-sqlite.rb
@@ -6,8 +6,9 @@ cask "db-browser-for-sqlite" do
     sha256 arm:   "0c2076e4479cb9db5c85123cfe9750641f92566694ff9f6c99906321a2c424e8",
            intel: "546d57b6c88c2be7517759c016c0bf0313dfcc14adfcb43967f3c5d24657f366"
 
-    url "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v#{version}/DB.Browser.for.SQLite#{arch}-#{version}.dmg",
-        livecheck do
+    url "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v#{version}/DB.Browser.for.SQLite#{arch}-#{version}.dmg"
+
+    livecheck do
       skip "Legacy version"
     end
   end
