9f6f253880dd42dd33ea6fe8ca7d7ccf218a73d5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-01 21:36:44 +0000

    gather 1.11.11

--- Casks/g/gather.rb
+++ Casks/g/gather.rb
@@ -1,9 +1,9 @@
 cask "gather" do
   arch arm: "-arm64"
 
-  version "1.10.1"
-  sha256 arm:   "75f1a28c7e035eda0d93f39fc291de150e99f79a3bed86b59bfe7b0bc1e18d66",
-         intel: "ca763c9fc25dc35e9bc4bf69013c3893bbc419a893c686924734e4c06bfd57ca"
+  version "1.11.11"
+  sha256 arm:   "7e9723b728a8f97c8b108ca2e32e7519679ba399f324f6ec4a77d355b3e1d4b2",
+         intel: "0a65406d42cd052e733f219f0928c1d0baa6611e28b661df6686e544b028aae8"
 
   url "https://github.com/gathertown/gather-town-desktop-releases/releases/download/v#{version}/Gather-#{version}#{arch}-mac.zip",
       verified: "github.com/gathertown/gather-town-desktop-releases/"
