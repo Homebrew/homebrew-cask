d94cf40c8277a4d27d333d20e23b36f4142a2973
SeongTae Jeong <seongtaejg@gmail.com>
2024-08-01 19:25:01 +0900

    Update db-browser-for-sqlite.rb
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/d/db-browser-for-sqlite.rb
+++ Casks/d/db-browser-for-sqlite.rb
@@ -6,7 +6,8 @@ cask "db-browser-for-sqlite" do
     sha256 arm:   "0c2076e4479cb9db5c85123cfe9750641f92566694ff9f6c99906321a2c424e8",
            intel: "546d57b6c88c2be7517759c016c0bf0313dfcc14adfcb43967f3c5d24657f366"
 
-    url "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v#{version}/DB.Browser.for.SQLite#{arch}-#{version}.dmg"
+    url "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v#{version}/DB.Browser.for.SQLite#{arch}-#{version}.dmg",
+        verified: "github.com/sqlitebrowser/sqlitebrowser/"
 
     livecheck do
       skip "Legacy version"
