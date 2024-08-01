d5e70f76ae0a8dbd351659ce434968242512508b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-01 12:46:09 +0000

    flipper 0.261.0

--- Casks/f/flipper.rb
+++ Casks/f/flipper.rb
@@ -1,9 +1,9 @@
 cask "flipper" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.260.0"
-  sha256 arm:   "b3b6cbbb1f6f51276a5f94f66d7728315425695c2343be3fe56ffe62700c8d3a",
-         intel: "b4f6412b8a268635fbbd99d5cc48a55a322cb3ff74b0cdfa1d2b77e8cc360d31"
+  version "0.261.0"
+  sha256 arm:   "3af66cd7c509b1ee2eec0966fc520726877aa74900822bf0b050f575a4cfa961",
+         intel: "000c9a1cbe1c94ed1ac8094fa2aab59630e53302179f1d070550832ce7c0c6a5"
 
   url "https://github.com/facebook/flipper/releases/download/v#{version}/Flipper-server-mac-#{arch}.dmg",
       verified: "github.com/facebook/flipper/"
