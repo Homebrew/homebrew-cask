43f978a83558bee53f66cdd239db07046452e96b
James Kerrane <hi@jameskerrane.com>
2024-08-02 14:59:04 -0600

    meta-quest-developer-hub: Update download URL, add sha256
    Note by @khipp (https://github.com/Homebrew/homebrew-cask/pull/181373#discussion_r1701539982): If this doesn't continue to work on the next version bump, we will need to also capture the d7fbf9110e4200cae75755db8cff6d5f part of the url in the livecheck.
    
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/m/meta-quest-developer-hub.rb
+++ Casks/m/meta-quest-developer-hub.rb
@@ -1,8 +1,8 @@
 cask "meta-quest-developer-hub" do
   version "4.8.0"
-  sha256 :no_check
+  sha256 "c32c569b0d2a803d122de2a2a281df041d368586aa7c61d18fda9821622ed89b"
 
-  url "https://securecdn.oculus.com/binaries/download/?id=26027232766892142"
+  url "https://www.oculus.com/x2asset/electron-apps/odh/d7fbf9110e4200cae75755db8cff6d5f/Meta%20Quest%20Developer%20Hub-#{version}.zip"
   name "meta-quest-developer-hub"
   desc "VR development tool"
   homepage "https://developer.oculus.com/meta-quest-developer-hub/"
