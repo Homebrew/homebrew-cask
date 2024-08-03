05d951e7bdd86182e8b053ea43ac5ed2cc05c1c4
Alin Lupascu <lupascu.marius@gmail.com>
2024-08-02 22:54:02 -0600

    Update pearcleaner.rb

--- Casks/p/pearcleaner.rb
+++ Casks/p/pearcleaner.rb
@@ -2,19 +2,23 @@ cask "pearcleaner" do
   version "3.8.3"
   sha256 "c7767d20fdb5ca544a061f1936ae9202922d25069eba85e83cdf770aaa838f2b"
 
-  url "https://github.com/alienator88/Pearcleaner/releases/download/#{version}/Pearcleaner.zip"
+  url "https://github.com/alienator88/Pearcleaner/releases/download/#{version}/Pearcleaner.zip",
+      verified: "github.com/alienator88/Pearcleaner/"
   name "Pearcleaner"
-  desc "Utility to cleanly uninstall apps and remove leftover files from old/uninstalled apps"
+  desc "Utility to uninstall apps and remove leftover files from old/uninstalled apps"
   homepage "https://itsalin.com/appInfo/?id=pearcleaner"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   depends_on macos: ">= :ventura"
 
   app "Pearcleaner.app"
 
-  uninstall quit:      "com.alienator88.Pearcleaner",
-            launchctl: "com.alienator88.PearcleanerSentinel"
+  uninstall launchctl: "com.alienator88.PearcleanerSentinel",
+            quit:      "com.alienator88.Pearcleaner"
 
-  zap trash: [
-    "~/Library/Application Support/Pearcleaner",
-  ]
+  zap trash: "~/Library/Application Support/Pearcleaner"
 end
