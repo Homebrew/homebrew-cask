5c90aa73f5e5f99d80b3b88cba3c3b30554e62a2
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-08-02 21:26:27 -0400

    boosteroid 1.9.1

--- Casks/b/boosteroid.rb
+++ Casks/b/boosteroid.rb
@@ -2,7 +2,7 @@ cask "boosteroid" do
   arch arm: "arm64", intel: "x64"
   folder = on_arch_conditional arm: "_ARM"
 
-  version "1.9.0"
+  version "1.9.1"
   sha256 :no_check
 
   url "https://boosteroid.com/macos#{folder}/installer/boosteroid-install-#{arch}.dmg",
