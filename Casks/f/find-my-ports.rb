bb3c5cbc0b74321da34c8b1f3fa14612a7249cb6
HeiTang <heitang.me@gmail.com>
2024-08-04 19:57:04 +0800

    find-my-ports 1.4.0

---
+++ Casks/f/find-my-ports.rb
@@ -0,0 +1,30 @@
+cask "find-my-ports" do
+  version "1.4.0,KmCUblOLkztgxfVd3uaPti8c5i6BnM"
+  sha256 "da35357bc51cc776854b60606f36258b70fb4d350ae7330f17f602beed285753"
+
+  url "https://12tlni7vrjp0srft.public.blob.vercel-storage.com/find-my-ports/Find%20My%20Ports%20#{version.csv.first}-#{version.csv.second}.dmg",
+      verified: "12tlni7vrjp0srft.public.blob.vercel-storage.com"
+  name "find-my-ports"
+  desc "Manage all your open development ports and remote Vercel deployment"
+  homepage "https://www.findmyports.com/"
+
+  livecheck do
+    url "https://www.findmyports.com/download"
+    strategy :page_match do |page|
+      match = page.match(%r{href=.*?/Find%20My%20Ports%20(\d+(?:\.\d+)*)-([a-zA-Z0-9]+)\.dmg}i)
+      "#{match[1]},#{match[2]}" if match
+    end
+  end
+
+  depends_on macos: ">= :sonoma"
+
+  app "Find My Ports.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.benny.FindMyPorts-Helper",
+    "~/Library/Application Support/Find My Ports",
+    "~/Library/Caches/com.apple.nsurlsessiond/Downloads/com.benny.FindMyPorts",
+    "~/Library/Containers/com.benny.FindMyPorts-Helper",
+    "~/Library/Preferences/com.benny.FindMyPorts.plist",
+  ]
+end
