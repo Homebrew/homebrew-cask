134a24816cd5f869e9c60df8ad21043488077391
Casper Rogild Storm <casper+github@rogildstorm.com>
2024-08-04 22:48:31 +0200

    halloy 2024.10

--- Casks/h/halloy.rb
+++ Casks/h/halloy.rb
@@ -1,6 +1,6 @@
 cask "halloy" do
-  version "2024.9"
-  sha256 "f0cbf8c2dd2caf990b9795735d05ef5d975bf334fe2e2aab34a1980cc442cbe3"
+  version "2024.10"
+  sha256 "af488f6cee9db2f7a018cd1ad3874301476e5b9691db0a2ab3339690ab2c0f1b"
 
   url "https://github.com/squidowl/halloy/releases/download/#{version}/halloy.dmg",
       verified: "github.com/squidowl/halloy/"
