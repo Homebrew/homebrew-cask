b5a32523bfca9877b403cb32d15f3ef780df1dcf
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-08-04 21:11:00 -0400

    openrct2 0.4.13

--- Casks/o/openrct2.rb
+++ Casks/o/openrct2.rb
@@ -15,8 +15,8 @@ cask "openrct2" do
         verified: "github.com/OpenRCT2/OpenRCT2/"
   end
   on_mojave :or_newer do
-    version "0.4.12"
-    sha256 "3f424fef9efac1412cc02e82e14e657f05dbd54d7ffa8a0fb8b4d14c59c6f182"
+    version "0.4.13"
+    sha256 "5a5ce5947b5dfcd471a53971b4065d865490c28254d6dfd2327512b375d46e84"
 
     url "https://github.com/OpenRCT2/OpenRCT2/releases/download/v#{version}/OpenRCT2-#{version}-macos-universal.zip",
         verified: "github.com/OpenRCT2/OpenRCT2/"
