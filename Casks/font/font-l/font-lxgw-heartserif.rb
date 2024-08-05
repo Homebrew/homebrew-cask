d9719ac85bd6ce1c3321b15fbbd38516683a9a4b
Bevan Kay <email@bevankay.me>
2024-08-05 13:08:50 +1000

    font-lxgw-heartserif 0.924.2

--- Casks/font/font-l/font-lxgw-heartserif.rb
+++ Casks/font/font-l/font-lxgw-heartserif.rb
@@ -1,11 +1,11 @@
 cask "font-lxgw-heartserif" do
-  version "0.924.1"
+  version "0.924.2"
   sha256 "ad5808fcef71962064804e01024b637bdea44e8b57a482d2048cc02e805d1182"
 
-  url "https://github.com/lxgw/LxgwNeoZhiSong/releases/download/v#{version}/LXGWHeartSerif.ttf"
+  url "https://github.com/lxgw/LxgwHeartSerif/releases/download/v#{version}/LXGWHeartSerif.ttf"
   name "LXGW Heart Serif"
   name "霞鹜铭心宋"
-  homepage "https://github.com/lxgw/LxgwNeoZhiSong"
+  homepage "https://github.com/lxgw/LxgwHeartSerif"
 
   font "LXGWHeartSerif.ttf"
 
