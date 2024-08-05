7e3afdf28feccd36ed8cb2d354e79b17b304e3d4
tiiiecherle <tiiiecherle@gmail.com>
2024-08-05 08:03:43 +0200

    orion 0.99,128.2.1
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/o/orion.rb
+++ Casks/o/orion.rb
@@ -1,8 +1,8 @@
 cask "orion" do
-  version "0.99,128.2"
+  version "0.99,128.2.1"
 
   on_mojave :or_older do
-    sha256 "e92bd55e164e2eebe817150945791ddd68bf543f4ed6a44340b56511799e88e1"
+    sha256 "61c5202748984b047c0c1367e36b37794caacc572f202f656ccbef04f4e01742"
 
     url "https://browser.kagi.com/updates/10_14/#{version.csv.second}.zip"
 
@@ -14,7 +14,7 @@ cask "orion" do
     depends_on macos: :mojave
   end
   on_catalina do
-    sha256 "bfea849fe58efb5e5594fd1f5acc1b93294c40bd01ab52c533e477578088031b"
+    sha256 "812de30d5179ef19544bb074de07df2222b6b35dc4cfdff1e1fd96eaccd9d60e"
 
     url "https://browser.kagi.com/updates/10_15/#{version.csv.second}.zip"
 
@@ -26,7 +26,7 @@ cask "orion" do
     depends_on macos: :catalina
   end
   on_big_sur do
-    sha256 "8880ee6ea9a0be33f306a4ea8f45c22d20c361c553f8ebae49459e99e29fc63c"
+    sha256 "54fcbb55ca6f3998adbbb49e10442369820d68b0fe026c3922399efad6d404fe"
 
     url "https://browser.kagi.com/updates/11_0/#{version.csv.second}.zip"
 
@@ -38,7 +38,7 @@ cask "orion" do
     depends_on macos: :big_sur
   end
   on_monterey do
-    sha256 "e183d81bd6926355340f9135dd111679fcca2a571a8b91de1ef9abbcc619eeed"
+    sha256 "0a3787994daaa98f4a9bc4719d463373a989e62e9258323cde12b0291ed0d49d"
 
     url "https://browser.kagi.com/updates/12_0/#{version.csv.second}.zip"
 
@@ -50,7 +50,7 @@ cask "orion" do
     depends_on macos: :monterey
   end
   on_ventura do
-    sha256 "60ce57d69ad709cac9fcb381d4845a7c4f355e351f54073b28baec02ab8b84a8"
+    sha256 "b1f8ad490664e96d3f435b7b95f192327507591a5b64365b1c19e00f28f93991"
 
     url "https://browser.kagi.com/updates/13_0/#{version.csv.second}.zip"
 
@@ -62,7 +62,7 @@ cask "orion" do
     depends_on macos: :ventura
   end
   on_sonoma :or_newer do
-    sha256 "c6306e3950e0d1d6465f0b8ec435232fc528e9bccae82e17f80bd718bf469beb"
+    sha256 "09ec195da5e1858ed63c2202cd63d5616183c056a6069f8cf4698701ebcd2d4c"
 
     url "https://browser.kagi.com/updates/14_0/#{version.csv.second}.zip"
 
