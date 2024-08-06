f4afaf243e3ac3aef73c21f227d5621fb8080f51
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-06 17:18:48 +0200

    loopback 2.4.3

--- Casks/l/loopback.rb
+++ Casks/l/loopback.rb
@@ -9,7 +9,7 @@ cask "loopback" do
     depends_on macos: ">= :big_sur"
   end
   on_sonoma :or_newer do
-    version "2.4.2"
+    version "2.4.3"
 
     url "https://cdn.rogueamoeba.com/loopback/download/Loopback-ARK.zip"
 
