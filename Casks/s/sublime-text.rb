ac978e3adc0f54edc8755917609f0829c6b4ce50
Marco Grassi <marco.gra@gmail.com>
2024-08-06 12:29:01 +0800

    sublime-text 4180

--- Casks/s/sublime-text.rb
+++ Casks/s/sublime-text.rb
@@ -1,7 +1,7 @@
 cask "sublime-text" do
   # NOTE: The first digit of the build number is the major version.
-  version "4169"
-  sha256 "e87c547c53f566b17a35a24b23c75896d503084fdfc84e472de4c3ef34e070a6"
+  version "4180"
+  sha256 "4ad971b932849ec5e40740e5052ee5108b0ed30867cbe68024d2e436a19e60d0"
 
   url "https://download.sublimetext.com/sublime_text_build_#{version}_mac.zip"
   name "Sublime Text"
