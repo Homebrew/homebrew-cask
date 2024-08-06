88058b760698644b1220e208ebf98f0eaff85493
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-06 18:40:36 +0200

    yealink-meeting 4.6.30

--- Casks/y/yealink-meeting.rb
+++ Casks/y/yealink-meeting.rb
@@ -1,6 +1,6 @@
 cask "yealink-meeting" do
-  version "4.6.26,a8e8023773218a38dbbc96a75392e767"
-  sha256 "6f61cec436108723cace15e59c302b730cb7dd1ca9b98556246c8a9463a2dd1f"
+  version "4.6.30,63c6f7424fad1db08db10b5025dbdb1a"
+  sha256 "24d704eac27337e918f87c7453f41c579ed074e33012d2a9dc2010231edf277e"
 
   url "https://download.ylyun.com/yealink-meeting/macos/latest/Yealink-Meeting.dmg?v=#{version.csv.second}"
   name "Yealink Meeting"
@@ -20,8 +20,4 @@ cask "yealink-meeting" do
     "~/Library/Application Support/Yealink Meeting",
     "~/Library/Application Support/YealinkSdk",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
