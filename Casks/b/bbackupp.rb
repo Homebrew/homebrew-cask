0389831028fb886f54ded2b8c01959304bf67af0
Zach <i@ssstttar.com>
2024-08-07 12:01:21 +0800

    bbackupp lastest (new cask)

---
+++ Casks/b/bbackupp.rb
@@ -0,0 +1,25 @@
+cask "bbackupp" do
+  version "2.13.73"
+  sha256 "f48b40c5d97d2f196730e51617de72b7d3526afcf5b8fd0e5fc67ba8a4807577"
+
+  url "https://github.com/Lakr233/BBackupp/releases/download/#{version}/BBackupp.zip"
+  name "BBackupp"
+  desc "Automated iOS Backup Robot"
+  homepage "https://github.com/Lakr233/BBackupp"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :ventura"
+
+  app "BBackupp.app"
+
+  zap trash: [
+    "~/Documents/BBackupp",
+    "~/Library/HTTPStorages/wiki.qaq.BBackupp",
+    "~/Library/HTTPStorages/wiki.qaq.BBackupp.binarycookies",
+    "~/Library/Preferences/wiki.qaq.BBackupp.plist",
+  ]
+end
