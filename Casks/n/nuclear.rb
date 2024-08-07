d01a9abeac3ec00ae596d84015c91db14c6adaea
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-08-06 20:31:46 -0400

    nuclear 0.6.36

--- Casks/n/nuclear.rb
+++ Casks/n/nuclear.rb
@@ -1,9 +1,9 @@
 cask "nuclear" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.6.35"
-  sha256 arm:   "e8fe79f5659069f885e680c257354ab8f855a2036cfcca2ac0ea3e419e104daf",
-         intel: "3a4017d96b5361f6d5697f5e3eae2a254a45b2d3beab9c061865bd97df72e4c7"
+  version "0.6.36"
+  sha256 arm:   "b9499b62fadb3d9050f9afb6085ff2bb21092cb2ef5c93f47e9a9fa3da185446",
+         intel: "5cf212272591f5bd4c40047b0391c787b5a0b02a476e0f4aff0cfdc0b80684ec"
 
   url "https://github.com/nukeop/nuclear/releases/download/v#{version}/nuclear-v#{version}-#{arch}.dmg",
       verified: "github.com/nukeop/nuclear/"
