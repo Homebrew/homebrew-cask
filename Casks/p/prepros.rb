c1bac57637836698788cbcb2e5018d642eec3568
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-07 23:57:02 +0200

    prepros: add arm arch

--- Casks/p/prepros.rb
+++ Casks/p/prepros.rb
@@ -1,15 +1,20 @@
 cask "prepros" do
-  version "7.26.0"
-  sha256 "5231ca24adaf7840a491a34fe1ab648e554ddee86db980d358b1b22d7c2947f8"
+  arch arm: "-Mac"
 
-  url "https://downloads.prepros.io/v#{version.major}/#{version}/Prepros-#{version}.zip"
+  version "7.26.0"
+  sha256 arm:   "8d638cc0947ed94950041740705a9a8b9d94f35424bc9dcc651ff16744c145be",
+         intel: "5231ca24adaf7840a491a34fe1ab648e554ddee86db980d358b1b22d7c2947f8"
+
+  url "https://downloads.prepros.io/v#{version.major}/#{version}/Prepros#{arch}-#{version}.zip"
   name "Prepros"
   desc "Web development companion"
   homepage "https://prepros.io/"
 
   livecheck do
-    url "https://prepros.io/downloads/stable/mac"
-    strategy :header_match
+    url "https://prepros.io/api/v#{version.major}/version/darwin/stable"
+    strategy :json do |json|
+      json.dig("data", "version")
+    end
   end
 
   depends_on macos: ">= :high_sierra"
@@ -22,8 +27,4 @@ cask "prepros" do
     "~/Library/Preferences/io.prepros.prepros.plist",
     "~/Library/Saved Application State/io.prepros.prepros.savedState",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
