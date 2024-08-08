5326ef8c7774f8dae7ce037a20ab18fe80848f93
Konstantin Vorobyov <vorobyov@gmail.com>
2024-08-08 18:56:13 +0600

    adds an arm version and removes some shared folder deletions that affect other vendors' applications

--- Casks/y/youtube-to-mp3.rb
+++ Casks/y/youtube-to-mp3.rb
@@ -1,9 +1,11 @@
 cask "youtube-to-mp3" do
   # NOTE: "3" is not a version number, but an intrinsic part of the product name
+  arch arm: "-arm"
+
   version "3.9.9.95"
   sha256 :no_check
 
-  url "https://www.mediahuman.net/files/YouTubeToMP3.dmg"
+  url "https://www.mediahuman.net/files/YouTubeToMP3#{arch}.dmg"
   name "MediaHuman YouTube to MP3 Converter"
   desc "Downloads music from playlists or channels"
   homepage "https://www.mediahuman.net/youtube-to-mp3/"
@@ -16,14 +18,10 @@ cask "youtube-to-mp3" do
   app "YouTube to MP3.app"
 
   zap trash: [
-    "~/Library/Application Support/MediaHuman/",
-    "~/Library/Caches/MediaHuman/",
+    "~/Library/Application Support/MediaHuman/YouTube to MP3/",
+    "~/Library/Caches/MediaHuman/YouTube to MP3/",
     "~/Library/Preferences/com.mediahuman.plist",
     "~/Library/Preferences/com.mediahuman.YouTube to MP3.plist",
     "~/Library/Saved Application State/com.mediahuman.YouTube to MP3.savedState",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
