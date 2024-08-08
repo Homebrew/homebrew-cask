90a5b765117d8fe726ddee8feb9c78819be4075d
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-08 13:35:21 +0200

    dash-dash 21.1.0

--- Casks/d/dash-dash.rb
+++ Casks/d/dash-dash.rb
@@ -1,9 +1,9 @@
 cask "dash-dash" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "21.0.2"
-  sha256 arm:   "66e9ea2a2aeebaa54a915a374c578bd6306da312c65af03e047f94268eb3981e",
-         intel: "06f798cfd59bc589c114fec838170ac4b261b3bd9dee649582b5baa2409c7777"
+  version "21.1.0"
+  sha256 arm:   "fb94a9213a8763ad2d290b8ddc63be5d63d94124c324387e3228b033dc368890",
+         intel: "ee72b4004186e00ce0ee5fca2c299842cb64a8622e4c09e0356fd75c60ffcf7e"
 
   url "https://github.com/dashpay/dash/releases/download/v#{version}/dashcore-#{version}-#{arch}-apple-darwin.dmg",
       verified: "github.com/dashpay/dash/"
