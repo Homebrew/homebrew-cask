2fc0905b6d66cf121621cf425ead07cac0305995
Yan Zhulanow <mail@yanex.org>
2024-08-09 03:50:27 +0900

    marta 0.8.2

--- Casks/m/marta.rb
+++ Casks/m/marta.rb
@@ -1,6 +1,6 @@
 cask "marta" do
-  version "0.8.1"
-  sha256 "0db364bcb0b2eaad0237c6f8fbca2178ce0469a2c042f1f73b9cd6558c44ca1f"
+  version "0.8.2"
+  sha256 "960f3529c099a6e1429dbb15ab120c09ab9d76c6424133a18bdb954c4465bdb6"
 
   url "https://updates.marta.sh/release/Marta-#{version}.dmg"
   name "Marta File Manager"
@@ -9,7 +9,7 @@ cask "marta" do
 
   livecheck do
     url "https://updates.marta.sh/release/appcast.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   depends_on macos: ">= :high_sierra"
