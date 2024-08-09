197cf320476e1eef468e9e99cbb2969076b8dc7a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-09 19:04:48 +0000

    sigmaos 1.17.0.6

--- Casks/s/sigmaos.rb
+++ Casks/s/sigmaos.rb
@@ -1,6 +1,6 @@
 cask "sigmaos" do
-  version "1.17.0.5"
-  sha256 "e28067143b907b3a5ec6735291a0ad674033844426899204d36af159615d87ea"
+  version "1.17.0.6"
+  sha256 "e35caa4b7ef664db22ca602113b05704032e2aa04a5cab88f142ad26c4d3e6cb"
 
   url "https://releases.sigmaos.com/SigmaOS-#{version}.dmg"
   name "SigmaOS"
