edc70f0b497790cc689897b47db92b3fb50c7fbf
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-08-08 22:35:21 -0400

    motion 0.83.0

--- Casks/m/motion.rb
+++ Casks/m/motion.rb
@@ -1,9 +1,9 @@
 cask "motion" do
   arch arm: "aarch64", intel: "amd64"
 
-  version "0.82.0"
-  sha256 arm:   "2ec28a0330930ab4ac95e53174394b006727d7d13eb2ecee16f37618ec0469fa",
-         intel: "f94a570e08b67bfeb8dae50ea1a30e12bca2e2425e0dbf3fdd1ff2e88a1f1f13"
+  version "0.83.0"
+  sha256 arm:   "9fd94189624c6208f6d856440715ee04c0d4885953e1479723e1b5539c2736ec",
+         intel: "e13c1567fa18dbdca191f50126208116cfd56702c61715ac7aefb7e8785289b7"
 
   url "https://github.com/usemotion/desktopapp/releases/download/#{version}/motion-#{version}-mac-#{arch}.zip",
       verified: "github.com/usemotion/desktopapp/"
