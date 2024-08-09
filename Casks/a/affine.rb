56d1527cdf6937ec3f28d17b3847e0c6618addb8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-09 06:34:22 +0000

    affine 0.16.0

--- Casks/a/affine.rb
+++ Casks/a/affine.rb
@@ -1,9 +1,9 @@
 cask "affine" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.15.7"
-  sha256 arm:   "7c388574a89b89a57c34351fb07aed0c974bfd8b0ba7203e5398883f973026be",
-         intel: "b10cb7b5af505096e44da34c4e536adc332ab92b52d1b9f2c964e3c440836cd0"
+  version "0.16.0"
+  sha256 arm:   "308e55a985bd210afb45a6edb8ad71759d1bbdb993b2a930e8962fc315d1b6fb",
+         intel: "d27aa85d4e75302cc2de6d5477b949dad460a44005986124efbfcf7bc9f224e7"
 
   url "https://github.com/toeverything/AFFiNE/releases/download/v#{version}/affine-#{version}-stable-macos-#{arch}.zip",
       verified: "github.com/toeverything/AFFiNE/"
