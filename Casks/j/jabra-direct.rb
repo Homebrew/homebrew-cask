c5b85a2dadde35825c2b1c3ff5cac5ab9334d323
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-08-08 22:34:28 -0400

    jabra-direct 6.17.21301

--- Casks/j/jabra-direct.rb
+++ Casks/j/jabra-direct.rb
@@ -1,5 +1,5 @@
 cask "jabra-direct" do
-  version "6.16.15901"
+  version "6.17.21301"
   sha256 :no_check
 
   url "https://jabraxpressonlineprdstor.blob.core.windows.net/jdo/JabraDirectSetup.dmg",
