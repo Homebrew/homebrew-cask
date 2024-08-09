7321986f1e3bc9491b985609fc70b9d2b7248dc2
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-09 18:41:53 +0200

    kindle-comic-creator: update livecheck

--- Casks/k/kindle-comic-creator.rb
+++ Casks/k/kindle-comic-creator.rb
@@ -6,11 +6,11 @@ cask "kindle-comic-creator" do
       verified: "kc2.s3.amazonaws.com/"
   name "Kindle Comic Creator"
   desc "Turns comics, graphic novels and manga into Kindle books"
-  homepage "https://www.amazon.com/gp/feature.html?ie=UTF8&docId=1001103761"
+  homepage "https://www.amazon.com/b?node=23496309011"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url :homepage
+    regex(/v?(\d+(?:\.\d+)+)\s+for\s+Intel\s+Mac/i)
   end
 
   pkg "Kindle Comic Creator.pkg"
