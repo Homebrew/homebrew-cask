f2e682bf7421e36521445e85c6a336edb4e56917
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-11 09:56:51 +0200

    touch-portal: update livecheck

--- Casks/t/touch-portal.rb
+++ Casks/t/touch-portal.rb
@@ -1,7 +1,7 @@
 cask "touch-portal" do
   arch arm: "M1"
 
-  version "4.2.0"
+  version "4.2,1"
   sha256 :no_check
 
   url "https://www.touch-portal.com/downloads/releases/mac/TouchPortalMacRelease#{arch}.dmg"
@@ -10,8 +10,11 @@ cask "touch-portal" do
   homepage "https://www.touch-portal.com/"
 
   livecheck do
-    url :url
-    strategy :extract_plist
+    url :homepage
+    regex(/v?(\d+(?:\.\d+)+).*?Build\s+(\d+)/i)
+    strategy :page_match do |page, regex|
+      page&.scan(regex)&.map { |match| "#{match[0]},#{match[1]}" }
+    end
   end
 
   depends_on macos: ">= :el_capitan"
