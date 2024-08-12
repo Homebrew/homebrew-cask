7e583ad64270c1c5104d38e5fab65fa914cd7ddd
Bevan Kay <email@bevankay.me>
2024-08-12 14:36:27 +1000

    chatwork 2.9.0

--- Casks/c/chatwork.rb
+++ Casks/c/chatwork.rb
@@ -1,7 +1,7 @@
 cask "chatwork" do
   livecheck_arch = on_arch_conditional arm: "arm64", intel: "x64"
 
-  version "2.8.4"
+  version "2.9.0"
   sha256 :no_check
 
   url "https://desktop-app.chatwork.com/installer/ChatWork.dmg"
