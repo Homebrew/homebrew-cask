3e5d318aca21527e699e85029446ca5267ec4917
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-12 09:39:44 +0200

    the-battle-for-wesnoth 1.18.2

--- Casks/t/the-battle-for-wesnoth.rb
+++ Casks/t/the-battle-for-wesnoth.rb
@@ -1,6 +1,6 @@
 cask "the-battle-for-wesnoth" do
-  version "1.18.1"
-  sha256 "54e1cc4f61a5b4269872d7a2dacd449f3cd27b872f586147199b2e2b629b671f"
+  version "1.18.2"
+  sha256 "d53dc8ab64a59e69736bc4e1087e0fc69df5187be9bb3b9c01a86f58a81221af"
 
   url "https://downloads.sourceforge.net/wesnoth/Wesnoth_#{version}.dmg",
       verified: "sourceforge.net/wesnoth/"
