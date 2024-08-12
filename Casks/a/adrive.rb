f2d99692a98a11f9e2a12408c2dafea32167b7ae
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-12 13:08:26 +0000

    adrive 6.2.0

--- Casks/a/adrive.rb
+++ Casks/a/adrive.rb
@@ -1,9 +1,9 @@
 cask "adrive" do
   arch arm: "-arm64"
 
-  version "6.1.0"
-  sha256 arm:   "39f00f245f6bebddfa30010233c4a3d5e731b7d350317dbc3b54c18fdfe8c34d",
-         intel: "fcc8357e11b04de3ff8ea1222c4829bea23e2e75927f60cf9feeba7e42519960"
+  version "6.2.0"
+  sha256 arm:   "eff132b7f9c1fbb80190a4714d93bc39f2adebbf97faed2a8c2d1a48f0fe10aa",
+         intel: "35131acfd0d48bd7c0445d7ed966f4b39236ca89ce05b495b27a63f75a1cc2af"
 
   url "https://cdn.aliyundrive.net/downloads/apps/desktop/aDrive-#{version}#{arch}.dmg",
       verified:   "cdn.aliyundrive.net/",
