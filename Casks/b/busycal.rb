38b10da1a84c4e65a2c5bd25f2730c315c92bd65
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-13 16:43:17 +0200

    busycal: update livecheck and depends_on

--- Casks/b/busycal.rb
+++ Casks/b/busycal.rb
@@ -1,21 +1,22 @@
 cask "busycal" do
-  version "2024.3.5"
+  version "2024.3.5,2024-08-13-07-54"
   sha256 "43b7cc0794ea72196310e21908dcf5ee28d05454b824a948a5286b3cc8fb3e7f"
 
-  url "https://www.busymac.com/download/bcl-#{version}.zip"
+  url "https://downloads.busymac.com/bcl-#{version.csv.first}-#{version.csv.second}.zip"
   name "BusyCal"
   desc "Calendar software focusing on flexibility and reliability"
   homepage "https://busymac.com/busycal/index.html"
 
   livecheck do
-    url "https://versioncheck.busymac.com/busycal/news.plist"
-    strategy :xml do |xml|
-      xml.elements["//dict/key[text()='current']"]&.next_element&.text&.strip
+    url "https://www.busymac.com/download/BusyCal.zip"
+    regex(%r{/bcl[._-]v?(\d+(?:\.\d+)+)[._-](\d+(?:-\d+)+)\.zip}i)
+    strategy :header_match do |headers, regex|
+      headers["location"]&.scan(regex)&.map { |match| "#{match[0]},#{match[1]}" }
     end
   end
 
   auto_updates true
-  depends_on macos: ">= :el_capitan"
+  depends_on macos: ">= :catalina"
 
   pkg "BusyCal Installer.pkg"
 
