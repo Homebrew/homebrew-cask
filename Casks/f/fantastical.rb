59e9c958d410986cc75c71644886f704e1e28e2d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-13 15:38:54 +0000

    fantastical 3.8.22

--- Casks/f/fantastical.rb
+++ Casks/f/fantastical.rb
@@ -1,6 +1,6 @@
 cask "fantastical" do
-  version "3.8.21"
-  sha256 "8fa74f5fa8024ea54d7c7063b054391d1a2b81abda8ec6010324ac105e4f15f4"
+  version "3.8.22"
+  sha256 "728fa4747c22e9fcc91835937ed0f798cc473c1a6a7a55f203cb2f5b74bc1446"
 
   url "https://cdn.flexibits.com/Fantastical_#{version}.zip"
   name "Fantastical"
