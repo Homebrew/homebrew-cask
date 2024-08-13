d69c7caef648295fff79e375ddbc9d8035a7a35a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-13 21:48:17 +0000

    microsoft-edge@dev 129.0.2766.0

--- Casks/m/microsoft-edge@dev.rb
+++ Casks/m/microsoft-edge@dev.rb
@@ -2,12 +2,12 @@ cask "microsoft-edge@dev" do
   linkid = on_arch_conditional arm: "2099619", intel: "2069340"
 
   on_arm do
-    version "129.0.2752.4,9c18c979-d230-4e05-81f2-48b72e6479c0"
-    sha256 "cae6af11651a21c34f8ac5c08805112076643c71a44b668655202183f351ab9b"
+    version "129.0.2766.0,d46c7da1-d454-4992-ba5f-dcc534bbbeb9"
+    sha256 "a4c04072e38aeab0903eff32e0445289d8e8568a460dcf3d25a6416e43d712f7"
   end
   on_intel do
-    version "129.0.2752.4,ac3d8e98-f0bd-416a-9301-b574fe65bc91"
-    sha256 "7a375cb226813eee9d2614558ed27bd8ab506eec6210d01d81f03f5698d74841"
+    version "129.0.2766.0,65f0b393-6bb8-486c-b0f8-41fee16493e0"
+    sha256 "fb21d6362b9100fb57245cfbd50506bb4c92ef31992a67f7c90a0c6d5a69d26b"
   end
 
   url "https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/#{version.csv.second}/MicrosoftEdgeDev-#{version.csv.first}.pkg"
