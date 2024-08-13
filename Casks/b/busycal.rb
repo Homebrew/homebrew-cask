1997ed989755ecb2b71e7cc3e4aace388beac383
Wes Bradley <work+github@wesbradley.com>
2024-08-13 08:51:45 -0500

    fix: update Busycal sha256 sum for v2024.3.5

--- Casks/b/busycal.rb
+++ Casks/b/busycal.rb
@@ -1,6 +1,6 @@
 cask "busycal" do
   version "2024.3.5"
-  sha256 "ca7a155eb1db10ff794dfc0259aba8d031f9e43ea171c4097a63205da0a3c0ef"
+  sha256 "43b7cc0794ea72196310e21908dcf5ee28d05454b824a948a5286b3cc8fb3e7f"
 
   url "https://www.busymac.com/download/bcl-#{version}.zip"
   name "BusyCal"
