8058cd032381dcf4dc357b7a053a2932d31ed085
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-14 18:55:27 +0000

    loom 0.247.3

--- Casks/l/loom.rb
+++ Casks/l/loom.rb
@@ -1,9 +1,9 @@
 cask "loom" do
   arch arm: "-arm64"
 
-  version "0.247.1"
-  sha256 arm:   "33b609212c2caceb997af6d139e63c0c9018645ba8ec4b234bd23375cd7089c9",
-         intel: "7bee9f61f903cdfc98aa2b6da582d930c8ac3804415f19c3cc3e7f0c25e3dde9"
+  version "0.247.3"
+  sha256 arm:   "049bf0ca2ddbabeaea8bdb019774b671552da3d1cde1267b6a35ab3669f99788",
+         intel: "4d465d8574f5260f032aa8b563610d1d6bb640238aa8bc711a1135c0419e53de"
 
   url "https://packages.loom.com/desktop-packages/Loom-#{version}#{arch}.dmg"
   name "Loom"
