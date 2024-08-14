ec848e0d4287e8a026f03b075f4c939aa4280e3f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-14 00:52:43 +0000

    ibkr 0.09.0v

--- Casks/i/ibkr.rb
+++ Casks/i/ibkr.rb
@@ -1,7 +1,7 @@
 cask "ibkr" do
   arch arm: "-arm", intel: "x-x64"
 
-  version "0.09.0u"
+  version "0.09.0v"
   sha256 :no_check
 
   url "https://download2.interactivebrokers.com/installers/ntws/latest-standalone/ntws-latest-standalone-macos#{arch}.dmg"
