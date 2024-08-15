8f1066838cd7496bb7f59e9c39b1c3a2cf3862e0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-15 15:37:15 +0000

    fleet 1.39.110

--- Casks/f/fleet.rb
+++ Casks/f/fleet.rb
@@ -2,9 +2,9 @@ cask "fleet" do
   arch arm: "aarch64", intel: "x64"
   arch_suffix = on_arch_conditional arm: "-aarch64"
 
-  version "1.38.89"
-  sha256 arm:   "edbf866f2998ff0ddaab36f2ea0919072a71dac19233577cf1eba0ac511c5a31",
-         intel: "52d079daf2317df29a86f1ade68bc63841e22429879242a7bfc4a4f5991a679d"
+  version "1.39.110"
+  sha256 arm:   "87f219ac521585691e0e766c82a5bd38a7f79a5a9ada85281492f44815728160",
+         intel: "bcff54ad06b88de8add428c99567f1b70a9263ae8abd1e01b44dbbde25b64420"
 
   url "https://download.jetbrains.com/fleet/installers/macos_#{arch}/Fleet-#{version}#{arch_suffix}.dmg"
   name "Fleet"
