600e66388c668514debf7519e65e1906feff57fe
Sebastian Sutter <mail@sutterseba.de>
2024-08-15 14:10:11 +0200

    bitbox: update url
    The BitBox organisation name on GitHub moved from digitalbitbox to
    BitBoxSwiss: https://github.com/BitBoxSwiss.

--- Casks/b/bitbox.rb
+++ Casks/b/bitbox.rb
@@ -2,8 +2,8 @@ cask "bitbox" do
   version "4.43.0"
   sha256 "6c35993d51a905fd75c8c331693289630e6323efa99ac952d37da6d638b73146"
 
-  url "https://github.com/digitalbitbox/bitbox-wallet-app/releases/download/v#{version}/BitBox-#{version}-macOS.dmg",
-      verified: "github.com/digitalbitbox/bitbox-wallet-app/releases/download/"
+  url "https://github.com/BitBoxSwiss/bitbox-wallet-app/releases/download/v#{version}/BitBox-#{version}-macOS.dmg",
+      verified: "github.com/BitBoxSwiss/bitbox-wallet-app/releases/download/"
   name "BitBox"
   desc "Protect your coins with the latest Swiss made hardware wallet"
   homepage "https://bitbox.swiss/"
