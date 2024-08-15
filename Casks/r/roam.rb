cae453699c52580349ea0c90cca383482a7eac29
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-15 09:01:26 +0200

    roam 122.0.1-beta001

--- Casks/r/roam.rb
+++ Casks/r/roam.rb
@@ -1,9 +1,9 @@
 cask "roam" do
   arch arm: "arm64", intel: "x64"
 
-  version "121.0.0-beta001"
-  sha256 arm:   "09dac900e0ef035d92551902552bac83845b69185f2a78d3ca657779052530c0",
-         intel: "4733d24b55d394a5de14db7d15d0c2c57f8a2c6d09ff1f9cf7c453b11328b762"
+  version "122.0.1-beta001"
+  sha256 arm:   "eacfdaf9022e59152510d8d230310d593531a22fd6d72a76a66b6c455d4d7241",
+         intel: "a258088c46be5167c6cfe4e9415fef4bb2f5f0a8418e1f25c9ef1643ae0bf5a4"
 
   url "https://download.ro.am/Roam/8a86d88cfc9da3551063102e9a4e2a83/latest/darwin/#{arch}/Roam.dmg"
   name "Roam"
