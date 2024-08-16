96c06b5dabe89be68c626dfb60fade287cc39f15
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-16 12:45:24 +0000

    dcp-o-matic-encode-server 2.16.91

--- Casks/d/dcp-o-matic-encode-server.rb
+++ Casks/d/dcp-o-matic-encode-server.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-encode-server" do
-  version "2.16.90"
-  sha256 "81b0c0dde65ca1aaa569471b748a5d681b1ffa608a5bb7a469ea5ab21ba60b74"
+  version "2.16.91"
+  sha256 "c0896bcf084a6c5ae70fe75caf9433fd106b533a051cbc2e6b88a1a2485b673c"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-server&version=#{version}"
   name "DCP-o-matic Encode Server"
