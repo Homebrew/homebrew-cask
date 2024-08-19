8a1bdfc346df5f53cef96524dbfc7e3598ae1ae0
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-19 23:04:51 +0200

    sync 2.2.44

--- Casks/s/sync.rb
+++ Casks/s/sync.rb
@@ -1,6 +1,6 @@
 cask "sync" do
-  version "2.2.42"
-  sha256 "11f45fd95ee37611bf84d95e33515f2dc1540d3a37ca2fd1bfea7a123c08fbf1"
+  version "2.2.44"
+  sha256 "38218b28fb307835b6d4f153a4ee21bcea4a3577651f082bd77bd976ea1ddc39"
 
   url "https://www.sync.com/download/apple/Sync-#{version}.dmg"
   name "Sync"
