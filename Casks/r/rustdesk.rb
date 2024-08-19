43ae8b3b228fcafe266225e2d7102a44517cd68d
djerfy <djerfy@gmail.com>
2024-08-19 19:27:29 +0200

    rustdesk 1.3.0

--- Casks/r/rustdesk.rb
+++ Casks/r/rustdesk.rb
@@ -1,9 +1,9 @@
 cask "rustdesk" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.2.7"
-  sha256 arm:   "06e90e7babf0613f6047c9ba0b7c2cba3355a3b57262a6b2cdc8f3b28714a992",
-         intel: "d9f04c0418da99e1289178647bfd6cc39803899f038d23439c8375cc8cee7a97"
+  version "1.3.0"
+  sha256 arm:   "6a183f775ab53a26ca6af49818201a56771f35d9648ad82cfe85460b49606a73",
+         intel: "d3ce403b8f98e5d6bce33bdc48a1d9b264d06edec89a7bf3237fb3e3b76fd93b"
 
   url "https://github.com/rustdesk/rustdesk/releases/download/#{version}/rustdesk-#{version}-#{arch}.dmg",
       verified: "github.com/rustdesk/rustdesk/"
