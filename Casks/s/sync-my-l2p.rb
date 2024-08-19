361d0dbfce21e487682ce0d41bbe953bb25e8717
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-19 08:37:39 +0200

    sync-my-l2p: add rosetta caveat

--- Casks/s/sync-my-l2p.rb
+++ Casks/s/sync-my-l2p.rb
@@ -8,5 +8,11 @@ cask "sync-my-l2p" do
   desc "Synchronises your documents from the L2P and Moodle of RWTH Aachen"
   homepage "https://www.syncmyl2p.de/"
 
+  depends_on macos: ">= :high_sierra"
+
   app "Sync-my-L2P.app"
+
+  caveats do
+    requires_rosetta
+  end
 end
