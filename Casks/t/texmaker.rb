8efc9c0302fefa95e1760d854ce593ac188b683e
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-19 23:15:30 +0200

    texmaker 6.0.0

--- Casks/t/texmaker.rb
+++ Casks/t/texmaker.rb
@@ -1,6 +1,6 @@
 cask "texmaker" do
-  version "5.1.4"
-  sha256 "4fb2896712f3aee93c0aae8f940dafab66c04334a76e055e1366adb21550f1fe"
+  version "6.0.0"
+  sha256 "8b67e6248a5e55c03790cb4854cc2c475bd35d29ae1a5526ee33956b9b1832df"
 
   url "https://www.xm1math.net/texmaker/assets/files/texmaker-#{version}.dmg"
   name "Texmaker"
@@ -18,8 +18,4 @@ cask "texmaker" do
     "~/Library/Preferences/texmaker.plist",
     "~/Library/Saved Application State/texmaker.savedState",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
