9baf52b3bfa6539eecdc7c2e9a3bc7dc035fd289
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-19 09:36:50 +0000

    elephicon 3.0.5

--- Casks/e/elephicon.rb
+++ Casks/e/elephicon.rb
@@ -1,9 +1,9 @@
 cask "elephicon" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.0.4"
-  sha256 arm:   "b183b3dea2ebd3c6558387ecc623a288f8186f3544bc529aa6d4fa49e42a4674",
-         intel: "ad6f853effd32343aee4bee0efc5e0f36c9d1ad141619d64aac146968b636159"
+  version "3.0.5"
+  sha256 arm:   "6b8a2a76d704486db120b0a2f6de82713e29aa00de2261fc25ae9f18fa8a1736",
+         intel: "605d6759121a5bec13c82bd0d53ae1f962f88f083f8bfd4f43142816c80ec41f"
 
   url "https://github.com/sprout2000/elephicon/releases/download/v#{version}/Elephicon-#{version}-darwin-#{arch}.dmg"
   name "Elephicon"
