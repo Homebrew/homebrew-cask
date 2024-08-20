bf8d270c0ea89bef6da8adf319f648c63e4c6bf6
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-20 09:57:20 +0200

    ea 13.277.0.5790

--- Casks/e/ea.rb
+++ Casks/e/ea.rb
@@ -1,5 +1,5 @@
 cask "ea" do
-  version "13.269.0.5782"
+  version "13.277.0.5790"
   sha256 :no_check
 
   url "https://origin-a.akamaihd.net/EA-Desktop-Client-Download/installer-releases/EA%20app.dmg",
