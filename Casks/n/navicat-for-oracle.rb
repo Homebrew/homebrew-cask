8452ff96f0ec55eeb7d1e763227ce7a464f0431b
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-21 13:36:43 +0200

    navicat-for-oracle 17.1.0

--- Casks/n/navicat-for-oracle.rb
+++ Casks/n/navicat-for-oracle.rb
@@ -1,8 +1,8 @@
 cask "navicat-for-oracle" do
-  version "17.0.14"
+  version "17.1.0"
   sha256 :no_check
 
-  url "https://dn.navicat.com/download/navicat#{version.major_minor.no_dots}_ora_en.dmg"
+  url "https://dn.navicat.com/download/navicat#{version.major}_ora_en.dmg"
   name "Navicat for Oracle"
   desc "Database administration and development tool for Oracle"
   homepage "https://www.navicat.com/products/navicat-for-oracle"
@@ -11,7 +11,7 @@ cask "navicat-for-oracle" do
     cask "navicat-premium"
   end
 
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= :big_sur"
 
   app "Navicat for Oracle.app"
 
