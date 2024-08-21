72116cfe5ff46634996e098dd1fc35355c80b113
Yuzu Jin <i@9902259.xyz>
2024-08-21 20:55:58 +0800

    kindle-create 1.93 (new cask)
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

---
+++ Casks/k/kindle-create.rb
@@ -0,0 +1,28 @@
+cask "kindle-create" do
+  version "1.93"
+  sha256 :no_check
+
+  url "https://d2bzeorukaqrvt.cloudfront.net/KindleCreateInstaller.pkg",
+      verified: "d2bzeorukaqrvt.cloudfront.net/"
+  name "Kindle Create"
+  desc "Creating beautiful books has never been easier"
+  homepage "https://www.amazon.com/Kindle-Create/b?node=18292298011"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
+  pkg "KindleCreateInstaller.pkg"
+
+  uninstall pkgutil: "com.amazon.kc"
+
+  zap trash: "~/Library/Saved Application State/com.amazon.kc.savedState"
+
+  caveats do
+    requires_rosetta
+  end
+end
