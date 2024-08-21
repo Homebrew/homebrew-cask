538cc36193a07d59a5a007dcab74973bc5cbe57b
David Lynch <kemayo@gmail.com>
2024-08-21 10:13:05 -0500

    elgato-control-center 1.7.1.20508

--- Casks/e/elgato-control-center.rb
+++ Casks/e/elgato-control-center.rb
@@ -1,8 +1,8 @@
 cask "elgato-control-center" do
-  version "1.7.20497"
-  sha256 "83931b9b7c9796b16547bb6afa7d016ffe4098020cfc75845631b79bf69a572c"
+  version "1.7.1.20508"
+  sha256 "8adc110411e409df00f99bfa4cbe67784793544c5d52cc4ca1886a5b0ffca1f3"
 
-  url "https://edge.elgato.com/egc/macos/eccm/#{version.major_minor}/ElgatoControlCenter-#{version}.app.zip"
+  url "https://edge.elgato.com/egc/macos/eccm/#{version.major_minor_patch}/ElgatoControlCenter-#{version}.app.zip"
   name "Elgato Control Center"
   desc "Control your Elgato key lights"
   homepage "https://www.elgato.com/ww/en/s/downloads"
