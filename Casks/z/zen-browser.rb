ea17af4fe82367cbf6bbfa32a747d4265f4b6654
Robert Boes <2871897+RobertBoes@users.noreply.github.com>
2024-08-21 16:52:45 +0200

    Fix code style

--- Casks/z/zen-browser.rb
+++ Casks/z/zen-browser.rb
@@ -1,7 +1,8 @@
 cask "zen-browser" do
   arch arm: "aarch64", intel: "x64"
+
   version "1.0.0-a.26"
-  sha256 arm: "acba4a1e47d49de0d9c3cb428ab30b4552eeb4ac4f5a4418952a1d484d56474b",
+  sha256 arm:   "acba4a1e47d49de0d9c3cb428ab30b4552eeb4ac4f5a4418952a1d484d56474b",
          intel: "25b4633da8b9622e8e8a671e7be9837fb9e17a5c571debc15749fb5ff0bc0397"
 
   url "https://github.com/zen-browser/desktop/releases/download/#{version}/zen.macos-#{arch}.dmg",
