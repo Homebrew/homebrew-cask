26430fa4d5b03da39d06ccaab2da167da68dfbf0
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-22 07:50:46 +0200

    meta-quest-developer-hub 4.9.0

--- Casks/m/meta-quest-developer-hub.rb
+++ Casks/m/meta-quest-developer-hub.rb
@@ -1,15 +1,21 @@
 cask "meta-quest-developer-hub" do
-  version "4.8.0"
-  sha256 "c32c569b0d2a803d122de2a2a281df041d368586aa7c61d18fda9821622ed89b"
+  version "4.9.0,173cd7ab2f5af21a4b91ddec26b4e12e"
+  sha256 "3219360e0ad59e33c6ff519c319ed0c07cc7511f6439df3c41edce2fdab6e171"
 
-  url "https://www.oculus.com/x2asset/electron-apps/odh/d7fbf9110e4200cae75755db8cff6d5f/Meta%20Quest%20Developer%20Hub-#{version}.zip"
+  url "https://www.oculus.com/x2asset/electron-apps/odh/#{version.csv.second}/Meta%20Quest%20Developer%20Hub-#{version.csv.first}.zip"
   name "meta-quest-developer-hub"
   desc "VR development tool"
   homepage "https://developer.oculus.com/meta-quest-developer-hub/"
 
   livecheck do
     url "https://www.oculus.com/electron-updates/mqdh/latest-mac.yml"
-    strategy :electron_builder
+    regex(%r{([^/]+)/Meta\s+Quest\s+Developer\s+Hub[._-]v?(\d+(?:\.\d+)+)\.zip}i)
+    strategy :electron_builder do |item|
+      match = item["path"]&.match(regex)
+      next if match.blank?
+
+      "#{match[2]},#{match[1]}"
+    end
   end
 
   depends_on macos: ">= :catalina"
