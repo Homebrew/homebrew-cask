14789762605077929043e12c415de9c2f7275e9b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-22 09:32:03 +0000

    bruno 1.26.1

--- Casks/b/bruno.rb
+++ Casks/b/bruno.rb
@@ -1,9 +1,9 @@
 cask "bruno" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.26.0"
-  sha256 arm:   "8ebc017d609f5b2c5be4f9eace2128474018094d245fa57f9960e1469cbe461e",
-         intel: "b1a5c9020dce7ae811fdf5aa907df711a4e4b51a8fe9390d2c5b997a7e662765"
+  version "1.26.1"
+  sha256 arm:   "321009a60d438002c7fc940252f3c83169bb233d4987d70f0e4fa2ea63a5fe82",
+         intel: "780f1e3732cf65cb1d9275fc73cd8c2fce9f0b6e6945b777a380e2a09e27b991"
 
   url "https://github.com/usebruno/bruno/releases/download/v#{version}/bruno_#{version}_#{arch}_mac.dmg",
       verified: "github.com/usebruno/bruno/"
