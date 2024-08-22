749e7e74d1ca6d273a5297d8c72855e6ca1e7950
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-22 07:24:44 +0200

    font-cubic-11 1.400

--- Casks/font/font-c/font-cubic-11.rb
+++ Casks/font/font-c/font-cubic-11.rb
@@ -1,13 +1,13 @@
 cask "font-cubic-11" do
-  version "1.300"
-  sha256 "1db4f204bbac140a386216ec7762eb616eb2cb67f8dc56d8de3f14de0d28b21a"
+  version "1.400"
+  sha256 "662749dc12204c76832883f7edf8523aeeda0a6f59b0b8ceb91d5c67cfa903a5"
 
   url "https://github.com/ACh-K/Cubic-11/archive/refs/tags/v#{version}.tar.gz"
   name "Cubic 11"
   name "俐方體11號"
   homepage "https://github.com/ACh-K/Cubic-11"
 
-  font "Cubic-11-#{version}/fonts/ttf/Cubic_11_#{version}_R.ttf"
+  font "Cubic-11-#{version}/fonts/ttf/Cubic_11.ttf"
 
   # No zap stanza required
 end
