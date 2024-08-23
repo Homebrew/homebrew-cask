e8b4a42e8c3a60542c325af40eb38a20f8986ad0
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-23 08:20:25 +0200

    moom: pad version, add depends_on

--- Casks/m/moom.rb
+++ Casks/m/moom.rb
@@ -1,8 +1,8 @@
 cask "moom" do
-  version "4.0.0"
+  version "4.0"
   sha256 "e3ae549b1f2916e7c5c2fc50c9141ca180fa9040e1bb9b219a7114c3556fca9e"
 
-  url "https://manytricks.com/download/_do_not_hotlink_/moom#{version.no_dots}.dmg"
+  url "https://manytricks.com/download/_do_not_hotlink_/moom#{version.no_dots.ljust(3, "0")}.dmg"
   name "Moom"
   desc "Utility to move and zoom windows—on one display"
   homepage "https://manytricks.com/moom/"
@@ -13,6 +13,7 @@ cask "moom" do
   end
 
   auto_updates true
+  depends_on macos: ">= :high_sierra"
 
   app "Moom.app"
 
