afa609280ffdf719c27f1fef1e7f0d017191add4
Sam Ford <1584702+samford@users.noreply.github.com>
2024-08-23 09:40:29 -0400

    moom 4.0.1
    Besides updating the cask to the newest release (4.0.1), this also
    updates the `livecheck` block to handle releases where the filename
    version differs from the `shortVersion` (e.g., moom400.dmg for 4.0).
    This will ensure that the cask uses a version that we can simply call
    `#no_dots` on without any issue. Besides that, this will account for
    omitted digits that aren't zero (unlike the previous `ljust(3, "0")`
    approach).
    
    One thing to note is that this only handles the situation where the
    filename has more digits than the `shortVersion`. There aren't any
    existing instances where the `shortVersion` is longer than the
    filename version but accounting for that is less straightforward, so
    I've simply left a `TODO` for if/when it becomes necessary in the
    future.

--- Casks/m/moom.rb
+++ Casks/m/moom.rb
@@ -1,15 +1,37 @@
 cask "moom" do
-  version "4.0"
-  sha256 "e3ae549b1f2916e7c5c2fc50c9141ca180fa9040e1bb9b219a7114c3556fca9e"
+  version "4.0.1"
+  sha256 "321118f6ff001c746e44b6e8ed6aecdf93e1c5837e992445e4c3776eef689740"
 
-  url "https://manytricks.com/download/_do_not_hotlink_/moom#{version.no_dots.ljust(3, "0")}.dmg"
+  url "https://manytricks.com/download/_do_not_hotlink_/moom#{version.no_dots}.dmg"
   name "Moom"
   desc "Utility to move and zoom windows—on one display"
   homepage "https://manytricks.com/moom/"
 
+  # Upstream has published releases where the version in the filename differs
+  # from the Sparkle shortVersion (e.g. moom400.dmg for 4.0), so we have to
+  # account for this mismatch.
   livecheck do
     url "https://manytricks.com/moom/appcast/"
-    strategy :sparkle, &:short_version
+    regex(/moom[._-]?v?(\d+(?:\.\d+)*)\.dmg/i)
+    strategy :sparkle do |item, regex|
+      dotless_short_version = DSL::Version.new(item.short_version).no_dots
+      next if dotless_short_version.blank?
+
+      file_version = item.url[regex, 1]&.tr(".", "")
+      next if file_version.blank?
+
+      if dotless_short_version == file_version
+        item.short_version
+      elsif dotless_short_version.length < file_version.length
+        # Naively add extra digits from the dotless file version to the end of
+        # the shortVersion
+        "#{item.short_version}.#{file_version.delete_prefix(dotless_short_version)}"
+      else
+        # TODO: Account for a filename version with fewer digits than the
+        # shortVersion if/when necessary in the future
+        next
+      end
+    end
   end
 
   auto_updates true
