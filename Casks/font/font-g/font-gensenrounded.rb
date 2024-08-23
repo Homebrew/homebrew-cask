8aa455b2315cabc78b17d43e272020590f547c69
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-23 08:25:46 +0200

    font-gensenrounded 2.100

--- Casks/font/font-g/font-gensenrounded.rb
+++ Casks/font/font-g/font-gensenrounded.rb
@@ -1,6 +1,6 @@
 cask "font-gensenrounded" do
-  version "2.000"
-  sha256 "82c439a688a7083c641164e09515c11dbe3ca23f8d9d4a8874f6bb06a8d028c0"
+  version "2.100"
+  sha256 "01f57ff5eac25143c654cfd37ab06d191703fb67aea94811ec48f101a2f6e203"
 
   url "https://github.com/ButTaiwan/gensen-font/releases/download/v#{version}/GenSenRounded#{version.major}-ttc.zip"
   name "GenSenRounded"
