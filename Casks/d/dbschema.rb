1bfd3b4f842c0e503ae066a88bbc7e3aa2980a22
Chris Coutinho <12901868+cbcoutinho@users.noreply.github.com>
2024-08-23 14:56:28 +0200

    Update sh256

--- Casks/d/dbschema.rb
+++ Casks/d/dbschema.rb
@@ -1,6 +1,6 @@
 cask "dbschema" do
   version "9.6.1"
-  sha256 "30bf470b85860984297e7aad0250964ab0cdae46673fd866f80fdbc4f8f58ba4" # See sha256sums at https://dbschema.com/download/sha256sums
+  sha256 "37b8ef2fa348a51bdf0cadd2fdfdd7a3c33e4eee81c4f60c16fe0f0e2a246f39" # See sha256sums at https://dbschema.com/download/sha256sums
   
   url "https://dbschema.com/download/DbSchema_macos_#{version.dots_to_underscores}.tgz"
   name "DbSchema"
