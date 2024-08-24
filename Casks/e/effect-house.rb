4d3d86ec6ea4c67e533d059f34ce9d8693dc5969
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-24 09:51:35 +0200

    effect-house 4.4.1

--- Casks/e/effect-house.rb
+++ Casks/e/effect-house.rb
@@ -2,12 +2,12 @@ cask "effect-house" do
   arch arm: "_M1_101", intel: "_Intel_101"
 
   on_arm do
-    version "4.4.0,2888,07302024"
-    sha256 "645a32dfc3e432dfad1f1701d3d8328d911a3e05d56c457e4000fbc4f87811bc"
+    version "4.4.1,1705,08142024"
+    sha256 "0c43a941bb891a95dddb8884e2822bbb0c25ae9c033ae85febc093138c0c4365"
   end
   on_intel do
-    version "4.4.0,2889,07302024"
-    sha256 "c080cabc8e411a4ed3e75b5c0128023482214dbe2bb1c45f3535b81a07c0dab5"
+    version "4.4.1,1709,08142024"
+    sha256 "7fac73e618cbe46969437ab3390e881f441c780a4dd3846ac8e3d518a45f4eb1"
   end
 
   url "https://sf16-va.tiktokcdn.com/obj/eden-va2/olaa_ajlmml_zlp/ljhwZthlaukjlkulzlp/V#{version.csv.first.no_dots}_External_Release_Builds_#{version.csv.third}/Effect_House_v#{version.csv.first}.#{version.csv.second}#{arch}.dmg",
