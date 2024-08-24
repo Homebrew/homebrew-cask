931e42e0417935aa375170313a23fc20a10aea5e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-24 18:32:54 +0000

    ankiapp 9.4.1

--- Casks/a/ankiapp.rb
+++ Casks/a/ankiapp.rb
@@ -1,6 +1,6 @@
 cask "ankiapp" do
-  version "9.4.0"
-  sha256 "c77cdb771ba1f71b99d356915fcc76605758bde2d0dc60342e69818c749123ec"
+  version "9.4.1"
+  sha256 "27ca3855bcb92891fef39128a7c411605dd58410daed995b758d2bcc934baaa8"
 
   url "https://updates.ankiapp.com/AnkiApp-#{version}-universal-mac.zip"
   name "AnkiApp"
