584a0a6dd5b060b0b24977e5231192cc1b53c849
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-08-25 08:58:48 -0400

    r-name: deprecate
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/r/r-name.rb
+++ Casks/r/r-name.rb
@@ -6,10 +6,7 @@ cask "r-name" do
   name "R-Name"
   homepage "https://www.jacek-dom.net/software/R-Name/"
 
-  livecheck do
-    url :homepage
-    regex(/version\s+.*?(\d+(?:\.\d+)+)/i)
-  end
+  deprecate! date: "2024-08-25", because: :unmaintained
 
   app "R-Name.app"
 
