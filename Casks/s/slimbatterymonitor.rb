f7a0f90820df8dc3a2f7f1cbbb61e8a3a89bdef0
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-08-25 11:57:56 -0400

    slimbatterymonitor: deprecate
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/s/slimbatterymonitor.rb
+++ Casks/s/slimbatterymonitor.rb
@@ -6,10 +6,7 @@ cask "slimbatterymonitor" do
   name "SlimBatteryMonitor"
   homepage "https://www.orange-carb.org/SBM/"
 
-  livecheck do
-    url "https://www.orange-carb.org/SBM/updates/sbm.xml"
-    strategy :sparkle
-  end
+  deprecate! date: "2024-08-25", because: :unmaintained
 
   app "SlimBatteryMonitor.app"
 
