3be737f44ec92959e20aa5c5f1a903477d146489
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-26 15:31:01 +0000

    atlauncher 3.4.37.1

--- Casks/a/atlauncher.rb
+++ Casks/a/atlauncher.rb
@@ -1,6 +1,6 @@
 cask "atlauncher" do
-  version "3.4.37.0"
-  sha256 "6d46cf793fff5f59c223ce39f6f25f89476fc7078175d705a2bd5cd36b04314b"
+  version "3.4.37.1"
+  sha256 "43d2838ab9614e4f2d03af63018137d3f402d120002eed95a08d4a3e800d0296"
 
   url "https://github.com/ATLauncher/ATLauncher/releases/download/v#{version}/ATLauncher-#{version}.zip",
       verified: "github.com/ATLauncher/ATLauncher/"
