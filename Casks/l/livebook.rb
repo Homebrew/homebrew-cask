6259d1ccc1eda49aa42732dc9b180d2773f0915c
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-08-26 10:39:51 -0400

    livebook 0.14.0

--- Casks/l/livebook.rb
+++ Casks/l/livebook.rb
@@ -1,6 +1,6 @@
 cask "livebook" do
-  version "0.13.3"
-  sha256 "5875bb0a0430c060e884189512319c70cd0af9d4b95c7a98ab7f7e4c8e70dcf2"
+  version "0.14.0"
+  sha256 "edbf8110be20e22d57ecca5296721a7ba39d088a814d0100bd6671f24618e0b3"
 
   url "https://github.com/livebook-dev/livebook/releases/download/v#{version}/LivebookInstall-v#{version}-macos-universal.dmg",
       verified: "github.com/livebook-dev/livebook/"
