a2ef9ea6a5f3b1022f80fbd4c5c13032b3abb171
Danny Su <danny.su@live.com>
2024-08-27 22:21:28 +0800

    Update eudic.rb
    Update the URL, the original URL points to an old version.

--- Casks/e/eudic.rb
+++ Casks/e/eudic.rb
@@ -2,7 +2,7 @@ cask "eudic" do
   version :latest
   sha256 :no_check
 
-  url "https://static.eudic.net/pkg/eudicmac.dmg",
+  url "https://static.frdic.com/pkg/eudicmac.dmg",
       user_agent: :fake
   name "Eudic"
   name "欧路词典"
