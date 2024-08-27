cbb5948a0f9af49ea29f89f45c0565c0eca1525f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-27 03:50:53 +0000

    tsh 16.2.0

--- Casks/t/tsh.rb
+++ Casks/t/tsh.rb
@@ -1,6 +1,6 @@
 cask "tsh" do
-  version "16.1.8"
-  sha256 "437afdd25fd7346686581407c311c43150c5f90b90c10752f21fd1566a397e97"
+  version "16.2.0"
+  sha256 "1cd7f8cff75286ca9b84abc6cafd72fa3d07ab2573db9022551066149f9bad6f"
 
   url "https://cdn.teleport.dev/tsh-#{version}.pkg",
       verified: "cdn.teleport.dev/"
