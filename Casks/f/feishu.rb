92878b646a934028fb1fc207d424c21c8ac154d6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-28 09:49:43 +0000

    feishu 7.25.4

--- Casks/f/feishu.rb
+++ Casks/f/feishu.rb
@@ -2,12 +2,12 @@ cask "feishu" do
   arch arm: "arm64", intel: "x64"
 
   on_arm do
-    version "7.24.4,5eb604e2"
-    sha256 "4b237af260457ad2a75305f8ee0cbc24d7bd6f384894309a2af6314e3af6fcca"
+    version "7.25.4,b8ebd45f"
+    sha256 "b463d9efb5139a25667253e7df39155f77d9d2c70184aadf716c0f1feb8ad8a6"
   end
   on_intel do
-    version "7.24.4,cfa538a4"
-    sha256 "99a1339b1b531743006be6dc490c44a4f144d9dc756a4c082ac4220b29bd5697"
+    version "7.25.4,1be6021b"
+    sha256 "6e88b70dede814f968fb078cdf7d17112086fffa3b2b5913946684af4e310898"
   end
 
   url "https://sf3-cn.feishucdn.com/obj/ee-appcenter/#{version.csv.second}/Feishu-darwin_#{arch}-#{version.csv.first}-signed.dmg",
