a0b8c6932d010e2f7e2c752cabde0ec5518d8823
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-28 07:48:10 +0200

    matterhorn 90000.0.1

--- Casks/m/matterhorn.rb
+++ Casks/m/matterhorn.rb
@@ -1,9 +1,9 @@
 cask "matterhorn" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "90000.0.0"
-  sha256 arm:   "e0ae467160202b7c36b95d980ee8f822fbf2776f55057f609e7bc744ce619919",
-         intel: "c49f9ad49ca745f629c1251eb4a75d6e8ff7c678aefa93939977bde1fb4223ff"
+  version "90000.0.1"
+  sha256 arm:   "a2f84efca249e7c69290fd88b85637f40935ecc879034f0ca1497cc436d8e177",
+         intel: "caf0117a67e1db005d9920fd9743e8872561fbdab9812dfe4e295f26bdaa2bf9"
 
   url "https://github.com/matterhorn-chat/matterhorn/releases/download/#{version}/matterhorn-#{version}-Darwin-#{arch}.tar.bz2"
   name "Matterhorn"
