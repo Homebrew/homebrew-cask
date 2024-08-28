736548a6ef57ded92f7210658571a03d56a38c26
Danny Su <danny.su@live.com>
2024-08-28 09:46:43 +0800

    Update eudic.rb
    Update the URL; the URL without the v parameter points to the old version.

--- Casks/e/eudic.rb
+++ Casks/e/eudic.rb
@@ -2,7 +2,7 @@ cask "eudic" do
   version :latest
   sha256 :no_check
 
-  url "https://static.frdic.com/pkg/eudicmac.dmg",
+  url "https://static.frdic.com/pkg/eudicmac.dmg?v=2024-07-28",
       verified:   "static.frdic.com/",
       user_agent: :fake
   name "Eudic"
