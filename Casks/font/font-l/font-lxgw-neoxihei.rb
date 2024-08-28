a425663671270bf0256ea925de252b3a3362969e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-28 06:40:17 +0000

    font-lxgw-neoxihei 1.201

--- Casks/font/font-l/font-lxgw-neoxihei.rb
+++ Casks/font/font-l/font-lxgw-neoxihei.rb
@@ -1,6 +1,6 @@
 cask "font-lxgw-neoxihei" do
-  version "1.200"
-  sha256 "f26fb5f6524d760fa606fbc702283c37267800e0b6344de4eeb965eeedf672ec"
+  version "1.201"
+  sha256 "db0db926f6f1eeef4ed1acecbcb497d53c5f5c035535d7f38df3402e1b315192"
 
   url "https://github.com/lxgw/LxgwNeoXiHei/releases/download/v#{version}/LXGWNeoXiHei.ttf"
   name "LXGW NeoXiHei"
