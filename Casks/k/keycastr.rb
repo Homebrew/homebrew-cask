ae015472554f6eda57e7f21c8b917e126280bdaa
Andrew Kitchen <akitchen@gmail.com>
2024-08-29 15:49:13 -0700

    keycastr 0.10.1

--- Casks/k/keycastr.rb
+++ Casks/k/keycastr.rb
@@ -1,6 +1,6 @@
 cask "keycastr" do
-  version "0.10.0"
-  sha256 "a3f1cac833addf829869c6336ff1e7d6a665a0294b9bcb04d4f5c46f7e4b0419"
+  version "0.10.1"
+  sha256 "11afd0b5888cec9d867394fc33e5a2cd206b826849f8d5b87ab9f6039f209549"
 
   url "https://github.com/keycastr/keycastr/releases/download/v#{version}/KeyCastr.app.zip"
   name "KeyCastr"
