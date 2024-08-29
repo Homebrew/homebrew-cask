3a86e304d82e8047f9d82394a85b682a2cddeecf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-29 07:04:26 +0000

    webcatalog 58.2.0

--- Casks/w/webcatalog.rb
+++ Casks/w/webcatalog.rb
@@ -1,6 +1,6 @@
 cask "webcatalog" do
-  version "58.1.0"
-  sha256 "ceef7e03f137f81b648792f06455cf3b5137d26e9be31d387b5652fbfe893642"
+  version "58.2.0"
+  sha256 "46fd6c6396e72d54ced48de46a0071eef0c16eb7e1994297cee7ee180daf560f"
 
   url "https://cdn-2.webcatalog.io/webcatalog/WebCatalog-#{version}-universal.dmg"
   name "WebCatalog"
