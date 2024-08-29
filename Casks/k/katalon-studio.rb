56dc32d81a6c47ce48cf9a8d8a1151f084a8cc20
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2024-08-29 11:06:21 +0700

    katalon-studio 9.7.0

--- Casks/k/katalon-studio.rb
+++ Casks/k/katalon-studio.rb
@@ -2,9 +2,9 @@ cask "katalon-studio" do
   arch arm: "%20Arm64"
   app_suffix = on_arch_conditional arm: " Arm64"
 
-  version "9.6.0"
-  sha256 arm:   "293e87b5cd4dd41de109d96b8f0d4284292c0bdb3921b675f1fd4655e61c3972",
-         intel: "104a8367b86907fc73b3e95031dadebd29a06b52731e75f8e2622e67c334b9a4"
+  version "9.7.0"
+  sha256 arm:   "1a9b2cd483c2290a40c8f7abd309fe02014b7797f27ae848cc55243dfa90a08a",
+         intel: "1ae9cccfa7a768aea8df298c04458c825e521bea7efad3d9be78d5650b188761"
 
   url "https://download.katalon.com/#{version}/Katalon%20Studio#{arch}.dmg"
   name "Katalon Studio"
