04a910a8d96ca565cd192df278d1e36a188d8298
Valentine Briese <valentinegb@icloud.com>
2024-08-30 09:52:23 -0700

    font-ibm-plex-sans-kr 1.0.0

--- Casks/font/font-i/font-ibm-plex-sans-kr.rb
+++ Casks/font/font-i/font-ibm-plex-sans-kr.rb
@@ -1,21 +1,24 @@
 cask "font-ibm-plex-sans-kr" do
-  version :latest
-  sha256 :no_check
+  version "1.0.0"
+  sha256 "b977effaabce2328f888db8590f0989c162cb9a9aad9d70949f3910552e6652b"
 
-  url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
-      branch:    "main",
-      only_path: "ofl/ibmplexsanskr"
+  url "https://github.com/IBM/plex/releases/download/%40ibm%2Fplex-sans-kr%40#{version}/ibm-plex-sans-kr.zip"
   name "IBM Plex Sans KR"
-  homepage "https://fonts.google.com/specimen/IBM+Plex+Sans+KR"
+  homepage "https://github.com/IBM/plex"
 
-  font "IBMPlexSansKR-Bold.ttf"
-  font "IBMPlexSansKR-ExtraLight.ttf"
-  font "IBMPlexSansKR-Light.ttf"
-  font "IBMPlexSansKR-Medium.ttf"
-  font "IBMPlexSansKR-Regular.ttf"
-  font "IBMPlexSansKR-SemiBold.ttf"
-  font "IBMPlexSansKR-Thin.ttf"
+  livecheck do
+    url :url
+    regex(%r{^@ibm/plex-sans-kr@?(\d+(?:\.\d+)+)$}i)
+  end
+
+  font "ibm-plex-sans-kr/fonts/complete/otf/IBMPlexSansKR-Bold.otf"
+  font "ibm-plex-sans-kr/fonts/complete/otf/IBMPlexSansKR-ExtraLight.otf"
+  font "ibm-plex-sans-kr/fonts/complete/otf/IBMPlexSansKR-Light.otf"
+  font "ibm-plex-sans-kr/fonts/complete/otf/IBMPlexSansKR-Medium.otf"
+  font "ibm-plex-sans-kr/fonts/complete/otf/IBMPlexSansKR-Regular.otf"
+  font "ibm-plex-sans-kr/fonts/complete/otf/IBMPlexSansKR-SemiBold.otf"
+  font "ibm-plex-sans-kr/fonts/complete/otf/IBMPlexSansKR-Text.otf"
+  font "ibm-plex-sans-kr/fonts/complete/otf/IBMPlexSansKR-Thin.otf"
 
   # No zap stanza required
 end
