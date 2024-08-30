ecc0f47597f6c741bf79ec433dbcfae6cfe05fd8
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-30 10:41:58 +0200

    dungeon-crawl-stone-soup-console 0.32.0

--- Casks/d/dungeon-crawl-stone-soup-console.rb
+++ Casks/d/dungeon-crawl-stone-soup-console.rb
@@ -1,6 +1,6 @@
 cask "dungeon-crawl-stone-soup-console" do
-  version "0.31.0"
-  sha256 "fc94ad5ad73a0abbf3cb7008ade0fa7d7d39f92cf81d0d01e408718dad873708"
+  version "0.32.0"
+  sha256 "6ab2a0cc13c315e1e400b7e7f20fca2445c865b492525358ca9846026762e841"
 
   url "https://github.com/crawl/crawl/releases/download/#{version}/dcss-#{version}-macos-console-universal.zip",
       verified: "github.com/crawl/crawl/releases/"
