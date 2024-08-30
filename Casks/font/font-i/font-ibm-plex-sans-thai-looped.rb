968ed9e3a2fb59d06a7235d8e0b992d1b5a3d829
Valentine Briese <valentinegb@icloud.com>
2024-08-30 09:37:30 -0700

    font-ibm-plex-sans-thai-looped 1.0.0

--- Casks/font/font-i/font-ibm-plex-sans-thai-looped.rb
+++ Casks/font/font-i/font-ibm-plex-sans-thai-looped.rb
@@ -1,21 +1,24 @@
 cask "font-ibm-plex-sans-thai-looped" do
-  version :latest
-  sha256 :no_check
+  version "1.0.0"
+  sha256 "734b854b745c715123713b283a077d587800c1de0e2d6c84e524862d58a649e6"
 
-  url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
-      branch:    "main",
-      only_path: "ofl/ibmplexsansthailooped"
+  url "https://github.com/IBM/plex/releases/download/%40ibm%2Fplex-sans-thai-looped%40#{version}/ibm-plex-sans-thai-looped.zip"
   name "IBM Plex Sans Thai Looped"
-  homepage "https://fonts.google.com/specimen/IBM+Plex+Sans+Thai+Looped"
+  homepage "https://github.com/IBM/plex"
 
-  font "IBMPlexSansThaiLooped-Bold.ttf"
-  font "IBMPlexSansThaiLooped-ExtraLight.ttf"
-  font "IBMPlexSansThaiLooped-Light.ttf"
-  font "IBMPlexSansThaiLooped-Medium.ttf"
-  font "IBMPlexSansThaiLooped-Regular.ttf"
-  font "IBMPlexSansThaiLooped-SemiBold.ttf"
-  font "IBMPlexSansThaiLooped-Thin.ttf"
+  livecheck do
+    url :url
+    regex(%r{^@ibm/plex-sans-thai-looped@?(\d+(?:\.\d+)+)$}i)
+  end
+
+  font "ibm-plex-sans-thai-looped/fonts/complete/otf/IBMPlexSansThaiLooped-Bold.otf"
+  font "ibm-plex-sans-thai-looped/fonts/complete/otf/IBMPlexSansThaiLooped-ExtraLight.otf"
+  font "ibm-plex-sans-thai-looped/fonts/complete/otf/IBMPlexSansThaiLooped-Light.otf"
+  font "ibm-plex-sans-thai-looped/fonts/complete/otf/IBMPlexSansThaiLooped-Medium.otf"
+  font "ibm-plex-sans-thai-looped/fonts/complete/otf/IBMPlexSansThaiLooped-Regular.otf"
+  font "ibm-plex-sans-thai-looped/fonts/complete/otf/IBMPlexSansThaiLooped-SemiBold.otf"
+  font "ibm-plex-sans-thai-looped/fonts/complete/otf/IBMPlexSansThaiLooped-Text.otf"
+  font "ibm-plex-sans-thai-looped/fonts/complete/otf/IBMPlexSansThaiLooped-Thin.otf"
 
   # No zap stanza required
 end
