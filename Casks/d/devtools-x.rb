2420e66c004db434203b880523c01bc528195b5b
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-08-30 16:57:38 -0500

    Fix stanza

--- Casks/d/devtools-x.rb
+++ Casks/d/devtools-x.rb
@@ -1,6 +1,6 @@
 cask "devtools-x" do
   version "2.15.0"
-  arch arm: "arm64", intel: "x86_64"
+  arch arm: "aarch64", intel: "x64"
 
   sha256 arm:   "c02ef8b1d77a254479aa6e59a49a5a6c828eb91fd806b605d57e008d27559d02",
          intel: "fbf6b020c9107c60231846b2e76d5e6a9bfbf0293ad0ec5bf46ec7ed889ea783"
