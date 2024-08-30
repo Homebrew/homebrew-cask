b6a972291f202288c973a5a68261507462e017dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-30 15:40:15 +0000

    ableton-live-suite 12.0.25

--- Casks/a/ableton-live-suite.rb
+++ Casks/a/ableton-live-suite.rb
@@ -1,6 +1,6 @@
 cask "ableton-live-suite" do
-  version "12.0.20"
-  sha256 "3c47bf358ffba13d4ee2bbbb0eb31d5d6fbee6824197ede77b54cdde8c264b31"
+  version "12.0.25"
+  sha256 "4c8a3ecc8f09544d23e345948df01feda3422f3f8d98c41d7c15254696223b73"
 
   url "https://cdn-downloads.ableton.com/channels/#{version}/ableton_live_suite_#{version}_universal.dmg"
   name "Ableton Live Suite"
