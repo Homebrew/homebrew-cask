f9ce4d53087f59c40997f47b1fe330a9f079d3e0
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-08-30 17:07:51 -0500

    Fix lint

--- Casks/d/devtools-x.rb
+++ Casks/d/devtools-x.rb
@@ -1,12 +1,12 @@
 cask "devtools-x" do
-  version "2.15.0"
   arch arm: "aarch64", intel: "x64"
+
+  version "2.15.0"
 
   sha256 arm:   "c02ef8b1d77a254479aa6e59a49a5a6c828eb91fd806b605d57e008d27559d02",
          intel: "fbf6b020c9107c60231846b2e76d5e6a9bfbf0293ad0ec5bf46ec7ed889ea783"
 
-  url "https://github.com/fosslife/devtools-x/releases/download/devtoolsx-v#{version}/dev-tools_#{version}_#{arch}.dmg",
-      verified: "github.com/fosslife/devtools-x/"
+  url "https://github.com/fosslife/devtools-x/releases/download/devtoolsx-v#{version}/dev-tools_#{version}_#{arch}.dmg"
 
   name "devtools-x"
   desc "Collection of offline first developer utilities available as desktop application. all in one place, cross-platform!"
