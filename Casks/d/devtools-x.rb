c4eb1ae07987e60d358502b0df5bb5f21095e926
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-08-30 17:19:35 -0500

    maybe

--- Casks/d/devtools-x.rb
+++ Casks/d/devtools-x.rb
@@ -2,7 +2,6 @@ cask "devtools-x" do
   arch arm: "aarch64", intel: "x64"
 
   version "2.15.0"
-
   sha256 arm:   "c02ef8b1d77a254479aa6e59a49a5a6c828eb91fd806b605d57e008d27559d02",
          intel: "fbf6b020c9107c60231846b2e76d5e6a9bfbf0293ad0ec5bf46ec7ed889ea783"
 
@@ -10,7 +9,6 @@ cask "devtools-x" do
 
   name "devtools-x"
   desc "Collection of offline first developer utilities available as desktop application"
-
   homepage "https://github.com/fosslife/devtools-x"
 
   depends_on macos: ">= :high_sierra"
