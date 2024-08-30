706217425626981f4f58e867a60313ad7970312f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-30 18:45:19 +0000

    http-toolkit 1.19.0

--- Casks/h/http-toolkit.rb
+++ Casks/h/http-toolkit.rb
@@ -1,6 +1,6 @@
 cask "http-toolkit" do
-  version "1.18.1"
-  sha256 "a1f07eb28128904736f9500788445f4fabc874d2ff55e5a1909d22ba5b4a96cc"
+  version "1.19.0"
+  sha256 "827acedf6f91a62f603b8462b374865ea206ebb735c962ddf5e718b9711fffad"
 
   url "https://github.com/httptoolkit/httptoolkit-desktop/releases/download/v#{version}/HttpToolkit-#{version}.dmg",
       verified: "github.com/httptoolkit/httptoolkit-desktop/"
