46b95e172bc8cc97402638aa022dfaccd7085187
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-08-30 22:21:36 -0400

    trojanx: deprecate
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/t/trojanx.rb
+++ Casks/t/trojanx.rb
@@ -7,6 +7,8 @@ cask "trojanx" do
   desc "Mechanism to bypass the Great Firewall"
   homepage "https://github.com/JimLee1996/TrojanX"
 
+  deprecate! date: "2024-08-30", because: :unmaintained
+
   depends_on macos: ">= :el_capitan"
 
   app "TrojanX.app"
@@ -18,4 +20,8 @@ cask "trojanx" do
     "~/Library/Application Support/TrojanX",
     "~/Library/Preferences/TrojanX.plist",
   ]
+
+  caveats do
+    requires_rosetta
+  end
 end
