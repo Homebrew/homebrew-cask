16ba02462245159491b2cdf59a5b1573e8b4efe5
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-09-01 17:18:20 -0400

    convert3dgui 1.4.0

--- Casks/c/convert3dgui.rb
+++ Casks/c/convert3dgui.rb
@@ -1,16 +1,11 @@
 cask "convert3dgui" do
-  version "1.0.0"
-  sha256 "f90fc3732578e9c9378e6d9340611ab93955f8ee182af1a59edac55f8df4b728"
+  version "1.4.0"
+  sha256 "bd56806e987eef2aa57c150a19fc6c9d8c357eff0f2bbc765dd5d9ff6ca37c29"
 
-  url "https://downloads.sourceforge.net/c3d/c3d-#{version}-MacOS-x86_64.dmg"
+  url "https://downloads.sourceforge.net/c3d/Experimental/c3d-#{version}-MacOS-x86_64.dmg"
   name "Convert3DGUI"
   desc "Command-line tool for converting 3D images between common file formats"
   homepage "https://sourceforge.net/projects/c3d"
-
-  livecheck do
-    url :url
-    regex(%r{url=.*?/c3d/(\d+(?:\.\d+)+)/c3d[._-]v?\1[._-]MacOS}i)
-  end
 
   app "Convert3DGUI.app"
   binary "#{appdir}/Convert3DGUI.app/Contents/bin/c2d"
