448e175eebd98cb5629391d5078aef7c65f0f9da
Daeho Ro <lamanus@outlook.kr>
2024-09-01 17:38:56 +0900

    min 1.33.0

--- Casks/m/min.rb
+++ Casks/m/min.rb
@@ -1,9 +1,9 @@
 cask "min" do
   arch arm: "arm64", intel: "x86"
 
-  version "1.32.1"
-  sha256 arm:   "02eddf536da721eea11f1fd035ac7e2e067d1868904df6ca9184fdc09082c182",
-         intel: "b78ed297534b7fd5ff9f62dee8c66218a3c13eae4497103c06febbfe0d90e90c"
+  version "1.33.0"
+  sha256 arm:   "4960f9dc258ba80d7933cc4c1322064cf891d56d38cab03d650b45b1af63c078",
+         intel: "1b8256b0714f5e065541b451afec3d775517cac855e519c96b50bbca53effdc3"
 
   url "https://github.com/minbrowser/min/releases/download/v#{version}/min-v#{version}-mac-#{arch}.zip",
       verified: "github.com/minbrowser/min/"
