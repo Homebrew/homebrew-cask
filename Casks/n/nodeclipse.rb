a5bc88efd6951ab36f885048585005fa8421563f
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-09-01 16:38:13 -0400

    nodeclipse: deprecate
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/n/nodeclipse.rb
+++ Casks/n/nodeclipse.rb
@@ -8,16 +8,14 @@ cask "nodeclipse" do
   desc "Node.js tooling with Eclipse"
   homepage "https://nodeclipse.github.io/"
 
-  livecheck do
-    url "https://nodeclipse.github.io/updates/"
-    regex(%r{/Enide-(\d+)-(\d+)-macosx-x64-(\d+)\.zip})
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| "#{match[0]}.#{match[1]},#{match[2]}" }
-    end
-  end
+  deprecate! date: "2024-09-01", because: :unmaintained
 
   # Renamed for clarity: app name is inconsistent with its branding.
   # Also renamed to avoid conflict with other eclipse Casks.
   # Original discussion: https://github.com/Homebrew/homebrew-cask/pull/8183
   app "Eclipse.app", target: "Nodeclipse.app"
+
+  caveats do
+    requires_rosetta
+  end
 end
