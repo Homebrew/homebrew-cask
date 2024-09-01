1d5f02c98f1e4997a846504033083a81576fab86
Bevan Kay <email@bevankay.me>
2024-09-01 12:02:01 +0000

    update Google Fonts
    This pull request was created automatically by the [`google-fonts`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-s/font-sankofa-display.rb
+++ Casks/font/font-s/font-sankofa-display.rb
@@ -2,9 +2,10 @@ cask "font-sankofa-display" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/sankofadisplay/SankofaDisplay-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/sankofadisplay/SankofaDisplay-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Sankofa Display"
-  homepage "https://github.com/batsimadz/Sankofa-Display"
+  homepage "https://fonts.google.com/specimen/Sankofa+Display"
 
   font "SankofaDisplay-Regular.ttf"
 
