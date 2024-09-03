e4b206a5c8d80ebc7beb099a5430747583248abf
Bevan Kay <email@bevankay.me>
2024-09-03 09:13:30 +0000

    update Google Fonts
    This commit was created automatically by the [`google-fonts`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-e/font-edu-au-vic-wa-nt-hand.rb
+++ Casks/font/font-e/font-edu-au-vic-wa-nt-hand.rb
@@ -2,9 +2,10 @@ cask "font-edu-au-vic-wa-nt-hand" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/eduauvicwanthand/EduAUVICWANTHand%5Bwght%5D.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/eduauvicwanthand/EduAUVICWANTHand%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Edu AU VIC WA NT Hand"
-  homepage "https://github.com/SorkinType/VICWANTSchoolhandAustralia"
+  homepage "https://fonts.google.com/specimen/Edu+AU+VIC+WA+NT+Hand"
 
   font "EduAUVICWANTHand[wght].ttf"
 
