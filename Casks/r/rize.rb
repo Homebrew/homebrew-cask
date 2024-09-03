ac7b3c8a5bece09dd4764c1c93eeaf35ce86676b
Haeward <haewardy@gmail.com>
2024-09-03 14:37:43 +0800

    rize 1.3.6

---
+++ Casks/r/rize.rb
@@ -0,0 +1,32 @@
+cask "rize" do
+  arch arm: "arm64", intel: "x64"
+
+  version "1.3.6"
+  sha256  arm:   "324afeb1a420ec8343ae356783463f342c1fe7eba92bc9421762426f23d352cf",
+          intel: "1f3800f60a72a206bc784bcc0cdab8074c6f78a1706ea5af1c0ab981244c853b"
+
+  url "https://github.com/rize-io/lua/releases/download/v#{version}/Rize-#{version}-#{arch}.dmg",
+      verified: "github.com/rize-io/lua/"
+  name "Rize"
+  desc "Maximize Your Productivity with A.I"
+  homepage "https://rize.io/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
+  app "Rize.app"
+
+  zap trash: [
+    "~/Library/Application Support/Rize",
+    "~/Library/Caches/io.rize",
+    "~/Library/Caches/io.rize.ShipIt",
+    "~/Library/HTTPStorages/io.rize",
+    "~/Library/Preferences/io.rize.plist",
+    "~/Library/Saved Application State/io.rize.savedState",
+  ]
+end
