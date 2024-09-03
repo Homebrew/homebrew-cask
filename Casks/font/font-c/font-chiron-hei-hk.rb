983ed6be9bdb5090f743bdf4a3dee03beabf7b4a
Klaus Hipp <khipp@users.noreply.github.com>
2024-09-03 22:23:10 +0200

    font-chiron-hei-hk 2.516

--- Casks/font/font-c/font-chiron-hei-hk.rb
+++ Casks/font/font-c/font-chiron-hei-hk.rb
@@ -1,6 +1,6 @@
 cask "font-chiron-hei-hk" do
-  version "2.515"
-  sha256 "c9ab581e12c3433962fe0274c092a39f1866264d583df4ece6576063869fafa1"
+  version "2.516"
+  sha256 "ccc42fdceed25cc5a96ed8db663d8a1d0e0b0fe16a1dad074b6eae8cff56446d"
 
   url "https://github.com/chiron-fonts/chiron-hei-hk/archive/refs/tags/v#{version}.tar.gz"
   name "Chiron Hei HK"
