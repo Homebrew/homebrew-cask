7005aebc80a112fb5eddc4120f87d176390d8ef0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-03 15:37:42 +0000

    elgato-camera-hub 1.11.0.4022

--- Casks/e/elgato-camera-hub.rb
+++ Casks/e/elgato-camera-hub.rb
@@ -1,6 +1,6 @@
 cask "elgato-camera-hub" do
-  version "1.10.2.3481"
-  sha256 "0f355438a81eb606f6e0784f2d6d07de8eec6897cc5e12d64988653d9feb793f"
+  version "1.11.0.4022"
+  sha256 "7a5425819ae98e439fe5fa46007814dbcbfc5f094913d4184e44591c91c6441b"
 
   url "https://edge.elgato.com/egc/macos/echm/#{version.major_minor_patch}/Camera_Hub_#{version}.pkg"
   name "Elgato Camera Hub"
