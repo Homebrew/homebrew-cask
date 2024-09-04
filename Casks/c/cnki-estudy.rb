d89b9dfab1d5e3f5f498abbcba573a6eaf4bad1d
Koeniy <koeniy@outlook.com>
2024-09-04 11:38:06 +0800

    cnki-estudy 4.3.1 (new cask)

---
+++ Casks/c/cnki-estudy.rb
@@ -0,0 +1,28 @@
+cask "cnki-estudy" do
+  version "4.3.1"
+  sha256 :no_check
+
+  url "https://picx.cnki.net/estudy/CNKI%20E-Study%20For%20Mac.dmg"
+  name "cnki-estudy"
+  name "知网研学"
+  desc "Academic research platform"
+  homepage "https://x.cnki.net/web/search/#/home"
+
+  livecheck do
+    url :url
+    strategy :extract_plist do |item|
+      item["net.cnki.EStudy"]&.short_version
+    end
+  end
+
+  depends_on macos: ">= :sierra"
+
+  app "知网研学.app"
+
+  zap trash: [
+    "~/Library/Caches/net.cnki.EStudy",
+    "~/Library/HTTPStorages/net.cnki.EStudy",
+    "~/Library/HTTPStorages/net.cnki.EStudy.binarycookies",
+    "~/Library/Preferences/net.cnki.EStudy.plist",
+  ]
+end
