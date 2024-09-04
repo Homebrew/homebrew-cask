beb2c153fb603ee71fb0f1f8df15b60a604010a9
Koeniy <koeniy@outlook.com>
2024-09-04 11:51:53 +0800

    change url

--- Casks/c/cnki-estudy.rb
+++ Casks/c/cnki-estudy.rb
@@ -2,7 +2,7 @@ cask "cnki-estudy" do
   version "4.3.1"
   sha256 :no_check
 
-  url "https://picx.cnki.net/estudy/CNKI%20E-Study%20For%20Mac.dmg"
+  url "https://download.cnki.net/CNKI%20E-Study%20For%20Mac.dmg"
   name "cnki-estudy"
   name "知网研学"
   desc "Academic research platform"
