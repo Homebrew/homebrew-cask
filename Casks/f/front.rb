bc5af8392d1b8b1798dffce1d1d10c2d69abd8be
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-04 18:54:06 +0000

    front 3.58.0

--- Casks/f/front.rb
+++ Casks/f/front.rb
@@ -1,9 +1,9 @@
 cask "front" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.57.5"
-  sha256 arm:   "dfafa3d827865b6545db512d5a85985fb0c2ab7f4f0627e330606b593276db39",
-         intel: "964a9863453a788aefb7ad55905a5af23840d611f41ea729d3674f661b7941a2"
+  version "3.58.0"
+  sha256 arm:   "025fe4bc7b3b8d90626febb4ec6faadbbf83f358b12a5a4559751a6f88b412ca",
+         intel: "0d1efb43de671620a8cb0c7c81936e8d27c97c30ca08f04cbc4a3cf398e81d35"
 
   url "https://dl.frontapp.com/desktop/builds/#{version}/Front-#{version}-#{arch}.zip",
       verified: "dl.frontapp.com/desktop/builds/"
