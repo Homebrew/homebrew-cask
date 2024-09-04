835b7f59b2dc18d91108c0d198e7f0c6e6aa9a4c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-04 18:55:04 +0000

    gitkraken-on-premise-serverless 10.3.0

--- Casks/g/gitkraken-on-premise-serverless.rb
+++ Casks/g/gitkraken-on-premise-serverless.rb
@@ -1,9 +1,9 @@
 cask "gitkraken-on-premise-serverless" do
   arch arm: "darwin-arm64", intel: "darwin"
 
-  version "10.2.0"
-  sha256 arm:   "f976a465accb823226d5c9d53c227d34d3e666ee1689a3b48c4f5355efaa74e6",
-         intel: "12f0f99e0aa845ec046e23e636ae2c8a92003d48f884ebeec4c31ff8f3e98f82"
+  version "10.3.0"
+  sha256 arm:   "0a57ef96b9ff0ac87e7ca8344485a94847517d4886114c8d4e4dd9c27caca070",
+         intel: "ff051a710fa142422a03cbcd60cad29bf939b54668d3f5b37033c538833ddf7d"
 
   url "https://release.axocdn.com/#{arch}-standalone/GitKraken-v#{version}.zip",
       verified: "release.axocdn.com/"
