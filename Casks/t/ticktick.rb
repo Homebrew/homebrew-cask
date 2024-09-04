9d18041dbf651c1c59f14e97a2ec2c1d56baf079
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-04 13:04:08 +0000

    ticktick 6.0.50

--- Casks/t/ticktick.rb
+++ Casks/t/ticktick.rb
@@ -1,6 +1,6 @@
 cask "ticktick" do
-  version "6.0.40,378"
-  sha256 "104418705fceac7f57cd816916a5d214dc714a4f5bdf3a961966eab51fd75819"
+  version "6.0.50,382"
+  sha256 "c9d43a5752ed173dc40c6a97d51028bbe7198e1f435b20792735e4cc1f2b9569"
 
   url "https://ticktick-download-mac.s3.amazonaws.com/download/mac/TickTick_#{version.csv.first}_#{version.csv.second}.dmg",
       verified: "ticktick-download-mac.s3.amazonaws.com/download/mac/"
