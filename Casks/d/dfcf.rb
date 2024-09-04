290bb89a2d4e25bd04e5eb50c8b48f33c84e9f73
Koeniy <koeniy@outlook.com>
2024-09-04 10:17:47 +0800

    dfcf 3.7.3 (new cask)

---
+++ Casks/d/dfcf.rb
@@ -0,0 +1,27 @@
+cask "dfcf" do
+  version "3.7.3"
+  sha256 :no_check
+
+  url "https://swdlcdn.eastmoney.com/Mac/Macdownload/dfcf.dmg"
+  name "East Money"
+  name "东方财富"
+  desc "Stock trading platform"
+  homepage "https://emdesk.eastmoney.com/pc_activity/AHome/Index"
+
+  livecheck do
+    url :url
+    strategy :extract_plist do |item|
+      item["com.emmac.mac"]&.short_version
+    end
+  end
+
+  depends_on macos: ">= :el_capitan"
+
+  app "东方财富.app"
+
+  caveats do
+    requires_rosetta
+  end
+
+  # No zap stanza required
+end
