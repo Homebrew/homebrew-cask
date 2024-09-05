c30fd6d1b536e7c56c985acb9463acb870607aa2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-05 18:59:50 +0000

    slack-cli 2.29.2

--- Casks/s/slack-cli.rb
+++ Casks/s/slack-cli.rb
@@ -1,6 +1,6 @@
 cask "slack-cli" do
-  version "2.29.1"
-  sha256 "772b68aff20850690fb34bdc78961e47cb0783ba829d4f36f4dde99cf6c43fcf"
+  version "2.29.2"
+  sha256 "bbb26beffdcc3bdfc2e741811071abaff71747d36f531c306606d041db86789c"
 
   url "https://downloads.slack-edge.com/slack-cli/slack_cli_#{version}_macOS_64-bit.tar.gz",
       verified: "downloads.slack-edge.com/slack-cli/"
