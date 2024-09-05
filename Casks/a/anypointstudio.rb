2d075ed6a9ef1b3128126a2753c151bb1924e218
Sachin <vcs@tuta.com>
2024-09-06 00:09:54 +0530

    feat(anypointstudio): modified script for dynamic install based on arch

--- Casks/a/anypointstudio.rb
+++ Casks/a/anypointstudio.rb
@@ -1,9 +1,16 @@
 cask "anypointstudio" do
   version "7.18.1"
-  sha256 "3f9dddf20bee30a3976a607a0c7138b265301e2284e7fd2ad5bae1bd68b0ce9f"
 
-  url "https://mule-studio.s3.amazonaws.com/#{version}-GA/AnypointStudio-#{version}-macos64.zip",
-      verified: "mule-studio.s3.amazonaws.com/"
+  if Hardware::CPU.arm?
+    sha256 "af7613ddaaa7274d92a982f64724720f6ddbab9c32be8aced73e36d6fd902c87"
+    url "https://mule-studio.s3.amazonaws.com/#{version}-GA/AnypointStudio-#{version}-macosArm.zip",
+        verified: "mule-studio.s3.amazonaws.com/"
+  else
+    sha256 "3f9dddf20bee30a3976a607a0c7138b265301e2284e7fd2ad5bae1bd68b0ce9f"
+    url "https://mule-studio.s3.amazonaws.com/#{version}-GA/AnypointStudio-#{version}-macos64.zip",
+        verified: "mule-studio.s3.amazonaws.com/"
+  end
+
   name "Anypoint Studio"
   desc "Eclipse-based IDE for designing and testing Mule applications"
   homepage "https://www.mulesoft.com/platform/studio"
@@ -22,6 +29,6 @@ cask "anypointstudio" do
   # No zap stanza required
 
   caveats do
-    requires_rosetta
   end
+    requires_rosetta if Hardware::CPU.intel?
 end
