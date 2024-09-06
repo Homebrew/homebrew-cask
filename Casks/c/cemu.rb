c51a9462c97aad1b0ea1bb70a23a71b5c22f4733
Klaus Hipp <khipp@users.noreply.github.com>
2024-09-06 07:09:50 +0200

    cemu 2.0

--- Casks/c/cemu.rb
+++ Casks/c/cemu.rb
@@ -1,8 +1,11 @@
 cask "cemu" do
-  version "1.3"
-  sha256 "db28a0497ea944a6118f869b50268d5f8c3730c37367033eeebc7cdec08fd60c"
+  arch arm: "arm", intel: "intel"
 
-  url "https://github.com/CE-Programming/CEmu/releases/download/v#{version}/macOS_CEmu.dmg",
+  version "2.0"
+  sha256 arm:   "e554ca753f263b2ebba75d36c1e724b5a31b35a24c19bd004c70c59b8135abc8",
+         intel: "ab903065536cd393f7c8c118be929753be542d4ab9314d91c3258daa87b6290a"
+
+  url "https://github.com/CE-Programming/CEmu/releases/download/v#{version}/CEmu-v#{version}_macOS_Qt6_#{arch}.dmg",
       verified: "github.com/CE-Programming/CEmu/"
   name "CEmu"
   desc "TI-84 Plus CE and TI-83 Premium CE calculator emulator"
@@ -12,7 +15,8 @@ cask "cemu" do
 
   app "CEmu.app"
 
-  caveats do
-    requires_rosetta
-  end
+  zap trash: [
+    "~/Library/Preferences/cemu-dev",
+    "~/Library/Preferences/com.yourcompany.CEmu.plist",
+  ]
 end
