87904be6d8a2f9ed71786b082555072d98aaa93a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-06 09:56:31 +0000

    proton-drive 1.10.0

--- Casks/p/proton-drive.rb
+++ Casks/p/proton-drive.rb
@@ -1,6 +1,6 @@
 cask "proton-drive" do
-  version "1.9.0"
-  sha256 "07a03cbf1ff0741bb4251f7d1fe848df6058b52d4d753cb5af3230ecd798b7c3"
+  version "1.10.0"
+  sha256 "a5d1d21ae3befff1c4284e4e568ac6f4289fe94c737463692d942ecb9e783fb8"
 
   url "https://proton.me/download/drive/macos/ProtonDrive-#{version}.dmg"
   name "Proton Drive"
