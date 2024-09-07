4da32b6f3987e44d3af5ad14f5df3d77c5d4b33c
Klaus Hipp <khipp@users.noreply.github.com>
2024-09-07 16:14:31 +0200

    macterm: add sha256

--- Casks/m/macterm.rb
+++ Casks/m/macterm.rb
@@ -1,6 +1,6 @@
 cask "macterm" do
   version "4.1.0"
-  sha256 :no_check
+  sha256 "48938d0d33a57de3859a6b7323e1f111e674b504aa99f422ceea1b8850ae454c"
 
   url "https://www.macterm.net/updates/macterm-#{version}.dmg"
   name "MacTerm"
