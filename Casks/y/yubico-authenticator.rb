de7a7fee0e61a90edd39a9f9b35286f2497cec10
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-09-07 11:34:29 -0400

    yubico-authenticator: update livecheck
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/y/yubico-authenticator.rb
+++ Casks/y/yubico-authenticator.rb
@@ -8,6 +8,27 @@ cask "yubico-authenticator" do
   desc "Application for generating TOTP and HOTP codes"
   homepage "https://developers.yubico.com/yubioath-flutter/"
 
+  # Releases sometimes don't have a macOS build, so we check multiple
+  # recent releases instead of only the "latest" release. NOTE: We should be
+  # able to use `strategy :github_latest` when subsequent releases provide
+  # files for macOS again.
+  livecheck do
+    url :url
+    regex(/^yubico[._-]authenticator[._-]v?(\d+(?:\.\d+)+)[._-]mac\.(?:dmg|pkg)$/i)
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        next if release["draft"] || release["prerelease"]
+
+        release["assets"]&.map do |asset|
+          match = asset["name"]&.match(regex)
+          next if match.blank?
+
+          match[1]
+        end
+      end.flatten
+    end
+  end
+
   depends_on macos: ">= :big_sur"
 
   app "Yubico Authenticator.app"
