525ff298c43ef321d2022840f42b7fb089a672e8
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-09-08 16:04:07 -0400

    tomighty: deprecate
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/t/tomighty.rb
+++ Casks/t/tomighty.rb
@@ -7,6 +7,8 @@ cask "tomighty" do
   desc "Pomodoro desktop timer"
   homepage "https://github.com/tomighty/tomighty-osx"
 
+  deprecate! date: "2024-09-08", because: :unmaintained
+
   app "Tomighty.app"
 
   zap trash: "~/Library/Preferences/org.tomighty.Tomighty.plist"
