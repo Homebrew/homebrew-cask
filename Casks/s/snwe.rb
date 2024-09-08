3ec72b6c5705cff7cfb8604ccf3ea7e87b823cd1
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-09-08 15:57:27 -0400

    snwe: deprecate
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/s/snwe.rb
+++ Casks/s/snwe.rb
@@ -7,6 +7,8 @@ cask "snwe" do
   desc "Extensible, customisable, menu bar replacement"
   homepage "https://github.com/blahsd/snwe"
 
+  deprecate! date: "2024-09-08", because: :unmaintained
+
   app "snwe.app"
 
   caveats do
