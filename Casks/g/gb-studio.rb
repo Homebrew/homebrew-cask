e85347a5696f54fee12bf433739b5a1fb9a55621
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-09 12:55:47 +0000

    gb-studio 4.1.2

--- Casks/g/gb-studio.rb
+++ Casks/g/gb-studio.rb
@@ -1,9 +1,9 @@
 cask "gb-studio" do
   arch arm: "apple-silicon", intel: "intel"
 
-  version "4.1.1"
-  sha256 arm:   "24eb69459b930daf4bc3fbf2a9eeafb9c10c367da51d9e5fdbd9df989690e990",
-         intel: "7254fafec357562a284bd6fc7d1c61d13f44ea475539c0cc570ed795427798c9"
+  version "4.1.2"
+  sha256 arm:   "1f67a12cadf955551b52a814667465dd0c89a599657e4653fc51c6242e625226",
+         intel: "7affaf080dbf1cc1f6f0d48c59f4a7f9e13f6663fdc84d3d26fb913928691744"
 
   url "https://github.com/chrismaltby/gb-studio/releases/download/v#{version}/gb-studio-mac-#{arch}.zip",
       verified: "github.com/chrismaltby/gb-studio/"
