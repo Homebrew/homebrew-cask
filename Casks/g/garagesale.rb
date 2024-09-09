fb69867d639081e7c45dcec0cec8cfef9032b08d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-09 18:56:09 +0000

    garagesale 9.8.2

--- Casks/g/garagesale.rb
+++ Casks/g/garagesale.rb
@@ -1,6 +1,6 @@
 cask "garagesale" do
-  version "9.8.1"
-  sha256 "1e4f75a0fc2bdb0b8925d00ff7a2817be9154d5b947cb3e7c6fd55dce5e9c34f"
+  version "9.8.2"
+  sha256 "afa0e4da6bb6e147490c20aecf79f37201d507cd11606e95bc29f25273392db1"
 
   url "https://downloads.iwascoding.com/downloads/GarageSale_#{version}.dmg"
   name "GarageSale"
