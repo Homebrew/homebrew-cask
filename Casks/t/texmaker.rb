7466889ad5b4515209e23f6f37d3b0ff7c732e15
Angelo Adkins <angeloadkins@me.com>
2024-09-08 20:46:21 -0700

    Modified texmaker to work with Intel or ARM Macs

--- Casks/t/texmaker.rb
+++ Casks/t/texmaker.rb
@@ -1,16 +1,25 @@
 cask "texmaker" do
-  version "6.0.0"
-  sha256 "8b67e6248a5e55c03790cb4854cc2c475bd35d29ae1a5526ee33956b9b1832df"
 
-  url "https://www.xm1math.net/texmaker/assets/files/texmaker-#{version}.dmg"
+  if Hardware::CPU.intel?
+    version "5.1.4"
+    sha256 "4fb2896712f3aee93c0aae8f940dafab66c04334a76e055e1366adb21550f1fe"
+    url "https://www.xm1math.net/texmaker/assets/files/texmaker-#{version}.dmg" 
+
+  elsif Hardware::CPU.arm?
+    version "6.0.0"
+    sha256 "8b67e6248a5e55c03790cb4854cc2c475bd35d29ae1a5526ee33956b9b1832df"
+    url "https://www.xm1math.net/texmaker/assets/files/texmaker-#{version}.dmg"
+
+  end
+  
+  livecheck do
+    skip "No version information available" 
+  end
   name "Texmaker"
   desc "LaTeX editor"
   homepage "https://www.xm1math.net/texmaker/"
 
-  livecheck do
-    url "https://www.xm1math.net/texmaker/download.html"
-    regex(%r{href=.*?/texmaker-(\d+(?:\.\d+)*)\.dmg}i)
-  end
+
 
   app "texmaker.app"
 
