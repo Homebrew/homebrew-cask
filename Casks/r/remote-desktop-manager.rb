22362d6bb8041dd5fddf533562d1b15c6c14a790
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-10 19:11:59 +0000

    remote-desktop-manager 2024.2.10.4

--- Casks/r/remote-desktop-manager.rb
+++ Casks/r/remote-desktop-manager.rb
@@ -1,6 +1,6 @@
 cask "remote-desktop-manager" do
-  version "2024.2.10.3"
-  sha256 "ebe6db1638a433f6b4c05955061249af0f848e9f5f4c6fde579ce05a6796ea87"
+  version "2024.2.10.4"
+  sha256 "eca0b18c0c35a9dd8e36e4bfe3fb4bd12407ed14d98b1d89a54da4effbf59459"
 
   url "https://cdn.devolutions.net/download/Mac/Devolutions.RemoteDesktopManager.Mac.#{version}.dmg",
       verified: "cdn.devolutions.net/download/Mac/"
