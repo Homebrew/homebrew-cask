d8ee419ea2d8ce6a26e9b634b8afa65a4ded9f9f
Cameron Roe <cameron@cameronbroe.com>
2024-09-10 11:40:08 -0500

    Update cask name to remove beta tag

--- Casks/o/orka-desktop.rb
+++ Casks/o/orka-desktop.rb
@@ -3,7 +3,7 @@ cask "orka-desktop" do
   sha256 "c0b5c99a73f7b627d5091eea3fa6458115c48a0d3bb64b0d47d4543d4a2e4588"
 
   url "https://github.com/macstadium/orka-desktop/releases/download/#{version}/OrkaDesktop-#{version}.dmg"
-  name "Orka Desktop (Beta)"
+  name "Orka Desktop"
   desc "Run macOS virtual machines locally and build images for use with Orka"
   homepage "https://github.com/macstadium/orka-desktop"
 
