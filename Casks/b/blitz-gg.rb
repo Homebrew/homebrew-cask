bf5446bcac3bac116968ff1580aac9381c73538a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-10 21:34:13 +0000

    blitz-gg 2.1.206

--- Casks/b/blitz-gg.rb
+++ Casks/b/blitz-gg.rb
@@ -1,6 +1,6 @@
 cask "blitz-gg" do
-  version "2.1.204"
-  sha256 "b5d90e8d71a8e76c7388bce458becd2881d9402841e71b20c9253271d026af80"
+  version "2.1.206"
+  sha256 "98187b9c3e9faee90a8a39249fe7720342827fce5ef69fb2fead80728aa36a13"
 
   url "https://blitz-main.blitz.gg/Blitz-x64-#{version}.dmg"
   name "Blitz"
