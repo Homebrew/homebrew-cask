0423ad50e8e507c5d4b64b9d6d0c76f948ca5d60
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-11 13:09:11 +0000

    volanta 1.10.0

--- Casks/v/volanta.rb
+++ Casks/v/volanta.rb
@@ -1,6 +1,6 @@
 cask "volanta" do
-  version "1.9.4,a807d1ee"
-  sha256 "9615cda611e0438e04babb984dcc1a29168f1a2683845e90c65cff0de0b24392"
+  version "1.10.0,1fba105e"
+  sha256 "d96a786b2fc6ec122b446a2afe7327aaaed9dd51bd37114fdc8308f74cabf1ec"
 
   url "https://cdn.volanta.app/software/volanta-app/#{version.csv.first}-#{version.csv.second}/volanta-#{version.csv.first}.dmg"
   name "Volanta"
