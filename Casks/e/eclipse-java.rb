a7f6e7e482afead9d0650a89c8ecd47c7fb787ba
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-09-11 11:53:56 -0400

    eclipse-java 4.33.0

--- Casks/e/eclipse-java.rb
+++ Casks/e/eclipse-java.rb
@@ -1,9 +1,9 @@
 cask "eclipse-java" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "4.31.0,2024-06"
-  sha256 arm:   "9c67d238fe77529f823bdaf12e0a13b7c1e78544535d4b8ff88cad65ffec8219",
-         intel: "8c3e3b642ea6f86258fe9a9b24d7538ef2e8fd94561d036748f43420f97127f2"
+  version "4.33.0,2024-09"
+  sha256 arm:   "468a06c7d87cdce674b14bf34f11c98d27d73d43d3ef4f5cbe153033d204535e",
+         intel: "3ec4d2a7c869e8cc15f88d55e048df0083305f37956c7fc9d53e65373cfbe087"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-java-#{version.csv.second}-R-macosx-cocoa-#{arch}.dmg&r=1"
   name "Eclipse IDE for Java Developers"
