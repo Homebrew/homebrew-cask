38ec6b3b88d7cb570a3d3747312eb3bb1f2de941
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-09-10 22:46:21 -0400

    thedesk 25.0.12

--- Casks/t/thedesk.rb
+++ Casks/t/thedesk.rb
@@ -1,6 +1,6 @@
 cask "thedesk" do
-  version "25.0.11"
-  sha256 "75d5484c1ad7cbc5d461c104563f3c0dc34a01b9cfc8309653fe178891d6aeab"
+  version "25.0.12"
+  sha256 "16992a60d7dc916b108f968ad60bf3f6817bd8cef16a1b62d4aaa0a6600da73b"
 
   url "https://github.com/cutls/thedesk-next/releases/download/v#{version}/TheDesk-#{version}-universal.dmg",
       verified: "github.com/cutls/thedesk-next/"
