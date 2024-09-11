d947ff0ce1826b991ce8a89cdbce187f60cd05a7
Klaus Hipp <khipp@users.noreply.github.com>
2024-09-11 10:35:17 +0200

    bookmacster 3.1.5

--- Casks/b/bookmacster.rb
+++ Casks/b/bookmacster.rb
@@ -1,6 +1,6 @@
 cask "bookmacster" do
-  version "3.1.4"
-  sha256 "cdcb3b3a7e9fd813c7280c99ae7a54582af08f38635ac87de295f0d08f3676b4"
+  version "3.1.5"
+  sha256 "fd9beac9b5b72465be5dc9d2dc55ab2093ac73713964c6737832228f4458934e"
 
   url "https://sheepsystems.com/bookmacster/BookMacster_#{version}.zip"
   name "BookMacster"
