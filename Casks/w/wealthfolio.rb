8f7dd3845ba9565e7b8552805ed473977408963d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-12 22:10:05 +0000

    wealthfolio 1.0.13

--- Casks/w/wealthfolio.rb
+++ Casks/w/wealthfolio.rb
@@ -1,9 +1,9 @@
 cask "wealthfolio" do
   arch arm: "aarch64", intel: "x64"
 
-  version "1.0.12"
-  sha256 arm:   "f6499369220b89b21b71ecbe418184d004fc07b0ddcf983f3b7ad410f1cd162d",
-         intel: "4da3dfca3076d20a3f6642eed1c7e39fd6e7470450c0c63741c7c5e429fbf8fd"
+  version "1.0.13"
+  sha256 arm:   "a88e13ffadb80ab593ca99aa2b07bbf92a11f7e31218267257b1751ee9d221e2",
+         intel: "0c8e74560bf13c5b524e4eb177a9a29496de60d5f6d196306b3bd9dfa55f15c7"
 
   url "https://github.com/afadil/wealthfolio/releases/download/v#{version}/Wealthfolio_#{version}_#{arch}.dmg",
       verified: "github.com/afadil/wealthfolio/"
