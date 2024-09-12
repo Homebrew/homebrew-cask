68b9b5f1dc05755aaebcd3ca7a57e247c90ab6a9
HuaDeity <hello@huadeity.com>
2024-09-12 23:39:44 +0800

    kekaexternalhelper 1.2.0,1.4.4

--- Casks/k/kekaexternalhelper.rb
+++ Casks/k/kekaexternalhelper.rb
@@ -1,6 +1,6 @@
 cask "kekaexternalhelper" do
-  version "1.1.3,1.2.59"
-  sha256 "a330bc6fcd00a93dfd95c52cdbdbbec5af0a0f091be0fc0efea58931906f43b6"
+  version "1.2.0,1.4.4"
+  sha256 "6857bb22694b6f6c01a13c40953b0a04b1704acf90da414a7476be63961d9827"
 
   url "https://github.com/aonez/Keka/releases/download/v#{version.csv.second}/KekaExternalHelper-v#{version.csv.first}.zip"
   name "Keka External Helper"
