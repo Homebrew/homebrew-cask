490c60f04631797f17b27c4206a417e1f9e5011d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-12 19:17:41 +0000

    wakatime 5.23.3

--- Casks/w/wakatime.rb
+++ Casks/w/wakatime.rb
@@ -1,6 +1,6 @@
 cask "wakatime" do
-  version "5.23.2"
-  sha256 "e0514bf49533ce3d58b3b43656e5367c668c338fb3b548aaa109683a90d63fae"
+  version "5.23.3"
+  sha256 "fc0f7f55135727b1e2de099c48c86e92ae9a7a19fcafcc59fad7e9eb8841b229"
 
   url "https://github.com/wakatime/macos-wakatime/releases/download/v#{version}/macos-wakatime.zip",
       verified: "github.com/wakatime/macos-wakatime/"
