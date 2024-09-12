b3b6625c89da1bbbfecc6828704b82edca229375
Klaus Hipp <khipp@users.noreply.github.com>
2024-09-12 12:48:57 +0200

    intellij-idea@eap 2024.3,243.12818.47

--- Casks/i/intellij-idea@eap.rb
+++ Casks/i/intellij-idea@eap.rb
@@ -1,9 +1,9 @@
 cask "intellij-idea@eap" do
   arch arm: "-aarch64"
 
-  version "2024.2.1,242.21829.15"
-  sha256 arm:   "08b13ed907c1970e2a9d48085baab2a1a9f241b99c800e678c30aedd0a6fe150",
-         intel: "25d0bf97b81ad4b50114f027f70629f2a9faf0087ccaddb62fbf31b9651e13ca"
+  version "2024.3,243.12818.47"
+  sha256 arm:   "0c53e72899d7276f48005d85ee497e010cd7a2671b128ad3e2e3121b88e2623a",
+         intel: "5596f3128a61d7b6712f792e286b5f17465aad3a4a2261d704d31ded48360750"
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version.csv.second}#{arch}.dmg"
   name "IntelliJ IDEA EAP"
@@ -23,8 +23,8 @@ cask "intellij-idea@eap" do
   conflicts_with cask: "intellij-idea"
   depends_on macos: ">= :high_sierra"
 
-  app "IntelliJ IDEA.app"
-  binary "#{appdir}/IntelliJ IDEA.app/Contents/MacOS/idea"
+  app "IntelliJ IDEA #{version.csv.first} EAP.app"
+  binary "#{appdir}/IntelliJ IDEA #{version.csv.first} EAP.app/Contents/MacOS/idea"
 
   uninstall_postflight do
     ENV["PATH"].split(File::PATH_SEPARATOR).map { |path| File.join(path, "idea") }.each do |path|
