e3468450a44e9b212d775c01c08601cae77eea8c
Klaus Hipp <khipp@users.noreply.github.com>
2024-09-13 19:36:07 +0200

    font-palemonasmufi-bold: add livecheck

--- Casks/font/font-p/font-palemonasmufi-bold.rb
+++ Casks/font/font-p/font-palemonasmufi-bold.rb
@@ -1,10 +1,15 @@
 cask "font-palemonasmufi-bold" do
-  version "2.0"
+  version "2.000"
   sha256 :no_check
 
   url "https://mufi.info/q.php?o=mufifontfiledownload&name=PalemMUFI-bd.ttf"
   name "PalemonasMUFI Italic"
   homepage "https://mufi.info/q.php?p=mufi/fonts/fontfamily/6"
+
+  livecheck do
+    url "https://mufi.info/q.php?p=mufi/fonts"
+    regex(/PalemonasMUFI-Bold\s+v?(\d+(?:\.\d+)+)/i)
+  end
 
   font "PalemMUFI-bd.ttf"
 
