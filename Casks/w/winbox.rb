26d088d5f761b26a0f5d5ce329c35e558d7b964a
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-09-13 12:12:45 -0400

    winbox 4.0beta6

--- Casks/w/winbox.rb
+++ Casks/w/winbox.rb
@@ -1,6 +1,6 @@
 cask "winbox" do
-  version "4.0beta4"
-  sha256 "0d15b0b6c25a4b11420d3dc00fc6a754d5ecada26d175501e0c8849ced409d5a"
+  version "4.0beta6"
+  sha256 "23ba0df177e7e8fc657399a4df715175970c90d23140b79ca4b4169765830d39"
 
   url "https://download.mikrotik.com/routeros/winbox/#{version}/WinBox.dmg"
   name "WinBox"
