d7506f84e5a06a397c28a203002c0a881596a1ff
Klaus Hipp <khipp@users.noreply.github.com>
2024-09-13 18:11:08 +0200

    font-takaoex: add livecheck

--- Casks/font/font-t/font-takaoex.rb
+++ Casks/font/font-t/font-takaoex.rb
@@ -6,6 +6,11 @@ cask "font-takaoex" do
   name "TakaoEx"
   homepage "https://launchpad.net/takao-fonts"
 
+  livecheck do
+    url :homepage
+    regex(/href=.*?TakaoExFonts[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+  end
+
   font "TakaoExFonts_#{version}/TakaoExGothic.ttf"
   font "TakaoExFonts_#{version}/TakaoExMincho.ttf"
 
