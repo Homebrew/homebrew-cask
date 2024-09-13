6713e8a3f545b4f5eb625fce545441eaf225311f
Klaus Hipp <khipp@users.noreply.github.com>
2024-09-13 11:08:27 +0200

    font-african-sans: add livecheck

--- Casks/font/font-a/font-african-sans.rb
+++ Casks/font/font-a/font-african-sans.rb
@@ -1,15 +1,20 @@
 cask "font-african-sans" do
-  version "9.380"
+  version "9.3.8"
   sha256 :no_check
 
   url "https://www.languagegeek.com/font/AfricanSans.zip"
   name "African Sans"
   homepage "https://www.languagegeek.com/font/fontdownload.html"
 
-  font "African Sans REGULAR 938.ttf"
-  font "African Sans BOLD 938.ttf"
-  font "African Sans BOLD ITALIC 938.ttf"
-  font "African Sans ITALIC 938.ttf"
+  livecheck do
+    url :homepage
+    regex(/African\s+Sans\s+Serif.*?\(\s*v?(\d+(?:\.\d+)+)[^)]*\)/i)
+  end
+
+  font "African Sans REGULAR #{version.no_dots}.ttf"
+  font "African Sans BOLD #{version.no_dots}.ttf"
+  font "African Sans BOLD ITALIC #{version.no_dots}.ttf"
+  font "African Sans ITALIC #{version.no_dots}.ttf"
 
   # No zap stanza required
 end
