98b086d1d84036422371d85b75341968b8fb1583
Klaus Hipp <khipp@users.noreply.github.com>
2024-09-13 18:12:01 +0200

    font-routed-gothic: add livecheck

--- Casks/font/font-r/font-routed-gothic.rb
+++ Casks/font/font-r/font-routed-gothic.rb
@@ -1,20 +1,21 @@
 cask "font-routed-gothic" do
   version "1.0.0"
-  sha256 "3ecffba109bf2619057815411253e5c6f47f82fedba38ade6c951e13a8b2ad5e"
+  sha256 "e0079b81fa068a4672f02585f7bc2910bf1535d8cd73b04d4a023bd2cbca361d"
 
-  url "https://webonastick.com/fonts/routed-gothic/download/routed-gothic-ttf-v#{version}.zip"
+  url "https://github.com/dse/routed-gothic/archive/refs/tags/v#{version}.tar.gz",
+      verified: "github.com/dse/routed-gothic/"
   name "Routed Gothic"
   homepage "https://webonastick.com/fonts/routed-gothic/"
 
-  font "routed-gothic-ttf-v#{version}/routed-gothic-half-italic.ttf"
-  font "routed-gothic-ttf-v#{version}/routed-gothic-italic.ttf"
-  font "routed-gothic-ttf-v#{version}/routed-gothic-narrow-half-italic.ttf"
-  font "routed-gothic-ttf-v#{version}/routed-gothic-narrow-italic.ttf"
-  font "routed-gothic-ttf-v#{version}/routed-gothic-narrow.ttf"
-  font "routed-gothic-ttf-v#{version}/routed-gothic-wide-half-italic.ttf"
-  font "routed-gothic-ttf-v#{version}/routed-gothic-wide-italic.ttf"
-  font "routed-gothic-ttf-v#{version}/routed-gothic-wide.ttf"
-  font "routed-gothic-ttf-v#{version}/routed-gothic.ttf"
+  font "routed-gothic-#{version}/dist/ttf/routed-gothic-half-italic.ttf"
+  font "routed-gothic-#{version}/dist/ttf/routed-gothic-italic.ttf"
+  font "routed-gothic-#{version}/dist/ttf/routed-gothic-narrow-half-italic.ttf"
+  font "routed-gothic-#{version}/dist/ttf/routed-gothic-narrow-italic.ttf"
+  font "routed-gothic-#{version}/dist/ttf/routed-gothic-narrow.ttf"
+  font "routed-gothic-#{version}/dist/ttf/routed-gothic-wide-half-italic.ttf"
+  font "routed-gothic-#{version}/dist/ttf/routed-gothic-wide-italic.ttf"
+  font "routed-gothic-#{version}/dist/ttf/routed-gothic-wide.ttf"
+  font "routed-gothic-#{version}/dist/ttf/routed-gothic.ttf"
 
   # No zap stanza required
 end
