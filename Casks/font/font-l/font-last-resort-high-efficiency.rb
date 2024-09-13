67eb02963f813768c574314521e3b48f1903366e
Klaus Hipp <khipp@users.noreply.github.com>
2024-09-13 19:07:52 +0200

    font-last-resort-high-efficiency 16.000

--- Casks/font/font-l/font-last-resort-high-efficiency.rb
+++ Casks/font/font-l/font-last-resort-high-efficiency.rb
@@ -1,6 +1,6 @@
 cask "font-last-resort-high-efficiency" do
-  version "15.100"
-  sha256 "74f93a8fb3ab8756e0e86cb3c2c0783fda10bdea6f979d5817869ba7284e56ca"
+  version "16.000"
+  sha256 "60c48abfc05e9f2ba33599c4dcba40105d1ece71d3761f0b26f75a38b5b17895"
 
   url "https://github.com/unicode-org/last-resort-font/releases/download/#{version}/LastResortHE-Regular.ttf"
   name "Last Resort High-Efficiency"
