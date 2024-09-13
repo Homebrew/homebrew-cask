096dfa9eae5d154412eeaf56c9af5785b796d82b
Dzmitry Neviadomski <nevack.d@gmail.com>
2024-09-13 18:21:39 +0300

    zerotier-one 1.14.1

--- Casks/z/zerotier-one.rb
+++ Casks/z/zerotier-one.rb
@@ -1,6 +1,6 @@
 cask "zerotier-one" do
-  version "1.14.0"
-  sha256 "7094abcd9525bd30d6682184e4cc955c58dfb3db0dae80e33130bff19aaa67ad"
+  version "1.14.1"
+  sha256 "fc974680cd91b20ee9045a39e271206172d0b887623f5ebbdd322cb3900e7526"
 
   url "https://download.zerotier.com/RELEASES/#{version}/dist/ZeroTier%20One.pkg"
   name "ZeroTier One"
