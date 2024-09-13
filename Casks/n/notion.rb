42bc6ea4cd5261bf0736c75fe7feddf9299d4c58
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-13 21:44:20 +0000

    notion 3.14.1

--- Casks/n/notion.rb
+++ Casks/n/notion.rb
@@ -2,9 +2,9 @@ cask "notion" do
   arch arm: "-arm64"
   livecheck_folder = on_arch_conditional arm: "arm64", intel: "latest"
 
-  version "3.14.0"
-  sha256 arm:   "281b681315f22fdb7919dabb7da40bec606cf0553e8d3dc3b95b1e280e22c048",
-         intel: "5d7b762f522754b74c78bd0f0ebf292e6750224ee7bc4a3837156bdb5722ab11"
+  version "3.14.1"
+  sha256 arm:   "8c96dc379938e66152d660b8f8978bdf509f16a14621e5b9187018da89c7f82c",
+         intel: "2e6be57cae39ce9602709b187108e7407b19702ae54e5f571aa7ec05a3af389b"
 
   url "https://desktop-release.notion-static.com/Notion-#{version}#{arch}.dmg",
       verified: "desktop-release.notion-static.com/"
