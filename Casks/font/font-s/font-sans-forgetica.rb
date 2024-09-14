b076c6bc99e3d13f73ec031f948ea81854a72636
Klaus Hipp <khipp@users.noreply.github.com>
2024-09-14 08:35:38 +0200

    font-sans-forgetica: disable

--- Casks/font/font-s/font-sans-forgetica.rb
+++ Casks/font/font-s/font-sans-forgetica.rb
@@ -6,6 +6,8 @@ cask "font-sans-forgetica" do
   name "Sans Forgetica"
   homepage "https://sansforgetica.rmit.edu.au/"
 
+  disable! date: "2024-09-13", because: :no_longer_available
+
   font "Sans Forgetica/SansForgetica-Regular.otf"
 
   # No zap stanza required
