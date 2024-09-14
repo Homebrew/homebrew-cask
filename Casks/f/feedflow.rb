fa36e4b10c812b4bb9bcb90ee2c2c854d4495d3e
Marco Gomiero <marco.gomiero.93@gmail.com>
2024-09-14 11:35:07 +0200

    Apply suggestions from code review
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/f/feedflow.rb
+++ Casks/f/feedflow.rb
@@ -1,11 +1,11 @@
 cask "feedflow" do
   version "1.0.77"
-  sha256 :no_check
+  sha256 "699b891da10e6dedd99956c159e444f79a24aa581e5c8c87a76ebd2dac044e84"
 
   url "https://github.com/prof18/feed-flow/releases/download/#{version}-desktop/FeedFlow-#{version}.dmg",
       verified: "github.com/prof18/feed-flow/"
   name "FeedFlow"
-  desc "Minimalistic RSS Reader"
+  desc "RSS reader"
   homepage "https://www.feedflow.dev/"
 
   livecheck do
@@ -24,5 +24,8 @@ cask "feedflow" do
 
   app "FeedFlow.app"
 
-  zap trash: "~/Library/Application Support/FeedFlow"
+  zap trash: [
+    "~/Library/Application Support/FeedFlow",
+    "~/Library/Saved Application State/com.prof18.feedflow.savedState",
+  ]
 end
