819f8d92a95eec4d3c41452b15ffc18956f89cf2
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-09-13 20:50:41 -0400

    shotcut 24.09.13

--- Casks/s/shotcut.rb
+++ Casks/s/shotcut.rb
@@ -1,6 +1,6 @@
 cask "shotcut" do
-  version "24.08.29"
-  sha256 "24c7da67038cc701bc921f899413e30762d84b36c39a6518b82caa983eba8466"
+  version "24.09.13"
+  sha256 "60ac07ccf48617ca3c344175a651698f80cd73068aa3b5a3c1cfb8c2831d2177"
 
   url "https://github.com/mltframework/shotcut/releases/download/v#{version}/shotcut-macos-#{version.no_dots}.dmg",
       verified: "github.com/mltframework/shotcut/"
