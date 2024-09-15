0ed78f6cff8c03a48ee0fcae6436f7876ee21802
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-09-15 10:52:02 -0400

    mailspring 1.14.0

--- Casks/m/mailspring.rb
+++ Casks/m/mailspring.rb
@@ -1,6 +1,6 @@
 cask "mailspring" do
-  version "1.13.3"
-  sha256 "99f7bb841b4b559b6a9ad28c044179907f8e1238797fa0c9979a410017605204"
+  version "1.14.0"
+  sha256 "1e7ffda30359062d0edf89cec8ac2e0fbf04727873715c2e6b7944d3f4dc5df9"
 
   url "https://github.com/Foundry376/Mailspring/releases/download/#{version}/Mailspring.zip",
       verified: "github.com/Foundry376/Mailspring/"
@@ -20,8 +20,4 @@ cask "mailspring" do
     "~/Library/Preferences/com.mailspring.*",
     "~/Library/Saved Application State/com.mailspring.*",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
