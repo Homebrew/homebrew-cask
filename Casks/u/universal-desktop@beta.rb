9db1942a6b45ff497d050ea2ed2cb1590c6a9b22
andre4ik3 <andre4ik3@fastmail.com>
2024-09-15 21:02:29 +0400

    Add Universal Desktop beta

---
+++ Casks/u/universal-desktop@beta.rb
@@ -0,0 +1,25 @@
+cask "universal-desktop@beta" do
+  version "1.1,65"
+  sha256 "fec78328bfd7afd613d636dd1d60b4ba75a745080a814f16c45c15dbb4c4ec9f"
+
+  url "https://github.com/adrianjagielak/universal-desktop-website/releases/download/v#{version.csv.first}-#{version.csv.second}/Universal.Desktop.dmg",
+      verified: "github.com/adrianjagielak/universal-desktop-website/"
+  name "Universal Desktop (Beta)"
+  desc "Stream Mac apps to Apple Vision Pro as native visionOS windows"
+  homepage "https://universaldesktop.app/"
+
+  livecheck do
+    url "https://www.universaldesktop.app/macos_host_beta_appcast.xml"
+    strategy :sparkle
+  end
+
+  auto_updates true
+  depends_on macos: ">= :monterey"
+
+  app "Universal Desktop.app"
+
+  zap trash: [
+    "~/Library/Preferences/dev.adrianjagielak.UniversalDesktop.plist",
+    "~/Library/HTTPStorages/dev.adrianjagielak.UniversalDesktop",
+  ]
+end
