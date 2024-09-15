e1629f55e9cc36030cd16d6136fd74af16cd3138
Klaus Hipp <khipp@users.noreply.github.com>
2024-09-15 09:45:09 +0200

    electricbinary: use default strategy

--- Casks/e/electricbinary.rb
+++ Casks/e/electricbinary.rb
@@ -2,13 +2,13 @@ cask "electricbinary" do
   version "9.07"
   sha256 "3e383f673183265700e0c13a5f7b1d3348247465c3d8817c845d62e4c92d86ce"
 
-  url "https://ftp.gnu.org/pub/gnu/electric/electricBinary-#{version}.jar"
+  url "https://ftp.gnu.org/gnu/electric/electricBinary-#{version}.jar"
   name "Electric VLSI Design System"
   desc "Electrical CAD system for the design of integrated circuits"
   homepage "https://www.gnu.org/software/electric/electric.html"
 
   livecheck do
-    url "https://ftp.gnu.org/pub/gnu/electric/"
+    url :url
     regex(/href=.*?electricBinary-(\d+(?:\.\d+)*)\.jar/i)
   end
 
