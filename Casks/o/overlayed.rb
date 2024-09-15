afe7f79b0543638ff6ef8bb6b421588248879630
Daeho Ro <lamanus@outlook.kr>
2024-09-15 20:49:56 +0900

    overlayed 0.6.0

--- Casks/o/overlayed.rb
+++ Casks/o/overlayed.rb
@@ -1,6 +1,6 @@
 cask "overlayed" do
-  version "0.5.0"
-  sha256 "73e7d3d0f946a0624045793e8f762e52ef7729c64589b42cc28a0c2cc70ccf3f"
+  version "0.6.0"
+  sha256 "c67b2a661d4a1f236981a1292dd8398f5dd0ad05e2fdf87c788b46a650dc19b4"
 
   url "https://github.com/overlayeddev/overlayed/releases/download/v#{version}/overlayed_#{version}_universal.dmg",
       verified: "github.com/overlayeddev/overlayed/"
