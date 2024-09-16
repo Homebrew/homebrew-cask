4c9de1746dc6c5e3334c2c3d8b30dba8c793aad6
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-09-16 09:04:36 -0400

    birdfont 6.8.1

--- Casks/b/birdfont.rb
+++ Casks/b/birdfont.rb
@@ -7,6 +7,10 @@ cask "birdfont" do
       url "https://birdfont.org/purchase.php"
       regex(%r{Mac\s*OS\s*10\.12.*?/birdfont[._-]v?(\d+(?:\.\d+)+)[._-]free\.dmg}i)
     end
+
+    caveats do
+      requires_rosetta
+    end
   end
   on_catalina :or_newer do
     on_ventura :or_older do
@@ -17,11 +21,15 @@ cask "birdfont" do
         url "https://birdfont.org/purchase.php"
         regex(%r{Mac\s*OS\s*10\.15.*?/birdfont[._-]v?(\d+(?:\.\d+)+)[._-]free\.dmg}i)
       end
+
+      caveats do
+        requires_rosetta
+      end
     end
   end
   on_sonoma :or_newer do
-    version "6.7.1"
-    sha256 "67041c24cd8de2585dc850081006d41e5169c456f3cee1d3d06f78e521378722"
+    version "6.8.1"
+    sha256 "bcd4f1c64ede96f6b1496a9ae8c88a0f7269e66698606ef3ce7d5f798593b325"
 
     livecheck do
       url "https://birdfont.org/purchase.php"
