d646b0381f9b164a50648c91415eb5b56e5432a0
Yusuf Kursad Kaya <ykursadkaya@gmail.com>
2024-09-16 22:22:14 +0300

    yaak 2024.9.0

--- Casks/y/yaak.rb
+++ Casks/y/yaak.rb
@@ -1,11 +1,12 @@
 cask "yaak" do
   arch arm: "aarch64", intel: "x64"
 
-  version "2024.8.2"
-  sha256 arm:   "d8c359e2444e7b94f18e1bc56d6cd8b630cde2d49ecbff3f4cbbb70c4544dcd0",
-         intel: "8432a2d4b426b3b885e6061cfc4709303e73513c031ce955eade36236f8b8401"
+  version "2024.9.0"
+  sha256 arm:   "195bfe6d6d80311e3d81b29c8366cd571efa31057b603206639f63015395c46e",
+         intel: "0cda068d2992e1289b434bebb9d20fd14f0c0a40bb77e558c8c15464344ff1e0"
 
-  url "https://releases.yaak.app/releases/#{version}/Yaak_#{version}_#{arch}.dmg"
+  url "https://github.com/yaakapp/app/releases/download/v#{version}/Yaak_#{version}_#{arch}.dmg",
+      verified: "github.com/yaakapp/app/"
   name "Yaak"
   desc "REST, GraphQL and gRPC client"
   homepage "https://yaak.app/"
