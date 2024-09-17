171c194cf3311d4035cdfd11a3979c50aa72f0c8
Lex.Chen <ctyytc@gmail.com>
2024-09-17 13:54:57 +0800

    font-lxgw-neoxihei 1.206

--- Casks/font/font-l/font-lxgw-neoxihei.rb
+++ Casks/font/font-l/font-lxgw-neoxihei.rb
@@ -1,6 +1,6 @@
 cask "font-lxgw-neoxihei" do
-  version "1.205"
-  sha256 "f3a11e9e08f39a1984566ef23a8bd117fa06ee2f27961ccb2f8e0df23fe77fdc"
+  version "1.206"
+  sha256 "8ec2d02c3e5d7f382e16493b437a077b925dea1f9703f42483d32acaa1d0f2fe"
 
   url "https://github.com/lxgw/LxgwNeoXiHei/releases/download/v#{version}/LXGWNeoXiHei.ttf"
   name "LXGW NeoXiHei"
