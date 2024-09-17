6a5721c39d6414c242173fab8135d092b91fded5
Keeyou <keeyou-cn@outlook.com>
2024-09-17 08:48:32 +0800

    yass 1.14.0

--- Casks/y/yass.rb
+++ Casks/y/yass.rb
@@ -1,9 +1,9 @@
 cask "yass" do
   arch arm: "-arm64", intel: "-x64"
 
-  version "1.13.2"
-  sha256 arm:   "3bc3b9600069dd059df7bc376c8adf008da6a881a09863b49f5b2492930ce599",
-         intel: "f4b3750713032e2ea2d3d55c1f3e5c81342608834493d32e0a626e39be9caf15"
+  version "1.14.0"
+  sha256 arm:   "7dc08ad0a6dc0da8ffc91349fd5c647942aa7a308917a675cdc2e2c2487a341b",
+         intel: "e0a6d9da63df421bed2315f2cae4ab66d5dadaa4c0776ae2c9a0ee8eeb752684"
 
   url "https://github.com/Chilledheart/yass/releases/download/#{version}/yass-macos-release#{arch}-#{version}.dmg"
   name "Yet Another Shadow Socket"
