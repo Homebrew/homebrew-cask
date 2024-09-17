cfe5a867dfaab51b9e5bcda3f6a2351529881860
Henry Wilkinson <henry@wilkinson.graphics>
2024-09-17 11:53:58 -0400

    Add min version

--- Casks/a/archivewebpage.rb
+++ Casks/a/archivewebpage.rb
@@ -6,24 +6,24 @@ cask "archivewebpage" do
       verified: "github.com/webrecorder/archiveweb.page/"
   name "ArchiveWeb.page"
   desc "Archive webpages manually to WARC or WACZ files as you browse the web"
-  homepage "https://archiveweb.page"
+  homepage "https://archiveweb.page/"
 
   livecheck do
     url :url
     strategy :github_latest
   end
 
-  depends_on macos: ""
+  depends_on macos: ">= :big_sur"
 
   app "ArchiveWeb.page.app"
 
   zap trash: [
     "~/Library/Application Support/ArchiveWeb.page",
-    "~/Library/Logs/ArchiveWeb.page",
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/net.webrecorder.archivewebpage.sfl3",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/net.webrecorder.archivewebpage.sfl*",
     "~/Library/Caches/net.webrecorder.archivewebpage",
     "~/Library/Caches/net.webrecorder.archivewebpage.ShipIt",
     "~/Library/HTTPStorages/net.webrecorder.archivewebpage",
+    "~/Library/Logs/ArchiveWeb.page",
     "~/Library/Preferences/net.webrecorder.archivewebpage.plst",
     "~/Library/Saved Application State/net.webrecorder.archivewebpage.savedState",
   ]
