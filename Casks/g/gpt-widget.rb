c66b448b1401cb7e84a48e7f5f200cc715fc75c9
Abhijeet Dash <ad@Abhijeets-MacBook-Pro.local>
2024-09-17 20:05:08 +0530

    A simple tool which let's you access Chat-GPT|Gemini|Ollama with ctrl+space

---
+++ Casks/g/gpt-widget.rb
@@ -0,0 +1,11 @@
+cask 'gpt-widget' do
+  version '1.0.2+1'
+  sha256 '6d9df14af55a22bb41f8eb2ca47957cdc65b7565c0f3239ff65f7e096d451be7'
+  
+  url 'https://github.com/AbhijeetDash/gpt_window/releases/download/Beta/gptwidget.1.0.2.dmg'
+  name 'GPT Widget'
+  desc 'A quick access tool for AI Chatbots'
+  homepage 'https://github.com/AbhijeetDash/gpt_window'
+
+  app 'gptwidget.app'
+end
