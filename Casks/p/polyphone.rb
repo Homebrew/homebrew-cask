c82d1949519a45fdd7c0a813d5319c497a1faf6f
Bevan Kay <email@bevankay.me>
2024-09-17 21:23:54 +1000

    polyphone 2.4

--- Casks/p/polyphone.rb
+++ Casks/p/polyphone.rb
@@ -1,15 +1,15 @@
 cask "polyphone" do
-  version "2.3.1,84"
-  sha256 "46d3abf42f88cc157d7f2d751619d7c599789ebef65b2d31f5873342f1184e9d"
+  version "2.4,95"
+  sha256 "d2791bba43fdefdc8c3c7fce5bf11d6ff9eb267495f78315931e9ac2251d082f"
 
-  url "https://www.polyphone.io/download/0/v#{version.csv.second}/polyphone_#{version.csv.first}.dmg"
+  url "https://www.polyphone.io/download/0/v#{version.csv.second}/Polyphone-macOS_11-15.dmg"
   name "Polyphone"
   desc "Soundfont editor for quickly designing musical instruments"
   homepage "https://www.polyphone.io/en"
 
   livecheck do
     url "https://www.polyphone.io/en/software"
-    regex(/Polyphone\s+(\d+(?:\.\d+)+).*download\?file_id=(\d+).*Mac OS X/im)
+    regex(/Polyphone\s+(\d+(?:\.\d+)+).*download\?file_id=(\d+).*Mac OS \(/im)
     strategy :page_match do |page, regex|
       match = page.match(regex)
       next if match.blank?
