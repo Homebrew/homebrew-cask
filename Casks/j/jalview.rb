f78ae29539f3b3aaeeb4c087c3f08957184c980e
Daeho Ro <lamanus@outlook.kr>
2024-09-18 19:53:44 +0900

    jalview 2.11.4.0

--- Casks/j/jalview.rb
+++ Casks/j/jalview.rb
@@ -1,9 +1,9 @@
 cask "jalview" do
   arch arm: "aarch64", intel: "x64"
 
-  version "2.11.3.3"
-  sha256 arm:   "b2414604e75ed029c7b520f1442ff3e3df00f677d88dd7068141ea8126a24423",
-         intel: "47bb1f11b5fc2b94479818ae615b2752cc4286684fd8a5f5f5619a0728fb99e0"
+  version "2.11.4.0"
+  sha256 arm:   "7c2c66b0dcf6e3aec70800870d9f4cdb97df5cb0b5427dbbed2ddd5f7a32c530",
+         intel: "fbd3f59ee0575fbe80df58da154042b751d2a151e9262ae98bb54957a58592a8"
 
   url "https://www.jalview.org/downloads/installers/release/Jalview-#{version.dots_to_underscores}-macos-#{arch}-java_8.dmg"
   name "Jalview"
