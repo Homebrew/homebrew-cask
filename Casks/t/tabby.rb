0d14b5c2c05a296f965a89cf63c3471031179fb7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-18 06:56:24 +0000

    tabby 1.0.212

--- Casks/t/tabby.rb
+++ Casks/t/tabby.rb
@@ -1,9 +1,9 @@
 cask "tabby" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.0.211"
-  sha256 arm:   "4cd46ad9d9a0a856a0b41e48a9466e190cf48f197019cebe2ca14a52add25149",
-         intel: "ce46751727761c7222df2f0616c9ae246d9f429c5c1ace95226758ec05828cf8"
+  version "1.0.212"
+  sha256 arm:   "00543ccf5e0e4d41639dd30cd6fd2649a88fb4ca5089171d4205b0a87f17e55b",
+         intel: "21eb1a6d2b6100534eb42fc253b1a4411946c91db3022d1254d95e519d9bde7d"
 
   url "https://github.com/Eugeny/tabby/releases/download/v#{version}/tabby-#{version}-macos-#{arch}.zip",
       verified: "github.com/Eugeny/tabby/"
