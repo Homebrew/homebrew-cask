11d026fa125e2f076279846e7e480152b24b5d4b
Daeho Ro <lamanus@outlook.kr>
2024-09-18 19:32:50 +0900

    parallels-virtualization-sdk 20.0.1-55659

--- Casks/p/parallels-virtualization-sdk.rb
+++ Casks/p/parallels-virtualization-sdk.rb
@@ -1,6 +1,6 @@
 cask "parallels-virtualization-sdk" do
-  version "20.0.0-55653"
-  sha256 "1b2b3a2c239a888036ec126a22a7b7a1252144d7babcf9bee0531cc696a2c7b0"
+  version "20.0.1-55659"
+  sha256 "c80b25d9f914b82f1622c17405865f31b6726b8a24d128e1d567c3650ece0e8a"
 
   url "https://download.parallels.com/desktop/v#{version.major}/#{version}/ParallelsVirtualizationSDK-#{version}-mac.dmg"
   name "Parallels Virtualization SDK"
