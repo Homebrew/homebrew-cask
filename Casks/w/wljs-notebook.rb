204ac496e106ab2e3bfb8bebc00f04ec72c4e506
Sam Ford <1584702+samford@users.noreply.github.com>
2024-09-17 21:36:21 -0400

    wljs-notebook: update livecheck
    Thinking about this some more, if upstream updates the tag format to
    include a leading `v` and we omit it in `tag_version`, we need to
    make sure to include the original tag in the cask `version (as using
    `tag_version` would lead to a broken URL).
    
    This also updates the language of the `livecheck` block comment to
    something that's not tied to a particular point in time (i.e.,
    "current version" would refer to a different version in the future).

--- Casks/w/wljs-notebook.rb
+++ Casks/w/wljs-notebook.rb
@@ -10,8 +10,9 @@ cask "wljs-notebook" do
   desc "Javascript frontend for Wolfram Engine"
   homepage "https://github.com/JerryI/wolfram-js-frontend"
 
-  # The current version of wljs-notebook has a tag that differs
-  # from the version present in the filename
+  # The upstream release tag can sometimes differ from the version in the
+  # filename (e.g. 2.5.6FIX vs. 2.5.6), so we include the tag in the cask
+  # `version` when this happens.
   livecheck do
     url :url
     regex(/wljs[._-]notebook[._-]v?(\d+(?:\.\d+)+)/i)
@@ -19,14 +20,15 @@ cask "wljs-notebook" do
       json.map do |release|
         next if release["draft"] || release["prerelease"]
 
-        tag_version = release["tag_name"][/^v?(\d+(?:\.\d+)+.*)$/i, 1]
+        tag = release["tag_name"]
+        tag_version = tag[/^v?(\d+(?:\.\d+)+.*)$/i, 1]
         next if tag_version.blank?
 
         release["assets"]&.map do |asset|
           match = asset["name"]&.match(regex)
           next if match.blank?
 
-          (match[1] == tag_version) ? tag_version : "#{match[1]},#{tag_version}"
+          (match[1] == tag_version) ? tag_version : "#{match[1]},#{tag}"
         end
       end.flatten
     end
