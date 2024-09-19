80f50002301eb75f4b9c44a7f077379cfe535136
Klaus Hipp <khipp@users.noreply.github.com>
2024-09-19 09:28:57 +0200

    dingtalk: update livecheck

--- Casks/d/dingtalk.rb
+++ Casks/d/dingtalk.rb
@@ -1,8 +1,8 @@
 cask "dingtalk" do
-  version "7.6.15_40104038"
+  version "7.6.15,40104038"
   sha256 "2a733ecaa5274a347931f3d5eca64812e82573bb2cc6dc7a3c1bdfab24747e47"
 
-  url "https://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/M1-Beta/DingTalk_v#{version}_universal.dmg"
+  url "https://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/M1-Beta/DingTalk_v#{version.csv.first}_#{version.csv.second}_universal.dmg"
   name "DingTalk"
   name "钉钉"
   desc "Teamwork app by Alibaba Group"
@@ -10,10 +10,7 @@ cask "dingtalk" do
 
   livecheck do
     url "https://im.dingtalk.com/manifest/appcast_gray_release.xml"
-    regex(/DingTalk[._-]v?(\d+(?:[._]\d+)+)[._-]universal\.dmg/i)
-    strategy :sparkle do |item|
-      item.url[regex, 1]
-    end
+    strategy :sparkle
   end
 
   auto_updates true
