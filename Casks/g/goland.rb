faa70d7dda547bc8064727ad4c023f0d6aa1e36f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-19 15:51:36 +0000

    goland 2024.2.2

--- Casks/g/goland.rb
+++ Casks/g/goland.rb
@@ -1,9 +1,9 @@
 cask "goland" do
   arch arm: "-aarch64"
 
-  version "2024.2.1.1,242.21829.220"
-  sha256 arm:   "0ba62dbed71550a9c1e1535f6a1d6dbfe826d0bf1f7da84e40d9dee9b8e358b8",
-         intel: "44c84b628665b4778ae4981419b796f0eed3942d43a99b2f7692706452aef493"
+  version "2024.2.2,242.22855.85"
+  sha256 arm:   "a1adfa54a4055fbd833e6b161c848c64ff1ae13b72356eb8febd1c22ea6a5c45",
+         intel: "d788731f83254780e612234265976cbdcbf7bb334aa2bd7c610795d7fb843948"
 
   url "https://download.jetbrains.com/go/goland-#{version.csv.first}#{arch}.dmg"
   name "Goland"
