dcfe9b63c7421df9c878870cdbe7815245963c4e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-19 07:18:29 +0000

    webull 8.3.4

--- Casks/w/webull.rb
+++ Casks/w/webull.rb
@@ -1,6 +1,6 @@
 cask "webull" do
-  version "8.3.2,80003019"
-  sha256 "2a742dbaa262803426101e13dc26c74141b9e1e3264b2eec70f1b1ddaa986bfe"
+  version "8.3.4,80003032"
+  sha256 "3d7f31db97109621dbcedd4b6033507e694b6c7422aec78ee26e3a6d812da3fe"
 
   url "https://u1sweb.webullfintech.com/us/Webull%20Desktop_#{version.csv.first}_#{version.csv.second}_global_universalsigned.dmg",
       verified: "u1sweb.webullfintech.com/us/"
