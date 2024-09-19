5edcfdfafedf4de7d9dd6aa77f1a20b107168b9f
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-09-18 20:36:01 -0400

    weasis 4.5.1

--- Casks/w/weasis.rb
+++ Casks/w/weasis.rb
@@ -1,9 +1,9 @@
 cask "weasis" do
   arch arm: "aarch64", intel: "x86-64"
 
-  version "4.5.0"
-  sha256 arm:   "eb483435041ebe587c906b570067538aa39888527a52ae42561fe299df1f6fd1",
-         intel: "4918d3142f8619800d2f46156781529ca463ebcb0a746d3e5124af68ccf1de34"
+  version "4.5.1"
+  sha256 arm:   "c9248037c6fd15af73227c80f97ef855f218a958aeec70379e606c82cb8b8257",
+         intel: "b6fcda972e5caf6865fedc3f4f9898e13a04257a6891afa06623d8390bf55422"
 
   url "https://github.com/nroduit/Weasis/releases/download/v#{version}/Weasis-#{version}-#{arch}.pkg"
   name "Weasis"
