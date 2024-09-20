9ea942cb0145832a4b8424f8bd0d7793a8054d8f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-20 00:54:02 +0000

    java@beta 24,16

--- Casks/j/java@beta.rb
+++ Casks/j/java@beta.rb
@@ -1,9 +1,9 @@
 cask "java@beta" do
   arch arm: "aarch64", intel: "x64"
 
-  version "24,15"
-  sha256 arm:   "675ac357bc6ff6cddaf1c11d7ac55a834989bb50863217b9dd608db2e3da9882",
-         intel: "87d00c34c019b1912b6f99a85b42cc9865a98b21f9aead70f6390cfa6a0430f1"
+  version "24,16"
+  sha256 arm:   "d032f02118d572e438745c2db459cb493ad2470510e4bd1bf914336cdf9a76f9",
+         intel: "d836dc1df1200fb325de46e6cfc701870b448a5f7547647ef50c3c6a3feced47"
 
   url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.csv.second}/GPL/openjdk-#{version.csv.first}-ea+#{version.csv.second}_macos-#{arch}_bin.tar.gz"
   name "OpenJDK Early Access Java Development Kit"
