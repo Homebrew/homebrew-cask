2a199bc9366df59a8b5060973098d45132ba31db
Bevan Kay <email@bevankay.me>
2024-09-20 21:59:48 +1000

    livebook 0.14.1

--- Casks/l/livebook.rb
+++ Casks/l/livebook.rb
@@ -1,12 +1,17 @@
 cask "livebook" do
-  version "0.14.0"
-  sha256 "edbf8110be20e22d57ecca5296721a7ba39d088a814d0100bd6671f24618e0b3"
+  version "0.14.1"
+  sha256 "d7486ea1d67f6825a52582a500208f71cae61b8234b125bc8f201aeabf9bc797"
 
   url "https://github.com/livebook-dev/livebook/releases/download/v#{version}/LivebookInstall-v#{version}-macos-universal.dmg",
       verified: "github.com/livebook-dev/livebook/"
   name "Livebook"
   desc "Code notebooks for Elixir developers"
   homepage "https://livebook.dev/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "Livebook.app"
 
