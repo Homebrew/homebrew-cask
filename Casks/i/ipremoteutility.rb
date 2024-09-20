163859fcbcbbe6fbc202d095a3fd85fc84d80e25
Bevan Kay <email@bevankay.me>
2024-09-20 21:34:07 +1000

    ipremoteutility 1.9.6

--- Casks/i/ipremoteutility.rb
+++ Casks/i/ipremoteutility.rb
@@ -1,6 +1,6 @@
 cask "ipremoteutility" do
-  version "1.9.5"
-  sha256 "a832a28000356830daafecc3fec4aa18a6dc1bfdaeab84ed1a3113b9cf0ae726"
+  version "1.9.6"
+  sha256 "f52a771b208b21774e45a1c69d538edc82fd09bc6632b3d8fd6a6d3234b00a90"
 
   url "https://www.flandersscientific.com/ip-remote/release/IPRemoteUtility-#{version}-macOS.zip"
   name "Flanders IP Remote Utility"
@@ -13,6 +13,7 @@ cask "ipremoteutility" do
   end
 
   depends_on macos: ">= :high_sierra"
+  container nested: "IPRemoteUtility-#{version} - ReleaseV3-macOSX/IPRemoteUtility.dmg"
 
   app "IPRemoteUtility.app"
 
