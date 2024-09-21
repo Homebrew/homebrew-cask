68b19aee6005c0012d57976afa759e943f736833
Sam Ford <1584702+samford@users.noreply.github.com>
2024-09-20 23:05:36 -0400

    8/8bitdo-ultimate-software: update url, livecheck

--- Casks/8/8bitdo-ultimate-software.rb
+++ Casks/8/8bitdo-ultimate-software.rb
@@ -2,13 +2,13 @@ cask "8bitdo-ultimate-software" do
   version "2.0.6"
   sha256 "21c0af67e6aad0cda117d0ded8d964abf51f2957ab1c7caf0258fdc3e0178abb"
 
-  url "http://tools.8bitdo.com/8BitdoUltimateSoftware/8BitDoUltimateSoftwareV#{version}.zip"
+  url "https://tools.8bitdo.com/8BitdoUltimateSoftware/8BitDoUltimateSoftwareV#{version}.zip"
   name "8BitDo Ultimate Software"
   desc "Control every piece of your controller"
   homepage "https://support.8bitdo.com/ultimate-software.html"
 
   livecheck do
-    url "http://tools.8bitdo.com/8BitdoUltimateSoftware/appcast.xml"
+    url "https://tools.8bitdo.com/8BitdoUltimateSoftware/appcast.xml"
     strategy :sparkle
   end
 
