f50b4ce634c6ec424044a039ea34b4b49c6f7a61
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-09-20 21:56:02 -0400

    rize 1.3.27

--- Casks/r/rize.rb
+++ Casks/r/rize.rb
@@ -1,9 +1,9 @@
 cask "rize" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.3.6"
-  sha256  arm:   "324afeb1a420ec8343ae356783463f342c1fe7eba92bc9421762426f23d352cf",
-          intel: "1f3800f60a72a206bc784bcc0cdab8074c6f78a1706ea5af1c0ab981244c853b"
+  version "1.3.27"
+  sha256  arm:   "439ee88f2a8e40e14501b5c4777c5d6c82b58a44b95a7bf426de67464a99e414",
+          intel: "542fa0e5c344227cf4c8dd1572045d1438a087d61f3d5f270930943eb3cd2085"
 
   url "https://github.com/rize-io/lua/releases/download/v#{version}/Rize-#{version}-#{arch}.dmg",
       verified: "github.com/rize-io/lua/"
