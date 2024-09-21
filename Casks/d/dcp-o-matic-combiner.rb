fc1007425b38ded56852d38bd59956c802e41105
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-21 09:37:50 +0000

    dcp-o-matic-combiner 2.16.93

--- Casks/d/dcp-o-matic-combiner.rb
+++ Casks/d/dcp-o-matic-combiner.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-combiner" do
-  version "2.16.92"
-  sha256 "b539328aa9346db70934028a109fff69bfaf2aff65c5012646aa6321f1834d68"
+  version "2.16.93"
+  sha256 "2054f5323574c1b9ddffcb79d620cbe69594420f01f7cc0e20797183ec9e7d55"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-combiner&version=#{version}"
   name "DCP-o-matic-combiner"
