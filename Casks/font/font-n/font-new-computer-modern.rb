0dcf0881c32f513e8263025d630a0950cf7a06a2
Peng Guanwen <pg999w@outlook.com>
2024-09-21 15:43:22 +0800

    font-new-computer-modern (new cask)

---
+++ Casks/font/font-n/font-new-computer-modern.rb
@@ -0,0 +1,56 @@
+cask "font-new-computer-modern" do
+  version "6.0.0"
+  sha256 "1013f36abab243e74ad892c443358896a6bdbc3e295286873e95c04cf8fe13c4"
+
+  url "https://download.gnu.org.ua/release/newcm/newcm-#{version}.txz"
+  name "New Computer Modern"
+  homepage "https://download.gnu.org.ua/release/newcm/"
+
+  livecheck do
+    url "https://download.gnu.org.ua/release/newcm/"
+    regex(/newcm-(\d+(?:\.\d+)+)\.txz/i)
+  end
+
+  font "newcm-6.0.0/otf/NewCM08-Book.otf"
+  font "newcm-6.0.0/otf/NewCM08-BookItalic.otf"
+  font "newcm-6.0.0/otf/NewCM08-Italic.otf"
+  font "newcm-6.0.0/otf/NewCM08-Regular.otf"
+  font "newcm-6.0.0/otf/NewCM08Devanagari-Book.otf"
+  font "newcm-6.0.0/otf/NewCM08Devanagari-Regular.otf"
+  font "newcm-6.0.0/otf/NewCM10-Bold.otf"
+  font "newcm-6.0.0/otf/NewCM10-BoldItalic.otf"
+  font "newcm-6.0.0/otf/NewCM10-Book.otf"
+  font "newcm-6.0.0/otf/NewCM10-BookItalic.otf"
+  font "newcm-6.0.0/otf/NewCM10-Italic.otf"
+  font "newcm-6.0.0/otf/NewCM10-Regular.otf"
+  font "newcm-6.0.0/otf/NewCM10Devanagari-Bold.otf"
+  font "newcm-6.0.0/otf/NewCM10Devanagari-Book.otf"
+  font "newcm-6.0.0/otf/NewCM10Devanagari-Regular.otf"
+  font "newcm-6.0.0/otf/NewCMMath-Bold.otf"
+  font "newcm-6.0.0/otf/NewCMMath-Book.otf"
+  font "newcm-6.0.0/otf/NewCMMath-Regular.otf"
+  font "newcm-6.0.0/otf/NewCMMono10-Bold.otf"
+  font "newcm-6.0.0/otf/NewCMMono10-BoldOblique.otf"
+  font "newcm-6.0.0/otf/NewCMMono10-Book.otf"
+  font "newcm-6.0.0/otf/NewCMMono10-BookItalic.otf"
+  font "newcm-6.0.0/otf/NewCMMono10-Italic.otf"
+  font "newcm-6.0.0/otf/NewCMMono10-Regular.otf"
+  font "newcm-6.0.0/otf/NewCMSans08-Book.otf"
+  font "newcm-6.0.0/otf/NewCMSans08-BookOblique.otf"
+  font "newcm-6.0.0/otf/NewCMSans08-Oblique.otf"
+  font "newcm-6.0.0/otf/NewCMSans08-Regular.otf"
+  font "newcm-6.0.0/otf/NewCMSans10-Bold.otf"
+  font "newcm-6.0.0/otf/NewCMSans10-BoldOblique.otf"
+  font "newcm-6.0.0/otf/NewCMSans10-Book.otf"
+  font "newcm-6.0.0/otf/NewCMSans10-BookOblique.otf"
+  font "newcm-6.0.0/otf/NewCMSans10-Oblique.otf"
+  font "newcm-6.0.0/otf/NewCMSans10-Regular.otf"
+  font "newcm-6.0.0/otf/NewCMUncial08-Bold.otf"
+  font "newcm-6.0.0/otf/NewCMUncial08-Book.otf"
+  font "newcm-6.0.0/otf/NewCMUncial08-Regular.otf"
+  font "newcm-6.0.0/otf/NewCMUncial10-Bold.otf"
+  font "newcm-6.0.0/otf/NewCMUncial10-Book.otf"
+  font "newcm-6.0.0/otf/NewCMUncial10-Regular.otf"
+
+  # No zap stanza required
+end
