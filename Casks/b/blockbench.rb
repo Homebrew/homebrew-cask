1aea6088dee78ef7a2f9cf8cfc6ac0f9b39a084f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-22 18:36:08 +0000

    blockbench 4.11.0

--- Casks/b/blockbench.rb
+++ Casks/b/blockbench.rb
@@ -1,9 +1,9 @@
 cask "blockbench" do
   arch arm: "arm64", intel: "x64"
 
-  version "4.10.4"
-  sha256 arm:   "281227fa7b454307d75a530bd68781c8ff7aaf3cf5dca8db2886025f04319abe",
-         intel: "4904607e39774b8b9370e8e7729f0a07d78070deeff7846e4bde9c1ae4c42519"
+  version "4.11.0"
+  sha256 arm:   "5d373cd3e428dca36d3511d655857bea92656065e928be076691135fb42e3537",
+         intel: "6a66024e18ebd12e4454c73ca84d2bdeb7b75a2faddc6811aaacb03cddc5bece"
 
   url "https://github.com/JannisX11/blockbench/releases/download/v#{version}/Blockbench_#{arch}_#{version}.dmg",
       verified: "github.com/JannisX11/blockbench/"
