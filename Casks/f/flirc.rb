bf4ad15194fc201caaf92e79a56ec151854af231
Klaus Hipp <khipp@users.noreply.github.com>
2024-09-24 21:40:39 +0200

    flirc: fix sha256

--- Casks/f/flirc.rb
+++ Casks/f/flirc.rb
@@ -1,6 +1,6 @@
 cask "flirc" do
   version "3.27.16"
-  sha256 "b75945cb1f175c1828330145035467a1c6116478ea0cdee12989b279f9205734"
+  sha256 "a872e83010782388b8aa061a7f89b4cd69fdb5308ae14644480f4137f12294ba"
 
   url "https://flirc.com/software/flirc-usb/GUI/release/mac/Flirc-#{version}.dmg"
   name "Flirc"
