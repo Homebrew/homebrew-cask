523c69acde207eed022615fc517ff0b3cfc48c41
Rylan Polster <rslpolster@gmail.com>
2024-09-24 17:59:04 -0400

    vlc@nightly: replace `url do` with versioned URL

--- Casks/v/vlc@nightly.rb
+++ Casks/v/vlc@nightly.rb
@@ -1,19 +1,32 @@
 cask "vlc@nightly" do
   arch arm: "arm64", intel: "x86_64"
 
-  version :latest
-  sha256 :no_check
+  on_arm do
+    version "20240924-0413,daab68e6"
+    sha256 "4ce9455a08682876d8a9660883d305d74e6edb144e9d830d5b81f8489babd6f2"
 
-  url "https://artifacts.videolan.org/vlc/nightly-macos-#{arch}/" do |page|
-    folder_path = page[%r{\d+-\d+/}]
-    url URI.join(page.url, folder_path) do |version_page|
-      file_path = version_page[/href="([^"]+.dmg)"/, 1]
-      URI.join(version_page.url, file_path)
-    end
+    url "https://artifacts.videolan.org/vlc/nightly-macos-#{arch}/#{version.csv.first}/vlc-4.0.0-dev-arm64-#{version.csv.second}.dmg"
   end
+  on_intel do
+    version "20240924-0416,daab68e6"
+    sha256 "f617a21edee78c509a8dbb31dbac8cd27705a48188adec88a2e53c2615c1a6e8"
+
+    url "https://artifacts.videolan.org/vlc/nightly-macos-#{arch}/#{version.csv.first}/vlc-4.0.0-dev-intel64-#{version.csv.second}.dmg"
+  end
+
   name "VLC media player"
   desc "Open-source cross-platform multimedia player"
   homepage "https://www.videolan.org/vlc/"
+
+  livecheck do
+    url "https://artifacts.videolan.org/vlc/nightly-macos-#{arch}/"
+    strategy :page_match do |page|
+      date = page[%r{href="(\d+-\d+)/"}, 1]
+      filename_arch = (arch == "x86_64") ? "intel64" : arch
+      version_page = Homebrew::Livecheck::Strategy.page_content("https://artifacts.videolan.org/vlc/nightly-macos-#{arch}/#{date}/")
+      "#{date},#{version_page[:content].scan(/href=.*?vlc-4\.0\.0-dev-#{filename_arch}-([0-9a-f]+)\.dmg/i).flatten.first}"
+    end
+  end
 
   deprecate! date: "2025-05-01", because: :unsigned
 
