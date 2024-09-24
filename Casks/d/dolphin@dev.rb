8565c7cefefea0ec44aec10769f50fea073ca2eb
Rylan Polster <rslpolster@gmail.com>
2024-09-24 16:25:52 -0400

    dolphin@dev: replace `url do` with versioned URL

--- Casks/d/dolphin@dev.rb
+++ Casks/d/dolphin@dev.rb
@@ -1,13 +1,16 @@
 cask "dolphin@dev" do
-  version :latest
-  sha256 :no_check
+  version "2409-45"
+  sha256 "17046a69f8cceda3a239f6b19e306545988a9c10e16429d355d648cfc2f8891a"
 
-  url "https://dolphin-emu.org/download/list/master/1/" do |page|
-    page[/href="([^"]+\.dmg)"/, 1]
-  end
+  url "https://dl.dolphin-emu.org/builds/d5/33/dolphin-master-#{version}-universal.dmg"
   name "Dolphin Dev"
   desc "Emulator to play GameCube and Wii games"
   homepage "https://dolphin-emu.org/"
+
+  livecheck do
+    url "https://dolphin-emu.org/download/list/master/1/"
+    regex(/href=.*?dolphin[._-]master[._-]v?(\d+(?:[._-]\d+)+)-universal\.dmg/i)
+  end
 
   conflicts_with cask: [
     "dolphin",
