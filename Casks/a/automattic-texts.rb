49abc3860fa81dd16608de1da111e7a0d01de99d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-24 00:48:07 +0000

    automattic-texts 0.84.0

--- Casks/a/automattic-texts.rb
+++ Casks/a/automattic-texts.rb
@@ -2,12 +2,12 @@ cask "automattic-texts" do
   arch arm: "arm64", intel: "x64"
 
   on_arm do
-    version "0.83.23,0fa306a3cc"
-    sha256 "04d48341d259c88c6b66eeeb2dac3a3d75527babcf621c28ac1de378cbc31e34"
+    version "0.84.0,8d3e5d6cdf"
+    sha256 "126c49d072baa0f2f0c975dcd48a92808dd670f7208fddecf04a5891e2430ee6"
   end
   on_intel do
-    version "0.83.23,46e69ba363"
-    sha256 "37e204ddc6b7c3e31f9c0f092e559c6bd85550445a457313cbbced0a89ffa388"
+    version "0.84.0,2805a1297d"
+    sha256 "e794b5b4c35f4af06abc6cd3fcb2302d8b8db5887ff56de1e6b730e1d8390ca7"
   end
 
   url "https://texts-binaries.texts.com/builds/Texts-macOS-#{arch}-v#{version.csv.first}-#{version.csv.second}.zip"
