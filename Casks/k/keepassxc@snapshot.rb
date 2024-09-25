2ae49cfcc6aa840248b1dbaa60531ca074b40bc6
Sam Ford <1584702+samford@users.noreply.github.com>
2024-09-25 13:08:08 -0400

    keepassxc@snapshot: update version, livecheck
    All of the recent snapshot builds of `keepassxc` use a 2.8.0 version
    in the filename. If we use the dmg file in the `/latest/` directory
    and the version doesn't change between builds, then we will get a
    checksum mismatch when the build is updated. Instead, we have to use
    a fully-versioned URL that uses a build directory instead, as this
    will remain static between builds.
    
    Similarly, `/latest/` is an actual directory and not just a url that
    redirects to the latest build, so checking the dmg file in that
    directory won't give us the full URL with the build number in it.
    With this setup, we have to find the newest build directory number
    on the main snapshots page, then check the directory listing page
    for that build directory to find the version in the dmg file name.

--- Casks/k/keepassxc@snapshot.rb
+++ Casks/k/keepassxc@snapshot.rb
@@ -1,15 +1,32 @@
 cask "keepassxc@snapshot" do
-  version "2.8.0"
+  version "2.8.0,252895"
   sha256 "bde7044cc2b7bb30c01d3991dcc756da052d2ce5538d56a789323f3168426221"
 
-  url "https://snapshot.keepassxc.org/latest/KeePassXC-#{version}-snapshot.dmg"
+  url "https://snapshot.keepassxc.org/build-#{version.csv.second}/KeePassXC-#{version.csv.first}-snapshot.dmg"
   name "KeePassXC"
   desc "Password manager app"
   homepage "https://keepassxc.org/"
 
   livecheck do
-    url "https://snapshot.keepassxc.org/latest/"
-    regex(/href=.*?KeePassXC[._-]v?(\d+(?:[._-]\d+)+)-snapshot\.dmg/i)
+    url "https://snapshot.keepassxc.org/"
+    regex(/href=.*?KeePassXC[._-]v?(\d+(?:\.\d+)+)-snapshot\.dmg/i)
+    strategy :page_match do |page, regex|
+      # Identify build numbers from directories like `build-123456`
+      newest_build = page.scan(%r{href=["']?build[._-]v?(\d+(?:\.\d+)*)/?["' >]}i)
+                         .flatten
+                         .uniq
+                         .max
+      next if newest_build.blank?
+
+      # Fetch the directory listing page for newest build
+      build_response = Homebrew::Livecheck::Strategy.page_content("https://snapshot.keepassxc.org/build-#{newest_build}/")
+      next if (build_page = build_response[:content]).blank?
+
+      match = build_page.match(regex)
+      next if match.blank?
+
+      "#{match[1]},#{newest_build}"
+    end
   end
 
   deprecate! date: "2025-05-01", because: :unsigned
