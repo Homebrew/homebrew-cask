d2be1fbaaf41ea403a286940af40d1a40bbb8fd5
Sam Ford <1584702+samford@users.noreply.github.com>
2024-09-25 13:03:01 -0400

    dolphin@dev: update version, livecheck
    The directories that precede the dmg file are different for each
    build, so we also need to capture those and store them in the cask
    `version`.

--- Casks/d/dolphin@dev.rb
+++ Casks/d/dolphin@dev.rb
@@ -1,15 +1,18 @@
 cask "dolphin@dev" do
-  version "2409-45"
-  sha256 "17046a69f8cceda3a239f6b19e306545988a9c10e16429d355d648cfc2f8891a"
+  version "2409-48,4c,e6"
+  sha256 "6a720704035b5b80bd9da8354456ef5dc0a0b629752bebafa986adbe61f08703"
 
-  url "https://dl.dolphin-emu.org/builds/d5/33/dolphin-master-#{version}-universal.dmg"
+  url "https://dl.dolphin-emu.org/builds/#{version.csv.second}/#{version.csv.third}/dolphin-master-#{version.csv.first}-universal.dmg"
   name "Dolphin Dev"
   desc "Emulator to play GameCube and Wii games"
   homepage "https://dolphin-emu.org/"
 
   livecheck do
-    url "https://dolphin-emu.org/download/list/master/1/"
-    regex(/href=.*?dolphin[._-]master[._-]v?(\d+(?:[._-]\d+)+)-universal\.dmg/i)
+    url "https://dolphin-emu.org/download/"
+    regex(%r{href=.*?/builds/([^/]+?)/([^/]+?)/dolphin[._-]master[._-]v?(\d+(?:[.-]\d+)+)-universal\.dmg}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[2]},#{match[0]},#{match[1]}" }
+    end
   end
 
   conflicts_with cask: [
