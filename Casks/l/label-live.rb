4af1bae8047caf8679889a073e8166ba80119a6c
Neil Grogan <neil@grogan.org>
2024-09-25 16:53:09 +0100

    Label LIVE 3.2.3 (new cask)
      Added Label LIVE which is a label design
      and printing software for thermal printers.

---
+++ Casks/l/label-live.rb
@@ -0,0 +1,24 @@
+cask "label-live" do
+  version "3.2.3"
+  sha256 "e5cf6eb208c37982afb5452160cd0d5d0380b7da7edf6b8ee7b629709ea91bf1"
+
+  url "https://download.label.live/Label-LIVE-#{version}.dmg"
+  name "Label LIVE"
+  desc "Label design and printer software"
+  homepage "https://label.live/"
+
+  livecheck do
+    url "https://label.live/thank-you"
+    regex(/"VERSION"\s*:\s*"(\d+(:?\.\d+)+)"/i)
+  end
+
+  depends_on macos: ">= :high_sierra"
+
+  app "Label LIVE.app"
+
+  zap trash: [
+    "~/Library/Application Support/Label LIVE",
+    "~/Library/Logs/Label LIVE",
+    "~/Library/Saved Application State/com.semireg.LabelLIVE.savedState",
+  ]
+end
