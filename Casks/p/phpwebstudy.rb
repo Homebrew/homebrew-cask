7916c30830b91c27881a55dd2a5b00a82c3b1782
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-25 16:14:50 +0000

    phpwebstudy 4.3.3

--- Casks/p/phpwebstudy.rb
+++ Casks/p/phpwebstudy.rb
@@ -1,9 +1,9 @@
 cask "phpwebstudy" do
   arch arm: "-arm64"
 
-  version "4.3.2"
-  sha256 arm:   "2ca2d879c4a7c4240ced57532a542cfeb63562e4fd0d40719b150a860f3d3556",
-         intel: "93b84da6bd2d2236964327e8d1131b6399668aeca5b172894f60e30f6d24d579"
+  version "4.3.3"
+  sha256 arm:   "c916f07809b7639c19a7c3160f05bcd1c098db074e2068d17c458e8972965ac1",
+         intel: "0a134dee85ebc5669fd233387ca0329c40426a38f3b073cc699a411fe59d5472"
 
   url "https://github.com/xpf0000/PhpWebStudy/releases/download/v#{version}/PhpWebStudy-#{version}#{arch}-mac.zip",
       verified: "github.com/xpf0000/PhpWebStudy/"
