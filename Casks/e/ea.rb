d5249d73e037dce780128fa047fa32edf1fb6962
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-09-24 20:58:12 -0400

    ea 13.305.0.5818

--- Casks/e/ea.rb
+++ Casks/e/ea.rb
@@ -1,5 +1,5 @@
 cask "ea" do
-  version "13.301.0.5814"
+  version "13.305.0.5818"
   sha256 :no_check
 
   url "https://origin-a.akamaihd.net/EA-Desktop-Client-Download/installer-releases/EA%20app.dmg",
