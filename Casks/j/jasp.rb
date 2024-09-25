f50ef1f0ccb7c1a85f2f56611012900e4d722de7
Sam Ford <1584702+samford@users.noreply.github.com>
2024-09-24 22:29:03 -0400

    jasp: fix version, livecheck
    The existing `livecheck` block for `jasp` contains a `strategy` block
    that uses a conditional `version` format but this is one of the ones
    where we mistakenly used `match.length`, which doesn't work as
    expected. This can lead to versions with a trailing comma, which
    works fine but looks strange.
    
    In this scenario we have to check for `match[1]` for this to work how
    we want. This updates the `version` and `strategy` block accordingly.

--- Casks/j/jasp.rb
+++ Casks/j/jasp.rb
@@ -2,7 +2,7 @@ cask "jasp" do
   arch arm: "arm64", intel: "x86_64"
   livecheck_folder = on_arch_conditional arm: "-apple-silicon"
 
-  version "0.19.1.0,"
+  version "0.19.1.0"
   sha256 arm:   "810d47f9a995865b8f8c0dc38db79e92d449669a2bd7b71cbf85e9c68179e12e",
          intel: "ec00c4822928c57afc1c8c37658d4d8e29717bdc6a704a6dd7573352618bc78c"
 
@@ -17,7 +17,7 @@ cask "jasp" do
     regex(/href=.*?JASP[._-]v?(\d+(?:\.\d+)+)[._-]macOS[._-]#{arch}(?:_(\d+))?\.dmg/i)
     strategy :page_match do |page, regex|
       page.scan(regex).map do |match|
-        (match.length > 1) ? "#{match[0]},#{match[1]}" : match[0]
+        match[1] ? "#{match[0]},#{match[1]}" : match[0]
       end
     end
   end
