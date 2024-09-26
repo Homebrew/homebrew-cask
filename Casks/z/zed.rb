a53c5252eaec5679e3d0dd15693a5ec022b1fc09
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-26 01:13:10 +0000

    zed 0.154.2

--- Casks/z/zed.rb
+++ Casks/z/zed.rb
@@ -1,9 +1,9 @@
 cask "zed" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.154.1"
-  sha256 arm:   "583c8f54b964dab531efdfa0fb1eb71adf6408c7e7cab1e68b724e436de61c8c",
-         intel: "a7f8d3f42ffd306e06083d97a2674e59e50a225843c80cb3a548d88f03c61224"
+  version "0.154.2"
+  sha256 arm:   "51fe718ac3af8c51ac828df8d20f63f51543ab5be48788f388d87852522e04c4",
+         intel: "86ead0550eb549e290295f24f4e2d73941d6f6816f89ce92f1f27e9d7fcad938"
 
   url "https://zed.dev/api/releases/stable/#{version}/Zed-#{arch}.dmg"
   name "Zed"
