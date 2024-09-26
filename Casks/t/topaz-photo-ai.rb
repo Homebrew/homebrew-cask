989d63682df9d72aebc6b34f53c0478fec532df1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-26 19:23:44 +0000

    topaz-photo-ai 3.2.2

--- Casks/t/topaz-photo-ai.rb
+++ Casks/t/topaz-photo-ai.rb
@@ -1,6 +1,6 @@
 cask "topaz-photo-ai" do
-  version "3.2.1"
-  sha256 "bdb5d6fb3d17cfb6ebc4849652e0320e97c1ded7098dde63253fac013ad7f8e1"
+  version "3.2.2"
+  sha256 "dcd99b8d2813f8db52e662456d99884c7be199125a3e35920bf39852a2d69a7b"
 
   url "https://downloads.topazlabs.com/deploy/TopazPhotoAI/#{version}/TopazPhotoAI-#{version}.pkg"
   name "Topaz Photo AI"
