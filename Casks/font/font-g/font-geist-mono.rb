bbf55108a0e609b69c6840024750ed6f31ab9379
Bevan Kay <email@bevankay.me>
2024-09-26 12:34:17 +1000

    font-geist-mono 1.4.0

--- Casks/font/font-g/font-geist-mono.rb
+++ Casks/font/font-g/font-geist-mono.rb
@@ -1,6 +1,6 @@
 cask "font-geist-mono" do
-  version "1.3.0"
-  sha256 "d6d19b0fc600110cb1595152c803a1f08e258f76637d1509baf76e2e0b9708d5"
+  version "1.4.0"
+  sha256 "91721f29a42d7e9e87348cd2db49a8f41c9363e43665be8c3aa44564c3e0cd3d"
 
   url "https://github.com/vercel/geist-font/releases/download/#{version}/GeistMono-#{version}.zip",
       verified: "github.com/vercel/geist-font/"
