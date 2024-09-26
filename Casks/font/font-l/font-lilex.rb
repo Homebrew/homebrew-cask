f7cc09f874c646d022a875216608e8f1d0a3ee34
Klaus Hipp <khipp@users.noreply.github.com>
2024-09-26 12:20:08 +0200

    font-lilex: add italic variant

--- Casks/font/font-l/font-lilex.rb
+++ Casks/font/font-l/font-lilex.rb
@@ -7,10 +7,16 @@ cask "font-lilex" do
   homepage "https://github.com/mishamyrt/Lilex"
 
   font "ttf/Lilex-Bold.ttf"
+  font "ttf/Lilex-BoldItalic.ttf"
   font "ttf/Lilex-ExtraLight.ttf"
+  font "ttf/Lilex-ExtraLightItalic.ttf"
+  font "ttf/Lilex-Italic.ttf"
   font "ttf/Lilex-Medium.ttf"
+  font "ttf/Lilex-MediumItalic.ttf"
   font "ttf/Lilex-Regular.ttf"
   font "ttf/Lilex-Thin.ttf"
+  font "ttf/Lilex-ThinItalic.ttf"
+  font "variable/Lilex-Italic[wght].ttf"
   font "variable/Lilex[wght].ttf"
 
   # No zap stanza required
