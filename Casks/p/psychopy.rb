078edc338295acaeccd7dfc804fb7d02c23b7216
Klaus Hipp <khipp@users.noreply.github.com>
2024-09-27 22:35:31 +0200

    psychopy 2024.2.2

--- Casks/p/psychopy.rb
+++ Casks/p/psychopy.rb
@@ -1,15 +1,27 @@
 cask "psychopy" do
-  version "2024.2.1"
-  sha256 "069a75e6c4a9fe849b347e2af8d743f609624bcb6941d40ac5b831ab87e0758b"
+  version "2024.2.2,2024-09-18_18-17"
+  sha256 "9c5f1471d103c24e33985f64569dbe3a8b7ecf06a8659e6a72d174980597286f"
 
-  url "https://github.com/psychopy/psychopy/releases/download/#{version.major_minor_patch}/StandalonePsychoPy-#{version}-macOS-py3.10.dmg"
+  url "https://github.com/psychopy/psychopy/releases/download/#{version.csv.first.major_minor_patch}/StandalonePsychoPy-#{version.csv.first}-macOS#{"_#{version.csv.second}" if version.csv.second}_3.10.dmg"
   name "PsychoPy"
   desc "Create experiments in behavioral science"
   homepage "https://github.com/psychopy/psychopy"
 
   livecheck do
     url :url
-    strategy :github_latest
+    regex(/StandalonePsychoPy[._-]v?(\d+(?:\.\d+)+)[._-]macOS[._-](\d+(?:[._-]\d+)+)?[._-](?:py)?3\.10\.dmg/i)
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        next if release["draft"] || release["prerelease"]
+
+        release["assets"]&.map do |asset|
+          match = asset["name"]&.match(regex)
+          next if match.blank?
+
+          match[2].present? ? "#{match[1]},#{match[2]}" : match[1]
+        end
+      end.flatten
+    end
   end
 
   app "PsychoPy.app"
