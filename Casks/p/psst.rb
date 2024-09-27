12e9de9c8c2886247ae0f5f8c1d38c43bcb7e1e9
Klaus Hipp <khipp@users.noreply.github.com>
2024-09-27 10:21:42 +0200

    psst 0.1.0,20240925.164916

--- Casks/p/psst.rb
+++ Casks/p/psst.rb
@@ -1,5 +1,5 @@
 cask "psst" do
-  version "0.1.0,20240923.220340"
+  version "0.1.0,20240925.164916"
   sha256 :no_check
 
   url "https://nightly.link/jpochyla/psst/workflows/build/master/Psst.dmg.zip",
