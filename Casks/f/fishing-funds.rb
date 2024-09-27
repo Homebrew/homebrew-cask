ffb50a9adc60ae3c224f5bcf8a084a822fc4bcf4
Anka <runner@Mac-1727406505327.local>
2024-09-27 03:23:39 +0000

    fishing-funds 8.3.0

--- Casks/f/fishing-funds.rb
+++ Casks/f/fishing-funds.rb
@@ -1,9 +1,9 @@
 cask "fishing-funds" do
   arch arm: "-arm64"
 
-  version "8.2.3"
-  sha256 arm:   "cfd99745c23aee47fdb27570fd15cdef1d45ff284c354a7077502784cb094e46",
-         intel: "201e40902da65f0880035599e5857b479b21a71ea264a74ff9390298b7e86611"
+  version "8.3.0"
+  sha256 arm:   "3cbbc103cde23927dd11ae172970155cc4ab98ba56c7ae633253144dfe7c6947",
+         intel: "7c90fbbe64da4eb9197455041399bdfa463d070916490d29d17099b2fdcf5c7e"
 
   url "https://github.com/1zilc/fishing-funds/releases/download/v#{version}/Fishing-Funds-#{version}#{arch}.dmg",
       verified: "github.com/1zilc/fishing-funds/"
