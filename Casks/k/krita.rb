2588bb02c0c5774c1161cc42dd6c1384f4f81244
Sam Ford <1584702+samford@users.noreply.github.com>
2024-09-26 21:14:14 -0400

    krita 5.2.5
    The existing `livecheck` block for `krita` is returning an `Unable to
    get versions` error, as the dmg file URL now includes a `-signed`
    suffix. This updates the regex to account for this suffix, fixing
    the check.

--- Casks/k/krita.rb
+++ Casks/k/krita.rb
@@ -1,8 +1,8 @@
 cask "krita" do
-  version "5.2.3"
-  sha256 "1e9014edfd8cef1bf618b6db06ebf6e727a9ce8f92cada2a78bfce15ff96b88a"
+  version "5.2.5"
+  sha256 "b1a84c3b0a83ef00b7099401976e12f86656dac2ae6f368c353a125c77093a56"
 
-  url "https://download.kde.org/stable/krita/#{version}/krita-#{version}.dmg",
+  url "https://download.kde.org/stable/krita/#{version}/krita-#{version}-signed.dmg",
       verified: "download.kde.org/stable/krita/"
   name "Krita"
   desc "Free and open-source painting and sketching program"
@@ -10,10 +10,10 @@ cask "krita" do
 
   livecheck do
     url "https://krita.org/en/download/"
-    regex(/href=.*?krita[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+    regex(/href=.*?krita[._-]v?(\d+(?:\.\d+)+)(?:-signed)?\.dmg/i)
   end
 
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :high_sierra"
 
   app "krita.app"
 
