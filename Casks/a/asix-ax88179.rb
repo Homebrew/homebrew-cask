f30ae3bd5334608c1fa592f38bb814abb76360d3
Klaus Hipp <khipp@users.noreply.github.com>
2024-09-27 09:12:36 +0200

    asix-ax88179 2.4.0,1867

--- Casks/a/asix-ax88179.rb
+++ Casks/a/asix-ax88179.rb
@@ -73,7 +73,7 @@ cask "asix-ax88179" do
     end
   end
   on_monterey :or_newer do
-    version "2.4.0,1472"
+    version "2.4.0,1867"
 
     livecheck do
       url "https://www.asix.com.tw/en/support/download/step2/11/2/3"
