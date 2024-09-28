29661c7fb291111c8fda4ca376359a541640fb14
Bevan Kay <bevanjkay@gmail.com>
2024-09-28 10:34:48 +1000

    Update Casks/f/fujifilm-tether-app.rb

--- Casks/f/fujifilm-tether-app.rb
+++ Casks/f/fujifilm-tether-app.rb
@@ -11,10 +11,10 @@ cask "fujifilm-tether-app" do
     url :homepage
     regex(%r{Mac\sVersion:\s(\d+(?:\.\d+)+).*href=.*?tether-app-mac\d+-([a-zA-Z0-9]+)/}im)
     strategy :page_match do |page, regex|
-      match = page.match(regex)\
+      match = page.match(regex)
       next if match.blank?
 
-      "#{match[1]},#{match[2]}" if match
+      "#{match[1]},#{match[2]}"
     end
   end
 
