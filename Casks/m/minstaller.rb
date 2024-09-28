a73ab14afdda87f85558a1071ac8d54a571e6b06
Bevan Kay <email@bevankay.me>
2024-09-28 10:48:38 +1000

    minstaller: add livecheck, version url

--- Casks/m/minstaller.rb
+++ Casks/m/minstaller.rb
@@ -1,11 +1,16 @@
 cask "minstaller" do
   version "3.1.0"
-  sha256 :no_check
+  sha256 "7a57d4c812f6ea5e366760eab123a6a0653f0a4af080ca3d129c556918ef58bf"
 
-  url "https://s3.motionvfx.com/mvfxpublic/mInstaller/mInstaller.dmg"
+  url "https://s3.motionvfx.com/mvfxpublic/mInstaller/sparkle/mInstaller-#{version}.zip"
   name "mInstaller"
   desc "Downloader and manager for MotionVFX products"
   homepage "https://www.motionvfx.com/"
+
+  livecheck do
+    url "https://s3.motionvfx.com/mvfxpublic/mInstaller/sparkle/appcast.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :big_sur"
