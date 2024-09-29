372810c99547d5eed50f7b3c8ffd5fd52556ad79
Jiahang Ren <jeffereyreng@Jiahangs-MacBook-Pro.local>
2024-09-30 00:48:38 +0800

    fix error

--- Casks/offergoose.rb
+++ Casks/offergoose.rb
@@ -1,29 +1,19 @@
 cask "offergoose" do
-    version :latest
-    sha256 :no_check
-  
-    # Chinese language version (macOS >= Ventura)
-    language "zh", "CN" do
-      url "https://cdn.offergoose.cn/assets/pc-releases/latest/cn/OfferGoose-latest-arm64.dmg",
-          verified: "cdn.offergoose.cn"
-      depends_on macos: ">= :ventura"
-    end
-  
-    # English language version (macOS >= Catalina)
-    language "en", default: true do
-      url "https://cdn.offergoose.com/assets/pc-releases/latest/com/OfferGoose-latest-arm64.dmg",
-          verified: "cdn.offergoose.com"
-      depends_on macos: ">= :catalina"
-    end
-  
-    name "OfferGoose"
-    desc "Tools for AI Interview Copilot for Job Seekers"
-    homepage "https://www.offergoose.com/"
-  
-    app "OfferGoose.app"
-  
-    zap trash: [
-      "~/Library/Application Support/OfferGoose",
-      "~/Library/Preferences/com.offergoose.app.plist"
-    ]
-  end
+  version :latest
+  sha256 :no_check
+
+  url "https://cdn.offergoose.com/assets/pc-releases/latest/com/OfferGoose-latest-arm64.dmg",
+      verified: "cdn.offergoose.com"
+  depends_on macos: ">= :catalina"
+
+  name "OfferGoose"
+  desc "AI Interview Copilot designed for job seekers"
+  homepage "https://www.offergoose.com/"
+
+  app "OfferGoose.app"
+
+  zap trash: [
+    "~/Library/Application Support/OfferGoose",
+    "~/Library/Preferences/com.offergoose.app.plist",
+  ]
+end
