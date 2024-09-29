754b0890c3cc5e33009811f7bd21f9a0e3e89f08
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-29 09:32:54 +0000

    brewtarget 4.0.5

--- Casks/b/brewtarget.rb
+++ Casks/b/brewtarget.rb
@@ -1,6 +1,6 @@
 cask "brewtarget" do
-  version "4.0.4"
-  sha256 "0869094f32461e2e6947dab57900162e8cd3b6cb5e15b4bd60591dcbbe08c963"
+  version "4.0.5"
+  sha256 "f899c9411feb91b15d45e76d4e75e0b5f1ab6abb091a20f8fada8b5194de037e"
 
   url "https://github.com/Brewtarget/brewtarget/releases/download/v#{version}/brewtarget_#{version}.dmg"
   name "brewtarget"
