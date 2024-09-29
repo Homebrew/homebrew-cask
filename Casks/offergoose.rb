bf8c20ef3ecf27282760d007915eeaab6043ed09
Jiahang Ren <jeffereyreng@Jiahangs-MacBook-Pro.local>
2024-09-30 00:08:17 +0800

    Update OfferGoose Cask description

--- Casks/offergoose.rb
+++ Casks/offergoose.rb
@@ -1,27 +1,27 @@
-cask 'offergoose' do
+cask "offergoose" do
     version :latest
     sha256 :no_check
   
-    # Select download links based on region and language
     language "zh", "CN" do
-      url 'https://cdn.offergoose.cn/assets/pc-releases/latest/cn/OfferGoose-latest-arm64.dmg',
+      url "https://cdn.offergoose.cn/assets/pc-releases/latest/cn/OfferGoose-latest-arm64.dmg",
           verified: "cdn.offergoose.cn"
-      depends_on macos: '>= :ventura'  # For zh_CN, macOS 13.1 or later
+      depends_on macos: ">= :ventura"
     end
   
     language "en", default: true do
-      url 'https://cdn.offergoose.com/assets/pc-releases/latest/com/OfferGoose-latest-arm64.dmg'
-      depends_on macos: '>= :catalina'  # For en_US, macOS 10.15 or later
+      url "https://cdn.offergoose.com/assets/pc-releases/latest/com/OfferGoose-latest-arm64.dmg",
+          verified: "cdn.offergoose.com"
+      depends_on macos: ">= :catalina"
     end
   
-    name 'OfferGoose'
-    desc 'OfferGoose app description'
-    homepage 'https://www.offergoose.com' # Global homepage
+    name "OfferGoose"
+    desc "Tools for AI Interview Copilot for Job Seekers"
+    homepage "https://www.offergoose.com/"
   
-    app 'OfferGoose.app'
+    app "OfferGoose.app"
   
     zap trash: [
-      '~/Library/Preferences/com.offergoose.app.plist',
-      '~/Library/Application Support/OfferGoose',
+      "~/Library/Application Support/OfferGoose",
+      "~/Library/Preferences/com.offergoose.app.plist"
     ]
   end
