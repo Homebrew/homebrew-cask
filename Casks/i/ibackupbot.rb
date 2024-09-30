9cc50e322d1185c0523f7ea9d7ff61ab6739bfb5
Klaus Hipp <khipp@users.noreply.github.com>
2024-09-30 10:40:33 +0200

    ibackupbot: add description

--- Casks/i/ibackupbot.rb
+++ Casks/i/ibackupbot.rb
@@ -4,6 +4,7 @@ cask "ibackupbot" do
 
   url "https://www.icopybot.com/iBackupBot-Setup.dmg"
   name "iBackupBot"
+  desc "Backup manager for iTunes"
   homepage "https://www.icopybot.com/itunes-backup-manager.htm"
 
   deprecate! date: "2024-09-30", because: :unmaintained
