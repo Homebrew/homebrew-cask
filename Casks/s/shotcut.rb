45155a18aae5b65f9271fdbaae746d3d636371f1
Klaus Hipp <khipp@users.noreply.github.com>
2024-09-30 14:19:18 +0200

    shotcut 24.09.13,240919

--- Casks/s/shotcut.rb
+++ Casks/s/shotcut.rb
@@ -1,17 +1,31 @@
 cask "shotcut" do
-  version "24.09.13"
-  sha256 "60ac07ccf48617ca3c344175a651698f80cd73068aa3b5a3c1cfb8c2831d2177"
+  version "24.09.13,240919"
+  sha256 "fa05e60dfe7be6c78155c3ce9aae18ceb7882c40baa66770badf2dbb7c54a4ce"
 
-  url "https://github.com/mltframework/shotcut/releases/download/v#{version}/shotcut-macos-#{version.no_dots}.dmg",
+  url "https://github.com/mltframework/shotcut/releases/download/v#{version.csv.first}/shotcut-macos-#{version.csv.second || version.csv.first.no_dots}.dmg",
       verified: "github.com/mltframework/shotcut/"
   name "Shotcut"
   desc "Video editor"
   homepage "https://www.shotcut.org/"
 
+  # The tag version can differ from the filename version, so we include both in
+  # the `version` when necessary.
   livecheck do
     url :url
-    strategy :github_latest
+    regex(%r{/v?(\d+(?:\.\d+)+)/shotcut[._-]macos[._-]v?(\d+(?:\.\d+)*)\.dmg$}i)
+    strategy :github_latest do |json, regex|
+      json["assets"]&.map do |asset|
+        match = asset["browser_download_url"]&.match(regex)
+        next if match.blank?
+
+        next match[1] if match[1].tr(".", "") == match[2].tr(".", "")
+
+        "#{match[1]},#{match[2]}"
+      end
+    end
   end
+
+  depends_on macos: ">= :big_sur"
 
   app "Shotcut.app"
 
