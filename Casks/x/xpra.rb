39d145bd39b2bcfebdfc90984635f648532d4eff
Bevan Kay <email@bevankay.me>
2024-09-30 17:43:41 +1000

    xpra 6.1.3

--- Casks/x/xpra.rb
+++ Casks/x/xpra.rb
@@ -2,12 +2,12 @@ cask "xpra" do
   arch arm: "arm64", intel: "x86_64"
 
   on_arm do
-    version "6.1.2,0"
-    sha256 "45b47c4f374cad40c248bdc534b6b23c469bf1832ff8dedfdcaa0a1ec49c4d37"
+    version "6.1.3,0"
+    sha256 "1f9af6aef560d532931fc299ed92341d3bdce5fc9fc7d9bd2013d43d7db29c2c"
   end
   on_intel do
-    version "6.1.2,0"
-    sha256 "b27859d3c48096443c3db96fd99ef5834b4516abe458c1be7d8614299211a1d5"
+    version "6.1.3,0"
+    sha256 "eeb7905746b55e5ce2037248398042ae8670881dd24afd1badf01eb17b0e07c6"
   end
 
   url "https://xpra.org/dists/MacOS/#{arch}/Xpra-#{arch}-#{version.csv.first}-r#{version.csv.second}.dmg",
