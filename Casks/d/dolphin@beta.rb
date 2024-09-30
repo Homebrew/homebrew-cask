a021a6b705c34bb278459a4e1d6c4c15074fcb23
Bevan Kay <email@bevankay.me>
2024-09-30 13:43:21 +1000

    dolphin@beta 2409

--- Casks/d/dolphin@beta.rb
+++ Casks/d/dolphin@beta.rb
@@ -1,19 +1,16 @@
 cask "dolphin@beta" do
-  version "5.0-21460,0b,8c"
-  sha256 "eea2cc9248bea9279bf895469e624c015ecb628017a4bfc6debf01c649e15dcf"
+  version "2409"
+  sha256 "17917940d84bcf1abc46e85d6d4cb960f1fd7e017dd6502f9c505ba3135def97"
 
-  url "https://dl.dolphin-emu.org/builds/#{version.csv.second}/#{version.csv.third}/dolphin-master-#{version.csv.first}-universal.dmg"
+  url "https://dl.dolphin-emu.org/releases/#{version}/dolphin-#{version}-universal.dmg"
   name "Dolphin Beta"
   desc "Emulator to play GameCube and Wii games"
   homepage "https://dolphin-emu.org/"
 
   livecheck do
     url "https://dolphin-emu.org/update/latest/beta/"
-    regex(%r{/builds/([^/]+)/([^/]+)/dolphin-master-v?(\d+(?:[.-]\d+)+)-universal.dmg}i)
     strategy :json do |json|
-      macos_artifact = json["artifacts"].find { |artifact| artifact["system"].include?("macOS") }
-      match = macos_artifact["url"].match(regex)
-      "#{match[3]},#{match[1]},#{match[2]}"
+      json["shortrev"]
     end
   end
 
@@ -24,7 +21,6 @@ cask "dolphin@beta" do
   depends_on macos: ">= :catalina"
 
   app "Dolphin.app"
-  app "Dolphin Updater.app"
 
   zap trash: [
     "~/Library/Application Support/Dolphin",
