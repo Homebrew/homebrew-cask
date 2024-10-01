029f71881d9332c63cbbac495c7a80655a4cb616
Bevan Kay <email@bevankay.me>
2024-10-01 10:49:04 +0000

    font-sixtyfour-convergence:  update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

--- Casks/font/font-s/font-sixtyfour-convergence.rb
+++ Casks/font/font-s/font-sixtyfour-convergence.rb
@@ -2,9 +2,10 @@ cask "font-sixtyfour-convergence" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/sixtyfourconvergence/SixtyfourConvergence%5BBLED%2CSCAN%2CXELA%2CYELA%5D.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/sixtyfourconvergence/SixtyfourConvergence%5BBLED%2CSCAN%2CXELA%2CYELA%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Sixtyfour Convergence"
-  homepage "https://github.com/jenskutilek/homecomputer-fonts"
+  homepage "https://fonts.google.com/specimen/Sixtyfour+Convergence"
 
   font "SixtyfourConvergence[BLED,SCAN,XELA,YELA].ttf"
 
