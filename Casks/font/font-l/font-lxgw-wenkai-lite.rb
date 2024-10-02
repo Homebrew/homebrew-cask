5b3f46b05fa9916e090f032f6c69e2bd7726ad93
Bevan Kay <email@bevankay.me>
2024-10-02 13:59:13 +1000

    font-lxgw-wenkai-lite: update paths

--- Casks/font/font-l/font-lxgw-wenkai-lite.rb
+++ Casks/font/font-l/font-lxgw-wenkai-lite.rb
@@ -7,11 +7,11 @@ cask "font-lxgw-wenkai-lite" do
   name "霞鹜文楷 轻便版"
   homepage "https://github.com/lxgw/LxgwWenKai-Lite"
 
-  font "lxgw-wenkai-lite-v#{version}/LXGWWenKaiLite-Bold.ttf"
   font "lxgw-wenkai-lite-v#{version}/LXGWWenKaiLite-Light.ttf"
+  font "lxgw-wenkai-lite-v#{version}/LXGWWenKaiLite-Medium.ttf"
   font "lxgw-wenkai-lite-v#{version}/LXGWWenKaiLite-Regular.ttf"
-  font "lxgw-wenkai-lite-v#{version}/LXGWWenKaiMonoLite-Bold.ttf"
   font "lxgw-wenkai-lite-v#{version}/LXGWWenKaiMonoLite-Light.ttf"
+  font "lxgw-wenkai-lite-v#{version}/LXGWWenKaiMonoLite-Medium.ttf"
   font "lxgw-wenkai-lite-v#{version}/LXGWWenKaiMonoLite-Regular.ttf"
 
   # No zap stanza required
