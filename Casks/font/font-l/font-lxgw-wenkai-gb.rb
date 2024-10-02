c78fddc04094232afdff10aaab352166ecbbf716
Bevan Kay <email@bevankay.me>
2024-10-02 12:45:33 +1000

    font-lxgw-wenkai-gb 1.500

--- Casks/font/font-l/font-lxgw-wenkai-gb.rb
+++ Casks/font/font-l/font-lxgw-wenkai-gb.rb
@@ -1,6 +1,6 @@
 cask "font-lxgw-wenkai-gb" do
-  version "1.330"
-  sha256 "4b842c2a12e6017f0e4495a12a364aefe1dd091edd8533c61a8518a8d1a2ffff"
+  version "1.500"
+  sha256 "36440525c599fa4134ec247d0f9814db622336b943a1fbad2c2785b3125605c0"
 
   url "https://github.com/lxgw/LxgwWenkaiGB/releases/download/v#{version}/lxgw-wenkai-gb-v#{version}.zip"
   name "LXGW WenKai GB"
