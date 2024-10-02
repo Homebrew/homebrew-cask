1c0a0a91432199f14845ae6263a354e6b1bdbe7a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-02 16:02:53 +0000

    zoc 8.08.8

--- Casks/z/zoc.rb
+++ Casks/z/zoc.rb
@@ -1,6 +1,6 @@
 cask "zoc" do
-  version "8.08.7"
-  sha256 "aecaf8df339378d0065a753e9e29f9f9e0b3f603ecfee373b89d2a5e745c741f"
+  version "8.08.8"
+  sha256 "cef9cf961ad334acc6ee52b89a2287b99f9cd35c15d5f490c69e7ec2c608038c"
 
   url "https://www.emtec.com/downloads/zoc/zoc#{version.no_dots}.dmg"
   name "ZOC"
