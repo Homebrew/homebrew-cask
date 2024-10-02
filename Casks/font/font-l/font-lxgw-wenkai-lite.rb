c5281a2a6060ebac367a9233930f1a1d88eb056e
Bevan Kay <email@bevankay.me>
2024-10-02 12:46:06 +1000

    font-lxgw-wenkai-lite 1.500

--- Casks/font/font-l/font-lxgw-wenkai-lite.rb
+++ Casks/font/font-l/font-lxgw-wenkai-lite.rb
@@ -1,6 +1,6 @@
 cask "font-lxgw-wenkai-lite" do
-  version "1.330"
-  sha256 "10beb11701118e6f1b301cdc2b2a2e6177e0fe53671c5f802603a9e9bd9c168d"
+  version "1.500"
+  sha256 "8705f7931b75ce33e65079af3756e256a0fbe4b28aa3b74156d339fb51e9c543"
 
   url "https://github.com/lxgw/LxgwWenKai-Lite/releases/download/v#{version}/lxgw-wenkai-lite-v#{version}.zip"
   name "LXGW WenKai Lite"
