4d896bc0a5d07c76377cff024a46d7c57aacc1b8
Nikhil Badyal <nikhill773384@gmail.com>
2024-10-02 16:11:35 +0530

    Added Keyguard

---
+++ Casks/k/keyguard.rb
@@ -0,0 +1,35 @@
+cask "keyguard" do
+  version "r20241002,1.6.0"
+  sha256 "3631112e36eb33d5b9c7d2019bd418650c777d98b1fe2af1f9797e47364bf0ef"
+
+  url "https://github.com/AChep/keyguard-app/releases/download/#{version.csv.first}/Keyguard-#{version.csv.second}.dmg"
+  name "Keyguard"
+  desc "Is an alternative client for the Bitwarden platform"
+  homepage "https://github.com/AChep/keyguard-app"
+
+  livecheck do
+    url :url
+    regex(%r{/r(\d+)/Keyguard[._-](\d+(?:\.\d+)*)\.dmg}i)
+    strategy :github_latest do |json, regex|
+      json["assets"]&.map do |asset|
+        match = asset["browser_download_url"]&.match(regex)
+        next if match.blank?
+
+        "r#{match[1]},#{match[2]}"
+      end
+    end
+  end
+
+  depends_on macos: ">= :high_sierra"
+
+  app "keyguard.app"
+
+  zap trash: [
+    "~/Library/Application Support/keyguard",
+    "~/Library/Saved Application State/com.artemchep.keyguard.savedState",
+  ]
+
+  caveats do
+    requires_rosetta
+  end
+end
