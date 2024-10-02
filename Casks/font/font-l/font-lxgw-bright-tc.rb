b36944595787e9ebdd8272730bd0abd75aa8f6c3
Bevan Kay <email@bevankay.me>
2024-10-02 12:44:19 +1000

    font-lxgw-bright-tc 5.500

--- Casks/font/font-l/font-lxgw-bright-tc.rb
+++ Casks/font/font-l/font-lxgw-bright-tc.rb
@@ -1,6 +1,6 @@
 cask "font-lxgw-bright-tc" do
-  version "5.330"
-  sha256 "5a01c41e45b43fc3ca5eb49b147a1103199002895906b8d6436b65226b5257fe"
+  version "5.500"
+  sha256 "e4fd51465429a122f0a4f92317914ec54483543769678ffa59ef8f55a1fb9d90"
 
   url "https://github.com/lxgw/LxgwBright/releases/download/v#{version}/LXGWBrightTC.7z"
   name "LXGW Bright TC"
