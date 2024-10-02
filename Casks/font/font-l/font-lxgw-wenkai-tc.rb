69c35f303099616004fde65a94dada64478ebedb
Bevan Kay <email@bevankay.me>
2024-10-02 13:59:40 +1000

    font-lxgw-wenkai-tc: update paths

--- Casks/font/font-l/font-lxgw-wenkai-tc.rb
+++ Casks/font/font-l/font-lxgw-wenkai-tc.rb
@@ -7,11 +7,11 @@ cask "font-lxgw-wenkai-tc" do
   name "霞鶩文楷 TC"
   homepage "https://github.com/lxgw/LxgwWenkaiTC"
 
-  font "lxgw-wenkai-tc-v#{version}/LXGWWenKaiMonoTC-Bold.ttf"
   font "lxgw-wenkai-tc-v#{version}/LXGWWenKaiMonoTC-Light.ttf"
+  font "lxgw-wenkai-tc-v#{version}/LXGWWenKaiMonoTC-Medium.ttf"
   font "lxgw-wenkai-tc-v#{version}/LXGWWenKaiMonoTC-Regular.ttf"
-  font "lxgw-wenkai-tc-v#{version}/LXGWWenKaiTC-Bold.ttf"
   font "lxgw-wenkai-tc-v#{version}/LXGWWenKaiTC-Light.ttf"
+  font "lxgw-wenkai-tc-v#{version}/LXGWWenKaiTC-Medium.ttf"
   font "lxgw-wenkai-tc-v#{version}/LXGWWenKaiTC-Regular.ttf"
 
   # No zap stanza required
