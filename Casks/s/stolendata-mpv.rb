bfd134a1305b1a96c3579399db32ba095f6b7ad5
Patrick Linnane <patrick@linnane.io>
2024-10-03 14:33:55 -0700

    stolendata-mpv: fix `on_arm` block
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/s/stolendata-mpv.rb
+++ Casks/s/stolendata-mpv.rb
@@ -1,16 +1,15 @@
 cask "stolendata-mpv" do
-  arch arm: "-arm64", intel: ""
+  arch arm: "-arm64"
 
   on_arm do
-    on_sonoma :or_newer do
-      version "0.39.0"
-      sha256 "9c81c5cf2e756cf9c2fef97a00627140ea7c6b46079469ce3b1a13b4cd4f5b2b"
+    version "0.39.0"
+    sha256 "9c81c5cf2e756cf9c2fef97a00627140ea7c6b46079469ce3b1a13b4cd4f5b2b"
 
-      livecheck do
-        url "https://laboratory.stolendata.net/~djinn/mpv_osx/"
-        regex(/mpv-arm64-(\d+(?:\.\d+)+)\.t/i)
-      end
+    livecheck do
+      url "https://laboratory.stolendata.net/~djinn/mpv_osx/"
+      regex(/mpv-arm64-(\d+(?:\.\d+)+)\.t/i)
     end
+
     depends_on macos: ">= :sonoma"
   end
   on_intel do
