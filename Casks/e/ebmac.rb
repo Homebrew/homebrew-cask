6f5c3032bd852d3974190e8f02e735b9406e514c
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-10-03 09:24:27 -0400

    ebmac 1.47.0

--- Casks/e/ebmac.rb
+++ Casks/e/ebmac.rb
@@ -1,6 +1,6 @@
 cask "ebmac" do
-  version "1.46.1"
-  sha256 "dc537911d917a694360bc739f9a188d5610a1ecc4b935beb3f89dad55a54ee4d"
+  version "1.47.0"
+  sha256 "8038fbdd306a4c6eb10680bcfa04dca873c86cbe7c2b82c4875d73de1c3043d3"
 
   url "https://ebstudio.info/download/ebpocket/EBMac#{version}.dmg"
   name "EBMac"
