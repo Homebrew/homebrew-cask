bba5d805e9056d35cbc5a19f0a5554398380e13c
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-10-02 22:16:10 -0400

    lw-scanner 0.27.0

--- Casks/l/lw-scanner.rb
+++ Casks/l/lw-scanner.rb
@@ -1,8 +1,8 @@
 cask "lw-scanner" do
-  version "0.24.0"
-  sha256 "a2bbd9b615143ca1f63824fa335d49da9dd2744a58776bff1c9a85e6bcd3c34e"
+  version "0.27.0"
+  sha256 "2bc25d2734d5f4a69982e9aed670003965505a875b3ea5495e792b6811bade1e"
 
-  url "https://github.com/lacework/lacework-vulnerability-scanner/releases/download/v#{version}/lw-scanner-darwin-amd64",
+  url "https://github.com/lacework/lacework-vulnerability-scanner/releases/download/#{version}/lw-scanner-darwin-amd64",
       verified: "github.com/lacework/lacework-vulnerability-scanner/"
   name "Lacework vulnerability scanner"
   desc "Lacework inline scanner"
