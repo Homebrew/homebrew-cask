d524d449e9f541025e72c00f64b59591268b3910
Matt Coster <opensource@mtcoster.net>
2024-10-03 16:54:50 +0100

    calibrite-profiler 1.3.3
    Upstream sometimes marks releases as "pre-release" on GitHub that are
    advertised on the first-party download page as the latest stable
    release, so we have to add this to `github_prerelease_allowlist.json`.

--- Casks/c/calibrite-profiler.rb
+++ Casks/c/calibrite-profiler.rb
@@ -1,6 +1,6 @@
 cask "calibrite-profiler" do
-  version "1.3.2"
-  sha256 "ac3ef76116de2efec11fbe67c68b1ba0ba225fa3108831788df5985d3a6dca50"
+  version "1.3.3"
+  sha256 "beab575669a526520a20a64f47f510e01471d0631246cfea77ef727d91cea083"
 
   url "https://github.com/LUMESCA/calibrite-profiler-releases/releases/download/v#{version}/calibrite-PROFILER-#{version}.dmg",
       verified: "github.com/LUMESCA/calibrite-profiler-releases/"
