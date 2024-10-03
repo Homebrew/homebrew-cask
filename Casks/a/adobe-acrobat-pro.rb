f47aa3f3a937498829d91bae48cb537145df9dc4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-03 15:33:30 +0000

    adobe-acrobat-pro 24.003.20180

--- Casks/a/adobe-acrobat-pro.rb
+++ Casks/a/adobe-acrobat-pro.rb
@@ -1,5 +1,5 @@
 cask "adobe-acrobat-pro" do
-  version "24.003.20121"
+  version "24.003.20180"
   sha256 :no_check
 
   url "https://trials.adobe.com/AdobeProducts/APRO/Acrobat_HelpX/osx10/Acrobat_DC_Web_WWMUI.dmg",
