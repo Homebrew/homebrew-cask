a4bf16de303f643bcf357185a610ddfbb985e589
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-10-02 22:09:06 -0400

    dcv-viewer 2024.0.6995

--- Casks/d/dcv-viewer.rb
+++ Casks/d/dcv-viewer.rb
@@ -1,9 +1,9 @@
 cask "dcv-viewer" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "2023.1.6809"
-  sha256 arm:   "bfe892e205a011c617a04ebabeb94b54fab313055e8d7736b51c9b8cf79c2b1b",
-         intel: "16a6ae4c541eaf3d47a6d3d8c8ab06a76a215f772656a5e6fb59630d13af3e98"
+  version "2024.0.6995"
+  sha256 arm:   "21f9542a1809ab4e57876b6d26946c0311a019b83821b6ab18ca5578b6c74705",
+         intel: "54d236f9a94feb34e82625cd39c9b7523d00794ea83da8c97059f4df7eeb1f41"
 
   url "https://d1uj6qtbmh3dt5.cloudfront.net/#{version.major_minor}/Clients/nice-dcv-viewer-#{version}.#{arch}.dmg",
       verified: "d1uj6qtbmh3dt5.cloudfront.net/"
@@ -16,7 +16,7 @@ cask "dcv-viewer" do
     regex(%r{href=.*?/nice-dcv-viewer[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}\.dmg}i)
   end
 
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= :high_sierra"
 
   app "DCV Viewer.app"
 
