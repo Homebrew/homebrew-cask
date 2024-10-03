3169d84d976a668f0dbb542d1930f05456f0651d
Bevan Kay <email@bevankay.me>
2024-10-03 12:53:10 +0000

    font-edu-au-vic-wa-nt-pre: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

---
+++ Casks/font/font-e/font-edu-au-vic-wa-nt-pre.rb
@@ -0,0 +1,12 @@
+cask "font-edu-au-vic-wa-nt-pre" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/eduauvicwantpre/EduAUVICWANTPre%5Bwght%5D.ttf"
+  name "Edu AU VIC WA NT Pre"
+  homepage "https://github.com/SorkinType/VICWANTSchoolhandAustralia"
+
+  font "EduAUVICWANTPre[wght].ttf"
+
+  # No zap stanza required
+end
