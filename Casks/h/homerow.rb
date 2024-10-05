5394729eee1ce46abc4132ac74a1bc5cb5e3d3bf
EmilioAK <emilio.alvarez96@gmail.com>
2024-10-05 19:28:13 +0200

    homerow 1.2.2 (new cask)

---
+++ Casks/h/homerow.rb
@@ -0,0 +1,27 @@
+cask "homerow" do
+  version "1.2.2"
+  sha256 "463e46c9695857638aa529bf27b3815f2c1fd3babd8887c6e2e5dd7f2584cd02"
+
+  url "https://builds.homerow.app/v#{version}/Homerow.zip"
+  name "Homerow"
+  desc "Keyboard shortcuts for every button on your screen"
+  homepage "https://www.homerow.app/"
+
+  livecheck do
+    url "https://www.homerow.app/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  depends_on macos: ">= :monterey"
+
+  app "Homerow.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.superultra.HomerowLauncher",
+    "~/Library/Application Support/com.superultra.Homerow",
+    "~/Library/Caches/com.superultra.Homerow",
+    "~/Library/HTTPStorages/com.superultra.Homerow",
+    "~/Library/Preferences/com.superultra.Homerow.plist",
+    "~/Library/Saved Application State/com.superultra.Homerow.savedState",
+  ]
+end
