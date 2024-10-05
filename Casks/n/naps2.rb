3b5f6a2da0cb44647b385138d587fa71bab259bd
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-10-04 21:03:13 -0400

    naps2 7.5.2

--- Casks/n/naps2.rb
+++ Casks/n/naps2.rb
@@ -2,9 +2,9 @@ cask "naps2" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
   arch arm: "arm64", intel: "x64"
 
-  version "7.5.1"
-  sha256 arm:   "839e42574fc5abf26768038f28caf5ca57223be28fc2ca75a00b31f98ff7f210",
-         intel: "4b8f30cdf0fb0f9e87cfc92930457af00667eaf6398f9f6e71dc591deecd5b10"
+  version "7.5.2"
+  sha256 arm:   "b919da17a5eb6ae5273d714a48ff61f0d65496047d63303b1f8561635b508b9d",
+         intel: "16c0e2f629a92b9ad5f4340dd56abab937b92f343600f1ce6230c547a8ead9a6"
 
   url "https://github.com/cyanfish/naps2/releases/download/v#{version}/naps2-#{version}-mac-#{arch}.pkg",
       verified: "github.com/cyanfish/naps2/"
