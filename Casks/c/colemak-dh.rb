67d0a7a409810b6859bc050b5185fa610252ef76
phwt <28344318+phwt@users.noreply.github.com>
2024-10-05 17:47:12 +0700

    colemak-dh 1.0 (new cask)

---
+++ Casks/c/colemak-dh.rb
@@ -0,0 +1,16 @@
+cask "colemak-dh" do
+  version "1.0"
+  sha256 :no_check
+
+  url "https://github.com/ColemakMods/mod-dh/archive/refs/heads/master.tar.gz",
+      verified: "github.com/ColemakMods/mod-dh/"
+  name "Colemak-DH Keyboard Layout"
+  desc "Colemak mod for more comfortable typing (DH variant)"
+  homepage "https://colemakmods.github.io/mod-dh/"
+
+  keyboard_layout "mod-dh-master/macOS/Colemak DH.bundle"
+
+  caveats do
+    logout
+  end
+end
