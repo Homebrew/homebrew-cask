c75f648b56446e3667d4f9947d01976461e28e6b
Tobias Stadler <28538704+devtobi@users.noreply.github.com>
2024-10-06 17:50:47 +0200

    Updated cask for MixedInKey11
    Updated the mixed-in-key.rb cask to Mixed In Key 11.

--- Casks/m/mixed-in-key.rb
+++ Casks/m/mixed-in-key.rb
@@ -1,8 +1,8 @@
 cask "mixed-in-key" do
-  version "10.3.1.5057"
+  version "11.0.1.5951"
   sha256 :no_check
 
-  url "https://builds.mixedinkey.com/download/43/release/latest?key=public"
+  url "https://builds.mixedinkey.com/download/67/release/latest?key=public"
   name "Mixed In Key"
   desc "Harmonic mixing for DJs and music producers"
   homepage "https://mixedinkey.com/get#{version.major}/"
@@ -20,12 +20,11 @@ cask "mixed-in-key" do
   uninstall quit: "com.mixedinkey.application"
 
   zap trash: [
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.mixedinkey.application.sfl*",
     "~/Library/Application Support/com.mixedinkey.application",
     "~/Library/Application Support/Mixedinkey",
     "~/Library/Caches/com.mixedinkey.application",
     "~/Library/Preferences/com.mixedinkey.application.plist",
-    "~/Library/Preferences/com.mixedinkey.Session.plist",
-    "~/Library/Saved Application State/com.mixedinkey.application.savedState",
+    "~/Library/HTTPStorages/com.mixedinkey.application",
+    "~/Library/WebKit/com.mixedinkey.application"
   ]
 end
