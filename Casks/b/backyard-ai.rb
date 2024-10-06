ac86b3216b760e7e5b2bd3a9e22eec6c974843be
Daeho Ro <lamanus@outlook.kr>
2024-10-07 00:13:32 +0900

    backyard-ai 0.28.22 (new cask)
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

---
+++ Casks/b/backyard-ai.rb
@@ -0,0 +1,27 @@
+cask "backyard-ai" do
+  arch arm: "Arm64"
+
+  version "0.28.22"
+  sha256 :no_check
+
+  url "https://update.backyard.ai/download/dmg#{arch}"
+  name "Backyard AI"
+  desc "Run AI models locally"
+  homepage "https://backyard.ai/"
+
+  livecheck do
+    url "https://update.backyard.ai/update/darwin#{arch}/0.0.1?beta_updates=true"
+    strategy :json do |json|
+      json["name"]
+    end
+  end
+
+  depends_on macos: ">= :catalina"
+
+  app "Backyard AI.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.ahoylabs.faraday.plist",
+    "~/Library/Saved Application State/com.ahoylabs.faraday.savedState",
+  ]
+end
