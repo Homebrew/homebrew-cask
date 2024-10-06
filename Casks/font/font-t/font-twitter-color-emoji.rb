1fc9cd6de97c57a776747cf9f8b8c84380c1ce98
Daeho Ro <lamanus@outlook.kr>
2024-10-06 18:32:03 +0900

    font-twitter-color-emoji 15.1.0

--- Casks/font/font-t/font-twitter-color-emoji.rb
+++ Casks/font/font-t/font-twitter-color-emoji.rb
@@ -1,6 +1,6 @@
 cask "font-twitter-color-emoji" do
-  version "15.0.3"
-  sha256 "f96d2c9c54b7dc868749e90a2dcb0b060c592de9388763a18ff0a95bee9bbf8c"
+  version "15.1.0"
+  sha256 "9075de7a1c9dd660782d02b5c5be1c1524e16db13a6d7d4264b9aabbd056b692"
 
   url "https://github.com/eosrei/twemoji-color-font/releases/download/v#{version}/TwitterColorEmoji-SVGinOT-#{version}.zip"
   name "Twitter Color Emoji"
