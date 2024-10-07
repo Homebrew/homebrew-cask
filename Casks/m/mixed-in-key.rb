f2f53b270b348f22a6a58f3cda5b009d32bde362
Bevan Kay <email@bevankay.me>
2024-10-07 21:35:38 +1100

    mixed-in-key: update livecheck, version

--- Casks/m/mixed-in-key.rb
+++ Casks/m/mixed-in-key.rb
@@ -1,16 +1,33 @@
 cask "mixed-in-key" do
-  version "11.0.1.5951"
-  sha256 :no_check
+  version "11.0.1.5951,67"
+  sha256 "67219c259a207c03735677aa90add65dee3cad0ac41a22cf30574bafdfbad6eb"
 
-  url "https://builds.mixedinkey.com/download/67/release/latest?key=public"
+  url "https://builds.mixedinkey.com/download/#{version.csv.second}/release/latest?key=public"
   name "Mixed In Key"
   desc "Harmonic mixing for DJs and music producers"
   homepage "https://mixedinkey.com/get#{version.major}/"
 
+  # The download page changes with each major version. The version can only be
+  # found by checking the headers on the download url, which has a variable
+  # download id. Therefore multiple steps are required to find an accurate version.
   livecheck do
-    url :url
+    url "https://mixedinkey.com/learn-more/"
     regex(%r{/Mixed%2BIn%2BKey[._-]v?(\d+(?:\.\d+)+)}i)
-    strategy :header_match
+    strategy :page_match do |page, regex|
+      major_version = page[/Mixed\s+in\s+Key\s+v?(\d+)["< ]/i, 1]
+      next if major_version.blank?
+
+      download_page = Homebrew::Livecheck::Strategy.page_content("https://mixedinkey.com/get#{major_version}/")
+      download_id = download_page[:content].scan(%r{href=.*download/(\d+)/release})&.flatten&.first
+      next if download_id.blank?
+
+      header_url = "https://builds.mixedinkey.com/download/#{download_id}/release/latest?key=public"
+      headers = Homebrew::Livecheck::Strategy.page_headers(header_url)
+      version = headers.first["location"].match(regex)&.captures&.first
+      next if version.blank?
+
+      "#{version},#{download_id}"
+    end
   end
 
   auto_updates true
