0c136fe9f80b85fd756dcaaef633076edc8cf84b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-07 15:49:00 +0000

    lingon-x 9.6.2

--- Casks/l/lingon-x.rb
+++ Casks/l/lingon-x.rb
@@ -16,8 +16,8 @@ cask "lingon-x" do
     sha256 "c1c839e8dc13bd295f2080980c5bea22299c33f3333b7c6981161b46d6f021d8"
   end
   on_monterey :or_newer do
-    version "9.6.1"
-    sha256 "0413668536c29aef56d87844c19b74b0aeb7260eae33215c81c649a2dcd87aee"
+    version "9.6.2"
+    sha256 "c9bf93eae6b61341d1e0d3ce394069b424b4aefd5b5744f9e865f713f93f8ee1"
   end
 
   url "https://www.peterborgapps.com/downloads/LingonX#{version.major}.zip"
