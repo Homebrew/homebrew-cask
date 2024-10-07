2bc96dcb326a27d0dbd7522279db0a4c39a910c2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-07 16:05:44 +0000

    proton-pass 1.23.1

--- Casks/p/proton-pass.rb
+++ Casks/p/proton-pass.rb
@@ -1,6 +1,6 @@
 cask "proton-pass" do
-  version "1.23.0"
-  sha256 "818df028ea498403dd5db830be19801bb69bfb48f1e50b2387b1b84c8066efba"
+  version "1.23.1"
+  sha256 "0d15eafc2f7708aba107b34209c3fca3fa5bd4aa510d88827238a91701372c3e"
 
   url "https://proton.me/download/PassDesktop/darwin/universal/ProtonPass_#{version}.dmg"
   name "Proton Pass"
