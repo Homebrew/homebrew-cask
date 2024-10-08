8713b82dd1baedc2d43eb04d55021016ab22bc55
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-08 10:44:05 +0000

    permute 3.11.13

--- Casks/p/permute.rb
+++ Casks/p/permute.rb
@@ -1,6 +1,6 @@
 cask "permute" do
-  version "3.11.12,2763"
-  sha256 "c2c766033346f5e09ff54dc91976ee13f0ca350470d874de67e9eeda45cce053"
+  version "3.11.13,2768"
+  sha256 "a90a372053ec7717ce22154e07b803b24fc8edc19615260b6f3af437bb39c373"
 
   url "https://software.charliemonroe.net/trial/permute/v#{version.major}/Permute_#{version.major}_#{version.csv.second}.dmg"
   name "Permute"
