37cb141c5347d8589fdad6e854e9c01f4a92938b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-08 00:52:17 +0000

    ibkr 0.10.0i

--- Casks/i/ibkr.rb
+++ Casks/i/ibkr.rb
@@ -1,7 +1,7 @@
 cask "ibkr" do
   arch arm: "-arm", intel: "x-x64"
 
-  version "0.10.0h"
+  version "0.10.0i"
   sha256 :no_check
 
   url "https://download2.interactivebrokers.com/installers/ntws/latest-standalone/ntws-latest-standalone-macos#{arch}.dmg"
@@ -19,10 +19,10 @@ cask "ibkr" do
     args:       ["-q"],
   }
 
-  uninstall quit:   [
-              "com.install4j.5557-0173-2810-0000",
-              "com.install4j.5557-0173-2810-0000.22",
-              "IBKR Desktop.app",
+  uninstall signal: [
+              ["TERM", "com.install4j.5557-0173-2810-0000"],
+              ["TERM", "com.install4j.5557-0173-2810-0000.22"],
+              ["TERM", "IBKR Desktop.app"],
             ],
             script: {
               executable: "/Applications/IBKR Desktop/IBKR Desktop Uninstaller.app/Contents/MacOS/JavaApplicationStub",
@@ -35,5 +35,9 @@ cask "ibkr" do
     "~/Desktop/IBKR Desktop",
     "~/Jts",
     "~/Library/Application Support/IBKR Desktop",
+    "~/Library/HTTPStorages/com.install4j.5557-0173-2810-0000.6858",
+    "~/Library/Preferences/com.install4j.5557-0173-2810-0000.22.plist",
+    "~/Library/Preferences/com.install4j.5557-0173-2810-0000.uninstaller.plist",
+    "~/Library/Saved Application State/com.install4j.5557-0173-2810-0000.22.savedState",
   ]
 end
