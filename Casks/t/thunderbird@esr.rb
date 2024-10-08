91368f49ce505e116e19a5a56c1632ec45fc7907
AltCode <12354672+AlternateRT@users.noreply.github.com>
2024-10-08 22:59:47 +0200

    thunderbird@esr 128.3.0esr (new cask)

---
+++ Casks/t/thunderbird@esr.rb
@@ -0,0 +1,252 @@
+cask "thunderbird@esr" do
+  version "128.3.0esr"
+
+  language "af" do
+    sha256 "c05bd4169cdb2b6e6d6b17911b2282ae2936afc5dbcd22e8d33b09b19d16479e"
+    "af"
+  end
+  language "ar" do
+    sha256 "0a2591ec49ef06d20f0c42f260a8be7f61de65417c39308a76463f3f4d0c44d6"
+    "ar"
+  end
+  language "be" do
+    sha256 "eb3e1b3535ea3b2a6f14ee999dcda6f12c56d7198c7583cb4d6bf651cc7455ba"
+    "be"
+  end
+  language "bg" do
+    sha256 "d301f28783921e782f9fed517edd07da9262fa79a2cb23c97a34ab679c7834f2"
+    "bg"
+  end
+  language "ca" do
+    sha256 "b0a4d075f2b0bbf6a84141ab81ff40e160f1fe151eb5aa7cac35a8c2b91aee35"
+    "ca"
+  end
+  language "cs" do
+    sha256 "a79ad91820e58d25a97392032147766a035c00d92e9c8c10c2c686ffe70a6e3b"
+    "cs"
+  end
+  language "cy" do
+    sha256 "49d7df7aed7f11eec15dba7114039b36154a7e334a30351e0e6d560e05e1f899"
+    "cy"
+  end
+  language "da" do
+    sha256 "5af4d30b969103ca77c3f7b6338f8e6d1915011495bf7e8c15d92d50137490db"
+    "da"
+  end
+  language "de" do
+    sha256 "35d88de7a41ae7dffd69d937e3fadc769622b75484bdd05da027cd930905c84e"
+    "de"
+  end
+  language "el" do
+    sha256 "dda3c5918bec441da389fed884d20a271b8939689b40205f3b6828df4302adb7"
+    "el"
+  end
+  language "en-CA" do
+    sha256 "64bbd754de07530b39bccae739a8b298680b20031368a552386637bcd9c2e6c2"
+    "en-CA"
+  end
+  language "en-GB" do
+    sha256 "a9561fc0ec8e57fff44f61632395d0d9bf94f96b73205e06302da3915e6dd772"
+    "en-GB"
+  end
+  language "en", default: true do
+    sha256 "2c334ece7d558cb35ce51bd358a35620b31539fda05fa0eebbba2a37bc8b050d"
+    "en-US"
+  end
+  language "es-ES" do
+    sha256 "f5e9c0df8a2fe6a39cab25fcc460924f48b2ceea620e33517e2d7b5089484cf6"
+    "es-ES"
+  end
+  language "es-MX" do
+    sha256 "dc9046ae425d6ed3760f156d75d2528de73e097f631de8fa46573cc8af0ed17b"
+    "es-MX"
+  end
+  language "et" do
+    sha256 "a294bb1c284faade08212b57fe5b220d8eefbc10f4d693888a0417796734c558"
+    "et"
+  end
+  language "fi" do
+    sha256 "08b39545b19d6aa2778bf084dde1a554127397e072c2a391a7f7c8b4a585e664"
+    "fi"
+  end
+  language "fr" do
+    sha256 "2912fc1a32298123dd44dc342718ebbb38425118a48c440afcf5e028d4d15636"
+    "fr"
+  end
+  language "fy" do
+    sha256 "da1a74dbc8c10db37fe8af2ae02f5dbf1fb4a9711eabbe0312d88c92c6523bd4"
+    "fy-NL"
+  end
+  language "ga" do
+    sha256 "ce1258eb36d7ecdab3ee03c8c110476b328f36eeb917c3d0fa8ed98a82fc47b8"
+    "ga-IE"
+  end
+  language "gd" do
+    sha256 "5f5a0609606853ea672f912d01da1444d767ad00206cf740d5c80c2d4ced128d"
+    "gd"
+  end
+  language "gl" do
+    sha256 "322318471fb62abc0250f887237bbac891bdc80e6219f793b89adc9c5e12d040"
+    "gl"
+  end
+  language "he" do
+    sha256 "986f37a7d8fdb7d4e0a092363539d7067342b2d73f8288007820f3f5c20400ec"
+    "he"
+  end
+  language "hr" do
+    sha256 "7dcbad11bfd6f46ee30f88f71284e8909e62cc78288f2762348101c71c0b0f3c"
+    "hr"
+  end
+  language "hu" do
+    sha256 "a20448fcd3281568b48ac771d3f7c588b7851d1743fa3d39be244d216d02bdb1"
+    "hu"
+  end
+  language "hy" do
+    sha256 "6a6e4af8b4a54585cb9f3f76d1b8a2c44ebb633a7ed61bc81c8a43b1099f905b"
+    "hy-AM"
+  end
+  language "is" do
+    sha256 "f40ff7eaa0690a5ccbd1d83b1ebdb94185816346c7bb581e452b396df2dab9ce"
+    "is"
+  end
+  language "it" do
+    sha256 "c72288210259dbbb231fc809af1aacd1a87d2fd8058a8dfb4ff4c5f4430bb5e1"
+    "it"
+  end
+  language "ja" do
+    sha256 "96f373b2beaa14742852fdad75ef01e774cb798bed5b68d5530420b5ad918e52"
+    "ja-JP-mac"
+  end
+  language "ka" do
+    sha256 "e368d73edc3d8d33ac28d6462ce72d1c595c37a2a710b714e28e15adec0f27c4"
+    "ka"
+  end
+  language "ko" do
+    sha256 "87d69ae5ff97bcebdb259aa9bf7eed8429478ffbcbfa98314ef8bfa872cdb21b"
+    "ko"
+  end
+  language "lt" do
+    sha256 "001c86a8971bd6ff51e81e571eac06418bfabc5c7bf802f7d7e04507ad468dd5"
+    "lt"
+  end
+  language "lv" do
+    sha256 "4d1801d481c16dc1f66b654883d95381d89f3b7a9dd0e199e7858fde178160c1"
+    "lv"
+  end
+  language "ms" do
+    sha256 "93eb10bcf13b965be7fcee54115230855e983faaf3071a4800ddc3372fd18d73"
+    "ms"
+  end
+  language "nb" do
+    sha256 "a07b58cf9959984b426cf9107934ddca1be594b2e0bb80812c37f702c46bc488"
+    "nb-NO"
+  end
+  language "nl" do
+    sha256 "d7b7d2b88f34ece1563f15d5bfb4caed6b986d4819618f98315849325c1af5a1"
+    "nl"
+  end
+  language "pa-IN" do
+    sha256 "719839490a1ad74f9a2b0c420255a3efb4b654cbb672373c5d3f19e91d10eea1"
+    "pa-IN"
+  end
+  language "pl" do
+    sha256 "b101af590bf8d05fb2b04dbb69e5e5738b4a39b15c6cdea67b8793f46e464fa7"
+    "pl"
+  end
+  language "pt" do
+    sha256 "70d3f8d6271342d72f647faecaa3048925c6299a8001e863128b4f74a4830d37"
+    "pt-PT"
+  end
+  language "pt-BR" do
+    sha256 "e6e587a00ad1849fac09bc485e5f9c2272d9dd482f33d0a92f0ab5a39895a471"
+    "pt-BR"
+  end
+  language "rm" do
+    sha256 "da6269734dfd8aac5ff8df1f616a998807d99185c4cba2954b6f5d0745cac49d"
+    "rm"
+  end
+  language "ro" do
+    sha256 "dbf3130d9abb94e4874073f61cac7bc23729f8e0da9ca2cdecfe74e504df9176"
+    "ro"
+  end
+  language "ru" do
+    sha256 "9b721ce6bdc7b0122e18d433bf93c24bd46620fb3e9a651ebd2e4ce78e06a8d4"
+    "ru"
+  end
+  language "sk" do
+    sha256 "c07866a0fcd08914af425615e03d91810ffa71d84b60e527c6daa2db185a5ce1"
+    "sk"
+  end
+  language "sl" do
+    sha256 "4600f40415f6ddf350aedade3371ca262afc652baf11ac2cab009b0fefcd6947"
+    "sl"
+  end
+  language "sq" do
+    sha256 "71915a7985447f1dae8d8b8f9a5f2844bf1fb0dba869dbc39e558a5dca312893"
+    "sq"
+  end
+  language "sr" do
+    sha256 "49fcef233a6b42ee40ca349e98eae4afe0a6cd826c6c3d1fb875e7393c40203c"
+    "sr"
+  end
+  language "sv" do
+    sha256 "a24f57ca9e42bacb53546afd4ed447e0eb876b799efbc44b1524d6513e12fa3d"
+    "sv-SE"
+  end
+  language "th" do
+    sha256 "be73aa978f01269567a684265730c18c8736809600316ed5c26c39429ba96001"
+    "th"
+  end
+  language "tr" do
+    sha256 "f87c61c0638490e3e53316446bc766e299650253a53a0b4aa072ad92692c015e"
+    "tr"
+  end
+  language "uk" do
+    sha256 "a5f3801254696980383d12750590d70a428a3f7ee695d0043ef8c056971bebea"
+    "uk"
+  end
+  language "uz" do
+    sha256 "fffb6fb86b653c73ad8c864d01cb2d3e2b301d8421cd25a92840c51a5269fa66"
+    "uz"
+  end
+  language "vi" do
+    sha256 "1542ea45cf82d01617eb2b847a5f468d55a3c77f7f5634ebc3ea566e6c067f58"
+    "vi"
+  end
+  language "zh-TW" do
+    sha256 "80e6989f1614fa778973976a0f83002d64ac920dce27774071d05271f8a3721a"
+    "zh-TW"
+  end
+  language "zh" do
+    sha256 "bebcadb0047faf984152bf8300a2bd5b82eaff86c9224febb06f114e55ad62b9"
+    "zh-CN"
+  end
+
+  url "https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/#{version}/mac/#{language}/Thunderbird%20#{version}.dmg",
+      verified: "download-installer.cdn.mozilla.net/pub/thunderbird/"
+  name "Mozilla Thunderbird ESR"
+  name "Mozilla Thunderbird Extended Support Release"
+  desc "Customizable email client"
+  homepage "https://www.thunderbird.net/#{language}/download/esr/"
+
+  livecheck do
+    url "https://product-details.mozilla.org/1.0/thunderbird_versions.json"
+    strategy :json do |json|
+      json["THUNDERBIRD_ESR"]
+    end
+  end
+
+  auto_updates true
+  conflicts_with cask: "thunderbird"
+  depends_on macos: ">= :catalina"
+
+  app "Thunderbird.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.mozilla.thunderbird*.sfl*",
+    "~/Library/Caches/Thunderbird",
+    "~/Library/Preferences/org.mozilla.thunderbird*.plist",
+    "~/Library/Saved Application State/org.mozilla.thunderbird*.savedState",
+    "~/Library/Thunderbird",
+  ]
+end
