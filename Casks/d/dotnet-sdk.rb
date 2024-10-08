446658b8bbce95e8721d0bb588b59555bec330c5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-08 18:44:57 +0000

    dotnet-sdk 8.0.403

--- Casks/d/dotnet-sdk.rb
+++ Casks/d/dotnet-sdk.rb
@@ -2,12 +2,12 @@ cask "dotnet-sdk" do
   arch arm: "arm64", intel: "x64"
 
   on_arm do
-    version "8.0.402,0fb159d5-d255-43ad-bd15-6f8b0787b884,841e854f82f94d29d70980f7775212b7"
-    sha256 "00bf6b7a7451865d40f604e7ce9418671d5db6652863d9095a2ad322b952a792"
+    version "8.0.403,35b0fb29-cadc-4083-aa26-6cecd2e7ffa1,1a9972a435b73ffdd0b462f979ea5b23"
+    sha256 "29844599f2e41002af2605a9811344399b6b50369cf993d91569a1a7909f1293"
   end
   on_intel do
-    version "8.0.402,b98e1dbc-5ba2-446c-b294-0cd01392a91d,13afc1c75aa0baa437f4710a2a6cb2e9"
-    sha256 "7e4121cbc738790ba28367e91834ee8758cb2c913eba9e7bb6e64c8306236905"
+    version "8.0.403,d6b3fe61-3c0e-45da-9e37-cef64d4fd3eb,28d536e0ecfbb330ab49454a5e3962f6"
+    sha256 "cda9f5737413adc752838efb55bc52d631da395d400884506182b8893d6a9949"
   end
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv.second}/#{version.csv.third}/dotnet-sdk-#{version.csv.first}-osx-#{arch}.pkg"
