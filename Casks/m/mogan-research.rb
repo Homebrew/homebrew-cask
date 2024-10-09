ac2376842b4ef6999995adfc9fa888fdfecf0253
fanyunqian <fanyunqian@sensetime.com>
2024-10-09 14:50:19 +0800

    mogan-research: v1.2.9.3

---
+++ Casks/m/mogan-research.rb
@@ -0,0 +1,24 @@
+cask "mogan-research" do
+  arch arm: "-arm"
+
+  version "v1.2.9.3"
+  sha256 arm:   "74cb464ddde67d8c717c87362d5ad4b29c095be19de85ee9363f6b459ad05049",
+         intel: "25bc5fc683bcc60ee720a5995b8ad5990fd702365baea5e7d0590973675cf746"
+
+  url "https://github.com/XmacsLabs/mogan/releases/download/v1.2.9.3/MoganResearch-#{version}#{arch}.dmg"
+  name "Mogan Research"
+  desc "one of the distribution of GNU TeXmacs which is a structured scientific editing platform"
+  homepage "https://mogan.app/"
+
+  livecheck do
+    url "https://github.com/XmacsLabs/mogan/releases"
+    strategy :github_latest
+  end
+
+  app "MoganResearch.app"
+
+  zap trash: [
+    "~/Library/Application Support/XmacsLabs",
+    "~/Library/Saved Application State/app.mogan.research.savedState",
+  ]
+end
