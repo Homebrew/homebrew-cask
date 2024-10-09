0d1fd94165abcc58ced050237f375f49b1b26e27
Aaron-212 <aaron212cn@outlook.com>
2024-10-09 22:48:40 +0800

    font-plangothic 1.9.5766

--- Casks/font/font-p/font-plangothic.rb
+++ Casks/font/font-p/font-plangothic.rb
@@ -1,6 +1,6 @@
 cask "font-plangothic" do
-  version "1.8.5764"
-  sha256 "5d4e3f113e1aaf9f95f2733e3ebd677a5853d17426f649058bd637487af27a85"
+  version "1.9.5766"
+  sha256 "0f0f153732c41212b8e0bdf2ca60bb45893f4fda2a8cfe8df96b0e90648e0d6f"
 
   url "https://github.com/Fitzgerald-Porthmouth-Koenigsegg/Plangothic-Project/archive/refs/tags/V#{version}.tar.gz"
   name "Plangothic"
