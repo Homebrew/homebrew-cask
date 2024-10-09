8b17e616be3198c496184f76181df09420e8e166
fanyunqian <fanyunqian@sensetime.com>
2024-10-09 15:15:06 +0800

    mogan-research: fix verified url

--- Casks/m/mogan-research.rb
+++ Casks/m/mogan-research.rb
@@ -1,11 +1,12 @@
 cask "mogan-research" do
   arch arm: "-arm"
 
-  version "v1.2.9.3"
+  version "1.2.9.3"
   sha256 arm:   "74cb464ddde67d8c717c87362d5ad4b29c095be19de85ee9363f6b459ad05049",
          intel: "25bc5fc683bcc60ee720a5995b8ad5990fd702365baea5e7d0590973675cf746"
 
-  url "https://github.com/XmacsLabs/mogan/releases/download/v1.2.9.3/MoganResearch-#{version}#{arch}.dmg"
+  url "https://github.com/XmacsLabs/mogan/releases/download/v#{version}/MoganResearch-v#{version}#{arch}.dmg",
+      verified: "XmacsLabs/mogan/releases/download/"
   name "Mogan Research"
   desc "one of the distribution of GNU TeXmacs which is a structured scientific editing platform"
   homepage "https://mogan.app/"
