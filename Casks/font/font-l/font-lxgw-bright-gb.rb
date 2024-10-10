917e0ae5e758814d409c5461d1ffc8eedf4ff21e
Klaus Hipp <khipp@users.noreply.github.com>
2024-10-10 11:32:36 +0200

    font-lxgw-bright-gb 5.501

--- Casks/font/font-l/font-lxgw-bright-gb.rb
+++ Casks/font/font-l/font-lxgw-bright-gb.rb
@@ -1,6 +1,6 @@
 cask "font-lxgw-bright-gb" do
-  version "5.500"
-  sha256 "8083e54f6e0576191738c3c7cb8e56a05594bf788feb870d5aed6e2a9214fdd0"
+  version "5.501"
+  sha256 "315b369c7c196b2f2410949883ceabaf26669d4f4bca89bf485e062af84d6299"
 
   url "https://github.com/lxgw/LxgwBright/releases/download/v#{version}/LXGWBrightGB.7z"
   name "LXGW Bright GB"
