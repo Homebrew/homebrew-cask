34ff9bd77f332ac16f785306e8e6b3c8b43bc9bd
Klaus Hipp <khipp@users.noreply.github.com>
2024-10-10 11:35:00 +0200

    chessx 1.6.4a
    Update Casks/c/chessx.rb
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/c/chessx.rb
+++ Casks/c/chessx.rb
@@ -1,8 +1,8 @@
 cask "chessx" do
-  version "1.6.4"
-  sha256 "cf8d57ceb7ebf400ab16a9b5fe5283dac4b3e80f4702ef5190a81f19c124d57f"
+  version "1.6.4a,1.6.4"
+  sha256 "a452933f0862ecf7df29d3f5018ef0ac554de95eefd3c1114d7984a85c66b324"
 
-  url "https://downloads.sourceforge.net/chessx/chessx/#{version}/chessx-#{version}.dmg",
+  url "https://downloads.sourceforge.net/chessx/chessx/#{version.csv.second || version.csv.first}/chessx-#{version.csv.first}.dmg",
       verified: "downloads.sourceforge.net/chessx/"
   name "ChessX"
   desc "Chess database"
@@ -10,7 +10,13 @@ cask "chessx" do
 
   livecheck do
     url "https://sourceforge.net/projects/chessx/rss?path=/chessx"
-    regex(%r{url=.*?/chessx[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
+    regex(%r{url=.*?/v?(\d+(?:\.\d+)+)/chessx[._-]v?(\d+(?:\.\d+)+[a-z]?)\.dmg}i)
+    strategy :page_match do |page, regex|
+      match = page.match(regex)
+      next if match.blank?
+
+      (match[1] == match[2]) ? match[2] : "#{match[2]},#{match[1]}"
+    end
   end
 
   pkg "chessx-installer.mpkg"
