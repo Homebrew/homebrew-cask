9d63b1661bc408620c8cefe3ad48ed3f422dbc7f
Bevan Kay <email@bevankay.me>
2024-10-10 14:50:57 +1100

    motu-m-series 96480

--- Casks/m/motu-m-series.rb
+++ Casks/m/motu-m-series.rb
@@ -1,8 +1,8 @@
 cask "motu-m-series" do
-  version "95979"
-  sha256 "6a9f0582cb2f9e4af31123169e0922b38d884e4041e1d1229a96166e7b6d629b"
+  version "96480"
+  sha256 "c1605afc03b563b2a1b8f4412c2329574bffd8083a0a7f242a2ed1a377cbd76b"
 
-  url "https://cdn-data.motu.com/downloads/audio/mseries/driver/RC/MOTU%20MSeries%20Installer%20(#{version}).pkg"
+  url "https://cdn-data.motu.com/downloads/audio/mseries/driver/RC/MOTU%20M%20Series%20Installer%20(#{version}).pkg"
   name "Motu M-Series"
   desc "Audio interface driver for Motu M-Series (M2, M4, M6) audio interfaces"
   homepage "https://motu.com/en-us/download/product/408/"
