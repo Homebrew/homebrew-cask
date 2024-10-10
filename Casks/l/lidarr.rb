931769e018a114f4cdfcf2929201d90c8bdcb8b6
Bevan Kay <email@bevankay.me>
2024-10-10 14:23:01 +1100

    lidarr 2.6.4.4402

--- Casks/l/lidarr.rb
+++ Casks/l/lidarr.rb
@@ -1,6 +1,6 @@
 cask "lidarr" do
-  version "2.5.3.4341"
-  sha256 "595cea9d087b9c6590271fc287df0817bd3137844b7006c8926eaf71139a84ae"
+  version "2.6.4.4402"
+  sha256 "5c5b7b111803f4447adbab80e6d4b2d28126094ec5459d39760648e9300af698"
 
   url "https://github.com/lidarr/Lidarr/releases/download/v#{version}/Lidarr.master.#{version}.osx-app-core-x64.zip",
       verified: "github.com/lidarr/Lidarr/"
