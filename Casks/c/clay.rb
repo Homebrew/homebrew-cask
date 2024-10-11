f4ce632fc59ed406b6296bb2e24f95396cf304ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-11 18:38:08 +0000

    clay 2.17.16

--- Casks/c/clay.rb
+++ Casks/c/clay.rb
@@ -1,6 +1,6 @@
 cask "clay" do
-  version "2.17.15"
-  sha256 "d99a5cb116fd87b1810a104e34c93a623dffb946551f8758896eb9275f5010e8"
+  version "2.17.16"
+  sha256 "4cb547bc599b41643d2d51e8fddb688e8f468f6c1d0afb4412e1660dbe0542c5"
 
   url "https://assets.clay.earth/desktop/mac/Clay-#{version}-universal.dmg"
   name "Clay"
