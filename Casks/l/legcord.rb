5574386ce9e754b4404a9b0bae8d7e4cb386d971
georgemoodier <84156418+georgemoodier@users.noreply.github.com>
2024-10-12 01:09:41 +0300

    Shortened desc
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/l/legcord.rb
+++ Casks/l/legcord.rb
@@ -5,7 +5,7 @@ cask "legcord" do
   url "https://github.com/legcord/legcord/releases/download/v#{version}/legcord-#{version}-mac-universal.dmg",
       verified: "github.com/legcord/legcord/"
   name "Legcord"
-  desc "Custom lightweight Discord client designed to enhance your experience"
+  desc "Custom Discord client"
   homepage "https://legcord.app/"
 
   depends_on macos: ">= :catalina"
