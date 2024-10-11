f4c792783b63a56468427655507b8f6f10e48229
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-11 09:39:03 +0000

    baidunetdisk 4.39.6

--- Casks/b/baidunetdisk.rb
+++ Casks/b/baidunetdisk.rb
@@ -1,9 +1,9 @@
 cask "baidunetdisk" do
   arch arm: "arm64", intel: "x64"
 
-  version "4.39.3"
-  sha256 arm:   "0cab876e07463af3f02384b87c5ab0ba491d3d3521578aebc1aae5f284de1375",
-         intel: "89c4e9f6e94e8dfdf804323b9daae94cbe642984698018786efa39f85ca1962f"
+  version "4.39.6"
+  sha256 arm:   "2f9c4c767fd1063f1a6339df3ab8d28fab02563b9b0b1b6bea8279312eb73e83",
+         intel: "a10f8144c69bfecebeb79d347e31fd6e17ee7ce0828ddd75c5faae6a1eab17f3"
 
   url "https://issuepcdn.baidupcs.com/issue/netdisk/MACguanjia/#{version}/BaiduNetdisk_mac_#{version}_#{arch}.dmg",
       verified: "issuepcdn.baidupcs.com/issue/netdisk/MACguanjia/"
