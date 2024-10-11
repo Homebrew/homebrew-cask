8b8ff947acc9fb597fdf97d42619a83ababbcec0
georgemoodier <84156418+georgemoodier@users.noreply.github.com>
2024-10-12 01:13:08 +0300

    livecheck fix
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/l/legcord.rb
+++ Casks/l/legcord.rb
@@ -8,6 +8,11 @@ cask "legcord" do
   desc "Custom Discord client"
   homepage "https://legcord.app/"
 
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
   depends_on macos: ">= :catalina"
 
   app "legcord.app"
