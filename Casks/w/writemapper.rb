147de6c82729d85f2022e5fbece5667c7a437718
Klaus Hipp <khipp@users.noreply.github.com>
2024-10-11 15:15:13 +0200

    writemapper 4.4.0

--- Casks/w/writemapper.rb
+++ Casks/w/writemapper.rb
@@ -1,20 +1,25 @@
 cask "writemapper" do
-  version "2.10.0"
-  sha256 "faa919450d0f92d1f6a93d94f51e3ff45a1c92b70c2a408773f299d6e15624a1"
+  version "4.4.0"
+  sha256 "2a877135511b1af412b6386b7efebf2ec2b0fa9bd58200d08c33f948dba1bf92"
 
-  url "https://writemapper.com/static/app/mac/WriteMapper-#{version}.dmg"
+  url "https://writemapper.sfo3.cdn.digitaloceanspaces.com/writemapper-#{version.major}/mac/WriteMapper-#{version}-universal.dmg",
+      verified: "writemapper.sfo3.cdn.digitaloceanspaces.com/"
   name "WriteMapper"
   desc "Writing tool that helps produce text documents using mind maps"
   homepage "https://writemapper.com/"
 
   livecheck do
-    url "https://writemapper.com/static/app/mac/latest-mac.yml"
+    url "https://writemapper.sfo3.digitaloceanspaces.com/writemapper-#{version.major}/mac/latest-mac.yml"
     strategy :electron_builder
   end
 
+  depends_on macos: ">= :catalina"
+
   app "WriteMapper.app"
 
-  caveats do
-    requires_rosetta
-  end
+  zap trash: [
+    "~/Library/Application Support/WriteMapper",
+    "~/Library/Preferences/com.gx.writemapper*.plist",
+    "~/Library/Saved Application State/com.gx.writemapper*.savedState",
+  ]
 end
