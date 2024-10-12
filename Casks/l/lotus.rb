41359dc9b6c3b0447ed47e19c04d6f91eca6f722
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-10-12 11:42:14 -0400

    lotus 1.3.3

--- Casks/l/lotus.rb
+++ Casks/l/lotus.rb
@@ -1,6 +1,6 @@
 cask "lotus" do
-  version "1.3.2"
-  sha256 "ca1535e7f2a2a83aad3b6b3cf694d17932d2e61a17ba4044f1b15fd1ac31ca5c"
+  version "1.3.3"
+  sha256 "198646e1fd02c07c2695b7d1ffcda1473103734f971b13524440e4fdf4fee1b0"
 
   url "https://app-updates.vadimdemedes.com/lotus/download/Lotus-#{version}-mac.zip",
       verified: "app-updates.vadimdemedes.com/lotus/download/"
@@ -25,8 +25,4 @@ cask "lotus" do
     "~/Library/Preferences/com.vadimdemedes.Lotus.plist",
     "~/Library/Saved Application State/com.vadimdemedes.Lotus.savedState",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
