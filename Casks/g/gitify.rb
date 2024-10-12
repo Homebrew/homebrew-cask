f09e24dbd5865de2a2c459d85ef6d2d78db792c8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-12 15:41:47 +0000

    gitify 5.16.1

--- Casks/g/gitify.rb
+++ Casks/g/gitify.rb
@@ -1,6 +1,6 @@
 cask "gitify" do
-  version "5.16.0"
-  sha256 "8ecb5bc74c9324097ae76ad644e11ec9ced5a9324e66add83d56731c572f6168"
+  version "5.16.1"
+  sha256 "3b09d0306200bb1526eb0af3d50c3887ceb209f4aa0c826a688771dbb1f9a111"
 
   url "https://github.com/gitify-app/gitify/releases/download/v#{version}/Gitify-#{version}-universal-mac.zip"
   name "Gitify"
