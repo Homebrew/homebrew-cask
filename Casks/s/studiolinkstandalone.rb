ff7caaabddde8d22491e7d6dc8fd572ddf92c525
Klaus Hipp <khipp@users.noreply.github.com>
2024-10-12 22:06:09 +0200

    studiolinkstandalone 21.07.0
    Update Casks/s/studiolinkstandalone.rb
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/s/studiolinkstandalone.rb
+++ Casks/s/studiolinkstandalone.rb
@@ -1,14 +1,14 @@
 cask "studiolinkstandalone" do
-  version "21.05.0"
+  version "21.07.0"
 
   on_mojave :or_older do
-    sha256 "d09e0b5a27c6a2dbdf206a6f44e966ddb58a8044f2d9ce8895084d5a26a51d88"
+    sha256 "6f8e9770482420936ba0356c986f07822978f91bda37aea1a1bdafd1a36a2144"
 
     url "https://download.studio.link/releases/v#{version}-stable/macos_x86_64/studio-link-standalone-v#{version}-stable.zip",
         verified: "download.studio.link/"
   end
   on_catalina :or_newer do
-    sha256 "72eda324f0084c820b621b849b302812dfb5fd97375f7805614848cbc2bab688"
+    sha256 "13e1e57eb209e451469ec8c973af4ff290306316a7e766a11d8595125abb8608"
 
     url "https://download.studio.link/releases/v#{version}-stable/macos_x86_64/signed/studio-link-standalone-v#{version}-stable.zip",
         verified: "download.studio.link/"
@@ -19,9 +19,11 @@ cask "studiolinkstandalone" do
   homepage "https://studio-link.de/"
 
   livecheck do
-    url "https://gitlab.com/studio.link/app.git"
-    regex(/^v?(\d+(?:\.\d+)*)[._-]stable$/i)
+    url "https://doku.studio-link.de/standalone/installation-standalone.html"
+    regex(%r{href=.*?/releases/v?(\d+(?:\.\d+)+)-stable/macos}i)
   end
 
   app "StudioLinkStandalone.app"
+
+  zap trash: "~/Library/Saved Application State/link.studio.standalone.savedState"
 end
