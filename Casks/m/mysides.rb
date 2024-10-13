6efefdc23c0186b3ef0c85d492348688ef2dec78
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-10-13 11:26:27 -0400

    mysides: deprecate
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/m/mysides.rb
+++ Casks/m/mysides.rb
@@ -6,6 +6,8 @@ cask "mysides" do
   name "mysides"
   homepage "https://github.com/mosen/mysides"
 
+  deprecate! date: "2024-10-13", because: :unmaintained
+
   pkg "mysides-#{version}.pkg"
 
   uninstall pkgutil: "com.github.mosen.mysides"
