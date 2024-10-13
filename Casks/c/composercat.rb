b642804a3357f7e7c6f05d435ea0332663e0b6c4
Klaus Hipp <khipp@users.noreply.github.com>
2024-10-13 15:50:00 +0200

    composercat: deprecate

--- Casks/c/composercat.rb
+++ Casks/c/composercat.rb
@@ -7,10 +7,7 @@ cask "composercat" do
   desc "Graphical interface for Composer (PHP)"
   homepage "https://getcomposercat.com/"
 
-  livecheck do
-    url :homepage
-    regex(%r{href=.*?/Composercat[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
-  end
+  deprecate! date: "2024-10-12", because: :unmaintained
 
   app "Composercat.app"
 
