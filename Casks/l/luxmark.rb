1f02f6cb0f0333ff2c144d0c87c407e64b41b86e
Klaus Hipp <khipp@users.noreply.github.com>
2024-10-13 05:55:30 +0200

    luxmark: update homepage

--- Casks/l/luxmark.rb
+++ Casks/l/luxmark.rb
@@ -2,11 +2,10 @@ cask "luxmark" do
   version "3.1"
   sha256 "eb103ac1bbee170c9fdecb2cd2bc6b70662a0a5f74bcf8e8edf1057d695291c0"
 
-  url "https://github.com/LuxCoreRender/LuxMark/releases/download/luxmark_v#{version}/luxmark-macos64-v#{version}.zip",
-      verified: "github.com/LuxCoreRender/LuxMark/"
+  url "https://github.com/LuxCoreRender/LuxMark/releases/download/luxmark_v#{version}/luxmark-macos64-v#{version}.zip"
   name "LuxMark"
   desc "OpenCL benchmark"
-  homepage "http://www.luxmark.info/"
+  homepage "https://github.com/LuxCoreRender/LuxMark/"
 
   deprecate! date: "2024-10-12", because: :unmaintained
 
