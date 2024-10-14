4c224c3a5386703afc57b7df623afa780c82fa2e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-14 10:03:59 +0000

    mongodb-compass-isolated-edition 1.44.5

--- Casks/m/mongodb-compass-isolated-edition.rb
+++ Casks/m/mongodb-compass-isolated-edition.rb
@@ -1,9 +1,9 @@
 cask "mongodb-compass-isolated-edition" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.44.4"
-  sha256 arm:   "26e43123d33a5452f6ae1751029cfd6b895aad899bd69e98d03ddb7e89c551b3",
-         intel: "f0834cb8f8159f01d0687f13801e96f358faa8ff0003c9ea7cbe92332de83212"
+  version "1.44.5"
+  sha256 arm:   "542d1a8a7686a6f373110ca17e2c6f78516a363948ac74e05bf439f38e101c3a",
+         intel: "9b33b2ddb1fe2d2138a05f278109b3e33df983632dd5087ad5fd16a140a83557"
 
   url "https://downloads.mongodb.com/compass/mongodb-compass-isolated-#{version}-darwin-#{arch}.dmg"
   name "MongoDB Compass Isolated"
