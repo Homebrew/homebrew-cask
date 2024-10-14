859179478861a4b5c749d8683b4bf302f583ab28
Bevan Kay <email@bevankay.me>
2024-10-14 11:25:50 +1100

    fldigi 4.2.06

--- Casks/f/fldigi.rb
+++ Casks/f/fldigi.rb
@@ -1,20 +1,23 @@
 cask "fldigi" do
-  version "4.2.05"
+  version "4.2.06"
 
   on_sierra :or_older do
-    sha256 "d9ca65970968d415bf56d783b4383188297f735c53fc8d379cfb735b3e062974"
+    sha256 "9826dd950661e3261a4233f53e28199d439b83008de178e18f35973ce5d5d10b"
 
-    url "https://downloads.sourceforge.net/fldigi/fldigi/fldigi-#{version}_LI.dmg"
+    url "https://downloads.sourceforge.net/fldigi/fldigi/fldigi-#{version}_LI.dmg",
+        verified: "sourceforge.net/fldigi/"
   end
   on_high_sierra :or_newer do
-    sha256 "88f2c5b018a1aceb2f64f05470e711f98f412ffef221c5276543d0c20303e60c"
+    sha256 "eb18e4d28ca86ad0667af80dcb8b973a7b1875fdcfc9df1b64203ce12237d322"
 
-    url "https://downloads.sourceforge.net/fldigi/fldigi/fldigi-#{version}_VN.dmg"
+    url "https://downloads.sourceforge.net/fldigi/fldigi/fldigi-#{version}_VN.dmg",
+        verified: "sourceforge.net/fldigi/"
   end
 
   name "fldigi"
+  name "flarq"
   desc "Ham radio digital modem application"
-  homepage "https://sourceforge.net/projects/fldigi/files/fldigi/"
+  homepage "http://www.w1hkj.com/"
 
   livecheck do
     url "https://sourceforge.net/projects/fldigi/rss?path=/fldigi"
