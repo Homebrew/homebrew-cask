a34bc5593ec228810951ffedee594e7f9284c814
Klaus Hipp <khipp@users.noreply.github.com>
2024-10-14 09:06:39 +0200

    time-tracker: deprecate

--- Casks/t/time-tracker.rb
+++ Casks/t/time-tracker.rb
@@ -7,10 +7,7 @@ cask "time-tracker" do
   desc "Time tracking app"
   homepage "https://github.com/rburgst/time-tracker-mac"
 
-  livecheck do
-    url :url
-    regex(/^v?(\d+(?:\.\d+)+)-binary$/i)
-  end
+  deprecate! date: "2024-10-14", because: :unmaintained
 
   app "Time Tracker.app"
 
