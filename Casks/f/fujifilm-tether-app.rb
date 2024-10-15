acefdc1a2090eddfeefb55271b2421abba89ec6d
Klaus Hipp <khipp@users.noreply.github.com>
2024-10-15 10:15:37 +0200

    fujifilm-tether-app 1.31.0

--- Casks/f/fujifilm-tether-app.rb
+++ Casks/f/fujifilm-tether-app.rb
@@ -1,6 +1,6 @@
 cask "fujifilm-tether-app" do
-  version "1.30.0,vvr6ik27"
-  sha256 "613e43b3688b6a362a661a767c43b22db87fe574ba41f2cecb88e07c56154437"
+  version "1.31.0,10k15aya"
+  sha256 "b3f73fab393ab9cfeba942bfdc3c10b65dc53ec252b4ceec75b63f676b0278b1"
 
   url "https://dl.fujifilm-x.com/support/software/tether-app-mac#{version.csv.first.no_dots}-#{version.csv.second}/FUJIFILM_TetherApp_Mac#{version.csv.first.no_dots}.pkg"
   name "FUJIFILM TETHER APP"
