d47c899a635ada6f5ff22404993eb7aadb4bd0af
Klaus Hipp <khipp@users.noreply.github.com>
2024-10-15 23:08:53 +0200

    graalvm-jdk 23.0.1

--- Casks/g/graalvm-jdk.rb
+++ Casks/g/graalvm-jdk.rb
@@ -1,9 +1,9 @@
 cask "graalvm-jdk" do
   arch arm: "aarch64", intel: "x64"
 
-  version "23"
-  sha256 arm:   "63290bf6242836ac46ca4c8c4ad048e4482fdb3f8276b9689088f059be430687",
-         intel: "d516ea854e361a821c7679d127ff7126c254b8c43c99e571e203df218df2764b"
+  version "23.0.1"
+  sha256 arm:   "c00a7a62ce453aa026bff65e5a18c63464f725c01e5a71771856226928ba5b0f",
+         intel: "539699d8ff4979623bc7bdf8282ac6f76cd2560f47d14ec5438bada24f136f96"
 
   url "https://download.oracle.com/graalvm/#{version.major}/archive/graalvm-jdk-#{version}_macos-#{arch}_bin.tar.gz",
       verified: "download.oracle.com/"
@@ -16,7 +16,7 @@ cask "graalvm-jdk" do
     regex(/GraalVM\s+for\s+JDK\s+v?(\d+(?:\.\d+)*)\s+downloads/im)
   end
 
-  artifact "graalvm-jdk-#{version}+37.1", target: "/Library/Java/JavaVirtualMachines/graalvm-#{version.major}.jdk"
+  artifact "graalvm-jdk-#{version}+11.1", target: "/Library/Java/JavaVirtualMachines/graalvm-#{version.major}.jdk"
 
   # No zap stanza required
 
