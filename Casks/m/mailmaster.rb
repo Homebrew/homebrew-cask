5b129b0b8d6c0b5a48c17e82f927f59ad64c13e8
Bevan Kay <email@bevankay.me>
2024-10-15 14:54:02 +1100

    mailmaster 5.1.2

--- Casks/m/mailmaster.rb
+++ Casks/m/mailmaster.rb
@@ -1,19 +1,17 @@
 cask "mailmaster" do
-  version "5.0.9.1362"
-  sha256 "47b484146e1d8286654f39fc157f7886eae0fe5de3c0e6924c817a7c67b979c8"
+  version "5.1.2,1378"
+  sha256 :no_check
 
-  url "http://fm.dl.126.net/mailmaster/updatemac/mailmaster-#{version}.dmg",
-      verified: "fm.dl.126.net/mailmaster/"
+  url "https://res.126.net/dl/client/macmail/dashi/mail#{version.major}.dmg",
+      verified: "res.126.net/dl/client/macmail/dashi/"
   name "NetEase Mail Master"
   name "网易邮箱大师"
   desc "Email client"
-  homepage "https://mail.163.com/dashi/"
+  homepage "https://dashi.163.com/"
 
   livecheck do
-    url "http://fm.dl.126.net/mailmaster/updatemac/update_config.json"
-    strategy :json do |json|
-      json["targets"].map { |target| target["version"] }
-    end
+    url "https://u.163.com/macds-beta"
+    strategy :extract_plist
   end
 
   auto_updates true
