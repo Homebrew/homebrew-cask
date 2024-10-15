0d3677e97a5647e2d44613bc93475e5fb3933773
Klaus Hipp <khipp@users.noreply.github.com>
2024-10-15 10:30:01 +0200

    mubu 4.0.6

--- Casks/m/mubu.rb
+++ Casks/m/mubu.rb
@@ -1,9 +1,9 @@
 cask "mubu" do
   arch arm: "-arm64"
 
-  version "4.0.5"
-  sha256 arm:   "3ff86c07dd29d11f59eda6e087d91bd191cd9eff9e8f0b7d2fc7c92188dbde25",
-         intel: "3ff86c07dd29d11f59eda6e087d91bd191cd9eff9e8f0b7d2fc7c92188dbde25"
+  version "4.0.6"
+  sha256 arm:   "e0d8893476a653afbbb4be8f9f6235490ae83e3a2a619d7c63c1ed76c250a0b9",
+         intel: "ee0f1acc1cbdc73299e41ebdb054a7c61544f83cb810dcecb3094b5c3534f10b"
 
   url "https://mubu-assets.tos-cn-shanghai.volces.com/client/Mubu-#{version}#{arch}.dmg",
       verified: "mubu-assets.tos-cn-shanghai.volces.com/client/"
