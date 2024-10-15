e120052337ee1baf2f31d729c44e9e2822e723ad
Klaus Hipp <khipp@users.noreply.github.com>
2024-10-15 10:16:30 +0200

    furtherance 24.10.3

--- Casks/f/furtherance.rb
+++ Casks/f/furtherance.rb
@@ -1,6 +1,6 @@
 cask "furtherance" do
-  version "24.10.2"
-  sha256 "800bf48a93ac758244f734c4755dda3857284f0b1fa920db07cbeff1c98f5f06"
+  version "24.10.3"
+  sha256 "550bd27f3130acb40f6d6cf1748e4691f089ff2f77551b888db25daac4a8a992"
 
   url "https://github.com/unobserved-io/Furtherance/releases/download/#{version}/furtherance-#{version}.dmg",
       verified: "github.com/unobserved-io/Furtherance/"
