c842647ce53e0018ebd421dffcb61a2a95dd740c
Bevan Kay <email@bevankay.me>
2024-10-15 14:29:47 +1100

    mars 4.5.1
    mars: deprecate

--- Casks/m/mars.rb
+++ Casks/m/mars.rb
@@ -1,19 +1,14 @@
 cask "mars" do
-  version "4.5,Aug2014"
+  version "4.5.1"
   sha256 "ac340b676ba2b62246b9df77e62f81ad4447bcfd329ab539716bcd09950b7096"
 
-  url "https://courses.missouristate.edu/KenVollmar/mars/MARS_#{version.csv.first.dots_to_underscores}_#{version.csv.second}/Mars#{version.csv.first.dots_to_underscores}.jar"
+  url "https://github.com/dpetersanderson/MARS/releases/download/v.#{version}/Mars#{version.major_minor.dots_to_underscores}.jar",
+      verified: "github.com/dpetersanderson/MARS/"
   name "MARS"
   desc "Mips Assembly and Runtime Simulator"
-  homepage "https://courses.missouristate.edu/KenVollmar/mars/index.htm"
+  homepage "https://computerscience.missouristate.edu/mars-mips-simulator.htm"
 
-  livecheck do
-    url "https://courses.missouristate.edu/KenVollmar/mars/download.htm"
-    regex(%r{href=.*?MARS_(\d+(?:_\d+)*)_(\w+\d+)/Mars(?:\d+(?:_\d+)*)\.jar}i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| "#{match[0].tr("_", ".")},#{match[1]}" }
-    end
-  end
+  deprecate! date: "2024-10-15", because: :unmaintained
 
   container type: :naked
 
