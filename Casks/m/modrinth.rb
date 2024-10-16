40b776217df95adbe38e81810e8704d40e650a9b
Klaus Hipp <khipp@users.noreply.github.com>
2024-10-16 08:39:24 +0200

    modrinth 0.8.9

--- Casks/m/modrinth.rb
+++ Casks/m/modrinth.rb
@@ -1,6 +1,6 @@
 cask "modrinth" do
-  version "0.8.8"
-  sha256 "1146a1a41c33365fa2574cfcd0951f8b65a224d4374fd8186f95e136aa098129"
+  version "0.8.9"
+  sha256 "f8627dbf326ab31ae01eb4f1634d1e969c8449faabf1608d8ef2b23471486038"
 
   url "https://launcher-files.modrinth.com/versions/#{version}/macos/Modrinth%20App_#{version}_universal.dmg"
   name "Modrinth App"
@@ -8,8 +8,10 @@ cask "modrinth" do
   homepage "https://modrinth.com/"
 
   livecheck do
-    url "https://modrinth.com/app"
-    regex(/Modrinth%20App[._-]v?(\d+(?:\.\d+)+)[._-]universal\.dmg/i)
+    url "https://launcher-files.modrinth.com/updates.json"
+    strategy :json do |json|
+      json["version"]
+    end
   end
 
   auto_updates true
