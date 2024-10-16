6c556beb08302f3f6e582169e54feb3e336c6969
Klaus Hipp <khipp@users.noreply.github.com>
2024-10-16 15:54:15 +0200

    simpholders: various updates

--- Casks/s/simpholders.rb
+++ Casks/s/simpholders.rb
@@ -4,19 +4,27 @@ cask "simpholders" do
 
   url "https://simpholders.com/site/assets/files/#{version.csv.second}/simpholders_#{version.csv.first.dots_to_underscores}.dmg"
   name "SimPholders"
+  desc "Access utility for iPhone Simulator apps"
   homepage "https://simpholders.com/"
 
   livecheck do
-    url "https://simpholders.com/latest/"
-    regex(%r{/(\d+)/simpholders_(\d+(?:_\d+)*).dmg}i)
-    strategy :header_match do |headers, regex|
-      headers["location"].scan(regex).map do |match|
-        "#{match[1].tr("_", ".")},#{match[0]}"
-      end
+    url "https://simpholders.com/releases/"
+    regex(%r{/(\d+)/simpholders[._-](\d+(?:[._]\d+)+)\.dmg}i)
+    strategy :sparkle do |item, regex|
+      item.url.scan(regex).map { |match| "#{match[1].tr("_", ".")},#{match[0]}" }
     end
   end
 
+  auto_updates true
   depends_on macos: ">= :catalina"
 
   app "simpholders_#{version.csv.first.dots_to_underscores}.app", target: "SimPholders.app"
+
+  zap trash: [
+    "~/Library/Application Support/SimPholders",
+    "~/Library/Caches/com.kf-interactive.simpholders.SimPholders",
+    "~/Library/HTTPStorages/com.kf-interactive.simpholders.SimPholders",
+    "~/Library/HTTPStorages/com.kf-interactive.simpholders.SimPholders.binarycookies",
+    "~/Library/Preferences/com.kf-interactive.simpholders.SimPholders.plist",
+  ]
 end
