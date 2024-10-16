c9bc02111bce3b4c3362247ebb10345d316f2df1
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-10-15 22:25:23 -0400

    sync 2.2.47

--- Casks/s/sync.rb
+++ Casks/s/sync.rb
@@ -1,6 +1,6 @@
 cask "sync" do
-  version "2.2.46"
-  sha256 "42061649f2bc3442c692f9a019e576cec4b00dcb72aaa8feb87b29ca48fd7d65"
+  version "2.2.47"
+  sha256 "e04bdea8a60ca119921ac7616767bbba29b92cf83844f777ea5c6c2c3f3c6261"
 
   url "https://www.sync.com/download/apple/Sync-#{version}.dmg"
   name "Sync"
