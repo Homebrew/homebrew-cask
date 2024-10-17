2e61baafbecdae6da769c0d1d4ca8b052ae5daeb
Tobias L. Dideriksen <git@vty.dk>
2024-10-17 14:09:18 +0200

    Update Casks/n/nagstamon.rb
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/n/nagstamon.rb
+++ Casks/n/nagstamon.rb
@@ -2,8 +2,8 @@ cask "nagstamon" do
   arch arm: "ARM", intel: "Intel"
 
   version "3.16.0"
-  sha256  arm:    "c74b110a924c9b8a813d7aa6da14035f500fd49495344acd93f07031d2f2d21f",
-          intel:  "195ad7fcd000e3396089f97746b899c093f16b9cfe7beb81e6225054e83cba4f"
+  sha256  arm:   "c74b110a924c9b8a813d7aa6da14035f500fd49495344acd93f07031d2f2d21f",
+          intel: "195ad7fcd000e3396089f97746b899c093f16b9cfe7beb81e6225054e83cba4f"
 
   url "https://github.com/HenriWahl/Nagstamon/releases/download/v#{version}/Nagstamon.#{version}.#{arch}.dmg",
       verified: "github.com/HenriWahl/Nagstamon/"
