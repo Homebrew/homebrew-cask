a6e642362b2e7521b6e72955b91db2bc627ee190
Daeho Ro <lamanus@outlook.kr>
2024-10-18 23:41:53 +0900

    xcodes 2.3.0b28

--- Casks/x/xcodes.rb
+++ Casks/x/xcodes.rb
@@ -1,6 +1,6 @@
 cask "xcodes" do
-  version "2.2.0b27"
-  sha256 "d9afb3aef44ab5ea2f4d869ec5fd63188c16f80f17a2abe9a01eeb104c9ee05b"
+  version "2.3.0b28"
+  sha256 "9ded84e8da5006e7e9e282c765290f0a5ab15a5e0d960e86125339e6ae63661e"
 
   url "https://github.com/XcodesOrg/XcodesApp/releases/download/v#{version}/Xcodes.zip"
   name "Xcodes"
