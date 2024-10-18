ec539a763be31a870af078f2896549f04a620654
kazz187 <kazzone87@gmail.com>
2024-10-18 15:43:07 +0900

    realforce 3.3.0

--- Casks/r/realforce.rb
+++ Casks/r/realforce.rb
@@ -1,6 +1,6 @@
 cask "realforce" do
-  version "3.2.3"
-  sha256 "8ae41312647bc779699fded0f257ba3767cb4c2814b92e7093362ce3dda2748b"
+  version "3.3.0"
+  sha256 "85c6a76425933c88070d6c61d61224c534e067addeaa99f5f93d973bd4c7d2b7"
 
   url "https://www.realforce.co.jp/support/download/software/mac/files/REALFORCE%20CONNECT%20Software_#{version.dots_to_hyphens}.pkg"
   name "REALFORCE for Mac"
