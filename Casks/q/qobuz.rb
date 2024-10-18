624304a8d8e7ab591fc4817b37501755d372c078
Klaus Hipp <khipp@users.noreply.github.com>
2024-10-18 10:42:07 +0200

    qobuz 7.2.0

--- Casks/q/qobuz.rb
+++ Casks/q/qobuz.rb
@@ -1,9 +1,9 @@
 cask "qobuz" do
   arch arm: "arm64/bigsur", intel: "x64/elCapitan_sierra"
 
-  version "7.1.4,008"
-  sha256 arm:   "4eaec2b1bf307b370122cc79899416fef52866f189f4f3f7b65e3bb5f7e942dd",
-         intel: "46d50274f60ab1e860a0db94ccfda7dbcde24cb4983d5518a281804256ff0500"
+  version "7.2.0,116"
+  sha256 arm:   "8de8f99fff3f87983e1899bc5091ca6ce6f27b17ae9ab0c745a9491c28294053",
+         intel: "f6fc28c7a6f07f81e7f5e4f69fbb95ae7df701b4789d721b0efe799431ba6d7c"
 
   url "https://desktop.qobuz.com/releases/darwin/#{arch}/#{version.csv.first}-b#{version.csv.second}/Qobuz.dmg"
   name "Qobuz"
