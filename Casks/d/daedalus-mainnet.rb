e958ec8119e36f4dab8e9f19d69c48ccb76f04eb
Klaus Hipp <khipp@users.noreply.github.com>
2024-10-18 15:49:51 +0200

    daedalus-mainnet: update livecheck
    Update Casks/d/daedalus-mainnet.rb
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/d/daedalus-mainnet.rb
+++ Casks/d/daedalus-mainnet.rb
@@ -10,9 +10,12 @@ cask "daedalus-mainnet" do
 
   livecheck do
     url "https://update-cardano-mainnet.iohk.io/daedalus-latest-version.json"
-    regex(%r{/daedalus[._-](\d+(?:\.\d+)+)[._-](\d+)[._-]mainnet[._-](\h+)[._-]x86_64[._-]darwin\.pkg}i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| "#{match[0]},#{match[1]},#{match[2]}" }
+    regex(/daedalus[._-]v?(\d+(?:\.\d+)+)[._-](\d+)(?:[._-]mainnet)?[._-](\h+)/i)
+    strategy :json do |json, regex|
+      match = json.dig("platforms", "darwin", "URL")&.match(regex)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]},#{match[3]}"
     end
   end
 
