6ef6a4aa2f5d2d3c1aebb13e78c37773030edaf1
Klaus Hipp <khipp@users.noreply.github.com>
2024-10-19 18:24:33 +0200

    qsync-client: update livecheck

--- Casks/q/qsync-client.rb
+++ Casks/q/qsync-client.rb
@@ -1,15 +1,24 @@
 cask "qsync-client" do
-  version "5.1.3.0913"
+  version "5.1.3,0913"
   sha256 "901ce3c9fd1fa6b0001fa84fd3be1e8f0865c7f9aa068898aadffd34e7acbb2e"
 
-  url "https://download.qnap.com/Storage/Utility/QNAPQsyncClientMac-#{version}.dmg"
+  url "https://download.qnap.com/Storage/Utility/QNAPQsyncClientMac-#{version.csv.first}.#{version.csv.second}.dmg"
   name "Qnap Qsync"
   desc "Automatic file synchronisation"
   homepage "https://www.qnap.com/en/utilities/essentials#utliity_3"
 
   livecheck do
     url "https://update.qnap.com/SoftwareRelease.xml"
-    regex(/QNAPQsyncClientMac[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+    strategy :xml do |xml|
+      item = xml.elements["//application[productName[text()='Qsync']]/platform[platformName[text()='Mac']]/software"]
+      next if item.blank?
+
+      version = item.elements["version"]&.text&.strip
+      build = item.elements["buildNumber"]&.text&.strip
+      next if version.blank? || build.blank?
+
+      "#{version},#{build}"
+    end
   end
 
   installer manual: "Qsync Client.pkg"
