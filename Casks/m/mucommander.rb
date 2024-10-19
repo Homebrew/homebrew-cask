d3dbcc55987d17649b4c37975d5aece8b6605fd7
Yurii Kolesnykov <root@yurikoles.com>
2024-10-19 20:50:14 +0100

    mucommander 1.5.2-1

--- Casks/m/mucommander.rb
+++ Casks/m/mucommander.rb
@@ -1,9 +1,9 @@
 cask "mucommander" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.5.1-1"
-  sha256 arm:   "a214856585a95b91426cf9306d7e9d65d1e1d8079b846eb9dd56f66ae3396283",
-         intel: "2eaca71da3f8e588b15fe28b08559c15cf0dcd34c62e778c0a5fae5478908489"
+  version "1.5.2-1"
+  sha256 arm:   "43f9206c7f886071cb3e2b15d7604acabd9d8b9940be0d284b88661da90037f7",
+         intel: "7acc221cb91511687937d3c5653bbc498906ae1a95594c7f1bac2ddc6d4cd1c4"
 
   url "https://github.com/mucommander/mucommander/releases/download/#{version}/muCommander-#{version}-#{arch}.dmg",
       verified: "github.com/mucommander/mucommander/"
