1b15a9f9cf7faa37070c728b539f55255990841b
Aadit Kamat <aadit.kamat@ufl.edu>
2024-10-20 13:03:26 -0400

    Fix comma issue
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/l/langgraph-studio.rb
+++ Casks/l/langgraph-studio.rb
@@ -2,7 +2,8 @@ cask "langgraph-studio" do
   version "0.0.28"
   sha256 "c518591bd2ae3c6aa37d9fa4fd3e713ca7b570377f78619997d4b36ed6dbfca2"
 
-  url "https://github.com/langchain-ai/langgraph-studio/releases/download/v#{version}/LangGraph-Studio-#{version}-universal.dmg"
+  url "https://github.com/langchain-ai/langgraph-studio/releases/download/v#{version}/LangGraph-Studio-#{version}-universal.dmg",
+      verified: "github.com/langchain-ai/langgraph-studio/"
   name "LangGraph Studio"
   desc "Desktop app for prototyping and debugging LangGraph applications locally"
   homepage "https://studio.langchain.com/"
