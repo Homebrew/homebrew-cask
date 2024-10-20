041333899e99fdf4fc32e5240b4c74ed85e95c1d
Aadit Kamat <aadit.kamat@ufl.edu>
2024-10-20 13:01:13 -0400

    Remove generated comments
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/l/langgraph-studio.rb
+++ Casks/l/langgraph-studio.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Cask-Cookbook
-#                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 cask "langgraph-studio" do
   version "0.0.28"
   sha256 "c518591bd2ae3c6aa37d9fa4fd3e713ca7b570377f78619997d4b36ed6dbfca2"
