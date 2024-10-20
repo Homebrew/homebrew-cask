ca2415b008143b3055ce1fdea594dbf23f6f8534
Aadit Kamat <aadit.kamat@ufl.edu>
2024-10-20 11:32:16 -0400

    Update cask

--- Casks/l/langgraph-studio.rb
+++ Casks/l/langgraph-studio.rb
@@ -2,18 +2,18 @@
 #                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 cask "langgraph-studio" do
-  version "0.0.27"
-  sha256 "64964bd2cb47c345184cb0894b61cc4b1181db4ba66678231cf380448e613111"
+  version "0.0.28"
+  sha256 "c518591bd2ae3c6aa37d9fa4fd3e713ca7b570377f78619997d4b36ed6dbfca2"
 
-  url "langgraph-studio"
+  url "https://github.com/langchain-ai/langgraph-studio/releases/download/v#{version}/LangGraph-Studio-#{version}-universal.dmg"
   name "langgraph-studio"
-  desc "Desktop app for prototyping and debugging LangGraph applications locally."
-  homepage "https://studio.langchain.com/"
+  desc "Desktop app for prototyping and debugging LangGraph applications locally"
+  homepage "https://github.com/langchain-ai/langgraph-studio/"
 
   # Documentation: https://docs.brew.sh/Brew-Livecheck
   livecheck do
     url :homepage
-    regex(/api/latest)
+    strategy :github_latest
   end
 
   depends_on macos: ">= :catalina"
