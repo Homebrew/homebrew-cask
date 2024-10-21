6b21ef511547cc04bdc58050b9b9173d5a535f69
Daeho Ro <lamanus@outlook.kr>
2024-10-21 23:12:30 +0900

    epilogue-playback: checksum update

--- Casks/e/epilogue-playback.rb
+++ Casks/e/epilogue-playback.rb
@@ -1,6 +1,6 @@
 cask "epilogue-playback" do
   version "1.5.0"
-  sha256 "fa28b163993d78acd9f7b012b9ac51a06f2c87a2a990f39bcc96e307a6c3d380"
+  sha256 "4748e80b8102527560019748311ff74244bc2e71c62e45e78fa343b80ae16543"
 
   url "https://epilogue.nyc3.digitaloceanspaces.com/releases/software/Playback/version/#{version}/release/mac/Playback.dmg",
       verified: "epilogue.nyc3.digitaloceanspaces.com/releases/software/Playback/version/"
