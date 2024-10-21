68cb46be412471a17e105b4202114ddd4594ad46
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-21 18:38:30 +0000

    chipmunk 3.13.5

--- Casks/c/chipmunk.rb
+++ Casks/c/chipmunk.rb
@@ -1,9 +1,9 @@
 cask "chipmunk" do
   arch arm: "-arm64"
 
-  version "3.13.3"
-  sha256 arm:   "908e81cf75516aaeab55227ad667183063c5bac86f1fc98a2f4a2685a945ede0",
-         intel: "231c1eea526af3bf9e106dfb5d3ce76c51cb56706c8050d8b7532887c3b456c1"
+  version "3.13.5"
+  sha256 arm:   "0d6776ad9bd7dbcccaca85ef7cd6d80d4febfb3d9f6774911b5599f96dbd7024",
+         intel: "c5f62599cdaf7ec91460c65d6e2158a732cb4f6f7967acc784249dcb278e0a66"
 
   url "https://github.com/esrlabs/chipmunk/releases/download/#{version}/chipmunk@#{version}-darwin#{arch}-portable.tgz"
   name "Chipmunk Log Analyzer & Viewer"
