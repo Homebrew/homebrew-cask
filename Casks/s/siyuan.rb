af0cf89665c9e6374c9a51c6950dbfa913ee996b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-22 03:58:01 +0000

    siyuan 3.1.10

--- Casks/s/siyuan.rb
+++ Casks/s/siyuan.rb
@@ -1,9 +1,9 @@
 cask "siyuan" do
   arch arm: "-arm64"
 
-  version "3.1.9"
-  sha256 arm:   "9c9b64e65e774b7cf98e03f950f34f25913889f0ad4ad3c246e06bd824aaec6e",
-         intel: "23ea5d6e5a56c917359a626f2f0c1f56a3e99f0a3451f354c958e223f87cc5e1"
+  version "3.1.10"
+  sha256 arm:   "c7b32b810561cff5efd3bba4cb580bf0d2bb8942bebaeaa5b9e807683c4075d9",
+         intel: "5d9fc93005ee7d57c1326ba1eb0b5bb86487475b98192395b4337c378bc670a4"
 
   url "https://github.com/siyuan-note/siyuan/releases/download/v#{version}/siyuan-#{version}-mac#{arch}.dmg"
   name "SiYuan"
