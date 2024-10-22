0a7245cd8957a78227335c8315d53602347e78f1
Kuingsmile <mashiqing@horosama.com>
2024-10-22 16:06:20 +0800

    piclist 2.9.4

--- Casks/p/piclist.rb
+++ Casks/p/piclist.rb
@@ -1,9 +1,9 @@
 cask "piclist" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.9.3"
-  sha256 arm:   "409f83806d639a928f0f12fa28406ab05fa4261357fc9a5f993899ce1a162bd6",
-         intel: "a6977b9ba2b6e883ab200893e189cfe5a4dd26cff674fa48dc39f071222d653f"
+  version "2.9.4"
+  sha256 arm:   "8d350757d77c506d74115668ff666f37e5bf776d0ed8f78bd259b3f7f98d724b",
+         intel: "02be28ac99ee7b22d890071a23ae2a243e1e4ed7cdcae92550889a13f9921f35"
 
   url "https://release.piclist.cn/latest/PicList-#{version}-#{arch}.dmg"
   name "PicList"
