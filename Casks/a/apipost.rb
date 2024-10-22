25c289493cd024542ffd795434d9c136883b0b05
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-22 09:35:04 +0000

    apipost 8.1.5

--- Casks/a/apipost.rb
+++ Casks/a/apipost.rb
@@ -1,9 +1,9 @@
 cask "apipost" do
   arch arm: "arm64", intel: "x64"
 
-  version "8.1.4"
-  sha256 arm:   "c8cc66adb5a88b39bcdd76ca7bb82ca6f3176110518f48986b93a530f159f4a0",
-         intel: "cda4dde5d29ecd784cd161b084d0324d26173aeefd43149b63f6615bf79151f8"
+  version "8.1.5"
+  sha256 arm:   "5396dea0a3cf0ac7e636083670b28dfb6a474ceebf6ced90dc1ba6e289ae8185",
+         intel: "ba5acf873e433641d73ebae85fabbcda36783d5d2fa0aa10be08efa57443fa42"
 
   url "https://www.apipost.cn/dl.php?client=Mac&arch=#{arch}&version=#{version}"
   name "Apipost"
