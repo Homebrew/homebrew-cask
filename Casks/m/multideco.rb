9bc2de8d464d3d4524a2284af00af1958791f7b3
rockyluke <github@offline.net>
2024-10-22 22:23:22 +0200

    fix: audit

--- Casks/m/multideco.rb
+++ Casks/m/multideco.rb
@@ -2,7 +2,7 @@ cask "multideco" do
   version "4.22.0"
   sha256 "8f7a938eeec2da4a1a772c030241343521b27ab1b0804f7774290cbad538a9c3"
 
-  url "https://www.hhssoftware.com/multideco/installfile.php?file=multideco_4.22.0_arm_intel.dmg&keytag=1608834&qwerty=1729625551&os=mac_intel"
+  url "https://www.hhssoftware.com/multideco/installfile.php?file=multideco_#{version}_arm_intel.dmg&keytag=1608834&qwerty=1729625551&os=mac_intel"
   name "multideco"
   desc "Dive decompression planning software"
   homepage "https://www.hhssoftware.com/multideco/"
