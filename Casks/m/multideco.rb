af6c6ccd2288546522deaeb411c5dda19beda527
rockyluke <github@offline.net>
2024-10-22 22:10:45 +0200

    fix: audit

--- Casks/m/multideco.rb
+++ Casks/m/multideco.rb
@@ -1,16 +1,12 @@
 cask "multideco" do
   version "4.22.0"
-  sha256 "51a5a7551ffd054cf034e37fa0e53bd322703625b553f5abcde0967f6ca91562"
+  sha256 "8f7a938eeec2da4a1a772c030241343521b27ab1b0804f7774290cbad538a9c3"
 
-  url "https://www.hhssoftware.com/multideco/installfile.php?file=multideco_4.22.0_arm_intel.dmg&keytag=1608834&qwerty=17296\
-25551&os=mac_intel",
-      verified: "hhssoftware.com"
+  url "https://www.hhssoftware.com/multideco/installfile.php?file=multideco_4.22.0_arm_intel.dmg&keytag=1608834&qwerty=1729625551&os=mac_intel"
   name "multideco"
   desc "Dive decompression planning software"
   homepage "https://www.hhssoftware.com/multideco/"
 
-  # Instead of 'pkg', use the installer method with 'manual'
-  # installer manual: "MultiDeco_arm_intel.pkg"
   pkg "MultiDeco_arm_intel.pkg"
 
   uninstall pkgutil: "com.hhssoftware.pkg.MultiDeco"
