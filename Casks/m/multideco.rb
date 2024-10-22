6d20ea9b0a477e66d55b108fc40cd4b7dcb9c433
rockyluke <github@offline.net>
2024-10-22 23:07:27 +0200

    fix: style

--- Casks/m/multideco.rb
+++ Casks/m/multideco.rb
@@ -6,15 +6,15 @@ cask "multideco" do
   name "MultiDeco"
   desc "Dive decompression planning software"
   homepage "https://www.hhssoftware.com/multideco/"
-  
+
   livecheck do
     url "https://www.hhssoftware.com/multideco/download.php"
     strategy :page_match
     regex(/Mac[[:space:]]v?(\d+(?:\.\d+)+)/i)
   end
-  
+
   auto_updates true
-  
+
   pkg "MultiDeco_arm_intel.pkg"
 
   uninstall quit:    "com.hhssoftware.pkg.MultiDeco",
@@ -22,6 +22,6 @@ cask "multideco" do
 
   zap trash: [
     "~/Library/Preferences/com.hhssoftware.multideco.plist",
-    "~/Library/Saved Application State/com.hhssoftware.multideco.savedState"
+    "~/Library/Saved Application State/com.hhssoftware.multideco.savedState",
   ]
 end
