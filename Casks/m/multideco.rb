55e22908f0aa246350e28d93ac16ac245630e649
rockyluke <github@offline.net>
2024-10-22 22:46:17 +0200

    fix: audit

--- Casks/m/multideco.rb
+++ Casks/m/multideco.rb
@@ -1,6 +1,6 @@
 cask "multideco" do
   version "4.22.0"
-  sha256 "8f7a938eeec2da4a1a772c030241343521b27ab1b0804f7774290cbad538a9c3"
+  sha256 "51a5a7551ffd054cf034e37fa0e53bd322703625b553f5abcde0967f6ca91562"
 
   url "https://www.hhssoftware.com/multideco/installfile.php?file=multideco_#{version}_arm_intel.dmg&keytag=1608834&qwerty=#{Time.now.to_i}&os=mac_intel",
       verified: "www.hhssoftware.com"
