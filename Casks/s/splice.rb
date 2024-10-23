df878511173e52878228297f41f9591bd8324c48
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-23 19:09:15 +0000

    splice 5.1.7

--- Casks/s/splice.rb
+++ Casks/s/splice.rb
@@ -1,7 +1,7 @@
 cask "splice" do
   arch arm: "arm64", intel: "x64"
 
-  version "5.1.6"
+  version "5.1.7"
   sha256 :no_check
 
   url "https://desktop.splice.com/darwin/stable/#{arch}/Splice.app.zip"
