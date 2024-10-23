fb01d851bdda641e2878f59b1a462302f1b588d6
John Benninghoff <jbenninghoff@mac.com>
2024-10-23 09:19:29 -0500

    update url

--- Casks/w/webex.rb
+++ Casks/w/webex.rb
@@ -1,10 +1,10 @@
 cask "webex" do
-  arch arm: "Desktop-MACOS-Apple-Silicon", intel: "TeamsDesktop-MACOS"
+  arch arm: "apple-silicon", intel: "intel"
 
   version "44.10.1.31028"
   sha256 :no_check
 
-  url "https://binaries.webex.com/Webex#{arch}-Gold/Webex.dmg"
+  url "https://binaries.webex.com/webex-macos-#{arch}/Webex.dmg"
   name "Webex Teams"
   desc "Video communication and virtual meeting platform"
   homepage "https://www.webex.com/"
