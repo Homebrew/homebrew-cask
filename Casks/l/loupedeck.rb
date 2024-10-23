76dc4965e28edcaa388b281f1428060cffed02d6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-23 12:57:40 +0000

    loupedeck 6.0.1.20874

--- Casks/l/loupedeck.rb
+++ Casks/l/loupedeck.rb
@@ -1,6 +1,6 @@
 cask "loupedeck" do
-  version "5.9.1.19365"
-  sha256 "cf0357880332e537827b328716345b9c6dff0537a189f3181f6ff8565aef2db7"
+  version "6.0.1.20874"
+  sha256 "797cba59a87e9334c63f55a351f976773cb32c4b805dcbcac5e10b5510acb040"
 
   url "https://5145542.fs1.hubspotusercontent-na1.net/hubfs/5145542/Knowledge%20Base/LD%20Software%20Downloads/#{version.major_minor_patch.chomp(".0")}/LoupedeckInstaller_#{version}.dmg",
       verified: "5145542.fs1.hubspotusercontent-na1.net/hubfs/5145542/"
@@ -19,7 +19,9 @@ cask "loupedeck" do
 
   uninstall launchctl: "com.loupedeck.loupedeck2.launch",
             quit:      [
+              "com.logi.pluginservice",
               "com.loupedeck.Loupedeck2",
+              "com.loupedeck.loupedeckApp",
               "com.loupedeck.loupedeckconfig",
             ],
             pkgutil:   [
@@ -32,16 +34,20 @@ cask "loupedeck" do
               "com.loupedeck.OBSClientPluginPackageInstaller",
               "com.loupedeck.PluginPackageInstaller",
               "com.loupedeck.UIPackageInstaller",
+              "com.loupedeck.UIPluginPackageInstaller",
             ],
             delete:    "/Applications/Loupedeck.app"
 
   zap trash: [
-    "~/Library/Application Support/Adobe/CameraRaw/Settings/Loupedeck - Karo Holmberg",
-    "~/Library/Application Support/Adobe/CameraRaw/Settings/Loupedeck - Loke Roos",
+    "/Library/Application Support/Loupedeck",
+    "~/Library/Application Support/Adobe/CameraRaw/Settings/Loupedeck*",
     "~/Library/Application Support/Adobe/Lightroom/Export Presets/Loupedeck Exports",
     "~/Library/Application Support/Adobe/Lightroom/Modules/loupedeck2.lrplugin",
     "~/Library/Application Support/Capture One/KeyboardShortcuts/Loupedeck_beta.plist",
+    "~/Library/Application Support/Loupedeck",
     "~/Library/Application Support/LoupedeckConfig",
     "~/Library/Logs/LoupedeckConfig",
+    "~/Library/Preferences/com.loupedeck.loupedeckApp.plist",
+    "~/Library/Saved Application State/com.loupedeck.loupedeckApp.savedState",
   ]
 end
