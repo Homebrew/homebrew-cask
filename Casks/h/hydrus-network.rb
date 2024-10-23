9a79101897a8641f31d92db545eb529be78130c8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-23 21:44:39 +0000

    hydrus-network 595

--- Casks/h/hydrus-network.rb
+++ Casks/h/hydrus-network.rb
@@ -1,6 +1,6 @@
 cask "hydrus-network" do
-  version "594"
-  sha256 "0a2a6bf4f75b101c28827ae572e2dc65de0c1fa7359c11bff164df719d705006"
+  version "595"
+  sha256 "e6c991d6e3d2f2f3fc81cd42f2fa73d6d2cf07e93f7f4e34c78659b10c60128c"
 
   url "https://github.com/hydrusnetwork/hydrus/releases/download/v#{version}/Hydrus.Network.#{version}.-.macOS.-.App.dmg",
       verified: "github.com/hydrusnetwork/hydrus/"
