befb09b136b617d2fb1a702e1e6e200115778537
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-23 01:06:55 +0000

    quicken 7.10.0

--- Casks/q/quicken.rb
+++ Casks/q/quicken.rb
@@ -1,6 +1,6 @@
 cask "quicken" do
-  version "7.9.1,709.54540.100"
-  sha256 "122a90e5eb446f9fb1778399979474bb20c0163620ac86c1a61bb98cdbd8f1f1"
+  version "7.10.0,710.55343.100"
+  sha256 "fd21bcbae96d10369b4d3e950622a7c3f9d9e4c30250c4f57c1ca5023cc58b26"
 
   url "https://download.quicken.com/mac/Quicken/001/Release/031A96D9-EFE6-4520-8B6A-7F465DDAA3E4/Quicken-#{version.csv.second}/Quicken-#{version.csv.second}.zip"
   name "Quicken"
