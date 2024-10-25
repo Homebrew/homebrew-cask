275f5a36085cbea1f7066c1bf0ebc17137e0ba80
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-25 15:43:58 +0000

    goodsync 12.7.7

--- Casks/g/goodsync.rb
+++ Casks/g/goodsync.rb
@@ -1,5 +1,5 @@
 cask "goodsync" do
-  version "12.7.5"
+  version "12.7.7"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.goodsync.com/download/goodsync-vsub-mac.dmg"
