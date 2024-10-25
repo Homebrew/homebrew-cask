a248b56b9fede496e40c68a18446395e969e2eb8
Bevan Kay <email@bevankay.me>
2024-10-26 10:52:44 +1100

    dockside 1.5.5 (new cask)

---
+++ Casks/d/dockside.rb
@@ -0,0 +1,22 @@
+cask "dockside" do
+  version "1.5.5"
+  sha256 "612a7cedc9f2a8eb79e4ad9c295f28199ae2617435a9acf6d35264265f08999d"
+
+  url "https://github.com/PrajwalSD/Dockside/releases/download/v#{version}/Dockside.dmg",
+      verified: "github.com/PrajwalSD/Dockside/"
+  name "Dockside"
+  desc "Dock utility"
+  homepage "https://hachipoo.com/dockside-app"
+
+  livecheck do
+    url "https://prajwalsd.github.io/Dockside/releases/appcast.xml"
+    strategy :sparkle
+  end
+
+  auto_updates true
+  depends_on macos: ">= :ventura"
+
+  app "Dockside.app"
+
+  zap trash: "~/Library/Preferences/com.hachipoo.Dockside.plist"
+end
