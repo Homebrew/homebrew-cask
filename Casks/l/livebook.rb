9d6e65bc2a59697ceabe44aedd9cc7cf4b4aa880
Klaus Hipp <khipp@users.noreply.github.com>
2024-10-25 18:42:21 +0200

    livebook 0.14.5

--- Casks/l/livebook.rb
+++ Casks/l/livebook.rb
@@ -1,8 +1,8 @@
 cask "livebook" do
-  version "0.14.4"
-  sha256 "9735680b5da4f2cbb672ac0c72adab58c283956649e65cb3ce9c5fd3c3ab5057"
+  version "0.14.5"
+  sha256 "e82926e11638775812d4c2ae5ee55396f20c485fb8e5c29a6d60ab43b8850a25"
 
-  url "https://github.com/livebook-dev/livebook/releases/download/v#{version}/LivebookInstall-v#{version}-macos-universal.dmg",
+  url "https://github.com/livebook-dev/livebook/releases/download/v#{version}/LivebookInstall-macos-universal.dmg",
       verified: "github.com/livebook-dev/livebook/"
   name "Livebook"
   desc "Code notebooks for Elixir developers"
