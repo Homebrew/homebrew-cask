47579a8b222609fb921e88f3bc62eacb21acc2b4
Klaus Hipp <khipp@users.noreply.github.com>
2024-10-26 13:50:09 +0200

    prusaslicer: add livecheck

--- Casks/p/prusaslicer.rb
+++ Casks/p/prusaslicer.rb
@@ -1,16 +1,24 @@
 cask "prusaslicer" do
-  version "2.8.1,202409181403"
+  version "2.8.1"
   sha256 "8a5e482a67ed674da66b26c84e93f3268e736b07e3ab7df4616c440074597cee"
 
-  url "https://github.com/prusa3d/PrusaSlicer/releases/download/version_#{version.csv.first}/PrusaSlicer-#{version.csv.first}+MacOS-universal-#{version.csv.second}.dmg",
-      verified: "github.com/prusa3d/PrusaSlicer/"
+  url "https://cdn.prusa3d.com/downloads/drivers/prusa3d_mac_#{version.dots_to_underscores}.dmg"
   name "PrusaSlicer"
   desc "G-code generator for 3D printers (RepRap, Makerbot, Ultimaker etc.)"
   homepage "https://www.prusa3d.com/slic3r-prusa-edition/"
 
   livecheck do
-    skip "No reliable way to get version info"
+    url "https://cache.prusa3d.com/help/api/v1/prusa3d_downloads"
+    strategy :json do |json|
+      json["data"]&.map do |item|
+        next if item.dig("meta", "type", "value") != "driver"
+
+        item["title"]
+      end
+    end
   end
+
+  depends_on macos: ">= :sierra"
 
   app "Original Prusa Drivers/PrusaSlicer.app"
 
