a9be37105fee03383075becb32dc26a9af417761
Klaus Hipp <khipp@users.noreply.github.com>
2024-10-26 13:27:10 +0200

    openinterminal-lite: add livecheck

--- Casks/o/openinterminal-lite.rb
+++ Casks/o/openinterminal-lite.rb
@@ -7,8 +7,24 @@ cask "openinterminal-lite" do
   desc "Finder Toolbar app to open the current directory in Terminal"
   homepage "https://github.com/Ji4n1ng/OpenInTerminal"
 
+  # Not every GitHub release provides a `openinterminal-lite` file, so we check multiple
+  # recent releases instead of only the "latest" release.
   livecheck do
-    skip "No reliable way to get version info"
+    url :url
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    strategy :github_releases do |json, regex|
+      file_regex = /^OpenInTerminal[._-]Lite\.zip$/i
+
+      json.map do |release|
+        next if release["draft"] || release["prerelease"]
+        next unless release["assets"]&.any? { |asset| asset["name"]&.match?(file_regex) }
+
+        match = release["tag_name"].match(regex)
+        next if match.blank?
+
+        match[1]
+      end
+    end
   end
 
   app "OpenInTerminal-Lite.app"
