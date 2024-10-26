3014a4d36f3f9d81a26781bc9560cb6dc524a8c4
Klaus Hipp <khipp@users.noreply.github.com>
2024-10-26 21:10:41 +0200

    dymo-label: add livecheck

--- Casks/d/dymo-label.rb
+++ Casks/d/dymo-label.rb
@@ -7,8 +7,11 @@ cask "dymo-label" do
   desc "Software for DYMO LabelWriters"
   homepage "https://www.dymo.com/support?cfid=online-support"
 
+  # This can return a page with a CAPTCHA instead of the expected content
+  # (e.g. when the check is run in the homebrew/cask CI environment).
   livecheck do
-    skip "No version information available"
+    url :homepage
+    regex(/href=.*?DCDMacv?(\d+(?:\.\d+)+)\.pkg/i)
   end
 
   pkg "DCDMac#{version}.pkg"
