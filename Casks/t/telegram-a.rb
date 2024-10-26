57ee37c7c18194c7d595aafbf41cebaf2906dfff
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-26 09:55:43 +0000

    telegram-a 10.9.19

--- Casks/t/telegram-a.rb
+++ Casks/t/telegram-a.rb
@@ -1,9 +1,9 @@
 cask "telegram-a" do
   arch arm: "arm64", intel: "x64"
 
-  version "10.9.18"
-  sha256 arm:   "b14b599dd05b407edb50a36487b39f50572c3bed30f79721c14d707bd705a1ab",
-         intel: "56dde3c2e1aed75740c34477f5798bfc91be61fd4bd98a6927cc5072b9fb2232"
+  version "10.9.19"
+  sha256 arm:   "ef33a112d23dd99c35b2c84452b49f876f68d74c0b6fe610e467914f970e89af",
+         intel: "5e852393849ae6cc6af03c17a0af1206bfb8e9787e59e40e09a9cc3c48c80ec1"
 
   url "https://github.com/Ajaxy/telegram-tt/releases/download/v#{version}/Telegram-A-#{arch}.dmg",
       verified: "github.com/Ajaxy/telegram-tt/"
