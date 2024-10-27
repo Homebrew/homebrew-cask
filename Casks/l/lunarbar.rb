e400556ae1f40226b2166461f334fc5168413916
Daeho Ro <lamanus@outlook.kr>
2024-10-27 17:30:41 +0900

    lunarbar: update livecheck

--- Casks/l/lunarbar.rb
+++ Casks/l/lunarbar.rb
@@ -7,6 +7,11 @@ cask "lunarbar" do
   desc "Lunar calendar for menu bar"
   homepage "https://github.com/LunarBar-app/LunarBar"
 
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
   depends_on macos: ">= :ventura"
 
   app "LunarBar.app"
