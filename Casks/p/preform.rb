f8adce2409aec43515f007083a262c3620247783
Daeho Ro <lamanus@outlook.kr>
2024-10-27 16:13:16 +0900

    preform 3.41.0 and livecheck

--- Casks/p/preform.rb
+++ Casks/p/preform.rb
@@ -1,6 +1,6 @@
 cask "preform" do
-  version "3.40.3,420_78545"
-  sha256 "cfca95a8a0e8be35e6f2cc982473ee08b2cacc52202dad86d354f763d3519941"
+  version "3.41.0,427_79488"
+  sha256 "b6b23bcb36027228957f51f9dc9a3f9960c0d8ceea7bae970bbbae20ff02f300"
 
   url "https://downloads.formlabs.com/PreForm/Release/#{version.csv.first}/PreForm_mac_#{version.csv.first}_release_releaser_#{version.csv.second}.dmg"
   name "PreForm"
@@ -10,11 +10,8 @@ cask "preform" do
   livecheck do
     url "https://formlabs.com/download-preform-mac/"
     regex(%r{/PreForm[._-]mac[._-]v?(\d+(?:\.\d+)+)[._-]release[._-]releaser[._-](\d+(?:[._-]\d+)+)\.dmg}i)
-    strategy :header_match do |headers, regex|
-      match = headers["location"].match(regex)
-      next if match.blank?
-
-      "#{match[1]},#{match[2]}"
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
     end
   end
 
