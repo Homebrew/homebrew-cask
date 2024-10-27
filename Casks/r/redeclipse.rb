3ca52e5a964456105e9fd48840feee9fc6edabe8
Klaus Hipp <khipp@users.noreply.github.com>
2024-10-27 11:56:16 +0100

    redeclipse: add rosetta caveat

--- Casks/r/redeclipse.rb
+++ Casks/r/redeclipse.rb
@@ -5,7 +5,12 @@ cask "redeclipse" do
   url "https://github.com/redeclipse/base/releases/download/v#{version}/redeclipse_#{version}_mac.tar.bz2",
       verified: "github.com/redeclipse/base/"
   name "Red Eclipse"
+  desc "Multiplayer & singleplayer first person shooter"
   homepage "https://www.redeclipse.net/"
 
   app "redeclipse.app"
+
+  caveats do
+    requires_rosetta
+  end
 end
