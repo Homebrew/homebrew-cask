981a14ff2fef06c088e217b89efddf2d92701ae0
Daeho Ro <lamanus@outlook.kr>
2024-10-27 17:23:36 +0900

    dosbox-staging 0.82.0

--- Casks/d/dosbox-staging.rb
+++ Casks/d/dosbox-staging.rb
@@ -1,6 +1,6 @@
 cask "dosbox-staging" do
-  version "0.81.2"
-  sha256 "718968485a0418ead3434285e3a6ab4853914d29d38e14289711af1b2152368e"
+  version "0.82.0"
+  sha256 "ff77f0647d2431f010c6d9dcc29bcf3ab1177cb40fef1009d9bafd1475180b5b"
 
   url "https://github.com/dosbox-staging/dosbox-staging/releases/download/v#{version}/dosbox-staging-macOS-v#{version}.dmg"
   name "DOSBox Staging"
