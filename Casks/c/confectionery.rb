dfe959216071ff84fb658fe85b4d0195e20bfc06
Daeho Ro <lamanus@outlook.kr>
2024-10-27 21:26:01 +0900

    confectionery: update livecheck

--- Casks/c/confectionery.rb
+++ Casks/c/confectionery.rb
@@ -1,15 +1,18 @@
 cask "confectionery" do
   version "1.2.1"
-  sha256 :no_check
+  sha256 "8f8d8e666a9809ddb171abe68b1d341327a13e4d649873648f129fdebd93d043"
 
-  url "https://confectioneryapp.com/download"
+  url "https://app-updates.vadimdemedes.com/confectionery/download/Confectionery-#{version}-mac.dmg",
+      verified: "app-updates.vadimdemedes.com/confectionery/download/"
   name "Confectionery"
   desc "Website screenshot tool"
   homepage "https://confectioneryapp.com/"
 
   livecheck do
-    url :url
-    strategy :header_match
+    url "https://app-updates.vadimdemedes.com/confectionery/update/darwin/0.0.0"
+    strategy :json do |json|
+      json["name"]&.tr("v", "")
+    end
   end
 
   app "Confectionery.app"
