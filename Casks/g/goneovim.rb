00154541fb8e4c3b84b80beabb1d17d2b8a539c7
Daeho Ro <lamanus@outlook.kr>
2024-10-27 17:28:34 +0900

    goneovim 0.6.10

--- Casks/g/goneovim.rb
+++ Casks/g/goneovim.rb
@@ -1,9 +1,9 @@
 cask "goneovim" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.6.9"
-  sha256 arm:   "92f92062e13908c9d04e69a82297569ad91e4930a001af87be9308ccd5afcb08",
-         intel: "27f65edc83ff8a8770877e76f5d4d11c8849a4aef461a421e4b2029bed9322f4"
+  version "0.6.10"
+  sha256 arm:   "1c83354f9a220dc2447cab9cdd9c156fb5d23981d78b44efc7ca2455ee2bc108",
+         intel: "0d63f9717738e3ef7a2efa1d6097c1ed0c0b8b67408f7f173e09494594fa59ed"
 
   url "https://github.com/akiyosi/goneovim/releases/download/v#{version}/Goneovim-v#{version}-macos-#{arch}.tar.bz2"
   name "Goneovim"
