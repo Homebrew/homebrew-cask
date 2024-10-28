bcb95ef811d43433d4a5bd31a9106a8b9bbb46cd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-28 09:51:21 +0000

    finalshell 4.5.11

--- Casks/f/finalshell.rb
+++ Casks/f/finalshell.rb
@@ -1,7 +1,7 @@
 cask "finalshell" do
   arch arm: "arm64", intel: "x64"
 
-  version "4.5.10"
+  version "4.5.11"
   sha256 :no_check
 
   url "https://dl.hostbuf.com/finalshell3/finalshell_macos_#{arch}.pkg"
