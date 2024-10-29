58814412ecfeb5480f754fa3b574acaa69291da5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-29 10:05:06 +0000

    sipgate-clinq 2.5.4

--- Casks/s/sipgate-clinq.rb
+++ Casks/s/sipgate-clinq.rb
@@ -1,6 +1,6 @@
 cask "sipgate-clinq" do
-  version "2.5.3"
-  sha256 "ea49e8c42a0074538e7843df2b8c38a063ddb17ff7697cd39b176323c8f22515"
+  version "2.5.4"
+  sha256 "c5f073220234ed218ad0d4649a215f486d452634bd4d6eeb30c22740972fa55c"
 
   url "https://s3-eu-central-1.amazonaws.com/desktop.download.sipgate.com/sipgate%20CLINQ-#{version}.zip",
       verified: "s3-eu-central-1.amazonaws.com/desktop.download.sipgate.com/"
