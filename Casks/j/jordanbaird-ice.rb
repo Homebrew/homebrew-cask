6fb1742c4f53a50093454d0c11bb1e611b640b62
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-29 21:50:48 +0000

    jordanbaird-ice 0.11.12

--- Casks/j/jordanbaird-ice.rb
+++ Casks/j/jordanbaird-ice.rb
@@ -1,6 +1,6 @@
 cask "jordanbaird-ice" do
-  version "0.11.11"
-  sha256 "21ea5ffa614def29bb699bb7c6725526840d0d999216fb75e504c6757b6f49cb"
+  version "0.11.12"
+  sha256 "d770e81597566dd2d2363feb350f808c7a92e363df95c51e48140eb30e452cc9"
 
   url "https://github.com/jordanbaird/Ice/releases/download/#{version}/Ice.zip",
       verified: "github.com/jordanbaird/Ice/"
