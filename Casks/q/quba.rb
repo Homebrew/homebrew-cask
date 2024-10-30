5b6379e54d1da97c88340c84d57a4bdfa10d2e18
ellie is not coding in the slightest <166862894+eleanordoesntcode@users.noreply.github.com>
2024-10-30 19:29:35 +0100

    quba 1.4.1

--- Casks/q/quba.rb
+++ Casks/q/quba.rb
@@ -1,8 +1,8 @@
 cask "quba" do
-  version "1.4.0"
+  version "1.4.1"
   sha256 "4a30c194d59fc850650bd007a717745d53dd060e26792642f47e5368f373f1bb"
 
-  url "https://quba-viewer.org/files/Quba-#{version}-arm64.dmg"
+  url "https://github.com/ZUGFeRD/quba-viewer/releases/download/v#{version}/Quba-#{version}-universal.dmg"
   name "Quba-Viewer"
   desc "Viewer for electronic invoices"
   homepage "https://quba-viewer.org/"
