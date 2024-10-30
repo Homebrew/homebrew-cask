27ed1e29b0bef8e8c557e8a50deb6f18408a1259
Klaus Hipp <khipp@users.noreply.github.com>
2024-10-30 18:50:38 +0100

    google-chrome@beta 131.0.6778.24

--- Casks/g/google-chrome@beta.rb
+++ Casks/g/google-chrome@beta.rb
@@ -1,5 +1,5 @@
 cask "google-chrome@beta" do
-  version "131.0.6778.13"
+  version "131.0.6778.24"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/beta/googlechromebeta.dmg"
