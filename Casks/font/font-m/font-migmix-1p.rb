b4d2ef915e958e6844c2e511173b55f77336e5c9
Klaus Hipp <khipp@users.noreply.github.com>
2024-10-31 19:04:34 +0100

    font-migmix-1p: update livecheck
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/font/font-m/font-migmix-1p.rb
+++ Casks/font/font-m/font-migmix-1p.rb
@@ -9,10 +9,7 @@ cask "font-migmix-1p" do
 
   livecheck do
     url :homepage
-    strategy :page_match do |page|
-      page.scan(/href=.*migmix-1p[._-]v?(\d+(?:\.\d+)*)\.zip"/i)
-          .map { |match| match[0].insert(4, ".") }
-    end
+    regex(%r{href=.*?/download/v?(\d+(?:\.\d+)+)/migmix-1p[._-]}i)
   end
 
   font "migmix-1p-#{version.no_dots}/migmix-1p-bold.ttf"
