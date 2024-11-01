8fc6737e1d6d2dc9c1e09f03398d38a3dd0399ba
Klaus Hipp <khipp@users.noreply.github.com>
2024-11-01 15:48:02 +0100

    rider: update livecheck

--- Casks/r/rider.rb
+++ Casks/r/rider.rb
@@ -13,8 +13,12 @@ cask "rider" do
   livecheck do
     url "https://data.services.jetbrains.com/products/releases?code=RD&latest=true&type=release"
     strategy :json do |json|
-      json["RD"].map do |release|
-        "#{release["version"]},#{release["build"]}"
+      json["RD"]&.map do |release|
+        version = release["version"]
+        build = release["build"]
+        next if version.blank? || build.blank?
+
+        "#{version},#{build}"
       end
     end
   end
