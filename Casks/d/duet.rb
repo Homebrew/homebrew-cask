7ccad982ce2a472c564d929da6c8a4a2afba745b
Daeho Ro <lamanus@outlook.kr>
2024-11-01 19:40:08 +0900

    duet: update livecheck

--- Casks/d/duet.rb
+++ Casks/d/duet.rb
@@ -12,14 +12,14 @@ cask "duet" do
   end
   on_big_sur :or_newer do
     version "3.20.0.0"
-    sha256 "a017119b75dc19c01ed60c9b7527baf0db355fd5a466a9b22ade07288aab4033"
+    sha256 "ed344e691d12d77037d9919b34aaa3aa7ce5fd3fc9c1f9f0b6327f2be67a0720"
 
-    url "https://duetdownload.com/Mac/#{version.major}_x/duet-dd-#{version.dots_to_hyphens}.zip",
+    url "https://duetdownload.com/Mac/#{version.major}_x/duet-dd-#{version.dots_to_hyphens}.dmg",
         verified: "duetdownload.com/Mac/"
 
     livecheck do
       url "https://updates.duetdisplay.com/AppleSilicon"
-      regex(/duet[._-]dd[._-]v?(\d+(?:-\d+)+)\.zip/i)
+      regex(/duet[._-]dd[._-]v?(\d+(?:-\d+)+)\.dmg/i)
       strategy :header_match do |headers, regex|
         headers["location"].scan(regex).map do |match|
           match[0].tr("-", ".")
