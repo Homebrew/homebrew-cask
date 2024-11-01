60871bc5158f976a785f8f296832bcf9879b6187
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-10-31 21:56:21 -0400

    regex fix
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/c/claude.rb
+++ Casks/c/claude.rb
@@ -10,7 +10,7 @@ cask "claude" do
 
   livecheck do
     url "https://storage.googleapis.com/osprey-downloads-c02f6a0d-347c-492b-a752-3e0651722e97/nest/update_manifest.json"
-    regex(/release[-_.](\d+(?:\.\d+)+)[-_.]artifact[-_.](\h+)\.zip$/i)
+    regex(/release[._-]v?(\d+(?:\.\d+)+)[._-]artifact[._-](\h+)\.zip/i)
     strategy :json do |json, regex|
       json["releases"]&.map do |item|
         match = item.dig("updateTo", "url")&.match(regex)
