15e94c7992867439b49958db48349bc9c7286f78
Qi Zhang <25192197+singularitti@users.noreply.github.com>
2024-10-31 19:05:22 -0600

    Update blip to v1.0.16,20241030101237

--- Casks/b/blip.rb
+++ Casks/b/blip.rb
@@ -1,6 +1,6 @@
 cask "blip" do
-  version "1.0.15,20241022113355"
-  sha256 "c56d70229736523641671733983cf4be27502aeb31744991fcf0290e123e2308"
+  version "1.0.16,20241030101237"
+  sha256 "dd7c7ad02720b26d182b05d2d47f269e472cc328f8776fa257504b327164678b"
 
   url "https://f000.backblazeb2.com/file/push-mac/Blip-#{version.csv.second}.zip",
       verified: "f000.backblazeb2.com/file/push-mac/"
