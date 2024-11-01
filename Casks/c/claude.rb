f906e59a4778d76530050cd2d8943ede7ec00d87
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-10-31 21:39:45 -0400

    claude 0.4.8 (new cask)
    Signed-off-by: Justin Krehel <39449589+krehel@users.noreply.github.com>

--- Casks/c/claude.rb
+++ Casks/c/claude.rb
@@ -1,17 +1,23 @@
 cask "claude" do
-  version "0.4.8"
+  version "0.4.8,abfc4944184bdf344113a54b0fcaddfc8040176b"
   sha256 "6e3cc0dfdb145128790855aee8ed08b76a32e0f01827fddf86a0f27836c4da73"
 
-  url "https://storage.googleapis.com/osprey-downloads-c02f6a0d-347c-492b-a752-3e0651722e97/nest/release-#{version}-artifact-abfc4944184bdf344113a54b0fcaddfc8040176b.zip",
+  url "https://storage.googleapis.com/osprey-downloads-c02f6a0d-347c-492b-a752-3e0651722e97/nest/release-#{version.csv.first}-artifact-#{version.csv.second}.zip",
       verified: "storage.googleapis.com/osprey-downloads-c02f6a0d-347c-492b-a752-3e0651722e97/nest/"
-  name "claude"
-  desc "AI partner on desktop"
-  homepage "https://claude.ai/"
+  name "Claude"
+  desc "Anthropic's official Claude AI desktop app"
+  homepage "https://claude.ai/download"
 
   livecheck do
     url "https://storage.googleapis.com/osprey-downloads-c02f6a0d-347c-492b-a752-3e0651722e97/nest/update_manifest.json"
-    strategy :json do |json|
-      json["currentRelease"]
+    regex(/release[-_.](\d+(?:\.\d+)+)[-_.]artifact[-_.](\h+)\.zip$/i)
+    strategy :json do |json, regex|
+      json["releases"]&.map do |item|
+        match = item.dig("updateTo", "url")&.match(regex)
+        next if match.blank?
+
+        "#{match[1]},#{match[2]}"
+      end
     end
   end
 
