07fbed7d3f21b14540b9dc3881f893b0ed7979af
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-02 15:44:19 +0000

    font-iosevka-etoile 32.0.0

--- Casks/font/font-i/font-iosevka-etoile.rb
+++ Casks/font/font-i/font-iosevka-etoile.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-etoile" do
-  version "31.9.1"
-  sha256 "fdf7f4ee2499b53ecde53927ca4c34a9d0aa72823165dba54a1283df1ffa2609"
+  version "32.0.0"
+  sha256 "30e16569a53fac8df32339901ac8644e6f2c92806c8ed8dd94f2c9fd5f19b4cc"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/SuperTTC-IosevkaEtoile-#{version}.zip"
   name "Iosevka Etoile"
