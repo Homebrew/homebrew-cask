2afa8d38c6bdbc5cebcfd961016e1b91a4d93b1a
Klaus Hipp <khipp@users.noreply.github.com>
2024-11-02 07:26:18 +0100

    get-lyrical: deprecate

--- Casks/g/get-lyrical.rb
+++ Casks/g/get-lyrical.rb
@@ -7,10 +7,7 @@ cask "get-lyrical" do
   desc "Automatically add lyrics to songs in iTunes"
   homepage "https://shullian.com/get_lyrical.php"
 
-  livecheck do
-    url :homepage
-    regex(/version\s+(\d+(?:\.\d+)+)/i)
-  end
+  deprecate! date: "2024-11-01", because: :unmaintained
 
   app "Get Lyrical/Get Lyrical.app"
 
