85927f9de95906526358e7716846df2d3054e896
i0ntempest <i0ntempest@i0ntempest.com>
2024-11-01 21:27:47 -0400

    caido 0.43.1

--- Casks/c/caido.rb
+++ Casks/c/caido.rb
@@ -1,9 +1,9 @@
 cask "caido" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.43.0"
-  sha256 arm:   "387e6f0d68aeda629f4348c74f57ee06e9d5be3b041f524113b53c65878d29bc",
-         intel: "b7fd61d70a777273982fa13105a39cef5f20ae06cd98b44f83c8bd924350bdc6"
+  version "0.43.1"
+  sha256 arm:   "73d8ac814997696994afedc2abbb6b7270453e116f9645b2ff5f378601d9dab0",
+         intel: "def7984932e62ca987ca37b6919aa0177fa0b347d924a6d75b0055826c8027d9"
 
   url "https://caido.download/releases/v#{version}/caido-desktop-v#{version}-mac-#{arch}.dmg",
       verified: "caido.download/"
