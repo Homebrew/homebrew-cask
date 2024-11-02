3291c4ecaa9a2e88d8df29206a80760386bb1586
Klaus Hipp <khipp@users.noreply.github.com>
2024-11-02 05:37:34 +0100

    droplr: update livecheck

--- Casks/d/droplr.rb
+++ Casks/d/droplr.rb
@@ -8,13 +8,8 @@ cask "droplr" do
   homepage "https://droplr.com/"
 
   livecheck do
-    url "https://files.droplr.com/apps/mac-current"
-    regex(%r{/Droplr(\d)(\d)(\d+)-(\d+)\.zip}i)
-    strategy :header_match do |headers, regex|
-      headers["location"].scan(regex).map do |match|
-        "#{match[0]}.#{match[1]}.#{match[2]},#{match[3]}"
-      end
-    end
+    url "https://files.droplr.com/apps/appcast/appcast-sandbox-v2.xml"
+    strategy :sparkle
   end
 
   auto_updates true
@@ -23,4 +18,9 @@ cask "droplr" do
   pkg "Droplr#{version.csv.first.no_dots}-#{version.csv.second}.pkg"
 
   uninstall pkgutil: "com.droplr.droplr-mac"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.droplr.droplr-mac",
+    "~/Library/Containers/com.droplr.droplr-mac",
+  ]
 end
