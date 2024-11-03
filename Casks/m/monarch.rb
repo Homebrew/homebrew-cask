fdb5a188b1bd55d31319ba798233d2f68fe8ef82
Bevan Kay <email@bevankay.me>
2024-11-03 22:15:32 +1100

    monarch: update livecheck, url

--- Casks/m/monarch.rb
+++ Casks/m/monarch.rb
@@ -1,24 +1,24 @@
 cask "monarch" do
   arch arm: "arm64", intel: "x64"
 
-  sha256 arm:   "f6b5ff907f9533a57cd3e59cb2e29f90ee5d620ce7b02009df634800ebfcb128",
+  sha256 arm:   "f5fe4bf861711b9911cc0ef2875f8e067d06d58acec2b9552351725fdd3a9467",
          intel: "637d3385a5efd3778205286c0297515806525c1458586c5e5e40fa6cb86a79ad"
 
   on_arm do
-    version "0.7.8"
+    version "0.7.9"
   end
   on_intel do
     version "0.7.7"
   end
 
-  url "https://monarchlauncher.s3.amazonaws.com/#{arch}/monarch-#{version}.dmg",
-      verified: "monarchlauncher.s3.amazonaws.com/"
+  url "https://storage.googleapis.com/monarchlauncher/#{arch}/monarch-#{version}.dmg",
+      verified: "storage.googleapis.com/monarchlauncher/"
   name "Monarch"
   desc "Spotlight Search"
   homepage "https://monarchlauncher.com/"
 
   livecheck do
-    url "https://monarchlauncher.s3.amazonaws.com/#{arch}/latest-mac.yml"
+    url "https://storage.googleapis.com/monarchlauncher/#{arch}/latest-mac.yml"
     strategy :electron_builder
   end
 
