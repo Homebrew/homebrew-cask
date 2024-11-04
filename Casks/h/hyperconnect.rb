cbfdd2a6e8688c680cd50ba8ddad296d1133e58b
warmepoch <warmepoch@icloud.com>
2024-11-04 20:00:47 +0800

    hyperconnect 1.0.3 (new cask)
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

---
+++ Casks/h/hyperconnect.rb
@@ -0,0 +1,35 @@
+cask "hyperconnect" do
+  version "1.0.3"
+  sha256 "c5ccba949f29f1fda54f9ebcf0f16a7c79e71a28732a50f4894406ef3a07727d"
+
+  url "https://cdn.cnbj1.fds.api.mi-img.com/mijia-ios-adhoc/hyperconnect/HyperConnect-#{version}.dmg",
+      verified: "mi-img.com/mijia-ios-adhoc/hyperconnect/"
+  name "HyperConnect"
+  name "小米互联服务"
+  desc "Cross-device interconnection service for the Xiaomi ecosystem"
+  homepage "https://hyperos.mi.com/continuity"
+
+  livecheck do
+    url "https://cn-device.interconnect.miui.com/package/mac/checkUpdate?version=0.0.0"
+    strategy :json do |json|
+      json.dig("data", "versionInfo", "version")
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :monterey"
+
+  app "小米互联服务.app"
+
+  uninstall quit: "com.xiaomi.hyperConnect"
+
+  zap trash: [
+    "~/Documents/XiaoMiDevicePropertyCache",
+    "~/Library/Caches/com.xiaomi.hyperConnect",
+    "~/Library/com.xiaomi.hyperConnect",
+    "~/Library/HTTPStorages/com.xiaomi.hyperConnect",
+    "~/Library/OneTrack/com.xiaomi.hyperConnect.data",
+    "~/Library/Preferences/com.xiaomi.hyperConnect.plist",
+    "~/Library/Saved Application State/com.xiaomi.hyperConnect.savedState",
+  ]
+end
