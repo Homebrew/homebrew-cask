fe5d06bd207d78dcba7b0f0878f0f6da399bbc3b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-04 19:32:44 +0000

    ppsspp 1.18.1

--- Casks/p/ppsspp.rb
+++ Casks/p/ppsspp.rb
@@ -1,6 +1,6 @@
 cask "ppsspp" do
-  version "1.18"
-  sha256 "17d940f4bb84e6f998e9e58d6097e834f58680eed6911dd701ea3cf13c372083"
+  version "1.18.1"
+  sha256 "6926796c544464ce28691d099c71861d29967366542a4b37c6e60575ec782b3f"
 
   url "https://github.com/hrydgard/ppsspp/releases/download/v#{version}/PPSSPPSDL-macOS-v#{version}.zip",
       verified: "github.com/hrydgard/ppsspp/"
