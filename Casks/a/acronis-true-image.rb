6b9f8e6ef4383480a4025feee452b1b2d61dd2e2
Valentin Kulesh <vkulesh@mts.ru>
2024-11-04 20:57:57 +0300

    acronis-true-image 41592: implement zap stanza
    Uses `acronis-true-image-cleanup-tool`

--- Casks/a/acronis-true-image.rb
+++ Casks/a/acronis-true-image.rb
@@ -1,16 +1,28 @@
 cask "acronis-true-image" do
-  version "2021"
-  sha256 :no_check # required as upstream package is updated in-place
+  version "41592"
+  sha256 "304335d0e8e26bc46571e6926baf20da0f2d694f20a8728c7f220236d1ed29c2"
 
-  url "https://dl.acronis.com/u/AcronisTrueImage#{version}.dmg"
+  url "https://dl.acronis.com/u/AcronisTrueImage_#{version}.dmg"
   name "Acronis True Image"
   desc "Full image backup and cloning software"
-  homepage "https://www.acronis.com/personal/computer-backup/"
+  homepage "https://www.acronis.com/products/true-image/"
 
-  deprecate! date: "2024-02-07", because: :discontinued
+  livecheck do
+    url "https://www.acronis.com/en-us/support/updates/changes.html?p=42798"
+    regex(%r{<span class="build">Build</span><span class="build-number"> (.+?)</span>})
+  end
 
-  pkg "Install Acronis True Image.pkg"
+  auto_updates true
+  depends_on macos: ">= :big_sur"
+  depends_on cask: "acronis-true-image-cleanup-tool"
 
-  uninstall pkgutil: "com.acronis.trueimageformac",
+  pkg "Installer.pkg"
+
+  uninstall pkgutil: "com.acronis.CyberProtectHomeOffice",
             delete:  "/Applications/Acronis True Image.app"
+
+  zap script: {
+    executable: "#{HOMEBREW_PREFIX}/lib/acronis-true-image/cleanup_tool",
+    sudo:       true,
+  }
 end
