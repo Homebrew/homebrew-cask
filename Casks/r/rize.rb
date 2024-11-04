fe47c619c68d6e1a8d6180a0c6d07e3d7f2ffc60
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-04 19:36:02 +0000

    rize 1.5.1

--- Casks/r/rize.rb
+++ Casks/r/rize.rb
@@ -1,9 +1,9 @@
 cask "rize" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.5.0"
-  sha256  arm:   "ae60f1f17d8ebe74fc6b0906b6dd0505e677c27cdd00a4ff7f65474390f7c124",
-          intel: "7d349a9e83c22b0e72da00a5231c19f9f9674e985a10e104a7a7e981830d8f16"
+  version "1.5.1"
+  sha256  arm:   "3d16d4b3949190c224a261c2e753d6a8d8f732aa10679b6f6cbe3b06090545e0",
+          intel: "74299843b4751f3c5f12b19694698378d973bcbae01e7c0918c23f8b38b21499"
 
   url "https://github.com/rize-io/lua/releases/download/v#{version}/Rize-#{version}-#{arch}.dmg",
       verified: "github.com/rize-io/lua/"
