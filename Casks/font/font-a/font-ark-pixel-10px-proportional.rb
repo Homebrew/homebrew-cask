24f57b4e7a71a04a6e6a942bd61969ce0827575f
TakWolf <takwolf@foxmail.com>
2024-11-04 12:37:56 +0800

    font-ark-pixel-10px-proportional 2024.11.04

--- Casks/font/font-a/font-ark-pixel-10px-proportional.rb
+++ Casks/font/font-a/font-ark-pixel-10px-proportional.rb
@@ -1,6 +1,6 @@
 cask "font-ark-pixel-10px-proportional" do
-  version "2024.05.12"
-  sha256 "b193e7b49fe17ad20df86115dbaf169ab4f49c5b6b38ac2653ea643bd7afd67a"
+  version "2024.11.04"
+  sha256 "bbf67734a9b43c4bd8b6055d7b9416d123b30cceff1f48d1b23eb3a91066e0fa"
 
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-10px-proportional-otf-v#{version}.zip"
   name "Ark Pixel 10px Proportional"
