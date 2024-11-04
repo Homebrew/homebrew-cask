ef8597f777685ae825e5c8c85f50e951ce0fe14a
Daeho Ro <lamanus@outlook.kr>
2024-11-04 21:19:49 +0900

    go-server 24.4.0

--- Casks/g/go-server.rb
+++ Casks/g/go-server.rb
@@ -1,9 +1,9 @@
 cask "go-server" do
   arch arm: "-aarch64"
 
-  version "24.3.0,19261"
-  sha256 arm:   "2be45a716287f65ac23e17681c095faa2414ab8b64eb6144a5f625681836a648",
-         intel: "95c7e3561f8175d80d8517e764f5d4e9dc75796309368a68298b7bfedc4b1716"
+  version "24.4.0,19686"
+  sha256 arm:   "3108ce55784466e7677f01d99621ffaca2e1f99bf36ff83fe7a13161c7ce497c",
+         intel: "8bc3b0381b7e94500c064979afd366b6b3e8306ffdd7caf7b8389e92d9744f50"
 
   url "https://download.gocd.org/binaries/#{version.csv.first}-#{version.csv.second}/osx/go-server-#{version.csv.first}-#{version.csv.second}-osx#{arch}.zip"
   name "Go Server"
