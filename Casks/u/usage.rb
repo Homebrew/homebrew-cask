21dbdaf11bc3cef80bb66aee22c42381aea5ecd9
Klaus Hipp <khipp@users.noreply.github.com>
2024-11-04 11:02:07 +0100

    usage: update livecheck

--- Casks/u/usage.rb
+++ Casks/u/usage.rb
@@ -9,8 +9,9 @@ cask "usage" do
 
   livecheck do
     url "https://www.mediaatelier.com/Usage/feed.php"
-    strategy :sparkle do |item|
-      item.url[/Usage[._-]?(\d+(?:\.\d+)*)\.dmg/i, 1]
+    regex(/Usage[._-]?(\d+(?:\.\d+)*)\.dmg/i)
+    strategy :sparkle do |item, regex|
+      item.url[regex, 1]
     end
   end
 
