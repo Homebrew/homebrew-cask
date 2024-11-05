573b4d4d45497fac4130e8fb7de8acab7b2c0bd7
Klaus Hipp <khipp@users.noreply.github.com>
2024-11-05 10:05:49 +0100

    webkinz 1.7.6

--- Casks/w/webkinz.rb
+++ Casks/w/webkinz.rb
@@ -1,6 +1,6 @@
 cask "webkinz" do
-  version "1.7.5"
-  sha256 "4261f9e83fa058a4c478bdcfb6f7fe7ce0f97831a2085f12659f64c6730db832"
+  version "1.7.6"
+  sha256 "1f8f6f5fb24e9e4681c3ec4354ea35a4b6f3beca846031cc2b17db0fd7509f3d"
 
   url "http://downloads.webkinz.com/apps/wxsetup-#{version}.dmg"
   name "Webkinz Classic"
