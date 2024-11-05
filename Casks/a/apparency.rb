63ffa0f58613e35dab0a1d0d357689f0ddbb249e
Sam Ford <1584702+samford@users.noreply.github.com>
2024-11-05 10:51:46 -0500

    apparency: update livecheck

--- Casks/a/apparency.rb
+++ Casks/a/apparency.rb
@@ -38,11 +38,11 @@ cask "apparency" do
     livecheck do
       url "https://www.mothersruin.com/software/Apparency/data/ApparencyVersionInfo.plist"
       strategy :xml do |xml|
-        short_version = xml.elements["//key[text()='CFBundleShortVersionString']"]&.next_element&.text&.strip
-        version = xml.elements["//key[text()='CFBundleVersion']"]&.next_element&.text&.strip
+        short_version = xml.elements["//key[text()='CFBundleShortVersionString']"]&.next_element&.text
+        version = xml.elements["//key[text()='CFBundleVersion']"]&.next_element&.text
         next if short_version.blank? || version.blank?
 
-        "#{short_version},#{version}"
+        "#{short_version.strip},#{version.strip}"
       end
     end
   end
