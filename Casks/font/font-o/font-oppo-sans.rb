c3c4a2a4e947b129211edb9c7460308f6547d8e0
Aaron-212 <aaron212cn@outlook.com>
2024-11-05 22:27:30 +0800

    OPPO Sans 4.0
    Signed-off-by: Aaron-212 <aaron212cn@outlook.com>

--- Casks/font/font-o/font-oppo-sans.rb
+++ Casks/font/font-o/font-oppo-sans.rb
@@ -2,15 +2,11 @@ cask "font-oppo-sans" do
   version :latest
   sha256 :no_check
 
-  url "https://static01.coloros.com/www/public/img/topic7/font-opposans.zip"
+  url "https://openfs.oppomobile.com/open/oop/202410/18/62d51f494591f1a9040d83b597745911.zip"
   name "OPPO Sans"
-  homepage "https://www.coloros.com/index/newsDetail?id=72"
+  homepage "https://open.oppomobile.com/new/developmentDoc/info?id=13223"
 
-  font "3.0 Designer_分级/OPlusSans3-ExtraLight.ttf"
-  font "3.0 Designer_分级/OPlusSans3-Light.ttf"
-  font "3.0 Designer_分级/OPlusSans3-Regular.ttf"
-  font "3.0 Designer_分级/OPlusSans3-Medium.ttf"
-  font "3.0 Designer_分级/OPlusSans3-Bold.ttf"
+  font "OPPO_Sans_4.0/OPPO Sans 4.0.ttf"
 
   # No zap stanza required
 end
