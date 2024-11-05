25354400e3dde22d4d0f3803f68045ae2906f0fb
Sam Ford <1584702+samford@users.noreply.github.com>
2024-11-05 10:51:51 -0500

    suspicious-package: update livecheck

--- Casks/s/suspicious-package.rb
+++ Casks/s/suspicious-package.rb
@@ -58,11 +58,11 @@ cask "suspicious-package" do
     livecheck do
       url "https://www.mothersruin.com/software/SuspiciousPackage/data/SuspiciousPackageVersionInfo.plist"
       strategy :xml do |xml|
-        short_version = xml.elements["//key[text()='CFBundleShortVersionString']"]&.next_element&.text&.strip
-        version = xml.elements["//key[text()='CFBundleVersion']"]&.next_element&.text&.strip
+        short_version = xml.elements["//key[text()='CFBundleShortVersionString']"]&.next_element&.text
+        version = xml.elements["//key[text()='CFBundleVersion']"]&.next_element&.text
         next if short_version.blank? || version.blank?
 
-        "#{short_version},#{version}"
+        "#{short_version.strip},#{version.strip}"
       end
     end
   end
