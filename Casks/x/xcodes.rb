36c9bfd012a89bf2e306f45aff125fd7ea714dd3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-05 04:04:47 +0000

    xcodes 2.4.1b30

--- Casks/x/xcodes.rb
+++ Casks/x/xcodes.rb
@@ -1,6 +1,6 @@
 cask "xcodes" do
-  version "2.4.0b29"
-  sha256 "bf79359087cb3cbe89d7ef8c361c1a79ffb9f1694694f51ee24f7c0860cb9523"
+  version "2.4.1b30"
+  sha256 "a4cc2059728c1ba4420b010b7092f0f752393fdba419cedfb8dd3f688ee5e81a"
 
   url "https://github.com/XcodesOrg/XcodesApp/releases/download/v#{version}/Xcodes.zip"
   name "Xcodes"
