e0d4d3d221b50df12e594697bb3e12306c30a97b
Sam Ford <1584702+samford@users.noreply.github.com>
2024-11-05 10:51:48 -0500

    easyfind: update livecheck

--- Casks/e/easyfind.rb
+++ Casks/e/easyfind.rb
@@ -10,7 +10,10 @@ cask "easyfind" do
   livecheck do
     url "https://api.devontechnologies.com/1/apps/updates.plist.php?product=EasyFind&version=#{version}"
     strategy :xml do |xml|
-      xml.elements["//key[text()='EasyFind']"]&.next_element&.text&.strip
+      version = xml.elements["//key[text()='EasyFind']"]&.next_element&.text
+      next if version.blank?
+
+      version.strip
     end
   end
 
