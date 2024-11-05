9b77500ff78bf1c84e84e27c9273f70939043db3
Daeho Ro <lamanus@outlook.kr>
2024-11-06 08:51:33 +0900

    webex 44.11.0.31172

--- Casks/w/webex.rb
+++ Casks/w/webex.rb
@@ -1,7 +1,7 @@
 cask "webex" do
   arch arm: "apple-silicon", intel: "intel"
 
-  version "44.10.1.31028"
+  version "44.11.0.31172"
   sha256 :no_check
 
   url "https://binaries.webex.com/webex-macos-#{arch}/Webex.dmg"
