e053a4493f6e71f5d1d9c7bda08bdfda0600f823
Sam Ford <1584702+samford@users.noreply.github.com>
2024-11-05 19:16:29 -0500

    tablecruncher 1.7.0
    This updates `tablecruncher` to the latest stable version, 1.7.0.
    Upstream assets are now split between arm64 and x86_64, so this
    updates the cask setup accordingly. Besides that, this also tweaks the
    `livecheck` block regex to bring it in line with prevailing standards.

--- Casks/t/tablecruncher.rb
+++ Casks/t/tablecruncher.rb
@@ -1,15 +1,18 @@
 cask "tablecruncher" do
-  version "1.6.0.1"
-  sha256 "897f9fabcabdd902343dad66e67ea2c68b911cf44d383e32adb94ea2908c87de"
+  arch arm: "arm64", intel: "x86_64"
 
-  url "https://tablecruncher.com/download/Tablecruncher-#{version}.zip"
+  version "1.7.0"
+  sha256 arm:   "11dfed89498a9f30ce323ce16159aa9a24b4665634f4bea293682f40eb07cdab",
+         intel: "316ae70fd1b34f9ab59c1bfcde7b0ae3c1a6000c3b0a433ca3b7963d28b1a080"
+
+  url "https://tablecruncher.com/download/Tablecruncher-#{version}-#{arch}.zip"
   name "Tablecruncher"
   desc "Lightweight CSV editor"
   homepage "https://tablecruncher.com/"
 
   livecheck do
     url "https://tablecruncher.com/download/"
-    regex(/Tablecruncher-(\d+(?:\.\d+)+)\.zip/i)
+    regex(/href=.*?Tablecruncher[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}\.zip/i)
   end
 
   app "Tablecruncher.app"
