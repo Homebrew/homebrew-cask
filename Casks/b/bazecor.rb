62baa10106759369de33a6a4c748a994857d26dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-06 15:38:00 +0000

    bazecor 1.5.3

--- Casks/b/bazecor.rb
+++ Casks/b/bazecor.rb
@@ -1,9 +1,9 @@
 cask "bazecor" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.5.2"
-  sha256 arm:   "12d5da03a19ad3107d3fe425cd58e135327ad3115cd924a38471a062318d7e61",
-         intel: "0be8e3c052617a58db306c40b57df99ce8d84ee46bdf0b46362cef79a247b9b6"
+  version "1.5.3"
+  sha256 arm:   "4e4d05e6bdbeb95f6feb5440c508b67ec5236ab3833268601b72afa0efafc8b0",
+         intel: "dd674690345bcee49bad171bea676435a6d769b9db0d95e88a447a879ca030bf"
 
   url "https://github.com/Dygmalab/Bazecor/releases/download/v#{version}/Bazecor-#{version}-#{arch}.dmg",
       verified: "github.com/Dygmalab/Bazecor/"
