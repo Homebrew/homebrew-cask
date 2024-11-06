6db1f0e45ca2090de9a11649fc34c39dea71c33c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-06 10:08:54 +0000

    vscodium@insiders 1.96.0.24311-insider

--- Casks/v/vscodium@insiders.rb
+++ Casks/v/vscodium@insiders.rb
@@ -1,9 +1,9 @@
 cask "vscodium@insiders" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.96.0.24310-insider"
-  sha256 arm:   "f6d46402a6126f698c46f5628bc994f721493a2276bc1ae06880868a2063d98a",
-         intel: "1f5e1277bb1a77ba3c076dd0e51a5fea12676357c7c7765a46803637a41bb8a1"
+  version "1.96.0.24311-insider"
+  sha256 arm:   "e7f999199530e8ea4c6d9eaf98d296fa856eba69c278647fbd1142e6a6f68d81",
+         intel: "df0e8c48a1e4daf8040d1e381d859f45e7f23bdf2242f713e88cc684b45925ec"
 
   url "https://github.com/VSCodium/vscodium-insiders/releases/download/#{version}/VSCodium.#{arch}.#{version}.dmg",
       verified: "github.com/VSCodium/vscodium-insiders/"
