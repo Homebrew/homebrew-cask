fefb584e35cace7d0ad9316e3cc14e1f97655d3e
Klaus Hipp <khipp@users.noreply.github.com>
2024-11-06 02:14:22 +0100

    pokemon-tcg-live 1.20.0

--- Casks/p/pokemon-tcg-live.rb
+++ Casks/p/pokemon-tcg-live.rb
@@ -1,5 +1,5 @@
 cask "pokemon-tcg-live" do
-  version "1.19.0,384348"
+  version "1.20.0,391121"
   sha256 :no_check
 
   url "https://installer.studio-prod.pokemon.com/installer/PokemonTCGLiveInstaller_Mac.dmg"
