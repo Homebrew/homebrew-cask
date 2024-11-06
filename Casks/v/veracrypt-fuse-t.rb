73fee7e2793130ede27600d5bf9a0fba8503b59d
vodium-vastro <shiny.bell8527@fastmail.com>
2024-11-06 19:30:15 +0000

    veracrypt-fuse-t 1.26.14 (new cask)

---
+++ Casks/v/veracrypt-fuse-t.rb
@@ -0,0 +1,27 @@
+cask "veracrypt-fuse-t" do
+  version "1.26.14"
+  sha256 "8f0cee74d5b0d68e57d13d4ffcdbeeca66fa0711c547b3ddd28ecdb1b9549580"
+
+  url "https://launchpad.net/veracrypt/trunk/#{version}/+download/VeraCrypt_FUSE-T_#{version}.dmg",
+      verified: "launchpad.net/veracrypt/trunk/"
+  name "VeraCrypt (fuse-t)"
+  desc "Disk encryption software focusing on security based on TrueCrypt (FUSE-T)"
+  homepage "https://www.veracrypt.fr/"
+
+  livecheck do
+    url "https://www.veracrypt.fr/en/Downloads.html"
+    regex(/href=.*?VeraCrypt_FUSE-T[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+  end
+
+  depends_on cask: "fuse-t"
+
+  pkg "VeraCrypt_Installer.pkg"
+
+  uninstall pkgutil: "com.idrix.pkg.veracrypt"
+
+  zap trash: [
+    "~/Library/Application Support/VeraCrypt",
+    "~/Library/Preferences/org.idrix.VeraCrypt.plist",
+    "~/Library/Saved Application State/org.idrix.VeraCrypt.savedState",
+  ]
+end
