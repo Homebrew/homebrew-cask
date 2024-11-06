85ad94a54fc680cc815681e7f50247df2421e397
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-06 19:28:06 +0000

    sf 2.65.8

--- Casks/s/sf.rb
+++ Casks/s/sf.rb
@@ -1,9 +1,9 @@
 cask "sf" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.64.8,21fd4e6"
-  sha256 arm:   "3ebd18a89e366e0a6c96e6757e7f51a138785386c5677e02ac880d6a80cafbef",
-         intel: "d8d88726e1b31c0e087c9d0f456439e73cd105dd2af6efa789a4db28ecad5d6e"
+  version "2.65.8,23a374a"
+  sha256 arm:   "6a19fca669eccc0a0a762d7b81e1df85251844c5defba6b8e42d2677256e8814",
+         intel: "af2917c4ecc738ed1e85158530c4e5c37f4fb4e9731cef21c611539d560c32c6"
 
   url "https://github.com/salesforcecli/cli/releases/download/#{version.csv.first}/sf-v#{version.csv.first}-#{version.csv.second}-#{arch}.pkg",
       verified: "github.com/salesforcecli/cli/"
