408e0f6c30af66d2d03aaf66ec82a15962ada73d
Aadit Kamat <aadit.kamat@ufl.edu>
2024-11-06 12:32:10 -0500

    Fix cask issues

--- Casks/l/liner@beta.rb
+++ Casks/l/liner@beta.rb
@@ -2,9 +2,10 @@ cask "liner@beta" do
   version "0.2.0"
   sha256 "65f574adc04814b99adaac3c9ac557993e9354a0e33c8c905f6b8db9675fabfb"
 
-  url "https://github.com/linerai/releases/releases/download/v#{version}/Liner-#{version}-arm64.dmg"
+  url "https://github.com/linerai/releases/releases/download/#{version}/Liner-#{version}.0004-arm64.dmg",
+      verified: "github.com/linerai/releases/"
   name "Liner.ai"
-  desc "A free tool that lets you train your ML models without coding"
+  desc "Free tool that lets you train your ML models without coding"
   homepage "https://liner.ai/"
 
   depends_on macos: ">= :el_capitan"
