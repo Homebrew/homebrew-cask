f533610138ebe1c2ed30d9ff6c84a997dfa3aa9b
Klaus Hipp <khipp@users.noreply.github.com>
2024-11-07 07:57:00 +0100

    orangedrangon-android-messages 5.5.0

--- Casks/o/orangedrangon-android-messages.rb
+++ Casks/o/orangedrangon-android-messages.rb
@@ -1,6 +1,6 @@
 cask "orangedrangon-android-messages" do
-  version "5.4.5"
-  sha256 "24d23012e35938056919c287bb8e765385bf20eb1ab83e7d45f264e8f800a33a"
+  version "5.5.0"
+  sha256 "50dfa270252541e2d73dfce9262574dfd2a6a3288d3a8a158d5f75b3576b615d"
 
   url "https://github.com/OrangeDrangon/android-messages-desktop/releases/download/v#{version}/Android-Messages-v#{version}-mac-universal.zip"
   name "Android Messages Desktop"
