d1cef4d05bb935db5f2e93d2408e75e2afbfac3a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-07 01:15:01 +0000

    warzone-2100 4.5.4

--- Casks/w/warzone-2100.rb
+++ Casks/w/warzone-2100.rb
@@ -1,6 +1,6 @@
 cask "warzone-2100" do
-  version "4.5.3"
-  sha256 "c15a39cfab236a5d9fb279a5a3bf8fa50e84db52e25889cfe371105593935de9"
+  version "4.5.4"
+  sha256 "f7d08cc798ca775dc1143aa037bc84ada1492b72d2d5bd1590107a396e2011bd"
 
   url "https://github.com/Warzone2100/warzone2100/releases/download/#{version}/warzone2100_macOS_universal.zip",
       verified: "github.com/Warzone2100/warzone2100/"
