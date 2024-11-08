4a8eb49c6a13565622a82cf19eaf830ff17731ce
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-11-08 11:47:57 -0500

    follow 0.2.0-beta.0

--- Casks/f/follow.rb
+++ Casks/f/follow.rb
@@ -1,8 +1,11 @@
 cask "follow" do
-  version "0.1.2-beta.0"
-  sha256 "13d7b5ee51bbc3a908bd571ec8da2eef2e3ba683dcfdbe60f85a92f75845c57e"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://github.com/RSSNext/Follow/releases/download/v#{version}/Follow-#{version}-macos-universal.dmg",
+  version "0.2.0-beta.0"
+  sha256 arm:   "cef7244f33824ed0693c04d2158bdf9e6c0db5d0ba34084b3061945412dda60c",
+         intel: "5989f7990f57e6d77bd9b896f978cb73cb71e253e0795e7c2b60691de7548956"
+
+  url "https://github.com/RSSNext/Follow/releases/download/v#{version}/Follow-#{version}-macos-#{arch}.dmg",
       verified: "github.com/RSSNext/Follow/"
   name "Follow"
   desc "Information browser"
@@ -11,6 +14,7 @@ cask "follow" do
   livecheck do
     url :url
     regex(/^v?(\d+(?:\.\d+)+(?:[._-]beta[._-]?\d+)?)$/i)
+    strategy :github_latest
   end
 
   auto_updates true
