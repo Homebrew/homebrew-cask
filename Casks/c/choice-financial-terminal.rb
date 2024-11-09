c810fd4dd276eee4b64f2ed2ef1017dbd2aee7ba
Klaus Hipp <khipp@users.noreply.github.com>
2024-11-09 08:42:37 +0100

    choice-financial-terminal 8.4.0.1

--- Casks/c/choice-financial-terminal.rb
+++ Casks/c/choice-financial-terminal.rb
@@ -1,13 +1,13 @@
 cask "choice-financial-terminal" do
   arch arm: "arm", intel: "x86"
 
-  version "8.3.0.2"
+  version "8.4.0.1"
   sha256 :no_check
 
   url "https://choice-app.eastmoney.com/choice/OfflinePackage/ChoiceSetup_mac_#{arch}.dmg"
   name "Choice Financial Terminal"
   name "Choice金融终端"
-  desc "Financial Information Acquisition Platform"
+  desc "Financial information acquisition platform"
   homepage "https://choice.eastmoney.com/"
 
   livecheck do
