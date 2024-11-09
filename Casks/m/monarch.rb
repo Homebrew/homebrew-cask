940b9ed044b0b032e279bf1c9fa490ae0139bd44
Michael Law <rmdashrfv@hey.com>
2024-11-08 23:28:47 -0500

    monarch 0.7.10

--- Casks/m/monarch.rb
+++ Casks/m/monarch.rb
@@ -1,24 +1,24 @@
 cask "monarch" do
   arch arm: "arm64", intel: "x64"
 
-  sha256 arm:   "f6b5ff907f9533a57cd3e59cb2e29f90ee5d620ce7b02009df634800ebfcb128",
-         intel: "637d3385a5efd3778205286c0297515806525c1458586c5e5e40fa6cb86a79ad"
+  sha256 arm:   "cdf327fbb2682e6a6f6ef0d3f65c2a734dfe2546e964e646b94940a656deea79",
+         intel: "65a79fc7befb19ad409bc12f584fbf1f3d328a975fbf947778700ddfe2703566"
 
   on_arm do
-    version "0.7.8"
+    version "0.7.10"
   end
   on_intel do
-    version "0.7.7"
+    version "0.7.10"
   end
 
-  url "https://monarchlauncher.s3.amazonaws.com/#{arch}/monarch-#{version}.dmg",
-      verified: "monarchlauncher.s3.amazonaws.com/"
+  url "https://storage.googleapis.com/monarchlauncher/#{arch}/monarch-#{version}.dmg",
+      verified: "storage.googleapis.com/monarchlauncher/"
   name "Monarch"
   desc "Spotlight Search"
   homepage "https://monarchlauncher.com/"
 
   livecheck do
-    url "https://monarchlauncher.s3.amazonaws.com/#{arch}/latest-mac.yml"
+    url "https://storage.googleapis.com/monarchlauncher/#{arch}/latest-mac.yml"
     strategy :electron_builder
   end
 
