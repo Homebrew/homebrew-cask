4c0d38a745699af262d31a32562e274e144c9234
Klaus Hipp <khipp@users.noreply.github.com>
2024-11-09 08:34:58 +0100

    ableset 2.7.2

--- Casks/a/ableset.rb
+++ Casks/a/ableset.rb
@@ -1,9 +1,9 @@
 cask "ableset" do
   arch arm: "-arm64"
 
-  version "2.7.1"
-  sha256 arm:   "27e7bd8d55b474c92589b79c6272269151a9941de82969698338d9c2d332a3a7",
-         intel: "6c312f3b5cf08943fd837eb8a5f81d8fc0e8ede592bb99c18b423f7a400ce084"
+  version "2.7.2"
+  sha256 arm:   "1d2ccfde0faa9e7712ab1a4d63d4965384a961ea6941462f6c9d3cc24aff674b",
+         intel: "8f90ff32db3cc02cbf09ac2dfa733fd7b00b7c72c87aa298ee56c1ee5a0f16e8"
 
   url "https://s3.amazonaws.com/download.ableset.app/mac#{arch}/AbleSet-#{version}#{arch}.dmg",
       verified: "s3.amazonaws.com/download.ableset.app/"
