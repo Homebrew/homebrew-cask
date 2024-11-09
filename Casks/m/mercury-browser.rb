9bcee323c640b11aad600819ff6d73b103ae2493
zwu7 <88484720+zwu7@users.noreply.github.com>
2024-11-09 11:54:25 -0500

    Update mercury-browser.rb

--- Casks/m/mercury-browser.rb
+++ Casks/m/mercury-browser.rb
@@ -1,30 +1,30 @@
+# typed: strict
+# frozen_string_literal: true
+
 cask "mercury-browser" do
   version "129.0.2"
-  sha256 "dd31d1150ead565f5ffa5d515638548d970f43f786359314d3cafe61833f7178"  
+  sha256 "dd31d1150ead565f5ffa5d515638548d970f43f786359314d3cafe61833f7178"
 
   url "https://github.com/Alex313031/Mercury/releases/download/v.#{version}/mercury-#{version}_mac_ARM64.dmg"
   name "Mercury"
-  desc "Firefox fork with compiler optimizations and patches from Librewolf, Waterfox, Ghostery, and BetterFox."
+  desc "Firefox fork with compiler optimizations and patches"
   homepage "https://github.com/Alex313031/Mercury"
-  
+
   livecheck do
     url "https://github.com/Alex313031/Mercury/releases/latest"
     strategy :page_match
-    regex(/href=.*?\/v\.(\d+(?:\.\d+)+)\/mercury-\1_mac_ARM64\.dmg/i)
+    regex(%r{href=.*?/v\.(\d+(?:\.\d+)+)/mercury-\1_mac_ARM64\.dmg}i)
   end
-  
+
   app "Mercury.app"
 
   uninstall quit: "com.alex313031.mercury.com.alex313031.mercury"
-  
+
   zap trash: [
         "/Library/Logs/DiagnosticReports/mercury_*",
         "~/Library/Application Support/mercury",
         "~/Library/Caches/mercury",
         "~/Library/Saved Application State/com.alex313031.mercury.com.alex313031.mercury.savedState",
       ],
-      rmdir: [
-        "~/Library/Application Support/Mozilla", #  May also contain non-Firefox data
-      ]
-  
+      rmdir: "~/Library/Application Support/Mozilla"
 end
