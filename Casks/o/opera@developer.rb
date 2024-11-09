6bbbca9f270f49550a293da4f62ff70b6996d6db
AltCode <12354672+AlternateRT@users.noreply.github.com>
2024-11-09 14:50:26 +0100

    opera@developer: update zap

--- Casks/o/opera@developer.rb
+++ Casks/o/opera@developer.rb
@@ -19,8 +19,10 @@ cask "opera@developer" do
 
   zap trash: [
     "~/Library/Application Support/com.operasoftware.OperaDeveloper",
+    "~/Library/Caches/com.operasoftware.Installer.OperaDeveloper",
     "~/Library/Caches/com.operasoftware.OperaDeveloper",
     "~/Library/Cookies/com.operasoftware.OperaDeveloper.binarycookies",
+    "~/Library/HTTPStorages/com.operasoftware.Installer.OperaDeveloper",
     "~/Library/Preferences/com.operasoftware.OperaDeveloper.plist",
     "~/Library/Saved Application State/com.operasoftware.OperaDeveloper.savedState",
   ]
