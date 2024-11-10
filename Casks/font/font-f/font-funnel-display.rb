6121b28b5ffee33955af43686f4e28f68f73559e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-10 00:14:05 +0000

    font-funnel-display: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

--- Casks/font/font-f/font-funnel-display.rb
+++ Casks/font/font-f/font-funnel-display.rb
@@ -2,9 +2,10 @@ cask "font-funnel-display" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/funneldisplay/FunnelDisplay%5Bwght%5D.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/funneldisplay/FunnelDisplay%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Funnel Display"
-  homepage "https://github.com/Dicotype/Funnel"
+  homepage "https://fonts.google.com/specimen/Funnel+Display"
 
   font "FunnelDisplay[wght].ttf"
 
