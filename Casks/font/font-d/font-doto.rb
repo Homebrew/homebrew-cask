ebb7fb5beb87ab80c2f4cf78880130a5841bb117
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-10 00:14:03 +0000

    font-doto: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

--- Casks/font/font-d/font-doto.rb
+++ Casks/font/font-d/font-doto.rb
@@ -2,9 +2,10 @@ cask "font-doto" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/doto/Doto%5BROND%2Cwght%5D.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/doto/Doto%5BROND%2Cwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Doto"
-  homepage "https://github.com/oliverlalan/Doto"
+  homepage "https://fonts.google.com/specimen/Doto"
 
   font "Doto[ROND,wght].ttf"
 
