3a88124b6b33847b527a6632fc5075b5ebf97ec9
Ollie Lloyd <96823347+silverfoxdoc@users.noreply.github.com>
2024-11-10 20:37:25 +0000

    Update vorta.rb
    changed url now that vorta file names have been made consistent

--- Casks/v/vorta.rb
+++ Casks/v/vorta.rb
@@ -1,11 +1,11 @@
 cask "vorta" do
-  arch arm: "", intel: "_Intel"
+  arch arm: "arm", intel: "intel"
 
   version "0.10.0"
   sha256 arm:   "3081d63e3e3b174ce5e9b0700a6ed26f406157601adfc4cd0a5028ad03bca020",
          intel: "4df3d955d4b323910a935b6fb542fb35babb669948fa8e21be8d11731e10b27b"
 
-  url "https://github.com/borgbase/vorta/releases/download/v#{version}/Vorta-#{version}#{arch}.dmg"
+  url "https://github.com/borgbase/vorta/releases/download/v#{version}/Vorta-v#{version}-#{arch}.dmg"
   name "Vorta"
   desc "Desktop Backup Client for Borg"
   homepage "https://github.com/borgbase/vorta"
