5fb6a836bf5b44a36fe8e0b809c16883c92eba53
Daeho Ro <lamanus@outlook.kr>
2024-11-10 11:23:12 +0900

    gridtracker2 2.241107.2 (new cask, renamed from gridtracker)
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>
    
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

---
+++ Casks/g/gridtracker2.rb
@@ -0,0 +1,25 @@
+cask "gridtracker2" do
+  version "2.241107.2"
+  sha256 "abd3b79192b109f7df2947c36d2ed5f6c4e258c85a7f44592f4c0df0d4ae5f73"
+
+  url "https://download2.gridtracker.org/GridTracker2-#{version}-universal-mac.zip"
+  name "GridTracker2"
+  desc "Warehouse of amateur radio information presented in an easy to use interface"
+  homepage "https://gridtracker.org/"
+
+  livecheck do
+    url "https://download2.gridtracker.org/latest-mac.yml"
+    strategy :electron_builder
+  end
+
+  depends_on macos: ">= :catalina"
+
+  app "GridTracker2.app"
+
+  zap trash: [
+    "~/Library/Application Support/GridTracker*",
+    "~/Library/Caches/GridTracker",
+    "~/Library/Preferences/org.gridtracker.GridTracker*.plist",
+    "~/Library/Saved Application State/org.gridtracker.GridTracker*.savedState",
+  ]
+end
