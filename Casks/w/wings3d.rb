f18de9524da4872adac9d1daf5ad096a498a9dce
Bevan Kay <email@bevankay.me>
2024-11-11 13:39:43 +1100

    wings3d 2.4 (arm only)

--- Casks/w/wings3d.rb
+++ Casks/w/wings3d.rb
@@ -1,15 +1,24 @@
 cask "wings3d" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "2.3"
-  sha256 arm:   "82b31a7a71ad470a59edd116181ac8d606a2ef83df065662ddec44c57912deda",
-         intel: "dbb1bbb229c3a3737fc2558d7a64996780dada3da3c75e46bc357789dde8092a"
+  on_arm do
+    version "2.4"
+    sha256  "8128f2cf6d22408b9bfa433b32a570915ce2d58642268ddb44919efe30d13ff5"
+  end
+  on_intel do
+    version "2.3"
+    sha256 "dbb1bbb229c3a3737fc2558d7a64996780dada3da3c75e46bc357789dde8092a"
+  end
 
   url "https://downloads.sourceforge.net/wings/#{version}/wings-#{version}-macos-#{arch}.dmg",
       verified: "sourceforge.net/wings/"
   name "Wings 3D"
   desc "Advanced subdivision modeller"
   homepage "https://www.wings3d.com/"
+
+  livecheck do
+    regex(/url=.*?wings[._-]v?(\d+(?:\.\d+)+)[._-]macos[._-]#{arch}\.dmg/i)
+  end
 
   app "Wings3D.app"
 end
