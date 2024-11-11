83bc1caa3519553864c31138d9c1edf70d1ff49d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-11 21:52:33 +0000

    pieces-cli 1.8.2

--- Casks/p/pieces-cli.rb
+++ Casks/p/pieces-cli.rb
@@ -1,9 +1,9 @@
 cask "pieces-cli" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.8.0"
-  sha256 arm:   "94605d092b51f10f6618f744453af5e423eb59fb91450d639f20a56feec27bae",
-         intel: "f9aa58b8ccb2c9d9445480ea0b0c0aaa6a9271def4950e3099cb44f46c0ad410"
+  version "1.8.2"
+  sha256 arm:   "eef4a9a7d3447e4daab7705c92cda56bb9213884e9c02bfaa4f7ffa582673ce7",
+         intel: "aedba93c9883536d1776f55cf7ecd96c129ac12f47216ec39dd1fb49514d1e02"
 
   url "https://storage.googleapis.com/app-releases-production/pieces_cli/release/pieces-cli-mac_#{arch}-#{version}.zip",
       verified: "storage.googleapis.com/app-releases-production/pieces_cli/release/"
