8c2c9211b615b74e21529563489dbdfa6b818a81
Sam Ford <1584702+samford@users.noreply.github.com>
2024-11-11 17:08:30 -0500

    shadow@beta 9.9.10131

--- Casks/s/shadow@beta.rb
+++ Casks/s/shadow@beta.rb
@@ -1,9 +1,9 @@
 cask "shadow@beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "9.9.10129"
-  sha256 arm:   "028eed95a5abbf688ef7447007bc565d739185da0e602faea9cdcb030ed0788c",
-         intel: "f139a526d66a895edb412cdb29b7c46ef4e441c0e1fa594611d0357a58ef4ef4"
+  version "9.9.10131"
+  sha256 arm:   "fe209b56359fbbe733d9737cbd35db08718d1a0a61fe8f1e6658af07acf7fe54",
+         intel: "cee6b55b98750d24d36fc8fe7a15bab80f3259b78b473db006dd81ea7ba11430"
 
   url "https://update.shadow.tech/launcher/preprod/mac/#{arch}/ShadowPCBeta-#{version}.dmg"
   name "Shadow PC Beta"
