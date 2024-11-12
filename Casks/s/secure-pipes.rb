d939d5c837c5b327eb05353c1213f1017b902613
Klaus Hipp <khipp@users.noreply.github.com>
2024-11-12 09:14:06 +0100

    secure-pipes: update livecheck

--- Casks/s/secure-pipes.rb
+++ Casks/s/secure-pipes.rb
@@ -19,7 +19,7 @@ cask "secure-pipes" do
       headers = Homebrew::Livecheck::Strategy.page_headers(download_url)
       next if headers.blank?
 
-      match = headers.first["content-disposition"].match(regex)
+      match = headers.first["content-disposition"]&.match(regex)
       next if match.blank?
 
       "#{match[1]},#{download_hash}"
