d13526429880989764b94e341bdb38a9851a3da6
Klaus Hipp <khipp@users.noreply.github.com>
2024-11-12 08:53:01 +0100

    noxappplayer: update livecheck

--- Casks/n/noxappplayer.rb
+++ Casks/n/noxappplayer.rb
@@ -11,7 +11,10 @@ cask "noxappplayer" do
     url "https://www.bignox.com/en/download/fullPackage/mac_fullzip"
     regex(%r{/(\d+)/([^/]+)\.dmg\?filename=NoxInstaller_(\d+(?:\.\d+)*)_en\.dmg}i)
     strategy :header_match do |headers, regex|
-      headers["location"].scan(regex).map { |match| "#{match[2]},#{match[0]},#{match[1]}" }
+      match = headers["location"]&.match(regex)
+      next if match.blank?
+
+      "#{match[3]},#{match[1]},#{match[2]}"
     end
   end
 
