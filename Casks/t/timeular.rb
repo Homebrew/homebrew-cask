87d42bbe57f35b5d0e0f98bcdbc6b06262ac2dd8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-12 13:13:06 +0000

    timeular 6.8.5

--- Casks/t/timeular.rb
+++ Casks/t/timeular.rb
@@ -2,9 +2,9 @@ cask "timeular" do
   arch arm: "-m1-arm64"
   livecheck_folder = on_arch_conditional arm: "m1", intel: "latest"
 
-  version "6.8.4"
-  sha256 arm:   "c0369be276a794ba260c42722d9892794263ebb36093e502122a6ceede9d6926",
-         intel: "8f13ee33e958fea89a182d40fc5d68963a10b636ba6a808728afaa6bd824d9c6"
+  version "6.8.5"
+  sha256 arm:   "bcc572f0c3d2d266dd388320ec0b48820b363b2b1664be78e4b59089bcebe7e2",
+         intel: "e3dbf1e2932f50f1690a436c51da430ec846942d2b33bc1e59254c599a2debf3"
 
   url "https://timeular-desktop-packages.s3.amazonaws.com/mac/production/Timeular-#{version}#{arch}.dmg",
       verified: "timeular-desktop-packages.s3.amazonaws.com/"
