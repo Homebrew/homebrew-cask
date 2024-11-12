eacc9e70a56fa510f2a7371ec7a5185802f88a98
Klaus Hipp <khipp@users.noreply.github.com>
2024-11-12 08:55:26 +0100

    mumu: update livecheck

--- Casks/m/mumu.rb
+++ Casks/m/mumu.rb
@@ -12,7 +12,10 @@ cask "mumu" do
     url "https://vendors.paddle.com/download/product/597910"
     regex(%r{/([^/]+)_Mumu%20(\d+(?:\.\d+)*)\.dmg}i)
     strategy :header_match do |headers, regex|
-      headers["location"].scan(regex).map { |match| "#{match[1]},#{match[0]}" }
+      match = headers["location"]&.match(regex)
+      next if match.blank?
+
+      "#{match[2]},#{match[1]}"
     end
   end
 
