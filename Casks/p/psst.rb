5e95f3d903279fbea478c86afe81a1693f2eb7df
Daeho Ro <lamanus@outlook.kr>
2024-11-13 15:40:43 +0900

    psst 0.1.0,20241112.195917

--- Casks/p/psst.rb
+++ Casks/p/psst.rb
@@ -1,5 +1,5 @@
 cask "psst" do
-  version "0.1.0,20241109.024326"
+  version "0.1.0,20241112.195917"
   sha256 :no_check
 
   url "https://nightly.link/jpochyla/psst/workflows/build/main/Psst.dmg.zip",
