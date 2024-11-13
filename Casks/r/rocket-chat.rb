bb36ed5e9d6224d61ec2799d385ce160ed37c060
eleanordoesntcode <166862894+eleanordoesntcode@users.noreply.github.com>
2024-11-14 00:24:37 +0100

    rocket-chat 4.1.2

--- Casks/r/rocket-chat.rb
+++ Casks/r/rocket-chat.rb
@@ -1,6 +1,6 @@
 cask "rocket-chat" do
-  version "4.1.1"
-  sha256 "6100591a09c477b112284dae99b523607b513eaeb7bb139cbc33fbdbfbaa438b"
+  version "4.1.2"
+  sha256 "4f47a2c60c910b0ec1740f75d365363a42b366462044aada5f1bf70f29194d9a"
 
   url "https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/#{version}/rocketchat-#{version}-mac.dmg",
       verified: "github.com/RocketChat/Rocket.Chat.Electron/"
