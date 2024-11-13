0f45112886282a20a78e2a58d571feac4a74e204
Daeho Ro <lamanus@outlook.kr>
2024-11-13 15:46:09 +0900

    streamlabs 1.17.2

--- Casks/s/streamlabs.rb
+++ Casks/s/streamlabs.rb
@@ -1,9 +1,9 @@
 cask "streamlabs" do
   arch arm: "-arm64"
 
-  version "1.17.0"
-  sha256 arm:   "859232c8ac81f3f58afbabf85bc78773e584fe4412af771b2f9cc592e78968c7",
-         intel: "d3f30634b7ce34cb031a783f37d87fad0d568a441ec41527ff1ed46cf38e96f0"
+  version "1.17.2"
+  sha256 arm:   "542cbe5d8b9ff3820aec158773d1b6cbc5aba80bd63033ea6d741c146eba00f5",
+         intel: "ada212dc006e668f39c5d167c201b5547dfe5f9bbb50b0566f2a6c93a9384573"
 
   url "https://slobs-cdn.streamlabs.com/Streamlabs+Desktop-#{version}#{arch}.dmg"
   name "Streamlabs Desktop"
