2c3119dad78ac06164750008234713cda374177d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-14 12:56:52 +0000

    libreoffice 24.8.3

--- Casks/l/libreoffice.rb
+++ Casks/l/libreoffice.rb
@@ -2,9 +2,9 @@ cask "libreoffice" do
   arch arm: "aarch64", intel: "x86-64"
   folder = on_arch_conditional arm: "aarch64", intel: "x86_64"
 
-  version "24.8.2"
-  sha256 arm:   "1edda831d0628b1db1b9289b8edcd04978b2d8b23550308078bf6a584bf49965",
-         intel: "04161f7e49089edaf22706565797569907a78ffe64fe4137bd3cc552eb71c3dd"
+  version "24.8.3"
+  sha256 arm:   "66f853ed8a7e1c402ae331cc3c0457a0fe6c3cba764054a6ff208cd0801f0140",
+         intel: "58a5fd80f4b33741127ff68d90a0eaaa68bd329429bf5b184ca9261f3b692639"
 
   url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/#{folder}/LibreOffice_#{version}_MacOS_#{arch}.dmg",
       verified: "download.documentfoundation.org/libreoffice/stable/"
