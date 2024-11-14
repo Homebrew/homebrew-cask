84eb287c25077ce48312cd9cf53efcaf25a8e74a
eleanordoesntcode <166862894+eleanordoesntcode@users.noreply.github.com>
2024-11-14 20:03:03 +0100

    mission-control-plus 1.24

--- Casks/m/mission-control-plus.rb
+++ Casks/m/mission-control-plus.rb
@@ -1,6 +1,6 @@
 cask "mission-control-plus" do
-  version "1.23"
-  sha256 "58ffd154bad723dbd413bb793b389c81e60f5a57611dae32e87c2fea65f2597b"
+  version "1.24"
+  sha256 "b791fc0f174c1c0082176178c5a1671841fc0a3c90de9d5cb9d13ed9c21cc765"
 
   url "https://github.com/ronyfadel/MissionControlPlusReleases/releases/download/v#{version}/Mission.Control.Plus.tgz",
       verified: "github.com/ronyfadel/MissionControlPlusReleases/"
