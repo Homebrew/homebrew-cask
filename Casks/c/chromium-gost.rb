b055a1705798f5bf93cc2e02e4cc97f8abf23d53
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-14 12:43:06 +0000

    chromium-gost 131.0.6778.69

--- Casks/c/chromium-gost.rb
+++ Casks/c/chromium-gost.rb
@@ -1,9 +1,9 @@
 cask "chromium-gost" do
   arch arm: "arm64", intel: "amd64"
 
-  version "130.0.6723.92"
-  sha256 arm:   "766132e5da7013e316a09fc3afd86988121175d5559b21aefd1093ead5b930e3",
-         intel: "6e9b01d7dbf91ae4014aae9e55fc4047d8f08eb544675d4dc97bcbbf39159a52"
+  version "131.0.6778.69"
+  sha256 arm:   "9dd20cbe7a843a79675059d97eeb11fb16f788e6625ecb8b837d7a9162a392a8",
+         intel: "50abc1cb39823d5e3a6686de2402d58a0dbdb7a375b165e9b36e2fcd930b050f"
 
   url "https://github.com/deemru/Chromium-Gost/releases/download/#{version}/chromium-gost-#{version}-macos-#{arch}.tar.bz2"
   name "Chromium-Gost"
