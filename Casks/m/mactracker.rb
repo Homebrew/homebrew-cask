366f230260fa64edc42bac0353e346d907817530
Philip Molares <philip.molares@udo.edu>
2024-11-15 17:20:31 +0100

    fix(mactracker): remove the www prefix
    All www.mactracker.ca URLs have a 301 to mactracker.ca so it seems reasonable
    to change this here and make it correct.

--- Casks/m/mactracker.rb
+++ Casks/m/mactracker.rb
@@ -2,7 +2,7 @@ cask "mactracker" do
   version "7.13"
   sha256 "4ec4ee578700e054e3c6ba3a18b69d10f417b0f20a0e4126e7d8881959a2dd6e"
 
-  url "https://www.mactracker.ca/downloads/Mactracker_#{version}.zip"
+  url "https://mactracker.ca/downloads/Mactracker_#{version}.zip"
   name "Mactracker"
   desc "Detailed information on every Apple product ever made"
   homepage "https://mactracker.ca/"
