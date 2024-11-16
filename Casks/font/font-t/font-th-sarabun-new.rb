a7e7d3183b5cee97685de73ef5fbb82d3329d025
phwt <28344318+phwt@users.noreply.github.com>
2024-11-16 16:59:28 +0700

    font-th-sarabun-new 1.0 (new cask)

---
+++ Casks/font/font-t/font-th-sarabun-new.rb
@@ -0,0 +1,15 @@
+cask "font-th-sarabun-new" do
+  version "1.0"
+  sha256 :no_check
+
+  url "https://www.f0nt.com/?dl_name=sipafonts/THSarabunNew.zip"
+  name "TH Sarabun New"
+  homepage "https://www.f0nt.com/release/th-sarabun-new/"
+
+  font "THSarabunNew Bold.ttf"
+  font "THSarabunNew BoldItalic.ttf"
+  font "THSarabunNew.ttf"
+  font "THSarabunNew Italic.ttf"
+
+  # No zap stanza required
+end
