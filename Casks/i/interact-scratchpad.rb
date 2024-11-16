3c9878d4f58f831933f7344079e828975e6e2d07
Klaus Hipp <khipp@users.noreply.github.com>
2024-11-16 06:54:02 +0100

    interact-scratchpad: deprecate

--- Casks/i/interact-scratchpad.rb
+++ Casks/i/interact-scratchpad.rb
@@ -8,10 +8,7 @@ cask "interact-scratchpad" do
   desc "Menu bar utility to create contacts from snippets of text"
   homepage "https://docs.getdrafts.com/docs/misc/interact-scratchpad"
 
-  livecheck do
-    url :url
-    strategy :extract_plist
-  end
+  deprecate! date: "2024-11-15", because: :discontinued
 
   depends_on macos: ">= :monterey"
 
