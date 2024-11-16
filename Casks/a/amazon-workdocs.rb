6edf05faa1256e336b79e8af7e4b26106918386d
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-11-16 16:11:30 -0500

    amazon-workdocs: disable

--- Casks/a/amazon-workdocs.rb
+++ Casks/a/amazon-workdocs.rb
@@ -8,13 +8,7 @@ cask "amazon-workdocs" do
   desc "Fully managed, secure content creation, storage, and collaboration service"
   homepage "https://aws.amazon.com/workdocs/"
 
-  livecheck do
-    url "https://d28gdqadgmua23.cloudfront.net/mac/appcast/appcast-workdocs-prod.xml"
-    regex(%r{/(\d+(?:\.\d+)+)/(\d+)/Amazon WorkDocs\.app\.zip}i)
-    strategy :sparkle do |item, regex|
-      item.url.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
-    end
-  end
+  disable! date: "2025-04-25", because: :discontinued
 
   app "Amazon WorkDocs.app"
 
