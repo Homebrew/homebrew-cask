ccee569756d0f9235edc4e0e3a404b1541043abb
Klaus Hipp <khipp@users.noreply.github.com>
2024-11-17 14:57:12 +0100

    djv 2.0.8

--- Casks/d/djv.rb
+++ Casks/d/djv.rb
@@ -1,18 +1,22 @@
 cask "djv" do
-  version "1.3.0"
-  sha256 "5702e1a084377e0c0486b68c0fb5014b4e7aa3f4dbcd61bd489359859ad7589f"
+  version "2.0.8"
+  sha256 "5df641ad2eb27d2beb35ec6ebb05a1d2a14c2af19b65aee370a1eac6fa5ae056"
 
-  url "https://downloads.sourceforge.net/djv/djv-stable/#{version}/DJV-#{version}-Darwin.dmg",
-      verified: "downloads.sourceforge.net/djv/"
+  url "https://github.com/darbyjohnston/DJV/releases/download/#{version}/DJV-#{version}-Darwin.dmg",
+      verified: "github.com/darbyjohnston/DJV/"
   name "DJV Imaging"
   desc "Review software for VFX, animation, and film production"
   homepage "https://darbyjohnston.github.io/DJV/"
 
-  livecheck do
-    url "https://sourceforge.net/projects/djv/rss?path=/djv-stable"
-  end
+  deprecate! date: "2024-11-17", because: :unmaintained
 
-  app "DJV.app"
+  app "DJV#{version.major}.app"
+
+  zap trash: [
+        "~/Documents/DJV/djv.log",
+        "~/Library/Preferences/com.djv-sourceforge-net-*.plist",
+      ],
+      rmdir: "~/Documents/DJV"
 
   caveats do
     requires_rosetta
