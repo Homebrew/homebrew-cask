2d05d9f0a36a5b96e724c76cdd03d41230039c98
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-17 01:02:25 +0000

    swiftformat-for-xcode 0.55.0

--- Casks/s/swiftformat-for-xcode.rb
+++ Casks/s/swiftformat-for-xcode.rb
@@ -1,6 +1,6 @@
 cask "swiftformat-for-xcode" do
-  version "0.54.6"
-  sha256 "108521f090574309fa1d9dbe8f2fc1268f9cd97553301a5d05c083cb6211a1a8"
+  version "0.55.0"
+  sha256 "35aaab2f8990687285e7f771cc0d588577eed9df9f3cb671b19bc045df268587"
 
   url "https://github.com/nicklockwood/SwiftFormat/releases/download/#{version}/SwiftFormat.for.Xcode.app.zip"
   name "SwiftFormat for Xcode"
