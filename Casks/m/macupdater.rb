5800cf86c1d94e4fdc0d419e1d2aa28876f1ec24
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-17 18:59:30 +0000

    macupdater 3.3.5

--- Casks/m/macupdater.rb
+++ Casks/m/macupdater.rb
@@ -4,7 +4,7 @@ cask "macupdater" do
     sha256 "31f081d2a447de66d52330ed6090a0ed29aec9e257114ff736cffee76a8e40a0"
   end
   on_ventura :or_newer do
-    version "3.3.4"
+    version "3.3.5"
     # required as upstream package is regularly updated in-place https://github.com/Homebrew/homebrew-cask/pull/182188#issuecomment-2284199515
     sha256 :no_check
 
