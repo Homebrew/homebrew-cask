1986a7ba2f22b9a9024d68297f5311e3b03a96b4
Daeho Ro <lamanus@outlook.kr>
2024-11-18 23:23:37 +0900

    teleport 17.0.1 (new cask)
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

---
+++ Casks/t/teleport.rb
@@ -0,0 +1,38 @@
+cask "teleport" do
+  version "17.0.1"
+  sha256 "aa3b8db9f9bcdddc4e3bff0bde6be7676d6a2c14dd0c661255b4ab34f3ce035d"
+
+  url "https://cdn.teleport.dev/teleport-#{version}.pkg",
+      verified: "cdn.teleport.dev/"
+  name "Teleport"
+  desc "Modern SSH server for teams managing distributed infrastructure"
+  homepage "https://goteleport.com/"
+
+  livecheck do
+    url "https://goteleport.com/download/"
+    regex(/teleport[._-]v?(\d+(?:\.\d+)+)\.pkg/i)
+  end
+
+  conflicts_with cask:    [
+                   "tsh",
+                   "tsh@13",
+                 ],
+                 formula: "teleport"
+
+  pkg "teleport-#{version}.pkg"
+
+  uninstall pkgutil: [
+              "(.*).com.gravitational.teleport.tctl",
+              "(.*).com.gravitational.teleport.tsh",
+              "com.gravitational.teleport",
+            ],
+            delete:  [
+              "/usr/local/bin/fdpass-teleport",
+              "/usr/local/bin/tbot",
+              "/usr/local/bin/tctl",
+              "/usr/local/bin/teleport",
+              "/usr/local/bin/tsh",
+            ]
+
+  zap trash: "~/.tsh"
+end
