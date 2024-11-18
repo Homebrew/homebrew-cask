4c930d67a2f05fe1f43eb709eb4964e669cf818f
Klaus Hipp <khipp@users.noreply.github.com>
2024-11-18 11:47:28 +0100

    simplistic: rename to jlutil

--- Casks/j/jlutil.rb
+++ Casks/j/jlutil.rb
@@ -1,15 +1,16 @@
-cask "simplistic" do
+cask "jlutil" do
   version :latest
   sha256 :no_check
 
-  url "https://newosxbook.com/tools/simplist.tar"
+  url "https://newosxbook.com/tools/jlutil.tgz"
   name "SimPLISTic"
   name "jlutil"
+  desc "Property list utility"
   homepage "https://newosxbook.com/tools/simplistic.html"
 
-  livecheck do
-    skip "unversioned command-line application"
-  end
+  deprecate! date: "2024-11-17", because: :unmaintained
 
   binary "jlutil.universal", target: "jlutil"
+
+  # No zap stanza required
 end
