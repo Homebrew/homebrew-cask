b9a5a8b31b1adaf4308408baebe36a7ca9c9db37
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-18 04:00:29 +0000

    snapmaker-luban 4.14.0

--- Casks/s/snapmaker-luban.rb
+++ Casks/s/snapmaker-luban.rb
@@ -1,9 +1,9 @@
 cask "snapmaker-luban" do
   arch arm: "-arm64", intel: "-x64"
 
-  version "4.13.0"
-  sha256 arm:   "54188d3696947c19d540eecc9c7aa3ce3bdd01f4878df71406ba9908e11df233",
-         intel: "144d0e7e30a35c4cb01dbef1f1c5b573265b2e693dbeb6fcd80712e1ec7304a4"
+  version "4.14.0"
+  sha256 arm:   "b90ef54a77d45aca5f2be06885b61488c622af5abcdae7ada5ddd75d11d8bbdf",
+         intel: "1938c3906e7090c3c06b1ce7f74337d37ec652b052ad5b4a57524bbd20844d0d"
 
   url "https://github.com/snapmaker/luban/releases/download/v#{version}/Snapmaker-Luban-#{version}-mac#{arch}.dmg",
       verified: "github.com/snapmaker/luban/"
