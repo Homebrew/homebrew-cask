378554116b2ac133f87b32b21f2a62f38f282feb
Sam Ford <1584702+samford@users.noreply.github.com>
2024-11-17 19:23:20 -0500

    brave-browser@nightly 1.75.18.0
    As was the case with `brave-browser@beta` previously, the most recent
    ARM release for `brave-browser@nightly` is 1.75.18.0 but the newest
    Intel release is still 1.75.15.0, so we have to split this by
    architecture for now.

--- Casks/b/brave-browser@nightly.rb
+++ Casks/b/brave-browser@nightly.rb
@@ -2,9 +2,14 @@ cask "brave-browser@nightly" do
   arch arm: "arm64", intel: "x64"
   folder = on_arch_conditional arm: "nightly-arm64", intel: "nightly"
 
-  version "1.75.15.0"
-  sha256 arm:   "95b554c0015fa09fe9ca81e5c71c2ba8403b21e906b734a20ee875a7c0d775e0",
-         intel: "7efd455bb70a004eec91c3798d87c7c0ff13876ad3b610a60480e4157eef16c5"
+  on_arm do
+    version "1.75.18.0"
+    sha256 "13b208e012876a1d5da07a7e33251bc625f7cfb6125a71cd06ef2eb0481f0dd3"
+  end
+  on_intel do
+    version "1.75.15.0"
+    sha256 "7efd455bb70a004eec91c3798d87c7c0ff13876ad3b610a60480e4157eef16c5"
+  end
 
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.major_minor_patch.sub(".", "")}/Brave-Browser-Nightly-#{arch}.dmg",
       verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
