84704ce8854c1e102328ab7827f39f2c37df8fae
viking1304 <viking1304@gmail.com>
2024-11-18 21:24:09 +0100

    Fix macos version logic

--- Casks/d/djstudio.rb
+++ Casks/d/djstudio.rb
@@ -1,18 +1,18 @@
 cask "djstudio" do
   arch arm: "-arm64", intel: ""
 
+  on_mojave :or_older do
+    version "2.6.28"
+    sha256 "a24739ad73b5fbda7c64aaae320b1486622c669ba3dc414d692def6026d2639d"
+
+    url "https://github.com/AppMachine/dj-studio-app-updates/releases/download/v#{version}/DJ.Studio-#{version}.dmg"
+  end
   on_catalina :or_newer do
     version "2.6.104"
     sha256 arm:   "8f6fb08230fb15044f83577ec4ec32735a4983cfed5e3a0cf9c4342dd38a55b7",
            intel: "cbb417f2802c0221b91d857c610fd583bfa9face941c8337c90977e1d0f98f5e"
 
     url "https://download.dj.studio/DJ.Studio-#{version}#{arch}.dmg"
-  end
-
-  if on_high_sierra || on_big_sur
-    version "2.6.28"
-    sha256 "a24739ad73b5fbda7c64aaae320b1486622c669ba3dc414d692def6026d2639d"
-    url "https://github.com/AppMachine/dj-studio-app-updates/releases/download/v#{version}/DJ.Studio-#{version}.dmg"
   end
 
   name "DJ.Studio"
