1241d085365088a9376efa85b4a1078eb9355857
Matthew Messina <mcmessina@outlook.com>
2024-11-19 10:37:23 -0600

    Testfully 1.154.0 (new cask)

---
+++ Casks/t/testfully.rb
@@ -0,0 +1,34 @@
+cask "testfully" do
+  version "1.154.0,319"
+  sha256 "8a4c084f39ac8b3ac95d96458cbe7458b5006fea849dd84896679f7670625eca"
+
+  url "https://releases.testfully.io/desktop/build-#{version.csv.second}/Testfully.app.zip"
+  name "Testfully"
+  desc "Simple & elegant platform for API testing and monitoring"
+  homepage "https://docs.testfully.io/"
+
+  livecheck do
+    url "https://docs.testfully.io/docs/download/"
+    regex(%r{build[._-](\d+)/Testfully(\.app)?\.zip}i)
+    strategy :page_match do |page, regex|
+      version = page.match(/latest\s*version\s*\((\d+(?:\.\d+)+)\)/i)
+      next if version.blank?
+
+      build = page.match(regex)
+      next if build.blank?
+
+      "#{version[1]},#{build[1]}"
+    end
+  end
+
+  depends_on macos: ">= :high_sierra"
+
+  app "Testfully.app"
+
+  zap trash: [
+    "~/Library/Application Support/io.testfully.app",
+    "~/Library/Caches/io.testfully.app",
+    "~/Library/Logs/io.testfully.app",
+    "~/Library/WebKit/io.testfully.app",
+  ]
+end
