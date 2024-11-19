60292b0c7bc18131280052fc78fc5b98c6a2f655
Bevan Kay <email@bevankay.me>
2024-11-19 23:20:18 +1100

    jamf-migrator: add livecheck

--- Casks/j/jamf-migrator.rb
+++ Casks/j/jamf-migrator.rb
@@ -2,10 +2,15 @@ cask "jamf-migrator" do
   version "7.4.2"
   sha256 "b66344e420810342849ae8509dc8bf16a0df5a4d7d5759ea6ecad9a030699988"
 
-  url "https://github.com/jamf/JamfMigrator/releases/download/v#{version}/jamf-migrator.zip"
+  url "https://github.com/jamf/Replicator/releases/download/v#{version}/jamf-migrator.zip"
   name "JamfMigrator"
   desc "Tool to migrate data granularly between Jamf Pro servers"
-  homepage "https://github.com/jamf/JamfMigrator"
+  homepage "https://github.com/jamf/Replicator"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   depends_on macos: ">= :high_sierra"
 
