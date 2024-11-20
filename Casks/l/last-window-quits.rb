17e990089d7bd5793281cc0fef29757b4b2e64e9
lawand-dot-io <info@lawand.io>
2024-11-21 01:43:43 +0300

    Changed the version number so its more clear for the users

--- Casks/l/last-window-quits.rb
+++ Casks/l/last-window-quits.rb
@@ -1,8 +1,8 @@
 cask "last-window-quits" do
-  version "1.1.3,2024,11"
+  version "1.1.3"
   sha256 "2b6645a0910f27a7021828a44a8744e03db7a7a99b75c3d88ad5fc09c31f29e0"
 
-  url "https://lawand.io/wp-content/uploads/#{version.csv.second}/#{version.csv.third}/last-window-quits-#{version.csv.first}.zip"
+  url "https://lawand.io/wp-content/uploads/2024/11/last-window-quits-#{version}.zip"
   name "Last Window Quits"
   desc "Automatically quit apps when their last window is closed"
   homepage "https://lawand.io/last-window-quits/"
