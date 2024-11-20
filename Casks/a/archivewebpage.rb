47bbb6245a2a739233adfa5ff78ec2aef3cf0d88
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-20 21:58:22 +0000

    archivewebpage 0.14.0

--- Casks/a/archivewebpage.rb
+++ Casks/a/archivewebpage.rb
@@ -1,6 +1,6 @@
 cask "archivewebpage" do
-  version "0.13.3"
-  sha256 "272f8234e2fcde0d552e3929eee30fb9cb6a2407778d01d002ce625580b7d4b7"
+  version "0.14.0"
+  sha256 "9a0dc1d8bf8cbc0e19cf58cd67a8107c4ae0c5a187d0dea148f8940e6aad25d1"
 
   url "https://github.com/webrecorder/archiveweb.page/releases/download/v#{version}/ArchiveWeb.page-#{version}.dmg",
       verified: "github.com/webrecorder/archiveweb.page/"
