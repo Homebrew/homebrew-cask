0458d4ea01cd670802408c6314ed8f44e417d22a
Yurii Kolesnykov <root@yurikoles.com>
2024-11-21 20:40:20 +0000

    google-drive 100.0.2
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/g/google-drive.rb
+++ Casks/g/google-drive.rb
@@ -1,5 +1,5 @@
 cask "google-drive" do
-  version "100.0"
+  version "100.0.2"
   sha256 :no_check
 
   # "5-percent" is included in the url to ensure that `brew upgrade` does not update to an older version as the
