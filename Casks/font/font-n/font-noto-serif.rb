dbfbf6df33d8be49c61029404a7277d4fc567d85
Daeho Ro <lamanus@outlook.kr>
2024-11-23 02:27:49 +0900

    font-noto-serif 2.015

--- Casks/font/font-n/font-noto-serif.rb
+++ Casks/font/font-n/font-noto-serif.rb
@@ -1,6 +1,6 @@
 cask "font-noto-serif" do
-  version "2.014"
-  sha256 "6abce0a80df4ef6d5a944d60c81099364481d6a7015b0721d87bc4c16acc1fd3"
+  version "2.015"
+  sha256 "0e9a43c8a4b94ac76f55069ed1d7385bbcaf6b99527a94deb5619e032b7e76c1"
 
   url "https://github.com/notofonts/latin-greek-cyrillic/releases/download/NotoSerif-v#{version}/NotoSerif-v#{version}.zip",
       verified: "github.com/notofonts/"
