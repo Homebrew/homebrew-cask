8376f46833083f6457feea9411310e27f259a567
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-24 00:15:20 +0000

    font-edu-au-vic-wa-nt-arrows: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

--- Casks/font/font-e/font-edu-au-vic-wa-nt-arrows.rb
+++ Casks/font/font-e/font-edu-au-vic-wa-nt-arrows.rb
@@ -2,9 +2,10 @@ cask "font-edu-au-vic-wa-nt-arrows" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/eduauvicwantarrows/EduAUVICWANTArrows%5Bwght%5D.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/eduauvicwantarrows/EduAUVICWANTArrows%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Edu AU VIC WA NT Arrows"
-  homepage "https://github.com/SorkinType/VICWANTSchoolhandAustralia"
+  homepage "https://fonts.google.com/specimen/Edu+AU+VIC+WA+NT+Arrows"
 
   font "EduAUVICWANTArrows[wght].ttf"
 
