28210a484a46d485de9ffb153292502355452a36
Klaus Hipp <khipp@users.noreply.github.com>
2024-11-25 13:44:13 +0100

    micro-snitch: various updates

--- Casks/m/micro-snitch.rb
+++ Casks/m/micro-snitch.rb
@@ -2,18 +2,23 @@ cask "micro-snitch" do
   version "1.6.1"
   sha256 "700a265156ae7c8138d7764a0ce87feba098bcaf77deed9b8d9aaa0b3c4fa197"
 
-  url "https://www.obdev.at/downloads/MicroSnitch/MicroSnitch-#{version}.dmg"
+  url "https://sw-update.obdev.at/ftp/pub/Products/MicroSnitch/MicroSnitch-#{version}.dmg"
   name "Micro Snitch"
   desc "Monitors and reports any microphone and camera activity"
   homepage "https://www.obdev.at/products/microsnitch/index.html"
 
   livecheck do
-    url "https://www.obdev.at/products/microsnitch/download.html"
-    regex(%r{href=.*?/MicroSnitch[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
+    url "https://sw-update.obdev.at/update-feeds/microsnitch-2.plist"
+    strategy :xml do |xml|
+      version = xml.elements["//key[text()='BundleShortVersionString']"]&.next_element&.text
+      next if version.blank?
+
+      version.strip
+    end
   end
 
   auto_updates true
-  depends_on macos: ">= :el_capitan"
+  depends_on macos: ">= :mojave"
 
   app "Micro Snitch.app"
 
