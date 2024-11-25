f9249418d5f970d370f45eb5e63b0f29d30e3fcd
Klaus Hipp <khipp@users.noreply.github.com>
2024-11-25 19:01:39 +0100

    avtouchbar: update livecheck

--- Casks/a/avtouchbar.rb
+++ Casks/a/avtouchbar.rb
@@ -1,17 +1,20 @@
 cask "avtouchbar" do
-  version "3.0.7,2021.08"
+  version "3.0.7,2021,08"
   sha256 "d04c1a6685e7ce59e10bae4464b6a113aa7bf302f5b515768055aa8326ecb8e1"
 
-  url "https://www.avtouchbar.com/wp-content/uploads/#{version.csv.second.major}/#{version.csv.second.minor}/AVTouchBar-#{version.csv.first}.zip"
+  url "https://www.avtouchbar.com/wp-content/uploads/#{version.csv.second}/#{version.csv.third}/AVTouchBar-#{version.csv.first}.zip"
   name "AVTouchBar"
   desc "Audio Visualiser for the Touch Bar"
   homepage "https://www.avtouchbar.com/"
 
   livecheck do
-    url "https://www.avtouchbar.com/downloads/"
-    regex(%r{href=.*?/uploads/(\d+)/(\d+)/AVTouchBar[._-]?(\d+(?:\.\d+)+)\.zip}i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| "#{match[2]},#{match[0]}.#{match[1]}" }
+    url "https://avtouchbar.com/avtouchbar.xml"
+    regex(%r{/(\d+)/(\d+)/[^/]+$}i)
+    strategy :sparkle do |item, regex|
+      match = item.url.match(regex)
+      next if match.blank?
+
+      "#{item.short_version},#{match[1]},#{match[2]}"
     end
   end
 
