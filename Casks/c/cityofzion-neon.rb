b1e9fdc642cf1d7d18f12da268349366d5d672ed
Klaus Hipp <khipp@users.noreply.github.com>
2024-11-25 12:49:17 +0100

    cityofzion-neon 2.25.0

--- Casks/c/cityofzion-neon.rb
+++ Casks/c/cityofzion-neon.rb
@@ -1,9 +1,9 @@
 cask "cityofzion-neon" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.24.1"
-  sha256 arm:   "c91a9dea23889bc62513fb947fb31e6ff15b7e9d26e053dce117012791bfa684",
-         intel: "16cc311213328d38512031a436fb1c1a20172482bfe514d5986fd50b999101c5"
+  version "2.25.0"
+  sha256 arm:   "68ef2daa7e0f6d415dbb3101cfd520b04a01cd9d303b11df44508f16406f42b0",
+         intel: "d21fdab52ed49861b85861b7ce2cd6823489db140b2e9c1864a6608c518ce5c4"
 
   url "https://github.com/CityOfZion/neon-wallet/releases/download/v#{version}/Neon.#{version}.#{arch}.dmg"
   name "Neon Wallet"
