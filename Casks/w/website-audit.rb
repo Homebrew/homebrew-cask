c3d84b3ff0047c85aba5f9a1fe510cc9010d0de6
viking1304 <viking1304@gmail.com>
2024-11-26 21:50:02 +0100

    website-audit 1.2.5 (new-cask)

--- Casks/w/website-audit.rb
+++ Casks/w/website-audit.rb
@@ -4,18 +4,20 @@ cask "website-audit" do
 
   url "https://code.europa.eu/api/v4/projects/615/packages/generic/wat/#{version}/website-audit-#{version}-universal.dmg"
   name "Website Audit"
-  desc "Analyse whether websites are compliant with the law"
+  desc "Analyze whether websites comply with GDPR according to EDPB guidelines"
   homepage "https://code.europa.eu/edpb/website-auditing-tool"
 
   livecheck do
-    url "https://code.europa.eu/edpb/website-auditing-tool/-/tags?format=atom"
-    strategy :atom
-    regex(/website-audit-(\d+(?:\.\d+)*)/i)
+    url "https://code.europa.eu/edpb/website-auditing-tool.git"
   end
 
   depends_on macos: ">= :catalina"
 
   app "website-audit.app"
 
-  zap trash: "~/Library/Application Support/website-audit"
+  zap trash: [
+    "~/Library/Application Support/website-audit",
+    "~/Library/Preferences/com.edpb.wat-.plist",
+    "~/Library/Saved Application State/com.edpb.wat-.savedState",
+  ]
 end
