fc72969c4308b8852ba17318c68c9d153b1201c1
Klaus Hipp <khipp@users.noreply.github.com>
2024-11-26 15:49:51 +0100

    feed-the-beast: update livecheck

--- Casks/f/feed-the-beast.rb
+++ Casks/f/feed-the-beast.rb
@@ -1,5 +1,6 @@
 cask "feed-the-beast" do
   arch arm: "arm64", intel: "x64"
+  livecheck_arch = on_arch_conditional arm: "arm", intel: "x64"
 
   version "1.26.3"
   sha256 arm:   "810bcd8b868791667db69da9aefd5bf6d49d4e722439df4a0c4cf72eb2ad8cbe",
@@ -13,6 +14,12 @@ cask "feed-the-beast" do
   livecheck do
     url "https://meta.feed-the-beast.com/v1/app/versions"
     regex(/ftb[._-]app[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}\.dmg/i)
+    strategy :json do |json, regex|
+      match = json.dig("macos", livecheck_arch, "dmg", "url")&.match(regex)
+      next if match.blank?
+
+      match[1]
+    end
   end
 
   auto_updates true
