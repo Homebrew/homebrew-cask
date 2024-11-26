985ac53080ae932a2dba3101039678822da0a2e3
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-11-26 11:21:59 -0500

    synology-surveillance-station-client 2.2.1

--- Casks/s/synology-surveillance-station-client.rb
+++ Casks/s/synology-surveillance-station-client.rb
@@ -1,6 +1,6 @@
 cask "synology-surveillance-station-client" do
-  version "2.2.0,2507"
-  sha256 "bf0b6f1a2a01ab685132fc92e1b86337515377bcabd74a2aba1782b076fedf18"
+  version "2.2.1,2564"
+  sha256 "ca5ed3645a1303230d66cbd0bc160c58af88db346e5266f06da0b2d3444d86e9"
 
   url "https://global.download.synology.com/download/Utility/SurveillanceStationClient/#{version.tr(",", "-")}/Mac/Synology%20Surveillance%20Station%20Client-#{version.tr(",", "-")}.dmg"
   name "Synology Surveillance Station Client"
