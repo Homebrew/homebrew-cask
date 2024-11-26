6dd9390783781a7b15811cb00f1011e0a9b5994c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-26 18:59:46 +0000

    sync 2.2.48

--- Casks/s/sync.rb
+++ Casks/s/sync.rb
@@ -1,6 +1,6 @@
 cask "sync" do
-  version "2.2.47"
-  sha256 "e04bdea8a60ca119921ac7616767bbba29b92cf83844f777ea5c6c2c3f3c6261"
+  version "2.2.48"
+  sha256 "9fec22de7091b64186cefac6f99c312df213fcc46034ae6e4c1631184cd97063"
 
   url "https://www.sync.com/download/apple/Sync-#{version}.dmg"
   name "Sync"
