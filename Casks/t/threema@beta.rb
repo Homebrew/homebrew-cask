191a72f1d2db6f202d4d489354e7133490fdb35c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-26 13:08:28 +0000

    threema@beta 2.0-beta45

--- Casks/t/threema@beta.rb
+++ Casks/t/threema@beta.rb
@@ -1,9 +1,9 @@
 cask "threema@beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.0-beta44"
-  sha256 arm:   "2759c6e2e8ba896bc844f1b619e7bf5a590a057104443c8675eaf771aed15ef8",
-         intel: "66bde64cc7cca5acdae3e36a9c7bc0e4fe8866bb169ed6d76f4fc88cc1cc780b"
+  version "2.0-beta45"
+  sha256 arm:   "a6caf240e85c78ab9c5290702a61477a6e2acac9a8065d5c9a9336239d87e517",
+         intel: "28efb172c42903767c7a71322fc02a2d1fdef3f61aeb931bad075d3566b0987b"
 
   url "https://releases.threema.ch/desktop/#{version}/threema-desktop-v#{version}-macos-#{arch}.dmg"
   name "Threema"
