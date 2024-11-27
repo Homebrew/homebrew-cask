730c295878e4eab62b887f381899548c0a95da32
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-27 09:56:27 +0000

    universal-media-server 14.7.0

--- Casks/u/universal-media-server.rb
+++ Casks/u/universal-media-server.rb
@@ -1,9 +1,9 @@
 cask "universal-media-server" do
   arch arm: "-arm"
 
-  version "14.6.0"
-  sha256 arm:   "8e059221ab28856343de39ee469e6f6cc1179c8a19418585dabcfc84405f64a6",
-         intel: "8867bd64de9fa03f0c05ffe34e6111f1a8a362cc29506ea78fb56814f6a5250f"
+  version "14.7.0"
+  sha256 arm:   "d88733f0281526e7002f37858050c9d3610f08bda8d51084bf71673702a42a96",
+         intel: "041a130ed8426b262c1121b3a4be4b0bcc583e4510a50ea0f1f9a43e6e184c26"
 
   url "https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/#{version}/UMS-#{version}#{arch}.dmg",
       verified: "github.com/UniversalMediaServer/UniversalMediaServer/"
