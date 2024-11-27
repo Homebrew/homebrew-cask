cfb45074cb97e6cdac0467f652004c2a07f575ac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-27 18:55:23 +0000

    fixkey 1.9.9

--- Casks/f/fixkey.rb
+++ Casks/f/fixkey.rb
@@ -1,6 +1,6 @@
 cask "fixkey" do
-  version "1.5.0"
-  sha256 "52e322d6b1375e68e716e1abd62f60ae6d740a7f0316c83a2614ec528342615a"
+  version "1.9.9"
+  sha256 "9ca2de9079166a1c215dacc3f9590ace0d83c2c487c7a0bf1c5f3e6c9f974241"
 
   url "https://fixkey.download/fixkey%20#{version}.dmg",
       verified: "fixkey.download/"
