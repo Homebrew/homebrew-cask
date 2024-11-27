2ba2c3013dbb213b27cdda18918295131183c72a
Joe Wicentowski <joewiz@gmail.com>
2024-11-27 09:51:28 -0500

    oxygen-xml-editor 27.0

--- Casks/o/oxygen-xml-editor.rb
+++ Casks/o/oxygen-xml-editor.rb
@@ -1,5 +1,5 @@
 cask "oxygen-xml-editor" do
-  version "26.1,2024091606"
+  version "27.0,2024112212"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://archives.oxygenxml.com/Oxygen/Editor/InstData#{version.csv.first}/MacOSX/VM/oxygen-openjdk.dmg"
