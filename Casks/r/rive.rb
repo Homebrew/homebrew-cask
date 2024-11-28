fd8fd425de2ef1d5e955b0c2ca1e0f924ed163ae
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-28 00:59:42 +0000

    rive 0.8.2233

--- Casks/r/rive.rb
+++ Casks/r/rive.rb
@@ -1,6 +1,6 @@
 cask "rive" do
-  version "0.8.2207"
-  sha256 "0b33e7d35e26f422fda270f6c42e29b4911c741d4a466232a0793adc02ec7720"
+  version "0.8.2233"
+  sha256 "726c6e1fe7f567e5a2d35c47427d8c7355e5731325983b379141299b0f5355d3"
 
   url "https://releases.rive.app/macos/#{version}/Rive.dmg"
   name "Rive"
