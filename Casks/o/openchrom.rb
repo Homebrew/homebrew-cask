f259730bfbbdd6e83015def3b7c1b34e2909e254
Jenkins <qa@lablicate.com>
2024-11-29 12:18:54 +0000

    openchrom 1.5.0

--- Casks/o/openchrom.rb
+++ Casks/o/openchrom.rb
@@ -2,8 +2,8 @@ cask "openchrom" do
   arch arm: "aarch64", intel: "x86_64"
 
   version "1.5.0"
-  sha256 arm:   "7fe8d108f47eda1d49b76135be4d2d5a48fe316ce0f945a2fa6df4ccccd7f740",
-         intel: "a09f7f1b2cbf69696149cd5a2db76e445780de1b5f95d28d7b9f71c07cfe4f97"
+  sha256 arm:   "ec7f5b523eec502486ce1ca3248ece5d648848998e555ec762441eac5ed5b7cc",
+         intel: "ce1cc403ed3a1b0bf378bc18aea1344460b01bc53360e2eaa4ae4fe6f9992a68"
 
   url "https://products.lablicate.com/openchrom/#{version}/OpenChrom_#{version}_#{arch}.dmg",
       verified: "products.lablicate.com/openchrom/"
