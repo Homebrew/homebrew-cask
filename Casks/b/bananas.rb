758906461d58b8d7c2ae9b11cc8ba421179ae369
bobbymannino <manninobobby@icloud.com>
2024-11-30 14:25:01 +0000

    bananas 0.0.11
    - changed min version to big sur
    - added zap dir

--- Casks/b/bananas.rb
+++ Casks/b/bananas.rb
@@ -3,17 +3,21 @@ cask "bananas" do
   sha256 "c70ba0b7d2f334d71e2d263f6c60b7d4d537a4b1206331757775af99a5ccc977"
 
   url "https://github.com/mistweaverco/bananas/releases/download/v#{version}/bananas_universal.dmg",
-    verified: "github.com/mistweaverco/bananas/"
+      verified: "github.com/mistweaverco/bananas/"
   name "bananas"
   desc "Cross-Platform screen sharing made simple"
-  homepage "https://getbananas.net"
+  homepage "https://getbananas.net/"
 
   livecheck do
     url :url
     strategy :github_latest
   end
 
-  depends_on macos: ">= :catalina"
+  depends_on macos: ">= :big_sur"
 
   app "bananas.app"
+
+  zap trash: [
+      "~/Library/Application Support/bananas",
+  ]
 end
