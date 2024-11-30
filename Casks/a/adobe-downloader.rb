499a0775f76524db1bfa0a55275463800fdb4ac7
i0ntempest <i0ntempest@i0ntempest.com>
2024-12-01 01:26:37 +0800

    adobe-downloader: new cask

---
+++ Casks/a/adobe-downloader.rb
@@ -0,0 +1,24 @@
+cask "adobe-downloader" do
+  version "1.3.1"
+  sha256 "a114fa39d8390c9bc48acb253637b94e7f1a57b17e53301a3c9be50a8c1f50c5"
+
+  url "https://github.com/X1a0He/Adobe-Downloader/releases/download/#{version}/Adobe.Downloader.dmg"
+  name "Adobe Downloader"
+  desc "Adobe product package downloader"
+  homepage "https://github.com/X1a0He/Adobe-Downloader"
+
+  depends_on macos: ">= :monterey"
+
+  app "Adobe Downloader.app"
+
+  zap trash: [
+    "/Library/LaunchDaemons/com.x1a0he.macOS.Adobe-Downloader.helper.plist",
+    "/Library/PrivilegedHelperTools/com.x1a0he.macOS.Adobe-Downloader.helper",
+    "~/Library/Application Support/Adobe Downloader",
+    "~/Library/Caches/com.x1a0he.macOS.Adobe-Downloader",
+    "~/Library/HTTPStorages/com.x1a0he.macOS.Adobe-Downloader",
+    "~/Library/HTTPStorages/com.x1a0he.macOS.Adobe-Downloader.binarycookies",
+    "~/Library/Preferences/com.x1a0he.macOS.Adobe-Downloader.plist",
+    "~/Library/Saved Application State/com.x1a0he.macOS.Adobe-Downloader.savedState",
+  ]
+end
