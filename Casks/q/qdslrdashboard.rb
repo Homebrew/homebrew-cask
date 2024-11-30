a214776aeadafb988324dd175a2039aee3256675
Klaus Hipp <khipp@users.noreply.github.com>
2024-11-30 22:09:23 +0100

    qdslrdashboard: update livecheck

--- Casks/q/qdslrdashboard.rb
+++ Casks/q/qdslrdashboard.rb
@@ -10,8 +10,12 @@ cask "qdslrdashboard" do
 
   livecheck do
     url "https://dslrdashboard.info/downloads/"
-    strategy :page_match do |page|
-      page[%r{href=.*?/qdslrdashboard[._-]v?(\d+(?:-\d+)*)[._-]macos}i, 1].tr("-", ".")
+    regex(/href=.*?qdslrdashboard[._-]v?(\d+(?:[.-]\d+)+)[._-]macos/i)
+    strategy :page_match do |page, regex|
+      match = page.match(regex)
+      next if match.blank?
+
+      match[1].tr("-", ".")
     end
   end
 
