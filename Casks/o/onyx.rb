e178008af020cff92567807e330b08f17fa4e745
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-11-29 19:39:39 -0500

    onyx 4.7.2

--- Casks/o/onyx.rb
+++ Casks/o/onyx.rb
@@ -47,7 +47,7 @@ cask "onyx" do
     url "https://www.titanium-software.fr/download/14/OnyX.dmg"
   end
   on_sequoia do
-    version "4.7.1"
+    version "4.7.2"
 
     url "https://www.titanium-software.fr/download/15/OnyX.dmg"
   end
