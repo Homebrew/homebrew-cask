edd1b5504d7aefbef73d1e2eab3dcae6ecddcbd3
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-12-01 10:00:15 -0500

    librepcb 1.2.0
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/l/librepcb.rb
+++ Casks/l/librepcb.rb
@@ -1,15 +1,18 @@
 cask "librepcb" do
-  version "1.1.0"
-  sha256 "c180b82c82d25d041429ada111ee1cb5d8aa892411064e60fa8bb8f5ec945e6c"
+  arch arm: "arm64", intel: "x86_64"
 
-  url "https://download.librepcb.org/releases/#{version}/librepcb-#{version}-mac-x86_64.dmg"
+  version "1.2.0"
+  sha256 arm:   "30d2c18589bb8bbb75c6d286c8a4933d473904ac0015d0ba598f9afebf2cf9c7",
+         intel: "69f84d12499a7a822fadc7732d773effbe0809e13ebccb2894fc56ee996d2cf1"
+
+  url "https://download.librepcb.org/releases/#{version}/librepcb-#{version}-mac-#{arch}.dmg"
   name "LibrePCB"
   desc "EDA software to develop printed circuit boards"
   homepage "https://librepcb.org/"
 
   livecheck do
     url "https://librepcb.org/download/"
-    regex(%r{href=.*?/librepcb[._-]v?(\d+(?:\.\d+)+)-mac-x86_64\.dmg}i)
+    regex(%r{href=.*?/librepcb[._-]v?(\d+(?:\.\d+)+)-mac-#{arch}\.dmg}i)
   end
 
   app "librepcb.app"
@@ -19,8 +22,4 @@ cask "librepcb" do
     "~/Library/Saved Application State/com.yourcompany.librepcb.savedState",
     "~/Library/Saved Application State/org.librepcb.LibrePCB.savedState",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
