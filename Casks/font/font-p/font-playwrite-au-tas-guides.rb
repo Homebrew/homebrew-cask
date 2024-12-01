26293ad2e96d50f85fd56e34b9d5307f38c929e5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-01 00:17:10 +0000

    font-playwrite-au-tas-guides: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

---
+++ Casks/font/font-p/font-playwrite-au-tas-guides.rb
@@ -0,0 +1,12 @@
+cask "font-playwrite-au-tas-guides" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/playwriteautasguides/PlaywriteAUTASGuides-Regular.ttf"
+  name "Playwrite AU TAS Guides"
+  homepage "https://github.com/TypeTogether/Playwrite"
+
+  font "PlaywriteAUTASGuides-Regular.ttf"
+
+  # No zap stanza required
+end
