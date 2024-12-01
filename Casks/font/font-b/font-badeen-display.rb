632d2e9a4658a842b99b9b987c459daa9471e874
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-01 00:17:08 +0000

    font-badeen-display: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

---
+++ Casks/font/font-b/font-badeen-display.rb
@@ -0,0 +1,12 @@
+cask "font-badeen-display" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/badeendisplay/BadeenDisplay-Regular.ttf"
+  name "Badeen Display"
+  homepage "https://github.com/haniadnansd/Badeen-Display"
+
+  font "BadeenDisplay-Regular.ttf"
+
+  # No zap stanza required
+end
