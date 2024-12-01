2e40c8420287c722606ae9a5a185482cbfe94e62
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-01 00:17:10 +0000

    font-playwrite-au-qld-guides: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

---
+++ Casks/font/font-p/font-playwrite-au-qld-guides.rb
@@ -0,0 +1,12 @@
+cask "font-playwrite-au-qld-guides" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/playwriteauqldguides/PlaywriteAUQLDGuides-Regular.ttf"
+  name "Playwrite AU QLD Guides"
+  homepage "https://github.com/TypeTogether/Playwrite"
+
+  font "PlaywriteAUQLDGuides-Regular.ttf"
+
+  # No zap stanza required
+end
