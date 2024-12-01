d83d31d70fb8293a00f05ec7d50236e6ef63c9ed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-01 00:17:15 +0000

    font-playwrite-dk-uloopet-guides: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

---
+++ Casks/font/font-p/font-playwrite-dk-uloopet-guides.rb
@@ -0,0 +1,12 @@
+cask "font-playwrite-dk-uloopet-guides" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/playwritedkuloopetguides/PlaywriteDKUloopetGuides-Regular.ttf"
+  name "Playwrite DK Uloopet Guides"
+  homepage "https://github.com/TypeTogether/Playwrite"
+
+  font "PlaywriteDKUloopetGuides-Regular.ttf"
+
+  # No zap stanza required
+end
