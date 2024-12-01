55da3fe34de4f6def28e5f2031440ae73f27cc24
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-01 00:17:18 +0000

    font-playwrite-it-trad-guides: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

---
+++ Casks/font/font-p/font-playwrite-it-trad-guides.rb
@@ -0,0 +1,12 @@
+cask "font-playwrite-it-trad-guides" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/playwriteittradguides/PlaywriteITTradGuides-Regular.ttf"
+  name "Playwrite IT Trad Guides"
+  homepage "https://github.com/TypeTogether/Playwrite"
+
+  font "PlaywriteITTradGuides-Regular.ttf"
+
+  # No zap stanza required
+end
