cb6f93751448d1303ce50e0f30f9f6024f16aa8c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-01 00:17:23 +0000

    font-playwrite-vn-guides: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

---
+++ Casks/font/font-p/font-playwrite-vn-guides.rb
@@ -0,0 +1,12 @@
+cask "font-playwrite-vn-guides" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/playwritevnguides/PlaywriteVNGuides-Regular.ttf"
+  name "Playwrite VN Guides"
+  homepage "https://github.com/TypeTogether/Playwrite"
+
+  font "PlaywriteVNGuides-Regular.ttf"
+
+  # No zap stanza required
+end
