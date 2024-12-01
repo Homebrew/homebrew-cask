bc3162c7b602ae969b9ae09c7ebf383f107b0e7f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-01 00:17:11 +0000

    font-playwrite-be-vlg-guides: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

---
+++ Casks/font/font-p/font-playwrite-be-vlg-guides.rb
@@ -0,0 +1,12 @@
+cask "font-playwrite-be-vlg-guides" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/playwritebevlgguides/PlaywriteBEVLGGuides-Regular.ttf"
+  name "Playwrite BE VLG Guides"
+  homepage "https://github.com/TypeTogether/Playwrite"
+
+  font "PlaywriteBEVLGGuides-Regular.ttf"
+
+  # No zap stanza required
+end
