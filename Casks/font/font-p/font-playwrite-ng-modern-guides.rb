e6c3b13e5a69d30546706c90db9aa307a3e61223
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-01 00:17:19 +0000

    font-playwrite-ng-modern-guides: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

---
+++ Casks/font/font-p/font-playwrite-ng-modern-guides.rb
@@ -0,0 +1,12 @@
+cask "font-playwrite-ng-modern-guides" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/playwritengmodernguides/PlaywriteNGModernGuides-Regular.ttf"
+  name "Playwrite NG Modern Guides"
+  homepage "https://github.com/TypeTogether/Playwrite"
+
+  font "PlaywriteNGModernGuides-Regular.ttf"
+
+  # No zap stanza required
+end
