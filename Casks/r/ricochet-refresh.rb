41a132b220456490ba84b078d9f4767d18aa9aed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-02 03:55:43 +0000

    ricochet-refresh 3.0.29

--- Casks/r/ricochet-refresh.rb
+++ Casks/r/ricochet-refresh.rb
@@ -1,9 +1,9 @@
 cask "ricochet-refresh" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "3.0.28"
-  sha256 arm:   "d6fd6f19ee7987672532d9884541d6c01ef0229f7d2c9917a85f1861db5c00a6",
-         intel: "bc1edaaa556c97d1fac63863df5cca2b3bc5971dd121d4678f303cad109424ed"
+  version "3.0.29"
+  sha256 arm:   "59d811eb38605a4d0267e5a5ea420dc770a0618488495f44032a56c8fadf6d12",
+         intel: "8a8f0ae09af2cf84e25d0bd0c87b3dc5b00469e3d470e60ec55612df58c1c267"
 
   url "https://github.com/blueprint-freespeech/ricochet-refresh/releases/download/v#{version}-release/ricochet-refresh-#{version}-macos-#{arch}.dmg",
       verified: "github.com/blueprint-freespeech/ricochet-refresh/"
