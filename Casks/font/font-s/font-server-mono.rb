50963217fd40c9f35a305d2ebf04f7124b126b38
Sam Ford <1584702+samford@users.noreply.github.com>
2024-12-02 00:11:21 -0500

    font-server-mono 0.0.5

--- Casks/font/font-s/font-server-mono.rb
+++ Casks/font/font-s/font-server-mono.rb
@@ -1,6 +1,6 @@
 cask "font-server-mono" do
-  version "0.0.4"
-  sha256 "677169c9ed971ccafea0d55f4f34f3784e61a3b848784e12247608b344f477c4"
+  version "0.0.5"
+  sha256 "ba94c13607a98a8bf303e785c5cdf03d90d77089469c68ccbb4f4f4564f21719"
 
   url "https://github.com/internet-development/www-server-mono/archive/refs/tags/#{version}.tar.gz",
       verified: "github.com/internet-development/www-server-mono/"
