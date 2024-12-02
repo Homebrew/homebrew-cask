b4acfe8f086eb9c1b59e63277a62b089b9bf5352
bevanjkay <email@bevankay.me>
2024-12-02 18:12:52 +1100

    bisq 1.9.18

--- Casks/b/bisq.rb
+++ Casks/b/bisq.rb
@@ -1,6 +1,6 @@
 cask "bisq" do
-  version "1.9.17"
-  sha256 "4ae9f1d1e8655616e4115a4038230248db4d2d238af19780d2f25fff576b0888"
+  version "1.9.18"
+  sha256 "522996cf070f0eb50d4f7f4872e6b4907351616e8e92b7215fb867d4a366d284"
 
   url "https://github.com/bisq-network/bisq/releases/download/v#{version}/Bisq-#{version}.dmg",
       verified: "github.com/bisq-network/bisq/"
