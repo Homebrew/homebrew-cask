96c159904198add826558c562e20164bb8e7e514
Aadit Kamat <aadit.kamat@ufl.edu>
2024-12-02 12:46:21 -0500

    Fix issues

--- Casks/f/firework.rb
+++ Casks/f/firework.rb
@@ -2,26 +2,19 @@ cask "firework" do
   version "3.39.13"
   sha256 :no_check
 
-  url "https://cdn-firework.com/fw/download/packages/FireworkInstaller.dmg"
+  url "https://cdn-firework.com/fw/download/packages/FireworkInstaller.dmg",
+      verified: "cdn-firework.com/"
   name "firework"
   desc "Easiest access to web sites and applications"
-  homepage "https://firework.cloud"
-
-  livecheck do
-    url :url
-    strategy :extract_plist
-  end
-
-  depends_on macos: ">= :yosemite"
+  homepage "https://firework.cloud/"
 
   app "Firework.app"
 
   zap trash: [
-    "~/.config/configstore/flow-desktop.json",
+    "~/Library/Application Support/Crash Reporter/Firework_61109777-DE87-5166-AC98-88DCB257BB59.plist",
     "~/Library/Application Support/Firework",
     "~/Library/Caches/Firework",
-    "~/Library/Saved Application State/cloud.firework.savedState",
-    "~/Library/Application Support/Crash Reporter/Firework_61109777-DE87-5166-AC98-88DCB257BB59.plist",
     "~/Library/Preferences/cloud.firework.plist",
+    "~/Library/Saved Application State/cloud.firework.savedState",
   ]
 end
