f5d53ad7c9d37f4fab4f7260bb1b85c60513d64e
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-12-03 20:05:25 -0500

    lunatask 2.0.14

--- Casks/l/lunatask.rb
+++ Casks/l/lunatask.rb
@@ -1,24 +1,12 @@
 cask "lunatask" do
-  version "2.0.13"
-  sha256 "1b4038cb59b0f33ebdb9e1a08d20f4321d6c8542b5536270229be0249f0e734f"
+  version "2.0.14"
+  sha256 "e3faf532a9e4338cc57b160488dd4efd29f4c70abe7f6dc9f7db00d5e5350599"
 
-  url "https://lunatask.app/download/Lunatask-#{version}-universal.dmg"
+  url "https://github.com/lunatask/lunatask/releases/download/v#{version}/Lunatask-#{version}-universal.dmg",
+      verified: "github.com/lunatask/lunatask/"
   name "Lunatask"
   desc "Encrypted to-do list, habit tracker, journaling, life-tracking and notes app"
   homepage "https://lunatask.app/"
-
-  livecheck do
-    url "https://lunatask.app/releases/atom.xml"
-    regex(%r{releases/(\d+(?:\.\d+)+)}i)
-    strategy :xml do |xml, regex|
-      xml.get_elements("//id").map do |item|
-        match = item.text&.strip&.match(regex)
-        next if match.blank?
-
-        match[1]
-      end
-    end
-  end
 
   depends_on macos: ">= :catalina"
 
