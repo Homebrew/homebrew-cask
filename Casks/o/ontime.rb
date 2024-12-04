a3a83ca0414810c109c62309d49c86428710345f
Daeho Ro <lamanus@outlook.kr>
2024-12-05 00:47:47 +0900

    ontime 3.9.1

--- Casks/o/ontime.rb
+++ Casks/o/ontime.rb
@@ -1,9 +1,9 @@
 cask "ontime" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.8.0"
-  sha256 arm:   "02f1023bdea737120f4d4f25ab9c841c0dc9ca176bee472241a83038749c7249",
-         intel: "aebf4d20607bba6016b876fc9bd2e39575fa3db7e98ab99b6b0d7729a943adeb"
+  version "3.9.1"
+  sha256 arm:   "00614c97432c5d2704f8fa2d64ae60bdba17b73853101b377144dea54703f94c",
+         intel: "ea1ca30c96004110583f441b93128fe774da4d3a1f352d3c9be78720d64441b4"
 
   url "https://github.com/cpvalente/ontime/releases/download/v#{version}/ontime-macOS-#{arch}.dmg",
       verified: "github.com/cpvalente/ontime/"
