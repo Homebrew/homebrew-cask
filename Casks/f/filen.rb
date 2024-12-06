8ba6d0d95f53357dcc51816c008b493d6c385425
scollovati <20740642+scollovati@users.noreply.github.com>
2024-12-06 14:35:51 +0100

    Update Casks/f/filen.rb
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/f/filen.rb
+++ Casks/f/filen.rb
@@ -2,8 +2,7 @@ cask "filen" do
   arch arm: "arm64", intel: "x64"
 
   version "3.0.40"
-  sha256 arm:   "dfb0a0c11e07a97567785c6be5fc53142256cdd68eb4fd6f9bb30a531ec94fc5",
-         intel: "4a5b644c632e49d0abe65957bb7a513ebb83753acfd1b8978af29602e085c6c9"
+  sha256 :no_check
 
   url "https://cdn.filen.io/@filen/desktop/release/latest/Filen_mac_#{arch}.dmg"
   name "Filen"
