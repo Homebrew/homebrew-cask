0456d6a33bc957f25fb000d0148f34346803df12
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-12-06 18:14:26 -0500

    eclipse-jee 4.34.0

--- Casks/e/eclipse-jee.rb
+++ Casks/e/eclipse-jee.rb
@@ -1,9 +1,9 @@
 cask "eclipse-jee" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "4.33.0,2024-09"
-  sha256 arm:   "8fb905dd622ec5b2a312dbe4be20b14e3047fce00dcad8e28b405207673f3292",
-         intel: "a2f3ec39eca8ceb8cd425fef276ed8978283e1320fb8caf86a05f61d0640259f"
+  version "4.34.0,2024-12"
+  sha256 arm:   "4386f42df8fe099d8a87212ceff157e29142ceac4b94b03f22abed88bde1e838",
+         intel: "f799a8adf964674e02491309eaf6f8a2f63aea11a61ca3d582e70e2dbe6b7562"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-jee-#{version.csv.second}-R-macosx-cocoa-#{arch}.dmg&mirror_id=1"
   name "Eclipse IDE for Java EE Developers"
