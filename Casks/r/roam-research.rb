70bac9838a81a2e443964c98bec0a434061bfb0b
bevanjkay <email@bevankay.me>
2024-12-07 11:03:11 +1100

    roam-research 0.0.23

--- Casks/r/roam-research.rb
+++ Casks/r/roam-research.rb
@@ -1,9 +1,9 @@
 cask "roam-research" do
   arch arm: "-arm64"
 
-  version "0.0.22"
-  sha256 arm:   "fa5815e53a53ccdee626fbd512905b9aaf9a04e04acb50a962430d85fa3184ad",
-         intel: "180f66e33fb70f2f3b0f3fd83c6e4c61b44443570401d5ff309be4009fdf7b7e"
+  version "0.0.23"
+  sha256 arm:   "d3d401941669e6a5d5d8c1b55b9e73e0f092c5afda62ce1950f831fa97ae91a0",
+         intel: "b6c45ffdd492e5a5566d9cee77c37b2344555c857caaffd9156723ce2e44ca18"
 
   url "https://roam-electron-deploy.s3.amazonaws.com/Roam+Research-#{version}#{arch}.dmg",
       verified: "roam-electron-deploy.s3.amazonaws.com/"
