33a0e81c9266cb15294eeb38b7345d74f9e96188
Brian Kuzmanoski <bkuzmanoski@icloud.com>
2024-12-07 17:33:51 +1100

    font-geist: update checksum, update artifact stanzas

--- Casks/font/font-g/font-geist.rb
+++ Casks/font/font-g/font-geist.rb
@@ -1,6 +1,6 @@
 cask "font-geist" do
   version "1.4.01"
-  sha256 "b013dce423d16c03238593f6b8e0a281322bafdc2a75b376a91de8721cf18315"
+  sha256 "d12b5e123bdd5a9facbd52ab6a24756587b086d6a76fc629a28456675bfad4e3"
 
   url "https://github.com/vercel/geist-font/releases/download/#{version}/Geist-v#{version}.zip",
       verified: "github.com/vercel/geist-font/"
@@ -12,16 +12,16 @@ cask "font-geist" do
     strategy :github_latest
   end
 
-  font "otf/Geist-Black.otf"
-  font "otf/Geist-Bold.otf"
-  font "otf/Geist-ExtraBold.otf"
-  font "otf/Geist-ExtraLight.otf"
-  font "otf/Geist-Light.otf"
-  font "otf/Geist-Medium.otf"
-  font "otf/Geist-Regular.otf"
-  font "otf/Geist-SemiBold.otf"
-  font "otf/Geist-Thin.otf"
-  font "variable/Geist[wght].ttf"
+  font "Geist-v#{version}/otf/Geist-Black.otf"
+  font "Geist-v#{version}/otf/Geist-Bold.otf"
+  font "Geist-v#{version}/otf/Geist-ExtraBold.otf"
+  font "Geist-v#{version}/otf/Geist-ExtraLight.otf"
+  font "Geist-v#{version}/otf/Geist-Light.otf"
+  font "Geist-v#{version}/otf/Geist-Medium.otf"
+  font "Geist-v#{version}/otf/Geist-Regular.otf"
+  font "Geist-v#{version}/otf/Geist-SemiBold.otf"
+  font "Geist-v#{version}/otf/Geist-Thin.otf"
+  font "Geist-v#{version}/variable/Geist[wght].ttf"
 
   # No zap stanza required
 end
