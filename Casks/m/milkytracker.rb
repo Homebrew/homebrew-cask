2581f793ae5771f0cc37be473ab2972cae922474
Daeho Ro <lamanus@outlook.kr>
2024-12-07 17:29:00 +0900

    milkytracker 1.05.01

--- Casks/m/milkytracker.rb
+++ Casks/m/milkytracker.rb
@@ -1,11 +1,8 @@
 cask "milkytracker" do
-  arch arm: "arm64", intel: "x86_64"
+  version "1.05.01"
+  sha256 "465e399e5174255f33d92490052c7a851019bf5c9ba798bd83fc384158b05c71"
 
-  version "1.04.00"
-  sha256 arm:   "e3a1fcead7c6149151042ee75985219ec1893be62f2e789887608c08c2b65c3d",
-         intel: "078b341100147d28661c9cfb61ca496e2890e8affc4df2434cb2c5aa5fc26bf3"
-
-  url "https://github.com/milkytracker/MilkyTracker/releases/download/v#{version}/milkytracker-#{version}-Darwin-#{arch}.dmg",
+  url "https://github.com/milkytracker/MilkyTracker/releases/download/v#{version}/milkytracker-#{version}-Universal.dmg",
       verified: "github.com/milkytracker/MilkyTracker/"
   name "MilkyTracker"
   desc "Music tracker compatible with FT2"
