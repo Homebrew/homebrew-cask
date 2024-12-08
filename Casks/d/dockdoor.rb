05295c512a4ea69f2e253afa3181f182ee5df3a0
i0ntempest <i0ntempest@i0ntempest.com>
2024-12-09 00:51:56 +0800

    dockdoor 1.3.3

--- Casks/d/dockdoor.rb
+++ Casks/d/dockdoor.rb
@@ -1,6 +1,6 @@
 cask "dockdoor" do
-  version "1.3.2"
-  sha256 "8e3b55755e0a2b88605cfd050a3d450b6e936f3aa78911bf4b303f0fdf0d39d2"
+  version "1.3.3"
+  sha256 "9eb9937e13151373ed8e8f307abc04d11cd19890c54c0782ee60676edec15dd4"
 
   url "https://github.com/ejbills/DockDoor/releases/download/v#{version}/DockDoor.dmg"
   name "DockDoor"
