085c6f6ef1a8092ec60f3f51bb48989786547aed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-08 00:16:27 +0000

    font-montserrat-underline: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

--- Casks/font/font-m/font-montserrat-underline.rb
+++ Casks/font/font-m/font-montserrat-underline.rb
@@ -3,10 +3,11 @@ cask "font-montserrat-underline" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/montserratunderline"
   name "Montserrat Underline"
-  homepage "https://github.com/JulietaUla/Montserrat"
+  homepage "https://fonts.google.com/specimen/Montserrat+Underline"
 
   font "MontserratUnderline-Italic[wght].ttf"
   font "MontserratUnderline[wght].ttf"
