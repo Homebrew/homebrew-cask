fcf6c6d2226c14d0804c914ee74653e11972f5b0
Klaus Hipp <khipp@users.noreply.github.com>
2024-12-09 13:42:52 +0100

    electronmail 5.3.0

--- Casks/e/electronmail.rb
+++ Casks/e/electronmail.rb
@@ -1,9 +1,9 @@
 cask "electronmail" do
   arch arm: "arm64", intel: "x64"
 
-  version "5.2.3"
-  sha256 arm:   "25e461840b0d40f84271d00910adb58fc0038a8910a3f72a8e929189e9ccf244",
-         intel: "d87a7e33298317c5e95380a239663cd1af09a2388f7121e39002406c2fd4fe18"
+  version "5.3.0"
+  sha256 arm:   "29d6aa0687dba05ed557d711eb551a67b6da4a4288855d37e39c57548564b1bd",
+         intel: "10c4aec7838aa0842ab25329c7309a692efca228a3c828f916188baacd01bec5"
 
   url "https://github.com/vladimiry/ElectronMail/releases/download/v#{version}/electron-mail-#{version}-mac-#{arch}.dmg"
   name "ElectronMail"
