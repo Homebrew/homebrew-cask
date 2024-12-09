5fa42465c1c530605d3b284e36ecd030e42f1510
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-09 03:56:13 +0000

    transmission@nightly 9706

--- Casks/t/transmission@nightly.rb
+++ Casks/t/transmission@nightly.rb
@@ -1,6 +1,6 @@
 cask "transmission@nightly" do
-  version "9704,002c891216"
-  sha256 "8599c2eae0e90b510b3a51d1c4cbe72306d8399a2eeff9ff77efa97d2d1f990f"
+  version "9706,100e88826c"
+  sha256 "865e85839707494f7a6147f7faf7aeaeccc6d24fbe80448a955b529cd756192e"
 
   url "https://build.transmissionbt.com/job/trunk-mac/#{version.csv.first}/artifact/release/Transmission-#{version.csv.second}.dmg"
   name "Transmission"
