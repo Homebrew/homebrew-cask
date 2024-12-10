c5d214cfafa005e22c0ab0f73c3a049396cd4ebe
bevanjkay <email@bevankay.me>
2024-12-10 23:41:49 +1100

    reflector 4.1.2

--- Casks/r/reflector.rb
+++ Casks/r/reflector.rb
@@ -1,6 +1,6 @@
 cask "reflector" do
-  version "4.1.1"
-  sha256 "56744b2fff29d7de58808f0702db854c14a9314f6c775464e44e2fb4f5b0eb29"
+  version "4.1.2"
+  sha256 "343c74569d8e6f5a0c33c76cd2fc6b7c4eb98db568e633d40c6f177b36294ce3"
 
   url "https://download.airsquirrels.com/Reflector#{version.major}/Mac/Reflector-#{version}.dmg"
   name "Reflector"
