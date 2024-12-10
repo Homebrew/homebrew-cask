b7daedf2198970df1d18903598b852b658ebbe6f
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-12-09 19:21:49 -0500

    productive 1.13.0

--- Casks/p/productive.rb
+++ Casks/p/productive.rb
@@ -1,9 +1,9 @@
 cask "productive" do
   arch arm: "-arm64"
 
-  version "1.12.0"
-  sha256 arm:   "6a8fdcfe18b506ba55c9787da9959bc7b908719179ff872d484f93c182241667",
-         intel: "c85ef5495edbb50511bd11cacee8d77370293e7a1627b833d0017ecc4be2c430"
+  version "1.13.0"
+  sha256 arm:   "428b737d972ac5ae651633aec99f2b44dc2ab924052e8d180beec97ede5f90f6",
+         intel: "845638bbfeb39d3fcd7eb0e85390b87b4e4704ee214fe4cf719f9dcc4ef7b475"
 
   url "https://download.productive.io/desktop/electron/Productive-#{version}#{arch}.dmg"
   name "Productive"
