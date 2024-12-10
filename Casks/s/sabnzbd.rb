a7efac4a41adbb615c42fe4d586e6413e67fa1c6
bevanjkay <email@bevankay.me>
2024-12-10 23:42:19 +1100

    sabnzbd 4.4.0

--- Casks/s/sabnzbd.rb
+++ Casks/s/sabnzbd.rb
@@ -1,6 +1,6 @@
 cask "sabnzbd" do
-  version "4.3.3"
-  sha256 "bfaf22c8008d4c95ad467f381e8ce070de7d752a0b0d4cd20593e64c04d95653"
+  version "4.4.0"
+  sha256 "764636d86387b9e293db8a383715d2303f716a3b59758902d352fb84e0511889"
 
   url "https://github.com/sabnzbd/sabnzbd/releases/download/#{version}/SABnzbd-#{version}-osx.dmg",
       verified: "github.com/sabnzbd/sabnzbd/"
