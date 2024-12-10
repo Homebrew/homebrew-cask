bc4b661e66d90c7e8308aab13e8d2e337a318c0f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-10 21:37:10 +0000

    descript 104.0.0-release.20241210.6512

--- Casks/d/descript.rb
+++ Casks/d/descript.rb
@@ -1,9 +1,9 @@
 cask "descript" do
   arch arm: "-arm64"
 
-  version "103.0.1-release.20241120.4528"
-  sha256 arm:   "5a10869f4b5a444a419f58b9af4b793ad7348dfd102dcf3c2d8c96de475047e5",
-         intel: "016a80b859441de4d2a03ba58eeefc87c5eaba5ad67ad3aa13196c8c4303985c"
+  version "104.0.0-release.20241210.6512"
+  sha256 arm:   "6c5991a143e1453d9e5800aa277d99a1f543a7291f74f9966ec0769ae236c15f",
+         intel: "d18d0d086ee2ddb3c95f04b9138411e44713a82943b16055d408dafb21f73701"
 
   url "https://electron.descript.com/Descript-#{version}#{arch}.dmg"
   name "Descript"
