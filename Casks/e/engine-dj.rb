d875dc86ab04039be3db9e8d7c1648275a9c0984
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-12-10 21:09:05 -0500

    engine-dj 4.2.1

--- Casks/e/engine-dj.rb
+++ Casks/e/engine-dj.rb
@@ -1,6 +1,6 @@
 cask "engine-dj" do
-  version "4.2.0,4bb95a6097f50ea7,7c209f5bc9"
-  sha256 "6cd0d65fe38f708257eac25c8ab6589a95643c971626bbe0df87a1c868e32c2e"
+  version "4.2.1,a226e168a515b5dd,23da16a65b"
+  sha256 "96beea3e002970f81169751d14e2600b7d7c78134bd1ead23afabc6c1bea2739"
 
   url "https://imb-cicd-public.s3.amazonaws.com/Engine/#{version.csv.first}/Release/#{version.csv.second}/Engine_DJ_#{version.csv.first}_#{version.csv.third}_Setup.dmg",
       verified: "imb-cicd-public.s3.amazonaws.com/Engine/"
