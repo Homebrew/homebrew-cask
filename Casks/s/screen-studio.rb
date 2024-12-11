e5b10909e04aeb3ab76953392cbb6cbef88ffad2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-11 10:00:44 +0000

    screen-studio 2.26.0-3079

--- Casks/s/screen-studio.rb
+++ Casks/s/screen-studio.rb
@@ -1,9 +1,9 @@
 cask "screen-studio" do
   arch arm: "-arm64"
 
-  version "2.26.0-3073"
-  sha256 arm:   "1dc3b4f6b8f1ae7df3e2c75e3a26f22b0bc3c99b1556595a68a51cac63f10117",
-         intel: "7aa975a82257e3abc246c669bb541557c50e6baaad5683314fc91815bdc675ef"
+  version "2.26.0-3079"
+  sha256 arm:   "15829903d805c0d797cb0616d40b1b203f2e85dfaf79fd958e5cc527217af05a",
+         intel: "0a786b239dba3cf37b567146fd9788258dca8e599544664e14abdd0a36b5f27e"
 
   url "https://screenstudioassets.com/releases/#{version}/Screen%20Studio-#{version}#{arch}-mac.zip",
       verified: "screenstudioassets.com/"
