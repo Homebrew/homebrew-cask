ed6a1927fa36be564abd38f32b4aad96ef38aab6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-11 18:43:59 +0000

    draw-things 1.20241210.0-3f8e81d4

--- Casks/d/draw-things.rb
+++ Casks/d/draw-things.rb
@@ -1,6 +1,6 @@
 cask "draw-things" do
-  version "1.20241203.1-df803b51"
-  sha256 "df803b51967dc19ea873f788e289b667aa6f497cd27f93fe26046a350254fca6"
+  version "1.20241210.0-3f8e81d4"
+  sha256 "3f8e81d400178230ba094b9838a0e52a8b50a1b439de05c89dc04cc113563a4a"
 
   url "https://static.drawthings.ai/DrawThings-#{version}.zip"
   name "Draw Things"
