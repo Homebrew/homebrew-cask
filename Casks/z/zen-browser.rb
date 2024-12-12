f2c487dd21a1f8d585ce87f2a85e13f88dd35c85
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-12 04:00:55 +0000

    zen-browser 1.0.2-b.1

--- Casks/z/zen-browser.rb
+++ Casks/z/zen-browser.rb
@@ -1,9 +1,9 @@
 cask "zen-browser" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.0.2-b.0"
-  sha256 arm:   "5bcd5134db5344f13047211e18f63424ab1288b45fc4b44591a6944b2c53d47d",
-         intel: "6fd55fbc7f5d212d3a9987b768071c61a374dc45089e975855a0a6086c9b28a6"
+  version "1.0.2-b.1"
+  sha256 arm:   "22230e3bfe9fabec3682302d22d5cd9e671c8e168a921c9a1306883ed56e38e3",
+         intel: "73850bbcb73df59e64e53f550904a51f284bf8ca53280f5c1a3a0956da62b84a"
 
   url "https://github.com/zen-browser/desktop/releases/download/#{version}/zen.macos-#{arch}.dmg",
       verified: "github.com/zen-browser/desktop/"
