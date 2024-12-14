e14138cd24ca5d8df0e886bbef27522a518ca156
k.wakitani <k.wakitani@gmail.com>
2024-12-15 00:32:18 +0900

    font-moralerspace-hw-jpdoc 1.0.2 (new cask)

---
+++ Casks/font/font-m/font-moralerspace-hw-jpdoc.rb
@@ -0,0 +1,31 @@
+cask "font-moralerspace-hw-jpdoc" do
+  version "1.0.2"
+  sha256 "f2e7a6ea84135157fadebba74120350903308a84b4fa53a6193ad3c4282b9596"
+
+  url "https://github.com/yuru7/moralerspace/releases/download/v#{version}/MoralerspaceHWJPDOC_v#{version}.zip"
+  name "Moralerspace HW JPDOC"
+  homepage "https://github.com/yuru7/moralerspace"
+
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceArgonHWJPDOC-Bold.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceArgonHWJPDOC-BoldItalic.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceArgonHWJPDOC-Italic.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceArgonHWJPDOC-Regular.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceKryptonHWJPDOC-Bold.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceKryptonHWJPDOC-BoldItalic.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceKryptonHWJPDOC-Italic.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceKryptonHWJPDOC-Regular.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceNeonHWJPDOC-Bold.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceNeonHWJPDOC-BoldItalic.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceNeonHWJPDOC-Italic.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceNeonHWJPDOC-Regular.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceRadonHWJPDOC-Bold.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceRadonHWJPDOC-BoldItalic.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceRadonHWJPDOC-Italic.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceRadonHWJPDOC-Regular.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceXenonHWJPDOC-Bold.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceXenonHWJPDOC-BoldItalic.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceXenonHWJPDOC-Italic.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceXenonHWJPDOC-Regular.ttf"
+
+  # No zap stanza required
+end
