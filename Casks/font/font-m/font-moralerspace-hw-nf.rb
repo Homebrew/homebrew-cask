7f1a2cbc6362a9b3c7142a667906d9e458b4f6f7
k.wakitani <k.wakitani@gmail.com>
2024-12-15 00:32:18 +0900

    font-moralerspace-hw-nf 1.0.2 (new cask)

---
+++ Casks/font/font-m/font-moralerspace-hw-nf.rb
@@ -0,0 +1,31 @@
+cask "font-moralerspace-hw-nf" do
+  version "1.0.2"
+  sha256 "dcdccaf2620422e63f00f39589afc5b5052fe62d8fa6ec1a8c244da3e6982868"
+
+  url "https://github.com/yuru7/moralerspace/releases/download/v#{version}/MoralerspaceHWNF_v#{version}.zip"
+  name "Moralerspace HW NF"
+  homepage "https://github.com/yuru7/moralerspace"
+
+  font "MoralerspaceHWNF_v#{version}/MoralerspaceArgonHWNF-Bold.ttf"
+  font "MoralerspaceHWNF_v#{version}/MoralerspaceArgonHWNF-BoldItalic.ttf"
+  font "MoralerspaceHWNF_v#{version}/MoralerspaceArgonHWNF-Italic.ttf"
+  font "MoralerspaceHWNF_v#{version}/MoralerspaceArgonHWNF-Regular.ttf"
+  font "MoralerspaceHWNF_v#{version}/MoralerspaceKryptonHWNF-Bold.ttf"
+  font "MoralerspaceHWNF_v#{version}/MoralerspaceKryptonHWNF-BoldItalic.ttf"
+  font "MoralerspaceHWNF_v#{version}/MoralerspaceKryptonHWNF-Italic.ttf"
+  font "MoralerspaceHWNF_v#{version}/MoralerspaceKryptonHWNF-Regular.ttf"
+  font "MoralerspaceHWNF_v#{version}/MoralerspaceNeonHWNF-Bold.ttf"
+  font "MoralerspaceHWNF_v#{version}/MoralerspaceNeonHWNF-BoldItalic.ttf"
+  font "MoralerspaceHWNF_v#{version}/MoralerspaceNeonHWNF-Italic.ttf"
+  font "MoralerspaceHWNF_v#{version}/MoralerspaceNeonHWNF-Regular.ttf"
+  font "MoralerspaceHWNF_v#{version}/MoralerspaceRadonHWNF-Bold.ttf"
+  font "MoralerspaceHWNF_v#{version}/MoralerspaceRadonHWNF-BoldItalic.ttf"
+  font "MoralerspaceHWNF_v#{version}/MoralerspaceRadonHWNF-Italic.ttf"
+  font "MoralerspaceHWNF_v#{version}/MoralerspaceRadonHWNF-Regular.ttf"
+  font "MoralerspaceHWNF_v#{version}/MoralerspaceXenonHWNF-Bold.ttf"
+  font "MoralerspaceHWNF_v#{version}/MoralerspaceXenonHWNF-BoldItalic.ttf"
+  font "MoralerspaceHWNF_v#{version}/MoralerspaceXenonHWNF-Italic.ttf"
+  font "MoralerspaceHWNF_v#{version}/MoralerspaceXenonHWNF-Regular.ttf"
+
+  # No zap stanza required
+end
