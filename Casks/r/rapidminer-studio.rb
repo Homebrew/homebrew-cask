afbd3f48a40d60f029b1b96c94685f0574dd8b02
Sam Ford <1584702+samford@users.noreply.github.com>
2024-12-14 09:37:14 -0500

    rapidminer-studio 2025.0.0

--- Casks/r/rapidminer-studio.rb
+++ Casks/r/rapidminer-studio.rb
@@ -1,5 +1,5 @@
 cask "rapidminer-studio" do
-  version "2024.1.0"
+  version "2025.0.0"
   sha256 :no_check
 
   url "https://releases.rapidminer.com/latest/rapidminer-studio/rapidminer-studio-osx.dmg"
