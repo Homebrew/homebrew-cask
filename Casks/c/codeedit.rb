fdd0b5828980af483480f3825beba8aa88d49166
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-14 18:38:41 +0000

    codeedit 0.3.3

--- Casks/c/codeedit.rb
+++ Casks/c/codeedit.rb
@@ -1,6 +1,6 @@
 cask "codeedit" do
-  version "0.3.2"
-  sha256 "71119dfe8f81b863d82526963674597b5740fa794a2e27b9ca5b75798af65d31"
+  version "0.3.3"
+  sha256 "fb9a76ce9c31a93ed333f72370b4a17e79e742beb4f1741c8ffd9a30618a66c4"
 
   url "https://github.com/CodeEditApp/CodeEdit/releases/download/v#{version}/CodeEdit.dmg",
       verified: "github.com/CodeEditApp/CodeEdit/"
