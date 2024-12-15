195794516061bde9d0f390917ee86b867d5270aa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-15 00:16:12 +0000

    font-playwrite-de-va-guides: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

--- Casks/font/font-p/font-playwrite-de-va-guides.rb
+++ Casks/font/font-p/font-playwrite-de-va-guides.rb
@@ -2,9 +2,10 @@ cask "font-playwrite-de-va-guides" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/playwritedevaguides/PlaywriteDEVAGuides-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/playwritedevaguides/PlaywriteDEVAGuides-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Playwrite DE VA Guides"
-  homepage "https://github.com/TypeTogether/Playwrite"
+  homepage "https://fonts.google.com/specimen/Playwrite+DE+VA+Guides"
 
   font "PlaywriteDEVAGuides-Regular.ttf"
 
