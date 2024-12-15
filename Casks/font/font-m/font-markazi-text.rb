36493f7e70d1a38a310c9c61ea33112a670d378f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-15 00:16:06 +0000

    font-markazi-text: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

--- Casks/font/font-m/font-markazi-text.rb
+++ Casks/font/font-m/font-markazi-text.rb
@@ -2,12 +2,12 @@ cask "font-markazi-text" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/markazitext/MarkaziText%5Bwght%5D.ttf",
+  url "https://github.com/google/fonts/raw/main/ofl/markazitextvfbeta/MarkaziText-VF.ttf",
       verified: "github.com/google/fonts/"
   name "Markazi Text"
   homepage "https://fonts.google.com/specimen/Markazi+Text"
 
-  font "MarkaziText[wght].ttf"
+  font "MarkaziText-VF.ttf"
 
   # No zap stanza required
 end
