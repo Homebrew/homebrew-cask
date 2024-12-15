b4e235626a13023bbb903a1b1ce89f78f65cef4f
bevanjkay <email@bevankay.me>
2024-12-15 22:05:55 +1100

    gearboy 3.6.0

--- Casks/g/gearboy.rb
+++ Casks/g/gearboy.rb
@@ -1,18 +1,21 @@
 cask "gearboy" do
   arch arm: "arm", intel: "intel"
 
-  version "3.5.0"
-  sha256 arm:   "fd1a592afc7847c41ad933ae7a39656ad84d45042521e27f7ac62fa5347335f3",
-         intel: "317678aa721fc6747a07bb255ea5497aa2a47761f0ddc6d4f44ff45aa88429dd"
+  version "3.6.0"
+  sha256 arm:   "dc62746c1bfe40eb577b0322737ac05408f6679f31dcb8b565015bf164ec83db",
+         intel: "952a41af3d823e9e261d2a4dccc11874b4f6ee9833e0cf7edc55896bbdaf4c7b"
 
   url "https://github.com/drhelius/Gearboy/releases/download/#{version}/Gearboy-#{version}-macos-#{arch}.zip"
   name "Gearboy"
+  desc "Game Boy and Game Boy Color emulator"
   homepage "https://github.com/drhelius/Gearboy"
 
   livecheck do
     url :url
     strategy :github_latest
   end
+
+  container nested: "Gearboy.app.zip"
 
   app "Gearboy.app"
 
