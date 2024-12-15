f0bc0741ca79e7575e7085b0a9aaedee415de865
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-15 00:16:14 +0000

    font-playwrite-gb-s-guides: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

--- Casks/font/font-p/font-playwrite-gb-s-guides.rb
+++ Casks/font/font-p/font-playwrite-gb-s-guides.rb
@@ -3,10 +3,11 @@ cask "font-playwrite-gb-s-guides" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/playwritegbsguides"
   name "Playwrite GB S Guides"
-  homepage "https://github.com/TypeTogether/Playwrite"
+  homepage "https://fonts.google.com/specimen/Playwrite+GB+S+Guides"
 
   font "PlaywriteGBSGuides-Italic.ttf"
   font "PlaywriteGBSGuides-Regular.ttf"
