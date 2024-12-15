4c2d06fe05540ba541cc8d9628fb6b07cbc10c2b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-15 00:16:12 +0000

    font-playwrite-de-la-guides: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

--- Casks/font/font-p/font-playwrite-de-la-guides.rb
+++ Casks/font/font-p/font-playwrite-de-la-guides.rb
@@ -2,9 +2,10 @@ cask "font-playwrite-de-la-guides" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/playwritedelaguides/PlaywriteDELAGuides-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/playwritedelaguides/PlaywriteDELAGuides-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Playwrite DE LA Guides"
-  homepage "https://github.com/TypeTogether/Playwrite"
+  homepage "https://fonts.google.com/specimen/Playwrite+DE+LA+Guides"
 
   font "PlaywriteDELAGuides-Regular.ttf"
 
