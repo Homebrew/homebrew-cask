bba7937ffa47521943e61483ca299c211b1de9e5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-15 00:16:10 +0000

    font-playwrite-ca-guides: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

--- Casks/font/font-p/font-playwrite-ca-guides.rb
+++ Casks/font/font-p/font-playwrite-ca-guides.rb
@@ -2,9 +2,10 @@ cask "font-playwrite-ca-guides" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/playwritecaguides/PlaywriteCAGuides-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/playwritecaguides/PlaywriteCAGuides-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Playwrite CA Guides"
-  homepage "https://github.com/TypeTogether/Playwrite"
+  homepage "https://fonts.google.com/specimen/Playwrite+CA+Guides"
 
   font "PlaywriteCAGuides-Regular.ttf"
 
