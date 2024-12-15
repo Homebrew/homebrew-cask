da1b26fea50b9080b1d1549a87e0f89c4190a72c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-15 00:16:09 +0000

    font-playwrite-be-wal-guides: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

--- Casks/font/font-p/font-playwrite-be-wal-guides.rb
+++ Casks/font/font-p/font-playwrite-be-wal-guides.rb
@@ -2,9 +2,10 @@ cask "font-playwrite-be-wal-guides" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/playwritebewalguides/PlaywriteBEWALGuides-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/playwritebewalguides/PlaywriteBEWALGuides-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Playwrite BE WAL Guides"
-  homepage "https://github.com/TypeTogether/Playwrite"
+  homepage "https://fonts.google.com/specimen/Playwrite+BE+WAL+Guides"
 
   font "PlaywriteBEWALGuides-Regular.ttf"
 
