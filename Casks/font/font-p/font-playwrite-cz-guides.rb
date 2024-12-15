4d114fdccb694095285d4c5db041f1cef196d4ed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-15 00:16:11 +0000

    font-playwrite-cz-guides: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

--- Casks/font/font-p/font-playwrite-cz-guides.rb
+++ Casks/font/font-p/font-playwrite-cz-guides.rb
@@ -2,9 +2,10 @@ cask "font-playwrite-cz-guides" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/playwriteczguides/PlaywriteCZGuides-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/playwriteczguides/PlaywriteCZGuides-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Playwrite CZ Guides"
-  homepage "https://github.com/TypeTogether/Playwrite"
+  homepage "https://fonts.google.com/specimen/Playwrite+CZ+Guides"
 
   font "PlaywriteCZGuides-Regular.ttf"
 
