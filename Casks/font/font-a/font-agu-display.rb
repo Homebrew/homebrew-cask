51ec40f4672e343118e33ff25093f6aeadaca2d9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-15 00:16:05 +0000

    font-agu-display: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

--- Casks/font/font-a/font-agu-display.rb
+++ Casks/font/font-a/font-agu-display.rb
@@ -2,9 +2,10 @@ cask "font-agu-display" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/agudisplay/AguDisplay%5BMORF%5D.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/agudisplay/AguDisplay%5BMORF%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Agu Display"
-  homepage "https://github.com/theseunbadejo/Agu-Display"
+  homepage "https://fonts.google.com/specimen/Agu+Display"
 
   font "AguDisplay[MORF].ttf"
 
