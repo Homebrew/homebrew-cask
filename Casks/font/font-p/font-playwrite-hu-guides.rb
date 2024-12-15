9f7b556beba36ed7eed1a93585404d2d905d9bca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-15 00:16:15 +0000

    font-playwrite-hu-guides: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

--- Casks/font/font-p/font-playwrite-hu-guides.rb
+++ Casks/font/font-p/font-playwrite-hu-guides.rb
@@ -2,9 +2,10 @@ cask "font-playwrite-hu-guides" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/playwritehuguides/PlaywriteHUGuides-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/playwritehuguides/PlaywriteHUGuides-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Playwrite HU Guides"
-  homepage "https://github.com/TypeTogether/Playwrite"
+  homepage "https://fonts.google.com/specimen/Playwrite+HU+Guides"
 
   font "PlaywriteHUGuides-Regular.ttf"
 
