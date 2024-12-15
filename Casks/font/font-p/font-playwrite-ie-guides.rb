909069fde1ed6f774357fbff0c88f7d2799fd2c5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-15 00:16:15 +0000

    font-playwrite-ie-guides: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

--- Casks/font/font-p/font-playwrite-ie-guides.rb
+++ Casks/font/font-p/font-playwrite-ie-guides.rb
@@ -2,9 +2,10 @@ cask "font-playwrite-ie-guides" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/playwriteieguides/PlaywriteIEGuides-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/playwriteieguides/PlaywriteIEGuides-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Playwrite IE Guides"
-  homepage "https://github.com/TypeTogether/Playwrite"
+  homepage "https://fonts.google.com/specimen/Playwrite+IE+Guides"
 
   font "PlaywriteIEGuides-Regular.ttf"
 
