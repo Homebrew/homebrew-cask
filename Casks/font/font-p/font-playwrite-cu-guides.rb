fc5ea883844764da4c4f55c62b5ee1f8cf6a6910
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-15 00:16:11 +0000

    font-playwrite-cu-guides: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

--- Casks/font/font-p/font-playwrite-cu-guides.rb
+++ Casks/font/font-p/font-playwrite-cu-guides.rb
@@ -2,9 +2,10 @@ cask "font-playwrite-cu-guides" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/playwritecuguides/PlaywriteCUGuides-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/playwritecuguides/PlaywriteCUGuides-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Playwrite CU Guides"
-  homepage "https://github.com/TypeTogether/Playwrite"
+  homepage "https://fonts.google.com/specimen/Playwrite+CU+Guides"
 
   font "PlaywriteCUGuides-Regular.ttf"
 
