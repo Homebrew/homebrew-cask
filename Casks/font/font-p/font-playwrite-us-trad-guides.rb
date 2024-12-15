d45ee11bf431a88e6b739541350884be24571bd9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-15 00:16:19 +0000

    font-playwrite-us-trad-guides: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

--- Casks/font/font-p/font-playwrite-us-trad-guides.rb
+++ Casks/font/font-p/font-playwrite-us-trad-guides.rb
@@ -2,9 +2,10 @@ cask "font-playwrite-us-trad-guides" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/playwriteustradguides/PlaywriteUSTradGuides-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/playwriteustradguides/PlaywriteUSTradGuides-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Playwrite US Trad Guides"
-  homepage "https://github.com/TypeTogether/Playwrite"
+  homepage "https://fonts.google.com/specimen/Playwrite+US+Trad+Guides"
 
   font "PlaywriteUSTradGuides-Regular.ttf"
 
