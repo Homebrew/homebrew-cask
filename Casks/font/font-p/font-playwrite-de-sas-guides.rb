b031a6d1622953d04f5e43c22283ae65d7ce845d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-15 00:16:12 +0000

    font-playwrite-de-sas-guides: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

--- Casks/font/font-p/font-playwrite-de-sas-guides.rb
+++ Casks/font/font-p/font-playwrite-de-sas-guides.rb
@@ -2,9 +2,10 @@ cask "font-playwrite-de-sas-guides" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/playwritedesasguides/PlaywriteDESASGuides-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/playwritedesasguides/PlaywriteDESASGuides-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Playwrite DE SAS Guides"
-  homepage "https://github.com/TypeTogether/Playwrite"
+  homepage "https://fonts.google.com/specimen/Playwrite+DE+SAS+Guides"
 
   font "PlaywriteDESASGuides-Regular.ttf"
 
