203fab44f94cf93c84266901b96f8263f2c2662a
Bevan Kay <email@bevankay.me>
2024-12-16 23:04:36 +1100

    ipartition: disable

--- Casks/i/ipartition.rb
+++ Casks/i/ipartition.rb
@@ -13,7 +13,7 @@ cask "ipartition" do
   desc "Disk partitioning tool"
   homepage "https://coriolis-systems.com/iPartition/"
 
-  deprecate! date: "2023-12-17", because: :discontinued
+  disable! date: "2024-12-16", because: :discontinued
 
   depends_on macos: "<= :high_sierra"
 
