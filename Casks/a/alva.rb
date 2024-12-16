7c4889f2246eca26ea5459736618961d53992c65
Bevan Kay <email@bevankay.me>
2024-12-16 23:04:05 +1100

    alva: disable

--- Casks/a/alva.rb
+++ Casks/a/alva.rb
@@ -8,7 +8,7 @@ cask "alva" do
   desc "Create living prototypes with code components"
   homepage "https://meetalva.io/"
 
-  deprecate! date: "2023-12-17", because: :discontinued
+  disable! date: "2024-12-16", because: :discontinued
 
   app "Alva.app"
 end
