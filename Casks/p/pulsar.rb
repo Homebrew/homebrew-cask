8f2af48da6400799ff504486be36a5a2d5c81129
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-16 06:51:21 +0000

    pulsar 1.124.0

--- Casks/p/pulsar.rb
+++ Casks/p/pulsar.rb
@@ -2,9 +2,9 @@ cask "pulsar" do
   arch arm: "Silicon", intel: "Intel"
   arch_suffix = on_arch_conditional arm: "-arm64"
 
-  version "1.123.0"
-  sha256 arm:   "06a9bd521f4ec75d72bbb6a8d72cba600d146792934cc9ebf9ddcaf15f36895b",
-         intel: "96eeb5a648e0123e547115830ceea0108999fec4272a78de916dd5051c0e0834"
+  version "1.124.0"
+  sha256 arm:   "73ba8dd1ad88321881f837b93454550df569d1dd6c85a8c9d1d3ff89de6663f6",
+         intel: "1534e26e2c5d7ce919dc10b9634760117ab9326257c5bdcecdbf4c733b1c501c"
 
   url "https://github.com/pulsar-edit/pulsar/releases/download/v#{version}/#{arch}.Mac.Pulsar-#{version}#{arch_suffix}-mac.zip",
       verified: "github.com/pulsar-edit/pulsar/"
