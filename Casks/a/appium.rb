b6f6d61421a4b287cc88e5feb28c55dba26bc293
Bevan Kay <email@bevankay.me>
2024-12-16 23:04:09 +1100

    appium: disable

--- Casks/a/appium.rb
+++ Casks/a/appium.rb
@@ -8,7 +8,7 @@ cask "appium" do
   desc "Graphical frontend to Appium automation server"
   homepage "https://appium.io/"
 
-  deprecate! date: "2023-12-17", because: :discontinued
+  disable! date: "2024-12-16", because: :discontinued
 
   app "Appium Server GUI.app"
 
