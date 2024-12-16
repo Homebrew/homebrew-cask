29bc606deef852e2c20ba3d9ac80f66e4f6e1e6b
Bevan Kay <email@bevankay.me>
2024-12-16 23:05:03 +1100

    sourcetrail: disable

--- Casks/s/sourcetrail.rb
+++ Casks/s/sourcetrail.rb
@@ -8,7 +8,7 @@ cask "sourcetrail" do
   desc "Code source explorer"
   homepage "https://www.sourcetrail.com/"
 
-  deprecate! date: "2023-12-17", because: :discontinued
+  disable! date: "2024-12-16", because: :discontinued
 
   app "Sourcetrail.app"
 
