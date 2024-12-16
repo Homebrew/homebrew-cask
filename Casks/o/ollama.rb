c1d6662ea4f60fe7a964132947b125dcd5d25e25
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-16 21:53:06 +0000

    ollama 0.5.2

--- Casks/o/ollama.rb
+++ Casks/o/ollama.rb
@@ -1,6 +1,6 @@
 cask "ollama" do
-  version "0.5.1"
-  sha256 "f8fa474bde2a9f5cc238d115f9edd51523d956b58013ee48fc276a0e330d789e"
+  version "0.5.2"
+  sha256 "d045a1b532e04412fbc378fa6242b098ce090a1f139ed3729ca882909593e6bf"
 
   url "https://github.com/ollama/ollama/releases/download/v#{version}/Ollama-darwin.zip",
       verified: "github.com/ollama/ollama/"
