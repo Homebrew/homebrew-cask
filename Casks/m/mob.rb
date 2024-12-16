6b61b2c09235085f1a774aa821e7d9f12c1e9fbd
Bevan Kay <email@bevankay.me>
2024-12-16 23:04:46 +1100

    mob: disable

--- Casks/m/mob.rb
+++ Casks/m/mob.rb
@@ -6,7 +6,7 @@ cask "mob" do
   name "Mob"
   homepage "https://github.com/zenghongtu/Mob"
 
-  deprecate! date: "2023-12-17", because: :discontinued
+  disable! date: "2024-12-16", because: :discontinued
 
   app "Mob.app"
 
