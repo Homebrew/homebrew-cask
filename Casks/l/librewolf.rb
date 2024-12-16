56433980e2b3a00eed6da503805af0c0defefab8
Klaus Hipp <khipp@users.noreply.github.com>
2024-12-16 20:09:39 +0100

    librewolf: update livecheck

--- Casks/l/librewolf.rb
+++ Casks/l/librewolf.rb
@@ -1,29 +1,18 @@
 cask "librewolf" do
   arch arm: "arm64", intel: "x86_64"
 
-  on_arm do
-    version "133.0.3,1"
-    sha256 "e0ba3376b5620ddd7cba223aeb2909e12b144e1e16bd93b992893aa5c453d487"
-  end
-  on_intel do
-    version "133.0.3,1"
-    sha256 "2a0e9325e366146a79cf579c90ec4ea68d057648564c796f2945282ed08fb212"
-  end
+  version "133.0.3-1"
+  sha256 arm:   "e0ba3376b5620ddd7cba223aeb2909e12b144e1e16bd93b992893aa5c453d487",
+         intel: "2a0e9325e366146a79cf579c90ec4ea68d057648564c796f2945282ed08fb212"
 
-  url "https://gitlab.com/api/v4/projects/44042130/packages/generic/librewolf/#{version.csv.first}-#{version.csv.second}/librewolf-#{version.csv.first}-#{version.csv.second}-macos-#{arch}-package.dmg",
+  url "https://gitlab.com/api/v4/projects/44042130/packages/generic/librewolf/#{version}/librewolf-#{version}-macos-#{arch}-package.dmg",
       verified: "gitlab.com/api/v4/projects/44042130/packages/generic/librewolf/"
   name "LibreWolf"
   desc "Web browser"
   homepage "https://librewolf.net/"
 
   livecheck do
-    url "https://gitlab.com/api/v4/projects/44042130/releases"
-    regex(/librewolf[._-]v?(\d+(?:\.\d+)+)[._-](\d+)[._-]macos[._-]#{arch}[._-]package\.dmg/i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map do |match|
-        "#{match[0]},#{match[1]}"
-      end
-    end
+    url "https://gitlab.com/librewolf-community/browser/bsys6.git"
   end
 
   depends_on macos: ">= :catalina"
