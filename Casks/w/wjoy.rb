8f4a5842512b77362ba1e7827a59dd428e94a01d
Bevan Kay <email@bevankay.me>
2024-12-16 23:05:10 +1100

    wjoy: disable

--- Casks/w/wjoy.rb
+++ Casks/w/wjoy.rb
@@ -7,7 +7,7 @@ cask "wjoy" do
   desc "Nintendo wiimote driver"
   homepage "https://github.com/alxn1/wjoy"
 
-  deprecate! date: "2023-12-17", because: :discontinued
+  disable! date: "2024-12-16", because: :discontinued
 
   app "Wjoy.app"
 end
