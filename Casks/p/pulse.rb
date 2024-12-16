a5d34d5a25c770da85910f1d6921172af34714b2
Bevan Kay <email@bevankay.me>
2024-12-16 23:04:55 +1100

    pulse: disable

--- Casks/p/pulse.rb
+++ Casks/p/pulse.rb
@@ -8,7 +8,7 @@ cask "pulse" do
   desc "Logger and network inspector"
   homepage "https://kean.blog/pulse/home"
 
-  deprecate! date: "2023-12-17", because: :discontinued
+  disable! date: "2024-12-16", because: :discontinued
 
   app "Pulse.app"
 
