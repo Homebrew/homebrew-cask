d2bab2053eef767cfba63d05b6b6929750df949f
Bevan Kay <email@bevankay.me>
2024-12-16 23:04:15 +1100

    caret: disable

--- Casks/c/caret.rb
+++ Casks/c/caret.rb
@@ -7,7 +7,7 @@ cask "caret" do
   name "Caret"
   homepage "https://caret.io/"
 
-  deprecate! date: "2023-12-17", because: :discontinued
+  disable! date: "2024-12-16", because: :discontinued
 
   app "Caret.app"
 
