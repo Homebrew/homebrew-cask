86ad3feefa18a33d1f7943c73b7a11e4027340c5
tsuwatch <tomohiro.suwa@zozo.com>
2024-12-16 16:43:18 +0900

    Fix font-0xproto sha256

--- Casks/font/font-0/font-0xproto.rb
+++ Casks/font/font-0/font-0xproto.rb
@@ -1,6 +1,6 @@
 cask "font-0xproto" do
   version "2.201"
-  sha256 "201c9ee7c8f32245c4d97798b46235e2caa3586e930522ec6885c4040d08fa70"
+  sha256 "cc383e1562085438d026ef6c3ad678ca0a12d8349013485adce9791890bf8821"
 
   url "https://github.com/0xType/0xProto/releases/download/#{version}/0xProto_#{version.dots_to_underscores}.zip"
   name "0xProto"
