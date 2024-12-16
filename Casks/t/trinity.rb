23dcecf1af9abcb6c8f5e592162b1fd4eddbd7a7
Bevan Kay <email@bevankay.me>
2024-12-16 23:05:07 +1100

    trinity: disable

--- Casks/t/trinity.rb
+++ Casks/t/trinity.rb
@@ -8,7 +8,7 @@ cask "trinity" do
   desc "Cryptocurrency wallet"
   homepage "https://trinity.iota.org/"
 
-  deprecate! date: "2023-12-17", because: :discontinued
+  disable! date: "2024-12-16", because: :discontinued
 
   app "Trinity.app"
 
