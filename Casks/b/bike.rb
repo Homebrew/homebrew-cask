67f1a9ab983e12e4b3bd51a50b4efdf69bdbf056
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-16 15:34:05 +0000

    bike 1.20

--- Casks/b/bike.rb
+++ Casks/b/bike.rb
@@ -1,6 +1,6 @@
 cask "bike" do
-  version "1.19,186"
-  sha256 "78f860b5f61f1660cb56f924d2583b6bb0cea3ee84c0a61903d39bae79394444"
+  version "1.20,187"
+  sha256 "3aca8ccfd878f851de45cdd2b912b0863313cf1541c5eae041faf0d1142281da"
 
   url "https://www.hogbaysoftware.com/bike/releases/#{version.csv.second}.dmg"
   name "Bike"
