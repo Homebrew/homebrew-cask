bcc299cf20df4fbfdc2156a848a78586b2a80de6
Bevan Kay <email@bevankay.me>
2024-12-16 23:04:34 +1100

    hwsensors: disable

--- Casks/h/hwsensors.rb
+++ Casks/h/hwsensors.rb
@@ -7,7 +7,7 @@ cask "hwsensors" do
   desc "Tool to access information from available hardware sensors"
   homepage "https://github.com/kozlekek/HWSensors/"
 
-  deprecate! date: "2023-12-17", because: :discontinued
+  disable! date: "2024-12-16", because: :discontinued
 
   pkg "HWSensors.#{version}.pkg"
 
