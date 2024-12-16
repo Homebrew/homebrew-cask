ec65b51eb62df5160615c13632cc16e9b5d78be6
Bevan Kay <email@bevankay.me>
2024-12-16 23:05:04 +1100

    spotifree: disable

--- Casks/s/spotifree.rb
+++ Casks/s/spotifree.rb
@@ -7,7 +7,7 @@ cask "spotifree" do
   desc "Automatically mutes ads on Spotify (not supported)"
   homepage "https://github.com/ArtemGordinsky/Spotifree/"
 
-  deprecate! date: "2023-12-17", because: :discontinued
+  disable! date: "2024-12-16", because: :discontinued
 
   app "Spotifree.app"
 end
