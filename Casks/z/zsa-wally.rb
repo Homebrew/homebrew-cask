a32cfc8de1c51f1b40675ccbe7a4e3a0eec9bc8c
Bevan Kay <email@bevankay.me>
2024-12-16 23:05:14 +1100

    zsa-wally: disable

--- Casks/z/zsa-wally.rb
+++ Casks/z/zsa-wally.rb
@@ -8,7 +8,7 @@ cask "zsa-wally" do
   desc "Flash tool for ZSA keyboards"
   homepage "https://ergodox-ez.com/pages/wally"
 
-  deprecate! date: "2023-12-17", because: :discontinued
+  disable! date: "2024-12-16", because: :discontinued
 
   app "Wally.app"
 
