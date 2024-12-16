f8f9c268659960c52aeb782997c5a64368fff6d2
Daeho Ro <lamanus@outlook.kr>
2024-12-16 20:42:23 +0900

    oxygen-xml-developer 27.0,2024121306

--- Casks/o/oxygen-xml-developer.rb
+++ Casks/o/oxygen-xml-developer.rb
@@ -1,5 +1,5 @@
 cask "oxygen-xml-developer" do
-  version "27.0,2024112212"
+  version "27.0,2024121306"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://archives.oxygenxml.com/Oxygen/Developer/InstData#{version.csv.first}/MacOSX/VM/oxygenDeveloper-openjdk.dmg"
