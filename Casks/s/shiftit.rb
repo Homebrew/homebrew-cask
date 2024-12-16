374e1be8fb8350a4fef278621c3d26fa28a10c88
Bevan Kay <email@bevankay.me>
2024-12-16 23:05:01 +1100

    shiftit: disable

--- Casks/s/shiftit.rb
+++ Casks/s/shiftit.rb
@@ -7,7 +7,7 @@ cask "shiftit" do
   desc "Tool to manage the size and position of windows"
   homepage "https://github.com/fikovnik/ShiftIt/"
 
-  deprecate! date: "2023-12-17", because: :discontinued
+  disable! date: "2024-12-16", because: :discontinued
 
   auto_updates true
 
