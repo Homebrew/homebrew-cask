7587606f28c9ef7cdfcfd64b65ba7322ef30e564
Bevan Kay <email@bevankay.me>
2024-12-16 23:04:43 +1100

    logisim: disable

--- Casks/l/logisim.rb
+++ Casks/l/logisim.rb
@@ -8,7 +8,7 @@ cask "logisim" do
   desc "Tool for designing and simulating digital logic circuits"
   homepage "http://www.cburch.com/logisim/"
 
-  deprecate! date: "2023-12-17", because: :discontinued
+  disable! date: "2024-12-16", because: :discontinued
 
   app "Logisim.app"
 
