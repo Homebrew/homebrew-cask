95d2507984aac6b97620593290ab409f1d9ae28b
Bevan Kay <email@bevankay.me>
2024-12-16 23:04:20 +1100

    dbglass: disable

--- Casks/d/dbglass.rb
+++ Casks/d/dbglass.rb
@@ -8,7 +8,7 @@ cask "dbglass" do
   desc "PostgreSQL client built with Electron"
   homepage "http://dbglass.web-pal.com/"
 
-  deprecate! date: "2023-12-17", because: :discontinued
+  disable! date: "2024-12-16", because: :discontinued
 
   app "DBGlass-darwin-x64/DBGlass.app"
 end
