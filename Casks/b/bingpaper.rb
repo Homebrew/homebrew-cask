e02fbf9c7a9295eddce213dc102b55ee558bd437
Bevan Kay <email@bevankay.me>
2024-12-16 23:04:13 +1100

    bingpaper: disable

--- Casks/b/bingpaper.rb
+++ Casks/b/bingpaper.rb
@@ -7,7 +7,7 @@ cask "bingpaper" do
   desc "Use the Bing daily photo as your wallpaper"
   homepage "https://github.com/pengsrc/BingPaper"
 
-  deprecate! date: "2023-12-17", because: :discontinued
+  disable! date: "2024-12-16", because: :discontinued
 
   depends_on macos: ">= :catalina"
 
