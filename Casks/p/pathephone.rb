188386dd4e5e0d77b6dafda87a2dbeadf7c2985a
Bevan Kay <email@bevankay.me>
2024-12-16 23:04:52 +1100

    pathephone: disable

--- Casks/p/pathephone.rb
+++ Casks/p/pathephone.rb
@@ -8,7 +8,7 @@ cask "pathephone" do
   desc "Distributed audio player"
   homepage "https://pathephone.github.io/"
 
-  deprecate! date: "2023-12-17", because: :discontinued
+  disable! date: "2024-12-16", because: :discontinued
 
   auto_updates true
 
