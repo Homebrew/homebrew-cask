fe466fb9692b311507461e8aca867eaf5fc00762
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-17 18:47:53 +0000

    excalidrawz 1.2.7

--- Casks/e/excalidrawz.rb
+++ Casks/e/excalidrawz.rb
@@ -1,6 +1,6 @@
 cask "excalidrawz" do
-  version "1.2.6"
-  sha256 "bf4759a2652d8b477df5544ac7cc687d382848723824a3aa4c4148b20fb50146"
+  version "1.2.7"
+  sha256 "9d824201f9123cf115c6a468ad8170c117587cfccb9de59c8fbcb97dc9e04e49"
 
   url "https://github.com/chocoford/ExcalidrawZ/releases/download/v#{version}/ExcalidrawZ.#{version}.dmg",
       verified: "github.com/chocoford/ExcalidrawZ/releases/download/"
