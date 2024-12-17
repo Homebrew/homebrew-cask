5098ca68166cf76a68ed3c9f8eae26e849afb2f1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-17 06:43:23 +0000

    frappe-books 0.25.1

--- Casks/f/frappe-books.rb
+++ Casks/f/frappe-books.rb
@@ -1,9 +1,9 @@
 cask "frappe-books" do
   arch arm: "-arm64"
 
-  version "0.25.0"
-  sha256 arm:   "ce685fc0c730a439a70c120bd8c0edbedd49d205d9faf3bb2d4371ae48c02888",
-         intel: "a8080d6e1d9551ec52b47417ef495b6586bbad7d83cbd2dc06fdc12e4770d1be"
+  version "0.25.1"
+  sha256 arm:   "3b7c233a24f05d4c63cf4a4014bee4ed9cc41fa10ea42d4ac847e173b904e41f",
+         intel: "692c7c26f71c486598c71b3b7ac0eb2f6fb5112949262985ed2fcbf1f4270433"
 
   url "https://github.com/frappe/books/releases/download/v#{version}/Frappe-Books-#{version}#{arch}.dmg",
       verified: "github.com/frappe/books/"
