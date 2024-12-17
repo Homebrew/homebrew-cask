c4b83727a4ff2f205a6bb94b706c32a1703dcda0
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2024-12-17 14:31:00 +0000

    piezo 1.9.5

--- Casks/p/piezo.rb
+++ Casks/p/piezo.rb
@@ -9,7 +9,7 @@ cask "piezo" do
     depends_on macos: ">= :big_sur"
   end
   on_sonoma :or_newer do
-    version "1.9.4"
+    version "1.9.5"
 
     url "https://cdn.rogueamoeba.com/piezo/download/Piezo.zip"
 
