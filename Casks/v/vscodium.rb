d3aa45ac154755b95ff5dbafc32afa1f3b14685c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-18 19:08:19 +0000

    vscodium 1.96.1.24353

--- Casks/v/vscodium.rb
+++ Casks/v/vscodium.rb
@@ -1,9 +1,9 @@
 cask "vscodium" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.96.0.24352"
-  sha256 arm:   "3e5aeeaac7891925a25ecfeb22d733a09f59b3764c3117eee049cefa2a6b58e8",
-         intel: "5ea17c6eb24d887f1c234798857aedbc8294a0e5440c04e552862ff6f3906eee"
+  version "1.96.1.24353"
+  sha256 arm:   "4d09d12f6aed9705f28336bd526fb493fbf6651613a8148b472aa62f8e59e0a3",
+         intel: "f619ca96fcfa098c06d30e4aee729b66e9f4d95dbd7be98fd28e52305e30bb43"
 
   url "https://github.com/VSCodium/vscodium/releases/download/#{version}/VSCodium.#{arch}.#{version}.dmg"
   name "VSCodium"
