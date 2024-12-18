43cbd53f38e106a267f328c33ea0e6a6aaee2b0c
Atris <vacekj@outlook.com>
2024-12-18 13:13:47 +0100

    fix: styling

--- Casks/r/railway-wallet.rb
+++ Casks/r/railway-wallet.rb
@@ -1,28 +1,26 @@
 cask "railway-wallet" do
-    arch arm: "arm64", intel: "x86_64"
-  
-    version :latest
-    sha256 :no_check
-  
-    url "https://github.com/Railway-Wallet/Railway-Wallet/releases/latest/download/Railway.mac.#{arch}.dmg",
-        verified: "github.com/Railway-Wallet/Railway-Wallet/"
-  
-    name "Railway Wallet"
-    desc "Railway Private DeFi Wallet"
-    homepage "https://www.railway.xyz/"
-  
-    livecheck do
-      url :url
-      strategy :github_latest
-    end
-  
-    app "Railway.app"
-  
-    zap trash: [
-      "~/Library/Application Support/Railway",
-      "~/Library/Preferences/com.railway.wallet.plist",
-      "~/Library/Caches/com.railway.wallet",
-      "~/Library/Logs/Railway"
-    ]
+  arch arm: "arm64", intel: "x86_64"
+
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/Railway-Wallet/Railway-Wallet/releases/latest/download/Railway.mac.#{arch}.dmg",
+      verified: "github.com/Railway-Wallet/Railway-Wallet/"
+  name "Railway Wallet"
+  desc "Railway Private DeFi Wallet"
+  homepage "https://www.railway.xyz/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
   end
-  
+
+  app "Railway.app"
+
+  zap trash: [
+    "~/Library/Application Support/Railway",
+    "~/Library/Caches/com.railway.wallet",
+    "~/Library/Logs/Railway",
+    "~/Library/Preferences/com.railway.wallet.plist",
+  ]
+end
