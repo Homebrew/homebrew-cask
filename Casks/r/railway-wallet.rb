57bd859d0a013983df2049fb7a71ebe68bf45bea
Atris <vacekj@outlook.com>
2024-12-18 13:00:23 +0100

    feat: add railway-wallet cask

---
+++ Casks/r/railway-wallet.rb
@@ -0,0 +1,28 @@
+cask "railway-wallet" do
+    arch arm: "arm64", intel: "x86_64"
+  
+    version :latest
+    sha256 :no_check
+  
+    url "https://github.com/Railway-Wallet/Railway-Wallet/releases/latest/download/Railway.mac.#{arch}.dmg",
+        verified: "github.com/Railway-Wallet/Railway-Wallet/"
+  
+    name "Railway Wallet"
+    desc "Railway Private DeFi Wallet"
+    homepage "https://www.railway.xyz/"
+  
+    livecheck do
+      url :url
+      strategy :github_latest
+    end
+  
+    app "Railway.app"
+  
+    zap trash: [
+      "~/Library/Application Support/Railway",
+      "~/Library/Preferences/com.railway.wallet.plist",
+      "~/Library/Caches/com.railway.wallet",
+      "~/Library/Logs/Railway"
+    ]
+  end
+  
