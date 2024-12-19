c7b920cbb457207df7bf0816e065fdf1b58fa031
taka-2120 <yu_tkhs@icloud.com>
2024-12-20 00:02:33 +0900

    quick-search 1.0.0

---
+++ Casks/q/quick-search.rb
@@ -0,0 +1,13 @@
+cask "quick-search" do
+  version "1.0.0"
+  sha256 "aed8a31b7fcd15760c2f657efdb12c0ecd67f7c06b1a3abc760b44ef3afe2ad7"
+
+  url "https://github.com/taka-2120/QuickSearchMac/releases/download/#{version}/QuickSearch.dmg"
+  name "QuickSearch"
+  desc "Search on Web Quickly"
+  homepage "https://github.com/taka-2120/QuickSearchMac"
+
+  depends_on macos: ">= :sequoia"
+
+  app "QuickSearch.app"
+end
