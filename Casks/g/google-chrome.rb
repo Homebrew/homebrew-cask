2fea8a2a901d44960dbdaea4c3fd176a425b282a
bevanjkay <email@bevankay.me>
2024-12-19 15:38:28 +1100

    google-chrome 131.0.6778.205

--- Casks/g/google-chrome.rb
+++ Casks/g/google-chrome.rb
@@ -1,5 +1,5 @@
 cask "google-chrome" do
-  version "131.0.6778.140"
+  version "131.0.6778.205"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
