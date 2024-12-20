2b756b2adbaddc828b5a233f3e117fc00ecaf2bc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-20 18:59:06 +0000

    readdle-spark 3.19.2.94902

--- Casks/r/readdle-spark.rb
+++ Casks/r/readdle-spark.rb
@@ -1,6 +1,6 @@
 cask "readdle-spark" do
-  version "3.19.2.94807"
-  sha256 "bd19305f90ba635c03426c832fc6aec031cfcef27ae43f7417006a98ca01bac7"
+  version "3.19.2.94902"
+  sha256 "979ca5eb6e36ae65eec3b29c88349007a2fe43de4efb750c18e46ace561d04b3"
 
   url "https://downloads.sparkmailapp.com/Spark#{version.major}/mac/dist/#{version}/Spark.zip"
   name "Spark"
