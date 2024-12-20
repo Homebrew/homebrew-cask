cbadb4c81b9e482e9e4211e151cd7924a7f10ccb
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-12-20 18:28:08 -0500

    macpilot 16.0.1

--- Casks/m/macpilot.rb
+++ Casks/m/macpilot.rb
@@ -10,7 +10,7 @@ cask "macpilot" do
     end
   end
   on_mojave :or_newer do
-    version "16.0"
+    version "16.0.1"
     sha256 :no_check
 
     url "https://www.koingosw.com/products/macpilot/download/macpilot.dmg"
