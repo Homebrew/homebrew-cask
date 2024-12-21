c4c81eb362cb5267f3c8e690b1302713cd0777c6
Klaus Hipp <khipp@users.noreply.github.com>
2024-12-21 12:09:15 +0100

    effect-house 4.9.2

--- Casks/e/effect-house.rb
+++ Casks/e/effect-house.rb
@@ -2,15 +2,15 @@ cask "effect-house" do
   arch arm: "Applesilicon", intel: "Intel"
 
   on_arm do
-    version "4.8.2,0659,11262024,104"
-    sha256 "0b7921b50ddd8aa49d9dbc203ea426b6a24699e4ca1b75d4c1aa861a9f266d63"
+    version "4.9.2,1549,12192024,104"
+    sha256 "0dd0dcbaaed585e1d1fb5bbc8c3661659fdc1c3a334e8688ab9619e9afcd6d92"
   end
   on_intel do
-    version "4.8.2,0664,11262024,104"
-    sha256 "5372fc9b889cc8b50d074a9aff0fdc07b8ae5c5c23cc213a45641c2108d1e6bd"
+    version "4.9.2,1553,12192024,104"
+    sha256 "11587dfb1670d6a313c27f47529f85de69c5a5e6ff789eb7e97286386dc68cd0"
   end
 
-  url "https://sf16-va.tiktokcdn.com/obj/eden-va2/olaa_ajlmml_zlp/ljhwZthlaukjlkulzlp/V#{version.csv.first.no_dots}_ExternalRelease_Builds_#{version.csv.third}/Effect_House_v#{version.csv.first}.#{version.csv.second}_#{arch}_#{version.csv.fourth}.dmg",
+  url "https://sf16-va.tiktokcdn.com/obj/eden-va2/olaa_ajlmml_zlp/ljhwZthlaukjlkulzlp/V#{version.csv.first.no_dots}_ExternalRelease_#{version.csv.third}/Effect_House_v#{version.csv.first}.#{version.csv.second}_#{arch}_#{version.csv.fourth}.dmg",
       verified: "sf16-va.tiktokcdn.com/obj/eden-va2/olaa_ajlmml_zlp/ljhwZthlaukjlkulzlp/"
   name "TikTok Effect House"
   desc "Create vibrant AR effects for TikTok"
