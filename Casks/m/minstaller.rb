75355b78526d052d351c8f9cf81ae1b505c77ea7
Klaus Hipp <khipp@users.noreply.github.com>
2024-12-21 12:44:06 +0100

    minstaller: add legacy version

--- Casks/m/minstaller.rb
+++ Casks/m/minstaller.rb
@@ -1,19 +1,29 @@
 cask "minstaller" do
-  version "3.2.0"
-  sha256 "a802933d33ce854df89ddd7907ab4008c874d34594f3d959ed4afb7e8b8f2127"
+  on_catalina :or_older do
+    version "3.0.2"
+    sha256 "e87e41c78dffaee4c01803b01fc324851de2e482a1c22e6d35a49b37f7f65192"
+
+    livecheck do
+      skip "Legacy version"
+    end
+  end
+  on_big_sur :or_newer do
+    version "3.2.0"
+    sha256 "a802933d33ce854df89ddd7907ab4008c874d34594f3d959ed4afb7e8b8f2127"
+
+    livecheck do
+      url "https://s3.motionvfx.com/mvfxpublic/mInstaller/sparkle/appcast.xml"
+      strategy :sparkle
+    end
+  end
 
   url "https://s3.motionvfx.com/mvfxpublic/mInstaller/sparkle/mInstaller-#{version}.zip"
   name "mInstaller"
   desc "Downloader and manager for MotionVFX products"
   homepage "https://www.motionvfx.com/"
 
-  livecheck do
-    url "https://s3.motionvfx.com/mvfxpublic/mInstaller/sparkle/appcast.xml"
-    strategy :sparkle
-  end
-
   auto_updates true
-  depends_on macos: ">= :big_sur"
+  depends_on macos: ">= :mojave"
 
   app "mInstaller.app"
 
