4cb541bf0890e0acee83986ebc1b22a2e1f35f6d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-22 00:14:59 +0000

    font-playwrite-nl-guides: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

--- Casks/font/font-p/font-playwrite-nl-guides.rb
+++ Casks/font/font-p/font-playwrite-nl-guides.rb
@@ -2,9 +2,10 @@ cask "font-playwrite-nl-guides" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/playwritenlguides/PlaywriteNLGuides-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/playwritenlguides/PlaywriteNLGuides-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Playwrite NL Guides"
-  homepage "https://github.com/TypeTogether/Playwrite"
+  homepage "https://fonts.google.com/specimen/Playwrite+NL+Guides"
 
   font "PlaywriteNLGuides-Regular.ttf"
 
