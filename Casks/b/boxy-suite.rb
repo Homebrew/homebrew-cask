d2166564874544cab4707c80314d9df6ae1902b5
Junian Triajianto <3881052+junian@users.noreply.github.com>
2024-12-22 15:05:07 +0700

    boxy-suite 3.0.0
    Boxy Suite just released v3.
    The binaries now is built as Universal binary, Rosetta no longer mandatory.

--- Casks/b/boxy-suite.rb
+++ Casks/b/boxy-suite.rb
@@ -1,9 +1,9 @@
 cask "boxy-suite" do
-  version "2.1.4"
+  version "3.0.0"
   sha256 :no_check
 
-  url "https://boxyteam-static.s3.amazonaws.com/release/Boxy%20Suite.dmg",
-      verified: "boxyteam-static.s3.amazonaws.com/release/"
+  url "https://boxysuite.s3.amazonaws.com/release/Boxy%20Suite.dmg",
+      verified: "boxysuite.s3.amazonaws.com/release/"
   name "Boxy Suite"
   desc "Gmail, Calendar, Keep and Contacts apps"
   homepage "https://www.boxysuite.com/"
@@ -21,8 +21,4 @@ cask "boxy-suite" do
   app "Boxy for Contacts.app"
 
   zap trash: "~/Library/Caches/co.boxyapp.gmail*"
-
-  caveats do
-    requires_rosetta
-  end
 end
