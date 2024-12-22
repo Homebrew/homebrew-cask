5fd0ce53a48e9140b51cb69e798c5fc79f22e483
Klaus Hipp <khipp@users.noreply.github.com>
2024-12-22 21:36:57 +0100

    redisinsight: update livecheck

--- Casks/r/redisinsight.rb
+++ Casks/r/redisinsight.rb
@@ -10,12 +10,9 @@ cask "redisinsight" do
   desc "GUI for streamlined Redis application development"
   homepage "https://redis.com/redis-enterprise/redis-insight/"
 
-  # The first-party site doesn't publish public version information (the page
-  # requires users to submit contact information to download files). We check
-  # GitHub releases as a best guess of when a new version is released.
   livecheck do
-    url "https://github.com/RedisInsight/RedisInsight"
-    strategy :github_latest
+    url "https://s3.amazonaws.com/redisinsight.download/public/latest/latest-mac.yml"
+    strategy :electron_builder
   end
 
   auto_updates true
