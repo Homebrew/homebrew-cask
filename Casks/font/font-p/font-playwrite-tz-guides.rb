099eeb894f41778e3ffcbbefe76d410d436c511e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-22 00:14:59 +0000

    font-playwrite-tz-guides: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

--- Casks/font/font-p/font-playwrite-tz-guides.rb
+++ Casks/font/font-p/font-playwrite-tz-guides.rb
@@ -2,9 +2,10 @@ cask "font-playwrite-tz-guides" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/playwritetzguides/PlaywriteTZGuides-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/playwritetzguides/PlaywriteTZGuides-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Playwrite TZ Guides"
-  homepage "https://github.com/TypeTogether/Playwrite"
+  homepage "https://fonts.google.com/specimen/Playwrite+TZ+Guides"
 
   font "PlaywriteTZGuides-Regular.ttf"
 
