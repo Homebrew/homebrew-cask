20888ce12dc32f51315aad4551f2cbdd043eeaba
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-22 00:47:52 +0000

    follow@nightly 0.2.8-nightly.20241221

--- Casks/f/follow@nightly.rb
+++ Casks/f/follow@nightly.rb
@@ -1,9 +1,9 @@
 cask "follow@nightly" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.2.8-nightly.20241220"
-  sha256 arm:   "1b2e0bb9528bd055a123a934f87744bde5f3fde0601acaf89435d5b962e61ddb",
-         intel: "473fd8dead9b487b324816da5b7e96a9614cdf77eb14315626550fc360f628ac"
+  version "0.2.8-nightly.20241221"
+  sha256 arm:   "5167405d949a59af63a4ad88e174ab3217dc8ec666de09392522a3b7d7000f76",
+         intel: "77c2908e8a97f0fa230c2297aa15d5cbc4eea51241c4a717fd11fcab96c5484d"
 
   url "https://github.com/RSSNext/Follow/releases/download/#{version}/Follow-#{version}-macos-#{arch}.dmg",
       verified: "github.com/RSSNext/Follow/"
