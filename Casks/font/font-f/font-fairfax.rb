12fd7d9c602553272ba5b1089cca97a9987c140c
Klaus Hipp <khipp@users.noreply.github.com>
2024-12-23 15:23:25 +0100

    font-fairfax 2024-06-01

--- Casks/font/font-f/font-fairfax.rb
+++ Casks/font/font-f/font-fairfax.rb
@@ -1,16 +1,26 @@
 cask "font-fairfax" do
-  version :latest
-  sha256 :no_check
+  version "2024-06-01"
+  sha256 "eaac64504ec44232a90677670e234c6905c05819448847fc1fa0ea44ca24a805"
 
-  url "https://www.kreativekorp.com/swdownload/fonts/relaybm/fairfax.zip",
-      user_agent: :fake
+  url "https://github.com/kreativekorp/open-relay/releases/download/#{version}/Fairfax.zip",
+      verified: "github.com/kreativekorp/open-relay/"
   name "Fairfax"
-  homepage "https://www.kreativekorp.com/software/fonts/constructium.shtml"
+  homepage "https://www.kreativekorp.com/software/fonts/fairfax/"
 
   font "Fairfax.ttf"
   font "FairfaxBold.ttf"
+  font "FairfaxHax.ttf"
+  font "FairfaxHaxBold.ttf"
+  font "FairfaxHaxItalic.ttf"
   font "FairfaxItalic.ttf"
+  font "FairfaxPona.ttf"
+  font "FairfaxPula.ttf"
   font "FairfaxSerif.ttf"
+  font "FairfaxSerifHax.ttf"
+  font "FairfaxSerifSM.ttf"
+  font "FairfaxSM.ttf"
+  font "FairfaxSMBold.ttf"
+  font "FairfaxSMItalic.ttf"
 
   # No zap stanza required
 end
