21bf8729f098401ce26dc647964dcae986370850
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2024-12-23 13:24:30 +0900

    bespoke 1.3.0

--- Casks/b/bespoke.rb
+++ Casks/b/bespoke.rb
@@ -1,6 +1,6 @@
 cask "bespoke" do
-  version "1.2.1"
-  sha256 "85ca71302c002a14bc92958044797dbf0d8b9baa52a753243012347070f4e4bb"
+  version "1.3.0"
+  sha256 "f31f37593fb4e1981701775be8bc1b700608d1ce3ddbdc4da336615d5eff79b6"
 
   url "https://github.com/BespokeSynth/BespokeSynth/releases/download/v#{version}/BespokeSynth-Mac-#{version.dots_to_underscores}.dmg",
       verified: "github.com/BespokeSynth/BespokeSynth/"
