279b4ddf76a4b4e2dd288bdcbb6cfd64f8b28adb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-23 12:43:41 +0000

    expressvpn 11.66.0.89440

--- Casks/e/expressvpn.rb
+++ Casks/e/expressvpn.rb
@@ -1,6 +1,6 @@
 cask "expressvpn" do
-  version "11.65.0.88733"
-  sha256 "683f17d5dc8bdb661696a8e0f1a24ea39aaef88e73e901816efa582d70d21576"
+  version "11.66.0.89440"
+  sha256 "006a3aa27cdcbd8957727ded52a5f10e434f6e588e5a0d4b3ebe45d636ddca1e"
 
   url "https://www.expressvpn.works/clients/mac/expressvpn_mac_#{version}_release.pkg"
   name "ExpressVPN"
