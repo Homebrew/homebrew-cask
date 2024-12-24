7974c18f91bdfbfb1369f4aea8d6dd11b411c466
Loxtera <loxetra@duck.com>
2024-12-24 22:10:39 +0300

    Update and rename ayugram-desktop.rb to ayugram.rb

--- Casks/a/ayugram.rb
+++ Casks/a/ayugram.rb
@@ -1,13 +1,14 @@
-cask "ayugram-desktop" do
+cask "ayugram" do
   version "5.8.3"
   sha256 "7769048139324700659f73f52b703a65abfb71e6768edc3d3298473565a31849"
-  
-  url "https://github.com/AyuGram/AyuGramDesktop/releases/download/v#{version}/AyuGram.dmg"
-  name "AyuGram Desktop"
+
+  # Build from https://github.com/AyuGram/AyuGramDesktop/releases/download/v5.8.3/AyuGram.dmg
+  url "https://huggingface.co/loxtera/ayu-builds-temp/resolve/main/AyuGram.dmg?download=true"
+  name "AyuGram"
   desc "Is a Telegram client with ghost mode and message history"
   homepage "https://github.com/AyuGram/AyuGramDesktop"
-  
-  depends_on macos: ">= :sierra"
+
+  depends_on macos: ">= :high_sierra"
 
   app "AyuGram.app", target: "AyuGram Desktop.app"
 
