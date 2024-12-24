3be1b20d53e6a6a3a47c90fe426addbc170946cd
Loxtera <loxetra@duck.com>
2024-12-24 21:39:37 +0300

    ayugram-desktop 5.8.3 (new cask)

--- Casks/a/ayugram-desktop.rb
+++ Casks/a/ayugram-desktop.rb
@@ -1,11 +1,12 @@
 cask "ayugram-desktop" do
   version "5.8.3"
-  url "https://github.com/AyuGram/AyuGramDesktop/releases/download/v#{version}/AyuGram.dmg"
   sha256 "7769048139324700659f73f52b703a65abfb71e6768edc3d3298473565a31849"
   
+  url "https://github.com/AyuGram/AyuGramDesktop/releases/download/v#{version}/AyuGram.dmg"
   name "AyuGram Desktop"
   desc "Is a Telegram client with ghost mode and message history"
   homepage "https://github.com/AyuGram/AyuGramDesktop"
+  
   depends_on macos: ">= :sierra"
 
   app "AyuGram.app", target: "AyuGram Desktop.app"
