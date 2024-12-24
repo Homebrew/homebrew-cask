b16691c4dbea11de80a8990f893b9a3ee648524f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-24 03:39:46 +0000

    android-studio-preview@canary 2024.3.1.7

--- Casks/a/android-studio-preview@canary.rb
+++ Casks/a/android-studio-preview@canary.rb
@@ -1,9 +1,9 @@
 cask "android-studio-preview@canary" do
   arch arm: "mac_arm", intel: "mac"
 
-  version "2024.3.1.6"
-  sha256 arm:   "3e196e5c2584a6313d24108821a603e70d867a985cccf4f1a426f6cc27d5e180",
-         intel: "06756bb85a2301b71caf67c2c63b68dba52d8fbb28f4ca79943b8265a10cf58d"
+  version "2024.3.1.7"
+  sha256 arm:   "0e8423bfbe70a74d08dee059ff61d228732bb53497bb461bf2923ba42443934c",
+         intel: "1a16067f852a1902c724ad8baaebe8e09719959712abe2b91de7102f4ad48b67"
 
   url "https://redirector.gvt1.com/edgedl/android/studio/install/#{version}/android-studio-#{version}-#{arch}.dmg",
       verified: "redirector.gvt1.com/edgedl/android/studio/install/"
