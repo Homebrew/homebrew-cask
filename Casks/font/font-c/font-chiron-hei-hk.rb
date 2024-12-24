56aa08a3b3887a1bfe751068c9ef3b789a845ab3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-24 03:46:21 +0000

    font-chiron-hei-hk 2.522

--- Casks/font/font-c/font-chiron-hei-hk.rb
+++ Casks/font/font-c/font-chiron-hei-hk.rb
@@ -1,6 +1,6 @@
 cask "font-chiron-hei-hk" do
-  version "2.521"
-  sha256 "506b7c0b3f909c2a37b20a3a7a6065c7dbeb914ef69797137d7d11334448f5aa"
+  version "2.522"
+  sha256 "61add237ae891f495815db63f8fb05635f7d43edf40c3e8b12ebbe40dc6c1233"
 
   url "https://github.com/chiron-fonts/chiron-hei-hk/archive/refs/tags/v#{version}.tar.gz"
   name "Chiron Hei HK"
