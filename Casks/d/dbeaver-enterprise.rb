91f631eff67ef88d77b812ab5c48fe922b6017a6
Maciej Bryński <maciek-github@brynski.pl>
2024-12-24 12:42:05 +0100

    fix: dbeaver-entreprise checksum

--- Casks/d/dbeaver-enterprise.rb
+++ Casks/d/dbeaver-enterprise.rb
@@ -2,7 +2,7 @@ cask "dbeaver-enterprise" do
   arch arm: "aarch64", intel: "x86_64"
 
   version "24.3.0"
-  sha256 arm:   "d2c6054157cf02a4394fecb7a3eee74b417d55e051be907b70bc88983cbf144c",
+  sha256 arm:   "400f62ff9a5ba0b797bc93500a3b97c679c290638b6e8673e973d34299909a80",
          intel: "963d74f10d33d0b92fe0073b4d9d3c5f20122f987198790e9f29404ae933a871"
 
   url "https://dbeaver.com/files/#{version}/dbeaver-ee-#{version}-macos-#{arch}.dmg"
