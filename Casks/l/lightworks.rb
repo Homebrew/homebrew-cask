fd0781afae561a311cf4b29aff92f42bdbfa561f
Klaus Hipp <khipp@users.noreply.github.com>
2024-12-25 19:41:10 +0100

    lightworks 2025.1,149111

--- Casks/l/lightworks.rb
+++ Casks/l/lightworks.rb
@@ -1,6 +1,6 @@
 cask "lightworks" do
-  version "2023.2,146471"
-  sha256 "663a74cb22a4b6bbd6e5543881d573de1b94630cb48e2ee797b47a848487d6f2"
+  version "2025.1,149111"
+  sha256 "848940400a620ccf6f178cb7962af7e0fc9f14f002d5797d3af1697baa15d307"
 
   url "https://cdn.lwks.com/releases/#{version.csv.first}/lightworks_#{version.csv.first.major_minor}_r#{version.csv.second}.dmg"
   name "Lightworks"
@@ -11,7 +11,13 @@ cask "lightworks" do
     skip "No version information available"
   end
 
+  depends_on macos: ">= :big_sur"
+
   app "Lightworks.app"
 
   zap trash: "~/Library/Saved Application State/com.editshare.lightworks.savedState"
+
+  caveats do
+    requires_rosetta
+  end
 end
