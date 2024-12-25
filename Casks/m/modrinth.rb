3b90365b2307257457e59daefa9160ebd3b8bf55
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-25 06:45:21 +0000

    modrinth 0.9.1

--- Casks/m/modrinth.rb
+++ Casks/m/modrinth.rb
@@ -1,6 +1,6 @@
 cask "modrinth" do
-  version "0.9.0"
-  sha256 "f2e4d69f3a40bbf57ca880fa2c9d5ed91be7b105ae95c16dfcc905a684e621a2"
+  version "0.9.1"
+  sha256 "cdcdde84f7e38ddbd0bda758d733c4bc61399001c9f61512f50588ec72f0edcd"
 
   url "https://launcher-files.modrinth.com/versions/#{version}/macos/Modrinth%20App_#{version}_universal.dmg"
   name "Modrinth App"
