18de20d123bc00661d1d54f4e3b4bf21271a51f6
grey-shirt <grey-shirt@hotmail.com>
2024-12-26 17:58:07 +0000

    Fix verified url

--- Casks/l/libresprite.rb
+++ Casks/l/libresprite.rb
@@ -2,7 +2,7 @@ cask "libresprite" do
     version "1.0"
     sha256 "85004254166204291a9c08007592cb6d38ac89a01faec3aa37c037a39d86b9eb"
 
-    url "https://github.com/LibreSprite/LibreSprite/releases/download/v#{version}/LibreSprite-macos-x86_64.zip"
+    url "https://github.com/LibreSprite/LibreSprite/releases/download/v#{version}/LibreSprite-macos-x86_64.zip",
       verified: "github.com/LibreSprite/LibreSprite/"
     name "LibreSprite"
     desc "Open-source program for creating 2D animations, pixel art, and sprites"
