fb10dae856cdb7d57efc67a1c43ea3c42b92a9a9
grey-shirt <grey-shirt@hotmail.com>
2024-12-26 17:12:56 +0000

    Fix whitespace

--- Casks/l/libresprite.rb
+++ Casks/l/libresprite.rb
@@ -1,12 +1,12 @@
 cask "libresprite" do
-    version "1.1"
+    version "1.0"
     sha256 "e76532c3bc435d9fd4dd6a6f4d6618f46a9c6b006699882f544e9a316eec658c"
 
-    url "https://github.com/LibreSprite/LibreSprite/releases/download/v#{version}/libresprite-development-macos-arm64.zip"
+    url "https://github.com/LibreSprite/LibreSprite/releases/download/v#{version}/LibreSprite-macos-x86_64.zip"
     name "LibreSprite"
     desc "Open-source program for creating 2D animations, pixel art, and sprites"
     homepage "https://libresprite.github.io/"
-  
+
     livecheck do
       url "https://github.com/LibreSprite/LibreSprite/releases"
       strategy :github_latest
@@ -15,7 +15,7 @@ cask "libresprite" do
     depends_on macos: ">= :big_sur"
   
     app "LibreSprite.app"
-  
+
     zap trash: [
       "~/Library/Preferences/org.libresprite.Libresprite.plist",
       "~/Library/Application Support/LibreSprite",
