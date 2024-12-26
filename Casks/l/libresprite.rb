36f583a12ed96e349ace3b0ca302a7bedc69c668
grey-shirt <grey-shirt@hotmail.com>
2024-12-26 18:02:13 +0000

    Fix indentation

--- Casks/l/libresprite.rb
+++ Casks/l/libresprite.rb
@@ -1,28 +1,28 @@
 cask "libresprite" do
-    version "1.0"
-    sha256 "85004254166204291a9c08007592cb6d38ac89a01faec3aa37c037a39d86b9eb"
+  version "1.0"
+  sha256 "85004254166204291a9c08007592cb6d38ac89a01faec3aa37c037a39d86b9eb"
 
-    url "https://github.com/LibreSprite/LibreSprite/releases/download/v#{version}/LibreSprite-macos-x86_64.zip",
-      verified: "github.com/LibreSprite/LibreSprite/"
-    name "LibreSprite"
-    desc "Open-source program for creating 2D animations, pixel art, and sprites"
-    homepage "https://libresprite.github.io/"
+  url "https://github.com/LibreSprite/LibreSprite/releases/download/v#{version}/LibreSprite-macos-x86_64.zip",
+    verified: "github.com/LibreSprite/LibreSprite/"
+  name "LibreSprite"
+  desc "Open-source program for creating 2D animations, pixel art, and sprites"
+  homepage "https://libresprite.github.io/"
 
-    livecheck do
-      url "https://github.com/LibreSprite/LibreSprite/releases"
-      strategy :github_latest
-    end
+  livecheck do
+    url "https://github.com/LibreSprite/LibreSprite/releases"
+    strategy :github_latest
+  end
 
-    depends_on macos: ">= :big_sur"
+  depends_on macos: ">= :big_sur"
   
-    app "LibreSprite.app"
+  app "LibreSprite.app"
 
-    zap trash: [
-        "~/Library/Application Support/LibreSprite",
-        "~/Library/Preferences/org.libresprite.Libresprite.plist",
-    ]
+  zap trash: [
+    "~/Library/Application Support/LibreSprite",
+    "~/Library/Preferences/org.libresprite.Libresprite.plist",
+  ]
 
-    caveats do
-        requires_rosetta
-      end
+  caveats do
+    requires_rosetta
+  end
 end
