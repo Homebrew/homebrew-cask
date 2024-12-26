8b66ebf2c48815a6c1eef4fe11cf97468debd97f
grey-shirt <grey-shirt@hotmail.com>
2024-12-26 10:01:24 +0000

    Fix whitespace

--- Casks/l/libresprite.rb
+++ Casks/l/libresprite.rb
@@ -1,5 +1,5 @@
 cask "libresprite" do
-    version "1.1" 
+    version "1.1"
     sha256 "e76532c3bc435d9fd4dd6a6f4d6618f46a9c6b006699882f544e9a316eec658c"
 
     url "https://github.com/LibreSprite/LibreSprite/releases/download/v#{version}/libresprite-development-macos-arm64.zip"
@@ -21,4 +21,3 @@ cask "libresprite" do
       "~/Library/Application Support/LibreSprite",
     ]
   end
-  
