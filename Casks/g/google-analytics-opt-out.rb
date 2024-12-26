5e7aa39bdd7137bf6b3cfb012771b482136a9e50
Klaus Hipp <khipp@users.noreply.github.com>
2024-12-26 11:50:11 +0100

    google-analytics-opt-out 1.0.2

--- Casks/g/google-analytics-opt-out.rb
+++ Casks/g/google-analytics-opt-out.rb
@@ -1,8 +1,8 @@
 cask "google-analytics-opt-out" do
-  version "1.0.1"
-  sha256 "ffe34fd0cb98bd0b9e29498af40876d65313d61b5bad5a1c388be35626c5e457"
+  version "1.0.2"
+  sha256 "3062d1470d6a18efe2afef62e57ab3632a80b29e6b94497f34332f47e960c748"
 
-  url "https://dl.google.com/analytics/optout/safari/GoogleAnalyticsOptOut_#{version}.dmg"
+  url "https://dl.google.com/release2/Analytics-Opt-Out/akrwr324vy2vt735ezemhey3ui_#{version}/GoogleAnalyticsOptOut.dmg"
   name "Google Analytics Opt Out"
   desc "Prevent website visitor's data from being used by Google Analytics JavaScript"
   homepage "https://tools.google.com/dlpage/gaoptout"
@@ -11,7 +11,7 @@ cask "google-analytics-opt-out" do
     skip "No version information available"
   end
 
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :high_sierra"
 
   app "Google Analytics Opt Out.app"
 
