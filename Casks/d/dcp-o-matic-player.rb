ac323590d2953902745c90da36b8efbe9be9dd06
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-27 03:42:44 +0000

    dcp-o-matic-player 2.18.1

--- Casks/d/dcp-o-matic-player.rb
+++ Casks/d/dcp-o-matic-player.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-player" do
-  version "2.18.0"
-  sha256 "cb4402fa80a173c7c558f776ea021a114ead819949de6d1ead8e8f389d50fa43"
+  version "2.18.1"
+  sha256 "1720dbb9655c9bec11a3700a8d056f9c796fd375f5d25dc8bf24f60b0a6f42d4"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-player&version=#{version}"
   name "DCP-o-matic Player"
