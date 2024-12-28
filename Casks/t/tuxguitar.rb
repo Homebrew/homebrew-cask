b40e32ad40e1295ec91fe9bc0cbfc22abe9fc315
eleanordoesntcode <166862894+eleanordoesntcode@users.noreply.github.com>
2024-12-28 01:15:07 +0100

    tuxguitar 1.6.6

--- Casks/t/tuxguitar.rb
+++ Casks/t/tuxguitar.rb
@@ -1,6 +1,6 @@
 cask "tuxguitar" do
-  version "1.6.5"
-  sha256 "cfd8b7b35a569fc60e6d704e07a6f3b3b3359d747323d3eb04e5e9b9e3d3798b"
+  version "1.6.6"
+  sha256 "87304ed1608495652645ec29d86bad56946618545575a25d87299bca39c9bead"
 
   url "https://github.com/helge17/tuxguitar/releases/download/#{version}/tuxguitar-#{version}-macosx-swt-cocoa-x86_64.app.tar.gz",
       verified: "github.com/helge17/tuxguitar/"
