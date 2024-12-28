414d3dcabcca5f1a70c8935a9f47a165276b6eaf
ellie is not coding in the slightest <166862894+eleanordoesntcode@users.noreply.github.com>
2024-12-28 21:37:29 +0100

    pixpin: update website and livecheck

--- Casks/p/pixpin.rb
+++ Casks/p/pixpin.rb
@@ -2,14 +2,14 @@ cask "pixpin" do
   version "1.9.5.0"
   sha256 "30e43609473a4894d880c8c0774f43c1d91497df9639c0e72ceef7d9ca275970"
 
-  url "https://download.pixpinapp.com/PixPin_#{version}.dmg"
+  url "https://download.pixpin.cn/PixPin_#{version}.dmg"
   name "PixPin"
   desc "Screenshot tool"
-  homepage "https://pixpinapp.com/"
+  homepage "https://pixpin.cn/"
 
   livecheck do
     url :homepage
-    regex(/href=.*?PixPin[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+    regex(%r{href="/docs/official-log/v?(\d+(?:\.\d+)+)}i)
   end
 
   auto_updates true
