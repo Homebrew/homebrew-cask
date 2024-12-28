8e094155526c83e41b2452dad9bd5ea15b3d1db4
ellie is not coding in the slightest <166862894+eleanordoesntcode@users.noreply.github.com>
2024-12-28 12:59:30 +0100

    koodo-reader 1.7.2

--- Casks/k/koodo-reader.rb
+++ Casks/k/koodo-reader.rb
@@ -1,11 +1,11 @@
 cask "koodo-reader" do
-  arch arm: "-arm64"
+  arch arm: "arm64", intel: "x64"
 
-  version "1.5.1"
-  sha256 arm:   "29939046e324835e2054d251e83f252992c51b5a45ae47bedeb2dd8d7cd82fa8",
-         intel: "6f06f60bc5ef34e947be07874914641cddd6379fe676b11bcdbb79493c9a4d79"
+  version "1.7.2"
+  sha256 arm:   "911cd8738da1c75d76547ab2fe88c74f75f401306e512ec9b3b4dddbf3c8210b",
+         intel: "dbe2434f40996e4e73a4ddc096be34e6036ebbf772d55c65ac0cf740bdb8beac"
 
-  url "https://github.com/troyeguo/koodo-reader/releases/download/v#{version}/Koodo-Reader-#{version}#{arch}.dmg",
+  url "https://github.com/troyeguo/koodo-reader/releases/download/v#{version}/Koodo-Reader-#{version}-#{arch}.dmg",
       verified: "github.com/troyeguo/koodo-reader/"
   name "Koodo Reader"
   desc "Open-source epub reader"
