ec1fade0a689a6d345f25f0b48e68eed6ba5a1b2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-30 21:47:15 +0000

    teleport-connect 17.1.2

--- Casks/t/teleport-connect.rb
+++ Casks/t/teleport-connect.rb
@@ -1,6 +1,6 @@
 cask "teleport-connect" do
-  version "17.1.1"
-  sha256 "e1435d41e1fb4a9ddb6c77ee1e6ce8d7e6be6b695f95a96cece966950d45c7a5"
+  version "17.1.2"
+  sha256 "35dd9920db052a445cc5c8c70351d27d19727c4d19480eb4a3cd14a291c33b35"
 
   url "https://cdn.teleport.dev/Teleport%20Connect-#{version}.dmg",
       verified: "cdn.teleport.dev/"
