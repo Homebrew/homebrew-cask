d5625153ee425cad7bbd014a4b3f5f0c10f3bb11
Matthew Rogers <matt@matthewrogers.org>
2024-12-30 18:32:37 -0500

    Add netVentory: 0.3.0n

--- Casks/n/netventory.rb
+++ Casks/n/netventory.rb
@@ -1,30 +1,30 @@
 cask "netventory" do
   version "0.3.0n"
+  sha256 arm:   "1046a11b9a7420120ce1ca9b0060f10b25028a8b3d3178bd6ebbf6842d34d825",
+         intel: "fea855f421a632353e6e7e3e4ea304044bea49613b74d45f2621080f046c056b"
 
   on_arm do
-    sha256 "1046a11b9a7420120ce1ca9b0060f10b25028a8b3d3178bd6ebbf6842d34d825"
+    url "https://github.com/ramborogers/netventory/releases/download/#{version}/netventory-darwin-arm64",
+        verified: "github.com/ramborogers/netventory/"
 
-    url "https://github.com/ramborogers/netventory/releases/download/#{version}/netventory-darwin-arm64"
-
-    binary "netventory-darwin-arm64", target: "netventory"
+    binary "#{staged_path}/netventory-darwin-arm64", target: "netventory"
   end
   on_intel do
-    sha256 "fea855f421a632353e6e7e3e4ea304044bea49613b74d45f2621080f046c056b"
+    url "https://github.com/ramborogers/netventory/releases/download/#{version}/netventory-darwin-amd64",
+        verified: "github.com/ramborogers/netventory/"
 
-    url "https://github.com/ramborogers/netventory/releases/download/#{version}/netventory-darwin-amd64"
-
-    binary "netventory-darwin-amd64", target: "netventory"
+    binary "#{staged_path}/netventory-darwin-amd64", target: "netventory"
   end
 
   name "Netventory"
   desc "Network inventory and discovery tool"
   homepage "https://github.com/ramborogers/netventory"
 
-  # Optional: Test that the binary works
   postflight do
-    system_command "#{staged_path}/netventory", args: ["--version"], print_stderr: false
+    # Remove quarantine attribute to allow execution of unsigned binary
+    system_command "xattr",
+                   args: ["-d", "com.apple.quarantine", "#{staged_path}/netventory-darwin-#{Hardware::CPU.arch}"]
   end
 
-  # Uninstallation cleanup if required
-  zap trash: "~/Library/Preferences/netventory"
+  zap trash: "~/.netventory"
 end
