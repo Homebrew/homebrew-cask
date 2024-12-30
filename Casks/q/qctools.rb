632e47272ad3f6ec164c4c22dd2babe3b02a0a0a
Justin Krehel <39449589+krehel@users.noreply.github.com>
2024-12-30 10:34:00 -0500

    qctools 1.4

--- Casks/q/qctools.rb
+++ Casks/q/qctools.rb
@@ -1,6 +1,6 @@
 cask "qctools" do
-  version "1.3.1"
-  sha256 "8151679f9e648e00008f60810dcb29632308fb78a2053ca178686e47dd9a3eea"
+  version "1.4"
+  sha256 "0023f0475cc22fbd87e5649db586db9b8c6f34d57563c2087b43b2d32b920fca"
 
   url "https://mediaarea.net/download/binary/qctools/#{version}/QCTools_#{version}_mac.dmg"
   name "QCTools"
