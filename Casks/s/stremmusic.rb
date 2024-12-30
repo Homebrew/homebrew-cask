af5053afeada8bfc512a667706b1a33a71e278b6
Bowen Ji <jibwf@hotmail.com>
2024-12-31 07:40:18 +0800

    update livecheck url, zap, bump to 1.3.4

--- Casks/s/stremmusic.rb
+++ Casks/s/stremmusic.rb
@@ -2,15 +2,14 @@ cask "streammusic" do
   version "1.3.4"
   sha256 "b33347422b5f823266f853f5b24b804d801dc73743bc5cbb4db12c7ece81bf21"
 
-  url "https://github.com/gitbobobo/StreamMusic/releases/download/v#{version}/StreamMusic_#{version}.dmg",
-      verified: "github.com/gitbobobo/StreamMusic/"
+  url "https://github.com/gitbobobo/StreamMusic/releases/download/#{version}/StreamMusic_#{version}.dmg"
   name "StreamMusic"
   desc "Music client compatible with self-hosted music services"
   homepage "https://www.aqzscn.cn/"
 
   livecheck do
     url :url
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    regex(/.*v?(\d+(?:\.\d+)+(?:\.\d+)+)\.dmg/i)
   end
 
   depends_on macos: ">= :catalina"
@@ -19,8 +18,8 @@ cask "streammusic" do
 
   zap trash: [
     "~/Library/Application Scripts/cn.aqzscn.streamMusic",
-    "~/Library/Application Support/cn.aqzscn.streamMusic",
     "~/Library/Containers/cn.aqzscn.streamMusic",
-    "~/Library/Preferences/cn.aqzscn.streamMusic.plist",
   ]
 end
+
+
