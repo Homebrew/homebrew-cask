eaf48db35c4c985029bac70a1a0fdccb205022a8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-30 09:50:35 +0000

    vbrokers 2.5.407

--- Casks/v/vbrokers.rb
+++ Casks/v/vbrokers.rb
@@ -1,6 +1,6 @@
 cask "vbrokers" do
-  version "2.5.320,600001,1734336440"
-  sha256 "383781813bfa4b52d8820a11d9218de3d553db746409768141c07c23208af44d"
+  version "2.5.407,600001,1735547744"
+  sha256 "b4c4357838489d9126e6da52c655fc8ae83e674d24b34f2720612a5c3f7dd08a"
 
   url "https://r.hstong.com/public/app/Mac/HSTONG_Mac_#{version.csv.second}_#{version.csv.first}_#{version.csv.third}.dmg"
   name "VBrokers"
