83cec85d80fc34c87382095f05ab317053b903f2
Matthew Rogers <matt@matthewrogers.org>
2024-12-30 18:13:43 -0500

    Add netVentory: 0.3.0n

--- Casks/n/netventory.rb
+++ Casks/n/netventory.rb
@@ -1,14 +1,19 @@
 cask "netventory" do
   version "0.3.0n"
 
-  if Hardware::CPU.intel?
-    sha256 "fea855f421a632353e6e7e3e4ea304044bea49613b74d45f2621080f046c056b"
-    url "https://github.com/ramborogers/netventory/releases/download/#{version}/netventory-darwin-amd64"
-    binary "netventory-darwin-amd64", target: "netventory"
-  else
+  on_arm do
     sha256 "1046a11b9a7420120ce1ca9b0060f10b25028a8b3d3178bd6ebbf6842d34d825"
+
     url "https://github.com/ramborogers/netventory/releases/download/#{version}/netventory-darwin-arm64"
+
     binary "netventory-darwin-arm64", target: "netventory"
+  end
+  on_intel do
+    sha256 "fea855f421a632353e6e7e3e4ea304044bea49613b74d45f2621080f046c056b"
+
+    url "https://github.com/ramborogers/netventory/releases/download/#{version}/netventory-darwin-amd64"
+
+    binary "netventory-darwin-amd64", target: "netventory"
   end
 
   name "Netventory"
@@ -23,4 +28,3 @@ cask "netventory" do
   # Uninstallation cleanup if required
   zap trash: "~/Library/Preferences/netventory"
 end
-
