b51e61f5f9412d40d26c1719edf2b8e1125c0384
Mathijs Meulendijks <mathijs.meulendijks@gmail.com>
2024-12-31 13:28:07 +0100

    Green-Go Control 5.1.0 (new cask)
    This commit adds a new cask for Green-Go Connect, a management package for the Green-Go digital IP-based intercom system.

---
+++ Casks/g/green-go-control.rb
@@ -0,0 +1,30 @@
+cask "green-go-control" do
+  version "5.1.0"
+  sha256 "5e654801997ae166c0218b4e1fd0cafe011b4361714904dc2fcb7b56698dd72e"
+
+  url "https://downloads.greengoconnect.com/#{version}/macos/green-go-control.dmg",
+      verified: "downloads.greengoconnect.com/"
+  name "Green-GO Control"
+  desc "Configure and manage Green-GO intercom systems"
+  homepage "https://www.greengodigital.com/"
+
+  livecheck do
+    url "https://manual.greengoconnect.com/en/release-notes/software/"
+    regex(%r{href=.*?/(\d+(?:\.\d+)+)/macos/green-go-control.dmg}i)
+  end
+
+  depends_on macos: ">= :big_sur"
+
+  app "Green-GO Control.app"
+
+  zap trash: [
+    "~/Library/Application Support/green-go-control",
+    "~/Library/Logs/green-go-control",
+    "~/Library/Preferences/com.green-go.control.plist",
+    "~/Library/Saved Application State/com.green-go.control.savedState",
+  ]
+
+  caveats do
+    requires_rosetta
+  end
+end
