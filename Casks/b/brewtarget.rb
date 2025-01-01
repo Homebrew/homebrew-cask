47d6d08cc2772dc2a70171df70d04a4c4d952b1f
Klaus Hipp <khipp@users.noreply.github.com>
2025-01-01 13:57:10 +0100

    brewtarget: update homepage

--- Casks/b/brewtarget.rb
+++ Casks/b/brewtarget.rb
@@ -2,10 +2,11 @@ cask "brewtarget" do
   version "4.0.15"
   sha256 "1c8b0eaa0be89b5708ff3d24b75e9e736e93d58cf4cbb7521d655b9438a359f9"
 
-  url "https://github.com/Brewtarget/brewtarget/releases/download/v#{version}/brewtarget_#{version}_MacOS.dmg"
+  url "https://github.com/Brewtarget/brewtarget/releases/download/v#{version}/brewtarget_#{version}_MacOS.dmg",
+      verified: "github.com/Brewtarget/brewtarget/"
   name "brewtarget"
   desc "Beer recipe creation tool"
-  homepage "https://github.com/Brewtarget/brewtarget/"
+  homepage "https://www.brewtarget.beer/"
 
   app "brewtarget_#{version}_MacOS.app"
 
