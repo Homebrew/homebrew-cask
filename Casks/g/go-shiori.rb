e63f235ef41b48d0bdba196d8a51c7ef27f94fd3
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-01-02 08:49:46 +0900

    go-shiori 1.7.3

--- Casks/g/go-shiori.rb
+++ Casks/g/go-shiori.rb
@@ -1,9 +1,9 @@
 cask "go-shiori" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.7.2"
-  sha256 arm:   "fcfd29dfb65ae14f36118568cc22979b7154f45f5c8319a84ed90ce1849bc390",
-         intel: "0bb2fb24b33d2a554bac0517a40bf3d3da1ad135a7389a98fb7c6ffe2ea1ff14"
+  version "1.7.3"
+  sha256 arm:   "170737f612c93222bf42e00a1e7e5c3887beb88420a37b5b511aab1c83771d14",
+         intel: "daf753c9e30abed808b47065499e821ae8e99fb9220c380c0ef83dd10d407757"
 
   url "https://github.com/go-shiori/shiori/releases/download/v#{version}/shiori_Darwin_#{arch}_#{version}.tar.gz"
   name "Shiori"
