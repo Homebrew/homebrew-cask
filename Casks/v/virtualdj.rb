90cdc6031f6fa867a84b508967b2e547e78e8b4a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-02 19:04:03 +0000

    virtualdj 2025,8490,566872A1

--- Casks/v/virtualdj.rb
+++ Casks/v/virtualdj.rb
@@ -1,6 +1,6 @@
 cask "virtualdj" do
-  version "2025,8472,397CC2A2"
-  sha256 "29a1f49484787847fb8e192cc37e6e6c042eac37e2586f7002af1bd7835f2883"
+  version "2025,8490,566872A1"
+  sha256 "3d38da6544893e3f26fb4db76fbac52da2b029071b7e3f4bab2fee643cd05967"
 
   url "https://download.virtualdj.com/b#{version.csv.second}/#{version.csv.third}/install_virtualdj_#{version.csv.first}_b#{version.csv.second}_mac.pkg"
   name "VirtualDJ"
