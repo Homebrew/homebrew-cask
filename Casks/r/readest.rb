baadf508f2c596af7899573263b446897288f32f
Bevan Kay <email@bevankay.me>
2025-01-02 22:40:59 +1100

    readest 0.8.8 (new cask)

---
+++ Casks/r/readest.rb
@@ -0,0 +1,23 @@
+cask "readest" do
+  version "0.8.8"
+  sha256 "5d3d5b7815d82c99c773db731b42c33534d698fa63caedb64633a086592fdce7"
+
+  url "https://github.com/readest/readest/releases/download/v#{version}/Readest_#{version}_universal.dmg",
+      verified: "github.com/readest/readest/"
+  name "Readest"
+  desc "Ebook reader"
+  homepage "https://readest.com/"
+
+  depends_on macos: ">= :monterey"
+
+  app "Readest.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.bilingify.readest",
+    "~/Library/Caches/com.bilingify.readest",
+    "~/Library/Caches/readest",
+    "~/Library/Preferences/com.bilingify.readest.plist",
+    "~/Library/WebKit/com.bilingify.readest",
+    "~/Library/WebKit/readest",
+  ]
+end
