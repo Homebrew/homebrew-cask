ec7ade8426b461fde51c9ea1f0ddf72e88072b3b
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-01-03 15:30:59 +0900

    codelite 18.0.0

--- Casks/c/codelite.rb
+++ Casks/c/codelite.rb
@@ -1,25 +1,60 @@
 cask "codelite" do
-  arch arm: "arm64", intel: "x86_64"
+  on_arm do
+    on_monterey :or_older do
+      arch arm: "arm64"
 
-  version "17.0.0"
-  sha256 arm:   "61e3cdfdbaa290feb9e35eab31375b1cb9af0ba1f7e4d33198fd9e293f636b5c",
-         intel: "42dd6bf46159c687a5739a3bbfaf925bcb16d27b52e6ff237f14f06f1221bbc9"
+      version "17.2.1"
+      sha256 "17704c86a98881ad08a77185707b0d26670a019645d359acbbb62f0bf13e7e52"
+
+      livecheck do
+        skip "Legacy version"
+      end
+    end
+    on_ventura do
+      arch arm: "arm64"
+
+      version "17.6.0"
+      sha256 "479058e8a32e6932198bf9bbbf141346252a6b5140f685d5584369500ccf4f28"
+
+      livecheck do
+        skip "Legacy version"
+      end
+    end
+    on_sonoma :or_newer do
+      arch arm: "macOS_14.7.2-arm64"
+
+      version "18.0.0"
+      sha256 "ebaa1649142033bbb7eeff2c2e4f2dab9deefd61c0304dfe4aa9c36012cf7dc6"
+
+      livecheck do
+        url "https://downloads.codelite.org/"
+        regex(/CodeLite\s*(\d+\.\d+)((?:\.\d+)*)\s*-\s*Stable/i)
+        strategy :page_match do |page, regex|
+          match = page.match(regex)
+          next if match.blank?
+
+          "#{match[1]}#{match[2].presence || ".0"}"
+        end
+      end
+    end
+  end
+  on_intel do
+    arch intel: "x86_64"
+
+    version "17.6.0"
+    sha256 "e13755ab8edbc6e213269d73b206935c102d5b8ae2c0b3da24ae0a87cab22f57"
+
+    livecheck do
+      skip "Legacy version"
+    end
+  end
 
   url "https://downloads.codelite.org/codelite/#{version}/codelite-#{arch}.app.tar.gz"
   name "CodeLite"
   desc "IDE for C, C++, PHP and Node.js"
   homepage "https://codelite.org/"
 
-  livecheck do
-    url "https://downloads.codelite.org/"
-    regex(/CodeLite\s*(\d+\.\d+)((?:\.\d+)*)\s*-\s*Stable/i)
-    strategy :page_match do |page, regex|
-      match = page.match(regex)
-      next if match.blank?
-
-      "#{match[1]}#{match[2].presence || ".0"}"
-    end
-  end
+  depends_on macos: ">= :monterey"
 
   app "codelite.app"
 
