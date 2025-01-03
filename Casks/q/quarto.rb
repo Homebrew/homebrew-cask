cbe6bc590a8ef6b447f7c93e8fa1096bb4038b22
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-03 21:46:09 +0000

    quarto 1.6.40

--- Casks/q/quarto.rb
+++ Casks/q/quarto.rb
@@ -1,6 +1,6 @@
 cask "quarto" do
-  version "1.6.39"
-  sha256 "3181cebb7cfbdfbfba568e453fbbf6fb084f5f8c43576c553208e3e3baffa2b5"
+  version "1.6.40"
+  sha256 "90e91b8be10ef9d5d3797890742c381bf64bfcb1f20f15b5a29cbefae02ced05"
 
   url "https://github.com/quarto-dev/quarto-cli/releases/download/v#{version}/quarto-#{version}-macos.pkg",
       verified: "github.com/quarto-dev/quarto-cli/"
