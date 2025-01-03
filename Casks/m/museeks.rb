8ffb325e6ad77696668e790022b38a8dd1bd38b0
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-01-02 19:58:58 -0500

    museeks 0.20.1

--- Casks/m/museeks.rb
+++ Casks/m/museeks.rb
@@ -1,6 +1,6 @@
 cask "museeks" do
-  version "0.20.0"
-  sha256 "59e537574e866a76e4106c1b358ffa1739bcffa5f629467b761970f900906b91"
+  version "0.20.1"
+  sha256 "53ec6a7f2b30aca6e4b1e0d1efac5bbb2e3b9d5132b4d295d38a6db5bee20c29"
 
   url "https://github.com/martpie/museeks/releases/download/#{version}/Museeks_#{version}_universal.dmg",
       verified: "github.com/martpie/museeks/"
