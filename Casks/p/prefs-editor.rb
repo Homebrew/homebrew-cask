f6fccaea4b3ab6616bf38a8e0a4a743190debf06
Klaus Hipp <khipp@users.noreply.github.com>
2025-01-03 20:45:51 +0100

    prefs-editor: update livecheck, use versioned url

--- Casks/p/prefs-editor.rb
+++ Casks/p/prefs-editor.rb
@@ -1,15 +1,15 @@
 cask "prefs-editor" do
-  version "1.4.1.3.1"
-  sha256 :no_check
+  version "1.4.1"
+  sha256 "bfa48939c288455b0f3b7647759d6bb59d022a046209b408290341a6493705a8"
 
-  url "https://files.tempel.org/Various/OSX_Prefs_Editor/PrefsEditor.zip"
+  url "https://files.tempel.org/Various/OSX_Prefs_Editor/PrefsEditor-#{version}.zip"
   name "Prefs Editor"
   desc "Graphical user interface for the 'defaults' command"
   homepage "https://apps.tempel.org/PrefsEditor/"
 
   livecheck do
     url "https://apps.tempel.org/PrefsEditor/appcast.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   auto_updates true
