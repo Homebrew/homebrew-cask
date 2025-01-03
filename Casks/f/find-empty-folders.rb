9c6ee740531679a64cd5dabdd798a5f1f4ef545f
Klaus Hipp <khipp@users.noreply.github.com>
2025-01-03 20:42:35 +0100

    find-empty-folders: update livecheck, use versioned url

--- Casks/f/find-empty-folders.rb
+++ Casks/f/find-empty-folders.rb
@@ -1,15 +1,15 @@
 cask "find-empty-folders" do
   version "1.3.1"
-  sha256 :no_check
+  sha256 "3b494e5015d16bf8658b0685f34a79098f23bb166f8881d27ae6bca6668ce77b"
 
-  url "https://files.tempel.org/FindEmptyFolders/FindEmptyFolders.zip"
+  url "https://files.tempel.org/FindEmptyFolders/FindEmptyFolders-#{version}.zip"
   name "Find Empty Folders"
   desc "Finds empty folders"
   homepage "https://www.tempel.org/FindEmptyFolders"
 
   livecheck do
-    url :homepage
-    regex(/>\s*Current\s*Version:\s*v?(\d+(?:\.\d+)+)\s*/i)
+    url "https://files.tempel.org/FindEmptyFolders/"
+    regex(/href=.*?FindEmptyFolders[._-]v?(\d+(?:\.\d+)+)\.zip/i)
   end
 
   app "Find Empty Folders.app"
