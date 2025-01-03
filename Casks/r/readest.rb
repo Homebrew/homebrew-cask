6a25fe23c39a7d49e74fe98a07980c1212699168
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-01-03 15:49:35 +0900

    readest 0.8.9

--- Casks/r/readest.rb
+++ Casks/r/readest.rb
@@ -1,6 +1,6 @@
 cask "readest" do
-  version "0.8.8"
-  sha256 "5d3d5b7815d82c99c773db731b42c33534d698fa63caedb64633a086592fdce7"
+  version "0.8.9"
+  sha256 "305281f4f3850144065aa23475eb18c92ede3519f4dfbe0b5c9b1559c867b0d8"
 
   url "https://github.com/readest/readest/releases/download/v#{version}/Readest_#{version}_universal.dmg",
       verified: "github.com/readest/readest/"
