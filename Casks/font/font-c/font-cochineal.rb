4574d655eb0d4c5477107c0aeafab5e3d1d415d1
Klaus Hipp <khipp@users.noreply.github.com>
2025-01-04 19:40:22 +0100

    font-cochineal: add livecheck

--- Casks/font/font-c/font-cochineal.rb
+++ Casks/font/font-c/font-cochineal.rb
@@ -1,10 +1,15 @@
 cask "font-cochineal" do
-  version :latest
+  version "1.085"
   sha256 :no_check
 
   url "https://mirrors.ctan.org/fonts/cochineal.zip"
   name "Cochineal"
   homepage "https://ctan.org/pkg/cochineal"
+
+  livecheck do
+    url :homepage
+    regex(/Version.*?v?(\d+(?:\.\d+)+)/i)
+  end
 
   font "cochineal/opentype/Cochineal-Bold.otf"
   font "cochineal/opentype/Cochineal-BoldItalic.otf"
