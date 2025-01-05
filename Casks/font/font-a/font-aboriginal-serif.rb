671b27f39fa52e177a4f5026111b9665a9f66816
Klaus Hipp <khipp@users.noreply.github.com>
2025-01-05 15:26:28 +0100

    font-aboriginal-serif: add livecheck

--- Casks/font/font-a/font-aboriginal-serif.rb
+++ Casks/font/font-a/font-aboriginal-serif.rb
@@ -1,10 +1,15 @@
 cask "font-aboriginal-serif" do
-  version :latest
+  version "9.606"
   sha256 :no_check
 
   url "https://www.languagegeek.com/font/AboriginalSerif.zip"
   name "Aboriginal Serif"
   homepage "https://www.languagegeek.com/font/fontdownload.html"
+
+  livecheck do
+    url :homepage
+    regex(/Aboriginal\s+Serif.*?\(\s*v?(\d+(?:\.\d+)*)[^)]*\)/i)
+  end
 
   font "AboriginalSerifREGULAR.ttf"
   font "AboriginalSerifBOLD.ttf"
