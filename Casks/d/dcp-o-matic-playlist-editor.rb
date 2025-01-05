cb88e64cc443d09aa5ce317350680ea6d3362131
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-05 21:39:50 +0000

    dcp-o-matic-playlist-editor 2.18.2

--- Casks/d/dcp-o-matic-playlist-editor.rb
+++ Casks/d/dcp-o-matic-playlist-editor.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-playlist-editor" do
-  version "2.18.1"
-  sha256 "c3242aeddf09340b5c82f58e7d18b39c569bcba949b836fa57ac9118a0384a7c"
+  version "2.18.2"
+  sha256 "be8ce9201360a4f5bcf6d62323f4c58a7624a410710866b416107107dc0039c8"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-playlist&version=#{version}"
   name "DCP-o-matic Playlist Editor"
