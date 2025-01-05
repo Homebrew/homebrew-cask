a17f04977ebb76028686e762c2d1c31119e47654
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-01-05 12:30:49 -0500

    juice: update minimum macOS

--- Casks/j/juice.rb
+++ Casks/j/juice.rb
@@ -7,6 +7,8 @@ cask "juice" do
   desc "Make your battery information a bit more interesting"
   homepage "https://github.com/brianmichel/Juice"
 
+  depends_on macos: ">= :sierra"
+
   app "Juice.app"
 
   zap trash: [
