079f834780ae32cef156bac6fe7bc4f5157ce89b
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-01-05 08:51:02 -0500

    dbeaverlite: update checksums

--- Casks/d/dbeaverlite.rb
+++ Casks/d/dbeaverlite.rb
@@ -2,8 +2,8 @@ cask "dbeaverlite" do
   arch arm: "aarch64", intel: "x86_64"
 
   version "24.3.0"
-  sha256 arm:   "b3a1322d795d4519d57108098c43b3ebe20ceb212d35f22bfe4d3a293d32a311",
-         intel: "572f9a4776bcb3eba5280f989143a96a5f89f42c62ea94edb84e2ec38af3d857"
+  sha256 arm:   "17b48da553ff340aac32d23418440316dda9d3cb7a9e87800b385c0cc788855a",
+         intel: "6d69d662d6b59ca11056317470a9a7da1d40b763b66fdf7a271bd69479dbc7e4"
 
   url "https://dbeaver.com/downloads-lite/#{version}/dbeaver-le-#{version}-macos-#{arch}.dmg"
   name "DBeaver Lite Edition"
@@ -16,6 +16,8 @@ cask "dbeaverlite" do
       xml.elements["//version/number"]&.text&.strip
     end
   end
+
+  depends_on macos: ">= :big_sur"
 
   app "DBeaverLite.app"
 
