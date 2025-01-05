031896c4ab848c814ea34118f06837f464278e21
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-01-05 15:15:01 -0500

    pibar: update minimum macOS

--- Casks/p/pibar.rb
+++ Casks/p/pibar.rb
@@ -8,6 +8,8 @@ cask "pibar" do
   desc "Pi-hole(s) management in the menu bar"
   homepage "https://github.com/amiantos/pibar"
 
+  depends_on macos: ">= :sierra"
+
   app "PiBar.app"
 
   zap trash: [
