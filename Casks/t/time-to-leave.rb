88e1816bef170a6063c664d2bcd2d0c000610c27
Tulio Leao <tupaschoal@gmail.com>
2025-01-05 06:54:29 -0300

    Update Time To Leave to point to new organization path

--- Casks/t/time-to-leave.rb
+++ Casks/t/time-to-leave.rb
@@ -2,10 +2,10 @@ cask "time-to-leave" do
   version "3.0.0"
   sha256 "25fef73ac373e37ba8c0363e25e7e0996afb048466698777e9f0a0e5bb8876a0"
 
-  url "https://github.com/thamara/time-to-leave/releases/download/#{version}/time-to-leave-#{version}.dmg"
+  url "https://github.com/TTLApp/time-to-leave/releases/download/#{version}/time-to-leave-#{version}.dmg"
   name "Time To Leave"
   desc "Log work hours and get notified when it's time to leave the office"
-  homepage "https://github.com/thamara/time-to-leave"
+  homepage "https://github.com/TTLApp/time-to-leave"
 
   # A tag using the stable version format is sometimes marked as "Pre-release"
   # on the GitHub releases page, so we have to use the `GithubLatest` strategy.
