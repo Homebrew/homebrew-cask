7dfd737f70c01c951b7ceef0055ff487c4abc092
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-01-05 10:24:35 -0500

    free42-decimal: update minimum macOS

--- Casks/f/free42-decimal.rb
+++ Casks/f/free42-decimal.rb
@@ -12,6 +12,8 @@ cask "free42-decimal" do
     regex(/:\s*release\s*(\d+(?:\.\d+)+[a-z]?)\s*(?:$|\([^\n)]*MacOS)/i)
   end
 
+  depends_on macos: ">= :high_sierra"
+
   app "Free42 Decimal.app"
 
   zap trash: [
