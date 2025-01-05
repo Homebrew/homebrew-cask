eca17bcc3303b4bf056320d3797e98cba8bc5b43
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-01-05 17:17:49 -0500

    teamviewer-quickjoin: update minimum macOS

--- Casks/t/teamviewer-quickjoin.rb
+++ Casks/t/teamviewer-quickjoin.rb
@@ -14,7 +14,7 @@ cask "teamviewer-quickjoin" do
     strategy :header_match
   end
 
-  depends_on macos: ">= :big_sur"
+  depends_on macos: ">= :monterey"
 
   # Renamed for consistency: app name is different in the Finder and in a shell.
   app "TeamViewerQJ.app", target: "TeamViewer QuickJoin.app"
