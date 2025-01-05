0632e6b917f921ac26c7a8c3dc24488a739647be
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-05 15:41:39 +0000

    prowlarr 1.29.2.4915

--- Casks/p/prowlarr.rb
+++ Casks/p/prowlarr.rb
@@ -1,9 +1,9 @@
 cask "prowlarr" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.28.2.4885"
-  sha256 arm:   "6164a2efe1ad1832adaeffa3d14cfaf11a6b234b0dc41695bb0c13f76352e1b1",
-         intel: "085281145d833abc96011bf377fa076e18aca1567ad2a273750412846ff6368e"
+  version "1.29.2.4915"
+  sha256 arm:   "661600462e77019323ab678bda900ed3c620ded67bbc2d1f516fb6856564302e",
+         intel: "2cf0f37117574e4139615023b179ee78730e9345c7944c93ec94f6788b563f20"
 
   url "https://github.com/Prowlarr/Prowlarr/releases/download/v#{version}/Prowlarr.master.#{version}.osx-app-core-#{arch}.zip",
       verified: "github.com/Prowlarr/Prowlarr/"
