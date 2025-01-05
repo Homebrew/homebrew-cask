0daea048b8fca439f60d91b3927ed02d007e34bf
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-01-05 20:55:35 +0900

    iterm2: add more zap entries

--- Casks/i/iterm2.rb
+++ Casks/i/iterm2.rb
@@ -45,14 +45,20 @@ cask "iterm2" do
   app "iTerm.app"
 
   zap trash: [
+    "~/Library/Application Scripts/com.googlecode.iterm2.iTermFileProvider",
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.googlecode.iterm2.itermai.sfl*",
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.googlecode.iterm2.sfl*",
     "~/Library/Application Support/iTerm",
     "~/Library/Application Support/iTerm2",
     "~/Library/Caches/com.googlecode.iterm2",
+    "~/Library/Containers/com.googlecode.iterm2.iTermFileProvider",
     "~/Library/Containers/iTermAI",
     "~/Library/Cookies/com.googlecode.iterm2.binarycookies",
+    "~/Library/HTTPStorages/com.googlecode.iterm2",
+    "~/Library/HTTPStorages/com.googlecode.iterm2.binarycookies",
     "~/Library/Preferences/com.googlecode.iterm2.plist",
+    "~/Library/Preferences/com.googlecode.iterm2.private.plist",
     "~/Library/Saved Application State/com.googlecode.iterm2*.savedState",
+    "~/Library/WebKit/com.googlecode.iterm2",
   ]
 end
