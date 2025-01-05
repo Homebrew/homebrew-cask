9e8b7a4d11cde2b0f132d6f567172bcc6265f91d
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-01-05 14:10:54 -0500

    monodraw: update minimum macOS

--- Casks/m/monodraw.rb
+++ Casks/m/monodraw.rb
@@ -13,7 +13,7 @@ cask "monodraw" do
   end
 
   auto_updates true
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= :big_sur"
 
   app "Monodraw.app"
   binary "#{appdir}/Monodraw.app/Contents/Resources/monodraw"
