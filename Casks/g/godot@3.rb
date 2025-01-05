58d75d03f3e00472e1a5eb5fd14e081e38628927
Timon Back <timonback@users.noreply.github.com>
2025-01-05 15:01:29 +0100

    godot@3: align url
    Use the same GitHub releases download urls as godot.rb

--- Casks/g/godot@3.rb
+++ Casks/g/godot@3.rb
@@ -2,14 +2,14 @@ cask "godot@3" do
   version "3.6"
   sha256 "00cc8c8708756ad336d36bbfafd0cb6becd61e5b8e7d826309a3f4d5dda2c275"
 
-  url "https://downloads.tuxfamily.org/godotengine/#{version}/Godot_v#{version}-stable_osx.universal.zip",
-      verified: "downloads.tuxfamily.org/godotengine/"
+  url "https://github.com/godotengine/godot/releases/download/#{version}-stable/Godot_v#{version}-stable_osx.universal.zip",
+      verified: "github.com/godotengine/godot/"
   name "Godot Engine"
   desc "Game development engine"
   homepage "https://godotengine.org/"
 
   livecheck do
-    url "https://github.com/godotengine/godot"
+    url :url
     regex(/^v?(3(?:\.\d+)+)[._-]stable$/i)
   end
 
