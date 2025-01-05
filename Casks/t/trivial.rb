b5c93fc0c7cde1dcea9930ef3899ce82e8ea9e58
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-01-05 17:17:57 -0500

    trivial: update minimum macOS

--- Casks/t/trivial.rb
+++ Casks/t/trivial.rb
@@ -12,6 +12,8 @@ cask "trivial" do
     strategy :sparkle, &:short_version
   end
 
+  depends_on macos: ">= :high_sierra"
+
   app "Trivial.app"
 
   uninstall launchctl: "com.decisivetactics.trivial-server",
