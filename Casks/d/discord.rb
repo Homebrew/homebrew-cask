7e84870b07c20d9aa4c3028878e0a68fc8526da5
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-01-05 20:55:37 +0900

    discord: add more zap entries

--- Casks/d/discord.rb
+++ Casks/d/discord.rb
@@ -19,10 +19,13 @@ cask "discord" do
   app "Discord.app"
 
   zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.hnc.discord.sfl*",
     "~/Library/Application Support/discord",
     "~/Library/Caches/com.hnc.Discord",
     "~/Library/Caches/com.hnc.Discord.ShipIt",
     "~/Library/Cookies/com.hnc.Discord.binarycookies",
+    "~/Library/HTTPStorages/com.hnc.Discord",
+    "~/Library/HTTPStorages/com.hnc.Discord.binarycookies",
     "~/Library/Preferences/com.hnc.Discord.helper.plist",
     "~/Library/Preferences/com.hnc.Discord.plist",
     "~/Library/Saved Application State/com.hnc.Discord.savedState",
