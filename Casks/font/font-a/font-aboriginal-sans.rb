03001e078b4feb45cb777f80e61d6274de04a7c5
Klaus Hipp <khipp@users.noreply.github.com>
2025-01-05 15:27:30 +0100

    font-aboriginal-sans: add livecheck

--- Casks/font/font-a/font-aboriginal-sans.rb
+++ Casks/font/font-a/font-aboriginal-sans.rb
@@ -1,10 +1,15 @@
 cask "font-aboriginal-sans" do
-  version :latest
+  version "9.602"
   sha256 :no_check
 
   url "https://www.languagegeek.com/font/AboriginalSans.zip"
   name "LanguageGeek Aboriginal Sans Serif"
   homepage "https://www.languagegeek.com/font/fontdownload.html"
+
+  livecheck do
+    url :homepage
+    regex(/Aboriginal\s+Sans\s+Serif.*?\(\s*v?(\d+(?:\.\d+)*)[^)]*\)/i)
+  end
 
   font "AboriginalSansREGULAR.ttf"
   font "AboriginalSansBOLD.ttf"
