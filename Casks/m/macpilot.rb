055c046b6de6d331affe19c268cd385a4367585d
Klaus Hipp <khipp@users.noreply.github.com>
2025-01-06 20:31:33 +0100

    macpilot: update legacy versions

--- Casks/m/macpilot.rb
+++ Casks/m/macpilot.rb
@@ -1,15 +1,23 @@
 cask "macpilot" do
-  on_high_sierra :or_older do
-    version "11.1.3"
-    sha256 "1990b04414896ef24767e58cd9b56901460375ee8fb805572ca34da019bcda58"
+  on_monterey :or_older do
+    on_mojave :or_older do
+      version "11.1.3"
+      sha256 "1990b04414896ef24767e58cd9b56901460375ee8fb805572ca34da019bcda58"
 
-    url "https://www.koingosw.com/products/macpilot/download/old/macpilot_#{version}_intel_for_1013to1015.dmg"
+      url "https://www.koingosw.com/products/macpilot/download/old/macpilot_#{version}_intel_for_1013to1015.dmg"
+    end
+    on_catalina :or_newer do
+      version "16.4"
+      sha256 "fec81ddf514938dee70831a769997a8ce540f08df49df12b1f0c9effc686daca"
+
+      url "https://www.koingosw.com/products/macpilot/download/old/macpilot_#{version}_intelarm_for_10.15_to_15.dmg"
+    end
 
     livecheck do
       skip "Legacy version"
     end
   end
-  on_mojave :or_newer do
+  on_ventura :or_newer do
     version "16.5"
     sha256 :no_check
 
