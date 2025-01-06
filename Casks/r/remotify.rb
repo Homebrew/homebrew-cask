b7dad7d9fd01042dd054aef4f116657f4889272d
hain <hain@joblogic.com>
2025-01-07 00:50:34 +0700

    fix: correct homepage url, change the verified url, adjust zap to single-element, enhance readable code by remove extract tab characters.

--- Casks/r/remotify.rb
+++ Casks/r/remotify.rb
@@ -1,15 +1,14 @@
 cask "remotify" do
-    version "1.0.1"
-    sha256 "244480027c103a2030a666f5c14dbd231bd0a2ab162ee88cc87653010b12ffa7"
+  version "1.0.1"
+  sha256 "244480027c103a2030a666f5c14dbd231bd0a2ab162ee88cc87653010b12ffa7"
 
-    url "https://github.com/haithngn/remotify/releases/download/v1.0.1/Remotify.tar.gz"
-    name "Remotify"
-    desc "Ultimate Push Notification Debugging Tools"
-    homepage "https://hainguyen.dev/remotify"
+  url "https://github.com/haithngn/remotify/releases/download/v1.0.1/Remotify.tar.gz",
+      verified: "github.com/haithngn/remotify/"
+  name "Remotify"
+  desc "Ultimate Push Notification Debugging Tools"
+  homepage "https://github.com/haithngn/remotify"
 
-    app "Remotify.app"
+  app "Remotify.app"
 
-    zap trash: [
-      "~/Library/Preferences/dev.hainguyen.remotify.desktop.app.plist",
-    ]
-  end
+  zap trash: "~/Library/Preferences/dev.hainguyen.remotify.plist"
+end
