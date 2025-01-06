45c1a20aa2988781d87502af3a47ae17bd4e2586
he110-c0de <157435319+he110-c0de@users.noreply.github.com>
2025-01-06 13:48:33 +0800

    hackintool: update sha256

--- Casks/h/hackintool.rb
+++ Casks/h/hackintool.rb
@@ -1,6 +1,6 @@
 cask "hackintool" do
   version "4.0.3"
-  sha256 "00af843d9a49e603fad1acba5ea1199927b254a487a9f3a5d04d8d6c3ad8083b"
+  sha256 :no_check # required as upstream package is updated in-place
 
   url "https://github.com/headkaze/Hackintool/releases/download/#{version}/Hackintool.zip"
   name "Hackintool"
@@ -18,4 +18,8 @@ cask "hackintool" do
     "~/Library/Cookies/com.Headsoft.Hackintool.binarycookies",
     "~/Library/Preferences/com.Headsoft.Hackintool.plist",
   ]
+
+  caveats do
+    requires_rosetta
+  end
 end
