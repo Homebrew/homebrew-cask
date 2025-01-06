06bd67b718fc0c374cf1a9768789584486e9ada0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-06 21:39:24 +0000

    microsoft-auto-update 4.77.24121924

--- Casks/m/microsoft-auto-update.rb
+++ Casks/m/microsoft-auto-update.rb
@@ -12,8 +12,8 @@ cask "microsoft-auto-update" do
     sha256 "abde56323f4753a90a99ca26a3c62060a498328bd90e0421395ec14a47d49101"
   end
   on_mojave :or_newer do
-    version "4.76.24101387"
-    sha256 "91fed0b500d85daf6e453bb7b2bee2948973781475c118c03c837e819a290fcc"
+    version "4.77.24121924"
+    sha256 "c818d32f268dfba316bfaedf27ab3e9224c529d2647454464270f58ebc9db4c7"
   end
 
   url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_AutoUpdate_#{version}_Updater.pkg"
