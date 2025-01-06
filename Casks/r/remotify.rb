f6538e7626e6568ca824f34f54755d6b6e8a771b
hain <hain@joblogic.com>
2025-01-07 01:00:59 +0700

    fix: removed unnecessary verified url, add versioned on URL

--- Casks/r/remotify.rb
+++ Casks/r/remotify.rb
@@ -2,8 +2,7 @@ cask "remotify" do
   version "1.0.1"
   sha256 "244480027c103a2030a666f5c14dbd231bd0a2ab162ee88cc87653010b12ffa7"
 
-  url "https://github.com/haithngn/remotify/releases/download/v1.0.1/Remotify.tar.gz",
-      verified: "github.com/haithngn/remotify/"
+  url "https://github.com/haithngn/remotify/releases/download/v#{version}/Remotify.tar.gz"
   name "Remotify"
   desc "Ultimate Push Notification Debugging Tools"
   homepage "https://github.com/haithngn/remotify"
