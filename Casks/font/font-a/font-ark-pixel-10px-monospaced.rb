a7bd15a29304bff5130ec914e06340fc9f7145e4
Klaus Hipp <khipp@users.noreply.github.com>
2025-01-07 12:22:27 +0100

    font-ark-pixel-10px-monospaced 2025.01.06

--- Casks/font/font-a/font-ark-pixel-10px-monospaced.rb
+++ Casks/font/font-a/font-ark-pixel-10px-monospaced.rb
@@ -1,6 +1,6 @@
 cask "font-ark-pixel-10px-monospaced" do
-  version "2024.11.04"
-  sha256 "b04e4a72a0b808adb9d577dc0ccf6635924f071fe41a7e594d3ab46556591ad9"
+  version "2025.01.06"
+  sha256 "0ee0f4b8b46d723b4cf9389cafb7f3ddb68003e97bd275510e227dca0769c1c3"
 
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-10px-monospaced-otf-v#{version}.zip"
   name "Ark Pixel 10px Monospaced"
