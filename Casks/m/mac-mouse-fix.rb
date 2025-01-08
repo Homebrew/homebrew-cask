0b10d31db137fc7477e134e34f673f95ab97bbf0
Klaus Hipp <khipp@users.noreply.github.com>
2025-01-08 22:20:08 +0100

    mac-mouse-fix: add livecheck

--- Casks/m/mac-mouse-fix.rb
+++ Casks/m/mac-mouse-fix.rb
@@ -9,8 +9,8 @@ cask "mac-mouse-fix" do
   homepage "https://macmousefix.com/"
 
   livecheck do
-    url :url
-    strategy :github_latest
+    url "https://raw.githubusercontent.com/noah-nuebling/mac-mouse-fix/update-feed/appcast.xml"
+    strategy :sparkle, &:short_version
   end
 
   auto_updates true
