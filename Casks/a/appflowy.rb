2b798f3897c3059015c143a1eb4f8aaeed7953de
Artur Martins <arturmartins@gmail.com>
2025-01-08 22:28:56 +0000

    Fix appflowy.rb 0.8.0 SHA256
    https://github.com/AppFlowy-IO/AppFlowy/releases/tag/0.8.0
    
    AppFlowy released new artifacts for 0.8.0, therefore new SHA265 is needed

--- Casks/a/appflowy.rb
+++ Casks/a/appflowy.rb
@@ -1,6 +1,6 @@
 cask "appflowy" do
   version "0.8.0"
-  sha256 "d68a241c694dd604ffe7389a73fa5c6a24e6c8237e4b14e139efdebc814161fa"
+  sha256 "7b0fbcff7641c78adc8fe6eb96aac79e9861d2c2d8dc99dafb47a4fc7af85f64"
 
   url "https://github.com/AppFlowy-IO/AppFlowy/releases/download/#{version}/Appflowy-#{version}-macos-universal.zip",
       verified: "github.com/AppFlowy-IO/AppFlowy/"
