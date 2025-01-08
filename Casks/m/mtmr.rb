5f4216250b7bef291e615997001bf9cb6fb56335
Klaus Hipp <khipp@users.noreply.github.com>
2025-01-08 22:01:06 +0100

    mtmr: update livecheck and homepage

--- Casks/m/mtmr.rb
+++ Casks/m/mtmr.rb
@@ -1,15 +1,15 @@
 cask "mtmr" do
-  version "0.27.0"
+  version "0.27"
   sha256 "cf0b1b8cb9d6758cd0b69d2c4c5f9f1a067416126a7daa76a8d94fea8189d608"
 
-  url "https://github.com/Toxblh/MTMR/releases/download/v#{version}/MTMR.#{version.major_minor}.dmg"
+  url "https://mtmr.app/MTMR%20#{version}.dmg"
   name "My TouchBar. My rules"
   desc "TouchBar customization app"
-  homepage "https://github.com/Toxblh/MTMR"
+  homepage "https://mtmr.app/"
 
   livecheck do
-    url :url
-    strategy :github_latest
+    url "https://mtmr.app/appcast.xml"
+    strategy :sparkle, &:short_version
   end
 
   auto_updates true
