4711884767dcbcd626549eb12c621b810d724425
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-01-09 14:53:15 +0900

    fathom: add intel arch

--- Casks/f/fathom.rb
+++ Casks/f/fathom.rb
@@ -1,15 +1,19 @@
 cask "fathom" do
-  version "1.36.0"
-  sha256 "a41140c4f92ed15bd33deb40136aff0b773c66f4fd660c72f83891c9c5934771"
+  arch arm: "arm64", intel: "x64"
+  livecheck_arch = on_arch_conditional arm: "_arm64"
 
-  url "https://storage.googleapis.com/electron_releases/v#{version}/Fathom-darwin-arm64-#{version}.dmg",
+  version "1.36.0"
+  sha256 arm:   "a41140c4f92ed15bd33deb40136aff0b773c66f4fd660c72f83891c9c5934771",
+         intel: "877c70b85c561b8c25978fd872343375bafc1d5ba770017d571d6a326e92561e"
+
+  url "https://storage.googleapis.com/electron_releases/v#{version}/Fathom-darwin-#{arch}-#{version}.dmg",
       verified: "storage.googleapis.com/electron_releases/"
   name "Fathom"
   desc "Record and transcribe video conferences"
   homepage "https://fathom.video/"
 
   livecheck do
-    url "https://electron-update.fathom.video/update/darwin_arm64/0.0.0"
+    url "https://electron-update.fathom.video/update/darwin#{livecheck_arch}/0.0.0"
     strategy :json do |json|
       json["name"]&.tr("v", "")
     end
