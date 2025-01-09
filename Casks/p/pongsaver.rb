8a879182b5a5b6f21886b37b262698d928829ea5
Klaus Hipp <khipp@users.noreply.github.com>
2025-01-09 11:02:22 +0100

    pongsaver 3.0

--- Casks/p/pongsaver.rb
+++ Casks/p/pongsaver.rb
@@ -1,13 +1,16 @@
 cask "pongsaver" do
-  version "2.1.1"
+  version "3.0"
   sha256 :no_check
 
   url "https://mikeash.com/software/pongsaver/PongSaver.saver.zip"
   name "PongSaver"
   desc "Screensaver which plays a game of Pong against itself"
-  homepage "https://mikeash.com/software/pongsaver"
+  homepage "https://mikeash.com/software/pongsaver/"
 
-  deprecate! date: "2024-11-03", because: :unmaintained
+  livecheck do
+    url :homepage
+    regex(/PongSaver\s+v?(\d+(?:\.\d+)+)/i)
+  end
 
   screen_saver "PongSaver.saver"
 end
