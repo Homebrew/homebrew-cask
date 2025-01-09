543e44d90c36e692877f28aaec2d9b35e2f56e06
Klaus Hipp <khipp@users.noreply.github.com>
2025-01-09 18:32:28 +0100

    dockdoor: add livecheck, update homepage

--- Casks/d/dockdoor.rb
+++ Casks/d/dockdoor.rb
@@ -2,10 +2,16 @@ cask "dockdoor" do
   version "1.6.1"
   sha256 "907595e192323e16e540741eaef2cebebb1a9580cf293da7775c4ab4795b0f6f"
 
-  url "https://github.com/ejbills/DockDoor/releases/download/v#{version}/DockDoor.dmg"
+  url "https://github.com/ejbills/DockDoor/releases/download/v#{version}/DockDoor.dmg",
+      verified: "github.com/ejbills/DockDoor/"
   name "DockDoor"
   desc "Window peeking utility app"
-  homepage "https://github.com/ejbills/DockDoor"
+  homepage "https://dockdoor.net/"
+
+  livecheck do
+    url "https://dockdoor.net/appcast.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :ventura"
