9265c1b8fd84c3eda1760e1adacebf012904ab48
Grant Gurvis <grangurv@amazon.com>
2025-01-09 11:00:57 -0800

    amazon q: update download/index url

--- Casks/a/amazon-q.rb
+++ Casks/a/amazon-q.rb
@@ -2,14 +2,14 @@ cask "amazon-q" do
   version "1.6.0"
   sha256 "63d9da1896138e7370966d0c85543bd61aa8e0594f872b067e0b35104597ce3c"
 
-  url "https://desktop-release.codewhisperer.us-east-1.amazonaws.com/#{version}/Amazon%20Q.dmg",
-      verified: "desktop-release.codewhisperer.us-east-1.amazonaws.com/"
+  url "https://desktop-release.q.us-east-1.amazonaws.com/#{version}/Amazon%20Q.dmg",
+      verified: "desktop-release.q.us-east-1.amazonaws.com/"
   name "Amazon Q"
   desc "AI-powered productivity tool for the command-line"
   homepage "https://aws.amazon.com/q/developer/"
 
   livecheck do
-    url "https://desktop-release.codewhisperer.us-east-1.amazonaws.com/index.json"
+    url "https://desktop-release.q.us-east-1.amazonaws.com/index.json"
     strategy :json do |json|
       json["versions"]&.map { |item| item["version"] }
     end
