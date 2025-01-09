895ca9ac7e896740b8178e5862a7cfa613021766
Klaus Hipp <khipp@users.noreply.github.com>
2025-01-09 23:14:19 +0100

    posture-pal: update livecheck

--- Casks/p/posture-pal.rb
+++ Casks/p/posture-pal.rb
@@ -13,7 +13,7 @@ cask "posture-pal" do
   livecheck do
     url "https://macwhisper-site.vercel.app/posturepal/appcast.xml"
     strategy :sparkle do |items|
-      items.map { |item| "#{item.short_version},#{item.version}" }
+      items.map(&:nice_version)
     end
   end
 
