554ac48a8e8a206b9878d0f66a05debde60f748a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-09 12:47:04 +0000

    fedistar 1.11.0

--- Casks/f/fedistar.rb
+++ Casks/f/fedistar.rb
@@ -1,6 +1,6 @@
 cask "fedistar" do
-  version "1.10.1"
-  sha256 "79ac899d60344b65832ba1d2431e3f1b75e4b7fffc1f322a43cd0d312ee001c0"
+  version "1.11.0"
+  sha256 "e56d773b7667ed1a94126687be1da53be5104f5a5ba378640a3651017cd30b31"
 
   url "https://github.com/h3poteto/fedistar/releases/download/v#{version}/fedistar_#{version}_universal.dmg",
       verified: "github.com/h3poteto/fedistar/"
