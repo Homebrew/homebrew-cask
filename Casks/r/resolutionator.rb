a2ae740ced137a8389d00b38e492985c7e7c11f7
Klaus Hipp <khipp@users.noreply.github.com>
2025-01-10 10:14:50 +0100

    resolutionator: update livecheck

--- Casks/r/resolutionator.rb
+++ Casks/r/resolutionator.rb
@@ -1,17 +1,15 @@
 cask "resolutionator" do
-  version "2.4.0"
+  version "2.4"
   sha256 "e9fb9b1946deddc62ff0bf5d46066a8cc91564c358e5d33bf85029f4ef75d5bb"
 
-  url "https://manytricks.com/download/_do_not_hotlink_/resolutionator#{version.no_dots}.dmg"
+  url "https://manytricks.com/download/_do_not_hotlink_/resolutionator#{version.no_dots.ljust(3, "0")}.dmg"
   name "Resolutionator"
   desc "Use any of your display's available resolutions"
   homepage "https://manytricks.com/resolutionator/"
 
   livecheck do
     url "https://manytricks.com/resolutionator/appcast/"
-    strategy :sparkle do |item|
-      (item.short_version + ".0").split(".")[0..2].join(".")
-    end
+    strategy :sparkle, &:short_version
   end
 
   auto_updates true
