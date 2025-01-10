e978b52bdfcb3169d80dc395d3f844c67304a5b2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-10 22:07:47 +0000

    warp 0.2025.01.08.08.02.stable_04

--- Casks/w/warp.rb
+++ Casks/w/warp.rb
@@ -1,6 +1,6 @@
 cask "warp" do
-  version "0.2025.01.08.08.02.stable_03"
-  sha256 "4172a753084e5d7deb24936058812b043552ff29305b30780b0cee5a65fef1c8"
+  version "0.2025.01.08.08.02.stable_04"
+  sha256 "4b580d9bc8e83d2d952ac4c5db3506f734adb459123c104eb386e972987eb1a1"
 
   url "https://app.warp.dev/download/brew?version=v#{version}"
   name "Warp"
