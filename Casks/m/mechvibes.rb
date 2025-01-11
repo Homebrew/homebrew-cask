7a0116034886c08d7b21372f6c46ee07e96ab276
Junian Triajianto <3881052+junian@users.noreply.github.com>
2025-01-11 15:30:56 +0700

    Update mechvibes.rb

--- Casks/m/mechvibes.rb
+++ Casks/m/mechvibes.rb
@@ -1,8 +1,8 @@
 cask "mechvibes" do
-  version "2.3.6"
+  version "2.3.6-hotfix"
   sha256 "29db0b74bdde3895e4d7b38165eebb1dec91ae546244be9d1285123999b4326a"
 
-  url "https://github.com/hainguyents13/mechvibes/releases/download/v#{version}/Mechvibes-#{version}-hotfix.dmg",
+  url "https://github.com/hainguyents13/mechvibes/releases/download/v#{version}/Mechvibes-#{version}.dmg",
       verified: "github.com/hainguyents13/mechvibes/"
   name "Mechvibes"
   desc "Play mechanical keyboard sounds as you type"
