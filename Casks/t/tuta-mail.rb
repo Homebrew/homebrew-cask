f9fcba9f3a337ddf5dde459a1c781e879929637f
Klaus Hipp <khipp@users.noreply.github.com>
2025-01-11 12:42:14 +0100

    tuta-mail 259.250108.1

--- Casks/t/tuta-mail.rb
+++ Casks/t/tuta-mail.rb
@@ -1,9 +1,8 @@
 cask "tuta-mail" do
-  version "259.241223.2"
-  sha256 "439087daf31f83f13dc51e38edca0d5a1838b11c6afc6429cf3550e3389f2875"
+  version "259.250108.1"
+  sha256 :no_check
 
-  url "https://github.com/tutao/tutanota/releases/download/tutanota-desktop-release-#{version}/tutanota-desktop-mac.dmg",
-      verified: "github.com/tutao/tutanota/"
+  url "https://app.tuta.com/desktop/tutanota-desktop-mac.dmg"
   name "Tuta Mail"
   desc "Email client"
   homepage "https://tuta.com/"
