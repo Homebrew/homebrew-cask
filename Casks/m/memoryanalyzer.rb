35446d96c4b99af83847bf47172837e9e51cd300
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-01-10 20:15:12 -0500

    memoryanalyzer 1.16.1.20250109

--- Casks/m/memoryanalyzer.rb
+++ Casks/m/memoryanalyzer.rb
@@ -1,9 +1,9 @@
 cask "memoryanalyzer" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.16.0.20241216"
-  sha256 arm:   "7bfc59d5f4be2d1d0ad9c79326d4ea44bfa6fe1c4aa98a4efdafc252454c368d",
-         intel: "c0c796dbe22ba1d4b005ff0a287dd6a7d74988ecb41107c822778cacb96d1fa6"
+  version "1.16.1.20250109"
+  sha256 arm:   "01ac5effe6479f013c32c2f8d75ed08e7ec0f848f5ce301b5fa47de7f34c2654",
+         intel: "9cd8ad2e726da6e1300bdb115cc22c929e69707de56cc2873cac5067848254ea"
 
   url "https://download.eclipse.org/mat/#{version.major_minor_patch}/rcp/MemoryAnalyzer-#{version}-macosx.cocoa.#{arch}.dmg",
       verified: "download.eclipse.org/mat/"
