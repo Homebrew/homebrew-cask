9f870ef109f86610a451e7240ec7f0e1f7d9fb83
Sgal Cheung <zhangsigao@live.com>
2025-01-11 12:53:50 +0800

    font-harmonyos-* latest (new casks)
    font-harmonyos-sans: fix name
    
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

---
+++ Casks/font/font-h/font-harmonyos-sans-naskh-arabic.rb
@@ -0,0 +1,29 @@
+cask "font-harmonyos-sans-naskh-arabic" do
+  version :latest
+  sha256 :no_check
+
+  url "https://developer.huawei.com/images/download/next/HarmonyOS-Sans.zip"
+  name "HarmonyOS Sans Naskh Arabic"
+  homepage "https://developer.huawei.com/consumer/en/doc/design-guides/font-0000001828772001"
+
+  font "HarmonyOS Sans/HarmonyOS Naskh Arabic/HarmonyOS_Sans_Naskh_Arabic_Regular.ttf"
+  font "HarmonyOS Sans/HarmonyOS Naskh Arabic/HarmonyOS_Sans_Naskh_Arabic_Thin.ttf"
+  font "HarmonyOS Sans/HarmonyOS Naskh Arabic/HarmonyOS_Sans_Naskh_Arabic_Light.ttf"
+  font "HarmonyOS Sans/HarmonyOS Naskh Arabic/HarmonyOS_Sans_Naskh_Arabic_Medium.ttf"
+  font "HarmonyOS Sans/HarmonyOS Naskh Arabic/HarmonyOS_Sans_Naskh_Arabic_Bold.ttf"
+  font "HarmonyOS Sans/HarmonyOS Naskh Arabic/HarmonyOS_Sans_Naskh_Arabic_Black.ttf"
+  font "HarmonyOS Sans/HarmonyOS Naskh Arabic/HarmonyOS_Sans_Naskh_Arabic_SemiBold.ttf"
+  font "HarmonyOS Sans/HarmonyOS Naskh Arabic/HarmonyOS_Sans_Naskh_Arabic_UltraLight.ttf"
+  font "HarmonyOS Sans/HarmonyOS Naskh Arabic/HarmonyOS_Sans_Naskh_Arabic_Heavy.ttf"
+  font "HarmonyOS Sans/HarmonyOS Naskh Arabic UI/HarmonyOS_Sans_Naskh_ArabicUI_Regular.ttf"
+  font "HarmonyOS Sans/HarmonyOS Naskh Arabic UI/HarmonyOS_Sans_Naskh_ArabicUI_Black.ttf"
+  font "HarmonyOS Sans/HarmonyOS Naskh Arabic UI/HarmonyOS_Sans_Naskh_ArabicUI_Medium.ttf"
+  font "HarmonyOS Sans/HarmonyOS Naskh Arabic UI/HarmonyOS_Sans_Naskh_ArabicUI_UltraLight.ttf"
+  font "HarmonyOS Sans/HarmonyOS Naskh Arabic UI/HarmonyOS_Sans_Naskh_ArabicUI_Heavy.ttf"
+  font "HarmonyOS Sans/HarmonyOS Naskh Arabic UI/HarmonyOS_Sans_Naskh_ArabicUI_Thin.ttf"
+  font "HarmonyOS Sans/HarmonyOS Naskh Arabic UI/HarmonyOS_Sans_Naskh_ArabicUI_Light.ttf"
+  font "HarmonyOS Sans/HarmonyOS Naskh Arabic UI/HarmonyOS_Sans_Naskh_ArabicUI_Bold.ttf"
+  font "HarmonyOS Sans/HarmonyOS Naskh Arabic UI/HarmonyOS_Sans_Naskh_ArabicUI_SemiBold.ttf"
+
+  # No zap stanza required
+end
