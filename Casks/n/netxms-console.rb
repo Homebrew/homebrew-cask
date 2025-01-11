df0ff20b01be7cbbb61d0f4d41e570b0f99c9a78
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-01-11 15:54:23 +0900

    netxms-console 5.1.3

--- Casks/n/netxms-console.rb
+++ Casks/n/netxms-console.rb
@@ -1,9 +1,9 @@
 cask "netxms-console" do
   arch arm: "-aarch64"
 
-  version "5.1.2"
-  sha256 arm:   "69d1cf0e5920e615a8721e6c5cdeea0f566e8b736dba0069ee6bf506cb6a0267",
-         intel: "8ead118b0e271d016a60989f4f1a1cce4f17e5573b82e2f732079213fecd5f74"
+  version "5.1.3"
+  sha256 arm:   "f8eeade56d907a82aa5c2c9b77550001e8e062e9e92921f54c5f7c3614833ea7",
+         intel: "0dcbfae58a0a715eae909e02897737ab8930a9412ce3b97553c5b557c0e412bb"
 
   url "https://netxms.org/download/releases/#{version.major_minor}/nxmc-#{version}#{arch}.dmg"
   name "NetXMS Management Console"
