9d188f370cce3bc2b8d5c8f3ec679289504d2d22
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-13 04:03:43 +0000

    yuque 4.0.5.1309

--- Casks/y/yuque.rb
+++ Casks/y/yuque.rb
@@ -1,6 +1,6 @@
 cask "yuque" do
-  version "3.4.7.1215,BJfb0c68b3aac848b1a9eedba51bb7bf85"
-  sha256 "c147e215d8399d06a1d05e4d4708063444da1b624705fc99d48ad578ae026a07"
+  version "4.0.5.1309,BJe833739b4f8f477fbd930ac4a3acbfed"
+  sha256 "40cc46c385debed7dca51a50723f3f9e861737e4baf6ab8fc286247dff0eebbf"
 
   url "https://app.nlark.com/yuque-desktop/#{version.csv.first}/#{version.csv.second}/Yuque-#{version.csv.first}.dmg",
       verified: "app.nlark.com/yuque-desktop/"
