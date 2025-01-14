9ed3a22570dca0acff3f9b550bb3b162b4b62f1e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-14 06:48:47 +0000

    reflect 3.0.2

--- Casks/r/reflect.rb
+++ Casks/r/reflect.rb
@@ -1,9 +1,9 @@
 cask "reflect" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.0.1"
-  sha256 arm:   "94df679b6cb0cd195abe0fdc0e32a50d394f6e076a5496f21a70e75532723e8e",
-         intel: "e79c6bdb4ac7f5291ee7fdb90be1923c8effca2d3739c974e88334cb7d524969"
+  version "3.0.2"
+  sha256 arm:   "029156fc0a7a3ec573ee4704d201ad980307ab60025167d981583687f841d773",
+         intel: "48ad5eec2170bf34d1efbd82ebfe3bf7b98d273e940e2becc62f68cbf2951c27"
 
   url "https://github.com/team-reflect/reflect-electron-updates/releases/download/v#{version}/Reflect-darwin-#{arch}-#{version}.zip",
       verified: "github.com/team-reflect/reflect-electron-updates/"
