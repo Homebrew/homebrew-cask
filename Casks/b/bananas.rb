da510fad36586bc75f3bbffc63403855f5dd919a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-14 18:37:45 +0000

    bananas 0.0.19

--- Casks/b/bananas.rb
+++ Casks/b/bananas.rb
@@ -1,6 +1,6 @@
 cask "bananas" do
-  version "0.0.18"
-  sha256 "ab0baff7763112795c6b44e519a6415f8a45b53fa18d23c88ae3a96b223aec28"
+  version "0.0.19"
+  sha256 "41626250a3c7684288fbef201ecfa742ac6353b45f31aea4169ea757fecdfa3e"
 
   url "https://github.com/mistweaverco/bananas/releases/download/v#{version}/bananas_universal.dmg",
       verified: "github.com/mistweaverco/bananas/"
