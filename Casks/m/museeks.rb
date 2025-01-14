58cea1ce749848fbb77d1c6de460924b471aebf5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-14 16:04:36 +0000

    museeks 0.20.5

--- Casks/m/museeks.rb
+++ Casks/m/museeks.rb
@@ -1,6 +1,6 @@
 cask "museeks" do
-  version "0.20.4"
-  sha256 "f2ad2dd56fb2d4a258fd02702f138aaaa97d270ed28fa5015090b0a887fe9185"
+  version "0.20.5"
+  sha256 "be38502a9ad347931059d4e2662d4d3620e61dd9760a755db8019ff604745f7c"
 
   url "https://github.com/martpie/museeks/releases/download/#{version}/Museeks_#{version}_universal.dmg",
       verified: "github.com/martpie/museeks/"
