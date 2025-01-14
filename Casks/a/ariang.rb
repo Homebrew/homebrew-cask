9a311d692e150e78330061c6f1ee87c82400f0e7
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-01-14 22:48:53 +0900

    ariang: add arm arch

--- Casks/a/ariang.rb
+++ Casks/a/ariang.rb
@@ -1,8 +1,11 @@
 cask "ariang" do
-  version "1.3.8"
-  sha256 "1a441eda861a34d4b8ae52604ee28a1ff8c5741b24b79ee7300b7b8471ad2ed9"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://github.com/mayswind/AriaNg-Native/releases/download/#{version}/AriaNg_Native-#{version}-macOS-x64.dmg"
+  version "1.3.8"
+  sha256 arm:   "c1d736f609a3c96f05d99437c62031ea839b41710bdb715d498e025ede17f185",
+         intel: "1a441eda861a34d4b8ae52604ee28a1ff8c5741b24b79ee7300b7b8471ad2ed9"
+
+  url "https://github.com/mayswind/AriaNg-Native/releases/download/#{version}/AriaNg_Native-#{version}-macOS-#{arch}.dmg"
   name "AriaNg Native"
   desc "Better aria2 desktop frontend than AriaNg"
   homepage "https://github.com/mayswind/AriaNg-Native"
@@ -15,8 +18,4 @@ cask "ariang" do
     "~/Library/Preferences/net.mayswind.ariang.plist",
     "~/Library/Saved Application State/net.mayswind.ariang.savedState",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
