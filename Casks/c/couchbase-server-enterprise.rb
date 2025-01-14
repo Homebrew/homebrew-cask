b0877a3bd87556cbe9fdd18ca3f5cda90563d05e
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-01-14 16:36:30 +0900

    couchbase-server-enterprise: add arm arch

--- Casks/c/couchbase-server-enterprise.rb
+++ Casks/c/couchbase-server-enterprise.rb
@@ -1,15 +1,18 @@
 cask "couchbase-server-enterprise" do
-  version "7.6.4"
-  sha256 "8c2f2c20b0cf8a55d4b6f7a81d769731f35c3f5d14c45851cea79d514c0ca777"
+  arch arm: "arm64", intel: "x86_64"
 
-  url "https://packages.couchbase.com/releases/#{version}/couchbase-server-enterprise_#{version}-macos_x86_64.dmg"
+  version "7.6.4"
+  sha256 arm:   "580112500c7bfab6ac3b3f69b7934500966acebce6c2f1a5494e0470e1b340cd",
+         intel: "8c2f2c20b0cf8a55d4b6f7a81d769731f35c3f5d14c45851cea79d514c0ca777"
+
+  url "https://packages.couchbase.com/releases/#{version}/couchbase-server-enterprise_#{version}-macos_#{arch}.dmg"
   name "Couchbase Server"
   desc "Distributed NoSQL cloud database"
   homepage "https://www.couchbase.com/"
 
   livecheck do
     url "https://www.couchbase.com/downloads/"
-    regex(/couchbase[._-]server[._-]enterprise[._-]v?(\d+(:?\.\d+)+)[._-]macos[._-]x86[._-]64\.dmg/i)
+    regex(/couchbase[._-]server[._-]enterprise[._-]v?(\d+(:?\.\d+)+)[._-]macos[._-]#{arch}\.dmg/i)
   end
 
   conflicts_with cask: "couchbase-server-community"
@@ -25,8 +28,4 @@ cask "couchbase-server-enterprise" do
     "~/Library/Preferences/com.couchbase.couchbase-server.plist",
     "~/Library/Preferences/couchbase-server.ini",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
