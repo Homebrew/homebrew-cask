420145acedbed1e624973acd9d8ce667d4435f0e
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-01-14 19:11:23 +0900

    ibm-aspera-connect 4.2.13.820

--- Casks/i/ibm-aspera-connect.rb
+++ Casks/i/ibm-aspera-connect.rb
@@ -1,24 +1,27 @@
 cask "ibm-aspera-connect" do
-  version "4.1.3.93"
-  sha256 "502fe4644ed79d214a9acc33922e55de88c50ddd37dd4072515a1a51c287d617"
+  arch arm: "arm64", intel: "x86_64"
 
-  url "https://d3gcli72yxqn2z.cloudfront.net/connect_latest/v4/bin/ibm-aspera-connect_#{version}_macOS_x86_64.dmg",
-      verified: "d3gcli72yxqn2z.cloudfront.net/"
+  version "4.2.13.820"
+  sha256 arm:   "58b42f814c95168e149491d330acb286920fcc19c581e958168305b78c8aa478",
+         intel: "7dd745e07145a39cfae516b797745541fb3774050e567f98adbe191651b10d1f"
+
+  url "https://download4.boulder.ibm.com/sar/CMA/OSA/0csnb/0/ibm-aspera-connect_#{version}_macOS_#{arch}.pkg"
   name "IBM Aspera Connect"
   desc "Facilitate uploads and downloads with an Aspera transfer server"
   homepage "https://www.ibm.com/aspera/connect/"
 
   livecheck do
-    url "https://d3gcli72yxqn2z.cloudfront.net/connect_latest/v4/connectversions.min.js"
-    regex(/ibm-aspera-connect[._-]v?(\d+(?:\.\d+)+)_macOS_x86_64\.dmg/i)
+    url "https://d3gcli72yxqn2z.cloudfront.net/downloads/connect/latest/versions.js"
+    regex(/ibm-aspera-connect[._-]v?(\d+(?:\.\d+)+)_macOS_x86_64\.pkg/i)
   end
 
-  installer manual: "IBM Aspera Connect Installer.app"
+  pkg "ibm-aspera-connect_#{version}_macOS_#{arch}.pkg"
 
-  uninstall script: {
-    executable: "~/Library/Application Support/Aspera/Aspera Connect/uninstall_connect.sh",
-    args:       ["-f"],
-  }
+  uninstall pkgutil: [
+    "com.ibm.software.aspera.connect",
+    "com.ibm.software.aspera.crypt",
+    "com.ibm.software.aspera.launcher",
+  ]
 
   zap trash: [
     "~/Library/Application Scripts/com.aspera.connect.SafariExtension",
@@ -30,8 +33,4 @@ cask "ibm-aspera-connect" do
     "~/Library/Preferences/com.aspera.connect.plist",
     "~/Library/Saved Application State/com.aspera.connect.savedState",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
