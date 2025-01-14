6ab660dd8da72a745ce5963f29ba8934cec4e4da
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-01-14 22:29:24 +0900

    xiaomi-cloud: add arm arch

--- Casks/x/xiaomi-cloud.rb
+++ Casks/x/xiaomi-cloud.rb
@@ -1,9 +1,12 @@
 cask "xiaomi-cloud" do
-  version "3.0.1"
-  sha256 "c4e59f296c3d1398d8cf89125d99617af2cc2adeac7fcf6232ccc6a93982843b"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://cdn.cnbj1.fds.api.mi-img.com/archive/update-server/public/darwin/x64/XiaomiCloud-#{version}.dmg",
-      verified: "cdn.cnbj1.fds.api.mi-img.com/archive/update-server/public/darwin/x64/"
+  version "3.0.1"
+  sha256 arm:   "e928c81d1803900a49be6fb70bef76f749aa19b628dda73c0fa4805fad350992",
+         intel: "c4e59f296c3d1398d8cf89125d99617af2cc2adeac7fcf6232ccc6a93982843b"
+
+  url "https://cdn.cnbj1.fds.api.mi-img.com/archive/update-server/public/darwin/#{arch}/XiaomiCloud-#{version}.dmg",
+      verified: "cdn.cnbj1.fds.api.mi-img.com/archive/update-server/public/darwin/"
   name "Xiaomi Cloud"
   name "小米云服务"
   desc "Sync photos, contacts, messages and devices"
@@ -26,8 +29,4 @@ cask "xiaomi-cloud" do
     "~/Library/Preferences/micloud.pc.xiaomi.helper.plist",
     "~/Library/Preferences/micloud.pc.xiaomi.plist",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
