fb86524413a81db035847fbd4a2e4f7330fcb76f
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-01-14 16:37:44 +0900

    couchbase-server-enterprise: update minimum macOS

--- Casks/c/couchbase-server-enterprise.rb
+++ Casks/c/couchbase-server-enterprise.rb
@@ -16,7 +16,7 @@ cask "couchbase-server-enterprise" do
   end
 
   conflicts_with cask: "couchbase-server-community"
-  depends_on macos: ">= :monterey"
+  depends_on macos: ">= :catalina"
 
   app "Couchbase Server.app"
 
