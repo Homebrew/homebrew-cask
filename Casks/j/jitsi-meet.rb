85fe89969af45d73e4058ed4e1782d35bcf3eee8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-15 22:11:08 +0000

    jitsi-meet 2025.1.1

--- Casks/j/jitsi-meet.rb
+++ Casks/j/jitsi-meet.rb
@@ -1,6 +1,6 @@
 cask "jitsi-meet" do
-  version "2025.1.0"
-  sha256 "56e737146689c6d247fadf13678565a747a7acaf22c19eff05ebb7b71d9d974c"
+  version "2025.1.1"
+  sha256 "c32378c375cb42e3aed980416c1c7fd937b7bf31bfa8f2bc13afc0c86d57d353"
 
   url "https://github.com/jitsi/jitsi-meet-electron/releases/download/v#{version}/jitsi-meet.dmg"
   name "Jitsi Meet"
