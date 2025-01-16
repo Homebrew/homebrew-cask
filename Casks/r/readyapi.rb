355d94b1f0bddd0a7169d6ec2f382bd22855605b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-16 10:11:37 +0000

    readyapi 3.58.0

--- Casks/r/readyapi.rb
+++ Casks/r/readyapi.rb
@@ -1,9 +1,9 @@
 cask "readyapi" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.57.0"
-  sha256 arm:   "2db61e797f63b150db3b27cf86fcb4e03a33c4dfe7f4695e7a5f7e51f3de65ed",
-         intel: "a34e8befeeecaef9efb266aa37aeb6038cbe2cd5a3a682f5c41591324cfe60a0"
+  version "3.58.0"
+  sha256 arm:   "17b4c46d6732c44c49c871c0b9bac3a0ca808dfc28ace850fa9bb71644c8fdc1",
+         intel: "968fee3e1315d4509c6b9d4eaa2568ec47b27dbd385635838bb63022cb111af9"
 
   url "https://dl.eviware.com/ready-api/#{version}/ReadyAPI-#{arch}-#{version}.dmg",
       verified: "dl.eviware.com/ready-api/"
