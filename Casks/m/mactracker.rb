9fd944e828994f88045daedb7e7c478d78388dcb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-16 06:45:44 +0000

    mactracker 7.13.3

--- Casks/m/mactracker.rb
+++ Casks/m/mactracker.rb
@@ -1,6 +1,6 @@
 cask "mactracker" do
-  version "7.13.2"
-  sha256 "36cd9eb3ab8ca9f1ca99e555e9e8465d5d2936dbf64f778d60b9d1bec8ce8691"
+  version "7.13.3"
+  sha256 "4b33b3eecde1cbe3fc7c2bb2d7d2aa4bb48c1634450986c7a040aad0eade563e"
 
   url "https://mactracker.ca/downloads/Mactracker_#{version}.zip"
   name "Mactracker"
