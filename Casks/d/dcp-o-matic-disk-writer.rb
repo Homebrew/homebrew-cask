2244aecb2ce74ea33f44521e6c649d31b03a719e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-16 09:38:58 +0000

    dcp-o-matic-disk-writer 2.18.5

--- Casks/d/dcp-o-matic-disk-writer.rb
+++ Casks/d/dcp-o-matic-disk-writer.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-disk-writer" do
-  version "2.18.4"
-  sha256 "037096954e0b97b79aa06969f27474c0db183cfb2b36fdbe9e1ea38769d08f10"
+  version "2.18.5"
+  sha256 "aeb23487024583dfc243a5e7598542003250e989f25ff7e0a621a185781c8085"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-disk&version=#{version}"
   name "DCP-o-matic Disk Writer"
