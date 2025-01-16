804a316996dc2d1c6f3168fd8cabee1806bf21fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-16 06:39:51 +0000

    ente-auth 4.2.8

--- Casks/e/ente-auth.rb
+++ Casks/e/ente-auth.rb
@@ -1,6 +1,6 @@
 cask "ente-auth" do
-  version "4.2.7"
-  sha256 "32392666334e05df98b32164029d35b2ed7169f2caaaa07bc2e56c7a9952091b"
+  version "4.2.8"
+  sha256 "6679ab20262d17e7c4d02b7be693855a9e24d2305dfc443afe5509822eb644fa"
 
   url "https://github.com/ente-io/ente/releases/download/auth-v#{version}/ente-auth-v#{version}.dmg",
       verified: "github.com/ente-io/ente/"
