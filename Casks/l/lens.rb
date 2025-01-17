1df1e74e1aa60e7e0d219384a17283944a036f98
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-17 19:09:52 +0000

    lens 2025.1.161916

--- Casks/l/lens.rb
+++ Casks/l/lens.rb
@@ -1,9 +1,9 @@
 cask "lens" do
   arch arm: "-arm64"
 
-  version "2025.1.100124"
-  sha256 arm:   "a5758967f29fb8c3d8cbc9ac3a83018e2c57d104a913a4a0e2ac80fb436bbfb7",
-         intel: "73340bde45a7789b7367103ec9cfe812fd99b975fd64d4be05390f9185403c21"
+  version "2025.1.161916"
+  sha256 arm:   "48787c632750cec1c1a3621de00b5428a2e6c9a79b1501cacaa3e0a15eb70177",
+         intel: "8a296ca83adf349b08b16ca352297668399005b118792dad1196e1012a880b1f"
 
   url "https://api.k8slens.dev/binaries/Lens-#{version}-latest#{arch}.dmg"
   name "Lens"
