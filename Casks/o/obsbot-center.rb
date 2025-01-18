dd40a5a59cd0974cf3d0aaa5d9b7ec82b956f37b
Klaus Hipp <khipp@users.noreply.github.com>
2025-01-18 14:44:29 +0100

    obsbot-center: update livecheck and url

--- Casks/o/obsbot-center.rb
+++ Casks/o/obsbot-center.rb
@@ -1,8 +1,8 @@
 cask "obsbot-center" do
-  version "2.0.10.37"
-  sha256 "6a6cdf2d7535957c4c1f0b0173a7bb4ff928cfb04d252f8396ffa0712b557592"
+  version "2.0.10.37,31fab900"
+  sha256 "18a297580388d9c13460574c3eaa3a395af03f2a6478b2cdca0197af6b63a987"
 
-  url "https://resource-cdn.obsbothk.com/download/obsbot-center/Obsbot_Center_OA_E_MacOS_#{version}_release.dmg",
+  url "https://resource-cdn.obsbothk.com/download/obsbot-center/obsbot_center_oa_e_mac_os_#{version.csv.first.dots_to_underscores}_release_#{version.csv.second}.dmg",
       verified: "resource-cdn.obsbothk.com/download/obsbot-center/"
   name "OBSBOT Center"
   desc "Configuration and firmware update utility for OBSBOT Tiny and Meet series"
@@ -10,7 +10,13 @@ cask "obsbot-center" do
 
   livecheck do
     url "https://www.obsbot.com/download/obsbot-tiny-series"
-    regex(/Obsbot[._-]Center[._-]OA[._-]E[._-]MacOS[._-](\d+(?:\.\d+)+)[._-]release\.dmg/i)
+    regex(/href=.*?obsbot[._-]center[._-]oa[._-]e[._-]mac[._-]os[._-]v?(\d+(?:[._]\d+)+)[._-]release[._-](\h+)\.dmg/i)
+    strategy :page_match do |page, regex|
+      match = page.match(regex)
+      next if match.blank?
+
+      "#{match[1].tr("_", ".")},#{match[2]}"
+    end
   end
 
   depends_on macos: ">= :big_sur"
