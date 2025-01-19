cdb86506ddac3ca85a542be22bf1d4e5c2b37f4d
ransome1 <11188741+ransome1@users.noreply.github.com>
2025-01-19 09:54:25 +0100

    Update sleek to v2.0.16

--- Casks/s/sleek.rb
+++ Casks/s/sleek.rb
@@ -1,9 +1,9 @@
 cask "sleek" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.0.15"
-  sha256  arm:   "fcc407071edba351969e0e493c46bccf231d588a6540a4107dff1dfeebe5d97d",
-          intel: "e1ad5322a608915125cfb27fc9716a390e7d09a41bb766f48233e30190ac85d6"
+  version "2.0.16"
+  sha256  arm:   "293cdfeab2adbf0474c64c40e829f870a782d37588a7b2d20f181e266b980add",
+          intel: "53f9497c5e383b478c4ee2c7fedc24c6f23e44fca01d235c8e0c84cf41c32fd2"
 
   url "https://github.com/ransome1/sleek/releases/download/v#{version}/sleek-#{version}-mac-#{arch}.dmg"
   name "sleek"
