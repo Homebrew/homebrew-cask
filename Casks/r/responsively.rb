6661b2b121bdc4c1ef02d93fcc7c2cb885858686
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-19 15:44:33 +0000

    responsively 1.16.0

--- Casks/r/responsively.rb
+++ Casks/r/responsively.rb
@@ -1,9 +1,9 @@
 cask "responsively" do
   arch arm: "-arm64"
 
-  version "1.15.0"
-  sha256 arm:   "50c8bdfae88eaed22f494403603b2d0782cbe1f06b48866d7a49067a64d485ee",
-         intel: "362ef7061db79143b56e5e57756ace40b0ef25b8a25f1333a3df77f28af6182f"
+  version "1.16.0"
+  sha256 arm:   "01288a26c94e50559af9f663d9a77a5917240ff0f6c07f98dfa0e944a213a92f",
+         intel: "2795b7d962f0f88bbf16f19e3745401778d30e28f475ba2c11d793e2900c3aa7"
 
   url "https://github.com/responsively-org/responsively-app-releases/releases/download/v#{version}/ResponsivelyApp-#{version}#{arch}.dmg",
       verified: "github.com/responsively-org/responsively-app-releases/"
