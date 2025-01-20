4b67321dd0a8d7279d7509d0a9ccdda6c993ed41
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-01-20 18:46:20 +0900

    stratoshark@dev 0.9.0rc1,66,96332b0f5500 (new cask)

---
+++ Casks/s/stratoshark@dev.rb
@@ -0,0 +1,38 @@
+cask "stratoshark@dev" do
+  arch arm: "Arm", intel: "Intel"
+
+  version "0.9.0rc1,66,96332b0f5500"
+  sha256 arm:   "8381ce9351d399f9fde8984fd39c8b7311e889177de88e5a3f9785cb6866a052",
+         intel: "91da4629ef488abe5b5170a59df294d893408163f0b4227d4bf32a97fe052c48"
+
+  url "https://www.wireshark.org/download/automated/osx/Stratoshark%20#{version.csv.first}-#{version.csv.second}-g#{version.csv.third}%20#{arch}%2064.dmg"
+  name "Stratoshark"
+  desc "System calls and log messages analyzer"
+  homepage "https://wiki.wireshark.org/Stratoshark"
+
+  livecheck do
+    url "https://www.wireshark.org/download/automated/osx/"
+    regex(/href=.*?Stratoshark\s+v?(\d+(?:\.\d+)+(?:rc\d+)?)[._-](\d+)[._-]g(\h+)\s+#{arch}\s+64\.dmg/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[0]},#{match[1]},#{match[2]}" }
+    end
+  end
+
+  auto_updates true
+  conflicts_with cask: "stratoshark"
+  depends_on macos: ">= :big_sur"
+
+  app "Stratoshark.app"
+  binary "#{appdir}/Stratoshark.app/Contents/MacOS/extcap/falcodump"
+  binary "#{appdir}/Stratoshark.app/Contents/MacOS/extcap/sshdig"
+  manpage "#{appdir}/Stratoshark.app/Contents/Resources/share/man/man1/falcodump.1"
+  manpage "#{appdir}/Stratoshark.app/Contents/Resources/share/man/man1/sshdig.1"
+
+  uninstall quit: "org.wireshark.Stratoshark"
+
+  zap trash: [
+    "~/.config/stratoshark",
+    "~/Library/HTTPStorages/org.wireshark.Stratoshark",
+    "~/Library/Preferences/org.wireshark.Stratoshark.plist",
+  ]
+end
