8c87bf6140637b70ae1ff8a1a98bdc3c9c546928
Jin <108377235+0126kjw@users.noreply.github.com>
2025-01-21 13:29:40 +0900

    filenic 1.0.5

---
+++ Casks/f/filenic.rb
@@ -0,0 +1,13 @@
+cask "filenic" do
+  version "1.0.5"
+  sha256 "bc5ed45d6792df5ccdeab6259fdf0fd5dcf88c946cd7934aa28ff76be7979512"
+
+  url "https://github.com/0126kjw/FileNic/releases/download/v#{version}/FileNic.app.zip"
+  name "FileNic"
+  desc "Menu bar tool for fixing Korean filename encoding through drag-and-drop"
+  homepage "https://github.com/0126kjw/FileNic"
+
+  depends_on macos: ">= :ventura"
+
+  app "FileNic.app"
+end
