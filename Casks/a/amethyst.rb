19959bc14ef45284ada123b9686741e1426f17a7
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-01-20 22:43:18 -0500

    amethyst 0.22.1

--- Casks/a/amethyst.rb
+++ Casks/a/amethyst.rb
@@ -10,8 +10,8 @@ cask "amethyst" do
     end
   end
   on_catalina :or_newer do
-    version "0.22.0"
-    sha256 "dc9dae316132c03ec5a7ff90767164f8410a94e2d327f1b525a39e90f6fd90fe"
+    version "0.22.1"
+    sha256 "b2547f1bdf160a5f3bf1d2bf686121a45c32097a46b5a3fba7c928489a39aacb"
 
     url "https://github.com/ianyh/Amethyst/releases/download/v#{version}/Amethyst.zip",
         verified: "github.com/ianyh/Amethyst/"
