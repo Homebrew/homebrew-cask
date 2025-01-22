329950b23fc8eb7f64b6f1a9d02bf52ee670a819
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-22 09:42:56 +0000

    gns3 3.0.3

--- Casks/g/gns3.rb
+++ Casks/g/gns3.rb
@@ -1,7 +1,7 @@
 cask "gns3" do
   # NOTE: "3" is not a version number, but an intrinsic part of the product name
-  version "3.0.2"
-  sha256 "c91df589900aed0ac08c365dea1715e01a16f93ceed25f0df4d038c0516ed74d"
+  version "3.0.3"
+  sha256 "182fbbfd6a2a4f60c639c45ea635daff4fc93439d5ab7e0a3038ba516d2b6129"
 
   url "https://github.com/GNS3/gns3-gui/releases/download/v#{version}/GNS3-#{version}.dmg",
       verified: "github.com/GNS3/gns3-gui/"
