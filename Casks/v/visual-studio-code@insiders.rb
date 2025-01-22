386b545421687ec46745ac80def514175ed9313a
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-01-22 11:53:10 +0900

    visual-studio-code@insiders: update version

--- Casks/v/visual-studio-code@insiders.rb
+++ Casks/v/visual-studio-code@insiders.rb
@@ -1,11 +1,11 @@
 cask "visual-studio-code@insiders" do
   arch arm: "-arm64"
 
-  version "1.97.0-insider,d9069970f94c133d4bb5d300e9f8d7f174843cd9"
+  version "1.97.0-insider,1737454596186,d9069970f94c133d4bb5d300e9f8d7f174843cd9"
   sha256 arm:   "5156a8489eb4b27cee987b48a45781964768b35f12254b9267d2a92f578d366f",
          intel: "2f75b30a7c055a853c29c5540274fdc0554b3fc8c20bcf75dfc5c58ff2cc8ebb"
 
-  url "https://vscode.download.prss.microsoft.com/dbazure/download/insider/#{version.csv.second}/VSCode-darwin#{arch}.zip",
+  url "https://vscode.download.prss.microsoft.com/dbazure/download/insider/#{version.csv.third}/VSCode-darwin#{arch}.zip",
       verified: "vscode.download.prss.microsoft.com/"
   name "Microsoft Visual Studio Code Insiders"
   name "VS Code Insiders"
@@ -16,10 +16,11 @@ cask "visual-studio-code@insiders" do
     url "https://update.code.visualstudio.com/api/update/darwin#{arch}/insider/latest"
     strategy :json do |json|
       version = json["productVersion"]
+      timestamp	= json["timestamp"]
       build = json["version"]
-      next if version.blank? || build.blank?
+      next if version.blank? || timestamp.blank? || build.blank?
 
-      "#{version},#{build}"
+      "#{version},#{timestamp},#{build}"
     end
   end
 
