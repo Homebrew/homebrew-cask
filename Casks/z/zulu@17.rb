b28fcb57d86f4c83bf0be20544a648a70402d5eb
MoonFruit <dkmoonfruit@gmail.com>
2025-01-22 09:40:35 +0800

    zulu@17 17.0.14

--- Casks/z/zulu@17.rb
+++ Casks/z/zulu@17.rb
@@ -1,9 +1,9 @@
 cask "zulu@17" do
   arch arm: "aarch64", intel: "x64"
 
-  version "17.0.13,17.54.21"
-  sha256 arm:   "07755fa757eabea997ebcea1ce33db8a652f93f8332669c7f003e79a4b7763a0",
-         intel: "dd4a1e1968ce72c90a14b788983ed3966f518fb5087342e5df8c86b8cbdf07c2"
+  version "17.0.14,17.56.15"
+  sha256 arm:   "25568102f24a3acb3f45d8b7792aaf7b9697da7124260e029da012665cf977e4",
+         intel: "7d8ccc406d1c9f11881fd36955a52748c4f2d7eef905ba093c049b1ae2161229"
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-ca-jdk#{version.csv.first}-macosx_#{arch}.dmg",
       referer: "https://www.azul.com/downloads/"
