bf3e06f6e61441eb3c5cd6c282ce43d9a2b93355
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-01-24 00:11:06 +0900

    rapidminer-studio: use versioned url

--- Casks/r/rapidminer-studio.rb
+++ Casks/r/rapidminer-studio.rb
@@ -1,14 +1,14 @@
 cask "rapidminer-studio" do
   version "2025.0.0"
-  sha256 :no_check
+  sha256 "b0b309995c34491713ee04c488ac923f84759c698948ac138458edde09a81019"
 
-  url "https://releases.rapidminer.com/latest/rapidminer-studio/rapidminer-studio-osx.dmg"
+  url "https://releases.rapidminer.com/archive/rapidminer-studio/#{version}/ai-studio-osx-#{version}.dmg"
   name "RapidMiner Studio"
   desc "Data science platform"
   homepage "https://rapidminer.com/products/studio/"
 
   livecheck do
-    url :url
+    url "https://releases.rapidminer.com/latest/rapidminer-studio/rapidminer-studio-osx.dmg"
     strategy :header_match
   end
 
