d16dbc66afb809dab58451d11c14b28f0b406765
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-23 15:41:22 +0000

    dcp-o-matic-batch-converter 2.18.6

--- Casks/d/dcp-o-matic-batch-converter.rb
+++ Casks/d/dcp-o-matic-batch-converter.rb
@@ -1,6 +1,6 @@
 cask "dcp-o-matic-batch-converter" do
-  version "2.18.5"
-  sha256 "24944624a93884d2a36608aaf0bac17cab21335c38b0d5b6e9051a007ed54b0f"
+  version "2.18.6"
+  sha256 "a06db50392e6d18eb419dfc1aef7760ab1104d0019bb277200cf8fc78a185e65"
 
   url "https://dcpomatic.com/dl.php?id=osx-10.10-batch&version=#{version}"
   name "DCP-o-matic Batch converter"
