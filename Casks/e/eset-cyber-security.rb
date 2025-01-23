25520cfe417315e03e1f08be91e375d2f373fb37
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-01-23 23:37:47 +0900

    eset-cyber-security: use versioned url

--- Casks/e/eset-cyber-security.rb
+++ Casks/e/eset-cyber-security.rb
@@ -1,8 +1,8 @@
 cask "eset-cyber-security" do
   version "8.2.3000.0"
-  sha256 :no_check
+  sha256 "f637b06bafdc8743c936cf37bf64770d1eb7fd128b7dd06fa4ff7d54acaff91e"
 
-  url "https://download.eset.com/com/eset/apps/home/eav/mac/latest/eset_cybersecurity.dmg"
+  url "https://download.eset.com/com/eset/apps/home/eav/mac/v#{version.major}/#{version}/eset_cybersecurity.dmg"
   name "ESET Cyber Security"
   desc "Security including web and email protection"
   homepage "https://www.eset.com/"
