5c46f3c4e53e29442985afba4f1cf574aa4eca37
Andrew Guldman <gnaryak@gmail.com>
2025-01-23 13:40:21 -0800

    Provide the correct location of the fly tgz file

--- Casks/f/fly.rb
+++ Casks/f/fly.rb
@@ -2,7 +2,7 @@ cask "fly" do
   version "7.12.0"
   sha256 "24c2feb62b293196cda2e08950affa7f2850b42a985336035fd03a606b25b935"
 
-  url "https://github.com/concourse/concourse/releases/download/v#{version}/fly-#{version}-darwin-amd64.tgz"
+  url "https://github.com/concourse/concourse/releases/download/v#{version}/fly-darwin-amd64.tgz"
   name "fly"
   desc "Official CLI tool for Concourse CI"
   homepage "https://github.com/concourse/concourse"
