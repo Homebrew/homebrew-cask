f6aa6ccddc658e71d7a15fa32e168fdd65a99c2d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-23 18:54:16 +0000

    livebook 0.14.6

--- Casks/l/livebook.rb
+++ Casks/l/livebook.rb
@@ -1,6 +1,6 @@
 cask "livebook" do
-  version "0.14.5"
-  sha256 "e82926e11638775812d4c2ae5ee55396f20c485fb8e5c29a6d60ab43b8850a25"
+  version "0.14.6"
+  sha256 "cf9ef7c8c81a98177e193080233c256234be7fd861a350f557ccc8fd1f895052"
 
   url "https://github.com/livebook-dev/livebook/releases/download/v#{version}/LivebookInstall-macos-universal.dmg",
       verified: "github.com/livebook-dev/livebook/"
