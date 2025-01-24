e7f0e7fc93f993edb92ff9ea286d51597bf331bf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-24 01:02:39 +0000

    mochi 1.18.7

--- Casks/m/mochi.rb
+++ Casks/m/mochi.rb
@@ -1,9 +1,9 @@
 cask "mochi" do
   arch arm: "-arm64"
 
-  version "1.18.5"
-  sha256 arm:   "55bd59e90528951cdb6010f873c9614aeaa793db9aece101335d7abc71a5a9fc",
-         intel: "e195a5adb495a93669f8dfe561d6c69f8abd51c0e4b0528eb9d15e46ddafc607"
+  version "1.18.7"
+  sha256 arm:   "db4bd4124e35b0c7fc2ea7abf2b8a7a8e4df3602737d9fdd3bb7dc77f4e50a7a",
+         intel: "303b8a43798a75e2b09bb9425c4bbabc60c64665e5395cd9bdcacd336318194c"
 
   url "https://mochi.cards/releases/Mochi-#{version}#{arch}.dmg"
   name "Mochi"
