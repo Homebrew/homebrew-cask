3516dd8d442813ecbc6ef6c681c81e317a06a333
Rui Chen <rui@chenrui.dev>
2025-01-24 18:42:38 -0500

    nvidia-nsight-systems 2025.1.1.103-3542797

--- Casks/n/nvidia-nsight-systems.rb
+++ Casks/n/nvidia-nsight-systems.rb
@@ -1,6 +1,6 @@
 cask "nvidia-nsight-systems" do
-  version "2024.7.1.84-3512561"
-  sha256 "9bc4027383649845f8f4ffe5bc9c30aaffbb83167f21c92504ffb8532ea5e018"
+  version "2025.1.1.103-3542797"
+  sha256 "9af04009a25c1c5a2a6e77a290354f194987f6b0f477582471a94512ad4ff8f4"
 
   url "https://developer.nvidia.com/downloads/assets/tools/secure/nsight-systems/#{version.major_minor.dots_to_underscores}/NsightSystems-macos-public-#{version}.dmg"
   name "NVIDIA Nsight Systems"
