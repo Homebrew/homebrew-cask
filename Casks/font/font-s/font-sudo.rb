4bce17634e10f53105283edb826301a4f4f2b2be
Klaus Hipp <khipp@users.noreply.github.com>
2025-01-26 17:01:55 +0100

    font-sudo 2.2

--- Casks/font/font-s/font-sudo.rb
+++ Casks/font/font-s/font-sudo.rb
@@ -1,10 +1,11 @@
 cask "font-sudo" do
-  version "2.1"
-  sha256 "4d7d108cb41dc9a8edf8b6912a192ff56dc810f058f76f499d2a732ea59e7223"
+  version "2.2"
+  sha256 "b9a5d14348f4ea01dfe02a75d2c5181136d3cbf10277ae0f0b4115c40763f47b"
 
-  url "https://github.com/jenskutilek/sudo-font/releases/download/v#{version}/sudo.zip"
+  url "https://github.com/jenskutilek/sudo-font/releases/download/v#{version}/sudo.zip",
+      verified: "github.com/jenskutilek/sudo-font/"
   name "Sudo"
-  homepage "https://github.com/jenskutilek/sudo-font/"
+  homepage "https://www.kutilek.de/sudo-font/"
 
   font "sudo/SudoUIVariable.ttf"
   font "sudo/SudoVariable.ttf"
