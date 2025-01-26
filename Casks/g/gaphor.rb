768f334a9741dabac967ee0d52907a8d26cc9ff5
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-01-26 09:51:45 -0500

    gaphor 3.0.0

--- Casks/g/gaphor.rb
+++ Casks/g/gaphor.rb
@@ -1,9 +1,9 @@
 cask "gaphor" do
   arch arm: "arm", intel: "intel"
 
-  version "2.27.0"
-  sha256 arm:   "22977a35ae01a410a1a15cffa85867dd35d718db9753d0f3615d427b6fc0059a",
-         intel: "17f6dcb0f862658d5ba08bff6a5e4ed1d1b36fd9232a4601b5b5a751f4289028"
+  version "3.0.0"
+  sha256 arm:   "6b5e4ca457f95fe8cc51e9d0800c284a44d3a6bf5a2a79587a502a227509b8e9",
+         intel: "7b360eb0de30a1171e61d04f8c38ce318f44f7eb6516b3f855c7145e6c9d3f84"
 
   url "https://github.com/gaphor/gaphor/releases/download/#{version}/Gaphor-#{version}-#{arch}.dmg",
       verified: "github.com/gaphor/gaphor/"
