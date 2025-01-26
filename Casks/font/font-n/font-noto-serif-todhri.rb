43fa482e58899e621344e0914cf028e1f1f956ef
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-26 00:14:06 +0000

    font-noto-serif-todhri: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml)  workflow.

--- Casks/font/font-n/font-noto-serif-todhri.rb
+++ Casks/font/font-n/font-noto-serif-todhri.rb
@@ -2,9 +2,10 @@ cask "font-noto-serif-todhri" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/notoseriftodhri/NotoSerifTodhri-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/notoseriftodhri/NotoSerifTodhri-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Noto Serif Todhri"
-  homepage "https://github.com/notofonts/todhri"
+  homepage "https://fonts.google.com/specimen/Noto+Serif+Todhri"
 
   font "NotoSerifTodhri-Regular.ttf"
 
