a3dfc50396e6ccc2bc215251a55ef75fa231491b
bevanjkay <email@bevankay.me>
2025-01-27 13:47:25 +1100

    macai 2.0.5

--- Casks/m/macai.rb
+++ Casks/m/macai.rb
@@ -1,6 +1,6 @@
 cask "macai" do
-  version "2.0.4"
-  sha256 "231e4ae414f6025d25d0d2fcff230ee98c195575f018bc158262969c2ddbd9d6"
+  version "2.0.5"
+  sha256 "31affa2a9a6239102106ed0182af12e6a3a3ff74379cbe774c98c7d766c3371d"
 
   url "https://github.com/Renset/macai/releases/download/v#{version}/macai.#{version}.Universal.zip"
   name "macai"
