7437e3d057b5e18b0e0054e2c0e6376897aa8b74
Bevan Kay <email@bevankay.me>
2025-01-27 22:41:28 +1100

    dockfix 3.5 (new cask)

---
+++ Casks/d/dockfix.rb
@@ -0,0 +1,25 @@
+cask "dockfix" do
+  version "3.5"
+  sha256 :no_check
+
+  url "https://lubker.dk/dockfix/download.php"
+  name "DockFix"
+  desc "Dock replacement"
+  homepage "https://dockfix.lubker.dk/"
+
+  livecheck do
+    url "https://dockfix.lubker.dk/changelog/"
+    regex(/<h2[^>]*?>[^<]*?v?(\d+(?:\.\d+)+)[ <"]/i)
+  end
+
+  auto_updates true
+  depends_on macos: ">= :ventura"
+
+  app "DockFix.app"
+
+  zap trash: [
+    "~/Library/Caches/dk.FIrstForm.DockFix",
+    "~/Library/HTTPStorages/dk.FIrstForm.DockFix",
+    "~/Library/Preferences/dk.FIrstForm.DockFix.plist",
+  ]
+end
