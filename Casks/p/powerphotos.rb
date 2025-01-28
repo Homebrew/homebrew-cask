9df96e6892d84439e82ab498ef90e698634c3919
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-28 21:49:45 +0000

    powerphotos 2.7.6

--- Casks/p/powerphotos.rb
+++ Casks/p/powerphotos.rb
@@ -60,7 +60,7 @@ cask "powerphotos" do
     end
   end
   on_monterey :or_newer do
-    version "2.7.5"
+    version "2.7.6"
     sha256 :no_check
 
     url "https://www.fatcatsoftware.com/powerphotos/PowerPhotos.zip"
