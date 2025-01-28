852c4a97a27217c8b363d731b8e275fa2df67d2b
Rui Chen <rui@chenrui.dev>
2025-01-28 10:59:09 -0500

    tencent-docs 3.9.6

--- Casks/t/tencent-docs.rb
+++ Casks/t/tencent-docs.rb
@@ -1,9 +1,9 @@
 cask "tencent-docs" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.9.0"
-  sha256 arm:   "5932dd7ea637371a48643e4434e9cc0a58fa9e58dfb314f8a7f9474d4ae36431",
-         intel: "6adaf4f4ae6c4ad3e4a4a623f2e0c8e6dbf96fffe1ed5cbe4b76b5894acc98d2"
+  version "3.9.6"
+  sha256 arm:   "c7a30997594965bf6c1f3c7a8cab541477c6098a9c18e2df8e4e230abbba259a",
+         intel: "ac3a82efe992544e8033b75961b33697e8130f93959034d1a34b4494c52ed32e"
 
   url "https://desktop.docs.qq.com/Installer/30001/#{version}/TencentDocs-#{arch}.dmg"
   name "Tencent Docs"
