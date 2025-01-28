0087b20a665c008b20793691ea193a92299746cd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-28 00:51:42 +0000

    gdevelop 5.5.223

--- Casks/g/gdevelop.rb
+++ Casks/g/gdevelop.rb
@@ -1,6 +1,6 @@
 cask "gdevelop" do
-  version "5.5.222"
-  sha256 "87d23e7a0b11453128262ea2ba56bd6d31d7c605b7ca0fbf1e0d0fe986a6664d"
+  version "5.5.223"
+  sha256 "6b5154a0feb18120208e5aabafbc96772a1867901b60f651cf5cf3c6534c2286"
 
   url "https://github.com/4ian/GDevelop/releases/download/v#{version}/GDevelop-#{version.major}-#{version}-universal.dmg",
       verified: "github.com/4ian/GDevelop/"
