37648b64ca7c143a58632b6ae6af02e12acfd00c
Rui Chen <rui@chenrui.dev>
2025-01-27 22:44:29 -0500

    vk-calls 1.38.27560

--- Casks/v/vk-calls.rb
+++ Casks/v/vk-calls.rb
@@ -1,6 +1,6 @@
 cask "vk-calls" do
-  version "1.38.26980"
-  sha256 "31116e605f7731fe19b1973503b1e2dfaaa2236ba7ce41b9146d0213af54bfb5"
+  version "1.38.27560"
+  sha256 "364cf6c331b6f4dc3972ab781207207212bc6f415c885db842eb3db5c752cceb"
 
   url "https://vkcalls-native-ac.vk-apps.com/#{version}/vk-calls.dmg",
       verified: "vkcalls-native-ac.vk-apps.com/"
