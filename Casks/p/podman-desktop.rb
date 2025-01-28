f19516fba6d2e12e66699f855e156111594d7789
Anka <runner@Mac-1738064896145.local>
2025-01-28 12:02:14 +0000

    podman-desktop 1.16.0

--- Casks/p/podman-desktop.rb
+++ Casks/p/podman-desktop.rb
@@ -1,9 +1,9 @@
 cask "podman-desktop" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.15.0"
-  sha256 arm:   "aecc4c566c260835ed2867505e9744134ed6457292c21420dc77066db106c890",
-         intel: "4d2db6df6a1aee59c3878137177677cb62065f643f49157fb56225be0a419045"
+  version "1.16.0"
+  sha256 arm:   "fb5e714c3414c33a2ba09825197331a6cbb0057d55a1ba00fca356d9221905ff",
+         intel: "276e3484d28ed164ac0113b59f77dde2962d0850eb1a8bc490c5d31757215bab"
 
   url "https://github.com/containers/podman-desktop/releases/download/v#{version}/podman-desktop-#{version}-#{arch}.dmg",
       verified: "github.com/containers/podman-desktop/"
