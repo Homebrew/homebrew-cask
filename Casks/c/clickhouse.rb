45d3431ce3a16263e2de4c42544959e8ce9419f6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-28 18:37:21 +0000

    clickhouse 25.1.1.4165-stable

--- Casks/c/clickhouse.rb
+++ Casks/c/clickhouse.rb
@@ -1,9 +1,9 @@
 cask "clickhouse" do
   arch arm: "-aarch64"
 
-  version "24.12.3.47-stable"
-  sha256 arm:   "686612fbdf5d4a388bb37eb29150e3748fbbe4b1a128d7c97a9890fb94d085c8",
-         intel: "32947c6ec81667466ba4320b971f6ee387fcc4137a8b21ef8396a97e7fdbd043"
+  version "25.1.1.4165-stable"
+  sha256 arm:   "217390ffa7103d1617a6e16d96f99d0e7cda83bb7e0cb0b6481c5ae0d78ae49c",
+         intel: "44760edf36a42b310a4b16a54df02a8e865654d59638e45dde4317f7ef4d9141"
 
   url "https://github.com/ClickHouse/ClickHouse/releases/download/v#{version}/clickhouse-macos#{arch}",
       verified: "github.com/ClickHouse/ClickHouse/"
