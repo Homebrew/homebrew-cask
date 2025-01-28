57ee085b8f7fd097a9de02574b49f92538d38842
Rui Chen <rui@chenrui.dev>
2025-01-28 17:12:18 -0500

    ipremoteutility 1.9.12

--- Casks/i/ipremoteutility.rb
+++ Casks/i/ipremoteutility.rb
@@ -1,6 +1,6 @@
 cask "ipremoteutility" do
-  version "1.9.11"
-  sha256 "6398e3979970d09ace6dd06fa21617ab3a722ca35d35321ed17926f84f5bd378"
+  version "1.9.12"
+  sha256 "8354e149e6cff95e66e151749f6e4ce831a721ec9fa59c09d87b1677769d6bb0"
 
   url "https://www.flandersscientific.com/ip-remote/release/IPRemoteUtility-#{version}-macOS.zip"
   name "Flanders IP Remote Utility"
@@ -13,7 +13,7 @@ cask "ipremoteutility" do
   end
 
   depends_on macos: ">= :high_sierra"
-  container nested: "IPRemoteUtility-#{version}-macOS/IPRemoteUtility-#{version}.dmg"
+  container nested: "IPRemoteUtility-#{version}-macOS/IPRemoteUtility.dmg"
 
   app "IPRemoteUtility.app"
 
