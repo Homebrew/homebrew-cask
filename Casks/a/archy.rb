71c8f5d56db3d2cb79a2d837ccec09c8f75917c2
Sean Molenaar <smillerdev@me.com>
2025-01-28 18:38:15 +0100

    archy: add linux support

--- Casks/a/archy.rb
+++ Casks/a/archy.rb
@@ -1,8 +1,11 @@
 cask "archy" do
   version "2.33.1"
-  sha256 "3bc1876ffb16e4f369e5f002d368dcf4f938449a70e6e010a565d00c223b0eaf"
+  os macos: "macos", linux: "linux"
 
-  url "https://sdk-cdn.mypurecloud.com/archy/#{version}/archy-macos.zip",
+  sha256 x86_64:       "3bc1876ffb16e4f369e5f002d368dcf4f938449a70e6e010a565d00c223b0eaf",
+         x86_64_linux: "42c00a0acc938ca5f21c9470bab2f3e8966a4a1c1517170aeefddf2c2ecfd479"
+
+  url "https://sdk-cdn.mypurecloud.com/archy/#{version}/archy-#{os}.zip",
       verified: "sdk-cdn.mypurecloud.com/archy/"
   name "Archy"
   desc "YAML processor"
@@ -15,7 +18,7 @@ cask "archy" do
     end
   end
 
-  binary "archyBin/archy-macos-#{version}", target: "archy"
+  binary "archyBin/archy-#{os}-#{version}", target: "archy"
 
   zap trash: "~/.archy_config"
 
