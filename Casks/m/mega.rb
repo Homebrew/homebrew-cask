6435703ec900932f7a6e40b0cb17691193bcff6f
Rui Chen <rui@chenrui.dev>
2025-01-28 19:58:14 -0500

    mega: update livecheck
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/m/mega.rb
+++ Casks/m/mega.rb
@@ -9,8 +9,21 @@ cask "mega" do
   homepage "https://megasoftware.net/"
 
   livecheck do
-    url "https://megasoftware.net/history"
-    regex(/MEGA\s\d+\sversion\s(\d+(?:\.\d+)+)/i)
+    url "https://www.megasoftware.net/current_release/"
+    strategy :json do |json|
+      json.map do |item|
+        next if item["operating_system"] != "mac" ||
+                item["release_type"] != "stable" ||
+                !item["interface"].match?(/Graphical|GUI/i)
+
+        major = item["major"]
+        minor = item["minor"]
+        release = item["release"]
+        next if major.blank? || minor.blank? || release.blank?
+
+        "#{major}.#{minor}.#{release}"
+      end
+    end
   end
 
   auto_updates true
