ee6a1ae6f3365ebdd0277ee20a0ab36409a215ff
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-30 00:47:21 +0000

    cork 1.5.1

--- Casks/c/cork.rb
+++ Casks/c/cork.rb
@@ -1,6 +1,6 @@
 cask "cork" do
-  version "1.5"
-  sha256 "288048aa25fc8814bc47b9198b7b859ecc63614731c626a54c8e444619598780"
+  version "1.5.1"
+  sha256 "29a29e14cb1b60366466065b263e1a64c22a28e6c625be3fe14fc472a6ae7dc1"
 
   url "https://github.com/buresdv/Cork/releases/download/v#{version}/Cork.zip",
       verified: "github.com/buresdv/Cork/"
