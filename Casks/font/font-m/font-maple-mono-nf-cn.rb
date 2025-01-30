88917d7df20cdbe9a161913745e2b0a4618f5cdd
Klaus Hipp <khipp@users.noreply.github.com>
2025-01-30 16:33:28 +0100

    font-maple-mono-nf-cn 7.0-beta36 (new cask)

---
+++ Casks/font/font-m/font-maple-mono-nf-cn.rb
@@ -0,0 +1,31 @@
+cask "font-maple-mono-nf-cn" do
+  version "7.0-beta36"
+  sha256 "c5f903ce1b98cd46fd7d22e66158c3331899514eb047f44a55c10a0c50e48f88"
+
+  url "https://github.com/subframe7536/Maple-font/releases/download/v#{version}/MapleMono-NF-CN-unhinted.zip"
+  name "Maple Mono NF CN"
+  homepage "https://github.com/subframe7536/Maple-font"
+
+  livecheck do
+    cask "font-maple-mono"
+  end
+
+  font "MapleMono-NF-CN-Bold.ttf"
+  font "MapleMono-NF-CN-BoldItalic.ttf"
+  font "MapleMono-NF-CN-ExtraBold.ttf"
+  font "MapleMono-NF-CN-ExtraBoldItalic.ttf"
+  font "MapleMono-NF-CN-ExtraLight.ttf"
+  font "MapleMono-NF-CN-ExtraLightItalic.ttf"
+  font "MapleMono-NF-CN-Italic.ttf"
+  font "MapleMono-NF-CN-Light.ttf"
+  font "MapleMono-NF-CN-LightItalic.ttf"
+  font "MapleMono-NF-CN-Medium.ttf"
+  font "MapleMono-NF-CN-MediumItalic.ttf"
+  font "MapleMono-NF-CN-Regular.ttf"
+  font "MapleMono-NF-CN-SemiBold.ttf"
+  font "MapleMono-NF-CN-SemiBoldItalic.ttf"
+  font "MapleMono-NF-CN-Thin.ttf"
+  font "MapleMono-NF-CN-ThinItalic.ttf"
+
+  # No zap stanza required
+end
