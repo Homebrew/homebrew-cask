1e4fa369b15cae3ed79b98cd9410261c4fbc1e25
LangLangbart <92653266+LangLangBart@users.noreply.github.com>
2025-01-31 22:48:41 +0100

    chore: freetube include legacy version
    The latest version requires macOS 11 or later.

--- Casks/f/freetube.rb
+++ Casks/f/freetube.rb
@@ -1,17 +1,31 @@
 cask "freetube" do
-  version "0.23.0"
-  sha256 "d7de2c77339c017b4631694275ccd58163da28dfadce4bde055f0f21eeb6e660"
+  on_catalina do
+    version "0.22.1"
+    sha256 "0e9eb9db841f36671c81fedff4580c39dbbd6bd541d5158ed4897218c4134946"
 
-  url "https://github.com/FreeTubeApp/FreeTube/releases/download/v#{version}-beta/freetube-#{version}-mac-x64.dmg",
-      verified: "github.com/FreeTubeApp/FreeTube/"
+    url "https://github.com/FreeTubeApp/FreeTube/releases/download/v#{version}-beta/freetube-#{version}-mac-x64.dmg",
+        verified: "github.com/FreeTubeApp/FreeTube/"
+
+    livecheck do
+      skip "Legacy version"
+    end
+  end
+
+  on_big_sur :or_newer do
+    version "0.23.0"
+    sha256 "d7de2c77339c017b4631694275ccd58163da28dfadce4bde055f0f21eeb6e660"
+
+    url "https://github.com/FreeTubeApp/FreeTube/releases/download/v#{version}-beta/freetube-#{version}-mac-x64.dmg",
+        verified: "github.com/FreeTubeApp/FreeTube/"
+
+    livecheck do
+      url :url
+      regex(/^v?(\d+(?:\.\d+)+)/i)
+    end
+  end
   name "FreeTube"
   desc "YouTube player focusing on privacy"
   homepage "https://freetubeapp.io/"
-
-  livecheck do
-    url :url
-    regex(/^v?(\d+(?:\.\d+)+)/i)
-  end
 
   depends_on macos: ">= :catalina"
 
