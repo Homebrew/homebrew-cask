82ac4fb4f9701802aa5d1827d1838ab594310e81
Mikkel Malmberg <mikkel@brnbw.com>
2025-01-31 11:24:00 +0100

    Apply suggestions from code review
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/l/leader-key.rb
+++ Casks/l/leader-key.rb
@@ -1,18 +1,25 @@
 cask "leader-key" do
-  version "1.6.0"
-  sha256 :no_check
+  version "1.6.0,57"
+  sha256 "79aae74c790166dd806cac27f94fd6aacdbee889622084e13f326f65ea19035f"
 
-  url "https://leader-key-updates.s3.amazonaws.com/Leader%20Key-latest.zip",
+  url "https://leader-key-updates.s3.amazonaws.com/Leader%20Key.b#{version.csv.second}.zip",
       verified: "leader-key-updates.s3.amazonaws.com/"
   name "Leader Key"
-  desc "*faster than your launcher* launcher"
+  desc "Application launcher"
   homepage "https://github.com/mikker/LeaderKey.app"
 
   livecheck do
     url "https://leader-key-updates.s3.amazonaws.com/appcast.xml"
-    strategy :sparkle, &:short_version
+    regex(%r{/Leader%20Key[._-]b(\d+(?:\.\d+)*)\.zip}i)
+    strategy :sparkle do |item, regex|
+      match = item.url.match(regex)
+      next if match.blank?
+
+      "#{item.short_version},#{match[1]}"
+    end
   end
 
+  auto_updates true
   depends_on macos: ">= :ventura"
 
   app "Leader Key.app"
