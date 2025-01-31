eb90530e9b625cee55d720ef7332dd781f07633f
Klaus Hipp <khipp@users.noreply.github.com>
2025-01-31 11:05:48 +0100

    drata-agent: add livecheck

--- Casks/d/drata-agent.rb
+++ Casks/d/drata-agent.rb
@@ -8,6 +8,11 @@ cask "drata-agent" do
   desc "Security audit software"
   homepage "https://drata.com/"
 
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
   depends_on macos: ">= :catalina"
 
   app "Drata Agent.app"
