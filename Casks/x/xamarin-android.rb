d353532fe70a3a721417fdfce4bb00bd705dbd38
Vinayak Nikam <vinayak.nikam@toro.com>
2025-02-01 16:06:39 -0600

    resolved CI errors

--- Casks/x/xamarin-android.rb
+++ Casks/x/xamarin-android.rb
@@ -1,8 +1,9 @@
 cask "xamarin-android" do
   version "13.2.2.0"
-  sha256 "aeb215d33324f8d8dbf41aae2a987249433a7a4c264588384a40695c162877f4"
+  sha256 :no_check
 
-  url "https://aka.ms/xamarin-android-commercial-d17-8-macos"
+  url "https://aka.ms/xamarin-android-commercial-d17-8-macos",
+      verified: "aka.ms"
   name "Xamarin.Android"
   desc "Gives .NET developers complete access to Android SDK's"
   homepage "https://www.xamarin.com/platform"
