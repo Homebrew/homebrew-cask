5547f3ec99361d181b32199683ae7344f84afdfe
Sam Ford <1584702+samford@users.noreply.github.com>
2025-02-02 09:22:03 -0500

    xamarin-android: update homepage
    This updates the homepage to resolve the redirection and also allow
    us to omit the `verified` `url` argument.

--- Casks/x/xamarin-android.rb
+++ Casks/x/xamarin-android.rb
@@ -2,11 +2,10 @@ cask "xamarin-android" do
   version "13.2.2.0,8cbf56b1-ef0d-466f-8cfe-fae4ba8c5080,e9e853fee3169b1c5128098942f19120"
   sha256 "aeb215d33324f8d8dbf41aae2a987249433a7a4c264588384a40695c162877f4"
 
-  url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv.second}/#{version.csv.third}/xamarin.android-#{version.csv.first}.pkg",
-      verified: "download.visualstudio.microsoft.com/download/pr/"
+  url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv.second}/#{version.csv.third}/xamarin.android-#{version.csv.first}.pkg"
   name "Xamarin.Android"
   desc "Gives .NET developers complete access to Android SDK's"
-  homepage "https://www.xamarin.com/platform"
+  homepage "https://dotnet.microsoft.com/en-us/apps/xamarin"
 
   deprecate! date: "2024-08-20", because: :discontinued
 
