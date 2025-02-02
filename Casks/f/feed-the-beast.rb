5568d973980612311b781f1ec5ef4faf165bc429
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-02-02 13:35:52 +0900

    feed-the-beast 1.27.2

--- Casks/f/feed-the-beast.rb
+++ Casks/f/feed-the-beast.rb
@@ -2,18 +2,18 @@ cask "feed-the-beast" do
   arch arm: "arm64", intel: "x64"
   livecheck_arch = on_arch_conditional arm: "arm", intel: "x64"
 
-  version "1.27.0"
-  sha256 arm:   "cf95928faaac180f2e6c55800250a76d88a5d39127238101bd19f065e3fbc2a4",
-         intel: "2b04f24dfa6d0288f1131ec31a430384eddf3a5383f918f85559f602a4f147f4"
+  version "1.27.2"
+  sha256 arm:   "28fd3c16d886edae188423842b8ebaadf61d6e976d2ded3aac0842e15013517f",
+         intel: "f28b00324afbca50b144c8c113e86e989db3d33c981ffbb8a5c31753489374dc"
 
-  url "https://piston.feed-the-beast.com/app/ftb-app-#{version}-#{arch}.dmg"
+  url "https://piston.feed-the-beast.com/app/ftb-app-macos-#{version}-#{arch}.dmg"
   name "Feed the Beast"
   desc "Minecraft mod downloader and manager"
   homepage "https://www.feed-the-beast.com/"
 
   livecheck do
     url "https://meta.feed-the-beast.com/v1/app/versions"
-    regex(/ftb[._-]app[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}\.dmg/i)
+    regex(/ftb[._-]app[._-]macos[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}\.dmg/i)
     strategy :json do |json, regex|
       match = json.dig("macos", livecheck_arch, "dmg", "url")&.match(regex)
       next if match.blank?
