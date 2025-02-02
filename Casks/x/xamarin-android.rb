ead8c1d243fd8926f6031de8c2d425ba4ca682a6
Sam Ford <1584702+samford@users.noreply.github.com>
2025-02-02 09:02:21 -0500

    xamarin: update url

--- Casks/x/xamarin-android.rb
+++ Casks/x/xamarin-android.rb
@@ -1,9 +1,9 @@
 cask "xamarin-android" do
-  version "13.2.2.0"
-  sha256 :no_check
+  version "13.2.2.0,8cbf56b1-ef0d-466f-8cfe-fae4ba8c5080,e9e853fee3169b1c5128098942f19120"
+  sha256 "aeb215d33324f8d8dbf41aae2a987249433a7a4c264588384a40695c162877f4"
 
-  url "https://aka.ms/xamarin-android-commercial-d17-8-macos",
-      verified: "aka.ms/"
+  url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv.second}/#{version.csv.third}/xamarin.android-#{version.csv.first}.pkg",
+      verified: "download.visualstudio.microsoft.com/download/pr/"
   name "Xamarin.Android"
   desc "Gives .NET developers complete access to Android SDK's"
   homepage "https://www.xamarin.com/platform"
