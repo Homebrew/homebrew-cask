286619b521dc7b4fdc9cf8a47d8c9260d9942070
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-03 15:35:41 +0000

    defold 1.9.7

--- Casks/d/defold.rb
+++ Casks/d/defold.rb
@@ -1,7 +1,7 @@
 cask "defold" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.9.6"
+  version "1.9.7"
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://github.com/defold/defold/releases/download/#{version}/Defold-#{arch}-macos.dmg",
