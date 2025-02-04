722352ac3b2577c33903a1bd5795ac615f8ea990
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-02-04 14:08:58 +0900

    leader-key 1.7.0

--- Casks/l/leader-key.rb
+++ Casks/l/leader-key.rb
@@ -1,6 +1,6 @@
 cask "leader-key" do
-  version "1.6.0,57"
-  sha256 "79aae74c790166dd806cac27f94fd6aacdbee889622084e13f326f65ea19035f"
+  version "1.7.0,66"
+  sha256 "18db81ac1d8b2789a4342c4548b65884d48b83699654837701c7863e7198a30d"
 
   url "https://leader-key-updates.s3.amazonaws.com/Leader%20Key.b#{version.csv.second}.zip",
       verified: "leader-key-updates.s3.amazonaws.com/"
