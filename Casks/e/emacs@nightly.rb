76b3e49daf243a077d9b9269896cb1f604bd1158
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-04 09:38:39 +0000

    emacs@nightly 2025-02-04_00-09-07

--- Casks/e/emacs@nightly.rb
+++ Casks/e/emacs@nightly.rb
@@ -1,8 +1,8 @@
 cask "emacs@nightly" do
   arch arm: "arm64-11", intel: "x86_64-10_12"
 
-  version "2025-02-03_00-09-13,a8e85243790b6ab9b9e261c77ddfdc65a9867069"
-  sha256 "2375a9c415e54f775b90ae227cbca726defbd75d5e125c4fe0b3d1d2d56374c2"
+  version "2025-02-04_00-09-07,c4a67a405072601b9d7dd95dc94c0b402e1e0ae6"
+  sha256 "ef76df91a64b4d655f1dca14a02a25530fd5701e6e5928b5b666321855ca5863"
 
   url "https://emacsformacosx.com/emacs-builds/Emacs-#{version.csv.first}-#{version.csv.second}-universal.dmg"
   name "Emacs"
