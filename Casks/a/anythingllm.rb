10a7d40c006c114640348cda0df3a31404f98fd2
timothycarambat <rambat1010@gmail.com>
2025-02-05 14:41:13 -0800

    Patch anythingllm to new CDN + allowlisting via referer

--- Casks/a/anythingllm.rb
+++ Casks/a/anythingllm.rb
@@ -5,14 +5,14 @@ cask "anythingllm" do
   sha256 arm:   "c642ee866d21ec5d6f867c6287de54a32cd4cbfb90aa80e4eee72b4c2d18eca1",
          intel: "c495348c176ba2ba7f8c790dd408be69e3375211294f63c95c8f37090449998a"
 
-  url "https://cdn.useanything.com/latest/AnythingLLMDesktop#{arch}.dmg",
-      verified: "cdn.useanything.com/"
+  url "https://cdn.anythingllm.com/latest/AnythingLLMDesktop#{arch}.dmg",
+      referer: "homebrew-cask"
   name "AnythingLLM"
   desc "Private desktop AI chat application"
   homepage "https://anythingllm.com/"
 
   livecheck do
-    url "https://cdn.useanything.com/latest/version.txt"
+    url "https://cdn.anythingllm.com/latest/version.txt"
     regex(/(\d+(?:\.\d+)+)/i)
   end
 
