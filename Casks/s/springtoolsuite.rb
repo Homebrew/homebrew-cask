ba7189a0c93db7d5dcdcae2e1d756690e2f3a7bb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-05 15:55:18 +0000

    springtoolsuite 4.28.0

--- Casks/s/springtoolsuite.rb
+++ Casks/s/springtoolsuite.rb
@@ -1,9 +1,9 @@
 cask "springtoolsuite" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "4.27.0,4.34.0"
-  sha256 arm:   "1ead93dd6c1c3f4f7e3408ad591e276967c45029010eb2263535d0251c1f194a",
-         intel: "eb9527008dff28a35b9309a8055efa6a878be31f9b6be98c08b4e42771116c67"
+  version "4.28.0,4.34.0"
+  sha256 arm:   "981876adbdab556c67fb048dd522671ddf368ed36103f864879b68a6bc728f92",
+         intel: "24a6bc6597b237475a602dc73be7504f8c1e881dc0906c2b79d38d3b71d9db37"
 
   url "https://download.springsource.com/release/STS#{version.major}/#{version.csv.first}.RELEASE/dist/e#{version.csv.second.major_minor}/spring-tool-suite-#{version.major}-#{version.csv.first}.RELEASE-e#{version.csv.second}-macosx.cocoa.#{arch}.dmg",
       verified: "download.springsource.com/release/"
