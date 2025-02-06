f0a600cdb728dd502ae44578bc209918b3063d06
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-02-06 20:05:15 +0900

    textual 7.2.4,250205.21,3b1c05cfe

--- Casks/t/textual.rb
+++ Casks/t/textual.rb
@@ -1,20 +1,20 @@
 cask "textual" do
-  version "7.2.4,7dca3d371"
-  sha256 "afbd4adf3fc25a267d89a02b8f5d4c9e2ecd00d2e15261eca2da5061ea21e521"
+  version "7.2.4,250205.21,3b1c05cfe"
+  sha256 "627f43539553290b96582c6bd4ef02451e00178689a8dcf03fd88acf0021ff28"
 
-  url "https://cached.codeux.com/textual/downloads/builds/stable/Textual-#{version.csv.second}/universal/Textual.zip"
+  url "https://cached.codeux.com/textual/downloads/builds/stable/Textual-#{version.csv.third}/universal/Textual.zip"
   name "Textual"
   desc "Application for interacting with Internet Relay Chat (IRC) chatrooms"
   homepage "https://www.codeux.com/textual/"
 
   livecheck do
-    url "https://textual-updates-backend.codeux.com/sparkle/feeds/v#{version.csv.first.major}/feed-one.xml"
+    url "https://updates.textualapp.com/sparkle/feeds/v#{version.csv.first.major}/feed-one.xml"
     regex(%r{/Textual[._-]v?(\h+)/universal/Textual\.zip}i)
     strategy :sparkle do |item, regex|
       match = item.url.match(regex)
       next if match.blank?
 
-      "#{item.short_version},#{match[1]}"
+      "#{item.short_version},#{item.version},#{match[1]}"
     end
   end
 
