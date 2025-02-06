1ddb58496659531baa287e9f717ccd94566ba543
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-02-06 19:43:38 +0900

    android-studio-preview@beta 2024.3.1.11

--- Casks/a/android-studio-preview@beta.rb
+++ Casks/a/android-studio-preview@beta.rb
@@ -1,9 +1,9 @@
 cask "android-studio-preview@beta" do
   arch arm: "mac_arm", intel: "mac"
 
-  version "2024.3.1.10"
-  sha256 arm:   "50e2c5443526f4fd61bdf36f94d5fc56b1f878be733d76932a3454e197e2f429",
-         intel: "3ccbc8d3268a445681c13daf07a13344a110e5631f548c62a462d669c64b77e4"
+  version "2024.3.1.11"
+  sha256 arm:   "e3d47299cbcc550317293a3983edb55c2a7b29b41bda36f4dd49a1cbd5b3833f",
+         intel: "c7bb4e52fb5177487e513b38f504b453a9baa6abe901023328539b746bdd9466"
 
   url "https://redirector.gvt1.com/edgedl/android/studio/install/#{version}/android-studio-#{version}-#{arch}.dmg",
       verified: "redirector.gvt1.com/edgedl/android/studio/install/"
@@ -19,7 +19,7 @@ cask "android-studio-preview@beta" do
   auto_updates true
   depends_on macos: ">= :high_sierra"
 
-  app "Android Studio Preview.app", target: "Android Studio Preview Beta.app"
+  app "Android Studio.app", target: "Android Studio Preview Beta.app"
   binary "#{appdir}/Android Studio Preview Beta.app/Contents/MacOS/studio", target: "studio-beta"
 
   zap trash: [
