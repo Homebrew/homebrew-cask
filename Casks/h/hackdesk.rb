c5512db52b46448bc8274baafe85ba67eb19182f
Michael Wang <michael19920327@gmail.com>
2025-02-07 02:50:32 +0800

    hackdesk 0.0.7 (new cask)

---
+++ Casks/h/hackdesk.rb
@@ -0,0 +1,30 @@
+cask "hackdesk" do
+  arch arm: "aarch64", intel: "x64"
+
+  version "0.0.7"
+  sha256 arm:   "c44c4d534e9d04c92ed8127a64cfd97dd07ce0d46746ca4c5454d3a0af68c15e",
+         intel: "35bbc531d0fb25f2e9832dac586ae488f0bcb398a91716a8e3f1f8022fabca6c"
+
+  url "https://github.com/EastSun5566/hackdesk/releases/download/hackdesk-v#{version}/HackDesk_#{version}_#{arch}.dmg",
+      verified: "github.com/EastSun5566/hackdesk/"
+  name "HackDesk"
+  desc "A hackable HackMD desktop application"
+  homepage "https://hackdesk.eastsun.me"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+
+  app "HackDesk.app"
+
+  zap trash: [
+    "~/.hackdesk",
+    "~/Library/Application Support/eastsun5566.github.hackdesk",
+    "~/Library/Preferences/eastsun5566.github.hackdesk.plist",
+    "~/Library/Saved Application State/eastsun5566.github.hackdesk.savedState",
+    "~/Library/Caches/eastsun5566.github.hackdesk"
+  ]
+end
