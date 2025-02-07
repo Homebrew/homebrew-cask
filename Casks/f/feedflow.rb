44fa1774b3158cf8dbee07fa10ea6649b4d5f118
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-07 15:39:07 +0000

    feedflow 1.0.92

--- Casks/f/feedflow.rb
+++ Casks/f/feedflow.rb
@@ -1,6 +1,6 @@
 cask "feedflow" do
-  version "1.0.88"
-  sha256 "4c89634bddff0eb7cebd50fe7dcacce4b22c21b54fd53955613f0a7fd534e508"
+  version "1.0.92"
+  sha256 "96684a396892ff8e07491bd8d41edc08238aee8526bde7612549ae171c0edb87"
 
   url "https://github.com/prof18/feed-flow/releases/download/#{version}-desktop/FeedFlow-#{version}.dmg",
       verified: "github.com/prof18/feed-flow/"
