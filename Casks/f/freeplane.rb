341cd439b29105148f04080661b123a7f13968aa
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-02-07 13:45:53 +0900

    freeplane 1.12.9

--- Casks/f/freeplane.rb
+++ Casks/f/freeplane.rb
@@ -1,9 +1,9 @@
 cask "freeplane" do
   arch arm: "apple", intel: "intel"
 
-  version "1.12.8"
-  sha256 arm:   "48d7dffa97b8f6905d967d4556d50f21cbde247cff5b744148e3fb204cb162b0",
-         intel: "75d7454de1456f638f5c95d284524e67d4eb818626edfb9aebe204bfbf22f2e9"
+  version "1.12.9"
+  sha256 arm:   "18edf2844f92fa4feccdc4b0c5cfe131db4d63794da47a453c45c5bfa34afb69",
+         intel: "bf502823fd6746c6fcd7b4f9d2c59519b2676288ffc59464b1df59ba44202525"
 
   url "https://downloads.sourceforge.net/freeplane/Freeplane-#{version}-#{arch}.dmg",
       verified: "downloads.sourceforge.net/freeplane/"
