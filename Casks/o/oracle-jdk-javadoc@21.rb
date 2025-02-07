01eb331efa41de2a18327800dd10a15c83c5c760
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-02-07 17:03:09 +0900

    oracle-jdk-javadoc@21 21.0.6,8,05784440095547069d067afba737ba16 (new cask)

---
+++ Casks/o/oracle-jdk-javadoc@21.rb
@@ -0,0 +1,30 @@
+cask "oracle-jdk-javadoc@21" do
+  version "21.0.6,8,05784440095547069d067afba737ba16"
+  sha256 "fad5e36bf6dbc09eaa6791de42622374721e93d723844e0533ddca80314f5690"
+
+  url "https://download.oracle.com/otn_software/java/jdk/#{version.csv.first}+#{version.csv.second}/#{version.csv.third}/jdk-#{version.csv.first}_doc-all.zip",
+      cookies: {
+        "oraclelicense" => "accept-securebackup-cookie",
+      }
+  name "Oracle Java Standard Edition Development Kit Documentation"
+  desc "Documentation for the Oracle JDK"
+  homepage "https://www.oracle.com/java/technologies/downloads/"
+
+  livecheck do
+    url "https://www.oracle.com/java/technologies/javase-jdk#{version.major}-doc-downloads.html"
+    regex(%r{/(\d+(?:\.\d+)*)(?:\+|%2B)(\d+(?:\.\d+)*)/(\h+)/jdk[._-]v?(\d+(?:\.\d+)*)_doc-all\.zip}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[0]},#{match[1]},#{match[2]}" }
+    end
+  end
+
+  artifact "docs", target: "/Library/Java/JavaVirtualMachines/jdk-#{version.major}.jdk/Contents/Home/docs"
+
+  uninstall rmdir: "/Library/Java/JavaVirtualMachines/jdk-#{version.major}.jdk"
+
+  # No zap stanza required
+
+  caveats do
+    license "https://download.oracle.com/otndocs/jcp/java_se-#{version.major}-final-spec/license.html"
+  end
+end
