f54fc055d1c674379894c68e2c74b33cdd1622a7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-08 09:57:47 +0000

    zen-browser 1.7.6b

--- Casks/z/zen-browser.rb
+++ Casks/z/zen-browser.rb
@@ -1,6 +1,6 @@
 cask "zen-browser" do
-  version "1.7.5b"
-  sha256 "1c1e4ba2808c787827e35ea7410b2a7e9d50ad46068bc7bbd6a4ad8389a5bee8"
+  version "1.7.6b"
+  sha256 "b4ef728a804fdc7060b243334377ca85c8c02bf5e967901f7ebd8aafaf46c731"
 
   url "https://github.com/zen-browser/desktop/releases/download/#{version}/zen.macos-universal.dmg",
       verified: "github.com/zen-browser/desktop/"
