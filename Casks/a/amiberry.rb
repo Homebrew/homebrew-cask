cf210e531d69fb5bba9f001a7684686c2c20cae7
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-02-09 16:06:48 +0900

    amiberry 7.0.4

--- Casks/a/amiberry.rb
+++ Casks/a/amiberry.rb
@@ -1,9 +1,9 @@
 cask "amiberry" do
   arch arm: "apple-silicon", intel: "x86_64"
 
-  version "7.0.2"
-  sha256 arm:   "9a854f92fa6b55c82c285b6ebf7c6b5027512514537bbceb00000b5c26cb4988",
-         intel: "1a948a0259996f9cdc8e62f61b38cf2dcb93ae4f0404c351c2e3b66760035f88"
+  version "7.0.4"
+  sha256 arm:   "5e2b36f2845d7bae9c1fa3cc638d9fc4bc7dcfff285530ad3dddd6ca8898340d",
+         intel: "c930943474244d66979e8a36733809eaf0cf990003d9d72271f358de3d872891"
 
   url "https://github.com/BlitterStudio/amiberry/releases/download/v#{version}/Amiberry-v#{version}-macOS-#{arch}.zip",
       verified: "github.com/BlitterStudio/amiberry/"
