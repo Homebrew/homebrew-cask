bb04ec26ee8a25cbec311367e90d9ed546428d71
Klaus Hipp <khipp@users.noreply.github.com>
2025-02-09 09:49:08 +0100

    scribus: update livecheck

--- Casks/s/scribus.rb
+++ Casks/s/scribus.rb
@@ -1,11 +1,16 @@
 cask "scribus" do
-  arch arm: "_1-arm64"
+  arch arm: "-arm64"
 
-  version "1.6.3"
-  sha256 arm:   "7b046918ad07cf11582436eb064d53b15ca41e5c0e003d4f73107e959d975586",
-         intel: "7fc542f8d36b8f8e4ffc345f32e1b34be510fba1cda5d34cddf8876f1b6d7489"
+  on_arm do
+    version "1.6.3,1.6.3_1"
+    sha256 "7b046918ad07cf11582436eb064d53b15ca41e5c0e003d4f73107e959d975586"
+  end
+  on_intel do
+    version "1.6.3"
+    sha256 "7fc542f8d36b8f8e4ffc345f32e1b34be510fba1cda5d34cddf8876f1b6d7489"
+  end
 
-  url "https://downloads.sourceforge.net/scribus/scribus/#{version}/scribus-#{version}#{arch}.dmg",
+  url "https://downloads.sourceforge.net/scribus/scribus/#{version.csv.first}/scribus-#{version.csv.second || version.csv.first}#{arch}.dmg",
       verified: "sourceforge.net/scribus/"
   name "Scribus"
   desc "Free and open-source page layout program"
@@ -13,7 +18,15 @@ cask "scribus" do
 
   livecheck do
     url "https://sourceforge.net/projects/scribus/rss?path=/scribus"
-    regex(%r{url=.*?/scribus[._-]v?(\d+(?:\.\d+)+)(?:#{arch})?\.(?:dmg|pkg)}i)
+    regex(%r{url=.*?/v?(\d+(?:\.\d+)+)/scribus[._-]v?(\d+(?:[._]\d+)+)(?:#{arch})?\.(?:dmg|pkg)}i)
+    strategy :sourceforge do |page, regex|
+      match = page.match(regex)
+      next if match.blank?
+
+      next match[1] if match[1] == match[2]
+
+      "#{match[1]},#{match[2]}"
+    end
   end
 
   app "Scribus.app"
