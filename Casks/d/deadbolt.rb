d61752d3725b84b8b558b729472eae7c1fc109ec
Daeho Ro <lamanus@outlook.kr>
2025-02-09 21:56:53 +0900

    deadbolt 2.0.0

--- Casks/d/deadbolt.rb
+++ Casks/d/deadbolt.rb
@@ -1,8 +1,11 @@
 cask "deadbolt" do
-  version "1.0.0"
-  sha256 "df5b9b0988f168946809f1db17711f589f62bc211a6dad724eac9b0844285370"
+  arch arm: "-arm64"
 
-  url "https://github.com/alichtman/deadbolt/releases/download/#{version}/Deadbolt-#{version}-mac.zip"
+  version "2.0.0"
+  sha256 arm:   "ad2d7780f7032018cb40241781f445c5fb1b4cf6adc481917735472ba9f2a8da",
+         intel: "3584529c35711c56d6f118ce06e76e7c14dafe8b71608aa25ee42ccf6e5dd633"
+
+  url "https://github.com/alichtman/deadbolt/releases/download/v#{version}/Deadbolt-#{version}#{arch}.dmg"
   name "Deadbolt"
   desc "File encryption tool"
   homepage "https://github.com/alichtman/deadbolt"
@@ -19,8 +22,4 @@ cask "deadbolt" do
     "~/Library/Preferences/org.alichtman.deadbolt.plist",
     "~/Library/Saved Application State/org.alichtman.deadbolt.savedState",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
