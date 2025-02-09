6081195dae9f4a9e4e857f102d88308abbd5c5a3
Daeho Ro <lamanus@outlook.kr>
2025-02-09 16:32:16 +0900

    dockfix: update homepage

--- Casks/d/dockfix.rb
+++ Casks/d/dockfix.rb
@@ -2,14 +2,14 @@ cask "dockfix" do
   version "3.5.1"
   sha256 :no_check
 
-  url "https://lubker.dk/dockfix/download.php"
+  url "https://www.dockfix.app/downloads/DockFix.dmg"
   name "DockFix"
   desc "Dock replacement"
-  homepage "https://dockfix.lubker.dk/"
+  homepage "https://www.dockfix.app/"
 
   livecheck do
-    url "https://dockfix.lubker.dk/changelog/"
-    regex(/<h\d[^>]*?>[^<]*?v?(\d+(?:\.\d+)+)[ <"]/i)
+    url "https://www.dockfix.app/changelog"
+    regex(/<h\d[^>]*>Version\s*<!--\s-->v?(\d+(?:\.\d+)+)[ <"]/i)
   end
 
   auto_updates true
