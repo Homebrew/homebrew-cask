6018f2e92a513e37eb0c77d7b88033f34d450211
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-02-09 13:44:25 +0900

    anythingllm: use sha256 :no_check

--- Casks/a/anythingllm.rb
+++ Casks/a/anythingllm.rb
@@ -2,8 +2,7 @@ cask "anythingllm" do
   arch arm: "-Silicon"
 
   version "1.7.3"
-  sha256 arm:   "c642ee866d21ec5d6f867c6287de54a32cd4cbfb90aa80e4eee72b4c2d18eca1",
-         intel: "c495348c176ba2ba7f8c790dd408be69e3375211294f63c95c8f37090449998a"
+  sha256 :no_check
 
   url "https://cdn.anythingllm.com/latest/AnythingLLMDesktop#{arch}.dmg",
       referer: "homebrew-cask"
