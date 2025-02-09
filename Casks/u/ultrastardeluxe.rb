e428ab22194b8f7b89ae82c6a696a8cec669b4f2
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-02-09 18:16:57 +0900

    ultrastardeluxe 2025.2.1

--- Casks/u/ultrastardeluxe.rb
+++ Casks/u/ultrastardeluxe.rb
@@ -1,9 +1,9 @@
 cask "ultrastardeluxe" do
   arch arm: "ARM", intel: "x86"
 
-  version "2025.2.0"
-  sha256 arm:   "fe9d83b3783ef8b9886a17cca9a1b52b091ef52cf3ec2d8e732e833d201ae7b0",
-         intel: "7162e10b2b27faee5e415d69928f140fe293ccba17d0917041ada2262986fb59"
+  version "2025.2.1"
+  sha256 arm:   "9b89eb8faa2b0eee900bc54865801ef6d7f2f2bd89ad2c86b03027a2ba72ac82",
+         intel: "c8302f7ef213067966383b6b785d28af192504811f8f0d15d3caf399c29e51e3"
 
   url "https://github.com/UltraStar-Deluxe/USDX/releases/download/v#{version}/UltraStarDeluxe-mac-#{arch}-#{version}.dmg",
       verified: "github.com/UltraStar-Deluxe/USDX/"
