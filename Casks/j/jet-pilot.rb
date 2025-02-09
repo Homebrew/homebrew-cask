1d194068dbb2ef314f85022cdfcfa233663259bf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-09 21:38:25 +0000

    jet-pilot 1.34.0

--- Casks/j/jet-pilot.rb
+++ Casks/j/jet-pilot.rb
@@ -1,9 +1,9 @@
 cask "jet-pilot" do
   arch arm: "aarch64", intel: "x64"
 
-  version "1.33.0"
-  sha256  arm:   "6f2c1ea91c3728660df02ee03d61ce5027dc480e722f13b46337feae76de30bf",
-          intel: "9a8bba082f87b9426d6e69e7d251d80ddcd54c74b05ecca0d61dd5a49ab05e43"
+  version "1.34.0"
+  sha256  arm:   "1588374c73026e639d6b74db70861be773dd65e5a32ec0993cc95e6c2c72820e",
+          intel: "ca0807b5a169b32d740667f335699605d96f966adfcfe4876c3d42a07e241fb2"
 
   url "https://github.com/unxsist/jet-pilot/releases/download/v#{version}/JET.Pilot_#{version}_#{arch}.dmg",
       verified: "github.com/unxsist/jet-pilot/"
