5e2406f3dbc0e1c53270ad7463d5be80f5084fdc
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-02-10 16:02:57 +0900

    ankerwork: use sha256 :no_check

--- Casks/a/ankerwork.rb
+++ Casks/a/ankerwork.rb
@@ -2,8 +2,7 @@ cask "ankerwork" do
   arch arm: "arm64", intel: "x64"
 
   version "3.0.4"
-  sha256 arm:   "d56a3b64f25daec7f4d4d41b42c14087e90244949ec520bfbd7defcb2faa143d",
-         intel: "3a99ddf50cc6594adf783a5f1b241fd8491295a2a719d61fec4ab1a1c59191d9"
+  sha256 :no_check
 
   url "https://ankerwork.s3.amazonaws.com/electron/AnkerWork-Setup-#{arch}.dmg",
       verified: "ankerwork.s3.amazonaws.com/electron/"
