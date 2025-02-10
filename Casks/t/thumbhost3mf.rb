03cc38b1603e71a21e8789bbff676dab57b88d9a
David Phillip Oster <DavidPhillipOster@gmail.com>
2025-02-09 19:44:00 -0800

    Removed unneeded livecheck, revised app name, description.

--- Casks/t/thumbhost3mf.rb
+++ Casks/t/thumbhost3mf.rb
@@ -3,14 +3,9 @@ cask "thumbhost3mf" do
   sha256 "76c6ad0cb2cefb2ef88efd96949afe0b8f03573871d0192146a6083df5d6e19e"
 
   url "https://github.com/DavidPhillipOster/ThumbHost3mf/releases/download/#{version}/ThumbHost3mf#{version}.zip"
-  name "thumbhost3mf"
-  desc "App makes the Finder displays thumbnails of some .gcode, .bgcode, and .3mf files"
+  name "ThumbHost3mf"
+  desc "Finder thumbnail provider for some .gcode, .bgcode and .3mf files"
   homepage "https://github.com/DavidPhillipOster/ThumbHost3mf/"
-
-  livecheck do
-    url "https://github.com/DavidPhillipOster/ThumbHost3mf/releases"
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
-  end
 
   depends_on macos: ">= :catalina"
 
