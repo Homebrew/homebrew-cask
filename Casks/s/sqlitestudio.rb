2bd4caa06ec4bc00c42b3b5fc8df1c8150bb42e2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-10 01:01:01 +0000

    sqlitestudio 3.4.16

--- Casks/s/sqlitestudio.rb
+++ Casks/s/sqlitestudio.rb
@@ -1,6 +1,6 @@
 cask "sqlitestudio" do
-  version "3.4.15"
-  sha256 "6b546bb03cf8d82ccd35f34c4633cb79b53adfe50dfd3c31d8a0c89bbf38de07"
+  version "3.4.16"
+  sha256 "879f6e3a6cfa76bce2d65348870b686fec6162a7bb22619ebd7a28670dc6df07"
 
   url "https://github.com/pawelsalawa/sqlitestudio/releases/download/#{version}/sqlitestudio-#{version}-macos-x64.dmg",
       verified: "github.com/pawelsalawa/sqlitestudio/releases/download/"
