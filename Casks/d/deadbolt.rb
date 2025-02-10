9e12729fd9650429bed19eff49a4aa64e4102bbf
bevanjkay <email@bevankay.me>
2025-02-10 16:58:01 +1100

    deadbolt 2.0.2

--- Casks/d/deadbolt.rb
+++ Casks/d/deadbolt.rb
@@ -1,9 +1,9 @@
 cask "deadbolt" do
   arch arm: "-arm64"
 
-  version "2.0.0"
-  sha256 arm:   "ad2d7780f7032018cb40241781f445c5fb1b4cf6adc481917735472ba9f2a8da",
-         intel: "3584529c35711c56d6f118ce06e76e7c14dafe8b71608aa25ee42ccf6e5dd633"
+  version "2.0.2"
+  sha256 arm:   "d5ede4239f0474124bfa11310938d5657d1fc6d550b32fabe1672506ca6e60a7",
+         intel: "7ef2584d67b102944da8ec823811488b6a4137f83a4ff8c30d50685e996385f7"
 
   url "https://github.com/alichtman/deadbolt/releases/download/v#{version}/Deadbolt-#{version}#{arch}.dmg"
   name "Deadbolt"
