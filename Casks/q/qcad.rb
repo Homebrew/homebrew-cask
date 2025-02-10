e133a602bd3297e78506747a9b201b5bea5f1475
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-10 16:19:47 +0000

    qcad 3.32.1

--- Casks/q/qcad.rb
+++ Casks/q/qcad.rb
@@ -2,12 +2,12 @@ cask "qcad" do
   arch arm: "-arm64"
 
   on_arm do
-    version "3.32.0,12.7-15-qt6"
-    sha256 "5b12924e27fb53a42720dd3a646a24f6cdaf19f767c048c7044ad20c89ac0933"
+    version "3.32.1,12.7-15-qt6"
+    sha256 "dde2deff3f520aed0ebed88f74c61125f65380f2096848fbfdf245740d661f12"
   end
   on_intel do
-    version "3.32.0,10.14-15"
-    sha256 "df882095365c10a3bc1b245bee74f124116bd201825d0b062efe228b2132ca0d"
+    version "3.32.1,10.14-15"
+    sha256 "85b47f86624b6d24d22b4bd3d718288a0c2972aec4b5571953ca670cb2333646"
   end
 
   url "https://www.qcad.org/archives/qcad/qcad-#{version.csv.first}-trial-macos-#{version.csv.second}#{arch}.dmg"
