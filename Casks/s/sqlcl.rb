ea6a98e16c6e8101781e148abe3c1e255f28c1f2
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-02-11 00:33:42 +0900

    sqlcl: use versioned url

--- Casks/s/sqlcl.rb
+++ Casks/s/sqlcl.rb
@@ -1,8 +1,8 @@
 cask "sqlcl" do
   version "24.3.2.330.1718"
-  sha256 :no_check
+  sha256 "efe702ed12787d7b4dc04a19bd3736a553e90a1417c61a765da7eb4cb9f2ea9c"
 
-  url "https://download.oracle.com/otn_software/java/sqldeveloper/sqlcl-latest.zip"
+  url "https://download.oracle.com/otn_software/java/sqldeveloper/sqlcl-#{version}.zip"
   name "sqlcl"
   desc "Oracle SQLcl is the modern command-line interface for the Oracle Database"
   homepage "https://www.oracle.com/sqlcl"
