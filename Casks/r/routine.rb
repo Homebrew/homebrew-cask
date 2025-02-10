6724e72056a0b3903d234eaef17fd31d5b60ec53
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-10 13:02:20 +0000

    routine 0.20.0

--- Casks/r/routine.rb
+++ Casks/r/routine.rb
@@ -1,9 +1,9 @@
 cask "routine" do
   arch arm: "-arm64"
 
-  version "0.19.11"
-  sha256 arm:   "050f3e2cf5fe4b11f7f29b8a1dd9cb54dbeef4af297e1e8764ccf3ea8aec7b4a",
-         intel: "52d7d191a78e721ca94b093c645f6c3c7b1aacf9429206bcc90c5d930ceef14c"
+  version "0.20.0"
+  sha256 arm:   "7a274c1b816384e6a7c0b1861f1ee5dfe406cc42f8d42a69bb701437e6858e05",
+         intel: "646564c3f57951d6e97ec5f5f970fa0941f41c6cfa52d95e97d5773f40702826"
 
   url "https://releases.routine.co/routine/osx#{arch}/Routine-#{version}.zip"
   name "Routine"
