22dfde9ce3e874cf7c37bbc4d487bf4e1fd15692
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-02-11 13:46:16 +0900

    freelens 0.1.4

--- Casks/f/freelens.rb
+++ Casks/f/freelens.rb
@@ -1,9 +1,9 @@
 cask "freelens" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.1.3"
-  sha256 arm:   "a60ab1700dae51eccfd2dc007fba2431a1f34d83e312630ff889074467b8c9fe",
-         intel: "56251b6496d2145b4b3fa5001c00866a66398bad65a149ce4dcc61fd6d6f073d"
+  version "0.1.4"
+  sha256 arm:   "72e7da728329d5f4f6fdad82c17725c1bb319f83d85817560b58d6f7b242c7dc",
+         intel: "cfa53a161033eb2cef18c2cd3d36fdc495354ec7856c7eea97dd93d83472fdeb"
 
   url "https://github.com/freelensapp/freelens/releases/download/v#{version}/Freelens-#{version}-macos-#{arch}.dmg",
       verified: "github.com/freelensapp/freelens/"
