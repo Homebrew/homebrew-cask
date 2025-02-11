748b4ee5977a0194fb88ad5996a72fba8ba8856d
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-02-12 08:51:11 +0900

    slab 1.6.0

--- Casks/s/slab.rb
+++ Casks/s/slab.rb
@@ -1,9 +1,9 @@
 cask "slab" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.5.3"
-  sha256 arm:   "f899111beaedba209aa11b64c3a52b6e3e8461bbc34722a919d8285f046ffe53",
-         intel: "d307cec07640568c5dd6eccfed3be64e84794b222c23b3f6d8b91ebacdff2b0f"
+  version "1.6.0"
+  sha256 arm:   "74381580bae8f750c35d977a3945d1255f8971f0ff05131bcfd65150c3135e50",
+         intel: "6bb4f3db1c0ea1c760e4d42ef262de94ff09aef2037c7e793697bd8f76cc0c8c"
 
   url "https://github.com/slab/desktop-releases/releases/download/v#{version}/Slab-#{version}-darwin-#{arch}.dmg",
       verified: "github.com/slab/desktop-releases/"
