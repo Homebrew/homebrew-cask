bff6b6b5a6dd22b8fa5b58d15feefad87aec6dff
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-11 18:40:55 +0000

    flashspace 1.1.17

--- Casks/f/flashspace.rb
+++ Casks/f/flashspace.rb
@@ -1,6 +1,6 @@
 cask "flashspace" do
-  version "1.0.16"
-  sha256 "33fb5556e991001b9d0e4ecde1731fc145ea22518fc2882345763f7eb9523e62"
+  version "1.1.17"
+  sha256 "c88fe13bd03b696d6b663f31019f69050e54650de6f6254145e2c747b165ace3"
 
   url "https://github.com/wojciech-kulik/FlashSpace/releases/download/v#{version}/FlashSpace.app.zip"
   name "FlashSpace"
