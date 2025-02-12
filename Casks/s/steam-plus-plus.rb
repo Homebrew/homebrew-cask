2963688676204a443bd8dcdb9a751640df4237b5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-12 19:27:22 +0000

    steam-plus-plus 3.0.0-rc.15

--- Casks/s/steam-plus-plus.rb
+++ Casks/s/steam-plus-plus.rb
@@ -1,6 +1,6 @@
 cask "steam-plus-plus" do
-  version "3.0.0-rc.14"
-  sha256 "c412db0a8a49ba9179c3398aed5d32d653d5f2d7feeb88150d7d15df6a18e2df"
+  version "3.0.0-rc.15"
+  sha256 "3266ce7761235f3ca5537068a376cfef8c0770d5d617a62c7e78b8db9c122b63"
 
   url "https://github.com/BeyondDimension/SteamTools/releases/download/#{version}/Steam++_v#{version}_macos.dmg",
       verified: "github.com/BeyondDimension/SteamTools/"
