6abba81b4a55ac3042a174507bceda9c43719ca6
eleanordoesntcode <166862894+eleanordoesntcode@users.noreply.github.com>
2025-02-12 19:27:21 +0100

    calmly-writer 2.0.56

--- Casks/c/calmly-writer.rb
+++ Casks/c/calmly-writer.rb
@@ -1,15 +1,15 @@
 cask "calmly-writer" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.0.55"
+  version "2.0.56"
 
   on_arm do
-    sha256 "9736bb13158eb9f76c1386778e8bfacc3aa4c732b1926d9bbb8bab7d8519ea2c"
+    sha256 "00f6b14dd1020d7b6235925f91fc4060c4db3f19cd20366b1f46fbd895a22491"
 
     url "https://www.calmlywriter.com/releases/Calmly%20Writer-#{version}-#{arch}.dmg"
   end
   on_intel do
-    sha256 "2b96979d4fcec839134197fa0406ee1107b19fef17721e854068074360a90a9e"
+    sha256 "947089ad8532f4f5ce63aff7e0d5ec68b026cabfbdc0163cafe078c5388df8d4"
 
     url "https://www.calmlywriter.com/releases/Calmly%20Writer-#{version}.dmg"
   end
