9edc6907e754817ab362e1a114b17f39c55bdde8
HuaDeity <hello@huadeity.com>
2025-02-12 17:07:51 +0800

    pinwhell 1.3.4 (new cask)

---
+++ Casks/p/pinwheel.rb
@@ -0,0 +1,29 @@
+cask "pinwheel" do
+  version "1.3.4"
+  sha256 "3135e12f0eec824e7479b23b14901e10b65296ebac23ec2863051dfca0e70f8f"
+
+  url "https://cdn.skala.app/pinwheel/versions/Pinwheel_#{version.dots_to_underscores}.zip",
+      verified: "cdn.skala.app/"
+  name "Pinwheel"
+  desc "Design systems and accessibility testing"
+  homepage "https://bjango.com/mac/pinwheel/"
+
+  livecheck do
+    url "https://updates.skala.app/pinwheel/pinwheel.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+  depends_on macos: ">= :sonoma"
+
+  app "Pinwheel.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.bjango.pinwheel.PinwheelPreviewer",
+    "~/Library/Caches/Pinwheel",
+    "~/Library/Containers/com.bjango.pinwheel.PinwheelPreviewer",
+    "~/Library/HTTPStorages/com.bjango.pinwheel",
+    "~/Library/Preferences/com.bjango.pinwheel.plist",
+    "~/Library/Saved Application State/com.bjango.pinwheel.savedState",
+  ]
+end
