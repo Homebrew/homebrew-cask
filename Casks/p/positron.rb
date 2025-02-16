85680f1065e69074885efca5af4be686bc54f7bf
Klaus Hipp <khipp@users.noreply.github.com>
2025-02-16 20:32:01 +0100

    positron: update livecheck

--- Casks/p/positron.rb
+++ Casks/p/positron.rb
@@ -7,6 +7,11 @@ cask "positron" do
   desc "Data science IDE"
   homepage "https://github.com/posit-dev/positron"
 
+  livecheck do
+    url :url
+    regex(/^v?(\d+(?:\.\d+)+(?:-\d+)?)$/i)
+  end
+
   depends_on macos: ">= :catalina"
 
   app "Positron.app"
