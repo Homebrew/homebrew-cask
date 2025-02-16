6675b49c33fb44f03818d4e757afdca19807717b
souhaiebtar <tarhounisouhaieb@gmail.com>
2025-02-16 01:21:16 +0100

    fix syntax

--- Casks/l/lite-xl.rb
+++ Casks/l/lite-xl.rb
@@ -5,8 +5,8 @@ cask "lite-xl" do
   sha256 arm:   "e6a16c5e0013858fcc58a908b001cca2178a76cd998433130e8831410d917a65",
          intel: "df49f0653600b7a132e5d5b3f1b65f7a5eb361b2f6224ba4fbe5908c93e6b11e"
 
-  url "https://github.com/lite-xl/lite-xl/releases/download/v#{version}/lite-xl-v#{version}-macos-#{arch}.dmg"
-  verified "github.com/lite-xl/lite-xl/"
+  url "https://github.com/lite-xl/lite-xl/releases/download/v#{version}/lite-xl-v#{version}-macos-#{arch}.dmg",
+      verified: "github.com/lite-xl/lite-xl/"
   name "Lite XL"
   desc "Lightweight text editor written in Lua, adapted from lite"
   homepage "https://lite-xl.com/"
