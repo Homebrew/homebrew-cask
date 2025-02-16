765d257ceac3fc2dcabb7b5a6b51bb39acaeef5d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-16 00:14:40 +0000

    font-monomakh: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-m/font-monomakh.rb
+++ Casks/font/font-m/font-monomakh.rb
@@ -2,9 +2,10 @@ cask "font-monomakh" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/monomakh/Monomakh-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/monomakh/Monomakh-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Monomakh"
-  homepage "https://github.com/slavonic/Monomakh"
+  homepage "https://fonts.google.com/specimen/Monomakh"
 
   font "Monomakh-Regular.ttf"
 
