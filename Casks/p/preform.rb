bb75b5c1e178d8e6bc486e124e6064ca2a34ed48
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-17 15:54:21 +0000

    preform 3.44.2

--- Casks/p/preform.rb
+++ Casks/p/preform.rb
@@ -1,6 +1,6 @@
 cask "preform" do
-  version "3.44.1,478_89000"
-  sha256 "be8391b468d4eeeb66598861df2507599d519441509a94ff78d0564a2eba1118"
+  version "3.44.2,480_89634"
+  sha256 "f23a85337d530085c89fd2cb28d05f87b311615303a7227fa85871b0980644b5"
 
   url "https://downloads.formlabs.com/PreForm/Release/#{version.csv.first}/PreForm_mac_#{version.csv.first}_release_releaser_#{version.csv.second}.dmg"
   name "PreForm"
