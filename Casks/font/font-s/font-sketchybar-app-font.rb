9aedd2348d8bbfd208532360c5aa67847aac6edb
bevanjkay <email@bevankay.me>
2025-02-17 21:38:15 +1100

    font-sketchybar-app-font 2.0.32

--- Casks/font/font-s/font-sketchybar-app-font.rb
+++ Casks/font/font-s/font-sketchybar-app-font.rb
@@ -1,6 +1,6 @@
 cask "font-sketchybar-app-font" do
-  version "2.0.31"
-  sha256 "e727626cf975b8e2217de6ecadac469ad13b5011bf6c6546a25600ee351284b5"
+  version "2.0.32"
+  sha256 "6119360368bb9862b89f5341d9ceb6112ee6b81a50c336a30c64167f77f9442c"
 
   url "https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v#{version}/sketchybar-app-font.ttf"
   name "sketchybar-app-font"
