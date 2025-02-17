e3302bed701926ed3cdbf621b612e4e0ab024221
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-02-17 14:56:38 +0900

    tidgi 0.12.0-fix

--- Casks/t/tidgi.rb
+++ Casks/t/tidgi.rb
@@ -1,11 +1,11 @@
 cask "tidgi" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.12.0"
-  sha256 arm:   "b2713f2d9e915003d261a5ca10622c4cfaa39a26a07662232f236100b42a0314",
-         intel: "1287684d30963d81f8986ff86f55e100c90dbef849972102b16edbcecd90f76b"
+  version "0.12.0-fix"
+  sha256 arm:   "ea93da7079d78518430b465f8074ee8ab1625db67ec7db8757abef5be206b847",
+         intel: "62a897f56285044da3ea0d0089dd7d446219355b41b60f7b14ed1ba9606b33f3"
 
-  url "https://github.com/tiddly-gittly/TidGi-Desktop/releases/download/v#{version}/TidGi-darwin-#{arch}-#{version}.zip"
+  url "https://github.com/tiddly-gittly/TidGi-Desktop/releases/download/v#{version}/TidGi-darwin-#{arch}-#{version.split("-").first}.zip"
   name "TidGi"
   desc "Personal knowledge-base app"
   homepage "https://github.com/tiddly-gittly/TidGi-Desktop"
