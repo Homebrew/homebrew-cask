a92113940cef7acfe9e3f46692a780bb4162242c
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-02-18 16:35:03 +0900

    osp-tracker 6.3.0

--- Casks/o/osp-tracker.rb
+++ Casks/o/osp-tracker.rb
@@ -1,28 +1,33 @@
 cask "osp-tracker" do
-  version "6.2.0"
-  sha256 "04a3041b0a628b8241aaa8f3a40d3545927c017f835a955827397c0352bf6ceb"
+  version "6.3.0"
+  sha256 "375eeaf93a9cfb437e54a5063d5722463dc8aa9818648f5fdc2344c797c45ce9"
 
-  url "https://physlets.org/tracker/installers/download.php?file=Tracker-#{version}-osx-installer.dmg"
+  url "https://physlets.org/tracker/installers/Tracker-#{version}-osx-installer.dmg",
+      verified: "physlets.org/tracker/installers/"
   name "Tracker"
   desc "Video analysis and modelling tool for physics education"
-  homepage "https://physlets.org/tracker/"
+  homepage "https://opensourcephysics.github.io/tracker-website/"
 
   livecheck do
     url :homepage
-    regex(/href=.*?file=Tracker[._-]v?(\d+(?:\.\d+)+)-osx-installer\.dmg/i)
+    regex(/href=.*?Tracker[._-]v?(\d+(?:\.\d+)+)[._-]osx[._-]installer\.dmg/i)
   end
 
   installer script: {
-    executable: "Tracker-#{version}-osx-installer.app/Contents/MacOS/installbuilder.sh",
+    executable: "Tracker-#{version}-osx-installer.app/Contents/MacOS/osx-x86_64",
     args:       ["--mode", "unattended"],
     sudo:       true,
   }
 
   uninstall script: {
-    executable: "/usr/local/tracker/uninstall_Tracker.app/Contents/MacOS/installbuilder.sh",
+    executable: "/usr/local/tracker/uninstall_Tracker.app/Contents/MacOS/osx-x86_64",
     args:       ["--mode", "unattended"],
     sudo:       true,
   }
 
   zap trash: "~/Library/Saved Application State/org.opensourcephysics.cabrillo.tracker.savedState"
+
+  caveats do
+    requires_rosetta
+  end
 end
