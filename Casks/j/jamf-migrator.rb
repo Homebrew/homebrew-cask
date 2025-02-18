433eb86813fa1ede39d509856f07ab894bb75a4e
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-02-18 15:13:15 +0900

    jamf-migrator 8.0.0

--- Casks/j/jamf-migrator.rb
+++ Casks/j/jamf-migrator.rb
@@ -1,8 +1,8 @@
 cask "jamf-migrator" do
-  version "7.4.2"
-  sha256 "b66344e420810342849ae8509dc8bf16a0df5a4d7d5759ea6ecad9a030699988"
+  version "8.0.0"
+  sha256 "68943248106a3b0c737e1d3bac3d3ba24fb7817cc4e54678d0f90783c6f0b504"
 
-  url "https://github.com/jamf/Replicator/releases/download/v#{version}/jamf-migrator.zip"
+  url "https://github.com/jamf/Replicator/releases/download/v#{version}/Replicator.zip"
   name "JamfMigrator"
   desc "Tool to migrate data granularly between Jamf Pro servers"
   homepage "https://github.com/jamf/Replicator"
@@ -14,7 +14,7 @@ cask "jamf-migrator" do
 
   depends_on macos: ">= :catalina"
 
-  app "jamf-migrator.app"
+  app "Replicator.app"
 
   zap trash: [
     "~/Library/Application Scripts/com.jamf.jamf-migrator",
