125fea6c0c85f39e33b6222ec33199c606121c33
Klaus Hipp <khipp@users.noreply.github.com>
2025-02-19 22:21:09 +0100

    font-intel-one-mono: update name

--- Casks/font/font-i/font-intel-one-mono.rb
+++ Casks/font/font-i/font-intel-one-mono.rb
@@ -3,7 +3,7 @@ cask "font-intel-one-mono" do
   sha256 "74ef8ee667403c760745bc12fc5e2cb1684544194fad3d5340919c173a8227fc"
 
   url "https://github.com/intel/intel-one-mono/releases/download/V#{version}/otf.zip"
-  name "font-intel-one-mono"
+  name "Intel One Mono"
   homepage "https://github.com/intel/intel-one-mono"
 
   font "otf/IntelOneMono-Bold.otf"
