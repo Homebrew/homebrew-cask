2908549f50bb47dcda9bd158cff33a90348cca75
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-19 12:45:46 +0000

    affinity-designer 2.6.0

--- Casks/a/affinity-designer.rb
+++ Casks/a/affinity-designer.rb
@@ -1,6 +1,6 @@
 cask "affinity-designer" do
-  version "2.5.7,2948"
-  sha256 "a31b2b884230ae00f635b2a904803762b18a4191df2bdf0847753d875c1df1ec"
+  version "2.6.0,3134"
+  sha256 "4ba7ff81a12b0a54c318d9e393c86b42932c25ad41bb0cdcb4481bce881985ec"
 
   url "https://affinity-update.s3.amazonaws.com/mac2/retail/Affinity%20Designer%20#{version.csv.first.major}%20Affinity%20Store%20#{version.csv.second}.zip",
       verified: "affinity-update.s3.amazonaws.com/"
