a764e282e546b5bdb5119b6d8962d8b94e3da62f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-19 01:06:48 +0000

    readest 0.9.13

--- Casks/r/readest.rb
+++ Casks/r/readest.rb
@@ -1,6 +1,6 @@
 cask "readest" do
-  version "0.9.11"
-  sha256 "d46fa7244325dfca2f236c571fa08e4b2a59e35fd1c96f575d8b7bbec6a5fa4a"
+  version "0.9.13"
+  sha256 "e72d2cd0f43ef86834a0ef06aa67b1e79e5bb3c08a64943034aae08cb5fe8852"
 
   url "https://github.com/readest/readest/releases/download/v#{version}/Readest_#{version}_universal.dmg",
       verified: "github.com/readest/readest/"
