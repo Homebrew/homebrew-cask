0a73433581847dac7eaf0376b9643437190dabd7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-20 18:39:01 +0000

    bitwig-studio 5.3.1

--- Casks/b/bitwig-studio.rb
+++ Casks/b/bitwig-studio.rb
@@ -1,6 +1,6 @@
 cask "bitwig-studio" do
-  version "5.3"
-  sha256 "c8f7f64a1c8e63c6cdeaf33cb6a5a6afecb87fc4b681451b70dd485b185780f1"
+  version "5.3.1"
+  sha256 "d4642a9d362ad97a85194b6e6a1c8cb06c8410e826e55bfe490f4ebef40b5b25"
 
   url "https://www.bitwig.com/dl/Bitwig%20Studio/#{version}/installer_mac"
   name "Bitwig Studio"
