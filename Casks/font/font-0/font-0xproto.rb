6bfdda45e2dc09b0bdb8fc73954a79500e2aa241
ellie is not coding in the slightest <166862894+eleanordoesntcode@users.noreply.github.com>
2025-02-21 14:03:49 +0100

    font-0xproto 2.202 Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/font/font-0/font-0xproto.rb
+++ Casks/font/font-0/font-0xproto.rb
@@ -1,14 +1,14 @@
 cask "font-0xproto" do
-  version "2.201"
-  sha256 "cc383e1562085438d026ef6c3ad678ca0a12d8349013485adce9791890bf8821"
+  version "2.202"
+  sha256 "4247363c00fafc5343d47e3a8a45e6dac7416019dabbf614b6aa24a5af056c1d"
 
   url "https://github.com/0xType/0xProto/releases/download/#{version}/0xProto_#{version.dots_to_underscores}.zip"
   name "0xProto"
   homepage "https://github.com/0xType/0xProto"
 
-  font "0xProto-Bold.otf"
-  font "0xProto-Regular.otf"
-  font "0xProto-Italic.otf"
+  font "fonts/0xProto-Bold.otf"
+  font "fonts/0xProto-Regular.otf"
+  font "fonts/0xProto-Italic.otf"
 
   # No zap stanza required
 end
