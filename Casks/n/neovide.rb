1529dc1d300649dbf10d3a39e2a2d0cff4f5dec4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-23 06:46:56 +0000

    neovide 0.14.1

--- Casks/n/neovide.rb
+++ Casks/n/neovide.rb
@@ -1,9 +1,9 @@
 cask "neovide" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.14.0"
-  sha256 arm:   "d076f6d2791b984f0d1cee205722903212fca37572ed1a87be5a669c501c7b42",
-         intel: "174beaa3695361e091653eb8b4a095d0e113b172f5aac9843d36ed0b06ed4826"
+  version "0.14.1"
+  sha256 arm:   "55e4cdff19f96903635c28b7521a9d504ad4406af4c99041be7506af418072fc",
+         intel: "d553ccc835382f31f2167e2fae4a812489c7b8ca36758b4f650d04fba3747217"
 
   url "https://github.com/neovide/neovide/releases/download/#{version}/Neovide-#{arch}-apple-darwin.dmg"
   name "Neovide"
