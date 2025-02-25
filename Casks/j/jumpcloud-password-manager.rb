f6a2c9394aa25b10ff9e6bac50addcbf532cec6d
Mike McQuaid <mike@mikemcquaid.com>
2025-02-25 15:47:10 +0000

    Apply suggestions from code review
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/j/jumpcloud-password-manager.rb
+++ Casks/j/jumpcloud-password-manager.rb
@@ -1,11 +1,16 @@
 cask "jumpcloud-password-manager" do
   version "3.0.149"
-  sha256 :no_check
+  sha256 "cff8776dc0396ebd32d2de53f0b364e028f2ffcd4a88645f6eb2d6701950cbcb"
 
-  url "https://cdn.pwm.jumpcloud.com/DA/release/JumpCloud-Password-Manager-latest.dmg"
-  name "jumpcloud-password-manager"
+  url "https://cdn.pwm.jumpcloud.com/DA/release/JumpCloud-Password-Manager-#{version}.dmg"
+  name "JumpCloud Password Manager"
   desc "Password management tool that provides authentication, sharing and credentials"
   homepage "https://cdn.pwm.jumpcloud.com/web/download.html#desktop"
+
+  livecheck do
+    url "https://cdn.pwm.jumpcloud.com/DA/release/latest-mac.yml"
+    strategy :electron_builder
+  end
 
   auto_updates true
   depends_on macos: ">= :catalina"
