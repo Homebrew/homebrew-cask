8973f2ec92f0893df56f18d94c3dfc67c541abf2
Salvatore Testa <sal@sal.dev>
2025-02-24 20:51:21 -0800

    block-goose: update homepage
    @khipp pointed out that this would be a more apt homepage.

--- Casks/b/block-goose.rb
+++ Casks/b/block-goose.rb
@@ -2,10 +2,11 @@ cask "block-goose" do
   version "1.0.9"
   sha256 "2660d8cbc2352e3de889c5a80fec8d24181de2b18e03bec03feff57532994c3d"
 
-  url "https://github.com/block/goose/releases/download/v#{version}/Goose.zip"
+  url "https://github.com/block/goose/releases/download/v#{version}/Goose.zip",
+      verified: "github.com/block/goose/"
   name "Goose"
   desc "Open source, extensible AI agent that goes beyond code suggestions"
-  homepage "https://github.com/block/goose"
+  homepage "https://block.github.io/goose/"
 
   livecheck do
     url :url
