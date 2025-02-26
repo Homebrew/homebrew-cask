13c4daa63c13bfbc304ccf3b1b2911e6e6bf720f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-26 22:00:36 +0000

    ontime 3.13.0

--- Casks/o/ontime.rb
+++ Casks/o/ontime.rb
@@ -1,9 +1,9 @@
 cask "ontime" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.12.0"
-  sha256 arm:   "a48830f9a61a3eb5ffd9b06c34e08cd65da301e84482fddb47781069e2a2fc1b",
-         intel: "97b0ca0cc16f6ebfe8a2b7ebb04136d772634827bc90e9ff852279d852312f70"
+  version "3.13.0"
+  sha256 arm:   "fedea0f8e22cd29192f7d96402c839d9921376663e6e0adc583d00ce7c6208e0",
+         intel: "4614f446d884bec49f3fc6bd750107c06c97c66b3702638ace07f3ec6f5f0b22"
 
   url "https://github.com/cpvalente/ontime/releases/download/v#{version}/ontime-macOS-#{arch}.dmg",
       verified: "github.com/cpvalente/ontime/"
