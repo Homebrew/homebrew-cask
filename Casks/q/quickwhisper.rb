dc26da05f7c0cb1ebda453faa8ef5300da7519e8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-26 01:01:46 +0000

    quickwhisper 1.7.51

--- Casks/q/quickwhisper.rb
+++ Casks/q/quickwhisper.rb
@@ -1,6 +1,6 @@
 cask "quickwhisper" do
-  version "1.7.50"
-  sha256 "09b2c2beebccec8472a1fbeec6ec0c0d2c8b883aac78cd00a1b392f61a92f4f6"
+  version "1.7.51"
+  sha256 "cdb4c01e22739e09b01d4c1389a7fb4670fd46f7adbd869ca1b9d8e38daa8df9"
 
   url "https://quickwhisperapp.s3.us-west-002.backblazeb2.com/QuickWhisper_#{version}.zip",
       verified: "quickwhisperapp.s3.us-west-002.backblazeb2.com/"
