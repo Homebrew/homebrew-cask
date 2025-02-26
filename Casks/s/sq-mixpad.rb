c6a4ff0d9991162a1f23d10d9fc54d4024c9710f
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-02-26 10:59:22 +0900

    sq-mixpad 1.6

--- Casks/s/sq-mixpad.rb
+++ Casks/s/sq-mixpad.rb
@@ -1,8 +1,8 @@
 cask "sq-mixpad" do
-  version "1.5.1"
-  sha256 "99eaf53e1ca0b63cfde57d9d97a5d7628da20d317c4803b51ba2fdcbf225bfb2"
+  version "1.6.0,1.6,2025,02"
+  sha256 "5e3e4d86c016ea1de457c78e89e0c5352e2a56e81fed2b32c3fb29c81d562243"
 
-  url "https://www.allen-heath.com/content/uploads/2023/05/SQ-MixPad-#{version}.zip",
+  url "https://www.allen-heath.com/content/uploads/#{version.csv.third}/#{version.csv.fourth}/SQ-MixPad-#{version.csv.second}-Mac-Installer.zip",
       user_agent: :browser
   name "SQ MixPad"
   desc "Remote control for Allen & Heath SQ audio consoles"
@@ -10,12 +10,18 @@ cask "sq-mixpad" do
 
   livecheck do
     url :homepage
-    regex(/href=.*?SQ[._-]MixPad[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+    regex(%r{href=.*?/(\d+)/(\d+)/SQ[._-]MixPad[._-]v?(\d+(?:\.\d+)+)[._-]Mac[^>]+SQ\s*MixPad\s*v?(\d+(?:\.\d+)+)}i)
+    strategy :page_match do |page, regex|
+      match = page.match(regex)
+      next if match.blank?
+
+      "#{match[4]},#{match[3]},#{match[1]},#{match[2]}"
+    end
   end
 
   depends_on macos: ">= :sierra"
 
-  app "SQ MixPad #{version}.app"
+  app "SQ MixPad #{version.csv.first}.app"
 
   zap trash: "~/Library/Preferences/com.allen-heath.SQ-MixPad*.plist"
 
