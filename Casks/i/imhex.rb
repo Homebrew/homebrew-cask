5611094a3834785e5e7601702e863e5a9e3e4b15
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-26 21:45:39 +0000

    imhex 1.37.4

--- Casks/i/imhex.rb
+++ Casks/i/imhex.rb
@@ -1,9 +1,9 @@
 cask "imhex" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.37.3"
-  sha256 arm:   "ee5e0816cc5c35bc3952df3084f866ba7d2f2fe5a8bc8163860b91228a6df772",
-         intel: "1977a8c0afb1377f2f951b609e1734e0cf654452b2bc500386c42d9de3069c47"
+  version "1.37.4"
+  sha256 arm:   "de93acf0b1ae4c45812270061e7ad8b82e29e4b16fe15fc35a063e4050568d4e",
+         intel: "d84c88246ca98b2462e970ff517bbcc351bda481bd34ab2d6e1fe14ee088e359"
 
   url "https://github.com/WerWolv/ImHex/releases/download/v#{version}/imhex-#{version}-macOS-#{arch}.dmg",
       verified: "github.com/WerWolv/ImHex/"
