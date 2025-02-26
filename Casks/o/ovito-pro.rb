b02434b2bb4033e937d0727df2f6203cf3bd0a42
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-26 13:08:19 +0000

    ovito-pro 3.12.0

--- Casks/o/ovito-pro.rb
+++ Casks/o/ovito-pro.rb
@@ -1,9 +1,9 @@
 cask "ovito-pro" do
   arch arm: "arm64", intel: "intel"
 
-  version "3.11.3"
-  sha256 arm:   "600ecdde3da5005e555ba8415c0b032c164fabecb186240db13e06d71edcf0a7",
-         intel: "8c5d6130e29650fcfdb6c838616b266b389d86b47bb2a1d3a8507914081f0309"
+  version "3.12.0"
+  sha256 arm:   "fc52fd54cfa9e1febe751e83fb05d6a82f87e24b32dc6f165c77ce0f39adc763",
+         intel: "09c3a773fac3426b376065234c24d5ea0c7b9b58d77456212e6e14ceb9ca7888"
 
   url "https://www.ovito.org/download/master/ovito-pro-#{version}-macos-#{arch}.dmg"
   name "OVITO Pro"
