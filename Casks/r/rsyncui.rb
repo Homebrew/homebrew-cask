c040aa973aaf4bd031db3ada9ebb018b3ec29b63
Thomas Evensen <thomeven@gmail.com>
2025-02-26 07:22:09 +0100

    Update rsyncui.rb

--- Casks/r/rsyncui.rb
+++ Casks/r/rsyncui.rb
@@ -1,6 +1,6 @@
 cask "rsyncui" do
-  version "2.3.4"
-  sha256 "4c42b017b2b0ceac1672e79dd49e21b66c8227b05127bec479d05ca901f44336"
+  version "2.3.5"
+  sha256 "601f24a3367b9c17582cd67015f39d614961906738b11f9f7f4762f5a50a1a55"
 
   url "https://github.com/rsyncOSX/RsyncUI/releases/download/v#{version}/RsyncUI.#{version}.dmg"
   name "RsyncUI"
