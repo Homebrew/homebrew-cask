f07f4c74ffa64c1b8467329358f5bdf0d0d170a5
eleanordoesntcode <166862894+eleanordoesntcode@users.noreply.github.com>
2025-02-27 13:21:23 +0100

    rapidminer-studio 2025.0.1

--- Casks/r/rapidminer-studio.rb
+++ Casks/r/rapidminer-studio.rb
@@ -1,6 +1,6 @@
 cask "rapidminer-studio" do
-  version "2025.0.0"
-  sha256 "b0b309995c34491713ee04c488ac923f84759c698948ac138458edde09a81019"
+  version "2025.0.1"
+  sha256 "abc0d19a433d6c736bbb9981435bd95aadc3ceb5f7483e6f25f0ee3837e25775"
 
   url "https://releases.rapidminer.com/archive/rapidminer-studio/#{version}/ai-studio-osx-#{version}.dmg"
   name "RapidMiner Studio"
