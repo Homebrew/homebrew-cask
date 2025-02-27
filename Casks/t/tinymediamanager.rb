aed1db4056ff184c40a55186dbc9ebdff95381e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-27 19:02:25 +0000

    tinymediamanager 5.1.3

--- Casks/t/tinymediamanager.rb
+++ Casks/t/tinymediamanager.rb
@@ -1,9 +1,9 @@
 cask "tinymediamanager" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "5.1.2"
-  sha256 arm:   "be6158ef43427fbcba77d832aa711688535a4553c82e129c17906dece2e04cc3",
-         intel: "98307c118aadcb92286a61ca314abb3cf8f5a414d046d97e081a7cbb3a3f452f"
+  version "5.1.3"
+  sha256 arm:   "135edc92de7cdaf21bb21953fae98d4a16b1b284ba1298d27a076f06e8168fe8",
+         intel: "892523ca511da39ec54c101272a2f0abaccad2969c034a1bec38f4540f098233"
 
   url "https://release.tinymediamanager.org/v#{version.major}/dist/tinyMediaManager-#{version}-macos-#{arch}.dmg"
   name "tinyMediaManager"
