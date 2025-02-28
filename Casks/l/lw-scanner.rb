b74ab4f57eaf8c01a21152767b8aca6df466a807
eleanordoesntcode <166862894+eleanordoesntcode@users.noreply.github.com>
2025-02-28 01:49:17 +0100

    lw-scanner 0.27.2

--- Casks/l/lw-scanner.rb
+++ Casks/l/lw-scanner.rb
@@ -1,6 +1,6 @@
 cask "lw-scanner" do
-  version "0.27.1"
-  sha256 "b9df89e3005d70ceb1c8fd3ce2735852aa4dfb96555e1867b32b1590377e8ba8"
+  version "0.27.2"
+  sha256 "066429df3a1e40a03126e37418eb39bb3ec38236637499809959c7dd9cb542ab"
 
   url "https://github.com/lacework/lacework-vulnerability-scanner/releases/download/v#{version}/lw-scanner-darwin-amd64",
       verified: "github.com/lacework/lacework-vulnerability-scanner/"
