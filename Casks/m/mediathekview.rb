b8112714cb825f61f51507bac48452f17d0584af
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-02-28 18:13:38 +0900

    mediathekview: update minimum macOS

--- Casks/m/mediathekview.rb
+++ Casks/m/mediathekview.rb
@@ -15,6 +15,8 @@ cask "mediathekview" do
     regex(%r{href=.*?/MediathekView-(\d+(?:\.\d+)+)-#{arch}\.dmg}i)
   end
 
+  depends_on macos: ">= :catalina"
+
   app "MediathekView.app"
 
   zap trash: "~/Library/Caches/MediathekView"
