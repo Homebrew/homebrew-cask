944d7efe3ebd513cfd2198b799fb2b7d77cb86b0
bevanjkay <email@bevankay.me>
2025-03-01 19:53:36 +1100

    expandrive 7,2025-2-28,2025-02-28_at_18_09_55

--- Casks/e/expandrive.rb
+++ Casks/e/expandrive.rb
@@ -1,20 +1,21 @@
 cask "expandrive" do
-  version "7,2023.4.1"
-  sha256 "360ae7ed44fe1374073af3682e69fe5e83193f5546712b3ce62a0c460f41b8eb"
+  version "7,2025-2-28,2025-02-28_at_18_09_55"
+  sha256 "e01325e9c14386473931b4d7b3400451acc08bb9ff25f7999bb01912d692fd7d"
 
-  url "https://updates.expandrive.com/apps/expandrive#{version.csv.first}/v/#{version.csv.second.dots_to_hyphens}/update_download"
+  url "https://s3.amazonaws.com/expandrive/expandrive#{version.csv.first}/v#{version.csv.second}_published_#{version.csv.third}/ExpanDrive.dmg",
+      verified: "s3.amazonaws.com/expandrive/"
   name "ExpanDrive"
   desc "Network drive and browser for cloud storage"
   homepage "https://www.expandrive.com/apps/expandrive/"
 
   livecheck do
     url "https://updates.expandrive.com/apps/expandrive#{version.csv.first}/download_latest"
-    regex(%r{expandrive(\d+)/.*/ExpanDrive[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
+    regex(%r{expandrive(\d+)/v?(\d+(?:[.-]\d+)+)[._-]published[._-]([^/]+)/ExpanDrive\.dmg}i)
     strategy :header_match do |headers, regex|
       match = headers["location"]&.match(regex)
       next if match.blank?
 
-      "#{match[1]},#{match[2]}"
+      "#{match[1]},#{match[2]},#{match[3]}"
     end
   end
 
@@ -28,8 +29,4 @@ cask "expandrive" do
     "~/Library/Preferences/com.expandrive.ExpanDrive*.plist",
     "~/Library/Preferences/com.expandrive.ExpanDrive.helper.plist",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
