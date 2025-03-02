17e2e72605c4244d44d40a9c738dd17efe6b116a
Sam Ford <1584702+samford@users.noreply.github.com>
2025-03-01 19:38:27 -0500

    shearwater-cloud: update livecheck
    The existing `livecheck` block for `shearwater-cloud` is giving an
    `Unable to get versions` error, as the download page no longer links
    to files. Instead, upstream has updated the download buttons for
    macOS and Windows to point to an intermediary page that uses
    JavaScript to fetch a JSON file containing filenames and redirect to
    the relevant file. This updates the `livecheck` block to match the
    version information from the dmg filename in the JSON file.

--- Casks/s/shearwater-cloud.rb
+++ Casks/s/shearwater-cloud.rb
@@ -7,9 +7,18 @@ cask "shearwater-cloud" do
   desc "Review, edit and share dive log data"
   homepage "https://shearwater.com/"
 
+  # The macOS link on the download page points to a page that fetches a JSON
+  # file containing the file URLs and then uses JavaScript to redirect the
+  # browser to the file, so we match the version information from the JSON file.
   livecheck do
-    url "https://shearwater.com/pages/shearwater-cloud"
-    regex(/href=.*?ShearwaterCloudInstaller[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+    url "https://downloads.shearwater.com/cloud_reference.json"
+    regex(/ShearwaterCloudInstaller[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+    strategy :json do |json, regex|
+      match = json["swcloud_dmg"]&.match(regex)
+      next if match.blank?
+
+      match[1]
+    end
   end
 
   depends_on macos: ">= :big_sur"
