ce013951ae59d7208c8b67e7db821261756442b6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-03 15:50:30 +0000

    kunkun 0.1.33

--- Casks/k/kunkun.rb
+++ Casks/k/kunkun.rb
@@ -1,9 +1,9 @@
 cask "kunkun" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.1.32"
-  sha256 arm:   "f9318e2dd7e3c9988dbe55d243b884ac7d5cee371ebab3d83439696c8739deee",
-         intel: "190056e8cea9811efec80746bd3e6327f56e3f3a1cb0831193c6d958522a54cc"
+  version "0.1.33"
+  sha256 arm:   "589db17e81a4a1ff394c22088a7d9a6ab295cbae5c4f8ac7104083069b5a4cb1",
+         intel: "176db7fc7be160b6940a83defef5081da8c59e92d2598c6d7afa638f65a72e0c"
 
   url "https://download.kunkun.sh/Kunkun-v#{version}/kunkun_#{arch}.app.tar.gz"
   name "Kunkun"
