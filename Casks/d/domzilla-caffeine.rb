1c8413b1c7316651f626af4f881524ea325b56da
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-03-03 22:56:35 +0900

    domzilla-caffeine: remove on_system blocks

--- Casks/d/domzilla-caffeine.rb
+++ Casks/d/domzilla-caffeine.rb
@@ -1,35 +1,17 @@
 cask "domzilla-caffeine" do
-  on_monterey :or_older do
-    version "1.2.1"
-    sha256 "e96a8b401e3f044d30dc534d1c8aee70930203013cb11c506b83fc6a1d328b06"
-
-    livecheck do
-      skip "Legacy version"
-    end
-  end
-  on_ventura do
-    version "1.3.0"
-    sha256 "201f3de57b11e1faec3307f00fb81ecab8ed2c433e94010de88b1edc4edbd15f"
-
-    livecheck do
-      skip "Legacy version"
-    end
-  end
-  on_sonoma :or_newer do
-    version "1.5.1"
-    sha256 "04f032f9616acda0a2ad3dadeaa6d7e863d6f6b13664a7c0caf6ed8b4478c4e7"
-
-    livecheck do
-      url "https://dr-caffeine-mac.s3.amazonaws.com/appcast.xml"
-      strategy :sparkle, &:short_version
-    end
-  end
+  version "1.5.1"
+  sha256 "04f032f9616acda0a2ad3dadeaa6d7e863d6f6b13664a7c0caf6ed8b4478c4e7"
 
   url "https://dr-caffeine-mac.s3.amazonaws.com/Caffeine_#{version}.zip",
       verified: "dr-caffeine-mac.s3.amazonaws.com/"
   name "Caffeine"
   desc "Utility that prevents the system from going to sleep"
   homepage "https://www.caffeine-app.net/"
+
+  livecheck do
+    url "https://dr-caffeine-mac.s3.amazonaws.com/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
 
   conflicts_with cask: "caffeine"
   depends_on macos: ">= :big_sur"
