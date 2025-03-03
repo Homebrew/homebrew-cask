9dc93d8b469356e4d71ee4a1e2b991a66684889b
Yu Hao <jinyu121@gmail.com>
2025-03-03 17:56:52 +0800

    Create trae@cn.rb

---
+++ Casks/t/trae@cn.rb
@@ -0,0 +1,40 @@
+cask "trae@cn" do
+  arch arm: "arm64", intel: "x64"
+
+  version "1.0.8055"
+  sha256 arm:   "0ff40c8c3645c06e019b8942b34ac0f008e52e044a2316f5169444750ab6a7a8",
+         intel: "7d1e9977bfdeaebb0c9d76a0deb882cbd7022b082fbefe464e18c9392cc4b77e"
+
+  url "https://lf-cdn.trae.com.cn/obj/trae-com-cn/pkg/app/releases/stable/#{version}/darwin/Trae%20CN-darwin-#{arch}.dmg"
+  name "Trae CN"
+  desc "Adaptive AI IDE"
+  homepage "https://www.trae.com.cn/"
+
+  livecheck do
+    url "https://api.trae.ai/icube/api/v1/native/version/trae/cn/latest"
+    strategy :json do |json|
+      json.dig("data", "manifest", "darwin", "version")
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
+  app "Trae CN.app"
+
+  uninstall launchctl: "cn.trae.ShipIt",
+            quit:      "cn.trae.app"
+
+  zap trash: [
+    "~/.trae-cn",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/cn.trae.app.sfl*",
+    "~/Library/Application Support/Trae CN",
+    "~/Library/Caches/cn.trae.app",
+    "~/Library/Caches/cn.trae.ShipIt",
+    "~/Library/HTTPStorages/cn.trae.app",
+    "~/Library/Preferences/ByHost/cn.trae.ShipIt.*.plist",
+    "~/Library/Preferences/cn.trae.app.helper.plist",
+    "~/Library/Preferences/cn.trae.app.plist",
+    "~/Library/Saved Application State/cn.trae.app.savedState",
+  ]
+end
