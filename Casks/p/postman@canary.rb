108066d3628a5741acff3f9c53aa2b2a598d42a4
Sam Ford <1584702+samford@users.noreply.github.com>
2025-03-03 08:12:07 -0500

    postman@canary: update url
    The `/download/version/...` URL for `postman@canary` now returns a 404
    (Not Found) response, so it seems like upstream may no longer support
    versioned URLs. This updates the cask `url` to the unversioned
    download URL (from the download page) used in the `livecheck` block.

--- Casks/p/postman@canary.rb
+++ Casks/p/postman@canary.rb
@@ -2,11 +2,10 @@ cask "postman@canary" do
   arch arm: "osx_arm64", intel: "osx64"
 
   version "11.2.14-canary240621-0734"
-  sha256 arm:   "a5630251d8463d567a3ea3da6b6d6bec5d47de47d594e9d64924aff0bca4bd4f",
-         intel: "907567df8bd91b3a9cce068779ff6746d089e578409169865cb89f341393019e"
+  sha256 :no_check
 
-  url "https://dl.pstmn.io/download/version/#{version}/#{arch}",
-      verified: "dl.pstmn.io/download/version/"
+  url "https://dl.pstmn.io/download/channel/canary/#{arch}",
+      verified: "dl.pstmn.io/download/channel/canary/"
   name "Postman Canary"
   desc "Collaboration platform for API development"
   homepage "https://www.postman.com/"
