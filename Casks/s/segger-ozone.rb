229322a9e82267eaf63ea4da91ccfe3f0cd0f663
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-04 16:42:20 +0000

    segger-ozone 3.38d

--- Casks/s/segger-ozone.rb
+++ Casks/s/segger-ozone.rb
@@ -1,9 +1,9 @@
 cask "segger-ozone" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "3.38c"
-  sha256 arm:   "cc9cf140fc5f220e3c920e184ef61d35f4efd79eaf1aa55756312dd56ed5aa12",
-         intel: "109029766e2a637554feb9239870d3228186587115ac7ced257121848ceaac4a"
+  version "3.38d"
+  sha256 arm:   "c96698f4852a8697fb712ccc7fff30cb9b31a142128f1cc3a1f27a148abec754",
+         intel: "030b229a863f52ec6df3a033b1bdc12020b2178e834c736123853030faca43e6"
 
   url "https://www.segger.com/downloads/jlink/Ozone_MacOSX_V#{version.no_dots}_#{arch}.pkg",
       using: :post,
