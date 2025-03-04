d12a268e6261ed312a4cb94cdc513deffdcaab7e
Kuingsmile <ma_shiqing@163.com>
2025-03-04 21:36:25 +0800

    piclist 2.9.8

--- Casks/p/piclist.rb
+++ Casks/p/piclist.rb
@@ -1,7 +1,7 @@
 cask "piclist" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.9.7"
+  version "2.9.8"
   sha256 arm:   "1b3f8ffe24774e584c9c07bc4807bf53f49dcb6809befa3a15c602158db3c448",
          intel: "2fa2c9d8f0f61d26fb232aa1f64cf27314843e8a018f65dea01f2ff74274d0b2"
 
