6fc3ec7580897f1764004a608f42fe4c64294f87
Om Duggineni <omduggineni@gmail.com>
2025-03-04 01:02:26 -0500

    Fix SHA256 hash for Apple Silicon version of publii

--- Casks/p/publii.rb
+++ Casks/p/publii.rb
@@ -2,7 +2,7 @@ cask "publii" do
   arch arm: "apple-silicon", intel: "intel"
 
   version "0.46.4"
-  sha256 arm:   "72892d7baca92acc045046f15641218da4c84f866d5709949a3bf18067c1c0d4",
+  sha256 arm:   "99dbb99716aaffec27c846f29f89260702e6a1c997e72dfe514be9b12698fb6d",
          intel: "5ac351c5da08c325e1e3d490a44c40408b874e68cceb4d82a58db7f409c0732d"
 
   url "https://getpublii.com/download/Publii-#{version}-#{arch}.dmg"
