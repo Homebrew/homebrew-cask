37def13239f23b4d62f73c8c2ef55752ed76b29f
shemeshg <gili.shemesh@gmail.com>
2025-03-04 05:26:24 +0200

    Bump version of midi-router-client to 2.0.0

--- Casks/m/midi-router-client.rb
+++ Casks/m/midi-router-client.rb
@@ -1,14 +1,13 @@
 cask "midi-router-client" do
-  version "1.4.3"
-  sha256 "69bc2d1dd0e34d447a276d0016caba4641bc24919ed5e1bdc3ce0c18b088c2c0"
+  version "2.0.0"
+  sha256 "c78ea158633f0d8a43d2b2f9e01ea075b781b74f35d9d3a732d76f3696d6738d"
 
-  url "https://downloads.sourceforge.net/midi-router-client/releaseOSX11_#{version}.zip"
+  url "https://downloads.sourceforge.net/midi-router-client/midi-router-client-#{version}-Darwin.dmg"
   name "Midi Router Client"
   desc "Create routes from anywhere to anywhere"
   homepage "https://sourceforge.net/projects/midi-router-client/"
 
-  app "releaseOSX11_#{version}/Midi router client.app"
-  app "releaseOSX11_#{version}/Midi router server.app"
+  app "midi-router-client.app"
 
   zap trash: [
     "~/Library/Application Support/Midi router client",
@@ -16,9 +15,6 @@ cask "midi-router-client" do
     "~/Library/Preferences/com.electron.midi-router-client.helper.plist",
     "~/Library/Preferences/com.electron.midi-router-client.plist",
     "~/Library/Preferences/com.shemeshg.MidiRouter.plist",
+    "~/Library/Preferences/com.shemeshg.midirouterclient.midi-router-client.plist",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
