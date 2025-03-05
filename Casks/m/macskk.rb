6fdeec6dc2a1989bb99194701c4ce8ee72dfb9bd
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-03-05 15:32:12 +0900

    macskk 1.11.0 (new cask)

---
+++ Casks/m/macskk.rb
@@ -0,0 +1,29 @@
+cask "macskk" do
+  version "1.11.0"
+  sha256 "0aab56e9b7d2b80a3ef55466453831d1a2a904a0440c8c6274e87b579bc3ef0c"
+
+  url "https://github.com/mtgto/macSKK/releases/download/#{version}/macSKK-#{version}.dmg"
+  name "macSKK"
+  desc "SKK Input Method"
+  homepage "https://github.com/mtgto/macSKK"
+
+  auto_updates true
+  depends_on macos: ">= :ventura"
+
+  pkg "macSKK-#{version}.pkg"
+
+  uninstall quit:    [
+              "net.mtgto.inputmethod.macSKK",
+              "net.mtgto.inputmethod.macSKK.FetchUpdateService",
+              "net.mtgto.inputmethod.macSKK.SKKServClient",
+            ],
+            pkgutil: [
+              "net.mtgto.inputmethod.macSKK.app",
+              "net.mtgto.inputmethod.macSKK.dict",
+            ]
+
+  zap trash: [
+    "~/Library/Application Scripts/net.mtgto.inputmethod.macSKK",
+    "~/Library/Containers/net.mtgto.inputmethod.macSKK",
+  ]
+end
