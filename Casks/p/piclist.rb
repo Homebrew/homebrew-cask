5e5bec127f7f72ab6aeacc6bd3668db84d2d718d
Kuingsmile <ma_shiqing@163.com>
2025-03-05 10:10:39 +0800

    piclist 2.9.8

--- Casks/p/piclist.rb
+++ Casks/p/piclist.rb
@@ -5,7 +5,7 @@ cask "piclist" do
   sha256 arm:   "1b3f8ffe24774e584c9c07bc4807bf53f49dcb6809befa3a15c602158db3c448",
          intel: "2fa2c9d8f0f61d26fb232aa1f64cf27314843e8a018f65dea01f2ff74274d0b2"
 
-  url "https://github.com/Kuingsmile/PicList/releases/download/v#{version}/PicList-#{version}-#{arch}.dmg"
+  url "https://github.com/Kuingsmile/PicList/releases/download/v#{version}/PicList-#{version}-#{arch}.dmg",
     verified: "github.com/Kuingsmile/PicList/"
   name "PicList"
   desc "Cloud storage manager tool"
