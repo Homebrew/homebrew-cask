b91586f44fcb06275a80349aec5070f752ce9372
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-03-05 21:37:33 +0900

    duplicati 2.1.0.5

--- Casks/d/duplicati.rb
+++ Casks/d/duplicati.rb
@@ -1,18 +1,18 @@
 cask "duplicati" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.1.0.3,2025-01-22"
-  sha256 intel: "65df38a887144c81e3783777f2ec7637c8468e2b95efed235cf8b13082a0aade",
-         arm:   "7f26fc3c3352b0731b7643cf3d12c79ea5719b29f224321d7872d4a7e384a627"
+  version "2.1.0.5,2025-03-04"
+  sha256 intel: "4362c0db54a4c4dfdbd71a8b933c6c9b838eb466809c5121fcd18a0bdf7b4fe8",
+         arm:   "f49184dcd6b2d852cafc2b46ba9b28a61c69e7cb115dd077261ea74c2ca58d02"
 
-  url "https://updates.duplicati.com/beta/duplicati-#{version.csv.first}_beta_#{version.csv.second}-osx-#{arch}-gui.dmg"
+  url "https://updates.duplicati.com/stable/duplicati-#{version.csv.first}_stable_#{version.csv.second}-osx-#{arch}-gui.dmg"
   name "Duplicati"
   desc "Store securely encrypted backups in the cloud"
-  homepage "https://www.duplicati.com/"
+  homepage "https://duplicati.com/"
 
   livecheck do
-    url "https://updates.duplicati.com/beta/latest-v2.manifest"
-    regex(/duplicati[._-]v?(\d+(?:\.\d+)+)[._-]beta[._-](\d+(?:[.-]\d+)+)[._-]osx[._-]#{arch}[._-]gui\.dmg/i)
+    url "https://updates.duplicati.com/stable/latest-v2.manifest"
+    regex(/duplicati[._-]v?(\d+(?:\.\d+)+)[._-]stable[._-](\d+(?:[.-]\d+)+)[._-]osx[._-]#{arch}[._-]gui\.dmg/i)
     strategy :page_match do |page, regex|
       match = page.match(regex)
       next if match.blank?
