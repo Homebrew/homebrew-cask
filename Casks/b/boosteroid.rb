08855c565871938e037e4bc867529f36e8c65a56
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-06 18:38:16 +0000

    boosteroid 1.9.16

--- Casks/b/boosteroid.rb
+++ Casks/b/boosteroid.rb
@@ -2,7 +2,7 @@ cask "boosteroid" do
   arch arm: "arm64", intel: "x64"
   folder = on_arch_conditional arm: "_ARM"
 
-  version "1.9.15"
+  version "1.9.16"
   sha256 :no_check
 
   url "https://boosteroid.com/macos#{folder}/installer/boosteroid-install-#{arch}.dmg",
