0216395ab4cf3c0a62440d5cf15deb4769e7e734
Klaus Hipp <khipp@users.noreply.github.com>
2025-03-06 06:48:14 +0100

    kindle-comic-converter: revert to 7.1.2

--- Casks/k/kindle-comic-converter.rb
+++ Casks/k/kindle-comic-converter.rb
@@ -1,16 +1,15 @@
 cask "kindle-comic-converter" do
   arch arm: "arm", intel: "i386"
 
-  version "7.2.3"
-  sha256 arm:   "612948894db1a6cee3d2c6695ecdef0f381e834ec676d1a9dc1ceb7eb53d023c",
-         intel: "7f0156fdab71fbf2386a6a78a8c8a892825374f0bf71a6bde43c8fb47d088ae0"
+  version "7.1.2"
+  sha256 arm:   "ab7bbe4b738c452ab66e88280b25f6c5d5bb50466d63875b8b7c893c949e3da0",
+         intel: "362a7565c44e0e2c32e5cf7bd030b614cb3f7ab3b7f504459c2ae5eb6bdf4fdf"
 
-  url "https://github.com/ciromattia/kcc/releases/download/v#{version}/kcc_macos_#{arch}_#{version}.dmg",
-      verified: "github.com/ciromattia/kcc/"
+  url "https://github.com/ciromattia/kcc/releases/download/v#{version}/kcc_macos_#{arch}_#{version}.dmg"
   name "Kindle Comic Converter"
   name "KCC"
   desc "Comic and manga converter for ebook readers"
-  homepage "https://kcc.iosphe.re/"
+  homepage "https://github.com/ciromattia/kcc"
 
   livecheck do
     url :url
