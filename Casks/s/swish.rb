428c4b1b60137e4cb4f2266ea80d6842c3d02b7c
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-03-06 11:00:59 +0900

    swish 1.12

--- Casks/s/swish.rb
+++ Casks/s/swish.rb
@@ -1,6 +1,6 @@
 cask "swish" do
-  version "1.11"
-  sha256 "119c3104ac020a40765ffa79a8c98d13a2dca4ca5d8c63b46ddcae2c31a40ebe"
+  version "1.12"
+  sha256 "52c99ab359f651ab01026db5c2cfb08e460da7bf06b6fe0b94d78f245553587e"
 
   url "https://github.com/chrenn/swish-dl/releases/download/#{version}/Swish.dmg",
       verified: "github.com/chrenn/swish-dl/"
