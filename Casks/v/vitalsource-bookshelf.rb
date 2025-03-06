03728fd4fc0814ca4fe3461b572e0ea189065718
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-03-06 12:04:00 +0900

    vitalsource-bookshelf: update minimum macOS

--- Casks/v/vitalsource-bookshelf.rb
+++ Casks/v/vitalsource-bookshelf.rb
@@ -13,7 +13,7 @@ cask "vitalsource-bookshelf" do
     regex(/href=.*?VitalSource[._-]Bookshelf[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
-  depends_on macos: ">= :catalina"
+  depends_on macos: ">= :big_sur"
 
   app "VitalSource Bookshelf.app"
 
