7c8635c4489f8ad062d8caada13be57b06253fd4
ratiron <ratironh188@gmail.com>
2025-03-08 00:02:42 +0900

    feat: add source-han-code-jp

---
+++ Casks/font/font-s/font-source-han-code-jp.rb
@@ -0,0 +1,23 @@
+cask "font-source-han-code-jp" do
+  version "2.012R"
+  sha256 "bedc74973220f1ce4bb16e1fa64a46604c3164bf95b62fa48c8a046dd468d6ef"
+
+  url "https://github.com/adobe-fonts/source-han-code-jp/archive/refs/tags/#{version}.zip"
+  name "Source Han Code JP"
+  homepage "https://github.com/adobe-fonts/source-han-code-jp"
+
+  font "source-han-code-jp-#{version}/OTF/SourceHanCodeJP-Bold.otf"
+  font "source-han-code-jp-#{version}/OTF/SourceHanCodeJP-BoldIt.otf"
+  font "source-han-code-jp-#{version}/OTF/SourceHanCodeJP-ExtraLight.otf"
+  font "source-han-code-jp-#{version}/OTF/SourceHanCodeJP-ExtraLightIt.otf"
+  font "source-han-code-jp-#{version}/OTF/SourceHanCodeJP-Heavy.otf"
+  font "source-han-code-jp-#{version}/OTF/SourceHanCodeJP-HeavyIt.otf"
+  font "source-han-code-jp-#{version}/OTF/SourceHanCodeJP-Light.otf"
+  font "source-han-code-jp-#{version}/OTF/SourceHanCodeJP-LightIt.otf"
+  font "source-han-code-jp-#{version}/OTF/SourceHanCodeJP-Medium.otf"
+  font "source-han-code-jp-#{version}/OTF/SourceHanCodeJP-MediumIt.otf"
+  font "source-han-code-jp-#{version}/OTF/SourceHanCodeJP-Normal.otf"
+  font "source-han-code-jp-#{version}/OTF/SourceHanCodeJP-NormalIt.otf"
+  font "source-han-code-jp-#{version}/OTF/SourceHanCodeJP-Regular.otf"
+  font "source-han-code-jp-#{version}/OTF/SourceHanCodeJP-RegularIt.otf"
+end
