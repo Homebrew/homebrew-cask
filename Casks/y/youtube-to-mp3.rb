a31720f59836988eb3058b6b7f9ec50c734b548f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-07 19:20:07 +0000

    youtube-to-mp3 3.9.9.98

--- Casks/y/youtube-to-mp3.rb
+++ Casks/y/youtube-to-mp3.rb
@@ -2,7 +2,7 @@ cask "youtube-to-mp3" do
   # NOTE: "3" is not a version number, but an intrinsic part of the product name
   arch arm: "-arm"
 
-  version "3.9.9.97"
+  version "3.9.9.98"
   sha256 :no_check
 
   on_sierra :or_older do
