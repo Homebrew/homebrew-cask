a91f5b2bf0f9e0600ffc12e5ee2886c485dc966e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-07 06:54:33 +0000

    stash 2.8.0,349

--- Casks/s/stash.rb
+++ Casks/s/stash.rb
@@ -1,6 +1,6 @@
 cask "stash" do
-  version "2.8.0,348"
-  sha256 "630c70bd74326a8c2072e0030d3538978c5cb6882ab7081e6f13817bb13a7b0d"
+  version "2.8.0,349"
+  sha256 "4e0e077324e54bb503895fb5222342648df00b820f07513480a2989dc5d711a4"
 
   url "https://mac-release-static.stash.ws/Stash-build-#{version.csv.second}.zip"
   name "Stash"
