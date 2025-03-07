521a4c14aff1a7684497752a7739baf8b986d190
ratiron <ratironh188@gmail.com>
2025-03-08 00:31:50 +0900

    fix: .zip -> .tar.gz

--- Casks/font/font-s/font-source-han-code-jp.rb
+++ Casks/font/font-s/font-source-han-code-jp.rb
@@ -1,8 +1,8 @@
 cask "font-source-han-code-jp" do
   version "2.012R"
-  sha256 "bedc74973220f1ce4bb16e1fa64a46604c3164bf95b62fa48c8a046dd468d6ef"
+  sha256 "678a3bf747269c7df6fa892ed9eb050139af77f2d50ec9408d99027da88f3b4b"
 
-  url "https://github.com/adobe-fonts/source-han-code-jp/archive/refs/tags/#{version}.zip"
+  url "https://github.com/adobe-fonts/source-han-code-jp/archive/refs/tags/#{version}.tar.gz"
   name "Source Han Code JP"
   homepage "https://github.com/adobe-fonts/source-han-code-jp"
 
