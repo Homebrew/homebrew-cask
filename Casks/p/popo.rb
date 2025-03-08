5154355cb5a25df033149a514d41798c822e2f52
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-03-08 22:06:19 +0900

    popo 4.18.0

--- Casks/p/popo.rb
+++ Casks/p/popo.rb
@@ -1,8 +1,17 @@
 cask "popo" do
-  version "4.17.0,1738934160442"
-  sha256 "ce1a803b84ba9df01b573d1d4d4ae1c8001d6be5b1ed9bb0136ec97a9e9aeec1"
+  arch arm: "arm64", intel: "x86_64"
+  livecheck_arch = on_arch_conditional arm: "arm", intel: "intel"
 
-  url "https://popo.netease.com/file/popomac/POPO-setup_prod_#{version.csv.second}.dmg"
+  on_arm do
+    version "4.18.0,1741272609925"
+    sha256 "2806322cc4ae19eb58d386f128ec899daeff7109ddbfa88ddac6132869211f29"
+  end
+  on_intel do
+    version "4.18.0,1741272502251"
+    sha256 "ee271d2345b8b276a393f53b538388e11651b3c85106bcf9dfd88bddb949efaa"
+  end
+
+  url "https://popo.netease.com/file/popomac/POPO_Mac_#{arch}_prod_#{version.csv.second}.dmg"
   name "NetEase POPO"
   desc "Instant messaging platform"
   homepage "https://popo.netease.com/"
@@ -18,7 +27,7 @@ cask "popo" do
 
       json = Homebrew::Livecheck::Strategy::Json.parse_json(match[1])
       version = json.dig("data", "version")
-      build = json.dig("data", "url")&.[](build_regex, 1)
+      build = json.dig("data", "#{livecheck_arch}Url")&.[](build_regex, 1)
       next if version.blank? || build.blank?
 
       "#{version},#{build}"
