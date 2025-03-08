c40154dbd940aabf2e3600c02c3477686c5ac4c4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-08 12:37:42 +0000

    amie 250307.0.1

--- Casks/a/amie.rb
+++ Casks/a/amie.rb
@@ -1,9 +1,9 @@
 cask "amie" do
   arch arm: "-arm64"
 
-  version "250130.1.1"
-  sha256 arm:   "47fba1dedeb50d79cc97291ca29d37bbc8b42aebd5611d69020cea2c4edccc97",
-         intel: "747dea9deeebc2a2bf6182957b851783723dc07c8e53c4a7a40b1b3610268118"
+  version "250307.0.1"
+  sha256 arm:   "9612e3cc030c041b780c06d6f938c00463cb7bd129995a2153feea5d1fc74d7c",
+         intel: "55b75681e1bc56a9ec2592bad63a270b96e305035ac750ec8b4d07e098eb52ed"
 
   url "https://github.com/amieso/electron-releases/releases/download/v#{version}/Amie-#{version}#{arch}-mac.zip",
       verified: "github.com/amieso/electron-releases/"
