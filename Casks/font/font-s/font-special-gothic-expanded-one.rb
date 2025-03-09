246945be565024f3b9f9486b68cdf514c1223f6d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-09 00:12:51 +0000

    font-special-gothic-expanded-one: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml) workflow.

---
+++ Casks/font/font-s/font-special-gothic-expanded-one.rb
@@ -0,0 +1,12 @@
+cask "font-special-gothic-expanded-one" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/specialgothicexpandedone/SpecialGothicExpandedOne-Regular.ttf"
+  name "Special Gothic Expanded One"
+  homepage "https://github.com/AlistairMcCready/Special-Gothic"
+
+  font "SpecialGothicExpandedOne-Regular.ttf"
+
+  # No zap stanza required
+end
