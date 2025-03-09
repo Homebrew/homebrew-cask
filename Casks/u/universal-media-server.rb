736d842e312290c47f1a031e66a1e2839a32b14c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-09 09:51:28 +0000

    universal-media-server 14.10.1

--- Casks/u/universal-media-server.rb
+++ Casks/u/universal-media-server.rb
@@ -1,9 +1,9 @@
 cask "universal-media-server" do
   arch arm: "arm", intel: "x86_64"
 
-  version "14.10.0"
-  sha256 arm:   "527d13df679461f01ef2773c6d0aa797519b104234653922757c244b4b1f1cd4",
-         intel: "738830a67ef0bf6ea9fab586c6c40402d41e791e2a54e0c832c33f973a116b29"
+  version "14.10.1"
+  sha256 arm:   "bd2b3d078b6df6a842c0c8c0d3d0047f342f36363a310c0bd57612e0343139a6",
+         intel: "b033de11bec2fe52bbe9a9dd4c80869c82dc6b5fe92075b0ee21ece24c7396e4"
 
   url "https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/#{version}/UMS-macOS-#{version}-#{arch}.dmg",
       verified: "github.com/UniversalMediaServer/UniversalMediaServer/"
