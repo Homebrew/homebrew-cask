cb31e059adf0a8d988ec9b5c6705077d28b8a55e
Patrick Linnane <patrick@linnane.io>
2025-03-08 19:49:27 -0800

    Apply suggestions from code review
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/d/desktime.rb
+++ Casks/d/desktime.rb
@@ -1,20 +1,21 @@
 cask "desktime" do
-  url_param = on_arch_conditional arm: "?m1", intel: ""
+  arch arm: "arm64", intel: "x64"
 
   version "6.0.7"
   sha256 arm:   "b199ad26a43d8f287938f4b77d38e2834de122d4a120885b7ae29d6ca0016884",
          intel: "ad7e767d5ea52396f6801e5473724ef79958523db163f76a5993bb622e198fd7"
 
-  url "https://desktime.com/updates/electron/mac#{url_param}"
+  url "https://desktime.com/storage/updates/electro-builder-auto-updater/stable/DeskTime-#{version}-#{arch}.dmg"
   name "DeskTime"
   desc "Time tracker with additional workforce management features"
   homepage "https://desktime.com/"
 
   livecheck do
-    url :url
-    strategy :header_match
-    regex(/filename=DeskTime-(\d+(?:\.\d+)+)-(?:x64|arm64)\.dmg/)
+    url "https://desktime.com/storage/updates/electro-builder-auto-updater/stable/latest-mac.yml"
+    strategy :electron_builder
   end
+
+  auto_updates true
 
   app "DeskTime.app"
 
