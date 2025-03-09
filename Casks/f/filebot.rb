bf4522de4068b619324391d0e2c1a901b8f07413
Patrick Linnane <patrick@linnane.io>
2025-03-08 19:36:28 -0800

    filebot: use pkg installer
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/f/filebot.rb
+++ Casks/f/filebot.rb
@@ -2,10 +2,10 @@ cask "filebot" do
   arch arm: "arm64", intel: "x64"
 
   version "5.1.7"
-  sha256 arm:   "29cca94a795ec621842e984ff56d66c289874342eaddb813af0964446e8f5404",
-         intel: "d8e28e64db826311523e8bbd1a4951937a5da1b6910827e940c9eeb0a38ff30d"
+  sha256 arm:   "262fbbf7d9503ba04aa3dfb9ac50d6f2a1850dd2d72538e0bbf016f97d3055fa",
+         intel: "f8a7976ac064a3effdcb08077aefe259357954d4654a6f712cec488cddd112ed"
 
-  url "https://get.filebot.net/filebot/FileBot_#{version}/FileBot_#{version}_#{arch}.app.tar.xz"
+  url "https://get.filebot.net/filebot/FileBot_#{version}/FileBot_#{version}_#{arch}.pkg"
   name "FileBot"
   desc "Tool for organising and renaming movies, TV shows, anime or music"
   homepage "https://www.filebot.net/"
@@ -17,10 +17,13 @@ cask "filebot" do
 
   depends_on macos: ">= :catalina"
 
-  app "FileBot.app"
+  pkg "FileBot_#{version}_#{arch}.pkg"
   binary "#{appdir}/FileBot.app/Contents/MacOS/filebot.sh", target: "filebot"
   binary "#{appdir}/FileBot.app/Contents/Resources/bash_completion.d/filebot_completion",
          target: "#{HOMEBREW_PREFIX}/etc/bash_completion.d/filebot"
+
+  uninstall pkgutil: "net.filebot.FileBot.pkg",
+            delete:  "#{appdir}/FileBot.app"
 
   zap trash: [
     "~/Library/Application Scripts/net.filebot.FileBot",
