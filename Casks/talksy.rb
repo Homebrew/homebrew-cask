c2e7594fdcc1078ee93c6a12cd64197d3b8ab043
rhiskey <first3395@gmail.com>
2025-03-10 09:56:40 +0300

    fix

--- Casks/talksy.rb
+++ Casks/talksy.rb
@@ -1,23 +1,18 @@
 cask "talksy" do
     version "0.0.23"
     sha256 "3760cf900f8a6d0515ccb82072d77746b887b97f9a44c8dd4ce5b11ff1a25991"
-  
     url "https://repo.cyberkoalastudios.com/mtalksy/TalksyInstaller.zip"
     name "Talksy"
     desc "Video calling app"
     homepage "https://talksy.ru/landing/"
-
     livecheck do
         url "https://repo.cyberkoalastudios.com/mtalksy/TalksyInstaller.zip"
         strategy :page_match do |page|
           match = page.match(/ffone(\d)(\d+)\.zip/i)
           next if match.blank?
-    
           "#{match[1]}.#{match[2]}"
         end
     end
-    
-
     app "Talksy.app"
     zap trash: [
         "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.cyberkoalastudios.talksy.*",
