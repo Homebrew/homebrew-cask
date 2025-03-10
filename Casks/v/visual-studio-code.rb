1d1f59acf9c4b56a52179a42efa3979d7b1dc794
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-10 22:05:03 +0000

    visual-studio-code 1.98.1

--- Casks/v/visual-studio-code.rb
+++ Casks/v/visual-studio-code.rb
@@ -11,9 +11,9 @@ cask "visual-studio-code" do
     end
   end
   on_big_sur :or_newer do
-    version "1.98.0"
-    sha256 arm:   "c794330a7e7b96a6f31d69c67b01c52f4d3a7574fc56d50d24e578b06495e72d",
-           intel: "0a5cebab8e2939592a631d162bec15589cfe573e2ebfb5cd466107ba1d0151d9"
+    version "1.98.1"
+    sha256 arm:   "3d684c63d1be57e40241fb0eb7b208e8d6ea27a55ecc88d63b988b23fce5fe8f",
+           intel: "eb265a7896e11ff8a6f648616334e7fff8e81e327837acaa0c9dab72c6171782"
 
     livecheck do
       url "https://update.code.visualstudio.com/api/update/#{arch}/stable/latest"
