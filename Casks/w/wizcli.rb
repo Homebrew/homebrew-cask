5831f6a8c420e5b078a9fdf10d71ba26edb63cdd
Patrick Linnane <patrick@linnane.io>
2025-03-10 11:21:27 -0700

    wizcli: bump linux versions
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/w/wizcli.rb
+++ Casks/w/wizcli.rb
@@ -6,8 +6,8 @@ cask "wizcli" do
   version "0.77.0"
   sha256 arm:          "a4f3c68cbd33f9db8355ad5c30cec787997824afdd2ca2e8b920cd3bc7e54e8d",
          x86_64:       "1c59d7bfa0249c086de9e0269488f4bd9c92a7a4533c54180606e2825668a16d",
-         arm64_linux:  "6f214b64238765488605b82d281fb238f0ee143a1e930bd5cba50636e9fb5d7c",
-         x86_64_linux: "33171ce0e32eb9f50d44efd462598a5d74702cb25aa70ae572e3d200a00b1b0a"
+         arm64_linux:  "c544efb5c25b63ed26ac5f2222dc2b280d5e8ad2871a8273ffa24b69e0d5a899",
+         x86_64_linux: "36036693d213b125f25a65a90baaa9e7f1e4f335ec592cf3ba8ee1bf59815515"
 
   url "https://downloads.wiz.io/wizcli/#{version}/wizcli-#{os}-#{arch}"
   name "Wiz CLI"
