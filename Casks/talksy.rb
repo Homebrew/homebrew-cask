37577bc65f0724ab487a1f60bfc44724f91541ad
rhiskey <first3395@gmail.com>
2025-03-10 08:58:08 +0300

    Add Talksy

---
+++ Casks/talksy.rb
@@ -0,0 +1,11 @@
+cask "talksy" do
+    version "0.0.23"
+    sha256 "3760cf900f8a6d0515ccb82072d77746b887b97f9a44c8dd4ce5b11ff1a25991"
+  
+    url "https://repo.cyberkoalastudios.com/mtalksy/TalksyInstaller.zip"
+    name "Talksy"
+    desc "Video calling app"
+    homepage "https://talksy.ru/landing/"
+  
+    app "Talksy.app"
+end
