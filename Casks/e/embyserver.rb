4acabaf0efee1d16b791a1d1cc7589ef78f1e02e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-10 09:37:41 +0000

    embyserver 4.8.11.0

--- Casks/e/embyserver.rb
+++ Casks/e/embyserver.rb
@@ -1,9 +1,9 @@
 cask "embyserver" do
   arch arm: "arm64", intel: "x64"
 
-  version "4.8.10.0"
-  sha256 arm:   "feb3b8f57e1ed92cfa4ae7e7ec21e449508bfffee053022050a435e22bdaaff4",
-         intel: "7e7166b88dbffa461a67e813fe71f8019b74eaef07ae42a1b3c4e99375778cc3"
+  version "4.8.11.0"
+  sha256 arm:   "753343c90c95eda6550637a4ff7029f5be1de40cd900dabc27bf259a389db9d0",
+         intel: "7b6dd08a69631394f43d8f8ab209887747b2854247b803ab3e17bd56620dc85e"
 
   url "https://github.com/MediaBrowser/Emby.Releases/releases/download/#{version}/embyserver-osx-#{arch}-#{version}.zip",
       verified: "github.com/MediaBrowser/Emby.Releases/"
