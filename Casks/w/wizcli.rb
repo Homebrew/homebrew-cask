6cf18950c64bc8e311abde6f8f425e84b3cbad6d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-10 16:04:04 +0000

    wizcli 0.77.0

--- Casks/w/wizcli.rb
+++ Casks/w/wizcli.rb
@@ -3,9 +3,9 @@ cask "wizcli" do
 
   os macos: "darwin", linux: "linux"
 
-  version "0.76.0"
-  sha256 arm:          "9f09e8a7885c1f272f8ad8ecd3979e532e10f5bf4a7e04e43cdc33ef0e5c1069",
-         x86_64:       "3155fe4ef256cfa402719cb0bd1e10e3b5bfeead27042c661283584cb06f3bb2",
+  version "0.77.0"
+  sha256 arm:          "a4f3c68cbd33f9db8355ad5c30cec787997824afdd2ca2e8b920cd3bc7e54e8d",
+         x86_64:       "1c59d7bfa0249c086de9e0269488f4bd9c92a7a4533c54180606e2825668a16d",
          arm64_linux:  "6f214b64238765488605b82d281fb238f0ee143a1e930bd5cba50636e9fb5d7c",
          x86_64_linux: "33171ce0e32eb9f50d44efd462598a5d74702cb25aa70ae572e3d200a00b1b0a"
 
