fb4af8e3e307bb18e21bb7fe4b76f1c1d76431b1
Qi (Ryan) Zhang <25192197+singularitti@users.noreply.github.com>
2025-03-12 13:14:27 -0600

    font-plangothic: 2.9.5771

--- Casks/font/font-p/font-plangothic.rb
+++ Casks/font/font-p/font-plangothic.rb
@@ -1,6 +1,6 @@
 cask "font-plangothic" do
-  version "1.9.5769"
-  sha256 "d58680d8a71ab80ec47bb6ce6d896cd528d01a7ee782298d312e2c4883d54a7e"
+  version "2.9.5771"
+  sha256 "7cec6a938a3fbd3ef58c9e6f06d32282277d86e17c3634de72f0adf427ad41ca"
 
   url "https://github.com/Fitzgerald-Porthmouth-Koenigsegg/Plangothic-Project/archive/refs/tags/V#{version}.tar.gz"
   name "Plangothic"
@@ -8,7 +8,7 @@ cask "font-plangothic" do
 
   font "Plangothic_Project-#{version}/PlangothicP1-Regular (allideo).otf"
   font "Plangothic_Project-#{version}/PlangothicP1-Regular (fallback).otf"
-  font "Plangothic_Project-#{version}/PlangothicP2-Regular.otf"
+  font "Plangothic_Project-#{version}/Plangothic.ttc"
 
   # No zap stanza required
 end
