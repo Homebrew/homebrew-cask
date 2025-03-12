bbba5d46dbfd55e742d2e6fc11f8523adaa38888
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-03-12 15:47:59 +0900

    google-ads-editor 2.9

--- Casks/g/google-ads-editor.rb
+++ Casks/g/google-ads-editor.rb
@@ -1,5 +1,5 @@
 cask "google-ads-editor" do
-  version "2.8"
+  version "2.9"
   sha256 :no_check
 
   url "https://dl.google.com/adwords_editor/Google_AdWords_Editor.dmg"
@@ -13,7 +13,7 @@ cask "google-ads-editor" do
   end
 
   auto_updates true
-  depends_on macos: ">= :big_sur"
+  depends_on macos: ">= :monterey"
 
   app "Google Ads Editor.app"
 
