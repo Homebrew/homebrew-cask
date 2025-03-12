485e30b3dafc2bd6a87cbdeff6944bb5a538556a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-12 07:02:35 +0000

    vlc@nightly 4.0.0,20250312-0413,d1409377

--- Casks/v/vlc@nightly.rb
+++ Casks/v/vlc@nightly.rb
@@ -3,12 +3,12 @@ cask "vlc@nightly" do
   livecheck_arch = on_arch_conditional arm: "arm64", intel: "intel64"
 
   on_arm do
-    version "4.0.0,20250311-0413,d01eace0"
-    sha256 "7d9c1baed4ebceace4d9baa6c729a34d6efaba730f1eb4bab4504e117ff15b5d"
+    version "4.0.0,20250312-0413,d1409377"
+    sha256 "a5955855f76d363d1dc2a3a8a54746e355f5c07ee3df67131773c8b0996f4e31"
   end
   on_intel do
-    version "4.0.0,20250311-0417,d01eace0"
-    sha256 "c60cfc0b92aa56f199d22e586babe209e1c8a389e8d1bab07f85d4497f591cb5"
+    version "4.0.0,20250312-0416,d1409377"
+    sha256 "22f7d27c7edb8edb7bf0a8def98117bbef9c43940bc801f3197f57510b2157db"
   end
 
   url "https://artifacts.videolan.org/vlc/nightly-macos-#{arch}/#{version.csv.second}/vlc-#{version.csv.first}-dev-#{livecheck_arch}-#{version.csv.third}.dmg"
