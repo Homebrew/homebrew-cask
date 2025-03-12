c91be2a62e27fb1bfa3dc14a802c46c2f797957c
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-03-12 21:23:43 +0900

    consul 1.20.5

--- Casks/c/consul.rb
+++ Casks/c/consul.rb
@@ -1,9 +1,9 @@
 cask "consul" do
   arch arm: "arm64", intel: "amd64"
 
-  version "1.20.4"
-  sha256 arm:   "6eb464db4fb7593722706340a4815fc86ba797b999c4a98c6b78e29c1078f3cd",
-         intel: "4130fe72c4e07c18fd426781145949743f6372a83299bf37d0a77b7680429352"
+  version "1.20.5"
+  sha256 arm:   "1bac78ed8a2ea869e227f9503db4fa2b04f90e4b7e8f19a285475a338d8dd755",
+         intel: "8eb12717124944f3177a4de8fae265d173c60c7219728fe9fa7dbbb3100f3567"
 
   url "https://releases.hashicorp.com/consul/#{version}/consul_#{version}_darwin_#{arch}.zip",
       verified: "releases.hashicorp.com/consul/"
