4c2c1ce25c559d0c04495033936483c7d35d25ea
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-03-13 10:06:24 +0900

    eclipse-rcp 4.35.0

--- Casks/e/eclipse-rcp.rb
+++ Casks/e/eclipse-rcp.rb
@@ -1,9 +1,9 @@
 cask "eclipse-rcp" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "4.34.0,2024-12"
-  sha256 arm:   "c4b5ee43c4bc64a87abd7c3ecaeda1f29abca420d1b43b5ace5514a2e8998eda",
-         intel: "d118a7d847dd390410c17869e77c9982b011d62cdf9c71498629597846582280"
+  version "4.35.0,2025-03"
+  sha256 arm:   "cca99e8aa25c2ca0002f5181972471fd0c47b2cc2a2cb16e2d2ee97b458e7376",
+         intel: "a3fa28e6a0d807d97a04cdd5bf34a45b538034d8d1ed53d26eaf8f460c06fc92"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-rcp-#{version.csv.second}-R-macosx-cocoa-#{arch}.dmg&r=1"
   name "Eclipse for RCP and RAP Developers"
