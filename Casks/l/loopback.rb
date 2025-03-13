28502815c60acf5507fd2763c52722c41e24eedf
Sean Molenaar <smillerdev@me.com>
2025-03-13 21:44:37 +0100

    loopback: use os_version

--- Casks/l/loopback.rb
+++ Casks/l/loopback.rb
@@ -24,7 +24,7 @@ cask "loopback" do
   homepage "https://rogueamoeba.com/loopback/"
 
   livecheck do
-    url "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&system=#{MacOS.full_version.to_s.delete(".")}&bundleid=com.rogueamoeba.Loopback&platform=osx&version=#{version.no_dots}8000"
+    url "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&system=#{os_version.to_s.delete(".")}&bundleid=com.rogueamoeba.Loopback&platform=osx&version=#{version.no_dots}8000"
     strategy :sparkle
   end
 
