b48908e82f94e391b5fc3a24d71cdb40d3993ee3
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-03-13 09:33:46 +0900

    eclipse-cpp 4.35.0

--- Casks/e/eclipse-cpp.rb
+++ Casks/e/eclipse-cpp.rb
@@ -1,9 +1,9 @@
 cask "eclipse-cpp" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "4.34.0,2024-12"
-  sha256 arm:   "ea1fa49107c0f06249dfd7837434f6465f911f43de77f72967bf56bbde1f99d6",
-         intel: "f9d5192842e42daa64516d70161825305746ec069317b0673fbc87871399b31c"
+  version "4.35.0,2025-03"
+  sha256 arm:   "7e51dd0e077de53da5f236cb3dcd6ba150f190bd336f697c09fe78430320614e",
+         intel: "5563c70410698dfef3b068f77152c46f905b68a57bd9110d0e58c70abc05bad7"
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.csv.second}/R/eclipse-cpp-#{version.csv.second}-R-macosx-cocoa-#{arch}.dmg&r=1"
   name "Eclipse IDE for C/C++ Developers"
