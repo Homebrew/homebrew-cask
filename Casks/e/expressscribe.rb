405c6b2eec3d5a7fdd4209e32ee69b63038d6d29
Sam Ford <1584702+samford@users.noreply.github.com>
2025-03-12 21:38:03 -0400

    expressscribe: correct version, update livecheck
    The current version of `expressscribe` is 13.11 and that's reflected
    in the app the cask is using. Subsequent versions like 14.00 have been
    Windows releases but the `livecheck` block regex incorrectly treats
    them all the same. This corrects the `version` and updates the
    `livecheck` block regex to only match macOS releases.

--- Casks/e/expressscribe.rb
+++ Casks/e/expressscribe.rb
@@ -1,5 +1,5 @@
 cask "expressscribe" do
-  version "14.00"
+  version "13.11"
   sha256 :no_check
 
   url "https://www.nch.com.au/scribe/scribemaci.zip"
@@ -9,7 +9,7 @@ cask "expressscribe" do
 
   livecheck do
     url "https://www.nch.com.au/scribe/versions.html"
-    regex(/Version\s*v?(\d+(?:\.\d+)+)/i)
+    regex(/Version\s*v?(\d+(?:\.\d+)+)\s*<[^>]+>\s*macOS\s+Release/im)
   end
 
   app "ExpressScribe.app"
