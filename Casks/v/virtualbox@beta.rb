3c2b36c1a9cfbc7a0eb012a4990a3c4759a3c67c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-18 01:13:27 +0000

    virtualbox@beta 7.1.7-167984

--- Casks/v/virtualbox@beta.rb
+++ Casks/v/virtualbox@beta.rb
@@ -1,9 +1,9 @@
 cask "virtualbox@beta" do
   arch arm: "macOSArm64", intel: "OSX"
 
-  version "7.1.7-167943"
-  sha256 arm:   "fec4354582c73225c7ef655bdbd1f49e24583aae08484c9875e9bf4e76cdb6f8",
-         intel: "d6abe632b5f22666bce98bdbb1ffb4107d07db3b2751a458442bedc27fb54ffd"
+  version "7.1.7-167984"
+  sha256 arm:   "55b2d3b75e5ea248a76791fdbf04596ce060d4726c9645f4a85b11b826610a63",
+         intel: "38b861c42d298fd6d70be1327bae2ba8c37d17517cdaea20c499f22781eed69b"
 
   url "https://www.virtualbox.org/download/testcase/VirtualBox-#{version}-#{arch}.dmg"
   name "Oracle VirtualBox"
