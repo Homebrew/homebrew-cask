22d68a0333568059db5ad945f3a2b800c3994a2c
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-03-18 23:07:10 +0900

    oracle-jdk-javadoc 24
    Co-authored-by: Bevan Kay <email@bevankay.me>
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/o/oracle-jdk-javadoc.rb
+++ Casks/o/oracle-jdk-javadoc.rb
@@ -1,6 +1,6 @@
 cask "oracle-jdk-javadoc" do
-  version "23.0.2,7,6da2a6609d6e406f85c491fcb119101b"
-  sha256 "4e4d79441664df525bba8b3041d481ce3c89e166df1f1488b5a8e7ef9fd23a03"
+  version "24,36,1f9ff9062db4449d8ca828c504ffae90"
+  sha256 "5560efa0b0f62e9231da82073e6b307b6397720dcf3e9ccaa129e39c4d74ff35"
 
   url "https://download.oracle.com/otn_software/java/jdk/#{version.csv.first}+#{version.csv.second}/#{version.csv.third}/jdk-#{version.csv.first}_doc-all.zip",
       cookies: {
@@ -11,10 +11,20 @@ cask "oracle-jdk-javadoc" do
   homepage "https://www.oracle.com/java/technologies/downloads/"
 
   livecheck do
-    url "https://www.oracle.com/java/technologies/javase-jdk#{version.major}-doc-downloads.html"
+    url :homepage
     regex(%r{/(\d+(?:\.\d+)*)(?:\+|%2B)(\d+(?:\.\d+)*)/(\h+)/jdk[._-]v?(\d+(?:\.\d+)*)_doc-all\.zip}i)
     strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| "#{match[0]},#{match[1]},#{match[2]}" }
+      major = page.scan(%r{href=.*?/technologies/javase-jdk(\d+)-doc-downloads\.html}i)
+                  .max_by { |match| Version.new(match[0]) }
+                  &.first
+      next if major.blank?
+
+      download_page = Homebrew::Livecheck::Strategy.page_content(
+        "https://www.oracle.com/java/technologies/javase-jdk#{major}-doc-downloads.html",
+      )
+      next if (download_page_content = download_page[:content]).blank?
+
+      download_page_content.scan(regex).map { |match| "#{match[0]},#{match[1]},#{match[2]}" }
     end
   end
 
