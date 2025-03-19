dfe6b75a438c98f7e23eb07e3d0e8eb852ea287a
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2025-03-18 22:40:44 -0400

    Add advancedrename v4.09
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>

---
+++ Casks/a/advanced-renamer.rb
@@ -0,0 +1,33 @@
+cask "advanced-renamer" do
+  arch arm: "arm", intel: "intel"
+
+  version "4.09"
+  sha256 arm:   "ff2cbf454d76793339a85f38fd35cdbb3ea216684856e6de107f852c2a3ea583",
+         intel: "19257a6906a893662aa2af2231a90395e9a33563cc03ce18fb6a03c742ac34cf"
+
+  url "https://www.advancedrenamer.com/down/macos/#{arch}/AdvancedRenamer_#{version.tr(".", "_")}.dmg"
+  name "Advanced Renamer"
+  desc "Batch file renaming utility"
+  homepage "https://www.advancedrenamer.com/"
+
+  livecheck do
+    url "https://www.advancedrenamer.com/download"
+    regex(%r{href=.*?/#{arch}/AdvancedRenamer[._-]v?(\d+(?:[._]\d+)+)\.dmg}i)
+    strategy :page_match do |page, regex|
+      match = page.match(regex)
+      next if match.blank?
+
+      match[1].tr("_", ".")
+    end
+  end
+
+  depends_on macos: ">= :big_sur"
+
+  app "Advanced Renamer.app"
+
+  zap trash: [
+    "~/Library/Caches/com.HulubuluSoftware.AdvancedRenamer",
+    "~/Library/HTTPStorages/com.HulubuluSoftware.AdvancedRenamer",
+    "~/Library/Saved Application State/com.HulubuluSoftware.AdvancedRenamer.savedState",
+  ]
+end
