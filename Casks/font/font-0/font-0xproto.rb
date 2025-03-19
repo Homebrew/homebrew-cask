33df4937ae9db5c8e2a07b31aaad45dc1f8e7fe5
eleanordoesntcode <166862894+eleanordoesntcode@users.noreply.github.com>
2025-03-19 16:02:55 +0100

    font-0xproto 2.300

--- Casks/font/font-0/font-0xproto.rb
+++ Casks/font/font-0/font-0xproto.rb
@@ -1,6 +1,6 @@
 cask "font-0xproto" do
-  version "2.202"
-  sha256 "4247363c00fafc5343d47e3a8a45e6dac7416019dabbf614b6aa24a5af056c1d"
+  version "2.300"
+  sha256 "9699314a7a061a8d4e5b1540755f9125c3321e0dee72b295797ea38691526cbe"
 
   url "https://github.com/0xType/0xProto/releases/download/#{version}/0xProto_#{version.dots_to_underscores}.zip"
   name "0xProto"
