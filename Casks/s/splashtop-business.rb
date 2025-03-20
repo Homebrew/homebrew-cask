28931ca2d70ec8a82caebf1cba2e03aafa509a7f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-20 07:02:02 +0000

    splashtop-business 3.7.4.0

--- Casks/s/splashtop-business.rb
+++ Casks/s/splashtop-business.rb
@@ -1,6 +1,6 @@
 cask "splashtop-business" do
-  version "3.7.2.4"
-  sha256 "629ef7dc049f2bcd687aca47d843e17ef45f9da6320ae63a5b673dfd3b24dc0f"
+  version "3.7.4.0"
+  sha256 "784427751ae9f126d01d20daf064ba30c1bbf67dd34e4e3c15c224df0c61a871"
 
   url "https://d17kmd0va0f0mp.cloudfront.net/macclient/STB/Splashtop_Business_Mac_INSTALLER_v#{version}.dmg",
       verified: "d17kmd0va0f0mp.cloudfront.net/macclient/STB/"
