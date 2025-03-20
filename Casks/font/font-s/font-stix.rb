ea9b9fee2f0162409089add72f74b1ac9c62eb0e
Klaus Hipp <khipp@users.noreply.github.com>
2025-03-20 13:41:17 +0100

    font-stix 2.14

--- Casks/font/font-s/font-stix.rb
+++ Casks/font/font-s/font-stix.rb
@@ -1,21 +1,21 @@
 cask "font-stix" do
-  version "2.13"
-  sha256 "c4671ec4a116d887c8ebf91b2706301bebc43e776ac00f549c36ad1f34964c98"
+  version "2.14"
+  sha256 "b9ce7effe9cf97185bc3bfd9b3c5e79e0928a500127d1f55d0a704e04d274420"
 
-  url "https://github.com/stipub/stixfonts/archive/refs/tags/v#{version}.tar.gz",
+  url "https://github.com/stipub/stixfonts/releases/download/v#{version}/fonts.zip",
       verified: "github.com/stipub/stixfonts/"
   name "STIX"
   homepage "https://stixfonts.org/"
 
-  font "stixfonts-#{version}/fonts/static_otf/STIXTwoMath-Regular.otf"
-  font "stixfonts-#{version}/fonts/static_otf/STIXTwoText-Bold.otf"
-  font "stixfonts-#{version}/fonts/static_otf/STIXTwoText-BoldItalic.otf"
-  font "stixfonts-#{version}/fonts/static_otf/STIXTwoText-Italic.otf"
-  font "stixfonts-#{version}/fonts/static_otf/STIXTwoText-Medium.otf"
-  font "stixfonts-#{version}/fonts/static_otf/STIXTwoText-MediumItalic.otf"
-  font "stixfonts-#{version}/fonts/static_otf/STIXTwoText-Regular.otf"
-  font "stixfonts-#{version}/fonts/static_otf/STIXTwoText-SemiBold.otf"
-  font "stixfonts-#{version}/fonts/static_otf/STIXTwoText-SemiBoldItalic.otf"
+  font "fonts/STIXTwoMath/OTF/STIXTwoMath.otf"
+  font "fonts/STIXTwoText/OTF/STIXTwoText-Bold.otf"
+  font "fonts/STIXTwoText/OTF/STIXTwoText-BoldItalic.otf"
+  font "fonts/STIXTwoText/OTF/STIXTwoText-Italic.otf"
+  font "fonts/STIXTwoText/OTF/STIXTwoText-Medium.otf"
+  font "fonts/STIXTwoText/OTF/STIXTwoText-MediumItalic.otf"
+  font "fonts/STIXTwoText/OTF/STIXTwoText-Regular.otf"
+  font "fonts/STIXTwoText/OTF/STIXTwoText-SemiBold.otf"
+  font "fonts/STIXTwoText/OTF/STIXTwoText-SemiBoldItalic.otf"
 
   # No zap stanza required
 end
