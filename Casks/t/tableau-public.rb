0a83156712a488d74928835d62be6f6a408055f7
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-03-20 13:37:19 +0900

    tableau-public 2025.1.0

--- Casks/t/tableau-public.rb
+++ Casks/t/tableau-public.rb
@@ -1,9 +1,9 @@
 cask "tableau-public" do
   arch arm: "-arm64"
 
-  version "2024.3.4"
-  sha256 arm:   "cf2d51efdefe6c889672af1059b86dbe998bbabd33099c01687f78b61c404651",
-         intel: "2fbabaf8c85a3989b62636bd83bf67b066557781b485d87a90324a51e3a5a32f"
+  version "2025.1.0"
+  sha256 arm:   "c903546f74dc08a79b64e006af08e0fe81e47dff36e769f359a1fcb7012f1c8d",
+         intel: "64c98cda7cbe3c87325895fae46855b66450fe5301c26518f7e81fd9d8147fc4"
 
   url "https://downloads.tableau.com/esdalt/#{version}/TableauPublic-#{version.dots_to_hyphens}#{arch}.pkg",
       user_agent: "curl/8.7.1"
