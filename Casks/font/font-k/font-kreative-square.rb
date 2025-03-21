883ec984b2d7908daab99ef6318b853df6261c98
Daeho Ro <lamanus@outlook.kr>
2025-03-21 17:44:41 +0900

    font-kreative-square 2025-03-20

--- Casks/font/font-k/font-kreative-square.rb
+++ Casks/font/font-k/font-kreative-square.rb
@@ -1,6 +1,6 @@
 cask "font-kreative-square" do
-  version "2024-06-01"
-  sha256 "d824b150dddfcb6215eb07fe890bc72721ff02dd03e8941b10e87deb25177e32"
+  version "2025-03-20"
+  sha256 "31fca4cd1f17ddc778377a47bf06a3e221b850b482bf6019251f30e4f8296a33"
 
   url "https://github.com/kreativekorp/open-relay/releases/download/#{version}/KreativeSquare.zip",
       verified: "github.com/kreativekorp/open-relay/"
