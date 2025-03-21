1a78acdb9f011e5ff1db546af7170f02331f4e16
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-21 16:00:22 +0000

    osu 2025.321.0

--- Casks/o/osu.rb
+++ Casks/o/osu.rb
@@ -1,9 +1,9 @@
 cask "osu" do
   arch arm: "Apple.Silicon", intel: "Intel"
 
-  version "2025.316.0"
-  sha256 arm:   "865cd0eb8280b4ecdf25355a37fc0146a3ce02f3da55a43e1bb6a9e81380aee6",
-         intel: "6512221e305ab5fb82ead78258cc8d8b35c014a911025992d5bd58df5abcb96f"
+  version "2025.321.0"
+  sha256 arm:   "5ac3f0fc932ec5aecbd8dda6f220112e0f370d96bffeb33dbd00e53439921ea9",
+         intel: "496b7b94d9e775f2bcf95d51d4de0747ff502e4cd1bd545a08c5707d35c0215c"
 
   url "https://github.com/ppy/osu/releases/download/#{version}/osu.app.#{arch}.zip"
   name "osu!"
