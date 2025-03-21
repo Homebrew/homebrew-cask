8a4e68d374c422f54baf1ebdda740b1174ea2610
Klaus Hipp <khipp@users.noreply.github.com>
2025-03-21 09:45:16 +0100

    trezor-bridge: update livecheck and url

--- Casks/t/trezor-bridge.rb
+++ Casks/t/trezor-bridge.rb
@@ -2,14 +2,13 @@ cask "trezor-bridge" do
   version "2.0.27"
   sha256 "a8a5352f888467cb1bc3b4273ee26c7f0da2a58f60e31aeffea46153aa03be07"
 
-  url "https://github.com/trezor/webwallet-data/raw/master/bridge/#{version}/trezor-bridge-#{version}.pkg",
-      verified: "github.com/trezor/webwallet-data/"
+  url "https://data.trezor.io/bridge/#{version}/trezor-bridge-#{version}.pkg"
   name "TREZOR Bridge"
   desc "Facilitates communication between the Trezor device and supported browsers"
   homepage "https://wallet.trezor.io/"
 
   livecheck do
-    url "https://raw.githubusercontent.com/trezor/webwallet-data/master/bridge/latest"
+    url "https://data.trezor.io/bridge/latest.txt"
     regex(/(\d+(?:\.\d+)+)/i)
   end
 
