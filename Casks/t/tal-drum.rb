8186d93327df0560e9bc46547e1cf330b9de9436
Ville Kärjä <ville.karja@gmail.com>
2025-03-22 22:36:31 +0200

    tal-drum 2.7.8 (new cask)
    Add TAL-Drum drum sampler plug-in to cask library.
    
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

---
+++ Casks/t/tal-drum.rb
@@ -0,0 +1,32 @@
+cask "tal-drum" do
+  version "2.7.8"
+  sha256 :no_check
+
+  url "https://tal-software.com/downloads/plugins/TAL-Drum-installer_macos.zip"
+  name "TAL-Drum"
+  desc "Drum sampler plug-in"
+  homepage "https://tal-software.com/products/tal-drum"
+
+  livecheck do
+    url "https://api.tal-software.com/api/page/byUrl/*products*tal-drum"
+    regex(/Version\s+v?(\d+(?:\.\d+)+)/i)
+  end
+
+  depends_on macos: ">= :el_capitan"
+
+  pkg "TAL-Drum-installer.pkg"
+
+  uninstall pkgutil: [
+    "com.talsoftware.pkg.TAL-Drum-AAX",
+    "com.talsoftware.pkg.TAL-Drum-AU",
+    "com.talsoftware.pkg.TAL-Drum-CLAP",
+    "com.talsoftware.pkg.TAL-Drum-VST",
+    "com.talsoftware.pkg.TAL-Drum-VST3",
+  ]
+
+  zap trash: [
+        "~/Library/Application Support/ToguAudioLine/TAL-Drum*",
+        "~/Library/Caches/TAL-Drum",
+      ],
+      rmdir: "~/Library/Application Support/ToguAudioLine"
+end
