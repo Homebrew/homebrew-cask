3b73fd8320d2ea8a0f30993cda6bb6c76ee355c8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-25 16:18:18 +0000

    font-chiron-sung-hk 1.017

--- Casks/font/font-c/font-chiron-sung-hk.rb
+++ Casks/font/font-c/font-chiron-sung-hk.rb
@@ -1,6 +1,6 @@
 cask "font-chiron-sung-hk" do
-  version "1.016"
-  sha256 "e3917cb333d100698910fe58b89eb12dd357319feca6d039dd35fb34e52bce4c"
+  version "1.017"
+  sha256 "dd0fb4ab440583d821075c6e6970f2ea20a9cf511d350289315ed6ce23f731b5"
 
   url "https://github.com/chiron-fonts/chiron-sung-hk/archive/refs/tags/v#{version}.tar.gz"
   name "Chiron Sung HK"
