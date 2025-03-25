49e4cfd2d78e05eec63035239e1d2ded57c20954
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-25 16:35:22 +0000

    propresenter@beta 18.4,302252042

--- Casks/p/propresenter@beta.rb
+++ Casks/p/propresenter@beta.rb
@@ -15,8 +15,8 @@ cask "propresenter@beta" do
     end
   end
   on_monterey :or_newer do
-    version "18.4,302252041"
-    sha256 "503190dd4815e8a7122667a70c04b7b611f5285052dfbf93b5e72f1c34d98a21"
+    version "18.4,302252042"
+    sha256 "0d75d2ebf9cf2f34455e3a5fea692d1dcb48ff1eaeb8874b8b97d0ffaec7fa4c"
 
     livecheck do
       url "https://api.renewedvision.com/v1/pro/upgrade?platform=macos&osVersion=#{MacOS.full_version}&appVersion=0&buildNumber=0&includeNotes=0&channel=beta"
