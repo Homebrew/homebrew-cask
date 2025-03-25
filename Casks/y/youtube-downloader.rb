518d40c0c83ad49ef9377368c51d1675105499d3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-25 22:04:42 +0000

    youtube-downloader 0.19

--- Casks/y/youtube-downloader.rb
+++ Casks/y/youtube-downloader.rb
@@ -1,6 +1,6 @@
 cask "youtube-downloader" do
-  version "0.18"
-  sha256 "94f84805944b9855db2384a6eee219a08fecf551edddaa045ce020f8ab8448ec"
+  version "0.19"
+  sha256 "23ae50372438f8fa94ffc45cb033e0b6c12e91ba6c46e498837274389c88bcf1"
 
   url "https://github.com/DenBeke/YouTube-Downloader-for-macOS/releases/download/v#{version}/Youtube.Downloader.zip"
   name "YouTube Downloader"
