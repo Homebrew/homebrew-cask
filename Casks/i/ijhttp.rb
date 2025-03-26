44ad4ec1aa3f5c73f5b1b0f17747fb6661f60e6b
Bartek Pacia <barpac02@gmail.com>
2025-03-26 15:57:13 +0100

    ijhttp 243.24978.46 (new cask)

---
+++ Casks/i/ijhttp.rb
@@ -0,0 +1,22 @@
+cask "ijhttp" do
+  version "243.24978.46"
+  sha256 "2fdbbf634a43fce0f6236597ea6815e6b88ff32ec893eeb355c33f59926ceeb1"
+
+  url "https://download.jetbrains.com/resources/intellij/http-client/#{version.major_minor_patch}/intellij-http-client.zip"
+  name "IntelliJ HTTP Client CLI"
+  desc "HTTP client from JetBrains IDEs available as a standalone CLI tool"
+  homepage "https://www.jetbrains.com/ijhttp/download"
+
+  livecheck do
+    url "https://jb.gg/ijhttp/latest"
+    strategy :header_match
+  end
+
+  binary "ijhttp/ijhttp"
+
+  # No zap stanza required
+
+  caveats do
+    depends_on_java "17+"
+  end
+end
