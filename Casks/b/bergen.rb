59c2cc81ffe5935677ddfb097a07a8c41f062fcf
Nima Karimi <nima@karimi.info>
2025-03-27 21:19:53 +0000

    Add bergen 0.1.4

---
+++ Casks/b/bergen.rb
@@ -0,0 +1,18 @@
+cask "bergen" do
+  version "0.1.4"
+  sha256 "6bb5846d739ddf59f03155be8d04465fa7d74cb080ebbc2261ffb57a945c9430"
+
+  url "https://github.com/kkarimi/bergen/releases/download/v#{version}/bergen-macos-v0.1.4.zip"
+  name "Bergen"
+  desc "A React Native macOS app for Markdown editing"
+  homepage "https://github.com/kkarimi/bergen"
+
+  app "bergen.app"
+
+  zap trash: [
+    "~/Library/Application Support/bergen",
+    "~/Library/Caches/bergen",
+    "~/Library/Preferences/com.kkarimi.bergen.plist",
+    "~/Library/Saved Application State/com.kkarimi.bergen.savedState"
+  ]
+end
