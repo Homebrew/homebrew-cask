2f260c66a9f5e1752e28f9a34b198461a41f3561
Ville Kärjä <ville.karja@gmail.com>
2025-03-27 19:24:47 +0200

    captainplugins 7.3.6.10032 (new cask)
    Add Captain Plugins music theory tool to the cask library.

---
+++ Casks/c/captainplugins.rb
@@ -0,0 +1,26 @@
+cask "captainplugins" do
+  version "7.3.6.10032"
+  sha256 :no_check
+
+  url "https://builds.mixedinkey.com/download/53/release/latest?key=dh-708a5f510d404bca9c44e2cecf5ced03"
+  name "Captain Plugins Epic"
+  desc "Music theory tool"
+  homepage "https://mixedinkey.com/get-captain-epic/"
+
+  livecheck do
+    url :url
+    regex(/CaptainPlugins.*?(\d+(?:\.\d+)*)\.zip/i)
+    strategy :header_match
+  end
+
+  depends_on macos: ">= :high_sierra"
+
+  pkg "CaptainPlugins.pkg"
+
+  uninstall pkgutil: "com.mixedinkey.CaptainPlugins.Epic.pkg"
+
+  zap trash: [
+    "~/Library/Application Support/Captain Plugins",
+    "~/Music/Captain Plugins",
+  ]
+end
