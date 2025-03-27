81e9e12fd66b29b251a33501146c2d518f13dcde
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-27 16:55:56 +0000

    mcreator 2025.1.13416

--- Casks/m/mcreator.rb
+++ Casks/m/mcreator.rb
@@ -1,9 +1,9 @@
 cask "mcreator" do
   arch arm: "aarch64", intel: "64bit"
 
-  version "2025.1.10519"
-  sha256 arm:   "bf88e2a2a78e69ceb98c0c0aa5a7c32b96f0c1616728422e81607a2cc8ae02f7",
-         intel: "0215f9086575986f2f5f737d824af703c1abf7d1b9da1e51d16036e473b8b20e"
+  version "2025.1.13416"
+  sha256 arm:   "d6ffca2255709369ce6c27cb1cb782536a54499f8bc803e1010fec0cbbc1a6c5",
+         intel: "e4cdb69c7f65faec0315abc14ae90c26387a953a9687278a77f039f45495709f"
 
   url "https://github.com/MCreator/MCreator/releases/download/#{version}/MCreator.#{version.major_minor}.Mac.#{arch}.dmg",
       verified: "github.com/MCreator/MCreator/"
