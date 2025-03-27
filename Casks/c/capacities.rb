95664edeb6d6dd4ad6f295d137905eec6f883de9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-27 15:37:48 +0000

    capacities 1.47.2

--- Casks/c/capacities.rb
+++ Casks/c/capacities.rb
@@ -1,9 +1,9 @@
 cask "capacities" do
   arch arm: "-arm64"
 
-  version "1.46.1"
-  sha256 arm:   "03cc98a862caf7a0804e6262bc7877cd61d3e3fbdd0569fece870e84639c4935",
-         intel: "1e4b0278b406b867e392ab2f7f449eb77922d604f2c86593cbf505b83acc5532"
+  version "1.47.2"
+  sha256 arm:   "46ee0c1ee99b624448a71c3cdd0c928449d71dce98e2415c1890a638f327d4da",
+         intel: "4a051ea831375ad80b01e3b2394d10234c5b56d3001555d2bdaf7c0fa856e716"
 
   url "https://capacities-desktop-app.fra1.cdn.digitaloceanspaces.com/Capacities-#{version}#{arch}.dmg",
       verified: "capacities-desktop-app.fra1.cdn.digitaloceanspaces.com/"
