6a43e8b566e5d26a2289b65040690495b923f817
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-27 12:56:13 +0000

    lunacy 11.2.1

--- Casks/l/lunacy.rb
+++ Casks/l/lunacy.rb
@@ -1,6 +1,6 @@
 cask "lunacy" do
-  version "11.2"
-  sha256 "e218a19b0b60389e90f4f81fb8ce05e896d1c11d0f61b1a9c2879c53ab2054ab"
+  version "11.2.1"
+  sha256 "39ec34732ac99119c4313a3aa0d5585429201b9b8496f18afaa8cb7d3099938d"
 
   url "https://lcdn.icons8.com/setup/Lunacy_#{version}.dmg"
   name "Lunacy"
