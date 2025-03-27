e65c925ddee0cce918969a5277a0f20a7b7f8f8f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-27 17:15:56 +0000

    tidelift 1.16.2

--- Casks/t/tidelift.rb
+++ Casks/t/tidelift.rb
@@ -1,6 +1,6 @@
 cask "tidelift" do
-  version "1.16.1"
-  sha256 "fc963892063ebe6e21df934a1c95ded83e8e59fe417deba3dacd365afee4730c"
+  version "1.16.2"
+  sha256 "cb5c93e29051ba2eec09c823a0bdd2f6b4de73fcafffddbc9902ef24649e08e5"
 
   url "https://download.tidelift.com/cli/#{version}/darwin_homebrew/tidelift"
   name "Tidelift CLI"
