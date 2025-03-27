955cbeb845822f4877c51d81d828b72106ad0237
netdcy <netdcy@163.com>
2025-03-27 14:11:17 +0800

    flowvision 1.5.3

--- Casks/f/flowvision.rb
+++ Casks/f/flowvision.rb
@@ -1,6 +1,6 @@
 cask "flowvision" do
-  version "1.5.2"
-  sha256 "e08383682f96a69a298cd944d6469b6de9d5ea6af66e133431557c2cc453c153"
+  version "1.5.3"
+  sha256 "7235ed587f7df90a73b5fa029e1deac7d736cca7f531d31e8d368fbee004d84e"
 
   url "https://github.com/netdcy/FlowVision/releases/download/#{version}/FlowVision.#{version}.zip"
   name "FlowVision"
