6f002432b54eab77d1aa52eb363acddcae35d83f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-27 01:07:05 +0000

    microsoft-edge@canary 136.0.3220.0

--- Casks/m/microsoft-edge@canary.rb
+++ Casks/m/microsoft-edge@canary.rb
@@ -2,12 +2,12 @@ cask "microsoft-edge@canary" do
   linkid = on_arch_conditional arm: "2093293", intel: "2069147"
 
   on_arm do
-    version "136.0.3219.0,dbd59dd0-9ec3-4725-ac0e-d4a274a8c97b"
-    sha256 "b8e87bb49d111f1f7bed1fb9cade536308d92bf4421605a666a523969568a659"
+    version "136.0.3220.0,9ad17de0-3052-484a-95db-da6d493fe0e8"
+    sha256 "5f8a295fd845916aed71534988868d2771195900a23f6106c6df386867e1eb71"
   end
   on_intel do
-    version "136.0.3219.0,d3d63c50-740c-4522-9de3-eea395788ac3"
-    sha256 "58b8714223e187e18358372c794119990613806684a16246eed3423f422c3c9e"
+    version "136.0.3220.0,92406c1a-7bca-4f16-9c5a-2afd5bdfcc26"
+    sha256 "03a77a6f910f54be2061e5f64f47e3b25ccc6d5019373c558cf373f4a2f6cb09"
   end
 
   url "https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/#{version.csv.second}/MicrosoftEdgeCanary-#{version.csv.first}.pkg"
