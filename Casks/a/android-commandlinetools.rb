9a0d5151a43be21e194ebe0a52bcf20ee6ffdf5c
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-03-28 11:03:06 +0900

    android-commandlinetools 13114758

--- Casks/a/android-commandlinetools.rb
+++ Casks/a/android-commandlinetools.rb
@@ -1,6 +1,6 @@
 cask "android-commandlinetools" do
-  version "11076708"
-  sha256 "7bc5c72ba0275c80a8f19684fb92793b83a6b5c94d4d179fc5988930282d7e64"
+  version "13114758"
+  sha256 "5673201e6f3869f418eeed3b5cb6c4be7401502bd0aae1b12a29d164d647a54e"
 
   url "https://dl.google.com/android/repository/commandlinetools-mac-#{version}_latest.zip",
       verified: "dl.google.com/android/repository/"
