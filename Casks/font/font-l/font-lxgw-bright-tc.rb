1bfd780434ab57584498fcc95c5875b408d540e4
Daeho Ro <lamanus@outlook.kr>
2025-03-28 21:22:15 +0900

    font-lxgw-bright-tc 5.517

--- Casks/font/font-l/font-lxgw-bright-tc.rb
+++ Casks/font/font-l/font-lxgw-bright-tc.rb
@@ -1,6 +1,6 @@
 cask "font-lxgw-bright-tc" do
-  version "5.510"
-  sha256 "405d888610f23eedf84fbb1b0827f09415b483602e787d1efe95ec8430376be3"
+  version "5.517"
+  sha256 "43ed03a42f2ca8004fa6c4647e1f85a9f08380f58365b185852f14d3d2f81651"
 
   url "https://github.com/lxgw/LxgwBright/releases/download/v#{version}/LXGWBrightTC.7z"
   name "LXGW Bright TC"
