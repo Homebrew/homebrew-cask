363dd819e95c41ca6fba111c15754ddf2fe11c28
eleanordoesntcode <166862894+eleanordoesntcode@users.noreply.github.com>
2025-03-29 00:13:54 +0100

    font-lxgw-wenkai-gb-lite: new cask

---
+++ Casks/font/font-l/font-lxgw-wenkai-gb-lite.rb
@@ -0,0 +1,18 @@
+cask "font-lxgw-wenkai-gb-lite" do
+  version "1.511"
+  sha256 "7593950dcb0e75741b9bed81e93791bca3495277dbb06bc23eef1f4e0935bd40"
+
+  url "https://github.com/lxgw/LxgwWenkaiGB-Lite/releases/download/v#{version}/lxgw-wenkai-gb-lite-v#{version}.zip"
+  name "LXGW WenKai GB Lite"
+  name "霞鹜文楷 GB 轻便版"
+  homepage "https://github.com/lxgw/LxgwWenkaiGB-Lite"
+
+  font "lxgw-wenkai-gb-lite-v#{version}/LXGWWenKaiGBLite-Light.ttf"
+  font "lxgw-wenkai-gb-lite-v#{version}/LXGWWenKaiGBLite-Medium.ttf"
+  font "lxgw-wenkai-gb-lite-v#{version}/LXGWWenKaiGBLite-Regular.ttf"
+  font "lxgw-wenkai-gb-lite-v#{version}/LXGWWenKaiMonoGBLite-Light.ttf"
+  font "lxgw-wenkai-gb-lite-v#{version}/LXGWWenKaiMonoGBLite-Medium.ttf"
+  font "lxgw-wenkai-gb-lite-v#{version}/LXGWWenKaiMonoGBLite-Regular.ttf"
+
+  # No zap stanza required
+end
