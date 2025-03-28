021f2095ba3aecc2a5d077857599d3f757ed4820
Klaus Hipp <khipp@users.noreply.github.com>
2025-03-28 14:28:14 +0100

    yubico-authenticator: update url and livecheck

--- Casks/y/yubico-authenticator.rb
+++ Casks/y/yubico-authenticator.rb
@@ -2,11 +2,15 @@ cask "yubico-authenticator" do
   version "7.2.0"
   sha256 "45ffd19c7d187f010856de4ec18d802518bc6ba1fe590ce937b3e809998f8309"
 
-  url "https://github.com/Yubico/yubioath-flutter/releases/download/#{version}/yubico-authenticator-#{version}-mac.dmg",
-      verified: "github.com/Yubico/yubioath-flutter/releases/download/"
+  url "https://developers.yubico.com/yubioath-flutter/Releases/yubico-authenticator-#{version}-mac.dmg"
   name "Yubico Authenticator"
   desc "Application for generating TOTP and HOTP codes"
   homepage "https://developers.yubico.com/yubioath-flutter/"
+
+  livecheck do
+    url "https://developers.yubico.com/yubioath-flutter/Releases/"
+    regex(/href=.*?yubico[._-]authenticator[._-]v?(\d+(?:\.\d+)+)[._-]mac\.dmg/i)
+  end
 
   depends_on macos: ">= :catalina"
 
