eed112dc4f057298da1582e2f0860624fad5298b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-29 12:41:28 +0000

    chatwise 0.8.17

--- Casks/c/chatwise.rb
+++ Casks/c/chatwise.rb
@@ -1,9 +1,9 @@
 cask "chatwise" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.8.16"
-  sha256 arm:   "27eed8e43fd3ed2eba345e4beb1af5d0764359019750860a845c72601d4ff27c",
-         intel: "21d5ba8e0114fc11d89aaddd25661031998410dfcc4a8000ba2938dfac3f48a0"
+  version "0.8.17"
+  sha256 arm:   "800ed79d2e68ef30d2876255a418785c56eb718a86b60db69dd4023f21c4449d",
+         intel: "0db1d403a129d3cf3e33ee8df745dffe47ff44ded22e956d0296c569294020c1"
 
   url "https://github.com/egoist/chatwise-releases/releases/download/v#{version}/ChatWise_#{version}_#{arch}.dmg",
       verified: "github.com/egoist/chatwise-releases/"
