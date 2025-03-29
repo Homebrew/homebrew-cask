81b82861ab62f1f5afbacce57b5253ab7fd70226
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-29 12:48:22 +0000

    gisto 2.1.0

--- Casks/g/gisto.rb
+++ Casks/g/gisto.rb
@@ -1,9 +1,9 @@
 cask "gisto" do
   arch arm: "aarch64", intel: "x64"
 
-  version "2.0.7"
-  sha256 arm:   "98d363140b3e147f355525124e15a31cea471009987adabd73ed398dfc205fb9",
-         intel: "a0270aacfe72431e1e0354ddb496bd495daae28ca896bc2663c05a7777753aa9"
+  version "2.1.0"
+  sha256 arm:   "3720aef0fa6abf19dfa6a42998012778ace0c5569b259f5d0065a60531f3d18f",
+         intel: "49ec1e438080ec59419bceaebf69d4292994a81efe8952eafb9c1dbc6a7193e8"
 
   url "https://github.com/Gisto/Gisto/releases/download/v#{version}/Gisto_#{version}_#{arch}.dmg",
       verified: "github.com/Gisto/Gisto/"
