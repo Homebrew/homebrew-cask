4fb46ccd8f41098a9dd1127c564011ab19d463bc
Nick Rimmer <NickRimmer@users.noreply.github.com>
2025-03-29 01:10:59 +0100

    Add RestApia cask v0.8.12

--- Casks/r/restapia.rb
+++ Casks/r/restapia.rb
@@ -1,23 +1,26 @@
 cask "restapia" do
+  arch arm: "arm64"
+
   version "0.8.12"
   sha256 "d2996269fc44aa86d26d79fcdceaf6c29a095b2370aee04acf48dd1b03946eb0"
 
-  arch arm: "arm64"
-
   url "https://github.com/RestApia/Releases.Osx.Stable/releases/download/v#{version}-osx-stable/RestApia-osx-stable-Setup.pkg",
-    verified: "https://github.com/RestApia/Releases.Osx.Stable/releases/download/"
-
+      verified: "github.com/RestApia/Releases.Osx.Stable/releases/download/"
   name "RestApia"
-  desc "RestApia is a HTTP API client that employs a code-driven approach to defining API requests. This method offers developers flexibility and control over their API interactions. The interface is designed to be intuitive and developer-friendly, utilizing a simple editor with features like syntax highlighting."
+  desc "HTTP API client offering flexible, code-based request definition"
   homepage "https://www.restapia.app/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+    regex(/^v?(\d+(?:\.\d+)+)/i)
+  end
 
   depends_on macos: ">= :big_sur"
 
   pkg "RestApia-osx-stable-Setup.pkg"
 
-  uninstall pkgutil: [
-              "com.RestApia.RestApia",
-            ]
+  uninstall pkgutil: "com.RestApia.RestApia"
 
   zap trash: "~/.restapia"
 end
