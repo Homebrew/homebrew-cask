077f2046a1e9c7ce9b35ea8cc936848790acf684
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-30 09:42:10 +0000

    josm 19369

--- Casks/j/josm.rb
+++ Casks/j/josm.rb
@@ -1,6 +1,6 @@
 cask "josm" do
-  version "19342"
-  sha256 "ce887ef73ae3a99f7efe1e63e2ed130d991eeb6be3abf29ceb8be433d02774cc"
+  version "19369"
+  sha256 "cc888eab5e28f7bd99f95e027372058dc81de741b5543128c5b71856d391e42e"
 
   url "https://github.com/JOSM/josm/releases/download/#{version}-tested/JOSM-macOS-java21-#{version}.zip",
       verified: "github.com/JOSM/josm/"
