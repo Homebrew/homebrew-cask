ec8de4b14062d65372b5475c0c2cdebafccd2652
Klaus Hipp <khipp@users.noreply.github.com>
2025-03-30 15:26:37 +0200

    tower: revert to 12.5.2

--- Casks/t/tower.rb
+++ Casks/t/tower.rb
@@ -1,6 +1,6 @@
 cask "tower" do
-  version "12.6.0,468,e9aceda0"
-  sha256 "2e1a921913e617ea00df8818a794107cb8983d2ba6fac92f7863071564127018"
+  version "12.5.2,466,9f52ea46"
+  sha256 "608ddc78cdbbf653aacdf1da701678dcae6a0631ee0096db68d6284ba03a0915"
 
   url "https://www.git-tower.com/apps/tower3-mac/#{version.csv.second}-#{version.csv.third}/Tower-#{version.csv.first}-#{version.csv.second}.zip"
   name "Tower"
@@ -8,13 +8,13 @@ cask "tower" do
   homepage "https://www.git-tower.com/"
 
   livecheck do
-    url "https://www.git-tower.com/updates/tower3-mac/stable/releases/latest/download"
-    regex(%r{(\d+(?:\.\d+)*)-([a-z0-9]+)/Tower-(\d+(?:\.\d+)+)-(\d+(?:\.\d+)*)\.zip}i)
-    strategy :header_match do |headers, regex|
-      match = headers["location"]&.match(regex)
+    url "https://updates.fournova.com/tower3-mac/stable/updates"
+    regex(%r{/tower3-mac/(?:\d+(?:\.\d+)*)-([a-z0-9]+)/}i)
+    strategy :sparkle do |item, regex|
+      match = item.url.match(regex)
       next if match.blank?
 
-      "#{match[3]},#{match[1]},#{match[2]}"
+      "#{item.nice_version},#{match[1]}"
     end
   end
 
