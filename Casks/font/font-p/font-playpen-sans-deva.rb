2ea51b04e6a74acccaf794ee6086e1ce5a6d00f3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-30 00:15:25 +0000

    font-playpen-sans-deva: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml) workflow.

---
+++ Casks/font/font-p/font-playpen-sans-deva.rb
@@ -0,0 +1,12 @@
+cask "font-playpen-sans-deva" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/playpensansdeva/PlaypenSansDeva%5Bwght%5D.ttf"
+  name "Playpen Sans Deva"
+  homepage "https://github.com/TypeTogether/Playpen-Sans"
+
+  font "PlaypenSansDeva[wght].ttf"
+
+  # No zap stanza required
+end
