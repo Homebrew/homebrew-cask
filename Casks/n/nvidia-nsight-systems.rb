19f99c6f8c60fe73ccffbea16410e8502cb6c8b4
Chun Cai <amoycaic@gmail.com>
2025-03-30 15:27:16 +0800

    nvidia-nsight-systems: add apple silicon support
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/n/nvidia-nsight-systems.rb
+++ Casks/n/nvidia-nsight-systems.rb
@@ -1,15 +1,18 @@
 cask "nvidia-nsight-systems" do
-  version "2025.2.1.130-3569061"
-  sha256 "6ffa5bcca3757bb4c632174bfe9b0703db805405d73ebed08a9e69334fcb2f7a"
+  arch arm: "-arm64"
 
-  url "https://developer.nvidia.com/downloads/assets/tools/secure/nsight-systems/#{version.major_minor.dots_to_underscores}/NsightSystems-macos-public-#{version}.dmg"
+  version "2025.2.1.130-3569061"
+  sha256 arm:   "d0cd36f6413922d54a66baca930c3980ac1771c082d73d17947fac5f3ee90212",
+         intel: "6ffa5bcca3757bb4c632174bfe9b0703db805405d73ebed08a9e69334fcb2f7a"
+
+  url "https://developer.nvidia.com/downloads/assets/tools/secure/nsight-systems/#{version.major_minor.dots_to_underscores}/NsightSystems-macos#{arch}-public-#{version}.dmg"
   name "NVIDIA Nsight Systems"
   desc "System-wide performance analysis tool"
   homepage "https://developer.nvidia.com/nsight-systems"
 
   livecheck do
     url "https://developer.nvidia.com/tools-downloads.json"
-    regex(/NsightSystems[._-]macos[._-]public[._-]v?(\d+(?:[.-]\d+)+)\.dmg/i)
+    regex(/NsightSystems[._-]macos#{arch}[._-]public[._-]v?(\d+(?:[.-]\d+)+)\.dmg/i)
     strategy :json do |json, regex|
       json["downloads"]&.map do |download|
         next unless download["development_platform"]&.include?("osx")
@@ -29,8 +32,4 @@ cask "nvidia-nsight-systems" do
   app "NVIDIA Nsight Systems.app"
 
   zap trash: "~/Library/Saved Application State/com.nvidia.devtools.QuadD.savedState"
-
-  caveats do
-    requires_rosetta
-  end
 end
