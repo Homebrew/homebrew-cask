25031205af95ed9aabfcc38b4d49b5f940b5901c
Klaus Hipp <khipp@users.noreply.github.com>
2025-03-31 10:36:49 +0200

    qlzipinfo: update name

--- Casks/q/qlzipinfo.rb
+++ Casks/q/qlzipinfo.rb
@@ -3,7 +3,7 @@ cask "qlzipinfo" do
   sha256 "397aef8f48bb62865ee6f8655b3e622e2b90eab2c67acbf775a95a23f63a0c25"
 
   url "https://github.com/srirangav/qlZipInfo/releases/download/v.#{version}/qlZipInfo-#{version}.dmg"
-  name "qlzipinfo"
+  name "qlZipInfo"
   desc "List out the contents of a zip file in the QuickLook preview"
   homepage "https://github.com/srirangav/qlZipInfo"
 
