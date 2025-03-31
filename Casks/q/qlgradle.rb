d72a1d4ecc351670aa08cb0502a8dff875a5b50a
Klaus Hipp <khipp@users.noreply.github.com>
2025-03-31 10:35:22 +0200

    qlgradle: update name

--- Casks/q/qlgradle.rb
+++ Casks/q/qlgradle.rb
@@ -3,7 +3,7 @@ cask "qlgradle" do
   sha256 "ea76846953ecfbd180d65167ec31cb7c316030f500a6669cd79857c03b951b63"
 
   url "https://github.com/Urucas/QLGradle/releases/download/#{version}/QLGradle.qlgenerator.zip"
-  name "qlgradle"
+  name "QLGradle"
   desc "Quick Look plugin for viewing gradle files"
   homepage "https://github.com/Urucas/QLGradle"
 
