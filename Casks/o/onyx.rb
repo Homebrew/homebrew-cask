3e21d968c553ca9dc4ca408251357d94d83dffe1
Klaus Hipp <khipp@users.noreply.github.com>
2025-03-31 15:13:49 +0200

    onyx: consolidate url

--- Casks/o/onyx.rb
+++ Casks/o/onyx.rb
@@ -1,57 +1,38 @@
 cask "onyx" do
   sha256 :no_check
 
-  on_el_capitan do
+  on_el_capitan :or_older do
     version "3.1.9"
-
-    url "https://www.titanium-software.fr/download/1011/OnyX.dmg"
   end
   on_sierra do
     version "3.3.1"
-
-    url "https://www.titanium-software.fr/download/1012/OnyX.dmg"
   end
   on_high_sierra do
     version "3.4.9"
-
-    url "https://www.titanium-software.fr/download/1013/OnyX.dmg"
   end
   on_mojave do
     version "3.6.8"
-
-    url "https://www.titanium-software.fr/download/1014/OnyX.dmg"
   end
   on_catalina do
     version "3.8.7"
-
-    url "https://www.titanium-software.fr/download/1015/OnyX.dmg"
   end
   on_big_sur do
     version "4.0.2"
-
-    url "https://www.titanium-software.fr/download/11/OnyX.dmg"
   end
   on_monterey do
     version "4.2.7"
-
-    url "https://www.titanium-software.fr/download/12/OnyX.dmg"
   end
   on_ventura do
     version "4.4.7"
-
-    url "https://www.titanium-software.fr/download/13/OnyX.dmg"
   end
   on_sonoma do
     version "4.6.2"
-
-    url "https://www.titanium-software.fr/download/14/OnyX.dmg"
   end
-  on_sequoia do
+  on_sequoia :or_newer do
     version "4.7.7"
-
-    url "https://www.titanium-software.fr/download/15/OnyX.dmg"
   end
 
+  url "https://www.titanium-software.fr/download/#{MacOS.version.to_s.delete(".")}/OnyX.dmg"
   name "OnyX"
   desc "Verify system files structure, run miscellaneous maintenance and more"
   homepage "https://www.titanium-software.fr/en/onyx.html"
