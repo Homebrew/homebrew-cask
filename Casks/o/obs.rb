bbc5e28a11cb0bb85fb4c26e44511d3aeac0bf8c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-31 19:00:32 +0000

    obs 31.0.3

--- Casks/o/obs.rb
+++ Casks/o/obs.rb
@@ -2,9 +2,9 @@ cask "obs" do
   arch arm: "Apple", intel: "Intel"
   livecheck_folder = on_arch_conditional arm: "arm64", intel: "x86_64"
 
-  version "31.0.2"
-  sha256 arm:   "d028ec5471869142e60c3ae886e0d2d333b14ce6001a87effe545a1754cfff24",
-         intel: "d4ff2577c36c0b1c1882a0ca8b22ff76cba480eef1de47d555335c5baa3c062f"
+  version "31.0.3"
+  sha256 arm:   "29fe18704eb5134e3cc1e486dcfd2e3c1e69df06b918a20529c2e67cb9d9590d",
+         intel: "1a91c95a05b7cfde6573b5d7c10aa00ef8f7a961e60fb38dd3167ac577657977"
 
   url "https://cdn-fastly.obsproject.com/downloads/OBS-Studio-#{version}-macOS-#{arch}.dmg"
   name "OBS"
