fc0c62247238279689e5425ba5d151189bc210df
Klaus Hipp <khipp@users.noreply.github.com>
2025-03-31 15:13:26 +0200

    deeper: update livecheck, consolidate url

--- Casks/d/deeper.rb
+++ Casks/d/deeper.rb
@@ -1,64 +1,50 @@
 cask "deeper" do
   sha256 :no_check
 
-  on_el_capitan do
+  on_el_capitan :or_older do
     version "2.1.4"
-
-    url "https://www.titanium-software.fr/download/1011/Deeper.dmg"
   end
   on_sierra do
     version "2.2.3"
-
-    url "https://www.titanium-software.fr/download/1012/Deeper.dmg"
   end
   on_high_sierra do
     version "2.3.3"
-
-    url "https://www.titanium-software.fr/download/1013/Deeper.dmg"
   end
   on_mojave do
     version "2.4.8"
-
-    url "https://www.titanium-software.fr/download/1014/Deeper.dmg"
   end
   on_catalina do
     version "2.6.0"
-
-    url "https://www.titanium-software.fr/download/1015/Deeper.dmg"
   end
   on_big_sur do
     version "2.7.1"
-
-    url "https://www.titanium-software.fr/download/11/Deeper.dmg"
   end
   on_monterey do
     version "2.8.0"
-
-    url "https://www.titanium-software.fr/download/12/Deeper.dmg"
   end
   on_ventura do
     version "2.9.2"
-
-    url "https://www.titanium-software.fr/download/13/Deeper.dmg"
   end
   on_sonoma do
     version "3.0.9"
-
-    url "https://www.titanium-software.fr/download/14/Deeper.dmg"
   end
   on_sequoia :or_newer do
     version "3.1.2"
-
-    url "https://www.titanium-software.fr/download/15/Deeper.dmg"
   end
 
+  url "https://www.titanium-software.fr/download/#{MacOS.version.to_s.delete(".")}/Deeper.dmg"
   name "Deeper"
   desc "Tool to enable and disable hidden functions of Finder and other apps"
   homepage "https://www.titanium-software.fr/en/deeper.html"
 
   livecheck do
-    url :homepage
-    regex(/>\s*Deeper\s+v?(\d+(?:\.\d+)+)\s+for\s+[\w\s.-]*\s+#{MacOS.version}\s*</i)
+    url "https://www.titanium-software.fr/download/#{MacOS.version}/Deeper.plist"
+    strategy :xml do |xml|
+      version = xml.elements["//key[text()='Version']"]&.next_element&.text
+      next if version.blank?
+
+      version.strip
+    end
   end
 
   depends_on macos: [
