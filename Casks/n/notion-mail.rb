8a0e13a837093cbbbcfc73406f1143ee55708173
Ryan Barclay <mrryanbarclay@gmail.com>
2025-03-31 12:00:17 -0700

    notion-mail 0.0.32 (new cask)
    notion-mail: update homepage
    
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

---
+++ Casks/n/notion-mail.rb
@@ -0,0 +1,32 @@
+cask "notion-mail" do
+  arch arm: "arm64", intel: "universal"
+  livecheck_arch = on_arch_conditional arm: "arm64", intel: "latest"
+
+  version "0.0.32"
+  sha256 arm:   "26ba79fe30964c7627e347da0281b855b7fa38f77a12e68c148ae7a24b16e839",
+         intel: "038f144d3c087563c7b92e3feae602ddc75297b0026dfc9048ceb615239f8f34"
+
+  url "https://s3.us-west-2.amazonaws.com/mail-desktop-release.notion-static.com/Notion%20Mail-#{version}-#{arch}.zip",
+      verified: "s3.us-west-2.amazonaws.com/mail-desktop-release.notion-static.com/"
+  name "Notion Mail"
+  desc "Email client integrated with Notion workspace"
+  homepage "https://www.notion.com/product/mail"
+
+  livecheck do
+    url "https://s3-us-west-2.amazonaws.com/mail-desktop-release.notion-static.com/notion-mail-#{livecheck_arch}-mac.yml"
+    strategy :electron_builder
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
+  app "Notion Mail.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/notion.mail.id.sfl*",
+    "~/Library/Application Support/Notion Mail",
+    "~/Library/Logs/Notion Mail",
+    "~/Library/Preferences/notion.mail.id.*",
+    "~/Library/Saved Application State/notion.mail.id.savedState",
+  ]
+end
