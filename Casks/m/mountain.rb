5eea95e356cc9c8728897fe6b2e3de090dfb6667
Klaus Hipp <khipp@users.noreply.github.com>
2025-04-01 10:59:10 +0200

    mountain: deprecate

--- Casks/m/mountain.rb
+++ Casks/m/mountain.rb
@@ -7,10 +7,7 @@ cask "mountain" do
   desc "Display notifications when mounting/unmounting volumes"
   homepage "https://appgineers.de/mountain/"
 
-  livecheck do
-    url "https://appgineers.de/mountain/files/mountaincast.xml"
-    strategy :sparkle, &:short_version
-  end
+  deprecate! date: "2025-03-31", because: :unmaintained
 
   app "Mountain.app"
 
