f18c9132fbc24e266ce8df67e5b44c1b42bf24b1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-01 16:08:23 +0000

    manictime 2025.1.1.0

--- Casks/m/manictime.rb
+++ Casks/m/manictime.rb
@@ -1,9 +1,9 @@
 cask "manictime" do
   arch arm: "arm64", intel: "x64"
 
-  version "2025.1.0.2"
-  sha256 arm:   "3ccb4ae4686adcd5089af808e9ece807772f93cda3c23700f3a7fa8d128e8120",
-         intel: "a56e6de5f713c024c8567c7b60d8f74d2ecc2fb39b30f63a75d557bafba95cfb"
+  version "2025.1.1.0"
+  sha256 arm:   "d7a7b45c0235cb132de4fa2e6c2f2a9fa07e4f626564f5fb90735c39d66409a9",
+         intel: "6d0069e17cd456a69d2e90855166a50addb580171d79c8edf9edad4bcb919670"
 
   url "https://cdn.manictime.com/setup/mac/v#{version.dots_to_underscores}/manictime-#{version}-osx-#{arch}.dmg"
   name "ManicTime"
