3160e0cb4dabe86ae3ff920321e1206c4c19c88c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-01 18:57:33 +0000

    leader-key 1.12.0

--- Casks/l/leader-key.rb
+++ Casks/l/leader-key.rb
@@ -1,6 +1,6 @@
 cask "leader-key" do
-  version "1.11.0"
-  sha256 "c61521ce11ceaff1fc074a3de6ff7fb3f91721bb71e057115301f486b3260b67"
+  version "1.12.0"
+  sha256 "9d66a67bad858191223f736fde0127f7eb80d8a6c001aaf95cef39ba6efc0024"
 
   url "https://github.com/mikker/LeaderKey.app/releases/download/v#{version}/Leader.Key.app.zip"
   name "Leader Key"
