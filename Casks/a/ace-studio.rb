facbbc374cf5b991eed014c1055e70dc776cb954
Klaus Hipp <khipp@users.noreply.github.com>
2025-04-01 12:25:41 +0200

    ace-studio 1.9.9

--- Casks/a/ace-studio.rb
+++ Casks/a/ace-studio.rb
@@ -1,9 +1,14 @@
 cask "ace-studio" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.9.8,1022"
-  sha256 arm:   "3a74a78bbae6163c67bdfc989a1545223efce468e4e86c92175bf734da735eaf",
-         intel: "a405589c5ff05331223fe6bef682cfb659bb9931f57a29a7ca005548e54cdfdf"
+  on_arm do
+    version "1.9.9,1058"
+    sha256 "374f40846755150849b5b7a75fce595889f02c8e85e27f3c11e2bea72f3b73e3"
+  end
+  on_intel do
+    version "1.9.9,1050"
+    sha256 "4698c3efd3b6420604bcc4e81f3be9ef9e2132778e5d45a07ae5df13c40396e5"
+  end
 
   url "https://as-api-ws-cdn-1.tdacestudio.com/web/app/ACE_Studio_#{version.csv.first}_#{version.csv.second}_#{arch}_general.dmg",
       verified: "as-api-ws-cdn-1.tdacestudio.com/web/app/"
@@ -14,8 +19,13 @@ cask "ace-studio" do
   livecheck do
     url "https://as-api.tdacestudio.com/api/as/web/app/v2/previous?cl=general"
     regex(/ACE[._-]Studio[._-]v?(\d+(?:[._]\d+)+)[._-]#{arch}[._-]general\.dmg/i)
-    strategy :page_match do |page, regex|
-      page.scan(regex)&.map { |match| match[0].tr("_", ",") }
+    strategy :json do |json, regex|
+      json.dig("data", "resources")&.map do |resource|
+        match = resource["resource_url"]&.match(regex)
+        next if match.blank?
+
+        match[1].tr("_", ",")
+      end
     end
   end
 
