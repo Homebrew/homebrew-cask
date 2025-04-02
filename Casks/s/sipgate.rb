f13affa4fdd9827ac36a40abe8fee55d61745af5
Sam Ford <1584702+samford@users.noreply.github.com>
2025-04-02 10:59:45 -0400

    sipgate 2.8.4
    `sipgate-clinq` is in the autobump list but the update for 2.8.4 is
    failing because sipgate CLINQ is now simply named "sipgate". This
    updates the cask to the latest version and renames it to `sipgate`.

--- Casks/s/sipgate.rb
+++ Casks/s/sipgate.rb
@@ -1,10 +1,10 @@
-cask "sipgate-clinq" do
-  version "2.7.1"
-  sha256 "3ee129ad11dc84548931d2b6c5d862daad3898400a6474de60e5438db8effd10"
+cask "sipgate" do
+  version "2.8.4"
+  sha256 "e17bdf2270b36b3a9bad19bf0404a69f57fa79c7e96d7f04303d0de3bb70dd5d"
 
-  url "https://s3-eu-central-1.amazonaws.com/desktop.download.sipgate.com/sipgate%20CLINQ-#{version}.zip",
+  url "https://s3-eu-central-1.amazonaws.com/desktop.download.sipgate.com/sipgate-#{version}.zip",
       verified: "s3-eu-central-1.amazonaws.com/desktop.download.sipgate.com/"
-  name "Sipgate CLINQ"
+  name "sipgate"
   desc "Softphone for making telephone calls over the internet"
   homepage "https://www.sipgate.de/app"
 
@@ -16,10 +16,14 @@ cask "sipgate-clinq" do
   auto_updates true
   depends_on macos: ">= :catalina"
 
-  app "sipgate CLINQ.app"
+  app "sipgate.app"
 
   zap trash: [
     "~/Library/Application Support/sipgate-desktop",
+    "~/Library/Caches/com.sipgate.desktop",
+    "~/Library/Caches/com.sipgate.desktop.ShipIt",
+    "~/Library/Caches/sipgate-desktop-updater",
+    "~/Library/HTTPStorages/com.sipgate.desktop",
     "~/Library/Logs/sipgate-desktop",
     "~/Library/Preferences/com.sipgate.desktop.plist",
     "~/Library/Saved Application State/com.sipgate.desktop.savedState",
