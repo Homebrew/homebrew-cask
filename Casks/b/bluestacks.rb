eeef06969554c6cd7e80b1ee1b0465bdbbc6c632
Klaus Hipp <khipp@users.noreply.github.com>
2025-04-02 11:31:02 +0200

    bluestacks 5.21.705.7515

--- Casks/b/bluestacks.rb
+++ Casks/b/bluestacks.rb
@@ -1,14 +1,14 @@
 cask "bluestacks" do
-  version "10.4.0.6907,f1d20c10cba982dc3ee4b6d975380943"
-  sha256 "b3c5220119bbd29bfcd9f7f3eea4b1054e9cbb7ca8be134b3647aebf64210c00"
+  version "5.21.705.7515,4609e383645949ee9e314c38b2d3d9b4"
+  sha256 "8b3b976abd34ae1f5ed5db9cfdc0ddb3e356af4221c7bea7f6948f022e1588d3"
 
-  url "https://mac-cdn.bluestacks.com/public/app-player/mac/nxt_mac/#{version.csv.first}/#{version.csv.second}/md5/x64/BlueStacks_X_#{version.csv.first}.dmg"
+  url "https://ak-build.bluestacks.com/public/app-player/mac/nxt_mac2/#{version.csv.first}/#{version.csv.second}/BlueStacksInstaller_#{version.csv.first}.pkg"
   name "BlueStacks"
   desc "Mobile gaming platform"
   homepage "https://www.bluestacks.com/"
 
   livecheck do
-    url "https://cloud.bluestacks.com/api/getdownloadnow?platform=mac&mac_version=#{MacOS.full_version}"
+    url "https://cloud.bluestacks.com/api/getdownloadnow?platform=mac"
     regex(%r{/(\d+(?:\.\d+)*)/([^/]+)/}i)
     strategy :header_match do |headers, regex|
       match = headers["location"]&.match(regex)
@@ -18,9 +18,17 @@ cask "bluestacks" do
     end
   end
 
-  depends_on macos: ">= :high_sierra"
+  depends_on arch: :arm64
+  depends_on macos: ">= :big_sur"
 
-  app "BlueStacks X.app"
+  pkg "BlueStacksInstaller_#{version.csv.first}.pkg"
+
+  uninstall launchctl: "com.now.gg.BlueStacks.cleanup",
+            pkgutil:   [
+              "com.now.gg.BlueStacks",
+              "com.now.gg.BlueStacksMIM",
+            ],
+            delete:    "/Applications/BlueStacksMIM.app"
 
   zap trash: [
         "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.bluestacks.bluestacks-support-tool.sfl*",
