0a58b73205a2891d8f391353405b2953435aa227
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-02 15:34:12 +0000

    arduino-ide 2.3.5

--- Casks/a/arduino-ide.rb
+++ Casks/a/arduino-ide.rb
@@ -1,9 +1,9 @@
 cask "arduino-ide" do
   arch arm: "ARM64", intel: "64bit"
 
-  version "2.3.4"
-  sha256 arm:   "bc3baaf66c5ac386c9bc7963796396b894fbeddb851e7eee75dabebc83c5c633",
-         intel: "ba8e522090f29d6715f78137fcab4c5857fbadd63f4376a26ec4661a6e5fc0e2"
+  version "2.3.5"
+  sha256 arm:   "fb55a8352f48928c18371a1300168b616b837c2a3abca02fdb458836fc3aef52",
+         intel: "b0014ae925ca6d3f9062a2858affc1c182be12efc269040e5e46720c9fc5611b"
 
   url "https://github.com/arduino/arduino-ide/releases/download/#{version}/arduino-ide_#{version}_macOS_#{arch}.dmg",
       verified: "github.com/arduino/arduino-ide/"
