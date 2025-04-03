e9cbdbcf30aa7874daad1c8a2d4bb8aa15d93d1a
Klaus Hipp <khipp@users.noreply.github.com>
2025-04-03 11:01:03 +0200

    lunasea: deprecate

--- Casks/l/lunasea.rb
+++ Casks/l/lunasea.rb
@@ -2,14 +2,10 @@ cask "lunasea" do
   on_mojave :or_older do
     version "10.2.2"
     sha256 "61ef622b70c31550ec6f700eba1e938c23fb97717344e8876d56e2d856a51be4"
-
-    livecheck do
-      skip "Legacy version"
-    end
   end
   on_catalina :or_newer do
-    version "10.2.6"
-    sha256 "6debb79ca5657161c63b93266455e5b4a48c97856dd65e93517231dd839cf079"
+    version "11.0.0"
+    sha256 "fa4ecb5bdf57d6f1326e356e248232040f1b2d0d409ea93ac96dc560eded980c"
   end
 
   url "https://github.com/JagandeepBrar/LunaSea/releases/download/v#{version}/lunasea-macos-amd64.zip",
@@ -17,6 +13,8 @@ cask "lunasea" do
   name "LunaSea"
   desc "Self-hosted controller built using the Flutter framework"
   homepage "https://www.lunasea.app/"
+
+  deprecate! date: "2025-04-02", because: :discontinued
 
   app "LunaSea.app"
 
