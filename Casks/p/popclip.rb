89ec64f2b01040621a75ee801ee142349cb7486b
Nick Moore <nick@pilotmoon.com>
2025-04-04 21:23:56 +0100

    Update popclip.rb to ignore beta updates
    Updates the livecheck to only include items on the default channel.

--- Casks/p/popclip.rb
+++ Casks/p/popclip.rb
@@ -8,9 +8,13 @@ cask "popclip" do
   desc "Used to access context-specific actions when text is selected"
   homepage "https://www.popclip.app/"
 
+  # The Sparkle feed can contain items on the "Beta" channel, so we restrict
+  # matching to the default channel.
   livecheck do
     url "https://softwareupdate.pilotmoon.com/update/popclip/appcast.xml"
-    strategy :sparkle, &:short_version
+    strategy :sparkle do |items|
+      items.find { |item| item.channel.nil? }&.short_version
+    end
   end
 
   auto_updates true
