93dd294b504df72083ac68938d9dc429a543ebed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-04 09:42:32 +0000

    downie 4.9.12

--- Casks/d/downie.rb
+++ Casks/d/downie.rb
@@ -1,6 +1,6 @@
 cask "downie" do
-  version "4.9.11,4933"
-  sha256 "a3568af704af217fc34ab9505b90bb2c7a2c2e32ece2c42490fabf63d19b7776"
+  version "4.9.12,4939"
+  sha256 "d993e6796b0981ba44c74ce7f27eac924b73da9928873d30ca49a900eecbde80"
 
   url "https://software.charliemonroe.net/trial/downie/v#{version.major}/Downie_#{version.major}_#{version.csv.second}.dmg"
   name "Downie"
