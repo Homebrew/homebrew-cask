8200344d0ff70bd92fac0f785255d8695a2cd366
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-04 09:56:03 +0000

    onekey 5.7.0

--- Casks/o/onekey.rb
+++ Casks/o/onekey.rb
@@ -1,9 +1,9 @@
 cask "onekey" do
   arch arm: "arm64", intel: "x64"
 
-  version "5.6.1"
-  sha256 arm:   "280b3c9d120f5bc8fc66c898856388db54ba7693ee40d26537f10c8854d549cc",
-         intel: "a57a5c21aafd2ee2856e2eef8a5a603d76db5552c604246557a9fe00eca42808"
+  version "5.7.0"
+  sha256 arm:   "0fbb5f1d8323718be81ad9389d571029d10a0b56c269ce34c8a782a9c77b0ca5",
+         intel: "e6220cf9dc2c6eb8c5734ec251da13ce0cc878b6cbd31f3653744871aa703a9b"
 
   url "https://github.com/OneKeyHQ/app-monorepo/releases/download/v#{version}/OneKey-Wallet-#{version}-mac-#{arch}.dmg",
       verified: "github.com/OneKeyHQ/app-monorepo/"
