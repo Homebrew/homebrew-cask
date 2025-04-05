b47308115548a2bcc1f29d48a27213f4830f3fb9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-05 06:35:36 +0000

    actual 25.4.0

--- Casks/a/actual.rb
+++ Casks/a/actual.rb
@@ -1,9 +1,9 @@
 cask "actual" do
   arch arm: "arm64", intel: "x64"
 
-  version "25.3.1"
-  sha256 arm:   "a1b8423feac1575b8742be7d497c2f9f5b360d61eb5623937a116c6a344eadb9",
-         intel: "260f34231611d70f17e54c2e73a396e3021a63c2217b52525a6a1d85acdd2391"
+  version "25.4.0"
+  sha256 arm:   "36651c32a75d86b11685175b4be700871276d44a23714ec3dd0169909dcb8fb9",
+         intel: "cc025fcf4979a8fd5e3c3245135f693d0c8cf61e2756b264d3e59272718325f0"
 
   url "https://github.com/actualbudget/actual/releases/download/v#{version}/Actual-mac-#{arch}.dmg",
       verified: "github.com/actualbudget/actual/"
