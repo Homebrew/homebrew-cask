2c24254413f12ea49b1c104f0a5ab3b8572f31ea
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-04-05 21:51:48 -0400

    clicker-for-youtube: add auto_updates true, update zap

--- Casks/c/clicker-for-youtube.rb
+++ Casks/c/clicker-for-youtube.rb
@@ -12,6 +12,7 @@ cask "clicker-for-youtube" do
     strategy :sparkle, &:short_version
   end
 
+  auto_updates true
   depends_on macos: ">= :sierra"
 
   app "Clicker for YouTube.app"
@@ -21,6 +22,8 @@ cask "clicker-for-youtube" do
     "~/Library/Application Scripts/com.dbklabs.clicker-for-youtube.Open-in-Clicker-for-YouTube",
     "~/Library/Caches/com.dbklabs.clicker-for-youtube",
     "~/Library/Containers/com.dbklabs.clicker-for-youtube.Open-in-Clicker-for-YouTube",
+    "~/Library/HTTPStorages/com.dbklabs.clicker-for-youtube",
+    "~/Library/HTTPStorages/com.dbklabs.clicker-for-youtube.binarycookies",
     "~/Library/Preferences/com.dbklabs.clicker-for-youtube.plist",
     "~/Library/WebKit/com.dbklabs.clicker-for-youtube",
   ]
