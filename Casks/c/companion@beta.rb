d49142ad1a3e08aa3388a54d20efbc26f7002467
Bevan Kay <email@bevankay.me>
2025-04-08 22:00:46 +1000

    companion@beta 4.0.0 (new cask)
    companion@beta: refine livecheck
    
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

---
+++ Casks/c/companion@beta.rb
@@ -0,0 +1,37 @@
+# typed: false
+
+cask "companion@beta" do
+  arch arm: "arm64", intel: "x64"
+  livecheck_arch = on_arch_conditional arm: "arm", intel: "intel"
+
+  version "4.0.0+7915-main-6b9a451a"
+  sha256 arm:   "2b9acc41700e5df5706f38df95f4995947048fc86406e22e71509ff7936ec618",
+         intel: "b6df5a2e809a22a3a7fd7496420abb101db162bb5783e391bc84315aa3062573"
+
+  url "https://s4.bitfocus.io/builds/companion/companion-mac-#{arch}-#{version}.dmg"
+  name "Bitfocus Companion"
+  desc "Streamdeck extension and emulation software"
+  homepage "https://bitfocus.io/companion"
+
+  livecheck do
+    url "https://api.bitfocus.io/v1/product/companion/packages?branch=beta&limit=150"
+    strategy :json do |json|
+      json["packages"]&.select { |c| c["target"] == "mac-#{livecheck_arch}" }
+                      &.map { |c| c["version"] }
+    end
+  end
+
+  depends_on macos: ">= :big_sur"
+
+  # Companion beta does not share preferences with the stable branch, so can be installed side by side
+  app "Companion.app", target: "Companion Beta.app"
+
+  zap trash: [
+    "~/Library/Application Support/@companion-app",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/test-companion.bitfocus.no.sfl*",
+    "~/Library/Application Support/companion",
+    "~/Library/Application Support/companion-launcher",
+    "~/Library/Preferences/companion-nodejs",
+    "~/Library/Preferences/test-companion.bitfocus.no.plist",
+  ]
+end
