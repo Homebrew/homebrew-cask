aa566deb8e1e6fba3b0bf1ee74475df4ab88dc02
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-08 16:28:27 +0000

    pdl 0.6.0

--- Casks/p/pdl.rb
+++ Casks/p/pdl.rb
@@ -1,6 +1,6 @@
 cask "pdl" do
-  version "0.5.1"
-  sha256 "2f4df261801454c704b31a041c2a22693f70d2377e750bc80bce050f3078c36e"
+  version "0.6.0"
+  sha256 "27c2f408adff296295bca5c8bde88003c4bf3fc0dcd4c53b298629c649ba8837"
 
   url "https://github.com/IBM/prompt-declaration-language/releases/download/v#{version}/PDL_#{version}_universal.dmg",
       verified: "github.com/IBM/prompt-declaration-language/"
