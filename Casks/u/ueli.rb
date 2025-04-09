8a61d4422e4bb86ea637304bbabffcdffb31edc4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-09 10:14:48 +0000

    ueli 9.21.0

--- Casks/u/ueli.rb
+++ Casks/u/ueli.rb
@@ -1,9 +1,9 @@
 cask "ueli" do
   arch arm: "-arm64"
 
-  version "9.20.0"
-  sha256 intel: "32c8196d7bc92d0bdf4d18ebfe1474db3c1825d32f76d0d4dfba46f0875f05d6",
-         arm:   "ca98bed7bea4bb3ff5724342850dfac34ff0e57d024217914fa12f61f83668ac"
+  version "9.21.0"
+  sha256 intel: "7bc3965dcf61ad016053b9d95d3260c213a8b035ca09a477f0854feb01d81a2c",
+         arm:   "5a3b63038267478f638018673986b5638b664e770c8fc833a1709a3ea56826cc"
 
   url "https://github.com/oliverschwendener/ueli/releases/download/v#{version}/Ueli-#{version}#{arch}.dmg",
       verified: "github.com/oliverschwendener/ueli/"
