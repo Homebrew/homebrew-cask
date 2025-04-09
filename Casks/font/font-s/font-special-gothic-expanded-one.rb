efa678159412bdd16b9312e4906811a71dab6c1b
Klaus Hipp <9824526+khipp@users.noreply.github.com>
2025-04-09 17:28:39 +0000

    font-special-gothic-expanded-one: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-s/font-special-gothic-expanded-one.rb
+++ Casks/font/font-s/font-special-gothic-expanded-one.rb
@@ -2,9 +2,10 @@ cask "font-special-gothic-expanded-one" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/specialgothicexpandedone/SpecialGothicExpandedOne-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/specialgothicexpandedone/SpecialGothicExpandedOne-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Special Gothic Expanded One"
-  homepage "https://github.com/AlistairMcCready/Special-Gothic"
+  homepage "https://fonts.google.com/specimen/Special+Gothic+Expanded+One"
 
   font "SpecialGothicExpandedOne-Regular.ttf"
 
