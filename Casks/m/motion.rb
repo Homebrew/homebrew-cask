8d9096b219702c11f43c9c60a2b4e828fa047419
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-09 19:14:37 +0000

    motion 0.99.0

--- Casks/m/motion.rb
+++ Casks/m/motion.rb
@@ -1,9 +1,9 @@
 cask "motion" do
   arch arm: "aarch64", intel: "amd64"
 
-  version "0.98.0"
-  sha256 arm:   "d430674077ad61af7dcf602fb888d5fd980994f428f3fc5ca44023821cb1d523",
-         intel: "3aaa2da54ba655b9513a2ef412f9e9bfa94a73e4bc7c08c82609f519905173c6"
+  version "0.99.0"
+  sha256 arm:   "46b4b271c9dc98655e5753d7b87685e36e0c7014cced2f933acc9e3ad0c7d21a",
+         intel: "ab6a2b6ce0a045e073c5cc98c705138cc9a14257f202ce237527f6556f62c860"
 
   url "https://github.com/usemotion/desktopapp/releases/download/#{version}/motion-#{version}-mac-#{arch}.zip",
       verified: "github.com/usemotion/desktopapp/"
