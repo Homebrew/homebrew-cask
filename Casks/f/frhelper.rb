19f0fcdda9e2411f0b1a23e7788450713f59a648
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2025-04-09 10:41:23 +0800

    frhelper.rb: add livecheck

--- Casks/f/frhelper.rb
+++ Casks/f/frhelper.rb
@@ -1,14 +1,19 @@
 cask "frhelper" do
-  version :latest
-  sha256 :no_check
+  version "11.6.0"
+  sha256 "9d8aa256c56868bde63f7a09cf53a271aa10a571ac1979d5e70f7f7b173363fc"
 
-  url "https://static.eudic.net/pkg/fhmac.dmg",
+  url "https://static.eudic.net/pkg/fhmac.dmg?v=#{version}",
       user_agent: :fake
   name "Frhelper"
   name "法语助手"
   desc "French-Chinese dictionary and learning tool"
   homepage "https://www.eudic.net/v4/fr/app/frhelper"
 
+  livecheck do
+    url :homepage
+    regex(/应用版本：(\d+(\.\d+)+)/i)
+  end
+  
   depends_on macos: ">= :high_sierra"
 
   app "Frhelper.app"
