aef01e182954e4dcf00647c4b746893219e4717d
Klaus Hipp <9824526+khipp@users.noreply.github.com>
2025-04-09 17:28:39 +0000

    font-special-gothic-condensed-one: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-s/font-special-gothic-condensed-one.rb
+++ Casks/font/font-s/font-special-gothic-condensed-one.rb
@@ -2,9 +2,10 @@ cask "font-special-gothic-condensed-one" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/specialgothiccondensedone/SpecialGothicCondensedOne-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/specialgothiccondensedone/SpecialGothicCondensedOne-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Special Gothic Condensed One"
-  homepage "https://github.com/AlistairMcCready/Special-Gothic"
+  homepage "https://fonts.google.com/specimen/Special+Gothic+Condensed+One"
 
   font "SpecialGothicCondensedOne-Regular.ttf"
 
