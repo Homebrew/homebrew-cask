a307a39310d210c47549443b08a7e51d3e3b7d66
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-09 13:10:24 +0000

    ultrastardeluxe 2025.4.0

--- Casks/u/ultrastardeluxe.rb
+++ Casks/u/ultrastardeluxe.rb
@@ -1,9 +1,9 @@
 cask "ultrastardeluxe" do
   arch arm: "ARM", intel: "x86"
 
-  version "2025.3.0"
-  sha256 arm:   "de9ef51ceaabe5f56c7a5aa356d31d1dd5b4fa9385e71202cda5b11b539ffcd5",
-         intel: "0da90d96bb20145408c04400aa7ef71a3a5e471305505032532b2c83b1408683"
+  version "2025.4.0"
+  sha256 arm:   "991e4fe2479b730f73f15f400a56dfbd2d7a6fbbd5d6ae32d97d131d761e5983",
+         intel: "b35a4108c67c03543c5151574c629cf23921882ca63926c11d02b1c0b1b70836"
 
   url "https://github.com/UltraStar-Deluxe/USDX/releases/download/v#{version}/UltraStarDeluxe-mac-#{arch}-#{version}.dmg",
       verified: "github.com/UltraStar-Deluxe/USDX/"
