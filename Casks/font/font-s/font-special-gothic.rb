061f88d08441af9c11368ff0c1d733232e6ae4dd
Klaus Hipp <9824526+khipp@users.noreply.github.com>
2025-04-09 17:28:39 +0000

    font-special-gothic: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-s/font-special-gothic.rb
+++ Casks/font/font-s/font-special-gothic.rb
@@ -2,9 +2,10 @@ cask "font-special-gothic" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/specialgothic/SpecialGothic-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/specialgothic/SpecialGothic-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Special Gothic"
-  homepage "https://github.com/AlistairMcCready/Special-Gothic"
+  homepage "https://fonts.google.com/specimen/Special+Gothic"
 
   font "SpecialGothic-Regular.ttf"
 
