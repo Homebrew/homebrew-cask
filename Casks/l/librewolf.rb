ab964c542ece61a4cd17af62813963abf798740d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-09 16:26:42 +0000

    librewolf 137.0.1

--- Casks/l/librewolf.rb
+++ Casks/l/librewolf.rb
@@ -1,9 +1,9 @@
 cask "librewolf" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "137.0,3"
-  sha256 arm:   "3282e53afdb55d39fc822dd9a4daccd2a3716ed02fac79c9f9f5b9c7ac941d72",
-         intel: "a55833135149c9a66bb8cd022d8cd3ec07a5bab73d83d9be957b8b67fe69aa06"
+  version "137.0.1,1"
+  sha256 arm:   "da02366abf24bd5d2886bf7b0c9523ae83d1dfd62b6be503b34c0a1ce380d1ac",
+         intel: "d449fb394156ea07e81244667f8f943b0818e9ca9a075f78cb8b20d6e729129d"
 
   url "https://gitlab.com/api/v4/projects/44042130/packages/generic/librewolf/#{version.tr(",", "-")}/librewolf-#{version.tr(",", "-")}-macos-#{arch}-package.dmg",
       verified: "gitlab.com/api/v4/projects/44042130/packages/generic/librewolf/"
