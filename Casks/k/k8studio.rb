97633602fdc9b5350af9103b1ad9d438a08136db
Klaus Hipp <khipp@users.noreply.github.com>
2025-04-10 08:09:53 +0200

    k8studio 2.1.0

--- Casks/k/k8studio.rb
+++ Casks/k/k8studio.rb
@@ -1,9 +1,9 @@
 cask "k8studio" do
   arch arm: "-arm64"
 
-  version "2.0.0"
-  sha256 arm:   "0aae3c3328f392057417c0d62e53155929bbef0b73f5ed5d524504bd7a668a06",
-         intel: "3e63ffe48ff6b790bfc7c4a58e78486d0c3be7e0d2c97ac80cc4bf85631d9d78"
+  version "2.1.0"
+  sha256 arm:   "c20060a92770c3ebf22d185a95181637923a3f413b793ed8237b404b9709606f",
+         intel: "0d3ed9153c6266f46604b898f386eba05267466f493f62bac796a59b376e7855"
 
   url "https://github.com/guiqui/k8Studio/releases/download/v#{version}/K8Studio-#{version}#{arch}.dmg",
       verified: "github.com/guiqui/k8Studio/"
