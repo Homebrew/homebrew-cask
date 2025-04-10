b5aa3e6c0c101ed8cdda342678c5bd5767acf6fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-10 15:40:18 +0000

    djstudio 3.1.18

--- Casks/d/djstudio.rb
+++ Casks/d/djstudio.rb
@@ -13,9 +13,9 @@ cask "djstudio" do
   on_catalina :or_newer do
     arch arm: "-arm64"
 
-    version "3.1.17"
-    sha256 arm:   "59f1509a79a058b8a3a856587b36203835b9c2bf642584197a2c17063a3b53d4",
-           intel: "fdf287c2ca1a8c66b96b11dfed5866481dbbf86cb916dac54fc617fb6bbe2153"
+    version "3.1.18"
+    sha256 arm:   "a142118b99bf3ffc7b1cb53830045ca225bdefa6dd54ee4a9c6a9fe48db8fe53",
+           intel: "8bc6366f2a2e3d442ca079f0f58e66ad3fe91052af63bd3d959cbb7f14ff0a72"
 
     url "https://github.com/AppMachine/dj-studio-app-updates/releases/download/v#{version}/DJ.Studio-#{version}#{arch}.dmg",
         verified: "github.com/AppMachine/dj-studio-app-updates/"
