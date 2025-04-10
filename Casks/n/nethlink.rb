7b2d2ebeb580ace2e6ce86723f0c5bd16ab4cb47
Edoardo Spadoni <edoardo.spadoni@gmail.com>
2025-04-10 10:28:21 +0200

    Update nethlink.rb

--- Casks/n/nethlink.rb
+++ Casks/n/nethlink.rb
@@ -2,7 +2,7 @@ cask "nethlink" do
   version "1.1.1"
   sha256 "c3fc2f4589d15b5be3854e62f918c4ce34a65ececd53da8e237ebacb97fcc144"
 
-  url "https://github.com/NethServer/nethlink/releases/download/v1.1.1/nethlink-1.1.1.dmg"
+  url "https://github.com/NethServer/nethlink/releases/download/v#{version}/nethlink-#{version}.dmg"
   name "NethLink"
   desc "Link NethServer systems and provide remote access tools"
   homepage "https://github.com/NethServer/nethlink"
