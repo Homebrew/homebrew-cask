73a78bd33481429c3c4b938b8fd4f60ac5421ebd
Klaus Hipp <khipp@users.noreply.github.com>
2025-04-10 08:02:50 +0200

    franz 5.11.0

--- Casks/f/franz.rb
+++ Casks/f/franz.rb
@@ -1,9 +1,9 @@
 cask "franz" do
   arch arm: "-arm64"
 
-  version "5.10.0"
-  sha256 arm:   "97fac95cdc2c4080c6f9a0659befae61e63c6f2332aa8aa2b75dcc07d4d00a82",
-         intel: "dc7bef96dabd0b86199430cd7699b17dae6cbc6eae5f6962b14d93381d0df853"
+  version "5.11.0"
+  sha256 arm:   "3390a32cab5510b34e2237f2215fc82086d1203cd621cdb8b5bd2f809db2297f",
+         intel: "ca1ac6fb5c51af0dfd63d89490bb557fe648eeb7f40e9af2f186d108210977b6"
 
   url "https://github.com/meetfranz/franz/releases/download/v#{version}/franz-#{version}#{arch}.dmg",
       verified: "github.com/meetfranz/franz/"
