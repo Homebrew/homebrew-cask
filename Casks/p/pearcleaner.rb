fea6ec7d1e71de9ba37cc842e6d996968fb55581
BingoKingo <24961267+BingoKingo@users.noreply.github.com>
2025-04-10 18:03:51 +0800

    pearcleaner: add cli symlink

--- Casks/p/pearcleaner.rb
+++ Casks/p/pearcleaner.rb
@@ -12,6 +12,7 @@ cask "pearcleaner" do
   depends_on macos: ">= :ventura"
 
   app "Pearcleaner.app"
+  binary "#{appdir}/Pearcleaner.app/Contents/MacOS/Pearcleaner"
 
   uninstall launchctl:  "com.alienator88.PearcleanerSentinel*",
             quit:       "com.alienator88.Pearcleaner",
