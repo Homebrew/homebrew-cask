a40fe20ffd964aa214242a57f47a07be46595520
Klaus Hipp <khipp@users.noreply.github.com>
2025-04-11 12:09:30 +0200

    telegram-desktop@beta 5.13.1

--- Casks/t/telegram-desktop@beta.rb
+++ Casks/t/telegram-desktop@beta.rb
@@ -1,18 +1,12 @@
 cask "telegram-desktop@beta" do
-  version "5.12.6"
-  sha256 "907d2079ffb615e2d67bcb84452afb9709e9bfb0db1615edb5a09655c72de044"
+  version "5.13.1"
+  sha256 "b026d5382b3ea55d5d8d03617a7107cc61b4e1e66580263a70cdab2b6863a370"
 
-  url "https://updates.tdesktop.com/tmac/tsetup.#{version}.beta.dmg",
-      verified: "updates.tdesktop.com/tmac/"
+  url "https://github.com/telegramdesktop/tdesktop/releases/download/v#{version}/tsetup.#{version}.dmg",
+      verified: "github.com/telegramdesktop/tdesktop/"
   name "Telegram Desktop"
   desc "Desktop client for Telegram messenger"
   homepage "https://desktop.telegram.org/"
-
-  livecheck do
-    url "https://telegram.org/dl/desktop/mac?beta=1"
-    regex(/tsetup[._-]v?(\d+(?:\.\d+)+)[._-]beta\.dmg/i)
-    strategy :header_match
-  end
 
   auto_updates true
   conflicts_with cask: "telegram-desktop"
