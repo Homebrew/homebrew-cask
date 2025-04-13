4709eb02e1b872e77808555ec2a35fe2b13de1f8
Klaus Hipp <khipp@users.noreply.github.com>
2025-04-13 11:50:52 +0200

    qutebrowser 3.5.0

--- Casks/q/qutebrowser.rb
+++ Casks/q/qutebrowser.rb
@@ -1,9 +1,9 @@
 cask "qutebrowser" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "3.4.0"
-  sha256 arm:   "1f9bbcabfc23c25c79a70fd2d78c157b6f931318a80fa3e0719661757a1b5ea1",
-         intel: "f9490294c854f6da9015e38e6a9d7a6c35f28b6451b32915fe166b3215047dd3"
+  version "3.5.0"
+  sha256 arm:   "89bede17bb26cd0e72cd4f2d8a55b42109133c8ad358a358e8bd1816489f05cd",
+         intel: "f2405d1c672b02dfdbdfc754dae9bdd1c14bc1e97602e7a339d35378eb63d816"
 
   url "https://github.com/qutebrowser/qutebrowser/releases/download/v#{version}/qutebrowser-#{version}-#{arch}.dmg",
       verified: "github.com/qutebrowser/qutebrowser/"
