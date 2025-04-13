d4fbcf7736435a752c814a884bf9f9741ae4e075
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-13 09:56:03 +0000

    trae-cn 1.0.11320

--- Casks/t/trae-cn.rb
+++ Casks/t/trae-cn.rb
@@ -1,9 +1,9 @@
 cask "trae-cn" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.0.10717"
-  sha256 arm:   "985f64b49770741bad47ef97ab56c4b67a3e5bc9621bdb4fb4086a2e02046d34",
-         intel: "dea087c9ccafb0566799b4fb480ab2719f8a15501e38b8e81df3df99efbe6adc"
+  version "1.0.11320"
+  sha256 arm:   "c1421920fb21bd4ab6735efb7b76e885f424b0f2173308b1f569d1cca0dc6e94",
+         intel: "d46a2d93b56cf27fa1227764e9d22f6ba78cf97d5eac0225fb1383baae0d7e1e"
 
   url "https://lf-cdn.trae.com.cn/obj/trae-com-cn/pkg/app/releases/stable/#{version}/darwin/Trae%20CN-darwin-#{arch}.dmg"
   name "Trae CN"
