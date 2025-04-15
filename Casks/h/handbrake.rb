cd2b44143955a535abcb949c9ff8c5fae9ff1838
Klaus Hipp <khipp@users.noreply.github.com>
2025-04-15 15:25:46 +0200

    handbrake: update url and livecheck

--- Casks/h/handbrake.rb
+++ Casks/h/handbrake.rb
@@ -1,16 +1,17 @@
 cask "handbrake" do
+  livecheck_arch = on_arch_conditional arm: "arm64", intel: "x86_64"
+
   version "1.9.2"
   sha256 "61a57c53311a0ca23e58367f512134af99428c14ac3a62665f245fc5e46c3791"
 
-  url "https://github.com/HandBrake/HandBrake/releases/download/#{version}/HandBrake-#{version}.dmg",
-      verified: "github.com/HandBrake/HandBrake/"
+  url "https://handbrake.fr/rotation.php?file=HandBrake-#{version}.dmg&update=true"
   name "HandBrake"
   desc "Open-source video transcoder"
   homepage "https://handbrake.fr/"
 
   livecheck do
-    url :url
-    strategy :github_latest
+    url "https://handbrake.fr/appcast.#{livecheck_arch}.xml"
+    strategy :sparkle, &:short_version
   end
 
   auto_updates true
