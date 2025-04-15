e68773b82b681345ee2fb611326e69b7c2151b0d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-15 10:05:48 +0000

    iina+ 0.8.7

--- Casks/i/iina+.rb
+++ Casks/i/iina+.rb
@@ -1,6 +1,6 @@
 cask "iina+" do
-  version "0.8.6"
-  sha256 "00314a5cdcb4e4f35233d2395539bd6bd4d9af2f07f2cfbbbd3f6e3314bd91ad"
+  version "0.8.7"
+  sha256 "0bdfd0436774cd6bbe714f5505e978cbe1a6f49fcd930dcb527a3e6a6f182507"
 
   url "https://github.com/xjbeta/iina-plus/releases/download/#{version}/IINA+.#{version}.dmg"
   name "IINA+"
