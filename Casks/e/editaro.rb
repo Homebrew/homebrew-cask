49ebc73b77fe4ec785655d35fcf0d4e934b8eb79
Klaus Hipp <khipp@users.noreply.github.com>
2025-04-15 16:55:06 +0200

    editaro: add livecheck

--- Casks/e/editaro.rb
+++ Casks/e/editaro.rb
@@ -8,6 +8,15 @@ cask "editaro" do
   desc "Text editor"
   homepage "https://editaro.com/"
 
+  livecheck do
+    url "https://hazel.editaro.com/update/mac/0.0.0"
+    strategy :json do |json|
+      json["name"]
+    end
+  end
+
+  auto_updates true
+
   app "Editaro.app"
 
   zap trash: [
