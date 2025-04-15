6e46b7c1e707886e2160cc8f86ba5178c7da8125
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-15 22:09:11 +0000

    microsoft-onenote 16.96.25041326

--- Casks/m/microsoft-onenote.rb
+++ Casks/m/microsoft-onenote.rb
@@ -1,6 +1,6 @@
 cask "microsoft-onenote" do
-  version "16.95.25030928"
-  sha256 "20173c3d2a6a3404875669ec3ab71159aed204abdad7ce5d00fb9c705229d00a"
+  version "16.96.25041326"
+  sha256 "d6683c68f9a27f24728993e9588e8d3b1c952813b31ff8a1d58f99cf56129b94"
 
   url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_OneNote_#{version}_Updater.pkg"
   name "Microsoft OneNote"
