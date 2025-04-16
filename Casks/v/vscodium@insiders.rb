8ad80124081a93a73c52bb22784b77088b5f5ecd
Klaus Hipp <khipp@users.noreply.github.com>
2025-04-16 11:23:43 +0200

    vscodium@insiders: update livecheck

--- Casks/v/vscodium@insiders.rb
+++ Casks/v/vscodium@insiders.rb
@@ -1,11 +1,11 @@
 cask "vscodium@insiders" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.100.02527-insider"
-  sha256 arm:   "1435a106c6b0bb92ee2e5f678ca022fc86ee74b779784ed95973c2b457045e1e",
-         intel: "0c9f8d3e4ad9101117a1dfc9c176748ca48559f7f654b9384968e695d8f9cfdc"
+  version "1.100.02551-insider"
+  sha256 arm:   "1ccee8365c274fc5bcbbb8bce3202c2030e0a95b439002743b38ead60782beed",
+         intel: "99416fdaab5b7b4a935a53622010a1170fd2a233592766cfcdce634fb56f9794"
 
-  url "https://github.com/VSCodium/vscodium-insiders/releases/download/#{version}/VSCodium.#{arch}.#{version}.dmg",
+  url "https://github.com/VSCodium/vscodium-insiders/releases/download/#{version}/VSCodium-darwin-#{arch}-#{version}.zip",
       verified: "github.com/VSCodium/vscodium-insiders/"
   name "VSCodium"
   name "VSCodium Insiders"
@@ -13,12 +13,14 @@ cask "vscodium@insiders" do
   homepage "https://vscodium.com/"
 
   livecheck do
-    url :url
-    regex(/^v?(\d+(?:\.\d+)+.*)$/i)
-    strategy :github_latest
+    url "https://raw.githubusercontent.com/VSCodium/versions/refs/heads/master/insider/darwin/#{arch}/latest.json"
+    strategy :json do |json|
+      json["name"]
+    end
   end
 
-  depends_on macos: ">= :catalina"
+  auto_updates true
+  depends_on macos: ">= :big_sur"
 
   app "VSCodium - Insiders.app"
   binary "#{appdir}/VSCodium - Insiders.app/Contents/Resources/app/bin/codium-insiders", target: "codium-insiders"
