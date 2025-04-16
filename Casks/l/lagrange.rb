4892b1485922963758aed592e632f1c3daf8fca4
Klaus Hipp <khipp@users.noreply.github.com>
2025-04-16 07:55:18 +0200

    lagrange: update livecheck

--- Casks/l/lagrange.rb
+++ Casks/l/lagrange.rb
@@ -1,19 +1,19 @@
 cask "lagrange" do
   arch arm: "11.0-arm64", intel: "10.13-x86_64"
+  livecheck_arch = on_arch_conditional arm: "arm64", intel: "x86_64"
 
   version "1.18.5"
   sha256 arm:   "7a9535468979a1dfbd19a1bc0550dcdab14e042d487bbc5ce040e9bb8f862b10",
          intel: "5c8fcee308defd9462ac051e4f1a29a3ab2334893a4da6935663890d7ce0ce6a"
 
-  url "https://github.com/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_macos#{arch}.tbz",
-      verified: "github.com/skyjake/lagrange/"
+  url "https://git.skyjake.fi/gemini/lagrange/releases/download/v#{version}/lagrange_v#{version}_macos#{arch}.tbz"
   name "Lagrange"
   desc "Desktop GUI client for browsing Geminispace"
   homepage "https://gmi.skyjake.fi/lagrange/"
 
   livecheck do
-    url :url
-    strategy :github_latest
+    url "https://etc.skyjake.fi/lagrange/appcast-#{livecheck_arch}.xml"
+    strategy :sparkle
   end
 
   auto_updates true
