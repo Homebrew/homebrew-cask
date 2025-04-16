d030d41c7c4ef4f25320912754c4434301f74ed5
Klaus Hipp <khipp@users.noreply.github.com>
2025-04-16 09:20:54 +0200

    processing 4.4.1,1301

--- Casks/p/processing.rb
+++ Casks/p/processing.rb
@@ -1,11 +1,11 @@
 cask "processing" do
   arch arm: "aarch64", intel: "x64"
 
-  version "4.3.4,1297"
-  sha256 arm:   "aefd88706c2bdca9590c38d7995df9f4192c5a7b28672f5791e0cf46ae3ef8d6",
-         intel: "60dd3648492986121f24313d9ea90f34b2043b50a06f6255aade0ea892e12ceb"
+  version "4.4.1,1301"
+  sha256 arm:   "773f30706da504ef205b64c08b5eb31ecd4e26e4bc00a806ec6877c35602bac2",
+         intel: "85755de2ec4c4d5f60bb533e80d73a4be41cadfa3e18c0e11d45a0d6f6dacb7a"
 
-  url "https://github.com/processing/processing4/releases/download/processing-#{version.csv.second}-#{version.csv.first}/processing-#{version.csv.first}-macos-#{arch}.zip",
+  url "https://github.com/processing/processing4/releases/download/processing-#{version.csv.second}-#{version.csv.first}/processing-#{version.csv.first}-macos-#{arch}.dmg",
       verified: "github.com/processing/processing4/"
   name "Processing"
   desc "Flexible software sketchbook and a language for learning how to code"
