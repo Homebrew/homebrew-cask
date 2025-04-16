269965555501d7cd8aea84cfc388b153b3fc45c3
Klaus Hipp <khipp@users.noreply.github.com>
2025-04-16 05:09:46 +0200

    prowlarr: update livecheck

--- Casks/p/prowlarr.rb
+++ Casks/p/prowlarr.rb
@@ -12,8 +12,10 @@ cask "prowlarr" do
   homepage "https://prowlarr.com/"
 
   livecheck do
-    url :url
-    strategy :github_latest
+    url "https://prowlarr.servarr.com/v1/update/master/changes?os=osx&arch=#{arch}"
+    strategy :json do |json|
+      json.map { |item| item["version"] }
+    end
   end
 
   auto_updates true
