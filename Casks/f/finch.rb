ed772380087941f34341d0aa7cbe0abbd8e9fecf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-16 18:47:46 +0000

    finch 1.7.2

--- Casks/f/finch.rb
+++ Casks/f/finch.rb
@@ -1,9 +1,9 @@
 cask "finch" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.7.1"
-  sha256 arm:   "034beffae0bb0baae97e905cd5d0fae1dad35896029cc09cc5624f9f988482ac",
-         intel: "0e1232f7ba502bc517d93509833d88c03f17bc4f3e5eedfda563c2a48947e24e"
+  version "1.7.2"
+  sha256 arm:   "e7d5b4af4c3623a8648e290583fd0cb1e2bc8d8d8d033326e5a3647c4cc55a7d",
+         intel: "92e63a0f26471772a37482cc861ac4d55adacfb00c8a869b0520feae6a295b22"
 
   url "https://github.com/runfinch/finch/releases/download/v#{version}/Finch-v#{version}-#{arch}.pkg"
   name "Finch"
