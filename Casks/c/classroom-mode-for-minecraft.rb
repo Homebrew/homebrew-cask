932a6755e1f123f0448b656fe4800477375b25dc
Klaus Hipp <khipp@users.noreply.github.com>
2025-04-17 10:54:10 +0200

    classroom-mode-for-minecraft: update url

--- Casks/c/classroom-mode-for-minecraft.rb
+++ Casks/c/classroom-mode-for-minecraft.rb
@@ -2,8 +2,8 @@ cask "classroom-mode-for-minecraft" do
   version "1.83"
   sha256 "5eaf55eb603229f003c02a4addde4e50eb5665d8c02de098d4586bf5f04e4ef0"
 
-  url "https://meedownloads.blob.core.windows.net/retailbuilds/ClassroomMode/mac/Classroom_Mode_#{version.dots_to_underscores}.dmg",
-      verified: "meedownloads.blob.core.windows.net/"
+  url "https://downloads.minecrafteduservices.com/retailbuilds/ClassroomMode/mac/Classroom_Mode_#{version.dots_to_underscores}.dmg",
+      verified: "downloads.minecrafteduservices.com/"
   name "Classroom Mode for Minecraft"
   desc "Classroom management app for Minecraft Education Edition"
   homepage "https://education.minecraft.net/"
