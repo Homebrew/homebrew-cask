d801e66283e7b816a6572182c83fa009ada639d0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-18 01:06:58 +0000

    nosqlbooster-for-mongodb 10.0.1

--- Casks/n/nosqlbooster-for-mongodb.rb
+++ Casks/n/nosqlbooster-for-mongodb.rb
@@ -1,6 +1,6 @@
 cask "nosqlbooster-for-mongodb" do
-  version "10.0.0"
-  sha256 "0e83c1677156afdb7c8f096e6ef5388faae71a4fdd9f8fbefd4c851caecbd4e6"
+  version "10.0.1"
+  sha256 "bf5236c3513855bbfa34ac484a7b950fa0933edf771a56dd7ec72d40d80ae428"
 
   url "https://s3.nosqlbooster.com/download/releasesv#{version.major}/nosqlbooster4mongo-#{version}.dmg"
   name "NoSQLBooster for MongoDB"
