15705470f1b8073b32e3352cb5dd510d10387bf8
Klaus Hipp <khipp@users.noreply.github.com>
2025-04-18 15:03:28 +0200

    lw-scanner: revert to 0.27.5

--- Casks/l/lw-scanner.rb
+++ Casks/l/lw-scanner.rb
@@ -1,8 +1,11 @@
 cask "lw-scanner" do
-  version "0.27.6"
-  sha256 "fa82461fad3bc17bf223469f97fd05a8657b67abfc53f1abdf237062d6228433"
+  arch arm: "arm64", intel: "amd64"
 
-  url "https://github.com/lacework/lacework-vulnerability-scanner/releases/download/v#{version}/lw-scanner-darwin-amd64",
+  version "0.27.5"
+  sha256 arm:   "1bfbf93574b04e84a20bcccbb368c91365e519200d389b4e8b55d016c13f179d",
+         intel: "8ec483d31d2a4a46c19a9a345da329ed08110e1717c2924118fdeb90c3d323c2"
+
+  url "https://github.com/lacework/lacework-vulnerability-scanner/releases/download/v#{version}/lw-scanner-darwin-#{arch}.zip",
       verified: "github.com/lacework/lacework-vulnerability-scanner/"
   name "Lacework vulnerability scanner"
   desc "Lacework inline scanner"
@@ -13,11 +16,7 @@ cask "lw-scanner" do
     strategy :github_latest
   end
 
-  binary "lw-scanner-darwin-amd64", target: "lw-scanner"
+  binary "bin/lw-scanner"
 
   zap trash: "~/.config/lw-scanner"
-
-  caveats do
-    requires_rosetta
-  end
 end
