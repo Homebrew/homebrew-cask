63a8cee5578688c479f72bec6345329ed11d4978
Klaus Hipp <khipp@users.noreply.github.com>
2025-04-19 13:05:46 +0200

    brisk: add livecheck

--- Casks/b/brisk.rb
+++ Casks/b/brisk.rb
@@ -7,6 +7,13 @@ cask "brisk" do
   desc "App for submitting radars"
   homepage "https://github.com/br1sk/brisk"
 
+  livecheck do
+    url "https://raw.githubusercontent.com/br1sk/brisk/master/appcast.xml"
+    strategy :sparkle
+  end
+
+  auto_updates true
+
   app "Brisk.app"
 
   zap trash: [
