4b828ac2faaee4ede887faff68b1b1f091bf6f02
Adon <dev@adon.dev>
2025-04-18 22:17:04 -0400

    Hex Voice-to-Text

---
+++ Casks/h/hex.rb
@@ -0,0 +1,21 @@
+cask "hex" do
+  arch arm: "ARM64"
+
+  version "0.1.33"
+  sha256 :no_check
+
+  url "https://hex-updates.s3.us-east-1.amazonaws.com/hex-latest.dmg",
+      verified: "hex-updates.s3.us-east-1.amazonaws.com/"
+  name "Hex"
+  desc "Hex — Voice → Text"
+  homepage "https://hex.kitlangton.com/"
+
+  app "Hex.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.kitlangton.Hex",
+    "~/Library/Caches/com.kitlangton.Hex",
+    "~/Library/HTTPStorages/com.kitlangton.Hex",
+    "~/Library/Preferences/com.kitlangton.Hex.plist",
+  ]
+end
