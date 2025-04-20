efc41d17c601d52a68d33bb9dc73b93a71387ede
Sam Ford <1584702+samford@users.noreply.github.com>
2025-04-20 09:33:45 -0400

    onyx: update url setup
    The `onyx` cask only includes one `url` that interpolates the macOS
    version, which is tidy but doesn't produce correct API data. When this
    cask is parsed to produce the API data, the `url` is simply a string
    and there's no indication that it varies based on macOS version after
    interpolation. As a result, the JSON data only contains one `url`
    that's appropriate for the execution environment where the API data
    is produced (currently Sonoma). This addresses the issue by including
    a `url` value in each macOS on_system block, so the variants will be
    correctly detected and included in the API data.
    
    Besides that, this updates the `livecheck` block URL to also use
    `MacOS.version.to_s.delete(".")`, as `MacOS.version` will produce an
    invalid URL for older macOS versions like 10.15.

--- Casks/o/onyx.rb
+++ Casks/o/onyx.rb
@@ -1,44 +1,65 @@
 cask "onyx" do
   sha256 :no_check
 
+  # NOTE: We use separate `url` values in each of the macOS on_system blocks
+  # so that the API data correctly includes URL variants for each.
   on_el_capitan :or_older do
     version "3.1.9"
+
+    url "https://www.titanium-software.fr/download/1011/OnyX.dmg"
   end
   on_sierra do
     version "3.3.1"
+
+    url "https://www.titanium-software.fr/download/1012/OnyX.dmg"
   end
   on_high_sierra do
     version "3.4.9"
+
+    url "https://www.titanium-software.fr/download/1013/OnyX.dmg"
   end
   on_mojave do
     version "3.6.8"
+
+    url "https://www.titanium-software.fr/download/1014/OnyX.dmg"
   end
   on_catalina do
     version "3.8.7"
+
+    url "https://www.titanium-software.fr/download/1015/OnyX.dmg"
   end
   on_big_sur do
     version "4.0.2"
+
+    url "https://www.titanium-software.fr/download/11/OnyX.dmg"
   end
   on_monterey do
     version "4.2.7"
+
+    url "https://www.titanium-software.fr/download/12/OnyX.dmg"
   end
   on_ventura do
     version "4.4.7"
+
+    url "https://www.titanium-software.fr/download/13/OnyX.dmg"
   end
   on_sonoma do
     version "4.6.2"
+
+    url "https://www.titanium-software.fr/download/14/OnyX.dmg"
   end
   on_sequoia :or_newer do
     version "4.7.7"
+
+    url "https://www.titanium-software.fr/download/#{MacOS.version.to_s.delete(".")}/OnyX.dmg"
   end
 
-  url "https://www.titanium-software.fr/download/#{MacOS.version.to_s.delete(".")}/OnyX.dmg"
   name "OnyX"
   desc "Verify system files structure, run miscellaneous maintenance and more"
   homepage "https://www.titanium-software.fr/en/onyx.html"
 
   livecheck do
-    url "https://www.titanium-software.fr/download/#{MacOS.version}/OnyX.plist"
+    url "https://www.titanium-software.fr/download/#{MacOS.version.to_s.delete(".")}/OnyX.plist"
     strategy :xml do |xml|
       version = xml.elements["//key[text()='Version']"]&.next_element&.text
       next if version.blank?
