ee79f1ea59bf0a0c76e2e1740f9f9fc1db047f7f
Sam Ford <1584702+samford@users.noreply.github.com>
2025-04-20 09:33:22 -0400

    maintenance: update url setup
    The `maintenance` cask only includes one `url` that interpolates the
    macOS version, which is tidy but doesn't produce correct API data.
    When this cask is parsed to produce the API data, the `url` is simply
    a string and there's no indication that it varies based on macOS
    version after interpolation. As a result, the JSON data only contains
    one `url` that's appropriate for the execution environment where the
    API data is produced (currently Sonoma). This addresses the issue by
    including a `url` value in each macOS on_system block, so the variants
    will be correctly detected and included in the API data.
    
    Besides that, this updates the `livecheck` block URL to also use
    `MacOS.version.to_s.delete(".")`, as `MacOS.version` will produce an
    invalid URL for older macOS versions like 10.15.

--- Casks/m/maintenance.rb
+++ Casks/m/maintenance.rb
@@ -1,44 +1,65 @@
 cask "maintenance" do
   sha256 :no_check
 
+  # NOTE: We use separate `url` values in each of the macOS on_system blocks
+  # so that the API data correctly includes URL variants for each.
   on_el_capitan :or_older do
     version "2.1.8"
+
+    url "https://www.titanium-software.fr/download/1011/Maintenance.dmg"
   end
   on_sierra do
     version "2.3.0"
+
+    url "https://www.titanium-software.fr/download/1012/Maintenance.dmg"
   end
   on_high_sierra do
     version "2.4.2"
+
+    url "https://www.titanium-software.fr/download/1013/Maintenance.dmg"
   end
   on_mojave do
     version "2.5.6"
+
+    url "https://www.titanium-software.fr/download/1014/Maintenance.dmg"
   end
   on_catalina do
     version "2.7.1"
+
+    url "https://www.titanium-software.fr/download/1015/Maintenance.dmg"
   end
   on_big_sur do
     version "2.8.2"
+
+    url "https://www.titanium-software.fr/download/11/Maintenance.dmg"
   end
   on_monterey do
     version "2.9.2"
+
+    url "https://www.titanium-software.fr/download/12/Maintenance.dmg"
   end
   on_ventura do
     version "3.0.2"
+
+    url "https://www.titanium-software.fr/download/13/Maintenance.dmg"
   end
   on_sonoma do
     version "3.2.0"
+
+    url "https://www.titanium-software.fr/download/14/Maintenance.dmg"
   end
   on_sequoia :or_newer do
     version "3.2.9"
+
+    url "https://www.titanium-software.fr/download/#{MacOS.version.to_s.delete(".")}/Maintenance.dmg"
   end
 
-  url "https://www.titanium-software.fr/download/#{MacOS.version.to_s.delete(".")}/Maintenance.dmg"
   name "Maintenance"
   desc "Operating system maintenance and cleaning utility"
   homepage "https://www.titanium-software.fr/en/maintenance.html"
 
   livecheck do
-    url "https://www.titanium-software.fr/download/#{MacOS.version}/Maintenance.plist"
+    url "https://www.titanium-software.fr/download/#{MacOS.version.to_s.delete(".")}/Maintenance.plist"
     strategy :xml do |xml|
       version = xml.elements["//key[text()='Version']"]&.next_element&.text
       next if version.blank?
