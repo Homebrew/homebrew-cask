8ab1bcccebda8e26524cfa325acb3b96695a9942
Klaus Hipp <khipp@users.noreply.github.com>
2025-04-21 19:21:44 +0200

    remote-wake-up: update zap

--- Casks/r/remote-wake-up.rb
+++ Casks/r/remote-wake-up.rb
@@ -17,7 +17,7 @@ cask "remote-wake-up" do
   zap trash: [
     "~/Library/Application Support/com.mac-attender.Remote-Wake-Up",
     "~/Library/Application Support/com.mac-attender.Remote_Wake_Up",
-    "~/Library/Containers/com.mac-attender.Remote-Wake-Up/",
+    "~/Library/Containers/com.mac-attender.Remote-Wake-Up",
     "~/Library/Preferences/com.mac-attender.Remote-Wake-Up.plist",
     "~/Library/Saved Application State/com.mac-attender.Remote-Wake-Up.savedState",
   ]
