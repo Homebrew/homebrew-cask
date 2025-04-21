c3e398b2e6cd5723793b240ec47f62337882ff53
Sam Ford <1584702+samford@users.noreply.github.com>
2025-04-21 10:24:32 -0400

    onyx: update livecheck
    This updates the `livecheck` block setup for `onyx`. The homepage
    states, "Titanium Software has discontinued developing the old
    versions and will no longer update them", so I've added `skip`
    `livecheck` blocks to the older versions and moved the existing
    `livecheck` block into the newest on_system block.
    
    I updated the `livecheck` block to use the approach from the `calhash`
    cask, which checks the versions on the homepage. The related plist
    files sometimes contain an outdated version with respect to what's
    listed on the homepage, so this seems like the more reliable source
    of version information.
    
    Besides that, I tweaked the regex and replaced the interpolated
    `MacOS.version` with a hardcoded version, so this will work
    predictably regardless of the execution environment.

--- Casks/o/onyx.rb
+++ Casks/o/onyx.rb
@@ -7,66 +7,99 @@ cask "onyx" do
     version "3.1.9"
 
     url "https://www.titanium-software.fr/download/1011/OnyX.dmg"
+
+    livecheck do
+      skip "Legacy version"
+    end
   end
   on_sierra do
     version "3.3.1"
 
     url "https://www.titanium-software.fr/download/1012/OnyX.dmg"
+
+    livecheck do
+      skip "Legacy version"
+    end
   end
   on_high_sierra do
     version "3.4.9"
 
     url "https://www.titanium-software.fr/download/1013/OnyX.dmg"
+
+    livecheck do
+      skip "Legacy version"
+    end
   end
   on_mojave do
     version "3.6.8"
 
     url "https://www.titanium-software.fr/download/1014/OnyX.dmg"
+
+    livecheck do
+      skip "Legacy version"
+    end
   end
   on_catalina do
     version "3.8.7"
 
     url "https://www.titanium-software.fr/download/1015/OnyX.dmg"
+
+    livecheck do
+      skip "Legacy version"
+    end
   end
   on_big_sur do
     version "4.0.2"
 
     url "https://www.titanium-software.fr/download/11/OnyX.dmg"
+
+    livecheck do
+      skip "Legacy version"
+    end
   end
   on_monterey do
     version "4.2.7"
 
     url "https://www.titanium-software.fr/download/12/OnyX.dmg"
+
+    livecheck do
+      skip "Legacy version"
+    end
   end
   on_ventura do
     version "4.4.7"
 
     url "https://www.titanium-software.fr/download/13/OnyX.dmg"
+
+    livecheck do
+      skip "Legacy version"
+    end
   end
   on_sonoma do
     version "4.6.2"
 
     url "https://www.titanium-software.fr/download/14/OnyX.dmg"
+
+    livecheck do
+      skip "Legacy version"
+    end
   end
   on_sequoia :or_newer do
     version "4.7.7"
 
     url "https://www.titanium-software.fr/download/15/OnyX.dmg"
+
+    # We check the version on the homepage, as the version in the related plist
+    # file can be out of date.
+    livecheck do
+      url :homepage
+      regex(/>\s*OnyX\s+v?(\d+(?:\.\d+)+)\s+for\s+[\w\s]*15\s*</i)
+    end
   end
 
   name "OnyX"
   desc "Verify system files structure, run miscellaneous maintenance and more"
   homepage "https://www.titanium-software.fr/en/onyx.html"
-
-  livecheck do
-    url "https://www.titanium-software.fr/download/#{MacOS.version.to_s.delete(".")}/OnyX.plist"
-    strategy :xml do |xml|
-      version = xml.elements["//key[text()='Version']"]&.next_element&.text
-      next if version.blank?
-
-      version.strip
-    end
-  end
 
   depends_on macos: [
     :el_capitan,
