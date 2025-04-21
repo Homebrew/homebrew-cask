09e7ab5ac32b5c0034e53aef4cfa7c85180940a3
Sam Ford <1584702+samford@users.noreply.github.com>
2025-04-21 12:02:59 -0400

    cocktail 17.10.1
    This updates the Sonoma version of `cocktail` to the latest version
    on the homepage, 17.10.1.

--- Casks/c/cocktail.rb
+++ Casks/c/cocktail.rb
@@ -55,7 +55,7 @@ cask "cocktail" do
       url "https://www.maintain.se/downloads/Cocktail#{version.major}VE.dmg"
     end
     on_sonoma do
-      version "17.10"
+      version "17.10.1"
       sha256 :no_check
 
       url "https://www.maintain.se/downloads/Cocktail#{version.major}SE.dmg"
