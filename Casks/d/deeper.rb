878df48b704a6bc3d2ae9c4a168c81e28cef459d
Sam Ford <1584702+samford@users.noreply.github.com>
2025-04-21 10:24:12 -0400

    deeper: update livecheck
    This updates the `livecheck` block setup for `deeper`. The homepage
    states, "Titanium Software has discontinued developing the old
    versions and will no longer update them", so I've added `skip`
    `livecheck` blocks to the older versions and moved the existing
    `livecheck` block into the newest on_system block.
    
    I updated the `livecheck` block to use the approach from the `calhash`
    cask, which checks the versions on the homepage. The related plist
    files sometimes contain an outdated version with respect to what's
    listed on the homepage, so this seems like the more reliable source
    of version information.
    
    Besides that, I tweaked the regex and replaced the interpolated
    `MacOS.version` with a hardcoded version, so this will work
    predictably regardless of the execution environment.

--- Casks/d/deeper.rb
+++ Casks/d/deeper.rb
@@ -7,66 +7,99 @@ cask "deeper" do
     version "2.1.4"
 
     url "https://www.titanium-software.fr/download/1011/Deeper.dmg"
+
+    livecheck do
+      skip "Legacy version"
+    end
   end
   on_sierra do
     version "2.2.3"
 
     url "https://www.titanium-software.fr/download/1012/Deeper.dmg"
+
+    livecheck do
+      skip "Legacy version"
+    end
   end
   on_high_sierra do
     version "2.3.3"
 
     url "https://www.titanium-software.fr/download/1013/Deeper.dmg"
+
+    livecheck do
+      skip "Legacy version"
+    end
   end
   on_mojave do
     version "2.4.8"
 
     url "https://www.titanium-software.fr/download/1014/Deeper.dmg"
+
+    livecheck do
+      skip "Legacy version"
+    end
   end
   on_catalina do
     version "2.6.0"
 
     url "https://www.titanium-software.fr/download/1015/Deeper.dmg"
+
+    livecheck do
+      skip "Legacy version"
+    end
   end
   on_big_sur do
     version "2.7.1"
 
     url "https://www.titanium-software.fr/download/11/Deeper.dmg"
+
+    livecheck do
+      skip "Legacy version"
+    end
   end
   on_monterey do
     version "2.8.0"
 
     url "https://www.titanium-software.fr/download/12/Deeper.dmg"
+
+    livecheck do
+      skip "Legacy version"
+    end
   end
   on_ventura do
     version "2.9.2"
 
     url "https://www.titanium-software.fr/download/13/Deeper.dmg"
+
+    livecheck do
+      skip "Legacy version"
+    end
   end
   on_sonoma do
     version "3.0.9"
 
     url "https://www.titanium-software.fr/download/14/Deeper.dmg"
+
+    livecheck do
+      skip "Legacy version"
+    end
   end
   on_sequoia :or_newer do
     version "3.1.4"
 
     url "https://www.titanium-software.fr/download/15/Deeper.dmg"
+
+    # We check the version on the homepage, as the version in the related plist
+    # file can be out of date.
+    livecheck do
+      url :homepage
+      regex(/>\s*Deeper\s+v?(\d+(?:\.\d+)+)\s+for\s+[\w\s]*15\s*</i)
+    end
   end
 
   name "Deeper"
   desc "Tool to enable and disable hidden functions of Finder and other apps"
   homepage "https://www.titanium-software.fr/en/deeper.html"
-
-  livecheck do
-    url "https://www.titanium-software.fr/download/#{MacOS.version.to_s.delete(".")}/Deeper.plist"
-    strategy :xml do |xml|
-      version = xml.elements["//key[text()='Version']"]&.next_element&.text
-      next if version.blank?
-
-      version.strip
-    end
-  end
 
   depends_on macos: [
     :el_capitan,
