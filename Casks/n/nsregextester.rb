192ae8dbad117db6774d0f3f86456b48eb14d54e
Bevan Kay <email@bevankay.me>
2025-04-22 22:29:07 +1000

    nsregextester: disable

--- Casks/n/nsregextester.rb
+++ Casks/n/nsregextester.rb
@@ -8,6 +8,7 @@ cask "nsregextester" do
   homepage "https://github.com/aaronvegh/nsregextester"
 
   deprecate! date: "2024-04-18", because: :unmaintained
+  disable! date: "2025-04-22", because: :unmaintained
 
   app "NSRegexTester.app"
 end
