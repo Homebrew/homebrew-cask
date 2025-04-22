653ce859796b2e4da290ee57e49ebe52b380f4b9
Klaus Hipp <khipp@users.noreply.github.com>
2025-04-22 08:05:46 +0200

    jellybeansoup-netflix: update url, add livecheck

--- Casks/j/jellybeansoup-netflix.rb
+++ Casks/j/jellybeansoup-netflix.rb
@@ -2,10 +2,18 @@ cask "jellybeansoup-netflix" do
   version "1.0.5"
   sha256 "6e62f4e8a5883f139a7259464e33039e7cd5c6040caf9dbb27d85f890b576c40"
 
-  url "https://github.com/jellybeansoup/macos-netflix/releases/download/v#{version}/Netflix.v#{version}.zip"
+  url "https://jellystyle-d5a.kxcdn.com/macos/netflix/Netflix.v#{version}.zip",
+      verified: "jellystyle-d5a.kxcdn.com/"
   name "Netflix"
   desc "Third-party app to use Netflix outside the browser"
   homepage "https://github.com/jellybeansoup/macos-netflix"
+
+  livecheck do
+    url "https://jellystyle.com/resources/macOS/Netflix.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
 
   app "Netflix.app"
 
