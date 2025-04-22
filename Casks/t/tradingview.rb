09204e81b9c2b3294316603a721d454d3d70e1be
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-22 10:08:57 +0000

    tradingview 2.10.0

--- Casks/t/tradingview.rb
+++ Casks/t/tradingview.rb
@@ -1,6 +1,6 @@
 cask "tradingview" do
-  version "2.9.6"
-  sha256 "b4605693f96fa47fe46f57ecd1af1c043f744cb62397a1609ade4dbdd63dfcba"
+  version "2.10.0"
+  sha256 "d869a8d2df55b7005d194b22db7b1b4a2f853defb3bc32c4321b3dbc5c95847e"
 
   url "https://tvd-packages.tradingview.com/stable/#{version}/darwin/TradingView.dmg"
   name "TradingView Desktop"
