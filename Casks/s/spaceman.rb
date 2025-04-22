d69b97209d5e01a820ac849e46334fa822af8b00
Klaus Hipp <khipp@users.noreply.github.com>
2025-04-22 05:44:36 +0200

    spaceman: various updates

--- Casks/s/spaceman.rb
+++ Casks/s/spaceman.rb
@@ -2,10 +2,16 @@ cask "spaceman" do
   version "1.0"
   sha256 "0d8b6005609fc274da11ca2e0f5b327ce4d998be88dbb670d9b3428de5995ae0"
 
-  url "https://github.com/Jaysce/Spaceman/releases/download/v#{version}/Spaceman.#{version}.dmg"
+  url "https://s3.amazonaws.com/dev.jaysce.spaceman/Spaceman%20#{version}.dmg",
+      verified: "s3.amazonaws.com/dev.jaysce.spaceman/"
   name "Spaceman"
   desc "View Spaces / Virtual Desktops in the menu bar"
-  homepage "https://github.com/Jaysce/Spaceman"
+  homepage "https://www.jaysce.dev/projects/spaceman"
+
+  livecheck do
+    url "https://s3.amazonaws.com/dev.jaysce.spaceman/appcast.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
   depends_on macos: ">= :big_sur"
