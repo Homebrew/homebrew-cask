bf81ffdd66f68dcb6ecfac5220eec671e2013f23
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-23 01:01:00 +0000

    milanote 3.18.5

--- Casks/m/milanote.rb
+++ Casks/m/milanote.rb
@@ -1,6 +1,6 @@
 cask "milanote" do
-  version "3.18.4"
-  sha256 "3e99a67578e3a73adef9a5b729b5ea6993513a90b0a51787fa7f21f814bb687a"
+  version "3.18.5"
+  sha256 "6dbc3fdbfbc54ee9c4d082b6e1bda007c3d8a94b3ea2fdf28d4160195161fc0c"
 
   url "https://milanote-app-releases.s3.amazonaws.com/Milanote-#{version}.dmg",
       verified: "milanote-app-releases.s3.amazonaws.com/"
