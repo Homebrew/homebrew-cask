ac1b905a72cc6d466061da9e49dd5f2c47106a6e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-23 12:51:04 +0000

    grammarly-desktop 1.115.0.0

--- Casks/g/grammarly-desktop.rb
+++ Casks/g/grammarly-desktop.rb
@@ -1,6 +1,6 @@
 cask "grammarly-desktop" do
-  version "1.114.5.0"
-  sha256 "b3ce5135b697f7ad7dd88299ca827a92ac42e89da25d7951d3a4b77355b7b6bc"
+  version "1.115.0.0"
+  sha256 "32b509f8837b2ac0a8bd5153086b5759ea301d6cd378302c925bcca50ab4e250"
 
   url "https://download-mac.grammarly.com/versions/#{version}/Grammarly.dmg"
   name "Grammarly Desktop"
