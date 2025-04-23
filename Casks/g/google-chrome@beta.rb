feaf8dd4162e07c2a21694279bd46bf41e91e331
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-23 18:51:14 +0000

    google-chrome@beta 136.0.7103.48

--- Casks/g/google-chrome@beta.rb
+++ Casks/g/google-chrome@beta.rb
@@ -1,5 +1,5 @@
 cask "google-chrome@beta" do
-  version "136.0.7103.33"
+  version "136.0.7103.48"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/beta/googlechromebeta.dmg"
