353f757ed8e90519a631202dc5baf7c34e612e11
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-25 16:17:58 +0000

    texstudio 4.8.7

--- Casks/t/texstudio.rb
+++ Casks/t/texstudio.rb
@@ -1,9 +1,9 @@
 cask "texstudio" do
   arch arm: "-m1"
 
-  version "4.8.6"
-  sha256 arm:   "3ba37f5bbf1232af5bdefbca7b859639c09b180aba26b2801119708f402b5c4a",
-         intel: "7d4326f2e4a59cc7022d950f1c7f01ce1978bc69c66a7199b4c73ac2cca37b37"
+  version "4.8.7"
+  sha256 arm:   "73c7195d4ae56bf1b5757012cff3b87ccce885c427c1233c2d584b641172a023",
+         intel: "4f325c3bf10e0f63e8b4fb46d5c2a7362060dbfbae63148af49ae337b780f752"
 
   on_arm do
     depends_on macos: ">= :sonoma"
