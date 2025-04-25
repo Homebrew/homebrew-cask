126b053fa12bac3b028f04c7c3da1144329716b6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-25 09:55:36 +0000

    sketch@beta 2025.1,202250

--- Casks/s/sketch@beta.rb
+++ Casks/s/sketch@beta.rb
@@ -1,6 +1,6 @@
 cask "sketch@beta" do
-  version "2025.1,202222"
-  sha256 "fcd04994858622f2746fcff1170e815ab720f83d66778b508d5dc7803aa4e53f"
+  version "2025.1,202250"
+  sha256 "b7d43cc336539910468c77b73f4abe284187e06f611f224b0aff1b5f2d2cd186"
 
   url "https://beta-download.sketch.com/sketch-#{version.csv.first}-#{version.csv.second}.zip"
   name "Sketch"
