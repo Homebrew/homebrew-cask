28602dad3844da91d4053e0efa8580b14691e48e
Hanwen Guo <guo@hanwen.io>
2025-04-24 19:11:38 -0600

    font-zhuque-fangsong: fix url

--- Casks/font/font-z/font-zhuque-fangsong.rb
+++ Casks/font/font-z/font-zhuque-fangsong.rb
@@ -2,7 +2,7 @@ cask "font-zhuque-fangsong" do
   version "0.211"
   sha256 "4141782e9f36bc669d042422505f5c506df47930ee7bfa82342d8cec631f8f10"
 
-  url "https://github.com/TrionesType/zhuque/releases/download/v#{version}/ZhuqueFangsong-Regular-v#{version}.zip"
+  url "https://github.com/TrionesType/zhuque/releases/download/v#{version}/ZhuqueFangsong-v#{version}.zip"
   name "Zhuque Fangsong"
   name "朱雀仿宋"
   homepage "https://github.com/TrionesType/zhuque"
