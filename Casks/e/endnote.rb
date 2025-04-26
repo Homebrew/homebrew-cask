f9c6f6c6c66593fd1dd3ea60fb10791352627198
John Benninghoff <jbenninghoff@mac.com>
2025-04-26 13:32:49 -0500

    endnote 22.0.0.0

--- Casks/e/endnote.rb
+++ Casks/e/endnote.rb
@@ -1,19 +1,19 @@
 cask "endnote" do
-  version "21.5.0.20846"
+  version "22.0.0.0"
   sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://download.endnote.com/downloads/#{version.major}/EndNote#{version.major}Installer.dmg"
+  url "https://download.endnote.com/downloads/2025/EndNote2025Installer.dmg"
   name "EndNote"
   desc "Reference manager"
   homepage "https://endnote.com/"
 
   livecheck do
-    url "https://download.endnote.com/updates/#{version.major}.0/EN#{version.major}MacUpdates.xml"
+    # update url to "https://download.endnote.com/updates/#{version.major}.0/EN#{version.major}MacUpdates.xml" after
+    # version 22 starts receiving updates, see: https://github.com/Homebrew/homebrew-cask/pull/210166
+    url "https://download.endnote.com/updates/21.0/EN21MacUpdates.xml"
     regex(/^v?(\d+(?:\.\d+)+)$/i)
     strategy :xml do |xml, regex|
       xml.get_elements("//updates/build").map do |item|
-        next if item.elements["type"]&.text&.include?("html")
-
         version = item.elements["updateTo"].text&.strip
         match = version.match(regex) if version
         next if match.blank?
@@ -23,7 +23,7 @@ cask "endnote" do
     end
   end
 
-  container nested: "Install EndNote #{version.major}.app/Contents/Resources/EndNote.zip"
+  container nested: "Install EndNote 2025.app/Contents/Resources/EndNote.zip"
 
   suite "EndNote"
 
@@ -31,8 +31,13 @@ cask "endnote" do
     "/Library/Application Support/ResearchSoft/EndNote",
     "~/Library/Application Support/EndNote",
     "~/Library/Caches/com.ThomsonResearchSoft.EndNote",
+    "~/Library/Cookies/com.ThomsonResearchSoft.EndNote.binarycookies",
+    "~/Library/HTTPStorages/com.ThomsonResearchSoft.EndNote",
+    "~/Library/HTTPStorages/com.ThomsonResearchSoft.EndNote.binarycookies",
     "~/Library/Preferences/com.ThomsonResearchSoft.EndNote.plist",
+    "~/Library/Preferences/com.ThomsonReuters.EndNoteCustomizer.plist",
     "~/Library/Services/ENService.app",
     "~/Library/Spotlight/EndNote.mdimporter",
+    "~/Library/WebKit/com.ThomsonResearchSoft.EndNote",
   ]
 end
