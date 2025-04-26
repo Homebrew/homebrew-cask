4944b9f335cd6ab610b9c82a858a6aefe21d88a6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-26 00:55:47 +0000

    kindle-comic-converter 7.4.1

--- Casks/k/kindle-comic-converter.rb
+++ Casks/k/kindle-comic-converter.rb
@@ -1,9 +1,9 @@
 cask "kindle-comic-converter" do
   arch arm: "arm", intel: "i386"
 
-  version "7.3.3"
-  sha256 arm:   "06e06370a4a76fdc014a88975094f75f2541df538f96cf2985881429e10fb020",
-         intel: "b9eafc0ce6162e733aa15544c82a5edda423556ad2d65563d231a38e2814cf07"
+  version "7.4.1"
+  sha256 arm:   "94f413b82c63e0be1fe7be9247e44c49b9d632432314d7ad2a5e9dca2cc60841",
+         intel: "24d52b3094b49e6a46bcd47eb8b1c85e915a4b3567709dbcdf2aaafe40671ce6"
 
   url "https://github.com/ciromattia/kcc/releases/download/v#{version}/kcc_macos_#{arch}_#{version}.dmg"
   name "Kindle Comic Converter"
