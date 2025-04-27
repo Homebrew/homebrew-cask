c928e86090a85ce88a2b77201818750e64c44741
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-27 00:16:24 +0000

    font-coral-pixels: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-c/font-coral-pixels.rb
+++ Casks/font/font-c/font-coral-pixels.rb
@@ -2,9 +2,10 @@ cask "font-coral-pixels" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/coralpixels/CoralPixels-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/coralpixels/CoralPixels-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Coral Pixels"
-  homepage "https://github.com/tanukifont/Coral-Pixels"
+  homepage "https://fonts.google.com/specimen/Coral+Pixels"
 
   font "CoralPixels-Regular.ttf"
 
