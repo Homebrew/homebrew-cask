8b1604d1a2b18e5d529bba3bc9214e5990e5a27d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-27 00:16:12 +0000

    font-noto-serif-dives-akuru: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml) workflow.

---
+++ Casks/font/font-n/font-noto-serif-dives-akuru.rb
@@ -0,0 +1,12 @@
+cask "font-noto-serif-dives-akuru" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/notoserifdivesakuru/NotoSerifDivesAkuru-Regular.ttf"
+  name "Noto Serif Dives Akuru"
+  homepage "https://github.com/notofonts/dives-akuru"
+
+  font "NotoSerifDivesAkuru-Regular.ttf"
+
+  # No zap stanza required
+end
