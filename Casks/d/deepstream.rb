79098b84b527d3c1ac873789bf2b13b0c8b42b4c
Klaus Hipp <khipp@users.noreply.github.com>
2025-04-27 08:39:08 +0200

    deepstream 8.0.0

--- Casks/d/deepstream.rb
+++ Casks/d/deepstream.rb
@@ -1,6 +1,6 @@
 cask "deepstream" do
-  version "7.0.10"
-  sha256 "3e82240b08ef33eae93a6f94d19af959a4664dac5d2589f057fb1f7b9a5d3e72"
+  version "8.0.0"
+  sha256 "6f4bf2be29ab22748c3e71c41a5932ff273db2b98536fd7ddfec2de40b5cccc1"
 
   url "https://github.com/deepstreamIO/deepstream.io/releases/download/v#{version}/deepstream.io-mac-#{version}.pkg",
       verified: "github.com/deepstreamIO/deepstream.io/"
