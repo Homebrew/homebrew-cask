62e7f1a31ff24cf515c89a6c35587f40edfa5c0b
John Benninghoff <jbenninghoff@mac.com>
2025-04-28 09:57:56 -0500

    Change endnote livecheck approach
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/e/endnote.rb
+++ Casks/e/endnote.rb
@@ -7,10 +7,12 @@ cask "endnote" do
   desc "Reference manager"
   homepage "https://endnote.com/"
 
+  # The XML file doesn't provide any update information for new major versions
+  # (e.g. 22.0.0.0) until a subsequent version is released. The check will fail
+  # when the cask is updated to a new major version but it should automatically
+  # work again when the next version is released.
   livecheck do
-    # update url to "https://download.endnote.com/updates/#{version.major}.0/EN#{version.major}MacUpdates.xml" after
-    # version 22 starts receiving updates, see: https://github.com/Homebrew/homebrew-cask/pull/210166
-    url "https://download.endnote.com/updates/21.0/EN21MacUpdates.xml"
+    url "https://download.endnote.com/updates/#{version.major}.0/EN#{version.major}MacUpdates.xml"
     regex(/^v?(\d+(?:\.\d+)+)$/i)
     strategy :xml do |xml, regex|
       xml.get_elements("//updates/build").map do |item|
