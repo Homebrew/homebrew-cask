e0232032a8c50ff2fa58ec4567d26dae8831475f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-28 15:35:51 +0000

    bezel 2.8.1

--- Casks/b/bezel.rb
+++ Casks/b/bezel.rb
@@ -1,6 +1,6 @@
 cask "bezel" do
-  version "2.8.0"
-  sha256 "515f9bf3d6d116321672aea25426e6d9bd134cde7162ed4024c97cab0f1601e0"
+  version "2.8.1"
+  sha256 "6a75efc46e484345cf6e7f6e0faebdaa99c9380a90094aeafcc7150eb69bcb26"
 
   url "https://download.nonstrict.eu/bezel/Bezel-#{version}.zip",
       verified: "download.nonstrict.eu/bezel/"
