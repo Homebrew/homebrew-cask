28f8654dc8a8202a1a11c2f9a1e96e09e47c3bf3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-29 03:39:46 +0000

    amazon-q 1.9.0

--- Casks/a/amazon-q.rb
+++ Casks/a/amazon-q.rb
@@ -1,6 +1,6 @@
 cask "amazon-q" do
-  version "1.8.1"
-  sha256 "54d1ebce2e9c64a3209ec12ff356a13e4ed68c97286d9cf73d18379973f8c03a"
+  version "1.9.0"
+  sha256 "d50680a4fcdff6c40c4afa37813f227c1d9cdece1f485c0cc2b82281ca6d730e"
 
   url "https://desktop-release.q.us-east-1.amazonaws.com/#{version}/Amazon%20Q.dmg",
       verified: "desktop-release.q.us-east-1.amazonaws.com/"
