ccae84622ac8a133461fcbe4f51982f1f17eec80
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-29 06:48:53 +0000

    github-copilot-for-xcode 0.34.0

--- Casks/g/github-copilot-for-xcode.rb
+++ Casks/g/github-copilot-for-xcode.rb
@@ -1,6 +1,6 @@
 cask "github-copilot-for-xcode" do
-  version "0.33.0"
-  sha256 "9c6f81d1abd9a28daf25d2574124747daedcf31eded658353f2bee1efbb29cf6"
+  version "0.34.0"
+  sha256 "d12a7b5d59dad4324faf54e9c950a8987c86fbb78bd81ff9833489ef9c243089"
 
   url "https://githubcopilotide.z13.web.core.windows.net/#{version}/GitHubCopilotForXcode.dmg",
       verified: "githubcopilotide.z13.web.core.windows.net/"
