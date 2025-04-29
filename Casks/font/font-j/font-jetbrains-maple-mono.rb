cb8928ec256e2299283aca5ec7fab353bb8651bf
Klaus Hipp <khipp@users.noreply.github.com>
2025-04-29 09:34:40 +0200

    font-jetbrains-maple-mono 1.2304.72

--- Casks/font/font-j/font-jetbrains-maple-mono.rb
+++ Casks/font/font-j/font-jetbrains-maple-mono.rb
@@ -1,6 +1,6 @@
 cask "font-jetbrains-maple-mono" do
-  version "1.2304.71"
-  sha256 "25a3cba1a43985958f71a5125aca215126fab7a2eade0ff4eb8a76081fa0bc48"
+  version "1.2304.72"
+  sha256 "05ae653f5712da5ec4d529c51583788bcab5ed42403f585dff3a325c6bc37aba"
 
   url "https://github.com/SpaceTimee/Fusion-JetBrainsMapleMono/releases/download/#{version}/JetBrainsMapleMono-XX-XX-XX.zip"
   name "JetBrains Maple Mono"
