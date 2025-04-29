564a104a6bad37560720ede8d72557cb4eb896cf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-29 18:47:16 +0000

    dropbox@beta 224.3.4654

--- Casks/d/dropbox@beta.rb
+++ Casks/d/dropbox@beta.rb
@@ -1,9 +1,9 @@
 cask "dropbox@beta" do
   arch arm: "&arch=arm64"
 
-  version "223.3.4858"
-  sha256 arm:   "839eb40e344bf828b6b14b1e2be1ef6917d75c768e6be7266865581145b735f3",
-         intel: "faaf38491381e2e2eb16a09b094c30e9ae721c78fb436c8f2301eefe54a3b6d9"
+  version "224.3.4654"
+  sha256 arm:   "de9b44cc6829ed90840c8cd6bb5e69f7e6590a48edb231cde8a90e288831bd57",
+         intel: "70aa5532ea6fcc7ece7ce7f32b8c852455a426b4fa66c49e39357e97c7f6309b"
 
   url "https://www.dropbox.com/download?build=#{version}&plat=mac&rtoken=&type=full#{arch}",
       verified: "dropbox.com/"
