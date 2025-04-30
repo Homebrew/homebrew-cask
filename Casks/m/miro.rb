5d5e5e3ce36ecb90d888a94fb259492de4411bbd
Klaus Hipp <khipp@users.noreply.github.com>
2025-04-30 14:10:18 +0200

    miro 0.10.96

--- Casks/m/miro.rb
+++ Casks/m/miro.rb
@@ -1,7 +1,7 @@
 cask "miro" do
   arch arm: "darwin-arm64", intel: "darwin"
 
-  version "0.10.95"
+  version "0.10.96"
   sha256 :no_check
 
   url "https://desktop.miro.com/platforms/#{arch}/Install-Miro.dmg"
