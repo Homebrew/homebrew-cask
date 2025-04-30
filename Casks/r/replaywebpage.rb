a6a28337f74894eeac0fe2661b54f5388148eece
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-30 04:17:03 +0000

    replaywebpage 2.3.5

--- Casks/r/replaywebpage.rb
+++ Casks/r/replaywebpage.rb
@@ -1,6 +1,6 @@
 cask "replaywebpage" do
-  version "2.3.4"
-  sha256 "78976f0f231cc5a8938eea5da1a9ef7a00d7b8a9586786353c29e8c03c4773b9"
+  version "2.3.5"
+  sha256 "a5b5c614614e74f4ee358dc454034aa5a47762117306b4cb9ec8a04b1d52bc7a"
 
   url "https://github.com/webrecorder/replayweb.page/releases/download/v#{version}/ReplayWeb.page-#{version}.dmg",
       verified: "github.com/webrecorder/replayweb.page/"
