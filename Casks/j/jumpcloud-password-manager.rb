fd33836d97a5d12060804a46a863f31b064687a1
Marijn Doeve <marijn.doeve@mywheels.nl>
2025-04-30 13:06:05 +0200

    jumpcloud-password-manager: add ARM64 version

--- Casks/j/jumpcloud-password-manager.rb
+++ Casks/j/jumpcloud-password-manager.rb
@@ -1,14 +1,17 @@
 cask "jumpcloud-password-manager" do
-  version "3.2.2"
-  sha256 "fc7debbdc60f75e5a16070c5ad24c5bbcb3c9b334a10705e70a6f285369cb2ee"
+  arch arm: "arm64/"
 
-  url "https://cdn.pwm.jumpcloud.com/DA/release/JumpCloud-Password-Manager-#{version}.dmg"
+  version "3.2.2"
+  sha256 arm:   "17ecdceb063dd3dc150733daa70b3bc898725e889d5ca38794fdc330dcb713b8",
+         intel: "fc7debbdc60f75e5a16070c5ad24c5bbcb3c9b334a10705e70a6f285369cb2ee"
+
+  url "https://cdn.pwm.jumpcloud.com/DA/release/#{arch}JumpCloud-Password-Manager-#{version}.dmg"
   name "JumpCloud Password Manager"
   desc "Password management tool that provides authentication, sharing and credentials"
   homepage "https://cdn.pwm.jumpcloud.com/web/download.html#desktop"
 
   livecheck do
-    url "https://cdn.pwm.jumpcloud.com/DA/release/latest-mac.yml"
+    url "https://cdn.pwm.jumpcloud.com/DA/release/#{arch}latest-mac.yml"
     strategy :electron_builder
   end
 
@@ -22,8 +25,4 @@ cask "jumpcloud-password-manager" do
     "~/Library/Preferences/com.jumpcloud.pwm.desktop.live.plist",
     "~/Library/Saved Application State/com.jumpcloud.pwm.desktop.live.savedState",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
