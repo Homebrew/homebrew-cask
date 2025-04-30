808fae035f80c4754c24e0b7bfdb920fb17265b1
DG <T3ll1nG@users.noreply.github.com>
2025-04-30 17:18:13 +0200

    theiaide: add ARM support
    Update Casks/t/theiaide.rb
    
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>
    
    Update Casks/t/theiaide.rb
    
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>
    
    Update Casks/t/theiaide.rb
    
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>
    
    Update Casks/t/theiaide.rb
    
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>
    
    Update Casks/t/theiaide.rb
    
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/t/theiaide.rb
+++ Casks/t/theiaide.rb
@@ -1,16 +1,19 @@
 cask "theiaide" do
-  version "1.60.200"
-  sha256 "c83ad723f7d3adb751c06d924cac6317eba8147533a5191e3b07626105ace53b"
+  arch arm: "-arm"
 
-  url "https://download.eclipse.org/theia/ide/#{version}/macos/TheiaIDE.dmg",
+  version "1.60.200"
+  sha256 arm:   "694e51126c637ce74b3ff707f081bf790ed8509dcd36bb5c5099e09931791972",
+         intel: "c83ad723f7d3adb751c06d924cac6317eba8147533a5191e3b07626105ace53b"
+
+  url "https://download.eclipse.org/theia/ide/#{version}/macos#{arch}/TheiaIDE.dmg",
       verified: "download.eclipse.org/theia/ide/"
   name "TheiaIDE"
   desc "IDE framework"
   homepage "https://theia-ide.org/"
 
   livecheck do
-    url "https://download.eclipse.org/theia/ide/latest/macos/"
-    regex(/href=.*?TheiaIDE[._-]v?(\d+(?:\.\d+)+)(?:-mac)?\.zip/i)
+    url "https://download.eclipse.org/theia/ide/latest/macos#{arch}/latest-mac.yml"
+    strategy :electron_builder
   end
 
   auto_updates true
@@ -26,8 +29,4 @@ cask "theiaide" do
     "~/Library/Preferences/eclipse.theia.plist",
     "~/Library/Saved Application State/eclipse.theia.savedState",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
