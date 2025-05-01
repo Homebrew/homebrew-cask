6870bf0f17cd32f1a448c86ab47e8fe0594e236d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-01 04:21:11 +0000

    hamrs-pro 2.36.2

--- Casks/h/hamrs-pro.rb
+++ Casks/h/hamrs-pro.rb
@@ -1,9 +1,9 @@
 cask "hamrs-pro" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.36.1"
-  sha256 arm:   "4ab159dcf81bb5c6452b4fa6d7f8b2299deec5d5cf48862d0ff9a8cc481e9f37",
-         intel: "96fc40fa18bae2cb780e42cc349a9c96fc6aa783f089f98d7de99420f61df50e"
+  version "2.36.2"
+  sha256 arm:   "66050aeae7dd09b14d9a333fd8a6fb63566c0f3c53d5f6d917e941d9d0bd4b6c",
+         intel: "d83efa98d557345349d647146f3af10c060037a103034c493c39e99041391187"
 
   url "https://hamrs-dist.s3.amazonaws.com/hamrs-pro-#{version}-mac-#{arch}.dmg",
       verified: "hamrs-dist.s3.amazonaws.com/"
