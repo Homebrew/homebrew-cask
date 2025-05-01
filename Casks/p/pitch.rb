56febdb8445247d5fc030fe693cf12439c1e07fe
Klaus Hipp <khipp@users.noreply.github.com>
2025-05-01 11:21:23 +0200

    pitch 2.72.0,stable.3,7194755

--- Casks/p/pitch.rb
+++ Casks/p/pitch.rb
@@ -1,6 +1,6 @@
 cask "pitch" do
-  version "2.72.0,stable.2,7191433"
-  sha256 "6d09b18a3e8fc98c3d18736a70be3a2f8c1fd153c2d123bb91f2b2e9d7c7a7d5"
+  version "2.72.0,stable.3,7194755"
+  sha256 "4a724aeaa3cda7e8d5927fcbbccdaff4c8c83077f7fd36e7dc2bcac1130d03ba"
 
   url "https://desktop-app-builds.pitch.com/Pitch-#{version.csv.first}-#{version.csv.second}-ci#{version.csv.third}.dmg"
   name "Pitch"
