58291effb531beeaeadc2d0e13b5b766fdd94374
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-01 12:52:54 +0000

    heptabase 1.55.9

--- Casks/h/heptabase.rb
+++ Casks/h/heptabase.rb
@@ -1,9 +1,9 @@
 cask "heptabase" do
   arch arm: "-arm64"
 
-  version "1.55.7"
-  sha256 arm:   "de765433f585405168ed73f8185909718fa248e395a87718d78c018f597e8d30",
-         intel: "9e83fb7e193190fb72e7d5df1d3331a9ece7438061ee7de1d5f2d1964c7b94c8"
+  version "1.55.9"
+  sha256 arm:   "c7ec5bf579b377c4c7e2eec65e454dbb5c15281b83b4e4602cd97a14e5c22bcd",
+         intel: "96a71a85e4940d238d103bbd9d426358299cdfe68c51116e48590286c23abd04"
 
   url "https://github.com/heptameta/project-meta/releases/download/v#{version}/Heptabase-#{version}#{arch}-mac.zip",
       verified: "github.com/heptameta/project-meta/"
