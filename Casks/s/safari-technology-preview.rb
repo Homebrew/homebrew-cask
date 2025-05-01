da5e56900688a7e67f776042e9ee622a965d02aa
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-05-01 13:34:50 +0900

    safari-technology-preview 218

--- Casks/s/safari-technology-preview.rb
+++ Casks/s/safari-technology-preview.rb
@@ -1,8 +1,8 @@
 cask "safari-technology-preview" do
   # when adjusting the on_{os} scoping, also update the livecheck regex
   on_sonoma :or_older do
-    version "217,082-24898-20250416-5e8886bb-673b-43bc-9ce2-87d0736dc075"
-    sha256 "d31bf2aac4d9edcac334f0e5fa733dadab30ab40c98ba25f74db794da50a0163"
+    version "218,082-41852-20250429-3d2f47a3-bdc5-4018-948b-72b536057034"
+    sha256 "39e11216615198d6bd10d2184fc4e4e0ee65fae64d5de823dce39318c1245d82"
 
     livecheck do
       url :homepage
@@ -18,8 +18,8 @@ cask "safari-technology-preview" do
     end
   end
   on_sequoia :or_newer do
-    version "217,082-24895-20250416-c0b70d7d-57a4-4cc4-a069-ed03f5dcb280"
-    sha256 "cea9d02a8d70de68ad54dbcf695fbeb40c91d25b19b57b38c7c8a94c3b06274f"
+    version "218,082-41661-20250429-c8c857e2-7213-4f54-8caf-096ac174005c"
+    sha256 "7b0f720e2c084151f74f09dd3e51c2d92430c8d62f72f249611a88be3dc83d8a"
 
     livecheck do
       url :homepage
