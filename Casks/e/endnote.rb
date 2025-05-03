b92bdc01cbb3d44cfdbac366ff55ddbcf6fd6214
Sam Ford <1584702+samford@users.noreply.github.com>
2025-05-03 10:56:15 -0400

    endnote: update version format, livecheck
    This modifies the `endnote` version to include the product version
    (e.g., X9, 2025, etc.) in the cask `version` and interpolates the
    value in the `url` and `container nested:` strings. This is primarily
    intended to ensure that a new major version update won't work
    unless/until the `version` is updated to include a product version.
    This allows us to set up the livecheck `strategy` block to add the
    existing product version to upstream versions in the XML file that
    use the same major version and omit it from versions with a different
    major. This setup ensures that a new major version from livecheck
    won't have a product version (e.g., `23.0.0.0`) and this will cause
    the cask to fail CI, unless/until the `version` is modified to include
    a product version. This will need to be manually identified, as
    mentioned in the relevant comment above the `version`.

--- Casks/e/endnote.rb
+++ Casks/e/endnote.rb
@@ -1,8 +1,12 @@
 cask "endnote" do
-  version "22.0.0.0"
+  # The second part of the `version` is the product version (e.g. X9, 2025).
+  # Until we can identify this using livecheck, we will need to manually find
+  # the new product version on each major version update. See:
+  # https://support.clarivate.com/Endnote/s/article/Download-EndNote
+  version "22.0.0.0,2025"
   sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://download.endnote.com/downloads/2025/EndNote2025Installer.dmg"
+  url "https://download.endnote.com/downloads/#{version.csv.second}/EndNote#{version.csv.second}Installer.dmg"
   name "EndNote"
   desc "Reference manager"
   homepage "https://endnote.com/"
@@ -10,22 +14,39 @@ cask "endnote" do
   # The XML file doesn't provide any update information for new major versions
   # (e.g. 22.0.0.0) until a subsequent version is released. The check will fail
   # when the cask is updated to a new major version but it should automatically
-  # work again when the next version is released.
+  # work again when the next version is released (i.e. livecheck will need to
+  # be skipped on CI for major version updates).
   livecheck do
-    url "https://download.endnote.com/updates/#{version.major}.0/EN#{version.major}MacUpdates.xml"
+    url "https://download.endnote.com/updates/#{version.csv.first.major}.0/EN#{version.csv.first.major}MacUpdates.xml"
     regex(/^v?(\d+(?:\.\d+)+)$/i)
     strategy :xml do |xml, regex|
+      current_major = version.csv.first.major
+      current_product_version = version.csv.second
+
       xml.get_elements("//updates/build").map do |item|
-        version = item.elements["updateTo"].text&.strip
-        match = version.match(regex) if version
+        update_to = item.elements["updateTo"].text&.strip
+        match = update_to.match(regex) if update_to
         next if match.blank?
 
-        match[1]
+        major = match[1][/^(\d+)/, 1]
+        if major == current_major && current_product_version
+          "#{match[1]},#{current_product_version}"
+        else
+          # This does not add the current product version to any versions in
+          # the upstream XML file that don't use the current major. This is
+          # intended to ensure that a major version update won't work until
+          # someone manually identifies the new product version (e.g. X9, 2025)
+          # and adds it as the second part of the cask `version` (e.g.
+          # `22.0.0.0,2025`). This `strategy` block setup can be reworked
+          # if/when upstream provides a way of identifying product versions
+          # that can be reliably checked by livecheck.
+          match[1]
+        end
       end
     end
   end
 
-  container nested: "Install EndNote 2025.app/Contents/Resources/EndNote.zip"
+  container nested: "Install EndNote #{version.csv.second}.app/Contents/Resources/EndNote.zip"
 
   suite "EndNote"
 
