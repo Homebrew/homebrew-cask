13b16d67c9a8414d9a4201d0c8ff48499f174ea2
Yusuf Kursad Kaya <ykursadkaya@gmail.com>
2025-05-03 14:57:45 +0300

    reaper 7.38

--- Casks/r/reaper.rb
+++ Casks/r/reaper.rb
@@ -1,8 +1,8 @@
 cask "reaper" do
-  version "7.37"
+  version "7.38"
 
   on_mojave :or_older do
-    sha256 "5a0225986dd66cba3768c3f2ecdfbd6613e9d5be5996e51d80b137a5e341dc22"
+    sha256 "4d57b2892b708665889f40a827a6f059b198858c16c5d45c18525cde448b3c1b"
 
     url "https://dlcf.reaper.fm/#{version.major}.x/reaper#{version.major_minor.no_dots}_x86_64.dmg"
 
@@ -11,7 +11,7 @@ cask "reaper" do
     end
   end
   on_catalina :or_newer do
-    sha256 "38bf60704171d2b9d5aefdd40ac4b43880993a5b6a882804ff7914bbe7e6adb2"
+    sha256 "d839b06ce435b0d54bde4ae51b6eca39d86ec1a95946369fb96cd6158896a678"
 
     url "https://dlcf.reaper.fm/#{version.major}.x/reaper#{version.major_minor.no_dots}_universal.dmg"
   end
