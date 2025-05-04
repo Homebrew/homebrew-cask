df94ef2d5117cf261874ddd9f9ca90390b0c6e00
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-04 00:16:55 +0000

    font-wdxl-lubrifont-jp-n: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml) workflow.

---
+++ Casks/font/font-w/font-wdxl-lubrifont-jp-n.rb
@@ -0,0 +1,12 @@
+cask "font-wdxl-lubrifont-jp-n" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/wdxllubrifontjpn/WDXLLubrifontJPN-Regular.ttf"
+  name "WDXL Lubrifont JP N"
+  homepage "https://github.com/NightFurySL2001/WD-XL-font"
+
+  font "WDXLLubrifontJPN-Regular.ttf"
+
+  # No zap stanza required
+end
