20fbfb6193db5ef547fe7b09d099cc9fe996bd70
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-04 12:47:49 +0000

    font-lxgw-simzhisong 1.022

--- Casks/font/font-l/font-lxgw-simzhisong.rb
+++ Casks/font/font-l/font-lxgw-simzhisong.rb
@@ -1,6 +1,6 @@
 cask "font-lxgw-simzhisong" do
-  version "1.021.2"
-  sha256 "84d8a2b498574a056aa3bdebcc14d0ff996c99116518adee41adc156341aca8c"
+  version "1.022"
+  sha256 "8bf5aac4e20c4114fbcb15146494ef46cbbdbbfb9b36efc1a915180f134ba5c2"
 
   url "https://github.com/lxgw/SimXiZhi/releases/download/v#{version}/SimZhiSong.ttf"
   name "LXGW SimZhiSong"
