e46d39d0feb5dec614f7bf698d5e49718a642a2d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-05 09:58:37 +0000

    soundanchor 1.3.1

--- Casks/s/soundanchor.rb
+++ Casks/s/soundanchor.rb
@@ -1,6 +1,6 @@
 cask "soundanchor" do
-  version "1.3.0"
-  sha256 "e98af6306da1626203f7a11168505e88bebea19ed4a5abebc6be154cd8eadadd"
+  version "1.3.1"
+  sha256 "8acc07fec6194c7d8eb175f4ae4bd1fa69e426f11f189ac0dd8811c423e23408"
 
   url "https://apps.kopiro.me/soundanchor/builds/SoundAnchor-#{version}.dmg"
   name "SoundAnchor"
