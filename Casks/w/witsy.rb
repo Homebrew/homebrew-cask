26b9c6129b9041bde3124efe0874c7ff7b220e4e
Nicolas Bonamy <nicolas@bonamy.fr>
2025-05-05 10:01:19 -0500

    witsy 2.5.0 (new cask)
    adding new cask for Witsy

---
+++ Casks/w/witsy.rb
@@ -0,0 +1,32 @@
+# typed: strict
+# frozen_string_literal: true
+
+cask "witsy" do
+  arch arm: "arm64", intel: "x86_64"
+
+  version "2.5.0"
+  sha256 arm:   "792e221ff74d8b9d76ca8a44cdd943aea1d8717811aa63b2396d647ab1d97f8b",
+         intel: "26ba273995a7a23cc38b92a287dbf92e5b4e67ce62b688134be62b7039e471f9"
+
+  url "https://github.com/nbonamy/witsy/releases/download/v#{version}/Witsy-#{version}-darwin-#{arch}.dmg",
+      verified: "github.com/nbonamy/witsy/"
+  name "Witsy"
+  desc ": desktop AI assistant / universal MCP client"
+  homepage "https://witsyai.com/"
+
+  livecheck do
+    url "https://witsyai.com/update/darwin"
+    strategy :json do |json|
+      json["name"]
+    end
+  end
+
+  auto_updates true
+
+  app "Witsy.app"
+
+  zap trash: [
+    "~/Library/Application Support/Witsy",
+    "~/Library/Logs/Witsy",
+  ]
+end
