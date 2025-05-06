1b4fd580c2ec11897434eed403eff073df5ff1ce
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-06 16:04:17 +0000

    semeru-jdk-open@11 11.0.27+6

--- Casks/s/semeru-jdk-open@11.rb
+++ Casks/s/semeru-jdk-open@11.rb
@@ -1,9 +1,9 @@
 cask "semeru-jdk-open@11" do
   arch arm: "aarch64", intel: "x64"
 
-  version "11.0.27+5,openj9-0.51.0-m2"
-  sha256 arm:   "d3966b1a1aae7519943a3f6456e3bfd24aff7279168fe25856de160c5186465e",
-         intel: "72628082ae590570c6eb8ab8c3f069a7b28f38bfade203bb028055474d06f099"
+  version "11.0.27+6,openj9-0.51.0"
+  sha256 arm:   "b4284149baad0cb3a8f6124a83845d4fa9659332a1cec04ea51a111ac90833c5",
+         intel: "f3f6c0981f7288367a8ac775b349db70e8d2bc67cfe9be8dfd4302c714adfd56"
 
   url "https://github.com/ibmruntimes/semeru#{version.major}-binaries/releases/download/jdk-#{version.csv.first}_#{version.csv.second}/ibm-semeru-open-jdk_#{arch}_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg",
       verified: "github.com/ibmruntimes/semeru#{version.major}-binaries/"
