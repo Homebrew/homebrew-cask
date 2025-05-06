22ad9d241dadd2ea71ad72950739245c9ae5a9f8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-06 15:47:21 +0000

    flyenv 4.9.8

--- Casks/f/flyenv.rb
+++ Casks/f/flyenv.rb
@@ -1,9 +1,9 @@
 cask "flyenv" do
   arch arm: "-arm64"
 
-  version "4.9.7"
-  sha256 arm:   "0c14164f3c7d32a4dea6ffcb87a4638538cffa9f822bc91247789dbe6e76b3cb",
-         intel: "2fb647aea082a0c909de1528280279421467aa9b006608d69e8fb00cc09bcdcd"
+  version "4.9.8"
+  sha256 arm:   "84edbc335f4abdd59c232e3c3c8292c9a70d05820664508e6d7e5749acd9b20a",
+         intel: "856a234b01b0a3fbb2714a30d1244503a58ab70c9d810717995817735aec9489"
 
   url "https://github.com/xpf0000/FlyEnv/releases/download/v#{version}/FlyEnv-#{version}#{arch}-mac.zip",
       verified: "github.com/xpf0000/FlyEnv/"
