e3e5f50c7c73ffc3b1509e1c46e604866d7b42ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-06 15:47:44 +0000

    focusrite-control-2 1.558.0.0

--- Casks/f/focusrite-control-2.rb
+++ Casks/f/focusrite-control-2.rb
@@ -1,7 +1,7 @@
 cask "focusrite-control-2" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "1.530.0.0"
-  sha256 "2697a6b9d0b2d610d09f22e51c54a7405cd3fd9bd6f0b9ce165cb3075a5727c7"
+  version "1.558.0.0"
+  sha256 "8cbe75e06798beb395e3df53ffc96c41f11ae65209a184bbb269249cdfe9263c"
 
   url "https://releases.focusrite.com/com.focusrite.focusrite-control/release/Focusrite-Control-2-#{version}.dmg"
   name "Focusrite Control 2"
