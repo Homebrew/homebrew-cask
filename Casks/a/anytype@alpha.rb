ff8d603fa272535071415ee361237425c18f15df
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-06 09:34:58 +0000

    anytype@alpha 0.46.14-alpha

--- Casks/a/anytype@alpha.rb
+++ Casks/a/anytype@alpha.rb
@@ -1,9 +1,9 @@
 cask "anytype@alpha" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.46.13-alpha"
-  sha256 arm:   "912da9c0877d20175cc92a074ecf5308049cf7ea845869d0ddf8cb43fe77cf8c",
-         intel: "db1f215c1f204e8663df4b7ec7ccaf2ad6b961b4b7ae1a8d64994dfe9c0f115e"
+  version "0.46.14-alpha"
+  sha256 arm:   "bbb6ffe1db8d4344430fff4df42eb8ed82995e109c881e0baafd96fbe04e29c8",
+         intel: "a0218d9d7ff7ce5cd567b4d271ae825611ba362d5825770fbc24cbf1ae2cb827"
 
   url "https://anytype-release.fra1.cdn.digitaloceanspaces.com/Anytype-#{version}-mac-#{arch}.dmg",
       verified: "anytype-release.fra1.cdn.digitaloceanspaces.com/"
