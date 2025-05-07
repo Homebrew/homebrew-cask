9decbde91b887013ea03487ab54f9c46733ad55e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-07 18:37:29 +0000

    1password-cli 2.31.0

--- Casks/1/1password-cli.rb
+++ Casks/1/1password-cli.rb
@@ -2,11 +2,11 @@ cask "1password-cli" do
   arch arm: "arm64", intel: "amd64"
   os macos: "darwin", linux: "linux"
 
-  version "2.30.3"
-  sha256 arm:          "0472133a09a0596119543c0ee7def0b3c08646354432516a18dbfe831d536c88",
-         x86_64:       "79fa39079b038bfefa8dafbc9e6cd1540ad869532ddbb6cf0a0b829edc7f4d48",
-         x86_64_linux: "a16307ebcecb40fd091d7a6ff4f0c380c3c0897c4f4616de2c5d285e57d5ee28",
-         arm64_linux:  "39bd361b2cd4819ea757583624eeda33f6062ba5ccfc2b90ad0764aed94cd1c4"
+  version "2.31.0"
+  sha256 arm:          "5253da8dff5dd6225bf865be8e4cf7b7a35b98253896aad976b5d0a5b731b663",
+         x86_64:       "2a3e9058b67521d664ab44ab189ed568206aca3ab59eed1b9eafb5f9ff077ae4",
+         x86_64_linux: "796dbfc3c00f1312688fd4de2f4002ffd45fb2393d5723755bccb02c52242730",
+         arm64_linux:  "869d2686a4d3ef3f87c97d0e324c1b80c39ca65879b89e201b97b4b6bf4a9c55"
 
   url "https://cache.agilebits.com/dist/1P/op2/pkg/v#{version}/op_#{os}_#{arch}_v#{version}.zip",
       verified: "cache.agilebits.com/dist/1P/op2/pkg/"
