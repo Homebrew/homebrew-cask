281f5781461564ca2ae51b8983a7bd912cb1b70c
Bevan Kay <email@bevankay.me>
2025-05-08 00:10:35 +1000

    hedy 2.3.1 (new cask)

---
+++ Casks/h/hedy.rb
@@ -0,0 +1,26 @@
+cask "hedy" do
+  version "2.3.1"
+  sha256 "065e475ef05b4934787beb684cdd376cd1b526ee72cfeaccbeb37c9616bc8365"
+
+  url "https://dl.hedy.ai/Hedy-MacOS-#{version}.dmg"
+  name "Hedy AI"
+  desc "AI-powered meeting coach"
+  homepage "https://hedy.ai/"
+
+  livecheck do
+    url "https://macos-update-xml.hedy.bot"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+  depends_on macos: ">= :sonoma"
+
+  app "Hedy.app"
+
+  uninstall quit: "bot.hedy.mobile"
+
+  zap trash: [
+    "~/Library/Application Scripts/bot.hedy.mobile",
+    "~/Library/Containers/bot.hedy.mobile",
+  ]
+end
