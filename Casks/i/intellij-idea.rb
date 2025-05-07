b0d5955f6d52640933d13d260ab20241a7017ffb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-07 15:49:20 +0000

    intellij-idea 2025.1.1

--- Casks/i/intellij-idea.rb
+++ Casks/i/intellij-idea.rb
@@ -1,9 +1,9 @@
 cask "intellij-idea" do
   arch arm: "-aarch64"
 
-  version "2025.1,251.23774.435"
-  sha256 arm:   "2877c9ec5cfef9afa09c489083aa4c93f400d35556953ee757a4ee599432c601",
-         intel: "86c568d302a2a25185fd912d82c6e99e423767ea1442c716f0ee34292736c35d"
+  version "2025.1.1,251.25410.109"
+  sha256 arm:   "f62879a25a3c63c4d32ea5e76df04d30663eda19a74a7ce1425f1e2616d26ebf",
+         intel: "16aad03ddc317d3bd3c124855ad8fa685a2bc6a6ef98e3a85de2bda745908274"
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version.csv.first}#{arch}.dmg"
   name "IntelliJ IDEA Ultimate"
