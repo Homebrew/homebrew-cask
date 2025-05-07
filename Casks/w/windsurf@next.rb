21d7fbe35ef66f940edba051d34b57d4d7d36c4d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-07 07:04:05 +0000

    windsurf@next 1.8.105+next.eccc45da0d

--- Casks/w/windsurf@next.rb
+++ Casks/w/windsurf@next.rb
@@ -1,9 +1,9 @@
 cask "windsurf@next" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.8.103+next.f9caedc3fb,f9caedc3fbe623d37250defe4970b5b2e8dc303d"
-  sha256 arm:   "a069a936b16017817d8c88fddd34d45e2fce389f4820669272eca83bc475a798",
-         intel: "333340e93354a17ff5c8c551f8d3dd5e387d7575f0f505f763f37491be5b66da"
+  version "1.8.105+next.eccc45da0d,eccc45da0d0c40e57275e0cce7db644c7b1278d8"
+  sha256 arm:   "ba93f2f1305faf639189284e46c74f685b7b81196f2a8f1a381067d4427e7bd2",
+         intel: "7bd42773cfd78d898901a0022aae5a03fb0cec1fc5837f9305a1c65ca05f622c"
 
   url "https://windsurf-stable.codeiumdata.com/darwin-#{arch}-dmg/next/#{version.csv.second}/Windsurf-darwin-#{arch}-#{version.csv.first}.dmg",
       verified: "windsurf-stable.codeiumdata.com/"
