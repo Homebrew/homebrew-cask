2d20099e2ea42bcf29d5e950bceedf56990ed1e3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-07 01:17:03 +0000

    wave 0.11.3

--- Casks/w/wave.rb
+++ Casks/w/wave.rb
@@ -1,9 +1,9 @@
 cask "wave" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.11.2"
-  sha256 arm:   "3a927f8de1a1000feeedd9febcda2db24facdfb79e5743e82bca5a86300cb8d6",
-         intel: "6c55fde92c9eac8d32caa06e80ba540a76bbf00f623178c1c8774f621dc1c6d5"
+  version "0.11.3"
+  sha256 arm:   "37d9e0472c2354c4d9f80eecb3f80d905fa5b58637cb3f375507f577aa8b2b85",
+         intel: "57a6b443899c4adfa20fb76356ac689547942de9aabf80b6324ca369f3b75426"
 
   url "https://dl.waveterm.dev/releases-w2/Wave-darwin-#{arch}-#{version}.dmg"
   name "Wave Terminal"
