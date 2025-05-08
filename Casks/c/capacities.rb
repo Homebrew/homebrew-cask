a771e97524878d17a5a152421a0938ec0d448ba8
Klaus Hipp <khipp@users.noreply.github.com>
2025-05-08 13:51:30 +0200

    capacities 1.48.3

--- Casks/c/capacities.rb
+++ Casks/c/capacities.rb
@@ -1,9 +1,9 @@
 cask "capacities" do
   arch arm: "-arm64"
 
-  version "1.47.2"
-  sha256 arm:   "46ee0c1ee99b624448a71c3cdd0c928449d71dce98e2415c1890a638f327d4da",
-         intel: "4a051ea831375ad80b01e3b2394d10234c5b56d3001555d2bdaf7c0fa856e716"
+  version "1.48.3"
+  sha256 arm:   "229450a738912e0d4b2e9cbf69e9aaaaa1dfa717058df717d13f2c2f20cd52c4",
+         intel: "98408821f35160ab929cfdb87e8a50142017825836ba79f14545c39e3bf70a85"
 
   url "https://capacities-desktop-app.fra1.cdn.digitaloceanspaces.com/Capacities-#{version}#{arch}.dmg",
       verified: "capacities-desktop-app.fra1.cdn.digitaloceanspaces.com/"
