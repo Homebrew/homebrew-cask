42a04adc1a05902e5cc3f04a8ca2ace787f7127c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-09 01:12:05 +0000

    topaz-video-ai 6.2.2

--- Casks/t/topaz-video-ai.rb
+++ Casks/t/topaz-video-ai.rb
@@ -1,6 +1,6 @@
 cask "topaz-video-ai" do
-  version "6.2.0"
-  sha256 "2c350ad469cc1dd8c1b8fddadcb6da91119eefcf6cfced02d4a12621e96e0790"
+  version "6.2.2"
+  sha256 "f49c2192d57d4a0d3c062fe7c3d60183c0c4aba27737958cdf4e0b929769de54"
 
   url "https://downloads.topazlabs.com/deploy/TopazVideoAI/#{version}/TopazVideoAI-#{version}.dmg"
   name "Topaz Video AI"
