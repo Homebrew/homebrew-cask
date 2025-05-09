1e646fdf7069cf8f922aa7b320eb435419268543
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-09 15:34:20 +0000

    115browser 35.19.0

--- Casks/1/115browser.rb
+++ Casks/1/115browser.rb
@@ -1,9 +1,9 @@
 cask "115browser" do
   arch arm: "arm64", intel: "x64"
 
-  version "35.18.0"
-  sha256 arm:   "2a9cfd1fa08d15402aa63cee287996269958eb1f0a311bb81c41cd50f0242497",
-         intel: "135029a79c1c9ea7b4f81cf232c9a8aa17c749819ccb0611e346806361603247"
+  version "35.19.0"
+  sha256 arm:   "6e4f67c3bdfd1fdb8834d4934b837ce382c5e2ab5b03faf3dab05b409c724454",
+         intel: "646abab066a0f2219de8dad21c71d8aea7ebdffb20f431b37213d9c4328d11b9"
 
   url "https://down.115.com/client/mac/115br_v#{version}_#{arch}.dmg"
   name "115Browser"
