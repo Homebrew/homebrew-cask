20bc20e1d1627cbd9e970980d7b7b2e3b736ccfc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-10 06:52:31 +0000

    openscad@snapshot 2025.05.09

--- Casks/o/openscad@snapshot.rb
+++ Casks/o/openscad@snapshot.rb
@@ -1,6 +1,6 @@
 cask "openscad@snapshot" do
-  version "2025.05.07"
-  sha256 "c0e23bf74208b514ff4ee29cb43e4bb090d15e8f5349216d5e8917930d1d3604"
+  version "2025.05.09"
+  sha256 "44ce6e1a17d5ff5b3228538720c2609e1f91b2622659d79ed2924af0ebf0ffad"
 
   url "https://files.openscad.org/snapshots/OpenSCAD-#{version}.dmg"
   name "OpenSCAD"
