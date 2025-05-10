6fc3d432a7aaa29df1261efab47015f52b1d6468
bevanjkay <email@bevankay.me>
2025-05-10 16:10:16 +1000

    chirp 20250509

--- Casks/c/chirp.rb
+++ Casks/c/chirp.rb
@@ -1,9 +1,10 @@
 cask "chirp" do
-  version "20250502"
-  sha256 "43a368770d827738faadb1e9e1124d238c3eeef2715f68fdffb48f4f345627f1"
+  version "20250509"
+  sha256 "caf9bb6d72a8e2c660e8b73647603eba7278be699edbbe3d24af364db1a33e17"
 
   url "https://archive.chirpmyradio.com/chirp_next/next-#{version}/chirp-next-#{version}.app.zip",
-      verified: "archive.chirpmyradio.com/"
+      verified: "archive.chirpmyradio.com/",
+      referer:  "https://archive.chirpmyradio.com/chirp_next/next-#{version}/"
   name "CHIRP"
   desc "Tool for programming amateur radio"
   homepage "https://chirp.danplanet.com/projects/chirp/wiki/Home"
