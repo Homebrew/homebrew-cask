7f4189b1f6713260a1f340df6fbe61ee77bb2bf4
Klaus Hipp <khipp@users.noreply.github.com>
2025-05-11 15:58:50 +0200

    electerm: add livecheck

--- Casks/e/electerm.rb
+++ Casks/e/electerm.rb
@@ -5,10 +5,18 @@ cask "electerm" do
   sha256 arm:   "ad3ad376240b294e22293d94922eb2706fdb8c3cf1255650291cedf849b95092",
          intel: "5abc965e4a0b0ba1d9b293c422852b17d4ebe9ec963feb0e57bd8ab6dde00f91"
 
-  url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac-#{arch}.dmg"
+  url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac-#{arch}.dmg",
+      verified: "github.com/electerm/electerm/"
   name "electerm"
   desc "Terminal/ssh/sftp client"
-  homepage "https://github.com/electerm/electerm/"
+  homepage "https://electerm.html5beta.com/"
+
+  livecheck do
+    url "https://electerm.html5beta.com/data/electerm-github-release.json"
+    strategy :json do |json|
+      json.dig("release", "tag_name")&.sub("v", "")
+    end
+  end
 
   auto_updates true
   depends_on macos: ">= :catalina"
