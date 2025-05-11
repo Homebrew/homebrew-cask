e98dd824736e9cc5e1f41d78e172bb68e9bb6579
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-11 15:49:03 +0000

    lunar 6.8.8

--- Casks/l/lunar.rb
+++ Casks/l/lunar.rb
@@ -1,6 +1,6 @@
 cask "lunar" do
-  version "6.8.7"
-  sha256 "fc9d0d5c8139edbd04c17f6d7aeb3b347dd35162e9dea87ee1d864f4b2705f26"
+  version "6.8.8"
+  sha256 "510d54bfdddb3d68dd602d2588640111a3380ec99ee9fb0a3a55d518578ba8c6"
 
   url "https://static.lunar.fyi/releases/Lunar-#{version}.dmg"
   name "Lunar"
