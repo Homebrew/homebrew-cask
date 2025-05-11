cf86a25c88b95ab1ab156f626033968dee05e6cc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-11 00:16:03 +0000

    font-baskervville-sc: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-b/font-baskervville-sc.rb
+++ Casks/font/font-b/font-baskervville-sc.rb
@@ -2,12 +2,12 @@ cask "font-baskervville-sc" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/baskervvillesc/BaskervvilleSC-Regular.ttf",
+  url "https://github.com/google/fonts/raw/main/ofl/baskervvillesc/BaskervvilleSC%5Bwght%5D.ttf",
       verified: "github.com/google/fonts/"
   name "Baskervville SC"
   homepage "https://fonts.google.com/specimen/Baskervville+SC"
 
-  font "BaskervvilleSC-Regular.ttf"
+  font "BaskervvilleSC[wght].ttf"
 
   # No zap stanza required
 end
