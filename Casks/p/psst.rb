cddccedde1e53c2b482cbc2b7d5609e2189e4afb
bevanjkay <email@bevankay.me>
2025-05-12 12:55:18 +1000

    psst 0.1.0,20250506.120019

--- Casks/p/psst.rb
+++ Casks/p/psst.rb
@@ -1,5 +1,5 @@
 cask "psst" do
-  version "0.1.0,20250420.085357"
+  version "0.1.0,20250506.120019"
   sha256 :no_check
 
   url "https://nightly.link/jpochyla/psst/workflows/build/main/Psst.dmg.zip",
