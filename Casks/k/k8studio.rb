19e0d3eb8c32f8ee1f0773c748302f976421137a
bevanjkay <email@bevankay.me>
2025-05-12 12:43:11 +1000

    k8studio 3.0.2-beta

--- Casks/k/k8studio.rb
+++ Casks/k/k8studio.rb
@@ -1,12 +1,12 @@
 cask "k8studio" do
   arch arm: "-arm64"
 
-  version "2.1.0"
-  sha256 arm:   "c20060a92770c3ebf22d185a95181637923a3f413b793ed8237b404b9709606f",
-         intel: "0d3ed9153c6266f46604b898f386eba05267466f493f62bac796a59b376e7855"
+  version "3.0.2-beta"
+  sha256 arm:   "1295963d5b5fab22346d20cd5920c4c1be0f772f4ba8b16ec09bc8cc31ff4e8e",
+         intel: "9ee02bcb9711df5782e73cedd2f65c652fae0fe80770290ea6e6a9fcd50598f7"
 
-  url "https://github.com/guiqui/k8Studio/releases/download/v#{version}/K8Studio-#{version}#{arch}.dmg",
-      verified: "github.com/guiqui/k8Studio/"
+  url "https://github.com/k8Studio/k8Studio/releases/download/v#{version}/K8Studio-#{version}#{arch}.dmg",
+      verified: "github.com/k8Studio/k8Studio/"
   name "K8studio"
   desc "Kubernetes GUI"
   homepage "https://k8studio.io/"
