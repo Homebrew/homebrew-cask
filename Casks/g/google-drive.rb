35307f1f73b3f6e4a911a30135c8dab808d82b30
bevanjkay <email@bevankay.me>
2025-05-12 12:37:49 +1000

    google-drive 108.0.1

--- Casks/g/google-drive.rb
+++ Casks/g/google-drive.rb
@@ -1,5 +1,5 @@
 cask "google-drive" do
-  version "107.0.3"
+  version "108.0.1"
   sha256 :no_check
 
   # "5-percent" is included in the url to ensure that `brew upgrade` does not update to an older version as the
