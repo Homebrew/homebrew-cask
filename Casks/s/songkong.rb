c750d8b34712976f852f1de2c6f676f8cb2c23c1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-12 16:05:11 +0000

    songkong 11.5.1

--- Casks/s/songkong.rb
+++ Casks/s/songkong.rb
@@ -1,7 +1,7 @@
 cask "songkong" do
   arch arm: "-m1"
 
-  version "11.5"
+  version "11.5.1"
   sha256 :no_check
 
   url "https://www.jthink.net/songkong/downloads/current/songkong-osx#{arch}.dmg"
