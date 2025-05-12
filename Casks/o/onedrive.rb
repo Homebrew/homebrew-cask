11a8c9eb9a29f888eaab9ec6e7c402d0c13863a5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-12 15:56:15 +0000

    onedrive 25.070.0413.0001

--- Casks/o/onedrive.rb
+++ Casks/o/onedrive.rb
@@ -1,6 +1,6 @@
 cask "onedrive" do
-  version "25.051.0317.0003"
-  sha256 "59024d3ec3198792ea2b01ce86400f5188756e663d89accad44e6b0ec708f553"
+  version "25.070.0413.0001"
+  sha256 "c209d5dab529f5bdc2cdda2b09754ba65dc22e70211633214e785021b6af1632"
 
   url "https://oneclient.sfx.ms/Mac/Installers/#{version}/universal/OneDrive.pkg",
       verified: "oneclient.sfx.ms/Mac/Installers/"
