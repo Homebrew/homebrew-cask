454337ddbf2b70e0c440d593885a5d5edfe0589b
Klaus Hipp <khipp@users.noreply.github.com>
2025-05-12 18:26:06 +0200

    psychopy 2025.1.1,2025-05-09_11-38

--- Casks/p/psychopy.rb
+++ Casks/p/psychopy.rb
@@ -1,8 +1,8 @@
 cask "psychopy" do
-  version "2025.1.0"
-  sha256 "cae2c76c19832568a913790a318d3260f090c1323c0b79bcc6971af5d51492f3"
+  version "2025.1.1,2025-05-09_11-38"
+  sha256 "9e791fb5323b2ae56f470a37f2a47738282bf2e853b653f44ff793094f3402fc"
 
-  url "https://github.com/psychopy/psychopy/releases/download/#{version.csv.first.major_minor_patch}/StandalonePsychoPy-#{version.csv.first}-macos#{"_#{version.csv.second}" if version.csv.second}-py3.10.dmg",
+  url "https://github.com/psychopy/psychopy/releases/download/#{version.csv.first.major_minor_patch}/StandalonePsychoPy-#{version.csv.first}-macOS#{"_#{version.csv.second}" if version.csv.second}_3.10.dmg",
       verified: "github.com/psychopy/psychopy/"
   name "PsychoPy"
   desc "Create experiments in behavioral science"
@@ -10,7 +10,7 @@ cask "psychopy" do
 
   livecheck do
     url :url
-    regex(/StandalonePsychoPy[._-]v?(\d+(?:\.\d+)+)[._-]macos[._-]?(\d+(?:[._-]\d+)+)?[._-](?:py)?3\.10\.dmg/i)
+    regex(/StandalonePsychoPy[._-]v?(\d+(?:\.\d+)+)[._-]macOS[._-]?(\d+(?:[._-]\d+)+)?[._-](?:py)?3\.10\.dmg/i)
     strategy :github_releases do |json, regex|
       json.map do |release|
         next if release["draft"] || release["prerelease"]
