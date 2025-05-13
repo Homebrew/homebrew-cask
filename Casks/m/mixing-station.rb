53209164c93b48b5c8b32b5c261e205e498931eb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-13 09:51:49 +0000

    mixing-station 2.3.1

--- Casks/m/mixing-station.rb
+++ Casks/m/mixing-station.rb
@@ -1,5 +1,5 @@
 cask "mixing-station" do
-  version "2.3.0"
+  version "2.3.1"
   sha256 :no_check
 
   url "https://mixingstation.app/backend/api/web/download/attachment/mixing-station-pc/release/OSX"
