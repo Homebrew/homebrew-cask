98723dba704aa3d3a3b26fa30c985ad1c74e9363
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-13 18:47:28 +0000

    dotnet@preview 9.0.5

--- Casks/d/dotnet@preview.rb
+++ Casks/d/dotnet@preview.rb
@@ -1,9 +1,9 @@
 cask "dotnet@preview" do
   arch arm: "arm64", intel: "x64"
 
-  version "9.0.4"
-  sha256 arm:   "402b1ddceb00074795d8221d4f9dd46c97da6a86fac372977b093fa0204f5fca",
-         intel: "c34d34a34a9b9f647c65639d1f8c3a6141f8de9f0098bc4e79d5f1d40bfbe262"
+  version "9.0.5"
+  sha256 arm:   "26f22a99c4cc07d98253d48574df0bde61acef8757e33a4706fd002451b7537b",
+         intel: "cddf416c680829e84e29b5886d8d5345474547dfe6ebf453870c36c6581faaa1"
 
   url "https://builds.dotnet.microsoft.com/dotnet/Runtime/#{version}/dotnet-runtime-#{version}-osx-#{arch}.pkg"
   name ".Net Runtime"
