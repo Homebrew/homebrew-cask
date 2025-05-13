167095637084453b041349dff083e1a29a1e11d3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-13 09:37:11 +0000

    clipbook 1.27.0

--- Casks/c/clipbook.rb
+++ Casks/c/clipbook.rb
@@ -1,9 +1,9 @@
 cask "clipbook" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.26.0"
-  sha256 arm:   "0db4ac698d8c9252e3ae4f5ff8228eaa86039da08aff882a1ddf3c7fbe85c435",
-         intel: "fa18ebf44130fb1c52845b289e6244954afacdccb0f2d6e30fd0db3821a69da8"
+  version "1.27.0"
+  sha256 arm:   "f584f6c06031b0b6f08c64bfb16a47609975db84d7303b8749572b2c05cab11f",
+         intel: "8005c7be31d93598d38f21eeac1d1cce1fa164ca253478303c6b748fdac52c39"
 
   url "https://f005.backblazeb2.com/file/clipbook/ClipBook-#{version}-#{arch}.dmg",
       verified: "f005.backblazeb2.com/file/clipbook/"
