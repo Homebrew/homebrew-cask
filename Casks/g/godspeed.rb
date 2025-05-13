d4a6f997502cec7492dc0efe99801afaa1b9cd6d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-13 06:47:42 +0000

    godspeed 1.8.98

--- Casks/g/godspeed.rb
+++ Casks/g/godspeed.rb
@@ -1,6 +1,6 @@
 cask "godspeed" do
-  version "1.8.97"
-  sha256 "8c97c9ff3965478ede4bc1b3a56d1717a441e6f420137d2fb63f2a78354add11"
+  version "1.8.98"
+  sha256 "00cefc5b0a1b6815f5154abd0d721f02906b6a7df61845fa9949920b1865ff85"
 
   url "https://app-updates.godspeedapp.com/#{version}%2FGodspeed.zip"
   name "Godspeed"
