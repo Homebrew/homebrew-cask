e31d8bf8d093a4575fddeb5743c4b6fc2ac64920
Baptiste Roux <arte.but.posix@gmail.com>
2025-05-14 15:51:37 +0200

    revert: livecheck customization

--- Casks/f/finicky.rb
+++ Casks/f/finicky.rb
@@ -7,22 +7,9 @@ cask "finicky" do
   desc "Utility for customizing which browser to start"
   homepage "https://github.com/johnste/finicky"
 
-  # The v4.0.0 release was flagged as a pre-release to avoid unattended auto-updates for old clients.
-  # Once the version is marked as stable the livecheck must be reverted to the standard github_releases strategy.
-  # An audit exception is set to allow pre-release versions, this will need to be removed as well.
   livecheck do
     url :url
-    regex(/^\D*?(\d+(?:\.\d+)+)$/i)
-    strategy :github_releases do |json, regex|
-      json.map do |release|
-        next if release["draft"]
-
-        match = release["tag_name"]&.match(regex)
-        next if match.blank?
-
-        match[1]
-      end
-    end
+    strategy :github_latest
   end
 
   depends_on macos: ">= :monterey"
