d0f9d7f1ff71d2ef9732cf6990f6add0c149eabe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-14 15:40:04 +0000

    dbgate 6.4.2

--- Casks/d/dbgate.rb
+++ Casks/d/dbgate.rb
@@ -1,6 +1,6 @@
 cask "dbgate" do
-  version "6.4.1"
-  sha256 "7cafa61a882bf26741a60f6d18adb7ccd0efef3afd0c95aaf18abee5d75db976"
+  version "6.4.2"
+  sha256 "c960dc217ac3f39aabfb3c79b1bb5c8b5630fc2ea05235bb34d8dfbbfb0295ab"
 
   url "https://github.com/dbgate/dbgate/releases/download/v#{version}/dbgate-#{version}-mac_universal.dmg",
       verified: "github.com/dbgate/dbgate/"
