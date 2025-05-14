5c599e5dee468a1a0e97a9b0ce88153f07129105
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-14 09:37:06 +0000

    bitrix24 17.0.17.84

--- Casks/b/bitrix24.rb
+++ Casks/b/bitrix24.rb
@@ -2,7 +2,7 @@ cask "bitrix24" do
   arch arm: "macos_arm", intel: "desktop"
 
   # NOTE: "24" is not a version number, but an intrinsic part of the product name
-  version "16.0.37.83"
+  version "17.0.17.84"
   sha256 :no_check
 
   url "https://dl.bitrix24.com/b24/bitrix24_#{arch}.dmg"
