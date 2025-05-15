13e176aa0d2cb57dfa245b96178be752874ca826
mfcmatheus <mfcmatheus@gmail.com>
2025-05-15 19:29:16 +0200

    Fix errors

--- Casks/w/wrkspace.rb
+++ Casks/w/wrkspace.rb
@@ -2,10 +2,12 @@
 #                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 cask "wrkspace" do
+  arch arm: "arm64", intel: "amd64"
   version "2.0.9"
   sha256 :no_check
 
-  url "https://updater.wrkspace.co/download/darwin"
+  url "https://updater.wrkspace.co/download/darwin-#{arch}"
+
   name "Wrkspace"
   desc "All-in-one dev bootstrapper: one-click startup for Docker, scripts, editor, and URLs"
   homepage "https://wrkspace.co"
@@ -13,10 +15,13 @@ cask "wrkspace" do
   # Documentation: https://docs.brew.sh/Brew-Livecheck
   livecheck do
     url :url
+    regex(/Wrkspace[._-]v?(\d+(?:\.\d+)+)(?:-arm64)?\.dmg/i)
     strategy :header_match
   end
 
   app "Wrkspace.app"
+
+  depends_on macos: ">= :high_sierra"
 
   # Documentation: https://docs.brew.sh/Cask-Cookbook#stanza-zap
   zap trash: [
