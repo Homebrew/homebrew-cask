1ef3feb95517ccaaf859455c8c4015e326b009ba
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-15 12:45:20 +0000

    burp-suite-professional 2025.4.2

--- Casks/b/burp-suite-professional.rb
+++ Casks/b/burp-suite-professional.rb
@@ -1,9 +1,9 @@
 cask "burp-suite-professional" do
   arch arm: "MacOsArm64", intel: "MacOsx"
 
-  version "2025.3.4"
-  sha256 arm:   "8a5a36c4138c432db80d8a38a19ae7ff5d1cf9257ced3952e4c7c10c0aa1cab7",
-         intel: "3e960c03350414d1c6f3457b97b937d0bcd8bc0e65964fb0ebf2f48e099bfb6a"
+  version "2025.4.2"
+  sha256 arm:   "64040b0cfcc5f385bdc79e0397ba3a0ed0445e295ea7234e05ff25e53e949389",
+         intel: "ade19e65eb6fabdf31f12949dfe36d7ef91d67d37dd5b3efe52fb677650052f8"
 
   url "https://portswigger-cdn.net/burp/releases/download?product=pro&version=#{version}&type=#{arch}",
       verified: "portswigger-cdn.net/burp/releases/"
