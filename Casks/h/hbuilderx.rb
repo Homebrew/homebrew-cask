ef8768ef0bb7734ba4372e71be779016dbdce610
Klaus Hipp <khipp@users.noreply.github.com>
2025-05-15 09:32:15 +0200

    hbuilderx: add arm arch

--- Casks/h/hbuilderx.rb
+++ Casks/h/hbuilderx.rb
@@ -1,8 +1,11 @@
 cask "hbuilderx" do
-  version "4.65.2025051206"
-  sha256 "6095974a84c3b73605145eae9106f5dd02b6863d5ac688732211116b811a7adc"
+  arch arm: ".arm64"
 
-  url "https://download1.dcloud.net.cn/download/HBuilderX.#{version}.dmg",
+  version "4.65.2025051206"
+  sha256 arm:   "a9be273f127f6bdc80bc09a249522ea586ca3dd1a23592cdefc1fd5ba553f60c",
+         intel: "6095974a84c3b73605145eae9106f5dd02b6863d5ac688732211116b811a7adc"
+
+  url "https://download1.dcloud.net.cn/download/HBuilderX.#{version}#{arch}.dmg",
       verified: "download1.dcloud.net.cn/download/"
   name "HBuilderX"
   desc "HTML editor"
@@ -18,8 +21,4 @@ cask "hbuilderx" do
   app "HBuilderX.app"
 
   zap trash: "~/Library/Application Support/HBuilder X"
-
-  caveats do
-    requires_rosetta
-  end
 end
