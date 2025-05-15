d75622a04a11c99e453fde0a2724efa752bb607e
Klaus Hipp <khipp@users.noreply.github.com>
2025-05-15 13:43:29 +0200

    aqua-voice 0.3.11

--- Casks/a/aqua-voice.rb
+++ Casks/a/aqua-voice.rb
@@ -1,9 +1,9 @@
 cask "aqua-voice" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.3.10"
-  sha256 arm:   "44e77a8a51342edbec168807bad1c1543d87beee98c68ae91668b9e14cad3634",
-         intel: "ac23a0b7577614c26e25a4f5b56ac1c8410ba4ce6dae6724854ca477e3c2d3c0"
+  version "0.3.11"
+  sha256 arm:   "cd2faed77845f2aa5811c9ffae979218d3b54bc2003336a35d2d24c499b40848",
+         intel: "563e25ed6393c274f37f028905a59d15c6b8983f280a17c983c2d60eb037e87e"
 
   url "https://d1a1dx1sgvjqrz.cloudfront.net/aqua-voice-updates/darwin/#{arch}/Aqua+Voice-#{version}-#{arch}.dmg",
       verified: "d1a1dx1sgvjqrz.cloudfront.net/"
