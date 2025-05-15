5dcf6954edb572310f6325ed07861ace39ca35ae
Klaus Hipp <khipp@users.noreply.github.com>
2025-05-15 09:44:55 +0200

    podman-desktop 1.18.1

--- Casks/p/podman-desktop.rb
+++ Casks/p/podman-desktop.rb
@@ -1,9 +1,9 @@
 cask "podman-desktop" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.18.0"
-  sha256 arm:   "dbff4ceb2d9caeab12e8a66f49d9e07399652606cfab115be129ade26a301feb",
-         intel: "b289d0ba4f6b8ccbcaa3b587306932bd09cae56af291ce4f8b7d8c1bf442d7fa"
+  version "1.18.1"
+  sha256 arm:   "2dd88c6aa47e00b35421404d1e7c0ea2ae8e74579f47bd1a0792e52e3c0b6e19",
+         intel: "b7a1d3aa2c780ffae833fcecc94ad6bf026f6413ff567c9ef1f8456853dadbb1"
 
   url "https://github.com/containers/podman-desktop/releases/download/v#{version}/podman-desktop-#{version}-#{arch}.dmg",
       verified: "github.com/containers/podman-desktop/"
