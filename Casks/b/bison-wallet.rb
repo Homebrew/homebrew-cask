092ca882eb6a62e8be9809ddf109abc62e2a46d2
Philemon Ukane <ukanephilemon@gmail.com>
2025-05-16 21:33:54 +0100

    Add bison-wallet cask

---
+++ Casks/b/bison-wallet.rb
@@ -0,0 +1,21 @@
+cask "bison-wallet" do
+  arch arm: "arm64", intel: "amd64"
+
+  version "1.0.3"
+  sha256 arm:   "0dcafd9509b435a5e762aeb11cbc63baa084dc71c58d017072707bc292c72bbc",
+         intel: "c0433ab203a5b9c631bb4c6bc21edaa27b90d6266203ef1c16c6c6a7a6a53c74"
+
+  url "https://github.com/decred/dcrdex/releases/download/v#{version}/bisonw-desktop-darwin-#{arch}-v#{version}.dmg"
+  name "Bison Wallet"
+  desc "Multi-coin wallet with built-in feeless DEX that offers atomic swaps, market-making, arbitrage, and more."
+  homepage "https://github.com/decred/dcrdex"
+  
+  depends_on macos: ">= :catalina"
+
+  app "Bison Wallet.app"
+
+  zap trash: [
+    "~/Library/Application Support/Dexc",
+    "~/Library/Preferences/com.decred.dcrdex.plist",
+  ]
+end
