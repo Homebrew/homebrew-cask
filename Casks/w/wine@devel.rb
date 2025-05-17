944e175293a2f5a83682fdddec8d13426bd6fe19
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-17 04:12:47 +0000

    wine@devel 10.8

--- Casks/w/wine@devel.rb
+++ Casks/w/wine@devel.rb
@@ -1,6 +1,6 @@
 cask "wine@devel" do
-  version "10.7"
-  sha256 "c0ac812525e1b9c858b03ac0c4f886f949696d21cc3cdf7bdfeab9840d001a85"
+  version "10.8"
+  sha256 "9abcfa8e06d0c1c7b8c4b54d11fc0a6c184c103964305ed0b967b4454f7d5274"
 
   # Current winehq packages are deprecated and these are packages from
   # the new maintainers that will eventually be pushed to Winehq.
