14c2e195826b042958dd06fb27dc07c6acc9020a
leaves chen <leaves615@gmail.com>
2025-05-17 19:16:36 +0800

    keyboardholder 1.13.8

--- Casks/k/keyboardholder.rb
+++ Casks/k/keyboardholder.rb
@@ -1,6 +1,6 @@
 cask "keyboardholder" do
-  version "1.13.7"
-  sha256 "4927d1bdd9eaa5cd41f5680b356cd80bf516129f72dc2e0868ff7811f2c3ce54"
+  version "1.13.8"
+  sha256 "9233f4fe0b3293b681566ce88fa62b1979d5a234367a96f2bb1c9ebe2d939933"
 
   url "https://github.com/leaves615/KeyboardHolder/releases/download/v#{version}/KeyboardHolder-#{version}.zip",
       verified: "github.com/leaves615/KeyboardHolder/"
