4c7797cd5ae7d7f2244d21093d883fe463534451
Klaus Hipp <khipp@users.noreply.github.com>
2025-05-17 15:33:11 +0200

    istat-server: update url

--- Casks/i/istat-server.rb
+++ Casks/i/istat-server.rb
@@ -2,8 +2,7 @@ cask "istat-server" do
   version "3.03"
   sha256 "0d6df9abe88aa7b29f53abb63413ede2853823cf6fd75b75818ef0190a07e8c7"
 
-  url "https://bjango.s3.amazonaws.com/files/istatserver#{version.major}/istatserver#{version}.zip",
-      verified: "bjango.s3.amazonaws.com/"
+  url "https://files.bjango.com/istatserver#{version.major}/istatserver#{version}.zip"
   name "iStat Server"
   desc "Transmits computer or server’s vital statistics"
   homepage "https://bjango.com/istatserver/"
