1a9505ae150c8cd510dfbf45241033d449041dfb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-17 09:44:18 +0000

    intune-company-portal 5.2504.0

--- Casks/i/intune-company-portal.rb
+++ Casks/i/intune-company-portal.rb
@@ -1,6 +1,6 @@
 cask "intune-company-portal" do
-  version "5.2502.1"
-  sha256 "72f773eec677deff1ca4440d6cb6c0b3c0e669c50c4966a56205c62e2f3f100b"
+  version "5.2504.0"
+  sha256 "abcc01870e7b1887ddf0bf51fa63602d1572b446bea4cb6c0a61cc7069a27c43"
 
   url "https://officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/CompanyPortal_#{version}-Upgrade.pkg"
   name "Company Portal"
