0c75ea50c79ac288cde7928d6565739ce820551d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-18 00:16:29 +0000

    font-playpen-sans-thai: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-p/font-playpen-sans-thai.rb
+++ Casks/font/font-p/font-playpen-sans-thai.rb
@@ -2,9 +2,10 @@ cask "font-playpen-sans-thai" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/playpensansthai/PlaypenSansThai%5Bwght%5D.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/playpensansthai/PlaypenSansThai%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Playpen Sans Thai"
-  homepage "https://github.com/TypeTogether/Playpen-Sans"
+  homepage "https://fonts.google.com/specimen/Playpen+Sans+Thai"
 
   font "PlaypenSansThai[wght].ttf"
 
