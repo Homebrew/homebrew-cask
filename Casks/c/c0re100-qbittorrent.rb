e260ed72d4f05619bf7183ed32aa7175032e3db7
Junyong Zhao <junyong@arizona.edu>
2025-05-17 18:31:40 -0700

    fix c0re100-qbittorrent 5.1.0.10 checksum

--- Casks/c/c0re100-qbittorrent.rb
+++ Casks/c/c0re100-qbittorrent.rb
@@ -1,6 +1,6 @@
 cask "c0re100-qbittorrent" do
   version "5.1.0.10"
-  sha256 "1fe6e784a464b2f201b99359044166c7d53480ad3838f78117a50435521000df"
+  sha256 "99ba143a33236ff25eb917e3ce04370f170c21c34164c5446d604afdd8263bcc"
 
   url "https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-#{version}/qBittorrent-Enhanced-Edition-release-#{version}-macOS-universal.dmg"
   name "qBittorrent Enhanced Edition"
