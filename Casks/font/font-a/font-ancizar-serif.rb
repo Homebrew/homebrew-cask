1731c22ea8082193d3663e6abc29b0f2e15e9301
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-18 00:16:29 +0000

    font-ancizar-serif: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-a/font-ancizar-serif.rb
+++ Casks/font/font-a/font-ancizar-serif.rb
@@ -3,10 +3,11 @@ cask "font-ancizar-serif" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/ancizarserif"
   name "Ancizar Serif"
-  homepage "https://github.com/UNAL-OMD/UNAL-Ancizar"
+  homepage "https://fonts.google.com/specimen/Ancizar+Serif"
 
   font "AncizarSerif-Italic[wght].ttf"
   font "AncizarSerif[wght].ttf"
