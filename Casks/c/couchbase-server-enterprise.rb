a39b868bdb6645b995f9f692eb9ab7ec4e2fe922
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-19 18:44:39 +0000

    couchbase-server-enterprise 7.6.6

--- Casks/c/couchbase-server-enterprise.rb
+++ Casks/c/couchbase-server-enterprise.rb
@@ -1,9 +1,9 @@
 cask "couchbase-server-enterprise" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "7.6.5"
-  sha256 arm:   "bd839693ca66a457c3613e1af129647ad4d189b6bf4ecdd76e106d9550213110",
-         intel: "0f7af6fcf7c350dc2afa5b57a28604e95dcd4b932f5fdf82108964f3ca18c97b"
+  version "7.6.6"
+  sha256 arm:   "7ef56a7addbdaae89d7c361ffe1d488e4ccad2f402a9f319973a91212a80ac25",
+         intel: "3f699dbd3827a996adb2247b9a7a3064ea0200ff9d25cc48f478e829aac1a853"
 
   url "https://packages.couchbase.com/releases/#{version}/couchbase-server-enterprise_#{version}-macos_#{arch}.dmg"
   name "Couchbase Server"
