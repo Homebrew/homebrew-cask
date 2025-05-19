aeec9f146b2782d3bda1f229e6a3b0e4fd89f368
Klaus Hipp <khipp@users.noreply.github.com>
2025-05-19 09:56:42 +0200

    tidelift: add arm arch and linux support

--- Casks/t/tidelift.rb
+++ Casks/t/tidelift.rb
@@ -1,15 +1,21 @@
 cask "tidelift" do
-  version "1.16.16"
-  sha256 "70d9746dae8e1c57ad0e7f303c68988531b7a3de1cef7ef5813b7b8a2323e87b"
+  arch arm: "_arm"
+  os macos: "darwin", linux: "linux"
 
-  url "https://download.tidelift.com/cli/#{version}/darwin_homebrew/tidelift"
+  version "1.16.16"
+  sha256 arm:          "20b12693ed2b110ae6b873265058dab9c36c0e33497ccec8686b5d024aa25bb0",
+         intel:        "557fb2105b0d3659187ac35c2307442b283ab2f3ad1acf183631c7f1bf6a3c9f",
+         arm64_linux:  "ff23e44684f806dd6ccb6662ab5318fc38d7cc04c4007a514d9e2d59eb501041",
+         x86_64_linux: "040ff98070d95fb6d7f4276510c6690608827cb0e7133a13fe0ed83a37570a04"
+
+  url "https://download.tidelift.com/cli/#{version}/#{os}#{arch}/tidelift"
   name "Tidelift CLI"
   desc "Tool to interact with the Tidelift system"
   homepage "https://tidelift.com/cli"
 
   livecheck do
     url "https://download.tidelift.com/cli/index.html"
-    regex(%r{href=.*?/cli/(\d+(?:\.\d+)+)/darwin/tidelift}i)
+    regex(%r{href=.*?/cli/(\d+(?:\.\d+)+)/#{os}#{arch}/tidelift}i)
   end
 
   binary "tidelift"
@@ -19,8 +25,4 @@ cask "tidelift" do
   end
 
   # No zap stanza required
-
-  caveats do
-    requires_rosetta
-  end
 end
