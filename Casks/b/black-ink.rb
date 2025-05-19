85e020d26a34c8a2f2301286a2068a53bad5a288
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-19 15:36:15 +0000

    black-ink 2.4.2

--- Casks/b/black-ink.rb
+++ Casks/b/black-ink.rb
@@ -1,6 +1,6 @@
 cask "black-ink" do
-  version "2.4.1"
-  sha256 "79ee076410a87c9003407574d25b075299de55285ec4047f92d1d485ae4a6a1c"
+  version "2.4.2"
+  sha256 "6712e870a49e0c96dde0223e120f91cfc67726c422200a56f0e02574e9ff421e"
 
   url "https://redsweater.com/blackink/BlackInk#{version}.zip"
   name "Black Ink"
