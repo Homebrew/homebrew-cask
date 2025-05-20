8337e96a32a040cb2a25fef54b54196b6e23e055
Nathan Clayton <nathanclayton@users.noreply.github.com>
2025-05-20 12:18:37 -0700

    simplex: Fix SHA-256 hashes
    Updated hashes per release notes at https://github.com/simplex-chat/simplex-chat/releases/tag/v6.3.4

--- Casks/s/simplex.rb
+++ Casks/s/simplex.rb
@@ -2,8 +2,8 @@ cask "simplex" do
   arch arm: "aarch64", intel: "x86_64"
 
   version "6.3.4"
-  sha256 arm:   "3d688d3c9fc16d9258bbc448e1d9e8c6bb6f8219a5e672ee116033bf94cbcf67",
-         intel: "82dc688a5926a250e30aff22e253487476cd36a01ee20e95f06fc3ca56c4ac7b"
+  sha256 arm:   "e2c11dd46d75fc05af37b3ece1320fbefbd58e6aa244c99636cef6165cf2ab43",
+         intel: "01eafa1774bb7658f63b436f782e34d089ed57a0cb9cb000b9b08cca9de9b33d"
 
   url "https://github.com/simplex-chat/simplex-chat/releases/download/v#{version}/simplex-desktop-macos-#{arch}.dmg",
       verified: "github.com/simplex-chat/simplex-chat/"
