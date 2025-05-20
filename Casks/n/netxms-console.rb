f13bb0671867db46e2cee053fb3d48dd0ec8a6a7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-20 16:00:05 +0000

    netxms-console 5.2.2

--- Casks/n/netxms-console.rb
+++ Casks/n/netxms-console.rb
@@ -1,9 +1,9 @@
 cask "netxms-console" do
   arch arm: "-aarch64"
 
-  version "5.2.1"
-  sha256 arm:   "8868589e0d3947524ec812bd2e449c71cd6325dc29a96edfb94b5259a4572020",
-         intel: "2637d1e599f6b99c19d2af8067298bca6d5300fffb33ca8b79b892222a36b3a0"
+  version "5.2.2"
+  sha256 arm:   "658683b89b304e14b6c18b08057f1510bbba0b867b6c0788cc349c295d665eec",
+         intel: "fe7575f561b48cc20bbbeb129647b8718f00180f6a79492938de8a685dda54d0"
 
   url "https://netxms.org/download/releases/#{version.major_minor}/nxmc-#{version}#{arch}.dmg"
   name "NetXMS Management Console"
