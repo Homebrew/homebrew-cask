5818be9ab3530cd8fd809645f2ca41ae33d18a48
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-21 22:02:39 +0000

    trader-workstation 10.37.1g

--- Casks/t/trader-workstation.rb
+++ Casks/t/trader-workstation.rb
@@ -1,7 +1,7 @@
 cask "trader-workstation" do
   arch arm: "-arm", intel: "x-x64"
 
-  version "10.37.1f"
+  version "10.37.1g"
   sha256 :no_check
 
   url "https://download2.interactivebrokers.com/installers/tws/latest/tws-latest-macos#{arch}.dmg"
