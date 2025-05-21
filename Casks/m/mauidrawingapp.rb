bc40f874fbafbef822f55b5e6958f002ccfae78f
Anusha Viswanathan <anusha.viswanathan@syncfusion.com>
2025-05-22 00:30:54 +0530

    Drawing App Package Changes Added

---
+++ Casks/m/mauidrawingapp.rb
@@ -0,0 +1,12 @@
+cask "mauidrawingapp" do
+  version "1.0.1"
+  sha256 "dd5bd949ef3cc567afa23f5d9b75eb7bdb0435589460885c7ff7f9ab3c01dcc3"
+
+  url "https://s3.us-east-1.amazonaws.com/files2.syncfusion.com/ContinueAI/TestContinue/Homebrew/apple-silicon/MauiDrawingApp.dmg"
+  name "MauiDrawingApp"
+  desc "This is the Maui Drawing App for MAC OS to sketch your Ideas"
+
+
+  app "MauiDrawingApp.app"
+
+end
