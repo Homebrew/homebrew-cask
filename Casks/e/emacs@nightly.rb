f2075ffbd2063a5948fa28b9b441795b9da19ecf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-21 09:44:08 +0000

    emacs@nightly 2025-05-21_00-09-06

--- Casks/e/emacs@nightly.rb
+++ Casks/e/emacs@nightly.rb
@@ -1,8 +1,8 @@
 cask "emacs@nightly" do
   arch arm: "arm64-11", intel: "x86_64-10_12"
 
-  version "2025-03-20_00-09-09,a7d2aa7e0c1a0984d6d1a3563b407efda123d6ae"
-  sha256 "ae62003c32606b8d2f8a3db7203ee32b635352f592924c9478a9bfc9bee5b82d"
+  version "2025-05-21_00-09-06,822b6cca1e1f80ab6bd28d03f29d288cc6f6ad0e"
+  sha256 "7c06af4ff4dfadd12cdf9106556da3e8d20434499c05e1a2f0e79e3b87afe378"
 
   url "https://emacsformacosx.com/emacs-builds/Emacs-#{version.csv.first}-#{version.csv.second}-universal.dmg"
   name "Emacs"
@@ -28,11 +28,9 @@ cask "emacs@nightly" do
 
   app "Emacs.app"
   binary "#{appdir}/Emacs.app/Contents/MacOS/Emacs", target: "emacs"
-  binary "#{appdir}/Emacs.app/Contents/MacOS/bin-#{arch}/ctags"
   binary "#{appdir}/Emacs.app/Contents/MacOS/bin-#{arch}/ebrowse"
   binary "#{appdir}/Emacs.app/Contents/MacOS/bin-#{arch}/emacsclient"
   binary "#{appdir}/Emacs.app/Contents/MacOS/bin-#{arch}/etags"
-  manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/ctags.1.gz"
   manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/ebrowse.1.gz"
   manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/emacs.1.gz"
   manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/emacsclient.1.gz"
