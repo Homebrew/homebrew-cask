e8e3ff22b29792de056a8b99aae689a389b43d6d
Klaus Hipp <khipp@users.noreply.github.com>
2025-05-21 23:05:31 +0200

    gaphor 3.1.0

--- Casks/g/gaphor.rb
+++ Casks/g/gaphor.rb
@@ -1,9 +1,9 @@
 cask "gaphor" do
   arch arm: "arm", intel: "intel"
 
-  version "3.0.0"
-  sha256 arm:   "6b5e4ca457f95fe8cc51e9d0800c284a44d3a6bf5a2a79587a502a227509b8e9",
-         intel: "7b360eb0de30a1171e61d04f8c38ce318f44f7eb6516b3f855c7145e6c9d3f84"
+  version "3.1.0"
+  sha256 arm:   "9772e9dfa42502d910f258fe9babc26837e952476a871d14c094c7af58dc8e29",
+         intel: "a59c7627ed62530ab232e60f30838d90ad83b84a4bf1f9b7e873a381696991b1"
 
   url "https://github.com/gaphor/gaphor/releases/download/#{version}/Gaphor-#{version}-#{arch}.dmg",
       verified: "github.com/gaphor/gaphor/"
