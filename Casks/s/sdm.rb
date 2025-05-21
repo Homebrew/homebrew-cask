a898ba2abe9f44b381f2d918e59ad548e261ee7d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-21 01:05:52 +0000

    sdm 22.31.0

--- Casks/s/sdm.rb
+++ Casks/s/sdm.rb
@@ -1,6 +1,6 @@
 cask "sdm" do
-  version "22.30.0,E3862C5FC64866B4D2027E1D2DFCD58D11C0B2E0"
-  sha256 "d85287f0d1277e2402fee24358fa0d6fdb142cf8b50280b05f78d0db26e38391"
+  version "22.31.0,C8B88536585A2F94B0C74645C22B1517B5261E50"
+  sha256 "9fef21a17d80faa051b494705acb4088e82d8ba3e166e967e39540ce04bb1c93"
 
   url "https://downloads.strongdm.com/builds/desktop/#{version.csv.first}/darwin-universal/#{version.csv.second}/SDM-#{version.csv.first}.universal.zip"
   name "sdm"
