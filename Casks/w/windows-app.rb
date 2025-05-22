507cdcdaf66611bd11ff078b15051fcae74ace91
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-22 22:36:59 +0000

    windows-app 11.1.6

--- Casks/w/windows-app.rb
+++ Casks/w/windows-app.rb
@@ -1,6 +1,6 @@
 cask "windows-app" do
-  version "11.1.5"
-  sha256 "af163e136d9d9021d743a064114f100f11c31416dfb6ad2d46b430951f346381"
+  version "11.1.6"
+  sha256 "a083b45556246e85d0dcd47befff8ad4e2851ae5039b9a13cb0dc84b1f35ce07"
 
   url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Windows_App_#{version}_installer.pkg",
       verified: "officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/"
