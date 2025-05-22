1e7e1e775f60f0eef7f5d92603b47f02b5d75c8f
Anka <runner@Mac-1747925760438.local>
2025-05-22 15:08:47 +0000

    raycast 1.99.2

--- Casks/r/raycast.rb
+++ Casks/r/raycast.rb
@@ -20,9 +20,9 @@ cask "raycast" do
     end
   end
   on_ventura :or_newer do
-    version "1.99.1"
-    sha256 arm:   "1f8a453f67ef9d591269f4df8bacb0e1fb5702d725268f64e1d175bf1f7eed7d",
-           intel: "396d16c7cbc9dc5019bff72a068951146a26b7b0066851e899e9e56c54046fc7"
+    version "1.99.2"
+    sha256 arm:   "56d672d6951ac8ae2bf0bef896582e062779549d5471a9cd83cad670eb30561e",
+           intel: "3e847b6e59fcf13b5f362c611e7042200f1d75eb234138a7da8ea76787573e6b"
 
     livecheck do
       url "https://releases.raycast.com/releases/latest?build=#{arch}"
