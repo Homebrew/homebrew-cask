08ee43c1247c62a00b1e2e07d69feef3248aebe9
James Ottaway <jamesottaway@users.noreply.github.com>
2025-05-22 16:35:13 +1000

    brainfm 0.0.191 (official)
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>

--- Casks/b/brainfm.rb
+++ Casks/b/brainfm.rb
@@ -1,21 +1,32 @@
 cask "brainfm" do
-  version "0.1.5"
-  sha256 "c6869346e1b68aa43db89f785bca6311d1aee117975a0df47c72cf7ab478e7c0"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://github.com/Dinius/Brain.fm-Desktop-Client/releases/download/v#{version}/brainfm-macos.zip"
+  version "0.0.191"
+  sha256 arm:   "a93d71fb5851bbfbd154f7ae95bf2e4922261169d40fbf108cbe89547dd4d9e2",
+         intel: "549adcf3c88d9b7e1b29d7807e787adabec8b89b3f604dd21f30615c7a961869"
+
+  url "https://download.brain.fm/darwin/#{arch}/Brain.fm-#{version}-#{arch}.dmg"
   name "Brain.fm"
   desc "Desktop client for brain.fm"
-  homepage "https://github.com/Dinius/Brain.fm-Desktop-Client"
+  homepage "https://www.brain.fm/download"
+
+  livecheck do
+    url "https://storage.googleapis.com/brainfm-desktop-app-v1/production/brain.fm/darwin/#{arch}/RELEASES.json"
+    strategy :json do |json|
+      json["currentRelease"]
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
 
   app "Brain.fm.app"
 
   zap trash: [
-    "~/Library/Preferences/com.electron.brain.fm.helper.plist",
+    "~/Library/Application Support/Brain.fm",
+    "~/Library/Caches/com.electron.brain.fm",
+    "~/Library/Caches/com.electron.brain.fm.ShipIt",
+    "~/Library/HTTPStorages/com.electron.brain.fm",
     "~/Library/Preferences/com.electron.brain.fm.plist",
-    "~/Library/Saved Application State/com.electron.brain.fm.savedState",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
