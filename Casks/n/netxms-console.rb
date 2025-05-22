1afd1b322da44b9062dd77ecea86054437df70a6
Klaus Hipp <khipp@users.noreply.github.com>
2025-05-22 15:55:21 +0200

    netxms-console 5.2.2.1

--- Casks/n/netxms-console.rb
+++ Casks/n/netxms-console.rb
@@ -1,14 +1,14 @@
 cask "netxms-console" do
   arch arm: "-aarch64"
 
-  version "5.2.2"
-  sha256 arm:   "658683b89b304e14b6c18b08057f1510bbba0b867b6c0788cc349c295d665eec",
-         intel: "fe7575f561b48cc20bbbeb129647b8718f00180f6a79492938de8a685dda54d0"
+  version "5.2.2.1"
+  sha256 arm:   "bd478d738ec2eb7928651c0bc72d11175bfae6c5c525aaf90afb63f087b33846",
+         intel: "3e78e2ca2b68101f7e12dddbc8ac3d502963c0b5608f0d798709a504dd3b51a2"
 
-  url "https://netxms.org/download/releases/#{version.major_minor}/nxmc-#{version}#{arch}.dmg"
+  url "https://netxms.com/download/releases/#{version.major_minor}/nxmc-#{version}#{arch}.dmg"
   name "NetXMS Management Console"
   desc "Network and infrastructure monitoring and management system"
-  homepage "https://netxms.org/"
+  homepage "https://netxms.com/"
 
   livecheck do
     url "https://netxms.com/downloads/"
