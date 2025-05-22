6bcf283679f0713cc4fb0e3ade76843d41b17a1e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-22 22:33:33 +0000

    tidelift 1.16.20

--- Casks/t/tidelift.rb
+++ Casks/t/tidelift.rb
@@ -2,11 +2,11 @@ cask "tidelift" do
   arch arm: "_arm"
   os macos: "darwin", linux: "linux"
 
-  version "1.16.19"
-  sha256 arm:          "7a33b71951ef0ef11b730d6e133138fd19e879169255a062f017df626a0cdae9",
-         intel:        "fe60a325c42fb036aad7aa7b6613b061cc67877c4ea18d69f2d358ef5d33ae37",
-         arm64_linux:  "50602da37bd4cd7444dcc26dac468ed2ca8fd7f74b48f2e5899f8e23103dadf6",
-         x86_64_linux: "8c67a406efc3a9af8adfbb85942266fa7628dfe29d2da5abad4699ef0aa3dc99"
+  version "1.16.20"
+  sha256 arm:          "39e61b26f0c20f65a993b65bfe7333aef9d494f29e82517b84a04fc7083e1d0e",
+         intel:        "20f5dc1cf24c738f139f47c3eef1d3373c4c59db1ba7b886644259b91276c9d3",
+         arm64_linux:  "c3e88860c65ac0d422250dd2bc5398ba7045e999a1d083ba5ed06d07b68464fc",
+         x86_64_linux: "f74d200da166ed65a214397d9f3acf0b7dc2f293b813a626f58325e7ab05be96"
 
   url "https://download.tidelift.com/cli/#{version}/#{os}#{arch}/tidelift"
   name "Tidelift CLI"
