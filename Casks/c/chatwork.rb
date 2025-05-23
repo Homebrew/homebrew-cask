8257a62628aaaeb4ac7f8ec4b00ff3857c089199
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-23 06:41:55 +0000

    chatwork 2.12.5

--- Casks/c/chatwork.rb
+++ Casks/c/chatwork.rb
@@ -1,7 +1,7 @@
 cask "chatwork" do
   livecheck_arch = on_arch_conditional arm: "arm64", intel: "x64"
 
-  version "2.12.4"
+  version "2.12.5"
   sha256 :no_check
 
   url "https://desktop-app.chatwork.com/installer/ChatWork.dmg"
