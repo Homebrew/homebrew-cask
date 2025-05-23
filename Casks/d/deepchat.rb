18fed80b0667faae5b785b3bb1334ffd56364a1e
Klaus Hipp <khipp@users.noreply.github.com>
2025-05-23 15:27:55 +0200

    deepchat 0.2.0

--- Casks/d/deepchat.rb
+++ Casks/d/deepchat.rb
@@ -1,15 +1,23 @@
 cask "deepchat" do
   arch arm: "arm64", intel: "x64"
+  livecheck_arch = on_arch_conditional arm: "arm", intel: "x64"
 
-  version "0.1.1"
-  sha256 arm:   "2fa5012bff8f93f295840c5d1c9de788eba1748c0c269b070dc27a91617727b7",
-         intel: "2c3ca1e586b40066a4f4e1da963c3bb09bbe4838fc36286e59215791033f09de"
+  version "0.2.0"
+  sha256 arm:   "9e91996b520f668f5548e99da81b9d021b1f93a9d27c44d230eea518707ce399",
+         intel: "24d2dc0475b7551a5d91546c62fe6ee37393fe143fe4c4f1c4757a72f8644172"
 
   url "https://github.com/ThinkInAIXYZ/deepchat/releases/download/v#{version}/DeepChat-#{version}-mac-#{arch}.dmg",
       verified: "github.com/ThinkInAIXYZ/deepchat/"
   name "DeepChat"
   desc "AI assistant"
   homepage "https://deepchat.thinkinai.xyz/"
+
+  livecheck do
+    url "https://cdn.deepchatai.cn/upgrade/mac#{livecheck_arch}.json"
+    strategy :json do |json|
+      json["version"]
+    end
+  end
 
   auto_updates true
   depends_on macos: ">= :big_sur"
