762aa5aa7e0532024543db4e8fa5f15ca874d868
Klaus Hipp <9824526+khipp@users.noreply.github.com>
2025-05-23 19:10:52 +0000

    anytype@alpha 0.46.30-alpha

--- Casks/a/anytype@alpha.rb
+++ Casks/a/anytype@alpha.rb
@@ -1,9 +1,9 @@
 cask "anytype@alpha" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.46.29-alpha"
-  sha256 arm:   "5a907a12fb42e43b4f925ec0937a9899828cf0c148538650f49df1876b830a40",
-         intel: "de4821077f55f5a98bf1c4a4cf0a6f8c22fc60fd1a9c57af0fdccc7aa3b8b188"
+  version "0.46.30-alpha"
+  sha256 arm:   "a09194bf02c175681e0ef7cc7190fbd4bea257fb20e0fd96e2d46f2a7cdde1ff",
+         intel: "7a7b867e936e24ef139852370f38ce097ca5193f3f4253481c9d0d5641541dfc"
 
   url "https://anytype-release.fra1.cdn.digitaloceanspaces.com/Anytype-#{version}-mac-#{arch}.dmg",
       verified: "anytype-release.fra1.cdn.digitaloceanspaces.com/"
