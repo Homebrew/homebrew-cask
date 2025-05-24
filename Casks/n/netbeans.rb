c9864092307a372acdfcef58d86b2035ecffe69d
Klaus Hipp <khipp@users.noreply.github.com>
2025-05-24 12:59:46 +0200

    netbeans: update livecheck

--- Casks/n/netbeans.rb
+++ Casks/n/netbeans.rb
@@ -10,8 +10,8 @@ cask "netbeans" do
   # Major releases sometimes only use the major version (13) instead of
   # major/minor (13.0).
   livecheck do
-    url "https://netbeans.apache.org/front/main/download/"
-    regex(/>\s*Apache\s*NetBeans\s*v?(\d+(?:\.\d+)*)\s*</im)
+    url "https://dlcdn.apache.org/netbeans/netbeans-installers/"
+    regex(%r{href=["']?v?(\d+(?:\.\d+)*)/?["' >]}i)
   end
 
   depends_on macos: ">= :big_sur"
