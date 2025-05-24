0f1e4b292a42d31cb12cd586bcb986921c83c5cb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-24 03:52:13 +0000

    font-0xproto 2.500

--- Casks/font/font-0/font-0xproto.rb
+++ Casks/font/font-0/font-0xproto.rb
@@ -1,6 +1,6 @@
 cask "font-0xproto" do
-  version "2.400"
-  sha256 "b34ee80633a2cfe6725dd5a9ab892f55eb321c7a7f6b7f561f1d9f7d87c2ba65"
+  version "2.500"
+  sha256 "eb7a786c9f4b4d89c6c8e4cf406f96b21f13464ed8b9631db30002045a5ac794"
 
   url "https://github.com/0xType/0xProto/releases/download/#{version}/0xProto_#{version.dots_to_underscores}.zip"
   name "0xProto"
