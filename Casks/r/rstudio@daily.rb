54ca6e7824d185ac6fc55b1f2c4310f7d1aaa176
Klaus Hipp <khipp@users.noreply.github.com>
2025-05-24 13:03:38 +0200

    rstudio@daily 2025.08.0-daily-135

--- Casks/r/rstudio@daily.rb
+++ Casks/r/rstudio@daily.rb
@@ -1,6 +1,6 @@
 cask "rstudio@daily" do
-  version "2025.08.1-daily-101"
-  sha256 "ca07f852c71dcf1a4ebb91c17c02a8c64682162dedd5208933e0e9cd24f3e06b"
+  version "2025.08.0-daily-135"
+  sha256 "769d4b66fdb317c1ef5824513ffc7a6f24354fafad68fd0663b7b38521dfe055"
 
   url "https://rstudio-ide-build.s3.amazonaws.com/electron/macos/RStudio-#{version}.dmg",
       verified: "rstudio-ide-build.s3.amazonaws.com/electron/macos/"
