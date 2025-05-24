3d957635beadc51a48264a12648acb2d4a5b0dce
Daiki <dongjiahui01@gmail.com>
2025-05-25 01:02:18 +0800

    cherry-studio: fix sha256 arm

--- Casks/c/cherry-studio.rb
+++ Casks/c/cherry-studio.rb
@@ -2,7 +2,7 @@ cask "cherry-studio" do
   arch arm: "arm64", intel: "x64"
 
   version "1.3.11"
-  sha256 arm:   "3156d8a86c5566a6a658c2280d1a0b04534f56d61d337ab5bc02f82adcc0e106",
+  sha256 arm:   "dd33f119d9401ae19456b279affb6b30fd42e830f2e0a95bcb37c0fa8d38935a",
          intel: "65423e37c814485fc4f212f190f3b9e9f2e31a7febc35bef6995ec4d8f3a30d4"
 
   url "https://github.com/CherryHQ/cherry-studio/releases/download/v#{version}/Cherry-Studio-#{version}-#{arch}.zip",
