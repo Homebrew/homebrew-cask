9b6423624dc04df6a5a3347dd8668ed1a1de5eab
Klaus Hipp <khipp@users.noreply.github.com>
2025-05-24 12:43:34 +0200

    dialpad 2504.3.1 (arm only)

--- Casks/d/dialpad.rb
+++ Casks/d/dialpad.rb
@@ -1,9 +1,14 @@
 cask "dialpad" do
   arch arm: "arm64", intel: "x64"
 
-  version "2504.3.0"
-  sha256 arm:   "8380632e2e423c2dfdf367248ddb7ffe7c06a1235f8dd9c7ae3d785437faa2f9",
-         intel: "cf5c35b47aca4f444280bd39e1d2e9d7a3d32a89364561a926f42ad57168dbc3"
+  on_arm do
+    version "2504.3.1"
+    sha256 "18dd8ab38b12feff01262d5251577528bdaef9d266d5e3800e1d864701a786b6"
+  end
+  on_intel do
+    version "2504.3.0"
+    sha256 "cf5c35b47aca4f444280bd39e1d2e9d7a3d32a89364561a926f42ad57168dbc3"
+  end
 
   url "https://storage.googleapis.com/dialpad_native/osx/#{arch}/Dialpad.#{version}.zip",
       verified: "storage.googleapis.com/dialpad_native/osx/#{arch}"
