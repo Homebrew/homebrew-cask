552e829262146afb028d7df9015bbb11f0406750
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-24 15:34:18 +0000

    c0re100-qbittorrent 5.1.0.11

--- Casks/c/c0re100-qbittorrent.rb
+++ Casks/c/c0re100-qbittorrent.rb
@@ -1,6 +1,6 @@
 cask "c0re100-qbittorrent" do
-  version "5.1.0.10"
-  sha256 "99ba143a33236ff25eb917e3ce04370f170c21c34164c5446d604afdd8263bcc"
+  version "5.1.0.11"
+  sha256 "e0bc679be0b59d7318936ec4cc76073e0cbef8ae87d06829512154633f4a3fa5"
 
   url "https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-#{version}/qBittorrent-Enhanced-Edition-release-#{version}-macOS-universal.dmg"
   name "qBittorrent Enhanced Edition"
