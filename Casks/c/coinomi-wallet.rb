dc220430b7dde5d52ec6270b6a2e26b557764d85
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-05-25 19:22:35 -0400

    coinomi-wallet: deprecate
    Application is now distributed via the Mac App Store

--- Casks/c/coinomi-wallet.rb
+++ Casks/c/coinomi-wallet.rb
@@ -8,10 +8,7 @@ cask "coinomi-wallet" do
   desc "Securely store, manage and exchange many blockchain assets"
   homepage "https://www.coinomi.com/en/"
 
-  livecheck do
-    url "https://www.coinomi.com/downloads/"
-    regex(/href=.*?coinomi[._-]wallet[._-]v?(\d+(?:\.\d+)+)[._-]macos\.dmg/i)
-  end
+  deprecate! date: "2025-05-25", because: :moved_to_mas
 
   depends_on macos: ">= :sierra"
 
