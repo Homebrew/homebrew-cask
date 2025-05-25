17b67e33dda39fe75ec069957786bd52ee876787
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-25 21:49:37 +0000

    mitmproxy 12.1.1

--- Casks/m/mitmproxy.rb
+++ Casks/m/mitmproxy.rb
@@ -1,9 +1,9 @@
 cask "mitmproxy" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "12.1.0"
-  sha256 arm:   "d2799748d40d2b743c5753debc4cd24fb8cd3ffe2efbdc55e22a512a3d9d56c8",
-         intel: "59e1a2f3f938fd16c98a40cf8e1ca13b4083a40a779d0c32d0d98ac3ec6a4498"
+  version "12.1.1"
+  sha256 arm:   "64cc0dbb68ee1c782c0156cfbd8747bf48b515307240e29fdf7567c8cfdfecc5",
+         intel: "59fd822c3c4a2cf5c3afdb8158341c66514040a94cbce194a019020b966b3406"
 
   url "https://downloads.mitmproxy.org/#{version}/mitmproxy-#{version}-macos-#{arch}.tar.gz"
   name "mitmproxy"
