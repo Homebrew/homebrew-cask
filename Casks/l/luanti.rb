c72719c8c62707f1e5e09b248b0cecae35e0be52
Klaus Hipp <khipp@users.noreply.github.com>
2025-05-25 13:07:33 +0200

    luanti 5.12.0

--- Casks/l/luanti.rb
+++ Casks/l/luanti.rb
@@ -1,9 +1,9 @@
 cask "luanti" do
   arch arm: "arm64", intel: "x86_64_flag_O1"
 
-  version "5.11.0,11.3"
-  sha256 arm:   "7f66316ccb37859ec1558a84d2701d9cb55107b7c97c0787b8a6556b790aadd2",
-         intel: "7e540ebfd97ee39c0f9825be4493da519ba6ae7d1f57f5fc13f6ef6bda1a3a30"
+  version "5.12.0,11.3"
+  sha256 arm:   "59b04cfef02fcd2ce0529f3ff1a48256730a322ca16018af9059154b4dd72606",
+         intel: "ce2d1174edaa4f2c3684cfacd6e686d90c299b775445dd11bad6daf9428f119b"
 
   url "https://github.com/minetest/minetest/releases/download/#{version.csv.first}/luanti_#{version.csv.first}-macos#{version.csv.second}_#{arch}.zip",
       verified: "github.com/minetest/minetest/"
