cd48af2734e5fb22933b98e5298dd41fcba381e7
Jacob Ford <j@cobford.com>
2025-05-25 19:22:15 -0400

    kaleidoscope@beta 5.4.3,7987

---
+++ Casks/k/kaleidoscope@beta.rb
@@ -0,0 +1,48 @@
+cask "kaleidoscope@beta" do
+  version "5.4.3,7987"
+  sha256 "d472e34b822e7946850efbec0fa5f2f23758df818f2ba985de4484fc5298770e"
+
+  url "https://updates.kaleidoscope.app/v#{version.major}/beta/Kaleidoscope-#{version.csv.first}-#{version.csv.second}.app.zip"
+  name "Kaleidoscope"
+  desc "Spot and merge differences in text and image files or folders"
+  homepage "https://kaleidoscope.app/"
+
+  livecheck do
+    url "https://updates.kaleidoscope.app/v#{version.major}/beta/appcast"
+    strategy :sparkle
+  end
+
+  auto_updates true
+  conflicts_with cask: %w[
+    kaleidoscope
+    kaleidoscope@2
+    kaleidoscope@3
+    ksdiff
+  ]
+  depends_on macos: ">= :ventura"
+
+  app "Kaleidoscope.app"
+
+  postflight do
+    contents = "#{appdir}/Kaleidoscope.app/Contents"
+    system_command "#{contents}/Resources/Integration/scripts/install_ksdiff",
+                   args: ["#{contents}/MacOS", "#{HOMEBREW_PREFIX}/bin"]
+  end
+
+  uninstall quit:    "app.kaleidoscope.v#{version.major}",
+            pkgutil: "app.kaleidoscope.uninstall_ksdiff"
+
+  zap trash: [
+    "~/Library/Application Support/app.kaleidoscope.v*",
+    "~/Library/Application Support/com.blackpixel.kaleidoscope",
+    "~/Library/Application Support/Kaleidoscope",
+    "~/Library/Caches/app.kaleidoscope.v*",
+    "~/Library/Caches/com.blackpixel.kaleidoscope",
+    "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.blackpixel.kaleidoscope",
+    "~/Library/Preferences/app.kaleidoscope.v*.plist",
+    "~/Library/Preferences/com.blackpixel.kaleidoscope.plist",
+    "~/Library/Saved Application State/app.kaleidoscope.v*.savedState",
+    "~/Library/Saved Application State/com.blackpixel.kaleidoscope.savedState",
+    "~/Library/WebKit/app.kaleidoscope.v*",
+  ]
+end
