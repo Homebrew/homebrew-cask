53106d39c32412944c7a0b8c3f1d858fa7e297af
Bevan Kay <email@bevankay.me>
2025-05-26 11:49:14 +1000

    font-bukyvede-bold: update url, homepage

--- Casks/font/font-b/font-bukyvede-bold.rb
+++ Casks/font/font-b/font-bukyvede-bold.rb
@@ -2,9 +2,9 @@ cask "font-bukyvede-bold" do
   version :latest
   sha256 :no_check
 
-  url "https://kodeks.uni-bamberg.de/aksl/media/BukyVede-Bold.ttf"
+  url "https://kodeks.de/AKSL/media/BukyVede-Bold.ttf"
   name "BukyVede Bold"
-  homepage "https://kodeks.uni-bamberg.de/aksl/Schrift/BukyVede.htm"
+  homepage "https://kodeks.de/AKSL/Schrift/BukyVede.htm"
 
   font "BukyVede-Bold.ttf"
 
