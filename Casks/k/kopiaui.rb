cfb2a304d4ae3bf703548e6719bbf2794e40e9ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-26 01:01:08 +0000

    kopiaui 0.20.1

--- Casks/k/kopiaui.rb
+++ Casks/k/kopiaui.rb
@@ -1,9 +1,9 @@
 cask "kopiaui" do
   arch arm: "-arm64"
 
-  version "0.20.0"
-  sha256 arm:   "730c32f6c79c8649d78e6d9abd602f8a16d2c90150144620d1ba0a29b992a4b4",
-         intel: "ac229663c521a49c5b0d57f7a48c81370f9398833c87a01f23e4068d7ba69155"
+  version "0.20.1"
+  sha256 arm:   "9c0c802bfc4fb68f1a4e0a57a611d761d467fc84627a416eacc8e083e9f8a71d",
+         intel: "9f1e2818cfc2fe7281be91dd985aec5f66f4669888832f9bbae810453cd2f3fb"
 
   url "https://github.com/kopia/kopia/releases/download/v#{version}/KopiaUI-#{version}#{arch}.dmg",
       verified: "github.com/kopia/kopia/"
