d7f4044cfbe35b080721674785fabd85408e37fe
Clean6 <noa.jukic@hotmail.com>
2025-05-26 15:27:09 +0200

    Remove ARM support from GeoPort cask (Intel only)

--- Casks/g/geoport.rb
+++ Casks/g/geoport.rb
@@ -1,11 +1,8 @@
 cask "geoport" do
-  arch arm: "arm", intel: "x86"
-
   version "4.0.2"
-  sha256 arm:   "f27ffd28d9c8cfd0afa56190cd67e6aa4b9a119b1903cedea25c8111a8e9cded",
-         intel: "60dcbf6c4ac02bd2661546229ad6ef981f2608890a9f8eabc4c153fb35d58887"
+  sha256 "60dcbf6c4ac02bd2661546229ad6ef981f2608890a9f8eabc4c153fb35d58887"
 
-  url "https://github.com/davesc63/GeoPort/releases/download/v#{version}/GeoPort-#{arch}-v#{version}.dmg",
+  url "https://github.com/davesc63/GeoPort/releases/download/v#{version}/GeoPort-x86-v#{version}.dmg",
       verified: "github.com/davesc63/GeoPort/"
   name "GeoPort"
   desc "iOS location simulator: spoof your location, test apps, and more"
