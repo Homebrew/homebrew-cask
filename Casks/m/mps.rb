9b7228193418da0a1aef35e9670bdd6f180e6831
MoonFruit <dkmoonfruit@gmail.com>
2025-05-26 14:20:50 +0800

    Update mps binary

--- Casks/m/mps.rb
+++ Casks/m/mps.rb
@@ -27,7 +27,16 @@ cask "mps" do
   depends_on macos: ">= :high_sierra"
 
   app "MPS.app"
-  binary "#{appdir}/MPS.app/Contents/MacOS/mps"
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/mps.wrapper.sh"
+  binary shimscript, target: "mps"
+
+  preflight do
+    File.write shimscript, <<~EOS
+      #!/bin/sh
+      exec '#{appdir}/MPS.app/Contents/MacOS/mps' "$@"
+    EOS
+  end
 
   zap trash: [
     "~/Library/Application Support/MPS#{version.csv.first.major_minor}",
