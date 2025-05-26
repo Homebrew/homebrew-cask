77debbc3468455ba8e7d2766240b9f3c4707802c
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-05-27 01:18:15 +0900

    license-control-center: deprecate

--- Casks/l/license-control-center.rb
+++ Casks/l/license-control-center.rb
@@ -13,6 +13,8 @@ cask "license-control-center" do
     strategy :header_match
   end
 
+  deprecate! date: "2025-05-27", because: :discontinued
+
   installer manual: "eLicenserControlSetup.app"
 
   uninstall delete: [
