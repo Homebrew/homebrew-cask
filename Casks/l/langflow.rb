a75bc388930830341974a4bb2b9e6681cb9af96b
Boris Klimenko <2@borisklimenko.ru>
2025-05-26 14:26:58 +0200

    Langflow 1.4.2 (new cask)

---
+++ Casks/l/langflow.rb
@@ -0,0 +1,28 @@
+cask "langflow" do
+  version "1.4.2"
+  sha256 "42a7d642db5b75c671cbdf40faa5d87163cf34bb35b50e1537cd47fef49eb637"
+
+  url "https://github.com/langflow-ai/langflow/releases/download/#{version}/Langflow_#{version}_aarch64.dmg",
+      verified: "github.com/langflow-ai/langflow/"
+  name "Langflow"
+  desc "Low-code AI-workflow building tool"
+  homepage "https://www.langflow.org/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :sonoma"
+
+  app "Langflow.app"
+
+  zap trash: [
+    "~/.langflow",
+    "~/Library/Caches/com.Langflow",
+    "~/Library/Caches/langflow",
+    "~/Library/Logs/com.Langflow",
+    "~/Library/Preferences/com.Langflow.plist",
+    "~/Library/WebKit/com.Langflow",
+  ]
+end
