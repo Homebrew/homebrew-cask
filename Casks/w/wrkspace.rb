25d07f85234254ad0a76094e27ddb533af6a0610
Klaus Hipp <khipp@users.noreply.github.com>
2025-05-26 23:26:11 +0200

    wrkspace: update livecheck, use versioned url

--- Casks/w/wrkspace.rb
+++ Casks/w/wrkspace.rb
@@ -2,17 +2,19 @@ cask "wrkspace" do
   arch arm: "arm64", intel: "x64"
 
   version "2.0.11"
-  sha256 :no_check
+  sha256 arm:   "3ed6dca05983088f329f528c00a7721c315cc6002976b3f2b0037edef37ed8a3",
+         intel: "2e8a8f0578d8ca660fdde482e184fc4a32b1e12da0a676ab3106da98ae8f7ce4"
 
-  url "https://updater.wrkspace.co/download/darwin-#{arch}"
+  url "https://updater.wrkspace.co/download/version/#{version}/darwin-#{arch}"
   name "Wrkspace"
   desc "All-in-one dev bootstrapper: one-click startup Docker, scripts, editor, and URLs"
   homepage "https://wrkspace.co/"
 
   livecheck do
-    url :url
-    regex(/Wrkspace[._-]v?(\d+(?:\.\d+)+)(?:-(?:arm64|x64|universal))?\.dmg/i)
-    strategy :header_match
+    url "https://updater.wrkspace.co/update/darwin-#{arch}/0.0.0"
+    strategy :json do |json|
+      json["name"]
+    end
   end
 
   depends_on macos: ">= :high_sierra"
