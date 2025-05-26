e600d84d86c4d957aa86809fbd072aa1bb8669b3
Clean6 <noa.jukic@hotmail.com>
2025-05-26 15:49:31 +0200

    Update geoport cask: formatting, zap stanza, and ensure Intel-only

--- Casks/g/geoport.rb
+++ Casks/g/geoport.rb
@@ -2,8 +2,7 @@ cask "geoport" do
   version "4.0.2"
   sha256 "60dcbf6c4ac02bd2661546229ad6ef981f2608890a9f8eabc4c153fb35d58887"
 
-  url "https://github.com/davesc63/GeoPort/releases/download/v#{version}/GeoPort-x86-v#{version}.dmg",
-      verified: "github.com/davesc63/GeoPort/"
+  url "https://github.com/davesc63/GeoPort/releases/download/v#{version}/GeoPort-x86-v#{version}.dmg"
   name "GeoPort"
   desc "iOS location simulator: spoof your location, test apps, and more"
   homepage "https://github.com/davesc63/GeoPort"
