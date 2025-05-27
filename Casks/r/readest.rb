ed69e8e339aed5932f09c72c998a760eff92b3d6
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-05-27 13:54:45 +0000

    readest 0.9.49

--- Casks/r/readest.rb
+++ Casks/r/readest.rb
@@ -1,6 +1,6 @@
 cask "readest" do
-  version "0.9.43"
-  sha256 "cd3db1fdcf7fc6c7f9d17565f23b54960f8bd242a2951422cf2f4d79a2053bfd"
+  version "0.9.49"
+  sha256 "c3d6d28eed50ad43060b82ff275bac27ab7ef67e2aca307c30dfb238ca5cd9f4"
 
   url "https://github.com/readest/readest/releases/download/v#{version}/Readest_#{version}_universal.dmg",
       verified: "github.com/readest/readest/"
