7a83ae36f976b93a51b4a395536256e88be101f3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-27 12:45:00 +0000

    cleanshot 4.8

--- Casks/c/cleanshot.rb
+++ Casks/c/cleanshot.rb
@@ -1,6 +1,6 @@
 cask "cleanshot" do
-  version "4.7.6"
-  sha256 "677178b8060c5e3d579d5a534792c2b9649c835b1d07aa307f18a28a73307b55"
+  version "4.8"
+  sha256 "046c642686eecfdfa7419ef2dc1d31a86f99f283097acd64280eba481a2d1c9a"
 
   url "https://updates.getcleanshot.com/v3/CleanShot-X-#{version}.dmg"
   name "CleanShot"
