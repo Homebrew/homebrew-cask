e16dee51cba2ef66a0b19ef9c48cfe105990f744
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-27 16:16:57 +0000

    vscodium@insiders 1.101.03543-insider

--- Casks/v/vscodium@insiders.rb
+++ Casks/v/vscodium@insiders.rb
@@ -1,9 +1,9 @@
 cask "vscodium@insiders" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.101.03463-insider"
-  sha256 arm:   "7522acfb44dc9ac8a778538909768ace65e1a2dc49cd117a694f8f562817045a",
-         intel: "041b01de83a1c5d05f11dfa0ed39e013e8dabd0a6040cec1880a5946ea241f8e"
+  version "1.101.03543-insider"
+  sha256 arm:   "cd1f6553e5092cc7a363adf00727a2e01b799b545f13692cb2f56d0176b4ea48",
+         intel: "b1b8e942f204c03e20cd2eaede86e8709df5225d6623df328b1187d0aafc34d0"
 
   url "https://github.com/VSCodium/vscodium-insiders/releases/download/#{version}/VSCodium-darwin-#{arch}-#{version}.zip",
       verified: "github.com/VSCodium/vscodium-insiders/"
