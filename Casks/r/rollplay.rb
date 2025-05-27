e478c5c9c1194f85ae04f63f5c47ba73955cb206
Bios Thusvill <87165098+thusvill@users.noreply.github.com>
2025-05-27 12:40:43 +0530

    fix issues

--- Casks/r/rollplay.rb
+++ Casks/r/rollplay.rb
@@ -4,21 +4,24 @@ cask "rollplay" do
 
   url "https://github.com/thusvill/LiveWallpaperMacOS/releases/download/V#{version}/RollPlay-Silicon.dmg"
   name "RollPlay"
-  desc "Open-source live wallpaper application for macOS 15+"
+  desc "Open-source live wallpaper application"
   homepage "https://github.com/thusvill/LiveWallpaperMacOS"
+
+  depends_on macos: ">= :sequoia"
 
   app "RollPlay.app"
 
   postflight do
-    system_command "/usr/bin/xattr",
-                   args: ["-d", "com.apple.quarantine", "#{appdir}/RollPlay.app"],
-                   sudo: false
+    if MacOS.version >= :sequoia
+      system_command "/usr/bin/xattr",
+        args: ["-d", "com.apple.quarantine", "/Applications/RollPlay.app"],
+        sudo: false,
+        print_stderr: false,
+        fail_if_error: false
+    end
   end
 
   zap trash: [
-    "~/Library/Application Support/RollPlay",
-    "~/Library/Preferences/com.thusvill.rollplay.plist",
+    "/Applications/RollPlay.app",
   ]
-
-  depends_on macos: ">= :sequoia"
 end
