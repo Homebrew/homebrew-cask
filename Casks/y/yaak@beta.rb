b947ab9c6a082676dab5adda58a448a64ed75f7e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-28 22:05:45 +0000

    yaak@beta 2025.3.0

--- Casks/y/yaak@beta.rb
+++ Casks/y/yaak@beta.rb
@@ -1,9 +1,9 @@
 cask "yaak@beta" do
   arch arm: "aarch64", intel: "x64"
 
-  version "2025.3.0-beta.4"
-  sha256 arm:   "a3446a3cdf1f60453759ad7b080aa8d2587559a59799751a34c68842efab9c0a",
-         intel: "b427b58c9c948cec3e7f005b0fd5955d3cf2a924028cb9aa35a2fa1493e06298"
+  version "2025.3.0"
+  sha256 arm:   "59c366c5a7fa91a9d3832d7f35eb049352c633956421cd6b3a5921f28be7267d",
+         intel: "70219e29d51f1639469956eacd27911d07d05ce5d5d65adfa80d7d11e564863a"
 
   url "https://github.com/mountain-loop/yaak/releases/download/v#{version}/Yaak_#{version}_#{arch}.dmg",
       verified: "github.com/mountain-loop/yaak/"
