95b1c42ed54d1b5c754da3ac44e084964b38b7e7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-28 18:44:04 +0000

    chia 2.5.4

--- Casks/c/chia.rb
+++ Casks/c/chia.rb
@@ -1,9 +1,9 @@
 cask "chia" do
   arch arm: "-arm64"
 
-  version "2.5.3"
-  sha256 arm:   "86ab2f52e8311a82d03eefb22a30e83a19f15c3c5388c64025951c021e23d3d2",
-         intel: "5ae33a980b9e3fdb55a51d6847c9a99e6ac9d0a877565ef5d12bf4cadb785311"
+  version "2.5.4"
+  sha256 arm:   "85ee484ff52bcc2aedf994425494dca81512737562d4b377a059556c4f3ac0b7",
+         intel: "1e31a988ce46f1e298cb7cd544180928825da7cd50b21f78c5b36f23d8cbc567"
 
   url "https://github.com/Chia-Network/chia-blockchain/releases/download/#{version}/Chia-#{version}#{arch}.dmg",
       verified: "github.com/Chia-Network/chia-blockchain/"
