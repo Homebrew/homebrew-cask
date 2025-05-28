50d234e08b6fb40764f61838a407e0106e559338
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-28 09:36:55 +0000

    banking-4 8.8.6

--- Casks/b/banking-4.rb
+++ Casks/b/banking-4.rb
@@ -1,6 +1,6 @@
 cask "banking-4" do
   # NOTE: "4" is not a version number, but an intrinsic part of the product name
-  version "8.8.5,9273"
+  version "8.8.6,9279"
   sha256 :no_check
 
   url "https://subsembly.com/download/MacBanking4.pkg"
