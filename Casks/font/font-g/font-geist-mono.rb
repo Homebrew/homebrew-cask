455310995bc7636a0f0e990511d4e1a0cb4c1445
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2025-05-29 17:10:22 -0300

    font-geist-mono 1.5.0

--- Casks/font/font-g/font-geist-mono.rb
+++ Casks/font/font-g/font-geist-mono.rb
@@ -1,11 +1,11 @@
 cask "font-geist-mono" do
-  version "1.4.2"
-  sha256 "e94caf733a6b019a0d4d97da9548c2ca8cbe4b2703ef1d07113a82d9e774cfe5"
+  version "1.5.0"
+  sha256 "8a57ecad52a78d5d4f90e1ac2f8cbf1ed9479c796e52ef2e564f67c8cf06c247"
 
   url "https://github.com/vercel/geist-font/releases/download/#{version}/geist-font-#{version}.zip",
       verified: "github.com/vercel/geist-font/"
   name "Geist Mono"
-  homepage "https://vercel.com/font/mono"
+  homepage "https://vercel.com/font"
 
   livecheck do
     url :url
@@ -13,14 +13,24 @@ cask "font-geist-mono" do
   end
 
   font "geist-font-#{version}/fonts/GeistMono/otf/GeistMono-Black.otf"
+  font "geist-font-#{version}/fonts/GeistMono/otf/GeistMono-BlackItalic.otf"
   font "geist-font-#{version}/fonts/GeistMono/otf/GeistMono-Bold.otf"
+  font "geist-font-#{version}/fonts/GeistMono/otf/GeistMono-BoldItalic.otf"
+  font "geist-font-#{version}/fonts/GeistMono/otf/GeistMono-ExtraBold.otf"
+  font "geist-font-#{version}/fonts/GeistMono/otf/GeistMono-ExtraBoldItalic.otf"
+  font "geist-font-#{version}/fonts/GeistMono/otf/GeistMono-ExtraLight.otf"
+  font "geist-font-#{version}/fonts/GeistMono/otf/GeistMono-ExtraLightItalic.otf"
+  font "geist-font-#{version}/fonts/GeistMono/otf/GeistMono-Italic.otf"
   font "geist-font-#{version}/fonts/GeistMono/otf/GeistMono-Light.otf"
+  font "geist-font-#{version}/fonts/GeistMono/otf/GeistMono-LightItalic.otf"
   font "geist-font-#{version}/fonts/GeistMono/otf/GeistMono-Medium.otf"
+  font "geist-font-#{version}/fonts/GeistMono/otf/GeistMono-MediumItalic.otf"
   font "geist-font-#{version}/fonts/GeistMono/otf/GeistMono-Regular.otf"
   font "geist-font-#{version}/fonts/GeistMono/otf/GeistMono-SemiBold.otf"
+  font "geist-font-#{version}/fonts/GeistMono/otf/GeistMono-SemiBoldItalic.otf"
   font "geist-font-#{version}/fonts/GeistMono/otf/GeistMono-Thin.otf"
-  font "geist-font-#{version}/fonts/GeistMono/otf/GeistMono-UltraBlack.otf"
-  font "geist-font-#{version}/fonts/GeistMono/otf/GeistMono-UltraLight.otf"
+  font "geist-font-#{version}/fonts/GeistMono/otf/GeistMono-ThinItalic.otf"
+  font "geist-font-#{version}/fonts/GeistMono/variable/GeistMono-Italic[wght].ttf"
   font "geist-font-#{version}/fonts/GeistMono/variable/GeistMono[wght].ttf"
 
   # No zap stanza required
