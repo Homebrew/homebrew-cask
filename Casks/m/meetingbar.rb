8553ab06a700bff0f8f120ab32d761a1657d05cd
Andrii Leitsius <leits.dev@gmail.com>
2025-05-29 11:33:48 +0300

    Up meetingbar to 4.11.2

--- Casks/m/meetingbar.rb
+++ Casks/m/meetingbar.rb
@@ -1,6 +1,6 @@
 cask "meetingbar" do
-  version "4.10.0"
-  sha256 "f1bed4c91e5f2dd9ce61afdfb2a9bbe67e27fac65829fef9848121e83a76b7a0"
+  version "4.11.2"
+  sha256 "8545631c3439d9ddac4fbc9ce058305adea78da9625aaf7b31f18b540459f920"
 
   url "https://github.com/leits/MeetingBar/releases/download/v#{version}/MeetingBar.dmg"
   name "MeetingBar"
