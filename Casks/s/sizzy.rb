bb3003a2ba5d08cfcff185732a7a324fb67affcf
Klaus Hipp <khipp@users.noreply.github.com>
2025-05-30 21:24:45 +0200

    sizzy 73.0.0

--- Casks/s/sizzy.rb
+++ Casks/s/sizzy.rb
@@ -1,7 +1,7 @@
 cask "sizzy" do
   arch arm: "arm64", intel: "intel"
 
-  version "72.2.0"
+  version "73.0.0"
   sha256 :no_check
 
   url "https://proxy.sizzy.co/updates/download/mac?arch=#{arch}"
