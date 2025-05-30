0f0ab71f259982f1c2329306e895882bca382e03
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-30 09:39:12 +0000

    creality-print 6.1.2.2458

--- Casks/c/creality-print.rb
+++ Casks/c/creality-print.rb
@@ -1,9 +1,9 @@
 cask "creality-print" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "6.1.1.2274"
-  sha256 arm:   "bb064847f8c408fa8d90045b4ffc6ee82c0775320a72da2c31b932ecab782761",
-         intel: "1445bd5f962c0d46c4193f2b5a9fb2b42369ef018ec8f2fccb781b94c3609555"
+  version "6.1.2.2458"
+  sha256 arm:   "35f10a4060c20f41f52afc8c3fcd34ad6efeae0ae10bb2600b2e4ea00e08d839",
+         intel: "b0b0cf93e84780c82ade95c304cfd40e2edef6f96f99d3086f706bef16f1851b"
 
   url "https://github.com/CrealityOfficial/CrealityPrint/releases/download/v#{version.major_minor_patch}/CrealityPrint-#{version}-macx-#{arch}-Release.dmg",
       verified: "github.com/CrealityOfficial/CrealityPrint/"
