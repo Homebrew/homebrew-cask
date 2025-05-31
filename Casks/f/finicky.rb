4948c2f8af5f7e43e1d1f6a1fe5cee17b3572425
John Sterling <john.sterling@soundtrackyourbrand.com>
2025-05-31 20:36:08 +0200

    finicky 4.1.1

--- Casks/f/finicky.rb
+++ Casks/f/finicky.rb
@@ -1,6 +1,6 @@
 cask "finicky" do
-  version "4.1.0"
-  sha256 "a142dfd14f81ce506f2f56fb4be9d44ba2ec7b8db5a969af0b7b640753c63186"
+  version "4.1.1"
+  sha256 "e423f5ba224ffe1d9cfa1340143a1496e602ab155aeeffbe59b9b0b1094fd3e7"
 
   url "https://github.com/johnste/finicky/releases/download/v#{version}/Finicky.dmg"
   name "Finicky"
