ca1676e8532776288b4caf46a672e8d24de84832
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-31 06:44:28 +0000

    eloston-chromium 137.0.7151.55-1.1

--- Casks/e/eloston-chromium.rb
+++ Casks/e/eloston-chromium.rb
@@ -1,9 +1,9 @@
 cask "eloston-chromium" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "136.0.7103.113-1.1"
-  sha256 arm:   "9656979768c47a02f40f25011c35b5e5df23357afe905f93264ac138d2255c25",
-         intel: "8fc4b374339886437de2974c834e286db46ecef556bb602a28bbe67218fab2a9"
+  version "137.0.7151.55-1.1"
+  sha256 arm:   "17b92c92843b89e19f1285f3592a5f6868e5b3f5f138055730a325fc44f15cd1",
+         intel: "3231571db095d93aa9b7b958058508a129dcc1588d27b7b722ddbfe51c5305e7"
 
   url "https://github.com/ungoogled-software/ungoogled-chromium-macos/releases/download/#{version}/ungoogled-chromium_#{version}_#{arch}-macos.dmg",
       verified: "github.com/ungoogled-software/ungoogled-chromium-macos/"
