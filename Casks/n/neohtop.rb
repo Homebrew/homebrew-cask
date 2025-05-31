25d9d29a5236dd253e6893ce7432e7733da4aaa6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-31 21:46:06 +0000

    neohtop 1.1.3

--- Casks/n/neohtop.rb
+++ Casks/n/neohtop.rb
@@ -2,9 +2,9 @@ cask "neohtop" do
   arch arm: "silicon", intel: "intel"
   platform = on_arch_conditional arm: "aarch64", intel: "x64"
 
-  version "1.1.2"
-  sha256 arm:   "41b8e8eb8a9d897d8485760a6da34f2e135902756c4d5edcedf54d09410a5d77",
-         intel: "1526320351b9199ba58d69d34450661262bbe42264fbc3facc7dcddd20ce7bb8"
+  version "1.1.3"
+  sha256 arm:   "8fe8d865d2f8f1e58b3062bf53d039c26330877004edd99067b7de17aaeb3639",
+         intel: "1b78eeed5167d0211125071d578e5794ac018fc17dba7f72eeb85461f4bc4ffc"
 
   url "https://github.com/Abdenasser/neohtop/releases/download/v#{version}/#{arch}-NeoHtop_#{version}_#{platform}.dmg",
       verified: "github.com/Abdenasser/neohtop/"
