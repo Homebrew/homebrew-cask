35ef6f8a2cc3eb679cd1e69cc89ab65e7677cc53
Klaus Hipp <khipp@users.noreply.github.com>
2025-06-01 09:00:55 +0200

    firebase-admin: deprecate

--- Casks/f/firebase-admin.rb
+++ Casks/f/firebase-admin.rb
@@ -4,12 +4,10 @@ cask "firebase-admin" do
 
   url "https://firebaseadmin.com/downloads/#{version}/firebase-admin-mac-#{version}.dmg"
   name "Firebase Admin"
+  desc "Admin user interface for Firebase"
   homepage "https://firebaseadmin.com/"
 
-  livecheck do
-    url "https://firebaseadmin.com/download.php?os=mac"
-    strategy :header_match
-  end
+  deprecate! date: "2025-06-01", because: :unmaintained
 
   app "firebase-admin.app"
 
