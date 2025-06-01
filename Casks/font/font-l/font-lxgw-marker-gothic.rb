ffcf6ee92fd0feb8f1bbde3ff5cc99330466c118
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-01 00:19:06 +0000

    font-lxgw-marker-gothic: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml) workflow.

---
+++ Casks/font/font-l/font-lxgw-marker-gothic.rb
@@ -0,0 +1,12 @@
+cask "font-lxgw-marker-gothic" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/lxgwmarkergothic/LXGWMarkerGothic-Regular.ttf"
+  name "LXGW Marker Gothic"
+  homepage "https://github.com/aaronbell/LxgwMarkerGothic"
+
+  font "LXGWMarkerGothic-Regular.ttf"
+
+  # No zap stanza required
+end
