4eac5935ecd6aaefa73070da6b36801ba1f55757
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-01 15:47:34 +0000

    lobehub 1.90.3

--- Casks/l/lobehub.rb
+++ Casks/l/lobehub.rb
@@ -1,9 +1,9 @@
 cask "lobehub" do
   arch arm: "-arm64"
 
-  version "1.90.1"
-  sha256 arm:   "68154dda5b6ec60240f706cf096af7e60efad81ef511468656e8f9dd13f9612f",
-         intel: "b4e134cccd4b3d141d58edc27fd118a7c0117120422d499e573b740b60549847"
+  version "1.90.3"
+  sha256 arm:   "4bda0456078d4f9c931ebbfb74fcf63a15c50d4824b0410776ea3489a7301294",
+         intel: "a8d67cd978492d48cd33655ac60ae4c1e83794b5866aeedb71b9404630d86951"
 
   url "https://github.com/lobehub/lobe-chat/releases/download/v#{version}/LobeHub-Beta-#{version}#{arch}-mac.zip"
   name "LobeHub"
