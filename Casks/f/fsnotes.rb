bf520321d916f25d5acabd64d053b00794272deb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-01 18:44:54 +0000

    fsnotes 6.10.1

--- Casks/f/fsnotes.rb
+++ Casks/f/fsnotes.rb
@@ -1,6 +1,6 @@
 cask "fsnotes" do
-  version "6.10.0"
-  sha256 "7538d36d112fe356a6e898a45b4a3765ceaf4d2e1adeefe9245d90b44500f38c"
+  version "6.10.1"
+  sha256 "68653947b9edf5e5a8790c44e0fdc354002b24f617cd1b4c026af3ba09c0ac01"
 
   url "https://github.com/glushchenko/fsnotes/releases/download/#{version}/FSNotes_#{version}.zip",
       verified: "github.com/glushchenko/fsnotes/"
