70e18f763a3593b4a27eaa2fbb34d8ba774b2240
bevanjkay <email@bevankay.me>
2025-06-01 17:45:04 +1000

    font-simple-icons 15.0.0

--- Casks/font/font-s/font-simple-icons.rb
+++ Casks/font/font-s/font-simple-icons.rb
@@ -1,13 +1,14 @@
 cask "font-simple-icons" do
-  version "14.15.0"
-  sha256 "56450f9cb730a48a20a6104000fedd91159a2d800d7d1e836bd46630b3ac0dc1"
+  version "15.0.0"
+  sha256 "6241ff475e0fab70d82ad98e711764be546280c373b02dcc211a39034304903f"
 
   url "https://github.com/simple-icons/simple-icons-font/releases/download/#{version}/simple-icons-font-#{version}.zip",
       verified: "github.com/simple-icons/simple-icons-font/"
   name "Simple Icons"
   homepage "https://simpleicons.org/"
 
-  font "font/SimpleIcons.ttf"
+  font "font/SimpleIcons-Fit.otf"
+  font "font/SimpleIcons.otf"
 
   # No zap stanza required
 end
