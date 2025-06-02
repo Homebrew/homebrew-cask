ed2b822959f8ec2752b6b504530c5a98ea7d127e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-02 07:03:29 +0000

    triliumnext-notes 0.94.0

--- Casks/t/triliumnext-notes.rb
+++ Casks/t/triliumnext-notes.rb
@@ -1,9 +1,9 @@
 cask "triliumnext-notes" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.93.0"
-  sha256 arm:   "9268ce38a584214b75f265b304968df1575480912ec3ce65e17f94dbccacad90",
-         intel: "66856caf81ed6236f2b47155a19534aaa2ec491fc19eb1ecd23fc2cbe18aba8f"
+  version "0.94.0"
+  sha256 arm:   "7e384342319f07670ec1a7a78578299def6b9eeb3823ee1a7245b8d39c6fbdc6",
+         intel: "2c63ae136b1fe5abc4effe2164a8d1fdb4d9a131740e8d10495b536c8a1d6a59"
 
   url "https://github.com/TriliumNext/Notes/releases/download/v#{version}/TriliumNextNotes-v#{version}-macos-#{arch}.dmg",
       verified: "github.com/TriliumNext/Notes/"
