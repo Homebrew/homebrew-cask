3f4a7b89b4ea32f226c830ccd7ac821efcfcd5bd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-02 21:39:06 +0000

    companion@beta 4.0.0+8042-main-ffb4de1ba4

--- Casks/c/companion@beta.rb
+++ Casks/c/companion@beta.rb
@@ -2,9 +2,9 @@ cask "companion@beta" do
   arch arm: "arm64", intel: "x64"
   livecheck_arch = on_arch_conditional arm: "arm", intel: "intel"
 
-  version "4.0.0+8038-main-0c24e69dee"
-  sha256 arm:   "a10fef23a3362ef8fbedcd0c489657c23148e8975919bf1a68da96ea3684b2e0",
-         intel: "963d3f58ac45d9f86f79f08aa10ca804941cd13cfee8f38fbc56d8a44f0f17d4"
+  version "4.0.0+8042-main-ffb4de1ba4"
+  sha256 arm:   "aa34d7d2e4d8dfb38ee5a53eb12cf51839a353faba5a4f5aa195fd12b1f00c1c",
+         intel: "6f1129d550710e259ef077d558b8e9afac9dba82bfc09c8d0060d00a1e9c685c"
 
   url "https://s4.bitfocus.io/builds/companion/companion-mac-#{arch}-#{version}.dmg"
   name "Bitfocus Companion"
