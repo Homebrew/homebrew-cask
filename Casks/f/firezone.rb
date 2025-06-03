a970c97b82ec92e64237ebb3495551a6f996dfb5
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-06-02 22:25:40 -0400

    firezone 1.5.1 (new cask)

---
+++ Casks/f/firezone.rb
@@ -0,0 +1,42 @@
+cask "firezone" do
+  version "1.5.1"
+  sha256 "25ae74da4d5ecc48923889c4c42fb41a3511cea3c0f5af4f6d69fb96559e7c7d"
+
+  url "https://github.com/firezone/firezone/releases/download/macos-client-#{version}/firezone-macos-client-#{version}.dmg",
+      verified: "github.com/firezone/firezone/"
+  name "Firezone"
+  desc "Zero-trust access platform built on WireGuard"
+  homepage "https://www.firezone.dev/"
+
+  # Not every GitHub release provides a file for macOS, so we check multiple
+  # recent releases instead of only the "latest" release.
+  livecheck do
+    url :url
+    regex(/^firezone[._-]macos[._-]client[._-]v?(\d+(?:\.\d+)+)\.(?:dmg|pkg)$/i)
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        next if release["draft"] || release["prerelease"]
+
+        release["assets"]&.map do |asset|
+          match = asset["name"]&.match(regex)
+          next if match.blank?
+
+          match[1]
+        end
+      end.flatten
+    end
+  end
+
+  depends_on macos: ">= :monterey"
+
+  app "Firezone.app"
+
+  # The app installs a system extension that cannot be removed systematically at this time.
+  # After the limitation is removed, `systemextensionsctl uninstall "dev.firezone.firezone.network-extension"`
+  # could be used to uninstall the extension.
+  zap trash: [
+    "~/Library/Application Scripts/*.dev.firezone.firezone",
+    "~/Library/Containers/dev.firezone.firezone,",
+    "~/Library/Group Containers/*.dev.firezone.firezone",
+  ]
+end
