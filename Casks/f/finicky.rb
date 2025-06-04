5abd228483ebe7ca4799a4c9d3f18b7d70400dbf
Kaleb Elwert <belak@coded.io>
2025-06-03 23:34:40 -0700

    finicky 4.1.2

--- Casks/f/finicky.rb
+++ Casks/f/finicky.rb
@@ -1,6 +1,6 @@
 cask "finicky" do
-  version "4.1.1"
-  sha256 "e423f5ba224ffe1d9cfa1340143a1496e602ab155aeeffbe59b9b0b1094fd3e7"
+  version "4.1.2"
+  sha256 "044b20ac326546f799e7a4ed9a87672b9a86f92f16a5d57bc06c07201f3b27a5"
 
   url "https://github.com/johnste/finicky/releases/download/v#{version}/Finicky.dmg"
   name "Finicky"
