a8d405583ea315874bb8cda9ac40e286ad380d28
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-04 01:06:56 +0000

    macfuse@dev 5.0.2

--- Casks/m/macfuse@dev.rb
+++ Casks/m/macfuse@dev.rb
@@ -1,6 +1,6 @@
 cask "macfuse@dev" do
-  version "5.0.1"
-  sha256 "8c0a65aba7c1937e6902f876295d6b9e27a6ce3675077763e51e9f4f76f8c11d"
+  version "5.0.2"
+  sha256 "5d0b1367b654f49e24cec4178a247adf93f0cd37aa931771cbfec35bd2bc27b0"
 
   url "https://github.com/macfuse/macfuse/releases/download/macfuse-#{version}/macfuse-#{version}.dmg",
       verified: "github.com/macfuse/macfuse/"
