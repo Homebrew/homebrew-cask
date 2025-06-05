38942822f1d5061735c9d72571e76c0eae015e2b
furkanksl <furkankoseoglu65@gmail.com>
2025-06-05 13:40:07 +0300

    Add FreeWhisper v0.0.6

---
+++ Casks/f/freewhisper.rb
@@ -0,0 +1,17 @@
+cask "freewhisper" do
+  version "0.0.6"
+  sha256 "da42224ee30b20e528ff8ef728e768dee4879f173d7004c6ead2274140569269"
+
+  url "https://github.com/furkanksl/FreeWhisper/releases/download/#{version}/FreeWhisper.dmg"
+  name "FreeWhisper"
+  desc "Open-source speech recognition app powered by OpenAI's Whisper"
+  homepage "https://github.com/furkanksl/FreeWhisper"
+
+  app "FreeWhisper.app"
+
+  zap trash: [
+    "~/Library/Application Support/FreeWhisper",
+    "~/Library/Caches/com.furkanksl.FreeWhisper",
+    "~/Library/Preferences/com.furkanksl.FreeWhisper.plist",
+  ]
+end
