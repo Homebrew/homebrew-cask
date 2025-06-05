f0fb1846712fc77acc1644d2a862fa253e316fce
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-05 09:49:56 +0000

    intellij-idea@eap 2025.2,252.19874.12

--- Casks/i/intellij-idea@eap.rb
+++ Casks/i/intellij-idea@eap.rb
@@ -1,9 +1,9 @@
 cask "intellij-idea@eap" do
   arch arm: "-aarch64"
 
-  version "2025.2,252.18003.27"
-  sha256 arm:   "45684ec70217e1cd06bd523f4e9b6f8c4113e612cf92cf43a5dcfb8cfa6fc07f",
-         intel: "78efb6ea0041e2bda9431a2bfdb94ebb4aa6da31cd67cf4dfd56a2281a1bb806"
+  version "2025.2,252.19874.12"
+  sha256 arm:   "e6de0e1d0f06726a95ed341956ed7c0b83b232665cebb8fef5f6bd60f1be2448",
+         intel: "5bbcede97a0348d3e201702c8c552c3941d383e0ae322e40fc3fc0049121c8b8"
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version.csv.second}#{arch}.dmg"
   name "IntelliJ IDEA EAP"
