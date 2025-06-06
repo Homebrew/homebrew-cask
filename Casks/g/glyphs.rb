fac0ad7799b262bcb3fe1a2500b57f0aa12c0977
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-06 00:53:28 +0000

    glyphs 3.4,3416

--- Casks/g/glyphs.rb
+++ Casks/g/glyphs.rb
@@ -1,6 +1,6 @@
 cask "glyphs" do
-  version "3.4,3414"
-  sha256 "5d611c8156f06abddc7d2dde0d7a9ba1ee14a8c5d082777e65e2c9bd41728a2f"
+  version "3.4,3416"
+  sha256 "2ddad081eeb36c60344af6abd585f955036850c6c948390acafb69947c6fe23b"
 
   url "https://updates.glyphsapp.com/Glyphs#{version.csv.first}-#{version.csv.second}.zip"
   name "Glyphs"
