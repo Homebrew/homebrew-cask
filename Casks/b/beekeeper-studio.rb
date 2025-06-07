804d711f1fa1752eadd79b52dec23353d631b64d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-07 03:43:10 +0000

    beekeeper-studio 5.2.12

--- Casks/b/beekeeper-studio.rb
+++ Casks/b/beekeeper-studio.rb
@@ -1,9 +1,9 @@
 cask "beekeeper-studio" do
   arch arm: "-arm64"
 
-  version "5.2.10"
-  sha256 arm:   "653fe2e457519cdbaecf0bb489a6b914fe3d797965f2d01485a3ce4170ce5718",
-         intel: "94ad3dd2730edb988fd1644a1a0ec97f71f22daaa90ab21a33164f0d9c6fb1f5"
+  version "5.2.12"
+  sha256 arm:   "efd22888604fa39ed379c505fb09871e21c9addb6c8b3fefb00a63a826cac53a",
+         intel: "af19a501f639e62f92cbab4794f9d4919077cb3cf56d6fc17103508d1e3645ac"
 
   url "https://github.com/beekeeper-studio/beekeeper-studio/releases/download/v#{version}/Beekeeper-Studio-#{version}#{arch}.dmg",
       verified: "github.com/beekeeper-studio/beekeeper-studio/"
