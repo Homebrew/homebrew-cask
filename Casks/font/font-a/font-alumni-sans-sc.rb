10cae48c2d42746b72981a9ddbecaf026e40be6b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-08 00:17:02 +0000

    font-alumni-sans-sc: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-a/font-alumni-sans-sc.rb
+++ Casks/font/font-a/font-alumni-sans-sc.rb
@@ -3,10 +3,11 @@ cask "font-alumni-sans-sc" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/alumnisanssc"
   name "Alumni Sans SC"
-  homepage "https://github.com/googlefonts/alumni"
+  homepage "https://fonts.google.com/specimen/Alumni+Sans+SC"
 
   font "AlumniSansSC-Italic[wght].ttf"
   font "AlumniSansSC[wght].ttf"
