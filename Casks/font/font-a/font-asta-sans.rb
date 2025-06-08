e11b9e7f39976a9b0f5dcfbed69c9eaadbea19ac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-08 00:17:02 +0000

    font-asta-sans: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/master/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-a/font-asta-sans.rb
+++ Casks/font/font-a/font-asta-sans.rb
@@ -2,9 +2,10 @@ cask "font-asta-sans" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/astasans/AstaSans%5Bwght%5D.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/astasans/AstaSans%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Asta Sans"
-  homepage "https://github.com/42dot/Asta-Sans"
+  homepage "https://fonts.google.com/specimen/Asta+Sans"
 
   font "AstaSans[wght].ttf"
 
