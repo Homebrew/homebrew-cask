921dcc241a29b49c030a04999662155663135cce
Eric Knibbe <enk3@outlook.com>
2025-06-09 11:05:37 -0400

    blockblock: remove legacy version

--- Casks/b/blockblock.rb
+++ Casks/b/blockblock.rb
@@ -1,46 +1,26 @@
 cask "blockblock" do
-  on_mojave :or_older do
-    version "0.9.9.4"
-    sha256 "6ab3a8224e8bc77b9abe8d41492c161454c6b0266e60e61b06931fed4b431282"
+  version "2.2.5"
+  sha256 "836d8c4ccf4bc33be46f019f1f0fcb2ec61e84afa84dbb55098bb0ed7350fc89"
 
-    url "https://bitbucket.org/objective-see/deploy/downloads/BlockBlock_#{version}.zip",
-        verified: "bitbucket.org/objective-see/"
-
-    installer script: {
-      executable: "#{staged_path}/BlockBlock Installer.app/Contents/MacOS/BlockBlock",
-      args:       ["-install"],
-      sudo:       true,
-    }
-
-    uninstall script: {
-      executable: "#{staged_path}/BlockBlock Installer.app/Contents/MacOS/BlockBlock",
-      args:       ["-uninstall"],
-      sudo:       true,
-    }
-  end
-  on_catalina :or_newer do
-    version "2.2.5"
-    sha256 "836d8c4ccf4bc33be46f019f1f0fcb2ec61e84afa84dbb55098bb0ed7350fc89"
-
-    url "https://github.com/objective-see/BlockBlock/releases/download/v#{version}/BlockBlock_#{version}.zip",
-        verified: "github.com/objective-see/BlockBlock/"
-
-    installer script: {
-      executable: "#{staged_path}/BlockBlock Installer.app/Contents/MacOS/BlockBlock Installer",
-      args:       ["-install"],
-      sudo:       true,
-    }
-
-    uninstall script: {
-      executable: "#{staged_path}/BlockBlock Installer.app/Contents/MacOS/BlockBlock Installer",
-      args:       ["-uninstall"],
-      sudo:       true,
-    }
-  end
-
+  url "https://github.com/objective-see/BlockBlock/releases/download/v#{version}/BlockBlock_#{version}.zip",
+      verified: "github.com/objective-see/BlockBlock/"
   name "BlockBlock"
   desc "Monitors common persistence locations"
   homepage "https://objective-see.org/products/blockblock.html"
+
+  depends_on macos: ">= :catalina"
+
+  installer script: {
+    executable: "#{staged_path}/BlockBlock Installer.app/Contents/MacOS/BlockBlock Installer",
+    args:       ["-install"],
+    sudo:       true,
+  }
+
+  uninstall script: {
+    executable: "#{staged_path}/BlockBlock Installer.app/Contents/MacOS/BlockBlock Installer",
+    args:       ["-uninstall"],
+    sudo:       true,
+  }
 
   zap trash: [
     "~/Library/Caches/com.objective-see.blockblock.helper",
