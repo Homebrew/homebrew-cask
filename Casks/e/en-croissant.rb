84548b8abab01a7a12002d368a42302f085f2b7a
Yodel <43578323+sid0-0@users.noreply.github.com>
2025-06-09 22:01:03 +0530

    style fixes

--- Casks/e/en-croissant.rb
+++ Casks/e/en-croissant.rb
@@ -3,9 +3,9 @@ cask "en-croissant" do
   sha256 "38b5a1e4858f2e557c51d307f4bddfe371e958ef37ddd3d37f1ba836be160d46"
 
   url "https://github.com/franciscoBSalgueiro/en-croissant/releases/download/v#{version}/en-croissant_#{version}_x64.dmg",
-      verified: "https://github.com/franciscoBSalgueiro/en-croissant/"
+      verified: "github.com/franciscoBSalgueiro/en-croissant/"
   name "En-Croissant"
-  desc "An open-source, cross-platform chess GUI that aims to be powerful, customizable and easy to use."
+  desc "Open-source, cross-platform chess GUI"
   homepage "https://encroissant.org/"
 
   livecheck do
@@ -18,15 +18,15 @@ cask "en-croissant" do
 
   app "en-croissant.app"
 
+  zap trash: [
+    "~/Library/Application Support/org.encroissant.app",
+    "~/Library/Caches/org.encroissant.app",
+    "~/Library/Logs/org.encroissant.app",
+    "~/Library/Saved Application State/org.encroissant.app.savedState",
+    "~/Library/WebKit/org.encroissant.app",
+  ]
+
   caveats do
     requires_rosetta
   end
-
-  zap trash: [
-    "~/Library/Application Support/org.encroissant.app",
-    "~/Library/Saved Application State/org.encroissant.app.savedState",
-    "~/Library/Caches/org.encroissant.app",
-    "~/Library/Logs/org.encroissant.app",
-    "~/Library/WebKit/org.encroissant.app",
-  ]
 end
