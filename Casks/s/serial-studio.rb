6a23849433813e8747367608669f66e23577f96d
Klaus Hipp <khipp@users.noreply.github.com>
2025-06-09 13:02:19 +0200

    serial-studio 3.1.1

--- Casks/s/serial-studio.rb
+++ Casks/s/serial-studio.rb
@@ -1,11 +1,8 @@
 cask "serial-studio" do
-  arch arm: "arm64", intel: "x86_64"
+  version "3.1.1"
+  sha256 "143c3f3708e33df5b238b442a78692464db0c3a5d7c74d753cebf443742b9354"
 
-  version "3.0.6"
-  sha256 arm:   "86c36cd424ac22e0c01b6f02f4f94f99d27d96f9f5ced3da2ef6e303a64ef282",
-         intel: "00cf58d347c5bc71f89895df0a191543e65e32a79ce6847e466e67061399c576"
-
-  url "https://github.com/Serial-Studio/Serial-Studio/releases/download/v#{version}/Serial-Studio-#{version}-macOS-#{arch}.dmg",
+  url "https://github.com/Serial-Studio/Serial-Studio/releases/download/v#{version}/Serial-Studio-#{version}-macOS-Universal.dmg",
       verified: "github.com/Serial-Studio/Serial-Studio/"
   name "Serial Studio"
   desc "Data visualisation software for embedded devices and projects"
