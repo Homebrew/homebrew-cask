e05634b23476659b164cdec5fcf2ffde39af288d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-09 18:44:16 +0000

    dataflare 2.0.2

--- Casks/d/dataflare.rb
+++ Casks/d/dataflare.rb
@@ -1,9 +1,9 @@
 cask "dataflare" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "2.0.1"
-  sha256 arm:   "65e29ddd0cc4e76970e3d0a400038316175630f2165379dda58f1f1750af4584",
-         intel: "7d3055e34166be5e68b29e26d574a014364b932bfafdf067e1f74c1d1182918d"
+  version "2.0.2"
+  sha256 arm:   "4476d14417f62be7095e12c65bd70b33f1b9c3585f4325231d63baaf2c7a78be",
+         intel: "d1ef62148c3747bea27bda594f010131d8fcc2cf89dd0b347e37b474af07c1d8"
 
   url "https://assets.dataflare.app/release/darwin/#{arch}/Dataflare-#{version}.dmg"
   name "Dataflare"
