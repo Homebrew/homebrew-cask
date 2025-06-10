4e6b9cc4b378b5112e7c65ee920443720ae36847
Eric Knibbe <enk3@outlook.com>
2025-06-10 10:25:34 -0400

    db-browser-for-sqlite@nightly: fix URL & livecheck
    Co-Authored-By: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/d/db-browser-for-sqlite@nightly.rb
+++ Casks/d/db-browser-for-sqlite@nightly.rb
@@ -1,11 +1,30 @@
 cask "db-browser-for-sqlite@nightly" do
-  version :latest
-  sha256 :no_check
+  version "20250608"
+  sha256 "d56d949d87e7712982caa5e43d51e6292c7e1e8489e4566736a873d6a6b17695"
 
-  url "https://nightlies.sqlitebrowser.org/latest/DB.Browser.for.SQLite-universal.dmg"
+  url "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/nightly/DB.Browser.for.SQLite-universal_#{version}.dmg",
+      verified: "github.com/sqlitebrowser/sqlitebrowser/"
   name "DB Browser for SQLite Nightly"
   desc "Database browser for SQLite"
   homepage "https://sqlitebrowser.org/"
+
+  livecheck do
+    url :url
+    regex(/^DB[._-]Browser[._-]for[._-]SQLite[._-]universal[._-]v?(\d+(?:\.\d+)*)\.dmg/i)
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        next if release["tag_name"] != "nightly"
+        next if release["draft"]
+
+        release["assets"]&.map do |asset|
+          match = asset["name"]&.match(regex)
+          next if match.blank?
+
+          match[1]
+        end
+      end.flatten
+    end
+  end
 
   app "DB Browser for SQLite Nightly.app"
 
