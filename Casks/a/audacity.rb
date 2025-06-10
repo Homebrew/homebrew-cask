d41cc3e5f58bcd02a32b4986c02a63ce6ec2c5fa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-10 15:35:33 +0000

    audacity 3.7.4

--- Casks/a/audacity.rb
+++ Casks/a/audacity.rb
@@ -1,9 +1,9 @@
 cask "audacity" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "3.7.3"
-  sha256 arm:   "9f763cf59fff9817da8f273f1fb28d3d8680eaf0444abcc70b84d8c5476d8bcd",
-         intel: "4f3472d2314f902892c68d74cb23a416a9027232aa54bbdd3017a081590ee0e3"
+  version "3.7.4"
+  sha256 arm:   "63c551cc74e16ff8e788b51aeacda8e866bb21213cf721a9b09a8a3b4e81d046",
+         intel: "11dd95c619dd1cb8e47ee78e0855affc199a669a05439d194c81ed1bfc90b263"
 
   url "https://github.com/audacity/audacity/releases/download/Audacity-#{version}/audacity-macOS-#{version}-#{arch}.dmg",
       verified: "github.com/audacity/audacity/"
