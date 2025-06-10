6db4d680d0ad148291aa595c72df6924aa78d0bf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-10 03:55:22 +0000

    fishing-funds 8.5.0

--- Casks/f/fishing-funds.rb
+++ Casks/f/fishing-funds.rb
@@ -1,9 +1,9 @@
 cask "fishing-funds" do
   arch arm: "-arm64"
 
-  version "8.4.2"
-  sha256 arm:   "136ab036e9e25dc4025a45ca427a46d9961cfcec5ca727f0be5169575533b698",
-         intel: "47e6898388c8e8e5772b1c3f5e1e931a8c2cb03d0c6c2696c3600082b2b4513a"
+  version "8.5.0"
+  sha256 arm:   "f21e6b0a15de1d125cff9b0c8baef16b9a7d265e8ea5741e238543ef1a5f2097",
+         intel: "95268a284a78ce712a31e2adbf3f972e782121b4597ee8a3b02f6f33690ef9d3"
 
   url "https://github.com/1zilc/fishing-funds/releases/download/v#{version}/Fishing-Funds-#{version}#{arch}.dmg",
       verified: "github.com/1zilc/fishing-funds/"
