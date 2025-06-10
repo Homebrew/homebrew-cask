dfce09a663021acbd27782f39c1ed9c8fd17322d
eleanordoesntcode <166862894+eleanordoesntcode@users.noreply.github.com>
2025-06-11 01:16:50 +0200

    frescobaldi 4.0.3

--- Casks/f/frescobaldi.rb
+++ Casks/f/frescobaldi.rb
@@ -1,9 +1,9 @@
 cask "frescobaldi" do
   arch intel: "-Intel"
 
-  version "4.0.2"
-  sha256 arm:   "f5e953fa2908c21bcef466489f840f7ea120c61894e7731fb1d4a467609cd0a7",
-         intel: "a5ae49e8f9568cd504b1d905415c6d11b47a8a14ae95284adaa5229a495106fe"
+  version "4.0.3"
+  sha256 arm:   "6242cf4a2ec783751f12d1afe6170f582a9be22a413790fc9e1708fdd3b0c1ce",
+         intel: "581f4942cb2e5d8bac37725623e03321fe545eefbabaaf4a68464033bfe2c1f9"
 
   url "https://github.com/frescobaldi/frescobaldi/releases/download/v#{version}/Frescobaldi-#{version}#{arch}.dmg",
       verified: "github.com/frescobaldi/frescobaldi/"
