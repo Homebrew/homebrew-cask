1a5cd92cb5da148c5b5e42332a613100e01d5eeb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-10 09:44:55 +0000

    fontgoggles 1.8.7

--- Casks/f/fontgoggles.rb
+++ Casks/f/fontgoggles.rb
@@ -1,6 +1,6 @@
 cask "fontgoggles" do
-  version "1.8.6"
-  sha256 "82b5d55078a02b03030dd0234caeef878df11372550a57d0e915cdd115027d44"
+  version "1.8.7"
+  sha256 "9361416c311455a02e9ddb36b6dac3cc06469282f4ef0ee40c12af7522bb6cb8"
 
   url "https://github.com/justvanrossum/fontgoggles/releases/download/v#{version}/FontGoggles.dmg",
       verified: "github.com/justvanrossum/fontgoggles/"
