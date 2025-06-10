72be367fcd926ccd788066ad65424f354c53ca7e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-10 03:44:24 +0000

    block-goose 1.0.27

--- Casks/b/block-goose.rb
+++ Casks/b/block-goose.rb
@@ -1,9 +1,9 @@
 cask "block-goose" do
   arch intel: "_intel_mac"
 
-  version "1.0.26"
-  sha256 arm:   "11e9d368caf434ed215125146e209f36f4a614a18c0ca0512f5de41185076032",
-         intel: "bccdb280a6f9fcc6fdfc9fbb37364cead8555a220f6200d6dc1caf249ce1555e"
+  version "1.0.27"
+  sha256 arm:   "3581b9649388cd0bd6807113d80319d88ef22df62ea9bf85aab1173394fb9daa",
+         intel: "f3673e2af7ce54c5ffd17d7fdb937f4a7df5aa59ab5a3736c4f876c3f3f9dd07"
 
   url "https://github.com/block/goose/releases/download/v#{version}/Goose#{arch}.zip",
       verified: "github.com/block/goose/"
