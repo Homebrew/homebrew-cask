68d695ffb7224d650f52b00c4aef43feb6009d44
Manuel <manuel@riok.ch>
2025-06-10 15:40:30 +0200

    Correct Kreya download URL and description

--- Casks/k/kreya.rb
+++ Casks/k/kreya.rb
@@ -2,11 +2,12 @@ cask "kreya" do
   arch arm: "arm64", intel: "x64"
 
   version "1.17.0"
-  sha256 :no_check
+  sha256 arm:   "947eace5f26c7ce0a895a1ed04a2f8222603e165a63c0c459eefb6a1bc4bc5bc",
+         intel: "826a7018e2c520e7a3cfab2a03827c9170dc1cbdabdced0e42f7f9c3c0763d28"
 
-  url "https://stable-downloads.kreya.app/Kreya-osx-#{arch}.zip"
+  url "https://stable-downloads.kreya.app/#{version}/Kreya-osx-#{arch}.zip"
   name "Kreya"
-  desc "GUI Client for interacting with gRPC services"
+  desc "GUI Client for interacting with gRPC, REST and WebSocket services"
   homepage "https://kreya.app/"
 
   livecheck do
