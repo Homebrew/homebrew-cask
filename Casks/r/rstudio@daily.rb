b968f52bd6220fea8eea374dc1548ad537542795
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-10 19:04:10 +0000

    rstudio@daily 2025.08.0-daily-195

--- Casks/r/rstudio@daily.rb
+++ Casks/r/rstudio@daily.rb
@@ -1,6 +1,6 @@
 cask "rstudio@daily" do
-  version "2025.08.0-daily-194"
-  sha256 "09849a1d52a99b8060772153e702ba501de6e3dd029668916a091574b3332f05"
+  version "2025.08.0-daily-195"
+  sha256 "b6a0eb750e4fffa7c03de9d81e1fca15645350d56907aea97eb1794181e536b1"
 
   url "https://rstudio-ide-build.s3.amazonaws.com/electron/macos/RStudio-#{version}.dmg",
       verified: "rstudio-ide-build.s3.amazonaws.com/electron/macos/"
