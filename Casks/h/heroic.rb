3c0cfce2080326d831f880b8505226ee7ed97a64
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-10 15:59:26 +0000

    heroic 2.17.2

--- Casks/h/heroic.rb
+++ Casks/h/heroic.rb
@@ -1,9 +1,9 @@
 cask "heroic" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.17.1"
-  sha256 arm:   "0e64b8f138534c7e0ff165197e26dd3bfac7004118dd6963e18f22e67969ea8e",
-         intel: "4ba3e887a679d4bca4e7e61d66a64d233c5db0931b3df8069135b126070e39f4"
+  version "2.17.2"
+  sha256 arm:   "bd5bbfd6d6a3252df3cd83384ecf2eacbb67ef8f5626b9ad294bda889e318e53",
+         intel: "9e54cd13cad2ec106f6df64c9e25bc1e27d277bd4886fc70c57e49ab331a7cbb"
 
   url "https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v#{version}/Heroic-#{version}-macOS-#{arch}.dmg"
   name "Heroic Games Launcher"
