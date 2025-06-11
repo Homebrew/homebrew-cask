8719d864035e9a1cb09840f24bff5a91cf6619b2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-11 21:52:46 +0000

    jami 2.34,2025061115

--- Casks/j/jami.rb
+++ Casks/j/jami.rb
@@ -1,6 +1,6 @@
 cask "jami" do
-  version "2.34,2025050710"
-  sha256 "c723f25c83535aa4c0b7ddefd62257c1f770e13e326116ce1279494f63726256"
+  version "2.34,2025061115"
+  sha256 "e8d29a833e9db16463071c3341e2507150c930491c5475da324282011368fed9"
 
   url "https://dl.jami.net/mac_osx/jami#{version.csv.second}.dmg"
   name "Jami"
