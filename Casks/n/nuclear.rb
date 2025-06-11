a8d9122c8e9743047764afd8e56e2a07cfd2e626
Klaus Hipp <khipp@users.noreply.github.com>
2025-06-11 21:22:48 +0200

    nuclear 0.6.48

--- Casks/n/nuclear.rb
+++ Casks/n/nuclear.rb
@@ -1,15 +1,15 @@
 cask "nuclear" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.6.47"
-  sha256 arm:   "9493e79b37cc72e8f7a888f7b52a0fa88ddc0fb20256ac749e8821d004eebc07",
-         intel: "458c2aa169758a1519f5e7dcd5a66fb5be9c3394199924bc299ad34b9fae0fca"
+  version "0.6.48"
+  sha256 arm:   "9749225105d3c4c8000ac4060a1a68f25be64999c54c2d786bd5faa1c0a3a619",
+         intel: "6c62adae6e054f2103b5697bdb4510ff4739fee84a9de8b8645df62bde74bbb3"
 
   url "https://github.com/nukeop/nuclear/releases/download/v#{version}/nuclear-v#{version}-#{arch}.dmg",
       verified: "github.com/nukeop/nuclear/"
   name "Nuclear"
   desc "Streaming music player"
-  homepage "https://nuclear.js.org/"
+  homepage "https://nuclearplayer.com/"
 
   livecheck do
     url :url
