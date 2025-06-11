c01281cf68ee6dba30db6c7c59abb4878a2d281a
Klaus Hipp <khipp@users.noreply.github.com>
2025-06-11 20:18:59 +0200

    tourbox-console 5.8.5

--- Casks/t/tourbox-console.rb
+++ Casks/t/tourbox-console.rb
@@ -1,6 +1,6 @@
 cask "tourbox-console" do
-  version "5.8.4"
-  sha256 "0252f2b99652356512faaa7f97684b5d9431f246656dfee2bec4ab005edfe0fc"
+  version "5.8.5"
+  sha256 "5a482c443a170cc10f843765da25e364a196fc427ff049c91d9dc5b5e9052a05"
 
   url "https://tourbox-web-files.s3.us-west-2.amazonaws.com/prod/console/TourBoxInstall#{version}.zip",
       verified: "tourbox-web-files.s3.us-west-2.amazonaws.com/prod/console/"
