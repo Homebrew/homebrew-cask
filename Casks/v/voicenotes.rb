648de8ee675d249df3bda4125277c877b759b2b3
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2025-06-11 17:07:55 -0400

    Use an arch: stanza and interpolate variables into the URL for the arch

--- Casks/v/voicenotes.rb
+++ Casks/v/voicenotes.rb
@@ -1,18 +1,10 @@
 cask "voicenotes" do
+  arch arm: "arm64", intel: "x86_64"
+
   version :latest
   sha256 :no_check
 
-  on_arm do
-    url "https://dl.voicenotes.com/release/latest/Voicenotes-latest-arm64.dmg"
-
-    depends_on arch: :arm64
-  end
-  on_intel do
-    url "https://dl.voicenotes.com/release/latest/Voicenotes-latest.dmg"
-
-    depends_on arch: :intel
-  end
-
+  url "https://dl.voicenotes.com/release/latest/Voicenotes-latest#{on_arch_conditional arm: "-arm64", intel: ""}.dmg"
   name "Voicenotes"
   desc "AI-powered app for recording, transcribing, and summarizing voice notes"
   homepage "https://voicenotes.com/"
@@ -20,6 +12,8 @@ cask "voicenotes" do
   livecheck do
     skip "No version information available in the download URL or headers"
   end
+
+  depends_on macos: ">= :big_sur"
 
   app "Voicenotes.app"
 
