7874aac5fcd9263f51ded14cc2a470251eb2b24c
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2025-06-11 18:11:53 -0400

    Specify the version of voicenotes

--- Casks/v/voicenotes.rb
+++ Casks/v/voicenotes.rb
@@ -1,16 +1,18 @@
 cask "voicenotes" do
   arch arm: "arm64", intel: "x86_64"
 
-  version :latest
+  version "1.3.8"
   sha256 :no_check
 
-  url "https://dl.voicenotes.com/release/latest/Voicenotes-latest#{on_arch_conditional arm: "-arm64", intel: ""}.dmg"
+  url "https://github.com/brewdotcom/vn-apps-release/releases/download/#{version}/Voicenotes-#{version}#{on_arch_conditional arm: "-arm64", intel: ""}.dmg"
+      verified: "github.com/brewdotcom/vn-apps-release/"
   name "Voicenotes"
   desc "AI-powered app for recording, transcribing, and summarizing voice notes"
   homepage "https://voicenotes.com/"
 
   livecheck do
-    skip "No version information available in the download URL or headers"
+    url :url
+    strategy :github_latest
   end
 
   depends_on macos: ">= :big_sur"
