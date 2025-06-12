a6a193a6676c391e266481595ec9735e6aadd8a4
botantony <antonsm21@gmail.com>
2025-06-12 17:34:29 +0200

    dintch: added `no_autobump!`

--- Casks/d/dintch.rb
+++ Casks/d/dintch.rb
@@ -23,6 +23,8 @@ cask "dintch" do
     end
   end
 
+  no_autobump! because: :requires_manual_review
+
   depends_on macos: ">= :high_sierra"
 
   app "dintch#{version.csv.first.no_dots}/Dintch.app"
