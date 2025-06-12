1e270d656592bf0a03ab4bc461977e59b34e0154
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2025-06-12 06:25:29 -0400

    Add missing checksum values
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>

--- Casks/v/voicenotes.rb
+++ Casks/v/voicenotes.rb
@@ -2,7 +2,8 @@ cask "voicenotes" do
   arch arm: "-arm64"
 
   version "1.3.8"
-  sha256 :no_check
+  sha256 arm:   "b8a366d6c6c310cf17c11ce05d7716d1cf6439b993f12c21d35769294e992817",
+         intel: "42251838a3728cce523d6e73499cb090112ed82dcae49fd3363f65935ca57ede"
 
   url "https://github.com/brewdotcom/vn-apps-release/releases/download/#{version}/Voicenotes-#{version}#{arch}.dmg",
       verified: "github.com/brewdotcom/vn-apps-release/"
