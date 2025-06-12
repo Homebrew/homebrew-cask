fedf4610e8f775f0578142e405daab93799beecf
botantony <antonsm21@gmail.com>
2025-06-12 17:41:18 +0200

    printopia: added `no_autobump!`

--- Casks/p/printopia.rb
+++ Casks/p/printopia.rb
@@ -12,6 +12,8 @@ cask "printopia" do
     regex(%r{/Printopia_(\d+(?:\.\d+)+)\.zip}i)
   end
 
+  no_autobump! because: :requires_manual_review
+
   depends_on macos: ">= :high_sierra"
 
   app "Printopia.app"
