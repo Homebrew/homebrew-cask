7030479ce9079eed2dbbf651423e9700cb4a1a91
botantony <antonsm21@gmail.com>
2025-06-12 17:33:34 +0200

    captainplugins: added `no_autobump!`

--- Casks/c/captainplugins.rb
+++ Casks/c/captainplugins.rb
@@ -12,6 +12,8 @@ cask "captainplugins" do
     strategy :header_match
   end
 
+  no_autobump! because: :requires_manual_review
+
   depends_on macos: ">= :high_sierra"
 
   pkg "CaptainPlugins.pkg"
