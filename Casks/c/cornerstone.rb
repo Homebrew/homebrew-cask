fbf99e63c654b8604ef56273745c61bb042912fb
botantony <antonsm21@gmail.com>
2025-06-12 17:33:59 +0200

    cornerstone: added `no_autobump!`

--- Casks/c/cornerstone.rb
+++ Casks/c/cornerstone.rb
@@ -12,6 +12,8 @@ cask "cornerstone" do
     regex(/href=.*?Cornerstone[._-]v?(\d+(?:\.\d+)+)\.zip/i)
   end
 
+  no_autobump! because: :requires_manual_review
+
   depends_on macos: ">= :mojave"
 
   app "Cornerstone.app"
