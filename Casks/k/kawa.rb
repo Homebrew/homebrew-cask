f3c75d3215bf97ca9ef5a5eb69a7e8c3cdc1b745
botantony <antonsm21@gmail.com>
2025-06-12 17:38:00 +0200

    kawa: added `no_autobump!`

--- Casks/k/kawa.rb
+++ Casks/k/kawa.rb
@@ -7,6 +7,8 @@ cask "kawa" do
   desc "Alternative input source switcher"
   homepage "https://github.com/utatti/kawa"
 
+  no_autobump! because: :requires_manual_review
+
   deprecate! date: "2024-07-17", because: :unmaintained
 
   app "Kawa.app"
