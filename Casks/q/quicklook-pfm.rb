dbc24f3e6abc2622ce4c23688a46df05a9b8d35b
botantony <antonsm21@gmail.com>
2025-06-12 17:41:46 +0200

    quicklook-pfm: added `no_autobump!`

--- Casks/q/quicklook-pfm.rb
+++ Casks/q/quicklook-pfm.rb
@@ -12,6 +12,8 @@ cask "quicklook-pfm" do
     strategy :github_latest
   end
 
+  no_autobump! because: :requires_manual_review
+
   qlplugin "Quicklook-PFM.qlgenerator"
 
   # No zap stanza required
