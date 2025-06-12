c8b5365f0fdd0f8118c3ace756568721a5a72ab2
botantony <antonsm21@gmail.com>
2025-06-12 17:43:49 +0200

    thetimemachinemechanic: added `no_autobump!`

--- Casks/t/thetimemachinemechanic.rb
+++ Casks/t/thetimemachinemechanic.rb
@@ -24,6 +24,8 @@ cask "thetimemachinemechanic" do
     end
   end
 
+  no_autobump! because: :requires_manual_review
+
   depends_on macos: ">= :big_sur"
 
   app "t2m2#{version.csv.first.major}#{version.csv.first.minor}/TheTimeMachineMechanic.app"
