6dfb923cef9794cbd91943b2e33ca415198c39a9
botantony <antonsm21@gmail.com>
2025-06-12 17:39:09 +0200

    mdimagesizemdimporter: added `no_autobump!`

--- Casks/m/mdimagesizemdimporter.rb
+++ Casks/m/mdimagesizemdimporter.rb
@@ -6,6 +6,8 @@ cask "mdimagesizemdimporter" do
   name "mdImageSize"
   homepage "https://github.com/Nyx0uf/qlImageSize"
 
+  no_autobump! because: :requires_manual_review
+
   disable! date: "2024-11-11", because: :discontinued
 
   depends_on macos: ">= :high_sierra"
