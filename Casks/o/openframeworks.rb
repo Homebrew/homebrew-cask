9be8d720924c4f230ba6b073cfea2cd245e193c9
botantony <antonsm21@gmail.com>
2025-06-12 17:40:26 +0200

    openframeworks: added `no_autobump!`

--- Casks/o/openframeworks.rb
+++ Casks/o/openframeworks.rb
@@ -17,6 +17,8 @@ cask "openframeworks" do
     regex(/href=.*?of[._-]v?(\d+(?:\.\d+)+)[._-]osx[._-]release\.(?:t|zip)/i)
   end
 
+  no_autobump! because: :requires_manual_review
+
   suite "of_v#{version}_osx_release"
 
   # No zap stanza required
