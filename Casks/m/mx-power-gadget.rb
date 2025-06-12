4493344a3664467b698dee99ed4458485b77ec6d
botantony <antonsm21@gmail.com>
2025-06-12 17:39:37 +0200

    mx-power-gadget: added `no_autobump!`

--- Casks/m/mx-power-gadget.rb
+++ Casks/m/mx-power-gadget.rb
@@ -12,6 +12,8 @@ cask "mx-power-gadget" do
     strategy :sparkle, &:short_version
   end
 
+  no_autobump! because: :requires_manual_review
+
   depends_on macos: ">= :ventura"
 
   app "Mx Power Gadget.app"
