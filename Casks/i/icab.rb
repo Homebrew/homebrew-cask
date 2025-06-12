bf29a92affea3b2706c7a38869bee9a187b09110
botantony <antonsm21@gmail.com>
2025-06-12 17:37:06 +0200

    icab: added `no_autobump!`

--- Casks/i/icab.rb
+++ Casks/i/icab.rb
@@ -13,6 +13,8 @@ cask "icab" do
     regex(/iCab\sv?(\d+(?:\.\d+)+)/i)
   end
 
+  no_autobump! because: :requires_manual_review
+
   depends_on macos: ">= :high_sierra"
 
   app "iCab #{version}/iCab.app"
