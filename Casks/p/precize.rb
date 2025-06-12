9942d4532b477ea4ba1a779c9b0ca2062d038427
botantony <antonsm21@gmail.com>
2025-06-12 17:41:16 +0200

    precize: added `no_autobump!`

--- Casks/p/precize.rb
+++ Casks/p/precize.rb
@@ -23,6 +23,8 @@ cask "precize" do
     end
   end
 
+  no_autobump! because: :requires_manual_review
+
   depends_on macos: ">= :high_sierra"
 
   app "precize#{version.csv.first.no_dots}/Precize.app"
