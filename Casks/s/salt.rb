2710db34c14c2e882c39a40883b69d0b9f718462
botantony <antonsm21@gmail.com>
2025-06-12 17:42:32 +0200

    salt: added `no_autobump!`

--- Casks/s/salt.rb
+++ Casks/s/salt.rb
@@ -16,6 +16,8 @@ cask "salt" do
     regex(/salt[._-]v?(\d+(?:\.\d+)+)-py3-#{arch}\.pkg/i)
   end
 
+  no_autobump! because: :requires_manual_review
+
   pkg "salt-#{version}-py3-#{arch}.pkg"
 
   uninstall launchctl: [
