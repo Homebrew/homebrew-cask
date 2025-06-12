b186f728eb046251c9ee67630d7366cecc00dfff
botantony <antonsm21@gmail.com>
2025-06-12 17:32:33 +0200

    blackhole-2ch: added `no_autobump!`

--- Casks/b/blackhole-2ch.rb
+++ Casks/b/blackhole-2ch.rb
@@ -14,6 +14,8 @@ cask "blackhole-2ch" do
     strategy :github_latest
   end
 
+  no_autobump! because: :requires_manual_review
+
   pkg "BlackHole2ch-#{version}.pkg"
 
   uninstall quit:    "com.apple.audio.AudioMIDISetup",
