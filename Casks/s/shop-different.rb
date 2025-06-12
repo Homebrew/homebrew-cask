f67b7d2d393104a2997603f8cfac6df25b612503
botantony <antonsm21@gmail.com>
2025-06-12 17:42:45 +0200

    shop-different: added `no_autobump!`

--- Casks/s/shop-different.rb
+++ Casks/s/shop-different.rb
@@ -12,6 +12,8 @@ cask "shop-different" do
     regex(%r{href=.*?/Shop\+Different\+(\d+(?:\.\d+)+)\.dmg}i)
   end
 
+  no_autobump! because: :requires_manual_review
+
   depends_on macos: ">= :high_sierra"
 
   app "Shop Different.app"
