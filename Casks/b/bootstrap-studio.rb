6c295c2ab2a2f63f09beffbf76dc9efae49819c3
botantony <antonsm21@gmail.com>
2025-06-12 17:32:39 +0200

    bootstrap-studio: added `no_autobump!`

--- Casks/b/bootstrap-studio.rb
+++ Casks/b/bootstrap-studio.rb
@@ -15,6 +15,8 @@ cask "bootstrap-studio" do
     regex(/Version\s*v?(\d+(?:\.\d+)+)/i)
   end
 
+  no_autobump! because: :requires_manual_review
+
   auto_updates true
   depends_on macos: ">= :high_sierra"
 
