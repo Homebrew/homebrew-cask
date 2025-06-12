51b4bbd8de5e71b5f7e5476fb0b51165aa025f6e
botantony <antonsm21@gmail.com>
2025-06-12 17:43:24 +0200

    switchresx: added `no_autobump!`

--- Casks/s/switchresx.rb
+++ Casks/s/switchresx.rb
@@ -12,6 +12,8 @@ cask "switchresx" do
     regex(/latest\s+version\s+\(v?(\d+(?:\.\d+)+)\)/i)
   end
 
+  no_autobump! because: :requires_manual_review
+
   auto_updates true
 
   prefpane "SwitchResX Installer.app/Contents/Plugins/SwitchResX.prefPane"
