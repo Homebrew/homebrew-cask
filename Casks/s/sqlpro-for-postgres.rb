90a87cf1f5be89c21968dd9118ad4b85d9f3fc82
botantony <antonsm21@gmail.com>
2025-06-12 17:43:10 +0200

    sqlpro-for-postgres: added `no_autobump!`

--- Casks/s/sqlpro-for-postgres.rb
+++ Casks/s/sqlpro-for-postgres.rb
@@ -13,6 +13,8 @@ cask "sqlpro-for-postgres" do
     strategy :header_match
   end
 
+  no_autobump! because: :requires_manual_review
+
   depends_on macos: ">= :high_sierra"
 
   app "SQLPro for Postgres.app"
