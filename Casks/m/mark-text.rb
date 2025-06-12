06e180182a9a3d684e2e847780ee79dc75edb965
botantony <antonsm21@gmail.com>
2025-06-12 17:39:05 +0200

    mark-text: added `no_autobump!`

--- Casks/m/mark-text.rb
+++ Casks/m/mark-text.rb
@@ -10,6 +10,8 @@ cask "mark-text" do
   desc "Markdown editor"
   homepage "https://github.com/marktext/marktext"
 
+  no_autobump! because: :requires_manual_review
+
   auto_updates true
 
   app "MarkText.app"
