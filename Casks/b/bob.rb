723c7a6debf132c4e22c93eddb8e440771453c01
botantony <antonsm21@gmail.com>
2025-06-12 17:32:37 +0200

    bob: added `no_autobump!`

--- Casks/b/bob.rb
+++ Casks/b/bob.rb
@@ -7,6 +7,8 @@ cask "bob" do
   desc "Translation application for text, pictures, and manual input"
   homepage "https://github.com/ripperhe/Bob"
 
+  no_autobump! because: :requires_manual_review
+
   deprecate! date: "2025-04-15", because: :moved_to_mas
 
   auto_updates true
