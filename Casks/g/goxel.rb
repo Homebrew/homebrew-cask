601426b7d488e33c81e9dbefa27313ebe457c795
botantony <antonsm21@gmail.com>
2025-06-12 17:36:17 +0200

    goxel: added `no_autobump!`

--- Casks/g/goxel.rb
+++ Casks/g/goxel.rb
@@ -8,6 +8,8 @@ cask "goxel" do
   desc "Open Source Voxel Editor"
   homepage "https://goxel.xyz/"
 
+  no_autobump! because: :requires_manual_review
+
   depends_on macos: ">= :mojave"
 
   app "Goxel.app"
