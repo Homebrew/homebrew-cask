a5b1c2f83d4659a51e741cd9ce85d87e11f73c53
botantony <antonsm21@gmail.com>
2025-06-12 17:35:40 +0200

    freesurfer: added `no_autobump!`

--- Casks/f/freesurfer.rb
+++ Casks/f/freesurfer.rb
@@ -12,6 +12,8 @@ cask "freesurfer" do
     regex(/v(\d+(?:\.\d+)+)/i)
   end
 
+  no_autobump! because: :requires_manual_review
+
   suite "freesurfer"
 
   zap trash: "~/Library/Preferences/edu.harvard.mgh.nmr.FreeView.plist"
