aba5ea814e43fcd9d5c5fcfbbabe889877a5314c
junwen-k <40173716+junwen-k@users.noreply.github.com>
2025-06-12 10:47:39 +0800

    fix(`tng-digital-mini-program-studio`): fix cask description

--- Casks/t/tng-digital-mini-program-studio.rb
+++ Casks/t/tng-digital-mini-program-studio.rb
@@ -13,7 +13,7 @@ cask "tng-digital-mini-program-studio" do
   url "https://ide-release.marmot-cloud.com/storage/miniprogram-studio/common/#{version.major_minor}/MiniProgramStudio-#{version}#{arch}.dmg",
       verified: "ide-release.marmot-cloud.com/storage/miniprogram-studio/common/"
   name "TnG Digital Mini Program Studio"
-  desc "One-stop development tool to quickly build mini programs"
+  desc "IDE for building mini programs"
   homepage "https://miniprogram.tngdigital.com.my/index"
 
   livecheck do
