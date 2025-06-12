e8c2986b997f58b64d7e7ac5e8d0b7970c88ea93
botantony <antonsm21@gmail.com>
2025-06-12 17:41:48 +0200

    qutebrowser: added `no_autobump!`

--- Casks/q/qutebrowser.rb
+++ Casks/q/qutebrowser.rb
@@ -11,6 +11,8 @@ cask "qutebrowser" do
   desc "Keyboard-driven, vim-like browser based on PyQt5"
   homepage "https://www.qutebrowser.org/"
 
+  no_autobump! because: :requires_manual_review
+
   app "qutebrowser.app"
   # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
   shimscript = "#{staged_path}/qutebrowser.wrapper.sh"
