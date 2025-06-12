284829e297f289e0d94886215962eb1718c371bd
Klaus Hipp <khipp@users.noreply.github.com>
2025-06-12 21:28:12 +0200

    stratoshark 0.9.2

--- Casks/s/stratoshark.rb
+++ Casks/s/stratoshark.rb
@@ -1,19 +1,17 @@
 cask "stratoshark" do
-  arch arm: "Arm", intel: "Intel"
-  livecheck_arch = on_arch_conditional arm: "arm", intel: "x86-"
+  arch arm: "arm64", intel: "x86-64"
 
-  version "0.9.1"
-  sha256 arm:   "6b8db90d758d305fbe267f7bb347bde7e97a6a6518f9c23cf9a3a9f143437418",
-         intel: "dfc8049d1ceacd7ce9e562abd2df02a18354d4743d3421c31ca04339878b12ba"
+  version "0.9.2"
+  sha256 "137577601c51b55f553c5d90cd5e53bf285ee5a71deeb20742baa9348d46c09b"
 
-  url "https://2.na.dl.wireshark.org/osx/all-versions/Stratoshark%20#{version}%20#{arch}%2064.dmg",
+  url "https://2.na.dl.wireshark.org/osx/all-versions/Stratoshark%20#{version}.dmg",
       verified: "wireshark.org/osx/all-versions/"
   name "Stratoshark"
   desc "System calls and log messages analyzer"
   homepage "https://stratoshark.org/"
 
   livecheck do
-    url "https://www.wireshark.org/update/0/Stratoshark/0.0.0/macOS/#{livecheck_arch}64/en-US/stable.xml"
+    url "https://www.wireshark.org/update/0/Stratoshark/0.0.0/macOS/#{arch}/en-US/stable.xml"
     strategy :sparkle
   end
 
