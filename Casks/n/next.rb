9b0a3d7a23caaa45261cd2b457bb2f16d7b2838b
botantony <antonsm21@gmail.com>
2025-06-12 17:39:56 +0200

    next: added `no_autobump!`

--- Casks/n/next.rb
+++ Casks/n/next.rb
@@ -7,6 +7,8 @@ cask "next" do
   desc "Web browser"
   homepage "https://next.atlas.engineer/"
 
+  no_autobump! because: :requires_manual_review
+
   disable! date: "2024-12-16", because: :discontinued
 
   app "Next.app"
