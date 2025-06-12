664294c2bdd8edd07d65abb7730cac851cf3fa7f
botantony <antonsm21@gmail.com>
2025-06-12 17:39:58 +0200

    ninja-download-manager-ndm: added `no_autobump!`

--- Casks/n/ninja-download-manager-ndm.rb
+++ Casks/n/ninja-download-manager-ndm.rb
@@ -12,6 +12,8 @@ cask "ninja-download-manager-ndm" do
     strategy :header_match
   end
 
+  no_autobump! because: :requires_manual_review
+
   pkg "ndm_#{version}.pkg"
 
   uninstall pkgutil: "com.knockoutsoft.pkg.ndm"
