ed0bfe3d8496045cd5789a4342b37bedb177666b
botantony <antonsm21@gmail.com>
2025-06-12 17:39:20 +0200

    minishift: added `no_autobump!`

--- Casks/m/minishift.rb
+++ Casks/m/minishift.rb
@@ -6,6 +6,8 @@ cask "minishift" do
   name "Minishift"
   homepage "https://github.com/minishift/minishift"
 
+  no_autobump! because: :requires_manual_review
+
   deprecate! date: "2020-09-26", because: :unmaintained
   disable! date: "2025-06-26", because: :unmaintained
 
