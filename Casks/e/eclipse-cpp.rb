cc67a1f1bc789b24bd66045bc78c890a11421f8a
botantony <antonsm21@gmail.com>
2025-06-12 17:34:54 +0200

    eclipse-cpp: added `no_autobump!`

--- Casks/e/eclipse-cpp.rb
+++ Casks/e/eclipse-cpp.rb
@@ -14,6 +14,8 @@ cask "eclipse-cpp" do
     cask "eclipse-ide"
   end
 
+  no_autobump! because: :requires_manual_review
+
   # Renamed to avoid conflict with other Eclipse.
   app "Eclipse.app", target: "Eclipse CPP.app"
 
