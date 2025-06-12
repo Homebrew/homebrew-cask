5b061703b9498f6b23347a60858409fe8dabc95a
botantony <antonsm21@gmail.com>
2025-06-12 17:44:25 +0200

    ubports-installer: added `no_autobump!`

--- Casks/u/ubports-installer.rb
+++ Casks/u/ubports-installer.rb
@@ -14,6 +14,8 @@ cask "ubports-installer" do
     strategy :github_latest
   end
 
+  no_autobump! because: :requires_manual_review
+
   depends_on macos: ">= :high_sierra"
 
   app "ubports-installer.app"
