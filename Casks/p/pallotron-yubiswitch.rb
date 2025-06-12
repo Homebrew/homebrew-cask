ba4856404fafd89b552acee7938cbad733ff050d
botantony <antonsm21@gmail.com>
2025-06-12 17:40:50 +0200

    pallotron-yubiswitch: added `no_autobump!`

--- Casks/p/pallotron-yubiswitch.rb
+++ Casks/p/pallotron-yubiswitch.rb
@@ -12,6 +12,8 @@ cask "pallotron-yubiswitch" do
     strategy :github_latest
   end
 
+  no_autobump! because: :requires_manual_review
+
   depends_on macos: ">= :high_sierra"
 
   app "yubiswitch.app"
