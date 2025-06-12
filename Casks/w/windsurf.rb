1e59b9d15256341eec06be8c6a91f46b6ddcc36f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-12 19:15:03 +0000

    windsurf 1.10.3

--- Casks/w/windsurf.rb
+++ Casks/w/windsurf.rb
@@ -1,9 +1,9 @@
 cask "windsurf" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.10.1,1c62a60cfa8be3ea8c2f98e0b2e3440d30b508dd"
-  sha256 arm:   "01c425d3f0457c6ab3809bd8a2badabdbc7e513db3c3d162c9fc969cef5ddf70",
-         intel: "fd92ba5439b0a0039cf5dcbf2ad71f722caca90dde28186e41303a2481680fa6"
+  version "1.10.3,c1afeb8ae2b17dbdda415f9aa5dec23422c1fe47"
+  sha256 arm:   "0e51964bb4dd39c3a6263258f036b2345e738d9bbef42d66db66dd735e5b6b48",
+         intel: "5b40034da6737e26a670b7d97b78f3fe933cc3e659525dc6fa28ef88e9eb837f"
 
   url "https://windsurf-stable.codeiumdata.com/darwin-#{arch}-dmg/stable/#{version.csv.second}/Windsurf-darwin-#{arch}-#{version.csv.first}.dmg",
       verified: "windsurf-stable.codeiumdata.com/"
