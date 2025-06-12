7bd8292887102c5800703e3bcfd053b9dec94fb7
botantony <antonsm21@gmail.com>
2025-06-12 17:43:03 +0200

    soundtoys: added `no_autobump!`

--- Casks/s/soundtoys.rb
+++ Casks/s/soundtoys.rb
@@ -19,6 +19,8 @@ cask "soundtoys" do
     end
   end
 
+  no_autobump! because: :requires_manual_review
+
   depends_on macos: ">= :sierra"
 
   pkg "Install Soundtoys #{version.major_minor} Bundle.pkg"
