56fb62ecda55dcd8f2c6ce9ca06293c69032928a
botantony <antonsm21@gmail.com>
2025-06-12 17:38:29 +0200

    licensed: added `no_autobump!`

--- Casks/l/licensed.rb
+++ Casks/l/licensed.rb
@@ -12,6 +12,8 @@ cask "licensed" do
     regex(%r{>\s*Version\s*v?(\d+(?:\.\d+)+)[^<]*</}i)
   end
 
+  no_autobump! because: :requires_manual_review
+
   depends_on macos: ">= :high_sierra"
 
   app "Licensed.app"
