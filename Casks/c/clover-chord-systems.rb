1b1986f4bb668e37a944b4ff6f6baa45a5b834dd
botantony <antonsm21@gmail.com>
2025-06-12 17:33:48 +0200

    clover-chord-systems: added `no_autobump!`

--- Casks/c/clover-chord-systems.rb
+++ Casks/c/clover-chord-systems.rb
@@ -15,6 +15,8 @@ cask "clover-chord-systems" do
     end
   end
 
+  no_autobump! because: :requires_manual_review
+
   depends_on macos: ">= :monterey"
 
   pkg "CloverChordSystems#{version.major}.pkg"
