5d65d12c52a6eed1759f1fad483bac3186facaf6
botantony <antonsm21@gmail.com>
2025-06-12 17:42:48 +0200

    signet: added `no_autobump!`

--- Casks/s/signet.rb
+++ Casks/s/signet.rb
@@ -23,6 +23,8 @@ cask "signet" do
     end
   end
 
+  no_autobump! because: :requires_manual_review
+
   depends_on macos: ">= :high_sierra"
 
   app "#{token}#{version.csv.first.no_dots}/Signet.app"
