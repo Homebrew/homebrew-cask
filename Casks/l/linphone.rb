95018f47940732a454285d0da77ac10dc3ef100e
botantony <antonsm21@gmail.com>
2025-06-12 17:38:31 +0200

    linphone: added `no_autobump!`

--- Casks/l/linphone.rb
+++ Casks/l/linphone.rb
@@ -12,6 +12,8 @@ cask "linphone" do
     regex(/Linphone[._-]v?(\d+(?:\.\d+)+)[._-]mac\.dmg/i)
   end
 
+  no_autobump! because: :requires_manual_review
+
   depends_on macos: ">= :mojave"
 
   app "Linphone.app"
