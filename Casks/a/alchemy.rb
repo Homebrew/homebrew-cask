f275266556af404066be6c0bd272f427bbfde2c0
botantony <antonsm21@gmail.com>
2025-06-12 17:31:48 +0200

    alchemy: added `no_autobump!`

--- Casks/a/alchemy.rb
+++ Casks/a/alchemy.rb
@@ -7,6 +7,8 @@ cask "alchemy" do
   desc "Open drawing project"
   homepage "https://al.chemy.org/"
 
+  no_autobump! because: :requires_manual_review
+
   deprecate! date: "2024-06-10", because: :discontinued
   disable! date: "2025-06-11", because: :discontinued
 
