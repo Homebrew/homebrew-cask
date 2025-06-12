04ea34be0586ee1ccaa1d972a485f2cae20ca92a
botantony <antonsm21@gmail.com>
2025-06-12 17:43:10 +0200

    sqlpro-for-mssql: added `no_autobump!`

--- Casks/s/sqlpro-for-mssql.rb
+++ Casks/s/sqlpro-for-mssql.rb
@@ -13,6 +13,8 @@ cask "sqlpro-for-mssql" do
     strategy :header_match
   end
 
+  no_autobump! because: :requires_manual_review
+
   depends_on macos: ">= :high_sierra"
 
   app "SQLPro for MSSQL.app"
