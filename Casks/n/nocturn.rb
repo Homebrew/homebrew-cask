a15e8a4225faec8e715530f48dfbd48afd76346b
botantony <antonsm21@gmail.com>
2025-06-12 17:39:59 +0200

    nocturn: added `no_autobump!`

--- Casks/n/nocturn.rb
+++ Casks/n/nocturn.rb
@@ -7,6 +7,8 @@ cask "nocturn" do
   desc "Multi-platform Twitter client"
   homepage "https://github.com/k0kubun/Nocturn"
 
+  no_autobump! because: :requires_manual_review
+
   deprecate! date: "2024-01-11", because: :discontinued
   disable! date: "2025-01-11", because: :discontinued
 
