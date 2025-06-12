7abe9942f809b9bcf8899ebe07ac7de050b3160c
botantony <antonsm21@gmail.com>
2025-06-12 17:42:14 +0200

    roaringapps: added `no_autobump!`

--- Casks/r/roaringapps.rb
+++ Casks/r/roaringapps.rb
@@ -12,6 +12,8 @@ cask "roaringapps" do
     regex(%r{href=.*?/RoaringApps-(\d+(?:\.\d+)*)\.zip}i)
   end
 
+  no_autobump! because: :requires_manual_review
+
   depends_on macos: ">= :high_sierra"
 
   app "RoaringApps.app"
