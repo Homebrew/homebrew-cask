5dad608af0d340853f59a49121ab7825884a065a
botantony <antonsm21@gmail.com>
2025-06-12 17:38:10 +0200

    kube-forwarder: added `no_autobump!`

--- Casks/k/kube-forwarder.rb
+++ Casks/k/kube-forwarder.rb
@@ -7,6 +7,8 @@ cask "kube-forwarder" do
   name "Kube Forwarder"
   homepage "https://kube-forwarder.pixelpoint.io/"
 
+  no_autobump! because: :requires_manual_review
+
   # https://github.com/pixel-point/kube-forwarder/issues/100#issuecomment-1065111816
   deprecate! date: "2024-07-17", because: :unmaintained
 
