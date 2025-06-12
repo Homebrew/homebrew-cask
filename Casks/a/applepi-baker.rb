fbef84221e347544671eb2b671753f70c159d9f8
botantony <antonsm21@gmail.com>
2025-06-12 17:31:57 +0200

    applepi-baker: added `no_autobump!`

--- Casks/a/applepi-baker.rb
+++ Casks/a/applepi-baker.rb
@@ -12,6 +12,8 @@ cask "applepi-baker" do
     regex(/href=.*?ApplePi-Baker[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
+  no_autobump! because: :requires_manual_review
+
   app "ApplePiBaker.app"
 
   zap trash: [
