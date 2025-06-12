7a180c7511ce8359fa7915c2e846408d6f29de6c
botantony <antonsm21@gmail.com>
2025-06-12 17:32:31 +0200

    bingpaper: added `no_autobump!`

--- Casks/b/bingpaper.rb
+++ Casks/b/bingpaper.rb
@@ -7,6 +7,8 @@ cask "bingpaper" do
   desc "Use the Bing daily photo as your wallpaper"
   homepage "https://github.com/pengsrc/BingPaper"
 
+  no_autobump! because: :requires_manual_review
+
   disable! date: "2024-12-16", because: :discontinued
 
   depends_on macos: ">= :catalina"
