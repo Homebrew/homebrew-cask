3a9c6e9c87416c5cb3ec182f3b3d4539e3f4031a
botantony <antonsm21@gmail.com>
2025-06-12 17:38:01 +0200

    kext-updater: added `no_autobump!`

--- Casks/k/kext-updater.rb
+++ Casks/k/kext-updater.rb
@@ -12,6 +12,8 @@ cask "kext-updater" do
     strategy :sparkle, &:short_version
   end
 
+  no_autobump! because: :requires_manual_review
+
   depends_on macos: ">= :high_sierra"
 
   app "Kext Updater.app"
