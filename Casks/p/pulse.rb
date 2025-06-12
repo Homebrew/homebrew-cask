6b2545edf0d737f9f0c820c076245df07a4a0555
botantony <antonsm21@gmail.com>
2025-06-12 17:41:24 +0200

    pulse: added `no_autobump!`

--- Casks/p/pulse.rb
+++ Casks/p/pulse.rb
@@ -8,6 +8,8 @@ cask "pulse" do
   desc "Logger and network inspector"
   homepage "https://kean.blog/pulse/home"
 
+  no_autobump! because: :requires_manual_review
+
   disable! date: "2024-12-16", because: :discontinued
 
   app "Pulse.app"
