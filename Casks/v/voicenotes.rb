3149a492a416d600d39207cc8d3e2f29ad03a873
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2025-06-11 20:16:00 -0400

    Simplify arch interpolation in URL
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/v/voicenotes.rb
+++ Casks/v/voicenotes.rb
@@ -1,10 +1,10 @@
 cask "voicenotes" do
-  arch arm: "arm64", intel: "x86_64"
+  arch arm: "-arm64"
 
   version "1.3.8"
   sha256 :no_check
 
-  url "https://github.com/brewdotcom/vn-apps-release/releases/download/#{version}/Voicenotes-#{version}#{on_arch_conditional arm: "-arm64", intel: ""}.dmg"
+  url "https://github.com/brewdotcom/vn-apps-release/releases/download/#{version}/Voicenotes-#{version}#{arch}.dmg",
       verified: "github.com/brewdotcom/vn-apps-release/"
   name "Voicenotes"
   desc "AI-powered app for recording, transcribing, and summarizing voice notes"
