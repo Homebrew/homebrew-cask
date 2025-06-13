14cc6586a955941ef18d9c25d239078e68707b4e
Paul <paul@brzz.dev>
2025-06-13 19:25:44 +0100

    Grab latest Sparkle version

--- Casks/d/deepl.rb
+++ Casks/d/deepl.rb
@@ -1,5 +1,5 @@
 cask "deepl" do
-  version :latest
+  version "25.6.12145056"
   sha256 :no_check
 
   url "https://www.deepl.com/macos/download/DeepL.dmg"
@@ -7,11 +7,15 @@ cask "deepl" do
   desc "AI-powered translator"
   homepage "https://www.deepl.com/"
 
+  livecheck do
+    url "https://appdownload.deepl.com/macos/appcast.xml"
+    strategy :sparkle
+  end
+
+  auto_updates true
   depends_on macos: ">= :ventura"
 
   app "DeepL.app"
-
-  auto_updates true
 
   uninstall quit: [
     "com.linguee.DeepLCopyTranslator",
