76d228a68de35ecae76936f2fa73b7ca87b04c8f
Paul <paul@brzz.dev>
2025-06-13 19:07:53 +0100

    Simplify and fix DeepL brew

--- Casks/d/deepl.rb
+++ Casks/d/deepl.rb
@@ -1,61 +1,17 @@
 cask "deepl" do
-  on_catalina :or_older do
-    version "3.7.292629"
-    sha256 "efcac4988a606d9793a3bdb8e7e73dce8e3d06ed2249a4434eb54c1624b40b87"
+  version :latest
+  sha256 :no_check
 
-    url "https://www.deepl.com/macos/download/old/#{version.major_minor}/#{version.patch}/DeepL.zip"
-
-    livecheck do
-      url "https://appdownload.deepl.com/macos/"
-      regex(%r{^old/v?(\d+(?:\.\d+)+)/(\d+(?:\.\d+)*)/DeepL\.(?:zip|t)}i)
-      strategy :xml do |xml, regex|
-        xml.get_elements("//Contents/Key").map do |item|
-          match = item.text&.strip&.match(regex)
-          next if match.blank?
-
-          "#{match[1]}.#{match[2]}"
-        end
-      end
-    end
-  end
-  on_big_sur do
-    version "24.2.1798840"
-    sha256 "dacbf3dbd42eab3b1d3c4b48e0f0672146d07d94627b7ad073985fe41e9e9217"
-
-    url "https://www.deepl.com/macos/download/#{version.major_minor}/#{version.patch}/DeepL.tar.gz"
-
-    livecheck do
-      skip "Legacy version"
-    end
-  end
-  on_monterey :or_newer do
-    version "25.6.12145056"
-    sha256 "bbe9d2e5969d62aa98068508dc2dfe5bbb637acb4d71927baa91dc5af63e0f45"
-
-    url "https://www.deepl.com/macos/download/#{version.major_minor}/#{version.patch}/DeepL.tar.gz"
-
-    livecheck do
-      url "https://appdownload.deepl.com/macos/"
-      regex(%r{^v?(\d+(?:\.\d+)+)/(\d+(?:\.\d+)*)/DeepL\.t}i)
-      strategy :xml do |xml, regex|
-        xml.get_elements("//Contents/Key").map do |item|
-          match = item.text&.strip&.match(regex)
-          next if match.blank?
-
-          "#{match[1]}.#{match[2]}"
-        end
-      end
-    end
-  end
-
+  url "https://www.deepl.com/macos/download/DeepL.dmg"
   name "DeepL"
-  desc "Trains AIs to understand and translate texts"
+  desc "AI-powered translator"
   homepage "https://www.deepl.com/"
 
-  auto_updates true
-  depends_on macos: ">= :catalina"
+  depends_on macos: ">= :ventura"
 
   app "DeepL.app"
+
+  auto_updates true
 
   uninstall quit: [
     "com.linguee.DeepLCopyTranslator",
