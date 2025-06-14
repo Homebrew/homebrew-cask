7f0719fb4e3d4d9261bf7ba5a564a8e38ddafcb5
botantony <antonsm21@gmail.com>
2025-06-14 12:39:03 +0200

    font-blex-mono-nerd-font: added `no_autobump!`

--- Casks/font/font-b/font-blex-mono-nerd-font.rb
+++ Casks/font/font-b/font-blex-mono-nerd-font.rb
@@ -6,6 +6,8 @@ cask "font-blex-mono-nerd-font" do
   name "BlexMono Nerd Font (IBM Plex Mono)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
+  no_autobump! because: :bumped_by_upstream
+
   livecheck do
     url :url
     strategy :github_latest
