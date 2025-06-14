d890da0c78dd471536f7484337d894837f6a7227
botantony <antonsm21@gmail.com>
2025-06-14 13:04:41 +0200

    versatility: add `no_autobump!`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/v/versatility.rb
+++ Casks/v/versatility.rb
@@ -23,6 +23,8 @@ cask "versatility" do
     end
   end
 
+  no_autobump! because: :requires_manual_review
+
   depends_on macos: ">= :big_sur"
 
   app "versatility#{version.csv.first.no_dots}/Versatility.app"
