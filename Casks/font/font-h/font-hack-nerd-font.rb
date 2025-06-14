807f9fa0023b6003b2283bc348cd7a3d74971c71
botantony <antonsm21@gmail.com>
2025-06-14 12:39:19 +0200

    font-hack-nerd-font: added `no_autobump!`

--- Casks/font/font-h/font-hack-nerd-font.rb
+++ Casks/font/font-h/font-hack-nerd-font.rb
@@ -6,6 +6,8 @@ cask "font-hack-nerd-font" do
   name "Hack Nerd Font (Hack)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
+  no_autobump! because: :bumped_by_upstream
+
   livecheck do
     url :url
     strategy :github_latest
