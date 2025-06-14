91a19a9e83f520027ae9f3e6c39795369d7c269a
botantony <antonsm21@gmail.com>
2025-06-14 12:39:51 +0200

    font-space-mono-nerd-font: added `no_autobump!`

--- Casks/font/font-s/font-space-mono-nerd-font.rb
+++ Casks/font/font-s/font-space-mono-nerd-font.rb
@@ -6,6 +6,8 @@ cask "font-space-mono-nerd-font" do
   name "SpaceMono Nerd Font (Space Mono)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url :url
     strategy :github_latest
