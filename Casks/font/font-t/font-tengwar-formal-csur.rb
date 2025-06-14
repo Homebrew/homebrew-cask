b029c0bbff4bd81353b59b2439ba6f42dcdb21bf
botantony <antonsm21@gmail.com>
2025-06-14 12:39:52 +0200

    font-tengwar-formal-csur: added `no_autobump!`

--- Casks/font/font-t/font-tengwar-formal-csur.rb
+++ Casks/font/font-t/font-tengwar-formal-csur.rb
@@ -6,6 +6,8 @@ cask "font-tengwar-formal-csur" do
   name "Tengwar Formal CSUR"
   homepage "https://freetengwar.sourceforge.net/formal.html"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url "https://sourceforge.net/projects/freetengwar/rss?path=/TengwarFont"
     regex(%r{url=.*?/TengwarFormalCSUR[._-]?v?(\d+(?:\.\d+)*)\.zip}i)
