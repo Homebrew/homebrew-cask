9fd2ab12bba79cd00a0a3e1fa8bb5946b4f2604c
botantony <antonsm21@gmail.com>
2025-06-14 12:39:26 +0200

    font-jetbrains-mono-nerd-font: added `no_autobump!`

--- Casks/font/font-j/font-jetbrains-mono-nerd-font.rb
+++ Casks/font/font-j/font-jetbrains-mono-nerd-font.rb
@@ -6,6 +6,8 @@ cask "font-jetbrains-mono-nerd-font" do
   name "JetBrainsMono Nerd Font families (JetBrains Mono)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
+  no_autobump! because: :bumped_by_upstream
+
   livecheck do
     url :url
     strategy :github_latest
