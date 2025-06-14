7b8beed5a6d795cb32811f876a7b982ed5e5f6e7
botantony <antonsm21@gmail.com>
2025-06-14 12:39:06 +0200

    font-caskaydia-mono-nerd-font: added `no_autobump!`

--- Casks/font/font-c/font-caskaydia-mono-nerd-font.rb
+++ Casks/font/font-c/font-caskaydia-mono-nerd-font.rb
@@ -6,6 +6,8 @@ cask "font-caskaydia-mono-nerd-font" do
   name "CaskaydiaMono Nerd Font (Cascadia Mono)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url :url
     strategy :github_latest
