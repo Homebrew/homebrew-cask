6dec8e7c417586593f52484fe09149381ac803ef
botantony <antonsm21@gmail.com>
2025-06-14 12:39:33 +0200

    font-m+-nerd-font: added `no_autobump!`

--- Casks/font/font-m/font-m+-nerd-font.rb
+++ Casks/font/font-m/font-m+-nerd-font.rb
@@ -6,6 +6,8 @@ cask "font-m+-nerd-font" do
   name "M+ Nerd Font families (MPlus)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url :url
     strategy :github_latest
