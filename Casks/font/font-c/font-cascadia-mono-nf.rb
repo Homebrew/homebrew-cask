454563fe67dc6f7c8d916d6e7f4067833db57f9c
botantony <antonsm21@gmail.com>
2025-06-14 12:39:05 +0200

    font-cascadia-mono-nf: added `no_autobump!`

--- Casks/font/font-c/font-cascadia-mono-nf.rb
+++ Casks/font/font-c/font-cascadia-mono-nf.rb
@@ -6,6 +6,8 @@ cask "font-cascadia-mono-nf" do
   name "Cascadia Mono NF"
   homepage "https://github.com/microsoft/cascadia-code"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url :url
     strategy :github_latest
