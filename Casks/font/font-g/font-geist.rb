e1289416a5d1c0a2c22fbd5f5631d1236166a271
botantony <antonsm21@gmail.com>
2025-06-14 12:39:15 +0200

    font-geist: added `no_autobump!`

--- Casks/font/font-g/font-geist.rb
+++ Casks/font/font-g/font-geist.rb
@@ -7,6 +7,8 @@ cask "font-geist" do
   name "Geist Sans"
   homepage "https://vercel.com/font"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url :url
     strategy :github_latest
