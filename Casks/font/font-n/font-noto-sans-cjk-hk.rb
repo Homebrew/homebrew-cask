25b00fc0ea3a7029d4f0a3ea9d0ccf998ce4b8d9
botantony <antonsm21@gmail.com>
2025-06-14 12:39:40 +0200

    font-noto-sans-cjk-hk: added `no_autobump!`

--- Casks/font/font-n/font-noto-sans-cjk-hk.rb
+++ Casks/font/font-n/font-noto-sans-cjk-hk.rb
@@ -6,6 +6,8 @@ cask "font-noto-sans-cjk-hk" do
   name "Noto Sans CJK HK"
   homepage "https://github.com/notofonts/noto-cjk/tree/main/Sans"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     cask "font-noto-sans-cjk"
   end
