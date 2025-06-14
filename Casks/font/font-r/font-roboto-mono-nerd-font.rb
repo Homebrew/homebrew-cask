981034dcd15efbb2e60ee7f59b5c1975bf2bab92
botantony <antonsm21@gmail.com>
2025-06-14 12:39:49 +0200

    font-roboto-mono-nerd-font: added `no_autobump!`

--- Casks/font/font-r/font-roboto-mono-nerd-font.rb
+++ Casks/font/font-r/font-roboto-mono-nerd-font.rb
@@ -6,6 +6,8 @@ cask "font-roboto-mono-nerd-font" do
   name "RobotoMono Nerd Font (Roboto Mono)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url :url
     strategy :github_latest
