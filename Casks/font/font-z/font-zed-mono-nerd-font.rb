8ff22742717d55aabcd39c78d5584511d6f2b173
botantony <antonsm21@gmail.com>
2025-06-14 12:39:58 +0200

    font-zed-mono-nerd-font: added `no_autobump!`

--- Casks/font/font-z/font-zed-mono-nerd-font.rb
+++ Casks/font/font-z/font-zed-mono-nerd-font.rb
@@ -6,6 +6,8 @@ cask "font-zed-mono-nerd-font" do
   name "ZedMono Nerd Font (Zed Mono)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url :url
     strategy :github_latest
