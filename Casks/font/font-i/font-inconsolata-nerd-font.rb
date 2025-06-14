7489423aec18b281519c23c96af74c38a348d337
botantony <antonsm21@gmail.com>
2025-06-14 12:39:25 +0200

    font-inconsolata-nerd-font: added `no_autobump!`

--- Casks/font/font-i/font-inconsolata-nerd-font.rb
+++ Casks/font/font-i/font-inconsolata-nerd-font.rb
@@ -6,6 +6,8 @@ cask "font-inconsolata-nerd-font" do
   name "Inconsolata Nerd Font (Inconsolata)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
+  no_autobump! because: :bumped_by_upstream
+
   livecheck do
     url :url
     strategy :github_latest
