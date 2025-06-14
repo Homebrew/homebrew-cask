fe4fd10d3d85977e056be2441d5d681d9e40ad70
botantony <antonsm21@gmail.com>
2025-06-14 12:39:28 +0200

    font-last-resort: added `no_autobump!`

--- Casks/font/font-l/font-last-resort.rb
+++ Casks/font/font-l/font-last-resort.rb
@@ -6,6 +6,8 @@ cask "font-last-resort" do
   name "Last Resort"
   homepage "https://github.com/unicode-org/last-resort-font"
 
+  no_autobump! because: :requires_manual_review
+
   font "LastResort-Regular.ttf"
 
   # No zap stanza required
