cfe35ecb9c2ea89bbd58d3ae26809db82f63d07d
botantony <antonsm21@gmail.com>
2025-06-14 12:39:43 +0200

    font-open-dyslexic-nerd-font: added `no_autobump!`

--- Casks/font/font-o/font-open-dyslexic-nerd-font.rb
+++ Casks/font/font-o/font-open-dyslexic-nerd-font.rb
@@ -6,6 +6,8 @@ cask "font-open-dyslexic-nerd-font" do
   name "OpenDyslexic Nerd Font families (OpenDyslexic)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
+  no_autobump! because: :bumped_by_upstream
+
   livecheck do
     url :url
     strategy :github_latest
