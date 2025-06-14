03595d8ff2ca7849fddd781cfc19db020d71c700
botantony <antonsm21@gmail.com>
2025-06-14 12:39:00 +0200

    font-arimo-nerd-font: added `no_autobump!`

--- Casks/font/font-a/font-arimo-nerd-font.rb
+++ Casks/font/font-a/font-arimo-nerd-font.rb
@@ -6,6 +6,8 @@ cask "font-arimo-nerd-font" do
   name "Arimo Nerd Font (Arimo)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
+  no_autobump! because: :bumped_by_upstream
+
   livecheck do
     url :url
     strategy :github_latest
