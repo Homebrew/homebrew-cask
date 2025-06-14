9c85c7b6004ad910f0cb94c5c0b462ce8d6dcbcc
botantony <antonsm21@gmail.com>
2025-06-14 12:39:47 +0200

    font-profont-nerd-font: added `no_autobump!`

--- Casks/font/font-p/font-profont-nerd-font.rb
+++ Casks/font/font-p/font-profont-nerd-font.rb
@@ -6,6 +6,8 @@ cask "font-profont-nerd-font" do
   name "ProFont Nerd Font families (ProFont)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url :url
     strategy :github_latest
