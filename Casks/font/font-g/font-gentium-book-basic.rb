953c30443a17201fac36fa576d21609259237134
botantony <antonsm21@gmail.com>
2025-06-14 12:39:17 +0200

    font-gentium-book-basic: added `no_autobump!`

--- Casks/font/font-g/font-gentium-book-basic.rb
+++ Casks/font/font-g/font-gentium-book-basic.rb
@@ -6,6 +6,8 @@ cask "font-gentium-book-basic" do
   name "Gentium Book Basic"
   homepage "https://software.sil.org/gentium/"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url "https://software.sil.org/gentium/download/"
     regex(/Gentium\s+Basic\s+v?(\d+(?:\.\d+)+)/i)
