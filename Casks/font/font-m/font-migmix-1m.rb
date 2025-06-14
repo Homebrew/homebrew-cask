c4cad5f38c0d842f7d65c4b01d0e0fac6d64d238
botantony <antonsm21@gmail.com>
2025-06-14 12:39:35 +0200

    font-migmix-1m: added `no_autobump!`

--- Casks/font/font-m/font-migmix-1m.rb
+++ Casks/font/font-m/font-migmix-1m.rb
@@ -7,6 +7,8 @@ cask "font-migmix-1m" do
   name "MigMix 1M"
   homepage "https://itouhiro.github.io/mixfont-mplus-ipa/migmix/"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url :homepage
     regex(%r{href=.*?/download/v?(\d+(?:\.\d+)+)/migmix-1m[._-]}i)
