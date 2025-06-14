0c51b1a4f9d8645816639ff91b33502bf91ab4fe
botantony <antonsm21@gmail.com>
2025-06-14 12:39:14 +0200

    font-fira-code-nerd-font: added `no_autobump!`

--- Casks/font/font-f/font-fira-code-nerd-font.rb
+++ Casks/font/font-f/font-fira-code-nerd-font.rb
@@ -6,6 +6,8 @@ cask "font-fira-code-nerd-font" do
   name "FiraCode Nerd Font (Fira Code)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
+  no_autobump! because: :bumped_by_upstream
+
   livecheck do
     url :url
     strategy :github_latest
