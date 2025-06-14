f593b19a70155da514d31248c70143acf6215a01
botantony <antonsm21@gmail.com>
2025-06-14 12:39:14 +0200

    font-fira-mono-nerd-font: added `no_autobump!`

--- Casks/font/font-f/font-fira-mono-nerd-font.rb
+++ Casks/font/font-f/font-fira-mono-nerd-font.rb
@@ -6,6 +6,8 @@ cask "font-fira-mono-nerd-font" do
   name "FiraMono Nerd Font (Fira)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url :url
     strategy :github_latest
