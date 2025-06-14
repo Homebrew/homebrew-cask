09988506ae70f9ea5733ad653568da2756af9e48
botantony <antonsm21@gmail.com>
2025-06-14 12:39:24 +0200

    font-im-writing-nerd-font: added `no_autobump!`

--- Casks/font/font-i/font-im-writing-nerd-font.rb
+++ Casks/font/font-i/font-im-writing-nerd-font.rb
@@ -6,6 +6,8 @@ cask "font-im-writing-nerd-font" do
   name "IMWriting Nerd Font families (iA Writer)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url :url
     strategy :github_latest
