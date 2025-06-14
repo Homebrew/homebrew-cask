17598f14ed2f4cf84c7934b89f7024ca395a987e
botantony <antonsm21@gmail.com>
2025-06-14 12:39:44 +0200

    font-palemonasmufi-bold: added `no_autobump!`

--- Casks/font/font-p/font-palemonasmufi-bold.rb
+++ Casks/font/font-p/font-palemonasmufi-bold.rb
@@ -6,6 +6,8 @@ cask "font-palemonasmufi-bold" do
   name "PalemonasMUFI Italic"
   homepage "https://mufi.info/q.php?p=mufi/fonts/fontfamily/6"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url "https://mufi.info/q.php?p=mufi/fonts"
     regex(/PalemonasMUFI-Bold\s+v?(\d+(?:\.\d+)+)/i)
