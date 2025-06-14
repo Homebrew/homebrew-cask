068bd7ca99458a9938a82d4bc579edc02cc49bfc
botantony <antonsm21@gmail.com>
2025-06-14 12:39:24 +0200

    font-ibm-plex-sans-thai-looped: added `no_autobump!`

--- Casks/font/font-i/font-ibm-plex-sans-thai-looped.rb
+++ Casks/font/font-i/font-ibm-plex-sans-thai-looped.rb
@@ -6,6 +6,8 @@ cask "font-ibm-plex-sans-thai-looped" do
   name "IBM Plex Sans Thai Looped"
   homepage "https://github.com/IBM/plex"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url :url
     regex(%r{^@ibm/plex-sans-thai-looped@?(\d+(?:\.\d+)+)$}i)
