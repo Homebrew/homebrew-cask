4fb497f607b7bb98d79721efc4f28177f114d5ef
botantony <antonsm21@gmail.com>
2025-06-14 12:39:08 +0200

    font-cozette: added `no_autobump!`

--- Casks/font/font-c/font-cozette.rb
+++ Casks/font/font-c/font-cozette.rb
@@ -6,6 +6,8 @@ cask "font-cozette" do
   name "Cozette"
   homepage "https://github.com/slavfox/Cozette"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url :url
     strategy :github_latest
