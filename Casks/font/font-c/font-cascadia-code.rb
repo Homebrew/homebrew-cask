994d72e714da414237b43a73a045de6eab7320c4
botantony <antonsm21@gmail.com>
2025-06-14 12:39:04 +0200

    font-cascadia-code: added `no_autobump!`

--- Casks/font/font-c/font-cascadia-code.rb
+++ Casks/font/font-c/font-cascadia-code.rb
@@ -6,6 +6,8 @@ cask "font-cascadia-code" do
   name "Cascadia Code"
   homepage "https://github.com/microsoft/cascadia-code"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url :url
     strategy :github_latest
