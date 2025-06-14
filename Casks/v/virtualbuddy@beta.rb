8485b5c0ef3935f45d0960a791f6b767aec52e6f
botantony <antonsm21@gmail.com>
2025-06-14 13:04:51 +0200

    virtualbuddy@beta: remove `no_autobump!`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Casks/v/virtualbuddy@beta.rb
+++ Casks/v/virtualbuddy@beta.rb
@@ -13,8 +13,6 @@ cask "virtualbuddy@beta" do
     strategy :sparkle
   end
 
-  no_autobump! because: :requires_manual_review
-
   conflicts_with cask: "virtualbuddy"
   depends_on arch: :arm64
   depends_on macos: ">= :ventura"
