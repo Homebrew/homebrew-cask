605a8eef314162f81a64b952cfd778bbf8384e43
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-14 15:52:50 +0000

    font-iosevka-ss15 33.2.5

--- Casks/font/font-i/font-iosevka-ss15.rb
+++ Casks/font/font-i/font-iosevka-ss15.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss15" do
-  version "33.2.4"
-  sha256 "6ea1f792574d369d07187b216ae5ecc8fac20f39cf190a64ffb17bd5b797a84f"
+  version "33.2.5"
+  sha256 "f2c9ee61397a3964071e617ab9a950563941549fbc4f951dedf800a9ea3c1143"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/SuperTTC-IosevkaSS15-#{version}.zip"
   name "Iosevka SS15"
