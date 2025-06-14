25cf4e0135b822ed8ef769d2229cf7f5902ee67b
botantony <antonsm21@gmail.com>
2025-06-14 12:39:09 +0200

    font-d2coding-nerd-font: added `no_autobump!`

--- Casks/font/font-d/font-d2coding-nerd-font.rb
+++ Casks/font/font-d/font-d2coding-nerd-font.rb
@@ -6,6 +6,8 @@ cask "font-d2coding-nerd-font" do
   name "D2CodingLigature Nerd Font (D2Coding)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url :url
     strategy :github_latest
