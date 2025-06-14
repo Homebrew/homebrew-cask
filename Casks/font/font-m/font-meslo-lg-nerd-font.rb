5ba009c650cd6b3cf62ae5794d6450790e828e49
botantony <antonsm21@gmail.com>
2025-06-14 12:39:34 +0200

    font-meslo-lg-nerd-font: added `no_autobump!`

--- Casks/font/font-m/font-meslo-lg-nerd-font.rb
+++ Casks/font/font-m/font-meslo-lg-nerd-font.rb
@@ -6,6 +6,8 @@ cask "font-meslo-lg-nerd-font" do
   name "MesloLG Nerd Font families (Meslo LG)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
+  no_autobump! because: :bumped_by_upstream
+
   livecheck do
     url :url
     strategy :github_latest
