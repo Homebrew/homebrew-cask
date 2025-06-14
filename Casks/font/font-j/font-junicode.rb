121aa466e4198e673c4c9efa1ac60ad7b986127a
botantony <antonsm21@gmail.com>
2025-06-14 12:39:27 +0200

    font-junicode: added `no_autobump!`

--- Casks/font/font-j/font-junicode.rb
+++ Casks/font/font-j/font-junicode.rb
@@ -6,6 +6,8 @@ cask "font-junicode" do
   name "Junicode"
   homepage "https://github.com/psb1558/Junicode-font"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url :url
     strategy :github_latest
