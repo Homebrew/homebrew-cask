e0ab9b5571246c02556853e11c78c0cc01756337
botantony <antonsm21@gmail.com>
2025-06-14 12:39:21 +0200

    font-hurmit-nerd-font: added `no_autobump!`

--- Casks/font/font-h/font-hurmit-nerd-font.rb
+++ Casks/font/font-h/font-hurmit-nerd-font.rb
@@ -6,6 +6,8 @@ cask "font-hurmit-nerd-font" do
   name "Hurmit Nerd Font (Hermit)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url :url
     strategy :github_latest
