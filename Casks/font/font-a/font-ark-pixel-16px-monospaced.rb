6e08726ac708bfbb5391440b1a4b92318359c49f
botantony <antonsm21@gmail.com>
2025-06-14 12:39:01 +0200

    font-ark-pixel-16px-monospaced: added `no_autobump!`

--- Casks/font/font-a/font-ark-pixel-16px-monospaced.rb
+++ Casks/font/font-a/font-ark-pixel-16px-monospaced.rb
@@ -6,6 +6,8 @@ cask "font-ark-pixel-16px-monospaced" do
   name "Ark Pixel 16px Monospaced"
   homepage "https://github.com/TakWolf/ark-pixel-font"
 
+  no_autobump! because: :requires_manual_review
+
   font "ark-pixel-16px-monospaced-ja.otf"
   font "ark-pixel-16px-monospaced-ko.otf"
   font "ark-pixel-16px-monospaced-latin.otf"
