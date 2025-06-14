e27fd76c4c287ba3101a8581fbfd8fb38f3e2af7
botantony <antonsm21@gmail.com>
2025-06-14 12:39:26 +0200

    font-ipaexfont: added `no_autobump!`

--- Casks/font/font-i/font-ipaexfont.rb
+++ Casks/font/font-i/font-ipaexfont.rb
@@ -6,6 +6,8 @@ cask "font-ipaexfont" do
   name "IPAex Fonts"
   homepage "https://moji.or.jp/ipafont/"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url :homepage
     regex(/IPAexフォント\(\s*Ver[._-](\d+(?:\.\d+)+)\s*\)/i)
