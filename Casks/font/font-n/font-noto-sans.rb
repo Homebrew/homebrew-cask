194cdcbbc7f51d06b2218ec6c02af8f64e8cfaca
botantony <antonsm21@gmail.com>
2025-06-14 12:39:39 +0200

    font-noto-sans: added `no_autobump!`

--- Casks/font/font-n/font-noto-sans.rb
+++ Casks/font/font-n/font-noto-sans.rb
@@ -7,6 +7,8 @@ cask "font-noto-sans" do
   name "Noto Sans"
   homepage "https://notofonts.github.io/"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url :url
     regex(/^NotoSans-v?(\d+(?:\.\d+)+)$/i)
