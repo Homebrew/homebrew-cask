32f176aac1b8d68a6a925bd38e98ea357a8578d7
botantony <antonsm21@gmail.com>
2025-06-14 12:39:22 +0200

    font-ibm-plex-sans: added `no_autobump!`

--- Casks/font/font-i/font-ibm-plex-sans.rb
+++ Casks/font/font-i/font-ibm-plex-sans.rb
@@ -6,6 +6,8 @@ cask "font-ibm-plex-sans" do
   name "IBM Plex Sans"
   homepage "https://github.com/IBM/plex"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url :url
     regex(%r{^@ibm/plex-sans@?(\d+(?:\.\d+)+)$}i)
