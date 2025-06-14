50d1e8f3a7d9df7559af68a09daf1f6b122ad4ed
botantony <antonsm21@gmail.com>
2025-06-14 12:39:20 +0200

    font-han-nom-a: added `no_autobump!`

--- Casks/font/font-h/font-han-nom-a.rb
+++ Casks/font/font-h/font-han-nom-a.rb
@@ -6,6 +6,8 @@ cask "font-han-nom-a" do
   name "HAN NOM A"
   homepage "https://sourceforge.net/projects/vietunicode/files/hannom/hannom%20v2005/"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url "https://sourceforge.net/projects/vietunicode/rss?path=/hannom"
     regex(%r{url=.*?/hannom(?:\s|%20)*v?(\d+(?:\.\d+)*)/}i)
