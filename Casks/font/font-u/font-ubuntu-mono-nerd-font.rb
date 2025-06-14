9ac7966d6308580a38e1b4cd57bbd3782d4bd3f7
botantony <antonsm21@gmail.com>
2025-06-14 12:39:55 +0200

    font-ubuntu-mono-nerd-font: added `no_autobump!`

--- Casks/font/font-u/font-ubuntu-mono-nerd-font.rb
+++ Casks/font/font-u/font-ubuntu-mono-nerd-font.rb
@@ -6,6 +6,8 @@ cask "font-ubuntu-mono-nerd-font" do
   name "UbuntuMono Nerd Font (Ubuntu Mono)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
+  no_autobump! because: :requires_manual_review
+
   livecheck do
     url :url
     strategy :github_latest
