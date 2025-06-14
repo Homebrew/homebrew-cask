ef1502dd1bd8d20082f2c3be01290ee18824e7ed
botantony <antonsm21@gmail.com>
2025-06-14 12:39:01 +0200

    font-atkynson-mono-nerd-font: added `no_autobump!`

--- Casks/font/font-a/font-atkynson-mono-nerd-font.rb
+++ Casks/font/font-a/font-atkynson-mono-nerd-font.rb
@@ -6,6 +6,8 @@ cask "font-atkynson-mono-nerd-font" do
   name "AtkynsonMono Nerd Font (Atkinson Hyperlegible Mono)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
+  no_autobump! because: :bumped_by_upstream
+
   livecheck do
     url :url
     strategy :github_latest
