42a7e3b082ac8450b03d43a9c7044a4bd76afab3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-15 21:36:57 +0000

    cursor 1.1.3

--- Casks/c/cursor.rb
+++ Casks/c/cursor.rb
@@ -1,9 +1,9 @@
 cask "cursor" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.1.2,87ea1604be1f602f173c5fb67582e647fcef6c48"
-  sha256 arm:   "165dca31501ff864c3502a602c8493a7551a73e3a24ee6eb5a7d9781612eb224",
-         intel: "7f42ef3de897d20d0b7c09ef4485ce83a2ec6488bf4bb1b2f0578623e41c0766"
+  version "1.1.3,979ba33804ac150108481c14e0b5cb970bda3266"
+  sha256 arm:   "59f0ee78a61565a698159328f77caf2ef3c5d1daa1b7b8aade1c483168d5d82d",
+         intel: "76424e41352c99df978956e1f835683082a103e87634029d3bbc985a3df683c2"
 
   url "https://downloads.cursor.com/production/#{version.csv.second}/darwin/#{arch}/Cursor-darwin-#{arch}.zip"
   name "Cursor"
