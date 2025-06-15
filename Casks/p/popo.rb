3541c72d0d893d77f98b0486fe1a5cc94345f440
Klaus Hipp <khipp@users.noreply.github.com>
2025-06-15 18:48:32 +0200

    popo 4.21.0
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/p/popo.rb
+++ Casks/p/popo.rb
@@ -1,35 +1,38 @@
 cask "popo" do
-  arch arm: "arm64", intel: "x64"
+  arch arm: "arm64", intel: "x86_64"
   livecheck_arch = on_arch_conditional arm: "arm", intel: "intel"
 
   on_arm do
-    version "4.20.0,1746718264806"
-    sha256 "929e63b7c39cd55e60dd0d0ef70fcab6608bb3b1d3b3456dea1a55caee9755a0"
+    version "4.21.0,1749200894233"
+    sha256 "2204c08b2a650cdb84bd858ab8dc7593db44ab6b77db5a897a3e4e9eb0496496"
   end
   on_intel do
-    version "4.20.0,1746718212529"
-    sha256 "c08715beecb672d83b76e484185c5f55d19f7e124715f2a99c6f5f7e74154e54"
+    version "4.21.0,1749200150839"
+    sha256 "619ecc6fe943e99453aabb47f7f873c25f0cecf3ec225c9d78e16ea4984c6c36"
   end
 
-  url "https://popo.netease.com/file/popomac/#{version.csv.first}_#{arch}_prod_#{version.csv.second}.dmg"
+  url "https://popo.netease.com/file/popomac/POPO_Mac_#{arch}_prod_#{version.csv.second}.dmg"
   name "NetEase POPO"
   desc "Instant messaging platform"
   homepage "https://popo.netease.com/"
 
   livecheck do
     url "https://popo.netease.com/api/open/jsonp/check_version?device=4&callback=callback"
-    regex(/callback\((.+)\)/i)
+    regex(/POPO[._-]Mac[._-]#{arch}[._-]prod[._-](\d+)\.dmg/i)
     strategy :page_match do |page, regex|
-      url_regex = /v?(\d+(?:\.\d+)+)[._-]#{arch}[._-]prod[._-](\d+)\.dmg/i
+      json_regex = /callback\((.+)\)/i
 
-      match = page.match(regex)
+      match = page.match(json_regex)
       next if match.blank?
 
       json = Homebrew::Livecheck::Strategy::Json.parse_json(match[1])
-      match = json.dig("data", "#{livecheck_arch}Url")&.match(url_regex)
+      version = json.dig("data", "version")
+      next if version.blank?
+
+      match = json.dig("data", "#{livecheck_arch}Url")&.match(regex)
       next if match.blank?
 
-      "#{match[1]},#{match[2]}"
+      "#{version},#{match[1]}"
     end
   end
 
