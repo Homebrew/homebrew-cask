51246f4aab9cb015c243cefccd5eae91cfab3a20
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-16 21:49:06 +0000

    kate 25.04,9579

--- Casks/k/kate.rb
+++ Casks/k/kate.rb
@@ -1,9 +1,9 @@
 cask "kate" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "25.04,9540"
-  sha256 arm:   "2b176fec1dd4eda744b21eb584f9495c79599e1c92fc907d8f94858e8e40c1d9",
-         intel: "778dcd2d684234ce530c8853ba843766944a052d7ec59d5794094040a2cdfe2a"
+  version "25.04,9579"
+  sha256 arm:   "12c9a27752a57af3b8ad3b5256b00db7ae8c59f7a8db09dc0320747b3d8be6af",
+         intel: "a96b03072cc6e2480e570470c0ef4ed0cfba8e3fd17f5ac61a3d17a2dc7fac33"
 
   url "https://cdn.kde.org/ci-builds/utilities/kate/release-#{version.csv.first}/macos-#{arch}/kate-release_#{version.csv.first}-#{version.csv.second}-macos-clang-#{arch}.dmg",
       verified: "cdn.kde.org/ci-builds/utilities/kate/"
