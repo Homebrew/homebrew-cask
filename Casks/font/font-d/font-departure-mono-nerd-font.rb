a3bfdc26597d56fa7d0d78570c5f8a042321e0e9
botantony <antonsm21@gmail.com>
2025-06-17 13:34:29 +0200

    font-departure-mono-nerd-font: `brew style --fix`

--- Casks/font/font-d/font-departure-mono-nerd-font.rb
+++ Casks/font/font-d/font-departure-mono-nerd-font.rb
@@ -6,12 +6,12 @@ cask "font-departure-mono-nerd-font" do
   name "DepartureMono Nerd Font (Departure Mono)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  no_autobump! because: :bumped_by_upstream
-
   livecheck do
     url :url
     strategy :github_latest
   end
+
+  no_autobump! because: :bumped_by_upstream
 
   font "DepartureMonoNerdFont-Regular.otf"
   font "DepartureMonoNerdFontMono-Regular.otf"
