5d68d9b3b749c978681998b25e0f42e7e0abac72
botantony <antonsm21@gmail.com>
2025-06-17 13:34:50 +0200

    font-proggy-clean-tt-nerd-font: `brew style --fix`

--- Casks/font/font-p/font-proggy-clean-tt-nerd-font.rb
+++ Casks/font/font-p/font-proggy-clean-tt-nerd-font.rb
@@ -6,12 +6,12 @@ cask "font-proggy-clean-tt-nerd-font" do
   name "ProggyClean Nerd Font families (ProggyCleanTT)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  no_autobump! because: :bumped_by_upstream
-
   livecheck do
     url :url
     strategy :github_latest
   end
+
+  no_autobump! because: :bumped_by_upstream
 
   font "ProggyCleanCENerdFont-Regular.ttf"
   font "ProggyCleanCENerdFontMono-Regular.ttf"
