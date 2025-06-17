ec7bdb556ad9bbe92dc5ce9fac628740e4ecd230
botantony <antonsm21@gmail.com>
2025-06-17 13:34:39 +0200

    font-iosevka-term-nerd-font: `brew style --fix`

--- Casks/font/font-i/font-iosevka-term-nerd-font.rb
+++ Casks/font/font-i/font-iosevka-term-nerd-font.rb
@@ -6,12 +6,12 @@ cask "font-iosevka-term-nerd-font" do
   name "IosevkaTerm Nerd Font (Iosevka Term)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  no_autobump! because: :bumped_by_upstream
-
   livecheck do
     url :url
     strategy :github_latest
   end
+
+  no_autobump! because: :bumped_by_upstream
 
   font "IosevkaTermNerdFont-Bold.ttf"
   font "IosevkaTermNerdFont-BoldItalic.ttf"
