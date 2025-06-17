baeb8e871696b2009f5d04f21a3944d3f2c2055a
botantony <antonsm21@gmail.com>
2025-06-17 13:34:35 +0200

    font-ibm-plex-math: `brew style --fix`

--- Casks/font/font-i/font-ibm-plex-math.rb
+++ Casks/font/font-i/font-ibm-plex-math.rb
@@ -6,12 +6,12 @@ cask "font-ibm-plex-math" do
   name "IBM Plex Math"
   homepage "https://github.com/IBM/plex"
 
-  no_autobump! because: :requires_manual_review
-
   livecheck do
     url :url
     regex(%r{^@ibm/plex-math@?(\d+(?:\.\d+)+)$}i)
   end
+
+  no_autobump! because: :requires_manual_review
 
   font "ibm-plex-math/fonts/complete/otf/IBMPlexMath-Regular.otf"
 
