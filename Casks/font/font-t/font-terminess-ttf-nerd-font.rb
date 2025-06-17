9be0c99b30b03639b784dea9118172ec4234ee3a
botantony <antonsm21@gmail.com>
2025-06-17 13:34:52 +0200

    font-terminess-ttf-nerd-font: `brew style --fix`

--- Casks/font/font-t/font-terminess-ttf-nerd-font.rb
+++ Casks/font/font-t/font-terminess-ttf-nerd-font.rb
@@ -6,12 +6,12 @@ cask "font-terminess-ttf-nerd-font" do
   name "Terminess Nerd Font (Terminus)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  no_autobump! because: :bumped_by_upstream
-
   livecheck do
     url :url
     strategy :github_latest
   end
+
+  no_autobump! because: :bumped_by_upstream
 
   font "TerminessNerdFont-Bold.ttf"
   font "TerminessNerdFont-BoldItalic.ttf"
