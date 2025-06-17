c65bfa34b02d3e0dc2ebc2e2b2eb6fa06226f60f
botantony <antonsm21@gmail.com>
2025-06-17 13:34:37 +0200

    font-ibm-plex-sans-sc: `brew style --fix`

--- Casks/font/font-i/font-ibm-plex-sans-sc.rb
+++ Casks/font/font-i/font-ibm-plex-sans-sc.rb
@@ -6,12 +6,12 @@ cask "font-ibm-plex-sans-sc" do
   name "IBM Plex Sans SC"
   homepage "https://github.com/IBM/plex"
 
-  no_autobump! because: :requires_manual_review
-
   livecheck do
     url :url
     regex(%r{^@ibm/plex-sans-sc@?(\d+(?:\.\d+)+)$}i)
   end
+
+  no_autobump! because: :requires_manual_review
 
   font "ibm-plex-sans-sc/fonts/complete/otf/hinted/IBMPlexSansSC-Bold.otf"
   font "ibm-plex-sans-sc/fonts/complete/otf/hinted/IBMPlexSansSC-ExtraLight.otf"
