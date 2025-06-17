118e1e2f1975ac61d351b311acf4860889e6f71f
botantony <antonsm21@gmail.com>
2025-06-17 13:34:24 +0200

    font-aurulent-sans-mono-nerd-font: `brew style --fix`

--- Casks/font/font-a/font-aurulent-sans-mono-nerd-font.rb
+++ Casks/font/font-a/font-aurulent-sans-mono-nerd-font.rb
@@ -6,12 +6,12 @@ cask "font-aurulent-sans-mono-nerd-font" do
   name "AurulentSansM Nerd Font (Aurulent Sans Mono)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  no_autobump! because: :bumped_by_upstream
-
   livecheck do
     url :url
     strategy :github_latest
   end
+
+  no_autobump! because: :bumped_by_upstream
 
   font "AurulentSansMNerdFont-Regular.otf"
   font "AurulentSansMNerdFontMono-Regular.otf"
