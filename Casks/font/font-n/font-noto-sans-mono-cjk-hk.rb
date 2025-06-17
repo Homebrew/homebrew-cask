499dc9173a151560cff104d3a999f89b0d144065
botantony <antonsm21@gmail.com>
2025-06-17 13:34:46 +0200

    font-noto-sans-mono-cjk-hk: `brew style --fix`

--- Casks/font/font-n/font-noto-sans-mono-cjk-hk.rb
+++ Casks/font/font-n/font-noto-sans-mono-cjk-hk.rb
@@ -6,11 +6,11 @@ cask "font-noto-sans-mono-cjk-hk" do
   name "Noto Sans Mono CJK HK"
   homepage "https://github.com/notofonts/noto-cjk/tree/main/Sans"
 
-  no_autobump! because: :requires_manual_review
-
   livecheck do
     cask "font-noto-sans-cjk"
   end
+
+  no_autobump! because: :requires_manual_review
 
   font "NotoSansMonoCJKhk-Bold.otf"
   font "NotoSansMonoCJKhk-Regular.otf"
