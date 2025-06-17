19d83dfd4ab634df6f6d954530952955844fa2c1
botantony <antonsm21@gmail.com>
2025-06-17 13:34:32 +0200

    font-genjyuugothic-l: `brew style --fix`

--- Casks/font/font-g/font-genjyuugothic-l.rb
+++ Casks/font/font-g/font-genjyuugothic-l.rb
@@ -7,12 +7,12 @@ cask "font-genjyuugothic-l" do
   name "Gen Jyuu GothicL"
   homepage "http://jikasei.me/font/genjyuu/"
 
-  no_autobump! because: :requires_manual_review
-
   livecheck do
     url :homepage
     regex(/href=.*genjyuugothic-l[._-]v?(\d+(?:\.\d+)*)\.zip"/i)
   end
+
+  no_autobump! because: :requires_manual_review
 
   font "GenJyuuGothicL-Bold.ttf"
   font "GenJyuuGothicL-ExtraLight.ttf"
