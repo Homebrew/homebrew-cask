8bfc6ed64ef8f49b3e61930713359848aae0c6d0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-17 09:46:59 +0000

    dosbox-staging 0.82.2

--- Casks/d/dosbox-staging.rb
+++ Casks/d/dosbox-staging.rb
@@ -1,6 +1,6 @@
 cask "dosbox-staging" do
-  version "0.82.1"
-  sha256 "8e2e7b68bdac71c1ae5258e0f24fd06c0778ce62c80042d604d141a0b5c1878a"
+  version "0.82.2"
+  sha256 "3b83bb63a7314212b207ae19b82ffd15bac6ddc3a2e96d65a425343b4e9bd4a2"
 
   url "https://github.com/dosbox-staging/dosbox-staging/releases/download/v#{version}/dosbox-staging-macOS-v#{version}.dmg"
   name "DOSBox Staging"
