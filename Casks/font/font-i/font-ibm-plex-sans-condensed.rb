608a97cc326b95087ee7d834667455a4a5d69d54
botantony <antonsm21@gmail.com>
2025-06-17 13:34:36 +0200

    font-ibm-plex-sans-condensed: `brew style --fix`

--- Casks/font/font-i/font-ibm-plex-sans-condensed.rb
+++ Casks/font/font-i/font-ibm-plex-sans-condensed.rb
@@ -6,12 +6,12 @@ cask "font-ibm-plex-sans-condensed" do
   name "IBM Plex Sans Condensed"
   homepage "https://github.com/IBM/plex"
 
-  no_autobump! because: :requires_manual_review
-
   livecheck do
     url :url
     regex(%r{^@ibm/plex-sans-condensed@?(\d+(?:\.\d+)+)$}i)
   end
+
+  no_autobump! because: :requires_manual_review
 
   font "ibm-plex-sans-condensed/fonts/complete/otf/IBMPlexSansCondensed-Bold.otf"
   font "ibm-plex-sans-condensed/fonts/complete/otf/IBMPlexSansCondensed-BoldItalic.otf"
