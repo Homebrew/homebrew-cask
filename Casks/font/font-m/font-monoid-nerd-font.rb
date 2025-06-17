c81751491144f8e2b5c6dd06e3dafc65bf6be532
botantony <antonsm21@gmail.com>
2025-06-17 13:34:44 +0200

    font-monoid-nerd-font: `brew style --fix`

--- Casks/font/font-m/font-monoid-nerd-font.rb
+++ Casks/font/font-m/font-monoid-nerd-font.rb
@@ -6,12 +6,12 @@ cask "font-monoid-nerd-font" do
   name "Monoid Nerd Font (Monoid)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  no_autobump! because: :bumped_by_upstream
-
   livecheck do
     url :url
     strategy :github_latest
   end
+
+  no_autobump! because: :bumped_by_upstream
 
   font "MonoidNerdFont-Bold.ttf"
   font "MonoidNerdFont-Italic.ttf"
