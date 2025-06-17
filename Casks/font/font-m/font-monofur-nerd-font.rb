eda6cd33fcb5c0efc9df7fd896a71dacea85b412
botantony <antonsm21@gmail.com>
2025-06-17 13:34:44 +0200

    font-monofur-nerd-font: `brew style --fix`

--- Casks/font/font-m/font-monofur-nerd-font.rb
+++ Casks/font/font-m/font-monofur-nerd-font.rb
@@ -6,12 +6,12 @@ cask "font-monofur-nerd-font" do
   name "Monofur Nerd Font (Monofur)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  no_autobump! because: :bumped_by_upstream
-
   livecheck do
     url :url
     strategy :github_latest
   end
+
+  no_autobump! because: :bumped_by_upstream
 
   font "MonofurNerdFont-Bold.ttf"
   font "MonofurNerdFont-Italic.ttf"
