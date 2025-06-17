82f627cc8baeec2ea1041c6bdaf1e50b9550ad29
botantony <antonsm21@gmail.com>
2025-06-17 13:34:55 +0200

    font-zed-mono-nerd-font: `brew style --fix`

--- Casks/font/font-z/font-zed-mono-nerd-font.rb
+++ Casks/font/font-z/font-zed-mono-nerd-font.rb
@@ -6,12 +6,12 @@ cask "font-zed-mono-nerd-font" do
   name "ZedMono Nerd Font (Zed Mono)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  no_autobump! because: :bumped_by_upstream
-
   livecheck do
     url :url
     strategy :github_latest
   end
+
+  no_autobump! because: :bumped_by_upstream
 
   font "ZedMonoNerdFont-Bold.ttf"
   font "ZedMonoNerdFont-BoldItalic.ttf"
