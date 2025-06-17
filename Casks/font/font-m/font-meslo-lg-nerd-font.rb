eccceb4f080274674e5276581b7531c46b7a54d6
botantony <antonsm21@gmail.com>
2025-06-17 13:34:42 +0200

    font-meslo-lg-nerd-font: `brew style --fix`

--- Casks/font/font-m/font-meslo-lg-nerd-font.rb
+++ Casks/font/font-m/font-meslo-lg-nerd-font.rb
@@ -6,12 +6,12 @@ cask "font-meslo-lg-nerd-font" do
   name "MesloLG Nerd Font families (Meslo LG)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  no_autobump! because: :bumped_by_upstream
-
   livecheck do
     url :url
     strategy :github_latest
   end
+
+  no_autobump! because: :bumped_by_upstream
 
   font "MesloLGLDZNerdFont-Bold.ttf"
   font "MesloLGLDZNerdFont-BoldItalic.ttf"
