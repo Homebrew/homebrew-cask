542e41c87395e930835e8ad22522ab7ebfe14b47
botantony <antonsm21@gmail.com>
2025-06-17 13:34:46 +0200

    font-noto-sans-mono-cjk-sc: `brew style --fix`

--- Casks/font/font-n/font-noto-sans-mono-cjk-sc.rb
+++ Casks/font/font-n/font-noto-sans-mono-cjk-sc.rb
@@ -6,11 +6,11 @@ cask "font-noto-sans-mono-cjk-sc" do
   name "Noto Sans Mono CJK SC"
   homepage "https://github.com/notofonts/noto-cjk/tree/main/Sans"
 
-  no_autobump! because: :requires_manual_review
-
   livecheck do
     cask "font-noto-sans-cjk"
   end
+
+  no_autobump! because: :requires_manual_review
 
   font "NotoSansMonoCJKsc-Bold.otf"
   font "NotoSansMonoCJKsc-Regular.otf"
