94d2ddb9d3526fa383d9a7aa1c47210733fd59e0
botantony <antonsm21@gmail.com>
2025-06-17 13:34:45 +0200

    font-noto-sans-cjk-jp: `brew style --fix`

--- Casks/font/font-n/font-noto-sans-cjk-jp.rb
+++ Casks/font/font-n/font-noto-sans-cjk-jp.rb
@@ -6,11 +6,11 @@ cask "font-noto-sans-cjk-jp" do
   name "Noto Sans CJK JP"
   homepage "https://github.com/notofonts/noto-cjk/tree/main/Sans"
 
-  no_autobump! because: :requires_manual_review
-
   livecheck do
     cask "font-noto-sans-cjk"
   end
+
+  no_autobump! because: :requires_manual_review
 
   font "NotoSansCJKjp-Black.otf"
   font "NotoSansCJKjp-Bold.otf"
