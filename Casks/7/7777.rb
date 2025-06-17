2af74968b0260a7c981b0f04daa161b32e9aff5c
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-06-16 20:24:23 -0400

    7777 1.1.20

--- Casks/7/7777.rb
+++ Casks/7/7777.rb
@@ -1,6 +1,6 @@
 cask "7777" do
-  version "1.1.19"
-  sha256 "de628ef65c1375a09618e65fd90c0456118691d0830fd288e289497dce98849a"
+  version "1.1.20"
+  sha256 "62a04ce83b8fe0230ebc5ec1aa9e8e8a4004f533d81ad9a47a175c7da2dd8e2b"
 
   url "https://releases.port7777.com/#{version}/macos/7777"
   name "7777"
