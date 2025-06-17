c64dafd71e7c15dc53a7820a06b82b60a08d71ba
botantony <antonsm21@gmail.com>
2025-06-17 13:34:37 +0200

    font-ibm-plex-sans-thai-looped: `brew style --fix`

--- Casks/font/font-i/font-ibm-plex-sans-thai-looped.rb
+++ Casks/font/font-i/font-ibm-plex-sans-thai-looped.rb
@@ -6,12 +6,12 @@ cask "font-ibm-plex-sans-thai-looped" do
   name "IBM Plex Sans Thai Looped"
   homepage "https://github.com/IBM/plex"
 
-  no_autobump! because: :requires_manual_review
-
   livecheck do
     url :url
     regex(%r{^@ibm/plex-sans-thai-looped@?(\d+(?:\.\d+)+)$}i)
   end
+
+  no_autobump! because: :requires_manual_review
 
   font "ibm-plex-sans-thai-looped/fonts/complete/otf/IBMPlexSansThaiLooped-Bold.otf"
   font "ibm-plex-sans-thai-looped/fonts/complete/otf/IBMPlexSansThaiLooped-ExtraLight.otf"
