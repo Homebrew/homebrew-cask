7cfae2c951b039040de34b78df5d9b35eaaac941
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-18 09:54:58 +0000

    longplay 0.9.2

--- Casks/l/longplay.rb
+++ Casks/l/longplay.rb
@@ -1,6 +1,6 @@
 cask "longplay" do
-  version "0.9.1,470"
-  sha256 "1f1f20b7d7f32cf2f88046c1c544b586856bf9d0e91ef165917396edea23ad8e"
+  version "0.9.2,471"
+  sha256 "cd6c75c2b85938415760fba0fca2d0824839f013f36de8318a74c8c710969c79"
 
   url "https://download.longplay.app/mac/longplay-#{version.csv.first}-#{version.csv.second}.zip",
       verified: "download.longplay.app/"
