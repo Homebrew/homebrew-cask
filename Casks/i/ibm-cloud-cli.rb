66bd7cd1af7153ce5d5f69571728ace2c4a9fe7f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-18 21:43:28 +0000

    ibm-cloud-cli 2.34.2

--- Casks/i/ibm-cloud-cli.rb
+++ Casks/i/ibm-cloud-cli.rb
@@ -1,9 +1,9 @@
 cask "ibm-cloud-cli" do
   arch arm: "_arm64"
 
-  version "2.34.1"
-  sha256 arm:   "904d3dc1987ff9207c3d0593a6b4fd7d09acffacf9d2633cbf4920fd3c7ab28f",
-         intel: "5eea38c654f68455c9430e892471f1300f1a5998e42f2b71355f0aa7e9a9c6da"
+  version "2.34.2"
+  sha256 arm:   "0140666421cf522c4633c187cdcdfee0af9753910f5b39e422c58e298bc388ab",
+         intel: "9dca15184e0a06cd2f27c3f37d565719a78923a181582a52e6beb59498e5cd84"
 
   url "https://download.clis.cloud.ibm.com/ibm-cloud-cli/#{version}/IBM_Cloud_CLI_#{version}#{arch}.pkg"
   name "IBM Cloud CLI"
