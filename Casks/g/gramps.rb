c12e9494f2ef032fb6cc0923201ff47f5543b92a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-19 21:47:14 +0000

    gramps 6.0.3

--- Casks/g/gramps.rb
+++ Casks/g/gramps.rb
@@ -1,9 +1,9 @@
 cask "gramps" do
   arch arm: "Arm", intel: "Intel"
 
-  version "6.0.2,1"
-  sha256 arm:   "6a9f56944160f67497b1b93b8c13efc443d2ef556b083ec03d3547ef1e24364b",
-         intel: "7934e4127135ab8784270da8434354244c41b48d4ff2d113d756d579b583caa5"
+  version "6.0.3,1"
+  sha256 arm:   "cf7b1ed3e1958f3bdf49bb7e1cab65cc33cf9dd808000c85e5d6b3afe01ab327",
+         intel: "ec8e14ba1c36510bf05b25e2055921316eccc18faaa1e325e9f74ea08b7e03e0"
 
   url "https://github.com/gramps-project/gramps/releases/download/v#{version.csv.first}/Gramps-#{arch}-#{version.csv.first}-#{version.csv.second}.dmg",
       verified: "github.com/gramps-project/gramps/"
