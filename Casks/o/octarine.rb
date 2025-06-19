643d7378053ecc8b4118c87cb46d1b79d3d5d285
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-19 16:07:29 +0000

    octarine 0.26.6

--- Casks/o/octarine.rb
+++ Casks/o/octarine.rb
@@ -2,9 +2,9 @@ cask "octarine" do
   arch arm: "aarch64", intel: "x64"
   folder = on_arch_conditional arm: "arm", intel: "intel"
 
-  version "0.26.5"
-  sha256 arm:   "62818b82ab5bd0a6262093fdb2fe3e4118f270dcc65b9602568a8266a7cd7f57",
-         intel: "f6e252066d09555e22f385b15b4574968e118a8ca48e44f0eaeb93c47701541c"
+  version "0.26.6"
+  sha256 arm:   "9809c77c3844df51a06aa275c4bc9e177369a8d1f09c980128915627f46689e5",
+         intel: "fcb6c5ada3460bac36667be5e5ea472a01eac282329460f0e0a5bde1bdc45590"
 
   url "https://pub-3d35bc018fc54f11bde129e3e73e8002.r2.dev/#{version}/#{folder}/octarine_#{version}_#{arch}.dmg",
       verified: "pub-3d35bc018fc54f11bde129e3e73e8002.r2.dev/"
