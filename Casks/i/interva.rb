c2b37e864abb603b8f5e47a6e8e1be547db38ddb
Daniel <17daniel.dev@gmail.com>
2025-06-20 22:35:30 +0300

    Create interva.rb

---
+++ Casks/i/interva.rb
@@ -0,0 +1,11 @@
+cask "interva" do
+  version "1.0.1"
+  sha256 "017c0c53e3aac2e911ea9e7c889c622174e8aabb97fcfa80d231c123f1ddd7f8"
+
+  url "https://github.com/chaosweasl/interva/releases/download/v1.0.1/interva_1.0.1_aarch64.dmg"
+  name "Interva"
+  desc "Modern, customizable Pomodoro timer"
+  homepage "https://github.com/chaosweasl/interva"
+
+  app "interva.app"
+end
