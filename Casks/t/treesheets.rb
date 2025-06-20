f8ea6bc9e9889da7167a48b215a8951fd81b4405
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-20 22:08:13 +0000

    treesheets 250620.1927

--- Casks/t/treesheets.rb
+++ Casks/t/treesheets.rb
@@ -1,6 +1,6 @@
 cask "treesheets" do
-  version "250620.1841,15785589299"
-  sha256 "0e6d3fa738d1ddfa9aa367c46a9263a4bbfa9f0c0903ff88d8a90fa21f913872"
+  version "250620.1927,15786337221"
+  sha256 "8da876f34419e7a6c4a1d21570bf36929e56b1ebbba9b47463695ac9dc65aa6f"
 
   url "https://github.com/aardappel/treesheets/releases/download/#{version.csv.second}/TreeSheets-#{version.csv.first}-Darwin.dmg",
       verified: "github.com/aardappel/treesheets/"
