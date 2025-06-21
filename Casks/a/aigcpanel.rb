940254484d67ff66b7d750ae94888c969cff1e76
MZ <78841409+modstart@users.noreply.github.com>
2025-06-21 18:20:03 +0800

    Update Casks/a/aigcpanel.rb
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/a/aigcpanel.rb
+++ Casks/a/aigcpanel.rb
@@ -2,7 +2,8 @@ cask "aigcpanel" do
   arch arm: "arm64", intel: "x64"
 
   version "0.12.0"
-  sha256 :no_check
+  sha256 arm:   "cf288a6f019d4eeba0110d7bbb3ad8c852c630eb818117ac8ba716d5aece0aa7",
+         intel: "73324d009b8a79417d2146355b38c9e9ce17419c565a35924949169e98a3a453"
 
   url "https://github.com/modstart-lib/aigcpanel/releases/download/v#{version}/AigcPanelPro-#{version}-mac-#{arch}.dmg",
       verified: "github.com/modstart-lib/aigcpanel/"
