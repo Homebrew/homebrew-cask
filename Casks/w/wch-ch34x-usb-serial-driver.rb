50bd8ec0bf452d5660bdce867b069d1a7a18ae16
Kryštof Korb <krystof@korb.cz>
2025-06-21 20:16:50 +0200

    Update `wch-ch34x-usb-serial-driver` URL

--- Casks/w/wch-ch34x-usb-serial-driver.rb
+++ Casks/w/wch-ch34x-usb-serial-driver.rb
@@ -2,7 +2,7 @@ cask "wch-ch34x-usb-serial-driver" do
   version "1.9"
   sha256 :no_check
 
-  url "https://www.wch.cn/downloads/file/369.html"
+  url "https://www.wch.cn/download/file?id=369"
   name "WCH USB serial driver for CH340/CH341/CH342/CH343/CH344/CH9101/CH9102/CH9103/CH9143"
   desc "USB serial driver"
   homepage "https://www.wch.cn/download/CH34XSER_MAC_ZIP.html"
