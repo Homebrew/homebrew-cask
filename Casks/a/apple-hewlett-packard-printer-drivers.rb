576daa41a88bad0fd7add62b2a0edc867f0a13cd
Kryštof Korb <krystof@korb.cz>
2025-06-22 14:27:02 +0200

    Replace Sequoia with 15.0.0

--- Casks/a/apple-hewlett-packard-printer-drivers.rb
+++ Casks/a/apple-hewlett-packard-printer-drivers.rb
@@ -19,7 +19,7 @@ cask "apple-hewlett-packard-printer-drivers" do
     end
   end
 
-  depends_on macos: "<= sequoia"
+  depends_on macos: "<= 15.0.0"
 
   no_autobump! because: :requires_manual_review
 
