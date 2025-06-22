97b744717a3ee34562a428c97b436588567b1286
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-06-22 09:42:57 -0400

    workman: diable

--- Casks/w/workman.rb
+++ Casks/w/workman.rb
@@ -11,6 +11,7 @@ cask "workman" do
   no_autobump! because: :requires_manual_review
 
   deprecate! date: "2024-06-21", because: :unmaintained
+  disable! date: "2025-06-21", because: :unmaintained
 
   keyboard_layout "Workman-master/mac/Workman.bundle"
 
