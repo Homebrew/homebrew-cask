3815de118d97a2c10d6b9db177d216a69504ce8c
Klaus Hipp <khipp@users.noreply.github.com>
2025-06-22 20:59:14 +0200

    worldpainter 2.24.3

--- Casks/w/worldpainter.rb
+++ Casks/w/worldpainter.rb
@@ -1,6 +1,6 @@
 cask "worldpainter" do
-  version "2.24.2"
-  sha256 "73ade2776e403fe62c5407effbbaadd1caa7fd8c46c1c0444a8d73bc390e7b34"
+  version "2.24.3"
+  sha256 "e918b1cb7f991eabe169daf958ec87f666370d81f984703428b02862a3b1cf55"
 
   url "https://www.worldpainter.net/files/worldpainter_#{version}.tgz"
   name "WorldPainter"
@@ -11,8 +11,6 @@ cask "worldpainter" do
     url :homepage
     regex(%r{href=.*?/files/worldpainter[._-]v?(\d+(?:\.\d+)+)\.t}i)
   end
-
-  no_autobump! because: :requires_manual_review
 
   depends_on macos: ">= :el_capitan"
 
