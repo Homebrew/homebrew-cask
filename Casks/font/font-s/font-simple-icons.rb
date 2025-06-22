ec38c4b5ea1a8ba7c01c603a8e43fc8029e9bad7
Klaus Hipp <khipp@users.noreply.github.com>
2025-06-22 20:44:15 +0200

    font-simple-icons 15.3.0

--- Casks/font/font-s/font-simple-icons.rb
+++ Casks/font/font-s/font-simple-icons.rb
@@ -1,13 +1,11 @@
 cask "font-simple-icons" do
-  version "15.2.0"
-  sha256 "37287fca9d93da6735bff1193c59d39bd2cc3f9e151fc9daec5edafaa22574dc"
+  version "15.3.0"
+  sha256 "56f6aa42459f30d6a74254fa874b013a8d1e83e69998022e05a8bfd0a4d4311c"
 
   url "https://github.com/simple-icons/simple-icons-font/releases/download/#{version}/simple-icons-font-#{version}.zip",
       verified: "github.com/simple-icons/simple-icons-font/"
   name "Simple Icons"
   homepage "https://simpleicons.org/"
-
-  no_autobump! because: :requires_manual_review
 
   font "font/SimpleIcons-Fit.otf"
   font "font/SimpleIcons.otf"
