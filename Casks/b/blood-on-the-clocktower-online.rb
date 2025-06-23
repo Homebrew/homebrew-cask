819faa67ab666e98882625bb73a11309066b59d1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-23 18:41:49 +0000

    blood-on-the-clocktower-online 3.44.0

--- Casks/b/blood-on-the-clocktower-online.rb
+++ Casks/b/blood-on-the-clocktower-online.rb
@@ -1,9 +1,9 @@
 cask "blood-on-the-clocktower-online" do
   arch arm: "aarch64", intel: "x64"
 
-  version "3.43.0"
-  sha256 arm:   "cef5fb9986cac2a6d6b89c52fc0f988cbac7a476665481db829c5158e2fe71f7",
-         intel: "080e13f7fe2ac8d8801dd15a7eb0d7cc93bcbb8e95a1f9b3cba2fdec5da19b43"
+  version "3.44.0"
+  sha256 arm:   "321322f8c5d06ad91da5076f4caff29118e35665e742c2e66618ef8d1a240c28",
+         intel: "1298dab697769e619b4038b87dc32ff14c95693932662ecadd50393aafa3637d"
 
   url "https://github.com/ThePandemoniumInstitute/botc-release/releases/download/v#{version}/Blood.on.the.Clocktower.Online_#{version}_#{arch}.dmg",
       verified: "github.com/ThePandemoniumInstitute/botc-release/"
