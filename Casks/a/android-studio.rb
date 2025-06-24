0222abde4dc0cd09dc5d1fae5f2c4bb43f72ef94
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-24 18:42:32 +0000

    android-studio 2025.1.1.13

--- Casks/a/android-studio.rb
+++ Casks/a/android-studio.rb
@@ -1,9 +1,9 @@
 cask "android-studio" do
   arch arm: "mac_arm", intel: "mac"
 
-  version "2024.3.2.15"
-  sha256 arm:   "925760e652db05b2c53bfdda4e5e26a2ae1cfb9227e26d1d617e1ffb7b830b34",
-         intel: "956ebbe6d0f16fb661cabbb9ede6181725b8f152c211ba70e7b5d4c0203fa6dc"
+  version "2025.1.1.13"
+  sha256 arm:   "0c537a043760de612fd011e39afdecd5f2839987dd6111703ca50ebb8e9d007b",
+         intel: "0df083f1ca23c91a8e8e7bc849abd92852c15f8e376007d02dc4917e48d1ee48"
 
   url "https://redirector.gvt1.com/edgedl/android/studio/install/#{version}/android-studio-#{version}-#{arch}.dmg",
       verified: "redirector.gvt1.com/edgedl/android/studio/"
