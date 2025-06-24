1a65a6df347fa9c207e923297025a22fc371c4be
János Orcsik <4984853+janosorcsik@users.noreply.github.com>
2025-06-24 23:12:35 +0200

    Fix formating

--- Casks/s/spacesaver.rb
+++ Casks/s/spacesaver.rb
@@ -3,9 +3,9 @@ cask "spacesaver" do
   sha256 "9abe68b52052c8b90460985cec95edfe0c4a689a2c6e09f58d4eee1ae6f59235"
 
   url "https://github.com/tranhuycong/space-saver/releases/download/v#{version}/SpaceSaver-Installer.dmg",
-    verified: "github.com/tranhuycong/space-saver/"
+      verified: "github.com/tranhuycong/space-saver/"
   name "SpaceSaver"
-  desc "SpaceSaver is a macOS application designed to help you manage and optimize your workspace. It leverages macOS accessibility features to provide a seamless experience"
+  desc "Application designed to help you manage and optimize your workspace"
   homepage "https://spacesaver.congdev.com/"
 
   livecheck do
@@ -18,8 +18,8 @@ cask "spacesaver" do
   app "SpaceSaver.app"
 
   zap trash: [
-    "~/Library/Preferences/com.congth504.spacesaver.plist",
     "~/Library/Caches/com.congth504.spacesaver",
     "~/Library/HTTPStorages/com.congth504.spacesaver",
+    "~/Library/Preferences/com.congth504.spacesaver.plist",
   ]
 end
