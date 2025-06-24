1655f0d9e7ea9f57893fe99552ddaf9ecc95d2fb
mfonville <maarten.fonville@gmail.com>
2025-06-24 23:15:52 +0200

    kdeconnect skip livecheck

--- Casks/k/kdeconnect.rb
+++ Casks/k/kdeconnect.rb
@@ -5,11 +5,14 @@ cask "kdeconnect" do
   sha256 arm:   "f75de03ef019f31849695ce787920b64a7e69c64c26725c7b827051a2c2a0476",
          intel: "a05bc5eb2fea06199eac3e3b7a1e283407b4c26fa463bcc4fb120bf24a2f274e"
 
-  url "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/release-#{version.major_minor}/macos-#{arch}/kdeconnect-kde-release_#{version.major_minor}-#{version.csv.second}-macos-clang-#{arch}.dmg",
-      verified: "cdn.kde.org/ci-builds/network/kdeconnect-kde/"
+  url "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/release-#{version.major_minor}/macos-#{arch}/kdeconnect-kde-release_#{version.major_minor}-#{version.csv.second}-macos-clang-#{arch}.dmg"
   name "KDE Connect"
   desc "Enabling communication between all your devices"
   homepage "https://kdeconnect.kde.org/"
+
+  livecheck do
+    skip "No version information available"
+  end
 
   app "kdeconnect-indicator.app"
 
