f760eba1c76d051947e0f84f6d0bd7bf4e818e73
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-25 21:51:32 +0000

    okta-advanced-server-access 1.91.0

--- Casks/o/okta-advanced-server-access.rb
+++ Casks/o/okta-advanced-server-access.rb
@@ -1,6 +1,6 @@
 cask "okta-advanced-server-access" do
-  version "1.90.0"
-  sha256 "243473134f470631b8a921f33fd76f7938091740734992f3d0f16cb4c8953be0"
+  version "1.91.0"
+  sha256 "c86f7df8e1322e7186f30d7cb81af42d36b908fc71facf588ed6b207722189a7"
 
   url "https://dist.scaleft.com/repos/macos/stable/all/macos-client/v#{version}/ScaleFT-#{version}.pkg",
       verified: "dist.scaleft.com/repos/macos/stable/all/macos-client/"
