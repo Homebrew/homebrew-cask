bd5026e1edf0ac3df015873d359e4f926a8b6558
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-25 10:16:45 +0000

    rar 7.12

--- Casks/r/rar.rb
+++ Casks/r/rar.rb
@@ -1,9 +1,9 @@
 cask "rar" do
   arch arm: "arm", intel: "x64"
 
-  version "7.11"
-  sha256 arm:   "58988a9a0331dc03c8903bb97a446f509075ffe4a1e20a339a3022aeb447a680",
-         intel: "6af686124587e4710724b35c4b933f361f59bf7f236884bd91fdaa292b1cf2fe"
+  version "7.12"
+  sha256 arm:   "95078e0f59ffd05ebe665a557e9d4d1c073156a2777d9167f6c420ee448a83c5",
+         intel: "5b3a79233a5ce2eb0d95d0446f739909e3720724d325e828c5b0c3f0736708fa"
 
   url "https://www.rarlab.com/rar/rarmacos-#{arch}-#{version.no_dots}.tar.gz"
   name "RAR Archiver"
