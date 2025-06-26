de716364f23a53b826e95dbdb00ac195eea7ca0f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-26 18:54:16 +0000

    discord@ptb 0.0.180

--- Casks/d/discord@ptb.rb
+++ Casks/d/discord@ptb.rb
@@ -1,6 +1,6 @@
 cask "discord@ptb" do
-  version "0.0.179"
-  sha256 "b4613b1c07162e91a5bf9a173bb34410b751b4d7db865a5078d739cc1edb6b50"
+  version "0.0.180"
+  sha256 "50c26817bfcb9b2f1e71dfb185235ba44b8cf84081d7fce241490c377aa75a45"
 
   url "https://dl-ptb.discordapp.net/apps/osx/#{version}/DiscordPTB.dmg",
       verified: "dl-ptb.discordapp.net/apps/osx/"
