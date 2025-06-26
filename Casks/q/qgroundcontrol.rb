dd239ee9f9f875a314942de2b6c4dc3098867ad1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-26 01:07:12 +0000

    qgroundcontrol 5.0.4

--- Casks/q/qgroundcontrol.rb
+++ Casks/q/qgroundcontrol.rb
@@ -1,6 +1,6 @@
 cask "qgroundcontrol" do
-  version "5.0.3"
-  sha256 "8e8678b08d677fd3ad4ddb2314dab382089e2b8119443931b83bfa2cad84d4b0"
+  version "5.0.4"
+  sha256 "88210b714dadcd888341f8751ad5ff3714bd342db627b70cfdf8dff9cac578a7"
 
   url "https://github.com/mavlink/qgroundcontrol/releases/download/v#{version}/QGroundControl.dmg",
       verified: "github.com/mavlink/qgroundcontrol/"
