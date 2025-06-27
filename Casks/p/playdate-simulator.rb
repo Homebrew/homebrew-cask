aedad146cb0a9a120ad5d04c27f5e1cd6811d689
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-27 01:06:15 +0000

    playdate-simulator 2.7.5

--- Casks/p/playdate-simulator.rb
+++ Casks/p/playdate-simulator.rb
@@ -1,6 +1,6 @@
 cask "playdate-simulator" do
-  version "2.7.4"
-  sha256 "65835e1c2a2457072c6158480537887b09b8d8f2775497e874e4182eede98f77"
+  version "2.7.5"
+  sha256 "de420a3f6e93d1ba53054142bd78b85a3c29a73a67947912c4d0187f0285eeac"
 
   url "https://download-cdn.panic.com/playdate_sdk/PlaydateSDK-#{version}.zip",
       verified: "download-cdn.panic.com/playdate_sdk/"
