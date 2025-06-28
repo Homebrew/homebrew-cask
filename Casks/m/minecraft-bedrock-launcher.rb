ac515e30af48c0598543139f315b66d97c71d146
Ruaneri Ferreira Portela <91219330+Ruaneri-Portela@users.noreply.github.com>
2025-06-28 18:39:32 -0300

    Update minecraft-bedrock-launcher.rb

--- Casks/m/minecraft-bedrock-launcher.rb
+++ Casks/m/minecraft-bedrock-launcher.rb
@@ -2,7 +2,7 @@ cask "minecraft-bedrock-launcher" do
   version "1.4.0-511"
   sha256 "0a153f435a026b0792e938eeb057484c1315dcde8a88d474ab815e9131eefb65"
 
-  url "https://github.com/minecraft-linux/macos-builder/releases/download/v1.4.0-511/Minecraft_Bedrock_Launcher_v1.4.0-macOS-x86_64-0.2.511_macOS_10.13.0.dmg",
+  url "https://github.com/minecraft-linux/macos-builder/releases/download/v#{version}/Minecraft_Bedrock_Launcher_v1.4.0-macOS-x86_64-0.2.511_macOS_10.13.0.dmg",
       verified: "github.com/minecraft-linux/macos-builder/"
   name "Minecraft Bedrock Launcher"
   desc "Launcher for Minecraft Bedrock Edition on macOS"
@@ -13,16 +13,15 @@ cask "minecraft-bedrock-launcher" do
     strategy :github_latest
   end
 
-  no_autobump! because: :requires_manual_review
-
   app "Minecraft Bedrock Launcher.app"
 
   depends_on macos: ">= :high_sierra"
 
-  uninstall quit: "mcpelauncher-ui-qt"
-
   zap trash: [
-    "~/Library/Application Support/minecraft"
+    "~/Library/Application Support/minecraft",
+    "~/Library/Application Support/mcpelauncher",
+    "~/Library/Preferences/io.mrarm.Minecraft Linux Launcher UI.plist",
+    "~/Library/Preferences/io.mrarm.Minecraft Linux Launcher UI.helper.plist",
+    "~/Library/Saved Application State/io.mrarm.Minecraft Linux Launcher UI.savedState"
   ]
-
 end
