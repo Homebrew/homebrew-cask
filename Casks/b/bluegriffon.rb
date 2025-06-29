561cb719ad49f3475e335eafbfaf60c575ee3eb5
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-06-29 16:31:23 -0400

    bluegriffon: disable

--- Casks/b/bluegriffon.rb
+++ Casks/b/bluegriffon.rb
@@ -10,6 +10,7 @@ cask "bluegriffon" do
   no_autobump! because: :requires_manual_review
 
   deprecate! date: "2024-06-28", because: :discontinued
+  disable! date: "2025-06-29", because: :discontinued
 
   app "BlueGriffon.app"
 
