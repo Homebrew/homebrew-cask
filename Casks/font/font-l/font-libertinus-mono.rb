9dce216a6df0c6f89b6329fa871c55bbddf3d88a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-29 00:17:57 +0000

    font-libertinus-mono: update
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

--- Casks/font/font-l/font-libertinus-mono.rb
+++ Casks/font/font-l/font-libertinus-mono.rb
@@ -2,9 +2,10 @@ cask "font-libertinus-mono" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/libertinusmono/LibertinusMono-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/libertinusmono/LibertinusMono-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Libertinus Mono"
-  homepage "https://github.com/googlefonts/libertinus"
+  homepage "https://fonts.google.com/specimen/Libertinus+Mono"
 
   font "LibertinusMono-Regular.ttf"
 
