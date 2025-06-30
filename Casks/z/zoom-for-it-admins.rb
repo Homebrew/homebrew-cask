7e28666c5bc090bf099d39afb4734d2c81d59080
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-30 10:14:20 +0000

    zoom-for-it-admins 6.5.3.58803

--- Casks/z/zoom-for-it-admins.rb
+++ Casks/z/zoom-for-it-admins.rb
@@ -1,6 +1,6 @@
 cask "zoom-for-it-admins" do
-  version "6.5.1.58208"
-  sha256 "d7cf25bf190330731ffd2e63aa380ce265207929a3c4eca427aca72b4224eb41"
+  version "6.5.3.58803"
+  sha256 "d7e55fdd2a9436f8aaf8c77c9089b847c741f5611ac6c8d1952576cae4dfc2a5"
 
   url "https://cdn.zoom.us/prod/#{version}/ZoomInstallerIT.pkg"
   name "Zoom for IT Admins"
