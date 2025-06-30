c0e4fef355ebd1d0eefa0d6e64ab27dcdf5c942a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-30 15:37:46 +0000

    burp-suite 2025.5.6

--- Casks/b/burp-suite.rb
+++ Casks/b/burp-suite.rb
@@ -1,9 +1,9 @@
 cask "burp-suite" do
   arch arm: "MacOsArm64", intel: "MacOsx"
 
-  version "2025.5.5"
-  sha256 arm:   "25b71c1c314f76bd48e4a5bc684013ce2ff26015015be486757250d208fec11d",
-         intel: "853845e21804b5834ce48a4812c633097ed59cabd5b155c8164a8e632741e831"
+  version "2025.5.6"
+  sha256 arm:   "1e696413118f6a04b47c5e0dfa7534a07d99f1e688f6e0fafef3c97949132259",
+         intel: "18eaa9230640f32c1877931854b3aa1f508bf5cb6a184d6939fd082bb798d9c1"
 
   url "https://portswigger-cdn.net/burp/releases/download?product=community&version=#{version}&type=#{arch}",
       verified: "portswigger-cdn.net/burp/releases/"
