7e9a7c3b6e37c3d85286838e0c984c3b851ed85b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-30 12:54:29 +0000

    katalon-studio 10.2.3

--- Casks/k/katalon-studio.rb
+++ Casks/k/katalon-studio.rb
@@ -1,9 +1,9 @@
 cask "katalon-studio" do
   arch arm: "%20Arm64"
 
-  version "10.2.2"
-  sha256 arm:   "8417a3b2a643854e83428a9f38d3766aac87eed2e986d5191a0880b16b009762",
-         intel: "10b3b58c49158098f22a72d01fcad01a1b488d55e0f177c561d9fc036328a9c1"
+  version "10.2.3"
+  sha256 arm:   "301ec470cc20f048ce5182ccb0f53143471f9fd479510cac12762fa6ddff04d1",
+         intel: "b02db5ef720af4dc3d9a0423e7f7f421d4078691327c69816f0d3595a36a9c41"
 
   url "https://download.katalon.com/free/#{version}/release/Katalon%20Studio#{arch}.dmg"
   name "Katalon Studio"
