c0db13873fa41f51f64959e0c9b9ce5de982469c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-01 18:57:15 +0000

    lw-scanner 0.27.8

--- Casks/l/lw-scanner.rb
+++ Casks/l/lw-scanner.rb
@@ -1,9 +1,9 @@
 cask "lw-scanner" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.27.7"
-  sha256 arm:   "c98129c6253adab2498586bc22959336215df75515c12a50f55ceb98313e4b27",
-         intel: "9613d0778433dddb3210217c03ed0b073906514e2a1a591c3392a8f20807a5d7"
+  version "0.27.8"
+  sha256 arm:   "cc59ef0a957fa06cc67f4bba7df3dcd31fb8390291aa8899310fe5a2094217d8",
+         intel: "3726ec23121655360f6d5c036dfeeb55a4bfa4c720ebd6021591c45509883fb8"
 
   url "https://github.com/lacework/lacework-vulnerability-scanner/releases/download/v#{version}/lw-scanner-darwin-#{arch}.zip",
       verified: "github.com/lacework/lacework-vulnerability-scanner/"
