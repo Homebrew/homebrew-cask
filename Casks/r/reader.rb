c7983f76ce61defad45516b59fae189dea6ce9d6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-01 10:01:12 +0000

    reader 0.1.1749

--- Casks/r/reader.rb
+++ Casks/r/reader.rb
@@ -1,6 +1,6 @@
 cask "reader" do
-  version "0.1.1748"
-  sha256 "99e7e6d4a0c63b9b266655e824adde1d4e163940b2219aa24a755411ffd6cb71"
+  version "0.1.1749"
+  sha256 "5577a0ea0ccce37ce776090a8147ef9441b11263c6e9278d86ce9d303519c421"
 
   url "https://github.com/readwiseio/reader-desktop-releases/releases/download/reader-desktop-v#{version}/Reader_#{version}_universal.dmg",
       verified: "github.com/readwiseio/reader-desktop-releases/"
