473dca3c33a8dcad14590bfe83b9b218b127a587
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-06-30 21:33:44 -0400

    nessus 10.9.0

--- Casks/n/nessus.rb
+++ Casks/n/nessus.rb
@@ -1,6 +1,6 @@
 cask "nessus" do
-  version "10.8.4"
-  sha256 "ba5e4148a60052117b9a53a83936c4c54d9b1e2395866c4f09ba7f370314845d"
+  version "10.9.0"
+  sha256 "6c9b8c7041ca7bce64f323029abcbc26a430719175b2479c5486bd4dcfb25841"
 
   url "https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-#{version}.dmg"
   name "Tenable Nessus"
@@ -11,8 +11,6 @@ cask "nessus" do
     url "https://www.tenable.com/downloads/nessus"
     regex(/Nessus[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
-
-  no_autobump! because: :requires_manual_review
 
   pkg ".Nessus.pkg"
   binary "/Library/Nessus/run/bin/nasl"
