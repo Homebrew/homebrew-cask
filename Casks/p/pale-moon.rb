ecd7818065b56b93fafd1e5e1987eb9b715eb125
botantony <antonsm21@gmail.com>
2025-07-01 17:55:18 +0200

    pale-moon 33.8.0

--- Casks/p/pale-moon.rb
+++ Casks/p/pale-moon.rb
@@ -1,6 +1,6 @@
 cask "pale-moon" do
-  version "33.7.2"
-  sha256 "07be329da8809328d95d96270e22d22585362f97cf73eced726786ee8df08169"
+  version "33.8.0"
+  sha256 "4b277454c6082ea658511170eacd360bd91c35ca130f40bfaabd55e3b2298a40"
 
   url "https://rm-us.palemoon.org/release/palemoon-#{version}.arm64.dmg"
   name "Pale Moon"
@@ -11,8 +11,6 @@ cask "pale-moon" do
     url "https://repo.palemoon.org/MoonchildProductions/Pale-Moon.git"
     regex(/^v?(\d+(?:\.\d+)+)[._-]Release$/i)
   end
-
-  no_autobump! because: :requires_manual_review
 
   depends_on macos: ">= :big_sur"
   depends_on arch: :arm64
