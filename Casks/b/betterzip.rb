670e8265674cd4088454382f83bea6ad27e20f7e
Qi Zhang <singularitti@outlook.com>
2025-07-02 12:28:42 -0500

    betterzip Fix `zap` stanza

--- Casks/b/betterzip.rb
+++ Casks/b/betterzip.rb
@@ -27,6 +27,9 @@ cask "betterzip" do
     "~/Library/Application Scripts/com.macitbetter.betterzip.findersyncextension",
     "~/Library/Application Scripts/com.macitbetter.betterzip.Quick-Look-Extension",
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.macitbetter.betterzip.sfl*",
+    "~/Library/Application Support/com.macitbetter.betterzip",
+    "~/Library/Caches/com.apple.helpd/Generated/com.macitbetter.betterzip.help*",
+    "~/Library/Caches/com.macitbetter.betterzip",
     "~/Library/Containers/com.macitbetter.betterzip.Compress-with-BetterZip",
     "~/Library/Containers/com.macitbetter.betterzip.findersyncextension",
     "~/Library/Containers/com.macitbetter.betterzip.Quick-Look-Extension",
@@ -34,5 +37,6 @@ cask "betterzip" do
     "~/Library/Group Containers/79RR9LPM2N.group.com.macitbetter.betterzip-setapp",
     "~/Library/HTTPStorages/com.macitbetter.betterzip",
     "~/Library/Preferences/com.macitbetter.betterzip.plist",
+    "~/Library/Saved Application State/com.macitbetter.betterzip.savedState",
   ]
 end
