af4aef3b60f1881b563b8435613ee71c5cfbceb1
botantony <antonsm21@gmail.com>
2025-07-02 04:01:02 +0200

    tev 2.2

--- Casks/t/tev.rb
+++ Casks/t/tev.rb
@@ -1,9 +1,9 @@
 cask "tev" do
   arch intel: "-intel"
 
-  version "2.1"
-  sha256 arm:   "1c2f4f6fdff28fe15f06256c74cf5177cb5eba947c1849eddce36f31901cb04f",
-         intel: "16d7553359bd8cd42b1d2dc0aeb922a2ea8cce8e1521bce80b6522fa49f88c15"
+  version "2.2"
+  sha256 arm:   "2068cb3d5516e325d459bfa1a6816d7a9245f736be7f23eaf01bacdb8d707b6d",
+         intel: "5ad9a44afd6e8d1dd89db347f44e6425aa86fbab036aa0a2bfb51535c7d11228"
 
   url "https://github.com/Tom94/tev/releases/download/v#{version}/tev#{arch}.dmg"
   name "tev"
