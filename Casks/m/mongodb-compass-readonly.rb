7b627b9c578ef2bb262f89cfad39eb398a314431
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-02 16:02:24 +0000

    mongodb-compass-readonly 1.46.5

--- Casks/m/mongodb-compass-readonly.rb
+++ Casks/m/mongodb-compass-readonly.rb
@@ -1,9 +1,9 @@
 cask "mongodb-compass-readonly" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.46.4"
-  sha256 arm:   "0aba898acc1ac0ac8f63be39cbe67b6f15c47abd2b372deb47ee795a1bab1ca5",
-         intel: "f7a1e0cf677885d22833e4b25031c20f4bafaad3e830856a77324f759e5fbe3c"
+  version "1.46.5"
+  sha256 arm:   "eb8f8d405964e26fd3bdd6672c412a483c0d722cca5075676c6527a0e562d47d",
+         intel: "4230eb7397d34a0d93fac528c6f845e634a115574f587ffc34792e52bf369d85"
 
   url "https://downloads.mongodb.com/compass/mongodb-compass-readonly-#{version}-darwin-#{arch}.dmg"
   name "MongoDB Compass Readonly"
