7904317987bb95de05bab9d5d5d51cbda93a1510
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-03 13:30:18 +0000

    wavebox 10.138.8.2

--- Casks/w/wavebox.rb
+++ Casks/w/wavebox.rb
@@ -1,9 +1,9 @@
 cask "wavebox" do
   arch arm: "arm64"
 
-  version "10.138.7.2"
-  sha256 arm:   "f3c81a2ae8b09ce920f5159638610bb5a6daf1343507fd2fd09ef63a64a526b4",
-         intel: "3ba27535296c39f2ff9425fff77c467306946700c7618f877d4d6412a525e653"
+  version "10.138.8.2"
+  sha256 arm:   "c29f1a7aa3ad9338e8c0d5c606a8fc607493bbcdbb5e5f2d2198064674590890",
+         intel: "ecbcc8b82d9b0a7a429c3447a6148344c366e2a0b0f70cbe1fe1e5a2c5516a0c"
 
   url "https://download.wavebox.app/stable/mac#{arch}/Wavebox_#{version}.zip",
       verified: "download.wavebox.app/"
