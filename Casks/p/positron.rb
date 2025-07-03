8d8f03996b5cec8af5380f8e1a400d36d9237abe
james.balamuta@gmail.com <james.balamuta@gmail.com>
2025-07-03 09:24:51 -0700

    positron: update URL to use releases instead of prereleases
    - Change URL from prereleases to releases endpoint
    - Update filename format to include -universal suffix

--- Casks/p/positron.rb
+++ Casks/p/positron.rb
@@ -1,14 +1,14 @@
 cask "positron" do
-  version "2025.06.0-167"
-  sha256 "813178aba2feec6581a78d11b52cb2830d031c3729e3735cf67a567122a1f1a5"
+  version "2025.07.0-204"
+  sha256 "7f5110c3a7ca1f8a60546efe61c2cfbfa15ac09d93379d3b8582c3d299fe3e53"
 
-  url "https://cdn.posit.co/positron/prereleases/mac/universal/Positron-#{version}.dmg"
+  url "https://cdn.posit.co/positron/releases/mac/universal/Positron-#{version}-universal.dmg"
   name "Positron"
   desc "Data science IDE"
   homepage "https://positron.posit.co/"
 
   livecheck do
-    url "https://cdn.posit.co/positron/prereleases/mac/universal/releases.json"
+    url "https://cdn.posit.co/positron/releases/mac/universal/releases.json"
     strategy :json do |json|
       json["version"]
     end
