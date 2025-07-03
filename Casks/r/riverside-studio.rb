c525689b5e1407cc3d88b1bade38fae41b1dce18
John Benninghoff <jbenninghoff@mac.com>
2025-07-03 12:24:35 -0500

    riverside-studio 1.10.4 (new cask)

---
+++ Casks/r/riverside-studio.rb
@@ -0,0 +1,24 @@
+cask "riverside-studio" do
+  version "1.10.4"
+  sha256 :no_check
+
+  url "https://assets.riverside.fm/mac-desktop-app/current-release/RiversideStudio.dmg"
+  name "Riverside Studio"
+  desc "Podcast and video recorder"
+  homepage "https://riverside.fm/"
+
+  livecheck do
+    url "https://assets.riverside.fm/mac-desktop-app/current-release/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+  depends_on macos: ">= :ventura"
+
+  app "Riverside Studio.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/RVS-Riverside.fm-Mac",
+    "~/Library/Containers/RVS-Riverside.fm-Mac",
+  ]
+end
