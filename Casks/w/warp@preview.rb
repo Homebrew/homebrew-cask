6ccf63c391221e9290785376e09f4022f85b3bac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-03 22:13:23 +0000

    warp@preview 0.2025.07.02.08.36.preview_02

--- Casks/w/warp@preview.rb
+++ Casks/w/warp@preview.rb
@@ -1,6 +1,6 @@
 cask "warp@preview" do
-  version "0.2025.06.25.08.12.preview_02"
-  sha256 "c3cb4be6ceeeb584443b8ac9cf1f333401365b28a242800c343decc5bc526eb9"
+  version "0.2025.07.02.08.36.preview_02"
+  sha256 "e6aa9f3d71ad7d6a97a6ff55e9191780642c138a3f3a3c49a8e6a3a80ed631ce"
 
   url "https://releases.warp.dev/preview/v#{version}/WarpPreview.dmg"
   name "Warp Preview"
