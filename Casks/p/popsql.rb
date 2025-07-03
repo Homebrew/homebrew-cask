a10c68b8bc1c9a05d38716f6828a55133c72109a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-03 21:59:13 +0000

    popsql 1.0.135

--- Casks/p/popsql.rb
+++ Casks/p/popsql.rb
@@ -1,9 +1,9 @@
 cask "popsql" do
   arch arm: "-arm64"
 
-  version "1.0.134"
-  sha256 arm:   "3d4fba78102359cca2f5d230f3dfa69c5c4522d1444b3459d0c1f6feff10915c",
-         intel: "0f87dbd318401a4b181c77e21a23aeb807184c837152d20db66d3770f9c0c8b9"
+  version "1.0.135"
+  sha256 arm:   "2a1e9af94cb191b80771c8bbb1952a85b628402fd6e783d91b2231d48236ae00",
+         intel: "d6ab634d6d6af2f2e3a6715b31498b506fe5610ffd8e294cd28d47f3bf761782"
 
   url "https://popsql-releases.s3.amazonaws.com/mac/PopSQL-#{version}#{arch}.dmg",
       verified: "popsql-releases.s3.amazonaws.com/"
