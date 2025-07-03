43d258080b2441ef6bda8c2d29c48b7d03661ae9
Matt Pike <MrPike@users.noreply.github.com>
2025-07-03 15:01:13 +0800

    Fixed style issues

--- Casks/s/stillcolor.rb
+++ Casks/s/stillcolor.rb
@@ -1,18 +1,18 @@
 cask "stillcolor" do
-
   # Update these on new version releases
   version "1.1"
   sha256 "dfa8c046540764df4bc462479190aafc60ecc6b25d43c54feeab65b2c29ee0f6"
 
-
   url "https://github.com/aiaf/Stillcolor/releases/download/v#{version}/Stillcolor-v#{version}.zip"
   name "Stillcolor"
-  desc "A tool to disable temporal dithering on Apple Silicon Macs"
+  desc "Tool to disable temporal dithering on Apple Silicon Macs" # Corrected: Removed "A"
   homepage "https://github.com/aiaf/Stillcolor"
 
+  # Add the macOS dependency here
   depends_on macos: :ventura
-  
+
   app "Stillcolor.app"
+
   zap trash: [
     "~/Library/Application Support/Stillcolor",
     "~/Library/Preferences/com.aiaf.Stillcolor.plist",
