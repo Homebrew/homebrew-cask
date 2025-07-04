dc20668ca6183203cbd8675fee9777b3b90fead5
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-07-03 20:18:33 -0400

    bfxr: disable

--- Casks/b/bfxr.rb
+++ Casks/b/bfxr.rb
@@ -10,6 +10,7 @@ cask "bfxr" do
   no_autobump! because: :requires_manual_review
 
   deprecate! date: "2024-07-04", because: :discontinued
+  disable! date: "2025-07-04", because: :discontinued
 
   app "Bfxr.app"
 
