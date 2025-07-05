f4cbc8802f77ab555246a78c0751cf381f8aee8f
Klaus Hipp <khipp@users.noreply.github.com>
2025-07-05 11:51:43 +0200

    nao 0.3.5

--- Casks/n/nao.rb
+++ Casks/n/nao.rb
@@ -1,9 +1,9 @@
 cask "nao" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.3.3"
-  sha256 arm:   "511bd7cc4d4dd1a31281f6a464664724cca1a87993f71500d4441ffe25aeaad7",
-         intel: "6994b72baf82153a379d91e4b15117dd7e36bb324fd54d1d038aa1df34810ba3"
+  version "0.3.5"
+  sha256 arm:   "6aa434c28a9372bb3befc922662c423be7b91f92d35b6a744846834af8cd1e09",
+         intel: "a911a9b4979cb151e961b54367182f452085efec6f7abc0e7ceb9846b9739c8a"
 
   url "https://storage.googleapis.com/nao-releases/nao/darwin-#{arch}/#{version}/nao.#{arch}.#{version}.dmg",
       verified: "storage.googleapis.com/nao-releases/nao/"
@@ -13,8 +13,14 @@ cask "nao" do
 
   livecheck do
     url "https://storage.googleapis.com/nao-releases/nao/darwin-#{arch}/releases.json"
-    strategy :json do |json|
-      json["currentRelease"]
+    regex(/nao[._-]darwin[._-]#{arch}[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+    strategy :json do |json, regex|
+      json["releases"]&.map do |release|
+        match = release.dig("updateTo", "name")&.match(regex)
+        next if match.blank?
+
+        match[1]
+      end
     end
   end
 
