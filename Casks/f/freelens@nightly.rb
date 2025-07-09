58ec084af06ce64e7ad53b812113bed2a16d178c
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-07-09 13:52:57 +0200

    Bump version

--- Casks/f/freelens@nightly.rb
+++ Casks/f/freelens@nightly.rb
@@ -1,9 +1,9 @@
 cask "freelens@nightly" do
   arch arm: "arm64", intel: "amd64"
 
-  version "1.5.0-0-nightly-2025-07-08"
-  sha256 arm:   "4940545023618d2ff91208a6f5ce608291d4c3927a84b5940cc94505cbe0df4f",
-         intel: "4afcea3ba97236eef04b6060c97362b99e3c86f27766bd04dcf519daf2443df8"
+  version "1.5.0-0-nightly-2025-07-09"
+  sha256 arm:   "ae07fe34f2e1c74d5ba30d293baf8c65deaa0ca8ec9d859183a94f8a03537aab",
+         intel: "3c5a9df61da3b1201d7aa7052c89ae265bf30eee1493c10edde5d7d073d336fa"
 
   url "https://github.com/freelensapp/freelens-nightly-builds/releases/download/v#{version}/Freelens-#{version}-macos-#{arch}.dmg",
       verified: "github.com/freelensapp/freelens-nightly-builds/"
