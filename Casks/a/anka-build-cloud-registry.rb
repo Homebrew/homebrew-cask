4169c200c897c58f2df0d9576f5baaea8e9dadb8
Nathan Pierce <norsegaud@gmail.com>
2025-07-09 15:12:18 -0500

    anka-build-cloud-registry 1.48.0-067b4c29

--- Casks/a/anka-build-cloud-registry.rb
+++ Casks/a/anka-build-cloud-registry.rb
@@ -1,9 +1,9 @@
 cask "anka-build-cloud-registry" do
   arch arm: "arm64", intel: "amd64"
 
-  version "1.47.0-4f4a81ff"
-  sha256 arm:   "1d4d522601373ccf606899151534730748f99e1daf2e4e7093fb7b6aecfb266b",
-         intel: "8b3b1793932465d007f76e47616acfa5ec8c4be135a401298b1276c6600ed58b"
+  version "1.48.0-067b4c29"
+  sha256 arm:   "525343aa5dff3ecfd9dd46390a4407768c41ec483cb20625edf170ef00bfd2cf",
+         intel: "c9e1d0cffdee0d1e0e08a6ac40f2e2b446e78b3c27cd9235b134b6d3f977c375"
 
   url "https://downloads.veertu.com/anka/anka-registry-#{arch}-#{version}.pkg"
   name "Anka Build Cloud Controller"
