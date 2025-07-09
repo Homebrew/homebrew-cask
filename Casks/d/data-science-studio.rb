661d170772925178ebeea5ac7540b86ca18ed73b
Bevan Kay <email@bevankay.me>
2025-07-09 13:58:44 +1000

    data-science-studio: remove `no_autobump`

--- Casks/d/data-science-studio.rb
+++ Casks/d/data-science-studio.rb
@@ -12,8 +12,6 @@ cask "data-science-studio" do
     strategy :electron_builder
   end
 
-  no_autobump! because: :requires_manual_review
-
   depends_on macos: ">= :catalina"
 
   app "DataScienceStudio.app"
