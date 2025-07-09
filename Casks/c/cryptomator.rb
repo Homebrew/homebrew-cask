58c5df4ce103118376d8486fd1728e2f154dc2f7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-09 09:44:59 +0000

    cryptomator 1.17.1

--- Casks/c/cryptomator.rb
+++ Casks/c/cryptomator.rb
@@ -1,9 +1,9 @@
 cask "cryptomator" do
   arch arm: "-arm64", intel: "-x64"
 
-  version "1.17.0"
-  sha256 arm:   "03f45e203204e93b39925cbb04e19c9316da4f77debaba4fb5071f0ec8e727e8",
-         intel: "69f93bafe0707a1210089205a99936da5431cca70fdc5fa290f2b02631270580"
+  version "1.17.1"
+  sha256 arm:   "34c75e5f744bcedb96cf900a7e73ae96a5b39568a2e0994a1239fc59b31bc0d5",
+         intel: "0111ad60e0a9749a63f11eb4eae183aaf05ceb6dde67b8c28bcc8b2447b86850"
 
   url "https://github.com/cryptomator/cryptomator/releases/download/#{version}/Cryptomator-#{version}#{arch}.dmg",
       verified: "github.com/cryptomator/cryptomator/"
