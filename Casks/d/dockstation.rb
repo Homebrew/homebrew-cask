05b87a6579e1483e6ba368b9fb8af75c9b92c9be
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-07-09 20:01:17 -0400

    dockstation: disable

--- Casks/d/dockstation.rb
+++ Casks/d/dockstation.rb
@@ -10,6 +10,7 @@ cask "dockstation" do
   no_autobump! because: :requires_manual_review
 
   deprecate! date: "2024-07-10", because: :unmaintained
+  disable! date: "2025-07-10", because: :unmaintained
 
   depends_on macos: ">= :el_capitan"
 
