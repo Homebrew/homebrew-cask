1e564931bc6fb275c5bea9ac74fcce92e988c82a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-10 13:08:28 +0000

    smartgit 24.1.4

--- Casks/s/smartgit.rb
+++ Casks/s/smartgit.rb
@@ -14,9 +14,9 @@ cask "smartgit" do
     url "https://www.syntevo.com/downloads/smartgit/archive/smartgit-#{arch}-#{version.dots_to_underscores}.dmg"
   end
   on_mojave :or_newer do
-    version "24.1.3"
-    sha256 arm:   "66065ce59a85144238171a73459d24936c71b9a6668574137e54685b51ce45ca",
-           intel: "703dbf7a256ffbb43b4e3ca066208d65f8139a2e1a226468cec028e226679414"
+    version "24.1.4"
+    sha256 arm:   "330274ba9923d64298cad928026c3db69fe3a899dac5e8cb995ccbe6644c48c0",
+           intel: "b44e5450edc9559fc5e5e7b7874318dbdfc682fdc80e4ae410ff8665dc998487"
 
     url "https://www.syntevo.com/downloads/smartgit/smartgit-#{arch}-#{version.dots_to_underscores}.dmg"
   end
