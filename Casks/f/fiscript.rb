f09a44aeecc94b39424d70bc8d6b7c89d9dc9918
Justin Krehel <39449589+krehel@users.noreply.github.com>
2025-07-09 20:01:17 -0400

    fiscript: disable

--- Casks/f/fiscript.rb
+++ Casks/f/fiscript.rb
@@ -9,6 +9,7 @@ cask "fiscript" do
   no_autobump! because: :requires_manual_review
 
   deprecate! date: "2024-07-10", because: :unmaintained
+  disable! date: "2025-07-10", because: :unmaintained
 
   depends_on macos: ">= :sierra"
 
