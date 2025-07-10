660ae1a3ed9708f51f123fec3055f299b323d997
Bevan Kay <email@bevankay.me>
2025-07-10 15:43:16 +1000

    aural: fix checksum
    Co-authored-by: Klaus Hipp <khipp@users.noreply.github.com>

--- Casks/a/aural.rb
+++ Casks/a/aural.rb
@@ -1,6 +1,6 @@
 cask "aural" do
   version "4.0.0-preview28"
-  sha256 "39687b8d068e435ef9253ed366b71250038bfe0927cad52650c5a5cf83cbb04d"
+  sha256 "5cacd6f1f5b6956bdb9c5ff7f9b05ebbb11516010000d1f0bc793e6045a17172"
 
   url "https://github.com/maculateConception/aural-player/releases/download/final/AuralPlayer-#{version}.dmg"
   name "Aural Player"
