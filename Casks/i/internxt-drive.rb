20665c496465c357d09b72aa15c621f5f9136412
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-10 15:55:25 +0000

    internxt-drive 2.5.7.76

--- Casks/i/internxt-drive.rb
+++ Casks/i/internxt-drive.rb
@@ -1,6 +1,6 @@
 cask "internxt-drive" do
-  version "2.5.6.75"
-  sha256 "1ce3f272d9677900c7680afe1b42ac3fe20530371588ea14f2d47bab05150bea"
+  version "2.5.7.76"
+  sha256 "5700e32355119e8d2d5cc061c5c6266126376f1f830bfd8f2b3d3c80be8d2be7"
 
   url "https://github.com/internxt/drive-desktop-macos/releases/download/v#{version}/Internxt_Drive_#{version}.dmg",
       verified: "github.com/internxt/drive-desktop-macos/"
