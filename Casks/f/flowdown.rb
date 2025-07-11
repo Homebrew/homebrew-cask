bd3d2798a3abba37b59aa453b47947585fb15441
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-11 12:51:07 +0000

    flowdown 2.4.349

--- Casks/f/flowdown.rb
+++ Casks/f/flowdown.rb
@@ -1,6 +1,6 @@
 cask "flowdown" do
-  version "2.3.344"
-  sha256 "ca2afcdd50c79a32b6a101a2d13b4fcf5d41d33839121a181e48cb78c33c3742"
+  version "2.4.349"
+  sha256 "1a2926d1a533b233248598acd19aebf449db4409ab4c40a99052514383e0a3db"
 
   url "https://github.com/Lakr233/FlowDown/releases/download/#{version}/FlowDown-v#{version}.zip",
       verified: "github.com/Lakr233/FlowDown/"
