ec698d3aa8dc00649644d9b69fcb300de5d02de9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-13 01:22:51 +0000

    vial 0.7.4

--- Casks/v/vial.rb
+++ Casks/v/vial.rb
@@ -1,6 +1,6 @@
 cask "vial" do
-  version "0.7.3"
-  sha256 "2479ceef2b30f6404962c5bed0d863758c519b7829d755e804ebeb5054689d1d"
+  version "0.7.4"
+  sha256 "3e8ca8c3658e197d04a51260d881d59a1829a6b0a0c0b2a7980aaeb6c61805ad"
 
   url "https://github.com/vial-kb/vial-gui/releases/download/v#{version}/Vial-v#{version}.dmg",
       verified: "github.com/vial-kb/vial-gui/"
