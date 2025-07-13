46c0011e841f19db3d546ae5cd7d48199d3cc115
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-13 00:17:55 +0000

    font-chiron-goround-tc: add
    This commit was created automatically by the [\`google-fonts\`](https://github.com/Homebrew/homebrew-cask/blob/HEAD/.github/workflows/google-fonts.yml) workflow.

---
+++ Casks/font/font-c/font-chiron-goround-tc.rb
@@ -0,0 +1,12 @@
+cask "font-chiron-goround-tc" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/chirongoroundtc/ChironGoRoundTC%5Bwght%5D.ttf"
+  name "Chiron GoRound TC"
+  homepage "https://github.com/chiron-fonts/chiron-go-round-tc"
+
+  font "ChironGoRoundTC[wght].ttf"
+
+  # No zap stanza required
+end
