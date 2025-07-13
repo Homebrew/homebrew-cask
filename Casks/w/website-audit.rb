f2fa999f7c7ad86e63f91419e76162d90ead1e52
Klaus Hipp <khipp@users.noreply.github.com>
2025-07-13 17:11:12 +0200

    website-audit 1.3.0a2

--- Casks/w/website-audit.rb
+++ Casks/w/website-audit.rb
@@ -1,6 +1,6 @@
 cask "website-audit" do
-  version "1.3.0a"
-  sha256 "5f6f4ecd9355a5cba1cd7acb9aa680cb9e6acf3797e6a8ee0c2255ac515e408b"
+  version "1.3.0a2"
+  sha256 "b7f53aa3cef3cd73b22820dc4393da368a8934b608f22d78b70c8f8bdf2ce792"
 
   url "https://code.europa.eu/api/v4/projects/615/packages/generic/wat/#{version}/website-audit-#{version}-universal.dmg"
   name "Website Audit"
@@ -8,7 +8,10 @@ cask "website-audit" do
   homepage "https://code.europa.eu/edpb/website-auditing-tool"
 
   livecheck do
-    url "https://code.europa.eu/edpb/website-auditing-tool.git"
+    url "https://code.europa.eu/api/v4/projects/615/packages/"
+    strategy :json do |json|
+      json.map { |item| item["version"] }
+    end
   end
 
   no_autobump! because: :requires_manual_review
