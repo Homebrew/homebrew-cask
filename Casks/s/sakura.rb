b22283d94eafa88bb6e75ff725d4b46596a37c33
eleanordoesntcode <166862894+eleanordoesntcode@users.noreply.github.com>
2025-07-14 04:21:29 +0200

    sakura 3.1.7

--- Casks/s/sakura.rb
+++ Casks/s/sakura.rb
@@ -1,9 +1,9 @@
 cask "sakura" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "3.1.5"
-  sha256 arm:   "fdee4f83c80dd70cc375a0835dc14653a184d6b7c4b77d8d4443897802065013",
-         intel: "48b71b61a3c9d6a6f2a05f5dcb1e09558137278bfdeff79ded7256f100038e99"
+  version "3.1.7"
+  sha256 arm:   "35cafaaf6261feaee0501ba4dc9d823f1defd5595458869311c566a3d0bd8d4e",
+         intel: "82e57056ef15389650a27bb919c406834d8236883f034665cd2bdef637d98a4c"
 
   url "https://nya.globalslb.net/natfrp/client/launcher-macos/#{version}/SakuraLauncher_macOS_#{arch}.dmg",
       verified: "nya.globalslb.net/natfrp/client/launcher-macos/"
