3d85ce6361edf42badcc395acba4e5000be1618b
eleanordoesntcode <166862894+eleanordoesntcode@users.noreply.github.com>
2025-07-14 03:43:21 +0200

    irpf2025 1.6

--- Casks/i/irpf2025.rb
+++ Casks/i/irpf2025.rb
@@ -1,6 +1,6 @@
 cask "irpf2025" do
-  version "1.5"
-  sha256 "750a60573c6364817ee72c160672ed9fb1a1e77c299d81ca13a8f2f54f218666"
+  version "1.6"
+  sha256 "f5abc15f1ac0ed9f8bb460ccafe96d0e337b51f06d904c85081d9ab5a0f7b380"
 
   url "https://downloadirpf.receita.fazenda.gov.br/irpf/2025/irpf/arquivos/IRPF2025-v#{version}.dmg"
   name "IRPF 2025"
