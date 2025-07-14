bb87f61a185b8f6639999811324d1645ed871d06
Aaron A. Kelley <AaronKelley@users.noreply.github.com>
2025-07-14 10:57:00 -0400

    dotnet-sdk@8 (new cask)
    This cask tracks .NET 8, the prior version of the SDK. (.NET 8 is an
    LTS version, but .NET 9 is not.)

---
+++ Casks/d/dotnet-sdk@8.rb
@@ -0,0 +1,55 @@
+cask "dotnet-sdk@8" do
+  arch arm: "arm64", intel: "x64"
+
+  version "8.0.412"
+  sha256 arm:   "e5abf744d87cb45beac2ee5bfae701850cee2d401eee1c959b707a7f9af516b5",
+         intel: "0bdebfe09858c870114fc28ed576d16dda7e6aa16a5123b7a4423379ea2a1e33"
+
+  url "https://builds.dotnet.microsoft.com/dotnet/Sdk/#{version}/dotnet-sdk-#{version}-osx-#{arch}.pkg"
+  name ".NET SDK"
+  desc "Developer platform"
+  homepage "https://www.microsoft.com/net/core#macos"
+
+  # This identifies releases with the same major/minor version as the current
+  # cask version. New major/minor releases occur annually in November and the
+  # check will automatically update its behavior when the cask is updated.
+  livecheck do
+    url "https://builds.dotnet.microsoft.com/dotnet/release-metadata/#{version.major_minor}/releases.json"
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    strategy :json do |json, regex|
+      json["releases"]&.map do |release|
+        version = release.dig("sdk", "version")
+        next unless version&.match(regex)
+
+        version
+      end
+    end
+  end
+
+  conflicts_with cask: [
+    "dotnet-runtime",
+    "dotnet-runtime@preview",
+    "dotnet-sdk",
+    "dotnet-sdk@preview",
+  ], formula: "dotnet"
+  depends_on macos: ">= :mojave"
+
+  pkg "dotnet-sdk-#{version.csv.first}-osx-#{arch}.pkg"
+  binary "/usr/local/share/dotnet/dotnet"
+
+  uninstall pkgutil: [
+    "com.microsoft.dotnet.*#{version.major_minor}*#{arch}",
+    "com.microsoft.dotnet.sharedhost*#{arch}",
+    "com.microsoft.netstandard.pack.targeting.*",
+  ]
+
+  zap pkgutil: "com.microsoft.dotnet.*",
+      delete:  [
+        "/etc/paths.d/dotnet",
+        "/etc/paths.d/dotnet-cli-tools",
+      ],
+      trash:   [
+        "~/.dotnet",
+        "~/.nuget",
+      ]
+end
