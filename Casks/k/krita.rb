ae621eb47477bca0b0489d101cc336b42a319ae3
botantony <antonsm21@gmail.com>
2025-07-16 17:14:09 +0200

    krita 5.2.11

--- Casks/k/krita.rb
+++ Casks/k/krita.rb
@@ -1,6 +1,6 @@
 cask "krita" do
-  version "5.2.10"
-  sha256 "fac6ccdea070a35be67e31c0b95b582186a3d4703ba43e9f8e8fff10d7440a5a"
+  version "5.2.11"
+  sha256 "c38443656c017e299264473c9d11f4f92aca63b1475bda325a0a0f969973a147"
 
   url "https://download.kde.org/stable/krita/#{version}/krita-#{version}.dmg",
       verified: "download.kde.org/stable/krita/"
@@ -12,8 +12,6 @@ cask "krita" do
     url "https://krita.org/en/download/"
     regex(/href=.*?krita[._-]v?(\d+(?:\.\d+)+)(?:-signed|-release)?\.dmg/i)
   end
-
-  no_autobump! because: :requires_manual_review
 
   depends_on macos: ">= :high_sierra"
 
