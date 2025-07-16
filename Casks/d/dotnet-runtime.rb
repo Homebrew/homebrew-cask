c8cbebfadd6d267781a3505222f5be8274edd347
Aaron A. Kelley <AaronKelley@users.noreply.github.com>
2025-07-16 12:06:01 -0400

    Update requirement for .NET packages to Ventura.
    Current versions of .NET (8 & 9) require macOS 13 "Ventura". See:
    https://github.com/dotnet/core/blob/main/release-notes/8.0/supported-os.md
    https://github.com/dotnet/core/blob/main/release-notes/9.0/supported-os.md

--- Casks/d/dotnet-runtime.rb
+++ Casks/d/dotnet-runtime.rb
@@ -32,7 +32,7 @@ cask "dotnet-runtime" do
     "dotnet-sdk@8",
     "dotnet-sdk@preview",
   ], formula: "dotnet"
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= :ventura"
 
   pkg "dotnet-runtime-#{version.csv.first}-osx-#{arch}.pkg"
   binary "/usr/local/share/dotnet/dotnet"
