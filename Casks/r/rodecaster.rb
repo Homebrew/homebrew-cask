61b0fb223fd526203a9f9086fde4c9febf0e5901
Marcus Maxwell <mindfulmonk@users.noreply.github.com>
2025-07-16 07:36:08 +0100

    added liveness and preflight

--- Casks/r/rodecaster.rb
+++ Casks/r/rodecaster.rb
@@ -1,5 +1,5 @@
 cask "rodecaster" do
-  version "2.0.94-145"
+  version "2.0.94"
   sha256 :no_check
 
   url "https://update.rode.com/rc-app/RODECaster_App_MACOS.zip"
@@ -7,9 +7,20 @@ cask "rodecaster" do
   desc "Easily manage your RØDECaster or Streamer X setup"
   homepage "https://rode.com/en/apps/rodecaster-app"
 
+  livecheck do
+    url "https://update.rode.com/rode-devices-manifest.json"
+    strategy :json do |json|
+      json.dig("rc-app-manifest", "macos", "main-version", "update-version")
+    end
+  end
+
   depends_on macos: ">= :catalina"
 
-  pkg "RØDECaster App (#{version}).pkg"
+  pkg "RØDECaster App.pkg"
+
+  preflight do
+    staged_path.glob("RØDECaster App*.pkg")&.first&.rename(staged_path/"RØDECaster App.pkg")
+  end
 
   uninstall quit:    "com.rode.rodecastercomp",
             pkgutil: "com.rodecastercomp.installer"
