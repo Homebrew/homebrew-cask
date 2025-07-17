48a5ef55b64710ed19b5a33a7afbc6411b85e417
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-17 22:05:13 +0000

    superwhisper 2.1.2

--- Casks/s/superwhisper.rb
+++ Casks/s/superwhisper.rb
@@ -1,6 +1,6 @@
 cask "superwhisper" do
-  version "2.1.1"
-  sha256 "ab3f9e3eae173439b4fea7222f50c0c9fdfe008598360b732df7a2f6ecac2880"
+  version "2.1.2"
+  sha256 "d3dfce5e1d7c3409fc68603c3a701abbd379f901d301cabcf0b812774cb6cf17"
 
   url "https://builds.superwhisper.com/v#{version}/superwhisper.zip"
   name "superwhisper"
