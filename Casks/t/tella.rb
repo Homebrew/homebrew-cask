086b1ddcd761b84f5817f0086274c0fa9f185a7e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-17 13:24:48 +0000

    tella 1.65

--- Casks/t/tella.rb
+++ Casks/t/tella.rb
@@ -1,6 +1,6 @@
 cask "tella" do
-  version "1.64,177"
-  sha256 "414f80f39c7cbc0706f60be19d0bbe7292952502f0a597a2a9494ffcd795dcbf"
+  version "1.65,178"
+  sha256 "f5a383f6e54a6630d363341947ce610e87919e4ebdd19d7b0940fe57618fb6e7"
 
   url "https://mac.tella.tv/Tella-#{version.csv.first}-#{version.csv.second}.dmg"
   name "Tella"
