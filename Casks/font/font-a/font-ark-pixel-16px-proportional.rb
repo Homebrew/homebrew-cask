f1d6d8a5b84574dc936faf773fc9d4e1a4f1565d
TakWolf <takwolf@foxmail.com>
2025-07-17 17:27:50 +0800

    font-ark-pixel-16px-proportional 2025.07.17
    font-ark-pixel-16px-proportional: remove no_autobump

--- Casks/font/font-a/font-ark-pixel-16px-proportional.rb
+++ Casks/font/font-a/font-ark-pixel-16px-proportional.rb
@@ -1,12 +1,10 @@
 cask "font-ark-pixel-16px-proportional" do
-  version "2025.03.14"
-  sha256 "9d8174e9bebf664a06b49651ba08b1e46ce4bea54e91fcacd429c61c6d4f7b23"
+  version "2025.07.17"
+  sha256 "6abde5a2659e2a7f0da953534a49edbbf1e532d32a9d03cc14e6cdacc0f63b6c"
 
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-16px-proportional-otf-v#{version}.zip"
   name "Ark Pixel 16px Proportional"
   homepage "https://github.com/TakWolf/ark-pixel-font"
-
-  no_autobump! because: :requires_manual_review
 
   font "ark-pixel-16px-proportional-ja.otf"
   font "ark-pixel-16px-proportional-ko.otf"
