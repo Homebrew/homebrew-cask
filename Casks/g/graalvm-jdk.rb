55e24a1c07d48916cad3bfef84d6333e352dea53
Matt Coster <opensource@mtcoster.net>
2025-07-17 11:35:15 +0100

    graalvm-jdk 24.0.2,12

--- Casks/g/graalvm-jdk.rb
+++ Casks/g/graalvm-jdk.rb
@@ -1,9 +1,9 @@
 cask "graalvm-jdk" do
   arch arm: "aarch64", intel: "x64"
 
-  version "24.0.1,9"
-  sha256 arm:   "875555f6063b4847b617504e8f8a36290a6726770be72388261f6118bcf28f81",
-         intel: "35852ef7040be5e73d4b8f23481c4f70e4dcb088d302e28f6a88e6a4d47de2b9"
+  version "24.0.2,12"
+  sha256 arm:   "2dc7634ed939c725d41a353f73443ff32e70f2fb577367f12a6936107e3494dc",
+         intel: "df0f9e5d101201c50bfa6baa3c657a81a2883c5b5097a9f5ab93dab86f7e3ba2"
 
   url "https://download.oracle.com/graalvm/#{version.major}/archive/graalvm-jdk-#{version.csv.first}_macos-#{arch}_bin.tar.gz",
       verified: "download.oracle.com/"
