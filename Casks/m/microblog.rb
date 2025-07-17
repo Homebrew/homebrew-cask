8d0d44a3b3163c489111e226deb450c11b1c510d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-17 21:53:20 +0000

    microblog 3.6.1

--- Casks/m/microblog.rb
+++ Casks/m/microblog.rb
@@ -1,6 +1,6 @@
 cask "microblog" do
-  version "3.6"
-  sha256 "94233fe65a0974c813198c8c5c57e7edf74aedc0ca870c048c648d60d9abc992"
+  version "3.6.1"
+  sha256 "226d0a74260fd974d6109e91b014c2532e11589d683ecdcae5dbfcc70a88a52f"
 
   url "https://s3.amazonaws.com/micro.blog/mac/Micro.blog_#{version}.zip",
       verified: "s3.amazonaws.com/micro.blog/mac/"
